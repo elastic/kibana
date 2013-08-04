b9f0307d4bacfbb352e5c0d7856fa08787927ad4
areman <andre.kullmann@googlemail.com>
2013-08-04 14:10:31 +0200

    add a plain text filter panel

---
+++ panels/condition/module.js
@@ -0,0 +1,65 @@
+/*jshint globalstrict:true */
+/*global angular:true */
+/*
+
+  ## Conditions
+  
+  a simple text field to input a filter expression.
+  
+  Author: Andre Kullmann
+
+  ### Parameters
+  * label ::  The label to stick over the field 
+*/
+
+'use strict';
+
+angular.module('kibana.condition', [])
+.controller('condition', function($scope, dashboard, filterSrv) {
+
+  $scope.panelMeta = {
+    status  : "Experimental",
+    description : ""
+  };
+
+  // Set and populate defaults
+  var _d = {
+    label   : "Condition",
+    history : [],
+    remember: 10 // max: 100, angular strap can't take a variable for items param
+  };
+  _.defaults($scope.panel,_d);
+  
+  $scope.filterSrv = filterSrv;
+
+  $scope.init = function() {
+    $scope.conditionId     = null;
+  };
+
+  var isBlank = function(str) {
+    return (!str || /^\s*$/.test(str));
+  }
+  
+  $scope.refresh = function() {
+	var id  = $scope.conditionId;
+	var blank = isBlank( $scope.conditionString );
+	
+	if( id != null && blank ) {
+	  filterSrv.remove( id );
+	} else if( id == null ) {
+		id = filterSrv.set({
+		  type:  'querystring',
+		  query: $scope.conditionString
+		} );	
+	} else {	
+		id = filterSrv.set({
+		  type:  'querystring',
+		  query: $scope.conditionString
+		}, id );
+	}
+	$scope.conditionId = id;
+	
+	dashboard.refresh();	
+  };
+
+});
