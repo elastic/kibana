6b16b96a20c152b5b3236dbc4a94656d3896d4b5
Tom Maiaroto <tom@shift8creative.com>
2013-11-24 22:27:26 -0600

    adding a number format filter

--- src/app/filters/all.js
+++ src/app/filters/all.js
@@ -129,4 +129,29 @@ define(['angular', 'jquery', 'underscore', 'moment'], function (angular, $, _, m
     };
   });
 
+  module.filter('numberFormat', function() {
+    return function(number, decimals, decPoint, thousandsSep) {
+      number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+      var n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep,
+      dec = (typeof decPoint === 'undefined') ? '.' : decPoint,
+      s = '',
+      toFixedFix = function (n, prec) {
+        var k = Math.pow(10, prec);
+        return '' + Math.round(n * k) / k;
+      };
+      // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+      s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+      if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+      }
+      if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+      }
+      return s.join(dec);
+    };
+  });
+
 });
