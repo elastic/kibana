bbffbad39bc4b614b97fef5e4acc243f477ba11a
Evan Tahler <evan@taskrabbit.com>
2013-12-17 14:26:04 -0800

    set default host as local + https

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ function (Settings) {
      * elasticsearch installed on. You probably want to set it to the FQDN of your
      * elasticsearch host
      */
-    elasticsearch: "http://"+window.location.hostname+":9200",
+    elasticsearch: "https://"+window.location.hostname,
 
     /** @scratch /configuration/config.js/5
      * ==== default_route
