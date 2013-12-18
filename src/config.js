94616a696b0fe3e8d8eeff9ce1681c92093e6805
Evan Tahler <evan@taskrabbit.com>
2013-12-18 15:07:16 -0800

    force port 5000:

--- src/config.js
+++ src/config.js
@@ -21,7 +21,8 @@ function (Settings) {
      * elasticsearch installed on. You probably want to set it to the FQDN of your
      * elasticsearch host
      */
-    elasticsearch: window.location.protocol + "//" + window.location.hostname,
+    // elasticsearch: window.location.protocol + "//" + window.location.hostname,
+    elasticsearch: window.location.protocol + "//" + window.location.hostname + ":5000",
 
     /** @scratch /configuration/config.js/5
      * ==== default_route
