937a392e5d0d6e1bf9dd633bcf086b52e7b16e8a
Boaz Leskes <b.leskes@gmail.com>
2014-01-06 14:00:44 +0100

    added the option to upload current packages as marvel/latest

--- tasks/options/s3.js
+++ tasks/options/s3.js
@@ -1,19 +1,36 @@
-module.exports = function (config) {
+module.exports = function (config, grunt) {
+  var upload;
+
+  if (grunt.option('latest')) {
+    upload = [
+      {
+        src: '<%= packageDir %>/<%= pkg.name %>-<%= pkg.version %>.zip',
+        dest: 'elasticsearch/<%= pkg.name %>/<%= pkg.name %>-latest.zip',
+      },
+      {
+        src: '<%= packageDir %>/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
+        dest: 'elasticsearch/<%= pkg.name %>/<%= pkg.name %>-latest.tar.gz',
+      }
+    ];
+  } else {
+    upload = [
+      {
+        src: '<%= packageDir %>/<%= pkg.name %>-<%= pkg.version %>.zip',
+        dest: 'elasticsearch/<%= pkg.name %>/<%= pkg.name %>-<%= pkg.version %>.zip',
+      },
+      {
+        src: '<%= packageDir %>/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
+        dest: 'elasticsearch/<%= pkg.name %>/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
+      }
+    ];
+  }
+
   return {
     release: {
       bucket: 'download.elasticsearch.org',
       access: 'private',
       //debug: true, // uncommment to prevent actual upload
-      upload: [
-        {
-          src: '<%= packageDir %>/<%= pkg.name %>-<%= pkg.version %>.zip',
-          dest: 'elasticsearch/<%= pkg.name %>/<%= pkg.name %>-<%= pkg.version %>.zip',
-        },
-        {
-          src: '<%= packageDir %>/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
-          dest: 'elasticsearch/<%= pkg.name %>/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
-        }
-      ]
+      upload: upload
     }
   };
 };
