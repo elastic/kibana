e997c0a7a54a588aabff3d05828db869f3a20835
nelson <nelson@wikia-inc.com>
2014-01-13 10:01:39 -0800

    setting config to datalog-s1

--- src/config.js
+++ src/config.js
@@ -19,7 +19,7 @@ function (Settings) {
      * @type {String}
      */
     elasticsearch: {
-      server: "http://search.kibana.nelson.wikia-dev.com",
+      server: "http://search.kibana.datalog-s1.prod.wikia.net",
       withCredentials: true
     },
 
