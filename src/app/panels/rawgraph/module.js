ab450b65d206f69148d3f93c5b6b53c7034604df
Benoit Dunand-Laisin$ <benoit.dunand-laisin@prologism.fr>
2014-01-17 16:33:09 +0100

    Update for grunt

--- src/app/panels/rawgraph/module.js
+++ src/app/panels/rawgraph/module.js
@@ -156,7 +156,7 @@ function (angular, app, $, _, kbn, moment, numeral) {
       /** @scratch /panels/rawgraph/5
        * colors:: Colors of the series
        */
-     colors          : [
+      colors        : [
         "#7EB26D","#EAB839","#6ED0E0","#EF843C","#E24D42","#1F78C1","#BA43A9","#705DA0",
         "#508642","#CCA300","#447EBC","#C15C17","#890F02","#0A437C","#6D1F62","#584477",
         "#B7DBAB","#F4D598","#70DBED","#F9BA8F","#F29191","#82B5D8","#E5A8E2","#AEA2E0",
