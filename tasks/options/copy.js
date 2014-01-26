fb17f73dfa0afe74b19ec0c96a01eb5ce12d777c
Spencer Alger <spencer@spenceralger.com>
2014-01-25 17:50:13 -0700

    Removed the tests from the build

--- tasks/options/copy.js
+++ tasks/options/copy.js
@@ -1,10 +1,10 @@
 module.exports = function(config) {
   return {
     // copy source to temp, we will minify in place for the dist build
-    everything_but_less_to_temp: {
+    almost_everything_to_temp: {
       cwd: '<%= srcDir %>',
       expand: true,
-      src: ['**/*', '!**/*.less'],
+      src: ['**/*', '!**/*.less', '!test/**/*'],
       dest: '<%= tempDir %>'
     }
   };
