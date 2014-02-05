08d2e9d339bd0d9be1115d3b94b2be8c12913955
Christian Mikkelsen <chmi@telenor.dk>
2014-02-05 11:36:31 +0100

    Update PreDeploy.ps1

--- deploy_scripts/PreDeploy.ps1
+++ deploy_scripts/PreDeploy.ps1
@@ -1,1 +1,23 @@
-.\Add-IISMimeType "application/json" ".json"
+function Add-IISMimeType
+{
+  param(
+    [Parameter(Mandatory=$true)][string] $mimeType,
+    [Parameter(Mandatory=$true)][string] $mimeFileExtension
+  )
+  Import-Module WebAdministration
+  
+  $mimePresent =  get-webconfigurationproperty //staticContent -name collection | Where { $_.mimeType -eq $mimeType -And $_.fileExtension -eq $mimeFileExtension }
+  
+  if ( $mimePresent -eq $null)
+  {
+  	add-webconfigurationproperty //staticContent -name collection -value @{fileExtension=$mimeFileExtension; mimeType=$mimeType}
+  	Write-Host "MimeType added to IIS: '$mimeType', '$mimeFileExtension'"
+  }
+  else
+  {
+  	Write-Host "MimeType allready present in IIS: '$mimeType', '$mimeFileExtension'"
+  }
+}
+
+Add-IISMimeType "application/json" ".json"
+
