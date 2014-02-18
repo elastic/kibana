8eebd792c1253cf9a5f258d4ae544cbeee674629
Rashid Khan <rashidkpc@gmail.com>
2014-02-18 13:32:49 -0700

    - Add mapper to courier - Add errors for mapper - Add mapper to resolve and cache field mappings - Add a couple tests for mapper - Add some debug output to the kibana controller

--- src/courier/errors.js
+++ src/courier/errors.js
@@ -9,4 +9,23 @@ define(function (require, module, exports) {
   HastyRefresh.prototype = new Error();
   HastyRefresh.prototype.constructor = HastyRefresh;
   exports.HastyRefresh = HastyRefresh;
+
+  // a non-critical cache write to elasticseach failed
+  function CacheWriteFailure() {
+    this.name = 'CacheWriteFailure';
+    this.message = 'A Elasticsearch cache write has failed.';
+  }
+  CacheWriteFailure.prototype = new Error();
+  CacheWriteFailure.prototype.constructor = CacheWriteFailure;
+  exports.CacheWriteFailure = CacheWriteFailure;
+
+  // when a field mapping is requested for an unknown field
+  function FieldNotFoundInCache(name) {
+    this.name = 'FieldNotFoundInCache';
+    this.message = 'The ' + name + ' field was not found in the cached mappings';
+  }
+  FieldNotFoundInCache.prototype = new Error();
+  FieldNotFoundInCache.prototype.constructor = FieldNotFoundInCache;
+  exports.FieldNotFoundInCache = FieldNotFoundInCache;
+
 });
