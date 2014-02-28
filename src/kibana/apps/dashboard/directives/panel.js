5e6cc7979de798de92c4ce425875a5a14ca5444a
Rashid Khan <rashidkpc@gmail.com>
2014-02-28 08:55:27 -0700

    Break out index.js into seperate directories and files

---
+++ src/kibana/apps/dashboard/directives/panel.js
@@ -0,0 +1,18 @@
+define(function (require) {
+  var angular = require('angular');
+
+  var app = angular.module('app/dashboard');
+
+  app.directive('dashboardPanel', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        params: '@'
+      },
+      compile: function (elem, attrs) {
+        var params = JSON.parse(attrs.params);
+        elem.replaceWith(params.type + '<i class="link pull-right fa fa-times remove" />');
+      }
+    };
+  });
+});
