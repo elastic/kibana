ad44c47ca63b62105438fedb1b02086134895193
Rashid Khan <rashidkpc@gmail.com>
2014-04-02 21:21:19 -0700

    Added field-name directive that appends type icon

---
+++ src/kibana/directives/field_name.js
@@ -0,0 +1,38 @@
+define(function (require) {
+  var module = require('modules').get('kibana/directives');
+  var $ = require('jquery');
+  var _ = require('lodash');
+
+  module.directive('fieldName', function ($compile) {
+    return {
+      restrict: 'A',
+      scope: {
+        fieldName: '=',
+        fieldType: '='
+      },
+      link: function ($scope, $elem, attrs) {
+
+        var typeIcon = function (fieldType) {
+          switch (fieldType)
+          {
+          case 'source':
+            return $('<i class="fa fa-file-text-o"></i> ');
+          case 'string':
+            return $('<i><strong>t</strong></i> ');
+          case 'number':
+            return $('<i><strong>#</strong></i> ');
+          case 'date':
+            return $('<i class="fa fa-clock-o"></i> ');
+          case 'ip':
+            return $('<i class="fa fa-laptop"></i> ');
+          default:
+          }
+        };
+
+        var icon = typeIcon($scope.fieldType).addClass('text-muted').css({'margin-right': '5px'});
+
+        $elem.text($scope.fieldName).prepend(icon);
+      }
+    };
+  });
+});
