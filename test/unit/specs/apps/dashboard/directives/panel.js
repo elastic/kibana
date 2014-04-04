cd15b0e9e6eaea604c2737955e8af3db189d4da3
Rashid Khan <rashidkpc@gmail.com>
2014-04-04 13:28:19 -0700

    Dashboard app controller and directive tests

---
+++ test/unit/specs/apps/dashboard/directives/panel.js
@@ -0,0 +1,51 @@
+define(function (require) {
+  var angular = require('angular');
+  var mocks = require('angular-mocks');
+  var _ = require('lodash');
+  var $ = require('jquery');
+
+  // Load the kibana app dependencies.
+  require('angular-route');
+
+  // Load the code for the directive
+  require('apps/dashboard/directives/panel');
+
+  describe('Dashboard panels', function () {
+    var $scope, $elem;
+
+    beforeEach(function () {
+      module('app/dashboard');
+
+      // Create the scope
+      inject(function ($rootScope, $compile) {
+
+        $scope = $rootScope;
+
+        var params = {
+          type: 'new'
+        };
+
+        $elem = angular.element(
+          '<dashboard-panel params=\'' + JSON.stringify(params) + '\'></dashboard-panel>'
+        );
+
+        $compile($elem)($scope);
+        $scope.$digest();
+      });
+
+    });
+
+    it('should have a close button', function (done) {
+      var closeIcon = $elem.find('i.remove');
+      expect(closeIcon.length).to.be(1);
+      done();
+    });
+
+    it('should have the name of the panel', function (done) {
+      expect($elem.text()).to.be('new');
+      done();
+    });
+
+  });
+
+});
