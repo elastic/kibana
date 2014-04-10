b00ee3a43ff6038f0b04a1305b4ac4ab116c0bab
Rashid Khan <rashidkpc@gmail.com>
2014-04-10 10:59:44 -0700

    Document savedDashboards service, move sidebar styles out of discover

--- src/kibana/apps/dashboard/services/saved_dashboards.js
+++ src/kibana/apps/dashboard/services/saved_dashboards.js
@@ -1,10 +1,16 @@
 define(function (require) {
   var module = require('modules').get('app/dashboard');
 
+ // bring in the factory
   require('../factories/saved_dashboard');
 
+  // This is the only thing that gets injected into controllers
   module.service('savedDashboards', function (SavedDashboard) {
+
+    // Returns a single dashboard by ID, should be the name of the dashboard
     this.get = function (id) {
+
+      // Returns a promise that contains a dashboard which is a subclass of docSource
       return (new SavedDashboard(id)).init();
     };
   });
