365a44ef316140b97eb31b6006f53fd5a1caf137
Spencer Alger <spencer@spenceralger.com>
2014-04-11 16:04:50 -0700

    half started vizard for the visualize app

---
+++ src/kibana/apps/visualize/controllers/editor.js
@@ -0,0 +1,123 @@
+define(function (require) {
+  var _ = require('lodash');
+
+  require('../saved_visualizations/saved_visualizations');
+  require('notify/notify');
+
+  var app = require('modules').get('app/visualize', [
+    'kibana/notify',
+    'kibana/courier'
+  ]);
+
+  var aggs = require('../saved_visualizations/_aggs');
+  var visConfigCategories = require('../saved_visualizations/_config_categories');
+
+  app.controller('VisualizeEditor', function ($route, $scope, courier, createNotifier, config, $location) {
+    var notify = createNotifier({
+      location: 'Visualization Editor'
+    });
+
+    var vis = $route.current.locals.vis;
+
+    $scope.refreshFields = function () {
+      $scope.fields = null;
+      vis.dataSource.clearFieldCache().then(getFields, notify.error);
+    };
+
+    function getFields() {
+      return vis.dataSource.getFields()
+      .then(function (fieldsHash) {
+        // create a sorted list of the fields for display purposes
+        $scope.fields = _(fieldsHash)
+          .keys()
+          .sort()
+          .transform(function (fields, name) {
+            var field = fieldsHash[name];
+            field.name = name;
+            fields.push(field);
+          })
+          .value();
+
+        $scope.fields.byName = fieldsHash;
+      });
+    }
+
+    getFields();
+
+    $scope.vis = vis;
+    $scope.aggs = aggs;
+    $scope.visConfigCategories = visConfigCategories;
+
+    $scope.$on('change:config.defaultIndex', function () {
+      if (!vis.dataSource.get('index')) {
+        vis.dataSource.index(config.get('defaultIndex'));
+        getFields();
+      }
+    });
+
+    var updateDataSource = function () {
+      notify.event('update data source');
+
+      var config = vis.getConfig();
+
+      config = _.groupBy(config, function (config) {
+        switch (config.categoryName) {
+        case 'group':
+        case 'segment':
+          return 'dimension';
+        default:
+          return config.categoryName;
+        }
+      });
+
+      if (!config.dimension) {
+        // use the global aggregation if we don't have any dimensions
+        config.dimension = [{
+          agg: 'global'
+        }];
+      }
+
+      var dsl = {};
+      var i = 0;
+
+      var nest = (function () {
+        var current = dsl;
+        return function (config) {
+          current.aggs = {};
+          var key = '_agg_' + (i++);
+
+          var aggDsl = {};
+          aggDsl[config.agg] = config.aggParams;
+
+          current = current.aggs[key] = aggDsl;
+        };
+      }());
+
+      config.split && config.split.forEach(nest);
+      config.dimension && config.dimension.forEach(nest);
+      config.metric && config.metric.forEach(nest);
+
+      notify.log('config', config);
+      notify.log('aggs', dsl.aggs);
+
+      vis.dataSource.aggs(dsl.aggs);
+      notify.event('update data source', true);
+    };
+
+    /*********
+     *** BUTTON EVENT HANDLERS
+     *********/
+    $scope.doVisualize = function () {
+      updateDataSource();
+      vis.dataSource.fetch();
+    };
+    $scope.doSave = function () {
+      updateDataSource();
+      vis.save()
+      .then(function () {
+        $location.url('/visualize/' + vis.typeName + '/' + vis.get('id'));
+      }, notify.fatal);
+    };
+
+  });
+});
---
+++ src/kibana/apps/visualize/saved_visualizations/_config_categories.js
@@ -0,0 +1,61 @@
+define(function (require) {
+  var _ = require('lodash');
+  var categories = [
+    {
+      name: 'segment',
+      displayOrder: 2,
+      fetchOrder: 1,
+      defaults: {
+        min: 0,
+        max: Infinity
+      },
+      configDefaults: {
+        size: 5
+      }
+    },
+    {
+      name: 'metric',
+      displayOrder: 1,
+      fetchOrder: 2,
+      defaults: {
+        min: 0,
+        max: 1
+      },
+      configDefaults: {
+        agg: 'count'
+      }
+    },
+    {
+      name: 'group',
+      displayOrder: 3,
+      fetchOrder: 3,
+      defaults: {
+        min: 0,
+        max: 1
+      },
+      configDefaults: {
+        global: true,
+        size: 5
+      }
+    },
+    {
+      name: 'split',
+      displayOrder: 4,
+      fetchOrder: 4,
+      defaults: {
+        min: 0,
+        max: 2
+      },
+      configDefaults: {
+        size: 5,
+        row: true
+      }
+    }
+  ];
+
+  categories.fetchOrder = _.sortBy(categories, 'fetchOrder');
+  categories.displayOrder = _.sortBy(categories, 'displayOrder');
+  categories.byName = _.indexBy(categories, 'name');
+
+  return categories;
+});
