fb463a420c91915befc8633e7aa68ee0b7bbc63c
Rashid Khan <rashidkpc@gmail.com>
2014-04-11 09:36:12 -0700

    setting wip

--- src/kibana/apps/settings/controllers/settings.js
+++ src/kibana/apps/settings/controllers/settings.js
@@ -4,7 +4,7 @@ define(function (require) {
   var app = require('modules').get('app/settings');
 
   require('services/state');
-  //require('../services/indices_pattern');
+  require('../services/patterns');
   require('directives/fixed_scroll');
 
   var navHtml = require('text!../partials/nav.html');
@@ -12,6 +12,8 @@ define(function (require) {
   // Grab the html and controllers for each section
   var sections = {
     indices: require('text!../partials/indices.html'),
+    newIndex: require('text!../partials/indices.html'),
+
   };
 
   // Order them correctly in the nav
@@ -27,27 +29,51 @@ define(function (require) {
 
   require('routes')
   .when('/settings', {
-    redirectTo: '/settings/general'
+    redirectTo: '/settings/indices'
+  })
+  .when('/settings/indices', {
+    template: template('indices'),
+    reloadOnSearch: false
   })
   .when('/settings/indices/:id?', {
     template: template('indices'),
+    /*
     resolve: {
-      index: function (indexPattern, $route) {
-        console.log(indexPattern);
-        //return indexPattern.get($route.current.params.id);
+      index: function (indexPatterns, $route) {
+        console.log(indexPatterns);
+        return indexPatterns.get($route.current.params.id);
       }
     },
+    */
     reloadOnSearch: false
   });
 
 
-  app.controller('settings', function ($scope, config, courier, createNotifier, state, $routeParams) {
+  app.controller('settings', function ($scope, config, courier, createNotifier, state, $route, $routeParams, es) {
+
+    console.log(config);
+    console.log(es);
+
+    var init = function () {
+      $scope.indices = {};
+      if (!$routeParams.id) {
+        $scope.indices.view = 'addPattern';
+      }
+    };
+
+    $scope.addPattern = function (pattern) {
+      console.log('adding');
+      var source = courier.createSource('search').index(pattern);
+      console.log(source);
+    };
 
     $scope.sectionList = sectionList;
-
     $scope.activeTab = function (tabName) {
-      return ($routeParams.section === tabName) ? 'active' : '';
+      return false;
+      //return ($routeParams.section === tabName) ? 'active' : '';
     };
+
+    init();
 
     $scope.$emit('application.load');
   });
