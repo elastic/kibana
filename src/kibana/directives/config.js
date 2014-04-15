f804a456a427f7575e7e18ec2eb1bbe8971ce8dc
Spencer Alger <spencer@spenceralger.com>
2014-04-15 15:17:28 -0700

    added ConfigTemplate object for wrapping a collection of templates, which can be toggled for display in the config panel

--- src/kibana/directives/config.js
+++ src/kibana/directives/config.js
@@ -1,6 +1,7 @@
 define(function (require) {
   var html = require('text!partials/table.html');
   var _ = require('lodash');
+  var ConfigTemplate = require('utils/config_template');
 
   var module = require('modules').get('kibana/directives');
 
@@ -25,27 +26,32 @@ define(function (require) {
       link: function ($scope, element, attr) {
         $scope[attr.configObject] = $scope.configObject;
 
-        $scope.$watch('configTemplate', function (newTemplate, oldTemplate) {
-          if (_.isString($scope.configTemplate) && oldTemplate !== newTemplate) {
-
-            var template = '' +
-              '<div class="config" ng-show="configTemplate">' +
-              '  <form role="form" class="container-fluid" ng-submit="configSubmit()">' +
-                $scope.configTemplate +
-              '  </form>' +
-              '  <div class="config-close remove" ng-click="close()">' +
-              '    <i class="fa fa-chevron-up"></i>' +
-              '  </div>' +
-              '</div>' +
-              '';
-
-            element.html($compile(template)($scope));
+        $scope.$watch('configTemplate.current || configTemplate', function (newTemplate, oldTemplate) {
+          var tmpl = $scope.configTemplate;
+          if (tmpl instanceof ConfigTemplate) {
+            tmpl = tmpl.toString();
           }
+
+          element.html(!tmpl ? '' : $compile('' +
+            '<div class="config" ng-show="configTemplate">' +
+            '  <form role="form" class="container-fluid" ng-submit="configSubmit()">' +
+              tmpl +
+            '  </form>' +
+            '  <div class="config-close remove" ng-click="close()">' +
+            '    <i class="fa fa-chevron-up"></i>' +
+            '  </div>' +
+            '</div>' +
+            ''
+          )($scope));
         });
 
         $scope.close = function () {
           if (_.isFunction($scope.configClose)) $scope.configClose();
-          $scope.configTemplate = undefined;
+          if ($scope.configTemplate instanceof ConfigTemplate) {
+            $scope.configTemplate.current = null;
+          } else {
+            $scope.configTemplate = null;
+          }
         };
       }
     };
