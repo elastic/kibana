45420d695308dc2ff505b2ff68ac7fc88510dd77
Spencer Alger <spencer@spenceralger.com>
2014-04-17 15:42:13 -0700

    switched out Dashboard loader with savedObjectFinder

--- src/kibana/apps/dashboard/services/saved_dashboards.js
+++ src/kibana/apps/dashboard/services/saved_dashboards.js
@@ -5,13 +5,38 @@ define(function (require) {
   require('./_saved_dashboard');
 
   // This is the only thing that gets injected into controllers
-  module.service('savedDashboards', function (SavedDashboard) {
+  module.service('savedDashboards', function (SavedDashboard, config, es) {
 
     // Returns a single dashboard by ID, should be the name of the dashboard
     this.get = function (id) {
 
       // Returns a promise that contains a dashboard which is a subclass of docSource
       return (new SavedDashboard(id)).init();
+    };
+
+    this.find = function (searchString) {
+      return es.search({
+        index: config.file.kibanaIndex,
+        type: 'dashboard',
+        body: {
+          query: {
+            multi_match: {
+              query: searchString || '',
+              type: 'phrase_prefix',
+              fields: ['title^3', 'description'],
+              zero_terms_query: 'all'
+            }
+          }
+        }
+      })
+      .then(function (resp) {
+        return resp.hits.hits.map(function (hit) {
+          var source = hit._source;
+          source.id = hit._id;
+          source.url = '#/dashboard/' + hit._id;
+          return source;
+        });
+      });
     };
   });
 });
