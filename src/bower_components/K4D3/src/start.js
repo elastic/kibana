274fce4541a5807d74e76a0c1226bf8cab7f603e
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-04-29 09:14:02 -0700

    resolving issues

--- src/bower_components/K4D3/src/start.js
+++
@@ -1,7 +0,0 @@
-(function(root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define([], factory);
-    } else {
-        root.k4 = factory();
-    }
-}(this, function() {
