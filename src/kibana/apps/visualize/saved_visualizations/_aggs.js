1f70d05b6dcace9539d7bd2b2c9c3babe6d6467e
Spencer Alger <spencer@spenceralger.com>
2014-04-29 13:26:14 -0700

    default min_doc_count for the date_histogram is now 0, force the default if it is empty

--- src/kibana/apps/visualize/saved_visualizations/_aggs.js
+++ src/kibana/apps/visualize/saved_visualizations/_aggs.js
@@ -112,7 +112,8 @@ define(function (require) {
           custom: true
         },
         min_doc_count: {
-          custom: true
+          custom: true,
+          default: 0
         },
         extended_bounds: {
           custom: true
