3e063bf80ca7956ba2d8087831557a8754b45bf7
Spencer Alger <spencer@spenceralger.com>
2014-05-05 17:15:23 -0700

    updated elasticsearch-js and added script for doing so to package.json

--- src/bower_components/elasticsearch/elasticsearch.angular.js
+++ src/bower_components/elasticsearch/elasticsearch.angular.js
@@ -1,4 +1,4 @@
-/*! elasticsearch - v2.1.4 - 2014-05-02
+/*! elasticsearch - v2.1.4 - 2014-05-05
  * http://www.elasticsearch.org/guide/en/elasticsearch/client/javascript-api/current/index.html
  * Copyright (c) 2014 ; Licensed Apache 2.0 */
 ;(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
