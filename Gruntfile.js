581ae6d80492f2baca8f4a86cb0b5d3a7673bbef
Spencer Alger <spencer@spenceralger.com>
2014-05-07 13:04:52 -0700

    use git to manage esjs and k4d3 deps.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,13 @@ module.exports = function (grunt) {
       app: __dirname + '/src/kibana',
       unitTestDir: __dirname + '/test/unit',
       testUtilsDir: __dirname + '/test/utils',
+      bowerComponentsDir: __dirname + '/src/bower_components',
+
+      k4d3Repo: 'git@github.com:elasticsearch/K4D3.git',
+      k4d3Dir: '<%= bowerComponentsDir %>/K4D3',
+      esjsRepo: 'git@github.com:elasticsearch/elasticsearch-js.git',
+      esjsDir: '<%= bowerComponentsDir %>/elasticsearch',
+
       meta: {
         banner: '/*! <%= package.name %> - v<%= package.version %> - ' +
           '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
