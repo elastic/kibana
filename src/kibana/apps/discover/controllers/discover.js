44bbaf2e50b500a17d4f3cc10bfe1ace3e010a8f
Spencer Alger <spencer@spenceralger.com>
2014-05-21 16:06:17 -0700

    fixed typo

--- src/kibana/apps/discover/controllers/discover.js
+++ src/kibana/apps/discover/controllers/discover.js
@@ -118,7 +118,7 @@ define(function (require) {
         // TODO: Switch this to watching time.string when we implement it
         $scope.$watchCollection('time', function (newTime, oldTime) {
           // don't fetch unless there was a previous value and the values are not loosly equal
-          if (!_.isUndefined(oldTime) && !angular.equal(newTime, oldTime)) $scope.fetch();
+          if (!_.isUndefined(oldTime) && !angular.equals(newTime, oldTime)) $scope.fetch();
         });
 
         $scope.$watch('state.sort', function (sort) {
