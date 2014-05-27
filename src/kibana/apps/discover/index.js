3e4286604225b96dbd548b28ee2955f5ccbc89fc
Rashid Khan <rashidkpc@gmail.com>
2014-05-27 11:08:54 -0700

    Move table directive to discover

--- src/kibana/apps/discover/index.js
+++ src/kibana/apps/discover/index.js
@@ -1,5 +1,5 @@
 define(function (require, module, exports) {
-  require('directives/table');
+  require('./directives/table');
   require('./saved_searches/saved_searches');
   require('./directives/timechart');
   require('./directives/field_chooser');
