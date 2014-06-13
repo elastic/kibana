ca6312b362547bab61ee99a66468db5e0a115f04
Chris Soyars <ctso@ctso.me>
2014-06-12 17:50:37 -0700

    Fix typo in map module description

--- src/app/panels/map/module.js
+++ src/app/panels/map/module.js
@@ -41,7 +41,7 @@ function (angular, app, _, $) {
       ],
       status  : "Stable",
       description : "Displays a map of shaded regions using a field containing a 2 letter country "+
-       ", or US state, code. Regions with more hit are shaded darker. Node that this does use the"+
+       ", or US state, code. Regions with more hit are shaded darker. Note that this does use the"+
        " Elasticsearch terms facet, so it is important that you set it to the correct field."
     };
 
