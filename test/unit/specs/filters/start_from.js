90401e8ea35e8dce49601778e1eb752410e306b3
Rashid Khan <rashidkpc@gmail.com>
2014-06-17 14:20:08 -0700

    Add tests for moment, startFrom and uriescape filters

---
+++ test/unit/specs/filters/start_from.js
@@ -0,0 +1,45 @@
+define(function (require) {
+  var angular = require('angular');
+  var mocks = require('angular-mocks');
+
+  // Load the kibana app dependencies.
+  require('angular-route');
+
+  // Load kibana and its applications
+  require('index');
+  require('filters/start_from');
+
+  var filter;
+
+  var init = function (expandable) {
+    // Load the application
+    module('kibana');
+
+    // Create the scope
+    inject(function ($filter) {
+      filter = $filter('startFrom');
+    });
+  };
+
+  describe('startFrom array filter', function () {
+
+    beforeEach(function () {
+      init();
+    });
+
+    it('should have a startFrom filter', function () {
+      expect(filter).to.not.be(null);
+    });
+
+    it('should return an empty array if passed undefined', function () {
+      expect(filter(undefined, 10)).to.eql([]);
+    });
+
+    it('should return an array with the first 3 elements removed', function () {
+      expect(filter([1, 2, 3, 4, 5, 6, 7, 8, 9], 3).length).to.be(6);
+    });
+
+
+  });
+
+});
---
+++ test/unit/specs/filters/uriescape.js
@@ -0,0 +1,42 @@
+define(function (require) {
+  var angular = require('angular');
+  var mocks = require('angular-mocks');
+  var _ = require('lodash');
+
+  // Load the kibana app dependencies.
+  require('angular-route');
+
+  // Load kibana and its applications
+  require('index');
+  require('apps/discover/index');
+
+  var filter;
+
+  var init = function (expandable) {
+    // Load the application
+    module('kibana');
+
+    // Create the scope
+    inject(function ($filter) {
+      filter = $filter('uriescape');
+    });
+  };
+
+
+  describe('uriescape filter', function () {
+
+    beforeEach(function () {
+      init();
+    });
+
+    it('should have a uriescape filter', function () {
+      expect(filter).to.not.be(null);
+    });
+
+    it('should encodeURIComponent a string', function () {
+      expect(filter('this and that')).to.be('this%20and%20that');
+    });
+
+  });
+
+});
