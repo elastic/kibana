6659a70b46836beacf2b5ab98ff4cc08e71d9225
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-06-18 15:19:21 -0700

    adding css link back

--- src/kibana/components/vislib/modules/histogram.js
+++ src/kibana/components/vislib/modules/histogram.js
@@ -8,6 +8,9 @@ define(function (require) {
   var legendFn = require('components/vislib/modules/legend');
   var getColor = require('components/vislib/utils/colorspace');
 
+  // Dynamically adds css file
+  require('css!components/vislib/styles/k4.d3');
+
   return function histogram(elem, args) {
     if (typeof args === 'undefined') {
       args = {};
