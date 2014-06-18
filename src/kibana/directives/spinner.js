f5a874180ccb29c045df54f2022bbedd303effec
Rashid Khan <rashidkpc@gmail.com>
2014-06-18 15:16:18 -0700

    Add confirm-click directive to index pattern delete button. Closes #133

--- src/kibana/directives/spinner.js
+++ src/kibana/directives/spinner.js
@@ -1,5 +1,5 @@
 define(function (require) {
-  var html = '<div class="kbn-spinner"><div class="bounce1"></div> <div class="bounce2"></div> <div class="bounce3"></div></div>';
+  var html = '<div class="bounce1"></div> <div class="bounce2"></div> <div class="bounce3"></div>';
   var module = require('modules').get('kibana/directives');
 
   module.directive('spinner', function () {
