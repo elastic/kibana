c58aec9455f8f56cf2f5d1839e8bbb6bf9f7640d
Juan Thomassie <juan.thomassie@elasticsearch.com>
2014-06-20 16:04:47 -0400

    working with refactored histogram added offsets for month and year added try/catch to handle widths with negative values (line 895-907)

--- src/kibana/components/vislib/modules/histogram.js
+++ src/kibana/components/vislib/modules/histogram.js
@@ -464,11 +464,19 @@ define(function (require) {
           var testInterval;
           var dateoffset;
 
-          if (milsInterval < 2419200000) {
-            testInterval = 'week';
-            dateoffset = (milsInterval / 604800000);
+          if (milsInterval >= 86400000 * 364) {
+            testInterval = 'year';
+            dateoffset = (milsInterval / 86400000 * 364);
           }
-          if (milsInterval < 604800000) {
+          if (milsInterval < 86400000 * 364) {
+            testInterval = 'month';
+            dateoffset = (milsInterval / 86400000 * 30);
+          }
+          if (milsInterval < 86400000 * 30) {
+            testInterval = 'week';
+            dateoffset = (milsInterval / 86400000 * 7);
+          }
+          if (milsInterval < 86400000 * 7) {
             testInterval = 'day';
             dateoffset = (milsInterval / 86400000);
           }
@@ -880,10 +888,23 @@ define(function (require) {
                 return xScale(d.x);
               })
               .attr('width', function () {
-                return data.ordered === undefined || !data.ordered.date ?
-                  xScale.rangeBand() :
-                  xScale(data.ordered.min + data.ordered.interval) -
-                  xScale(data.ordered.min) - 2;
+                // return data.ordered === undefined || !data.ordered.date ?
+                //   xScale.rangeBand() :
+                //   xScale(data.ordered.min + data.ordered.interval) -
+                //   xScale(data.ordered.min) - 2;
+                var ww;
+                if (data.ordered === undefined || !data.ordered.date) {
+                  ww = xScale.rangeBand();
+                } else {
+                  ww = xScale(data.ordered.min + data.ordered.interval) - xScale(data.ordered.min) - 2;
+                }
+                try {
+                  if (ww === undefined || ww <= 0) {
+                    throw new Error('Negative width!');
+                  }
+                } catch (error) {
+                  console.group('819: Bars attr width: ' + error);
+                }
               })
               .attr('y', function (d) {
                 return yScale(d.y0 + d.y);
