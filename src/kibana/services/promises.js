c19fc7770b5d8ebd86fb52b62b2b12c11f0a4646
Spencer Alger <spencer@spenceralger.com>
2014-07-15 11:36:04 -0700

    removed old promises module

--- src/kibana/services/promises.js
+++ src/kibana/services/promises.js
@@ -3,39 +3,6 @@ define(function (require) {
 
   var module = require('modules').get('kibana/services');
 
-  module.service('promises', function ($q) {
-    function playNice(fn, fns) {
-      if (fns && _.isArray(fns) && _.isObject(fn)) {
-        fns.forEach(function (method) {
-          fn[method] = playNice(fn[method]);
-        });
-        return fn;
-      }
-
-      return function playNiceWrapper() {
-        // if the last arg is a callback then don't do anything
-        if (typeof arguments[arguments.length - 1] === 'function') {
-          return fn.apply(this, arguments);
-        }
-
-        // otherwise create a callback and pass it in
-        var args = Array.prototype.slice.call(arguments);
-        var defer = $q.defer();
-        args.push(function (err, result) {
-          if (err) return defer.reject(err);
-          defer.resolve(result);
-        });
-        fn.apply(this, args);
-        return defer.promise;
-      };
-    }
-
-
-    return {
-      playNice: playNice
-    };
-  });
-
   // Provides a tiny subset of the excelent API from
   // bluebird, reimplemented using the $q service
   module.service('Promise', function ($q, $timeout) {
