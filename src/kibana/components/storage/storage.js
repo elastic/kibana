42ef1984e3cef1b60436b03d28a435231a53cb11
Joe Fleming <joe.fleming@gmail.com>
2014-08-05 14:06:37 -0700

    storage -> self

--- src/kibana/components/storage/storage.js
+++ src/kibana/components/storage/storage.js
@@ -4,31 +4,31 @@ define(function (require) {
   modules.get('kibana/storage')
   .service('storage', function ($window) {
     function Storage() {
-      var storage = this;
-      storage.store = $window.localStorage;
+      var self = this;
+      self.store = $window.localStorage;
 
-      storage.get = function (key) {
+      self.get = function (key) {
         try {
-          return JSON.parse(storage.store.getItem(key));
+          return JSON.parse(self.store.getItem(key));
         } catch (e) {
           return null;
         }
       };
 
-      storage.set = function (key, value) {
+      self.set = function (key, value) {
         try {
-          return storage.store.setItem(key, JSON.stringify(value));
+          return self.store.setItem(key, JSON.stringify(value));
         } catch (e) {
           return false;
         }
       };
 
-      storage.remove = function (key) {
-        return storage.store.removeItem(key);
+      self.remove = function (key) {
+        return self.store.removeItem(key);
       };
 
-      storage.clear = function () {
-        return storage.store.clear();
+      self.clear = function () {
+        return self.store.clear();
       };
     }
 
