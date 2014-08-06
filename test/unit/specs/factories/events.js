d650cf92a4821eedf528752475c58d98c9085441
Joe Fleming <joe.fleming@gmail.com>
2014-08-06 15:01:02 -0700

    use bluebird promises instead of scopeApply, since it gives us verbose errors

--- test/unit/specs/factories/events.js
+++ test/unit/specs/factories/events.js
@@ -8,115 +8,124 @@ define(function (require) {
   // Load kibana
   require('index');
 
-  describe('State Management', function () {
-    describe('Events', function () {
-      var $rootScope;
-      var Events;
-      var Notifier;
+  describe('Events', function () {
+    require('test_utils/no_digest_promises').activateForSuite();
 
-      beforeEach(function () {
-        module('kibana');
+    var $rootScope;
+    var Events;
+    var Notifier;
+    var Promise;
 
-        inject(function ($injector, Private) {
-          $rootScope = $injector.get('$rootScope');
-          Notifier = $injector.get('Notifier');
-          Events = Private(require('factories/events'));
-        });
+    beforeEach(function () {
+      module('kibana');
+
+      inject(function ($injector, Private) {
+        $rootScope = $injector.get('$rootScope');
+        Notifier = $injector.get('Notifier');
+        Promise = $injector.get('Promise');
+        Events = Private(require('factories/events'));
       });
+    });
 
-      it('should handle on events', function (done) {
-        var obj = new Events();
-        obj.on('test', function (message) {
-          expect(message).to.equal('Hello World');
-          done();
-        });
-        obj.emit('test', 'Hello World');
-        $rootScope.$apply();
+    it('should handle on events', function (done) {
+      var obj = new Events();
+      obj.on('test', function (message) {
+        expect(message).to.equal('Hello World');
+        done();
       });
+      obj.emit('test', 'Hello World');
+      // $rootScope.$apply();
+    });
 
-      it('should work with inherited objects', function (done) {
-        _.inherits(MyEventedObject, Events);
-        function MyEventedObject() {
-          MyEventedObject.Super.call(this);
-        }
-        var obj = new MyEventedObject();
-        obj.on('test', function (message) {
-          expect(message).to.equal('Hello World');
-          done();
-        });
-        obj.emit('test', 'Hello World');
-        $rootScope.$apply();
+    it('should work with inherited objects', function (done) {
+      _.inherits(MyEventedObject, Events);
+      function MyEventedObject() {
+        MyEventedObject.Super.call(this);
+      }
+      var obj = new MyEventedObject();
+      obj.on('test', function (message) {
+        expect(message).to.equal('Hello World');
+        done();
       });
+      obj.emit('test', 'Hello World');
+      // $rootScope.$apply();
+    });
 
-      it('should clear events when off is called', function () {
-        var obj = new Events();
-        obj.on('test', _.noop);
-        expect(obj._listeners).to.have.property('test');
-        expect(obj._listeners['test']).to.have.length(1);
-        obj.off();
-        expect(obj._listeners).to.not.have.property('test');
-      });
+    it('should clear events when off is called', function () {
+      var obj = new Events();
+      obj.on('test', _.noop);
+      expect(obj._listeners).to.have.property('test');
+      expect(obj._listeners['test']).to.have.length(1);
+      obj.off();
+      expect(obj._listeners).to.not.have.property('test');
+    });
 
-      it('should clear a specific handler when off is called for an event', function () {
-        var obj = new Events();
-        var handler1 = sinon.stub();
-        var handler2 = sinon.stub();
-        obj.on('test', handler1);
-        obj.on('test', handler2);
-        expect(obj._listeners).to.have.property('test');
-        obj.off('test', handler1);
-        obj.emit('test', 'Hello World');
-        $rootScope.$apply();
+    it('should clear a specific handler when off is called for an event', function (done) {
+      var obj = new Events();
+      var handler1 = sinon.stub();
+      var handler2 = sinon.stub();
+      obj.on('test', handler1);
+      obj.on('test', handler2);
+      expect(obj._listeners).to.have.property('test');
+      obj.off('test', handler1);
+      obj.emit('test', 'Hello World').then(function () {
         sinon.assert.calledOnce(handler2);
         sinon.assert.notCalled(handler1);
+        done();
       });
+    });
 
-      it('should clear a all handlers when off is called for an event', function () {
-        var obj = new Events();
-        var handler1 = sinon.stub();
-        obj.on('test', handler1);
-        expect(obj._listeners).to.have.property('test');
-        obj.off('test');
-        expect(obj._listeners).to.not.have.property('test');
-        obj.emit('test', 'Hello World');
-        $rootScope.$apply();
-        sinon.assert.notCalled(handler1);
-      });
+    it('should clear a all handlers when off is called for an event', function () {
+      var obj = new Events();
+      var handler1 = sinon.stub();
+      obj.on('test', handler1);
+      expect(obj._listeners).to.have.property('test');
+      obj.off('test');
+      expect(obj._listeners).to.not.have.property('test');
+      obj.emit('test', 'Hello World');
+      // $rootScope.$apply();
+      sinon.assert.notCalled(handler1);
+    });
 
-      it('should handle mulitple identical emits in the same tick', function () {
-        var obj = new Events();
-        var handler1 = sinon.stub();
+    it('should handle mulitple identical emits in the same tick', function (done) {
+      var obj = new Events();
+      var handler1 = sinon.stub();
 
-        obj.on('test', handler1);
-        obj.emit('test', 'one');
-        obj.emit('test', 'two');
-        obj.emit('test', 'three');
+      obj.on('test', handler1);
+      var emits = [
+        obj.emit('test', 'one'),
+        obj.emit('test', 'two'),
+        obj.emit('test', 'three')
+      ];
 
-        $rootScope.$apply();
-
+      Promise.all(emits).then(function () {
         expect(handler1.callCount).to.be(3);
         expect(handler1.getCall(0).calledWith('one')).to.be(true);
         expect(handler1.getCall(1).calledWith('two')).to.be(true);
         expect(handler1.getCall(2).calledWith('three')).to.be(true);
+        done();
+      });
+    });
+
+    it('should handle emits from the handler', function (done) {
+      var obj = new Events();
+      var secondEmit = Promise.defer();
+      var handler1 = sinon.spy(function () {
+        if (handler1.calledTwice) {
+          return;
+        }
+        obj.emit('test').then(_.bindKey(secondEmit, 'resolve'));
       });
 
-      it('should handle emits from the handler', function () {
-        var obj = new Events();
-        var handler1 = sinon.spy(function () {
-          if (handler1.calledTwice) {
-            return;
-          }
-          obj.emit('test');
-        });
+      obj.on('test', handler1);
 
-        obj.on('test', handler1);
-        obj.emit('test');
-
-        $rootScope.$apply();
-
+      Promise.all([
+        obj.emit('test'),
+        secondEmit.promise
+      ]).then(function () {
         expect(handler1.callCount).to.be(2);
+        done();
       });
     });
   });
-
 });
