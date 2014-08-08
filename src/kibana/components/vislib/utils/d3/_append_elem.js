1588bfb8ea04b72a81dc5f2bc8f50db63e3b6e10
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-08-08 16:17:53 +0300

    adding first d3 test

--- src/kibana/components/vislib/utils/d3/_append_elem.js
+++ src/kibana/components/vislib/utils/d3/_append_elem.js
@@ -1,8 +1,11 @@
 define(function () {
   return function AppendElemUtilService(d3) {
+    // Accepts an DOM element, an DOM element or SVG type,
+    // and a optional class name
     return function (el, type, name) {
       name = typeof name !== 'undefined' ? name : type;
 
+      // Returns a d3 selection
       return d3.select(el).append(type)
         .attr('class', name);
     };
