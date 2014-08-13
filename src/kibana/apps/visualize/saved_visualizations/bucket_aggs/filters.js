c8bf2e1165f066ad5300c27c48e921f48a56ab13
Rashid Khan <rashidkpc@gmail.com>
2014-08-13 10:43:03 -0700

    Add filters agg, properly disable Apply button in visualize when form is invalid

--- src/kibana/apps/visualize/saved_visualizations/bucket_aggs/filters.js
+++ src/kibana/apps/visualize/saved_visualizations/bucket_aggs/filters.js
@@ -7,22 +7,29 @@ define(function (require) {
     var agg = this;
     agg.name = 'filters';
     agg.display = 'Filters';
-    //agg.ordered = {};
 
     agg.makeLabel = function (params) {
-      return params.query_string;
+      return 'Filters';
     };
+
+    function getTickLabel(query) {
+      if (query.query_string && query.query_string.query) return query.query_string.query;
+      return JSON.stringify(query);
+    }
 
     agg.params = {};
 
     agg.params.filters = {
       custom: true,
-      default: [{query_string: '*'}],
+      default: {query_string: {query: '*'}},
       write: function (input, output) {
         output.aggParams = {
           filters: _.zipObject(_.map(input.val, function (filter, iterator) {
             // We need to check here
-            return [filter.query_string, {query: {query_string: {query: filter.query_string || '*'}}}];
+            return [
+              getTickLabel(filter.input),
+              {query: filter.input || {query_string: {query: '*'}}}
+            ];
           }))
         };
       }
