f13a88569a791037d09005415e2ae623bb6521fe
Rashid Khan <rashidkpc@gmail.com>
2014-08-13 16:09:38 -0700

    Add IP range aggregation, add ip address input validator. Closes #240

--- src/kibana/apps/visualize/saved_visualizations/bucket_aggs/_index.js
+++ src/kibana/apps/visualize/saved_visualizations/bucket_aggs/_index.js
@@ -4,6 +4,7 @@ define(function (require) {
       Private(require('apps/visualize/saved_visualizations/bucket_aggs/date_histogram')),
       Private(require('apps/visualize/saved_visualizations/bucket_aggs/histogram')),
       Private(require('apps/visualize/saved_visualizations/bucket_aggs/range')),
+      Private(require('apps/visualize/saved_visualizations/bucket_aggs/ip_range')),
       Private(require('apps/visualize/saved_visualizations/bucket_aggs/terms')),
       Private(require('apps/visualize/saved_visualizations/bucket_aggs/filters')),
       Private(require('apps/visualize/saved_visualizations/bucket_aggs/significant_terms'))
