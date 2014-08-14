d4973bd03a02837bea542bf879f6d0dcd25beac2
Chris Cowan <chris@chriscowan.us>
2014-08-14 15:56:40 -0700

    Closes #308 - Adding license header to all source files

--- kibana/panels/shard_allocation/lib/getValueFromArrayOrString.js
+++ kibana/panels/shard_allocation/lib/getValueFromArrayOrString.js
@@ -1,3 +1,22 @@
+/**
+ * ELASTICSEARCH CONFIDENTIAL
+ * _____________________________
+ *
+ *  [2014] Elasticsearch Incorporated All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Elasticsearch Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Elasticsearch Incorporated
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Elasticsearch Incorporated.
+ */
+
+
+
 // The function is needed because in Elasticsaerch 0.90.x field data is just a string
 // but in 1.x it's the first element in an array. So we need a way to access it depending
 // on the version.
