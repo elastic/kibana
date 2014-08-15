4bf4cb64e5a611ff01615a091dad2bafc67845a3
Rashid Khan <rashidkpc@gmail.com>
2014-08-15 10:26:18 -0700

    Allow input-focus to take the name of another input to focus on click

--- src/kibana/directives/input_focus.js
+++ src/kibana/directives/input_focus.js
@@ -1,13 +1,28 @@
 define(function (require) {
   var module = require('modules').get('kibana');
+  var _ = require('lodash');
+  var $ = require('jquery');
 
   module.directive('inputFocus', function ($timeout) {
     return {
+      scope: {
+        inputFocus: '=?'
+      },
       restrict: 'A',
       link: function ($scope, $elem, attrs) {
-        $timeout(function () {
-          $elem[0].focus();
-        });
+        // If we are passed anything, treat it as the name for a input, and focus that.
+        if (!_.isUndefined($scope.inputFocus)) {
+          $elem.bind('click', function () {
+            var focusElem = $.find('input[name=' + $scope.inputFocus + ']');
+            if (focusElem[0]) focusElem[0].focus();
+          });
+          $scope.$on('$destroy', $elem.unbind);
+        } else {
+          // Need to wait for a tick, otherwise the focus() doesn't work
+          $timeout(function () {
+            $elem[0].focus();
+          });
+        }
       }
     };
   });
