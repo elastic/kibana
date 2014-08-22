26354f969773fbfd302bbfea739706bffd70f588
Rashid Khan <rashidkpc@gmail.com>
2014-08-22 08:46:59 -0700

    Add shortDots filter and tests

---
+++ src/kibana/filters/short_dots.js
@@ -0,0 +1,16 @@
+// Shorts dot notated strings
+// eg: foo.bar.baz becomes f.b.baz
+// 'foo.bar.baz'.replace(/(.+?\.)/g,function(v) {return v[0]+'.';});
+define(function (require) {
+  var _ = require('lodash');
+  require('modules')
+    .get('kbn/filters')
+    .filter('shortDots', function (config) {
+      return function (str) {
+        if (!_.isString(str) || config.get('shortDots:enable') !== true) {
+          return str;
+        }
+        return str.replace(/(.+?\.)/g, function (v) { return v[0] + '.'; });
+      };
+    });
+});
