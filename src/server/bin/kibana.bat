440ba941d71923c90226d405924fb0d0270806dc
Chris Cowan <chris@chriscowan.us>
2014-08-27 14:10:42 -0700

    Shell Scripts
    - Fixing the *nix version to use the right script variable
    - Adding windows bat file
    - Adding an INSTALL readme

--- src/server/bin/kibana.bat
+++ src/server/bin/kibana.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SETLOCAL
-if not defined JAVA_HOME goto java_home_err
+if NOT DEFINED JAVA_HOME goto java_home_err
 
 set SCRIPT_DIR=%~dp0
 for %%I in ("%SCRIPT_DIR%..") do set DIR=%%~dpfI
