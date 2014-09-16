2488a758a152eba0d4772d4c3a359668f75b551d
Spencer Alger <spencer@spenceralger.com>
2014-09-16 10:26:38 -0700

    add --shhh option for the dev task

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,4 +1,9 @@
 module.exports = function (grunt) {
+  if (grunt.option('shhh')) {
+    process.env.RACK_ENV = 'staging';
+    grunt.option('no-test-watcher', true);
+  }
+
   grunt.registerTask('dev', [
     'less',
     'jade',
