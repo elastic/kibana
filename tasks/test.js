94e55f3f67633aa4d3f70baff5d738c06f98eaa8
Joe Fleming <joe.fleming@gmail.com>
2014-09-16 13:50:01 -0700

    check for sauce key when running on travis, abort if missing

--- tasks/test.js
+++ tasks/test.js
@@ -1,16 +1,24 @@
 var _ = require('lodash');
 module.exports = function (grunt) {
 
-  var testTask = process.env.TRAVIS ? 'saucelabs-mocha:unit' : 'mocha:unit';
 
-  grunt.registerTask('test', [
-    'jshint',
-    'ruby_server',
-    'maybe_start_server',
-    'jade',
-    'less',
-    testTask
-  ]);
+  grunt.registerTask('test', function () {
+    if (process.env.TRAVIS && !process.env.SAUCE_ACCESS_KEY) {
+      grunt.fail.fatal('SAUCE_ACCESS_KEY not set in env, can not run tests on Sauce Labs');
+    }
+
+    var testTask = process.env.TRAVIS ? 'saucelabs-mocha:unit' : 'mocha:unit';
+
+    var tasks = [
+      'jshint',
+      'ruby_server',
+      'maybe_start_server',
+      'jade',
+      'less',
+      testTask
+    ];
+    grunt.task.run(tasks);
+  });
 
   grunt.registerTask('coverage', [
     'blanket',
