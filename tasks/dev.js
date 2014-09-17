6af446e7a0b7449b77426c268a85b05a605c9d07
Spencer Alger <spencer@spenceralger.com>
2014-09-16 22:59:19 -0700

    a quite dev process is no longer a two stage process

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,9 +1,4 @@
 module.exports = function (grunt) {
-  if (grunt.option('shhh')) {
-    process.env.RACK_ENV = 'staging';
-    grunt.option('no-test-watcher', true);
-  }
-
   grunt.registerTask('dev', [
     'less',
     'jade',
