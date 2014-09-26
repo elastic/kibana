a53f6e5df33f588dba707925c5729a5380760eed
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-09-26 17:11:06 +0300

    fixing relevant issue in legend, where legend does not receive the entire list of names to be displayed with the charts

--- src/kibana/components/vislib/visualizations/pie_chart.js
+++ src/kibana/components/vislib/visualizations/pie_chart.js
@@ -97,6 +97,7 @@ define(function (require) {
         })
         .style('stroke', '#fff')
         .style('fill', function (d) {
+          if (d.depth === 0) { return 'none'; }
           return color(d.name);
         })
         .attr('fill-rule', 'evenodd');
