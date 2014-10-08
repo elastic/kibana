7304ade3a6d59a750475d86446bdb698f2378b5b
Spencer Alger <spencer@spenceralger.com>
2014-10-08 15:36:35 -0700

    remove old config file and reference from Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,10 @@ module.exports = function (grunt) {
     src: __dirname + '/src', // unbuild version of build
     build: __dirname + '/build', // copy of source, but optimized
     app: __dirname + '/src/kibana', // source directory for the app
+    server: __dirname + '/src/server', // source directory for the server
     target: __dirname + '/target',  // location of the compressed build targets
     buildApp: __dirname + '/build/kibana', // build directory for the app
+    configFile: __dirname + '/src/server/config/kibana.yml',
 
     jrubyVersion: '1.7.14',
     jrubyPath: __dirname + '/.jruby',
