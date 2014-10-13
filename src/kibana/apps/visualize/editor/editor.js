a5a16ba7e91c2520dc58a4f755664a923dbe7a60
Spencer Alger <spencer@spenceralger.com>
2014-10-13 13:51:52 -0700

    [vis/editor] remove kbnValidOptions directive, use null option

--- src/kibana/apps/visualize/editor/editor.js
+++ src/kibana/apps/visualize/editor/editor.js
@@ -4,7 +4,6 @@ define(function (require) {
   require('apps/visualize/editor/agg_filter');
 
   require('directives/saved_object_finder');
-  require('directives/valid_options');
   require('components/visualize/visualize');
   require('filters/uriescape');
 
