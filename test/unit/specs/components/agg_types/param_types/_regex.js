cfd20ab3c427867771290cf0988439b24cda623d
Joe Fleming <joe.fleming@gmail.com>
2014-10-13 17:02:47 -0700

    add some tests for the regex param type

--- test/unit/specs/components/agg_types/param_types/_regex.js
+++ test/unit/specs/components/agg_types/param_types/_regex.js
@@ -13,13 +13,56 @@ define(function (require) {
     }));
 
     describe('constructor', function () {
-      it('it is an instance of BaseAggParam', function () {
+      it('should be an instance of BaseAggParam', function () {
         var aggParam = new RegexAggParam({
           name: 'some_param',
           type: 'regex'
         });
 
         expect(aggParam).to.be.a(BaseAggParam);
+      });
+
+      it('should not include param in output', function () {
+        var paramName = 'exclude';
+        var aggParam = new RegexAggParam({
+          name: paramName,
+          type: 'regex'
+        });
+
+        var aggConfig = { params: {} };
+        var output = { params: {} };
+        aggConfig.params[paramName] = {
+          pattern: ''
+        };
+
+        expect(aggParam).to.have.property('write');
+
+        aggParam.write(aggConfig, output);
+        expect(output).to.be.an('object');
+        expect(output).to.have.property('params');
+        expect(output.params).not.to.have.property(paramName);
+      });
+
+      it('should include param in output', function () {
+        var paramName = 'exclude';
+        var aggParam = new RegexAggParam({
+          name: paramName,
+          type: 'regex'
+        });
+
+        var aggConfig = { params: {} };
+        var output = { params: {} };
+        aggConfig.params[paramName] = {
+          pattern: 'testing'
+        };
+
+        expect(aggParam).to.have.property('write');
+
+        aggParam.write(aggConfig, output);
+        expect(output).to.be.an('object');
+        expect(output).to.have.property('params');
+        expect(output.params).to.have.property(paramName);
+        expect(output.params[paramName]).to.eql({ pattern: 'testing' });
       });
     });
   }];
