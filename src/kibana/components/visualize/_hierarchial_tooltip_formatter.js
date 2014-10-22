1e1cedf6aa561e797e7c7b2a1e200d894e709265
Chris Cowan <chris@chriscowan.us>
2014-10-22 12:05:31 -0700

    Teasing apart the branch walking from the hierarchial tooltip

--- src/kibana/components/visualize/_hierarchial_tooltip_formatter.js
+++ src/kibana/components/visualize/_hierarchial_tooltip_formatter.js
@@ -3,6 +3,7 @@ define(function (require) {
     var _ = require('lodash');
     var $ = require('jquery');
     var $tooltip = $(require('text!plugins/vis_types/tooltips/pie.html'));
+    var collectBranch = require('components/visualize/_collect_branch');
     var $tooltipScope = $rootScope.$new();
     $compile($tooltip)($tooltipScope);
 
@@ -18,40 +19,15 @@ define(function (require) {
           sum = parent.value;
         }
 
-        var rows = $tooltipScope.rows = [];
+        // Collect the current leaf and parents into an array of values
+        var rows = collectBranch(datum);
 
-        // walk up the branch for each parent
-        (function walk(item) {
-          // record the the depth
-          var i = item.depth - 1;
-
-          // Using the aggConfig determin what the field name is. If the aggConfig
-          // doesn't exist (which means it's an _all agg) then use the level for
-          // the field name
-          var col = item.aggConfig;
-          var field = (col && col.params && col.params.field && col.params.field.name)
-            || (col && col.label)
-            || ('level ' + datum.depth);
-
-          // Set the bucket name, and use the converter to format the field if
-          // the field exists.
-          var bucket = item.name;
-          if (col && col.field) bucket = col.field.format.convert(bucket);
-
-          // Add the row to the tooltipScope.rows
-          rows.unshift({
-            spacer: $sce.trustAsHtml(_.repeat('&nbsp;', i)),
-            field: field,
-            bucket: bucket,
-            metric: item.value + ' (' + Math.round((item.value / sum) * 100) + '%)'
-          });
-
-          // If the item has a parent and it's also a child then continue walking
-          // up the branch
-          if (item.parent && item.parent.parent) {
-            walk(item.parent);
-          }
-        })(datum);
+        // Map those values to what the tooltipSource.rows format.
+        $tooltipScope.rows = _.map(rows, function (row) {
+          row.spacer = $sce.trustAsHtml(_.repeat('&nbsp;', row.depth));
+          row.metric = row.metric + ' (' + Math.round((row.metric / sum) * 100) + '%)';
+          return row;
+        });
 
         $tooltipScope.metricCol = _.find(columns, { categoryName: 'metric' });
 
