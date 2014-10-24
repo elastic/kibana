2e932f1e2303378192a57f670f3c61debc9c3408
Joe Fleming <joe.fleming@gmail.com>
2014-10-24 10:46:32 -0700

    added more tests to the query input

--- test/unit/specs/directives/query_input.js
+++ test/unit/specs/directives/query_input.js
@@ -37,7 +37,7 @@ define(function (require) {
   };
 
   var invalidEsResponse = function () {
-    return Promise.resolve({ body: { error: 'mock invalid query' } });
+    return Promise.reject({ body: { error: 'mock invalid query' } });
   };
 
   var init = function () {
@@ -95,11 +95,43 @@ define(function (require) {
       it('should call validate on input change', function () {
         // once for $watch, once for change
         var checkCount = 2;
-        $elem.val('key:value');
+        $elem.val('someValue');
         $elem.scope().$digest();
         expect(mockEs.indices.validateQuery.callCount).to.be(checkCount);
       });
     });
 
+    describe('valid querystring', function () {
+      beforeEach(function () {
+        mockValidateReturns = validEsResponse();
+        init();
+      });
+
+      it('should set valid state', function (done) {
+        // give angular time to set up the directive
+        setTimeout(function () {
+          expect($elem.hasClass('ng-valid-query-input')).to.be(true);
+          expect($elem.hasClass('ng-valid')).to.be(true);
+          done();
+        }, 0);
+      });
+    });
+
+    describe('invalid querystring', function () {
+      beforeEach(function () {
+        mockValidateReturns = invalidEsResponse();
+        init();
+      });
+
+      it('should set invalid state', function (done) {
+        // give angular time to set up the directive
+        setTimeout(function () {
+          expect($elem.hasClass('ng-invalid')).to.be(true);
+          done();
+        }, 0);
+      });
+
+      it('should change to invalid state');
+    });
   });
 });
