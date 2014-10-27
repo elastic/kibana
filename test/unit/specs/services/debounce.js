657a49e7dddd35cf16c024ac441670706d4f1003
Joe Fleming <joe.fleming@gmail.com>
2014-10-27 11:33:47 -0700

    add debounce service, with mock tests

---
+++ test/unit/specs/services/debounce.js
@@ -0,0 +1,7 @@
+define(function (require) {
+  describe('debounce service', function () {
+    it('should delay execution');
+    it('should fire on leading edge');
+    it('should only fire on leading edge');
+  });
+});
