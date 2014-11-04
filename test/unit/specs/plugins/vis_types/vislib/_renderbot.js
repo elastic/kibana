b737d041eb7eb2487609dee1ea43c163084bfefc
Joe Fleming <joe.fleming@gmail.com>
2014-11-04 11:44:33 -0700

    update the vis types tests - wrap all vis types tests - wrap vislib tests - add basic renderbot and vislib renderbot tests

--- test/unit/specs/plugins/vis_types/vislib/_renderbot.js
+++ test/unit/specs/plugins/vis_types/vislib/_renderbot.js
@@ -1,21 +1,49 @@
 define(function (require) {
+  var $ = require('jquery');
+  var sinon = require('test_utils/auto_release_sinon');
+
   return ['renderbot', exportWrapper];
 
   function exportWrapper() {
     var vislib;
+    var Renderbot;
+    var VislibRenderbot;
 
     function init() {
-      // module('kibana', function ($provide) {
-      // });
       module('kibana');
 
-      inject(function ($injector) {
+      inject(function ($injector, Private) {
         vislib = $injector.get('vislib');
+        Renderbot = Private(require('plugins/vis_types/_renderbot'));
+        VislibRenderbot = Private(require('plugins/vis_types/vislib/_vislib_renderbot'));
       });
     }
 
     describe('creation', function () {
-      it('should create a new Vis object');
+      var vis;
+      var $el;
+      var createVisStub;
+      var renderbot;
+
+      beforeEach(init);
+
+      beforeEach(function () {
+        vis = { type: 'vis' };
+        $el = 'element';
+        createVisStub = sinon.stub(VislibRenderbot.prototype, '_createVis');
+        renderbot = new VislibRenderbot(vis, $el);
+      });
+
+      it('should be a Renderbot', function () {
+        expect(renderbot).to.be.a(Renderbot);
+      });
+
+      it('should create a new Vis object', function () {
+        expect(createVisStub.callCount).to.be(1);
+      });
+    });
+
+    describe('_createVis', function () {
       it('should attach listeners');
     });
 
