1f84108580434205ffa3389aab8633a157988028
Joe Fleming <joe.fleming@gmail.com>
2014-11-10 16:51:22 -0700

    re-org the clipboard tests, with flash enabled

--- test/unit/specs/components/clipboard.js
+++ test/unit/specs/components/clipboard.js
@@ -7,14 +7,16 @@ define(function (require) {
   require('components/clipboard/clipboard');
 
   describe('Clipboard directive', function () {
+    var $scope;
+    var $rootScope;
+    var $compile;
+    var $interpolate;
+    var el;
+    var tips;
 
-    var $scope, $rootScope, $compile, $interpolate, el, tips;
-
-    beforeEach(function (done) {
+    function init() {
       // load the application
       module('kibana');
-
-      sinon.stub(window.ZeroClipboard, 'isFlashUnusable', _.constant(false));
 
       inject(function (_$rootScope_, _$compile_, _$interpolate_) {
         $rootScope = _$rootScope_;
@@ -26,48 +28,51 @@ define(function (require) {
 
         $scope = el.scope();
         $scope.$digest();
+      });
+    }
 
-        done();
+    describe('With flash enabled', function () {
+      beforeEach(function () {
+        sinon.stub(window.ZeroClipboard, 'isFlashUnusable', _.constant(false));
+        init();
       });
 
+      it('should contain an element with clip-copy', function () {
+        var clip = el.find('[clip-copy]');
+        expect(clip).to.have.length(1);
+      });
+
+      it('should have a tooltip', function () {
+        var clip = el.find('[tooltip]');
+        expect(clip).to.have.length(1);
+
+        var clipText = $interpolate($(clip).attr('tooltip'))();
+        expect(clipText).to.be('Copy to clipboard');
+
+      });
+
+      it('should change the tooltip text when clicked, back when mouse leaves', function () {
+        el.mouseenter();
+        el.click();
+        $scope.$digest();
+
+        var clipText = $interpolate($('[tooltip]', el).attr('tooltip'))();
+        expect(clipText).to.be('Copied!');
+
+        el.mouseleave();
+        $scope.$digest();
+
+        clipText = $interpolate($('[tooltip]', el).attr('tooltip'))();
+        expect(clipText).to.be('Copy to clipboard');
+      });
+
+      it('should unbind all handlers on destroy', function () {
+        var handlers = $._data(el.get(0), 'events');
+        expect(Object.keys(handlers)).to.have.length(2);
+
+        $scope.$destroy();
+        expect(Object.keys(handlers)).to.have.length(0);
+      });
     });
-
-    it('should contain an element with clip-copy', function () {
-      var clip = el.find('[clip-copy]');
-      expect(clip).to.have.length(1);
-    });
-
-    it('should have a tooltip', function () {
-      var clip = el.find('[tooltip]');
-      expect(clip).to.have.length(1);
-
-      var clipText = $interpolate($(clip).attr('tooltip'))();
-      expect(clipText).to.be('Copy to clipboard');
-
-    });
-
-    it('should change the tooltip text when clicked, back when mouse leaves', function () {
-      el.mouseenter();
-      el.click();
-      $scope.$digest();
-
-      var clipText = $interpolate($('[tooltip]', el).attr('tooltip'))();
-      expect(clipText).to.be('Copied!');
-
-      el.mouseleave();
-      $scope.$digest();
-
-      clipText = $interpolate($('[tooltip]', el).attr('tooltip'))();
-      expect(clipText).to.be('Copy to clipboard');
-    });
-
-    it('should unbind all handlers on destroy', function () {
-      var handlers = $._data(el.get(0), 'events');
-      expect(Object.keys(handlers)).to.have.length(2);
-
-      $scope.$destroy();
-      expect(Object.keys(handlers)).to.have.length(0);
-    });
-
   });
 });
