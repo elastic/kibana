ed9e2818f4e0ca8b3d91b0e7e129c478f70d5230
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-13 02:09:32 +0200

    removing console.log

--- src/kibana/components/vislib/vis.js
+++ src/kibana/components/vislib/vis.js
@@ -50,7 +50,6 @@ define(function (require) {
         throw new Error('No valid data!');
       }
 
-      console.log(JSON.stringify(data, null, 4));
       this.data = data;
       this.handler = handlerTypes[chartType](this) || handlerTypes.column(this);
       this._runOnHandler('render');
