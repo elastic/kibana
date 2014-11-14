f82983820ab0fe2530339ca89e38680b82387545
Spencer Alger <spencer@spenceralger.com>
2014-11-14 16:16:31 -0700

    [kibana] teased the kibana controller, broke it into pieces, placed it in a plugin

---
+++ src/kibana/plugins/kibana/_apps.js
@@ -0,0 +1,36 @@
+define(function (require) {
+  return function KbnControllerApps(Private, $rootScope, $scope, $location, globalState, sessionStorage) {
+    var _ = require('lodash');
+
+    function appKey(app) {
+      return 'lastPath:' + app.id;
+    }
+
+    function getLastPath(app) {
+      app.lastPath = sessionStorage.get(appKey(app)) || '/' + app.id;
+      return app.lastPath;
+    }
+
+    function setLastPath(app, path) {
+      app.lastPath = path;
+      return sessionStorage.set(appKey(app), path);
+    }
+
+    $scope.apps = Private(require('registry/apps'));
+    // initialize each apps lastPath (fetch it from storage)
+    $scope.apps.forEach(getLastPath);
+
+    function onRouteChange() {
+      var route = $location.path().split(/\//);
+      var app = $scope.activeApp = _.find($scope.apps, { id: route[1] });
+
+      if (!app) return;
+
+      // Record the last URL w/ state of the app, use for tab.
+      setLastPath(app, globalState.removeFromUrl($location.url()));
+    }
+
+    $rootScope.$on('$routeChangeSuccess', onRouteChange);
+    $rootScope.$on('$routeUpdate', onRouteChange);
+  };
+});
