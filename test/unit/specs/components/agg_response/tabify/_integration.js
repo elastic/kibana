9710c83da39f5c7d2c9fcc6f080a6610649fb988
Spencer Alger <spencer@spenceralger.com>
2014-11-15 17:14:07 -0700

    [tabify] added tests to proved 2fcfceb and 9fea632

--- test/unit/specs/components/agg_response/tabify/_integration.js
+++ test/unit/specs/components/agg_response/tabify/_integration.js
@@ -1,5 +1,5 @@
 define(function (require) {
-  return ['tabifyAggResponse Simple Integration', function () {
+  return ['tabifyAggResponse Integration', function () {
     var _ = require('lodash');
     var fixtures = require('fixtures/fake_hierarchical_data');
 
@@ -39,144 +39,226 @@ define(function (require) {
       expect(resp.columns[0]).to.have.property('aggConfig', vis.aggs[0]);
     });
 
-    it('transforms a complex, non-hierarchical response properly', function () {
-      var vis = new Vis(indexPattern, {
-        type: 'histogram',
-        aggs: [
-          { type: 'avg', schema: 'metric', params: { field: 'bytes' } },
-          { type: 'terms', schema: 'split', params: { field: 'extension' } },
-          { type: 'terms', schema: 'segment', params: { field: 'geo.src' } },
-          { type: 'terms', schema: 'segment', params: { field: 'machine.os' } }
-        ]
+    describe('transforms a complex response', function () {
+      this.slow(1000);
+
+      var vis;
+      var avg;
+      var ext;
+      var src;
+      var os;
+      var esResp;
+
+      beforeEach(function () {
+        vis = new Vis(indexPattern, {
+          type: 'pie',
+          aggs: [
+            { type: 'avg', schema: 'metric', params: { field: 'bytes' } },
+            { type: 'terms', schema: 'split', params: { field: 'extension' } },
+            { type: 'terms', schema: 'segment', params: { field: 'geo.src' } },
+            { type: 'terms', schema: 'segment', params: { field: 'machine.os' } }
+          ]
+        });
+        normalizeIds(vis);
+
+        avg = vis.aggs[0];
+        ext = vis.aggs[1];
+        src = vis.aggs[2];
+        os = vis.aggs[3];
+
+        esResp = _.cloneDeep(fixtures.threeTermBuckets);
+        // remove the buckets for css              in MX
+        esResp.aggregations.agg_2.buckets[1].agg_3.buckets[0].agg_4.buckets = [];
       });
-      normalizeIds(vis);
 
-      var avg = vis.aggs[0];
-      var ext = vis.aggs[1];
-      var src = vis.aggs[2];
-      var os = vis.aggs[3];
-      var esResp = _.cloneDeep(fixtures.threeTermBuckets);
-      // remove the buckets for css              in MX
-      esResp.aggregations.agg_2.buckets[1].agg_3.buckets[0].agg_4.buckets = [];
-      var resp = tabifyAggResponse(vis, esResp);
+      // check that the root table group is formed properly, then pass
+      // each table to expectExtensionSplit, along with the expectInnerTables()
+      // function.
+      function expectRootGroup(rootTableGroup, expectInnerTables) {
+        expect(rootTableGroup).to.have.property('tables');
 
-      function verifyExtensionSplit(tableGroup, key) {
+        var tables = rootTableGroup.tables;
+        expect(tables).to.be.an('array').and.have.length(3);
+        expectExtensionSplit(tables[0], 'png', expectInnerTables);
+        expectExtensionSplit(tables[1], 'css', expectInnerTables);
+        expectExtensionSplit(tables[2], 'html', expectInnerTables);
+      }
+
+      // check that the tableGroup for the extension agg was formed properly
+      // then call expectTable() on each table inside. it should validate that
+      // each table is formed properly
+      function expectExtensionSplit(tableGroup, key, expectTable) {
         expect(tableGroup).to.have.property('tables');
         expect(tableGroup).to.have.property('aggConfig', ext);
         expect(tableGroup).to.have.property('key', key);
-        expect(tableGroup.tables).to.have.length(1);
+        expect(tableGroup.tables).to.be.an('array').and.have.length(1);
 
         tableGroup.tables.forEach(function (table) {
-          verifyTable(table, key);
+          expectTable(table, key);
         });
       }
 
-      function verifyTable(table, splitKey) {
-        expect(table.columns).to.have.length(3);
-        expect(table.columns[0]).to.have.property('aggConfig', src);
-        expect(table.columns[1]).to.have.property('aggConfig', os);
-        expect(table.columns[2]).to.have.property('aggConfig', avg);
-
-        table.rows.forEach(function (row) {
-          expect(row).to.have.length(3);
-
-          // two character country code
-          expect(row[0]).to.be.a('string');
-          expect(row[0]).to.have.length(2);
-
-          if (splitKey === 'css' && row[0] === 'MX') {
-            // we removed these buckets, the row shouldn't be there
-            throw new Error('expected the MX row in the CSS table to be removed');
-          } else {
-            // os
-            expect(row[1]).to.match(/^(win|mac|linux)$/);
-
-            // average bytes
-            expect(row[2]).to.be.a('number');
-            expect(row[2] === 0 || row[2] > 1000).to.be.ok();
-          }
+      // check that the columns of a table are formed properly
+      function expectColumns(table, aggs) {
+        expect(table.columns).to.be.an('array').and.have.length(aggs.length);
+        aggs.forEach(function (agg, i) {
+          expect(table.columns[i]).to.have.property('aggConfig', agg);
         });
       }
 
-      expect(resp).to.have.property('tables');
-      expect(resp.tables).to.have.length(3);
-      verifyExtensionSplit(resp.tables[0], 'png');
-      verifyExtensionSplit(resp.tables[1], 'css');
-      verifyExtensionSplit(resp.tables[2], 'html');
-    });
+      // check that a row has expected values
+      function expectRow(row, asserts) {
+        expect(row).to.be.an('array');
+        expect(row).to.have.length(asserts.length);
+        asserts.forEach(function (assert, i) {
+          assert(row[i]);
+        });
+      }
 
-    it('transforms a complex response with partial rows properly', function () {
-      var vis = new Vis(indexPattern, {
-        type: 'pie',
-        aggs: [
-          { type: 'avg', schema: 'metric', params: { field: 'bytes' } },
-          { type: 'terms', schema: 'split', params: { field: 'extension' } },
-          { type: 'terms', schema: 'segment', params: { field: 'geo.src' } },
-          { type: 'terms', schema: 'segment', params: { field: 'machine.os' } }
-        ]
+      // check for two character country code
+      function expectCountry(val) {
+        expect(val).to.be.a('string');
+        expect(val).to.have.length(2);
+      }
+
+      // check for an empty cell
+      function expectEmpty(val) {
+        expect(val)
+        .to.be('');
+      }
+
+      // check for an OS term
+      function expectOS(val) {
+        expect(val)
+        .to.match(/^(win|mac|linux)$/);
+      }
+
+      // check for something like an average bytes result
+      function expectAvgBytes(val) {
+        expect(val).to.be.a('number');
+        expect(val === 0 || val > 1000).to.be.ok();
+      }
+
+      // create an assert that checks for an expected value
+      function expectVal(expected) {
+        return function (val) {
+          expect(val).to.be(expected);
+        };
+      }
+
+      it('for non-hierarchical vis', function () {
+        // the default for a non-hierarchical vis is to display
+        // only complete rows, and only put the metrics at the end.
+
+        vis.isHierarchical = _.constant(false);
+        var tabbed = tabifyAggResponse(vis, esResp);
+
+        expectRootGroup(tabbed, function expectTable(table, splitKey) {
+          expectColumns(table, [src, os, avg]);
+
+          table.rows.forEach(function (row) {
+            if (splitKey === 'css' && row[0] === 'MX') {
+              throw new Error('expected the MX row in the css table to be removed');
+            } else {
+              expectRow(row, [
+                expectCountry,
+                expectOS,
+                expectAvgBytes
+              ]);
+            }
+          });
+        });
       });
-      normalizeIds(vis);
 
-      var avg = vis.aggs[0];
-      var ext = vis.aggs[1];
-      var src = vis.aggs[2];
-      var os = vis.aggs[3];
+      it('for hierarchical vis, with partial rows', function () {
+        // since we have partialRows we expect that one row will have some empty
+        // values, and since the vis is hierarchical and we are NOT using
+        // minimalColumns we should expect the partial row to be completely after
+        // the existing bucket and it's metric
 
-      var esResp = _.cloneDeep(fixtures.threeTermBuckets);
-      // remove the buckets for css              in MX
-      esResp.aggregations.agg_2.buckets[1].agg_3.buckets[0].agg_4.buckets = [];
-      var resp = tabifyAggResponse(vis, esResp);
-
-      function verifyExtensionSplit(tableGroup, key) {
-        expect(tableGroup).to.have.property('tables');
-        expect(tableGroup).to.have.property('aggConfig', ext);
-        expect(tableGroup).to.have.property('key', key);
-        expect(tableGroup.tables).to.have.length(1);
-
-        tableGroup.tables.forEach(function (table) {
-          verifyTable(table, key);
+        vis.isHierarchical = _.constant(true);
+        var tabbed = tabifyAggResponse(vis, esResp, {
+          partialRows: true
         });
-      }
 
-      function verifyTable(table, splitKey) {
-        expect(table.columns).to.have.length(4);
-        expect(table.columns[0]).to.have.property('aggConfig', src);
-        expect(table.columns[1]).to.have.property('aggConfig', avg);
-        expect(table.columns[2]).to.have.property('aggConfig', os);
-        expect(table.columns[3]).to.have.property('aggConfig', avg);
+        expectRootGroup(tabbed, function expectTable(table, splitKey) {
+          expectColumns(table, [src, avg, os, avg]);
 
-        table.rows.forEach(function (row) {
-          expect(row).to.have.length(4);
-
-          // two character country code
-          expect(row[0]).to.be.a('string');
-          expect(row[0]).to.have.length(2);
-
-          // average bytes
-          expect(row[1]).to.be.a('number');
-          expect(row[1] === 0 || row[1] > 1000).to.be.ok();
-
-          if (splitKey === 'css' && row[0] === 'MX') {
-            // removed these buckets, we should get empty values for the bucket
-            expect(row[2]).to.be('');
-            // but we should have the metric from the MX bucket
-            expect(row[3]).to.be(9299);
-          } else {
-            // os
-            expect(row[2]).to.match(/^(win|mac|linux)$/);
-
-            // average bytes
-            expect(row[3]).to.be.a('number');
-            expect(row[3] === 0 || row[3] > 1000).to.be.ok();
-          }
+          table.rows.forEach(function (row) {
+            if (splitKey === 'css' && row[0] === 'MX') {
+              expectRow(row, [
+                expectCountry,
+                expectAvgBytes,
+                expectEmpty,
+                expectEmpty
+              ]);
+            } else {
+              expectRow(row, [
+                expectCountry,
+                expectAvgBytes,
+                expectOS,
+                expectAvgBytes
+              ]);
+            }
+          });
         });
-      }
+      });
 
-      expect(resp).to.have.property('tables');
-      expect(resp.tables).to.have.length(3);
-      verifyExtensionSplit(resp.tables[0], 'png');
-      verifyExtensionSplit(resp.tables[1], 'css');
-      verifyExtensionSplit(resp.tables[2], 'html');
+      it('for hierarchical vis, with partial rows, and minimal columns', function () {
+        // since we have partialRows we expect that one row has some empty
+        // values, and since the vis is hierarchical and we are displaying using
+        // minimalColumns, we should expect the partial row to have a metric at
+        // the end
+
+        vis.isHierarchical = _.constant(true);
+        var tabbed = tabifyAggResponse(vis, esResp, {
+          partialRows: true,
+          minimalColumns: true
+        });
+
+        expectRootGroup(tabbed, function expectTable(table, splitKey) {
+          expectColumns(table, [src, os, avg]);
+
+          table.rows.forEach(function (row) {
+            if (splitKey === 'css' && row[0] === 'MX') {
+              expectRow(row, [
+                expectCountry,
+                expectEmpty,
+                expectVal(9299)
+              ]);
+            } else {
+              expectRow(row, [
+                expectCountry,
+                expectOS,
+                expectAvgBytes
+              ]);
+            }
+          });
+        });
+      });
+
+      it('for non-hierarchical vis, minimal columns set to false', function () {
+        // the reason for this test is mainly to check that setting
+        // minimalColumns = false on a non-hierarchical vis doesn't
+        // create metric columns after each bucket
+
+        vis.isHierarchical = _.constant(false);
+        var tabbed = tabifyAggResponse(vis, esResp, {
+          minimalColumns: false
+        });
+
+        expectRootGroup(tabbed, function expectTable(table, splitKey) {
+          expectColumns(table, [src, os, avg]);
+
+          table.rows.forEach(function (row) {
+            expectRow(row, [
+              expectCountry,
+              expectOS,
+              expectAvgBytes
+            ]);
+          });
+        });
+      });
     });
   }];
 });
