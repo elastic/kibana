70d614063531b6402440caeab6c3dcd6c9070e0e
Spencer Alger <spencer@spenceralger.com>
2014-11-18 15:47:05 -0700

    [geoJson] goodbye 'flat' converter

--- src/kibana/components/agg_response/index.js
+++ src/kibana/components/agg_response/index.js
@@ -2,9 +2,9 @@ define(function (require) {
   return function NormalizeChartDataFactory(Private) {
     return {
       hierarchical: Private(require('components/agg_response/hierarchical/build_hierarchical_data')),
-      point_series: Private(require('components/agg_response/point_series/point_series')),
+      pointSeries: Private(require('components/agg_response/point_series/point_series')),
       tabify: Private(require('components/agg_response/tabify/tabify')),
-      flat: Private(require('components/agg_response/flat'))
+      geoJson: Private(require('components/agg_response/geo_json/geo_json'))
     };
   };
 });
---
+++ src/kibana/utils/decode_geo_hash.js
@@ -0,0 +1,60 @@
+define(function (require) {
+
+  /*
+   * Decodes geohash to object containing
+   * top-left and bottom-right corners of
+   * rectangle and center point.
+   *
+   * geohash.js
+   * Geohash library for Javascript
+   * (c) 2008 David Troy
+   * Distributed under the MIT License
+   *
+   * @method refine_interval
+   * @param interval {Array} [long, lat]
+   * @param cd {Number}
+   * @param mask {Number}
+   * @return {Object} interval
+   */
+  function decodeGeoHash(geohash) {
+    var BITS = [16, 8, 4, 2, 1];
+    var BASE32 = '0123456789bcdefghjkmnpqrstuvwxyz';
+    var is_even = 1;
+    var lat = [];
+    var lon = [];
+    lat[0] = -90.0;
+    lat[1] = 90.0;
+    lon[0] = -180.0;
+    lon[1] = 180.0;
+    var lat_err = 90.0;
+    var lon_err = 180.0;
+    for (var i = 0; i < geohash.length; i++) {
+      var c = geohash[i];
+      var cd = BASE32.indexOf(c);
+      for (var j = 0; j < 5; j++) {
+        var mask = BITS[j];
+        if (is_even) {
+          lon_err /= 2;
+          refine_interval(lon, cd, mask);
+        } else {
+          lat_err /= 2;
+          refine_interval(lat, cd, mask);
+        }
+        is_even = !is_even;
+      }
+    }
+    lat[2] = (lat[0] + lat[1]) / 2;
+    lon[2] = (lon[0] + lon[1]) / 2;
+    return { latitude: lat, longitude: lon};
+  }
+
+  function refine_interval(interval, cd, mask) {
+    if (cd & mask) {
+      interval[0] = (interval[0] + interval[1]) / 2;
+    } else {
+      interval[1] = (interval[0] + interval[1]) / 2;
+    }
+  }
+
+  return decodeGeoHash;
+});
