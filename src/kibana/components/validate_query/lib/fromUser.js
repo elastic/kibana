da0b1abe8deb6a6bb0ff63cb69cb2464413ec01c
Rashid Khan <rashidkpc@gmail.com>
2014-11-18 16:56:45 -0700

    Fix validation of query strings containing only a number. Closes #1832

--- src/kibana/components/validate_query/lib/fromUser.js
+++ src/kibana/components/validate_query/lib/fromUser.js
@@ -7,21 +7,29 @@ define(function (require) {
    * @returns {object}
    */
   return function (text) {
+    var matchAll = {query_string: {query: '*'}};
+
     // If we get an empty object, treat it as a *
     if (_.isObject(text)) {
       if (Object.keys(text).length) {
         return text;
       } else {
-        return {query_string: {query: '*'}};
+        return matchAll;
       }
     }
 
     // Nope, not an object.
     text = (text || '').trim();
-    try {
-      return JSON.parse(text);
-    } catch (e) {
-      return {query_string: {query: text || '*'}};
+    if (text.length === 0) return matchAll;
+
+    if (text[0] === '{') {
+      try {
+        return JSON.parse(text);
+      } catch (e) {
+        return {query_string: {query: text}};
+      }
+    } else {
+      return {query_string: {query: text}};
     }
   };
 });
