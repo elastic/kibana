e2d36277cada26b26700b6cb463d05659fdad421
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-20 22:49:47 +0200

    removing data file

--- test/unit/specs/vislib/fixture/mock_data/terms/slices.js
+++
@@ -1,7 +0,0 @@
-define(function (require) {
-  var module = require('modules').get('test/unit/spec/vislib/pie', ['kibana']);
-  require('service/private');
-
-  module.service('pie', function (Private) {
-  });
-});
