c64236b747590e178c212a5f3bf72b8823383c18
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-11-21 01:21:02 +0200

    Closes issue #1919. Adding unit tests for the line chart

---
+++ test/unit/specs/vislib/visualizations/line_chart.js
@@ -0,0 +1,155 @@
+define(function (require) {
+  var angular = require('angular');
+  var _ = require('lodash');
+  var $ = require('jquery');
+
+  // Data
+  var series = require('vislib_fixtures/mock_data/date_histogram/_series');
+  var columns = require('vislib_fixtures/mock_data/date_histogram/_columns');
+  var rows = require('vislib_fixtures/mock_data/date_histogram/_rows');
+  var termSeries = require('vislib_fixtures/mock_data/terms/_series');
+  var termColumns = require('vislib_fixtures/mock_data/terms/_columns');
+  var dateHistogramArray = [
+    series,
+    columns,
+    rows,
+    termSeries,
+    termColumns
+  ];
+  var names = [
+    'series',
+    'columns',
+    'rows',
+    'term series',
+    'term columns'
+  ];
+
+  angular.module('LineChartFactory', ['kibana']);
+
+  dateHistogramArray.forEach(function (data, i) {
+    describe('VisLib Line Chart Test Suite for ' + names[i] + ' Data', function () {
+      var vis;
+      var visLibParams = {
+        type: 'line',
+        addLegend: true,
+        addTooltip: true
+      };
+
+      beforeEach(function () {
+        module('LineChartFactory');
+      });
+
+      beforeEach(function () {
+        inject(function (Private) {
+          vis = Private(require('vislib_fixtures/_vis_fixture'))(visLibParams);
+          require('css!components/vislib/styles/main');
+
+          vis.render(data);
+        });
+      });
+
+      afterEach(function () {
+        $(vis.el).remove();
+        vis = null;
+      });
+
+      describe('addCircleEvents method', function () {
+        var circle;
+        var d3selectedCircle;
+        var onClick;
+        var onMouseOver;
+
+        beforeEach(function () {
+          inject(function (d3) {
+            vis.handler.charts.forEach(function (chart) {
+              circle = $(chart.chartEl).find('circle')[0];
+              d3selectedCircle = d3.select(circle)[0][0];
+
+              // d3 instance of click and hover
+              onClick = (!!d3selectedCircle.__onclick);
+              onMouseOver = (!!d3selectedCircle.__onmouseover);
+            });
+          });
+        });
+
+        it('should attach a click event', function () {
+          vis.handler.charts.forEach(function () {
+            expect(onClick).to.be(true);
+          });
+        });
+
+        it('should attach a hover event', function () {
+          vis.handler.charts.forEach(function () {
+            expect(onMouseOver).to.be(true);
+          });
+        });
+      });
+
+      describe('addCircles method', function () {
+        it('should append circles', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect($(chart.chartEl).find('circle').length).to.be.greaterThan(0);
+          });
+        });
+      });
+
+      describe('addLines method', function () {
+        it('should append a paths', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect($(chart.chartEl).find('path').length).to.be.greaterThan(0);
+          });
+        });
+      });
+
+      describe('addClipPath method', function () {
+        var clipPathY;
+
+        beforeEach(function () {
+          inject(function (d3) {
+            vis.handler.charts.forEach(function (chart) {
+
+              // First rect should be the clipPath
+              clipPathY = parseInt(d3.select(chart.chartEl).select('rect').attr('y'), 10);
+            });
+          });
+        });
+
+        it('should append a clipPath', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect($(chart.chartEl).find('clipPath').length).to.be(1);
+          });
+        });
+
+        // Having a clipPath y value of -5 means that the circles
+        // at the top of the chart will not be cut off by the clipPath
+        it('should have a clipPath y value of -5', function () {
+          expect(clipPathY).to.be(-5);
+        });
+      });
+
+      describe('draw method', function () {
+        it('should return a function', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect(_.isFunction(chart.draw())).to.be(true);
+          });
+        });
+      });
+
+      describe('containerTooSmall error', function () {
+        beforeEach(function () {
+          $(vis.el).height(0);
+          $(vis.el).width(0);
+        });
+
+        it('should throw an error', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect(function () {
+              chart.render();
+            }).to.throwError();
+          });
+        });
+      });
+
+    });
+  });
+});
