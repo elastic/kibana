97719b3dfe31ec094ec59497a66a6c5a7cd4cfac
Joe Fleming <joe.fleming@gmail.com>
2014-11-21 17:54:21 -0700

    leave sorting up to the parginated table, but allow custom sorter if needed

--- src/kibana/components/paginated_table/paginated_table.js
+++ src/kibana/components/paginated_table/paginated_table.js
@@ -1,8 +1,9 @@
 define(function (require) {
   require('modules')
   .get('kibana')
-  .directive('paginatedTable', function (config, Private) {
+  .directive('paginatedTable', function ($filter, config, Private) {
     var _ = require('lodash');
+    var orderBy = $filter('orderBy');
 
     return {
       restrict: 'E',
@@ -24,29 +25,51 @@ define(function (require) {
         };
 
         self.sortColumn = function (col) {
-          if (!$scope.sortHandler) return;
+          // use custom sort handler if one was defined
+          if (_.isFunction($scope.sortHandler)) {
+            $scope.sortHandler(col, self.sort);
+          }
 
-          // TODO: track sort direction
-
+          // use generic sort handler
           self._setSortDirection(col.title);
-          $scope.sortHandler(col, self.sort);
         };
 
         self._setSortDirection = function (columnName) {
+          var sortDirection;
+          var cols = _.pluck($scope.columns, 'title');
+          var index = cols.indexOf(columnName);
+
+          if (index === -1) return;
+
+          var sortGetter = function (row) {
+            return row[index];
+          };
+
           if (self.sort.columnName !== columnName) {
-            self.sort.direction = 'asc';
+            sortDirection = 'asc';
           } else {
             var directions = {
               null: 'asc',
               'asc': 'desc',
               'desc': null
             };
-            var direction = directions[self.sort.direction];
-            self.sort.direction = direction;
-
+            sortDirection = directions[self.sort.direction];
           }
+
           self.sort.columnName = columnName;
+          self.sort.getter = sortGetter;
+          self.sort.direction = sortDirection;
         };
+
+        // update the sordedRows result
+        $scope.$watch('paginatedTable.sort.direction', function () {
+          if (self.sort.direction == null) {
+            $scope.sortedRows = $scope.rows.slice(0);
+            return;
+          }
+
+          $scope.sortedRows = orderBy($scope.rows, self.sort.getter, self.sort.direction === 'desc');
+        });
       }
     };
   });
