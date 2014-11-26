d520b37cea884b2c03407919be4db1567487a3d0
Joe Fleming <joe.fleming@gmail.com>
2014-11-25 17:42:31 -0700

    consolidate router, add real title

--- src/kibana/plugins/settings/sections/indices/scripted_fields/index.js
+++ src/kibana/plugins/settings/sections/indices/scripted_fields/index.js
@@ -4,29 +4,24 @@ define(function (require) {
   require('plugins/settings/sections/indices/_scripted_fields');
 
   require('routes')
+  .addResolves(/settings\/indices\/(.+)\/.+Field/, {
+    indexPattern: function ($route, courier) {
+      return courier.indexPatterns.get($route.current.params.id)
+      .catch(courier.redirectWhenMissing('/settings/indices'));
+    }
+  })
   .when('/settings/indices/:id/createField', {
     template: require('text!plugins/settings/sections/indices/scripted_fields/index.html'),
-    resolve: {
-      indexPattern: function ($route, courier) {
-        return courier.indexPatterns.get($route.current.params.id)
-        .catch(courier.redirectWhenMissing('/settings/indices'));
-      }
-    }
   })
   .when('/settings/indices/:id/editField/:field', {
     template: require('text!plugins/settings/sections/indices/scripted_fields/index.html'),
-    resolve: {
-      indexPattern: function ($route, courier) {
-        return courier.indexPatterns.get($route.current.params.id)
-        .catch(courier.redirectWhenMissing('/settings/indices'));
-      }
-    }
   });
 
   require('modules').get('apps/settings')
   .controller('scriptedFieldsEdit', function ($scope, $route, Notifier) {
     var notify = new Notifier();
 
-    console.log($route.current);
+    $scope.action = ($route.current.params.field) ? 'Edit' : 'Create';
+    $scope.indexPattern = $route.current.locals.indexPattern;
   });
 });
