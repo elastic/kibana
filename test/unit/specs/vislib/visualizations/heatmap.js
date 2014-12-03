004af6164fd2a6440c941487f447968fc97f9e91
Juan Thomassie <juan.thomassie@elasticsearch.com>
2014-12-03 17:10:30 -0600

    added basic heatmap test

--- test/unit/specs/vislib/visualizations/heatmap.js
+++ test/unit/specs/vislib/visualizations/heatmap.js
@@ -4,27 +4,29 @@ define(function (require) {
   var $ = require('jquery');
 
   // Data
-  var series = require('vislib_fixtures/mock_data/terms/_series');
-  var columns = require('vislib_fixtures/mock_data/terms/_columns');
-  var rows = require('vislib_fixtures/mock_data/terms/_rows');
-  //var termSeries = require('vislib_fixtures/mock_data/terms/_series');
-  //var termColumns = require('vislib_fixtures/mock_data/terms/_columns');
+  var series = require('vislib_fixtures/mock_data/date_histogram/_series');
+  var columns = require('vislib_fixtures/mock_data/date_histogram/_columns');
+  var rows = require('vislib_fixtures/mock_data/date_histogram/_rows');
+  var stackedSeries = require('vislib_fixtures/mock_data/date_histogram/_stacked_series');
+  var termSeries = require('vislib_fixtures/mock_data/terms/_series');
+  var termColumns = require('vislib_fixtures/mock_data/terms/_columns');
 
   var dataArray = [
     series,
     columns,
-    rows
-    //termSeries,
-    //termColumns
+    rows,
+    stackedSeries,
+    termSeries,
+    termColumns
   ];
 
   var names = [
     'series',
     'columns',
-    'rows'
-    //'stackedSeries',
-    //'term series',
-    //'term columns'
+    'rows',
+    'stackedSeries',
+    'term series',
+    'term columns'
   ];
 
   var visLibParams = {
@@ -33,65 +35,52 @@ define(function (require) {
     addTooltip: true
   };
 
-  angular.module('HeatMapFactory', ['kibana']);
+  angular.module('HeatmapFactory', ['kibana']);
 
   dataArray.forEach(function (data, i) {
     describe('VisLib Heatmap Test Suite for ' + names[i] + ' Data', function () {
       var vis;
 
       beforeEach(function () {
-        module('HeatMapFactory');
+        module('HeatmapFactory');
       });
 
       beforeEach(function () {
         inject(function (Private) {
           vis = Private(require('vislib_fixtures/_vis_fixture'))(visLibParams);
           require('css!components/vislib/styles/main');
-          console.log('data', data);
+
           vis.render(data);
         });
       });
 
-      //afterEach(function () {
+      afterEach(function () {
         //$(vis.el).remove();
         //vis = null;
-      //});
+      });
 
-      describe('checkTest method', function () {
-        //var errorVis;
-        var goodVis;
-        //var notEnoughData;
-        var enoughData;
-
-        beforeEach(function () {
-          inject(function (d3, Private) {
-            //errorVis = Private(require('vislib_fixtures/_vis_fixture'))(visLibParams);
-            goodVis = Private(require('vislib_fixtures/_vis_fixture'))(visLibParams);
-            enoughData = require('vislib_fixtures/mock_data/date_histogram/_series');
-            //notEnoughData = require('vislib_fixtures/mock_data/not_enough_data/_one_point');
-            require('css!components/vislib/styles/main');
-
-            //errorVis.render(notEnoughData);
-            goodVis.render(enoughData);
-          });
-        });
-
-        afterEach(function () {
-          //$(errorVis.el).remove();
-          //$(goodVis.el).remove();
-          //errorVis = null;
-          //goodVis = null;
-        });
-
-        it('should not throw a Not Enough Data Error', function () {
-          goodVis.handler.charts.forEach(function (chart) {
-            expect(function () {
-              chart.checkTest();
-            }).to.throwError();
+      describe('draw method', function () {
+        it('should return a function', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect(_.isFunction(chart.draw())).to.be(true);
           });
         });
       });
 
+      describe('containerTooSmall error', function () {
+        beforeEach(function () {
+          $(vis.el).height(0);
+          $(vis.el).width(0);
+        });
+
+        it('should throw an error', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect(function () {
+              chart.render();
+            }).to.throwError();
+          });
+        });
+      });
     });
   });
 });
