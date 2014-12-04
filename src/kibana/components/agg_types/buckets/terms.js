44ebbecd658a9093436583572cb2af6c5e1d7dff
Joe Fleming <joe.fleming@gmail.com>
2014-12-03 18:09:34 -0700

    remove script param, remove script param input, fix indexedFields tab state

--- src/kibana/components/agg_types/buckets/terms.js
+++ src/kibana/components/agg_types/buckets/terms.js
@@ -18,10 +18,6 @@ define(function (require) {
           filterFieldTypes: ['number', 'boolean', 'date', 'ip',  'string']
         },
         {
-          name: 'script'
-          // no editor, will be used in place of field
-        },
-        {
           name: 'size',
           default: 5
           // editor: batched with order
