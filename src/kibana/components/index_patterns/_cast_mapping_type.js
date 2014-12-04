cf1f8f74bc6612e2b201112fa1549ca4239306aa
Joe Fleming <joe.fleming@gmail.com>
2014-12-03 19:01:08 -0700

    expose type maps on the CastMappingType function

--- src/kibana/components/index_patterns/_cast_mapping_type.js
+++ src/kibana/components/index_patterns/_cast_mapping_type.js
@@ -1,30 +1,33 @@
 define(function (require) {
   return function CastMappingTypeFn() {
+    castMappingType.types = {
+      'float': 'number',
+      'double': 'number',
+      'integer': 'number',
+      'long': 'number',
+      'short': 'number',
+      'byte': 'number',
+      'token_count': 'number',
+
+      'date': 'date',
+      'boolean': 'boolean',
+      'ip': 'ip',
+      'attachment': 'attachment',
+      'geo_point': 'geo_point',
+      'geo_shape': 'geo_shape',
+      'string': 'string'
+    };
+
     /**
      * Accepts a mapping type, and converts it into it's js equivilent
      * @param  {String} type - the type from the mapping's 'type' field
      * @return {String} - the most specific type that we care for
      */
-    return function castMappingType(type) {
-      switch (type) {
-      case 'float':
-      case 'double':
-      case 'integer':
-      case 'long':
-      case 'short':
-      case 'byte':
-      case 'token_count':
-        return 'number';
-      case 'date':
-      case 'boolean':
-      case 'ip':
-      case 'attachment':
-      case 'geo_point':
-      case 'geo_shape':
-        return type;
-      default: // including 'string'
-        return 'string';
-      }
-    };
+    function castMappingType(type) {
+      if (castMappingType.types[type]) return castMappingType.types[type];
+      return 'string';
+    }
+
+    return castMappingType;
   };
 });
