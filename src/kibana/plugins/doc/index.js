a755b738aadac24ba4ba10dd959eddfc03425bd8
Rashid Khan <rashidkpc@gmail.com>
2014-12-05 11:28:26 -0700

    Remove unused less

--- src/kibana/plugins/doc/index.js
+++ src/kibana/plugins/doc/index.js
@@ -1,6 +1,5 @@
 define(function (require, module, exports) {
   require('plugins/doc/controllers/doc');
-  require('css!plugins/doc/styles/main.css');
 
   var apps = require('registry/apps');
   apps.register(function DocAppModule() {
