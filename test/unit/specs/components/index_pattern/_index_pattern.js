b35a2a36ff491fc5cdef9fd88ada5d3fae4dfdf3
Joe Fleming <joe.fleming@gmail.com>
2014-12-09 16:41:55 -0700

    mock dependencies and create first passing test

---
+++ test/unit/specs/components/index_pattern/_index_pattern.js
@@ -0,0 +1,47 @@
+define(function (require) {
+  return ['Index Pattern', function () {
+    var _ = require('lodash');
+    var sinon = require('test_utils/auto_release_sinon');
+    var Promise = require('bluebird');
+    var IndexPattern;
+    var mappingSetup;
+    var DocSource;
+    var config;
+    var docSourceResponse;
+
+    beforeEach(module('kibana'));
+    beforeEach(inject(function (Private, $injector, _config_) {
+      config = _config_;
+      docSourceResponse = Private(require('fixtures/stubbed_doc_source_response'));
+
+      DocSource = Private(require('components/courier/data_source/doc_source'));
+      sinon.stub(DocSource.prototype, 'doUpdate');
+      sinon.stub(DocSource.prototype, 'doIndex');
+      sinon.stub(DocSource.prototype, 'fetch');
+
+      // stub mappingSetup
+      mappingSetup = Private(require('utils/mapping_setup'));
+      sinon.stub(mappingSetup, 'isDefined', function () {
+        return Promise.resolve(true);
+      });
+
+      IndexPattern = Private(require('components/index_patterns/_index_pattern'));
+    }));
+
+    // helper function to create index patterns
+    function create(id, payload) {
+      var indexPattern = new IndexPattern(id);
+      payload = _.defaults(payload || {}, docSourceResponse(id));
+      DocSource.prototype.fetch.returns(Promise.resolve(payload));
+      return indexPattern.init();
+    }
+
+    describe('init', function () {
+      it('should call docsource fetch', function () {
+        return create('test-pattern').then(function (indexPattern) {
+          expect(DocSource.prototype.fetch.callCount).to.be(1);
+        });
+      });
+    });
+  }];
+});
