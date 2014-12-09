a4567d6f9e54d07c384b668598b04a9d10924d6f
Juan Thomassie <juan.thomassie@elasticsearch.com>
2014-12-09 11:01:48 -0600

    added heatmap tests

--- test/unit/specs/vislib/visualizations/heatmap.js
+++ test/unit/specs/vislib/visualizations/heatmap.js
@@ -7,7 +7,6 @@ define(function (require) {
   var series = require('vislib_fixtures/mock_data/date_histogram/_series');
   var columns = require('vislib_fixtures/mock_data/date_histogram/_columns');
   var rows = require('vislib_fixtures/mock_data/date_histogram/_rows');
-  var stackedSeries = require('vislib_fixtures/mock_data/date_histogram/_stacked_series');
   var termSeries = require('vislib_fixtures/mock_data/terms/_series');
   var termColumns = require('vislib_fixtures/mock_data/terms/_columns');
 
@@ -15,7 +14,6 @@ define(function (require) {
     series,
     columns,
     rows,
-    stackedSeries,
     termSeries,
     termColumns
   ];
@@ -24,7 +22,6 @@ define(function (require) {
     'series',
     'columns',
     'rows',
-    'stackedSeries',
     'term series',
     'term columns'
   ];
@@ -55,8 +52,8 @@ define(function (require) {
       });
 
       afterEach(function () {
-        //$(vis.el).remove();
-        //vis = null;
+        $(vis.el).remove();
+        vis = null;
       });
 
       describe('draw method', function () {
@@ -81,6 +78,91 @@ define(function (require) {
           });
         });
       });
+
+
+      describe('addRowLabels Method', function () {
+        var minGridLabelHeight = 9;
+        var padding = 3;
+
+        it('should return proper response', function () {
+          vis.handler.charts.forEach(function (chart) {
+            var margin = chart._attr.margin;
+            var elHeight = $(chart.chartEl).height();
+            var height = elHeight - margin.top - margin.bottom;
+            var heightN = chart.chartData.series.length;
+            var gridHeight = height / heightN;
+            var maxTextLength = chart.maxRowTextLength;
+
+            if (gridHeight < minGridLabelHeight) {
+              expect($('.heat-axis-label').length).to.be(0);
+            } else if (gridHeight >= minGridLabelHeight && (maxTextLength + padding) <= margin.left) {
+              expect($('.heat-axis-label').length).to.be.greaterThan(0);
+            }
+          });
+        });
+      });
+
+      describe('addColLabels Method', function () {
+        var minGridLabelWidth = 9;
+        var padding = 3;
+
+        it('should return proper response', function () {
+          vis.handler.charts.forEach(function (chart) {
+            var margin = chart._attr.margin;
+            var elWidth = $(chart.chartEl).width();
+            var width = elWidth - margin.left - margin.right;
+            var widthN = chart.chartData.series[0].values.length;
+            var gridWidth = width / widthN;
+            var maxTextLength = chart.maxColTextLength;
+
+            if (gridWidth >= minGridLabelWidth && (maxTextLength + padding) <= gridWidth) {
+              expect($('.heat-axis-label').length).to.be.greaterThan(0);
+            }
+          });
+        });
+      });
+
+      describe('maxTextLength Method', function () {
+        var selection;
+
+        beforeEach(function () {
+          inject(function (d3) {
+            selection = d3.selectAll('.heat-axis-label');
+          });
+        });
+
+        it('should return a number', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect(_.isNumber(chart.maxTextLength(selection))).to.be(true);
+          });
+        });
+      });
+
+      describe('dataColor Method', function () {
+        var zero = {x: '', row: '', y: 0};
+        var val = {x: '', row: '', y: 120};
+        var hexColor;
+        var isHexColor;
+
+        it('should return a hex color', function () {
+          vis.handler.charts.forEach(function (chart) {
+            hexColor = chart.dataColor(val);
+            isHexColor = /^#[0-9A-F]{6}$/i.test(hexColor);
+
+            expect(chart.dataColor(zero)).to.be('#ededed');
+            expect(isHexColor).to.be(true);
+          });
+        });
+      });
+
+      describe('addRects Method', function () {
+        it('should return rects', function () {
+          vis.handler.charts.forEach(function (chart) {
+            expect($(chart.chartEl).find('rect').length).to.be.greaterThan(0);
+          });
+        });
+      });
+
     });
   });
 });
