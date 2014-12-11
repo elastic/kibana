5351c2e36ba5383282d71d9d613f5e8772c6d1a8
Rashid Khan <rashidkpc@gmail.com>
2014-12-11 10:11:41 -0700

    Bump min version to ES 1.4.0

--- src/kibana/index.js
+++ src/kibana/index.js
@@ -33,7 +33,7 @@ define(function (require) {
     // Use this for cache busting partials
     .constant('cacheBust', window.KIBANA_COMMIT_SHA)
     // The minimum Elasticsearch version required to run Kibana
-    .constant('minimumElasticsearchVersion', '1.4.0.Beta1')
+    .constant('minimumElasticsearchVersion', '1.4.0')
     // When we need to identify the current session of the app, ef shard preference
     .constant('sessionId', Date.now())
     // attach the route manager's known routes
