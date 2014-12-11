elastic/kibana

commit e97a1ddea782723376cca531ee40ddb109f3332a

Fixing non-timebased filtering to make sure changeTimeFilter is set

src/kibana/components/filter_bar/filter_bar.js
@@ -29,9 +29,10 @@ define(function (require) {
 
         $scope.applyFilters = function (filters) {
           var newFilters = filterAppliedAndUnwrap(filters);
+          var changeTimeFilter = $scope.changeTimeFilter;
           $scope.state.filters = _.union($scope.state.filters, newFilters);
           $scope.newFilters = [];
-          if ($scope.changeTimeFilter.meta.apply) {
+          if (changeTimeFilter && changeTimeFilter.meta && changeTimeFilter.meta.apply) {
             changeTimeFilter($scope.changeTimeFilter);
           }
         };

commit 0f7b687d4340ec523f1f720af7db956407e92105

[filterBar] fix bug introduced in e97a1dde

src/kibana/components/filter_bar/filter_bar.js
@@ -29,10 +29,9 @@ define(function (require) {
 
         $scope.applyFilters = function (filters) {
           var newFilters = filterAppliedAndUnwrap(filters);
-          var changeTimeFilter = $scope.changeTimeFilter;
           $scope.state.filters = _.union($scope.state.filters, newFilters);
           $scope.newFilters = [];
-          if (changeTimeFilter && changeTimeFilter.meta && changeTimeFilter.meta.apply) {
+          if ($scope.changeTimeFilter && $scope.changeTimeFilter.meta && $scope.changeTimeFilter.meta.apply) {
             changeTimeFilter($scope.changeTimeFilter);
           }
         };