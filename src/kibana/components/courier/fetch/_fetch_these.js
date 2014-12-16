40cc3cb932d36bb4ce9c68532b6dd118c8574664
Spencer Alger <spencer@spenceralger.com>
2014-12-16 02:06:37 -0700

    [courier/fetchThese] reformatted and teased a bit

--- src/kibana/components/courier/fetch/_fetch_these.js
+++ src/kibana/components/courier/fetch/_fetch_these.js
@@ -9,122 +9,140 @@ define(function (require) {
       location: 'Courier Fetch'
     });
 
-    function perStrategy(requests, each) {
-      each = Promise.method(each);
+    function eachStrategy(requests, block) {
+      block = Promise.method(block);
       var sets = [];
 
       requests.forEach(function (req) {
-        var strategy = req.strategy || req.source._fetchStrategy;
+        var strategy = req.strategy;
         var set = _.find(sets, { 0: strategy });
         if (set) set[1].push(req);
         else sets.push([strategy, [req]]);
       });
 
       return Promise.all(sets.map(function (set) {
-        (function fetch(requests, strategy) {
-          return each(requests, strategy)
-          .then(function (result) {
+        return (function fetch(requests, strategy) {
+          return block(requests, strategy)
+          .then(function checkForIncompleteRequests(result) {
             if (_.isFunction(strategy.getIncompleteRequests)) {
               var incomplete = strategy.getIncompleteRequests(pendingRequests);
               if (incomplete.length) {
                 return fetch(incomplete, strategy);
               }
             }
+            return result;
           });
         }(set[1], set[0]));
-      }));
+      }))
+      .catch(notify.fatal);
     }
 
-    function fetchThese(requests, reqErrHandler) {
-      return perStrategy(requests, function (requests, strategy) {
-        var all, body;
+    function initRequest(req) {
+      if (req.source.activeFetchCount) {
+        req.source.activeFetchCount += 1;
+      } else {
+        req.source.activeFetchCount = 1;
+      }
 
-        // dedupe requests
-        var uniqs = {};
-        all = requests.splice(0).filter(function (req) {
-          if (req.source.activeFetchCount) {
-            req.source.activeFetchCount += 1;
-          } else {
-            req.source.activeFetchCount = 1;
-          }
+      req.moment = moment();
+    }
 
-          req.moment = moment();
+    function mergeDuplicateRequests(requests) {
+      // dedupe requests
+      var index = {};
+      return requests.splice(0).filter(function (req) {
+        var iid = req.source._instanceid;
+        if (!index[iid]) {
+          // this request is unique so far
+          index[iid] = req;
+          // keep the request
+          return true;
+        }
 
-          var iid = req.source._instanceid;
-          if (!uniqs[iid]) {
-            // this request is unique so far
-            uniqs[iid] = req;
-            // keep the request
-            return true;
-          }
+        // the source was requested at least twice
+        var uniq = index[iid];
+        if (uniq._merged) {
+          // already setup the merged list
+          uniq._merged.push(req);
+        } else {
+          // put all requests into this array and itterate them on response
+          uniq._merged = [uniq, req];
+        }
+      });
+    }
 
-          // the source was requested at least twice
-          var uniq = uniqs[iid];
-          if (uniq._merged) {
-            // already setup the merged list
-            uniq._merged.push(req);
-          } else {
-            // put all requests into this array and itterate them on response
-            uniq._merged = [uniq, req];
-          }
-        });
+    function reqComplete(req, resp, errorHandler) {
+      if (resp.timed_out) {
+        notify.warning(new errors.SearchTimeout());
+      }
 
-        return Promise.map(all, _.limit(strategy.getSourceStateFromRequest, 1))
-        .then(function (states) {
+      req.complete = true;
+      req.resp = resp;
+      req.ms = req.moment.diff() * -1;
+      req.source.activeFetchCount -= 1;
+
+      if (resp.error) {
+        return errorHandler.handle(req, new errors.FetchFailure(resp));
+      }
+
+      req.strategy.resolveRequest(req, resp);
+    }
+
+    function fetchThese(requests, errorHandler) {
+      return eachStrategy(requests, function (requests, strategy) {
+        requests.forEach(initRequest);
+
+        var uniq = mergeDuplicateRequests(requests);
+        var states;
+
+        return Promise.map(uniq, function (req) {
+          return strategy.getSourceStateFromRequest(req);
+        })
+        .then(function (_states_) {
+          states = _states_;
+
           // all requests must have been disabled
-          if (!states.length) return Promise.resolve();
-
-          body = strategy.convertStatesToBody(states);
+          if (!states.length) return Promise.resolve(false);
 
           return es[strategy.clientMethod]({
             timeout: configFile.shard_timeout,
             preference: sessionId,
-            body: body
-          })
-          .then(function (resp) {
-            var sendResponse = function (req, resp) {
-              if (resp.timed_out) {
-                notify.warning(new errors.SearchTimeout());
-              }
-              req.complete = true;
-              req.resp = resp;
-              req.ms = req.moment.diff() * -1;
-              req.source.activeFetchCount -= 1;
-
-              if (resp.error) return reqErrHandler.handle(req, new errors.FetchFailure(resp));
-              else strategy.resolveRequest(req, resp);
-            };
-
-            strategy.getResponses(resp).forEach(function (resp) {
-              var req = all.shift();
-              var state = states.shift();
-              if (!req._merged) {
-                req.state = state;
-                sendResponse(req, resp);
-              } else {
-                req._merged.forEach(function (mergedReq) {
-                  mergedReq.state = state;
-                  sendResponse(mergedReq, _.cloneDeep(resp));
-                });
-              }
-            });
-
-            // pass the response along to the next promise
-            return resp;
-          })
-          .catch(function (err) {
-            var sendFailure = function (req) {
-              req.source.activeFetchCount -= 1;
-              reqErrHandler.handle(req, err);
-            };
-
-            all.forEach(function (req) {
-              if (!req._merged) sendFailure(req);
-              else req._merged.forEach(sendFailure);
-            });
-            throw err;
+            body: strategy.convertStatesToBody(states)
           });
-        }, notify.fatal);
+        })
+        .then(strategy.getResponses)
+        .then(function (responses) {
+
+          responses.forEach(function (resp) {
+            var req = uniq.shift();
+            var state = states.shift();
+            if (!req._merged) {
+              req.state = state;
+              reqComplete(req, resp, errorHandler);
+            } else {
+              req._merged.forEach(function (mergedReq) {
+                mergedReq.state = state;
+                var respClone = _.cloneDeep(resp);
+                reqComplete(mergedReq, respClone, errorHandler);
+              });
+            }
+          });
+
+          return responses;
+        })
+        .catch(function (err) {
+
+          function sendFailure(req) {
+            req.source.activeFetchCount -= 1;
+            errorHandler.handle(req, err);
+          }
+
+          uniq.forEach(function (req) {
+            if (!req._merged) sendFailure(req);
+            else req._merged.forEach(sendFailure);
+          });
+          throw err;
+        });
       });
     }
 
