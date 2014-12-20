e2c9ae50d865aab25ce17817bba930f4415f94db
Spencer Alger <spencer@spenceralger.com>
2014-12-19 17:00:34 -0700

    [courier] deleted tests for now

--- test/unit/specs/components/courier/fetch/fetch.tests.js
+++
@@ -1,7 +0,0 @@
-define(function (require) {
-  describe('Courier Fetch', function () {
-    run(require('specs/components/courier/fetch/strategy/_segmented'));
-    run(require('specs/components/courier/fetch/_segmented_state'));
-    function run(mod) { describe(mod[0], mod[1]); }
-  });
-});
