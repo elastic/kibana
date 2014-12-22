4bb43986c739e0014cbeca9cd11eb578ee9eb168
Joe Fleming <joe.fleming@gmail.com>
2014-12-22 16:47:14 -0700

    add test for handling filters that are just null

--- test/unit/specs/components/filter_bar/onlyDisabled.js
+++ test/unit/specs/components/filter_bar/onlyDisabled.js
@@ -13,7 +13,7 @@ define(function (require) {
         expect(onlyDisabled(newFilters, filters)).to.be(true);
       });
 
-      it('should return true if all filters remove are disabled', function () {
+      it('should return false if all filters remove are not disabled', function () {
         var filters = [
           { meta: { disabled: true } },
           { meta: { disabled: false } },
@@ -21,6 +21,17 @@ define(function (require) {
         ];
         var newFilters = [filters[1]];
         expect(onlyDisabled(newFilters, filters)).to.be(false);
+      });
+
+      it('should not throw with null filters', function () {
+        var filters = [
+          null,
+          { meta: { disabled: true } }
+        ];
+        var newFilters = [filters[1]];
+        expect(function () {
+          onlyDisabled(newFilters, filters);
+        }).to.not.throwError();
       });
 
     });
