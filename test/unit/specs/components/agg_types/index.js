c4176c90f0ea08dfb0e1cdafacc00cc6ca097388
Spencer Alger <spencer@spenceralger.com>
2015-01-04 22:32:00 -0700

    [aggTypes] tests that all AggTypes extend proper classes

--- test/unit/specs/components/agg_types/index.js
+++ test/unit/specs/components/agg_types/index.js
@@ -4,11 +4,45 @@ define(function (require) {
       require('specs/components/agg_types/_agg_type'),
       require('specs/components/agg_types/_agg_params'),
       require('specs/components/agg_types/_bucket_count_between'),
-      require('specs/components/agg_types/bucket_aggs/_histogram'),
-      require('specs/components/agg_types/bucket_aggs/_date_histogram'),
+      require('specs/components/agg_types/buckets/_histogram'),
+      require('specs/components/agg_types/buckets/_date_histogram'),
       require('specs/components/agg_types/_metric_aggs')
     ].forEach(function (s) {
       describe(s[0], s[1]);
+    });
+
+    describe('bucket aggs', function () {
+      var bucketAggs;
+      var BucketAggType;
+
+      beforeEach(module('kibana'));
+      beforeEach(inject(function (Private) {
+        bucketAggs = Private(require('components/agg_types/index')).byType.buckets;
+        BucketAggType = Private(require('components/agg_types/buckets/_bucket_agg_type'));
+      }));
+
+      it('all extend BucketAggType', function () {
+        bucketAggs.forEach(function (bucketAgg) {
+          expect(bucketAgg).to.be.a(BucketAggType);
+        });
+      });
+    });
+
+    describe('metric aggs', function () {
+      var metricAggs;
+      var MetricAggType;
+
+      beforeEach(module('kibana'));
+      beforeEach(inject(function (Private) {
+        metricAggs = Private(require('components/agg_types/index')).byType.metrics;
+        MetricAggType = Private(require('components/agg_types/metrics/_metric_agg_type'));
+      }));
+
+      it('all extend MetricAggType', function () {
+        metricAggs.forEach(function (metricAgg) {
+          expect(metricAgg).to.be.a(MetricAggType);
+        });
+      });
     });
   });
 });
