edfa33d15aefba3a8f388b91bda3b40257d594d3
Spencer Alger <spencer@spenceralger.com>
2015-01-07 07:23:16 -0700

    [$watchMulti] added collection and deep equality watching

--- src/kibana/components/watch_multi.js
+++ src/kibana/components/watch_multi.js
@@ -10,7 +10,26 @@ define(function (require) {
        * with making code simpler it also merges all of the watcher
        * handlers within a single tick.
        *
-       * @param  {array[string|function]} expressions - the list of expressions to $watch
+       * # expression format
+       * expressions can be specified in one of the following ways:
+       * 1. string that evaluates to a value on scope. Creates a regular $watch
+       *    expression.
+       *     'someScopeValue.prop' === $scope.$watch('someScopeValue.prop', fn);
+       *
+       * 2. #1 prefixed with '[]', which uses $watchCollection rather than $watch.
+       *     '[]expr' === $scope.$watchCollection('expr', fn);
+       *
+       * 3. #1 prefixed with '=', which uses $watch with objectEquality turned on
+       *     '=expr' === $scope.$watch('expr', fn, true);
+       *
+       * 4. a function that will be called, like a normal function water
+       *
+       * 5. an object with any of the properties:
+       *   `get`: the getter called on each itteration
+       *   `deep`: a flag to turn on objectEquality in $watch
+       *   `fn`: the watch registration function ($scope.$watch or $scope.$watchCollection)
+       *
+       * @param  {array[string|function|obj]} expressions - the list of expressions to $watch
        * @param  {Function} fn - the callback function
        * @param  {boolean} deep - should the watchers be created as deep watchers?
        * @return {undefined}
@@ -20,31 +39,53 @@ define(function (require) {
         if (!_.isFunction(fn)) throw new TypeError('expexted a function that is triggered on each watch');
 
         var $scope = this;
-        var initQueue = _.clone(expressions);
         var fired = false;
-        var vals = {
-          new: new Array(expressions.length),
-          old: new Array(expressions.length)
-        };
+        var queue = [];
+        var vals = new Array(expressions.length);
+        var prev = new Array(expressions.length);
+
+        function normalizeExpression(expr) {
+          if (!expr) return;
+
+          var norm = {
+            fn: $scope.$watch,
+            deep: false
+          };
+
+          if (_.isFunction(expr)) return _.assign(norm, { get: expr });
+          if (_.isObject(expr)) return _.assign(norm, expr);
+          if (!_.isString(expr)) return;
+
+          if (expr.substr(0, 2) === '[]') {
+            return _.assign(norm, {
+              fn: $scope.$watchCollection,
+              get: expr.substr(2)
+            });
+          }
+
+          if (expr.charAt(0) === '=') {
+            return _.assign(norm, {
+              deep: true,
+              get: expr.substr(1)
+            });
+          }
+
+          return _.assign(norm, { get: expr });
+        }
 
         expressions.forEach(function (expr, i) {
-          $scope.$watch(expr, function (newVal, oldVal) {
-            vals.new[i] = newVal;
+          expr = normalizeExpression(expr);
+          if (!expr) return;
 
-            if (initQueue) {
-              vals.old[i] = oldVal;
+          queue.push(expr);
+          expr.fn.call($scope, expr.get, function (newVal, oldVal) {
+            vals[i] = newVal;
 
-              var qIdx = initQueue.indexOf(expr);
-              if (qIdx !== -1) initQueue.splice(qIdx, 1);
-              if (initQueue.length === 0) {
-                initQueue = false;
-                if (fn.length) {
-                  fn(vals.new.slice(0), vals.old.slice(0));
-                } else {
-                  fn();
-                }
-              }
-              return;
+            if (queue) {
+              prev[i] = oldVal;
+              _.pull(queue, expr);
+              if (queue.length > 0) return;
+              queue = false;
             }
 
             if (fired) return;
@@ -53,16 +94,16 @@ define(function (require) {
               fired = false;
 
               if (fn.length) {
-                fn(vals.new.slice(0), vals.old.slice(0));
+                fn(vals.slice(0), prev.slice(0));
               } else {
                 fn();
               }
 
-              for (var i = 0; i < vals.new.length; i++) {
-                vals.old[i] = vals.new[i];
+              for (var i = 0; i < vals.length; i++) {
+                prev[i] = vals[i];
               }
             });
-          });
+          }, expr.deep);
         });
       };
 
