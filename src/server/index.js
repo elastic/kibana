e3244229e877e4c0d3446027cf339fac8b466c3c
Chris Cowan <chris@chriscowan.us>
2015-01-08 09:18:26 -0700

    Moving bin/kibana.js to server/index.js

---
+++ src/server/index.js
@@ -0,0 +1,69 @@
+/**
+ * Module dependencies.
+ */
+
+var app = require('./app');
+var debug = require('debug')('node-server:server');
+var http = require('http');
+var config = require('./config');
+
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error('Port ' + app.get('port') + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error('Port ' + app.get('port') + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var address = server.address();
+  debug('Listening on ' + address.address + ':' + address.port);
+}
+
+module.exports = {
+  server: server,
+  start: function (cb) {
+    var port = parseInt(process.env.PORT, 10) || config.port || 3000;
+    var host = process.env.HOST || config.host || '127.0.0.1';
+    app.set('port', port);
+    server.listen(port, host, cb);
+  }
+};
+
+if (require.main === module) {
+  module.exports.start();
+}
