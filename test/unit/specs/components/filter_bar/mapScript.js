f8d2c3bbfbe088d51d5925c69a7ed9bf7713c33d
Rashid Khan <rashidkpc@gmail.com>
2015-01-08 07:49:46 -0700

    Add mapScript

---
+++ test/unit/specs/components/filter_bar/mapScript.js
@@ -0,0 +1,50 @@
+/* global sinon */
+define(function (require) {
+  describe('Filter Bar Directive', function () {
+    describe('mapScript()', function () {
+
+      var indexPattern, mapScript, $rootScope, getIndexPatternStub;
+      beforeEach(module('kibana'));
+
+      beforeEach(function () {
+        getIndexPatternStub = sinon.stub();
+        module('kibana/courier', function ($provide) {
+          $provide.service('courier', function () {
+            var courier = { indexPatterns: { get: getIndexPatternStub } };
+            return courier;
+          });
+        });
+      });
+
+      beforeEach(inject(function (Private, _$rootScope_, Promise) {
+        $rootScope = _$rootScope_;
+        mapScript = Private(require('components/filter_bar/lib/mapScript'));
+        indexPattern = Private(require('fixtures/stubbed_logstash_index_pattern'));
+        getIndexPatternStub.returns(Promise.resolve(indexPattern));
+      }));
+
+      it('should return the key and value for matching filters', function (done) {
+        var filter = {
+          meta: { index: 'logstash-*', field: 'scritped number' },
+          script: { script: 'doc["scritped number"].value * 5', params: { value: 35}}
+        };
+        mapScript(filter).then(function (result) {
+          expect(result).to.have.property('key', 'scritped number');
+          expect(result).to.have.property('value', 35);
+          done();
+        });
+        $rootScope.$apply();
+      });
+
+      it('should return undefined for none matching', function (done) {
+        var filter = { meta: { index: 'logstash-*' }, query: { query_string: { query: 'foo:bar' } } };
+        mapScript(filter).catch(function (result) {
+          expect(result).to.be(filter);
+          done();
+        });
+        $rootScope.$apply();
+      });
+
+    });
+  });
+});
