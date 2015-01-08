3bfd79e384148c7f3c24d53513e7c64860457a33
Joe Fleming <joe.fleming@gmail.com>
2015-01-07 18:36:49 -0700

    add proper deduping to the save process

--- src/kibana/components/filter_bar/lib/saveFilterState.js
+++ src/kibana/components/filter_bar/lib/saveFilterState.js
@@ -1,5 +1,6 @@
 define(function (require) {
   var _ = require('lodash');
+  var angular = require('angular');
 
   return function (state, globalState) {
     return saveState;
@@ -9,12 +10,17 @@ define(function (require) {
      * @returns {void}
      */
     function saveState(filters) {
+      // always dedupe filters
+      filters = dedupe(filters);
+
       saveGlobalState(filters);
 
       // only save state if state exists
       if (state) {
-        state.filters = _.union(filters);
+        state.filters = filters;
       }
+
+      return filters;
     }
 
     /**
@@ -22,8 +28,32 @@ define(function (require) {
      * @returns {void}
      */
     function saveGlobalState(filters) {
-      globalState.filters = _.union(_.filter(filters, { meta: { pinned: true } }));
+      globalState.filters = _.filter(filters, { meta: { pinned: true } });
       globalState.save();
+    }
+
+    /**
+     * Remove duplicate filters from an array of filters
+     * @returns {array} deduped filters
+     */
+    function dedupe(filters) {
+      var results = [];
+      var seenFilters = [];
+      var flatFilters = _.map(filters, function (filter) {
+        // use angular.extend to remove any hash values
+        return _.flattenWith('.', angular.extend({}, filter));
+      });
+
+      _.each(flatFilters, function (filter, i) {
+        for (var n = 0; n < seenFilters.length; n++) {
+          if (_.isEqual(seenFilters[n], filter)) return;
+        }
+
+        seenFilters.push(filter);
+        results.push(filters[i]);
+      });
+
+      return results;
     }
   };
 });
