a7cb09a028dedaa581eca0e4dab2b7815185615e
Chris Cowan <chris@chriscowan.us>
2015-01-11 12:23:04 -0700

    Adding bat and shell scripts for starting the node server

---
+++ src/server/bin/kibana.bat
@@ -0,0 +1,20 @@
+@echo off
+
+SETLOCAL
+
+set SCRIPT_DIR=%~dp0
+for %%I in ("%SCRIPT_DIR%..") do set DIR=%%~dpfI
+
+set NODE=%FULL_PATH%\node\node.exe
+set SERVER=%DIR%/src/bin/kibana.js
+set NODE_ENV="production"
+
+TITLE Kibana Server @@version
+
+%NODE% %SERVER%
+
+:finally
+
+ENDLOCAL
+
+
