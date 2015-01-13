16b7cff0d5bf4d30fbb44f84d28ee4cca7b4aae8
Joe Fleming <joe.fleming@gmail.com>
2015-01-13 15:33:42 -0700

    clean up dedupe filters a little

--- src/kibana/components/filter_bar/lib/dedupFilters.js
+++ src/kibana/components/filter_bar/lib/dedupFilters.js
@@ -1,12 +1,26 @@
 define(function (require) {
   var _ = require('lodash');
-  var excludedAttributes = ['meta', '$$hashKey'];
+  var angular = require('angular');
+  var excludedAttributes = ['meta'];
+
   return function (existing, filters) {
+    existing = cleanFilters(existing);
+
     filters = _.filter(filters, function (item) {
       return !_.find(existing, function (existingFilter) {
-        return _.isEqual(_.omit(existingFilter, excludedAttributes), _.omit(item, excludedAttributes));
+        return _.isEqual(existingFilter, cleanFilter(item));
       });
     });
     return filters;
   };
+
+  function cleanFilter(filter) {
+    // use angular.extend to string $$hashKey and other angular props
+    filter = angular.extend({}, filter);
+    return _.omit(filter, excludedAttributes);
+  }
+
+  function cleanFilters(filters) {
+    return _.map(filters, cleanFilter);
+  }
 });
