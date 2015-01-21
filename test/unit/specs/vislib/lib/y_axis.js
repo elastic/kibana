1340710884666c4ac303b01478b211b61b9f3778
Joe Fleming <joe.fleming@gmail.com>
2015-01-20 18:27:55 -0700

    major refactor - createData before each suite, instead of just once

--- test/unit/specs/vislib/lib/y_axis.js
+++ test/unit/specs/vislib/lib/y_axis.js
@@ -3,6 +3,12 @@ define(function (require) {
   var d3 = require('d3');
   var $ = require('jquery');
 
+  var YAxis;
+  var Data;
+  var el;
+  var yAxis;
+  var yAxisDiv;
+
   var timeSeries = [
     1408734060000,
     1408734090000,
@@ -16,6 +22,11 @@ define(function (require) {
     1408734330000
   ];
 
+  var defaultGraphData = [
+    [ 8, 23, 30, 28, 36, 30, 26, 22, 29, 24 ],
+    [ 2, 13, 20, 18, 26, 20, 16, 12, 19, 14 ]
+  ];
+
   function makeSeriesData(data) {
     return timeSeries.map(function (timestamp, i) {
       return {
@@ -25,14 +36,7 @@ define(function (require) {
     });
   }
 
-  describe('Vislib yAxis Class Test Suite', function () {
-    var YAxis;
-    var Data;
-    var yAxis;
-    var el;
-    var yAxisDiv;
-    var dataObj;
-
+  function createData(seriesData) {
     var data = {
       hits: 621,
       label: 'test',
@@ -42,47 +46,51 @@ define(function (require) {
         max: 1408734982458,
         min: 1408734082458
       },
-      series: [
-        { values: makeSeriesData([ 8, 23, 30, 28, 36, 30, 26, 22, 29, 24 ]) },
-        { values: makeSeriesData([ 2, 13, 20, 18, 26, 20, 16, 12, 19, 14 ]) },
-        { values: makeSeriesData([ 22, 8, -30, -4, 0, 0, 3, -22, 14, 24 ]) }
-      ],
+      series: seriesData.map(function (series) {
+        return { values: makeSeriesData(series) };
+      }),
       xAxisLabel: 'Date Histogram',
       yAxisLabel: 'Count'
     };
 
+    var node = $('<div>').css({
+      height: 40,
+      width: 40
+    })
+    .appendTo('body')
+    .addClass('y-axis-wrapper')
+    .get(0);
+
+    el = d3.select(node).datum(data);
+
+    yAxisDiv = el.append('div')
+    .attr('class', 'y-axis-div');
+
+    var dataObj = new Data(data, {
+      defaultYMin: true
+    });
+
+    yAxis = new YAxis({
+      el: node,
+      yMin: dataObj.getYMinValue(),
+      yMax: dataObj.getYMaxValue(),
+      _attr: {
+        margin: { top: 0, right: 0, bottom: 0, left: 0 }
+      }
+    });
+  }
+
+  describe('Vislib yAxis Class Test Suite', function () {
+    var d3Provider;
+
     beforeEach(module('kibana'));
-    beforeEach(inject(function (d3, Private) {
+
+    beforeEach(inject(function (Private, _d3_) {
+      d3Provider = _d3_;
       Data = Private(require('components/vislib/lib/data'));
       YAxis = Private(require('components/vislib/lib/y_axis'));
 
       expect($('.y-axis-wrapper')).to.have.length(0);
-
-      var $node = $('<div>').css({
-        height: 40,
-        width: 40
-      })
-      .appendTo('body')
-      .addClass('y-axis-wrapper');
-
-      var node = $node.get(0);
-
-      el = d3.select(node).datum(data);
-
-      yAxisDiv = el.append('div')
-        .attr('class', 'y-axis-div');
-
-      dataObj = new Data(data, {
-        defaultYMin: true
-      });
-      yAxis = new YAxis({
-        el: node,
-        yMin: dataObj.getYMinValue(),
-        yMax: dataObj.getYMaxValue(),
-        _attr: {
-          margin: { top: 0, right: 0, bottom: 0, left: 0 }
-        }
-      });
     }));
 
     afterEach(function () {
@@ -92,6 +100,7 @@ define(function (require) {
 
     describe('render Method', function () {
       beforeEach(function () {
+        createData(defaultGraphData);
         expect(d3.select(yAxis.el).selectAll('.y-axis-div')).to.have.length(1);
         yAxis.render();
       });
@@ -139,6 +148,7 @@ define(function (require) {
       var val;
 
       beforeEach(function () {
+        createData(defaultGraphData);
         val = yAxis.formatAxisLabel(num);
       });
 
@@ -150,6 +160,7 @@ define(function (require) {
     describe('getYAxis method', function () {
       var mode, yMax, yScale;
       beforeEach(function () {
+        createData(defaultGraphData);
         mode = yAxis._attr.mode;
         yMax = yAxis.yMax;
         yScale = yAxis.getYScale;
@@ -182,12 +193,20 @@ define(function (require) {
     });
 
     describe('draw Method', function () {
+      beforeEach(function () {
+        createData(defaultGraphData);
+      });
+
       it('should be a function', function () {
         expect(_.isFunction(yAxis.draw())).to.be(true);
       });
     });
 
     describe('tickScale Method', function () {
+      beforeEach(function () {
+        createData(defaultGraphData);
+      });
+
       it('should return the correct number of ticks', function () {
         expect(yAxis.tickScale(1000)).to.be(11);
         expect(yAxis.tickScale(40)).to.be(3);
