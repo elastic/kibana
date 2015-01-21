4e74d435b89d5aca092f8b2cafd19a8635bfd44d
Joe Fleming <joe.fleming@gmail.com>
2015-01-21 14:26:38 -0700

    update the readme, remove java requirement

--- src/server/app.js
+++ src/server/app.js
@@ -30,6 +30,7 @@ if (app.get('env') === 'development') {
 // TODO: WE might want to move the middleware to each of the individual routes
 // so we don't have weird conflicts in the future.
 app.use('/elasticsearch', proxy);
+app.use('/enforcer', require('./lib/enforce'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -39,6 +40,7 @@ app.use(express.static(config.public_folder));
 if (config.external_plugins_folder) app.use('/plugins', express.static(config.external_plugins_folder));
 
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
