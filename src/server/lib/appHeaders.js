39034a16d059624559ffe7ba5f93573dc92ee87b
Joe Fleming <joe.fleming@gmail.com>
2015-01-21 12:06:49 -0700

    remove debugging code

--- src/server/lib/appHeaders.js
+++ src/server/lib/appHeaders.js
@@ -4,7 +4,6 @@ var pkg = require(pkgPath);
 
 module.exports = function () {
   return function (req, res, next) {
-    console.log('middleware');
     res.header('X-App-Name', 'kibana');
     res.header('X-App-Version', pkg.version);
     next();
