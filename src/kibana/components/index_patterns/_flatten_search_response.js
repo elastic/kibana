1510e2a9d38239090a32c85619094039d3d25815
Spencer Alger <spencer@spenceralger.com>
2015-01-21 14:16:05 -0700

    [indexPatterns/flattenSearchResponse] pass indexPattern as an arg, don't force .bind()

--- src/kibana/components/index_patterns/_flatten_search_response.js
+++ src/kibana/components/index_patterns/_flatten_search_response.js
@@ -1,16 +1,17 @@
 define(function (require) {
   var _ = require('lodash');
-  return function (nestedObj) {
+  return function (indexPattern, nestedObj) {
     var key; // original key
     var stack = []; // track key stack
     var flatObj = {};
-    var self = this;
+    var fields = indexPattern.fields;
+
     (function flattenObj(obj) {
       _.keys(obj).forEach(function (key) {
         stack.push(key);
         var flattenKey = stack.join('.');
 
-        if ((self.fields.byName[flattenKey] || _.isArray(obj[key]) || !_.isObject(obj[key]))) {
+        if ((fields.byName[flattenKey] || _.isArray(obj[key]) || !_.isObject(obj[key]))) {
           flatObj[flattenKey] = obj[key];
         } else if (_.isObject(obj[key])) {
           flattenObj(obj[key]);
@@ -19,6 +20,7 @@ define(function (require) {
         stack.pop();
       });
     }(nestedObj));
+
     return flatObj;
   };
 });
