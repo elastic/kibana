fa6dbca32e0570e805fab62dda0c7f8230309a41
Spencer Alger <spencer@spenceralger.com>
2015-01-21 22:10:20 -0700

    [indexPattern/flattenSearchResponse] update tests

--- test/unit/specs/index_patterns/flatten_search_response.js
+++ test/unit/specs/index_patterns/flatten_search_response.js
@@ -12,7 +12,10 @@ define(function (require) {
           'geo.dest': { type: 'string' },
           'geo.src': { type: 'string' },
           'bytes': { type: 'number' },
-          '@timestamp': { type: 'date' }
+          '@timestamp': { type: 'date' },
+          'team': { type: 'nested' },
+          'team.name': { type: 'string' },
+          'team.role': { type: 'string' }
         }
       }
     };
@@ -27,29 +30,41 @@ define(function (require) {
       bytes: 10039103,
       '@timestamp': (new Date()).toString(),
       tags: [{ text: 'foo' }, { text: 'bar' }],
-      noMapping: true
+      noMapping: true,
+      team: [
+        { name: 'foo', role: 'leader' },
+        { name: 'bar', role: 'follower' },
+        { name: 'baz', role: 'party boy' },
+      ]
     };
 
+    var flat = flattenSearchResponse(indexPattern, fixture);
+
     it('should flatten keys as far down as the mapping goes', function () {
-      var obj = flattenSearchResponse(indexPattern, fixture);
-      expect(obj).to.have.property('geo.coordinates', fixture.geo.coordinates);
-      expect(obj).to.not.have.property('geo.coordinates.lat');
-      expect(obj).to.not.have.property('geo.coordinates.lon');
-      expect(obj).to.have.property('geo.dest', 'US');
-      expect(obj).to.have.property('geo.src', 'IN');
-      expect(obj).to.have.property('@timestamp', fixture['@timestamp']);
-      expect(obj).to.have.property('message', 'Hello World');
-      expect(obj).to.have.property('bytes', 10039103);
+      expect(flat).to.have.property('geo.coordinates', fixture.geo.coordinates);
+      expect(flat).to.not.have.property('geo.coordinates.lat');
+      expect(flat).to.not.have.property('geo.coordinates.lon');
+      expect(flat).to.have.property('geo.dest', 'US');
+      expect(flat).to.have.property('geo.src', 'IN');
+      expect(flat).to.have.property('@timestamp', fixture['@timestamp']);
+      expect(flat).to.have.property('message', 'Hello World');
+      expect(flat).to.have.property('bytes', 10039103);
     });
 
     it('should flatten keys not in the mapping', function () {
-      var obj = flattenSearchResponse(indexPattern, fixture);
-      expect(obj).to.have.property('noMapping', true);
+      expect(flat).to.have.property('noMapping', true);
     });
 
     it('should preserve objects in arrays', function () {
-      var obj = flattenSearchResponse(indexPattern, fixture);
-      expect(obj).to.have.property('tags', fixture['tags']);
+      expect(flat).to.have.property('tags', fixture['tags']);
+    });
+
+    it('should completely flatten nested docs', function () {
+      expect(flat).to.have.property('team', fixture.team);
+      expect(flat).to.not.have.property('team.name');
+      expect(flat).to.not.have.property('team.role');
+      expect(flat).to.not.have.property('team[0]');
+      expect(flat).to.not.have.property('team.0');
     });
   });
 });
