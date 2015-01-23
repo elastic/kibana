253eeabe316f2628699ecc23f0677b7634f64b26
Chris Cowan <chris@chriscowan.us>
2015-01-23 11:48:28 -0700

    Refactoring the else statements out of isUpgradeable

--- src/server/lib/isUpgradeable.js
+++ src/server/lib/isUpgradeable.js
@@ -6,23 +6,21 @@ module.exports = function (doc) {
   if (/beta|snapshot/i.test(doc._id)) return false;
   if (doc._id === config.package.version) return false;
 
-  var packageVersion, packageRcRelease, version, rcRelease;
-  packageRcRelease = rcRelease = Infinity;
+  var packageRcRelease = Infinity;
+  var rcRelease = Infinity;
+  var packageVersion = config.package.version;
+  var version = doc._id;
   var matches = doc._id.match(rcVersionRegex);
   var packageMatches = config.package.version.match(rcVersionRegex);
 
   if (matches) {
     version = matches[1];
     rcRelease = parseInt(matches[2], 10);
-  } else {
-    version = doc._id;
   }
 
   if (packageMatches) {
     packageVersion = packageMatches[1];
     packageRcRelease = parseInt(packageMatches[2], 10);
-  } else {
-    packageVersion = config.package.version;
   }
 
   if (semver.gte(version, packageVersion) && rcRelease >= packageRcRelease) return false;
