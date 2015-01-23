d1886d8b953f54659f35347070a73f22da4bde4c
Chris Cowan <chris@chriscowan.us>
2015-01-23 13:01:19 -0700

    Fixing esvm:dev

--- tasks/config/esvm.js
+++ tasks/config/esvm.js
@@ -1,16 +1,22 @@
 module.exports = function (grunt) {
-  var rel = require('path').join.bind(null, __dirname, '../../');
+  var join = require('path').join;
+  var rel = require('path').join.bind(null, grunt.config.get('root'));
+  var directory = join(__dirname, '../../', 'esvm');
 
   return {
     options: {
-      version: '^1.4',
+      directory: directory,
+      version: '1.4.2',
       plugins: [
         'elasticsearch/marvel/latest'
       ],
       config: {
         path: {
-          data: rel('esvm/data_dir'),
-          logs: rel('esvm/logs')
+          home: rel('1.4.2'),
+          config: rel('1.4.2/config'),
+          data: rel('data_dir'),
+          logs: rel('logs'),
+          plugins: rel('1.4.2/plugins')
         },
         network: {
           host: '127.0.0.1'
