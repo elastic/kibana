7040887d544729cfb640df88ba5c2b5c45451104
Jimmy Jones <jimmyjones2@gmx.co.uk>
2015-01-25 13:45:49 +0000

    Add link to leaflet provider demos

--- src/kibana/components/config/defaults.js
+++ src/kibana/components/config/defaults.js
@@ -49,11 +49,13 @@ define(function (require) {
     },
     'visualization:tileMap:url': {
       value: 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg',
-      description: 'URL of mapping provider, see <a href="http://leafletjs.com/reference.html#tilelayer">leafletjs reference</a>'
+      description: 'URL of mapping provider, see <a href="http://leafletjs.com/reference.html#tilelayer">leafletjs reference</a>' +
+        ' or <a href="http://leaflet-extras.github.io/leaflet-providers/preview/">provider demos</a>'
     },
     'visualization:tileMap:options': {
       value: null,
-      description: 'TileLayer options, see <a href="http://leafletjs.com/reference.html#tilelayer">leafletjs reference</a>',
+      description: 'TileLayer options, see <a href="http://leafletjs.com/reference.html#tilelayer">leafletjs reference</a>' +
+        ' or <a href="http://leaflet-extras.github.io/leaflet-providers/preview/">provider demos</a>',
       type: 'json'
     },
     'csv:separator': {
