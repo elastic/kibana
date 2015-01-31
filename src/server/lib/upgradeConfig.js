e91f17ac8b68e131383c562cb7269c96992b3d76
Jimmy Jones <jimmyjones2@gmx.co.uk>
2015-01-31 21:16:26 +0000

    Fix kibana not starting up

--- src/server/lib/upgradeConfig.js
+++ src/server/lib/upgradeConfig.js
@@ -16,7 +16,7 @@ module.exports = function (response) {
   // Look for upgradeable configs. If none of them are upgradeable
   // then resolve with null.
   var body = _.find(response.hits.hits, isUpgradeable);
-  if (body) return Promise.resolve();
+  if (!body) return Promise.resolve();
 
   return client.create({
     index: '.kibana',
