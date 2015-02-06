e93984e89035800ee8596571dd554c08d9251884
lukasolson <olson.lukas@gmail.com>
2015-02-06 10:13:15 -0700

    Remove weekyear and change name to weekOfYear instead of weekOfWeekyear

--- src/kibana/plugins/settings/sections/indices/_date_scripts.js
+++ src/kibana/plugins/settings/sections/indices/_date_scripts.js
@@ -3,23 +3,22 @@ define(function (require) {
 
   return function (indexPattern) {
     var dateScripts = {};
-    var scripts = [
-      'dayOfMonth',
-      'dayOfWeek',
-      'dayOfYear',
-      'hourOfDay',
-      'minuteOfDay',
-      'minuteOfHour',
-      'monthOfYear',
-      'weekOfWeekyear',
-      'weekyear',
-      'year'
-    ];
+    var scripts = {
+      __dayOfMonth: 'dayOfMonth',
+      __dayOfWeek: 'dayOfWeek',
+      __dayOfYear: 'dayOfYear',
+      __hourOfDay: 'hourOfDay',
+      __minuteOfDay: 'minuteOfDay',
+      __minuteOfHour: 'minuteOfHour',
+      __monthOfYear: 'monthOfYear',
+      __weekOfYear: 'weekOfWeekyear',
+      __year: 'year'
+    };
 
     _.each(indexPattern.fields.byType['date'], function (field) {
       if (field.indexed) {
-        scripts.forEach(function (script) {
-          dateScripts[field.name + '.__' + script] = 'doc["' + field.name + '"].date.' + script;
+        _.each(scripts, function (value, key) {
+          dateScripts[field.name + '.' + key] = 'doc["' + field.name + '"].date.' + value;
         });
       }
     });
