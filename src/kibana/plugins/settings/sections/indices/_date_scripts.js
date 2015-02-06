72d80872255c737f8b09deae7ca1e9ffb534659e
lukasolson <olson.lukas@gmail.com>
2015-02-06 09:13:30 -0700

    Update sample date scripts to use more efficient scripts

--- src/kibana/plugins/settings/sections/indices/_date_scripts.js
+++ src/kibana/plugins/settings/sections/indices/_date_scripts.js
@@ -2,28 +2,25 @@ define(function (require) {
   var _ = require('lodash');
 
   return function (indexPattern) {
-
-
-    function getScript(field, format) {
-      return 'Integer.parseInt(new Date(doc["' + field + '"].value).format("' + format + '"))';
-    }
-
     var dateScripts = {};
-    var scripts = {
-      __minuteOfHour: 'm',
-      __hourOfDay:    'H',
-      __dayOfWeek:    'u',
-      __dayOfMonth:   'd',
-      __dayOfYear:    'D',
-      __weekOfMonth:  'W',
-      __weekOfYear:   'w',
-      __monthOfYear:  'M'
-    };
+    var scripts = [
+      'dayOfMonth',
+      'dayOfWeek',
+      'dayOfYear',
+      'hourOfDay',
+      'minuteOfDay',
+      'minuteOfHour',
+      'minuteOfHour',
+      'monthOfYear',
+      'weekOfWeekyear',
+      'weekyear',
+      'year'
+    ];
 
     _.each(indexPattern.fields.byType['date'], function (field) {
       if (field.indexed) {
-        _.each(scripts, function (format, scriptKey) {
-          dateScripts[field.name + '.' + scriptKey] = getScript(field.name, format);
+        scripts.forEach(function (script) {
+          dateScripts[field.name + '.__' + script] = 'doc["' + field.name + '"].date.' + script;
         });
       }
     });
