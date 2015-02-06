c59a5233af37a83905cd22cc99d6941db92728e5
Spencer Alger <spencer@spenceralger.com>
2015-02-06 10:32:57 -0700

    [vislib/handler/area] use the unstacked values to detect pos/neg warning

--- src/kibana/components/vislib/lib/handler/types/point_series.js
+++ src/kibana/components/vislib/lib/handler/types/point_series.js
@@ -67,7 +67,7 @@ define(function (require) {
             msg: 'Positive and negative values are not accurately represented by stacked ' +
                  'area charts. The line chart is better suited for this type of data.',
             test: function (vis, data) {
-              return vis._attr.mode === 'stacked' && data.gitYMax() > 0 && data.gitYMin() < 0;
+              return vis._attr.mode === 'stacked' && data.gitYMax(data._getY) > 0 && data.gitYMin(data._getY) < 0;
             }
           }
         ]
