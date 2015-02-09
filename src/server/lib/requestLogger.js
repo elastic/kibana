5982268679c52bfc46cebd74d750a146f27759a0
Chris Cowan <chris@chriscowan.us>
2015-02-09 13:31:29 -0700

    oops :D

--- src/server/lib/requestLogger.js
+++ src/server/lib/requestLogger.js
@@ -7,7 +7,7 @@ module.exports = function (options) {
     var _req = _.pick(req, ['url', 'headers', 'method']);
     _req.connection = _.pick(req.connection, ['remoteAddress', 'remotePort']);
     res.end = function (chunk, encoding) {
-      res.contentLength = parseInt(res.getHeader('content-length') || 0);
+      res.contentLength = parseInt(res.getHeader('content-length') || 0, 10);
       res.responseTime = (new Date()).getTime() - startTime.getTime();
       end.call(res, chunk, encoding);
       logger.info({ req: _req, res: res }, '%s %s %d - %dms', req.method, req.url, res.statusCode, res.responseTime);
