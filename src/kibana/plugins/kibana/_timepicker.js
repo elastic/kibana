2cb114993fd3543b5b226dd1c40d64fec515ba20
Chris Cowan <chris@chriscowan.us>
2015-02-13 14:48:26 -0700

    Closes #1956 - Adding a way to navigate forward/backwards through time

--- src/kibana/plugins/kibana/_timepicker.js
+++ src/kibana/plugins/kibana/_timepicker.js
@@ -29,5 +29,18 @@ define(function (require) {
       }
     };
 
+    function slideTime(op) {
+      op = op || 'subtract';
+      var bounds = timefilter.getBounds();
+      var interval = bounds.max.diff(bounds.min);
+      var from = bounds.min.clone()[op](interval, 'ms');
+      var to = bounds.max.clone()[op](interval, 'ms');
+      timefilter.time.from = from;
+      timefilter.time.to = to;
+      timefilter.time.mode = 'absolute';
+    }
+
+    $scope.previousTimeperiod = _.partial(slideTime, 'subtract');
+    $scope.nextTimeperiod = _.partial(slideTime, 'add');
   };
 });
