5d7b359d0cb4882439fda4dd6a8fe5b5b4325b68
Rashid Khan <rashidkpc@gmail.com>
2015-02-27 13:30:16 -0700

    Use isolate scope and allow for filters to collections

--- src/kibana/directives/auto_select_if_only_one.js
+++ src/kibana/directives/auto_select_if_only_one.js
@@ -1,12 +1,16 @@
 define(function (require) {
   var module = require('modules').get('kibana');
 
-  module.directive('autoSelectIfOnlyOne', function () {
+  module.directive('autoSelectIfOnlyOne', function ($parse) {
     return {
       restrict: 'A',
       require: 'ngModel',
+      scope: {
+        autoSelectIfOnlyOne: '&autoSelectIfOnlyOne'
+      },
       link: function (scope, element, attributes, ngModelCtrl) {
-        scope.$watch(attributes.autoSelectIfOnlyOne, function (options) {
+        scope.$watch('autoSelectIfOnlyOne', function (result) {
+          var options = result();
           if (options && options.length === 1) {
             ngModelCtrl.$setViewValue(options[0]);
             ngModelCtrl.$render();
