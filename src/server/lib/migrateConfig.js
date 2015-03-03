341453ab1eb5aae0d6f53c4e616eff2361f609ad
Joe Fleming <joe.fleming@gmail.com>
2015-03-03 14:47:08 -0700

    no need to wrap this in a function

--- src/server/lib/migrateConfig.js
+++ src/server/lib/migrateConfig.js
@@ -2,30 +2,28 @@ var config = require('../config');
 var upgrade = require('./upgradeConfig');
 
 module.exports = function (client) {
-  return function () {
-    var options =  {
-      index: config.kibana.kibana_index,
-      type: 'config',
-      body: {
-        size: 1000,
-        sort: [ { buildNum: { order: 'desc' } } ],
-        query: {
-          filtered: {
-            filter: {
-              bool: {
-                must_not: [ { query: { match: { _id: '@@version' } } } ]
-              }
+  var options =  {
+    index: config.kibana.kibana_index,
+    type: 'config',
+    body: {
+      size: 1000,
+      sort: [ { buildNum: { order: 'desc' } } ],
+      query: {
+        filtered: {
+          filter: {
+            bool: {
+              must_not: [ { query: { match: { _id: '@@version' } } } ]
             }
           }
         }
       }
-    };
-
-    return client.search(options)
-    .then(upgrade(client))
-    .catch(function (err) {
-      if (!/SearchParseException.+mapping.+\[buildNum\]|^IndexMissingException/.test(err.message)) throw err;
-    });
+    }
   };
+
+  return client.search(options)
+  .then(upgrade(client))
+  .catch(function (err) {
+    if (!/SearchParseException.+mapping.+\[buildNum\]|^IndexMissingException/.test(err.message)) throw err;
+  });
 };
 
