fb6fc07a925836f11ec88dfc58878d85df4c43b2
Khalah Jones-Golden <khasan222@gmail.com>
2015-03-03 14:58:48 -0400

    [Forms/validation] Added a directive that simply doesn't show errors until the user has actually made a mistake

---
+++ src/kibana/directives/in_time_errors.js
@@ -0,0 +1,32 @@
+define(function (require) {
+  var module = require('modules').get('kibana');
+  var $ = require('jquery');
+
+  module.directive('inTimeErrors', function () {
+    return {
+      // can't require since there is no controller
+      // require: 'ng-submit',
+      restrict: 'C',
+      link: function ($scope, $elem, attrs) {
+        // this directive requires a Form element, with a name to reference the errors
+        if(!$elem.is('form') || !attrs.name) {
+          // TODO maybe throw an error here?
+          return false;
+        }
+
+        var formObj = $scope[attrs.name];
+        // expose a variable for other sections to not show errors
+        formObj.hideErrors = true;
+
+        // set the errors to hide off of the back
+        $elem.addClass('hide-errors');
+
+        // wait for a submit and then show the errors
+        $elem.one('submit.inTimeErrors', function() {
+          $elem.removeClass('hide-errors');
+          formObj.hideErrors = false;
+        });
+      }
+    };
+  });
+});
