2263d2236eb21c7e931a0d44f081f37fa45bddd6
Rashid Khan <rashidkpc@gmail.com>
2015-03-04 12:04:05 -0700

    Fix _timestamp and arrays in advanced settings, add test for value type

--- src/kibana/plugins/settings/sections/advanced/index.js
+++ src/kibana/plugins/settings/sections/advanced/index.js
@@ -1,6 +1,8 @@
 define(function (require) {
   var _ = require('lodash');
   var configDefaults = require('components/config/defaults');
+  var getValType = require('plugins/settings/sections/advanced/lib/get_val_type');
+
 
   require('plugins/settings/sections/advanced/advanced_row');
 
@@ -38,7 +40,7 @@ define(function (require) {
             var conf = {
               name: name,
               defVal: def.value,
-              type: (def.type || _.isArray(val) || typeof val),
+              type: getValType(def, val),
               description: def.description,
               value: val,
             };
