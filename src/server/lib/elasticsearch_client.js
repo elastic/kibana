ccccb696e9a4379dab5c2eec760fa3748fa2366b
Flavia Saplacan <flavia.saplacan@gmail.com>
2015-03-16 16:31:01 +0100

    Client certificate and key added on requests to elasticsearch. Issue # 3339

--- src/server/lib/elasticsearch_client.js
+++ src/server/lib/elasticsearch_client.js
@@ -2,13 +2,14 @@ var config = require('../config');
 var elasticsearch = require('elasticsearch');
 var logger = require('./logger');
 var _ = require('lodash');
+var fs = require('fs');
 var util = require('util');
 var url = require('url');
 var uri = url.parse(config.elasticsearch);
 if (config.kibana.kibana_elasticsearch_username && config.kibana.kibana_elasticsearch_password) {
   uri.auth = util.format('%s:%s', config.kibana.kibana_elasticsearch_username, config.kibana.kibana_elasticsearch_password);
 }
-module.exports = new elasticsearch.Client({
+options = {
   host: url.format(uri),
   log: function (config) {
     this.error = function (err) {
@@ -20,5 +21,13 @@ module.exports = new elasticsearch.Client({
     this.trace = _.noop;
     this.close = _.noop;
   }
-});
+}
+if (config.kibana.kibana_elasticsearch_client_crt && config.kibana.kibana_elasticsearch_client_key) {
+  options.ssl = {
+    cert: fs.readFileSync(config.kibana.kibana_elasticsearch_client_crt , 'utf8'),
+    key: fs.readFileSync(config.kibana.kibana_elasticsearch_client_key , 'utf8')
+  }
+}
+
+module.exports = new elasticsearch.Client(options);
 
