e3f4188e8d71a0438e2847d371ff1b49d32f6a89
Spencer Alger <email@spalger.com>
2015-03-24 14:56:49 -0700

    [visualize] stop trying to hide errors

--- src/kibana/plugins/visualize/editor/sidebar.js
+++ src/kibana/plugins/visualize/editor/sidebar.js
@@ -13,11 +13,6 @@ define(function (require) {
       scope: true,
       link: function ($scope) {
         $scope.$bind('vis', 'editableVis');
-
-        $scope.hideErrors = true;
-        $scope.dontApply = function () {
-          $scope.hideErrors = false;
-        };
       }
     };
   });
