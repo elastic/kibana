71d94be27388b9c931e7629c4317ef938a31c336
Khalah Jones-Golden <khasan222@gmail.com>
2015-03-24 08:55:05 -0700

    [Forms/InTimeErrors] Added the form controller to the inTimeErrors directive, cleaned up and made the code easier to read

--- src/kibana/directives/in_time_errors.js
+++ src/kibana/directives/in_time_errors.js
@@ -5,21 +5,15 @@ define(function (require) {
   module.directive('inTimeErrors', function () {
     return {
       restrict: 'C',
-      link: function ($scope, $elem, attrs) {
-        // this directive requires a Form element, with a name to reference the errors
-        if (!$elem.is('form') || !attrs.name) {
-          // TODO maybe throw an error here?
-          return false;
-        }
-
-        var formObj = $scope[attrs.name];
+      require: 'form',
+      link: function ($scope, $elem, attrs, formController) {
         // expose a variable for other sections to not show errors
-        formObj.hideErrors = true;
+        formController.hideErrors = true;
 
         // set the errors to hide off of the back
         $elem.addClass('hide-errors');
 
-        var handleAttemptedSub = function (evt) {
+        var handleAttemptedSubmit = function (evt) {
           var $el = $(evt.target);
           var isInput = $el.is(':input');
           var isSubmit = $el.is('button[type="submit"]');
@@ -27,21 +21,21 @@ define(function (require) {
           var clickedMouse = evt.type === 'click';
 
           // if the evt is a click on the submit
-          // or if the evt is a enter key on an input not a textarea
+          // or if the evt is a enter key on an input ?not a textarea?
           // unbind this event and show the errors on the form
           if ((isInput && pressedEnterKey) || (isSubmit && clickedMouse)) {
             // unbind events
             $elem.off('.inTimeErrors');
             // switch off the flag
-            formObj.hideErrors = false;
+            formController.hideErrors = false;
             // remove the class from the form
             $elem.removeClass('hide-errors');
           }
         };
         // Since the submit gets default prevented if there are errors,
         // listen for lowerlevel events, and determine if it was an attempted submit
-        $elem.on('keydown.inTimeErrors', handleAttemptedSub);
-        $elem.on('click.inTimeErrors', handleAttemptedSub);
+        $elem.on('keydown.inTimeErrors', handleAttemptedSubmit);
+        $elem.on('click.inTimeErrors', handleAttemptedSubmit);
       }
     };
   });
