1ad56c7864d35c8107039f764d7d0f3d42709087
Khalah Jones-Golden <khasan222@gmail.com>
2015-03-23 18:03:46 -0700

    [Forms/InTimeErrors] Improved the InTimeErrors Directive to try to guess when the user is attempting to submit a form, and show the errors at that point

--- src/kibana/directives/in_time_errors.js
+++ src/kibana/directives/in_time_errors.js
@@ -19,11 +19,29 @@ define(function (require) {
         // set the errors to hide off of the back
         $elem.addClass('hide-errors');
 
-        // wait for a submit and then show the errors
-        $elem.one('submit.inTimeErrors', function () {
-          $elem.removeClass('hide-errors');
-          formObj.hideErrors = false;
-        });
+        var handleAttemptedSub = function (evt) {
+          var $el = $(evt.target);
+          var isInput = $el.is(':input');
+          var isSubmit = $el.is('button[type="submit"]');
+          var pressedEnterKey = (evt.type === 'keydown' && evt.which === 13);
+          var clickedMouse = evt.type === 'click';
+
+          // if the evt is a click on the submit
+          // or if the evt is a enter key on an input not a textarea
+          // unbind this event and show the errors on the form
+          if ((isInput && pressedEnterKey) || (isSubmit && clickedMouse)) {
+            // unbind events
+            $elem.off('.inTimeErrors');
+            // switch off the flag
+            formObj.hideErrors = false;
+            // remove the class from the form
+            $elem.removeClass('hide-errors');
+          }
+        };
+        // Since the submit gets default prevented if there are errors,
+        // listen for lowerlevel events, and determine if it was an attempted submit
+        $elem.on('keydown.inTimeErrors', handleAttemptedSub);
+        $elem.on('click.inTimeErrors', handleAttemptedSub);
       }
     };
   });
