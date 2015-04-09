c108cdf58bd41ee31437cb04119709cb71ab17da
Juan Thomassie <juan.thomassie@elasticsearch.com>
2015-04-09 16:39:10 -0500

    fixed require config for heat

--- src/kibana/require.config.js
+++ src/kibana/require.config.js
@@ -25,6 +25,7 @@ require.config({
     faker: 'bower_components/Faker/faker',
     file_saver: 'bower_components/FileSaver/FileSaver',
     gridster: 'bower_components/gridster/dist/jquery.gridster',
+    heat: 'bower_components/Leaflet.heat/dist/leaflet-heat',
     inflection: 'bower_components/inflection/lib/inflection',
     jquery: 'bower_components/jquery/dist/jquery',
     leaflet: 'bower_components/leaflet/dist/leaflet',
@@ -50,6 +51,9 @@ require.config({
     'ace-json': ['ace'],
     'angular-ui-ace': ['angular', 'ace', 'ace-json'],
     'ng-clip': ['angular', 'zeroclipboard'],
+    heat: {
+      deps: ['leaflet']
+    },
     inflection: {
       exports: 'inflection'
     },
