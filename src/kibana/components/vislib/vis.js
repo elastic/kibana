f959f15602cacf989d3eef7c4128f2b4519e30cf
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-04-09 16:01:33 -0400

    adding remove statements before anything is rendered to ensure that previous charts are removed, fixing syntax error that caused tests to fail

--- src/kibana/components/vislib/vis.js
+++ src/kibana/components/vislib/vis.js
@@ -100,7 +100,7 @@ define(function (require) {
      * @method destroy
      */
     Vis.prototype.destroy = function () {
-      var selection = d3.selection(this.el);
+      var selection = d3.select('.vis-wrapper');
 
       this.resizeChecker.off('resize', this.resize);
       this.resizeChecker.destroy();
