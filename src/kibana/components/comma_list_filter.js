d12663bc74076b54ccf0279842bd3da9500d11d1
Spencer Alger <email@spalger.com>
2015-04-13 11:55:52 -0700

    [filter/commaList] document the filter's arguments

--- src/kibana/components/comma_list_filter.js
+++ src/kibana/components/comma_list_filter.js
@@ -4,6 +4,14 @@ define(function (require) {
   require('modules')
   .get('kibana')
   .filter('commaList', function () {
+    /**
+     * Angular filter that accepts either an array or a comma-seperated string
+     * and outputs either an array, or a comma-seperated string for presentation.
+     *
+     * @param {String|Array} input - The comma-seperated list or array
+     * @param {Boolean} inclusive - Should the list be joined with an "and"?
+     * @return {String}
+     */
     return function (input, inclusive) {
       var list = _.commaSeperatedList(input);
       if (list.length < 2) {
