aabe64251558b332538427108a01697fe6dfdbc9
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-04-17 12:05:58 -0400

    removing removal or zero values function from pie chart

--- src/kibana/components/vislib/visualizations/pie_chart.js
+++ src/kibana/components/vislib/visualizations/pie_chart.js
@@ -38,19 +38,6 @@ define(function (require) {
     PieChart.prototype._validatePieData = function () {
       var data = this.chartData;
 
-      data.slices = (function withoutZeroSlices(slices) {
-        if (!slices.children) return slices;
-
-        slices = _.clone(slices);
-        slices.children = slices.children.reduce(function (children, child) {
-          if (child.size !== 0) {
-            children.push(withoutZeroSlices(child));
-          }
-          return children;
-        }, []);
-        return slices;
-      }(data.slices));
-
       if (data.slices.children.length === 0) {
         throw new errors.PieContainsAllZeros();
       }
