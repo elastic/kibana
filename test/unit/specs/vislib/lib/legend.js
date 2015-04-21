416cb6a89ee49dcc0337296e762629492e616660
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-04-21 16:15:49 -0400

    adding tests for the legend class

--- test/unit/specs/vislib/lib/legend.js
+++ test/unit/specs/vislib/lib/legend.js
@@ -35,6 +35,7 @@ define(function (require) {
         type: chartTypes[i],
         addLegend: true
       };
+      var Legend;
       var vis;
 
       beforeEach(function () {
@@ -44,6 +45,7 @@ define(function (require) {
       beforeEach(function () {
         inject(function (Private) {
           vis = Private(require('vislib_fixtures/_vis_fixture'))(visLibParams);
+          Legend = Private(require('components/vislib/lib/legend'));
           require('css!components/vislib/styles/main');
 
           vis.render(data);
@@ -53,6 +55,90 @@ define(function (require) {
       afterEach(function () {
         $(vis.el).remove();
         vis = null;
+      });
+
+      describe('_transformPieData method', function () {
+        var pieData = {
+          slices: {
+            children: [
+              { name: 'm', size: 20 },
+              {
+                name: 'n',
+                size: 30,
+                children: [
+                  { name: 's', size: 10 },
+                  { name: 't', size: 20 },
+                  { name: 'u', size: 4 }
+                ]
+              },
+              { name: 'b', size: 40 }
+            ]
+          }
+        };
+
+        it('should flatten the nested objects', function () {
+          var items = Legend.prototype._transformPieData([pieData]);
+          expect(items.length).to.be(6);
+        });
+      });
+
+      describe('_filterZeroInjectedValues method', function () {
+        // Zero injected values do not contain aggConfigResults
+        var seriesData = [
+          { aggConfigResult: {} },
+          { aggConfigResult: {} },
+          {}
+        ];
+
+        it('should remove zero injected values', function () {
+          var items = Legend.prototype._filterZeroInjectedValues(seriesData);
+          expect(items.length).to.be(2);
+        });
+      });
+
+      describe('_transformSeriesData method', function () {
+        var seriesData = [
+          {
+            series: [
+              {
+                label: 'html',
+                values: [{y: 2}, {y: 3}, {y: 4}]
+              },
+              {
+                label: 'css',
+                values: [{y: 5}, {y: 6}, {y: 7}]
+              },
+              {
+                label: 'png',
+                values: [{y: 8}, {y: 9}, {y: 10}]
+              }
+            ]
+          },
+          {
+            series: [
+              {
+                label: 'html',
+                values: [{y: 2}, {y: 3}, {y: 4}]
+              },
+              {
+                label: 'css',
+                values: [{y: 5}, {y: 6}, {y: 7}]
+              },
+              {
+                label: 'png',
+                values: [{y: 8}, {y: 9}, {y: 10}]
+              }
+            ]
+          },
+        ];
+
+        it('should combine values arrays of objects with identical labels', function () {
+          var items = Legend.prototype._transformSeriesData(seriesData);
+          expect(items.length).to.be(3);
+          items.forEach(function (item) {
+            expect(item.values.length).to.be(6);
+          });
+        });
       });
 
       describe('legend item label matches vis item label', function () {
