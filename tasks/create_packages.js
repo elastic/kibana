016323bda31f34e1188706367a57d5f2531ec236
Jordan Sissel <jls@semicomplete.com>
2015-04-28 14:01:48 -0700

    Create rpm, deb, and osx .pkg files
    This uses fpm to create rpm, deb, and osx package files for Kibana.
    
    For rpm and deb, sysv init scripts are included from the
    `create_services` task.

--- tasks/create_packages.js
+++ tasks/create_packages.js
@@ -19,23 +19,45 @@ function createPackages(grunt) {
     var done = this.async();
     var target = grunt.config.get('target');
     var distPath = join(grunt.config.get('build'), 'dist');
+    var version = grunt.config.get('pkg.version');
 
     var createPackage = function (name) {
       var options = { cwd: distPath };
       var archiveName = join(target, name);
-      var tgzCmd = [ 'tar', '-zcf', archiveName + '.tar.gz', name ];
-      var zipCmd = [ 'zip', '-rq', archiveName + '.zip', name ];
+      var commands = [];
+      var arch = /x64$/.test(name) ? 'x86_64' : 'i686';
 
+      var fpm_options = [ 'fpm', '-f', '-p', target, '-s', 'dir', '-n', 'kibana', '-v', version ];
+      var fpm_files = join(distPath, name) + '/=/opt/kibana';
+
+      // kibana.tar.gz
+      commands.push([ 'tar', '-zcf', archiveName + '.tar.gz', name ]);
+
+      // kibana.zip
       if (/windows/.test(name)) {
-        zipCmd = [ 'zip', '-rq', '-ll', archiveName + '.zip', name ];
+        commands.push([ 'zip', '-rq', '-ll', archiveName + '.zip', name ]);
+      } else {
+        commands.push([ 'zip', '-rq', archiveName + '.zip', name ]);
+      }
+
+      // TODO(sissel): Add before-install scripts to create kibana user
+      // TODO(sissel): Check if `fpm` is available
+      if (/linux-x(86|64)$/.test(name)) {
+        // kibana.rpm and kibana.deb
+        var sysv_init = join(distPath, 'services', 'sysv') + '/etc/=/etc/';
+        commands.push(fpm_options.concat(['-t', 'rpm', '-a', arch, fpm_files, sysv_init]));
+        commands.push(fpm_options.concat(['-t', 'deb', '-a', arch, fpm_files, sysv_init]));
+      } else if (/darwin-x(86|64)$/.test(name)) {
+        // kibana.pkg
+        var launchd = join(distPath, 'services', 'launchd') + '/=/';
+        commands.push(fpm_options.concat(['-t', 'osxpkg', '-a', arch, fpm_files, launchd]));
       }
 
       return mkdirp.mkdirpAsync(target)
         .then(function (arg) {
-          return execFile(tgzCmd.shift(), tgzCmd, options);
-        }, function (err) { console.log('Failure on ' + name + ': ' + err); })
-        .then(function (arg) {
-          return execFile(zipCmd.shift(), zipCmd, options);
+          return Promise.map(commands, function (cmd) {
+            return execFile(cmd.shift(), cmd, options);
+          });
         }, function (err) { console.log('Failure on ' + name + ': ' + err); });
     };
 
