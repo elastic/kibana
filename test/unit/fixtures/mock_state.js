f4f65662b5c273b25c398068866c026f52c1cc5a
Joe Fleming <joe.fleming@gmail.com>
2015-04-28 15:16:27 -0700

    [mockState] allow passing in default values

--- test/unit/fixtures/mock_state.js
+++ test/unit/fixtures/mock_state.js
@@ -2,10 +2,11 @@ define(function (require) {
   var _ = require('lodash');
   var sinon = require('test_utils/auto_release_sinon');
 
-  function MockState() {
+  function MockState(defaults) {
     this.on = _.noop;
     this.off = _.noop;
     this.save = sinon.stub();
+    _.assign(this, defaults);
   }
 
   MockState.prototype.resetStub = function () {
