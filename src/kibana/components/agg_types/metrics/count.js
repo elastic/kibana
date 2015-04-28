72e4e576726c930f45e9831efef660ca1e2bbe5d
Spencer Alger <email@spalger.com>
2015-04-28 16:19:30 -0700

    [aggTypes/count] remove custom format

--- src/kibana/components/agg_types/metrics/count.js
+++ src/kibana/components/agg_types/metrics/count.js
@@ -1,16 +1,6 @@
 define(function (require) {
   return function AggTypeMetricCountProvider(Private) {
-    var _ = require('lodash');
     var MetricAggType = Private(require('components/agg_types/metrics/_metric_agg_type'));
-    var fieldFormats = Private(require('registry/field_formats'));
-
-    _(CountFormat).inherits(Private(require('components/index_patterns/_field_format')));
-    function CountFormat() {
-      this._convert = function (val) {
-        return (val || 0) + ' hits';
-      };
-      CountFormat.Super.call(this);
-    }
 
     return new MetricAggType({
       name: 'count',
@@ -21,8 +11,7 @@ define(function (require) {
       },
       getValue: function (agg, bucket) {
         return bucket.doc_count;
-      },
-      getFormat: _.constant(new CountFormat())
+      }
     });
   };
 });
