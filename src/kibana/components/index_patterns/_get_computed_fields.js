58fff3deb4bf70dd5fcac2dfdaf1886280dad47b
Brady Vidovic <bradvido@users.noreply.github.com>
2015-05-11 16:52:46 -0500

    Check for script_file in scripted field
    Checks if the script value provided by the user ends with '.[known_script_extension]'. If it does, then `script_file` is used instead of `script` as the key sent to ES. 
    The language is also updated to whatever language the user provided as the extension.
    
    I've tested myself on 4.1 it is working for me.

--- src/kibana/components/index_patterns/_get_computed_fields.js
+++ src/kibana/components/index_patterns/_get_computed_fields.js
@@ -10,7 +10,32 @@ define(function (require) {
     fielddataFields = _.pluck(self.fields.byType.date, 'name');
 
     _.each(self.getScriptedFields(), function (field) {
-      scriptFields[field.name] = { script: field.script, lang: field.lang };
+        
+      //if the value of the script field ends with '.[knownscriptlang]'
+      //then it is assumed to reference a script file and we use the 'script_file' param instead of 'script'
+      var langs = ['groovy', 'expression', 'mustache', 'mvel', 'js', 'python'];
+      function getLangUsedInScriptFile(scriptVal) {
+          for (var i in langs) {
+              var lang = langs[i];
+              var suffix = "." + lang;
+              if (scriptVal.toLowerCase().indexOf(suffix, scriptVal.length - suffix.length) !== -1) {
+                //checks if scriptVal ends with suffix
+                return lang;
+              }
+          }
+          return false; //no match for any known lang
+      }
+      var scriptFileLang = getLangUsedInScriptFile(field.script);
+      var scriptObj = {};
+      if (scriptFileLang) {
+          scriptObj.lang = scriptFileLang;
+          //trim the extension from the script val and specify script_file as param
+          scriptObj.script_file = field.script.substring(0, field.script.toLowerCase().indexOf("." + scriptFileLang));
+      } else {//normal dynamic script
+          scriptObj.lang = field.lang;
+          scriptObj.script = field.script;
+      }
+      scriptFields[field.name] = scriptObj;
     });
 
     return {
