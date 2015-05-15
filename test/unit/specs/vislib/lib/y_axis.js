d3bca416bbe395a5250602b7819677c4e3bbd381
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-05-15 16:21:30 -0700

    making testing corrections

--- test/unit/specs/vislib/lib/y_axis.js
+++ test/unit/specs/vislib/lib/y_axis.js
@@ -288,11 +288,11 @@ define(function (require) {
 
       it('should return a function', function () {
         fnNames.forEach(function (fnName) {
-          expect(typeof yAxis._getScaleType(fnName)).to.be('function');
+          expect(yAxis._getScaleType(fnName)).to.be.a(Function);
         });
 
         // if no value is provided to the function, scale should default to a linear scale
-        expect(typeof yAxis._getScaleType()).to.be('function');
+        expect(yAxis._getScaleType()).to.be.a(Function);
       });
 
       it('should throw an error if function name is undefined', function () {
