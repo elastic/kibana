5e58d8723a27f28f832b572c0eb4010c8218b24f
Spencer Alger <email@spalger.com>
2015-05-22 11:07:36 -0700

    [testing] wrap mocha's describe function to accept an array

--- test/unit/specs/components/stringify/index.js
+++ test/unit/specs/components/stringify/index.js
@@ -1,12 +1,8 @@
 define(function (require) {
   describe('Stringify Component', function () {
-    run(require('specs/components/stringify/_conformance'));
-    run(require('specs/components/stringify/_ip'));
-    run(require('specs/components/stringify/_source'));
-    run(require('specs/components/stringify/_url'));
-
-    function run(suite) {
-      describe(suite[0], suite[1]);
-    }
+    describe(require('specs/components/stringify/_conformance'));
+    describe(require('specs/components/stringify/_ip'));
+    describe(require('specs/components/stringify/_source'));
+    describe(require('specs/components/stringify/_url'));
   });
 });
--- test/unit/specs/components/vis/index.js
+++ test/unit/specs/components/vis/index.js
@@ -1,10 +1,6 @@
 define(function (require) {
   describe('Vis Component', function () {
-    var childSuites = [
-      require('specs/components/vis/_agg_config'),
-      require('specs/components/vis/_agg_configs')
-    ].forEach(function (s) {
-      describe(s[0], s[1]);
-    });
+    describe(require('specs/components/vis/_agg_config'));
+    describe(require('specs/components/vis/_agg_configs'));
   });
 });
