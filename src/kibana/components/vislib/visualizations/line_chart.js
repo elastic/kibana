58942786b8374da289052193861b714ab8c5a4c0
Joe Fleming <joe.fleming@gmail.com>
2015-06-01 15:33:34 -0700

    check for values less than 1
    log scale can't graph values below 1, so the method name was also changed to reflect intention

--- src/kibana/components/vislib/visualizations/line_chart.js
+++ src/kibana/components/vislib/visualizations/line_chart.js
@@ -297,7 +297,9 @@ define(function (require) {
             timeMarker = new TimeMarker(times, xScale, height);
           }
 
-          if (self._attr.scale === 'log' && self._checkForNegVals(data)) throw new errors.CannotLogScaleNegVals();
+          if (self._attr.scale === 'log' && self._invalidLogScaleValues(data)) {
+            throw new errors.CannotLogScaleNegVals();
+          }
 
           if (width < minWidth || height < minHeight) {
             throw new errors.ContainerTooSmall();
