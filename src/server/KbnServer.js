daadf9709ba2dc03772ee66ff782405d735b3c32
Spencer Alger <email@spalger.com>
2015-06-25 16:53:12 -0700

    document KbnServer class

--- src/server/KbnServer.js
+++ src/server/KbnServer.js
@@ -23,16 +23,33 @@ function KbnServer(settings) {
   ));
 }
 
-KbnServer.prototype.mixin = function () {
+/**
+ * Extend the KbnServer outside of the constraits of a plugin. This allows access
+ * to APIs that are not exposed (intentionally) to the plugins and should only
+ * be used when the code will be kept up to date with Kibana.
+ *
+ * @param {...function} - functions that should be called to mixin functionality.
+ *                         They are called with the arguments (kibana, server, config)
+ *                         and can return a promise to delay execution of the next mixin
+ * @return {Promise} - promise that is resolved when the final mixin completes.
+ */
+KbnServer.prototype.mixin = function (/* ...fns */) {
+  var fns = _.toArray(arguments);
   var self = this;
   var server = self.server;
 
-  return Promise.each(_.toArray(arguments), function (fn) {
+  return Promise.each(fns, function (fn) {
     return fn(self, server, server.config && server.config());
   })
+  // clear the return value
   .then(_.noop);
 };
 
+/**
+ * Tell the server to listen for incoming requests.
+ *
+ * @return {Promise} resolved with the server once it is listening
+ */
 KbnServer.prototype.listen = function () {
   var self = this;
   var server = self.server;
@@ -54,6 +71,7 @@ KbnServer.prototype.listen = function () {
   );
 };
 
+// if this module was called from the command line, go ahead and start listening
 if (require.main === module) {
   (new KbnServer())
   .listen()
