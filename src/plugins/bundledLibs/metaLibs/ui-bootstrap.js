af305c79e440cf4e167449e3eb0caf96ba41f961
spalger <email@spalger.com>
2015-07-21 23:20:07 -0700

    use ui-bootstrap with bundled templates

--- src/plugins/bundledLibs/metaLibs/ui-bootstrap.js
+++ src/plugins/bundledLibs/metaLibs/ui-bootstrap.js
@@ -1,6 +1,6 @@
 define(function (require) {
   require('angular');
-  require('bower_components/angular-bootstrap/ui-bootstrap.js');
+  require('bower_components/angular-bootstrap/ui-bootstrap-tpls');
 
   return require('ui/modules')
   .get('kibana', ['ui.bootstrap'])
