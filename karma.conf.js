d05d6a7f323a7ef020cc9677b3b6922f015f3a7f
spalger <email@spalger.com>
2015-07-28 17:24:45 -0700

    stretch out the browser-start timeout, Jenkins seems to be really slow from time to time

--- karma.conf.js
+++ karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    captureTimeout: 30000,
     browserNoActivityTimeout: 120000,
 
     // frameworks to use
