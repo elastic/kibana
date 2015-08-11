08b90681744d7312f8bfe50dd4baee98488f2b5d
spalger <email@spalger.com>
2015-08-11 11:40:10 -0700

    manually preload to prevent sourcemap error

--- src/plugins/kibana/public/visualize/index.js
+++ src/plugins/kibana/public/visualize/index.js
@@ -4,11 +4,23 @@ define(function (require) {
   require('plugins/kibana/visualize/editor/editor');
   require('plugins/kibana/visualize/wizard/wizard');
 
-  require.context('./editor', false);
-  require.context('./editor/panels', false);
-
   require('ui/routes')
   .when('/visualize', {
     redirectTo: '/visualize/step/1'
   });
+
+  // preloading
+  require('plugins/kibana/visualize/editor/add_bucket_agg');
+  require('plugins/kibana/visualize/editor/agg');
+  require('plugins/kibana/visualize/editor/agg_add');
+  require('plugins/kibana/visualize/editor/agg_filter');
+  require('plugins/kibana/visualize/editor/agg_group');
+  require('plugins/kibana/visualize/editor/agg_param');
+  require('plugins/kibana/visualize/editor/agg_params');
+  require('plugins/kibana/visualize/editor/editor');
+  require('plugins/kibana/visualize/editor/nesting_indicator');
+  require('plugins/kibana/visualize/editor/sidebar');
+  require('plugins/kibana/visualize/editor/vis_options');
+  require('plugins/kibana/visualize/saved_visualizations/_saved_vis');
+  require('plugins/kibana/visualize/saved_visualizations/saved_visualizations');
 });
