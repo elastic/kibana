63bc181871d1ec3a4bf0ddeaa1759db1f134a07f
Lukas Olson <olson.lukas@gmail.com>
2015-08-21 17:11:18 -0700

    Attempt to fix issues with elasticsearch and ssl

--- src/plugins/elasticsearch/lib/create_agent.js
+++ src/plugins/elasticsearch/lib/create_agent.js
@@ -1,30 +1,34 @@
 var url = require('url');
 var fs = require('fs');
 var http = require('http');
+var https = require('https');
 var agentOptions;
 module.exports = function (server) {
   var config = server.config();
   var target = url.parse(config.get('elasticsearch.url'));
 
+  var protocol = http;
+
   if (!agentOptions) {
-    agentOptions = {
-      rejectUnauthorized: config.get('elasticsearch.ssl.verify')
-    };
+    agentOptions = {};
 
     var customCA;
-    if (/^https/.test(target.protocol) && config.get('elasticsearch.ssl.ca')) {
-      customCA = fs.readFileSync(config.get('elasticsearch.ssl.ca'), 'utf8');
-      agentOptions.ca = [customCA];
-    }
+    if (/^https/.test(target.protocol)) {
+      protocol = https;
+      agentOptions.rejectUnauthorized = config.get('elasticsearch.ssl.verify');
 
-    // Add client certificate and key if required by elasticsearch
-    if (/^https/.test(target.protocol) &&
-        config.get('elasticsearch.ssl.cert') &&
-        config.get('elasticsearch.ssl.key')) {
-      agentOptions.crt = fs.readFileSync(config.get('elasticsearch.ssl.cert'), 'utf8');
-      agentOptions.key = fs.readFileSync(config.get('elasticsearch.ssl.key'), 'utf8');
+      if (config.get('elasticsearch.ssl.ca')) {
+        customCA = fs.readFileSync(config.get('elasticsearch.ssl.ca'), 'utf8');
+        agentOptions.ca = [customCA];
+      }
+
+      // Add client certificate and key if required by elasticsearch
+      if (config.get('elasticsearch.ssl.cert') && config.get('elasticsearch.ssl.key')) {
+        agentOptions.crt = fs.readFileSync(config.get('elasticsearch.ssl.cert'), 'utf8');
+        agentOptions.key = fs.readFileSync(config.get('elasticsearch.ssl.key'), 'utf8');
+      }
     }
   }
 
-  return new http.Agent(agentOptions);
+  return new protocol.Agent(agentOptions);
 };
