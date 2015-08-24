86808b744b97368a8921d53b0206cc2abadccc2b
Lukas Olson <olson.lukas@gmail.com>
2015-08-24 08:49:58 -0700

    Fix issues with https agent creation and caching agentOptions

--- src/plugins/elasticsearch/lib/create_agent.js
+++ src/plugins/elasticsearch/lib/create_agent.js
@@ -1,34 +1,28 @@
 var url = require('url');
-var fs = require('fs');
+var _ = require('lodash');
+var readFile = _.partialRight(require('fs').readFileSync, 'utf8');
 var http = require('http');
 var https = require('https');
-var agentOptions;
-module.exports = function (server) {
+
+module.exports = _.memoize(function (server) {
   var config = server.config();
   var target = url.parse(config.get('elasticsearch.url'));
 
-  var protocol = http;
+  if (!/^https/.test(target.protocol)) return new http.Agent();
 
-  if (!agentOptions) {
-    agentOptions = {};
+  var agentOptions = {
+    rejectUnauthorized: config.get('elasticsearch.ssl.verify')
+  };
 
-    var customCA;
-    if (/^https/.test(target.protocol)) {
-      protocol = https;
-      agentOptions.rejectUnauthorized = config.get('elasticsearch.ssl.verify');
-
-      if (config.get('elasticsearch.ssl.ca')) {
-        customCA = fs.readFileSync(config.get('elasticsearch.ssl.ca'), 'utf8');
-        agentOptions.ca = [customCA];
-      }
-
-      // Add client certificate and key if required by elasticsearch
-      if (config.get('elasticsearch.ssl.cert') && config.get('elasticsearch.ssl.key')) {
-        agentOptions.crt = fs.readFileSync(config.get('elasticsearch.ssl.cert'), 'utf8');
-        agentOptions.key = fs.readFileSync(config.get('elasticsearch.ssl.key'), 'utf8');
-      }
-    }
+  if (config.get('elasticsearch.ssl.ca')) {
+    agentOptions.ca = [readFile(config.get('elasticsearch.ssl.ca'))];
   }
 
-  return new protocol.Agent(agentOptions);
-};
+  // Add client certificate and key if required by elasticsearch
+  if (config.get('elasticsearch.ssl.cert') && config.get('elasticsearch.ssl.key')) {
+    agentOptions.cert = readFile(config.get('elasticsearch.ssl.cert'));
+    agentOptions.key = readFile(config.get('elasticsearch.ssl.key'));
+  }
+
+  return new https.Agent(agentOptions);
+});
