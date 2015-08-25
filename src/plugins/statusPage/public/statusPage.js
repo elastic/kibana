feecd26f42cc2551792b489d0c80cee4c8221586
Jim Unger <jim.unger@elastic.co>
2015-08-25 13:31:07 -0500

    Simplified the status page style

--- src/plugins/statusPage/public/statusPage.js
+++ src/plugins/statusPage/public/statusPage.js
@@ -34,16 +34,10 @@ require('ui/chrome')
       ui.metrics = data.metrics;
       ui.statuses = data.status.statuses;
 
-      ui.issues = _.filter(ui.statuses, function (n) {
-        return n.state !== 'green';
-      });
-
-      console.log(ui);
-
       var overall = data.status.overall;
       if (!ui.serverState || (ui.serverState !== overall.state)) {
         ui.serverState = overall.state;
-        ui.serverStateMessage = /* overall.nickname || */ overall.title;
+        ui.serverStateMessage = overall.title;
       }
     })
     .catch(function () {
