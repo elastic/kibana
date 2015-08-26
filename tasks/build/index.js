8918f26e21eeaf850f10c048d9ef774850202d6e
Joe Fleming <joe.fleming@gmail.com>
2015-08-26 13:35:01 -0700

    clearer task naming

--- tasks/build/index.js
+++ tasks/build/index.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
     '_build:getProps',
     'clean:build',
     'clean:target',
-    '_build:downloadNodes:start',
+    '_build:downloadNodeBuilds:start',
     'copy:devSource',
     'babel:build',
     '_build:cliIndex',
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
     'clean:testsFromModules',
     'run:optimizeBuild',
     'stop:optimizeBuild',
-    '_build:downloadNodes:finish',
+    '_build:downloadNodeBuilds:finish',
     '_build:versionedLinks',
     '_build:archives',
     !grunt.option('os-packages') ? [] : [
