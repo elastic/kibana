254b382324d212854d7a8fe9243ddb093bd815e9
Joe Fleming <joe.fleming@gmail.com>
2015-09-03 17:17:34 -0700

    backport range filter changes from 30bd32f156

--- src/kibana/components/agg_types/buckets/create_filter/range.js
+++ src/kibana/components/agg_types/buckets/create_filter/range.js
@@ -2,7 +2,12 @@ define(function (require) {
   var buildRangeFilter = require('components/filter_manager/lib/range');
   return function createRangeFilterProvider(Private) {
     return function (aggConfig, params) {
-      return buildRangeFilter(aggConfig.params.field, params, aggConfig.vis.indexPattern);
+      return buildRangeFilter(
+        aggConfig.params.field,
+        params,
+        aggConfig.vis.indexPattern,
+        aggConfig.fieldFormatter()(params)
+      );
     };
   };
 });
