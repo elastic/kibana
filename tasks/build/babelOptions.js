8aa0287dd7f7c6f47ec50d3e3653c1864a884285
spalger <email@spalger.com>
2015-09-07 17:22:42 -0700

    [build] expose build specific babel config
    During build we transpile the server source to prevent runtime transpilation with babel/register. In order to support untranspiled plugin code, we still use babel/register with special ignore options, which used to be included in cli/index.js and now is moved to src/optimize/babelOptions.js so other code that wants to initialize a server (without the cli) can also configure babel/register.

---
+++ tasks/build/babelOptions.js
@@ -0,0 +1,13 @@
+module.exports = function (grunt) {
+
+  var srcFile = 'build/kibana/src/optimize/babelOptions.js';
+  var buildFile = 'build/kibana/src/optimize/babelOptions.build.js';
+  var rename = require('fs').renameSync;
+  var unlink = require('fs').unlinkSync;
+
+  grunt.registerTask('_build:babelOptions', function () {
+    unlink(srcFile);
+    rename(buildFile, srcFile);
+  });
+
+};
