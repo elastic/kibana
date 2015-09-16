71657e01c41b49497a23930d664385f124101212
Paul Echeverri <pecheverri@elastic.co>
2015-09-16 11:41:22 -0700

    Typo fixes unkown->unknown throughout.

--- src/ui/index.js
+++ src/ui/index.js
@@ -66,7 +66,7 @@ module.exports = async (kbnServer, server, config) => {
     handler: function (req, reply) {
       let id = req.params.id;
       let app = uiExports.apps.byId[id];
-      if (!app) return reply(Boom.notFound('Unkown app ' + id));
+      if (!app) return reply(Boom.notFound('Unknown app ' + id));
 
       if (kbnServer.status.isGreen()) {
         return reply.renderApp(app);
