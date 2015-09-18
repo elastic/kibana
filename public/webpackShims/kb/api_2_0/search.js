529037f879dacaceb9b38eeed8e081236c5e4519
Nik Everett <nik9000@gmail.com>
2015-09-18 11:39:56 -0400

    [kb] Add terminate_after to search and count

--- public/webpackShims/kb/api_2_0/search.js
+++ public/webpackShims/kb/api_2_0/search.js
@@ -45,6 +45,7 @@ define(function () {
         from: 0,
         size: 10,
         search_type: ["dfs_query_then_fetch", "dfs_query_and_fetch", "query_then_fetch", "query_and_fetch", "count", "scan"],
+        terminate_after: 10,
         lowercase_expanded_terms: ["true", "false"],
         analyze_wildcard: "__flag__",
         preference: ["_primary", "_primary_first", "_local", "_only_node:xyz", "_prefer_node:xyz", "_shards:2,3"],
