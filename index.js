693ce452ab859586c08964c37dd9e46a8a566610
Rashid Khan <github.fliplap@spamgourmet.com>
2015-09-18 11:21:06 -0700

    Make timelion dependent on elasticsearch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 module.exports = function (kibana) {
   return new kibana.Plugin({
-    require: ['kibana'],
+    require: ['kibana', 'elasticsearch'],
     uiExports: {
       app: {
         title: 'Timelion',
