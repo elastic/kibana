351033607789a0b7f9b5fb9b38ac9a50ee7fea91
spalger <email@spalger.com>
2015-09-22 18:58:14 -0700

    [npm] added lint and lintroller scripts

--- tasks/config/eslint.js
+++ tasks/config/eslint.js
@@ -1,7 +1,25 @@
+var resolve = require('path').resolve;
+
 module.exports = function (grunt) {
   return {
     // just lint the source dir
     source: {
+      options: {
+        cache: resolve(grunt.config.get('root'), '.eslint.fixSource.cache')
+      },
+
+      files: {
+        src: '<%= lintThese %>'
+      }
+    },
+
+    // just lint the source dir
+    fixSource: {
+      options: {
+        cache: resolve(grunt.config.get('root'), '.eslint.fixSource.cache'),
+        fix: true
+      },
+
       files: {
         src: '<%= lintThese %>'
       }
