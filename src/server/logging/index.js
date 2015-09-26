3902c380cd72c65c774e795dbae8cd1f02b41126
Chris Cowan <chris@chriscowan.us>
2015-09-26 10:09:56 -0700

    Fixing Edge Cases
    - Add ability to specify filters from CLI options
    - Add test and fix for removing branch with censor
    - Add change regex to use strings

--- src/server/logging/index.js
+++ src/server/logging/index.js
@@ -43,9 +43,13 @@ module.exports = function (kbnServer, server, config) {
             config: {
               json: config.get('logging.json'),
               dest: config.get('logging.dest'),
-              filter: {
+              // I'm adding the default here because if you add another filter
+              // using the commandline it will remove authorization. I want users
+              // to have to explicitly set --logging.filter.authorization=none to
+              // have it show up int he logs.
+              filter: _.defaults(config.get('logging.filter'), {
                 authorization: 'remove'
-              }
+              })
             },
             events: _.transform(events, function (filtered, val, key) {
               // provide a string compatible way to remove events
