a69b0271307c5e57d08f693aaf7c5ae712330aac
spalger <email@spalger.com>
2015-09-30 03:53:04 +0200

    [server/log] simplify the log messages before filtering
    `applyFilterToKey()` used to choke on circular structures by simply recursing through the object passed in. When we create a circular structure though, we generally provide a simplified version of the object via `obj#toJSON()`. This makes logging those objects simple, and now the filter will use the same mechanism before attempting to itterate the object.

--- src/server/logging/__tests__/applyFilterToKey.js
+++ src/server/logging/__tests__/applyFilterToKey.js
@@ -1,4 +1,5 @@
 var applyFilterToKey = require('../applyFilterToKey');
+var applyFiltersToKeys = applyFilterToKey.applyFiltersToKeys;
 var expect = require('expect.js');
 
 function fixture() {
@@ -15,7 +16,7 @@ describe('applyFilterToKey(obj, key, action)', function () {
 
   it('should remove a key from an object recursivly', function () {
     var data = fixture();
-    applyFilterToKey(data, 'authorization', 'remove');
+    data = applyFilterToKey(data, 'authorization', 'remove');
     expect(data).to.eql({
       req: { headers: {} }
     });
@@ -23,7 +24,7 @@ describe('applyFilterToKey(obj, key, action)', function () {
 
   it('should remove an entire branch', function () {
     var data = fixture();
-    applyFilterToKey(data, 'headers', 'remove');
+    data = applyFilterToKey(data, 'headers', 'remove');
     expect(data).to.eql({
       req: { }
     });
@@ -31,7 +32,7 @@ describe('applyFilterToKey(obj, key, action)', function () {
 
   it('should remove an entire branch with censor', function () {
     var data = fixture();
-    applyFilterToKey(data, 'headers', 'censor');
+    data = applyFilterToKey(data, 'headers', 'censor');
     expect(data).to.eql({
       req: { }
     });
@@ -39,7 +40,7 @@ describe('applyFilterToKey(obj, key, action)', function () {
 
   it('should censor a key in an object recursivly', function () {
     var data = fixture();
-    applyFilterToKey(data, 'authorization', 'censor');
+    data = applyFilterToKey(data, 'authorization', 'censor');
     expect(data).to.eql({
       req: {
         headers: {
@@ -52,7 +53,7 @@ describe('applyFilterToKey(obj, key, action)', function () {
   it('should censor key with a RegEx in an object recursivly', function () {
     var data = fixture();
     var regex = '/([^\\s]+)$/';
-    applyFilterToKey(data, 'authorization', regex);
+    data = applyFilterToKey(data, 'authorization', regex);
     expect(data).to.eql({
       req: {
         headers: {
@@ -62,4 +63,44 @@ describe('applyFilterToKey(obj, key, action)', function () {
     });
   });
 
+  it('uses the JSON version of objects for serializaion', function () {
+    var data = applyFilterToKey({
+      a: {
+        b: 1,
+        toJSON: () => ({ c: 10 })
+      }
+    }, 'c', 'censor');
+
+    expect(data).to.eql({
+      a: {
+        c: 'XX'
+      }
+    });
+  });
+
+  describe('applyFiltersToKeys(obj, actionsByKey)', function () {
+    it('applies applyFilterToKey() for each key+prop in actionsByKey', function () {
+      var data = applyFiltersToKeys({
+        a: {
+          b: {
+            c: 1
+          },
+          d: {
+            e: 'foobar'
+          }
+        }
+      }, {
+        b: 'remove',
+        e: 'censor',
+      });
+
+      expect(data).to.eql({
+        a: {
+          d: {
+            e: 'XXXXXX',
+          },
+        },
+      });
+    });
+  });
 });
