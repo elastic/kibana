d6b82bc4be9d7df0bebaa6e21f1e5e1833c9604a
leedr <lee.drengenberg@elastic.co>
2015-10-09 09:52:03 -0500

    Many new tests, updates to tests, and refactoring.

--- test/support/pages/HeaderPage.js
+++ test/support/pages/HeaderPage.js
@@ -1,5 +1,6 @@
 // in test/support/pages/HeaderPage.js
 define(function (require) {
+
   // the page object is created as a constructor
   // so we can provide the remote Command object
   // at runtime
@@ -7,44 +8,42 @@ define(function (require) {
     this.remote = remote;
   }
 
+  var defaultTimeout = 5000;
+
   HeaderPage.prototype = {
     constructor: HeaderPage,
 
-    clickDiscover: function () {
+    clickDiscover: function clickDiscover() {
       return this.remote
-        .setFindTimeout(5000)
+        .setFindTimeout(defaultTimeout)
         .findByCssSelector('a[href*=\'discover\']')
         .then(function (tab) {
           return tab.click();
         });
     },
-    clickVisualize: function () {
+    clickVisualize: function clickVisualize() {
       return this.remote
-        .setFindTimeout(5000)
+        .setFindTimeout(defaultTimeout)
         .findByCssSelector('a[href*=\'visualize\']')
         .then(function (tab) {
           return tab.click();
         });
     },
-    clickDashboard: function () {
+    clickDashboard: function clickDashboard() {
       return this.remote
-        .setFindTimeout(5000)
+        .setFindTimeout(defaultTimeout)
         .findByCssSelector('a[href*=\'dashboard\']')
         .then(function (tab) {
           return tab.click();
         });
     },
-    clickSettings: function () {
+    clickSettings: function clickSettings() {
       return this.remote
-        .setFindTimeout(5000)
+        .setFindTimeout(defaultTimeout)
         .findByCssSelector('a[href*=\'settings\']')
         .then(function (tab) {
           return tab.click();
         });
-    },
-
-    log: function (logString) {
-      console.log(Date.now() + ' : ' + logString);
     }
   };
 
