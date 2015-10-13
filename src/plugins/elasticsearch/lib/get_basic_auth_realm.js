cd38f9a5663adf03670cf1dba0ac2a1bba541134
Rashid Khan <github.fliplap@spamgourmet.com>
2015-10-12 17:23:09 -0700

    Fix conflict, closes #5082

---
+++ src/plugins/elasticsearch/lib/get_basic_auth_realm.js
@@ -0,0 +1,7 @@
+module.exports = function getBasicAuthRealm(message) {
+  if (!message || typeof message !== 'string') return null;
+
+  const parts = message.match(/Basic\ realm=\\"(.*)\\"/);
+  if (parts && parts.length === 2) return parts[1];
+  else return null;
+};
