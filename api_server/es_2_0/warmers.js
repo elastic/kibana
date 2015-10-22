ff39217f84505fb71903fed63370712cb198b59c
spalger <email@spalger.com>
2015-10-22 10:23:22 -0500

    move to apache 2.0

--- api_server/es_2_0/warmers.js
+++ api_server/es_2_0/warmers.js
@@ -1,23 +1,3 @@
-/**
- * ELASTICSEARCH CONFIDENTIAL
- * _____________________________
- *
- *  [2014] Elasticsearch Incorporated All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Elasticsearch Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Elasticsearch Incorporated
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Elasticsearch Incorporated.
- */
-
-
-'use strict';
-
 module.exports = function (api) {
   api.addEndpointDescription('_get_warmer', {
     patterns: ["_warmer", "_warmer/{name}", "{indices}/_warmer", "{indices}/_warmer/{name}"]
