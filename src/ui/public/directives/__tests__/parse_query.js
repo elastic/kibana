c2f2c95a470603da0e13df28e371445a60f663c8
Jim Unger <BigFunger@gmail.com>
2015-10-27 14:23:25 -0500

    fixed parse_query tests

--- src/ui/public/directives/__tests__/parse_query.js
+++ src/ui/public/directives/__tests__/parse_query.js
@@ -4,7 +4,7 @@ var expect = require('expect.js');
 var ngMock = require('ngMock');
 
 // Load the kibana app dependencies.
-require('ui/validate_query');
+require('ui/parse_query');
 
 var $rootScope;
 var $timeout;
@@ -18,42 +18,15 @@ var $elem;
 
 var cycleIndex = 0;
 var mockValidateQuery;
-var markup = '<input ng-model="mockModel" validate-query="mockQueryInput" input-focus type="text">';
+var markup = '<input ng-model="mockModel" parse-query input-focus type="text">';
 var fromUser;
-var toUser = require('ui/validate_query/lib/to_user');
-
-
-var validEsResponse = function () {
-  return Promise.resolve({ valid: true });
-};
-
-var invalidEsResponse = function () {
-  return Promise.reject({ body: { error: 'mock invalid query' } });
-};
-
-var checkClass = function (className) {
-  expect($elem.hasClass(className)).to.be(true);
-};
+var toUser = require('ui/parse_query/lib/to_user');
 
 var init = function () {
   // Load the application
   ngMock.module('kibana');
 
   ngMock.module('kibana', function ($provide) {
-    $provide.service('es', function () {
-      return { indices: { validateQuery: function () {} } };
-    });
-
-    // Super simple config stub
-    $provide.service('config', function () {
-      var keys = {};
-      return {
-        get: function (key) { return keys[key]; },
-        set: function (key, value) { keys[key] = value; }
-      };
-    });
-
-    $provide.constant('kbnIndex', 'test-index');
   });
 
   // Create the scope
@@ -66,9 +39,6 @@ var init = function () {
 
     // Give us a scope
     $rootScope = _$rootScope_;
-
-    var es = $injector.get('es');
-    mockValidateQuery = sinon.stub(es.indices, 'validateQuery');
   });
 };
 
@@ -82,96 +52,22 @@ var compile = function () {
   $rootScope.$digest();
 };
 
-describe('validate-query directive', function () {
+describe('parse-query directive', function () {
   describe('initialization', function () {
     beforeEach(function () {
       init();
-      mockValidateQuery.returns(validEsResponse());
       compile();
     });
 
     it('should use the model', function () {
       expect($elemScope).to.have.property('ngModel');
     });
-
-    it('should call validate with changes', function () {
-      // once for init
-      expect(mockValidateQuery.callCount).to.be(1);
-
-      $rootScope.mockModel = 'different input';
-      $rootScope.$digest();
-      // once on change (leading edge)
-      expect(mockValidateQuery.callCount).to.be(2);
-      $timeout.flush();
-      // once on change (trailing edge)
-      expect(mockValidateQuery.callCount).to.be(3);
-    });
-  });
-
-  describe('valid querystring', function () {
-    var mockValidateReturns;
-
-    beforeEach(function () {
-      init();
-      mockValidateQuery.returns(validEsResponse());
-      compile();
-    });
-
-    it('should set valid state', function () {
-      // give angular time to set up the directive
-      checkClass('ng-valid-query-input');
-      checkClass('ng-valid');
-    });
-  });
-
-  describe('invalid querystring', function () {
-    var mockValidateReturns;
-
-    beforeEach(function () {
-      init();
-      mockValidateQuery.returns(invalidEsResponse());
-      compile();
-    });
-
-    it('should set invalid state', function () {
-      checkClass('ng-invalid');
-    });
-  });
-
-  describe('changing input', function () {
-    it('should change validity based on response', function () {
-      init();
-      mockValidateQuery.onCall(0).returns(validEsResponse());
-      compile();
-      $rootScope.$digest();
-      checkClass('ng-valid');
-
-      // leading and trailing edges
-      mockValidateQuery.onCall(1).returns(invalidEsResponse());
-      mockValidateQuery.onCall(2).returns(invalidEsResponse());
-      $rootScope.mockModel = 'invalid:';
-      // trigger model change, which fires watcher
-      $rootScope.$digest();
-      // leading edge
-      $timeout.flush(); // trailing edge
-      checkClass('ng-invalid');
-
-      // leading and trailing edges
-      mockValidateQuery.onCall(3).returns(validEsResponse());
-      mockValidateQuery.onCall(4).returns(validEsResponse());
-      $rootScope.mockModel = 'valid';
-      // trigger model change, which fires watcher
-      $rootScope.$digest();
-      // leading edge
-      $timeout.flush(); // trailing edge
-      checkClass('ng-valid');
-    });
   });
 
   describe('user input parser', function () {
 
     beforeEach(function () {
-      fromUser = Private(require('ui/validate_query/lib/from_user'));
+      fromUser = Private(require('ui/parse_query/lib/from_user'));
       config.set('query:queryString:options', {});
     });
 
@@ -192,7 +88,6 @@ describe('validate-query directive', function () {
       expect(fromUser('foo')).to.eql({query_string: {query: 'foo', analyze_wildcard: true}});
       expect(fromUser('')).to.eql({query_string: {query: '*', analyze_wildcard: true}});
     });
-
 
     it('should treat input that does not start with { as a query string', function () {
       expect(fromUser('foo')).to.eql({query_string: {query: 'foo'}});
