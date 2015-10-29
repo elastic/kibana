f51ea0b916a3d92fdbfc54752fb84bead4b12eb5
Matthew Bargar <mbargar@gmail.com>
2015-10-29 18:37:10 -0400

    adding broken tests

---
+++ src/plugins/api/index_pattern_endpoints.js
@@ -0,0 +1,89 @@
+export default function (server) {
+  server.route({
+    path: '/api/index-patterns',
+    method: 'GET',
+    handler: function (req, reply) {
+      let client = server.plugins.elasticsearch.client;
+
+      client.search({
+        index: '.kibana',
+        type: 'index-pattern',
+        body: {
+          query: {
+            match_all: {}
+          }
+        }
+      }).then(function (patterns) {
+        reply(patterns);
+      });
+    }
+  });
+
+  server.route({
+    path: '/api/index-patterns/{id}',
+    method: 'GET',
+    handler: function (req, reply) {
+      let client = server.plugins.elasticsearch.client;
+
+      client.get({
+        index: '.kibana',
+        type: 'index-pattern',
+        id: req.params.id
+      }).then(function (pattern) {
+        reply(pattern);
+      });
+    }
+  });
+
+  server.route({
+    path: '/api/index-patterns',
+    method: 'POST',
+    handler: function (req, reply) {
+      let client = server.plugins.elasticsearch.client;
+
+      client.create({
+        index: '.kibana',
+        type: 'index-pattern',
+        id: req.payload.title,
+        body: req.payload
+      }).then(function (pattern) {
+        reply(pattern);
+      });
+    }
+  });
+
+  server.route({
+    path: '/api/index-patterns/{id}',
+    method: 'PUT',
+    handler: function (req, reply) {
+      let client = server.plugins.elasticsearch.client;
+
+      client.update({
+        index: '.kibana',
+        type: 'index-pattern',
+        id: req.params.id,
+        body: {
+          doc: req.payload
+        }
+      }).then(function (pattern) {
+        reply(pattern);
+      });
+    }
+  });
+
+  server.route({
+    path: '/api/index-patterns/{id}',
+    method: 'DELETE',
+    handler: function (req, reply) {
+      let client = server.plugins.elasticsearch.client;
+
+      client.delete({
+        index: '.kibana',
+        type: 'index-pattern',
+        id: req.params.id
+      }).then(function (pattern) {
+        reply(pattern);
+      });
+    }
+  });
+}
