2e4140e94b955e37d3ae1591719328195020adfd
spalger <email@spalger.com>
2015-10-30 10:56:50 -0500

    [babel] configure a specific cache path
    Babel needs a path to write its cache, which it automatically places to $HOME or tmp if
    $HOME is not defined. This causes problems when the $HOME directory is not writable or
    we just do not want to write to such a central location. So This moves the cache to the
    project directory by setting the BABEL_CACHE_PATH environment variable.

--- src/optimize/babelOptions.build.js
+++ src/optimize/babelOptions.build.js
@@ -1,4 +1,5 @@
-var fromRoot = require('requirefrom')('src/utils')('fromRoot');
+var fromRoot = require('path').resolve.bind(null, __dirname, '../../');
+process.env.BABEL_CACHE_PATH = fromRoot('optimize/.babelcache.json');
 
 exports.webpack = {
   stage: 1,
--- src/optimize/babelOptions.js
+++ src/optimize/babelOptions.js
@@ -1,3 +1,5 @@
+process.env.BABEL_CACHE_PATH = require('path').resolve(__dirname, '../../optimize/.babelcache.json');
+
 exports.webpack = {
   stage: 1,
   nonStandard: false,
