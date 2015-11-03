f6c442ba7076a179a9f03697fc441102c3f11ca1
Joe Fleming <joe.fleming@gmail.com>
2015-11-03 15:53:57 -0700

    add back the status tests
    in bdd form this time

---
+++ test/functional/status_page/index.js
@@ -0,0 +1,34 @@
+define(function (require) {
+  var bdd = require('intern!bdd');
+  var expect = require('intern/dojo/node!expect.js');
+  var config = require('intern').config;
+  var getUrl = require('intern/dojo/node!../../utils/getUrl');
+  var Common = require('../../support/pages/Common');
+
+  bdd.describe('status page', function () {
+    var common;
+
+    bdd.before(function () {
+      common = new Common(this.remote);
+    });
+
+    bdd.beforeEach(function () {
+      // load the status page
+      return this.remote.get(getUrl(config.kibana, 'status'));
+    });
+
+    bdd.it('should show the kibana plugin as ready', function () {
+      var self = this;
+
+      return common.tryForTime(6000, function () {
+        return self.remote
+        .findByCssSelector('.plugin_status_breakdown')
+        .getVisibleText()
+        .then(function (text) {
+          expect(text.indexOf('plugin:kibana Ready')).to.be.above(-1);
+        })
+      })
+      .catch(common.handleError(self));
+    });
+  });
+});
