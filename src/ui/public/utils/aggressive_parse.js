fe56065978939b7b8b2b7d525c04bc40fc9a9988
Court Ewing <court@epixa.com>
2015-11-04 14:45:51 -0500

    Utility to parse $ properties while serializing json
    toJson() will strip out any properties in the object that begin with a
    dollar sign ($). If an optional custom toJson function is passed, then
    the given object is first sent through that toJson function before being
    reparsed and stringified with our dollar-sign-stripping behavior.

---
+++ src/ui/public/utils/aggressive_parse.js
@@ -0,0 +1,27 @@
+import { isString, startsWith } from 'lodash';
+
+/**
+ * Serializes the given object into a JSON string
+ *
+ * All properties that begin with $ throughout the entire object are omitted.
+ * If a custom JSON serializer function is passed, then the given object is
+ * passed through it before being re-stringified with the native stringify.
+ *
+ * The space argument is passed unaltered to the native stringify.
+ */
+export function toJson(object, jsonFn, space) {
+  if (jsonFn) {
+    // We reparse the stringified json so that we can lean on JSON.stringify's
+    // avoiding-infinite-recursion capabilities when stripping out any
+    // remaining properties that begin with a dollar sign ($)
+    object = JSON.parse(jsonFn(object));
+  }
+  return JSON.stringify(object, replacer, space);
+};
+
+/**
+ * Returns the given value if the key does not begin with a dollar sign ($)
+ */
+export function replacer(key, value) {
+  return isString(key) && startsWith(key, '$') ? undefined : value;
+}
