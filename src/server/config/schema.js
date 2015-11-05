1ae4b2ccd7768fc6fa2f699d60ac843f4157aadd
spalger <email@spalger.com>
2015-11-05 13:45:50 -0600

    add basePath config

--- src/server/config/schema.js
+++ src/server/config/schema.js
@@ -30,6 +30,7 @@ module.exports = () => Joi.object({
     port: Joi.number().default(5601),
     autoListen: Joi.boolean().default(true),
     defaultRoute: Joi.string(),
+    basePath: Joi.string().default('').allow('').regex(/[^\/]$/, 'no trailing slashes'),
     ssl: Joi.object({
       cert: Joi.string(),
       key: Joi.string()
