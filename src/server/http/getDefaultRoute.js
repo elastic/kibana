f7bc4da49acaa6496c5a332f0c157229a87385cb
spalger <email@spalger.com>
2015-11-06 15:58:32 -0600

    [server] add baseUrl to /app/kibana redirects

--- src/server/http/getDefaultRoute.js
+++ src/server/http/getDefaultRoute.js
@@ -1,11 +1,10 @@
 let _ = require('lodash');
 
 module.exports = _.once(function (kbnServer) {
+  const { uiExports, config } = kbnServer;
   // user configured default route
-  let defaultConfig = kbnServer.config.get('server.defaultRoute');
+  let defaultConfig = config.get('server.defaultRoute');
   if (defaultConfig) return defaultConfig;
 
-  // redirect to the single app
-  let apps = kbnServer.uiExports.apps.toArray();
-  return '/app/kibana';
+  return `${config.get('basePath')}/app/kibana`;
 });
