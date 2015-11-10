bbc4e53789523b8be10176c6ec7ca88477a46a55
spalger <email@spalger.com>
2015-11-09 22:29:44 -0600

    [xsrf] issue tokens via chrome vars and inject manually

--- src/server/http/xsrf.js
+++ src/server/http/xsrf.js
@@ -4,27 +4,17 @@ export default function (kbnServer, server, config) {
   const token = config.get('server.xsrf.token');
   const disabled = config.get('server.xsrf.disableProtection');
 
-  const stateOpts = {
-    isSecure: config.get('server.ssl.enabled'),
-    isHttpOnly: false,
-    path: '/',
-  };
+  server.decorate('reply', 'issueXsrfToken', function () {
+    return token;
+  });
 
   server.ext('onPostAuth', function (req, reply) {
-    if (disabled) return reply.continue();
+    if (disabled || req.method === 'get') return reply.continue();
 
-    if (req.method === 'get' && !req.state['XSRF-TOKEN'] && !req.headers['x-xsrf-token']) {
-      reply.state('XSRF-TOKEN', token, stateOpts);
-    }
+    const attempt = req.headers['kbn-xsrf-token'];
+    if (!attempt) return reply(forbidden('Missing XSRF token'));
+    if (attempt !== token) return reply(forbidden('Invalid XSRF token'));
 
-    if (req.method === 'get' || req.headers['x-xsrf-token'] === token) {
-      return reply.continue();
-    }
-
-    if (!req.headers['x-xsrf-token']) {
-      return reply(forbidden('Missing XSRF token'));
-    }
-
-    return reply(forbidden('Invalid XSRF token'));
+    return reply.continue();
   });
 }
