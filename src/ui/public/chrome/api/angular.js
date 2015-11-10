b267b950a762e2c6f8f547408e446ec7bc7f08d9
spalger <email@spalger.com>
2015-11-09 23:28:27 -0600

    [chrome/csrf] polish up some tests

--- src/ui/public/chrome/api/angular.js
+++ src/ui/public/chrome/api/angular.js
@@ -24,7 +24,7 @@ module.exports = function (chrome, internals) {
       a.href = '/elasticsearch';
       return a.href;
     }()))
-    .config(chrome.$setupCsrfRequestInterceptor)
+    .config(chrome.$setupXsrfRequestInterceptor)
     .directive('kbnChrome', function ($rootScope) {
       return {
         template: function ($el) {
