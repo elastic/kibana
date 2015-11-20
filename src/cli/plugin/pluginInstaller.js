d84df6e7ad0b56e9600a1f37c3e08bc592029554
Jim Unger <BigFunger@gmail.com>
2015-11-20 10:55:28 -0600

    [plugin installer] reformatted to use async/await syntax

--- src/cli/plugin/pluginInstaller.js
+++ src/cli/plugin/pluginInstaller.js
@@ -6,79 +6,82 @@ const pluginCleaner = require('./pluginCleaner');
 const pluginExtractor = require('./pluginExtractor');
 const KbnServer = require('../../server/KbnServer');
 const readYamlConfig = require('../serve/readYamlConfig');
-const fs = require('fs');
+const { statSync, renameSync } = require('fs');
 const Promise = require('bluebird');
-const rimraf = require('rimraf');
+const rimrafSync = require('rimraf').sync;
 const mkdirp = Promise.promisify(require('mkdirp'));
 
 export default {
   install: install
 };
 
-function install(settings, logger) {
-  logger.log(`Installing ${settings.package}`);
-
+function checkForExistingInstall(settings, logger) {
   try {
-    fs.statSync(settings.pluginPath);
+    statSync(settings.pluginPath);
 
     logger.error(`Plugin ${settings.package} already exists, please remove before installing a new version`);
     process.exit(70); // eslint-disable-line no-process-exit
   } catch (e) {
     if (e.code !== 'ENOENT') throw e;
   }
+}
+
+async function rebuildKibanaCache(settings, logger) {
+  logger.log('Optimizing and caching browser bundles...');
+  const serverConfig = _.merge(
+    readYamlConfig(settings.config),
+    {
+      env: 'production',
+      logging: {
+        silent: settings.silent,
+        quiet: !settings.silent,
+        verbose: false
+      },
+      optimize: {
+        useBundleCache: false
+      },
+      server: {
+        autoListen: false
+      },
+      plugins: {
+        initialize: false,
+        scanDirs: [settings.pluginDir, fromRoot('src/plugins')],
+        paths: [settings.workingPath]
+      }
+    }
+  );
+
+  const kbnServer = new KbnServer(serverConfig);
+  await kbnServer.ready();
+  await kbnServer.close();
+}
+
+async function install(settings, logger) {
+  logger.log(`Installing ${settings.package}`);
 
   const cleaner = pluginCleaner(settings, logger);
-  const downloader = pluginDownloader(settings, logger);
 
-  return cleaner.cleanPrevious()
-  .then(() => {
-    return mkdirp(settings.workingPath);
-  })
-  .then(() => {
-    return downloader.download();
-  })
-  .then(({ archiveType }) => {
-    return pluginExtractor (settings, logger, archiveType);
-  })
-  .then(async function() {
-    logger.log('Optimizing and caching browser bundles...');
-    const serverConfig = _.merge(
-      readYamlConfig(settings.config),
-      {
-        env: 'production',
-        logging: {
-          silent: settings.silent,
-          quiet: !settings.silent,
-          verbose: false
-        },
-        optimize: {
-          useBundleCache: false
-        },
-        server: {
-          autoListen: false
-        },
-        plugins: {
-          initialize: false,
-          scanDirs: [settings.pluginDir, fromRoot('src/plugins')],
-          paths: [settings.workingPath]
-        }
-      }
-    );
+  try {
+    checkForExistingInstall(settings, logger);
 
-    const kbnServer = new KbnServer(serverConfig);
-    await kbnServer.ready();
-    await kbnServer.close();
-  })
-  .then(() => {
-    rimraf.sync(settings.tempArchiveFile);
-  })
-  .then(() => {
-    fs.renameSync(settings.workingPath, settings.pluginPath);
+    await cleaner.cleanPrevious();
+
+    await mkdirp(settings.workingPath);
+
+    const downloader = pluginDownloader(settings, logger);
+    const { archiveType } = await downloader.download();
+
+    await pluginExtractor (settings, logger, archiveType);
+
+    await rebuildKibanaCache(settings, logger);
+
+    rimrafSync(settings.tempArchiveFile);
+
+    renameSync(settings.workingPath, settings.pluginPath);
     logger.log('Plugin installation complete');
-  })
-  .catch((err) => {
+  } catch (err) {
     logger.error(`Plugin installation was unsuccessful due to error "${err.message}"`);
     cleaner.cleanError();
     process.exit(70); // eslint-disable-line no-process-exit
-  });
+  }
 }
