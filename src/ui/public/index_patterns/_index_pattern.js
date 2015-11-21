f484b04794365aabaf8cab90d54d35d869dbfbe1
spalger <email@spalger.com>
2015-11-21 00:30:17 -0600

    destroy index patterns that are overwritten

--- src/ui/public/index_patterns/_index_pattern.js
+++ src/ui/public/index_patterns/_index_pattern.js
@@ -15,6 +15,7 @@ define(function (require) {
 
     var flattenHit = Private(require('ui/index_patterns/_flatten_hit'));
     var formatHit = require('ui/index_patterns/_format_hit');
+    var patternCache = Private(require('ui/index_patterns/_pattern_cache'));
 
     var type = 'index-pattern';
 
@@ -217,7 +218,14 @@ define(function (require) {
             var confirmMessage = 'Are you sure you want to overwrite this?';
 
             return safeConfirm(confirmMessage).then(
-              function () {
+              function overwrite() {
+                if (patternCache.get(self.id)) {
+                  return patternCache.get(self.id)
+                  .then(pattern => pattern.destroy())
+                  .then(() => patternCache.clear(self.id))
+                  .then(overwrite);
+                }
+
                 return docSource.doIndex(body).then(setId);
               },
               _.constant(false) // if the user doesn't overwrite, resolve with false
