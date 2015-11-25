f65434071324ba4ffed4c04845f09859f727eb76
spalger <email@spalger.com>
2015-11-25 14:23:28 -0600

    use intern require syntax

--- test/support/pages/Common.js
+++ test/support/pages/Common.js
@@ -3,7 +3,7 @@ define(function (require) {
   var config = require('intern').config;
   var Promise = require('bluebird');
   var moment = require('moment');
-  var testSubjSelector = require('@spalger/test-subj-selector');
+  var testSubjSelector = require('intern/dojo/node!@spalger/test-subj-selector');
   var getUrl = require('intern/dojo/node!../../utils/getUrl');
   var fs = require('intern/dojo/node!fs');
   var path = require('intern/dojo/node!path');
