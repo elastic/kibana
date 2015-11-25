d060dd4c3982ecef4f76690c085fb324be841a1e
Joe Fleming <joe.fleming@gmail.com>
2015-11-25 15:53:52 -0700

    check for scope.opts.timefield, return early if missing

--- src/kibana/plugins/discover/controllers/discover.js
+++ src/kibana/plugins/discover/controllers/discover.js
@@ -189,6 +189,9 @@ define(function (require) {
         });
 
         $scope.$watch('vis.aggs', function () {
+          // no timefield, no vis, nothing to update
+          if (!$scope.opts.timefield) return;
+
           var buckets = $scope.vis.aggs.bySchemaGroup.buckets;
 
           if (buckets && buckets.length === 1) {
