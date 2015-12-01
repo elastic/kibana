4f07e268e962b6d008b504bc40e64693feeac9f9
Matthew Bargar <mbargar@gmail.com>
2015-12-01 17:58:49 -0500

    add tests and documentation for stitch_pattern_and_mappings util function

--- src/plugins/kibana/server/lib/stitch_pattern_and_mappings.js
+++ src/plugins/kibana/server/lib/stitch_pattern_and_mappings.js
@@ -1,5 +1,12 @@
 const _ = require('lodash');
 
+/**
+ *
+ * @param {Object} pattern - An index pattern from .kibana
+ * @param {Object} mappings - Mappings from the template or indices that match the pattern, keyed by field name
+ * @returns {Object} - The index pattern with the 'mapping' property added to each object in the fields array, matched
+ *  from given array of mappings
+ */
 module.exports = function stitchPatternAndMappings(pattern, mappings) {
   _.forEach(mappings, (value, key) => {
     let field = _.find(pattern.fields, {name: key});
