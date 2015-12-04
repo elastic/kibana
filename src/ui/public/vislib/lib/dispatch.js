44a30a05c6e5dbe47a8f3b7dd6cf86bec8f39a42
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-12-04 12:23:56 -0800

    adding hasTimeField to as a vislib param to determine whether to add brushing
    Fixes #5560

--- src/ui/public/vislib/lib/dispatch.js
+++ src/ui/public/vislib/lib/dispatch.js
@@ -161,11 +161,10 @@ define(function (require) {
      */
     Dispatch.prototype.allowBrushing = function () {
       var xAxis = this.handler.xAxis;
-
       // Don't allow brushing for time based charts from non-time-based indices
-      if (xAxis.ordered && xAxis.ordered.endzones === false) return false;
+      var hasTimeField = this.handler.vis._attr.hasTimeField;
 
-      return Boolean(xAxis.ordered && xAxis.xScale && _.isFunction(xAxis.xScale.invert));
+      return Boolean(xAxis.ordered && xAxis.xScale && _.isFunction(xAxis.xScale.invert) && hasTimeField);
     };
 
     /**
