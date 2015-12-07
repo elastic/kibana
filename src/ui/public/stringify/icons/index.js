b7b974bf4d31dcf88911eedf428b2e8a691c9310
Jonathan Budzenski <jbudz@users.noreply.github.com>
2015-12-07 14:08:42 -0600

    [formatters] Move icon requires to an index file

---
+++ src/ui/public/stringify/icons/index.js
@@ -0,0 +1,7 @@
+require('!!file?name=[path][name].[ext]!ui/stringify/icons/go.png');
+require('!!file?name=[path][name].[ext]!ui/stringify/icons/stop.png');
+require('!!file?name=[path][name].[ext]!ui/stringify/icons/de.png');
+require('!!file?name=[path][name].[ext]!ui/stringify/icons/ne.png');
+require('!!file?name=[path][name].[ext]!ui/stringify/icons/us.png');
+require('!!file?name=[path][name].[ext]!ui/stringify/icons/ni.png');
+require('!!file?name=[path][name].[ext]!ui/stringify/icons/cv.png');
