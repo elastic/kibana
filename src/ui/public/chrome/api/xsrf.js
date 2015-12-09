ccc61b5a522167e84034764fd06b286330733ac2
spalger <email@spalger.com>
2015-12-09 12:05:12 -0700

    [chrome/xsrf] if a user sends a custom xsrfToken, it should be ignored

--- src/ui/public/chrome/api/xsrf.js
+++ src/ui/public/chrome/api/xsrf.js
@@ -7,9 +7,9 @@ export default function (chrome, internals) {
     return internals.version;
   };
 
-  $.ajaxPrefilter(function ({ kbnXsrfToken = internals.version }, originalOptions, jqXHR) {
+  $.ajaxPrefilter(function ({ kbnXsrfToken = true }, originalOptions, jqXHR) {
     if (kbnXsrfToken) {
-      jqXHR.setRequestHeader('kbn-version', kbnXsrfToken);
+      jqXHR.setRequestHeader('kbn-version', internals.version);
     }
   });
 
@@ -17,9 +17,9 @@ export default function (chrome, internals) {
     $httpProvider.interceptors.push(function () {
       return {
         request: function (opts) {
-          const { kbnXsrfToken = internals.version } = opts;
+          const { kbnXsrfToken = true } = opts;
           if (kbnXsrfToken) {
-            set(opts, ['headers', 'kbn-version'], kbnXsrfToken);
+            set(opts, ['headers', 'kbn-version'], internals.version);
           }
           return opts;
         }
