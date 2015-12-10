e63838dc6d0a27098ddcfd2ba776742206389531
spalger <email@spalger.com>
2015-12-10 13:52:29 -0700

    [indexPattern] added #toDetailedIndexList()

--- src/ui/public/index_patterns/__tests__/intervals.js
+++ src/ui/public/index_patterns/__tests__/intervals.js
@@ -1,5 +1,6 @@
-
+var pluck = require('lodash').pluck;
 var moment = require('moment');
+
 describe('Index Patterns', function () {
   describe('interval.toIndexList()', function () {
     var expect = require('expect.js');
@@ -16,8 +17,18 @@ describe('Index Patterns', function () {
       var end = moment.utc('2014-01-01T08:30:00Z');
       var interval = { name: 'hours', startOf: 'hour', display: 'Hourly' };
       var list = intervals.toIndexList('[logstash-]YYYY.MM.DD.HH', interval, start, end);
-      expect(list).to.contain('logstash-2014.01.01.07');
-      expect(list).to.contain('logstash-2014.01.01.08');
+      expect(list).to.eql([
+        {
+          index: 'logstash-2014.01.01.07',
+          min: moment.utc('2014-01-01T07:00:00').valueOf(),
+          max: moment.utc('2014-01-01T07:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.01.01.08',
+          min: moment.utc('2014-01-01T08:00:00').valueOf(),
+          max: moment.utc('2014-01-01T08:59:59.999').valueOf(),
+        }
+      ]);
     });
 
     it('should return correct indices for daily [logstash-]YYYY.MM.DD', function () {
@@ -25,23 +36,48 @@ describe('Index Patterns', function () {
       var end = moment(1418849261281);
       var interval = { name: 'days', startOf: 'day', display: 'Daily' };
       var list = intervals.toIndexList('[logstash-]YYYY.MM.DD', interval, start, end);
-      expect(list).to.contain('logstash-2014.12.10');
-      expect(list).to.contain('logstash-2014.12.11');
-      expect(list).to.contain('logstash-2014.12.12');
-      expect(list).to.contain('logstash-2014.12.13');
-      expect(list).to.contain('logstash-2014.12.14');
-      expect(list).to.contain('logstash-2014.12.15');
-      expect(list).to.contain('logstash-2014.12.16');
-      expect(list).to.contain('logstash-2014.12.17');
-    });
-
-    it('should return correct indices for weekly [logstash-]GGGG.WW', function () {
-      var start = moment.utc(1418244231248);
-      var end = moment.utc(1418849261281);
-      var interval = { name: 'weeks', startOf: 'isoWeek', display: 'Weekly' };
-      var list = intervals.toIndexList('[logstash-]GGGG.WW', interval, start, end);
-      expect(list).to.contain('logstash-2014.50');
-      expect(list).to.contain('logstash-2014.51');
+      expect(list).to.eql([
+        {
+          index: 'logstash-2014.12.10',
+          min: moment.utc('2014-12-10T00:00:00').valueOf(),
+          max: moment.utc('2014-12-10T23:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.12.11',
+          min: moment.utc('2014-12-11T00:00:00').valueOf(),
+          max: moment.utc('2014-12-11T23:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.12.12',
+          min: moment.utc('2014-12-12T00:00:00').valueOf(),
+          max: moment.utc('2014-12-12T23:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.12.13',
+          min: moment.utc('2014-12-13T00:00:00').valueOf(),
+          max: moment.utc('2014-12-13T23:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.12.14',
+          min: moment.utc('2014-12-14T00:00:00').valueOf(),
+          max: moment.utc('2014-12-14T23:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.12.15',
+          min: moment.utc('2014-12-15T00:00:00').valueOf(),
+          max: moment.utc('2014-12-15T23:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.12.16',
+          min: moment.utc('2014-12-16T00:00:00').valueOf(),
+          max: moment.utc('2014-12-16T23:59:59.999').valueOf(),
+        },
+        {
+          index: 'logstash-2014.12.17',
+          min: moment.utc('2014-12-17T00:00:00').valueOf(),
+          max: moment.utc('2014-12-17T23:59:59.999').valueOf(),
+        },
+      ]);
     });
 
     it('should return correct indices for monthly [logstash-]YYYY.MM', function () {
@@ -49,9 +85,23 @@ describe('Index Patterns', function () {
       var end = moment.utc('2015-02-01');
       var interval = { name: 'months', startOf: 'month', display: 'Monthly' };
       var list = intervals.toIndexList('[logstash-]YYYY.MM', interval, start, end);
-      expect(list).to.contain('logstash-2014.12');
-      expect(list).to.contain('logstash-2015.01');
-      expect(list).to.contain('logstash-2015.02');
+      expect(list).to.eql([
+        {
+          index: 'logstash-2014.12',
+          min: moment.utc(0).year(2014).month(11).valueOf(),
+          max: moment.utc(0).year(2015).month(0).subtract(1, 'ms').valueOf(),
+        },
+        {
+          index: 'logstash-2015.01',
+          min: moment.utc(0).year(2015).month(0).valueOf(),
+          max: moment.utc(0).year(2015).month(1).subtract(1, 'ms').valueOf(),
+        },
+        {
+          index: 'logstash-2015.02',
+          min: moment.utc(0).year(2015).month(1).valueOf(),
+          max: moment.utc(0).year(2015).month(2).subtract(1, 'ms').valueOf(),
+        },
+      ]);
     });
 
     it('should return correct indices for yearly [logstash-]YYYY', function () {
@@ -59,9 +109,60 @@ describe('Index Patterns', function () {
       var end = moment.utc('2015-02-01');
       var interval = { name: 'years', startOf: 'year', display: 'Yearly' };
       var list = intervals.toIndexList('[logstash-]YYYY', interval, start, end);
-      expect(list).to.contain('logstash-2014');
-      expect(list).to.contain('logstash-2015');
+      expect(list).to.eql([
+        {
+          index: 'logstash-2014',
+          min: moment.utc(0).year(2014).valueOf(),
+          max: moment.utc(0).year(2015).subtract(1, 'ms').valueOf(),
+        },
+        {
+          index: 'logstash-2015',
+          min: moment.utc(0).year(2015).valueOf(),
+          max: moment.utc(0).year(2016).subtract(1, 'ms').valueOf(),
+        },
+      ]);
     });
 
+    context('with sortDirection=asc', function () {
+      it('returns values in ascending order', function () {
+        var start = moment.utc('2014-12-01');
+        var end = moment.utc('2015-02-01');
+        var interval = { name: 'years', startOf: 'year', display: 'Yearly' };
+        var list = intervals.toIndexList('[logstash-]YYYY', interval, start, end, 'asc');
+        expect(list).to.eql([
+          {
+            index: 'logstash-2014',
+            min: moment.utc(0).year(2014).valueOf(),
+            max: moment.utc(0).year(2015).subtract(1, 'ms').valueOf(),
+          },
+          {
+            index: 'logstash-2015',
+            min: moment.utc(0).year(2015).valueOf(),
+            max: moment.utc(0).year(2016).subtract(1, 'ms').valueOf(),
+          },
+        ]);
+      });
+    });
+
+    context('with sortDirection=desc', function () {
+      it('returns values in descending order', function () {
+        var start = moment.utc('2014-12-01');
+        var end = moment.utc('2015-02-01');
+        var interval = { name: 'years', startOf: 'year', display: 'Yearly' };
+        var list = intervals.toIndexList('[logstash-]YYYY', interval, start, end, 'desc');
+        expect(list).to.eql([
+          {
+            index: 'logstash-2015',
+            min: moment.utc(0).year(2015).valueOf(),
+            max: moment.utc(0).year(2016).subtract(1, 'ms').valueOf(),
+          },
+          {
+            index: 'logstash-2014',
+            min: moment.utc(0).year(2014).valueOf(),
+            max: moment.utc(0).year(2015).subtract(1, 'ms').valueOf(),
+          },
+        ]);
+      });
+    });
   });
 });
