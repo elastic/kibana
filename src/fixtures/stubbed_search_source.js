ec404bbad5108844914d81867d946dcea939d485
spalger <email@spalger.com>
2015-12-10 18:14:28 -0700

    resort every segment
    When using segmented fetch we currently assume that documents are stored in index patterns based on time, but this is no long necessary with the use of the field stats API. Since documents can be in any order we need to fetch documents from every index and then sort them client side. Since fetching the documents can take some time, we are using the bounds of the time field in an index to try and identify indices which can't produce hits for a result set, and then not fetch their documents.
    
    It works like this:
     - get the list of indices and the min/max of their timefield
     - fetch the entire sample size from each index until we have enough documents to satisfy the sample.
     - for each remaining index pattern
       - if the min/max of it's timefield overlaps the min/max of the fetched documents also fetch the documents for that index pattern
       - otherwise fetch with size=0
    
    Fixes #5642

--- src/fixtures/stubbed_search_source.js
+++ src/fixtures/stubbed_search_source.js
@@ -2,8 +2,9 @@ define(function (require) {
   var sinon = require('auto-release-sinon');
   var searchResponse = require('fixtures/search_response');
 
-  return function stubSearchSource(Private, $q) {
+  return function stubSearchSource(Private, $q, Promise) {
     var deferedResult = $q.defer();
+    var indexPattern = Private(require('fixtures/stubbed_logstash_index_pattern'));
 
     return {
       sort: sinon.spy(),
@@ -13,7 +14,7 @@ define(function (require) {
       get: function (param) {
         switch (param) {
           case 'index':
-            return Private(require('fixtures/stubbed_logstash_index_pattern'));
+            return indexPattern;
           default:
             throw new Error('Param "' + param + '" is not implemented in the stubbed search source');
         }
@@ -29,7 +30,9 @@ define(function (require) {
         return deferedResult.promise;
       },
       onError: function () { return $q.defer().promise; },
-
+      _flatten() {
+        return Promise.resolve({ index: indexPattern, body: {} });
+      }
     };
 
   };
