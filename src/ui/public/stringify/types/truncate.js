867c9444ff78ac6a55d8dcf8d9ddb38f36221904
Gabriel Moskovicz <gabriel.moskovicz@elastic.co>
2015-12-14 15:30:19 -0300

    last fix for trunctation
    Fixes #5280

--- src/ui/public/stringify/types/truncate.js
+++ src/ui/public/stringify/types/truncate.js
@@ -2,6 +2,7 @@ define(function (require) {
   return function TruncateFormatProvider(Private) {
     var _ = require('lodash');
     var FieldFormat = Private(require('ui/index_patterns/_field_format/FieldFormat'));
+    var omission = '...';
 
     _.class(Truncate).inherits(FieldFormat);
 
@@ -16,7 +17,10 @@ define(function (require) {
     Truncate.prototype._convert = function (val) {
       var length = this.param('fieldLength');
       if (length > 0) {
-        return _.trunc(val, length);
+        return _.trunc(val, {
+          'length': length + omission.length,
+          'omission': omission
+        });
       }
 
       return val;
