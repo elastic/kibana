35001a318b25516422c7c73d8f4a2bede8435984
Jim Unger <BigFunger@gmail.com>
2015-12-17 16:55:38 -0600

    [url shortener] added url type dropdown button

--- src/ui/public/share/directives/share_object_url.js
+++ src/ui/public/share/directives/share_object_url.js
@@ -1,7 +1,9 @@
 const app = require('ui/modules').get('kibana');
 const Clipboard = require('clipboard');
+const removeQueryStringValue = require('../lib/remove_query_string_value');
 
 require('../styles/index.less');
+require('../styles/dropdown_button.less');
 
 app.directive('shareObjectUrl', function (Private, Notifier) {
   const urlShortener = Private(require('../lib/url_shortener'));
@@ -9,7 +11,8 @@ app.directive('shareObjectUrl', function (Private, Notifier) {
   return {
     restrict: 'E',
     scope: {
-      getShareAsEmbed: '&shareAsEmbed'
+      getShareAsEmbed: '&shareAsEmbed',
+      objectId: '='
     },
     template: require('ui/share/views/share_object_url.html'),
     link: function ($scope, $el) {
@@ -50,11 +53,7 @@ app.directive('shareObjectUrl', function (Private, Notifier) {
         $scope.shortGenerated = false;
       }
 
-      $scope.toggleValue = true;
-      $scope.toggle = function () {
-        $scope.toggleValue = !$scope.toggleValue;
-      };
-
+      $scope.stripAppState = false;
       $scope.shareAsEmbed = $scope.getShareAsEmbed();
 
       $scope.generateShortUrl = function () {
@@ -70,6 +69,16 @@ app.directive('shareObjectUrl', function (Private, Notifier) {
       $scope.getUrl = function () {
         return $location.absUrl();
       };
+
+      $scope.$watch('stripAppState', function (enabled) {
+        const currentUrl = $scope.getUrl();
+        if (enabled) {
+          let newUrl = removeQueryStringValue(currentUrl, '_a');
+          updateUrl(newUrl);
+        } else {
+          updateUrl(currentUrl);
+        }
+      });
 
       $scope.$watch('getUrl()', updateUrl);
     }
