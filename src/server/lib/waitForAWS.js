14313e25f35d7851ceee02ccef9b07bdf4c83a2f
Leon Gibat <brendan.gibat@gmail.com>
2015-12-18 13:25:36 -0500

    refresh callback for when credentials need changed

--- src/server/lib/waitForAWS.js
+++ src/server/lib/waitForAWS.js
@@ -3,14 +3,36 @@ var AWS = require('aws-sdk');
 var logger = require('./logger');
 var config = require('../config');
 
+function refreshAWSCredentials() {
+  AWS.config.credentials.get(function(err) {
+    if(err) {
+      logger.error(err);
+    } else {
+      if(AWS.config.credentials.expired) {
+        refreshAWSCredentials();
+      } else if(AWS.config.credentials.expireTime) {
+        var date = new Date();
+        var msWait = date - AWS.config.credentials.expireTime - 5000;
+        if(msWait < 0) msWait = 1000;
+        logger.info("Refreshing AWS credentials in %s", new Date(Number(date) + msWait));
+        setTimeout(refreshAWSCredentials, msWait);
+      }
+    }
+  });
+}
+
 function waitForAWSCredentials() {
   return new Promise(function(fulfill, reject) {
     if(config.kibana.transport == "AWS") {
       AWS.config.getCredentials(function(err) {
-        if(err) {
-          reject(err);
+        if(err) reject(err);
+        else {
+          var msWait = new Date() - AWS.config.credentials.expireTime - 5000;
+          if(msWait < 0) msWait = 1000;
+
+          setTimeout(refreshAWSCredentials, msWait);
+          fulfill(AWS.config.credentials);
         }
-        else fulfill(AWS.config.credentials);
       });
     }
   });
