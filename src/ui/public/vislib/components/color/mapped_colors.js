0b2bed7e82b5db79bad459a2b04c15773b92d555
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-12-21 09:55:12 -0800

    attempting to fix failing tests

--- src/ui/public/vislib/components/color/mapped_colors.js
+++ src/ui/public/vislib/components/color/mapped_colors.js
@@ -1,53 +1,92 @@
-define((require) => (Private, config) => {
-  const _ = require('lodash');
-  const d3 = require('d3');
-  const createColorPalette = Private(require('ui/vislib/components/color/color_palette'));
+define(function (require) {
+  return function ColorMappingClass(Private, config) {
+    const _ = require('lodash');
+    const d3 = require('d3');
+    const createColorPalette = Private(require('ui/vislib/components/color/color_palette'));
+    const standardizeColor = (color) => d3.rgb(color).toString();
 
-  const standardizeColor = (color) => d3.rgb(color).toString();
-  function getConfigColorMapping() {
-    return _.mapValues(config.get('visualization:colorMapping'), standardizeColor);
-  }
-
-  /*
-   * Maintains a lookup table that associates the value (key) with a hex color (value)
-   * across the visualizations.
-   * Provides functions to interact with the lookup table
-   */
-  class MappedColors {
-    constructor() {
-      this.mapping = {};
+    function getConfigColorMapping() {
+      return _.mapValues(config.get('visualization:colorMapping'), standardizeColor);
     }
 
-    get(key) {
-      return getConfigColorMapping()[key] || this.mapping[key];
-    }
-
-    mapKeys(keys) {
-      const configMapping = getConfigColorMapping();
-      const configColors = _.values(configMapping);
-
-      const keysToMap = [];
-      _.each(keys, (key) => {
-        // If this key is mapped in the config, it's unnecessary to have it mapped here
-        if (configMapping[key]) delete this.mapping[key];
-
-        // If this key is mapped to a color used by the config color mapping, we need to remap it
-        if (_.contains(configColors, this.mapping[key])) keysToMap.push(key);
-
-        // If this key isn't mapped, we need to map it
-        if (this.get(key) == null) keysToMap.push(key);
+    function areColorConflicts(newColors, configColors) {
+      return newColors.every(function (color) {
+        return _.includes(configColors, color);
       });
-
-      const colorPalette = createColorPalette(keysToMap.length);
-      const mappedKeys = keysToMap.slice(0);
-      _.merge(this.mapping, _.zipObject(keysToMap, colorPalette));
-
-      // Need to remove any keys whose generated color matches the configColor
-      keysToMap.length = 0;
-
-      if (mappedKeys.length) this.mapKeys(mappedKeys);
     }
-  }
 
-  return new MappedColors();
+    function generateRandomColor(num) {
+      var colors = createColorPalette(num); // Total number of colors generated
+      return colors[Math.floor(Math.random() * colors.length)];
+    }
+
+    function replaceColors(newColors, configColors, num) {
+      num = num || 56; // Number of colors to use for color generation
+
+      function mapColor(color) {
+        if (_.includes(configColors, color)) {
+          var randomColor = generateRandomColor(num);
+
+          if (_.includes(configColors, randomColor)) {
+            return mapColor(randomColor);
+          }
+          return randomColor;
+        }
+
+        return color;
+      }
+
+      return newColors.map(mapColor);
+    }
+
+    function validateColors(newColors, configColors, num) {
+      var conflicts = areColorConflicts(newColors, configColors);
+
+      if (conflicts) {
+        return replaceColors(newColors, configColors, num);
+      }
+
+      return newColors;
+    }
+
+    /*
+     * Maintains a lookup table that associates the value (key) with a hex color (value)
+     * across the visualizations.
+     * Provides functions to interact with the lookup table
+     */
+    class MappedColors {
+      constructor() {
+        this.mapping = {};
+      }
+
+      get(key) {
+        return getConfigColorMapping()[key] || this.mapping[key];
+      }
+
+      mapKeys(keys) {
+        const configMapping = getConfigColorMapping();
+        const configColors = _.values(configMapping);
+
+        const keysToMap = [];
+        _.each(keys, (key) => {
+          // If this key is mapped in the config, it's unnecessary to have it mapped here
+          if (configMapping[key]) delete this.mapping[key];
+
+          // If this key is mapped to a color used by the config color mapping, we need to remap it
+          if (_.contains(configColors, this.mapping[key])) keysToMap.push(key);
+
+          // If this key isn't mapped, we need to map it
+          if (this.get(key) == null) keysToMap.push(key);
+        });
+
+        const allKeys = _.keys(this.mapping);
+        const colorPalette = createColorPalette(allKeys.length + keysToMap.length);
+        const newColors = colorPalette.slice(allKeys.length);
+        const validatedColors = validateColors(newColors, configColors);
+        _.merge(this.mapping, _.zipObject(keysToMap, validatedColors));
+      }
+    }
+
+    return new MappedColors();
+  };
 });
