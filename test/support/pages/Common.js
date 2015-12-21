1e31e1d2369f0fb7864012cc7aa889413c4a9bcd
LeeDr <lee.drengenberg@elastic.co>
2015-12-21 15:35:26 -0600

    Bump Selenium version from 2.47.1 to 2.48.2 to work with latest Firefox 43. Add more debug logging around get url.
    Fixes #5750

--- test/support/pages/Common.js
+++ test/support/pages/Common.js
@@ -31,12 +31,16 @@ define(function (require) {
           self.debug('navigate to: ' + url);
           return self.remote.get(url)
           .then(function () {
+            self.debug('returned from get, calling refresh');
             return self.remote.refresh();
           })
           .then(function () {
+            self.debug('check testStatusPage');
             if (testStatusPage !== false) {
+              self.debug('self.checkForKibanaApp()');
               return self.checkForKibanaApp()
               .then(function (kibanaLoaded) {
+                self.debug('kibanaLoaded = ' + kibanaLoaded);
                 if (!kibanaLoaded) {
                   var msg = 'Kibana is not loaded, retrying';
                   self.debug(msg);
