c591dd2118d3f56dc2902b31373aa55a5e4980d9
Rashid Khan <github.fliplap@spamgourmet.com>
2016-01-04 10:31:49 -0700

    Revert "No single letter variables"
    This reverts commit d87f249865c9fbbdde7e128d944febfd9b52ca4d.

--- src/ui/public/vislib/components/color/color_palette.js
+++ src/ui/public/vislib/components/color/color_palette.js
@@ -3,12 +3,12 @@ define(function (require) {
     var d3 = require('d3');
     var _ = require('lodash');
 
-   //var seedColors = Private(require('ui/vislib/components/color/seed_colors'));
-    var seedColors = ['#666'];
+    var seedColors = Private(require('ui/vislib/components/color/seed_colors'));
+
     // Shamelessly borrowed from flot.colorhelpers
-    function scale(color, toVary, variation) {
-      for (var i = 0; i < toVary.length; ++i) {
-        color[toVary.charAt(i)] *= variation;
+    function scale(color, c, f) {
+      for (var i = 0; i < c.length; ++i) {
+        color[c.charAt(i)] *= f;
       }
       return normalize(color);
     };
