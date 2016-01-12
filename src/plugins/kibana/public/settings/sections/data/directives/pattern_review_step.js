7dbef2fb9a25a3122ceab828a1096766d4cc9636
Matthew Bargar <mbargar@gmail.com>
2016-01-12 13:04:54 -0500

    Starting work on pattern review page

--- src/plugins/kibana/public/settings/sections/data/directives/pattern_review_step.js
+++ src/plugins/kibana/public/settings/sections/data/directives/pattern_review_step.js
@@ -1,5 +1,17 @@
 var modules = require('ui/modules');
 var template = require('plugins/kibana/settings/sections/data/directives/pattern_review_step.html');
+var _ = require('lodash');
+
+var testData = {
+  message: 'src=1.1.1.1 evil=1',
+  src: '1.1.1.1',
+  evil: '1',
+  coordinates: {
+    lat: 37.3894,
+    lon: 122.0819
+  },
+  '@timestamp': '2015-11-24T00:00:00.000Z'
+};
 
 modules.get('apps/settings')
   .directive('patternReviewStep', function () {
@@ -8,6 +20,18 @@ modules.get('apps/settings')
       scope: {
         docs: '=',
         save: '&onSave'
+      },
+      controller: function ($scope, Private) {
+        $scope.docs = testData;
+        $scope.columns = [
+          {title: 'Field'},
+          {title: 'Type'},
+          {title: 'Example', sortable: false}
+        ];
+
+        $scope.rows = _.map($scope.docs, (value, key) => {
+          return [key, 'type', value];
+        });
       }
     };
   });
