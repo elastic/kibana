a47fc05ef79b334fbfe5d0aa30af5ab3bf4ee745
Matthew Bargar <mbargar@gmail.com>
2016-01-13 12:03:30 -0500

    Detect geo_points and dates by looking at the pipeline

--- src/plugins/kibana/public/settings/sections/data/directives/pattern_review_step.js
+++ src/plugins/kibana/public/settings/sections/data/directives/pattern_review_step.js
@@ -4,15 +4,54 @@ var _ = require('lodash');
 var editFieldTypeHTML = require('plugins/kibana/settings/sections/data/partials/_edit_field_type.html');
 
 var testData = {
-  message: 'src=1.1.1.1 evil=1',
-  src: '1.1.1.1',
-  evil: '1',
-  coordinates: {
+  message: '11/24/2015 ip=1.1.1.1 bytes=1234',
+  clientip: '1.1.1.1',
+  bytes: 1234,
+  geoip: {
     lat: 37.3894,
     lon: 122.0819
   },
-  '@timestamp': '2015-11-24T00:00:00.000Z'
+  location: {
+    lat: 37.3894,
+    lon: 122.0819
+  },
+  '@timestamp': '2015-11-24T00:00:00.000Z',
+  otherdate: '2015-11-24T00:00:00.000Z',
+  codes: [1, 2, 3, 4]
 };
+
+var testPipeline = [
+  {
+    grok: {
+      match_field: 'message',
+      match_pattern: 'foo'
+    }
+  },
+  {
+    geoip: {
+      source_field: 'ip'
+    }
+  },
+  {
+    geoip: {
+      source_field: 'ip',
+      target_field: 'location'
+    }
+  },
+  {
+    date: {
+      match_field: 'initialDate',
+      match_formats: ['dd/MM/yyyy hh:mm:ss']
+    }
+  },
+  {
+    date: {
+      match_field: 'initialDate',
+      match_formats: ['dd/MM/yyyy hh:mm:ss'],
+      target_field: 'otherdate'
+    }
+  }
+];
 
 modules.get('apps/settings')
   .directive('patternReviewStep', function () {
@@ -25,16 +64,31 @@ modules.get('apps/settings')
       },
       controller: function ($scope, Private) {
         $scope.sampleDocs = testData;
+        $scope.pipeline = testPipeline;
+
         if (_.isUndefined($scope.indexPattern)) {
           $scope.indexPattern = {};
         }
+
+        const knownFieldTypes = {};
+        $scope.pipeline.forEach(function (processor) {
+          if (processor.geoip) {
+            let field = processor.geoip.target_field || 'geoip';
+            knownFieldTypes[field] = 'geo_point';
+          }
+          if (processor.date) {
+            let field = processor.date.target_field || '@timestamp';
+            knownFieldTypes[field] = 'date';
+          }
+        });
 
         _.defaults($scope.indexPattern, {
           id: 'filebeat-*',
           title: 'filebeat-*',
           timeFieldName: '@timestamp',
           fields: _.map($scope.sampleDocs, (value, key) => {
-            return {name: key, type: typeof value};
+            let type = knownFieldTypes[key] || typeof value;
+            return {name: key, type: type};
           })
         });
 
@@ -51,13 +105,14 @@ modules.get('apps/settings')
         ];
 
         $scope.rows = _.map($scope.indexPattern.fields, (field) => {
+          const sampleValue = $scope.sampleDocs[field.name];
           return [
             field.name,
             {
               markup: editFieldTypeHTML,
               scope: _.assign($scope.$new(), {field: field})
             },
-            $scope.sampleDocs[field.name]
+            typeof sampleValue === 'object' ? JSON.stringify(sampleValue) : sampleValue
           ];
         });
       }
