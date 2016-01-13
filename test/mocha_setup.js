7b15ee05d1df291d316c22fff16f858f6eb0a28a
spalger <spalger@users.noreply.github.com>
2016-01-13 16:50:55 -0700

    [mochaSetup] added notes to explain the purpose for the workarounds in place

--- test/mocha_setup.js
+++ test/mocha_setup.js
@@ -3,6 +3,16 @@ var autoRelease = require('auto-release-sinon');
 
 require('babel/register')(require('../src/optimize/babelOptions').node);
 
+// hook into the global afterEach variable to allow autoReleaseSinon to register
+// an afterEach handler before mocha has exposed itself to the test files.
+//
+// This works by telling autoReleaseSinon to use a fake "afterEach" function.
+// Rather than actually record an afterEach handler the function tracks all of
+// the calls it received and queues them up in queuedAfterEachArgs.
+//
+// The global "afterEach" variable is also tracked, and once it is assigned by mocha
+// the variable global is reconfigured to point directly to the new value (from mocha)
+// and all of the queued invocations are executed.
 var queuedAfterEachArgs = [];
 Object.defineProperty(global, 'afterEach', {
   configurable: true,
