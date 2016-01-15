4b0ac79c6fade83325b1bde6916310780e2e6b9b
Court Ewing <court@epixa.com>
2016-01-15 11:48:46 -0500

    Inline documentation for kbn_server test utility

--- test/utils/kbn_server.js
+++ test/utils/kbn_server.js
@@ -31,16 +31,36 @@ const SERVER_DEFAULTS = {
   }
 };
 
+/**
+ * Creates an instance of KbnServer with default configuration
+ * tailored for unit tests
+ *
+ * @param {object} params Any config overrides for this instance
+ */
 export function createServer(params = {}) {
   params = defaultsDeep({}, params, SERVER_DEFAULTS);
   return new KbnServer(params);
 };
 
+/**
+ * Creates request configuration with a basic auth header
+ */
 export function authOptions() {
   const authHeader = basicAuthHeader('user', 'notsecure');
   return set({}, 'headers.Authorization', authHeader);
 };
 
+/**
+ * Makes a request with test headers via hapi server inject()
+ *
+ * The given options are decorated with default testing options, so it's
+ * recommended to use this function instead of using inject() directly whenever
+ * possible throughout the tests.
+ *
+ * @param {KbnServer} kbnServer
+ * @param {object}    options Any additional options or overrides for inject()
+ * @param {Function}  fn The callback to pass as the second arg to inject()
+ */
 export function makeRequest(kbnServer, options, fn) {
   options = defaultsDeep({}, authOptions(), options);
   return kbnServer.server.inject(options, fn);
