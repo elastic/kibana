ff8f18dc6212e9773bf56d73f57997dbe252e24d
Shaunak Kashyap <ycombinator@gmail.com>
2016-01-15 12:59:14 -0800

    Removing unnecessary filter
    Fixes #5920

--- src/cli/plugin/plugin_lister.js
+++ src/cli/plugin/plugin_lister.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 export function list(settings, logger) {
   const files = fs.readdirSync(settings.pluginDir);
   files
-  .filter(function (file) {
-    return file[0] !== '.';
-  })
   .forEach(function (pluginFile) {
     logger.log(pluginFile);
   });
