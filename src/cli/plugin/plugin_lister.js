9a0be4e543375f03f97c0605dc980f959a9d5a7a
Shaunak Kashyap <ycombinator@gmail.com>
2016-01-15 11:28:12 -0800

    Using synch flavor of readdir to bubble up exception
    Fixes #5920

--- src/cli/plugin/plugin_lister.js
+++ src/cli/plugin/plugin_lister.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
 
 export function list(settings, logger) {
-  fs.readdir(settings.pluginDir, function (err, files) {
-
-    files
-    .filter(function (file) {
-      return file[0] !== '.';
-    })
-    .forEach(function (pluginFile) {
-      logger.log(pluginFile);
-    });
+  const files = fs.readdirSync(settings.pluginDir);
+  files
+  .filter(function (file) {
+    return file[0] !== '.';
+  })
+  .forEach(function (pluginFile) {
+    logger.log(pluginFile);
   });
 }
