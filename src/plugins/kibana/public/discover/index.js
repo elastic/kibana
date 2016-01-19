c55f3c43c78a5f220d20e305e60af1104cba52a1
Joe Fleming <joe.fleming@gmail.com>
2016-01-19 13:26:59 -0700

    consume navbar directive
    used anywhere the navbar is being used currently

--- src/plugins/kibana/public/discover/index.js
+++ src/plugins/kibana/public/discover/index.js
@@ -1,6 +1,7 @@
 define(function (require, module, exports) {
   require('plugins/kibana/discover/saved_searches/saved_searches');
   require('plugins/kibana/discover/directives/timechart');
+  require('ui/navbar');
   require('ui/collapsible_sidebar');
   require('plugins/kibana/discover/components/field_chooser/field_chooser');
   require('plugins/kibana/discover/controllers/discover');
@@ -9,6 +10,6 @@ define(function (require, module, exports) {
   // preload
   require('ui/doc_table/components/table_row');
 
-  require('ui/saved_objects/saved_object_registry').register(require('plugins/kibana/discover/saved_searches/saved_search_register'));
-
+  var savedObjectRegistry = require('ui/saved_objects/saved_object_registry');
+  savedObjectRegistry.register(require('plugins/kibana/discover/saved_searches/saved_search_register'));
 });
