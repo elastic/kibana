86ab222d4d096b583436f7b8054a87abae5bd0cf
LeeDr <lee.drengenberg@elastic.co>
2016-01-19 12:20:09 -0600

    Fix screenshots Data Table and Tile Map.
    Fixes #5932

--- test/functional/apps/visualize/_vertical_bar_chart.js
+++ test/functional/apps/visualize/_vertical_bar_chart.js
@@ -86,15 +86,11 @@ define(function (require) {
       });
 
       bdd.describe('vertical bar chart', function indexPatternCreation() {
+        var testSubName = 'VerticalBarChart';
+        var vizName1 = 'Visualization ' + testSubName;
 
 
-        bdd.it('should save and load, take screenshot', function pageHeader() {
-
-          var testSubName = 'VerticalBarChart';
-          common.debug('Start of test' + testSubName + 'Visualization');
-          var vizName1 = 'Visualization ' + testSubName;
-          // var remote = this.remote;
-
+        bdd.it('should save and load', function pageHeader() {
           return visualizePage.saveVisualization(vizName1)
           .then(function (message) {
             common.debug('Saved viz message = ' + message);
@@ -112,15 +108,11 @@ define(function (require) {
           .then(function waitForVisualization() {
             return visualizePage.waitForVisualization();
           })
-          .then(function takeScreenshot() {
-            common.debug('Take screenshot');
-            common.saveScreenshot('./screenshot-' + testSubName + '.png');
-          })
           .catch(common.handleError(this));
         });
 
 
-        bdd.it('should show correct chart', function pageHeader() {
+        bdd.it('should show correct chart, take screenshot', function pageHeader() {
 
           var expectedChartValues = [37, 202, 740, 1437, 1371, 751, 188, 31, 42, 202, 683,
             1361, 1415, 707, 177, 27, 32, 175, 707, 1408, 1355, 726, 201, 29
@@ -137,6 +129,10 @@ define(function (require) {
             common.debug('data=' + data);
             common.debug('data.length=' + data.length);
             expect(data).to.eql(expectedChartValues);
+          })
+          .then(function takeScreenshot() {
+            common.debug('Take screenshot');
+            common.saveScreenshot('./screenshot-' + testSubName + '.png');
           })
           .catch(common.handleError(this));
         });
