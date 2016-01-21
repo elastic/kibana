ee78fde20fb1a4b1099b0a8e513a64203c5f3499
spalger <spalger@users.noreply.github.com>
2016-01-20 18:44:26 -0700

    [https] automatically redirect http traffic to https
    When we moved the dev server to use https it became obvious that the Kibana server fails very quick when it receives http traffic. In most cases this is a good thing, but automatically redirecting to https is a far more user-friendly experience.
    
    To do this we tell Hapi to listen to an alternate Server when we are using https. That server is provided by the httpolygot library, which knows how to handle both http and https requests. Then, when the server sees these requests it simply redirects when necessary.

--- src/server/http/setup_connection.js
+++ src/server/http/setup_connection.js
@@ -1,13 +1,19 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
+import { format as formatUrl } from 'url';
+import httpolyglot from 'httpolyglot';
+
+import tlsCiphers from './tls_ciphers';
 
 export default function (kbnServer, server, config) {
   // this mixin is used outside of the kbn server, so it MUST work without a full kbnServer object.
   kbnServer = null;
 
-  // Create a new connection
-  var connectionOptions = {
-    host: config.get('server.host'),
-    port: config.get('server.port'),
+  const host = config.get('server.host');
+  const port = config.get('server.port');
+
+  const connectionOptions = {
+    host,
+    port,
     state: {
       strictHeader: false
     },
@@ -19,42 +25,42 @@ export default function (kbnServer, server, config) {
     }
   };
 
-  // enable tls if ssl key and cert are defined
-  if (config.get('server.ssl.key') && config.get('server.ssl.cert')) {
-    connectionOptions.tls = {
-      key: fs.readFileSync(config.get('server.ssl.key')),
-      cert: fs.readFileSync(config.get('server.ssl.cert')),
-      // The default ciphers in node 0.12.x include insecure ciphers, so until
-      // we enforce a more recent version of node, we craft our own list
-      // @see https://github.com/nodejs/node/blob/master/src/node_constants.h#L8-L28
-      ciphers: [
-        'ECDHE-RSA-AES128-GCM-SHA256',
-        'ECDHE-ECDSA-AES128-GCM-SHA256',
-        'ECDHE-RSA-AES256-GCM-SHA384',
-        'ECDHE-ECDSA-AES256-GCM-SHA384',
-        'DHE-RSA-AES128-GCM-SHA256',
-        'ECDHE-RSA-AES128-SHA256',
-        'DHE-RSA-AES128-SHA256',
-        'ECDHE-RSA-AES256-SHA384',
-        'DHE-RSA-AES256-SHA384',
-        'ECDHE-RSA-AES256-SHA256',
-        'DHE-RSA-AES256-SHA256',
-        'HIGH',
-        '!aNULL',
-        '!eNULL',
-        '!EXPORT',
-        '!DES',
-        '!RC4',
-        '!MD5',
-        '!PSK',
-        '!SRP',
-        '!CAMELLIA'
-      ].join(':'),
-      // We use the server's cipher order rather than the client's to prevent
-      // the BEAST attack
-      honorCipherOrder: true
-    };
+  // enable tlsOpts if ssl key and cert are defined
+  const useSsl = config.get('server.ssl.key') && config.get('server.ssl.cert');
+
+  // not using https? well that's easy!
+  if (!useSsl) {
+    server.connection(connectionOptions);
+    return;
   }
 
-  server.connection(connectionOptions);
+  // for https servers we have some extra settings
+  const tls = {
+    key: readFileSync(config.get('server.ssl.key')),
+    cert: readFileSync(config.get('server.ssl.cert')),
+
+    ciphers: tlsCiphers,
+    // We use the server's cipher order rather than the client's to prevent the BEAST attack
+    honorCipherOrder: true
+  };
+
+  server.connection({
+    ...connectionOptions,
+    tls: tls,
+    listener: httpolyglot.createServer(tls)
+  });
+
+  server.ext('onRequest', function (req, reply) {
+    if (req.raw.req.socket.encrypted) {
+      reply.continue();
+    } else {
+      reply.redirect(formatUrl({
+        port,
+        protocol: 'https',
+        hostname: host,
+        pathname: req.url.pathname,
+        search: req.url.search,
+      }));
+    }
+  });
 }
