3fa9352309a695b4b0dc1e33be2d0417906cab78
Rashid Khan <github.fliplap@spamgourmet.com>
2016-01-22 13:19:20 -0700

    Remove some duplication, clean up linting

--- series_functions/movingaverage.js
+++ series_functions/movingaverage.js
@@ -15,7 +15,7 @@ module.exports = new Chainable('movingaverage', {
     {
       name: 'position',
       types: ['string', 'null'],
-      help: "Position of the averaged points relative to the result's time.  Options are left, right, and center (default)."
+      help: 'Position of the averaged points relative to the result time.  Options are left, right, and center (default).'
     }
   ],
   aliases: ['mvavg'],
@@ -23,73 +23,44 @@ module.exports = new Chainable('movingaverage', {
   fn: function movingaverageFn(args) {
     return alter(args, function (eachSeries, _window, _position) {
 
-      _position = _position || "center";
-      var validPositions = ['left', 'right', 'center']
-      if (!_.contains(['left', 'right', 'center'], _position)) {
-        throw new Error('Valid positions are: ' + validPositions.join(', '));
+      _position = _position || 'center';
+      var validPositions = ['left', 'right', 'center'];
+      if (!_.contains(validPositions, _position)) throw new Error('Valid positions are: ' + validPositions.join(', '));
+
+      var pairs = eachSeries.data;
+      var pairsLen = pairs.length;
+      eachSeries.label = eachSeries.label + ' mvavg=' + _window;
+
+      function toPoint(point, pairSlice) {
+        var average = _.chain(pairSlice)
+        .map(1).reduce(function (memo, num) {
+          return (memo + num);
+        }).value() / _window;
+
+        return [point[0], average];
       }
 
-      if(_position === "center") {
-        
-        var windowLeft = Math.floor( _window / 2);
+      if (_position === 'center') {
+        var windowLeft = Math.floor(_window / 2);
         var windowRight = _window - windowLeft;
-
-        var pairs = eachSeries.data;
-      
-        var pairsLen = pairs.length;
-      
-        eachSeries.label = eachSeries.label + ' mvavg=' + _window;
         eachSeries.data = _.map(pairs, function (point, i) {
-          if (i < windowLeft || i >= pairsLen - windowRight ) { return [point[0], null]; }
-
-          var average = _.chain(pairs.slice(i - windowLeft, i + windowRight ))
-          .map(function (point) {
-            return point[1];
-          }).reduce(function (memo, num) {
-            return (memo + num);
-          }).value() / _window;
-
-          return [point[0], average];
+          if (i < windowLeft || i >= pairsLen - windowRight) return [point[0], null];
+          return toPoint(point, pairs.slice(i - windowLeft, i + windowRight));
         });
-      
-      } else if(_position === "left"){
-        
-        var pairs = eachSeries.data;
-        eachSeries.label = eachSeries.label + ' mvavg=' + _window;
+      } else if (_position === 'left') {
         eachSeries.data = _.map(pairs, function (point, i) {
-          if (i < _window) { return [point[0], null]; }
-
-          var average = _.chain(pairs.slice(i - _window, i))
-          .map(function (point) {
-            return point[1];
-          }).reduce(function (memo, num) {
-            return (memo + num);
-          }).value() / _window;
-
-          return [point[0], average];
+          if (i < _window) return [point[0], null];
+          return toPoint(point, pairs.slice(i - _window, i));
         });
-        
-      } else if(_position === "right"){
-        
-        var pairs = eachSeries.data;
-        var pairsLen = pairs.length;
-        
-        eachSeries.label = eachSeries.label + ' mvavg=' + _window;
+
+      } else if (_position === 'right') {
         eachSeries.data = _.map(pairs, function (point, i) {
-         if (i >= pairsLen - _window) { return [point[0], null]; }
-
-         var average = _.chain(pairs.slice(i , i + _window))
-         .map(function (point) {
-           return point[1];
-         }).reduce(function (memo, num) {
-           return (memo + num);
-         }).value() / _window;
-
-         return [point[0], average];
+          if (i >= pairsLen - _window) return [point[0], null];
+          return toPoint(point, pairs.slice(i , i + _window));
         });
 
       }
-      
+
       return eachSeries;
     });
   }
