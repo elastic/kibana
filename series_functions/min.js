fa9d44ea0f9c7f587c4c8cac3302e2e7a339e4fb
Rashid Khan <github.fliplap@spamgourmet.com>
2016-01-22 13:58:48 -0700

    Add min function to go with max

---
+++ series_functions/min.js
@@ -0,0 +1,24 @@
+var reduce = require('../lib/reduce.js');
+
+var Chainable = require('../lib/classes/chainable');
+module.exports = new Chainable('max', {
+  args: [
+    {
+      name: 'inputSeries',
+      types: ['seriesList']
+    },
+    {
+      name: 'value',
+      types: ['seriesList', 'number'],
+      help: 'Number, series to min with the input series. If passing a seriesList it must contain exactly 1 series.'
+
+    }
+
+  ],
+  help: 'Minimum values of one or more series in a seriesList to each position, in each series, of the input seriesList',
+  fn: function minFn(args) {
+    return reduce(args, function (a, b) {
+      return Math.min(a, b);
+    });
+  }
+});
