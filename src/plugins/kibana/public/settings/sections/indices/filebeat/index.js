20b1e6e24a922e7b187db5bcc3125b6492d7b63e
Matthew Bargar <mbargar@gmail.com>
2016-01-22 16:53:50 -0500

    Delete changes to current step when navigating backwards, warning the user first

--- src/plugins/kibana/public/settings/sections/indices/filebeat/index.js
+++ src/plugins/kibana/public/settings/sections/indices/filebeat/index.js
@@ -13,25 +13,49 @@ routes.when('/settings/indices/filebeat', {
 
 // wrapper directive, which sets up the breadcrumb for all filebeat steps
 modules.get('apps/settings')
-  .controller('kbnSettingsIndicesFilebeat', function ($scope, AppState) {
-    var $state = $scope.state = new AppState();
-    var totalSteps = 4;
-    $scope.stepResults = {};
+.controller('kbnSettingsIndicesFilebeat', function ($scope, AppState, safeConfirm) {
+  var $state = $scope.state = new AppState();
+  var totalSteps = 4;
+  $scope.stepResults = {};
 
-    $scope.setCurrentStep = function (step) {
-      $state.currentStep = step;
-      $state.save();
-    };
-    $scope.setCurrentStep(0);
+  $scope.setCurrentStep = function (step) {
+    $state.currentStep = step;
+    $state.save();
+  };
+  $scope.setCurrentStep(0);
 
-    $scope.nextStep = function () {
-      if ($state.currentStep + 1 < totalSteps) {
-        $scope.setCurrentStep($state.currentStep + 1);
-      }
-    };
-    $scope.prevStep = function () {
-      if ($state.currentStep > 0) {
-        $scope.setCurrentStep($state.currentStep - 1);
-      }
-    };
+  $scope.nextStep = function () {
+    if ($state.currentStep + 1 < totalSteps) {
+      $scope.setCurrentStep($state.currentStep + 1);
+    }
+  };
+  $scope.prevStep = function () {
+    if ($state.currentStep > 0) {
+      $scope.setCurrentStep($state.currentStep - 1);
+    }
+  };
+
+  $scope.$watch('state.currentStep', function (newValue, oldValue) {
+    if (newValue < oldValue) {
+      return safeConfirm('Going back will reset any changes you\'ve made to this step, do you want to continue?')
+      .then(
+        function () {
+          if ($state.currentStep < 1) {
+            delete $scope.stepResults.pipeline;
+          }
+          if ($state.currentStep < 2) {
+            delete $scope.stepResults.indexPattern;
+          }
+          $scope.currentStep = newValue;
+        },
+        function () {
+          $state.currentStep = oldValue;
+          $state.save();
+        }
+      );
+    }
+    else {
+      $scope.currentStep = newValue;
+    }
   });
+});
