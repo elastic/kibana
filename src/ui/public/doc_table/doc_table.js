5a97b6b668876ed60032472ef0f644aee7e0f2fa
spalger <spalger@users.noreply.github.com>
2016-01-22 18:08:52 -0700

    [courier/searchSource] auto add source filter for index pattern

--- src/ui/public/doc_table/doc_table.js
+++ src/ui/public/doc_table/doc_table.js
@@ -80,10 +80,6 @@ uiModules.get('kibana')
 
         $scope.searchSource.size(config.get('discover:sampleSize'));
         $scope.searchSource.sort(getSort($scope.sorting, $scope.indexPattern));
-        const sourceFiltering = $scope.indexPattern.getSourceFiltering($scope.columns);
-        if (sourceFiltering) {
-          $scope.searchSource.source(sourceFiltering);
-        }
 
         // Set the watcher after initialization
         $scope.$watchCollection('sorting', function (newSort, oldSort) {
