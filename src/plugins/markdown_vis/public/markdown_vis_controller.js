5d4fd2586ce7dcd0f8163bcf3f5b4fda351839d3
Court Ewing <court@epixa.com>
2016-01-26 11:08:21 -0500

    Use const/let in markdown_vis plugin

--- src/plugins/markdown_vis/public/markdown_vis_controller.js
+++ src/plugins/markdown_vis/public/markdown_vis_controller.js
@@ -1,11 +1,11 @@
 define(function (require) {
-  var marked = require('marked');
+  const marked = require('marked');
   marked.setOptions({
     gfm: true, // Github-flavored markdown
     sanitize: true // Sanitize HTML tags
   });
 
-  var module = require('ui/modules').get('kibana/markdown_vis', ['kibana']);
+  const module = require('ui/modules').get('kibana/markdown_vis', ['kibana']);
   module.controller('KbnMarkdownVisController', function ($scope, $sce) {
     $scope.$watch('vis.params.markdown', function (html) {
       if (!html) return;
