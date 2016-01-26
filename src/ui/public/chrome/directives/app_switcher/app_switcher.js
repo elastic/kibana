6892bfa6502bb5916ff85def69c6795837a89295
spalger <spalger@users.noreply.github.com>
2016-01-25 20:23:26 -0700

    [appSwitcher] allow hash-based navigation by default, disable on status page

--- src/ui/public/chrome/directives/app_switcher/app_switcher.js
+++ src/ui/public/chrome/directives/app_switcher/app_switcher.js
@@ -6,14 +6,45 @@ var DomLocationProvider = require('ui/domLocation');
 
 require('ui/modules')
 .get('kibana')
+.provider('appSwitcherEnsureNavigation', function () {
+  var forceNavigation = false;
+
+  this.forceNavigation = function (val) {
+    forceNavigation = !!val;
+  };
+
+  this.$get = ['Private', function (Private) {
+    var domLocation = Private(DomLocationProvider);
+
+    return function (event) {
+      if (!forceNavigation || event.isDefaultPrevented() || event.altKey || event.metaKey || event.ctrlKey) {
+        return;
+      }
+
+      var toParsed = parse(event.delegateTarget.href);
+      var fromParsed = parse(domLocation.href);
+      var sameProto = toParsed.protocol === fromParsed.protocol;
+      var sameHost = toParsed.host === fromParsed.host;
+      var samePath = toParsed.path === fromParsed.path;
+
+      if (sameProto && sameHost && samePath) {
+        toParsed.hash && domLocation.reload();
+
+        // event.preventDefault() keeps the browser from seeing the new url as an update
+        // and even setting window.location does not mimic that behavior, so instead
+        // we use stopPropagation() to prevent angular from seeing the click and
+        // starting a digest cycle/attempting to handle it in the router.
+        event.stopPropagation();
+      }
+    };
+  }];
+})
 .directive('appSwitcher', function () {
   return {
     restrict: 'E',
     template: require('./app_switcher.html'),
     controllerAs: 'switcher',
-    controller: function ($scope, Private) {
-      var domLocation = Private(DomLocationProvider);
-
+    controller: function ($scope, appSwitcherEnsureNavigation) {
       // since we render this in an isolate scope we can't "require: ^chrome", but
       // rather than remove all helpfull checks we can just check here.
       if (!$scope.chrome || !$scope.chrome.getNavLinks) {
@@ -24,28 +55,7 @@ require('ui/modules')
 
       // links don't cause full-navigation events in certain scenarios
       // so we force them when needed
-      this.ensureNavigation = function (event, app) {
-        if (event.isDefaultPrevented() || event.altKey || event.metaKey || event.ctrlKey) {
-          return;
-        }
-
-        var toParsed = parse(event.delegateTarget.href);
-        var fromParsed = parse(domLocation.href);
-        var sameProto = toParsed.protocol === fromParsed.protocol;
-        var sameHost = toParsed.host === fromParsed.host;
-        var samePath = toParsed.path === fromParsed.path;
-
-        if (sameProto && sameHost && samePath) {
-          toParsed.hash && domLocation.reload();
-
-          // event.preventDefault() keeps the browser from seeing the new url as an update
-          // and even setting window.location does not mimic that behavior, so instead
-          // we use stopPropagation() to prevent angular from seeing the click and
-          // starting a digest cycle/attempting to handle it in the router.
-          event.stopPropagation();
-        }
-      };
-
+      this.ensureNavigation = appSwitcherEnsureNavigation;
     }
   };
 });
