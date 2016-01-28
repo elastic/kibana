98b15dbefa7cc1f79181212062db5c713e1de057
LeeDr <lee.drengenberg@elastic.co>
2016-01-28 16:17:23 -0600

    backport a4088bba

--- test/functional/apps/discover/index.js
+++ test/functional/apps/discover/index.js
@@ -2,7 +2,7 @@ define(function (require) {
   var bdd = require('intern!bdd');
   var config = require('intern').config;
   var url = require('intern/dojo/node!url');
-  var ScenarioManager = require('intern/dojo/node!../../../fixtures/scenarioManager');
+  var ScenarioManager = require('intern/dojo/node!../../../fixtures/scenario_manager');
   var discoverTest = require('./_discover');
   var fieldData = require('./_field_data');
   var sharedLinks = require('./_shared_links');
