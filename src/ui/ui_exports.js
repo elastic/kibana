7fbe6ffebb1b55e3ff901431f75754e0a8b44554
spalger <spalger@users.noreply.github.com>
2016-02-04 16:34:53 -0700

    [plugins] allow breaking nuts

--- src/ui/ui_exports.js
+++ src/ui/ui_exports.js
@@ -63,6 +63,7 @@ class UiExports {
       case 'chromeNavControls':
       case 'navbarExtensions':
       case 'settingsSections':
+      case 'sledgehammers':
         return (plugin, spec) => {
           this.aliases[type] = _.union(this.aliases[type] || [], spec);
         };
