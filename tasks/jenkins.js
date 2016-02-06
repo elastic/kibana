a4acbaf3e7606f5452996e29d2a59a968097e7d3
spalger <spalger@users.noreply.github.com>
2016-02-05 17:31:32 -0700

    use jdk8 on jenkins
    (cherry picked from commit 6ee704772d40d567e37dca711952fcc5fa943b43)
    
    Fixes #6594

--- tasks/jenkins.js
+++ tasks/jenkins.js
@@ -1,8 +1,9 @@
 module.exports = function (grunt) {
   let { compact } = require('lodash');
-
   grunt.registerTask('jenkins', 'Jenkins build script', function () {
+    process.env.JAVA_HOME = '/usr/lib/jvm/jdk8';
     grunt.option('os-packages', true);
+
     grunt.task.run(compact([
       'test',
       process.env.JOB_NAME === 'kibana_core' ? 'build' : null
