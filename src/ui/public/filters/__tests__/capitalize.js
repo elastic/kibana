735a197e6eb025ba4befdfb0c438f71b4a1a3583
Shelby Sturgis <shelbyjsturgis@gmail.com>
2016-02-12 17:43:03 -0800

    finishing tests for capitalize filter, adding ability to add non-capitalized words to the filter

--- src/ui/public/filters/__tests__/capitalize.js
+++ src/ui/public/filters/__tests__/capitalize.js
@@ -4,9 +4,9 @@ import expect from 'expect.js';
 import ngMock from 'ngMock';
 import 'ui/filters/capitalize';
 
-var filter;
+let filter;
 
-var strings = [
+const titles = [
   'capitalize',
   'saved searches',
   'an awesome visualization',
@@ -14,9 +14,9 @@ var strings = [
   'the title'
 ];
 
-var notAString = 10;
+const notAString = 10;
 
-var init = function (expandable) {
+const init = function (expandable) {
   // Load the application
   ngMock.module('kibana');
 
@@ -36,15 +36,37 @@ describe('capitalize filter', function () {
     expect(filter).to.not.be(null);
   });
 
-  it('should not capitalize certain words', function () {});
-
-  it('should always capitalize the first letter of the first word of a title', function () {});
-
-  it('should capitalize the first letter of a qualified string', function () {
-    expect(filter(string).charAt(0)).to.be(string.charAt(0).toUpperCase());
+  it('should simply return non-string types as is', function () {
+    expect(filter(notAString)).to.be(notAString);
   });
 
-  it('should simply return non-string types', function () {
-    expect(filter(notAString)).to.be(notAString);
+  titles.forEach((title) => {
+    const nonCapitalizedWords = filter.nonCapitalizedWords();
+    const titleString = filter(title);
+    const splitTitleString = titleString.split(' ');
+
+    it('should always capitalize the first letter of the first word of a title', function () {
+      expect(titleString.charAt(0)).to.be(title.chartAt(0).toUpperCase());
+    });
+
+    it('should capitalize the first letter of a qualified string', function () {
+      splitTitleString.forEach((word) => {
+        if (_.indexOf(nonCapitalizedWords, word) === -1) {
+          expect(word.charAt(0)).to.be(word.charAt(0).toUpperCase());
+        }
+      });
+    });
+
+    it('should not capitalize certain words', function () {
+      splitTitleString.forEach((word) => {
+        if (_.indexOf(nonCapitalizedWords, word) !== -1) {
+          expect(word.charAt(0)).to.be(word.charAt(0).toLowerCase());
+        }
+      });
+    });
+
+    it('should not be a trailing space at the end of words', function () {
+      expect(titleString.charAt(titleString.length - 1)).to.not.be(' ');
+    });
   });
 });
--- src/ui/public/filters/capitalize.js
+++ src/ui/public/filters/capitalize.js
@@ -5,27 +5,53 @@ import uiModules from 'ui/modules';
 uiModules
 .get('kibana')
 .filter('capitalize', function () {
-  const nonCapitalizedWords = ['a', 'an', 'the', 'and', 'but', 'or', 'for', 'nor', 'etc', 'on', 'at', 'to', 'from', 'by'];
+  let nonCapitalizedWords = ['a', 'an', 'the', 'and', 'but', 'or', 'for', 'nor', 'etc', 'on', 'at', 'to', 'from', 'by'];
 
-  return function (token) {
-    if (!_.isString(token)) return token;
+  function titlize(token) {
+    if (_.isString(token)) {
+      let words = token.split(' ');
+      let str = '';
 
-    const words = token.split(' ');
-    let str = '';
+      words.forEach((word, index) => {
+        // Do not capitalize non-capitalized words and always
+        // capitalize the first letter of the first word in a title.
+        if (_.indexOf(nonCapitalizedWords, word) !== -1 && index !== 0) {
+          str = str + word + ' ';
+        } else {
+          str = str + word.charAt(0).toUpperCase() + word.slice(1) + ' ';
+        }
 
-    words.forEach((word, index) => {
-      // Always capitalize the first letter of the first word in a title.
-      if (_.indexOf(nonCapitalizedWords, word) !== -1 && index !== 0) {
-        str = str + word + ' ';
-      }
+        if (index === words.length - 1) {
+          str = str.trim();
+        }
+      });
 
-      if (index === words.length - 1) {
-        str = str + word.charAt(0).toUpperCase() + word.slice(1);
-      } else {
-        str = str + word.charAt(0).toUpperCase() + word.slice(1) + ' ';
-      }
-    });
+      return str;
+    }
 
-    return str;
+    return token;
   };
+
+  function isAllStrings(value) {
+    if (_.isString(value)) return true;
+
+    if (_.isArray(value)) {
+      return value.every((item) => {
+        return _.isString(item);
+      });
+    }
+
+    return false;
+  }
+
+  titlize.nonCapitalizedWords = function (v) {
+    if (!arguments.length) return nonCapitalizedWords;
+    if (!isAllStrings(v)) {
+      throw new Error('titlize.nonCapitalizedWords expects a string or array of strings as input');
+    }
+    nonCapitalizedWords = _.isArray(v) ? v : nonCapitalizedWords.push(v);
+    return titlize;
+  };
+
+  return titlize;
 });
