2f2e579e3e2334a25cf4acd09afd6fe70d1a546a
Pavel Vaněček <corkscreewe+github@gmail.com>
2016-03-09 16:26:57 +0100

    build:installNpmDeps is now silent
    Without this fix, the build fails with "stderr maxBuffer exceeded". Probably because new npm version is more verbose and outputs more information, the child process buffer is filled earlier. I started seeing the build fail after I merged https://github.com/elastic/kibana/pull/6433 (I am following the 4.4 branch but I believe this applies everywhere).
    
    A workaround is to increase the buffer size, or disable the output altogether. I chose silent mode because all output from this command is ignored anyway.
    
    I am not sure if/why the official build works, though.

--- tasks/build/installNpmDeps.js
+++ tasks/build/installNpmDeps.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
   grunt.registerTask('_build:installNpmDeps', function () {
     grunt.file.mkdir('build/kibana/node_modules');
 
-    exec('npm install  --production --no-optional', {
+    exec('npm install  --production --no-optional --silent', {
       cwd: grunt.config.process('<%= root %>/build/kibana')
     }, this.async());
   });
