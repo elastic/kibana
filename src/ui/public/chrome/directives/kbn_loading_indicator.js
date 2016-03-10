f7112d49bee393af2155703c49809e5d83e77b5c
Khalah Jones-Golden <khasan222@gmail.com>
2016-03-10 14:02:26 -0400

    [Chrome] Append nav controls changes

---
+++ src/ui/public/chrome/directives/kbn_loading_indicator.js
@@ -0,0 +1,12 @@
+import UiModules from 'ui/modules';
+
+const spinnerTemplate = '<div class="spinner" ng-show="chrome.httpActive.length"></div>';
+
+UiModules
+.get('ui/kibana')
+.directive('kbnLoadingIndicator', function() {
+  return {
+    restrict: 'E',
+    template: spinnerTemplate
+  }
+});
