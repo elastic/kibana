99863dfddd067a8e46fa1994d8eba82621787d03
Spencer <spalger@users.noreply.github.com>
2016-03-11 12:13:24 -0700

    Merge pull request #123 from spalger/fix/proxyReqsWithBody
    Fix proxy reqs with body
    (cherry picked from commit babc531bc68f14cc7eba70dca3a1eabcb10b7787)

--- index.js
+++ index.js
@@ -85,36 +85,30 @@ module.exports = function (kibana) {
       }
 
       const proxyConfigCollection = new ProxyConfigCollection(options.proxyConfig);
-
-      // http://hapijs.com/api/8.8.1#route-configuration
-      server.route({
-        path: '/api/sense/proxy',
-        method: ['*', 'GET'],
-        config: {
-          validate: {
-            query: Joi.object().keys({
-              uri: Joi.string().uri({
-                allowRelative: false,
-                shema: ['http:', 'https:'],
-              }),
-            }).unknown(true),
-          },
-
-          pre: [
-            function filterUri(req, reply) {
-              const { uri } = req.query;
-
-              if (!filters.some(re => re.test(uri))) {
-                const err = Boom.forbidden();
-                err.output.payload = "Error connecting to '" + uri + "':\n\nUnable to send requests to that url.";
-                err.output.headers['content-type'] = 'text/plain';
-                reply(err);
-              } else {
-                reply();
-              }
-            }
-          ]
+      const proxyRouteConfig = {
+        validate: {
+          query: Joi.object().keys({
+            uri: Joi.string().uri({
+              allowRelative: false,
+              shema: ['http:', 'https:'],
+            }),
+          }).unknown(true),
         },
+
+        pre: [
+          function filterUri(req, reply) {
+            const { uri } = req.query;
+
+            if (!filters.some(re => re.test(uri))) {
+              const err = Boom.forbidden();
+              err.output.payload = "Error connecting to '" + uri + "':\n\nUnable to send requests to that url.";
+              err.output.headers['content-type'] = 'text/plain';
+              reply(err);
+            } else {
+              reply();
+            }
+          }
+        ],
 
         handler(req, reply) {
           const { uri } = req.query;
@@ -134,7 +128,28 @@ module.exports = function (kibana) {
             ...proxyConfigCollection.configForUri(uri)
           })
         }
+      };
+
+      server.route({
+        path: '/api/sense/proxy',
+        method: '*',
+        config: {
+          ...proxyRouteConfig,
+
+          payload: {
+            output: 'stream',
+            parse: false
+          },
+        }
       });
+
+      server.route({
+        path: '/api/sense/proxy',
+        method: 'GET',
+        config: {
+          ...proxyRouteConfig
+        }
+      })
 
       server.route({
         path: '/api/sense/api_server',
