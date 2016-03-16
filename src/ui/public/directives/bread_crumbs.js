0e3933860cd98949b2959fa06a5daecb663fded7
Shelby Sturgis <shelbyjsturgis@gmail.com>
2016-03-16 14:29:21 -0700

    adding breadcrumbs

---
+++ src/ui/public/directives/bread_crumbs.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import chrome from 'ui/chrome/chrome';
+import breadCrumbsTemplate from 'ui/partials/bread_crumbs.html';
+import uiModules from 'ui/modules';
+var module = uiModules.get('kibana');
+
+module.directive('breadCrumbs', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      userMakeUrl: '=?makeUrl'
+    },
+    template: breadCrumbsTemplate,
+    controller: function ($scope) {
+      $scope.crumbs = chrome.getBreadcrumbs();
+
+      if (_.last($scope.crumbs) === '') {
+        // Remove the empty string from the end of the array
+        $scope.crumbs.pop();
+      }
+
+      $scope.makeUrl = function (val) {
+        if ($scope.userMakeUrl) {
+          return $scope.userMakeUrl(val);
+        }
+      };
+    }
+  };
+});
