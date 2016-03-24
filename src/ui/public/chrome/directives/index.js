cabec73f928670061d32f706f19a7f258c38b455
Khalah Jones-Golden <khasan222@gmail.com>
2016-03-24 16:26:19 -0400

    [Chrome] some cleanup

--- src/ui/public/chrome/directives/index.js
+++ src/ui/public/chrome/directives/index.js
@@ -3,7 +3,7 @@ import 'ui/directives/config';
 import './app_switcher';
 import kbnChromeProv from './kbn_chrome';
 import kbnChromeNavControlsProv from './append_nav_controls';
-import courierLoadingIndicator from './kbn_loading_indicator';
+import './kbn_loading_indicator';
 
 export default function (chrome, internals) {
   kbnChromeProv(chrome, internals);
