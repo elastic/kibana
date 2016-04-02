ded3e79f017c2f960daf43e164cd35c9fb270723
spalger <spalger@users.noreply.github.com>
2016-04-01 18:36:41 -0700

    [kbnTopNav] expand the tests to verify 9924a58

--- src/ui/public/kbn_top_nav/__tests__/kbn_top_nav.js
+++ src/ui/public/kbn_top_nav/__tests__/kbn_top_nav.js
@@ -1,55 +1,59 @@
 import ngMock from 'ng_mock';
 import expect from 'expect.js';
-import { assign } from 'lodash';
+import { assign, pluck } from 'lodash';
 import $ from 'jquery';
 
-import '../';
+import '../kbn_top_nav';
+import KbnTopNavControllerProvider from '../kbn_top_nav_controller';
 import navbarExtensionsRegistry from 'ui/registry/navbar_extensions';
 import Registry from 'ui/registry/_registry';
 import 'ui/navbar_extensions';
 
 describe('kbnTopNav directive', function () {
-  var build = function () {};
-  let $testScope = null;
-  let stubRegistry;
+  let build;
+  let KbnTopNavController;
 
-  beforeEach(ngMock.module('kibana', function ($compileProvider, PrivateProvider) {
-    var renderCount = 0;
-    $compileProvider.directive('renderCounter', function () {
-      return {
-        link: function ($scope, $el) {
-          $el.html(++renderCount);
-        }
-      };
-    });
+  beforeEach(ngMock.module('kibana'));
+  beforeEach(ngMock.inject(function ($compile, $rootScope, Private) {
+    KbnTopNavController = Private(KbnTopNavControllerProvider);
 
-    stubRegistry = new Registry({
-      index: ['name'],
-      group: ['appName'],
-      order: ['order']
-    });
-
-    PrivateProvider.swap(navbarExtensionsRegistry, stubRegistry);
-    ngMock.module('kibana/navbar');
-  }));
-
-  beforeEach(ngMock.inject(function ($compile, $rootScope) {
-    build = function (scopeVars) {
-      var $el = $('<kbn-top-nav name="foo">');
-      $testScope = $rootScope.$new();
-      assign($testScope, scopeVars || {});
-      $compile($el)($testScope);
-      $testScope.$digest();
-      return $el;
+    build = function (scopeVars = {}, attrs = {}) {
+      const $el = $('<kbn-top-nav name="foo">').attr(attrs);
+      const $scope = $rootScope.$new();
+      assign($scope, scopeVars);
+      $compile($el)($scope);
+      $scope.$digest();
+      return { $el, $scope };
     };
-
   }));
 
   it('sets the proper functions on the kbnTopNav prop on scope', function () {
-    var $config = build();
-    expect($testScope.kbnTopNav.open).to.be.a(Function);
-    expect($testScope.kbnTopNav.close).to.be.a(Function);
-    expect($testScope.kbnTopNav.is).to.be.a(Function);
-    expect($testScope.kbnTopNav.toggle).to.be.a(Function);
+    const { $scope } = build();
+    expect($scope.kbnTopNav.open).to.be.a(Function);
+    expect($scope.kbnTopNav.close).to.be.a(Function);
+    expect($scope.kbnTopNav.is).to.be.a(Function);
+    expect($scope.kbnTopNav.toggle).to.be.a(Function);
+  });
+
+  it('allows config at nested keys', function () {
+    const scopeVars = {
+      kbn: {
+        top: {
+          nav: [
+            { key: 'foo' }
+          ]
+        }
+      }
+    };
+
+    const { $scope } = build(scopeVars, { config: 'kbn.top.nav' });
+    const optKeys = pluck($scope.kbnTopNav.opts, 'key');
+    expect(optKeys).to.eql(['foo']);
+  });
+
+  it('uses the KbnTopNavController if passed via config attribute', function () {
+    const controller = new KbnTopNavController();
+    const { $scope } = build({ controller }, { config: 'controller' });
+    expect($scope.kbnTopNav).to.be(controller);
   });
 });
