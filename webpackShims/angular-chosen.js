4d1996a270252bbb59c797d32378b71dc1047bc9
Lukas Olson <olson.lukas@gmail.com>
2016-04-05 15:54:46 -0700

    Add angular-chosen component

---
+++ webpackShims/angular-chosen.js
@@ -0,0 +1,7 @@
+require('jquery');
+require('angular');
+require('node_modules/chosenjs/chosen.jquery');
+require('node_modules/chosenjs/chosen.css');
+require('node_modules/angular-chosen-localytics/dist/angular-chosen');
+
+require('ui/modules').get('kibana', ['localytics.directives']);
