81821fc99de800e01d2697c133639ee726fc525e
LeeDr <lee.drengenberg@elastic.co>
2016-04-05 15:46:06 -0500

    Update selenium from 2.48.2 to 2.53.0 to work with latest Firefox 45.0.1

--- test/intern.js
+++ test/intern.js
@@ -5,7 +5,7 @@ define(function (require) {
   return _.assign({
     debug: true,
     capabilities: {
-      'selenium-version': '2.48.2',
+      'selenium-version': '2.53.0',
       'idle-timeout': 99
     },
     environments: [{
