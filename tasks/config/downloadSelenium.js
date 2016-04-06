4002140a9cfec2fdd8645523868d3f5ca310b01c
LeeDr <lee.drengenberg@elastic.co>
2016-04-06 08:46:17 -0500

    Add selenium-server-standalone jar md5.

--- tasks/config/downloadSelenium.js
+++ tasks/config/downloadSelenium.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
       selenium: {
         filename: FILE,
         server: DIR,
-        md5: 'b2784fc67c149d3c13c83d2108104689',
+        md5: '774efe2d84987fb679f2dea038c2fa32',
         path: path.join(grunt.config.get('root'), 'selenium', FILE)
       }
     }
