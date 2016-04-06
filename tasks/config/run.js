5bc2539f471e187ddef45b449fb8fb0f0470c6c2
Elastic Jasper <elastic-jasper@users.noreply.github.com>
2016-04-06 14:35:47 -0400

    Backport PR #6792 ---------
    **Commit 1:**
    Update selenium from 2.48.2 to 2.53.0 to work with latest Firefox 45.0.1
    
    * Original sha: 81821fc99de800e01d2697c133639ee726fc525e
    * Authored by LeeDr <lee.drengenberg@elastic.co> on 2016-04-05T20:46:06Z
    
    **Commit 2:**
    Update downloadSelenium to match version in intern.js.
    
    * Original sha: 431fe52cecd8ec55c2945e7bbfac90f2fd6eb2c5
    * Authored by LeeDr <lee.drengenberg@elastic.co> on 2016-04-06T12:36:06Z
    
    **Commit 3:**
    Add selenium-server-standalone jar md5.
    
    * Original sha: 4002140a9cfec2fdd8645523868d3f5ca310b01c
    * Authored by LeeDr <lee.drengenberg@elastic.co> on 2016-04-06T13:46:17Z

--- tasks/config/run.js
+++ tasks/config/run.js
@@ -88,7 +88,7 @@ module.exports = function (grunt) {
       cmd: 'java',
       args: [
         '-jar',
-        'selenium/selenium-server-standalone-2.48.2.jar',
+        'selenium/selenium-server-standalone-2.53.0.jar',
         '-port',
         uiConfig.servers.webdriver.port
       ]
@@ -104,7 +104,7 @@ module.exports = function (grunt) {
       cmd: 'java',
       args: [
         '-jar',
-        'selenium/selenium-server-standalone-2.48.2.jar',
+        'selenium/selenium-server-standalone-2.53.0.jar',
         '-port',
         uiConfig.servers.webdriver.port
       ]
