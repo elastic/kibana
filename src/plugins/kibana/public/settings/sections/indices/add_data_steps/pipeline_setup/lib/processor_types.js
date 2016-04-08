14799bf1828a9d8460a6b5ae1958435bc6060f39
Jim Unger <BigFunger@gmail.com>
2016-04-08 13:27:23 -0500

    [add data] adds the date processor

--- src/plugins/kibana/public/settings/sections/indices/add_data_steps/pipeline_setup/lib/processor_types.js
+++ src/plugins/kibana/public/settings/sections/indices/add_data_steps/pipeline_setup/lib/processor_types.js
@@ -70,6 +70,37 @@ export class Convert extends Processor {
   }
 };
 
+export class Date extends Processor {
+  constructor(processorId) {
+    super(processorId, 'date', 'Date');
+    this.sourceField = '';
+    this.targetField = '@timestamp';
+    this.formats = [];
+    this.timezone = 'Etc/UTC';
+    this.locale = 'ENGLISH';
+    this.customFormat = '';
+  }
+
+  get description() {
+    const source = this.sourceField || '?';
+    const target = this.targetField || '?';
+    return `[${source}] -> [${target}]`;
+  }
+
+  get model() {
+    return {
+      processorId: this.processorId,
+      typeId: this.typeId,
+      sourceField: this.sourceField || '',
+      targetField: this.targetField || '',
+      formats: this.formats || [],
+      timezone: this.timezone || '',
+      locale: this.locale || '',
+      customFormat: this.customFormat || ''
+    };
+  }
+};
+
 export class Gsub extends Processor {
   constructor(processorId) {
     super(processorId, 'gsub', 'Gsub');
