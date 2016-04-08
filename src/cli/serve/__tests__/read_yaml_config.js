fe7abcb6148297ccd36c762c0f1cbc221ae7236c
spalger <email@spalger.com>
2016-04-07 23:53:31 -0700

    [cli/serve] accept mulitple --config flags

---
+++ src/cli/serve/__tests__/read_yaml_config.js
@@ -0,0 +1,7 @@
+describe('cli/serve/read_yaml_config', function () {
+  it('merges together multiple files');
+  it('accepts a single config file');
+  it('warns about deprecated settings');
+  it('warns about legacy config value usage');
+  it('only warns once about legacy and deprecated settings');
+});
