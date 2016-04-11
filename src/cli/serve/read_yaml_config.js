b7738f18b665cf47f0942634acc900dc2fa60611
spalger <email@spalger.com>
2016-04-11 12:41:20 -0700

    [cli/server] split up file merging and deprecated settings logic

--- src/cli/serve/read_yaml_config.js
+++ src/cli/serve/read_yaml_config.js
@@ -1,82 +1,35 @@
-import _ from 'lodash';
-import ansicolors from 'ansicolors';
+import { chain, isArray, isPlainObject, forOwn, set, transform } from 'lodash';
 import { readFileSync as read } from 'fs';
 import { safeLoad } from 'js-yaml';
 
 import { fromRoot } from '../../utils';
+import { rewriteDeprecatedConfig } from './deprecated_config';
 
-const warnAboutDeprecation = _.memoize(function (key, message) {
-  console.log(ansicolors.red('WARNING:'), message);
-});
-
-let legacySettingMap = {
-  // server
-  port: 'server.port',
-  host: 'server.host',
-  pid_file: 'pid.file',
-  ssl_cert_file: 'server.ssl.cert',
-  ssl_key_file: 'server.ssl.key',
-
-  // logging
-  log_file: 'logging.dest',
-
-  // kibana
-  kibana_index: 'kibana.index',
-  default_app_id: 'kibana.defaultAppId',
-
-  // es
-  ca: 'elasticsearch.ssl.ca',
-  elasticsearch_preserve_host: 'elasticsearch.preserveHost',
-  elasticsearch_url: 'elasticsearch.url',
-  kibana_elasticsearch_client_crt: 'elasticsearch.ssl.cert',
-  kibana_elasticsearch_client_key: 'elasticsearch.ssl.key',
-  kibana_elasticsearch_password: 'elasticsearch.password',
-  kibana_elasticsearch_username: 'elasticsearch.username',
-  ping_timeout: 'elasticsearch.pingTimeout',
-  request_timeout: 'elasticsearch.requestTimeout',
-  shard_timeout: 'elasticsearch.shardTimeout',
-  startup_timeout: 'elasticsearch.startupTimeout',
-  verify_ssl: 'elasticsearch.ssl.verify',
-};
-
-const deprecatedSettings = {
-  'server.xsrf.token': 'server.xsrf.token is deprecated. It is no longer used when providing xsrf protection.'
-};
-
-export default function (paths) {
-  if (!paths) return {};
-
-  const files = [].concat(paths).map(path => {
-    return safeLoad(read(path, 'utf8'));
-  });
-
-  return _.transform(files, function applyConfigFile(config, file) {
-    _.forOwn(file, function apply(val, key) {
-      // transform legeacy options into new namespaced versions
-      if (legacySettingMap.hasOwnProperty(key)) {
-        const replacement = legacySettingMap[key];
-        warnAboutDeprecation(key, `Config key "${key}" is deprecated. It has been replaced with "${replacement}"`);
-        key = replacement;
-      }
-
-      if (_.isPlainObject(val)) {
-        _.forOwn(val, function (subVal, subKey) {
+export function merge(sources) {
+  return transform(sources, (merged, source) => {
+    forOwn(source, function apply(val, key) {
+      if (isPlainObject(val)) {
+        forOwn(val, function (subVal, subKey) {
           apply(subVal, key + '.' + subKey);
         });
+        return;
       }
-      else if (_.isArray(val)) {
-        _.set(config, key, []);
-        val.forEach((subVal, i) => {
-          apply(subVal, key + '.' + i);
-        });
-      }
-      else {
-        if (deprecatedSettings[key]) {
-          warnAboutDeprecation(key, deprecatedSettings[key]);
-        }
 
-        _.set(config, key, val);
+      if (isArray(val)) {
+        set(merged, key, []);
+        val.forEach((subVal, i) => apply(subVal, key + '.' + i));
+        return;
       }
+
+      set(merged, key, val);
     });
   }, {});
+}
+
+export default function (paths) {
+  const files = [].concat(paths || [])
+    .map(path => safeLoad(read(path, 'utf8')))
+    .map(rewriteDeprecatedConfig);
+
+  return merge(files);
 }
