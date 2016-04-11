dc47246867a76ab9a1962847dbbbc3c10d594d85
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-04-11 17:28:53 -0300

    [settings] DELETE route forked from POST route.

--- src/plugins/kibana/server/routes/api/settings/index.js
+++ src/plugins/kibana/server/routes/api/settings/index.js
@@ -1,4 +1,5 @@
 export default function (server) {
   require('./register_get')(server);
   require('./register_set')(server);
+  require('./register_delete')(server);
 }
--- src/plugins/kibana/server/routes/api/settings/register_delete.js
+++ src/plugins/kibana/server/routes/api/settings/register_delete.js
@@ -3,10 +3,9 @@ import Boom from 'boom';
 export default function registerSet(server) {
   server.route({
     path: '/api/kibana/settings/{key}',
-    method: 'POST',
+    method: 'DELETE',
     handler: async function (req, reply) {
       const key = req.params.key;
-      const value = req.payload.value;
       const client = server.plugins.elasticsearch.client;
       const config = server.config();
       const index = config.get('kibana.index');
@@ -20,7 +19,7 @@ export default function registerSet(server) {
           id,
           body: {
             doc: {
-              [key]: value
+              [key]: null
             }
           }
         })
