dc316a65c24cd457754c790fa77dd17be69134f2
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-04-11 12:56:28 -0300

    [settings] Implemented GET /api/kibana/settings.

--- src/plugins/kibana/server/routes/api/settings/register_get.js
+++ src/plugins/kibana/server/routes/api/settings/register_get.js
@@ -1,14 +1,25 @@
+import Boom from 'boom';
+import { assign } from 'lodash';
 import defaultsProvider from './defaults';
 
 export default function registerGet(server) {
   server.route({
     path: '/api/kibana/settings',
     method: 'GET',
-    handler: function (req, reply) {
+    handler: async function (req, reply) {
       const defaults = defaultsProvider();
-      const settings = defaults;
+      const client = server.plugins.elasticsearch.client;
+      const config = server.config();
+      const index = config.get('kibana.index');
+      const id = config.get('pkg.version');
+      const type = 'config';
 
-      reply(settings).type('application/json');
+      client
+        .get({ index, type, id })
+        .then(res => res._source)
+        .then(user => assign(defaults, user))
+        .then(settings => reply(settings).type('application/json'))
+        .catch(reason => reply(Boom.create(500, `Elasticsearch failure.`)));
     }
   });
 }
