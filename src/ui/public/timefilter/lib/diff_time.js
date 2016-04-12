fb0592cd2e2d6c7b71e1824a559fb447c4eda766
Court Ewing <court@epixa.com>
2016-04-12 18:31:57 -0400

    [internal] Replace var with let in ui/public P-T
    This change was applied to any .js files under directories beginning
    with `p` through `t` in src/ui/public. There are no `o` directories.
    
    This was an automatic replacement from var to let for any variable
    declaration that doubles as the initial assignment. Ultimately we want
    most of these to be converted to const, but this commit is so large that
    it warrants breaking each step of automation up into its own commit.
    
    For example:
    
    `var foo = 'bar';` becomes `let foo = 'var';`
    
    This was accomplished by replacing:
    find: `var ([a-zA-Z_$][0-9a-zA-Z_$]*)(\s+)=`
    replace: `let $1$2=`

--- src/ui/public/timefilter/lib/diff_time.js
+++ src/ui/public/timefilter/lib/diff_time.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import UtilsDiffTimePickerValsProvider from 'ui/utils/diff_time_picker_vals';
 export default function diffTimeProvider(Private) {
-  var diff = Private(UtilsDiffTimePickerValsProvider);
+  let diff = Private(UtilsDiffTimePickerValsProvider);
 
   return function (self) {
-    var oldTime = _.clone(self.time);
+    let oldTime = _.clone(self.time);
     return function () {
       if (diff(self.time, oldTime)) {
         self.emit('update');
--- src/ui/public/typeahead/_input.js
+++ src/ui/public/typeahead/_input.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import 'ui/notify/directives';
 import uiModules from 'ui/modules';
-var typeahead = uiModules.get('kibana/typeahead');
+let typeahead = uiModules.get('kibana/typeahead');
 
 
 typeahead.directive('kbnTypeaheadInput', function ($rootScope) {
@@ -11,8 +11,8 @@ typeahead.directive('kbnTypeaheadInput', function ($rootScope) {
     require: ['^ngModel', '^kbnTypeahead'],
 
     link: function ($scope, $el, $attr, deps) {
-      var model = deps[0];
-      var typeaheadCtrl = deps[1];
+      let model = deps[0];
+      let typeaheadCtrl = deps[1];
 
       typeaheadCtrl.setInputModel(model);
 
