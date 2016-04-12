efb7647ab2e7fd3d8cf7eff488903ca7c8323792
Jim Unger <BigFunger@gmail.com>
2016-04-12 10:32:10 -0500

    [add data] adds database_file and fields to the geoip processor

--- src/plugins/kibana/server/lib/schemas/resources/ingest_processor_schemas.js
+++ src/plugins/kibana/server/lib/schemas/resources/ingest_processor_schemas.js
@@ -30,7 +30,9 @@ export const date = base.keys({
 export const geoip = base.keys({
   type_id: Joi.string().only('geoip').required(),
   source_field: Joi.string().allow(''),
-  target_field: Joi.string().allow('')
+  target_field: Joi.string().allow(''),
+  database_file: Joi.string().allow(''),
+  database_fields: Joi.array().items(Joi.string().allow('')),
 });
 
 export const gsub = base.keys({
