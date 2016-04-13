73de902debcb016bfaec69d8b2098390610fe421
Jim Unger <BigFunger@gmail.com>
2016-04-13 09:02:09 -0500

    [add data] adds the trim processor

--- src/plugins/kibana/public/settings/sections/indices/add_data_steps/pipeline_setup/lib/processor_types.js
+++ src/plugins/kibana/public/settings/sections/indices/add_data_steps/pipeline_setup/lib/processor_types.js
@@ -308,3 +308,23 @@ export class Split extends Processor {
     };
   }
 };
+
+export class Trim extends Processor {
+  constructor(processorId) {
+    super(processorId, 'trim', 'Trim');
+    this.sourceField = '';
+  }
+
+  get description() {
+    const source = this.sourceField || '?';
+    return `[${source}]`;
+  }
+
+  get model() {
+    return {
+      processorId: this.processorId,
+      typeId: this.typeId,
+      sourceField: this.sourceField || ''
+    };
+  }
+};
