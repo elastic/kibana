d7fad8d6573d1eaff415a7465208221635e56c23
spalger <email@spalger.com>
2016-04-13 10:30:23 -0700

    [cli/server] flags do not have values

--- src/cli/serve/serve.js
+++ src/cli/serve/serve.js
@@ -77,7 +77,7 @@ module.exports = function (program) {
   .option(
     '-c, --config <path>',
     'Path to the config file, can be changed with the CONFIG_PATH environment variable as well. ' +
-    'Use mulitple --config flags to include multiple config files.',
+    'Use mulitple --config args to include multiple config files.',
     configPathCollector,
     [ process.env.CONFIG_PATH || fromRoot('config/kibana.yml') ]
   )
