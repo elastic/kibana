ab5463a7bdbfe1e7a1c79a50ba11c4db24e08167
spalger <email@spalger.com>
2016-04-13 14:27:42 -0700

    [cli/serve] simplify deprecated config check by doing it after merge()

--- src/cli/serve/__tests__/deprecated_config.js
+++ src/cli/serve/__tests__/deprecated_config.js
@@ -1,81 +1,48 @@
 import expect from 'expect.js';
-import { rewriteDeprecatedConfig } from '../deprecated_config';
+import { set } from 'lodash';
+import { checkForDeprecatedConfig } from '../deprecated_config';
 import sinon from 'auto-release-sinon';
 
 describe('cli/serve/deprecated_config', function () {
-  it('returns a clone of the input', function () {
-    const file = {};
-    const output = rewriteDeprecatedConfig(file);
-    expect(output).to.not.be(file);
+  it('passes original config through', function () {
+    const config = {};
+    set(config, 'server.xsrf.token', 'xxtokenxx');
+    const output = checkForDeprecatedConfig(config);
+    expect(output).to.be(config);
+    expect(output.server).to.be(config.server);
+    expect(output.server.xsrf).to.be(config.server.xsrf);
+    expect(output.server.xsrf.token).to.be(config.server.xsrf.token);
   });
 
-  describe('legacy config values', function () {
-    it('rewrites legacy config values with literal path replacement', function () {
-      const file = { port: 4000, host: 'kibana.com' };
-      const output = rewriteDeprecatedConfig(file);
-      expect(output).to.not.be(file);
-      expect(output).to.eql({
-        'server.port': 4000,
-        'server.host': 'kibana.com',
-      });
-    });
-
-    it('logs warnings when legacy config properties are encountered', function () {
-      const log = sinon.stub();
-      rewriteDeprecatedConfig({ port: 5555 }, log);
-      sinon.assert.calledOnce(log);
-      expect(log.firstCall.args[0]).to.match(/port.+deprecated.+server\.port/);
-    });
+  it('logs warnings about deprecated config values', function () {
+    const log = sinon.stub();
+    const config = {};
+    set(config, 'server.xsrf.token', 'xxtokenxx');
+    checkForDeprecatedConfig(config, log);
+    sinon.assert.calledOnce(log);
+    expect(log.firstCall.args[0]).to.match(/server\.xsrf\.token.+deprecated/);
   });
 
-  describe('deprecated config values', function () {
-    it('logs warnings about deprecated config values', function () {
+  describe('does not support compound.keys', function () {
+    it('ignores fully compound keys', function () {
       const log = sinon.stub();
-      rewriteDeprecatedConfig({ 'server.xsrf.token': 'xxtokenxx' }, log);
-      sinon.assert.calledOnce(log);
-      expect(log.firstCall.args[0]).to.match(/server\.xsrf\.token.+deprecated/);
+      const config = { 'server.xsrf.token': 'xxtokenxx' };
+      checkForDeprecatedConfig(config, log);
+      sinon.assert.notCalled(log);
     });
 
-    it('passes deprecated values through', function () {
-      const prop = 'server.xsrf.token';
-      const file = { [prop]: 'xxtokenxx' };
-      const output = rewriteDeprecatedConfig(file);
-      expect(output).to.not.be(file);
-      expect(output).to.have.property(prop, file[prop]);
+    it('ignores partially compound keys', function () {
+      const log = sinon.stub();
+      const config = { server: { 'xsrf.token': 'xxtokenxx' } };
+      checkForDeprecatedConfig(config, log);
+      sinon.assert.notCalled(log);
     });
 
-    context('defined in a mixture of path keys and objects', function () {
-      it('detects nested propertie', function () {
-        const log = sinon.stub();
-        rewriteDeprecatedConfig({
-          server: {
-            xsrf: {
-              token: 'x'
-            }
-          }
-        }, log);
-        sinon.assert.calledOnce(log);
-      });
-
-      it('detects compound properties inside an object', function () {
-        const log = sinon.stub();
-        rewriteDeprecatedConfig({
-          server: {
-            'xsrf.token': 'x'
-          }
-        }, log);
-        sinon.assert.calledOnce(log);
-      });
-
-      it('detects a property under a compound property', function () {
-        const log = sinon.stub();
-        rewriteDeprecatedConfig({
-          'server.xsrf': {
-            token: 'x'
-          }
-        }, log);
-        sinon.assert.calledOnce(log);
-      });
+    it('ignores partially compound keys', function () {
+      const log = sinon.stub();
+      const config = { 'server.xsrf': { token: 'xxtokenxx' } };
+      checkForDeprecatedConfig(config, log);
+      sinon.assert.notCalled(log);
     });
   });
 });
