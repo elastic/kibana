116521c6be68676ac2c808bfb065d209f3fa708c
spalger <email@spalger.com>
2016-04-15 16:40:18 -0700

    [chrome] rework url overflow detection
    The previous version of this pr relied on the State service to catch times when the URL would grow out of control. While overflows will commonly occur in the State service, this didn't handle urls that were navigated to using a link. They worked because the state service would eventually be called, but the failure was unexpected and required interaction to trigger.
    
    This new approach does the checking at a higher level, in the chrome.
    
    We also removed the `url:limit` configuration value in favor of browser detection (I was not able to find or come up with a way to quietly and quickly feature detect this). The new limits are 2000 characters for IE and 25000 for all other browsers.

---
+++ src/ui/public/error_url_overflow/url_overflow_service.js
@@ -0,0 +1,62 @@
+
+export class UrlOverflowService {
+  constructor() {
+    const key = 'error/url-overflow/url';
+    const store = window.sessionStorage || {
+      getItem() {},
+      setItem() {},
+      removeItem() {},
+    };
+
+    // FIXME: Couldn't find a way to test for browser compatibility without
+    // complex redirect and cookie based "feature-detection" page, so going
+    // with user-agent detection for now.
+    this._ieLike = /(;MSIE |Edge\/\d)/.test(window.navigator.userAgent);
+
+    this._val = store.getItem(key);
+    this._sync = () => {
+      if (this._val == null) store.removeItem(key);
+      else store.setItem(key, this._val);
+    };
+  }
+
+  failLength() {
+    return this._ieLike ? 2000 : 25000;
+  }
+
+  set(v) {
+    this._val = v;
+    this._sync();
+  }
+
+  get() {
+    return this._val;
+  }
+
+  check(absUrl) {
+    if (!this.get()) {
+      const urlLength = absUrl.length;
+      const remaining = this.failLength() - urlLength;
+
+      if (remaining > 0) {
+        return remaining;
+      }
+
+      this.set(absUrl);
+    }
+
+    throw new Error(`
+      The URL has gotten too big and kibana can no longer
+      continue. Please refresh to return to your previous state.
+    `);
+  }
+
+  clear() {
+    this._val = undefined;
+    this._sync();
+  }
+}
+
+export function UrlOverflowServiceProvider() {
+  return new UrlOverflowService();
+}
