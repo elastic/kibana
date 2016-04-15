2bb71817cbc4d88969762a5ea400d937feaec3ed
Elastic Jasper <elastic-jasper@users.noreply.github.com>
2016-04-15 15:20:26 -0400

    Backport PR #6936 ---------
    **Commit 1:**
    [internal] Replace var with const/let in ui/public/utils/lodash-mixins
    
    This change was applied only to files in
    src/ui/public/utils/lodash-mixins directory.
    
    All instances of var were replaced with let, and then all instances
    of let that wouldn't flag the no-const-assign rule in the linter were
    replaced with const.
    
    * Original sha: 18e97256628e7821e6bfae3c6a78e1b51cade0fb
    * Authored by Court Ewing <court@epixa.com> on 2016-04-14T18:32:49Z

--- src/ui/public/utils/lodash-mixins/__tests__/_push_all.js
+++ src/ui/public/utils/lodash-mixins/__tests__/_push_all.js
@@ -1,12 +1,12 @@
 describe('_.pushAll', function () {
-  let _ = require('lodash');
-  let expect = require('expect.js');
+  const _ = require('lodash');
+  const expect = require('expect.js');
 
   it('pushes an entire array into another', function () {
-    let a = [1, 2, 3, 4];
-    let b = [5, 6, 7, 8];
+    const a = [1, 2, 3, 4];
+    const b = [5, 6, 7, 8];
 
-    let output = _.pushAll(b, a);
+    const output = _.pushAll(b, a);
     expect(output).to.be(a);
     expect(a).to.eql([1, 2, 3, 4, 5, 6, 7, 8]);
     expect(b).to.eql([5, 6, 7, 8]);
