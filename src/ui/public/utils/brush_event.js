a9e3cfe24a20819cfed5762372039c9c2fa1fa7a
Court Ewing <court@epixa.com>
2016-04-18 10:35:24 -0400

    Backport PR #6951 ---------
    **Commit 1:**
    [internal] Replace var with let in ui/public/utils (no rison)
    
    This change was applied to any .js files under the src/ui/public/utils
    directory except for the `rison.js` file. The rison code has block level
    hoisting issues, but it seems safer to just not touch the file rather
    than fix them.
    
    This was an automatic replacement from var to let for any variable
    declaration that doubles as the initial assignment. Ultimately we want
    most of these to be converted to const, but this commit is so large that
    it warrants breaking each step of automation up into its own commit.
    
    For example:
    
    `var foo = 'bar';` becomes `let foo = 'var';`
    
    This was accomplished by replacing:
    find: `var ([a-zA-Z_$][0-9a-zA-Z_$]*)(\s+)=`
    replace: `let $1$2=`
    
    * Original sha: ec0707f092e8726c484410d59c83ea5fb0aed062
    * Authored by Court Ewing <court@epixa.com> on 2016-04-15T21:20:57Z

--- src/ui/public/utils/brush_event.js
+++ src/ui/public/utils/brush_event.js
@@ -1,9 +1,9 @@
 define(function (require) {
-  var moment = require('moment');
+  let moment = require('moment');
   return function brushEventProvider(timefilter) {
     return function (event) {
-      var from = moment(event.range[0]);
-      var to = moment(event.range[1]);
+      let from = moment(event.range[0]);
+      let to = moment(event.range[1]);
 
       if (to - from === 0) return;
 
--- src/ui/public/utils/sequencer.js
+++ src/ui/public/utils/sequencer.js
@@ -1,19 +1,19 @@
 define(function (require) {
-  var _ = require('lodash');
+  let _ = require('lodash');
 
   function create(min, max, length, mod) {
-    var seq = new Array(length);
+    let seq = new Array(length);
 
-    var valueDist = max - min;
+    let valueDist = max - min;
 
     // range of values that the mod creates
-    var modRange = [mod(0, length), mod(length - 1, length)];
+    let modRange = [mod(0, length), mod(length - 1, length)];
 
     // distance between
-    var modRangeDist = modRange[1] - modRange[0];
+    let modRangeDist = modRange[1] - modRange[0];
 
     _.times(length, function (i) {
-      var modIPercent = (mod(i, length) - modRange[0]) / modRangeDist;
+      let modIPercent = (mod(i, length) - modRange[0]) / modRangeDist;
 
       // percent applied to distance and added to min to
       // produce value
@@ -81,8 +81,8 @@ define(function (require) {
       // adapted from output of http://www.timotheegroleau.com/Flash/experiments/easing_function_generator.htm
       // generates numbers from 0 to 100
 
-      var ts = (i /= length) * i;
-      var tc = ts * i;
+      let ts = (i /= length) * i;
+      let tc = ts * i;
       return 100 * (
         0.5 * tc * ts +
         -3 * ts * ts +
