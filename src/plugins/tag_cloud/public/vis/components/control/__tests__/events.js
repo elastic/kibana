e5907d45453231a9944c3b4411a145cf22276da7
Shelby Sturgis <shelbyjsturgis@gmail.com>
2016-04-19 11:15:52 -0700

    adding first test file and adding  tag cloud to functional tests

--- src/plugins/tag_cloud/public/vis/components/control/__tests__/events.js
+++ src/plugins/tag_cloud/public/vis/components/control/__tests__/events.js
@@ -1,45 +1,61 @@
-// import eventsFunction from 'src/modules/component/events/events';
-// import d3fixture from 'fixtures/fixture';
-// import remove from 'fixtures/remove';
-// import expect from 'expect.js';
-//
-// describe('events tests', function () {
-//   let totalListenerCount;
-//   let listeners;
-//   let fixture;
-//   let events;
-//
-//   beforeEach(function () {
-//     fixture = d3fixture;
-//     events = eventsFunction();
-//     listeners = {
-//       click: [function (e) { console.log(e); }],
-//       brush: [function (brush) { return brush.extent(); }],
-//       mouseover: [function (e, d) { return d; }]
-//     };
-//     totalListenerCount = 3;
-//   });
-//
-//   afterEach(function () {
-//     remove(fixture);
-//   });
-//
-//   it('should return a function', function () {
-//     chai.assert.isFunction(events);
-//   });
-//
-//   describe('listeners API', function () {
-//     afterEach(function () {
-//       events.listeners({});
-//     });
-//
-//     it('should return the listeners object', function () {
-//       chai.assert.deepEqual(events.listeners(), {});
-//     });
-//
-//     it('should set the listeners object', function () {
-//       events.listeners(listeners); // Add listeners
-//       chai.assert.deepEqual(events.listeners(), listeners);
-//     });
-//   });
-// });
+import d3 from 'd3';
+import _ from 'lodash';
+import expect from 'expect.js';
+import eventsFunction from 'plugins/tagcloud/vis/components/control/events';
+
+function visFixture() {
+  let div = document.createElement('div');
+  let element = document.body.appendChild(div);
+
+  element.setAttribute('style', 'position: relative');
+  element.style.width = '500px';
+  element.style.height = '500px';
+
+  return element;
+}
+
+function remove(element) {
+  let el = d3.select(element);
+  el.remove();
+  el = null;
+}
+
+describe('events tests', function () {
+  let totalListenerCount;
+  let listeners;
+  let fixture;
+  let events;
+
+  beforeEach(function () {
+    fixture = visFixture();
+    events = eventsFunction();
+    listeners = {
+      click: [function (e) { console.log(e); }],
+      mouseover: [function (e, d) { return d; }]
+    };
+    totalListenerCount = Object.keys(listeners).length;
+  });
+
+  afterEach(function () {
+    remove(fixture);
+  });
+
+  it('should return a function', function () {
+    expect(_.isFunction(events)).to.be(true);
+  });
+
+  describe('listeners API', function () {
+    afterEach(function () {
+      events.listeners({});
+    });
+
+    it('should return the listeners object', function () {
+      expect(_.isEqual(events.listeners(), {})).to.be.ok();
+    });
+
+    it('should set the listeners object', function () {
+      events.listeners(listeners); // Add listeners
+      expect(_.deepEqual(events.listeners(), listeners)).to.be.ok();
+    });
+  });
+});
