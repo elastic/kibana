c821159b6add28ba2b1ebdf0c988b89f40d52083
Matthew Bargar <mbargar@gmail.com>
2016-04-20 14:35:00 -0400

    [FilebeatInstall] Remove hardcoded links and use the new documentation link service instead

--- src/plugins/kibana/public/settings/sections/indices/add_data_steps/install_filebeat_step.js
+++ src/plugins/kibana/public/settings/sections/indices/add_data_steps/install_filebeat_step.js
@@ -2,6 +2,7 @@ import modules from 'ui/modules';
 import template from 'plugins/kibana/settings/sections/indices/add_data_steps/install_filebeat_step.html';
 import 'ui/pattern_checker';
 import { patternToIngest } from '../../../../../common/lib/convert_pattern_and_ingest_name';
+import { filebeat as docLinks } from '../../../../../../../ui/public/documentation_links';
 
 modules.get('apps/settings')
   .directive('installFilebeatStep', function () {
@@ -14,6 +15,7 @@ modules.get('apps/settings')
       controllerAs: 'installStep',
       controller: function ($scope) {
         this.pipelineId = patternToIngest(this.results.indexPattern.id);
+        this.docLinks = docLinks;
       }
     };
   });
