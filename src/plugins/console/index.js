88a834ea51d73cee1889708c66451e49aa5e35d9
Shaunak Kashyap <ycombinator@gmail.com>
2016-04-26 13:23:54 -0700

    Use elasticsearch.url as the Elasticsearch base URL

--- src/plugins/console/index.js
+++ src/plugins/console/index.js
@@ -37,7 +37,6 @@ module.exports = function (kibana) {
     config: function (Joi) {
       return Joi.object({
         enabled: Joi.boolean().default(true),
-        defaultServerUrl: Joi.string().default('http://localhost:9200'),
         proxyFilter: Joi.array().items(Joi.string()).single().default(['.*']),
         ssl: Joi.object({
           verify: Joi.boolean(),
@@ -88,10 +87,7 @@ module.exports = function (kibana) {
       const proxyRouteConfig = {
         validate: {
           query: Joi.object().keys({
-            uri: Joi.string().uri({
-              allowRelative: false,
-              shema: ['http:', 'https:'],
-            }),
+            uri: Joi.string()
           }).unknown(true),
         },
 
@@ -111,15 +107,23 @@ module.exports = function (kibana) {
         ],
 
         handler(req, reply) {
-          const { uri } = req.query;
+          let baseUri = server.config().get('elasticsearch.url');
+          let { uri:path } = req.query;
 
+          baseUri = baseUri.replace(/\/+$/, '');
+          path = path.replace(/^\/+/, '');
+          const uri = baseUri + '/' + path;
+
+          const requestHeadersWhitelist = server.config().get('elasticsearch.requestHeadersWhitelist');
+          const filterHeaders = server.plugins.elasticsearch.filterHeaders;
           reply.proxy({
-            uri,
+            mapUri: function (request, done) {
+              done(null, uri, filterHeaders(request.headers, requestHeadersWhitelist))
+            },
             xforward: true,
-            passThrough: true,
             onResponse(err, res, request, reply, settings, ttl) {
               if (err != null) {
-                reply("Error connecting to '" + request.query.uri + "':\n\n" + err.message).type("text/plain").statusCode = 502;
+                reply("Error connecting to '" + uri + "':\n\n" + err.message).type("text/plain").statusCode = 502;
               } else {
                 reply(null, res);
               }
