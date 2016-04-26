a9048e745249683310e8198f0fecd2e39e299d95
Jim Unger <BigFunger@gmail.com>
2016-04-26 13:54:54 -0500

    [add data] hides disabled processors

--- src/plugins/kibana/server/routes/api/ingest/index.js
+++ src/plugins/kibana/server/routes/api/ingest/index.js
@@ -1,9 +1,11 @@
 import { registerPost } from './register_post';
 import { registerDelete } from './register_delete';
+import { registerProcessors } from './register_processors';
 import { registerSimulate } from './register_simulate';
 
 export default function (server) {
   registerPost(server);
   registerDelete(server);
+  registerProcessors(server);
   registerSimulate(server);
 }
