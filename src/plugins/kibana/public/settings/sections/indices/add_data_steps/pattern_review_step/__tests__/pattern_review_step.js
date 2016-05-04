86a6e20b1dfc9823190db4f9d8bcef7b1d8aac7a
Matthew Bargar <mbargar@gmail.com>
2016-05-04 17:54:32 -0400

    Add tests for detecting date fields and conflicting array values

--- src/plugins/kibana/public/settings/sections/indices/add_data_steps/pattern_review_step/__tests__/pattern_review_step.js
+++ src/plugins/kibana/public/settings/sections/indices/add_data_steps/pattern_review_step/__tests__/pattern_review_step.js
@@ -52,4 +52,40 @@ describe('pattern review directive', function () {
     });
   });
 
+  describe('detecting date fields', function () {
+
+    it('should detect sample strings in ISO 8601 format as date fields', function () {
+      const scope = $rootScope.$new();
+      scope.sampleDoc = {
+        isodate: '2004-03-08T00:05:49.000Z'
+      };
+
+      $compile('<pattern-review-step sample-doc="sampleDoc" index-pattern="indexPattern"></pattern-review-step>')(scope);
+      scope.$digest();
+
+      expect(scope).to.have.property('indexPattern');
+      expect(scope.indexPattern.fields[0].type).to.be('date');
+    });
+
+  });
+
+  describe('conflicting array values', function () {
+
+    it('should detect heterogeneous arrays and flag them with an error message', function () {
+      const scope = $rootScope.$new();
+      scope.sampleDoc = {
+        badarray: ['foo', 42]
+      };
+
+      const element = $compile('<pattern-review-step sample-doc="sampleDoc" index-pattern="indexPattern"></pattern-review-step>')(scope);
+      const controller = element.controller('patternReviewStep');
+      scope.$digest();
+
+      expect(controller).to.have.property('errors');
+
+      // error message should mentioned the conflicting field
+      expect(controller.errors[0]).to.contain('badarray');
+    });
+
+  });
 });
