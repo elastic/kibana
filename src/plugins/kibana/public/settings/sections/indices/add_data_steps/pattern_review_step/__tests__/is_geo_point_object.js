6b2e50388543d337c288ec4e6d8f733a543c676d
Matthew Bargar <mbargar@gmail.com>
2016-05-04 16:42:19 -0400

    Make isGeoPointObject return false instead of undefined when passed a plain object that's not a geo_point

--- src/plugins/kibana/public/settings/sections/indices/add_data_steps/pattern_review_step/__tests__/is_geo_point_object.js
+++ src/plugins/kibana/public/settings/sections/indices/add_data_steps/pattern_review_step/__tests__/is_geo_point_object.js
@@ -4,18 +4,18 @@ import expect from 'expect.js';
 describe('isGeoPointObject', function () {
 
   it('should return true if an object has lat and lon properties', function () {
-    expect(isGeoPointObject({lat: 38.6631, lon: -90.5771})).to.be.ok();
+    expect(isGeoPointObject({lat: 38.6631, lon: -90.5771})).to.be(true);
   });
 
   it('should return false if the value is not an object', function () {
-    expect(isGeoPointObject('foo')).to.not.be.ok();
-    expect(isGeoPointObject(1)).to.not.be.ok();
-    expect(isGeoPointObject(true)).to.not.be.ok();
-    expect(isGeoPointObject(null)).to.not.be.ok();
+    expect(isGeoPointObject('foo')).to.be(false);
+    expect(isGeoPointObject(1)).to.be(false);
+    expect(isGeoPointObject(true)).to.be(false);
+    expect(isGeoPointObject(null)).to.be(false);
   });
 
   it('should return false if the value is an object without lat an lon properties', function () {
-    expect(isGeoPointObject({foo: 'bar'})).to.not.be.ok();
+    expect(isGeoPointObject({foo: 'bar'})).to.be(false);
   });
 
 });
