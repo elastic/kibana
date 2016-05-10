4ddf80d5ef6d1238257b17b0bc0918373e02c80e
Rashid Khan <github.fliplap@spamgourmet.com>
2016-05-10 09:09:35 -0700

    add tests for subtract, sum and title

---
+++ series_functions/__test__/sum.js
@@ -0,0 +1,21 @@
+const filename = require('path').basename(__filename);
+const fn = require(`../${filename}`);
+
+const _ = require('lodash');
+const expect = require('chai').expect;
+const invoke = require('./helpers/invoke_series_fn.js');
+
+describe(filename, () => {
+
+  let seriesList;
+  beforeEach(() => {
+    seriesList = require('./fixtures/seriesList.js')();
+  });
+
+  it('it adds a number', () => {
+    return invoke(fn, [seriesList, 2]).then((r) => {
+      expect(_.map(r.output.list[1].data, 1)).to.eql([102, 52, 52, 22]);
+    });
+  });
+
+});
