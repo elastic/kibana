da0d4c18afef37885107558f72bfd380127b0911
Lukas Olson <olson.lukas@gmail.com>
2016-05-10 11:35:02 -0700

    [statusPage] Rename config option

--- src/server/status/wrap_auth_config.js
+++ src/server/status/wrap_auth_config.js
@@ -1,6 +1,6 @@
 import {assign, identity} from 'lodash';
 
-export default (disableAuth) => {
-  if (disableAuth) return options => assign(options, {config: {auth: false}});
+export default (allowAnonymous) => {
+  if (allowAnonymous) return options => assign(options, {config: {auth: false}});
   return identity;
 };
