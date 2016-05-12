22877bcbbf4097360a688d265b122fed876a377a
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-05-12 11:46:34 +0200

    [test] Added draggable container, item, and handles tests.

--- src/plugins/kibana/public/visualize/editor/draggable_item.js
+++ src/plugins/kibana/public/visualize/editor/draggable_item.js
@@ -4,21 +4,21 @@ import uiModules from 'ui/modules';
 uiModules
 .get('app/visualize')
 .directive('draggableItem', function () {
-
   return {
     restrict: 'A',
     require: '^draggableContainer',
     scope: true,
+    bindToController: true,
     controllerAs: 'draggableItemCtrl',
     controller($scope, $attrs, $parse) {
+      this.dragHandles = this.dragHandles || $();
       this.getItem = () => $parse($attrs.draggableItem)($scope);
       this.registerHandle = $el => {
-        $scope.dragHandles.push(...$el);
+        this.dragHandles.push(...$el);
       };
     },
     link($scope, $el, attr, ctrl) {
       $scope.dragHandles = $();
     }
   };
-
 });
