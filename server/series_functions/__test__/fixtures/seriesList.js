a3ca4b11326b14626991bf53ec00289b2ca99a7b
Rashid Khan <github.fliplap@spamgourmet.com>
2016-05-13 13:10:00 -0700

    clean up creation of seriesList in tests

--- server/series_functions/__test__/fixtures/seriesList.js
+++ server/series_functions/__test__/fixtures/seriesList.js
@@ -2,29 +2,23 @@ var Promise = require('bluebird');
 var buckets = require('./bucketList');
 var _ = require('lodash');
 
-var fill = _.partial(_.zip, _.map(buckets, function (bucket) { return bucket.valueOf(); }));
-
 module.exports = function () {
   return {
     type: 'seriesList',
-    list: [{
-      data:  fill([-51, 17, 82, 20]),
-      type: 'series',
-      label: 'Negative'
-    }, {
-      data: fill([100, 50, 50, 20]),
-      type: 'series',
-      label: 'Nice'
-    },
-    {
-      data:  fill([1, 1, 1, 1]),
-      type: 'series',
-      label: 'All the same'
-    },
-    {
-      data:  fill([3.1415926535, 2, 1.439, 0.3424235]),
-      type: 'series',
-      label: 'Decimauled'
-    }]
+    list: [
+      getSeries('Negative',     [-51, 17, 82, 20]),
+      getSeries('Nice',         [100, 50, 50, 20]),
+      getSeries('All the same', [1, 1, 1, 1]),
+      getSeries('Decimals',     [3.1415926535, 2, 1.439, 0.3424235])
+    ]
   };
 };
+
+function getSeries(name, points) {
+  var fill = _.partial(_.zip, _.map(buckets, function (bucket) { return bucket.valueOf(); }));
+  return {
+    data:  fill(points),
+    type: 'series',
+    label: name
+  };
+}
