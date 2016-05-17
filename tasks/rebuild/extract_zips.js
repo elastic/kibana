28aa6c9b9cc13ed8ec3c724c05e9ab2d390fec2a
Court Ewing <court@epixa.com>
2016-05-16 20:48:54 -0400

    [grunt] Task to rebuild archives with new version
    This is useful for when a pre-release build is completely tested and
    could be released as-is if it weren't for the pre-release suffix and
    commit hash. It will extract the archives in the target, replace the
    version, sha, and build numbers with the current working copy's, and
    then recreate the archives and shas.

---
+++ tasks/rebuild/extract_zips.js
@@ -0,0 +1,14 @@
+import { execFileSync } from 'child_process';
+
+export default (grunt) => {
+  grunt.registerTask('_rebuild:extractZips', function () {
+    const buildDir = grunt.config.get('build');
+    const targetDir = grunt.config.get('target');
+
+    const zips = grunt.file.expand({ cwd: targetDir }, '*.zip');
+
+    zips.forEach(zip => {
+      execFileSync('unzip', [zip, '-d', buildDir], { cwd: targetDir });
+    });
+  });
+};
