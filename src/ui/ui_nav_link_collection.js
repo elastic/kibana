4cbf70c57a7236eff8978141df8ac81e193a67f1
Shaunak Kashyap <ycombinator@gmail.com>
2016-05-17 18:38:28 -0700

    If value is deleted from the nav link collection, invalidate cache
    Otherwise, the next time inOrder is called, the stale nav link collection that is
    in the cache is returned, with the deleted object still in it.

--- src/ui/ui_nav_link_collection.js
+++ src/ui/ui_nav_link_collection.js
@@ -26,4 +26,9 @@ export default class UiNavLinkCollection extends Collection {
     return this[inOrderCache];
   }
 
+  delete(value) {
+    super.delete(value);
+    this[inOrderCache] = null;
+  }
+
 };
