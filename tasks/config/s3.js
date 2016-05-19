032de94fe2c9649bd4ccb610d3bb31433dfd4e01
Jonathan Budzenski <jbudz@users.noreply.github.com>
2016-05-19 13:16:11 -0500

    [build] Check for release files after build task

--- tasks/config/s3.js
+++ tasks/config/s3.js
@@ -1,5 +1,3 @@
-var _ = require('lodash');
-var fs = require('fs');
 module.exports = function (grunt) {
   var { config } = grunt;
 
@@ -7,35 +5,7 @@ module.exports = function (grunt) {
     release: {
       bucket: 'download.elasticsearch.org',
       access: 'private',
-      debug: false,
-      upload: config.get('platforms')
-      .reduce(function (files, platform) {
-        return files.concat(
-          platform.tarName,
-          platform.tarName + '.sha1.txt',
-          platform.zipName,
-          platform.zipName + '.sha1.txt',
-          platform.rpmName,
-          platform.rpmName && platform.rpmName + '.sha1.txt',
-          platform.debName,
-          platform.debName && platform.debName + '.sha1.txt'
-        );
-      }, [])
-      .filter(function (filename) {
-        if (_.isUndefined(filename)) return false;
-        try {
-          fs.accessSync('target/' + filename, fs.F_OK);
-          return true;
-        } catch (e) {
-          return false;
-        }
-      })
-      .map(function (filename) {
-        return {
-          src: 'target/' + filename,
-          dest: 'kibana/kibana/' + filename
-        };
-      })
+      debug: false
     }
   };
 };
--- tasks/release.js
+++ tasks/release.js
@@ -1,12 +1,15 @@
 module.exports = function (grunt) {
   var readline = require('readline');
   var url = require('url');
+  var fs = require('fs');
+  var _ = require('lodash');
 
   // build, then zip and upload to s3
   grunt.registerTask('release', [
     '_release:confirmUpload',
     '_release:loadS3Config',
     'build',
+    '_release:setS3Uploads',
     's3:release',
     '_release:complete'
   ]);
@@ -32,6 +35,38 @@ module.exports = function (grunt) {
       key: config.key,
       secret: config.secret
     });
+  });
+
+  grunt.registerTask('_release:setS3Uploads', function () {
+    var uploads = grunt.config.get('platforms')
+    .reduce(function (files, platform) {
+      return files.concat(
+        platform.tarName,
+        platform.tarName + '.sha1.txt',
+        platform.zipName,
+        platform.zipName + '.sha1.txt',
+        platform.rpmName,
+        platform.rpmName && platform.rpmName + '.sha1.txt',
+        platform.debName,
+        platform.debName && platform.debName + '.sha1.txt'
+      );
+    }, [])
+    .filter(function (filename) {
+      if (_.isUndefined(filename)) return false;
+      try {
+        fs.accessSync('target/' + filename, fs.F_OK);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    })
+    .map(function (filename) {
+      return {
+        src: 'target/' + filename,
+        dest: 'kibana/kibana/' + filename
+      };
+    });
+    grunt.config.set('s3.release.upload', uploads);
   });
 
   grunt.registerTask('_release:complete', function () {
