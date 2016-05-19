5bfa4be5cd484ee91ab6f90ff40850c80cd7d3a8
Court Ewing <court@epixa.com>
2016-05-19 11:13:19 -0400

    Backport PR #6676 ---------
    **Commit 1:**
    [build] sha1 packages
    
    * Original sha: ac2ae334fc91b56c89787389917ca5baaadc5ae5
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-03-09T21:03:09Z
    
    **Commit 2:**
    [build] Build i386
    
    * Original sha: b0dd6706d5e800d6ef0f0e6327196f910de732c9
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-03-09T21:40:16Z
    
    **Commit 3:**
    [build] Set group kibana
    
    * Original sha: 5b3e178976192c6c1d4464570afc33734b6b38e6
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-03-09T21:54:30Z
    
    **Commit 4:**
    [build] Add rpm, deb name and path
    
    * Original sha: 6041ff88521e4bb5681053c7cc3ad638697be2ef
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-03-09T22:45:55Z
    
    **Commit 5:**
    [build] Add tasks for publishing rpm, deb packages
    
    * Original sha: cde84ecb9a73a57cb9ebe90ea96c9cf44f7852a6
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-03-10T23:35:56Z
    
    **Commit 6:**
    [build] Rename publish to publishPackages
    
    * Original sha: da942d2f62f585a72778783ee786ba21a59f9781
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-03-29T14:32:18Z
    
    **Commit 7:**
    [build] Move signing config to a file
    
    * Original sha: 51e8633a4da30997c130f7cbadb42e2198e6b958
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-04-22T20:02:53Z
    
    **Commit 8:**
    [build] Pass env variables to rpm-s3
    
    * Original sha: 86269bca6d78ddc9d13cee9b7a766aa93154eebf
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-04-22T20:12:29Z
    
    **Commit 9:**
    [build] Keep process env variables, re-add done
    
    * Original sha: f3cd91a0d0ed6573fb9625b6208f287b7845d11b
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-04-27T22:23:13Z

---
+++ tasks/release_packages.js
@@ -0,0 +1,113 @@
+import exec from './utils/exec';
+import SimpleGit from 'simple-git';
+import { promisify } from 'bluebird';
+import readline from 'readline';
+
+export default (grunt) => {
+  const packages = grunt.config.get('packages');
+  const platforms = grunt.config.get('platforms');
+
+  function debS3(deb) {
+    exec('deb-s3', [
+      'upload',
+      '--preserve-versions',
+      deb.filePath,
+      '--bucket', deb.bucket,
+      '--prefix', deb.prefix,
+      '--sign', deb.signatureKeyId,
+      '--arch', deb.arch,
+      `--access-key-id=${deb.awsKey}`,
+      `--secret-access-key=${deb.awsSecret}`
+    ]);
+  }
+
+  function rpmS3(rpm) {
+    exec('rpm', [
+      '--resign', rpm.filePath,
+      '--define', '_signature gpg',
+      '--define', `_gpg_name ${rpm.signingKeyName}`
+    ]);
+
+    exec('rpm-s3', [
+      '-v',
+      '-b', rpm.bucket,
+      '-p', rpm.prefix,
+      '--sign',
+      '--visibility', 'public-read',
+      '-k', '100',
+      rpm.filePath,
+      '-r', 'external-1'
+    ], {
+      env: Object.assign({}, {
+        'AWS_ACCESS_KEY': rpm.awsKey,
+        'AWS_SECRET_KEY': rpm.awsSecret
+      }, process.env)
+    });
+  }
+
+  grunt.registerTask('publishPackages:staging', [
+    '_publishPackages:confirm',
+    '_publishPackages:upload:staging',
+  ]);
+
+  grunt.registerTask('publishPackages:production', [
+    '_publishPackages:confirm',
+    '_publishPackages:upload:production',
+  ]);
+
+  grunt.registerTask('_publishPackages:confirm', function () {
+    function abort() {
+      grunt.fail.fatal('Aborting publish');
+    }
+
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.on('close', this.async());
+    rl.on('SIGINT', () => abort());
+    rl.question('Publish packages to s3? [N/y] ', function (resp) {
+      if (resp.toLowerCase().trim()[0] === 'y') return rl.close();
+      abort();
+    });
+  });
+
+  grunt.registerTask('_publishPackages:upload', function (environment) {
+    const aws = grunt.file.readJSON('.aws-config.json');
+    const signature = grunt.file.readJSON('.signing-config.json');
+    const done = this.async();
+    const simpleGit = new SimpleGit();
+    const revparse = promisify(simpleGit.revparse, simpleGit);
+
+    return revparse(['--short', 'HEAD'])
+    .then(hash => {
+      const trimmedHash = hash.trim();
+      platforms.forEach((platform) => {
+        if (platform.debPath) {
+          debS3({
+            filePath: platform.debPath,
+            bucket: packages[environment].bucket,
+            prefix: packages[environment].debPrefix.replace('XXXXXXX', trimmedHash),
+            signatureKeyId: signature.id,
+            arch: platform.name.match('x64') ? 'amd64' : 'i386',
+            awsKey: aws.key,
+            awsSecret: aws.secret
+          });
+        }
+
+        if (platform.rpmPath) {
+          rpmS3({
+            filePath: platform.rpmPath,
+            bucket: packages[environment].bucket,
+            prefix: packages[environment].rpmPrefix.replace('XXXXXXX', trimmedHash),
+            signingKeyName: signature.name,
+            awsKey: aws.key,
+            awsSecret: aws.secret
+          });
+        }
+      });
+      done();
+    });
+  });
+};
