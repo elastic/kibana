e0b1be705ddbe187139646e294890e6e984dfb77
Jonathan Budzenski <jbudz@users.noreply.github.com>
2016-05-20 15:45:14 -0500

    Backport PR #7257 ---------
    **Commit 1:**
    [field formatters] IP should return - on null or undefined
    
    * Original sha: 959d9cf374d3d670a883ba99465920d666331737
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-05-20T18:34:57Z

--- src/ui/public/stringify/__tests__/_ip.js
+++ src/ui/public/stringify/__tests__/_ip.js
@@ -1,16 +1,21 @@
 describe('IP Address Format', function () {
-  let fieldFormats;
   let expect = require('expect.js');
   let ngMock = require('ngMock');
 
+  let ip;
   beforeEach(ngMock.module('kibana'));
   beforeEach(ngMock.inject(function (Private) {
-    fieldFormats = Private(require('ui/registry/field_formats'));
+    const fieldFormats = Private(require('ui/registry/field_formats'));
+    ip = fieldFormats.getInstance('ip');
   }));
 
   it('convers a value from a decimal to a string', function () {
-    let ip = fieldFormats.getInstance('ip');
     expect(ip.convert(1186489492)).to.be('70.184.100.148');
+  });
+
+  it('converts null and undefined to -',  function () {
+    expect(ip.convert(null)).to.be('-');
+    expect(ip.convert(undefined)).to.be('-');
   });
 
 });
