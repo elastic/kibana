6d293afbfb23ef81e716781b0dee54c24c0f2fdf
Jonathan Budzenski <jbudz@users.noreply.github.com>
2016-05-25 09:12:08 -0500

    [build] move install to /usr/share, config to /etc/kibana

--- tasks/build/pleaserun.js
+++ tasks/build/pleaserun.js
@@ -19,8 +19,16 @@ module.exports = function createServices(grunt) {
         '--sysv-log-path', '/var/log/kibana/',
         '-p', service.name,
         '-v', service.version,
-        '/opt/kibana/bin/kibana'
+        '/usr/share/kibana/bin/kibana',
+        '-c /etc/kibana/kibana.yml'
       ]);
     });
+<<<<<<< a9f1c863a78d71b9d350badac9d65a204898e566
+=======
+
+    grunt.file.mkdir(userScriptsDir);
+    exec('please-manage-user', ['--output', userScriptsDir, 'kibana']);
+    appendFileSync(resolve(userScriptsDir, 'installer.sh'), 'chown -R kibana:kibana /usr/share/kibana/optimize');
+>>>>>>> [build] move install to /usr/share, config to /etc/kibana
   });
 };
