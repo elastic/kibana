8f0be48768fb6bdff49cf509ff5fe84d540ee1ee
Jonathan Budzenski <jbudz@users.noreply.github.com>
2016-05-25 14:21:36 -0500

    [build] consolidate package config

--- tasks/build/os_packages.js
+++ tasks/build/os_packages.js
@@ -1,16 +1,16 @@
 module.exports = function (grunt) {
   const { resolve } = require('path');
   const { indexBy } = require('lodash');
-
   const { config } = grunt;
   const exec = require('../utils/exec');
+
   const targetDir = config.get('target');
-  const version = config.get('pkg.version');
   const packageScriptsDir = config.get('packageScriptsDir');
   const servicesByName = indexBy(config.get('services'), 'name');
+  const packageConfig = config.get('packages');
 
   grunt.registerTask('_build:osPackages', function () {
-    grunt.config.get('platforms').forEach(({ name, buildDir }) => {
+    config.get('platforms').forEach(({ name, buildDir }) => {
       // TODO(sissel): Check if `fpm` is available
       if (!(/linux-x(86|64)$/.test(name))) return;
 
@@ -21,28 +21,29 @@ module.exports = function (grunt) {
         '--force',
         '--package', targetDir,
         '-s', 'dir', // input type
-        '--name', 'kibana',
-        '--description', 'Explore\ and\ visualize\ your\ Elasticsearch\ data',
-        '--version', version,
-        '--url', 'https://www.elastic.co',
-        '--vendor', 'Elasticsearch,\ Inc.',
-        '--maintainer', 'Kibana Team\ \<info@elastic.co\>',
-        '--license', 'Apache\ 2.0',
+        '--name', packageConfig.name,
+        '--description', packageConfig.description,
+        '--version', packageConfig.version,
+        '--url', packageConfig.site,
+        '--vendor', packageConfig.vendor,
+        '--maintainer', packageConfig.maintainer,
+        '--license', packageConfig.license,
         '--after-install', resolve(packageScriptsDir, 'post_install.sh'),
         '--before-install', resolve(packageScriptsDir, 'pre_install.sh'),
         '--before-remove', resolve(packageScriptsDir, 'pre_remove.sh'),
         '--after-remove', resolve(packageScriptsDir, 'post_remove.sh'),
-        '--config-files', '/etc/kibana/kibana.yml',
+        '--config-files', packageConfig.path.kibanaConfig,
         '--template-value', 'user=kibana',
         '--template-value', 'group=kibana'
 
-        //uses relative path to --prefix
-        '--exclude', 'usr/share/kibana/config'
+        //config folder is moved to path.conf, exclude {path.home}/config
+        //uses relative path to --prefix, strip the leading /
+        '--exclude', `${packageConfig.path.home.slice(1)}/config`
       ];
 
       const files = [
-        `${buildDir}/=/usr/share/kibana/`,
-        `${buildDir}/config/=/etc/kibana/`,
+        `${buildDir}/=${packageConfig.path.home}/`,
+        `${buildDir}/config/=${packageConfig.path.conf}/`,
         `${servicesByName.sysv.outputDir}/etc/=/etc/`,
         `${servicesByName.systemd.outputDir}/lib/=/lib/`
       ];
