b0ed04cc71715723fcd3836045a9c98e9d094693
Jonathan Budzenski <jbudz@users.noreply.github.com>
2016-05-25 14:21:36 -0500

    [build] consolidate package config

--- tasks/build/pleaserun.js
+++ tasks/build/pleaserun.js
@@ -3,6 +3,9 @@ module.exports = function createServices(grunt) {
   const { appendFileSync } = require('fs');
   const exec = require('../utils/exec');
 
+  const userScriptsDir = grunt.config.get('userScriptsDir');
+  const { path, user, group } = grunt.config.get('packages');
+
   grunt.registerTask('_build:pleaseRun', function () {
     // TODO(sissel): Detect if 'pleaserun' is found, and provide a useful error
     // to the user if it is missing.
@@ -14,21 +17,14 @@ module.exports = function createServices(grunt) {
         '--no-install-actions',
         '--install-prefix', service.outputDir,
         '--overwrite',
-        '--user', 'kibana',
-        '--group', 'kibana',
-        '--sysv-log-path', '/var/log/kibana/',
+        '--user', user,
+        '--group', group,
+        '--sysv-log-path', path.logs,
         '-p', service.name,
         '-v', service.version,
-        '/usr/share/kibana/bin/kibana',
-        '-c /etc/kibana/kibana.yml'
+        path.kibanaBin,
+        `-c ${path.kibanaConfig}`
       ]);
     });
-<<<<<<< a9f1c863a78d71b9d350badac9d65a204898e566
-=======
-
-    grunt.file.mkdir(userScriptsDir);
-    exec('please-manage-user', ['--output', userScriptsDir, 'kibana']);
-    appendFileSync(resolve(userScriptsDir, 'installer.sh'), 'chown -R kibana:kibana /usr/share/kibana/optimize');
->>>>>>> [build] move install to /usr/share, config to /etc/kibana
   });
 };
