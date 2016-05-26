3a7ae151ddce3901926eec02ff047663b6a37833
Rashid Khan <github.fliplap@spamgourmet.com>
2016-05-26 14:53:23 -0700

    Fix exceptions about crosshairs

--- public/directives/chart/chart.js
+++ public/directives/chart/chart.js
@@ -108,11 +108,13 @@ app.directive('chart', function ($compile, $rootScope, timefilter, $timeout, Pri
       });
 
       $scope.$on('timelionPlotHover', function (angularEvent, flotEvent, pos, time) {
+        if (!$scope.plot) return;
         $scope.plot.setCrosshair(pos);
         debouncedSetLegendNumbers(pos);
       });
 
       $scope.$on('timelionPlotLeave', function (angularEvent, flotEvent, pos, time) {
+        if (!$scope.plot) return;
         $scope.plot.clearCrosshair();
         clearLegendNumbers();
       });
