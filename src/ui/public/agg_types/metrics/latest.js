22bab6246e21820094aacc5c17c2db191235fe97
Stéphane Campinas <stephane.campinas@gmail.com>
2016-05-26 23:02:08 +0100

    add top_hits aggregation

---
+++ src/ui/public/agg_types/metrics/latest.js
@@ -0,0 +1,42 @@
+import AggTypesMetricsMetricAggTypeProvider from 'ui/agg_types/metrics/metric_agg_type';
+import latestEditor from 'ui/agg_types/controls/latest.html';
+
+export default function AggTypeMetricAvgProvider(Private) {
+  let MetricAggType = Private(AggTypesMetricsMetricAggTypeProvider);
+
+  return new MetricAggType({
+    name: 'top_hits',
+    title: 'Latest',
+    makeLabel: function (aggConfig) {
+      return 'Latest ' + aggConfig.params.field.displayName;
+    },
+    params: [
+      {
+        name: 'field',
+        write(agg, output) {
+          output.params = {
+            size: 1,
+            _source: agg.params.field.name
+          };
+        }
+      },
+      {
+        name: 'sort',
+        type: 'field',
+        editor: latestEditor,
+        write(agg, output) {
+          output.params.sort = [
+            {
+              [ agg.params.sort.name ]: {
+                order: 'desc'
+              }
+            }
+          ];
+        }
+      }
+    ],
+    getValue(agg, bucket) {
+      return bucket[agg.id].hits.hits[0]._source[agg.params.field.name];
+    }
+  });
+};
