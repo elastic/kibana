584e19aeea692afa2abe487249c32ada363d2653
Chris Earle <chris.earle@elastic.co>
2016-05-28 18:23:09 -0400

    Add version to status output
    This adds a version field to all Status output, including for built-in plugins.
    
    - It adds the version to the Status API and Status page.
    - It adds the version to plugin status change logging.
    - It drops the "plugin:" prefix from all status output (as it's implied), except the log output.

--- src/server/status/__tests__/status.js
+++ src/server/status/__tests__/status.js
@@ -4,6 +4,8 @@ import Status from '../status';
 import ServerStatus from '../server_status';
 
 describe('Status class', function () {
+  const plugin = {id: 'test', version: '1.2.3'};
+
   let server;
   let serverStatus;
 
@@ -13,11 +15,11 @@ describe('Status class', function () {
   });
 
   it('should have an "uninitialized" state initially', function () {
-    expect(serverStatus.create('test')).to.have.property('state', 'uninitialized');
+    expect(serverStatus.create(plugin)).to.have.property('state', 'uninitialized');
   });
 
   it('emits change when the status is set', function (done) {
-    let status = serverStatus.create('test');
+    let status = serverStatus.create(plugin);
 
     status.once('change', function (prev, prevMsg) {
       expect(status.state).to.be('green');
@@ -40,7 +42,7 @@ describe('Status class', function () {
   });
 
   it('should only trigger the change listener when something changes', function () {
-    let status = serverStatus.create('test');
+    let status = serverStatus.create(plugin);
     let stub = sinon.stub();
     status.on('change', stub);
     status.green('Ready');
@@ -50,16 +52,18 @@ describe('Status class', function () {
   });
 
   it('should create a JSON representation of the status', function () {
-    let status = serverStatus.create('test');
+    let status = serverStatus.create(plugin);
     status.green('Ready');
 
     let json = status.toJSON();
+    expect(json.name).to.eql(plugin.id);
+    expect(json.version).to.eql(plugin.version);
     expect(json.state).to.eql('green');
     expect(json.message).to.eql('Ready');
   });
 
   it('should call on handler if status is already matched', function (done) {
-    let status = serverStatus.create('test');
+    let status = serverStatus.create(plugin);
     let msg = 'Test Ready';
     status.green(msg);
 
@@ -73,7 +77,7 @@ describe('Status class', function () {
   });
 
   it('should call once handler if status is already matched', function (done) {
-    let status = serverStatus.create('test');
+    let status = serverStatus.create(plugin);
     let msg = 'Test Ready';
     status.green(msg);
 
@@ -88,7 +92,7 @@ describe('Status class', function () {
 
   function testState(color) {
     it(`should change the state to ${color} when #${color}() is called`, function () {
-      let status = serverStatus.create('test');
+      let status = serverStatus.create(plugin);
       let message = 'testing ' + color;
       status[color](message);
       expect(status).to.have.property('state', color);
@@ -96,7 +100,7 @@ describe('Status class', function () {
     });
 
     it(`should trigger the "change" listner when #${color}() is called`, function (done) {
-      let status = serverStatus.create('test');
+      let status = serverStatus.create(plugin);
       let message = 'testing ' + color;
       status.on('change', function (prev, prevMsg) {
         expect(status.state).to.be(color);
@@ -110,7 +114,7 @@ describe('Status class', function () {
     });
 
     it(`should trigger the "${color}" listner when #${color}() is called`, function (done) {
-      let status = serverStatus.create('test');
+      let status = serverStatus.create(plugin);
       let message = 'testing ' + color;
       status.on(color, function (prev, prevMsg) {
         expect(status.state).to.be(color);
