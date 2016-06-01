07e56861692eec83dc719fd8c17d736ed9eff042
LeeDr <lee.drengenberg@elastic.co>
2016-06-01 13:09:44 -0500

    Use elasticdump to preload default index for visualization tests.

--- test/functional/apps/visualize/index.js
+++ test/functional/apps/visualize/index.js
@@ -1,4 +1,12 @@
-import { bdd, remote, common, defaultTimeout, scenarioManager } from '../../../support';
+import {
+  bdd,
+  remote,
+  common,
+  defaultTimeout,
+  scenarioManager,
+  esClient,
+  elasticDump
+ } from '../../../support';
 
 (function () {
   bdd.describe('visualize app', function () {
@@ -7,14 +15,24 @@ import { bdd, remote, common, defaultTimeout, scenarioManager } from '../../../s
     bdd.before(function () {
       var self = this;
       remote.setWindowSize(1200,800);
-      // load a set of makelogs data
-      common.debug('loadIfEmpty logstashFunctional ' + self.timeout);
-      return scenarioManager.loadIfEmpty('logstashFunctional');
-    });
 
-
-    bdd.after(function unloadMakelogs() {
-      return scenarioManager.unload('logstashFunctional');
+      common.debug('Starting visualize before method');
+      // Calling loadIfEmpty but not chaining the response.  We can start the
+      // testing before this finishes.
+      // The first test is _chart_types which does not require the data to be loaded.
+      // Loading this data takes about 10 seconds.
+      scenarioManager.loadIfEmpty('logstashFunctional');
+      return esClient.delete('.kibana')
+      .then(function () {
+        return common.try(function () {
+          return esClient.updateConfigDoc({'dateFormat:tz':'UTC', 'defaultIndex':'logstash-*'});
+        });
+      })
+      .then(function loadkibanaIndexPattern() {
+        common.debug('load kibana index with default index pattern');
+        return elasticDump.elasticLoad('visualize','.kibana');
+      })
+      .catch(common.handleError(this));
     });
 
     require('./_chart_types');
