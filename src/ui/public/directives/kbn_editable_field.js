14e6137d2bfec7ac050eee490f9000a6b653d2d7
Khalah Jones Golden <khalah@elastic.co>
2016-06-06 21:06:51 -0400

    [KbnEditField] Refactor to move all operations to a controller

--- src/ui/public/directives/kbn_editable_field.js
+++ src/ui/public/directives/kbn_editable_field.js
@@ -21,76 +21,93 @@ module.directive('kbnEditField', ['$parse', function ($parse) {
           <span><i class="fa fa-check"></i></span>
           <span><i class="fa fa-remove"></i></span>
           <span><i class="fa fa-undo"></i></span>`,
-    // require: 'ngModel',
-    link: function ($scope, $element, attrs, ngModelController) {
-      const fieldKey = attrs.kbnEditField;
-      const $inputEl = $element.children('input');
+    controller: function ($scope, $element, $attrs) {
+      const fieldKey = $attrs.kbnEditField;
+      // On external change show the user
+      $scope.$watch(fieldKey, function (newVal) { setInput(newVal); });
 
+      const $inputEl = $element.children('input');
       // Inspired by
       // https://github.com/angular/angular.js/blob/master/src/ng/directive/ngModel.js#L245
-      const parsedModel = $parse(attrs.kbnEditField);
+      const parsedModel = $parse(fieldKey);
       const getVal = parsedModel;
       const setVal = parsedModel.assign;
-      const initialValue = attrs.initialVal || getModelVal();
+
+      function toggleEditClass() { $element.toggleClass('editing', getModelVal() !== $inputEl.val()); }
+      // Get the val
+      function getModelVal() { return getVal($scope); }
+      // Sync the veiwVal, and what is.
+      function setInput(val) { $inputEl.val(val); }
+      // Set the model value
+      function setModel(val) { setVal($scope, val); }
+
+      this.model = function (val) {
+        const retVal = getModelVal();
+        if (val) { setModel(val); }
+        return retVal;
+      };
+      this.input = function (val) {
+        const retVal = $inputEl.val();
+        if (val) { setInput(val); }
+        return retVal;
+      };
+      this.blurInput = function () { $inputEl.blur(); };
+      this.toggleEditClass = function () { $element.toggleClass('editing', getModelVal() !== $inputEl.val()); };
+    },
+    link: function ($scope, $element, attrs, kbnEditFieldCtrl) {
+      // Add necessary classesto the markup
+      $element.addClass('kbn-edit-field');
+      const $inputEl = $element.children('input');
+      const ctrl = kbnEditFieldCtrl;
+
       $inputEl.on('keyup', evt => {
         const isEnterKey = evt.keyCode === 13;
         const isEscKey = evt.keyCode === 27;
         if (isEnterKey) {
           // This is much slower in setting than the click, not really sure why honestly
           safeSetModel();
-        } else if (isEscKey) {
-          setInput();
+        } else if (isEscKey) { // reset the input
+          ctrl.input(ctrl.model());
         }
 
         if (isEnterKey || isEscKey) {
-          blurInput();
+          ctrl.blurInput();
         }
-        toggleEditClass();
+        ctrl.toggleEditClass();
       });
       // click on the check button
       $element.find('.fa-check').click(() => {
         safeSetModel();
-        toggleEditClass();
-        blurInput();
+        ctrl.toggleEditClass();
+        ctrl.blurInput();
       });
+
       // Click on the undo button
+      const initialValue = attrs.initialVal || ctrl.model();
       $element.find('.fa-undo').click(() => {
         safeSetModel(initialValue);
-        setInput(initialValue);
-        toggleEditClass();
+        ctrl.input(initialValue);
+        ctrl.toggleEditClass();
       });
-      // click on the x, button
+
+      // click on the x, button; same as ESC
       $element.find('.fa-remove').click(() => {
-        setInput();
-        toggleEditClass();
+        ctrl.input(ctrl.model());
+        ctrl.toggleEditClass();
       });
-      // On external change show the user
-      $scope.$watch(fieldKey, function (newVal) { setInput(newVal); });
 
-
-      function toggleEditClass() {
-        $element.toggleClass('editing', getModelVal() !== $inputEl.val());
-      }
-      function safeSetModel(val = $inputEl.val()) {
+      function safeSetModel(val = ctrl.input()) {
         const safeVal = trimWhiteSpace(val);
         if (safeVal.length) {
-          setModel(safeVal);
+          ctrl.model(safeVal);
           if (safeVal !== val) { // If trimWhiteSpace had to, you know, trimWhiteSpace
-            setInput(safeVal);
+            ctrl.input(safeVal);
           }
         } else {
-          setInput();
+          ctrl.input(ctrl.model());
         }
       }
-      function blurInput() {
-        $inputEl.blur();
-      }
 
-      function getModelVal() { return getVal($scope); }
-      // Sync the veiwVal, and what is.
-      function setInput(val = getModelVal()) { $inputEl.val(val); }
-      // Set the model value
-      function setModel(val) { setVal($scope, val); }
     }
   };
 }]);
