0ca46d897c52213dec93bec1ccdbbcbc2ffb8c32
Shaunak Kashyap <ycombinator@gmail.com>
2016-06-14 23:26:31 -0700

    Ignore the extra log line about the settings status

--- src/cli/serve/__tests__/reload_logging_config.js
+++ src/cli/serve/__tests__/reload_logging_config.js
@@ -66,7 +66,7 @@ ${err.stack || err.message || err}`).to.eql(true);
     }
 
     function switchToPlainTextLog() {
-      json = 2; // ignore both "reloading" messages
+      json = 3; // ignore both "reloading" messages
       setLoggingJson(false);
       child.kill(`SIGHUP`); // reload logging config
     }
