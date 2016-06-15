431f2fa4de2ed2d5d5586c74a2718ed43ff34d4c
Elastic Jasper <elastic-jasper@users.noreply.github.com>
2016-06-15 15:22:46 -0400

    Backport PR #7202 ---------
    **Commit 1:**
    [build] Release packages to s3
    
    * Original sha: d0c835d1722ab2f80e1fdcb4f394d65661381ec4
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-05-13T16:45:41Z
    
    **Commit 2:**
    [build] Check for release files after build task
    
    * Original sha: 032de94fe2c9649bd4ccb610d3bb31433dfd4e01
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-05-19T18:16:11Z

--- tasks/config/s3.js
+++ tasks/config/s3.js
@@ -1,4 +1,3 @@
-var _ = require('lodash');
 module.exports = function (grunt) {
   var { config } = grunt;
 
@@ -6,22 +5,7 @@ module.exports = function (grunt) {
     release: {
       bucket: 'download.elasticsearch.org',
       access: 'private',
-      debug: false,
-      upload: config.get('platforms')
-      .reduce(function (files, platform) {
-        return files.concat(
-          platform.tarName,
-          platform.tarName + '.sha1.txt',
-          platform.zipName,
-          platform.zipName + '.sha1.txt'
-        );
-      }, [])
-      .map(function (filename) {
-        return {
-          src: 'target/' + filename,
-          dest: 'kibana/kibana/' + filename
-        };
-      })
+      debug: false
     }
   };
 };
