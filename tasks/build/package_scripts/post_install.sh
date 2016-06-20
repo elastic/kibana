b54ef4ed0010a61c56f9d07dfa6792bab970746c
Jonathan Budzenski <jbudz@users.noreply.github.com>
2016-06-20 10:27:36 -0500

    [build] Ensure group kibana is added, stricter user creation

--- tasks/build/package_scripts/post_install.sh
+++ tasks/build/package_scripts/post_install.sh
@@ -1,19 +1,39 @@
 #!/bin/sh
 set -e
 
-user_check() {
-  getent passwd "$1" > /dev/null 2>&1
-}
+case $1 in
+  # Debian
+  configure)
+    if ! getent group "<%= group %>" >/dev/null; then
+      addgroup --quiet --system "<%= group %>"
+    fi
 
-user_create() {
-  # Create a system user. A system user is one within the system uid range and
-  # has no expiration
-  useradd -r "$1"
-}
+    if ! getent passwd "<%= user %>" >/dev/null; then
+      adduser --quiet --system --no-create-home --disabled-password \
+      --ingroup "<%= group %>" --shell /bin/false "<%= user %>"
+    fi
+  ;;
+  abort-deconfigure|abort-upgrade|abort-remove)
+  ;;
 
-if ! user_check "<%= user %>" ; then
-  user_create "<%= user %>"
-fi
+  # Red Hat
+  1|2)
+    if ! getent group "<%= group %>" >/dev/null; then
+      groupadd -r "<%= group %>"
+    fi
+
+    if ! getent passwd "<%= user %>" >/dev/null; then
+      useradd -r -g "<%= group %>" -M -s /sbin/nologin \
+      -c "kibana service user" "<%= user %>"
+    fi
+  ;;
+
+  *)
+      echo "post install script called with unknown argument \`$1'" >&2
+      exit 1
+  ;;
+esac
+
 chown -R <%= user %>:<%= group %> <%= optimizeDir %>
 chown <%= user %>:<%= group %> <%= dataDir %>
 chown <%= user %>:<%= group %> <%= pluginsDir %>
