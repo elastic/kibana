cec3c1e816a74af65476ff1c127e6d10ec485a95
CJ Cenizal <cj@cenizal.com>
2016-06-27 15:39:40 -0700

    Migrate management functional tests to use PageObjects.

--- test/functional/apps/management/_creation_form_changes.js
+++ test/functional/apps/management/_creation_form_changes.js
@@ -1,28 +1,29 @@
+
+import expect from 'expect.js';
+
 import {
   bdd,
-  common,
-  settingsPage,
   scenarioManager,
   esClient
 } from '../../../support';
 
-var expect = require('expect.js');
+import PageObjects from '../../../support/page_objects';
 
 bdd.describe('user input reactions', function () {
   bdd.beforeEach(function () {
     // delete .kibana index and then wait for Kibana to re-create it
     return esClient.deleteAndUpdateConfigDoc()
     .then(function () {
-      return settingsPage.navigateTo();
+      return PageObjects.settings.navigateTo();
     })
     .then(function () {
-      return settingsPage.clickExistingData();
+      return PageObjects.settings.clickExistingData();
     });
   });
 
   bdd.it('should hide time-based index pattern when time-based option is unchecked', function () {
     var self = this;
-    return settingsPage.getTimeBasedEventsCheckbox()
+    return PageObjects.settings.getTimeBasedEventsCheckbox()
     .then(function (selected) {
       // uncheck the 'time-based events' checkbox
       return selected.click();
@@ -30,12 +31,12 @@ bdd.describe('user input reactions', function () {
     // try to find the checkbox (this shouldn fail)
     .then(function () {
       var waitTime = 10000;
-      return settingsPage.getTimeBasedIndexPatternCheckbox(waitTime);
+      return PageObjects.settings.getTimeBasedIndexPatternCheckbox(waitTime);
     })
     .then(function () {
-      common.saveScreenshot('Settings-indices-hide-time-based-index-pattern');
+      PageObjects.common.saveScreenshot('Settings-indices-hide-time-based-index-pattern');
       // we expect the promise above to fail
-      var handler = common.createErrorHandler(self);
+      var handler = PageObjects.common.createErrorHandler(self);
       var msg = 'Found time based index pattern checkbox';
       handler(msg);
     })
@@ -47,11 +48,11 @@ bdd.describe('user input reactions', function () {
 
   bdd.it('should enable creation after selecting time field', function () {
     // select a time field and check that Create button is enabled
-    return settingsPage.selectTimeFieldOption('@timestamp')
+    return PageObjects.settings.selectTimeFieldOption('@timestamp')
     .then(function () {
-      return settingsPage.getCreateButton().isEnabled()
+      return PageObjects.settings.getCreateButton().isEnabled()
       .then(function (enabled) {
-        common.saveScreenshot('Settings-indices-enable-creation');
+        PageObjects.common.saveScreenshot('Settings-indices-enable-creation');
         expect(enabled).to.be.ok();
       });
     });
