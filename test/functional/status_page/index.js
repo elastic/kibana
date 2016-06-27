10ff5c6212afb96d8adeeae71b43f979feb04523
CJ Cenizal <cj@cenizal.com>
2016-06-27 13:14:10 -0700

    Migrate status page functional tests to use PageObjects.

--- test/functional/status_page/index.js
+++ test/functional/status_page/index.js
@@ -1,26 +1,27 @@
 import {
   bdd,
-  common
 } from '../../support';
+
+import PageObjects from '../../support/page_objects';
 
 var expect = require('expect.js');
 
 bdd.describe('status page', function () {
   bdd.before(function () {
-    return common.navigateToApp('status_page', false);
+    return PageObjects.common.navigateToApp('status_page', false);
   });
 
   bdd.it('should show the kibana plugin as ready', function () {
     var self = this;
 
-    return common.tryForTime(6000, function () {
-      return common.findTestSubject('statusBreakdown')
+    return PageObjects.common.tryForTime(6000, function () {
+      return PageObjects.common.findTestSubject('statusBreakdown')
       .getVisibleText()
       .then(function (text) {
-        common.saveScreenshot('Status');
+        PageObjects.common.saveScreenshot('Status');
         expect(text.indexOf('plugin:kibana')).to.be.above(-1);
       });
     })
-    .catch(common.createErrorHandler(self));
+    .catch(PageObjects.common.createErrorHandler(self));
   });
 });
