cb0659f98df5553023bd2d50decb787d924cfb34
spalger <email@spalger.com>
2016-06-30 12:50:56 -0700

    [ci] move ci setup tasks into source control
    Until now, the ci setup tasks were stored in the Jenkins job configuration. This moves the setup into source control so that jenkins can be told to execute `./test/scripts/jenkins_{JOB_TYPE}.sh` no matter what branch of kibana it is dealing with.

---
+++ test/scripts/jenkins_setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+###*****###
+### CWD ###
+###*****###
+if [ -n "$WORKSPACE" ]; then
+  cd "$WORKSPACE"
+else
+  echo 'Unable to find the $WORKSPACE environment variable, is this jenkins?'
+  exit 1
+fi
+
+
+###*****###
+### NVM ###
+###*****###
+export NVM_DIR="/var/lib/jenkins/.nvm"
+NVM_SCRIPT="$NVM_DIR/nvm.sh"
+if [ -s "$NVM_SCRIPT" ]; then
+  . "$NVM_SCRIPT"  # load nvm
+else
+  echo "Unable to find the nvm script at \"$NVM_SCRIPT\""
+  exit 1
+fi
+
+nvm install "$(cat .node-version)"
+
+###*****###
+### NPM ###
+###*****###
+npm install
