eaa35b62f545563f7d65d3c15776898814d545f1
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-01 11:54:01 -0700

    Adding unit tests for default paths involving various properties

--- src/ui/__tests__/ui_nav_link.js
+++ src/ui/__tests__/ui_nav_link.js
@@ -24,7 +24,66 @@ describe('UiNavLink', () => {
       expect(link.url).to.be(`${uiExports.urlBasePath}${spec.url}`);
       expect(link.description).to.be(spec.description);
       expect(link.icon).to.be(spec.icon);
+    });
 
+    it ('initializes the url property without a base path when one is not specified in the spec', () => {
+      const uiExports = {};
+      const spec = {
+        id: 'kibana:discover',
+        title: 'Discover',
+        order: -1003,
+        url: '/app/kibana#/discover',
+        description: 'interactively explore your data',
+        icon: 'plugins/kibana/assets/discover.svg',
+      };
+      const link = new UiNavLink(uiExports, spec);
+
+      expect(link.url).to.be(spec.url);
+    });
+
+    it ('initializes the order property to 0 when order is not specified in the spec', () => {
+      const uiExports = {};
+      const spec = {
+        id: 'kibana:discover',
+        title: 'Discover',
+        url: '/app/kibana#/discover',
+        description: 'interactively explore your data',
+        icon: 'plugins/kibana/assets/discover.svg',
+      };
+      const link = new UiNavLink(uiExports, spec);
+
+      expect(link.order).to.be(0);
+    });
+
+    it ('initializes the linkToLastSubUrl property to false when false is specified in the spec', () => {
+      const uiExports = {};
+      const spec = {
+        id: 'kibana:discover',
+        title: 'Discover',
+        order: -1003,
+        url: '/app/kibana#/discover',
+        description: 'interactively explore your data',
+        icon: 'plugins/kibana/assets/discover.svg',
+        linkToLastSubUrl: false
+      };
+      const link = new UiNavLink(uiExports, spec);
+
+      expect(link.linkToLastSubUrl).to.be(false);
+    });
+
+    it ('initializes the linkToLastSubUrl property to true by default', () => {
+      const uiExports = {};
+      const spec = {
+        id: 'kibana:discover',
+        title: 'Discover',
+        order: -1003,
+        url: '/app/kibana#/discover',
+        description: 'interactively explore your data',
+        icon: 'plugins/kibana/assets/discover.svg',
+      };
+      const link = new UiNavLink(uiExports, spec);
+
+      expect(link.linkToLastSubUrl).to.be(true);
     });
   });
 
