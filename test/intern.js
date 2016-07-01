a8c1b4e8049cc22ba3dda9a79006bfb1a9455548
LeeDr <lee.drengenberg@elastic.co>
2016-07-01 17:40:01 -0500

    Try to fix another staleElementReference in data table test

--- test/intern.js
+++ test/intern.js
@@ -3,8 +3,6 @@ define(function (require) {
   return Object.assign({
     debug: true,
     capabilities: {
-      'selenium-version': '2.53.0',
-      // must match URL in tasks/config/downloadSelenium.js
       'idle-timeout': 99
     },
     environments: [{
