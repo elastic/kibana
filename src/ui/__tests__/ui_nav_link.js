bb45a390f8801301feaa7860a738a44c9152ad0e
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-01 06:58:53 -0700

    Adding unit test for toJSON()

---
+++ src/ui/__tests__/ui_nav_link.js
@@ -0,0 +1,27 @@
+import expect from 'expect.js';
+
+import UiNavLink from '../ui_nav_link';
+
+describe('UiNavLink', () => {
+  describe('#toJSON', () => {
+    it ('returns the expected properties', () => {
+      const uiExports = {
+        urlBasePath: 'http://localhost:5601/rnd/'
+      };
+      const spec = {
+        id: 'kibana:discover',
+        title: 'Discover',
+        order: -1003,
+        url: '/app/kibana#/discover',
+        description: 'interactively explore your data',
+        icon: 'plugins/kibana/assets/discover.svg',
+      };
+      const link = new UiNavLink(uiExports, spec);
+      const json = link.toJSON();
+
+      ['id', 'title', 'url', 'order', 'description', 'icon', 'linkToLastSubUrl'].forEach(expectedProperty => {
+        expect(json).to.have.property(expectedProperty);
+      });
+    });
+  });
+});
