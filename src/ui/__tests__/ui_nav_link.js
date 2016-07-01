7e931bd3a3a0899610bee66d7f3640da581adb6d
Shaunak Kashyap <ycombinator@gmail.com>
2016-07-01 07:03:39 -0700

    Adding unit test for constructor

--- src/ui/__tests__/ui_nav_link.js
+++ src/ui/__tests__/ui_nav_link.js
@@ -3,6 +3,31 @@ import expect from 'expect.js';
 import UiNavLink from '../ui_nav_link';
 
 describe('UiNavLink', () => {
+  describe('constructor', () => {
+    it ('initializes the object properties as expected', () => {
+      const uiExports = {
+        urlBasePath: 'http://localhost:5601/rnd'
+      };
+      const spec = {
+        id: 'kibana:discover',
+        title: 'Discover',
+        order: -1003,
+        url: '/app/kibana#/discover',
+        description: 'interactively explore your data',
+        icon: 'plugins/kibana/assets/discover.svg',
+      };
+      const link = new UiNavLink(uiExports, spec);
+
+      expect(link.id).to.be(spec.id);
+      expect(link.title).to.be(spec.title);
+      expect(link.order).to.be(spec.order);
+      expect(link.url).to.be(uiExports.urlBasePath + spec.url);
+      expect(link.description).to.be(spec.description);
+      expect(link.icon).to.be(spec.icon);
+
+    });
+  });
+
   describe('#toJSON', () => {
     it ('returns the expected properties', () => {
       const uiExports = {
