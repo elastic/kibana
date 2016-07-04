b08f016cc6af2e96b43d8a02fa4f1e33e05e9b06
CJ Cenizal <cj@cenizal.com>
2016-07-04 15:29:40 -0700

    Replace compiled angular-bootstrap file with original HTML and JS source files.

--- webpackShims/ui-bootstrap.js
+++ webpackShims/ui-bootstrap.js
@@ -1,6 +1,6 @@
 define(function (require) {
   require('angular');
-  require('ui/angular-bootstrap/ui-bootstrap-tpls');
+  require('ui/angular-bootstrap/index');
 
   return require('ui/modules')
   .get('kibana', ['ui.bootstrap'])
