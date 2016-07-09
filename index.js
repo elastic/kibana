cd6d5ff5f7c3f2f7e28b747027a97d17d247fe09
Rashid Khan <github.fliplap@spamgourmet.com>
2016-07-08 17:15:03 -0700

    Add hacks

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ module.exports = function (kibana) {
           };
         }
       },
+      hacks: [
+        'plugins/timelion/lib/panel_registry',
+        'plugins/timelion/panels/timechart'
+      ],
       visTypes: [
         'plugins/timelion/vis'
       ],
