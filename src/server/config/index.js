af33bc2b35abd93c83070a75dd6c27f322724c1f
Court Ewing <court@epixa.com>
2016-07-13 11:35:41 -0400

    Default tilemap config values and pass version to service

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -16,6 +16,14 @@ function checkPath(path) {
   }
 }
 
+var packagePath = path.resolve(__dirname, '..', 'package.json');
+try {
+  fs.statSync(packagePath);
+} catch (err) {
+  packagePath = path.resolve(__dirname, '..', '..', '..', 'package.json');
+}
+var pkg = require(packagePath);
+
 // Set defaults for config file stuff
 kibana.port = kibana.port || 5601;
 kibana.host = kibana.host || '0.0.0.0';
@@ -23,6 +31,12 @@ kibana.elasticsearch_url = kibana.elasticsearch_url || 'http://localhost:9200';
 kibana.maxSockets = kibana.maxSockets || Infinity;
 kibana.log_file = kibana.log_file || null;
 kibana.xsrf_token = kibana.xsrf_token || 'kibana';
+
+kibana.tilemap_url = kibana.tilemap_url || 'https://tiles.elastic.co/v1/default/{z}/{x}/{y}.png?my_app_name=kibana&my_app_version=' + pkg.version + '&elastic_tile_service_tos=agree'; // jscs:disable
+kibana.tilemap_min_zoom = kibana.tilemap_min_zoom || 0;
+kibana.tilemap_max_zoom = kibana.tilemap_max_zoom || 7;
+kibana.tilemap_attribution = kibana.tilemap_attribution || '© [Elastic Tile Service](https://www.elastic.co/elastic-tile-service)';
+kibana.tilemap_subdomains = kibana.tilemap_subdomains || [];
 
 kibana.request_timeout = kibana.startup_timeout == null ? 0 : kibana.request_timeout;
 kibana.ping_timeout = kibana.ping_timeout == null ? kibana.request_timeout : kibana.ping_timeout;
@@ -38,13 +52,6 @@ var htpasswdPath = path.resolve(__dirname, '..', '.htpasswd');
 if (!checkPath(htpasswdPath)) htpasswdPath = path.resolve(__dirname, '..', '..', '..', '.htpasswd');
 if (!checkPath(htpasswdPath)) htpasswdPath = false;
 
-var packagePath = path.resolve(__dirname, '..', 'package.json');
-try {
-  fs.statSync(packagePath);
-} catch (err) {
-  packagePath = path.resolve(__dirname, '..', '..', '..', 'package.json');
-}
-
 var config = module.exports = {
   port                    : kibana.port,
   host                    : kibana.host,
@@ -55,7 +62,7 @@ var config = module.exports = {
   external_plugins_folder : process.env.PLUGINS_FOLDER || null,
   bundled_plugins_folder  : path.resolve(public_folder, 'plugins'),
   kibana                  : kibana,
-  package                 : require(packagePath),
+  package                 : pkg,
   htpasswd                : htpasswdPath,
   buildNum                : '@@buildNum',
   maxSockets              : kibana.maxSockets,
