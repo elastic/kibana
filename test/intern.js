ef4eae08a3b92912fdef735139a5f09f384f9ec0
spalger <email@spalger.com>
2016-07-25 09:38:04 -0700

    [ci] move from Firefox to Chrome

--- test/intern.js
+++ test/intern.js
@@ -9,7 +9,7 @@ define(function (require) {
       'idle-timeout': 99
     },
     environments: [{
-      browserName: 'firefox'
+      browserName: 'chrome'
     }],
     tunnelOptions: serverConfig.servers.webdriver,
     functionalSuites: [
