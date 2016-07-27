365096b05aaef59cd149d6be833dfd84ad5e9330
Jonathan Budzenski <jon@jbudz.me>
2016-07-27 15:12:01 -0500

    Backport PR #7855 ---------
    **Commit 1:**
    Bump marked
    
    * Original sha: 7cf64c75a5d730798b66e50b52332ed407596433
    * Authored by Jonathan Budzenski <jbudz@users.noreply.github.com> on 2016-07-21T18:44:47Z
    * Committed by Jonathan Budzenski <jon@jbudz.me> on 2016-07-27T13:54:33Z

--- src/plugins/markdown_vis/public/markdown_vis_controller.js
+++ src/plugins/markdown_vis/public/markdown_vis_controller.js
@@ -1,15 +1,17 @@
 define(function (require) {
   const marked = require('marked');
+  require('angular-sanitize');
+
   marked.setOptions({
     gfm: true, // Github-flavored markdown
     sanitize: true // Sanitize HTML tags
   });
 
-  const module = require('ui/modules').get('kibana/markdown_vis', ['kibana']);
+  const module = require('ui/modules').get('kibana/markdown_vis', ['kibana', 'ngSanitize']);
   module.controller('KbnMarkdownVisController', function ($scope, $sce) {
     $scope.$watch('vis.params.markdown', function (html) {
       if (!html) return;
-      $scope.html = $sce.trustAsHtml(marked(html));
+      $scope.html = marked(html);
     });
   });
 });
