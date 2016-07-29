95859d9c5e3bcaea98e85fad9bc880c6b9aaec43
Jonathan Budzenski <jon@jbudz.me>
2016-07-28 21:08:52 -0500

    Cleanup logging output

--- src/server/lib/requestLogger.js
+++ src/server/lib/requestLogger.js
@@ -1,11 +1,16 @@
 var logger = require('./logger');
 var _ = require('lodash');
 module.exports = function (options) {
+  var omitHeaders = _.reduce(options, function (headers, value, key) {
+    return (value === 'none') ? headers : headers.concat(key);
+  }, []);
+
   return function (req, res, next) {
     var startTime = new Date();
     var end = res.end;
-    var _req = _.pick(req, ['url', 'headers', 'method']);
+    var _req = _.pick(req, ['url', 'method']);
     _req.connection = _.pick(req.connection, ['remoteAddress', 'remotePort']);
+    _req.headers = _.omit(req.headers, omitHeaders);
     res.end = function (chunk, encoding) {
       res.contentLength = parseInt(res.getHeader('content-length') || 0, 10);
       res.responseTime = (new Date()).getTime() - startTime.getTime();
