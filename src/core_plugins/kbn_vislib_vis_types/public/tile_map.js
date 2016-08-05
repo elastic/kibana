639138ce207ef31ade5f048b4f5bdcb3bbdeb658
Thomas Neirynck <thomas@elastic.co>
2016-08-05 17:10:27 -0400

    Deprecate desaturization map option.
    - adds a flag that client-side desaturization is deprecated
    - desaturization is turned off by default

--- src/core_plugins/kbn_vislib_vis_types/public/tile_map.js
+++ src/core_plugins/kbn_vislib_vis_types/public/tile_map.js
@@ -20,7 +20,7 @@ export default function TileMapVisType(Private, getAppState, courier, config) {
     params: {
       defaults: {
         mapType: 'Scaled Circle Markers',
-        isDesaturated: true,
+        isDesaturated: false,
         addTooltip: true,
         heatMaxZoom: 16,
         heatMinOpacity: 0.1,
