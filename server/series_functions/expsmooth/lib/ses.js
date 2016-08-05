11359e2fe10c848e688b549aff7a472ff56572c7
Rashid Khan <github.fliplap@spamgourmet.com>
2016-08-04 22:51:00 -0700

    Rename value to level

--- server/series_functions/expsmooth/lib/ses.js
+++ server/series_functions/expsmooth/lib/ses.js
@@ -6,12 +6,12 @@ const _ = require('lodash');
 
 module.exports = function ses(series, alpha) {
   let origin;
-  let value;
+  let level;
 
   return _.reduce(series, (result, point, i) => {
     if (i === 0) {
       origin = point[1];
-      value = point[1];
+      level = point[1];
     } else {
       // In the case that point[1] is null, we keep origin the same
       // and forecast the point
@@ -19,14 +19,14 @@ module.exports = function ses(series, alpha) {
         origin = point[1];
       }
       if (origin == null) {
-        value = null;
+        level = null;
       } else {
         const prevSmoothed = result[i - 1][1];
-        value = alpha * origin + (1 - alpha) * prevSmoothed;
+        level = alpha * origin + (1 - alpha) * prevSmoothed;
       }
     }
 
-    result.push([point[0], value]);
+    result.push([point[0], level]);
     return result;
   }, []);
 };
