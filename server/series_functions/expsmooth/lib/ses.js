1f5c439215c2f54e7d267f90198e238e0d849424
Rashid Khan <github.fliplap@spamgourmet.com>
2016-08-06 14:14:23 -0700

    Rename variable to be inline with des and tes

--- server/series_functions/expsmooth/lib/ses.js
+++ server/series_functions/expsmooth/lib/ses.js
@@ -8,25 +8,30 @@ module.exports = function ses(series, alpha) {
   let origin;
   let level;
 
-  return _.reduce(series, (result, point, i) => {
+  var times = _.map(series, 0);
+  var points = _.map(series, 1);
+
+  var smoothedPoints = _.reduce(points, (result, point, i) => {
     if (i === 0) {
-      origin = point[1];
-      level = point[1];
+      origin = point;
+      level = point;
     } else {
       // In the case that point[1] is null, we keep origin the same
       // and forecast the point
-      if (point[1] != null) {
-        origin = point[1];
+      if (point != null) {
+        origin = point;
       }
       if (origin == null) {
         level = null;
       } else {
-        const prevSmoothed = result[i - 1][1];
+        const prevSmoothed = result[i - 1];
         level = alpha * origin + (1 - alpha) * prevSmoothed;
       }
     }
 
-    result.push([point[0], level]);
+    result.push(level);
     return result;
   }, []);
+
+  return _.zip(times, smoothedPoints);
 };
