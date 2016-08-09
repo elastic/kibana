d65576d45272685fbdf9f3e6bbd91018e563a2c6
Timothy Sullivan <tsullivan@elastic.co>
2016-08-09 09:59:58 -0700

    ui/routes: add isEnabled method to module
    This will allow plugins that export Angular routes in hacks to safety
    check themselves and ensure the current app supports UI Routing.

--- src/ui/public/routes/routes.js
+++ src/ui/public/routes/routes.js
@@ -1,13 +1,19 @@
 import RouteManager from './route_manager';
 import 'angular-route/angular-route';
 import uiModules from 'ui/modules';
+let isEnabled = false; // UI Routing is false by default, until `enable()` is called
 let defaultRouteManager = new RouteManager();
 
 module.exports = {
   ...defaultRouteManager,
+  isEnabled() {
+    return isEnabled;
+  },
   enable() {
     uiModules
     .get('kibana', ['ngRoute'])
     .config(defaultRouteManager.config);
+
+    isEnabled = true;
   }
 };
