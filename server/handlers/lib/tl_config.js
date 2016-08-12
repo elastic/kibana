c78283fdf471be67378fd8acf20aa10467f60eb2
Rashid Khan <github.fliplap@spamgourmet.com>
2016-08-12 12:33:08 -0700

    Remove obsolete tlConfig properties

--- server/handlers/lib/tl_config.js
+++ server/handlers/lib/tl_config.js
@@ -1,6 +1,4 @@
 var _ = require('lodash');
-var config = require('../../../timelion.json');
-try {config = _.merge(config, require('../../../timelion.private.json'));} catch (e) {}; //eslint-disable-line no-empty
 
 var buildTarget = require('../../lib/build_target.js');
 
@@ -8,12 +6,6 @@ module.exports = function (setup) {
   var targetSeries;
 
   var tlConfig = {
-    time: {
-      from: 'now-12M',
-      to: 'now',
-      interval: config.default_interval
-    },
-    file: config,
     getTargetSeries: function () {
       return _.map(targetSeries, function (bucket) { // eslint-disable-line no-use-before-define
         return [bucket, null];
