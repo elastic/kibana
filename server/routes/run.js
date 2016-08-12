3a7ce2fc85b7b0ebe5338653e4c6c265f37781d4
Rashid Khan <github.fliplap@spamgourmet.com>
2016-08-12 13:28:43 -0700

    Fill defaults in case anything at all is missing

--- server/routes/run.js
+++ server/routes/run.js
@@ -1,6 +1,8 @@
 var Promise = require('bluebird');
+var _ = require('lodash');
 var Boom = require('boom');
 var chainRunnerFn = require('../handlers/chain_runner.js');
+var timelionDefaults = require('../lib/get_namespaced_settings')();
 
 function replyWithError(e, reply) {
   reply({title: e.toString(), message: e.toString(), stack: e.stack}).code(400);
@@ -18,12 +20,11 @@ module.exports = (server) => {
       // before every request. This just sucks because its going to slow things
       // down. Meh.
       return server.uiSettings().getAll().then((uiSettings) => {
-
         var sheet;
         var tlConfig = require('../handlers/lib/tl_config.js')({
           server: server,
           request: request,
-          settings: uiSettings
+          settings: _.defaults(uiSettings, timelionDefaults) // Just in case they delete some setting.
         });
         var chainRunner = chainRunnerFn(tlConfig);
 
