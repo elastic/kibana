4ee0513c7d00529016d21a573ba6bb8685e18af1
Rashid Khan <github.fliplap@spamgourmet.com>
2016-08-12 12:40:09 -0700

    Fix the validation endpoint

--- server/routes/validate_es.js
+++ server/routes/validate_es.js
@@ -3,25 +3,29 @@ module.exports = function (server) {
     method: 'GET',
     path: '/api/timelion/validate/es',
     handler: function (request, reply) {
-      var config = require('../../timelion.json');
-      var callWithRequest = server.plugins.elasticsearch.callWithRequest;
 
-      var body = {
-        index: config.es.default_index,
-        fields: config.es.timefield
-      };
+      return server.uiSettings().getAll().then((uiSettings) => {
+        var callWithRequest = server.plugins.elasticsearch.callWithRequest;
 
-      callWithRequest(request, 'fieldStats', body).then(function (resp) {
-        reply({
-          ok: true,
-          field: config.es.timefield,
-          min: resp.indices._all.fields[config.es.timefield].min_value,
-          max: resp.indices._all.fields[config.es.timefield].max_value
-        });
-      }).catch(function (resp) {
-        reply({
-          ok: false,
-          resp: resp
+        var timefield = uiSettings['timelion:es.timefield'];
+
+        var body = {
+          index: uiSettings['es.default_index'],
+          fields:timefield
+        };
+
+        callWithRequest(request, 'fieldStats', body).then(function (resp) {
+          reply({
+            ok: true,
+            field: timefield,
+            min: resp.indices._all.fields[timefield].min_value,
+            max: resp.indices._all.fields[timefield].max_value
+          });
+        }).catch(function (resp) {
+          reply({
+            ok: false,
+            resp: resp
+          });
         });
       });
 
