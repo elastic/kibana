c50c5fba530d0c9b9d7f5bdd76b566a2e7d7c06b
Rashid Khan <github.fliplap@spamgourmet.com>
2016-08-15 11:29:17 -0700

    Add timelion tests

--- src/core_plugins/timelion/server/series_functions/__tests__/static.js
+++ src/core_plugins/timelion/server/series_functions/__tests__/static.js
@@ -8,7 +8,7 @@ const invoke = require('./helpers/invoke_series_fn.js');
 describe(filename, () => {
   it('returns a series in which all numbers are the same', () => {
     return invoke(fn, [5]).then((r) => {
-      expect(_.map(r.output.list[0].data, 1)).to.eql([5, 5, 5, 5]);
+      expect(_.unique(_.map(r.output.list[0].data, 1))).to.eql([5]);
     });
   });
 
