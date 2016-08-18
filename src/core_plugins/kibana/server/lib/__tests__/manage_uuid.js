56e25323533c4d97a6f5b0a98d895fa25e9a87e8
Timothy Sullivan <tsullivan@elastic.co>
2016-08-17 22:21:13 -0700

    test/uuid: simplify test to not rely on elasticsearch

--- src/core_plugins/kibana/server/lib/__tests__/manage_uuid.js
+++ src/core_plugins/kibana/server/lib/__tests__/manage_uuid.js
@@ -1,11 +1,11 @@
 import expect from 'expect.js';
 import sinon from 'sinon';
 import Joi from 'joi';
-import * as kbnTestServer from '../../../../../test/utils/kbn_server.js';
-import fromRoot from '../../../../utils/from_root';
+import * as kbnTestServer from '../../../../../../test/utils/kbn_server.js';
+import fromRoot from '../../../../../utils/from_root';
 import manageUuid from '../manage_uuid';
 
-describe('plugins/elasticsearch', function () {
+describe('core_plugins/kibana/server/lib', function () {
   describe('manage_uuid', function () {
     const testUuid = 'c4add484-0cba-4e05-86fe-4baa112d9e53';
     let kbnServer;
@@ -23,7 +23,6 @@ describe('plugins/elasticsearch', function () {
       });
 
       await kbnServer.ready();
-      await kbnServer.server.plugins.elasticsearch.waitUntilReady();
     });
 
     // clear uuid stuff from previous test runs
@@ -49,42 +48,37 @@ describe('plugins/elasticsearch', function () {
     });
 
     it('finds the previously set uuid with config match', async function () {
-      const uuidManagement = manageUuid(kbnServer.server);
       const msg = `Kibana instance UUID: ${testUuid}`;
       config.set('uuid', testUuid);
 
-      await uuidManagement();
-      await uuidManagement();
+      await manageUuid(kbnServer.server);
+      await manageUuid(kbnServer.server);
 
       expect(kbnServer.server.log.lastCall.args[1]).to.be.eql(msg);
     });
 
     it('updates the previously set uuid with config value', async function () {
-      const uuidManagement = manageUuid(kbnServer.server);
       config.set('uuid', testUuid);
 
-      await uuidManagement();
+      await manageUuid(kbnServer.server);
 
       const newUuid = '5b2de169-2785-441b-ae8c-186a1936b17d';
       const msg = `Updating Kibana instance UUID to: ${newUuid} (was: ${testUuid})`;
 
       config.set('uuid', newUuid);
-      await uuidManagement();
+      await manageUuid(kbnServer.server);
 
       expect(kbnServer.server.log.lastCall.args[1]).to.be(msg);
     });
 
     it('resumes the uuid stored in data and sets it to the config', async function () {
-      const uuidManagement = manageUuid(kbnServer.server);
       const partialMsg = 'Resuming persistent Kibana instance UUID';
       config.set('uuid'); // set to undefined
 
-      await uuidManagement();
+      await manageUuid(kbnServer.server);
 
       expect(config.get('uuid')).to.be.ok(); // not undefined any more
       expect(kbnServer.server.log.lastCall.args[1]).to.match(new RegExp(`^${partialMsg}`));
     });
-
-
   });
 });
