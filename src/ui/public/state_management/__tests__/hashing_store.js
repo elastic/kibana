6237b313aec2c38ac286b0d79f8b2ae276f74e87
spalger <email@spalger.com>
2016-08-19 17:26:15 -0700

    [state] store url states into session storage
    Kibana currently stores it's entire application state in the URL by rison encoding it and sticking it into a query string parameter, _a for AppState and _g for GlobalState.
    
    This has functioned fine for a while, but Internet Explorer's short URL length restriction are starting to become a problem for more and more users. To provide these users with a workaround this adds an advanced config option that will store the state in sessionStorage instead of the URL.
    
    This is accomplished by hashing the serialized version of the state, storing a short version of the hash in the URL, and storing the whole serialized state in sessionStorage using the hash + state-type as a key.
    
    Since sessionStorage is limited in size, we must clean up old stored states after they become unreachable to the application. This is done using the new `LazyLruStore` class, a wrapper around sessionStorage. This wrapper helps us maintain the list of stored states based on the time they are accessed (On each set the access time is updates).
    
    It's cleanup style is configured with it's maxItems, idealClearRatio, and maxIdealClearPercent configurations. The defaults for which should be sufficient.
    
    `maxItems`: limits the store to n items, removing the oldest item when the list overflows
    `idealClearRatio+maxIdealClearPercent`: when `store.setItem(key, value)` throws an error we try to clear space equal to `idealClearRatio * (key+value).length`, but no more space than `totalSize * maxIdealClearPercent`

---
+++ src/ui/public/state_management/__tests__/hashing_store.js
@@ -0,0 +1,139 @@
+import expect from 'expect.js';
+import sinon from 'sinon';
+import { encode as encodeRison } from 'rison-node';
+
+import StubBrowserStorage from 'test_utils/stub_browser_storage';
+import { HashingStore } from 'ui/state_management/hashing_store';
+
+const setup = ({ createHash } = {}) => {
+  const store = new StubBrowserStorage();
+  const hashingStore = new HashingStore({ store, createHash });
+  return { store, hashingStore };
+};
+
+describe('State Management Hashing Store', () => {
+  describe('#add', () => {
+    it('adds a value to the store and returns its hash', () => {
+      const { hashingStore, store } = setup();
+      const val = { foo: 'bar' };
+      const hash = hashingStore.add(val);
+      expect(hash).to.be.a('string');
+      expect(hash).to.be.ok();
+      expect(store).to.have.length(1);
+    });
+
+    it('json encodes the values it stores', () => {
+      const { hashingStore, store } = setup();
+      const val = { toJSON() { return 1; } };
+      const hash = hashingStore.add(val);
+      expect(hashingStore.lookup(hash)).to.eql(1);
+    });
+
+    it('addresses values with a short hash', () => {
+      const val = { foo: 'bar' };
+      const longHash = 'longlonglonglonglonglonglonglonglonglonghash';
+      const { hashingStore } = setup({
+        createHash: () => longHash
+      });
+
+      const hash = hashingStore.add(val);
+      expect(hash.length < longHash.length).to.be.ok();
+    });
+
+    it('addresses values with a slightly longer hash when short hashes collide', () => {
+      const fixtures = [
+        {
+          hash: '1234567890-1',
+          val: { foo: 'bar' }
+        },
+        {
+          hash: '1234567890-2',
+          val: { foo: 'baz' }
+        },
+        {
+          hash: '1234567890-3',
+          val: { foo: 'boo' }
+        }
+      ];
+
+      const matchVal = json => f => JSON.stringify(f.val) === json;
+      const { hashingStore } = setup({
+        createHash: val => {
+          const fixture = fixtures.find(matchVal(val));
+          return fixture.hash;
+        }
+      });
+
+      const hash1 = hashingStore.add(fixtures[0].val);
+      const hash2 = hashingStore.add(fixtures[1].val);
+      const hash3 = hashingStore.add(fixtures[2].val);
+
+      expect(hash3).to.have.length(hash2.length + 1);
+      expect(hash2).to.have.length(hash1.length + 1);
+    });
+
+    it('bubbles up the error if the store fails to setItem', () => {
+      const { store, hashingStore } = setup();
+      const err = new Error();
+      sinon.stub(store, 'setItem').throws(err);
+      expect(() => {
+        hashingStore.add({});
+      }).to.throwError(e => expect(e).to.be(err));
+    });
+  });
+
+  describe('#lookup', () => {
+    it('reads a value from the store by its hash', () => {
+      const { hashingStore } = setup();
+      const val = { foo: 'bar' };
+      const hash = hashingStore.add(val);
+      expect(hashingStore.lookup(hash)).to.eql(val);
+    });
+
+    it('returns null when the value is not in the store', () => {
+      const { hashingStore } = setup();
+      const val = { foo: 'bar' };
+      const hash = hashingStore.add(val);
+      expect(hashingStore.lookup(`${hash} break`)).to.be(null);
+    });
+  });
+
+  describe('#remove', () => {
+    it('removes the value by its hash', () => {
+      const { hashingStore } = setup();
+      const val = { foo: 'bar' };
+      const hash = hashingStore.add(val);
+      expect(hashingStore.lookup(hash)).to.eql(val);
+      hashingStore.remove(hash);
+      expect(hashingStore.lookup(hash)).to.be(null);
+    });
+  });
+
+  describe('#isHash', () => {
+    it('can identify values that look like hashes', () => {
+      const { hashingStore } = setup();
+      const val = { foo: 'bar' };
+      const hash = hashingStore.add(val);
+      expect(hashingStore.isHash(hash)).to.be(true);
+    });
+
+    describe('rison', () => {
+      const tests = [
+        ['object', { foo: 'bar' }],
+        ['number', 1],
+        ['number', 1000],
+        ['number', Math.round(Math.random() * 10000000)],
+        ['string', 'this is a string'],
+        ['array', [1,2,3]],
+      ];
+
+      tests.forEach(([type, val]) => {
+        it(`is not fooled by rison ${type} "${val}"`, () => {
+          const { hashingStore } = setup();
+          const rison = encodeRison(val);
+          expect(hashingStore.isHash(rison)).to.be(false);
+        });
+      });
+    });
+  });
+});
--- src/ui/public/state_management/__tests__/state.js
+++ src/ui/public/state_management/__tests__/state.js
@@ -3,46 +3,71 @@ import _ from 'lodash';
 import sinon from 'sinon';
 import expect from 'expect.js';
 import ngMock from 'ng_mock';
+import { encode as encodeRison } from 'rison-node';
 import 'ui/private';
+import Notifier from 'ui/notify/notifier';
 import StateManagementStateProvider from 'ui/state_management/state';
+import { UnhashStatesProvider } from 'ui/state_management/unhash_states';
+import { HashingStore } from 'ui/state_management/hashing_store';
+import StubBrowserStorage from 'test_utils/stub_browser_storage';
 import EventsProvider from 'ui/events';
 
 describe('State Management', function () {
+  const notify = new Notifier();
   let $rootScope;
   let $location;
   let State;
   let Events;
+  let setup;
 
   beforeEach(ngMock.module('kibana'));
-  beforeEach(ngMock.inject(function (_$rootScope_, _$location_, Private) {
+  beforeEach(ngMock.inject(function (_$rootScope_, _$location_, Private, config) {
     $location = _$location_;
     $rootScope = _$rootScope_;
     State = Private(StateManagementStateProvider);
     Events = Private(EventsProvider);
+    Notifier.prototype._notifs.splice(0);
+
+    setup = opts => {
+      const { param, initial, storeInHash } = (opts || {});
+      sinon.stub(config, 'get').withArgs('state:storeInSessionStorage').returns(!!storeInHash);
+      const store = new StubBrowserStorage();
+      const hashingStore = new HashingStore({ store });
+      const state = new State(param, initial, { hashingStore, notify });
+
+      const getUnhashedSearch = (state) => {
+        const unhashStates = Private(UnhashStatesProvider);
+        return unhashStates.inParsedQueryString($location.search(), [ state ]);
+      };
+
+      return { notify, store, hashingStore, state, getUnhashedSearch };
+    };
   }));
+
+  afterEach(() => Notifier.prototype._notifs.splice(0));
 
   describe('Provider', function () {
     it('should reset the state to the defaults', function () {
-      let state = new State('_s', { message: ['test'] });
+      const { state, getUnhashedSearch } = setup({ initial: { message: ['test'] } });
       state.reset();
-      let search = $location.search();
+      let search = getUnhashedSearch(state);
       expect(search).to.have.property('_s');
       expect(search._s).to.equal('(message:!(test))');
       expect(state.message).to.eql(['test']);
     });
 
     it('should apply the defaults upon initialization', function () {
-      let state = new State('_s', { message: 'test' });
+      const { state } = setup({ initial: { message: 'test' } });
       expect(state).to.have.property('message', 'test');
     });
 
     it('should inherit from Events', function () {
-      let state = new State();
+      const { state } = setup();
       expect(state).to.be.an(Events);
     });
 
     it('should emit an event if reset with changes', function (done) {
-      let state = new State('_s', { message: 'test' });
+      const { state } = setup({ initial: { message: ['test'] } });
       state.on('reset_with_changes', function (keys) {
         expect(keys).to.eql(['message']);
         done();
@@ -54,7 +79,7 @@ describe('State Management', function () {
     });
 
     it('should not emit an event if reset without changes', function () {
-      let state = new State('_s', { message: 'test' });
+      const { state } = setup({ initial: { message: 'test' } });
       state.on('reset_with_changes', function () {
         expect().fail();
       });
@@ -67,29 +92,29 @@ describe('State Management', function () {
 
   describe('Search', function () {
     it('should save to $location.search()', function () {
-      let state = new State('_s', { test: 'foo' });
+      const { state, getUnhashedSearch } = setup({ initial: { test: 'foo' } });
       state.save();
-      let search = $location.search();
+      let search = getUnhashedSearch(state);
       expect(search).to.have.property('_s');
       expect(search._s).to.equal('(test:foo)');
     });
 
     it('should emit an event if changes are saved', function (done) {
-      let state = new State();
+      const { state, getUnhashedSearch } = setup();
       state.on('save_with_changes', function (keys) {
         expect(keys).to.eql(['test']);
         done();
       });
       state.test = 'foo';
       state.save();
-      let search = $location.search();
+      let search = getUnhashedSearch(state);
       $rootScope.$apply();
     });
   });
 
   describe('Fetch', function () {
     it('should emit an event if changes are fetched', function (done) {
-      let state = new State();
+      const { state } = setup();
       state.on('fetch_with_changes', function (keys) {
         expect(keys).to.eql(['foo']);
         done();
@@ -101,7 +126,7 @@ describe('State Management', function () {
     });
 
     it('should have events that attach to scope', function (done) {
-      let state = new State();
+      const { state } = setup();
       state.on('test', function (message) {
         expect(message).to.equal('foo');
         done();
@@ -111,7 +136,7 @@ describe('State Management', function () {
     });
 
     it('should fire listeners for #onUpdate() on #fetch()', function (done) {
-      let state = new State();
+      const { state } = setup();
       state.on('fetch_with_changes', function (keys) {
         expect(keys).to.eql(['foo']);
         done();
@@ -123,7 +148,7 @@ describe('State Management', function () {
     });
 
     it('should apply defaults to fetches', function () {
-      let state = new State('_s', { message: 'test' });
+      const { state } = setup({ initial: { message: 'test' } });
       $location.search({ _s: '(foo:bar)' });
       state.fetch();
       expect(state).to.have.property('foo', 'bar');
@@ -131,7 +156,7 @@ describe('State Management', function () {
     });
 
     it('should call fetch when $routeUpdate is fired on $rootScope', function () {
-      let state = new State();
+      const { state } = setup();
       let spy = sinon.spy(state, 'fetch');
       $rootScope.$emit('$routeUpdate', 'test');
       sinon.assert.calledOnce(spy);
@@ -139,9 +164,9 @@ describe('State Management', function () {
 
     it('should clear state when missing form URL', function () {
       let stateObj;
-      let state = new State();
+      const { state } = setup();
 
-      // set satte via URL
+      // set state via URL
       $location.search({ _s: '(foo:(bar:baz))' });
       state.fetch();
       stateObj = state.toObject();
@@ -158,6 +183,64 @@ describe('State Management', function () {
       state.fetch();
       stateObj = state.toObject();
       expect(stateObj).to.eql({});
+    });
+  });
+
+
+  describe('Hashing', () => {
+    it('stores state values in a hashingStore, writing the hash to the url', () => {
+      const { state, hashingStore } = setup({ storeInHash: true });
+      state.foo = 'bar';
+      state.save();
+      const urlVal = $location.search()[state.getQueryParamName()];
+
+      expect(hashingStore.isHash(urlVal)).to.be(true);
+      expect(hashingStore.lookup(urlVal)).to.eql({ foo: 'bar' });
+    });
+
+    it('should replace rison in the URL with a hash', () => {
+      const { state, hashingStore } = setup({ storeInHash: true });
+      const obj = { foo: { bar: 'baz' } };
+      const rison = encodeRison(obj);
+
+      $location.search({ _s: rison });
+      state.fetch();
+
+      const urlVal = $location.search()._s;
+      expect(urlVal).to.not.be(rison);
+      expect(hashingStore.isHash(urlVal)).to.be(true);
+      expect(hashingStore.lookup(urlVal)).to.eql(obj);
+    });
+
+    context('error handling', () => {
+      it('notifies the user when a hash value does not map to a stored value', () => {
+        const { state, hashingStore, notify } = setup({ storeInHash: true });
+        const search = $location.search();
+        const badHash = hashingStore.add({});
+        hashingStore.remove(badHash);
+
+        search[state.getQueryParamName()] = badHash;
+        $location.search(search);
+
+        expect(notify._notifs).to.have.length(0);
+        state.fetch();
+        expect(notify._notifs).to.have.length(1);
+        expect(notify._notifs[0].content).to.match(/use the share functionality/i);
+      });
+
+      it('presents fatal error linking to github when hashingStore.add fails', () => {
+        const { state, hashingStore, notify } = setup({ storeInHash: true });
+        const fatalStub = sinon.stub(notify, 'fatal').throws();
+        sinon.stub(hashingStore, 'add').throws();
+
+        expect(() => {
+          state.toQueryParam();
+        }).to.throwError();
+
+        sinon.assert.calledOnce(fatalStub);
+        expect(fatalStub.firstCall.args[0]).to.be.an(Error);
+        expect(fatalStub.firstCall.args[0].message).to.match(/github\.com/);
+      });
     });
   });
 });
