68932dee014a282396e1f439c7ada9a3f1472d99
Court Ewing <court@epixa.com>
2016-08-23 10:54:06 -0400

    Only show asc sort terms agg deprecation once per route
    The deprecation notification when using terms aggregations that are
    using ascending order by count was appearing too frequently, often
    multiple times on the same screen just when performing common UI
    actions that may not have anything specifically to do with the sort
    order of the terms aggregation.
    
    The notification will now appear once whenever the route changes and the
    offending terms aggregation is present, but it will not render again
    until at least the route changes.

---
+++ src/ui/public/route_based_notifier/index.js
@@ -0,0 +1,47 @@
+import { includes, mapValues } from 'lodash';
+import Notifier from 'ui/notify/notifier';
+
+/*
+ * Caches notification attempts so each one is only actually sent to the
+ * notifier service once per route.
+ */
+export default function routeBasedNotifierProvider($rootScope) {
+  const notifier = new Notifier();
+
+  let notifications = {
+    warnings: []
+  };
+
+  // empty the tracked notifications whenever the route changes so we can start
+  // fresh for the next route cycle
+  $rootScope.$on('$routeChangeSuccess', () => {
+    notifications = mapValues(notifications, () => []);
+  });
+
+  // Executes the given notify function if the message has not been seen in
+  // this route cycle
+  function executeIfNew(messages, message, notifyFn) {
+    if (includes(messages, message)) {
+      return;
+    }
+
+    messages.push(message);
+    notifyFn.call(notifier, message);
+  }
+
+  return {
+    /**
+     * Notify a given warning once in this route cycle
+     * @param {string} message
+     */
+    warning(message) {
+      executeIfNew(
+        notifications.warnings,
+        message,
+        notifier.warning
+      );
+    }
+  };
+};
+
+
