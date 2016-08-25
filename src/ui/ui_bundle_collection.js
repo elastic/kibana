3572ab8e17ff1baec6c8d372a97b294c604e1478
spalger <email@spalger.com>
2016-08-25 10:48:30 -0700

    move import statements before their use

--- src/ui/ui_bundle_collection.js
+++ src/ui/ui_bundle_collection.js
@@ -1,14 +1,14 @@
-const rimraf = promisify(require('rimraf'));
-const mkdirp = promisify(require('mkdirp'));
-const unlink = promisify(require('fs').unlink);
-const readdir = promisify(require('fs').readdir);
-
 import UiBundle from './ui_bundle';
 import appEntryTemplate from './app_entry_template';
 import { readFileSync as readSync } from 'fs';
 import { pull, transform, pluck } from 'lodash';
 import { promisify } from 'bluebird';
 import { makeRe } from 'minimatch';
+
+const rimraf = promisify(require('rimraf'));
+const mkdirp = promisify(require('mkdirp'));
+const unlink = promisify(require('fs').unlink);
+const readdir = promisify(require('fs').readdir);
 
 class UiBundleCollection {
   constructor(bundlerEnv, filter) {
