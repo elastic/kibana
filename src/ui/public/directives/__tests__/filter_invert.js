ad9dd83f8e60a32bdd7ad53dc48e083f3b63b7d2
ppisljar <peter.pisljar@gmail.com>
2016-08-27 10:06:09 +0200

    trying to fix tests

--- src/ui/public/directives/__tests__/filter_invert.js
+++ src/ui/public/directives/__tests__/filter_invert.js
@@ -3,33 +3,38 @@ import expect from 'expect.js';
 import ngMock from 'ng_mock';
 
 describe('FilterInvert directive', function () {
-  let $scope;
   let $parentScope;
   let $elem;
   let $compile;
+  let $controller;
 
-  beforeEach(ngMock.module('kibana'));
+  beforeEach(ngMock.module('kibana', function ($controllerProvider) {
+    $controller = $controllerProvider;
+  }));
   beforeEach(ngMock.inject(function ($injector) {
-    $elem = angular.element('<img src="test.png" filter-invert>');
+    $elem = angular.element('<img src="/bundles/c398c57f18e4955558703a61596db982.png" filter-invert>');
     $parentScope = $injector.get('$rootScope');
     $compile = $injector.get('$compile');
   }));
 
   it('sets CSS filter:invert property in supported browsers', function () {
-    const element = $compile($elem)($parentScope);
-    const controller = element.controller('filterInvert');
-    controller.isFilterSupported = () => true;
+    $controller.register('filterInvertController', () => {
+      this.isFilterSupported = () => true;
+    });
+    $compile($elem)($parentScope);
     $elem.scope().$digest();
-    $scope = $elem.isolateScope();
     expect($elem.attr('style')).to.contain('filter: invert(100%)');
   });
 
   it('adds svg filter in unsupported browsers', function () {
-    const element = $compile($elem)($parentScope);
-    const controller = element.controller('filterInvert');
-    controller.isFilterSupported = () => false;
+    $controller.register('filterInvertController', function () {
+      this.isFilterSupported = () => false;
+    });
+    $compile($elem)($parentScope);
     $elem.scope().$digest();
-    $scope = $elem.isolateScope();
-    expect($elem.attr('style')).to.contain('filter: invert(100%)'); // this should fail ... it doesnt
+    expect($elem.attr('style')).to.contain('hidden');
+    $elem.scope().$digest();
+
+    expect($elem.next().attr('class')).to.equal('svg');
   });
 });
