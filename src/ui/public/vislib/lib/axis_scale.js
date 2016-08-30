9864f49e918a31bd0024fed4fd8c828a323c0ed9
ppisljar <peter.pisljar@gmail.com>
2016-08-30 18:13:44 +0200

    party fixing endzones issue

--- src/ui/public/vislib/lib/axis_scale.js
+++ src/ui/public/vislib/lib/axis_scale.js
@@ -236,7 +236,7 @@ export default function AxisScaleFactory(Private) {
       this.scale.range(range);
     }
 
-    if (!this.isUserDefined() && !this.isOrdinal()) this.scale.nice(); // round extents when not user defined
+    if (!this.isUserDefined() && !this.isOrdinal() && !this.isTimeDomain()) this.scale.nice(); // round extents when not user defined
     // Prevents bars from going off the chart when the y extents are within the domain range
     if (this.axis._attr.type === 'histogram') this.scale.clamp(true);
 
