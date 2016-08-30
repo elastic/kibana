c575318ece8e748bf77f4b2353beac64b24ff0b3
Rashid Khan <github.fliplap@spamgourmet.com>
2016-08-30 11:10:29 -0700

    Add log function

--- src/core_plugins/timelion/server/series_functions/__tests__/fixtures/seriesList.js
+++ src/core_plugins/timelion/server/series_functions/__tests__/fixtures/seriesList.js
@@ -9,6 +9,7 @@ module.exports = function () {
     getSeries('Negative', buckets,     [-51, 17, 82, 20]),
     getSeries('Nice', buckets,         [100, 50, 50, 20]),
     getSeries('All the same', buckets, [1, 1, 1, 1]),
-    getSeries('Decimals', buckets,     [3.1415926535, 2, 1.439, 0.3424235])
+    getSeries('Decimals', buckets,     [3.1415926535, 2, 1.439, 0.3424235]),
+    getSeries('PowerOfTen', buckets,   [10, 100, 10, 1]),
   ]);
 };
