f592318cde7b36aca2eb7a5510e6084eaff23b35
Jim Unger <BigFunger@gmail.com>
2016-09-01 09:43:48 -0500

    adds state_monitor

---
+++ src/ui/public/state_management/state_monitor.js
@@ -0,0 +1,85 @@
+import { cloneDeep, isEqual, set } from 'lodash';
+
+export default {
+  create: (state, defaultState) => stateMonitor(state, defaultState)
+};
+
+function stateMonitor(state, defaultState) {
+  let destroyed = false;
+  let ignoredProps = [];
+  let changeHandlers = [];
+
+  const originalState = cloneDeep(defaultState) || cloneDeep(state.toJSON());
+
+  function filterState(state) {
+    ignoredProps.forEach(path => {
+      set(state, path, true);
+    });
+    return state;
+  }
+
+  function getStatus() {
+    const currentState = filterState(state.toJSON());
+    const isClean = isEqual(currentState, originalState);
+
+    return {
+      clean: isClean,
+      dirty: !isClean,
+    };
+  }
+
+  function dispatchChange(type = null, keys = []) {
+    const status = getStatus();
+    changeHandlers.forEach(changeHandler => {
+      changeHandler(status, type, keys);
+    });
+  }
+
+  function dispatchFetch(keys) {
+    dispatchChange('fetch_with_changes', keys);
+  };
+
+  function dispatchSave(keys) {
+    dispatchChange('save_with_changes', keys);
+  };
+
+  function dispatchReset(keys) {
+    dispatchChange('reset_with_changes', keys);
+  };
+
+  return {
+    ignoreProps(props) {
+      ignoredProps = ignoredProps.concat(props);
+      filterState(originalState);
+      return this;
+    },
+
+    onChange(callback) {
+      if (destroyed) throw new Error('Monitor has been destroyed');
+      if (typeof callback !== 'function') throw new Error('onChange handler must be a function');
+
+      changeHandlers.push(callback);
+
+      // Listen for state events.
+      state.on('fetch_with_changes', dispatchFetch);
+      state.on('save_with_changes', dispatchSave);
+      state.on('reset_with_changes', dispatchReset);
+
+      // if the state is already dirty, fire the change handler immediately
+      const status = getStatus();
+      if (status.dirty) {
+        dispatchChange();
+      }
+
+      return this;
+    },
+
+    destroy() {
+      destroyed = true;
+      changeHandlers = undefined;
+      state.off('fetch_with_changes', dispatchFetch);
+      state.off('save_with_changes', dispatchSave);
+      state.off('reset_with_changes', dispatchReset);
+    }
+  };
+}
