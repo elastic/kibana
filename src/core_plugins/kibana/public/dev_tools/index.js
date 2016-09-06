2ab0ea76bc1de18d1c8ed46c4bb28757e571bffe
Lukas Olson <olson.lukas@gmail.com>
2016-09-06 15:16:31 -0700

    [dev-tools] Update to use flexbox so console looks nice

--- src/core_plugins/kibana/public/dev_tools/index.js
+++ src/core_plugins/kibana/public/dev_tools/index.js
@@ -1,23 +1,7 @@
 import uiRoutes from 'ui/routes';
-import uiModules from 'ui/modules';
-import template from 'plugins/kibana/dev_tools/index.html';
-import chrome from 'ui/chrome/chrome';
-import devTools from 'ui/registry/dev_tools';
+import 'plugins/kibana/dev_tools/directives/dev_tools_app';
 
 uiRoutes
 .when('/dev_tools', {
   template: '<kbn-dev-tools-app></kbn-dev-tools-app>'
-});
-
-uiModules
-.get('apps/dev_tools')
-.directive('kbnDevToolsApp', function (Private) {
-  return {
-    restrict: 'E',
-    template,
-    transclude: true,
-    link: function ($scope) {
-      $scope.devTools = Private(devTools).inOrder;
-    }
-  };
 });
