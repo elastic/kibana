3e37d38a05826f734a723bfd2dc76e568ac6cee5
Jim Unger <BigFunger@gmail.com>
2016-09-12 15:40:46 -0500

    Added some comments, and clarified error message

--- src/cli_plugin/install/pack.js
+++ src/cli_plugin/install/pack.js
@@ -57,7 +57,8 @@ function assertValidPackageName(plugin) {
 
 /**
  * Examine each package.json file to determine the plugin name,
- *  version, and platform. Mutates the package objects in the packages array
+ *  version, kibanaVersion, and platform. Mutates the package objects
+ *  in the packages array
  * @param {object} settings - a plugin installer settings object
  * @param {array} packages - array of package objects from listPackages()
  */
@@ -69,10 +70,12 @@ async function mergePackageData(settings, packages) {
     pkg.version = _.get(packageInfo, 'version');
     pkg.name = _.get(packageInfo, 'name');
     pkg.path =  resolve(settings.pluginDir, pkg.name);
-    pkg.kibanaVersion = _.get(packageInfo, 'kibana.version');
-    if (!pkg.kibanaVersion) {
-      pkg.kibanaVersion = pkg.version;
-    }
+
+    // Plugins must specify their version, and by default that version should match
+    // the version of kibana down to the patch level. If these two versions need
+    // to diverge, they can specify a kibana.version to indicate the version of
+    // kibana the plugin is intended to work with.
+    pkg.kibanaVersion = _.get(packageInfo, 'kibana.version', pkg.version);
 
     const regExp = new RegExp(`${pkg.name}-(.+)`, 'i');
     const matches = pkg.folder.match(regExp);
