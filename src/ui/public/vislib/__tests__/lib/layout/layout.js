676a33df7df09e16b72a5a84f71ac50cedfab8af
ppisljar <peter.pisljar@gmail.com>
2016-09-15 13:52:58 +0200

    fixing layout tests

--- src/ui/public/vislib/__tests__/lib/layout/layout.js
+++ src/ui/public/vislib/__tests__/lib/layout/layout.js
@@ -52,16 +52,16 @@ dateHistogramArray.forEach(function (data, i) {
     describe('createLayout Method', function () {
       it('should append all the divs', function () {
         expect($(vis.el).find('.vis-wrapper').length).to.be(1);
-        expect($(vis.el).find('.y-axis-col-wrapper').length).to.be(1);
+        expect($(vis.el).find('.y-axis-col-wrapper').length).to.be(2);
         expect($(vis.el).find('.vis-col-wrapper').length).to.be(1);
-        expect($(vis.el).find('.y-axis-col').length).to.be(1);
-        expect($(vis.el).find('.y-axis-title').length).to.be(1);
-        expect($(vis.el).find('.y-axis-div-wrapper').length).to.be(1);
-        expect($(vis.el).find('.y-axis-spacer-block').length).to.be(1);
+        expect($(vis.el).find('.y-axis-col').length).to.be(2);
+        expect($(vis.el).find('.y-axis-title').length).to.be(2);
+        expect($(vis.el).find('.y-axis-div-wrapper').length).to.be(2);
+        expect($(vis.el).find('.y-axis-spacer-block').length).to.be(2);
         expect($(vis.el).find('.chart-wrapper').length).to.be(numberOfCharts);
-        expect($(vis.el).find('.x-axis-wrapper').length).to.be(1);
-        expect($(vis.el).find('.x-axis-div-wrapper').length).to.be(1);
-        expect($(vis.el).find('.x-axis-title').length).to.be(1);
+        expect($(vis.el).find('.x-axis-wrapper').length).to.be(2);
+        expect($(vis.el).find('.x-axis-div-wrapper').length).to.be(2);
+        expect($(vis.el).find('.x-axis-title').length).to.be(2);
       });
     });
 
