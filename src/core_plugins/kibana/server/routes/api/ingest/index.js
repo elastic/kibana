b15625b9d853e9047e83385477fa3c759afde8f7
Matthew Bargar <mbargar@gmail.com>
2016-09-29 14:55:48 -0400

    Remove Upload Data API

--- src/core_plugins/kibana/server/routes/api/ingest/index.js
+++ src/core_plugins/kibana/server/routes/api/ingest/index.js
@@ -2,7 +2,6 @@ import { registerPost } from './register_post';
 import { registerDelete } from './register_delete';
 import { registerProcessors } from './register_processors';
 import { registerSimulate } from './register_simulate';
-import { registerData } from './register_data';
 import { registerFieldCapabilities } from './register_field_capabilities';
 
 export default function (server) {
@@ -10,6 +9,5 @@ export default function (server) {
   registerDelete(server);
   registerProcessors(server);
   registerSimulate(server);
-  registerData(server);
   registerFieldCapabilities(server);
 }
