dfde19a0b3e205daef379ce377627ad6dc5bd67c
= <brandon.kobel@elastic.co>
2016-10-06 13:29:03 -0400

    Removing the input-whole-number, no longer used.

--- src/ui/public/directives/__tests__/input_number.js
+++ src/ui/public/directives/__tests__/input_number.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
 import expect from 'expect.js';
 import ngMock from 'ng_mock';
-import 'ui/directives/input_whole_number';
+import 'ui/directives/input_number';
 
 describe('Number input directive', function () {
   let $compile;
