3b6652f745b1353f8a9c744775764308e570fbce
nreese <reese.nathan@gmail.com>
2016-10-18 14:42:23 -0600

    brush histogram

--- src/ui/public/utils/brush_event.js
+++ src/ui/public/utils/brush_event.js
@@ -1,50 +1,36 @@
-import _ from 'lodash';
 import moment from 'moment';
 import buildRangeFilter from 'ui/filter_manager/lib/range';
 export default function brushEventProvider(timefilter) {
-  return $state => {
-    return event => {
-      if (!event.data.xAxisField) {
-        return;
-      }
+  return function ($state) {
+    return function (event) {
+      if (event.data.xAxisField && event.data.xAxisField.type === 'date') {
+        let from = moment(event.range[0]);
+        let to = moment(event.range[1]);
 
-      switch (event.data.xAxisField.type) {
-        case 'date':
-          let from = moment(event.range[0]);
-          let to = moment(event.range[1]);
+        if (to - from === 0) return;
 
-          if (to - from === 0) return;
-
-          timefilter.time.from = from;
-          timefilter.time.to = to;
-          timefilter.time.mode = 'absolute';
-          break;
-
-        case 'number':
-          if (event.range.length <= 1) return;
-
-          const existingFilter = $state.filters.find(filter => (
-            filter.meta && filter.meta.key === event.data.xAxisField.name
-          ));
-
-          const min = event.range[0];
-          const max = event.range[event.range.length - 1];
-          const range = {gte: min, lt: max};
-          if (_.has(existingFilter, 'range')) {
-            existingFilter.range[event.data.xAxisField.name] = range;
-          } else if (_.has(existingFilter, 'script.script.params.gte')
-            && _.has(existingFilter, 'script.script.params.lt')) {
-            existingFilter.script.script.params.gte = min;
-            existingFilter.script.script.params.lt = max;
-          } else {
-            const newFilter = buildRangeFilter(
-              event.data.xAxisField,
-              range,
-              event.data.indexPattern,
-              event.data.xAxisFormatter);
-            $state.$newFilters = [newFilter];
+        timefilter.time.from = from;
+        timefilter.time.to = to;
+        timefilter.time.mode = 'absolute';
+      } else if (event.data.xAxisField && event.data.xAxisField.type === 'number') {
+        let existingFilter = null;
+        $state.filters.forEach(function (it) {
+          if (it.meta && it.meta.key === event.data.xAxisField.name) {
+            existingFilter = it;
           }
-          break;
+        });
+
+        const range = {gte: event.range[0], lt: event.range[event.range.length - 1]};
+        if (existingFilter) {
+          existingFilter.range[event.data.xAxisField.name] = range;
+        } else {
+          const newFilter = buildRangeFilter(
+            event.data.xAxisField,
+            range,
+            event.data.indexPattern,
+            event.data.xAxisFormatter);
+          $state.$newFilters = [newFilter];
+        }
       }
     };
   };
