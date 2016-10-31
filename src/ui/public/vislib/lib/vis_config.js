867fdb1bc0892514d18b47f77dc436debe5a015d
ppisljar <peter.pisljar@gmail.com>
2016-10-31 10:39:55 +0100

    injectZeros should be applied per value axis (cherry picked from commit fb81bcb)

--- src/ui/public/vislib/lib/vis_config.js
+++ src/ui/public/vislib/lib/vis_config.js
@@ -1,12 +1,11 @@
 import _ from 'lodash';
 import VisTypesProvider from 'ui/vislib/lib/types';
 import VislibLibDataProvider from 'ui/vislib/lib/data';
-import VislibComponentsZeroInjectionInjectZerosProvider from 'ui/vislib/components/zero_injection/inject_zeros';
+
 
 export default function VisConfigFactory(Private) {
 
   const Data = Private(VislibLibDataProvider);
-  const injectZeros = Private(VislibComponentsZeroInjectionInjectZerosProvider);
   const visTypes = Private(VisTypesProvider);
   const defaults = {
     style: {
@@ -20,11 +19,7 @@ export default function VisConfigFactory(Private) {
 
   class VisConfig {
     constructor(visConfigArgs, data, uiState) {
-      if (visConfigArgs.zeroFill || ['area', 'histogram'].includes(visConfigArgs.type)) {
-        this.data = new Data(injectZeros(data), uiState);
-      } else {
-        this.data = new Data(data, uiState);
-      }
+      this.data = new Data(data, uiState);
 
       const typeDefaults = visTypes[visConfigArgs.type](visConfigArgs, this.data);
       this._values = _.defaultsDeep({}, typeDefaults, defaults);
