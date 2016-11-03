501ecd85652d8756343786b7df7388111455f27e
Matt Bargar <matt@devcorr.com>
2016-11-03 18:34:10 -0400

    Avoid printing notify object in lang selector on error (#8639)
    When ES is unavailable and we can't get the list of enabled scripting
    langs we display a notification to the user. We were also accidentally
    returning that notification object from the service method and
    displaying its toString representation in the language select drop down.
    Now we simplay return an empty array so the dropdown is rendered without
    options.
    
    Fixes https://github.com/elastic/kibana/issues/8636

--- src/ui/public/field_editor/field_editor.js
+++ src/ui/public/field_editor/field_editor.js
@@ -162,7 +162,8 @@ uiModules
         return $http.get(chrome.addBasePath('/api/kibana/scripts/languages'))
         .then((res) => res.data)
         .catch(() => {
-          return notify.error('Error getting available scripting languages from Elasticsearch');
+          notify.error('Error getting available scripting languages from Elasticsearch');
+          return [];
         });
       }
 
