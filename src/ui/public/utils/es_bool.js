76e3a4b9a3eeb1287bc07226cd1e367df181ba23
spalger <email@spalger.com>
2016-11-04 12:51:08 -0700

    apply unwrap-simple-amd jscodeshift

--- src/ui/public/utils/es_bool.js
+++ src/ui/public/utils/es_bool.js
@@ -1,22 +1,20 @@
-define(function () {
-  let map = {
-    'false': false,
-    'off': false,
-    'no': false,
-    '0': false,
-    'true': true,
-    'on': true,
-    'yes': true,
-    '1': true
-  };
+let map = {
+  'false': false,
+  'off': false,
+  'no': false,
+  '0': false,
+  'true': true,
+  'on': true,
+  'yes': true,
+  '1': true
+};
 
-  return function (str) {
-    let bool = map[String(str)];
+export default function (str) {
+  let bool = map[String(str)];
 
-    if (typeof bool !== 'boolean') {
-      throw new TypeError('"' + str + '" does not map to an esBool');
-    }
+  if (typeof bool !== 'boolean') {
+    throw new TypeError('"' + str + '" does not map to an esBool');
+  }
 
-    return bool;
-  };
-});
+  return bool;
+};
--- test/unit/api/scripts/index.js
+++ test/unit/api/scripts/index.js
@@ -1,13 +1,11 @@
-define(function (require) {
-  var bdd = require('intern!bdd');
-  var serverConfig = require('intern/dojo/node!../../../server_config');
-  var request = require('intern/dojo/node!supertest-as-promised');
-  var url = require('intern/dojo/node!url');
-  var languages = require('./_languages');
+var bdd = require('intern!bdd');
+var serverConfig = require('intern/dojo/node!../../../server_config');
+var request = require('intern/dojo/node!supertest-as-promised');
+var url = require('intern/dojo/node!url');
+var languages = require('./_languages');
 
-  bdd.describe('scripts API', function () {
-    request = request(url.format(serverConfig.servers.kibana) + '/api');
+bdd.describe('scripts API', function () {
+  request = request(url.format(serverConfig.servers.kibana) + '/api');
 
-    languages(bdd, request);
-  });
+  languages(bdd, request);
 });
