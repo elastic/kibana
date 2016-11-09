e34b628d610d90aa31be7fd5e99eb84ae8b9cdc3
Elastic Jasper <elastic-jasper@users.noreply.github.com>
2016-11-09 06:33:27 -0500

    pie chart doesnt render small slices correctly
    Backports PR #8986
    
    **Commit 1:**
    pie chart doesnt render small slices correctly
    
    * Original sha: a5c23a59601a1d9f3f25af5c0118a49ac4bf702d
    * Authored by ppisljar <peter.pisljar@gmail.com> on 2016-11-07T09:39:07Z
    
    **Commit 2:**
    changing to scientific notation
    
    * Original sha: b31258da3659bd0caddc1053dc855e20eb2aad85
    * Authored by ppisljar <peter.pisljar@gmail.com> on 2016-11-08T09:43:02Z

--- src/ui/public/vislib/visualizations/pie_chart.js
+++ src/ui/public/vislib/visualizations/pie_chart.js
@@ -119,6 +119,7 @@ export default function PieChartFactory(Private) {
         return Math.max(0, Math.min(2 * Math.PI, x(d.x)));
       })
       .endAngle(function (d) {
+        if (d.dx < 1e-8) return x(d.x);
         return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx)));
       })
       .innerRadius(function (d) {
