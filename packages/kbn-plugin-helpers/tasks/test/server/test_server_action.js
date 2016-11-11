b2369866d2d40361b6a4b44aaabf971d268a5b6e
spalger <email@spalger.com>
2016-11-11 09:41:38 -0700

    [test:server] include Kibana node_modules/.bin in path when running server tests
    Original commit: elastic/kibana-plugin-helpers@bbda8013518ba1909ceea12b3bb60f01a4ab9991

--- packages/kbn-plugin-helpers/tasks/test/server/test_server_action.js
+++ packages/kbn-plugin-helpers/tasks/test/server/test_server_action.js
@@ -1,13 +1,19 @@
 module.exports = function (plugin) {
   var resolve = require('path').resolve;
+  var delimiter = require('path').delimiter;
   var execFileSync = require('child_process').execFileSync;
 
+  var kibanaBins = resolve(plugin.kibanaRoot, 'node_modules/.bin');
   var mochaSetupJs = resolve(plugin.kibanaRoot, 'test/mocha_setup.js');
 
   var cmd = 'mocha';
   var args = ['--require', mochaSetupJs, 'server/**/__tests__/**/*.js'];
+  var path = `${kibanaBins}${delimiter}${process.env.PATH}`;
   execFileSync(cmd, args, {
     cwd: plugin.root,
-    stdio: 'inherit'
+    stdio: 'inherit',
+    env: Object.assign({}, process.env, {
+      PATH: path
+    })
   });
 };
