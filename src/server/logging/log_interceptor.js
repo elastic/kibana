1cce23cbd918916871096ec02cede766bc2d900b
jasper <elastic-jasper@users.noreply.github.com>
2016-11-15 03:39:04 -0500

    [server/logger] downgrade EPIPE errors to debug level (#9041)
    Backports PR #9023
    
    **Commit 1:**
    [server/logger] downgrade EPIPE errors to debug level
    
    * Original sha: 4703b367aacc6f0ea8ebbd23ce8ae7ee322300b6
    * Authored by spalger <email@spalger.com> on 2016-11-09T23:48:48Z
    
    **Commit 2:**
    fix variable naming
    
    * Original sha: fd272c5b3b3b106c886c60f87cb0f7b8cf4df62c
    * Authored by spalger <email@spalger.com> on 2016-11-11T01:06:55Z

--- src/server/logging/log_interceptor.js
+++ src/server/logging/log_interceptor.js
@@ -38,8 +38,33 @@ export class LogInterceptor extends Stream.Transform {
     };
   }
 
+  /**
+   *  Since the upgrade to hapi 14, any socket write
+   *  error is surfaced as a generic "client error"
+   *  but "EPIPE" specifically is not useful for the
+   *  logs unless you are trying to debug edge-case behaviors.
+   *
+   *  For that reason, we downgrade this from error to debug level
+   *
+   *  @param {object} - log event
+   */
+  downgradeIfEpipe(event) {
+    const isClientError = doTagsMatch(event, ['connection', 'client', 'error']);
+    const isEpipe = isClientError && get(event, 'data.errno') === 'EPIPE';
+
+    if (!isEpipe) return null;
+
+    return {
+      event: 'log',
+      pid: event.pid,
+      timestamp: event.timestamp,
+      tags: ['debug', 'connection', 'epipe'],
+      data: 'EPIPE: Socket was closed by the client (probably the browser) before the response could be completed'
+    };
+  }
+
   _transform(event, enc, next) {
-    const downgraded = this.downgradeIfEconnreset(event);
+    const downgraded = this.downgradeIfEconnreset(event) || this.downgradeIfEpipe(event);
 
     this.push(downgraded || event);
     next();
