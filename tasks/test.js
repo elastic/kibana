bba727461dcb2d3a1588f3e07a09aa6bb1fce317
CJ Cenizal <cj@cenizal.com>
2016-11-15 15:40:41 -0800

    Move checkPlugins task into its own module.

--- tasks/test.js
+++ tasks/test.js
@@ -1,35 +1,8 @@
 import _, { keys } from 'lodash';
-import fs from 'fs';
-import path from 'path';
 
 const visualRegression = require('../utilities/visual_regression');
 
 module.exports = function (grunt) {
-  grunt.registerTask('checkPlugins', 'Checks for plugins which may disrupt tests', function checkPlugins() {
-    const done = this.async();
-    const pluginsDir = path.resolve('./plugins/');
-
-    fs.readdir(pluginsDir, (err, files) => {
-      if (!files) {
-        return done();
-      }
-
-      const plugins = files.filter(file => {
-        return fs.statSync(path.join(pluginsDir, file)).isDirectory();
-      });
-
-      if (plugins.length) {
-        grunt.log.error('===================================================================================================');
-        plugins.forEach(plugin => {
-          grunt.log.error(`The ${plugin} plugin may disrupt the test process. Consider removing it and re-running your tests.`);
-        });
-        grunt.log.error('===================================================================================================');
-      }
-
-      done();
-    });
-  });
-
   grunt.registerTask('test:visualRegression', [
     'intern:visualRegression:takeScreenshots',
     'test:visualRegression:buildGallery'
