f72e45ac889427cb07546567fc67f57714c345e4
Timothy Sullivan <tsullivan@elastic.co>
2016-11-18 15:40:10 -0700

    more organization and more tests for ui_app

--- src/ui/__tests__/ui_app.js
+++ src/ui/__tests__/ui_app.js
@@ -1,8 +1,9 @@
 import expect from 'expect.js';
 import UiApp from '../ui_app.js';
 import UiExports from '../ui_exports';
+import { isUndefined, noop } from 'lodash';
 
-function getMockSpec() {
+function getMockSpec(extraParams) {
   return {
     id: 'uiapp-test',
     main: 'main.js',
@@ -14,13 +15,15 @@ function getMockSpec() {
     hidden: false,
     listed: null,
     templateName: 'ui_app_test',
+    ...extraParams
   };
 }
 describe('UiApp', () => {
   describe('constructor', () => {
+    const uiExports = new UiExports({});
+
     it('throw if ID is not given', () => {
       function newAppMissingID() {
-        const uiExports = new UiExports({});
         const spec = {}; // should have id property
         const newApp = new UiApp(uiExports, spec);
         return newApp;
@@ -28,52 +31,184 @@ describe('UiApp', () => {
       expect(newAppMissingID).to.throwException();
     });
 
-    it('successful construction with defaults', () => {
-      const uiExports = new UiExports({});
-      const spec = { id: 'uiapp-test' };
-      const newApp = new UiApp(uiExports, spec);
+    describe('defaults', () => {
+      const spec = { id: 'uiapp-test-defaults' };
+      let newApp;
+      beforeEach(() => {
+        newApp = new UiApp(uiExports, spec);
+      });
 
-      expect(newApp.uiExports).to.be.an('object');
-      expect(newApp.navLink).to.be.an('object');
-      expect(newApp.getModules).to.be.a('function');
-      expect(newApp.getInjectedVars).to.be.an('function');
+      it('use the spec ID', () => {
+        expect(newApp.id).to.be('uiapp-test-defaults');
+      });
 
-      expect(newApp.spec).to.be.an('object');
-      expect(newApp.id).to.be('uiapp-test');
+      it('creates fields with undefined value', () => {
+        // test that the fields exist, but have undefined value
+        expect('main' in newApp).to.be(true);
+        expect(isUndefined(newApp.main)).to.be(true);
 
-      expect(newApp.main).to.be(undefined);
-      expect(newApp.title).to.be(undefined);
-      expect(newApp.order).to.be(0);
-      expect(newApp.description).to.be(undefined);
-      expect(newApp.icon).to.be(undefined);
-      expect(newApp.linkToLastSubUrl).to.be(undefined);
-      expect(newApp.hidden).to.be(false);
-      expect(newApp.listed).to.be(true);
-      expect(newApp.templateName).to.be('ui_app');
+        expect('title' in newApp).to.be(true);
+        expect(isUndefined(newApp.title)).to.be(true);
+
+        expect('description' in newApp).to.be(true);
+        expect(isUndefined(newApp.description)).to.be(true);
+
+        expect('icon' in newApp).to.be(true);
+        expect(isUndefined(newApp.icon)).to.be(true);
+
+        expect('linkToLastSubUrl' in newApp).to.be(true);
+        expect(isUndefined(newApp.linkToLastSubUrl)).to.be(true);
+      });
+
+      it('default navLink', () => {
+        expect(newApp.navLink).to.eql({
+          id: 'uiapp-test-defaults',
+          title: undefined,
+          order: 0,
+          url: '/app/uiapp-test-defaults',
+          description: undefined,
+          icon: undefined,
+          linkToLastSubUrl: true,
+          hidden: false,
+          disabled: false,
+          tooltip: ''
+        });
+      });
+
+      it('default order of 0', () => {
+        expect(newApp.order).to.be(0);
+      });
+
+      it('template name is ui_app', () => {
+        expect(newApp.templateName).to.be('ui_app');
+      });
     });
 
-    it('successful construction with spec', () => {
-      const uiExports = new UiExports({});
+    describe('with spec', () => {
       const spec = getMockSpec();
-      const newApp = new UiApp(uiExports, spec);
+      let newApp;
+      beforeEach(() => {
+        newApp = new UiApp(uiExports, spec);
+      });
 
-      expect(newApp.uiExports).to.be.an('object');
-      expect(newApp.navLink).to.be.an('object');
-      expect(newApp.getModules).to.be.a('function');
-      expect(newApp.getInjectedVars).to.be.an('function');
+      it('use the spec ID', () => {
+        expect(newApp.id).to.be('uiapp-test');
+      });
 
-      expect(newApp.spec).to.be.an('object');
-      expect(newApp.id).to.be('uiapp-test');
+      it('copies field values from spec', () => {
+        // test that the fields exist, but have undefined value
+        expect(newApp.main).to.be('main.js');
+        expect(newApp.title).to.be('UIApp Test');
+        expect(newApp.description).to.be('Test of UI App Constructor');
+        expect(newApp.icon).to.be('ui_app_test.svg');
+        expect(newApp.linkToLastSubUrl).to.be(true);
+        expect(newApp.templateName).to.be('ui_app_test');
+        expect(newApp.order).to.be(9000);
+        expect(newApp.navLink).to.eql({
+          id: 'uiapp-test',
+          title: 'UIApp Test',
+          order: 9000,
+          url: '/app/uiapp-test',
+          description: 'Test of UI App Constructor',
+          icon: 'ui_app_test.svg',
+          linkToLastSubUrl: true,
+          hidden: false,
+          disabled: false,
+          tooltip: ''
+        });
+      });
+    });
 
-      expect(newApp.main).to.be('main.js');
-      expect(newApp.title).to.be('UIApp Test');
-      expect(newApp.order).to.be(9000);
-      expect(newApp.description).to.be('Test of UI App Constructor');
-      expect(newApp.icon).to.be('ui_app_test.svg');
-      expect(newApp.linkToLastSubUrl).to.be(true);
-      expect(newApp.hidden).to.be(false);
-      expect(newApp.listed).to.be(true);
-      expect(newApp.templateName).to.be('ui_app_test');
+    describe('reference fields', () => {
+      const spec = getMockSpec({ testSpec: true });
+      let newApp;
+      beforeEach(() => {
+        newApp = new UiApp(uiExports, spec);
+      });
+
+      it('uiExports', () => {
+        expect(newApp.uiExports).to.be(uiExports);
+      });
+      it('spec', () => {
+        expect(newApp.spec).to.be(spec);
+      });
+    });
+
+    describe('getInjectedVars', () => {
+      it('noop function by default', () => {
+        const spec = {
+          id: 'uiapp-test'
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.getInjectedVars).to.be(noop);
+      });
+      it('reference to spec.injectVars', () => {
+        const helloFunction = () => 'hello';
+        const spec = {
+          id: 'uiapp-test',
+          injectVars: helloFunction
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.getInjectedVars).to.be(helloFunction);
+      });
+    });
+
+    describe('hidden and listed', () => {
+      it('if hidden and listed are not set, hidden is set as false and listed is set as true', () => {
+        const spec = {
+          id: 'uiapp-test'
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.hidden).to.be(false);
+        expect(newApp.listed).to.be(true);
+      });
+      it('if listed is passed as null, and hidden is true, listed is set as false', () => {
+        const spec = {
+          id: 'uiapp-test',
+          hidden: true,
+          listed: null
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.hidden).to.be(true);
+        expect(newApp.listed).to.be(false);
+      });
+      it('if listed is passed as null, and hidden is false, listed is set as true', () => {
+        const spec = {
+          id: 'uiapp-test',
+          hidden: false,
+          listed: null
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.hidden).to.be(false);
+        expect(newApp.listed).to.be(true);
+      });
+      it('if listed is passed as null, and hidden not set, listed is set as true', () => {
+        const spec = {
+          id: 'uiapp-test',
+          listed: null
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.hidden).to.be(false);
+        expect(newApp.listed).to.be(true);
+      });
+      it('if listed is passed as true, is set with that value', () => {
+        const spec = {
+          id: 'uiapp-test',
+          listed: true
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.hidden).to.be(false);
+        expect(newApp.listed).to.be(true);
+      });
+      it('if listed is passed as false, is set with that value', () => {
+        const spec = {
+          id: 'uiapp-test',
+          listed: false
+        };
+        const newApp = new UiApp(uiExports, spec);
+        expect(newApp.hidden).to.be(false);
+        expect(newApp.listed).to.be(false);
+      });
     });
   });
 
