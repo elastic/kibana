719026009e509da75d5bf9373e47a7bdd172391e
Joe Fleming <joe.fleming@gmail.com>
2016-11-22 09:49:16 -0700

    BACKPORT: Trigger renderComplete on DOM instead of the Vis (#9176)
    * correctly use implementsRenderComplete from vis config
    
    * set implementsRenderComplete on all vis types
    
    * emit on DOM instead of the vis
    
    * vis doesn't need to be an EventEmitter
    
    * listen for renderComplete on visualize
    
    set and update element attributes as the event(s) fire, and indicate if
    they ever will
    
    * [vislib/handler] fall through to lower return
    
    * [vislibRenderbot/tests] reduce expected call count
    
    * [vis/tests] add $element to test injectors
    
    * [markdownVis] fix test

--- src/core_plugins/markdown_vis/public/__tests__/markdown_vis_controller.js
+++ src/core_plugins/markdown_vis/public/__tests__/markdown_vis_controller.js
@@ -1,24 +1,24 @@
 import ngMock from 'ng_mock';
 import expect from 'expect.js';
+import $ from 'jquery';
 
 describe('markdown vis controller', function () {
   let $scope;
-  let $el;
 
   beforeEach(ngMock.module('kibana/markdown_vis'));
   beforeEach(ngMock.inject(function ($rootScope, $controller) {
     $scope = $rootScope.$new();
-    $scope.vis = {
-      emit: function () {}
-    };
-    $controller('KbnMarkdownVisController', {$scope: $scope});
+    const $element = $('<div>');
+    $controller('KbnMarkdownVisController', { $scope, $element });
     $scope.$digest();
   }));
 
   it('should set html from markdown params', function () {
     expect($scope).to.not.have.property('html');
-    $scope.vis.params = {
-      markdown: 'This is a test of the [markdown](http://daringfireball.net/projects/markdown) vis.'
+    $scope.vis = {
+      params: {
+        markdown: 'This is a test of the [markdown](http://daringfireball.net/projects/markdown) vis.'
+      }
     };
     $scope.$digest();
 
