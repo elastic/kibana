275bf70a955f76d4ca5642cdd93f84f2ec9eacbb
spalger <email@spalger.com>
2016-11-23 15:00:52 -0700

    [docTable] remove unused var

--- src/ui/public/doc_table/doc_table.js
+++ src/ui/public/doc_table/doc_table.js
@@ -24,7 +24,7 @@ uiModules.get('kibana')
       infiniteScroll: '=?',
       filter: '=?',
     },
-    link: function ($scope, $el) {
+    link: function ($scope) {
       let notify = new Notifier();
       $scope.limit = 50;
       $scope.persist = {
