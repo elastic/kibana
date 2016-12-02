9b52ba45d2b719dda29f26e940ea5d55fc90a6ea
Thomas Neirynck <thomas@elastic.co>
2016-12-02 17:58:54 -0500

    Tag Cloud should deal with empty responses correctly
    tag cloud did not work with empty responses correctly. One of the side-effects is that the back button did not work properly. For example, pressing the back button would not empty out the screen and show stale clouds.
    
    This also caused a type-error. Empty configurations meant that we could not access any aggregation-configs to produce a label.

--- src/core_plugins/tagcloud/public/tag_cloud_controller.js
+++ src/core_plugins/tagcloud/public/tag_cloud_controller.js
@@ -21,13 +21,23 @@ module.controller('KbnTagCloudController', function ($scope, $element, Private, 
     clickHandler({point: {aggConfigResult: aggConfigResult}});
   });
   tagCloud.on('renderComplete', () => {
+
+    const truncatedMessage = containerNode.querySelector('.tagcloud-truncated-message');
+    const incompleteMessage = containerNode.querySelector('.tagcloud-incomplete-message');
+
+    if (!$scope.vis.aggs[0] || !$scope.vis.aggs[1]) {
+      incompleteMessage.style.display = 'none';
+      truncatedMessage.style.display = 'none';
+      return;
+    }
+
     const bucketName = containerNode.querySelector('.tagcloud-custom-label');
     bucketName.innerHTML = `${$scope.vis.aggs[0].makeLabel()} - ${$scope.vis.aggs[1].makeLabel()}`;
 
-    const truncatedMessage = containerNode.querySelector('.tagcloud-truncated-message');
+
     truncatedMessage.style.display = truncated ? 'block' : 'none';
 
-    const incompleteMessage = containerNode.querySelector('.tagcloud-incomplete-message');
+
     const status = tagCloud.getStatus();
 
     if (TagCloud.STATUS.COMPLETE === status) {
@@ -36,17 +46,20 @@ module.controller('KbnTagCloudController', function ($scope, $element, Private, 
       incompleteMessage.style.display = 'block';
     }
 
+
     $element.trigger('renderComplete');
   });
 
   $scope.$watch('esResponse', async function (response) {
 
     if (!response) {
+      tagCloud.setData([]);
       return;
     }
 
     const tagsAggId = _.first(_.pluck($scope.vis.aggs.bySchemaName.segment, 'id'));
     if (!tagsAggId || !response.aggregations) {
+      tagCloud.setData([]);
       return;
     }
 
