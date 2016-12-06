f36b6fc6583e1d902c99c01ca6d96a29cff7a3e7
ppisljar <peter.pisljar@gmail.com>
2016-12-06 12:20:30 +0100

    fixing unstable selenium test

--- test/support/page_objects/visualize_page.js
+++ test/support/page_objects/visualize_page.js
@@ -581,7 +581,7 @@ export default class VisualizePage {
       return self
       .setFindTimeout(defaultFindTimeout * 2)
       // #kibana-body > div.content > div > div > div > div.vis-editor-canvas > visualize > div.visualize-chart > div > div.vis-col-wrapper > div.chart-wrapper > div > svg > g > g.series.\30 > rect:nth-child(1)
-      .findAllByCssSelector('svg > g > g.series.\\30 > rect') // rect
+      .findAllByCssSelector('svg > g > g.series > rect') // rect
       .then(function (chartTypes) {
         function getChartType(chart) {
           return chart
