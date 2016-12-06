aaab047e1cad27012c8a6fb2ba17555d84852a80
Jonathan Budzenski <jon@jbudz.me>
2016-12-06 11:38:51 -0600

    [tribe] Style fixes on disabling dev tools

--- src/core_plugins/kibana/public/kibana.js
+++ src/core_plugins/kibana/public/kibana.js
@@ -51,11 +51,15 @@ chrome
 modules.get('kibana').run(Notifier.pullMessageFromUrl);
 
 modules.get('kibana').run(function ($rootScope, $location) {
-  if (!chrome.getInjected('tribeEnabled')) return;
+  if (!chrome.getInjected('tribeEnabled')) {
+    return;
+  }
   const navLink = chrome.getNavLinkById('kibana:dev_tools');
   navLink.disabled = true;
   navLink.tooltip = 'Dev Tools are disabled when using tribe nodes';
   $rootScope.$on('$locationChangeStart', function (event, newUrl) {
-    if (~newUrl.indexOf(navLink.url)) $location.path('/').replace();
+    if (~newUrl.indexOf(navLink.url)) {
+      $location.path('/').replace();
+    }
   });
 });
