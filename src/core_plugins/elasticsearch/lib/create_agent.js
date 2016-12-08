077d408690125b81079858b8b425f2ba9b6c037f
Jonathan Budzenski <jbudz@users.noreply.github.com>
2016-12-08 11:01:12 -0600

    [tribe] Pull proxy agent config from cluster (#9411)
    * [tribe] Pull proxy agent config from cluster
    
    * [tribe] Reuse proxy config

--- src/core_plugins/elasticsearch/lib/create_agent.js
+++ src/core_plugins/elasticsearch/lib/create_agent.js
@@ -4,24 +4,23 @@ const readFile = (file) => require('fs').readFileSync(file, 'utf8');
 import http from 'http';
 import https from 'https';
 
-module.exports = _.memoize(function (server) {
-  const config = server.config();
-  const target = url.parse(config.get('elasticsearch.url'));
+module.exports = _.memoize(function (config) {
+  const target = url.parse(_.get(config, 'url'));
 
   if (!/^https/.test(target.protocol)) return new http.Agent();
 
   const agentOptions = {
-    rejectUnauthorized: config.get('elasticsearch.ssl.verify')
+    rejectUnauthorized: _.get(config, 'ssl.verify')
   };
 
-  if (_.size(config.get('elasticsearch.ssl.ca'))) {
-    agentOptions.ca = config.get('elasticsearch.ssl.ca').map(readFile);
+  if (_.size(_.get(config, 'ssl.ca'))) {
+    agentOptions.ca = _.get(config, 'ssl.ca').map(readFile);
   }
 
   // Add client certificate and key if required by elasticsearch
-  if (config.get('elasticsearch.ssl.cert') && config.get('elasticsearch.ssl.key')) {
-    agentOptions.cert = readFile(config.get('elasticsearch.ssl.cert'));
-    agentOptions.key = readFile(config.get('elasticsearch.ssl.key'));
+  if (_.get(config, 'ssl.cert') && _.get(config, 'ssl.key')) {
+    agentOptions.cert = readFile(_.get(config, 'ssl.cert'));
+    agentOptions.key = readFile(_.get(config, 'ssl.key'));
   }
 
   return new https.Agent(agentOptions);
