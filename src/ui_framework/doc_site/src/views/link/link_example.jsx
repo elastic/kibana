644a267adab1cc58080cf530c2f0eabef7821465
CJ Cenizal <cj@cenizal.com>
2016-12-13 09:22:39 -0800

    Add new components to UI Framework. - Icon, Link, Button, TextInput, CheckBox, Table, ControlledTable, ToolBar, ToolBarFooter. - Update Local Nav to use classes with kui prefix. - Add PostCSS loader to UI Framework webpack.

---
+++ src/ui_framework/doc_site/src/views/link/link_example.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {
+  createExample,
+} from '../../services';
+
+export default createExample([{
+  title: 'Link',
+  html: require('./link.html'),
+  hasDarkTheme: false,
+}]);
