1e315d92c9ccf0f0102f3c8f1d773c1c1dbbe852
CJ Cenizal <cj@cenizal.com>
2016-12-12 17:54:56 -0800

    Migrate UI Framework source into Kibana. (#9192)
    Migrate UI Framework source into Kibana.
    - Add dependencies to package.json.
    - Add task for building UI Framework docs and serving locally.
    - Import UI Framework scss from autoload/styles.js instead of importing the CSS in base.less.
    - Refactor UI Framework directory structure.
    - Remove babel-polyfill.
    - Include SCSS synchronously.

---
+++ src/ui_framework/doc_site/src/actions/action_types.js
@@ -0,0 +1,13 @@
+
+import keyMirror from 'keymirror';
+
+export default keyMirror({
+
+  // Source code viewer actions
+  OPEN_CODE_VIEWER: null,
+  UPDATE_CODE_VIEWER: null,
+  CLOSE_CODE_VIEWER: null,
+  REGISTER_CODE: null,
+  UNREGISTER_CODE: null,
+
+});
---
+++ src/ui_framework/doc_site/src/components/index.js
@@ -0,0 +1,21 @@
+
+export * from './guide_code_viewer/guide_code_viewer.jsx';
+export { default as GuideCodeViewer } from './guide_code_viewer/guide_code_viewer.jsx';
+
+export * from './guide_example/guide_example.jsx';
+export { default as GuideExample } from './guide_example/guide_example.jsx';
+
+export * from './guide_nav/guide_nav.jsx';
+export { default as GuideNav } from './guide_nav/guide_nav.jsx';
+
+export * from './guide_page/guide_page.jsx';
+export { default as GuidePage } from './guide_page/guide_page.jsx';
+
+export * from './guide_page_section/guide_page_section.jsx';
+export { default as GuidePageSection } from './guide_page_section/guide_page_section.jsx';
+
+export * from './guide_page_side_nav/guide_page_side_nav.jsx';
+export { default as GuidePageSideNav } from './guide_page_side_nav/guide_page_side_nav.jsx';
+
+export * from './guide_page_side_nav/guide_page_side_nav_item.jsx';
+export { default as GuidePageSideNavItem } from './guide_page_side_nav/guide_page_side_nav_item.jsx';
