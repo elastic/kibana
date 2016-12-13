10adba3d8b2285d2f74f248beebdaf2503c1acb7
CJ Cenizal <cj@cenizal.com>
2016-12-12 17:56:16 -0800

    Migrate UI Framework source into Kibana.
    Backports PR #9192
    
    **Commit 1:**
    Migrate UI Framework source into Kibana.
    - Add dependencies to package.json.
    - Add task for building UI Framework docs and serving locally.
    - Import UI Framework scss from autoload/styles.js instead of importing the CSS in base.less.
    
    * Original sha: de3f34f8d5eed3d08b2d3009dfb29e2318ace8b9
    * Authored by CJ Cenizal <cj@cenizal.com> on 2016-11-22T22:27:11Z
    
    **Commit 2:**
    Refactor UI Framework directory structure.
    
    * Original sha: 4e548445771fa04cffb6d5a230c9d4be51f8e1c7
    * Authored by CJ Cenizal <cj@cenizal.com> on 2016-12-05T23:03:01Z
    
    **Commit 3:**
    Remove babel-polyfill.
    
    * Original sha: 43854bccc4312c63a7243d8309cdda802530f4ab
    * Authored by CJ Cenizal <cj@cenizal.com> on 2016-12-05T23:47:42Z
    
    **Commit 4:**
    Fix typos and include SCSS synchronously.
    
    * Original sha: fe5bab1d81af843e997027db8eb76e45be5a28f7
    * Authored by CJ Cenizal <cj@cenizal.com> on 2016-12-10T00:33:17Z
    
    **Commit 5:**
    Update with latest changes to UI Framework.
    
    * Original sha: a44140a59d066ef735a184dbf8d4b72db4f9b048
    * Authored by CJ Cenizal <cj@cenizal.com> on 2016-12-13T01:47:09Z

---
+++ src/ui_framework/doc_site/src/views/not_found/not_found_view.jsx
@@ -0,0 +1,20 @@
+
+import React, {
+  Component,
+} from 'react';
+
+export default class NotFoundView extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Page not found.</h1>
+      </div>
+    );
+  }
+
+}
