f87e80838ab19e68c631d1de0cde6c3ca2581351
ppisljar <peter.pisljar@gmail.com>
2016-12-14 11:20:57 +0100

    fixes Cannot read property 'orderBucketsBySum' of undefined

--- src/ui/public/vislib/lib/data.js
+++ src/ui/public/vislib/lib/data.js
@@ -42,7 +42,6 @@ export default function DataFactory(Private) {
 
       this.labels = this._getLabels(this.data);
       this.color = this.labels ? color(this.labels, uiState.get('vis.colors')) : undefined;
-      this._normalizeOrdered();
 
       this._attr = _.defaults(attr || {}, {
         stack: d3.layout.stack()
@@ -63,6 +62,8 @@ export default function DataFactory(Private) {
           return self._stackNegAndPosVals(d, y0, y);
         });
       }
+
+      this._normalizeOrdered();
     }
 
     _getLabels(data) {
