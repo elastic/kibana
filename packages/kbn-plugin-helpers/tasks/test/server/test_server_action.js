8b6ac6469cc9b2f892f7a01cb97a67f2ef17891d
Joe Fleming <joe.fleming@gmail.com>
2016-12-15 12:33:40 -0700

    split and concat and included server test paths
    Original commit: elastic/kibana-plugin-helpers@e7dffb6ffe526c621e2fd1522c1c6d0f18a6abc2

--- packages/kbn-plugin-helpers/tasks/test/server/test_server_action.js
+++ packages/kbn-plugin-helpers/tasks/test/server/test_server_action.js
@@ -2,13 +2,21 @@ var resolve = require('path').resolve;
 var delimiter = require('path').delimiter;
 var execFileSync = require('child_process').execFileSync;
 
-module.exports = function (plugin) {
+module.exports = function (plugin, run, command) {
+  command = command || {};
+
   var kibanaBins = resolve(plugin.kibanaRoot, 'node_modules/.bin');
   var mochaSetupJs = resolve(plugin.kibanaRoot, 'test/mocha_setup.js');
 
   var cmd = 'mocha';
   var args = ['--require', mochaSetupJs, 'server/**/__tests__/**/*.js'];
   var path = `${kibanaBins}${delimiter}${process.env.PATH}`;
+
+  if (command.include) {
+    var globs = command.include.split(',');
+    args = args.concat(globs);
+  }
+
   execFileSync(cmd, args, {
     cwd: plugin.root,
     stdio: ['ignore', 1, 2],
