1e4f1e600600bf69c24a340874932715a4fc2e93
Jonathan Budzenski <jon@jbudz.me>
2016-12-16 15:47:46 -0600

    [tribe] Use class syntax for client request wrapper

--- src/ui/public/es.js
+++ src/ui/public/es.js
@@ -12,11 +12,8 @@ import uiModules from 'ui/modules';
 const plugins = [function (Client, config) {
   // esFactory automatically injects the AngularConnector to the config
   // https://github.com/elastic/elasticsearch-js/blob/master/src/lib/connectors/angular.js
-  _.class(CustomAngularConnector).inherits(config.connectionClass);
-  function CustomAngularConnector(host, config) {
-    CustomAngularConnector.Super.call(this, host, config);
-
-    this.request = _.wrap(this.request, function (request, params, cb) {
+  class CustomAngularConnector extends config.connectionClass {
+    request = _.wrap(this.request, function (request, params, cb) {
       if (String(params.method).toUpperCase() === 'GET') {
         params.query = _.defaults({ _: Date.now() }, params.query);
       }
