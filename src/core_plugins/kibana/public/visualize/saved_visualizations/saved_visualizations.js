3fafc31cec487136014a2892dabbca4bfb6c9e6b
Stacey Gammon <gammon@elastic.co>
2016-12-19 12:26:51 -0500

    [5.x] Refactor loading saved objects (Manual Backport)

--- src/core_plugins/kibana/public/visualize/saved_visualizations/saved_visualizations.js
+++ src/core_plugins/kibana/public/visualize/saved_visualizations/saved_visualizations.js
@@ -3,6 +3,8 @@ import Scanner from 'ui/utils/scanner';
 import 'plugins/kibana/visualize/saved_visualizations/_saved_vis';
 import RegistryVisTypesProvider from 'ui/registry/vis_types';
 import uiModules from 'ui/modules';
+import { SavedObjectLoader } from 'ui/courier/saved_object/saved_object_loader';
+
 const app = uiModules.get('app/visualize');
 
 
@@ -15,48 +17,12 @@ require('plugins/kibana/management/saved_object_registry').register({
 
 app.service('savedVisualizations', function (Promise, es, kbnIndex, SavedVis, Private, Notifier, kbnUrl) {
   const visTypes = Private(RegistryVisTypesProvider);
-
-  const scanner = new Scanner(es, {
-    index: kbnIndex,
-    type: 'visualization'
-  });
-
   const notify = new Notifier({
     location: 'Saved Visualization Service'
   });
 
-  this.type = SavedVis.type;
-  this.Class = SavedVis;
-
-  this.loaderProperties = {
-    name: 'visualizations',
-    noun: 'Visualization',
-    nouns: 'visualizations'
-  };
-
-  this.get = function (id) {
-    return (new SavedVis(id)).init();
-  };
-
-  this.urlFor = function (id) {
-    return kbnUrl.eval('#/visualize/edit/{{id}}', {id: id});
-  };
-
-  this.delete = function (ids) {
-    ids = !_.isArray(ids) ? [ids] : ids;
-    return Promise.map(ids, function (id) {
-      return (new SavedVis(id)).delete();
-    });
-  };
-
-  this.scanAll = function (queryString, pageSize = 1000) {
-    return scanner.scanAndMap(queryString, {
-      pageSize,
-      docCount: Infinity
-    }, (hit) => this.mapHits(hit));
-  };
-
-  this.mapHits = function (hit) {
+  const saveVisualizationLoader = new SavedObjectLoader(SavedVis, kbnIndex, es, kbnUrl);
+  saveVisualizationLoader.mapHits = function (hit) {
     const source = hit._source;
     source.id = hit._id;
     source.url = this.urlFor(hit._id);
@@ -70,7 +36,7 @@ app.service('savedVisualizations', function (Promise, es, kbnIndex, SavedVis, Pr
     if (!typeName || !visTypes.byName[typeName]) {
       if (!typeName) notify.error('Visualization type is missing. Please add a type to this visualization.', hit);
       else notify.error('Visualization type of "' + typeName + '" is invalid. Please change to a valid type.', hit);
-      return kbnUrl.redirect('/management/kibana/objects/savedVisualizations/{{id}}', {id: source.id});
+      return kbnUrl.redirect('/management/kibana/objects/savedVisualizations/{{id}}', { id: source.id });
     }
 
     source.type = visTypes.byName[typeName];
@@ -78,34 +44,9 @@ app.service('savedVisualizations', function (Promise, es, kbnIndex, SavedVis, Pr
     return source;
   };
 
-  this.find = function (searchString, size = 100) {
-    let body;
-    if (searchString) {
-      body = {
-        query: {
-          simple_query_string: {
-            query: searchString + '*',
-            fields: ['title^3', 'description'],
-            default_operator: 'AND',
-            analyze_wildcard: true
-          }
-        }
-      };
-    } else {
-      body = { query: {match_all: {}}};
-    }
-
-    return es.search({
-      index: kbnIndex,
-      type: 'visualization',
-      body: body,
-      size: size
-    })
-    .then((resp) => {
-      return {
-        total: resp.hits.total,
-        hits: resp.hits.hits.map((hit) => this.mapHits(hit))
-      };
-    });
+  saveVisualizationLoader.urlFor = function (id) {
+    return kbnUrl.eval('#/visualize/edit/{{id}}', { id: id });
   };
+
+  return saveVisualizationLoader;
 });
--- src/core_plugins/timelion/public/services/saved_sheets.js
+++ src/core_plugins/timelion/public/services/saved_sheets.js
@@ -1,3 +1,5 @@
+import { SavedObjectLoader } from 'ui/courier/saved_object/saved_object_loader';
+
 define(function (require) {
   var module = require('ui/modules').get('app/sheet');
   var _ = require('lodash');
@@ -14,66 +16,15 @@ define(function (require) {
 
   // This is the only thing that gets injected into controllers
   module.service('savedSheets', function (Promise, SavedSheet, kbnIndex, es, kbnUrl) {
-    this.type = SavedSheet.type;
-    this.Class = SavedSheet;
-
-    this.loaderProperties = {
+    const savedSheetLoader = new SavedObjectLoader(SavedSheet, kbnIndex, es, kbnUrl);
+    savedSheetLoader.urlFor = function (id) {
+      return kbnUrl.eval('#/{{id}}', { id: id });
+    };
+    savedSheetLoader.loaderProperties = {
       name: 'timelion-sheet',
       noun: 'Saved Sheets',
       nouns: 'saved sheets'
     };
-
-    // Returns a single sheet by ID, should be the name of the sheet
-    this.get = function (id) {
-      // Returns a promise that contains a sheet which is a subclass of docSource
-      return (new SavedSheet(id)).init();
-    };
-
-    this.urlFor = function (id) {
-      return kbnUrl.eval('#/{{id}}', {id: id});
-    };
-
-    this.delete = function (ids) {
-      ids = !_.isArray(ids) ? [ids] : ids;
-      return Promise.map(ids, function (id) {
-        return (new SavedSheet(id)).delete();
-      });
-    };
-
-    this.find = function (searchString) {
-      var self = this;
-      var body;
-      if (searchString) {
-        body = {
-          query: {
-            simple_query_string: {
-              query: searchString + '*',
-              fields: ['title^3', 'description'],
-              default_operator: 'AND'
-            }
-          }
-        };
-      } else {
-        body = { query: {match_all: {}}};
-      }
-
-      return es.search({
-        index: kbnIndex,
-        type: 'timelion-sheet',
-        body: body,
-        size: 1000
-      })
-      .then(function (resp) {
-        return {
-          total: resp.hits.total,
-          hits: resp.hits.hits.map(function (hit) {
-            var source = hit._source;
-            source.id = hit._id;
-            source.url = self.urlFor(hit._id);
-            return source;
-          })
-        };
-      });
-    };
+    return savedSheetLoader;
   });
 });
