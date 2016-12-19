1924d28ed86bf02eb04c081bf31b2344212a872c
Stacey Gammon <gammon@elastic.co>
2016-12-19 10:32:52 -0500

    Refactor loading saved objects (#9406)
    * Refactor code that loads saved objects
    
    * couple fixes and comments
    
    cleanup & comments
    
    cleanup
    
    * Use es6 classes, make a note about getting rid of the 100 hard limit on results
    
    * Address comments
    
    * fix linter
    
    * lint fixes with { } spacing
    
    * Fix linter
    
    * fix forgotten merge conflict
    
    * and linter

--- src/core_plugins/kibana/public/discover/saved_searches/saved_searches.js
+++ src/core_plugins/kibana/public/discover/saved_searches/saved_searches.js
@@ -3,7 +3,7 @@ import Scanner from 'ui/utils/scanner';
 import 'plugins/kibana/discover/saved_searches/_saved_search';
 import 'ui/notify';
 import uiModules from 'ui/modules';
-
+import { SavedObjectLoader } from 'ui/courier/saved_object/saved_object_loader';
 
 const module = uiModules.get('discover/saved_searches', [
   'kibana/notify'
@@ -17,82 +17,16 @@ require('plugins/kibana/management/saved_object_registry').register({
 });
 
 module.service('savedSearches', function (Promise, config, kbnIndex, es, createNotifier, SavedSearch, kbnUrl) {
-  const scanner = new Scanner(es, {
-    index: kbnIndex,
-    type: 'search'
-  });
-
-  const notify = createNotifier({
-    location: 'Saved Searches'
-  });
-
-  this.type = SavedSearch.type;
-  this.Class = SavedSearch;
-
-  this.loaderProperties = {
+  const savedSearchLoader = new SavedObjectLoader(SavedSearch, kbnIndex, es, kbnUrl);
+  // Customize loader properties since adding an 's' on type doesn't work for type 'search' .
+  savedSearchLoader.loaderProperties = {
     name: 'searches',
     noun: 'Saved Search',
     nouns: 'saved searches'
   };
 
-
-  this.scanAll = function (queryString, pageSize = 1000) {
-    return scanner.scanAndMap(queryString, {
-      pageSize,
-      docCount: Infinity
-    }, (hit) => this.mapHits(hit));
-  };
-
-
-  this.get = function (id) {
-    return (new SavedSearch(id)).init();
-  };
-
-  this.urlFor = function (id) {
+  savedSearchLoader.urlFor = function (id) {
     return kbnUrl.eval('#/discover/{{id}}', { id: id });
   };
-
-  this.delete = function (ids) {
-    ids = !_.isArray(ids) ? [ids] : ids;
-    return Promise.map(ids, function (id) {
-      return (new SavedSearch(id)).delete();
-    });
-  };
-
-  this.mapHits = function (hit) {
-    const source = hit._source;
-    source.id = hit._id;
-    source.url = this.urlFor(hit._id);
-    return source;
-  };
-
-  this.find = function (searchString, size = 100) {
-    let body;
-    if (searchString) {
-      body = {
-        query: {
-          simple_query_string: {
-            query: searchString + '*',
-            fields: ['title^3', 'description'],
-            default_operator: 'AND'
-          }
-        }
-      };
-    } else {
-      body = { query: { match_all: {} } };
-    }
-
-    return es.search({
-      index: kbnIndex,
-      type: 'search',
-      body: body,
-      size: size
-    })
-    .then((resp) => {
-      return {
-        total: resp.hits.total,
-        hits: resp.hits.hits.map((hit) => this.mapHits(hit))
-      };
-    });
-  };
+  return savedSearchLoader;
 });
