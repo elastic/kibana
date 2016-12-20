fb6ba11ab89920a367f4e8a52a63e0ef31eba8d0
Court Ewing <court@epixa.com>
2016-12-20 18:47:53 -0500

    build: remove deepModules hackery
    Backports PR #9327
    
    **Commit 1:**
    build: remove deepModules hackery
    
    The deepModules hacks in the build system were added to support the long
    paths that resulted from npm2, but npm3 fundamentally addresses that
    problem, so deepModules is no longer necessary. In practical terms, npm3
    shouldn't ever cause path lengths to become so long that they trigger
    path length problems on certain operating systems.
    
    * Original sha: 19fcc93ad3a6b97fea30a8f837d5256675aaf3b8
    * Authored by Court Ewing <court@epixa.com> on 2016-12-02T00:13:13Z

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,22 +45,7 @@ module.exports = function (grunt) {
       '<%= src %>/**/*.js',
       '!<%= src %>/fixtures/**/*.js',
       '!<%= root %>/test/fixtures/scenarios/**/*.js'
-    ],
-    deepModules: {
-      'caniuse-db': '1.0.30000265',
-      'chalk': '1.1.0',
-      'glob': '4.5.3',
-      'har-validator': '1.8.0',
-      'json5': '0.4.0',
-      'loader-utils': '0.2.11',
-      'micromatch': '2.2.0',
-      'postcss-normalize-url': '2.1.1',
-      'postcss-reduce-idents': '1.0.2',
-      'postcss-unique-selectors': '1.0.0',
-      'postcss-minify-selectors': '1.4.6',
-      'postcss-single-charset': '0.3.0',
-      'regenerator': '0.8.36'
-    }
+    ]
   };
 
   grunt.config.merge(config);
