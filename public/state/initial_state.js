55934eae18f6ec4515ca21762d9b1e81dfd3f366
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-10 14:24:15 -0700

    Reorganize state into a friendlier structure

--- public/state/initial_state.js
+++ public/state/initial_state.js
@@ -5,54 +5,59 @@ export default {
   }, // Things that don't survive a refresh
   persistent: { // Stuff that should survive, and be serialized and saved
     workpad: {
-      pages: [
-        {
-          id: 'foo',
-          style: {
-            backgroundColor: '#c66'
-          },
-          elements: [
-            {
-              id: '345263-f275g9w-43523-34-2524',
-              type: 'json',
-              height: 100,
-              width: 200,
-              top: 20,
-              left: 30,
-              props: {}
-            },
-            {
-              id: '34785fh2-425-22-22524',
-              type: 'table',
-              height: 100,
-              width: 100,
-              top: 200,
-              left: 200,
-              props: {}
-            }
-          ],
-        },
-        {
-          id: 'bar',
-          style: {
-            backgroundColor: '#6c6'
-          },
-          elements: [
-            {
-              id: '43859-34534252452-2345-34-2524',
-              type: 'table',
-              height: 100,
-              width: 100,
-              top: 300,
-              left: 50,
-              props: {}
-            }
-          ],
-        }
-      ],
+      id: 'workpad-485723-3425-2345324',
+      pages: ['page-0', 'page-1'], // In theory you could reference a page multiple times, but you know, don't.
       height: 600,
       width: 400,
       page: 0,
+    },
+    pages: {
+      'page-0': {
+        id: 'page-0',
+        style: {
+          backgroundColor: '#c66'
+        },
+        elements: ['element-0', 'element-2'] // Same deal here. I'm watching you.
+      },
+      'page-1': {
+        id: 'page-1',
+        style: {
+          backgroundColor: '#6c6'
+        },
+        elements: ['element-1']
+      }
+    },
+    elements: {
+      'element-0': {
+        id: 'element-0',
+        type: 'json',
+        height: 100,
+        width: 200,
+        top: 20,
+        left: 30,
+        angle: 0,
+        props: {}
+      },
+      'element-2': {
+        id: 'element-2',
+        type: 'table',
+        height: 100,
+        width: 100,
+        top: 200,
+        left: 200,
+        angle: 30,
+        props: {}
+      },
+      'element-1': {
+        id: 'element-1',
+        type: 'table',
+        height: 100,
+        width: 100,
+        top: 300,
+        left: 50,
+        angle: 270,
+        props: {}
+      }
     },
     dataframes: {}
   },
