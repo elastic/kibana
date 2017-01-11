bd3bbb12678a52d9515c15f94a8b7aea1f8455f3
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-10 21:53:54 -0700

    Inline styles, make component child agnostic

--- public/components/page/page.js
+++ public/components/page/page.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import './page.less';
-import _ from 'lodash';
-import PageElement from 'plugins/rework/components/page_element/page_element';
-import Element from 'plugins/rework/components/element/element';
 
 export default React.createClass({
   render() {
-    const {page, elements} = this.props;
-    const {id, style} = page;
+    const {id, style} = this.props.page;
+    const pageStyle = {
+      ...style,
+      height: '100%',
+      width: '100%',
+      position: 'relative',
+    };
 
-    const orderedElements = page.elements;
-
-    const elementComponents = _.map(orderedElements, (elementId) => {
-      const element = elements[elementId];
-      const layout = _.pick(element, ['top', 'left', 'height', 'width', 'angle']);
-      return (
-        <PageElement key={element.id} id={element.id} layout={layout} style={element.props._style}>
-          <Element type={element.type} properties={element.props}></Element>
-        </PageElement>
-      );
-    });
     return (
-      <div className="rework--page" style={style}>
-        {elementComponents}
+      <div className="rework--page" style={pageStyle}>
+        {this.props.children}
       </div>
     );
   }
