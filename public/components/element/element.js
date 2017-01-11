d5f3b74d229af737a9a04c20497b4cc8a0994ac1
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-11 15:43:25 -0700

    Make element component dumber, it doesnt need the entire element object, just the type and RESOLVED args

--- public/components/element/element.js
+++ public/components/element/element.js
@@ -3,7 +3,7 @@ import elements from 'plugins/rework/elements/elements';
 
 export default React.createClass({
   render() {
-    const {type, args} = this.props.element;
+    const {type, args} = this.props;
     const ElementContent = elements.byName[type].template;
     return (
       <div className="rework--element">
