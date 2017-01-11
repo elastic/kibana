e8205c5056fdb86d0e81721ef3e3a99c01594386
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-10 21:52:19 -0700

    Remove unused state binding

--- public/components/editor/editor.js
+++ public/components/editor/editor.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import _ from 'lodash';
-import { connect } from 'react-redux';
-import { editorToggle } from 'plugins/rework/state/actions';
 
-import './editor.less';
-
-const Editor = React.createClass({
-  editorToggle() {
-    const { dispatch } = this.props;
-    dispatch(editorToggle());
-  },
+export default React.createClass({
   render() {
     return (
       <div className="rework--editor">
@@ -17,11 +8,3 @@ const Editor = React.createClass({
     );
   }
 });
-
-function mapStateToProps(state) {
-  return {
-    editor: state.transient.editor
-  };
-}
-
-export default connect(mapStateToProps)(Editor);
