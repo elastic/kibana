2dcb8bd3be58c50fdba6338fa61b62d840559696
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-12 08:44:19 -0700

    Use loading image

--- public/components/element/element.js
+++ public/components/element/element.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import elements from 'plugins/rework/elements/elements';
+import Loading from 'plugins/rework/components/loading/loading';
 
 export default React.createClass({
   render() {
     const {type, args} = this.props;
-    const ElementContent = args ? elements.byName[type].template : () => (<div>Pending</div>);
+    const ElementContent = args ? elements.byName[type].template : () => (<Loading />);
     return (
       <div className="rework--element">
         <ElementContent args={args}></ElementContent>
