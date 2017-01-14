3192a3da8d2da12d07c043032fba31b77ea8c010
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-13 17:20:37 -0700

    A component for alterning arguments

---
+++ public/components/argument_form/argument_form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import argTypes from 'plugins/rework/arg_types/arg_types';
+
+export default React.createClass({
+  render() {
+    const {type, value, commit, help} = this.props;
+    const Form = argTypes.byName[type].form;
+    const helpText = help || argTypes.byName[type].help;
+
+    return (
+      <div className="rework--argument-form">
+        <Form commit={commit} value={value}></Form>
+        <small>{helpText}</small>
+      </div>
+    );
+  }
+});
