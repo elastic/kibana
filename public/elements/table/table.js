810b74ab8c57e5554fc95c5c6b1add9e5b3a0997
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-17 14:17:00 -0700

    Add something to render dataframes

---
+++ public/elements/table/table.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import _ from 'lodash';
+import Element from 'plugins/rework/elements/element';
+import elements from 'plugins/rework/elements/elements';
+import Dataframe from 'plugins/rework/arg_types/dataframe/lib/dataframe';
+
+import Arg from 'plugins/rework/arg_types/arg';
+
+elements.push(new Element('table', {
+  displayName: 'Table',
+  args: [
+    new Arg('dataframe', {
+      type: 'dataframe',
+      default: null
+    })
+  ],
+  template: ({args}) => {
+    const dataframe = new Dataframe(args.dataframe);
+    const header = _.map(dataframe.columns.ordered, (column) => (
+      <th className={`reframe--type--${column.type}`} key={column.name}>
+        {column.name}
+      </th>
+    ));
+
+    const rows = _.map(dataframe.toTuples, (row, i) => {
+      const fields = _.map(row, field => (
+        <td className={`reframe--type--${field.column.type}`} key={field.column.name}>
+          {field.value}
+        </td>)
+      );
+
+      return (<tr key={i}>{fields}</tr>);
+    });
+
+    return (
+      <div>
+        <table className="table">
+          <thead><tr>{header}</tr></thead>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    );
+  }
+}));
