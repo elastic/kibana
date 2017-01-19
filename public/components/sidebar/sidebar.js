b5cde38bb4bbb8f2174e848d2397c895ae1e6815
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-19 11:14:22 -0800

    Rename left sidebar to sidebar

--- public/components/sidebar/sidebar.js
+++ public/components/sidebar/sidebar.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default React.createClass({
   render() {
+    const {position} = this.props;
     const style = {
       display: 'flex',
       position: 'relative'
     };
 
+    const sidebarClass = `rework--sidebar-${position}`;
+
     return (
-      <div className="rework--left-sidebar" style={style}>
+      <div className={sidebarClass} style={style}>
         {this.props.children}
       </div>
     );
