2fccac67d5343ea3bffe8e17088fb74d713cc372
Stacey Gammon <gammon@elastic.co>
2017-01-19 14:51:22 -0500

    Remove the need to pass state and parentUiState to dashboard_panel directive (#9769)
    * - Remove need for passing state to dashboard_panel directive.
    
    get rid of parentUiState
    
    * Address code review comments
    
    * Only do savedSearch specific init stuff for searches
    
    * Throw Error object, not string
    
    * Remove new patterns from this PR
    
    Removing the new patterns from this PR to preserve consistency.

---
+++ src/core_plugins/kibana/public/dashboard/services/get_object_loaders_for_dashboard.js
@@ -0,0 +1,11 @@
+import uiModules from 'ui/modules';
+const module = uiModules.get('app/dashboard');
+
+/**
+ * We have more types available than just 'search' and 'visualization' but as of now, they
+ * can't be added to a dashboard.
+ */
+module.factory('getObjectLoadersForDashboard', function (savedSearches, savedVisualizations) {
+  return () => [savedSearches, savedVisualizations];
+});
+
