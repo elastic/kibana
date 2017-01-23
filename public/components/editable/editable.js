cc67a42b984c68cb37be943aa390ca542277f26d
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-23 09:41:25 -0700

    Add an Editable element for in place content editing

---
+++ public/components/editable/editable.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import _ from 'lodash';
+import './editable.less';
+
+export default React.createClass({
+  change(e) {
+    this.props.onChange(e.target.value);
+  },
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.refs.editableInput.blur();
+    }
+  },
+  render() {
+    const {value, onChange, className} = this.props;
+
+    return (
+      <input
+        type="text"
+        ref="editableInput"
+        className={['form-control rework--editable', className].join(' ')}
+        value={value}
+        onChange={this.change}
+        onKeyPress={this.handleKeyPress}/>
+    );
+  }
+});
