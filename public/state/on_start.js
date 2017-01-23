6c2b0425e39c2e6e946d3d17b6b5e60fd68653c7
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-22 21:37:22 -0700

    Write persistent state on change

--- public/state/on_start.js
+++ public/state/on_start.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import {dataframeResolve} from './actions/dataframe';
 import {elementResolve} from './actions/element';
+import fetch from 'isomorphic-fetch';
+import moment from 'moment';
 
 
 /*
@@ -28,9 +30,36 @@ POST /.kibana/the_rework_1/_mapping
 // Stuff todo when you bootstrap the store
 export default function (store) {
   const {getState, dispatch} = store;
+  const safeSave = _.debounce(saveOnChange, 1000, {maxWait: 5000});
 
   resolveElements();
   resolveDataframes();
+  store.subscribe(safeSave);
+
+  function propDidChange(previousState, currentState, prop) {
+    return (_.get(previousState, prop) !== _.get(currentState, prop));
+  }
+
+  let currentState;
+  function saveOnChange() {
+    let previousState = currentState;
+    currentState = store.getState();
+    if (!propDidChange(previousState, currentState, 'persistent')) return;
+
+    const storable = Object.assign({}, currentState.persistent, {'@timestamp': moment().toISOString()});
+    const timelionResp =  fetch('../api/rework/save', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'kbn-xsrf': 'turdSandwich',
+      },
+      body: JSON.stringify(storable)
+    })
+    .then(resp => resp.json()).then(resp => {
+      console.log(resp);
+    });
+  }
 
   function resolveDataframes() {
     const ids = _.keys(getState().persistent.dataframes);
