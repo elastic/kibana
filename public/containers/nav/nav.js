885ca44338989bb25296d5e290242b24074102c8
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-23 17:57:50 -0700

    Wire up layer buttons

--- public/containers/nav/nav.js
+++ public/containers/nav/nav.js
@@ -5,7 +5,10 @@ import TopNav from 'plugins/rework/components/top_nav/top_nav';
 import NavButton from 'plugins/rework/components/nav_button/nav_button';
 import Editable from 'plugins/rework/components/editable/editable';
 import { workpadName, workpadNew } from 'plugins/rework/state/actions/workpad';
+import { elementLayerMove } from 'plugins/rework/state/actions/element';
 import { dropdownToggle } from 'plugins/rework/state/actions/misc';
+
+import './nav.less';
 
 
 const DataframeDialog = React.createClass({
@@ -20,8 +23,16 @@ const DataframeDialog = React.createClass({
       this.props.dispatch(dropdownToggle(name));
     };
   },
+  elementLayer(movement) {
+    return () => {
+      const {dispatch, selectedElementId, currentPageId} = this.props;
+      this.props.dispatch(elementLayerMove(selectedElementId, currentPageId, movement));
+    };
+  },
   render() {
-    const {workpad} = this.props;
+    const {workpad, selectedElementId, currentPageId} = this.props;
+    const layerClasses = ['rework--nav--layer-buttons'];
+    if (!selectedElementId) layerClasses.push('rework--nav--layer-buttons-disabled');
 
     return (
       <TopNav>
@@ -38,9 +49,35 @@ const DataframeDialog = React.createClass({
             className="fa fa-plus-circle"
             onClick={this.dropdown('element')}></NavButton>
           <NavButton
+            tooltip="Open Workpad"
+            className="fa fa-folder-open"
+            onClick={this.dropdown('workpads')}></NavButton>
+          <NavButton
             tooltip="New Workpad"
             className="fa fa-star"
             onClick={this.newWorkpad}></NavButton>
+
+          <vhr/>
+
+
+          <div className={layerClasses.join(' ')}>
+            <NavButton
+              tooltip="Move to top"
+              className="fa fa-angle-double-up"
+              onClick={this.elementLayer('++')}></NavButton>
+            <NavButton
+              tooltip="Move up"
+              className="fa fa-angle-up"
+              onClick={this.elementLayer('+')}></NavButton>
+            <NavButton
+              tooltip="Move Down"
+              className="fa fa-angle-down"
+              onClick={this.elementLayer('-')}></NavButton>
+            <NavButton
+              tooltip="Move to bottom"
+              className="fa fa-angle-double-down"
+              onClick={this.elementLayer('--')}></NavButton>
+          </div>
 
         </div>
       </TopNav>
@@ -49,8 +86,11 @@ const DataframeDialog = React.createClass({
 });
 
 function mapStateToProps(state) {
+  const {workpad} = state.persistent;
   return {
-    workpad: state.persistent.workpad,
+    workpad: workpad,
+    selectedElementId: state.transient.selectedElement,
+    currentPageId: workpad.pages[workpad.page]
   };
 }
 
