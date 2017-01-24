9dc461933b23c73a2f526833650d6c1e8b9c3a62
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-23 21:19:35 -0700

    Add workpad deletion

--- public/components/workpad_list/workpad_list.js
+++ public/components/workpad_list/workpad_list.js
@@ -9,6 +9,9 @@ export default React.createClass({
     return {workpads: []};
   }, // This could be initialized with the defaults for the form right?
   componentDidMount() {
+    this.fetchWorkpads();
+  },
+  fetchWorkpads() {
     fetch('../api/rework/find?name=', {
       method: 'GET',
       headers: {
@@ -21,24 +24,48 @@ export default React.createClass({
       this.setState(_.assign({}, {workpads: resp.workpads}));
     });
   },
+  handleDelete(id) {
+    return () => {
+      fetch('../api/rework/delete/' + id, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'kbn-xsrf': 'turdSandwich',
+        }
+      })
+      .then(resp => resp.json()).then(resp => {
+        this.fetchWorkpads();
+      });
+    };
+  },
   handleSelect(id) {
     return () => {this.props.onSelect(id);};
   },
   render() {
-    const {onSelect, onRemove} = this.props;
+    const {onSelect} = this.props;
     return (
       <div className="rework--workpad-list">
         <table className="table">
           <thead>
             <tr>
-              <th>Name</th><th>Last Modified</th>
+              <th width="1px"></th>
+              <th>Name</th>
+              <th style={{textAlign: 'right'}}>Last Modified</th>
             </tr>
           </thead>
           <tbody>
             {this.state.workpads.map(workpad => (
               <tr key={workpad.id}>
-                <td onClick={this.handleSelect(workpad.id)}>{workpad.name}</td>
-                <td onClick={this.handleSelect(workpad.id)}>{moment(workpad.timestamp).fromNow()}</td>
+                <td>
+                  <i onClick={this.handleDelete(workpad.id)} className="fa fa-ban rework--workpad-list-delete"></i>
+                </td>
+                <td className="rework--workpad-list-name" onClick={this.handleSelect(workpad.id)}>
+                  {workpad.name}
+                </td>
+                <td className="rework--workpad-list-timestamp" onClick={this.handleSelect(workpad.id)} style={{textAlign: 'right'}}>
+                  {moment(workpad.timestamp).fromNow()}
+                </td>
               </tr>
             ))}
           </tbody>
