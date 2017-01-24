886f69b4c81d604e2ac8e61c424a6c665b7ac60d
ppisljar <peter.pisljar@gmail.com>
2017-01-24 09:24:13 +0100

    moving average metric agg

--- src/ui/public/agg_types/index.js
+++ src/ui/public/agg_types/index.js
@@ -12,6 +12,7 @@ import AggTypesMetricsCardinalityProvider from 'ui/agg_types/metrics/cardinality
 import AggTypesMetricsPercentilesProvider from 'ui/agg_types/metrics/percentiles';
 import AggTypesMetricsPercentileRanksProvider from 'ui/agg_types/metrics/percentile_ranks';
 import AggTypesMetricsDerivativeProvider from 'ui/agg_types/metrics/derivative';
+import AggTypesMetricsMovingAverageProvider from 'ui/agg_types/metrics/moving_avg';
 import AggTypesBucketsDateHistogramProvider from 'ui/agg_types/buckets/date_histogram';
 import AggTypesBucketsHistogramProvider from 'ui/agg_types/buckets/histogram';
 import AggTypesBucketsRangeProvider from 'ui/agg_types/buckets/range';
@@ -37,6 +38,7 @@ export default function AggTypeService(Private) {
       Private(AggTypesMetricsPercentileRanksProvider),
       Private(AggTypesMetricsTopHitProvider),
       Private(AggTypesMetricsDerivativeProvider),
+      Private(AggTypesMetricsMovingAverageProvider),
     ],
     buckets: [
       Private(AggTypesBucketsDateHistogramProvider),
