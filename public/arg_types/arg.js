fd6df18022f9d4f3a4532110aef351719b12c2fc
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-25 11:45:24 -0700

    Add support for a displayName for args

--- public/arg_types/arg.js
+++ public/arg_types/arg.js
@@ -3,6 +3,7 @@ import argTypes from 'plugins/rework/arg_types/arg_types';
 export default class Arg {
   constructor(name, props) {
     this.name = name;
+    this.displayName = props.displayName || name;
     this.type = argTypes.byName[props.type];
 
     // This can be a value or a function. If it is a function then it will be passed state.
