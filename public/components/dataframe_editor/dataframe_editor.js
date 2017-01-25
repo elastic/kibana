583d80521bc2689ca9cfc9fb9b4d5bee61bd6a13
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-25 09:58:26 -0700

    Fix editing of components that keep initial state

--- public/components/dataframe_editor/dataframe_editor.js
+++ public/components/dataframe_editor/dataframe_editor.js
@@ -3,13 +3,16 @@ import _ from 'lodash';
 import frameSources from 'plugins/rework/arg_types/dataframe/frame_sources/frame_sources';
 
 export default React.createClass({
+  componentWillReceiveProps(nextProps) {
+    console.log('editor', nextProps.dataframe.value);
+  },
   render() {
     const {dataframe, commit} = this.props;
 
     const DataframeForm = frameSources.byName[dataframe.type].form;
     return (
       <div className="rework--dataframe-editor">
-        <DataframeForm value={dataframe.value} commit={commit}></DataframeForm>
+        <DataframeForm key={dataframe.id} value={dataframe.value} commit={commit}></DataframeForm>
       </div>
     );
   }
