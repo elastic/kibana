0566459e750aa410d4811e3fff300b3091850e48
Felix Stürmer <stuermer@weltenwort.de>
2017-01-25 18:43:07 +0100

    Add docstrings and annotations

--- src/core_plugins/kibana/public/context/api/utils/sorting.js
+++ src/core_plugins/kibana/public/context/api/utils/sorting.js
@@ -1,6 +1,44 @@
 import _ from 'lodash';
 
+/**
+ * A sort directive in object or string form.
+ *
+ * @typedef {(SortDirectiveString|SortDirectiveObject)} SortDirective
+ */
 
+/**
+ * A sort directive in object form.
+ *
+ * @typedef {Object.<FieldName, (SortDirection|SortOptions)>} SortDirectiveObject
+ */
+
+/**
+ * A sort order string.
+ *
+ * @typedef {('asc'|'desc')} SortDirection
+ */
+
+/**
+ * A field name.
+ *
+ * @typedef {string} FieldName
+ */
+
+/**
+ * A sort options object
+ *
+ * @typedef {Object} SortOptions
+ * @property {SortDirection} order
+ */
+
+
+/**
+ * Return a copy of a query with the sort direction reversed.
+ *
+ * @param {Object} query - The query to reverse the sort direction of.
+ *
+ * @returns {Object}
+ */
 function reverseQuerySort(query) {
   return Object.assign(
     {},
@@ -11,6 +49,16 @@ function reverseQuerySort(query) {
   );
 }
 
+/**
+ * Return a copy of the directive with the sort direction reversed. If the
+ * field name is '_score', it inverts the default sort direction in the same
+ * way as Elasticsearch itself.
+ *
+ * @param {SortDirective} sortDirective - The directive to reverse the
+ *     sort direction of
+ *
+ * @returns {SortDirective}
+ */
 function reverseSortDirective(sortDirective) {
   if (_.isString(sortDirective)) {
     return {
@@ -23,6 +71,13 @@ function reverseSortDirective(sortDirective) {
   }
 }
 
+/**
+ * Return the reversed sort direction.
+ *
+ * @param {(SortDirection|SortOptions)} sortDirection
+ *
+ * @returns {(SortDirection|SortOptions)}
+ */
 function reverseSortDirection(sortDirection) {
   if (_.isPlainObject(sortDirection)) {
     return _.assign({}, sortDirection, {
