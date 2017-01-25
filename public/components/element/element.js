d53b67a76dc675fa6fa79a81083f549d72d13c28
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-25 11:46:39 -0700

    Use style in the right place, allow for overflow

--- public/components/element/element.js
+++ public/components/element/element.js
@@ -5,9 +5,10 @@ import Loading from 'plugins/rework/components/loading/loading';
 class Element extends React.PureComponent {
   render() {
     const {type, args} = this.props;
+    const style = args ? args._style : {};
     const ElementContent = args ? elements.byName[type].template : () => (<Loading />);
     return (
-      <div className="rework--element">
+      <div className="rework--element" style={style}>
         <ElementContent args={args}></ElementContent>
       </div>
     );
