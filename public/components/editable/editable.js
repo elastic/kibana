90566ac5556c826f67d7ba91dc1ef545d0eac176
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-25 07:44:34 -0700

    make onChange optional

--- public/components/editable/editable.js
+++ public/components/editable/editable.js
@@ -2,22 +2,34 @@ import React from 'react';
 import _ from 'lodash';
 import './editable.less';
 
+// Must have value & onDone, onChange is optional. If no onChange is specificed this will maintain
+// internal state, which, if something else changes your value, could get dicey
+
 export default React.createClass({
+  getInitialState() {
+    return {value: this.props.value};
+  },
+  componentWillReceiveProps(nextProps) {
+    if (this.props.value !== nextProps.value) this.setState({value: nextProps.value});
+  },
+  componentDidMount() {
+    if (this.props.focus) this.refs.editableInput.focus();
+  },
   change(e) {
-    this.props.onChange(e.target.value);
+    const onChange = this.props.onChange || _.noop;
+    onChange(e.target.value);
+    this.setState({value: e.target.value});
   },
   handleKeyPress(e) {
     const onDone = this.props.onDone || _.noop;
     if (e.key === 'Enter' || e.key === 'Escape') {
       this.refs.editableInput.blur();
-      onDone();
+      onDone(this.state.value);
     }
   },
-  componentDidMount() {
-    if (this.props.focus) this.refs.editableInput.focus();
-  },
   render() {
-    const {value, onChange, className, style} = this.props;
+    const {className, style} = this.props;
+    const {value} = this.state;
 
     return (
       <input
@@ -26,8 +38,8 @@ export default React.createClass({
         className={['form-control rework--editable', className].join(' ')}
         value={value}
         onChange={this.change}
-        style={style}
-        onKeyDown={this.handleKeyPress}/>
+        onKeyDown={this.handleKeyPress}
+        style={style}/>
     );
   }
 });
