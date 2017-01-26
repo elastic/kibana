d45fa1b9467bad8e538ad35cff370dac96912e30
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-26 10:35:02 -0700

    Make element list look good

--- public/components/element_type_list/element_type_list.js
+++ public/components/element_type_list/element_type_list.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import _ from 'lodash';
 import elementTypes from 'plugins/rework/elements/elements';
+import './element_type_list.less';
+import Tooltip from 'plugins/rework/components/tooltip/tooltip';
 
 export default React.createClass({
   select(elementType) {
@@ -11,13 +13,13 @@ export default React.createClass({
     const {onSelect} = this.props;
 
     const elementButtons = _.map(elementTypes, (elementType) => (
+      <Tooltip key={elementType.name} content={elementType.name}>
         <button
           className="btn btn-default"
-          key={elementType.name}
-          onClick={this.select(elementType)}>{elementType.name}
+          onClick={this.select(elementType)}><center><img src={elementType.icon} width="30"/></center>
         </button>
-      )
-    );
+      </Tooltip>
+    ));
 
     return (
       <div className="rework--element-type-list">
