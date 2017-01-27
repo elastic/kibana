abff810b8a7e57d67c954ba11b00e525f376c144
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-26 19:01:57 -0700

    Handle null construction

--- public/arg_types/dataframe/lib/dataframe/dataframe.js
+++ public/arg_types/dataframe/lib/dataframe/dataframe.js
@@ -21,8 +21,8 @@ export default class Dataframe {
   constructor(data) {
     const {columns, rows, keys} = data;
     // Consider using immutable.js in this class
-    this.columns = new Columns(columns);
-    this.rows = _.map(rows, (row) => new Row(this.columns, row));
+    this.columns = new Columns(columns || []);
+    this.rows = _.map(rows || [], (row) => new Row(this.columns, row));
   }
 
   get toTuples() {
