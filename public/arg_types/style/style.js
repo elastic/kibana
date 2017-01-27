9ffbb89aa87885d62b1731e6b66125f0393d8d86
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-26 17:50:51 -0700

    Fix default style

--- public/arg_types/style/style.js
+++ public/arg_types/style/style.js
@@ -4,12 +4,20 @@ import argTypes from 'plugins/rework/arg_types/arg_types';
 import transform from './transform';
 
 argTypes.push(new ArgType('style', {
-  default: {},
+  default: '',
   help: 'Custom CSS styles to apply to the element.',
-  form: ({commit, value}) => {
+  /*
+    value = current value,
+    commit = function to call to commit changes,
+  */
+  form: ({value, commit}) => {
     const storeValue = (e) => commit(e.target.value);
     return (<textarea className="form-control" rows={10} onChange={storeValue} value={value}></textarea>);
   },
+  /*
+    value = current value,
+    state = current redux state
+  */
   resolve: (value, state) => {
     try {
       return transform(value);
