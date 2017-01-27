949ea2ca5815ef87acb1ea9537e456086923ba06
Rashid Khan <github.fliplap@spamgourmet.com>
2017-01-27 11:52:44 -0700

    Document usage of arg type

--- public/arg_types/arg_type.js
+++ public/arg_types/arg_type.js
@@ -1,10 +1,19 @@
 export default class ArgType {
   constructor(name, props) {
+
+    // The name of this argument type
     this.name = name;
 
+    // The function to resolve the argument.
     this.resolve = props.resolve;
+
+    // The default unresolved value
     this.default = props.default;
+
+    // A React component to use as the form
     this.form = props.form; // (value, state) => {}
+
+    // Default help text if not overridden on the arg level
     this.help = props.help;
   }
 }
