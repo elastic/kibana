416760aa857e0cca4caa11106a368f39ccc43985
CJ Cenizal <cj@cenizal.com>
2017-01-30 17:27:05 -0800

    Add support for Panel with and without a PanelHeader. (#10112)

--- ui_framework/doc_site/src/views/panel/panel_example.jsx
+++ ui_framework/doc_site/src/views/panel/panel_example.jsx
@@ -8,4 +8,11 @@ export default createExample([{
   title: 'Panel',
   html: require('./panel.html'),
   hasDarkTheme: false,
+}, {
+  title: 'Panel with PanelHeader',
+  description: (
+    <p>The Panel requires a special class when used with a PanelHeader.</p>
+  ),
+  html: require('./panel_with_header.html'),
+  hasDarkTheme: false,
 }]);
