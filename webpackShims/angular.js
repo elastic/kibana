0ab48f0a7ed5d22785891dcd6c32461947eab2d7
Martin Hickey <martin.hickey@ie.ibm.com>
2017-02-01 22:03:17 +0000

    Add angular-translate as the Angular globalization framework
    The angular-translate framework integrates with the Kibana i18n engine whereby it
    consumes translations from the i18n engine and not the translations directly.
    This is achieved by using a custom loader which calls the client side getTranslations()
    method. This method return the translations loaded by the i18n engine during instantation
    of Kibana server instance.

--- webpackShims/angular.js
+++ webpackShims/angular.js
@@ -1,7 +1,8 @@
 require('jquery');
 require('node_modules/angular/angular');
+require('node_modules/angular-translate/dist/angular-translate.min');
 module.exports = window.angular;
 
 require('node_modules/angular-elastic/elastic');
 
-require('ui/modules').get('kibana', ['monospaced.elastic']);
+require('ui/modules').get('kibana', ['monospaced.elastic', 'pascalprecht.translate']);
--- webpackShims/ui-bootstrap.js
+++ webpackShims/ui-bootstrap.js
@@ -1,11 +1,27 @@
 define(function (require) {
   require('angular');
   require('ui/angular-bootstrap/index');
+  const chrome = require('../src/ui/public/chrome/chrome');
 
   return require('ui/modules')
-  .get('kibana', ['ui.bootstrap'])
+  .get('kibana', ['ui.bootstrap', 'pascalprecht.translate'])
   .config(function ($tooltipProvider) {
     $tooltipProvider.setTriggers({ 'mouseenter': 'mouseleave click' });
-  });
+  })
+  .factory('translationsLoader', function ($q) {
+      return function (options) {
+        var deferred = $q.defer();
+        const translations = chrome.getTranslations();
+        deferred.resolve(translations);
+        return deferred.promise;
+      };
+  })
+  .config(['$translateProvider', function ($translateProvider) {
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.useLoader('translationsLoader');
+    // Enable escaping of HTML
+    // issue in https://angular-translate.github.io/docs/#/guide/19_security
+    $translateProvider.useSanitizeValueStrategy('escape');
+  }]);
 
 });
