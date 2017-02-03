20b68cd5a61d431b716c247e45ac63822a64099a
Lukas Olson <olson.lukas@gmail.com>
2017-02-03 10:44:00 -0700

    Improve highlighting by using highlight_query with all_fields enabled (#9671)
    * Add all_fields to highlight query to improve highlighting
    
    * Refactor highlighting and move out of _flatten
    
    * Make changes as per @bargs' requests
    
    * Add documentation about highlightAll setting
    
    * Fix docs typo
    
    * Remove unused function
    
    * Remove unused code

---
+++ src/ui/public/highlight/highlight_request.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import highlightTags from './highlight_tags';
+
+const FRAGMENT_SIZE = Math.pow(2, 31) - 1; // Max allowed value for fragment_size (limit of a java int)
+
+/**
+  * Returns a clone of the query with `"all_fields": true` set on any `query_string` queries
+  */
+function getHighlightQuery(query) {
+  const clone = _.cloneDeep(query);
+
+  if (_.has(clone, 'query_string')) {
+    clone.query_string.all_fields = true;
+  } else if (_.has(clone, 'bool.must')) {
+    if (Array.isArray(clone.bool.must)) {
+      clone.bool.must = clone.bool.must.map(getHighlightQuery);
+    } else {
+      clone.bool.must = getHighlightQuery(clone.bool.must);
+    }
+  }
+
+  return clone;
+}
+
+export default function getHighlightRequestProvider(config) {
+  if (!config.get('doc_table:highlight')) {
+    return _.noop;
+  }
+
+  return function getHighlightRequest(query) {
+    const fieldsParams = config.get('doc_table:highlight:all_fields')
+      ? { highlight_query: getHighlightQuery(query) }
+      : {};
+
+    return {
+      pre_tags: [highlightTags.pre],
+      post_tags: [highlightTags.post],
+      fields: {
+        '*': fieldsParams
+      },
+      fragment_size: FRAGMENT_SIZE
+    };
+  };
+}
