7838a12a4c933f6d2a95b116f3e81da95388a7e9
Rashid Khan <github.fliplap@spamgourmet.com>
2017-02-03 16:52:43 -0700

    Remove commented out code

--- public/components/color_picker/color_picker.js
+++ public/components/color_picker/color_picker.js
@@ -26,15 +26,6 @@ export default React.createClass({
   render() {
     const {color, onChange, popover} = this.props;
 
-    /*
-    colors = colors || [
-      '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3',
-      '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39',
-      '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#607d8b',
-      '#FFFFFF', '#CCCCCC', '#999999', '#666666', '#333333', '#000000',
-    ];
-    */
-
     const setTransparent = () => this.onChange({rgb: {r:255,g:255,b:255,a:0}});
 
     const colors = this.props.colors || [
