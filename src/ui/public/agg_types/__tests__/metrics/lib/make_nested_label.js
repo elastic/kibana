b043ce1f05b44097046fea57a653d66a5faeee41
ppisljar <peter.pisljar@gmail.com>
2017-02-06 10:59:35 +0100

    fixing test

--- src/ui/public/agg_types/__tests__/metrics/lib/make_nested_label.js
+++ src/ui/public/agg_types/__tests__/metrics/lib/make_nested_label.js
@@ -1,5 +1,5 @@
 import expect from 'expect.js';
-import makeNestedLabel from 'ui/agg_types/metrics/lib/make_nested_label';
+import { makeNestedLabel } from 'ui/agg_types/metrics/lib/make_nested_label';
 
 describe('metric agg make_nested_label', function () {
 
@@ -15,19 +15,19 @@ describe('metric agg make_nested_label', function () {
 
   it('should return a metric label with prefix', function () {
     const aggConfig = generateAggConfig('Count');
-    const label = makeNestedLabel(aggConfig, 'derivative of');
+    const label = makeNestedLabel(aggConfig, 'derivative');
     expect(label).to.eql('Derivative of Count');
   });
 
   it('should return a numbered prefix', function () {
     const aggConfig = generateAggConfig('Derivative of Count');
-    const label = makeNestedLabel(aggConfig, 'derivative of');
+    const label = makeNestedLabel(aggConfig, 'derivative');
     expect(label).to.eql('2. derivative of Count');
   });
 
   it('should return a prefix with correct order', function () {
     const aggConfig = generateAggConfig('3. derivative of Count');
-    const label = makeNestedLabel(aggConfig, 'derivative of');
+    const label = makeNestedLabel(aggConfig, 'derivative');
     expect(label).to.eql('4. derivative of Count');
   });
 
