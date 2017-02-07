8114ff762a6dc3d4b5fa5cc30d454e21ccb20c7a
Rashid Khan <github.fliplap@spamgourmet.com>
2017-02-07 15:06:16 -0700

    Show a warning dialog if element type is missing

--- public/components/element/element.js
+++ public/components/element/element.js
@@ -4,6 +4,7 @@ import Loading from 'plugins/rework/components/loading/loading';
 import ScopedStyle from 'plugins/rework/components/scoped_style/scoped_style';
 import uuid from 'uuid/v4';
 import handlebars from 'handlebars/dist/handlebars';
+import Warning from 'plugins/rework/components/warning/warning';
 
 const Element = React.createClass({
   getInitialState() {
@@ -16,6 +17,9 @@ const Element = React.createClass({
   render() {
     const {type, args, setArg} = this.props;
     const {styleId, elementType} = this.state;
+
+    if (!elementType) return (<Warning>Unknown Element Type</Warning>);
+
     const ElementContent = args ? elementType.template : () => (<Loading />);
 
     // Build the inline style for the wrapper
