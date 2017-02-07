2de7cecaef208d373404dbf4e681b17f83a78ed7
jasper <elastic-jasper@users.noreply.github.com>
2017-02-06 19:12:05 -0500

    Implement functional test helpers (#10209)
    Backports PR #8362
    
    **Commit 1:**
    [functionalTests] implement common.tryMethod() helper
    
    * Original sha: 69fb068d5519f12cd65edbb09ae4dc2a706bd398
    * Authored by spalger <email@spalger.com> on 2016-09-20T01:43:59Z
    
    **Commit 2:**
    [functionalTests] implement common.findAllTestSubjects() helper
    
    * Original sha: 6b220cfc2bff8248ad87c0c4ad5aa29c4ad7d06d
    * Authored by spalger <email@spalger.com> on 2016-09-20T01:44:48Z

--- test/support/page_objects/common.js
+++ test/support/page_objects/common.js
@@ -1,6 +1,7 @@
 
 import bluebird, {
-  promisify
+  promisify,
+  filter as filterAsync
 } from 'bluebird';
 import fs from 'fs';
 import _ from 'lodash';
@@ -214,6 +215,10 @@ export default class Common {
     return Try.try(block);
   }
 
+  tryMethod(object, method, ...args) {
+    return this.try(() => object[method](...args));
+  }
+
   log(...args) {
     Log.log(...args);
   }
@@ -263,6 +268,13 @@ export default class Common {
     return this.remote
       .setFindTimeout(defaultFindTimeout)
       .findDisplayedByCssSelector(testSubjSelector(selector));
+  }
+
+  async findAllTestSubjects(selector) {
+    this.debug('in findAllTestSubjects: ' + testSubjSelector(selector));
+    const remote = this.remote.setFindTimeout(defaultFindTimeout);
+    const all = await remote.findAllByCssSelector(testSubjSelector(selector));
+    return await filterAsync(all, el => el.isDisplayed());
   }
 
 }
