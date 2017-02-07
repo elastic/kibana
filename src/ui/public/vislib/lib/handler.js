5c436d58742ec4d9935caa7b74b818dbc11e839c
Peter Pisljar <peter.pisljar@gmail.com>
2017-02-07 17:42:53 +0100

    showing too many series error just on heatmap (#10134)
    * showing too many series error just on heatmap
    
    * adding tests
    
    * adding test for non heatmap chart

--- src/ui/public/vislib/lib/handler.js
+++ src/ui/public/vislib/lib/handler.js
@@ -113,6 +113,8 @@ export default function HandlerBaseClass(Private) {
      * @returns {HTMLElement} With the visualization child element
      */
     render() {
+      if (this.visConfig.get('error', null)) return this.error(this.visConfig.get('error'));
+
       const self = this;
       const { binder, charts = [] } = this;
       const selection = d3.select(this.el);
