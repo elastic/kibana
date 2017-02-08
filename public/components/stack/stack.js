8553a71cc9d192ebfa32446b13ea88808a907c37
Rashid Khan <github.fliplap@spamgourmet.com>
2017-02-08 09:10:39 -0700

    Pages must be displayed to be previewed

--- public/components/stack/stack.js
+++ public/components/stack/stack.js
@@ -5,12 +5,16 @@ export default React.createClass({
     const { top, children } = this.props;
     const style = {
       height: '100%',
-      width: '100%'
+      width: '100%',
+      position: 'absolute',
+      top: 0,
+      left: 0
     };
 
     const stack = React.Children.map(children, (child, i) => {
-      const newStyle = i !== top ? {display: 'none'} : style;
-      child = i !== top ? null : child; // Don't render children that are not visisble
+      const newStyle = i !== top ? {...style, opacity: 0, pointerEvents: 'none'} : style;
+      // Commented out the bit below because we can't have previews without rendering somewhere
+      //child = i !== top ? null : child; // Don't render children that are not visisble
       return (<div style={newStyle}>{child}</div>);
     });
 
