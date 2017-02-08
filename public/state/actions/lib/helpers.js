63100cd901764869aee42dd514bf38c4f32a5106
Joe Fleming <joe.fleming@gmail.com>
2017-02-08 11:40:36 -0700

    move mutation helpers

--- public/state/actions/lib/helpers.js
+++
@@ -1,4 +0,0 @@
-export const mutateWithId = (id, value) => {return {id, value};};
-export const mutateWithIdAndName = (id, name, value) => {return {id, name, value};};
-export const mutateElement = mutateWithId;
-export const mutateArgument = mutateWithIdAndName;
