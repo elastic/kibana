1fad596e1b6cd2c130d96938ec09ebe5c179ebbd
Rashid Khan <github.fliplap@spamgourmet.com>
2017-02-08 10:44:17 -0700

    Fix crash when page added

--- public/components/dom_preview/dom_preview.js
+++ public/components/dom_preview/dom_preview.js
@@ -5,60 +5,64 @@ import $ from 'jquery';
 export class DomPreview extends React.PureComponent {
   constructor(props) {    /* Note props is passed into the constructor in order to be used */
     super(props);
-    this.slowUpdate = _.debounce(this.update, 250);
-    this.state = {original: $(`#${props.id}`)};
   }
 
   shouldComponentUpdate() {
     return false;
   }
 
-  update() {
-    var original = this.state.original;
-    var thumb = this.state.original.clone();
+  update(original) {
+    return () => {
+      var thumb = original.clone();
 
-    //var width = 320;
-    //var height = workpad.current.size.height * (width / workpad.current.size.width);
+      //var width = 320;
+      //var height = workpad.current.size.height * (width / workpad.current.size.width);
 
-    var height = this.props.height || 100;
-    var width = original.width() * (height / original.height());
+      var height = this.props.height || 100;
+      var width = original.width() * (height / original.height());
 
-    $(this.refs.content).html(thumb);
+      $(this.refs.content).html(thumb);
 
-    // Copy canvas data
-    var originalCanvas = $('canvas', original);
-    var thumbCanvas = $('canvas', thumb);
+      // Copy canvas data
+      var originalCanvas = $('canvas', original);
+      var thumbCanvas = $('canvas', thumb);
 
-    _.each(originalCanvas, function (img, i) {
-      thumbCanvas[i].getContext('2d').drawImage(img, 0, 0);
-    });
+      _.each(originalCanvas, function (img, i) {
+        thumbCanvas[i].getContext('2d').drawImage(img, 0, 0);
+      });
 
-    $(this.refs.container).css({
-      width: width,
-      height: height,
-      overflow: 'hidden'
-    });
+      $(this.refs.container).css({
+        width: width,
+        height: height,
+        overflow: 'hidden'
+      });
 
-    $(this.refs.content).css({
-      transform: `scale(${(width / original.width())})`,
-      'transform-origin': 'top left',
-      height: original.height(),
-      width: original.width(),
-    });
+      $(this.refs.content).css({
+        transform: `scale(${(width / original.width())})`,
+        'transform-origin': 'top left',
+        height: original.height(),
+        width: original.width(),
+      });
+    };
+
   }
 
   componentDidMount() {
-    this.update();
 
-    const observer = new MutationObserver(this.slowUpdate.bind(this));
+    const original = $(`#${this.props.id}`);
+    const update = this.update(original);
+    update();
+    const slowUpdate = _.debounce(update, 250);
+
+    const observer = new MutationObserver(slowUpdate.bind(this));
     // configuration of the observer:
     const config = { attributes: true, childList: true, subtree: true };
     // pass in the target node, as well as the observer options
-    observer.observe(this.state.original[0], config);
+    observer.observe(original[0], config);
   }
 
   componentDidUpdate() {
-    this.update();
+    //this.update();
   }
 
   render() {
