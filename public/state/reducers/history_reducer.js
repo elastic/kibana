fe8de8140d5bc081edf4f93fa44864fefbeaca23
Joe Fleming <joe.fleming@gmail.com>
2017-02-09 11:30:22 -0700

    hook history up to window.history

--- public/state/reducers/history_reducer.js
+++ public/state/reducers/history_reducer.js
@@ -1,32 +1,49 @@
+import { isEqual } from 'lodash';
+import lzString from 'lz-string';
+import { historyUndo, historyRedo } from '../actions/history';
 import stateHistory from '../lib/state_history';
 
-const history = stateHistory.create();
+const wh = window.history;
 
 const historyReducer = reducer => (state, action) => {
-  let newState = state;
+  let newState = { ...state };
 
   switch (action.type) {
     case 'HISTORY_UNDO':
-      history.previous();
+      wh.back();
       break;
 
     case 'HISTORY_REDO':
-      history.next();
+      wh.forward();
       break;
+
+    case 'HISTORY_RESTORE':
+      if (action.error) {
+        // TODO: handle history restore error
+        console.log('history restore failed', action.payload);
+        return state;
+      }
+
+      return {
+        ...state,
+        persistent: action.payload,
+      };
 
     default:
       newState = reducer(state, action);
-      history.push(newState.persistent);
+      if (!isEqual(state.persistent, newState.persistent)) {
+        try {
+          const stateJSON = JSON.stringify(newState.persistent);
+          const payload = lzString.compress(stateJSON);
+          wh.pushState(payload, '');
+        } catch (e) {
+          // TODO: handle state saving errors
+          console.log('pushstate failed', e);
+        }
+      }
   }
 
-  return {
-    ...newState,
-    persistent: history.current,
-    history: {
-      hasPrevious: history.hasPrevious,
-      hasNext: history.hasNext,
-    },
-  };
+  return newState;
 };
 
 export default historyReducer;
