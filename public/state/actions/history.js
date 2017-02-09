e265257d6d96faab0fdefd76bdfdc37bf065a317
Joe Fleming <joe.fleming@gmail.com>
2017-02-09 11:29:31 -0700

    add history restore action

--- public/state/actions/history.js
+++ public/state/actions/history.js
@@ -1,4 +1,10 @@
 import { createAction } from 'redux-actions';
+import lzString from 'lz-string';
+
+export const historyRestore = createAction('HISTORY_RESTORE', persistedHistory => {
+  const payload = lzString.decompress(persistedHistory);
+  return JSON.parse(payload);
+});
 
 export const historyUndo = createAction('HISTORY_UNDO');
 export const historyRedo = createAction('HISTORY_REDO');
