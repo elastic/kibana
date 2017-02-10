e8e44e126b8f899b9f9a257ca61592d0d91a7981
Joe Fleming <joe.fleming@gmail.com>
2017-02-10 14:20:14 -0700

    correct handle errors in resolve_fetch

--- public/lib/resolve_fetch.js
+++ public/lib/resolve_fetch.js
@@ -4,7 +4,7 @@ export function toJson(prop) {
   return (resp) => {
     return resp.json()
     .then(resp => {
-      if (resp.ok === false) throw new Error(resp.resp);
+      if (resp.ok === false) throw new Error(get(resp, 'resp.response', 'response was not ok'));
       if (prop) return get(resp, prop);
       return resp;
     });
