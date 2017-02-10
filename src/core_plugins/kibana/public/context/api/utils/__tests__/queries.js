21c9098c62b12d98be742a37323f1dba27dd8e70
Felix Stürmer <stuermer@weltenwort.de>
2017-02-10 16:17:54 +0100

    Fix function names in unit tests

--- src/core_plugins/kibana/public/context/api/utils/__tests__/queries.js
+++ src/core_plugins/kibana/public/context/api/utils/__tests__/queries.js
@@ -1,20 +1,20 @@
 import expect from 'expect.js';
 
 import {
-  createAnchorQuery,
-  createSuccessorsQuery,
+  createAnchorQueryBody,
+  createSuccessorsQueryBody,
 } from 'plugins/kibana/context/api/utils/queries';
 
 
 describe('context app', function () {
-  describe('function createAnchorQuery', function () {
+  describe('function createAnchorQueryBody', function () {
     it('should return a search definition that searches the given uid', function () {
-      const query = createAnchorQuery('UID', { '@timestamp': 'desc' });
+      const query = createAnchorQueryBody('UID', { '@timestamp': 'desc' });
       expect(query.query.terms._uid[0]).to.eql('UID');
     });
 
     it('should return a search definition that sorts by the given criteria and uid', function () {
-      const query = createAnchorQuery('UID', { '@timestamp': 'desc' });
+      const query = createAnchorQueryBody('UID', { '@timestamp': 'desc' });
       expect(query.sort).to.eql([
         { '@timestamp': 'desc' },
         { _uid: 'asc' },
@@ -22,14 +22,14 @@ describe('context app', function () {
     });
   });
 
-  describe('function createSuccessorsQuery', function () {
+  describe('function createSuccessorsQueryBody', function () {
     it('should return a search definition that includes the given size', function () {
-      const query = createSuccessorsQuery([0, 'UID'], { '@timestamp' : 'desc' }, 10);
+      const query = createSuccessorsQueryBody([0, 'UID'], { '@timestamp' : 'desc' }, 10);
       expect(query).to.have.property('size', 10);
     });
 
     it('should return a search definition that sorts by the given criteria and uid', function () {
-      const query = createSuccessorsQuery([0, 'UID'], { '@timestamp' : 'desc' }, 10);
+      const query = createSuccessorsQueryBody([0, 'UID'], { '@timestamp' : 'desc' }, 10);
       expect(query).to.have.property('sort');
       expect(query.sort).to.eql([
         { '@timestamp': 'desc' },
@@ -38,7 +38,7 @@ describe('context app', function () {
     });
 
     it('should return a search definition that searches after the given uid', function () {
-      const query = createSuccessorsQuery([0, 'UID'], { '@timestamp' : 'desc' }, 10);
+      const query = createSuccessorsQueryBody([0, 'UID'], { '@timestamp' : 'desc' }, 10);
       expect(query).to.have.property('search_after');
       expect(query.search_after).to.eql([0, 'UID']);
     });
