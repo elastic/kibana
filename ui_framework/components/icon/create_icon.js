6280d72894c655432f306b3c11132c0d91e51e7d
Stacey Gammon <gammon@elastic.co>
2017-02-10 12:23:18 -0500

    add missing files

---
+++ ui_framework/components/icon/create_icon.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { KuiIcon } from './kui_icon';
+
+export function CreateIcon() {
+  return <KuiIcon className="fa-plus"/>;
+}
---
+++ ui_framework/components/icon/delete_icon.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { KuiIcon } from './kui_icon';
+
+export function DeleteIcon() {
+  return <KuiIcon className="fa-trash"/>;
+}
