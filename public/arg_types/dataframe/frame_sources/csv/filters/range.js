c87289ad3d38886ba07c886a1760b786e35240b1
Rashid Khan <github.fliplap@spamgourmet.com>
2017-02-10 14:10:19 -0700

    Fix range filters

--- public/arg_types/dataframe/frame_sources/csv/filters/range.js
+++ public/arg_types/dataframe/frame_sources/csv/filters/range.js
@@ -1,7 +1,13 @@
 import _ from 'lodash';
 
-export const exactly = (filter, rows) => {
+export const range = (filter, rows) => {
   return _.filter(rows, row => {
-    return row[filter.column] === filter.value;
+    if (_.isUndefined(row[filter.column])) return true;
+    return (
+      (_.isUndefined(filter.gte) ? true : row[filter.column] >= filter.gte) &&
+      (_.isUndefined(filter.gt)  ? true : row[filter.column] >  filter.gt) &&
+      (_.isUndefined(filter.lt)  ? true : row[filter.column] <  filter.lt) &&
+      (_.isUndefined(filter.lte) ? true : row[filter.column] <= filter.lte)
+    );
   });
 };
--- public/arg_types/dataframe/frame_sources/timelion/filters/range.js
+++ public/arg_types/dataframe/frame_sources/timelion/filters/range.js
@@ -1,9 +1,10 @@
 export const range = (filter) => {
+  const {column, gte, gt, lt, lte} = filter;
   return {
     bool: {
       should: [
-        {bool: {must_not: {exists: {field: filter.column}}}},
-        {range: {[filter.column]: filter.value}},
+        {bool: {must_not: {exists: {field: column}}}},
+        {range: {[column]: {gte, gt, lt, lte}}},
       ]
     }
   };
