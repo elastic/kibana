f80726c5a0d27f1502cc921120b24c5278df079b
Elastic Jasper <elastic-jasper@users.noreply.github.com>
2017-02-11 02:34:31 -0500

    [utils/streams] implement some generic stream helpers
    Backports PR #10187
    
    **Commit 1:**
    [utils/streams] implement some generic stream helpers
    
    While working on pr 9853 I have needed to deal with streams in several different ways and along the way ended up with a core set of stream helpers. Since these helpers are not browser or server specific, and are used across components, and tested with generic use in mind I put them in src/utils.
    
    * Original sha: a2f437ffd5b38aa45b57aa2d9e37bdd094b243bd
    * Authored by spalger <spalger@users.noreply.github.com> on 2017-02-03T22:07:24Z
    
    **Commit 2:**
    Merge branch 'master' of github.com:elastic/kibana into implement/stream-utils
    
    * Original sha: b638d21beb8aed28cacc8d7ec49f195fdf67888a
    * Authored by spalger <spalger@users.noreply.github.com> on 2017-02-08T19:17:01Z
    
    **Commit 3:**
    [utils/streams] address kim's feedback
    
    * Original sha: 8566549e94ae49fb68c9ca7fb7fed5f13b16d331
    * Authored by spalger <spalger@users.noreply.github.com> on 2017-02-08T19:22:11Z
    
    **Commit 4:**
    Merge branch 'master' of github.com:elastic/kibana into implement/stream-utils
    
    * Original sha: a93df5a0ae6ed02875d026946031c80a5198723a
    * Authored by spalger <spalger@users.noreply.github.com> on 2017-02-10T20:47:20Z
    
    **Commit 5:**
    [utils/stream/list] move default value into arg list
    
    * Original sha: 747218e2a4239f37ff97f98c4d0882490123bd59
    * Authored by spalger <spalger@users.noreply.github.com> on 2017-02-10T20:54:42Z

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,14 @@ export deepCloneWithBuffers from './deep_clone_with_buffers';
 export fromRoot from './from_root';
 export pkg from './package_json';
 export unset from './unset';
+
+export {
+  createConcatStream,
+  createIntersperseStream,
+  createJsonParseStream,
+  createJsonStringifyStream,
+  createListStream,
+  createPromiseFromStreams,
+  createReduceStream,
+  createSplitStream,
+} from './streams';
