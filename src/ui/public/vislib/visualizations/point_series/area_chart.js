be27727399b85f610e009e0dced63a524d1af54f
Peter Pisljar <peter.pisljar@gmail.com>
2017-02-13 19:54:08 +0100

    derivative metric aggregation (#10020)
    * updating agg_config to allow adding parent aggregations
    
    * derivative metric aggregation
    
    * disable on metric and tagcloud
    
    * fixing point series to correctly handle missing y values
    
    * adding unit tests
    
    * fix broken ui
    
    * removing percentile ranks
    
    * updating based on review
    
    * fixing bug and adding make_nested_label tests
    
    * fixing error
    
    * fixing test
    
    * fixing based on last review
    
    * fixing based on thomas review
    
    * allow nested aggs
    
    * prevent sorting on derivative metric
    
    * updating based on last review
    
    * adding error message when histogram is not the last bucket
    
    * fixing test
    
    * updating error message

--- src/ui/public/vislib/visualizations/point_series/area_chart.js
+++ src/ui/public/vislib/visualizations/point_series/area_chart.js
@@ -96,7 +96,8 @@ export default function AreaChartFactory(Private) {
 
       function y1(d) {
         const y0 = d.y0 || 0;
-        return yScale(y0 + d.y);
+        const y = d.y || 0;
+        return yScale(y0 + y);
       }
 
       function y0(d) {
@@ -125,7 +126,9 @@ export default function AreaChartFactory(Private) {
           return !_.isNull(d.y);
         })
         .interpolate(interpolate);
-        return area(data.values);
+        return area(data.values.filter(function (d) {
+          return !_.isNull(d.y);
+        }));
       });
 
       return path;
@@ -185,10 +188,11 @@ export default function AreaChartFactory(Private) {
       }
 
       function cy(d) {
+        const y = d.y || 0;
         if (isOverlapping) {
-          return yScale(d.y);
+          return yScale(y);
         }
-        return yScale(d.y0 + d.y);
+        return yScale(d.y0 + y);
       }
 
       // update
