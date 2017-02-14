96da0647a3a250b743f908ec9e6af3e74aed7ee5
Joe Fleming <joe.fleming@gmail.com>
2017-02-14 12:39:10 -0700

    add export controls

--- public/components/workpad_list/workpad_list.js
+++ public/components/workpad_list/workpad_list.js
@@ -3,12 +3,29 @@ import { connect } from 'react-redux';
 import _ from 'lodash';
 import moment from 'moment';
 
-import { workdpadLoadAll, workpadDelete } from 'plugins/rework/state/actions/workpad';
+import Tooltip from 'plugins/rework/components/tooltip/tooltip';
+import { workdpadLoadAll, workpadDelete, workpadExport } from 'plugins/rework/state/actions/workpad';
 import './workpad_list.less';
 
 class WorkpadList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isExporting: _.get(props, 'workpadExporting', false),
+    };
+  }
+
   componentDidMount() {
     this.fetchWorkpads();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.state.isExporting && nextProps.workpadExportData) {
+      return this.setState({ isExporting: false });
+    }
+
+    this.setState({ isExporting: nextProps.workpadExporting });
   }
 
   fetchWorkpads() {
@@ -23,32 +40,76 @@ class WorkpadList extends React.Component {
     return () => this.props.onSelect(id);
   }
 
+  handleExport(id) {
+    return () => this.props.dispatch(workpadExport(id));
+  }
+
+  renderExport(workpad) {
+    return (
+      <a onClick={this.handleExport(workpad.id)} className="fa fa-download"></a>
+    );
+  }
+
+  renderDownload() {
+    try {
+      const { workpadExportData } = this.props;
+      const json = JSON.stringify(workpadExportData, null, 2);
+      const fileBlob = new Blob([json], { type: 'application/json' });
+      const url = window.URL || window.webkitURL;
+      const exportBlob = url.createObjectURL(fileBlob);
+
+      return (
+        <a className="fa fa-save"
+          href={exportBlob}
+          download={`${workpadExportData.workpad.name}.json`}>
+        </a>
+      );
+    } catch (e) {
+      // TODO: better error handling
+      console.log('export failed', e);
+      return (
+        <Tooltip content="Export Failed">
+          <i className="fa fa-times"></i>
+        </Tooltip>
+      );
+    }
+  }
+
   render() {
     const { onSelect, workpads } = this.props;
+    const isExported = id => {
+      const exportId = _.get(this.props, 'workpadExportData.workpad.id');
+      return exportId === id;
+    };
+
+    const workpadElements = workpads.map(workpad => (
+      <tr key={workpad.id} data-id={workpad.id}>
+        <td className="rework--workpad-list-name" onClick={this.handleSelect(workpad.id)}>
+          {workpad.name}
+        </td>
+        <td className="rework--workpad-list-timestamp" onClick={this.handleSelect(workpad.id)} style={{textAlign: 'right'}}>
+          {moment(workpad.timestamp).fromNow()}
+        </td>
+        <td className="rework--workpad-list-controls">
+          <a onClick={this.handleDelete(workpad.id)} className="fa fa-ban"></a>
+          { isExported(workpad.id) ? this.renderDownload() : this.renderExport(workpad) }
+        </td>
+
+      </tr>
+    ));
+
     return (
       <div className="rework--workpad-list">
         <table className="table">
           <thead>
             <tr>
-              <th width="1px"></th>
               <th>Name</th>
               <th style={{textAlign: 'right'}}>Last Modified</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {workpads.map(workpad => (
-              <tr key={workpad.id} data-id={workpad.id}>
-                <td>
-                  <i onClick={this.handleDelete(workpad.id)} className="fa fa-ban rework--workpad-list-delete"></i>
-                </td>
-                <td className="rework--workpad-list-name" onClick={this.handleSelect(workpad.id)}>
-                  {workpad.name}
-                </td>
-                <td className="rework--workpad-list-timestamp" onClick={this.handleSelect(workpad.id)} style={{textAlign: 'right'}}>
-                  {moment(workpad.timestamp).fromNow()}
-                </td>
-              </tr>
-            ))}
+            {workpadElements}
           </tbody>
         </table>
       </div>
@@ -59,6 +120,9 @@ class WorkpadList extends React.Component {
 function mapStateToProps(state) {
   return {
     workpads: state.transient.workpads,
+    workpadState: state.persistent,
+    workpadExporting: state.transient.workpadExporting,
+    workpadExportData: state.transient.workpadExportData,
   };
 }
 
