f73de34dc02058d1ea2991c655d7cb8dcc5abd95
Joe Fleming <joe.fleming@gmail.com>
2017-02-14 12:38:38 -0700

    add export actions

--- public/state/actions/workpad.js
+++ public/state/actions/workpad.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch';
+import { get } from 'lodash';
 import { createAction } from 'redux-actions';
 import { dataframeResolveAll } from './dataframe';
 import { elementResolveAll } from './element';
@@ -24,17 +25,7 @@ export function workpadInit() {
 
 export function workpadLoad(id) {
   return (dispatch, getState) => {
-    const action = createAction('WORKPAD_LOAD', id => {
-      return fetch('../api/rework/get/' + id, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'kbn-xsrf': 'turdSandwich',
-        }
-      })
-      .then(toJson('resp._source'));
-    });
+    const action = createAction('WORKPAD_LOAD', id => getWorkpad(id));
 
     dispatch(action(id));
     dispatch(workpadInit());
@@ -60,6 +51,26 @@ export function workdpadLoadAll() {
   };
 }
 
+export function workpadExport(id) {
+  return (dispatch, getState) => {
+    const startAction = createAction('WORKPAD_EXPORT_START');
+    const action = createAction('WORKPAD_EXPORT', id => {
+      const { persistent, transient } = getState();
+      const { workpadExportData } = transient;
+
+      // return state from loader or previously exported data if it matches
+      if (get(persistent, 'workpad.id') === id) return Promise.resolve(persistent);
+      if (get(workpadExportData, 'workpad.id') === id) return Promise.resolve(workpadExportData);
+
+      // otherwise load the workpad state from the server
+      return getWorkpad(id);
+    });
+
+    dispatch(startAction());
+    dispatch(action(id));
+  };
+}
+
 export function workpadDelete(id) {
   return (dispatch) => {
     const startAction = createAction('WORKPAD_DELETE_START');
@@ -79,4 +90,16 @@ export function workpadDelete(id) {
     dispatch(action(id));
     dispatch(workpadInit());
   };
+}
+
+function getWorkpad(id) {
+  return fetch('../api/rework/get/' + id, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'kbn-xsrf': 'turdSandwich',
+    }
+  })
+  .then(toJson('resp._source'));
 }
