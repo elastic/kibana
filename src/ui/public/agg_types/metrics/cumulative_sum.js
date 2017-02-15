723c7c15c9ee22db70094669c02d471d8a0abfdc
Peter Pisljar <peter.pisljar@gmail.com>
2017-02-15 20:03:45 +0100

    cumulative sum, moving avg and serial diff pipeline aggregation metric (#10033)
    * extracting to parent agg helper
    
    * adding comulative sum agg
    
    * adding serial diff and moving avg
    
    * adding tests

--- src/ui/public/agg_types/metrics/cumulative_sum.js
+++ src/ui/public/agg_types/metrics/cumulative_sum.js
@@ -2,14 +2,14 @@ import AggTypesMetricsMetricAggTypeProvider from 'ui/agg_types/metrics/metric_ag
 import ParentPipelineAggHelperProvider from './lib/parent_pipeline_agg_helper';
 import { makeNestedLabel } from './lib/make_nested_label';
 
-export default function AggTypeMetricDerivativeProvider(Private) {
+export default function AggTypeMetricComulativeSumProvider(Private) {
   const MetricAggType = Private(AggTypesMetricsMetricAggTypeProvider);
   const parentPipelineAggHelper = Private(ParentPipelineAggHelperProvider);
 
   return new MetricAggType({
-    name: 'derivative',
-    title: 'Derivative',
-    makeLabel: agg => makeNestedLabel(agg, 'derivative'),
+    name: 'cumulative_sum',
+    title: 'Cumulative Sum',
+    makeLabel: agg => makeNestedLabel(agg, 'cumulative sum'),
     params: [
       ...parentPipelineAggHelper.params()
     ]
