311f06f0a90f6d510cf03c6194b1dcad22fae59b
Joe Fleming <joe.fleming@gmail.com>
2017-02-15 10:14:01 -0700

    make linter happy

--- server/routes/import.js
+++ server/routes/import.js
@@ -39,7 +39,7 @@ module.exports = function (server) {
             ok: true,
             resp: { workpads },
           });
-        })
+        });
       })
       .catch(function (resp) {
         reply({
