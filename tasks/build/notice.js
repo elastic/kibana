090a2d928203c9b384302003b9742181ed887f09
Jonathan Budzenski <jon@jbudz.me>
2017-02-15 10:37:10 -0600

    [build] Look for both license and notice files

--- tasks/build/notice.js
+++ tasks/build/notice.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import npm from 'npm';
 import npmLicense from 'license-checker';
+import glob from 'glob';
 import path from 'path';
 import fs from 'fs';
 import { execSync } from 'child_process';
@@ -20,9 +21,24 @@ export default function licenses(grunt) {
       });
       installedPackages.toString().trim().split('\n').forEach(pkg => {
         const packageDetails = pkg.split(':');
-        packagePaths[packageDetails[1]] = packageDetails[0].replace(/.*\/kibana\//, '');
+        const [modulePath, packageName] = packageDetails;
+        const licenses = glob.sync(path.join(modulePath, '*LICENSE*'));
+        const notices = glob.sync(path.join(modulePath, '*NOTICE*'));
+        packagePaths[packageName] = {
+          relative: modulePath.replace(/.*\/kibana\//, ''),
+          licenses,
+          notices
+        };
       });
       return packagePaths;
+    }
+
+    function combineFiles(filePaths) {
+      let content = '';
+      filePaths.forEach(filePath => {
+        content += fs.readFileSync(filePath) + '\n';
+      });
+      return content;
     }
 
     fs.appendFileSync(fd,
@@ -39,13 +55,13 @@ export default function licenses(grunt) {
       _.forOwn(result, (value, key) => {
         const licenses = [].concat(overrides.hasOwnProperty(key) ? overrides[key] : value.licenses);
         if (!licenses.length || licenses.includes('UNKNOWN')) return grunt.fail.fatal(`Unknown license for ${key}`);
+        const packagePath = packagePaths[key];
+        const readLicenseAndNotice = combineFiles([].concat(packagePath.licenses, packagePath.notices));
+        const licenseOverview = licenses.length > 1 ? `the\n"${licenses.join('", ')} licenses` : `a\n"${licenses[0]}" license`;
+        const licenseAndNotice = readLicenseAndNotice ? `\n${readLicenseAndNotice}` : `  For details, see ${packagePath.relative}/.`;
+        const combinedText = `This product bundles ${key} which is available under ${licenseOverview}.${licenseAndNotice}\n---\n`;
 
-        const licenseFile = value.licenseFile && fs.readFileSync(value.licenseFile);
-        const licenseText = licenses.length > 1 ? `the\n"${licenses.join('", ')} licenses` : `a\n"${licenses[0]}" license`;
-        const licenseFileText = licenseFile ? `\n\n${licenseFile}` : `  For details, see ${packagePaths[key]}/.`;
-        const noticeText = `This product bundles ${key} which is available under ${licenseText}.${licenseFileText}\n\n---\n`;
-
-        fs.appendFileSync(fd, noticeText);
+        fs.appendFileSync(fd, combinedText);
       });
       fs.closeSync(fd);
       done();
