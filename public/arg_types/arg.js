5b050d8278c385727b5ef65fbe2fad753c4603e5
Rashid Khan <github.fliplap@spamgourmet.com>
2017-02-16 16:59:43 -0700

    Add ability for args to declare if their form is expanded by default

--- public/arg_types/arg.js
+++ public/arg_types/arg.js
@@ -2,16 +2,27 @@ import argTypes from 'plugins/rework/arg_types/arg_types';
 
 export default class Arg {
   constructor(name, props) {
+
+    // The name of the arg as passed into the element renderer
     this.name = name;
+
+    // A display name for the arg when rendering its form
     this.displayName = props.displayName || name;
+
+    // Argument's type definition
     this.type = argTypes.byName[props.type];
 
     // This can be a value or a function. If it is a function then it will be passed state.
-    // eg (state) => {return state.some.value}; is acceptable
+    // eg (state) => {return state.some.value};
     this.default = props.default === undefined ? this.type.default : props.default;
 
+    // Should this argument's form be expanded by default?
+    this.expand = props.expand === undefined ? this.type.expand : props.expand;
+
+    // A short help text
     this.help = props.help === undefined ? this.type.help : props.help;
 
+    // Any options to pass to the argument's form
     this.options = props.options || {};
 
   }
--- public/arg_types/arg_type.js
+++ public/arg_types/arg_type.js
@@ -7,8 +7,11 @@ export default class ArgType {
     // The function to resolve the argument.
     this.resolve = props.resolve;
 
-    // The default unresolved value
+    // The default unresolved value, can be overridden at the arg level
     this.default = props.default;
+
+    // Should this argument type be expanded by default? You can override this at the arg level
+    this.expand = props.expand === undefined ? false : true;
 
     // A React component to use as the form
     this.form = props.form; // (value, state) => {}
