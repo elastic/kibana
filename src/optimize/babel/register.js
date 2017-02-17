45c0cac2fb50c57a4aeb6d6bc1f649c3b929c4f5
Jonathan Budzenski <jon@jbudz.me>
2017-02-17 10:10:42 -0600

    [optimize] Load babel environment variables before babel-register

--- src/optimize/babel/register.js
+++ src/optimize/babel/register.js
@@ -1,4 +1,8 @@
 // this file is not transpiled in dev
 
+// this will set babel cache paths as environment variables,
+// it needs to be loaded before babel-register
+const babelOptions = require('./options').node
+
 require('babel-polyfill');
-require('babel-register')(require('./options').node);
+require('babel-register')(babelOptions);
