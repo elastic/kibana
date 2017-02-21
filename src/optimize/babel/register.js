a6c4e86770f39263f8d5ea04c2492a6d16174376
Jonathan Budzenski <jon@jbudz.me>
2017-02-21 09:56:56 -0600

    [optimize] Move babel register to function call

--- src/optimize/babel/register.js
+++ src/optimize/babel/register.js
@@ -1,8 +1,4 @@
 // this file is not transpiled in dev
 
-// this will set babel cache paths as environment variables,
-// it needs to be loaded before babel-register
-const babelOptions = require('./options').node
-
 require('babel-polyfill');
-require('babel-register')(babelOptions);
+require('./options').registerNodeOptions();
