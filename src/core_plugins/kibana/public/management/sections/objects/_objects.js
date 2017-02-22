9ca1c28e87ffa2e8ddff0d6e76878f25bd5c1f14
Joe Fleming <joe.fleming@gmail.com>
2017-02-22 15:08:08 -0700

    show warning when importing a missing type

--- src/core_plugins/kibana/public/management/sections/objects/_objects.js
+++ src/core_plugins/kibana/public/management/sections/objects/_objects.js
@@ -164,8 +164,18 @@ uiModules.get('apps/management')
           notify.error('The file could not be processed.');
         }
 
-        return Promise.map(docs, function (doc) {
-          const service = find($scope.services, { type: doc._type }).service;
+        return Promise.map(docs, (doc) => {
+          const { service } = find($scope.services, { type: doc._type }) || {};
+
+          if (!service) {
+            const msg = `Skipped import of "${doc._source.title}" (${doc._id}), ` +
+              `invalid type: "${doc._type}"`;
+            notify.warning(msg, {
+              lifetime: 0,
+            });
+            return;
+          }
+
           return service.get().then(function (obj) {
             obj.id = doc._id;
             return obj.applyESResp(doc).then(function () {
@@ -174,7 +184,8 @@ uiModules.get('apps/management')
           });
         })
         .then(refreshIndex)
-        .then(refreshData, notify.error);
+        .then(refreshData)
+        .catch(notify.error);
       };
 
       function refreshIndex() {
