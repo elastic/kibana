91a342f96e4172b006b558df0bcececdd4014ba6
Jonathan Budzenski <jon@jbudz.me>
2017-02-23 12:56:59 -0600

    [status api] Fix returning old status

--- src/server/status/index.js
+++ src/server/status/index.js
@@ -34,13 +34,10 @@ export default function (kbnServer, server, config) {
 
       return reply({
         name: config.get('server.name'),
+        version: config.get('pkg.version'),
+        buildNum: config.get('pkg.buildNum'),
+        buildSha: config.get('pkg.buildSha'),
         uuid: config.get('server.uuid'),
-        version: {
-          number: config.get('pkg.version').replace(matchSnapshot, ''),
-          build_hash: config.get('pkg.buildSha'),
-          build_number: config.get('pkg.buildNum'),
-          build_snapshot: matchSnapshot.test(config.get('pkg.version'))
-        },
         status: kbnServer.status.toJSON(),
         metrics: kbnServer.metrics
       });
