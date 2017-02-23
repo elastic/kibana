ef51496d0c8461824172bcf3a42a67f154129648
jasper <elastic-jasper@users.noreply.github.com>
2017-02-23 11:54:30 -0500

    [UI Framework] Add View component. Add support for full-screen examples. (#10536)
    Backports PR #10454
    
    **Commit 1:**
    Move kuiView into UI Framework. Add View example. Add support for full-screen examples.
    
    * Original sha: 0b14299ea865c3b2f1976e441aec8f4c7d0b5bbd
    * Authored by CJ Cenizal <cj@cenizal.com> on 2017-02-18T23:38:43Z
    
    **Commit 2:**
    Organize nav items by components and sandboxes.
    
    * Original sha: 52aba11447bd5843e9861dd255566dbcb1b76b3e
    * Authored by CJ Cenizal <cj@cenizal.com> on 2017-02-19T19:13:32Z

--- ui_framework/doc_site/src/components/guide_section/guide_section.jsx
+++ ui_framework/doc_site/src/components/guide_section/guide_section.jsx
@@ -37,10 +37,12 @@ export class GuideSection extends Component {
           <div className="guideSection__title">
             {this.props.title}
           </div>
-          <div
-            className="guideSection__sourceButton fa fa-code"
+          <button
+            className="guideSection__sourceButton"
             onClick={this.onClickSource}
-          />
+          >
+            <span className="fa fa-code"></span>
+          </button>
         </div>
 
         {this.props.children}
