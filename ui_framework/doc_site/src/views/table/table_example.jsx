89d9c29e08b202e474018dc905c4c1939fbaefc0
Elastic Jasper <elastic-jasper@users.noreply.github.com>
2017-02-23 15:35:12 -0500

    [UI Framework] Add Fluid Table component.
    Backports PR #10516
    
    **Commit 1:**
    Add Fluid Table component.
    
    * Original sha: d801bab856fca01da892f191858bc49ac2d6e2ad
    * Authored by CJ Cenizal <cj@cenizal.com> on 2017-02-22T18:50:13Z

--- ui_framework/doc_site/src/views/table/table_example.jsx
+++ ui_framework/doc_site/src/views/table/table_example.jsx
@@ -9,10 +9,12 @@ import {
   GuidePage,
   GuideSection,
   GuideSectionTypes,
+  GuideText,
 } from '../../components';
 
 const tableHtml = require('./table.html');
 const tableJs = require('raw!./table.js');
+const fluidTableHtml = require('./fluid_table.html');
 const controlledTableHtml = require('./controlled_table.html');
 const controlledTableWithLoadingItemsHtml = require('./controlled_table_loading_items.html');
 const controlledTableWithNoItemsHtml = require('./controlled_table_no_items.html');
@@ -33,6 +35,22 @@ export default props => (
       <GuideDemo
         html={tableHtml}
         js={tableJs}
+      />
+    </GuideSection>
+
+    <GuideSection
+      title="Fluid Table"
+      source={[{
+        type: GuideSectionTypes.HTML,
+        code: fluidTableHtml,
+      }]}
+    >
+      <GuideText>
+        For when you want the content of a table&rsquo;s cells to determine its width.
+      </GuideText>
+
+      <GuideDemo
+        html={fluidTableHtml}
       />
     </GuideSection>
 
