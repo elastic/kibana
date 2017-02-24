918ea4b4b38354af1d4727ef75320a8457119818
Matthew Bargar <mbargar@gmail.com>
2017-02-24 14:08:16 -0500

    More tests

--- src/ui/public/filter_manager/__tests__/range.js
+++ src/ui/public/filter_manager/__tests__/range.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import expect from 'expect.js';
 import buildRangeFilter from '../lib/range';
 import ngMock from 'ng_mock';
@@ -8,10 +9,6 @@ describe('range filter', function () {
   describe('buildRangeFilter', function () {
 
     let indexPattern;
-    const validParams = {
-      gt: 1000,
-      lt: 2000,
-    };
 
     beforeEach(ngMock.module('kibana'));
     beforeEach(function () {
@@ -26,5 +23,61 @@ describe('range filter', function () {
       expect(buildRangeFilter).withArgs(indexPattern.fields.byName.bytes, {}).to.throwException(/indexPattern is a required argument/);
     });
 
+    it('should disallow passing both gte and gt params', function () {
+      const params = {
+        gt: 1000,
+        gte: 1000
+      };
+
+      expect(buildRangeFilter).withArgs(indexPattern.fields.byName.bytes, params, indexPattern)
+      .to.throwException(/gte and gt are mutually exclusive/);
+    });
+
+    it('should disallow passing both lte and lt params', function () {
+      const params = {
+        lt: 1000,
+        lte: 1000
+      };
+
+      expect(buildRangeFilter).withArgs(indexPattern.fields.byName.bytes, params, indexPattern)
+      .to.throwException(/lte and lt are mutually exclusive/);
+    });
+
+    it('should return a range filter object with the given params', function () {
+      const params = {
+        gt: 1000,
+        lt: 2000
+      };
+      const expectedFilter = {
+        meta: {
+          index: indexPattern.id
+        },
+        range: {
+          bytes: params
+        }
+      };
+
+      const filter = buildRangeFilter(indexPattern.fields.byName.bytes, params, indexPattern);
+      expect(_.isEqual(expectedFilter, filter)).to.be(true);
+    });
+
+    it('should return a match_all filter if both ends of the range are infinite according to javascript', function () {
+      const params = {
+        gt: Math.pow(10, 1000),
+        lt: Math.pow(10, 1001)
+      };
+      const expectedFilter = {
+        meta: {
+          index: indexPattern.id,
+          field: 'bytes'
+        },
+        match_all: {}
+      };
+      const filter = buildRangeFilter(indexPattern.fields.byName.bytes, params, indexPattern);
+      expect(_.isEqual(expectedFilter, filter)).to.be(true);
+    });
+
+    // todo scripted field tests
+    // todo formattedValue param tests
   });
 });
