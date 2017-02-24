be177cc9f11261bb15fae4c07795c73f44b4dbc6
Peter Pisljar <peter.pisljar@gmail.com>
2017-02-24 18:33:57 +0100

    fixing custom label issue with point series charts (#10465)

--- src/ui/public/vislib/lib/types/point_series.js
+++ src/ui/public/vislib/lib/types/point_series.js
@@ -4,7 +4,7 @@ export default function ColumnHandler(Private) {
 
   const createSerieFromParams = (cfg, seri) => {
     const matchingSeriParams = cfg.seriesParams ? cfg.seriesParams.find(seriConfig => {
-      return seri.aggLabel === seriConfig.data.label;
+      return seri.label === seriConfig.data.label;
     }) : null;
 
 
