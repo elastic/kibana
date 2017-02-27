f514c19e908bb9bb6a91a4191d3158ae6b70faf2
Lukas Olson <olson.lukas@gmail.com>
2017-02-27 16:21:51 -0700

    Use ngModelOptions instead of implicit bindings

--- src/ui/public/timepicker/timepicker.js
+++ src/ui/public/timepicker/timepicker.js
@@ -75,21 +75,17 @@ module.directive('kbnTimepicker', function (quickRanges, timeUnits, refreshInter
         }
       });
 
-      $scope.$watch('absolute.from', function (date) {
-        // If it is a date, it was set from the timepicker, so we set it to the start of day and transform it into a moment
-        if (_.isDate(date)) {
-          date.setHours(0, 0, 0, 0); // Start of day
-          $scope.absolute.from = moment(date);
-        }
-      });
+      $scope.pickFromDate = function (date) {
+        if (!date) return $scope.absolute.from;
+        date.setHours(0, 0, 0, 0); // Start of day
+        return $scope.absolute.from = moment(date);
+      };
 
-      $scope.$watch('absolute.to', function (date) {
-        // If it is a date, it was set from the timepicker, so we set it to the end of day and transform it into a moment
-        if (_.isDate(date)) {
-          date.setHours(23, 59, 59, 999); // End of day
-          $scope.absolute.to = moment(date);
-        }
-      });
+      $scope.pickToDate = function (date) {
+        if (!date) return $scope.absolute.to;
+        date.setHours(23, 59, 59, 999); // End of day
+        return $scope.absolute.to = moment(date);
+      };
 
       $scope.setMode = function (thisMode) {
         switch (thisMode) {
