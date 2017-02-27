5deb6a0f8c3ab6df21355b76b81f3876a8c284e2
CJ Cenizal <cj@cenizal.com>
2017-02-27 12:23:50 -0800

    Remove unused cases from CodeViewerReducer.

--- ui_framework/doc_site/src/store/reducers/code_viewer_reducer.js
+++ ui_framework/doc_site/src/store/reducers/code_viewer_reducer.js
@@ -28,38 +28,10 @@ export default function codeViewerReducer(state = defaultState, action) {
       });
     }
 
-    case ActionTypes.UPDATE_CODE_VIEWER: {
-      if (state.isOpen) {
-        return Object.assign({}, state, {
-          source: state.codesBySlug[action.slug],
-        });
-      }
-      return state;
-    }
-
     case ActionTypes.CLOSE_CODE_VIEWER: {
       return Object.assign({}, state, {
         isOpen: false,
         source: undefined,
-      });
-    }
-
-    case ActionTypes.REGISTER_CODE: {
-      const codesBySlug = Object.assign({}, state.codesBySlug, {
-        [action.code.slug]: action.code,
-      });
-
-      return Object.assign({}, state, {
-        codesBySlug
-      });
-    }
-
-    case ActionTypes.UNREGISTER_CODE: {
-      const codesBySlug = Object.assign({}, state.codesBySlug);
-      delete codesBySlug[action.code.slug];
-
-      return Object.assign({}, state, {
-        codesBySlug
       });
     }
 
