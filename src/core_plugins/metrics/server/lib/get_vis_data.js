93bfc8902a1b020062158ad483b9caa316eec9ef
Chris Cowan <chris@chriscowan.us>
2017-02-27 10:09:07 -0700

    Fixing per PR

--- src/core_plugins/metrics/server/lib/get_vis_data.js
+++ src/core_plugins/metrics/server/lib/get_vis_data.js
@@ -6,7 +6,7 @@ function getVisData(req) {
   return Promise.all(reqs)
     .then(res => {
       return res.reduce((acc, data) => {
-        return _.assign({}, acc, data);
+        return _.assign(acc, data);
       }, {});
     });
 }
