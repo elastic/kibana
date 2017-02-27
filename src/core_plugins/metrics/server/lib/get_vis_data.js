46316118fd7f041065cff9b21793b4287cd92857
Chris Cowan <chris@chriscowan.us>
2017-02-27 10:15:04 -0700

    Renaming vars to make it more clear what's happening

--- src/core_plugins/metrics/server/lib/get_vis_data.js
+++ src/core_plugins/metrics/server/lib/get_vis_data.js
@@ -2,8 +2,8 @@ import _ from 'lodash';
 import getPanelData from './vis_data/get_panel_data';
 
 function getVisData(req) {
-  const reqs = req.payload.panels.map(getPanelData(req));
-  return Promise.all(reqs)
+  const promises = req.payload.panels.map(getPanelData(req));
+  return Promise.all(promises)
     .then(res => {
       return res.reduce((acc, data) => {
         return _.assign(acc, data);
