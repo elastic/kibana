82b37aa524f5cc154e9171cbcacefe9198af710d
Joe Fleming <w33ble@users.noreply.github.com>
2017-03-03 17:02:09 -0700

    Prompt about overwriting existing saved objects (#10663)
    * prompt about overwriting existing saved objects
    
    If the user selects now, things work the same as before, but selecting yes will just overwrite existing saved objects without prompting
    
    * add more white space
    
    because https://github.com/elastic/kibana/blob/master/style_guides/js_style_guide.md#chaining-operations
    
    * re-word the overwrite all dialog

--- src/core_plugins/kibana/public/management/sections/objects/_objects.js
+++ src/core_plugins/kibana/public/management/sections/objects/_objects.js
@@ -169,30 +169,41 @@ uiModules.get('apps/management')
           notify.error('The file could not be processed.');
         }
 
-        return Promise.map(docs, (doc) => {
-          const { service } = find($scope.services, { type: doc._type }) || {};
-
-          if (!service) {
-            const msg = `Skipped import of "${doc._source.title}" (${doc._id})`;
-            const reason = `Invalid type: "${doc._type}"`;
-
-            notify.warning(`${msg}, ${reason}`, {
-              lifetime: 0,
-            });
-
-            return;
-          }
-
-          return service.get().then(function (obj) {
-            obj.id = doc._id;
-            return obj.applyESResp(doc).then(function () {
-              return obj.save({ confirmOverwrite : true });
-            });
-          });
+        return new Promise((resolve) => {
+          confirmModal(
+            `If any of the objects already exist, do you want to automatically overwrite them?`, {
+              confirmButtonText: `Yes, overwrite all`,
+              cancelButtonText: `No, prompt me for each one`,
+              onConfirm: () => resolve(true),
+              onCancel: () => resolve(false),
+            }
+          );
         })
-        .then(refreshIndex)
-        .then(refreshData)
-        .catch(notify.error);
+          .then((overwriteAll) => {
+            return Promise.map(docs, (doc) => {
+              const { service } = find($scope.services, { type: doc._type }) || {};
+
+              if (!service) {
+                const msg = `Skipped import of "${doc._source.title}" (${doc._id})`;
+                const reason = `Invalid type: "${doc._type}"`;
+
+                notify.warning(`${msg}, ${reason}`, {
+                  lifetime: 0,
+                });
+
+                return;
+              }
+
+              return service.get()
+                .then(function (obj) {
+                  obj.id = doc._id;
+                  return obj.applyESResp(doc).then(() => obj.save({ confirmOverwrite : !overwriteAll }));
+                });
+            })
+              .then(refreshIndex)
+              .then(refreshData)
+              .catch(notify.error);
+          });
       };
 
       function refreshIndex() {
