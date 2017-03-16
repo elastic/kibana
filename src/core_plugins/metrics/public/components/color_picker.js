98015161a6cf87bd2a1f195e68379a533a24c33a
Chris Cowan <chris@chriscowan.us>
2017-03-16 14:22:15 -0700

    fixing number inputs for color rules and fixing picker

--- src/core_plugins/metrics/public/components/color_picker.js
+++ src/core_plugins/metrics/public/components/color_picker.js
@@ -87,7 +87,7 @@ class ColorPicker extends Component {
 
 ColorPicker.propTypes = {
   name: PropTypes.string.isRequired,
-  value: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   disableTrash: PropTypes.bool,
   onChange: PropTypes.func
 };
