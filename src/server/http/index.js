db1d6ef0ef4d82373e646f8c4a021b1fa633ab28
kobelb <brandon.kobel@elastic.co>
2017-03-16 06:47:19 -0400

    Adding an app for redirects when storing state in session storage

--- src/server/http/index.js
+++ src/server/http/index.js
@@ -119,8 +119,19 @@ module.exports = async function (kbnServer, server, config) {
       try {
         const url = await shortUrlLookup.getUrl(request.params.urlId, request);
         shortUrlAssertValid(url);
-        reply().redirect(config.get('server.basePath') + url);
+
+        const uiSettings = server.uiSettings();
+        const stateStoreInSessionStorage = await uiSettings.get(request, 'state:storeInSessionStorage');
+        if (!stateStoreInSessionStorage) {
+          reply().redirect(config.get('server.basePath') + url);
+        }
+
+        const app = kbnServer.uiExports.apps.byId.stateSessionStorageRedirect;
+        reply.renderApp(app, {
+          redirectUrl: url,
+        });
       } catch (err) {
+        console.log(err);
         reply(handleShortUrlError(err));
       }
     }
