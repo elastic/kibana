34bf62a7a655e98bc6c36daa282cb1a8cacd7428
Thomas Neirynck <thomas@elastic.co>
2017-03-17 16:38:14 -0400

    do not hardcode map zoom level to calculate max zoom

--- src/ui/public/utils/zoom_to_precision.js
+++ src/ui/public/utils/zoom_to_precision.js
@@ -5,22 +5,34 @@ const maxPrecision = 12;
  * Map Leaflet zoom levels to geohash precision levels.
  * The size of a geohash column-width on the map should be at least `minGeohashPixels` pixels wide.
  */
-const zoomPrecision = {};
+
+
+
+
+const zoomPrecisionMap = {};
 const minGeohashPixels = 16;
-for (let zoom = 0; zoom <= 21; zoom += 1) {
-  const worldPixels = 256 * Math.pow(2, zoom);
-  zoomPrecision[zoom] = 1;
-  for (let precision = 2; precision <= maxPrecision; precision += 1) {
-    const columns = geohashColumns(precision);
-    if ((worldPixels / columns) >= minGeohashPixels) {
-      zoomPrecision[zoom] = precision;
-    } else {
-      break;
+
+function calculateZoomToPrecisionMap(maxZoom) {
+
+  for (let zoom = 0; zoom <= maxZoom; zoom += 1) {
+    if (typeof zoomPrecisionMap[zoom] === 'number') {
+      continue;
+    }
+    const worldPixels = 256 * Math.pow(2, zoom);
+    zoomPrecisionMap[zoom] = 1;
+    for (let precision = 2; precision <= maxPrecision; precision += 1) {
+      const columns = geohashColumns(precision);
+      if ((worldPixels / columns) >= minGeohashPixels) {
+        zoomPrecisionMap[zoom] = precision;
+      } else {
+        break;
+      }
     }
   }
 }
 
 
-export default function zoomToPrecision(mapZoom, maxPrecision) {
-  return Math.min(zoomPrecision[mapZoom], maxPrecision);
+export default function zoomToPrecision(mapZoom, maxPrecision, maxZoom) {
+  calculateZoomToPrecisionMap(typeof maxZoom === 'number' ? maxZoom : 21);
+  return Math.min(zoomPrecisionMap[mapZoom], maxPrecision);
 }
