6d724fc1499d3db2c413b5bc61b04b48c37a3d81
CJ Cenizal <cj@cenizal.com>
2017-03-22 14:27:30 -0700

    Fix Jest coverage configuration for deeply-nested dirs.

--- tasks/ui_framework_test.js
+++ tasks/ui_framework_test.js
@@ -7,7 +7,14 @@ const resolve = relativePath => path.resolve(__dirname, `..`, ``, relativePath);
 const config = {
   rootDir,
   collectCoverage: true,
-  collectCoverageFrom: ['components/**/*.js', '!components/index.js', '!components/**/*/index.js'],
+  collectCoverageFrom: [
+    'components/**/*.js',
+    // Seems to be a bug with jest or micromatch, in which the above glob doesn't match subsequent
+    // levels of directories, making this glob necessary.
+    'components/**/**/*.js',
+    '!components/index.js',
+    '!components/**/*/index.js',
+  ],
   coverageDirectory: '<rootDir>/jest/report',
   coverageReporters: ['html'],
   moduleFileExtensions: ['jsx', 'js', 'json'],
