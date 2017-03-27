142d2dff2d1fe2a4ab52b10fadee43a1c4e91892
Joe Fleming <w33ble@users.noreply.github.com>
2017-03-27 11:30:19 -0700

    change let to var (elastic/kibana-plugin-helpers#35)
    allow helpers to run on pre-6.0 node
    
    Original commit: elastic/kibana-plugin-helpers@91de9a84d32138e7b7896712daf8c7f4dce67656

--- packages/kbn-plugin-helpers/lib/enable_collecting_unknown_options.js
+++ packages/kbn-plugin-helpers/lib/enable_collecting_unknown_options.js
@@ -3,7 +3,7 @@ module.exports = function enableCollectingUnknownOptions(command) {
   var origParse = command.parseOptions;
   command.allowUnknownOption();
   command.parseOptions = function (argv) {
-    let opts = origParse.call(this, argv);
+    var opts = origParse.call(this, argv);
     this.unknownOptions = opts.unknown;
     return opts;
   };
