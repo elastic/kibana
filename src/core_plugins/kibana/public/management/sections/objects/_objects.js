6bffd08af0d2687eec27bd5ebe960333c1ed3d93
Joe Fleming <w33ble@users.noreply.github.com>
2017-03-28 11:26:09 -0700

    Import saved searches first (#10740) (#10906)
    * import saved searches first
    * move saved object type grouping into a helper

--- src/core_plugins/kibana/public/management/sections/objects/_objects.js
+++ src/core_plugins/kibana/public/management/sections/objects/_objects.js
@@ -164,7 +164,7 @@ uiModules.get('apps/management')
           notify.error('The file could not be processed.');
         }
 
-        return Promise.map(docs, (doc) => {
+        function importDocument(doc) {
           const { service } = find($scope.services, { type: doc._type }) || {};
 
           if (!service) {
@@ -190,7 +190,30 @@ uiModules.get('apps/management')
               notify.error(err);
             });
           });
-        })
+        }
+
+        function groupByType(docs) {
+          const defaultDocTypes = {
+            searches: [],
+            other: [],
+          };
+
+          return docs.reduce((types, doc) => {
+            switch (doc._type) {
+              case 'search':
+                types.searches.push(doc);
+                break;
+              default:
+                types.other.push(doc);
+            }
+            return types;
+          }, defaultDocTypes);
+        }
+
+        const docTypes = groupByType(docs);
+
+        return Promise.map(docTypes.searches, importDocument)
+        .then(() => Promise.map(docTypes.other, importDocument))
         .then(refreshIndex)
         .then(refreshData)
         .catch(notify.error);
