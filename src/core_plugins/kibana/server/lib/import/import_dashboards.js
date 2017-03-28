58d3853d2aadd54b775132651435706e65c661d7
Chris Cowan <chris@chriscowan.us>
2017-03-28 12:28:57 -0700

    Finishing import implimentation

--- src/core_plugins/kibana/server/lib/import/import_dashboards.js
+++ src/core_plugins/kibana/server/lib/import/import_dashboards.js
@@ -2,13 +2,18 @@ export default function importDashboards(req) {
   const { payload } = req;
   const config = req.server.config();
   const { callWithRequest } = req.server.plugins.elasticsearch.getCluster('admin');
+  const index = config.get('kibana.index');
 
 
   if (payload.version !== config.get('pkg.version')) {
     throw new Error(`Version ${payload.version} does not match ${config.get('pkg.version')}.`);
   }
 
-  const body = [];
+  const body = payload.objects.reduce((acc, item) => {
+    acc.push({ create: { _index: index, _type: item._type, _id: item._id } });
+    acc.push(item._source);
+    return acc;
+  }, []);
 
   return callWithRequest(req, 'bulk', { body });
 
