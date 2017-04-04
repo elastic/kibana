3e490f39cf9d51c834f232dbf00d84f9d1e2f8c7
Jonathan Budzenski <jon@jbudz.me>
2017-04-04 16:23:19 -0500

    Add tests for KibanaMappings class

---
+++ src/core_plugins/elasticsearch/lib/__tests__/kibana_index_mappings.js
@@ -0,0 +1,44 @@
+import expect from 'expect.js';
+import sinon from 'sinon';
+import { has } from 'lodash';
+import { KibanaMappings } from '../kibana_index_mappings';
+
+
+describe('plugins/elasticsearch', function () {
+  describe('lib/kibana_index_mappings', function () {
+    let kibanaMappings;
+    const plugin = {
+      status: {
+        red: sinon.stub()
+      }
+    };
+    beforeEach(function () {
+      kibanaMappings = new KibanaMappings(plugin);
+    });
+
+    it('provides default mappings', function () {
+      expect(kibanaMappings.getCombined()).to.an('object');
+    });
+
+    it('registers new mappings', () => {
+      kibanaMappings.register({
+        foo: {
+          'properties': {
+            'bar': {
+              'type': 'text'
+            }
+          }
+        }
+      });
+
+      const mappings = kibanaMappings.getCombined();
+      expect(has(mappings, 'foo.properties.bar')).to.be(true);
+    });
+
+    it('sets the plugin red if there is a mapping conflict', () => {
+      kibanaMappings.register({ foo: 'bar' });
+      kibanaMappings.register({ foo: 'bar' });
+      expect(plugin.status.red.called).to.be(true);
+    });
+  });
+});
