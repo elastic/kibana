c7beaa71d7fc772520128a1e52021d5ef56cbaee
Stacey Gammon <gammon@elastic.co>
2017-04-05 06:50:32 -0400

    Remove one comment, add another

--- src/core_plugins/kibana/public/dashboard/dashboard_constants.js
+++ src/core_plugins/kibana/public/dashboard/dashboard_constants.js
@@ -4,9 +4,6 @@ export const DashboardConstants = {
   ADD_VISUALIZATION_TO_DASHBOARD_MODE_PARAM: 'addToDashboard',
   NEW_VISUALIZATION_ID_PARAM: 'addVisualization',
   LANDING_PAGE_PATH: '/dashboards',
-  // NOTE, the following two urls have to have LANDING_PAGE_PATH as their sub url for
-  // the chrome nav links to work correctly (eg. so navigating away from an opened dashboard, and back to
-  // the dashboard app, will reopen the dashboard, not show the landing page.
   CREATE_NEW_DASHBOARD_URL: '/dashboard',
 };
 
