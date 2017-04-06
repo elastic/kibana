a08f0dbb5a2de2b169474e131486d758cdf9e1fe
Stacey Gammon <gammon@elastic.co>
2017-04-06 09:02:39 -0400

    Indicate search results are limited to the sample size in a saved search on dashboard (#10827)
    * Indicate results are limited to 500 in a saved search on dashboard
    
    Showing the message only on the last page, at the bottom of the search
    results.
    
    * Put warning on top, not on bottom, separate styles
    
    * Use landing page style pager buttons and show the correct total hit count
    
    * Move back to the right, shorten error message, lighten text
    
    * swap order of pager buttons and numbers
    
    * Address code review comments
    
    * Code review comments part 2

--- src/ui/public/doc_table/doc_table.js
+++ src/ui/public/doc_table/doc_table.js
@@ -8,10 +8,10 @@ import 'ui/doc_table/components/table_header';
 import 'ui/doc_table/components/table_row';
 import uiModules from 'ui/modules';
 
-
+import { getLimitedSearchResultsMessage } from './doc_table_strings';
 
 uiModules.get('kibana')
-.directive('docTable', function (config, Notifier, getAppState) {
+.directive('docTable', function (config, Notifier, getAppState, pagerFactory, $filter) {
   return {
     restrict: 'E',
     template: html,
@@ -54,6 +54,13 @@ uiModules.get('kibana')
           };
         };
       }());
+      const limitTo = $filter('limitTo');
+      const calculateItemsOnPage = () => {
+        $scope.pager.setTotalItems($scope.hits.length);
+        $scope.pageOfItems = limitTo($scope.hits, $scope.pager.pageSize, $scope.pager.startIndex);
+      };
+
+      $scope.limitedResultsWarning = getLimitedSearchResultsMessage(config.get('discover:sampleSize'));
 
       $scope.addRows = function () {
         $scope.limit += 50;
@@ -106,6 +113,11 @@ uiModules.get('kibana')
           if ($scope.searchSource !== $scope.searchSource) return;
 
           $scope.hits = resp.hits.hits;
+          // We limit the number of returned results, but we want to show the actual number of hits, not
+          // just how many we retrieved.
+          $scope.totalHitCount = resp.hits.total;
+          $scope.pager = pagerFactory.create($scope.hits.length, 50, 1);
+          calculateItemsOnPage();
 
           return $scope.searchSource.onResults().then(onResults);
         }).catch(notify.fatal);
@@ -113,6 +125,20 @@ uiModules.get('kibana')
         $scope.searchSource.onError(notify.error).catch(notify.fatal);
       }));
 
+      $scope.pageOfItems = [];
+      $scope.onPageNext = () => {
+        $scope.pager.nextPage();
+        calculateItemsOnPage();
+      };
+
+      $scope.onPagePrevious = () => {
+        $scope.pager.previousPage();
+        calculateItemsOnPage();
+      };
+
+      $scope.shouldShowLimitedResultsWarning = () => (
+        !$scope.pager.hasNextPage && $scope.pager.totalItems < $scope.totalHitCount
+      );
     }
   };
 });
