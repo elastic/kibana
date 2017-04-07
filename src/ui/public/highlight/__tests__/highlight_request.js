435c6fe706c2348c9ae5a0f47e55516cd9b4384a
Lukas Olson <olson.lukas@gmail.com>
2017-04-07 10:23:03 -0700

    Fix highlight request module (#11087)
    * Fix highlight request module
    This fixes the highlight request module so that it doesn't add the "all_fields" parameter if the user has specified a "default_field", because these two don't work in conjunction.
    
    * Check for fields too

--- src/ui/public/highlight/__tests__/highlight_request.js
+++ src/ui/public/highlight/__tests__/highlight_request.js
@@ -4,6 +4,8 @@ import getHighlightRequestProvider from '../highlight_request';
 
 describe('getHighlightRequest', () => {
   const queryStringQuery = { query_string: { query: 'foo' } };
+  const queryStringWithDefaultFieldQuery = { query_string: { query: 'foo', default_field: 'bar' } };
+  const queryStringWithFieldQuery = { query_string: { query: 'foo', fields: ['bar'] } };
   const rangeQuery = { range: { '@timestamp': { gte: 0, lte: 0 } } };
   const boolQueryWithSingleCondition = {
     bool: {
@@ -78,5 +80,33 @@ describe('getHighlightRequest', () => {
     getHighlightRequest = getHighlightRequestProvider(config);
     const request = getHighlightRequest(queryStringQuery);
     expect(request.fields['*']).to.not.have.property('highlight_query');
+  });
+
+  it('should enable/disable highlighting if config is changed', () => {
+    getHighlightRequest = getHighlightRequestProvider(config);
+    let request = getHighlightRequest(queryStringQuery);
+    expect(request).to.not.be(undefined);
+
+    config.set('doc_table:highlight', false);
+    request = getHighlightRequest(queryStringQuery);
+    expect(request).to.be(undefined);
+  });
+
+  it('should not add the all_fields param with query_string query when default_field is specified', () => {
+    getHighlightRequest = getHighlightRequestProvider(config);
+    const request = getHighlightRequest(queryStringWithDefaultFieldQuery);
+    expect(request.fields['*']).to.have.property('highlight_query');
+    expect(request.fields['*'].highlight_query.query_string).to.have.property('default_field');
+    expect(request.fields['*'].highlight_query.query_string).to.not.have.property('all_fields');
+    expect(queryStringQuery.query_string).to.not.have.property('all_fields');
+  });
+
+  it('should not add the all_fields param with query_string query when fields are specified', () => {
+    getHighlightRequest = getHighlightRequestProvider(config);
+    const request = getHighlightRequest(queryStringWithFieldQuery);
+    expect(request.fields['*']).to.have.property('highlight_query');
+    expect(request.fields['*'].highlight_query.query_string).to.have.property('fields');
+    expect(request.fields['*'].highlight_query.query_string).to.not.have.property('all_fields');
+    expect(queryStringQuery.query_string).to.not.have.property('all_fields');
   });
 });
