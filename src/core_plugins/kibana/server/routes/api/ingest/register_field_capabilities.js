8392596611725cb20cc804aa8c040cb887699dba
Mathieu Parent <math.parent@gmail.com>
2017-04-12 11:26:52 +0200

    Fix closed index in alias breaking _field_stats #8542

--- src/core_plugins/kibana/server/routes/api/ingest/register_field_capabilities.js
+++ src/core_plugins/kibana/server/routes/api/ingest/register_field_capabilities.js
@@ -13,6 +13,7 @@ export function registerFieldCapabilities(server) {
         fields: '*',
         level: 'cluster',
         index: indices,
+        ignoreUnavailable: true,
         allowNoIndices: false
       })
       .then(
