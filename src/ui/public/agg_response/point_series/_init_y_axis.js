5fca81c5f09159f69dada11c430de8159f0a02af
Peter Pisljar <peter.pisljar@gmail.com>
2017-04-12 17:44:03 +0200

    fixing heatmap y axis title and labels (#11181) (#11186)
    backport da7ab52

--- src/ui/public/agg_response/point_series/_init_y_axis.js
+++ src/ui/public/agg_response/point_series/_init_y_axis.js
@@ -14,6 +14,18 @@ export default function PointSeriesInitYAxis() {
       chart.yAxisLabel = y.col.title;
     }
 
+    const z = chart.aspects.series;
+    if (z) {
+      if (_.isArray(z)) {
+        chart.zAxisFormatter = z[0].agg.fieldFormatter();
+        chart.zAxisLabel = ''; // use the legend
+      } else {
+        chart.zAxisFormatter = z.agg.fieldFormatter();
+        chart.zAxisLabel = z.col.title;
+      }
+    }
+
+
     const xAggOutput = x.agg.write();
     chart.yScale = xAggOutput.metricScale || null;
   };
