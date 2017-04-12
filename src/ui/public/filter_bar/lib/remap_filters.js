afaf3ec2a87f57e50f2cc03736766f89324bd8ce
Stacey Gammon <gammon@elastic.co>
2017-04-12 15:31:44 -0400

    Removing unused files and their tests

--- src/ui/public/filter_bar/lib/remap_filters.js
+++
@@ -1,9 +0,0 @@
-/**
- * Remap the filter from the intermediary back to it's original.
- * @param {object} filter The original filter
- * @returns {object}
- */
-export default function (filter) {
-  return filter.filter;
-}
-
