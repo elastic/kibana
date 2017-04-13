23f920f7cf2595b33342e17680993522a6c65c0d
archanid <artistic.archana@gmail.com>
2017-04-13 16:47:21 -0500

    WIP: what is in the build dir

--- test/scripts/jenkins_selenium.sh
+++ test/scripts/jenkins_selenium.sh
@@ -5,6 +5,7 @@ source "$(dirname $0)/_jenkins_setup.sh"
 
 "$(npm bin)/grunt" build;
 
-cd "build/kibana-6.0.0.-alpha1-SNAPSHOT-linux-x86_64"
+ls build
+#cd "build/kibana-6.0.0.-alpha1-SNAPSHOT-linux-x86_64"
 
-xvfb-run "$(npm bin)/grunt" jenkins:selenium;
+#xvfb-run "$(npm bin)/grunt" jenkins:selenium;
