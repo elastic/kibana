5983d8a4efb89c65afc9870789ef526e88985a5a
Stacey Gammon <gammon@elastic.co>
2017-04-13 10:00:56 -0400

    Fix 5.x tests - something accidentally got back rted that shouldn't have. (#11207) (#11213)

--- test/functional/apps/management/_scripted_fields.js
+++ test/functional/apps/management/_scripted_fields.js
@@ -329,7 +329,7 @@ export default function ({ getService, getPageObjects }) {
         await PageObjects.settings
           .addScriptedField(scriptedPainlessFieldName2, 'painless', 'date',
           { format: 'Date', datePattern: 'YYYY-MM-DD HH:00' }, '1',
-          'doc[\'utc_time\'].value.getMillis() + (1000) * 60 * 60');
+          'doc[\'utc_time\'].value + (1000) * 60 * 60');
         await retry.try(async function() {
           expect(parseInt(await PageObjects.settings.getScriptedFieldsTabCount())).to.be(startingCount + 1);
         });
