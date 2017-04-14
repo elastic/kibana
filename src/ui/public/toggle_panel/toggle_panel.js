2cd6f6b5fc101ab120fcedf30009482183ef265f
Jim Unger <BigFunger@gmail.com>
2017-04-14 09:50:44 -0500

    Adds toggle_panel angular component and ui framework styles

---
+++ src/ui/public/toggle_panel/toggle_panel.js
@@ -0,0 +1,27 @@
+import uiModules from 'ui/modules';
+import template from './toggle_panel.html';
+import 'ui/toggle_button';
+
+const app = uiModules.get('kibana');
+
+app.directive('togglePanel', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    transclude: true,
+    template: template,
+    scope: {
+      togglePanelId: '@',
+      text: '@',
+      isCollapsed: '=',
+      onToggleButtonClick: '='
+    },
+    controllerAs: 'togglePanel',
+    bindToController: true,
+    controller: class TogglePanelController {
+      toggle = () => {
+        this.onToggleButtonClick(this.togglePanelId);
+      };
+    }
+  };
+});
