4f0b5d9234a6a7e65939acadd75ddde4d268de33
archana <archanid@users.noreply.github.com>
2017-04-17 13:28:52 -0500

    Update pleaserun.js to use `--log-file-stdout`

--- tasks/build/pleaserun.js
+++ tasks/build/pleaserun.js
@@ -16,7 +16,7 @@ export default (grunt) => {
         '--description', capitalize(name),
         '--user', user,
         '--group', group,
-        '--log-path', `${path.logs}/`,
+        '--log-file-stdout', `${path.logs}/`,
         '-p', service.name,
         '-v', service.version,
         path.kibanaBin,
