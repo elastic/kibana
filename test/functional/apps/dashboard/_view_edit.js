697f363558dfe4605e48e1d9f5aba4443f9625d7
Jonathan Budzenski <jbudz@users.noreply.github.com>
2017-04-21 16:01:35 -0500

    [test/dashboard] Retry filter check (#11372)
    * [test/dashboard] Retry filter check
    
    * [test/dashboard] Use await on getFilters

--- test/functional/apps/dashboard/_view_edit.js
+++ test/functional/apps/dashboard/_view_edit.js
@@ -3,6 +3,7 @@ import expect from 'expect.js';
 export default function ({ getService, getPageObjects }) {
   const testSubjects = getService('testSubjects');
   const kibanaServer = getService('kibanaServer');
+  const retry = getService('retry');
   const remote = getService('remote');
   const PageObjects = getPageObjects(['dashboard', 'header', 'common', 'visualize']);
   const dashboardName = 'Dashboard View Edit Test';
@@ -173,9 +174,11 @@ export default function ({ getService, getPageObjects }) {
           // was loaded initially
           await PageObjects.dashboard.loadSavedDashboard(dashboardName);
           await PageObjects.dashboard.clickEdit();
-
-          const originalFilters = await PageObjects.dashboard.getFilters();
-
+          const originalFilters = await retry.try(async () => {
+            const filters = await PageObjects.dashboard.getFilters();
+            if (!filters.length) throw new Error('expected filters');
+            return filters;
+          });
           // Click to cause hover menu to show up, but it will also actually click the filter, which will turn
           // it off, so we need to click twice to turn it back on.
           await originalFilters[0].click();
