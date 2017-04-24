8c838c86565d21a89c120b09cdb105a641733ff1
Thomas Neirynck <thomas@elastic.co>
2017-04-24 14:12:19 -0400

    Fix map center and zoom persistence (#11299)
    Previously, we would listen to the generic moveend event to listen to map changes. However, when the panels of "save" and "reporting" would slide open, it would cause minor jitters to the map-center (in the order of the 5th decimal digit), due to the map being slightly nudged down and back up. Now, we listen to drag and zoom events specifically, and no longer the catch-all moveend event.
    
    This commit also improves the initialization of the map/zoom-settings by applying them at map-construction. It also now correctly uses the default settings.

--- src/ui/public/vis_maps/kibana_map.js
+++ src/ui/public/vis_maps/kibana_map.js
@@ -96,11 +96,15 @@ class KibanaMap extends EventEmitter {
     this._listeners = [];
     this._showTooltip = false;
 
-    this._leafletMap = L.map(containerNode, {
+
+    const leafletOptions = {
       minZoom: options.minZoom,
-      maxZoom: options.maxZoom
-    });
-    this._leafletMap.fitWorld();
+      maxZoom: options.maxZoom,
+      center: options.center ? options.center : [0, 0],
+      zoom: options.zoom ? options.zoom : 0
+    };
+
+    this._leafletMap = L.map(containerNode, leafletOptions);
     const worldBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
     this._leafletMap.setMaxBounds(worldBounds);
 
@@ -112,7 +116,7 @@ class KibanaMap extends EventEmitter {
       }
     });
     this._leafletMap.on('zoomend', () => this.emit('zoomend'));
-    this._leafletMap.on('moveend', () => this.emit('moveend'));
+    this._leafletMap.on('dragend', () => this.emit('dragend'));
     this._leafletMap.on('dragend', e => this._layers.forEach(layer => layer.updateExtent('dragend', e)));
     this._leafletMap.on('mousemove', e => this._layers.forEach(layer => layer.movePointer('mousemove', e)));
     this._leafletMap.on('mouseout', e => this._layers.forEach(layer => layer.movePointer('mouseout', e)));
@@ -170,6 +174,7 @@ class KibanaMap extends EventEmitter {
 
     this.resize();
 
+
   }
 
   setShowTooltip(showTooltip) {
@@ -205,7 +210,6 @@ class KibanaMap extends EventEmitter {
           this._popup.setContent(event.content);
         }
       }
-
 
     };
 
@@ -508,7 +512,7 @@ class KibanaMap extends EventEmitter {
   }
 
   persistUiStateForVisualization(visualization) {
-    this.on('moveend', () => {
+    function persistMapStateInUiState() {
       const uiState = visualization.getUiState();
       const centerFromUIState = uiState.get('mapCenter');
       const zoomFromUiState = parseInt(uiState.get('mapZoom'));
@@ -519,7 +523,10 @@ class KibanaMap extends EventEmitter {
       if (!centerFromUIState || centerFromMap.lon !== centerFromUIState[1] || centerFromMap.lat !== centerFromUIState[0]) {
         uiState.set('mapCenter', [centerFromMap.lat, centerFromMap.lon]);
       }
-    });
+    }
+
+    this.on('dragend', persistMapStateInUiState);
+    this.on('zoomend', persistMapStateInUiState);
   }
 
   useUiStateFromVisualization(visualization) {
