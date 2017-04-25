fee2d6bde06bc2c1d209fb0f9fadda366dd24489
ppisljar <peter.pisljar@gmail.com>
2017-04-25 08:08:25 +0200

    adding test for whitespace chars (newline and return)

--- src/core_plugins/timelion/server/handlers/__tests__/parse_sheet.js
+++ src/core_plugins/timelion/server/handlers/__tests__/parse_sheet.js
@@ -20,4 +20,12 @@ describe('timelion parse_sheet function', function () {
     expect(ast[0][0].type).to.equal('chain');
     expect(ast[0][1].type).to.equal('chain');
   });
+
+  it('allows new line for white space', function () {
+    const data = [`.es()\n\r ,\n\r .es(404)`];
+    const ast = parseSheet(data);
+    expect(ast[0].length).to.equal(2);
+    expect(ast[0][0].type).to.equal('chain');
+    expect(ast[0][1].type).to.equal('chain');
+  });
 });
