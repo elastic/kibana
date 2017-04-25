46c24bc3cc187fa8206268ddc44c4c82ed686d93
Stacey Gammon <gammon@elastic.co>
2017-04-25 09:02:55 -0400

    Fix issue with default to named conversion merge

--- src/ui/public/react_components.js
+++ src/ui/public/react_components.js
@@ -4,7 +4,7 @@ import {
   KuiToolBarSearchBox,
 } from 'ui_framework/components';
 
-import uiModules from 'ui/modules';
+import { uiModules } from 'ui/modules';
 const app = uiModules.get('app/kibana', ['react']);
 app.directive('toolBarSearchBox', function (reactDirective) {
   return reactDirective(KuiToolBarSearchBox);
