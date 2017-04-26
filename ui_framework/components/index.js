7d4ce8c60199b3c486fc22cc4ae9a16568563ec5
Stacey Gammon <gammon@elastic.co>
2017-04-26 10:57:52 -0400

    React search box tool bar (#10821) (#11443)
    * Add a react tool bar search box
    
    Create basic react toolbar elements and update uiframework docs
    
    * Package.json: put back ngreact (edits got overwritten)
    
    * Add jest tests
    
    * Combine basic tests into one, eliminate helper functions
    
    * Address code review comments
    
    - move tool_bar_footer into it’s own file.
    - some stylistic html changes
    - comments
    
    * Remove toolbar_with_search_only
    
    It isn’t being used in kibana currently, so we probably don’t need to
    support it in our ui_Framework, and the need for the custom
    “className="kuiToolBar--searchOnly”” indicates we should resdesign it
    if we do need it some day.
    
    * Fix issue with default to named conversion merge
    
    * rename commonHtmlProps = requiredProps

--- ui_framework/components/index.js
+++ ui_framework/components/index.js
@@ -5,3 +5,8 @@ export {
   KuiLinkButton,
   KuiSubmitButton,
 } from './button';
+export {
+  KuiToolBarSearchBox,
+  KuiToolBar,
+  KuiToolBarFooter,
+} from './tool_bar';
---
+++ ui_framework/components/tool_bar/index.js
@@ -0,0 +1,3 @@
+export { KuiToolBarSearchBox } from './tool_bar_search_box';
+export { KuiToolBar } from './tool_bar';
+export { KuiToolBarFooter } from './tool_bar_footer';
