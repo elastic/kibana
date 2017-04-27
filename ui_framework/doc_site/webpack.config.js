3041e6069f5067a1db55665a6e4a7810ab5e028a
CJ Cenizal <cj@cenizal.com>
2017-04-27 11:41:36 -0700

    [UI Framework] Add uiFramework:build task. (#11402)
    * Add uiFramework:build task.

--- ui_framework/doc_site/webpack.config.js
+++ ui_framework/doc_site/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, 'ui_framework/doc_site/build'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
 
