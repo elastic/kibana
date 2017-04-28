1cff416495c5e2d65f3952d71048b0321a5ada77
Peter Pisljar <peter.pisljar@gmail.com>
2017-04-28 09:01:29 +0200

    showing error when cyclic metric definition happens (#11438) backport  35fbe23
    # Conflicts:
    #	src/ui/public/agg_types/index.js

--- src/ui/public/agg_types/index.js
+++ src/ui/public/agg_types/index.js
@@ -29,7 +29,7 @@ import AggTypesMetricsBucketSumProvider from 'ui/agg_types/metrics/bucket_sum';
 import AggTypesMetricsBucketAvgProvider from 'ui/agg_types/metrics/bucket_avg';
 import AggTypesMetricsBucketMinProvider from 'ui/agg_types/metrics/bucket_min';
 import AggTypesMetricsBucketMaxProvider from 'ui/agg_types/metrics/bucket_max';
-
+import 'ui/directives/validate_agg';
 
 export default function AggTypeService(Private) {
 
