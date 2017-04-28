c006e847b4301adcf0ffada4972c16003eda1969
Lee Drengenberg <lee.drengenberg@elastic.co>
2017-04-28 13:41:45 -0500

    Change expected data to match change in table UI (#11495)
    * Change expected data to match change in table UI
    
    * fix more expected results for data UI change
    
    * Revert one previous change and fix one other test
    
    * fix last issues
    
    * missed splitting the last array element
    
    * missed splitting the last array element

--- test/functional/apps/visualize/_heatmap_chart.js
+++ test/functional/apps/visualize/_heatmap_chart.js
@@ -93,16 +93,16 @@ export default function ({ getService, getPageObjects }) {
 
       it('should show correct data', function () {
         // this is only the first page of the tabular data.
-        const expectedChartData =  [ 'September 20th 2015, 00:00:00.000 37',
-          'September 20th 2015, 03:00:00.000 202',
-          'September 20th 2015, 06:00:00.000 740',
-          'September 20th 2015, 09:00:00.000 1,437',
-          'September 20th 2015, 12:00:00.000 1,371',
-          'September 20th 2015, 15:00:00.000 751',
-          'September 20th 2015, 18:00:00.000 188',
-          'September 20th 2015, 21:00:00.000 31',
-          'September 21st 2015, 00:00:00.000 42',
-          'September 21st 2015, 03:00:00.000 202'
+        const expectedChartData =  [ 'September 20th 2015, 00:00:00.000', '37',
+          'September 20th 2015, 03:00:00.000', '202',
+          'September 20th 2015, 06:00:00.000', '740',
+          'September 20th 2015, 09:00:00.000', '1,437',
+          'September 20th 2015, 12:00:00.000', '1,371',
+          'September 20th 2015, 15:00:00.000', '751',
+          'September 20th 2015, 18:00:00.000', '188',
+          'September 20th 2015, 21:00:00.000', '31',
+          'September 21st 2015, 00:00:00.000', '42',
+          'September 21st 2015, 03:00:00.000', '202'
         ];
 
         return PageObjects.visualize.collapseChart()
