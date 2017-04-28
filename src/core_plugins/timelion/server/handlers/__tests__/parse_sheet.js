068d4bd6db42b9d0708990065b1acdb6a033f624
ppisljar <peter.pisljar@gmail.com>
2017-04-28 08:50:25 +0200

    improving readability

--- src/core_plugins/timelion/server/handlers/__tests__/parse_sheet.js
+++ src/core_plugins/timelion/server/handlers/__tests__/parse_sheet.js
@@ -3,29 +3,32 @@ const parseSheet = require('../lib/parse_sheet');
 const expect = require('chai').expect;
 
 describe('timelion parse_sheet function', function () {
-
-  it('does not allow whitespace used between two expressions', async function () {
+  it(`doesn't split expressions on whitespace`, async function () {
     const data = ['.es() .es(404)'];
     const ast = parseSheet(data);
 
-    expect(ast[0].length).to.equal(1);
-    expect(ast[0][0].type).to.equal('chain');
+    const expressions = ast[0];
+    expect(expressions.length).to.equal(1);
+    expect(expressions[0].type).to.equal('chain');
   });
 
-  it('allows comma-delimited expressions', function () {
+  it('splits expressions on commas', function () {
     const data = ['.es(), .es(404)'];
     const ast = parseSheet(data);
 
-    expect(ast[0].length).to.equal(2);
-    expect(ast[0][0].type).to.equal('chain');
-    expect(ast[0][1].type).to.equal('chain');
+    const expressions = ast[0];
+    expect(expressions.length).to.equal(2);
+    expect(expressions[0].type).to.equal('chain');
+    expect(expressions[1].type).to.equal('chain');
   });
 
-  it('allows new line for white space', function () {
+  it('splits expressions on newlines', function () {
     const data = [`.es()\n\r ,\n\r .es(404)`];
     const ast = parseSheet(data);
-    expect(ast[0].length).to.equal(2);
-    expect(ast[0][0].type).to.equal('chain');
-    expect(ast[0][1].type).to.equal('chain');
+
+    const expressions = ast[0];
+    expect(expressions.length).to.equal(2);
+    expect(expressions[0].type).to.equal('chain');
+    expect(expressions[1].type).to.equal('chain');
   });
 });
