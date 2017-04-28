ba9457dc250f27a268d01aa7d5e48424f170b584
Matt Bargar <mbargar@gmail.com>
2017-04-28 15:37:12 -0400

    Report shard failures in the field_capabilities response (#11450)

--- src/ui/public/index_patterns/_enhance_fields_with_capabilities.js
+++ src/ui/public/index_patterns/_enhance_fields_with_capabilities.js
@@ -1,11 +1,20 @@
 import chrome from 'ui/chrome';
 import _ from 'lodash';
+import Notifier from 'ui/notify/notifier';
+import { ShardFailure } from 'ui/errors';
 
 export default function ($http) {
+  const notifier = new Notifier({
+    location: 'Field Capabilities'
+  });
 
   return function (fields, indices) {
     return $http.get(chrome.addBasePath(`/api/kibana/${indices}/field_capabilities`))
     .then((res) => {
+      if (_.get(res, 'data.shard_failure_response')) {
+        notifier.warning(new ShardFailure(res.data.shard_failure_response));
+      }
+
       const stats = _.get(res, 'data.fields', {});
 
       return _.map(fields, (field) => {
