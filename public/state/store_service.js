d75fe770337eb0cd2ba3f3a4b4d49af97e5c3d5c
Joe Fleming <joe.fleming@gmail.com>
2017-05-02 13:09:31 -0700

    strip dependencies down to the core

--- public/state/store_service.js
+++ public/state/store_service.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { createStore } from 'redux';
 import rootReducer from './reducers/root_reducer';
 import getInitialState from './initial_state';
 
@@ -11,13 +10,7 @@ app.service('$store', (kbnVersion, basePath) => {
   // Set the defaults from Kibana plugin
   initialState.app = { kbnVersion, basePath };
 
-  // Limit redux updates to 20FPS
-  //const debounceNotify = _.debounce(notify => notify(), 50, {maxWait: 50});
-  const persistentStore = compose(
-    applyMiddleware(thunkMiddleware),
-  );
-
-  const store = createStore(rootReducer, initialState, persistentStore);
+  const store = createStore(rootReducer, initialState);
 
   // TODO: Sticking this here so I can dispatch events from the console;
   window.store = store;
