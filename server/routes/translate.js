a62d1a9b6b48060d8c2e54b8246137382c3042bc
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-02 15:21:34 -0700

    Add expression POST route

--- server/routes/translate.js
+++ server/routes/translate.js
@@ -1,4 +1,4 @@
-import { fromExpression, /*toExpression*/ } from '../../common/lib/ast';
+import { fromExpression, toExpression } from '../../common/lib/ast';
 
 export function translate(server) {
   /*
@@ -16,29 +16,15 @@ export function translate(server) {
 
   server.route({
     method: 'POST',
-    path: '/api/canvas/ast',
+    path: '/api/canvas/expression',
     handler: function (request, reply) {
-      console.log('word');
-      if (!request.query.expression) return reply({ error: '"expression" query is required' }).code(400);
-      reply(fromExpression(request.query.expression));
+      try {
+        const exp = toExpression(request.payload);
+        reply(exp);
+      } catch (e) {
+        reply({ error: e.message }).code(400);
+      }
     }
   });
-
-  /*
-  app.get('/api/ast', function response(req, res) {
-    if (!req.query.expression) return res.end('"expression" query is required').status(400);
-    res.json(fromExpression(req.query.expression));
-  });
-
-
-  app.post('/api/expression', function response(req, res) {
-    try {
-      const exp = toExpression(req.body);
-      res.end(exp);
-    } catch (e) {
-      res.status(400).send(e.message);
-    }
-  });
-  */
 
 }
