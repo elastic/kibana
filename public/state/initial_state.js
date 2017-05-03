1a3c44bbefd7fd3520959311849fb3e85d608274
Joe Fleming <joe.fleming@gmail.com>
2017-05-02 19:30:36 -0700

    make initial_state a getter

--- public/state/initial_state.js
+++ public/state/initial_state.js
@@ -1,7 +1,15 @@
-export default () => {
-  return {
+import { get } from 'lodash';
+
+export default (path) => {
+  const state = {
     app: {}, // Kibana stuff in here
     transient: {},
     persistent: {}
   };
+
+  if (!path) {
+    return state;
+  }
+
+  return get(state, path);
 };
