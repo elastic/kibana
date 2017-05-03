0a3f128495b32990679e975bfbcdfdf4ccb8c2c7
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-03 11:06:04 -0700

    Un-nesting styles

--- src/core_plugins/getting_started/public/components/getting_started/getting_started.js
+++ src/core_plugins/getting_started/public/components/getting_started/getting_started.js
@@ -1,14 +1,23 @@
 import { uiModules } from 'ui/modules';
 import template from './getting_started.html';
 import './getting_started.less';
+import 'ui/storage';
+// import { gettingStartedRegistry } from 'ui/getting_started_registry';
 
 import kibanaLogo from 'ui/images/logo-kibana-small.svg';
 import beatsLogo from 'ui/images/logo-beats-small.svg';
 import logstashLogo from 'ui/images/logo-logstash-small.svg';
+import dashboardIcon from 'ui/images/icon-dashboard.svg';
+import shieldIcon from 'ui/images/icon-shield.svg';
 
 const app = uiModules.get('kibana');
 
-app.directive('gettingStarted', function () {
+const GETTING_STARTED_OPT_OUT = 'kibana.isGettingStartedOptedOut';
+
+app.directive('gettingStarted', function ($injector) {
+
+  const localStorageService = $injector.get('localStorage');
+
   return {
     restrict: 'E',
     template: template,
@@ -18,20 +27,29 @@ app.directive('gettingStarted', function () {
     controllerAs: 'gettingStarted',
     controller: class GettingStartedController {
       constructor() {
-        this.manageAndMonitorItems = [
-          'foo bar.',
-          'baz qux quux'
-        ];
+        // this.manageAndMonitorMessages = gettingStartedRegistry.manageAndMonitorMessages;
+        this.manageAndMonitorMessages = [
+          'Manage your <a href="">users</a> and <a href="">roles</a>.',
+          'Use live <a href="">monitoring</a> for health checks.',
+          'Set up <a href="">watches</a> to keep tabs on your data.'
+        ].join(' ');
 
-        this.logoUrls = {
-          kibana: kibanaLogo,
-          beats: beatsLogo,
-          logstash: logstashLogo
+        this.imageUrls = {
+          kibanaLogo,
+          beatsLogo,
+          logstashLogo,
+          dashboardIcon,
+          shieldIcon
         };
       }
 
-      get hasManageAndMonitorItems() {
-        return this.manageAndMonitorItems.length > 0;
+      get hasManageAndMonitorMessages() {
+        // return this.manageAndMonitorMessages.length === 0;
+        return this.manageAndMonitorMessages.length > 0;
+      }
+
+      recordOptOut = () => {
+        localStorageService.set(GETTING_STARTED_OPT_OUT, true);
       }
     }
   };
