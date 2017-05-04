020ceaa68b407afa6c353151b1f4015067e9e57c
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-04 14:02:43 -0700

    wire up thunk, for now

--- public/state/store_service.js
+++ public/state/store_service.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/root_reducer';
 import getInitialState from './initial_state';
 import { uiModules } from 'ui/modules';
@@ -9,9 +10,12 @@ app.service('$store', (kbnVersion, basePath) => {
 
   const initialState = getInitialState();
   // Set the defaults from Kibana plugin
-  initialState.app = { kbnVersion, basePath };
+  initialState.app = {
+    kbnVersion,
+    basePath,
+  };
 
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
   // TODO: Sticking this here so I can dispatch events from the console;
   window.store = store;
