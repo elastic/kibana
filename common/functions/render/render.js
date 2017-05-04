4f876a4f98866c065a3a1f079a181d35e35e0e2e
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-04 15:49:38 -0700

    Use render() to render an type with a default view, if available

---
+++ common/functions/render/render.js
@@ -0,0 +1,15 @@
+const Fn = require('../fn.js');
+
+module.exports = new Fn({
+  name: 'render',
+  aliases: [],
+  type: 'render',
+  help: 'Render an object with its default renderer, if one exists',
+  context: {
+    types: [
+      'render'
+    ]
+  },
+  args: {},
+  fn: (context) => context
+});
