c33a7b4180a1d73713aba49248e8bdbce21c6b31
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2017-05-05 16:38:47 -0700

    incorporating the feedback

--- test/functional/config.js
+++ test/functional/config.js
@@ -30,14 +30,14 @@ import { servers, apps } from '../server_config';
 export default function () {
   return {
     testFiles: [
-      // resolve(__dirname, './apps/console/index.js'),
-      // resolve(__dirname, './apps/context/index.js'),
-      // resolve(__dirname, './apps/dashboard/index.js'),
-      // resolve(__dirname, './apps/discover/index.js'),
-      // resolve(__dirname, './apps/management/index.js'),
-      // resolve(__dirname, './apps/status_page/index.js'),
+      resolve(__dirname, './apps/console/index.js'),
+      resolve(__dirname, './apps/context/index.js'),
+      resolve(__dirname, './apps/dashboard/index.js'),
+      resolve(__dirname, './apps/discover/index.js'),
+      resolve(__dirname, './apps/management/index.js'),
+      resolve(__dirname, './apps/status_page/index.js'),
       resolve(__dirname, './apps/visualize/index.js'),
-      // resolve(__dirname, './apps/xpack/index.js'),
+      resolve(__dirname, './apps/xpack/index.js'),
     ],
     pageObjects: {
       common: CommonPageProvider,
