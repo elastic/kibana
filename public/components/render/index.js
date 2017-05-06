39621a1672add99eaf3c7d6702ed7758b772fc2a
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-05 17:10:51 -0700

    Remove wrapper

--- public/components/render/index.js
+++ public/components/render/index.js
@@ -3,12 +3,11 @@ import { Render as Component } from './render';
 import { getType } from '../../../common/types/get_type';
 import { Loading } from '../loading';
 
-const whenReady = () =>
-  branch(
-    props => props.expressionType !== 'render',
-    renderComponent(Loading)
-  );
+const whenReady = branch(
+  props => props.expressionType !== 'render',
+  renderComponent(Loading)
+);
 
 export const Render = withProps(({ expressionOutput }) => ({
   expressionType: getType(expressionOutput),
-}))(whenReady()(Component));
+}))(whenReady(Component));
