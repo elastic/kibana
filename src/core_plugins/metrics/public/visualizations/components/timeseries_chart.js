b34fa7f4542112382f9432c8ae61d5411a27bd33
Chris Cowan <chris@chriscowan.us>
2017-05-05 17:46:20 -0700

    Adding x-axis label to show the bucket size

--- src/core_plugins/metrics/public/visualizations/components/timeseries_chart.js
+++ src/core_plugins/metrics/public/visualizations/components/timeseries_chart.js
@@ -197,12 +197,17 @@ class TimeseriesChart extends Component {
     };
 
     const annotations = this.state.annotations.map(this.renderAnnotations);
+    let axisLabelClass = 'rhythm_chart__axis-label';
+    if (this.props.reversed) {
+      axisLabelClass += ' reversed';
+    }
 
     return (
       <div ref={(el) => this.container = el} className="rhythm_chart__timeseries-container">
         { tooltip }
         { annotations }
         <FlotChart {...params}/>
+        <div className={axisLabelClass}>{this.props.xaxisLabel}</div>
       </div>
     );
   }
@@ -221,6 +226,7 @@ TimeseriesChart.propTypes = {
   show: PropTypes.array,
   tickFormatter: PropTypes.func,
   yaxes: PropTypes.array,
+  xaxisLabel: PropTypes.string
 };
 
 export default TimeseriesChart;
