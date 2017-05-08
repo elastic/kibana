a5ffb5dbfd1e4e798e9fd70218dd5246e6cb2940
Chris Cowan <chris@chriscowan.us>
2017-05-08 12:31:26 -0700

    Static Series for Time Series Visual Builder (#11496)
    * Adding ability to add static value to Time Series Visual Builder
    
    * Fixing tests

--- src/core_plugins/metrics/public/components/lib/agg_to_component.js
+++ src/core_plugins/metrics/public/components/lib/agg_to_component.js
@@ -11,6 +11,7 @@ import SerialDiff from '../aggs/serial_diff';
 import PositiveOnly from '../aggs/positive_only';
 import FilterRatio from '../aggs/filter_ratio';
 import PercentileRank from '../aggs/percentile_rank';
+import Static from '../aggs/static';
 export default {
   count: StdAgg,
   avg: StdAgg,
@@ -38,7 +39,8 @@ export default {
   series_agg: SeriesAgg,
   serial_diff: SerialDiff,
   filter_ratio: FilterRatio,
-  positive_only: PositiveOnly
+  positive_only: PositiveOnly,
+  static: Static
 };
 
 
