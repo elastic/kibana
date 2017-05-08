933e4c9fd95da03dbf921e8c18c97c163dfe428b
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-08 10:59:18 -0700

    Add alterColumn

---
+++ common/functions/alterColumn/alterColumn.js
@@ -0,0 +1,55 @@
+const Fn = require('../fn.js');
+
+const _ = require('lodash');
+module.exports = new Fn({
+  name: 'alterColumn',
+  type: 'datatable',
+  help: 'Converts between core types, eg string, number, null, boolean, date',
+  context: {
+    types: ['datatable'],
+  },
+  args: {
+    column: {
+      types: ['string'],
+    },
+    type: {
+      types: ['string'],
+    },
+    name: {
+      types: ['string'],
+    },
+  },
+  fn: (context, args) => {
+    const column = _.find(context.columns, { name: args.column });
+    if (!column) throw new Error(`Column not found: '${args.column}'`);
+
+    let destination = args.column;
+    if (args.name) {
+      destination = args.name;
+    }
+
+    const type = args.type || column.type;
+
+    let handler = _.noop;
+    if (args.type) {
+      handler = (function getHandler() {
+        switch (type) {
+          case 'string': return String;
+          case 'number': return Number;
+          case 'date': return (v) => (new Date(v)).valueOf();
+          case 'boolean': return Boolean;
+          default: throw new Error(`can not convert to ${type}`);
+        }
+      }());
+    }
+
+    column.name = destination;
+    column.type = type;
+
+    context.rows = _.map(context.rows, row =>
+      Object.assign(_.omit(row, args.column), { [destination]: handler(row[args.column]) })
+    );
+
+    return context;
+  },
+});
--- public/lib/load_functions.js
+++ public/lib/load_functions.js
@@ -4,5 +4,7 @@ addFunction(require('../functions/clientdata/clientdata'));
 addFunction(require('../functions/navigator/navigator'));
 
 addFunction(require('../../common/functions/mapColumn/mapColumn'));
+addFunction(require('../../common/functions/alterColumn/alterColumn'));
+
 addFunction(require('../../common/functions/sort/sort'));
 addFunction(require('../../common/functions/render/render'));
