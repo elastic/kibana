57ffdc9edb6a446b6675edb5d8b47dfe1013a748
CJ Cenizal <cj@cenizal.com>
2017-05-09 11:08:59 -0700

    [UI Framework] Improve navigability of the docs (#11666)
    * Add search to navigation.
    * Add pagination buttons to UI Framework header.

--- ui_framework/doc_site/src/components/guide_nav/guide_nav.js
+++ ui_framework/doc_site/src/components/guide_nav/guide_nav.js
@@ -1,4 +1,5 @@
 import React, {
+  Component,
   PropTypes,
 } from 'react';
 
@@ -8,75 +9,116 @@ import {
 
 import classNames from 'classnames';
 
-export const GuideNav = props => {
-  const classes = classNames('guideNav', {
-    'is-guide-nav-open': props.isNavOpen,
-  });
+export class GuideNav extends Component {
+  constructor() {
+    super();
 
-  const buttonClasses = classNames('guideNav__menu fa fa-bars', {
-    'is-menu-button-pinned': props.isNavOpen,
-  });
+    this.state = {
+      search: '',
+    };
 
-  const componentNavItems = props.components.map((item, index) => {
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({
+      search: event.target.value,
+    });
+  }
+
+  renderNoItems(name) {
     return (
-      <Link
-        key={index}
-        className="guideNavItem"
-        to={item.path}
-        onClick={props.onClickNavItem}
-      >
-        {item.name}
-      </Link>
+      <p className="guideNavNoItems">
+        { `No ${name} match your search` }
+      </p>
     );
-  });
+  }
 
-  const sandboxNavItems = props.sandboxes.map((item, index) => {
+  render() {
+    const classes = classNames('guideNav', {
+      'is-guide-nav-open': this.props.isNavOpen,
+    });
+
+    const buttonClasses = classNames('guideNav__menu fa fa-bars', {
+      'is-menu-button-pinned': this.props.isNavOpen,
+    });
+
+    const componentNavItems =
+      this.props.components.filter(item => (
+        item.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+      )).map((item, index) => {
+        return (
+          <Link
+            key={index}
+            className="guideNavItem"
+            to={item.path}
+            onClick={this.props.onClickNavItem}
+          >
+            {item.name}
+          </Link>
+        );
+      });
+
+    const sandboxNavItems =
+      this.props.sandboxes.filter(item => (
+        item.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+      )).map((item, index) => {
+        return (
+          <Link
+            key={index}
+            className="guideNavItem"
+            to={item.path}
+            onClick={this.props.onClickNavItem}
+          >
+            {item.name}
+          </Link>
+        );
+      });
+
     return (
-      <Link
-        key={index}
-        className="guideNavItem"
-        to={item.path}
-        onClick={props.onClickNavItem}
-      >
-        {item.name}
-      </Link>
-    );
-  });
+      <div className={classes}>
+        <div className="guideNav__header">
+          <div
+            className={buttonClasses}
+            onClick={this.props.onToggleNav}
+          />
+          <Link
+            className="guideNav__title"
+            to="/"
+            onClick={this.props.onClickNavItem}
+          >
+            Kibana UI Framework <span className="guideNav__version">{this.props.version}</span>
+          </Link>
+        </div>
 
-  return (
-    <div className={classes}>
-      <div className="guideNav__header">
-        <div
-          className={buttonClasses}
-          onClick={props.onToggleNav}
-        />
-        <Link
-          className="guideNav__title"
-          to="/"
-          onClick={props.onClickNavItem}
-        >
-          Kibana UI Framework <span className="guideNav__version">{props.version}</span>
-        </Link>
-      </div>
+        <div>
+          <input
+            type="text"
+            className="guideNavSearch"
+            value={this.state.search}
+            onChange={this.onSearchChange}
+          />
+        </div>
 
-      <div className="guideNavItemsContainer">
-        <div className="guideNavItems">
-          <div className="guideNavSectionTitle">
-            Components
+        <div className="guideNavItemsContainer">
+          <div className="guideNavItems">
+            <div className="guideNavSectionTitle">
+              Components
+            </div>
+
+            { componentNavItems.length ? componentNavItems : this.renderNoItems('components') }
+
+            <div className="guideNavSectionTitle">
+              Sandboxes
+            </div>
+
+            { sandboxNavItems.length ? sandboxNavItems : this.renderNoItems('sandboxes') }
           </div>
-
-          {componentNavItems}
-
-          <div className="guideNavSectionTitle">
-            Sandboxes
-          </div>
-
-          {sandboxNavItems}
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 GuideNav.propTypes = {
   isNavOpen: PropTypes.bool,
@@ -86,4 +128,3 @@ GuideNav.propTypes = {
   components: PropTypes.array,
   sandboxes: PropTypes.array,
 };
-
--- ui_framework/doc_site/src/services/routes/routes.js
+++ ui_framework/doc_site/src/services/routes/routes.js
@@ -197,10 +197,26 @@ const sandboxes = [{
   component: ViewSandbox,
 }];
 
+const allRoutes = components.concat(sandboxes);
+
 export default {
   components: Slugify.each(components, 'name', 'path'),
   sandboxes: Slugify.each(sandboxes, 'name', 'path'),
   getAppRoutes: function getAppRoutes() {
-    return this.components.concat(this.sandboxes);
+    return allRoutes;
+  },
+  getPreviousRoute: function getPreviousRoute(routeName) {
+    const index = allRoutes.findIndex(item => {
+      return item.name === routeName;
+    });
+
+    return index >= 0 ? allRoutes[index - 1] : undefined;
+  },
+  getNextRoute: function getNextRoute(routeName) {
+    const index = allRoutes.findIndex(item => {
+      return item.name === routeName;
+    });
+
+    return index < allRoutes.length - 1 ? allRoutes[index + 1] : undefined;
   },
 };
