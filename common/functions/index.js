968eed58a24c553f632768b4e85cc61e7e0df87d
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-09 13:41:22 -0700

    Add missing index.js

---
+++ common/functions/index.js
@@ -0,0 +1,12 @@
+export const commonFunctions = [
+  require('./mapColumn/mapColumn'),
+  require('./alterColumn/alterColumn'),
+  require('./math/math'),
+  require('./sort/sort'),
+  require('./render/render'),
+  require('./line/line'),
+  require('./bar/bar'),
+  require('./pointseries/pointseries'),
+  require('./replace/replace'),
+  require('./droprows/droprows'),
+];
