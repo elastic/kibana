fba547ece352b24c5fa8e101efe7275257924a06
Joe Fleming <joe.fleming@gmail.com>
2017-05-09 13:22:33 -0700

    make elements reducer test pass

--- public/state/reducers/elements.js
+++ public/state/reducers/elements.js
@@ -6,8 +6,11 @@ import * as actions from '../actions/elements';
 export default handleActions({
   [actions.setAstAndExpression]: (workpadState, { payload }) => {
     const { expression, ast, pageId, element } = payload;
-    const elementsPath = ['pages', pageId, 'elements'];
+    const pageIndex = findIndex(get(workpadState, 'pages'), { id: pageId });
+    const elementsPath = ['pages', pageIndex, 'elements'];
     const elementIndex = findIndex(get(workpadState, elementsPath), { id: element.id });
+
+    if (pageIndex === -1 || elementIndex === -1) return workpadState;
     return assign(workpadState, elementsPath.concat(elementIndex), { ast, expression });
   },
 }, {});
