1a850524e62c4e452066f2283e2f4281049e300b
Joe Fleming <joe.fleming@gmail.com>
2017-05-09 15:29:22 -0700

    replace calls to expression api
    the code it uses is common, just use the ast lib

--- public/state/actions/elements.js
+++ public/state/actions/elements.js
@@ -1,36 +1,32 @@
 import { createAction } from 'redux-actions';
 import { isEqual } from 'lodash';
 import { set } from 'object-path-immutable';
-import { fetch } from '../../../common/lib/fetch';
 import { interpretAst } from '../../lib/interpreter';
+import { fromExpression, toExpression } from '../../../common/lib/ast';
 import * as args from './resolved_args';
 
 // exported actions, used by reducers
 export const setAstAndExpression = createAction('setElementAstAndExpression');
 
-export const setAst = createAction('setAst', ({ ast }) => {
-  return fetch.post('/api/expression', JSON.stringify(ast)).then(res => res.data);
-}, ({ ast, element, pageId }) => ({
-  onComplete: (dispatch, getState, expression) => {
-    if (element.expression === expression && isEqual(element.ast, ast)) return;
-    dispatch(setAstAndExpression({ expression, ast, element, pageId }));
-  },
-}));
+export const setAst = createAction('setAst', ({ ast, element, pageId }) => {
+  const expression = toExpression(ast);
+  if (element.expression === expression && isEqual(element.ast, ast)) return;
+  return setAstAndExpression({ expression, ast, element, pageId });
+});
 
 export const setExpressionAst = ({ ast, element, pageId, index }) => {
   const newAst = set(element, ['ast', 'chain', index], ast);
   return setAst({ ast: newAst, element, pageId });
 };
 
-export const setExpression = createAction('setExpression', ({ expression }) => {
-  return fetch.get(`/api/ast/?expression=${expression}`).then(res => res.data);
-}, ({ expression, element, pageId }) => ({
-  onComplete: (dispatch, getState, ast) => {
+export const setExpression = createAction('setExpression', ({ expression, element, pageId }) => {
+  return (dispatch) => {
+    const ast = fromExpression(expression);
     if (element.expression === expression && isEqual(element.ast, ast)) return;
     dispatch(args.clear({ path: element.id }));
-    dispatch(setAstAndExpression({ expression, ast, element, pageId }));
-  },
-}));
+    return setAstAndExpression({ expression, ast, element, pageId });
+  };
+});
 
 export const fetchContext = createAction('fetchContext', ({ element, index }) => {
   const invalidAst = !Array.isArray(element.ast.chain);
