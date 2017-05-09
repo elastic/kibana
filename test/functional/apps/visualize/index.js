b28be0cfa0638a57a651f0c895d41551c56426f0
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2017-05-09 16:47:42 -0700

    Selenium Test for TagCloud Visualization  (#11636)
    Selenium Test incorporating TagCloud Viz

--- test/functional/apps/visualize/index.js
+++ test/functional/apps/visualize/index.js
@@ -30,6 +30,7 @@ export default function ({ getService, loadTestFile }) {
     loadTestFile(require.resolve('./_data_table'));
     loadTestFile(require.resolve('./_metric_chart'));
     loadTestFile(require.resolve('./_pie_chart'));
+    loadTestFile(require.resolve('./_tag_cloud'));
     loadTestFile(require.resolve('./_tile_map'));
     loadTestFile(require.resolve('./_vertical_bar_chart'));
     loadTestFile(require.resolve('./_heatmap_chart'));
