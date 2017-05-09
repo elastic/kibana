dbcad09fa6365f97fa97efaf07aafc248d2e7157
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-09 16:09:20 -0700

    Allow topMessage to be a directive

--- src/core_plugins/getting_started/public/components/getting_started/getting_started.js
+++ src/core_plugins/getting_started/public/components/getting_started/getting_started.js
@@ -4,6 +4,7 @@ import 'ui/storage';
 import 'ui/filters/trust_as_html';
 import { gettingStartedRegistry } from 'ui/getting_started_registry';
 import { GETTING_STARTED_OPT_OUT } from '../../lib/constants';
+import angular from 'angular';
 
 import kibanaLogo from 'ui/images/logo-kibana-small.svg';
 import beatsLogo from 'ui/images/logo-beats-small.svg';
@@ -19,6 +20,7 @@ const app = uiModules.get('kibana');
 app.directive('gettingStarted', function ($injector) {
 
   const localStorageService = $injector.get('localStorage');
+  const $compile = $injector.get('$compile');
 
   return {
     restrict: 'E',
@@ -28,14 +30,18 @@ app.directive('gettingStarted', function ($injector) {
     bindToController: true,
     controllerAs: 'gettingStarted',
     controller: class GettingStartedController {
-      constructor() {
+      constructor($scope) {
         if (this.hasOptedOut) {
           uiChrome.setVisible(true);
         } else {
           uiChrome.setVisible(false);
         }
 
-        this.topMessage = gettingStartedRegistry.topMessage;
+        // Compile topMessageHtml and inject it into the DOM
+        const topMessageContainer = document.getElementById('gettingStartedTopMessageContainer');
+        const topMessageHtml = $compile(gettingStartedRegistry.topMessage)($scope);
+        angular.element(topMessageContainer).append(topMessageHtml);
+
         this.manageAndMonitorMessages = gettingStartedRegistry.manageAndMonitorMessages.join(' ');
         this.imageUrls = {
           kibanaLogo,
