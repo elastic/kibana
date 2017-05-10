5b4ac25b3c6db7323b53f60d48db7a86e783b83e
ppisljar <peter.pisljar@gmail.com>
2017-05-10 12:00:27 +0200

    removing unused stuff from Vis

--- src/ui/public/vis/vis.js
+++ src/ui/public/vis/vis.js
@@ -9,19 +9,13 @@
  */
 
 import _ from 'lodash';
-import { AggTypesIndexProvider } from 'ui/agg_types/index';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 import { VisAggConfigsProvider } from 'ui/vis/agg_configs';
 import { PersistedState } from 'ui/persisted_state';
 
-export function VisProvider(Notifier, Private) {
-  const aggTypes = Private(AggTypesIndexProvider);
+export function VisProvider(Private) {
   const visTypes = Private(VisTypesRegistryProvider);
   const AggConfigs = Private(VisAggConfigsProvider);
-
-  const notify = new Notifier({
-    location: 'Vis'
-  });
 
   class Vis {
     constructor(indexPattern, state, uiState) {
@@ -37,47 +31,6 @@ export function VisProvider(Notifier, Private) {
 
       this.setState(state);
       this.setUiState(uiState);
-    }
-
-    convertOldState(type, oldState) {
-      if (!type || _.isString(type)) {
-        type = visTypes.byName[type || 'histogram'];
-      }
-
-      const schemas = type.schemas;
-
-      // This was put in place to do migrations at runtime. It's used to support people who had saved
-      // visualizations during the 4.0 betas.
-      const aggs = _.transform(oldState, function (newConfigs, oldConfigs, oldGroupName) {
-        const schema = schemas.all.byName[oldGroupName];
-
-        if (!schema) {
-          notify.log('unable to match old schema', oldGroupName, 'to a new schema');
-          return;
-        }
-
-        oldConfigs.forEach(function (oldConfig) {
-          const agg = {
-            schema: schema.name,
-            type: oldConfig.agg
-          };
-
-          const aggType = aggTypes.byName[agg.type];
-          if (!aggType) {
-            notify.log('unable to find an agg type for old confg', oldConfig);
-            return;
-          }
-
-          agg.params = _.pick(oldConfig, _.keys(aggType.params.byName));
-
-          newConfigs.push(agg);
-        });
-      }, []);
-
-      return {
-        type: type,
-        aggs: aggs
-      };
     }
 
     setState(state) {
@@ -120,14 +73,6 @@ export function VisProvider(Notifier, Private) {
 
     getState() {
       return this.getStateInternal(true);
-    }
-
-    createEditableVis() {
-      return this._editableVis || (this._editableVis = this.clone());
-    }
-
-    getEditableVis() {
-      return this._editableVis || undefined;
     }
 
     clone() {
