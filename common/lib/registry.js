95caaba4e35d4b8675f71803b25db6f72cb8b172
Joe Fleming <joe.fleming@gmail.com>
2017-05-10 16:35:02 -0700

    update registry to pass tests

--- common/lib/registry.js
+++ common/lib/registry.js
@@ -1,15 +1,26 @@
 export function Registry(prop = 'name') {
-  const _indexed = new Object();
+  let _indexed = new Object();
 
   this.register = (obj) => {
+    if (typeof obj !== 'object' || !obj[prop]) {
+      throw new Error(`Register requires an object with a ${prop} property`);
+    }
+
     _indexed[obj[prop]] = obj;
   };
 
   this.toJS = () => {
-    return { ..._indexed };
+    return Object.keys(_indexed).reduce((acc, key) => {
+      acc[key] = Object.assign({}, _indexed[key]);
+      return acc;
+    }, {});
   };
 
   this.get = (name) => {
-    return _indexed[name];
+    return _indexed[name] ? { ..._indexed[name] } : null;
+  };
+
+  this.reset = () => {
+    _indexed = new Object();
   };
 }
