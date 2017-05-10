6e23bb11baedc410642992f3cda00adc67bd91f8
Nathan Reese <reese.nathan@gmail.com>
2017-05-10 13:55:34 -0600

    react hello world

---
+++ src/core_plugins/control_visualizations/public/terms_vis/components/terms_vis.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class TermsVis extends Component {
+  render() {
+    return (
+      <h1>Terms Visualization</h1>
+    );
+  }
+}
--- src/core_plugins/control_visualizations/public/terms_vis/vis_controller.js
+++ src/core_plugins/control_visualizations/public/terms_vis/vis_controller.js
@@ -1,6 +1,9 @@
 import { uiModules } from 'ui/modules';
+import { TermsVis } from './components/terms_vis';
 
-const module = uiModules.get('kibana/terms_vis', ['kibana']);
+const module = uiModules.get('kibana/terms_vis', ['kibana', 'react']);
 module.controller('KbnTermsController', function () {
 
 });
+
+module.value('TermsVis', TermsVis);
