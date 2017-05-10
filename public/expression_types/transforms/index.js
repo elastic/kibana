2b316444a5c89041644273c20484fdd88d22a0d1
Joe Fleming <joe.fleming@gmail.com>
2017-05-10 15:03:00 -0700

    import transforms

---
+++ public/expression_types/transforms/index.js
@@ -0,0 +1,1 @@
+export { sort } from './sort';
---
+++ public/expression_types/transforms/sort.js
@@ -0,0 +1,21 @@
+import { get } from 'lodash';
+import { Transform } from '../transform';
+import { Arg } from '../arg';
+import { getState, getValue } from '../../state/selectors/resolved_args';
+
+export const sort = () => new Transform('sort', {
+  displayName: 'Sort Transform',
+  args: [
+    new Arg('_', {
+      displayName: 'Sort Field',
+      argType: 'dataframe_column',
+      types: ['string'],
+    }),
+  ],
+  resolve({ context }) {
+    if (getState(context) === 'ready') {
+      return { columns: get(getValue(context), 'columns', []) };
+    }
+    return { columns: [] };
+  },
+});
