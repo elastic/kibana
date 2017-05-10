0849eb5f8cbe64901fe5b176aab94cdc148fa37d
Joe Fleming <joe.fleming@gmail.com>
2017-05-10 15:08:10 -0700

    import pointseries model

---
+++ public/expression_types/models/index.js
@@ -0,0 +1,1 @@
+export { pointSeries } from './point_series';
---
+++ public/expression_types/models/point_series.js
@@ -0,0 +1,31 @@
+import { get } from 'lodash';
+import { Model } from '../model';
+import { Arg } from '../arg';
+import { getState, getValue } from '../../state/selectors/resolved_args';
+
+export const pointSeries = () => new Model('pointSeries', {
+  displayName: 'Pointseries Model',
+  args: [
+    new Arg('x', {
+      displayName: 'X-axis',
+      argType: 'dataframe_column',
+      types: ['string'],
+    }),
+    new Arg('y', {
+      displayName: 'Y-axis',
+      argType: 'dataframe_column',
+      types: ['string'],
+    }),
+    new Arg('color', {
+      displayName: 'Color',
+      argType: 'dataframe_column',
+      types: ['string'],
+    }),
+  ],
+  resolve({ context }) {
+    if (getState(context) === 'ready') {
+      return { columns: get(getValue(context), 'columns', []) };
+    }
+    return { columns: [] };
+  },
+});
