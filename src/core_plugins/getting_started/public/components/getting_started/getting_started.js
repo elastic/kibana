a4fac827114bbe29a285015ad3a91d8f14f945fd
Jim Unger <BigFunger@gmail.com>
2017-05-11 18:16:14 -0500

    Breaks out the inject logic into another directive

--- src/core_plugins/getting_started/public/components/getting_started/getting_started.js
+++ src/core_plugins/getting_started/public/components/getting_started/getting_started.js
@@ -1,13 +1,11 @@
 import { uiModules } from 'ui/modules';
 import uiChrome from 'ui/chrome';
-import 'ui/storage';
 import 'ui/filters/trust_as_html';
 import 'ui/getting_started/opt_out_directive';
 import { GettingStartedTopMessagesRegistryProvider } from 'ui/getting_started/top_messages_registry';
 import { GettingStartedMonitorAndManageMessagesRegistryProvider } from 'ui/getting_started/monitor_and_manage_messages_registry';
-import { hasOptedOutOfGettingStarted, optOutOfGettingStarted } from 'ui/getting_started/opt_out_helpers';
+import { hasOptedOutOfGettingStarted } from 'ui/getting_started/opt_out_helpers';
 import { documentationLinks } from 'ui/documentation_links';
-import angular from 'angular';
 
 import kibanaLogo from 'ui/images/logo-kibana-small.svg';
 import beatsLogo from 'ui/images/logo-beats-small.svg';
@@ -17,16 +15,11 @@ import shieldIcon from 'ui/images/icon-shield.svg';
 
 import template from './getting_started.html';
 import './getting_started.less';
-
-function makeAngularParseableExpression(text) {
-  return `<span>${text}</span>`;
-}
+import '../injected_items';
 
 const app = uiModules.get('kibana');
 
 app.directive('gettingStarted', function ($injector) {
-
-  const $compile = $injector.get('$compile');
   const Private = $injector.get('Private');
 
   const topMessagesRegistry = Private(GettingStartedTopMessagesRegistryProvider);
@@ -40,7 +33,7 @@ app.directive('gettingStarted', function ($injector) {
     bindToController: true,
     controllerAs: 'gettingStarted',
     controller: class GettingStartedController {
-      constructor($scope) {
+      constructor() {
         if (this.hasOptedOut()) {
           uiChrome.setVisible(true);
         } else {
@@ -59,11 +52,6 @@ app.directive('gettingStarted', function ($injector) {
         };
 
         this.documentationLinks = documentationLinks;
-
-        // Inject top messages and manage-and-monitor messages into page
-        this.appendMessagesToContainer(this.topMessages, 'gettingStartedTopMessagesContainer', $scope);
-        this.appendMessagesToContainer(this.manageAndMonitorMessages, 'gettingStartedManageAndMonitorMessagesContainer', $scope);
-
       }
 
       hasManageAndMonitorMessages = () => {
@@ -71,23 +59,6 @@ app.directive('gettingStarted', function ($injector) {
       }
 
       hasOptedOut = hasOptedOutOfGettingStarted;
-
-      optOut = optOutOfGettingStarted;
-
-      appendMessagesToContainer = (messages, containerName, $scope) => {
-        const container = document.getElementById(containerName);
-        if ((messages.length > 0) && Boolean(container)) {
-          messages.forEach(message => {
-            const paddedMessage = `${message}&nbsp;`;
-
-            // Compile messageHtml with current $scope and append it into the container DOM element.
-            // We do this because we want to dynamically inject content (strings) into the DOM. This content
-            // may contain Angular directives so it must first be $compiled with the current $scope.
-            const messageHtml = $compile(makeAngularParseableExpression(paddedMessage))($scope);
-            angular.element(container).append(messageHtml);
-          });
-        }
-      }
     }
   };
 });
--- src/core_plugins/testbed/public/index.js
+++ src/core_plugins/testbed/public/index.js
@@ -1,1 +1,21 @@
 import './testbed';
+
+
+import './components/blahblah';
+import { GettingStartedTopMessagesRegistryProvider } from 'ui/getting_started/top_messages_registry';
+import { GettingStartedMonitorAndManageMessagesRegistryProvider } from 'ui/getting_started/monitor_and_manage_messages_registry';
+
+GettingStartedTopMessagesRegistryProvider.register(() => ({
+  template: `<blahblah></blahblah>`
+}));
+GettingStartedTopMessagesRegistryProvider.register(() => ({
+  template: `Nam hendrerit augue id egestas ultricies.`
+}));
+
+GettingStartedMonitorAndManageMessagesRegistryProvider.register(() => ({
+  template: `Lorem ipsum dolor sit amet, consectetur <a href="http://www.google.com">consectetur</a> adipiscing elit.`
+}));
+
+GettingStartedMonitorAndManageMessagesRegistryProvider.register(() => ({
+  template: `Nam luctus mattis urna, ac <a href="http://www.google.com">fringilla</a> tellus efficitur at.`
+}));
