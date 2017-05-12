acf01b681cbe6a99d9f348c4619f3feb1bed016d
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-12 11:35:01 -0700

    Getting Started page (#11673)
    * Initial code
    
    * Renaming files/folders
    
    * Notes and changes from HTML/CSS review
    
    * CSS changes
    
    * Fixing transform units
    
    * Fleshing out logo circle
    
    * Adding logos
    
    * Un-nesting styles
    
    * Adding kuiText class on <p> elements
    
    * Making shield icon 404 go away
    
    * Using trustAsHtml filter; using registry
    
    * Adding code for registry
    
    * Adding images
    
    * Hooking up the opt-out link
    
    * Fixing linter issues
    
    * Fix issue with refresh bringing back nav
    
    * Removing load_default module
    
    * Use GETTING_STARTED_ROUTE constant
    
    * Allow topMessage to be a directive
    
    * Adding kuiLink CSS class to anchor elements
    
    * Adding alt text to images
    
    * Replace getters with regular methods
    
    * Remove leftover comment
    
    * Removing unnecessary comment
    
    * Fixing typo in comment
    
    * Adding comments for message properties
    
    * Fixing typo in method name
    
    * Defining custom CSS class
    
    * Fixing method call
    
    * Creating custom CSS classes
    
    * Adding documentation links
    
    * Use a instead of button for demo link
    
    * Allow directives in manage and monitor messages
    
    * Refactoring shared code into single ui/public/getting_started folder
    
    * Adding README
    
    * Fixing whitespace in/around links
    
    * Trim messages before setting them
    
    * Decorate in-app links with opt-out attribute
    
    * Opt out of the Getting Started page if the user already has index patterns
    
    * Adding more commentary around expectations of setTopMessage use
    
    * Using ng-src and ng-href attributes where attr values are angular expressions
    
    * Importing the directive before using it
    
    * Using ui/registry for message registries
    
    * Renaming service => helpers to clarify intent
    
    * Adding explanatory comment
    
    * Adding "or"
    
    * Fixing div id
    
    * Breaks out the inject logic into another directive
    
    * Adding Getting Started page object for functional tests
    
    * Consolidate into one registry
    
    * Fixes incorrect classes
    
    * Changes how styles are applied to injected-items items
    
    * Modifying class name per CSS style guide
    
    * Adding getting started doc link
    
    * Removing unused import

--- src/core_plugins/testbed/public/index.js
+++ src/core_plugins/testbed/public/index.js
@@ -1,1 +1,25 @@
 import './testbed';
+
+
+import './components/blahblah';
+import { GettingStartedRegistryProvider } from 'ui/getting_started/registry';
+import { GETTING_STARTED_REGISTRY_TYPES } from 'ui/getting_started/constants';
+
+GettingStartedRegistryProvider.register(() => ({
+  type: GETTING_STARTED_REGISTRY_TYPES.TOP_MESSAGE,
+  template: `<blahblah></blahblah>`
+}));
+GettingStartedRegistryProvider.register(() => ({
+  type: GETTING_STARTED_REGISTRY_TYPES.TOP_MESSAGE,
+  template: `Nam hendrerit augue id egestas ultricies.`
+}));
+
+GettingStartedRegistryProvider.register(() => ({
+  type: GETTING_STARTED_REGISTRY_TYPES.MANAGE_AND_MONITOR_MESSAGE,
+  template: `Lorem ipsum dolor sit amet, consectetur <a href="http://www.google.com">consectetur</a> adipiscing elit.`
+}));
+
+GettingStartedRegistryProvider.register(() => ({
+  type: GETTING_STARTED_REGISTRY_TYPES.MANAGE_AND_MONITOR_MESSAGE,
+  template: `Nam luctus mattis urna, ac <a href="http://www.google.com">fringilla</a> tellus efficitur at.`
+}));
