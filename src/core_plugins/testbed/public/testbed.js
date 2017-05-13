4f20a076900b1a5481e0756fd11da8032b750127
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-12 17:06:39 -0700

    Adding example usage on testbed

--- src/core_plugins/testbed/public/testbed.js
+++ src/core_plugins/testbed/public/testbed.js
@@ -2,6 +2,8 @@ import uiRoutes from 'ui/routes';
 import template from './testbed.html';
 import './testbed.less';
 
+import 'ui/loading_results';
+
 uiRoutes.when('/testbed', {
   template: template,
   controllerAs: 'testbed',
