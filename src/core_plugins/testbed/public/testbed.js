46c28069f92e3203423c76161288459ebd8f02e7
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-12 18:21:18 -0700

    Adding testbed example

--- src/core_plugins/testbed/public/testbed.js
+++ src/core_plugins/testbed/public/testbed.js
@@ -8,7 +8,34 @@ uiRoutes.when('/testbed', {
   template: template,
   controllerAs: 'testbed',
   controller: class TestbedController {
-    constructor() {
+    constructor($scope, $injector) {
+
+      const $filter = $injector.get('$filter');
+      this.orderBy = $filter('orderBy');
+
+      this.items = [
+        { firstName: 'Jim', lastName: 'Unger' },
+        { firstName: 'CJ', lastName: 'Cenizal' },
+        { firstName: 'Shaunak', lastName: 'Kashyap' }
+      ];
+
+      // Initial sort state for the table
+      this.sortField = 'lastName';
+      this.sortReverse = false;
+
+      $scope.$watchMulti([
+        'testbed.sortField',
+        'testbed.sortReverse'
+      ], this.calculateItems);
+    }
+
+    onSortChange = (field, reverse) => {
+      this.sortField = field;
+      this.sortReverse = reverse;
+    };
+
+    calculateItems = () => {
+      this.items = this.orderBy(this.items, this.sortField, this.sortReverse);
     }
   }
 });
