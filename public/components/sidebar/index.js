e80493f2446d238d99e56e6496ec4d7b88d20433
Joe Fleming <joe.fleming@gmail.com>
2017-05-12 17:34:18 -0700

    add sidebar component

---
+++ public/components/sidebar/index.js
@@ -0,0 +1,4 @@
+import { pure } from 'recompose';
+import { Sidebar as Component } from './sidebar';
+
+export const Sidebar = pure(Component);
---
+++ public/components/sidebar/sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ArgTypes } from '../arg_types';
+
+export const Sidebar = () => (
+  <div>
+    <h3>Configure Chart</h3>
+    <ArgTypes />
+  </div>
+);
