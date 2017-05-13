ec1f55d4a08e1ff578badab627924d5295708521
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-12 18:10:22 -0700

    Adding sortable-column component

---
+++ src/ui/public/sortable_column/sortable_column.js
@@ -0,0 +1,38 @@
+import { uiModules } from 'ui/modules';
+import template from './sortable_column.html';
+import './sortable_column.less';
+
+const app = uiModules.get('kibana');
+
+app.directive('sortableColumn', function () {
+  return {
+    restrict: 'E',
+    transclude: true,
+    template: template,
+    scope: {
+      field: '@',
+      sortField: '=',
+      sortReverse: '=',
+      onSortChange: '=',
+    },
+    controllerAs: 'sortableColumn',
+    bindToController: true,
+    controller: class SortableColumnController {
+      toggle = () => {
+        if (this.sortField === this.field) {
+          this.onSortChange(this.field, !this.sortReverse);
+        } else {
+          this.onSortChange(this.field, false);
+        }
+      }
+
+      get isSortedAscending() {
+        return (this.sortField === this.field) && (!this.sortReverse);
+      }
+
+      get isSortedDescending() {
+        return (this.sortField === this.field) && (this.sortReverse);
+      }
+    }
+  };
+});
