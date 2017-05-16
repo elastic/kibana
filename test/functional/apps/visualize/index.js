96414778ff99bf98b9df1c052c2c7ab5ce8b1a2f
Stéphane Campinas <stephane.campinas@gmail.com>
2017-05-16 14:47:46 +0100

    disable save button if visualization is dirty (#11576)
    (backport: manual merge edits in test file)

--- test/functional/apps/visualize/index.js
+++ test/functional/apps/visualize/index.js
@@ -21,6 +21,8 @@ export default function ({ getService, loadTestFile }) {
         return logstash;
       });
     });
+
+    loadTestFile(require.resolve('./_editor'));
     loadTestFile(require.resolve('./_chart_types'));
     loadTestFile(require.resolve('./_area_chart'));
     loadTestFile(require.resolve('./_line_chart'));
