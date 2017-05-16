8b5ed899e28483cb7593a354f114f0d4ec9fe29e
Nathan Reese <reese.nathan@gmail.com>
2017-05-16 13:21:13 -0600

    build phrase filter when control changes

--- src/core_plugins/control_visualizations/public/terms_vis/components/terms_vis.js
+++ src/core_plugins/control_visualizations/public/terms_vis/components/terms_vis.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
 
 export class TermsVis extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(control, evt) {
+    this.props.setFilter(control.field, evt.value, control.indexPattern);
+  }
+
   render() {
     return (
       <div>
-        {this.props.visParams.fields.map((field, index) =>
+        {this.props.controls.map((control, index) =>
           <div key={index}>
-            <h1>{field.label}</h1>
-            <div>{field.indexPattern}</div>
-            <div>{field.fieldName}</div>
+            <span>{control.label}</span>
+            <Select
+              placeholder="Select..."
+              value={control.selected}
+              options={control.terms}
+              onChange={this.handleOnChange.bind(null, control)}/>
           </div>
         )}
       </div>
--- src/core_plugins/control_visualizations/public/terms_vis/vis_controller.js
+++ src/core_plugins/control_visualizations/public/terms_vis/vis_controller.js
@@ -1,11 +1,15 @@
+import _ from 'lodash';
 import { uiModules } from 'ui/modules';
 import { TermsVis } from './components/terms_vis';
 import { FetchProvider } from 'ui/courier/fetch/fetch';
 import { SearchSourceProvider } from 'ui/courier/data_source/search_source';
+import { FilterBarQueryFilterProvider } from 'ui/filter_bar/query_filter';
+import { buildPhraseFilter } from 'ui/filter_manager/lib/phrase';
 
 const module = uiModules.get('kibana/terms_vis', ['kibana', 'react']);
 module.controller('KbnTermsController', function ($scope, indexPatterns, Private, Promise) {
   const fetch = Private(FetchProvider);
+  const queryFilter = Private(FilterBarQueryFilterProvider);
   const SearchSource = Private(SearchSourceProvider);
 
   const termsAgg = (fieldName, size, direction) => ({
@@ -20,14 +24,24 @@ module.controller('KbnTermsController', function ($scope, indexPatterns, Private
     }
   });
 
+  const findFilter = (indexPatternId, fieldName) => {
+    return _.flatten([queryFilter.getAppFilters(), queryFilter.getGlobalFilters()]).filter(function (filter) {
+      return filter.meta && filter.meta.index === indexPatternId && filter.meta.key === fieldName;
+    });
+  };
+
   $scope.reactProps = {
-    visParams: $scope.vis.params
+    controls: [],
+    setFilter: (field, value, indexPattern) => {
+      const filter = buildPhraseFilter(field, value, indexPattern);
+      queryFilter.addFilters(filter);
+    }
   };
 
   $scope.$watch('vis.params', function (visParams) {
-    $scope.reactProps.visParams = visParams;
+    $scope.reactProps.controls = [];
 
-    const createRequestPromises = $scope.vis.params.fields.map(async (field) => {
+    const createRequestPromises = visParams.fields.map(async (field) => {
       const indexPattern = await indexPatterns.get(field.indexPattern);
       const searchSource = new SearchSource();
       searchSource.inherits(false); //Do not filter by time so can not inherit from rootSearchSource
@@ -37,15 +51,32 @@ module.controller('KbnTermsController', function ($scope, indexPatterns, Private
 
       const defer = Promise.defer();
       defer.promise.then(function (resp) {
-        const terms = resp.aggregations.termsAgg.buckets.map((bucket) => {
-          return bucket.key;
+        const terms = _.get(resp, 'aggregations.termsAgg.buckets', []).map((bucket) => {
+          return { label: bucket.key, value: bucket.key };
         });
-        console.log('terms', terms);
+        $scope.reactProps.controls.push({
+          indexPattern: indexPattern,
+          field: indexPattern.fields.byName[field.fieldName],
+          label: field.label ? field.label : field.fieldName,
+          terms: terms
+        });
       });
       return searchSource._createRequest(defer);
     });
     Promise.all(createRequestPromises).then(requests => {
       fetch.these(requests);
+    });
+  });
+
+  queryFilter.on('update', function () {
+    $scope.reactProps.controls = $scope.reactProps.controls.map((control) => {
+      const filters = findFilter(control.indexPattern.id, control.field.name);
+      if (filters.length === 0) {
+        control.selected = '';
+      } else {
+        control.selected = filters[0].meta.value;
+      }
+      return control;
     });
   });
 });
