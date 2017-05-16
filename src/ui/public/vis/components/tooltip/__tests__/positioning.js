621a3e2dc66895f8ae89c59b5fc3189578b9d2b6
Nathan Reese <reese.nathan@gmail.com>
2017-05-16 07:32:38 -0600

    Tooltip gets cutoff when tooltip is larger than chart container (#11769)
    * Tooltip overflows both sides of smaller container
    
    * add tests
    
    * clean up comments
    
    * better clarity in test case
    
    * add space at start of comment

--- src/ui/public/vis/components/tooltip/__tests__/positioning.js
+++ src/ui/public/vis/components/tooltip/__tests__/positioning.js
@@ -7,7 +7,7 @@ import { positionTooltip } from '../position_tooltip';
 describe('Tooltip Positioning', function () {
 
   const positions = ['north', 'south', 'east', 'west'];
-  const bounds = ['top', 'left', 'bottom', 'right'];
+  const bounds = ['top', 'left', 'bottom', 'right', 'area'];
   let $window;
   let $chart;
   let $tooltip;
@@ -146,6 +146,31 @@ describe('Tooltip Positioning', function () {
           expect(overflow[p]).to.be.lessThan(0);
         }
       });
+    });
+
+    it('identifies only right overflow when tooltip overflows both sides of inner container but outer contains tooltip', function () {
+      // Size $tooltip larger than chart
+      const largeWidth = $chart.width() + 10;
+      $tooltip.css({ width: largeWidth });
+      $sizer.css({ width: largeWidth });
+      const size = positionTooltip.getTtSize($tooltip.html(), $sizer);
+      expect(size).to.have.property('width', largeWidth);
+
+      // $chart is flush with the $window on the left side
+      expect(positionTooltip.getBounds($chart).left).to.be(0);
+
+      // Size $window large enough for tooltip on right side
+      $window.css({ width: $chart.width() * 3 });
+
+      // Position click event in center of $chart so $tooltip overflows both sides of chart
+      const pos = positionTooltip.getBasePosition(size, makeEvent(0.5, 0.5));
+
+      const overflow = positionTooltip.getOverflow(size, pos, [$chart, $window]);
+
+      // no overflow on left (east)
+      expect(overflow.east).to.be.lessThan(0);
+      // overflow on right (west)
+      expect(overflow.west).to.be.greaterThan(0);
     });
   });
 
