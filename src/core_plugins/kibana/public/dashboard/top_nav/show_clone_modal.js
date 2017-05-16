3bbd73b612e1975bb57da2f4b977cf12279fe335
Stacey Gammon <gammon@elastic.co>
2017-05-16 10:01:37 -0400

    Append 'Copy' to the title in the clone dialog so by default it doesn't clash

--- src/core_plugins/kibana/public/dashboard/top_nav/show_clone_modal.js
+++ src/core_plugins/kibana/public/dashboard/top_nav/show_clone_modal.js
@@ -17,7 +17,7 @@ export function showCloneModal(onClone, title) {
   };
   document.body.appendChild(container);
   const element = (
-    <DashboardCloneModal onClone={onCloneConfirmed} onClose={closeModal} title={title}></DashboardCloneModal>
+    <DashboardCloneModal onClone={onCloneConfirmed} onClose={closeModal} title={title + ' Copy'}></DashboardCloneModal>
   );
   ReactDOM.render(element, container);
 }
