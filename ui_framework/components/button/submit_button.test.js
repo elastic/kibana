5ad8aa5a8898940ddee592f7d3811ff49334c5ee
Nathan Reese <reese.nathan@gmail.com>
2017-05-17 16:00:58 -0600

    remove type=submit from KuiSubmitButton since component automatically sets type

--- ui_framework/components/button/submit_button.test.js
+++ ui_framework/components/button/submit_button.test.js
@@ -24,7 +24,6 @@ describe('KuiSubmitButton', () => {
           aria-label="aria label"
           className="testClass1 testClass2"
           data-test-subj="test subject string"
-          type="submit"
           disabled
         />
       );
