4bdc458926ed732d3507ecbfaca876cf8f19a72c
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-17 07:59:13 -0700

    Adding unit tests for handleGettingStartedOptedOutScenario function

--- src/core_plugins/getting_started/public/lib/__tests__/add_setup_work.js
+++ src/core_plugins/getting_started/public/lib/__tests__/add_setup_work.js
@@ -1,22 +1,27 @@
 import expect from 'expect.js';
 import sinon from 'sinon';
+import uiChrome from 'ui/chrome';
 
 import {
-  showGettingStartedPage
+  showGettingStartedPage,
+  handleGettingStartedOptedOutScenario
 } from '../add_setup_work';
-import {  GETTING_STARTED_ROUTE } from '../constants';
+import {
+  GETTING_STARTED_ROUTE,
+  CREATE_INDEX_PATTERN_ROUTE
+} from '../constants';
 
 describe('Getting Started page', () => {
   describe('add_setup_work', () => {
-    describe('showGettingStartedPage', () => {
-      let spyKbnUrl;
-      let isOnGettingstartedPage;
+    let spyKbnUrl;
+    beforeEach(() => {
+      spyKbnUrl = {
+        change: sinon.spy()
+      };
+    });
 
-      beforeEach(() => {
-        spyKbnUrl = {
-          change: sinon.spy()
-        };
-      });
+    describe('showGettingStartedPage', () => {
+      let isOnGettingstartedPage;
 
       describe('user is not already on Getting Started page', () => {
         beforeEach(() => {
@@ -37,6 +42,40 @@ describe('Getting Started page', () => {
         it ('redirects the user to the Getting Started page', () => {
           showGettingStartedPage(spyKbnUrl, isOnGettingstartedPage);
           expect(spyKbnUrl.change.called).to.be(false);
+        });
+      });
+    });
+
+    describe('handleGettingStartedOptedOutScenario', () => {
+      let currentRoute;
+      beforeEach(() => {
+        currentRoute = {};
+      });
+
+      it ('sets the chrome to visible', () => {
+        handleGettingStartedOptedOutScenario(currentRoute, spyKbnUrl);
+        expect(uiChrome.getVisible()).to.be(true);
+      });
+
+      describe('current route does not require a default index pattern', () => {
+        beforeEach(() => {
+          currentRoute.requireDefaultIndex = false;
+        });
+
+        it ('returns without redirecting the user', () => {
+          handleGettingStartedOptedOutScenario(currentRoute, spyKbnUrl);
+          expect(spyKbnUrl.change.called).to.be(false);
+        });
+      });
+
+      describe('current route requires a default index pattern', () => {
+        beforeEach(() => {
+          currentRoute.requireDefaultIndex = true;
+        });
+
+        it ('redirects the user to the Create Index Pattern page', () => {
+          handleGettingStartedOptedOutScenario(currentRoute, spyKbnUrl);
+          expect(spyKbnUrl.change.calledWith(CREATE_INDEX_PATTERN_ROUTE)).to.be(true);
         });
       });
     });
