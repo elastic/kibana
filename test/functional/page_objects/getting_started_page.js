d56e3e03f3524db675d70e8f604dae7340b49b8d
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-17 05:18:58 -0700

    Adding some more functionality to the Getting Started page object

--- test/functional/page_objects/getting_started_page.js
+++ test/functional/page_objects/getting_started_page.js
@@ -1,8 +1,10 @@
-export function GettingStartedPageProvider({ getService }) {
+export function GettingStartedPageProvider({ getService, getPageObjects }) {
 
   const log = getService('log');
   const retry = getService('retry');
   const testSubjects = getService('testSubjects');
+
+  const PageObjects = getPageObjects(['common']);
 
   class GettingStartedPage {
     async doesContainerExist() {
@@ -17,6 +19,21 @@ export function GettingStartedPageProvider({ getService }) {
           throw new Error('Still on getting started page');
         }
       });
+    }
+
+    async optOutLinkExists() {
+      return await testSubjects.exists('lnkGettingStartedOptOut');
+    }
+
+    async navigateTo() {
+      log.debug('Navigating directly to Getting Started page');
+      await PageObjects.common.navigateToUrl('settings', 'kibana/getting_started');
+    }
+
+    async optOut() {
+      log.debug('Opting out of Getting Started page');
+      await this.navigateTo();
+      await this.clickOptOutLink();
     }
   }
 
