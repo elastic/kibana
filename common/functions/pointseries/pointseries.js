690ee87e5ad164b33d92dbe9d72d291c18e91a6d
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-17 16:27:09 -0700

    Do not support passing in functions, only mathJS expressions

--- common/functions/pointseries/pointseries.js
+++ common/functions/pointseries/pointseries.js
@@ -1,6 +1,9 @@
 import Fn from '../fn.js';
+import { findInObject } from '../../lib/find_in_object';
+import math from 'mathjs';
 import moment from 'moment';
-import { groupBy, zipObject, uniqBy, omit, pickBy, find } from 'lodash';
+import { groupBy, zipObject, uniqBy, omit, pickBy, find, uniq, map, mapValues } from 'lodash';
+import { getMathjsScope } from '../math/get_mathjs_scope';
 
 module.exports = new Fn({
   name: 'pointseries',
@@ -13,42 +16,68 @@ module.exports = new Fn({
   args: {
     // Dimensions
     x: {
-      types: ['string', 'function', 'null'],
+      types: ['string', 'null'],
       help: 'A mathmatic expression that returns the values for a dimension or measure',
     },
     color: {
-      types: ['string', 'function', 'null'],
+      types: ['string', 'null'],
       help: 'A string representing the column name or a function that returns a number', // If you need categorization, transform the field.
     },
     // Metrics
     y: {
-      types: ['string', 'function', 'null'],
+      types: ['string', 'null'],
       help: 'A static number, or a function (starts with a .) to use to aggregate when there are ' +
       'several points for the same x-axis value',
     },
     // Size only makes sense as a number
     size: {
-      types: ['string', 'function', 'null'], // pointseries(size=.sum(profit))
+      types: ['string', 'null'], // pointseries(size=.sum(profit))
       help: 'For use in charts that support it, a function that returns a number, eg .math() to use for calculating size',
     },
     // In the future it may make sense to add things like shape, or tooltip values, but I think what we have is good for now
   },
   fn: (context, args) => {
-    function getType(arg) {
-      return typeof arg === 'string' ? find(context.columns, { name: arg }).type : 'number';
+    const mathScope = getMathjsScope(context);
+
+    function getFieldType(field) {
+      if (!field) return 'null';
+      return find(context.columns, { name: field }).type;
     }
 
-    const columns = {
-      // It seems reasonable that all dimensions would be strings and all measures would be numbers, right?
-      color: { type: getType(args.color) }, // This will always be a string since it is effectly the label
-      size: { type: getType(args.size) }, // This will always be a number
-      y: { type: getType(args.y) }, // We could probably make this dynamic if we wanted to be able todo vertical & horizontal
-      x: { type: getType(args.x) }, // This will determine if the renderer uses ordinal, nominal or temporal scale
-    };
+    function getType(mathExpression) {
+      if (isColumnReference(mathExpression)) return getFieldType(mathExpression);
 
-    // TODO: break this out into a utility
-    function normalizeValue(columnName, value) {
-      switch (columns[columnName].type) {
+      const parsedMath = math.parse(mathExpression);
+      const symbolNames = map(findInObject(parsedMath, (val, name) => val.type === 'SymbolNode' && name !== 'fn'), 'name');
+      const symbolTypes = uniq(symbolNames.map(getFieldType));
+      return (symbolTypes.length === 1) ? symbolTypes[0] : 'string';
+    }
+
+    function isColumnReference(mathExpression) {
+      const parsedMath = math.parse(mathExpression);
+      if (parsedMath.type === 'SymbolNode') return true;
+    }
+
+    function isMeasure(mathExpression) {
+      if (isColumnReference(mathExpression)) return false;
+      const parsedMath = math.parse(mathExpression);
+      if (parsedMath.type !== 'FunctionNode') {
+        throw new Error ('Expressions must be wrapped in a function such as sum()');
+      }
+
+      // This will throw if the field isn't found on scope.
+      const evaluated = math.eval(mathExpression, mathScope);
+      if (typeof evaluated !== 'number') return false;
+
+      return true;
+    }
+
+    const columns = mapValues(args, arg => {
+      return { type: getType(arg) };
+    });
+
+    function normalizeValue(expression, value) {
+      switch (getType(expression)) {
         case 'string':
           return String(value);
         case 'number':
@@ -61,50 +90,43 @@ module.exports = new Fn({
     }
 
     // Dimensions. There's probably a better way todo this
-    const dimensionNames = Object.keys(pickBy(args, val => typeof val === 'string'));
-    const result = context.rows.map(row => {
+    // const dimensionNames = Object.keys(pickBy(args, val => typeof val === 'string'));
+    const dimensionNames = Object.keys(pickBy(args, val => !isMeasure(val)));
+    const result = context.rows.map((row, i) => {
       const newRow = { _rowId: row._rowId };
       dimensionNames.forEach(dimension =>
-        newRow[dimension] = args[dimension] ? normalizeValue(dimension, row[args[dimension]]) : '_all');
+        newRow[dimension] = args[dimension] ? normalizeValue(args[dimension], math.eval(args[dimension], mathScope)[i]) : '_all');
       return newRow;
     });
 
     // Measures
     // First group up all of the distinct dimensioned bits. Each of these will be reduced to just 1 value
     // for each measure
-    const measureNames = Object.keys(pickBy(args, val => typeof val === 'function'));
+    const measureNames = Object.keys(pickBy(args, val => isMeasure(val)));
     const measureDimensions = groupBy(context.rows, (row) => {
       const dimensions = dimensionNames.map(dimension => args[dimension] ? row[args[dimension]] : '_all');
       return dimensions.join('::%BURLAP%::');
     });
 
     // Then compute that 1 value for each measure
-    const rowPromises = [];
     Object.values(measureDimensions).forEach(rows => {
       const subtable = { type: 'datatable', columns: context.columns, rows: rows };
-      const measurePromises = measureNames.map(measure =>
-        typeof args[measure] === 'function' ? args[measure](subtable) : Promise.resolve(args[measure]));
+      const subScope = getMathjsScope(subtable);
+      const measureValues = measureNames.map(measure => math.eval(args[measure], subScope));
 
       rows.forEach(row => {
-        rowPromises[row._rowId] = Promise.all(measurePromises).then(measureValues => zipObject(measureNames, measureValues));
+        Object.assign(result[row._rowId], zipObject(measureNames, measureValues));
       });
     });
 
-    return Promise.all(rowPromises).then(measureRows => {
-      result.forEach((row, i) => {
-        Object.assign(row, measureRows[i]);
-      });
+    // It only makes sense to uniq the rows in a point series as 2 values can not exist in the exact same place at the same time.
+    const rows = uniqBy(result, row => JSON.stringify(omit(row, '_rowId')));
+    console.log(rows.length);
 
-      const rows = uniqBy(result, row => JSON.stringify(omit(row, '_rowId')));
-      console.log(rows.length);
-
-      return {
-        type: 'pointseries',
-        columns: columns,
-        //rows: sortBy(result, ['x']),
-        // It only makes sense to uniq the rows in a point series as 2 values can not exist in the exact same place at the same time.
-        rows: rows,
-      };
-    });
+    return {
+      type: 'pointseries',
+      columns: columns,
+      rows: rows,
+    };
   },
 });
