b82612a8f22ad0fe480ab36a14e1b6f3d96c5030
spalger <spalger@users.noreply.github.com>
2017-05-18 08:10:47 -0700

    [ui/routes] rename reference to WAIT_FOR_URL_CHANGE_TOKEN

--- src/ui/public/routes/routes.js
+++ src/ui/public/routes/routes.js
@@ -2,11 +2,11 @@ import RouteManager from './route_manager';
 import 'angular-route/angular-route';
 import { uiModules } from 'ui/modules';
 const defaultRouteManager = new RouteManager();
-import { ABORT_SETUP_WORK_TOKEN } from './route_setup_manager';
+import { WAIT_FOR_URL_CHANGE_TOKEN } from './route_setup_manager';
 
 module.exports = {
   ...defaultRouteManager,
-  ABORT_SETUP_WORK_TOKEN,
+  WAIT_FOR_URL_CHANGE_TOKEN,
   enable() {
     uiModules
     .get('kibana', ['ngRoute'])
