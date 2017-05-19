1b4a4dc3a43f0046e43f908cdd8bc6a320e56ce8
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-19 10:43:49 -0700

    Adding table_info component (#11778)
    * Adding loading-results component
    
    * Adding example usage on testbed
    
    * Fixing module name
    
    * Adding kuiControlledTable container
    
    * Replacing kuiNoItems and kuiLoadingResults with kuiTableInfo
    
    * Making directive compositional
    
    * Fixing imports
    
    * Re-generating fixed CSS

--- src/core_plugins/testbed/public/testbed.js
+++ src/core_plugins/testbed/public/testbed.js
@@ -2,6 +2,8 @@ import uiRoutes from 'ui/routes';
 import template from './testbed.html';
 import './testbed.less';
 
+import 'ui/table_info';
+
 uiRoutes.when('/testbed', {
   template: template,
   controllerAs: 'testbed',
