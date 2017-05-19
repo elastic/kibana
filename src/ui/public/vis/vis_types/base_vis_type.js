990a0cd9a810ae65a936b241f3bcff049d380e48
ppisljar <peter.pisljar@gmail.com>
2017-05-19 09:03:37 +0200

    base type should know as little as possible about editor (and nothing about schemas)

--- src/ui/public/vis/vis_types/base_vis_type.js
+++ src/ui/public/vis/vis_types/base_vis_type.js
@@ -1,23 +1,16 @@
-import { VisSchemasProvider } from '../editors/default/schemas';
 import { CATEGORY } from '../vis_category';
 import _ from 'lodash';
 
-
-export function VisTypeProvider(Private) {
-  const VisTypeSchemas = Private(VisSchemasProvider);
-
+export function VisTypeProvider() {
   class VisType {
     constructor(opts) {
       opts = opts || {};
 
-      //todo: clean this up!
-      if (opts.schemas && !opts.editorConfig) {
-        throw 'can\'t configure schemas without using default-editor';
-      }
-
-      if (opts.editorConfig && opts.schemas) {
-        throw 'should configure schemas on the editorConfig object, not the top-level options';
-      }
+      if (!opts.name) throw('vis_type must define its name');
+      if (!opts.title) throw('vis_type must define its title');
+      if (!opts.description) throw('vis_type must define its description');
+      if (!opts.icon && !opts.image) throw('vis_type must define its icon or image');
+      if (!opts.visualization) throw('vis_type must define visualization controller');
 
       const _defaults = {
         // name, title, description, icon, image
@@ -27,13 +20,10 @@ export function VisTypeProvider(Private) {
           defaults: {},            // default configuration
         },
         requestHandler: 'courier',    // select one from registry or pass a function
-        responseHandler: 'none',      // ...
+        responseHandler: 'none',
         editor: 'default',
         editorConfig: {
-          //optionTabs: {},          // default editor needs a list of option tabs
-          optionsTemplate: '',      // default editor needs an optionsTemplate if optionsTab is not provided
           collections: {},         // collections used for configuration (list of positions, ...)
-          schemas: new VisTypeSchemas()            // default editor needs a list of schemas ...
         },
         options: {                // controls the visualize editor
           showTimePicker: true,
@@ -44,27 +34,17 @@ export function VisTypeProvider(Private) {
         isExperimental: false
       };
 
-
-      //todo: validate options here, not later
       _.defaultsDeep(this, opts, _defaults);
-
-      //todo: move validation up to options object, not after we already create
-      if (!this.name) throw('vis_type must define its name');
-      if (!this.title) throw('vis_type must define its title');
-      if (!this.description) throw('vis_type must define its description');
-      if (!this.icon && !this.image) throw('vis_type must define its icon or image');
-      if (typeof this.visualization !== 'function') throw('vis_type must define visualization controller');
 
       if (!this.editorConfig.optionTabs) {
         this.editorConfig.optionTabs = [
-          { name: 'options', title: 'Options', editor: this.editorConfig.optionsTemplate }
+          { name: 'options', title: 'Options', editor: this.editorConfig.optionsTemplate || '' }
         ];
       }
 
       this.requiresSearch = !(this.requestHandler === 'none');
     }
   }
-
 
   Object.defineProperty(VisType.prototype, 'schemas', {
     get() {
