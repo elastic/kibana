0a9d9a837ff6715f8fb2a3a6c312c6c9c71b2591
Lukas Olson <olson.lukas@gmail.com>
2017-05-19 14:20:33 -0700

    Move params under operator type

--- src/ui/public/filter_editor/lib/filter_editor_utils.js
+++ src/ui/public/filter_editor/lib/filter_editor_utils.js
@@ -28,18 +28,20 @@ export function getOperatorFromFilter(filter) {
 
 export function getParamsFromFilter(filter) {
   const { type, key } = filter.meta;
+  let params;
   if (type === 'phrase') {
-    const value = filter.query ? filter.query.match[key].query : filter.script.script.params.value;
-    return { value };
+    params = filter.query ? filter.query.match[key].query : filter.script.script.params.value;
   } else if (type === 'phrases') {
-    return { values: filter.meta.values };
+    params = filter.meta.params;
   } else if (type === 'range') {
-    const params = filter.range ? filter.range[key] : filter.script.script.params;
-    const from = _.has(params, 'gte') ? params.gte : params.gt;
-    const to = _.has(params, 'lte') ? params.lte : params.lt;
-    return { from, to };
+    const range = filter.range ? filter.range[key] : filter.script.script.params;
+    const from = _.has(range, 'gte') ? range.gte : range.gt;
+    const to = _.has(range, 'lte') ? range.lte : range.lt;
+    params = { from, to };
   }
-  return {};
+  return {
+    [type]: params
+  };
 }
 
 export function getFieldOptions(indexPatterns) {
@@ -60,12 +62,12 @@ export function isFilterValid({ field, operator, params }) {
   if (!field || !operator) {
     return false;
   } else if (operator.type === 'phrase') {
-    return _.has(params, 'value') && params.value !== '';
+    return _.has(params, 'phrase') && params.phrase !== '';
   } else if (operator.type === 'phrases') {
-    return _.has(params, 'values') && params.values.length > 0;
+    return _.has(params, 'phrases') && params.phrases.length > 0;
   } else if (operator.type === 'range') {
-    const hasFrom = _.has(params, 'from') && params.from !== '';
-    const hasTo = _.has(params, 'to') && params.to !== '';
+    const hasFrom = _.has(params, ['range', 'from']) && params.range.from !== '';
+    const hasTo = _.has(params, ['range', 'to']) && params.range.to !== '';
     return hasFrom || hasTo;
   }
   return true;
@@ -74,11 +76,11 @@ export function isFilterValid({ field, operator, params }) {
 export function buildFilter({ field, operator, params }) {
   let filter;
   if (operator.type === 'phrase') {
-    filter = buildPhraseFilter(field, params.value, field.indexPattern);
+    filter = buildPhraseFilter(field, params.phrase, field.indexPattern);
   } else if (operator.type === 'phrases') {
-    filter = buildPhrasesFilter(field, params.values, field.indexPattern);
+    filter = buildPhrasesFilter(field, params.phrases, field.indexPattern);
   } else if (operator.type === 'range') {
-    filter = buildRangeFilter(field, { gte: params.from, lt: params.to }, field.indexPattern);
+    filter = buildRangeFilter(field, { gte: params.range.from, lt: params.range.to }, field.indexPattern);
   } else if (operator.type === 'exists') {
     filter = buildExistsFilter(field, field.indexPattern);
   }
--- src/ui/public/filter_manager/lib/phrases.js
+++ src/ui/public/filter_manager/lib/phrases.js
@@ -1,24 +1,24 @@
 import { getPhraseScript } from './phrase';
 
-export function buildPhrasesFilter(field, values, indexPattern) {
+export function buildPhrasesFilter(field, params, indexPattern) {
   const index = indexPattern.id;
   const type = 'phrases';
   const key = field.name;
-  const value = values
+  const value = params
     .map(value => field.format.convert(value))
     .join(', ');
 
   const filter = {
-    meta: { index, type, key, value, values }
+    meta: { index, type, key, value, params }
   };
 
   let should;
   if (field.scripted) {
-    should = values.map((value) => ({
+    should = params.map((value) => ({
       script: getPhraseScript(field, value)
     }));
   } else {
-    should = values.map((value) => ({
+    should = params.map((value) => ({
       match_phrase: {
         [field.name]: value
       }
