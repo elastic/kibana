c9861035b44cce1717e4100df98ba0b84ff2244b
ppisljar <peter.pisljar@gmail.com>
2017-05-22 19:24:21 +0200

    fixing default editor

--- src/ui/public/vis/editors/default/default.js
+++ src/ui/public/vis/editors/default/default.js
@@ -25,45 +25,49 @@ const defaultEditor = function ($rootScope, $compile) {
         $scope.searchSource = searchSource;
       };
 
-      if (!this.$scope) {
-        this.$scope = $scope = $rootScope.$new();
+      return new Promise(resolve => {
+        if (!this.$scope) {
+          this.$scope = $scope = $rootScope.$new();
 
-        updateScope();
+          updateScope();
 
-        // track state of editable vis vs. "actual" vis
-        $scope.stageEditableVis = () => {
-          $scope.vis.updateState();
-          $scope.vis.dirty = false;
-        };
-        $scope.resetEditableVis = () => {
-          $scope.vis.resetState();
-          $scope.vis.dirty = false;
-        };
+          // track state of editable vis vs. "actual" vis
+          $scope.stageEditableVis = () => {
+            $scope.vis.updateState();
+            $scope.vis.dirty = false;
+          };
+          $scope.resetEditableVis = () => {
+            $scope.vis.resetState();
+            $scope.vis.dirty = false;
+          };
 
-        $scope.$watch(function () {
-          return $scope.vis.getCurrentState(false);
-        }, function (newState) {
-          $scope.vis.dirty = !angular.equals(newState, $scope.vis.getEnabledState());
+          $scope.$watch(function () {
+            return $scope.vis.getCurrentState(false);
+          }, function (newState) {
+            $scope.vis.dirty = !angular.equals(newState, $scope.vis.getEnabledState());
 
-          $scope.responseValueAggs = null;
-          try {
-            $scope.responseValueAggs = $scope.vis.aggs.getResponseAggs().filter(function (agg) {
-              return _.get(agg, 'schema.group') === 'metrics';
-            });
-          }
-            // this can fail when the agg.type is changed but the
-            // params have not been set yet. watcher will trigger again
-            // when the params update
-          catch (e) {} // eslint-disable-line no-empty
-        }, true);
+            $scope.responseValueAggs = null;
+            try {
+              $scope.responseValueAggs = $scope.vis.aggs.getResponseAggs().filter(function (agg) {
+                return _.get(agg, 'schema.group') === 'metrics';
+              });
+            }
+              // this can fail when the agg.type is changed but the
+              // params have not been set yet. watcher will trigger again
+              // when the params update
+            catch (e) {} // eslint-disable-line no-empty
+          }, true);
 
-        this.el.html($compile(defaultEditorTemplate)($scope));
-      } else {
-        $scope = this.$scope;
-        updateScope();
-      }
+          this.el.html($compile(defaultEditorTemplate)($scope));
+        } else {
+          $scope = this.$scope;
+          updateScope();
+        }
 
-      $scope.$broadcast('render');
+        $scope.$broadcast('render');
+
+        resolve(true);
+      });
     }
 
     resize() {
