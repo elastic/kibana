c4b3ade27d8be5b34daf47a0d1c64fb6b87c12c6
Shaunak Kashyap <ycombinator@gmail.com>
2017-05-23 11:53:32 -0700

    Prevent flicker on Getting Started page (#11826)
    * Consolide chrome visibility toggle code into single module
    
    * Extracting code into helper functions for better readability
    
    * Removing redundant setting
    
    * Extracting functions to higher level in scope so they are not defined multiple times
    
    * Fixing typo in comment
    
    * Starting to add unit tests
    
    * Actually adding the tests this time
    
    * Adding unit tests for handleGettingStartedOptedOutScenario function
    
    * Binding this to window to prevent Uncaught TypeError: Illegal invocation
    
    * Adding unit tests for handleExistingIndexPatternsScenario function
    
    * Add helper function to aid unit testing
    
    * Refactor unit tests
    
    * Inlining function to prevent naming awkwardness
    
    * Binding this to window to remove ambiguity
    
    * Adding missing import
    
    * Fixing import
    
    * Catching expected error

--- src/core_plugins/getting_started/public/lib/add_setup_work.js
+++ src/core_plugins/getting_started/public/lib/add_setup_work.js
@@ -3,6 +3,7 @@ import uiRoutes, { WAIT_FOR_URL_CHANGE_TOKEN } from 'ui/routes';
 import uiChrome from 'ui/chrome';
 import { Notifier } from 'ui/notify/notifier';
 import { IndexPatternsGetIdsProvider } from 'ui/index_patterns/_get_ids';
+import KbnUrlProvider from 'ui/url';
 import { hasOptedOutOfGettingStarted, optOutOfGettingStarted } from 'ui/getting_started/opt_out_helpers';
 
 import {
@@ -10,71 +11,94 @@ import {
   CREATE_INDEX_PATTERN_ROUTE
 } from './constants';
 
-uiRoutes
-  .addSetupWork(function gettingStartedGateCheck(Private, $injector) {
-    const getIds = Private(IndexPatternsGetIdsProvider);
-    const config = $injector.get('config');
-    const kbnUrl = $injector.get('kbnUrl');
-    const $route = $injector.get('$route');
+function handleExistingIndexPatternsScenario(indexPatterns, currentRoute, config) {
+  // If index patterns exist, we're not going to show the user the Getting Started page.
+  // So we can show the chrome again at this point.
+  uiChrome.setVisible(true);
 
-    const currentRoute = get($route, 'current.$$route');
+  // The user need not see the Getting Started page, so opt them out of it
+  optOutOfGettingStarted();
 
-    return getIds()
-    .then(indexPatterns => {
-      const indexPatternsExist = Array.isArray(indexPatterns) && indexPatterns.length > 0;
-      const isOnGettingStartedPage = get(currentRoute, 'originalPath') === GETTING_STARTED_ROUTE;
+  // Some routes require a default index pattern to be present. If we're
+  // NOT on such a route, there's nothing more to do; send the user on their way
+  if (!currentRoute.requireDefaultIndex) {
+    return;
+  }
 
-      if (indexPatternsExist) {
+  // Otherwise, check if we have a default index pattern
+  let defaultIndexPattern = config.get('defaultIndex');
 
-        // The user need not see the Getting Started page, so opt them out of it
-        optOutOfGettingStarted();
+  // If we don't have an default index pattern, make the first index pattern the
+  // default one
+  if (!Boolean(defaultIndexPattern)) {
+    defaultIndexPattern = indexPatterns[0];
+    config.set('defaultIndex', defaultIndexPattern);
+  }
 
-        // Some routes require a default index pattern to be present. If we're
-        // NOT on such a route, there's nothing more to do; send the user on their way
-        if (!currentRoute.requireDefaultIndex) {
-          return;
-        }
+  // At this point, we have a default index pattern and are all set!
+  return;
+}
 
-        // Otherwise, check if we have a default index pattern
-        let defaultIndexPattern = config.get('defaultIndex');
+function handleGettingStartedOptedOutScenario(currentRoute, kbnUrl) {
+  // If the user has opted out of the Getting Started page, we're not going to show them that page.
+  // So we can show the chrome again at this point.
+  uiChrome.setVisible(true);
 
-        // If we don't have an default index pattern, make the first index pattern the
-        // default one
-        if (!Boolean(defaultIndexPattern)) {
-          defaultIndexPattern = indexPatterns[0];
-          config.set('defaultIndex', defaultIndexPattern);
-        }
+  // Some routes require a default index pattern to be present. If we're
+  // NOT on such a route, there's nothing more to do; send the user on their way
+  if (!currentRoute.requireDefaultIndex) {
+    return;
+  }
 
-        // At this point, we have a default index pattern and are all set!
-        return;
-      }
-
-      // At this point, no index patterns exist.
-
-      // If the user has explicitly opted out of the Getting Started page
-      if (hasOptedOutOfGettingStarted()) {
-
-        // Some routes require a default index pattern to be present. If we're
-        // NOT on such a route, there's nothing more to do; send the user on their way
-        if (!currentRoute.requireDefaultIndex) {
-          return;
-        }
-
-        // Otherwise, redirect the user to the index pattern creation page with
-        // a notification about creating an index pattern
-        const notify = new Notifier({
-          location: 'Index Patterns'
-        });
-        notify.error('Please create a new index pattern');
-        kbnUrl.change(CREATE_INDEX_PATTERN_ROUTE);
-        throw WAIT_FOR_URL_CHANGE_TOKEN;
-      }
-
-      // Redirect the user to the Getting Started page (unless they are on it already)
-      if (!isOnGettingStartedPage) {
-        uiChrome.setVisible(false);
-        kbnUrl.change(GETTING_STARTED_ROUTE);
-        throw WAIT_FOR_URL_CHANGE_TOKEN;
-      }
-    });
+  // Otherwise, redirect the user to the index pattern creation page with
+  // a notification about creating an index pattern
+  const notify = new Notifier({
+    location: 'Index Patterns'
   });
+  notify.error('Please create a new index pattern');
+
+  kbnUrl.change(CREATE_INDEX_PATTERN_ROUTE);
+  throw WAIT_FOR_URL_CHANGE_TOKEN;
+}
+
+function showGettingStartedPage(kbnUrl, isOnGettingStartedPage) {
+  // Redirect the user to the Getting Started page (unless they are on it already)
+  if (!isOnGettingStartedPage) {
+    kbnUrl.change(GETTING_STARTED_ROUTE);
+    throw WAIT_FOR_URL_CHANGE_TOKEN;
+  }
+}
+
+/*
+ * This function is exported for unit testing
+ */
+export function gettingStartedGateCheck(getIds, kbnUrl, config, $route) {
+  const currentRoute = get($route, 'current.$$route');
+  const isOnGettingStartedPage = get(currentRoute, 'originalPath') === GETTING_STARTED_ROUTE;
+
+  return getIds()
+  .then(indexPatterns => {
+    const indexPatternsExist = Array.isArray(indexPatterns) && indexPatterns.length > 0;
+
+    if (indexPatternsExist) {
+      return handleExistingIndexPatternsScenario(indexPatterns, currentRoute, config);
+    }
+
+    if (hasOptedOutOfGettingStarted()) {
+      return handleGettingStartedOptedOutScenario(currentRoute, kbnUrl);
+    }
+
+    return showGettingStartedPage(kbnUrl, isOnGettingStartedPage);
+  });
+}
+
+// Start out with the chrome not being shown to prevent a flicker by
+// hiding it later
+uiChrome.setVisible(false);
+uiRoutes.addSetupWork((Private, $injector) => {
+  const getIds = Private(IndexPatternsGetIdsProvider);
+  const kbnUrl = Private(KbnUrlProvider);
+  const config = $injector.get('config');
+  const $route = $injector.get('$route');
+  return gettingStartedGateCheck(getIds, kbnUrl, config, $route);
+});
