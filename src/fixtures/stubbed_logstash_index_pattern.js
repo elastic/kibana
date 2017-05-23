a6e90e4cd6bcf7a693bf77cd32037a6829f5fe15
spalger <spalger@users.noreply.github.com>
2017-05-23 16:17:53 -0700

    [test/stubbedLogstashIndexPattern] fix kbnFieldType use

--- src/fixtures/stubbed_logstash_index_pattern.js
+++ src/fixtures/stubbed_logstash_index_pattern.js
@@ -1,13 +1,18 @@
 import TestUtilsStubIndexPatternProvider from 'test_utils/stub_index_pattern';
 import FixturesLogstashFieldsProvider from 'fixtures/logstash_fields';
-import { castEsToKbnFieldType } from '../utils';
+import { getKbnFieldType } from '../utils';
 
 export default function stubbedLogstashIndexPatternService(Private) {
   const StubIndexPattern = Private(TestUtilsStubIndexPatternProvider);
   const mockLogstashFields = Private(FixturesLogstashFieldsProvider);
 
   const fields = mockLogstashFields.map(function (field) {
-    const kbnType = castEsToKbnFieldType(field.type);
+    const kbnType = getKbnFieldType(field.type);
+
+    if (kbnType.name === 'unknown') {
+      throw new TypeError(`unknown type ${field.type}`);
+    }
+
     return {
       ...field,
       sortable: ('sortable' in field) ? !!field.sortable : kbnType.sortable,
