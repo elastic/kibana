87c11e9ade9d3cf0e89de27292bc6a6dec2abbef
Joe Fleming <joe.fleming@gmail.com>
2017-05-23 11:11:40 -0700

    refactor recompose out of ArgType container

--- public/components/arg_type/arg_type.js
+++ public/components/arg_type/arg_type.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { compose, branch, renderComponent } from 'recompose';
 import { Label } from 'react-bootstrap';
+import { ArgTypeUnknown } from './arg_type_unknown';
+import { ArgTypeContextPending } from './arg_type_context_pending';
+import { ArgTypeContextError } from './arg_type_context_error';
 
-export function ArgType(props) {
-  const { name, expressionType } = props;
-  const expressionProps = {
-    args: props.args,
-    context: props.context,
-    nextExpressionType: props.nextExpressionType,
-    onValueChange: props.onValueChange,
+function checkState(state) {
+  return ({ context, expressionType }) => {
+    const matchState = (!context || context.state === state);
+    return Boolean(expressionType && expressionType.requiresContext) && matchState;
   };
+}
+
+const nullExpressionType = branch(props => !props.expressionType, renderComponent(ArgTypeUnknown));
+const contextPending = branch(checkState('pending'), renderComponent(ArgTypeContextPending));
+const contextError = branch(checkState('error'), renderComponent(ArgTypeContextError));
+
+export const ArgType = compose(nullExpressionType, contextPending, contextError)((props) => {
+  const {
+    name,
+    args,
+    context,
+    expressionType,
+    nextExpressionType,
+    onValueChange,
+  } = props;
+
+  const expressionProps = { args, context, nextExpressionType, onValueChange };
 
   return (
     <div>
@@ -20,13 +38,13 @@ export function ArgType(props) {
       { expressionType.render(expressionProps) }
     </div>
   );
-}
+});
 
 ArgType.propTypes = {
   name: PropTypes.string.isRequired,
-  expressionType: PropTypes.object.isRequired,
   args: PropTypes.object.isRequired,
   context: PropTypes.object,
+  expressionType: PropTypes.object.isRequired,
   nextExpressionType: PropTypes.object,
   onValueChange: PropTypes.func,
 };
--- public/components/arg_type/index.js
+++ public/components/arg_type/index.js
@@ -1,10 +1,6 @@
 import { connect } from 'react-redux';
-import { get, flowRight } from 'lodash';
-import { branch, renderComponent, withProps } from 'recompose';
+import { get } from 'lodash';
 import { ArgType as Component } from './arg_type';
-import { ArgTypeUnknown } from './arg_type_unknown';
-import { ArgTypeContextPending } from './arg_type_context_pending';
-import { ArgTypeContextError } from './arg_type_context_error';
 import { findExpressionType } from '../../lib/find_expression_type';
 import { fetchContext, setArgumentAtIndex } from '../../state/actions/elements';
 import {
@@ -12,10 +8,6 @@ import {
   getSelectedPage,
   getSelectedResolvedArgs,
 } from '../../state/selectors/workpad';
-
-function contextRequired(expressionType) {
-  return Boolean(expressionType && expressionType.requiresContext);
-}
 
 const mapStateToProps = (state, { expressionIndex }) => {
   const resolvedArgs = getSelectedResolvedArgs(state);
@@ -34,9 +26,14 @@ const mapDispatchToProps = ({
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => {
   const { context, element, pageId } = stateProps;
-  const { expressionIndex, expressionType } = ownProps;
+  const { expressionIndex, argType, nextArgType } = ownProps;
+  const expressionType = findExpressionType(argType);
+  const nextExpressionType = nextArgType ? findExpressionType(nextArgType) : nextArgType;
 
   const props = Object.assign({}, stateProps, dispatchProps, ownProps, {
+    expressionType,
+    nextExpressionType,
+    name: get(expressionType, 'displayName', argType),
     updateContext: () => dispatchProps.fetchContext({ index: expressionIndex }),
     onValueChange: (arg) => dispatchProps.setArgumentAtIndex({
       arg,
@@ -46,48 +43,11 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => {
     }),
   });
 
-  if (context == null && contextRequired(expressionType)) {
+  if (context == null && Boolean(expressionType && expressionType.requiresContext)) {
     props.updateContext();
   }
 
   return props;
 };
 
-const contextPending = branch(
-  ({ context, expressionType }) => {
-    const isPending = (!context || context.state === 'pending');
-    return contextRequired(expressionType) && isPending;
-  },
-  renderComponent(ArgTypeContextPending)
-);
-
-const contextError = branch(
-  ({ context, expressionType }) => {
-    const isError = (context && context.state === 'error');
-    return contextRequired(expressionType) && isError;
-  },
-  renderComponent(ArgTypeContextError)
-);
-
-const nullExpressionType = branch(
-  props => !props.expressionType,
-  renderComponent(ArgTypeUnknown)
-);
-
-const appendProps = withProps(({ argType, nextArgType }) => {
-  const expressionType = findExpressionType(argType);
-  const nextExpressionType = nextArgType ? findExpressionType(nextArgType) : nextArgType;
-  return {
-    expressionType,
-    nextExpressionType,
-    name: get(expressionType, 'displayName', argType),
-  };
-});
-
-export const ArgType = flowRight([
-  appendProps,
-  connect(mapStateToProps, mapDispatchToProps, mergeProps),
-  nullExpressionType,
-  contextPending,
-  contextError,
-])(Component);
+export const ArgType = connect(mapStateToProps, mapDispatchToProps, mergeProps)(Component);
