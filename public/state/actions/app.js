8c67d3c9fb870764748002b89071047cbe214a1b
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-24 13:35:53 -0700

    Add action for toggling edit

--- public/state/actions/app.js
+++ public/state/actions/app.js
@@ -1,3 +1,8 @@
 import { createAction } from 'redux-actions';
+import { getEditing } from '../selectors/app';
 
 export const setEditing = createAction('setEditing');
+export const toggleEditing = () => (dispatch, getState) => {
+  const editing = getEditing(getState());
+  dispatch(setEditing(!editing));
+};
