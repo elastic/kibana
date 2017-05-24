d74e42e4333248e6485188a240aea51246d9465a
Joe Fleming <joe.fleming@gmail.com>
2017-05-24 12:34:22 -0700

    add comments to the arg_type component
    try to explain how compose and the branch components are working

--- public/components/arg_type/arg_type.js
+++ public/components/arg_type/arg_type.js
@@ -6,6 +6,7 @@ import { ArgTypeUnknown } from './arg_type_unknown';
 import { ArgTypeContextPending } from './arg_type_context_pending';
 import { ArgTypeContextError } from './arg_type_context_error';
 
+// helper to check the state of the passed in expression type
 function checkState(state) {
   return ({ context, expressionType }) => {
     const matchState = (!context || context.state === state);
@@ -13,10 +14,14 @@ function checkState(state) {
   };
 }
 
+// if no expressionType was provided, render the ArgTypeUnknown component
 const nullExpressionType = branch(props => !props.expressionType, renderComponent(ArgTypeUnknown));
+// if the expressionType is in a pending state, render ArgTypeContextPending
 const contextPending = branch(checkState('pending'), renderComponent(ArgTypeContextPending));
+// if the expressionType is in an error state, render ArgTypeContextError
 const contextError = branch(checkState('error'), renderComponent(ArgTypeContextError));
 
+// compose the above branch components, to short-circuit rending this ArgType component
 export const ArgType = compose(nullExpressionType, contextPending, contextError)((props) => {
   const {
     name,
