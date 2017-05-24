27cfe411d7274eefcab806bf3f14058df88d9a73
ppisljar <peter.pisljar@gmail.com>
2017-05-24 11:21:46 +0200

    adding missing react factory call

--- src/ui/public/vis/vis_factory.js
+++ src/ui/public/vis/vis_factory.js
@@ -1,8 +1,9 @@
-import { VisTypeProvider, AngularVisTypeProvider, VislibVisTypeProvider } from './vis_types';
+import { VisTypeProvider, AngularVisTypeProvider, ReactVisTypeProvider, VislibVisTypeProvider } from './vis_types';
 
 export const VisFactoryProvider = (Private) => {
   const VisType = Private(VisTypeProvider);
   const AngularVisType = Private(AngularVisTypeProvider);
+  const ReactVisType = Private(ReactVisTypeProvider);
   const VislibVisType = Private(VislibVisTypeProvider);
 
   return {
@@ -11,6 +12,9 @@ export const VisFactoryProvider = (Private) => {
     },
     createAngularVisualization: (config) => {
       return new AngularVisType(config);
+    },
+    createReactVisualization: (config) => {
+      return new ReactVisType(config);
     },
     createVislibVisualization: (config) => {
       return new VislibVisType(config);
