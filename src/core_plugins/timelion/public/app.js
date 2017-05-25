c553b81bf0525ae1b19d6a25cbd3c6ed38d03c4d
CJ Cenizal <cj@cenizal.com>
2017-05-25 09:59:54 -0700

    Rename Timelion Docs to Help, for consistency with Console.

--- src/core_plugins/timelion/public/app.js
+++ src/core_plugins/timelion/public/app.js
@@ -6,11 +6,11 @@ import { SavedObjectRegistryProvider } from 'ui/saved_objects/saved_object_regis
 import { notify } from 'ui/notify';
 
 require('plugins/timelion/directives/cells/cells');
-require('plugins/timelion/directives/fullscreen/fullscreen');
-require('plugins/timelion/directives/timelion_interval/timelion_interval');
-require('plugins/timelion/directives/timelion_expression_input');
 require('plugins/timelion/directives/fixed_element');
-require('plugins/timelion/directives/docs');
+require('plugins/timelion/directives/fullscreen/fullscreen');
+require('plugins/timelion/directives/timelion_expression_input');
+require('plugins/timelion/directives/timelion_help/timelion_help');
+require('plugins/timelion/directives/timelion_interval/timelion_interval');
 
 require('plugins/timelion/app.less');
 
@@ -131,15 +131,15 @@ app.controller('timelion', function (
     template: require('plugins/timelion/partials/sheet_options.html'),
     testId: 'timelionOptionsButton',
   }, {
-    key: 'docs',
-    description: 'Documentation',
-    template: '<timelion-docs></timelion-docs>',
+    key: 'help',
+    description: 'Help',
+    template: '<timelion-help></timelion-help>',
     testId: 'timelionDocsButton',
   }];
 
   $timeout(function () {
     if (config.get('timelion:showTutorial', true)) {
-      $scope.kbnTopNav.open('docs');
+      $scope.kbnTopNav.open('help');
     }
   }, 0);
 
@@ -171,7 +171,7 @@ app.controller('timelion', function (
       dontShowHelp: function () {
         config.set('timelion:showTutorial', false);
         $scope.setPage(0);
-        $scope.kbnTopNav.close('docs');
+        $scope.kbnTopNav.close('help');
       }
     };
   };
