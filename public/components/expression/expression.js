d7fd66417c02dc8174ebdf40b3305bb846cca0f5
Rashid Khan <github.fliplap@spamgourmet.com>
2017-05-25 14:42:34 -0700

    Add minimal expression styling, add support for done button

--- public/components/expression/expression.js
+++ public/components/expression/expression.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl, Button, ButtonToolbar } from 'react-bootstrap';
+import './expression.less';
 
-
-export const Expression = ({ expression, onChange, setExpression }) => {
+export const Expression = ({ expression, onChange, setExpression, done }) => {
   let input;
 
   return (
-    <div>
+    <div className="canvas__expression">
       <FormGroup controlId="formControlsTextarea">
-        <ControlLabel>
-          Expression &nbsp;
-          <a className="pull-right" onClick={() => setExpression(input.value)}> <i className="fa fa-play-circle"/></a>
-        </ControlLabel>
+
 
 
         <FormControl
@@ -23,7 +20,16 @@ export const Expression = ({ expression, onChange, setExpression }) => {
           onChange={(ev) => onChange(ev.target.value)}
           value={expression}
         />
+        <ControlLabel>
+          The Canvas expression backing the element. Better know what you're doing here.
+        </ControlLabel>
       </FormGroup>
+      <ButtonToolbar>
+        <Button bsStyle="primary" onClick={() => setExpression(input.value)}> Run</Button>
+        {done ?
+          (<Button onClick={done}> Done</Button>)
+        : null}
+      </ButtonToolbar>
     </div>
   );
 };
@@ -32,4 +38,5 @@ Expression.propTypes = {
   expression: PropTypes.string,
   onChange: PropTypes.func,
   setExpression: PropTypes.func,
+  done: PropTypes.func,
 };
