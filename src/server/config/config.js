807eeca16117397e5cebc24db3452245b8f37021
Court Ewing <court@epixa.com>
2017-05-25 16:02:34 -0400

    Always set NODE_ENV to production
    Using the --dev flag is the only support way to change the internal
    Kibana environment to something else. Allowing NODE_ENV to be set at all
    can have unexpected results with our dependencies, so we now always set
    it to production.

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -74,7 +74,7 @@ module.exports = class Config {
     let env = newVals.env;
     delete newVals.env;
     if (_.isObject(env)) env = env.name;
-    if (!env) env = process.env.NODE_ENV || 'production';
+    if (!env) env = 'production';
 
     const dev = env === 'development';
     const prod = env === 'production';
