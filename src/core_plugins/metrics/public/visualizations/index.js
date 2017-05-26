b4d1296832e671cd9d063a7b71609f80b974b908
Joe Fleming <joe.fleming@gmail.com>
2017-05-26 14:59:04 -0700

    fix named exports
    mostly fix the way exports works so the linter could resolve them, but fix a few incorrect imports as well

--- src/core_plugins/metrics/public/visualizations/index.js
+++ src/core_plugins/metrics/public/visualizations/index.js
@@ -1,20 +1,22 @@
+// utilities
 import getLastValue from './lib/get_last_value';
 import flot from './lib/flot';
 import events from './lib/events';
 
+// visualizations
 import Timeseries from './components/timeseries';
 import Metric from './components/metric';
 import Gauge from './components/gauge';
 import TopN from './components/top_n';
 
 export default {
-  // visualizations
-  TopN,
-  Timeseries,
-  Metric,
-  Gauge,
   // utilities
   getLastValue,
   flot,
   events,
+  // visualizations
+  Timeseries,
+  Metric,
+  Gauge,
+  TopN,
 };
