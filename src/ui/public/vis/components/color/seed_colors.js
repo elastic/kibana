fcb181d22bbb1b4af942fa70dae4c1e3bb784ea3
Dave Snider <dave.snider@gmail.com>
2017-05-30 12:16:07 -0700

    more coloring

--- src/ui/public/vis/components/color/seed_colors.js
+++ src/ui/public/vis/components/color/seed_colors.js
@@ -3,4 +3,4 @@
  * So we needed to come up with a color scheme of our own that creates consistent, pleasing color patterns.
  * The order allows us to guarantee that 1st, 2nd, 3rd, etc values always get the same color.
  */
-export const seedColors = ['#228AE6', '#22B8CF', '#0C8599', '#C0EB75', '#E599F7', '#7048E8', '#FA5252', '#C2255C', '#FFA94D', '#343A40'];
+export const seedColors = ['#228AE6', '#C0EB75', '#22B8CF', '#E599F7', '#7048E8', '#FA5252', '#0C8599', '#C2255C', '#FFA94D', '#343A40'];
