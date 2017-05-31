8e634febbbbf1cbeee7f7b95f0d1cc62dc1a5007
Joe Fleming <joe.fleming@gmail.com>
2017-05-30 17:22:48 -0700

    replace define() with exports

--- src/core_plugins/timelion/public/vis/index.js
+++ src/core_plugins/timelion/public/vis/index.js
@@ -2,40 +2,34 @@ import { VisVisTypeProvider } from 'ui/vis/vis_type';
 import image from '../images/icon-timelion.svg';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 import { TemplateVisTypeProvider } from 'ui/template_vis_type';
+import template from 'plugins/timelion/vis/timelion_vis.html';
+import editorTemplate from 'plugins/timelion/vis/timelion_vis_params.html';
+import 'plugins/timelion/vis/timelion_vis_controller';
+import 'plugins/timelion/vis/timelion_vis_params_controller';
+import 'plugins/timelion/vis/timelion_vis.less';
 
-define(function (require) {
-  // we also need to load the controller and used by the template
-  require('plugins/timelion/vis/timelion_vis_controller');
-  require('plugins/timelion/vis/timelion_vis_params_controller');
+// export the provider so that the visType can be required with Private()
+export default function TimelionVisProvider(Private) {
+  const VisType = Private(VisVisTypeProvider);
+  const TemplateVisType = Private(TemplateVisTypeProvider);
 
-  // Stylin
-  require('plugins/timelion/vis/timelion_vis.less');
+  // return the visType object, which kibana will use to display and configure new
+  // Vis object of this type.
+  return new TemplateVisType({
+    name: 'timelion',
+    title: 'Timelion',
+    image,
+    description: 'Build time-series using functional expressions',
+    category: VisType.CATEGORY.TIME,
+    template,
+    params: {
+      editor: editorTemplate,
+    },
+    requiresSearch: false,
+    requiresTimePicker: true,
+    implementsRenderComplete: true,
+  });
+}
 
-  // register the provider with the visTypes registry so that other know it exists
-  VisTypesRegistryProvider.register(TimelionVisProvider);
-
-  function TimelionVisProvider(Private) {
-    const VisType = Private(VisVisTypeProvider);
-    const TemplateVisType = Private(TemplateVisTypeProvider);
-
-    // return the visType object, which kibana will use to display and configure new
-    // Vis object of this type.
-    return new TemplateVisType({
-      name: 'timelion',
-      title: 'Timelion',
-      image,
-      description: 'Build time-series using functional expressions',
-      category: VisType.CATEGORY.TIME,
-      template: require('plugins/timelion/vis/timelion_vis.html'),
-      params: {
-        editor: require('plugins/timelion/vis/timelion_vis_params.html')
-      },
-      requiresSearch: false,
-      requiresTimePicker: true,
-      implementsRenderComplete: true,
-    });
-  }
-
-  // export the provider so that the visType can be required with Private()
-  return TimelionVisProvider;
-});
+// register the provider with the visTypes registry so that other know it exists
+VisTypesRegistryProvider.register(TimelionVisProvider);
--- src/ui/public/vislib/lib/layout/splits/column_chart/chart_split.js
+++ src/ui/public/vislib/lib/layout/splits/column_chart/chart_split.js
@@ -1,63 +1,61 @@
 import d3 from 'd3';
-define(function () {
-  return function ChartSplitFactory() {
 
-    /*
-     * Adds div DOM elements to the `.chart-wrapper` element based on the data layout.
-     * For example, if the data has rows, it returns the same number of
-     * `.chart` elements as row objects.
-     */
-    return function split(selection, parent) {
-      selection.each(function (data) {
-        const div = d3.select(this)
-        .attr('class', function () {
-          if (data.rows) {
-            return 'chart-wrapper-row';
-          } else if (data.columns) {
-            return 'chart-wrapper-column';
-          } else {
-            if (parent) {
-              return 'chart-first chart-last chart-wrapper';
-            }
-            return this.className + ' chart-wrapper';
+export function VislibLibLayoutSplitsColumnChartChartSplitProvider() {
+  /*
+   * Adds div DOM elements to the `.chart-wrapper` element based on the data layout.
+   * For example, if the data has rows, it returns the same number of
+   * `.chart` elements as row objects.
+   */
+  return function split(selection, parent) {
+    selection.each(function (data) {
+      const div = d3.select(this)
+      .attr('class', function () {
+        if (data.rows) {
+          return 'chart-wrapper-row';
+        } else if (data.columns) {
+          return 'chart-wrapper-column';
+        } else {
+          if (parent) {
+            return 'chart-first chart-last chart-wrapper';
           }
-        });
-        let divClass = '';
-        let chartsNumber;
-
-        const charts = div.selectAll('charts')
-        .append('div')
-        .data(function (d) {
-          if (d.rows) {
-            chartsNumber = d.rows.length;
-            return d.rows;
-          } else if (d.columns) {
-            chartsNumber = d.columns.length;
-            return d.columns;
-          } else {
-            divClass = 'chart';
-            chartsNumber = 1;
-            return [d];
-          }
-        })
-        .enter()
-          .append('div')
-          .attr('class', function (d, i) {
-            let fullDivClass = divClass;
-            if (chartsNumber > 1) {
-              if (i === 0) {
-                fullDivClass += ' chart-first';
-              } else if (i === chartsNumber - 1) {
-                fullDivClass += ' chart-last';
-              }
-            }
-            return fullDivClass;
-          });
-
-        if (!data.series) {
-          charts.call(split);
+          return this.className + ' chart-wrapper';
         }
       });
-    };
+      let divClass = '';
+      let chartsNumber;
+
+      const charts = div.selectAll('charts')
+      .append('div')
+      .data(function (d) {
+        if (d.rows) {
+          chartsNumber = d.rows.length;
+          return d.rows;
+        } else if (d.columns) {
+          chartsNumber = d.columns.length;
+          return d.columns;
+        } else {
+          divClass = 'chart';
+          chartsNumber = 1;
+          return [d];
+        }
+      })
+      .enter()
+        .append('div')
+        .attr('class', function (d, i) {
+          let fullDivClass = divClass;
+          if (chartsNumber > 1) {
+            if (i === 0) {
+              fullDivClass += ' chart-first';
+            } else if (i === chartsNumber - 1) {
+              fullDivClass += ' chart-last';
+            }
+          }
+          return fullDivClass;
+        });
+
+      if (!data.series) {
+        charts.call(split);
+      }
+    });
   };
-});
+}
