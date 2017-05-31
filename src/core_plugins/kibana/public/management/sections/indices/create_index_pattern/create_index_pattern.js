d7049c23b42a9d06d7e50f51e4b7f033f1c30ce7
Spencer <spalger@users.noreply.github.com>
2017-05-31 11:49:35 -0700

    [indexPatterns/create] remove check for interval/name in name watcher (#12101)
    (cherry picked from commit 66a127d3b88f731a98a363b7f216c41ec1edddd2)

--- src/core_plugins/kibana/public/management/sections/indices/create_index_pattern/create_index_pattern.js
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern/create_index_pattern.js
@@ -344,9 +344,6 @@ uiModules.get('apps/management')
     'controller.formValues.nameInterval'
   ], () => {
     resetIndex();
-    if (!this.formValues.nameInterval || !this.formValues.name) {
-      return;
-    }
 
     // track the latestUpdateSampleId at the time we started
     // so that we can avoid mutating the controller if the
