747736b455a86ff930397eb771e30b8be9f7c745
Stacey Gammon <gammon@elastic.co>
2017-06-02 15:28:26 -0400

    [5.4] fix dashboard state url (#12148)
    * Fix for react-input-autosize bug (#11543)
    
    * Fix for react-select bug
    
    Description of the issue here:
    https://github.com/JedWatson/react-select/issues/1694
    
    * Can't put comments in package.json I guess
    
    * Update dashboard based on url changes (#12123)
    
    * Update dashboard based on url changes
    
    * make clickSelector a retry in case it fails the first time
    
    # Conflicts:
    #	src/core_plugins/kibana/public/dashboard/dashboard.js
    #	test/functional/apps/dashboard/_dashboard.js

--- test/functional/services/remote/interceptors.js
+++ test/functional/services/remote/interceptors.js
@@ -2,12 +2,15 @@ import { modifyUrl } from '../../../../src/utils';
 
 export const createRemoteInterceptors = remote => ({
   // inject _t=Date query param on navigation
-  async get(url) {
-    const urlWithTime = modifyUrl(url, parsed => {
-      parsed.query._t = Date.now();
-    });
+  async get(url, insertTimestamp = true) {
+    if (insertTimestamp) {
+      const urlWithTime = modifyUrl(url, parsed => {
+        parsed.query._t = Date.now();
+      });
 
-    return await remote.get(urlWithTime);
+      return await remote.get(urlWithTime);
+    }
+    return await remote.get(url);
   },
 
   // strip _t=Date query param when url is read
