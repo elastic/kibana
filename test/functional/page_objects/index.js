c0ebc24780bbbaf072ef47a9fd3f475f56b4f7e9
Shaunak Kashyap <ycombinator@gmail.com>
2017-06-02 13:12:26 -0700

    Revert "Getting Started page (#11805)"
    This reverts commit 32eff371f2367b9ffa6e1fabf2b3a8a61f90aa35.

--- test/functional/page_objects/index.js
+++ test/functional/page_objects/index.js
@@ -9,4 +9,3 @@ export { VisualizePageProvider } from './visualize_page';
 export { SettingsPageProvider } from './settings_page';
 export { MonitoringPageProvider } from './monitoring_page';
 export { PointSeriesPageProvider } from './point_series_page';
-export { GettingStartedPageProvider } from './getting_started_page';
