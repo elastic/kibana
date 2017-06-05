27745f72a382a523dce72d2264bc701ea6b97b3f
Shaunak Kashyap <ycombinator@gmail.com>
2017-06-05 14:18:30 -0700

    Revert the Getting Started page (#12154)
    * Revert "When on an embedded page, bypass Getting Started gate check (#12040)"
    
    This reverts commit 05293f1c8d765179a0d2973df16617e653323f83.
    
    * Revert "Making tweaks. (#12003)"
    
    This reverts commit aa3fa06f61a7795e428370e251a24acb19335e5b.
    
    * Revert "Functional tests for the Getting Started page (#11850)"
    
    This reverts commit 099178a7cab514344550962aad629b5b438d48e1.
    
    * Revert "Prevent flicker on Getting Started page (#11826)"
    
    This reverts commit c4b3ade27d8be5b34daf47a0d1c64fb6b87c12c6.
    
    * Revert "Getting Started page (#11805)"
    
    This reverts commit 32eff371f2367b9ffa6e1fabf2b3a8a61f90aa35.
    
    * Remove check for Getting Started page from navigateToApp

--- src/ui/public/notify/notifier.js
+++ src/ui/public/notify/notifier.js
@@ -230,8 +230,8 @@ Notifier.config = {
   errorLifetime: 300000,
   warningLifetime: 10000,
   infoLifetime: 5000,
-  setInterval: window.setInterval.bind(window),
-  clearInterval: window.clearInterval.bind(window)
+  setInterval: window.setInterval,
+  clearInterval: window.clearInterval
 };
 
 Notifier.applyConfig = function (config) {
