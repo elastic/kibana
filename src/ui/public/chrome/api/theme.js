c8d8e766f340cadbb625d8eafcdccb84bf643deb
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2017-06-06 23:49:41 +0200

    Remove unused nav background code (#11026)
    (cherry picked from commit 87231216769be94b568808b1f61afae17458dde8)

--- src/ui/public/chrome/api/theme.js
+++ src/ui/public/chrome/api/theme.js
@@ -4,10 +4,6 @@ module.exports = function (chrome, internals) {
   /**
    * ui/chrome Theme API
    *
-   *   Nav Background
-   *     applies to the entire nav bar and is specified as a css string.
-   *     eg. 'red' or 'url(..) no-repeat left center'
-   *
    *   Logo
    *     Set the background for the logo and small logo in the navbar.
    *     When the app is in the "small" category, a modified version of the
@@ -17,22 +13,6 @@ module.exports = function (chrome, internals) {
    *   Brand
    *     Similar to a logo, but is just text with styles to make it stick out.
    */
-
-  /**
-   * @param {string} background - css background definition
-   * @return {chrome}
-   */
-  chrome.setNavBackground = function (cssBackground) {
-    internals.navBackground = cssBackground;
-    return chrome;
-  };
-
-  /**
-   * @return {string} - css background
-   */
-  chrome.getNavBackground = function () {
-    return internals.navBackground;
-  };
 
   /**
    * @param {string|object} item - brand key to set, or object to apply
