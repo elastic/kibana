a5f1e10b1e6cf7176e0789ed3fca7a81e5e95fdb
Stacey Gammon <gammon@elastic.co>
2017-06-08 09:14:02 -0400

    Preserve the saved vis url when using the add new vis link from dashboard (#11932) (#12199)
    * Preserve the saved vis url when using the add new vis link from dashboard
    
    Fixes https://github.com/elastic/kibana/issues/11078
    
    * Store the edit url in browser history as well
    
    * Fix tests by reordering them.
    
    * add a more explanatory comment

--- test/functional/apps/dashboard/_dashboard.js
+++ test/functional/apps/dashboard/_dashboard.js
@@ -5,6 +5,10 @@ import {
   DEFAULT_PANEL_HEIGHT,
 } from '../../../../src/core_plugins/kibana/public/dashboard/panel/panel_state';
 
+import {
+  VisualizeConstants
+} from '../../../../src/core_plugins/kibana/public/visualize/visualize_constants';
+
 export default function ({ getService, getPageObjects }) {
   const retry = getService('retry');
   const log = getService('log');
@@ -130,23 +134,6 @@ export default function ({ getService, getPageObjects }) {
       });
     });
 
-    it('add new visualization link', async function () {
-      await PageObjects.dashboard.clickAddVisualization();
-      await PageObjects.dashboard.clickAddNewVisualizationLink();
-      await PageObjects.visualize.clickAreaChart();
-      await PageObjects.visualize.clickNewSearch();
-      await PageObjects.visualize.saveVisualization('visualization from add new link');
-      await PageObjects.header.clickToastOK();
-
-      const visualizations = PageObjects.dashboard.getTestVisualizations();
-      return retry.tryForTime(10000, async function () {
-        const panelTitles = await PageObjects.dashboard.getPanelSizeData();
-        log.info('visualization titles = ' + panelTitles.map(item => item.title));
-        screenshots.take('Dashboard-visualization-sizes');
-        expect(panelTitles.length).to.eql(visualizations.length + 1);
-      });
-    });
-
     describe('Directly modifying url updates dashboard state', () => {
       it('for query parameter', async function () {
         const currentQuery = await PageObjects.dashboard.getQuery();
@@ -166,6 +153,31 @@ export default function ({ getService, getPageObjects }) {
         await remote.get(newUrl.toString(), false);
         const allPanelInfo = await PageObjects.dashboard.getPanelSizeData();
         expect(allPanelInfo[0].dataSizeX).to.equal(`${DEFAULT_PANEL_WIDTH * 2}`);
+      });
+    });
+
+    describe('add new visualization link', () => {
+      it('adds a new visualization', async () => {
+        await PageObjects.dashboard.clickAddVisualization();
+        await PageObjects.dashboard.clickAddNewVisualizationLink();
+        await PageObjects.visualize.clickAreaChart();
+        await PageObjects.visualize.clickNewSearch();
+        await PageObjects.visualize.saveVisualization('visualization from add new link');
+        await PageObjects.header.clickToastOK();
+
+        const visualizations = PageObjects.dashboard.getTestVisualizations();
+        return retry.tryForTime(10000, async function () {
+          const panelTitles = await PageObjects.dashboard.getPanelSizeData();
+          log.info('visualization titles = ' + panelTitles.map(item => item.title));
+          screenshots.take('Dashboard-visualization-sizes');
+          expect(panelTitles.length).to.eql(visualizations.length + 1);
+        });
+      });
+
+      it('saves the saved visualization url to the app link', async () => {
+        await PageObjects.header.clickVisualize();
+        const currentUrl = await remote.getCurrentUrl();
+        expect(currentUrl).to.contain(VisualizeConstants.EDIT_PATH);
       });
     });
   });
