19279f3dedcdc16a7a428bd99d23a94cd99f52b7
spalger <spalger@users.noreply.github.com>
2017-06-12 12:57:39 -0700

    [uiSettings/serviceFactory] fix doc block

--- src/ui/ui_settings/ui_settings_service_factory.js
+++ src/ui/ui_settings/ui_settings_service_factory.js
@@ -4,10 +4,16 @@ import { UiSettingsService } from './ui_settings_service';
  *  Create an instance of UiSettingsService that will use the
  *  passed `callCluster` function to communicate with elasticsearch
  *
- *  @param  {Function} callCluster should accept a method name as a string and a params object
+ *  @param {Hapi.Server} server
+ *  @param {Object} options
+ *  @property {AsyncFunction} options.callCluster function that accepts a method name and
+ *                            param object which causes a request via some elasticsearch client
+ *  @property {AsyncFunction} [options.readInterceptor] async function that is called when the
+ *                            UiSettingsService does a read() an has an oportunity to intercept the
+ *                            request and return an alternate `_source` value to use.
  *  @return {UiSettingsService}
  */
-export function uiSettingsServiceFactory(server, options = {}) {
+export function uiSettingsServiceFactory(server, options) {
   const config = server.config();
 
   const {
