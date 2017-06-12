8a66d8f8f97757c47a556eb58bb85c78deb30237
CJ Cenizal <cj@cenizal.com>
2017-06-12 16:09:55 -0700

    Update LocalNav examples to use tabIndex instead of tabindex, to eliminate React console warnings. (#12284)

--- ui_framework/doc_site/src/views/local_nav/local_nav_breadcrumbs.js
+++ ui_framework/doc_site/src/views/local_nav/local_nav_breadcrumbs.js
@@ -12,12 +12,12 @@ export function LocalNavWithBreadcrumbs() {
       <KuiLocalNavRow>
         <KuiLocalNavRowSection>
           <div className="kuiLocalBreadcrumbs">
-            <h1 tabindex="0" id="kui_local_breadcrumb" className="kuiLocalBreadcrumb">
+            <h1 tabIndex="0" id="kui_local_breadcrumb" className="kuiLocalBreadcrumb">
               <a className="kuiLocalBreadcrumb__link" href="#">
                 Discover
               </a>
             </h1>
-            <h1 tabindex="0" id="kui_local_breadcrumb" className="kuiLocalBreadcrumb">
+            <h1 tabIndex="0" id="kui_local_breadcrumb" className="kuiLocalBreadcrumb">
               <span className="kuiLocalBreadcrumb__emphasis">0</span> hits
             </h1>
           </div>
