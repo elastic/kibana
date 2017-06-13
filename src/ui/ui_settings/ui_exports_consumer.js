725471cc02cb1dd84043dc38be62b5a007bfbd6b
spalger <spalger@users.noreply.github.com>
2017-06-13 11:18:22 -0700

    describe the UiExportsConsumer class

--- src/ui/ui_settings/ui_exports_consumer.js
+++ src/ui/ui_settings/ui_exports_consumer.js
@@ -1,3 +1,20 @@
+/**
+ *  The UiExports class accepts consumer objects that is consults while
+ *  trying to consume all of the `uiExport` declarations provided by
+ *  plugins.
+ *
+ *  UiExportConsumer is instantiated and passed to UiExports, then for
+ *  every `uiExport` declaration it calls `exportConsumer(type)` with
+ *  the key of the declaration. If this consumer knows how to handle
+ *  that key it should return a function that will be called with the
+ *  plugins and values of all matching declarations.
+ *
+ *  With this, the consumer merges all of the declarations into the
+ *  _uiSettingDefaults map, ensuring that there are not collisions along
+ *  the way.
+ *
+ *  @class UiExportsConsumer
+ */
 export class UiExportsConsumer {
   _uiSettingDefaults = {};
 
@@ -16,6 +33,10 @@ export class UiExportsConsumer {
     }
   }
 
+  /**
+   *  Get the map of uiSettingNames to "default" specifications
+   *  @return {Object<String,UiSettingsDefault>}
+   */
   getUiSettingDefaults() {
     return this._uiSettingDefaults;
   }
