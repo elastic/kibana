51a4c735c78355432b639bd0e68fda84abd0584b
spalger <spalger@users.noreply.github.com>
2017-06-13 11:38:56 -0700

    fix typos

--- src/ui/ui_settings/ui_exports_consumer.js
+++ src/ui/ui_settings/ui_exports_consumer.js
@@ -1,13 +1,13 @@
 /**
- *  The UiExports class accepts consumer objects that is consults while
+ *  The UiExports class accepts consumer objects that it consults while
  *  trying to consume all of the `uiExport` declarations provided by
  *  plugins.
  *
  *  UiExportConsumer is instantiated and passed to UiExports, then for
- *  every `uiExport` declaration it calls `exportConsumer(type)` with
- *  the key of the declaration. If this consumer knows how to handle
- *  that key it should return a function that will be called with the
- *  plugins and values of all matching declarations.
+ *  every `uiExport` declaration the `exportConsumer(type)` method is
+ *  with the key of the declaration. If this consumer knows how to handle
+ *  that key we return a function that will be called with the plugins
+ *  and values of all declarations using that key.
  *
  *  With this, the consumer merges all of the declarations into the
  *  _uiSettingDefaults map, ensuring that there are not collisions along
