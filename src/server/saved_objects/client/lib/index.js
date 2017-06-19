89fe57359487d635b13e660620eeb8382c1420a1
Jonathan Budzenski <jon@jbudz.me>
2017-06-19 15:49:53 -0500

    [saved objects api] Export getDocType

--- src/server/saved_objects/client/lib/index.js
+++ src/server/saved_objects/client/lib/index.js
@@ -1,3 +1,4 @@
 export { createFindQuery } from './create_find_query';
 export { createIdQuery } from './create_id_query';
+export { getDocType } from './get_doc_type';
 export { handleEsError } from './handle_es_error';
