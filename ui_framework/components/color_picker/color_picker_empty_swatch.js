896d975b68ef85d04470b6c5b0f3bdb1e8842810
Stacey Gammon <gammon@elastic.co>
2017-06-20 15:12:46 -0400

    Remove unnecessary div

--- ui_framework/components/color_picker/color_picker_empty_swatch.js
+++ ui_framework/components/color_picker/color_picker_empty_swatch.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 export function KuiColorPickerEmptySwatch() {
   return (
-    <div className="kuiColorPicker__swatch kuiColorPicker__emptySwatch">
-      <svg>
-        <line x1="0" y1="100%" x2="100%" y2="0" />
-      </svg>
-    </div>
+    <svg>
+      <line x1="0" y1="100%" x2="100%" y2="0" />
+    </svg>
   );
 }
 
