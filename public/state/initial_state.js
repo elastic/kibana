ff67b55cf71f24034220346be3a729df5bf31d80
Rashid Khan <github.fliplap@spamgourmet.com>
2017-06-22 14:24:07 -0700

    Use default getters

--- public/state/initial_state.js
+++ public/state/initial_state.js
@@ -1,11 +1,12 @@
 import { get } from 'lodash';
+import { getDefaultWorkpad } from './defaults';
 
 export default (path) => {
+
   const state = {
     app: {}, // Kibana stuff in here
     transient: {
       editing: true,
-      selectedPage: 'page-f3ce-4bb7-86c8-0417606d6592',
       selectedElement: null,
       resolvedArgs: {},
       // values in resolvedArgs should live under a unique index so they can be looked up.
@@ -16,35 +17,7 @@ export default (path) => {
     },
     persistent: {
       schemaVersion: 0,
-      workpad: {
-        name: 'Untitled Workpad',
-        id: 'workpad-2235-4af1-b781-24e42a453e5b',
-        width: 600,
-        height: 720,
-        page: 0,
-        pages: [{
-          id: 'page-f3ce-4bb7-86c8-0417606d6592',
-          name: 'Name of the page',
-          style: {
-            backgroundColor: '#fff',
-          },
-          elements: [{
-            id: 'element-d88c-4bbd-9453-db22e949b92e',
-            // datasource: 'asset-f5d8-4615-9463-a7b1e2850523',
-            position: {
-              top: 100,
-              left: 200,
-              height: 400,
-              width: 600,
-              rotation: 90,
-            },
-            expression: `demodata().pointseries(y="median(cost)", x=time, color="project").plot(
-              palette=palette(#04BFBF, #CAFCD8, #F7E967, #A9CF54, #588F27),
-              defaultStyle=seriesStyle(bars=1, lines=0, weight=0, points=1)
-            )`,
-          }],
-        }],
-      },
+      workpad: getDefaultWorkpad(),
     },
   };
 
