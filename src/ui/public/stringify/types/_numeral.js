3c9eceb24033c613d6241a4eaade2aca46879764
Nathan Reese <reese.nathan@gmail.com>
2017-06-23 10:54:48 -0600

    remove _.class from _numeral.js

--- src/ui/public/stringify/types/_numeral.js
+++ src/ui/public/stringify/types/_numeral.js
@@ -4,45 +4,46 @@ import { FieldFormat } from 'ui/index_patterns/_field_format/field_format';
 
 const numeralInst = numeral();
 
-_.class(Numeral).inherits(FieldFormat);
-export function Numeral(params) {
-  Numeral.Super.call(this, params);
-}
-
-Numeral.prototype._convert = function (val) {
-  if (val === -Infinity) return '-∞';
-  if (val === +Infinity) return '+∞';
-  if (typeof val !== 'number') {
-    val = parseFloat(val);
+export class Numeral extends FieldFormat {
+  constructor(params) {
+    super(params);
   }
 
-  if (isNaN(val)) return '';
+  _convert(val) {
+    if (val === -Infinity) return '-∞';
+    if (val === +Infinity) return '+∞';
+    if (typeof val !== 'number') {
+      val = parseFloat(val);
+    }
 
-  return numeralInst.set(val).format(this.param('pattern'));
-};
+    if (isNaN(val)) return '';
 
+    return numeralInst.set(val).format(this.param('pattern'));
+  }
+}
 
 Numeral.factory = function (opts) {
-  _.class(Class).inherits(Numeral);
-  function Class(params, getConfig) {
-    Class.Super.call(this, params);
+  class Class extends Numeral {
+    constructor(params, getConfig) {
+      super(params);
 
-    this.getConfig = getConfig;
+      this.getConfig = getConfig;
+    }
+
+    getParamDefaults = function () {
+      if (_.has(opts, 'getParamDefaults')) {
+        return opts.getParamDefaults(this.getConfig);
+      }
+
+      return {
+        pattern: this.getConfig(`format:${opts.id}:defaultPattern`)
+      };
+    }
   }
 
   Class.id = opts.id;
   Class.title = opts.title;
   Class.fieldType = 'number';
-
-  Class.prototype.getParamDefaults = function () {
-    if (_.has(opts, 'getParamDefaults')) {
-      return opts.getParamDefaults(this.getConfig);
-    }
-
-    return {
-      pattern: this.getConfig(`format:${opts.id}:defaultPattern`)
-    };
-  };
 
   if (opts.prototype) {
     _.assign(Class.prototype, opts.prototype);
