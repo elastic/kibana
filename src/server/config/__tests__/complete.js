d34dc575a5d5bff90f9d34052bc233ea7a7a8531
spalger <spalger@users.noreply.github.com>
2017-06-28 14:57:17 -0700

    [server/config/complete] parameterize test setup

--- src/server/config/__tests__/complete.js
+++ src/server/config/__tests__/complete.js
@@ -1,14 +1,15 @@
-import complete from '../complete';
+import completeMixin from '../complete';
 import expect from 'expect.js';
 import { noop } from 'lodash';
 import sinon from 'sinon';
 
-describe('server config complete', function () {
-  it(`should call server.log when there's an unused setting`, function () {
+describe('server/config completeMixin()', function () {
+  const sandbox = sinon.sandbox.create();
+  afterEach(() => sandbox.restore());
+
+  const setup = ({ settings, configValues }) => {
     const kbnServer = {
-      settings: {
-        unused: true
-      }
+      settings,
     };
 
     const server = {
@@ -17,89 +18,78 @@ describe('server config complete', function () {
     };
 
     const config = {
-      get: sinon.stub().returns({
-        used: true
-      })
+      get: sinon.stub().returns(configValues)
     };
 
-    complete(kbnServer, server, config);
+    const callCompleteMixin = () => {
+      completeMixin(kbnServer, server, config);
+    };
 
+    return { server, callCompleteMixin };
+  };
+
+  it(`should call server.log when there's an unused setting`, function () {
+    const { server, callCompleteMixin } = setup({
+      settings: {
+        unused: true
+      },
+      configValues: {
+        used: true
+      }
+    });
+
+    callCompleteMixin();
     expect(server.log.calledOnce).to.be(true);
   });
 
   it(`shouldn't call server.log when there isn't an unused setting`, function () {
-    const kbnServer = {
+    const { server, callCompleteMixin } = setup({
       settings: {
         used: true
-      }
-    };
-
-    const server = {
-      decorate: noop,
-      log: sinon.spy()
-    };
-
-    const config = {
-      get: sinon.stub().returns({
+      },
+      configValues: {
         used: true
-      })
-    };
+      },
+    });
 
-    complete(kbnServer, server, config);
-
+    callCompleteMixin();
     expect(server.log.called).to.be(false);
   });
 
   it(`shouldn't call server.log when there are more config values than settings`, function () {
-    const kbnServer = {
+    const { server, callCompleteMixin } = setup({
       settings: {
         used: true
-      }
-    };
-
-    const server = {
-      decorate: noop,
-      log: sinon.spy()
-    };
-
-    const config = {
-      get: sinon.stub().returns({
+      },
+      configValues: {
         used: true,
         foo: 'bar'
-      })
-    };
+      }
+    });
 
-    complete(kbnServer, server, config);
+    callCompleteMixin();
     expect(server.log.called).to.be(false);
   });
 
   it('should transform server.ssl.cert to server.ssl.certificate', function () {
-    const kbnServer = {
+    const { server, callCompleteMixin } = setup({
       settings: {
         server: {
           ssl: {
             cert: 'path/to/cert'
           }
         }
-      }
-    };
-
-    const server = {
-      decorate: noop,
-      log: sinon.spy()
-    };
-
-    const config = {
-      get: sinon.stub().returns({
+      },
+      configValues: {
         server: {
           ssl: {
             certificate: 'path/to/cert'
           }
         }
-      })
-    };
+      }
+    });
 
-    complete(kbnServer, server, config);
+    callCompleteMixin();
     expect(server.log.called).to.be(false);
   });
 });
