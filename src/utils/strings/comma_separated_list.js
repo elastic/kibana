34302dd4095e601a74a36e249d0fde9d761b9764
spalger <spalger@users.noreply.github.com>
2017-06-28 11:59:27 -0700

    [utils] remove mutation in parseCommaSeparatedList()

--- src/utils/strings/comma_separated_list.js
+++ src/utils/strings/comma_separated_list.js
@@ -1,12 +1,7 @@
 export function parseCommaSeparatedList(input) {
   if (Array.isArray(input)) return input;
-
-  const source = String(input || '').split(',');
-  const list = [];
-  while (source.length) {
-    const item = source.shift().trim();
-    if (item) list.push(item);
-  }
-
-  return list;
+  return String(input || '')
+    .split(',')
+    .map(word => word.trim())
+    .filter(Boolean);
 }
