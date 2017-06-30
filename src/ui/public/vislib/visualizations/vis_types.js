8abd809bdead3755b0978f332f5517e3fc2b4e32
Thomas Neirynck <thomas@elastic.co>
2017-06-30 11:49:19 -0400

    goal-type should be discoverable (#12603)

--- src/ui/public/vislib/visualizations/vis_types.js
+++ src/ui/public/vislib/visualizations/vis_types.js
@@ -16,6 +16,7 @@ export function VislibVisualizationsVisTypesProvider(Private) {
     pie: Private(VislibVisualizationsPieChartProvider),
     point_series: Private(VislibVisualizationsPointSeriesProvider),
     gauge: Private(GaugeChartProvider),
-    metric: Private(GaugeChartProvider),
+    goal: Private(GaugeChartProvider),
+    metric: Private(GaugeChartProvider)
   };
 }
