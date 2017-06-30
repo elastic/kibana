578961fc57c039a0488b5c63a7c657d062d16c02
Chris Cowan <chris@chriscowan.us>
2017-06-30 12:06:09 -0700

    removing the extra sortBy

--- src/core_plugins/metrics/server/lib/get_fields.js
+++ src/core_plugins/metrics/server/lib/get_fields.js
@@ -1,9 +1,9 @@
-import { sortBy, uniq } from 'lodash';
+import { uniq } from 'lodash';
 
 export async function getFields(req) {
   const { indexPatternsService } = req.pre;
   const index = req.query.index || '*';
   const resp = await indexPatternsService.getFieldsForWildcard({ pattern: index });
   const fields = resp.filter(field => field.aggregatable);
-  return sortBy(uniq(fields, field => field.name), 'name');
+  return uniq(fields, field => field.name);
 }
