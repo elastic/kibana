e1950ab7d0686b3a01caa0899faf4bf5a5e4904c
CJ Cenizal <cj@cenizal.com>
2017-06-30 12:49:02 -0700

    Allow kuiLocalNavRow to expand vertically if the content wraps. (#12621)

--- ui_framework/doc_site/src/views/local_nav/local_nav_simple.js
+++ ui_framework/doc_site/src/views/local_nav/local_nav_simple.js
@@ -23,7 +23,7 @@ export function SimpleLocalNav() {
             <div className="kuiLocalMenuItem">Open</div>
             <button className="kuiLocalMenuItem">
               <div className="kuiLocalMenuItem__icon kuiIcon fa-clock-o"></div>
-              Last 5 minutes
+              September 29, 2017, 12:21.05.696 to November 28, 2019 11:03:22.108
             </button>
           </div>
         </KuiLocalNavRowSection>
