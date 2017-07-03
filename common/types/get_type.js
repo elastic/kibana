b66b93221e4782acc06483a6bfd1b84f09640233
Rashid Khan <github.fliplap@spamgourmet.com>
2017-07-03 10:58:31 -0700

    Keep core types pure

--- common/types/get_type.js
+++ common/types/get_type.js
@@ -1,17 +1,8 @@
-import { isValid as isValidDataurl } from '../lib/dataurl';
-
 export function getType(node) {
   if (node == null) return 'null';
   if (typeof node === 'object') {
     if (!node.type) throw new Error('Objects must have a type propery');
     return node.type;
-  }
-
-  const type = typeof node;
-
-  // check for dataurl encoding
-  if (type === 'string') {
-    if (isValidDataurl(node)) return 'dataurl';
   }
 
   return typeof node;
