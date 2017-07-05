8f046c824ecf6ef57e458f964b79c0bf392e7314
Nathan Reese <reese.nathan@gmail.com>
2017-07-05 12:24:50 -0600

    expose uiSettingDefaults on server so fieldFormats knows how to parse uiSettings

--- src/ui/field_formats_mixin.js
+++ src/ui/field_formats_mixin.js
@@ -1,5 +1,7 @@
 import { FieldFormatsService } from '../core_plugins/kibana/common/field_formats/field_formats';
+import { getUiSettingDefaults } from '../core_plugins/kibana/ui_setting_defaults';
 
 export function fieldFormatsMixin(kbnServer, server) {
+  server.decorate('server', 'uiSettingDefaults', getUiSettingDefaults());
   server.decorate('server', 'fieldFormats', new FieldFormatsService);
 }
