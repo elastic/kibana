7f0cfac4f4aa38d65340d3b78150e62847ee3cff
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-07-05 20:01:21 +0200

    Correctly handle array config values and expand flat objects within them. (#12662)

--- platform/config/ensureDeepObject.ts
+++ platform/config/ensureDeepObject.ts
@@ -1,29 +1,42 @@
 const separator = '.';
 
-export function ensureDeepObject(obj: any) {
+/**
+ * Recursively traverses through the object's properties and expands ones with
+ * dot-separated names into nested objects (eg. { a.b: 'c'} -> { a: { b: 'c' }).
+ * @param obj Object to traverse through.
+ * @returns Same object instance with expanded properties.
+ */
+export function ensureDeepObject(obj: any): any {
   if (obj == null || typeof obj !== 'object') {
     return obj;
   }
 
-  const fullObject = {} as any;
-  Object.keys(obj).forEach(k => {
-    if (!k.includes(separator)) {
-      fullObject[k] = ensureDeepObject(obj[k]);
+  if (Array.isArray(obj)) {
+    return obj.map((item) => ensureDeepObject(item));
+  }
+
+  return Object.keys(obj).reduce((fullObject, propertyKey) => {
+    const propertyValue = obj[propertyKey];
+    if (!propertyKey.includes(separator)) {
+      fullObject[propertyKey] = ensureDeepObject(propertyValue);
     } else {
-      walk(fullObject, k.split(separator), obj[k]);
+      walk(fullObject, propertyKey.split(separator), propertyValue);
     }
-  });
-  return fullObject;
+
+    return fullObject;
+  }, {} as any);
 }
 
 function walk(obj: any, keys: string[], value: any) {
   const key = keys.shift()!;
   if (keys.length === 0) {
     obj[key] = value;
-  } else {
-    if (obj[key] === undefined) {
-      obj[key] = {}
-    }
-    walk(obj[key], keys, ensureDeepObject(value));
+    return;
   }
+
+  if (obj[key] === undefined) {
+    obj[key] = {}
+  }
+
+  walk(obj[key], keys, ensureDeepObject(value));
 }
