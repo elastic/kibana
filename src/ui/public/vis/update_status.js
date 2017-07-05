e6bf076cc87491539eb4d9fe3e3302cf9046f2da
ppisljar <peter.pisljar@gmail.com>
2017-07-05 18:46:03 +0200

    fixing circular json issue

--- src/ui/public/vis/update_status.js
+++ src/ui/public/vis/update_status.js
@@ -1,7 +1,30 @@
 const oldValues = [];
 
+// adapted from https://github.com/isaacs/json-stringify-safe/blob/02cfafd45f06d076ac4bf0dd28be6738a07a72f9/stringify.js
+function serializer() {
+  const stack = [];
+  const keys = [];
+
+  const cycleReplacer = function (key, value) {
+    if (stack[0] === value) return '[Circular ~]';
+    return `[Circular ~.${keys.slice(0, stack.indexOf(value)).join('.')}]`;
+  };
+
+  return function (key, value) {
+    if (stack.length > 0) {
+      const thisPos = stack.indexOf(this);
+      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this);
+      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key);
+      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value);
+    }
+    else stack.push(value);
+
+    return value;
+  };
+}
+
 const hasChanged = (name, value) => {
-  const currentValue = JSON.stringify(value);
+  const currentValue = JSON.stringify(value, serializer());
   if (currentValue !== oldValues[name]) {
     oldValues[name] = currentValue;
     return false;
