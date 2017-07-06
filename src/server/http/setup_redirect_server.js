6e415ce03f6ddd009a9aa00756f9796406d97198
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2017-07-06 13:04:17 +0200

    Redirect from http to https

---
+++ src/server/http/setup_redirect_server.js
@@ -0,0 +1,40 @@
+import { format as formatUrl } from 'url';
+import Hapi from 'hapi';
+
+// If a redirect port is specified, we need to start a server a non-ssl
+// server at this port, and redirect all requests to the ssl port.
+export default function (kbnServer, server, config) {
+  const isSslEnabled = config.get('server.ssl.enabled');
+  const portToRedirectFrom = config.get('server.ssl.redirectHttpFromPort');
+
+  // Both ssl and port to redirect from must be specified
+  if (!isSslEnabled || portToRedirectFrom === undefined) {
+    return;
+  }
+
+  const host = config.get('server.host');
+  const sslPort = config.get('server.port');
+
+  const redirectServer = new Hapi.Server();
+
+  redirectServer.connection({
+    host,
+    port: portToRedirectFrom
+  });
+
+  redirectServer.ext('onRequest', (req, reply) => {
+    reply.redirect(formatUrl({
+      protocol: 'https',
+      hostname: host,
+      port: sslPort,
+      pathname: req.url.pathname,
+      search: req.url.search,
+    }));
+  });
+
+  redirectServer.start((err) => {
+    if (err) {
+      throw err;
+    }
+  });
+}
