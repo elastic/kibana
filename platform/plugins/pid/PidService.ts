d287ca9b69ea033c3e1ccb999e9d09072c8f39a5
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2017-07-07 18:21:01 +0200

    Cleaner Pid observable code (#12702)
    * Cleaner Pid observable code
    
    * remove unused arg

--- platform/plugins/pid/PidService.ts
+++ platform/plugins/pid/PidService.ts
@@ -6,17 +6,14 @@ import { PidFile } from './PidFile';
 import { LoggerFactory } from '../../logger';
 
 export class PidService {
-  private readonly pid$: Observable<PidFile | void>;
+  private readonly pid$: Observable<undefined>;
   private subscription?: Subscription;
 
-  constructor(pidConfig$: Observable<PidConfig | void>, logger: LoggerFactory) {
+  constructor(
+    pidConfig$: Observable<PidConfig | undefined>,
+    logger: LoggerFactory
+  ) {
     this.pid$ = pidConfig$
-      .map(
-        config =>
-          config !== undefined
-            ? new PidFile(process.pid, config, logger)
-            : undefined
-      )
       // Explanation of `switchMap`:
       // It's kinda like a normal `flatMap`, except it's producing observables
       // and you _only_ care about the latest observable it produced. It's
@@ -24,17 +21,19 @@ export class PidService {
       // and when you're done with an observable, like here where we want to
       // write the pid file we receive a pid config, and delete it when we
       // receive new config values (or when we stop the pid service).
-      .switchMap(pid => {
-        if (pid === undefined) {
-          // If pid is not specified, we return an observable that does nothing
-          return new Observable<PidFile | void>(noop);
+      .switchMap(config => {
+        if (config === undefined) {
+          // If there is no pid config we return an observable that does nothing
+          return new Observable(noop);
         }
 
         // Otherwise we return an observable that writes the pid when
         // subscribed to and deletes it when unsubscribed (e.g. if new config
         // is received or if `stop` is called below.)
 
-        return new Observable<PidFile | void>(observable => {
+        return new Observable(() => {
+          const pid = new PidFile(process.pid, config, logger);
+
           pid.writeFile();
 
           return function unsubscribe() {
