3db8e3a3c43e36806cc819440f0c88049dc37663
Brandon Kobel <brandon.kobel@gmail.com>
2017-07-10 15:15:04 -0400

    ES Healthcheck v6 mapping compatibility (#12714)
    * Beginning to update the healthcheck to use the SavedObjectsClient
    
    Some tests are still broken
    The sort method on the SavedObjectsClient isn't there yet
    
    * Adding sort to create_find_query
    
    * Fixing the tests
    
    * Fixing upgrade_config tests
    
    * Making the SavedObjectsClient be dependant on the mappings to enable
    sorting
    
    * Fixing disabled tests
    
    * Fiixng test wording
    
    * Passing the savedObjectsClient to the stats route handler
    
    * Passing the savedObjectsClient to upgradeConfig from migratConfig
    
    * Using array of keys with _.get instead of manual string concatenation

--- src/server/saved_objects/client/lib/__tests__/create_find_query.js
+++ src/server/saved_objects/client/lib/__tests__/create_find_query.js
@@ -1,14 +1,16 @@
 import expect from 'expect.js';
 import { createFindQuery } from '../create_find_query';
 
+const mappings = {};
+
 describe('createFindQuery', () => {
   it('matches all when there is no type or filter', () => {
-    const query = createFindQuery();
+    const query = createFindQuery(mappings);
     expect(query).to.eql({ query: { match_all: {} }, version: true });
   });
 
   it('adds bool filter for type', () => {
-    const query = createFindQuery({ type: 'index-pattern' });
+    const query = createFindQuery(mappings, { type: 'index-pattern' });
     expect(query).to.eql({
       query: {
         bool: {
@@ -27,7 +29,7 @@ describe('createFindQuery', () => {
   });
 
   it('can search across all fields', () => {
-    const query = createFindQuery({ search: 'foo' });
+    const query = createFindQuery(mappings, { search: 'foo' });
     expect(query).to.eql({
       query: {
         bool: {
@@ -45,7 +47,7 @@ describe('createFindQuery', () => {
   });
 
   it('can search a single field', () => {
-    const query = createFindQuery({ search: 'foo', searchFields: 'title' });
+    const query = createFindQuery(mappings, { search: 'foo', searchFields: 'title' });
     expect(query).to.eql({
       query: {
         bool: {
@@ -63,7 +65,7 @@ describe('createFindQuery', () => {
   });
 
   it('can search across multiple fields', () => {
-    const query = createFindQuery({ search: 'foo', searchFields: ['title', 'description'] });
+    const query = createFindQuery(mappings, { search: 'foo', searchFields: ['title', 'description'] });
     expect(query).to.eql({
       query: {
         bool: {
