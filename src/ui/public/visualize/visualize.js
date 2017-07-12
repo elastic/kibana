e330b2a2ddc0a5505206917fc0b88251f91f3278
Thomas Neirynck <thomas@elastic.co>
2017-07-12 12:21:36 -0400

    Revert "Remove dashboard refresh handler. (#12798)"
    This reverts commit 5b411426a8fbb279a12ff91b0cf7ad88c73e3b67.

--- src/ui/public/visualize/visualize.js
+++ src/ui/public/visualize/visualize.js
@@ -89,6 +89,8 @@ uiModules
       };
       $scope.vis.on('reload', reload);
       $scope.$on('courier:searchRefresh', reload);
+      // dashboard will trigger this event when refreshing
+      $scope.$on('fetch', reload);
 
       if ($scope.appState) {
         let oldUiState;
