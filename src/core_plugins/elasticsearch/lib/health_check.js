b3716d212516dacfad409fb0f8d3e4f0f829e1b4
Spencer <spalger@users.noreply.github.com>
2017-07-12 16:15:59 -0700

    [elasticsearch] patch mappings that are missing types (#12783)
    * [elasticsearch] patch mappings that are missing types
    
    * [elasticsearch/healthCheck] fix tests
    
    * fix doc typo
    
    * [tests/functional/dashboard] fix suite name
    
    * [es/healthCheck/ensureTypesExist] limit randomness a bit
    
    * [test/functional] update es archives with complete mappings
    
    (cherry picked from commit 929aa8efae20a19e38dee8462cee6682035db7cb)

--- src/core_plugins/elasticsearch/lib/health_check.js
+++ src/core_plugins/elasticsearch/lib/health_check.js
@@ -7,6 +7,7 @@ import kibanaVersion from './kibana_version';
 import { ensureEsVersion } from './ensure_es_version';
 import { ensureNotTribe } from './ensure_not_tribe';
 import { ensureAllowExplicitIndex } from './ensure_allow_explicit_index';
+import { ensureTypesExist } from './ensure_types_exist';
 
 const NoConnections = elasticsearch.errors.NoConnections;
 import util from 'util';
@@ -98,6 +99,12 @@ export default function (plugin, server, { mappings }) {
       .then(() => ensureNotTribe(callAdminAsKibanaUser))
       .then(() => ensureAllowExplicitIndex(callAdminAsKibanaUser, config))
       .then(waitForShards)
+      .then(() => ensureTypesExist({
+        callCluster: callAdminAsKibanaUser,
+        log: (...args) => server.log(...args),
+        indexName: config.get('kibana.index'),
+        types: Object.keys(mappings).map(name => ({ name, mapping: mappings[name] }))
+      }))
       .then(_.partial(migrateConfig, server, { mappings }))
       .then(() => {
         const tribeUrl = config.get('elasticsearch.tribe.url');
