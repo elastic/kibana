054cf5f773e79e8670d4de732beb82ffaada3040
spalger <spalger@users.noreply.github.com>
2017-07-12 20:50:54 -0700

    [ftr/mocha] assignment interceptors to mocha ui recursively

--- src/functional_test_runner/lib/mocha/decorate_mocha_ui.js
+++ src/functional_test_runner/lib/mocha/decorate_mocha_ui.js
@@ -105,16 +105,24 @@ export function decorateMochaUi(lifecycle, context) {
       return value;
     }
 
+    value = createAssignmentProxy(value, (subProperty, subValue) => {
+      return assignmentInterceptor(`${property}.${subProperty}`, subValue);
+    });
+
     switch (property) {
       case 'describe':
+      case 'describe.only':
       case 'xdescribe':
       case 'context':
+      case 'context.only':
       case 'xcontext':
         return wrapSuiteFunction(property, value);
 
       case 'it':
+      case 'it.only':
       case 'xit':
       case 'specify':
+      case 'specify.only':
       case 'xspecify':
         return wrapTestFunction(property, value);
 
