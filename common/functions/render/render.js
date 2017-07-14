9fa1835b66dd20e783c6c2c7f925f1b2654d3679
Rashid Khan <github.fliplap@spamgourmet.com>
2017-07-14 14:20:30 -0700

    Add ability to change plot type to render()

--- common/functions/render/render.js
+++ common/functions/render/render.js
@@ -11,13 +11,20 @@ module.exports = new Fn({
     ],
   },
   args: {
+    as: {
+      types: ['string', 'null'],
+      help: 'The element type to use in rendering. You probably want a specialized function instead, such as plot() or grid()',
+    },
     css: {
       types: ['string', 'null'],
+      default: '* > * {}',
       help: 'Any block of custom CSS to be scoped to this element.',
     },
   },
   fn: (context, args) => {
     context.css = args.css;
+    if (args.as) context.as = args.as;
+    console.log(context);
     return context;
   },
 });
