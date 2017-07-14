483857247551eb14c985a8e3d1475f8ce5702022
Court Ewing <court@epixa.com>
2017-07-14 13:40:44 -0400

    saved_objects: remove isSingleTypeError reference

--- src/server/saved_objects/client/lib/index.js
+++ src/server/saved_objects/client/lib/index.js
@@ -1,6 +1,6 @@
 export { createFindQuery } from './create_find_query';
 export { createIdQuery } from './create_id_query';
-export { handleEsError, isSingleTypeError } from './handle_es_error';
+export { handleEsError } from './handle_es_error';
 export { v5BulkCreate, v6BulkCreate } from './compatibility';
 export { normalizeEsDoc } from './normalize_es_doc';
 export { includedFields } from './included_fields';
