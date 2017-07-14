1cb65627a808e40f55d89a75c55a34cbc6317cdb
Rashid Khan <github.fliplap@spamgourmet.com>
2017-07-14 14:20:45 -0700

    Add new debug element

---
+++ public/elements/debug/debug.js
@@ -0,0 +1,19 @@
+import { Element } from '../element';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import header from './header.png';
+
+module.exports = new Element({
+  name: 'debug',
+  displayName: 'Debug',
+  description: 'Just dumps the configuration of the element',
+  image: header,
+  expression: 'demodata().render(as=debug)',
+  render(domNode, config, done) {
+    ReactDOM.render(
+      <div style={{ overflow: 'auto', height: '100%' }}>
+        <pre>{JSON.stringify(config, null, ' ')}</pre>
+      </div>, domNode);
+    done();
+  },
+});
--- public/elements/index.js
+++ public/elements/index.js
@@ -2,5 +2,6 @@ export const elementSpecs = [
   require('./plot/plot'),
   require('./table/table'),
   require('./image/image'),
+  require('./debug/debug'),
   require('./grid/grid'),
 ];
