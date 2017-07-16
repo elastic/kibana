f6d0b49404c9907cd63433e3b4d2b18d7a97fdbd
spalger <spalger@users.noreply.github.com>
2017-07-15 23:32:54 -0700

    [es/migrateConfig] use savedObjectClient factory

--- src/core_plugins/elasticsearch/lib/migrate_config.js
+++ src/core_plugins/elasticsearch/lib/migrate_config.js
@@ -1,11 +1,12 @@
 import upgrade from './upgrade_config';
-import { SavedObjectsClient } from '../../../server/saved_objects';
 
-export default async function (server, { mappings }) {
-  const config = server.config();
+export default async function (server) {
   const { callWithInternalUser } = server.plugins.elasticsearch.getCluster('admin');
 
-  const savedObjectsClient = new SavedObjectsClient(config.get('kibana.index'), mappings, callWithInternalUser);
+  const savedObjectsClient = server.savedObjectsClientFactory({
+    callCluster: callWithInternalUser
+  });
+
   const { saved_objects: configSavedObjects } = await savedObjectsClient.find({
     type: 'config',
     page: 1,
