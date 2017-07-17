683335fcaff4006f3e6107d3d4215037bcc530ae
CJ Cenizal <cj@cenizal.com>
2017-07-17 16:59:32 -0700

    Remove small and xSmall icon sizes.

--- ui_framework/doc_site/src/views/icon/icon_sizes.js
+++ ui_framework/doc_site/src/views/icon/icon_sizes.js
@@ -2,18 +2,6 @@ import React from 'react';
 
 export default () => (
   <div>
-    <svg className="kuiIcon kuiIcon--xSmall">
-      <use href="#search" />
-    </svg>
-
-    &nbsp;
-
-    <svg className="kuiIcon kuiIcon--small">
-      <use href="#search" />
-    </svg>
-
-    &nbsp;
-
     <svg className="kuiIcon kuiIcon--medium">
       <use href="#search" />
     </svg>
