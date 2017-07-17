15cc1b48c9b28da6712d1c8d483d362b422c6eb6
CJ Cenizal <cj@cenizal.com>
2017-07-17 11:54:33 -0700

    Add Icon component with SVG icon support. Add Search and User icons.

---
+++ ui_framework/doc_site/src/views/icon/icon_sizes.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <svg className="kuiIcon kuiIcon--xSmall">
+      <use href="#search" />
+    </svg>
+
+    &nbsp;
+
+    <svg className="kuiIcon kuiIcon--small">
+      <use href="#search" />
+    </svg>
+
+    &nbsp;
+
+    <svg className="kuiIcon kuiIcon--medium">
+      <use href="#search" />
+    </svg>
+
+    &nbsp;
+
+    <svg className="kuiIcon kuiIcon--large">
+      <use href="#search" />
+    </svg>
+
+    &nbsp;
+
+    <svg className="kuiIcon kuiIcon--xLarge">
+      <use href="#search" />
+    </svg>
+
+    &nbsp;
+
+    <svg className="kuiIcon kuiIcon--xxLarge">
+      <use href="#search" />
+    </svg>
+  </div>
+);
