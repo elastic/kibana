a7878ff84707dd6bcbc7f74251a575fe52cff3f9
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2017-07-18 17:27:18 +0200

    node 8 + npm 5 in new platform (#12828)
    * Change to Node 8 and npm 5
    
    * Commit package-lock file
    
    * node+npm info in readme
    
    * npm 5.3.0
    
    * Skip test for now, as it fails on Node 8

--- platform/lib/Errors/index.ts
+++ platform/lib/Errors/index.ts
@@ -1,10 +1,4 @@
-// TODO https://github.com/sindresorhus/clean-stack for cleaner stack
 export class KibanaError extends Error {
-  // `cause` lets us chain errors, e.g. so we can wrap underlying errors and
-  // get a "full" stack trace that includes the causes.
-  // TODO Handle stacks from `cause`, see e.g.
-  // - https://github.com/joyent/node-verror
-  // - https://github.com/bluejamesbond/TraceError.js
   cause?: Error;
 
   constructor(message: string, cause?: Error) {
