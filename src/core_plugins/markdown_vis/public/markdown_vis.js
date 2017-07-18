1de958f7bba244d9790ea7ab82cc4dca5ee5d5d3
ppisljar <peter.pisljar@gmail.com>
2017-07-18 09:54:38 +0200

    params to defaults

--- src/core_plugins/markdown_vis/public/markdown_vis.js
+++ src/core_plugins/markdown_vis/public/markdown_vis.js
@@ -26,7 +26,7 @@ function MarkdownVisProvider(Private) {
     category: CATEGORY.OTHER,
     visConfig: {
       template: markdownVisTemplate,
-      params: {
+      defaults: {
         fontSize: 12
       }
     },
