cdd5a1105348b33080eff3495adee60830aa7105
spalger <spalger@users.noreply.github.com>
2017-07-18 17:44:52 -0400

    [savedObjects/client/searchDsl] improve searchDsl tests

---
+++ src/server/saved_objects/client/lib/search_dsl/__tests__/query_params.js
@@ -0,0 +1,243 @@
+import expect from 'expect.js';
+
+import { getQueryParams } from '../query_params';
+
+const MAPPINGS = {
+  rootType: {
+    properties: {
+      type: {
+        type: 'keyword'
+      },
+      pending: {
+        properties: {
+          title: {
+            type: 'text',
+          }
+        }
+      },
+      saved: {
+        properties: {
+          title: {
+            type: 'text',
+            fields: {
+              raw: {
+                type: 'keyword'
+              }
+            }
+          },
+          obj: {
+            properties: {
+              key1: {
+                type: 'text'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+describe('searchDsl/queryParams', () => {
+  describe('{}', () => {
+    it('searches for everything', () => {
+      expect(getQueryParams(MAPPINGS))
+        .to.eql({});
+    });
+  });
+
+  describe('{type}', () => {
+    it('includes just a terms filter', () => {
+      expect(getQueryParams(MAPPINGS, 'saved'))
+        .to.eql({
+          query: {
+            bool: {
+              filter: [
+                {
+                  term: { type: 'saved' }
+                }
+              ]
+            }
+          }
+        });
+    });
+  });
+
+  describe('{search}', () => {
+    it('includes just a sqs query', () => {
+      expect(getQueryParams(MAPPINGS, null, 'us*'))
+        .to.eql({
+          query: {
+            bool: {
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'us*',
+                    all_fields: true
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+  });
+
+  describe('{type,search}', () => {
+    it('includes bool with sqs query and term filter for type', () => {
+      expect(getQueryParams(MAPPINGS, 'saved', 'y*'))
+        .to.eql({
+          query: {
+            bool: {
+              filter: [
+                { term: { type: 'saved' } }
+              ],
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'y*',
+                    all_fields: true
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+  });
+
+  describe('{search,searchFields}', () => {
+    it('includes sqs with field list', () => {
+      expect(getQueryParams(MAPPINGS, null, 'y*', ['title']))
+        .to.eql({
+          query: {
+            bool: {
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'y*',
+                    fields: [
+                      'pending.title',
+                      'saved.title'
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+    it('supports fields pointing to multi-fields', () => {
+      expect(getQueryParams(MAPPINGS, null, 'y*', ['title.raw']))
+        .to.eql({
+          query: {
+            bool: {
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'y*',
+                    fields: [
+                      'pending.title.raw',
+                      'saved.title.raw'
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+    it('supports multiple search fields', () => {
+      expect(getQueryParams(MAPPINGS, null, 'y*', ['title', 'title.raw']))
+        .to.eql({
+          query: {
+            bool: {
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'y*',
+                    fields: [
+                      'saved.title',
+                      'pending.title',
+                      'saved.title.raw',
+                      'pending.title.raw',
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+  });
+
+  describe('{type,search,searchFields}', () => {
+    it('includes bool, and sqs with field list', () => {
+      expect(getQueryParams(MAPPINGS, 'saved', 'y*', ['title']))
+        .to.eql({
+          query: {
+            bool: {
+              filter: [
+                { term: { type: 'saved' } }
+              ],
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'y*',
+                    fields: [
+                      'saved.title'
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+    it('supports fields pointing to multi-fields', () => {
+      expect(getQueryParams(MAPPINGS, 'saved', 'y*', ['title.raw']))
+        .to.eql({
+          query: {
+            bool: {
+              filter: [
+                { term: { type: 'saved' } }
+              ],
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'y*',
+                    fields: [
+                      'saved.title.raw'
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+    it('supports multiple search fields', () => {
+      expect(getQueryParams(MAPPINGS, 'saved', 'y*', ['title', 'title.raw']))
+        .to.eql({
+          query: {
+            bool: {
+              filter: [
+                { term: { type: 'saved' } }
+              ],
+              must: [
+                {
+                  simple_query_string: {
+                    query: 'y*',
+                    fields: [
+                      'saved.title',
+                      'saved.title.raw'
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        });
+    });
+  });
+});
--- src/server/saved_objects/client/lib/search_dsl/__tests__/search_dsl.js
+++ src/server/saved_objects/client/lib/search_dsl/__tests__/search_dsl.js
@@ -1,299 +1,77 @@
+import sinon from 'sinon';
 import expect from 'expect.js';
 import { getSearchDsl } from '../search_dsl';
-
-function createMappings(types = []) {
-  return {
-    rootType: {
-      properties: types
-        .reduce((acc, type) => ({
-          ...acc,
-          [type]: {
-            properties: {
-              name: {
-                type: 'keyword'
-              },
-              order: {
-                type: 'integer'
-              }
-            }
-          }
-        }), {})
-    }
-  };
-}
-
-function test({ mappings, params, expected }) {
-  expect(getSearchDsl(mappings, params)).to.eql(expected);
-}
+import * as queryParamsNS from '../query_params';
+import * as sortParamsNS from '../sorting_params';
 
 describe('getSearchDsl', () => {
-  describe('{}', () => {
-    it('matches all', () => {
-      test({
-        mappings: createMappings(['foo']),
-        params: {},
-        expected: {}
-      });
-    });
-  });
+  const sandbox = sinon.sandbox.create();
+  afterEach(() => sandbox.restore());
 
-  describe('{type}', () => {
-    it('includes type filter', () => {
-      test({
-        mappings: createMappings(['foo', 'bar']),
-        params: {
-          type: 'bar'
-        },
-        expected: {
-          query: {
-            bool: {
-              filter: [
-                { term: { type: 'bar' } }
-              ]
-            }
-          }
-        }
-      });
-    });
-  });
-
-  describe('{search}', () => {
-    it('includes a search across all fields on all types', () => {
-      test({
-        mappings: createMappings(['foo', 'bar']),
-        params: {
-          search: 'foo'
-        },
-        expected: {
-          query: {
-            bool: {
-              must: [{
-                simple_query_string: {
-                  query: 'foo',
-                  all_fields: true,
-                }
-              }]
-            }
-          },
-        }
-      });
-    });
-  });
-
-  describe('{sortOrder}', () => {
-    it('only accepts sort order when sorting on a field', () => {
+  describe('validation', () => {
+    it('throws when sortField is passed without type', () => {
       expect(() => {
-        test({
-          mappings: createMappings(['a']),
-          params: {
-            sortOrder: 'asc',
-          }
+        getSearchDsl({}, {
+          type: undefined,
+          sortField: 'title'
         });
-      }).to.throwError(error => {
-        expect(error).to.have.property('message').contain('requires a sortField');
-      });
+      }).to.throwException(/sort without .+ type/);
     });
-  });
-
-  describe('{sortField}', () => {
-    it('requires a type', () => {
+    it('throws when sortOrder without sortField', () => {
       expect(() => {
-        test({
-          mappings: createMappings(['foo', 'bar']),
-          params: {
-            sortOrder: 'asc',
-            sortField: 'name'
-          }
+        getSearchDsl({}, {
+          type: 'foo',
+          sortOrder: 'desc'
         });
-      }).to.throwError(error => {
-        expect(error).to.have.property('message').contain('without filtering by type');
-      });
+      }).to.throwException(/sortOrder requires a sortField/);
     });
   });
 
-  describe('{type,search}', () => {
-    it('includes search for all fields', () => {
-      test({
-        mappings: createMappings(['foo', 'bar']),
-        params: {
-          type: 'foo',
-          search: 'f*'
-        },
-        expected: {
-          query: {
-            bool: {
-              filter: [
-                { term: { type: 'foo' } }
-              ],
-              must: [
-                {
-                  simple_query_string: {
-                    query: 'f*',
-                    all_fields: true
-                  }
-                }
-              ]
-            }
-          }
-        }
-      });
-    });
-  });
+  describe('passes control', () => {
+    it('passes (mappings, type, search, searchFields) to getQueryParams', () => {
+      const spy = sandbox.spy(queryParamsNS, 'getQueryParams');
+      const mappings = { type: { properties: {} } };
+      const opts = {
+        type: 'foo',
+        search: 'bar',
+        searchFields: ['baz']
+      };
 
-  describe('{search,searchFields}', () => {
-    it('includes a search on a single field, on all types', () => {
-      test({
-        mappings: createMappings(['foo', 'bar']),
-        params: {
-          search: 'foo',
-          searchFields: ['title']
-        },
-        expected: {
-          query: {
-            bool: {
-              must: [{
-                simple_query_string: {
-                  query: 'foo',
-                  fields: [
-                    'foo.title',
-                    'bar.title',
-                  ]
-                }
-              }]
-            }
-          },
-        }
-      });
+      getSearchDsl(mappings, opts);
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWithExactly(
+        spy,
+        mappings,
+        opts.type,
+        opts.search,
+        opts.searchFields,
+      );
     });
-  });
 
-  describe('{search,searchFieldsx2}', () => {
-    it('includes a search on a single field, on all types', () => {
-      test({
-        mappings: createMappings(['foo', 'bar']),
-        params: {
-          search: 'foo',
-          searchFields: ['title', 'name']
-        },
-        expected: {
-          query: {
-            bool: {
-              must: [{
-                simple_query_string: {
-                  query: 'foo',
-                  fields: [
-                    'foo.title',
-                    'bar.title',
-                    'foo.name',
-                    'bar.name',
-                  ]
-                }
-              }]
-            }
-          },
-        }
-      });
+    it('passes (mappings, type, sortField, sortOrder) to getSortingParams', () => {
+      const spy = sandbox.stub(sortParamsNS, 'getSortingParams').returns({});
+      const mappings = { type: { properties: {} } };
+      const opts = {
+        type: 'foo',
+        sortField: 'bar',
+        sortOrder: 'baz'
+      };
+
+      getSearchDsl(mappings, opts);
+      sinon.assert.calledOnce(spy);
+      sinon.assert.calledWithExactly(
+        spy,
+        mappings,
+        opts.type,
+        opts.sortField,
+        opts.sortOrder,
+      );
     });
-  });
 
-  describe('{search,type}', () => {
-    it('includes search for all fields', () => {
-      test({
-        mappings: createMappings(['foo', 'bar']),
-        params: {
-          type: 'foo',
-          search: 'f*'
-        },
-        expected: {
-          query: {
-            bool: {
-              filter: [
-                { term: { type: 'foo' } }
-              ],
-              must: [
-                {
-                  simple_query_string: {
-                    query: 'f*',
-                    all_fields: true
-                  }
-                }
-              ]
-            }
-          }
-        }
-      });
-    });
-  });
-
-  describe('{search,searchFields,type}', () => {
-    it('includes search for specific fields', () => {
-      test({
-        mappings: createMappings(['foo', 'bar']),
-        params: {
-          type: 'bar',
-          search: 'f*',
-          searchFields: ['name^2', 'body']
-        },
-        expected: {
-          query: {
-            bool: {
-              filter: [
-                { term: { type: 'bar' } }
-              ],
-              must: [
-                {
-                  simple_query_string: {
-                    query: 'f*',
-                    fields: [
-                      'bar.name^2',
-                      'bar.body'
-                    ]
-                  }
-                }
-              ]
-            }
-          }
-        }
-      });
-    });
-  });
-
-  describe('{search,type,sortField,sortOrder}', () => {
-    it('includes sort with search on all fields', () => {
-      test({
-        mappings: createMappings(['baz', 'bar']),
-        params: {
-          type: 'baz',
-          search: 'f*',
-          sortField: 'name',
-          sortOrder: 'desc',
-        },
-        expected: {
-          sort: [
-            {
-              'baz.name': {
-                order: 'desc',
-                unmapped_type: 'keyword'
-              }
-            }
-          ],
-          query: {
-            bool: {
-              filter: [
-                { term: { type: 'baz' } }
-              ],
-              must: [
-                {
-                  simple_query_string: {
-                    query: 'f*',
-                    all_fields: true,
-                  }
-                }
-              ]
-            }
-          }
-        }
-      });
+    it('returns combination of getQueryParams and getSortingParams', () => {
+      sandbox.stub(queryParamsNS, 'getQueryParams').returns({ a: 'a' });
+      sandbox.stub(sortParamsNS, 'getSortingParams').returns({ b: 'b' });
+      expect(getSearchDsl({})).to.eql({ a: 'a', b: 'b' });
     });
   });
 });
