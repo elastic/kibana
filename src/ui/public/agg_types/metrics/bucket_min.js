0ee63e62499f24f0456195e8effce1030cf5218c
trevan <trevan@users.noreply.github.com>
2017-07-19 14:42:28 -0600

    Use the customMetric's formatter for pipeline aggregations (#11933)

--- src/ui/public/agg_types/metrics/bucket_min.js
+++ src/ui/public/agg_types/metrics/bucket_min.js
@@ -13,6 +13,7 @@ export function AggTypesMetricsBucketMinProvider(Private) {
     subtype: siblingPipelineHelper.subtype,
     params: [
       ...siblingPipelineHelper.params()
-    ]
+    ],
+    getFormat: siblingPipelineHelper.getFormat
   });
 }
