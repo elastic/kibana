6d6eecd19b04b230f7629bb17305e93fca13b4bc
Court Ewing <court@epixa.com>
2017-07-20 19:54:23 -0400

    Remove the es_admin proxy (#13000)
    Saved object access is now handled through the saved object client
    rather than by making direct requests to the admin cluster through a
    proxy. The indices module and scanner module were both still relying on
    the proxy, so temporary "legacy" endpoints have been created for their
    specific use-cases. We should add more purposeful endpoints to the
    public API in the future to replace these.
    
    The admin cluster itself is still readily available on the server and
    should still be used for all requests for internal kibana indices.

--- src/core_plugins/timelion/public/services/saved_sheets.js
+++ src/core_plugins/timelion/public/services/saved_sheets.js
@@ -13,8 +13,8 @@ savedObjectManagementRegistry.register({
 });
 
 // This is the only thing that gets injected into controllers
-module.service('savedSheets', function (Promise, SavedSheet, kbnIndex, esAdmin, kbnUrl, $http) {
-  const savedSheetLoader = new SavedObjectLoader(SavedSheet, kbnIndex, esAdmin, kbnUrl, $http);
+module.service('savedSheets', function (Promise, SavedSheet, kbnIndex, kbnUrl, $http) {
+  const savedSheetLoader = new SavedObjectLoader(SavedSheet, kbnIndex, kbnUrl, $http);
   savedSheetLoader.urlFor = function (id) {
     return kbnUrl.eval('#/{{id}}', { id: id });
   };
