dd4d6a34a90af0d282993cebf20c095c901d2407
Rashid Khan <github.fliplap@spamgourmet.com>
2017-07-21 14:39:29 -0700

    Remove unused event emitter

--- public/components/render_element/index.js
+++ public/components/render_element/index.js
@@ -1,19 +1,13 @@
 import { pure, compose, withProps, withState } from 'recompose';
 import { RenderElement as Component } from './render_element';
 import PropTypes from 'prop-types';
-import { Events } from '../../lib/events';
 
-const events = new Events();
 export const RenderElement = compose(
   pure,
   withProps(({ renderFn, done, config, size, css }) => ({
     renderFn: (domNode, events) => {
       renderFn(domNode, config, done || (() => {}), events);
     },
-    destroyFn: () => {
-      events.emit('destroy');
-    },
-    events: events,
     size,
     css,
   })),
@@ -23,7 +17,6 @@ export const RenderElement = compose(
 
 RenderElement.propTypes = {
   renderFn: PropTypes.func.isRequired,
-  destroyFn: PropTypes.func,
   done: PropTypes.func,
   config: PropTypes.object,
   size: PropTypes.object,
