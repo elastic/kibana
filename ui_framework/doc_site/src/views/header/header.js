feeee9fa7431d788737ee9725057bd26e972d972
dave.snider@gmail.com <dave.snider@gmail.com>
2017-07-21 16:38:00 -0700

    [K7]: Adds Pop Menu and Key Pad Menu css. Replace bemify. (#13037)
    Adds kuiPopMenu and kuiKeyPad menu systems and shows them in the header. Replaces bemify with our own system.

--- ui_framework/doc_site/src/views/header/header.js
+++ ui_framework/doc_site/src/views/header/header.js
@@ -1,1 +1,23 @@
 /* eslint-disable */
+
+let isPopoverCollapsed = true;
+let $toggleMenu = $('[data-id="toggleMenu"]')
+let $toggleButton = $('[data-id="toggleButton"]')
+
+$('[data-id="toggleButton"]').click(function() {
+
+  if (isPopoverCollapsed) {
+    $(this).addClass('isOpen');
+  } else {
+    $(this).removeClass('isOpen');
+  }
+
+  isPopoverCollapsed = !isPopoverCollapsed;
+});
+
+$(document).mouseup(function(e) {
+  if (!isPopoverCollapsed && !$toggleMenu.is(e.target) && $toggleMenu.has(e.target).length === 0) {
+    $toggleButton.removeClass('isOpen');
+    isPopoverCollapsed = !isPopoverCollapsed;
+  }
+});/* eslint-disable */
