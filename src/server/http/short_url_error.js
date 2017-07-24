d5f622fe65dacb1a78f2db7224d5dd55f0398270
Stacey Gammon <gammon@elastic.co>
2017-07-24 10:56:35 -0400

    Boom.wrap can't handle non Error instances, as exist in some of the tests.

--- src/server/http/short_url_error.js
+++ src/server/http/short_url_error.js
@@ -1,5 +1,8 @@
 import Boom from 'boom';
 
 export function handleShortUrlError(error) {
+  if (!error.isBoom && !(error instanceof Error)) {
+    return Boom.create(error.status || 500);
+  }
   return Boom.wrap(error, error.status || 500);
 }
