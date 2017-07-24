08a5f1b0fb58818f23ebcc80a481d038bedbc6b1
Chris Roberson <chrisronline@gmail.com>
2017-07-24 15:52:12 -0400

    [Management] Provide a way to fetch index pattern titles (#13030)
    * Refactor how `get_ids` works to enable fetching other properties of index patterns, specifically the `title` or name
    
    * Remove unnecessary lines
    
    * Only cache for `id` calls

--- src/ui/public/index_patterns/_get.js
+++ src/ui/public/index_patterns/_get.js
@@ -1,38 +1,46 @@
 import _ from 'lodash';
 import { SavedObjectsClientProvider } from 'ui/saved_objects';
 
-export function IndexPatternsGetIdsProvider(Private) {
+export function IndexPatternsGetProvider(Private) {
   const savedObjectsClient = Private(SavedObjectsClientProvider);
 
   // many places may require the id list, so we will cache it separately
   // didn't incorporate with the indexPattern cache to prevent id collisions.
-  let cachedPromise;
+  let cachedIdPromise;
 
-  const getIds = function () {
-    if (cachedPromise) {
+  const get = function (field) {
+    if (field === 'id' && cachedIdPromise) {
       // return a clone of the cached response
-      return cachedPromise.then(function (cachedResp) {
+      return cachedIdPromise.then(function (cachedResp) {
         return _.clone(cachedResp);
       });
     }
 
-    cachedPromise = savedObjectsClient.find({
+    const promise = savedObjectsClient.find({
       type: 'index-pattern',
       fields: [],
       perPage: 10000
     }).then(resp => {
-      return resp.savedObjects.map(obj => obj.id);
+      return resp.savedObjects.map(obj => _.get(obj, field));
     });
 
+    if (field === 'id') {
+      cachedIdPromise = promise;
+    }
+
     // ensure that the response stays pristine by cloning it here too
-    return cachedPromise.then(function (resp) {
+    return promise.then(function (resp) {
       return _.clone(resp);
     });
   };
 
-  getIds.clearCache = function () {
-    cachedPromise = null;
+  return (field) => {
+    const getter = get.bind(get, field);
+    if (field === 'id') {
+      getter.clearCache = function () {
+        cachedIdPromise = null;
+      };
+    }
+    return getter;
   };
-
-  return getIds;
 }
