d8b7aded3a0a84d2cc4d5de49a87889072ab0aad
Stacey Gammon <gammon@elastic.co>
2017-07-25 15:07:02 -0400

    remove inline awaits (#13043)
    * remove inline awaits
    
    * testSubjects.find now always returns a promise, so we need to make sure there is no more inlining
    
    * more find conversions
    
    * need to pass property name
    
    * Make sure async functions are awaited on.

--- test/functional/page_objects/settings_page.js
+++ test/functional/page_objects/settings_page.js
@@ -5,6 +5,7 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
   const retry = getService('retry');
   const config = getService('config');
   const remote = getService('remote');
+  const find = getService('find');
   const testSubjects = getService('testSubjects');
   const PageObjects = getPageObjects(['header', 'common']);
 
@@ -31,10 +32,9 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
       await this.clickLinkText('Index Patterns');
     }
 
-    getAdvancedSettings(propertyName) {
+    async getAdvancedSettings(propertyName) {
       log.debug('in setAdvancedSettings');
-      return testSubjects.find(`advancedSetting-${propertyName}-currentValue`)
-      .getVisibleText();
+      return await testSubjects.getVisibleText(`advancedSetting-${propertyName}-currentValue`);
     }
 
     async setAdvancedSettings(propertyName, propertyValue) {
@@ -62,53 +62,56 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
       await PageObjects.common.navigateToApp('settings');
     }
 
-    getIndexPatternField() {
-      return testSubjects.find('createIndexPatternNameInput');
+    async getIndexPatternField() {
+      return await testSubjects.find('createIndexPatternNameInput');
     }
 
-    getTimeFieldNameField() {
-      return testSubjects.find('createIndexPatternTimeFieldSelect');
+    async clickTimeFieldNameField() {
+      return await testSubjects.click('createIndexPatternTimeFieldSelect');
+    }
+
+    async getTimeFieldNameField() {
+      return await testSubjects.find('createIndexPatternTimeFieldSelect');
     }
 
     async selectTimeFieldOption(selection) {
       // open dropdown
-      (await this.getTimeFieldNameField()).click();
+      await this.clickTimeFieldNameField();
       // close dropdown, keep focus
-      (await this.getTimeFieldNameField()).click();
+      await this.clickTimeFieldNameField();
       await PageObjects.header.waitUntilLoadingHasFinished();
-      await retry.try(async () => {
+      return await retry.try(async () => {
         log.debug(`selectTimeFieldOption(${selection})`);
-        (await this.getTimeFieldOption(selection)).click();
-        const selected = (await this.getTimeFieldOption(selection)).isSelected();
+        const timeFieldOption = await this.getTimeFieldOption(selection);
+        await timeFieldOption.click();
+        const selected = await timeFieldOption.isSelected();
         if (!selected) throw new Error('option not selected: ' + selected);
       });
     }
 
-    getTimeFieldOption(selection) {
-      return remote.setFindTimeout(defaultFindTimeout)
-        .findDisplayedByCssSelector('option[label="' + selection + '"]');
+    async getTimeFieldOption(selection) {
+      return await find.displayedByCssSelector('option[label="' + selection + '"]');
     }
 
-    getCreateIndexPatternButton() {
-      return testSubjects.find('createIndexPatternCreateButton');
+    async getCreateIndexPatternButton() {
+      return await testSubjects.find('createIndexPatternCreateButton');
     }
 
-    getCreateButton() {
-      return remote.setFindTimeout(defaultFindTimeout)
-        .findDisplayedByCssSelector('[type="submit"]');
+    async getCreateButton() {
+      return await find.displayedByCssSelector('[type="submit"]');
     }
 
     async clickDefaultIndexButton() {
-      await testSubjects.find('setDefaultIndexPatternButton').click();
+      await testSubjects.click('setDefaultIndexPatternButton');
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
     async clickDeletePattern() {
-      await testSubjects.find('deleteIndexPatternButton').click();
+      await testSubjects.click('deleteIndexPatternButton');
     }
 
-    getIndexPageHeading() {
-      return testSubjects.find('indexPatternTitle');
+    async getIndexPageHeading() {
+      return await testSubjects.find('indexPatternTitle');
     }
 
     getConfigureHeader() {
@@ -151,14 +154,11 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
         );
     }
 
-    getFieldsTabCount() {
-      return retry.try(() => {
-        return testSubjects.find('tab-count-indexedFields')
-          .getVisibleText()
-          .then((theText) => {
-          // the value has () around it, remove them
-            return theText.replace(/\((.*)\)/, '$1');
-          });
+    async getFieldsTabCount() {
+      return await retry.try(async () => {
+        const text = await testSubjects.getVisibleText('tab-count-indexedFields');
+        // the value has () around it, remove them
+        return text.replace(/\((.*)\)/, '$1');
       });
     }
 
@@ -278,7 +278,8 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
         await this.clickKibanaIndices();
         await this.setIndexPatternField(indexPatternName);
         await this.selectTimeFieldOption(timefield);
-        await this.getCreateButton().click();
+        const createButton = await this.getCreateButton();
+        await createButton.click();
       });
       await PageObjects.header.waitUntilLoadingHasFinished();
       await retry.try(async () => {
@@ -305,8 +306,7 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
 
     async setIndexPatternField(pattern) {
       log.debug(`setIndexPatternField(${pattern})`);
-      return testSubjects.find('createIndexPatternNameInput')
-        .clearValue().type(pattern);
+      return await testSubjects.setValue('createIndexPatternNameInput', pattern);
     }
 
 
@@ -318,7 +318,7 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
       });
       await retry.try(async () => {
         log.debug('getAlertText');
-        alertText = await testSubjects.find('confirmModalBodyText').getVisibleText();
+        alertText = await testSubjects.getVisibleText('confirmModalBodyText');
       });
       await retry.try(async () => {
         log.debug('acceptConfirmation');
@@ -392,7 +392,7 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
 
     async setScriptedFieldName(name) {
       log.debug('set scripted field name = ' + name);
-      await testSubjects.find('editorFieldName').type(name);
+      await testSubjects.setValue('editorFieldName', name);
     }
 
     async setScriptedFieldLanguage(language) {
@@ -453,12 +453,12 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
 
     async setScriptedFieldPopularity(popularity) {
       log.debug('set scripted field popularity = ' + popularity);
-      await testSubjects.find('editorFieldCount').type(popularity);
+      await testSubjects.setValue('editorFieldCount', popularity);
     }
 
     async setScriptedFieldScript(script) {
       log.debug('set scripted field script = ' + script);
-      await testSubjects.find('editorFieldScript').type(script);
+      await testSubjects.setValue('editorFieldScript', script);
     }
   }
 
