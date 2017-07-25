b0ffd80c99237e6d8d13a7af558c24487a046e87
CJ Cenizal <cj@cenizal.com>
2017-07-25 10:14:02 -0700

    Update eslint config to lint the new UI Framework directory structure. (#13102)

--- ui_framework/src/components/typography/typography.test.js
+++ ui_framework/src/components/typography/typography.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { render, shallow } from 'enzyme';
-import sinon from 'sinon';
+import { render } from 'enzyme';
 import { requiredProps } from '../../test/required_props';
 
 import {
