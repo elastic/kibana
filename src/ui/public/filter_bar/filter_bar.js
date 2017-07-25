5a2ee2c3465681812d0cd50533d4edb3d19dbafb
Nathan Reese <reese.nathan@gmail.com>
2017-07-25 14:01:31 -0600

    use existing newFilters logic and GUI in filter_bar

--- src/ui/public/filter_bar/filter_bar.js
+++ src/ui/public/filter_bar/filter_bar.js
@@ -14,6 +14,18 @@ import { uiModules } from 'ui/modules';
 
 export { disableFilter, enableFilter, toggleFilterDisabled } from './lib/disable_filter';
 
+const MULTI_SELECT_KEY = 18; // alt
+let isMultiSelect = false;
+window.addEventListener('keydown', (event) => {
+  if (event.keyCode === MULTI_SELECT_KEY) {
+    isMultiSelect = true;
+  }
+}, false);
+window.addEventListener('keyup', async (event) => {
+  if (event.keyCode === MULTI_SELECT_KEY) {
+    isMultiSelect = false;
+  }
+}, false);
 
 const module = uiModules.get('kibana');
 
@@ -48,13 +60,14 @@ module.directive('filterBar', function (Private, Promise, getAppState) {
       });
 
       $scope.state = getAppState();
+      $scope.newFilters = [];
 
       $scope.showAddFilterButton = () => {
         return _.compact($scope.indexPatterns).length > 0;
       };
 
       $scope.applyFilters = function (filters) {
-        addAndInvertFilters(filterAppliedAndUnwrap(filters));
+        addAndInvertFilters(mergeFilters(filterAppliedAndUnwrap(filters)));
         $scope.newFilters = [];
 
         // change time filter
@@ -104,12 +117,13 @@ module.directive('filterBar', function (Private, Promise, getAppState) {
       });
 
       $scope.$watch('state.$newFilters', function (filters) {
-        if (!filters) return;
+        if (!filters || filters.length === 0) return;
 
-        // If filters is not undefined and the length is greater than
-        // one we need to set the newFilters attribute and allow the
-        // users to decide what they want to apply.
-        if (filters.length > 1) {
+        // Allow the users to decide what filters to apply when
+        // - more than one new filter getting added
+        // - new filters exist that have not been applied
+        // - multiselect is enabled
+        if (filters.length > 1 || $scope.newFilters.length > 0 || isMultiSelect) {
           return mapFlattenAndWrapFilters(filters)
           .then(function (results) {
             extractTimeFilter(results).then(function (filter) {
@@ -119,7 +133,7 @@ module.directive('filterBar', function (Private, Promise, getAppState) {
           })
           .then(filterOutTimeBasedFilter)
           .then(function (results) {
-            $scope.newFilters = results;
+            $scope.newFilters = $scope.newFilters.concat(results);
           });
         }
 
@@ -163,6 +177,69 @@ module.directive('filterBar', function (Private, Promise, getAppState) {
           });
           $scope.$emit('filterbar:updated');
         });
+      }
+
+      function mergeFilters(filters) {
+        // Group filters by key
+        const filterGroups = new Map();
+        const filtersWithNoKey = [];
+        filters.forEach((filter) => {
+          const key = _.get(filter, 'meta.key');
+          if (!key) {
+            filtersWithNoKey.push(filter);
+            return;
+          }
+
+          if (!filterGroups.has(key)) {
+            filterGroups.set(key, [filter]);
+          } else {
+            filterGroups.set(key, filterGroups.get(key).concat(filter));
+          }
+        });
+
+        const filtersWithKey = [];
+        filterGroups.forEach((filterGroup, key) => {
+          if (filterGroup.length === 1) {
+            // Key with only one filter
+            filtersWithKey.push(filterGroup[0]);
+          } else {
+            const values = [];
+            const shouldFilters = [];
+            const mustNotFilters = [];
+            filterGroup.forEach((filter) => {
+              const value = _.get(filter, 'meta.value', '');
+              if (_.get(filter, 'meta.negate', false)) {
+                values.push('!' + value);
+                mustNotFilters.push(cleanFilter(filter));
+              } else {
+                values.push(value);
+                shouldFilters.push(cleanFilter(filter));
+              }
+            });
+
+            // Key with multiple filters - combine into OR query
+            filtersWithKey.push({
+              bool: {
+                should: shouldFilters,
+                must_not: mustNotFilters
+              },
+              meta: {
+                alias: `${key}: ${values.join()}`
+              }
+            });
+          }
+        });
+
+        return filtersWithKey.concat(filtersWithNoKey);
+      }
+
+      function cleanFilter(filter) {
+        if (_.has(filter, 'query')) {
+          return filter.query;
+        }
+
+        delete filter.meta;
+        return filter;
       }
 
       updateFilters();
