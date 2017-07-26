26321643d7601e169903dc928ae1ef38569d4576
Rashid Khan <github.fliplap@spamgourmet.com>
2017-07-26 13:07:28 -0700

    Add size model arg

--- public/expression_types/views/plot.js
+++ public/expression_types/views/plot.js
@@ -3,6 +3,6 @@ import { View } from '../view';
 export const plot = () => new View('plot', {
   displayName: 'Plot Chart',
   description: 'Show your data, as plots',
-  modelArgs: ['x', 'y', 'color'],
+  modelArgs: ['x', 'y', 'color', 'size'],
   args: [],
 });
