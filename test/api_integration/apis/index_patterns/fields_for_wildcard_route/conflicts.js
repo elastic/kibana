7048fce62619837679a60cbe7d9398e4d6135351
Chris Roberson <chrisronline@gmail.com>
2017-07-27 15:33:22 -0400

    Add functional test to ensure similar ES types are properly merged

--- test/api_integration/apis/index_patterns/fields_for_wildcard_route/conflicts.js
+++ test/api_integration/apis/index_patterns/fields_for_wildcard_route/conflicts.js
@@ -14,31 +14,54 @@ export default function ({ getService }) {
         .query({ pattern: 'logs-*' })
         .expect(200)
         .then(resp => {
-          expect(resp.body).to.eql({
-            fields: [
-              {
-                name: '@timestamp',
-                type: 'date',
-                aggregatable: true,
-                searchable: true,
-                readFromDocValues: true,
-              },
-              {
-                name: 'success',
-                type: 'conflict',
-                aggregatable: true,
-                searchable: true,
-                readFromDocValues: false,
-                conflictDescriptions: {
-                  boolean: [
-                    'logs-2017.01.02'
-                  ],
-                  keyword: [
-                    'logs-2017.01.01'
-                  ]
-                }
-              }
-            ]
+          const dateField = resp.body.fields.find(f => f.type === 'date');
+          const successField = resp.body.fields.find(f => f.type === 'conflict');
+          expect(dateField).to.eql({
+            name: '@timestamp',
+            type: 'date',
+            aggregatable: true,
+            searchable: true,
+            readFromDocValues: true,
+          });
+          expect(successField).to.eql({
+            name: 'success',
+            type: 'conflict',
+            aggregatable: true,
+            searchable: true,
+            readFromDocValues: false,
+            conflictDescriptions: {
+              boolean: [
+                'logs-2017.01.02'
+              ],
+              keyword: [
+                'logs-2017.01.01'
+              ]
+            }
+          });
+        })
+    ));
+
+    it('does not mark mismatched types as conflicted if they resolve to the same kibana type', () => (
+      supertest
+        .get('/api/index_patterns/_fields_for_wildcard')
+        .query({ pattern: 'logs-*' })
+        .expect(200)
+        .then(resp => {
+          const resolvedToString = resp.body.fields.find(f => f.type === 'string');
+          const resolvedToNumber = resp.body.fields.find(f => f.type === 'number');
+          expect(resolvedToString).to.eql({
+            name: 'string_conflict',
+            type: 'string',
+            aggregatable: true,
+            searchable: true,
+            readFromDocValues: false,
+          });
+          expect(resolvedToNumber).to.eql({
+            name: 'number_conflict',
+            type: 'number',
+            aggregatable: true,
+            searchable: true,
+            readFromDocValues: true,
           });
         })
     ));
