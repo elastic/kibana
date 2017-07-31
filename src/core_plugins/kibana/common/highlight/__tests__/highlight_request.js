c487720d9a630d1ea01e71a7bef2c90b4f7e3d1a
Lukas Olson <olson.lukas@gmail.com>
2017-07-31 16:49:51 -0700

    Fix tests

--- src/core_plugins/kibana/common/highlight/__tests__/highlight_request.js
+++ src/core_plugins/kibana/common/highlight/__tests__/highlight_request.js
@@ -5,22 +5,6 @@ describe('getHighlightRequest', () => {
   let configMock;
   const getConfig = (key) => configMock[key];
   const queryStringQuery = { query_string: { query: 'foo' } };
-  const queryStringWithDefaultFieldQuery = { query_string: { query: 'foo', default_field: 'bar' } };
-  const queryStringWithFieldQuery = { query_string: { query: 'foo', fields: ['bar'] } };
-  const rangeQuery = { range: { '@timestamp': { gte: 0, lte: 0 } } };
-  const boolQueryWithSingleCondition = {
-    bool: {
-      must: queryStringQuery,
-    }
-  };
-  const boolQueryWithMultipleConditions = {
-    bool: {
-      must: [
-        queryStringQuery,
-        rangeQuery
-      ]
-    }
-  };
 
   beforeEach(function () {
     configMock = {};
@@ -37,33 +21,10 @@ describe('getHighlightRequest', () => {
     expect(queryStringQuery.query_string).to.not.have.property('highlight');
   });
 
-  it('should add the default_field param with bool query with single condition without modifying original query', () => {
-    const request = getHighlightRequest(boolQueryWithSingleCondition, getConfig);
-    expect(request.fields['*']).to.have.property('highlight_query');
-    expect(request.fields['*'].highlight_query.bool.must.query_string).to.have.property('default_field');
-    expect(request.fields['*'].highlight_query.bool.must.query_string.default_field).to.be('*');
-    expect(queryStringQuery.query_string).to.not.have.property('default_field');
-  });
-
-  it('should add the default_field param with bool query with multiple conditions without modifying original query', () => {
-    const request = getHighlightRequest(boolQueryWithMultipleConditions, getConfig);
-    expect(request.fields['*']).to.have.property('highlight_query');
-    expect(request.fields['*'].highlight_query.bool.must).to.have.length(boolQueryWithMultipleConditions.bool.must.length);
-    expect(request.fields['*'].highlight_query.bool.must[0].query_string).to.have.property('default_field');
-    expect(request.fields['*'].highlight_query.bool.must[0].query_string.default_field).to.be('*');
-    expect(queryStringQuery.query_string).to.not.have.property('default_field');
-  });
-
   it('should return undefined if highlighting is turned off', () => {
     configMock['doc_table:highlight'] = false;
     const request = getHighlightRequest(queryStringQuery, getConfig);
     expect(request).to.be(undefined);
-  });
-
-  it('should not add the highlight_query param if all_fields is turned off', () => {
-    configMock['doc_table:highlight:all_fields'] = false;
-    const request = getHighlightRequest(queryStringQuery, getConfig);
-    expect(request.fields['*']).to.not.have.property('highlight_query');
   });
 
   it('should enable/disable highlighting if config is changed', () => {
@@ -73,22 +34,5 @@ describe('getHighlightRequest', () => {
     configMock['doc_table:highlight'] = false;
     request = getHighlightRequest(queryStringQuery, getConfig);
     expect(request).to.be(undefined);
-  });
-
-  it('should not modify the default_field param with query_string query when default_field is specified', () => {
-    const defaultField = queryStringWithDefaultFieldQuery.query_string.default_field;
-    const request = getHighlightRequest(queryStringWithDefaultFieldQuery, getConfig);
-    expect(request.fields['*']).to.have.property('highlight_query');
-    expect(request.fields['*'].highlight_query.query_string).to.have.property('default_field');
-    expect(request.fields['*'].highlight_query.query_string.default_field).to.be(defaultField);
-    expect(queryStringWithDefaultFieldQuery.query_string.default_field).to.be(defaultField);
-  });
-
-  it('should not add the default_field param with query_string query when fields are specified', () => {
-    const request = getHighlightRequest(queryStringWithFieldQuery, getConfig);
-    expect(request.fields['*']).to.have.property('highlight_query');
-    expect(request.fields['*'].highlight_query.query_string).to.have.property('fields');
-    expect(request.fields['*'].highlight_query.query_string).to.not.have.property('default_field');
-    expect(queryStringQuery.query_string).to.not.have.property('default_field');
   });
 });
