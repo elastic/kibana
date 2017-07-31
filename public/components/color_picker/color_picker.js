19b16bd76e142f7e453dcbfac2847b1e404392fe
Rashid Khan <github.fliplap@spamgourmet.com>
2017-07-31 13:35:33 -0700

    Improve look and feel of color picker

--- public/components/color_picker/color_picker.js
+++ public/components/color_picker/color_picker.js
@@ -10,27 +10,45 @@ export const ColorPicker = ({ value, colors, colorsPerRow, onChange }) => {
   colors = colors || [];
   colorsPerRow = colorsPerRow || 6;
 
-  let selectColor;
-  try {
-    selectColor = chroma.contrast(value, '#000') < 4.5 ? 'rgba(255,255,255,0.5)' : 'rgba(0,0,0,0.5)';
-  } catch (e) {
-    selectColor = 'rgba(0,0,0,0.5)';
-  }
-
   const table = colors.reduce((rows, color) => {
     if (last(rows).length >= colorsPerRow) rows.push([]);
 
-    const classNames = ['fa fa-stack-2x'];
-    classNames.push(color === 'rgba(0,0,0,0)' ? 'fa fa-ban' : 'fa fa-circle');
+    let fontColor;
+    try {
+      fontColor = chroma.contrast(value, '#000') < 4.5 ? '#FFF' : '#333';
+    } catch (e) {
+      fontColor = '#333';
+    }
 
-    const fontColor = color === 'rgba(0,0,0,0)' ? '#000000' : color;
+    const background = color === 'rgba(0,0,0,0)' ?
+    {
+      backgroundImage: `
+        linear-gradient(45deg, #aaa 25%, transparent 25%),
+        linear-gradient(-45deg, #aaa 25%, transparent 25%),
+        linear-gradient(45deg, transparent 75%, #aaa 75%),
+        linear-gradient(-45deg, transparent 75%, #aaa 75%)`,
+      backgroundSize: '8px 8px',
+    } :
+    { background: color };
+
     const dot = (
-      <span key={color} className="fa-stack" onClick={() => onChange(color)}>
-        <i className={ classNames.join(' ')} style={{ color: fontColor }}/>
-        { value !== color ? null :
-          (<i className="fa fa-circle-o fa-stack-2x" style={{ color: selectColor }}/>)
-        }
-      </span>
+      <div
+      onClick={() => onChange(color)}
+      className="canvas__color-picker--dot"
+      style={Object.assign({
+        display: 'inline-block',
+        height: 25,
+        width: 25,
+        borderRadius: 15,
+        margin: '0px 5px 5px 0px',
+        border: '2px solid #666',
+      },
+        background,
+      )}>
+      { color !== value ? <i className="fa"/> :
+        <i className="fa fa-check" style={{ color: fontColor }}/>
+      }
+      </div>
     );
 
     last(rows).push(dot);
@@ -49,4 +67,6 @@ ColorPicker.propTypes = {
   colors: PropTypes.array,
   colorsPerRow: PropTypes.number,
   onChange: PropTypes.func,
+  addColor: PropTypes.func,
+  removeColor: PropTypes.func,
 };
