a6633c37f4a5b0b6ade6700c6650b7bb08e7deab
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-01 15:46:39 -0700

    Change seriesStyle to allow for color and label selection

--- public/expression_types/arg_types/series_style/series_style.js
+++ public/expression_types/arg_types/series_style/series_style.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, FormGroup, ControlLabel } from 'react-bootstrap';
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import { get } from 'lodash';
 import { set, del } from 'object-path-immutable';
-import { LabeledInput } from './labeled_input';
+import { ColorPickerMini } from '../../../components/color_picker_mini';
 import { LabeledSelect } from './labeled_select';
 import { ArgType } from '../../arg_type';
 import './series_style.less';
 
 const template = (props) => {
-  const { typeInstance, onValueChange, argValue } = props;
+  const { typeInstance, onValueChange, labels, argValue } = props;
   const chain = get(argValue, 'chain.0', {});
   const chainArgs = get(chain, 'arguments', {});
   const { name, displayName } = typeInstance;
@@ -24,23 +24,42 @@ const template = (props) => {
     return onValueChange(newValue);
   };
 
+  const handlePlain = (argName, val) => handleChange(argName, { target: { value: val } });
+
   // TODO: add fill and stack options
   // TODO: add label name auto-complete
+  const labelName = get(chainArgs, 'label.0.value', '');
+
   return (
-    <Form inline onSubmit={() => false}>
+    <Form onSubmit={() => false}>
       <FormGroup controlId="formControlsSelect">
-        <ControlLabel>{ displayName }</ControlLabel>
+        <ControlLabel>{displayName}</ControlLabel>
         <div className="canvas__argtype--seriesStyle">
           {name !== 'defaultStyle' &&
-            (<LabeledInput label="Series Label" argName="label" value={get(chainArgs, 'label.0.value', '')} onChange={handleChange} />)
+            (
+              <FormControl
+                componentClass="select"
+                placeholder="Select Series"
+                value={labelName}
+                onChange={(ev) => handleChange('label', ev)}
+              >
+                <option value={null} disabled>Select a Series Label</option>
+                { labels.map(label => <option key={label} value={label}>{label}</option>) }
+              </FormControl>
+            )
           }
-          <LabeledInput label="Color" argName="color" value={get(chainArgs, 'color.0.value', '')} onChange={handleChange} />
-
-          <div className="canvas__argtype--seriesStyle--plotStyle">
+          <div className="canvas__argtype--seriesStyle--properties">
             <LabeledSelect label="Line" argName="lines" value={get(chainArgs, 'lines.0.value', 0)} onChange={handleChange} />
             <LabeledSelect label="Bar" argName="bars" value={get(chainArgs, 'bars.0.value', 0)} onChange={handleChange} />
             <LabeledSelect label="Point" argName="points" value={get(chainArgs, 'points.0.value', 0)} onChange={handleChange} />
+            <div className="canvas__argtype--seriesStyle--color">
+              <label>Color</label>
+              <ColorPickerMini
+                value={get(chainArgs, 'color.0.value', '')}
+                onChange={(val) => handlePlain('color', val)} />
+            </div>
           </div>
+
       </div>
       </FormGroup>
     </Form>
@@ -51,6 +70,7 @@ template.propTypes = {
   onValueChange: PropTypes.func.isRequired,
   argValue: PropTypes.object.isRequired,
   typeInstance: PropTypes.object,
+  labels: PropTypes.array,
 };
 
 export const seriesStyle = () => new ArgType('seriesStyle', {
