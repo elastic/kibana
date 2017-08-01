db1451917376b7b6c1ef865307fd426535aef101
Joe Fleming <joe.fleming@gmail.com>
2017-08-01 09:47:01 -0700

    fix: use webpack shims for jquery flot
    should stop conflicts with other plugins that use flot

--- src/core_plugins/timelion/public/webpackShims/jquery-flot.js
+++ src/core_plugins/timelion/public/webpackShims/jquery-flot.js
@@ -1,7 +1,16 @@
+const $ = require('jquery');
+
+if (window) window.jQuery = $;
+
+// load flot
 require('jquery.flot');
+
+// load flot plugins
 require('jquery.flot.time');
 require('jquery.flot.symbol');
 require('jquery.flot.crosshair');
 require('jquery.flot.selection');
 require('jquery.flot.stack');
 require('jquery.flot.axislabels');
+
+module.exports = $;
