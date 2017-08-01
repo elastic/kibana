91fc133b0ba91acd8c328f016698f07d7186ea03
Trevan Richins <trichins@eastwindnetworks.com>
2017-08-01 08:45:24 -0600

    Fix functional tests

--- test/functional/page_objects/visualize_page.js
+++ test/functional/page_objects/visualize_page.js
@@ -215,10 +215,10 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
       await Promise.all(getChartTypesPromises);
     }
 
-    async selectAggregation(myString) {
+    async selectAggregation(myString, groupName = 'buckets') {
       await retry.try(async () => {
-        await find.click('visEditorAggSelect');
-        const input = await find.byCssSelector(`input.ui-select-search`);
+        await find.clickByCssSelector(`[group-name="${groupName}"] .agg-select`);
+        const input = await find.byCssSelector(`[group-name="${groupName}"] .agg-select input.ui-select-search`);
         await input.type(myString);
         await remote.pressKeys('\uE006');
       });
@@ -232,8 +232,8 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
 
     async selectField(fieldValue, groupName = 'buckets') {
       await retry.try(async () => {
-        await find.clickByCssSelector(`[group-name="${groupName}"] .ui-select-container`);
-        const input = await find.byCssSelector(`[group-name="${groupName}"] input.ui-select-search`);
+        await find.clickByCssSelector(`[group-name="${groupName}"] .field-select`);
+        const input = await find.byCssSelector(`[group-name="${groupName}"] .field-select input.ui-select-search`);
         await input.type(fieldValue);
         await remote.pressKeys('\uE006');
       });
