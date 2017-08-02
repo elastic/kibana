32ba30da229c88866ed856be8f4af0540e7300f2
CJ Cenizal <cj@cenizal.com>
2017-08-02 08:30:50 -0700

    [UI Framework] [K7] Add KuiTable component. (#13249)
    * Add KuiTable component with compressed prop.

---
+++ ui_framework/src/components/table/table_row.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from 'enzyme';
+import { requiredProps } from '../../test/required_props';
+
+import {
+  KuiTableRow,
+} from './table_row';
+
+import {
+  KuiTableRowCell
+} from './table_row_cell';
+
+test('renders KuiTableRow', () => {
+  const component = (
+    <KuiTableRow { ...requiredProps }>
+      <KuiTableRowCell>hi</KuiTableRowCell>
+    </KuiTableRow>
+  );
+
+  expect(render(component)).toMatchSnapshot();
+});
+
+describe('isSelected', () => {
+  test('renders true when specified', () => {
+    const component = (
+      <KuiTableRow isSelected={true}>
+        <KuiTableRowCell />
+      </KuiTableRow>
+    );
+
+    expect(render(component)).toMatchSnapshot();
+  });
+});
--- ui_framework/src/services/index.js
+++ ui_framework/src/services/index.js
@@ -5,5 +5,14 @@ export {
   SPACE_KEY,
 } from './accessibility';
 
-export { SortableProperties } from './sort';
-export { ESC_KEY_CODE } from './key_codes';
+export {
+  SortableProperties,
+} from './sort';
+export {
+  ESC_KEY_CODE,
+} from './key_codes';
+
+export {
+  LEFT_ALIGNMENT,
+  RIGHT_ALIGNMENT,
+} from './alignment';
