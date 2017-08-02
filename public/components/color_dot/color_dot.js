f7ebc7efe26e5e2b207e3e9b6c9742285f7a354a
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-02 13:27:31 -0700

    Remove comments

--- public/components/color_dot/color_dot.js
+++ public/components/color_dot/color_dot.js
@@ -4,19 +4,6 @@ import './color_dot.less';
 
 
 export const ColorDot = ({ value, children }) => {
-  /*
-  const background = color === 'rgba(255,255,255,0)' ?
-  {
-    backgroundImage: `
-      linear-gradient(45deg, #aaa 25%, transparent 25%),
-      linear-gradient(-45deg, #aaa 25%, transparent 25%),
-      linear-gradient(45deg, transparent 75%, #aaa 75%),
-      linear-gradient(-45deg, transparent 75%, #aaa 75%)`,
-    backgroundSize: '8px 8px',
-  } :
-  { background: color };
-  */
-
   return (
     <div className="canvas__color-dot">
       <div className="canvas__color-dot--background canvas__checkered"/>
