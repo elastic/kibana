4ab70722dd8d90a2a0ebd1b14e4dcefb5d28b4c8
Stacey Gammon <gammon@elastic.co>
2017-08-03 17:37:24 -0400

    no need for async or Promise.reject on interface

--- src/ui/public/embeddable/embeddable_handler.js
+++ src/ui/public/embeddable/embeddable_handler.js
@@ -8,7 +8,7 @@ export class EmbeddableHandler {
    * @return {Promise.<string>} a promise that resolves with the path that dictates where the user will be navigated to
    * when they click the edit icon.
    */
-  async getEditPath(/* panelId */) {
+  getEditPath(/* panelId */) {
     throw new Error('Must implement getEditPath.');
   }
 
@@ -16,7 +16,7 @@ export class EmbeddableHandler {
    * @param {string} panelId - the id of the panel to grab the title for.
    * @return {Promise.<string>} - Promise that resolves with the title to display for the particular panel.
    */
-  async getTitleFor(/* panelId */) {
+  getTitleFor(/* panelId */) {
     throw new Error('Must implement getTitleFor.');
   }
 
@@ -27,7 +27,7 @@ export class EmbeddableHandler {
    * @property {ContainerApi} containerApi - an id to specify the object that this panel contains.
    * @param {Promise.<void>} A promise that resolves when the object is finished rendering.
    */
-  async render(/* domNode, panel, container */) {
-    Promise.reject(new Error('Must implement render.'));
+  render(/* domNode, panel, container */) {
+    throw new Error('Must implement render.');
   }
 }
