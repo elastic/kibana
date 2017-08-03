e34307c68f25fb7a63e43e57b35d4409d49a1abd
Dave Snider <dave.snider@gmail.com>
2017-08-03 09:42:20 -0700

    clean up code, set up proper state

--- ui_framework/src/components/side_nav/side_nav.js
+++ ui_framework/src/components/side_nav/side_nav.js
@@ -4,22 +4,29 @@ import React, {
 import classNames from 'classnames';
 import { KuiIcon } from '../../components';
 
-export const KuiSideNav = ({ children, className, ...rest }) => {
-  const classes = classNames('kuiSideNav', className);
+export const KuiSideNav = ({ children, isOpenMobile, className, ...rest }) => {
+  const classes = classNames(
+    'kuiSideNav',
+    className,
+    {
+      'isOpenMobile': isOpenMobile,
+    },
+  );
 
   return (
     <nav
       className={classes}
       {...rest}
     >
-      <div className="kuiSideNav__mobileToggle">
-        <div className="kuiSideNav__mobileTitle">Navigate within management</div>
-        <div className="kuiSideNav__mobileIconWrap"><KuiIcon className="kuiSideNav__mobileIcon" type="apps" size="medium"/></div>
-      </div>
+      <button className="kuiSideNav__mobileToggle kuiLink">
+        <span className="kuiSideNav__mobileTitle">Navigate within management</span>
+        <span><KuiIcon className="kuiSideNav__mobileIcon" type="apps" size="medium"/></span>
+      </button>
       <div className="kuiSideNav__content">{children}</div>
     </nav>
   );
 };
 
 KuiSideNav.propTypes = {
+  isOpenMobile: PropTypes.string,
 };
