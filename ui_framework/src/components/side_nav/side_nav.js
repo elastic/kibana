1d9a03e5743d41d64abacb0f68e8b573fafdf3d2
Dave Snider <dave.snider@gmail.com>
2017-08-03 09:45:11 -0700

    add some better docs

--- ui_framework/src/components/side_nav/side_nav.js
+++ ui_framework/src/components/side_nav/side_nav.js
@@ -18,10 +18,13 @@ export const KuiSideNav = ({ children, isOpenMobile, className, ...rest }) => {
       className={classes}
       {...rest}
     >
+      {/* Hidden from view, except in mobile */}
       <button className="kuiSideNav__mobileToggle kuiLink">
         <span className="kuiSideNav__mobileTitle">Navigate within management</span>
         <span><KuiIcon className="kuiSideNav__mobileIcon" type="apps" size="medium"/></span>
       </button>
+
+      {/* Hidden from view in mobile, but toggled from the button above */}
       <div className="kuiSideNav__content">{children}</div>
     </nav>
   );
