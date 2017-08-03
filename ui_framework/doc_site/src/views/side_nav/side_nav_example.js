bca9907511b3376c529774c7c1fd3e7b85d79f6e
dave.snider@gmail.com <dave.snider@gmail.com>
2017-08-03 16:12:08 -0700

    [UI Framework][K7]: Side nav component (#13304)
    Adds a responsive side nav component to KUI.

---
+++ ui_framework/doc_site/src/views/side_nav/side_nav_example.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { renderToHtml } from '../../services';
+
+import {
+  GuideDemo,
+  GuidePage,
+  GuideSection,
+  GuideSectionTypes,
+  GuideText,
+} from '../../components';
+
+import SideNav from './side_nav';
+const sideNavSource = require('!!raw!./side_nav');
+const sideNavHtml = renderToHtml(SideNav);
+
+export default props => (
+  <GuidePage title={props.route.name}>
+    <GuideSection
+      title="SideNav"
+      source={[{
+        type: GuideSectionTypes.JS,
+        code: sideNavSource,
+      }, {
+        type: GuideSectionTypes.HTML,
+        code: sideNavHtml,
+      }]}
+    >
+      <GuideText>
+        The sidenav is a responsive menu system that usually sits on the left side of a page layout.
+        It will exapand to the width of its container.
+      </GuideText>
+
+      <GuideDemo>
+        <SideNav />
+      </GuideDemo>
+    </GuideSection>
+  </GuidePage>
+);
