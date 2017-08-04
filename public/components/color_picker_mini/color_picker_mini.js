12d88c0cd31f7804b1a37193515e8d102970923a
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-04 14:09:19 -0700

    Switch to rootClose

--- public/components/color_picker_mini/color_picker_mini.js
+++ public/components/color_picker_mini/color_picker_mini.js
@@ -2,35 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ColorPicker } from '../color_picker';
 import { ColorDot } from '../color_dot';
-import { Popover, Overlay } from 'react-bootstrap';
+import { Popover, OverlayTrigger } from 'react-bootstrap';
 import './color_picker_mini.less';
 
 
-export const ColorPickerMini = ({ onChange, value, placement, popover, setPopover, target, setTarget }) => {
-  function open(e) {
-    setTarget(e.target);
-    setPopover(!popover);
-  }
+export const ColorPickerMini = ({ onChange, value, placement }) => {
 
-  function setColor(color) {
-    onChange(color);
-    setPopover(false);
-  }
+  const picker = (
+    <Popover id="popover-trigger-click" style={{ width: 207 }}>
+      <ColorPicker onChange={onChange} value={value}/>
+    </Popover>
+  );
 
   return (
     <div className="canvas__color-picker-mini">
-      <span onClick={open}><ColorDot value={value}/></span>
 
-      <Overlay
-          show={popover}
+      <OverlayTrigger
+          rootClose
+          overlay={picker}
           placement={placement || 'bottom'}
-          container={this}
-          target={target}
-        >
-          <Popover id="popover-trigger-click" style={{ width: 207 }}>
-            <ColorPicker onChange={color => setColor(color)} value={value}/>
-          </Popover>
-        </Overlay>
+          trigger="click"
+      >
+        <div><ColorDot value={value}/></div>
+      </OverlayTrigger>
     </div>
   );
 };
