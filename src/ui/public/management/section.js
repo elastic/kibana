d71bea67df865249c619296dd1a213c6ccb69345
Tim Roes <github@timroes.de>
2017-08-08 12:11:26 +0200

    Move build-info to status page (#13341)
    * Move build-info to status page
    * Remove info property from management section

--- src/ui/public/management/section.js
+++ src/ui/public/management/section.js
@@ -9,7 +9,6 @@ export class ManagementSection {
    * @param {number|null} options.order
    * @param {string|null} options.display - defaults to id
    * @param {string|null} options.url - defaults to ''
-   * @param {string|null} options.info
    * @param {boolean|null} options.visible - defaults to true
    * @param {boolean|null} options.disabled - defaults to false
    * @param {string|null} options.tooltip - defaults to ''
