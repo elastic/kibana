61b94619013828e4a7de408ab165b7772a407a42
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-08 13:24:20 -0700

    Add palette picker component

---
+++ public/components/palette_picker/palette_picker.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { PaletteSwatch } from '../palette_swatch';
+import { Popover, OverlayTrigger } from 'react-bootstrap';
+import './palette_picker.less';
+import { palettes } from '../../../common/lib/palettes';
+import { map } from 'lodash';
+
+
+export const PalettePicker = ({ onChange, value, placement }) => {
+
+  const picker = (
+    <Popover id="popover-trigger-click">
+      <div className="canvas__palette-picker--swatches">
+        {map(palettes, (palette, name) => (
+          <div key={name} onClick={() => onChange(palette)} className="clickable canvas__palette-picker--swatch">
+            <div className="clickable canvas__palette-picker--label">{name.replace(/_/g, ' ')}</div>
+            <PaletteSwatch colors={palette.colors} gradient={palette.gradient}/>
+          </div>
+        ))}
+      </div>
+    </Popover>
+  );
+
+  return (
+    <div className="canvas__palette-picker">
+
+      <OverlayTrigger
+          rootClose
+          overlay={picker}
+          placement={placement || 'bottom'}
+          trigger="click"
+      >
+        <div style={{ width: '100%' }}><PaletteSwatch colors={value.colors} gradient={value.gradient}/></div>
+      </OverlayTrigger>
+    </div>
+  );
+};
+
+PalettePicker.propTypes = {
+  value: PropTypes.object,
+  onChange: PropTypes.func,
+  placement: PropTypes.string,
+};
