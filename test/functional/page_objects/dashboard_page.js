89df1620f94cb3cc7df20d07189ba5bd36ebefc0
Matt Bargar <mbargar@gmail.com>
2017-08-08 17:03:26 -0400

    Make filter pills keyboard accessible (#13331) (#13393)
    Fixes #12639
    
    It's now possible to tab to each filter's actions and interact with them via the keyboard.
    
    In order to get the actions to show/hide on both mouse hover and action focus I had to create a new filter-pill component that could manage a bit of state to track whether the user was interacting with a given pill or not.

--- test/functional/page_objects/dashboard_page.js
+++ test/functional/page_objects/dashboard_page.js
@@ -429,7 +429,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
     }
 
     async getFilters(timeout = defaultFindTimeout) {
-      return await find.allByCssSelector('.filter-bar > .filter', timeout);
+      return await find.allByCssSelector('.filter-bar .filter', timeout);
     }
 
     async getFilterDescriptions(timeout = defaultFindTimeout) {
