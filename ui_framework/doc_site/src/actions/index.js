5118daa5c8b77423b4f59d7bc32cf1c53e4dc3ea
CJ Cenizal <cj@cenizal.com>
2017-08-09 14:03:25 -0700

    Update UI Framework doc site with functionality for hiding and showing the chrome when viewing Sandboxes. (#13437)

--- ui_framework/doc_site/src/actions/index.js
+++ ui_framework/doc_site/src/actions/index.js
@@ -4,6 +4,11 @@ export {
 } from './code_viewer_actions';
 
 export {
+  openSandbox,
+  closeSandbox,
+} from './sandbox_actions';
+
+export {
   registerSection,
   unregisterSection,
 } from './example_nav_actions';
