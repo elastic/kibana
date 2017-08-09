70608dcaf1d5aee7450f8509bfc9beadce61d666
Spencer <spalger@users.noreply.github.com>
2017-08-09 12:31:30 -0700

    [npm] move personal forks into elastic org (#13410)
    (cherry picked from commit 7e16d19249879f2379e3cc31b24bf03dcbdd7740)

--- webpackShims/leaflet.js
+++ webpackShims/leaflet.js
@@ -1,7 +1,7 @@
 require('node_modules/leaflet/dist/leaflet.css');
 window.L = module.exports = require('node_modules/leaflet/dist/leaflet');
 
-require('node_modules/@spalger/leaflet-heat/dist/leaflet-heat.js');
+require('node_modules/@elastic/leaflet-heat/dist/leaflet-heat.js');
 
-require('node_modules/@spalger/leaflet-draw/dist/leaflet.draw.css');
-require('node_modules/@spalger/leaflet-draw/dist/leaflet.draw.js');
+require('node_modules/@elastic/leaflet-draw/dist/leaflet.draw.css');
+require('node_modules/@elastic/leaflet-draw/dist/leaflet.draw.js');
