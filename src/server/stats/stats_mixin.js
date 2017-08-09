12142da71b7bf7310d81ff57d705a87e5f26befb
Spencer <spalger@users.noreply.github.com>
2017-08-09 09:27:54 -0700

    [server/stats] add server.getKibanaStats() for programatic access (#13411)

--- src/server/stats/stats_mixin.js
+++ src/server/stats/stats_mixin.js
@@ -1,6 +1,21 @@
 import { getStats } from './stats';
 
 export function statsMixin(kbnServer, server) {
+
+  /**
+   *  Get a summary about the number of objects in the kibana index
+   *  @name server.getKibanaStats
+   *  @param {Object} options
+   *  @property {Function} options.callCluster method for calling the elasticsearch cluster
+   */
+  server.decorate('server', 'getKibanaStats', async ({ callCluster }) => {
+    const savedObjectsClient = server.savedObjectsClientFactory({ callCluster });
+    return await getStats(
+      server.config().get('kibana.index'),
+      savedObjectsClient
+    );
+  });
+
   server.route({
     method: 'GET',
     path: '/api/stats',
