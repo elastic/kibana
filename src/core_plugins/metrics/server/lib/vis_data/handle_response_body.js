805c15cc7812693a9e412f407b6a9923fe08a9e9
Chris Cowan <chris@chriscowan.us>
2017-08-10 23:42:10 +0200

    Fixes #12685 - Improve error handling, add error for missing aggregations key. (#12688)

--- src/core_plugins/metrics/server/lib/vis_data/handle_response_body.js
+++ src/core_plugins/metrics/server/lib/vis_data/handle_response_body.js
@@ -1,5 +1,6 @@
 import buildProcessorFunction from './build_processor_function';
 import processors from './response_processors/series';
+import { get } from 'lodash';
 
 export default function handleResponseBody(panel) {
   return resp => {
@@ -8,7 +9,13 @@ export default function handleResponseBody(panel) {
       err.response = JSON.stringify(resp);
       throw err;
     }
-    const keys = Object.keys(resp.aggregations);
+    const aggregations = get(resp, 'aggregations');
+    if (!aggregations) {
+      const message = `The aggregations key is missing from the response,
+        check your permissions for this request.`;
+      throw Error(message);
+    }
+    const keys = Object.keys(aggregations);
     if (keys.length !== 1) throw Error('There should only be one series per request.');
     const seriesId = keys[0];
     const series = panel.series.find(s => s.id === seriesId);
