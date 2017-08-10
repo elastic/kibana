dc0788d2287011a40e4c309cc6c860cd10165b9e
Nathan Reese <reese.nathan@gmail.com>
2017-08-10 13:22:54 -0600

    rename top level vis folder

--- src/core_plugins/input_control_vis/index.js
+++ src/core_plugins/input_control_vis/index.js
@@ -2,7 +2,7 @@ export default function (kibana) {
   return new kibana.Plugin({
     uiExports: {
       visTypes: [
-        'plugins/terms_vis/terms_vis/terms_vis'
+        'plugins/input_control_vis/register_vis'
       ]
     }
   });
--- src/core_plugins/kibana/public/visualize/wizard/wizard.js
+++ src/core_plugins/kibana/public/visualize/wizard/wizard.js
@@ -36,7 +36,6 @@ module.controller('VisualizeWizardStep1', function ($scope, $route, kbnUrl, time
 
   const visTypeCategoryToHumanReadableMap = {
     [CATEGORY.BASIC]: 'Basic Charts',
-    [CATEGORY.CONTROL]: 'Dashboard Controls',
     [CATEGORY.DATA]: 'Data',
     [CATEGORY.GRAPHIC]: 'Graphic',
     [CATEGORY.MAP]: 'Maps',
