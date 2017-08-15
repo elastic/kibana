c5458a4859a7275b71339a998e1960aae9951000
CJ Cenizal <cj@cenizal.com>
2017-08-15 14:24:58 -0700

    Add KuiOutsideClickDetector.

---
+++ ui_framework/src/components/outside_click_detector/outside_click_detector.js
@@ -0,0 +1,46 @@
+import {
+  cloneElement,
+  Component,
+} from 'react';
+import PropTypes from 'prop-types';
+
+export class KuiOutsideClickDetector extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    onOutsideClick: PropTypes.func.isRequired,
+  }
+
+  onClickOutside = event => {
+    if (!this.wrapperRef) {
+      return;
+    }
+
+    if (this.wrapperRef === event.target) {
+      return;
+    }
+
+    if (this.wrapperRef.contains(event.target)) {
+      return;
+    }
+
+    this.props.onOutsideClick();
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.onClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onClickOutside);
+  }
+
+  render() {
+    const props = Object.assign({}, this.props.children.props, {
+      ref: node => {
+        this.wrapperRef = node;
+      },
+    });
+
+    return cloneElement(this.props.children, props);
+  }
+}
