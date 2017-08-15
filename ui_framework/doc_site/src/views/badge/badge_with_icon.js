c1ce402c4d41abda3f3e5226ce5642bd45b7388d
dave.snider@gmail.com <dave.snider@gmail.com>
2017-08-15 13:52:29 -0700

    [UI Framework][K7]: Badge component (#13520)
    Adds badge component to K7 UI.

---
+++ ui_framework/doc_site/src/views/badge/badge_with_icon.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+  KuiBadge,
+} from '../../../../components';
+
+export default () => (
+  <div>
+
+    <KuiBadge iconType="help">
+      Primary
+    </KuiBadge>
+
+    &nbsp;&nbsp;
+
+    <KuiBadge type="primary" iconType="user" iconSide="left">
+      Secondary
+    </KuiBadge>
+
+  </div>
+
+);
