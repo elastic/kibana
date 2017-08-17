3b040c5ac959cdef57617400c2f8e3cba3f45cf1
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-17 13:24:08 -0700

    Enable index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,12 @@ import { workpad } from './workpad';
 import { socketApi } from './socket';
 import { translate } from './translate';
 import { esFields } from './es_fields';
+import { esIndices } from './es_indices';
 
 export function routes(server) {
   workpad(server);
   socketApi(server);
   translate(server);
   esFields(server);
+  esIndices(server);
 }
