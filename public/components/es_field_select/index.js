e1a64c877a83f34727c60614d21badb8b67f7232
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-17 15:08:27 -0700

    Add a single field selector

---
+++ public/components/es_field_select/index.js
@@ -0,0 +1,29 @@
+import { compose, withState, lifecycle } from 'recompose';
+import { ESFieldSelect as Component } from './es_field_select';
+import { fetch } from '../../../common/lib/fetch';
+import chrome from 'ui/chrome';
+
+const basePath = chrome.getBasePath();
+const apiPath = `${basePath}/api/canvas/es_fields`;
+
+const getFields = (index) => {
+  index = index || '_all';
+  return fetch.get(`${apiPath}?index=${index}`)
+    .then(res => Object.keys(res.data).sort());
+};
+
+export const ESFieldSelect = compose(
+  withState('fields', 'setFields', []),
+  lifecycle({
+    componentDidMount() {
+      getFields(this.props.index)
+      .then(this.props.setFields);
+    },
+    componentDidUpdate({ index }) {
+      if (this.props.index !== index) {
+        getFields(this.props.index)
+        .then(this.props.setFields);
+      }
+    },
+  })
+)(Component);
