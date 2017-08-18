ea983c3c59418b52020450cbbcf77784202f6d33
Chris Roberson <chrisronline@gmail.com>
2017-08-18 14:47:40 -0400

    Fix breaking scope changes in the field format editor samples (#13549)

--- src/ui/public/field_format_editor/pattern/pattern.js
+++ src/ui/public/field_format_editor/pattern/pattern.js
@@ -10,14 +10,13 @@ uiModules
     replace: true,
     template: patternTemplate,
     require: ['ngModel', '^fieldEditor'],
-    scope: {
-      id: '@'
-    },
+    scope: true,
     link: function ($scope, $el, attrs, cntrls) {
       const ngModelCntrl = cntrls[0];
 
       $scope.$bind('inputs', attrs.inputs);
       $scope.$bind('placeholder', attrs.placeholder);
+      attrs.$observe('id', () => $scope.id = attrs.id);
 
       // bind our local model with the outside ngModel
       $scope.$watch('model', ngModelCntrl.$setViewValue);
