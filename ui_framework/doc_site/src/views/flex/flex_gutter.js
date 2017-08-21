483196db1bc291d56e74b76c94dd32f1963a0ca3
dave.snider@gmail.com <dave.snider@gmail.com>
2017-08-21 10:45:31 -0700

    [UI FRAMEWORK][K7]: Flex Group and Flex Grid components (#13603)
    FlexGroup and FlexGrid allows for layout and positioning inside content.

---
+++ ui_framework/doc_site/src/views/flex/flex_gutter.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import {
+  KuiFlexGroup,
+  KuiFlexItem,
+} from '../../../../components';
+
+export default () => (
+  <div>
+    <KuiFlexGroup gutterSize="none">
+      <KuiFlexItem>None</KuiFlexItem>
+      <KuiFlexItem>None</KuiFlexItem>
+      <KuiFlexItem>None</KuiFlexItem>
+      <KuiFlexItem>None</KuiFlexItem>
+    </KuiFlexGroup>
+
+    <br/>
+    <br/>
+
+    <KuiFlexGroup gutterSize="small">
+      <KuiFlexItem>Small</KuiFlexItem>
+      <KuiFlexItem>Small</KuiFlexItem>
+      <KuiFlexItem>Small</KuiFlexItem>
+      <KuiFlexItem>Small</KuiFlexItem>
+    </KuiFlexGroup>
+
+    <br/>
+    <br/>
+
+    <KuiFlexGroup gutterSize="medium">
+      <KuiFlexItem>Medium</KuiFlexItem>
+      <KuiFlexItem>Medium</KuiFlexItem>
+      <KuiFlexItem>Medium</KuiFlexItem>
+      <KuiFlexItem>Medium</KuiFlexItem>
+    </KuiFlexGroup>
+
+    <br/>
+    <br/>
+
+    <KuiFlexGroup gutterSize="large">
+      <KuiFlexItem>Large (default)</KuiFlexItem>
+      <KuiFlexItem>Large (default)</KuiFlexItem>
+      <KuiFlexItem>Large (default)</KuiFlexItem>
+      <KuiFlexItem>Large (default)</KuiFlexItem>
+    </KuiFlexGroup>
+
+    <br/>
+    <br/>
+
+    <KuiFlexGroup gutterSize="extraLarge">
+      <KuiFlexItem>Extra Large</KuiFlexItem>
+      <KuiFlexItem>Extra Large</KuiFlexItem>
+      <KuiFlexItem>Extra Large</KuiFlexItem>
+      <KuiFlexItem>Extra Large</KuiFlexItem>
+    </KuiFlexGroup>
+  </div>
+);
