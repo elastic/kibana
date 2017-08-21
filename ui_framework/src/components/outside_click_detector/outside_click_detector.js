dc423ac7a6e211e0ad15ca82ddf106178c02393f
CJ Cenizal <cj@cenizal.com>
2017-08-21 08:58:56 -0700

    [UI Framework] Add KuiOutsideClickDetector (#13521)
    * Add KuiOutsideClickDetector.
    * Convert KuiColorPicker and KuiPopover to use KuiOutsideClickDetector.

---
+++ ui_framework/src/components/outside_click_detector/outside_click_detector.js
@@ -0,0 +1,48 @@
+import {
+  Children,
+  cloneElement,
+  Component,
+} from 'react';
+import PropTypes from 'prop-types';
+
+export class KuiOutsideClickDetector extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    onOutsideClick: PropTypes.func.isRequired,
+  }
+
+  onClickOutside = event => {
+    if (!this.wrapperRef) {
+      return;
+    }
+
+    if (this.wrapperRef === event.target) {
+      return;
+    }
+
+    if (this.wrapperRef.contains(event.target)) {
+      return;
+    }
+
+    this.props.onOutsideClick();
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.onClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onClickOutside);
+  }
+
+  render() {
+    const props = Object.assign({}, this.props.children.props, {
+      ref: node => {
+        this.wrapperRef = node;
+      },
+    });
+
+    const child = Children.only(this.props.children);
+    return cloneElement(child, props);
+  }
+}
