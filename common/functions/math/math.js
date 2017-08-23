5e3089ddd5cb4a9ba84119f24b987c9a350c81f3
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-23 13:58:57 -0700

    Allow math to work without a datatable

--- common/functions/math/math.js
+++ common/functions/math/math.js
@@ -5,14 +5,17 @@ import { getMathjsScope } from './get_mathjs_scope';
 export default new Fn({
   name: 'math',
   type: 'number',
-  help: 'Turn a datatable into a single number using a MathJS formula',
+  help: 'Interpret a mathJS expression, with a datatable as context, or not',
   context: {
-    types: ['datatable'],
+    types: ['null', 'datatable'],
   },
   args: {
     _: {
       types: ['string'],
     },
   },
-  fn: (context, args) => math.eval(args._, getMathjsScope(context)),
+  fn: (context, args) => {
+    const mathContext = context && context.type === 'datatable' ? getMathjsScope(context) : null;
+    return math.eval(args._, mathContext);
+  },
 });
