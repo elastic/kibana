c96fec8ba2d4bba46f3a404ca4567aaf711cb0ff
ppisljar <peter.pisljar@gmail.com>
2017-08-23 10:42:07 +0200

    update visualization configuration

--- src/ui/public/vis/vis.js
+++ src/ui/public/vis/vis.js
@@ -16,6 +16,7 @@ import { PersistedState } from 'ui/persisted_state';
 import { UtilsBrushEventProvider } from 'ui/utils/brush_event';
 import { FilterBarQueryFilterProvider } from 'ui/filter_bar/query_filter';
 import { FilterBarClickHandlerProvider } from 'ui/filter_bar/filter_bar_click_handler';
+import { updateVisualizationConfig } from './vis_update';
 
 export function VisProvider(Private, indexPatterns, timefilter, getAppState) {
   const visTypes = Private(VisTypesRegistryProvider);
@@ -80,6 +81,8 @@ export function VisProvider(Private, indexPatterns, timefilter, getAppState) {
         _.cloneDeep(state.params || {}),
         _.cloneDeep(this.type.visConfig.defaults || {})
       );
+
+      updateVisualizationConfig(state.params, this.params);
 
       this.aggs = new AggConfigs(this, state.aggs);
     }
