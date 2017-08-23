4faadb972e4e103a8bb7177d8f9d54a641d7261f
Dave Snider <dave.snider@gmail.com>
2017-08-23 12:25:06 -0700

    form gallery start

---
+++ ui_framework/doc_site/src/views/form_gallery/form_gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+  KuiPage,
+} from '../../../../components/';
+
+import InlineForm from './inline_form';
+
+export default () => (
+
+  <KuiPage>
+    <InlineForm />
+  </KuiPage>
+);
