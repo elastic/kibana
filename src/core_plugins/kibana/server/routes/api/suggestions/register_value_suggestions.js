5da57b587492f7f0c010caeac8f6b61fc0d13fa4
Lukas Olson <olson.lukas@gmail.com>
2017-08-24 08:36:07 -0700

    Add additional regex and explanation for parameters

--- src/core_plugins/kibana/server/routes/api/suggestions/register_value_suggestions.js
+++ src/core_plugins/kibana/server/routes/api/suggestions/register_value_suggestions.js
@@ -21,16 +21,28 @@ export function registerValueSuggestions(server) {
 }
 
 function getBody({ field, query }) {
+  // Helps ensure that the regex is not evaluated eagerly against the terms dictionary
+  const executionHint = 'map';
+
+  // Helps keep the number of buckets that need to be tracked at the shard level contained in case
+  // this is a high cardinality field
+  const terminateAfter = 100000;
+
+  // We don't care about the accuracy of the counts, just the content of the terms, so this reduces
+  // the amount of information that needs to be transmitted to the coordinating node
+  const shardSize = 10;
+
   return {
     size: 0,
     timeout: '1s',
-    terminate_after: 100000,
+    terminate_after: terminateAfter,
     aggs: {
       suggestions: {
         terms: {
           field,
           include: `${getEscapedQuery(query)}.*`,
-          execution_hint: 'map'
+          execution_hint: executionHint,
+          shard_size: shardSize
         }
       }
     }
@@ -39,5 +51,5 @@ function getBody({ field, query }) {
 
 function getEscapedQuery(query = '') {
   // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-regexp-query.html#_standard_operators
-  return query.replace(/[.?+*|{}[\]()"\\]/g, (match) => `\\${match}`);
+  return query.replace(/[.?+*|{}[\]()"\\#@&<>~]/g, (match) => `\\${match}`);
 }
