fcd4ddfb2d2b78191f34010e0c57fa23f462b379
CJ Cenizal <cj@cenizal.com>
2017-08-24 11:48:04 -0700

    [Accessibility] Give kuiTables keyboard-accessible column headers  (#13586)
    * Make KuiTable headers keyboard-accessible in UI Framework.
    * Use flexbox to consistently apply spacing between table header cell text and sort icon.
    * Add aria-label attributes to KuiTableHeaderCell, KuiTableHeaderCheckBoxCell, and KuiTableRowCheckBoxCell.
    * Update Jest snapshots.
    * Update Dashboard listing table with new markup.
    * Update Saved Objects tables with new markup.
    * Update Settings table with new markup.
    * Update Visualize listing table with new markup.
    * Remove padding-right for cells containing checkboxes.
    * Update sortableColumn directive with new markup.

--- ui_framework/doc_site/src/views/table/fluid_table.js
+++ ui_framework/doc_site/src/views/table/fluid_table.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {
+  Component,
+} from 'react';
 
 import {
   KuiTable,
@@ -9,58 +11,99 @@ import {
   KuiTableBody,
 } from '../../../../components';
 
-export function FluidTable() {
-  return (
-    <KuiTable shrinkToContent={true}>
-      <KuiTableHeader>
-        <KuiTableHeaderCell>
-              System
-        </KuiTableHeaderCell>
-        <KuiTableHeaderCell>
-              Action
-        </KuiTableHeaderCell>
-      </KuiTableHeader>
+import {
+  SortableProperties,
+} from '../../../../services';
 
-      <KuiTableBody>
-        <KuiTableRow>
-          <KuiTableRowCell>
-              Cryogenics
-          </KuiTableRowCell>
-          <KuiTableRowCell>
-            <select className="kuiSelect" defaultValue="on">
-              <option value="on">On</option>
-              <option value="off">Off</option>
-              <option value="selfDestruct">Self-destruct</option>
-            </select>
-          </KuiTableRowCell>
-        </KuiTableRow>
+export class FluidTable extends Component {
+  constructor(props) {
+    super(props);
 
-        <KuiTableRow>
-          <KuiTableRowCell>
-              Propellant
-          </KuiTableRowCell>
-          <KuiTableRowCell>
-            <select className="kuiSelect" defaultValue="on">
-              <option value="on">On</option>
-              <option value="off">Off</option>
-              <option value="selfDestruct">Self-destruct</option>
-            </select>
-          </KuiTableRowCell>
-        </KuiTableRow>
+    this.state = {
+      sortedColumn: 'title',
+    };
 
-        <KuiTableRow>
-          <KuiTableRowCell>
-              Rockets
-          </KuiTableRowCell>
-          <KuiTableRowCell>
-            <select className="kuiSelect" defaultValue="off">
-              <option value="on">On</option>
-              <option value="off">Off</option>
-              <option value="selfDestruct">Self-destruct</option>
-            </select>
-          </KuiTableRowCell>
-        </KuiTableRow>
-      </KuiTableBody>
-    </KuiTable>
-  );
+    this.items = [{
+      title: 'Cryogenics',
+      description: 'AC turned to 11',
+    }, {
+      title: 'Propellant',
+      description: 'Go fast',
+    }, {
+      title: 'Rockets',
+      description: 'Hot and burny',
+    }];
+
+    this.sortableProperties = new SortableProperties([{
+      name: 'title',
+      getValue: item => item.title.toLowerCase(),
+      isAscending: true,
+    }, {
+      name: 'description',
+      getValue: item => item.description.toLowerCase(),
+      isAscending: true,
+    }], this.state.sortedColumn);
+  }
+
+  onSort = prop => {
+    this.sortableProperties.sortOn(prop);
+
+    this.setState({
+      sortedColumn: prop,
+    });
+  }
+
+  renderRows() {
+    return this.items.map(item => (
+      <KuiTableRow key={item.title}>
+        <KuiTableRowCell>
+          {item.title}
+        </KuiTableRowCell>
+
+        <KuiTableRowCell>
+          {item.description}
+        </KuiTableRowCell>
+
+        <KuiTableRowCell>
+          <select className="kuiSelect" defaultValue="on">
+            <option value="on">On</option>
+            <option value="off">Off</option>
+            <option value="selfDestruct">Self-destruct</option>
+          </select>
+        </KuiTableRowCell>
+      </KuiTableRow>
+    ));
+  }
+
+  render() {
+    return (
+      <KuiTable shrinkToContent={true}>
+        <KuiTableHeader>
+          <KuiTableHeaderCell
+            onSort={this.onSort.bind(this, 'title')}
+            isSorted={this.state.sortedColumn === 'title'}
+            isSortAscending={this.sortableProperties.isAscendingByName('title')}
+          >
+            System
+          </KuiTableHeaderCell>
+
+          <KuiTableHeaderCell
+            onSort={this.onSort.bind(this, 'description')}
+            isSorted={this.state.sortedColumn === 'description'}
+            isSortAscending={this.sortableProperties.isAscendingByName('description')}
+          >
+            Description
+          </KuiTableHeaderCell>
+
+          <KuiTableHeaderCell>
+            Action
+          </KuiTableHeaderCell>
+        </KuiTableHeader>
+
+        <KuiTableBody>
+          {this.renderRows()}
+        </KuiTableBody>
+      </KuiTable>
+    );
+  }
 }
