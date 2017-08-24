314cdf036c9750fc8fdf2fb9d3ec87f4ba8ebae2
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-24 15:19:36 -0700

    Add starter text

--- public/elements/markdown/markdown.js
+++ public/elements/markdown/markdown.js
@@ -5,7 +5,7 @@ export default new Element('markdown', {
   displayName: 'Markdown',
   description: 'Markup from Markdown',
   image: header,
-  expression: 'markdown ""',
+  expression: 'markdown "Your Text Here"',
   render(domNode, config, done) {
     domNode.innerHTML = config.markup;
     done();
