70b1d344c231d2fca0e2d918610f45b7ce4a03c6
Rashid Khan <github.fliplap@spamgourmet.com>
2017-08-24 16:48:57 -0700

    Simplify element renderer

--- public/components/render_element/index.js
+++ public/components/render_element/index.js
@@ -1,11 +1,50 @@
-import { pure, compose, withState } from 'recompose';
+import { compose, withState, lifecycle, withProps } from 'recompose';
 import { RenderElement as Component } from './render_element';
 import PropTypes from 'prop-types';
+import { isEqual } from 'lodash';
+import { Events } from '../../lib/events';
 
 export const RenderElement = compose(
-  pure,
-  withState('domNode', 'setDomNode'),
-  withState('events', 'setEventEmitter'),
+  withState('domNode', 'setDomNode'), // Still don't like this, seems to be the only way todo it.
+  withProps({ events: new Events() }),
+  lifecycle({
+    componentDidUpdate(prevProps) {
+      const { events, config, domNode, done, size, renderFn } = this.props;
+
+      // Config changes
+      if (this.shouldFullRerender(prevProps)) {
+        this.destroy();
+        return renderFn(domNode, config, done || (() => {}), events);
+      }
+
+      // Size changes
+      if (!isEqual(size, prevProps.size)) return events.emit('resize', size);
+    },
+
+    shouldComponentUpdate(prevProps) {
+      return this.shouldFullRerender(prevProps) || !isEqual(this.props.size, prevProps.size);
+    },
+
+    componentWillUnmount() {
+      this.destroy();
+    },
+
+    shouldFullRerender(prevProps) {
+      // TODO: What a shitty hack. None of these props should update when you move the element.
+      // This should be fixed at a higher level.
+      return !isEqual(this.props.config, prevProps.config) ||
+      !isEqual(this.props.domNode, prevProps.domNode) ||
+      !isEqual(this.props.renderFn.toString(), prevProps.renderFn.toString());
+    },
+
+    destroy() {
+      const { events } = this.props;
+      events.emit('destroy');
+
+      events.off('destroy');
+      events.off('resize');
+    },
+  }),
 )(Component);
 
 RenderElement.propTypes = {
