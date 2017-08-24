c0319cd6ac70507390ea00a4fd9e6e549ab6a8a8
CJ Cenizal <cj@cenizal.com>
2017-08-24 12:10:02 -0700

    [UI Framework] [K7] Integrate into Kibana (#13693)
    * Remove Bootstrap reset and incorporate UI Framework reset.
    * Remove naked element selectors from base.less.
    * Incorporate K7 UI Framework into Kibana core.

--- src/ui/public/autoload/styles.js
+++ src/ui/public/autoload/styles.js
@@ -1,5 +1,5 @@
 // Kibana UI Framework
-require('../../../../ui_framework/dist/ui_framework.css');
+require('../../../../ui_framework/dist/ui_framework_theme_light.css');
 
 // All Kibana styles inside of the /styles dir
 const context = require.context('../styles', false, /[\/\\](?!mixins|variables|_|\.)[^\/\\]+\.less/);
