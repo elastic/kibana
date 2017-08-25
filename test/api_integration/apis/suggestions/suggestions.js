44bd82d438337998d0e6a1da47d9a26b83d2fc36
Lukas Olson <olson.lukas@gmail.com>
2017-08-25 08:50:11 -0700

    Fix suggestions API test

--- test/api_integration/apis/suggestions/suggestions.js
+++ test/api_integration/apis/suggestions/suggestions.js
@@ -1,12 +1,16 @@
 export default function ({ getService }) {
+  const esArchiver = getService('esArchiver');
   const supertest = getService('supertest');
 
   describe('Suggestions API', function () {
-    it('should return 200 with a regex', () => (
+    before(() => esArchiver.load('index_patterns/basic_index'));
+    after(() => esArchiver.unload('index_patterns/basic_index'));
+
+    it('should return 200 with special characters', () => (
       supertest
-        .post('/api/kibana/suggestions/values/*')
+        .post('/api/kibana/suggestions/values/basic_index')
         .send({
-          field: 'bar.keyword',
+          field: 'baz.keyword',
           query: '<something?with:lots&of^ bad characters'
         })
         .expect(200)
