ad760fec6c20383b9fd75495ffb5b4f600b3d82b
Nathan Reese <reese.nathan@gmail.com>
2017-08-25 12:32:24 -0600

    Keep current cursor or top line in view when resizing Console to avoid losing user context (#13695)
    * keep current cursor or top line in view when resizing to avoid losing user context
    
    * rename variable to more descriptive name
    
    * move state from editor to smart_resize

--- src/core_plugins/console/public/src/smart_resize.js
+++ src/core_plugins/console/public/src/smart_resize.js
@@ -1,6 +1,16 @@
-import { throttle } from 'lodash';
+import { get, throttle } from 'lodash';
 
 module.exports = function (editor) {
   const resize = editor.resize;
-  return throttle(() => resize.call(editor), 35)
-};
+  const throttledResize = throttle(() => {
+
+    resize.call(editor);
+
+    // Keep current top line in view when resizing to avoid losing user context
+    let userRow = get(throttledResize, 'topRow', 0);
+    if (userRow !== 0) {
+      editor.renderer.scrollToLine(userRow, false, false, () => {});
+    }
+  }, 35);
+  return throttledResize;
+}
