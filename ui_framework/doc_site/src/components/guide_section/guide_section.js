dd9be4cbbac8bb508f3d9ce00b9926cb79c08815
Dave Snider <dave.snider@gmail.com>
2017-08-27 16:25:49 -0700

    remove more unneeded code

--- ui_framework/doc_site/src/components/guide_section/guide_section.js
+++ ui_framework/doc_site/src/components/guide_section/guide_section.js
@@ -90,8 +90,8 @@ export class GuideSection extends Component {
 
     if (code && (codeClass === this.state.selectedTabId)) {
       return (
-        <div className="KuiCodeViewer__section" key={type} ref={type}>
-          <pre className="KuiCodeViewer__content">
+        <div key={type} ref={type}>
+          <pre>
             <code
               ref={codeClass}
               className={codeClass}
