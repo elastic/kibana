a13d5618ced9f8553c29c87bb107430c28a61286
ppisljar <peter.pisljar@gmail.com>
2017-08-29 14:25:55 +0200

    upgrading metric fontSize setting (cherry picked from commit 2bcbfff)

--- src/ui/public/vislib/lib/types/gauge.js
+++ src/ui/public/vislib/lib/types/gauge.js
@@ -1,14 +1,6 @@
-import _ from 'lodash';
-
 export function vislibGaugeProvider() {
 
   return function (config) {
-    if (!config.chart) {
-      config.chart = _.defaults({}, config, {
-        type: 'gauge'
-      });
-    }
-
     return config;
   };
 }
