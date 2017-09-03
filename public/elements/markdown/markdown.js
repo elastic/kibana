08434d4969969072a3a5a1ffe8ebda1d63978e9a
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-03 13:31:06 -0700

    Ignore jquery stuff in webpackShims

--- public/elements/markdown/markdown.js
+++ public/elements/markdown/markdown.js
@@ -5,7 +5,20 @@ export default new Element('markdown', {
   displayName: 'Markdown',
   description: 'Markup from Markdown',
   image: header,
-  expression: 'markdown "Your Text Here"',
+  expression: `demodata | markdown "### Welcome to the Markdown Element.
+
+Good news! You're already connected to some demo data!
+
+The datatable contains
+**{{rows.length}} rows**, each containing
+ the following columns:
+{{#each columns}}
+ **{{name}}**
+{{/each}}
+
+You can use standard Markdown in here, but you can also access your piped-in data using Handlebars. If you want to know more, check out the [Handlebars Documentation](http://handlebarsjs.com/expressions.html)
+
+#### Enjoy!"`,
   render(domNode, config, handlers) {
     domNode.innerHTML = config.markup;
     handlers.done();
