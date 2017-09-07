69a2d2559eac3d8ae57438ae860ef6d2279201c9
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-07 16:44:55 -0700

    Add a simple string type

---
+++ public/expression_types/arg_types/string.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { ArgType } from '../arg_type';
+import { get } from 'lodash';
+import { statefulProp } from '../../lib/stateful_component';
+import { compose, withProps } from 'recompose';
+
+const component = ({ updateValue, value, confirm, commit }) => (
+  <Form inline>
+    <FormGroup>
+      <FormControl
+        spellCheck={false}
+        value={value}
+        onChange={confirm ? updateValue : (ev) => commit(ev.target.value)}
+      />
+    </FormGroup>
+    {confirm && (
+      <Button bsStyle="primary" bsSize="xsmall" onClick={() => commit(value)}>{confirm}</Button>
+    )}
+  </Form>
+);
+
+component.propTypes = {
+  updateValue: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  confirm: PropTypes.string,
+  commit: PropTypes.func.isRequired,
+};
+
+const template = compose(
+  withProps(({ onValueChange, typeInstance, argValue }) => ({
+    confirm: get(typeInstance, 'options.confirm'),
+    commit: (value) => onValueChange({
+      type: 'string',
+      value,
+    }),
+    value: argValue.value,
+  })),
+  statefulProp('value'),
+)(component);
+
+template.propTypes = {
+  argValue: PropTypes.object.isRequired,
+  onValueChange: PropTypes.func.isRequired,
+  typeInstance: PropTypes.object.isRequired,
+};
+
+export const string = () => new ArgType('string', {
+  displayName: 'string',
+  description: 'Input short strings',
+  simpleTemplate: template,
+});
