176d74acd9fd428b83286c6420915b443aaef534
Tim Roes <mail@timroes.de>
2017-09-08 15:24:29 +0200

    Move htmlIdGenerator to ui_framework

--- ui_framework/src/services/accessibility/html_id_generator.test.js
+++ ui_framework/src/services/accessibility/html_id_generator.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { htmlIdGenerator } from '../html_id_generator';
+import { htmlIdGenerator } from './html_id_generator';
 
 describe('htmlIdGenerator', () => {
 
