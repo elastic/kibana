5d5a979599a26b7b30103ca09950c9d989618336
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-08 13:36:38 -0700

    Remove objectified literal formater

--- public/expression_types/arg_types/container_style/container_style.js
+++ public/expression_types/arg_types/container_style/container_style.js
@@ -2,7 +2,6 @@ import { withHandlers } from 'recompose';
 import { get } from 'lodash';
 import { set } from 'object-path-immutable';
 import { ArgType } from '../../arg_type';
-import { getStringType } from '../../../../common/types/get_type';
 import { simpleTemplate } from './simple_template';
 import { extendedTemplate } from './extended_template';
 
@@ -11,14 +10,10 @@ import './container_style.less';
 const wrap = (Component) => withHandlers({
   getArgValue: ({ argValue }) => (name, alt) => {
     const args = get(argValue, 'chain.0.arguments', {});
-    return get(args, [name, 0, 'value'], alt);
+    return get(args, [name, 0], alt);
   },
   setArgValue: ({ argValue, onValueChange }) => (name, val) => {
-    const value = {
-      value: val,
-      type: getStringType(val),
-    };
-    const newValue = set(argValue, ['chain', 0, 'arguments', name, 0], value);
+    const newValue = set(argValue, ['chain', 0, 'arguments', name, 0], val);
     onValueChange(newValue);
   },
 })(Component);
