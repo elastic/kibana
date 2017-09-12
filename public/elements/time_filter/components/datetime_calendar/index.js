c753c922b1ce16d2a66f14d2d46dafab0429e06d
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-11 17:28:11 -0700

    Add a simple calendar input

---
+++ public/elements/time_filter/components/datetime_calendar/index.js
@@ -0,0 +1,5 @@
+import { compose } from 'recompose';
+import { DatetimeCalendar as Component } from './datetime_calendar';
+
+export const DatetimeCalendar = compose(
+)(Component);
---
+++ public/elements/time_filter/components/datetime_input/datetime_input.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormControl } from 'react-bootstrap';
+import moment from 'moment';
+import './datetime_input.less';
+
+export const DatetimeInput = ({ strValue, setStrValue, setMoment, valid, setValid }) => {
+  function check(e) {
+    const parsed = moment(e.target.value, 'YYYY-MM-DD hh:mm:ss', true);
+    if (parsed.isValid()) {
+      setMoment(parsed);
+      setValid(true);
+    } else {
+      console.log('Invalid input');
+      setValid(false);
+    }
+    setStrValue(e.target.value);
+  }
+
+  return (
+    <FormControl
+      spellCheck={false}
+      value={strValue}
+      onChange={check}
+      className={ valid ? 'input-valid' : 'input-invalid' }
+      style={{ textAlign: 'center' }}
+    />
+  );
+};
+
+DatetimeInput.propTypes = {
+  setMoment: PropTypes.func,
+  strValue: PropTypes.string,
+  setStrValue: PropTypes.func,
+  valid: PropTypes.bool,
+  setValid: PropTypes.func,
+};
