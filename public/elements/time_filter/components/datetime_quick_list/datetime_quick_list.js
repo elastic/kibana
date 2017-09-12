d47404e6ed8716ac8e1fb31a2f36e54f734367d1
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-12 10:35:14 -0700

    Highlight the selected quick range

--- public/elements/time_filter/components/datetime_quick_list/datetime_quick_list.js
+++ public/elements/time_filter/components/datetime_quick_list/datetime_quick_list.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'react-datetime/css/react-datetime.css';
 
-export const DatetimeQuickList = ({ ranges, onSelect }) => (
+export const DatetimeQuickList = ({ from, to, ranges, onSelect }) => (
   <div>
     <ul className="nav nav-pills nav-stacked">
       {ranges.map((range, i) => (
-        <li key={i} onClick={() => onSelect(range.from, range.to)}>
+        <li
+          className={(from === range.from && to === range.to) ? 'active' : null}
+          key={i}
+          onClick={() => onSelect(range.from, range.to)}>
           <a>{range.display}</a>
         </li>
       ))}
