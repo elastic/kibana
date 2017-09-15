38fecdb4b9f6e31c4205f7321a4d587fffb9d114
dave.snider@gmail.com <dave.snider@gmail.com>
2017-09-15 12:46:23 -0700

    [K7][UI Framework]: Progress bar component (#14003)
    * progress bar component
    
    * fix some moz issues
    
    * typo
    
    * indeterminate progress bar uses a div because of FF

---
+++ ui_framework/src/components/progress/progress.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const sizeToClassNameMap = {
+  xs: 'kuiProgress--xs',
+  s: 'kuiProgress--s',
+  m: 'kuiProgress--m',
+  l: 'kuiProgress--l',
+};
+
+export const SIZES = Object.keys(sizeToClassNameMap);
+
+const colorToClassNameMap = {
+  primary: 'kuiProgress--primary',
+  secondary: 'kuiProgress--secondary',
+  danger: 'kuiProgress--danger',
+  subdued: 'kuiProgress--subdued',
+  accent: 'kuiProgress--accent',
+};
+
+export const COLORS = Object.keys(colorToClassNameMap);
+
+const positionsToClassNameMap = {
+  fixed: 'kuiProgress--fixed',
+  absolute: 'kuiProgress--absolute',
+  static: '',
+};
+
+export const POSITIONS = Object.keys(positionsToClassNameMap);
+
+export const KuiProgress = ({
+  className,
+  color,
+  value,
+  max,
+  size,
+  position,
+  ...rest,
+}) => {
+  const classes = classNames(
+    'kuiProgress',
+    {
+      'kuiProgress--indeterminate': max === null,
+    },
+    sizeToClassNameMap[size],
+    colorToClassNameMap[color],
+    positionsToClassNameMap[position],
+    className
+  );
+
+  // Because of a FireFox issue with animation,
+  // indeterminate progress needs to use a div.
+  let progressType = null;
+  if (max) {
+    progressType = (
+      <progress
+        value={value}
+        max={max}
+        className={classes}
+        {...rest}
+      />
+    );
+  } else {
+    progressType = (
+      <div
+        className={classes}
+        {...rest}
+      />
+    );
+  }
+
+  return (
+    <div>{progressType}</div>
+  );
+};
+
+KuiProgress.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  size: PropTypes.oneOf(SIZES),
+  color: PropTypes.oneOf(COLORS),
+  position: PropTypes.oneOf(POSITIONS),
+  max: PropTypes.number,
+  indeterminate: PropTypes.bool,
+};
+
+KuiProgress.defaultProps = {
+  max: null,
+  size: 'm',
+  color: 'secondary',
+  position: 'static',
+};
