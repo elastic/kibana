e72a20670d62ddfb1f5d3b7c91887d1d0864597f
Spencer <spalger@users.noreply.github.com>
2017-09-14 19:21:56 -0700

    [ftr] remove timestamp prefix, jenkins has it built in now (#13825)

--- tasks/functional_test_runner.js
+++ tasks/functional_test_runner.js
@@ -1,6 +1,5 @@
-import moment from 'moment';
 import { createFunctionalTestRunner } from '../src/functional_test_runner';
-import { createToolingLog, createMapStream } from '../src/utils';
+import { createToolingLog } from '../src/utils';
 
 export default function (grunt) {
   grunt.registerMultiTask('functional_test_runner', 'run tests with the functional test runner', function () {
@@ -11,11 +10,7 @@ export default function (grunt) {
     } = this.options();
 
     const log = createToolingLog(logLevel);
-    log
-      .pipe(createMapStream(line => {
-        return `${moment().format('hh:mm:ss.SSS')} ${line}`;
-      }))
-      .pipe(process.stdout);
+    log.pipe(process.stdout);
 
     const functionalTestRunner = createFunctionalTestRunner({
       log,
