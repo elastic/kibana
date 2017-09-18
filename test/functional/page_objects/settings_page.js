c89bb84e4b82014259c5a7c05ca0881fdbd307c8
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2017-09-18 11:44:36 -0700

    added a small sleep

--- test/functional/page_objects/settings_page.js
+++ test/functional/page_objects/settings_page.js
@@ -292,7 +292,7 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
         await this.navigateTo();
         await this.clickKibanaIndices();
         await this.setIndexPatternField(indexPatternName);
-        await PageObjects.common.sleep(2000);
+        await PageObjects.common.sleep(4000);
         if (timefield) {
           await this.selectTimeFieldOption(timefield);
         }
