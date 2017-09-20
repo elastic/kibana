6dd54bec0710657f37b3f872220ba85769ec7256
Tim Roes <mail@timroes.de>
2017-09-20 13:45:03 +0300

    Add default aria-label to InfoButton if not specified

--- ui_framework/src/components/info_button/info_button.js
+++ ui_framework/src/components/info_button/info_button.js
@@ -6,14 +6,16 @@ import classNames from 'classnames';
 const KuiInfoButton = props => {
   const iconClasses = classNames('kuiInfoButton', props.className);
 
+  const ariaLabel = props['aria-label'] || 'Info';
   return (
-    <button className={iconClasses} {...props}>
+    <button className={iconClasses} aria-label={ariaLabel} {...props}>
       <span className="kuiIcon fa-info-circle" />
     </button>
   );
 };
 
 KuiInfoButton.propTypes = {
+  'aria-label': PropTypes.string,
   className: PropTypes.string,
 };
 
