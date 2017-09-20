5a67b74716bd877ae55bb7ef1b671930503e3fa7
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-19 22:22:56 -0700

    Explain demodata

--- public/expression_types/datasources/demodata/index.js
+++ public/expression_types/datasources/demodata/index.js
@@ -5,5 +5,14 @@ import header from './header.png';
 export const demodata = () => new Datasource('demodata', {
   displayName: 'Demo Data',
   image: header,
-  template() { return (<div/>); },
+  template() { return (
+    <div>
+      <h3>The demodata source</h3>
+      <p>
+        This data source is connected to every Canvas element by default. Its purpose is to give you lightweight data to
+        use in getting to know an element. The demo data set contains 4 strings, 3 numbers and a date. Feel free to
+        experiment and, when you're ready, click the <i>Change Datasource</i> link above to connect to your own data.
+      </p>
+    </div>
+  ); },
 });
