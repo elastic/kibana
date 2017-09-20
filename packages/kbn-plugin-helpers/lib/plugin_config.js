d12e9902a7048c88fdfb86de1946583ec6e47185
Joe Fleming <w33ble@users.noreply.github.com>
2017-09-20 11:01:47 -0700

    Feat: add translations to default build source paths (elastic/kibana-plugin-helpers#48)
    * feat: add translations path to build
    
    included by default, to better match what's in the elastic products and even the plugin template
    
    also add a translations path to the mock plugin for tests
    
    * chore: add script to run tests with linting
    
    Original commit: elastic/kibana-plugin-helpers@9871a4b11dad7ad820c251c20d4d7a6f46373890

--- packages/kbn-plugin-helpers/lib/plugin_config.js
+++ packages/kbn-plugin-helpers/lib/plugin_config.js
@@ -11,7 +11,7 @@ module.exports = function (root) {
   const buildSourcePatterns = [
     'package.json',
     'index.js',
-    '{lib,public,server,webpackShims}/**/*',
+    '{lib,public,server,webpackShims,translations}/**/*',
   ];
 
   // add shrinkwrap and lock files, if they exist
