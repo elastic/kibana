bc7480634906d2517b4a0fa26ff755e5dedc74ab
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-20 09:31:03 -0700

    Add an asset manage component for deleting unused assets

---
+++ public/components/asset_manager/asset_manager.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './asset_manager.less';
+
+export const AssetManager = ({ assets, removeAsset }) => {
+
+  return (
+    <div className="canvas__asset-manager">
+      <div>
+        <h3>Manage Workpad Assets</h3>
+        <p>
+          Below are the image assets you have added to this workpad via image uploads, sorted by when you added them.
+          Workpads are limited in size. <strong>You can reclaim space</strong> by deleting assets that you no longer needed.
+          Sorry, I can't tell you which assets are currently in use: We're working on that, I promise.
+        </p>
+        <p>
+          I can tell you that the current approximate size of your uncompressed assets
+          is <strong>{Math.round(assets.reduce((total, asset) => total + asset.value.length, 0) / 1024)}KB</strong>
+        </p>
+      </div>
+
+      {assets.map(asset => (
+        <div key={asset.id} className="canvas__asset-manager--thumb" style={{
+          backgroundImage: `url("${asset.value}")`,
+        }}>
+          <i className="fa fa-times-circle canvas__asset-manager--remove" onClick={() => removeAsset(asset.id)}/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+AssetManager.propTypes = {
+  assets: PropTypes.array,
+  removeAsset: PropTypes.func,
+};
