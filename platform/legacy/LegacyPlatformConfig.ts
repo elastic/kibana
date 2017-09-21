400f0c961cbb57eee5dbf2e7e42ec573c95c6ab2
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-09-21 17:29:06 +0200

    Split config transformers into separate functions.

--- platform/legacy/LegacyPlatformConfig.ts
+++ platform/legacy/LegacyPlatformConfig.ts
@@ -2,6 +2,73 @@ import { Observable } from 'rxjs/Observable';
 
 const configProperty = Symbol('legacy-config');
 
+const transformLogging = (configValue: any) => {
+  // Use default `logging` config for now.
+  return {};
+};
+
+const transformXPack = (configValue: any) => {
+  // No `xpack` config support for now.
+  return {};
+};
+
+const transformPid = (configValue: any) => {
+  // Currently pid plugin always expects non-empty pid file path if plugin is not disabled.
+  // TODO: Should be fixed in Pid plugin.
+  if (!configValue.file) {
+    return { enabled: false };
+  }
+
+  return configValue;
+};
+
+const transformServer = (configValue: any) => {
+  // TODO: New platform doesn't support too many fields right now, we should fix it.
+  return {
+    host: configValue.host,
+    port: configValue.port,
+    maxPayload: `${configValue.maxPayloadBytes || 0}b`,
+    basePath: configValue.basePath,
+    ssl: configValue.ssl
+  };
+};
+
+const transformElasticsearch = (configValue: any) => {
+  const clusterTimeoutNames = [
+    'shardTimeout',
+    'requestTimeout',
+    'pingTimeout',
+    'startupTimeout'
+  ];
+
+  // Fix `duration` formats.
+  clusterTimeoutNames.forEach(key => {
+    if (key in configValue) {
+      configValue[key] = `${configValue[key]}ms`;
+    }
+
+    if (key in configValue.tribe) {
+      configValue.tribe[key] = `${configValue.tribe[key]}ms`;
+    }
+  });
+
+  // TODO: Review SSL config format and make it work.
+  configValue.ssl = undefined;
+
+  if (configValue.tribe) {
+    configValue.tribe.ssl = undefined;
+  }
+
+  if (configValue.healthCheck) {
+    // TODO: Implement full `healthCheck` schema.
+    configValue.healthCheck = {
+      delay: `${configValue.healthCheck.delay}ms`
+    };
+  }
+
+  return configValue;
+};
+
 const configProxyHandler = {
   get(target: any, configKey: string): any {
     const config = target[configProperty];
@@ -9,67 +76,26 @@ const configProxyHandler = {
       return undefined;
     }
 
-    // Use default `logging` config for now.
-    if (configKey === 'logging') {
-      return {};
-    }
-
-    // No `xpack` config support for now.
-    if (configKey === 'xpack') {
-      return {};
-    }
-
     const configValue = config.get(configKey);
 
-    // Currently pid plugin always expects non-empty pid file path if plugin is not disabled.
-    // TODO: Should be fixed in Pid plugin.
-    if (configKey === 'pid' && !configValue.file) {
-      return { enabled: false };
+    if (configKey === 'logging') {
+      return transformLogging(configValue);
     }
 
-    // TODO: New platform doesn't support too many fields right now, we should fix it.
+    if (configKey === 'xpack') {
+      return transformXPack(configValue);
+    }
+
+    if (configKey === 'pid') {
+      return transformPid(configValue);
+    }
+
     if (configKey === 'server') {
-      return {
-        host: configValue.host,
-        port: configValue.port,
-        maxPayload: `${configValue.maxPayloadBytes || 0}b`,
-        basePath: configValue.basePath,
-        ssl: configValue.ssl
-      };
+      return transformServer(configValue);
     }
 
     if (configKey === 'elasticsearch') {
-      const clusterTimeoutNames = [
-        'shardTimeout',
-        'requestTimeout',
-        'pingTimeout',
-        'startupTimeout'
-      ];
-
-      // Fix `duration` formats.
-      clusterTimeoutNames.forEach(key => {
-        if (key in configValue) {
-          configValue[key] = `${configValue[key]}ms`;
-        }
-
-        if (key in configValue.tribe) {
-          configValue.tribe[key] = `${configValue.tribe[key]}ms`;
-        }
-      });
-
-      if (
-        configValue.healthCheck &&
-        typeof configValue.healthCheck.delay === 'number'
-      ) {
-        configValue.healthCheck.delay = `${configValue.healthCheck.delay}ms`;
-      }
-
-      // TODO: Review SSL config format and make it work.
-      configValue.ssl = undefined;
-
-      if (configValue.tribe) {
-        configValue.tribe.ssl = undefined;
-      }
+      return transformElasticsearch(configValue);
     }
 
     return configValue;
