18ac2ecea31216f462f3b1c8777c706083bcf8fa
Chris Cowan <chris@chriscowan.us>
2017-09-21 08:35:25 -0700

    [TSVB] Fix chart legend formatting in bottom position (#14084)

--- src/core_plugins/metrics/public/visualizations/lib/create_legend_series.js
+++ src/core_plugins/metrics/public/visualizations/lib/create_legend_series.js
@@ -12,17 +12,22 @@ export default props => (row, i) => {
   const classes = ['rhythm_chart__legend_item'];
   const key = row.id;
   if (!_.includes(props.seriesFilter, row.id)) classes.push('disabled');
-  if (row.label == null || row.legend === false) return (<div key={ key } style={{ display: 'none' }}/>);
+  if (row.label == null || row.legend === false) return (<div key={key} style={{ display: 'none' }}/>);
   return (
     <div
-      className={ classes.join(' ') }
-      onClick={ event => props.onToggle(event, row.id) }
-      key={ key }>
-      <div className="rhythm_chart__legend_label">
-        <i className="fa fa-circle" style={{ color: row.color }}></i>
-        <span>{ row.label }</span>
-      </div>
-      <div className="rhythm_chart__legend_value">{ value }</div>
+      key={key}
+      className={classes.join(' ')}
+    >
+      <button
+        onClick={event => props.onToggle(event, row.id)}
+        className="rhythm_chart__legend_button"
+      >
+        <div className="rhythm_chart__legend_label">
+          <i className="fa fa-circle" style={{ color: row.color }} />
+          <span>{ row.label }</span>
+        </div>
+        <div className="rhythm_chart__legend_value">{ value }</div>
+      </button>
     </div>
   );
 };
