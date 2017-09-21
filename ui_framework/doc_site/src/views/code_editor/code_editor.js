62ee51370491fcbc7caf7fad8516c07479f8f662
Tim Roes <mail@timroes.de>
2017-09-21 17:04:50 +0300

    Don't lose value in KuiCodeEditor example

--- ui_framework/doc_site/src/views/code_editor/code_editor.js
+++ ui_framework/doc_site/src/views/code_editor/code_editor.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
   KuiCodeEditor
 } from '../../../../components';
 
-export default () => (
-  <KuiCodeEditor
-    mode="less"
-    theme="github"
-    width="100%"
-    setOptions={{ fontSize: '14px' }}
-    onBlur={() => window.alert('KuiCodeEditor.onBlur() called')}
-  />
-);
+export default class extends Component {
+  state = {
+    value: ''
+  };
+
+  render() {
+    return (
+      <KuiCodeEditor
+        mode="less"
+        theme="github"
+        width="100%"
+        value={this.state.value}
+        onChange={(value) => this.setState({ value })}
+        setOptions={{ fontSize: '14px' }}
+        onBlur={() => window.alert('KuiCodeEditor.onBlur() called')}
+      />
+    );
+  }
+}
