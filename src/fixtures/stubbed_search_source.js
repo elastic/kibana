404e12c2d287521c3ec90a245d33aff8ac4a8be1
Spencer <spalger@users.noreply.github.com>
2017-09-22 02:40:47 -0700

    [vis/requesting] only call when actually requesting (#14017)
    * [vis/requesting] only call when actually requesting
    
    * [courier/dataSource] use Promise.map to catch sync errors
    
    * [courier/request] move source mutation logic out of request
    
    (cherry picked from commit b0dc2469c7033f287f96eafe9560b6b36808cf58)

--- src/fixtures/stubbed_search_source.js
+++ src/fixtures/stubbed_search_source.js
@@ -37,7 +37,15 @@ export default function stubSearchSource(Private, $q, Promise) {
     },
     _flatten: function () {
       return Promise.resolve({ index: indexPattern, body: {} });
-    }
+    },
+    _requestStartHandlers: [],
+    onRequestStart(fn) {
+      this._requestStartHandlers.push(fn);
+    },
+    requestIsStarting(req) {
+      return Promise.map(this._requestStartHandlers, fn => fn(req));
+    },
+    requestIsStopped() {}
   };
 
 }
