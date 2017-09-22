5c8a1ed8a94efcd303995dfd5a7fd29d9a26fa1e
Rashid Khan <github.fliplap@spamgourmet.com>
2017-09-22 15:36:08 -0700

    alignment buttons

--- public/expression_types/arg_types/font/extended_template.js
+++ public/expression_types/arg_types/font/extended_template.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, ButtonGroup, FormGroup, FormControl } from 'react-bootstrap';
-import { get } from 'lodash';
-import { set, del } from 'object-path-immutable';
-import { LabeledInput } from '../../../components/labeled_input';
+import { Button, ButtonGroup, FormControl } from 'react-bootstrap';
+//import { get } from 'lodash';
+//import { set, del } from 'object-path-immutable';
+//import { LabeledInput } from '../../../components/labeled_input';
 import { FontPicker } from '../../../components/font_picker';
 import { fontSizes } from './font_sizes';
 
-export const extendedTemplate = (props) => {
-  const { typeInstance, onValueChange, labels, argValue } = props;
-  const chain = get(argValue, 'chain.0', {});
-  const chainArgs = get(chain, 'arguments', {});
+export const extendedTemplate = (/*props*/) => {
+  //const { typeInstance, onValueChange, labels, argValue } = props;
+  //const chain = get(argValue, 'chain.0', {});
+  //const chainArgs = get(chain, 'arguments', {});
 
   // TODO: Validate input
 
   return (
     <div>
-      <div style={{ display: 'inline-block', width: 80 }}>
-        <FormControl componentClass="select" placeholder="select">
-          {fontSizes.map(size => (
-            <option key={ size } value={size}>{ size }</option>
-          ))}
-        </FormControl>
+      <div>
+        <div style={{ display: 'inline-block', width: 60 }}>
+          <FormControl componentClass="select" placeholder="select">
+            {fontSizes.map(size => (
+              <option key={ size } value={size}>{ size }</option>
+            ))}
+          </FormControl>
+        </div>
+        <FontPicker
+          value="'Gill Sans', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Helvetica, Arial, sans-serif"
+          onSelect={console.log}
+        />
       </div>
-      <FontPicker
-        value="'Gill Sans', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Helvetica, Arial, sans-serif"
-        onSelect={console.log}
-      />
-      <ButtonGroup bsSize="small">
-        <Button><span style={{ fontWeight: 'bold' }}>B</span></Button>
-        <Button><span style={{ fontStyle: 'italic' }}>I</span></Button>
-        <Button><span style={{ textDecoration: 'underline' }}>U</span></Button>
-      </ButtonGroup>
+      <div>
+        <ButtonGroup bsSize="small">
+          <Button><span style={{ fontWeight: 'bold' }}>B</span></Button>
+          <Button><span style={{ fontStyle: 'italic' }}>I</span></Button>
+          <Button><span style={{ textDecoration: 'underline' }}>U</span></Button>
+        </ButtonGroup>
 
+        <ButtonGroup bsSize="small">
+          <Button><i className="fa fa-align-left"/></Button>
+          <Button><i className="fa fa-align-center"/></Button>
+          <Button><i className="fa fa-align-right"/></Button>
+        </ButtonGroup>
+      </div>
     </div>
-
   );
 };
 
