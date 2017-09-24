6ef8534ff9fc12efcd4b3b603d986b85c6d690ac
CJ Cenizal <cj@cenizal.com>
2017-09-24 10:49:49 -0700

    Add FocusTrap to KuiModal.

--- ui_framework/doc_site/src/views/modal/modal_example.js
+++ ui_framework/doc_site/src/views/modal/modal_example.js
@@ -12,37 +12,12 @@ import {
   KuiCode,
 } from '../../../../components';
 
-import { StaticConfirmModal } from './static';
-const staticConfirmModalSource = require('!!raw!./static');
-const staticConfirmModalHtml = renderToHtml(StaticConfirmModal);
-
 import { ConfirmModalExample } from './confirm_modal_example';
 const showConfirmModalSource = require('!!raw!./confirm_modal_example');
 const showConfirmModalHtml = renderToHtml(ConfirmModalExample);
 
 export default props => (
   <GuidePage title={props.route.name}>
-
-    <GuideSection
-      title="Confirmation Modal"
-      source={[{
-        type: GuideSectionTypes.JS,
-        code: staticConfirmModalSource,
-      }, {
-        type: GuideSectionTypes.HTML,
-        code: staticConfirmModalHtml,
-      }]}
-      text={
-        <p>
-          <KuiCode>KuiConfirmModal</KuiCode> loaded staticly onto the page. More
-          likely you want the popup version, shown in the next example.
-        </p>
-      }
-      demo={
-        <StaticConfirmModal />
-      }
-    />
-
     <GuideSection
       title="Pop up Confirmation Modal with Overlay"
       source={[{
