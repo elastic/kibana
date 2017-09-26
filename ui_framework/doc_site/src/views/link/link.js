064b116f9983fc109a78c604a9c650a54f4152e9
Dave Snider <dave.snider@gmail.com>
2017-09-26 13:24:44 -0700

    links are a button when an onclick is present. add colors to links

--- ui_framework/doc_site/src/views/link/link.js
+++ ui_framework/doc_site/src/views/link/link.js
@@ -17,5 +17,48 @@ export default () => (
         </KuiLink>
       )} in a new tab.
     </p>
+    <p>
+      This link is actually a {(
+        <KuiLink
+          onClick={() => window.alert('Button clicked')}
+        >
+          button
+        </KuiLink>
+      )} with an onClick event.
+
+    </p>
+    <p>Links can be colored as well.</p>
+    <ul>
+      <li>
+        <KuiLink color="subdued" href="#">
+          subdued
+        </KuiLink>
+      </li>
+      <li>
+        <KuiLink color="secondary" href="#">
+          secondary
+        </KuiLink>
+      </li>
+      <li>
+        <KuiLink color="accent" href="#">
+          accent
+        </KuiLink>
+      </li>
+      <li>
+        <KuiLink color="danger" href="#">
+          danger
+        </KuiLink>
+      </li>
+      <li>
+        <KuiLink color="warning" href="#">
+          warning
+        </KuiLink>
+      </li>
+      <li>
+        <KuiLink color="ghost" href="#">
+          ghost
+        </KuiLink>
+      </li>
+    </ul>
   </KuiText>
 );
