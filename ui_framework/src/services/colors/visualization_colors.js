ecfd5390599686f82d97c07755a6a32c0d40a581
Dave Snider <dave.snider@gmail.com>
2017-09-27 14:16:48 -0700

    replace fff with a ghost var

---
+++ ui_framework/src/services/colors/visualization_colors.js
@@ -0,0 +1,16 @@
+
+// Array of color-blind safe colors to use in visualizations or other
+// spots that need a large range of varied, qualitative colors.
+
+export const VISUALIZATION_COLORS = [
+  '#00B3A4',
+  '#3185FC',
+  '#DB1374',
+  '#490092',
+  '#FEB6DB',
+  '#E6C220',
+  '#F98510',
+  '#BFA180',
+  '#461A0A',
+  '#920000',
+];
