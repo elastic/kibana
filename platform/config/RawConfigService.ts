7893ab0162803bb2aa326c43f2fc3d76243948de
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-09-28 17:24:21 +0200

    Update `RawConfigService` tests.

--- platform/config/RawConfigService.ts
+++ platform/config/RawConfigService.ts
@@ -5,9 +5,28 @@ import typeDetect from 'type-detect';
 import { ConfigPath } from './ConfigService';
 import { getConfigFromFile } from './readConfig';
 
+/**
+ * Represents raw config store.
+ */
 export interface RawConfig {
+  /**
+   * Returns config value located at the specified path.
+   * @param configPath Path to locate value at.
+   * @returns Config value.
+   */
   get(configPath: ConfigPath): any;
+
+  /**
+   * Sets config value at the specified path.
+   * @param configPath Path to set value for.
+   * @param value Value to set for the specified path.
+   */
   set(configPath: ConfigPath, value: any): void;
+
+  /**
+   * Returns full flattened list of the config paths that config contains.
+   * @returns List of the string config paths.
+   */
   getFlattenedPaths(): string[];
 }
 
