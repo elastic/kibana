7a94a4926df6706e5463cd6e70640b24c33c21de
dave.snider@gmail.com <dave.snider@gmail.com>
2017-09-28 10:53:30 -0700

    [k7][UI Framework]: Advanced settings cleanup (#14211)
    * add ghost docs, proper prop type
    
    * advanced settings titling

--- ui_framework/src/components/form/form_row/form_row.js
+++ ui_framework/src/components/form/form_row/form_row.js
@@ -116,7 +116,7 @@ KuiFormRow.propTypes = {
   id: PropTypes.string,
   isInvalid: PropTypes.bool,
   error: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
-  helpText: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  helpText: PropTypes.node,
   hasEmptyLabelSpace: PropTypes.bool,
 };
 
