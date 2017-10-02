3ef3912e3434f1d44d9276de6c22613892c9fb4c
Tim Roes <tim.roes@elastic.co>
2017-10-02 12:42:43 +0200

    KuiGalleryItem automatically becomes link or button

--- ui_framework/src/components/gallery/gallery_item/gallery_item.test.js
+++ ui_framework/src/components/gallery/gallery_item/gallery_item.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import sinon from 'sinon';
 import { render } from 'enzyme';
 import { requiredProps } from '../../../test/required_props';
 
@@ -6,7 +7,26 @@ import {
   KuiGalleryItem,
 } from './gallery_item';
 
-test('renders KuiGalleryItem href', () => {
+test('renders KuiGalleryItem with href', () => {
   const component = <KuiGalleryItem href="#" {...requiredProps}>children</KuiGalleryItem>;
   expect(render(component)).toMatchSnapshot();
+});
+
+test('renders KuiGalleryItem with onClick', () => {
+  const component = <KuiGalleryItem onClick={() => {}} {...requiredProps}>children</KuiGalleryItem>;
+  expect(render(component)).toMatchSnapshot();
+});
+
+test('renders KuiGalleryItem without href and onClick', () => {
+  const component = <KuiGalleryItem {...requiredProps}>children</KuiGalleryItem>;
+  expect(render(component)).toMatchSnapshot();
+});
+
+test('KuiGalleryItem will throw when specified href and onClick', () => {
+  const consoleError = sinon.stub(console, 'error');
+  render(<KuiGalleryItem href="#" onClick={() => {}} {...requiredProps}>children</KuiGalleryItem>);
+  expect(consoleError.calledOnce).toBe(true);
+  const msg = consoleError.getCalls()[0].args[0];
+  expect(msg).toContain('Failed prop type');
+  console.error.restore();
 });
