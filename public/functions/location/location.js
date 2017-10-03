dbe564ea58e27e242c85aa50a03d04d6163c69ee
Rashid Khan <github.fliplap@spamgourmet.com>
2017-10-03 16:52:39 -0700

    Return location object instead

--- public/functions/location/location.js
+++ public/functions/location/location.js
@@ -2,13 +2,19 @@ import Fn from '../../../common/functions/fn.js';
 
 export default new Fn({
   name: 'location',
-  type: 'string',
+  type: 'location',
   help: 'Get the users location',
   fn: () => {
     return new Promise((resolve) => {
       function createLocation(geoposition) {
         const { latitude, longitude } = geoposition.coords;
-        return resolve(`${latitude}, ${longitude}`);
+        return resolve(
+          {
+            type: 'location',
+            longitude,
+            latitude,
+          }
+        );
       }
       return navigator.geolocation.getCurrentPosition(createLocation);
     });
