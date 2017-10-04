70e5c23d69b1709f8303e385153471f9d37235cd
ppisljar <peter.pisljar@gmail.com>
2017-10-04 17:56:48 +0200

    adding loader function

---
+++ src/ui/public/visualize/loader.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+
+export function VisualizeLoaderProvider($compile, $rootScope, savedVisualizations) {
+  return (el, savedVisualizationId, timeRange) => {
+    savedVisualizations.get(savedVisualizationId).then(savedObj => {
+      const scope = $rootScope.$new();
+      scope.savedObj = savedObj;
+      scope.timeRange = timeRange;
+
+      $(el).html('');
+      const visEl = $('<visualize saved-obj="savedObj" time-range="timeRange"></visualize>');
+      const visHtml = $compile(visEl)(scope);
+      $(el).html(visHtml);
+    });
+  };
+}
