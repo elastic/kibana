065c4ebc95f790496b8807924e5b8a9aa1d8bdc3
ppisljar <peter.pisljar@gmail.com>
2017-10-04 19:46:37 +0200

    allow to pass appState and uiState to loader function

--- src/ui/public/visualize/loader.js
+++ src/ui/public/visualize/loader.js
@@ -1,14 +1,16 @@
 import $ from 'jquery';
 
 export function VisualizeLoaderProvider($compile, $rootScope, savedVisualizations) {
-  return (el, savedVisualizationId, timeRange) => {
+  return (el, savedVisualizationId, timeRange, appState, uiState) => {
     savedVisualizations.get(savedVisualizationId).then(savedObj => {
       const scope = $rootScope.$new();
       scope.savedObj = savedObj;
+      scope.appState = appState;
+      scope.uiState = uiState;
       scope.timeRange = timeRange;
 
       $(el).html('');
-      const visEl = $('<visualize saved-obj="savedObj" time-range="timeRange"></visualize>');
+      const visEl = $('<visualize saved-obj="savedObj" app-state="appState" ui-state="uiState" time-range="timeRange"></visualize>');
       const visHtml = $compile(visEl)(scope);
       $(el).html(visHtml);
       return visEl[0];
