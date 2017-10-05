c18fba317e03d3dabe9347168077b62cd320bfb7
Matt Apperson <matthew.apperson@elastic.co>
2017-10-05 14:34:37 -0400

    color_picker using ui_framework

--- src/core_plugins/metrics/public/components/color_picker.js
+++ src/core_plugins/metrics/public/components/color_picker.js
@@ -4,100 +4,45 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import Tooltip from './tooltip';
-import Picker from './custom_color_picker';
+import { KuiColorPicker } from 'ui_framework/components';
 
 class ColorPicker extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      displayPlicker: false,
       color: {}
     };
 
-    this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleClear = this.handleClear.bind(this);
-    this.handleClose = this.handleClose.bind(this);
   }
 
   handleChange(color) {
-    const { rgb } = color;
-    const part = {};
-    part[this.props.name] = `rgba(${rgb.r},${rgb.g},${rgb.b},${rgb.a})`;
-    if (this.props.onChange) this.props.onChange(part);
-  }
-
-  handleClick() {
-    this.setState({ displayPicker: !this.state.displayColorPicker });
-  }
-
-  handleClose() {
-    this.setState({ displayPicker: false });
+    if (this.props.onChange) this.props.onChange({ [this.props.name]: color });
   }
 
   handleClear() {
-    const part = {};
-    part[this.props.name] = null;
-    this.props.onChange(part);
-  }
-
-  renderSwatch() {
-    if (!this.props.value) {
-      return (
-        <button
-          aria-label="Color picker, not accessible"
-          className="vis_editor__color_picker-swatch-empty"
-          onClick={this.handleClick}
-        />
-      );
-    }
-    return (
-      <button
-        aria-label={`Color picker ({this.props.value}), not accessible`}
-        style={{ backgroundColor: this.props.value }}
-        className="vis_editor__color_picker-swatch"
-        onClick={this.handleClick}
-      />
-    );
+    this.props.onChange({
+      [this.props.name]: null
+    });
   }
 
   render() {
-    const swatch = this.renderSwatch();
-    const value = this.props.value || undefined;
-    let clear;
-    if (!this.props.disableTrash) {
-      clear = (
-        <div className="vis_editor__color_picker-clear" onClick={this.handleClear}>
-          <Tooltip text="Clear">
-            <i className="fa fa-ban"/>
-          </Tooltip>
-        </div>
-      );
-    }
+    const { value, disableTrash } = this.props;
+
     return (
-      <div className="vis_editor__color_picker">
-        { swatch }
-        { clear }
-        {
-          this.state.displayPicker
-          ? (
-            <div className="vis_editor__color_picker-popover">
-              <div
-                className="vis_editor__color_picker-cover"
-                onClick={this.handleClose}
-              />
-              <Picker
-                color={value}
-                onChangeComplete={this.handleChange}
-              />
-            </div>
-          ) : null
-        }
-      </div>
+      <span>
+        {!disableTrash && (
+          <div className="vis_editor__color_picker-clear" onClick={this.handleClear}>
+            <Tooltip text="Clear">
+              <i className="fa fa-ban" />
+            </Tooltip>
+          </div>
+        )}
+        <KuiColorPicker onChange={this.handleChange} color={value} showColorLabel={false} />
+      </span>
     );
   }
-
 }
 
 ColorPicker.propTypes = {
