9af528cf6ef3ea3cfca5c6406e41804d2331176f
Chris Roberson <chrisronline@gmail.com>
2017-10-05 11:53:24 -0400

    PR feedback

--- src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/create_index_pattern_wizard.js
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/create_index_pattern_wizard.js
@@ -28,8 +28,13 @@ uiModules.get('apps/management')
   Notifier,
   Promise
 ) {
-  const MAX_SEARCH_SIZE = 40;
+  // This isn't ideal. We want to avoid searching for 20 indices
+  // then filtering out the majority of them because they are sysetm indices.
+  // We'd like to filter system indices out in the query
+  // so if we can accomplish that in the future, this logic can go away
+  const ESTIMATED_NUMBER_OF_SYSTEM_INDICES = 20;
   const MAX_NUMBER_OF_MATCHING_INDICES = 20;
+  const MAX_SEARCH_SIZE = MAX_NUMBER_OF_MATCHING_INDICES + ESTIMATED_NUMBER_OF_SYSTEM_INDICES;
   const notify = new Notifier();
   const disabledDividerOption = {
     isDisabled: true,
@@ -110,12 +115,12 @@ uiModules.get('apps/management')
       return indices;
     }
 
-    if (this.doesIncludeSystemIndices) {
-      return indices;
-    }
+    const acceptableIndices = this.doesIncludeSystemIndices
+      ? indices
+      // All system indices begin with a period.
+      : indices.filter(index => !index.name.startsWith('.'));
 
-    // All system indices begin with a period.
-    return indices.filter(index => !index.name.startsWith('.')).slice(0, MAX_NUMBER_OF_MATCHING_INDICES);
+    return acceptableIndices.slice(0, MAX_NUMBER_OF_MATCHING_INDICES);
   };
 
   const updateWhiteListedIndices = () => {
@@ -166,10 +171,10 @@ uiModules.get('apps/management')
 
   this.fetchExistingIndices = () => {
     this.isFetchingExistingIndices = true;
-    const allExistingLocalAndRemoteIndicesPattern = '*';
+    const allExistingLocalIndicesPattern = '*';
 
     Promise.all([
-      getIndices(allExistingLocalAndRemoteIndicesPattern),
+      getIndices(allExistingLocalIndicesPattern),
       createReasonableWait()
     ])
     .then(([allIndicesResponse]) => {
