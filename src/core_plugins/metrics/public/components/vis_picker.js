c576f06ed6c3b8eec9603a04f8728fc9cf35f734
Matt Apperson <matthew.apperson@elastic.co>
2017-10-05 13:25:59 -0400

    vis-picker using ui_framework

--- src/core_plugins/metrics/public/components/vis_picker.js
+++ src/core_plugins/metrics/public/components/vis_picker.js
@@ -1,66 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { KuiLocalNav, KuiLocalNavRow, KuiLocalTab, KuiLocalTabs } from 'ui_framework/components';
 
-function VisPickerItem(props) {
-  const { label, icon, type } = props;
-  let itemClassName = 'vis_editor__vis_picker-item';
-  let iconClassName = 'vis_editor__vis_picker-icon';
-  let labelClassName = 'vis_editor__vis_picker-label';
-  if (props.selected) {
-    itemClassName += ' selected';
-    iconClassName += ' selected';
-    labelClassName += ' selected';
-  }
-  return (
-    <button role="tab" className={itemClassName} onClick={() => props.onClick(type)}>
-      <div className={iconClassName}>
-        <i className={`fa ${icon}`} />
-      </div>
-      <div className={labelClassName}>
-        { label }
-      </div>
-    </button>
-  );
-}
+const nav = [
+  { type: 'timeseries', label: 'Time Series' },
+  { type: 'metric', label: 'Metric' },
+  { type: 'top_n', label: 'Top N' },
+  { type: 'gauge', label: 'Gauge' },
+  { type: 'markdown', label: 'Markdown' }
+];
 
-VisPickerItem.propTypes = {
-  icon: PropTypes.string,
-  label: PropTypes.string,
-  onClick: PropTypes.func,
-  type: PropTypes.string,
-  selected: PropTypes.bool
-};
-
-function VisPicker(props) {
-  const handleChange = (type) => {
-    props.onChange({ type });
-  };
-
-  const { model } = props;
-  const icons = [
-    { type: 'timeseries', icon: 'fa-line-chart', label: 'Time Series' },
-    { type: 'metric', icon: 'fa-superscript', label: 'Metric' },
-    { type: 'top_n', icon: 'fa-bar-chart fa-rotate-90', label: 'Top N' },
-    { type: 'gauge', icon: 'fa-circle-o-notch', label: 'Gauge' },
-    { type: 'markdown', icon: 'fa-paragraph', label: 'Markdown' }
-  ].map(item => {
-    return (
-      <VisPickerItem
-        key={item.type}
-        onClick={handleChange}
-        selected={item.type === model.type}
-        {...item}
-      />
-    );
-  });
-
-  return (
-    <div className="vis_editor__vis_picker-container" role="tablist">
-      { icons }
-    </div>
-  );
-
-}
+const VisPicker = ({ onChange, model }) => (
+  <KuiLocalNav>
+    <KuiLocalNavRow isSecondary>
+      <KuiLocalTabs>
+        {nav.map(({ label, type }) => (
+          <KuiLocalTab onClick={() => onChange({ type })} isSelected={type === model.type}>
+            {label}
+          </KuiLocalTab>
+        ))}
+      </KuiLocalTabs>
+    </KuiLocalNavRow>
+  </KuiLocalNav>
+);
 
 VisPicker.propTypes = {
   model: PropTypes.object,
