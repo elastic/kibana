d2465af90fabce36b9ca18222cf2e66c8e2a5e78
Rashid Khan <github.fliplap@spamgourmet.com>
2017-10-06 16:12:18 -0700

    Add jsonquery and escount

---
+++ common/functions/jsonquery/jsonquery.js
@@ -0,0 +1,37 @@
+import Fn from '../fn.js';
+import jsonQuery from 'json-query';
+import { isArray, get } from 'lodash';
+
+// TODO: Decide if we actually want to get data from table like this
+// We need *some* way of accessing the data in tables, that could be via a collection of our own functions
+// Or via something like this. I'm using JSON Query for the moment because it is easy, but we should make
+// an actual decision, and replace this if needed.
+export default new Fn({
+  name: 'jsonquery',
+  aliases: [],
+  type: 'string',
+  help: 'Retrieve a string from a datatable using JSON Query. (See https://github.com/mmckegg/json-query)',
+  context: {
+    types: [
+      'datatable',
+    ],
+  },
+  args: {
+    _: {
+      types: [
+        'string',
+      ],
+      'aliases': ['q', 'query'],
+      'multi': false,
+      help: 'A JSON Query to run on the datatable rows.',
+    },
+  },
+  fn: (context, args) => {
+    const result = get(jsonQuery(args._, {
+      data: context.rows,
+    }), 'value');
+
+    if (isArray(result)) return result.map(item => String(item)).join(', ');
+    return String(result);
+  },
+});
---
+++ server/functions/escount/escount.js
@@ -0,0 +1,48 @@
+import Fn from '../../../common/functions/fn.js';
+import { buildESRequest } from '../esdocs/lib/build_es_request';
+
+export default new Fn({
+  name: 'escount',
+  context: {
+    types: ['filter'],
+  },
+  args: {
+    index: {
+      types: ['string', 'null'],
+      default: '_all',
+    },
+    q: {
+      types: ['string'],
+      aliases: ['query'],
+      help: 'A Lucene query string',
+      default: '"-_index:.kibana"',
+    },
+  },
+  type: 'number',
+  help: 'Query elasticsearch and get back raw documents.',
+  fn: (context, args, handlers) => {
+    context.and = context.and
+      .concat([{ // q
+        type: 'luceneQueryString',
+        query: args.q,
+      }]);
+
+    const esRequest = buildESRequest({
+      index: args.index,
+      body: {
+        query: {
+          bool: {
+            must: [ { match_all: {} } ],
+          },
+        },
+        size: 0,
+      },
+    }, context);
+
+    return handlers
+      .elasticsearchClient('search', esRequest)
+      .then(resp => {
+        return resp.hits.total;
+      });
+  },
+});
