e0059b467b8ed042148b0b56351ed1cc4d78d52d
ppisljar <peter.pisljar@gmail.com>
2017-10-06 11:40:36 +0200

    updating to support bucket aggs and other options

--- src/core_plugins/metric_vis/public/metric_vis.js
+++ src/core_plugins/metric_vis/public/metric_vis.js
@@ -1,11 +1,12 @@
 import 'plugins/metric_vis/metric_vis.less';
+import 'plugins/metric_vis/metric_vis_params';
 import 'plugins/metric_vis/metric_vis_controller';
 import { VisFactoryProvider } from 'ui/vis/vis_factory';
 import { CATEGORY } from 'ui/vis/vis_category';
 import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
 import metricVisTemplate from 'plugins/metric_vis/metric_vis.html';
-import metricVisParamsTemplate from 'plugins/metric_vis/metric_vis_params.html';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import { vislibColorMaps } from 'ui/vislib/components/color/colormaps';
 import image from './images/icon-number.svg';
 // we need to load the css ourselves
 
@@ -28,28 +29,56 @@ function MetricVisProvider(Private) {
     category: CATEGORY.DATA,
     visConfig: {
       defaults: {
-        handleNoResults: true,
-        fontSize: 60
+        addTooltip: true,
+        addLegend: false,
+        type: 'metric',
+        metric: {
+          percentageMode: true,
+          useRanges: false,
+          colorSchema: 'Green to Red',
+          metricColorMode: 'None',
+          colorsRange: [
+            { from: 0, to: 10000 }
+          ],
+          invertColors: false,
+          style: {
+            bgFill: '#000',
+            bgColor: false,
+            labelColor: false,
+            subText: '',
+            fontSize: 60,
+          }
+        }
       },
       template: metricVisTemplate,
     },
     editorConfig: {
-      optionsTemplate: metricVisParamsTemplate,
+      collections: {
+        metricColorMode: ['None', 'Labels', 'Background'],
+        colorSchemas: Object.keys(vislibColorMaps),
+      },
+      optionsTemplate: '<metric-vis-params></metric-vis-params>',
       schemas: new Schemas([
         {
           group: 'metrics',
           name: 'metric',
           title: 'Metric',
           min: 1,
-          aggFilter: ['!derivative', '!geo_centroid', '!cumulative_sum', '!moving_avg', '!serial_diff'],
+          aggFilter: [
+            '!std_dev', '!geo_centroid', '!percentiles', '!percentile_ranks',
+            '!derivative', '!serial_diff', '!moving_avg', '!cumulative_sum','!geo_bounds'],
           defaults: [
             { type: 'count', schema: 'metric' }
           ]
+        }, {
+          group: 'buckets',
+          name: 'group',
+          title: 'Split Group',
+          min: 0,
+          max: 1,
+          aggFilter: ['!geohash_grid', '!filter']
         }
       ])
-    },
-    responseHandlerConfig: {
-      asAggConfigResults: true
     },
     implementsRenderComplete: true,
   });
--- src/core_plugins/metric_vis/public/metric_vis_controller.js
+++ src/core_plugins/metric_vis/public/metric_vis_controller.js
@@ -1,4 +1,6 @@
+import _ from 'lodash';
 import { uiModules } from 'ui/modules';
+import { getHeatmapColors } from 'ui/vislib/components/color/heatmap_color';
 // get the kibana/metric_vis module, and make sure that it requires the "kibana" module if it
 // didn't already
 const module = uiModules.get('kibana/metric_vis', ['kibana']);
@@ -6,15 +8,103 @@ const module = uiModules.get('kibana/metric_vis', ['kibana']);
 module.controller('KbnMetricVisController', function ($scope, $element) {
 
   const metrics = $scope.metrics = [];
+  let labels = [];
+  let colors = [];
+
+  const getLabels = () => {
+    const config = $scope.vis.params.metric;
+    const isPercentageMode = config.percentageMode;
+    const colorsRange = config.colorsRange;
+    const max = _.last(colorsRange).to;
+    const labels = [];
+    colorsRange.forEach(range => {
+      const from = isPercentageMode ? Math.round(100 * range.from / max) : range.from;
+      const to = isPercentageMode ? Math.round(100 * range.to / max) : range.to;
+      labels.push(`${from} - ${to}`);
+    });
+
+    return labels;
+  };
+
+  const getColors = () => {
+    const config = $scope.vis.params.metric;
+    const invertColors = config.invertColors;
+    const colorSchema = config.colorSchema;
+    const colorsRange = config.colorsRange;
+    const labels = getLabels();
+    const colors = {};
+    for (let i = 0; i < labels.length; i += 1) {
+      const divider = Math.max(colorsRange.length - 1, 1);
+      const val = invertColors ? 1 - i / divider : i / divider;
+      colors[labels[i]] = getHeatmapColors(val, colorSchema);
+    }
+    return colors;
+  };
+
+  const getBucket = (val) => {
+    const config = $scope.vis.params.metric;
+    let bucket = _.findIndex(config.colorsRange, range => {
+      return range.from <= val && range.to > val;
+    });
+
+    if (bucket === -1) {
+      if (val < config.colorsRange[0].from) bucket = 0;
+      else bucket = config.colorsRange.length - 1;
+    }
+
+    return bucket;
+  };
+
+  const getColor = (val) => {
+    const bucket = getBucket(val);
+    const label = labels[bucket];
+    return colors[label];
+  };
 
   $scope.processTableGroups = function (tableGroups) {
-    tableGroups.tables.forEach(function (table) {
-      table.columns.forEach(function (column, i) {
-        const value = table.rows[0][i];
+    const config = $scope.vis.params.metric;
+    const isPercentageMode = config.percentageMode;
+    const min = config.colorsRange[0].from;
+    const max = _.last(config.colorsRange).to;
 
-        metrics.push({
-          label: column.title,
-          value: value.toString('html')
+    tableGroups.tables.forEach(function (table) {
+      let bucketAgg;
+
+      table.columns.forEach(function (column, i) {
+        const aggConfig = column.aggConfig;
+
+        if (aggConfig.schema.group === 'buckets') {
+          bucketAgg = aggConfig;
+          return;
+        }
+
+        table.rows.forEach(row => {
+
+          let title = column.title;
+          let value = row[i];
+          const color = getColor(value);
+
+          if (isPercentageMode) {
+            const percentage = Math.round(100 * (value - min) / (max - min));
+            value = `${percentage}%`;
+          }
+
+          if (aggConfig) {
+            if (!isPercentageMode) value = aggConfig.fieldFormatter('html')(value);
+            if (bucketAgg) {
+              const bucketValue = bucketAgg.fieldFormatter('text')(row[0]);
+              title = `${bucketValue} - ${aggConfig.makeLabel()}`;
+            } else {
+              title = aggConfig.makeLabel();
+            }
+          }
+
+          metrics.push({
+            label: title,
+            value: value,
+            color: config.style.labelColor ? color : null,
+            bgColor: config.style.bgColor ? color : null
+          });
         });
       });
     });
@@ -23,6 +113,10 @@ module.controller('KbnMetricVisController', function ($scope, $element) {
   $scope.$watch('esResponse', function (resp) {
     if (resp) {
       metrics.length = 0;
+      labels.length = 0;
+      colors.length = 0;
+      colors = getColors();
+      labels = getLabels();
       $scope.processTableGroups(resp);
       $element.trigger('renderComplete');
     }
