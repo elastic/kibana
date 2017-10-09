7214734b5e0e2f3d6406d50691842f579becded8
Rashid Khan <github.fliplap@spamgourmet.com>
2017-10-09 14:21:02 -0700

    Add {if}

---
+++ common/functions/if/if.js
@@ -0,0 +1,33 @@
+import Fn from '../fn.js';
+
+export default new Fn({
+  name: 'if',
+  help: 'Perform conditional logic',
+  args: {
+    _: {
+      types: [
+        'boolean',
+      ],
+      default: 0,
+      'aliases': ['condition'],
+      help: 'A boolean true or false, usually returned by a subexpression',
+    },
+    then: {
+      help: 'The return value if true',
+    },
+    else: {
+      help: `The return value if false. If else is not specified, and the condition is false,
+then the input context to the function will be returned`,
+    },
+    // TODO: When we add "action" functions, this could be used a perform an action without
+    passthru: {
+      help: 'Instead of returning the value of then, simply execute it and pass the input context through',
+      default: false,
+    },
+  },
+  fn: (context, args) => {
+    if (args._) return args.then;
+    if (typeof args.else !== 'undefined') return args.else;
+    return context;
+  },
+});
