1fe16fa0b3b87740eaee7aba8dc01109c0f633a7
Court Ewing <court@epixa.com>
2017-10-10 14:28:51 -0400

    test: move es proxy tests to api_integration
    Integration tests should go into the corresponding directory under
    `test` and should use the appropriate testing framework that is designed
    for long-running tests like those. Tests under __tests__ directories
    should be fast-running unit tests only.

--- test/api_integration/apis/index.js
+++ test/api_integration/apis/index.js
@@ -1,5 +1,6 @@
 export default function ({ loadTestFile }) {
   describe('apis', () => {
+    loadTestFile(require.resolve('./elasticsearch'));
     loadTestFile(require.resolve('./index_patterns'));
     loadTestFile(require.resolve('./saved_objects'));
     loadTestFile(require.resolve('./scripts'));
