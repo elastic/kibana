e635d25b26bcb8e71c45e1d69f709a406af1d1aa
Court Ewing <court@epixa.com>
2017-10-10 13:27:47 -0400

    fork flot into Kibana (#14366)
    The flot-charts project has been abandoned since 2014, but we use it
    pretty extensively in Kibana. By forking it, we can fix bugs and improve
    on the library in ways that we need to for Kibana.

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -15,6 +15,7 @@ module.exports = function () {
         '!src/es_archiver/**',
         '!src/functional_test_runner/**',
         'bin/**',
+        'packages/flot-charts/**',
         'ui_framework/components/**',
         'ui_framework/services/**',
         'ui_framework/dist/**',
