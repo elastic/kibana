6f9d5635258adf700ae8155a027fbcb19569f886
Tim Roes <tim.roes@elastic.co>
2017-10-12 11:00:45 +0200

    Remove ua-parser-js from overwrites

--- tasks/config/licenses.js
+++ tasks/config/licenses.js
@@ -52,7 +52,6 @@ module.exports = function () {
         'jsonify@0.0.0': ['Public domain'],
         'ripemd160@0.2.0': ['MIT'],
         'select@1.0.6': ['MIT'],
-        'ua-parser-js@0.7.14': ['MIT'],
         'uglify-js@2.2.5': ['BSD'],
       }
     }
