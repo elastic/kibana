3400a353515db759a68502b4c80a3af110345ee3
Rashid Khan <github.fliplap@spamgourmet.com>
2017-10-12 17:35:20 -0700

    Show formatted dates

--- public/components/datatable/datatable.js
+++ public/components/datatable/datatable.js
@@ -3,6 +3,7 @@ import { Table } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { Paginate } from '../paginate';
 import { PageControls } from '../paginate_controls';
+import moment from 'moment';
 import './datatable.less';
 
 const getIcon = (type) => {
@@ -24,6 +25,11 @@ const getColumnName = col => (typeof col === 'string') ? col : col.name;
 
 const getColumnType = col => col.type || null;
 
+const getFormattedValue = (val, type) => {
+  if (type === 'date') return moment(val).format();
+  return val;
+};
+
 export const Datatable = ({ datatable, perPage, paginate }) => (
   <Paginate rows={datatable.rows} perPage={perPage || 10}>
     {({ rows, nextPage, prevPage, setPage, prevPageEnabled, nextPageEnabled, pageNumber, totalPages }) => (
@@ -43,7 +49,9 @@ export const Datatable = ({ datatable, perPage, paginate }) => (
               {rows.map((row, i) => (
                 <tr key={`row-${row._rowId || i}`}>
                   {datatable.columns.map(col => (
-                    <td key={`row-${row._rowId || i}-${getColumnName(col)}`}>{row[getColumnName(col)]}</td>
+                    <td key={`row-${row._rowId || i}-${getColumnName(col)}`}>{
+                      getFormattedValue(row[getColumnName(col)], getColumnType(col))
+                    }</td>
                   ))}
                 </tr>
               ))}
