374759cd1289760f72228a9fd5a7f1e19ef1a983
Rashid Khan <github.fliplap@spamgourmet.com>
2017-10-14 15:48:37 -0400

    Add perpage and paginate options

--- public/expression_types/views/table.js
+++ public/expression_types/views/table.js
@@ -11,5 +11,20 @@ export const table = () => new View('table', {
       description: 'Fonts, alignment and color',
       argType: 'font',
     }),
+    new Arg('perPage', {
+      displayName: 'Rows per page',
+      description: 'Number of rows to display per table page.',
+      argType: 'select',
+      defaultValue: 10,
+      options: {
+        choices: ['', 5, 10, 25, 50, 100].map(v => ({ name: v, value: v })),
+      },
+    }),
+    new Arg('paginate', {
+      displayName: 'Pagination',
+      description: 'Show or hide pagination controls. If disabled only the first page will be shown.',
+      argType: 'checkbox',
+      defaultValue: true,
+    }),
   ],
 });
