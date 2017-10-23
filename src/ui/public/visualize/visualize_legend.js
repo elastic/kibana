170932af4030c32152f5ebcf28a1d5ffda03236d
Tim Roes <tim.roes@elastic.co>
2017-10-23 10:45:20 +0200

    Make legend color picker accessible

--- src/ui/public/visualize/visualize_legend.js
+++ src/ui/public/visualize/visualize_legend.js
@@ -50,13 +50,16 @@ uiModules.get('kibana')
         handler.unHighlight.call(el, handler.el);
       };
 
-      $scope.setColor = function (label, color) {
+      $scope.setColor = function (label, color, event) {
         const colors = $scope.uiState.get('vis.colors') || {};
         if (colors[label] === color) delete colors[label];
         else colors[label] = color;
         $scope.uiState.setSilent('vis.colors', null);
         $scope.uiState.set('vis.colors', colors);
         $scope.uiState.emit('colorChanged');
+        if (event) {
+          event.currentTarget.blur();
+        }
         refresh();
       };
 
