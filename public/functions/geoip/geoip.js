17b73c551458f244ce9220edd725d26e88eaa849
Rashid Khan <github.fliplap@spamgourmet.com>
2017-10-23 10:57:22 -0700

    Add geoip function

---
+++ public/functions/geoip/geoip.js
@@ -0,0 +1,35 @@
+import Fn from '../../../common/functions/fn.js';
+import fetch from 'axios';
+
+// TODO: We should implement our own Elastic GeoIP service instead of using freegeoip.net
+export default new Fn({
+  name: 'geoip',
+  aliases: [],
+  type: 'location',
+  help: 'Uses freegeoip.net to perform ',
+  context: {},
+  args: {
+    _: {
+      types: [
+        'string',
+      ],
+      aliases: ['ip', 'host'],
+      default: '""',
+      help: 'An optional IP or hostname. If left blank your own IP will be used',
+      multi: false,
+    },
+  },
+  fn: (context, args) => {
+    return fetch(`https://freegeoip.net/json/${args._}`, {
+      method: 'GET',
+    })
+    .then(res => {
+
+      return {
+        type: 'location',
+        latitude: res.data.latitude,
+        longitude: res.data.longitude,
+      };
+    });
+  },
+});
--- public/functions/index.js
+++ public/functions/index.js
@@ -1,4 +1,5 @@
 import clientdata from './clientdata/clientdata';
+import geoip from './geoip/geoip';
 import location from './location/location';
 import navigator from './navigator/navigator';
 import filters from './filters/filters';
@@ -8,6 +9,7 @@ import urlparam from './urlparam/urlparam';
 
 export const clientFunctions = [
   clientdata,
+  geoip,
   location,
   navigator,
   filters,
