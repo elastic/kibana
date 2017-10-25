e95a609a4c91bc2b85de4a93e75038b76d4842b8
Matt Apperson <me@mattapperson.com>
2017-10-25 17:17:19 -0400

    Hide only TSVB controls, not charts (#14545)
    * hide only TSVB controls, not charts
    
    * revert format changes
    
    * Update vis_editor_visualization.js
    
    * walk back more style changes
    
    * fix regression
    
    * fix more regressions
    
    * add new lines back

--- src/core_plugins/metrics/public/components/vis_editor.js
+++ src/core_plugins/metrics/public/components/vis_editor.js
@@ -8,7 +8,6 @@ import brushHandler from '../lib/create_brush_handler';
 import { get } from 'lodash';
 
 class VisEditor extends Component {
-
   constructor(props) {
     super(props);
     const { appState } = props;
@@ -80,16 +79,14 @@ class VisEditor extends Component {
       );
     }
 
-
     const { model } = this.state;
 
     if (model && this.props.visData) {
       return (
         <div className="vis_editor">
-          <VisPicker
-            model={model}
-            onChange={handleChange}
-          />
+          <div className="vis-editor-hide-for-reporting">
+            <VisPicker model={model} onChange={handleChange} />
+          </div>
           <VisEditorVisualization
             dirty={this.state.dirty}
             autoApply={this.state.autoApply}
@@ -103,13 +100,15 @@ class VisEditor extends Component {
             onChange={handleChange}
             dateFormat={this.props.config.get('dateFormat')}
           />
-          <PanelConfig
-            fields={this.props.vis.fields}
-            model={model}
-            visData={this.props.visData}
-            dateFormat={this.props.config.get('dateFormat')}
-            onChange={handleChange}
-          />
+          <div className="vis-editor-hide-for-reporting">
+            <PanelConfig
+              fields={this.props.vis.fields}
+              model={model}
+              visData={this.props.visData}
+              dateFormat={this.props.config.get('dateFormat')}
+              onChange={handleChange}
+            />
+          </div>
         </div>
       );
     }
@@ -120,7 +119,6 @@ class VisEditor extends Component {
   componentDidMount() {
     this.props.renderComplete();
   }
-
 }
 
 VisEditor.defaultProps = {
