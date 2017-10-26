0b1bd839358090bbf946115626ceff784a80d701
CJ Cenizal <cj@cenizal.com>
2017-10-26 09:27:30 -0700

    Rename KuiPopover isFocusable prop to ownFocus. Focus on first focusable element by default.

---
+++ ui_framework/doc_site/src/views/popover/trap_focus.js
@@ -0,0 +1,75 @@
+import React, {
+  Component,
+} from 'react';
+
+import {
+  KuiButton,
+  KuiFieldGroup,
+  KuiFieldGroupSection,
+  KuiPopover,
+} from '../../../../components';
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPopoverOpen: false,
+    };
+  }
+
+  onButtonClick() {
+    this.setState({
+      isPopoverOpen: !this.state.isPopoverOpen,
+    });
+  }
+
+  closePopover() {
+    this.setState({
+      isPopoverOpen: false,
+    });
+  }
+
+  render() {
+    const button = (
+      <KuiButton buttonType="basic" onClick={this.onButtonClick.bind(this)}>
+        Show popover
+      </KuiButton>
+    );
+
+    return (
+      <KuiPopover
+        ownFocus
+        button={button}
+        isOpen={this.state.isPopoverOpen}
+        closePopover={this.closePopover.bind(this)}
+      >
+        <div className="kuiVerticalRhythmSmall">
+          <KuiFieldGroup>
+            <KuiFieldGroupSection isWide>
+              <div className="kuiSearchInput">
+                <div className="kuiSearchInput__icon kuiIcon fa-search" />
+                <input
+                  className="kuiSearchInput__input"
+                  type="text"
+                />
+              </div>
+            </KuiFieldGroupSection>
+
+            <KuiFieldGroupSection>
+              <select className="kuiSelect">
+                <option>Animal</option>
+                <option>Mineral</option>
+                <option>Vegetable</option>
+              </select>
+            </KuiFieldGroupSection>
+          </KuiFieldGroup>
+        </div>
+
+        <div className="kuiVerticalRhythmSmall">
+          <KuiButton buttonType="primary">Save</KuiButton>
+        </div>
+      </KuiPopover>
+    );
+  }
+}
