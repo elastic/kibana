79fdf95612d614051c04bd91c4dca9e398b55d40
Spencer <spalger@users.noreply.github.com>
2017-10-30 14:12:24 -0700

    [6.x] [es/clusters] improve cleanup (#14188) (#14676)
    * [es/clusters] automatically close all clusters on server close
    
    * [es/cluster] track and close all created clients
    
    * [es/clusters] remove old close tests

--- src/core_plugins/elasticsearch/lib/__tests__/create_clusters.js
+++ src/core_plugins/elasticsearch/lib/__tests__/create_clusters.js
@@ -1,8 +1,10 @@
 import expect from 'expect.js';
 import { createClusters } from '../create_clusters';
-import { Cluster } from '../cluster';
 import sinon from 'sinon';
 import { partial } from 'lodash';
+import Hapi from 'hapi';
+
+import * as ClusterNS from '../cluster';
 
 describe('plugins/elasticsearch', function () {
   describe('createClusters', function () {
@@ -14,7 +16,8 @@ describe('plugins/elasticsearch', function () {
         plugins: {
           elasticsearch: {}
         },
-        expose: sinon.mock()
+        expose: sinon.mock(),
+        on: sinon.stub(),
       };
 
       clusters = createClusters(server);
@@ -34,17 +37,45 @@ describe('plugins/elasticsearch', function () {
       });
 
       it('returns a cluster', () => {
-        expect(cluster).to.be.a(Cluster);
+        expect(cluster).to.be.a(ClusterNS.Cluster);
       });
 
       it('persists the cluster', () => {
-        expect(clusters.get('admin')).to.be.a(Cluster);
+        expect(clusters.get('admin')).to.be.a(ClusterNS.Cluster);
       });
 
       it('throws if cluster already exists', () => {
         const fn = partial(clusters.create, 'admin', config);
         expect(fn).to.throwException(/cluster \'admin\' already exists/);
       });
+    });
+  });
+
+  describe('server stop', () => {
+    const sandbox = sinon.sandbox.create();
+
+    beforeEach(() => {
+      sandbox.stub(ClusterNS, 'Cluster', function () {
+        this.stub = true;
+        this.close = sinon.stub();
+      });
+    });
+
+    after(() => {
+      sandbox.restore();
+    });
+
+    it('closes all clusters', async () => {
+      const server = new Hapi.Server();
+      server.connection({ port: 0 });
+      const clusters = createClusters(server);
+      const cluster = clusters.create('name', { config: true });
+      expect(cluster).to.have.property('stub', true);
+      sinon.assert.notCalled(cluster.close);
+      await server.start();
+      sinon.assert.notCalled(cluster.close);
+      await server.stop();
+      sinon.assert.calledOnce(cluster.close);
     });
   });
 });
