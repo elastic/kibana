dd67d1d51ea1b4fbf79d88db05deff45fa2f6599
Rashid Khan <github.fliplap@spamgourmet.com>
2017-10-30 15:32:12 -0700

    Add another demo data set

--- server/functions/demodata/demodata.js
+++ server/functions/demodata/demodata.js
@@ -1,7 +1,38 @@
 import _ from 'lodash';
 import moment from 'moment';
-import rows from './mock.json';
+import ci from './ci.json';
+import shirts from './shirts.json';
 import { queryDatatable } from '../../../common/lib/datatable/query';
+
+const sets = {
+  ci: {
+    columns: [
+      { name: '_rowId', type: 'number' },
+      { name: 'time', type: 'date' },
+      { name: 'cost', type: 'number' },
+      { name: 'username', type: 'string' },
+      { name: 'price', type: 'number' },
+      { name: 'age', type: 'number' },
+      { name: 'country', type: 'string' },
+      { name: 'state', type: 'string' },
+      { name: 'project', type: 'string' },
+    ],
+    rows: _.map(_.cloneDeep(ci), (row, i) => _.assign(row, {
+      _rowId: i,
+      time: moment(moment(row.time).format('YYYY-MM-DD'), 'YYYY-MM-DD').format(),
+    })),
+  },
+  shirts: {
+    columns: [
+      { name: '_rowId', type: 'number' },
+      { name: 'size', type: 'string' },
+      { name: 'color', type: 'string' },
+      { name: 'price', type: 'number' },
+      { name: 'cut', type: 'string' },
+    ],
+    rows: shirts,
+  },
+};
 
 export const demodata = {
   name: 'demodata',
@@ -11,25 +42,20 @@ export const demodata = {
   context: {
     types: ['filter'],
   },
-  args: {},
-  fn: (context) => {
+  args: {
+    _: {
+      types: ['string', 'null'],
+      aliases: ['type'],
+      help: 'The name of the demo data set to use',
+      default: 'ci',
+    },
+  },
+  fn: (context, args) => {
+    const { columns, rows } = sets[args._];
     return queryDatatable({
       type: 'datatable',
-      columns: [
-        { name: '_rowId', type: 'number' },
-        { name: 'time', type: 'date' },
-        { name: 'cost', type: 'number' },
-        { name: 'username', type: 'string' },
-        { name: 'price', type: 'number' },
-        { name: 'age', type: 'number' },
-        { name: 'country', type: 'string' },
-        { name: 'state', type: 'string' },
-        { name: 'project', type: 'string' },
-      ],
-      rows: _.map(_.cloneDeep(rows), (row, i) => _.assign(row, {
-        _rowId: i,
-        time: moment(moment(row.time).format('YYYY-MM-DD'), 'YYYY-MM-DD').format(),
-      })),
+      columns,
+      rows,
     }, context);
   },
 };
