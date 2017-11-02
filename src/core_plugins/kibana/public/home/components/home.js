8815bf11498f6725caaca5028f6766ffad64256e
Nathan Reese <reese.nathan@gmail.com>
2017-11-02 12:58:46 -0600

    Kibana Home page - phase one (#14673)
    * make kibana home app default when defaultAppId not set
    
    * make kibana icon link to home page, add react-router for routing within home app
    
    * directory registry
    
    * add href to directory listings
    
    * add tabs to directory page
    
    * add promo section to home page
    
    * home page styling
    
    * use kuiFlex components to display directory in columns
    
    * fix react array missing key warning
    
    * add icons
    
    * remove feature directory title from home page, change data sources to integrations
    
    * add tutorials registry
    
    * fix rebase mistake
    
    * start tutorial component
    
    * wrap tutorial registration in helper function to hide implemenation details
    
    * add constants for categry and instruction variant ids
    
    * filter tutorial directory by tab category
    
    * remove later phase stuff
    
    * clean-up feature directory styling
    
    * add kbnDirectory to uiExports
    
    * remove unused file
    
    * cleanup timelion plugin definition
    
    * fix lint errors
    
    * css work recommended by formgeist review
    
    * cleanup from pairing session with snide
    
    * rename kbnDirectory registry to featureCatalogue, rename kbnDirectory uiExports to home
    
    * update kibana index uses name to match ui-exports name
    
    * remove carot from package versions
    
    * remove kuiViewContent--constrainedWidth from feature directory view
    
    * updates from Stacey-Gammon review
    
    * import FeatureCatalogueCategory instead of passing as parameter
    
    * wrap KuiButton in href to fix button click bug
    
    * remove conditional check for ADMIN and DATA feature category tabs
    
    * consider apps for the 'OTHER' tab as anything not in ADMIN or DATA
    
    * remove temp variable tabs and just store in this
    
    * avoid overwriting kui class styles
    
    * prefix class names with home
    
    * updates from timroes review

---
+++ src/core_plugins/kibana/public/home/components/home.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Synopsis } from './synopsis';
+import {
+  KuiLinkButton,
+  KuiFlexGroup,
+  KuiFlexItem,
+  KuiFlexGrid,
+} from 'ui_framework/components';
+import { FeatureCatalogueCategory } from 'ui/registry/feature_catalogue';
+
+export function Home({ addBasePath, directories }) {
+
+  const renderDirectories = (category) => {
+    return directories
+    .filter((directory) => {
+      return directory.showOnHomePage && directory.category === category;
+    })
+    .map((directory) => {
+      return (
+        <KuiFlexItem style={{ minHeight: 64 }} key={directory.id}>
+          <Synopsis
+            description={directory.description}
+            iconUrl={addBasePath(directory.icon)}
+            title={directory.title}
+            url={addBasePath(directory.path)}
+          />
+        </KuiFlexItem>
+      );
+    });
+  };
+
+
+  return (
+    <div className="kuiView home">
+      <div className="kuiViewContent">
+
+        <div className="kuiViewContentItem kuiVerticalRhythmXLarge">
+          <KuiFlexGroup
+            className="kuiVerticalRhythmSmall"
+            justifyContent="spaceBetween"
+            alignItems="flexEnd"
+          >
+            <KuiFlexItem>
+              <h1 className="kuiTitle">
+                Welcome to Kibana
+              </h1>
+            </KuiFlexItem>
+
+            <KuiFlexItem grow={false}>
+              <KuiFlexGroup alignItems="center">
+                <KuiFlexItem grow={false}>
+                  <p className="kuiText kuiSubduedText">
+                    Data already in Elasticsearch?
+                  </p>
+                </KuiFlexItem>
+
+                <KuiFlexItem grow={false}>
+                  <KuiLinkButton
+                    buttonType="secondary"
+                    href={addBasePath('/app/kibana#/management/kibana/index')}
+                  >
+                    Set up index patterns
+                  </KuiLinkButton>
+                </KuiFlexItem>
+              </KuiFlexGroup>
+
+            </KuiFlexItem>
+          </KuiFlexGroup>
+        </div>
+
+        <div className="kuiViewContentItem kuiVerticalRhythmXLarge">
+          <KuiFlexGroup className="kuiVerticalRhythm">
+            <KuiFlexItem className="kuiPanel homePanel">
+              <h3 className="kuiSubTitle kuiVerticalRhythm">
+                Visualize and explore data
+              </h3>
+              <KuiFlexGrid className="kuiVerticalRhythmSmall homeTopFeatures" columns={2}>
+                { renderDirectories(FeatureCatalogueCategory.DATA) }
+              </KuiFlexGrid>
+            </KuiFlexItem>
+            <KuiFlexItem className="kuiPanel homePanel">
+              <h3 className="kuiSubTitle kuiVerticalRhythm">
+                Manage and administer the Elastic stack
+              </h3>
+              <KuiFlexGrid className="kuiVerticalRhythmSmall homeTopFeatures" columns={2}>
+                { renderDirectories(FeatureCatalogueCategory.ADMIN) }
+              </KuiFlexGrid>
+            </KuiFlexItem>
+          </KuiFlexGroup>
+        </div>
+
+        <div className="kuiViewContentItem kuiVerticalRhythmXLarge text-center">
+          <h4 className="kuiSubduedText kuiVerticalRhythmSmall">
+            {`Didn't find what you were looking for?`}
+          </h4>
+          <KuiLinkButton
+            buttonType="secondary"
+            href="#/home/feature_directory"
+          >
+            View full directory of Kibana features
+          </KuiLinkButton>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+Home.propTypes = {
+  addBasePath: PropTypes.func.isRequired,
+  directories: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired,
+    showOnHomePage: PropTypes.bool.isRequired,
+    category: PropTypes.string.isRequired
+  }))
+};
