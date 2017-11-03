81e79e87dd91b5fdddd3839423befd72f005cd4d
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2017-11-03 20:23:23 +0100

    LogMeta type

--- platform/logging/Logger.ts
+++ platform/logging/Logger.ts
@@ -2,17 +2,19 @@ import { Appender } from './appenders/Appenders';
 import { LogLevel } from './LogLevel';
 import { LogRecord } from './LogRecord';
 
+export type LogMeta = { [key: string]: any };
+
 /**
  * Logger exposes all the necessary methods to log any type of information and
  * this is the interface used by the logging consumers including plugins.
  */
 export interface Logger {
-  trace(message: string, meta?: { [key: string]: any }): void;
-  debug(message: string, meta?: { [key: string]: any }): void;
-  info(message: string, meta?: { [key: string]: any }): void;
-  warn(errorOrMessage: string | Error, meta?: { [key: string]: any }): void;
-  error(errorOrMessage: string | Error, meta?: { [key: string]: any }): void;
-  fatal(errorOrMessage: string | Error, meta?: { [key: string]: any }): void;
+  trace(message: string, meta?: LogMeta): void;
+  debug(message: string, meta?: LogMeta): void;
+  info(message: string, meta?: LogMeta): void;
+  warn(errorOrMessage: string | Error, meta?: LogMeta): void;
+  error(errorOrMessage: string | Error, meta?: LogMeta): void;
+  fatal(errorOrMessage: string | Error, meta?: LogMeta): void;
 
   /** @internal */
   log(record: LogRecord): void;
@@ -30,27 +32,27 @@ export class BaseLogger implements Logger {
     private readonly appenders: Appender[]
   ) {}
 
-  trace(message: string, meta?: { [key: string]: any }): void {
+  trace(message: string, meta?: LogMeta): void {
     this.log(this.createLogRecord(LogLevel.Trace, message, meta));
   }
 
-  debug(message: string, meta?: { [key: string]: any }): void {
+  debug(message: string, meta?: LogMeta): void {
     this.log(this.createLogRecord(LogLevel.Debug, message, meta));
   }
 
-  info(message: string, meta?: { [key: string]: any }): void {
+  info(message: string, meta?: LogMeta): void {
     this.log(this.createLogRecord(LogLevel.Info, message, meta));
   }
 
-  warn(errorOrMessage: string | Error, meta?: { [key: string]: any }): void {
+  warn(errorOrMessage: string | Error, meta?: LogMeta): void {
     this.log(this.createLogRecord(LogLevel.Warn, errorOrMessage, meta));
   }
 
-  error(errorOrMessage: string | Error, meta?: { [key: string]: any }): void {
+  error(errorOrMessage: string | Error, meta?: LogMeta): void {
     this.log(this.createLogRecord(LogLevel.Error, errorOrMessage, meta));
   }
 
-  fatal(errorOrMessage: string | Error, meta?: { [key: string]: any }): void {
+  fatal(errorOrMessage: string | Error, meta?: LogMeta): void {
     this.log(this.createLogRecord(LogLevel.Fatal, errorOrMessage, meta));
   }
 
@@ -67,7 +69,7 @@ export class BaseLogger implements Logger {
   private createLogRecord(
     level: LogLevel,
     errorOrMessage: string | Error,
-    meta?: { [key: string]: any }
+    meta?: LogMeta
   ): LogRecord {
     const record = {
       timestamp: new Date(),
@@ -96,27 +98,27 @@ export class BaseLogger implements Logger {
 export class LoggerAdapter implements Logger {
   constructor(public logger: Logger) {}
 
-  trace(message: string, meta?: { [key: string]: any }): void {
+  trace(message: string, meta?: LogMeta): void {
     this.logger.trace(message, meta);
   }
 
-  debug(message: string, meta?: { [key: string]: any }): void {
+  debug(message: string, meta?: LogMeta): void {
     this.logger.debug(message, meta);
   }
 
-  info(message: string, meta?: { [key: string]: any }): void {
+  info(message: string, meta?: LogMeta): void {
     this.logger.info(message, meta);
   }
 
-  warn(errorOrMessage: string | Error, meta?: { [key: string]: any }): void {
+  warn(errorOrMessage: string | Error, meta?: LogMeta): void {
     this.logger.warn(errorOrMessage, meta);
   }
 
-  error(errorOrMessage: string | Error, meta?: { [key: string]: any }): void {
+  error(errorOrMessage: string | Error, meta?: LogMeta): void {
     this.logger.error(errorOrMessage, meta);
   }
 
-  fatal(errorOrMessage: string | Error, meta?: { [key: string]: any }): void {
+  fatal(errorOrMessage: string | Error, meta?: LogMeta): void {
     this.logger.fatal(errorOrMessage, meta);
   }
 
