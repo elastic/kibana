743257adc8c7d531a9eec6c526da8ed84cae98e2
Stacey Gammon <gammon@elastic.co>
2017-11-06 16:05:09 -0500

    get rid of funneling map entry updates through
    This was one of the suggestions in the redux discussion but after
    sitting with it, I decided I don’t like it.  I think it is too much
    overhead and makes the code more difficult to read.  There are not so
    many actions at both levels (touching the whole mapping vs touching a
    single mapping object) that justifies this, IMO.

--- src/core_plugins/kibana/public/dashboard/reducers/panels.js
+++ src/core_plugins/kibana/public/dashboard/reducers/panels.js
@@ -8,24 +8,53 @@ import {
   setPanels,
 } from '../actions';
 
-import { panel } from './panel';
-
 export const panels = handleActions({
-  [setPanels]: (state, { payload }) => _.cloneDeep(payload),
-  [updatePanels]: (state, { payload }) => {
-    const stateCopy = { ...state };
-    Object.values(payload).forEach(updatedPanel => {
-      stateCopy[updatedPanel.panelIndex] = _.defaultsDeep(updatedPanel, stateCopy[updatedPanel.panelIndex]);
-    });
-    return stateCopy;
-  },
-  [deletePanel]: (state, { payload }) => {
-    const stateCopy = { ...state };
-    delete stateCopy[payload];
-    return stateCopy;
-  },
-  [updatePanel]: (state, action) => ({
-    ...state,
-    [action.payload.panelIndex]: panel(state[action.payload.panelIndex], action),
-  }),
+  [setPanels]:
+    /**
+     *
+     * @param panels {Object.<string, PanelState>}
+     * @param payload {Object.<string, PanelState>}
+     * @return {Object.<string, PanelState>}
+     */
+    (panels, { payload }) => _.cloneDeep(payload),
+
+  [updatePanels]:
+    /**
+     *
+     * @param panels {Object.<string, PanelState>}
+     * @param payload {Object.<string, PanelState>}
+     * @return {Object.<string, PanelState>}
+     */
+    (panels, { payload }) => {
+      const panelsCopy = { ...panels };
+      Object.values(payload).forEach(updatedPanel => {
+        panelsCopy[updatedPanel.panelIndex] = _.defaultsDeep(updatedPanel, panelsCopy[updatedPanel.panelIndex]);
+      });
+      return panelsCopy;
+    },
+
+  [deletePanel]:
+    /**
+     *
+     * @param panels {Object.<string, PanelState>}
+     * @param payload {string} The panelIndex of the panel to delete
+     * @return {Object.<string, PanelState>}
+     */
+    (panels, { payload }) => {
+      const panelsCopy = { ...panels };
+      delete panelsCopy[payload];
+      return panelsCopy;
+    },
+
+  [updatePanel]:
+    /**
+     * @param panels {Object.<string, PanelState>}
+     * @param payload {PanelState} The new panel state (is merged with existing).
+     * @param payload.panelIndex {string} The id of the panel to update.
+     * @return {Object.<string, PanelState>}
+     */
+    (panels, { payload }) => ({
+      ...panels,
+      [payload.panelIndex]: _.defaultsDeep(payload, panels[[payload.panelIndex]]),
+    }),
 }, {});
