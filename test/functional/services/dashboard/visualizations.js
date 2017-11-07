f806d64e60f6f17e9f89ee5d80f63ad490b58173
Stacey Gammon <gammon@elastic.co>
2017-11-07 14:59:18 -0500

    Add dashboard tests (#14775)
    * add a test that will fail due to the current TSVB bug
    
    * add a test to catch the latest bug with saved searches
    
    * reorder array so tests pass
    
    * Update tests since the first panel is TSVB and won't have a spy pane toggle

---
+++ test/functional/services/dashboard/visualizations.js
@@ -0,0 +1,39 @@
+
+export function DashboardVisualizationProvider({ getService, getPageObjects }) {
+  const log = getService('log');
+  const PageObjects = getPageObjects(['dashboard', 'visualize', 'header', 'discover']);
+
+  return new class DashboardVisualizations {
+    async createAndAddTSVBVisualization(name) {
+      log.debug(`createAndAddTSVBVisualization(${name})`);
+      const inViewMode = await PageObjects.dashboard.getIsInViewMode();
+      if (inViewMode) {
+        await PageObjects.dashboard.clickEdit();
+      }
+      await PageObjects.dashboard.clickAddVisualization();
+      await PageObjects.dashboard.clickAddNewVisualizationLink();
+      await PageObjects.visualize.clickVisualBuilder();
+      await PageObjects.visualize.saveVisualization(name);
+      await PageObjects.header.clickToastOK();
+    }
+
+    async createAndAddSavedSearch(name) {
+      log.debug(`createAndAddSavedSearch(${name})`);
+      await PageObjects.header.clickDiscover();
+      await PageObjects.dashboard.setTimepickerInDataRange();
+      await PageObjects.discover.clickFieldListItemAdd('bytes');
+      await PageObjects.discover.clickFieldListItemAdd('agent');
+      await PageObjects.discover.saveSearch(name);
+      await PageObjects.header.waitUntilLoadingHasFinished();
+      await PageObjects.header.clickToastOK();
+
+      await PageObjects.header.clickDashboard();
+
+      const inViewMode = await PageObjects.dashboard.getIsInViewMode();
+      if (inViewMode) {
+        await PageObjects.dashboard.clickEdit();
+      }
+      await PageObjects.dashboard.addSavedSearch(name);
+    }
+  };
+}
