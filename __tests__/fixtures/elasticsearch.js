7a900a3bcc3727b6f0c3f5a6ac9ee4a5e6d465b1
Rashid Khan <github.fliplap@spamgourmet.com>
2017-11-08 12:30:13 -0700

    Fix test

--- __tests__/fixtures/elasticsearch.js
+++ __tests__/fixtures/elasticsearch.js
@@ -1,6 +1,6 @@
 export default function Client() {
   this.indices = {
-    create: () => Promise.resolve({ acknowledged: true }),
+    putMapping: () => Promise.resolve({ acknowledged: true }),
     exists: () => Promise.resolve(false),
     refresh: () => Promise.resolve(),
   };
