b3a4c43d37583df5c70c14216db584ebe747255d
Nathan Reese <reese.nathan@gmail.com>
2017-11-09 18:56:48 -0700

    allow seriesList with multiple series

--- src/core_plugins/timelion/server/lib/reduce.js
+++ src/core_plugins/timelion/server/lib/reduce.js
@@ -1,6 +1,4 @@
 import _ from 'lodash';
-import Promise from 'bluebird';
-
 
 /**
  * Reduces multiple arrays into a single array using a function
@@ -12,72 +10,85 @@ import Promise from 'bluebird';
  * @params {Function} fn - Function used to combine points at same index in each array of each series in the seriesList.
  * @return {seriesList}
  */
-export default function reduce(args, fn) {
-  return Promise.all(args).then(function (args) {
+export default async function reduce(argsPromises, fn) {
+  const args = await Promise.all(argsPromises);
 
-    const seriesList = args.shift();
-    let argument = args.shift();
+  const seriesList = args.shift();
+  let argument = args.shift();
 
-    if (seriesList.type !== 'seriesList') {
-      throw new Error ('input must be a seriesList');
-    }
+  if (seriesList.type !== 'seriesList') {
+    throw new Error ('input must be a seriesList');
+  }
 
-    if (_.isObject(argument) && argument.type === 'seriesList') {
-      if (argument.list.length !== 1) {
-        throw new Error ('argument must be a seriesList with a single series');
-      } else {
-        argument = argument.list[0];
+  if (_.isObject(argument) && argument.type === 'seriesList') {
+    if (argument.list.length > 1) {
+      // reduce seriesList on a per-label basis
+      if (seriesList.list.length !== argument.list.length) {
+        throw new Error ('Unable to reduce seriesList on a per-label basis, number of series are not the same');
       }
-    }
 
-
-    function reduceSeries(series) {
-      return _.reduce(series, function (destinationObject, argument, i, p) {
-
-        let output = _.map(destinationObject.data, function (point, index) {
-
-          const value = point[1];
-
-          if (value == null) {
-            return [point[0], null];
-          }
-
-          if (_.isNumber(argument)) {
-            return [point[0], fn(value, argument, i, p)];
-          }
-
-          if (argument.data[index] == null || argument.data[index][1] == null) {
-            return [point[0], null];
-          }
-          return [point[0], fn(value, argument.data[index][1], i, p)];
-        });
-
-        // Output = single series
-
-        output = {
-          data: output
-        };
-        output = _.defaults(output, destinationObject);
-        return output;
-
+      const labelwiseSeriesList = { type: 'seriesList', list: [] };
+      const indexedByLabel = _.indexBy(argument.list, 'label');
+      seriesList.list.forEach(async (series) => {
+        if (!indexedByLabel[series.label]) {
+          throw new Error (`series could not be found for label ${series.label}`);
+        }
+        const first = { type: 'seriesList', list: [series] };
+        const second = { type: 'seriesList', list: [indexedByLabel[series.label]] };
+        const labelSeriesList = await reduce([first, second], fn);
+        const labelSeries = labelSeriesList.list[0];
+        labelwiseSeriesList.list.push(labelSeries);
       });
-
-    }
-
-    let reduced;
-
-    if (argument != null) {
-      reduced = _.map(seriesList.list, function (series) {
-        return reduceSeries([series].concat(argument));
-      });
+      return labelwiseSeriesList;
     } else {
-      reduced = [reduceSeries(seriesList.list)];
+      argument = argument.list[0];
     }
+  }
 
-    seriesList.list = reduced;
-    return seriesList;
 
-  }).catch(function (e) {
-    throw e;
-  });
+  function reduceSeries(series) {
+    return _.reduce(series, function (destinationObject, argument, i, p) {
+
+      let output = _.map(destinationObject.data, function (point, index) {
+
+        const value = point[1];
+
+        if (value == null) {
+          return [point[0], null];
+        }
+
+        if (_.isNumber(argument)) {
+          return [point[0], fn(value, argument, i, p)];
+        }
+
+        if (argument.data[index] == null || argument.data[index][1] == null) {
+          return [point[0], null];
+        }
+        return [point[0], fn(value, argument.data[index][1], i, p)];
+      });
+
+      // Output = single series
+
+      output = {
+        data: output
+      };
+      output = _.defaults(output, destinationObject);
+      return output;
+
+    });
+
+  }
+
+  let reduced;
+
+  if (argument != null) {
+    reduced = _.map(seriesList.list, function (series) {
+      return reduceSeries([series].concat(argument));
+    });
+  } else {
+    reduced = [reduceSeries(seriesList.list)];
+  }
+
+  seriesList.list = reduced;
+  return seriesList;
 }
