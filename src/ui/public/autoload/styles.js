d0b59c9bc2d0549c6c6b91792a210b6a56a11a46
CJ Cenizal <cj@cenizal.com>
2017-11-12 10:01:57 -0800

    Autoload eui_theme_light.css.
    Remove EUI dependency.

--- src/ui/public/autoload/styles.js
+++ src/ui/public/autoload/styles.js
@@ -1,6 +1,9 @@
 // Kibana UI Framework
 require('../../../../ui_framework/dist/ui_framework.css');
 
+// Elastic UI Framework
+require('@elastic/eui/dist/eui_theme_light.css');
+
 // All Kibana styles inside of the /styles dir
 const context = require.context('../styles', false, /[\/\\](?!mixins|variables|_|\.)[^\/\\]+\.less/);
 context.keys().forEach(key => context(key));
