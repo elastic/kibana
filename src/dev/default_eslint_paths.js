2173effecfa36377ae1840365dc5639162775a90
Spencer <spalger@users.noreply.github.com>
2017-11-14 10:11:59 -0700

    [eslint] add eslint dev script (#14889)

---
+++ src/dev/default_eslint_paths.js
@@ -0,0 +1,12 @@
+export const DEFAULT_ESLINT_PATHS = [
+  'Gruntfile.js',
+  'bin',
+  'config',
+  'src',
+  'scripts',
+  'tasks',
+  'test',
+  'ui_framework/components',
+  'ui_framework/doc_site',
+  'utilities',
+];
