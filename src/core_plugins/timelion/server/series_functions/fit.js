94b2b324aa50889f1a6dbc0b7a91e7effe8b9c6c
Nathan Reese <reese.nathan@gmail.com>
2017-11-14 10:56:16 -0700

    Timelion typeahead for argument values (#14801)
    * timelion argument value suggestions for legend function
    
    * update functions with argument value suggestions
    
    * use async/await instead of promise resolve/reject for suggestion generation
    
    * lookup value suggestions for es index and timefield arguments
    
    * custom handlers for es metric and split arguments
    
    * update suggestions unit test
    
    * remove duplicate code from insertSuggestion switch
    
    * refactor arg_value_suggestions to provide three methods instead of single getSuggetions method
    
    * template literal
    
    * update es index argument desc to include note about type ahead support

--- src/core_plugins/timelion/server/series_functions/fit.js
+++ src/core_plugins/timelion/server/series_functions/fit.js
@@ -13,7 +13,10 @@ export default new Chainable('fit', {
     {
       name: 'mode',
       types: ['string'],
-      help: 'The algorithm to use for fitting the series to the target. One of: ' + _.keys(fitFunctions).join(', ')
+      help: `The algorithm to use for fitting the series to the target. One of: ${_.keys(fitFunctions).join(', ')}`,
+      suggestions: _.keys(fitFunctions).map(key => {
+        return { name: key };
+      })
     }
   ],
   help: 'Fills null values using a defined fit function',
