77f5dae68208fa15b51bea68dd65a7c83eb9b9a8
Chris Cowan <chris@chriscowan.us>
2017-11-14 10:23:33 -0700

    [TSVB] Add params._interval to mathjs

--- src/core_plugins/metrics/server/lib/vis_data/response_processors/series/math.js
+++ src/core_plugins/metrics/server/lib/vis_data/response_processors/series/math.js
@@ -83,7 +83,15 @@ export function mathAgg(resp, panel, series) {
           const someNull = values(params).some(v => v == null);
           if (someNull) return [ts, null];
           // calculate the result based on the user's script and return the value
-          const result = limitedEval(mathMetric.script, { params: { ...params, _index: index, _timestamp: ts, _all: all } });
+          const result = limitedEval(mathMetric.script, {
+            params: {
+              ...params,
+              _index: index,
+              _timestamp: ts,
+              _all: all,
+              _interval: split.meta.bucketSize * 1000
+            }
+          });
           // if the result is an object (usually when the user is working with maps and functions) flatten the results and return the last value.
           if (typeof result === 'object') return [ts, last(flatten(result.valueOf()))];
           return [ts, result];
