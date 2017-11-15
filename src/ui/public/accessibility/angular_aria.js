ca72156477554c402c13e55eb7e538952f596a77
Tim Roes <tim.roes@elastic.co>
2017-11-15 18:59:22 +0100

    Load angular-aria for kibana module

---
+++ src/ui/public/accessibility/angular_aria.js
@@ -0,0 +1,12 @@
+import 'angular-aria';
+import { uiModules } from 'ui/modules';
+
+uiModules
+.get('kibana', ['ngAria'])
+.config(($ariaProvider) => {
+  $ariaProvider.config({
+    bindKeydown: false,
+    bindRoleForClick: false,
+    tabindex: false,
+  });
+});
