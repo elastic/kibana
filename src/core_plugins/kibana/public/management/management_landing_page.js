f63b04d71ada758b890ba065305396eea5e53121
CJ Cenizal <cj@cenizal.com>
2017-11-14 16:36:58 -0800

    Rebuild Management landing page using EUI components.

--- src/core_plugins/kibana/public/management/management_landing_page.js
+++ src/core_plugins/kibana/public/management/management_landing_page.js
@@ -2,62 +2,77 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiHorizontalRule,
+  EuiLink,
+  EuiPage,
+  EuiPageContentBody,
+  EuiPanel,
+  EuiSpacer,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
+
 export class ManagementLandingPage extends Component {
-  renderSection = (section, index) => {
-    const items = section.visibleItems.map((item, itemIndex) => {
-      const itemClasses = classNames('management-panel__link', {
-        'management-panel__link--disabled': item.disabled || !item.url,
+  renderSections = sections => {
+    return sections.map((section, index) => {
+      const items = section.visibleItems.map((item, itemIndex) => {
+        // TO-DO: Links need a disabled state
+        return (
+          <EuiFlexItem
+            key={itemIndex}
+          >
+            <EuiLink
+              data-test-subj={item.name}
+              href={item.disabled ? '' : this.props.addBasePath(item.url)}
+              isDisabled={item.disabled || !item.url}
+            >
+              {item.display}
+            </EuiLink>
+          </EuiFlexItem>
+        );
       });
 
+      const iconClasses = classNames(
+        'management-panel__heading-icon',
+        `management-panel__heading-icon--${section.id}`,
+      );
+
+      const title = (
+        <EuiFlexGroup alignItems="center">
+          <EuiFlexItem grow={false}>
+            <div className={iconClasses} />
+          </EuiFlexItem>
+
+          <EuiFlexItem>
+            <EuiTitle>
+              <h3>{section.display}</h3>
+            </EuiTitle>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      );
+
       return (
-        <li
-          className="col-xs-4 col-md-3"
-          key={itemIndex}
-        >
-          <a
-            data-test-subj={item.name}
-            className={itemClasses}
-            href={item.disabled ? '' : this.props.addBasePath(item.url)}
-          >
-            {item.display}
-          </a>
-        </li>
+        <div key={index}>
+          <EuiPanel paddingSize="l">
+            {title}
+            <EuiHorizontalRule margin="s" />
+
+            <EuiPageContentBody>
+              <EuiFlexGroup>
+                {items}
+              </EuiFlexGroup>
+            </EuiPageContentBody>
+          </EuiPanel>
+
+          {(index < sections.length - 1) &&
+            <EuiSpacer size="m" />
+          }
+        </div>
       );
     });
-
-    const iconClasses = classNames(
-      'management-panel__heading-icon',
-      `management-panel__heading-icon--${section.id}`,
-    );
-
-    return (
-      <div
-        className="page-row"
-        key={index}
-      >
-        <div className="kuiPanel management-panel">
-          <div className="kuiPanelHeader">
-            <div className="kuiPanelHeaderSection">
-              <div
-                className={iconClasses}
-              />
-
-              <div className="kuiPanelHeader__title">
-                {section.display}
-              </div>
-            </div>
-          </div>
-
-          <div className="kuiPanelBody management-panel__body">
-            <div className="row">
-              <ul className="list-unstyled">
-                {items}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
   };
 
   render() {
@@ -67,16 +82,18 @@ export class ManagementLandingPage extends Component {
     } = this.props;
 
     const renderedSections =
-      sections.filter(section => section.visibleItems.length > 0).map(this.renderSection);
+      this.renderSections(sections.filter(section => section.visibleItems.length > 0));
 
     return (
-      <div>
-        <div className="page-row">
-          <div className="page-row-text">Version: {version}</div>
-        </div>
+      <EuiPage>
+        <EuiText>
+          <p>Version: {version}</p>
+        </EuiText>
+
+        <EuiSpacer size="l" />
 
         {renderedSections}
-      </div>
+      </EuiPage>
     );
   }
 }
