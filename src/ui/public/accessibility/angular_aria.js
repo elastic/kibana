518f13897717c397493ac775a493de1cbd27161e
Tim Roes <tim.roes@elastic.co>
2017-11-16 11:29:01 +0100

    Fix indentation

--- src/ui/public/accessibility/angular_aria.js
+++ src/ui/public/accessibility/angular_aria.js
@@ -2,11 +2,11 @@ import 'angular-aria';
 import { uiModules } from 'ui/modules';
 
 uiModules
-.get('kibana', ['ngAria'])
-.config(($ariaProvider) => {
-  $ariaProvider.config({
-    bindKeydown: false,
-    bindRoleForClick: false,
-    tabindex: false,
+  .get('kibana', ['ngAria'])
+  .config(($ariaProvider) => {
+    $ariaProvider.config({
+      bindKeydown: false,
+      bindRoleForClick: false,
+      tabindex: false,
+    });
   });
-});
