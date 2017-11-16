908d85c31879a2b8b4d9a7ff0a74e909f11845c0
ppisljar <peter.pisljar@gmail.com>
2017-11-16 10:17:37 +0100

    updating truncate_labels to work with chars instead pixels

--- src/ui/public/vislib/components/labels/truncate_labels.js
+++ src/ui/public/vislib/components/labels/truncate_labels.js
@@ -1,25 +1,18 @@
 import $ from 'jquery';
 import d3 from 'd3';
 
+/***
+ *
+ * @param text (d3 node containing text)
+ * @param size (number of characters to leave)
+ * @returns {text} the updated text
+ */
 const truncateLabel = function (text, size) {
   const node = d3.select(text).node();
-  let str = $(node).text();
+  const str = $(node).text();
   if (size === 0) return str;
-
-  const width = node.getBBox().width;
-  const chars = str.length;
-  const pxPerChar = width / chars;
-  let endChar = 0;
-  const ellipsesPad = 4;
-
-  if (width > size) {
-    endChar = Math.floor((size / pxPerChar) - ellipsesPad);
-    while (str[endChar - 1] === ' ' || str[endChar - 1] === '-' || str[endChar - 1] === ',') {
-      endChar = endChar - 1;
-    }
-    str = str.substr(0, endChar) + '…';
-  }
-  return str;
+  if (size >= str.length) return str;
+  return str.substr(0, size) + '…';
 };
 
 export { truncateLabel };
