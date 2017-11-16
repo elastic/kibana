211268b0f7db53a385a2da1af24b6ef7239a4e73
Chris Roberson <chrisronline@gmail.com>
2017-11-16 10:40:02 -0500

    More tests

--- src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/step_index_pattern/lib/__tests__/append_wildcard.js
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/step_index_pattern/lib/__tests__/append_wildcard.js
@@ -4,15 +4,26 @@ import { appendWildcard } from '../append_wildcard';
 
 describe('append_wildcard', function () {
   it('should add a wildcard for an alphabet input', () => {
-    expect(appendWildcard({ key: 'a' }, '')).to.be('a*');
+    [
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+      'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+      's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ].forEach(char => {
+      expect(appendWildcard({ key: char }, '')).to.be(`${char}*`);
+      expect(appendWildcard({ key: char.toUpperCase() }, '')).to.be(`${char.toUpperCase()}*`);
+    });
   });
 
   it('should add a wildcard for a number input', () => {
-    expect(appendWildcard({ key: '8' }, '')).to.be('8*');
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach(num => {
+      expect(appendWildcard({ key: num }, '')).to.be(`${num}*`);
+    });
   });
 
   it('should NOT add a wildcard for a non alphanumeric input', () => {
-    expect(appendWildcard({ key: '-' }, '')).to.be(undefined);
+    ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+'].forEach(char => {
+      expect(appendWildcard({ key: char }, '')).to.be(undefined);
+    });
   });
 
   it('should NOT add a wildcard for multi-length input', () => {
