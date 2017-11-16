cb9198541af0e95686f3a3f36a591783f8005489
archanid <artistic.archana@gmail.com>
2017-11-16 17:42:19 -0600

    [http] Inject es service into es api. :heart: commas

--- platform/server/elasticsearch/api.ts
+++ platform/server/elasticsearch/api.ts
@@ -6,7 +6,8 @@ import { LoggerFactory } from '../../logging';
 
 export function registerElasticsearchRoutes(
   router: Router<ElasticsearchRequestHelpers>,
-  logger: LoggerFactory
+  logger: LoggerFactory,
+  service: ElasticsearchService,
 ) {
   const log = logger.get('elasticsearch', 'routes');
 
@@ -17,35 +18,23 @@ export function registerElasticsearchRoutes(
       path: '/:field',
       validate: {
         params: object({
-          field: string()
+          field: string(),
         }),
         query: object({
-          key: maybe(string())
-        })
-      }
+          key: maybe(string()),
+        }),
+      },
     },
     async (req, res) => {
       // WOHO! Both of these are typed!
       log.info(`field param: ${req.params.field}`);
       log.info(`query param: ${req.query.key}`);
 
-      // NB: All right, so at this point when we're
-      // exposing the es API, shouldn't we have an
-      // instance of ElasticsearchService already
-      // from kibana-core? I'm a bit confused.
-      //
-      // My understanding is that this const elasticsearch is the
-      // underlying elasticsearch-js client. But this whole
-      // code lives inside of a kibana-core system that we're
-      // also calling elasticsearch.
-      //
       // The following code needs work. We don't have `withRequest`
       // anymore.
-      const elasticsearch = new ElasticsearchService(req);
-
       log.info('request received on [data] cluster');
 
-      const cluster = await elasticsearch.getClusterOfType('data');
+      const cluster = await service.getClusterOfType$('data');
 
       log.info('got [data] cluster, now calling it');
 
