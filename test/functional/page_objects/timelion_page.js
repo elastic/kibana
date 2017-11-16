a6d379f5eff3a67d8601958c021ab4a1fbe23609
Nathan Reese <reese.nathan@gmail.com>
2017-11-16 13:38:48 -0700

    add tests for expression type ahead functions, arguments, and argument values

--- test/functional/page_objects/timelion_page.js
+++ test/functional/page_objects/timelion_page.js
@@ -1,18 +1,51 @@
 export function TimelionPageProvider({ getService, getPageObjects }) {
-  const remote = getService('remote');
-  const config = getService('config');
   const testSubjects = getService('testSubjects');
-  const retry = getService('retry');
   const find = getService('find');
   const log = getService('log');
   const PageObjects = getPageObjects(['common', 'header']);
-  const defaultFindTimeout = config.get('timeouts.find');
+  const esArchiver = getService('esArchiver');
+  const kibanaServer = getService('kibanaServer');
 
   class TimelionPage {
+    async initTests() {
+      await kibanaServer.uiSettings.replace({
+        'dateFormat:tz': 'UTC',
+        'defaultIndex': 'logstash-*'
+      });
+
+      log.debug('load kibana index');
+      await esArchiver.load('timelion');
+
+      await PageObjects.common.navigateToApp('timelion');
+    }
+
     async setExpression(expression) {
       const input = await testSubjects.find('timelionExpressionTextArea');
       await input.clearValue();
       await input.type(expression);
+    }
+
+    async updateExpression(updates) {
+      const input = await testSubjects.find('timelionExpressionTextArea');
+      await input.type(updates);
+    }
+
+    async getExpression() {
+      const input = await testSubjects.find('timelionExpressionTextArea');
+      return input.getVisibleText();
+    }
+
+    async getSuggestionItemsText() {
+      const elements = await find.allByCssSelector('.suggestions .suggestion');
+      return await Promise.all(elements.map(async element => await element.getVisibleText()));
+    }
+
+    async clickSuggestion(suggestionIndex = 0) {
+      const elements = await find.allByCssSelector('.suggestions .suggestion');
+      if (suggestionIndex > elements.length) {
+        throw new Error(`Unable to select suggestion ${suggestionIndex}, only ${elements.length} suggestions available.`);
+      }
+      await elements[suggestionIndex].click();
     }
   }
 
