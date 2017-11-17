237ee635a4b2fe8cdc555740510248b7dbc02f07
Spencer <spalger@users.noreply.github.com>
2017-11-17 15:33:58 -0600

    [server/rootRedirect] remove browser-based redirect (#14698)
    * [server/rootRedirect] remove browser-based redirect
    
    With Kibana 4.2 we added the /app/kibana portion of the URL. This introduced a breaking change for URLs from 4.0-4.1, which required a redirect from '/#discover' to '/app/kibana#discover', but we still have the `defaultRoute` parameter, which meant that we needed to distinguish between `/` and `/#discover`. This is impossible on the server alone, unfortunately, so we had to do the redirect in the browser.
    
    We are a long way from Kibana 4.2 at this point, and I think it's time we reduce the number of moving parts and just rely on the server for redirection.
    
    * [server/rootRedirect] test location header

--- src/server/http/get_default_route.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-
-export default _.once(function (kbnServer) {
-  const {
-    config
-  } = kbnServer;
-  return `${config.get('server.basePath')}${config.get('server.defaultRoute')}`;
-});
