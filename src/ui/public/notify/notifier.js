4e15befa3dcfa31e5fdec37cba23706cf527f1d4
Nathan Reese <reese.nathan@gmail.com>
2017-11-16 18:51:00 -0700

    fix tests

--- src/ui/public/notify/notifier.js
+++ src/ui/public/notify/notifier.js
@@ -421,7 +421,7 @@ Notifier.prototype.info = function (msg, opts, cb) {
     content: formatMsg(msg, this.from),
     icon: 'info-circle',
     title: 'Debug',
-    lifetime: opts.lifetime ? opts.lifetime : Notifier.config.infoLifetime,
+    lifetime: _.has(opts, 'lifetime') ? opts.lifetime : Notifier.config.infoLifetime,
     actions: ['accept']
   }, _.pick(opts, overrideableOptions));
   return add(config, cb);
