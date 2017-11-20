6497e7408d5c735c44a0cac25e020dbd3540ccf6
Chris Roberson <chrisronline@gmail.com>
2017-11-20 15:55:12 -0500

    Handle ccs better

--- src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/create_index_pattern_wizard.js
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/create_index_pattern_wizard.js
@@ -105,6 +105,14 @@ uiModules.get('apps/management')
               name: bucket.key
             };
           }), 'name');
+        })
+        .catch(err => {
+          if (err.body && err.body.error && err.body.error.caused_by.type === 'index_not_found_exception') {
+            // This happens in a CSS environment when the controlling node returns a 500 even though the data
+            // nodes returned a 404. Remove this when/if this is handled: https://github.com/elastic/elasticsearch/issues/27461
+            return [];
+          }
+          throw err;
         });
     }
 
