2b5605dd662fe0d196a2b5a3d4db44e7d9b88e0f
ppisljar <peter.pisljar@gmail.com>
2017-11-21 08:49:58 +0100

    fixing github link

--- src/ui/public/vis/default_feedback_message.js
+++ src/ui/public/vis/default_feedback_message.js
@@ -1,2 +1,2 @@
 export const defaultFeedbackMessage = `Have feedback? Please create an issue in 
-<a ng-href="https://github.com/elastic/kibana/issues/new" rel="noopener noreferrer">GitHub</a>.`;
+<a href="https://github.com/elastic/kibana/issues/new" rel="noopener noreferrer" target="_blank">GitHub</a>.`;
