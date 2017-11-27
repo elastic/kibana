ad8a89318cc59ac6bed7d0be9fdd3e18a06ce779
Jonathan Budzenski <jon@jbudz.me>
2017-11-27 13:32:40 -0600

    [build] Experiment with cleaning more files

--- tasks/config/clean.js
+++ tasks/config/clean.js
@@ -1,9 +1,59 @@
 module.exports = function () {
+  // https://github.com/yarnpkg/yarn/blob/b97c797983babd03c68bc36f1ee98a9a6dff9246/src/cli/commands/autoclean.js
+  const moduleGlobs = [
+    // https://github.com/gruntjs/grunt-contrib-clean/issues/65
+    // 'docs',
+    // 'images',
+    // '!mathjs/lib/expression/**',
+    // '!leaflet-draw/dist/images/**',
+
+    '__tests__',
+    'test',
+    'tests',
+    'powered-test',
+    'doc',
+    'website',
+    'assets',
+    'example',
+    'examples',
+    'coverage',
+    '.nyc_output',
+    'Makefile',
+    'Gulpfile.js',
+    'Gruntfile.js',
+    'appveyor.yml',
+    'circle.yml',
+    'codeship-services.yml',
+    'codeship-steps.yml',
+    'wercker.yml',
+    '.tern-project',
+    '.gitattributes',
+    '.editorconfig',
+    '.*ignore',
+    '.eslintrc',
+    '.jshintrc',
+    '.flowconfig',
+    '.documentup.json',
+    '.yarn-metadata.json',
+    '.travis.yml',
+    '*.txt',
+    '*.md',
+  ].map(p => `build/kibana/node_modules/**/${p}`);
+
+
+  const srcGlobs = [
+    'build/kibana/src/core_plugins/testbed'
+  ];
+
+  const extraneousBuildFiles = [].concat(
+    moduleGlobs,
+    srcGlobs
+  );
+
+
   return {
     build: 'build',
     target: 'target',
-    testsFromModules: 'build/kibana/node_modules/**/{test,tests}/**',
-    examplesFromModules: 'build/kibana/node_modules/**/{example,examples}/**',
-    devSourceForTestbed: 'build/kibana/src/core_plugins/testbed/',
+    extraneousBuildFiles,
   };
 };
