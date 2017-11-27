eb6b95bfd529837e6e2947c64c6fcaa0671f6b26
Nathan Reese <reese.nathan@gmail.com>
2017-11-27 11:25:27 -0700

    Refactor: extract common LS instructions

--- src/core_plugins/kibana/common/tutorials/logstash_instructions.js
+++ src/core_plugins/kibana/common/tutorials/logstash_instructions.js
@@ -8,8 +8,7 @@ export const LOGSTASH_INSTRUCTIONS = {
       {
         title: 'Download and install the Java runtime environment',
         textPre: `Follow the installation instructions [here](https://docs.oracle.com/javase/8/docs/technotes/guides/install/mac_jre.html). ` +
-          `${SKIP_INSTALL_SENTENCE} ${FIRST_TIME_SENTENCE}`,
-        commands: []
+          `${SKIP_INSTALL_SENTENCE} ${FIRST_TIME_SENTENCE}`
       },
       {
         title: 'Download and install Logstash',
@@ -17,23 +16,6 @@ export const LOGSTASH_INSTRUCTIONS = {
         commands: [
           'curl -L -O https://artifacts.elastic.co/downloads/logstash/logstash-{config.kibana.version}.tar.gz',
           'tar xzvf logstash-{config.kibana.version}.tar.gz'
-        ]
-      }
-    ],
-    DEB: [
-      {
-        title: 'Download and install the Java runtime environment',
-        textPre: `${SKIP_INSTALL_SENTENCE} ${FIRST_TIME_SENTENCE}`,
-        commands: [
-          'sudo apt-get install default-jre'
-        ]
-      },
-      {
-        title: 'Download and install Logstash',
-        textPre: SKIP_INSTALL_SENTENCE,
-        commands: [
-          'curl -L -O https://artifacts.elastic.co/downloads/logstash/logstash-{config.kibana.version}.deb',
-          'sudo apt install ./logstash-{config.kibana.version}.deb'
         ]
       }
     ]
--- src/core_plugins/kibana/server/tutorials/netflow/index.js
+++ src/core_plugins/kibana/server/tutorials/netflow/index.js
@@ -1,7 +1,7 @@
-import { PARAM_TYPES } from '../../../common/tutorials/param_types';
 import { TUTORIAL_CATEGORY } from '../../../common/tutorials/tutorial_category';
-import { INSTRUCTION_VARIANT } from '../../../common/tutorials/instruction_variant';
-import { LOGSTASH_INSTRUCTIONS } from '../../../common/tutorials/logstash_instructions';
+import { ON_PREM_INSTRUCTIONS } from './on_prem';
+import { ELASTIC_CLOUD_INSTRUCTIONS } from './elastic_cloud';
+import { ON_PREM_ELASTIC_CLOUD_INSTRUCTIONS } from './on_prem_elastic_cloud';
 
 export function netflowSpecProvider() {
   return {
@@ -16,42 +16,8 @@ export function netflowSpecProvider() {
     //iconPath: '', TODO
     completionTimeMinutes: 10,
     //previewImagePath: 'kibana-apache.png', TODO
-    params: [
-      {
-        id: 'netflow_var_input_udp_port',
-        label: 'netflow.var.input.udp.port',
-        type: PARAM_TYPES.NUMBER,
-        defaultValue: 2055
-      }
-    ],
-    instructionSets: [
-      {
-        title: 'Getting Started',
-        instructionVariants: [
-          {
-            id: INSTRUCTION_VARIANT.OSX,
-            instructions: [
-              ...LOGSTASH_INSTRUCTIONS.INSTALL.OSX,
-              {
-                title: 'Set up the Netflow module',
-                textPre: 'In the Logstash install directory, run the following command to set up the Netflow module.',
-                commands: [
-                  './bin/logstash --modules netflow --setup',
-                ],
-                textPost: 'The `--setup` option creates a `netflow-*` index pattern in Elasticsearch and imports' +
-                  ' Kibana dashboards and visualizations. Running `--setup` is a one-time setup step. Omit this step' +
-                  ' for subsequent runs of the module to avoid overwriting existing Kibana dashboards.'
-              },
-              {
-                title: 'Start Logstash',
-                commands: [
-                  './bin/logstash --modules netflow -M netflow.var.input.udp.port={params.netflow_var_input_udp_port}'
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
+    onPrem: ON_PREM_INSTRUCTIONS,
+    elasticCloud: ELASTIC_CLOUD_INSTRUCTIONS,
+    onPremElasticCloud: ON_PREM_ELASTIC_CLOUD_INSTRUCTIONS
   };
 }
