82f3293eff936f5d5af0d375c60cc8b6e0d5516d
Jonathan Budzenski <jbudz@users.noreply.github.com>
2017-11-28 13:51:20 -0600

    Add support for passing in node options via environment variable (elastic/kibana-plugin-helpers#56)
    * Add support for passing in node options via environment variable
    
    * Split node options
    
    * semicolon
    
    Original commit: elastic/kibana-plugin-helpers@3fc4ec0e87f72075e60563d0978c27677ce6bfde

--- packages/kbn-plugin-helpers/tasks/start/start_action.js
+++ packages/kbn-plugin-helpers/tasks/start/start_action.js
@@ -1,12 +1,15 @@
 const execFileSync = require('child_process').execFileSync;
 const  { join } = require('path');
+const split = require('argv-split');
 
 module.exports = function (plugin, run, options) {
   options = options || {};
 
   const cmd = 'node';
   const script = join('scripts', 'kibana.js');
-  let args = [script, '--dev', '--plugin-path', plugin.root];
+  const nodeOptions = split(process.env.NODE_OPTIONS || '');
+
+  let args = nodeOptions.concat([script, '--dev', '--plugin-path', plugin.root]);
 
   if (Array.isArray(plugin.includePlugins)) {
     plugin.includePlugins.forEach((path) => {
