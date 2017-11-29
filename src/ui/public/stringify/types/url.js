fdd62b03ccd07b6eca3c1c3b89ec4570dc57e4eb
Chris Roberson <chrisronline@gmail.com>
2017-11-29 12:32:44 -0500

    Port #14994 (#15261)

--- src/ui/public/stringify/types/url.js
+++ src/ui/public/stringify/types/url.js
@@ -8,7 +8,7 @@ import { getHighlightHtml } from 'ui/highlight';
 export function stringifyUrl(Private) {
 
   const FieldFormat = Private(IndexPatternsFieldFormatProvider);
-
+  const whitelistUrlSchemes = ['http://', 'https://'];
 
   _.class(Url).inherits(FieldFormat);
   function Url(params) {
@@ -87,7 +87,7 @@ export function stringifyUrl(Private) {
       return this._formatLabel(value);
     },
 
-    html: function (rawValue, field, hit) {
+    html: function (rawValue, field, hit, parsedUrl) {
       const url = _.escape(this._formatUrl(rawValue));
       const label = _.escape(this._formatLabel(rawValue, url));
 
@@ -102,6 +102,38 @@ export function stringifyUrl(Private) {
 
           return `<img src="${url}" alt="${imageLabel}">`;
         default:
+          const inWhitelist = whitelistUrlSchemes.some(scheme => url.indexOf(scheme) === 0);
+          if (!inWhitelist && !parsedUrl) {
+            return url;
+          }
+
+          let prefix = '';
+          /**
+           * This code attempts to convert a relative url into a kibana absolute url
+           *
+           * SUPPORTED:
+           *  - /app/kibana/
+           *  - ../app/kibana
+           *  - #/discover
+           *
+           * UNSUPPORTED
+           *  - app/kibana
+           */
+          if (!inWhitelist) {
+            // Handles urls like: `#/discover`
+            if (url[0] === '#') {
+              prefix = `${parsedUrl.origin}${parsedUrl.pathname}`;
+            }
+            // Handle urls like: `/app/kibana` or `/xyz/app/kibana`
+            else if (url.indexOf(parsedUrl.basePath || '/') === 0) {
+              prefix = `${parsedUrl.origin}`;
+            }
+            // Handle urls like: `../app/kibana`
+            else {
+              prefix = `${parsedUrl.origin}${parsedUrl.basePath}/app/`;
+            }
+          }
+
           let linkLabel;
 
           if (hit && hit.highlight && hit.highlight[field.name]) {
@@ -110,7 +142,7 @@ export function stringifyUrl(Private) {
             linkLabel = label;
           }
 
-          return `<a href="${url}" target="_blank">${linkLabel}</a>`;
+          return `<a href="${prefix}${url}" target="_blank">${linkLabel}</a>`;
       }
     }
   };
