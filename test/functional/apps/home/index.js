231bf0214d877a2e896b27c00096e066d96a7b79
Bhavya RM <bhavya@elastic.co>
2017-11-29 16:24:00 -0500

    Kibana home page tests (#15098)
    Couple of tests to make sure Kibana home page isn't breaking.

---
+++ test/functional/apps/home/index.js
@@ -0,0 +1,11 @@
+export default function ({ getService, loadTestFile }) {
+  const remote = getService('remote');
+
+  describe('homepage app', function () {
+    before(function () {
+      return remote.setWindowSize(1200, 800);
+    });
+
+    loadTestFile(require.resolve('./_home'));
+  });
+}
