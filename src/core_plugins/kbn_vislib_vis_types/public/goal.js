98f7d9b26507ad18084b720fb7faecb1dae24381
ppisljar <peter.pisljar@gmail.com>
2017-11-29 11:38:36 +0100

    fixing based on review

--- src/core_plugins/kbn_vislib_vis_types/public/goal.js
+++ src/core_plugins/kbn_vislib_vis_types/public/goal.js
@@ -19,7 +19,7 @@ export default function GoalVisType(Private) {
       defaults: {
         addTooltip: true,
         addLegend: false,
-        isDisplayWarning: true,
+        isDisplayWarning: false,
         type: 'gauge',
         gauge: {
           verticalSplit: false,
