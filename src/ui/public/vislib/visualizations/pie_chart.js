d105cdf6e26c7e5419d0e6812b1979a7677a7bbe
ppisljar <peter.pisljar@gmail.com>
2017-11-29 19:32:53 +0100

    fixes pie labels for firefox and IE

--- src/ui/public/vislib/visualizations/pie_chart.js
+++ src/ui/public/vislib/visualizations/pie_chart.js
@@ -192,9 +192,10 @@ export function VislibVisualizationsPieChartProvider(Private) {
           .data(partition.nodes);
 
         // create an empty quadtree to hold label positions
+        const svgParentNode = svg.node().parentNode.parentNode;
         const svgBBox = {
-          width: svg.node().parentElement.clientWidth,
-          height: svg.node().parentElement.clientHeight
+          width: svgParentNode.clientWidth,
+          height: svgParentNode.clientWidth
         };
 
         const labelLayout = d3.geom.quadtree()
@@ -210,7 +211,7 @@ export function VislibVisualizationsPieChartProvider(Private) {
           .append('text')
           .text(function (d) {
             if (d.depth === 0) {
-              this.parentElement.remove();
+              d3.select(this.parentNode).remove();
               return;
             }
             if (showValues) {
@@ -228,9 +229,9 @@ export function VislibVisualizationsPieChartProvider(Private) {
           .each(function resolveConflicts(d) {
             if (d.depth === 0) return;
 
-            const parentElement = this.parentElement;
+            const parentNode = this.parentNode;
             if (showOnlyOnLastLevel && maxDepth !== d.depth) {
-              parentElement.remove();
+              d3.select(parentNode).remove();
               return;
             }
 
@@ -270,7 +271,7 @@ export function VislibVisualizationsPieChartProvider(Private) {
             });
 
             if (conflicts.length) {
-              parentElement.remove();
+              d3.select(parentNode).remove();
               return;
             }
 
