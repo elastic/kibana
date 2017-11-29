dc7345a5585d658758f5e34bedd75ef79e5d9b99
Thomas Neirynck <thomas@elastic.co>
2017-11-29 14:40:56 -0500

    reverts metric visualization (#14052) (#15177) (#14882)
    Uses the previous html/css rendering code instead of the svg-based rendering for simple metrics.
    
    This backport required manual edits and js-linting.

---
+++ src/core_plugins/metric_vis/public/metric_vis.js
@@ -0,0 +1,91 @@
+import 'plugins/metric_vis/metric_vis.less';
+import 'plugins/metric_vis/metric_vis_params';
+import 'plugins/metric_vis/metric_vis_controller';
+import { VisFactoryProvider } from 'ui/vis/vis_factory';
+import { CATEGORY } from 'ui/vis/vis_category';
+import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
+import metricVisTemplate from 'plugins/metric_vis/metric_vis.html';
+import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import { vislibColorMaps } from 'ui/vislib/components/color/colormaps';
+import image from './images/icon_number.svg';
+// we need to load the css ourselves
+
+// we also need to load the controller and used by the template
+
+// register the provider with the visTypes registry
+VisTypesRegistryProvider.register(MetricVisProvider);
+
+function MetricVisProvider(Private) {
+  const Schemas = Private(VisSchemasProvider);
+  const VisFactory = Private(VisFactoryProvider);
+
+  // return the visType object, which kibana will use to display and configure new
+  // Vis object of this type.
+  return VisFactory.createAngularVisualization({
+    name: 'metric',
+    title: 'Metric',
+    image,
+    description: 'Display a calculation as a single number',
+    category: CATEGORY.DATA,
+    visConfig: {
+      defaults: {
+        addTooltip: true,
+        addLegend: false,
+        type: 'metric',
+        metric: {
+          percentageMode: false,
+          useRanges: false,
+          colorSchema: 'Green to Red',
+          metricColorMode: 'None',
+          colorsRange: [
+            { from: 0, to: 10000 }
+          ],
+          labels: {
+            show: true
+          },
+          invertColors: false,
+          style: {
+            bgFill: '#000',
+            bgColor: false,
+            labelColor: false,
+            subText: '',
+            fontSize: 60,
+          }
+        }
+      },
+      template: metricVisTemplate,
+    },
+    editorConfig: {
+      collections: {
+        metricColorMode: ['None', 'Labels', 'Background'],
+        colorSchemas: Object.keys(vislibColorMaps),
+      },
+      optionsTemplate: '<metric-vis-params></metric-vis-params>',
+      schemas: new Schemas([
+        {
+          group: 'metrics',
+          name: 'metric',
+          title: 'Metric',
+          min: 1,
+          aggFilter: [
+            '!std_dev', '!geo_centroid', '!percentiles', '!percentile_ranks',
+            '!derivative', '!serial_diff', '!moving_avg', '!cumulative_sum', '!geo_bounds'],
+          defaults: [
+            { type: 'count', schema: 'metric' }
+          ]
+        }, {
+          group: 'buckets',
+          name: 'group',
+          title: 'Split Group',
+          min: 0,
+          max: 1,
+          aggFilter: ['!geohash_grid', '!filter']
+        }
+      ])
+    },
+    implementsRenderComplete: true,
+  });
+}
+
+// export the provider so that the visType can be required with Private()
+export default MetricVisProvider;
