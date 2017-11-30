04915b729412b83dbefdfc255b94ef0e9c8df662
Stacey Gammon <gammon@elastic.co>
2017-11-30 15:40:41 -0500

    attempt to fix flaky edit visualization link (#15265) (#15313)

--- test/functional/page_objects/dashboard_page.js
+++ test/functional/page_objects/dashboard_page.js
@@ -35,10 +35,10 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
 
     async clickEditVisualization() {
       log.debug('clickEditVisualization');
-      await testSubjects.click('dashboardPanelToggleMenuIcon');
 
       // Edit link may sometimes be disabled if the embeddable isn't rendered yet.
       await retry.try(async () => {
+        await this.showPanelEditControlsDropdownMenu();
         await testSubjects.click('dashboardPanelEditLink');
         const current = await remote.getCurrentUrl();
         if (current.indexOf('visualize') < 0) {
@@ -465,6 +465,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
     }
 
     async showPanelEditControlsDropdownMenu() {
+      log.debug('showPanelEditControlsDropdownMenu');
       const editLinkExists = await testSubjects.exists('dashboardPanelEditLink');
       if (editLinkExists) return;
       await testSubjects.click('dashboardPanelToggleMenuIcon');
