ab68ade4d1de1a54d2e48609dce1dee124ef1bd7
Stacey Gammon <gammon@elastic.co>
2017-12-01 09:40:33 -0500

    Remove duplicate function name

--- test/functional/page_objects/dashboard_page.js
+++ test/functional/page_objects/dashboard_page.js
@@ -245,11 +245,6 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
       }
     }
 
-    async filterVizNames(vizName) {
-      await testSubjects.setValue('savedObjectFinderSearchInput', vizName);
-      await PageObjects.header.waitUntilLoadingHasFinished();
-    }
-
     async clickVizNameLink(vizName) {
       await find.clickByPartialLinkText(vizName);
     }
@@ -264,7 +259,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
       await this.clickEdit();
     }
 
-    async filterSearchNames(name) {
+    async filterEmbeddableNames(name) {
       await testSubjects.setValue('savedObjectFinderSearchInput', name);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
@@ -276,7 +271,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
     async addSavedSearch(searchName) {
       await this.clickAddVisualization();
       await this.clickSavedSearchTab();
-      await this.filterSearchNames(searchName);
+      await this.filterEmbeddableNames(searchName);
 
       await find.clickByPartialLinkText(searchName);
       await PageObjects.header.clickToastOK();
@@ -286,7 +281,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
     async addVisualization(vizName) {
       await this.clickAddVisualization();
       log.debug('filter visualization (' + vizName + ')');
-      await this.filterVizNames(vizName);
+      await this.filterEmbeddableNames(vizName);
       await this.clickVizNameLink(vizName);
       await PageObjects.header.clickToastOK();
       // this second click of 'Add' collapses the Add Visualization pane
