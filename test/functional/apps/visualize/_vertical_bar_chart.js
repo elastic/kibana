86f44379682b9e437fccef4c3a4c37cdf609d595
Nathan Reese <reese.nathan@gmail.com>
2017-12-04 11:11:40 -0700

    pass showSpyPanel to Editor (#15283)

--- test/functional/apps/visualize/_vertical_bar_chart.js
+++ test/functional/apps/visualize/_vertical_bar_chart.js
@@ -70,6 +70,11 @@ export default function ({ getService, getPageObjects }) {
           });
       });
 
+      it('should display spy panel toggle button', async function () {
+        const spyToggleExists = await PageObjects.visualize.getSpyToggleExists();
+        expect(spyToggleExists).to.be(true);
+      });
+
       it('should show correct chart, take screenshot', async function () {
         const expectedChartValues = [37, 202, 740, 1437, 1371, 751, 188, 31, 42, 202, 683,
           1361, 1415, 707, 177, 27, 32, 175, 707, 1408, 1355, 726, 201, 29
