c7be0540e60c7c74009a98d0cb099ebc9967a290
Joe Fleming <w33ble@users.noreply.github.com>
2017-12-04 14:55:36 -0700

    feat: add info about leaving feedback (#253)
    explain that feedback is private and offer the kibana forums as a public way to ask questions and discuss feedback

--- public/components/feedback_button/feedback_button.js
+++ public/components/feedback_button/feedback_button.js
@@ -6,27 +6,63 @@ import { NavbarButton } from '../navbar_button';
 import './feedback_button.less';
 
 export const FeedbackButton = ({ show, setShow }) => {
+  const formId = '1FAIpQLSfrHmSrLAWcLx8s4RM_pKP8_BvRGbASGeG-F_fagxUtGu7q4A';
+  const feedbackFormUrl = `https://docs.google.com/a/elastic.co/forms/d/e/${formId}/viewform?embedded=true`;
+  const forumLink = text => (
+    <a
+      rel="noopener noreferrer"
+      target="_blank"
+      href="https://discuss.elastic.co/c/kibana"
+    >
+      {text}
+    </a>
+  );
+
   return (
     <span>
-      <NavbarButton onClick={() => setShow(true)} className="pull-right"><i className="fa fa-comment" /> Give Feedback</NavbarButton>
+      <NavbarButton onClick={() => setShow(true)} className="pull-right"><i className="fa fa-comment" />
+        Give Feedback
+      </NavbarButton>
+
       {show &&
-        <Modal.Dialog>
-          <Modal.Header className="canvas__feedback-modal--header"/>
-          <Modal.Body className="canvas__feedback-modal" style={{ overflow: 'none', position: 'relative' }}>
+        <Modal.Dialog dialogClassName="canvas__feedback-modal">
+          <Modal.Header className="canvas__feedback-modal--header">Give Feedback</Modal.Header>
+          <Modal.Body className="canvas__feedback-modal--body" style={{ overflow: 'none', position: 'relative' }}>
+            <div className="message">
+              <p>
+                Use the form below to provide feedback about Canvas, which only the people working
+                on Canvas will be able to see. Keep in mind that we won't be able to contact you
+                about your feedback.
+              </p>
+              <p>
+                If you have a question or would like to post public feedback and have a public
+                discussion, {forumLink('please use the Kibana forums instead')}.
+              </p>
+
+              <hr />
+
+            </div>
+
             <div style={{ position: 'absolute', width: '100%' }}>
               <div style={{ overflow: 'hidden', position: 'relative' }}>
-                <iframe src="https://docs.google.com/a/elastic.co/forms/d/e/1FAIpQLSfrHmSrLAWcLx8s4RM_pKP8_BvRGbASGeG-F_fagxUtGu7q4A/viewform?embedded=true"
-                width="100%" height="700px" scrolling="no"
-                frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
+                <iframe
+                  src={feedbackFormUrl}
+                  width="100%"
+                  height="600px"
+                  scrolling="no"
+                  frameBorder="0"
+                  marginHeight="0"
+                  marginWidth="0"
+                >
+                  Loading...
+                </iframe>
               </div>
             </div>
 
           </Modal.Body>
 
           <Modal.Footer>
-            <Button
-              onClick={ () => setShow(false) }
-            >
+            <Button onClick={ () => setShow(false) }>
               Dismiss
             </Button>
           </Modal.Footer>
@@ -34,7 +70,6 @@ export const FeedbackButton = ({ show, setShow }) => {
         </Modal.Dialog>
       }
     </span>
-
   );
 };
 
