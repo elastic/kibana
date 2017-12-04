27d36d50c852fce111591e6adb257d79ec05db06
Stacey Gammon <gammon@elastic.co>
2017-12-04 13:49:17 -0500

    Attempt to stabilize filterVizNames failures (#15316)
    * Attempt to stabilize filterVizNames failures
    
    * Remove duplicate function name

--- test/functional/page_objects/dashboard_page.js
+++ test/functional/page_objects/dashboard_page.js
@@ -244,13 +244,6 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
       }
     }
 
-    async filterVizNames(vizName) {
-      const visFilter = await find.byCssSelector('input[placeholder="Visualizations Filter..."]');
-      await visFilter.click();
-      await remote.pressKeys(vizName);
-      await PageObjects.header.waitUntilLoadingHasFinished();
-    }
-
     async clickVizNameLink(vizName) {
       await find.clickByPartialLinkText(vizName);
     }
@@ -265,7 +258,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
       await this.clickEdit();
     }
 
-    async filterSearchNames(name) {
+    async filterEmbeddableNames(name) {
       await testSubjects.setValue('savedObjectFinderSearchInput', name);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
@@ -277,7 +270,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
     async addSavedSearch(searchName) {
       await this.clickAddVisualization();
       await this.clickSavedSearchTab();
-      await this.filterSearchNames(searchName);
+      await this.filterEmbeddableNames(searchName);
 
       await find.clickByPartialLinkText(searchName);
       await PageObjects.header.clickToastOK();
@@ -287,7 +280,7 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
     async addVisualization(vizName) {
       await this.clickAddVisualization();
       log.debug('filter visualization (' + vizName + ')');
-      await this.filterVizNames(vizName);
+      await this.filterEmbeddableNames(vizName);
       await this.clickVizNameLink(vizName);
       await PageObjects.header.clickToastOK();
       // this second click of 'Add' collapses the Add Visualization pane
