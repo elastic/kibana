41b7e3bea77fa315f6d3b077a144d16da2661e4f
Shaunak Kashyap <ycombinator@gmail.com>
2017-12-04 19:21:36 -0800

    [Netflow tutorial] Simplify OSX instructions

--- src/core_plugins/kibana/server/tutorials/netflow/on_prem.js
+++ src/core_plugins/kibana/server/tutorials/netflow/on_prem.js
@@ -20,20 +20,14 @@ export const ON_PREM_INSTRUCTIONS = {
           instructions: [
             ...LOGSTASH_INSTRUCTIONS.INSTALL.OSX,
             {
-              title: 'Set up the Netflow module',
+              title: 'Set up and run the Netflow module',
               textPre: 'In the Logstash install directory, run the following command to set up the Netflow module.',
               commands: [
-                './bin/logstash --modules netflow --setup',
+                './bin/logstash --modules netflow -M netflow.var.input.udp.port={params.netflow_var_input_udp_port} --setup',
               ],
               textPost: 'The `--setup` option creates a `netflow-*` index pattern in Elasticsearch and imports' +
-                ' Kibana dashboards and visualizations. Running `--setup` is a one-time setup step. Omit this step' +
-                ' for subsequent runs of the module to avoid overwriting existing Kibana dashboards.'
-            },
-            {
-              title: 'Start Logstash',
-              commands: [
-                './bin/logstash --modules netflow -M netflow.var.input.udp.port={params.netflow_var_input_udp_port}'
-              ]
+                ' Kibana dashboards and visualizations. Omit this option for subsequent runs of the module to avoid' +
+                '  overwriting existing Kibana dashboards.'
             }
           ]
         }
