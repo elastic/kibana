80e365c7317f5badce30ac46bf6552848d39b4a0
ppisljar <peter.pisljar@gmail.com>
2017-12-05 16:01:15 +0100

    fixes pie label conflict resolution

--- src/ui/public/vislib/visualizations/pie_chart.js
+++ src/ui/public/vislib/visualizations/pie_chart.js
@@ -244,7 +244,7 @@ export function VislibVisualizationsPieChartProvider(Private) {
               x: pos[0],
               y: pos[1],
               left: midAngle < Math.PI ? pos[0] : pos[0] - bbox.width,
-              right: midAngle > Math.PI ? pos[0] + bbox.width : pos[0],
+              right: midAngle < Math.PI ? pos[0] + bbox.width : pos[0],
               bottom: pos[1] + 5,
               top: pos[1] - bbox.height - 5,
             };
@@ -258,8 +258,8 @@ export function VislibVisualizationsPieChartProvider(Private) {
               const current = d.position;
               if (point) {
                 const horizontalConflict = (point.left < 0 && current.left < 0) || (point.left > 0 && current.left > 0);
-                const verticalConflict = (point.top > current.top && point.top <= current.bottom) ||
-                                          (point.top < current.top && point.bottom >= current.top);
+                const verticalConflict = (point.top >= current.top && point.top <= current.bottom) ||
+                                          (point.top <= current.top && point.bottom >= current.top);
 
                 if (horizontalConflict && verticalConflict) {
                   point.point = node.point;
