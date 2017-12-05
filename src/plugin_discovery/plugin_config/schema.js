b8a95b7239af2cca478a01cbc3c29583b6f5c3e1
spalger <spalger@users.noreply.github.com>
2017-12-04 21:00:14 -0700

    [pluginDiscovery] remove needless use of lodash

--- src/plugin_discovery/plugin_config/schema.js
+++ src/plugin_discovery/plugin_config/schema.js
@@ -1,5 +1,4 @@
 import Joi from 'joi';
-import { noop } from 'lodash';
 
 const STUB_CONFIG_SCHEMA = Joi.object().keys({
   enabled: Joi.valid(false)
@@ -16,8 +15,8 @@ const DEFAULT_CONFIG_SCHEMA = Joi.object().keys({
  *  @return {Promise<Joi>}
  */
 export async function getSchema(spec) {
-  const provider = spec.getConfigSchemaProvider() || noop;
-  return (await provider(Joi)) || DEFAULT_CONFIG_SCHEMA;
+  const provider = spec.getConfigSchemaProvider();
+  return (provider && await provider(Joi)) || DEFAULT_CONFIG_SCHEMA;
 }
 
 export function getStubSchema() {
