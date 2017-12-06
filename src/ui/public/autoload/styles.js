c0e9574b3da2ea35166a99ac3a99c73b1f22cc76
CJ Cenizal <cj@cenizal.com>
2017-12-05 20:51:51 -0800

    Set light theme as the default.

--- src/ui/public/autoload/styles.js
+++ src/ui/public/autoload/styles.js
@@ -4,12 +4,15 @@ const theme = require('../theme');
 require('../../../../ui_framework/dist/ui_framework.css');
 
 // Elastic UI Framework, light theme
-const euiThemeLight = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_light.css');
+const euiThemeLight = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_k6.css');
 theme.registerTheme('light', [euiThemeLight]);
 
 // Elastic UI Framework, dark theme
-const euiThemeDark = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_dark.css');
+const euiThemeDark = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_k6_dark.css');
 theme.registerTheme('dark', [euiThemeDark]);
+
+// Set default theme.
+theme.applyTheme('light');
 
 // All Kibana styles inside of the /styles dir
 const context = require.context('../styles', false, /[\/\\](?!mixins|variables|_|\.)[^\/\\]+\.less/);
--- src/ui/public/theme/index.js
+++ src/ui/public/theme/index.js
@@ -1,4 +1,5 @@
 export {
   registerTheme,
   applyTheme,
+  getCurrentTheme,
 } from './theme';
