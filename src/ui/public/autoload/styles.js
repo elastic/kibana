435ac2d971f89301e8415634bfeeac56d75c63ca
CJ Cenizal <cj@cenizal.com>
2017-12-05 20:03:34 -0800

    Remove commented-out CSS import.

--- src/ui/public/autoload/styles.js
+++ src/ui/public/autoload/styles.js
@@ -3,12 +3,11 @@ const theme = require('../theme');
 // Kibana UI Framework
 require('../../../../ui_framework/dist/ui_framework.css');
 
-// Elastic UI Framework
-// require('@elastic/eui/dist/eui_theme_light.css');
-
+// Elastic UI Framework, light theme
 const euiThemeLight = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_light.css');
 theme.registerTheme('light', [euiThemeLight]);
 
+// Elastic UI Framework, dark theme
 const euiThemeDark = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_dark.css');
 theme.registerTheme('dark', [euiThemeDark]);
 
