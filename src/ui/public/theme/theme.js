0c2c3cfc1dcd0c04205f7158e9e32cd4e248fafa
CJ Cenizal <cj@cenizal.com>
2017-12-07 14:07:28 -0800

    Add XSS note to themes.js.

--- src/ui/public/theme/theme.js
+++ src/ui/public/theme/theme.js
@@ -7,6 +7,10 @@ export function registerTheme(theme, styles) {
 
 export function applyTheme(newTheme) {
   currentTheme = newTheme;
+
+  // NOTE: The use of innerHTML opens up to XSS attacks, so we can't support user-generated themes
+  // as long as this implementation is in use. Ideally we would use the webpack style-loader/useable
+  // to activate and deactivate themes, but that causes the optimize step to fail.
   document.getElementById('themeCss').innerHTML = themes[currentTheme];
 }
 
