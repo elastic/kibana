700017dd5a5beb7697c78a4e63b574672e1459d8
CJ Cenizal <cj@cenizal.com>
2017-12-06 18:12:49 -0800

    Fix bug with style-loader by using raw-loader and innerHTML to set the style node's CSS.

--- src/ui/public/autoload/styles.js
+++ src/ui/public/autoload/styles.js
@@ -4,11 +4,11 @@ const theme = require('../theme');
 require('../../../../ui_framework/dist/ui_framework.css');
 
 // Elastic UI Framework, light theme
-const euiThemeLight = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_k6.css');
-theme.registerTheme('light', [euiThemeLight]);
+const euiThemeLight = require('!!raw-loader!@elastic/eui/dist/eui_theme_k6.css');
+theme.registerTheme('light', euiThemeLight);
 
 // Elastic UI Framework, dark theme
-const euiThemeDark = require('!!style-loader/useable!css-loader!@elastic/eui/dist/eui_theme_k6_dark.css');
+const euiThemeDark = require('!!raw-loader!@elastic/eui/dist/eui_theme_k6_dark.css');
 theme.registerTheme('dark', [euiThemeDark]);
 
 // Set default theme.
