74e7da4dd479532e8a3828b105a33a440c1e29d9
CJ Cenizal <cj@cenizal.com>
2017-12-06 18:12:49 -0800

    Fix bug with style-loader by using raw-loader and innerHTML to set the style node's CSS.

--- src/ui/public/theme/theme.js
+++ src/ui/public/theme/theme.js
@@ -1,14 +1,15 @@
 const themes = {};
 let currentTheme = undefined;
 
-export function registerTheme(theme, cssFiles) {
-  themes[theme] = cssFiles;
+export function registerTheme(theme, styles) {
+  themes[theme] = styles;
 }
 
 export function applyTheme(newTheme) {
-  Object.keys(themes).forEach(theme => themes[theme].forEach(cssFile => cssFile.unuse()));
-  themes[newTheme].forEach(cssFile => cssFile.use());
+  // Object.keys(themes).forEach(theme => themes[theme].forEach(cssFile => cssFile.unuse()));
+  // themes[newTheme].forEach(cssFile => cssFile.use());
   currentTheme = newTheme;
+  document.getElementById('themeCss').innerHTML = themes[currentTheme];
 }
 
 export function getCurrentTheme() {
