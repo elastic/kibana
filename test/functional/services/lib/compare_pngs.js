367e6873990ab7bca8d0dd8270aea488fc2a770c
Stacey Gammon <gammon@elastic.co>
2017-12-07 16:04:40 -0500

    resize images using new library so comparisons work across different screen resolutions

--- test/functional/services/lib/compare_pngs.js
+++ test/functional/services/lib/compare_pngs.js
@@ -1,40 +1,61 @@
 import fs from 'fs';
 import pixelmatch from 'pixelmatch';
+import Jimp from 'jimp';
 import { PNG } from 'pngjs';
 
-export function comparePngs(actualPath, expectedPath, diffPath, log) {
-  log.debug(`comparePngs: ${actualPath} vs ${expectedPath}`);
-  return new Promise(resolve => {
-    const actual = fs.createReadStream(actualPath).pipe(new PNG()).on('parsed', doneReading);
-    const expected = fs.createReadStream(expectedPath).pipe(new PNG()).on('parsed', doneReading);
-    let filesRead = 0;
-
-    // Note that this threshold value only affects color comparison from pixel to pixel. It won't have
-    // any affect when comparing neighboring pixels - so slight shifts, font variations, or "blurry-ness"
-    // will still show up as diffs, but upping this will not help that.  Instead we keep the threshold low, and expect
-    // some the diffCount to be lower than our own threshold value.
-    const THRESHOLD = .1;
-
-    function doneReading() {
-      if (++filesRead < 2) return;
-      const diffPng = new PNG({ width: actual.width, height: actual.height });
-      log.debug(`calculating diff pixels...`);
-      const diffPixels = pixelmatch(
-        actual.data,
-        expected.data,
-        diffPng.data,
-        actual.width,
-        actual.height,
-        {
-          threshold: THRESHOLD,
-          // Adding this doesn't seem to make a difference at all, but ideally we want to avoid picking up anti aliasing
-          // differences from fonts on different OSs.
-          includeAA: true
-        }
-      );
-      log.debug(`diff pixels: ${diffPixels}`);
-      diffPng.pack().pipe(fs.createWriteStream(diffPath));
-      resolve(diffPixels);
-    }
+async function getImageBuffer(image) {
+  return new Promise((resolve, reject) => {
+    image.getBuffer(Jimp.MIME_PNG, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
   });
+}
+
+export async function comparePngs(actualPath, expectedPath, diffPath, log) {
+  log.debug(`comparePngs: ${actualPath} vs ${expectedPath}`);
+  const actual = await Jimp.read(actualPath);
+  const expected = await Jimp.read(expectedPath);
+
+  if (actual.bitmap.width !== expected.bitmap.width || actual.bitmap.height !== expected.bitmap.height) {
+    const width = Math.min(actual.bitmap.width, expected.bitmap.width);
+    const height = Math.min(actual.bitmap.height, expected.bitmap.height);
+    actual.resize(width, height);
+    expected.resize(width, height);
+
+    actual.write(actualPath);
+    expected.write(expectedPath);
+  }
+
+  const actualBuffer = await getImageBuffer(actual);
+  const expectedBuffer = await getImageBuffer(expected);
+
+  const diffPng = new PNG({ width: actual.bitmap.width, height: actual.bitmap.height });
+
+  log.debug(`calculating diff pixels...`);
+  // Note that this threshold value only affects color comparison from pixel to pixel. It won't have
+  // any affect when comparing neighboring pixels - so slight shifts, font variations, or "blurry-ness"
+  // will still show up as diffs, but upping this will not help that.  Instead we keep the threshold low, and expect
+  // some the diffCount to be lower than our own threshold value.
+  const THRESHOLD = .1;
+  const diffPixels = pixelmatch(
+    actualBuffer,
+    expectedBuffer,
+    diffPng.data,
+    expected.bitmap.width,
+    expected.bitmap.height,
+    {
+      threshold: THRESHOLD,
+      // Adding this doesn't seem to make a difference at all, but ideally we want to avoid picking up anti aliasing
+      // differences from fonts on different OSs.
+      includeAA: true
+    }
+  );
+  log.debug(`diff pixels: ${diffPixels}`);
+  if (diffPixels > 0) {
+    diffPng.pack().pipe(fs.createWriteStream(diffPath));
+  }
+  return diffPixels;
 }
