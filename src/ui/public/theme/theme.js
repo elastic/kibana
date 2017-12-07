b06170d469012d31105b99a872a87dc6282cd932
CJ Cenizal <cj@cenizal.com>
2017-12-06 18:14:50 -0800

    Remove commented-out code.

--- src/ui/public/theme/theme.js
+++ src/ui/public/theme/theme.js
@@ -6,8 +6,6 @@ export function registerTheme(theme, styles) {
 }
 
 export function applyTheme(newTheme) {
-  // Object.keys(themes).forEach(theme => themes[theme].forEach(cssFile => cssFile.unuse()));
-  // themes[newTheme].forEach(cssFile => cssFile.use());
   currentTheme = newTheme;
   document.getElementById('themeCss').innerHTML = themes[currentTheme];
 }
