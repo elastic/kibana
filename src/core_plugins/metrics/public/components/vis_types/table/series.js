82820e3eb4a3b4c4f756f1c87f90e39310950747
Nathan Reese <reese.nathan@gmail.com>
2017-12-11 14:02:39 -0700

    Upgrade to react 16.0.0 (#15469)
    * get kibana to start with react 16.0.0
    
    * ensure width and height for dashboard grid
    
    * Update to Enzyme 3.x. (#12)
    
    * Fix tests broken by Enzyme upgrade (#13)
    
    * Update to Enzyme 3.x. (#12)
    
    * Add findTestSubject test helper. Fix KuiCodeEditor tests.
    
    * Fix KuiContextMenu tests.
    
    * Fix KuiConfirmModal tests.
    
    * Fix tests for KuiPager.
    
    * Update KuiConfirmModal tests to use findTestSubject helper.
    
    * Publish test helpers from ui_framework. Fix DashboardCloneModal tests.
    
    * Fix more tests (#14)
    
    * Fix DashboardPanelContainer tests.
    
    * Update KuiColorPicker tests.
    
    * Fix ControlsTab tests.
    
    * Rename vis to input_control_vis. Update and fix InputControlVis tests.
    
    * Fix KuiListingTable tests.
    
    * Fix KuiTextInput tests.
    
    * Update DashboardGrid test snapshots.
    
    * Fix test file names (#15)
    
    * Remove old enzyme setup file.
    
    * Add .test to YesNo and AddDeleteButtons test files.
    
    * needed window.cancelAnimationFrame to accompany requestAnimationFrame
    
    * remove getDOMNode function call from findTestSubject

--- src/core_plugins/metrics/public/components/vis_types/table/series.js
+++ src/core_plugins/metrics/public/components/vis_types/table/series.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import AddDeleteButtons from '../../add_delete_buttons';
 import SeriesConfig from './config';
 import Sortable from 'react-anything-sortable';
