ead6980e7e91a21029a15f103a5fb300f8195c42
archanid <artistic.archana@gmail.com>
2017-12-14 10:00:52 -0600

    [tests] more tests for call_api

--- platform/server/elasticsearch/__tests__/call_api.test.ts
+++ platform/server/elasticsearch/__tests__/call_api.test.ts
@@ -58,21 +58,63 @@ test('should handle failing api call', async () => {
 // TODO: change this test after implementing
 // homegrown error lib or boom
 // https://github.com/elastic/kibana/issues/12464
-test('should wrap 401 errors', async () => {
-  expect.assertions(2);
+describe('should wrap 401 errors', () => {
+  test('when wrap401Errors is undefined', async () => {
+    expect.assertions(2);
 
-  const fooFn = () => {
-    const err: any = new Error('api call failed');
-    err.statusCode = 401;
-    throw err;
-  };
+    const fooFn = () => {
+      const err: any = new Error('api call failed');
+      err.statusCode = 401;
+      throw err;
+    };
 
-  const client: any = { foo: fooFn };
+    const client: any = { foo: fooFn };
 
-  try {
-    await callAPI(client, 'foo', {}, {});
-  } catch (error) {
-    expect(error.message).toEqual('api call failed');
-    expect(error.wrap401Errors).toBe(true);
-  }
+    try {
+      await callAPI(client, 'foo', {}, {});
+    } catch (error) {
+      expect(error.message).toEqual('api call failed');
+      expect(error.wrap401Errors).toBe(true);
+    }
+  });
+});
+
+describe('should not wrap 401 errors', () => {
+  test('when wrap401Errors is false', async () => {
+    expect.assertions(2);
+
+    const fooFn = () => {
+      const err: any = new Error('api call failed');
+      err.statusCode = 401;
+      throw err;
+    };
+
+    const client: any = { foo: fooFn };
+
+    try {
+      await callAPI(client, 'foo', {}, { wrap401Errors: false });
+    } catch (error) {
+      expect(error.message).toEqual('api call failed');
+      expect(error.wrap401Errors).toBeUndefined();
+    }
+  });
+
+  test('when statusCode is not 401', async () => {
+    expect.assertions(2);
+
+    const fooFn = () => {
+      const err: any = new Error('api call failed');
+      err.statusCode = 400;
+      throw err;
+    };
+
+    const client: any = { foo: fooFn };
+
+    try {
+      await callAPI(client, 'foo', {}, { wrap401Errors: false });
+    } catch (error) {
+      expect(error.message).toEqual('api call failed');
+      expect(error.wrap401Errors).toBeUndefined();
+    }
+  });
 });
