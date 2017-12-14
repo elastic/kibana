3537d579545310fa42fcaf5c64bd17b36a626615
archanid <artistic.archana@gmail.com>
2017-12-14 09:17:00 -0600

    [color] backgrounds, cleanup

--- src/cli/color.js
+++ src/cli/color.js
@@ -1,1 +1,6 @@
-export { green, red, yellow } from 'chalk';
+import _ from 'lodash';
+import chalk from 'chalk';
+
+export const green = _.flow(chalk.black, chalk.bgGreen);
+export const red = _.flow(chalk.white, chalk.bgRed);
+export const yellow = _.flow(chalk.black, chalk.bgYellow);
