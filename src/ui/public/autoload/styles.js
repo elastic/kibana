c7f7cf3333ef09cc763811b30c48a46614c7f69e
CJ Cenizal <cj@cenizal.com>
2017-12-17 14:03:46 -0800

    Experiment: downgrade to 0.0.3 to see if tests pass.

--- src/ui/public/autoload/styles.js
+++ src/ui/public/autoload/styles.js
@@ -4,11 +4,11 @@ const theme = require('../theme');
 require('../../../../ui_framework/dist/ui_framework.css');
 
 // Elastic UI Framework, light theme
-const euiThemeLight = require('!!raw-loader!@elastic/eui/dist/eui_theme_k6_light.css');
+const euiThemeLight = require('!!raw-loader!@elastic/eui/dist/eui_theme_light.css');
 theme.registerTheme('light', euiThemeLight);
 
 // Elastic UI Framework, dark theme
-const euiThemeDark = require('!!raw-loader!@elastic/eui/dist/eui_theme_k6_dark.css');
+const euiThemeDark = require('!!raw-loader!@elastic/eui/dist/eui_theme_dark.css');
 theme.registerTheme('dark', euiThemeDark);
 
 // Set default theme.
