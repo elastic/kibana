11c085b370f36e6d582003b7d68475794ba99ad0
Chris Roberson <chrisronline@gmail.com>
2017-12-18 13:59:49 -0500

    Add all dashboard tests back in

--- test/functional/apps/dashboard/index.js
+++ test/functional/apps/dashboard/index.js
@@ -3,17 +3,17 @@ export default function ({ getService, loadTestFile }) {
 
   describe('dashboard app', function () {
     before(() => remote.setWindowSize(1200, 900));
-    // loadTestFile(require.resolve('./_bwc_shared_urls'));
-    // loadTestFile(require.resolve('./_dashboard_queries'));
-    // loadTestFile(require.resolve('./_dashboard_snapshots'));
-    // loadTestFile(require.resolve('./_dashboard_grid'));
-    // loadTestFile(require.resolve('./_panel_controls'));
+    loadTestFile(require.resolve('./_bwc_shared_urls'));
+    loadTestFile(require.resolve('./_dashboard_queries'));
+    loadTestFile(require.resolve('./_dashboard_snapshots'));
+    loadTestFile(require.resolve('./_dashboard_grid'));
+    loadTestFile(require.resolve('./_panel_controls'));
     loadTestFile(require.resolve('./_view_edit'));
-    // loadTestFile(require.resolve('./_dashboard'));
-    // loadTestFile(require.resolve('./_dashboard_state'));
-    // loadTestFile(require.resolve('./_dashboard_save'));
-    // loadTestFile(require.resolve('./_dashboard_time'));
-    // loadTestFile(require.resolve('./_dashboard_listing'));
-    // loadTestFile(require.resolve('./_dashboard_clone'));
+    loadTestFile(require.resolve('./_dashboard'));
+    loadTestFile(require.resolve('./_dashboard_state'));
+    loadTestFile(require.resolve('./_dashboard_save'));
+    loadTestFile(require.resolve('./_dashboard_time'));
+    loadTestFile(require.resolve('./_dashboard_listing'));
+    loadTestFile(require.resolve('./_dashboard_clone'));
   });
 }
