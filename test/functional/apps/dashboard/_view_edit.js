3baf18a756850bf61b73df692426a253bcec4be0
Chris Roberson <chrisronline@gmail.com>
2017-12-18 13:18:45 -0500

    Delete flaky view_edit.js test: 'and preserves edits on cancel > when time changed is stored with dashboard'.

--- test/functional/apps/dashboard/_view_edit.js
+++ test/functional/apps/dashboard/_view_edit.js
@@ -135,32 +135,6 @@ export default function ({ getService, getPageObjects }) {
           expect(panelCount).to.eql(visualizations.length);
         });
       });
-
-      describe('and preserves edits on cancel', function () {
-        it('when time changed is stored with dashboard', async function () {
-          await PageObjects.dashboard.gotoDashboardEditMode(dashboardName);
-          const newFromTime = '2015-09-19 06:31:44.000';
-          const newToTime = '2015-09-19 06:31:44.000';
-          await PageObjects.header.setAbsoluteRange('2013-09-19 06:31:44.000', '2013-09-19 06:31:44.000');
-          await PageObjects.dashboard.saveDashboard(dashboardName, true);
-          await PageObjects.header.clickToastOK();
-          await PageObjects.dashboard.clickEdit();
-          await PageObjects.header.setAbsoluteRange(newToTime, newToTime);
-          await PageObjects.dashboard.clickCancelOutOfEditMode();
-
-          await PageObjects.common.clickCancelOnModal();
-          await PageObjects.dashboard.saveDashboard(dashboardName, { storeTimeWithDashboard: true });
-          await PageObjects.header.clickToastOK();
-
-          await PageObjects.dashboard.loadSavedDashboard(dashboardName);
-
-          const fromTime = await PageObjects.header.getFromTime();
-          const toTime = await PageObjects.header.getToTime();
-
-          expect(fromTime).to.equal(newFromTime);
-          expect(toTime).to.equal(newToTime);
-        });
-      });
     });
 
     describe('Does not show lose changes warning', async function () {
