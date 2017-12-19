c45caeb9f4283917dbf1801a6b76510bd194ea67
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2017-12-19 14:03:27 -0500

    fixing tests

--- src/core_plugins/vega/index.js
+++ src/core_plugins/vega/index.js
@@ -15,12 +15,10 @@ export default kibana => new kibana.Plugin({
 
   // todo: does this plugin need this?   isEnabled(config) => config.get('vega.enabled'),
 
-  config: function (Joi) {
-    return Joi.object({
-      // todo: other plugins declare 'enabled' param, but it doesn't work by default. It doesn't work without it
-      enabled: Joi.boolean().default(true),
-      enableExternalUrls: Joi.boolean().default(true)
-    }).default();
-  },
+  config: Joi => Joi.object({
+    // todo: other plugins declare 'enabled' param, but it doesn't work by default. It doesn't work without it
+    enabled: Joi.boolean().default(true),
+    enableExternalUrls: Joi.boolean().default(true)
+  }).default(),
 
 });
