a1a8aacc22ad76d548d6b78828934bffb4027c98
Yuri Astrakhan <yuriastrakhan@gmail.com>
2017-12-22 12:46:39 -0500

    Upgrade to jQuery 3.2.1 (#15674)
    * Upgrade to jQuery 3.2.1
    
    The jquery-migrate showed a number of warnings,
    but no errors, so it should be safe to use.
    
    Warnings mostly related to bind, unbind, focus, ...
    https://github.com/jquery/jquery-migrate/blob/master/warnings.md
    
    To enable jquery-migrate, I added it to package.json, and added
    a require('jquery-migrate') to the webpackShims/jquery.js right
    after the require('jquery').
    
    * Breaks in jQuery3: $(window).offset()
    
    * await field.type(), update chromedriver

--- test/functional/page_objects/settings_page.js
+++ test/functional/page_objects/settings_page.js
@@ -336,7 +336,7 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
       log.debug(`setIndexPatternField(${indexPatternName})`);
       const field = await this.getIndexPatternField();
       await field.clearValue();
-      field.type(indexPatternName);
+      await field.type(indexPatternName);
     }
 
     async getCreateIndexPatternGoToStep2Button() {
