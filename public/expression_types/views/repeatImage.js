6f9c4a89847be512f80a6847340398f30df57673
Rashid Khan <github.fliplap@spamgourmet.com>
2017-12-25 14:08:19 -0700

    Improve help and add emptyImage support

--- public/expression_types/views/repeatImage.js
+++ public/expression_types/views/repeatImage.js
@@ -9,7 +9,22 @@ export const repeatImage = () => ({
   modelArgs: [['size', { label: 'Count' }]],
   args: [{
     name: 'image',
-    displayName: 'Upload Image',
+    displayName: 'Image',
+    help: 'An image to repeat',
+    argType: 'imageUpload',
+
+    // TODO: This code is repeated in both image.js and this file. Shouldn't this be handled in the imageUpload argType?
+    resolve({ args }) {
+      const wrap = (val) => ({ dataurl: val });
+      const dataurl = get(args, 'dataurl.0', null);
+
+      if (dataurl && isValid(dataurl)) return wrap(dataurl);
+      return wrap(elasticLogo);
+    },
+  }, {
+    name: 'emptyImage',
+    displayName: 'Empty Image',
+    help: 'An image to fill up the difference between the value and the max count',
     argType: 'imageUpload',
 
     // TODO: This code is repeated in both image.js and this file. Shouldn't this be handled in the imageUpload argType?
@@ -23,11 +38,13 @@ export const repeatImage = () => ({
   }, {
     name: 'size',
     displayName: 'Image size',
+    help: 'The size of the largest dimension of the image. Eg, if the image is tall but not wide, this is the height',
     argType: 'number',
     default: '100',
   }, {
     name: 'max',
-    displayName: 'Max repeated images',
+    displayName: 'Max count',
+    help: 'The maximum number of repeated images',
     argType: 'number',
     default: '1000',
   }],
