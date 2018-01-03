93da67518678061a2fbf868dd594da261173b3b5
Brandon Kobel <brandon.kobel@gmail.com>
2018-01-03 11:11:30 -0500

    Advanced Settings - Image Input (#15342)
    * Beginning to work on image advanced setting type
    
    * Making the maxSize code and UI more user-friendly (hopefully).
    
    * Getting rid of that border, I was using the wrong class
    
    * Displaying an image for the preview
    
    * Not displaying the image if we don't have a value
    
    * Removing no longer used code
    
    * Adding explicit throw in the subscription to make our intents more clear
    
    * Changing some of the Observable style based on  input from the peer review
    
    * Using the maxSizeKB option to enforce the size
    
    * Adding support for no max
    
    * Throwing better errors
    
    * No longer duplicated the multiple usage error message
    
    * Using the maxSize attribute to control the max size of the settings
    
    * No longer using JSON.stringify, it's already a string and that approach
    won't really do anything for other data-types
    
    * Extracting validate to it's own method
    
    * No longer hardcoding the maxLength validation in the uiSettings
    
    * Using a Joi schema instead
    
    * Punting on server-side validation for the time being

---
+++ src/ui/public/directives/input_base64.js
@@ -0,0 +1,89 @@
+import { uiModules } from 'ui/modules';
+import { Observable } from 'rxjs/Rx';
+const module = uiModules.get('kibana');
+
+const multipleUsageErrorMessage = 'Cannot use input-base-sixty-four directive on input with `multiple` attribute';
+
+const createFileContent$ = (file) => {
+  return Observable.create(observer => {
+    const reader = new FileReader();
+    reader.onerror = (err) => {
+      observer.error(err);
+    };
+
+    reader.onload = () => {
+      observer.next(reader.result);
+      observer.complete();
+    };
+
+    reader.readAsDataURL(file);
+
+    return () => {
+      reader.abort();
+    };
+  });
+};
+
+module.directive('inputBaseSixtyFour', function () {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    scope: 'isolate',
+    link: function ($scope, $elem, attrs, ngModel) {
+      if ($elem.prop('multiple')) {
+        throw new Error(multipleUsageErrorMessage);
+      }
+
+      const maxSizeValidator = (dataUrl) => {
+        return {
+          errorKey: 'maxSize',
+          isValid: attrs.max === '' || dataUrl.length <= parseInt(attrs.max)
+        };
+      };
+
+      const validators = [ maxSizeValidator ];
+
+      // produce fileContent$ whenever the $element 'change' event is triggered.
+      const fileContent$ = Observable
+        .fromEvent($elem, 'change')
+        .map(e => e.target.files)
+        .switchMap(files => {
+          if (files.length === 0) {
+            return [];
+          }
+
+          if (files.length > 1) {
+            throw new Error(multipleUsageErrorMessage);
+          }
+
+          return createFileContent$(files[0]);
+        })
+        .share();
+
+      // validate the content of the files after it is loaded
+      const validations$ = fileContent$
+        .map(fileContent => (
+          validators.map(validator => validator(fileContent))
+        ));
+
+      // push results from input/validation to the ngModel
+      const unsubscribe = Observable
+        .combineLatest(fileContent$, validations$)
+        .subscribe(([ fileContent, validations ]) => {
+          $scope.$evalAsync(() => {
+            validations.forEach(validation => {
+              ngModel.$setValidity(validation.errorKey, validation.isValid);
+            });
+
+            if (validations.every(validation => validation.isValid)) {
+              ngModel.$setViewValue(fileContent);
+            }
+          });
+        }, (err) => {
+          throw err;
+        });
+
+      $scope.$on('destroy', unsubscribe);
+    }
+  };
+});
