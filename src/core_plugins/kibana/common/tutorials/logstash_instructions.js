d089b1660d842d3cf8d3097ef9faf93b778910f6
Shaunak Kashyap <ycombinator@gmail.com>
2018-01-04 09:53:45 -0800

    Removing unrelated instructions

--- src/core_plugins/kibana/common/tutorials/logstash_instructions.js
+++ src/core_plugins/kibana/common/tutorials/logstash_instructions.js
@@ -1,18 +1,12 @@
-const SKIP_INSTALL_SENTENCE = '_Skip this step if Logstash is already installed._';
-const FIRST_TIME_SENTENCE = '_First time using Logstash? See the [Getting Started Guide]({config.docs.logstash}/'
-  + 'getting-started-with-logstash.html)._';
-
 export const LOGSTASH_INSTRUCTIONS = {
   INSTALL: {
     OSX: [
       {
         title: 'Download and install the Java runtime environment',
-        textPre: `Follow the installation instructions [here](https://docs.oracle.com/javase/8/docs/technotes/guides/install/mac_jre.html). ` +
-          `${SKIP_INSTALL_SENTENCE} ${FIRST_TIME_SENTENCE}`
+        textPre: 'Follow the installation instructions [here](https://docs.oracle.com/javase/8/docs/technotes/guides/install/mac_jre.html).'
       },
       {
         title: 'Download and install Logstash',
-        textPre: SKIP_INSTALL_SENTENCE,
         commands: [
           'curl -L -O https://artifacts.elastic.co/downloads/logstash/logstash-{config.kibana.version}.tar.gz',
           'tar xzvf logstash-{config.kibana.version}.tar.gz'
