21c9637022304f5d2b79e3741f62a69b770a8fd0
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2018-01-05 14:12:53 -0500

    better vega tests

--- src/core_plugins/vega/public/__tests__/vega_visualization.js
+++ src/core_plugins/vega/public/__tests__/vega_visualization.js
@@ -5,9 +5,12 @@ import LogstashIndexPatternStubProvider from 'fixtures/stubbed_logstash_index_pa
 import * as visModule from 'ui/vis';
 import { ImageComparator } from 'test_utils/image_comparator';
 
-import embeddedDataGraph from '!!raw-loader!./embedded_data_graph.json';
-import embeddedDataImage256 from './embedded_data_image_256.png';
-import embeddedDataImage512 from './embedded_data_image_512.png';
+import vegaliteGraph from '!!raw-loader!./vegalite_graph.hjson';
+import vegaliteImage256 from './vegalite_image_256.png';
+import vegaliteImage512 from './vegalite_image_512.png';
+
+import vegaGraph from '!!raw-loader!./vega_graph.hjson';
+import vegaImage512 from './vega_image_512.png';
 
 import { VegaParser } from '../data_model/vega_parser';
 import { SearchCache } from '../data_model/search_cache';
@@ -47,24 +50,43 @@ describe('VegaVisualizations', () => {
       imageComparator.destroy();
     });
 
-    it('should show the graph and update on resize', async function () {
+    it('should show vegalite graph and update on resize', async function () {
 
       let vegaVis;
       try {
         vegaVis = new VegaVisualization(domNode, vis);
-        const vegaParser = new VegaParser(JSON.parse(embeddedDataGraph), new SearchCache());
+        const vegaParser = new VegaParser(vegaliteGraph, new SearchCache());
         await vegaParser.parseAsync();
 
         await vegaVis.render(vegaParser, { data: true });
-        const mismatchedPixels1 = await compareImage(embeddedDataImage512);
+        const mismatchedPixels1 = await compareImage(vegaliteImage512);
         expect(mismatchedPixels1).to.be.lessThan(PIXEL_DIFF);
 
         domNode.style.width = '256px';
         domNode.style.height = '256px';
 
         await vegaVis.render(vegaParser, { resize: true });
-        const mismatchedPixels2 = await compareImage(embeddedDataImage256);
+        const mismatchedPixels2 = await compareImage(vegaliteImage256);
         expect(mismatchedPixels2).to.be.lessThan(PIXEL_DIFF);
+
+      } finally {
+        vegaVis.destroy();
+      }
+
+    });
+
+    it('should show vega graph', async function () {
+
+      let vegaVis;
+      try {
+
+        vegaVis = new VegaVisualization(domNode, vis);
+        const vegaParser = new VegaParser(vegaGraph, new SearchCache());
+        await vegaParser.parseAsync();
+
+        await vegaVis.render(vegaParser, { data: true });
+        const mismatchedPixels = await compareImage(vegaImage512);
+        expect(mismatchedPixels).to.be.lessThan(PIXEL_DIFF);
 
       } finally {
         vegaVis.destroy();
