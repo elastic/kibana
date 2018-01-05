56dc541bdd00b2033ce18d30262621fa2c24c558
Brandon Kobel <brandon.kobel@gmail.com>
2018-01-05 08:05:13 -0500

    Sharing render-complete (#15574) (#15853)
    * Naively switching to Observables
    
    * Dispatching native renderStart and renderComplete events
    
    * Emitting actual event...
    
    * Removing the visualize debounce
    
    * Fixing unsubscribe
    
    * Dispatching correct event for the metric visualization
    
    * Moving the $scope.$apply to right after the call to render so it does
    what it's supposed to
    
    * Switching angularjs attribute name
    
    * Removing the renderStart since it doesn't do anything
    
    * Removing the renderComplete trigger from visualize, since no one listens
    at this level
    
    * Using vis.requestError to communicate when there's a request error
    
    * Grabbing the container when creating the observable
    
    * Dispatching renderStart as soon as we can
    
    * Only emitting renderComplete after we wait for a new animation frame
    
    * Can't ignore the first resize, sometimes it's actually a resize
    
    * Use $timeout in visualiationEditor render, it can be in a digest already
    
    * Calling renderComplete in componentDidUpdate as well
    
    * Switching tests to use render-complete
    
    * Fixing peer review corrections from @gammon
    
    * Removing visDiv reference, we don't need it anymore
    
    * Removing requestAnimationFrame
    
    * Removing unused $timeout
    
    * Removing test that was testing an implementation detail
    
    * Adding $timeout.flush
    
    * Moving the failures to the actual tests don't work.
    
    * Deleting tests that weren't running before
    
    * Removing import of removed test
    
    * Consolidating the dispatchRenderComplete/dispatchRenderStart

--- test/functional/apps/dashboard/_dashboard_snapshots.js
+++ test/functional/apps/dashboard/_dashboard_snapshots.js
@@ -37,7 +37,7 @@ export default function ({ getService, getPageObjects, updateBaselines }) {
       await PageObjects.dashboard.clickFullScreenMode();
       await PageObjects.dashboard.toggleExpandPanel();
 
-      await PageObjects.dashboard.waitForRenderCounter(2);
+      await PageObjects.dashboard.waitForRenderComplete();
       const percentSimilar = await screenshot.compareAgainstBaseline('tsvb_dashboard', updateBaselines);
 
       await PageObjects.dashboard.clickExitFullScreenLogoButton();
@@ -56,10 +56,7 @@ export default function ({ getService, getPageObjects, updateBaselines }) {
       await PageObjects.dashboard.clickFullScreenMode();
       await PageObjects.dashboard.toggleExpandPanel();
 
-      // TODO: hopefully we will be able to get rid of this sleep once render-complete is implemented.
-      // right now the render-counter value is unstable and unreliable.  Sometimes it'll be 4 when it's "settled",
-      // sometimes it'll be 6.
-      await PageObjects.dashboard.waitForRenderCounter(4);
+      await PageObjects.dashboard.waitForRenderComplete();
       await PageObjects.common.sleep(1000);
 
       const percentSimilar = await screenshot.compareAgainstBaseline('area_chart', updateBaselines);
