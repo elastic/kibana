73eced9e52c49cbca44c3b108063f3e33850f93c
Court Ewing <court@epixa.com>
2018-01-07 09:31:15 -0500

    Revert "[optimizer] More aggressive chunking of common/vendor code (#15816)" (#15878) (#15879)
    This reverts commit 92b373b3048f1be2b380781b76e4255f31e6a6a1.

--- tasks/config/karma.js
+++ tasks/config/karma.js
@@ -37,11 +37,8 @@ module.exports = function (grunt) {
 
       // list of files / patterns to load in the browser
       files: [
-        'http://localhost:5610/bundles/vendors.bundle.js',
         'http://localhost:5610/bundles/commons.bundle.js',
         'http://localhost:5610/bundles/tests.bundle.js',
-
-        'http://localhost:5610/bundles/vendors.style.css',
         'http://localhost:5610/bundles/commons.style.css',
         'http://localhost:5610/bundles/tests.style.css'
       ],
@@ -129,11 +126,8 @@ module.exports = function (grunt) {
       singleRun: true,
       options: {
         files: [
-          'http://localhost:5610/bundles/vendors.bundle.js',
           'http://localhost:5610/bundles/commons.bundle.js',
           `http://localhost:5610/bundles/tests.bundle.js?shards=${TOTAL_CI_SHARDS}&shard_num=${n}`,
-
-          'http://localhost:5610/bundles/vendors.style.css',
           'http://localhost:5610/bundles/commons.style.css',
           'http://localhost:5610/bundles/tests.style.css'
         ]
