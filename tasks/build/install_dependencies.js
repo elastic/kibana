3614219323bf86f05de0ab6b39c43151776fbde3
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-01-09 00:00:28 +0100

    Use local yarn

--- tasks/build/install_dependencies.js
+++ tasks/build/install_dependencies.js
@@ -3,7 +3,9 @@ module.exports = function (grunt) {
   grunt.registerTask('_build:installDependencies', function () {
     grunt.file.mkdir('<%= root %>/build/kibana/node_modules');
 
-    exec('yarn --production --ignore-optional', {
+    const yarn = `${__dirname}/../vendor/yarn-1.3.2.js`;
+
+    exec(`${yarn} --production --ignore-optional`, {
       cwd: grunt.config.process('<%= root %>/build/kibana')
     }, this.async());
   });
