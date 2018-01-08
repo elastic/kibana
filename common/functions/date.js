b2175a6e14ec9014446d92a6f54367d1b31c9242
Joe Fleming <w33ble@users.noreply.github.com>
2018-01-08 15:03:11 -0700

    Fix/moment use (#283)
    * fix: date function doesn't pass input to moment
    
    moment doesn't take generic string input anymore, this relies on the native Date type, unless format is provided
    
    * fix: wrap formatdate's context input in Date type
    
    moment doesn't allow passing arbitrary strings in

--- common/functions/date.js
+++ common/functions/date.js
@@ -1,25 +1,42 @@
 import moment from 'moment';
 
+const getInputDate = (input) => {
+  // return current date if no input
+  if (!input) return new Date();
+
+  // attempt to cast to a number
+  const numInput = Number(input);
+  if (!isNaN(numInput)) return numInput;
+
+  // return the input
+  return input;
+};
+
 export const date = {
   name: 'date',
   type: 'number',
   context: {
     types: ['null'],
   },
-  help: 'Returns the current time, or a time parsed from a string, as milliseconds since epoch',
+  help: 'Returns the current time, or a time parsed from a string, as milliseconds since epoch.',
   args: {
     _: {
       types: ['string', 'null'],
-      help: 'An optional date string to parse into milliseconds since epoch',
+      help: 'An optional date string to parse into milliseconds since epoch. ' +
+        'Can be either a valid Javascript Date input or a string to parse using the format argument. ',
     },
     format: {
       types: ['string', 'null'],
-      help: 'The momentJS format for parsing the optional date string (See https://momentjs.com/docs/#/displaying/)',
+      help: 'The momentJS format for parsing the optional date string (See https://momentjs.com/docs/#/displaying/).',
     },
   },
   fn: (context, args) => {
-    if (!args._) return moment().valueOf();
-    if (!args.format) return moment(args._).valueOf();
-    return moment(args._, args.format).valueOf();
+    const { _: date, format } = args;
+    const useMoment = date && format;
+    const outputDate = useMoment ? moment(date, format).toDate() : new Date(getInputDate(date));
+
+    if (isNaN(outputDate.getTime())) throw new Error(`Invalid date input: ${args._}`);
+
+    return outputDate.valueOf();
   },
 };
