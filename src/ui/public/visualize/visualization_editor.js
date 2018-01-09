4389695ea07593a0b3de86995bdf690a8547205d
Tim Roes <github@timroes.de>
2018-01-09 09:56:46 +0100

    Refactor rendering events (#15895)
    * Rename timelion internal event for more clarity
    
    * Add test for native DOM events
    
    * Remove Angular renderComplete event

--- src/ui/public/visualize/visualization_editor.js
+++ src/ui/public/visualize/visualization_editor.js
@@ -29,9 +29,7 @@ uiModules
 
         $scope.renderFunction = () => {
           if (!$scope.vis) return;
-          editor.render($scope.visData, $scope.searchSource, getUpdateStatus($scope), $scope.uiState).then(() => {
-            $scope.$emit('renderComplete');
-          });
+          editor.render($scope.visData, $scope.searchSource, getUpdateStatus($scope), $scope.uiState);
         };
 
         $scope.$on('render', (event) => {
