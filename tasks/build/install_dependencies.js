35573d12ced27592ddeb7e4b4b380ca8dc5c57a7
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-01-09 22:40:08 +0100

    Review fixes

--- tasks/build/install_dependencies.js
+++ tasks/build/install_dependencies.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     // We rely on a local version of Yarn that contains the bugfix from
     // https://github.com/yarnpkg/yarn/pull/5059. Once this fix is merged
     // and released we can use Yarn directly in the build.
-    const yarn = `${__dirname}/../vendor/yarn-1.3.2-with-ignore-fix.js`;
+    const yarn = require.resolve('../vendor/yarn-1.3.2-with-ignore-fix.js');
 
     exec(`${yarn} --production --ignore-optional --frozen-lockfile`, {
       cwd: grunt.config.process('<%= root %>/build/kibana')
