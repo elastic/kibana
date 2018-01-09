ad9709b75cf2d79d197a98855fe09b7585d67fd4
ppisljar <peter.pisljar@gmail.com>
2018-01-09 13:14:30 +0100

    fixing boolean filters

--- src/ui/public/agg_types/buckets/create_filter/terms.js
+++ src/ui/public/agg_types/buckets/create_filter/terms.js
@@ -2,6 +2,15 @@ import { buildPhraseFilter } from 'ui/filter_manager/lib/phrase';
 
 export function AggTypesBucketsCreateFilterTermsProvider() {
   return function (aggConfig, key) {
-    return buildPhraseFilter(aggConfig.params.field, key, aggConfig.vis.indexPattern);
+    const phraseFilter = buildPhraseFilter(aggConfig.params.field, key, aggConfig.vis.indexPattern);
+    if (aggConfig.params.field.type === 'boolean') {
+      const matchFilter = phraseFilter.query.match[aggConfig.params.field.name];
+      if (matchFilter.query === 0) {
+        matchFilter.query = 'false';
+      } else {
+        matchFilter.query = 'true';
+      }
+    }
+    return phraseFilter;
   };
 }
