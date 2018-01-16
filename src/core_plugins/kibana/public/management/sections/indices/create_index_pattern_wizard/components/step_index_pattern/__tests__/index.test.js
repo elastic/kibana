92219eb933b22cc78aafb714eddd09d8d9d16d2f
Chris Roberson <chrisronline@gmail.com>
2018-01-16 11:08:40 -0500

    [Management] Index pattern step in React! (#15936)
    * Index pattern step in React!
    
    * Remove dead lines
    
    * Ensure this only shows up when applicable
    
    * PR feedback
    
    * Use pager
    
    * Add tests for lib/
    
    * PR feedback
    
    * Tests and PR feedback
    
    * More tests and PR feedback
    
    * New jest functionality

---
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/components/step_index_pattern/__tests__/index.test.js
@@ -0,0 +1,31 @@
+const render = jest.fn();
+const unmountComponentAtNode = jest.fn();
+
+jest.doMock('react-dom', () => ({ render, unmountComponentAtNode }));
+
+const { renderStepIndexPattern, destroyStepIndexPattern } = require('../index');
+
+describe('StepIndexPatternRender', () => {
+  beforeEach(() => {
+    render.mockClear();
+    unmountComponentAtNode.mockClear();
+  });
+
+  it('should call render', () => {
+    renderStepIndexPattern(
+      'reactDiv',
+      [],
+      '',
+      false,
+      {},
+      () => {}
+    );
+
+    expect(render.mock.calls.length).toBe(1);
+  });
+
+  it('should call unmountComponentAtNode', () => {
+    destroyStepIndexPattern('reactDiv');
+    expect(unmountComponentAtNode.mock.calls.length).toBe(1);
+  });
+});
---
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/components/step_index_pattern/components/header/header.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import {
+  EuiTitle,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiSpacer,
+  EuiButton,
+  EuiForm,
+  EuiFormRow,
+  EuiFieldText,
+} from '@elastic/eui';
+
+export const Header = ({
+  isInputInvalid,
+  errors,
+  characterList,
+  query,
+  onQueryChanged,
+  goToNextStep,
+  isNextStepDisabled,
+}) => (
+  <div>
+    <EuiTitle size="s">
+      <h2>
+        Step 1 of 2: Define index pattern
+      </h2>
+    </EuiTitle>
+    <EuiSpacer size="m"/>
+    <EuiFlexGroup justifyContent="spaceBetween" alignItems="flexEnd">
+      <EuiFlexItem grow={false}>
+        <EuiForm
+          isInvalid={isInputInvalid}
+        >
+          <EuiFormRow
+            label="Index pattern"
+            isInvalid={isInputInvalid}
+            error={errors}
+            helpText={
+              <div>
+                <p>You can use a <strong>*</strong> as a wildcard in your index pattern.</p>
+                <p>You can&apos;t use empty spaces or the characters <strong>{characterList}</strong>.</p>
+              </div>
+            }
+          >
+            <EuiFieldText
+              name="indexPattern"
+              placeholder="index-name-*"
+              value={query}
+              isInvalid={isInputInvalid}
+              onChange={onQueryChanged}
+              data-test-subj="createIndexPatternNameInput"
+            />
+          </EuiFormRow>
+        </EuiForm>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiButton
+          iconType="arrowRight"
+          onClick={() => goToNextStep(query)}
+          isDisabled={isNextStepDisabled}
+          data-test-subj="createIndexPatternGoToStep2Button"
+        >
+          Next step
+        </EuiButton>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  </div>
+);
