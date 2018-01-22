dc551383e6ff25e35c4e5c676455dba760698bcf
Nathan Reese <reese.nathan@gmail.com>
2018-01-22 09:52:26 -0700

    Timepicker - add 'recently selected' range option (#15868)
    * store time filter history in localStorage
    
    * allow selection of relative history
    
    * use persisted log for storage
    
    * display time history in quick list
    
    * add recent timepicker panel
    
    * set timehistory mode to recent
    
    * add message to display when no time history exists yet
    
    * migrate persisted_logs tests from mocha to jest
    
    * reword text for no history
    
    * better wording
    
    * move logic from pretty-duration directive into its own file so logic can be shared by recent history view
    
    * some small changes
    
    * fix list styling
    
    * make recent be a single column
    
    * store quick times in history
    
    * set default timezone for moment in pretty_duration tests

---
+++ src/ui/public/timepicker/pretty_duration.js
@@ -0,0 +1,55 @@
+import dateMath from '@elastic/datemath';
+import moment from 'moment';
+import { timeUnits } from './time_units';
+
+function cantLookup(timeFrom, timeTo, dateFormat) {
+  const displayFrom = formatTimeString(timeFrom, dateFormat);
+  const displayTo = formatTimeString(timeTo, dateFormat, true);
+  return `${displayFrom} to ${displayTo}`;
+}
+
+function formatTimeString(timeString, dateFormat, roundUp = false) {
+  if (moment(timeString).isValid()) {
+    return moment(timeString).format(dateFormat);
+  } else {
+    if (timeString === 'now') {
+      return 'now';
+    } else {
+      const tryParse = dateMath.parse(timeString, { roundUp: roundUp });
+      return moment.isMoment(tryParse) ? '~ ' + tryParse.fromNow() : timeString;
+    }
+  }
+}
+
+export function prettyDuration(timeFrom, timeTo, getConfig) {
+  const quickRanges = getConfig('timepicker:quickRanges');
+  const dateFormat = getConfig('dateFormat');
+
+  const lookupByRange = {};
+  quickRanges.forEach((frame) => {
+    lookupByRange[frame.from + ' to ' + frame.to] = frame;
+  });
+
+  // If both parts are date math, try to look up a reasonable string
+  if (timeFrom && timeTo && !moment.isMoment(timeFrom) && !moment.isMoment(timeTo)) {
+    const tryLookup = lookupByRange[timeFrom.toString() + ' to ' + timeTo.toString()];
+    if (tryLookup) {
+      return tryLookup.display;
+    } else {
+      const fromParts = timeFrom.toString().split('-');
+      if (timeTo.toString() === 'now' && fromParts[0] === 'now' && fromParts[1]) {
+        const rounded = fromParts[1].split('/');
+        let text = 'Last ' + rounded[0];
+        if (rounded[1]) {
+          text = text + ' rounded to the ' + timeUnits[rounded[1]];
+        }
+        return text;
+      } else {
+        return cantLookup(timeFrom, timeTo, dateFormat);
+      }
+    }
+  }
+
+  // If at least one part is a moment, try to make pretty strings by parsing date math
+  return cantLookup(timeFrom, timeTo, dateFormat);
+}
