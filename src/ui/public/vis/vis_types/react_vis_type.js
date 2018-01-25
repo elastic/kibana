0fe6320f2bad3cba3b4d4e86d1f261c2be46c172
Peter Pisljar <peter.pisljar@gmail.com>
2018-01-25 09:19:52 +0100

    refactoring markdown_vis  to not use angular (#16153)

--- src/ui/public/vis/vis_types/react_vis_type.js
+++ src/ui/public/vis/vis_types/react_vis_type.js
@@ -14,8 +14,7 @@ export function ReactVisTypeProvider(Private, getAppState, config) {
     render(visData) {
       this.visData = visData;
 
-      return new Promise((resolve, reject) => {
-        if (!this.visData) return reject();
+      return new Promise((resolve) => {
         const Component = this.vis.type.visConfig.component;
         render(<Component config={config} vis={this.vis} appState={getAppState()} visData={visData} renderComplete={resolve} />, this.el);
       });
