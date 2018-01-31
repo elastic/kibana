a9a8d1cc601dd2e37afa6549edb6c13f48ff82e6
Peter Pisljar <peter.pisljar@gmail.com>
2018-01-31 12:14:10 +0100

    postFlightRequest should only run if the main request did (#16248)

--- src/ui/public/vis/request_handlers/courier.js
+++ src/ui/public/vis/request_handlers/courier.js
@@ -15,11 +15,22 @@ const CourierRequestHandlerProvider = function (Private, courier, timefilter) {
         searchSource.set('query', appState.query);
       }
 
+      // AggConfig contains circular reference to vis, which contains visualization parameters,
+      // which we should not look at
+      const copyAggs = (aggs) => {
+        return aggs.map(agg => {
+          return {
+            type: agg.type,
+            params: agg.params
+          };
+        });
+      };
+
       const shouldQuery = () => {
         if (!searchSource.lastQuery || vis.reload) return true;
         if (!_.isEqual(_.cloneDeep(searchSource.get('filter')), searchSource.lastQuery.filter)) return true;
         if (!_.isEqual(_.cloneDeep(searchSource.get('query')), searchSource.lastQuery.query)) return true;
-        if (!_.isEqual(_.cloneDeep(searchSource.get('aggs')()), searchSource.lastQuery.aggs)) return true;
+        if (!_.isEqual(_.cloneDeep(copyAggs(vis.aggs)), searchSource.lastQuery.aggs)) return true;
         if (!_.isEqual(_.cloneDeep(timefilter.time), searchSource.lastQuery.time)) return true;
 
         return false;
@@ -32,25 +43,27 @@ const CourierRequestHandlerProvider = function (Private, courier, timefilter) {
             searchSource.lastQuery = {
               filter: _.cloneDeep(searchSource.get('filter')),
               query: _.cloneDeep(searchSource.get('query')),
-              aggs: _.cloneDeep(searchSource.get('aggs')()),
+              aggs: _.cloneDeep(copyAggs(vis.aggs)),
               time: _.cloneDeep(timefilter.time)
             };
 
             searchSource.rawResponse = resp;
 
-            resolve(_.cloneDeep(resp));
+            return _.cloneDeep(resp);
+          }).then(async resp => {
+            for (const agg of vis.aggs) {
+              const nestedSearchSource = new SearchSource().inherits(searchSource);
+              resp = await agg.type.postFlightRequest(resp, vis.aggs, agg, nestedSearchSource);
+            }
+
+            searchSource.finalResponse = resp;
+            resolve(resp);
           }).catch(e => reject(e));
 
           courier.fetch();
         } else {
-          resolve(_.cloneDeep(searchSource.rawResponse));
+          resolve(_.cloneDeep(searchSource.finalResponse));
         }
-      }).then(async resp => {
-        for (const agg of vis.aggs) {
-          const nestedSearchSource = new SearchSource().inherits(searchSource);
-          resp = await agg.type.postFlightRequest(resp, vis.aggs, agg, nestedSearchSource);
-        }
-        return resp;
       });
     }
   };
