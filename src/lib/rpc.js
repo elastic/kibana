14868075e66cef8802a0c7abe5a825478d1c9494
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-02-03 01:33:55 +0100

    Bump dependencies (#47)

--- src/lib/rpc.js
+++ src/lib/rpc.js
@@ -1,4 +1,4 @@
-const promisify = require('es6-promisify');
+const { promisify } = require('es6-promisify');
 const process = require('child_process');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
