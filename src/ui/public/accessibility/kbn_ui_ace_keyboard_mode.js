bd4187f1e625e960b1e9a57a8efab1a884c8e848
CJ Cenizal <cj@cenizal.com>
2018-02-02 16:01:40 -0800

    Fix bug where ESC was closing the Console autocompletion suggestions menu, but then also exiting edit mode. (#16500)

--- src/ui/public/accessibility/kbn_ui_ace_keyboard_mode.js
+++ src/ui/public/accessibility/kbn_ui_ace_keyboard_mode.js
@@ -26,24 +26,24 @@ uiModules.get('kibana')
 
       const hint = angular.element(
         `<div
-        class="uiAceKeyboardHint"
-        id="${uniqueId}"
-        tabindex="0"
-        role="application"
-      >
-        <p class="kuiText kuiVerticalRhythmSmall">
-          Press Enter to start editing.
-        </p>
-        <p class="kuiText kuiVerticalRhythmSmall">
-          When you&rsquo;re done, press Escape to stop editing.
-        </p>
-      </div>
-    `);
+          class="uiAceKeyboardHint"
+          id="${uniqueId}"
+          tabindex="0"
+          role="application"
+        >
+          <p class="kuiText kuiVerticalRhythmSmall">
+            Press Enter to start editing.
+          </p>
+          <p class="kuiText kuiVerticalRhythmSmall">
+            When you&rsquo;re done, press Escape to stop editing.
+          </p>
+        </div>`
+      );
 
       const uiAceTextbox = element.find('textarea');
 
       function startEditing() {
-      // We are not using ng-class in the element, so that we won't need to $compile it
+        // We are not using ng-class in the element, so that we won't need to $compile it
         hint.addClass('uiAceKeyboardHint-isInactive');
         uiAceTextbox.focus();
       }
@@ -63,12 +63,32 @@ uiModules.get('kibana')
         enableOverlay();
       });
 
+      let isAutoCompleterOpen;
+
+      // We have to capture this event on the 'capture' phase, otherewise Ace will have already
+      // dismissed the autocompleter when the user hits ESC.
+      document.addEventListener('keydown', () => {
+        const autoCompleter = document.querySelector('.ace_autocomplete');
+
+        if (!autoCompleter) {
+          isAutoCompleterOpen = false;
+          return;
+        }
+
+        // The autoComplete is just hidden when it's closed, not removed from the DOM.
+        isAutoCompleterOpen = autoCompleter.style.display !== 'none';
+      }, { capture: true });
+
       uiAceTextbox.keydown((ev) => {
         if (ev.keyCode === keyCodes.ESCAPE) {
-          ev.preventDefault();
-          ev.stopPropagation();
-          enableOverlay();
-          hint.focus();
+          // If the autocompletion context menu is open then we want to let ESC close it but
+          // **not** exit out of editing mode.
+          if (!isAutoCompleterOpen) {
+            ev.preventDefault();
+            ev.stopPropagation();
+            enableOverlay();
+            hint.focus();
+          }
         }
       });
 
