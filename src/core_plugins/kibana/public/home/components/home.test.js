56d5bef407905cd75880ded9cd1db448cc1e127d
Nathan Reese <reese.nathan@gmail.com>
2018-02-06 14:50:09 -0700

    add test cases for home feature panels

--- src/core_plugins/kibana/public/home/components/home.test.js
+++ src/core_plugins/kibana/public/home/components/home.test.js
@@ -4,46 +4,17 @@ import { Home } from './home';
 import { FeatureCatalogueCategory } from 'ui/registry/feature_catalogue';
 
 const addBasePath = (url) => { return `base_path/${url}`; };
-const directories = [
-  {
-    id: 'dashboard',
-    title: 'Dashboard',
-    description: 'Display and share a collection of visualizations and saved searches.',
-    icon: '/plugins/kibana/assets/app_dashboard.svg',
-    path: 'dashboard_landing_page',
-    showOnHomePage: true,
-    category: FeatureCatalogueCategory.DATA
-  },
-  {
-    id: 'index_patterns',
-    title: 'Index Patterns',
-    description: 'Manage the index patterns that help retrieve your data from Elasticsearch.',
-    icon: '/plugins/kibana/assets/app_index_pattern.svg',
-    path: 'index_pattern_landing_page',
-    showOnHomePage: true,
-    category: FeatureCatalogueCategory.ADMIN
-  },
-  {
-    id: 'management',
-    title: 'Management',
-    description: 'Your center console for managing the Elastic Stack.',
-    icon: '/plugins/kibana/assets/app_management.svg',
-    path: 'management_landing_page',
-    showOnHomePage: false,
-    category: FeatureCatalogueCategory.ADMIN
-  }
-];
-const recentlyAccessed = [
-  {
-    label: 'my vis',
-    link: 'link_to_my_vis'
-  }
-];
 
 test('should render home component', () => {
+  const recentlyAccessed = [
+    {
+      label: 'my vis',
+      link: 'link_to_my_vis'
+    }
+  ];
   const component = shallow(<Home
     addBasePath={addBasePath}
-    directories={directories}
+    directories={[]}
     isCloudEnabled={true}
     recentlyAccessed={recentlyAccessed}
   />);
@@ -58,4 +29,66 @@ test('should not contain RecentlyAccessed panel when there is no recentlyAccesse
     recentlyAccessed={[]}
   />);
   expect(component).toMatchSnapshot(); // eslint-disable-line
+});
+
+describe('directories', () => {
+  test('should render DATA directory entry in "Explore Data" panel', () => {
+    const directoryEntry = {
+      id: 'dashboard',
+      title: 'Dashboard',
+      description: 'Display and share a collection of visualizations and saved searches.',
+      icon: '/plugins/kibana/assets/app_dashboard.svg',
+      path: 'dashboard_landing_page',
+      showOnHomePage: true,
+      category: FeatureCatalogueCategory.DATA
+    };
+
+    const component = shallow(<Home
+      addBasePath={addBasePath}
+      directories={[directoryEntry]}
+      isCloudEnabled={true}
+      recentlyAccessed={[]}
+    />);
+    expect(component).toMatchSnapshot(); // eslint-disable-line
+  });
+
+  test('should render ADMIN directory entry in "Manage" panel', () => {
+    const directoryEntry = {
+      id: 'index_patterns',
+      title: 'Index Patterns',
+      description: 'Manage the index patterns that help retrieve your data from Elasticsearch.',
+      icon: '/plugins/kibana/assets/app_index_pattern.svg',
+      path: 'index_management_landing_page',
+      showOnHomePage: true,
+      category: FeatureCatalogueCategory.ADMIN
+    };
+
+    const component = shallow(<Home
+      addBasePath={addBasePath}
+      directories={[directoryEntry]}
+      isCloudEnabled={true}
+      recentlyAccessed={[]}
+    />);
+    expect(component).toMatchSnapshot(); // eslint-disable-line
+  });
+
+  test('should not render directory entry when showOnHomePage is false', () => {
+    const directoryEntry = {
+      id: 'management',
+      title: 'Management',
+      description: 'Your center console for managing the Elastic Stack.',
+      icon: '/plugins/kibana/assets/app_management.svg',
+      path: 'management_landing_page',
+      showOnHomePage: false,
+      category: FeatureCatalogueCategory.ADMIN
+    };
+
+    const component = shallow(<Home
+      addBasePath={addBasePath}
+      directories={[directoryEntry]}
+      isCloudEnabled={true}
+      recentlyAccessed={[]}
+    />);
+    expect(component).toMatchSnapshot(); // eslint-disable-line
+  });
 });
