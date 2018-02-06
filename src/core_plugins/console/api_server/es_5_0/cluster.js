4097f2389842faabc7ea8e91d21ada505b5106ff
Jonathan Budzenski <jon@jbudz.me>
2018-02-06 11:12:06 -0600

    cluster.state.json

--- src/core_plugins/console/api_server/es_5_0/cluster.js
+++ src/core_plugins/console/api_server/es_5_0/cluster.js
@@ -1,12 +1,3 @@
 export default function (api) {
-  api.addEndpointDescription('_cluster/state', {
-    patterns: [
-      "_cluster/state",
-      "_cluster/state/{metrics}",
-      "_cluster/state/{metrics}/{indices}"
-    ],
-    url_components: {
-      "metrics": ["version", "master_node", "nodes", "routing_table", "routing_node", "metadata", "blocks"]
-    }
-  });
+
 }
