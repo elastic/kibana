e46e67697b93769b018b98e7e6fa81035a3f63f5
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-02-08 00:50:38 +0100

    mock linkProjectExecutables in bootstrap tests

--- packages/kbn-build/src/commands/bootstrap.test.js
+++ packages/kbn-build/src/commands/bootstrap.test.js
@@ -2,6 +2,9 @@ jest.mock('../utils/scripts', () => ({
   installInDir: jest.fn(),
   runScriptInPackageStreaming: jest.fn(),
 }));
+jest.mock('../utils/link_project_executables', () => ({
+  linkProjectExecutables: jest.fn(),
+}));
 
 import { resolve } from 'path';
 
@@ -13,6 +16,7 @@ import { run } from './bootstrap';
 import { Project } from '../utils/project';
 import { buildProjectGraph } from '../utils/projects';
 import { installInDir, runScriptInPackageStreaming } from '../utils/scripts';
+import { linkProjectExecutables } from '../utils/link_project_executables';
 
 const createProject = (fields, path = '.') =>
   new Project(
@@ -133,7 +137,7 @@ test('handles "frozen-lockfile"', async () => {
   spy.mockRestore();
 });
 
-test('calls "kbn:bootstrap" script after installing deps, if it exists', async () => {
+test('calls "kbn:bootstrap" scripts and links executables after installing deps', async () => {
   const kibana = createProject({
     dependencies: {
       bar: 'link:packages/bar',
@@ -158,6 +162,7 @@ test('calls "kbn:bootstrap" script after installing deps, if it exists', async (
     options: {},
   });
 
+  expect(linkProjectExecutables.mock.calls).toMatchSnapshot('link bins');
   expect(runScriptInPackageStreaming.mock.calls).toMatchSnapshot('script');
 
   spy.mockRestore();
