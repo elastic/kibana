0247b338781fbb74794178b148d7bba8c63a38fb
spalger <spalger@users.noreply.github.com>
2018-02-07 15:39:53 -0700

    [plugin-helpers] replace last few uses of old package name

--- packages/kbn-plugin-helpers/lib/index.js
+++ packages/kbn-plugin-helpers/lib/index.js
@@ -3,8 +3,8 @@ const utils = require('./utils');
 
 module.exports = function () {
   console.error(
-    'running tasks with the default export of @elastic/plugin-helpers is deprecated.' +
-    'use `require(\'@elastic/plugin-helpers\').run()` instead'
+    'running tasks with the default export of @kbn/plugin-helpers is deprecated.' +
+    'use `require(\'@kbn/plugin-helpers\').run()` instead'
   );
 
   return run.apply(this, arguments);
