bca7217d43ae17bbfd6d076705ea1e3869e84ec4
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-02-07 23:39:55 +0100

    Clarify lockfile is used for resolution

--- tasks/build/install_dependencies.js
+++ tasks/build/install_dependencies.js
@@ -9,7 +9,8 @@ module.exports = function (grunt) {
     // We're using `pure-lockfile` instead of `frozen-lockfile` because we
     // rewrite `link:` dependencies to `file:` dependencies earlier in the
     // build. This means the lockfile won't be consistent, so instead of
-    // verifying it, we just skip writing a new lockfile.
+    // verifying it, we just skip writing a new lockfile. However, this does
+    // still use the existing lockfile for dependency resolution.
     exec(`${yarn} --production --ignore-optional --pure-lockfile`, {
       cwd: grunt.config.process('<%= root %>/build/kibana')
     }, this.async());
