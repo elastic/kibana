8787aeafba908522bfeba24500e686602dcd4703
spalger <spalger@users.noreply.github.com>
2018-02-08 14:37:48 -0700

    Revert "Move kibana-plugin-helpers to kbn-plugin-helpers package (#16565)"
    This reverts commit 5ecdaa7e7b13dce0c824ded1a26abbdfc5d89295.

--- packages/kbn-plugin-helpers/bin/plugin-helpers.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const nodeMajorVersion = parseFloat(process.version.replace(/^v(\d+)\..+/, '$1'));
-if (nodeMajorVersion < 6) {
-  console.error('FATAL: kibana-plugin-helpers requires node 6+');
-  process.exit(1);
-}
-
-require('../cli');
--- packages/kbn-plugin-helpers/tasks/test/browser/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./test_browser_action');
