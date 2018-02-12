5b377cafb85b3d6a06d09a42e3e9a26f2ffefab2
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-02-12 13:54:06 +0100

    clarification

--- packages/kbn-build/src/production/prepare_project_dependencies.test.js
+++ packages/kbn-build/src/production/prepare_project_dependencies.test.js
@@ -7,6 +7,8 @@ const packagesFixtures = resolve(__dirname, '__fixtures__/external_packages');
 test('does nothing when Kibana `link:` dependencies', async () => {
   const projectPath = join(packagesFixtures, 'with_kibana_link_deps');
 
+  // We're checking for undefined, but we don't really care about what's
+  // returned, we only care about it resolving.
   await expect(
     prepareExternalProjectDependencies(projectPath)
   ).resolves.toBeUndefined();
