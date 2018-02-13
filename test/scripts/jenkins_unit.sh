f436601da35bdc5370d1dfa00b401acb4f9b7654
CJ Cenizal <cj@cenizal.com>
2018-02-13 10:34:47 -0800

    Add test-subj-selector module to packages dir. (#16623)
    * Add test-subj-selector module to packages dir.
    * Rename to @kbn/test-subj-selector.
    * Simplify rejectRejFiles Grunt task and ignore nested node_modules.
    * Update jenkins_unit script to use yarn kbn CLI.

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -3,8 +3,6 @@
 set -e
 source "$(dirname $0)/../../src/dev/ci_setup/setup.sh"
 
-cd packages/kbn-plugin-helpers
-yarn test
-cd -
+yarn kbn run test --skip-kibana --skip-kibana-extra
 
 xvfb-run "$(yarn bin)/grunt" jenkins:unit;
