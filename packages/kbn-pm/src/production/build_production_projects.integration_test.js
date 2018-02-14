033123c482cf7252281ead66c8520d2f7e2e8136
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-02-14 18:07:04 +0100

    Use jest timeout as the 3rd `it` parameter.

--- packages/kbn-pm/src/production/build_production_projects.integration_test.js
+++ packages/kbn-pm/src/production/build_production_projects.integration_test.js
@@ -7,36 +7,38 @@ import { buildProductionProjects } from './build_production_projects';
 import { getProjects } from '../utils/projects';
 
 describe('kbn-pm production', function() {
-  it('builds and copies projects for production', async function() {
-    jest.setTimeout(60 * 1000);
+  it(
+    'builds and copies projects for production',
+    async function() {
+      const tmpDir = tempy.directory();
+      const buildRoot = tempy.directory();
+      const fixturesPath = resolve(__dirname, '__fixtures__');
 
-    const tmpDir = tempy.directory();
-    const buildRoot = tempy.directory();
-    const fixturesPath = resolve(__dirname, '__fixtures__');
-
-    // Copy all the test fixtures into a tmp dir, as we will be mutating them
-    await copy(['**/*'], tmpDir, {
-      cwd: fixturesPath,
-      parents: true,
-      nodir: true,
-      dot: true,
-    });
-
-    const projects = await getProjects(tmpDir, ['./packages/*']);
-
-    for (const project of projects.values()) {
-      // This will both install dependencies and generate `yarn.lock` files
-      await project.installDependencies({
-        extraArgs: ['--silent', '--no-progress'],
+      // Copy all the test fixtures into a tmp dir, as we will be mutating them
+      await copy(['**/*'], tmpDir, {
+        cwd: fixturesPath,
+        parents: true,
+        nodir: true,
+        dot: true,
       });
-    }
 
-    await buildProductionProjects({ kibanaRoot: tmpDir, buildRoot });
+      const projects = await getProjects(tmpDir, ['./packages/*']);
 
-    const files = await globby(['**/*', '!**/node_modules/**'], {
-      cwd: buildRoot,
-    });
+      for (const project of projects.values()) {
+        // This will both install dependencies and generate `yarn.lock` files
+        await project.installDependencies({
+          extraArgs: ['--silent', '--no-progress'],
+        });
+      }
 
-    expect(files.sort()).toMatchSnapshot();
-  });
+      await buildProductionProjects({ kibanaRoot: tmpDir, buildRoot });
+
+      const files = await globby(['**/*', '!**/node_modules/**'], {
+        cwd: buildRoot,
+      });
+
+      expect(files.sort()).toMatchSnapshot();
+    },
+    60 * 1000
+  );
 });
