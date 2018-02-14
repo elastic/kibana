6edd2a7a75d5e757a81c4a5ae0cdac43c5ad6f31
Jonathan Budzenski <jon@jbudz.me>
2018-02-14 12:24:50 -0600

    indices.validate_query.json

--- src/core_plugins/console/api_server/es_5_0/indices.js
+++ src/core_plugins/console/api_server/es_5_0/indices.js
@@ -11,23 +11,6 @@ export default function (api) {
     }
   });
 
-  api.addEndpointDescription('_validate_query', {
-    methods: ['GET', 'POST'],
-    patterns: [
-      "{indices}/_validate/query",
-      "_validate/query"
-    ],
-    url_params: {
-      explain: "__flag__",
-      rewrite: "__flag__"
-    },
-    data_autocomplete_rules: {
-      query: {
-        // populated by a global rule
-      }
-    }
-  });
-
   api.addEndpointDescription('__create_index__', {
     methods: ['PUT'],
     patterns: [
