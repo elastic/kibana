a152d481e8a2f9444fa510471f38a57687de7781
Jonathan Budzenski <jon@jbudz.me>
2018-02-14 12:21:23 -0600

    update_aliases.json

--- src/core_plugins/console/api_server/es_5_0/aliases.js
+++ src/core_plugins/console/api_server/es_5_0/aliases.js
@@ -1,33 +1,4 @@
 export default function (api) {
-  api.addEndpointDescription('_post_aliases', {
-    methods: ['POST'],
-    patterns: [
-      "_aliases",
-    ],
-    data_autocomplete_rules: {
-      'actions': {
-        __template: [
-          { 'add': { 'index': 'test1', 'alias': 'alias1' } }
-        ],
-        __any_of: [
-          {
-            add: {
-              index: '{index}',
-              alias: '',
-              filter: {},
-              routing: '1',
-              search_routing: '1,2',
-              index_routing: '1'
-            },
-            remove: {
-              index: '',
-              alias: ''
-            }
-          }
-        ]
-      }
-    }
-  });
   api.addEndpointDescription('_get_aliases', {
     methods: ['GET'],
     patterns: [
