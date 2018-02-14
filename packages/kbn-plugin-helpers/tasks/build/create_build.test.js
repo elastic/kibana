917295980baebc99ba0a6b384e806bd45043d7ea
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-02-14 11:28:44 +0100

    Remove outer 'describe'

--- packages/kbn-plugin-helpers/tasks/build/create_build.test.js
+++ packages/kbn-plugin-helpers/tasks/build/create_build.test.js
@@ -1,4 +1,3 @@
-/*eslint-env jest*/
 const { resolve } = require('path');
 const del = require('del');
 const createBuild = require('./create_build');
@@ -8,38 +7,36 @@ const PLUGIN = require('../../lib/plugin_config')(PLUGIN_FIXTURE);
 const PLUGIN_BUILD_DIR = resolve(PLUGIN_FIXTURE, 'build');
 const PLUGIN_BUILD_TARGET = resolve(PLUGIN_BUILD_DIR, 'kibana', PLUGIN.id);
 
-describe('create_build', () => {
-  beforeEach(() => del(PLUGIN_BUILD_DIR));
-  afterEach(() => del(PLUGIN_BUILD_DIR));
+beforeEach(() => del(PLUGIN_BUILD_DIR));
+afterEach(() => del(PLUGIN_BUILD_DIR));
 
-  describe('creating the build', function () {
-    const buildTarget = resolve(PLUGIN.root, 'build');
-    const buildVersion = PLUGIN.version;
-    const kibanaVersion = PLUGIN.version;
-    const buildFiles = PLUGIN.buildSourcePatterns;
+describe('creating the build', function () {
+  const buildTarget = resolve(PLUGIN.root, 'build');
+  const buildVersion = PLUGIN.version;
+  const kibanaVersion = PLUGIN.version;
+  const buildFiles = PLUGIN.buildSourcePatterns;
 
-    it('removes development properties from package.json', function () {
-      expect(PLUGIN.pkg.scripts).not.toBeUndefined();
-      expect(PLUGIN.pkg.devDependencies).not.toBeUndefined();
+  it('removes development properties from package.json', function () {
+    expect(PLUGIN.pkg.scripts).not.toBeUndefined();
+    expect(PLUGIN.pkg.devDependencies).not.toBeUndefined();
 
-      return createBuild(PLUGIN, buildTarget, buildVersion, kibanaVersion, buildFiles)
-        .then(() => {
-          const pkg = require(resolve(PLUGIN_BUILD_TARGET, 'package.json'));
-          expect(pkg.scripts).toBeUndefined();
-          expect(pkg.devDependencies).toBeUndefined();
-        });
-    });
+    return createBuild(PLUGIN, buildTarget, buildVersion, kibanaVersion, buildFiles)
+      .then(() => {
+        const pkg = require(resolve(PLUGIN_BUILD_TARGET, 'package.json'));
+        expect(pkg.scripts).toBeUndefined();
+        expect(pkg.devDependencies).toBeUndefined();
+      });
+  });
 
-    it('adds build metadata to package.json', function () {
-      expect(PLUGIN.pkg.build).toBeUndefined();
+  it('adds build metadata to package.json', function () {
+    expect(PLUGIN.pkg.build).toBeUndefined();
 
-      return createBuild(PLUGIN, buildTarget, buildVersion, kibanaVersion, buildFiles)
-        .then(() => {
-          const pkg = require(resolve(PLUGIN_BUILD_TARGET, 'package.json'));
-          expect(pkg.build).not.toBeUndefined();
-          expect(pkg.build.git).not.toBeUndefined();
-          expect(pkg.build.date).not.toBeUndefined();
-        });
-    });
+    return createBuild(PLUGIN, buildTarget, buildVersion, kibanaVersion, buildFiles)
+      .then(() => {
+        const pkg = require(resolve(PLUGIN_BUILD_TARGET, 'package.json'));
+        expect(pkg.build).not.toBeUndefined();
+        expect(pkg.build.git).not.toBeUndefined();
+        expect(pkg.build.date).not.toBeUndefined();
+      });
   });
 });
--- packages/kbn-plugin-helpers/tasks/build/create_package.test.js
+++ packages/kbn-plugin-helpers/tasks/build/create_package.test.js
@@ -1,4 +1,3 @@
-/*eslint-env jest*/
 const { resolve } = require('path');
 const { statSync } = require('fs');
 const del = require('del');
@@ -9,24 +8,22 @@ const PLUGIN_FIXTURE = resolve(__dirname, '__fixtures__/create_package_test_plug
 const PLUGIN = require('../../lib/plugin_config')(PLUGIN_FIXTURE);
 const PLUGIN_BUILD_DIR = resolve(PLUGIN_FIXTURE, 'build-custom');
 
-describe('create_build', () => {
-  const buildVersion = PLUGIN.version;
-  const kibanaVersion = PLUGIN.version;
-  const buildFiles = PLUGIN.buildSourcePatterns;
-  const packageFile = `${PLUGIN.id}-${buildVersion}.zip`;
-  const doBuild = () => createBuild(PLUGIN, PLUGIN_BUILD_DIR, buildVersion, kibanaVersion, buildFiles);
+const buildVersion = PLUGIN.version;
+const kibanaVersion = PLUGIN.version;
+const buildFiles = PLUGIN.buildSourcePatterns;
+const packageFile = `${PLUGIN.id}-${buildVersion}.zip`;
+const doBuild = () => createBuild(PLUGIN, PLUGIN_BUILD_DIR, buildVersion, kibanaVersion, buildFiles);
 
-  beforeAll(() => del(PLUGIN_BUILD_DIR).then(doBuild));
-  afterAll(() => del(PLUGIN_BUILD_DIR));
+beforeAll(() => del(PLUGIN_BUILD_DIR).then(doBuild));
+afterAll(() => del(PLUGIN_BUILD_DIR));
 
-  describe('creating the package', function () {
-    it('creates zip file in build target path', function () {
-      return createPackage(PLUGIN, PLUGIN_BUILD_DIR, buildVersion)
-        .then(() => {
-          const zipFile = resolve(PLUGIN_BUILD_DIR, packageFile);
-          const stats = statSync(zipFile);
-          expect(stats.isFile()).toBeTruthy();
-        });
-    });
+describe('creating the package', function () {
+  it('creates zip file in build target path', function () {
+    return createPackage(PLUGIN, PLUGIN_BUILD_DIR, buildVersion)
+      .then(() => {
+        const zipFile = resolve(PLUGIN_BUILD_DIR, packageFile);
+        const stats = statSync(zipFile);
+        expect(stats.isFile()).toBeTruthy();
+      });
   });
 });
