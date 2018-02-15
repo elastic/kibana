932819f4c921f990c270b016217ffefe784ee74b
Jonathan Budzenski <jon@jbudz.me>
2018-02-15 16:01:52 -0600

    snapshot.restore.json

--- src/core_plugins/console/api_server/es_5_0/snapshot_restore.js
+++ src/core_plugins/console/api_server/es_5_0/snapshot_restore.js
@@ -1,21 +1,4 @@
 export default function (api) {
-  api.addEndpointDescription('restore_snapshot', {
-    methods: ['POST'],
-    patterns: [
-      '_snapshot/{id}/{id}/_restore'
-    ],
-    url_params: {
-      wait_for_completion: "__flag__"
-    },
-    data_autocomplete_rules: {
-      indices: "*",
-      ignore_unavailable: { __one_of: [true, false] },
-      include_global_state: false,
-      rename_pattern: "index_(.+)",
-      rename_replacement: "restored_index_$1"
-    }
-  });
-
   api.addEndpointDescription('_snapshot_status', {
     methods: ['GET'],
     patterns: [
