612ac1a1df4c61f646c3598e73e17d248f837d8e
Jonathan Budzenski <jon@jbudz.me>
2018-02-15 16:02:34 -0600

    snapshot.status.json

--- src/core_plugins/console/api_server/es_5_0/snapshot_restore.js
+++ src/core_plugins/console/api_server/es_5_0/snapshot_restore.js
@@ -1,10 +1,3 @@
-export default function (api) {
-  api.addEndpointDescription('_snapshot_status', {
-    methods: ['GET'],
-    patterns: [
-      '_snapshot/_status',
-      '_snapshot/{id}/_status',
-      '_snapshot/{id}/{ids}/_status'
-    ]
-  });
+export default function () {
+
 }
