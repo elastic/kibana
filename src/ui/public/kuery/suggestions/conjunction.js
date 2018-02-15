bd409db3dec0fffeac270932edcc458596ed8baf
Lukas Olson <olson.lukas@gmail.com>
2018-02-15 16:31:35 -0700

    Update conjunctions

--- src/ui/public/kuery/suggestions/conjunction.js
+++ src/ui/public/kuery/suggestions/conjunction.js
@@ -1,21 +1,18 @@
 const type = 'conjunction';
 
-const conjunctions = ['and', 'or'];
+const conjunctions = {
+  and: `<p>Requires that <span class="suggestionItem__callout">both arguments</span> joined together both return true</p>`,
+  or: `<p>Requires <span class="suggestionItem__callout">only one of</span> the arguments joined together must return true</p>`
+};
 
 function getDescription(conjunction) {
-  if (conjunction === 'and') {
-    return `<p>Requires that <span class="suggestionItem__callout">both arguments</span> joined together both return true</p>`;
-  } else if (conjunction === 'or') {
-    return `<p>Requires <span class="suggestionItem__callout">only one of</span> the arguments joined together must return true</p>`;
-  } else {
-    return `<p>Add an ${conjunction.toUpperCase()} clause</p>`;
-  }
+  return conjunctions[conjunction];
 }
 
 export function getSuggestionsProvider() {
   return function getConjunctionSuggestions({ prefix, start }) {
     if (!prefix.endsWith(' ')) return [];
-    const suggestions = conjunctions.map(conjunction => {
+    const suggestions = Object.keys(conjunctions).map(conjunction => {
       const text = conjunction + ' ';
       const description = getDescription(conjunction);
       return {
