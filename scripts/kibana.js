fdc6dd76669629e6af124f62d57a738d7b352b01
Raghavendra Vedula <vedularaghavendra03@gmail.com>
2018-02-19 16:30:57 +0530

    Add node js version warning message

--- scripts/kibana.js
+++ scripts/kibana.js
@@ -1,2 +1,7 @@
+if(process.version < 'v6.0.0') {
+  console.error('Kibana does not support Node version ' + process.versions.node + ', please use Node.js 6.0 or higher.');
+  process.exit(1);
+}
+
 require('../src/babel-register');
 require('../src/cli/cli');
