1ee793c633d6ab7e570db4baef35fd87e3b72c5b
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-02-20 23:02:29 +0100

    [6.x] Introduce Jest integration tests. (#16830)

--- scripts/jest_integration.js
+++ scripts/jest_integration.js
@@ -1,17 +1,17 @@
-// # Run Jest tests
+// # Run Jest integration tests
 //
 // All args will be forwarded directly to Jest, e.g. to watch tests run:
 //
-//     node scripts/jest --watch
+//     node scripts/jest_integration --watch
 //
 // or to build code coverage:
 //
-//     node scripts/jest --coverage
+//     node scripts/jest_integration --coverage
 //
 // See all cli options in https://facebook.github.io/jest/docs/cli.html
 
 const { resolve } = require('path');
-process.argv.push('--config', resolve(__dirname, '../src/dev/jest/config.js'));
+process.argv.push('--config', resolve(__dirname, '../src/dev/jest/config.integration.js'));
 
 require('../src/babel-register');
 require('../src/dev/jest/cli');
