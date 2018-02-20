00074e000c7ee7636cb1ec77da1238813e25cea6
Raghavendra Vedula <vedularaghavendra03@gmail.com>
2018-02-20 23:27:57 +0530

    Updated the version check

--- scripts/kibana.js
+++ scripts/kibana.js
@@ -1,4 +1,4 @@
-if(process.version < 'v6.0.0') {
+if((require('../package.json')).engines.node  !== process.version) {
   console.error('Kibana does not support Node version ' + process.versions.node + ', please use Node.js 6.0 or higher.');
   process.exit(1);
 }
