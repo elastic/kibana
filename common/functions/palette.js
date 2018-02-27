eb66a0d196d510ea0a7e7db434712472c1c30e97
Rashid Khan <github.fliplap@spamgourmet.com>
2018-02-26 23:34:54 -0800

    Release 1843

--- common/functions/palette.js
+++ common/functions/palette.js
@@ -14,15 +14,21 @@ export const palette = () => ({
       help: 'Palette colors, rgba, hex, or HTML color string. Pass this multiple times.',
     },
     gradient: {
-      types: ['boolean', 'null'],
+      types: ['boolean'],
       default: false,
       help: 'Prefer to make a gradient where supported and useful?',
     },
+    reverse: {
+      type: ['boolean'],
+      default: false,
+      help: 'Reverse the palette',
+    },
   },
   fn: (context, args) => {
+    const colors = [].concat(args._ || palettes.paul_tor_14.colors);
     return {
       type: 'palette',
-      colors: args._ || palettes.paul_tor_14.colors,
+      colors: args.reverse ? colors.reverse() : colors,
       gradient: args.gradient,
     };
   },
