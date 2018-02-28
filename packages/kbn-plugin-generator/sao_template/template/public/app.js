fe7fc050605ef1beb8e2f1a9b64960e009d3afe0
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-02-28 10:23:58 -0500

    adding whitespace and comments per PR feedback

--- packages/kbn-plugin-generator/sao_template/template/public/app.js
+++ packages/kbn-plugin-generator/sao_template/template/public/app.js
@@ -11,9 +11,11 @@ import template from "./templates/index.html";
 import { Main } from "./components/main";
 
 const app = uiModules.get("apps/<%= name %>");
+
 app.config(stateManagementConfigProvider => {
   stateManagementConfigProvider.disable();
 });
+
 const controller = ($scope, $route, $interval) => {
   const elem = document.getElementById("pluginReactRoot");
   let currentTime = moment($route.current.locals.currentTime);
@@ -22,18 +24,24 @@ const controller = ($scope, $route, $interval) => {
     description: "<%= description %>",
     currentTime: currentTime.format("HH:mm:ss")
   };
+  // Mount the React component tree on the DOM node.
   render(<Main {...props} />, elem);
   const stop = $interval(function() {
     currentTime = currentTime.add(1, "second");
+    // Every time the props change, we need to re-mount the React component tree so that it updates the DOM.
     render(<Main {...props} currentTime={currentTime.format("HH:mm:ss")} />, elem);
   }, 1000);
+
   $scope.$watch("$destroy", () => {
     $interval.cancel(stop);
+    // To clean up after ourselves, we need to tell React to unmount from the DOM node.
     elem && unmountComponentAtNode(elem);
   });
 };
+
 const controllerName = "<%= camelCase(name) %>HelloWorld";
 app.controller(controllerName, controller);
+
 uiRoutes.enable();
 uiRoutes.when("/", {
   template,
