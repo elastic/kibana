11736ff09f0850655e220b06545763fc4414d3cf
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-03-01 21:07:08 -0800

    remove debug

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -5,4 +5,4 @@ source "$(dirname $0)/../../src/dev/ci_setup/setup.sh"
 
 yarn kbn run test --skip-kibana --skip-kibana-extra
 
-xvfb-run "$(yarn bin)/grunt" jenkins:unit --debug --stack;
+xvfb-run "$(yarn bin)/grunt" jenkins:unit;
