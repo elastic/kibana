3083fad96f9630b3648858cac2ae8b97901489a3
Rashid Khan <github.fliplap@spamgourmet.com>
2018-03-04 08:09:47 -0800

    better description

--- public/elements/dropdown_filter/index.js
+++ public/elements/dropdown_filter/index.js
@@ -3,8 +3,7 @@ import header from './header.png';
 export const dropdownFilter = () => ({
   name: 'dropdown_filter',
   displayName: 'Dropdown Filter',
-  help:
-    'Extract values from a datatable and populate a dropdown for use in filtering rows for a column with the selected value',
+  help: 'A dropdown from which you can select values for an "exactly" filter',
   image: header,
   expression: 'demodata | dropdownControl valueColumn=project filterColumn=project',
   filter: '',
