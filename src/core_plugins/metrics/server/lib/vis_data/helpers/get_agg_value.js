12ae737ce3a7a44b492e87879d8f088bacdf04f6
Chris Cowan <chris@chriscowan.us>
2018-03-08 14:49:26 -0700

    [TSVB] Add Top Hits Aggregation (#17047)
    * [TSVB] Adding Top Hits Aggregation
    
    * Fixing tests

--- src/core_plugins/metrics/server/lib/vis_data/helpers/get_agg_value.js
+++ src/core_plugins/metrics/server/lib/vis_data/helpers/get_agg_value.js
@@ -1,14 +1,22 @@
-import _ from 'lodash';
+import { get, includes, max, min, sum } from 'lodash';
 import extendStatsTypes from './extended_stats_types';
+
+const aggFns = {
+  max,
+  min,
+  sum,
+  avg: values => sum(values) / values.length,
+};
+
 export default (row, metric) => {
   // Extended Stats
-  if (_.includes(extendStatsTypes, metric.type)) {
+  if (includes(extendStatsTypes, metric.type)) {
     const isStdDeviation = /^std_deviation/.test(metric.type);
     const modeIsBounds = ~['upper', 'lower'].indexOf(metric.mode);
     if (isStdDeviation && modeIsBounds) {
-      return _.get(row, `${metric.id}.std_deviation_bounds.${metric.mode}`);
+      return get(row, `${metric.id}.std_deviation_bounds.${metric.mode}`);
     }
-    return _.get(row, `${metric.id}.${metric.type}`);
+    return get(row, `${metric.id}.${metric.type}`);
   }
 
   // Percentiles
@@ -22,14 +30,27 @@ export default (row, metric) => {
 
   if (metric.type === 'percentile_rank') {
     const percentileRankKey = `${metric.value}`;
-    return row[metric.id] && row[metric.id].values && row[metric.id].values[percentileRankKey];
+    return (
+      row[metric.id] &&
+      row[metric.id].values &&
+      row[metric.id].values[percentileRankKey]
+    );
+  }
+
+  if (metric.type === 'top_hit') {
+    if (row[metric.id].doc_count === 0) return null;
+    const hits = get(row, [metric.id, 'docs', 'hits', 'hits'], []);
+    const values = hits.map(doc => {
+      return get(doc, `_source.${metric.field}`, 0);
+    });
+    const aggWith = (metric.agg_with && aggFns[metric.agg_with]) || aggFns.avg;
+    return aggWith(values);
   }
 
   // Derivatives
-  const normalizedValue = _.get(row, `${metric.id}.normalized_value`, null);
+  const normalizedValue = get(row, `${metric.id}.normalized_value`, null);
 
   // Everything else
-  const value = _.get(row, `${metric.id}.value`, null);
+  const value = get(row, `${metric.id}.value`, null);
   return normalizedValue || value;
-
 };
