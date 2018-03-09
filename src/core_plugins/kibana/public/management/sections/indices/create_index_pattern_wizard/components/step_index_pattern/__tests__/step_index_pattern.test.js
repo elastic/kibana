f8bdc3954c2fad4ebcb2a22add6aa3d4ba1bb8d5
CJ Cenizal <cj@cenizal.com>
2018-03-08 20:23:48 -0800

    Update tests.

--- src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/components/step_index_pattern/__tests__/step_index_pattern.test.js
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/components/step_index_pattern/__tests__/step_index_pattern.test.js
@@ -34,61 +34,59 @@ const createComponent = props => {
 };
 
 describe('StepIndexPattern', () => {
-  describe('step 1', () => {
-    it('renders the loading state', () => {
-      const component = createComponent();
-      component.setState({ isLoadingIndices: true });
-      expect(component.find('[data-test-subj="createIndexPatternStep1Loading"]')).toMatchSnapshot();
-    });
+  it('renders the loading state', () => {
+    const component = createComponent();
+    component.setState({ isLoadingIndices: true });
+    expect(component.find('[data-test-subj="createIndexPatternStep1Loading"]')).toMatchSnapshot();
+  });
 
-    it('renders indices which match the initial query', async () => {
-      const component = createComponent({ initialQuery: 'kibana' });
+  it('renders indices which match the initial query', async () => {
+    const component = createComponent({ initialQuery: 'kibana' });
 
-      // Ensure all promises resolve
-      await new Promise(resolve => process.nextTick(resolve));
-      // Ensure the state changes are reflected
-      await component.update();
+    // Ensure all promises resolve
+    await new Promise(resolve => process.nextTick(resolve));
+    // Ensure the state changes are reflected
+    await component.update();
 
-      expect(component.find('[data-test-subj="createIndexPatternStep1IndicesList"]')).toMatchSnapshot();
-    });
+    expect(component.find('[data-test-subj="createIndexPatternStep1IndicesList"]')).toMatchSnapshot();
+  });
 
-    it('renders errors when input is invalid', async () => {
-      const component = createComponent();
-      const instance = component.instance();
-      instance.onQueryChanged({ target: { value: '?' } });
+  it('renders errors when input is invalid', async () => {
+    const component = createComponent();
+    const instance = component.instance();
+    instance.onQueryChanged({ target: { value: '?' } });
 
-      // Ensure all promises resolve
-      await new Promise(resolve => process.nextTick(resolve));
-      // Ensure the state changes are reflected
-      component.update();
+    // Ensure all promises resolve
+    await new Promise(resolve => process.nextTick(resolve));
+    // Ensure the state changes are reflected
+    component.update();
 
-      expect(component.find('[data-test-subj="createIndexPatternStep1Header"]')).toMatchSnapshot();
-    });
+    expect(component.find('[data-test-subj="createIndexPatternStep1Header"]')).toMatchSnapshot();
+  });
 
-    it('renders matching indices when input is valid', async () => {
-      const component = createComponent();
-      const instance = component.instance();
-      instance.onQueryChanged({ target: { value: 'k' } });
+  it('renders matching indices when input is valid', async () => {
+    const component = createComponent();
+    const instance = component.instance();
+    instance.onQueryChanged({ target: { value: 'k' } });
 
-      // Ensure all promises resolve
-      await new Promise(resolve => process.nextTick(resolve));
-      // Ensure the state changes are reflected
-      component.update();
+    // Ensure all promises resolve
+    await new Promise(resolve => process.nextTick(resolve));
+    // Ensure the state changes are reflected
+    component.update();
 
-      expect(component.find('[data-test-subj="createIndexPatternStep1IndicesList"]')).toMatchSnapshot();
-    });
+    expect(component.find('[data-test-subj="createIndexPatternStep1IndicesList"]')).toMatchSnapshot();
+  });
 
-    it('appends a wildcard automatically to queries', async () => {
-      const component = createComponent();
-      const instance = component.instance();
-      instance.onQueryChanged({ target: { value: 'k' } });
-      expect(component.state('query')).toBe('k*');
-    });
+  it('appends a wildcard automatically to queries', async () => {
+    const component = createComponent();
+    const instance = component.instance();
+    instance.onQueryChanged({ target: { value: 'k' } });
+    expect(component.state('query')).toBe('k*');
+  });
 
-    it('disables step 2 if the index pattern exists', async () => {
-      const component = createComponent();
-      component.setState({ indexPatternExists: true });
-      expect(component.find('Header').prop('isNextStepDisabled')).toBe(true);
-    });
+  it('disables the next step if the index pattern exists', async () => {
+    const component = createComponent();
+    component.setState({ indexPatternExists: true });
+    expect(component.find('Header').prop('isNextStepDisabled')).toBe(true);
   });
 });
