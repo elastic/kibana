f2a0832e13ceaf5c2292e96abe3de2961bb4fc21
Lukas Olson <olson.lukas@gmail.com>
2018-03-12 12:58:29 -0700

    Simplify and improve match pairs

--- src/ui/public/directives/match_pairs.js
+++ src/ui/public/directives/match_pairs.js
@@ -2,18 +2,16 @@ import { uiModules } from 'ui/modules';
 const module = uiModules.get('kibana');
 
 /**
- * This directively automatically inserts matching pairs.
+ * This directively automatically handles matching pairs.
  * Specifically, it does the following:
  *
- * 1. If we don't have a multi-character selection, and the key is a closer,
- *    and the character in front of the cursor is the same, simply move the
- *    cursor forward.
+ * 1. If the key is a closer, and the character in front of the cursor is the
+ *    same, simply move the cursor forward.
  * 2. If the key is an opener, insert the opener at the beginning of the
  *    selection, and the closer at the end of the selection, and move the
  *    selection forward.
- * 3. If we don't have a multi-character selection, and the backspace is hit,
- *    and the characters before and after the cursor correspond to a pair,
- *    remove both characters and move the cursor backward.
+ * 3. If the backspace is hit, and the characters before and after the cursor
+ *    are a pair, remove both characters and move the cursor backward.
  */
 
 const pairs = ['()', '[]', '{}', `''`, '""'];
@@ -25,21 +23,59 @@ module.directive('matchPairs', () => ({
   require: 'ngModel',
   link: function (scope, elem, attrs, ngModel) {
     elem.on('keydown', (e) => {
-      const { target, key } = e;
+      const { target, key, metaKey } = e;
       const { value, selectionStart, selectionEnd } = target;
-      if (selectionStart === selectionEnd && closers.includes(key) && value.charAt(selectionEnd) === key) {
-        // 1. (See above)
+
+      if (shouldMoveSelectionForward()) {
+        moveSelectionForward();
+      } else if (shouldInsertMatchingCloser()) {
+        insertMatchingCloser();
+      } else if (shouldRemovePair()) {
+        removePair();
+      }
+
+      function shouldMoveSelectionForward() {
+        if (!closers.includes(key)) return false;
+
+        // Never move selection forward for multi-character selections
+        if (selectionStart !== selectionEnd) return false;
+
+        // Move selection foward if the key is the same as the closer in front of the selection
+        return value.charAt(selectionEnd) === key;
+      }
+
+      function shouldInsertMatchingCloser() {
+        if (!openers.includes(key)) return false;
+
+        // Always insert for multi-character selections
+        if (selectionStart !== selectionEnd) return true;
+
+        const precedingCharacter = value.charAt(selectionStart - 1);
+        const followingCharacter = value.charAt(selectionStart + 1);
+
+        // Don't insert if the preceding character is a backslash
+        if (precedingCharacter === '\\') return false;
+
+        // Don't insert if it's a quote and the either of the preceding/following characters is alphanumeric
+        return !(['"', `'`].includes(key) && (isAlphanumeric(precedingCharacter) || isAlphanumeric(followingCharacter)));
+      }
+
+      function shouldRemovePair() {
+        if (key !== 'Backspace' || metaKey) return false;
+
+        // Never remove for multi-character selections
+        if (selectionStart !== selectionEnd) return false;
+
+        // Remove if the preceding/following characters are a pair
+        return pairs.includes(value.substr(selectionEnd - 1, 2));
+      }
+
+      function moveSelectionForward() {
         e.preventDefault();
         target.setSelectionRange(selectionStart + 1, selectionEnd + 1);
-      } else if (
-        openers.includes(key) && (
-          // Don't match quotes following alphanumeric characters when there isn't a selection
-          !['"', `'`].includes(key) ||
-          selectionStart !== selectionEnd ||
-          !(value.charAt(selectionStart - 1) || '').match(/[a-zA-Z0-9_]/)
-        )
-      ) {
-        // 2. (See above)
+      }
+
+      function insertMatchingCloser() {
         e.preventDefault();
         ngModel.$setViewValue(
           value.substr(0, selectionStart) + key +
@@ -48,12 +84,9 @@ module.directive('matchPairs', () => ({
         );
         ngModel.$render();
         target.setSelectionRange(selectionStart + 1, selectionEnd + 1);
-      } else if (
-        selectionStart === selectionEnd &&
-        key === 'Backspace' &&
-        !e.metaKey && pairs.includes(value.substr(selectionEnd - 1, 2))
-      ) {
-        // 3. (See above)
+      }
+
+      function removePair() {
         e.preventDefault();
         ngModel.$setViewValue(value.substr(0, selectionEnd - 1) + value.substr(selectionEnd + 1));
         ngModel.$render();
@@ -62,3 +95,7 @@ module.directive('matchPairs', () => ({
     });
   }
 }));
+
+function isAlphanumeric(value = '') {
+  return value.match(/[a-zA-Z0-9_]/);
+}
