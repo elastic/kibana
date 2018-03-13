d49d59ece6a68e7560a8f27456a3369b4bea6328
Nathan Reese <reese.nathan@gmail.com>
2018-03-13 10:29:14 -0600

    [Home] APM tutorial - show launch app button when APM application is running (#16818)
    * add launch application to footer, update APM tutorial with applicaton artifacts when APM xpack is enabled
    
    * remove launch and dashboard logic from footer to external component
    
    * return null when url not provided to footer
    
    * make url and label props required
    
    * remove eslint comment
    
    * change dashboard linkLabel

--- src/core_plugins/kibana/server/tutorials/apm/index.js
+++ src/core_plugins/kibana/server/tutorials/apm/index.js
@@ -3,7 +3,33 @@ import { ON_PREM_INSTRUCTIONS } from './on_prem';
 
 const apmIntro = 'Collect in-depth performance metrics and errors from inside your applications.';
 
-export function apmSpecProvider() {
+function isEnabled(config, key) {
+  try {
+    return config.get(key);
+  } catch (err) {
+    return false;
+  }
+}
+
+export function apmSpecProvider(server) {
+  const config = server.config();
+
+  const artifacts = {
+    dashboards: [
+      {
+        id: '8d3ed660-7828-11e7-8c47-65b845b5cfb3',
+        linkLabel: 'APM dashboard',
+        isOverview: true
+      }
+    ]
+  };
+  if (isEnabled(config, 'xpack.apm.ui.enabled')) {
+    artifacts.application = {
+      path: '/app/apm',
+      label: 'Launch APM'
+    };
+  }
+
   return {
     id: 'apm',
     name: 'APM',
@@ -14,15 +40,7 @@ export function apmSpecProvider() {
       ' It allows you to monitor the performance of thousands of applications in real time.' +
       ' [Learn more]({config.docs.base_url}guide/en/apm/get-started/{config.docs.version}/index.html).',
     euiIconType: 'apmApp',
-    artifacts: {
-      dashboards: [
-        {
-          id: '8d3ed660-7828-11e7-8c47-65b845b5cfb3',
-          linkLabel: 'APM Services dashboard',
-          isOverview: true
-        }
-      ]
-    },
+    artifacts: artifacts,
     onPrem: ON_PREM_INSTRUCTIONS,
     previewImagePath: '/plugins/kibana/home/tutorial_resources/apm/apm_dashboard_transactions.jpg',
   };
