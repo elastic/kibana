96a9acd96cafa98f9e88effe0ea16aac42b44bb6
kobelb <brandon.kobel@elastic.co>
2018-03-13 12:24:16 -0400

    Updating getExecArgv for Node 8, debug port command arguments changed

--- src/server/sandbox/spawn_gatekeeper.js
+++ src/server/sandbox/spawn_gatekeeper.js
@@ -1,30 +1,29 @@
 import { fork } from 'child_process';
 import { safeChildProcess } from './safe_child_process';
 
+
+let debugPortOffset = 1;
+
 // When we fork the child_process we need to use a different debug port
 // if we're using `--inspect` et al. The following code is heavily borrowed
 // from the cluster module in NodeJS 6.12.x and when we upgrade to 8.x it
 // will likely need to be updated, as they changed the arguments...
 const getExecArgv = () => {
-  if (!process.version.match(/^v6./)) {
+  if (!process.version.match(/^v8./)) {
     throw new Error('Unable to determine execArgv for the gatekeeper process with the version of NodeJS');
   }
 
-  const debugArgvRE = /^(--inspect|--debug|--debug-(brk|port))(=\d+)?$/;
-  let debugPort = 0;
+  const debugArgRegex = /--inspect(?:-brk|-port)?|--debug-port/;
+
   const execArgv = [...process.execArgv];
 
-  for (let i = 0; i < execArgv.length; i++) {
-    const match = execArgv[i].match(debugArgvRE);
+  if (execArgv.some((arg) => arg.match(debugArgRegex))) {
+    const inspectPort = process.debugPort + debugPortOffset;
+    debugPortOffset++;
 
-    if (match) {
-      if (debugPort === 0) {
-        debugPort = process.debugPort + 100;
-      }
-
-      execArgv[i] = match[1] + '=' + debugPort;
-    }
+    execArgv.push(`--inspect-port=${inspectPort}`);
   }
+
   return execArgv;
 };
 
