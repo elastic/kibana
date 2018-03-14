c88d84f902aabb9ed846bd420b680d4d474e55c6
Chris Roberson <chrisronline@gmail.com>
2018-03-14 15:25:47 -0400

    [Management] Discard results of older queries (#17148)
    * Discard results of older queries
    
    * PR feedback

--- src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/components/step_index_pattern/__tests__/step_index_pattern.test.js
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/components/step_index_pattern/__tests__/step_index_pattern.test.js
@@ -6,7 +6,13 @@ jest.mock('../../../lib/ensure_minimum_time', () => ({
   ensureMinimumTime: async (promises) => Array.isArray(promises) ? await Promise.all(promises) : await promises
 }));
 jest.mock('../../../lib/get_indices', () => ({
-  getIndices: () => {
+  getIndices: (service, query) => {
+    if (query.startsWith('e')) {
+      return [
+        { name: 'es' },
+      ];
+    }
+
     return [
       { name: 'kibana' },
     ];
@@ -88,5 +94,27 @@ describe('StepIndexPattern', () => {
     const component = createComponent();
     component.setState({ indexPatternExists: true });
     expect(component.find('Header').prop('isNextStepDisabled')).toBe(true);
+  });
+
+  it('ensures the response of the latest request is persisted', async () => {
+    const component = createComponent();
+    const instance = component.instance();
+    instance.onQueryChanged({ target: { value: 'e' } });
+    instance.lastQuery = 'k';
+    await new Promise(resolve => process.nextTick(resolve));
+
+    // Honesty, the state would match the result of the `k` query but
+    // it's hard to mock this in tests but if remove our fix
+    // (the early return if the queries do not match) then this
+    // equals [{name: 'es'}]
+    expect(component.state('exactMatchedIndices')).toEqual([]);
+
+    // Ensure it works in the other code flow too (the other early return)
+
+    // Provide `es` so we do not auto append * and enter our other code flow
+    instance.onQueryChanged({ target: { value: 'es' } });
+    instance.lastQuery = 'k';
+    await new Promise(resolve => process.nextTick(resolve));
+    expect(component.state('exactMatchedIndices')).toEqual([]);
   });
 });
