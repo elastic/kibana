e2f279efd69a98855458c5c4b274a64ace906502
Matthew Bargar <mbargar@gmail.com>
2018-03-15 11:35:10 -0400

    Don't error on non-existent fields, use given name instead
    We found out these errors cause problems on dashboards with panels based
    on different index patterns. If a field exists in one index pattern
    and not another each search will throw an error. It also changes the
    filtering behaviour of the panels with the missing field. Unlike lucene
    which will filter out all results that don't have that field, these
    errors will prevent the query for the missing field from being added
    to the request to ES and the docs with the missing field will still show
    up in the results. We could consider changing this behaviour, there are
    valid reasons why someone might want to see those results, but to keep
    things familiar I think we should preserve lucene's behaviour for now.

--- src/ui/public/kuery/functions/utils/get_fields.js
+++ src/ui/public/kuery/functions/utils/get_fields.js
@@ -6,13 +6,19 @@ export function getFields(node, indexPattern) {
     const fieldName = literal.toElasticsearchQuery(node);
     const field = indexPattern.fields.byName[fieldName];
     if (!field) {
-      throw new Error(`Field ${fieldName} does not exist in index pattern ${indexPattern.title}`);
+      return [{
+        name: fieldName,
+        scripted: false,
+      }];
     }
     return [field];
   } else if (node.type === 'wildcard') {
     const fields = indexPattern.fields.filter(field => wildcard.test(node, field.name));
     if (fields.length === 0) {
-      throw new Error(`No fields match the pattern ${wildcard.toElasticsearchQuery(node)} in index pattern ${indexPattern.title}`);
+      return [{
+        name: wildcard.toElasticsearchQuery(node),
+        scripted: false,
+      }];
     }
     return fields;
   }
