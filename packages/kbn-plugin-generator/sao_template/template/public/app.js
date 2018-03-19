5c4351ff705c592e7c026f8307248e7fe96b57e2
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-03-19 10:36:28 -0400

    reducing Angular dependency and fixing issue with hack not loading properly

--- packages/kbn-plugin-generator/sao_template/template/public/app.js
+++ packages/kbn-plugin-generator/sao_template/template/public/app.js
@@ -10,46 +10,26 @@ import "./less/main.less";
 import template from "./templates/index.html";
 import { Main } from "./components/main";
 
-const app = uiModules.get("apps/<%= name %>");
+const app = uiModules.get("apps/<%= kebabCase(name) %>");
 
-app.config(stateManagementConfigProvider => {
-  stateManagementConfigProvider.disable();
-});
-
-const controller = ($scope, $route, $interval) => {
-  const elem = document.getElementById("pluginReactRoot");
-  let currentTime = moment($route.current.locals.currentTime);
-  const props = {
-    title: "<%= startCase(name) %>",
-    description: "<%= description %>",
-    currentTime: currentTime.format("HH:mm:ss")
-  };
-  // Mount the React component tree on the DOM node.
-  render(<Main {...props} />, elem);
-  const stop = $interval(function() {
-    currentTime = currentTime.add(1, "second");
-    // Every time the props change, we need to re-mount the React component tree so that it updates the DOM.
-    render(<Main {...props} currentTime={currentTime.format("HH:mm:ss")} />, elem);
-  }, 1000);
-
-  $scope.$watch("$destroy", () => {
-    $interval.cancel(stop);
-    // To clean up after ourselves, we need to tell React to unmount from the DOM node.
-    elem && unmountComponentAtNode(elem);
+app.config($locationProvider => {
+  $locationProvider.html5Mode({
+    enabled: false,
+    requireBase: false,
+    rewriteLinks: false,
   });
-};
-
-app.controller("<%= camelCase(name) %>HelloWorld", controller);
-
-uiRoutes.enable();
-uiRoutes.when("/", {
-  template,
-  controller,
-  resolve: {
-    currentTime($http) {
-      return $http.get("../api/<%= name %>/example").then(function(resp) {
-        return resp.data.time;
-      });
-    }
-  }
 });
+
+function RootController($scope, $element, $http) {
+  const domNode = $element[0];
+
+  // render react to DOM
+  render(<Main title={"<%= name %>"} httpClient={$http}/>, domNode);
+
+  // unmount react on controller destroy
+  $scope.$on('$destroy', () => {
+    unmountComponentAtNode(domNode);
+  });
+}
+
+chrome.setRootController("<%= kebabCase(name) %>", RootController);
