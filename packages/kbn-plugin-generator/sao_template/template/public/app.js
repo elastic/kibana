8f56425d8604a16134d3cb097a60e43649d7bbac
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-03-19 14:05:57 -0400

    reacting to PR feedback

--- packages/kbn-plugin-generator/sao_template/template/public/app.js
+++ packages/kbn-plugin-generator/sao_template/template/public/app.js
@@ -1,14 +1,11 @@
-import React from "react";
-import moment from "moment";
-import { uiModules } from "ui/modules";
-import chrome from "ui/chrome";
-import uiRoutes from "ui/routes";
-import { render, unmountComponentAtNode } from "react-dom";
+import React from 'react';
+import { uiModules } from 'ui/modules';
+import chrome from 'ui/chrome';
+import { render, unmountComponentAtNode } from 'react-dom';
 
-import "ui/autoload/styles";
-import "./less/main.less";
-import template from "./templates/index.html";
-import { Main } from "./components/main";
+import 'ui/autoload/styles';
+import './less/main.less';
+import { Main } from './components/main';
 
 const app = uiModules.get("apps/<%= camelCase(name) %>");
 
@@ -19,12 +16,15 @@ app.config($locationProvider => {
     rewriteLinks: false,
   });
 });
+app.config(stateManagementConfigProvider =>
+  stateManagementConfigProvider.disable()
+);
 
 function RootController($scope, $element, $http) {
   const domNode = $element[0];
 
   // render react to DOM
-  render(<Main title={"<%= name %>"} httpClient={$http}/>, domNode);
+  render(<Main title="<%= name %>" httpClient={$http} />, domNode);
 
   // unmount react on controller destroy
   $scope.$on('$destroy', () => {
