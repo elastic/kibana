9cb656945247108f52967b18719825a39c09cb54
Nathan Reese <reese.nathan@gmail.com>
2018-03-19 09:17:32 -0600

    Replace angular-bootstrap-colorpicker with EuiColorPicker (#17222)
    * remove angular-bootstrap-colorpicker
    
    * add form-control class to make things like better

--- src/ui/public/field_format_editor/editors/color/color.js
+++ src/ui/public/field_format_editor/editors/color/color.js
@@ -18,6 +18,31 @@ export function colorEditor() {
       $scope.removeColor = function (index) {
         $scope.editor.formatParams.colors.splice(index, 1);
       };
+
+      $scope.setColor = function (index, propName, value) {
+        $scope.editor.formatParams.colors[index][propName] = value;
+      };
+
+      // EuiColorPicker onChange prop expects a function taking a single parameter: 'value'
+      // Updating color in $scope requires other parameters (like colors index).
+      // Use bind to create a function to pass to react.
+      // Must use a single bind and then store the function so react does not infinitely re-render.
+      $scope.getSetBackgroundColor = function (index) {
+        if ($scope.editor.formatParams.colors[index].setBackgroundColor) {
+          return $scope.editor.formatParams.colors[index].setBackgroundColor;
+        }
+
+        $scope.editor.formatParams.colors[index].setBackgroundColor = $scope.setColor.bind(null, index, 'background');
+        return $scope.editor.formatParams.colors[index].setBackgroundColor;
+      };
+      $scope.getSetTextColor = function (index) {
+        if ($scope.editor.formatParams.colors[index].setTextColor) {
+          return $scope.editor.formatParams.colors[index].setTextColor;
+        }
+
+        $scope.editor.formatParams.colors[index].setTextColor = $scope.setColor.bind(null, index, 'text');
+        return $scope.editor.formatParams.colors[index].setTextColor;
+      };
     }
   };
 }
