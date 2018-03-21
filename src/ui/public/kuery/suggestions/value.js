61e75eff0aa1a36095cc51b10251f1b3f981f32b
Lukas Olson <olson.lukas@gmail.com>
2018-03-21 13:02:46 -0700

    Fix value suggestions memoizing

--- src/ui/public/kuery/suggestions/value.js
+++ src/ui/public/kuery/suggestions/value.js
@@ -7,34 +7,35 @@ const type = 'value';
 
 export function getSuggestionsProvider({ $http, config, indexPatterns }) {
   const allFields = flatten(indexPatterns.map(indexPattern => indexPattern.fields.raw));
+  const requestSuggestions = memoize((query, field) => {
+    const queryParams = { query, field: field.name };
+    return $http.post(`${baseUrl}/${field.indexPattern.title}`, queryParams);
+  }, resolver);
   const shouldSuggestValues = config.get('filterEditor:suggestValues');
-  return shouldSuggestValues ? memoize(getValueSuggestions, getFieldQueryHash) : () => [];
 
-  function getValueSuggestions({ start, end, prefix, suffix, fieldName }) {
+  return function getValueSuggestions({ start, end, prefix, suffix, fieldName }) {
     const fields = allFields.filter(field => field.name === fieldName);
     const query = `${prefix}${suffix}`;
 
     const suggestionsByField = fields.map(field => {
       if (field.type === 'boolean') {
-        return getSuggestions(start, end, query, ['true', 'false']);
-      } else if (!field.aggregatable || field.type !== 'string') {
+        return wrapAsSuggestions(start, end, query, ['true', 'false']);
+      } else if (!shouldSuggestValues || !field.aggregatable || field.type !== 'string') {
         return [];
       }
 
-      const queryParams = { query, field: field.name };
-      return $http.post(`${baseUrl}/${field.indexPattern.title}`, queryParams)
-        .then(({ data }) => {
-          const quotedValues = data.map(value => `"${escapeQuotes(value)}"`);
-          return getSuggestions(start, end, query, quotedValues);
-        });
+      return requestSuggestions(query, field).then(({ data }) => {
+        const quotedValues = data.map(value => `"${escapeQuotes(value)}"`);
+        return wrapAsSuggestions(start, end, query, quotedValues);
+      });
     });
 
     return Promise.all(suggestionsByField)
       .then(suggestions => flatten(suggestions));
-  }
+  };
 }
 
-function getSuggestions(start, end, query, values) {
+function wrapAsSuggestions(start, end, query, values) {
   return values
     .filter(value => value.toLowerCase().includes(query.toLowerCase()))
     .map(value => {
@@ -43,6 +44,6 @@ function getSuggestions(start, end, query, values) {
     });
 }
 
-function getFieldQueryHash({ prefix, suffix, fieldName }) {
-  return `${prefix}${suffix}/${fieldName}`;
+function resolver(query, field) {
+  return `${query}/${field.name}`;
 }
