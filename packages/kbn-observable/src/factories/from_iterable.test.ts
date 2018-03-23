9679df6c4e315a938f56c57e1e4844497e8e2d93
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-03-23 12:54:32 +0100

    Improve $fromIterable tests

--- packages/kbn-observable/src/factories/from_iterable.test.ts
+++ packages/kbn-observable/src/factories/from_iterable.test.ts
@@ -25,6 +25,7 @@ test('handles iterable', () => {
   };
 
   const values: number[] = [];
+  const error = jest.fn();
   const complete = jest.fn();
 
   $fromIterable(fooIterable).subscribe({
@@ -35,19 +36,48 @@ test('handles iterable', () => {
   });
 
   expect(values).toEqual([1, 2, 3]);
+  expect(error).not.toHaveBeenCalled();
   expect(complete).toHaveBeenCalledTimes(1);
+});
+
+test('emits error if iterator throws', () => {
+  const err = new Error('foo');
+
+  const fooIterable: Iterable<number> = {
+    [Symbol.iterator]: function*() {
+      yield 1;
+      throw err;
+    },
+  };
+
+  const values: number[] = [];
+  const error = jest.fn();
+  const complete = jest.fn();
+
+  $fromIterable(fooIterable).subscribe({
+    next(x) {
+      values.push(x);
+    },
+    error,
+    complete,
+  });
+
+  expect(values).toEqual([1]);
+  expect(error).toHaveBeenCalledTimes(1);
+  expect(error).toHaveBeenLastCalledWith(err);
+  expect(complete).not.toHaveBeenCalled();
 });
 
 test('throws for "null"', () => {
   expect(() => {
     $fromIterable(null as any);
-  }).toThrow(TypeError);
+  }).toThrowErrorMatchingSnapshot();
 });
 
 test('throws for "undefined"', () => {
   expect(() => {
     $fromIterable(undefined as any);
-  }).toThrow(TypeError);
+  }).toThrowErrorMatchingSnapshot();
 });
 
 test('throws if no argument specified', () => {
