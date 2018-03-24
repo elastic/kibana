71b6c35c38214b0dc8572d94fe000afd7594dd3d
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-03-24 16:07:54 +0100

    Test 'reduce' projection error

--- packages/kbn-observable/src/operators/reduce.test.ts
+++ packages/kbn-observable/src/operators/reduce.test.ts
@@ -40,7 +40,7 @@ test('completes with initial value if no values received', async () => {
   const subject = new Subject<string>();
 
   const observable = subject.pipe(
-    reduce((acc, val, index) => {
+    reduce((acc, val) => {
       return acc + val;
     }, 'foo')
   );
@@ -48,4 +48,25 @@ test('completes with initial value if no values received', async () => {
   subject.complete();
 
   expect(await res).toEqual(['foo', 'C']);
+});
+
+test('errors if projection errors', async () => {
+  const subject = new Subject<string>();
+  const error = new Error('baz');
+
+  const observable = subject.pipe(
+    reduce((acc, val) => {
+      if (val === 'baz') {
+        throw error;
+      }
+      return val;
+    }, 'foo')
+  );
+  const res = collect(observable);
+
+  subject.next('bar');
+  subject.next('baz');
+  subject.complete();
+
+  expect(await res).toEqual([error]);
 });
