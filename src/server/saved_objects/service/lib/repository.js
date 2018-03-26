b9bbd4c98f9da79efc1bc6b0295624a923edbf32
Larry Gregory <larry.gregory@elastic.co>
2018-03-26 10:40:36 -0400

    partial implementation for OLS Phase 1

--- src/server/saved_objects/service/lib/repository.js
+++ src/server/saved_objects/service/lib/repository.js
@@ -37,12 +37,14 @@ export class SavedObjectsRepository {
       mappings,
       callCluster,
       onBeforeWrite = () => {},
+      interceptors = []
     } = options;
 
     this._index = index;
     this._mappings = mappings;
     this._type = getRootType(this._mappings);
     this._onBeforeWrite = onBeforeWrite;
+    this._interceptors = interceptors;
     this._unwrappedCallCluster = callCluster;
   }
 
@@ -66,6 +68,8 @@ export class SavedObjectsRepository {
     const time = this._getCurrentTime();
 
     try {
+      await this._invokeRequestInterceptors(method, type, attributes, options);
+
       const response = await this._writeToCluster(method, {
         id: this._generateEsId(type, id),
         type: this._type,
@@ -108,8 +112,11 @@ export class SavedObjectsRepository {
       overwrite = false
     } = options;
     const time = this._getCurrentTime();
-    const objectToBulkRequest = (object) => {
+
+    const objectToBulkRequest = async (object) => {
       const method = object.id && !overwrite ? 'create' : 'index';
+
+      await this._invokeRequestInterceptors(method, object.type, object.attributes, options);
 
       return [
         {
@@ -126,13 +133,18 @@ export class SavedObjectsRepository {
       ];
     };
 
+    const bulkRequestBody = await objects.reduce(async (acc, object) => {
+      const collection = await acc;
+
+      const objectRequestBody = await objectToBulkRequest(object);
+
+      return [...collection, ...objectRequestBody];
+    }, Promise.resolve([]));
+
     const { items } = await this._writeToCluster('bulk', {
       index: this._index,
       refresh: 'wait_for',
-      body: objects.reduce((acc, object) => ([
-        ...acc,
-        ...objectToBulkRequest(object)
-      ]), []),
+      body: bulkRequestBody
     });
 
     return items.map((response, i) => {
@@ -176,6 +188,8 @@ export class SavedObjectsRepository {
    * @returns {promise}
    */
   async delete(type, id) {
+    await this._invokeRequestInterceptors('delete', type, id);
+
     const response = await this._writeToCluster('delete', {
       id: this._generateEsId(type, id),
       type: this._type,
@@ -233,6 +247,10 @@ export class SavedObjectsRepository {
     if (fields && !Array.isArray(fields)) {
       throw new TypeError('options.searchFields must be an array');
     }
+
+    const method = 'search';
+
+    await this._invokeRequestInterceptors(method, type);
 
     const esOptions = {
       index: this._index,
@@ -299,6 +317,10 @@ export class SavedObjectsRepository {
       return { saved_objects: [] };
     }
 
+    const method = 'mget';
+
+    await this._invokeRequestInterceptors(method, null);
+
     const response = await this._callCluster('mget', {
       index: this._index,
       body: {
@@ -341,6 +363,10 @@ export class SavedObjectsRepository {
    * @returns {promise} - { id, type, version, attributes }
    */
   async get(type, id) {
+    const method = 'get';
+
+    await this._invokeRequestInterceptors(method, type, id);
+
     const response = await this._callCluster('get', {
       id: this._generateEsId(type, id),
       type: this._type,
@@ -376,6 +402,8 @@ export class SavedObjectsRepository {
    * @returns {promise}
    */
   async update(type, id, attributes, options = {}) {
+    await this._invokeRequestInterceptors('update', type, attributes, options);
+
     const time = this._getCurrentTime();
     const response = await this._writeToCluster('update', {
       id: this._generateEsId(type, id),
@@ -429,5 +457,20 @@ export class SavedObjectsRepository {
 
   _getCurrentTime() {
     return new Date().toISOString();
+  }
+
+  _collectRequestInterceptors(method) {
+    return this._interceptors.filter(interceptor => interceptor.method === method || interceptor.method === 'all');
+  }
+
+  async _invokeRequestInterceptors(method, type, ...args) {
+    const interceptors = this._collectRequestInterceptors(method);
+
+    for (const interceptor of interceptors) {
+      if (typeof interceptor.intercept !== 'function') {
+        throw new Error(`Request interceptor missing 'intercept' function`);
+      }
+      await interceptor.intercept(this, method, type, ...args);
+    }
   }
 }
