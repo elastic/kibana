94b1ec086b25c334744efb950971d55ada470aaf
ruflin <spam@ruflin.com>
2018-03-28 15:41:59 +0200

    Add Beta Ceph module to Kibana Home
    This is a first example on how to use the `isBeat` feature.

---
+++ src/core_plugins/kibana/server/tutorials/ceph_metrics/index.js
@@ -0,0 +1,53 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { TUTORIAL_CATEGORY } from '../../../common/tutorials/tutorial_category';
+import { ON_PREM_INSTRUCTIONS } from './on_prem';
+import { ELASTIC_CLOUD_INSTRUCTIONS } from './elastic_cloud';
+import { ON_PREM_ELASTIC_CLOUD_INSTRUCTIONS } from './on_prem_elastic_cloud';
+
+export function cephMetricsSpecProvider() {
+  return {
+    id: 'cephMetrics',
+    name: 'Ceph metrics',
+    isBeta: true,
+    category: TUTORIAL_CATEGORY.METRICS,
+    shortDescription: 'Fetch internal metrics from the Ceph server.',
+    longDescription: 'The `ceph` Metricbeat module fetches internal metrics from Ceph.' +
+                     ' [Learn more]({config.docs.beats.metricbeat}/metricbeat-module-ceph.html).',
+    euiIconType: 'logoCeph',
+    artifacts: {
+      dashboards: [
+        {
+          id: 'ewer-f1a5-11e7-a9ef-93c69af7b129',
+          linkLabel: 'Ceph metrics dashboard',
+          isOverview: true
+        }
+      ],
+      exportedFields: {
+        documentationUrl: '{config.docs.beats.metricbeat}/exported-fields-ceph.html'
+      }
+    },
+    completionTimeMinutes: 10,
+    //previewImagePath: '/plugins/kibana/home/tutorial_resources/elasticsearch_metrics/screenshot.png',
+    onPrem: ON_PREM_INSTRUCTIONS,
+    elasticCloud: ELASTIC_CLOUD_INSTRUCTIONS,
+    onPremElasticCloud: ON_PREM_ELASTIC_CLOUD_INSTRUCTIONS
+  };
+}
---
+++ src/core_plugins/kibana/server/tutorials/ceph_metrics/on_prem_elastic_cloud.js
@@ -0,0 +1,80 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { INSTRUCTION_VARIANT } from '../../../common/tutorials/instruction_variant';
+import { METRICBEAT_INSTRUCTIONS } from '../../../common/tutorials/metricbeat_instructions';
+import {
+  TRYCLOUD_OPTION1,
+  TRYCLOUD_OPTION2
+} from '../../../common/tutorials/onprem_cloud_instructions';
+import { ENABLE_INSTRUCTIONS } from './enable';
+
+export const ON_PREM_ELASTIC_CLOUD_INSTRUCTIONS = {
+  instructionSets: [
+    {
+      title: 'Getting Started',
+      instructionVariants: [
+        {
+          id: INSTRUCTION_VARIANT.OSX,
+          instructions: [
+            TRYCLOUD_OPTION1,
+            TRYCLOUD_OPTION2,
+            METRICBEAT_INSTRUCTIONS.INSTALL.OSX,
+            METRICBEAT_INSTRUCTIONS.CONFIG.OSX,
+            ENABLE_INSTRUCTIONS.OSX,
+            METRICBEAT_INSTRUCTIONS.START.OSX
+          ]
+        },
+        {
+          id: INSTRUCTION_VARIANT.DEB,
+          instructions: [
+            TRYCLOUD_OPTION1,
+            TRYCLOUD_OPTION2,
+            METRICBEAT_INSTRUCTIONS.INSTALL.DEB,
+            METRICBEAT_INSTRUCTIONS.CONFIG.DEB,
+            ENABLE_INSTRUCTIONS.DEB,
+            METRICBEAT_INSTRUCTIONS.START.DEB
+          ]
+        },
+        {
+          id: INSTRUCTION_VARIANT.RPM,
+          instructions: [
+            TRYCLOUD_OPTION1,
+            TRYCLOUD_OPTION2,
+            METRICBEAT_INSTRUCTIONS.INSTALL.RPM,
+            METRICBEAT_INSTRUCTIONS.CONFIG.RPM,
+            ENABLE_INSTRUCTIONS.RPM,
+            METRICBEAT_INSTRUCTIONS.START.RPM
+          ]
+        },
+        {
+          id: INSTRUCTION_VARIANT.WINDOWS,
+          instructions: [
+            TRYCLOUD_OPTION1,
+            TRYCLOUD_OPTION2,
+            METRICBEAT_INSTRUCTIONS.INSTALL.WINDOWS,
+            METRICBEAT_INSTRUCTIONS.CONFIG.WINDOWS,
+            ENABLE_INSTRUCTIONS.WINDOWS,
+            METRICBEAT_INSTRUCTIONS.START.WINDOWS
+          ]
+        }
+      ]
+    }
+  ]
+};
