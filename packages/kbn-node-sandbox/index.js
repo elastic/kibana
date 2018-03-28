dcd2e4f2b6ed76acd1e2642e057739b383b39459
kobelb <brandon.kobel@elastic.co>
2018-03-28 14:34:37 -0400

    Fixing some linting issues

--- packages/kbn-node-sandbox/index.js
+++ packages/kbn-node-sandbox/index.js
@@ -1,12 +1,12 @@
-const platform = require("os").platform();
+const platform = require('os').platform();
 
 switch (platform) {
-  case "win32":
-  case "linux":
-    exports.activate = require("./build/Release/sandbox_" + platform).activate;
+  case 'win32':
+  case 'linux':
+    exports.activate = require('./build/Release/sandbox_' + platform).activate;
     break;
   default:
-    exports.activate = function() {
-      return { success: false, message: "Platform " + platform + " has no sandbox support." }
-    }
+    exports.activate = () => {
+      return { success: false, message: `Platform ${platform} has no sandbox support.` };
+    };
 }
--- packages/kbn-node-sandbox/test.js
+++ packages/kbn-node-sandbox/test.js
@@ -1,18 +1,17 @@
-'use strict';
 const assert = require('assert');
-const sandbox = require("./index.js");
-const child_process = require("child_process");
+const sandbox = require('./index.js');
+const { exec } = require('child_process');
 
 function tryExec() {
   // child_process.spawn() throws an exception immediately if it fails.
   // so there's no need to bind to the 'error' event.
-  child_process.exec("true");
+  exec('true');
 }
 
 assert.doesNotThrow(tryExec);
 
-let s = sandbox.activate()
-assert.ok(s.success, "Sandbox failed to activate: " + s.message);
+const result = sandbox.activate();
+assert.ok(result.success, 'Sandbox failed to activate: ' + result.message);
 
 // EACCES on Linux, UNKNOWN on Windows.
 assert.throws(tryExec, /Error: spawn (EACCES|UNKNOWN)/);
