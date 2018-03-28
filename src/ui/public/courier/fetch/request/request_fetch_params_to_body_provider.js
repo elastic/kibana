df9a82a57cd1d00d3b7884ff02e77540138fc0a7
Nathan Reese <reese.nathan@gmail.com>
2018-03-28 14:02:41 -0600

    advanced setting to control search request preference (#17271)
    * advanced setting to control search request preference
    
    * add header tests
    
    * add sentince about caching to description
    
    * change courier:setRequestPreference to list and add courier:customRequestPreference
    
    * update setting text

--- src/ui/public/courier/fetch/request/request_fetch_params_to_body_provider.js
+++ src/ui/public/courier/fetch/request/request_fetch_params_to_body_provider.js
@@ -1,12 +1,13 @@
 import { requestFetchParamsToBody } from './request_fetch_params_to_body';
 
-export function RequestFetchParamsToBodyProvider(Promise, timefilter, kbnIndex, sessionId) {
+export function RequestFetchParamsToBodyProvider(Promise, timefilter, kbnIndex, sessionId, config) {
   return (requestsFetchParams) => (
     requestFetchParamsToBody(
       requestsFetchParams,
       Promise,
       timefilter,
       kbnIndex,
-      sessionId)
+      sessionId,
+      config)
   );
 }
