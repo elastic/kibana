7e51f67484504d172b63e5da9f60c41c4d4ce6cb
archanid <artistic.archana@gmail.com>
2018-04-02 15:30:27 -0500

    stub some tests

--- test/integration/config.js
+++ test/integration/config.js
@@ -1,29 +1,26 @@
-// import {
-// SupertestProvider,
-// ChanceProvider,
-// } from './services';
-
-export async function runIntegrationTests({ readConfigFile }) {
+//export async function runIntegrationTests({ readConfigFile }) {
+export default async function ({ readConfigFile }) {
   const commonConfig = await readConfigFile(require.resolve('../common/config'));
+  const kibanaAPITestsConfig = await readConfigFile(require.resolve('../api_integration/config.js'));
 
   return {
     // TODO: including testFiles like this isn't really going to work.
     // we need a way to run each kind with its own server setup
     // and teardown.
     testFiles: [
-      require.resolve('./with-basepath/index.js'),
-      require.resolve('./without-basepath/index.js'),
+      // nb: start with assuming only one simple setup
       require.resolve('./with-ssl/index.js'),
+      // require.resolve('./with-basepath/index.js'),
+      // require.resolve('./without-basepath/index.js'),
     ],
+    servers: commonConfig.get('servers'),
     services: {
       es: commonConfig.get('services.es'),
       esArchiver: commonConfig.get('services.esArchiver'),
       retry: commonConfig.get('services.retry'),
-      // TODO: determine if these are needed
-      // supertest: SupertestProvider,
-      // chance: ChanceProvider,
+      supertest: kibanaAPITestsConfig.get('services.supertest'),
+      chance: kibanaAPITestsConfig.get('services.chance'),
     },
-    servers: commonConfig.get('servers'),
     junit: {
       reportName: 'Integration Tests'
     }
