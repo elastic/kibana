52676b4c9f71bf4ba1f3559447823fc7ecf11497
kobelb <brandon.kobel@elastic.co>
2018-04-04 14:17:03 -0400

    Disabling system call filters to see if the tests pass then

--- src/server/system_call_filters/index.js
+++ src/server/system_call_filters/index.js
@@ -1,6 +1,8 @@
 import sandbox from '@kbn/sandbox';
 
 export async function activateSystemCallFilters() {
+  return;
+
   // Darwin doesn't support system call filtering/sandboxing so we don't do anything here
   if (process.platform === 'darwin') {
     return;
