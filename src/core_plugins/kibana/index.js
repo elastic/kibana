e2670b82c2d12f0952558803fca7d4ffb357b376
Tim Sullivan <tsullivan@users.noreply.github.com>
2018-04-05 18:41:56 -0700

    [Server/Routes] organize status routes together with others (#17558)
    * [Server/Routes] organize status routes together with others
    
    * remove flaky assertion
    
    * move all the status stuff into src/server/status
    
    * sugar on imports/exports
    
    * fix lint/jest test
    
    * comment and todo

--- src/core_plugins/kibana/index.js
+++ src/core_plugins/kibana/index.js
@@ -4,12 +4,12 @@ import Promise from 'bluebird';
 import { mkdirp as mkdirpNode } from 'mkdirp';
 
 import manageUuid from './server/lib/manage_uuid';
-import search from './server/routes/api/search';
+import { searchApi } from './server/routes/api/search';
 import { scrollSearchApi } from './server/routes/api/scroll_search';
 import { importApi } from './server/routes/api/import';
 import { exportApi } from './server/routes/api/export';
 import { homeApi } from './server/routes/api/home';
-import scripts from './server/routes/api/scripts';
+import { scriptsApi } from './server/routes/api/scripts';
 import { registerSuggestionsApi } from './server/routes/api/suggestions';
 import { registerFieldFormats } from './server/field_formats/register';
 import { registerTutorials } from './server/tutorials/register';
@@ -144,8 +144,8 @@ export default function (kibana) {
       // uuid
       manageUuid(server);
       // routes
-      search(server);
-      scripts(server);
+      searchApi(server);
+      scriptsApi(server);
       scrollSearchApi(server);
       importApi(server);
       exportApi(server);
