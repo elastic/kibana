53cb1044a3ec10987ef45fad0b26dab8080f1a4c
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-04-11 17:36:17 -0400

    generic fix for issue with spec collisions

--- src/core_plugins/console/api_server/spec/index.js
+++ src/core_plugins/console/api_server/spec/index.js
@@ -1,19 +1,29 @@
-const glob = require('glob')
-const { join, basename } = require('path')
-const { readFileSync } = require('fs')
-const { merge } = require('lodash')
+const glob = require('glob');
+const { join, basename } = require('path');
+const { readFileSync } = require('fs');
+const { merge, pick } = require('lodash');
 
 export function getSpec() {
-  const generatedFiles = glob.sync(join(__dirname, 'generated', '*.json'))
-  const overrideFiles = glob.sync(join(__dirname, 'overrides', '*.json'))
+  const generatedFiles = glob.sync(join(__dirname, 'generated', '*.json'));
+  const overrideFiles = glob.sync(join(__dirname, 'overrides', '*.json'));
 
   return generatedFiles.reduce((acc, file) => {
-    const overrideFile = overrideFiles.find(f => basename(f) === basename(file))
-    const spec = JSON.parse(readFileSync(file, 'utf8'))
+    const overrideFile = overrideFiles.find(f => basename(f) === basename(file));
+    let spec = JSON.parse(readFileSync(file, 'utf8'));
     if (overrideFile) {
-      merge(spec, JSON.parse(readFileSync(overrideFile, 'utf8')))
+      merge(spec, JSON.parse(readFileSync(overrideFile, 'utf8')));
     }
+    const collisions = pick(spec, function (v, k) { return acc[k]; });
+    if (JSON.stringify(collisions) !== '{}') {
+      const newSpec = {};
+      Object.entries(collisions).forEach(([ endpoint, definition ]) => {
+        // make the key of the spec unique when there is a collision with another spec
+        newSpec[`${endpoint}${Date.now()}`] = definition;
+      });
+      spec = newSpec;
+    }
+
     return { ...acc, ...spec };
   }, {});
-  return result
+  return result;
 }
