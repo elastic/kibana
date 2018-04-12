2ee2c7a6020a0f7df43ef0efb98626d761d5667f
Joe Fleming <w33ble@users.noreply.github.com>
2018-04-12 11:57:38 -0700

    fix: make homepage usable (#495)
    style the container a little and make the workpad loader usable, instead of the white on white that was there before

--- public/apps/home/home_app.js
+++ public/apps/home/home_app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { WorkpadLoader } from '../../components/workpad_loader';
+import './home_app.less';
 
 export const HomeApp = () => (
-  <div>
+  <div className="canvas__home_app">
     <h1>Canvas</h1>
     <p>
       Welcome to Canvas! To get started, create a new workpad, or load an existing workpad from the
