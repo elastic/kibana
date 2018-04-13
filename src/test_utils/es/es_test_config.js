a1fa8f8eb32aa42180bd29c2032fe0e4db6d352a
LeeDr <lee.drengenberg@elastic.co>
2018-04-13 09:21:36 -0500

    Change es_test_config.js similar to kibana_test_server_url_parts.js

--- src/test_utils/es/es_test_config.js
+++ src/test_utils/es/es_test_config.js
@@ -20,40 +20,32 @@ export const esTestConfig = new class EsTestConfig {
   }
 
   getUrlParts() {
-
-    let testEsUrl;
-    let protocol;
-    let hostname;
-    let port;
-    let username;
-    let password;
-
     // Allow setting one complete TEST_ES_URL for Es like https://elastic:changeme@myCloudInstance:9200
     if (process.env.TEST_ES_URL) {
-      testEsUrl = url.parse(process.env.TEST_ES_URL);
-      // have to remove the ":" off protocol
-      protocol = testEsUrl.protocol.slice(0, -1);
-      hostname = testEsUrl.hostname;
-      port = parseInt(testEsUrl.port, 10);
-      username = testEsUrl.auth.split(':')[0];
-      password = testEsUrl.auth.split(':')[1];
-    } else {
-      // Allow setting any individual component(s) of the URL,
-      // or use default values (username and password from shield.js)
-      protocol = process.env.TEST_ES_PROTOCOL || 'http';
-      hostname = process.env.TEST_ES_HOSTNAME || 'localhost';
-      port = parseInt(process.env.TEST_ES_PORT, 10) || 9220;
-      username = process.env.TEST_ES_USERNAME || admin.username;
-      password = process.env.TEST_ES_PASSWORD || admin.password;
+      const testEsUrl = url.parse(process.env.TEST_ES_URL);
+      return {
+        // have to remove the ":" off protocol
+        protocol: testEsUrl.protocol.slice(0, -1),
+        hostname: testEsUrl.hostname,
+        port: parseInt(testEsUrl.port, 10),
+        username: testEsUrl.auth.split(':')[0],
+        password: testEsUrl.auth.split(':')[1],
+        auth: testEsUrl.auth
+      };
     }
 
+    const username = process.env.TEST_KIBANA_USERNAME || admin.username;
+    const password = process.env.TEST_KIBANA_PASSWORD || admin.password;
     return {
-      protocol,
-      hostname,
-      port,
-      auth: username + ':' + password,
-      username,
-      password,
+      // Allow setting any individual component(s) of the URL,
+      // or use default values (username and password from shield.js)
+      protocol: process.env.TEST_ES_PROTOCOL || 'http',
+      hostname: process.env.TEST_ES_HOSTNAME || 'localhost',
+      port: parseInt(process.env.TEST_ES_PORT, 10) || 9220,
+      auth: `${username}:${password}`,
+      username: username,
+      password: password,
     };
+
   }
 };
