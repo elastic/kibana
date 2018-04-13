98115747ee1a84f00b3836d55cccf3e24f7c79a9
kobelb <brandon.kobel@elastic.co>
2018-04-13 11:19:46 -0400

    Adding architecture check to the bpf filter and always including __NR_execveat

--- packages/kbn-sandbox/src/native/sandbox_linux.cpp
+++ packages/kbn-sandbox/src/native/sandbox_linux.cpp
@@ -1,5 +1,5 @@
 #include "sandbox.hpp"
-
+#include <linux/audit.h>
 #include <linux/seccomp.h>
 #include <linux/filter.h>
 #include <sys/syscall.h>
@@ -9,17 +9,9 @@
 
 #include <string.h> /* for strerror */
 
-// execveat was added in Linux 3.19
-#ifndef __NR_execveat
-#define JMP_START 4
-#else
-#define JMP_START 5
-#endif
-
 struct sock_filter reject_syscalls[] = {
-  // TODO(sissel): Verify arch of syscall
-  //BPF_STMT(BPF_LD|BPF_W|BPF_ABS, (offsetof(struct seccomp_data, arch))),
-  //BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, ARCH_NR, 1, 0), 
+  BPF_STMT(BPF_LD|BPF_W|BPF_ABS, (offsetof(struct seccomp_data, arch))),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, AUDIT_ARCH_X86_64, 0, 7),
 
   // LD|W|ABS == Load Word at ABSolute offset
   // Load the syscall number
@@ -27,17 +19,14 @@ struct sock_filter reject_syscalls[] = {
 
   // JMP|JEQ|K Do a jump after comparing EQuality of the loaded value and a
   // constant. If equal, jump N positions forward, if not equal, do not jump(zero jump).
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_seccomp, JMP_START, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_fork, JMP_START - 1, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_vfork, JMP_START - 2, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execve, JMP_START - 3, 0),
-
-#ifdef __NR_execveat
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execveat, JMP_START - 4, 0),
-#endif
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_seccomp, 5, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_fork, 4, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_vfork, 3, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execve, 2, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execveat, 1, 0),
 
   // RET|K Return a constant.
-  // If none of the conditions above are met, let's allow this syscall. 
+  // If none of the conditions above are met, let's allow this syscall.
   BPF_STMT(BPF_RET|BPF_K, SECCOMP_RET_ALLOW),
 
   // RET|K Return a constant
