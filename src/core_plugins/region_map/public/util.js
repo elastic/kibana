e87ad67bd810c076fcd0af710cbc8414bd4aabff
Thomas Neirynck <thomas@elastic.co>
2018-04-16 17:23:28 -0400

    track by id

--- src/core_plugins/region_map/public/util.js
+++ src/core_plugins/region_map/public/util.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 export function mapToLayerWithId(prefix, layer) {
   const clonedLayer = _.cloneDeep(layer);
-  clonedLayer.layerId = prefix + '.' + layer.name;
+  const idTracker = prefix === 'self_hosted' ? layer.name : JSON.stringify(layer.id);
+  clonedLayer.layerId = prefix + '.' + idTracker;
   return clonedLayer;
 }
