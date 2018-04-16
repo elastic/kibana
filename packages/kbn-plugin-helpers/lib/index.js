6bd6afbc61aea59a29f940b05d1389cdd50350aa
Christopher Davies <github@christophilus.com>
2018-04-16 15:08:47 -0400

    Remove migrations import from kbn-plugin-helpers

--- packages/kbn-plugin-helpers/lib/index.js
+++ packages/kbn-plugin-helpers/lib/index.js
@@ -1,6 +1,5 @@
 const run = require('./run');
 const utils = require('./utils');
-const migrations = require('./migrations');
 
 module.exports = function () {
   console.error(
@@ -11,4 +10,4 @@ module.exports = function () {
   return run.apply(this, arguments);
 };
 
-Object.assign(module.exports, { run: run, migrations: migrations }, utils);
+Object.assign(module.exports, { run: run }, utils);
