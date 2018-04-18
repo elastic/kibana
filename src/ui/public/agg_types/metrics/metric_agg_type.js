a77293f2102b74f8a6b698e735524692452b50f3
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2018-04-18 08:18:38 +0200

    Get rid of Lodash mixins (#17737) (#17762)
    * Get rid of _.class mixin
    
    * Get rid of _.flattenWith
    
    * Get rid of _.isNumeric
    
    * Get rid of _.toggleInOut
    
    * Get rid of _.onceWithCb
    
    * Get rid of _.callEach
    
    * Get rid of _.organizeBy, _.pushAll, _.move
    
    * Delete Lodash webpackShim

--- src/ui/public/agg_types/metrics/metric_agg_type.js
+++ src/ui/public/agg_types/metrics/metric_agg_type.js
@@ -1,12 +1,13 @@
 import _ from 'lodash';
 import { AggTypesAggTypeProvider } from 'ui/agg_types/agg_type';
 import { fieldFormats } from 'ui/registry/field_formats';
+import { createLegacyClass } from '../../utils/legacy_class';
 
 export function AggTypesMetricsMetricAggTypeProvider(Private) {
   const AggType = Private(AggTypesAggTypeProvider);
 
 
-  _.class(MetricAggType).inherits(AggType);
+  createLegacyClass(MetricAggType).inherits(AggType);
   function MetricAggType(config) {
     MetricAggType.Super.call(this, config);
 
