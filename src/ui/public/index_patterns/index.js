c3a68cd5adfeca3325b351e5fb181e4b9367c881
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-04-22 23:37:43 +0200

    [kuery] Remove `byName` dependency

--- src/ui/public/index_patterns/index.js
+++ src/ui/public/index_patterns/index.js
@@ -1,2 +1,5 @@
 export { IndexPatternsProvider } from './index_patterns';
-export { IndexPatternsApiClientProvider } from './index_patterns_api_client_provider';
+export {
+  IndexPatternsApiClientProvider,
+} from './index_patterns_api_client_provider';
+export { getFieldByName } from './static_utils';
--- src/ui/public/kuery/functions/utils/get_fields.js
+++ src/ui/public/kuery/functions/utils/get_fields.js
@@ -1,10 +1,13 @@
 import * as literal from '../../node_types/literal';
 import * as wildcard from '../../node_types/wildcard';
+import { getFieldByName } from '../../../index_patterns/static_utils';
+
+
 
 export function getFields(node, indexPattern) {
   if (node.type === 'literal') {
     const fieldName = literal.toElasticsearchQuery(node);
-    const field = indexPattern.fields.byName[fieldName];
+    const field = getFieldByName(indexPattern.fields, fieldName);
     if (!field) {
       return [];
     }
