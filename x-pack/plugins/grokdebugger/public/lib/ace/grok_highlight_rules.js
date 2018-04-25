290c0209dbeb14b7b826be98d298020a682c7c09
Shaunak Kashyap <ycombinator@gmail.com>
2018-04-25 09:17:58 -0700

    Adding basic syntax highlighting for grok expressions

---
+++ x-pack/plugins/grokdebugger/public/lib/ace/grok_highlight_rules.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import ace from 'ace';
+
+const TextHighlightRules = ace.acequire("ace/mode/text_highlight_rules").TextHighlightRules;
+
+export class GrokHighlightRules extends TextHighlightRules {
+  constructor() {
+    super();
+    this.$rules = this.getHighlightRules();
+  }
+
+  getHighlightRules() {
+    return {
+      start: [
+        {
+          token: [
+            "grokStart",
+            "grokPatternName",
+            "grokSeparator",
+            "grokFieldName",
+            "grokEnd"
+          ],
+          regex: "(%{)([^:]+)(:)([^:]+)(})"
+        },
+        {
+          token: [
+            "grokStart",
+            "grokPatternName",
+            "grokSeparator",
+            "grokFieldName",
+            "grokSeparator",
+            "grokFieldType",
+            "grokEnd"
+          ],
+          regex: "(%{)([^:]+)(:)([^:]+)(:)([^:]+)(})"
+        }
+      ]
+    };
+  }
+}
+
+/*
+%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip};%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}
+*/
