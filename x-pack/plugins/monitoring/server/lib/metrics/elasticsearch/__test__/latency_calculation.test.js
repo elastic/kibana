3d314cdd5fc125b2847e71ad3f8ab50f252132b0
Timothy Sullivan <tsullivan@elastic.co>
2018-04-25 15:36:55 -0700

    [Monitoring] reorganize metrics per stack product
    This organizes the metrics classes per product as a starting point for adding more functions to the metrics so external logic in the `get_metrics` and `get_series` modules can be simplified.
    
    New structure:
    ```
    plugins/monitoring/server/lib/metrics
    ├── __test__
    │   ├── __snapshots__
    │   │   └── metrics.test.js.snap
    │   └── metrics.test.js
    ├── beats
    │   ├── __test__
    │   │   └── cpu_utilization_calculation.test.js
    │   ├── classes.js
    │   └── metrics.js
    ├── classes
    │   ├── __test__
    │   │   ├── latency_metric_calculation.test.js
    │   │   └── quota_metric_calculation.test.js
    │   ├── cluster_metric.js
    │   ├── index.js
    │   ├── metric.js
    │   └── quota_metric.js
    ├── elasticsearch
    │   ├── __test__
    │   │   └── latency_calculation.test.js
    │   ├── classes.js
    │   └── metrics.js
    ├── index.js
    ├── kibana
    │   ├── classes.js
    │   └── metrics.js
    ├── logstash
    │   ├── classes.js
    │   └── metrics.js
    └── metrics.js
    ```

--- x-pack/plugins/monitoring/server/lib/metrics/elasticsearch/__test__/latency_calculation.test.js
+++ x-pack/plugins/monitoring/server/lib/metrics/elasticsearch/__test__/latency_calculation.test.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { LatencyMetric } from '../';
+import { LatencyMetric } from '../classes';
 
 describe('LatencyMetric for Query/Index Metric derivatives', () => {
 
