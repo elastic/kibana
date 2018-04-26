b52fe4cf5476138e1b123055100c20d4faaad449
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-04-26 02:03:46 +0200

    Increase maxBuffer to 100mb closes #65

--- src/lib/rpc.js
+++ src/lib/rpc.js
@@ -4,9 +4,12 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const findUp = require('find-up');
 
+const execAsPromised = promisify(exec);
+
 module.exports = {
   execVanilla: exec,
-  exec: promisify(exec),
+  exec: (cmd, options) =>
+    execAsPromised(cmd, { maxBuffer: 100 * 1024 * 1024, ...options }),
   writeFile: promisify(fs.writeFile),
   readFile: promisify(fs.readFile),
   stat: promisify(fs.stat),
