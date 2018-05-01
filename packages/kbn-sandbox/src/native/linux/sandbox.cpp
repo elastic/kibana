3325f9b2ef4b5f3d91d931f57ca5b287eedb369a
kobelb <brandon.kobel@elastic.co>
2018-05-01 14:10:05 -0400

    Removing __NR_seccomp from the filter

--- packages/kbn-sandbox/src/native/linux/sandbox.cpp
+++ packages/kbn-sandbox/src/native/linux/sandbox.cpp
@@ -12,7 +12,7 @@
 
 struct sock_filter reject_syscalls[] = {
   BPF_STMT(BPF_LD|BPF_W|BPF_ABS, (offsetof(struct seccomp_data, arch))),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, AUDIT_ARCH_X86_64, 0, 8),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, AUDIT_ARCH_X86_64, 0, 7),
 
   // LD|W|ABS == Load Word at ABSolute offset
   // Load the syscall number
@@ -20,8 +20,7 @@ struct sock_filter reject_syscalls[] = {
 
   // JMP|JEQ|K Do a jump after comparing EQuality of the loaded value and a
   // constant. If equal, jump N positions forward, if not equal, do not jump(zero jump).
-  BPF_JUMP(BPF_JMP|BPF_JGT|BPF_K, __X32_SYSCALL_BIT - 1, 6, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_seccomp, 5, 0),
+  BPF_JUMP(BPF_JMP|BPF_JGT|BPF_K, __X32_SYSCALL_BIT - 1, 5, 0),
   BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_fork, 4, 0),
   BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_vfork, 3, 0),
   BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execve, 2, 0),
