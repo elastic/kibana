fb4e9c2bb1792056fa363ae2baacd23bd0b1b2e5
Justin Kambic <justin.kambic@elastic.co>
2018-05-04 12:20:36 -0400

    [Grok Debugger] Better editor expansion/Fix border hiding bottom line (#18752)
    * Update to grok debugger editors: let them grow taller and stop hiding half of bottom line input.
    
    * Update custom pattern size to 10 LOC.
    
    * Remove unnecessary options, add constants for scroll margin.
    
    * Encapsulate editor options in mutate function.
    
    * Move editor helper file and prefer "function" declaration over ES6 const.
    
    * Add test for new helper function.

---
+++ x-pack/plugins/grokdebugger/public/lib/ace/apply_editor_options.test.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { applyEditorOptions } from './apply_editor_options';
+import { EDITOR } from '../../../common/constants';
+
+describe('applyEditorOptions', () => {
+  let editor;
+  let setUseWrapMode;
+  let setScrollMargin;
+  let setOptions;
+
+  beforeEach(() => {
+    setUseWrapMode = jest.fn();
+    setScrollMargin = jest.fn();
+    setOptions = jest.fn();
+
+    editor = {
+      getSession: () => {
+        return { setUseWrapMode };
+      },
+      renderer: {
+        setScrollMargin,
+      },
+      setOptions,
+    };
+  });
+
+  it('creates default props and given line sizes', () => {
+    const minLines = 14;
+    const maxLines = 90;
+
+    applyEditorOptions(editor, minLines, maxLines);
+
+    expect(setUseWrapMode.mock.calls).toHaveLength(1);
+    expect(setUseWrapMode.mock.calls[0][0]).toBe(true);
+
+    expect(setScrollMargin.mock.calls).toHaveLength(1);
+    expect(setScrollMargin.mock.calls[0][0]).toEqual(EDITOR.SCROLL_MARGIN_TOP);
+    expect(setScrollMargin.mock.calls[0][1]).toEqual(EDITOR.SCROLL_MARGIN_BOTTOM);
+    expect(setScrollMargin.mock.calls[0][2]).toEqual(0);
+    expect(setScrollMargin.mock.calls[0][3]).toEqual(0);
+
+    expect(setOptions.mock.calls).toHaveLength(1);
+    expect(setOptions.mock.calls[0][0]).toEqual({
+      highlightActiveLine: false,
+      highlightGutterLine: false,
+      minLines: 14,
+      maxLines: 90,
+    });
+
+    expect(editor.$blockScrolling).toEqual(Infinity);
+  });
+});
