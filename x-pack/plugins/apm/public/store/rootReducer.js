7904ce10147988676e2f0bad0ce949b0eec0333e
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-05-04 20:29:00 +0200

    Replace api calls with ReduxRequest (#18815)

--- x-pack/plugins/apm/public/store/rootReducer.js
+++ x-pack/plugins/apm/public/store/rootReducer.js
@@ -5,39 +5,16 @@
  */
 
 import { combineReducers } from 'redux';
-
-import detailsCharts from './detailsCharts';
-import errorDistribution from './errorDistribution';
-import errorGroup from './errorGroup';
-import errorGroupList from './errorGroupList';
-import license from './license';
 import location from './location';
-import overviewCharts from './overviewCharts';
-import service from './service';
-import serviceList from './serviceList';
 import sorting from './sorting';
-import spans from './spans';
-import transaction from './transaction';
-import transactionDistribution from './transactionDistribution';
-import transactionList from './transactionList';
 import urlParams from './urlParams';
+import { reduxRequestReducer } from '../components/shared/ReduxRequest';
 
 const rootReducer = combineReducers({
-  detailsCharts,
-  errorDistribution,
-  errorGroup,
-  errorGroupList,
-  license,
   location,
-  overviewCharts,
-  service,
-  serviceList,
   sorting,
-  spans,
-  transaction,
-  transactionDistribution,
-  transactionList,
-  urlParams
+  urlParams,
+  reduxRequest: reduxRequestReducer
 });
 
 export default rootReducer;
