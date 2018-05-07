8079bec770cda8e3be6d5a21283731e2baa9fa2a
Jen Huang <its.jenetic@gmail.com>
2018-05-07 16:20:56 -0700

    Advanced Settings to React/EUI (#18878)

---
+++ src/core_plugins/kibana/public/management/sections/settings/components/call_outs/call_outs.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { CallOuts } from './call_outs';
+
+describe('CallOuts', () => {
+  it('should render normally', async () => {
+    const component = shallow(
+      <CallOuts />
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
