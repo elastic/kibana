4ec43852f5b3cdb31ffbe4cb1f8533022ec2f298
Brandon Kobel <brandon.kobel@gmail.com>
2018-05-08 19:14:26 -0400

    Making schema async, and plugin discovery expose raw package jsons (#18926)
    * Making schema async, and plugin discovery expose raw package jsons
    
    * Addressing some peer review comments
    
    * Modifying the way we emit the extendedConfig
    
    * Removing errant config
    
    * Adding filter and last so we only get the last non-null one

--- src/plugin_discovery/plugin_pack/__tests__/package_jsons_in_directory.js
+++ src/plugin_discovery/plugin_pack/__tests__/package_jsons_in_directory.js
@@ -2,8 +2,7 @@ import { resolve } from 'path';
 
 import expect from 'expect.js';
 
-import { createPacksInDirectory$ } from '../packs_in_directory';
-import { PluginPack } from '../plugin_pack';
+import { createPackageJsonsInDirectory$ } from '../package_jsons_in_directory';
 
 import {
   PLUGINS_DIR,
@@ -11,10 +10,10 @@ import {
 } from './utils';
 
 describe('plugin discovery/packs in directory', () => {
-  describe('createPacksInDirectory$()', () => {
+  describe('createPackageJsonsInDirectory$()', () => {
     describe('errors emitted as { error } results', () => {
       async function checkError(path, check) {
-        const results = await createPacksInDirectory$(path).toArray().toPromise();
+        const results = await createPackageJsonsInDirectory$(path).toArray().toPromise();
         expect(results).to.have.length(1);
         expect(results[0]).to.only.have.keys('error');
         const { error } = results[0];
@@ -43,24 +42,24 @@ describe('plugin discovery/packs in directory', () => {
       }));
     });
 
-    it('includes child errors for invalid packs within a valid directory', async () => {
-      const results = await createPacksInDirectory$(PLUGINS_DIR).toArray().toPromise();
+    it('includes child errors for invalid packageJsons within a valid directory', async () => {
+      const results = await createPackageJsonsInDirectory$(PLUGINS_DIR).toArray().toPromise();
 
       const errors = results
         .map(result => result.error)
         .filter(Boolean);
 
-      const packs = results
-        .map(result => result.pack)
+      const packageJsons = results
+        .map(result => result.packageJson)
         .filter(Boolean);
 
-      packs.forEach(pack => expect(pack).to.be.a(PluginPack));
+      packageJsons.forEach(pack => expect(pack).to.be.an(Object));
       // there should be one result for each item in PLUGINS_DIR
       expect(results).to.have.length(9);
-      // six of the fixtures are errors of some sort
-      expect(errors).to.have.length(7);
-      // two of them are valid
-      expect(packs).to.have.length(2);
+      // three of the fixtures are errors of some sort
+      expect(errors).to.have.length(3);
+      // six of them are valid
+      expect(packageJsons).to.have.length(6);
     });
   });
 });
