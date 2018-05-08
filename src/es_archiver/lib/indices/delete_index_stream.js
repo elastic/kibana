7d153525e60b1fa1c0f8a8e05fd5d91a3c3887cd
Spencer <spalger@users.noreply.github.com>
2018-05-08 16:03:35 -0700

    [6.x] [esArchiver/deleteIndex] wait and retry if snapshot in progress (#18624) (#18934)
    * [esArchiver/deleteIndex] wait and retry if snapshot in progress
    
    * [esArchiver/deleteIndex] use recursion for retry
    
    * [esArchiver/waitForSnapshot] invert status check
    
    * [esArchiver] share delete-with-retry with create stream
    
    * [esArchiver/stats] include index name in message
    
    * [esArchiver/indexDelete] wait for snapshot completion up to three times
    
    * [esArchiver] log status of snapshot during checks

--- src/es_archiver/lib/indices/delete_index_stream.js
+++ src/es_archiver/lib/indices/delete_index_stream.js
@@ -1,27 +1,15 @@
 import { Transform } from 'stream';
 
-import { get } from 'lodash';
+import { deleteIndex } from './delete_index';
 
-export function createDeleteIndexStream(client, stats) {
-
-  async function deleteIndex(index) {
-    try {
-      await client.indices.delete({ index });
-      stats.deletedIndex(index);
-    } catch (err) {
-      if (get(err, 'body.error.type') !== 'index_not_found_exception') {
-        throw err;
-      }
-    }
-  }
-
+export function createDeleteIndexStream(client, stats, log) {
   return new Transform({
     readableObjectMode: true,
     writableObjectMode: true,
     async transform(record, enc, callback) {
       try {
         if (!record || record.type === 'index') {
-          await deleteIndex(record.value.index);
+          await deleteIndex({ client, stats, log, index: record.value.index });
         } else {
           this.push(record);
         }
