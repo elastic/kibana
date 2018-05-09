16d33c189cf17f3f0d40bc156680b8c541e164bd
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-05-09 10:39:42 -0400

    using raw loader to load worker file

--- src/core_plugins/console/public/src/sense_editor/mode/script.js
+++ src/core_plugins/console/public/src/sense_editor/mode/script.js
@@ -5,7 +5,6 @@ const TextMode = ace.acequire('ace/mode/text').Mode;
 const MatchingBraceOutdent = ace.acequire('ace/mode/matching_brace_outdent').MatchingBraceOutdent;
 const CstyleBehaviour = ace.acequire('ace/mode/behaviour/cstyle').CstyleBehaviour;
 const CStyleFoldMode = ace.acequire('ace/mode/folding/cstyle').FoldMode;
-//const WorkerClient = ace.acequire('ace/worker/worker_client').WorkerClient;
 ace.acequire('ace/tokenizer');
 
 const ScriptHighlightRules = require('./script_highlight_rules').ScriptHighlightRules;
@@ -39,22 +38,4 @@ oop.inherits(ScriptMode, TextMode);
   this.autoOutdent = function (state, doc, row) {
     this.$outdent.autoOutdent(doc, row);
   };
-
-  // this.createWorker = function (session) {
-  //   const worker = new WorkerClient(['ace', 'sense_editor'], 'sense_editor/mode/worker', 'SenseWorker', 'sense_editor/mode/worker');
-  //   worker.attachToDocument(session.getDocument());
-
-
-  //   worker.on('error', function (e) {
-  //     session.setAnnotations([e.data]);
-  //   });
-
-  //   worker.on('ok', function (anno) {
-  //     session.setAnnotations(anno.data);
-  //   });
-
-  //   return worker;
-  // };
-
-
 }).call(ScriptMode.prototype);
---
+++ src/core_plugins/console/public/src/sense_editor/mode/worker/index.js
@@ -0,0 +1,4 @@
+export const workerModule = {
+  id: 'sense_editor/mode/worker',
+  source: require('raw-loader!./worker.js')
+};
