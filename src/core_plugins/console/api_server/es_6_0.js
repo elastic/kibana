b95d9e1968c8df18ab78fea1042413f18a2b0881
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-05-09 10:45:05 -0400

    addressing PR feedback

--- src/core_plugins/console/api_server/es_6_0.js
+++ src/core_plugins/console/api_server/es_6_0.js
@@ -1,27 +1,23 @@
-import  _ from 'lodash';
 import Api from './api';
 import { getSpec } from './spec';
-const parts = [
-  require('./es_6_0/aliases'),
-  require('./es_6_0/aggregations'),
-  require('./es_6_0/document'),
-  require('./es_6_0/filter'),
-  require('./es_6_0/globals'),
-  require('./es_6_0/ingest'),
-  require('./es_6_0/mappings'),
-  require('./es_6_0/query'),
-  require('./es_6_0/reindex'),
-  require('./es_6_0/search'),
-];
-
 const ES_6_0 = new Api('es_6_0');
-
 const spec = getSpec();
+
+// adding generated specs
 Object.keys(spec).forEach(endpoint => {
   ES_6_0.addEndpointDescription(endpoint, spec[endpoint]);
 });
-_.each(parts, function (apiSection) {
-  apiSection(ES_6_0);
-});
+
+//adding globals and custom API definitions
+require('./es_6_0/aliases')(ES_6_0);
+require('./es_6_0/aggregations')(ES_6_0);
+require('./es_6_0/document')(ES_6_0);
+require('./es_6_0/filter')(ES_6_0);
+require('./es_6_0/globals')(ES_6_0);
+require('./es_6_0/ingest')(ES_6_0);
+require('./es_6_0/mappings')(ES_6_0);
+require('./es_6_0/query')(ES_6_0);
+require('./es_6_0/reindex')(ES_6_0);
+require('./es_6_0/search')(ES_6_0);
 
 export default ES_6_0;
