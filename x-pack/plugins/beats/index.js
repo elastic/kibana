53709476fa020f0056fa176e60bd595f1b3efc7c
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-10 14:56:48 -0700

    Removing bits not (yet) necessary in initial scaffolding

--- x-pack/plugins/beats/index.js
+++ x-pack/plugins/beats/index.js
@@ -4,19 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { resolve } from 'path';
 import { PLUGIN } from './common/constants';
 
 export function beats(kibana)  {
   return new kibana.Plugin({
     id: PLUGIN.ID,
-    publicDir: resolve(__dirname, 'public'),
     require: ['kibana', 'elasticsearch', 'xpack_main'],
-    uiExports: {
-      managementSections: [
-        'plugins/beats',
-      ]
-    },
     init: function () {
     }
   });
