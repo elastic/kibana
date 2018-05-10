b4105db65b247ad4942ece6fa8a335c947b8c8bb
Tim Sullivan <tsullivan@users.noreply.github.com>
2018-05-09 21:37:24 -0700

    [Monitoring/React] Render ES Indices Listing with Base Controller / React (#18595)
    * [Monitoring/React] Render ES Indices Listing with Base Controller
    
    Refactors the ES Indices Listing to use Base Controller and a React component instead of an Angular directive
    
    * fix functional tests
    
    * fix another test
    
    * better default for summary status component
    
    * nicer code for whitespace after label
    
    * apiFn => apiUrlFn
    
    * Deleted / Closed

--- x-pack/plugins/monitoring/public/components/elasticsearch/indices/index.js
+++ x-pack/plugins/monitoring/public/components/elasticsearch/indices/index.js
@@ -4,4 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { ShowSytemIndicesCheckbox } from './show_system_indices_checkbox';
+export { ElasticsearchIndices } from './indices';
