886cccc5d75e46ae80ccc5c4e6fe7c05b64229bb
Walter Rafelsberger <walter@elastic.co>
2018-05-11 12:40:29 +0200

    [ML] Updated test to reflect new DOM structure.

--- x-pack/plugins/ml/public/components/tooltip/tooltip_directive.js
+++ x-pack/plugins/ml/public/components/tooltip/tooltip_directive.js
@@ -4,9 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// import './styles/main.less';
-
-// import angular from 'angular';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
