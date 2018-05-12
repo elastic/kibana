5e9bc089c7fd44a1bcef74f2080ced80478ce214
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-12 09:22:39 -0700

    Converting to Jest test

--- x-pack/plugins/beats/server/lib/error_wrappers/wrap_es_error.test.js
+++ x-pack/plugins/beats/server/lib/error_wrappers/wrap_es_error.test.js
@@ -4,8 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import expect from 'expect.js';
-import { wrapEsError } from '../wrap_es_error';
+import { wrapEsError } from './wrap_es_error';
 
 describe('wrap_es_error', () => {
   describe('#wrapEsError', () => {
