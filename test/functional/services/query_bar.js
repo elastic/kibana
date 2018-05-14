e742967edbace1cf095d6cfe2b961a7e9e094412
Stacey Gammon <gammon@elastic.co>
2018-05-14 11:20:22 -0400

    Fix, or debug, flaky nested visualization filter test (#18957) (#19036)
    * Debug flaky test, or at least retry the spot that seems to be failing
    
    * Fix missing await, use queryBar service, use more detailed error message
    
    * update remaining references to dashboard.getQuery
    
    * getQueryString not getQuery

--- test/functional/services/query_bar.js
+++ test/functional/services/query_bar.js
@@ -1,11 +1,32 @@
-export function QueryBarProvider({ getService }) {
+export function QueryBarProvider({ getService, getPageObjects }) {
   const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+  const log = getService('log');
+  const PageObjects = getPageObjects(['header']);
 
   class QueryBar {
 
     async getQueryString() {
-      const queryInput = await testSubjects.find('queryInput');
-      return await queryInput.getProperty('value');
+      return await testSubjects.getProperty('queryInput', 'value');
+    }
+
+    async setQuery(query) {
+      log.debug(`QueryBar.setQuery(${query})`);
+      // Extra caution used because of flaky test here: https://github.com/elastic/kibana/issues/16978 doesn't seem
+      // to be actually setting the query in the query input based off
+      await retry.try(async () => {
+        await testSubjects.setValue('queryInput', query);
+        const currentQuery = await this.getQueryString();
+        if (currentQuery !== query) {
+          throw new Error(`Failed to set query input to ${query}, instead query is ${currentQuery}`);
+        }
+      });
+    }
+
+    async submitQuery() {
+      log.debug('QueryBar.submitQuery');
+      await testSubjects.click('querySubmitButton');
+      await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
   }
