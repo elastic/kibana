3019006e121ed8cd1ec2d74fa32d424194a32d2a
kobelb <brandon.kobel@elastic.co>
2018-05-14 09:31:53 -0400

    We're gonna throw the find error in another PR

--- src/server/saved_objects/client/saved_objects_client.js
+++ src/server/saved_objects/client/saved_objects_client.js
@@ -282,10 +282,6 @@ export class SavedObjectsClient {
       throw new TypeError('options.searchFields must be an array');
     }
 
-    if (!type) {
-      throw new Error('options.type is required');
-    }
-
     const esOptions = {
       index: this._index,
       size: perPage,
