d7f9055e3dcbb9f44e856c1e9f323594de811190
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-15 11:29:46 -0700

    [Beats Management] Install Beats index template on plugin init (#19072)
    * Install Beats index template on plugin init
    
    * Adding missing files

---
+++ x-pack/plugins/beats/server/lib/client/call_with_internal_user_factory.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { once } from 'lodash';
+
+const callWithInternalUser = once((server) => {
+  const { callWithInternalUser } = server.plugins.elasticsearch.getCluster('admin');
+  return callWithInternalUser;
+});
+
+export const callWithInternalUserFactory = (server) => {
+  return callWithInternalUser(server);
+};
