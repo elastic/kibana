951c270f745d0eaa4e92dbb8ba60814b14771bf4
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-05-15 17:23:20 +0200

    Rename `@kbn/babel-preset/common & node & webpack` to `@kbn/babel-preset/common_preset & node_preset & webpack_preset`. (#19025)

--- src/optimize/base_optimizer.js
+++ src/optimize/base_optimizer.js
@@ -13,7 +13,7 @@ import { fromRoot } from '../utils';
 import { PUBLIC_PATH_PLACEHOLDER } from './public_path_placeholder';
 
 const POSTCSS_CONFIG_PATH = require.resolve('./postcss.config');
-const BABEL_PRESET_PATH = require.resolve('@kbn/babel-preset/webpack');
+const BABEL_PRESET_PATH = require.resolve('@kbn/babel-preset/webpack_preset');
 const BABEL_EXCLUDE_RE = [
   /[\/\\](webpackShims|node_modules|bower_components)[\/\\]/,
 ];
