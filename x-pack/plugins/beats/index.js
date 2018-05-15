f48ba09e32cd902746dd2146d04f3c971840cf89
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-15 11:29:46 -0700

    [Beats Management] Install Beats index template on plugin init (#19072)
    * Install Beats index template on plugin init
    
    * Adding missing files

--- x-pack/plugins/beats/index.js
+++ x-pack/plugins/beats/index.js
@@ -4,13 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { installIndexTemplate } from './server/lib/index_template';
 import { PLUGIN } from './common/constants';
 
 export function beats(kibana)  {
   return new kibana.Plugin({
     id: PLUGIN.ID,
     require: ['kibana', 'elasticsearch', 'xpack_main'],
-    init: function () {
+    init: async function (server) {
+      await installIndexTemplate(server);
     }
   });
 }
