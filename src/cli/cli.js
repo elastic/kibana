53adf3c5179d3cf41b3fce527a25561d8f0f973e
Tiago Costa <tiagoffcc@hotmail.com>
2018-05-15 04:39:38 +0100

    feat(12976): node version validation at runtime.

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -2,15 +2,40 @@ import _ from 'lodash';
 import { pkg } from '../utils';
 import Command from './command';
 import serveCommand from './serve/serve';
+import semver from 'semver';
 
 const argv = process.env.kbnWorkerArgv ? JSON.parse(process.env.kbnWorkerArgv) : process.argv.slice();
 const program = new Command('bin/kibana');
+const currentNodeVersion = semver.valid(
+  semver.coerce(
+    _.get(process, 'version', null)
+  )
+);
+const requiredNodeVersion = _.get(pkg, 'engines.node', null);
+
+// Validates at Kibana's start if the current node version satisfies
+// the one defined into the package.json file.
+// If a valid node version is being used the process will continue,
+// otherwise it will exit.
+//
+// Note: It supports comparision with a node version range.
+//
+if (!!currentNodeVersion
+  && !!requiredNodeVersion
+  && !semver.satisfies(currentNodeVersion, requiredNodeVersion)) {
+
+  console.error(
+    `Kibana does not support the current Node.js version ' + ${currentNodeVersion} 
+    + '. Please use Node.js ${requiredNodeVersion}.`
+  );
+  process.exit(1);
+}
 
 program
   .version(pkg.version)
   .description(
     'Kibana is an open source (Apache Licensed), browser ' +
-  'based analytics and search dashboard for Elasticsearch.'
+    'based analytics and search dashboard for Elasticsearch.'
   );
 
 // attach commands
