d679cf55392fa2a9ccdc32400b4a84ae15d59c64
Brandon Kobel <brandon.kobel@gmail.com>
2018-05-16 10:01:25 -0400

    Beginning to use the ES APIs to insert/check privileges (#18645)
    * Beginning to use the ES APIs to insert/check privileges
    
    * Removing todo comment, I think we're good with the current check
    
    * Adding ability to edit kibana application privileges
    
    * Introducing DEFAULT_RESOURCE constant
    
    * Removing unused arguments when performing saved objects auth check
    
    * Performing bulkCreate auth more efficiently
    
    * Throwing error in SavedObjectClient.find if type isn't provided
    
    * Fixing Reporting and removing errant console.log
    
    * Introducing a separate hasPrivileges "service"
    
    * Adding tests and fleshing out the has privileges "service"
    
    * Fixing error message
    
    * You can now edit whatever roles you want
    
    * We're gonna throw the find error in another PR
    
    * Changing conflicting version detection to work when user has no
    application privileges
    
    * Throwing correct error when user is forbidden
    
    * Removing unused interceptor
    
    * Adding warning if they're editing a role with application privileges we
    can't edit
    
    * Fixing filter...
    
    * Beginning to only update privileges when they need to be
    
    * More tests
    
    * One more test...
    
    * Restricting the rbac application name that can be chosen
    
    * Removing DEFAULT_RESOURCE check
    
    * Supporting 1024 characters for the role name
    
    * Renaming some variables, fixing issue with role w/ no kibana privileges
    
    * Throwing decorated general error when appropriate
    
    * Fixing test description
    
    * Dedent does nothing...
    
    * Renaming some functions

--- x-pack/plugins/security/server/lib/privileges/privilege_action_registry.js
+++ x-pack/plugins/security/server/lib/privileges/privilege_action_registry.js
@@ -9,24 +9,29 @@
 
 import { buildPrivilegeMap } from './privileges';
 import { getClient } from '../../../../../server/lib/get_client_shield';
+import { equivalentPrivileges } from './equivalent_privileges';
 
 export async function registerPrivilegesWithCluster(server) {
-  return;
-
   const config = server.config();
   const kibanaVersion = config.get('pkg.version');
   const application = config.get('xpack.security.rbac.application');
 
-  const privilegeActionMapping = buildPrivilegeMap(application, kibanaVersion);
+  const expectedPrivileges = buildPrivilegeMap(application, kibanaVersion);
 
   server.log(['security', 'debug'], `Registering Kibana Privileges with Elasticsearch for ${application}`);
 
   const callCluster = getClient(server).callWithInternalUser;
 
-  // TODO(legrego) - non-working stub
+  // we only want to post the privileges when they're going to change as Elasticsearch has
+  // to clear the role cache to get these changes reflected in the _has_privileges API
+  const existingPrivileges = await callCluster(`shield.getPrivilege`, { privilege: application });
+  if (equivalentPrivileges(existingPrivileges, expectedPrivileges)) {
+    server.log(['security', 'debug'], `Kibana Privileges already registered with Elasticearch for ${application}`);
+    return;
+  }
+
+  server.log(['security', 'debug'], `Updated Kibana Privileges with Elasticearch for ${application}`);
   await callCluster('shield.postPrivileges', {
-    body: {
-      [application]: privilegeActionMapping
-    }
+    body: expectedPrivileges
   });
 }
