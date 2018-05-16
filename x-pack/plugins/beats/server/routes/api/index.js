7b297c7769356add105e2100e4f7ecee3f12b0fe
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-16 06:38:31 -0700

    Fixing file name

--- x-pack/plugins/beats/server/routes/api/index.js
+++ x-pack/plugins/beats/server/routes/api/index.js
@@ -7,7 +7,7 @@
 import { registerCreateEnrollmentTokensRoute } from './register_create_enrollment_tokens_route';
 import { registerEnrollBeatRoute } from './register_enroll_beat_route';
 import { registerListBeatsRoute } from './register_list_beats_route';
-import { registerVerifyBeatsRoute } from './register_verify_beats_routes';
+import { registerVerifyBeatsRoute } from './register_verify_beats_route';
 
 export function registerApiRoutes(server) {
   registerCreateEnrollmentTokensRoute(server);
