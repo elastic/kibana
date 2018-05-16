f24a7251445be531f90b2be0650801f8ee8101c8
Spencer <email@spalger.com>
2018-05-16 10:43:55 -0700

    [mocha] remove grunt-simple-mocha (#19079)
    * [mocha] run tests with mocha script, remove grunt-simple-mocha
    
    * [ci] force colored output
    
    * [ci] disable color when reading the yarn bin
    
    * [dev/build/exec] support tooling log having its own chalk
    
    * [dev/mocha] avoid changing the cwd of the process
    
    * [dev/mocha] only match files, speed up negative matches
    
    * [cli/serve/test] strip ansi control characters from logs
    
    * [ci] disable color in all `yarn bin` invocations
    
    * [dev/mocha] cleanup old runInBand check
    
    * [dev/mocha] enable gloabl leak checks

--- src/dev/mocha/index.js
+++ src/dev/mocha/index.js
@@ -1,2 +1,3 @@
 export { createAutoJunitReporter } from './auto_junit_reporter';
 export { setupJunitReportGeneration } from './junit_report_generation';
+export { runMochaCli } from './run_mocha_cli';
