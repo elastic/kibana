efe1f0754d2c33c7f6405aa29c7a228e9cc2e309
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-16 05:59:12 -0700

    Expanding TODO note so I won't forget :)

--- x-pack/plugins/beats/server/routes/api/register_enroll_beat_route.js
+++ x-pack/plugins/beats/server/routes/api/register_enroll_beat_route.js
@@ -61,7 +61,7 @@ function persistBeat(callWithInternalUser, beat) {
 }
 
 // TODO: add license check pre-hook
-// TODO: write to Kibana audit log file
+// TODO: write to Kibana audit log file (include who did the verification as well)
 export function registerEnrollBeatRoute(server) {
   server.route({
     method: 'POST',
