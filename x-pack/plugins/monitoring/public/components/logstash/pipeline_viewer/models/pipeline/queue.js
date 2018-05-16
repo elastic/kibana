701c5abb165789de3c7cf1ed5d925e3e1b89e532
Justin Kambic <justin.kambic@elastic.co>
2018-05-16 14:31:18 -0400

    Add vertex reference to pipeline statement classes. (#19134)

--- x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/models/pipeline/queue.js
+++ x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/models/pipeline/queue.js
@@ -9,10 +9,7 @@ import { Statement } from './statement';
 export class Queue extends Statement {
   static fromPipelineGraphVertex(queueVertex) {
     return new Queue(
-      queueVertex.id,
-      queueVertex.hasExplicitId,
-      queueVertex.stats,
-      queueVertex.meta
+      queueVertex
     );
   }
 }
