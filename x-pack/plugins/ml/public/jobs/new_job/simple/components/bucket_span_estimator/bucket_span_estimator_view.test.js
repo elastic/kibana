a9d47320d5a7f94026ed7f69c3c976ee9283c79a
Walter Rafelsberger <walter@rafelsberger.at>
2018-05-17 09:26:01 +0200

    [ML] Migrate bucket span estimator button to React/EUI. (#19045)
    This migrates the bucket span estimator button to use React/EUI. The existing mlBucketSpanEstimator is refactored to use React without ngReact to have more detailed control in regards to the $scope being transformed to a trimmed down props object with only the information necessary for the React component. This also allows more control in regards to the $scope attributes being watched. Instead of custom styles and classes this now uses the options available for EuiButton. However, there were some minimal overrides necessary to the EUI styles to replicate the narrower height and padding to fit within the input element.

---
+++ x-pack/plugins/ml/public/jobs/new_job/simple/components/bucket_span_estimator/bucket_span_estimator_view.test.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import { BucketSpanEstimator } from './bucket_span_estimator_view';
+
+describe('BucketSpanEstimator', () => {
+  const props = {
+    buttonDisabled: false,
+    estimatorRunning: false,
+    guessBucketSpan: () => {},
+    buttonText: 'Estimate bucket span'
+  };
+
+  const component = (
+    <BucketSpanEstimator {...props} />
+  );
+
+  const wrapper = shallow(component);
+
+  test('renders the button', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  props.buttonDisabled = true;
+  props.estimatorRunning = true;
+  props.buttonText = 'Estimating bucket span';
+  wrapper.setProps(props);
+
+  test('renders the loading button', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
