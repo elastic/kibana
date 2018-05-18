7c1c9a1b59812c460171975481e359db36094b3b
Justin Kambic <justin.kambic@elastic.co>
2018-05-18 12:39:55 -0400

    Simplify flattening, remove non-native dependency. Add more tests.

--- x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/models/list/flatten_pipeline_section.js
+++ x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/models/list/flatten_pipeline_section.js
@@ -4,14 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { flattenDeep } from 'lodash';
-
 export function flattenPipelineSection(pipelineSection, depth, parentId) {
   const list = [];
 
   pipelineSection.forEach(statement => {
-    list.push(statement.toList(depth, parentId));
+    list.push(...statement.toList(depth, parentId));
   });
 
-  return flattenDeep(list);
+  return list;
 }
