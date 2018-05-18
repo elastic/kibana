6dee536da0f2258cbadfc58989d6743816a66929
Justin Kambic <justin.kambic@elastic.co>
2018-05-18 12:54:00 -0400

    Add defaults to simplify function call.

--- x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/models/list/flatten_pipeline_section.js
+++ x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/models/list/flatten_pipeline_section.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export function flattenPipelineSection(pipelineSection, depth, parentId) {
+export function flattenPipelineSection(pipelineSection, depth = 0, parentId = null) {
   const list = [];
 
   pipelineSection.forEach(statement => {
