6676e55b94b7e8919053e0f480033337e84f778d
Joe Fleming <w33ble@users.noreply.github.com>
2018-05-21 15:15:14 -0700

    Fix: swallow uncaught errors in dev mode (#544)
    * chore: remove debugging line
    
    * fix: argtype react wrapping should be sync
    
    * fix: use know error definition
    
    instead of hard-coding the array of errors to check for, just use the error definitions file
    
    * fix: wrap arg type templates
    
    the helper wraps the provided component in an error boundary so it can recover on failures
    
    * fix: check for react errors, ignore them
    
    * feat: uncaught error notification
    
    it doesn't matter if errors are from react, we'll just catch ALL uncaught errors and show an error message

--- public/lib/template_from_react_component.js
+++ public/lib/template_from_react_component.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
+import { ErrorBoundary } from '../components/enhance/error_boundary';
 
 export const templateFromReactComponent = Component => {
-  return async (domNode, config, handlers) => {
+  const WrappedComponent = props => (
+    <ErrorBoundary>
+      {({ error }) => {
+        if (error) {
+          props.renderError();
+          return null;
+        }
+
+        return <Component {...props} />;
+      }}
+    </ErrorBoundary>
+  );
+
+  WrappedComponent.propTypes = {
+    renderError: PropTypes.func.isRequired,
+  };
+
+  return (domNode, config, handlers) => {
     try {
-      ReactDom.render(React.createElement(Component, config), domNode);
+      const el = React.createElement(WrappedComponent, config);
+      ReactDom.render(el, domNode, () => {
+        handlers.done();
+      });
+
+      handlers.onDestroy(() => {
+        ReactDom.unmountComponentAtNode(domNode);
+      });
+    } catch (err) {
       handlers.done();
-    } catch (e) {
       config.renderError();
     }
-
-    handlers.onDestroy(() => {
-      ReactDom.unmountComponentAtNode(domNode);
-    });
   };
 };
