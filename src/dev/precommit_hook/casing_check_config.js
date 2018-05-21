4083d287376360db96b4d0b65a1872e60c0fef14
spalger <email@spalger.com>
2018-05-21 15:41:45 -0700

    [dev/casingCheck] fix es-ES.json violation

--- src/dev/precommit_hook/casing_check_config.js
+++ src/dev/precommit_hook/casing_check_config.js
@@ -20,6 +20,9 @@ export const IGNORE_FILE_GLOBS = [
   '**/.*',
   '**/{webpackShims,__mocks__}/**/*',
   'x-pack/docs/**/*',
+
+  // filename must match language code which requires capital letters
+  'src/ui/ui_i18n/__tests__/fixtures/translations/test_plugin_1/es-ES.json',
 ];
 
 
