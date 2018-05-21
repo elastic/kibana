da98caf861012a1ebca9cf3c98a658ddb6214136
spalger <email@spalger.com>
2018-05-21 08:08:49 -0700

    [dev/ts] add note about why using glob

--- src/dev/typescript/projects.ts
+++ src/dev/typescript/projects.ts
@@ -7,5 +7,8 @@ import { Project } from './project';
 export const PROJECTS = [
   'tsconfig.json',
   'x-pack/tsconfig.json',
+  // NOTE: using glob.sync rather than glob-all or globby
+  // because it takes less than 10 ms, while the other modules
+  // both took closer to 1000ms.
   ...glob.sync('packages/*/tsconfig.json', { cwd: REPO_ROOT }),
 ].map(path => Project.fromConfig(resolve(REPO_ROOT, path)));
