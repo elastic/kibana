470004d66bb7a438c01b691a42635f208d34d8ee
Nathan Reese <reese.nathan@gmail.com>
2018-05-22 11:43:12 -0600

    remove double modal when cloning dashboard with duplicate title (#19049) (#19313)
    * remove double modal when cloning dashboard with duplicate title
    
    * reset duplicate state when input is updated
    
    * update functional test for new clone workflow
    
    * change warning message to EuiCallout
    
    * updates from Stacey-Gammon review

--- src/core_plugins/kibana/public/dashboard/top_nav/clone_modal.js
+++ src/core_plugins/kibana/public/dashboard/top_nav/clone_modal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -12,6 +12,7 @@ import {
   EuiOverlayMask,
   EuiSpacer,
   EuiText,
+  EuiCallOut,
 } from '@elastic/eui';
 
 export class DashboardCloneModal extends React.Component {
@@ -19,23 +20,75 @@ export class DashboardCloneModal extends React.Component {
     super(props);
 
     this.state = {
-      newDashboardName: props.title
+      newDashboardName: props.title,
+      isTitleDuplicateConfirmed: false,
+      hasTitleDuplicate: false,
+      isLoading: false,
     };
   }
+  componentDidMount() {
+    this._isMounted = true;
+  }
 
-  cloneDashboard = () => {
-    this.props.onClone(this.state.newDashboardName);
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  onTitleDuplicate = () => {
+    this.setState({
+      isTitleDuplicateConfirmed: true,
+      hasTitleDuplicate: true,
+    });
+  }
+
+  cloneDashboard = async () => {
+    this.setState({
+      isLoading: true,
+    });
+
+    await this.props.onClone(this.state.newDashboardName, this.state.isTitleDuplicateConfirmed, this.onTitleDuplicate);
+
+    if (this._isMounted) {
+      this.setState({
+        isLoading: false,
+      });
+    }
   };
 
   onInputChange = (event) => {
-    this.setState({ newDashboardName: event.target.value });
+    this.setState({
+      newDashboardName: event.target.value,
+      isTitleDuplicateConfirmed: false,
+      hasTitleDuplicate: false,
+    });
   };
+
+  renderDuplicateTitleCallout = () => {
+    if (!this.state.hasTitleDuplicate) {
+      return;
+    }
+
+    return (
+      <Fragment>
+        <EuiCallOut
+          title={`A Dashboard with the title '${this.state.newDashboardName}' already exists.`}
+          color="warning"
+          data-test-subj="cloneModalTitleDupicateWarnMsg"
+        >
+          <p>
+            Click <strong>Confirm Clone</strong> to clone the dashboard with the duplicate title.
+          </p>
+        </EuiCallOut>
+        <EuiSpacer />
+      </Fragment>
+    );
+  }
 
   render() {
     return (
       <EuiOverlayMask>
         <EuiModal
-          data-tests-subj="dashboardCloneModal"
+          data-test-subj="dashboardCloneModal"
           className="dashboardCloneModal"
           onClose={this.props.onClose}
         >
@@ -54,12 +107,16 @@ export class DashboardCloneModal extends React.Component {
 
             <EuiSpacer />
 
+            {this.renderDuplicateTitleCallout()}
+
             <EuiFieldText
               autoFocus
               data-test-subj="clonedDashboardTitle"
               value={this.state.newDashboardName}
               onChange={this.onInputChange}
+              isInvalid={this.state.hasTitleDuplicate}
             />
+
           </EuiModalBody>
 
           <EuiModalFooter>
@@ -74,6 +131,7 @@ export class DashboardCloneModal extends React.Component {
               fill
               data-test-subj="cloneConfirmButton"
               onClick={this.cloneDashboard}
+              isLoading={this.state.isLoading}
             >
               Confirm Clone
             </EuiButton>
