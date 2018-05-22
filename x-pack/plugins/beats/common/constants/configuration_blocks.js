f8972c7c94f7a06c267e0eb4623e5d8799698e5d
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-22 12:49:11 -0700

    [Beats Management] APIs: Create configuration block (#19270)
    * WIP checkin
    
    * WIP checkin
    
    * Add API integration test
    
    * Converting to Jest test
    
    * Fixing API for default case + adding test for it
    
    * Fixing copy pasta typos
    
    * Fixing variable name
    
    * Using a single index
    
    * Implementing GET /api/beats/agents API
    
    * Creating POST /api/beats/agents/verify API
    
    * Refactoring: extracting out helper functions
    
    * Expanding TODO note so I won't forget :)
    
    * Fixing file name
    
    * Updating mapping
    
    * Fixing minor typo in TODO comment
    
    * Implementing POST /api/beats/configuration_blocks API
    
    * Removing unnecessary escaping
    
    * Fleshing out types + adding validation for them
    
    * Making output singular (was outputs)
    
    * Removing metricbeat.inputs

---
+++ x-pack/plugins/beats/common/constants/configuration_blocks.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const CONFIGURATION_BLOCKS = {
+  TYPES: {
+    OUTPUT: 'output',
+    PROCESSORS: 'processors',
+    FILEBEAT_INPUTS: 'filebeat.inputs',
+    FILEBEAT_MODULES: 'filebeat.modules',
+    METRICBEAT_MODULES: 'metricbeat.modules'
+  }
+};
+
+CONFIGURATION_BLOCKS.UNIQUENESS_ENFORCING_TYPES = [
+  CONFIGURATION_BLOCKS.TYPES.OUTPUT
+];
