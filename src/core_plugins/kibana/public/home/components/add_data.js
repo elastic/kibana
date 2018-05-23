40090ade330855f5294a816a63ae2427279d8eda
Nathan Reese <reese.nathan@gmail.com>
2018-05-23 08:06:25 -0600

    make add data links be side-by-side on home page

--- src/core_plugins/kibana/public/home/components/add_data.js
+++ src/core_plugins/kibana/public/home/components/add_data.js
@@ -13,6 +13,7 @@ import {
   EuiText,
   EuiCard,
   EuiIcon,
+  EuiHorizontalRule,
 } from '@elastic/eui';
 
 export function AddData({ apmUiEnabled }) {
@@ -115,28 +116,14 @@ export function AddData({ apmUiEnabled }) {
 
       {renderCards()}
 
-      <EuiFlexGroup justifyContent="center">
-        <EuiFlexItem grow={false}>
-          <EuiText>
-            <span style={{ height: 38 }}>
-              Data already in Elasticsearch?
-            </span>
-            <EuiLink
-              style={{ marginLeft: 8 }}
-              href="#/management/kibana/index"
-            >
-              Set up index patterns
-            </EuiLink>
-          </EuiText>
-        </EuiFlexItem>
-      </EuiFlexGroup>
+      <EuiHorizontalRule />
 
-      <EuiFlexGroup justifyContent="center">
+      <EuiFlexGroup justifyContent="spaceAround">
         <EuiFlexItem grow={false}>
           <EuiText>
-            <span style={{ height: 38 }}>
+            <strong style={{ height: 38 }}>
               Fresh Elastic stack installation?
-            </span>
+            </strong>
             <EuiLink
               style={{ marginLeft: 8 }}
               href="#/home/tutorial_directory/sampleData"
@@ -145,6 +132,21 @@ export function AddData({ apmUiEnabled }) {
             </EuiLink>
           </EuiText>
         </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiText>
+            <strong style={{ height: 38 }}>
+              Data already in Elasticsearch?
+            </strong>
+            <EuiLink
+              style={{ marginLeft: 8 }}
+              href="#/management/kibana/index"
+            >
+              Set up index patterns
+            </EuiLink>
+          </EuiText>
+        </EuiFlexItem>
+
+
       </EuiFlexGroup>
 
     </EuiPanel>
