464f73abb2a3f7c855ff157998647b80ba095abc
kobelb <brandon.kobel@elastic.co>
2018-05-25 11:48:14 -0400

    Changing more usages of kbnTestServer.createServer

--- src/server/config/__tests__/fixtures/run_kbn_server_startup.js
+++ src/server/config/__tests__/fixtures/run_kbn_server_startup.js
@@ -1,7 +1,7 @@
-import { createServer } from '../../../../test_utils/kbn_server';
+import { createServerWithCorePlugins } from '../../../../test_utils/kbn_server';
 
 (async function run() {
-  const server = createServer(JSON.parse(process.env.CREATE_SERVER_OPTS));
+  const server = createServerWithCorePlugins(JSON.parse(process.env.CREATE_SERVER_OPTS));
 
   // We just need the server to run through startup so that it will
   // log the deprecation messages. Once it has started up we close it
