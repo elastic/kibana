812c4fe2701f67363d32198c6cf00ac35972e721
Tyler Smalley <tyler.smalley@elastic.co>
2018-05-25 10:53:54 -0700

    Updates an assertion and removed irrelevant test
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- src/utils/__tests__/watch_stdio_for_line.js
+++ src/utils/__tests__/watch_stdio_for_line.js
@@ -1,5 +1,3 @@
-import { resolve } from 'path';
-
 import execa from 'execa';
 import stripAnsi from 'strip-ansi';
 import sinon from 'sinon';
@@ -19,7 +17,7 @@ describe('src/utils/watch_stdio_for_line', function () {
     await watchStdioForLine(proc, logFn);
 
     // log output of the process
-    sinon.assert.calledWithExactly(onLogLine, sinon.match(/info\s+hi/));
+    sinon.assert.calledWithExactly(onLogLine, sinon.match(/hi/));
   });
 
   it('send the proc SIGKILL if it logs a line matching exitAfter regexp', async function () {
@@ -34,19 +32,5 @@ describe('src/utils/watch_stdio_for_line', function () {
 
     sinon.assert.calledWithExactly(onLogLine, sinon.match(/listening for SIGINT/));
     sinon.assert.neverCalledWith(onLogLine, sinon.match(/SIGINT not received/));
-  });
-
-  it('logs using level: option', async () => {
-    const parentDir = resolve(process.cwd(), '..');
-
-    const proc = execa(process.execPath, ['-e', 'console.log(process.cwd())'], {
-      cwd: parentDir,
-    });
-
-    await watchStdioForLine(proc, logFn);
-
-    // log output of the process, checking for \n to ensure cwd() doesn't log
-    // the subdir that this process is executing in
-    sinon.assert.calledWithExactly(onLogLine, sinon.match(parentDir + '\n'));
   });
 });
