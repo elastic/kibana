e9d23a260045505fb21c6b6f45bc69bf142e544c
Shaunak Kashyap <ycombinator@gmail.com>
2018-05-24 18:55:06 -0700

    Updating tests for changes to API

--- x-pack/test/api_integration/apis/beats/add_tags_to_beats.js
+++ x-pack/test/api_integration/apis/beats/add_tags_to_beats.js
@@ -23,22 +23,21 @@ export default function ({ getService }) {
     afterEach('unload beats archive', () => esArchiver.unload(archive));
 
     it('should add a single tag to a single beat', async () => {
-      const tags = [ chance.word() ];
       const { body: apiResponse } = await supertest
         .post(
-          '/api/beats/beats_tags'
+          '/api/beats/agents_tags'
         )
         .set('kbn-xsrf', 'xxx')
         .send({
-          beat_ids: {
-            bar: { tags }
-          }
+          additions: [
+            { beat_id: 'bar', tag: 'production' }
+          ]
         })
         .expect(200);
 
-      expect(apiResponse.beat_ids).to.eql({
-        bar: { status: 200, result: 'added' }
-      });
+      expect(apiResponse.additions).to.eql([
+        { status: 200, result: 'updated' }
+      ]);
 
       const esResponse = await es.get({
         index: ES_INDEX_NAME,
@@ -47,26 +46,245 @@ export default function ({ getService }) {
       });
 
       const beat = esResponse._source.beat;
+      expect(beat.tags).to.eql(['production']);
+    });
 
+    it('should not re-add an existing tag to a beat', async () => {
+      const tags = ['production'];
+
+      let esResponse;
+      let beat;
+
+      // Before adding the existing tag
+      esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:foo`
+      });
+
+      beat = esResponse._source.beat;
+      expect(beat.tags).to.eql(tags);
+
+      // Adding the existing tag
+      const { body: apiResponse } = await supertest
+        .post(
+          '/api/beats/agents_tags'
+        )
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          additions: [
+            { beat_id: 'foo', tag: 'production' }
+          ]
+        })
+        .expect(200);
+
+      expect(apiResponse.additions).to.eql([
+        { status: 200, result: 'updated' }
+      ]);
+
+      // After adding the existing tag
+      esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:foo`
+      });
+
+      beat = esResponse._source.beat;
       expect(beat.tags).to.eql(tags);
     });
 
     it('should add a single tag to a multiple beats', async () => {
+      const { body: apiResponse } = await supertest
+        .post(
+          '/api/beats/agents_tags'
+        )
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          additions: [
+            { beat_id: 'foo', tag: 'development' },
+            { beat_id: 'bar', tag: 'development' }
+          ]
+        })
+        .expect(200);
+
+      expect(apiResponse.additions).to.eql([
+        { status: 200, result: 'updated' },
+        { status: 200, result: 'updated' }
+      ]);
+
+      let esResponse;
+      let beat;
+
+      // Beat foo
+      esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:foo`
+      });
+
+      beat = esResponse._source.beat;
+      expect(beat.tags).to.eql(['production', 'development']); // as beat 'foo' already had 'production' tag attached to it
+
+      // Beat bar
+      esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:bar`
+      });
+
+      beat = esResponse._source.beat;
+      expect(beat.tags).to.eql(['development']);
     });
 
     it('should add multiple tags to a single beat', async () => {
+      const { body: apiResponse } = await supertest
+        .post(
+          '/api/beats/agents_tags'
+        )
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          additions: [
+            { beat_id: 'bar', tag: 'development' },
+            { beat_id: 'bar', tag: 'production' }
+          ]
+        })
+        .expect(200);
+
+      expect(apiResponse.additions).to.eql([
+        { status: 200, result: 'updated' },
+        { status: 200, result: 'updated' }
+      ]);
+
+      const esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:bar`
+      });
+
+      const beat = esResponse._source.beat;
+      expect(beat.tags).to.eql(['development', 'production']);
     });
 
     it('should add multiple tags to a multiple beats', async () => {
-    });
+      const { body: apiResponse } = await supertest
+        .post(
+          '/api/beats/agents_tags'
+        )
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          additions: [
+            { beat_id: 'foo', tag: 'development' },
+            { beat_id: 'bar', tag: 'production' }
+          ]
+        })
+        .expect(200);
 
-    it('should not re-add an existing tag to a beat', async () => {
+      expect(apiResponse.additions).to.eql([
+        { status: 200, result: 'updated' },
+        { status: 200, result: 'updated' }
+      ]);
+
+      let esResponse;
+      let beat;
+
+      // Beat foo
+      esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:foo`
+      });
+
+      beat = esResponse._source.beat;
+      expect(beat.tags).to.eql(['production', 'development']); // as beat 'foo' already had 'production' tag attached to it
+
+      // Beat bar
+      esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:bar`
+      });
+
+      beat = esResponse._source.beat;
+      expect(beat.tags).to.eql(['production']);
     });
 
     it('should return errors for non-existent beats', async () => {
+      const nonExistentBeatId = chance.word();
+
+      const { body: apiResponse } = await supertest
+        .post(
+          '/api/beats/agents_tags'
+        )
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          additions: [
+            { beat_id: nonExistentBeatId, tag: 'production' }
+          ]
+        })
+        .expect(200);
+
+      expect(apiResponse.additions).to.eql([
+        { status: 404, result: `Beat ${nonExistentBeatId} not found` }
+      ]);
     });
 
     it('should return errors for non-existent tags', async () => {
+      const nonExistentTag = chance.word();
+
+      const { body: apiResponse } = await supertest
+        .post(
+          '/api/beats/agents_tags'
+        )
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          additions: [
+            { beat_id: 'bar', tag: nonExistentTag }
+          ]
+        })
+        .expect(200);
+
+      expect(apiResponse.additions).to.eql([
+        { status: 404, result: `Tag ${nonExistentTag} not found` }
+      ]);
+
+      const esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:bar`
+      });
+
+      const beat = esResponse._source.beat;
+      expect(beat).to.not.have.property('tags');
+    });
+
+    it('should return errors for non-existent beats and tags', async () => {
+      const nonExistentBeatId = chance.word();
+      const nonExistentTag = chance.word();
+
+      const { body: apiResponse } = await supertest
+        .post(
+          '/api/beats/agents_tags'
+        )
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          additions: [
+            { beat_id: nonExistentBeatId, tag: nonExistentTag }
+          ]
+        })
+        .expect(200);
+
+      expect(apiResponse.additions).to.eql([
+        { status: 404, result: `Beat ${nonExistentBeatId} and tag ${nonExistentTag} not found` }
+      ]);
+
+      const esResponse = await es.get({
+        index: ES_INDEX_NAME,
+        type: ES_TYPE_NAME,
+        id: `beat:bar`
+      });
+
+      const beat = esResponse._source.beat;
+      expect(beat).to.not.have.property('tags');
     });
   });
 }
