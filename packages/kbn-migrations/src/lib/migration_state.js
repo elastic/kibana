17158a6405dd88014e1d5cafc95f515c6e2ea36a
Christopher Davies <github@christophilus.com>
2018-05-29 11:30:03 -0400

    Add Elastic license comment to migration source files

--- packages/kbn-migrations/src/lib/migration_state.js
+++ packages/kbn-migrations/src/lib/migration_state.js
@@ -1,3 +1,21 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 // Migration state contains all of the data we need to persist in an index
 // in order to know: 1. do we need to migrate? 2. what migrations
 // and mappings have already been applied? 3. mapping info for disabled plugins
