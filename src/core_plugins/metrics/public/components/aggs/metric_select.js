7aebb3bf9b24ae8afe9084ab80b93063b1bf947b
Nathan Reese <reese.nathan@gmail.com>
2018-05-30 09:21:38 -0600

    Remove react-select from kibana (#18876)
    * remove react-select from AggSelect
    
    * update field_select to use EuiComboBox
    
    * metric_select
    
    * moving_average
    
    * percentile
    
    * series_agg
    
    * std_deviation
    
    * removing some more react-select instances
    
    * icon_select and group_by_select
    
    * gauge type
    
    * markdown
    
    * set isClearable prop
    
    * remove react-select from timeseries config
    
    * remove react-select from timeseries panel options
    
    * remove react select from terms
    
    * remove react-select from table config
    
    * remove react-select from data_format_picker
    
    * fix create_select_handler mocha test
    
    * remove react-select from kibana
    
    * update tsvb functional tests
    
    * set isDisabled prop on EuiComboBox where disabld prop was set for Select
    
    * use durationOutputOptions for duration 'to' options

--- src/core_plugins/metrics/public/components/aggs/metric_select.js
+++ src/core_plugins/metrics/public/components/aggs/metric_select.js
@@ -20,7 +20,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import _ from 'lodash';
-import Select from 'react-select';
+import {
+  EuiComboBox,
+} from '@elastic/eui';
 import calculateSiblings from '../lib/calculate_siblings';
 import calculateLabel from '../../../common/calculate_label';
 import basicAggs from '../../../common/basic_aggs';
@@ -86,14 +88,20 @@ function MetricSelect(props) {
     const label = calculateLabel(row, metrics);
     return { value: row.id, label };
   });
+  const allOptions = [...options, ...props.additionalOptions, ...percentileOptions];
+
+  const selectedOption = allOptions.find(option => {
+    return value === option.value;
+  });
+  const selectedOptions = selectedOption ? [selectedOption] : [];
 
   return (
-    <Select
-      aria-label="Select metric"
+    <EuiComboBox
       placeholder="Select metric..."
-      options={[...options, ...props.additionalOptions, ...percentileOptions]}
-      value={value}
+      options={allOptions}
+      selectedOptions={selectedOptions}
       onChange={onChange}
+      singleSelection={true}
     />
   );
 }
