e1791b457cffcb7ad04b50144fa3796b1d87afa1
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-05-29 20:47:30 -0400

    bringing console in line with Kibana eslint rules (#19438)

--- src/core_plugins/console/public/src/sense_editor/row_parser.js
+++ src/core_plugins/console/public/src/sense_editor/row_parser.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-let MODE = {
+const MODE = {
   REQUEST_START: 2,
   IN_REQUEST: 4,
   MULTI_DOC_CUR_DOC_END: 8,
@@ -27,36 +27,36 @@ let MODE = {
 };
 
 function RowParser(editor) {
-  var defaultEditor = editor;
+  const defaultEditor = editor;
 
   this.getRowParseMode = function (row) {
-    if (row == null || typeof row == "undefined") {
+    if (row === null || typeof row === 'undefined') {
       row = editor.getCursorPosition().row;
     }
 
-    var session = editor.getSession();
+    const session = editor.getSession();
     if (row >= session.getLength() || row < 0) {
       return MODE.BETWEEN_REQUESTS;
     }
-    var mode = session.getState(row);
+    const mode = session.getState(row);
     if (!mode) {
       return MODE.BETWEEN_REQUESTS;
     } // shouldn't really happen
 
-    if (mode !== "start") {
+    if (mode !== 'start') {
       return MODE.IN_REQUEST;
     }
-    var line = (session.getLine(row) || "").trim();
+    let line = (session.getLine(row) || '').trim();
     if (!line || line[0] === '#') {
       return MODE.BETWEEN_REQUESTS;
     } // empty line or a comment waiting for a new req to start
 
-    if (line.indexOf("}", line.length - 1) >= 0) {
+    if (line.indexOf('}', line.length - 1) >= 0) {
       // check for a multi doc request (must start a new json doc immediately after this one end.
       row++;
       if (row < session.getLength()) {
-        line = (session.getLine(row) || "").trim();
-        if (line.indexOf("{") === 0) { // next line is another doc in a multi doc
+        line = (session.getLine(row) || '').trim();
+        if (line.indexOf('{') === 0) { // next line is another doc in a multi doc
           return MODE.MULTI_DOC_CUR_DOC_END | MODE.IN_REQUEST;
         }
 
@@ -69,8 +69,8 @@ function RowParser(editor) {
     if (row >= session.getLength()) {
       return MODE.REQUEST_START | MODE.REQUEST_END;
     }
-    line = (session.getLine(row) || "").trim();
-    if (line.indexOf("{") !== 0) { // next line is another request
+    line = (session.getLine(row) || '').trim();
+    if (line.indexOf('{') !== 0) { // next line is another request
       return MODE.REQUEST_START | MODE.REQUEST_END;
     }
 
@@ -78,59 +78,59 @@ function RowParser(editor) {
   };
 
   this.rowPredicate = function (row, editor, value) {
-    var mode = this.getRowParseMode(row, editor);
+    const mode = this.getRowParseMode(row, editor);
     return (mode & value) > 0;
   };
 
   this.isEndRequestRow = function (row, _e) {
-    var editor = _e || defaultEditor;
+    const editor = _e || defaultEditor;
     return this.rowPredicate(row, editor, MODE.REQUEST_END);
   };
 
   this.isRequestEdge = function (row, _e) {
-    var editor = _e || defaultEditor;
+    const editor = _e || defaultEditor;
     return this.rowPredicate(row, editor, MODE.REQUEST_END | MODE.REQUEST_START);
   };
 
   this.isStartRequestRow = function (row, _e) {
-    var editor = _e || defaultEditor;
+    const editor = _e || defaultEditor;
     return this.rowPredicate(row, editor, MODE.REQUEST_START);
   };
 
   this.isInBetweenRequestsRow = function (row, _e) {
-    var editor = _e || defaultEditor;
+    const editor = _e || defaultEditor;
     return this.rowPredicate(row, editor, MODE.BETWEEN_REQUESTS);
   };
 
   this.isInRequestsRow = function (row, _e) {
-    var editor = _e || defaultEditor;
+    const editor = _e || defaultEditor;
     return this.rowPredicate(row, editor, MODE.IN_REQUEST);
   };
 
   this.isMultiDocDocEndRow = function (row, _e) {
-    var editor = _e || defaultEditor;
+    const editor = _e || defaultEditor;
     return this.rowPredicate(row, editor, MODE.MULTI_DOC_CUR_DOC_END);
   };
 
   this.isEmptyToken = function (tokenOrTokenIter) {
-    var token = tokenOrTokenIter && tokenOrTokenIter.getCurrentToken ? tokenOrTokenIter.getCurrentToken() : tokenOrTokenIter;
-    return !token || token.type == "whitespace"
+    const token = tokenOrTokenIter && tokenOrTokenIter.getCurrentToken ? tokenOrTokenIter.getCurrentToken() : tokenOrTokenIter;
+    return !token || token.type === 'whitespace';
   };
 
   this.isUrlOrMethodToken = function (tokenOrTokenIter) {
-    var t = tokenOrTokenIter.getCurrentToken ? tokenOrTokenIter.getCurrentToken() : tokenOrTokenIter;
-    return t && t.type && (t.type == "method" || t.type.indexOf("url") === 0);
+    const t = tokenOrTokenIter.getCurrentToken ? tokenOrTokenIter.getCurrentToken() : tokenOrTokenIter;
+    return t && t.type && (t.type === 'method' || t.type.indexOf('url') === 0);
   };
 
 
   this.nextNonEmptyToken = function (tokenIter) {
-    var t = tokenIter.stepForward();
+    let t = tokenIter.stepForward();
     while (t && this.isEmptyToken(t)) t = tokenIter.stepForward();
     return t;
   };
 
   this.prevNonEmptyToken = function (tokenIter) {
-    var t = tokenIter.stepBackward();
+    let t = tokenIter.stepBackward();
     // empty rows return null token.
     while ((t || tokenIter.getCurrentTokenRow() > 0) && this.isEmptyToken(t)) t = tokenIter.stepBackward();
     return t;
