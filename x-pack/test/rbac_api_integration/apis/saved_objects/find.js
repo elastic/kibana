b67cdeecafe096e319dcd13649aa1232b82205b5
kobelb <brandon.kobel@elastic.co>
2018-05-31 09:32:59 -0400

    Porting over the saved objects tests, a bunch are failing, I believe because security is preventing the requests

---
+++ x-pack/test/rbac_api_integration/apis/saved_objects/find.js
@@ -0,0 +1,163 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from 'expect.js';
+
+export default function ({ getService }) {
+  const supertest = getService('supertest');
+  const es = getService('es');
+  const esArchiver = getService('esArchiver');
+
+  describe('find', () => {
+    describe('with kibana index', () => {
+      before(() => esArchiver.load('saved_objects/basic'));
+      after(() => esArchiver.unload('saved_objects/basic'));
+
+      it('should return 200 with individual responses', async () => (
+        await supertest
+          .get('/api/saved_objects/_find?type=visualization&fields=title')
+          .expect(200)
+          .then(resp => {
+            expect(resp.body).to.eql({
+              page: 1,
+              per_page: 20,
+              total: 1,
+              saved_objects: [
+                {
+                  type: 'visualization',
+                  id: 'dd7caf20-9efd-11e7-acb3-3dab96693fab',
+                  version: 1,
+                  attributes: {
+                    'title': 'Count of requests'
+                  }
+                }
+              ]
+            });
+          })
+      ));
+
+      describe('unknown type', () => {
+        it('should return 200 with empty response', async () => (
+          await supertest
+            .get('/api/saved_objects/_find?type=wigwags')
+            .expect(200)
+            .then(resp => {
+              expect(resp.body).to.eql({
+                page: 1,
+                per_page: 20,
+                total: 0,
+                saved_objects: []
+              });
+            })
+        ));
+      });
+
+      describe('page beyond total', () => {
+        it('should return 200 with empty response', async () => (
+          await supertest
+            .get('/api/saved_objects/_find?type=visualization&page=100&per_page=100')
+            .expect(200)
+            .then(resp => {
+              expect(resp.body).to.eql({
+                page: 100,
+                per_page: 100,
+                total: 1,
+                saved_objects: []
+              });
+            })
+        ));
+      });
+
+      describe('unknown search field', () => {
+        it('should return 200 with empty response', async () => (
+          await supertest
+            .get('/api/saved_objects/_find?type=wigwags&search_fields=a')
+            .expect(200)
+            .then(resp => {
+              expect(resp.body).to.eql({
+                page: 1,
+                per_page: 20,
+                total: 0,
+                saved_objects: []
+              });
+            })
+        ));
+      });
+    });
+
+    describe('without kibana index', () => {
+      before(async () => (
+        // just in case the kibana server has recreated it
+        await es.indices.delete({
+          index: '.kibana',
+          ignore: [404],
+        })
+      ));
+
+      it('should return 200 with empty response', async () => (
+        await supertest
+          .get('/api/saved_objects/_find?type=visualization')
+          .expect(200)
+          .then(resp => {
+            expect(resp.body).to.eql({
+              page: 1,
+              per_page: 20,
+              total: 0,
+              saved_objects: []
+            });
+          })
+      ));
+
+      describe('unknown type', () => {
+        it('should return 200 with empty response', async () => (
+          await supertest
+            .get('/api/saved_objects/_find?type=wigwags')
+            .expect(200)
+            .then(resp => {
+              expect(resp.body).to.eql({
+                page: 1,
+                per_page: 20,
+                total: 0,
+                saved_objects: []
+              });
+            })
+        ));
+      });
+
+      describe('page beyond total', () => {
+        it('should return 200 with empty response', async () => (
+          await supertest
+            .get('/api/saved_objects/_find?type=visualization&page=100&per_page=100')
+            .expect(200)
+            .then(resp => {
+              expect(resp.body).to.eql({
+                page: 100,
+                per_page: 100,
+                total: 0,
+                saved_objects: []
+              });
+            })
+        ));
+      });
+
+      describe('unknown search field', () => {
+        it('should return 200 with empty response', async () => (
+          await supertest
+            .get('/api/saved_objects/_find?type=wigwags&search_fields=a')
+            .expect(200)
+            .then(resp => {
+              expect(resp.body).to.eql({
+                page: 1,
+                per_page: 20,
+                total: 0,
+                saved_objects: []
+              });
+            })
+        ));
+      });
+    });
+  });
+}
---
+++ x-pack/test/rbac_api_integration/config.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import path from 'path';
+import { resolveKibanaPath } from '@kbn/plugin-helpers';
+
+export default async function ({ readConfigFile }) {
+
+  const config = {
+    kibana: {
+      api: await readConfigFile(resolveKibanaPath('test/api_integration/config.js')),
+      functional: await readConfigFile(require.resolve('../../../test/functional/config.js'))
+    },
+    xpack: {
+      api: await readConfigFile(require.resolve('../api_integration/config.js'))
+    }
+  };
+
+  return {
+    testFiles: [require.resolve('./apis')],
+    servers: config.xpack.api.get('servers'),
+    services: {
+      es: config.kibana.functional.get('services.es'),
+      supertest: config.kibana.api.get('services.supertest'),
+      esArchiver: config.kibana.functional.get('services.esArchiver'),
+    },
+    junit: {
+      reportName: 'X-Pack RBAC API Integration Tests',
+    },
+
+    esArchiver: {
+      directory: resolveKibanaPath(path.join('test', 'api_integration', 'fixtures', 'es_archiver'))
+    },
+
+    esTestCluster: {
+      ...config.xpack.api.get('esTestCluster'),
+      serverArgs: [
+        ...config.xpack.api.get('esTestCluster.serverArgs'),
+      ],
+    },
+
+    kbnTestServer: {
+      ...config.xpack.api.get('kbnTestServer'),
+      serverArgs: [
+        ...config.xpack.api.get('kbnTestServer.serverArgs'),
+        '--optimize.enabled=false',
+        // '--xpack.security.rbac.enabled=true'
+      ],
+    },
+  };
+}
