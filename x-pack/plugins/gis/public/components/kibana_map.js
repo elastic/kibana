05c9f1c703cfe8f23f30c644b05e31df163fa73a
Aaron Caldwell <aaron.caldwell@elastic.co>
2018-06-01 15:48:49 -0600

    Flyout placeholder logic embedded in kibana map component

--- x-pack/plugins/gis/public/components/kibana_map.js
+++ x-pack/plugins/gis/public/components/kibana_map.js
@@ -7,9 +7,18 @@
 import eventEmitter from 'event-emitter';
 import React from 'react';
 import * as ol from 'openlayers';
-
-import {} from '@elastic/eui';
-
+import {
+  EuiFlyout,
+  EuiFlyoutBody,
+  EuiText,
+  EuiHorizontalRule,
+  EuiAccordion,
+  EuiSpacer,
+  EuiFlyoutHeader,
+  EuiTitle,
+  EuiTextColor,
+} from '@elastic/eui';
+import IndexPatternImport from './index_pattern_import';
 export class KibanaMap extends React.Component {
 
   constructor() {
@@ -41,9 +50,65 @@ export class KibanaMap extends React.Component {
   }
 
   render() {
+    const handlePreviewLayer = ()=> console.log("Handle preview layer placeholder");
     return (
       <div>
         <div className="mapContainer" ref="mapContainer"/>
+        <EuiFlyout onClose={this.props.onClose} style={{ maxWidth: 768 }}>
+          <EuiFlyoutHeader>
+            <EuiTitle size="l">
+              <h2>Add layer</h2>
+            </EuiTitle>
+            <EuiSpacer size="m"/>
+            <EuiTextColor color="subdued">
+              <EuiText size="s">
+                <p>Choose a source from one of the following options, then click Add to map to continue.</p>
+              </EuiText>
+            </EuiTextColor>
+            <EuiSpacer />
+            <EuiHorizontalRule margin="none"/>
+          </EuiFlyoutHeader>
+
+          <EuiFlyoutBody style={{ paddingTop: 0 }}>
+            <EuiAccordion
+              id="addIndexPattern"
+              className="euiAccordionForm"
+              buttonClassName="euiAccordionForm__button"
+              buttonContent="From Elasticsearch index"
+              initialIsOpen={true}
+              ref={(ref)=>this._ipAccordion = ref}
+              onClick={this._onIPClick}
+            >
+              <div className="euiAccordionForm__children">
+                <IndexPatternImport kibanaMap={this._kibanaMap} onPreviewLayer={handlePreviewLayer}/>
+              </div>
+            </EuiAccordion>
+
+            <EuiAccordion
+              id="addEMS"
+              className="euiAccordionForm"
+              buttonClassName="euiAccordionForm__button"
+              buttonContent="Import from Elastic Maps Service"
+              initialIsOpen={false}
+              ref={(ref)=>this._emsAccordion = ref}
+              onClick={this._onEMSClick}
+            >
+              <div className="euiAccordionForm__children"/>
+            </EuiAccordion>
+
+            <EuiAccordion
+              id="addFile"
+              className="euiAccordionForm"
+              buttonClassName="euiAccordionForm__button"
+              buttonContent="Import from local file"
+              initialIsOpen={false}
+              ref={(ref)=>this._fileAccordion = ref}
+              onClick={this._onFileClick}
+            >
+              <div className="euiAccordionForm__children"/>
+            </EuiAccordion>
+          </EuiFlyoutBody>
+        </EuiFlyout>
       </div>
     );
   }
