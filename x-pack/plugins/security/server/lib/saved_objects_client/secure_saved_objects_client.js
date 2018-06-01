d8d98103a9b073504b46a29be28806236bb6a4fb
Brandon Kobel <brandon.kobel@gmail.com>
2018-06-01 08:00:04 -0400

    RBAC - SecurityAuditLogger (#19571)
    * Manually porting over the AuditLogger for use within the security audit
    logger
    
    * HasPrivileges now returns the user from the request
    
    * Has privileges returns username from privilegeCheck
    
    * Adding first eventType to the security audit logger
    
    * Adding authorization success message
    
    * Logging arguments when authorization success
    
    * Fixing test description
    
    * Logging args during audit failures

--- x-pack/plugins/security/server/lib/saved_objects_client/secure_saved_objects_client.js
+++ x-pack/plugins/security/server/lib/saved_objects_client/secure_saved_objects_client.js
@@ -12,60 +12,83 @@ export class SecureSavedObjectsClient {
       request,
       hasPrivilegesWithRequest,
       baseClient,
+      auditLogger,
     } = options;
 
     this.errors = baseClient.errors;
 
     this._client = baseClient;
     this._hasPrivileges = hasPrivilegesWithRequest(request);
+    this._auditLogger = auditLogger;
   }
 
   async create(type, attributes = {}, options = {}) {
-    await this._performAuthorizationCheck(type, 'create');
+    await this._performAuthorizationCheck(type, 'create', {
+      type,
+      attributes,
+      options,
+    });
 
     return await this._client.create(type, attributes, options);
   }
 
   async bulkCreate(objects, options = {}) {
     const types = uniq(objects.map(o => o.type));
-    await this._performAuthorizationCheck(types, 'create');
+    await this._performAuthorizationCheck(types, 'create', {
+      objects,
+      options,
+    });
 
     return await this._client.bulkCreate(objects, options);
   }
 
   async delete(type, id) {
-    await this._performAuthorizationCheck(type, 'delete');
+    await this._performAuthorizationCheck(type, 'delete', {
+      type,
+      id,
+    });
 
     return await this._client.delete(type, id);
   }
 
   async find(options = {}) {
-    await this._performAuthorizationCheck(options.type, 'search');
+    await this._performAuthorizationCheck(options.type, 'search', {
+      options,
+    });
 
     return await this._client.find(options);
   }
 
   async bulkGet(objects = []) {
-    for (const object of objects) {
-      await this._performAuthorizationCheck(object.type, 'mget');
-    }
+    const types = uniq(objects.map(o => o.type));
+    await this._performAuthorizationCheck(types, 'mget', {
+      objects,
+    });
 
     return await this._client.bulkGet(objects);
   }
 
   async get(type, id) {
-    await this._performAuthorizationCheck(type, 'get');
+    await this._performAuthorizationCheck(type, 'get', {
+      type,
+      id,
+    });
 
     return await this._client.get(type, id);
   }
 
   async update(type, id, attributes, options = {}) {
-    await this._performAuthorizationCheck(type, 'update');
+    await this._performAuthorizationCheck(type, 'update', {
+      type,
+      id,
+      attributes,
+      options,
+    });
 
     return await this._client.update(type, id, attributes, options);
   }
 
-  async _performAuthorizationCheck(typeOrTypes, action) {
+  async _performAuthorizationCheck(typeOrTypes, action, args) {
     const types = Array.isArray(typeOrTypes) ? typeOrTypes : [typeOrTypes];
     const actions = types.map(type => `action:saved-objects/${type}/${action}`);
 
@@ -77,7 +100,10 @@ export class SecureSavedObjectsClient {
       throw this._client.errors.decorateGeneralError(error, reason);
     }
 
-    if (!result.success) {
+    if (result.success) {
+      this._auditLogger.savedObjectsAuthorizationSuccess(result.username, action, types, args);
+    } else {
+      this._auditLogger.savedObjectsAuthorizationFailure(result.username, action, types, result.missing, args);
       const msg = `Unable to ${action} ${types.join(',')}, missing ${result.missing.join(',')}`;
       throw this._client.errors.decorateForbiddenError(new Error(msg));
     }
