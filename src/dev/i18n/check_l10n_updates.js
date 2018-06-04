17fc5500c5868acd352fd802f0ecb08222b10f29
Leanid Shutau <leanidshutau@gmail.com>
2018-06-04 16:46:43 +0300

    Implement a tool for checking l10n updates

---
+++ src/dev/i18n/check_l10n_updates.js
@@ -0,0 +1,73 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import path from 'path';
+
+import {
+  arraysDiff,
+  plainify,
+  readFileAsync,
+  touchPath,
+  writeFileAsync,
+} from './utils';
+
+export async function checkUpdates(pluginPath, log) {
+  const defaultMessagesBuffer = await readFileAsync(
+    path.resolve(pluginPath, 'translations/defaultMessages.json')
+  );
+  let messagesCacheBuffer;
+
+  try {
+    const resolvedPath = path.resolve(
+      pluginPath,
+      'translations/messagesCache.json'
+    );
+    await touchPath(resolvedPath);
+    messagesCacheBuffer = await readFileAsync(resolvedPath);
+  } catch (_) {
+    messagesCacheBuffer = new Buffer('[]');
+  }
+
+  const defaultMessagesIds = Object.keys(
+    plainify(JSON.parse(defaultMessagesBuffer.toString()))
+  );
+  const cachedMessagesIds = JSON.parse(messagesCacheBuffer.toString());
+
+  const [addedMessages, removedMessages] = arraysDiff(
+    defaultMessagesIds,
+    cachedMessagesIds
+  );
+
+  if (addedMessages.length > 0) {
+    log.success(
+      `New messages ids in ${pluginPath}:\n${addedMessages.join(', ')}`
+    );
+  }
+
+  if (removedMessages.length > 0) {
+    log.success(
+      `Removed messages ids in ${pluginPath}:\n${removedMessages.join(', ')}`
+    );
+  }
+
+  await writeFileAsync(
+    path.resolve(pluginPath, 'translations/messagesCache.json'),
+    JSON.stringify(defaultMessagesIds, null, 2)
+  );
+}
