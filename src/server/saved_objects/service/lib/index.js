a5b986c145b756349022e99049ca08677ec2b640
Brandon Kobel <brandon.kobel@gmail.com>
2018-06-04 17:02:25 -0400

    SavedObjects service and client provider (#19349)
    * Renaming client to service
    
    * Implementing the SavedObjects service and client provider
    
    * Making saved objects service lazy instantiated so the test server works
    
    * Fixing import
    
    * Fixing another import
    
    * Fixing reporting's usage of the savedObjectsClientFactory
    
    * No longer passing the callCluster function to the kbnTestServer
    
    * Passing empty request...
    
    * Fixing reporting
    
    * Exposing server.savedObjects without a getter
    
    This required me to remove the kbnTestServer.createServer() method and
    change all usages to createServerWithCorePlugins since we're reading the
    kibana.index, which is part of the core plugins, when configuring the
    saved objects service which is attached to the server.
    
    * Don't need to set kibana.index
    
    * Changing more usages of kbnTestServer.createServer
    
    * Revert "Changing more usages of kbnTestServer.createServer"
    
    This reverts commit 464f73abb2a3f7c855ff157998647b80ba095abc.
    
    * Partially reverting 17d36b3e9a9525babb324656886e6892157a7442
    
    * Fixing headers
    
    * Improving tests
    
    * Addressing some peer review feedback
    
    * Addressing more peer review feedback
    
    * Adding server.log warning when the saved objects can't initialize
    
    * Getting rid of the savedObjectsClientFactory
    
    * Branches are for trees
    
    * Fixing comment to be proper english
    
    * Fixing fat-fingering method call
    
    * Fixing other usages of getScopedSavedObjectsClient

--- src/server/saved_objects/service/lib/index.js
+++ src/server/saved_objects/service/lib/index.js
@@ -18,6 +18,7 @@
  */
 
 export { SavedObjectsRepository } from './repository';
+export { ScopedSavedObjectsClientProvider } from './scoped_client_provider';
 
 import * as errors from './errors';
 export { errors };
---
+++ src/server/saved_objects/service/lib/scoped_client_provider.js
@@ -0,0 +1,74 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Provider for the Scoped Saved Object Client.
+ */
+export class ScopedSavedObjectsClientProvider {
+
+  _wrapperFactories = [];
+
+  constructor({
+    index,
+    mappings,
+    onBeforeWrite,
+    defaultClientFactory
+  }) {
+    this._index = index;
+    this._mappings = mappings;
+    this._onBeforeWrite = onBeforeWrite;
+    this._originalClientFactory = this._clientFactory = defaultClientFactory;
+  }
+
+  // the client wrapper factories are put at the front of the array, so that
+  // when we use `reduce` below they're invoked in LIFO order. This is so that
+  // if multiple plugins register their client wrapper factories, then we can use
+  // the plugin dependencies/optionalDependencies to implicitly control the order
+  // in which these are used. For example, if we have a plugin a that declares a
+  // dependency on plugin b, that means that plugin b's client wrapper would want
+  // to be able to run first when the SavedObjectClient methods are invoked to
+  // provide additional context to plugin a's client wrapper.
+  addClientWrapperFactory(wrapperFactory) {
+    this._wrapperFactories.unshift(wrapperFactory);
+  }
+
+  setClientFactory(customClientFactory) {
+    if (this._clientFactory !== this._originalClientFactory) {
+      throw new Error(`custom client factory is already set, unable to replace the current one`);
+    }
+
+    this._clientFactory = customClientFactory;
+  }
+
+  getClient(request) {
+    const client = this._clientFactory({
+      request,
+      index: this._index,
+      mappings: this._mappings,
+      onBeforeWrite: this._onBeforeWrite,
+    });
+
+    return this._wrapperFactories.reduce((clientToWrap, wrapperFactory) => {
+      return wrapperFactory({
+        request,
+        client: clientToWrap,
+      });
+    }, client);
+  }
+}
