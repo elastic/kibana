e00637344c8eb148cc933497e39cfaa379528e22
Stacey Gammon <gammon@elastic.co>
2018-06-05 13:58:54 -0400

    Typescriptify ui/embeddable folder (#19648)
    * Typescriptify ui/embeddable folder
    
    * Allow ts modules from ui/public folders to be imported
    
    * Address review comments from Tim
    
    * Address code review from Oleg
    
    * remove lodash usage
    
    * remove mappng to non public folder

--- src/ui/public/embeddable/embeddable_factory.ts
+++ src/ui/public/embeddable/embeddable_factory.ts
@@ -17,33 +17,24 @@
  * under the License.
  */
 
-/**
- * @typedef {Object} EmbeddableState
- * @property {Object} customization - any customization data that should be stored at the panel level. For
- * example, pie slice colors, or custom per panel sort order or columns.
- * @property {Object} stagedFilter - a possible filter the embeddable wishes dashboard to apply.
- */
-
-
-/**
- * @callback onEmbeddableStateChanged
- * @param {EmbeddableState} embeddableState
- */
+import { Embeddable } from './embeddable';
+import { EmbeddableState } from './types';
 
 /**
  * The EmbeddableFactory creates and initializes an embeddable instance
  */
-export class EmbeddableFactory {
+export abstract class EmbeddableFactory {
   /**
    *
-   * @param {Object} containerMetadata. Currently just passing in panelState but it's more than we need, so we should
+   * @param {{ id: string }} containerMetadata. Currently just passing in panelState but it's more than we need, so we should
    * decouple this to only include data given to us from the embeddable when it's added to the dashboard. Generally
    * will be just the object id, but could be anything depending on the plugin.
    * @param {onEmbeddableStateChanged} onEmbeddableStateChanged - embeddable should call this function with updated
    * state whenever something changes that the dashboard should know about.
    * @return {Promise.<Embeddable>}
    */
-  create(/* containerMetadata, onEmbeddableStateChanged*/) {
-    throw new Error('Must implement create.');
-  }
+  public abstract create(
+    containerMetadata: { id: string },
+    onEmbeddableStateChanged: (embeddableStateChanges: EmbeddableState) => void
+  ): Promise<Embeddable>;
 }
