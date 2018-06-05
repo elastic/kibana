d131af8da81d02373cc7a6d88949203808fa8067
Stacey Gammon <gammon@elastic.co>
2018-06-05 15:23:32 -0400

    Typescriptify ui/embeddable folder (#19648) (#19683)
    * Typescriptify ui/embeddable folder
    
    * Allow ts modules from ui/public folders to be imported
    
    * Address review comments from Tim
    
    * Address code review from Oleg
    
    * remove lodash usage
    
    * remove mappng to non public folder

--- src/ui/public/embeddable/index.ts
+++ src/ui/public/embeddable/index.ts
@@ -19,4 +19,6 @@
 
 export { EmbeddableFactory } from './embeddable_factory';
 export * from './embeddable';
-export { EmbeddableFactoriesRegistryProvider } from './embeddable_factories_registry';
+export {
+  EmbeddableFactoriesRegistryProvider,
+} from './embeddable_factories_registry';
