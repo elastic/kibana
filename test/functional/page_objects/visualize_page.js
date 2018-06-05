84d678bbb9c13966e9622df9244def57afac19f9
Bhavya RM <bhavya@elastic.co>
2018-06-05 19:14:29 -0400

    Tests for visualization screenshot comparison (#17545)
    * undoing a messy merge
    
    * updating screenshots
    
    * changing the variance to account for data table failure
    
    * trying a different variance for data table and a general one for the rest of the screenshots
    
    * changing the variance for general to .065
    
    * adding xy position to adjust the screensize
    
    * changing variance and setting a small window
    
    * create calibrateForScreenshots method
    
    * remove empty lines

--- test/functional/page_objects/visualize_page.js
+++ test/functional/page_objects/visualize_page.js
@@ -623,7 +623,7 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
     }
 
     async filterVisByName(vizName) {
-      const input = await find.byCssSelector('input[name="filter"]');
+      const input = await find.byCssSelector('input[aria-label="Filter"]');
       await input.click();
       // can't uses dashes in saved visualizations when filtering
       // or extended character sets
@@ -650,6 +650,7 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
     }
 
     async openSavedVisualization(vizName) {
+      await this.filterVisByName(vizName);
       await this.clickVisualizationByName(vizName);
     }
 
