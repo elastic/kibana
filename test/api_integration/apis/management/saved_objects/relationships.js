bf3517101afeac072e518a3edc7282a7cfd737b8
John Dorlus <jsdorlus@elastic.co>
2018-06-07 13:06:26 -0400

    Added tests to check for whether the resource is available. If not, return 404.

--- test/api_integration/apis/management/saved_objects/relationships.js
+++ test/api_integration/apis/management/saved_objects/relationships.js
@@ -24,6 +24,7 @@ const ajv = new Ajv();
 export default function ({ getService }) {
   const supertest = getService('supertest');
   const esArchiver = getService('esArchiver');
+  //TODO: Add pattern matching to ensure that the id is a UUID.
   const GENERIC_RESPONSE_SCHEMA = {
     type: 'array',
     items: {
@@ -96,7 +97,7 @@ export default function ({ getService }) {
       const DASHBOARD_RESPONSE_SCHEMA = {
         type: 'object',
         properties: {
-          dashboars: GENERIC_RESPONSE_SCHEMA,
+          visualizations: GENERIC_RESPONSE_SCHEMA,
         },
       };
 
@@ -131,49 +132,102 @@ export default function ({ getService }) {
           });
       });
 
-      it('should return 404 if search finds no results', async () => {
+      it('should return 404 if dashboard finds no results', async () => {
         await supertest
           .get(`/api/kibana/management/saved_objects/relationships/dashboard/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx`)
           .expect(404);
       });
     });
 
-    it('should work for visualizations', async () => {
-      await supertest
-        .get(`/api/kibana/management/saved_objects/relationships/visualization/a42c0580-3224-11e8-a572-ffca06da1357`)
-        .expect(200)
-        .then(resp => {
-          expect(resp.body).to.eql({
-            dashboards: [
-              {
-                id: 'b70c7ae0-3224-11e8-a572-ffca06da1357',
-                title: 'Dashboard',
-              },
-            ],
+    describe('visualizations', async () => {
+      const VISUALIZATIONS_RESPONSE_SCHEMA = {
+        type: 'object',
+        properties: {
+          visualizations: GENERIC_RESPONSE_SCHEMA,
+        },
+      };
+
+      const validate = ajv.compile(VISUALIZATIONS_RESPONSE_SCHEMA);
+      it('should validate visualization response schema', async () => {
+        await supertest
+          .get(`/api/kibana/management/saved_objects/relationships/visualization/a42c0580-3224-11e8-a572-ffca06da1357`)
+          .expect(200)
+          .then(resp => {
+            const validationResult = validate(resp.body);
+            expect(validationResult).to.eql(true);
           });
-        });
+      });
+
+      it('should work for visualizations', async () => {
+        await supertest
+          .get(`/api/kibana/management/saved_objects/relationships/visualization/a42c0580-3224-11e8-a572-ffca06da1357`)
+          .expect(200)
+          .then(resp => {
+            expect(resp.body).to.eql({
+              dashboards: [
+                {
+                  id: 'b70c7ae0-3224-11e8-a572-ffca06da1357',
+                  title: 'Dashboard',
+                },
+              ],
+            });
+          });
+      });
+
+      it('should return 404 if  visualizations finds no results', async () => {
+        await supertest
+          .get(`/api/kibana/management/saved_objects/relationships/visualization/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx`)
+          .expect(404);
+      });
     });
 
-    it('should work for index patterns', async () => {
-      await supertest
-        .get(`/api/kibana/management/saved_objects/relationships/index-pattern/8963ca30-3224-11e8-a572-ffca06da1357`)
-        .expect(200)
-        .then(resp => {
-          expect(resp.body).to.eql({
-            searches: [
-              {
-                id: '960372e0-3224-11e8-a572-ffca06da1357',
-                title: 'OneRecord',
-              },
-            ],
-            visualizations: [
-              {
-                id: 'add810b0-3224-11e8-a572-ffca06da1357',
-                title: 'Visualization',
-              },
-            ],
+    describe('index patterns', async () => {
+      const INDEX_PATTERN_RESPONSE_SCHEMA = {
+        type: 'object',
+        properties: {
+          searches: GENERIC_RESPONSE_SCHEMA,
+          visualizations: GENERIC_RESPONSE_SCHEMA,
+        },
+      };
+
+      const validate = ajv.compile(INDEX_PATTERN_RESPONSE_SCHEMA);
+      it('should validate visualization response schema', async () => {
+        await supertest
+          .get(`/api/kibana/management/saved_objects/relationships/index-pattern/8963ca30-3224-11e8-a572-ffca06da1357`)
+          .expect(200)
+          .then(resp => {
+            const validationResult = validate(resp.body);
+            expect(validationResult).to.eql(true);
           });
-        });
+      });
+
+      it('should work for index patterns', async () => {
+        await supertest
+          .get(`/api/kibana/management/saved_objects/relationships/index-pattern/8963ca30-3224-11e8-a572-ffca06da1357`)
+          .expect(200)
+          .then(resp => {
+            expect(resp.body).to.eql({
+              searches: [
+                {
+                  id: '960372e0-3224-11e8-a572-ffca06da1357',
+                  title: 'OneRecord',
+                },
+              ],
+              visualizations: [
+                {
+                  id: 'add810b0-3224-11e8-a572-ffca06da1357',
+                  title: 'Visualization',
+                },
+              ],
+            });
+          });
+      });
+
+      it('should return 404 if index pattern finds no results', async () => {
+        await supertest
+          .get(`/api/kibana/management/saved_objects/relationships/index-pattern/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx`)
+          .expect(404);
+      });
     });
   });
 }
