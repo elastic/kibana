b0f1305048eb25f53b5a60a33925dbd8b119112a
Jonathan Budzenski <jbudz@users.noreply.github.com>
2018-06-08 11:35:09 -0500

    [plugin installer] Allow x-pack removal if it exists (#19327)
    * [plugin installer] Allow x-pack removal if it exists
    
    * remove lingering exit

--- src/cli_plugin/lib/is_oss.test.js
+++ src/cli_plugin/lib/is_oss.test.js
@@ -17,14 +17,12 @@
  * under the License.
  */
 
-import expect from 'expect.js';
-
-import { isOSS } from '../is_oss';
+import { isOSS } from './is_oss';
 
 describe('is_oss', () => {
   describe('x-pack installed', () => {
     it('should return false', () => {
-      expect(isOSS()).to.be(false);
+      expect(isOSS()).toEqual(false);
     });
   });
 });
