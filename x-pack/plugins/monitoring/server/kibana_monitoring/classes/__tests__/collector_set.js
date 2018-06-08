1eb28de4d65b211bcc0c4b07a271ccd9f7467595
Timothy Sullivan <tsullivan@elastic.co>
2018-06-07 18:19:21 -0700

    fix collector_set tests

--- x-pack/plugins/monitoring/server/kibana_monitoring/classes/__tests__/collector_set.js
+++ x-pack/plugins/monitoring/server/kibana_monitoring/classes/__tests__/collector_set.js
@@ -10,24 +10,19 @@ import expect from 'expect.js';
 import { Collector } from '../collector';
 import { CollectorSet } from '../collector_set';
 
-const INFO_LOG = ['info', 'monitoring-ui', 'kibana-monitoring'];
-
 describe('CollectorSet', () => {
   describe('registers a collector set and runs lifecycle events', () => {
     let server;
     let init;
     let fetch;
     beforeEach(() => {
-      server = {
-        log: sinon.spy(),
-      };
+      server = { log: sinon.spy() };
       init = noop;
       fetch = noop;
     });
 
     it('should throw an error if non-Collector type of object is registered', () => {
       const collectors = new CollectorSet(server);
-
       const registerPojo = () => {
         collectors.register({
           type: 'type_collector_test',
@@ -37,27 +32,29 @@ describe('CollectorSet', () => {
       };
 
       expect(registerPojo).to.throwException(({ message }) => {
-        expect(message).to.be(
-          'CollectorSet can only have Collector instances registered'
-        );
+        expect(message).to.be('CollectorSet can only have Collector instances registered');
       });
     });
 
-    it('should log the info-level status of stopping and restarting', done => {
+    it('should log debug status of fetching from the collector', async () => {
+      const mockCallCluster = () => Promise.resolve({ passTest: 1000 });
       const collectors = new CollectorSet(server);
+      collectors.register(new Collector(server, {
+        type: 'MY_TEST_COLLECTOR',
+        fetch: caller => caller()
+      }));
 
-      collectors.register(new Collector(server));
-
-      collectors.start();
-      expect(
-        server.log.calledWith(INFO_LOG, 'Starting all stats collectors')
-      ).to.be(true);
-
-      collectors.start();
-      expect(
-        server.log.calledWith(INFO_LOG, 'Starting all stats collectors')
-      ).to.be(true);
-      done();
+      const result = await collectors.bulkFetch(mockCallCluster);
+      const calls = server.log.getCalls();
+      expect(calls.length).to.be(1);
+      expect(calls[0].args).to.eql([
+        ['debug', 'monitoring-ui', 'kibana-monitoring'],
+        'Fetching data from MY_TEST_COLLECTOR collector',
+      ]);
+      expect(result).to.eql([{
+        type: 'MY_TEST_COLLECTOR',
+        result: { passTest: 1000 }
+      }]);
     });
   });
 });
