7ce7de36f40334670499c84cb3918df99d975b52
kobelb <brandon.kobel@elastic.co>
2018-06-11 16:48:54 -0400

    Cleaning stuff up a bit

--- x-pack/plugins/security/server/lib/authorization/has_privileges.js
+++ x-pack/plugins/security/server/lib/authorization/has_privileges.js
@@ -8,35 +8,22 @@ import { getClient } from '../../../../../server/lib/get_client_shield';
 import { DEFAULT_RESOURCE } from '../../../common/constants';
 import { buildPrivilegeMap, getVersionPrivilege, getLoginPrivilege } from '../privileges';
 
-const getMissingPrivileges = (resource, application, privilegeCheck) => {
-  const privileges = privilegeCheck.application[application][resource];
-  return Object.keys(privileges).filter(key => privileges[key] === false);
-};
-
 const hasApplicationPrivileges = async (callWithRequest, request, kibanaVersion, application, privileges) => {
-  const versionPrivilege = getVersionPrivilege(kibanaVersion);
-  const loginPrivilege = getLoginPrivilege();
-
   const privilegeCheck = await callWithRequest(request, 'shield.hasPrivileges', {
     body: {
       applications: [{
         application,
         resources: [DEFAULT_RESOURCE],
-        privileges: [versionPrivilege, loginPrivilege, ...privileges]
+        privileges
       }]
     }
   });
 
-  const privilegesResult = privilegeCheck.application[application][DEFAULT_RESOURCE];
-
-  // We include the login privilege on all privileges, so the existence of it and not the version privilege
-  // lets us know that we're running in an incorrect configuration. Without the login privilege check, we wouldn't
-  // know whether the user just wasn't authorized for this instance of Kibana in general
-  if (!privilegesResult[versionPrivilege] && privilegesResult[loginPrivilege]) {
-    throw new Error('Multiple versions of Kibana are running against the same Elasticsearch cluster, unable to authorize user.');
-  }
-
-  return privilegeCheck;
+  return {
+    username: privilegeCheck.username,
+    hasAllRequested: privilegeCheck.has_all_requested,
+    privileges: privilegeCheck.application[application][DEFAULT_RESOURCE]
+  };
 };
 
 const hasLegacyPrivileges = async (callWithRequest, request, kibanaVersion, application, kibanaIndex, privileges) => {
@@ -52,16 +39,12 @@ const hasLegacyPrivileges = async (callWithRequest, request, kibanaVersion, appl
   if (privilegeCheck.index[kibanaIndex].index) {
     return {
       username: privilegeCheck.username,
-      has_all_requested: true,
-      application: {
-        [application]: {
-          [DEFAULT_RESOURCE]: {
-            ...privileges.reduce((acc, name) => {
-              acc[name] = true;
-              return acc;
-            }, {})
-          }
-        }
+      hasAllRequested: true,
+      privileges: {
+        ...privileges.reduce((acc, name) => {
+          acc[name] = true;
+          return acc;
+        }, {})
       }
     };
   }
@@ -72,32 +55,22 @@ const hasLegacyPrivileges = async (callWithRequest, request, kibanaVersion, appl
       acc[name] = privilegeMap[application].read.actions.includes(name);
       return acc;
     }, {});
+
     return {
       username: privilegeCheck.username,
-      has_all_requested: Object.values(implicitPrivileges).every(x => x),
-      application: {
-        [application]: {
-          [DEFAULT_RESOURCE]: implicitPrivileges
-        }
-      }
+      hasAllRequested: Object.values(implicitPrivileges).every(x => x),
+      privileges: implicitPrivileges,
     };
   }
 
   return {
     username: privilegeCheck.username,
-    has_all_requested: false,
-    application: {
-      [application]: {
-        [DEFAULT_RESOURCE]: {
-          ...privileges.reduce((acc, name) => {
-            acc[name] = false;
-            return acc;
-          }, {})
-        }
-      }
-    }
+    hasAllRequested: false,
+    privileges: privileges.reduce((acc, name) => {
+      acc[name] = false;
+      return acc;
+    }, {})
   };
-
 };
 
 export function hasPrivilegesWithServer(server) {
@@ -110,28 +83,39 @@ export function hasPrivilegesWithServer(server) {
 
   return function hasPrivilegesWithRequest(request) {
     return async function hasPrivileges(privileges) {
+      const loginPrivilege = getLoginPrivilege();
+      const versionPrivilege = getVersionPrivilege(kibanaVersion);
 
-      let privilegeCheck = await hasApplicationPrivileges(callWithRequest, request, kibanaVersion, application, privileges);
+      const allPrivileges = [...privileges, loginPrivilege, versionPrivilege];
+      let privilegesCheck = await hasApplicationPrivileges(callWithRequest, request, kibanaVersion, application, allPrivileges);
 
-      if (!privilegeCheck.application[application][DEFAULT_RESOURCE][getLoginPrivilege()]) {
-        privilegeCheck = await hasLegacyPrivileges(
+      if (!privilegesCheck.privileges[loginPrivilege]) {
+        privilegesCheck = await hasLegacyPrivileges(
           callWithRequest,
           request,
           kibanaVersion,
           application,
           kibanaIndex,
-          [...privileges, getLoginPrivilege()]
+          allPrivileges
         );
       }
 
-      const success = privilegeCheck.has_all_requested;
-      const missingPrivileges = getMissingPrivileges(DEFAULT_RESOURCE, application, privilegeCheck);
+      const success = privilegesCheck.hasAllRequested;
+
+      // We include the login privilege on all privileges, so the existence of it and not the version privilege
+      // lets us know that we're running in an incorrect configuration. Without the login privilege check, we wouldn't
+      // know whether the user just wasn't authorized for this instance of Kibana in general
+      if (!privilegesCheck.privileges[versionPrivilege] && privilegesCheck.privileges[loginPrivilege]) {
+        throw new Error('Multiple versions of Kibana are running against the same Elasticsearch cluster, unable to authorize user.');
+      }
 
       return {
         success,
         // We don't want to expose the version privilege to consumers, as it's an implementation detail only to detect version mismatch
-        missing: missingPrivileges.filter(p => p !== getVersionPrivilege(kibanaVersion)),
-        username: privilegeCheck.username,
+        missing: Object.keys(privilegesCheck.privileges)
+          .filter(key => privilegesCheck.privileges[key] === false)
+          .filter(p => p !== versionPrivilege),
+        username: privilegesCheck.username,
       };
     };
   };
