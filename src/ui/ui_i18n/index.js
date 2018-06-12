6f6aea7ca4b3f0cff5004d7f399a9dd88c7e3f5b
Maxim Tolochko <maksim.tolo@gmail.com>
2018-06-12 16:36:48 +0300

    Add default locale as fallback if translation files were not registered

--- src/ui/ui_i18n/index.js
+++ src/ui/ui_i18n/index.js
@@ -45,6 +45,7 @@ export function uiI18nMixin(kbnServer, server, config) {
     ]);
 
     return {
+      locale: defaultLocale,
       ...defaultTranslations,
       ...requestedTranslations,
     };
