24bd796967fd813fbeb5d211bc54846cb0242ec0
kobelb <brandon.kobel@elastic.co>
2018-06-13 08:57:29 -0400

    Introducing watchStatusAndLicenseToInitialize

--- x-pack/plugins/security/server/lib/privileges/index.js
+++ x-pack/plugins/security/server/lib/privileges/index.js
@@ -4,5 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { registerPrivilegesIfNecessary } from './privilege_action_registry';
+export { registerPrivilegesWithCluster } from './privilege_action_registry';
 export { getLoginPrivilege, getVersionPrivilege } from './privileges';
--- x-pack/plugins/security/server/lib/privileges/privilege_action_registry.test.js
+++ x-pack/plugins/security/server/lib/privileges/privilege_action_registry.test.js
@@ -4,101 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { registerPrivilegesWithCluster, registerPrivilegesIfNecessary } from './privilege_action_registry';
+import { registerPrivilegesWithCluster } from './privilege_action_registry';
 import { getClient } from '../../../../../server/lib/get_client_shield';
 import { buildPrivilegeMap } from './privileges';
-import { XPackInfo } from '../../../../xpack_main/server/lib/xpack_info';
-import { checkLicense } from '../check_license';
 jest.mock('../../../../../server/lib/get_client_shield', () => ({
   getClient: jest.fn(),
 }));
 jest.mock('./privileges', () => ({
   buildPrivilegeMap: jest.fn(),
 }));
-
-const registerPrivilegesIfNecessaryTest = (description, { settings = {}, simulateFailure = false, allowRbac, assert }) => {
-  const registerMockCallWithInternalUser = () => {
-    const callWithInternalUser = jest.fn(() => {
-      if (simulateFailure) {
-        throw new Error('Something happened');
-      }
-    });
-
-    getClient.mockReturnValue({
-      callWithInternalUser,
-    });
-    return callWithInternalUser;
-  };
-
-  const defaultVersion = 'default-version';
-  const defaultApplication = 'default-application';
-
-  const createMockServer = () => {
-    const mockServer = {
-      config: jest.fn().mockReturnValue({
-        get: jest.fn(),
-      }),
-      log: jest.fn(),
-      plugins: {
-        // Only needed/used for XPackInfo constructor
-        elasticsearch: {
-          getCluster: jest.fn().mockReturnValue({
-            callWithInternalUser: jest.fn().mockReturnValue({
-              features: {
-                security: {
-                  enabled: true
-                }
-              },
-              license: {
-                mode: allowRbac ? 'platinum' : 'basic',
-                status: 'active'
-              }
-            })
-          })
-        }
-      }
-    };
-
-    const defaultSettings = {
-      'pkg.version': defaultVersion,
-      'xpack.security.rbac.application': defaultApplication,
-    };
-
-    mockServer.config().get.mockImplementation(key => {
-      return key in settings ? settings[key] : defaultSettings[key];
-    });
-
-    return mockServer;
-  };
-
-  const createMockPlugin = () => {
-    return {
-      status: {
-        red: jest.fn()
-      }
-    };
-  };
-
-  test(description, async () => {
-    const mockServer = createMockServer();
-    const mockPlugin = createMockPlugin();
-    const mockCallWithInternalUser = registerMockCallWithInternalUser();
-
-    const xpackInfo = new XPackInfo(mockServer, {});
-    xpackInfo.feature('security').registerLicenseCheckResultsGenerator(checkLicense);
-
-    await xpackInfo.refreshNow();
-
-    await registerPrivilegesIfNecessary(mockServer, mockPlugin, xpackInfo);
-
-    assert({
-      mocks: {
-        plugin: mockPlugin,
-        callWithInternalUser: mockCallWithInternalUser
-      }
-    });
-  });
-};
 
 const registerPrivilegesWithClusterTest = (description, { settings = {}, expectedPrivileges, existingPrivileges, assert }) => {
   const registerMockCallWithInternalUser = () => {
@@ -194,130 +108,102 @@ const registerPrivilegesWithClusterTest = (description, { settings = {}, expecte
   });
 };
 
-describe('registerPrivilegesIfNecessary', () => {
-  registerPrivilegesIfNecessaryTest('does not register privileges if rbac is disabled', {
-    allowRbac: false,
-    assert: ({ mocks }) => {
-      expect(mocks.callWithInternalUser).not.toHaveBeenCalled();
-      expect(mocks.plugin.status.red).not.toHaveBeenCalled();
-    }
-  });
-
-  registerPrivilegesIfNecessaryTest('registers privileges if rbac is enabled', {
-    allowRbac: true,
-    assert: ({ mocks }) => {
-      expect(mocks.callWithInternalUser).toHaveBeenCalledWith('shield.getPrivilege', expect.anything());
-      expect(mocks.plugin.status.red).not.toHaveBeenCalled();
-    }
-  });
-
-  registerPrivilegesIfNecessaryTest('sets plugin status to red on error', {
-    allowRbac: true,
-    simulateFailure: true,
-    assert: ({ mocks }) => {
-      expect(mocks.plugin.status.red).toHaveBeenCalledWith('Unable to register privileges');
-    }
-  });
+registerPrivilegesWithClusterTest(`passes application and kibanaVersion to buildPrivilegeMap`, {
+  settings: {
+    'pkg.version': 'foo-version',
+    'xpack.security.rbac.application': 'foo-application',
+  },
+  assert: ({ mocks }) => {
+    expect(mocks.buildPrivilegeMap).toHaveBeenCalledWith('foo-application', 'foo-version');
+  },
 });
 
-describe('registerPrivilegesWithCluster', () => {
-  registerPrivilegesWithClusterTest(`passes application and kibanaVersion to buildPrivilegeMap`, {
-    settings: {
-      'pkg.version': 'foo-version',
-      'xpack.security.rbac.application': 'foo-application',
-    },
-    assert: ({ mocks }) => {
-      expect(mocks.buildPrivilegeMap).toHaveBeenCalledWith('foo-application', 'foo-version');
-    },
-  });
+registerPrivilegesWithClusterTest(`updates privileges when simple top-level privileges don't match`, {
+  expectedPrivileges: {
+    expected: true
+  },
+  existingPrivileges: {
+    expected: false
+  },
+  assert: ({ expectUpdatedPrivileges }) => {
+    expectUpdatedPrivileges();
+  }
+});
 
-  registerPrivilegesWithClusterTest(`updates privileges when simple top-level privileges don't match`, {
-    expectedPrivileges: {
+registerPrivilegesWithClusterTest(`updates privileges when nested privileges don't match`, {
+  expectedPrivileges: {
+    kibana: {
       expected: true
-    },
-    existingPrivileges: {
+    }
+  },
+  existingPrivileges: {
+    kibana: {
       expected: false
-    },
-    assert: ({ expectUpdatedPrivileges }) => {
-      expectUpdatedPrivileges();
     }
-  });
+  },
+  assert: ({ expectUpdatedPrivileges }) => {
+    expectUpdatedPrivileges();
+  }
+});
 
-  registerPrivilegesWithClusterTest(`updates privileges when nested privileges don't match`, {
-    expectedPrivileges: {
-      kibana: {
-        expected: true
-      }
-    },
-    existingPrivileges: {
-      kibana: {
-        expected: false
-      }
-    },
-    assert: ({ expectUpdatedPrivileges }) => {
-      expectUpdatedPrivileges();
+registerPrivilegesWithClusterTest(`updates privileges when nested privileges arrays don't match`, {
+  expectedPrivileges: {
+    kibana: {
+      expected: ['one', 'two']
     }
-  });
+  },
+  existingPrivileges: {
+    kibana: {
+      expected: ['one']
+    }
+  },
+  assert: ({ expectUpdatedPrivileges }) => {
+    expectUpdatedPrivileges();
+  }
+});
 
-  registerPrivilegesWithClusterTest(`updates privileges when nested privileges arrays don't match`, {
-    expectedPrivileges: {
-      kibana: {
-        expected: ['one', 'two']
-      }
-    },
-    existingPrivileges: {
-      kibana: {
-        expected: ['one']
-      }
-    },
-    assert: ({ expectUpdatedPrivileges }) => {
-      expectUpdatedPrivileges();
+registerPrivilegesWithClusterTest(`updates privileges when nested property array values are reordered`, {
+  expectedPrivileges: {
+    kibana: {
+      foo: ['one', 'two']
     }
-  });
+  },
+  existingPrivileges: {
+    kibana: {
+      foo: ['two', 'one']
+    }
+  },
+  assert: ({ expectUpdatedPrivileges }) => {
+    expectUpdatedPrivileges();
+  }
+});
 
-  registerPrivilegesWithClusterTest(`updates privileges when nested property array values are reordered`, {
-    expectedPrivileges: {
-      kibana: {
-        foo: ['one', 'two']
-      }
-    },
-    existingPrivileges: {
-      kibana: {
-        foo: ['two', 'one']
-      }
-    },
-    assert: ({ expectUpdatedPrivileges }) => {
-      expectUpdatedPrivileges();
-    }
-  });
+registerPrivilegesWithClusterTest(`doesn't update privileges when simple top-level privileges match`, {
+  expectedPrivileges: {
+    expected: true
+  },
+  existingPrivileges: {
+    expected: true
+  },
+  assert: ({ expectDidntUpdatePrivileges }) => {
+    expectDidntUpdatePrivileges();
+  }
+});
 
-  registerPrivilegesWithClusterTest(`doesn't update privileges when simple top-level privileges match`, {
-    expectedPrivileges: {
-      expected: true
-    },
-    existingPrivileges: {
-      expected: true
-    },
-    assert: ({ expectDidntUpdatePrivileges }) => {
-      expectDidntUpdatePrivileges();
+registerPrivilegesWithClusterTest(`doesn't update privileges when nested properties are reordered`, {
+  expectedPrivileges: {
+    kibana: {
+      foo: true,
+      bar: false
     }
-  });
-
-  registerPrivilegesWithClusterTest(`doesn't update privileges when nested properties are reordered`, {
-    expectedPrivileges: {
-      kibana: {
-        foo: true,
-        bar: false
-      }
-    },
-    existingPrivileges: {
-      kibana: {
-        bar: false,
-        foo: true
-      }
-    },
-    assert: ({ expectDidntUpdatePrivileges }) => {
-      expectDidntUpdatePrivileges();
+  },
+  existingPrivileges: {
+    kibana: {
+      bar: false,
+      foo: true
     }
-  });
+  },
+  assert: ({ expectDidntUpdatePrivileges }) => {
+    expectDidntUpdatePrivileges();
+  }
 });
