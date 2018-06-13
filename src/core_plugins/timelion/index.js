260c9ee1fbb78f16f4e752019de389660fa46b38
Tyler Smalley <tyler.smalley@elastic.co>
2018-06-13 14:49:50 -0700

    Removes config from Timelion
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- src/core_plugins/timelion/index.js
+++ src/core_plugins/timelion/index.js
@@ -27,8 +27,6 @@ export default function (kibana) {
         description: 'Time series expressions for everything',
         icon: 'plugins/timelion/icon.svg',
         main: 'plugins/timelion/app',
-        styleSheetPath: 'timelion.css',
-        styleSheetToCompile: 'timelion.scss',
       },
       hacks: [
         'plugins/timelion/lib/panel_registry',
