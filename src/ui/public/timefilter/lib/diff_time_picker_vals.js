3156de7c2d22ebe96415e0a1ad87dc7105a65f86
Nathan Reese <reese.nathan@gmail.com>
2018-06-13 06:53:47 -0600

    remove Private from timefilter lib (#19651) (#19849)

--- src/ui/public/timefilter/lib/diff_time_picker_vals.js
+++ src/ui/public/timefilter/lib/diff_time_picker_vals.js
@@ -18,28 +18,24 @@
  */
 
 import _ from 'lodash';
-import angular from 'angular';
 
-export function UtilsDiffTimePickerValsProvider() {
+const valueOf = function (o) {
+  if (o) return o.valueOf();
+};
 
-  const valueOf = function (o) {
-    if (o) return o.valueOf();
-  };
-
-  return function (rangeA, rangeB) {
-    if (_.isObject(rangeA) && _.isObject(rangeB)) {
-      if (
-        valueOf(rangeA.to) !== valueOf(rangeB.to)
-        || valueOf(rangeA.from) !== valueOf(rangeB.from)
-        || valueOf(rangeA.value) !== valueOf(rangeB.value)
-        || valueOf(rangeA.pause) !== valueOf(rangeB.pause)
-      ) {
-        return true;
-      }
-    } else {
-      return !angular.equals(rangeA, rangeB);
+export function areTimePickerValsDifferent(rangeA, rangeB) {
+  if (_.isObject(rangeA) && _.isObject(rangeB)) {
+    if (
+      valueOf(rangeA.to) !== valueOf(rangeB.to)
+      || valueOf(rangeA.from) !== valueOf(rangeB.from)
+      || valueOf(rangeA.value) !== valueOf(rangeB.value)
+      || valueOf(rangeA.pause) !== valueOf(rangeB.pause)
+    ) {
+      return true;
     }
+  } else {
+    return !_.isEqual(rangeA, rangeB);
+  }
 
-    return false;
-  };
+  return false;
 }
