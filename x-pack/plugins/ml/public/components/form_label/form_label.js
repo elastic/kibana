c552b5af8e55dca7c4f40c1a37ea575a3d121fae
Walter Rafelsberger <walter@rafelsberger.at>
2018-06-14 09:22:44 +0200

    [ML] Fixes the tooltip position of job groups to avoid being overlapped by the input element. (#19292)
    This changes the option to place the info icon tooltip above the icon to avoid overlap with the job group input element below. The fix adds the tooltip-placement option the mlFormLabel directive. Using the top-placement is used because the append-to-body option didn't solve the problem because of the higher z-index of the ui-select element.

--- x-pack/plugins/ml/public/components/form_label/form_label.js
+++ x-pack/plugins/ml/public/components/form_label/form_label.js
@@ -24,14 +24,19 @@ module.directive('mlFormLabel', function () {
   return {
     scope: {
       labelId: '@',
-      tooltipAppendToBody: '@'
+      tooltipAppendToBody: '@',
+      tooltipPlacement: '@'
     },
     restrict: 'E',
     replace: false,
     transclude: true,
     template: `
       <label class="kuiFormLabel" id="ml_aria_label_{{labelId}}" ng-transclude></label>
-      <i ml-info-icon="{{labelId}}" tooltip-append-to-body="{{tooltipAppendToBody}}" />
+      <i
+        ml-info-icon="{{labelId}}"
+        tooltip-append-to-body="{{tooltipAppendToBody || false}}"
+        tooltip-placement="{{tooltipPlacement || 'bottom'}}"
+      />
     `
   };
 });
