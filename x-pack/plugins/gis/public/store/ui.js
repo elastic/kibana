f9cd7f5178ff220ad095b42ef12360b81adf0f87
Aaron Caldwell <Aaron.Caldwell@elastic.co>
2018-06-14 10:48:09 -0600

    Update ui store to support multiple flyout states. Add corresponding selectors and actions

--- x-pack/plugins/gis/public/store/ui.js
+++ x-pack/plugins/gis/public/store/ui.js
@@ -3,29 +3,48 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-export const TOGGLE_FLYOUT = 'TOGGLE_FLYOUT';
-
+const UPDATE_FLYOUT = 'UPDATE_FLYOUT';
+const SET_SELECTED_LAYER = 'SET_SELECTED_LAYER';
+export const FLYOUT_STATE = {
+  NONE: 'NONE',
+  LAYER_PANEL: 'LAYER_PANEL',
+  ADD_LAYER_WIZARD: 'ADD_LAYER_WIZARD'
+};
 const INITIAL_STATE = {
-  flyoutOpen: false
+  flyoutDisplay: FLYOUT_STATE.NONE,
+  selectedLayer: null
 };
 
+// Reducer
 function ui(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case TOGGLE_FLYOUT:
-      return { ...state, flyoutOpen: !state.flyoutOpen };
+    case UPDATE_FLYOUT:
+      return { ...state, flyoutDisplay: action.display };
+    case SET_SELECTED_LAYER:
+      return { ...state, selectedLayer: action.selectedLayer };
     default:
       return state;
   }
 }
 
 // Actions
-export function toggleFlyout() {
+export function updateFlyout(display) {
   return {
-    type: TOGGLE_FLYOUT
+    display,
+    type: UPDATE_FLYOUT
+  };
+}
+export function setSelectedLayer(layer) {
+  return {
+    type: SET_SELECTED_LAYER,
+    selectedLayer: layer
   };
 }
 
 // Selectors
-export const getFlyoutOpen = ({ ui }) => ui && ui.flyoutOpen;
+export const getFlyoutDisplay = ({ ui }) => ui && ui.flyoutDisplay
+  || INITIAL_STATE.flyoutDisplay;
+export const getSelectedLayer = ({ ui }) => ui && ui.selectedLayer
+  || INITIAL_STATE.selectedLayer;
 
 export default ui;
