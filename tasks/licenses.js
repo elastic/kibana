ad4fec36c16140df2e044ee5f4fd24aa3315519e
Tiago Costa <tiagoffcc@hotmail.com>
2018-06-14 14:24:44 +0100

    chore(19502): change license checker and generator behaviour to only add dev dependencies with --dev flag. (#19626) (#19866)

--- tasks/licenses.js
+++ tasks/licenses.js
@@ -10,10 +10,13 @@ export default function licenses(grunt) {
     const done = this.async();
 
     try {
+      const dev = Boolean(grunt.option('dev'));
+
       assertLicensesValid({
         packages: await getInstalledPackages({
           directory: grunt.config.get('root'),
-          licenseOverrides: LICENSE_OVERRIDES
+          licenseOverrides: LICENSE_OVERRIDES,
+          dev
         }),
         validLicenses: LICENSE_WHITELIST
       });
