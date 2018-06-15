0bf5db9184cce128e00869162c526c510532f915
Josh Soref <jsoref@users.noreply.github.com>
2018-06-15 03:03:19 +0000

    spelling: choropleth

--- src/core_plugins/region_map/public/region_map_visualization.js
+++ src/core_plugins/region_map/public/region_map_visualization.js
@@ -95,7 +95,7 @@ export function RegionMapsVisualizationProvider(Private, Notifier, config) {
         return;
       }
 
-      this._updateChoroplehLayerForNewProperties(
+      this._updateChoroplethLayerForNewProperties(
         visParams.selectedLayer.url,
         visParams.selectedLayer.attribution,
         this._vis.params.showAllShapes
@@ -114,7 +114,7 @@ export function RegionMapsVisualizationProvider(Private, Notifier, config) {
       return this._recreateChoroplethLayer(url, attribution, showAllData);
     }
 
-    _updateChoroplehLayerForNewProperties(url, attribution, showAllData) {
+    _updateChoroplethLayerForNewProperties(url, attribution, showAllData) {
       if (this._choroplethLayer && this._choroplethLayer.canReuseInstance(url, showAllData)) {
         return;
       }
