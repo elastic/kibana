1cb348dfc0cf80f0f52237d0fe518f1774a6cd0e
Josh Soref <jsoref@users.noreply.github.com>
2018-06-17 18:04:32 +0000

    spelling: formatted

--- src/core_plugins/timelion/public/directives/timelion_expression_input_helpers.js
+++ src/core_plugins/timelion/public/directives/timelion_expression_input_helpers.js
@@ -181,7 +181,7 @@ export async function suggest(expression, functionList, Parser, cursorPosition, 
     let message;
     try {
       // The grammar will throw an error containing a message if the expression is formatted
-      // correctly and is prepared to accept suggestions. If the expression is not formmated
+      // correctly and is prepared to accept suggestions. If the expression is not formatted
       // correctly the grammar will just throw a regular PEG SyntaxError, and this JSON.parse
       // attempt will throw an error.
       message = JSON.parse(e.message);
