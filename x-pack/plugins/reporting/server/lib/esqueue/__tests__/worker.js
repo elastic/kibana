4f0e85c7c303b60bc9e7a8a4c62ea576472317d8
Josh Soref <jsoref@users.noreply.github.com>
2018-06-17 05:13:38 +0000

    spelling: document

--- x-pack/plugins/reporting/server/lib/esqueue/__tests__/worker.js
+++ x-pack/plugins/reporting/server/lib/esqueue/__tests__/worker.js
@@ -577,7 +577,7 @@ describe('Worker class', function () {
         .then((res) => expect(res).to.equal(true));
     });
 
-    it('should return false on other docuemnt update errors', function () {
+    it('should return false on other document update errors', function () {
       mockQueue.client.update.restore();
       sinon.stub(mockQueue.client, 'update').returns(Promise.reject({ statusCode: 401 }));
       return worker._failJob(job)
@@ -613,7 +613,7 @@ describe('Worker class', function () {
       return worker._failJob(job);
     });
 
-    it('should emit on other docuemnt update errors', function (done) {
+    it('should emit on other document update errors', function (done) {
       mockQueue.client.update.restore();
       sinon.stub(mockQueue.client, 'update').returns(Promise.reject({ statusCode: 401 }));
 
