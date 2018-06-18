a3d8f243428e78911b3e8b7c724acd69d63620e6
Chris Roberson <chrisronline@gmail.com>
2018-06-18 15:59:00 -0400

    Aliases are not defined here anymore

--- x-pack/plugins/index_lifecycle_management/public/store/selectors/policies.js
+++ x-pack/plugins/index_lifecycle_management/public/store/selectors/policies.js
@@ -31,7 +31,6 @@ import {
   PHASE_PRIMARY_SHARD_COUNT,
   PHASE_REPLICA_COUNT,
   PHASE_ENABLED,
-  PHASE_ROLLOVER_ALIAS,
   PHASE_ATTRIBUTES_THAT_ARE_NUMBERS,
   MAX_SIZE_TYPE_DOCUMENT
 } from '../constants';
@@ -182,9 +181,7 @@ export const phaseToES = (state, phase) => {
   esPhase.actions = {};
 
   if (phase[PHASE_ROLLOVER_ENABLED]) {
-    esPhase.actions.rollover = {
-      alias: phase[PHASE_ROLLOVER_ALIAS],
-    };
+    esPhase.actions.rollover = {};
 
     if (isNumber(phase[PHASE_ROLLOVER_MAX_AGE])) {
       esPhase.actions.rollover.max_age = `${phase[PHASE_ROLLOVER_MAX_AGE]}${
