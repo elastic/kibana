ea8ec831236ad22b88f256616ec4707c39f55675
Nathan Reese <reese.nathan@gmail.com>
2018-06-19 05:05:24 +0300

    fix adjust timestamp tests so they run correctly in any timezone

--- src/server/sample_data/routes/lib/adjust_timestamp.test.js
+++ src/server/sample_data/routes/lib/adjust_timestamp.test.js
@@ -17,13 +17,12 @@
  * under the License.
  */
 
-
 import { adjustTimestamp } from './adjust_timestamp';
 
-const currentTimeMarker = new Date(Date.parse('2018-01-02T00:00:00Z'));
-const now = new Date(Date.parse('2018-04-25T18:24:58.650Z')); // Wednesday
-
 describe('relative to now', () => {
+  const currentTimeMarker = new Date(Date.parse('2018-01-02T00:00:00Z'));
+  const now = new Date(Date.parse('2018-04-25T18:24:58.650Z')); // Wednesday
+
   test('adjusts time to 10 minutes in past from now', () => {
     const originalTimestamp = '2018-01-01T23:50:00Z'; // -10 minutes relative to currentTimeMarker
     const timestamp = adjustTimestamp(originalTimestamp, currentTimeMarker, now, false);
@@ -38,28 +37,31 @@ describe('relative to now', () => {
 });
 
 describe('preserve day of week and time of day', () => {
+  const currentTimeMarker = new Date(Date.parse('2018-01-02T00:00:00'));
+  const now = new Date(Date.parse('2018-04-25T18:24:58.650Z')); // Wednesday
+
   test('adjusts time to monday of the same week as now', () => {
-    const originalTimestamp = '2018-01-01T23:50:00Z'; // Monday, same week relative to currentTimeMarker
+    const originalTimestamp = '2018-01-01T23:50:00'; // Monday, same week relative to currentTimeMarker
     const timestamp = adjustTimestamp(originalTimestamp, currentTimeMarker, now, true);
-    expect(timestamp).toBe('2018-04-23T23:50:00Z');
+    expect(timestamp).toBe('2018-04-23T23:50:00');
   });
 
   test('adjusts time to friday of the same week as now', () => {
-    const originalTimestamp = '2017-12-29T23:50:00Z'; // Friday, same week relative to currentTimeMarker
+    const originalTimestamp = '2017-12-29T23:50:00'; // Friday, same week relative to currentTimeMarker
     const timestamp = adjustTimestamp(originalTimestamp, currentTimeMarker, now, true);
-    expect(timestamp).toBe('2018-04-27T23:50:00Z');
+    expect(timestamp).toBe('2018-04-27T23:50:00');
   });
 
   test('adjusts time to monday of the previous week as now', () => {
-    const originalTimestamp = '2017-12-25T23:50:00Z'; // Monday, previous week relative to currentTimeMarker
+    const originalTimestamp = '2017-12-25T23:50:00'; // Monday, previous week relative to currentTimeMarker
     const timestamp = adjustTimestamp(originalTimestamp, currentTimeMarker, now, true);
-    expect(timestamp).toBe('2018-04-16T23:50:00Z');
+    expect(timestamp).toBe('2018-04-16T23:50:00');
   });
 
   test('adjusts time to friday of the week after now', () => {
-    const originalTimestamp = '2018-01-05T23:50:00Z'; // Friday, next week relative to currentTimeMarker
+    const originalTimestamp = '2018-01-05T23:50:00'; // Friday, next week relative to currentTimeMarker
     const timestamp = adjustTimestamp(originalTimestamp, currentTimeMarker, now, true);
-    expect(timestamp).toBe('2018-05-04T23:50:00Z');
+    expect(timestamp).toBe('2018-05-04T23:50:00');
   });
 
   test('adjusts timestamp to correct day of week even when UTC day is on different day.', () => {
