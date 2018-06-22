c0de3b7aed03582c800a43c6fa87cd9f9eed1117
CJ Cenizal <cj@cenizal.com>
2018-06-22 14:47:38 -0700

    Convert SearchLooper to be a class.

--- src/ui/public/courier/search_looper/index.js
+++ src/ui/public/courier/search_looper/index.js
@@ -27,203 +27,198 @@ import { fatalError } from '../../notify';
 export function SearchLooperProvider(Private, Promise, $timeout, $rootScope) {
   const fetchSoon = Private(FetchSoonProvider);
 
-  function Looper() {
-    this._ms = 1500;
-    this._timer = null;
-    this._started = false;
+  class SearchLooper {
+    constructor() {
+      this._ms = 1500;
+      this._timer = null;
+      this._started = false;
+    }
 
-    this._loopTheLoop = _.bind(this._loopTheLoop, this);
+    /**
+     * Set the number of milliseconds between
+     * each loop
+     *
+     * @param  {integer} ms
+     * @chainable
+     */
+    ms = (ms) => {
+      this._ms = _.parseInt(ms) || 0;
+
+      if (!this._started) return;
+
+      if (this._ms) {
+        this.start(false);
+      } else {
+        this._unScheduleLoop();
+      }
+
+      return this;
+    };
+
+    /**
+     * Cancels the current looper while keeping internal
+     * state as started
+     *
+     * @chainable
+     */
+    pause = () => {
+      this._unScheduleLoop();
+      return this;
+    };
+
+    /**
+     * Start the looping madness
+     *
+     * @chainable
+     */
+    start = (loopOver) => {
+      if (loopOver == null) loopOver = true;
+
+      if (!this._started) {
+        this._started = true;
+      } else {
+        this._unScheduleLoop();
+      }
+
+      if (loopOver) {
+        this._loopTheLoop();
+      } else {
+        this._scheduleLoop();
+      }
+
+      return this;
+    };
+
+    /**
+     * ...
+     *
+     * @chainable
+     */
+    stop = () => {
+      this._unScheduleLoop();
+      this._started = false;
+      return this;
+    };
+
+    /**
+     * Restart the looper only if it is already started.
+     * Called automatically when ms is changed
+     *
+     * @chainable
+     */
+    restart = () => {
+      this.start(false);
+      return this;
+    };
+
+    /**
+     * Is the looper currently started/running/scheduled/going to execute
+     *
+     * @return {boolean}
+     */
+    started = () => {
+      return !!this._started;
+    };
+
+    /**
+     * Returns the current loop interval
+     *
+     * @return {number}
+     */
+    loopInterval = () => {
+      return this._ms;
+    };
+
+    /**
+     * Called when the loop is executed before the previous
+     * run has completed.
+     *
+     * @override
+     * @return {undefined}
+     */
+    _onHastyLoop = () => {
+      if (this.afterHastyQueued) return;
+
+      this.afterHastyQueued = Promise.resolve(this.active)
+        .then(() => {
+          return this._loopTheLoop();
+        })
+        .finally(() => {
+          this.afterHastyQueued = null;
+        });
+    };
+
+    /**
+     * Wraps this._fn so that this._fn can be changed
+     * without rescheduling and schedules
+     * the next iteration
+     *
+     * @private
+     * @return {undefined}
+     */
+    _loopTheLoop = () => {
+      if (this.active) {
+        this._onHastyLoop();
+        return;
+      }
+
+      this.active = Promise
+        .try(this._action)
+        .then(() => {
+          this._scheduleLoop();
+        })
+        .catch((err) => {
+          this.stop();
+          fatalError(err);
+        })
+        .finally(() => {
+          this.active = null;
+        });
+    };
+
+    _action = () => {
+      $rootScope.$broadcast('courier:searchRefresh');
+      const requests = requestQueue.getInactive();
+      // promise returned from fetch.fetchSearchRequests() only resolves when
+      // the requests complete, but we want to continue even if
+      // the requests abort so we make our own
+      fetchSoon.fetchSearchRequests(requests);
+      return Promise.all(requests.map(request => request.getCompleteOrAbortedPromise()));
+    };
+
+    /**
+     * Schedule the next iteration of the loop
+     *
+     * @private
+     * @return {number} - the timer promise
+     */
+    _scheduleLoop = () => {
+      this._unScheduleLoop();
+      this._timer = this._ms ? $timeout(this._loopTheLoop, this._ms) : null;
+      return this._timer;
+    };
+
+    /**
+     * Cancel the next iteration of the loop
+     *
+     * @private
+     * @return {number} - the timer promise
+     */
+    _unScheduleLoop = () => {
+      if (this._timer) {
+        $timeout.cancel(this._timer);
+        this._timer = null;
+      }
+    };
+
+    /**
+     * execute the this._fn, and restart the timer
+     */
+    run = () => {
+      this.start();
+    };
   }
 
-  /**
-   * Set the number of milliseconds between
-   * each loop
-   *
-   * @param  {integer} ms
-   * @chainable
-   */
-  Looper.prototype.ms = function (ms) {
-    this._ms = _.parseInt(ms) || 0;
-
-    if (!this._started) return;
-
-    if (this._ms) {
-      this.start(false);
-    } else {
-      this._unScheduleLoop();
-    }
-
-    return this;
-  };
-
-  /**
-   * Cancels the current looper while keeping internal
-   * state as started
-   *
-   * @chainable
-   */
-  Looper.prototype.pause = function () {
-    this._unScheduleLoop();
-    return this;
-  };
-
-  /**
-   * Start the looping madness
-   *
-   * @chainable
-   */
-  Looper.prototype.start = function (loopOver) {
-    if (loopOver == null) loopOver = true;
-
-    if (!this._started) {
-      this._started = true;
-    } else {
-      this._unScheduleLoop();
-    }
-
-    if (loopOver) {
-      this._loopTheLoop();
-    } else {
-      this._scheduleLoop();
-    }
-
-    return this;
-  };
-
-  /**
-   * ...
-   *
-   * @chainable
-   */
-  Looper.prototype.stop = function () {
-    this._unScheduleLoop();
-    this._started = false;
-    return this;
-  };
-
-  /**
-   * Restart the looper only if it is already started.
-   * Called automatically when ms is changed
-   *
-   * @chainable
-   */
-  Looper.prototype.restart = function () {
-    this.start(false);
-    return this;
-  };
-
-  /**
-   * Is the looper currently started/running/scheduled/going to execute
-   *
-   * @return {boolean}
-   */
-  Looper.prototype.started = function () {
-    return !!this._started;
-  };
-
-  /**
-   * Returns the current loop interval
-   *
-   * @return {number}
-   */
-  Looper.prototype.loopInterval = function () {
-    return this._ms;
-  };
-
-  /**
-   * Called when the loop is executed before the previous
-   * run has completed.
-   *
-   * @override
-   * @return {undefined}
-   */
-  Looper.prototype._onHastyLoop = function () {
-    if (this.afterHastyQueued) return;
-
-    this.afterHastyQueued = Promise.resolve(this.active)
-      .then(() => {
-        return this._loopTheLoop();
-      })
-      .finally(() => {
-        this.afterHastyQueued = null;
-      });
-  };
-
-  /**
-   * Wraps this._fn so that this._fn can be changed
-   * without rescheduling and schedules
-   * the next iteration
-   *
-   * @private
-   * @return {undefined}
-   */
-  Looper.prototype._loopTheLoop = function () {
-    const self = this;
-
-    if (self.active) {
-      self._onHastyLoop();
-      return;
-    }
-
-    self.active = Promise
-      .try(this._action)
-      .then(function () {
-        self._scheduleLoop();
-      })
-      .catch(function (err) {
-        self.stop();
-        fatalError(err);
-      })
-      .finally(function () {
-        self.active = null;
-      });
-  };
-
-  Looper.prototype._action = function () {
-    $rootScope.$broadcast('courier:searchRefresh');
-    const requests = requestQueue.getInactive();
-    // promise returned from fetch.fetchSearchRequests() only resolves when
-    // the requests complete, but we want to continue even if
-    // the requests abort so we make our own
-    fetchSoon.fetchSearchRequests(requests);
-    return Promise.all(requests.map(request => request.getCompleteOrAbortedPromise()));
-  };
-
-  /**
-   * Schedule the next iteration of the loop
-   *
-   * @private
-   * @return {number} - the timer promise
-   */
-  Looper.prototype._scheduleLoop = function () {
-    this._unScheduleLoop();
-    this._timer = this._ms ? $timeout(this._loopTheLoop, this._ms) : null;
-    return this._timer;
-  };
-
-  /**
-   * Cancel the next iteration of the loop
-   *
-   * @private
-   * @return {number} - the timer promise
-   */
-  Looper.prototype._unScheduleLoop = function () {
-    if (this._timer) {
-      $timeout.cancel(this._timer);
-      this._timer = null;
-    }
-  };
-
-  /**
-   * execute the this._fn, and restart the timer
-   */
-  Looper.prototype.run = function () {
-    this.start();
-  };
-
-  /**
-   * The Looper which will manage the doc fetch interval
-   * @type {Looper}
-   */
-  return new Looper();
+  // This manages the doc fetch interval.
+  return new SearchLooper();
 }
