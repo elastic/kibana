0adee112e826ffc54713fb48d49e4432996681de
Aaron Caldwell <Aaron.Caldwell@elastic.co>
2018-06-22 15:09:03 -0600

    Add basic vector layer transformation to Open Layers format

--- x-pack/plugins/gis/public/selectors/ol_map_selectors.js
+++ x-pack/plugins/gis/public/selectors/ol_map_selectors.js
@@ -9,6 +9,8 @@ import { getLayerList } from "./map_selectors";
 import * as ol from 'openlayers';
 import { LAYER_TYPE } from "../actions/map_actions";
 
+const FEATURE_PROJECTION = 'EPSG:3857';
+
 // Layer-specific logic
 function convertTmsLayersToOl({ details }) {
   return new ol.layer.Tile({
@@ -17,12 +19,28 @@ function convertTmsLayersToOl({ details }) {
     })
   });
 }
+const convertVectorLayersToOl = (() => {
+  const geojsonFormat = new ol.format.GeoJSON({
+    featureProjection: FEATURE_PROJECTION
+  });
+  return ({ details }) => {
+    const olFeatures = geojsonFormat.readFeatures(details.service);
+    return new ol.layer.Vector({
+      source: new ol.source.Vector({
+        features: olFeatures
+      })
+    });
+  };
+})();
 
 function updateLayersByType(layerList) {
   return layerList.map(layer => {
     switch (layer.appData.layerType) {
       case LAYER_TYPE.TMS:
         layer.olLayer = convertTmsLayersToOl(layer);
+        break;
+      case LAYER_TYPE.VECTOR_FILE:
+        layer.olLayer = convertVectorLayersToOl(layer);
         break;
       default:
         break;
