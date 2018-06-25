d104455876e631938b6f1712aea529ff398bc9c2
Aaron Caldwell <Aaron.Caldwell@elastic.co>
2018-06-25 05:26:00 -0600

    Create new toc entry component

---
+++ x-pack/plugins/gis/public/components/layer_toc/toc_entry.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+} from '@elastic/eui';
+import { VisibilityToggle } from './visiblity_toggle';
+
+export function TOCEntry(props) {
+  const {
+    layerId,
+    visible,
+    layerName,
+    onButtonClick
+  } = props;
+
+  return (
+    <div
+      className={`layerEntry`}
+      id={layerId}
+      data-layerid={layerId}
+    >
+      <EuiFlexGroup gutterSize="s" responsive={false} className={visible ? 'visible' : 'notvisible'}>
+        <EuiFlexItem grow={false} className="layerEntry--visibility">
+          <VisibilityToggle
+            checked={visible}
+            // onChange={this._onVisibilityChange}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem className="layerEntry--name">
+          <button onClick={() => onButtonClick(this)}>
+            {layerName}
+          </button>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <span className="grab"><EuiIcon type="grab" className="grab"/></span>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </div>
+  );
+}
