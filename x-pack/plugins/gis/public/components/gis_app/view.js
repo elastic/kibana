0dcd1c308b4bfe242e48c21276811e4837a1eee9
Aaron Caldwell <Aaron.Caldwell@elastic.co>
2018-06-25 13:31:03 -0600

    Clean up unused logic in GIS App, convert to stateless function

--- x-pack/plugins/gis/public/components/gis_app/view.js
+++ x-pack/plugins/gis/public/components/gis_app/view.js
@@ -6,79 +6,13 @@
 
 import React from 'react';
 import { KibanaMap } from '../map';
-import { TMSSource } from '../map/layers/sources/tms_source';
-import { EMSVectorSource } from '../map/layers/sources/ems_vector_source';
-import { EMSTMSSource } from '../map/layers/sources/ems_tms_source';
-import { KbnYmlTMSSource } from '../map/layers/sources/kbnyml_tms_source';
-import { KbnYmlVectorSource } from '../map/layers/sources/kbnyml_vector_source';
-
-import { TileLayer } from '../map/layers/tile_layer';
-import { VectorLayer } from '../map/layers/vector_layer';
 import { LayerControl } from '../layer_control/index';
 
-export class GISApp extends React.Component {
-
-  constructor() {
-    super();
-    this._kbnMap = null;
-  }
-
-  async _createPlaceholders() {
-
-    //todo: some hardcoded example layers
-    const emsTMSSource = new EMSTMSSource({
-      kbnCoreAPI: this.props.kbnCoreAPI,
-      serviceId: "road_map"
-    });
-    const tmsLayer = new TileLayer(emsTMSSource);
-    await this._kbnMap.addLayer(tmsLayer);
-    const osmSource = new TMSSource({
-      urlTemplate: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    });
-
-    const osmLayer = new TileLayer(osmSource);
-    await this._kbnMap.addLayer(osmLayer);
-
-    try {
-      const kbnymlTmsSource = new KbnYmlTMSSource({ kbnCoreAPI: this.props.kbnCoreAPI });
-      const tmsLayer = new TileLayer(kbnymlTmsSource);
-      await this._kbnMap.addLayer(tmsLayer);
-    } catch(e) {
-      console.error(e);
-      console.warn('Missing map.tilemap configuration in yml');
-    }
-
-    const vectorSource = new EMSVectorSource({
-      kbnCoreAPI: this.props.kbnCoreAPI,
-      layerName: "World Countries"
-    });
-
-    const vectorLayer = new VectorLayer(vectorSource);
-    await this._kbnMap.addLayer(vectorLayer);
-
-
-    try {
-      const vectorSource = new KbnYmlVectorSource({
-        layerName: "foobar (self hosted)",
-        kbnCoreAPI: this.props.kbnCoreAPI
-      });
-
-      const vectorLayer = new VectorLayer(vectorSource);
-      await this._kbnMap.addLayer(vectorLayer);
-    } catch(e) {
-      console.error(e);
-      console.warn('Missing regionmap configuration in yml');
-    }
-
-  }
-
-  render() {
-    return (
-      <div className="wrapper">
-        <KibanaMap ref={(kbnMap) => this._kbnMap = kbnMap}/>
-        <LayerControl/>
-      </div>
-    );
-  }
-
+export function GISApp() {
+  return (
+    <div className="wrapper">
+      <KibanaMap/>
+      <LayerControl/>
+    </div>
+  );
 }
