5c1a6c4f5a7559cfccf97e86348d8fb7a4a98a60
archana <archanid@users.noreply.github.com>
2018-06-26 18:08:40 -0500

    [Functional Tests] Use @kbn/test on Kibana CI (#18967)
    * Replace test:api with @kbn/test runTests
    
    * Improve CLI help menu :sos:
    
    * Use --es-from
    
    * Replace jenkins:selenium with kbn-test
    
    * Validate cli args, fixing test in the process
    
    * Clean up some stuff
    
    * Code review fixes
    
    * Explanation for collectCliArgs
    
    * Remove exit codes, they're useless anyway.
    
    * Make markdown vis test pass with dev_mode setting
    
    * Tests
    
    * Remove unneeded export
    
    * Code review: move console logging up to cli.js
    
    * Code review: refactor startServers and runTests to take single options collection
    
    * Code review: Remove all things I am sure we do not use
    
    * Improve tests
    
    * Code review fixes
    
    * Pass created log to runFtr, runElasticsearch, runKibanaServer
    
    * Update --es-from option to --esFrom

--- packages/kbn-test/src/functional_tests/lib/run_ftr.js
+++ packages/kbn-test/src/functional_tests/lib/run_ftr.js
@@ -22,15 +22,16 @@ import { KIBANA_FTR_SCRIPT, PROJECT_ROOT } from './paths';
 export async function runFtr({
   procs,
   configPath,
-  bail,
-  log,
   cwd = PROJECT_ROOT,
+  options: { log, bail, grep, updateBaselines },
 }) {
   const args = [KIBANA_FTR_SCRIPT];
 
   if (getLogFlag(log)) args.push(`--${getLogFlag(log)}`);
   if (bail) args.push('--bail');
   if (configPath) args.push('--config', configPath);
+  if (grep) args.push('--grep', grep);
+  if (updateBaselines) args.push('--updateBaselines');
 
   await procs.run('ftr', {
     cmd: 'node',
