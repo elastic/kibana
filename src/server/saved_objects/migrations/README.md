b4bb4bcad818e5ee1d0ecfabde3ec7440049f14e
Christopher Davies <github@christophilus.com>
2018-06-26 16:03:45 -0400

    Add core document transform logic

--- src/server/saved_objects/migrations/README.md
+++ src/server/saved_objects/migrations/README.md
@@ -13,3 +13,77 @@ Migrations are organized into two folders:
 ## Testing
 
 Run jest tests: `node scripts/jest --testPathPattern=migrations --watch`
+
+
+## General migration process
+
+- Store a migrationVersion with each document `{ migrationVersion: { dashboard: '6.4.3' } }`
+- On Kibana start, check if the index needs migrating
+  - Run an agg query that gets the distinct values for each migrationVersion key / value
+  - If any are out of date, migration is necessary, kick off migrations
+  - If some are *ahead* of our system, fail to start
+- Index migration
+  - Ensure the index is aliased, do a reindex if the index is not aliased
+  - Create a new index w/ predictive name
+  - Keep any mappings from previous index which aren't defined by active plugins
+    - Con: We never actually delete a mapping :/
+    - Pro: We never have to drop docs :)
+    - If a plugin wants to "delete" a mapping, they can modify it to be useless: `{ dashboard: { type: 'boolean' } }`
+  - Move all docs, passing them through the appropriate migrations
+  - Point the alias to the new index and wait for it to refresh
+- Multiple Kibana instances can run migrations in parallel
+  - Makes for simpler coordination logic
+  - On startup, if migration is needed, and the destination index already exists
+    - We'll assume it's due to multi-instance migration or a previously cancelled migration attempt
+    - Patch the dest index mappings, just to be safe
+    - Begin moving docs over, overwriting any existing docs
+  - This does mean that we have to ignore certain classes of failure (e.g. destination index already exists, document failed to write due to version conflict, etc)
+    - Ignoring such errors may be unadviseable
+    - Alternatively, at a future date, we could disallow multi-instance migration,
+      - We would need to coordinate migrations such that only one Kibana instance runs migrations at a time
+      - The others poll for completion
+      - If the migrating instance fails for some reason, the others will poll indefinitely unless we put a heartbeat in
+      - We'd need a `--force-migration` start option, which tells Kibana to recover from a previously failed migration attempt
+
+
+## Disabled plugins
+
+If a plugin is disabled, we need to know what the last supported version was so that we can reject incoming documents that would make the index inconsistent. To do this:
+
+- Store "migrationVersion" in the mapping _meta field
+  - This is a dictionary of `doc_type: semver`
+  - Allows us to know the migration version info of disabled types / plugins
+
+
+## Saved object API
+
+- Assume docs w/ no migration version are up to date (see [this comment](https://github.com/elastic/kibana/issues/15100#issuecomment-400000325))
+- Docs w/ migration version are migrated if need be
+- Modify import logic to add an empty migrationVersion property to docs that have no migration version
+  - This will force migrations to run on those docs (since docs w/ no migrationVersion are assumed by the REST API to be up to date)
+- If we receive a document whose migration version is greater than that of our index,
+  - Reject it
+- If we receive a CREATE document whose migration version is less than / equal to our index
+  - Migrate it if the requisite plugins are available
+  - Save it if the requisite plugins are unavailable (this will trigger a full index migration when the plugin is enabled)
+- If we receive an UPDATE document whose version is less than our index
+  - Reject it
+
+
+## Migration version
+
+Migrations add an index mapping for a `migrationVersion` property which looks something like this:
+
+```js
+migrationVersion: '1.2.3',
+```
+
+This is stored per-document. Future versions of Kibana may allow multiple plugins to define migrations for a single document type (e.g. maybe a security plugin which adds acls to various documents, and may want to migrate the acl property). If that ends up becoming a reality, we won't need to change the `migrationVersion` mapping; it will simply be used as an array, instead: `migrationVersion: ['dashboard:1.2.3', 'acl:3.4.5']`.
+
+Each migrated index also has a `migrationVersion` which is stored in the index's mapping metdata and is used to quickly determine whether or not an index needs to be migrated. This `migrationVersion` might look something like this:
+
+```js
+_meta.migrationVersion: ['dashboard:1.2.3', 'acl:3.4.5']
+```
+
+Here, the format is `type:version`. It is stored this way simply so that the index migration version is compatible with the possible future multi-type document `migrationVersion`. If it is ever stored in a document, it won't require an explosion in properties.
--- src/server/saved_objects/migrations/core/types.ts
+++ src/server/saved_objects/migrations/core/types.ts
@@ -17,6 +17,38 @@
  * under the License.
  */
 
+export type LogFn = (path: string[], message: string) => void;
+
+///////////////////////////////////////////////////////////////////
+// elasticsearch.js wrapper function type definition
+///////////////////////////////////////////////////////////////////
+
+export interface CallCluster {
+  (path: 'bulk', opts: { body: object[] }): Promise<BulkResult>;
+  (path: 'indices.create', opts: IndexCreationOpts): Promise<any>;
+  (path: 'indices.get', opts: IndexOpts & Ignorable): Promise<
+    IndicesInfo | NotFound
+  >;
+  (path: 'indices.getAlias', opts: { name: string }): Promise<AliasResult>;
+  (path: 'indices.getMapping', opts: IndexOpts): Promise<MappingResult>;
+  (path: 'indices.getSettings', opts: IndexOpts): Promise<IndexSettingsResult>;
+  (path: 'indices.putMapping', opts: PutMappingOpts): Promise<any>;
+  (path: 'indices.putTemplate', opts: PutTemplateOpts): Promise<any>;
+  (path: 'indices.refresh', opts: IndexOpts): Promise<any>;
+  (path: 'indices.updateAliases', opts: UpdateAliasesOpts): Promise<any>;
+  (path: 'reindex', opts: ReindexOpts): Promise<any>;
+  (path: 'scroll', opts: ScrollOpts): Promise<SearchResults>;
+  (path: 'search', opts: SearchOpts): Promise<SearchResults>;
+}
+
+///////////////////////////////////////////////////////////////////
+// elasticsearch.js method argument types
+///////////////////////////////////////////////////////////////////
+
+export interface Ignorable {
+  ignore: number[];
+}
+
 export interface PutMappingOpts {
   body: DocMapping;
   index: string;
@@ -35,17 +67,133 @@ export interface PutTemplateOpts {
   };
 }
 
+export interface IndexOpts {
+  index: string;
+}
+
+export interface IndexCreationOpts {
+  index: string;
+  body?: {
+    mappings?: IndexMapping;
+    settings?: {
+      index: {
+        number_of_shards: string;
+        number_of_replicas: string;
+      };
+    };
+  };
+}
+
+export interface ReindexOpts {
+  body: {
+    dest: IndexOpts;
+    source: IndexOpts;
+  };
+  refresh: boolean;
+  waitForCompletion: boolean;
+}
+
+export type AliasAction =
+  | { remove_index: IndexOpts }
+  | { remove: { index: string; alias: string } }
+  | { add: { index: string; alias: string } };
+
+export interface UpdateAliasesOpts {
+  body: {
+    actions: AliasAction[];
+  };
+}
+
+export interface SearchOpts {
+  body: object;
+  index: string;
+  scroll?: string;
+}
+
+export interface ScrollOpts {
+  scroll: string;
+  scrollId: string;
+}
+
+///////////////////////////////////////////////////////////////////
+// elasticsearch.js method return value types
+///////////////////////////////////////////////////////////////////
+
+export interface NotFound {
+  status: 404;
+}
+
 export interface MappingResult {
   [index: string]: {
     mappings: IndexMapping;
   };
 }
 
-export interface CallCluster {
-  (path: 'indices.putMapping', opts: PutMappingOpts): Promise<any>;
-  (path: 'indices.getMapping', opts: { index: string }): Promise<MappingResult>;
-  (path: 'indices.putTemplate', opts: PutTemplateOpts): Promise<any>;
-  (path: 'indices.exists', opts: { index: string }): Promise<boolean>;
+export interface AliasResult {
+  [alias: string]: object;
+}
+
+export interface IndexSettingsResult {
+  [indexName: string]: {
+    settings: {
+      index: {
+        number_of_shards: string;
+        auto_expand_replicas: string;
+        provided_name: string;
+        creation_date: string;
+        number_of_replicas: string;
+        uuid: string;
+        version: { created: '7000001' };
+      };
+    };
+  };
+}
+
+export interface SearchResults {
+  hits: {
+    hits: RawDoc[];
+  };
+  _scroll_id?: string;
+}
+
+export interface BulkResult {
+  items: Array<{ index: { error?: { type: string; reason: string } } }>;
+}
+
+export interface IndexInfo {
+  aliases: AliasResult;
+  mappings: IndexMapping;
+}
+
+export interface IndicesInfo {
+  [index: string]: IndexInfo;
+}
+
+///////////////////////////////////////////////////////////////////
+// Documents
+///////////////////////////////////////////////////////////////////
+
+export interface RawDoc {
+  _id: string;
+  _source: any;
+  _type?: string;
+}
+
+export interface SavedObjectDoc {
+  attributes: any;
+  id: string;
+  type: string;
+  migrationVersion?: string;
+}
+
+///////////////////////////////////////////////////////////////////
+// Migration plugin and dependent types
+///////////////////////////////////////////////////////////////////
+
+export interface MigrationPlugin {
+  id: string;
+  mappings?: MappingDefinition;
+  migrations?: MigrationDefinition;
 }
 
 export interface MappingDefinition {
@@ -54,6 +202,7 @@ export interface MappingDefinition {
 
 export interface DocMapping {
   dynamic: string;
+  _meta?: { migrationVersion: string[] };
   properties: MappingDefinition;
 }
 
@@ -61,7 +210,12 @@ export interface IndexMapping {
   doc: DocMapping;
 }
 
-export interface MigrationPlugin {
-  id: string;
-  mappings?: MappingDefinition;
+export type TransformFn = (doc: SavedObjectDoc) => SavedObjectDoc;
+
+export interface VersionTransforms {
+  [version: string]: TransformFn;
+}
+
+export interface MigrationDefinition {
+  [type: string]: VersionTransforms;
 }
