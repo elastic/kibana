46e7c714bf0915753bb2f57046e59f7671b8747a
Larry Gregory <larry.gregory@elastic.co>
2018-06-27 14:18:44 -0400

    update for rxjs6 compat

--- x-pack/plugins/spaces/server/lib/mirror_status_and_initialize.js
+++ x-pack/plugins/spaces/server/lib/mirror_status_and_initialize.js
@@ -4,16 +4,18 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Observable } from 'rxjs';
+import * as Rx from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+
 
 export function mirrorStatusAndInitialize(upstreamStatus, downstreamStatus, onGreen) {
-  const currentState$ = Observable
+  const currentState$ = Rx
     .of({
       state: upstreamStatus.state,
       message: upstreamStatus.message,
     });
 
-  const newState$ = Observable
+  const newState$ = Rx
     .fromEvent(upstreamStatus, 'change', null, (previousState, previousMsg, state, message) => {
       return {
         state,
@@ -21,10 +23,10 @@ export function mirrorStatusAndInitialize(upstreamStatus, downstreamStatus, onGr
       };
     });
 
-  const state$ = Observable.merge(currentState$, newState$);
+  const state$ = Rx.merge(currentState$, newState$);
 
   let onGreenPromise;
-  const onGreen$ = Observable.create(observer => {
+  const onGreen$ = Rx.Observable.create(observer => {
     if (!onGreenPromise) {
       onGreenPromise = onGreen();
     }
@@ -47,15 +49,17 @@ export function mirrorStatusAndInitialize(upstreamStatus, downstreamStatus, onGr
 
 
   state$
-    .switchMap(({ state, message }) => {
-      if (state !== 'green') {
-        return Observable.of({ state, message });
-      }
+    .pipe(
+      switchMap(({ state, message }) => {
+        if (state !== 'green') {
+          return Rx.of({ state, message });
+        }
 
-      return onGreen$;
-    })
-    .do(({ state, message }) => {
-      downstreamStatus[state](message);
-    })
+        return onGreen$;
+      }),
+      tap(({ state, message }) => {
+        downstreamStatus[state](message);
+      })
+    )
     .subscribe();
 }
