fb0d0b28bcf5ed40043dfb6e32cc7856217c1368
Lee Drengenberg <lee.drengenberg@elastic.co>
2018-06-27 16:59:40 -0500

    ES 6 visualize tests (#20178)
    * ES 6 visualize tests
    
    * metric_chart es6
    
    * correct test names, no screenshots
    
    * switch spy panel to inspector
    
    * ES 6 visualize tests
    
    * metric_chart es6
    
    * correct test names, no screenshots
    
    * switch spy panel to inspector
    
    * es6 some more tests
    
    * un-skip tag_cloud tests
    
    * fix data table indentation

--- test/functional/apps/visualize/_pie_chart.js
+++ test/functional/apps/visualize/_pie_chart.js
@@ -25,165 +25,93 @@ export default function ({ getService, getPageObjects }) {
   const fromTime = '2015-09-19 06:31:44.000';
   const toTime = '2015-09-23 18:31:44.000';
 
-  describe('visualize app', function describeIndexTests() {
-    before(function () {
+  describe('pie chart', async function () {
+    const vizName1 = 'Visualization PieChart';
+    before(async function () {
 
       log.debug('navigateToApp visualize');
-      return PageObjects.common.navigateToUrl('visualize', 'new')
-        .then(function () {
-          log.debug('clickPieChart');
-          return PageObjects.visualize.clickPieChart();
-        })
-        .then(function clickNewSearch() {
-          return PageObjects.visualize.clickNewSearch();
-        })
-        .then(function setAbsoluteRange() {
-          log.debug('Set absolute time range from \"' + fromTime + '\" to \"' + toTime + '\"');
-          return PageObjects.header.setAbsoluteRange(fromTime, toTime);
-        })
-        .then(function () {
-          log.debug('select bucket Split Slices');
-          return PageObjects.visualize.clickBucket('Split Slices');
-        })
-        .then(function () {
-          log.debug('Click aggregation Histogram');
-          return PageObjects.visualize.selectAggregation('Histogram');
-        })
-        .then(function () {
-          log.debug('Click field memory');
-          return PageObjects.visualize.selectField('memory');
-        })
-        .then(function () {
-          return PageObjects.header.waitUntilLoadingHasFinished();
-        })
-        .then(function sleep() {
-          return PageObjects.common.sleep(1003);
-        })
-        .then(function () {
-          log.debug('setNumericInterval 4000');
-          return PageObjects.visualize.setNumericInterval('40000');
-        })
-        .then(function () {
-          log.debug('clickGo');
-          return PageObjects.visualize.clickGo();
-        })
-        .then(function () {
-          return PageObjects.header.waitUntilLoadingHasFinished();
-        });
+      await PageObjects.common.navigateToUrl('visualize', 'new');
+      log.debug('clickPieChart');
+      await PageObjects.visualize.clickPieChart();
+      await PageObjects.visualize.clickNewSearch();
+      log.debug('Set absolute time range from \"' + fromTime + '\" to \"' + toTime + '\"');
+      await PageObjects.header.setAbsoluteRange(fromTime, toTime);
+      log.debug('select bucket Split Slices');
+      await PageObjects.visualize.clickBucket('Split Slices');
+      log.debug('Click aggregation Histogram');
+      await PageObjects.visualize.selectAggregation('Histogram');
+      log.debug('Click field memory');
+      await PageObjects.visualize.selectField('memory');
+      await PageObjects.header.waitUntilLoadingHasFinished();
+      await PageObjects.common.sleep(1003);
+      log.debug('setNumericInterval 4000');
+      await PageObjects.visualize.setNumericInterval('40000');
+      log.debug('clickGo');
+      await PageObjects.visualize.clickGo();
+    });
+
+    it('should save and load', async function () {
+      await PageObjects.visualize.saveVisualization(vizName1);
+      const pageTitle = await PageObjects.common.getBreadcrumbPageTitle();
+      log.debug(`Save viz page title is ${pageTitle}`);
+      expect(pageTitle).to.contain(vizName1);
+      await PageObjects.header.waitForToastMessageGone();
+      await PageObjects.visualize.loadSavedVisualization(vizName1);
+      await PageObjects.visualize.waitForVisualization();
+      // sleep a bit before trying to get the pie chart data below
+      await PageObjects.common.sleep(2000);
+    });
+
+    it('should have inspector enabled', async function () {
+      const spyToggleExists = await PageObjects.visualize.isInspectorButtonEnabled();
+      expect(spyToggleExists).to.be(true);
     });
 
 
-    describe('pie chart', function indexPatternCreation() {
-      const vizName1 = 'Visualization PieChart';
+    it('should show 10 slices in pie chart', async function () {
+      const expectedPieChartSliceCount = 10;
 
-      it('should save and load', function () {
-        return PageObjects.visualize.saveVisualization(vizName1)
-          .then(() => {
-            return PageObjects.common.getBreadcrumbPageTitle();
-          })
-          .then(pageTitle => {
-            log.debug(`Save viz page title is ${pageTitle}`);
-            expect(pageTitle).to.contain(vizName1);
-          })
-          .then(function testVisualizeWaitForToastMessageGone() {
-            return PageObjects.header.waitForToastMessageGone();
-          })
-          .then(function () {
-            return PageObjects.visualize.loadSavedVisualization(vizName1);
-          })
-          .then(function waitForVisualization() {
-            return PageObjects.visualize.waitForVisualization();
-          })
-        // sleep a bit before trying to get the pie chart data below
-          .then(function sleep() {
-            return PageObjects.common.sleep(2000);
-          });
-      });
+      const pieData = await PageObjects.visualize.getPieChartData();
+      log.debug('pieData.length = ' + pieData.length);
+      expect(pieData.length).to.be(expectedPieChartSliceCount);
+    });
 
-      it('should have inspector enabled', async function () {
-        const spyToggleExists = await PageObjects.visualize.isInspectorButtonEnabled();
-        expect(spyToggleExists).to.be(true);
-      });
+    it('should show correct data', async function () {
+      const expectedTableData =  [['0', '55'], ['40,000', '50'], ['80,000', '41'], ['120,000', '43'],
+        ['160,000', '44'], ['200,000', '40'], ['240,000', '46'], ['280,000', '39'], ['320,000', '40'], ['360,000', '47']
+      ];
 
-      it('should show 10 slices in pie chart, take screenshot', function () {
-        const expectedPieChartSliceCount = 10;
 
-        return PageObjects.visualize.getPieChartData()
-          .then(function (pieData) {
-            log.debug('pieData.length = ' + pieData.length);
-            expect(pieData.length).to.be(expectedPieChartSliceCount);
-          });
-      });
+      await PageObjects.visualize.openInspector();
+      await PageObjects.visualize.setInspectorTablePageSize(50);
+      const data =  await PageObjects.visualize.getInspectorTableData();
+      log.debug(data);
+      expect(data).to.eql(expectedTableData);
+    });
 
-      it('should show correct data', function () {
-        const expectedTableData =  [['0', '55'], ['40,000', '50'], ['80,000', '41'], ['120,000', '43'],
-          ['160,000', '44'], ['200,000', '40'], ['240,000', '46'], ['280,000', '39'], ['320,000', '40'], ['360,000', '47']
-        ];
+    it('should show other and missing bucket', async function () {
+      const expectedTableData =  [ 'win 8', 'win xp', 'win 7', 'ios', 'Missing', 'Other' ];
 
-        return PageObjects.visualize.openInspector()
-          .then(function () {
-            return PageObjects.visualize.setInspectorTablePageSize(50);
-          })
-          .then(function getInspectorTableData() {
-            return PageObjects.visualize.getInspectorTableData();
-          })
-          .then(function showData(data) {
-            log.debug(data);
-            expect(data).to.eql(expectedTableData);
-          });
-      });
-
-      it('should show other and missing bucket', function () {
-        const expectedTableData =  [ 'win 8', 'win xp', 'win 7', 'ios', 'Missing', 'Other' ];
-
-        return PageObjects.common.navigateToUrl('visualize', 'new')
-          .then(function () {
-            log.debug('clickPieChart');
-            return PageObjects.visualize.clickPieChart();
-          })
-          .then(function clickNewSearch() {
-            return PageObjects.visualize.clickNewSearch();
-          })
-          .then(function setAbsoluteRange() {
-            log.debug('Set absolute time range from \"' + fromTime + '\" to \"' + toTime + '\"');
-            return PageObjects.header.setAbsoluteRange(fromTime, toTime);
-          })
-          .then(function () {
-            log.debug('select bucket Split Slices');
-            return PageObjects.visualize.clickBucket('Split Slices');
-          })
-          .then(function () {
-            log.debug('Click aggregation Histogram');
-            return PageObjects.visualize.selectAggregation('Terms');
-          })
-          .then(function () {
-            log.debug('Click field memory');
-            return PageObjects.visualize.selectField('machine.os.raw');
-          })
-          .then(function () {
-            return PageObjects.visualize.toggleOtherBucket();
-          })
-          .then(function () {
-            return PageObjects.visualize.toggleMissingBucket();
-          })
-          .then(function () {
-            log.debug('clickGo');
-            return PageObjects.visualize.clickGo();
-          })
-          .then(function waitForVisualization() {
-            return PageObjects.header.waitUntilLoadingHasFinished();
-          })
-          .then(function sleep() {
-            return PageObjects.common.sleep(1003);
-          })
-          .then(function () {
-            return PageObjects.visualize.getPieChartLabels();
-          })
-          .then(function (pieData) {
-            log.debug('pieData.length = ' + pieData.length);
-            expect(pieData).to.eql(expectedTableData);
-          });
-      });
+      await PageObjects.common.navigateToUrl('visualize', 'new');
+      log.debug('clickPieChart');
+      await PageObjects.visualize.clickPieChart();
+      await PageObjects.visualize.clickNewSearch();
+      log.debug('Set absolute time range from \"' + fromTime + '\" to \"' + toTime + '\"');
+      await PageObjects.header.setAbsoluteRange(fromTime, toTime);
+      log.debug('select bucket Split Slices');
+      await PageObjects.visualize.clickBucket('Split Slices');
+      log.debug('Click aggregation Histogram');
+      await PageObjects.visualize.selectAggregation('Terms');
+      log.debug('Click field memory');
+      await PageObjects.visualize.selectField('machine.os.raw');
+      await PageObjects.visualize.toggleOtherBucket();
+      await PageObjects.visualize.toggleMissingBucket();
+      log.debug('clickGo');
+      await PageObjects.visualize.clickGo();
+      await PageObjects.common.sleep(1003);
+      const pieData = await PageObjects.visualize.getPieChartLabels();
+      log.debug('pieData.length = ' + pieData.length);
+      expect(pieData).to.eql(expectedTableData);
     });
   });
 }
