30068bdf7fe2d8a23c12d5b502c02117390e9d85
Aaron Caldwell <Aaron.Caldwell@elastic.co>
2018-06-27 15:13:47 -0600

    Add vector source selectors to support getting vector lists as well as those lists sorted by name

--- x-pack/plugins/gis/public/selectors/map_selectors.js
+++ x-pack/plugins/gis/public/selectors/map_selectors.js
@@ -4,6 +4,22 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { createSelector } from 'reselect';
+import _ from 'lodash';
+
 export const getSelectedLayer = ({ map }) => map && map.selectedLayer;
 
 export const getLayerList = ({ map }) => map && map.layerList;
+
+export const getVectorLayerOptions = ({ map }) => map && map.vectorSources;
+
+export function getVectorLayerOptionsByName(state) {
+  return createSelector(
+    getVectorLayerOptions,
+    vectorLayerOptions => {
+      return _.isEmpty(vectorLayerOptions) ? []
+        : _.groupBy(vectorLayerOptions, ({ name }) => name);
+    }
+  )(state);
+}
+
