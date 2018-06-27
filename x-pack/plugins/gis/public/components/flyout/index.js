895ebec199439ccab2ac1d572c32920a7561aa8b
Aaron Caldwell <Aaron.Caldwell@elastic.co>
2018-06-27 15:15:26 -0600

    Bind ems service logic to flyout for basic 'add' capability

--- x-pack/plugins/gis/public/components/flyout/index.js
+++ x-pack/plugins/gis/public/components/flyout/index.js
@@ -7,15 +7,23 @@
 import { connect } from 'react-redux';
 import { FlyOut } from './view';
 import { getFlyoutDisplay, updateFlyout, FLYOUT_STATE } from '../../store/ui';
+import { getVectorLayerOptionsByName } from "../../selectors/map_selectors";
+import { addVectorLayer } from "../../actions/map_actions";
+import _ from 'lodash';
 
 function mapStateToProps(state = {}) {
+  const namedVectorLayers = getVectorLayerOptionsByName(state);
+  const emsVectorLayers = _.isEmpty(namedVectorLayers) ? null
+    : namedVectorLayers.ems_source[0].service;
   return {
-    flyoutVisible: getFlyoutDisplay(state) !== FLYOUT_STATE.NONE
+    flyoutVisible: getFlyoutDisplay(state) !== FLYOUT_STATE.NONE,
+    emsLayerOptions: emsVectorLayers
   };
 }
 
 const mapDispatchToProps = {
-  closeFlyout: () => updateFlyout(FLYOUT_STATE.NONE)
+  closeFlyout: () => updateFlyout(FLYOUT_STATE.NONE),
+  selectAction: layerName => addVectorLayer('ems_source', layerName)
 };
 
 const connectedFlyOut = connect(mapStateToProps, mapDispatchToProps, null, { withRef: true })(FlyOut);
