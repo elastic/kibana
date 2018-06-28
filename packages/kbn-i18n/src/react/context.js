25c13f803507e6129b0962ebb582b7d01929b039
Maxim Tolochko <maksim.tolo@gmail.com>
2018-06-28 12:38:39 +0300

    Add draft implementation of I18n engine (#19555)
    * Add draft implementation of I18n engine
    
    * Add i18n loader
    
    * kbn-i18n refactoring
    
    * Fix react i18n context and update doc
    
    * i18n engine refactoring
    
    * Fix locales data loading and add more jsdoc comments
    
    * Fix verify_translations task
    
    * I18n tests refactoring
    
    * Add build scripts to kbn-i18n package
    
    * Fix some bugs
    
    * Move uiI18nMixin into ui_i18n folder
    
    * Add 'browser' field to kbn-i18n package.json
    
    * Get rid of "showError" method
    
    * Make i18n and i18nLoader a singleton object
    
    * Add default locale as fallback if translation files were not registered
    
    * Update yarn.lock
    
    * kbn-i18n fix
    
    * Add default formats
    
    * Try to fix build
    
    * Add more examples into kbn-i18n/README.md
    
    * kbn-i18n fix
    
    * Fix app_bootstrap tests
    
    * Add links to issues in TODO comments

---
+++ packages/kbn-i18n/src/react/context.js
@@ -0,0 +1,50 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { intlShape } from 'react-intl';
+
+/**
+ * Provides intl context to a child component using React render callback pattern
+ * @example
+ * <I18nContext>
+ *   {intl => (
+ *     <input
+ *       placeholder={intl.formatMessage({
+           id: 'my-id',
+           defaultMessage: 'my default message',
+         })}
+ *     />
+ *   )}
+ * </I18nContext>
+ */
+export class I18nContext extends PureComponent {
+  static propTypes = {
+    children: PropTypes.func.isRequired,
+  };
+
+  static contextTypes = {
+    intl: intlShape,
+  };
+
+  render() {
+    return this.props.children(this.context.intl);
+  }
+}
--- src/ui/public/autoload/modules.js
+++ src/ui/public/autoload/modules.js
@@ -53,3 +53,4 @@ import '../validate_date_interval';
 import '../watch_multi';
 import '../courier/saved_object/ui/saved_object_save_as_checkbox';
 import '../react_components';
+import '../i18n';
