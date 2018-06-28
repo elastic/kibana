8b7d552c63cde1a34ea03875a39b87db265181b3
Justin Kambic <justin.kambic@elastic.co>
2018-06-28 16:51:18 -0400

    Test StatementSection component. Move StatementList component to dedicated file.

--- x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/views/statement_section.js
+++ x-pack/plugins/monitoring/public/components/logstash/pipeline_viewer/views/statement_section.js
@@ -7,8 +7,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StatementListHeading } from './statement_list_heading';
-import { Statement } from './statement';
 import { EuiSpacer } from '@elastic/eui';
+import { StatementList } from './statement_list';
 
 export function StatementSection({
   iconType,
@@ -33,82 +33,7 @@ export function StatementSection({
   );
 }
 
-function getCollapsedChildIds(elements, collapsedIds) {
-  const collapsedChildIds = new Set();
-  elements.forEach(({ id, parentId }) => {
-    if (collapsedIds.has(parentId) || collapsedChildIds.has(parentId)) {
-      collapsedChildIds.add(id);
-    }
-  });
-  return collapsedChildIds;
-}
-
-class StatementList extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collapsedIds: new Set(),
-      collapsedChildIds: new Set()
-    };
-  }
-
-  expand = elementId => {
-    const collapsedIds = new Set(this.state.collapsedIds);
-    collapsedIds.delete(elementId);
-    this.updateCollapsedElement(collapsedIds);
-  }
-
-  collapse = elementId => {
-    const collapsedIds = new Set(this.state.collapsedIds);
-    collapsedIds.add(elementId);
-    this.updateCollapsedElement(collapsedIds);
-  }
-
-  updateCollapsedElement = collapsedIds => {
-    const { elements } = this.props;
-    const collapsedChildIds = getCollapsedChildIds(elements, collapsedIds);
-
-    this.setState({
-      collapsedIds,
-      collapsedChildIds
-    });
-  }
-
-  elementIsCollapsed = elementId => this.state.collapsedIds.has(elementId);
-
-  renderStatement = element => {
-    const { id, parentId } = element;
-    const { onShowVertexDetails } = this.props;
-
-    return this.state.collapsedIds.has(parentId) || this.state.collapsedChildIds.has(parentId)
-      ? null
-      : (
-        <Statement
-          key={id}
-          element={element}
-          collapse={this.collapse}
-          expand={this.expand}
-          isCollapsed={this.elementIsCollapsed(id)}
-          onShowVertexDetails={onShowVertexDetails}
-        />
-      );
-  }
-
-  render() {
-    const { elements } = this.props;
-
-    return (
-      <ul className="pipelineViewer__list">
-        {
-          elements.map(this.renderStatement)
-        }
-      </ul>
-    );
-  }
-}
-
-StatementList.propTypes = {
+StatementSection.propTypes = {
   elements: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
@@ -116,5 +41,7 @@ StatementList.propTypes = {
       parentId: PropTypes.string
     })
   ).isRequired,
+  headingText: PropTypes.string.isRequired,
+  iconType: PropTypes.string.isRequired,
   onShowVertexDetails: PropTypes.func.isRequired,
 };
