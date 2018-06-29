376ea9332ff934a80f1346102599be6ef7ded89d
Maxim Tolochko <maksim.tolo@gmail.com>
2018-06-29 13:02:20 +0300

    Add unit-tests for i18n engine

---
+++ packages/kbn-i18n/src/react/provider.test.js
@@ -0,0 +1,57 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from 'react';
+import { intlShape } from 'react-intl';
+import { shallow, mount } from 'enzyme';
+import { I18nProvider } from './provider';
+import { I18nContext } from './context';
+
+describe('I18nProvider', () => {
+  it('renders children', () => {
+    const ChildrenMock = () => {};
+
+    const wrapper = shallow(
+      <I18nProvider>
+        <ChildrenMock />
+      </I18nProvider>
+    );
+
+    expect(wrapper.children().length).toEqual(1);
+    expect(wrapper.children().type()).toEqual(ChildrenMock);
+  });
+
+  it('provides with context', () => {
+    const childrenMock = jest.fn().mockImplementationOnce(() => <div />);
+
+    mount(
+      <I18nProvider>
+        <I18nContext>{childrenMock}</I18nContext>
+      </I18nProvider>,
+      {
+        childContextTypes: {
+          intl: intlShape,
+        },
+      }
+    );
+
+    expect(childrenMock).toHaveBeenCalled();
+    expect(childrenMock.mock.calls[0]).toBeTruthy();
+  });
+});
