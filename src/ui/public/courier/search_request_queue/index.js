57f23065e4e3282a75c9a7fe84d47bcc83ec57f1
CJ Cenizal <cj@cenizal.com>
2018-06-29 19:35:22 -0700

    Aggregate SearchRequestQueue functions into a single module (#20332) (#20363)
    * Refactor all functions operating on requestQueue to be instance methods of the SearchRequestQueue class.
    * Rename requestQueue to searchRequestQueue.

--- src/ui/public/courier/search_request_queue/index.js
+++ src/ui/public/courier/search_request_queue/index.js
@@ -17,26 +17,4 @@
  * under the License.
  */
 
-
-/**
- * Queue of pending requests, requests are removed as
- * they are processed by fetch.[sourceType]().
- * @type {Array}
- */
-export const requestQueue = [];
-
-requestQueue.clear = function () {
-  requestQueue.splice(0, requestQueue.length);
-};
-
-requestQueue.getInactive = function () {
-  return requestQueue.filter(function (req) {
-    return !req.started;
-  });
-};
-
-requestQueue.getStartable = function () {
-  return requestQueue.filter(req => req.canStart());
-};
-
-
+export { searchRequestQueue } from './search_request_queue';
