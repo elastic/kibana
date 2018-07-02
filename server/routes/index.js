06f16c5ef81f8abba5900ae8c3a613d96d150833
Matt Bargar <mbargar@gmail.com>
2018-07-02 15:54:06 -0400

    Remove run API (#742)
    It's not used anywhere and is no longer necessary. We can always recreate
    it if the need arises.

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,9 @@ import { socketApi } from './socket';
 import { translate } from './translate';
 import { esFields } from './es_fields';
 import { esIndices } from './es_indices';
-import { runApi } from './run';
 import { getAuth } from './get_auth';
 
 export function routes(server) {
-  runApi(server);
   workpad(server);
   socketApi(server);
   translate(server);
