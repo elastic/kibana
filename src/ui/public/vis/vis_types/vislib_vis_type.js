fb96647b6020b2f5712bac2c3e21ea7840979461
Peter Pisljar <peter.pisljar@gmail.com>
2018-07-05 13:31:28 +0200

    moving visualize legend into vislib legend (#20479)

--- src/ui/public/vis/vis_types/vislib_vis_type.js
+++ src/ui/public/vis/vis_types/vislib_vis_type.js
@@ -23,7 +23,7 @@ import 'plugins/kbn_vislib_vis_types/controls/line_interpolation_option';
 import 'plugins/kbn_vislib_vis_types/controls/heatmap_options';
 import 'plugins/kbn_vislib_vis_types/controls/gauge_options';
 import 'plugins/kbn_vislib_vis_types/controls/point_series';
-import '../../visualize/visualize_legend';
+import './vislib_vis_legend';
 import { BaseVisType } from './base_vis_type';
 import { AggResponsePointSeriesProvider } from '../../agg_response/point_series/point_series';
 import VislibProvider from '../../vislib';
@@ -76,7 +76,7 @@ export function VislibVisTypeProvider(Private, $rootScope, $timeout, $compile) {
           $scope.vis = this.vis;
           $scope.visData = esResponse;
           $scope.uiState = $scope.vis.getUiState();
-          const legendHtml = $compile('<visualize-legend></visualize-legend>')($scope);
+          const legendHtml = $compile('<vislib-legend></vislib-legend>')($scope);
           this.container.appendChild(legendHtml[0]);
           $scope.$digest();
           // We need to wait one digest cycle for the legend to render, before
@@ -104,7 +104,7 @@ export function VislibVisTypeProvider(Private, $rootScope, $timeout, $compile) {
         this.vis.vislibVis.destroy();
         delete this.vis.vislibVis;
       }
-      $(this.container).find('visualize-legend').remove();
+      $(this.container).find('vislib-legend').remove();
     }
   }
 
