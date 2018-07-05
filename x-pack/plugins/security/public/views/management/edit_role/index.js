a1720afb8bb64f1599fd5328d401411b7fa3f2ff
Larry Gregory <larry.gregory@elastic.co>
2018-07-05 16:45:40 -0400

    fix merge 'conflict'

--- x-pack/plugins/security/public/views/management/edit_role/index.js
+++ x-pack/plugins/security/public/views/management/edit_role/index.js
@@ -60,8 +60,8 @@ routes.when(`${EDIT_ROLES_PATH}/:name?`, {
 
       return role.then(res => res.toJSON());
     },
-    kibanaApplicationPrivilege(ApplicationPrivilege, kbnUrl, Promise, Private) {
-      return ApplicationPrivilege.query().$promise
+    kibanaApplicationPrivilege(ApplicationPrivileges, kbnUrl, Promise, Private) {
+      return ApplicationPrivileges.query().$promise
         .then(privileges => privileges.map(p => p.toJSON()))
         .catch(checkLicenseError(kbnUrl, Promise, Private));
     },
