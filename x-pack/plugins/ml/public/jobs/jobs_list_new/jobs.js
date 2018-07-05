7cc91f7787c5bbc2b1a6be1600efb1a7c019aa59
James Gowdy <jgowdy@elastic.co>
2018-07-05 14:13:31 +0100

    [ML] Adding permission checks to jobs list

--- x-pack/plugins/ml/public/jobs/jobs_list_new/jobs.js
+++ x-pack/plugins/ml/public/jobs/jobs_list_new/jobs.js
@@ -8,6 +8,7 @@
 import './styles/main.less';
 import { NewJobButton } from './components/new_job_button';
 import { JobsListView } from './components/jobs_list_view';
+import { mlNodesAvailable, permissionToViewMlNodeCount } from 'plugins/ml/ml_nodes_check/check_ml_nodes';
 
 import React, {
   Component
@@ -15,12 +16,41 @@ import React, {
 
 import {
   EuiHorizontalRule,
+  EuiCallOut,
+  EuiLink,
+  EuiSpacer,
 } from '@elastic/eui';
+
+function NodeAvailableWarning() {
+  const isCloud = false;
+  if ((mlNodesAvailable() === true) || (permissionToViewMlNodeCount() === false)) {
+    return (<span />);
+  } else {
+    return (
+      <React.Fragment>
+        <EuiCallOut
+          title="No ML Nodes available"
+          color="warning"
+          iconType="alert"
+        >
+          <p>
+            There are no ML nodes available.
+            {isCloud &&
+              <span ng-if="isCloud">
+                &nbsp;This can be configured in Cloud <EuiLink href="#">here</EuiLink>.
+              </span>
+            }
+          </p>
+        </EuiCallOut>
+        <EuiSpacer size="s" />
+      </React.Fragment>
+    );
+  }
+}
 
 export class JobsPage extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
     };
   }
@@ -28,6 +58,7 @@ export class JobsPage extends Component {
   render() {
     return (
       <div className="job-management">
+        <NodeAvailableWarning />
         <header>
           <div className="new-job-button-container">
             <NewJobButton />
