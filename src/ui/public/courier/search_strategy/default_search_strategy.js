a0386eaf16ed845e21d256cf6e0221e0cb9e80ae
CJ Cenizal <cj@cenizal.com>
2018-07-05 21:06:42 -0700

    Move fetch param logic from CallClient into defaultSearchStrategy.

--- src/ui/public/courier/search_strategy/default_search_strategy.js
+++ src/ui/public/courier/search_strategy/default_search_strategy.js
@@ -17,11 +17,46 @@
  * under the License.
  */
 
+function getAllFetchParams(searchRequests, Promise) {
+  return Promise.map(searchRequests, (searchRequest) => {
+    return Promise.try(searchRequest.getFetchParams, void 0, searchRequest)
+      .then((fetchParams) => {
+        return (searchRequest.fetchParams = fetchParams);
+      })
+      .then(value => ({ resolved: value }))
+      .catch(error => ({ rejected: error }));
+  });
+}
+
+function serializeAllFetchParams(fetchParams, searchRequests, serializeFetchParams) {
+  const requestsWithFetchParams = [];
+
+  // Gather the fetch param responses from all the successful requests.
+  fetchParams.forEach((result, index) => {
+    if (result.resolved) {
+      requestsWithFetchParams.push(result.resolved);
+    } else {
+      const request = searchRequests[index];
+      request.handleFailure(result.rejected);
+      searchRequests[index] = undefined;
+    }
+  });
+
+  return serializeFetchParams(requestsWithFetchParams);
+}
+
 export const defaultSearchStrategy = {
   id: 'default',
 
-  search: ({ body, es }) => {
-    return es.msearch({ body });
+  search: async ({ searchRequests, es, Promise, serializeFetchParams }) => {
+    // Flatten the searchSource within each searchRequest to get the fetch params,
+    // e.g. body, filters, index pattern, query.
+    const allFetchParams = await getAllFetchParams(searchRequests, Promise);
+
+    // Serialize the fetch params into a format suitable for the body of an ES query.
+    const serializedFetchParams = await serializeAllFetchParams(allFetchParams, searchRequests, serializeFetchParams);
+
+    return es.msearch({ body: serializedFetchParams });
   },
 
   isValidForSearchRequest: searchRequest => {
