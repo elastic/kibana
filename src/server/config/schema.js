aa932ec4a8fa477c77a67055e1250ff05e7b93ee
spalger <email@spalger.com>
2018-07-05 18:10:28 -0700

    [optimizer] implement cache invalidation checks

--- src/server/config/schema.js
+++ src/server/config/schema.js
@@ -163,6 +163,10 @@ export default async () => Joi.object({
     watchPrebuild: Joi.boolean().default(false),
     watchProxyTimeout: Joi.number().default(5 * 60000),
     useBundleCache: Joi.boolean().default(Joi.ref('$prod')),
+    cache: Joi.object({
+      checkYarnLock: Joi.boolean().default(true),
+      checkIncompleteCompile: Joi.boolean().default(true)
+    }),
     unsafeCache: Joi.when('$prod', {
       is: true,
       then: Joi.boolean().valid(false),
