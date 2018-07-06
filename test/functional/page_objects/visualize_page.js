61d7b918c3139d010b0db69aefb455b2ecd5c1d8
Nathan Reese <reese.nathan@gmail.com>
2018-07-06 12:15:55 -0600

    Upgrade to EUI 1.1.0 (#20411)
    * Upgrade to EUI 1.0.1
    
    * upgraded to eui 1.1.0
    
    * fix setInspectorTablePageSize function in functional tests
    
    * better comment about basic table pagination buttons

--- test/functional/page_objects/visualize_page.js
+++ test/functional/page_objects/visualize_page.js
@@ -364,7 +364,11 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
     async setInspectorTablePageSize(size) {
       const panel = await testSubjects.find('inspectorPanel');
       await find.clickByButtonText('Rows per page: 20', panel);
-      await find.clickByButtonText(`${size} rows`, panel);
+      // The buttons for setting table page size are in a popover element. This popover
+      // element appears as if it's part of the inspectorPanel but it's really attached
+      // to the body element by a portal.
+      const tableSizesPopover = await find.byCssSelector('.euiPanel');
+      await find.clickByButtonText(`${size} rows`, tableSizesPopover);
     }
 
     async getMetric() {
