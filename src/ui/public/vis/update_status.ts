b0a3f59bef89584d4550a3ffccf761f84fd3b650
Tim Roes <mail@timroes.de>
2018-07-07 00:03:15 +0200

    Improve getUpdateStatus

--- src/ui/public/vis/update_status.ts
+++ src/ui/public/vis/update_status.ts
@@ -18,61 +18,43 @@
  */
 
 import { calculateObjectHash } from './lib/calculate_object_hash';
+import { Vis } from './vis';
 
-const Status = {
-  AGGS: 'aggs',
-  DATA: 'data',
-  PARAMS: 'params',
-  RESIZE: 'resize',
-  TIME: 'time',
-  UI_STATE: 'uiState',
-};
-
-// adapted from https://github.com/isaacs/json-stringify-safe/blob/02cfafd45f06d076ac4bf0dd28be6738a07a72f9/stringify.js
-function serializer() {
-  const stack = [];
-  const keys = [];
-
-  const cycleReplacer = function (key, value) {
-    if (stack[0] === value) return '[Circular ~]';
-    return `[Circular ~.${keys.slice(0, stack.indexOf(value)).join('.')}]`;
-  };
-
-  return function (key, value) {
-    if (stack.length > 0) {
-      const thisPos = stack.indexOf(this);
-      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this);
-      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key);
-      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value);
-    }
-    else stack.push(value);
-
-    return value;
-  };
+enum Status {
+  AGGS = 'aggs',
+  DATA = 'data',
+  PARAMS = 'params',
+  RESIZE = 'resize',
+  TIME = 'time',
+  UI_STATE = 'uiState',
 }
 
-function getUpdateStatus(requiresUpdateStatus, obj, param) {
+function getUpdateStatus<T extends Status>(
+  requiresUpdateStatus: T[] = [],
+  obj: any,
+  param: { vis: Vis; visData: any; uiState: any }
+): { [reqStats in T]: boolean } {
+  const status = {} as { [reqStats in T]: boolean };
 
   // If the vis type doesn't need update status, skip all calculations
-  if (!requiresUpdateStatus) {
-    return {};
+  if (requiresUpdateStatus.length === 0) {
+    return status;
   }
 
   if (!obj._oldStatus) {
     obj._oldStatus = {};
   }
 
-  const hasChangedUsingGenericHashComparison = (name, value) => {
-    const currentValue = JSON.stringify(value, serializer());
-    if (currentValue !== obj._oldStatus[name]) {
-      obj._oldStatus[name] = currentValue;
+  const hasChangedUsingGenericHashComparison = (name: string, value: any) => {
+    const hash = calculateObjectHash(value);
+    if (hash !== obj._oldStatus[name]) {
+      obj._oldStatus[name] = hash;
       return true;
     }
     return false;
   };
 
-  const hasSizeChanged = (currentWidth, currentHeight) => {
-
+  const hasSizeChanged = (currentWidth: number, currentHeight: number) => {
     if (!obj._oldStatus.resize) {
       obj._oldStatus.resize = { width: currentWidth, height: currentHeight };
       return true;
@@ -85,17 +67,6 @@ function getUpdateStatus(requiresUpdateStatus, obj, param) {
     return false;
   };
 
-  const hasDataChanged = (visData) => {
-    const hash = calculateObjectHash(visData);
-    if (hash !== obj._oldStatus.data) {
-      obj._oldStatus.data = hash;
-      return true;
-    }
-    return false;
-  };
-
-  const status = {};
-
   for (const requiredStatus of requiresUpdateStatus) {
     // Calculate all required status updates for this visualization
     switch (requiredStatus) {
@@ -103,7 +74,7 @@ function getUpdateStatus(requiresUpdateStatus, obj, param) {
         status.aggs = hasChangedUsingGenericHashComparison('aggs', param.vis.aggs);
         break;
       case Status.DATA:
-        status.data = hasDataChanged(param.visData);
+        status.data = hasChangedUsingGenericHashComparison('data', param.visData);
         break;
       case Status.PARAMS:
         status.params = hasChangedUsingGenericHashComparison('param', param.vis.params);
@@ -114,8 +85,7 @@ function getUpdateStatus(requiresUpdateStatus, obj, param) {
         status.resize = hasSizeChanged(width, height);
         break;
       case Status.TIME:
-        const time = param.vis.params.timeRange ? param.vis.params.timeRange : param.vis.API.timeFilter.getBounds();
-        status.time = hasChangedUsingGenericHashComparison('time', time);
+        status.time = hasChangedUsingGenericHashComparison('time', param.vis.filters.timeRange);
         break;
       case Status.UI_STATE:
         status.uiState = hasChangedUsingGenericHashComparison('uiState', param.uiState);
