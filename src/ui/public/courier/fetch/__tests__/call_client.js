79e2ce67ed86b520447ca98ac69ac35b784eba8e
CJ Cenizal <cj@cenizal.com>
2018-07-09 21:23:49 -0700

    Add tests about aborting situations.

--- src/ui/public/courier/fetch/__tests__/call_client.js
+++ src/ui/public/courier/fetch/__tests__/call_client.js
@@ -23,41 +23,35 @@ import ngMock from 'ng_mock';
 import NoDigestPromises from 'test_utils/no_digest_promises';
 
 import { CallClientProvider } from '../call_client';
-import { IsRequestProvider } from '../is_request';
+import { RequestStatus } from '../req_status';
+import { SearchRequestProvider } from '../request';
 import { MergeDuplicatesRequestProvider } from '../merge_duplicate_requests';
-
-function mockRequest() {
-  return {
-    strategy: 'mock',
-    started: true,
-    aborted: false,
-    handleFailure: sinon.spy(),
-    retry: sinon.spy(function () { return this; }),
-    continue: sinon.spy(function () { return this; }),
-    start: sinon.spy(function () { return this; }),
-    getFetchParams: () => ({}),
-    source: {},
-    erroHandler: () => {},
-    whenAborted: () => {}, // TODO: Test this
-  };
-}
 
 describe('callClient', () => {
   NoDigestPromises.activateForSuite();
 
+  const ABORTED = RequestStatus.ABORTED;
+
+  let SearchRequest;
   let callClient;
   let fakeSearch;
   let searchRequests;
 
+  const createSearchRequest = (overrides = {}) => {
+    // TODO: Spy on handleFailure, whenAborted
+    const source = {
+      _flatten: () => ({}),
+      requestIsStopped: () => {},
+    };
+
+    const errorHandler = () => {};
+
+    return new SearchRequest({ source, errorHandler, ...overrides });
+  };
+
   beforeEach(ngMock.module('kibana', PrivateProvider => {
-    function FakeIsRequestProvider() {
-      return function isRequest() {
-        return true;
-      };
-    }
-
-    PrivateProvider.swap(IsRequestProvider, FakeIsRequestProvider);
-
+    // We mock this so that we don't need to stub out methods for searchRequest.source, e.g. getId(),
+    // which is used by mergeDuplicateRequests.
     function FakeMergeDuplicatesRequestProvider() {
       return function mergeDuplicateRequests(searchRequests) {
         return searchRequests;
@@ -83,29 +77,18 @@ describe('callClient', () => {
 
   beforeEach(ngMock.inject(Private => {
     callClient = Private(CallClientProvider);
+    SearchRequest = Private(SearchRequestProvider);
   }));
 
   describe('basic contract', () => {
     it('returns a promise', () => {
-      searchRequests = [ mockRequest() ];
+      searchRequests = [ createSearchRequest() ];
       const callingClient = callClient(searchRequests);
       expect(callingClient.then).to.be.a('function');
     });
 
-    it('calls es.msearch() once, regardless of a large number of searchRequests', done => {
-      expect(fakeSearch.callCount).to.be(0);
-
-      searchRequests = [ mockRequest(), mockRequest(), mockRequest(), mockRequest() ];
-      const callingClient = callClient(searchRequests);
-
-      callingClient.then(() => {
-        expect(fakeSearch.callCount).to.be(1);
-        done();
-      });
-    });
-
     it(`resolves the promise with the 'responses' property of the es.msearch() result`, done => {
-      searchRequests = [ mockRequest() ];
+      searchRequests = [ createSearchRequest() ];
       const callingClient = callClient(searchRequests);
 
       callingClient.then(results => {
@@ -113,8 +96,94 @@ describe('callClient', () => {
         // the searchRequests we provided to callClient.
         expect(results).to.eql(searchRequests);
         done();
+      }).catch(error => done(error));
+    });
+  });
+
+  describe('implementation', () => {
+    it('calls es.msearch() once, regardless of number of searchRequests', done => {
+      expect(fakeSearch.callCount).to.be(0);
+
+      searchRequests = [ createSearchRequest(), createSearchRequest(), createSearchRequest() ];
+      const callingClient = callClient(searchRequests);
+
+      callingClient.then(() => {
+        expect(fakeSearch.callCount).to.be(1);
+        done();
+      }).catch(error => done(error));
+    });
+  });
+
+  describe('aborting', () => {
+    it(`when searchSource's _flatten method throws an error resolves with an ABORTED response`, done => {
+      const searchRequest = createSearchRequest({
+        source: {
+          _flatten: () => { throw new Error(); },
+        },
       });
+
+      searchRequests = [ searchRequest ];
+      const callingClient = callClient(searchRequests);
+
+      callingClient.then(results => {
+        // The result is ABORTED because it was never included in the body sent to es.msearch().
+        expect(results).to.eql([ ABORTED ]);
+        done();
+      }).catch(error => done(error));
     });
 
+    it('while search params are being fetched resolves with an undefined response', done => {
+      const searchRequest = createSearchRequest({
+        source: {
+          _flatten: () => {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve({});
+              }, 100);
+            });
+          },
+          requestIsStopped: () => {},
+        },
+      });
+
+      searchRequests = [ searchRequest ];
+      const callingClient = callClient(searchRequests);
+
+      setTimeout(() => {
+        searchRequest.abort();
+      }, 20);
+
+      callingClient.then(results => {
+        expect(results).to.eql([ undefined ]);
+        done();
+      }).catch(error => done(error));
+    });
+
+    it(`all searchRequests resolves with undefined responses`, done => {
+      const searchRequest1 = createSearchRequest();
+      const searchRequest2 = createSearchRequest();
+      searchRequests = [ searchRequest1, searchRequest2 ];
+      const callingClient = callClient(searchRequests);
+      searchRequest1.abort();
+      searchRequest2.abort();
+
+      callingClient.then(results => {
+        expect(results).to.eql([undefined, undefined]);
+        done();
+      }).catch(error => done(error));
+    });
+
+    it('one searchRequest but not all of them resolves with full responses', done => {
+      const searchRequest1 = createSearchRequest();
+      const searchRequest2 = createSearchRequest();
+      searchRequests = [ searchRequest1, searchRequest2 ];
+      const callingClient = callClient(searchRequests);
+      searchRequest2.abort();
+
+      callingClient.then(results => {
+        expect(results).to.eql([ searchRequest1, searchRequest2 ]);
+        done();
+      }).catch(error => done(error));
+    });
   });
 });
