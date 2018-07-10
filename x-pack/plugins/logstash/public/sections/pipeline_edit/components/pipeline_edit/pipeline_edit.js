4044f3cff20ff8fd85d5093ee7f6877cd0aa6c3c
Justin Kambic <justin.kambic@elastic.co>
2018-07-10 15:39:22 -0400

    Remove obsolete code.

--- x-pack/plugins/logstash/public/sections/pipeline_edit/components/pipeline_edit/pipeline_edit.js
+++ x-pack/plugins/logstash/public/sections/pipeline_edit/components/pipeline_edit/pipeline_edit.js
@@ -467,17 +467,12 @@ export class PipelineEditor extends React.Component {
 
 import { isEmpty } from 'lodash';
 import { uiModules } from 'ui/modules';
-// import { InitAfterBindingsWorkaround } from 'ui/compat';
-// import { Notifier, toastNotifications } from 'ui/notify';
 import { Notifier, toastNotifications } from 'ui/notify';
-// import template from './pipeline_edit.html';
 import 'plugins/logstash/services/license';
 import 'plugins/logstash/services/security';
 import './pipeline_edit.less';
 import '../../../../components/tooltip';
-// import { EDITOR } from '../../../../../common/constants';
 import 'ace';
-// import { width } from 'window-size';
 
 const app = uiModules.get('xpack/logstash');
 
@@ -487,7 +482,6 @@ app.directive('pipelineEdit', function ($injector) {
   const securityService = $injector.get('logstashSecurityService');
   const kbnUrl = $injector.get('kbnUrl');
   const shieldUser = $injector.get('ShieldUser');
-  // const confirmModal = $injector.get('confirmModal');
 
   return {
     restrict: 'E',
@@ -519,106 +513,8 @@ app.directive('pipelineEdit', function ($injector) {
           notifier={new Notifier({ location: 'Logstash' })}
         />, el[0]);
     },
-    // template: template,
     scope: {
       pipeline: '=',
     },
-    // bindToController: true,
-    // controllerAs: 'pipelineEdit',
-    // controller: class PipelineEditController extends InitAfterBindingsWorkaround {
-    //   initAfterBindings($scope) {
-    //     this.originalPipeline = { ...this.pipeline };
-    //     this.notifier = new Notifier({ location: 'Logstash' });
-    //     this.isNewPipeline = isEmpty(this.pipeline.id);
-    //     // only if security is enabled and available, we tack on the username.
-    //     if (securityService.isSecurityEnabled) {
-    //       $scope.user = $injector.get('ShieldUser').getCurrent();
-    //     } else {
-    //       $scope.user = null;
-    //     }
-    //     $scope.aceLoaded = editor => {
-    //       this.editor = editor;
-    //       /*
-    //        * This sets the space between the editor's borders and the
-    //        * edges of the top/bottom lines to make for a less-crowded
-    //        * typing experience.
-    //        */
-    //       editor.renderer.setScrollMargin(
-    //         EDITOR.PIPELINE_EDITOR_SCROLL_MARGIN_TOP_PX,
-    //         EDITOR.PIPELINE_EDITOR_SCROLL_MARGIN_BOTTOM_PX,
-    //         0,
-    //         0
-    //       );
-    //       editor.setReadOnly(this.isReadOnly);
-    //       editor.setOptions({
-    //         minLines: 25,
-    //         maxLines: Infinity,
-    //       });
-    //       editor.$blockScrolling = Infinity;
-    //     };
-    //     if (this.isReadOnly) {
-    //       toastNotifications.addWarning(licenseService.message);
-    //     }
-
-    //     this.tooltips = TOOLTIPS;
-    //   }
-
-    //   onPipelineSave = username => {
-    //     this.pipeline.username = username;
-    //     return pipelineService
-    //       .savePipeline(this.pipeline)
-    //       .then(() => {
-    //         toastNotifications.addSuccess(`Saved '${this.pipeline.id}'`);
-    //         this.close();
-    //       })
-    //       .catch(err => {
-    //         return licenseService
-    //           .checkValidity()
-    //           .then(() => this.notifier.error(err));
-    //       });
-    //   };
-
-    //   onPipelineDelete = pipelineId => {
-    //     const confirmModalOptions = {
-    //       onConfirm: this.deletePipeline,
-    //       confirmButtonText: `Delete pipeline ${pipelineId}`,
-    //     };
-
-    //     return confirmModal(
-    //       'You cannot recover a deleted pipeline.',
-    //       confirmModalOptions
-    //     );
-    //   };
-
-    //   onClose = () => {
-    //     this.close();
-    //   };
-
-    //   deletePipeline = () => {
-    //     return pipelineService
-    //       .deletePipeline(this.pipeline.id)
-    //       .then(() => {
-    //         toastNotifications.addSuccess(`Deleted '${this.pipeline.id}'`);
-    //         this.close();
-    //       })
-    //       .catch(err => {
-    //         return licenseService
-    //           .checkValidity()
-    //           .then(() => this.notifier.error(err));
-    //       });
-    //   };
-
-    //   close = () => {
-    //     kbnUrl.change('/management/logstash/pipelines', {});
-    //   };
-
-    //   get isSaveEnabled() {
-    //     return !(this.form.$invalid || this.jsonForm.$invalid);
-    //   }
-
-    //   get isReadOnly() {
-    //     return licenseService.isReadOnly;
-    //   }
-    // },
   };
 });
