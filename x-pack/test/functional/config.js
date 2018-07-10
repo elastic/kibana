37afc042cd391649dfcfb137f5d664669e222f3a
archana <archanid@users.noreply.github.com>
2018-07-10 05:25:55 -0500

    [Tests] Run es from snapshot locally in xpack (#20585)

--- x-pack/test/functional/config.js
+++ x-pack/test/functional/config.js
@@ -150,7 +150,7 @@ export default async function ({ readConfigFile }) {
 
     esTestCluster: {
       license: 'trial',
-      from: 'source',
+      from: 'snapshot',
       serverArgs: [
         'xpack.license.self_generated.type=trial',
         'xpack.security.enabled=true',
