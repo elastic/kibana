f88d0b92a2aa3d36d069092c10782660799375ca
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-07-11 13:12:33 +0300

    Integrate new platform (core) server side into Kibana (#20661)
    Co-authored-by: Kim Joar Bekkelund <kjbekkelund@gmail.com>
    Co-authored-by: archana <archanid@users.noreply.github.com>
    Co-authored-by: Spencer <spalger@users.noreply.github.com>
    Co-authored-by: Court Ewing <court@epixa.com>

---
+++ src/core/lib/kbn_internal_native_observable/index.d.ts
@@ -0,0 +1,139 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/* tslint:disable */
+// This adds a symbol type for `Symbol.observable`, which doesn't exist globally
+// in TypeScript yet.
+declare global {
+  export interface SymbolConstructor {
+    readonly observable: symbol;
+  }
+}
+
+// These types are based on the Observable proposal readme, see
+// https://github.com/tc39/proposal-observable#api, with the addition of using
+// generics to define the type of the `value`.
+
+declare namespace Observable {
+  interface Subscription {
+    // Cancels the subscription
+    unsubscribe(): void;
+
+    // A boolean value indicating whether the subscription is closed
+    closed: boolean;
+  }
+
+  interface Subscribable<T> {
+    subscribe(
+      observerOrNext?: SubscriptionObserver<T> | ((value: T) => void),
+      error?: (error: any) => void,
+      complete?: () => void
+    ): Subscription;
+  }
+
+  type ObservableInput<T> = Subscribable<T> | Iterable<T>;
+
+  interface SubscriptionObserver<T> {
+    // Sends the next value in the sequence
+    next(value: T): void;
+
+    // Sends the sequence error
+    error(errorValue: Error): void;
+
+    // Sends the completion notification
+    complete(): void;
+
+    // A boolean value indicating whether the subscription is closed
+    closed: boolean;
+  }
+
+  export interface StartObserver<T> {
+    start(subscription: Subscription): void;
+    next?(value: T): void;
+    error?(err: any): void;
+    complete?(): void;
+  }
+
+  export interface NextObserver<T> {
+    start?(subscription: Subscription): void;
+    next(value: T): void;
+    error?(err: any): void;
+    complete?(): void;
+  }
+
+  interface ErrorObserver<T> {
+    start?(subscription: Subscription): void;
+    next?(value: T): void;
+    error(err: any): void;
+    complete?(): void;
+  }
+
+  interface CompletionObserver<T> {
+    start?(subscription: Subscription): void;
+    next?(value: T): void;
+    error?(err: any): void;
+    complete(): void;
+  }
+
+  type PartialObserver<T> =
+    | StartObserver<T>
+    | NextObserver<T>
+    | ErrorObserver<T>
+    | CompletionObserver<T>;
+
+  interface Observer<T> {
+    // Receives the subscription object when `subscribe` is called
+    start(subscription: Subscription): void;
+
+    // Receives the next value in the sequence
+    next(value: T): void;
+
+    // Receives the sequence error
+    error(errorValue: Error): void;
+
+    // Receives a completion notification
+    complete(): void;
+  }
+
+  type SubscriberFunction<T> = (
+    observer: SubscriptionObserver<T>
+  ) => void | null | undefined | (() => void) | Subscription;
+
+  class Observable<T> {
+    constructor(subscriber: SubscriberFunction<T>);
+
+    // Subscribes to the sequence with an observer
+    subscribe(): Subscription;
+    subscribe(observer: PartialObserver<T>): Subscription;
+
+    // Subscribes to the sequence with callbacks
+    subscribe(
+      onNext: (val: T) => void,
+      onError?: (err: Error) => void,
+      onComplete?: () => void
+    ): Subscription;
+
+    // Returns itself
+    [Symbol.observable](): Observable<T>;
+
+    static of<T>(...items: T[]): Observable<T>;
+    static from<T>(x: ObservableInput<T>): Observable<T>;
+  }
+}
+
+export = Observable;
