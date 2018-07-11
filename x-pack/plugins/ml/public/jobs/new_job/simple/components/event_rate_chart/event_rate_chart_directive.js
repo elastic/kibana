elastic/kibana

commit 01c097f6dc94857c9dbb383853b867163bc839cc

[ML] Fix cosmetic issues with cut off chart overflows. (#19794)
1. Increases the top margin to 5 and the right margin to 1 to avoid cutting off the chart because overflow is hidden for the directives mlEventRateChart and mlMultiMetricJobChart. The top margin gives enough room to avoid cutting off y-axis labels. The right margin is a tweak to not cut off the gray border by 1 pixel to the right.
2. Fixes how the domain for mlEventRateChart is calculated. The domain gets now extended by 1 barsInterval, otherwise the last bar will start at the end of vizWidth and overflow the chart (the overflow is hidden so the visible chart missed one bar).

x-pack/plugins/ml/public/jobs/new_job/simple/components/event_rate_chart/event_rate_chart_directive.js
@@ -29,7 +29,7 @@ module.directive('mlEventRateChart', function () {
 
     let svgWidth = 0;
     const barChartHeight = scope.eventRateChartHeight;
-    const margin = { top: 0, right: 0, bottom: 20, left: scope.chartTicksMargin.width };
+    const margin = { top: 5, right: 1, bottom: 20, left: scope.chartTicksMargin.width };
     const svgHeight = barChartHeight + margin.top + margin.bottom;
     let vizWidth  = svgWidth  - margin.left - margin.right;
     const chartLimits = { max: 0, min: 0 };
@@ -120,9 +120,15 @@ module.directive('mlEventRateChart', function () {
       }
       swimlaneXScale.domain(d3.extent(finerData, d => d.date));
 
+
+      // Extend the time range/domain at the end by 1 barsInterval,
+      // otherwise the last bar will start at the end of vizWidth
+      // and overflow the chart.
+      const timeExtent = d3.extent(data, d => d.date);
+      timeExtent[1] = new Date(timeExtent[1].getTime() + scope.chartData.barsInterval);
       barChartXScale = d3.time.scale()
         .range([0, vizWidth])
-        .domain(d3.extent(data, d => d.date));
+        .domain(timeExtent);
 
       chartLimits.max = d3.max(data, (d) => d.value);
       chartLimits.min = 0;

x-pack/plugins/ml/public/jobs/new_job/simple/multi_metric/create_job/create_job_chart_directive.js
@@ -27,7 +27,7 @@ module.directive('mlMultiMetricJobChart', function () {
 
     let svgWidth = 0;
     const lineChartHeight = scope.chartHeight;
-    const margin = { top: 0, right: 0, bottom: 20, left: scope.chartTicksMargin.width };
+    const margin = { top: 5, right: 1, bottom: 20, left: scope.chartTicksMargin.width };
     const svgHeight = lineChartHeight + margin.top + margin.bottom;
     let vizWidth  = svgWidth  - margin.left - margin.right;
     const chartLimits = { max: 0, min: 0 };

commit 0dbb8195639ea40bbbfe492f88cdace13b89ced8

[ML] Fixes a regression bug which made the progress bar invisible. (#20618)
Fixes a regression introduced by #19794. This PR added a 5px top margin to the affected charts. The hard coded bottom margin of the progress bars wasn't adjusted accordingly so the progress bars ended up being hidden behind the actual chart. This PR fixes the problem by taking the chart's top margin into account for progressBarMarginBottom.

x-pack/plugins/ml/public/jobs/new_job/simple/components/event_rate_chart/event_rate_chart_directive.js
@@ -28,7 +28,9 @@ module.directive('mlEventRateChart', function () {
 
     let svgWidth = 0;
     const barChartHeight = scope.eventRateChartHeight;
-    const margin = { top: 5, right: 1, bottom: 20, left: scope.chartTicksMargin.width };
+    const marginTop = 5;
+    const progressBarMarginBottom = barChartHeight + marginTop - 15;
+    const margin = { top: marginTop, right: 1, bottom: 20, left: scope.chartTicksMargin.width };
     const svgHeight = barChartHeight + margin.top + margin.bottom;
     let vizWidth  = svgWidth  - margin.left - margin.right;
     const chartLimits = { max: 0, min: 0 };
@@ -83,7 +85,7 @@ module.directive('mlEventRateChart', function () {
 
       if (chartElement.select('.progress-bar')[0][0] === null) {
         const style = `width: ${(+vizWidth + 2)}px;
-          margin-bottom: -${(+barChartHeight - 15)}px;
+          margin-bottom: -${(+progressBarMarginBottom)}px;
           margin-left: ${(+margin.left - 1)}px;'`;
 
         chartElement.append('div')

x-pack/plugins/ml/public/jobs/new_job/simple/multi_metric/create_job/create_job_chart_directive.js
@@ -27,7 +27,9 @@ module.directive('mlMultiMetricJobChart', function () {
 
     let svgWidth = 0;
     const lineChartHeight = scope.chartHeight;
-    const margin = { top: 5, right: 1, bottom: 20, left: scope.chartTicksMargin.width };
+    const marginTop = 5;
+    const progressBarMarginBottom = lineChartHeight + marginTop - 15;
+    const margin = { top: marginTop, right: 1, bottom: 20, left: scope.chartTicksMargin.width };
     const svgHeight = lineChartHeight + margin.top + margin.bottom;
     let vizWidth  = svgWidth  - margin.left - margin.right;
     const chartLimits = { max: 0, min: 0 };
@@ -88,7 +90,7 @@ module.directive('mlMultiMetricJobChart', function () {
 
       if (chartElement.select('.progress-bar')[0][0] === null) {
         const style = `width: ${(+vizWidth + 2)}px;
-          margin-bottom: -${(+lineChartHeight - 15)}px;
+          margin-bottom: -${(+progressBarMarginBottom)}px;
           margin-left: ${(+margin.left - 1)}px;'`;
 
         chartElement.append('div')