c7744107491ec0d70ed14f261bb3a8b72b7b5379
Stacey Gammon <gammon@elastic.co>
2018-07-12 08:52:33 -0400

    Revert "Focus only on flaky test suite and run 10x"
    This reverts commit a6616b1140c29d8d3f13666d20ae94db9a97267f.

--- tasks/jenkins.js
+++ tasks/jenkins.js
@@ -18,25 +18,27 @@
  */
 
 module.exports = function (grunt) {
-  grunt.registerTask('jenkins:docs', ['docker:docs']);
+  grunt.registerTask('jenkins:docs', [
+    'docker:docs'
+  ]);
 
   grunt.registerTask('jenkins:unit', [
-    // 'run:eslint',
-    // 'run:tslint',
-    // 'run:checkFileCasing',
-    // 'licenses',
-    // 'verifyDependencyVersions',
-    // 'run:verifyNotice',
-    // 'test:server',
-    // 'test:jest',
-    // 'test:jest_integration',
-    // 'test:projects',
-    // 'test:browser-ci',
-    // 'run:apiIntegrationTests',
-    // 'verifyTranslations',
+    'run:eslint',
+    'run:tslint',
+    'run:checkFileCasing',
+    'licenses',
+    'verifyDependencyVersions',
+    'run:verifyNotice',
+    'test:server',
+    'test:jest',
+    'test:jest_integration',
+    'test:projects',
+    'test:browser-ci',
+    'run:apiIntegrationTests',
+    'verifyTranslations',
   ]);
 
   grunt.registerTask('jenkins:selenium', [
-    //'test:uiRelease'
+    'test:uiRelease'
   ]);
 };
