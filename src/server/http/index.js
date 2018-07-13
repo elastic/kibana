99f12e800571473062eecb03f745d9d5a3dfa59a
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-07-13 14:36:44 +0200

    Clarify that `tls` property is an exception.

--- src/server/http/index.js
+++ src/server/http/index.js
@@ -37,6 +37,12 @@ export default async function (kbnServer, server, config) {
 
   // Note that all connection options configured here should be exactly the same
   // as in `getServerOptions()` in the new platform (see `src/core/server/http/http_tools`).
+  //
+  // The only exception is `tls` property: TLS is entirely handled by the new
+  // platform and we don't have to duplicate all TLS related settings here, we just need
+  // to indicate to Hapi connection that TLS is used so that it can use correct protocol
+  // name in `server.info` and `request.connection.info` that are used throughout Kibana.
+  //
   // Any change SHOULD BE applied in both places.
   server.connection({
     host: config.get('server.host'),
