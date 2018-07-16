9b07abb5219bc66b02d006c65722a192d3b38645
Robert Monfera <monfera.robert@gmail.com>
2018-07-16 21:43:36 +0200

    Chore: bypassig a dangerouslySetInnerHTML call lint

--- public/components/shape_preview/shape_preview.js
+++ public/components/shape_preview/shape_preview.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { shapes } from '../../render_functions/shape/shapes';
 
 export const ShapePreview = ({ value }) => {
+  // eslint-disable-next-line react/no-danger
   return <div className="canvasShapePreview" dangerouslySetInnerHTML={{ __html: shapes[value] }} />;
 };
 
