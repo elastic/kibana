17eebb9ba3fa4ae66eebfba2ba98f0eed244b1df
Hanqing Zhao <aaron.elite1993@gmail.com>
2018-07-17 14:01:52 -0500

    [kbn-plugin-generator] Fix plugin naming inconsistency (#20808)
    <!--
    Thank you for your interest in and contributing to Kibana! There
    are a few simple things to check before submitting your pull request
    that can help with the review process. You should delete these items
    from your submission, but they are here to help bring them to your
    attention.
    
    - Have you signed the [contributor license agreement](https://www.elastic.co/contributor-agreement)?
    - Have you followed the [contributor guidelines](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md)?
    - If submitting code, have you included unit tests that cover the changes?
    - If submitting code, have you tested and built your code locally prior to submission with `yarn test && yarn build`?
    - If submitting code, is your pull request against master? Unless there is a good reason otherwise, we prefer pull requests against master and will backport as needed.
    -->
    Make relevant generated files use snakeCase instead of kebabCase
    Closes #20524

--- packages/kbn-plugin-generator/sao_template/sao.js
+++ packages/kbn-plugin-generator/sao_template/sao.js
@@ -19,7 +19,6 @@
 
 const { resolve, relative, dirname } = require('path');
 
-const kebabCase = require('lodash.kebabcase');
 const startCase = require('lodash.startcase');
 const camelCase = require('lodash.camelcase');
 const snakeCase = require('lodash.snakecase');
@@ -86,9 +85,9 @@ module.exports = function({ name }) {
       Object.assign(
         {
           templateVersion: pkg.version,
-          kebabCase,
           startCase,
           camelCase,
+          snakeCase,
           name,
         },
         answers
