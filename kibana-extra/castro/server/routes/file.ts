1cac19f02de90f1b25615decd91f07c5880c48c5
Yulong <allendragon@gmail.com>
2018-07-18 11:16:00 +0800

    response differently by identify file types (#90)

--- kibana-extra/castro/server/routes/file.ts
+++ kibana-extra/castro/server/routes/file.ts
@@ -5,6 +5,7 @@
  */
 
 import Boom from 'boom';
+import fileType from 'file-type';
 import * as Hapi from 'hapi';
 import GitOperations from '../GitOperations';
 import ServerOptions from '../ServerOptions';
@@ -29,12 +30,44 @@ export default function(server: Hapi.Server, options: ServerOptions) {
       }
     },
   });
+
   server.route({
     path: '/api/castro/repo/{site}/{org}/{repo}/blob/{rev}/{path*}',
     method: 'GET',
     async handler(req: Hapi.Request, reply: Hapi.IReply) {
       const fileResolver = new GitOperations(options.repoPath);
+      const { site, org, repo, path, rev } = req.params;
+      const uri = `${site}/${org}/${repo}`;
+      try {
+        const blob = await fileResolver.fileContent(uri, path, rev);
+        if (blob.isBinary()) {
+          const type = fileType(blob.content());
+          if (type && type.mime && type.mime.startsWith('image/')) {
+            reply(blob.content()).type(type.mime);
+          } else {
+            // this api will return a empty response with http code 204
+            reply('')
+              .type('application/octet-stream')
+              .code(204);
+          }
+        } else {
+          reply(blob.content()).type('text/plain');
+        }
+      } catch (e) {
+        if (e.isBoom) {
+          reply(e);
+        } else {
+          reply(Boom.internal(e.message || e.name));
+        }
+      }
+    },
+  });
 
+  server.route({
+    path: '/api/castro/repo/{site}/{org}/{repo}/raw/{rev}/{path*}',
+    method: 'GET',
+    async handler(req: Hapi.Request, reply: Hapi.IReply) {
+      const fileResolver = new GitOperations(options.repoPath);
       const { site, org, repo, path, rev } = req.params;
       const uri = `${site}/${org}/${repo}`;
       try {
