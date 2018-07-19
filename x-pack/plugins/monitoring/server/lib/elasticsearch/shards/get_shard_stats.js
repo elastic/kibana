ec34ff30f1afc02610d3b5f8834541147e57ec9f
Chris Roberson <chrisronline@gmail.com>
2018-07-19 14:03:16 -0400

    Use 0 as the default for shard count if the node is not found

--- x-pack/plugins/monitoring/server/lib/elasticsearch/shards/get_shard_stats.js
+++ x-pack/plugins/monitoring/server/lib/elasticsearch/shards/get_shard_stats.js
@@ -58,6 +58,8 @@ export function getShardStats(req, esIndexPattern, cluster, { includeNodes = fal
     }
   };
 
+  console.log('CHRIS', JSON.stringify(params));
+
   const { callWithRequest } = req.server.plugins.elasticsearch.getCluster('monitoring');
   return callWithRequest(req, 'search', params)
     .then(resp => {
