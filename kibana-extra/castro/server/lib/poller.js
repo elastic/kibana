871569ff7ebe053b11543dbee843dc5f066814e4
spalger <email@spalger.com>
2018-07-20 23:50:47 -0700

    add server/lib/poller module

---
+++ kibana-extra/castro/server/lib/poller.js
@@ -0,0 +1,85 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+/*
+ * Borrowed from https://github.com/elastic/kibana/blob/master/x-pack/common/poller.js
+ */
+
+const noop = () => {
+  // noop
+};
+
+export class Poller {
+  constructor(options) {
+    this.functionToPoll = options.functionToPoll; // Must return a Promise
+    this.successFunction = options.successFunction || noop;
+    this.errorFunction = options.errorFunction || noop;
+    this.pollFrequencyInMillis = options.pollFrequencyInMillis;
+    this.trailing = options.trailing || false;
+    this.continuePollingOnError = options.continuePollingOnError || false;
+    this.pollFrequencyErrorMultiplier = options.pollFrequencyErrorMultiplier || 1;
+    this._timeoutId = null;
+    this._isRunning = false;
+  }
+
+  public getPollFrequency() {
+    return this.pollFrequencyInMillis;
+  }
+
+  public _poll() {
+    return this.functionToPoll()
+      .then(this.successFunction)
+      .then(() => {
+        if (!this._isRunning) {
+          return;
+        }
+
+        this._timeoutId = setTimeout(this._poll.bind(this), this.pollFrequencyInMillis);
+      })
+      .catch(e => {
+        this.errorFunction(e);
+        if (!this._isRunning) {
+          return;
+        }
+
+        if (this.continuePollingOnError) {
+          this._timeoutId = setTimeout(
+            this._poll.bind(this),
+            this.pollFrequencyInMillis * this.pollFrequencyErrorMultiplier
+          );
+        } else {
+          this.stop();
+        }
+      });
+  }
+
+  public start() {
+    if (this._isRunning) {
+      return;
+    }
+
+    this._isRunning = true;
+    if (this.trailing) {
+      this._timeoutId = setTimeout(this._poll.bind(this), this.pollFrequencyInMillis);
+    } else {
+      this._poll();
+    }
+  }
+
+  public stop() {
+    if (!this._isRunning) {
+      return;
+    }
+
+    this._isRunning = false;
+    clearTimeout(this._timeoutId);
+    this._timeoutId = null;
+  }
+
+  public isRunning() {
+    return this._isRunning;
+  }
+}
