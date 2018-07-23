d4a2dd616f67cd86cb89282a2dcf5884ea5cdd8c
Catherine Liu <catherineqliu@outlook.com>
2018-07-23 13:10:10 -0700

    Improves Shape Resizing and Adds Shapes (#827)
    * Changed border offset calculation in shape render function
    
    * Removed unnecessary white space from existing shapes
    
    * Added cross, kite, semicircle, and right triangle shapes

--- public/render_functions/shape/shape.js
+++ public/render_functions/shape/shape.js
@@ -8,7 +8,6 @@ export const shape = () => ({
   reuseDomNode: true,
   render(domNode, config, handlers) {
     const { shape, fill, border, borderWidth, maintainAspect } = config;
-
     const parser = new DOMParser();
     const [shapeSvg] = parser
       .parseFromString(shapes[shape], 'image/svg+xml')
@@ -22,14 +21,34 @@ export const shape = () => ({
     shapeContent.setAttribute('stroke-miterlimit', 999);
     shapeContent.setAttribute('vector-effect', 'non-scaling-stroke');
 
-    shapeSvg.style.padding = borderWidth / 2;
     shapeSvg.setAttribute('preserveAspectRatio', maintainAspect ? 'xMidYmid meet' : 'none');
+
+    const initialViewBox = shapeSvg
+      .getAttribute('viewBox')
+      .split(' ')
+      .map(v => parseInt(v, 10));
 
     const draw = () => {
       const width = domNode.offsetWidth;
       const height = domNode.offsetHeight;
+
+      // adjust viewBox based on border width
+      let [minX, minY, maxX, maxY] = initialViewBox;
+
+      const xScale = (maxX - minX) / width;
+      const yScale = (maxY - minY) / height;
+      const borderOffset = borderWidth / 2;
+      const xOffset = borderOffset * xScale;
+      const yOffset = borderOffset * yScale;
+
+      minX -= xOffset; // min-x
+      minY -= yOffset; // min-y
+      maxX += xOffset * 2; // width
+      maxY += yOffset * 2; // height
+
       shapeSvg.setAttribute('width', width);
       shapeSvg.setAttribute('height', height);
+      shapeSvg.setAttribute('viewBox', [minX, minY, maxX, maxY].join(' '));
 
       const oldShape = domNode.firstElementChild;
       if (oldShape) domNode.removeChild(oldShape);
