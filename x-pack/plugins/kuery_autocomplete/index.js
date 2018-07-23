dfde48bb990ab481da0b6d2eeea532a3bcc78cb0
Lukas Olson <olson.lukas@gmail.com>
2018-07-23 16:48:16 -0700

    fix: remove unnecessary stuff from the plugin spec

--- x-pack/plugins/kuery_autocomplete/index.js
+++ x-pack/plugins/kuery_autocomplete/index.js
@@ -10,14 +10,9 @@ import { resolve } from 'path';
 export const kueryAutocomplete = (kibana) => new kibana.Plugin({
   id: 'kuery_autocomplete',
   publicDir: resolve(__dirname, 'public'),
-  require: ['kibana', 'xpack_main'],
-  config: (Joi) => Joi.object({
-    enabled: Joi.boolean().default(true)
-  }).default(),
   uiExports: {
     autocompleteProviders: [
       'plugins/kuery_autocomplete/autocomplete_providers'
     ],
-  },
-  init: () => {}
+  }
 });
