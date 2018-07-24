3bbb9c5b203527e9b45cfacdb19a9036010d10d4
Lukas Olson <olson.lukas@gmail.com>
2018-07-24 12:33:15 -0700

    Extract KQL autocomplete to a plugin (#20747)
    * fix: move autocomplete to x-pack basic
    
    * fix: apm support
    
    * fix: renames
    
    * [uiExports] switch to new autocompleteProviders export type
    
    * fix: remove unnecessary stuff from the plugin spec

--- src/ui/public/autocomplete_providers/index.js
+++ src/ui/public/autocomplete_providers/index.js
@@ -17,22 +17,12 @@
  * under the License.
  */
 
-export function escapeQuotes(string) {
-  return string.replace(/"/g, '\\"');
+const autocompleteProviders = new Map();
+
+export function addAutocompleteProvider(language, provider) {
+  autocompleteProviders.set(language, provider);
 }
 
-export const escapeKuery = (string) => escapeNot(escapeAndOr(escapeSpecialCharacters(string)));
-
-// See the SpecialCharacter rule in kuery.peg
-function escapeSpecialCharacters(string) {
-  return string.replace(/[\\():<>"*]/g, '\\$&'); // $& means the whole matched string
-}
-
-// See the Keyword rule in kuery.peg
-function escapeAndOr(string) {
-  return string.replace(/(\s+)(and|or)(\s+)/ig, '$1\\$2$3');
-}
-
-function escapeNot(string) {
-  return string.replace(/not(\s+)/ig, '\\$&');
+export function getAutocompleteProvider(language) {
+  return autocompleteProviders.get(language);
 }
