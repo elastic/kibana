33bad59e1c1c64c196576f39df70983cd6d58a3a
James Gowdy <jgowdy@elastic.co>
2018-07-24 11:54:22 +0100

    [ML] Create watch from new jobs list (#21112)
    * [ML] [WIP] Create watch from new jobs list
    
    * removing comments
    
    * adding interval calculation
    
    * adding checkbox to start datafeed modal
    
    * adding proptypes check to SelectSeverity
    
    * fixing typo
    
    * changes based on review
    
    * correcting input labels

--- x-pack/plugins/ml/public/jobs/jobs_list_new/components/edit_job_flyout/edit_job_flyout.js
+++ x-pack/plugins/ml/public/jobs/jobs_list_new/components/edit_job_flyout/edit_job_flyout.js
@@ -36,7 +36,7 @@ export class EditJobFlyout extends Component {
     this.state = {
       job: {},
       hasDatafeed: false,
-      isModalVisible: false,
+      isFlyoutVisible: false,
       jobDescription: '',
       jobGroups: [],
       jobModelMemoryLimit: '',
@@ -68,7 +68,7 @@ export class EditJobFlyout extends Component {
   }
 
   closeFlyout = () => {
-    this.setState({ isModalVisible: false });
+    this.setState({ isFlyoutVisible: false });
   }
 
   showFlyout = (jobLite) => {
@@ -78,7 +78,7 @@ export class EditJobFlyout extends Component {
         this.extractJob(job, hasDatafeed);
         this.setState({
           job,
-          isModalVisible: true,
+          isFlyoutVisible: true,
         });
       })
       .catch((error) => {
@@ -185,7 +185,7 @@ export class EditJobFlyout extends Component {
   render() {
     let flyout;
 
-    if (this.state.isModalVisible) {
+    if (this.state.isFlyoutVisible) {
       const {
         job,
         jobDescription,
