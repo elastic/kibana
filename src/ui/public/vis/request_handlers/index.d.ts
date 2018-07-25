91f50afead6547fcc266995bef417843abaf5235
Tim Roes <mail@timroes.de>
2018-07-25 09:22:26 +0200

    TypeScriptify visualize loader (#21025) (#21190)
    * TypeScriptify Vis loaders
    
    * Fix issue with undefined timeRange
    
    * Fix chrome typing
    
    * Fix unit tests
    
    * Fix this issue
    
    * Add missing uiState to request handler
    
    * Implement Felix's suggestions
    
    * Add timefilter listener

--- src/ui/public/vis/request_handlers/index.d.ts
+++ src/ui/public/vis/request_handlers/index.d.ts
@@ -17,17 +17,8 @@
  * under the License.
  */
 
-interface IInjector {
-  get<T>(injectable: string): T;
-}
-
-declare class Chrome {
-  public addBasePath<T = string>(path: T): T;
-  public dangerouslyGetActiveInjector(): Promise<IInjector>;
-  public getBasePath(): string;
-  public getXsrfToken(): string;
-}
-
-declare const chrome: Chrome;
-
-export default chrome;
+export {
+  RequestHandler,
+  RequestHandlerDescription,
+  RequestHandlerParams,
+} from './request_handlers';
