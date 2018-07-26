639134bb81444367eb1264522df06e5cbc11e521
Stacey Gammon <gammon@elastic.co>
2018-07-26 15:17:34 -0400

    Make visualization.waitForRender more generic and put in an embeddable class.

--- test/functional/services/dashboard/index.js
+++ test/functional/services/dashboard/index.js
@@ -21,3 +21,5 @@ export { DashboardVisualizationProvider } from './visualizations';
 export { DashboardExpectProvider } from './expectations';
 export { DashboardAddPanelProvider } from './add_panel';
 export { DashboardPanelActionsProvider } from './panel_actions';
+export { EmbeddableProvider } from './embeddable';
+
