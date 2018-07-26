d18218fa33df97db1f1c5a2f954a60672c228d49
Matt Apperson <me@mattapperson.com>
2018-07-26 11:40:02 -0400

    Beats/initial ui (#20994)
    * initial layout and main nav
    
    * modal UI and pattern for UI established
    
    * fix path
    
    * wire up in-memroy adapters
    
    * tweak adapters

---
+++ x-pack/plugins/beats_management/public/lib/adapters/beats/adapter_types.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { CMBeat } from '../../../../common/domain_types';
+
+export interface CMBeatsAdapter {
+  get(id: string): Promise<CMBeat | null>;
+  getAll(): Promise<CMBeat[]>;
+  getWithIds(beatIds: string[]): Promise<CMBeat[]>;
+  removeTagsFromBeats(removals: BeatsTagAssignment[]): Promise<BeatsTagAssignment[]>;
+  assignTagsToBeats(assignments: BeatsTagAssignment[]): Promise<BeatsTagAssignment[]>;
+}
+
+export interface BeatsTagAssignment {
+  beatId: string;
+  tag: string;
+  idxInRequest?: number;
+}
+
+interface BeatsReturnedTagAssignment {
+  status: number | null;
+  result?: string;
+}
+
+export interface CMAssignmentReturn {
+  assignments: BeatsReturnedTagAssignment[];
+}
+
+export interface BeatsRemovalReturn {
+  removals: BeatsReturnedTagAssignment[];
+}
