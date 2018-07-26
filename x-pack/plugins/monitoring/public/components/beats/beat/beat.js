b3625ff40ed32ac8f37085c1e112640c46987ad5
Dave Snider <dave.snider@gmail.com>
2018-07-26 12:29:29 -0700

    tests and summary_status fixes

--- x-pack/plugins/monitoring/public/components/beats/beat/beat.js
+++ x-pack/plugins/monitoring/public/components/beats/beat/beat.js
@@ -4,90 +4,69 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import { MonitoringTimeseriesContainer } from '../../chart';
 import { formatMetric } from '../../../lib/format_number';
-
-function renderTransportAddress(summary) {
-  let output = null;
-  if (summary.name !== summary.transportAddress) {
-    output = (
-      <Fragment>
-        Host: <strong data-test-subj="host">{summary.transportAddress}</strong>
-      </Fragment>
-    );
-  }
-  return output;
-}
+import { EuiFlexItem, EuiFlexGroup } from '@elastic/eui';
 
 export function Beat({ summary, metrics, ...props }) {
+
+  const wrapChild = ({ label, value, dataTestSubj }, index) => (
+    <EuiFlexItem
+      key={`summary-status-item-${index}`}
+      grow={false}
+      data-test-subj={dataTestSubj}
+    >
+      <EuiFlexGroup responsive={false} gutterSize="xs" alignItems="center">
+        <EuiFlexItem grow={false}>
+          {label ? label + ': ' : null}
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <strong>{value}</strong>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiFlexItem>
+  );
+
+  const summarytStatsTop = [
+    { label: 'Name', value: summary.name, dataTestSubj: 'name' },
+    { label: 'Version', value: summary.version, dataTestSubj: 'version' },
+    { label: 'Type', value: summary.type, dataTestSubj: 'type' },
+    { label: 'Output', value: summary.output, dataTestSubj: 'output' },
+    { label: 'Config reloads', value: formatMetric(summary.configReloads, 'int_commas'), dataTestSubj: 'configReloads' },
+    { label: 'Uptime', value: formatMetric(summary.uptime, 'time_since'), dataTestSubj: 'uptime' },
+  ];
+
+  const summarytStatsBot = [
+    { label: 'Events total', value: formatMetric(summary.eventsTotal, 'int_commas'), dataTestSubj: 'eventsTotal' },
+    { label: 'Events emitted', value: formatMetric(summary.eventsEmitted, 'int_commas'), dataTestSubj: 'eventsEmitted' },
+    { label: 'Events dropped', value: formatMetric(summary.eventsDropped, 'int_commas'), dataTestSubj: 'eventsDropped' },
+    { label: 'Bytes sent', value: formatMetric(summary.bytesWritten, 'byte'), dataTestSubj: 'bytesWritten' },
+  ];
+
   return (
     <div>
-      <div role="status">
-        <div className="monSummaryStatus">
-          <div
-            className="monSummaryStatus__content"
-            data-test-subj="beatSummaryStatus01"
-          >
-            <div>
-              <strong data-test-subj="name">{summary.name}</strong>
-            </div>
-            <div>
-              Version:{' '}
-              <strong data-test-subj="version">{summary.version}</strong>
-            </div>
-            <div>
-              Beat Type: <strong data-test-subj="type">{summary.type}</strong>
-            </div>
-            {renderTransportAddress(summary)}
-            <div>
-              Output: <strong data-test-subj="output">{summary.output}</strong>
-            </div>
-            <div>
-              Config Reloads:{' '}
-              <strong data-test-subj="configReloads">
-                {formatMetric(summary.configReloads, 'int_commas')}
-              </strong>
-            </div>
-            <div>
-              Uptime:{' '}
-              <strong data-test-subj="uptime">
-                {formatMetric(summary.uptime, 'time_since')}
-              </strong>
-            </div>
-          </div>
+      <div className="monSummaryStatus" role="status">
+        <div {...props}>
+          <EuiFlexGroup gutterSize="none" alignItems="center" data-test-subj="beatSummaryStatus01">
+            <EuiFlexItem grow={false}>
+              <EuiFlexGroup>
+                {summarytStatsTop.map(wrapChild)}
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </div>
+      </div>
 
-        <div className="monSummaryStatus" role="status">
-          <div
-            className="monSummaryStatus__content"
-            data-test-subj="beatSummaryStatus02"
-          >
-            <div>
-              Events Total:{' '}
-              <strong data-test-subj="eventsTotal">
-                {formatMetric(summary.eventsTotal, 'int_commas')}
-              </strong>
-            </div>
-            <div>
-              Events Emitted:{' '}
-              <strong data-test-subj="eventsEmitted">
-                {formatMetric(summary.eventsEmitted, 'int_commas')}
-              </strong>
-            </div>
-            <div>
-              Events Dropped:{' '}
-              <strong data-test-subj="eventsDropped">
-                {formatMetric(summary.eventsDropped, 'int_commas')}
-              </strong>
-            </div>
-            <div>
-              Bytes Sent:{' '}
-              <strong data-test-subj="bytesWritten">
-                {formatMetric(summary.bytesWritten, 'byte')}
-              </strong>
-            </div>
-          </div>
+      <div className="monSummaryStatus" role="status">
+        <div {...props}>
+          <EuiFlexGroup gutterSize="none" alignItems="center" data-test-subj="beatSummaryStatus02">
+            <EuiFlexItem grow={false}>
+              <EuiFlexGroup>
+                {summarytStatsBot.map(wrapChild)}
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </div>
       </div>
 
