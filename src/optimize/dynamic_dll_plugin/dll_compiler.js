e648ec8611acafeeb813512089db345e36271f1c
Tiago Costa <tiagoffcc@hotmail.com>
2018-07-27 18:55:20 +0100

    refact(20749): string interpolation when creating the path to delete in every optimizer cycle. feat(20749): creating the DynamicDllPlugin foundations.

---
+++ src/optimize/dynamic_dll_plugin/dll_compiler.js
@@ -0,0 +1,92 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import configModel from './dll_config_model';
+import { runWebpack } from './webpack_wrapper';
+import fs from 'fs';
+
+export class DllCompiler {
+  static existsDLLsFromConfig({ dllEntries, outputPath }) {
+    return dllEntries.reduce(
+      (accumulator, currentValue) => {
+        const dllEntryFile = `${outputPath}/${currentValue.name}.entry.dll.js`;
+        const dllManifestFile = `${outputPath}/${currentValue.name}.manifest.dll.json`;
+        const dllFile = `${outputPath}/${currentValue.name}.dll.js`;
+
+        return fs.existsSync(dllEntryFile) && fs.existsSync(dllManifestFile) && fs.existsSync(dllFile);
+      },
+      true);
+  }
+
+  constructor(options, log) {
+    this.dllEntries = options.dllEntries;
+    this.isDistributable = options.isDistributable;
+    this.outputPath = options.outputPath;
+    this.dllsConfigs = this.createDLLsConfigs(options);
+    this.log =  log;
+
+    if (!this.existsDLLs()) {
+      // this.upsertDllEntryFile();
+      // this.touchDllManifests();
+    }
+  }
+
+  createDLLsConfigs(options) {
+    return options.dllEntries.map((dllEntry) => this.dllConfigGenerator({
+      dllEntry,
+      ...options
+    }));
+  }
+
+  dllConfigGenerator(dllConfig) {
+    return configModel.bind(this, dllConfig);
+  }
+
+  upsertDllEntryFile(entryPaths = '', entryName = 'vendor') {
+    const dllEntry = this.dllEntries.find((entry) => {
+      return entry.name === entryName;
+    });
+
+    if (!dllEntry) {
+      return;
+    }
+
+    const entryFileName = `${this.outputPath}/${entryName}.entry.dll.js`;
+    fs.writeFileSync(entryFileName, entryPaths);
+  }
+
+  touchDllManifests() {
+    this.dllEntries.forEach((dllEntry) => {
+      fs.writeFileSync(
+        `${this.outputPath}/${dllEntry.name}.json`,
+        '{}'
+      );
+    });
+  }
+
+  existsDLLs() {
+    return DllCompiler.existsDLLsFromConfig({ dllEntries: this.dllEntries, outputPath: this.outputPath });
+  }
+
+  async run() {
+    for (const dllConfig of this.dllsConfigs) {
+      await runWebpack(dllConfig());
+    }
+  }
+}
---
+++ src/optimize/dynamic_dll_plugin/dll_config_model.js
@@ -0,0 +1,86 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import webpack from 'webpack';
+import webpackMerge from 'webpack-merge';
+
+function generateDLLS({ context, entries, output }) {
+  const finalEntries = {};
+
+  entries.forEach((entry) => {
+    finalEntries[entry.name] = [`${output.path}/${entry.name}.entry.dll.js`];
+  });
+
+  return {
+    entry: finalEntries,
+    context,
+    output: {
+      filename: `${output.dllName}.dll.js`,
+      path: output.path,
+      publicPath: output.publicPath,
+      library: output.dllName
+    },
+    plugins: [
+      new webpack.DllPlugin({
+        context,
+        name: output.dllName,
+        path: `${output.path}/${output.manifestName}.manifest.dll.json`
+      })
+    ]
+  };
+}
+
+function common(options) {
+  return webpackMerge(
+    generateDLLS({
+      context: options.context,
+      entries: options.dllEntries,
+      output: {
+        manifestName: '[name]',
+        dllName: '[name]',
+        path: options.outputPath,
+        publicPath: options.publicPath
+      }
+    })
+  );
+}
+
+function optimized() {
+  return webpackMerge(
+    {
+      mode: 'production'
+    }
+  );
+}
+
+function unoptimized() {
+  return webpackMerge(
+    {
+      mode: 'development'
+    }
+  );
+}
+
+export default (options = {}) => {
+  if (options.isDistributable) {
+    return webpackMerge(common(options), optimized(), options.mergeConfig);
+  }
+
+  return webpackMerge(common(options), unoptimized(), options.mergeConfig);
+};
