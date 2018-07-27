918ea5925206de2a4338af305c79d1f236cfb213
CJ Cenizal <cj@cenizal.com>
2018-07-27 10:03:50 -0700

    Uncomment test config.

--- test/functional/apps/dashboard/index.js
+++ test/functional/apps/dashboard/index.js
@@ -42,19 +42,19 @@ export default function ({ getService, loadTestFile, getPageObjects }) {
 
       // This has to be first since the other tests create some embeddables as side affects and our counting assumes
       // a fresh index.
-      // loadTestFile(require.resolve('./_embeddable_rendering'));
-      // loadTestFile(require.resolve('./_create_and_add_embeddables'));
-      // loadTestFile(require.resolve('./_time_zones'));
-      // loadTestFile(require.resolve('./_dashboard_options'));
-      // loadTestFile(require.resolve('./_data_shared_attributes'));
-      // loadTestFile(require.resolve('./_embed_mode'));
-      // loadTestFile(require.resolve('./_full_screen_mode'));
-      // loadTestFile(require.resolve('./_dashboard_filter_bar'));
-      // loadTestFile(require.resolve('./_dashboard_filtering'));
-      // loadTestFile(require.resolve('./_panel_expand_toggle'));
-      // loadTestFile(require.resolve('./_dashboard_grid'));
-      // loadTestFile(require.resolve('./_dashboard_snapshots'));
-      // loadTestFile(require.resolve('./_view_edit'));
+      loadTestFile(require.resolve('./_embeddable_rendering'));
+      loadTestFile(require.resolve('./_create_and_add_embeddables'));
+      loadTestFile(require.resolve('./_time_zones'));
+      loadTestFile(require.resolve('./_dashboard_options'));
+      loadTestFile(require.resolve('./_data_shared_attributes'));
+      loadTestFile(require.resolve('./_embed_mode'));
+      loadTestFile(require.resolve('./_full_screen_mode'));
+      loadTestFile(require.resolve('./_dashboard_filter_bar'));
+      loadTestFile(require.resolve('./_dashboard_filtering'));
+      loadTestFile(require.resolve('./_panel_expand_toggle'));
+      loadTestFile(require.resolve('./_dashboard_grid'));
+      loadTestFile(require.resolve('./_dashboard_snapshots'));
+      loadTestFile(require.resolve('./_view_edit'));
     });
 
     // Each of these tests call initTests themselves, the way it was originally written.  The above tests only load
@@ -63,33 +63,14 @@ export default function ({ getService, loadTestFile, getPageObjects }) {
     describe('using legacy data', function () {
       before(() => remote.setWindowSize(1200, 900));
 
-      // loadTestFile(require.resolve('./_dashboard_time_picker'));
-      // loadTestFile(require.resolve('./_bwc_shared_urls'));
-      // loadTestFile(require.resolve('./_panel_controls'));
-      // loadTestFile(require.resolve('./_dashboard_state'));
-      // loadTestFile(require.resolve('./_dashboard_save'));
+      loadTestFile(require.resolve('./_dashboard_time_picker'));
+      loadTestFile(require.resolve('./_bwc_shared_urls'));
+      loadTestFile(require.resolve('./_panel_controls'));
+      loadTestFile(require.resolve('./_dashboard_state'));
+      loadTestFile(require.resolve('./_dashboard_save'));
       loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      loadTestFile(require.resolve('./_dashboard_time'));
-      // loadTestFile(require.resolve('./_dashboard_listing'));
-      // loadTestFile(require.resolve('./_dashboard_clone'));
+      loadTestFile(require.resolve('./_dashboard_listing'));
+      loadTestFile(require.resolve('./_dashboard_clone'));
     });
   });
 }
