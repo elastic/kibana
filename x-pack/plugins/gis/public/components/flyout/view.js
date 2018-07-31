f70173542a6b7dac075b758bb5361be8b220f9ee
Thomas Neirynck <thomas@elastic.co>
2018-07-31 15:14:58 -0400

    add tms

--- x-pack/plugins/gis/public/components/flyout/view.js
+++ x-pack/plugins/gis/public/components/flyout/view.js
@@ -7,6 +7,7 @@
 import React, { Fragment } from 'react';
 import {
   EuiAccordion,
+  EuiFieldText,
   EuiText,
   EuiSelect,
   EuiSpacer,
@@ -24,21 +25,27 @@ import {
 } from '@elastic/eui';
 
 
+let idCounter = 0;
+
 export class FlyOut extends React.Component {
 
   constructor() {
     super();
+    this.state = {
+      tmsInput: ''
+    };
   }
+
 
   _renderFlexibleSelect = (options, selectAction) => {
     const onChange = ({ target }) => {
-      const [ source, id ] = target.value.split(':');
+      const [source, id] = target.value.split(':');
       selectAction(source, +id);
     };
 
     return (
       <Fragment>
-        <EuiSpacer size="m" />
+        <EuiSpacer size="m"/>
         <EuiSelect
           options={options}
           onChange={onChange}
@@ -47,6 +54,14 @@ export class FlyOut extends React.Component {
       </Fragment>
     );
   };
+
+
+  _handleTMSInputChange(e) {
+    this.setState({
+      tmsInput: e.target.value,
+      tmsCanPreview: (e.target.value.indexOf('{x}') >= 0 && e.target.value.indexOf('{y}') >= 0 && e.target.value.indexOf('{z}') >= 0)
+    });
+  }
 
   _addToMapBtn() {
     const { layerLoading, temporaryLayers, addAction } = this.props;
@@ -65,11 +80,18 @@ export class FlyOut extends React.Component {
     );
   }
 
-  _renderFlyout() {
-    const { emsVectorOptions, selectAction, closeFlyout } = this.props;
+  _previewTMS() {
+    if (!this.state.tmsInput) {
+      return;
+    }
+    this.props.previewXYZLayer(this.state.tmsInput, this.state.tmsInput + (idCounter++).toString(), {});
+  }
 
+  _renderFlyout() {
+
+    const { emsVectorOptions, selectAction, closeFlyout } = this.props;
     return (
-      <EuiFlyout onClose={this.props.onClose} style={{ maxWidth: 768 }}>
+      <EuiFlyout onClose={() => console.log('onClose todo')} style={{ maxWidth: 768 }}>
         <EuiFlyoutHeader>
           <EuiTitle size="l">
             <h2>Add layer</h2>
@@ -86,15 +108,38 @@ export class FlyOut extends React.Component {
 
         <EuiFlyoutBody style={{ paddingTop: 0 }}>
           <div>
-            <EuiSpacer size="l" />
+            <EuiSpacer size="l"/>
             <EuiAccordion
               id="accordion2"
-              buttonContent="Import from Elastic Maps Service"
+              buttonContent="From Elastic Maps Service"
               paddingSize="l"
             >
               <EuiText>
-                { this._renderFlexibleSelect(emsVectorOptions, selectAction) }
+                {this._renderFlexibleSelect(emsVectorOptions, selectAction)}
               </EuiText>
+            </EuiAccordion>
+          </div>
+          <div>
+            <EuiSpacer size="l"/>
+            <EuiAccordion
+              id="accordion2"
+              buttonContent="Tilemap service with XYZ url"
+              paddingSize="l"
+            >
+              <Fragment>
+                <EuiFieldText
+                  placeholder="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  onChange={(e) => this._handleTMSInputChange(e)}
+                  aria-label="Use aria labels when no actual label is in use"
+                />
+                <EuiButton
+                  size="s"
+                  onClick={(e) => this._previewTMS(e)}
+                  isDisabled={!this.state.tmsCanPreview}
+                >
+                  {this.state.tmsCanPreview ? "Preview on Map" : "Enter URL with {x}/{y}/{x} pattern." }
+                </EuiButton>
+              </Fragment>
             </EuiAccordion>
           </div>
         </EuiFlyoutBody>
