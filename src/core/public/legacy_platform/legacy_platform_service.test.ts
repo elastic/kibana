64b29d412ddd1986db95db9b6b5c92c942038c7c
spalger <email@spalger.com>
2018-08-01 09:23:02 -0700

    avoid unnecessary recreation of default param

--- src/core/public/legacy_platform/legacy_platform_service.test.ts
+++ src/core/public/legacy_platform/legacy_platform_service.test.ts
@@ -69,7 +69,6 @@ const defaultParams = {
 };
 
 afterEach(() => {
-  defaultParams.targetDomElement = document.createElement('div');
   jest.clearAllMocks();
   injectedMetadataStartContract.getLegacyMetadata.mockReset();
   jest.resetModules();
