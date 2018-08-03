b68dab8c9753a49d975a24248f4bd4ffb1656a7c
Justin Kambic <justin.kambic@elastic.co>
2018-08-03 10:28:55 -0400

    [Beats Management] Add BeatsTable/Bulk Action Search Component (#21182)
    * Add BeatsTable and control bar components.
    
    * Clean yarn.lock.
    
    * Move raw numbers/strings to constants. Remove obsolete state/props.
    
    * Update/add tests.
    
    * Change prop name from "items" to "beats".
    
    * Rename some variables.
    
    * Move search bar filter definitions to table render.
    
    * Update table to support assignment options.
    
    * Update action control position.
    
    * Refactor split render function into custom components.

---
+++ x-pack/plugins/beats_management/public/components/table/table_type_configs.tsx
@@ -0,0 +1,116 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+// @ts-ignore
+import { EuiBadge, EuiFlexGroup, EuiIcon, EuiLink } from '@elastic/eui';
+import { flatten, uniq } from 'lodash';
+import moment from 'moment';
+import React from 'react';
+import { CMPopulatedBeat } from '../../../common/domain_types';
+
+export interface ColumnDefinition {
+  field: string;
+  name: string;
+  sortable?: boolean;
+  render?(value: any, object?: any): any;
+}
+
+export interface ActionDefinition {
+  action: string;
+  danger?: boolean;
+  icon?: any;
+  name: string;
+}
+
+interface FilterOption {
+  value: string;
+}
+
+export interface FilterDefinition {
+  field: string;
+  name: string;
+  options?: FilterOption[];
+  type: string;
+}
+
+export interface ControlDefinitions {
+  actions: ActionDefinition[];
+  filters: FilterDefinition[];
+}
+
+export interface TableType {
+  columnDefinitions: ColumnDefinition[];
+  controlDefinitions(items: any[]): ControlDefinitions;
+}
+
+export const BeatsTableType: TableType = {
+  columnDefinitions: [
+    {
+      field: 'id',
+      name: 'Beat name',
+      render: (id: string) => <EuiLink>{id}</EuiLink>,
+      sortable: true,
+    },
+    {
+      field: 'type',
+      name: 'Type',
+      sortable: true,
+    },
+    {
+      field: 'full_tags',
+      name: 'Tags',
+      render: (value: string, beat: CMPopulatedBeat) => (
+        <EuiFlexGroup wrap responsive={true}>
+          {beat.full_tags.map(tag => (
+            <EuiBadge key={tag.id} color={tag.color ? tag.color : 'primary'}>
+              {tag.id}
+            </EuiBadge>
+          ))}
+        </EuiFlexGroup>
+      ),
+      sortable: false,
+    },
+    {
+      // TODO: update to use actual metadata field
+      field: 'event_rate',
+      name: 'Event rate',
+      sortable: true,
+    },
+    {
+      // TODO: update to use actual metadata field
+      field: 'last_updated',
+      name: 'Last config update',
+      render: (value: Date) => <div>{moment(value).fromNow()}</div>,
+      sortable: true,
+    },
+  ],
+  controlDefinitions: (data: any) => ({
+    actions: [
+      {
+        name: 'Disenroll Selected',
+        action: 'delete',
+        danger: true,
+      },
+    ],
+    filters: [
+      {
+        type: 'field_value_selection',
+        field: 'type',
+        name: 'Type',
+        options: uniq(data.map(({ type }: { type: any }) => ({ value: type })), 'value'),
+      },
+      {
+        type: 'field_value_selection',
+        field: 'full_tags',
+        name: 'Tags',
+        options: uniq(
+          flatten(data.map((item: any) => item.full_tags)).map((tag: any) => ({ value: tag.id })),
+          'value'
+        ),
+      },
+    ],
+  }),
+};
--- x-pack/plugins/beats_management/public/pages/main/beats.tsx
+++ x-pack/plugins/beats_management/public/pages/main/beats.tsx
@@ -5,8 +5,11 @@
  */
 
 import {
+  // @ts-ignore typings for EuiBadge not present in current version
+  EuiBadge,
   EuiButton,
   EuiButtonEmpty,
+  EuiFlexItem,
   EuiModal,
   EuiModalBody,
   EuiModalFooter,
@@ -15,16 +18,20 @@ import {
   EuiOverlayMask,
 } from '@elastic/eui';
 
-import { CMBeat } from '../../../common/domain_types';
+import React from 'react';
+import { BeatTag, CMBeat, CMPopulatedBeat } from '../../../common/domain_types';
+import { BeatsTagAssignment } from '../../../server/lib/adapters/beats/adapter_types';
+import { BeatsTableType, Table } from '../../components/table';
 import { FrontendLibs } from '../../lib/lib';
 
-import React from 'react';
 interface BeatsPageProps {
   libs: FrontendLibs;
 }
 
 interface BeatsPageState {
   beats: CMBeat[];
+  tags: any[] | null;
+  tableRef: any;
 }
 
 export class BeatsPage extends React.PureComponent<BeatsPageProps, BeatsPageState> {
@@ -72,17 +79,109 @@ export class BeatsPage extends React.PureComponent<BeatsPageProps, BeatsPageStat
 
     this.state = {
       beats: [],
+      tableRef: React.createRef(),
+      tags: null,
     };
 
     this.loadBeats();
   }
   public render() {
-    return <div>beats table and stuff - {this.state.beats.length}</div>;
+    return (
+      <Table
+        actionHandler={this.handleBeatsActions}
+        assignmentOptions={this.state.tags}
+        assignmentTitle="Set tags"
+        items={this.state.beats}
+        ref={this.state.tableRef}
+        type={BeatsTableType}
+      />
+    );
   }
+
+  private handleBeatsActions = (action: string, payload: any) => {
+    switch (action) {
+      case 'edit':
+        // TODO: navigate to edit page
+        break;
+      case 'delete':
+        this.deleteSelected();
+        break;
+      case 'search':
+        this.handleSearchQuery(payload);
+        break;
+      case 'loadAssignmentOptions':
+        this.loadTags();
+        break;
+    }
+
+    this.loadBeats();
+  };
+
+  // TODO: call delete endpoint
+  private deleteSelected = async () => {
+    // const selected = this.getSelectedBeats();
+    // await this.props.libs.beats.delete(selected);
+  };
+
   private async loadBeats() {
     const beats = await this.props.libs.beats.getAll();
     this.setState({
       beats,
     });
   }
+
+  // todo: add reference to ES filter endpoint
+  private handleSearchQuery = (query: any) => {
+    // await this.props.libs.beats.searach(query);
+  };
+
+  private loadTags = async () => {
+    const tags = await this.props.libs.tags.getAll();
+    const selectedBeats = this.getSelectedBeats();
+
+    const renderedTags = tags.map((tag: BeatTag) => {
+      const hasMatches = selectedBeats.some((beat: any) =>
+        beat.full_tags.some((t: any) => t.id === tag.id)
+      );
+
+      return (
+        <EuiFlexItem key={tag.id}>
+          <EuiBadge
+            color={tag.color}
+            iconType={hasMatches ? 'cross' : null}
+            onClick={
+              hasMatches
+                ? () => this.removeTagsFromBeats(selectedBeats, tag)
+                : () => this.assignTagsToBeats(selectedBeats, tag)
+            }
+            onClickAriaLabel={tag.id}
+          >
+            {tag.id}
+          </EuiBadge>
+        </EuiFlexItem>
+      );
+    });
+    this.setState({
+      tags: renderedTags,
+    });
+  };
+
+  private createBeatTagAssignments = (
+    beats: CMPopulatedBeat[],
+    tag: BeatTag
+  ): BeatsTagAssignment[] => beats.map(({ id }) => ({ beatId: id, tag: tag.id }));
+
+  private removeTagsFromBeats = async (beats: CMPopulatedBeat[], tag: BeatTag) => {
+    await this.props.libs.beats.removeTagsFromBeats(this.createBeatTagAssignments(beats, tag));
+    this.loadBeats();
+  };
+
+  private assignTagsToBeats = async (beats: CMPopulatedBeat[], tag: BeatTag) => {
+    await this.props.libs.beats.assignTagsToBeats(this.createBeatTagAssignments(beats, tag));
+    this.loadBeats();
+  };
+
+  private getSelectedBeats = () => {
+    return this.state.tableRef.current.state.selection;
+  };
 }
