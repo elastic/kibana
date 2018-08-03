816772419ac6dd8db29ace5a765a2fb49200b783
Justin Kambic <justin.kambic@elastic.co>
2018-08-03 10:28:55 -0400

    [Beats Management] Add BeatsTable/Bulk Action Search Component (#21182)
    * Add BeatsTable and control bar components.
    
    * Clean yarn.lock.
    
    * Move raw numbers/strings to constants. Remove obsolete state/props.
    
    * Update/add tests.
    
    * Change prop name from "items" to "beats".
    
    * Rename some variables.
    
    * Move search bar filter definitions to table render.
    
    * Update table to support assignment options.
    
    * Update action control position.
    
    * Refactor split render function into custom components.

---
+++ x-pack/plugins/beats_management/public/components/table/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export { Table } from './table';
+export { ControlBar } from './controls';
+export { BeatsTableType } from './table_type_configs';
