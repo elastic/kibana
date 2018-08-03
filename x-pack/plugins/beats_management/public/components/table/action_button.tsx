1369e68b2540a4d9e2b3d23cca68d81de6113cba
Justin Kambic <justin.kambic@elastic.co>
2018-08-03 10:28:55 -0400

    [Beats Management] Add BeatsTable/Bulk Action Search Component (#21182)
    * Add BeatsTable and control bar components.
    
    * Clean yarn.lock.
    
    * Move raw numbers/strings to constants. Remove obsolete state/props.
    
    * Update/add tests.
    
    * Change prop name from "items" to "beats".
    
    * Rename some variables.
    
    * Move search bar filter definitions to table render.
    
    * Update table to support assignment options.
    
    * Update action control position.
    
    * Refactor split render function into custom components.

---
+++ x-pack/plugins/beats_management/public/components/table/action_button.tsx
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { EuiButton, EuiContextMenu, EuiPopover } from '@elastic/eui';
+import React from 'react';
+import { ActionDefinition } from './table_type_configs';
+
+interface ActionButtonProps {
+  actions: ActionDefinition[];
+  isPopoverVisible: boolean;
+  actionHandler(action: string, payload?: any): void;
+  hidePopover(): void;
+  showPopover(): void;
+}
+
+export function ActionButton(props: ActionButtonProps) {
+  const { actions, actionHandler, hidePopover, isPopoverVisible, showPopover } = props;
+  if (actions.length === 0) {
+    return null;
+  } else if (actions.length === 1) {
+    const action = actions[0];
+    return (
+      <EuiButton
+        color={action.danger ? 'danger' : 'primary'}
+        onClick={() => actionHandler(action.action)}
+      >
+        {action.name}
+      </EuiButton>
+    );
+  }
+  return (
+    <EuiPopover
+      anchorPosition="downLeft"
+      button={
+        <EuiButton iconSide="right" iconType="arrowDown" onClick={showPopover}>
+          Bulk Action
+        </EuiButton>
+      }
+      closePopover={hidePopover}
+      id="contextMenu"
+      isOpen={isPopoverVisible}
+      panelPaddingSize="none"
+      withTitle
+    >
+      <EuiContextMenu
+        initialPanelId={0}
+        panels={[
+          {
+            id: 0,
+            title: 'Bulk Actions',
+            items: actions.map(action => ({
+              ...action,
+              onClick: () => actionHandler(action.action),
+            })),
+          },
+        ]}
+      />
+    </EuiPopover>
+  );
+}
