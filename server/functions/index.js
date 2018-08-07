734256ec1ffa4bbc7472049e350bb4da1c9de690
joe fleming <joe.fleming@gmail.com>
2018-08-07 15:31:19 -0700

    feat: add server function

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,7 +2,8 @@ import { demodata } from './demodata';
 import { escount } from './escount';
 import { esdocs } from './esdocs';
 import { pointseries } from './pointseries';
+import { server } from './server';
 import { timelion } from './timelion';
 import { essql } from './essql';
 
-export const serverFunctions = [esdocs, escount, demodata, pointseries, timelion, essql];
+export const serverFunctions = [esdocs, escount, demodata, pointseries, server, timelion, essql];
