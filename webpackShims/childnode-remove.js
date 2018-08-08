08005f870acc0e3cdc13cdb6e5f7e0793a7299fd
Nathan Reese <reese.nathan@gmail.com>
2018-08-08 16:59:47 -0600

    just use polyfill that mozilla recommends

--- webpackShims/childnode-remove.js
+++ webpackShims/childnode-remove.js
@@ -17,6 +17,22 @@
  * under the License.
  */
 
-const childNodeRemove = require('childnode-remove');
+/* eslint-disable */
 
-childNodeRemove.polyfill();
+// https://opensource.org/licenses/MIT
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        if (this.parentNode !== null)
+          this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
