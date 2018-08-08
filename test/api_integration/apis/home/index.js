0124dcac06996b944f4605ae88fe09683310a05c
Nathan Reese <reese.nathan@gmail.com>
2018-08-08 09:13:05 -0600

    add "now" query parameter to sample data install endpoint (#20678)
    * add now query parameter to sample data install endpoint
    
    * joi returns date, so no need to create a new one
    
    * add API functional tests for now parameter

--- test/api_integration/apis/home/index.js
+++ test/api_integration/apis/home/index.js
@@ -18,18 +18,7 @@
  */
 
 export default function ({ loadTestFile }) {
-  describe('apis', () => {
-    loadTestFile(require.resolve('./elasticsearch'));
-    loadTestFile(require.resolve('./general'));
-    loadTestFile(require.resolve('./home'));
-    loadTestFile(require.resolve('./index_patterns'));
-    loadTestFile(require.resolve('./management'));
-    loadTestFile(require.resolve('./saved_objects'));
-    loadTestFile(require.resolve('./scripts'));
-    loadTestFile(require.resolve('./search'));
-    loadTestFile(require.resolve('./shorten'));
-    loadTestFile(require.resolve('./suggestions'));
-    loadTestFile(require.resolve('./status'));
-    loadTestFile(require.resolve('./stats'));
+  describe('home apis', () => {
+    loadTestFile(require.resolve('./sample_data'));
   });
 }
