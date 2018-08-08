6d52fdf2853d85984d34888640f4b2df1ba65165
Tyler Smalley <tyler.smalley@elastic.co>
2018-08-07 19:58:46 -0700

    postcss-loader does not respect .browserslistrc
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- src/optimize/postcss.config.js
+++ src/optimize/postcss.config.js
@@ -19,6 +19,12 @@
 
 module.exports = {
   plugins: [
-    require('autoprefixer')()
+    require('autoprefixer')({
+      browsers: [
+        'last 2 versions',
+        '> 5%',
+        'Safari 7' // for PhantomJS support
+      ]
+    })
   ]
 };
