d3360bf367551e036eebe693f20f51c691bd79dd
Timothy Sullivan <tsullivan@elastic.co>
2018-08-07 18:53:19 -0700

    move usage collector to oss

--- x-pack/plugins/monitoring/server/kibana_monitoring/collectors/index.js
+++ x-pack/plugins/monitoring/server/kibana_monitoring/collectors/index.js
@@ -4,6 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { getKibanaUsageCollector } from './get_kibana_usage_collector';
 export { getOpsStatsCollector } from './get_ops_stats_collector';
 export { getSettingsCollector } from './get_settings_collector';
