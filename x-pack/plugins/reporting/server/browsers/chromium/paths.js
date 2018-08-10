564e634b5af806ed7e2196f111a5896539048c2e
Christopher Davies <github@christophilus.com>
2018-08-10 10:17:10 -0400

    Remove all but Linux Chromium packages

--- x-pack/plugins/reporting/server/browsers/chromium/paths.js
+++ x-pack/plugins/reporting/server/browsers/chromium/paths.js
@@ -10,19 +10,9 @@ export const paths = {
   archivesPath: path.resolve(__dirname, '../../../.chromium'),
   baseUrl: 'https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-53/',
   packages: [{
-    platforms: ['darwin', 'freebsd', 'openbsd'],
-    archiveFilename: 'chromium-503a3e4-darwin.zip',
-    archiveChecksum: 'c1b530f99374e122c0bd7ba663867a95',
-    binaryRelativePath: 'headless_shell-darwin/headless_shell',
-  }, {
     platforms: ['linux'],
     archiveFilename: 'stable-headless-chromium-amazonlinux-2017-03.zip',
     archiveChecksum: 'd752c95520d42c666574c271dda8fd65',
     binaryRelativePath: ''
-  }, {
-    platforms: ['win32'],
-    archiveFilename: 'chromium-503a3e4-win32.zip',
-    archiveChecksum: 'a71ce5565791767492f6d0fb4fe5360d',
-    binaryRelativePath: 'headless_shell-win32\\headless_shell.exe'
   }]
 };
