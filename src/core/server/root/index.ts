1bb3d3dc6e7bca34befd65dad35c6c840de95ee7
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-08-10 14:25:56 +0200

    Merge `MutableLoggerFactory` and `LoggingService`.

--- src/core/server/root/index.ts
+++ src/core/server/root/index.ts
@@ -17,15 +17,13 @@
  * under the License.
  */
 
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { first, skip } from 'rxjs/operators';
 
 import { Server } from '..';
 import { ConfigService, Env, RawConfig } from '../config';
 
-import { Logger } from '../logging';
-import { LoggerFactory, MutableLoggerFactory } from '../logging/logger_factory';
-import { LoggingConfig } from '../logging/logging_config';
-import { LoggingService } from '../logging/logging_service';
+import { Logger, LoggerFactory, LoggingConfig, LoggingService } from '../logging';
 
 export type OnShutdown = (reason?: Error) => void;
 
@@ -33,11 +31,12 @@ export type OnShutdown = (reason?: Error) => void;
  * Top-level entry point to kick off the app and start the Kibana server.
  */
 export class Root {
-  public configService: ConfigService;
-  public readonly log: Logger;
   public readonly logger: LoggerFactory;
+  protected readonly configService: ConfigService;
+  private readonly log: Logger;
   private server?: Server;
   private readonly loggingService: LoggingService;
+  private loggingConfigSubscription?: Subscription;
 
   constructor(
     rawConfig$: Observable<RawConfig>,
@@ -46,40 +45,34 @@ export class Root {
       // noop
     }
   ) {
-    const loggerFactory = new MutableLoggerFactory();
-    this.loggingService = new LoggingService(loggerFactory);
-    this.logger = loggerFactory;
+    this.loggingService = new LoggingService();
+    this.logger = this.loggingService.asLoggerFactory();
 
     this.log = this.logger.get('root');
     this.configService = new ConfigService(rawConfig$, env, this.logger);
   }
 
   public async start() {
-    try {
-      const loggingConfig$ = this.configService.atPath('logging', LoggingConfig);
-      this.loggingService.upgrade(loggingConfig$);
-    } catch (e) {
-      // This specifically console.logs because we were not able to configure
-      // the logger.
-      // tslint:disable no-console
-      console.error('Configuring logger failed:', e.message);
-
-      await this.shutdown(e);
-      throw e;
-    }
+    this.log.debug('starting root');
 
     try {
+      await this.setupLogging();
       await this.startServer();
     } catch (e) {
-      this.log.error(e);
-
       await this.shutdown(e);
       throw e;
     }
   }
 
   public async shutdown(reason?: Error) {
+    this.log.debug('shutting root down');
+
     await this.stopServer();
+
+    if (this.loggingConfigSubscription !== undefined) {
+      this.loggingConfigSubscription.unsubscribe();
+      this.loggingConfigSubscription = undefined;
+    }
 
     await this.loggingService.stop();
 
@@ -98,5 +91,40 @@ export class Root {
 
     await this.server.stop();
     this.server = undefined;
+  }
+
+  private async setupLogging() {
+    function onLogUpgradeError(err: Error) {
+      // This specifically console.logs because we were not able to configure the logger.
+      // tslint:disable no-console
+      console.error('Configuring logger failed:', err);
+    }
+
+    // We should try to upgrade logger as soon as the root starts and shut root
+    // down if we can't do that, and subscribe to the consequent updates only if
+    // initial upgrade succeeded.
+    const config$ = this.configService.atPath('logging', LoggingConfig);
+    try {
+      this.loggingService.upgrade(await config$.pipe(first()).toPromise());
+    } catch (err) {
+      onLogUpgradeError(err);
+      throw err;
+    }
+
+    // Skip the first emission as it has just been processed above.
+    this.loggingConfigSubscription = config$.pipe(skip(1)).subscribe({
+      next: async config => {
+        try {
+          this.loggingService.upgrade(config);
+        } catch (err) {
+          onLogUpgradeError(err);
+          await this.shutdown(err);
+        }
+      },
+      error: async err => {
+        onLogUpgradeError(err);
+        await this.shutdown(err);
+      },
+    });
   }
 }
