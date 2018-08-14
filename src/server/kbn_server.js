999fe7ad23b4ed68c4268275d79411081f2d109c
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-08-14 07:37:01 +0200

    Do not limit payload size for requests that are proxied to the legacy Kibana. (#21750)

--- src/server/kbn_server.js
+++ src/server/kbn_server.js
@@ -144,9 +144,9 @@ export default class KbnServer {
    * @return undefined
    */
   async listen() {
-    const { server } = this;
-
     await this.ready();
+
+    const { server } = this;
     await fromNode(cb => server.start(cb));
 
     if (isWorker) {
