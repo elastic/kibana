55be6a341ca8a8bb79d40b91afec558a2c335542
Robert Monfera <monfera@users.noreply.github.com>
2018-08-16 23:25:50 +0200

    Fix: remove event handler (#976)

--- public/components/workpad_page/event_handlers.js
+++ public/components/workpad_page/event_handlers.js
@@ -25,6 +25,8 @@ const setupHandler = (commit, target) => {
     e.stopPropagation();
     const { clientX, clientY } = e;
     const { x, y } = localMousePosition(target, clientX, clientY);
+    window.onmousemove = null;
+    window.onmouseup = null;
     commit('mouseEvent', { event: 'mouseUp', x, y });
   };
 };
