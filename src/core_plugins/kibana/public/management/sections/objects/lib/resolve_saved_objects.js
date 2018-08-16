c87f439b53d97af7ce27775ff7b389c0f8ddfa92
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-08-16 17:24:57 -0400

    Revert "fixing issue with importing vis with missing saved search (#22029) (#22076)"
    This reverts commit 4401b02365db625080bcdc7b8c283a0809cc20e0.

--- src/core_plugins/kibana/public/management/sections/objects/lib/resolve_saved_objects.js
+++ src/core_plugins/kibana/public/management/sections/objects/lib/resolve_saved_objects.js
@@ -217,12 +217,9 @@ export async function resolveSavedObjects(
       if (await importDocument(obj, otherDoc, overwriteAll)) {
         importedObjectCount++;
       }
-    } catch (error) {
-      if (error instanceof SavedObjectNotFound) {
-        if (error.savedObjectType === 'search') {
-          failedImports.push({ obj, error });
-        }
-        if (error.savedObjectType === 'index-pattern') {
+    } catch (err) {
+      if (err instanceof SavedObjectNotFound) {
+        if (err.savedObjectType === 'index-pattern') {
           if (obj.savedSearchId) {
             conflictedSavedObjectsLinkedToSavedSearches.push(obj);
           } else {
