0dd9ccd38425e7f22a28b085033cbec3e7b80c74
Larry Gregory <lgregorydev@gmail.com>
2018-08-16 09:29:09 -0400

    [Spaces] - Reporting updates (#21457)
    @kobelb -- this PR in its current state is just enough to get the reporting API tests passing again.
    
    This doesn't account for space-awareness, and I'll need to pick your brain as to how to do that for reporting, but I figured that would be better addressed when we tackle space-aware advanced settings (it appears that reporting is only using the SOC via the UISettingsService below)

--- x-pack/plugins/reporting/export_types/printable_pdf/server/execute_job/index.js
+++ x-pack/plugins/reporting/export_types/printable_pdf/server/execute_job/index.js
@@ -31,6 +31,8 @@ function executeJobFn(server) {
   const crypto = cryptoFactory(server);
   const compatibilityShim = compatibilityShimFactory(server);
 
+  const serverBasePath = server.config().get('server.basePath');
+
   const decryptJobHeaders = async (job) => {
     const decryptedHeaders = await crypto.decrypt(job.headers);
     return { job, decryptedHeaders };
@@ -44,6 +46,10 @@ function executeJobFn(server) {
   const getCustomLogo = async ({ job, filteredHeaders }) => {
     const fakeRequest = {
       headers: filteredHeaders,
+      // This is used by the spaces SavedObjectClientWrapper to determine the existing space.
+      // We use the basePath from the saved job, which we'll have post spaces being implemented;
+      // or we use the server base path, which uses the default space
+      getBasePath: () => job.basePath || serverBasePath
     };
 
     const savedObjects = server.savedObjects;
