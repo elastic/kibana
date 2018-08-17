36177f4b28947773470c777d29472dfdc64b6841
archana <archanid@users.noreply.github.com>
2018-08-17 01:36:15 -0500

    [Tests] Add http integration test setup (#19261)
    * [Tests] Add http integration test setup
    
    * Base path tests
    
    * SSL tests
    
    * Eslint fixes
    
    * Remove env from config schema
    
    * Rename folders so no_rewrite and rewrite match configs/tests
    
    * wip
    
    * Use self-signed cert for SSL test
    
    * Improve basepath tests
    
    * Run base path proxy server in dev mode for now
    
    * Remove env from x-pack reporting config
    
    * Remove redundant base-path tests
    
    * Test SSL with redirectHttpFromPort set
    
    * Test SSL with redirectHttpFromPort set
    
    * Flesh out comments
    
    * Remove some cruft
    
    * Add SSL tests to CI run

--- tasks/config/run.js
+++ tasks/config/run.js
@@ -162,6 +162,19 @@ module.exports = function (grunt) {
       ],
     },
 
+    serverIntegrationTests: {
+      cmd: process.execPath,
+      args: [
+        'scripts/functional_tests',
+        '--config', 'test/server_integration/http/ssl/config.js',
+        '--config', 'test/server_integration/http/ssl_redirect/config.js',
+        '--esFrom', 'source',
+        '--bail',
+        '--debug',
+        '--kibana-install-dir', `./build/oss/kibana-${PKG_VERSION}-${process.platform}-x86_64`,
+      ],
+    },
+
     panelActionTests: {
       cmd: process.execPath,
       args: [
@@ -182,7 +195,7 @@ module.exports = function (grunt) {
         '--bail',
         '--debug',
         '--',
-        '--server.maxPayloadBytes=1648576',
+        '--server.maxPayloadBytes=1648576', //default is 1048576
       ],
     },
 
