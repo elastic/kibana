559efec1a39b772df444bbe34395b2910d4c9c6f
CJ Cenizal <cj@cenizal.com>
2018-08-17 11:07:19 -0700

    Add a simple read-only table of rollup jobs and a details panel (#21900)

---
+++ x-pack/plugins/rollup/public/crud_app/services/api.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import chrome from 'ui/chrome';
+
+let httpClient;
+
+export const setHttpClient = (client) => {
+  httpClient = client;
+};
+
+const apiPrefix = chrome.addBasePath('/api/rollup');
+
+export async function loadJobs() {
+  const { data: { jobs } } = await httpClient.get(`${apiPrefix}/jobs`);
+  return jobs;
+}
