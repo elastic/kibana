8ea13b5cf5bec6a81532a2276ca303e18ecb6f92
CJ Cenizal <cj@cenizal.com>
2018-08-17 11:07:19 -0700

    Add a simple read-only table of rollup jobs and a details panel (#21900)

--- x-pack/plugins/rollup/public/crud_app/sections/job_list/job_table/index.js
+++ x-pack/plugins/rollup/public/crud_app/sections/job_list/job_table/index.js
@@ -4,5 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { PLUGIN } from './plugin';
-export { CRUD_APP_BASE_PATH } from './crud_app';
+export { JobTable } from './job_table.container';
