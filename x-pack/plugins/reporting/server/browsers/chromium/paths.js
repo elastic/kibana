e8bb0747de5a764a37a41e85e6f408a3590169f4
Christopher Davies <github@christophilus.com>
2018-08-20 16:08:12 -0400

    Attempt to run Chromium + puppeteer

--- x-pack/plugins/reporting/server/browsers/chromium/paths.js
+++ x-pack/plugins/reporting/server/browsers/chromium/paths.js
@@ -8,21 +8,21 @@ import path from 'path';
 
 export const paths = {
   archivesPath: path.resolve(__dirname, '../../../.chromium'),
-  baseUrl: 'https://s3.amazonaws.com/headless-shell/',
+  baseUrl: 'https://s3.amazonaws.com/headless-shell-dev/',
   packages: [{
     platforms: ['darwin', 'freebsd', 'openbsd'],
-    archiveFilename: 'chromium-503a3e4-darwin.zip',
-    archiveChecksum: 'c1b530f99374e122c0bd7ba663867a95',
+    archiveFilename: 'chromium-68.0.3440.106-darwin.zip',
+    archiveChecksum: '68ffc0a821c98b539c6158ed82fa37fc',
     binaryRelativePath: 'headless_shell-darwin/headless_shell',
   }, {
     platforms: ['linux'],
-    archiveFilename: 'chromium-503a3e4-linux.zip',
-    archiveChecksum: '9486d8eff9fc4f94c899aa72f5e59520',
+    archiveFilename: 'chromium-68.0.3440.106-linux.zip',
+    archiveChecksum: '38db7736585dbf8e4d11dbf25d23bab2',
     binaryRelativePath: 'headless_shell-linux/headless_shell'
   }, {
     platforms: ['win32'],
-    archiveFilename: 'chromium-503a3e4-win32.zip',
-    archiveChecksum: 'a71ce5565791767492f6d0fb4fe5360d',
+    archiveFilename: 'chromium-68.0.3440.106-win32.zip',
+    archiveChecksum: '37416b13e074e80f5bbb9796a5fb6198',
     binaryRelativePath: 'headless_shell-win32\\headless_shell.exe'
   }]
 };
