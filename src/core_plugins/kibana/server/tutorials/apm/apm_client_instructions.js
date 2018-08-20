746dd32b8c9e959268dda50afdd21d1d7e80d9d2
Leanid Shutau <leanidshutau@gmail.com>
2018-08-20 18:14:14 +0300

    Wrap consts in kibana tutorials

--- src/core_plugins/kibana/server/tutorials/apm/apm_client_instructions.js
+++ src/core_plugins/kibana/server/tutorials/apm/apm_client_instructions.js
@@ -19,20 +19,20 @@
 
 /* eslint-disable max-len */
 
-export const NODE_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Install the APM agent',
-    textPre:
-      'Install the APM agent for Node.js as a dependency to your application.',
-    commands: ['npm install elastic-apm-node --save'],
-  },
-  {
-    title: 'Configure the agent',
-    textPre:
-      'Agents are libraries that run inside of your application process.' +
-      ' APM services are created programmatically based on the `serviceName`.' +
-      ' This agent supports Express, Koa, hapi, and custom Node.js.',
-    commands: `// Add this to the VERY top of the first file loaded in your app
+export function createNodeClientInstructions() {
+  return [
+    {
+      title: 'Install the APM agent',
+      textPre: 'Install the APM agent for Node.js as a dependency to your application.',
+      commands: ['npm install elastic-apm-node --save'],
+    },
+    {
+      title: 'Configure the agent',
+      textPre:
+        'Agents are libraries that run inside of your application process.' +
+        ' APM services are created programmatically based on the `serviceName`.' +
+        ' This agent supports Express, Koa, hapi, and custom Node.js.',
+      commands: `// Add this to the VERY top of the first file loaded in your app
 var apm = require('elastic-apm-node').start({curlyOpen}
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
   serviceName: '',
@@ -43,22 +43,24 @@ var apm = require('elastic-apm-node').start({curlyOpen}
   // Set custom APM Server URL (default: http://localhost:8200)
   serverUrl: ''
 {curlyClose})`.split('\n'),
-    textPost: `See [the documentation]({config.docs.base_url}guide/en/apm/agent/nodejs/1.x/index.html) for advanced usage, including how to use with [Babel/ES Modules]({config.docs.base_url}guide/en/apm/agent/nodejs/1.x/advanced-setup.html#es-modules).`,
-  },
-];
+      textPost: `See [the documentation]({config.docs.base_url}guide/en/apm/agent/nodejs/1.x/index.html) for advanced usage, including how to use with [Babel/ES Modules]({config.docs.base_url}guide/en/apm/agent/nodejs/1.x/advanced-setup.html#es-modules).`,
+    },
+  ];
+}
 
-export const DJANGO_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Install the APM agent',
-    textPre: 'Install the APM agent for Python as a dependency.',
-    commands: ['$ pip install elastic-apm'],
-  },
-  {
-    title: 'Configure the agent',
-    textPre:
-      'Agents are libraries that run inside of your application process.' +
-      ' APM services are created programmatically based on the `SERVICE_NAME`.',
-    commands: `# Add the agent to the installed apps
+export function createDjangoClientInstructions() {
+  return [
+    {
+      title: 'Install the APM agent',
+      textPre: 'Install the APM agent for Python as a dependency.',
+      commands: ['$ pip install elastic-apm'],
+    },
+    {
+      title: 'Configure the agent',
+      textPre:
+        'Agents are libraries that run inside of your application process.' +
+        ' APM services are created programmatically based on the `SERVICE_NAME`.',
+      commands: `# Add the agent to the installed apps
 INSTALLED_APPS = (
   'elasticapm.contrib.django',
   # ...
@@ -81,24 +83,26 @@ MIDDLEWARE = (
   'elasticapm.contrib.django.middleware.TracingMiddleware',
   #...
 )`.split('\n'),
-    textPost:
-      'See the [documentation]' +
-      '({config.docs.base_url}guide/en/apm/agent/python/2.x/django-support.html) for advanced usage.',
-  },
-];
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/python/2.x/django-support.html) for advanced usage.',
+    },
+  ];
+}
 
-export const FLASK_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Install the APM agent',
-    textPre: 'Install the APM agent for Python as a dependency.',
-    commands: ['$ pip install elastic-apm[flask]'],
-  },
-  {
-    title: 'Configure the agent',
-    textPre:
-      'Agents are libraries that run inside of your application process.' +
-      ' APM services are created programmatically based on the `SERVICE_NAME`.',
-    commands: `# initialize using environment variables
+export function createFlaskClientInstructions() {
+  return [
+    {
+      title: 'Install the APM agent',
+      textPre: 'Install the APM agent for Python as a dependency.',
+      commands: ['$ pip install elastic-apm[flask]'],
+    },
+    {
+      title: 'Configure the agent',
+      textPre:
+        'Agents are libraries that run inside of your application process.' +
+        ' APM services are created programmatically based on the `SERVICE_NAME`.',
+      commands: `# initialize using environment variables
 from elasticapm.contrib.flask import ElasticAPM
 app = Flask(__name__)
 apm = ElasticAPM(app)
@@ -118,23 +122,25 @@ app.config['ELASTIC_APM'] = {curlyOpen}
 {curlyClose}
 
 apm = ElasticAPM(app)`.split('\n'),
-    textPost:
-      'See the [documentation]' +
-      '({config.docs.base_url}guide/en/apm/agent/python/2.x/flask-support.html) for advanced usage.',
-  },
-];
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/python/2.x/flask-support.html) for advanced usage.',
+    },
+  ];
+}
 
-export const RAILS_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Install the APM agent',
-    textPre: 'Add the agent to your Gemfile.',
-    commands: [`gem 'elastic-apm'`],
-  },
-  {
-    title: 'Configure the agent',
-    textPre:
-      'APM is automatically started when your app boots. Configure the agent, by creating the config file `config/elastic_apm.yml`',
-    commands: `# config/elastic_apm.yml:
+export function createRailsClientInstructions() {
+  [
+    {
+      title: 'Install the APM agent',
+      textPre: 'Add the agent to your Gemfile.',
+      commands: [`gem 'elastic-apm'`],
+    },
+    {
+      title: 'Configure the agent',
+      textPre:
+        'APM is automatically started when your app boots. Configure the agent, by creating the config file `config/elastic_apm.yml`',
+      commands: `# config/elastic_apm.yml:
 
 # Set service name - allowed characters: a-z, A-Z, 0-9, -, _ and space
 # Defaults to the name of your Rails app
@@ -145,23 +151,25 @@ export const RAILS_CLIENT_INSTRUCTIONS = [
 
 # Set custom APM Server URL (default: http://localhost:8200)
 # server_url: 'http://localhost:8200'`.split('\n'),
-    textPost:
-      'See the [documentation]' +
-      '({config.docs.base_url}guide/en/apm/agent/ruby/1.x/index.html) for configuration options and advanced usage.\n\n',
-  },
-];
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/ruby/1.x/index.html) for configuration options and advanced usage.\n\n',
+    },
+  ];
+}
 
-export const RACK_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Install the APM agent',
-    textPre: 'Add the agent to your Gemfile.',
-    commands: [`gem 'elastic-apm'`],
-  },
-  {
-    title: 'Configure the agent',
-    textPre:
-      'For Rack or a compatible framework (e.g. Sinatra), include the middleware in your app and start the agent.',
-    commands: `# config.ru
+export function createRackClientInstructions() {
+  return [
+    {
+      title: 'Install the APM agent',
+      textPre: 'Add the agent to your Gemfile.',
+      commands: [`gem 'elastic-apm'`],
+    },
+    {
+      title: 'Configure the agent',
+      textPre:
+        'For Rack or a compatible framework (e.g. Sinatra), include the middleware in your app and start the agent.',
+      commands: `# config.ru
   require 'sinatra/base'
 
   class MySinatraApp < Sinatra::Base
@@ -178,11 +186,11 @@ export const RACK_CLIENT_INSTRUCTIONS = [
   run MySinatraApp
 
   at_exit {curlyOpen} ElasticAPM.stop {curlyClose}`.split('\n'),
-  },
-  {
-    title: 'Create config file',
-    textPre: 'Create a config file `config/elastic_apm.yml`:',
-    commands: `# config/elastic_apm.yml:
+    },
+    {
+      title: 'Create config file',
+      textPre: 'Create a config file `config/elastic_apm.yml`:',
+      commands: `# config/elastic_apm.yml:
 
 # Set service name - allowed characters: a-z, A-Z, 0-9, -, _ and space
 # Defaults to the name of your Rack app's class.
@@ -193,28 +201,29 @@ export const RACK_CLIENT_INSTRUCTIONS = [
 
 # Set custom APM Server URL (default: http://localhost:8200)
 # server_url: 'http://localhost:8200'`.split('\n'),
-    textPost:
-      'See the [documentation]' +
-      '({config.docs.base_url}guide/en/apm/agent/ruby/1.x/index.html) for configuration options and advanced usage.\n\n',
-  },
-];
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/ruby/1.x/index.html) for configuration options and advanced usage.\n\n',
+    },
+  ];
+}
 
-export const JS_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Enable Real User Monitoring support in the APM server',
-    textPre:
-      'Please refer to [the documentation]({config.docs.base_url}guide/en/apm/server/{config.docs.version}/rum.html).',
-  },
-  {
-    title: 'Install the APM agent',
-    textPre:
-      'Install the APM agent for JavaScript as a dependency to your application:',
-    commands: [`npm install elastic-apm-js-base --save`],
-  },
-  {
-    title: 'Configure the agent',
-    textPre: 'Agents are libraries that run inside of your application.',
-    commands: `import {curlyOpen} init as initApm {curlyClose} from 'elastic-apm-js-base'
+export function createJsClientInstructions() {
+  return [
+    {
+      title: 'Enable Real User Monitoring support in the APM server',
+      textPre:
+        'Please refer to [the documentation]({config.docs.base_url}guide/en/apm/server/{config.docs.version}/rum.html).',
+    },
+    {
+      title: 'Install the APM agent',
+      textPre: 'Install the APM agent for JavaScript as a dependency to your application:',
+      commands: [`npm install elastic-apm-js-base --save`],
+    },
+    {
+      title: 'Configure the agent',
+      textPre: 'Agents are libraries that run inside of your application.',
+      commands: `import {curlyOpen} init as initApm {curlyClose} from 'elastic-apm-js-base'
 var apm = initApm({curlyOpen}
 
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
@@ -226,25 +235,27 @@ var apm = initApm({curlyOpen}
   // Set service version (required for sourcemap feature)
   serviceVersion: ''
 {curlyClose})`.split('\n'),
-    textPost:
-      'See the [documentation]' +
-      '({config.docs.base_url}guide/en/apm/agent/js-base/current/index.html) for advanced usage.',
-  },
-];
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/js-base/current/index.html) for advanced usage.',
+    },
+  ];
+}
 
-export const GO_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Install the APM agent',
-    textPre: 'Install the APM agent packages for Go.',
-    commands: ['go get github.com/elastic/apm-agent-go'],
-  },
-  {
-    title: 'Configure the agent',
-    textPre:
-      'Agents are libraries that run inside of your application process.' +
-      ' APM services are created programmatically based on the executable ' +
-      ' file name, or the `ELASTIC_APM_SERVICE_NAME` environment variable.',
-    commands: `# Initialize using environment variables:
+export function createGoClientInstructions() {
+  return [
+    {
+      title: 'Install the APM agent',
+      textPre: 'Install the APM agent packages for Go.',
+      commands: ['go get github.com/elastic/apm-agent-go'],
+    },
+    {
+      title: 'Configure the agent',
+      textPre:
+        'Agents are libraries that run inside of your application process.' +
+        ' APM services are created programmatically based on the executable ' +
+        ' file name, or the `ELASTIC_APM_SERVICE_NAME` environment variable.',
+      commands: `# Initialize using environment variables:
 
 # Set the service name. Allowed characters: # a-z, A-Z, 0-9, -, _, and space.
 # If ELASTIC_APM_SERVICE_NAME is not specified, the executable name will be used.
@@ -256,16 +267,16 @@ export ELASTIC_APM_SERVER_URL=
 # Set if APM Server requires a token.
 export ELASTIC_APM_SECRET_TOKEN=
 `.split('\n'),
-    textPost:
-      'See the [documentation]' +
-      '({config.docs.base_url}guide/en/apm/agent/go/current/configuration.html) for advanced configuration.',
-  },
-  {
-    title: 'Instrument your application',
-    textPre:
-      'Instrument your Go application by using one of the provided instrumentation modules or ' +
-      'by using the tracer API directly.',
-    commands: `
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/go/current/configuration.html) for advanced configuration.',
+    },
+    {
+      title: 'Instrument your application',
+      textPre:
+        'Instrument your Go application by using one of the provided instrumentation modules or ' +
+        'by using the tracer API directly.',
+      commands: `
 import (
 	"net/http"
 
@@ -278,35 +289,40 @@ func main() {curlyOpen}
 	http.ListenAndServe(":8080", apmhttp.Wrap(mux))
 {curlyClose}
 `.split('\n'),
-    textPost:
-      'See the [documentation]' +
-      '({config.docs.base_url}guide/en/apm/agent/go/current/instrumenting-source.html) for a detailed ' +
-      'guide to instrumenting Go source code.\n\n' +
-      '**Warning: The Go agent is currently in Beta and not meant for production use.**',
-  },
-];
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/go/current/instrumenting-source.html) for a detailed ' +
+        'guide to instrumenting Go source code.\n\n' +
+        '**Warning: The Go agent is currently in Beta and not meant for production use.**',
+    },
+  ];
+}
 
-export const JAVA_CLIENT_INSTRUCTIONS = [
-  {
-    title: 'Download the APM agent',
-    textPre: 'Download the agent jar from [Maven Central](http://search.maven.org/#search%7Cga%7C1%7Ca%3Aelastic-apm-agent). ' +
-    'Do **not** add the agent as a dependency to your application.'
-  },
-  {
-    title: 'Start your application with the javaagent flag',
-    textPre: 'Add the `-javaagent` flag and configure the agent with system properties.\n' +
-    '\n' +
-    ' * Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)\n' +
-    ' * Set custom APM Server URL (default: http://localhost:8200)\n' +
-    ' * Set the base package of your application',
-    commands: `java -javaagent:/path/to/elastic-apm-agent-<version>.jar \\
+export function createJavaClientInstructions() {
+  return [
+    {
+      title: 'Download the APM agent',
+      textPre:
+        'Download the agent jar from [Maven Central](http://search.maven.org/#search%7Cga%7C1%7Ca%3Aelastic-apm-agent). ' +
+        'Do **not** add the agent as a dependency to your application.',
+    },
+    {
+      title: 'Start your application with the javaagent flag',
+      textPre:
+        'Add the `-javaagent` flag and configure the agent with system properties.\n' +
+        '\n' +
+        ' * Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)\n' +
+        ' * Set custom APM Server URL (default: http://localhost:8200)\n' +
+        ' * Set the base package of your application',
+      commands: `java -javaagent:/path/to/elastic-apm-agent-<version>.jar \\
      -Delastic.apm.service_name=my-application \\
-     -Delastic.apm.server_url=http://localhost:8200 \\ 
-     -Delastic.apm.application_packages=org.example \\ 
+     -Delastic.apm.server_url=http://localhost:8200 \\
+     -Delastic.apm.application_packages=org.example \\
      -jar my-application.jar`.split('\n'),
-    textPost:
-    'See the [documentation]' +
-    '({config.docs.base_url}guide/en/apm/agent/java/current/index.html) for configuration options and advanced usage.\n\n' +
-    '**Warning: The Java agent is currently in Beta and not meant for production use.**',
-  },
-];
+      textPost:
+        'See the [documentation]' +
+        '({config.docs.base_url}guide/en/apm/agent/java/current/index.html) for configuration options and advanced usage.\n\n' +
+        '**Warning: The Java agent is currently in Beta and not meant for production use.**',
+    },
+  ];
+}
--- src/core_plugins/kibana/server/tutorials/apm/apm_server_instructions.js
+++ src/core_plugins/kibana/server/tutorials/apm/apm_server_instructions.js
@@ -17,84 +17,103 @@
  * under the License.
  */
 
-export const EDIT_CONFIG = {
-  title: 'Edit the configuration',
-  textPre:
-    `If you're using an X-Pack secured version of Elastic Stack, you must specify` +
-    ' credentials in the `apm-server.yml` config file.',
-  commands: [
-    'output.elasticsearch:',
-    '    hosts: ["<es_url>"]',
-    '    username: <username>',
-    '    password: <password>',
-  ],
-};
-
-const START_SERVER = {
-  title: 'Start APM Server',
-  textPre:
-    'The server processes and stores application performance metrics in Elasticsearch.',
-};
-
-export const START_SERVER_UNIX = {
-  title: START_SERVER.title,
-  textPre: START_SERVER.textPre,
-  commands: ['./apm-server -e'],
-};
-
-const DOWNLOAD_SERVER_TITLE = 'Download and unpack APM Server';
-
-export const DOWNLOAD_SERVER_OSX = {
-  title: DOWNLOAD_SERVER_TITLE,
-  commands: [
-    'curl -L -O https://artifacts.elastic.co/downloads/apm-server/apm-server-{config.kibana.version}-darwin-x86_64.tar.gz',
-    'tar xzvf apm-server-{config.kibana.version}-darwin-x86_64.tar.gz',
-    'cd apm-server-{config.kibana.version}-darwin-x86_64/',
-  ],
-};
-
-export const DOWNLOAD_SERVER_DEB = {
-  title: DOWNLOAD_SERVER_TITLE,
-  commands: [
-    'curl -L -O https://artifacts.elastic.co/downloads/apm-server/apm-server-{config.kibana.version}-amd64.deb',
-    'sudo dpkg -i apm-server-{config.kibana.version}-amd64.deb',
-  ],
-  textPost:
-    'Looking for the 32-bit packages? See the [Download page]({config.docs.base_url}downloads/apm/apm-server).',
-};
-
-export const DOWNLOAD_SERVER_RPM = {
-  title: DOWNLOAD_SERVER_TITLE,
-  commands: [
-    'curl -L -O https://artifacts.elastic.co/downloads/apm-server/apm-server-{config.kibana.version}-x86_64.rpm',
-    'sudo rpm -vi apm-server-{config.kibana.version}-x86_64.rpm',
-  ],
-  textPost:
-    'Looking for the 32-bit packages? See the [Download page]({config.docs.base_url}downloads/apm/apm-server).',
-};
-
-export const WINDOWS_SERVER_INSTRUCTIONS = [
-  {
-    title: DOWNLOAD_SERVER_TITLE,
+export function createEditConfig() {
+  return {
+    title: 'Edit the configuration',
     textPre:
-      '1. Download the APM Server Windows zip file from the [Download page](https://www.elastic.co/downloads/apm/apm-server).\n' +
-      '2. Extract the contents of the zip file into `C:\\Program Files`.\n' +
-      '3. Rename the `apm-server-{config.kibana.version}-windows` directory to `APM-Server`.\n' +
-      '4. Open a PowerShell prompt as an Administrator (right-click the PowerShell icon and select' +
-      ' **Run As Administrator**). If you are running Windows XP, you might need to download and install PowerShell.\n' +
-      '5. From the PowerShell prompt, run the following commands to install APM Server as a Windows service:',
+      `If you're using an X-Pack secured version of Elastic Stack, you must specify` +
+      ' credentials in the `apm-server.yml` config file.',
     commands: [
-      `PS > cd 'C:\\Program Files\\APM-Server'`,
-      `PS C:\\Program Files\\APM-Server> .\\install-service-apm-server.ps1`,
+      'output.elasticsearch:',
+      '    hosts: ["<es_url>"]',
+      '    username: <username>',
+      '    password: <password>',
     ],
-    textPost:
-      'Note: If script execution is disabled on your system, you need to set the execution policy for the current session' +
-      ' to allow the script to run. For example: `PowerShell.exe -ExecutionPolicy UnRestricted -File .\\install-service-apm-server.ps1`.',
-  },
-  EDIT_CONFIG,
-  {
+  };
+}
+
+function createStartServer() {
+  return {
+    title: 'Start APM Server',
+    textPre: 'The server processes and stores application performance metrics in Elasticsearch.',
+  };
+}
+
+export function createStartServerUnix() {
+  const START_SERVER = createStartServer();
+
+  return {
     title: START_SERVER.title,
     textPre: START_SERVER.textPre,
-    commands: ['apm-server.exe -e'],
-  },
-];
+    commands: ['./apm-server -e'],
+  };
+}
+
+function createDownloadServerTitle() {
+  return 'Download and unpack APM Server';
+}
+
+export function createDownloadServerOsx() {
+  return {
+    title: createDownloadServerTitle(),
+    commands: [
+      'curl -L -O https://artifacts.elastic.co/downloads/apm-server/apm-server-{config.kibana.version}-darwin-x86_64.tar.gz',
+      'tar xzvf apm-server-{config.kibana.version}-darwin-x86_64.tar.gz',
+      'cd apm-server-{config.kibana.version}-darwin-x86_64/',
+    ],
+  };
+}
+
+export function createDownloadServerDeb() {
+  return {
+    title: createDownloadServerTitle(),
+    commands: [
+      'curl -L -O https://artifacts.elastic.co/downloads/apm-server/apm-server-{config.kibana.version}-amd64.deb',
+      'sudo dpkg -i apm-server-{config.kibana.version}-amd64.deb',
+    ],
+    textPost:
+      'Looking for the 32-bit packages? See the [Download page]({config.docs.base_url}downloads/apm/apm-server).',
+  };
+}
+
+export function createDownloadServerRpm() {
+  return {
+    title: createDownloadServerTitle(),
+    commands: [
+      'curl -L -O https://artifacts.elastic.co/downloads/apm-server/apm-server-{config.kibana.version}-x86_64.rpm',
+      'sudo rpm -vi apm-server-{config.kibana.version}-x86_64.rpm',
+    ],
+    textPost:
+      'Looking for the 32-bit packages? See the [Download page]({config.docs.base_url}downloads/apm/apm-server).',
+  };
+}
+
+export function createWindowsServerInstructions() {
+  const START_SERVER = createStartServer();
+
+  return [
+    {
+      title: createDownloadServerTitle(),
+      textPre:
+        '1. Download the APM Server Windows zip file from the [Download page](https://www.elastic.co/downloads/apm/apm-server).\n' +
+        '2. Extract the contents of the zip file into `C:\\Program Files`.\n' +
+        '3. Rename the `apm-server-{config.kibana.version}-windows` directory to `APM-Server`.\n' +
+        '4. Open a PowerShell prompt as an Administrator (right-click the PowerShell icon and select' +
+        ' **Run As Administrator**). If you are running Windows XP, you might need to download and install PowerShell.\n' +
+        '5. From the PowerShell prompt, run the following commands to install APM Server as a Windows service:',
+      commands: [
+        `PS > cd 'C:\\Program Files\\APM-Server'`,
+        `PS C:\\Program Files\\APM-Server> .\\install-service-apm-server.ps1`,
+      ],
+      textPost:
+        'Note: If script execution is disabled on your system, you need to set the execution policy for the current session' +
+        ' to allow the script to run. For example: `PowerShell.exe -ExecutionPolicy UnRestricted -File .\\install-service-apm-server.ps1`.',
+    },
+    createEditConfig(),
+    {
+      title: START_SERVER.title,
+      textPre: START_SERVER.textPre,
+      commands: ['apm-server.exe -e'],
+    },
+  ];
+}
