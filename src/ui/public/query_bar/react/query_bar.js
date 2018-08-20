82490eb6c52a9795a21a72bab670ac9d9d1abe55
Matthew Bargar <mbargar@gmail.com>
2018-08-20 15:05:06 -0400

    Get state flow working how I want it

--- src/ui/public/query_bar/react/query_bar.js
+++ src/ui/public/query_bar/react/query_bar.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
   EuiFlexGroup,
@@ -25,18 +25,60 @@ import {
   EuiFieldSearch,
 } from '@elastic/eui';
 
-export function QueryBar({ query, onSubmit }) {
-  return (
-    <EuiFlexGroup>
-      <EuiFlexItem>
-        {/*Need an onChange to update state, but should this be a stateful component or should it call a callback */}
-        <EuiFieldSearch
-          placeholder="Search..."
-          value={query.query}
-          onSearch={onSubmit}
-          fullWidth
-        />
-      </EuiFlexItem>
-    </EuiFlexGroup>
-  );
+export class QueryBar extends Component {
+
+  /*
+   Keep the "draft" value in local state until the user actually submits the query. There are a couple advantages:
+
+    1. Each app doesn't have to maintain its own "draft" value if it wants to put off updating the query in app state
+    until the user manually submits their changes. Most apps have watches on the query value in app state so we don't
+    want to trigger those on every keypress. Also, some apps (e.g. dashboard) already juggle multiple query values,
+    each with slightly different semantics and I'd rather not add yet another variable to the mix.
+
+    2. Changes to the local component state won't trigger an Angular digest cycle. Triggering digest cycles on every
+    keypress has been a major source of performance issues for us in previous implementations of the query bar.
+    See https://github.com/elastic/kibana/issues/14086
+  */
+  state = {
+    query: this.props.query,
+    language: this.props.language,
+  };
+
+  onChange = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.query !== this.props.query) {
+      this.setState({
+        query: nextProps.query,
+        language: nextProps.language,
+      });
+    }
+    else if (nextProps.language !== nextProps.language) {
+      this.setState({
+        query: '',
+        language: nextProps.language,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          {/*Need an onChange to update state, but should this be a stateful component or should it call a callback */}
+          <EuiFieldSearch
+            placeholder="Search..."
+            value={this.state.query}
+            onChange={this.onChange}
+            onSearch={this.props.onSubmit}
+            fullWidth
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  }
 }
