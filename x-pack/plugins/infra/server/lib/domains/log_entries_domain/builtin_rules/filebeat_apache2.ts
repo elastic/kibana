f32059284cb11220eba4d78d6b9229725335922f
Felix Stürmer <stuermer@weltenwort.de>
2018-08-21 15:18:03 +0200

    Split log formatting rules by module, add new ones

---
+++ x-pack/plugins/infra/server/lib/domains/log_entries_domain/builtin_rules/filebeat_apache2.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const filebeatApache2Rules = [
+  {
+    when: {
+      exists: ['apache2.access'],
+    },
+    format: [
+      {
+        constant: 'apache2',
+      },
+      {
+        constant: ' ',
+      },
+      {
+        field: 'apache2.access.remote_ip',
+      },
+      {
+        constant: ' ',
+      },
+      {
+        field: 'apache2.access.user_name',
+      },
+      {
+        constant: ' "',
+      },
+      {
+        field: 'apache2.access.method',
+      },
+      {
+        constant: ' ',
+      },
+      {
+        field: 'apache2.access.url',
+      },
+      {
+        constant: ' HTTP/',
+      },
+      {
+        field: 'apache2.access.http_version',
+      },
+      {
+        constant: '" ',
+      },
+      {
+        field: 'apache2.access.response_code',
+      },
+      {
+        constant: ' ',
+      },
+      {
+        field: 'apache2.access.body_sent.bytes',
+      },
+    ],
+  },
+];
