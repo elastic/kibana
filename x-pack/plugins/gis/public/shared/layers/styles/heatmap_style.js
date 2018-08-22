5f39acdecb80cfc74c20f34d3350a6ec42b83c49
Thomas Neirynck <thomas@elastic.co>
2018-08-22 15:46:37 -0400

    add file-scaffolding for heatmap layer implementation

---
+++ x-pack/plugins/gis/public/shared/layers/styles/heatmap_style.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+
+export class HeatmapStyle {
+
+  static type = 'HEATMAP';
+
+  constructor(styleDescriptor) {
+    this._descriptor = styleDescriptor;
+  }
+
+  static canEdit(styleInstance) {
+    return styleInstance.constructor === HeatmapStyle;
+  }
+
+  static createDescriptor() {
+    return {
+      type: HeatmapStyle.type
+    };
+  }
+
+  static getDisplayName() {
+    return 'Heatmap Settings';
+  }
+
+  static renderEditor({}) {
+    return (<div>Here be heatmap style editor.</div>);
+  }
+}
