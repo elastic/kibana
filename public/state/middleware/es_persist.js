0ee6eac382208b0b7eed523fd25def5b7be41695
Joe Fleming <w33ble@users.noreply.github.com>
2018-08-24 10:49:50 -0500

    Chore: persist action cleanup (#1016)
    * chore: skip persist on resetAssets
    
    * chore: skip persist on other noisy actions
    
    transient actions, resolved args, ajax in flight... none of these affect the persisted state, so there's no point even testing them

--- public/state/middleware/es_persist.js
+++ public/state/middleware/es_persist.js
@@ -2,7 +2,9 @@ import { isEqual } from 'lodash';
 import { getWorkpad, getWorkpadPersisted } from '../selectors/workpad';
 import { getAssetIds } from '../selectors/assets';
 import { setWorkpad } from '../actions/workpad';
-import { setAssets } from '../actions/assets';
+import { setAssets, resetAssets } from '../actions/assets';
+import * as transientActions from '../actions/transient';
+import * as resolvedArgsActions from '../actions/resolved_args';
 import { update } from '../../lib/workpad_service';
 import { notify } from '../../lib/notify';
 
@@ -16,10 +18,17 @@ const assetsChanged = (before, after) => {
   return !isEqual(assets, getAssetIds(after));
 };
 
-const skippedActions = [setWorkpad.toString(), setAssets.toString()];
+// these are the actions we don't want to trigger a persist call
+const skippedActions = [
+  setWorkpad, // used for loading and creating workpads
+  setAssets, // used when loading assets
+  resetAssets, // used when creating new workpads
+  ...Object.values(resolvedArgsActions), // no resolved args affect persisted values
+  ...Object.values(transientActions), // no transient actions cause persisted state changes
+].map(a => a.toString());
 
 export const esPersistMiddleware = ({ getState }) => next => action => {
-  // if the workpad was just loaded, don't save it
+  // if the action is in the skipped list, do not persist
   if (skippedActions.indexOf(action.type) >= 0) return next(action);
 
   // capture state before and after the action
