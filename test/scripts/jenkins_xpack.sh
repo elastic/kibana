941fdb612777c2598686f22d74f42c09bdbc189d
Christopher Davies <github@christophilus.com>
2018-08-27 11:16:20 -0400

    Disable x-pack tests except for Chromium

--- test/scripts/jenkins_xpack.sh
+++ test/scripts/jenkins_xpack.sh
@@ -12,18 +12,18 @@ export XPACK_DIR="$(cd "$(dirname "$0")/../../x-pack"; pwd)"
 echo "-> XPACK_DIR ${XPACK_DIR}"
 
 
-echo " -> Running mocha tests"
-cd "$XPACK_DIR"
-xvfb-run yarn test
-echo ""
-echo ""
+# echo " -> Running mocha tests"
+# cd "$XPACK_DIR"
+# xvfb-run yarn test
+# echo ""
+# echo ""
 
 
-echo " -> Running jest tests"
-cd "$XPACK_DIR"
-node scripts/jest --ci --no-cache --verbose
-echo ""
-echo ""
+# echo " -> Running jest tests"
+# cd "$XPACK_DIR"
+# node scripts/jest --ci --no-cache --verbose
+# echo ""
+# echo ""
 
 
 echo " -> building and extracting default Kibana distributable for use in functional tests"
