0536031db11bb6497c3de8b4c78ec34cb9b63999
spalger <email@spalger.com>
2018-08-27 14:49:08 -0700

    test async/sync withProcRunner functions

--- packages/kbn-dev-utils/src/proc_runner/with_proc_runner.test.js
+++ packages/kbn-dev-utils/src/proc_runner/with_proc_runner.test.js
@@ -22,30 +22,58 @@ import { withProcRunner } from './with_proc_runner';
 import { ProcRunner } from './proc_runner';
 
 it('passes proc runner to a function', async () => {
-  await withProcRunner(new ToolingLog(), async proc => {
+  await withProcRunner(new ToolingLog(), proc => {
     expect(proc).toBeInstanceOf(ProcRunner);
   });
 });
 
-it('calls procRunner.teardown() if function resolves', async () => {
+it('calls procRunner.teardown() if function returns synchronously', async () => {
   let teardownSpy;
-  await withProcRunner(new ToolingLog(), async proc => {
+  await withProcRunner(new ToolingLog(), proc => {
     teardownSpy = jest.spyOn(proc, 'teardown');
   });
 
   expect(teardownSpy).toHaveBeenCalled();
 });
 
-it('calls procRunner.teardown() if function rejects, and rethrows the error', async () => {
+it('calls procRunner.teardown() if function throw synchronous error, and rejects with the error', async () => {
   const error = new Error('foo');
   let teardownSpy;
 
   await expect(
-    withProcRunner(new ToolingLog(), async proc => {
+    withProcRunner(new ToolingLog(), proc => {
       teardownSpy = jest.spyOn(proc, 'teardown');
       throw error;
     })
   ).rejects.toThrowError(error);
 
+  expect(teardownSpy).toHaveBeenCalled();
+});
+
+it('waits for promise to resolve before tearing down proc', async () => {
+  let teardownSpy;
+
+  await withProcRunner(new ToolingLog(), async proc => {
+    await new Promise(resolve => setTimeout(resolve, 500));
+    teardownSpy = jest.spyOn(proc, 'teardown');
+  });
+
+  expect(teardownSpy).not.toBe(undefined);
+  expect(teardownSpy).toHaveBeenCalled();
+});
+
+it('waits for promise to reject before tearing down proc and rejecting with the error', async () => {
+  const error = new Error('foo');
+  let teardownSpy;
+
+  await expect(
+    withProcRunner(new ToolingLog(), async proc => {
+      await new Promise(resolve => setTimeout(resolve, 500));
+      teardownSpy = jest.spyOn(proc, 'teardown');
+      throw error;
+    })
+  ).rejects.toThrowError(error);
+
+  expect(teardownSpy).not.toBe(undefined);
   expect(teardownSpy).toHaveBeenCalled();
 });
