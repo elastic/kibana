93faead83605022a5e772f6e8b10bf5981b76189
Timothy Sullivan <tsullivan@elastic.co>
2018-08-27 13:02:18 -0700

    Saved Objects Client handle RequestEntityTooLarge error from Elasticsearch

--- src/server/saved_objects/service/lib/decorate_es_error.js
+++ src/server/saved_objects/service/lib/decorate_es_error.js
@@ -28,6 +28,7 @@ const {
   Conflict,
   401: NotAuthorized,
   403: Forbidden,
+  413: RequestEntityTooLarge,
   NotFound,
   BadRequest
 } = elasticsearch.errors;
@@ -36,6 +37,7 @@ import {
   decorateBadRequestError,
   decorateNotAuthorizedError,
   decorateForbiddenError,
+  decorateRequestEntityTooLargeError,
   createGenericNotFoundError,
   decorateConflictError,
   decorateEsUnavailableError,
@@ -46,6 +48,7 @@ export function decorateEsError(error) {
   if (!(error instanceof Error)) {
     throw new Error('Expected an instance of Error');
   }
+  console.log({ errorStatus: error.status, errorCode: error.statusCode });
 
   const { reason } = get(error, 'body.error', {});
   if (
@@ -67,6 +70,10 @@ export function decorateEsError(error) {
 
   if (error instanceof Forbidden) {
     return decorateForbiddenError(error, reason);
+  }
+
+  if (error instanceof RequestEntityTooLarge) {
+    return decorateRequestEntityTooLargeError(error, reason);
   }
 
   if (error instanceof NotFound) {
