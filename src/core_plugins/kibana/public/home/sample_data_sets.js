63ae8a38d14217daf7f82178e705d0ac8ea100a2
Nathan Reese <reese.nathan@gmail.com>
2018-08-27 11:51:36 -0600

    Refactor Sample Data Home section which also fixes flaky sample data functional test (#21655)
    * add retry to sample data unstall test
    
    * fix flaky sample data test
    
    * remove retry around expect
    
    * simplify solution
    
    * remove onRequestComplete and just track status in state
    
    * move all state into new SampleDataSetCards
    
    * track processing status in seperate object than sampleDataSets
    
    * display toast notifications from SampleDataSetCards component
    
    * remove checks for toast notifications, just causing flakyness
    
    * only load sample data set status on initial component load
    
    * set processing to false when there is an install failure

--- src/core_plugins/kibana/public/home/sample_data_sets.js
+++ src/core_plugins/kibana/public/home/sample_data_sets.js
@@ -17,57 +17,16 @@
  * under the License.
  */
 
-import chrome from 'ui/chrome';
-import { toastNotifications } from 'ui/notify';
+import { kfetch } from 'ui/kfetch';
 
-const sampleDataUrl = chrome.addBasePath('/api/sample_data');
-const headers = new Headers({
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-  'kbn-xsrf': 'kibana',
-});
+const sampleDataUrl = '/api/sample_data';
 
 export async function listSampleDataSets() {
-  try {
-    const response = await fetch(sampleDataUrl, {
-      method: 'get',
-      credentials: 'include',
-      headers: headers,
-    });
-
-    if (response.status >= 300) {
-      throw new Error(`Request failed with status code: ${response.status}`);
-    }
-
-    return await response.json();
-  } catch (err) {
-    toastNotifications.addDanger({
-      title: `Unable to load sample data sets list`,
-      text: `${err.message}`,
-    });
-    return [];
-  }
+  return await kfetch({ method: 'GET', pathname: sampleDataUrl });
 }
 
-export async function installSampleDataSet(id, name, defaultIndex, getConfig, setConfig, clearIndexPatternsCache) {
-  try {
-    const response = await fetch(`${sampleDataUrl}/${id}`, {
-      method: 'post',
-      credentials: 'include',
-      headers: headers,
-    });
-
-    if (response.status >= 300) {
-      const body = await response.text();
-      throw new Error(`Request failed with status code: ${response.status}, message: ${body}`);
-    }
-  } catch (err) {
-    toastNotifications.addDanger({
-      title: `Unable to install sample data set: ${name}`,
-      text: `${err.message}`,
-    });
-    return;
-  }
+export async function installSampleDataSet(id, defaultIndex, getConfig, setConfig, clearIndexPatternsCache) {
+  await kfetch({ method: 'POST', pathname: `${sampleDataUrl}/${id}` });
 
   const existingDefaultIndex = await getConfig('defaultIndex');
   if (existingDefaultIndex === null) {
@@ -75,31 +34,10 @@ export async function installSampleDataSet(id, name, defaultIndex, getConfig, se
   }
 
   clearIndexPatternsCache();
-
-  toastNotifications.addSuccess({
-    title: `${name} installed`,
-    ['data-test-subj']: 'sampleDataSetInstallToast'
-  });
 }
 
-export async function uninstallSampleDataSet(id, name, defaultIndex, getConfig, setConfig, clearIndexPatternsCache) {
-  try {
-    const response = await fetch(`${sampleDataUrl}/${id}`, {
-      method: 'delete',
-      credentials: 'include',
-      headers: headers,
-    });
-    if (response.status >= 300) {
-      const body = await response.text();
-      throw new Error(`Request failed with status code: ${response.status}, message: ${body}`);
-    }
-  } catch (err) {
-    toastNotifications.addDanger({
-      title: `Unable to uninstall sample data set`,
-      text: `${err.message}`,
-    });
-    return;
-  }
+export async function uninstallSampleDataSet(id, defaultIndex, getConfig, setConfig, clearIndexPatternsCache) {
+  await kfetch({ method: 'DELETE', pathname: `${sampleDataUrl}/${id}` });
 
   const existingDefaultIndex = await getConfig('defaultIndex');
   if (existingDefaultIndex && existingDefaultIndex === defaultIndex) {
@@ -107,9 +45,4 @@ export async function uninstallSampleDataSet(id, name, defaultIndex, getConfig, 
   }
 
   clearIndexPatternsCache();
-
-  toastNotifications.addSuccess({
-    title: `${name} uninstalled`,
-    ['data-test-subj']: 'sampleDataSetUninstallToast'
-  });
 }
