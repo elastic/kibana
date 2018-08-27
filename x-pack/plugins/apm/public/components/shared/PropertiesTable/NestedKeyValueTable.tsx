51c5c56f7b46a7b0feb2b130d702bbf64877b281
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2018-08-27 09:17:28 -0400

    Refactors properties table, implements typescript

---
+++ x-pack/plugins/apm/public/components/shared/PropertiesTable/NestedKeyValueTable.tsx
@@ -0,0 +1,132 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import styled from 'styled-components';
+import {
+  colors,
+  fontFamilyCode,
+  fontSizes,
+  units
+} from '../../../style/variables';
+
+interface StringMap<T> {
+  [key: string]: T;
+}
+
+type KeySorter = (data: StringMap<any>, parentKey?: string) => string[];
+
+const Table = styled.table`
+  font-family: ${fontFamilyCode};
+  font-size: ${fontSizes.small};
+  width: 100%;
+`;
+
+const Row = styled.tr`
+  border-bottom: 1px solid ${colors.gray4};
+  &:last-child {
+    border: 0;
+  }
+`;
+
+const Cell = styled.td`
+  vertical-align: top;
+  padding: ${units.half}px 0;
+
+  ${Row}:first-child> & {
+    padding-top: 0;
+  }
+
+  ${Row}:last-child> & {
+    padding-bottom: 0;
+  }
+
+  &:first-child {
+    width: 300px;
+    font-weight: bold;
+  }
+`;
+
+const EmptyValue = styled.span`
+  color: ${colors.gray3};
+`;
+
+function formatValue(value: any): JSX.Element {
+  if (_.isObject(value)) {
+    return <pre>{JSON.stringify(value, null, 4)}</pre>;
+  } else if (_.isBoolean(value) || _.isNumber(value)) {
+    return <React.Fragment>{String(value)}</React.Fragment>;
+  } else if (!value) {
+    return <EmptyValue>N/A</EmptyValue>;
+  }
+
+  return value;
+}
+
+function formatKey(key: string, value: any): string | JSX.Element {
+  if (value == null) {
+    return <EmptyValue>{key}</EmptyValue>;
+  }
+
+  return key;
+}
+
+export function NestedValue({
+  parentKey,
+  value,
+  depth,
+  keySorter
+}: {
+  parentKey: string;
+  value: any;
+  depth: number;
+  keySorter: KeySorter;
+}): JSX.Element {
+  if (depth > 0 && _.isObject(value)) {
+    return (
+      <NestedKeyValueTable
+        data={value}
+        parentKey={parentKey}
+        keySorter={keySorter}
+        depth={depth - 1}
+      />
+    );
+  }
+
+  return formatValue(value);
+}
+
+export function NestedKeyValueTable({
+  data,
+  parentKey,
+  keySorter = Object.keys,
+  depth = 0
+}: {
+  data: StringMap<any>;
+  parentKey: string;
+  keySorter?: KeySorter;
+  depth?: number;
+}): JSX.Element {
+  return (
+    <Table>
+      <tbody>
+        {keySorter(data, parentKey).map((key: string) => (
+          <Row key={key}>
+            <Cell>{formatKey(key, data[key])}</Cell>
+            <Cell>
+              <NestedValue
+                parentKey={key}
+                value={data[key]}
+                keySorter={keySorter}
+                depth={depth}
+              />
+            </Cell>
+          </Row>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
--- x-pack/plugins/apm/public/components/shared/PropertiesTable/index.js
+++ x-pack/plugins/apm/public/components/shared/PropertiesTable/index.js
@@ -7,54 +7,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import _ from 'lodash';
-import STATIC_PROPS from './staticProperties.json';
-import {
-  units,
-  colors,
-  px,
-  fontFamilyCode,
-  fontSizes,
-  unit,
-  fontSize
-} from '../../../style/variables';
+import PROP_CONFIG, { indexedPropertyConfig } from './propertyConfig';
+import { colors, units, px, unit, fontSize } from '../../../style/variables';
 import { EuiIcon } from '@elastic/eui';
 
 import { getFeatureDocs } from '../../../utils/documentation';
 import { ExternalLink } from '../../../utils/url';
+import { NestedKeyValueTable } from './NestedKeyValueTable';
 
 const TableContainer = styled.div`
   padding-bottom: ${px(units.double)};
-`;
-
-const Table = styled.table`
-  font-family: ${fontFamilyCode};
-  font-size: ${fontSizes.small};
-  width: 100%;
-`;
-
-const Row = styled.tr`
-  border-bottom: 1px solid ${colors.gray4};
-  &:last-child {
-    border: 0;
-  }
-`;
-
-const Cell = styled.td`
-  vertical-align: top;
-  padding: ${units.half}px 0;
-
-  ${Row}:first-child> & {
-    padding-top: 0;
-  }
-
-  ${Row}:last-child> & {
-    padding-bottom: 0;
-  }
-
-  &:first-child {
-    width: 300px;
-    font-weight: bold;
-  }
 `;
 
 const TableInfo = styled.div`
@@ -65,75 +27,10 @@ const TableInfo = styled.div`
   line-height: 1.5;
 `;
 
-const EmptyValue = styled.span`
-  color: ${colors.gray3};
-`;
-
-function getSortedProps(propData, levelTwoKey, level) {
-  if (level === 2) {
-    return getLevelTwoProps(propData, levelTwoKey);
-  }
-
-  return _.sortBy(_.map(propData, (value, key) => ({ value, key })), 'key');
-}
-
-function formatValue(value) {
-  if (_.isObject(value)) {
-    return <pre>{JSON.stringify(value, null, 4)}</pre>;
-  } else if (_.isBoolean(value) || _.isNumber(value)) {
-    return String(value);
-  } else if (!value) {
-    return <EmptyValue>N/A</EmptyValue>;
-  }
-
-  return value;
-}
-
-function formatKey(key, value) {
-  if (value == null) {
-    return <EmptyValue>{key}</EmptyValue>;
-  }
-
-  return key;
-}
-
 export function getLevelOneProps(dynamicProps) {
-  return STATIC_PROPS.filter(
+  return PROP_CONFIG.filter(
     ({ key, required }) => required || dynamicProps.includes(key)
   ).map(({ key }) => key);
-}
-
-function getLevelTwoProps(dynamicProps, currentKey) {
-  const staticProps = _.get(
-    _.find(STATIC_PROPS, { key: currentKey }),
-    'children'
-  );
-  const dynamicPropsSorted = Object.keys(dynamicProps).sort();
-  return _.uniq([...staticProps, ...dynamicPropsSorted]).map(key => ({
-    key,
-    value: dynamicProps[key]
-  }));
-}
-
-function recursiveSort(propData, levelTwoKey, level) {
-  return (
-    <Table>
-      <tbody>
-        {getSortedProps(propData, levelTwoKey, level).map(({ key, value }) => {
-          return (
-            <Row key={key}>
-              <Cell>{formatKey(key, value)}</Cell>
-              <Cell>
-                {level < 3 && _.isObject(value)
-                  ? recursiveSort(value, levelTwoKey, level + 1)
-                  : formatValue(value)}
-              </Cell>
-            </Row>
-          );
-        })}
-      </tbody>
-    </Table>
-  );
 }
 
 function AgentFeatureTipMessage({ featureName, agentName }) {
@@ -156,6 +53,17 @@ function AgentFeatureTipMessage({ featureName, agentName }) {
   );
 }
 
+function sortKeysByConfig(object, currentKey) {
+  const presorted = _.get(
+    indexedPropertyConfig,
+    `${currentKey}.presortedKeys`,
+    []
+  );
+  return _.uniq([...presorted, ...Object.keys(object).sort()]);
+}
+
+// TODO: if propData defaults to an empty object, the if (!propData)
+// will never be truthy unless propData is explicitly null or false...
 export function PropertiesTable({ propData = {}, propKey, agentName }) {
   if (!propData) {
     return (
@@ -169,7 +77,12 @@ export function PropertiesTable({ propData = {}, propKey, agentName }) {
 
   return (
     <TableContainer>
-      {recursiveSort(propData, propKey, 2, agentName)}
+      <NestedKeyValueTable
+        data={propData}
+        parentKey={propKey}
+        keySorter={sortKeysByConfig}
+        depth={1}
+      />
       <AgentFeatureTipMessage
         featureName={`context-${propKey}`}
         agentName={agentName}
