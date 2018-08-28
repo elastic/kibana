8979f2eae66e7dfd273ef6e0656df53e8cca81c5
Brian <gaddisb@gmail.com>
2018-08-28 18:07:45 -0400

    Name Change
    Name Change

--- x-pack/plugins/reporting/export_types/printable_pdf/server/lib/layouts/index.ts
+++ x-pack/plugins/reporting/export_types/printable_pdf/server/lib/layouts/index.ts
@@ -4,6 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { createlayout } from './layout_factory';
+export { createLayout } from './layout_factory';
 export { PrintLayout } from './print_layout';
 export { PreserveLayout } from './preserve_layout';
