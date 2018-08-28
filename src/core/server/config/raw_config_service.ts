d21d69395d1845fdfe2b8b1b988d0b43782223fc
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-08-28 11:00:35 +0200

    Use `ReplaySubject` instead of `BehaviorSubject` in `RawConfigService`.

--- src/core/server/config/raw_config_service.ts
+++ src/core/server/config/raw_config_service.ts
@@ -18,29 +18,21 @@
  */
 
 import { cloneDeep, isEqual, isPlainObject } from 'lodash';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { distinctUntilChanged, filter, map } from 'rxjs/operators';
+import { Observable, ReplaySubject } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 import typeDetect from 'type-detect';
 
 import { Config } from './config';
 import { ObjectToConfigAdapter } from './object_to_config_adapter';
 import { getConfigFromFiles } from './read_config';
 
-// Used to indicate that no config has been received yet
-const notRead = Symbol('config not yet read');
-
 export class RawConfigService {
   /**
-   * The stream of configs read from the config file. Will be the symbol
-   * `notRead` before the config is initially read, and after that it can
-   * potentially be `null` for an empty yaml file.
+   * The stream of configs read from the config file.
    *
    * This is the _raw_ config before any overrides are applied.
-   *
-   * As we have a notion of a _current_ config we rely on a BehaviorSubject so
-   * every new subscription will immediately receive the current config.
    */
-  private readonly rawConfigFromFile$ = new BehaviorSubject<any>(notRead);
+  private readonly rawConfigFromFile$ = new ReplaySubject<number>(1);
 
   private readonly config$: Observable<Config>;
 
@@ -50,7 +42,6 @@ export class RawConfigService {
       new ObjectToConfigAdapter(rawConfig)
   ) {
     this.config$ = this.rawConfigFromFile$.pipe(
-      filter(rawConfig => rawConfig !== notRead),
       // We only want to update the config if there are changes to it.
       distinctUntilChanged(isEqual),
       map(rawConfig => {
