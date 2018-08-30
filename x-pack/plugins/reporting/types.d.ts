4464fb2d550e5a1a167995ef3b1eebba073cb300
Brian <gaddisb@gmail.com>
2018-08-30 12:00:51 -0400

    Move types locally

--- x-pack/plugins/reporting/types.d.ts
+++ x-pack/plugins/reporting/types.d.ts
@@ -3,14 +3,19 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+export interface KbnServer {
+  config: () => ConfigObject;
+}
 
-export type KbnServer = import('../../../src/server').KbnServer;
+export interface ConfigObject {
+  get: (path: string) => any;
+}
 
 export interface Size {
   width: number;
   height: number;
 }
-  
+
 export interface Logger {
   debug: (message: string) => void;
   error: (message: string) => void;
