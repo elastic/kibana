620915019294525e085db2d8d33f3f5fe955f4c5
Chris Cowan <chris@chriscowan.us>
2018-09-04 10:45:19 -0700

    Removing unused dependicies

--- x-pack/plugins/infra/types/kibana.d.ts
+++ x-pack/plugins/infra/types/kibana.d.ts
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Moment } from 'moment';
-
 declare module '*.json' {
   const content: any;
   export default content;
