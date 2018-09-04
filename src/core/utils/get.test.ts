7fe6893c9674737184a43c582e4fb39dd8d620ed
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-09-04 17:36:19 +0300

    Get rid of `__tests__` folders in the core.

--- src/core/utils/get.test.ts
+++ src/core/utils/get.test.ts
@@ -17,10 +17,33 @@
  * under the License.
  */
 
-export { ConfigService } from './config_service';
-export { RawConfigService } from './raw_config_service';
-export { Config, ConfigPath } from './config';
-/** @internal */
-export { ObjectToConfigAdapter } from './object_to_config_adapter';
-export { Env, CliArgs } from './env';
-export { ConfigWithSchema } from './config_with_schema';
+import { get } from './get';
+
+const obj = {
+  bar: {
+    quux: 123,
+  },
+  'dotted.value': 'dots',
+  foo: 'value',
+};
+
+test('get with string', () => {
+  const value = get(obj, 'foo');
+  expect(value).toBe('value');
+});
+
+test('get with array', () => {
+  const value = get(obj, ['bar', 'quux']);
+  expect(value).toBe(123);
+});
+
+test('throws if dot in string', () => {
+  expect(() => {
+    get(obj, 'dotted.value');
+  }).toThrowErrorMatchingSnapshot();
+});
+
+test('does not throw if dot in array', () => {
+  const value = get(obj, ['dotted.value']);
+  expect(value).toBe('dots');
+});
