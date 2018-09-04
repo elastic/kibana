6b8294a24b32aa45d9f69cb45221f7e4cd854823
Jonathan Budzenski <jon@jbudz.me>
2018-09-04 10:22:50 -0500

    from flag is kibana, fromEs is es

--- test/scripts/jenkins_selenium.sh
+++ test/scripts/jenkins_selenium.sh
@@ -7,4 +7,4 @@ source "$(dirname $0)/../../src/dev/ci_setup/java_setup.sh"
 
 node scripts/build --release --debug --oss;
 
-xvfb-run "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:selenium --from=snapshot;
+xvfb-run "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:selenium --from=source;
