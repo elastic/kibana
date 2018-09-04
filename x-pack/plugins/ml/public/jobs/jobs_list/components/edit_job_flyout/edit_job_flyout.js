0069b6da00f73bcd78ed751ef89d393f0ddfeda2
James Gowdy <jgowdy@elastic.co>
2018-09-04 07:12:34 +0100

    [ML] Edit job group duplicate validation (#21987) (#22631)
    * [ML] Edit job group duplicate validation
    
    * changes based on review

--- x-pack/plugins/ml/public/jobs/jobs_list/components/edit_job_flyout/edit_job_flyout.js
+++ x-pack/plugins/ml/public/jobs/jobs_list/components/edit_job_flyout/edit_job_flyout.js
@@ -129,7 +129,11 @@ export class EditJobFlyout extends Component {
     }
 
     if (jobDetails.jobGroups !== undefined) {
-      jobGroupsValidationError = validateGroupNames(jobDetails.jobGroups).message;
+      if (jobDetails.jobGroups.some(j => this.props.allJobIds.includes(j))) {
+        jobGroupsValidationError = 'A job with this ID already exists. Groups and jobs cannot use the same ID.';
+      } else {
+        jobGroupsValidationError = validateGroupNames(jobDetails.jobGroups).message;
+      }
     }
 
     const valid = (jobModelMemoryLimitValidationError === '' && jobGroupsValidationError === '');
@@ -308,4 +312,5 @@ EditJobFlyout.propTypes = {
   setShowFunction: PropTypes.func.isRequired,
   unsetShowFunction: PropTypes.func.isRequired,
   refreshJobs: PropTypes.func.isRequired,
+  allJobIds: PropTypes.array.isRequired,
 };
