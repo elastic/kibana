bdbfa1ab3a1f10cda63ba6950749b42740c417d1
CJ Cenizal <cj@cenizal.com>
2018-09-05 09:49:52 -0700

    Add "Create rollup job" wizard foundation and logistics step (#22381)
    * Lay foundation for wizard: chrome, navigation, steps configuration, error-reporting.
    * Add first step for rollup logistics: form, help text, documentation link, validation.
    * You can create a job with the wizard: add createJob API endpoint, deep-link to newly-created job.
    * Show 'Loading' and 'No job found' feedback inside of detail panel.
    * Internationalize job list page, create page, and logistics step.

---
+++ x-pack/plugins/rollup/public/crud_app/store/actions/create_job.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { toastNotifications } from 'ui/notify';
+
+import { CRUD_APP_BASE_PATH } from '../../constants';
+import {
+  createJob as sendCreateJobRequest,
+  serializeJob,
+  deserializeJob,
+  getRouter,
+} from '../../services';
+
+import {
+  CREATE_JOB_START,
+  CREATE_JOB_SUCCESS,
+  CREATE_JOB_FAILURE,
+  CLEAR_CREATE_JOB_ERRORS,
+} from '../action_types';
+
+export const createJob = (jobConfig) => async (dispatch) => {
+  dispatch({
+    type: CREATE_JOB_START,
+  });
+
+  let newJob;
+
+  try {
+    [ newJob ] = await Promise.all([
+      sendCreateJobRequest(serializeJob(jobConfig)),
+      // Wait at least half a second to avoid a weird flicker of the saving feedback.
+      new Promise(resolve => setTimeout(resolve, 500)),
+    ]);
+  } catch (error) {
+    const { status, data } = error;
+
+    switch (status) {
+      case 409:
+        dispatch({
+          type: CREATE_JOB_FAILURE,
+          payload: {
+            error: `A job with ID '${jobConfig.id}' already exists.`
+          }
+        });
+        break;
+
+      default:
+        dispatch({
+          type: CREATE_JOB_FAILURE,
+          payload: {
+            error: `Request failed with a ${status} error. ${data.message}`,
+          }
+        });
+    }
+
+    return;
+  }
+
+  toastNotifications.addSuccess(`Rollup job '${jobConfig.id}' was created`);
+
+  dispatch({
+    type: CREATE_JOB_SUCCESS,
+    payload: { job: deserializeJob(newJob.data) },
+  });
+
+  // This will open the new job in the detail panel.
+  getRouter().history.push({
+    pathname: CRUD_APP_BASE_PATH,
+    search: `?job=${jobConfig.id}`,
+  });
+};
+
+export const clearCreateJobErrors = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_CREATE_JOB_ERRORS,
+  });
+};
--- x-pack/plugins/rollup/server/routes/api/jobs.js
+++ x-pack/plugins/rollup/server/routes/api/jobs.js
@@ -29,6 +29,37 @@ export function registerJobsRoute(server) {
   });
 
   server.route({
+    path: '/api/rollup/create',
+    method: 'PUT',
+    handler: async (request, reply) => {
+      try {
+        const {
+          id,
+          ...rest
+        } = request.payload.job;
+
+        const callWithRequest = callWithRequestFactory(server, request);
+
+        // Create job.
+        await callWithRequest('rollup.createJob', {
+          id,
+          body: rest,
+        });
+
+        // Then request the newly created job.
+        const results = await callWithRequest('rollup.job', { id });
+        reply(results.jobs[0]);
+      } catch(err) {
+        if (isEsError(err)) {
+          return reply(wrapEsError(err));
+        }
+
+        reply(wrapUnknownError(err));
+      }
+    },
+  });
+
+  server.route({
     path: '/api/rollup/start',
     method: 'POST',
     handler: async (request, reply) => {
