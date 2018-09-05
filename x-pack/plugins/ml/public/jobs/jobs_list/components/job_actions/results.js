d4e3cdb8539676373b111a61f719e39ae52683ff
James Gowdy <jgowdy@elastic.co>
2018-09-05 19:57:32 +0100

    [ML] Fixing issue with incorrect timezones in jobs list (#22714) (#22723)
    * [ML] Fixing issue with incorrect timezones in jobs list
    
    * refactoring min and max calculation
    
    * changes based on review
    
    * changing TimeStamp to Timestamp

--- x-pack/plugins/ml/public/jobs/jobs_list/components/job_actions/results.js
+++ x-pack/plugins/ml/public/jobs/jobs_list/components/job_actions/results.js
@@ -14,6 +14,8 @@ import {
 } from '@elastic/eui';
 
 import chrome from 'ui/chrome';
+import moment from 'moment';
+const TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 import { mlJobService } from 'plugins/ml/services/job_service';
 
@@ -21,22 +23,18 @@ function getLink(location, jobs) {
   let from = 0;
   let to = 0;
   if (jobs.length === 1) {
-    from = jobs[0].earliestTimeStamp.string;
-    to = jobs[0].latestTimeStamp.string;
+    from = jobs[0].earliestTimestampMs;
+    to = jobs[0].latestTimestampMs;
   } else {
-    const froms = jobs.map(j => j.earliestTimeStamp).sort((a, b) => a.unix > b.unix);
-    const tos = jobs.map(j => j.latestTimeStamp).sort((a, b) => a.unix < b.unix);
-    from = froms[0].string;
-    to = tos[0].string;
+    from = Math.min(...jobs.map(j => j.earliestTimestampMs));
+    to = Math.max(...jobs.map(j => j.latestTimestampMs));
   }
 
-  // if either of the dates are empty, set them to undefined
-  // moment will convert undefined to now.
-  from = (from === '') ? undefined : from;
-  to = (to === '') ? undefined : to;
+  const fromString = moment(from).format(TIME_FORMAT);
+  const toString = moment(to).format(TIME_FORMAT);
 
   const jobIds = jobs.map(j => j.id);
-  const url = mlJobService.createResultsUrl(jobIds, from, to, location);
+  const url = mlJobService.createResultsUrl(jobIds, fromString, toString, location);
   return `${chrome.getBasePath()}/app/${url}`;
 }
 
