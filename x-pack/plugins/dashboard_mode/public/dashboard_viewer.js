86bffd0d2a019b2e30c4c03bf2224642a4b8a3cc
Chris Davies <github@christophilus.com>
2018-09-04 21:46:59 -0400

    Fix #22510, dashboard-only mode doesn't display saved searches (#22685)

--- x-pack/plugins/dashboard_mode/public/dashboard_viewer.js
+++ x-pack/plugins/dashboard_mode/public/dashboard_viewer.js
@@ -33,6 +33,7 @@ import 'ui/vislib';
 import 'ui/agg_response';
 import 'ui/agg_types';
 import 'ui/timepicker';
+import 'ui/pager';
 import 'leaflet';
 
 import { showAppRedirectNotification } from 'ui/notify';
