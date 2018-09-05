3cf91443ff4b4e8c1201016af7bfbaa2c973b854
CJ Cenizal <cj@cenizal.com>
2018-09-04 17:08:54 -0700

    Extract index pattern validation rules into ui/public (#22606) (#22680)
    * Rename containsInvalidCharacters to containsIllegalCharacters and return a value which accurately reflects the name.
    * Move illegal index pattern characters from Management into ui/public/index_patterns.

--- src/ui/public/index_patterns/constants/index.js
+++ src/ui/public/index_patterns/constants/index.js
@@ -17,16 +17,5 @@
  * under the License.
  */
 
-import { containsIllegalCharacters } from '../contains_illegal_characters';
-
-describe('containsIllegalCharacters', () => {
-  it('returns true with illegal characters', () => {
-    const isInvalid = containsIllegalCharacters('abc', ['a']);
-    expect(isInvalid).toBe(true);
-  });
-
-  it('returns false with no illegal characters', () => {
-    const isInvalid = containsIllegalCharacters('abc', ['%']);
-    expect(isInvalid).toBe(false);
-  });
-});
+export const INDEX_PATTERN_ILLEGAL_CHARACTERS_VISIBLE = ['\\', '/', '?', '"', '<', '>', '|'];
+export const INDEX_PATTERN_ILLEGAL_CHARACTERS = INDEX_PATTERN_ILLEGAL_CHARACTERS_VISIBLE.concat(' ');
