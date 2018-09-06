f6c3b4b5b231d47c613f15aa501c03debb0152ed
Walter Rafelsberger <walter@elastic.co>
2018-09-06 13:48:55 +0200

    [ML] Tests for ExplorerChartsContainer.

---
+++ x-pack/plugins/ml/public/explorer/explorer_charts/explorer_charts_container.test.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import mockChartData from './__mocks__/mock_chart_data.json';
+
+// Mock TimeBuckets and mlFieldFormatService, they don't play well
+// with the jest based test setup yet.
+jest.mock('ui/time_buckets', () => ({
+  TimeBuckets: function () {
+    this.setBounds = jest.fn();
+    this.setInterval = jest.fn();
+    this.getScaledDateFormat = jest.fn();
+  }
+}));
+jest.mock('../../services/field_format_service', () => ({
+  mlFieldFormatService: {
+    getFieldFormat: jest.fn()
+  }
+}));
+
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import { mlChartTooltipService } from '../../components/chart_tooltip/chart_tooltip_service';
+
+import { ExplorerChartsContainer } from './explorer_charts_container';
+
+describe('ExplorerChartsContainer', () => {
+  const mlSelectSeverityServiceMock = {
+    state: {
+      get: () => ({
+        val: ''
+      })
+    }
+  };
+
+  const mockedGetBBox = { x: 0, y: -11.5, width: 12.1875, height: 14.5 };
+  const originalGetBBox = SVGElement.prototype.getBBox;
+  beforeEach(() => SVGElement.prototype.getBBox = () => {
+    return mockedGetBBox;
+  });
+  afterEach(() => (SVGElement.prototype.getBBox = originalGetBBox));
+
+  test('Minimal Initialization', () => {
+    const wrapper = shallow(<ExplorerChartsContainer
+      seriesToPlot={[]}
+      layoutCellsPerChart={12}
+      tooManyBuckets={false}
+      mlSelectSeverityService={mlSelectSeverityServiceMock}
+      mlChartTooltipService={mlChartTooltipService}
+    />);
+
+    expect(wrapper.html()).toBe('<div class="explorer-charts"></div>');
+  });
+
+  test('Initialization with chart data', () => {
+    const wrapper = shallow(<ExplorerChartsContainer
+      seriesToPlot={mockChartData.seriesToPlot}
+      layoutCellsPerChart={mockChartData.layoutCellsPerChart}
+      tooManyBuckets={mockChartData.tooManyBuckets}
+      mlSelectSeverityService={mlSelectSeverityServiceMock}
+      mlChartTooltipService={mlChartTooltipService}
+    />);
+
+    // Only do a snapshot of the label section, the included
+    // ExplorerChart component does that in its own tests anyway.
+    expect(wrapper.find('.explorer-chart-label')).toMatchSnapshot();
+  });
+});
