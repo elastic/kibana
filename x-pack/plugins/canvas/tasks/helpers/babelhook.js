60ea466b1aec055dbea6e7fffb6ef25e670a952c
joe fleming <joe.fleming@gmail.com>
2018-09-07 16:58:47 -0700

    chore: canvas tasks - local tests

---
+++ x-pack/plugins/canvas/tasks/helpers/babelhook.js
@@ -0,0 +1,48 @@
+const { resolve } = require('path');
+const register = require('babel-register');
+
+const options = {
+  sourceMaps: false,
+  plugins: [
+    [
+      'mock-imports',
+      [
+        {
+          pattern: 'scriptjs',
+          location: resolve(__dirname, '..', 'mocks', 'noop'),
+        },
+        {
+          pattern: 'ui/chrome',
+          location: resolve(__dirname, '..', 'mocks', 'uiChrome'),
+        },
+        {
+          pattern: 'ui/notify',
+          location: resolve(__dirname, '..', 'mocks', 'uiNotify'),
+        },
+        {
+          pattern: 'ui/url/absolute_to_parsed_url',
+          location: resolve(__dirname, '..', 'mocks', 'absoluteToParsedUrl'),
+        },
+        {
+          pattern: 'socket.io-client',
+          location: resolve(__dirname, '..', 'mocks', 'socketClient'),
+        },
+        {
+          // ugly hack so that importing non-js files works, required for the function docs
+          pattern: '.(less|png|svg)$',
+          location: resolve(__dirname, '..', 'mocks', 'noop'),
+        },
+        {
+          pattern: 'plugins/canvas/apps',
+          location: resolve(__dirname, '..', 'mocks', 'noop'),
+        },
+        {
+          pattern: '/state/store',
+          location: resolve(__dirname, '..', 'mocks', 'stateStore'),
+        },
+      ],
+    ],
+  ],
+};
+
+register(options);
---
+++ x-pack/plugins/canvas/tasks/mocks/uiChrome.js
@@ -0,0 +1,4 @@
+export default {
+  getBasePath: () => '/abc',
+  trackSubUrlForApp: () => undefined, // noop
+};
