6701bee4a460c7aed0b270616b6b1d6f86c4ef47
Rashid Khan <github.fliplap@spamgourmet.com>
2018-09-07 08:53:12 -0700

    I dont think this does anything (#1103)

--- public/angular/controllers/canvas.js
+++ public/angular/controllers/canvas.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { Provider } from 'react-redux';
-import { uiModules } from 'ui/modules';
 import { App } from '../../components/app';
 
 export function CanvasRootController(canvasStore, $scope, $element) {
@@ -18,6 +17,3 @@ export function CanvasRootController(canvasStore, $scope, $element) {
     unmountComponentAtNode(domNode);
   });
 }
-
-const app = uiModules.get('apps/canvas');
-app.controller('CanvasRootController', CanvasRootController);
