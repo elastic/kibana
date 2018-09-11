b0600a419e2eb0b30422eb23ea9703706e8564f8
Catherine Liu <catherineqliu@outlook.com>
2018-09-10 21:14:15 -0400

    Adds workpads to Recently Viewed (#1090)
    * Adds workpads to recentlyAccessed
    
    * Adds workpad to recently viewed when name changes
    
    * Moved recentlyAccessed logic from setWorkpad action to reducer

--- common/lib/constants.js
+++ common/lib/constants.js
@@ -1,6 +1,7 @@
 export const CANVAS_TYPE = 'canvas-workpad';
 export const CANVAS_APP = 'canvas';
 export const APP_ROUTE = '/app/canvas';
+export const APP_ROUTE_WORKPAD = `${APP_ROUTE}#/workpad`;
 export const API_ROUTE = '/api/canvas';
 export const API_ROUTE_WORKPAD = `${API_ROUTE}/workpad`;
 export const LOCALSTORAGE_LASTPAGE = 'canvas:lastpage';
--- public/state/reducers/workpad.js
+++ public/state/reducers/workpad.js
@@ -1,9 +1,12 @@
 import { handleActions } from 'redux-actions';
+import { recentlyAccessed } from 'ui/persisted_log';
 import { setWorkpad, sizeWorkpad, setColors, setName } from '../actions/workpad';
+import { APP_ROUTE_WORKPAD } from '../../../common/lib/constants';
 
 export const workpadReducer = handleActions(
   {
     [setWorkpad]: (workpadState, { payload }) => {
+      recentlyAccessed.add(`${APP_ROUTE_WORKPAD}/${payload.id}`, payload.name, payload.id);
       return payload;
     },
 
@@ -16,6 +19,7 @@ export const workpadReducer = handleActions(
     },
 
     [setName]: (workpadState, { payload }) => {
+      recentlyAccessed.add(`${APP_ROUTE_WORKPAD}/${workpadState.id}`, payload, workpadState.id);
       return { ...workpadState, name: payload };
     },
   },
