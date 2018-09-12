bbc54cba7ad51ad8cfafcfda95a31c049141baef
kobelb <brandon.kobel@elastic.co>
2018-09-12 13:55:49 -0400

    Relying on the errors thrown by the SpacedClient when determining active space

--- x-pack/plugins/spaces/server/lib/get_active_space.js
+++ x-pack/plugins/spaces/server/lib/get_active_space.js
@@ -4,27 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import Boom from 'boom';
 import { wrapError } from './errors';
 import { getSpaceIdFromPath } from './spaces_url_parser';
 
 export async function getActiveSpace(spacesClient, requestBasePath, serverBasePath) {
   const spaceId = getSpaceIdFromPath(requestBasePath, serverBasePath);
 
-  let space;
-
   try {
-    space = spacesClient.get(spaceId);
+    return spacesClient.get(spaceId);
   }
   catch (e) {
     throw wrapError(e);
   }
-
-  if (!space) {
-    throw Boom.notFound(
-      `The Space you requested could not be found. Please select a different Space to continue.`
-    );
-  }
-
-  return space;
 }
