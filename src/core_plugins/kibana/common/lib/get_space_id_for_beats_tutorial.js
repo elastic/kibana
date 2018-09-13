1590cc0b0bb5448bfbacc53ce5e30b9c619f88f3
Chris Roberson <chrisronline@gmail.com>
2018-09-13 12:56:36 -0400

    Add comment

--- src/core_plugins/kibana/common/lib/get_space_id_for_beats_tutorial.js
+++ src/core_plugins/kibana/common/lib/get_space_id_for_beats_tutorial.js
@@ -17,6 +17,12 @@
  * under the License.
  */
 
+/**
+ * Returns valid configuration for a beat.yml file for adding the space id
+ * if there is an active space and that space is not the default one.
+ *
+ * @param {object} context - Context object generated from tutorial factory (see #22760)
+ */
 export function getSpaceIdForBeatsTutorial(context) {
   if (!context || !context.spaceId || context.spaceId === 'default') {
     return '';
