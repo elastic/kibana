37f31a1e095346a63d8467d8a433f76698f3deb1
Nathan Reese <reese.nathan@gmail.com>
2018-09-18 09:38:08 -0600

    add comment in createIndexName

--- src/server/sample_data/routes/lib/create_index_name.js
+++ src/server/sample_data/routes/lib/create_index_name.js
@@ -18,6 +18,9 @@
  */
 
 export function createIndexName(sampleDataSetId, dataIndexId) {
+  // Sample data schema was updated to support multiple indices in 6.5.
+  // This if statement ensures that sample data sets that used a single index prior to the schema change
+  // have the same index name to avoid orphaned indices when uninstalling.
   if (sampleDataSetId === dataIndexId) {
     return `kibana_sample_data_${sampleDataSetId}`;
   }
