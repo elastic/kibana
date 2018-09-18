e566bdeab114656b2e9bd451bc36c3d0597a5dd1
Aaron Caldwell <aaron.caldwell@elastic.co>
2018-09-18 10:29:45 -0600

    Use resize checker for both nav & banner. Dispatch updates on change

--- x-pack/plugins/gis/public/shared/utils/observers.js
+++ x-pack/plugins/gis/public/shared/utils/observers.js
@@ -4,25 +4,27 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-let navElement;
-const checkExist = setInterval(() => {
-  navElement = document.querySelector('.global-nav');
-  if (navElement) {
-    assignNavObserver();
-    clearInterval(checkExist);
-  }
-}, 300); // check every 100ms
+import { ResizeChecker } from 'ui/resize_checker';
+import { getStore } from '../../store/store';
+import { changeBannerSize, changeNavSize } from '../../actions/ui_actions';
 
-function assignNavObserver() {
-  const mutationObserver = new MutationObserver(mutations => {
-    mutations.forEach(() => {
-      console.log(window.localStorage.getItem('kibana.isGlobalNavOpen'));
-    });
-  });
+const navElementSelector = '.global-nav';
+const globalBannerSelector = '#globalBannerList';
 
-  mutationObserver.observe(navElement, {
-    attributes: true,
-    attributeOldValue: true,
-    attributeFilter: ['class']
-  });
-}
+const assignResizeWatch = (qSelector, actionUpdate) => {
+  let checker;
+  let resizeElement;
+  const checkExist = setInterval(() => {
+    resizeElement = document.querySelector(qSelector);
+    if (resizeElement) {
+      checker = new ResizeChecker(resizeElement);
+      checker.on('resize', () => {
+        getStore().then(store => store.dispatch(actionUpdate));
+      });
+      clearInterval(checkExist);
+    }
+  }, 300); // check every 300ms
+};
+
+assignResizeWatch(navElementSelector, changeNavSize);
+assignResizeWatch(globalBannerSelector, changeBannerSize);
