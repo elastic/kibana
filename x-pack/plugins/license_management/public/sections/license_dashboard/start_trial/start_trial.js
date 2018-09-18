3b9679960add58183feda2e429db06a39031bf0c
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-09-18 12:51:57 -0400

    adding opt in for telemetry to start trial and upload license (#22925) (#23273)
    * adding opt in for telemetry to start trial and upload license
    
    * fixing scrolling issue with start basic modal in small browser window and improving a11y for read more
    
    * Design cleanup
    
    Mostly spacing and alignment fixes.
    
    * updating jest snapshots
    
    * IE Fixes
    
    * PR feedback

--- x-pack/plugins/license_management/public/sections/license_dashboard/start_trial/start_trial.js
+++ x-pack/plugins/license_management/public/sections/license_dashboard/start_trial/start_trial.js
@@ -7,17 +7,24 @@
 import React from 'react';
 
 import {
+  EuiButtonEmpty,
   EuiButton,
+  EuiFlexGroup,
   EuiFlexItem,
   EuiCard,
   EuiLink,
   EuiOverlayMask,
-  EuiConfirmModal,
-  EuiText
+  EuiText,
+  EuiModal,
+  EuiModalFooter,
+  EuiModalHeader,
+  EuiModalBody,
+  EuiModalHeaderTitle
 } from '@elastic/eui';
 
 import { ELASTIC_WEBSITE_URL, DOC_LINK_VERSION } from 'ui/documentation_links';
-
+import { TelemetryOptIn } from '../../../components/telemetry_opt_in';
+import { optInToTelemetry } from '../../../lib/telemetry';
 const esBase = `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/reference/${DOC_LINK_VERSION}`;
 const securityDocumentationLink = `${esBase}/security-settings.html`;
 
@@ -30,63 +37,110 @@ export class StartTrial extends React.PureComponent {
   componentWillMount() {
     this.props.loadTrialStatus();
   }
-
+  startLicenseTrial = () => {
+    const { startLicenseTrial } = this.props;
+    if (this.telemetryOptIn.isOptingInToTelemetry()) {
+      optInToTelemetry(true);
+    }
+    startLicenseTrial();
+  }
+  cancel = () => {
+    this.setState({ showConfirmation: false });
+  }
   acknowledgeModal() {
     const { showConfirmation } = this.state;
     if (!showConfirmation) {
       return null;
     }
-    const { startLicenseTrial } = this.props;
+
     return (
       <EuiOverlayMask>
-        <EuiConfirmModal
-          title="Start your free 30-day trial"
-          onCancel={() => this.setState({ showConfirmation: false })}
-          onConfirm={() => startLicenseTrial()}
-          cancelButtonText="Cancel"
-          confirmButtonText="Start my trial"
+        <EuiModal
+          className="licenseManagement__modal"
+          onClose={this.cancel}
         >
-          <div>
-            <EuiText>
-              <p>
+          <EuiModalHeader>
+            <EuiModalHeaderTitle data-test-subj="confirmModalTitleText">
+            Start your free 30-day trial
+            </EuiModalHeaderTitle>
+          </EuiModalHeader>
+          <EuiModalBody>
+            <EuiText data-test-subj="confirmModalBodyText">
+              <div>
+                <EuiText>
+                  <p>
                 This trial is for the full set of{' '}
-                <EuiLink
-                  href="https://www.elastic.co/subscriptions/xpack"
-                  target="_blank"
-                >
+                    <EuiLink
+                      href="https://www.elastic.co/subscriptions/xpack"
+                      target="_blank"
+                    >
                   Platinum features
-                </EuiLink> of the Elastic Stack. You&apos;ll get immediate access to:
-              </p>
-              <ul>
-                <li>Machine learning</li>
-                <li>Alerting</li>
-                <li>Graph capabilities</li>
-                <li>JDBC connectivity for SQL</li>
-              </ul>
-              <p>
+                    </EuiLink> of the Elastic Stack. You&apos;ll get immediate access to:
+                  </p>
+                  <ul>
+                    <li>Machine learning</li>
+                    <li>Alerting</li>
+                    <li>Graph capabilities</li>
+                    <li>JDBC connectivity for SQL</li>
+                  </ul>
+                  <p>
                 Security features, such as authentication (native, AD/LDAP, SAML,
                 PKI), role-based access control, and auditing, require
                 configuration. See the{' '}
-                <EuiLink
-                  href={securityDocumentationLink}
-                  target="_blank"
-                >
+                    <EuiLink
+                      href={securityDocumentationLink}
+                      target="_blank"
+                    >
                   documentation
-                </EuiLink>{' '}
+                    </EuiLink>{' '}
                 for instructions.
-              </p>
-              <p>
+                  </p>
+                  <p>
                 By starting this trial, you agree that it is subject to these{' '}
-                <EuiLink
-                  href="https://elastic.co/legal/trial_license"
-                  target="_blank"
-                >
+                    <EuiLink
+                      href="https://elastic.co/legal/trial_license"
+                      target="_blank"
+                    >
                   terms and conditions
-                </EuiLink>.
-              </p>
+                    </EuiLink>.
+                  </p>
+                </EuiText>
+              </div>
             </EuiText>
-          </div>
-        </EuiConfirmModal>
+          </EuiModalBody>
+
+          <EuiModalFooter>
+            <EuiFlexGroup justifyContent="spaceBetween" alignItems="center">
+              <EuiFlexItem grow={false}>
+                <TelemetryOptIn isStartTrial={true} ref={(ref) => {this.telemetryOptIn = ref; }}/>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false} className="licenseManagement__ieFlex">
+                <EuiFlexGroup responsive={false}>
+                  <EuiFlexItem grow={false} className="licenseManagement__ieFlex">
+                    <EuiButtonEmpty
+                      data-test-subj="confirmModalCancelButton"
+                      onClick={this.cancel}
+                      buttonRef={this.cancelRef}
+                    >
+                Cancel
+                    </EuiButtonEmpty>
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false} className="licenseManagement__ieFlex">
+                    <EuiButton
+                      data-test-subj="confirmModalConfirmButton"
+                      onClick={this.startLicenseTrial}
+                      fill
+                      buttonRef={this.confirmRef}
+                      color="primary"
+                    >
+              Start my trial
+                    </EuiButton>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiModalFooter>
+        </EuiModal>
       </EuiOverlayMask>
     );
   }
