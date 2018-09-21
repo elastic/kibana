43c09a4fdf2702e316ad13f472cfec769ec72930
Justin Kambic <justin.kambic@elastic.co>
2018-09-21 08:59:09 -0400

    Update beat detail view info. (#23369)
    * Update beat detail view info.
    
    * Add period to end of update field.

--- x-pack/plugins/beats_management/public/pages/beat/action_section.tsx
+++ x-pack/plugins/beats_management/public/pages/beat/action_section.tsx
@@ -5,6 +5,7 @@
  */
 
 import { EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
+import { first, sortByOrder } from 'lodash';
 import moment from 'moment';
 import React from 'react';
 import { CMPopulatedBeat } from '../../../common/domain_types';
@@ -23,17 +24,26 @@ export const BeatDetailsActionSection = ({ beat }: BeatDetailsActionSectionProps
             <strong>{beat.version}</strong>.
           </EuiText>
         </EuiFlexItem>
+        {/* TODO: We need a populated field before we can run this code
         <EuiFlexItem grow={false}>
-          {/* TODO: What field is used to populate this value? */}
           <EuiText size="xs">
             Uptime: <strong>12min.</strong>
           </EuiText>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiText size="xs">
-            Last Config Update: <strong>{moment(beat.last_updated).fromNow()}</strong>
-          </EuiText>
-        </EuiFlexItem>
+        </EuiFlexItem> */}
+        {beat.full_tags &&
+          beat.full_tags.length > 0 && (
+            <EuiFlexItem grow={false}>
+              <EuiText size="xs">
+                Last Config Update:{' '}
+                <strong>
+                  {moment(
+                    first(sortByOrder(beat.full_tags, 'last_updated')).last_updated
+                  ).fromNow()}
+                </strong>
+                .
+              </EuiText>
+            </EuiFlexItem>
+          )}
       </EuiFlexGroup>
     ) : (
       <div>Beat not found</div>
