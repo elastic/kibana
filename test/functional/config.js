d7f78c8fa95212ff7a28d405ef9023f4760882c3
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2018-09-24 08:11:43 -0700

    user email validation (#23346)
    * Added automation for a support-dev-help ticket - user email validation  https://github.com/elastic/support-dev-help/issues/4571

--- test/functional/config.js
+++ test/functional/config.js
@@ -151,6 +151,10 @@ export default async function ({ readConfigFile }) {
         pathname: '/app/kibana',
         hash: '/dev_tools/console',
       },
+      account: {
+        pathname: '/app/kibana',
+        hash: '/account',
+      },
       home: {
         pathname: '/app/kibana',
         hash: '/home',
