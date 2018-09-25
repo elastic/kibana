4c331b1348b6f66d7be09a8a1d41e719e0fb74e0
pavel06081991 <pavel.1991@tut.by>
2018-09-25 16:18:05 +0300

    Update versions of @babel/parser and @babel/types (#23268) (#23469)
    Update versions of @babel/parser, @babel/types, eslint, babel-eslint

--- x-pack/plugins/canvas/tasks/plugins.js
+++ x-pack/plugins/canvas/tasks/plugins.js
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+// eslint-disable-next-line import/no-extraneous-dependencies
 import webpack from 'webpack';
 import webpackConfig from './helpers/webpack.plugins';
 
