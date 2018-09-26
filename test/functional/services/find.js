6fcb800f0a2661be2cf5a97996990c321a65f739
Stacey Gammon <gammon@elastic.co>
2018-09-26 13:11:21 -0400

    Fixes #21446
    An attempt to fix the above by making sure the click only happens when the button is enabled.

--- test/functional/services/find.js
+++ test/functional/services/find.js
@@ -187,6 +187,27 @@ export function FindProvider({ getService }) {
       return await this.exists(async remote => await remote.findByCssSelector(selector), timeout);
     }
 
+    async clickWhenNotDisabled(element, options = { doubleClick: false }) {
+      log.debug(`clickWhenNotDisabled`);
+      // Don't wrap this code in a retry, or stale element checks may get caught here and the element
+      // will never be re-grabbed.  Let errors bubble, but continue checking for disabled property until
+      // it's gone.
+      await remote.moveMouseTo(element);
+
+      const clickIfNotDisabled = async (element, resolve) => {
+        const disabled = await element.getProperty('disabled');
+        if (disabled) {
+          log.debug('Element is disabled, try again');
+          setTimeout(() => clickIfNotDisabled(element, resolve), 250);
+        } else {
+          options.doubleClick ? await remote.doubleClick() : await element.click();
+          resolve();
+        }
+      };
+
+      await new Promise(resolve => clickIfNotDisabled(element, resolve));
+    }
+
     async clickByPartialLinkText(linkText, timeout = defaultFindTimeout) {
       log.debug(`clickByPartialLinkText(${linkText})`);
       await retry.try(async () => {
--- test/functional/services/test_subjects.js
+++ test/functional/services/test_subjects.js
@@ -63,13 +63,14 @@ export function TestSubjectsProvider({ getService }) {
       });
     }
 
+    async clickWhenNotDisabled(selector, timeout = defaultFindTimeout) {
+      log.debug(`TestSubjects.click(${selector})`);
+      await find.clickWhenNotDisabled(testSubjSelector(selector), timeout);
+    }
+
     async click(selector, timeout = defaultFindTimeout) {
       log.debug(`TestSubjects.click(${selector})`);
-      return await retry.try(async () => {
-        const element = await this.find(selector, timeout);
-        await remote.moveMouseTo(element);
-        await element.click();
-      });
+      await find.click(testSubjSelector(selector), timeout);
     }
 
     async doubleClick(selector, timeout = defaultFindTimeout) {
@@ -80,7 +81,6 @@ export function TestSubjectsProvider({ getService }) {
         await remote.doubleClick();
       });
     }
-
 
     async descendantExists(selector, parentElement) {
       return await find.descendantExistsByCssSelector(testSubjSelector(selector), parentElement);
