ed9e47e23c21acd333c42ebfab72ff80c0d496cd
Josh Dover <me@joshdover.com>
2018-09-26 09:12:24 -0500

    Setup yarn in current shell [skip ci]

--- test/scripts/jenkins_report_failed_tests.sh
+++ test/scripts/jenkins_report_failed_tests.sh
@@ -2,4 +2,15 @@
 
 set -e
 
+###
+### "install" yarn into this shell
+###
+cacheDir="${CACHE_DIR:-"$HOME/.kibana"}"
+yarnVersion="$(node -e "console.log(String(require('./package.json').engines.yarn || '').replace(/^[^\d]+/,''))")"
+yarnDir="$cacheDir/yarn/$yarnVersion"
+export PATH="$yarnDir/bin:$PATH"
+yarnGlobalDir="$(yarn global bin)"
+export PATH="$PATH:$yarnGlobalDir"
+hash -r
+
 xvfb-run "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:report;
