68bb3f889d6a9e146fb303e51b6a3251e2a29565
Walter Rafelsberger <walter@elastic.co>
2018-09-27 11:25:44 +0200

    [ML] Updated PropTypes and argument default value for wrapLabel.

--- x-pack/plugins/ml/public/explorer/explorer_charts/components/explorer_chart_label/explorer_chart_label.js
+++ x-pack/plugins/ml/public/explorer/explorer_charts/components/explorer_chart_label/explorer_chart_label.js
@@ -16,7 +16,7 @@ import {
 import { ExplorerChartLabelBadge } from './explorer_chart_label_badge';
 import { ExplorerChartTooltip } from '../../explorer_chart_tooltip';
 
-export function ExplorerChartLabel({ detectorLabel, entityFields, infoTooltip, wrapLabel }) {
+export function ExplorerChartLabel({ detectorLabel, entityFields, infoTooltip, wrapLabel = false }) {
   // Depending on whether we wrap the entityField badges to a new line, we render this differently:
   //
   // 1. All in one line:
@@ -59,8 +59,11 @@ export function ExplorerChartLabel({ detectorLabel, entityFields, infoTooltip, w
   );
 }
 ExplorerChartLabel.propTypes = {
-  series: PropTypes.shape({
-    detectorLabel: PropTypes.string.isRequired,
-    entityFields: PropTypes.array.isRequired
-  })
+  detectorLabel: PropTypes.string.isRequired,
+  entityFields: PropTypes.arrayOf(PropTypes.shape({
+    fieldName: PropTypes.string.isRequired,
+    fieldValue: PropTypes.string.isRequired
+  })),
+  infoTooltip: PropTypes.object,
+  wrapLabel: PropTypes.bool
 };
