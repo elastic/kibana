c2a6fb2b4930755218802f8da241fc5d96fc38ce
Timothy Sullivan <tsullivan@elastic.co>
2018-10-02 16:46:24 -0700

    fix tests

--- x-pack/plugins/task_manager/task_manager.test.ts
+++ x-pack/plugins/task_manager/task_manager.test.ts
@@ -6,9 +6,11 @@
 
 import _ from 'lodash';
 import sinon from 'sinon';
-import { taskManagerMixin } from './task_manager_mixin';
+import { TaskManager } from './task_manager';
+import { TaskPoller } from './task_poller';
+import { TaskStore } from './task_store';
 
-describe('taskManagerMixin', () => {
+describe('TaskManager', () => {
   let clock: sinon.SinonFakeTimers;
   const defaultConfig = {
     task_manager: {
@@ -28,10 +30,11 @@ describe('taskManagerMixin', () => {
 
   test('starts / stops the poller when es goes green / red', async () => {
     const { $test, opts } = testOpts();
-    taskManagerMixin(opts.server, opts.config);
+    const client = new TaskManager(opts.kbnServer, opts.server, opts.config);
+
     $test.afterPluginsInit();
 
-    const { store, poller } = (opts.server as any).taskManager;
+    const { store = new TaskStore(), poller = new TaskPoller() } = client;
 
     store.init = sinon.spy(async () => undefined);
     poller.start = sinon.spy(async () => undefined);
@@ -55,44 +58,39 @@ describe('taskManagerMixin', () => {
 
   test('disallows schedule before init', async () => {
     const { opts } = testOpts();
-    taskManagerMixin(opts.server, opts.config);
-    const { taskManager } = opts.server as any;
+    const client = new TaskManager(opts.kbnServer, opts.server, opts.config);
     const task = {
       taskType: 'foo',
       params: {},
     };
-    await expect(taskManager.schedule(task)).rejects.toThrow(/The task manager is initializing/i);
+    await expect(client.schedule(task)).rejects.toThrow(/The task manager is initializing/i);
   });
 
   test('disallows fetch before init', async () => {
     const { opts } = testOpts();
-    taskManagerMixin(opts.server, opts.config);
-    const { taskManager } = opts.server as any;
-    await expect(taskManager.fetch({})).rejects.toThrow(/The task manager is initializing/i);
+    const client = new TaskManager(opts.kbnServer, opts.server, opts.config);
+    await expect(client.fetch({})).rejects.toThrow(/The task manager is initializing/i);
   });
 
   test('disallows remove before init', async () => {
     const { opts } = testOpts();
-    taskManagerMixin(opts.server, opts.config);
-    const { taskManager } = opts.server as any;
-    await expect(taskManager.remove('23')).rejects.toThrow(/The task manager is initializing/i);
+    const client = new TaskManager(opts.kbnServer, opts.server, opts.config);
+    await expect(client.remove('23')).rejects.toThrow(/The task manager is initializing/i);
   });
 
   test('allows middleware registration before init', () => {
     const { opts } = testOpts();
-    taskManagerMixin(opts.server, opts.config);
-    const { taskManager } = opts.server as any;
+    const client = new TaskManager(opts.kbnServer, opts.server, opts.config);
     const middleware = {
       beforeSave: async (saveOpts: any) => saveOpts,
       beforeRun: async (runOpts: any) => runOpts,
     };
-    expect(() => taskManager.addMiddleware(middleware)).not.toThrow();
+    expect(() => client.addMiddleware(middleware)).not.toThrow();
   });
 
   test('disallows middleware registration after init', async () => {
     const { $test, opts } = testOpts();
-    taskManagerMixin(opts.server, opts.config);
-    const { taskManager } = opts.server as any;
+    const client = new TaskManager(opts.kbnServer, opts.server, opts.config);
     const middleware = {
       beforeSave: async (saveOpts: any) => saveOpts,
       beforeRun: async (runOpts: any) => runOpts,
@@ -100,7 +98,7 @@ describe('taskManagerMixin', () => {
 
     $test.afterPluginsInit();
 
-    expect(() => taskManager.addMiddleware(middleware)).toThrow(
+    expect(() => client.addMiddleware(middleware)).toThrow(
       /Cannot add middleware after the task manager is initialized/i
     );
   });
@@ -114,6 +112,14 @@ describe('taskManagerMixin', () => {
     const opts = {
       config: {
         get: (path: string) => _.get(defaultConfig, path),
+      },
+      kbnServer: {
+        uiExports: {
+          taskDefinitions: {},
+        },
+        afterPluginsInit(callback: any) {
+          $test.afterPluginsInit = callback;
+        },
       },
       server: {
         log: sinon.spy(),
