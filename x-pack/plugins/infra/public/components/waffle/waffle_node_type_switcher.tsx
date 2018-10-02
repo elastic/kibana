2bd934c82a239947c034eaaf01f12a390282ece4
Chris Cowan <chris@chriscowan.us>
2018-10-02 15:22:33 -0700

    Fixing image paths to work with basePath (#23725)

--- x-pack/plugins/infra/public/components/waffle/waffle_node_type_switcher.tsx
+++ x-pack/plugins/infra/public/components/waffle/waffle_node_type_switcher.tsx
@@ -25,13 +25,13 @@ export class WaffleNodeTypeSwitcher extends React.PureComponent<Props> {
     return (
       <EuiKeyPadMenu>
         <EuiKeyPadMenuItem label="Hosts" onClick={this.handleClick(InfraNodeType.host)}>
-          <img src="/plugins/infra/images/hosts.svg" className="euiIcon euiIcon--large" />
+          <img src="../plugins/infra/images/hosts.svg" className="euiIcon euiIcon--large" />
         </EuiKeyPadMenuItem>
         <EuiKeyPadMenuItem label="Kubernetes" onClick={this.handleClick(InfraNodeType.pod)}>
-          <img src="/plugins/infra/images/k8.svg" className="euiIcon euiIcon--large" />
+          <img src="../plugins/infra/images/k8.svg" className="euiIcon euiIcon--large" />
         </EuiKeyPadMenuItem>
         <EuiKeyPadMenuItem label="Docker" onClick={this.handleClick(InfraNodeType.container)}>
-          <img src="/plugins/infra/images/docker.svg" className="euiIcon euiIcon--large" />
+          <img src="../plugins/infra/images/docker.svg" className="euiIcon euiIcon--large" />
         </EuiKeyPadMenuItem>
       </EuiKeyPadMenu>
     );
