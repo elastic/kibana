d8f3192c7c42758df1020ab7163b52dff5bcd2c6
Josh Dover <me@joshdover.com>
2018-10-02 15:48:18 -0500

    Add validation error shim

--- src/core/server/http/http_tools.ts
+++ src/core/server/http/http_tools.ts
@@ -18,8 +18,10 @@
  */
 
 import { readFileSync } from 'fs';
-import { Lifecycle, Request, ResponseToolkit, Server, ServerOptions } from 'hapi';
+import { Lifecycle, Request, ResponseToolkit, Server, ServerOptions, Util } from 'hapi';
+import Hoek from 'hoek';
 import { ServerOptions as TLSOptions } from 'https';
+import { ValidationError } from 'joi';
 import { HttpConfig } from './http_config';
 
 /**
@@ -93,6 +95,25 @@ export function createServer(options: ServerOptions) {
 }
 
 /**
+ * Hapi extends the ValidationError interface to add this output key with more data.
+ */
+export interface HapiValidationError extends ValidationError {
+  output: {
+    statusCode: number;
+    headers: Util.Dictionary<string | string[]>;
+    payload: {
+      statusCode: number;
+      error: string;
+      message?: string;
+      validation: {
+        source: string;
+        keys: string[];
+      };
+    };
+  };
+}
+
+/**
  * Used to replicate Hapi v16 and below's validation responses. Should be used in the routes.validate.failAction key.
  */
 export function defaultValidationErrorHandler(
@@ -100,5 +121,27 @@ export function defaultValidationErrorHandler(
   h: ResponseToolkit,
   err?: Error
 ): Lifecycle.ReturnValue {
+  // Newer versions of Joi don't format the key for missing params the same way. This shim
+  // provides backwards compatibility. Unfortunately, Joi doesn't export it's own Error class
+  // in JS so we have to rely on the `name` key before we can cast it.
+  //
+  // The Hapi code we're 'overwriting' can be found here:
+  //     https://github.com/hapijs/hapi/blob/master/lib/validation.js#L102
+  if (err && err.name === 'ValidationError' && err.hasOwnProperty('output')) {
+    const validationError: HapiValidationError = err as HapiValidationError;
+    const validationKeys: string[] = [];
+
+    validationError.details.forEach(detail => {
+      if (detail.path.length > 0) {
+        validationKeys.push(Hoek.escapeHtml(detail.path.join('.')));
+      } else {
+        // If no path, use the value sigil to signal the entire value had an issue.
+        validationKeys.push('value');
+      }
+    });
+
+    validationError.output.payload.validation.keys = validationKeys;
+  }
+
   throw err;
 }
