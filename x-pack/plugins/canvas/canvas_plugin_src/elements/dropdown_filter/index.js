e773fc35c0135feb96cd3dcd80701dc8d82cb97b
Alexandr Ogarkov <nox.911@gmail.com>
2018-10-03 16:49:52 +0300

    translate canvas canvas_plugin_src elements

--- x-pack/plugins/canvas/canvas_plugin_src/elements/dropdown_filter/index.js
+++ x-pack/plugins/canvas/canvas_plugin_src/elements/dropdown_filter/index.js
@@ -4,12 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { i18n } from '@kbn/i18n';
 import header from './header.png';
 
 export const dropdownFilter = () => ({
   name: 'dropdown_filter',
-  displayName: 'Dropdown filter',
-  help: 'A dropdown from which you can select values for an "exactly" filter',
+  displayName: i18n.translate('xpack.canvas.elements.dropdownFilterDisplayName', {
+    defaultMessage: 'Dropdown filter',
+  }),
+  help: i18n.translate('xpack.canvas.elements.dropdownFilterHelpText', {
+    defaultMessage: 'A dropdown from which you can select values for an "exactly" filter',
+  }),
   image: header,
   height: 50,
   expression: `demodata
--- x-pack/plugins/canvas/canvas_plugin_src/elements/metric/index.js
+++ x-pack/plugins/canvas/canvas_plugin_src/elements/metric/index.js
@@ -4,21 +4,26 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { i18n } from '@kbn/i18n';
 import { openSans } from '../../../common/lib/fonts';
 import header from './header.png';
 
 export const metric = () => ({
   name: 'metric',
-  displayName: 'Metric',
-  help: 'A number with a label',
+  displayName: i18n.translate('xpack.canvas.elements.metricDisplayName', {
+    defaultMessage: 'Metric',
+  }),
+  help: i18n.translate('xpack.canvas.elements.metricHelpText', {
+    defaultMessage: 'A number with a label',
+  }),
   width: 200,
   height: 100,
   image: header,
   expression: `filters
 | demodata
 | math "unique(country)"
-| metric "Countries" 
-  metricFont={font size=48 family="${openSans.value}" color="#000000" align="center" lHeight=48} 
+| metric "Countries"
+  metricFont={font size=48 family="${openSans.value}" color="#000000" align="center" lHeight=48}
   labelFont={font size=14 family="${openSans.value}" color="#000000" align="center"}
 | render`,
 });
