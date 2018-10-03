352c2197694a57619c12f4bffd522bdb807f7bc5
Ryan Keairns <contactryank@gmail.com>
2018-10-03 09:50:45 -0500

    removes unused less styles

--- x-pack/plugins/logstash/public/sections/pipeline_edit/components/pipeline_edit/pipeline_edit.js
+++ x-pack/plugins/logstash/public/sections/pipeline_edit/components/pipeline_edit/pipeline_edit.js
@@ -12,7 +12,6 @@ import { Notifier, toastNotifications } from 'ui/notify';
 import { PipelineEditor } from '../../../../components/pipeline_editor';
 import 'plugins/logstash/services/license';
 import 'plugins/logstash/services/security';
-import './pipeline_edit.less';
 import 'ace';
 
 const app = uiModules.get('xpack/logstash');
