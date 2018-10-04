bc1405b7d61144d5b234dd91a5fb2f716c1c4cdf
Chris Roberson <chrisronline@gmail.com>
2018-10-04 13:36:51 -0400

    Backport #22998 (#23689)

--- src/core_plugins/kibana/server/tutorials/haproxy_metrics/index.js
+++ src/core_plugins/kibana/server/tutorials/haproxy_metrics/index.js
@@ -21,7 +21,7 @@ import { i18n }  from '@kbn/i18n';
 import { TUTORIAL_CATEGORY } from '../../../common/tutorials/tutorial_category';
 import { onPremInstructions, cloudInstructions, onPremCloudInstructions } from '../../../common/tutorials/metricbeat_instructions';
 
-export function haproxyMetricsSpecProvider() {
+export function haproxyMetricsSpecProvider(server, context) {
   const moduleName = 'haproxy';
   return {
     id: 'haproxyMetrics',
@@ -53,7 +53,7 @@ export function haproxyMetricsSpecProvider() {
       }
     },
     completionTimeMinutes: 10,
-    onPrem: onPremInstructions(moduleName),
+    onPrem: onPremInstructions(moduleName, null, null, null, context),
     elasticCloud: cloudInstructions(moduleName),
     onPremElasticCloud: onPremCloudInstructions(moduleName)
   };
