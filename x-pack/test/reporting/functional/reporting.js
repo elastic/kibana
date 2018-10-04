95e53c947344fe0c58e75fda344e615651ae5d30
kobelb <brandon.kobel@elastic.co>
2018-10-04 12:05:39 -0700

    Reenabling Preserve Layout reporting tests

--- x-pack/test/reporting/functional/reporting.js
+++ x-pack/test/reporting/functional/reporting.js
@@ -153,7 +153,7 @@ export default function ({ getService, getPageObjects }) {
         });
       });
 
-      describe.skip('Preserve Layout', () => {
+      describe('Preserve Layout', () => {
         it('matches baseline report', async function () {
 
           // Generating and then comparing reports can take longer than the default 60s timeout because the comparePngs
