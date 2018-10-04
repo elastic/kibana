dd1f602f4c125769270ccd93173cdae923dc567a
Chris Cowan <chris@chriscowan.us>
2018-10-04 09:52:30 -0700

    [Infra UI] Use link-to service for detail links (#23665)
    * [Infra UI] Use link-to service for detail links
    
    This PR used the link-to route for the detail page links for hosts, pods
    and containers.
    
    * Simplifying redirects

--- x-pack/plugins/infra/public/pages/link_to/index.ts
+++ x-pack/plugins/infra/public/pages/link_to/index.ts
@@ -8,3 +8,6 @@ export { LinkToPage } from './link_to';
 export { getContainerLogsUrl, RedirectToContainerLogs } from './redirect_to_container_logs';
 export { getHostLogsUrl, RedirectToHostLogs } from './redirect_to_host_logs';
 export { getPodLogsUrl, RedirectToPodLogs } from './redirect_to_pod_logs';
+export { getHostDetailUrl, RedirectToHostDetail } from './redirect_to_host_detail';
+export { getContainerDetailUrl, RedirectToContainerDetail } from './redirect_to_container_detail';
+export { getPodDetailUrl, RedirectToPodDetail } from './redirect_to_pod_detail';
