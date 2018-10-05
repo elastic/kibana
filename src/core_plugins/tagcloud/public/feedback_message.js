c5bbb41bd68177897c96f72745c83d71b4cccb17
maryia-lapata <mary.lopato@gmail.com>
2018-10-05 08:42:39 +0300

    Update ids

--- src/core_plugins/tagcloud/public/feedback_message.js
+++ src/core_plugins/tagcloud/public/feedback_message.js
@@ -32,13 +32,13 @@ export class FeedbackMessage extends Component {
       <div className="tagcloud-notifications" >
         <div className="tagcloud-truncated-message" style={{ display: this.state.shouldShowTruncate ? 'block' : 'none' }}>
           <FormattedMessage
-            id="tagCloud.public.feedbackMessage.truncatedTagsDescription"
+            id="tagCloud.feedbackMessage.truncatedTagsDescription"
             defaultMessage="The number of tags has been truncated to avoid long draw times."
           />
         </div>
         <div className="tagcloud-incomplete-message" style={{ display: this.state.shouldShowIncomplete ? 'block' : 'none' }}>
           <FormattedMessage
-            id="tagCloud.public.feedbackMessage.containerDescription"
+            id="tagCloud.feedbackMessage.tooSmallContainerDescription"
             defaultMessage="The container is too small to display the entire cloud. Tags might be cropped or omitted."
           />
         </div>
