5ee8c1d94e3d68c700fbec123506b54055d25f7b
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2018-10-15 12:20:48 +0200

    Cache and report access counts for 24 hours

--- x-pack/plugins/infra/server/usage/usage_collector.ts
+++ x-pack/plugins/infra/server/usage/usage_collector.ts
@@ -9,6 +9,13 @@ import { KbnServer } from '../kibana.index';
 
 const KIBANA_REPORTING_TYPE = 'infraops';
 
+interface InfraopsSum {
+  infraopsHosts: number;
+  infraopsDocker: number;
+  infraopsKubernetes: number;
+  logs: number;
+}
+
 export class UsageCollector {
   public static getUsageCollector(server: KbnServer) {
     const { collectorSet } = server.usage;
@@ -16,46 +23,97 @@ export class UsageCollector {
     return collectorSet.makeUsageCollector({
       type: KIBANA_REPORTING_TYPE,
       fetch: async () => {
-        const report = {
-          hits: {
-            infraops_hosts: this.infraopsHosts,
-            infraops_docker: this.infraopsDocker,
-            infraops_kubernetes: this.infraopsKubernetes,
-            logs: this.logs + 0,
-          },
-        };
-        this.resetCounters();
+        const report = this.getReport();
+        // tslint:disable:no-console
+        console.log('counters: ', this.counters);
+        console.log('number of buckets: ', Object.keys(this.counters).length);
+        console.log('report:', report);
         return report;
       },
     });
   }
 
   public static countNode(nodeType: InfraNodeType) {
+    const bucket = this.getBucket();
+    this.maybeInitializeBucket(bucket);
+
     switch (nodeType) {
       case InfraNodeType.pod:
-        this.infraopsKubernetes += 1;
+        this.counters[bucket].infraopsKubernetes += 1;
         break;
       case InfraNodeType.container:
-        this.infraopsDocker += 1;
+        this.counters[bucket].infraopsDocker += 1;
         break;
       default:
-        this.infraopsHosts += 1;
+        this.counters[bucket].infraopsHosts += 1;
     }
   }
 
   public static countLogs() {
-    this.logs += 1;
+    const bucket = this.getBucket();
+    this.maybeInitializeBucket(bucket);
+    this.counters[bucket].logs += 1;
   }
 
-  private static infraopsHosts = 0;
-  private static infraopsDocker = 0;
-  private static infraopsKubernetes = 0;
-  private static logs = 0;
+  private static counters: any = {};
+  private static BUCKET_SIZE = 3600; // seconds in an hour
+  private static BUCKET_NUMBER = 24; // report the last 24 hours
+  // private static BUCKET_SIZE = 20; // a real small bucket size for testing
+  // private static BUCKET_NUMBER = 3; // report the last minute
 
-  private static resetCounters() {
-    this.infraopsHosts = 0;
-    this.infraopsDocker = 0;
-    this.infraopsKubernetes = 0;
-    this.logs = 0;
+  private static getBucket() {
+    const now = Math.floor(Date.now() / 1000);
+    return now - (now % this.BUCKET_SIZE);
+  }
+
+  private static maybeInitializeBucket(bucket: any) {
+    if (!this.counters[bucket]) {
+      this.counters[bucket] = {
+        infraopsHosts: 0,
+        infraopsDocker: 0,
+        infraopsKubernetes: 0,
+        logs: 0,
+      };
+    }
+  }
+
+  private static getReport() {
+    const keys = Object.keys(this.counters);
+    // only keep the newest BUCKET_NUMBER buckets
+    keys.forEach(key => {
+      if (parseInt(key, 10) < this.getBucket() - this.BUCKET_SIZE * (this.BUCKET_NUMBER - 1)) {
+        delete this.counters[key];
+      }
+    });
+
+    // all remaining buckets are current
+    const sums = Object.keys(this.counters).reduce(
+      (a: InfraopsSum, b: any) => {
+        const key = parseInt(b, 10);
+        return {
+          infraopsHosts: a.infraopsHosts + this.counters[key].infraopsHosts,
+          infraopsDocker: a.infraopsDocker + this.counters[key].infraopsDocker,
+          infraopsKubernetes: a.infraopsKubernetes + this.counters[key].infraopsKubernetes,
+          logs: a.logs + this.counters[key].logs,
+        };
+      },
+      {
+        infraopsHosts: 0,
+        infraopsDocker: 0,
+        infraopsKubernetes: 0,
+        logs: 0,
+      }
+    );
+
+    return {
+      last_24hours: {
+        hits: {
+          infraops_hosts: sums.infraopsHosts,
+          infraops_docker: sums.infraopsDocker,
+          infraops_kubernetes: sums.infraopsKubernetes,
+          logs: sums.logs,
+        },
+      },
+    };
   }
 }
