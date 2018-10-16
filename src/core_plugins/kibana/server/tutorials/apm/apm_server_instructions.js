34340f0587a6fdd01c224d8e711856771fb4b73a
Gil Raphaelli <g@raphaelli.com>
2018-10-16 17:59:27 -0400

    update apm-server start instructions for windows

--- src/core_plugins/kibana/server/tutorials/apm/apm_server_instructions.js
+++ src/core_plugins/kibana/server/tutorials/apm/apm_server_instructions.js
@@ -138,7 +138,7 @@ to allow the script to run. For example: {command}.',
     {
       title: START_SERVER.title,
       textPre: START_SERVER.textPre,
-      commands: ['apm-server.exe -e'],
+      commands: ['Start-Service apm-server'],
     },
   ];
 }
