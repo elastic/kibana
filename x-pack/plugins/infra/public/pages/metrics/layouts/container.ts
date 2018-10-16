f4e6cf11fb40fea6e2c1b5b87f9c1a5a7093e800
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2018-10-16 11:07:36 +0200

    Remove unused 'requires' on layouts.

--- x-pack/plugins/infra/public/pages/metrics/layouts/container.ts
+++ x-pack/plugins/infra/public/pages/metrics/layouts/container.ts
@@ -16,7 +16,6 @@ export const containerLayoutCreator: InfraMetricLayoutCreator = theme => [
   {
     id: 'containerOverview',
     label: 'Container Overview',
-    requires: ['docker'],
     sections: [
       {
         id: InfraMetric.containerOverview,
