f7074b1f2ed598624b46e04c615fa9568bbd51a4
Caroline Horn <549577+cchaos@users.noreply.github.com>
2018-10-17 12:50:55 -0400

    Vis types LESS to SASS (#23513)
    Also fixes #23969

--- src/core_plugins/tagcloud/public/feedback_message.js
+++ src/core_plugins/tagcloud/public/feedback_message.js
@@ -29,14 +29,14 @@ export class FeedbackMessage extends Component {
 
   render() {
     return (
-      <div className="tagcloud-notifications" >
-        <div className="tagcloud-truncated-message" style={{ display: this.state.shouldShowTruncate ? 'block' : 'none' }}>
+      <div className="tgcVisFeedback" >
+        <div className="tgcVisFeedback__message" style={{ display: this.state.shouldShowTruncate ? 'block' : 'none' }}>
           <FormattedMessage
             id="tagCloud.feedbackMessage.truncatedTagsDescription"
             defaultMessage="The number of tags has been truncated to avoid long draw times."
           />
         </div>
-        <div className="tagcloud-incomplete-message" style={{ display: this.state.shouldShowIncomplete ? 'block' : 'none' }}>
+        <div className="ttgcVisFeedback__message" style={{ display: this.state.shouldShowIncomplete ? 'block' : 'none' }}>
           <FormattedMessage
             id="tagCloud.feedbackMessage.tooSmallContainerDescription"
             defaultMessage="The container is too small to display the entire cloud. Tags might be cropped or omitted."
