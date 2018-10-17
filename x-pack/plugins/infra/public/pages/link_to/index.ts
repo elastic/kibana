db8b86538a4b85586f06687892ba8e6cbc5c89a8
Felix Stürmer <weltenwort@users.noreply.github.com>
2018-10-17 17:36:45 +0200

    [Infra UI] Merge InfraOps feature branch (#24068) (#24127)

---
+++ x-pack/plugins/infra/public/pages/link_to/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export { LinkToPage } from './link_to';
+export { getContainerLogsUrl, RedirectToContainerLogs } from './redirect_to_container_logs';
+export { getHostLogsUrl, RedirectToHostLogs } from './redirect_to_host_logs';
+export { getPodLogsUrl, RedirectToPodLogs } from './redirect_to_pod_logs';
+export { getHostDetailUrl, RedirectToHostDetail } from './redirect_to_host_detail';
+export { getContainerDetailUrl, RedirectToContainerDetail } from './redirect_to_container_detail';
+export { getPodDetailUrl, RedirectToPodDetail } from './redirect_to_pod_detail';
