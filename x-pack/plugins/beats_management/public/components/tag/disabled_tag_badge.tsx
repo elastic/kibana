62e36f102cce0ae95e398ec490b28e4fbef7d7ed
Justin Kambic <justin.kambic@elastic.co>
2018-10-18 11:36:00 -0400

    Remove unused import.

--- x-pack/plugins/beats_management/public/components/tag/disabled_tag_badge.tsx
+++ x-pack/plugins/beats_management/public/components/tag/disabled_tag_badge.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiBadge, EuiToolTip } from '@elastic/eui';
+import { EuiBadge } from '@elastic/eui';
 import React from 'react';
 import { TABLE_CONFIG } from '../../../common/constants';
 
