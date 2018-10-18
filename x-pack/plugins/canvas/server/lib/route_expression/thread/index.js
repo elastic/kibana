a6e680c1647c6a214825a4e64a14d1d8617b6539
Rashid Khan <github.fliplap@spamgourmet.com>
2018-10-18 11:03:49 -0700

    Restart worker as needed

--- x-pack/plugins/canvas/server/lib/route_expression/thread/index.js
+++ x-pack/plugins/canvas/server/lib/route_expression/thread/index.js
@@ -14,91 +14,81 @@ const serialization = populateServerRegistries(['types']).then(({ types }) =>
   serializeProvider(types.toJS())
 );
 
+const WORKER_TIMEOUT = 10000;
+
 const heap = {};
-let pingCount = 0;
-let workerCount = 0;
+let worker = null;
 
-function startWorker() {
-  if (global._canvasWorker) return;
-  workerCount++;
-  console.log('STARTING WORKER', workerCount, 'MAIN PID', process.pid);
-  // Start our workers in global space
+function getWorker() {
+  if (worker) return worker;
   const workerPath = resolve(__dirname, 'babeled.js');
-  global._canvasWorker = fork(workerPath, {});
+  worker = fork(workerPath, {});
 
-  global._canvasWorker.on('message', msg => {
+  // 'exit' happens whether we kill the worker or it just dies. No need to look for 'error', our worker is intended to be long lived so it isn't running, it's an issue
+  worker.on('exit', () => {
+    worker = null;
+    worker = getWorker();
+  });
+
+  worker.on('message', msg => {
     const { type, value, id } = msg;
-    if (type === 'pong') {
-      console.log('GOT PONG', new Date());
-      pingCount--;
-    }
-
     if (type === 'run') {
       // TODO: deserialize
       const { threadId } = msg;
       const { ast, context } = value;
       heap[threadId].onFunctionNotFound(ast, context).then(value => {
-        global._canvasWorker.send({ type: 'result', id, value: value });
+        worker.send({ type: 'result', id, value: value });
       });
     }
     if (type === 'result') {
-      heap[id].resolve(value);
+      if (!heap[id]) console.log('Got response from timed out request', id);
+      else heap[id].resolve(value);
+
       delete heap[id];
     }
 
     if (type === 'error') throw new Error(value);
   });
+  return worker;
 }
-startWorker();
-
-// This only needs to happen once, so it's fine if we don't re-do it after starting the first worker
-const functionList = new Promise(resolve => {
-  global._canvasWorker.send({ type: 'getFunctions' });
-  global._canvasWorker.on('message', msg => {
-    if (msg.type === 'functionList') resolve(msg.value);
-  });
-});
-
-function ping() {
-  if (pingCount > 1) {
-    pingCount--;
-    console.log('KILLING WORKER');
-    global._canvasWorker.kill('SIGINT');
-    global._canvasWorker = null;
-    startWorker();
-    /*
-      worker fail
-      - empty heap,
-      - fail all expressions
-      - respawn worker
-    */
-  }
-  setTimeout(() => {
-    global._canvasWorker.send({ type: 'ping' });
-    pingCount++;
-    ping();
-  }, 5000);
-}
-ping();
 
 // Tip: This can return a promise
 export const thread = ({ onFunctionNotFound }) => {
+  const functionList = new Promise(resolve => {
+    const worker = getWorker();
+    worker.send({ type: 'getFunctions' });
+    worker.on('message', msg => {
+      if (msg.type === 'functionList') resolve(msg.value);
+    });
+  });
   return Promise.all([functionList, serialization]).then(([functions, serialization]) => {
-    // Start the worker down here so importing doesn't immediately cause it to start?
-
     const { serialize, deserialize } = serialization;
 
     return {
       interpret: (ast, context) => {
+        const worker = getWorker();
         const id = uuid();
-        global._canvasWorker.send({ type: 'run', id, value: { ast, context: serialize(context) } });
+        worker.send({ type: 'run', id, value: { ast, context: serialize(context) } });
 
         return new Promise(resolve => {
           heap[id] = {
+            time: new Date().getTime(),
             resolve: value => resolve(deserialize(value)),
             onFunctionNotFound: (ast, context) =>
               onFunctionNotFound(ast, deserialize(context)).then(serialize),
           };
+
+          //
+          setTimeout(() => {
+            if (!heap[id]) return; // Looks like this has already been cleared from the heap.
+            heap[id].resolve({
+              type: 'error',
+              error: {
+                message: `Request timed out.`,
+              },
+            });
+            if (worker) worker.kill();
+          }, WORKER_TIMEOUT);
         });
       },
 
