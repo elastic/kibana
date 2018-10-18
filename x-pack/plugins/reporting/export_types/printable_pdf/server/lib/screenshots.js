9f4ec18000a74e269276ff943979799ccbd4d950
Brandon Kobel <brandon.kobel@gmail.com>
2018-10-18 09:30:42 -0700

    Reporting cookies (#24177)
    * Switching Reporting to use session cookies explicitly
    
    * Fixing bug when security is explicitly disabled
    
    * Responding to feedback
    
    * Fixing yarn.lock

--- x-pack/plugins/reporting/export_types/printable_pdf/server/lib/screenshots.js
+++ x-pack/plugins/reporting/export_types/printable_pdf/server/lib/screenshots.js
@@ -29,11 +29,11 @@ export function screenshotsObservableFactory(server) {
     return result;
   };
 
-  const openUrl = async (browser, url, headers) => {
+  const openUrl = async (browser, url, sessionCookie) => {
     const waitForSelector = '.application';
 
     await browser.open(url, {
-      headers,
+      sessionCookie,
       waitForSelector,
     });
   };
@@ -223,7 +223,7 @@ export function screenshotsObservableFactory(server) {
     return screenshots;
   };
 
-  return function screenshotsObservable(url, headers, layout, browserTimezone) {
+  return function screenshotsObservable(url, sessionCookie, layout, browserTimezone) {
 
     return Rx.defer(async () => await getPort()).pipe(
       mergeMap(bridgePort => {
@@ -251,7 +251,7 @@ export function screenshotsObservableFactory(server) {
         const screenshot$ = driver$.pipe(
           tap(() => logger.debug(`opening ${url}`)),
           mergeMap(
-            browser => openUrl(browser, url, headers),
+            browser => openUrl(browser, url, sessionCookie),
             browser => browser
           ),
           tap(() => logger.debug('injecting custom css')),
