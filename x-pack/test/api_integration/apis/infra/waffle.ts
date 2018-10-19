08abe23e11aff26791e91adecec14985d7a0d6eb
Chris Cowan <chris@chriscowan.us>
2018-10-19 08:06:15 -0700

    renaming from infraops to infra

--- x-pack/test/api_integration/apis/infra/waffle.ts
+++ x-pack/test/api_integration/apis/infra/waffle.ts
@@ -15,8 +15,8 @@ const waffleTests: KbnTestProvider = ({ getService }) => {
   const client = getService('infraOpsGraphQLClient');
 
   describe('waffle nodes', () => {
-    before(() => esArchiver.load('infraops'));
-    after(() => esArchiver.unload('infraops'));
+    before(() => esArchiver.load('infra'));
+    after(() => esArchiver.unload('infra'));
 
     it('should basically work', () => {
       return client
