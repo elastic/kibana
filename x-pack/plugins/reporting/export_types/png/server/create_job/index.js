35ebc4f370621c051c9479756993aaf1d3d9d7de
Brian <gaddisb@gmail.com>
2018-10-21 08:09:28 -0400

    Merge of security changes for PNG reporting

--- x-pack/plugins/reporting/export_types/png/server/create_job/index.js
+++ x-pack/plugins/reporting/export_types/png/server/create_job/index.js
@@ -16,8 +16,10 @@ function createJobFn(server) {
     relativeUrl,
     browserTimezone,
     layout
-  }, headers) {
+  }, headers, serializedSession, request) {
     const serializedEncryptedHeaders = await crypto.encrypt(headers);
+    const encryptedSerializedSession = await crypto.encrypt(serializedSession);
+
 
     return {
       type: objectType,
@@ -25,7 +27,9 @@ function createJobFn(server) {
       relativeUrl,
       headers: serializedEncryptedHeaders,
       browserTimezone,
+      session: encryptedSerializedSession,
       layout,
+      basePath: request.getBasePath(),
       forceNow: new Date().toISOString(),
     };
   };
