3c41fea6575db7e1287710ad5399b76b6ba9a234
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-10-22 11:09:20 +0200

    Remove `translateAgentName` method

--- x-pack/plugins/apm/public/utils/documentation/agents.ts
+++ x-pack/plugins/apm/public/utils/documentation/agents.ts
@@ -4,12 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-const APM_URL_ROOT = 'https://www.elastic.co/guide/en/apm';
+const AGENT_URL_ROOT = 'https://www.elastic.co/guide/en/apm/agent';
 
 // TODO: currently unused but should be added to timeline view
 export const APM_AGENT_DROPPED_SPANS_DOCS = {
-  nodejs: `${APM_URL_ROOT}/agent/nodejs/1.x/agent-api.html#transaction-max-spans`,
-  python: `${APM_URL_ROOT}/agent/python/2.x/configuration.html#config-transaction-max-spans`
+  nodejs: `${AGENT_URL_ROOT}/nodejs/1.x/agent-api.html#transaction-max-spans`,
+  python: `${AGENT_URL_ROOT}/python/2.x/configuration.html#config-transaction-max-spans`
 };
 
 const APM_AGENT_FEATURE_DOCS: {
@@ -18,42 +18,33 @@ const APM_AGENT_FEATURE_DOCS: {
   };
 } = {
   user: {
-    nodejs: `${APM_URL_ROOT}/agent/nodejs/1.x/agent-api.html#apm-set-user-context`,
-    python: `${APM_URL_ROOT}/agent/python/2.x/api.html#api-set-user-context`,
-    ruby: `${APM_URL_ROOT}/agent/ruby/1.x/advanced.html#_providing_info_about_the_user`,
-    javascript: `${APM_URL_ROOT}/agent/js-base/0.x/api.html#apm-set-user-context`
+    nodejs: `${AGENT_URL_ROOT}/nodejs/1.x/agent-api.html#apm-set-user-context`,
+    python: `${AGENT_URL_ROOT}/python/2.x/api.html#api-set-user-context`,
+    ruby: `${AGENT_URL_ROOT}/ruby/1.x/advanced.html#_providing_info_about_the_user`,
+    'js-react': `${AGENT_URL_ROOT}/js-base/0.x/api.html#apm-set-user-context`,
+    'js-base': `${AGENT_URL_ROOT}/js-base/0.x/api.html#apm-set-user-context`
   },
   tags: {
-    nodejs: `${APM_URL_ROOT}/agent/nodejs/1.x/agent-api.html#apm-set-tag`,
-    python: `${APM_URL_ROOT}/agent/python/2.x/api.html#api-tag`,
-    ruby: `${APM_URL_ROOT}/agent/ruby/1.x/advanced.html#_adding_tags`,
-    javascript: `${APM_URL_ROOT}/agent/js-base/0.x/api.html#apm-set-tags`
+    nodejs: `${AGENT_URL_ROOT}/nodejs/1.x/agent-api.html#apm-set-tag`,
+    python: `${AGENT_URL_ROOT}/python/2.x/api.html#api-tag`,
+    ruby: `${AGENT_URL_ROOT}/ruby/1.x/advanced.html#_adding_tags`,
+    'js-react': `${AGENT_URL_ROOT}/js-base/0.x/api.html#apm-set-tags`,
+    'js-base': `${AGENT_URL_ROOT}/js-base/0.x/api.html#apm-set-tags`
   },
   custom: {
-    nodejs: `${APM_URL_ROOT}/agent/nodejs/1.x/agent-api.html#apm-set-custom-context`,
-    python: `${APM_URL_ROOT}/agent/python/2.x/api.html#api-set-custom-context`,
-    ruby: `${APM_URL_ROOT}/agent/ruby/1.x/advanced.html#_adding_custom_context`,
-    javascript: `${APM_URL_ROOT}/agent/js-base/0.x/api.html#apm-set-custom-context`
+    nodejs: `${AGENT_URL_ROOT}/nodejs/1.x/agent-api.html#apm-set-custom-context`,
+    python: `${AGENT_URL_ROOT}/python/2.x/api.html#api-set-custom-context`,
+    ruby: `${AGENT_URL_ROOT}/ruby/1.x/advanced.html#_adding_custom_context`,
+    'js-react': `${AGENT_URL_ROOT}/js-base/0.x/api.html#apm-set-custom-context`,
+    'js-base': `${AGENT_URL_ROOT}/js-base/0.x/api.html#apm-set-custom-context`
   }
 };
-
-function translateAgentName(agentName?: string) {
-  switch (agentName) {
-    case 'js-react':
-    case 'js-base':
-      return 'javascript';
-
-    default:
-      return agentName;
-  }
-}
 
 export function getAgentFeatureDocsUrl(
   featureName: string,
   agentName?: string
 ) {
-  const translatedAgentName = translateAgentName(agentName);
-  if (APM_AGENT_FEATURE_DOCS[featureName] && translatedAgentName) {
-    return APM_AGENT_FEATURE_DOCS[featureName][translatedAgentName];
+  if (APM_AGENT_FEATURE_DOCS[featureName] && agentName) {
+    return APM_AGENT_FEATURE_DOCS[featureName][agentName];
   }
 }
