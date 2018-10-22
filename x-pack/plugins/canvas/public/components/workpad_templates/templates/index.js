520f7423b10ae06c5bbafc2acee134243f56799b
Ryan Keairns <contactryank@gmail.com>
2018-10-22 15:56:00 -0500

    refine list of initial templates

--- x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
+++ x-pack/plugins/canvas/public/components/workpad_templates/templates/index.js
@@ -4,26 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-const canvasWeather = require('./canvas_weather.json');
-const conditionalAssets = require('./conditional_assets.json');
-const dublinDemo = require('./dublin_eah_workshop.json');
 const ecommerceRevenueTracking = require('./ecommerce_revenue_tracking.json');
-const elasticTemplate = require('./elastic_template.json');
 const flightsOverview = require('./flights_overview.json');
 const logsWebTraffic = require('./logs_web_traffic.json');
-const markdownStyles = require('./markdown_styles.json');
-const operationalMonitoring = require('./sample_data_operational_monitoring.json');
-const randomizedTour2018 = require('./randomized_tour_2018.json');
+const themeDark = require('./theme_dark.json');
+const themeLight = require('./theme_light.json');
 
 export const templates = [
-  canvasWeather,
-  conditionalAssets,
-  dublinDemo,
   ecommerceRevenueTracking,
-  elasticTemplate,
   flightsOverview,
   logsWebTraffic,
-  markdownStyles,
-  operationalMonitoring,
-  randomizedTour2018,
+  themeDark,
+  themeLight,
 ];
