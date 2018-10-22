4016572ba52e66c7ed323f9920c55511a45084d4
Matt Bargar <mbargar@gmail.com>
2018-10-22 11:14:54 -0400

    Add telemetry for KQL (#23547)
    This PR adds two usage stats to our telemetry for KQL:
    
    * How many times people click the opt in/out toggle in the query bar UI
    * Which language Kibana admins have set as the global default in advanced settings

--- src/core_plugins/kibana/server/lib/kql_usage_collector/index.js
+++ src/core_plugins/kibana/server/lib/kql_usage_collector/index.js
@@ -17,20 +17,4 @@
  * under the License.
  */
 
-export default function ({ loadTestFile }) {
-  describe('apis', () => {
-    loadTestFile(require.resolve('./elasticsearch'));
-    loadTestFile(require.resolve('./general'));
-    loadTestFile(require.resolve('./home'));
-    loadTestFile(require.resolve('./index_patterns'));
-    loadTestFile(require.resolve('./kql_telemetry'));
-    loadTestFile(require.resolve('./management'));
-    loadTestFile(require.resolve('./saved_objects'));
-    loadTestFile(require.resolve('./scripts'));
-    loadTestFile(require.resolve('./search'));
-    loadTestFile(require.resolve('./shorten'));
-    loadTestFile(require.resolve('./suggestions'));
-    loadTestFile(require.resolve('./status'));
-    loadTestFile(require.resolve('./stats'));
-  });
-}
+export { makeKQLUsageCollector } from './make_kql_usage_collector';
--- test/api_integration/apis/kql_telemetry/index.js
+++ test/api_integration/apis/kql_telemetry/index.js
@@ -18,19 +18,7 @@
  */
 
 export default function ({ loadTestFile }) {
-  describe('apis', () => {
-    loadTestFile(require.resolve('./elasticsearch'));
-    loadTestFile(require.resolve('./general'));
-    loadTestFile(require.resolve('./home'));
-    loadTestFile(require.resolve('./index_patterns'));
+  describe('KQL', () => {
     loadTestFile(require.resolve('./kql_telemetry'));
-    loadTestFile(require.resolve('./management'));
-    loadTestFile(require.resolve('./saved_objects'));
-    loadTestFile(require.resolve('./scripts'));
-    loadTestFile(require.resolve('./search'));
-    loadTestFile(require.resolve('./shorten'));
-    loadTestFile(require.resolve('./suggestions'));
-    loadTestFile(require.resolve('./status'));
-    loadTestFile(require.resolve('./stats'));
   });
 }
