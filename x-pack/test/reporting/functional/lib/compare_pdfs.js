519d7c9e6c48229877d012bb08f09d1622cd941f
Stacey Gammon <gammon@elastic.co>
2018-10-23 13:22:55 -0400

    Don’t use density option

--- x-pack/test/reporting/functional/lib/compare_pdfs.js
+++ x-pack/test/reporting/functional/lib/compare_pdfs.js
@@ -105,7 +105,7 @@ export async function checkIfPdfsMatch(actualPdfPath, baselinePdfPath, screensho
   fs.writeFileSync(actualCopyPath, fs.readFileSync(actualPdfPath));
 
   const convertOptions = {
-    '-density': '300',
+    // '-density': '300',
   };
   const actualPdfImage = new PDFImage(actualCopyPath, { convertOptions });
   const expectedPdfImage = new PDFImage(baselineCopyPath, { convertOptions });
