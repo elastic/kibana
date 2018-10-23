92d6ed34af37b3c42ebba83960ede2df56e8f925
Larry Gregory <larry.gregory@elastic.co>
2018-10-23 08:41:23 -0400

    Resolve known APIs

--- src/core_plugins/console/api_server/server.js
+++ src/core_plugins/console/api_server/server.js
@@ -1,12 +1,14 @@
 let _ = require("lodash");
-
+const KNOWN_APIS = ['es_5_0'];
 module.exports.resolveApi = function (sense_version, apis, reply) {
   let result = {};
   _.each(apis, function (name) {
     {
-      // for now we ignore sense_version. might add it in the api name later
-      let api = require('./' + name);
-      result[name] = api.asJson();
+      if (KNOWN_APIS.includes(name)) {
+        // for now we ignore sense_version. might add it in the api name later
+        let api = require('./' + name);
+        result[name] = api.asJson();
+      }
     }
   });
 
