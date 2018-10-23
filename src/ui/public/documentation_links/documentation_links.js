96c86f5ca9864956ecf783c8845e1b61c69f9258
Matt Bargar <matt.bargar@elastic.co>
2018-10-23 17:11:26 -0400

    Fix broken documentation links

--- src/ui/public/documentation_links/documentation_links.js
+++ src/ui/public/documentation_links/documentation_links.js
@@ -3,12 +3,13 @@ import { metadata } from '../metadata';
 const urlVersion = metadata.branch;
 const baseUrl = 'https://www.elastic.co/';
 
+const PAINLESS_DOCS = `${baseUrl}guide/en/elasticsearch/painless/${urlVersion}/`;
+
 export const documentationLinks = {
   filebeat: {
     installation: `${baseUrl}guide/en/beats/filebeat/${urlVersion}/filebeat-installation.html`,
     configuration: `${baseUrl}guide/en/beats/filebeat/${urlVersion}/filebeat-configuration.html`,
     elasticsearchOutput: `${baseUrl}guide/en/beats/filebeat/${urlVersion}/elasticsearch-output.html`,
-    elasticsearchOutputAnchorParameters: `${baseUrl}guide/en/beats/filebeat/${urlVersion}/elasticsearch-output.html#_parameters`,
     startup: `${baseUrl}guide/en/beats/filebeat/${urlVersion}/filebeat-starting.html`,
     exportedFields: `${baseUrl}guide/en/beats/filebeat/${urlVersion}/exported-fields.html`
   },
@@ -16,7 +17,7 @@ export const documentationLinks = {
     scriptFields: `${baseUrl}guide/en/elasticsearch/reference/${urlVersion}/search-request-script-fields.html`,
     scriptAggs: `${baseUrl}guide/en/elasticsearch/reference/${urlVersion}/search-aggregations.html#_values_source`,
     painless: `${baseUrl}guide/en/elasticsearch/reference/${urlVersion}/modules-scripting-painless.html`,
-    painlessApi: `${baseUrl}guide/en/elasticsearch/reference/${urlVersion}/modules-scripting-painless.html#painless-api`,
+    painlessApi: `${PAINLESS_DOCS}painless-api-reference.html`,
     painlessSyntax: `${baseUrl}guide/en/elasticsearch/reference/${urlVersion}/modules-scripting-painless-syntax.html`,
     luceneExpressions: `${baseUrl}guide/en/elasticsearch/reference/${urlVersion}/modules-scripting-expression.html`
   },
