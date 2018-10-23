64f11992a67d9b5a0dd0e8d091aa197910302fd1
Stacey Gammon <gammon@elastic.co>
2018-10-23 07:38:51 -0400

    EAH - Typescript state_monitor_factory (#23945) (#24273)
    * Typescript state_monitor_factory
    
    * Fix linter error with possibly undefined
    
    * Expand typings to include hash stuff and expand the State type definition more.
    
    * Mark readonly

--- src/ui/public/state_management/state_hashing/get_unhashable_states_provider.ts
+++ src/ui/public/state_management/state_hashing/get_unhashable_states_provider.ts
@@ -16,6 +16,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { State } from './state';
 
-export type AppState = State;
+import { AppState } from '../app_state';
+import { GlobalState } from '../global_state';
+import { State } from '../state';
+
+export function getUnhashableStatesProvider(getAppState: () => AppState, globalState: GlobalState) {
+  return function getUnhashableStates(): State[] {
+    return [getAppState(), globalState].filter(Boolean);
+  };
+}
