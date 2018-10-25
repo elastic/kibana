4b17c442d49130142ef99686f27428c7f45586c9
James Gowdy <jgowdy@elastic.co>
2018-10-25 13:00:04 +0200

    [Ml] Fixing results for known fields

--- x-pack/plugins/ml/public/file_datavisualizer/components/fields_stats/fields_stats.js
+++ x-pack/plugins/ml/public/file_datavisualizer/components/fields_stats/fields_stats.js
@@ -10,6 +10,8 @@ import React, {
 } from 'react';
 
 import { FieldStatsCard } from './field_stats_card';
+import { getFieldNames } from './get_field_names';
+import { ML_JOB_FIELD_TYPES } from '../../../../common/constants/field_types';
 
 export class FieldsStats extends Component {
   constructor(props) {
@@ -46,26 +48,24 @@ function createFields(results) {
   const {
     mappings,
     field_stats: fieldStats,
-    column_names: columnNames,
     num_messages_analyzed: numMessagesAnalyzed,
     timestamp_field: timestampField,
   } = results;
 
   if (mappings && fieldStats) {
-    // if columnNames exists (i.e delimited) use it for the field list
-    // so we get the same order
-    const tempFields = (columnNames !== undefined) ? columnNames : Object.keys(fieldStats);
 
-    return tempFields.map((fName) => {
-      if (fieldStats[fName] !== undefined) {
-        const field = { name: fName };
-        const f  = fieldStats[fName];
-        const m  = mappings[fName];
+    const fieldNames = getFieldNames(results);
+
+    return fieldNames.map((name) => {
+      if (fieldStats[name] !== undefined) {
+        const field = { name };
+        const f  = fieldStats[name];
+        const m  = mappings[name];
 
         // sometimes the timestamp field is not in the mappings, and so our
         // collection of fields will be missing a time field with a type of date
-        if (fName === timestampField && field.type === undefined) {
-          field.type = 'date';
+        if (name === timestampField && field.type === undefined) {
+          field.type = ML_JOB_FIELD_TYPES.DATE;
         }
 
         if (f !== undefined) {
@@ -83,8 +83,15 @@ function createFields(results) {
 
         return field;
       } else {
+        // field is not in the field stats
+        // this could be a text field or a field which the endpoint has
+        // not been able to work out any information for
+        const type = (mappings[name] && mappings[name].type === ML_JOB_FIELD_TYPES.TEXT) ?
+          ML_JOB_FIELD_TYPES.TEXT : ML_JOB_FIELD_TYPES.UNKNOWN_TYPE;
+
         return {
-          name: fName,
+          name: name,
+          type,
           mean_value: 0,
           count: 0,
           cardinality: 0,
