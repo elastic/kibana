6287617c243f6592cab01832ac938a9235e1dbd0
kobelb <brandon.kobel@elastic.co>
2018-10-25 08:28:11 -0700

    Switching to using a n-api module

---
+++ x-pack/packages/kbn-krb5/src/binding.c
@@ -0,0 +1,23 @@
+#include <node_api.h>
+#include <assert.h>
+
+napi_value Method(napi_env env, napi_callback_info info) {
+  napi_status status;
+  napi_value world;
+  status = napi_create_string_utf8(env, "world", 5, &world);
+  assert(status == napi_ok);
+  return world;
+}
+
+#define DECLARE_NAPI_METHOD(name, func)                          \
+  { name, 0, func, 0, 0, 0, napi_default, 0 }
+
+napi_value Init(napi_env env, napi_value exports) {
+  napi_status status;
+  napi_property_descriptor desc = DECLARE_NAPI_METHOD("hello", Method);
+  status = napi_define_properties(env, exports, 1, &desc);
+  assert(status == napi_ok);
+  return exports;
+}
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init)
