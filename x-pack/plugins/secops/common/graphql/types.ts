0fc01afada86408bdd2b99c52b09b7441a9b5305
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2018-10-26 14:00:44 -0400

    prepend elastic license to generated file

--- x-pack/plugins/secops/common/graphql/types.ts
+++ x-pack/plugins/secops/common/graphql/types.ts
@@ -1,4 +1,9 @@
 /* tslint:disable */
+/*
+     * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+     * or more contributor license agreements. Licensed under the Elastic License;
+     * you may not use this file except in compliance with the Elastic License.
+     */
 import { GraphQLResolveInfo } from 'graphql';
 
 export type Resolver<Result, Parent = any, Context = any, Args = any> = (
