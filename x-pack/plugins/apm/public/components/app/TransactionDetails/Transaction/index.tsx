2515d7f0b1ac12e46c0e4da80a408f275f0b8df9
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2018-10-29 10:45:23 -0400

    Removes infra links and replaces context menu with single discover links for now

--- x-pack/plugins/apm/public/components/app/TransactionDetails/Transaction/index.tsx
+++ x-pack/plugins/apm/public/components/app/TransactionDetails/Transaction/index.tsx
@@ -19,7 +19,7 @@ import { Transaction as ITransaction } from '../../../../../typings/Transaction'
 import { IUrlParams } from '../../../../store/urlParams';
 import EmptyMessage from '../../../shared/EmptyMessage';
 import { TransactionLink } from '../../../shared/TransactionLink';
-import { ActionMenu } from './ActionMenu';
+import { DiscoverTransactionLink } from './ActionMenu';
 import { StickyTransactionProperties } from './StickyTransactionProperties';
 // @ts-ignore
 import { TransactionPropertiesTable } from './TransactionPropertiesTable';
@@ -89,7 +89,11 @@ export const Transaction: React.SFC<Props> = ({
         <EuiFlexItem>
           <EuiFlexGroup justifyContent="flexEnd">
             <EuiFlexItem grow={false}>
-              <ActionMenu transaction={transaction} />
+              <DiscoverTransactionLink transaction={transaction}>
+                <EuiButton iconType="discoverApp">
+                  View transaction in Discover
+                </EuiButton>
+              </DiscoverTransactionLink>
             </EuiFlexItem>
             <MaybeViewTraceLink transaction={transaction} root={root} />
           </EuiFlexGroup>
