0ed0e3c6ae7bd2a6d8c160e81a8a9ee4c03d1621
Oliver Gupte <olivergupte@gmail.com>
2018-10-30 00:27:57 -0700

    [APM] Add type definitions for apm formatters

--- x-pack/plugins/apm/public/utils/formatters.ts
+++ x-pack/plugins/apm/public/utils/formatters.ts
@@ -4,61 +4,66 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import numeral from '@elastic/numeral';
 import { memoize } from 'lodash';
+
+// TODO Add DefinitelyTyped definitions for @types/elastic__numeral
+// import numeral from '@elastic/numeral';
+import numeral = require('@elastic/numeral');
 
 const UNIT_CUT_OFF = 10 * 1000000; // 10 seconds in microseconds
 
-export function asSeconds(value, withUnit = true) {
+export function asSeconds(value: number, withUnit: boolean = true) {
   const formatted = asDecimal(value / 1000000);
   return `${formatted}${withUnit ? ' s' : ''}`;
 }
 
-export function asMillis(value, withUnit = true) {
+export function asMillis(value: number, withUnit: boolean = true) {
   const formatted = asInteger(value / 1000);
   return `${formatted}${withUnit ? ' ms' : ''}`;
 }
 
-export function asMillisWithDefault(value) {
+export function asMillisWithDefault(value?: number) {
   if (value == null) {
     return `N/A`;
   }
   return asMillis(value);
 }
 
-export const getTimeFormatter = memoize(
-  max => (max > UNIT_CUT_OFF ? asSeconds : asMillis)
+export const getTimeFormatter: (
+  max: any
+) => (value: any, withUnit?: boolean) => string = memoize(
+  (max: any) => (max > UNIT_CUT_OFF ? asSeconds : asMillis)
 );
 
-export function timeUnit(max) {
+export function timeUnit(max: any) {
   return max > UNIT_CUT_OFF ? 's' : 'ms';
 }
 
 /*
  * value: time in microseconds
  */
-export function asTime(value) {
+export function asTime(value: number): string {
   return getTimeFormatter(value)(value);
 }
 
-export function asDecimal(value) {
+export function asDecimal(value: number): string {
   return numeral(value).format('0,0.0');
 }
 
-export function asInteger(value) {
+export function asInteger(value: number): string {
   return numeral(value).format('0,0');
 }
 
-export function tpmUnit(type) {
+export function tpmUnit(type: string): string {
   return type === 'request' ? 'rpm' : 'tpm';
 }
 
 export function getDurationPercent(
   itemDuration: number,
-  totalDuration?: number,
-  fallbackResult?: any = ''
-) {
-  if (totalDuration === undefined || totalDuration === 0) {
+  totalDuration: number = 0,
+  fallbackResult: any = ''
+): string {
+  if (totalDuration === 0) {
     return fallbackResult;
   }
   return numeral(itemDuration / totalDuration).format('0.00%');
