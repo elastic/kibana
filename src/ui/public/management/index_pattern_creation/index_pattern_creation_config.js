e70e0e74cb64ded957a6b3fd1b67726009120a18
CJ Cenizal <cj@cenizal.com>
2018-10-30 14:13:08 -0700

    [Rollups] Fix i18n bugs (#23848)
    * Internationalize job details tabs and wrap instances in EuiErrorBoundary to visually localize the error.
    * Localize no default index pattern message.
    * Localize es interval errors.
    * Localize job action menu and confirm delete modal.
    * Remove unnecessary use of injectI18n from tabs.
    * Localize job status.
    * Localize steps.
    * Remove template literals from FormattedMessages.

--- src/ui/public/management/index_pattern_creation/index_pattern_creation_config.js
+++ src/ui/public/management/index_pattern_creation/index_pattern_creation_config.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import { i18n } from '@kbn/i18n';
 
 const indexPatternTypeName = i18n.translate('common.ui.management.editIndexPattern.createIndex.defaultTypeName',
