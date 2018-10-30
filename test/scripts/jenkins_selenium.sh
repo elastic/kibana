1229d8ed3136a36407da8cce59626215ae388da5
Jonathan Budzenski <jon@jbudz.me>
2018-10-30 11:17:02 -0500

    remove ci build flag

--- test/scripts/jenkins_selenium.sh
+++ test/scripts/jenkins_selenium.sh
@@ -5,7 +5,7 @@ source "$(dirname $0)/../../src/dev/ci_setup/setup.sh"
 source "$(dirname $0)/../../src/dev/ci_setup/git_setup.sh"
 source "$(dirname $0)/../../src/dev/ci_setup/java_setup.sh"
 
-node scripts/build --release --debug --oss --version-qualifier=alpha1;
+node scripts/build --release --debug --oss;
 
 export TEST_ES_FROM=${TEST_ES_FROM:-source}
 xvfb-run "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:selenium --from=source;
