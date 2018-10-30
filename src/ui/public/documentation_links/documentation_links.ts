eeb78405ef3999789ac828e71e7529fed2fce826
Matt Bargar <matt.bargar@elastic.co>
2018-10-30 15:15:35 -0400

    Use Clint's full name

--- src/ui/public/documentation_links/documentation_links.ts
+++ src/ui/public/documentation_links/documentation_links.ts
@@ -22,7 +22,7 @@ import { metadata } from '../metadata';
 /*
   WARNING: The links in this file are validated during the docs build. This is accomplished with some regex magic that
   looks for these particular constants. As a result, we should not add new constants or change the existing ones.
-  If you absolutely must make a change, talk to Clint first so he can update his Perl scripts.
+  If you absolutely must make a change, talk to Clinton Gormley first so he can update his Perl scripts.
  */
 export const DOC_LINK_VERSION = metadata.branch;
 export const ELASTIC_WEBSITE_URL = 'https://www.elastic.co/';
