a01beca7bc1bb511cf15de5e054754d7cb6d34d5
bhavyarm <bhavya@elastic.co>
2018-10-31 09:58:51 -0400

    disabling eslint for Canvas because we are adding in jsx-a11y rules to eslint.

--- x-pack/plugins/canvas/public/components/element_content/invalid_expression.js
+++ x-pack/plugins/canvas/public/components/element_content/invalid_expression.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/* Disabling eslint because of this jsx-a11y error:
+/* Disabling eslint because of this jsx-a11y error(https://www.npmjs.com/package/eslint-plugin-jsx-a11y):
  11:3  error  Non-interactive elements should not be assigned mouse or keyboard event listeners  jsx-a11y/no-noninteractive-element-interactions
  */
 /* eslint-disable */
