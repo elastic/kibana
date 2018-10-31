7752cbd7dfca66a0f51683416db4f57e2bd1d208
Aaron Caldwell <aaron.caldwell@elastic.co>
2018-10-31 12:03:01 -0600

    Remove temp flag to disable max-len lint

--- x-pack/plugins/gis/public/shared/icons/additional_layer_icons.js
+++ x-pack/plugins/gis/public/shared/icons/additional_layer_icons.js
@@ -3,7 +3,6 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-/* eslint-disable max-len */
 
 
 import React from 'react';
