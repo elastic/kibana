d0eb6487fc3abd1653ec4628d05e7ec09824d714
liza-mae <liza-mae@users.noreply.github.com>
2018-11-05 10:40:42 -0700

    Replace xvfb with headless browser (#24904)
    * Replace xvfb with headless browser
    
    * Update karma to run in headless chrome
    
    Update Karma tests for an option to run chrome in headless.
    
    * Fix lint issues
    
    * Fix spaces

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -5,5 +5,6 @@ source "$(dirname $0)/../../src/dev/ci_setup/setup.sh"
 source "$(dirname $0)/../../src/dev/ci_setup/git_setup.sh"
 source "$(dirname $0)/../../src/dev/ci_setup/java_setup.sh"
 
+export TEST_BROWSER_HEADLESS=1
 export TEST_ES_FROM=${TEST_ES_FROM:-source}
-xvfb-run "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --from=source;
+"$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --from=source;
