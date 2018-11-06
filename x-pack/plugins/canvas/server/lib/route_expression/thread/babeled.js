d65bce703ddf3579694be6e565e6becbe7305830
joe fleming <joe.fleming@gmail.com>
2018-11-06 10:37:28 -0700

    chore: use @kbn/babel-preset for babel-register settings

--- x-pack/plugins/canvas/server/lib/route_expression/thread/babeled.js
+++ x-pack/plugins/canvas/server/lib/route_expression/thread/babeled.js
@@ -5,36 +5,14 @@
  */
 
 require('babel-register')({
-  compact: false,
-  minified: false,
   ignore: [
     // stolen from kibana/src/setup_node_env/babel_register/register.js
     // ignore paths matching `/node_modules/{a}/{b}`, unless `a`
     // is `x-pack` and `b` is not `node_modules`
     /\/node_modules\/(?!x-pack\/(?!node_modules)([^\/]+))([^\/]+\/[^\/]+)/,
   ],
-  plugins: [
-    'transform-object-rest-spread',
-    'transform-async-to-generator',
-    'transform-class-properties',
-  ],
-  presets: [
-    'react',
-    [
-      'env',
-      {
-        targets: {
-          node: 'current',
-        },
-
-        // replaces `import "babel-polyfill"` with a list of require statements
-        // for just the polyfills that the target versions don't already supply
-        // on their own
-        useBuiltIns: true,
-      },
-    ],
-  ],
   babelrc: false,
+  presets: [require.resolve('@kbn/babel-preset/node_preset')],
 });
 
 require('./worker');
