73b7b780e33cd99e737e14e31e861f9ee58d9c9a
Walter Rafelsberger <walter@elastic.co>
2018-11-07 17:44:27 +0100

    [ML] Save Status Modal Controller test.

---
+++ x-pack/plugins/ml/public/jobs/new_job/advanced/save_status_modal/__tests__/save_status_modal_controller.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+
+
+import ngMock from 'ng_mock';
+import expect from 'expect.js';
+
+const mockModalInstance = { close: function () { }, dismiss: function () { } };
+
+describe('ML - Save Status Modal Controller', () => {
+  beforeEach(() => {
+    ngMock.module('kibana');
+  });
+
+  it('Initialize Save Status Modal Controller', (done) => {
+    ngMock.inject(function ($rootScope, $controller) {
+      const scope = $rootScope.$new();
+      $controller('MlSaveStatusModal', {
+        $scope: scope,
+        $modalInstance: mockModalInstance,
+        params: {}
+      });
+
+      expect(scope.ui.showTimepicker).to.eql(false);
+      done();
+    });
+  });
+});
