0f86d5c534dc03f0d247079b06532c4c615940f2
Pete Harverson <pete@elastic.co>
2018-11-07 14:18:30 +0000

    [ML] Fixes file data viz file size check and format as bytes

--- x-pack/plugins/ml/public/file_datavisualizer/components/file_datavisualizer_view/file_error_callouts.js
+++ x-pack/plugins/ml/public/file_datavisualizer/components/file_datavisualizer_view/file_error_callouts.js
@@ -10,16 +10,42 @@ import {
   EuiCallOut,
 } from '@elastic/eui';
 
+import numeral from '@elastic/numeral';
+
+const FILE_SIZE_DISPLAY_FORMAT = '0,0.[0] b';
+
 export function FileTooLarge({ fileSize, maxFileSize }) {
+  const fileSizeFormatted = numeral(fileSize).format(FILE_SIZE_DISPLAY_FORMAT);
+  const maxFileSizeFormatted = numeral(maxFileSize).format(FILE_SIZE_DISPLAY_FORMAT);
+
+  // Format the byte values, using the second format if the difference between
+  // the file size and the max is so small that the formatted values are identical
+  // e.g. 100.01 MB and 100.0 MB
+  let errorText;
+  if (fileSizeFormatted !== maxFileSizeFormatted) {
+    errorText = (
+      <p>
+        The size of the file you selected for upload is {fileSizeFormatted} which
+        which exceeds the maximum permitted size of {maxFileSizeFormatted}
+      </p>
+    );
+  } else {
+    const diffFormatted = numeral(fileSize - maxFileSize).format(FILE_SIZE_DISPLAY_FORMAT);
+    errorText = (
+      <p>
+        The size of the file you selected for upload exceeds the maximum
+        permitted size of {maxFileSizeFormatted} by {diffFormatted}
+      </p>
+    );
+  }
+
   return (
     <EuiCallOut
       title="File size is too large"
       color="danger"
       iconType="cross"
     >
-      <p>
-        The size of the file you selected for upload is {fileSize} which exceeds the maximum permitted size of {maxFileSize}
-      </p>
+      {errorText}
     </EuiCallOut>
   );
 }
