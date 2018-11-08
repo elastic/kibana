46843c4c9e3f704fded48bc6dd118717d12f5d3e
Tim Roes <tim.roes@elastic.co>
2018-11-08 15:11:50 +0100

    Replace moving_avg by moving_fn

--- src/ui/public/agg_types/metrics/moving_avg.js
+++ src/ui/public/agg_types/metrics/moving_avg.js
@@ -23,11 +23,32 @@ import { makeNestedLabel } from './lib/make_nested_label';
 
 export const movingAvgMetricAgg = new MetricAggType({
   name: 'moving_avg',
+  dslName: 'moving_fn',
   title: 'Moving Avg',
   subtype: parentPipelineAggHelper.subtype,
   makeLabel: agg => makeNestedLabel(agg, 'moving avg'),
   params: [
-    ...parentPipelineAggHelper.params()
+    ...parentPipelineAggHelper.params(),
+    {
+      name: 'window',
+      default: 5,
+    },
+    {
+      name: 'script',
+      default: 'MovingFunctions.unweightedAvg(values)'
+    }
   ],
+  getValue(agg, bucket) {
+    /**
+     * The moving_avg aggregation (that was used originally here) does not return
+     * a bucket if the child aggregation doesn't have any documents, and thus
+     * we would return 0 via the getValue function in MetricAggType.
+     * The moving_fn does return a bucket with the value `null` instead, which would
+     * not be converted to 0 by MetricAggType.getValue. To have the same behavior as
+     * moving_avg had earlier in Kibana, we will now also convert an existing bucket with
+     * the value `null` to 0.
+     */
+    return bucket[agg.id] ? (bucket[agg.id].value || 0) : 0;
+  },
   getFormat: parentPipelineAggHelper.getFormat
 });
