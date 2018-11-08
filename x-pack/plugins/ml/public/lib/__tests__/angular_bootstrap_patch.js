75e5a969f87629be09bb318e075d1086464550c8
Walter Rafelsberger <walter@elastic.co>
2018-11-08 12:40:33 +0100

    [ML] Angular Bootstrap Patch Dropdown Controller test.

---
+++ x-pack/plugins/ml/public/lib/__tests__/angular_bootstrap_patch.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+
+
+import ngMock from 'ng_mock';
+import expect from 'expect.js';
+
+describe('ML - Angular Patch - Dropdown Controller', () => {
+  beforeEach(() => {
+    ngMock.module('ui.bootstrap.dropdown');
+  });
+
+  it('Initialize Dropdown Controller', (done) => {
+    ngMock.inject(function ($rootScope, $controller) {
+      const scope = $rootScope.$new();
+
+      expect(scope.$$watchersCount).to.eql(0);
+
+      $controller('DropdownController', {
+        $attrs: [],
+        $scope: scope
+      });
+
+      expect(scope.$$watchersCount).to.eql(1);
+      done();
+    });
+  });
+});
