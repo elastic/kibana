0a25a4ccf2182583eea154c9d8ade5960f772e8e
Clint Andrew Hall <clint.hall@elastic.co>
2018-11-07 19:00:25 -0600

    Fix File Upload fatal

--- x-pack/plugins/canvas/canvas_plugin_src/uis/arguments/image_upload/index.js
+++ x-pack/plugins/canvas/canvas_plugin_src/uis/arguments/image_upload/index.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+  EuiForm,
   EuiFlexGroup,
   EuiFlexItem,
   EuiImage,
@@ -15,7 +16,7 @@ import {
   EuiButton,
   EuiFieldText,
 } from '@elastic/eui';
-import { Loading } from '../../../../public/components/loading';
+import { Loading } from '../../../../public/components/loading/loading';
 import { FileUpload } from '../../../../public/components/file_upload';
 import { elasticOutline } from '../../../lib/elastic_outline';
 import { resolveFromArgs } from '../../../../common/lib/resolve_dataurl';
@@ -123,18 +124,19 @@ class ImageUpload extends React.Component {
     }
 
     const pasteImageUrl = urlTypeSrc ? (
-      <form onSubmit={this.setSrcUrl} className="eui-textRight">
+      <EuiForm onSubmit={this.setSrcUrl} className="eui-textRight">
         <EuiFieldText
           compressed
-          value={this.state.url}
+          defaultValue={this.state.url}
           inputRef={ref => (this.inputRefs.srcUrlText = ref)}
           placeholder="Image URL"
           aria-label="Image URL"
         />
+        <EuiSpacer size="m" />
         <EuiButton type="submit" size="s" onClick={this.setSrcUrl}>
           Set
         </EuiButton>
-      </form>
+      </EuiForm>
     ) : null;
 
     const shouldPreview =
