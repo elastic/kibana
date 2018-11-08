937761145f1daf8f2253bfc1930a3e0b43d1e8dc
Walter Rafelsberger <walter@elastic.co>
2018-11-08 12:23:52 +0100

    [ML] Index Or Search Controller test.

---
+++ x-pack/plugins/ml/public/jobs/new_job/wizard/steps/index_or_search/__tests__/index_or_search_controller.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+
+
+import ngMock from 'ng_mock';
+import expect from 'expect.js';
+
+describe('ML - Index Or Search Controller', () => {
+  beforeEach(() => {
+    ngMock.module('kibana');
+  });
+
+  it('Initialize Index Or Sesarch Controller', (done) => {
+    ngMock.inject(function ($rootScope, $controller) {
+      const scope = $rootScope.$new();
+      $controller('MlNewJobStepIndexOrSearch', {
+        $route: {
+          current: {
+            locals: {}
+          }
+        },
+        $scope: scope
+      });
+
+      expect(scope.indexPatterns).to.eql([]);
+      done();
+    });
+  });
+});
