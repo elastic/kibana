65ae04a5bd921dda37fdf49a4e4fcdff7904d52d
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-11-09 00:19:20 +0100

    [APM] Format as microseconds (#24974)
    * [APM] Format as microseconds
    
    * Split interface into `value` and `options`
    
    * Update formatters.test.ts

--- x-pack/plugins/apm/public/utils/__test__/formatters.test.ts
+++ x-pack/plugins/apm/public/utils/__test__/formatters.test.ts
@@ -7,11 +7,25 @@
 import { asPercent, asTime } from '../formatters';
 
 describe('formatters', () => {
-  it('asTime', () => {
-    expect(asTime(1000)).toBe('1 ms');
-    expect(asTime(1000 * 1000)).toBe('1,000 ms');
-    expect(asTime(1000 * 1000 * 10)).toBe('10,000 ms');
-    expect(asTime(1000 * 1000 * 20)).toBe('20.0 s');
+  describe('asTime', () => {
+    it('formats correctly with defaults', () => {
+      expect(asTime(null)).toBe('N/A');
+      expect(asTime(undefined)).toBe('N/A');
+      expect(asTime(0)).toBe('0 μs');
+      expect(asTime(1)).toBe('1 μs');
+      expect(asTime(1000)).toBe('1,000 μs');
+      expect(asTime(1000 * 1000)).toBe('1,000 ms');
+      expect(asTime(1000 * 1000 * 10)).toBe('10,000 ms');
+      expect(asTime(1000 * 1000 * 20)).toBe('20.0 s');
+    });
+
+    it('formats without unit', () => {
+      expect(asTime(1000, { withUnit: false })).toBe('1,000');
+    });
+
+    it('falls back to default value', () => {
+      expect(asTime(undefined, { defaultValue: 'nope' })).toBe('nope');
+    });
   });
 
   describe('asPercent', () => {
