67badf3dbd307ed9311552df368913585f448db8
Walter Rafelsberger <walter@elastic.co>
2018-11-08 10:36:58 +0100

    [ML] Recognize Create Job Controller test.

---
+++ x-pack/plugins/ml/public/jobs/new_job/simple/recognize/create_job/__tests__/create_job_controller.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+
+
+import ngMock from 'ng_mock';
+import expect from 'expect.js';
+import sinon from 'sinon';
+
+// Import this way to be able to stub/mock functions later on in the tests using sinon.
+import * as newJobUtils from 'plugins/ml/jobs/new_job/utils/new_job_utils';
+
+describe('ML - Recognize Wizard - Create Job Controller', () => {
+  beforeEach(() => {
+    ngMock.module('kibana');
+  });
+
+  it('Initialize Create Job Controller', (done) => {
+    sinon.stub(newJobUtils, 'createSearchItems').callsFake(() => ({
+      indexPattern: {},
+      savedSearch: {},
+      combinedQuery: {}
+    }));
+    ngMock.inject(function ($rootScope, $controller) {
+      const scope = $rootScope.$new();
+      $controller('MlCreateRecognizerJobs', {
+        $route: {
+          current: {
+            params: {}
+          }
+        },
+        $scope: scope
+      });
+
+      expect(scope.ui.formValid).to.eql(true);
+      done();
+    });
+  });
+});
