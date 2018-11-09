818e85be921d649ef9629b5e3a99decb48565cae
spalger <email@spalger.com>
2018-11-08 19:00:27 -0800

    [ci] execute sub-scripts

--- .ci/run.sh
+++ .ci/run.sh
@@ -2,18 +2,21 @@
 
 set -e
 
+# move to Kibana root
+cd "$(dirname "$0")/.."
+
 case "$KBN_CI_JOB" in
 "selenium")
-  source "$(dirname "$0")/../test/scripts/jenkins_selenium.sh"
+  ./test/scripts/jenkins_selenium.sh
   ;;
 "intake")
-  source "$(dirname "$0")/../test/scripts/jenkins_unit.sh"
+  ./test/scripts/jenkins_unit.sh
   ;;
 "x-pack")
-  source "$(dirname "$0")/../test/scripts/jenkins_xpack.sh"
+  ./test/scripts/jenkins_xpack.sh
   ;;
 *)
-  echo "'$KBN_CI_JOB' is not implemented."
+  echo "CI_JOB '$KBN_CI_JOB' is not implemented."
   exit 1
   ;;
 esac
