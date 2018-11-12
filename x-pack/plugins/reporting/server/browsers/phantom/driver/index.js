c46409c413b0236de193c2cc447ffeb6eebc7a6b
Leanid Shutau <leanidshutau@gmail.com>
2018-11-12 11:05:36 +0300

    Refactor injection code

--- x-pack/plugins/reporting/server/browsers/phantom/driver/index.js
+++ x-pack/plugins/reporting/server/browsers/phantom/driver/index.js
@@ -125,146 +125,137 @@ export function PhantomDriver({ page, browser, zoom, logger }) {
         randomBytes(6).toString('base64'),
       ].join('-');
 
-      function checkForIntl() {
-        return fromCallback(cb => {
-          page.evaluate(function hasIntl() {
-            return (window.Intl !== undefined);
-          }, cb);
-        });
-      }
+      const nodeModules = path.resolve(__dirname, '..', '..', '..', '..', '..', '..', 'node_modules');
+      const intlPath = path.join(nodeModules, 'intl', 'dist', 'Intl.js');
+      const promisePath = path.join(nodeModules, 'bluebird', 'js', 'browser', 'bluebird.js');
 
-      return checkForIntl()
-        .then(hasIntl => {
-          if (hasIntl) return;
+      return injectPolyfill(
+        page,
+        intlPath,
+        function hasIntl() {
+          return (window.Intl !== undefined);
+        },
+        'Intl'
+      )
+        .then(() =>
+          injectPolyfill(
+            page,
+            promisePath,
+            function hasPromise() {
+              return (typeof window.Promise !== 'undefined');
+            },
+            'Promise'
+          ))
+        .then(() => {
+          return fromCallback(cb => {
+            page.evaluate(transformFn(evaluateWrapper), transformFn(fn).toString(), uniqId, args, cb);
 
-          const nodeModules = path.resolve(__dirname, '..', '..', '..', '..', '..', '..', 'node_modules');
-          const intlPath = path.join(nodeModules, 'intl', 'dist', 'Intl.js');
+            // The original function is passed here as a string, and eval'd in phantom's context.
+            // It's then executed in phantom's context and the result is attached to a __reporting
+            // property on window. Promises can be used, and the result will be handled in the next
+            // block. If the original function does not return a promise, its result is passed on.
+            function evaluateWrapper(userFnStr, cbIndex, origArgs) {
+            // you can't pass a function to phantom, so we pass the string and eval back into a function
+              let userFn;
+              eval('userFn = ' + userFnStr); // eslint-disable-line no-eval
 
-          return fromCallback(cb => page.injectJs(intlPath, cb))
-            .then(status => {
-              if (status !== true) {
-                return Promise.reject('Failed to load Intl library');
+              // keep a record of the resulting execution for future calls (used when async)
+              window.__reporting = window.__reporting || {};
+              window.__reporting[cbIndex] = undefined;
+
+              // used to format the response consistently
+              function done(err, res) {
+                if (window.__reporting[cbIndex]) {
+                  return;
+                }
+
+                const isErr = err instanceof Error;
+                if (isErr) {
+                  const keys = Object.getOwnPropertyNames(err);
+                  err = keys.reduce(function copyErr(obj, key) {
+                    obj[key] = err[key];
+                    return obj;
+                  }, {});
+                }
+
+                return window.__reporting[cbIndex] = {
+                  err: err,
+                  res: res,
+                };
               }
+
+              try {
+              // execute the original function
+                const res = userFn.apply(this, origArgs);
+
+                if (res && typeof res.then === 'function') {
+                // handle async resolution via Promises
+                  res.then((val) => {
+                    done(null, val);
+                  }, (err) => {
+                    if (!(err instanceof Error)) {
+                      err = new Error(err || 'Unspecified error');
+                    }
+                    done(err);
+                  });
+                  return '__promise__';
+                } else {
+                // if not given a promise, execute as sync
+                  return done(null, res);
+                }
+              } catch (err) {
+              // any error during execution should be dealt with
+                return done(err);
+              }
+            }
+          })
+            .then((res) => {
+              // if the response is not a promise, pass it along
+              if (res !== '__promise__') {
+                return res;
+              }
+
+              // promise response means async, so wait for its resolution
+              return this.waitFor({
+                fn: function (cbIndex) {
+                  // resolves when the result object is no longer undefined
+                  return !!window.__reporting[cbIndex];
+                },
+                args: [uniqId],
+                toEqual: true,
+              })
+                .then(() => {
+                  // once the original promise is resolved, pass along its value
+                  return fromCallback(cb => {
+                    page.evaluate(function (cbIndex) {
+                      return window.__reporting[cbIndex];
+                    }, uniqId, cb);
+                  });
+                });
             })
-            .then(checkForIntl)
-            .then(hasIntlLoaded => {
-              if (hasIntlLoaded !== true) {
-                return Promise.reject('Failed to inject Intl');
-              }
-            });
-        })
-        .then(() => _injectPromise(page)
-          .then(() => {
-            return fromCallback(cb => {
-              page.evaluate(transformFn(evaluateWrapper), transformFn(fn).toString(), uniqId, args, cb);
+            .then((res) => {
+              if (res.err) {
+                // Make long/normal stack traces work
+                res.err.name = res.err.name || 'Error';
 
-              // The original function is passed here as a string, and eval'd in phantom's context.
-              // It's then executed in phantom's context and the result is attached to a __reporting
-              // property on window. Promises can be used, and the result will be handled in the next
-              // block. If the original function does not return a promise, its result is passed on.
-              function evaluateWrapper(userFnStr, cbIndex, origArgs) {
-              // you can't pass a function to phantom, so we pass the string and eval back into a function
-                let userFn;
-                eval('userFn = ' + userFnStr); // eslint-disable-line no-eval
+                if (!res.err.stack) {
+                  res.err.stack = res.err.toString();
+                }
 
-                // keep a record of the resulting execution for future calls (used when async)
-                window.__reporting = window.__reporting || {};
-                window.__reporting[cbIndex] = undefined;
+                res.err.stack.replace(/\n*$/g, '\n');
 
-                // used to format the response consistently
-                function done(err, res) {
-                  if (window.__reporting[cbIndex]) {
-                    return;
-                  }
-
-                  const isErr = err instanceof Error;
-                  if (isErr) {
-                    const keys = Object.getOwnPropertyNames(err);
-                    err = keys.reduce(function copyErr(obj, key) {
-                      obj[key] = err[key];
-                      return obj;
-                    }, {});
-                  }
-
-                  return window.__reporting[cbIndex] = {
-                    err: err,
-                    res: res,
+                if (res.err.stack) {
+                  res.err.toString = function () {
+                    return this.name + ': ' + this.message;
                   };
                 }
 
-                try {
-                // execute the original function
-                  const res = userFn.apply(this, origArgs);
-
-                  if (res && typeof res.then === 'function') {
-                  // handle async resolution via Promises
-                    res.then((val) => {
-                      done(null, val);
-                    }, (err) => {
-                      if (!(err instanceof Error)) {
-                        err = new Error(err || 'Unspecified error');
-                      }
-                      done(err);
-                    });
-                    return '__promise__';
-                  } else {
-                  // if not given a promise, execute as sync
-                    return done(null, res);
-                  }
-                } catch (err) {
-                // any error during execution should be dealt with
-                  return done(err);
-                }
+                return Promise.reject(res.err);
               }
-            })
-              .then((res) => {
-                // if the response is not a promise, pass it along
-                if (res !== '__promise__') {
-                  return res;
-                }
 
-                // promise response means async, so wait for its resolution
-                return this.waitFor({
-                  fn: function (cbIndex) {
-                    // resolves when the result object is no longer undefined
-                    return !!window.__reporting[cbIndex];
-                  },
-                  args: [uniqId],
-                  toEqual: true,
-                })
-                  .then(() => {
-                    // once the original promise is resolved, pass along its value
-                    return fromCallback(cb => {
-                      page.evaluate(function (cbIndex) {
-                        return window.__reporting[cbIndex];
-                      }, uniqId, cb);
-                    });
-                  });
-              })
-              .then((res) => {
-                if (res.err) {
-                  // Make long/normal stack traces work
-                  res.err.name = res.err.name || 'Error';
-
-                  if (!res.err.stack) {
-                    res.err.stack = res.err.toString();
-                  }
-
-                  res.err.stack.replace(/\n*$/g, '\n');
-
-                  if (res.err.stack) {
-                    res.err.toString = function () {
-                      return this.name + ': ' + this.message;
-                    };
-                  }
-
-                  return Promise.reject(res.err);
-                }
-
-                return res.res;
-              });
-          })
-        );
+              return res.res;
+            });
+        });
     },
 
     wait(timeout) {
@@ -344,33 +335,26 @@ export function PhantomDriver({ page, browser, zoom, logger }) {
   };
 }
 
+async function injectPolyfill(page, pathToPolyfillFile, checkFunction, libraryName) {
+  const hasPolyfill = await fromCallback(cb => {
+    page.evaluate(checkFunction, cb);
+  });
 
-function _injectPromise(page) {
-  function checkForPromise() {
-    return fromCallback(cb => {
-      page.evaluate(function hasPromise() {
-        return (typeof window.Promise !== 'undefined');
-      }, cb);
-    });
+  if (hasPolyfill) {
+    return;
   }
 
-  return checkForPromise()
-    .then(hasPromise => {
-      if (hasPromise) return;
+  const status = await fromCallback(cb => page.injectJs(pathToPolyfillFile, cb));
 
-      const nodeModules = path.resolve(__dirname, '..', '..', '..', '..', '..', '..', 'node_modules');
-      const promisePath = path.join(nodeModules, 'bluebird', 'js', 'browser', 'bluebird.js');
-      return fromCallback(cb => page.injectJs(promisePath, cb))
-        .then(status => {
-          if (status !== true) {
-            return Promise.reject('Failed to load Promise library');
-          }
-        })
-        .then(checkForPromise)
-        .then(hasPromiseLoaded => {
-          if (hasPromiseLoaded !== true) {
-            return Promise.reject('Failed to inject Promise');
-          }
-        });
-    });
+  if (status !== true) {
+    return Promise.reject(`Failed to load ${libraryName} library`);
+  }
+
+  const hasPolyfillLoaded = await fromCallback(cb => {
+    page.evaluate(checkFunction, cb);
+  });
+
+  if (hasPolyfillLoaded !== true) {
+    return Promise.reject(`Failed to inject ${libraryName}`);
+  }
 }
