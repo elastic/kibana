da0c882b27f4ca55821568e39f63b48d45c2752e
maryia-lapata <mary.lopato@gmail.com>
2018-11-13 16:28:19 +0300

    Remove unused dependency.

--- x-pack/plugins/spaces/public/views/management/page_routes.tsx
+++ x-pack/plugins/spaces/public/views/management/page_routes.tsx
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import { I18nProvider } from '@kbn/i18n/react';
-import 'plugins/spaces/views/management/manage_spaces.less';
 
 // @ts-ignore
 import template from 'plugins/spaces/views/management/template.html';
