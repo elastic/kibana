de3038846132ef3a8c796691b5c422d2ccc75638
Matthew Kime <matt@mattki.me>
2018-11-13 14:00:27 -0600

    Fixes index pattern wizard when there are remote clusters but no local indices  (#24339)
    * don't hide wizard if clusters exist
    
    * catch errors
    
    * add toast notifs if unable to load data

--- src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/lib/get_remote_clusters.js
+++ src/core_plugins/kibana/public/management/sections/indices/create_index_pattern_wizard/lib/get_remote_clusters.js
@@ -17,16 +17,10 @@
  * under the License.
  */
 
-export { canAppendWildcard } from './can_append_wildcard';
+import chrome from 'ui/chrome';
+const apiPrefix = chrome.addBasePath('/api/kibana');
 
-export { ensureMinimumTime } from './ensure_minimum_time';
-
-export { getIndices } from './get_indices';
-
-export { getMatchedIndices } from './get_matched_indices';
-
-export { containsIllegalCharacters } from './contains_illegal_characters';
-
-export { extractTimeFields } from './extract_time_fields';
-
-export { getRemoteClusters } from './get_remote_clusters';
+export async function getRemoteClusters($http) {
+  const response = await $http.get(`${apiPrefix}/clusters`);
+  return response.data;
+}
