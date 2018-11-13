725eb6c6cbdd929043ae56d926b79cc5ed8dba7c
Ryan Keairns <contactryank@gmail.com>
2018-11-13 09:00:44 -0600

    fix after rebase

--- x-pack/plugins/canvas/public/state/reducers/workpad.js
+++ x-pack/plugins/canvas/public/state/reducers/workpad.js
@@ -17,29 +17,33 @@ import {
 
 import { APP_ROUTE_WORKPAD } from '../../../common/lib/constants';
 
-export const workpadReducer = handleActions({
-  [setWorkpad]: (workpadState, { payload }) => {
-    recentlyAccessed.add(`${APP_ROUTE_WORKPAD}/${payload.id}`, payload.name, payload.id);
-    return payload;
-  },
+export const workpadReducer = handleActions(
+  {
+    [setWorkpad]: (workpadState, { payload }) => {
+      recentlyAccessed.add(`${APP_ROUTE_WORKPAD}/${payload.id}`, payload.name, payload.id);
+      return payload;
+    },
 
-  [sizeWorkpad]: (workpadState, { payload }) => {
-    return { ...workpadState, ...payload };
-  },
+    [sizeWorkpad]: (workpadState, { payload }) => {
+      return { ...workpadState, ...payload };
+    },
 
-  [setColors]: (workpadState, { payload }) => {
-    return { ...workpadState, colors: payload };
-  },
+    [setColors]: (workpadState, { payload }) => {
+      return { ...workpadState, colors: payload };
+    },
 
-  [setName]: (workpadState, { payload }) => {
-    recentlyAccessed.add(`${APP_ROUTE_WORKPAD}/${workpadState.id}`, payload, workpadState.id);
-    return { ...workpadState, name: payload };
-  },
+    [setName]: (workpadState, { payload }) => {
+      recentlyAccessed.add(`${APP_ROUTE_WORKPAD}/${workpadState.id}`, payload, workpadState.id);
+      return { ...workpadState, name: payload };
+    },
 
-  [setWriteable]: (workpadState, { payload }) => {
-    return { ...workpadState, isWriteable: Boolean(payload) };
+    [setWriteable]: (workpadState, { payload }) => {
+      return { ...workpadState, isWriteable: Boolean(payload) };
+    },
+
+    [setWorkpadStyle]: (workpadState, { payload }) => {
+      return { ...workpadState, style: payload };
+    },
   },
-  [setWorkpadStyle]: (workpadState, { payload }) => {
-    return { ...workpadState, style: payload };
-  },
-});
+  {}
+);
