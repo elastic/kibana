e73966f181aff3575a4733f7a42a90f387feef2c
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-11-14 15:43:34 -0500

    Revert "removing unnecessary colon"
    This reverts commit 19712807bbcd9c43a0bb71fce22d5d0112b6b146.

--- x-pack/plugins/index_management/public/sections/index_list/components/detail_panel/summary/summary.js
+++ x-pack/plugins/index_management/public/sections/index_list/components/detail_panel/summary/summary.js
@@ -80,7 +80,7 @@ export class Summary extends React.PureComponent {
       }
       const cell = [
         <EuiDescriptionListTitle key={fieldName}>
-          <strong>services/</strong>
+          <strong>{HEADERS[fieldName]}:</strong>
         </EuiDescriptionListTitle>,
         <EuiDescriptionListDescription key={fieldName + "_desc"}>
           {content}
