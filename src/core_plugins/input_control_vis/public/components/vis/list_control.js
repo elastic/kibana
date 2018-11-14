24407f019d5eaaa2e50674f14256a0e2451c07c8
Nathan Reese <reese.nathan@gmail.com>
2018-11-14 09:32:28 -0700

    use field format to create label

--- src/core_plugins/input_control_vis/public/components/vis/list_control.js
+++ src/core_plugins/input_control_vis/public/components/vis/list_control.js
@@ -43,7 +43,10 @@ class ListControlUi extends Component {
   }
 
   handleOnChange = (selectedOptions) => {
-    this.props.stageFilter(this.props.controlIndex, selectedOptions);
+    const selectedValues = selectedOptions.map(({ value }) => {
+      return value;
+    });
+    this.props.stageFilter(this.props.controlIndex, selectedValues);
   }
 
   debouncedFetch = _.debounce(async (searchValue) => {
@@ -77,25 +80,32 @@ class ListControlUi extends Component {
       );
     }
 
-    const options = this.props.options.map(option => {
-      return {
-        label: option.label,
-        value: option.value,
-        ['data-test-subj']: `option_${option.value.replace(' ', '_')}`
-      };
-    });
-
     return (
       <EuiComboBox
         placeholder={intl.formatMessage({
           id: 'inputControl.vis.listControl.selectPlaceholder',
           defaultMessage: 'Select...'
         })}
-        options={options}
+        options={this.props.options.map(option => {
+          return {
+            label: this.props.formatOptionLabel(option),
+            value: option,
+            ['data-test-subj']: `option_${option.toString().replace(' ', '_')}`
+          };
+        })
+          .sort((a, b) => {
+            return a.label.toLowerCase().localeCompare(b.label.toLowerCase());
+          })
+        }
         isLoading={this.state.isLoading}
         async={this.props.dynamicOptions}
         onSearchChange={this.props.dynamicOptions ? this.onSearchChange : undefined}
-        selectedOptions={this.props.selectedOptions}
+        selectedOptions={this.props.selectedOptions.map(selectedOption => {
+          return {
+            label: this.props.formatOptionLabel(selectedOption),
+            value: selectedOption,
+          };
+        })}
         onChange={this.handleOnChange}
         singleSelection={!this.props.multiselect}
         data-test-subj={`listControlSelect${this.props.controlIndex}`}
@@ -117,16 +127,12 @@ class ListControlUi extends Component {
   }
 }
 
-const comboBoxOptionShape = PropTypes.shape({
-  label: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-});
-
 ListControlUi.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  selectedOptions: PropTypes.arrayOf(comboBoxOptionShape).isRequired,
-  options: PropTypes.arrayOf(comboBoxOptionShape),
+  selectedOptions: PropTypes.array.isRequired,
+  options: PropTypes.array,
+  formatOptionLabel: PropTypes.func.isRequired,
   disableMsg: PropTypes.string,
   multiselect: PropTypes.bool,
   dynamicOptions: PropTypes.bool,
