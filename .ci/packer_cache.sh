65faf13062d620d389e6b977ac4993ccb4d7d1f9
spalger <email@spalger.com>
2018-11-16 15:15:12 -0800

    cache es snapshots, remove date from logs

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -6,14 +6,19 @@ source "src/dev/ci_setup/setup.sh";
 
 echo ""
 echo ""
-echo "building kibana == $(date)"
-time node scripts/build;
+echo "building kibana"
+node scripts/build;
+
+echo ""
+echo ""
+echo "caching es snapshots"
+node scripts/es snapshot --download;
 
 cacheDir="${CACHE_DIR:-"$HOME/.kibana"}";
 
 echo ""
 echo ""
-echo "archiving node_modules and other cache files == $(date)"
+echo "archiving node_modules and other cache files"
 tar -cf "$cacheDir/untracked_file_cache.tar" \
   node_modules \
   packages/*/node_modules \
@@ -26,19 +31,19 @@ tar -cf "$cacheDir/untracked_file_cache.tar" \
 
 echo ""
 echo ""
-echo "cleaning untracked and ignored files == $(date)"
+echo "cleaning untracked and ignored files"
 git clean -fdx;
 
 echo ""
 echo ""
-echo "extracting untracked and ignored files from archive == $(date)"
+echo "extracting untracked and ignored files from archive"
 tar xf "$cacheDir/untracked_file_cache.tar";
 
 echo ""
 echo ""
-echo "noting files after extraction == $(date)"
+echo "noting files after extraction"
 find . > "$HOME/kibana-extracted-cache";
 
 echo ""
 echo ""
-echo "done == $(date)"
+echo "done"
