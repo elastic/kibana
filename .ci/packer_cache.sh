66c52f036ec5ff20566c552dc78ef88a320adb69
spalger <email@spalger.com>
2018-11-16 12:53:30 -0800

    choose specific directories to cache, include times in log

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -4,11 +4,41 @@ find . > "$HOME/kibana-clean";
 
 source "src/dev/ci_setup/setup.sh";
 
-node scripts/build;
+echo ""
+echo ""
+echo "building kibana == $(date)"
+time node scripts/build;
 
 cacheDir="${CACHE_DIR:-"$HOME/.kibana"}";
-git ls-files --others --exclude-standard --ignored -z | xargs -0 tar -cf "$cacheDir/untracked_files";
 
+echo ""
+echo ""
+echo "archiving untracked and ignored files == $(date)"
+tar cf "$cacheDir/untracked.tar" \
+  node_modules \
+  packages/*/node_modules \
+  x-pack/node_modules \
+  x-pack/plugins/*/node_modules \
+  optimize \
+  data \
+  .es;
+
+
+echo ""
+echo ""
+echo "cleaning untracked and ignored files == $(date)"
 git clean -fdx;
+
+echo ""
+echo ""
+echo "extracting untracked and ignored files from archive == $(date)"
 tar xf "$cacheDir/untracked_files";
+
+echo ""
+echo ""
+echo "noting files after extraction == $(date)"
 find . > "$HOME/kibana-extracted-cache";
+
+echo ""
+echo ""
+echo "done == $(date)"
