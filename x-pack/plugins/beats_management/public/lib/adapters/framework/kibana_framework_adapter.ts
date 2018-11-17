98f27ceee081d113448204828a81e48ff9e26a61
Matt Apperson <me@mattapperson.com>
2018-11-17 10:09:13 -0500

    remove duplicate comments

--- x-pack/plugins/beats_management/public/lib/adapters/framework/kibana_framework_adapter.ts
+++ x-pack/plugins/beats_management/public/lib/adapters/framework/kibana_framework_adapter.ts
@@ -4,12 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 import { IScope } from 'angular';
 import { PathReporter } from 'io-ts/lib/PathReporter';
 import * as React from 'react';
