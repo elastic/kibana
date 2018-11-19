295456398dd503d700a25103615122372bc8bdcc
joe fleming <joe.fleming@gmail.com>
2018-11-19 12:26:05 -0700

    test: add check for timelion element

--- x-pack/test/functional/apps/canvas/smoke_test.js
+++ x-pack/test/functional/apps/canvas/smoke_test.js
@@ -15,7 +15,7 @@ export default function canvasSmokeTest({ getService, getPageObjects }) {
 
   describe('smoke test', async () => {
     const workpadListSelector = 'canvasWorkpadLoaderTable canvasWorkpadLoaderWorkpad';
-    const testWorkpadId = 'workpad-a3524db3-4014-403d-9ef6-2d6ffa655285';
+    const testWorkpadId = 'workpad-1705f884-6224-47de-ba49-ca224fe6ec31';
 
     before(async () => {
       // init data
@@ -35,7 +35,7 @@ export default function canvasSmokeTest({ getService, getPageObjects }) {
       expect(await workpadRows[0].getVisibleText()).to.equal('Test Workpad');
     });
 
-    it('loads workpage and renders elements', async () => {
+    it('loads workpage when clicked', async () => {
       // click the first workpad in the list to load it
       const link = await testSubjects.find(workpadListSelector);
       await link.click();
@@ -46,23 +46,30 @@ export default function canvasSmokeTest({ getService, getPageObjects }) {
       // check that workpad loaded in url
       const url = await remote.getCurrentUrl();
       expect(parse(url).hash).to.equal(`#/workpad/${testWorkpadId}/page/1`);
+    });
 
+    it('renders elements on workpad', async () => {
       // check for elements on the page
       const elements = await testSubjects.findAll('canvasWorkpadPage canvasWorkpadPageElementContent');
-      expect(elements).to.have.length(3);
+      expect(elements).to.have.length(4);
 
       // check that the elements are what we expect
-      // first element is a datatable that uses essql
-      const rows = await elements[0].findAllByCssSelector('.canvasDataTable tbody tr');
-      expect(rows).to.have.length(10);
 
-      // second is a markdown element
-      const md = await elements[1].findByCssSelector('.canvasMarkdown');
+      // first is a markdown element
+      const md = await elements[0].findByCssSelector('.canvasMarkdown');
       expect(await md.getVisibleText()).to.contain('Welcome to Canvas');
 
+      // second element is a datatable that uses essql
+      const serverRows = await elements[1].findAllByCssSelector('.canvasDataTable tbody tr');
+      expect(serverRows).to.have.length(10);
+
       // third is a datatable that uses csv
-      const rows2 = await elements[2].findAllByCssSelector('.canvasDataTable tbody tr');
-      expect(rows2).to.have.length(2);
+      const commonRows = await elements[2].findAllByCssSelector('.canvasDataTable tbody tr');
+      expect(commonRows).to.have.length(2);
+
+      // fourth is a datatable that uses timelion
+      const timelionRows = await elements[3].findAllByCssSelector('.canvasDataTable tbody tr');
+      expect(timelionRows).to.have.length(12);
     });
   });
 }
