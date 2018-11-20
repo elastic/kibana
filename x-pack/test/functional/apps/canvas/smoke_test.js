fca6f74e05110fef814dc406e49356dd63484d51
joe fleming <joe.fleming@gmail.com>
2018-11-20 10:26:36 -0700

    test: wrap smoke tests in retry
    should help with some of the flakey behavior with the selectors

--- x-pack/test/functional/apps/canvas/smoke_test.js
+++ x-pack/test/functional/apps/canvas/smoke_test.js
@@ -11,6 +11,7 @@ export default function canvasSmokeTest({ getService, getPageObjects }) {
   const esArchiver = getService('esArchiver');
   const testSubjects = getService('testSubjects');
   const remote = getService('remote');
+  const retry = getService('retry');
   const PageObjects = getPageObjects(['common']);
 
   describe('smoke test', async () => {
@@ -30,18 +31,19 @@ export default function canvasSmokeTest({ getService, getPageObjects }) {
     });
 
     it('loads workpad list', async () => {
-      const workpadRows = await testSubjects.findAll(workpadListSelector);
-      expect(workpadRows).to.have.length(1);
-      expect(await workpadRows[0].getVisibleText()).to.equal('Test Workpad');
+      await retry.try(async () => {
+        const workpadRows = await testSubjects.findAll(workpadListSelector);
+        expect(workpadRows).to.have.length(1);
+        expect(await workpadRows[0].getVisibleText()).to.equal('Test Workpad');
+      });
     });
 
     it('loads workpage when clicked', async () => {
       // click the first workpad in the list to load it
-      const link = await testSubjects.find(workpadListSelector);
-      await link.click();
+      await testSubjects.click(workpadListSelector);
 
       // wait for the workpad page to load
-      await testSubjects.find('canvasWorkpadPage');
+      await retry.waitFor('workpad page', () => testSubjects.exists('canvasWorkpadPage'));
 
       // check that workpad loaded in url
       const url = await remote.getCurrentUrl();
@@ -49,27 +51,31 @@ export default function canvasSmokeTest({ getService, getPageObjects }) {
     });
 
     it('renders elements on workpad', async () => {
-      // check for elements on the page
-      const elements = await testSubjects.findAll('canvasWorkpadPage canvasWorkpadPageElementContent');
-      expect(elements).to.have.length(4);
+      await retry.try(async () => {
+        // check for elements on the page
+        const elements = await testSubjects.findAll(
+          'canvasWorkpadPage canvasWorkpadPageElementContent'
+        );
+        expect(elements).to.have.length(4);
 
-      // check that the elements are what we expect
+        // check that the elements are what we expect
 
-      // first is a markdown element
-      const md = await elements[0].findByCssSelector('.canvasMarkdown');
-      expect(await md.getVisibleText()).to.contain('Welcome to Canvas');
+        // first is a markdown element
+        const md = await elements[0].findByCssSelector('.canvasMarkdown');
+        expect(await md.getVisibleText()).to.contain('Welcome to Canvas');
 
-      // second element is a datatable that uses essql
-      const serverRows = await elements[1].findAllByCssSelector('.canvasDataTable tbody tr');
-      expect(serverRows).to.have.length(10);
+        // second element is a datatable that uses essql
+        const serverRows = await elements[1].findAllByCssSelector('.canvasDataTable tbody tr');
+        expect(serverRows).to.have.length(10);
 
-      // third is a datatable that uses csv
-      const commonRows = await elements[2].findAllByCssSelector('.canvasDataTable tbody tr');
-      expect(commonRows).to.have.length(2);
+        // third is a datatable that uses csv
+        const commonRows = await elements[2].findAllByCssSelector('.canvasDataTable tbody tr');
+        expect(commonRows).to.have.length(2);
 
-      // fourth is a datatable that uses timelion
-      const timelionRows = await elements[3].findAllByCssSelector('.canvasDataTable tbody tr');
-      expect(timelionRows).to.have.length(12);
+        // fourth is a datatable that uses timelion
+        const timelionRows = await elements[3].findAllByCssSelector('.canvasDataTable tbody tr');
+        expect(timelionRows).to.have.length(12);
+      });
     });
   });
 }
