f0032023f042034c6948bc80dc156e23c09bd49e
Josh Dover <me@joshdover.com>
2018-11-20 12:46:28 -0600

    Allow console to load relative URLs

--- src/core_plugins/console/public/src/app.js
+++ src/core_plugins/console/public/src/app.js
@@ -22,7 +22,7 @@ const $ = require('jquery');
 const history = require('./history');
 const mappings = require('./mappings');
 
-export default function init(input, output, sourceLocation = 'stored') {
+export default function init(input, output, sourceLocation = 'stored', source) {
   $(document.body).removeClass('fouc');
 
   // set the value of the input and clear the output
@@ -66,16 +66,16 @@ export default function init(input, output, sourceLocation = 'stored') {
         input.autoIndent();
       }
     }
-    else if (/^https?:\/\//.test(sourceLocation)) {
+    else if (sourceLocation === 'http') {
       const loadFrom = {
-        url: sourceLocation,
+        url: source,
         // Having dataType here is required as it doesn't allow jQuery to `eval` content
         // coming from the external source thereby preventing XSS attack.
         dataType: 'text',
         kbnXsrfToken: false
       };
 
-      if (/https?:\/\/api.github.com/.test(sourceLocation)) {
+      if (/https?:\/\/api.github.com/.test(source)) {
         loadFrom.headers = { Accept: 'application/vnd.github.v3.raw' };
       }
 
--- src/core_plugins/console/public/src/controllers/sense_controller.js
+++ src/core_plugins/console/public/src/controllers/sense_controller.js
@@ -46,7 +46,14 @@ module.controller('SenseController', function SenseController(Private, $scope, $
   $timeout(async () => {
     output = initializeOutput($('#ConAppOutput'));
     input = initializeInput($('#ConAppEditor'), $('#ConAppEditorActions'), $('#ConCopyAsCurl'), output, $scope.openDocumentation);
-    init(input, output, $location.search().load_from);
+
+    const urlParams = $location.search();
+    if (urlParams.load_from) {
+      init(input, output, 'http', urlParams.load_from);
+    } else {
+      init(input, output);
+    }
+
     kbnUiAceKeyboardModeService.initialize($scope, $('#ConAppEditor'));
     const session = input.getSession();
     session.getSelection().on('changeCursor', () => {
