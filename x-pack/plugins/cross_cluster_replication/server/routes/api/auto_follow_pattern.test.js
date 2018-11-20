4f6c30367fadedeffd8ef4adb8922ec148ae7f6e
Sébastien Loix <sabee77@gmail.com>
2018-11-19 23:29:53 -0800

    Feature/ccr get auto follow patterns changes (#25875)
    * [CCR] CR changes for the auto follow patterns GET API route

--- x-pack/plugins/cross_cluster_replication/server/routes/api/auto_follow_pattern.test.js
+++ x-pack/plugins/cross_cluster_replication/server/routes/api/auto_follow_pattern.test.js
@@ -6,12 +6,47 @@
 
 
 import { callWithRequestFactory } from '../../lib/call_with_request_factory';
-import { registerAutoFollowPatternRoutes } from './register_auto_follow_pattern_routes';
+import { registerAutoFollowPatternRoutes } from './auto_follow_pattern';
 
 jest.mock('../../lib/call_with_request_factory', () => ({ callWithRequestFactory: jest.fn() }));
 jest.mock('../../lib/is_es_error_factory', () => ({ isEsErrorFactory: () => () => true }));
 jest.mock('../../lib/license_pre_routing_factory', () => ({ licensePreRoutingFactory: () => null }));
 
+/**
+ * Hashtable to save the route handlers
+ */
+const routeHandlers = {};
+
+/**
+ * Helper to extract all the different server route handler so we can easily call them in our tests.
+ *
+ * Important: This method registers the handlers in the order that they appear in the file, so
+ * if a "server.route()" call is moved or deleted, then the HANDLER_INDEX_TO_ACTION must be updated here.
+ */
+const registerHandlers = () => {
+  let index = 0;
+
+  const HANDLER_INDEX_TO_ACTION = {
+    0: 'list'
+  };
+
+  const server = {
+    route({ handler }) {
+      // Save handler and increment index
+      routeHandlers[HANDLER_INDEX_TO_ACTION[index]] = handler;
+      index++;
+    },
+  };
+
+  registerAutoFollowPatternRoutes(server);
+};
+
+/**
+ * Helper to mock the response from the call to Elasticsearch
+ *
+ * @param {*} err The mock error to throw
+ * @param {*} response The response to return
+ */
 const setHttpRequestResponse = (err, response) => {
   if (err) {
     return callWithRequestFactory.mockReturnValueOnce(() => {
@@ -22,24 +57,22 @@ const setHttpRequestResponse = (err, response) => {
   callWithRequestFactory.mockReturnValueOnce(() => response);
 };
 
-describe('[API Routes] Auto Follow Pattern', () => {
-  let server;
+describe('[CCR API Routes] Auto Follow Pattern', () => {
   let routeHandler;
 
-  beforeEach(() => {
-    server = {
-      route({ handler }) {
-        routeHandler = handler;
-      },
-    };
+  beforeAll(() => {
+    registerHandlers();
   });
 
   describe('list()', () => {
+    beforeEach(() => {
+      routeHandler = routeHandlers.list;
+    });
+
     it('should forward the response from Elasticsearch', async () => {
       const mock = { foo: 'bar' };
       setHttpRequestResponse(null, mock);
 
-      registerAutoFollowPatternRoutes(server);
       const response = await routeHandler();
 
       expect(response).toBe(mock);
@@ -48,7 +81,6 @@ describe('[API Routes] Auto Follow Pattern', () => {
     it('should prevent a 404 response on the route', async () => {
       setHttpRequestResponse({ status: 404 });
 
-      registerAutoFollowPatternRoutes(server);
       const response = await routeHandler();
 
       expect(response).toEqual({});
