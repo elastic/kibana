616b2049ed1037905bbc5c25c4b4a7543025a46a
Pete Harverson <peteharverson@users.noreply.github.com>
2018-11-21 15:53:47 +0000

    [ML] Ensures jobs from saved searches are cloned in advanced wizard (#26002)
    * [ML] Ensures jobs from saved searches are cloned in advanced wizard
    
    * [ML] Use useSavedSearch property for setting job created_by

--- x-pack/plugins/ml/public/jobs/new_job/simple/multi_metric/create_job/create_job_service.js
+++ x-pack/plugins/ml/public/jobs/new_job/simple/multi_metric/create_job/create_job_service.js
@@ -235,9 +235,13 @@ export function MultiMetricJobServiceProvider() {
         job.results_index_name = job.job_id;
       }
 
-      job.custom_settings = {
-        created_by: WIZARD_TYPE.MULTI_METRIC
-      };
+      if (formConfig.usesSavedSearch === false) {
+        // Jobs created from saved searches cannot be cloned in the wizard as the
+        // ML job config holds no reference to the saved search ID.
+        job.custom_settings = {
+          created_by: WIZARD_TYPE.MULTI_METRIC
+        };
+      }
 
       return job;
     }
