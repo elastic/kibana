6d58f773e97f1d90c983444e8d440775aa24ab93
Thomas Neirynck <thomas@elastic.co>
2018-11-21 17:21:30 -0500

    minor fixes

--- x-pack/plugins/gis/public/shared/layers/sources/wms_source.js
+++ x-pack/plugins/gis/public/shared/layers/sources/wms_source.js
@@ -21,11 +21,12 @@ export class WMSSource extends TMSSource {
 
   static typeDisplayName = 'WMS';
 
-  static createDescriptor({ serviceUrl, layers }) {
+  static createDescriptor({ serviceUrl, layers, styles }) {
     return {
       type: WMSSource.type,
       serviceUrl: serviceUrl,
-      layers: layers
+      layers: layers,
+      styles: styles
     };
   }
 
@@ -47,6 +48,8 @@ export class WMSSource extends TMSSource {
           <span className="eui-textBreakAll">{this._descriptor.serviceUrl}</span><br/>
           <strong className="gisLayerDetails__label">Layers: </strong>
           <span className="eui-textBreakAll">{this._descriptor.layers}</span><br/>
+          <strong className="gisLayerDetails__label">Styles: </strong>
+          <span className="eui-textBreakAll">{this._descriptor.styles}</span><br/>
         </p>
       </EuiText>
     );
@@ -72,8 +75,9 @@ export class WMSSource extends TMSSource {
 
   getUrlTemplate() {
     console.warn('should compose url using url formatter, not string formatting');
+    const styles = this._descriptor.styles ? this._descriptor.styles : '';
     // eslint-disable-next-line max-len
-    return `${this._descriptor.serviceUrl}?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=${this._descriptor.layers}&styles=${this._descriptor.styles}`;
+    return `${this._descriptor.serviceUrl}?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=${this._descriptor.layers}&styles=${styles}`;
   }
 }
 
@@ -137,7 +141,7 @@ class WMSEditor extends  React.Component {
             onChange={(e) => this._handleLayersChange(e)}
           />
         </EuiFormRow>
-        <EuiFormRow label="Layers (comma-separated list)">
+        <EuiFormRow label="Styles (comma-separated list)">
           <EuiFieldText
             onChange={(e) => this._handleStylesChange(e)}
           />
