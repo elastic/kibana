a22cd1cd71244d16309d12bbc99c420284b16cb0
Sébastien Loix <sabee77@gmail.com>
2018-11-23 16:36:12 +0100

    [Typescript]: Move elastic/eui typings to single file
    * [Typescript]: Move elastic/eui typings to single file
    
    * [Typescript]: Add typings folder to copyTask when building distributable

--- src/dev/build/tasks/copy_source_task.js
+++ src/dev/build/tasks/copy_source_task.js
@@ -40,6 +40,7 @@ export const CopySourceTask = {
         '!src/functional_test_runner/**',
         '!src/dev/**',
         'bin/**',
+        'typings/**',
         'webpackShims/**',
         'config/kibana.yml',
         'tsconfig*.json',
