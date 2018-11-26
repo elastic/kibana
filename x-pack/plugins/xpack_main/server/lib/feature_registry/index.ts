d3cc25c5f8cc8343b80a9dd7458dfb7b1fd65779
Larry Gregory <larry.gregory@elastic.co>
2018-11-26 16:32:45 -0500

    add missing export

--- x-pack/plugins/xpack_main/server/lib/feature_registry/index.ts
+++ x-pack/plugins/xpack_main/server/lib/feature_registry/index.ts
@@ -4,4 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { Feature, registerFeature, getFeatures } from './feature_registry';
+export { Feature, registerFeature, unregisterFeature, getFeatures } from './feature_registry';
