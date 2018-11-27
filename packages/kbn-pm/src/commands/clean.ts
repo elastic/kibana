b3ef265f692dfeab577efa618560ea2ab79d63f5
spalger <email@spalger.com>
2018-11-27 08:57:49 -0800

    [kbn/pm] document why we're using chdir instead of cwd

--- packages/kbn-pm/src/commands/clean.ts
+++ packages/kbn-pm/src/commands/clean.ts
@@ -61,6 +61,15 @@ export const CleanCommand: ICommand = {
     } else {
       log.write(chalk.bold.red('\n\nDeleting:\n'));
 
+      /**
+       * In order to avoid patterns like `/build` in packages from accidentally
+       * impacting files outside the package we use `process.chdir()` to change
+       * the cwd to the package and execute `del()` without the `force` option
+       * so it will check that each file being deleted is within the package.
+       *
+       * `del()` does support a `cwd` option, but it's only for resolving the
+       * patterns and does not impact the cwd check.
+       */
       const originalCwd = process.cwd();
       try {
         for (const { pattern, cwd } of toDelete) {
