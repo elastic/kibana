17b7fc251794b523f51dad67d021851b134f98c1
Marco Vettorello <vettorello.marco@gmail.com>
2018-11-28 09:56:31 +0100

    Decoupling UI Settings from buildEsQuery  (#25994)
    * Decuple the UI setting config from kbn-es-query package
    
    * Remove BuildESQueryProvider on ml utils

--- src/legacy/core_plugins/timelion/public/vis/timelion_request_handler.js
+++ src/legacy/core_plugins/timelion/public/vis/timelion_request_handler.js
@@ -18,12 +18,11 @@
  */
 
 import _ from 'lodash';
-import { BuildESQueryProvider } from '@kbn/es-query';
+import { buildEsQuery } from '@kbn/es-query';
 import { timezoneProvider } from 'ui/vis/lib/timezone';
 
-const TimelionRequestHandlerProvider = function (Private, Notifier, $http) {
+const TimelionRequestHandlerProvider = function (Private, Notifier, $http, config) {
   const timezone = Private(timezoneProvider)();
-  const buildEsQuery = Private(BuildESQueryProvider);
 
   const notify = new Notifier({
     location: 'Timelion'
@@ -36,12 +35,15 @@ const TimelionRequestHandlerProvider = function (Private, Notifier, $http) {
       return new Promise((resolve, reject) => {
         const expression = visParams.expression;
         if (!expression) return;
-
+        const esQueryConfigs = {
+          allowLeadingWildcards: config.get('query:allowLeadingWildcards'),
+          queryStringOptions: config.get('query:queryString:options'),
+        };
         const httpResult = $http.post('../api/timelion/run', {
           sheet: [expression],
           extended: {
             es: {
-              filter: buildEsQuery(undefined, [query], filters)
+              filter: buildEsQuery(undefined, [query], filters, esQueryConfigs)
             }
           },
           time: _.extend(timeRange, {
