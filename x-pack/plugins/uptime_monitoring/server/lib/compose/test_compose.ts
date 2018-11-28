996bc8b80e209bea96ebe4071af50a572a6e8acf
Justin Kambic <justin.kambic@elastic.co>
2018-11-28 16:19:59 -0500

    [Uptime Monitoring] Add user auth checks (#26070)
    * Add Uptime Monitoring (#25480)
    
    * Add authentication checks and tests. WIP.
    
    * Add auth adapter types. Finish work on adapter and update tests.
    
    * Add route errors. Rename license symbols.

--- x-pack/plugins/uptime_monitoring/server/lib/compose/test_compose.ts
+++ x-pack/plugins/uptime_monitoring/server/lib/compose/test_compose.ts
@@ -4,17 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { UMMemoryAuthAdapter } from '../adapters/auth';
 import { UMTestBackendFrameworkAdapter } from '../adapters/framework/test_backend_framework_adapter';
 import { MemoryPingsAdapter } from '../adapters/pings/memory_pings_adapter';
-import { UMPingsDomain } from '../domains';
+import { UMAuthDomain, UMPingsDomain } from '../domains';
 import { UMServerLibs } from '../lib';
 
 export function compose(server: any): UMServerLibs {
   const framework = new UMTestBackendFrameworkAdapter(server);
 
   const pingsDomain = new UMPingsDomain(new MemoryPingsAdapter(server.pingsDB || []), framework);
+  const authDomain = new UMAuthDomain(new UMMemoryAuthAdapter(server.xpack), framework);
 
   const libs: UMServerLibs = {
+    auth: authDomain,
     framework,
     pings: pingsDomain,
   };
--- x-pack/plugins/uptime_monitoring/server/lib/lib.ts
+++ x-pack/plugins/uptime_monitoring/server/lib/lib.ts
@@ -7,9 +7,11 @@
 import { DatabaseAdapter } from './adapters/database';
 import { UMBackendFrameworkAdapter } from './adapters/framework';
 import { UMPingsDomain } from './domains';
+import { UMAuthDomain } from './domains/auth';
 
 export interface UMDomainLibs {
   pings: UMPingsDomain;
+  auth: UMAuthDomain;
 }
 
 export interface UMServerLibs extends UMDomainLibs {
