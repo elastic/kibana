544552bb44135ec4deb2e05b75ceadf832fb78fc
Catherine Liu <catherineqliu@outlook.com>
2018-11-28 16:43:44 -0700

    Scrolls to selected asset

--- x-pack/plugins/canvas/public/components/asset_picker/asset_picker.js
+++ x-pack/plugins/canvas/public/components/asset_picker/asset_picker.js
@@ -4,40 +4,46 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { EuiFlexGrid, EuiFlexItem, EuiLink, EuiImage, EuiIcon } from '@elastic/eui';
 
-export const AssetPicker = ({ assets, selected, onChange }) => {
-  assets = assets.reduce((acc, asset) => {
-    // move selected asset to the front
-    if (asset.id === selected) acc.unshift(asset);
-    else acc.push(asset);
-    return acc;
-  }, []);
+export class AssetPicker extends PureComponent {
+  static propTypes = {
+    assets: PropTypes.array,
+    selected: PropTypes.string,
+    onChange: PropTypes.func,
+  };
 
-  return (
-    <EuiFlexGrid className="canvasAssetPicker" gutterSize="s" columns={4}>
-      {assets.map(asset => (
-        <EuiFlexItem key={asset.id} className="canvasCheckered">
-          <EuiLink
-            className={`canvasAssetPicker__link`}
-            disabled={asset.id === selected}
-            onClick={() => onChange(asset)}
+  componentDidMount() {
+    const selectedAsset = document.getElementById('canvasAssetPicker__selectedAsset');
+    if (selectedAsset) selectedAsset.scrollIntoView();
+  }
+
+  render() {
+    const { assets, selected, onChange } = this.props;
+
+    return (
+      <EuiFlexGrid id="canvasAssetPicker" className="canvasAssetPicker" gutterSize="s" columns={4}>
+        {assets.map(asset => (
+          <EuiFlexItem
+            key={asset.id}
+            id={asset.id === selected ? 'canvasAssetPicker__selectedAsset' : ''}
+            className="canvasCheckered"
           >
-            <EuiImage url={asset.value} alt="Asset thumbnail" />
-            {asset.id === selected && (
-              <EuiIcon className="canvasAssetPicker__selected" type="checkInCircleFilled" />
-            )}
-          </EuiLink>
-        </EuiFlexItem>
-      ))}
-    </EuiFlexGrid>
-  );
-};
-
-AssetPicker.propTypes = {
-  assets: PropTypes.array,
-  selected: PropTypes.string,
-  onChange: PropTypes.func,
-};
+            <EuiLink
+              className={`canvasAssetPicker__link`}
+              disabled={asset.id === selected}
+              onClick={() => onChange(asset)}
+            >
+              <EuiImage url={asset.value} alt="Asset thumbnail" />
+              {asset.id === selected && (
+                <EuiIcon className="canvasAssetPicker__selected" type="checkInCircleFilled" />
+              )}
+            </EuiLink>
+          </EuiFlexItem>
+        ))}
+      </EuiFlexGrid>
+    );
+  }
+}
