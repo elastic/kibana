1941a415774d48c74394888ab3f9514ee888292f
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-11-29 19:42:13 +0100

    [APM] Refactor Discover buttons

--- x-pack/plugins/apm/public/components/shared/DiscoverButtons/DiscoverButton.tsx
+++ x-pack/plugins/apm/public/components/shared/DiscoverButtons/DiscoverButton.tsx
@@ -4,31 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// @ts-ignore
-import { EuiButtonEmpty } from '@elastic/eui';
 import React from 'react';
 import { StringMap } from 'x-pack/plugins/apm/typings/common';
-import { KibanaLink } from '../../utils/url';
+import { KibanaLink } from '../../../utils/url';
 
-interface Props {
+export const DiscoverButton: React.SFC<{
   query: StringMap;
-}
-
-export const DiscoverButton: React.SFC<Props> = ({
-  query,
-  children,
-  ...rest
-}) => {
+}> = ({ query, children, ...rest }) => {
   return (
     <KibanaLink
       pathname={'/app/kibana'}
       hash={'/discover'}
       query={query}
+      children={children}
       {...rest}
-    >
-      <EuiButtonEmpty iconType="discoverApp">
-        {children || 'View in Discover'}
-      </EuiButtonEmpty>
-    </KibanaLink>
+    />
   );
 };
