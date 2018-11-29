b112b7168f84befef434d175e25c5331374870ca
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2018-11-29 14:17:22 +0100

    [core_plugins/test_bundle] fix paths.

--- src/legacy/core_plugins/tests_bundle/tests_entry_template.js
+++ src/legacy/core_plugins/tests_bundle/tests_entry_template.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import pkg from '../../../package.json';
+import pkg from '../../../../package.json';
 
 export const createTestEntryTemplate = (defaultUiSettings) => (bundle) => `
 /**
