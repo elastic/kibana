1a7d43572c3fec13c7635704e34c147dffddfe0d
Timothy Sullivan <tsullivan@elastic.co>
2018-11-30 12:57:35 -0700

    format the stats for telemetry

--- x-pack/plugins/xpack_main/server/lib/telemetry/local/get_kibana.js
+++ x-pack/plugins/xpack_main/server/lib/telemetry/local/get_kibana.js
@@ -4,12 +4,29 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { get } from 'lodash';
+
 /*
  * Check user privileges for read access to monitoring
  * Pass callWithInternalUser to bulkFetchUsage
  */
 export async function getKibana(server, callCluster) {
   const { collectorSet } = server.usage;
-  const usage = await collectorSet.bulkFetchUsage(callCluster);
-  return collectorSet.toObject(usage);
+  const usage = await collectorSet.bulkFetch(callCluster);
+  const { kibana, kibana_stats: stats, ...plugins } = collectorSet.toObject(usage);
+
+  const platform = get(stats, 'os.platform');
+  const platformRelease = get(stats, 'os.platformRelease');
+
+  // organize the object into telemetry format
+  return {
+    count: 1,
+    indices: 1,
+    distros: {
+      platforms: [platform],
+      platformReleases: [platformRelease],
+    },
+    plugins,
+    ...kibana,
+  };
 }
---
+++ x-pack/test/api_integration/apis/xpack_main/telemetry/telemetry_local.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from 'expect.js';
+
+const disableCollection = {
+  "persistent":
+  {
+    xpack: {
+      monitoring: {
+        collection: {
+          enabled: false
+        }
+      }
+    }
+  }
+};
+
+export default function ({ getService }) {
+  const supertest = getService('supertest');
+  const esSupertest = getService('esSupertest');
+
+  describe('/api/telemetry/v1/clusters/_stats with monitoring disabled', () => {
+    before('', async () => {
+      await esSupertest.put('/_cluster/settings').send(disableCollection).expect(200);
+    });
+
+    it('should pull local stats', async () => {
+      const timeRange = {
+        min: '2018-07-23T22:07:00Z',
+        max: '2018-07-23T22:13:00Z'
+      };
+
+      const { body } = await supertest
+        .post('/api/telemetry/v1/clusters/_stats')
+        .set('kbn-xsrf', 'xxx')
+        .send({ timeRange })
+        .expect(200);
+
+      expect(body.length).to.be(1);
+      const stats = body[0];
+
+      expect(stats.collection).to.be('local');
+      expect(stats.license.issuer).to.be('elasticsearch');
+      expect(stats.license.status).to.be('active');
+
+      expect(stats.stack_stats.kibana.index).to.be('.kibana');
+
+      expect(stats.stack_stats.kibana.count).to.be(1);
+      expect(stats.stack_stats.kibana.indices).to.be(1);
+
+      expect(stats.stack_stats.kibana.dashboard.total).to.be.a('number');
+      expect(stats.stack_stats.kibana.graph_workspace.total).to.be.a('number');
+      expect(stats.stack_stats.kibana.index_pattern.total).to.be.a('number');
+      expect(stats.stack_stats.kibana.search.total).to.be.a('number');
+      expect(stats.stack_stats.kibana.timelion_sheet.total).to.be.a('number');
+      expect(stats.stack_stats.kibana.visualization.total).to.be.a('number');
+
+      expect(stats.stack_stats.kibana.plugins.apm.services_per_agent).to.be.an('object');
+      expect(stats.stack_stats.kibana.plugins.infraops.last_24_hours).to.be.an('object');
+      expect(stats.stack_stats.kibana.plugins.kql.defaultQueryLanguage).to.be.a('string');
+      expect(stats.stack_stats.kibana.plugins.reporting.enabled).to.be(true);
+      expect(stats.stack_stats.kibana.plugins.rollups.index_patterns).to.be.an('object');
+      expect(stats.stack_stats.kibana.plugins.spaces.available).to.be(true);
+
+      expect(stats.stack_stats.kibana.distros).to.be.an('object');
+      expect(stats.stack_stats.kibana.distros.platforms).to.be.an('array');
+      expect(stats.stack_stats.kibana.distros.platformReleases).to.be.an('array');
+
+      expect(stats.stack_stats.xpack.graph).to.be.an('object');
+      expect(stats.stack_stats.xpack.ilm).to.be.an('object');
+      expect(stats.stack_stats.xpack.logstash).to.be.an('object');
+      expect(stats.stack_stats.xpack.ml).to.be.an('object');
+      expect(stats.stack_stats.xpack.monitoring).to.be.an('object');
+      expect(stats.stack_stats.xpack.rollup).to.be.an('object');
+    });
+  });
+}
+
