a6f2cf43200a65479dbe088245806dd80f105a34
maryia-lapata <mary.lopato@gmail.com>
2018-12-03 15:03:02 +0300

    Fix CI

--- x-pack/plugins/reporting/public/constants/job_statuses.tsx
+++ x-pack/plugins/reporting/public/constants/job_statuses.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export const enum JobStatuses {
+export enum JobStatuses {
   PENDING = 'pending',
   PROCESSING = 'processing',
   COMPLETED = 'completed',
