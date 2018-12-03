1109277bada8804a1eda0d6a55c9a1cee99e80c6
spalger <email@spalger.com>
2018-12-03 09:32:37 -0800

    fix i18n message id

--- src/ui/public/management/breadcrumbs.js
+++ src/ui/public/management/breadcrumbs.js
@@ -20,7 +20,7 @@
 import { i18n } from '@kbn/i18n';
 
 export const MANAGEMENT_BREADCRUMB = Object.freeze({
-  text: i18n.translate('kbn.management.breadcrum', {
+  text: i18n.translate('common.ui.management.breadcrumb', {
     defaultMessage: 'Management'
   }),
   href: '#/management'
