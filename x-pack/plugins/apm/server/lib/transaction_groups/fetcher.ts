05762c41d00f7c2b22a762e4b4b47b7e74d86c67
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-12-04 00:13:13 +0100

    [APM] Move impact calculation to Elasticsearch (#26436)
    * [APM] Move impact calculation to Elasticsearch
    
    * Renamed “durationSum” to “sum” and went back to single, mutable impact

--- x-pack/plugins/apm/server/lib/transaction_groups/fetcher.ts
+++ x-pack/plugins/apm/server/lib/transaction_groups/fetcher.ts
@@ -16,14 +16,9 @@ import { Setup } from '../helpers/setup_request';
 interface Bucket {
   key: string;
   doc_count: number;
-  avg: {
-    value: number;
-  };
-  p95: {
-    values: {
-      '95.0': number;
-    };
-  };
+  avg: { value: number };
+  p95: { values: { '95.0': number } };
+  sum: { value: number };
   sample: {
     hits: {
       total: number;
@@ -57,7 +52,7 @@ export function transactionGroupsFetcher(
         transactions: {
           terms: {
             field: `${TRANSACTION_NAME}.keyword`,
-            order: { avg: 'desc' },
+            order: { sum: 'desc' },
             size: 100
           },
           aggs: {
@@ -73,7 +68,8 @@ export function transactionGroupsFetcher(
             avg: { avg: { field: TRANSACTION_DURATION } },
             p95: {
               percentiles: { field: TRANSACTION_DURATION, percents: [95] }
-            }
+            },
+            sum: { sum: { field: TRANSACTION_DURATION } }
           }
         }
       }
