af685ce26817e3c65ae4918e9029df1b1ecaa71b
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-12-04 11:39:30 +0100

    Make DiscoverButton indexPattern aware

--- x-pack/plugins/apm/public/components/shared/DiscoverButtons/DiscoverButton.tsx
+++ x-pack/plugins/apm/public/components/shared/DiscoverButtons/DiscoverButton.tsx
@@ -4,20 +4,46 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { get } from 'lodash';
 import React from 'react';
 import { StringMap } from 'x-pack/plugins/apm/typings/common';
+import {
+  getAPMIndexPattern,
+  ISavedObject
+} from '../../../services/rest/savedObjects';
 import { KibanaLink } from '../../../utils/url';
 
-export const DiscoverButton: React.SFC<{
+interface Props {
   query: StringMap;
-}> = ({ query, children, ...rest }) => {
-  return (
-    <KibanaLink
-      pathname={'/app/kibana'}
-      hash={'/discover'}
-      query={query}
-      children={children}
-      {...rest}
-    />
-  );
-};
+}
+
+interface State {
+  indexPattern?: ISavedObject;
+}
+
+export class DiscoverButton extends React.Component<Props, State> {
+  public state = {};
+  public async componentDidMount() {
+    const indexPattern = await getAPMIndexPattern();
+    this.setState({ indexPattern });
+  }
+
+  public render() {
+    const { query, children, ...rest } = this.props;
+    const id: string = get(this.state, 'indexPattern.id');
+
+    if (!query._a.index) {
+      query._a.index = id;
+    }
+
+    return (
+      <KibanaLink
+        pathname={'/app/kibana'}
+        hash={'/discover'}
+        query={query}
+        children={children}
+        {...rest}
+      />
+    );
+  }
+}
