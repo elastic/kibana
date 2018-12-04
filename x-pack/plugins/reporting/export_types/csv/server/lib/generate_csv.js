230d4768abd6ecc112e9c93dcae16562826382ea
Joel Griffith <joel.griffith@elastic.co>
2018-12-04 14:36:58 -0800

    Adding a `size` field for reports, logging them and exposing them in report UI

--- x-pack/plugins/reporting/export_types/csv/server/lib/generate_csv.js
+++ x-pack/plugins/reporting/export_types/csv/server/lib/generate_csv.js
@@ -58,12 +58,13 @@ export function createGenerateCsv(logger) {
     } finally {
       await iterator.return();
     }
+    const size = builder.getSizeInBytes();
+    logger(`finished generating, total size in bytes: ${size}`);
 
-    logger('finished generating');
     return {
       content: builder.getString(),
       maxSizeReached,
-      size: builder.getSizeInBytes(),
+      size,
     };
   };
 }
