83325bf560bdc5fea43e5a1bb9e7efa078634ae7
Peter Pisljar <peter.pisljar@gmail.com>
2018-12-05 06:36:51 +0100

    back to canvas way of loading plugins (#26463)

--- packages/kbn-interpreter/src/server/plugin_paths.js
+++ packages/kbn-interpreter/src/server/plugin_paths.js
@@ -17,9 +17,17 @@
  * under the License.
  */
 
-const { resolve } = require('path');
-
-exports.pluginPaths = {
-  commonFunctions: resolve(__dirname, 'target/plugin/functions/common'),
-  types: resolve(__dirname, 'target/plugin/types'),
+export const pluginPaths = {
+  serverFunctions: ['functions', 'server'],
+  browserFunctions: ['functions', 'browser'],
+  commonFunctions: ['functions', 'common'],
+  types: ['types'],
+  elements: ['elements'],
+  renderers: ['renderers'],
+  interfaces: ['interfaces'],
+  transformUIs: ['uis', 'transforms'],
+  datasourceUIs: ['uis', 'datasources'],
+  modelUIs: ['uis', 'models'],
+  viewUIs: ['uis', 'views'],
+  argumentUIs: ['uis', 'arguments'],
 };
