74a5b5f4e1896b83af5e65eaa3053ba97338fdf0
Larry Gregory <lgregorydev@gmail.com>
2018-12-06 05:47:57 -0800

    Update x-pack/plugins/security/server/lib/authorization/privilege_serializer.test.ts
    Co-Authored-By: kobelb <brandon.kobel@gmail.com>

--- x-pack/plugins/security/server/lib/authorization/privilege_serializer.test.ts
+++ x-pack/plugins/security/server/lib/authorization/privilege_serializer.test.ts
@@ -16,7 +16,7 @@ describe(`#isGlobalMinimumPrivilege`, () => {
   ['space_all', 'space_read', 'foo', 'bar', 'feature_foo', 'feature_foo.privilege1'].forEach(
     validValue => {
       test(`returns false for '${invalidValue}'`, () => {
-        expect(PrivilegeSerializer.isGlobalMinimumPrivilege(validValue)).toBe(false);
+        expect(PrivilegeSerializer.isGlobalMinimumPrivilege(invalidValue)).toBe(false);
       });
     }
   );
