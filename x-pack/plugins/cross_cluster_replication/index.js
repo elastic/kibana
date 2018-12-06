ad1e3754c05c6c404ca8e8afad0fedf47a10074e
CJ Cenizal <cj@cenizal.com>
2018-12-06 15:15:31 -0800

    [CCR] Support settings for disabling the CCR and RCM UIs (#26665)
    * Add support for xpack.ccr.ui.enabled and xpack.remote_clusters.ui.enabled settings.

--- x-pack/plugins/cross_cluster_replication/index.js
+++ x-pack/plugins/cross_cluster_replication/index.js
@@ -12,13 +12,33 @@ import { registerRoutes } from './server/routes/register_routes';
 export function crossClusterReplication(kibana) {
   return new kibana.Plugin({
     id: PLUGIN.ID,
-    configPrefix: 'xpack.cross_cluster_replication',
+    configPrefix: 'xpack.ccr',
     publicDir: resolve(__dirname, 'public'),
     require: ['kibana', 'elasticsearch', 'xpack_main'],
+
     uiExports: {
       styleSheetPaths: `${__dirname}/public/index.scss`,
       managementSections: ['plugins/cross_cluster_replication'],
+      injectDefaultVars(server) {
+        const config = server.config();
+        return {
+          ccrUiEnabled: config.get('xpack.ccr.ui.enabled'),
+        };
+      },
     },
+
+    config(Joi) {
+      return Joi.object({
+        // display menu item
+        ui: Joi.object({
+          enabled: Joi.boolean().default(true)
+        }).default(),
+
+        // enable plugin
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
     init: function initCcrPlugin(server) {
       registerLicenseChecker(server);
       registerRoutes(server);
