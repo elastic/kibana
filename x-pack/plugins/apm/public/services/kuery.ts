0cab638ef0891a32c3316e66cb6aa00284ce2bc9
Søren Louv-Jansen <sorenlouv@gmail.com>
2018-12-06 23:46:26 +0100

    [6.x] [APM] Discover buttons (#26622) (#26768)
    * [APM] Discover buttons (#26622)
    
    * Fix order of imports

--- x-pack/plugins/apm/public/services/kuery.ts
+++ x-pack/plugins/apm/public/services/kuery.ts
@@ -4,22 +4,27 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { fromKueryExpression, toElasticsearchQuery } from 'ui/kuery';
 import { getAutocompleteProvider } from 'ui/autocomplete_providers';
+// @ts-ignore
+import { getFromSavedObject } from 'ui/index_patterns/static_utils';
+import { fromKueryExpression, toElasticsearchQuery } from 'ui/kuery';
+import { getAPMIndexPattern } from './rest/savedObjects';
 
-export function convertKueryToEsQuery(kuery, indexPattern) {
+export function convertKueryToEsQuery(kuery: string, indexPattern: any) {
   const ast = fromKueryExpression(kuery);
   return toElasticsearchQuery(ast, indexPattern);
 }
 
 export async function getSuggestions(
-  query,
-  selectionStart,
-  apmIndexPattern,
-  boolFilter
+  query: string,
+  selectionStart: number,
+  apmIndexPattern: any,
+  boolFilter: any
 ) {
   const autocompleteProvider = getAutocompleteProvider('kuery');
-  if (!autocompleteProvider) return [];
+  if (!autocompleteProvider) {
+    return [];
+  }
   const config = {
     get: () => true
   };
@@ -34,4 +39,19 @@ export async function getSuggestions(
     selectionStart,
     selectionEnd: selectionStart
   });
+}
+
+interface IIndexPatternForKuery {
+  title: string;
+  fields: any[];
+}
+
+export async function getAPMIndexPatternForKuery(): Promise<
+  IIndexPatternForKuery | undefined
+> {
+  const apmIndexPattern = await getAPMIndexPattern();
+  if (!apmIndexPattern) {
+    return;
+  }
+  return getFromSavedObject(apmIndexPattern);
 }
