51cea7220915aac4a29b1f5118f68eb3540187a0
Tyler Smalley <tyler.smalley@elastic.co>
2018-12-07 11:01:00 -0800

    [ci] Use trap to generate report for non-PR build
    Since this is no longer part of a reporting task in Jenkins, we no
    longer need to re-run kbn bootstrap when generating a report.
    
    We use the PR_SOURCE_BRANCH environment variable to prevent genererating
    Github issues when we are on a PR.
    
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- test/scripts/jenkins_ci_group.sh
+++ test/scripts/jenkins_ci_group.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+function report {
+  if [[ -z "$PR_SOURCE_BRANCH" ]]; then
+    node src/dev/failed_tests/cli
+  else
+    echo "Failure issues not created on pull requests"
+
+  fi
+}
+
+trap report EXIT
+
 source src/dev/ci_setup/checkout_sibling_es.sh
 
 "$(FORCE_COLOR=0 yarn bin)/grunt" functionalTests:ensureAllTestsInCiGroup;
