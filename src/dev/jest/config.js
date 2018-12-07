0ed4726bd09f415d0d8c2662aa852cc8faa0afa6
Nicholas Dziedzic <nicholas.dziedzic@elastic.co>
2018-12-07 13:01:09 -0500

    Separates configs for jest integration core/x-pack.

--- src/dev/jest/config.js
+++ src/dev/jest/config.js
@@ -32,7 +32,6 @@ export default {
     '<rootDir>/src/utils',
     '<rootDir>/src/setup_node_env',
     '<rootDir>/packages',
-    '<rootDir>/x-pack',
   ],
   collectCoverageFrom: [
     'packages/kbn-ui-framework/src/components/**/*.js',
