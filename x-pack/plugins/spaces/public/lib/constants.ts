faea1fba683d556dd489736d035eca94cb9bdb91
Alexandr Ogarkov <nox.911@gmail.com>
2018-12-07 10:22:32 +0300

    Fix issues

--- x-pack/plugins/spaces/public/lib/constants.ts
+++ x-pack/plugins/spaces/public/lib/constants.ts
@@ -4,26 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 import { i18n } from '@kbn/i18n';
 import chrome from 'ui/chrome';
 
-let SpacesFeatureDescription;
+let spacesFeatureDescription: string;
 
 export const getSpacesFeatureDescription = () => {
-  if (!SpacesFeatureDescription) {
-    SpacesFeatureDescription = i18n.translate('xpack.spaces.featureDescription', {
+  if (!spacesFeatureDescription) {
+    spacesFeatureDescription = i18n.translate('xpack.spaces.featureDescription', {
       defaultMessage:
         'Organize your dashboards and other saved objects into meaningful categories.',
     });
   }
 
-  return SpacesFeatureDescription;
+  return spacesFeatureDescription;
 };
 
 export const MANAGE_SPACES_URL = chrome.addBasePath(`/app/kibana#/management/spaces/list`);
