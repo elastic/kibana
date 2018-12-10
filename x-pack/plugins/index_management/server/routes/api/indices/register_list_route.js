ec145129b01cc034db34fb5759f025df6c13f822
Bill McConaghy <bill.mcconaghy@elastic.co>
2018-12-10 15:06:09 -0500

    Show aliases in index details (#24048) (#26912)
    * Adds aliases to details for indicies.
    
    * Lists aliases when there is more than one.
    
    Fixes reload call using duplicate code atm.
    Removes unintentional formatting change.
    
    * Silly line ending removed by accident.
    
    * Simplifies fetching aliases to single method/file.
    
    * Adds superfluous logs statement.
    
    * Uses the correct i18n translation variable for the header.
    
    * Sets the translation variable to the correctly spelled variable.
    
    * Simplifies building an array of aliases.
    
    * Does not export fetchAliases as default.
    
    * Makes the code more readable by destructing results.
    
    * Adds test for expected results and no results.

--- x-pack/plugins/index_management/server/routes/api/indices/register_list_route.js
+++ x-pack/plugins/index_management/server/routes/api/indices/register_list_route.js
@@ -8,8 +8,9 @@ import { callWithRequestFactory } from '../../../lib/call_with_request_factory';
 import { isEsErrorFactory } from '../../../lib/is_es_error_factory';
 import { wrapEsError, wrapUnknownError } from '../../../lib/error_wrappers';
 import { licensePreRoutingFactory } from'../../../lib/license_pre_routing_factory';
+import { fetchAliases } from './fetch_aliases';
 
-function formatHits(hits) {
+function formatHits(hits, aliases) {
   return hits.map(hit => {
     return {
       health: hit.health,
@@ -22,6 +23,7 @@ function formatHits(hits) {
       documents_deleted: hit["docs.deleted"],
       size: hit["store.size"],
       primary_size: hit["pri.store.size"],
+      aliases: aliases.hasOwnProperty(hit.index) ? aliases[hit.index] : 'none',
     };
   });
 }
@@ -45,8 +47,9 @@ export function registerListRoute(server) {
       const callWithRequest = callWithRequestFactory(server, request);
 
       try {
+        const aliases = await fetchAliases(callWithRequest);
         const hits = await fetchIndices(callWithRequest);
-        const response = formatHits(hits);
+        const response = formatHits(hits, aliases);
         return response;
       } catch (err) {
         if (isEsError(err)) {
