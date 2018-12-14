dc5b33a9ec26651c5036fd6d8b91708278e1eece
Melissa Alvarez <melissa.alvarez@elastic.co>
2018-12-14 16:08:47 -0600

    [6.x] [ML] Converts Settings page to react (#27144) (#27245)
    * [ML] Converts Settings page to react (#27144)
    
    * Replace settings page with react directive
    
    * Adds test for Settings component
    
    * add calendar permission check
    
    * Update settings test
    
    * Remove outdated angular settings tests
    
    * Remove k7 breadcrumbs

---
+++ x-pack/plugins/ml/public/settings/settings_directive.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+
+import 'ngreact';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { uiModules } from 'ui/modules';
+const module = uiModules.get('apps/ml', ['react']);
+
+import { checkFullLicense } from '../license/check_license';
+import { checkGetJobsPrivilege, checkPermission } from '../privilege/check_privilege';
+import { getMlNodeCount } from '../ml_nodes_check/check_ml_nodes';
+import { initPromise } from '../util/promise';
+
+import uiRoutes from 'ui/routes';
+import { timefilter } from 'ui/timefilter';
+
+const template = `
+  <ml-nav-menu name="settings" />
+  <div class="mlSettingsPage">
+    <ml-settings />
+  </div>
+`;
+
+uiRoutes
+  .when('/settings', {
+    template,
+    resolve: {
+      CheckLicense: checkFullLicense,
+      privileges: checkGetJobsPrivilege,
+      mlNodeCount: getMlNodeCount,
+      initPromise: initPromise(false)
+    }
+  });
+
+
+import { Settings } from './settings.js';
+
+module.directive('mlSettings', function () {
+
+  timefilter.disableTimeRangeSelector(); // remove time picker from top of page
+  timefilter.disableAutoRefreshSelector(); // remove time picker from top of page
+
+  const canCreateFilter = checkPermission('canCreateFilter');
+  const canCreateCalendar = checkPermission('canCreateCalendar');
+
+  return {
+    restrict: 'E',
+    replace: false,
+    scope: {},
+    link: function (scope, element) {
+      ReactDOM.render(
+        React.createElement(Settings, { canCreateFilter, canCreateCalendar }),
+        element[0]
+      );
+    }
+  };
+});
