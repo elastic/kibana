29ffb0cd363ca9408ab619d25c89153b00c2fcdd
Matt Simerson <matt@tnpi.net>
2018-12-14 13:38:55 -0800

    revert whitespace

--- x-pack/plugins/reporting/server/browsers/phantom/paths.js
+++ x-pack/plugins/reporting/server/browsers/phantom/paths.js
@@ -9,7 +9,7 @@ import path from 'path';
 export const paths = {
   archivesPath: path.resolve(__dirname, '../../../.phantom'),
   baseUrl: 'https://github.com/Medium/phantomjs/releases/download/v2.1.1/',
-  packages: [ {
+  packages: [{
     platforms: ['darwin'],
     archiveFilename: 'phantomjs-2.1.1-macosx.zip',
     archiveChecksum: 'b0c038bd139b9ecaad8fd321070c1651',
