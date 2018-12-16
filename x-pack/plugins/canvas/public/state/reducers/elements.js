e7c90b3e20d0f3f60c9e45f9f4444763660ab3d5
monfera <monfera.robert@gmail.com>
2018-12-16 23:01:31 +0100

    renamed `element...` to `node...` (except exported names and action payload props, for now)

--- x-pack/plugins/canvas/public/state/reducers/elements.js
+++ x-pack/plugins/canvas/public/state/reducers/elements.js
@@ -11,8 +11,8 @@ import * as actions from '../actions/elements';
 
 const getLocation = type => (type === 'group' ? 'groups' : 'elements');
 
-const getLocationFromIds = (workpadState, pageId, elementId) =>
-  workpadState.pages.find(p => p.id === pageId).groups.find(e => e.id === elementId)
+const getLocationFromIds = (workpadState, pageId, nodeId) =>
+  workpadState.pages.find(p => p.id === pageId).groups.find(e => e.id === nodeId)
     ? 'groups'
     : 'elements';
 
@@ -20,47 +20,45 @@ function getPageIndexById(workpadState, pageId) {
   return get(workpadState, 'pages', []).findIndex(page => page.id === pageId);
 }
 
-function getElementIndexById(page, elementId, location) {
-  return page[location].findIndex(element => element.id === elementId);
+function getNodeIndexById(page, nodeId, location) {
+  return page[location].findIndex(node => node.id === nodeId);
 }
 
-function assignElementProperties(workpadState, pageId, elementId, props) {
+function assignNodeProperties(workpadState, pageId, nodeId, props) {
   const pageIndex = getPageIndexById(workpadState, pageId);
-  const location = getLocationFromIds(workpadState, pageId, elementId);
-  const elementsPath = ['pages', pageIndex, location];
-  const elementIndex = get(workpadState, elementsPath, []).findIndex(
-    element => element.id === elementId
-  );
+  const location = getLocationFromIds(workpadState, pageId, nodeId);
+  const nodesPath = ['pages', pageIndex, location];
+  const nodeIndex = get(workpadState, nodesPath, []).findIndex(node => node.id === nodeId);
 
-  if (pageIndex === -1 || elementIndex === -1) return workpadState;
+  if (pageIndex === -1 || nodeIndex === -1) return workpadState;
 
   // remove any AST value from the element caused by https://github.com/elastic/kibana-canvas/issues/260
   // TODO: remove this after a bit of time
-  const cleanWorkpadState = del(workpadState, elementsPath.concat([elementIndex, 'ast']));
+  const cleanWorkpadState = del(workpadState, nodesPath.concat([nodeIndex, 'ast']));
 
-  return assign(cleanWorkpadState, elementsPath.concat(elementIndex), props);
+  return assign(cleanWorkpadState, nodesPath.concat(nodeIndex), props);
 }
 
-function moveElementLayer(workpadState, pageId, elementId, movement, location) {
+function moveNodeLayer(workpadState, pageId, nodeId, movement, location) {
   const pageIndex = getPageIndexById(workpadState, pageId);
-  const elementIndex = getElementIndexById(workpadState.pages[pageIndex], elementId, location);
-  const elements = get(workpadState, ['pages', pageIndex, location]);
-  const from = elementIndex;
+  const nodeIndex = getNodeIndexById(workpadState.pages[pageIndex], nodeId, location);
+  const nodes = get(workpadState, ['pages', pageIndex, location]);
+  const from = nodeIndex;
 
   const to = (function() {
-    if (movement < Infinity && movement > -Infinity) return elementIndex + movement;
-    if (movement === Infinity) return elements.length - 1;
+    if (movement < Infinity && movement > -Infinity) return nodeIndex + movement;
+    if (movement === Infinity) return nodes.length - 1;
     if (movement === -Infinity) return 0;
     throw new Error('Invalid element layer movement');
   })();
 
-  if (to > elements.length - 1 || to < 0) return workpadState;
+  if (to > nodes.length - 1 || to < 0) return workpadState;
 
   // Common
-  const newElements = elements.slice(0);
-  newElements.splice(to, 0, newElements.splice(from, 1)[0]);
+  const newNodes = nodes.slice(0);
+  newNodes.splice(to, 0, newNodes.splice(from, 1)[0]);
 
-  return set(workpadState, ['pages', pageIndex, location], newElements);
+  return set(workpadState, ['pages', pageIndex, location], newNodes);
 }
 
 export const elementsReducer = handleActions(
@@ -68,21 +66,21 @@ export const elementsReducer = handleActions(
     // TODO: This takes the entire element, which is not necessary, it could just take the id.
     [actions.setExpression]: (workpadState, { payload }) => {
       const { expression, pageId, elementId } = payload;
-      return assignElementProperties(workpadState, pageId, elementId, { expression });
+      return assignNodeProperties(workpadState, pageId, elementId, { expression });
     },
     [actions.setFilter]: (workpadState, { payload }) => {
       const { filter, pageId, elementId } = payload;
-      return assignElementProperties(workpadState, pageId, elementId, { filter });
+      return assignNodeProperties(workpadState, pageId, elementId, { filter });
     },
     [actions.setMultiplePositions]: (workpadState, { payload }) =>
       payload.repositionedElements.reduce(
         (previousWorkpadState, { position, pageId, elementId }) =>
-          assignElementProperties(previousWorkpadState, pageId, elementId, { position }),
+          assignNodeProperties(previousWorkpadState, pageId, elementId, { position }),
         workpadState
       ),
     [actions.elementLayer]: (workpadState, { payload: { pageId, elementId, movement } }) => {
       const location = getLocationFromIds(workpadState, pageId, elementId);
-      return moveElementLayer(workpadState, pageId, elementId, movement, location);
+      return moveNodeLayer(workpadState, pageId, elementId, movement, location);
     },
     [actions.addElement]: (workpadState, { payload: { pageId, element } }) => {
       const pageIndex = getPageIndexById(workpadState, pageId);
@@ -98,17 +96,17 @@ export const elementsReducer = handleActions(
       const pageIndex = getPageIndexById(workpadState, pageId);
       if (pageIndex < 0) return workpadState;
 
-      const elementIndices = elementIds
-        .map(elementId => {
-          const location = getLocationFromIds(workpadState, pageId, elementId);
+      const nodeIndices = elementIds
+        .map(nodeId => {
+          const location = getLocationFromIds(workpadState, pageId, nodeId);
           return {
             location,
-            index: getElementIndexById(workpadState.pages[pageIndex], elementId, location),
+            index: getNodeIndexById(workpadState.pages[pageIndex], nodeId, location),
           };
         })
         .sort((a, b) => b.index - a.index); // deleting from end toward beginning, otherwise indices will become off - todo fuse loops!
 
-      return elementIndices.reduce((state, { location, index }) => {
+      return nodeIndices.reduce((state, { location, index }) => {
         return del(state, ['pages', pageIndex, location, index]);
       }, workpadState);
     },
