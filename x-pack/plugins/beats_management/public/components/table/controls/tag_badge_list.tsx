d7a5e631d56278466983a94c2d4f8cad2aeb9785
Matt Apperson <me@mattapperson.com>
2018-12-17 12:29:03 -0500

    [BeatsCM] Update table controls for better discoverability (#27043)
    * working new control layout
    
    # Conflicts:
    #	x-pack/plugins/beats_management/public/components/table/table.tsx
    
    * Finish new table controls
    
    * check for tags
    
    * update type to match reality
    
    * when all beats are removed, no more error
    
    * update label for uniformity

--- x-pack/plugins/beats_management/public/components/table/controls/tag_badge_list.tsx
+++ x-pack/plugins/beats_management/public/components/table/controls/tag_badge_list.tsx
@@ -4,26 +4,91 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiContextMenuPanel,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPopover,
+} from '@elastic/eui';
 import React from 'react';
+import { TABLE_CONFIG } from '../../../../common/constants/table';
+import { TagBadge } from '../../tag/tag_badge';
 import { AssignmentActionType } from '../index';
-import { TagAssignment } from './tag_assignment';
 
 interface TagBadgeListProps {
   items: any[];
+  disabled: boolean;
+  name: string;
+  action?: AssignmentActionType;
   actionHandler(action: AssignmentActionType, payload?: any): void;
 }
 
-export const TagBadgeList = (props: TagBadgeListProps) => (
-  // @ts-ignore direction prop type "column" not defined in current EUI version
-  <EuiFlexGroup direction="column" gutterSize="xs" style={{ margin: 10 }}>
-    {props.items.map((item: any) => (
-      <EuiFlexItem key={`${item.id}`}>
-        <TagAssignment
-          tag={item}
-          assignTag={(id: string) => props.actionHandler(AssignmentActionType.Assign, id)}
-        />
-      </EuiFlexItem>
-    ))}
-  </EuiFlexGroup>
-);
+interface ComponentState {
+  isPopoverOpen: boolean;
+}
+
+export class TagBadgeList extends React.Component<TagBadgeListProps, ComponentState> {
+  constructor(props: TagBadgeListProps) {
+    super(props);
+
+    this.state = {
+      isPopoverOpen: false,
+    };
+  }
+
+  public render() {
+    const button = (
+      <EuiButton
+        size="s"
+        iconType="arrowDown"
+        iconSide="right"
+        onClick={this.onButtonClick}
+        disabled={this.props.disabled}
+      >
+        {this.props.name}
+      </EuiButton>
+    );
+
+    return (
+      <EuiPopover
+        id="contentPanel"
+        button={button}
+        isOpen={this.state.isPopoverOpen}
+        closePopover={this.closePopover}
+        panelPaddingSize="none"
+        anchorPosition="downLeft"
+      >
+        <EuiContextMenuPanel>
+          <EuiFlexGroup direction="column" gutterSize="xs" style={{ margin: 10 }}>
+            {this.props.items.map((tag: any) => (
+              <EuiFlexItem key={`${tag.id}`}>
+                <EuiFlexGroup gutterSize="xs">
+                  <EuiFlexItem>
+                    <TagBadge
+                      maxIdRenderSize={TABLE_CONFIG.TRUNCATE_TAG_LENGTH_SMALL}
+                      onClick={() => this.props.actionHandler(AssignmentActionType.Assign, tag.id)}
+                      onClickAriaLabel={tag.id}
+                      tag={tag}
+                    />
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            ))}
+          </EuiFlexGroup>
+        </EuiContextMenuPanel>
+      </EuiPopover>
+    );
+  }
+  private onButtonClick = () => {
+    this.setState(prevState => ({
+      isPopoverOpen: !prevState.isPopoverOpen,
+    }));
+  };
+
+  private closePopover = () => {
+    this.setState({
+      isPopoverOpen: false,
+    });
+  };
+}
