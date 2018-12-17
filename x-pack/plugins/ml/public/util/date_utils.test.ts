cbed95be10ab63fe269b80c6abd643e67fded241
Walter Rafelsberger <walter@elastic.co>
2018-12-17 09:33:34 +0100

    [ML] Fixes tz issue in date_utils test.

--- x-pack/plugins/ml/public/util/date_utils.test.ts
+++ x-pack/plugins/ml/public/util/date_utils.test.ts
@@ -4,6 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import moment from 'moment-timezone';
+
 import {
   formatHumanReadableDate,
   formatHumanReadableDateTime,
@@ -11,16 +13,23 @@ import {
 } from './date_utils';
 
 describe('formatHumanReadableDate', () => {
+  beforeEach(() => {
+    moment.tz.setDefault('UTC');
+  });
+  afterEach(() => {
+    moment.tz.setDefault('Browser');
+  });
+
   test('formatHumanReadableDate', () => {
     const formattedDate = formatHumanReadableDate(0);
     expect(formattedDate).toBe('January 1st 1970');
   });
   test('formatHumanReadableDateTime', () => {
     const formattedDate = formatHumanReadableDateTime(0);
-    expect(formattedDate).toBe('January 1st 1970, 01:00');
+    expect(formattedDate).toBe('January 1st 1970, 00:00');
   });
   test('formatHumanReadableDateTimeSeconds', () => {
     const formattedDate = formatHumanReadableDateTimeSeconds(0);
-    expect(formattedDate).toBe('January 1st 1970, 01:00:00');
+    expect(formattedDate).toBe('January 1st 1970, 00:00:00');
   });
 });
