4539aea1e5079e550e0002ab7d238e79300cd2d2
spacedragon <yulong.huang@elastic.co>
2018-12-17 11:06:50 +0800

    try not using network during git tests.

--- x-pack/plugins/code/server/git_operations.test.ts
+++ x-pack/plugins/code/server/git_operations.test.ts
@@ -6,7 +6,7 @@
 
 import { execSync } from 'child_process';
 import fs from 'fs';
-import { Clone, CloneOptions } from 'nodegit';
+import Git from 'nodegit';
 import os from 'os';
 import path from 'path';
 import rimraf from 'rimraf';
@@ -17,7 +17,6 @@ jest.setTimeout(10000);
 test('get default branch from a non master repo', async () => {
   const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'test_git'));
   // create a non-master using git commands
-  console.log('1');
   const shell = `
     git init
     git add 'run.sh'
@@ -25,38 +24,24 @@ test('get default branch from a non master repo', async () => {
     git branch -m trunk
   `;
   fs.writeFileSync(path.join(tmpDir, 'run.sh'), shell, 'utf-8');
-  console.log('2');
-
   execSync('sh ./run.sh', {
     cwd: tmpDir,
   });
-  console.log('3');
 
   try {
     const defaultBranch = await getDefaultBranch(tmpDir);
-    console.log('4');
-
     expect(defaultBranch).toEqual('trunk');
-    console.log('5');
   } finally {
     rimraf.sync(tmpDir);
-    console.log('6');
   }
+  return '';
 });
 
 test('nodegit should be able to clone a repo from github.com', async () => {
-  const opts: CloneOptions = {
-    fetchOpts: {
-      callbacks: {
-        certificateCheck: () => 1,
-      },
-    },
-  };
-
   const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'test_clone'));
   const url = 'https://github.com/Microsoft/TypeScript-Node-Starter.git';
   try {
-    await Clone.clone(url, tmpDir, opts);
+    await Git.Clone.clone(url, tmpDir);
     expect(fs.existsSync(path.join(tmpDir, '.git'))).toBeTruthy();
   } finally {
     rimraf.sync(tmpDir);
