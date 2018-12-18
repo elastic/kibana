90556dde8315c402c075416992b41044f52fb2d2
Dave Snider <dave.snider@gmail.com>
2018-12-17 16:50:38 -0800

    fix nav menu, get collapse button back in kui

--- src/legacy/core_plugins/kibana/public/dashboard/panel/panel_header/panel_options_menu_form.tsx
+++ src/legacy/core_plugins/kibana/public/dashboard/panel/panel_header/panel_options_menu_form.tsx
@@ -19,8 +19,7 @@
 
 import React, { ChangeEvent, KeyboardEvent } from 'react';
 
-import { EuiButtonEmpty, EuiFieldText, EuiFormRow } from '@elastic/eui';
-import { keyCodes } from '@elastic/eui/src/services/key_codes';
+import { EuiButtonEmpty, EuiFieldText, EuiFormRow, keyCodes } from '@elastic/eui';
 import { FormattedMessage, InjectedIntl, injectI18n } from '@kbn/i18n/react';
 
 export interface PanelOptionsMenuFormProps {
