8317c825f44dbfa4f39da73685f0baff49ab0c7b
Nicholas Dziedzic <nicholas.dziedzic@elastic.co>
2018-12-20 15:12:13 -0500

    Saved Objects routes and service should be able to hide objects.

--- src/server/saved_objects/routes/bulk_create.js
+++ src/server/saved_objects/routes/bulk_create.js
@@ -19,7 +19,7 @@
 
 import Joi from 'joi';
 
-export const createBulkCreateRoute = prereqs => ({
+export const createBulkCreateRoute = (prereqs, schema) => ({
   path: '/api/saved_objects/_bulk_create',
   method: 'POST',
   config: {
@@ -32,7 +32,7 @@ export const createBulkCreateRoute = prereqs => ({
         .default(),
       payload: Joi.array().items(
         Joi.object({
-          type: Joi.string().required(),
+          type: Joi.string().valid(schema.getAvailableTypes()).required(),
           id: Joi.string(),
           attributes: Joi.object().required(),
           version: Joi.number(),
