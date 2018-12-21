990e48960ca93d15681f4036854a915f6a3329e7
Mike Côté <mikecote@users.noreply.github.com>
2018-12-21 09:06:42 -0500

    Convert stream util tests to jest (#27499) (#27675)
    * Convert concat_stream_providers to jest
    
    * Convert concat_stream to jest
    
    * Convert intersperse_stream to jest
    
    * Convert json_parse_stream and json_stringify_stream to jest
    
    * Convert list_stream to jest
    
    * Convert map_stream to jest
    
    * Convert promise_from_streams to jest
    
    * Convert reduce_stream to jest
    
    * Convert replace_stream to jest
    
    * Convert split_stream to jest
    
    * Use jest built in error handling

--- src/utils/streams/replace_stream.test.js
+++ src/utils/streams/replace_stream.test.js
@@ -17,15 +17,13 @@
  * under the License.
  */
 
-import expect from 'expect.js';
-
 import {
   createReplaceStream,
   createConcatStream,
   createPromiseFromStreams,
   createListStream,
   createMapStream,
-} from '../';
+} from './';
 
 async function concatToString(streams) {
   return await createPromiseFromStreams([
@@ -36,7 +34,7 @@ async function concatToString(streams) {
 }
 
 describe('replaceStream', () => {
-  it('produces buffers when it receives buffers', async () => {
+  test('produces buffers when it receives buffers', async () => {
     const chunks = await createPromiseFromStreams([
       createListStream([Buffer.from('foo'), Buffer.from('bar')]),
       createReplaceStream('o', '0'),
@@ -44,11 +42,11 @@ describe('replaceStream', () => {
     ]);
 
     chunks.forEach(chunk => {
-      expect(chunk).to.be.a(Buffer);
+      expect(chunk).toBeInstanceOf(Buffer);
     });
   });
 
-  it('produces buffers when it receives strings', async () => {
+  test('produces buffers when it receives strings', async () => {
     const chunks = await createPromiseFromStreams([
       createListStream(['foo', 'bar']),
       createReplaceStream('o', '0'),
@@ -56,46 +54,43 @@ describe('replaceStream', () => {
     ]);
 
     chunks.forEach(chunk => {
-      expect(chunk).to.be.a(Buffer);
+      expect(chunk).toBeInstanceOf(Buffer);
     });
   });
 
-  it('expects toReplace to be a string', () => {
-    expect(() => createReplaceStream(Buffer.from('foo')))
-      .to.throwError(error => {
-        expect(error.message).to.match(/be a string/);
-      });
+  test('expects toReplace to be a string', () => {
+    expect(() => createReplaceStream(Buffer.from('foo'))).toThrowError(/be a string/);
   });
 
-  it('replaces multiple single-char instances in a single chunk', async () => {
+  test('replaces multiple single-char instances in a single chunk', async () => {
     expect(await concatToString([
       createListStream([Buffer.from('f00 bar')]),
       createReplaceStream('0', 'o'),
-    ])).to.be('foo bar');
+    ])).toBe('foo bar');
   });
 
-  it('replaces multiple single-char instances in multiple chunks', async () => {
+  test('replaces multiple single-char instances in multiple chunks', async () => {
     expect(await concatToString([
       createListStream([Buffer.from('f0'), Buffer.from('0 bar')]),
       createReplaceStream('0', 'o'),
-    ])).to.be('foo bar');
+    ])).toBe('foo bar');
   });
 
-  it('replaces single multi-char instances in single chunks', async () => {
+  test('replaces single multi-char instances in single chunks', async () => {
     expect(await concatToString([
       createListStream([Buffer.from('f0'), Buffer.from('0 bar')]),
       createReplaceStream('0', 'o'),
-    ])).to.be('foo bar');
+    ])).toBe('foo bar');
   });
 
-  it('replaces multiple multi-char instances in single chunks', async () => {
+  test('replaces multiple multi-char instances in single chunks', async () => {
     expect(await concatToString([
       createListStream([Buffer.from('foo ba'), Buffer.from('r b'), Buffer.from('az bar')]),
       createReplaceStream('bar', '*'),
-    ])).to.be('foo * baz *');
+    ])).toBe('foo * baz *');
   });
 
-  it('replaces multi-char instance that stretches multiple chunks', async () => {
+  test('replaces multi-char instance that stretches multiple chunks', async () => {
     expect(await concatToString([
       createListStream([
         Buffer.from('foo supe'),
@@ -105,10 +100,10 @@ describe('replaceStream', () => {
         Buffer.from('ocious bar'),
       ]),
       createReplaceStream('supercalifragilisticexpialidocious', '*'),
-    ])).to.be('foo * bar');
+    ])).toBe('foo * bar');
   });
 
-  it('ignores missing multi-char instance', async () => {
+  test('ignores missing multi-char instance', async () => {
     expect(await concatToString([
       createListStream([
         Buffer.from('foo supe'),
@@ -118,6 +113,6 @@ describe('replaceStream', () => {
         Buffer.from('ocious bar'),
       ]),
       createReplaceStream('supercalifragilisticexpialidocious', '*'),
-    ])).to.be('foo supercalifragili sticexpialidocious bar');
+    ])).toBe('foo supercalifragili sticexpialidocious bar');
   });
 });
