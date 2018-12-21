b1b72d55bd0c25918c9f75075a82a1e472c9dd0f
maryia-lapata <mary.lopato@gmail.com>
2018-12-21 18:25:33 +0300

    Remove unused dependency

--- x-pack/plugins/apm/public/components/shared/TransactionActionMenu/__test__/TransactionActionMenu.test.tsx
+++ x-pack/plugins/apm/public/components/shared/TransactionActionMenu/__test__/TransactionActionMenu.test.tsx
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { shallow } from 'enzyme';
 import 'jest-styled-components';
 import React from 'react';
 import { shallowWithIntl } from 'test_utils/enzyme_helpers';
