d92a223d17e3ac4de42e402ae9ff51525496a689
Aaron Caldwell <aaronjcaldwell@gmail.com>
2018-12-21 13:31:15 -0700

    Remove KY dataset (#27708)
    Resolves #27586. Removes the KY Dataset!
    # Conflicts:
    #	x-pack/plugins/gis/index.js

--- x-pack/plugins/gis/index.js
+++ x-pack/plugins/gis/index.js
@@ -6,7 +6,6 @@
 
 import { resolve } from 'path';
 import { initRoutes } from './server/routes';
-import { kySaltTrucksSpecProvider } from './server/sample_data/ky_salt_trucks';
 import webLogsSavedObjects from './server/sample_data/web_logs_saved_objects.json';
 import mappings from './mappings.json';
 import { checkLicense } from './check_license';
@@ -16,7 +15,7 @@ import { watchStatusAndLicenseToInitialize } from
 export function gis(kibana) {
 
   return new kibana.Plugin({
-    require: ['kibana', 'elasticsearch', 'xpack_main', 'tile_map'],
+    require: ['kibana', 'elasticsearch', 'xpack_main'],
     id: 'gis',
     configPrefix: 'xpack.gis',
     publicDir: resolve(__dirname, 'public'),
@@ -42,31 +41,30 @@ export function gis(kibana) {
     },
 
     init(server) {
-      const thisPlugin = this;
-      const xpackMainPlugin = server.plugins.xpack_main;
+      const gisEnabled = server.config().get('xpack.gis.enabled');
 
-      watchStatusAndLicenseToInitialize(xpackMainPlugin, thisPlugin,
-        async license => {
-          if (license) {
-            if (license.gis) {
-              // TODO: Replace with content to 'activate' app on license verify
-              console.info('Valid GIS License');
-            } else {
-              console.warn('No GIS for YOU!');
+      if (gisEnabled) {
+        const thisPlugin = this;
+        const xpackMainPlugin = server.plugins.xpack_main;
+
+        watchStatusAndLicenseToInitialize(xpackMainPlugin, thisPlugin,
+          async license => {
+            if (license && license.gis) {
+              initRoutes(server);
             }
-            return license;
-          }
-        });
-      xpackMainPlugin.info
-        .feature(thisPlugin.id)
-        .registerLicenseCheckResultsGenerator(checkLicense);
+          });
 
-      initRoutes(server);
-      server.registerSampleDataset(kySaltTrucksSpecProvider);
-      server.addSavedObjectsToSampleDataset('logs', webLogsSavedObjects);
-      server.injectUiAppVars('gis', async () => {
-        return await server.getInjectedUiAppVars('kibana');
-      });
+        xpackMainPlugin.info
+          .feature(thisPlugin.id)
+          .registerLicenseCheckResultsGenerator(checkLicense);
+
+        server.addSavedObjectsToSampleDataset('logs', webLogsSavedObjects);
+        server.injectUiAppVars('gis', async () => {
+          return await server.getInjectedUiAppVars('kibana');
+        });
+      } else {
+        server.log(['info', 'maps'], 'Maps app disabled by configuration');
+      }
     }
   });
 }
