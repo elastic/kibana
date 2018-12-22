f37e9742635069c63cb544285c64a8478005060d
Leanid Shutau <leanidshutau@gmail.com>
2018-12-22 13:06:40 +0300

    Implement a build tool for locale files integration (#19826)
    Implement a build tool for locale files verification

--- src/dev/i18n/serializers/json.js
+++ src/dev/i18n/serializers/json.js
@@ -19,10 +19,10 @@
 
 import { i18n } from '@kbn/i18n';
 
-export function serializeToJson(defaultMessages) {
-  const resultJsonObject = { formats: i18n.formats, messages: {} };
+export function serializeToJson(messages, formats = i18n.formats) {
+  const resultJsonObject = { formats, messages: {} };
 
-  for (const [mapKey, mapValue] of defaultMessages) {
+  for (const [mapKey, mapValue] of messages) {
     if (mapValue.description) {
       resultJsonObject.messages[mapKey] = { text: mapValue.message, comment: mapValue.description };
     } else {
