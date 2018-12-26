aa0d2f23c698f127494b5ead710bc260cb32975f
Thomas Neirynck <thomas@elastic.co>
2018-12-26 17:29:31 -0500

    [Maps] use EMS v6.6 (#27560)
    Maps plugin should use correct EMS version.

--- x-pack/plugins/gis/server/routes.js
+++ x-pack/plugins/gis/server/routes.js
@@ -5,7 +5,6 @@
  */
 
 
-import { EMS_V2 } from '../common/ems_v2';
 import { GIS_API_PATH } from '../common/constants';
 import fetch from 'node-fetch';
 import _ from 'lodash';
@@ -17,11 +16,10 @@ export function initRoutes(server) {
   const serverConfig = server.config();
   const mapConfig = serverConfig.get('map');
 
-  const emsV2 = new EMS_V2({
+  const emsClient = new server.plugins.tile_map.ems_client.EMSClientV66({
     kbnVersion: serverConfig.get('pkg.version'),
-    license: server.plugins.xpack_main.info.license.getUid(),
     manifestServiceUrl: mapConfig.manifestServiceUrl,
-    emsLandingPageUrl: mapConfig.emsLandingPageUrl
+    landingPageUrl: mapConfig.emsLandingPageUrl
   });
 
   server.route({
@@ -29,20 +27,20 @@ export function initRoutes(server) {
     path: `${ROOT}/data/ems`,
     handler: async (request) => {
 
-      if (!request.query.name) {
+      if (!request.query.id) {
+        server.log('warning', 'Must supply id parameters to retrieve EMS file');
         return null;
       }
 
-      const ems = await getEMSResources();//todo: should do this lazily from emsV2 instance
-      const layer = ems.fileLayers.find(layer => layer.name === request.query.name);
+      const ems = await getEMSResources();
+
+      const layer = ems.fileLayers.find(layer => layer.id === request.query.id);
       if (!layer) {
         return null;
       }
 
       const file = await fetch(layer.url);
-      const fileGeoJson = await file.json();
-
-      return fileGeoJson;
+      return await file.json();
 
     }
   });
@@ -80,8 +78,39 @@ export function initRoutes(server) {
   });
 
   async function getEMSResources() {
-    const fileLayers = await emsV2.getFileLayers();
-    const tmsServices = await emsV2.getTMSServices();
+
+    const fileLayerObjs = await emsClient.getFileLayers();
+    const tmsServicesObjs = await emsClient.getTMSServices();
+
+    const fileLayers = fileLayerObjs.map(fileLayer => {
+      //backfill to static settings
+      const format = fileLayer.getDefaultFormatType();
+      const meta = fileLayer.getDefaultFormatMeta();
+
+      return {
+        name: fileLayer.getDisplayName(),
+        origin: fileLayer.getOrigin(),
+        id: fileLayer.getId(),
+        created_at: fileLayer.getCreatedAt(),
+        attribution: fileLayer.getHTMLAttribution(),
+        fields: fileLayer.getFieldsInLanguage(),
+        url: fileLayer.getDefaultFormatUrl(),
+        format: format, //legacy: format and meta are split up
+        meta: meta //legacy, format and meta are split up
+      };
+    });
+
+    const tmsServices = tmsServicesObjs.map(tmsService => {
+      return {
+        origin: tmsService.getOrigin(),
+        id: tmsService.getId(),
+        minZoom: tmsService.getMinZoom(),
+        maxZoom: tmsService.getMaxZoom(),
+        attribution: tmsService.getHTMLAttribution(),
+        url: tmsService.getUrlTemplate()
+      };
+    });
+
     return { fileLayers, tmsServices };
   }
 }
