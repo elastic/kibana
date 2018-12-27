140a05a4f4d113592554457021f353dcff3762f1
Nathan Reese <reese.nathan@gmail.com>
2018-12-27 14:54:50 -0700

    replace kbnTimepicker directive with EuiSuperDatePicker

--- src/legacy/core_plugins/kibana/ui_setting_defaults.js
+++ src/legacy/core_plugins/kibana/ui_setting_defaults.js
@@ -119,7 +119,7 @@ export function getUiSettingDefaults() {
       name: i18n.translate('kbn.advancedSettings.dateFormatTitle', {
         defaultMessage: 'Date format',
       }),
-      value: 'MMMM Do YYYY, HH:mm:ss.SSS',
+      value: 'MMM D, YYYY @ HH:mm:ss.SSS',
       description: i18n.translate('kbn.advancedSettings.dateFormatText', {
         defaultMessage: 'When displaying a pretty formatted date, use this {formatLink}',
         description: 'Part of composite text: kbn.advancedSettings.dateFormatText + ' +
@@ -769,31 +769,14 @@ export function getUiSettingDefaults() {
         defaultMessage: 'Time picker quick ranges',
       }),
       value: JSON.stringify([
-        { from: 'now/d',    to: 'now/d',    display: 'Today',                 section: 0 },
-        { from: 'now/w',    to: 'now/w',    display: 'This week',             section: 0 },
-        { from: 'now/M',    to: 'now/M',    display: 'This month',            section: 0 },
-        { from: 'now/y',    to: 'now/y',    display: 'This year',             section: 0 },
-        { from: 'now/d',    to: 'now',      display: 'Today so far',          section: 0 },
-        { from: 'now/w',    to: 'now',      display: 'Week to date',          section: 0 },
-        { from: 'now/M',    to: 'now',      display: 'Month to date',         section: 0 },
-        { from: 'now/y',    to: 'now',      display: 'Year to date',          section: 0 },
-
-        { from: 'now-15m',  to: 'now',      display: 'Last 15 minutes',       section: 1 },
-        { from: 'now-30m',  to: 'now',      display: 'Last 30 minutes',       section: 1 },
-        { from: 'now-1h',   to: 'now',      display: 'Last 1 hour',           section: 1 },
-        { from: 'now-4h',   to: 'now',      display: 'Last 4 hours',          section: 1 },
-        { from: 'now-12h',  to: 'now',      display: 'Last 12 hours',         section: 1 },
-        { from: 'now-24h',  to: 'now',      display: 'Last 24 hours',         section: 1 },
-        { from: 'now-7d',   to: 'now',      display: 'Last 7 days',           section: 1 },
-
-        { from: 'now-30d',  to: 'now',      display: 'Last 30 days',          section: 2 },
-        { from: 'now-60d',  to: 'now',      display: 'Last 60 days',          section: 2 },
-        { from: 'now-90d',  to: 'now',      display: 'Last 90 days',          section: 2 },
-        { from: 'now-6M',   to: 'now',      display: 'Last 6 months',         section: 2 },
-        { from: 'now-1y',   to: 'now',      display: 'Last 1 year',           section: 2 },
-        { from: 'now-2y',   to: 'now',      display: 'Last 2 years',          section: 2 },
-        { from: 'now-5y',   to: 'now',      display: 'Last 5 years',          section: 2 },
-
+        { from: 'now/d',    to: 'now/d',    display: 'Today',                 },
+        { from: 'now/w',    to: 'now/w',    display: 'This week',             },
+        { from: 'now/M',    to: 'now/M',    display: 'This month',            },
+        { from: 'now/y',    to: 'now/y',    display: 'This year',             },
+        { from: 'now/d',    to: 'now',      display: 'Today so far',          },
+        { from: 'now/w',    to: 'now',      display: 'Week to date',          },
+        { from: 'now/M',    to: 'now',      display: 'Month to date',         },
+        { from: 'now/y',    to: 'now',      display: 'Year to date',          },
       ], null, 2),
       type: 'json',
       description: i18n.translate('kbn.advancedSettings.timepicker.quickRangesText', {
