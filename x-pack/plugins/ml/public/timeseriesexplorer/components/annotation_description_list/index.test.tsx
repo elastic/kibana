d8234f7933e95fdbddaa26b3d76e3e2f087eb5c7
Walter Rafelsberger <walter@elastic.co>
2019-01-03 18:20:19 +0100

    [ML] Fix time zone issue with test.

--- x-pack/plugins/ml/public/timeseriesexplorer/components/annotation_description_list/index.test.tsx
+++ x-pack/plugins/ml/public/timeseriesexplorer/components/annotation_description_list/index.test.tsx
@@ -7,11 +7,19 @@
 import mockAnnotations from '../../../components/annotations_table/__mocks__/mock_annotations.json';
 
 import { shallow } from 'enzyme';
+import moment from 'moment-timezone';
 import React from 'react';
 
 import { AnnotationDescriptionList } from './index';
 
 describe('AnnotationDescriptionList', () => {
+  beforeEach(() => {
+    moment.tz.setDefault('UTC');
+  });
+  afterEach(() => {
+    moment.tz.setDefault('Browser');
+  });
+
   test('Initialization with annotation.', () => {
     const wrapper = shallow(<AnnotationDescriptionList annotation={mockAnnotations[0]} />);
     expect(wrapper).toMatchSnapshot();
