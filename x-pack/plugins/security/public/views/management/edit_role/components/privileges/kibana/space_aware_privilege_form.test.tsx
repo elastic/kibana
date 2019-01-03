6db5ae64ef45a570cd58f6dee89da6e1e8ec66ba
Larry Gregory <larry.gregory@elastic.co>
2019-01-03 14:18:56 -0500

    remove unused components as a result of the redesign

--- x-pack/plugins/security/public/views/management/edit_role/components/privileges/kibana/space_aware_privilege_form.test.tsx
+++ x-pack/plugins/security/public/views/management/edit_role/components/privileges/kibana/space_aware_privilege_form.test.tsx
@@ -7,7 +7,6 @@
 import React from 'react';
 import { mountWithIntl, shallowWithIntl } from 'test_utils/enzyme_helpers';
 import { RoleValidator } from '../../../lib/validate_role';
-import { PrivilegeCalloutWarning } from './privilege_callout_warning';
 import { PrivilegeSpaceForm } from './privilege_space_form';
 import { PrivilegeSpaceTable } from './privilege_space_table';
 import { SpaceAwarePrivilegeForm } from './space_aware_privilege_form';
@@ -111,61 +110,7 @@ describe('<SpaceAwarePrivilegeForm>', () => {
     expect(wrapper.find(PrivilegeSpaceForm)).toHaveLength(1);
   });
 
-  describe('with minimum privilege set to "all"', () => {
-    it('does not allow space privileges to be customized', () => {
-      const props = buildProps({
-        role: {
-          elasticsearch: {
-            cluster: ['manage'],
-          },
-          kibana: {
-            global: ['all'],
-            space: {
-              default: ['all'],
-            },
-          },
-        },
-      });
-
-      const wrapper = mountWithIntl(<SpaceAwarePrivilegeForm.WrappedComponent {...props} />);
-
-      const warning = wrapper.find(PrivilegeCalloutWarning);
-      expect(warning.props()).toMatchObject({
-        basePrivilege: 'all',
-      });
-
-      const table = wrapper.find(PrivilegeSpaceTable);
-      expect(table).toHaveLength(0);
-
-      const addPrivilegeButton = wrapper.find('[data-test-subj="addSpacePrivilegeButton"]');
-      expect(addPrivilegeButton).toHaveLength(0);
-    });
-  });
-
   describe('with minimum privilege set to "read"', () => {
-    it('shows a warning about minimum privilege', () => {
-      const props = buildProps({
-        role: {
-          elasticsearch: {
-            cluster: ['manage'],
-          },
-          kibana: {
-            global: ['read'],
-            space: {
-              default: ['all'],
-            },
-          },
-        },
-      });
-
-      const wrapper = mountWithIntl(<SpaceAwarePrivilegeForm.WrappedComponent {...props} />);
-
-      const warning = wrapper.find(PrivilegeCalloutWarning);
-      expect(warning.props()).toMatchObject({
-        basePrivilege: 'read',
-      });
-    });
-
     it('allows space privileges to be customized', () => {
       const props = buildProps({
         role: {
@@ -192,27 +137,6 @@ describe('<SpaceAwarePrivilegeForm>', () => {
   });
 
   describe('with minimum privilege set to "none"', () => {
-    it('does not show a warning about minimum privilege', () => {
-      const props = buildProps({
-        role: {
-          elasticsearch: {
-            cluster: ['manage'],
-          },
-          kibana: {
-            global: [],
-            space: {
-              default: ['all'],
-            },
-          },
-        },
-      });
-
-      const wrapper = mountWithIntl(<SpaceAwarePrivilegeForm.WrappedComponent {...props} />);
-
-      const warning = wrapper.find(PrivilegeCalloutWarning);
-      expect(warning).toHaveLength(0);
-    });
-
     it('allows space privileges to be customized', () => {
       const props = buildProps({
         role: {
