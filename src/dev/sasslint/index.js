dd47972cecf3873224ea9da033854809b0d1f3ba
Chandler Prall <chandler.prall@gmail.com>
2019-01-04 12:14:03 -0700

    Sasslint precommit hook (#28095)
    * Remove SCSS linting from dev server
    
    * Add sasslint to precommit hook

--- src/dev/sasslint/index.js
+++ src/dev/sasslint/index.js
@@ -17,34 +17,5 @@
  * under the License.
  */
 
-import { run, combineErrors } from './run';
-import * as Eslint from './eslint';
-import * as Tslint from './tslint';
-import * as Sasslint from './sasslint';
-import { getFilesForCommit, checkFileCasing } from './precommit_hook';
-
-run(async ({ log }) => {
-  const files = await getFilesForCommit();
-  const errors = [];
-
-  try {
-    await checkFileCasing(log, files);
-  } catch (error) {
-    errors.push(error);
-  }
-
-  for (const Linter of [Eslint, Tslint, Sasslint]) {
-    const filesToLint = Linter.pickFilesToLint(log, files);
-    if (filesToLint.length > 0) {
-      try {
-        await Linter.lintFiles(log, filesToLint);
-      } catch (error) {
-        errors.push(error);
-      }
-    }
-  }
-
-  if (errors.length) {
-    throw combineErrors(errors);
-  }
-});
+export { pickFilesToLint } from './pick_files_to_lint';
+export { lintFiles } from './lint_files';
