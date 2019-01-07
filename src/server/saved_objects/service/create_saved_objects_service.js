57d570685a6bcfaf6bba86250b595918534e2bc1
Christopher Davies <github@christophilus.com>
2019-01-07 14:46:33 -0500

    Remove saved object client's Kibana index template creation logic

--- src/server/saved_objects/service/create_saved_objects_service.js
+++ src/server/saved_objects/service/create_saved_objects_service.js
@@ -31,42 +31,6 @@ import { SavedObjectsClient } from './saved_objects_client';
 // Eventually, we hope to build a first-class import / export API, at which point, we can
 // remove the migrator from the saved objects client and leave only document validation here.
 export function createSavedObjectsService(server, schema, serializer, migrator) {
-  const onBeforeWrite = async () => {
-    const adminCluster = server.plugins.elasticsearch.getCluster('admin');
-
-    try {
-      const index = server.config().get('kibana.index');
-      await adminCluster.callWithInternalUser('indices.putTemplate', {
-        name: `kibana_index_template:${index}`,
-        body: {
-          template: index,
-          settings: {
-            number_of_shards: 1,
-            auto_expand_replicas: '0-1',
-          },
-          mappings: server.getKibanaIndexMappingsDsl(),
-        },
-      });
-    } catch (error) {
-      server.log(['debug', 'savedObjects'], {
-        tmpl: 'Attempt to write indexTemplate for SavedObjects index failed: <%= err.message %>',
-        es: {
-          resp: error.body,
-          status: error.status,
-        },
-        err: {
-          message: error.message,
-          stack: error.stack,
-        },
-      });
-
-      // We reject with `es.ServiceUnavailable` because writing an index
-      // template is a very simple operation so if we get an error here
-      // then something must be very broken
-      throw new adminCluster.errors.ServiceUnavailable();
-    }
-  };
-
   const mappings = server.getKibanaIndexMappingsDsl();
   const repositoryProvider = new SavedObjectsRepositoryProvider({
     index: server.config().get('kibana.index'),
@@ -74,13 +38,11 @@ export function createSavedObjectsService(server, schema, serializer, migrator) 
     mappings,
     schema,
     serializer,
-    onBeforeWrite,
   });
 
   const scopedClientProvider = new ScopedSavedObjectsClientProvider({
     index: server.config().get('kibana.index'),
     mappings,
-    onBeforeWrite,
     defaultClientFactory({
       request,
     }) {
