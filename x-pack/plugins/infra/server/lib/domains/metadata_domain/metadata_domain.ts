f73c7793f6b6313586d76a09b37dce0680f9c886
Chris Cowan <chris@chriscowan.us>
2019-01-07 12:04:52 -0700

    Migrate to ESC event.dataset

--- x-pack/plugins/infra/server/lib/domains/metadata_domain/metadata_domain.ts
+++ x-pack/plugins/infra/server/lib/domains/metadata_domain/metadata_domain.ts
@@ -48,17 +48,9 @@ export class InfraMetadataDomain {
 
 const pickMetadata = (buckets: InfraMetadataAggregationBucket[]): string[] => {
   if (buckets) {
-    const metadata = buckets
-      .map(module => {
-        if (module.names) {
-          return module.names.buckets.map(name => {
-            return `${module.key}.${name.key}`;
-          });
-        } else {
-          return [];
-        }
-      })
-      .reduce((a: string[], b: string[]) => a.concat(b), []);
+    const metadata = buckets.map(module => {
+      return module.key;
+    });
     return metadata;
   } else {
     return [];
