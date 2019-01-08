8cd29fcb27ba0413c3ba54e3e935781979a668f0
Joel Griffith <joel.griffith@elastic.co>
2019-01-08 10:53:54 -0800

    Reverting another TS change

--- x-pack/plugins/reporting/server/browsers/safe_child_process.js
+++ x-pack/plugins/reporting/server/browsers/safe_child_process.js
@@ -5,44 +5,45 @@
  */
 
 import * as Rx from 'rxjs';
-import { delay, ignoreElements, mapTo, share, take, tap } from 'rxjs/operators';
-
-interface IChildProcess {
-  kill: (signal: string) => any;
-}
+import { take, share, mapTo, delay, tap, ignoreElements } from 'rxjs/operators';
 
 // Our process can get sent various signals, and when these occur we wish to
 // kill the subprocess and then kill our process as long as the observer isn't cancelled
-export function safeChildProcess(childProcess: IChildProcess, observer: any) {
+export function safeChildProcess(childProcess, observer) {
   const ownTerminateSignal$ = Rx.merge(
-    Rx.fromEvent(process as any, 'SIGTERM').pipe(mapTo('SIGTERM')),
-    Rx.fromEvent(process as any, 'SIGINT').pipe(mapTo('SIGINT')),
-    Rx.fromEvent(process as any, 'SIGBREAK').pipe(mapTo('SIGBREAK'))
-  ).pipe(
-    take(1),
-    share()
-  );
+    Rx.fromEvent(process, 'SIGTERM').pipe(mapTo('SIGTERM')),
+    Rx.fromEvent(process, 'SIGINT').pipe(mapTo('SIGINT')),
+    Rx.fromEvent(process, 'SIGBREAK').pipe(mapTo('SIGBREAK')),
+  )
+    .pipe(
+      take(1),
+      share()
+    );
 
   // signals that will be sent to the child process as a result of the main process
   // being sent these signals, or the exit being triggered
   const signalForChildProcess$ = Rx.merge(
     // SIGKILL when this process gets a terminal signal
-    ownTerminateSignal$.pipe(mapTo('SIGKILL')),
+    ownTerminateSignal$.pipe(
+      mapTo('SIGKILL')
+    ),
 
     // SIGKILL when this process forcefully exits
-    Rx.fromEvent(process as any, 'exit').pipe(
+    Rx.fromEvent(process, 'exit').pipe(
       take(1),
       mapTo('SIGKILL')
-    )
+    ),
   );
 
   // send termination signals
   const terminate$ = Rx.merge(
-    signalForChildProcess$.pipe(tap((signal: string) => childProcess.kill(signal))),
+    signalForChildProcess$.pipe(
+      tap(signal => childProcess.kill(signal))
+    ),
 
     ownTerminateSignal$.pipe(
       delay(1),
-      tap((signal: number) => process.kill(process.pid, signal))
+      tap(signal => process.kill(process.pid, signal)),
     )
   );
 
@@ -57,7 +58,7 @@ export function safeChildProcess(childProcess: IChildProcess, observer: any) {
 
 // If a process exits ungracefully, we can try to help the user make sense of why
 // by giving them a suggestion based on the code.
-export function exitCodeSuggestion(code: number | null) {
+export function exitCodeSuggestion(code) {
   if (code === null) {
     return 'Your report may be too large. Try removing some visualizations or increasing the RAM available to Kibana.';
   }
