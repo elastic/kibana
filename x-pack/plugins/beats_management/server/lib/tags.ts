3859158a10f94fb3abc85e7c333e66df17f26567
Matt Apperson <me@mattapperson.com>
2019-01-08 07:56:21 -0500

    fix typo causing several failing tests.

--- x-pack/plugins/beats_management/server/lib/tags.ts
+++ x-pack/plugins/beats_management/server/lib/tags.ts
@@ -8,24 +8,24 @@ import { FrameworkUser } from './adapters/framework/adapter_types';
 import { CMTagsAdapter } from './adapters/tags/adapter_types';
 
 export class CMTagsDomain {
-  constructor(private readonly tagAdapter: CMTagsAdapter) {}
+  constructor(private readonly adapter: CMTagsAdapter) {}
 
   public async getAll(user: FrameworkUser, ESQuery?: any): Promise<BeatTag[]> {
-    const tags = await this.tagAdapter.getAll(user, ESQuery);
+    const tags = await this.adapter.getAll(user, ESQuery);
     return tags;
   }
 
   public async getWithIds(user: FrameworkUser, tagIds: string[]): Promise<BeatTag[]> {
-    const tags = await this.tagAdapter.getTagsWithIds(user, tagIds);
+    const tags = await this.adapter.getTagsWithIds(user, tagIds);
     return tags;
   }
 
   public async delete(user: FrameworkUser, tagIds: string[]) {
-    return await this.tagAdapter.delete(user, tagIds);
+    return await this.adapter.delete(user, tagIds);
   }
 
   public async upsertTag(user: FrameworkUser, tag: BeatTag): Promise<string> {
-    const tagId = await this.tagAdapter.upsertTag(user, {
+    const tagId = await this.adapter.upsertTag(user, {
       ...tag,
       name: tag.id,
     });
