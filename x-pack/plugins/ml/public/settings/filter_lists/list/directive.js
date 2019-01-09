b67f6be0370c4873862bb2e05fb4f0fd82da7229
James Gowdy <jgowdy@elastic.co>
2019-01-09 17:15:44 +0000

    fixing conflicts

--- x-pack/plugins/ml/public/settings/filter_lists/list/directive.js
+++ x-pack/plugins/ml/public/settings/filter_lists/list/directive.js
@@ -17,7 +17,6 @@ const module = uiModules.get('apps/ml', ['react']);
 import { checkFullLicense } from 'plugins/ml/license/check_license';
 import { checkGetJobsPrivilege, checkPermission } from 'plugins/ml/privilege/check_privilege';
 import { getMlNodeCount } from 'plugins/ml/ml_nodes_check/check_ml_nodes';
-import { initPromise } from 'plugins/ml/util/promise';
 import { FilterLists } from './filter_lists';
 
 import uiRoutes from 'ui/routes';
@@ -36,7 +35,6 @@ uiRoutes
       CheckLicense: checkFullLicense,
       privileges: checkGetJobsPrivilege,
       mlNodeCount: getMlNodeCount,
-      initPromise: initPromise(false)
     }
   });
 
