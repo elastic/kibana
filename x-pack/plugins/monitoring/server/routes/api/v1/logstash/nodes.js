62b63dfc6f0d9bec4cac172e6b569fab9d393184
Nicholas Dziedzic <nicholas.dziedzic@elastic.co>
2019-01-09 12:38:07 -0500

    Util is only for promisify, duh!

--- x-pack/plugins/monitoring/server/routes/api/v1/logstash/nodes.js
+++ x-pack/plugins/monitoring/server/routes/api/v1/logstash/nodes.js
@@ -5,7 +5,6 @@
  */
 
 import Joi from 'joi';
-import Promise from 'util';
 import { getClusterStatus } from '../../../../lib/logstash/get_cluster_status';
 import { getNodes } from '../../../../lib/logstash/get_nodes';
 import { handleError } from '../../../../lib/errors';
