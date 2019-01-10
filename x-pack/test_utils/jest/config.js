39ead0ca0a6cf67380905d4c42e0b8999bc42746
nicknak <njd5475@gmail.com>
2019-01-10 14:31:12 -0500

    Testing x pack jest integration (#26825)
    * progress
    
    * progress
    
    * cleanup and elastic configs
    
    * make upgrades to support adding aditional users, with
    
    * use defaultDeep to ensure settings pass correctly
    
    * move needed configs to start servers into kbn_server (except x-pack plugin paths and users)
    
    * move xpack config to an export
    
    * add more time
    
    * diff rollbacks
    
    * roll back prettier diff
    
    * revert setupUsers signature
    
    * remove more bluebird
    
    * update bluebird for fixes with jest compatability
    
    * fix ts errors
    
    * dont allow jest to keep going making errors confising
    
    * Separates configs for jest integration core/x-pack.
    
    * Pass nested kbn config parameters.
    
    * Adds example x-pack integration test using live es.
    
    * Cloud detectors should be configurable for tests.
    
    * Cloud detectors should use native promises only.
    
    * No erroneous comments...
    
    * Util is only for promisify, duh!
    
    * New tests should have docuementation to help those looking to utilize them.
    
    * Doc section headings should be consistent with each other.
    
    * With git there is no need to commit commented code.

---
+++ x-pack/test_utils/jest/config.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export default {
+  rootDir: '../../',
+  roots: [
+    '<rootDir>/plugins',
+    '<rootDir>/server',
+    '<rootDir>/common',
+    '<rootDir>/test_utils/jest/integration_tests',
+  ],
+  collectCoverageFrom: [
+    'plugins/**/*.js',
+    'common/**/*.js',
+    'server/**/*.js',
+  ],
+  moduleNameMapper: {
+    '^ui/(.*)': '<rootDir>**/public/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/dev/jest/mocks/file_mock.js',
+    '\\.(css|less|scss)$': '<rootDir>/../src/dev/jest/mocks/style_mock.js',
+  },
+  setupFiles: [
+    '<rootDir>/../src/dev/jest/setup/babel_polyfill.js',
+    '<rootDir>/../src/dev/jest/setup/polyfills.js',
+    '<rootDir>/../src/dev/jest/setup/enzyme.js',
+    '<rootDir>/../src/dev/jest/setup/throw_on_console_error.js',
+  ],
+  coverageDirectory: '<rootDir>/../target/jest-coverage',
+  coverageReporters: [
+    'html',
+  ],
+  globals: {
+    'ts-jest': {
+      skipBabel: true,
+    },
+  },
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'ts',
+    'tsx',
+  ],
+  modulePathIgnorePatterns: [
+    '__fixtures__/',
+    'target/',
+  ],
+  testMatch: [
+    '**/*.test.{js,ts,tsx}'
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/packages/kbn-ui-framework/(dist|doc_site|generator-kui)/',
+    '<rootDir>/packages/kbn-pm/dist/',
+    'integration_tests/'
+  ],
+  transform: {
+    '^.+\\.js$': '<rootDir>/../src/dev/jest/babel_transform.js',
+    '^.+\\.tsx?$': '<rootDir>/../src/dev/jest/ts_transform.js',
+    '^.+\\.txt?$': 'jest-raw-loader',
+    '^.+\\.html?$': 'jest-raw-loader',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.js$',
+    'packages/kbn-pm/dist/index.js'
+  ],
+  snapshotSerializers: [
+    '<rootDir>/../node_modules/enzyme-to-json/serializer',
+  ],
+  reporters: [
+    'default',
+    '<rootDir>/../src/dev/jest/junit_reporter.js',
+  ],
+};
