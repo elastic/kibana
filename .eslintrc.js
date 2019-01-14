045696831ec89ad4de56d063f1e185c535d8388b
spalger <email@spalger.com>
2019-01-14 15:23:31 -0800

    [ftr/element] custom scrollIntoView to support fixed header

--- .eslintrc.js
+++ .eslintrc.js
@@ -204,6 +204,34 @@ module.exports = {
     },
 
     /**
+     * Files that run in the browser with only node-level transpilation
+     */
+    {
+      files: [
+        'test/functional/services/lib/leadfoot_element_wrapper/scroll_into_view_if_necessary.js',
+      ],
+      rules: {
+        'prefer-object-spread/prefer-object-spread': 'off',
+        'no-var': 'off',
+        'prefer-const': 'off',
+        'prefer-destructuring': 'off',
+        'no-restricted-syntax': [
+          'error',
+          'ArrowFunctionExpression',
+          'AwaitExpression',
+          'ClassDeclaration',
+          'RestElement',
+          'SpreadElement',
+          'YieldExpression',
+          'VariableDeclaration[kind="const"]',
+          'VariableDeclaration[kind="let"]',
+          'VariableDeclarator[id.type="ArrayPattern"]',
+          'VariableDeclarator[id.type="ObjectPattern"]',
+        ],
+      },
+    },
+
+    /**
      * Files that run AFTER node version check
      * and are not also transpiled with babel
      */
