8b55c1d28ddd3caa7f5ec4c53546b95148eeb8a0
Thomas Neirynck <thomas@elastic.co>
2019-01-14 11:59:41 -0500

    add test

--- x-pack/plugins/gis/public/shared/layers/sources/ems_tms_source/ems_tms_source.js
+++ x-pack/plugins/gis/public/shared/layers/sources/ems_tms_source/ems_tms_source.js
@@ -4,8 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React, { Fragment } from 'react';
-import { TMSSource } from './tms_source';
-import { TileLayer } from '../tile_layer';
+import { TMSSource } from '../tms_source';
+import { TileLayer } from '../../tile_layer';
 import {
   EuiText,
   EuiSelect,
