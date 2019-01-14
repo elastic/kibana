19b84f0bc2380a06a5910d3a54809d2838db03d6
pavel06081991 <pavel.1991@tut.by>
2019-01-14 14:04:39 +0300

    [ML] translate job validation messages (#28235)
    [ML] translate job validation messages

--- x-pack/plugins/ml/server/models/job_validation/job_validation.js
+++ x-pack/plugins/ml/server/models/job_validation/job_validation.js
@@ -11,7 +11,7 @@ import Boom from 'boom';
 
 import { fieldsServiceProvider } from '../fields_service';
 import { renderTemplate } from '../../../common/util/string_utils';
-import messages from './messages.json';
+import { getMessages } from './messages';
 import { VALIDATION_STATUS } from '../../../common/constants/validation';
 
 import { basicJobValidation, uniqWithIsEqual } from '../../../common/util/job_utils';
@@ -22,6 +22,8 @@ import { validateModelMemoryLimit } from './validate_model_memory_limit';
 import { validateTimeRange, isValidTimeField } from './validate_time_range';
 
 export async function validateJob(callWithRequest, payload, kbnVersion = 'current', server) {
+  const messages = getMessages();
+
   try {
     if (typeof payload !== 'object' || payload === null) {
       throw new Error(i18n.translate('xpack.ml.models.jobValidation.payloadIsNotObjectErrorMessage', {
