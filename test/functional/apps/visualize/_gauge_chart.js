1078782106523852412da67995aecdc7a1549bff
Stacey Gammon <gammon@elastic.co>
2019-01-15 15:05:12 -0500

    Refactor, clean up, new inspector, pie chart services (#28509)
    * Refactor, clean up, new inspector, pie chart services
    
    * Move inspector service to top level, outside of visualizations folder
    
    * Refactor some more instances of getTableData into expectTableData

--- test/functional/apps/visualize/_gauge_chart.js
+++ test/functional/apps/visualize/_gauge_chart.js
@@ -22,6 +22,7 @@ import expect from 'expect.js';
 export default function ({ getService, getPageObjects }) {
   const log = getService('log');
   const retry = getService('retry');
+  const inspector = getService('inspector');
   const PageObjects = getPageObjects(['common', 'visualize', 'header']);
 
   describe('gauge chart', function indexPatternCreation() {
@@ -40,8 +41,7 @@ export default function ({ getService, getPageObjects }) {
 
 
     it('should have inspector enabled', async function () {
-      const spyToggleExists = await PageObjects.visualize.isInspectorButtonEnabled();
-      expect(spyToggleExists).to.be(true);
+      await inspector.expectIsEnabled();
     });
 
     it('should show Count', function () {
