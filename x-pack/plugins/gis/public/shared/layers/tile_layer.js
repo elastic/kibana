6b911c306a3c4049f207bc73439fccd1722c2501
Aaron Caldwell <aaronjcaldwell@gmail.com>
2019-01-16 09:02:14 -0700

    Update tile layer sync to return promise and handle errors related to both obtaining url and tile loading

--- x-pack/plugins/gis/public/shared/layers/tile_layer.js
+++ x-pack/plugins/gis/public/shared/layers/tile_layer.js
@@ -13,6 +13,7 @@ import _ from 'lodash';
 export class TileLayer extends ALayer {
 
   static type = "TILE";
+  TMS_LOAD_TIMEOUT = 32000;
 
   constructor({ layerDescriptor, source, style }) {
     super({ layerDescriptor, source, style });
@@ -32,14 +33,48 @@ export class TileLayer extends ALayer {
     return tileLayerDescriptor;
   }
 
+  _tileLoadErrorTracker(map, url) {
+    let tileLoadCount = 0;
+    map.on('tileload', ({ tile }) => tile && tileLoadCount++);
 
-  syncLayerWithMB(mbMap) {
+    return new Promise((resolve, reject) => {
+      const tileLoadTimer = setTimeout(() => {
+        map.off('sourcedata');
+        if (!tileLoadCount) {
+          try {
+            throw new Error(`Tiles from "${url}" could not be loaded`);
+          } catch (e) {
+            reject(e);
+          }
+        } else {
+          resolve();
+        }
+      }, this.TMS_LOAD_TIMEOUT);
+      if (tileLoadCount) {
+        clearTimeout(tileLoadTimer);
+        resolve();
+      }
+    });
+  }
 
+  async syncLayerWithMB(mbMap) {
     const source = mbMap.getSource(this.getId());
     const layerId = this.getId() + '_raster';
-    if (!source) {
-      const url = this._source.getUrlTemplate();
-      mbMap.addSource(this.getId(), {
+
+    if (source) {
+      return;
+    }
+
+    let url;
+    return new Promise((resolve, reject) => {
+      try {
+        url = this._source.getUrlTemplate();
+      } catch (e) {
+        reject(e);
+      }
+
+      const sourceId = this.getId();
+      mbMap.addSource(sourceId, {
         type: 'raster',
         tiles: [url],
         tileSize: 256,
@@ -49,15 +84,20 @@ export class TileLayer extends ALayer {
       mbMap.addLayer({
         id: layerId,
         type: 'raster',
-        source: this.getId(),
+        source: sourceId,
         minzoom: 0,
         maxzoom: 22,
       });
-    }
-
-    mbMap.setLayoutProperty(layerId, 'visibility', this.isVisible() ? 'visible' : 'none');
-    mbMap.setLayerZoomRange(layerId, this._descriptor.minZoom, this._descriptor.maxZoom);
-    this._style && this._style.setMBPaintProperties(mbMap, layerId);
+      resolve();
+    }).then(() => this._tileLoadErrorTracker(mbMap, url)
+    ).then(() => {
+      mbMap.setLayoutProperty(layerId, 'visibility', this.isVisible()
+        ? 'visible'
+        : 'none');
+      mbMap.setLayerZoomRange(layerId, this._descriptor.minZoom,
+        this._descriptor.maxZoom);
+      this._style && this._style.setMBPaintProperties(mbMap, layerId);
+    });
   }
 
   getIcon() {
