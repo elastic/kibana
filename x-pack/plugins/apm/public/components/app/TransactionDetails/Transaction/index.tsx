cd05fce997bdbaaa0e755833d07d6292ea48b44f
Casper Hübertz <casper@formgeist.com>
2019-01-17 15:26:28 +0100

    [APM] Fix icon for View trace button (#28908) (#28918)

--- x-pack/plugins/apm/public/components/app/TransactionDetails/Transaction/index.tsx
+++ x-pack/plugins/apm/public/components/app/TransactionDetails/Transaction/index.tsx
@@ -34,7 +34,7 @@ function MaybeViewTraceLink({
     return (
       <EuiFlexItem grow={false}>
         <EuiToolTip content="The trace parent cannot be found">
-          <EuiButton iconType="apmApp" disabled={true}>
+          <EuiButton iconType="apmTrace" disabled={true}>
             View full trace
           </EuiButton>
         </EuiToolTip>
@@ -50,7 +50,7 @@ function MaybeViewTraceLink({
     return (
       <EuiFlexItem grow={false}>
         <EuiToolTip content="Currently viewing the full trace">
-          <EuiButton iconType="apmApp" disabled={true}>
+          <EuiButton iconType="apmTrace" disabled={true}>
             View full trace
           </EuiButton>
         </EuiToolTip>
@@ -62,7 +62,7 @@ function MaybeViewTraceLink({
     return (
       <EuiFlexItem grow={false}>
         <TransactionLink transaction={waterfall.traceRoot}>
-          <EuiButton iconType="apmApp">View full trace</EuiButton>
+          <EuiButton iconType="apmTrace">View full trace</EuiButton>
         </TransactionLink>
       </EuiFlexItem>
     );
