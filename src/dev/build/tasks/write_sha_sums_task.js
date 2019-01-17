46ab4018a472052647026d397ea16434c5a0515e
Tiago Costa <tiagoffcc@hotmail.com>
2019-01-17 17:24:18 +0000

    feat(NA): missing sha sum generation for docker packages.

--- src/dev/build/tasks/write_sha_sums_task.js
+++ src/dev/build/tasks/write_sha_sums_task.js
@@ -26,7 +26,7 @@ export const WriteShaSumsTask = {
   description: 'Writing sha1sums of archives and packages in target directory',
 
   async run(config) {
-    const artifacts = await globby(['*.zip', '*.tar.gz', '*.deb', '*.rpm'], {
+    const artifacts = await globby(['*.zip', '*.tar', '*.tar.gz', '*.deb', '*.rpm'], {
       cwd: config.resolveFromTarget('.'),
       absolute: true,
     });
