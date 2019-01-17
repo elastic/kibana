0adf7727a3d566dc0e9c1c204f9c87376ee5a6cd
kobelb <brandon.kobel@elastic.co>
2019-01-17 09:28:57 -0800

    Switching ensureEsVersion to logWithMetadata

--- src/legacy/core_plugins/elasticsearch/lib/ensure_es_version.js
+++ src/legacy/core_plugins/elasticsearch/lib/ensure_es_version.js
@@ -92,15 +92,14 @@ export function ensureEsVersion(server, kibanaVersion) {
         const warningNodeNames = getHumanizedNodeNames(simplifiedNodes).join(', ');
         if (lastWarnedNodesForServer.get(server) !== warningNodeNames) {
           lastWarnedNodesForServer.set(server, warningNodeNames);
-          server.log(['warning'], {
-            tmpl: (
-              `You're running Kibana ${kibanaVersion} with some different versions of ` +
+          server.logWithMetadata(['warning'],
+            `You're running Kibana ${kibanaVersion} with some different versions of ` +
             'Elasticsearch. Update Kibana or Elasticsearch to the same ' +
-            `version to prevent compatibility issues: ${warningNodeNames}`
-            ),
-            kibanaVersion,
-            nodes: simplifiedNodes,
-          });
+            `version to prevent compatibility issues: ${warningNodeNames}`,
+            {
+              kibanaVersion,
+              nodes: simplifiedNodes,
+            });
         }
       }
 
