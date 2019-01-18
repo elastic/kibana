32be17749e2757571dea827269bc9fd409dfc2fa
Spencer <email@spalger.com>
2019-01-18 15:34:28 -0800

    [pageObjects/visualize] combine common assertions into helper (#28945) (#29008)
    While working on https://github.com/elastic/kibana/pull/28503 an update needs to be made to an assertion that is copy/pasted all over the place. To make the process simpler I've added a new helper that does the work in one place and allows updating once.

--- test/functional/apps/visualize/_pie_chart.js
+++ test/functional/apps/visualize/_pie_chart.js
@@ -53,10 +53,7 @@ export default function ({ getService, getPageObjects }) {
     });
 
     it('should save and load', async function () {
-      await PageObjects.visualize.saveVisualizationExpectSuccess(vizName1);
-      const pageTitle = await PageObjects.common.getBreadcrumbPageTitle();
-      log.debug(`Save viz page title is ${pageTitle}`);
-      expect(pageTitle).to.contain(vizName1);
+      await PageObjects.visualize.saveVisualizationExpectSuccessAndBreadcrumb(vizName1);
       await PageObjects.visualize.waitForVisualizationSavedToastGone();
       await PageObjects.visualize.loadSavedVisualization(vizName1);
       await PageObjects.visualize.waitForVisualization();
@@ -166,10 +163,7 @@ export default function ({ getService, getPageObjects }) {
       });
 
       it('should correctly save disabled agg', async () => {
-        await PageObjects.visualize.saveVisualizationExpectSuccess(vizName1);
-        const pageTitle = await PageObjects.common.getBreadcrumbPageTitle();
-        log.debug(`Save viz page title is ${pageTitle}`);
-        expect(pageTitle).to.contain(vizName1);
+        await PageObjects.visualize.saveVisualizationExpectSuccessAndBreadcrumb(vizName1);
         await PageObjects.visualize.waitForVisualizationSavedToastGone();
         await PageObjects.visualize.loadSavedVisualization(vizName1);
         await PageObjects.visualize.waitForVisualization();
