b3cfe0bb3e89ae0beca3505d51454da2d797d9b6
Mengwei Ding <mengwei.ding@gmail.com>
2019-01-22 20:55:00 +0800

    [Code] Apply socket.io request authentication

--- x-pack/plugins/code/server/routes/socket.ts
+++ x-pack/plugins/code/server/routes/socket.ts
@@ -4,21 +4,59 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import Boom from 'boom';
 import { Server } from 'hapi';
-import { Socket } from 'socket.io';
+import { Handshake, Socket } from 'socket.io';
 
 import { Logger } from '../log';
 import { SocketService } from '../socket_service';
 
+function getRequest(server: Server, { headers }: Handshake) {
+  const url = `/api/code/ping`;
+
+  return server
+    .inject({
+      method: 'POST',
+      url,
+      headers,
+    })
+    .then(res => {
+      if (res.statusCode !== 200) {
+        throw Boom.unauthorized('Failed to authenticate socket connection');
+      }
+      return res.request;
+    });
+}
+
+async function getModifiedRequest(server: Server, socket: Socket) {
+  try {
+    return await getRequest(server, socket.handshake);
+  } catch (err) {
+    // on errors, notify the client and close the connection
+    socket.emit('connectionFailed', { reason: err.message || 'Socket connection failed' });
+    socket.disconnect(true);
+    return false;
+  }
+}
+
 export function socketRoute(server: Server, socketService: SocketService, log: Logger) {
   const socketIO = socketService.io;
 
-  socketIO.on('connection', (socket: Socket) => {
-    log.debug(`User ${socket.id} connected, attaching handlers and register socket.`);
+  // add a POST ping route for `getRequest` to use
+  server.route({
+    method: 'POST',
+    path: `/api/code/ping`,
+    handler: () => 'pong',
+  });
 
-    // TODO(mengwei): apply the same security check as Canvas does.
-    // const request = socket.handshake;
-    // const authHeader = getAuthHeader(request, server);
+  socketIO.on('connection', async (socket: Socket) => {
+    log.info(`User ${socket.id} connected, attaching handlers and register socket.`);
+    // 'request' is the modified hapi request object
+    const request = await getModifiedRequest(server, socket);
+    if (!request) {
+      log.error(`Request object not found. Disconnect the socket.`);
+      return socket.disconnect();
+    }
 
     socket.on('disconnect', () => {
       log.debug('User disconnected, removing handlers and unregister sockets.');
