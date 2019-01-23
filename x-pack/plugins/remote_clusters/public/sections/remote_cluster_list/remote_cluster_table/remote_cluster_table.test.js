eca8711145ff39436687e5012d0752a922392d3c
CJ Cenizal <cj@cenizal.com>
2019-01-22 19:35:28 -0800

    Add unit tests for different types of remote clusters in RemoteClusterTable.

--- x-pack/plugins/remote_clusters/public/sections/remote_cluster_list/remote_cluster_table/remote_cluster_table.test.js
+++ x-pack/plugins/remote_clusters/public/sections/remote_cluster_list/remote_cluster_table/remote_cluster_table.test.js
@@ -11,13 +11,11 @@ import { renderWithIntl } from 'test_utils/enzyme_helpers';
 import { remoteClustersStore } from '../../../store';
 import { RemoteClusterTable } from './remote_cluster_table';
 
-/**
- * Make sure we have deterministic aria ID
- */
-jest.mock('@elastic/eui/lib/components/form/form_row/make_id', () => () => 'my-id');
+// Make sure we have deterministic aria IDs.
+jest.mock('@elastic/eui/lib/components/form/form_row/make_id', () => () => 'fakeId');
 
 describe('RemoteClusterTable', () => {
-  test(`renders a row for a default remote cluster`, () => {
+  test(`renders a row for an API-defined remote cluster`, () => {
     const clusters = [{
       name: 'Default remote cluster',
       seeds: ['seed', 'seed2'],
@@ -35,7 +33,7 @@ describe('RemoteClusterTable', () => {
     expect(component.find('tbody > tr').first()).toMatchSnapshot();
   });
 
-  test(`renders a row for a remote cluster defined in elasticsearch.yml`, () => {
+  test(`renders a row with a tooltip for a remote cluster defined in elasticsearch.yml`, () => {
     const clusters = [{
       name: 'Remote cluster in elasticsearch.yml',
       seeds: ['seed', 'seed2'],
