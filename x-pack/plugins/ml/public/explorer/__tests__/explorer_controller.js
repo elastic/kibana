94cd00904b0a4589c8d93d9853f002ba474f94de
Walter Rafelsberger <walter@elastic.co>
2019-01-23 15:50:18 +0100

    [ML] Fixes explorer_controller test to reflect code changes.

--- x-pack/plugins/ml/public/explorer/__tests__/explorer_controller.js
+++ x-pack/plugins/ml/public/explorer/__tests__/explorer_controller.js
@@ -21,7 +21,6 @@ describe('ML - Explorer Controller', () => {
 
       expect(Array.isArray(scope.jobs)).to.be(true);
       expect(Array.isArray(scope.queryFilters)).to.be(true);
-      expect(scope.loading).to.be(true);
     });
   });
 });
