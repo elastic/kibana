2d90e64a4ad0f29c6977b963db1ad4e982bacee6
delvedor <tommydelved@gmail.com>
2019-01-23 18:32:46 +0100

    Added client type definition also inside x-pack

--- x-pack/typings/kibana.d.ts
+++ x-pack/typings/kibana.d.ts
@@ -4,7 +4,50 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// Import the global Kibana typings until we are able to reference them directly
-// in the tsconfig
+declare module '@elastic/elasticsearch' {
+  export interface ShardsResponse {
+    total: number;
+    successful: number;
+    failed: number;
+    skipped: number;
+  }
 
-import '../../typings/@elastic/elasticsearch';
+  export interface Explanation {
+    value: number;
+    description: string;
+    details: Explanation[];
+  }
+
+  export interface SearchResponse<T> {
+    took: number;
+    timed_out: boolean;
+    _scroll_id?: string;
+    _shards: ShardsResponse;
+    hits: {
+      total: number;
+      max_score: number;
+      hits: Array<{
+        _index: string;
+        _type: string;
+        _id: string;
+        _score: number;
+        _source: T;
+        _version?: number;
+        _explanation?: Explanation;
+        fields?: any;
+        highlight?: any;
+        inner_hits?: any;
+        matched_queries?: string[];
+        sort?: string[];
+      }>;
+    };
+    aggregations?: any;
+  }
+
+  export interface MSearchResponse<T> {
+    responses?: Array<SearchResponse<T>>;
+  }
+}
+
+// We need this so TypeScript doesn't overwrite the typings from the @elastic/elasticsearch library with this file.
+export {};
