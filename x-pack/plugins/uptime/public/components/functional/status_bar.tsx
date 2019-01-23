f60650bdbae5165037201ba672759319dac3311c
Justin Kambic <justin.kambic@elastic.co>
2019-01-23 12:54:12 -0500

    Add aria-labels for monitor status bar.

--- x-pack/plugins/uptime/public/components/functional/status_bar.tsx
+++ x-pack/plugins/uptime/public/components/functional/status_bar.tsx
@@ -21,31 +21,41 @@ export const StatusBar = ({ timestamp, url, duration, status }: Props) => (
   <EuiPanel>
     <EuiFlexGroup gutterSize="l">
       <EuiFlexItem grow={false}>
-        <EuiFlexGroup>
-          <EuiFlexItem>
-            <EuiHealth
-              color={status === 'up' ? 'success' : 'danger'}
-              style={{ lineHeight: 'inherit' }}
-            >
-              {status === 'up'
-                ? i18n.translate('xpack.uptime.monitorStatusBar.healthStatusMessage.upLabel', {
-                    defaultMessage: 'Up',
-                  })
-                : i18n.translate('xpack.uptime.monitorStatusBar.healthStatusMessage.downLabel', {
-                    defaultMessage: 'Down',
-                  })}
-            </EuiHealth>
-          </EuiFlexItem>
-        </EuiFlexGroup>
+        <EuiHealth
+          aria-label={i18n.translate('xpack.uptime.monitorStatusBar.healthStatusMessageAriaLabel', {
+            defaultMessage: 'Monitor status',
+          })}
+          color={status === 'up' ? 'success' : 'danger'}
+          style={{ lineHeight: 'inherit' }}
+        >
+          {status === 'up'
+            ? i18n.translate('xpack.uptime.monitorStatusBar.healthStatusMessage.upLabel', {
+                defaultMessage: 'Up',
+              })
+            : i18n.translate('xpack.uptime.monitorStatusBar.healthStatusMessage.downLabel', {
+                defaultMessage: 'Down',
+              })}
+        </EuiHealth>
       </EuiFlexItem>
       <EuiFlexItem grow={false}>
         <EuiFlexItem grow={false}>
-          <EuiLink href={url} target="_blank">
+          <EuiLink
+            aria-label={i18n.translate('xpack.uptime.monitorStatusBar.monitorUrlLinkAriaLabel', {
+              defaultMessage: 'Monitor URL link',
+            })}
+            href={url}
+            target="_blank"
+          >
             {url}
           </EuiLink>
         </EuiFlexItem>
       </EuiFlexItem>
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem
+        aria-label={i18n.translate('xpack.uptime.monitorStatusBar.durationTextAriaLabel', {
+          defaultMessage: 'Monitor duration in milliseconds',
+        })}
+        grow={false}
+      >
         <FormattedMessage
           id="xpack.uptime.monitorStatusBar.healthStatus.durationInMillisecondsMessage"
           // TODO: this should not be computed inline
@@ -54,7 +64,14 @@ export const StatusBar = ({ timestamp, url, duration, status }: Props) => (
           description="The 'ms' is an abbreviation for 'milliseconds'."
         />
       </EuiFlexItem>
-      <EuiFlexItem grow={false}>{moment(timestamp).fromNow()}</EuiFlexItem>
+      <EuiFlexItem
+        aria-label={i18n.translate('xpack.uptime.monitorStatusBar.timestampFromNowTextAriaLabel', {
+          defaultMessage: 'Time since last check',
+        })}
+        grow={false}
+      >
+        {moment(timestamp).fromNow()}
+      </EuiFlexItem>
     </EuiFlexGroup>
   </EuiPanel>
 );
