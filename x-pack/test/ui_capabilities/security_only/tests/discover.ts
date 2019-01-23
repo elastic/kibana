a4ade89650c448dd2e03810ae3b210e0d20743b5
Brandon Kobel <brandon.kobel@gmail.com>
2019-01-22 16:26:16 -0800

    Feature Controls - Fixing fallout of removing the legacy fallback (#29141)
    * Fixing use of mode.useRbacFoRequest to mode.useRbac
    
    * Fixing ui capability tests

--- x-pack/test/ui_capabilities/security_only/tests/discover.ts
+++ x-pack/test/ui_capabilities/security_only/tests/discover.ts
@@ -6,7 +6,10 @@
 
 import expect from 'expect.js';
 import { KibanaFunctionalTestDefaultProviders } from '../../../types/providers';
-import { UICapabilitiesService } from '../../common/services/ui_capabilities';
+import {
+  GetUICapabilitiesFailureReason,
+  UICapabilitiesService,
+} from '../../common/services/ui_capabilities';
 import { UserScenarios } from '../scenarios';
 
 // tslint:disable:no-default-export
@@ -22,11 +25,8 @@ export default function navLinksTests({ getService }: KibanaFunctionalTestDefaul
         });
         switch (scenario.username) {
           // these users have a read/write view of Discover
-          case 'no_kibana_privileges': // we're stuck with this one until post 7.0
           case 'superuser':
           case 'all':
-          case 'legacy_all':
-          case 'legacy_read':
           case 'dual_privileges_all':
           case 'discover_all':
             expect(uiCapabilities.success).to.be(true);
@@ -72,6 +72,11 @@ export default function navLinksTests({ getService }: KibanaFunctionalTestDefaul
               show: false,
               save: false,
             });
+            break;
+          case 'no_kibana_privileges':
+          case 'legacy_all':
+            expect(uiCapabilities.success).to.be(false);
+            expect(uiCapabilities.failureReason).to.be(GetUICapabilitiesFailureReason.NotFound);
             break;
           default:
             throw new UnreachableError(scenario);
