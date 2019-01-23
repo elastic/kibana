f2112eb2ca7314c6d0157a610afb9e4cc54417b2
Alexandr Ogarkov <nox.911@gmail.com>
2019-01-23 11:38:17 +0300

    Translate state_session_storage_redirect

--- src/legacy/core_plugins/state_session_storage_redirect/index.js
+++ src/legacy/core_plugins/state_session_storage_redirect/index.js
@@ -17,12 +17,16 @@
  * under the License.
  */
 
+import { i18n } from '@kbn/i18n';
+
 export default function (kibana) {
   return new kibana.Plugin({
     uiExports: {
       app: {
         require: ['kibana'],
-        title: 'Redirecting',
+        title: i18n.translate('stateSessionStorageRedirect.redirectingTitle', {
+          defaultMessage: 'Redirecting'
+        }),
         id: 'stateSessionStorageRedirect',
         main: 'plugins/state_session_storage_redirect',
         hidden: true,
