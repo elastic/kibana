03b01e53df29dd0c949096c4c2901abf08ff44ef
Justin Kambic <justin.kambic@elastic.co>
2019-01-22 22:18:22 -0500

    Disable update button for SDP.

--- x-pack/plugins/uptime/public/uptime_app.tsx
+++ x-pack/plugins/uptime/public/uptime_app.tsx
@@ -178,6 +178,7 @@ class Application extends React.Component<UptimeAppProps, UptimeAppState> {
                             this.persistState
                           );
                         }}
+                        showUpdateButton={false}
                       />
                     </div>
                   </EuiHeaderSectionItem>
