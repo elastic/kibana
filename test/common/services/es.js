fc5adf01122399d8fde67f97d08c290781dbfaec
delvedor <tommydelved@gmail.com>
2019-01-24 09:06:48 +0100

    Updated client parameters

--- test/common/services/es.js
+++ test/common/services/es.js
@@ -19,13 +19,13 @@
 
 import { format as formatUrl } from 'url';
 
-import elasticsearch from '@elastic/elasticsearch';
+import { Client } from '@elastic/elasticsearch';
 
 export function EsProvider({ getService }) {
   const config = getService('config');
 
-  return new elasticsearch.Client({
-    host: formatUrl(config.get('servers.elasticsearch')),
+  return new Client({
+    node: formatUrl(config.get('servers.elasticsearch')),
     requestTimeout: config.get('timeouts.esRequestTimeout'),
   });
 }
