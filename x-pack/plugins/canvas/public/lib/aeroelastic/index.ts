0de0b591b4d3f0970697f29c048c6a6ecc5978f3
monfera <monfera.robert@gmail.com>
2019-01-25 15:34:43 +0100

    Lint: TS 04

--- x-pack/plugins/canvas/public/lib/aeroelastic/index.ts
+++ x-pack/plugins/canvas/public/lib/aeroelastic/index.ts
@@ -1,15 +1,13 @@
-/* tslint:disable */
-
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { matrixToCSS } from './dom';
+import { nextScene, primaryUpdate } from './layout';
 import { multiply, rotateZ, translate } from './matrix';
 import { createStore, select } from './state';
-import { nextScene, primaryUpdate } from './layout';
-import { matrixToCSS } from './dom';
 
 export const layout = { nextScene, primaryUpdate };
 export const matrix = { multiply, rotateZ, translate };
