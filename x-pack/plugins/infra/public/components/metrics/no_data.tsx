695363c5535547c3fa63a4ccef0264574c29280d
Kerry Gallagher <k.gallagher.05@gmail.com>
2019-01-25 14:41:46 +0000

    Amend for Prettier

--- x-pack/plugins/infra/public/components/metrics/no_data.tsx
+++ x-pack/plugins/infra/public/components/metrics/no_data.tsx
@@ -8,48 +8,34 @@ import { EuiButton, EuiEmptyPrompt } from '@elastic/eui';
 import React from 'react';
 import styled from 'styled-components';
 
- interface NoDataProps {
-    titleText: string;
-    bodyText: string;
-    refetchText: string;
-    onRefetch: () => void;
-    testString?: string;
- }
+interface NoDataProps {
+  titleText: string;
+  bodyText: string;
+  refetchText: string;
+  onRefetch: () => void;
+  testString?: string;
+}
 
- export const NoData: React.SFC<NoDataProps> = ({
+export const NoData: React.SFC<NoDataProps> = ({
   titleText,
   bodyText,
   refetchText,
   onRefetch,
-  testString
- }) => (
-    <CenteredEmptyPrompt
-        title={
-          <h2>
-            {titleText}
-          </h2>
-        }
-        titleSize="m"
-        body={
-          <p>
-            {bodyText}
-          </p>
-        }
-        actions={
-          <EuiButton
-              iconType="refresh"
-              color="primary"
-              fill
-              onClick={onRefetch}
-          >
-            {refetchText}
-          </EuiButton>
-        }
+  testString,
+}) => (
+  <CenteredEmptyPrompt
+    title={<h2>{titleText}</h2>}
+    titleSize="m"
+    body={<p>{bodyText}</p>}
+    actions={
+      <EuiButton iconType="refresh" color="primary" fill onClick={onRefetch}>
+        {refetchText}
+      </EuiButton>
+    }
+    data-test-subj={testString}
+  />
+);
 
-        data-test-subj={testString}
-    />
- );
-
- const CenteredEmptyPrompt = styled(EuiEmptyPrompt)`
+const CenteredEmptyPrompt = styled(EuiEmptyPrompt)`
   align-self: center;
 `;
