48a1140c680c125ada1e37583ce3c505cad698b1
Garrett Spong <garrett.spong@elastic.co>
2019-01-26 21:45:56 -0700

    First pass at adding dnd to the summary values

--- x-pack/plugins/secops/public/components/page/hosts/hosts_details_panel/index.tsx
+++ x-pack/plugins/secops/public/components/page/hosts/hosts_details_panel/index.tsx
@@ -6,15 +6,20 @@
 
 import {
   EuiDescriptionList,
+  EuiDescriptionListDescription,
+  EuiDescriptionListTitle,
   EuiFlexItem,
   EuiHorizontalRule,
   EuiPanel,
   EuiTitle,
 } from '@elastic/eui';
+import moment from 'moment';
 import React from 'react';
 import { pure } from 'recompose';
 import { HostsEdges } from 'x-pack/plugins/secops/server/graphql/types';
+import { DragEffects, DraggableWrapper } from '../../../drag_and_drop/draggable_wrapper';
 import { getOrEmpty } from '../../../empty_value';
+import { Provider } from '../../../timeline/data_providers/provider';
 import * as i18n from './translations';
 
 interface OwnProps {
@@ -32,54 +37,67 @@ export const HostDetailsPanel = pure<HostDetailsPanelProps>(({ data, loading }) 
       </EuiTitle>
 
       <EuiHorizontalRule margin="xs" />
-      <EuiDescriptionList listItems={getHostDetailsItems(data[0])} type="column" compressed />
+      {getEuiDescriptionList(data[0])}
     </EuiPanel>
   </EuiFlexItem>
 ));
 
-const getHostDetailsItems = (host: HostsEdges) => [
-  {
-    title: i18n.NAME,
-    description: getOrEmpty('node.host.name', host),
-  },
-  {
-    title: i18n.LAST_BEAT,
-    description: getOrEmpty('node.host.last_beat', host),
-  },
-  {
-    title: i18n.ID,
-    description: getOrEmpty('node.host.id', host),
-  },
-  {
-    title: i18n.IP_ADDRESS,
-    description: getOrEmpty('node.host.ip', host),
-  },
-  {
-    title: i18n.MAC_ADDRESS,
-    description: getOrEmpty('node.host.mac', host),
-  },
-  {
-    title: i18n.TYPE,
-    description: getOrEmpty('node.lastSeen', host),
-  },
-  {
-    title: i18n.PLATFORM,
-    description: getOrEmpty('node.lastSeen', host),
-  },
-  {
-    title: i18n.OS_NAME,
-    description: getOrEmpty('node.host.os.name', host),
-  },
-  {
-    title: i18n.FAMILY,
-    description: getOrEmpty('node.lastSeen', host),
-  },
-  {
-    title: i18n.VERSION,
-    description: getOrEmpty('node.host.os.version', host),
-  },
-  {
-    title: i18n.ARCHITECTURE,
-    description: getOrEmpty('node.host.architecture', host),
-  },
-];
+const fieldTitleMapping = {
+  'node.host.name': i18n.NAME,
+  'node.host.last_beat': i18n.LAST_BEAT,
+  'node.host.id': i18n.ID,
+  'node.host.ip': i18n.IP_ADDRESS,
+  'node.host.mac': i18n.MAC_ADDRESS,
+  'node.type': i18n.TYPE,
+  'node.lastSeen': i18n.PLATFORM,
+  'node.host.os.name': i18n.OS_NAME,
+  'node.family': i18n.FAMILY,
+  'node.host.os.version': i18n.VERSION,
+  'node.host.architecture': i18n.ARCHITECTURE,
+};
+
+const getEuiDescriptionList = (host: HostsEdges) => {
+  return (
+    <EuiDescriptionList type="column" compressed>
+      {Object.entries(fieldTitleMapping).map(k => {
+        const summaryValue = getOrEmpty(k[0], host);
+        return (
+          <>
+            <EuiDescriptionListTitle>{k[1]}</EuiDescriptionListTitle>
+            <EuiDescriptionListDescription>
+              <DraggableWrapper
+                dataProvider={{
+                  and: [],
+                  enabled: true,
+                  excluded: false,
+                  id: `${getOrEmpty('node.host.id', host)}-${k[0]}`,
+                  name: summaryValue,
+                  kqlQuery: '',
+                  queryMatch: {
+                    displayField: k[0],
+                    displayValue: summaryValue,
+                    field: k[0],
+                    value: summaryValue,
+                  },
+                  queryDate: {
+                    from: moment().valueOf(),
+                    to: moment().valueOf(),
+                  },
+                }}
+                render={(dataProvider, _, snapshot) =>
+                  snapshot.isDragging ? (
+                    <DragEffects>
+                      <Provider dataProvider={dataProvider} />
+                    </DragEffects>
+                  ) : (
+                    <>{summaryValue}</>
+                  )
+                }
+              />
+            </EuiDescriptionListDescription>
+          </>
+        );
+      })}
+    </EuiDescriptionList>
+  );
+};
