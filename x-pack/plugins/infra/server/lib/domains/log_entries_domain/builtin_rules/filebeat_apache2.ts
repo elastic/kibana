d7a14663de9e5c639a7d5190ff2dd1c8bb5a6d84
Felix Stürmer <stuermer@weltenwort.de>
2019-01-29 18:39:25 +0100

    Fix ECS-compatible apache rules and restore old ones

--- x-pack/plugins/infra/server/lib/domains/log_entries_domain/builtin_rules/filebeat_apache2.ts
+++ x-pack/plugins/infra/server/lib/domains/log_entries_domain/builtin_rules/filebeat_apache2.ts
@@ -6,8 +6,11 @@
 
 export const filebeatApache2Rules = [
   {
+    // ECS
     when: {
-      exists: ['apache2.access'],
+      values: {
+        'event.dataset': 'apache.access',
+      },
     },
     format: [
       {
@@ -50,11 +53,84 @@ export const filebeatApache2Rules = [
         constant: ' ',
       },
       {
+        field: 'http.response.body.bytes',
+      },
+    ],
+  },
+  {
+    // pre-ECS
+    when: {
+      exists: ['apache2.access'],
+    },
+    format: [
+      {
+        constant: '[Apache][access] ',
+      },
+      {
+        field: 'apache2.access.remote_ip',
+      },
+      {
+        constant: ' ',
+      },
+      {
+        field: 'apache2.access.user_name',
+      },
+      {
+        constant: ' "',
+      },
+      {
+        field: 'apache2.access.method',
+      },
+      {
+        constant: ' ',
+      },
+      {
+        field: 'apache2.access.url',
+      },
+      {
+        constant: ' HTTP/',
+      },
+      {
+        field: 'apache2.access.http_version',
+      },
+      {
+        constant: '" ',
+      },
+      {
+        field: 'apache2.access.response_code',
+      },
+      {
+        constant: ' ',
+      },
+      {
         field: 'apache2.access.body_sent.bytes',
       },
     ],
   },
   {
+    // ECS
+    when: {
+      values: {
+        'event.dataset': 'apache.error',
+      },
+    },
+    format: [
+      {
+        constant: '[Apache][',
+      },
+      {
+        field: 'log.level',
+      },
+      {
+        constant: '] ',
+      },
+      {
+        field: 'message',
+      },
+    ],
+  },
+  {
+    // pre-ECS
     when: {
       exists: ['apache2.error.message'],
     },
