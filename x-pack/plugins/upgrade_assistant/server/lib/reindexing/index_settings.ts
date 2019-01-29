c589a8db6a9fb7e0c26fc38ba2eba662c8c5271a
Josh Dover <me@joshdover.com>
2019-01-29 10:14:32 -0600

    Add reindex feature to Upgrade Assistant (#27457)
    * Fixup saved objects
    
    * WIP reindex state machine
    
    * WIP UI
    
    * Separate status from current step
    
    * Add error messages
    
    * Copy types to CallWithInternalUser
    
    * Use backend worker
    
    * Add progress bar for large indices
    
    * Cleanup worker implementation
    
    * Add tests for ReindexService
    
    * Fix types
    
    * Fix CI
    
    * Add support for mapping coerced boolean fields
    
    * Add basic functional integration test
    
    * Cleanup reindexing backend, add more checks
    
    * Cleanup frontend code and add tests
    
    * Support removal of _default_ mapping + add tests
    
    * Add reindex warnings to reindex service
    
    * Add confirmation modal for reindex warnings
    
    * Cleanup flyout appearance
    
    * Generate new index name intelligently
    
    * Design tweaks
    
    * Show reindex button in both grouping modes
    
    * Add data archive integration tests
    
    * Change flyout design to two step process
    
    * Reorganize flyout files
    
    * Use custom steps component
    
    * Allow writes to indices if anything fails
    
    * Misc cleanup
    
    * Design edits
    
    * Only show warnings panel when reindex has not begun
    
    * Fix types
    
    * Handle moving existing aliases
    
    * Move integration tests to separate config
    
    * Fix ReindexButton bugs when changing pages
    
    * Fix polling service tests
    
    * Refactor ReindexService and ML handling (backend only)
    
    * Add credential caching and paused state to backend
    
    * Add paused state to frontend
    
    * Fix ts errors
    
    * Update copy
    
    * Add check for node version before ML step
    
    * Update data archive format
    
    * Update snapshots for React upgrade
    
    * Handle _default_ mappings correctly in warning checks
    
    * Use hashed state of reindex operation for credential storage
    
    * Address tyler's comments
    
    * Add watcher stop/starting
    
    * Use json-stable-stringify and sha256 for CredentialStore
    
    * Add types for json-stable-stringify

---
+++ x-pack/plugins/upgrade_assistant/server/lib/reindexing/index_settings.ts
@@ -0,0 +1,140 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { flow, omit } from 'lodash';
+import { ReindexWarning } from '../../../common/types';
+import { FlatSettings, MappingProperties, TypeMapping } from './types';
+
+/**
+ * Validates, and updates deprecated settings and mappings to be applied to the
+ * new updated index.
+ */
+export const transformFlatSettings = (flatSettings: FlatSettings) => {
+  const settings = transformSettings(flatSettings.settings);
+  const mappings = transformMappings(flatSettings.mappings);
+
+  return { settings, mappings };
+};
+
+/**
+ * Returns an array of warnings that should be displayed to user before reindexing begins.
+ * @param flatSettings
+ */
+export const getReindexWarnings = (flatSettings: FlatSettings): ReindexWarning[] => {
+  const mapping = getSingleMappingType(flatSettings.mappings);
+
+  const warnings = [
+    [ReindexWarning.allField, Boolean(mapping && mapping._all && mapping._all.enabled)],
+    [
+      ReindexWarning.booleanFields,
+      Boolean(mapping && mapping.properties && findBooleanFields(mapping.properties).length > 0),
+    ],
+  ] as Array<[ReindexWarning, boolean]>;
+
+  return warnings.filter(([_, applies]) => applies).map(([warning, _]) => warning);
+};
+
+/**
+ * Returns an array of field paths for all boolean fields, where each field path is an array of strings.
+ * Example:
+ *    For the mapping type:
+ *    ```
+ *      {
+ *        "field1": { "type": "boolean" },
+ *        "nested": {
+ *          "field2": { "type": "boolean" }
+ *        }
+ *      }
+ *    ```
+ *    The fieldPaths would be: `[['field1'], ['nested', 'field2']]`
+ * @param properties
+ */
+export const findBooleanFields = (properties: MappingProperties): string[][] =>
+  Object.keys(properties).reduce(
+    (res, propertyName) => {
+      if (properties[propertyName].type === 'boolean') {
+        // If this field is a boolean, add it
+        res.push([propertyName]);
+      } else if (properties[propertyName].properties) {
+        // If this is a nested object/array get the nested fields and prepend the field path with the current field.
+        const nested = findBooleanFields(properties[propertyName].properties!);
+        res = [...res, ...nested.map(n => [propertyName, ...n])];
+      }
+
+      return res;
+    },
+    [] as string[][]
+  );
+
+const removeUnsettableSettings = (settings: FlatSettings['settings']) =>
+  omit(settings, [
+    'index.uuid',
+    'index.blocks.write',
+    'index.creation_date',
+    'index.legacy',
+    'index.mapping.single_type',
+    'index.provided_name',
+    'index.routing.allocation.initial_recovery._id',
+    'index.version.created',
+    'index.version.upgraded',
+  ]);
+
+const updateFixableSettings = (settings: FlatSettings['settings']) => {
+  const delayedTimeout = settings['index.unassigned.node_left.delayed_timeout'];
+  if (delayedTimeout && parseInt(delayedTimeout, 10) < 0) {
+    settings['index.unassigned.node_left.delayed_timeout'] = '0';
+  }
+
+  return settings;
+};
+
+const validateSettings = (settings: FlatSettings['settings']) => {
+  if (settings['index.shard.check_on_startup'] === 'fix') {
+    throw new Error(`index.shard.check_on_startup cannot be set to 'fix'`);
+  }
+
+  if (settings['index.percolator.map_unmapped_fields_as_string']) {
+    throw new Error(`index.percolator.map_unmapped_fields_as_string is no longer supported.`);
+  }
+
+  return settings;
+};
+
+// Use `flow` to pipe the settings through each function.
+const transformSettings = flow(
+  removeUnsettableSettings,
+  updateFixableSettings,
+  validateSettings
+);
+
+const updateFixableMappings = (mappings: FlatSettings['mappings']) => {
+  if (mappings._default_) {
+    delete mappings._default_;
+  }
+
+  const mapping = getSingleMappingType(mappings);
+  if (mapping && mapping._all) {
+    delete mapping._all;
+  }
+
+  return mappings;
+};
+
+const transformMappings = flow(updateFixableMappings);
+
+export const getSingleMappingType = (
+  mappings: FlatSettings['mappings']
+): TypeMapping | undefined => {
+  const mappingTypes = Object.keys(mappings)
+    // Ignore _default_ mapping types.
+    .filter(t => t !== '_default_');
+
+  if (mappingTypes.length > 1) {
+    throw new Error(`Indices with more than one mapping type are not supported in 7.0.`);
+  }
+
+  return mappings[mappingTypes[0]];
+};
