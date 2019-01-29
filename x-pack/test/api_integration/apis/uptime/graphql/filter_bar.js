3832749821e93a28344edf4afe757d9e0cc8e2ab
Justin Kambic <justin.kambic@elastic.co>
2019-01-29 12:31:53 -0500

    Update filter bar query to work with updated 7.x data.

--- x-pack/test/api_integration/apis/uptime/graphql/filter_bar.js
+++ x-pack/test/api_integration/apis/uptime/graphql/filter_bar.js
@@ -17,8 +17,8 @@ export default function ({ getService }) {
         operationName: 'FilterBar',
         query: getFilterBarQueryString,
         variables: {
-          dateRangeStart: '2019-01-25T04:30:54.740Z',
-          dateRangeEnd: '2019-01-28T04:50:54.740Z',
+          dateRangeStart: '2019-01-28T17:40:08.078Z',
+          dateRangeEnd: '2019-01-28T19:00:16.078Z',
         },
       };
       const {
