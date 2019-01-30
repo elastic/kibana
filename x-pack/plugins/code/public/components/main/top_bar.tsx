adf08535bf0c25527341b2f7bd521268f30c7d04
wangqianliang <wangqianliang@live.com>
2019-01-30 20:52:17 +0800

    feature(code/frontend): implement new main page design

--- x-pack/plugins/code/public/components/main/top_bar.tsx
+++ x-pack/plugins/code/public/components/main/top_bar.tsx
@@ -4,29 +4,23 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { euiBorderThick, paddingSizes } from '@elastic/eui/dist/eui_theme_light.json';
 import React from 'react';
 import styled from 'styled-components';
-
 import { SearchScope } from '../../../model';
 import { MainRouteParams } from '../../common/types';
 import { Breadcrumb } from './breadcrumb';
 import { SearchBar } from './search_bar';
 
 const TopBarContainer = styled.div`
-  --topBarContainerHeight: calc(40rem / 14);
   box-sizing: content-box;
   display: flex;
-  flex-grow: 0;
-  flex-shrink: 0;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-between;
   padding: ${paddingSizes.s};
-  height: var(--topBarContainerHeight);
-  min-height: var(--topBarContainerHeight);
   border-bottom: ${euiBorderThick};
   nav {
-    line-height: var(--topBarContainerHeight);
     a {
       display: inline;
     }
@@ -39,14 +33,20 @@ const TopBarContainer = styled.div`
 interface Props {
   routeParams: MainRouteParams;
   onSearchScopeChanged: (s: SearchScope) => void;
+  buttons: React.ReactNode;
 }
 
 export class TopBar extends React.Component<Props> {
   public render() {
     return (
       <TopBarContainer>
-        <Breadcrumb routeParams={this.props.routeParams} />
         <SearchBar onSearchScopeChanged={this.props.onSearchScopeChanged} />
+        <EuiFlexGroup gutterSize="none" justifyContent="spaceBetween">
+          <EuiFlexItem grow={false}>
+            <Breadcrumb routeParams={this.props.routeParams} />
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>{this.props.buttons}</EuiFlexItem>
+        </EuiFlexGroup>
       </TopBarContainer>
     );
   }
