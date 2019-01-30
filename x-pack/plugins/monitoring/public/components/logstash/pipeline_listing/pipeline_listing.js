743a79d0ae11a3ebe6e468acd01f9784c34ddbed
Chris Roberson <chrisronline@gmail.com>
2019-01-30 14:25:32 -0500

    Fix issue where the date format isn't passed in properly (#29637)

--- x-pack/plugins/monitoring/public/components/logstash/pipeline_listing/pipeline_listing.js
+++ x-pack/plugins/monitoring/public/components/logstash/pipeline_listing/pipeline_listing.js
@@ -16,8 +16,8 @@ import { injectI18n } from '@kbn/i18n/react';
 import { i18n } from '@kbn/i18n';
 
 class PipelineListingUI extends Component {
-  tooltipXValueFormatter(xValue) {
-    return moment(xValue).format(this.props.dateFormat);
+  tooltipXValueFormatter(xValue, dateFormat) {
+    return moment(xValue).format(dateFormat);
   }
 
   tooltipYValueFormatter(yValue, format, units) {
@@ -25,7 +25,7 @@ class PipelineListingUI extends Component {
   }
 
   getColumns() {
-    const { onBrush } = this.props;
+    const { onBrush, dateFormat } = this.props;
     const { kbnUrl, scope } = this.props.angular;
 
     return [
@@ -66,7 +66,7 @@ class PipelineListingUI extends Component {
                   series={throughput.data}
                   onBrush={onBrush}
                   tooltip={{
-                    xValueFormatter: value => this.tooltipXValueFormatter(value),
+                    xValueFormatter: value => this.tooltipXValueFormatter(value, dateFormat),
                     yValueFormatter: partialRight(this.tooltipYValueFormatter, throughput.metric.format, throughput.metric.units)
                   }}
                   options={{ xaxis: throughput.timeRange }}
@@ -100,7 +100,7 @@ class PipelineListingUI extends Component {
                   series={nodesCount.data}
                   onBrush={onBrush}
                   tooltip={{
-                    xValueFormatter: this.tooltipXValueFormatter,
+                    xValueFormatter: value => this.tooltipXValueFormatter(value, dateFormat),
                     yValueFormatter: partialRight(this.tooltipYValueFormatter, nodesCount.metric.format, nodesCount.metric.units)
                   }}
                   options={{ xaxis: nodesCount.timeRange }}
