996d65a3e5e63f60c851bccac5687c067eeff5c1
Bill McConaghy <bill.mcconaghy@elastic.co>
2019-01-30 12:59:41 -0500

    making badges clickable to filter in index management

--- x-pack/plugins/index_management/public/lib/render_badges.js
+++ x-pack/plugins/index_management/public/lib/render_badges.js
@@ -6,15 +6,26 @@
 
 
 import React, { Fragment } from 'react';
-import { EuiBadge } from '@elastic/eui';
+import { EuiBadge, EuiSearchBar } from '@elastic/eui';
 import { getBadgeExtensions } from '../index_management_extensions';
-export const renderBadges = (index) => {
+export const renderBadges = (index, filterChanged) => {
   const badgeLabels = [];
-  getBadgeExtensions().forEach(({ matchIndex, label, color }) => {
+  getBadgeExtensions().forEach(({ matchIndex, label, color, filterExpression }) => {
     if (matchIndex(index)) {
+      const clickHandler = () => {
+        filterChanged
+          && filterExpression
+          && filterChanged(EuiSearchBar.Query.parse(filterExpression));
+      };
       badgeLabels.push(
         <Fragment key={label}>
-          {' '}<EuiBadge color={color}>{label}</EuiBadge>
+          {' '}
+          <EuiBadge
+            color={color}
+            onClick={clickHandler}
+          >
+            {label}
+          </EuiBadge>
         </Fragment>
       );
     }
