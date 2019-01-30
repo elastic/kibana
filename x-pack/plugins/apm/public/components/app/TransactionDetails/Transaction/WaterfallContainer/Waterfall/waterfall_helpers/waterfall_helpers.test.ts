d4c0adee35a7a2ab471729c9d7158e7b5892165d
Oliver Gupte <olivergupte@gmail.com>
2019-01-30 13:48:19 -0800

    [APM] minimized test fixure to isolate logic being tested

--- x-pack/plugins/apm/public/components/app/TransactionDetails/Transaction/WaterfallContainer/Waterfall/waterfall_helpers/waterfall_helpers.test.ts
+++ x-pack/plugins/apm/public/components/app/TransactionDetails/Transaction/WaterfallContainer/Waterfall/waterfall_helpers/waterfall_helpers.test.ts
@@ -100,78 +100,9 @@ describe('waterfall_helpers', () => {
     });
 
     it('should handle cyclic references', () => {
-      const items: IWaterfallItem[] = [
-        {
-          id: 'c',
-          parentId: 'c',
-          serviceName: 'opbeans-java',
-          name: 'SELECT',
-          duration: 210,
-          timestamp: 1536763736371000,
-          offset: 0,
-          skew: 0,
-          docType: 'span',
-          span: {
-            transaction: {
-              id: 'c'
-            }
-          } as Span
-        },
-        {
-          id: 'b',
-          parentId: 'a',
-          serviceName: 'opbeans-java',
-          name: 'GET [0:0:0:0:0:0:0:1]',
-          duration: 4694,
-          timestamp: 1536763736368000,
-          offset: 0,
-          skew: 0,
-          docType: 'span',
-          span: {
-            transaction: {
-              id: 'a'
-            }
-          } as Span
-        },
-        {
-          id: 'b2',
-          parentId: 'a',
-          serviceName: 'opbeans-java',
-          name: 'GET [0:0:0:0:0:0:0:1]',
-          duration: 4694,
-          timestamp: 1536763736367000,
-          offset: 0,
-          skew: 0,
-          docType: 'span',
-          span: {
-            transaction: {
-              id: 'a'
-            }
-          } as Span
-        },
-        {
-          id: 'c',
-          parentId: 'b',
-          serviceName: 'opbeans-java',
-          name: 'APIRestController#productsRemote',
-          duration: 3581,
-          timestamp: 1536763736369000,
-          offset: 0,
-          skew: 0,
-          docType: 'transaction',
-          transaction: {} as Transaction
-        },
-        {
-          id: 'a',
-          serviceName: 'opbeans-java',
-          name: 'APIRestController#products',
-          duration: 9480,
-          timestamp: 1536763736366000,
-          offset: 0,
-          skew: 0,
-          docType: 'transaction',
-          transaction: {} as Transaction
-        }
+      const items = [
+        { id: 'a', timestamp: 10 } as IWaterfallItem,
+        { id: 'a', parentId: 'a', timestamp: 20 } as IWaterfallItem
       ];
       const childrenByParentId = groupBy(items, hit =>
         hit.parentId ? hit.parentId : 'root'
