dc4836b563f065b2135ba962bbe46f177b14096a
Thomas Neirynck <thomas@elastic.co>
2019-01-30 18:33:24 -0500

    remove cruft

--- src/legacy/core_plugins/tile_map/common/ems_client.js
+++ src/legacy/core_plugins/tile_map/common/ems_client.js
@@ -107,8 +107,6 @@ export class EMSClientV66 {
     this._loadFileLayers = null;
     this._loadTMSServices = null;
     this._emsLandingPageUrl = landingPageUrl;
-
-    console.log(language);
     this._language = typeof language === 'string' ? language : DEFAULT_LANGUAGE;
 
     this._invalidateSettings();
