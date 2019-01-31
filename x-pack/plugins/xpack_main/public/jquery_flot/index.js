ee267608683c182c5ba2ea1fc0a431e7d74016d2
Tiago Costa <tiagoffcc@hotmail.com>
2019-01-31 20:47:49 +0000

    refact(NA): use es6 for xpack jquery flots.

--- x-pack/plugins/xpack_main/public/jquery_flot/index.js
+++ x-pack/plugins/xpack_main/public/jquery_flot/index.js
@@ -4,13 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-const $ = require('jquery');
-if (window) { window.jQuery = $; }
-require('ui/flot-charts/jquery.flot');
-
-// load flot plugins
-// avoid the `canvas` plugin, it causes blurry fonts
-require('ui/flot-charts/jquery.flot.time');
-require('ui/flot-charts/jquery.flot.crosshair');
-require('ui/flot-charts/jquery.flot.selection');
-module.exports = $;
+export { default } from './jquery_flot';
