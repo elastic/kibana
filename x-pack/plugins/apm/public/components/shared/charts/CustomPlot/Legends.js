93cd898e2cd0260e2a59a6ff623ed1723e62c475
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-01-31 14:33:33 +0100

    Re-apply wrap and margin-top

--- x-pack/plugins/apm/public/components/shared/charts/CustomPlot/Legends.js
+++ x-pack/plugins/apm/public/components/shared/charts/CustomPlot/Legends.js
@@ -20,10 +20,11 @@ import {
 const Container = styled.div`
   display: flex;
   margin-left: ${px(unit * 5)};
-  margin-top: ${px(units.half)};
+  flex-wrap: wrap;
 
   /* add margin to all direct descendant divs */
   & > div {
+    margin-top: ${px(units.half)};
     margin-right: ${px(unit)};
     &:last-child {
       margin-right: 0;
