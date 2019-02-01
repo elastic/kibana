d26072c353d9fdf6ae4c0b0b8581a67217a1b4ad
Josh Dover <me@joshdover.com>
2019-02-01 12:14:33 -0600

    Add backend for deleting .tasks index

---
+++ x-pack/plugins/upgrade_assistant/server/routes/delete_tasks.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import Boom from 'boom';
+import { Server } from 'hapi';
+
+export function registerDeleteTasksRoutes(server: Server) {
+  const { callWithRequest } = server.plugins.elasticsearch.getCluster('admin');
+
+  server.route({
+    path: '/api/upgrade_assistant/delete_tasks_index',
+    method: 'POST',
+    async handler(request) {
+      try {
+        const { acknowledged } = await callWithRequest(request, 'indices.delete', {
+          index: '.tasks',
+        });
+
+        if (!acknowledged) {
+          throw new Error('Could not delete .tasks index');
+        }
+
+        return { success: true };
+      } catch (e) {
+        if (!e.isBoom) {
+          return Boom.boomify(e, { statusCode: 500 });
+        }
+
+        return e;
+      }
+    },
+  });
+}
