b795a68a11813c8f17c8424c2e6e12e61352b7ad
Tim Roes <mail@timroes.de>
2019-02-01 11:21:15 +0100

    Revert "Remove shallow option"
    This reverts commit fea974897f57ca3ba710a36e7e260f3288ff0f77.

--- src/ui/public/vis/vis_types/vislib_vis_legend.js
+++ src/ui/public/vis/vis_types/vislib_vis_legend.js
@@ -104,13 +104,13 @@ uiModules.get('kibana')
         $scope.filter = function (legendData, negate) {
           const value = Array.isArray(legendData.values) ? legendData.values[0] : legendData.values;
           const datum = { aggConfigResult: value.aggConfigResult.$parent };
-          $scope.vis.API.events.filter({ datum, negate: negate });
+          $scope.vis.API.events.filter({ datum, negate: negate, shallow: true });
         };
 
         $scope.canFilter = function (legendData) {
           const value = Array.isArray(legendData.values) ? legendData.values[0] : legendData.values;
           const datum = { aggConfigResult: value.aggConfigResult.$parent };
-          const filters = visFilters.filter({ datum }, { simulate: true });
+          const filters = visFilters.filter({ datum, shallow: true }, { simulate: true });
           return filters.length;
         };
 
