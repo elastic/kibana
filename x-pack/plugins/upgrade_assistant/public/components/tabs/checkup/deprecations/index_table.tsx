ede668cd2c1fab98153238ffc0a2e17f27df6bb7
Tyler Smalley <tyler.smalley@elastic.co>
2019-02-01 22:42:53 -0800

    Fix types
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- x-pack/plugins/upgrade_assistant/public/components/tabs/checkup/deprecations/index_table.tsx
+++ x-pack/plugins/upgrade_assistant/public/components/tabs/checkup/deprecations/index_table.tsx
@@ -15,7 +15,7 @@ const PAGE_SIZES = [10, 25, 50, 100, 250, 500, 1000];
 
 export interface IndexDeprecationDetails {
   index: string;
-  reindex: boolean;
+  reindex?: boolean;
   details?: string;
 }
 
