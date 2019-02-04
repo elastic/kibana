7966b2ff78d2279265df8269938b07120b226d01
Walter Rafelsberger <walter@rafelsberger.at>
2019-02-04 13:14:25 +0100

    [ML] adds telemetry to ML (#29121)
    Adds telemetry to collect the amount of indices created using File Data Visualizer.

--- x-pack/plugins/ml/common/constants/file_datavisualizer.ts
+++ x-pack/plugins/ml/common/constants/file_datavisualizer.ts
@@ -4,9 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-
 export const MAX_BYTES = 104857600;
 
 // Value to use in the Elasticsearch index mapping meta data to identify the
 // index as having been created by the ML File Data Visualizer.
-export const INDEX_META_DATA_CREATED_BY = 'ml_file_data_visualizer';
+export const INDEX_META_DATA_CREATED_BY = 'ml-file-data-visualizer';
