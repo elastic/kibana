b7684368cad1400a1ed50bf01124823626ad11a6
Larry Gregory <larry.gregory@elastic.co>
2019-02-05 14:47:35 -0500

    remove unnecessary call to set ui settings

--- x-pack/test/functional/apps/timelion/feature_controls/timelion_spaces.ts
+++ x-pack/test/functional/apps/timelion/feature_controls/timelion_spaces.ts
@@ -61,11 +61,6 @@ export default function({ getPageObjects, getService }: KibanaFunctionalTestDefa
         // we need to load the following in every situation as deleting
         // a space deletes all of the associated saved objects
         await esArchiver.load('timelion/feature_controls');
-        await kibanaServer.uiSettings.replace({
-          'accessibility:disableAnimations': true,
-          'telemetry:optIn': false,
-          defaultIndex: 'logstash-*',
-        });
         await spacesService.create({
           id: 'custom_space',
           name: 'custom_space',
