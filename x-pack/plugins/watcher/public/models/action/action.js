f783d34d327987469d15fccc1b668aa62e1b1528
Sébastien <sebastien.loix@elastic.co>
2019-02-05 08:06:07 +0100

    [Watcher] Add missing PagerDuty action on the client

--- x-pack/plugins/watcher/public/models/action/action.js
+++ x-pack/plugins/watcher/public/models/action/action.js
@@ -12,6 +12,7 @@ import { SlackAction } from './slack_action';
 import { WebhookAction } from './webhook.action';
 import { IndexAction } from './index.action';
 import { HipchatAction } from './hipchat.action';
+import { PagerDutyAction } from './pagerduty.action';
 import { UnknownAction } from './unknown_action';
 
 const ActionTypes = {};
@@ -21,6 +22,7 @@ set(ActionTypes, ACTION_TYPES.SLACK, SlackAction);
 set(ActionTypes, ACTION_TYPES.WEBHOOK, WebhookAction);
 set(ActionTypes, ACTION_TYPES.INDEX, IndexAction);
 set(ActionTypes, ACTION_TYPES.HIPCHAT, HipchatAction);
+set(ActionTypes, ACTION_TYPES.HIPCHAT, PagerDutyAction);
 
 export class Action {
   static getActionTypes = () => {
