919d5d04f3234f57afe88a95be10d4fc2b033162
Tim Roes <mail@timroes.de>
2019-02-06 18:09:20 +0100

    Replace ts-ignore by any imports

--- src/ui/public/saved_objects/saved_object_registry.d.ts
+++ src/ui/public/saved_objects/saved_object_registry.d.ts
@@ -17,8 +17,4 @@
  * under the License.
  */
 
-export { SavedObjectsClient } from './saved_objects_client';
-export { SavedObjectRegistryProvider } from './saved_object_registry';
-export { SavedObjectsClientProvider } from './saved_objects_client_provider';
-export { SavedObject } from './saved_object';
-export { findObjectByTitle } from './find_object_by_title';
+export type SavedObjectRegistryProvider = any;
