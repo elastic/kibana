1d2d79132fa01f47d01d2e4f0ab2e68b5fc19a24
Larry Gregory <larry.gregory@elastic.co>
2019-02-06 09:58:04 -0500

    remove duplicate license header

--- x-pack/test/functional/apps/advanced_settings/index.ts
+++ x-pack/test/functional/apps/advanced_settings/index.ts
@@ -6,11 +6,6 @@
 
 import { KibanaFunctionalTestDefaultProviders } from 'x-pack/test/types/providers';
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
 // tslint:disable:no-default-export
 export default function advancedSettingsApp({
   loadTestFile,
