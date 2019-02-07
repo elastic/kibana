9c98c44eb27d59ee116746070905bceb38aeb9a6
Larry Gregory <lgregorydev@gmail.com>
2019-02-07 14:11:01 -0500

    [Feature Controls] - Readonly view for Advanced Settings using UICapabilities (#30243)
    ## Summary
    This builds on the work done in https://github.com/elastic/kibana/pull/30106 to enable a read-only mode for the Advanced Settings screen:
    
    - Input fields are disabled
    - Save options are not displayed
    - "Reset to default" options are not displayed

--- x-pack/test/ui_capabilities/security_only/tests/advanced_settings.ts
+++ x-pack/test/ui_capabilities/security_only/tests/advanced_settings.ts
@@ -13,10 +13,12 @@ import {
 import { UserScenarios } from '../scenarios';
 
 // tslint:disable:no-default-export
-export default function navLinksTests({ getService }: KibanaFunctionalTestDefaultProviders) {
+export default function advancedSettingsTests({
+  getService,
+}: KibanaFunctionalTestDefaultProviders) {
   const uiCapabilitiesService: UICapabilitiesService = getService('uiCapabilities');
 
-  describe('dashboard', () => {
+  describe('advancedSettings', () => {
     UserScenarios.forEach(scenario => {
       it(`${scenario.fullName}`, async () => {
         const uiCapabilities = await uiCapabilitiesService.get({
@@ -24,37 +26,33 @@ export default function navLinksTests({ getService }: KibanaFunctionalTestDefaul
           password: scenario.password,
         });
         switch (scenario.username) {
-          // these users have a read/write view of Dashboard
+          // these users have a read/write view of Advanced Settings
           case 'superuser':
           case 'all':
           case 'dual_privileges_all':
-          case 'dashboard_all':
-            expect(uiCapabilities.success).to.be(true);
-            expect(uiCapabilities.value).to.have.property('dashboard');
-            expect(uiCapabilities.value!.dashboard).to.eql({
-              createNew: true,
-              show: true,
-              showWriteControls: true,
-            });
-            break;
-          // these users have a read-only view of Dashboard
-          case 'dual_privileges_read':
-          case 'dashboard_read':
-            expect(uiCapabilities.success).to.be(true);
-            expect(uiCapabilities.value).to.have.property('dashboard');
-            expect(uiCapabilities.value!.dashboard).to.eql({
-              createNew: false,
-              show: true,
-              showWriteControls: false,
-            });
-            break;
-          // these users can't do anything with Dashboard
           case 'advancedSettings_all':
+            expect(uiCapabilities.success).to.be(true);
+            expect(uiCapabilities.value).to.have.property('advancedSettings');
+            expect(uiCapabilities.value!.advancedSettings).to.eql({
+              save: true,
+            });
+            break;
+          // these users have a read-only view of Advanced Settings
+          case 'dual_privileges_read':
           case 'advancedSettings_read':
+            expect(uiCapabilities.success).to.be(true);
+            expect(uiCapabilities.value).to.have.property('advancedSettings');
+            expect(uiCapabilities.value!.advancedSettings).to.eql({
+              save: false,
+            });
+            break;
+          // these users can't do anything with Advanced Settings
           case 'apm_all':
           case 'canvas_all':
           case 'canvas_read':
           case 'dev_tools_read':
+          case 'dashboard_all':
+          case 'dashboard_read':
           case 'discover_all':
           case 'discover_read':
           case 'graph_all':
@@ -71,11 +69,9 @@ export default function navLinksTests({ getService }: KibanaFunctionalTestDefaul
           case 'visualize_all':
           case 'visualize_read':
             expect(uiCapabilities.success).to.be(true);
-            expect(uiCapabilities.value).to.have.property('dashboard');
-            expect(uiCapabilities.value!.dashboard).to.eql({
-              createNew: false,
-              show: false,
-              showWriteControls: false,
+            expect(uiCapabilities.value).to.have.property('advancedSettings');
+            expect(uiCapabilities.value!.advancedSettings).to.eql({
+              save: false,
             });
             break;
           case 'legacy_all':
