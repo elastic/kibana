c34f412e2a76b2d9b70b58d5b17860bf0cba018e
Mike Cote <mikecote@users.noreply.github.com>
2019-02-07 16:25:11 -0500

    Log instead of throw

--- src/server/saved_objects/service/lib/repository.js
+++ src/server/saved_objects/service/lib/repository.js
@@ -46,7 +46,7 @@ function removeFromStr(str, key, val) {
 }
 
 async function throwIfIdFound({ type, savedObjectsRepository, attributes }) {
-  if (['config', 'url'].includes(type)) return;
+  // if (['config', 'url'].includes(type)) return;
   const allTypes = Object.keys(getRootPropertiesObjects(savedObjectsRepository._mappings));
   const { saved_objects: allObjects } = await savedObjectsRepository.find({ type: allTypes, perPage: 10000 });
   const objectIds = allObjects.map((obj) => {
@@ -61,7 +61,8 @@ async function throwIfIdFound({ type, savedObjectsRepository, attributes }) {
     searchStr = removeFromStr(searchStr, 'title', id);
     searchStr = removeFromStr(searchStr, 'version', id);
     if (searchStr.indexOf(id) !== -1) {
-      throw new Error(`Id ${id} found in type "${type}" for ${searchStr}`);
+      // throw new Error(`Id ${id} found in type "${type}" for ${searchStr}`);
+      console.log(`\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nId "${id}" found in type "${type}" for ${searchStr}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`);
     }
   });
 }
