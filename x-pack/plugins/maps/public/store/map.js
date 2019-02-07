c32a89d593833fbca31b897380863b26747295fe
Thomas Neirynck <thomas@elastic.co>
2019-02-07 17:22:44 -0500

    [Maps] Fix tileload error messaging / Fix heatmap error message (#30327)

--- x-pack/plugins/maps/public/store/map.js
+++ x-pack/plugins/maps/public/store/map.js
@@ -95,8 +95,6 @@ const INITIAL_STATE = {
 };
 
 export function map(state = INITIAL_STATE, action) {
-  window._state = state;
-  //todo throw actions with actual objects so this doesn't get so cluttered
   switch (action.type) {
     case SET_MOUSE_COORDINATES:
       return {
@@ -251,19 +249,19 @@ export function map(state = INITIAL_STATE, action) {
       const styleLayerId = action.layerId;
       const styleLayerIdx = getLayerIndex(state.layerList, styleLayerId);
       const layerStyle = state.layerList[styleLayerIdx].style;
-      const layerPrevStyle = layerStyle.previousStyle || layerStyle;
+      const layerPrevStyle = layerStyle.__previousStyle || layerStyle;
       return updateLayerInList(state, styleLayerId, 'style',
-        { ...action.style, previousStyle: { ...layerPrevStyle } });
+        { ...action.style, __previousStyle: { ...layerPrevStyle } });
     case PROMOTE_TEMPORARY_STYLES:
       const stylePromoteIdx = getLayerIndex(state.layerList, state.selectedLayerId);
       const styleToSet = {
         ...state.layerList[stylePromoteIdx].style,
-        previousStyle: null
+        __previousStyle: null
       };
       return updateLayerInList(state, state.selectedLayerId, 'style', styleToSet);
     case CLEAR_TEMPORARY_STYLES:
       const styleClearIdx = getLayerIndex(state.layerList, state.selectedLayerId);
-      const prevStyleToLoad = state.layerList[styleClearIdx].style.previousStyle || state.layerList[styleClearIdx].style || {};
+      const prevStyleToLoad = state.layerList[styleClearIdx].style.__previousStyle || state.layerList[styleClearIdx].style || {};
       return updateLayerInList(state, state.selectedLayerId, 'style', prevStyleToLoad);
     default:
       return state;
@@ -274,18 +272,18 @@ function setErrorStatus(state, { layerId, errorMessage }) {
   const tmsErrorLayer = state.layerList.find(({ id }) => id === layerId);
   return tmsErrorLayer
     ? updateLayerInList(
-      updateLayerInList(state, tmsErrorLayer.id, 'isInErrorState', true),
-      tmsErrorLayer.id, 'errorMessage', errorMessage)
+      updateLayerInList(state, tmsErrorLayer.id, '__isInErrorState', true),
+      tmsErrorLayer.id, '__errorMessage', errorMessage)
     : state;
 }
 
 function findDataRequest(layerDescriptor, dataRequestAction) {
 
-  if (!layerDescriptor.dataRequests) {
+  if (!layerDescriptor.__dataRequests) {
     return;
   }
 
-  return layerDescriptor.dataRequests.find(dataRequest => {
+  return layerDescriptor.__dataRequests.find(dataRequest => {
     return dataRequest.dataId === dataRequestAction.dataId;
   });
 }
@@ -299,9 +297,9 @@ function updateWithDataRequest(state, action) {
     dataRequest = {
       dataId: action.dataId
     };
-    layerRequestingData.dataRequests = [
-      ...(layerRequestingData.dataRequests
-        ? layerRequestingData.dataRequests : []), dataRequest ];
+    layerRequestingData.__dataRequests = [
+      ...(layerRequestingData.__dataRequests
+        ? layerRequestingData.__dataRequests : []), dataRequest ];
   }
   dataRequest.dataMetaAtStart = action.meta;
   dataRequest.dataRequestToken = action.requestToken;
