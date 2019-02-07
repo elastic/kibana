31959931768f98e74aca2334b8448d66b56978c7
Walter Rafelsberger <walter@elastic.co>
2019-02-07 10:20:12 +0100

    Cleanup phantomjs.

--- phantomjs-image-renderer/phantom-server.js
+++ phantomjs-image-renderer/phantom-server.js
@@ -1,14 +1,10 @@
 /*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
+  Uses phantomjs for this PoC
+*/
 
-// uses https://www.npmjs.com/package/phantomjs-prebuilt
-// run: phantomjs phantom-server.js
-
-// https://gist.github.com/tompng/6433480
+// eslint-disable-next-line import/no-unresolved
 const server = require('webserver').create();
+// eslint-disable-next-line import/no-unresolved
 const webpage = require('webpage');
 const fs = require('fs');
 
@@ -16,33 +12,29 @@ const port = 8080;
 
 const CSS = fs.read('../../node_modules/@elastic/eui/dist/eui_theme_k6_light.css', 'utf8');
 
+const queue = [];
+
 server.listen(port, function (request, response) {
-  const queryIndex = request.url.indexOf("?");
-  var params = {};
+  const queryIndex = request.url.indexOf('?');
+  const params = {};
   if (queryIndex >= 0) {
-    var queries = request.url.substr(queryIndex + 1).split('&');
-    for (var i = 0; i < queries.length; i++) {
+    const queries = request.url.substr(queryIndex + 1).split('&');
+    for (let i = 0; i < queries.length; i++) {
       try {
-        var match = queries[i].match(/([^=]*)=(.*)/);
+        const match = queries[i].match(/([^=]*)=(.*)/);
         params[match[1]] = decodeURIComponent(match[2]);
-      } catch (e) { }
+      } catch (e) { /* silent fail */ }
     }
   }
-  if (true) {
-    queue.push({ response: response, html: request.post });
-    consumeCapture();
-  } else {
-    response.statusCode = 404;
-    response.write('404 not found');
-    response.close();
-  }
+
+  queue.push({ response: response, html: request.post });
+  consumeCapture();
 });
 
 console.log('running on port: ', port);
 
-var queue = [];
-var NUM_PAGES = 10;
-var pages = NUM_PAGES;
+const NUM_PAGES = 10;
+let pages = NUM_PAGES;
 
 function consumeCapture() {
   while (queue.length && pages > 0) {
@@ -56,15 +48,16 @@ function capture(data) {
 
   console.log('start', data.selector);
 
-  var page = webpage.create();
+  const page = webpage.create();
 
-  // https://stackoverflow.com/a/11771464/2266116
-  page.content = '<html><head><style>' + CSS + '</style></head><body style="width: 800px;"><div class="content" style="padding: 20px; display: inline-block">' + data.html + '</div></body></html>';
+  page.content = '<html><head><style>' + CSS + '</style></head>'
+    + '<body style="width: 800px;"><div class="content" style="padding: 20px; display: inline-block">'
+    + data.html + '</div></body></html>';
 
-  var originalRect = { left: 0, top: 0, width: 500, height: 500 };
+  const originalRect = { left: 0, top: 0, width: 500, height: 500 };
 
-  var rect = page.evaluate(function (data) {
-    var element = document.querySelector(data.selector);
+  const rect = page.evaluate(function (data) {
+    const element = document.querySelector(data.selector);
     if (element) return element.getClientRects()[0];
   }, data);
   console.log('rect', JSON.stringify(rect));
