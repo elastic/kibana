c34a72e99bdf169fd26bce8c0ab7dfbe4d9844ba
Court Ewing <court@epixa.com>
2019-02-08 17:07:40 -0500

    Skip upgrade assistant test until fixture is updated (#30587)
    The test's setup routine loads a data directory into ES that is not
    compatible with version 8.0.0, so ES fails to start and the test always
    fails.
    
    It is skipped for now to unblock CI, but this should be resolved for 8.0

--- x-pack/scripts/functional_tests.js
+++ x-pack/scripts/functional_tests.js
@@ -18,5 +18,6 @@ require('@kbn/test').runTestsCli([
   require.resolve('../test/saved_object_api_integration/security_and_spaces/config'),
   require.resolve('../test/saved_object_api_integration/security_only/config'),
   require.resolve('../test/saved_object_api_integration/spaces_only/config'),
-  require.resolve('../test/upgrade_assistant_integration/config'),
+  // todo: re-enable when https://github.com/elastic/kibana/issues/30586 is fixed
+  //require.resolve('../test/upgrade_assistant_integration/config'),
 ]);
