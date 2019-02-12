6624c19754e7afc8cd7f9813d72bc1876e273ca4
Justin Kambic <justin.kambic@elastic.co>
2019-02-12 12:13:44 -0500

    Fix typo.

--- x-pack/plugins/uptime/server/lib/helper/drop_latest_bucket.ts
+++ x-pack/plugins/uptime/server/lib/helper/drop_latest_bucket.ts
@@ -8,7 +8,7 @@
  * We've had numerous requests to not display semi-full buckets (i.e. it is 13:01 and the
  * bounds of our bucket are 13:00-13:05). If the first bucket isn't done filling, we'll
  * start out with nothing returned, otherwise we drop the most recent bucket.
- * @package bucket The bucket list
+ * @param buckets The bucket list
  */
-export const dropLatestBucket = (bucket: any[]) =>
-  bucket.length > 1 ? bucket.slice(0, bucket.length - 1) : [];
+export const dropLatestBucket = (buckets: any[]) =>
+  buckets.length > 1 ? buckets.slice(0, buckets.length - 1) : [];
