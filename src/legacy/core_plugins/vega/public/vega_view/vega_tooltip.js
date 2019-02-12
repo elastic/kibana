3ba039989002102c253ff483678360d8a5e1853f
Johannes Reuter <johannes.reuter@elastic.co>
2019-02-12 11:20:41 +0100

    fix vega tooltip position on scroll

--- src/legacy/core_plugins/vega/public/vega_view/vega_tooltip.js
+++ src/legacy/core_plugins/vega/public/vega_view/vega_tooltip.js
@@ -79,8 +79,8 @@ export class TooltipHandler {
     } else {
       const containerBox = this.container.getBoundingClientRect();
       anchorBounds = createRect(
-        containerBox.left + view._origin[0] + item.bounds.x1,
-        containerBox.top + view._origin[1] + item.bounds.y1,
+        containerBox.left + view._origin[0] + item.bounds.x1 + window.pageXOffset,
+        containerBox.top + view._origin[1] + item.bounds.y1 + window.pageYOffset,
         item.bounds.width(),
         item.bounds.height()
       );
