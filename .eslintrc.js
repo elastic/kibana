ca4b4c5c304b4269955e0dc2432ba43e9e9bd7b2
CJ Cenizal <cj@cenizal.com>
2019-02-12 13:25:39 -0800

    Reintroduce linting rule that requires single-quotes in x-pack.

--- .eslintrc.js
+++ .eslintrc.js
@@ -285,16 +285,6 @@ module.exports = {
     },
 
     /**
-     * X-Pack global overrides
-     */
-    {
-      files: ['x-pack/**/*'],
-      rules: {
-        quotes: 'off',
-      },
-    },
-
-    /**
      * Files that require Elastic license headers instead of Apache 2.0 header
      */
     {
