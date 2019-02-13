bc66231e91defa6ac3b72004be32151569f5857f
Nicholas Dziedzic <nicholas.dziedzic@elastic.co>
2019-02-13 10:35:30 -0500

    Should cover as many parts of the mixin as possible.

--- src/legacy/server/saved_objects/saved_objects_mixin.test.js
+++ src/legacy/server/saved_objects/saved_objects_mixin.test.js
@@ -23,9 +23,35 @@ import sinon from 'sinon';
 describe('Saved Objects Mixin', () => {
   let mockKbnServer;
   let mockServer;
+  const mockCallCluster = sinon.spy();
+  const stubCallCluster = sinon.stub();
+  const stubConfig = sinon.stub();
 
   beforeEach(() => {
+    mockServer = {
+      log: sinon.spy(),
+      route: sinon.spy(),
+      decorate: sinon.spy(),
+      config: () => {
+        return {
+          get: stubConfig,
+        };
+      },
+      plugins: {
+        elasticsearch: {
+          getCluster: () => {
+            return {
+              callWithRequest: mockCallCluster,
+              callWithInternalUser: stubCallCluster,
+            };
+          },
+          waitUntilReady: sinon.spy(),
+        },
+      },
+    };
     mockKbnServer = {
+      server: mockServer,
+      ready: () => {},
       pluginSpecs: { some: () => { return true; } },
       uiExports: {
         savedObjectSchemas: {
@@ -56,16 +82,6 @@ describe('Saved Objects Mixin', () => {
           },
         ],
       },
-    };
-    mockServer = {
-      log: sinon.spy(),
-      route: sinon.spy(),
-      decorate: sinon.spy(),
-      config: () => {
-        return {
-          get: sinon.spy(),
-        };
-      }
     };
   });
 
@@ -151,6 +167,74 @@ describe('Saved Objects Mixin', () => {
         expect(() => {
           service.getSavedObjectsRepository({});
         }).toThrow(new Error('Repository requires a "callCluster" function to be provided.'));
+      });
+    });
+
+    describe('get client', () => {
+      it('should return a valid client object', () => {
+        const client = service.getScopedSavedObjectsClient();
+        expect(client).toBeDefined();
+      });
+
+      it('should have a method to set the client factory', () => {
+        expect(service).toHaveProperty('setScopedSavedObjectsClientFactory');
+      });
+
+      it('should have a method to add a client wrapper factory', () => {
+        expect(service).toHaveProperty('addScopedSavedObjectsClientWrapperFactory');
+      });
+
+      it('should allow you to set a scoped saved objects client factory', () => {
+        expect(() => {
+          service.setScopedSavedObjectsClientFactory({});
+        }).not.toThrowError();
+      });
+
+      it('should allow you to add a scoped saved objects client wrapper factory', () => {
+        expect(() => {
+          service.addScopedSavedObjectsClientWrapperFactory({});
+        }).not.toThrowError();
+      });
+
+      it('should call underlining callCluster', async () => {
+        stubCallCluster.withArgs('indices.get').returns({
+          status: 404
+        });
+        stubCallCluster.withArgs('cat.templates').returns([]);
+        stubConfig.withArgs('kibana.index').returns('kibana-index');
+        const client = await service.getScopedSavedObjectsClient();
+        await client.create('testtype');
+        expect(stubCallCluster.calledWithMatch('create', sinon.match.object)).toBeTruthy();
+      });
+    });
+
+    describe('#getSavedObjectsClient', () => {
+      let getSavedObjectsClient;
+
+      beforeEach(() => {
+        savedObjectsMixin(mockKbnServer, mockServer);
+        for(let n = 0; n < mockServer.decorate.callCount; ++n) {
+          const decorateCall = mockServer.decorate.getCall(n);
+          if(decorateCall.calledWithMatch('request', 'getSavedObjectsClient', sinon.match.func)) {
+            getSavedObjectsClient = decorateCall.args[2];
+            break;
+          }
+        }
+      });
+
+      it('should be callable', () => {
+        mockServer.savedObjects = service;
+        getSavedObjectsClient = getSavedObjectsClient.bind({});
+        expect(() => {
+          getSavedObjectsClient();
+        }).not.toThrowError();
+      });
+
+      it('should use cached request object', () => {
+        mockServer.savedObjects = service;
+        getSavedObjectsClient = getSavedObjectsClient.bind({ _test: 'me' });
+        const savedObjectsClient = getSavedObjectsClient();
+        expect(getSavedObjectsClient()).toEqual(savedObjectsClient);
       });
     });
   });
