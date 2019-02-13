5c3b68c910a5edc299b9a3e3e78e8117c56d7902
Liza Katz <liza.katz@elastic.co>
2019-02-13 12:30:23 +0200

    typescript search embedable

--- src/legacy/core_plugins/kibana/public/discover/embeddable/search_embeddable_factory.ts
+++ src/legacy/core_plugins/kibana/public/discover/embeddable/search_embeddable_factory.ts
@@ -20,17 +20,23 @@
 import 'ui/doc_table';
 
 import { EmbeddableFactory } from 'ui/embeddable';
+import {
+  EmbeddableInstanceConfiguration,
+  OnEmbeddableStateChanged,
+} from 'ui/embeddable/embeddable_factory';
+import { SavedSearch, SavedSearchLoader } from '../types';
 import { SearchEmbeddable } from './search_embeddable';
 
 export class SearchEmbeddableFactory extends EmbeddableFactory {
-  constructor($compile, $rootScope, searchLoader) {
+  constructor(
+    private $compile: ng.ICompileService,
+    private $rootScope: ng.IRootScopeService,
+    private searchLoader: SavedSearchLoader
+  ) {
     super({ name: 'search' });
-    this.$compile = $compile;
-    this.searchLoader = searchLoader;
-    this.$rootScope = $rootScope;
   }
 
-  getEditPath(panelId) {
+  public getEditPath(panelId: string) {
     return this.searchLoader.urlFor(panelId);
   }
 
@@ -42,19 +48,19 @@ export class SearchEmbeddableFactory extends EmbeddableFactory {
    * @param onEmbeddableStateChanged
    * @return {Promise.<Embeddable>}
    */
-  create(panelMetadata, onEmbeddableStateChanged) {
-    const searchId = panelMetadata.id;
-    const editUrl = this.getEditPath(searchId);
+  public async create(
+    { id }: EmbeddableInstanceConfiguration,
+    onEmbeddableStateChanged: OnEmbeddableStateChanged
+  ) {
+    const editUrl = this.getEditPath(id);
 
-    return this.searchLoader.get(searchId)
-      .then(savedObject => {
-        return new SearchEmbeddable({
-          onEmbeddableStateChanged,
-          savedSearch: savedObject,
-          editUrl,
-          $rootScope: this.$rootScope,
-          $compile: this.$compile,
-        });
-      });
+    const savedObject: SavedSearch = await this.searchLoader.get(id);
+    return new SearchEmbeddable({
+      onEmbeddableStateChanged,
+      savedSearch: savedObject,
+      editUrl,
+      $rootScope: this.$rootScope,
+      $compile: this.$compile,
+    });
   }
 }
