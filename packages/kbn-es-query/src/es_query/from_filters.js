13fea2e359b87685ed0b6f01212e341f5393d7c9
Lukas Olson <olson.lukas@gmail.com>
2019-02-13 16:20:27 -0700

    Fix tests

--- packages/kbn-es-query/src/es_query/from_filters.js
+++ packages/kbn-es-query/src/es_query/from_filters.js
@@ -60,7 +60,7 @@ const cleanFilter = function (filter) {
   return _.omit(filter, ['meta', '$state']);
 };
 
-export function buildQueryFromFilters(filters = [], indexPattern, { queryStringOptions, ignoreFilterIfFieldNotInIndex }) {
+export function buildQueryFromFilters(filters = [], indexPattern, { queryStringOptions, ignoreFilterIfFieldNotInIndex } = {}) {
   _.each(filters, function (filter) {
     if (filter.query) {
       decorateQuery(filter.query, queryStringOptions);
