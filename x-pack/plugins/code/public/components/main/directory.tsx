c4f2d40eddf23e15d4e82b120d0f8ca4f19e8a7c
Davey Holler <daveyholler@gmail.com>
2019-02-12 16:47:23 -0800

    Initial changes for dark mode focusing on the file tree and directory view.

--- x-pack/plugins/code/public/components/main/directory.tsx
+++ x-pack/plugins/code/public/components/main/directory.tsx
@@ -11,6 +11,7 @@ import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import { FileTree, FileTreeItemType } from '../../../model';
 import { MainRouteParams, PathTypes } from '../../common/types';
+import { COLORS } from '@elastic/eui/src/components/icon/icon';
 
 const Root = styled.div`
   padding: ${theme.paddingSizes.m};
@@ -70,12 +71,12 @@ const DirectoryNodes = (props: DirectoryNodesProps) => {
     [FileTreeItemType.Submodule]: 'submodule',
   };
   const nodes = props.nodes.map(n => (
-    <DirectoryNode key={n.path}>
+    <div className="code-directory__node" key={n.path}>
       <Link to={props.getUrl(n.path!)} data-test-subj={`codeFileExplorerNode-${n.name}`}>
-        <EuiIcon type={typeIconMap[n.type]} color="black" />
+        <EuiIcon type={typeIconMap[n.type]} color="ghost" />
         <NodeName>{n.name}</NodeName>
       </Link>
-    </DirectoryNode>
+    </div>
   ));
   return (
     <Container>
