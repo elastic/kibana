ebfb1b7aa43060bdfc1ea4879d8eb93cf4f5f8ce
kobelb <brandon.kobel@elastic.co>
2019-02-13 07:36:00 -0800

    Using the default in the switch for devtools/visualize

--- x-pack/test/ui_capabilities/security_only/tests/dev_tools.ts
+++ x-pack/test/ui_capabilities/security_only/tests/dev_tools.ts
@@ -46,42 +46,19 @@ export default function devToolsTests({ getService }: KibanaFunctionalTestDefaul
               show: true,
             });
             break;
-          // these users can't do anything with Dev Tools
-          case 'advancedSettings_all':
-          case 'advancedSettings_read':
-          case 'apm_all':
-          case 'canvas_all':
-          case 'canvas_read':
-          case 'dashboard_all':
-          case 'dashboard_read':
-          case 'discover_all':
-          case 'discover_read':
-          case 'graph_all':
-          case 'graph_read':
-          case 'maps_all':
-          case 'maps_read':
-          case 'infrastructure_read':
-          case 'logs_read':
-          case 'ml_all':
-          case 'monitoring_all':
-          case 'timelion_all':
-          case 'timelion_read':
-          case 'uptime_read':
-          case 'visualize_all':
-          case 'visualize_read':
-            expect(uiCapabilities.success).to.be(true);
-            expect(uiCapabilities.value).to.have.property('dev_tools');
-            expect(uiCapabilities.value!.dev_tools).to.eql({
-              show: false,
-            });
-            break;
+          // these users have no access to even get the ui capabilities
           case 'legacy_all':
           case 'no_kibana_privileges':
             expect(uiCapabilities.success).to.be(false);
             expect(uiCapabilities.failureReason).to.be(GetUICapabilitiesFailureReason.NotFound);
             break;
+          // all other users can't do anything with Dashboard
           default:
-            throw new UnreachableError(scenario);
+            expect(uiCapabilities.success).to.be(true);
+            expect(uiCapabilities.value).to.have.property('dev_tools');
+            expect(uiCapabilities.value!.dev_tools).to.eql({
+              show: false,
+            });
         }
       });
     });
