9d154a663c945e89c2c7a4773a3d71b25ecd3741
Thomas Neirynck <thomas@elastic.co>
2019-02-13 13:30:00 -0500

    [Maps] Revert layer-state correctly on cancel  (#30451)

--- x-pack/plugins/maps/public/selectors/map_selectors.js
+++ x-pack/plugins/maps/public/selectors/map_selectors.js
@@ -15,6 +15,8 @@ import { HeatmapStyle } from '../shared/layers/styles/heatmap_style';
 import { TileStyle } from '../shared/layers/styles/tile_style';
 import { timefilter } from 'ui/timefilter';
 
+import { copyPersistentState, TRACKED_LAYER_DESCRIPTOR } from '../store/util';
+
 function createLayerInstance(layerDescriptor) {
   const source = createSourceInstance(layerDescriptor.sourceDescriptor);
   const style = createStyleInstance(layerDescriptor.style);
@@ -63,7 +65,7 @@ export const getMapReady = ({ map }) => map && map.ready;
 
 export const getGoto = ({ map }) => map && map.goto;
 
-const getSelectedLayerId = ({ map }) => {
+export const getSelectedLayerId = ({ map }) => {
   return (!map.selectedLayerId || !map.layerList) ? null : map.selectedLayerId;
 };
 
@@ -163,3 +165,11 @@ export const getUniqueIndexPatternIds = createSelector(
 );
 
 export const getTemporaryLayers = createSelector(getLayerList, (layerList) => layerList.filter(layer => layer.isTemporary()));
+
+export const hasDirtyState = createSelector(getLayerListRaw, (layerListRaw) => {
+  return layerListRaw.some(layerDescriptor => {
+    const currentState = copyPersistentState(layerDescriptor);
+    const trackedState = layerDescriptor[TRACKED_LAYER_DESCRIPTOR];
+    return (trackedState) ? !_.isEqual(currentState, trackedState) : false;
+  });
+});
