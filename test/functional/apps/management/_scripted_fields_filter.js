2c0d09828fc568ecf5fd87c610273e4b68376aa8
Spencer <email@spalger.com>
2019-02-13 22:46:45 -0800

    [ftr/defaultTimezone] make UTC the default timezone (#30460)

--- test/functional/apps/management/_scripted_fields_filter.js
+++ test/functional/apps/management/_scripted_fields_filter.js
@@ -34,14 +34,13 @@ export default function ({ getService, getPageObjects }) {
       await browser.setWindowSize(1200, 800);
       await esArchiver.load('management');
       await kibanaServer.uiSettings.replace({
-        'dateFormat:tz': 'UTC',
         'defaultIndex': 'f1e4c910-a2e6-11e7-bb30-233be9be6a15'
       });
     });
 
     after(async function () {
       await esArchiver.unload('management');
-      await kibanaServer.uiSettings.replace({ 'dateFormat:tz': 'UTC' });
+      await kibanaServer.uiSettings.replace({});
     });
 
     const scriptedPainlessFieldName = 'ram_pain1';
