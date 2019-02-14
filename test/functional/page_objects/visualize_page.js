419e35eea7632d7ee6392069c30414ec508b6269
Liza Katz <liza.katz@elastic.co>
2019-02-14 18:47:27 +0200

    added tests for visualize app

--- test/functional/page_objects/visualize_page.js
+++ test/functional/page_objects/visualize_page.js
@@ -43,11 +43,82 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
       };
     }
 
+    async gotoVisualizationLandingPage() {
+      log.debug('gotoVisualizationLandingPage');
+      await PageObjects.common.navigateToApp('visualize');
+    }
+
+    async checkListingSelectAllCheckbox() {
+      const element = await testSubjects.find('checkboxSelectAll');
+      const isSelected = await element.isSelected();
+      if (!isSelected) {
+        log.debug(`checking checkbox "checkboxSelectAll"`);
+        await testSubjects.click('checkboxSelectAll');
+      }
+    }
+
     async navigateToNewVisualization() {
       log.debug('navigateToApp visualize');
       await PageObjects.common.navigateToApp('visualize');
-      await testSubjects.click('newItemButton');
+      await this.clickNewVisualization();
       await this.waitForVisualizationSelectPage();
+    }
+
+    async clickNewVisualization() {
+      // newItemButton button is only visible when there are items in the listing table is displayed.
+      let exists = await testSubjects.exists('newItemButton');
+      if (exists) {
+        return await testSubjects.click('newItemButton');
+      }
+
+      exists = await testSubjects.exists('createVisualizationPromptButton');
+      // no viz exist, click createVisualizationPromptButton to create new dashboard
+      return await this.createVisualizationPromptButton();
+    }
+
+    async createVisualizationPromptButton() {
+      await testSubjects.click('createVisualizationPromptButton');
+    }
+
+
+    async getSearchFilter() {
+      const searchFilter = await find.allByCssSelector('.euiFieldSearch');
+      return searchFilter[0];
+    }
+
+    async clearFilter() {
+      const searchFilter = await this.getSearchFilter();
+      await searchFilter.clearValue();
+      await searchFilter.click();
+    }
+
+    async searchForItemWithName(name) {
+      log.debug(`searchForItemWithName: ${name}`);
+
+      await retry.try(async () => {
+        const searchFilter = await this.getSearchFilter();
+        await searchFilter.clearValue();
+        await searchFilter.click();
+        // Note: this replacement of - to space is to preserve original logic but I'm not sure why or if it's needed.
+        await searchFilter.type(name.replace('-', ' '));
+        await PageObjects.common.pressEnterKey();
+      });
+
+      await PageObjects.header.waitUntilLoadingHasFinished();
+    }
+
+    async clickDeleteSelected() {
+      await testSubjects.click('deleteSelectedItems');
+    }
+
+    async getCreatePromptExists() {
+      log.debug('getCreatePromptExists');
+      return await testSubjects.exists('createVisualizationPromptButton');
+    }
+
+    async getCountOfItemsInListingTable() {
+      const elements = await find.allByCssSelector('[data-test-subj^="visListingTitleLink"]');
+      return elements.length;
     }
 
     async waitForVisualizationSelectPage() {
@@ -312,7 +383,7 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
     }
 
     async clickSavedSearch(savedSearchName) {
-      await find.clickByPartialLinkText(savedSearchName);
+      await find.clickByButtonText(savedSearchName);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
@@ -713,7 +784,7 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
 
     async clickVisualizationByName(vizName) {
       log.debug('clickVisualizationByLinkText(' + vizName + ')');
-      return find.clickByPartialLinkText(vizName);
+      return find.clickByButtonText(vizName);
     }
 
     async loadSavedVisualization(vizName, { navigateToVisualize = true } = {}) {
