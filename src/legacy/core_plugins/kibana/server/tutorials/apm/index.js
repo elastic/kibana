7a98cfe36b04ed07890c5eaf150cdf8005d1aaf6
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-02-14 08:46:33 -0500

    Updates load object text now that we do not load visualizations (#30829) (#30941)

--- src/legacy/core_plugins/kibana/server/tutorials/apm/index.js
+++ src/legacy/core_plugins/kibana/server/tutorials/apm/index.js
@@ -91,9 +91,7 @@ It allows you to monitor the performance of thousands of applications in real ti
     savedObjectsInstallMsg: i18n.translate(
       'kbn.server.tutorials.apm.specProvider.savedObjectsInstallMsg',
       {
-        defaultMessage:
-          'Load index pattern, visualizations, and pre-defined dashboards. \
-An index pattern is required for some features in the APM UI.',
+        defaultMessage: 'An APM index pattern is required for some features in the APM UI.',
       }
     ),
   };
