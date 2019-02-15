b4e40946c088948e09192898fac5dd702e7522cb
spalger <email@spalger.com>
2019-02-15 14:07:08 -0800

    Revert "[ftr/browser] explicitly export specific keys, keep `BACKSPACE` name"
    This reverts commit 779335f2d44469f4c33c9abe19a47f9550254173.

--- test/functional/services/browser.js
+++ test/functional/services/browser.js
@@ -28,16 +28,7 @@ export async function BrowserProvider({ getService }) {
     /**
      * Keyboard events
      */
-    keys = {
-      ENTER: Key.ENTER,
-      COMMAND: Key.COMMAND,
-      CONTROL: Key.CONTROL,
-      NULL: Key.NULL,
-      RETURN: Key.RETURN,
-      SPACE: Key.SPACE,
-      ARROW_RIGHT: Key.ARROW_RIGHT,
-      BACKSPACE: Key.BACK_SPACE
-    };
+    keys = Key;
 
     /**
      * Retrieves the a rect describing the current top-level window's size and position.
