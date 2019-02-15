57b9e4d949440f4e2ecd1f1e8d7737f252c28dc4
monfera <monfera.robert@gmail.com>
2019-02-15 10:13:20 +0100

    Refactor: spike to get rid of `select` 1

--- x-pack/plugins/canvas/public/lib/aeroelastic/select.ts
+++ x-pack/plugins/canvas/public/lib/aeroelastic/select.ts
@@ -6,27 +6,20 @@
 
 import { ActionId, NodeFunction, NodeResult } from './types';
 
-const shallowArrayEqual = (a: any, b: any): boolean =>
-  a.length === b.length && a.every((v: any, i: number) => v === b[i]);
-
 export const select = (fun: NodeFunction): NodeFunction => (
   ...inputs: NodeFunction[]
 ): NodeResult => {
   // last-value memoizing version of this single line function:
   // fun => (...inputs) => state => fun(...inputs.map(input => input(state)))
-  let argumentValues = [] as NodeResult[];
   let value: NodeResult;
   let actionId: ActionId;
   return (state: NodeResult) => {
     const lastActionId: ActionId = state.primaryUpdate.payload.uid;
-    if (
-      actionId === lastActionId ||
-      shallowArrayEqual(argumentValues, (argumentValues = inputs.map(input => input(state))))
-    ) {
+    if (actionId === lastActionId) {
       return value;
     }
 
-    value = fun(...argumentValues);
+    value = fun(...inputs.map(input => input(state)));
     actionId = lastActionId;
     return value;
   };
