399cdb96c5983b70ab800d73564b6aece4b7e890
joe fleming <joe.fleming@gmail.com>
2019-02-15 10:31:57 -0700

    fix: handle empty response from functions

--- packages/kbn-interpreter/src/public/batched_fetch.js
+++ packages/kbn-interpreter/src/public/batched_fetch.js
@@ -97,7 +97,7 @@ async function processBatch(kfetch, batch) {
 
     results.forEach(({ id, result }) => {
       const { future } = batch[id];
-      if (result.statusCode && result.err) {
+      if (result && result.statusCode && result.err) {
         future.reject(result);
       } else {
         future.resolve(result);
