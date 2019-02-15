5b6d8032bec56a24cad52c9fb5e76feb48dcd119
maryia-lapata <mary.lopato@gmail.com>
2019-02-15 10:28:40 +0300

    Make test subj consistent with initial data test subj

--- test/functional/page_objects/visualize_page.js
+++ test/functional/page_objects/visualize_page.js
@@ -307,13 +307,13 @@ export function VisualizePageProvider({ getService, getPageObjects }) {
     }
 
     async clickNewSearch(indexPattern = this.index.LOGSTASH_TIME_BASED) {
-      await testSubjects.click(`savedObjectTitle${indexPattern}`);
+      await testSubjects.click(`savedObjectTitle${indexPattern.split(' ').join('-')}`);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
     async clickSavedSearch(savedSearchName) {
       await testSubjects.click('savedSearchesTab');
-      await testSubjects.click(`savedObjectTitle${savedSearchName}`);
+      await testSubjects.click(`savedObjectTitle${savedSearchName.split(' ').join('-')}`);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
