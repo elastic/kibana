2f18ee402aa1ccd17db420daa3b0983984f6eb92
Stamatis Katsaounis <katsaouniss@gmail.com>
2019-02-16 10:39:48 +0200

    resolve conflict

--- src/legacy/core_plugins/kibana/server/tutorials/register.js
+++ src/legacy/core_plugins/kibana/server/tutorials/register.js
@@ -67,6 +67,7 @@ import { awsMetricsSpecProvider } from './aws_metrics';
 import { mssqlMetricsSpecProvider } from './mssql_metrics';
 import { zeekLogsSpecProvider } from './zeek_logs';
 import { natsMetricsSpecProvider } from './nats_metrics';
+import { natsLogsSpecProvider } from './nats_logs';
 
 export function registerTutorials(server) {
   server.registerTutorial(systemLogsSpecProvider);
@@ -119,4 +120,5 @@ export function registerTutorials(server) {
   server.registerTutorial(mssqlMetricsSpecProvider);
   server.registerTutorial(zeekLogsSpecProvider);
   server.registerTutorial(natsMetricsSpecProvider);
+  server.registerTutorial(natsLogsSpecProvider);
 }
