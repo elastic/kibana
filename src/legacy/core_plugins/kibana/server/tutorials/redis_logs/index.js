f64d49e555f58348a29a99a79441867b9e8ff5b9
kaiyan-sheng <kaiyan.sheng@elastic.co>
2019-02-17 17:10:14 -0700

    Cleanup geoipRequired and uaRequired (#31173)
    * Cleanup geoipRequired and uaRequired
    
    * Remove geoip and user-agent plugins in chinese translations

--- src/legacy/core_plugins/kibana/server/tutorials/redis_logs/index.js
+++ src/legacy/core_plugins/kibana/server/tutorials/redis_logs/index.js
@@ -23,8 +23,6 @@ import { onPremInstructions, cloudInstructions, onPremCloudInstructions } from '
 
 export function redisLogsSpecProvider(server, context) {
   const moduleName = 'redis';
-  const geoipRequired = false;
-  const uaRequired = false;
   const platforms = ['OSX', 'DEB', 'RPM', 'WINDOWS'];
   return {
     id: 'redisLogs',
@@ -65,7 +63,7 @@ Note that the `slowlog` fileset is experimental. \
     },
     completionTimeMinutes: 10,
     previewImagePath: '/plugins/kibana/home/tutorial_resources/redis_logs/screenshot.png',
-    onPrem: onPremInstructions(moduleName, platforms, geoipRequired, uaRequired, context),
+    onPrem: onPremInstructions(moduleName, platforms, context),
     elasticCloud: cloudInstructions(moduleName, platforms),
     onPremElasticCloud: onPremCloudInstructions(moduleName, platforms)
   };
