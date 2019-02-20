dc654e4aa3d407bef5de3bebe356c52cbf86405f
Liza Katz <lizka.k@gmail.com>
2019-02-20 16:50:36 +0200

    moved vis table to use EUI (#30610)
    * Create base class for table list view, using EuiInMemoryTable.
    * Reimplemented dashboards and visualization listing views using table list view component.
    * Added tests for visualizations page
    * Updated experimental visualization badge and tooltip

--- src/legacy/core_plugins/kibana/public/table_list_view/index.js
+++ src/legacy/core_plugins/kibana/public/table_list_view/index.js
@@ -17,14 +17,5 @@
  * under the License.
  */
 
-export const VisualizeConstants = {
-  LANDING_PAGE_PATH: '/visualize',
-  WIZARD_STEP_1_PAGE_PATH: '/visualize/new',
-  WIZARD_STEP_2_PAGE_PATH: '/visualize/new/configure',
-  CREATE_PATH: '/visualize/create',
-  EDIT_PATH: '/visualize/edit',
-};
+export { TableListView } from './table_list_view';
 
-export function createVisualizeEditUrl(id: string) {
-  return `${VisualizeConstants.EDIT_PATH}/${id}`;
-}
