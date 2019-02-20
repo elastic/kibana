1d4a8064f395c26ab146b6296c48dbd143067261
Aaron Caldwell <aaronjcaldwell@gmail.com>
2019-02-19 18:09:17 -0700

    [Maps] Update layer add workflow and revise temp layer handling (#30542)
    * Move layer add flow to the add layer panel
    
    * Remove layer add toast. It's pretty annoying in this new workflow and is planned to be removed/revised anyway
    
    * Add/remove layers without setting temp status
    
    * Dispatch save function on ws save and clear/remove transient layer
    
    * Lot of cleanup of temp refs. Add __ to transient layer ref
    
    * Activate layer property save button on style prop change
    
    * Review feedback
    
    * Await setting selected layer before opening flyout panel to prevent occasional undefined error
    
    * Review feedback
    
    * Review feedback
    
    * Review feedback
    
    * Remove style temp settings
    
    * Return null as fallback
    
    * Review feedback
    
    # Conflicts:
    #	x-pack/plugins/maps/public/angular/get_initial_layers.test.js
    #	x-pack/plugins/maps/public/angular/map_controller.js
    #	x-pack/plugins/maps/public/components/layer_addpanel/index.js
    #	x-pack/plugins/maps/public/components/layer_addpanel/view.js

--- x-pack/plugins/maps/public/components/layer_addpanel/view.js
+++ x-pack/plugins/maps/public/components/layer_addpanel/view.js
@@ -23,26 +23,25 @@ import {
 
 export class AddLayerPanel extends Component {
 
-  constructor() {
-    super();
-
-    this.state = {
-      sourceType: null,
-    };
+  state = {
+    sourceType: null,
+    isLoading: false,
+    hasLayerSelected: false,
+    layer: null
   }
 
   _previewLayer = (source) => {
-    this.layer = source.createDefaultLayer({
-      temporary: true,
-    });
-    this.props.previewLayer(this.layer);
+    this.setState({
+      layer: source.createDefaultLayer({})
+    },
+    () => this.props.previewLayer(this.state.layer));
   };
 
   _clearSource = () => {
     this.setState({ sourceType: null });
 
-    if (this.layer) {
-      this.props.removeLayer(this.layer.getId());
+    if (this.state.layer) {
+      this.props.removeTransientLayer();
     }
   }
 
@@ -55,17 +54,16 @@ export class AddLayerPanel extends Component {
       return null;
     }
 
-    const { layerLoading, temporaryLayers, nextAction } = this.props;
+    const {  hasLayerSelected, isLoading, selectLayerAndAdd } = this.props;
     return (
       <EuiButton
-        disabled={!temporaryLayers || layerLoading}
-        isLoading={layerLoading}
+        disabled={!hasLayerSelected}
+        isLoading={hasLayerSelected && isLoading}
         iconSide="right"
         iconType={'sortRight'}
         onClick={() => {
-          const layerId = this.layer.getId();
-          this.layer = null;
-          return nextAction(layerId);
+          this.setState({ layer: null });
+          selectLayerAndAdd();
         }}
         fill
       >
@@ -164,7 +162,11 @@ export class AddLayerPanel extends Component {
           <EuiFlexGroup justifyContent="spaceBetween" responsive={false}>
             <EuiFlexItem grow={false}>
               <EuiButtonEmpty
-                onClick={this.props.closeFlyout}
+                onClick={() => {
+                  if (this.state.layer) {
+                    this.props.closeFlyout();
+                  }
+                }}
                 flush="left"
               >
                 Cancel
