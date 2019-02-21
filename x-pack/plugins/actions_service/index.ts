96a3a58eec41f3c13e73ef27a6844f29d377c234
Nicholas Dziedzic <nicholas.dziedzic@elastic.co>
2019-02-21 17:04:33 -0500

    Adds example definition and creation of action type, connector, instance, and exeuction.

--- x-pack/plugins/actions_service/index.ts
+++ x-pack/plugins/actions_service/index.ts
@@ -4,7 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { find } from 'lodash';
+import fetch from 'node-fetch';
 import { resolve } from 'path';
+import url from 'url';
 import mappings from './mappings.json';
 // import './types.d';
 
@@ -35,43 +38,154 @@ export const actionsService = (kibana: any) => {
     },
 
     init(server: any) {
-      const registrar: ActionHandler[] = [];
-      const instances: ActionInstance[] = [];
+      const registrar: Map<string, ActionType> = new Map();
+      const connectors: Map<string, ActionConnector> = new Map();
       const info = (message: string) => server.log(['actions_service', 'info'], message);
+      const warn = (message: string) => server.log(['actions_service', 'warning'], message);
       const { callWithInternalUser } = server.plugins.elasticsearch.getCluster('admin');
       const so = server.savedObjects.getSavedObjectsRepository(callWithInternalUser);
 
-      const loadActionConfigurations = async () => {
-        const results = await so.find({ type: 'actionConfiguration' });
-        info('Search results have been returned');
-        results.saved_objects.forEach((config: any) => {
-          info(`We found a action configuration ${config.id}`);
-        });
+      const registerActionType = (actionDef: ActionType) => {
+        if (!registrar.has(actionDef.name)) {
+          registrar.set(actionDef.name, actionDef);
+        }
       };
 
-      const register = (actionDef: ActionHandler) => {
-        registrar.push(actionDef);
+      const registerConnector = (connectorDef: ActionConnector) => {
+        const connectorName = `${connectorDef.actionType}:${connectorDef.connectorType}`;
+        if (!connectors.has(connectorName)) {
+          info(`Registering connector ${connectorName}`);
+          connectors.set(connectorName, connectorDef);
+        }
       };
 
       const instance = async (instDef: ActionInstance) => {
-        const saved = await so.create('actionConfiguration', instDef);
+        const { name, actionType, connectorType, params, connectorParams } = instDef;
+        const saved = await so.create(
+          'actionConfiguration',
+          {
+            name,
+            actionType,
+            connectorType,
+            instanceParams: JSON.stringify({ params, connectorParams }),
+          },
+          {
+            id: `${actionType}:${name}`,
+            overwrite: true,
+          }
+        );
         if (!saved) {
-          info(`Trouble saving the Action instance ${instDef}`);
+          warn(`Trouble saving the Action instance ${instDef}`);
         }
-        instances.push(instDef);
+      };
+
+      const fire = async (execution: ActionExecution) => {
+        const { action, actionType, params } = execution;
+
+        try {
+          const response: any = await so.get('actionConfiguration', `${actionType}:${action}`);
+
+          if (response === undefined) {
+            warn(
+              `Could not find any action instances to execute with name ${actionType}:${action}`
+            );
+            return;
+          }
+
+          const { connectorType, instanceParams } = response.attributes;
+          const connector = connectors.get(`${actionType}:${connectorType}`);
+
+          if (connector) {
+            try {
+              info(`Trying to execute connector ${connectorType}`);
+              const instanceParamObj = JSON.parse(instanceParams);
+              await connector.handler({
+                params: {
+                  ...params,
+                  ...instanceParamObj.params,
+                },
+                connectorParams: instanceParamObj.connectorParams,
+              });
+            } catch (e) {
+              warn(`[${connectorType}] failed with message ${e.message}`);
+            }
+          } else {
+            info(
+              `Could not execute action ${action} due to missing connector ${connectorType} in ${JSON.stringify(
+                response
+              )}`
+            );
+          }
+        } catch (e) {
+          info(`Bad Request to saved objects sevice ${e.message}`);
+          return;
+        }
       };
 
       server.expose('actions', {
-        register,
+        registrar,
         instance,
       });
 
-      instance({
+      registerActionType({
+        name: 'send message',
         initParams: [{ name: 'username', type: 'string' }, { name: 'password', type: 'secret' }],
+        executionParams: [
+          { name: 'destination', type: 'string' },
+          { name: 'message', type: 'string' },
+          { name: 'title', type: 'string' },
+        ],
       });
 
-      loadActionConfigurations().then(() => {
-        info('Action configurations loaded');
+      registerConnector({
+        actionType: 'send message',
+        connectorType: 'slack',
+        async handler({ params, connectorParams }) {
+          try {
+            const body = JSON.stringify({
+              ...connectorParams,
+              username: 'webhookbot',
+              text: params.message,
+              icon_emoji: ':ghost:',
+            });
+            const result = new url.URL(params.destination);
+            await fetch(result.toString(), {
+              method: 'POST',
+              body,
+            });
+          } catch (e) {
+            warn(`[slack] failed with ${e.message}`);
+          }
+        },
+      });
+
+      instance({
+        name: 'send message to slack',
+        actionType: 'send message',
+        connectorType: 'slack',
+        params: {
+          destination: '<example slack url>',
+        },
+        connectorParams: {
+          channel: '#bot-playground',
+        },
+      });
+
+      fire({
+        action: 'send message to slack',
+        actionType: 'send message',
+        params: {
+          message: 'This is a test message from kibana actions service',
+          title: 'custom title',
+        },
+      });
+
+      server.route({
+        path: '/api/actions/connectors',
+        method: 'GET',
+        handler() {
+          return [...connectors.values()].map(connector => JSON.stringify(connector));
+        },
       });
 
       this.kbnServer.afterPluginsInit(() => {
--- x-pack/plugins/actions_service/public/components/main/item.js
+++ x-pack/plugins/actions_service/public/components/main/item.js
@@ -9,7 +9,7 @@ import React from 'react';
 export const Item = (props) => {
   return (
     <li>
-      {JSON.stringify(props)}
+      {props.attributes.connector}
     </li>
   );
 };
