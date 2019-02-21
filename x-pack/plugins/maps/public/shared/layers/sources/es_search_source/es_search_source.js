013ac858148f42f0dd9e9211af8ced0dba64f152
Thomas Neirynck <thomas@elastic.co>
2019-02-21 16:52:13 -0500

    [Maps] display metric tooltips for grid source the same as for joins (#31621)

--- x-pack/plugins/maps/public/shared/layers/sources/es_search_source/es_search_source.js
+++ x-pack/plugins/maps/public/shared/layers/sources/es_search_source/es_search_source.js
@@ -66,6 +66,10 @@ export class ESSearchSource extends AbstractESSource {
     }
   }
 
+  getMetricFields() {
+    return [];
+  }
+
   getFieldNames() {
     return [
       this._descriptor.geoField,
