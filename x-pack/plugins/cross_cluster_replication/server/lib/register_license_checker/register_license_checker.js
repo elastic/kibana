3c6f28d8a77aaa970e4dcff8b25666dfd9db2dc5
CJ Cenizal <cj@cenizal.com>
2019-02-22 19:54:14 -0800

    Fix spacing error in license header in CCR, Remote Clusters, and Rollups.

--- x-pack/plugins/cross_cluster_replication/server/lib/register_license_checker/register_license_checker.js
+++ x-pack/plugins/cross_cluster_replication/server/lib/register_license_checker/register_license_checker.js
@@ -5,10 +5,10 @@
  */
 
 /*
-* Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-* or more contributor license agreements. Licensed under the Elastic License;
-* you may not use this file except in compliance with the Elastic License.
-*/
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
 
 import { mirrorPluginStatus } from '../../../../../server/lib/mirror_plugin_status';
 import { PLUGIN } from '../../../common/constants';
