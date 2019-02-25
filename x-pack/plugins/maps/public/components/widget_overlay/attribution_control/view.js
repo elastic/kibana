e6d473247db2c045d54fb37912dff9802a1ddd6f
Nathan Reese <reese.nathan@gmail.com>
2019-02-25 06:09:30 -0700

    [Maps] render attribution with no url as text instead of EuiLink (#31873)
    * [Maps] render attribution with no url as text instead of EuiLink
    
    * remove check for length

--- x-pack/plugins/maps/public/components/widget_overlay/attribution_control/view.js
+++ x-pack/plugins/maps/public/components/widget_overlay/attribution_control/view.js
@@ -24,7 +24,7 @@ export class AttributionControl  extends React.Component {
 
   componentDidMount() {
     this._isMounted = true;
-    this._syncMbMapWithAttribution();
+    this._loadAttributions();
   }
 
   componentWillUnmount() {
@@ -32,11 +32,10 @@ export class AttributionControl  extends React.Component {
   }
 
   componentDidUpdate() {
-    this._syncMbMapWithAttribution();
+    this._loadAttributions();
   }
 
-  _syncMbMapWithAttribution = async () => {
-
+  _loadAttributions = async () => {
     const attributionPromises = this.props.layerList.map(async (layer) => {
       try {
         return await layer.getAttributions();
@@ -66,11 +65,21 @@ export class AttributionControl  extends React.Component {
     }
   };
 
+  _renderAttribution({ url, label }) {
+    if (!url) {
+      return label;
+    }
+
+    return (
+      <EuiLink color="subdued" href={url} target="_blank">{label}</EuiLink>
+    );
+  }
+
   _renderAttributions() {
     return this.state.uniqueAttributions.map((attribution, index) => {
       return (
         <Fragment key={index}>
-          <EuiLink color="subdued" href={attribution.url} target="_blank">{attribution.label}</EuiLink>
+          {this._renderAttribution(attribution)}
           {index < (this.state.uniqueAttributions.length - 1) && ', '}
         </Fragment>
       );
