9100feb79d4352c500f483a78c618abe0fc17a23
Lukas Olson <olson.lukas@gmail.com>
2019-02-26 10:33:22 -0700

    Move getIndexPattern service to visualize

--- src/legacy/core_plugins/kibana/public/visualize/embeddable/visualize_embeddable_factory.ts
+++ src/legacy/core_plugins/kibana/public/visualize/embeddable/visualize_embeddable_factory.ts
@@ -26,9 +26,9 @@ import {
   EmbeddableInstanceConfiguration,
   OnEmbeddableStateChanged,
 } from 'ui/embeddable/embeddable_factory';
-import { getIndexPattern } from 'ui/embeddable/get_index_pattern';
 import { SavedVisualizations } from '../types';
 import { DisabledLabEmbeddable } from './disabled_lab_embeddable';
+import { getIndexPattern } from './get_index_pattern';
 
 export class VisualizeEmbeddableFactory extends EmbeddableFactory {
   private savedVisualizations: SavedVisualizations;
