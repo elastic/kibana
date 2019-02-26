31c4e4644b0a900db1bff39ff8c573e2ba31cf32
Justin Kambic <justin.kambic@elastic.co>
2019-02-26 12:40:26 -0500

    Switch uptime internationalization from i18n provider to context. (#31601)

--- x-pack/plugins/uptime/public/uptime_app.tsx
+++ x-pack/plugins/uptime/public/uptime_app.tsx
@@ -23,10 +23,11 @@ import {
 import euiDarkVars from '@elastic/eui/dist/eui_theme_dark.json';
 import euiLightVars from '@elastic/eui/dist/eui_theme_light.json';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage, I18nProvider } from '@kbn/i18n/react';
+import { FormattedMessage } from '@kbn/i18n/react';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { I18nContext } from 'ui/i18n';
 import { overviewBreadcrumb, UMBreadcrumb } from './breadcrumbs';
 import { UMGraphQLClient, UMUpdateBreadcrumbs } from './lib/lib';
 import { MonitorPage, OverviewPage } from './pages';
@@ -135,7 +136,7 @@ class Application extends React.Component<UptimeAppProps, UptimeAppState> {
   public render() {
     const { routerBasename, graphQLClient } = this.props;
     return (
-      <I18nProvider>
+      <I18nContext>
         <Router basename={routerBasename}>
           <ApolloProvider client={graphQLClient}>
             <EuiPage className="app-wrapper-panel">
@@ -249,7 +250,7 @@ class Application extends React.Component<UptimeAppProps, UptimeAppState> {
             </EuiPage>
           </ApolloProvider>
         </Router>
-      </I18nProvider>
+      </I18nContext>
     );
   }
 
