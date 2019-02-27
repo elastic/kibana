0a2a8f94f5154baa47ea16d478d5671728328c8f
Dmitry Lemeshko <dzmitry.lemechko@elastic.co>
2019-02-27 19:58:24 +0100

    Fix Dashboard Save tests (#32112)
    * [page_objects/dashboard_page] add waitDialogIsClosed to manage dashboard popup behaviour
    
    * [page_objects/dashboard_page] add waitDialogIsClosed flag

--- test/functional/page_objects/dashboard_page.js
+++ test/functional/page_objects/dashboard_page.js
@@ -307,9 +307,9 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
      * verify that the save was successful
      *
      * @param dashName {String}
-     * @param saveOptions {{storeTimeWithDashboard: boolean, saveAsNew: boolean, needsConfirm: false}}
+     * @param saveOptions {{storeTimeWithDashboard: boolean, saveAsNew: boolean, needsConfirm: false,  waitDialogIsClosed: boolean }}
      */
-    async saveDashboard(dashName, saveOptions = {}) {
+    async saveDashboard(dashName, saveOptions = { waitDialogIsClosed: true }) {
       await this.enterDashboardTitleAndClickSave(dashName, saveOptions);
 
       if (saveOptions.needsConfirm) {
@@ -352,9 +352,9 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
     /**
      *
      * @param dashboardTitle {String}
-     * @param saveOptions {{storeTimeWithDashboard: boolean, saveAsNew: boolean}}
+     * @param saveOptions {{storeTimeWithDashboard: boolean, saveAsNew: boolean, waitDialogIsClosed: boolean}}
      */
-    async enterDashboardTitleAndClickSave(dashboardTitle, saveOptions = {}) {
+    async enterDashboardTitleAndClickSave(dashboardTitle, saveOptions = { waitDialogIsClosed: true }) {
       await testSubjects.click('dashboardSaveMenuItem');
       const modalDialog = await testSubjects.find('savedObjectSaveModal');
 
@@ -370,7 +370,9 @@ export function DashboardPageProvider({ getService, getPageObjects }) {
       }
 
       await this.clickSave();
-      await testSubjects.waitForDeleted(modalDialog);
+      if (saveOptions.waitDialogIsClosed) {
+        await testSubjects.waitForDeleted(modalDialog);
+      }
     }
 
     async selectDashboard(dashName) {
