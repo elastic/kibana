5adc9d6d3c3c6201a01b4157eba05ff7028bfc39
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-02-27 15:14:44 +0100

    Reproduce Angular router issue

--- x-pack/plugins/apm/public/index.js
+++ x-pack/plugins/apm/public/index.js
@@ -4,50 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { uiModules } from 'ui/modules'; // eslint-disable-line no-unused-vars
-import chrome from 'ui/chrome';
-import React, { Fragment } from 'react';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import 'ui/autoload/styles';
-import 'ui/autoload/all';
-import 'uiExports/autocompleteProviders';
-import 'react-vis/dist/style.css';
-import './style/global_overrides.css';
-import template from './templates/index.html';
-import { Main } from './components/app/Main';
-import { initTimepicker } from './utils/timepicker';
-import configureStore from './store/config/configureStore';
-import GlobalProgress from './components/app/Main/GlobalProgress';
-import { GlobalHelpExtension } from './components/app/GlobalHelpExtension';
+import createHistory from 'history/createHashHistory';
+const history = createHistory();
 
-import { history } from './components/shared/Links/url_helpers';
-import { I18nContext } from 'ui/i18n';
-
-// render APM feedback link in global help menu
-chrome.helpExtension.set(domElement => {
-  ReactDOM.render(<GlobalHelpExtension />, domElement);
-  return () => {
-    ReactDOM.unmountComponentAtNode(domElement);
-  };
-});
-
-chrome.setRootTemplate(template);
-const store = configureStore();
-
-initTimepicker(history, store.dispatch).then(() => {
-  ReactDOM.render(
-    <I18nContext>
-      <Provider store={store}>
-        <Fragment>
-          <GlobalProgress />
-          <Router history={history}>
-            <Main />
-          </Router>
-        </Fragment>
-      </Provider>
-    </I18nContext>,
-    document.getElementById('react-apm-root')
-  );
+history.listen((location, action) => {
+  console.log({ location, action }); // eslint-disable-line no-console
 });
