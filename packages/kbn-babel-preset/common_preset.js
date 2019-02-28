8e8a4f6a39c8277bcd8d3952a7d11454d2340ea7
Tiago Costa <tiagoffcc@hotmail.com>
2019-02-28 05:47:15 +0000

    chore(NA): patch babel register to load typescrit

--- packages/kbn-babel-preset/common_preset.js
+++ packages/kbn-babel-preset/common_preset.js
@@ -17,23 +17,25 @@
  * under the License.
  */
 
-module.exports = {
-  presets: [
-    require.resolve('@babel/preset-react'),
-    require.resolve('@babel/preset-typescript'),
-  ],
-  plugins: [
-    require.resolve('babel-plugin-add-module-exports'),
-    // stage 3
-    require.resolve('@babel/plugin-proposal-object-rest-spread'),
-    require.resolve('@babel/plugin-transform-async-to-generator'),
+module.exports = () => {
+  return {
+    presets: [
+      require.resolve('@babel/preset-typescript'),
+      require.resolve('@babel/preset-react'),
+    ],
+    plugins: [
+      require.resolve('babel-plugin-add-module-exports'),
+      // stage 3
+      require.resolve('@babel/plugin-proposal-object-rest-spread'),
+      require.resolve('@babel/plugin-transform-async-to-generator'),
 
-    // the class properties proposal was merged with the private fields proposal
-    // into the "class fields" proposal. Babel doesn't support this combined
-    // proposal yet, which includes private field, so this transform is
-    // TECHNICALLY stage 2, but for all intents and purposes it's stage 3
-    //
-    // See https://github.com/babel/proposals/issues/12 for progress
-    require.resolve('@babel/plugin-proposal-class-properties'),
-  ],
+      // the class properties proposal was merged with the private fields proposal
+      // into the "class fields" proposal. Babel doesn't support this combined
+      // proposal yet, which includes private field, so this transform is
+      // TECHNICALLY stage 2, but for all intents and purposes it's stage 3
+      //
+      // See https://github.com/babel/proposals/issues/12 for progress
+      require.resolve('@babel/plugin-proposal-class-properties'),
+    ],
+  };
 };
