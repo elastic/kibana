f4d0bee684ce72c727994864f74c1bd5bd78ba10
Garrett Spong <garrett.spong@elastic.co>
2019-02-28 12:44:24 -0700

    Updating ecs mapping for host.ip and host.mac

--- x-pack/plugins/secops/server/lib/ecs_fields/index.ts
+++ x-pack/plugins/secops/server/lib/ecs_fields/index.ts
@@ -18,8 +18,8 @@ export const osFieldsMap: Readonly<Record<string, string>> = {
 export const hostFieldsMap: Readonly<Record<string, string>> = {
   'host.architecture': 'host.architecture',
   'host.id': 'host.id',
-  'host.ip': 'system.audit.host.ip',
-  'host.mac': 'system.audit.host.mac',
+  'host.ip': 'host.ip',
+  'host.mac': 'host.mac',
   'host.name': 'host.name',
   'host.type': 'cloud.machine.type',
   ...extendMap('host', osFieldsMap),
