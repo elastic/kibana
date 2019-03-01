3f8e9ccc68c677901f3798ecd9ba5c9ae216e87a
Justin Kambic <justin.kambic@elastic.co>
2019-03-01 18:33:34 +0530

    Added description
    Co-Authored-By: ganeshpatro321 <30416891+ganeshpatro321@users.noreply.github.com>

--- x-pack/plugins/uptime/public/components/functional/filter_bar_loading.tsx
+++ x-pack/plugins/uptime/public/components/functional/filter_bar_loading.tsx
@@ -15,4 +15,8 @@ const searchBox = {
   }),
 };
 
+/**
+ * This component provides a visual placeholder while the FilterBar is loading.
+ * The onChange prop is required, so we provide an empty function to suppress the warning.
+ */
 export const FilterBarLoading = () => <EuiSearchBar box={searchBox} onChange={()=>{ /* */}}/>;
