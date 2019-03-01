f5896cfeb7d88a81031b79680a079e9f101e18e5
Mikhail Shustov <restrry@gmail.com>
2019-03-01 18:17:22 +0100

    update babel-jest to v24

--- src/dev/jest/babel_transform.js
+++ src/dev/jest/babel_transform.js
@@ -21,6 +21,6 @@ const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
   presets: [
-    require.resolve('@kbn/babel-preset/node_preset')
+    require.resolve('@kbn/babel-preset/node_preset_7')
   ]
 });
