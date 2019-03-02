d98d3e093ef9d8ace59aa6bf85ec3d06aef5f6a6
Spencer <email@spalger.com>
2019-03-01 17:53:05 -0800

    Restore webdriver-based functional test runner driver (#31446)

--- test/functional/services/filter_bar.js
+++ test/functional/services/filter_bar.js
@@ -26,7 +26,10 @@ export function FilterBarProvider({ getService, getPageObjects }) {
     hasFilter(key, value, enabled = true) {
       const filterActivationState = enabled ? 'enabled' : 'disabled';
       return testSubjects.exists(
-        `filter & filter-key-${key} & filter-value-${value} & filter-${filterActivationState}`
+        `filter & filter-key-${key} & filter-value-${value} & filter-${filterActivationState}`,
+        {
+          allowHidden: true
+        }
       );
     }
 
