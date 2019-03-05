c343f80ffad543625615c976764a0338b152448f
Rudolf Meijering <skaapgif@gmail.com>
2019-03-05 22:37:17 +0100

    Suppress logging when running api-extractor from script

--- src/dev/run_check_core_api_changes.ts
+++ src/dev/run_check_core_api_changes.ts
@@ -17,24 +17,59 @@
  * under the License.
  */
 
-
 import { Extractor } from '@microsoft/api-extractor';
+import * as fs from 'fs';
+import * as path from 'path';
 import { run } from './run';
 
-const config = require('../../api-extractor.json');
+const config = JSON.parse(fs.readFileSync(path.resolve('./api-extractor.json')).toString());
+
+// Because of the internals of api-extractor ILogger can't be implemented as a typescript Class
+const memoryLogger = {
+  warnings: [] as string[],
+  errors: [] as string[],
+  logVerbose(message: string) {
+    return null;
+  },
+
+  logInfo(message: string) {
+    return null;
+  },
+
+  logWarning(message: string) {
+    this.warnings.push(message);
+  },
+
+  logError(message: string) {
+    this.errors.push(message);
+  },
+};
 
 const options = {
   // Indicates that API Extractor is running as part of a local build,
   // e.g. on developer's machine. For example, if the *.api.ts output file
   // has differences, it will be automatically overwritten for a
   // local build, whereas this should report an error for a production build.
-  localBuild: false
+  localBuild: false,
+  customLogger: memoryLogger,
 };
 
 run(({ log }) => {
   const extractor = new Extractor(config, options);
   const apiChanged = !extractor.processProject();
+
+  memoryLogger.errors.forEach(e => log.warning(e));
+  memoryLogger.warnings
+    .filter(msg => !msg.startsWith('You have changed the public API signature for this project.'))
+    .forEach(msg => log.warning(msg));
+
   if (apiChanged) {
-    log.warning('Remember to regenarate core documentation with `yarn docs:api`');
+    log.warning('You have changed the public signature of the Kibana Core API\n');
+    log.warning(
+      'Review your changes and then:\n' +
+        '\t 1. Overwrite common/core_api_review/kibana.api.ts with a copy of build/kibana.api.ts\n' +
+        '\t 2. Regenarate core documentation with `yarn docs:api`\n' +
+        "\t 3. Describe the change in your PR including whether it's a major, minor or patch"
+    );
   }
 });
