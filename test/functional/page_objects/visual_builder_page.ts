4befafbb9943820c02cab20104a717b97f49df4f
Vitali Haradkou <vitalicset@yandex.ru>
2019-03-05 15:53:42 +0300

    [TSVB]:  initial markdown test implementation  (#31893) (#32471)
    * Improve test coverage for TSVB markdown

--- test/functional/page_objects/visual_builder_page.ts
+++ test/functional/page_objects/visual_builder_page.ts
@@ -17,7 +17,9 @@
  * under the License.
  */
 
-export function VisualBuilderPageProvider({ getService, getPageObjects }) {
+import { FtrProviderContext } from '../ftr_provider_context.d';
+
+export function VisualBuilderPageProvider({ getService, getPageObjects }: FtrProviderContext) {
   const find = getService('find');
   const retry = getService('retry');
   const log = getService('log');
@@ -27,38 +29,48 @@ export function VisualBuilderPageProvider({ getService, getPageObjects }) {
   const PageObjects = getPageObjects(['common', 'header', 'visualize', 'timePicker']);
 
   class VisualBuilderPage {
-
-    async resetPage() {
-      const fromTime = '2015-09-19 06:31:44.000';
-      const toTime = '2015-09-22 18:31:44.000';
+    public async resetPage(
+      fromTime = '2015-09-19 06:31:44.000',
+      toTime = '2015-09-22 18:31:44.000'
+    ) {
       log.debug('navigateToApp visualize');
       await PageObjects.visualize.navigateToNewVisualization();
       log.debug('clickVisualBuilderChart');
       await PageObjects.visualize.clickVisualBuilder();
-      log.debug('Set absolute time range from \"' + fromTime + '\" to \"' + toTime + '\"');
+      log.debug('Set absolute time range from "' + fromTime + '" to "' + toTime + '"');
       await PageObjects.timePicker.setAbsoluteRange(fromTime, toTime);
     }
 
-    async clickMetric() {
+    public async clickMetric() {
       const button = await testSubjects.find('metricTsvbTypeBtn');
       await button.click();
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async clickMarkdown() {
+    public async clickMarkdown() {
       const button = await testSubjects.find('markdownTsvbTypeBtn');
       await button.click();
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async getMetricValue() {
+    public async getMetricValue() {
       const metricValue = await find.byCssSelector('.tvbVisMetric__value--primary');
       return metricValue.getVisibleText();
     }
 
-    async enterMarkdown(markdown) {
-      const prevRenderingCount = await PageObjects.visualize.getVisualizationRenderingCount();
+    public async enterMarkdown(markdown: string) {
       const input = await find.byCssSelector('.tvbMarkdownEditor__editor textarea');
+      await this.clearMarkdown();
+      const prevRenderingCount = await PageObjects.visualize.getVisualizationRenderingCount();
+      await input.type(markdown);
+      await PageObjects.visualize.waitForVisualizationRenderingStabilized();
+      await PageObjects.visualize.waitForRenderingCount(prevRenderingCount + 1);
+    }
+
+    public async clearMarkdown() {
+      const input = await find.byCssSelector('.tvbMarkdownEditor__editor textarea');
+      // click for switching context(fix for "should render first table variable" test)
+      // see _tsvb_markdown.js
       // Since we use ACE editor and that isn't really storing its value inside
       // a textarea we must really select all text and remove it, and cannot use
       // clearValue().
@@ -69,80 +81,142 @@ export function VisualBuilderPageProvider({ getService, getPageObjects }) {
       }
       await input.pressKeys(browser.keys.NULL); // Release modifier keys
       await input.pressKeys(browser.keys.BACKSPACE); // Delete all content
-      await input.type(markdown);
-      await PageObjects.visualize.waitForRenderingCount(prevRenderingCount + 1);
     }
 
-    async getMarkdownText() {
+    public async getMarkdownText() {
       const el = await find.byCssSelector('.tvbEditorVisualization');
-      return await el.getVisibleText();
+      const text = await el.getVisibleText();
+      return text;
     }
 
-    async clickMarkdownData() {
-      await testSubjects.click('markdownDataBtn');
+    /**
+     *
+     * getting all markdown variables list which located on `table` section
+     *
+     * **Note**: if `table` not have variables, use `getMarkdownTableNoVariables` method instead
+     * @see {getMarkdownTableNoVariables}
+     * @returns {Promise<Array<{key:string, value:string, selector:any}>>}
+     * @memberof VisualBuilderPage
+     */
+    public async getMarkdownTableVariables(): Promise<
+      Array<{ key: string; value: string; selector: any }>
+    > {
+      const testTableVariables = await testSubjects.find('tsvbMarkdownVariablesTable');
+      const variablesSelector = 'tbody tr';
+      const exists = await find.existsByDisplayedByCssSelector(variablesSelector);
+      if (!exists) {
+        log.debug('variable list is empty');
+        return [];
+      }
+      const variables: any[] = await testTableVariables.findAllByCssSelector(variablesSelector);
+
+      const variablesKeyValueSelectorMap = await Promise.all(
+        variables.map(async (variable: any) => {
+          const subVars = await variable.findAllByCssSelector('td');
+          const selector = await subVars[0].findByTagName('a');
+          const key = await selector.getVisibleText();
+          const value = await subVars[1].getVisibleText();
+          log.debug(`markdown table variables table is: ${key} ${value}`);
+          return { key, value, selector };
+        })
+      );
+      return variablesKeyValueSelectorMap;
     }
 
-    async clickSeriesOption(nth = 0) {
+    /**
+     * return variable table message, if `table` is empty it will be fail
+     *
+     * **Note:** if `table` have variables, use `getMarkdownTableVariables` method instead
+     * @see {@link VisualBuilderPage#getMarkdownTableVariables}
+     * @returns
+     * @memberof VisualBuilderPage
+     */
+    public async getMarkdownTableNoVariables() {
+      return await testSubjects.getVisibleText('tvbMarkdownEditor__noVariables');
+    }
+
+    /**
+     * get all sub-tabs count for `time series`, `metric`, `top n`, `gauge`, `markdown` or `table` tab.
+     *
+     * @returns {Promise<any[]>}
+     * @memberof VisualBuilderPage
+     */
+    public async getSubTabs() {
+      return await find.allByCssSelector('[data-test-subj$="-subtab"]');
+    }
+
+    /**
+     * switch markdown sub-tab for visualization
+     *
+     * @param {'data' | 'options'| 'markdown'} subTab
+     * @memberof VisualBuilderPage
+     */
+    public async markdownSwitchSubTab(subTab: 'data' | 'options' | 'markdown') {
+      const element = await testSubjects.find(`${subTab}-subtab`);
+      await element.click();
+    }
+
+    public async clickSeriesOption(nth = 0) {
       const el = await testSubjects.findAll('seriesOptions');
       await el[nth].click();
-      await PageObjects.common.sleep(300);
+      await PageObjects.common.sleep(500);
     }
 
-    async clearOffsetSeries() {
+    public async clearOffsetSeries() {
       const el = await testSubjects.find('offsetTimeSeries');
       await el.clearValue();
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async enterOffsetSeries(value) {
+    public async enterOffsetSeries(value: string) {
       const el = await testSubjects.find('offsetTimeSeries');
       await el.clearValue();
       await el.type(value);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async getRhythmChartLegendValue() {
+    public async getRhythmChartLegendValue() {
       const metricValue = await find.byCssSelector('.tvbLegend__itemValue');
       await metricValue.moveMouseTo();
       return await metricValue.getVisibleText();
     }
 
-    async clickGauge() {
+    public async clickGauge() {
       await testSubjects.click('gaugeTsvbTypeBtn');
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async getGaugeLabel() {
+    public async getGaugeLabel() {
       const gaugeLabel = await find.byCssSelector('.tvbVisGauge__label');
       return await gaugeLabel.getVisibleText();
     }
 
-    async getGaugeCount() {
+    public async getGaugeCount() {
       const gaugeCount = await find.byCssSelector('.tvbVisGauge__value');
       return await gaugeCount.getVisibleText();
     }
 
-    async clickTopN() {
+    public async clickTopN() {
       await testSubjects.click('top_nTsvbTypeBtn');
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async getTopNLabel() {
+    public async getTopNLabel() {
       const topNLabel = await find.byCssSelector('.tvbVisTopN__label');
       return await topNLabel.getVisibleText();
     }
 
-    async getTopNCount() {
+    public async getTopNCount() {
       const gaugeCount = await find.byCssSelector('.tvbVisTopN__value');
       return await gaugeCount.getVisibleText();
     }
 
-    async clickTable() {
+    public async clickTable() {
       await testSubjects.click('tableTsvbTypeBtn');
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async createNewAgg(nth = 0) {
+    public async createNewAgg(nth = 0) {
       return await retry.try(async () => {
         const elements = await testSubjects.findAll('addMetricAddBtn');
         await elements[nth].click();
@@ -154,55 +228,59 @@ export function VisualBuilderPageProvider({ getService, getPageObjects }) {
       });
     }
 
-    async selectAggType(value, nth = 0) {
+    public async selectAggType(value: string | number, nth = 0) {
       const elements = await testSubjects.findAll('aggSelector');
       await comboBox.setElement(elements[nth], value);
       return await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async fillInExpression(expression, nth = 0) {
+    public async fillInExpression(expression: string, nth = 0) {
       const expressions = await testSubjects.findAll('mathExpression');
       await expressions[nth].type(expression);
       return await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async fillInVariable(name = 'test', metric = 'count', nth = 0) {
+    public async fillInVariable(name = 'test', metric = 'count', nth = 0) {
       const elements = await testSubjects.findAll('varRow');
       const varNameInput = await elements[nth].findByCssSelector('.tvbAggs__varName');
       await varNameInput.type(name);
-      const metricSelectWrapper = await elements[nth].findByCssSelector('.tvbAggs__varMetricWrapper');
+      const metricSelectWrapper = await elements[nth].findByCssSelector(
+        '.tvbAggs__varMetricWrapper'
+      );
       await comboBox.setElement(metricSelectWrapper, metric);
       return await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-
-    async selectGroupByField(fieldName) {
+    public async selectGroupByField(fieldName: string) {
       await comboBox.set('groupByField', fieldName);
     }
 
-    async setLabelValue(value) {
+    public async setLabelValue(value: string) {
       const el = await testSubjects.find('columnLabelName');
       await el.clearValue();
       await el.type(value);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
 
-    async getViewTable() {
+    public async getViewTable() {
       const tableView = await testSubjects.find('tableView');
       return await tableView.getVisibleText();
     }
-    async clickMetricPanelOptions() {
+
+    public async clickMetricPanelOptions() {
       const button = await testSubjects.find('metricEditorPanelOptionsBtn');
       await button.click();
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
-    async setIndexPatternValue(value) {
+
+    public async setIndexPatternValue(value: string) {
       const el = await testSubjects.find('metricsIndexPatternInput');
       await el.clearValue();
       await el.type(value);
       await PageObjects.header.waitUntilLoadingHasFinished();
     }
-    async selectIndexPatternTimeField(timeField) {
+
+    public async selectIndexPatternTimeField(timeField: string) {
       const el = await testSubjects.find('comboBoxSearchInput');
       await el.clearValue();
       await el.type(timeField);
