2319f98b5db30fe15bc30b824d23cb62b5413ec2
Timothy Sullivan <tsullivan@elastic.co>
2019-03-04 18:03:00 -0700

    remove boring diff

--- x-pack/plugins/reporting/export_types/csv_from_savedobject/server/lib/get_data_source.ts
+++ x-pack/plugins/reporting/export_types/csv_from_savedobject/server/lib/get_data_source.ts
@@ -41,7 +41,6 @@ export async function getDataSource(
       throw new Error(`Could not get saved search info! ${err}`);
     }
   }
-
   try {
     const { attributes: indexPatternAttributes } = await savedObjectsClient.get(
       'index-pattern',
@@ -59,6 +58,5 @@ export async function getDataSource(
   } catch (err) {
     throw new Error(`Could not get index pattern saved object! ${err}`);
   }
-
   return { indexPatternSavedObject, searchSource };
 }
