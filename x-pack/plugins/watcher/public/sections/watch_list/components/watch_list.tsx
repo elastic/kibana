322f0da83968cedaddb3ee7d53feaa5b9063501d
Bill McConaghy <bill.mcconaghy@elastic.co>
2019-03-05 11:44:05 -0500

    fixing typp

--- x-pack/plugins/watcher/public/sections/watch_list/components/watch_list.tsx
+++ x-pack/plugins/watcher/public/sections/watch_list/components/watch_list.tsx
@@ -73,7 +73,7 @@ const WatchListUi = ({ intl }: { intl: InjectedIntl }) => {
       },
     },
     {
-      field: 'name',˝
+      field: 'name',
       name: i18n.translate('xpack.watcher.sections.watchList.watchTable.nameHeader', {
         defaultMessage: 'Name',
       }),
