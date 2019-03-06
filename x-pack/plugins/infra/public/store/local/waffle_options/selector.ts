cf332bca2283a30077d0958b831d0355def6e344
Chris Cowan <chris@chriscowan.us>
2019-03-06 06:42:18 -0700

    [Infra UI] Add Legend Settings for Waffle Map (#32228)
    * Adding legend controls
    
    * Adding url support and finishing feature
    
    * Moving config to right side; setting min/max to data boundries when deactivating auto
    
    * Removing legend from max and min labels
    
    * removing uneseccary unshift(0)
    
    * Change autobounds behavior

--- x-pack/plugins/infra/public/store/local/waffle_options/selector.ts
+++ x-pack/plugins/infra/public/store/local/waffle_options/selector.ts
@@ -11,3 +11,5 @@ export const selectGroupBy = (state: WaffleOptionsState) => state.groupBy;
 export const selectCustomOptions = (state: WaffleOptionsState) => state.customOptions;
 export const selectNodeType = (state: WaffleOptionsState) => state.nodeType;
 export const selectView = (state: WaffleOptionsState) => state.view;
+export const selectBoundsOverride = (state: WaffleOptionsState) => state.boundsOverride;
+export const selectAutoBounds = (state: WaffleOptionsState) => state.autoBounds;
