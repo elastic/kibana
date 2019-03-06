253a2102cdca5dbd61743c1c49cfc96ec7f42faa
Jen Huang <its.jenetic@gmail.com>
2019-03-06 12:27:49 -0800

    Remove once() from common license checker and call with request factories

--- x-pack/server/lib/create_router/call_with_request_factory/call_with_request_factory.js
+++ x-pack/server/lib/create_router/call_with_request_factory/call_with_request_factory.js
@@ -4,14 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { once } from 'lodash';
-
 export const callWithRequestFactory = (server, request) => {
+  const { callWithRequest } = server.plugins.elasticsearch.getCluster('data');
   return (...args) => {
-    const callWithRequest = server => {
-      const cluster = server.plugins.elasticsearch.getCluster('data');
-      return cluster.callWithRequest;
-    };
-    return once(callWithRequest)(server)(request, ...args);
+    return callWithRequest(request, ...args);
   };
 };
