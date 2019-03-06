750b853b3d17201f8037abef2b83e007889b7490
Matt Kime <matt@mattki.me>
2019-03-05 20:57:31 -0600

    lets try octokit

--- .ci/checks_api_test.js
+++ .ci/checks_api_test.js
@@ -1,1 +1,11 @@
+const Octokit = require('@octokit/rest')
+const octokit = new Octokit ();
+
+octokit.repos.listForOrg({
+  org: 'octokit',
+  type: 'public'
+}).then(({data}) => {
+  console.log('data: ', data);
+})
+
 console.log('hi from script');
