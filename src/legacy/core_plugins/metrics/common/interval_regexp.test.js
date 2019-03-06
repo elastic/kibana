018f348cb6bf7d53a21359aee718eade771063d7
Alexey Antonov <alexwizp@gmail.com>
2019-03-06 12:36:03 +0300

    mocha -> jest

--- src/legacy/core_plugins/metrics/common/interval_regexp.test.js
+++ src/legacy/core_plugins/metrics/common/interval_regexp.test.js
@@ -16,80 +16,79 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { expect } from 'chai';
-import { GTE_INTERVAL_RE, INTERVAL_STRING_RE } from '../interval_regexp';
+
+import { GTE_INTERVAL_RE, INTERVAL_STRING_RE } from './interval_regexp';
 
 describe('interval_regexp', () => {
-
   describe('GTE_INTERVAL_RE', () => {
-    it('returns true for">=12h"', () => {
+    test('returns true for">=12h"', () => {
       const value = GTE_INTERVAL_RE.test('>=12h');
 
-      expect(value).to.be.true;
+      expect(value).toBeTruthy();
     });
-    it('returns true for ">=1y"', () => {
+    test('returns true for ">=1y"', () => {
       const value = GTE_INTERVAL_RE.test('>=12h');
 
-      expect(value).to.be.true;
+      expect(value).toBeTruthy();
     });
-    it('returns true for ">=25m"', () => {
+    test('returns true for ">=25m"', () => {
       const value = GTE_INTERVAL_RE.test('>=12h');
 
-      expect(value).to.be.true;
+      expect(value).toBeTruthy();
     });
-    it('returns false "auto"', () => {
+    test('returns false "auto"', () => {
       const value = GTE_INTERVAL_RE.test('auto');
 
-      expect(value).to.be.false;
+      expect(value).toBeFalsy();
     });
-    it('returns false "wrongInput"', () => {
+    test('returns false "wrongInput"', () => {
       const value = GTE_INTERVAL_RE.test('wrongInput');
 
-      expect(value).to.be.false;
+      expect(value).toBeFalsy();
     });
-    it('returns false "d"', () => {
+    test('returns false "d"', () => {
       const value = GTE_INTERVAL_RE.test('d');
 
-      expect(value).to.be.false;
+      expect(value).toBeFalsy();
     });
 
-    it('returns false "y"', () => {
+    test('returns false "y"', () => {
       const value = GTE_INTERVAL_RE.test('y');
 
-      expect(value).to.be.false;
+      expect(value).toBeFalsy();
     });
   });
 
   describe('INTERVAL_STRING_RE', () => {
-    it('returns true for "8d"', () => {
+    test('returns true for "8d"', () => {
       const value = INTERVAL_STRING_RE.test('8d');
 
-      expect(value).to.be.true;
+      expect(value).toBeTruthy();
     });
-    it('returns true for "1y"', () => {
+    test('returns true for "1y"', () => {
       const value = INTERVAL_STRING_RE.test('1y');
 
-      expect(value).to.be.true;
+      expect(value).toBeTruthy();
     });
-    it('returns true for "6M"', () => {
+    test('returns true for "6M"', () => {
       const value = INTERVAL_STRING_RE.test('6M');
 
-      expect(value).to.be.true;
+      expect(value).toBeTruthy();
     });
-    it('returns false "auto"', () => {
+    test('returns false "auto"', () => {
       const value = INTERVAL_STRING_RE.test('auto');
 
-      expect(value).to.be.false;
+      expect(value).toBeFalsy();
     });
-    it('returns false "wrongInput"', () => {
+    test('returns false "wrongInput"', () => {
       const value = INTERVAL_STRING_RE.test('wrongInput');
 
-      expect(value).to.be.false;
+      expect(value).toBeFalsy();
     });
-    it('returns false for">=21h"', () => {
+    test('returns false for">=21h"', () => {
       const value = INTERVAL_STRING_RE.test('>=21h');
 
-      expect(value).to.be.false;
+      expect(value).toBeFalsy();
     });
   });
 });
