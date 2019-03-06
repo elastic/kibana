d01192b0e5f8d0aebec630b93a99b6aee1bc147d
Mike Cote <mikecote@users.noreply.github.com>
2019-03-06 09:05:24 -0500

    Add back resolve import conflicts x-pack tests

---
+++ x-pack/test/saved_object_api_integration/common/suites/resolve_import_conflicts.ts
@@ -0,0 +1,110 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from 'expect.js';
+import { SuperTest } from 'supertest';
+import { DEFAULT_SPACE_ID } from '../../../../plugins/spaces/common/constants';
+import { getIdPrefix, getUrlPrefix } from '../lib/space_test_utils';
+import { DescribeFn, TestDefinitionAuthentication } from '../lib/types';
+
+interface ResolveImportConflictsTest {
+  statusCode: number;
+  response: (resp: { [key: string]: any }) => void;
+}
+
+interface ResolveImportConflictsTests {
+  default: ResolveImportConflictsTest;
+}
+
+interface ResolveImportConflictsTestDefinition {
+  user?: TestDefinitionAuthentication;
+  spaceId?: string;
+  tests: ResolveImportConflictsTests;
+}
+
+const createImportData = (spaceId: string) => [
+  {
+    type: 'dashboard',
+    id: `${getIdPrefix(spaceId)}a01b2f57-fcfd-4864-b735-09e28f0d815e`,
+    attributes: {
+      title: 'A great new dashboard',
+    },
+  },
+  {
+    type: 'globaltype',
+    id: '05976c65-1145-4858-bbf0-d225cc78a06e',
+    attributes: {
+      name: 'A new globaltype object',
+    },
+  },
+];
+
+export function resolveImportConflictsTestSuiteFactory(
+  es: any,
+  esArchiver: any,
+  supertest: SuperTest<any>
+) {
+  const createExpectResults = (spaceId = DEFAULT_SPACE_ID) => async (resp: {
+    [key: string]: any;
+  }) => {
+    expect(resp.body).to.eql({
+      success: true,
+    });
+  };
+
+  const expectRbacForbidden = (resp: { [key: string]: any }) => {
+    expect(resp.body).to.eql({
+      statusCode: 403,
+      error: 'Forbidden',
+      message: `Unable to bulk_create dashboard, missing action:saved_objects/dashboard/bulk_create`,
+    });
+  };
+
+  const makeResolveImportConflictsTest = (describeFn: DescribeFn) => (
+    description: string,
+    definition: ResolveImportConflictsTestDefinition
+  ) => {
+    const { user = {}, spaceId = DEFAULT_SPACE_ID, tests } = definition;
+
+    describeFn(description, () => {
+      before(() => esArchiver.load('saved_objects/spaces'));
+      after(() => esArchiver.unload('saved_objects/spaces'));
+
+      it(`should return ${tests.default.statusCode}`, async () => {
+        const data = createImportData(spaceId);
+        await supertest
+          .post(`${getUrlPrefix(spaceId)}/api/saved_objects/_resolve_import_conflicts`)
+          .auth(user.username, user.password)
+          .field(
+            'overwrites',
+            JSON.stringify([
+              {
+                type: 'dashboard',
+                id: `${getIdPrefix(spaceId)}a01b2f57-fcfd-4864-b735-09e28f0d815e`,
+              },
+            ])
+          )
+          .attach(
+            'file',
+            Buffer.from(data.map(obj => JSON.stringify(obj)).join('\n'), 'utf8'),
+            'export.ndjson'
+          )
+          .expect(tests.default.statusCode)
+          .then(tests.default.response);
+      });
+    });
+  };
+
+  const resolveImportConflictsTest = makeResolveImportConflictsTest(describe);
+  // @ts-ignore
+  resolveImportConflictsTest.only = makeResolveImportConflictsTest(describe.only);
+
+  return {
+    resolveImportConflictsTest,
+    createExpectResults,
+    expectRbacForbidden,
+  };
+}
--- x-pack/test/saved_object_api_integration/spaces_only/apis/index.ts
+++ x-pack/test/saved_object_api_integration/spaces_only/apis/index.ts
@@ -19,6 +19,7 @@ export default function({ loadTestFile }: TestInvoker) {
     loadTestFile(require.resolve('./find'));
     loadTestFile(require.resolve('./get'));
     loadTestFile(require.resolve('./import'));
+    loadTestFile(require.resolve('./resolve_import_conflicts'));
     loadTestFile(require.resolve('./update'));
   });
 }
