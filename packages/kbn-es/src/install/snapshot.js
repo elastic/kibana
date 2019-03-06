62f90b5dac8cf1675b0e83ef5b6540a190d9eb06
Jonathan Budzenski <jon@jbudz.me>
2019-03-06 12:38:44 -0600

    Revert "[kbn/es] pin 7.x snapshot until elastic/elasticsearch#39582 is merged (#32340)"
    This reverts commit 5285e8b6b626cd538d86758feb0e27b8259008b9.

--- packages/kbn-es/src/install/snapshot.js
+++ packages/kbn-es/src/install/snapshot.js
@@ -27,11 +27,6 @@ const { BASE_PATH } = require('../paths');
 const { installArchive } = require('./archive');
 const { log: defaultLog, cache } = require('../utils');
 
-// pin TEST_ES_SNAPSHOT_VERSION until https://github.com/elastic/elasticsearch/pull/39582 is merged and snapshots have updated
-if (!process.env.TEST_ES_SNAPSHOT_VERSION) {
-  process.env.TEST_ES_SNAPSHOT_VERSION = '7.1.0-36d8499e';
-}
-
 /**
  * Download an ES snapshot
  *
