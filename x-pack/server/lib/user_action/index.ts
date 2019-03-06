554e7be400216fa1d0bc683fde2ff3f8e3fd1f04
CJ Cenizal <cj@cenizal.com>
2019-03-05 16:53:34 -0800

    Add user_action app for gathering telemetry on user actions (#31543)
    * Add user_action app with API endpoint for tracking user actions.
    * Track 'Create rollup job' action and publish it in the collector.
    * Extract fetchUserActions into a generic helper inside of xpack/server/lib.
    * Add trackUserRequest helper.

--- x-pack/server/lib/user_action/index.ts
+++ x-pack/server/lib/user_action/index.ts
@@ -4,13 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export const PLUGIN = {
-  ID: 'rollup'
-};
-
-export const UA_APP_NAME = 'rollup-job-wizard';
-export const UA_ROLLUP_JOB_CREATE = 'create';
-
-export const USER_ACTIONS = [
-  UA_ROLLUP_JOB_CREATE,
-];
+export { fetchUserActions } from './fetch_user_actions';
