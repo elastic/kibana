3b8eeaf5fc227e4bc9c4033c1fb179419675dc49
nicknak <njd5475@gmail.com>
2019-03-07 14:21:11 -0500

    Testing/Removal of throw on console error (#29983)
    * Use hosts instead of url for elasticsearch.
    
    * Adds jest integration tests to run in xpack intake.
    
    * Reverting the exclusion of throwing errors in tests.
    
    * Strange test that seems to fail without console error exception being thrown.
    
    * Fix issues with wrong props for EuiInMemoryTable.
    
    * Removes the masking of console errors in tests.

--- src/legacy/core_plugins/kibana/public/management/sections/objects/components/objects_table/components/relationships/relationships.js
+++ src/legacy/core_plugins/kibana/public/management/sections/objects/components/objects_table/components/relationships/relationships.js
@@ -205,7 +205,7 @@ class RelationshipsUI extends Component {
               items={list}
               columns={[
                 {
-                  width: 24,
+                  width: '24px',
                   render: () => (
                     <EuiToolTip
                       position="top"
