ee7e6dc277cddf5615b454be3c19bd4a4a90ca63
Mikhail Shustov <restrry@gmail.com>
2019-03-07 21:53:21 +0100

    [New platform] Expose NP core services and plugins contracts to the legacy server (#32468) (#32666)
    * expose NewPlatform core and plugins to legacy server
    
    * get rid of .core in favor of .newPlatform
    
    * decalre start/stop contract explicitly
    
    * expose newPlatform typings
    
    * Remove dependency on legacy package_json util from core
    
    * Remove dependencies on core/server in core/types

--- src/core/server/elasticsearch/elasticsearch_service.test.ts
+++ src/core/server/elasticsearch/elasticsearch_service.test.ts
@@ -23,9 +23,9 @@ const MockClusterClient = jest.fn();
 jest.mock('./cluster_client', () => ({ ClusterClient: MockClusterClient }));
 
 import { BehaviorSubject, combineLatest } from 'rxjs';
-import { CoreContext } from '../../types';
 import { Config, ConfigService, Env, ObjectToConfigAdapter } from '../config';
 import { getEnvOptions } from '../config/__mocks__/env';
+import { CoreContext } from '../core_context';
 import { loggingServiceMock } from '../logging/logging_service.mock';
 import { ElasticsearchConfig } from './elasticsearch_config';
 import { ElasticsearchService } from './elasticsearch_service';
