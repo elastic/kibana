a1b4acbdec4832b8e7c09653e1e617eec59c162a
Justin Kambic <justin.kambic@elastic.co>
2019-03-08 16:23:16 -0500

    Get functional and unit tests passing.

--- x-pack/test/api_integration/apis/uptime/get_all_pings.js
+++ x-pack/test/api_integration/apis/uptime/get_all_pings.js
@@ -16,8 +16,8 @@ export default function ({ getService }) {
   describe('get_all_pings', () => {
     const archive = 'uptime/pings';
 
-    before(() => esArchiver.load(archive));
-    after(() => esArchiver.unload(archive));
+    before('load heartbeat data', async () => await esArchiver.load(archive));
+    after('unload heartbeat data', async () => await esArchiver.unload(archive));
 
     it('should get all pings stored in index', async () => {
       const { body: apiResponse } = await supertest
