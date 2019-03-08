0767f379ef308d4d54eecba5008df76fdbf54be7
spalger <email@spalger.com>
2019-03-07 21:41:18 -0800

    use loosy assertions since stack traces are different on CI

--- src/dev/jest/integration_tests/junit_reporter.test.js
+++ src/dev/jest/integration_tests/junit_reporter.test.js
@@ -60,52 +60,42 @@ it(
     );
 
     expect(result.code).toBe(1);
-    await expect(parseXml(readFileSync(XML_PATH, 'utf8'))).resolves.toMatchInlineSnapshot(`
-Object {
-  "testsuites": Object {
-    "$": Object {
-      "name": "jest",
-      "skipped": "0",
-      "tests": "1",
-      "time": "0.735",
-      "timestamp": "2019-03-07T19:19:57",
-    },
-    "testsuite": Array [
-      Object {
-        "$": Object {
-          "failures": "1",
-          "file": "<absolute path>/src/dev/jest/integration_tests/__fixtures__/test.js",
-          "name": "test.js",
-          "skipped": "0",
-          "tests": "1",
-          "time": "0.340",
-          "timestamp": "2019-03-07T19:19:58",
+    await expect(parseXml(readFileSync(XML_PATH, 'utf8'))).resolves.toEqual({
+      testsuites: {
+        $: {
+          name: 'jest',
+          skipped: '0',
+          tests: '1',
+          time: expect.anything(),
+          timestamp: expect.anything(),
         },
-        "testcase": Array [
-          Object {
-            "$": Object {
-              "classname": "Jest Tests.·",
-              "name": "fails",
-              "time": "0.002",
+        testsuite: [
+          {
+            $: {
+              failures: '1',
+              file: resolve(ROOT_DIR, 'src/dev/jest/integration_tests/__fixtures__/test.js'),
+              name: 'test.js',
+              skipped: '0',
+              tests: '1',
+              time: expect.anything(),
+              timestamp: expect.anything(),
             },
-            "failure": Array [
-              "
-        Error: failure
-    at Object.<anonymous>.it (<absolute path>/src/dev/jest/integration_tests/__fixtures__/test.js:21:9)
-    at Object.asyncJestTest (<absolute path>/node_modules/jest-jasmine2/build/jasmineAsyncInstall.js:102:37)
-    at resolve (<absolute path>/node_modules/jest-jasmine2/build/queueRunner.js:41:12)
-    at new Promise (<anonymous>)
-    at mapper (<absolute path>/node_modules/jest-jasmine2/build/queueRunner.js:26:19)
-    at promise.then (<absolute path>/node_modules/jest-jasmine2/build/queueRunner.js:71:41)
-      ",
-            ],
-          },
-        ],
-      },
-    ],
-  },
-}
-`);
+            testcase: [
+              {
+                $: {
+                  classname: 'Jest Tests.·',
+                  name: 'fails',
+                  time: expect.anything()
+                },
+                failure: [
+                  expect.stringMatching(/Error: failure\s+at /m)
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    });
   },
   3 * MINUTE
 );
