4cdaf7e328f80f8d49b1bdcbf527d00d68c9bd7c
Nathan Reese <reese.nathan@gmail.com>
2019-03-08 08:55:30 -0700

    implement getCapabilities in WmsClient

--- x-pack/plugins/maps/public/shared/layers/sources/wms_source/wms_client.js
+++ x-pack/plugins/maps/public/shared/layers/sources/wms_source/wms_client.js
@@ -4,9 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import _ from 'lodash';
 import { parseString } from 'xml2js';
-
-//https://idpgis.ncep.noaa.gov/arcgis/services/NWS_Forecasts_Guidance_Warnings/NDFD_temp/MapServer/WMSServer
 
 export class WmsClient {
   constructor({ serviceUrl, version = '1.1.1' }) {
@@ -37,11 +36,64 @@ export class WmsClient {
     return await parsePromise;
   }
 
-  async getCapabilities() {
-    const capabilities = await this._fetchCapabilities();
-    console.log('capabilities', capabilities);
+  reduceLayers(path, layers) {
+    const emptyCapabilities = {
+      layers: [],
+      styles: [],
+    };
+    function groupOption(groups, optionGroupName, optionLabel, optionValue) {
+      if (!optionLabel) {
+        return groups;
+      }
 
-    return [];
+      const newOption = { label: optionLabel, value: optionValue };
+      const matchingGroup = groups.find(group => {
+        return group.label === optionGroupName;
+      });
+
+      // no matching group found, create new group
+      if (!matchingGroup) {
+        const newGroup = {
+          label: optionGroupName,
+          options: [newOption]
+        };
+        return [...groups, newGroup];
+      }
+
+      // add option to matching group
+      matchingGroup.options.push(newOption);
+      return [...groups];
+    }
+
+    return layers.reduce((accumulatedCapabilities, layer) => {
+      // Layer is hierarchical, continue traversing
+      if (layer.Layer) {
+        const hierarchicalCapabilities  = this.reduceLayers([...path, layer.Title], layer.Layer);
+        return {
+          layers: [...accumulatedCapabilities.layers, ...hierarchicalCapabilities.layers],
+          styles: [...accumulatedCapabilities.styles, ...hierarchicalCapabilities.styles]
+        };
+      }
+
+      return {
+        layers: groupOption(
+          accumulatedCapabilities.layers,
+          path.join(' - '),
+          layer.Title[0],
+          layer.Name[0]),
+        styles: groupOption(
+          accumulatedCapabilities.styles,
+          path.join(' - '),
+          _.get(layer, 'Style[0].Title[0]'),
+          _.get(layer, 'Style[0].Name[0]'))
+      };
+    }, emptyCapabilities);
+  }
+
+  async getCapabilities() {
+    const rawCapabilities = await this._fetchCapabilities();
+
+    return this.reduceLayers([], _.get(rawCapabilities, 'WMT_MS_Capabilities.Capability[0].Layer', []));
   }
 
 
