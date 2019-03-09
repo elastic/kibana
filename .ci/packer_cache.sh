ac5da1ec946273132b2d6bed005433d192eb12ce
spalger <email@spalger.com>
2019-03-09 11:03:06 -0800

    [ci/packer] fix type and skip delete of kibana checkout

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -43,12 +43,9 @@ tar -cf "$HOME/.kibana/bootstrap_cache/$branch.tar" \
 echo "created $HOME/.kibana/bootstrap_cache/$branch.tar"
 
 if [ "$branch" == "master" ]; then
-  echo "Creatting bootstrap cache for 7.x";
+  echo "Creating bootstrap cache for 7.x";
 
   git clone https://github.com/elastic/kibana.git --branch 7.x --depth 1 /tmp/kibana-7.x
-  cd /tmp/kibana-7.x;
-  rm -rf $KIBANA_DIR;
-  ./.ci/packer_cache.sh;
-  cd ~
+  (cd /tmp/kibana-7.x && ./.ci/packer_cache.sh);
   rm -rf /tmp/kibana-7.x;
 fi
