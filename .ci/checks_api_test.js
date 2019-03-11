ead76d5d3ae13580220ac3c7e310b39eb26966d9
Matt Kime <matt@mattki.me>
2019-03-11 15:34:56 -0500

    try using env vars

--- .ci/checks_api_test.js
+++ .ci/checks_api_test.js
@@ -1,62 +1,64 @@
 const Octokit = require('@octokit/rest')
+const App = require('@octokit/app');
+const request = require('@octokit/request');
 
-/*
-octokit.repos.listForOrg({
-  org: 'octokit',
-  type: 'public'
-}).then(({data}) => {
-  console.log('data: ', data);
-})
+const getInstallation = async function(jwt){
+  return await request('GET /repos/:owner/:repo/installation', {
+  owner: 'elastic',
+  repo: 'kibana',
+  headers: {
+    authorization: `Bearer ${jwt}`,
+    accept: 'application/vnd.github.machine-man-preview+json'
+  }
+});
+}
 
-function getGithubClient() {
-  const client = new Octokit();
 
-  client.authenticate({
-    type: 'token',
-    token: process.env.GITHUB_TOKEN
+
+
+
+const start = async function(){
+  console.log('hi from script');
+  const app = new App({
+    id: 26774,
+    privateKey: process.env.kibana_ci_reporter_key
   });
 
-  return client;
+  const jwt = app.getSignedJsonWebToken();
+  const { data } = await requesty('GET /repos/:owner/:repo/installation', {
+    owner: 'elastic',
+    repo: 'kibana',
+    headers: {
+      authorization: `Bearer ${jwt}`,
+      accept: 'application/vnd.github.machine-man-preview+json'
+    }
+  });
+
+  const installationId = data.id;
+  const installationAccessToken = await app.getInstallationAccessToken({ installationId })
+
+
+  const clientWithAuth = new Octokit({ auth: `token ${installationAccessToken}` });
+
+  console.log('creating check');
+  clientWithAuth.checks.create({
+    owner:'elastic',
+    repo:'kibana',
+    name: 'check name',
+    head_sha: process.env.ghprbActualCommit,
+    // head_sha: '7680ee538b1443fbb5f8d7a1e3c335bf477dbbdf',
+    details_url: 'http://www.google.com',
+    external_id: 'external id',
+    status: 'in_progress',
+    output: {
+      title: 'title',
+      summary: 'summary',
+      text: 'text',
+    },
+  }).then(({headers: {'x-ratelimit-limit': limit, 'x-ratelimit-remaining': remaining}}) => console.log(`limit: ${remaining} / ${limit}`));
+  console.log('check created');
+
 }
-console.log('START TEST');
 
-const client = getGithubClient();
+start();
 
-client.repos.getForOrg({
-  org: 'octokit',
-  type: 'public'
-}).then(({data}) => {
-  console.log('data: ', data);
-})
-*/
-
-const clientWithAuth = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
-console.log('creating check');
-clientWithAuth.checks.create({
-  owner:'elastic',
-  repo:'kibana',
-  name: 'check name',
-  head_sha: process.env.ghprbActualCommit,
-  details_url: 'http://www.google.com',
-  external_id: 'external id',
-  status: 'in_progress',
-  output: {
-    title: 'title',
-    summary: 'summary',
-    text: 'text',
-  },
-});
-console.log('check created');
-
-/*
-clientWithAuth.repos.listForOrg({
-  org: 'octokit',
-  type: 'public'
-}).then(({data}) => {
-  console.log('data: ', data);
-})
-*/
-
-
-console.log('hi from script');
--- src/dev/github_utils/index.js
+++ src/dev/github_utils/index.js
@@ -25,10 +25,9 @@ export { markdownMetadata };
 export function getGithubClient() {
   const client = new Octokit();
   client.authenticate({
-    //auth: process.env.GITHUB_TOKEN
     auth: {
-      clientId: '25454',
-      clientSecret: '-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAw+5PPK7gdH8MSqeZpunMAju4K/MfNTo6Z+yVtcEZJumg9cZd\nD+mCOnCwEZbaFznjdntBHQcTXLYWfYqB+Eve27EtwSvNMDtcGRn0zZ1bTtjn0Xz0\nSk594R1xF7RfQwMRvESgNALs8XjsQp87Xmy0RV5iSK4BDChCxjFLf2rzt85j9KyV\nvHo6FxUaKpVw+JArMCuFn6T3vm397mKbPRRnHcl126dWf8o/PwOsYC53Vq4YLAnD\nXIFBmBhkyUaEQN2dpfjpgJA8uTW73k3qnGbGWzU21ELJ4YoIEXIhU9DEjAfqRYmS\nwCiAZxaCvMt6OBGLceO4Re+WNBEl9C9neDN1TwIDAQABAoIBAA3WAlpyrv8LCb8r\n3rnewL1XokXYQVfs6Lr2Bgxzumb5EJjKjT/WaY/e7VAQqw4A1INFaO+31DIz2Zq7\n8Tmrge1ZsbV2dmL1jidnodXzSsNipdwxSr8Ek/cVsSrPXfrJeLMm1XlWy4yx0V1T\nCUo9uu07IQ2o3AITxcNhL8ersdJE7P0Q3/T1MpJMw0ttaxDK5N74DupO+Kgrhh17\nC2pBp070gyiSt2c2kgxqyZKBsb1hYlpx3V6aSgEafuhXuKuYmhBjHuYunGAwG7Vu\nLVZuhy63xCrxK8Vc8Y6i1JpsRiZ14JyBnOVOIShBDUoT+OPpmIH4/JVGHE8VH+s8\nmskZUKECgYEA/srthFkvSRFlMkJoiG8NGNe/aGAtM0J6W5XEsbL8rQJMqOL6jIZL\n8rV2/A+m4/eZA1YBl3bEXyVlsgsTEbq5cMcPF4RMzPGBkwW4T/ysDMjhgcb5U0h2\nEr00psau3L710c6JCa8OEboyw5FkCh8vnilNriGnIL6s//IIOVGTIoUCgYEAxNv6\n+Ss/54knTJIQq5FeeK/E9RGP8ToBcn9+qi/CO7uw5syKRf7qbR5pnorDFNxdcuG/\nKlKeGHgzsXrKvAVL6RwjKqOs1kpxWBxoUD8LnhDyaeTxeOREeXIWzMqYP7ZHeJT4\nfhBFqdd4khSLVuE1UcKj1nZxhqanPv2y5+qoosMCgYALhfoABliRIWxGPaKkqQEa\noodRnSfuLY+DHN7sen/bA0OcT8etG1XMAFEFTV/q54PQFs+znfd7piFXUBDouF0y\nsI4KLj2a90E4+QVA+Nh/K4ana+xSy/ArIMuEz2+RJEHR31lrSrOEMbuiBK1Vl9t7\nd7q/qV35qSYB3Vxp2zqgUQKBgH/H+sS6GiUElgnR1hhHp/bWwRYCbFi8uivELkfx\n8DYtwspNgoOk0C48S3qpv69OyynC9v9V2WEwxP0zI12gYoHRzdHjpFEEyUXUSrGr\n+rKDdZbfEQ7TvM7IC5yq9OCGRSY0LIhHr7BrOMw5oainTqDfotGW2GH60xzEONjp\nLMVZAoGBAIKaBkrtKuMkxNE+tM5G59rvkp+y+blj/99weYgBZRMZOE/sWTc70yvQ\n2Wm2c8wt1LmcwqUz61uILRlupiDGS35CpCk3wY03D/GUNIDcvrHk/YsfXoAkBTZ5\n8WvmKWwHrL2jxIUk+QHFkkgzeRib3oGzqtDMxcMMCDLvrGBlFhos\n-----END RSA PRIVATE KEY-----\n' // eslint-disable-line
+      clientId: 26774,
+      clientSecret: process.env.kibana_ci_reporter_key
     }
   });
 
