54ebfc1bea78da98d4912c81a4cdb57dd0792e13
CJ Cenizal <cj@cenizal.com>
2019-03-11 15:55:31 -0700

    Instrument Index Management with user action telemetry (#32595)
    * Fix API endpoint typo indices/clear_caches -> indices/clear_cache.
    * Track Index Management index actions.
    * Track user actions for opening detail tab and viewing various tabs.
      - Use constants instead of hard-coded strings to identify tabs.
      - Internationalize tab labels.
    * Track update settings action.
    * Track refresh action.
    * Track app load.

--- x-pack/plugins/index_management/server/usage/index.js
+++ x-pack/plugins/index_management/server/usage/index.js
@@ -4,12 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { REFRESH_RATE_INDEX_LIST } from './refresh_intervals';
-
-export {
-  TAB_SUMMARY,
-  TAB_SETTINGS,
-  TAB_MAPPING,
-  TAB_STATS,
-  TAB_EDIT_SETTINGS,
-} from './detail_panel_tabs';
+export { registerIndexManagementUsageCollector } from './collector';
