d7155b552f0233fc073869d2feea5d4d3738f71c
Josh Dover <me@joshdover.com>
2019-03-12 14:19:58 -0500

    Update functional tests

--- x-pack/test/upgrade_assistant_integration/upgrade_assistant/query_default_field.js
+++ x-pack/test/upgrade_assistant_integration/upgrade_assistant/query_default_field.js
@@ -14,7 +14,7 @@ export default function ({ getService }) {
 
   const indexName = `metricbeat-6.7.0-2019.03.11`;
 
-  describe('metricbeat default_field setting', () => {
+  describe('add default_field setting', () => {
     beforeEach(async () => {
       await esArchiver.load('upgrade_assistant/metricbeat');
     });
@@ -23,21 +23,12 @@ export default function ({ getService }) {
       await esArchiver.unload('upgrade_assistant/metricbeat');
     });
 
-    it('returns true for metricbeat index', async () => {
-      // Upgrade assistant should show deprecation message
-      const { body: uaBody } = await supertest.get('/api/upgrade_assistant/status').expect(200);
-      const depMessage = uaBody.indices.find(
-        dep => dep.index === indexName && dep.message === 'Number of fields exceeds automatic field expansion limit'
-      );
-      expect(depMessage).to.not.be(undefined);
-
-      // Upgrade assistant should correctly recognize this as a metricbeat index
-      const { body: metricbeatBody } = await supertest.get(`/api/upgrade_assistant/metricbeat/${indexName}`).expect(200);
-      expect(metricbeatBody).to.be(true);
-    });
-
     it('adds index.query.default_field to metricbeat index', async () => {
-      const { body } = await supertest.post(`/api/upgrade_assistant/metricbeat/${indexName}/fix`).set('kbn-xsrf', 'xxx').expect(200);
+      const { body } = await supertest
+        .post(`/api/upgrade_assistant/add_query_default_field/${indexName}`)
+        .set('kbn-xsrf', 'xxx')
+        .send({ fieldTypes: ['text', 'keyword', 'ip'], otherFields: ['fields.*'] })
+        .expect(200);
       expect(body.acknowledged).to.be(true);
 
       // The index.query.default_field setting should now be set
