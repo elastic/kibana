3280ab8ddcb7e263f90fccfe0479fb5f51f84b95
Spencer <email@spalger.com>
2019-03-13 13:20:41 -0700

    [6.7] Refactor ES-related environment variables (#33036) (#33171)
    Backports the following commits to 6.7:
     - Refactor ES-related environment variables  (#33036)

--- tasks/config/run.js
+++ tasks/config/run.js
@@ -65,7 +65,6 @@ module.exports = function (grunt) {
     '--server.port=5610',
   ];
 
-  const esFrom = process.env.TEST_ES_FROM || 'source';
   return {
     // used by the test and jenkins:unit tasks
     //    runs the eslint script to check for linting errors
@@ -195,7 +194,6 @@ module.exports = function (grunt) {
       args: [
         'scripts/functional_tests',
         '--config', 'test/api_integration/config.js',
-        '--esFrom', esFrom,
         '--bail',
         '--debug',
       ],
@@ -207,7 +205,6 @@ module.exports = function (grunt) {
         'scripts/functional_tests',
         '--config', 'test/server_integration/http/ssl/config.js',
         '--config', 'test/server_integration/http/ssl_redirect/config.js',
-        '--esFrom', esFrom,
         '--bail',
         '--debug',
         '--kibana-install-dir', KIBANA_INSTALL_DIR,
@@ -219,7 +216,6 @@ module.exports = function (grunt) {
       args: [
         'scripts/functional_tests',
         '--config', 'test/plugin_functional/config.js',
-        '--esFrom', esFrom,
         '--bail',
         '--debug',
         '--kibana-install-dir', KIBANA_INSTALL_DIR,
@@ -233,7 +229,6 @@ module.exports = function (grunt) {
       args: [
         'scripts/functional_tests',
         '--config', 'test/functional/config.js',
-        '--esFrom', esFrom,
         '--bail',
         '--debug',
         '--',
@@ -242,7 +237,6 @@ module.exports = function (grunt) {
     },
 
     ...getFunctionalTestGroupRunConfigs({
-      esFrom,
       kibanaInstallDir: KIBANA_INSTALL_DIR
     })
   };
