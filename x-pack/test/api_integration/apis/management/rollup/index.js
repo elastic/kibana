5c3ac275a6d774c8f2f3041fe62d8f164ba5c5fc
spalger <email@spalger.com>
2019-03-13 11:02:37 -0700

    Revert "[Rollup] Api integration test wildcard + search (#32884)"
    This reverts commit 6374c9676f4edd4f0289b0b4a9869600a214102b.

--- x-pack/test/api_integration/apis/management/rollup/index.js
+++ x-pack/test/api_integration/apis/management/rollup/index.js
@@ -7,7 +7,5 @@
 export default function ({ loadTestFile }) {
   describe('rollup', () => {
     loadTestFile(require.resolve('./rollup'));
-    loadTestFile(require.resolve('./index_patterns_extensions'));
-    loadTestFile(require.resolve('./rollup_search'));
   });
 }
