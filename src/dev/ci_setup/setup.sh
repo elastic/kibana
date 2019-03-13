b9baec2a6ad6990a65d638a48ef064dc72c238b4
spalger <email@spalger.com>
2019-03-12 18:49:43 -0700

    echo the actual variable value

--- src/dev/ci_setup/setup.sh
+++ src/dev/ci_setup/setup.sh
@@ -151,9 +151,10 @@ export GRUNT_BIN="$yarnBin/grunt"
 ###
 gitCommit="$(git rev-parse HEAD)"
 ciGroupCount="$(node ./src/dev/ci_setup/count_ci_groups)"
+# shellcheck disable=SC2154
 export PERCY_PULL_REQUEST="$ghprbPullId"
 export PERCY_PARALLEL_NONCE="$gitCommit/$BUILD_TAG"
 export PERCY_PARALLEL_TOTAL="$ciGroupCount"
-echo " -- PERCY_PULL_REQUEST='$ghprbPullId'"
+echo " -- PERCY_PULL_REQUEST='$PERCY_PULL_REQUEST'"
 echo " -- PERCY_PARALLEL_NONCE='$PERCY_PARALLEL_NONCE'"
 echo " -- PERCY_PARALLEL_TOTAL='$PERCY_PARALLEL_TOTAL'"
