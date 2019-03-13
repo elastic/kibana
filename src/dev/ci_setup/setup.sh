19fbbe140db30f41798b47e94a87c2720ee88b53
Spencer <email@spalger.com>
2019-03-13 13:19:14 -0700

    [6.6] Refactor ES-related environment variables (#33036) (#33173)
    Backports the following commits to 6.6:
     - Refactor ES-related environment variables  (#33036)

--- src/dev/ci_setup/setup.sh
+++ src/dev/ci_setup/setup.sh
@@ -26,12 +26,20 @@ else
   exit 1
 fi
 
+
 export KIBANA_DIR="$dir"
 export XPACK_DIR="$KIBANA_DIR/x-pack"
-export PARENT_DIR="$(cd "$KIBANA_DIR/.."; pwd)"
-echo "-> KIBANA_DIR $KIBANA_DIR"
-echo "-> XPACK_DIR $XPACK_DIR"
-echo "-> PARENT_DIR $PARENT_DIR"
+
+parentDir="$(cd "$KIBANA_DIR/.."; pwd)"
+export PARENT_DIR="$parentDir"
+
+kbnBranch="$(jq -r .branch "$KIBANA_DIR/package.json")"
+export KIBANA_PKG_BRANCH="$kbnBranch"
+
+echo " -- KIBANA_DIR='$KIBANA_DIR'"
+echo " -- XPACK_DIR='$XPACK_DIR'"
+echo " -- PARENT_DIR='$PARENT_DIR'"
+echo " -- KIBANA_PKG_BRANCH='$KIBANA_PKG_BRANCH'"
 
 ###
 ### download node
@@ -75,7 +83,6 @@ else
   else
     curl --silent "$nodeUrl" | tar -xz -C "$nodeDir" --strip-components=1
   fi
-
 fi
 
 ###
