13ba82d0cc6adc4dfd12ddba88f640452ec27659
Justin Kambic <justin.kambic@elastic.co>
2019-03-14 10:15:34 -0400

    Update empty state tests for new changes.

--- x-pack/plugins/uptime/public/components/functional/empty_state/__tests__/empty_state.test.tsx
+++ x-pack/plugins/uptime/public/components/functional/empty_state/__tests__/empty_state.test.tsx
@@ -11,7 +11,7 @@ import { EmptyState } from '../empty_state';
 describe('EmptyState component', () => {
   it('renders child components when count is truthy', () => {
     const component = shallowWithIntl(
-      <EmptyState count={1}>
+      <EmptyState basePath="" count={1}>
         <div>Foo</div>
         <div>Bar</div>
         <div>Baz</div>
@@ -21,7 +21,7 @@ describe('EmptyState component', () => {
   });
   it(`doesn't render child components when count is falsey`, () => {
     const component = mountWithIntl(
-      <EmptyState count={undefined}>
+      <EmptyState basePath="" count={undefined}>
         <div>Shouldn't be rendered</div>
       </EmptyState>
     );
@@ -29,7 +29,7 @@ describe('EmptyState component', () => {
   });
   it(`renders the message when an error occurs`, () => {
     const component = mountWithIntl(
-      <EmptyState error={'An error occurred'} count={1}>
+      <EmptyState basePath="" error={'An error occurred'} count={1}>
         <div>Shouldn't appear...</div>
       </EmptyState>
     );
@@ -37,8 +37,16 @@ describe('EmptyState component', () => {
   });
   it('renders message while loading', () => {
     const component = mountWithIntl(
-      <EmptyState loading={true} count={1}>
+      <EmptyState basePath="" count={1} loading={true}>
         <div>Shouldn't appear...</div>
+      </EmptyState>
+    );
+    expect(component).toMatchSnapshot();
+  });
+  it('renders empty state with appropriate base path', () => {
+    const component = mountWithIntl(
+      <EmptyState basePath="foo" count={0} loading={false}>
+        <div>If this is in the snapshot the test should fail</div>
       </EmptyState>
     );
     expect(component).toMatchSnapshot();
