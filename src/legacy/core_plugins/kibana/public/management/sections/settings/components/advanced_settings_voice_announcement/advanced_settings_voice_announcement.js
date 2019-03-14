344c53d807da9ce4d219a50d085fbb516caf33c7
rockfield <philipp.b@ya.ru>
2019-03-14 14:16:34 +0300

    screenreader does let the user know that contents of advanced settings are getting adjusted

--- src/legacy/core_plugins/kibana/public/management/sections/settings/components/advanced_settings_voice_announcement/advanced_settings_voice_announcement.js
+++ src/legacy/core_plugins/kibana/public/management/sections/settings/components/advanced_settings_voice_announcement/advanced_settings_voice_announcement.js
@@ -17,26 +17,39 @@
  * under the License.
  */
 
+/*
+    This component aims to insert assertive live region on the page,
+    to make sure that a screen reader announces layout changes.
+
+    Due to the fact that it has a specific way of detecting what-and-when announce
+    as well as delay of announcement (which depends on what a user is doing at the moment)
+    I place a 500ms delay of re-render the text of anouncement.
+    That time period is best fits the time of screen reader reaction.
+    That anouncement depends on what user is typying into search box as well as
+    the speed of ordinary screen reader pronouns what user is typing before start reading this anouncement.
+
+    The order of triggering functions:
+    1: React trigs the component to be updated
+    2: It places a timer and block render
+    3: The time is over
+    4: Component renders
+
+    5: If there is another component call, the timer is dropped (cleared).
+ */
+
 import React, { Component } from 'react';
-// import { FormattedMessage } from '@kbn/i18n/react';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { EuiScreenReaderOnly } from '@elastic/eui';
 
 export class AdvancedSettingsVoiceAnnouncement extends Component {
 
   constructor() {
     super();
+
+    // The state's isDealying is used to prevent component updating whether the time is not over yet.
     this.state = { isDelaying: true };
     this.delayID = null;
   }
-
-  turnDelayOff = () => {
-    this.setState({ isDelaying: false });
-  };
-
-  resetDelayOffTiming = (needsReset) => {
-    if (!needsReset) { return; }
-    clearTimeout(this.delayID);
-    this.delayID = setTimeout(() => this.turnDelayOff(), 350);
-  };
 
   static getDerivedStateFromProps(props) {
     const filteredSections = Object.values(props.settings).map(setting => setting.map(option => option.ariaName));
@@ -49,29 +62,50 @@ export class AdvancedSettingsVoiceAnnouncement extends Component {
   }
 
   shouldComponentUpdate = (nextProps, nextState) => {
+    /*
+      If a user typed smth new, we should clear the previous timer
+      and start another one + block component rendering.
+
+      When it is reset and delaying is over as well as no new string came,
+      it's ready to be rendered.
+    */
     const needsReset = nextState.query !== this.state.query;
     this.resetDelayOffTiming(needsReset);
     return !nextState.isDelaying && !needsReset;
   };
 
+  resetDelayOffTiming = (needsReset) => {
+    /*
+      Just clears prev timer and sets another before prev rings
+    */
+    if (!needsReset) { return; }
+    clearTimeout(this.delayID);
+    this.delayID = setTimeout(() => this.turnDelayOff(), 500);
+  };
+
+  turnDelayOff = () => {
+    this.setState({ isDelaying: false });
+  };
+
   render() {
     if (this.state.query === '') { return null; }
     return (
-      <div role="region" aria-live="polite">
-        asd
-        {/*<FormattedMessage
-          id="xpack.settings.AdvancedSettings.voiceAnnouncement"
-          defaultMessage="You searched for {query}. There {are} {optionLenght} {options} in {sectionLenght} {sections}"
-          values={{
-            query: this.state.query,
-            sectionLenght: this.state.filteredSections.length,
-            are: this.state.filteredOptions.length > 1 ? 'are' : 'is',
-            optionLenght: this.state.filteredOptions.length,
-            options: this.state.filteredOptions.length > 1 ? 'options' : 'option',
-            sections: this.state.filteredSections.length > 1 ? 'sections' : 'section'
-          }}
-        />*/}
-      </div>
+      <EuiScreenReaderOnly>
+        <div role="region" aria-live="polite">
+          <FormattedMessage
+            id="xpack.settings.AdvancedSettings.voiceAnnouncement"
+            defaultMessage="You searched for {query}. There {are} {optionLenght} {options} in {sectionLenght} {sections}"
+            values={{
+              query: this.state.query,
+              sectionLenght: this.state.filteredSections.length,
+              are: this.state.filteredOptions.length > 1 ? 'are' : 'is',
+              optionLenght: this.state.filteredOptions.length,
+              options: this.state.filteredOptions.length > 1 ? 'options' : 'option',
+              sections: this.state.filteredSections.length > 1 ? 'sections' : 'section'
+            }}
+          />
+        </div>
+      </EuiScreenReaderOnly>
     );
   }
 }
