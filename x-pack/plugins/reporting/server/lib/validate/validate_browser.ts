87357b3ef75b0830179007cb10e0262e61a893d4
Tim Sullivan <tsullivan@users.noreply.github.com>
2019-03-14 09:22:55 -0700

    [Reporting] Consolidate startup self-checks (#31931) (#33213)
    * [Reporting] Open test page in reporting browser self-check
    
    * comment correction
    
    * fix tests
    
    * fix test
    
    * fix tests more
    
    * remove test of open Kibana URL

--- x-pack/plugins/reporting/server/lib/validate/validate_browser.ts
+++ x-pack/plugins/reporting/server/lib/validate/validate_browser.ts
@@ -4,23 +4,27 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import * as puppeteer from 'puppeteer-core';
-import { CHROMIUM } from '../browsers/browser_types';
+import { KbnServer, Logger } from '../../../types';
+import { CHROMIUM } from '../../browsers/browser_types';
 
-export const validateBrowser = async (browserFactory: any, log: (message: string) => any) => {
+/*
+ * Validate the Reporting headless browser can launch, and that it can connect
+ * to the locally running Kibana instance.
+ */
+export const validateBrowser = async (server: KbnServer, browserFactory: any, logger: Logger) => {
   if (browserFactory.type === CHROMIUM) {
     return browserFactory
       .test(
         {
-          viewport: {
-            width: 800,
-            height: 600,
-          },
+          viewport: { width: 800, height: 600 },
         },
-        log
+        logger
       )
       .then((browser: puppeteer.Browser | null) => {
         if (browser && browser.close) {
           browser.close();
+        } else {
+          throw new Error('Could not close browser client handle!');
         }
       });
   }
