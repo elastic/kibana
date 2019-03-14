88cbca9806ba919246272474fa9a0eb967622308
Rudolf Meijering <skaapgif@gmail.com>
2019-03-14 15:13:17 -0700

    Remove src/type_exports.ts

--- kibana.d.ts
+++ kibana.d.ts
@@ -20,7 +20,9 @@
 /**
  * All exports from TS source files (where the implementation is actually done in TS).
  */
-export * from './target/types/type_exports';
+export * from './target/types/public';
+export * from './target/types/server';
+
 /**
  * All exports from TS ambient definitions (where types are added for JS source in a .d.ts file).
  */
