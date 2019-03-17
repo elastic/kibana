3503393d3809f983262313c91affe0dc8dda087d
Liza Katz <lizka.k@gmail.com>
2019-03-17 09:54:27 +0200

    New platform cleanup (#33244)
    * delete file upload directive
    
    * delte focus on directive
    
    * delete confirm-click directive
    
    * delete input-base-sixty-four directive
    
    * delete input-date-time directive
    
    *  delete string-to-number directive
    
    *  delete validate-index-pattern directive
    
    *  delete validate-lowercase directive
    
    *  delete ui-select-focus-on directive
    
    *  delete dismiss-on-timeout directive
    
    *  delete accordion angular-bootstrap directive
    
    *  delete collapse angular-bootstrap directive
    
    *  delete collapse angular-bootstrap directive
    
    * remove elastic-textarea directive
    
    * remove color-picker directive
    
    * remove rating directive
    
    * remove btn-checkbox directive
    
    * removed angular dateparser service
    
    * remove unused string

--- src/legacy/ui/public/angular-bootstrap/buttons/buttons.js
+++ src/legacy/ui/public/angular-bootstrap/buttons/buttons.js
@@ -35,40 +35,4 @@ angular.module('ui.bootstrap.buttons', [])
       });
     }
   };
-})
-
-.directive('btnCheckbox', function () {
-  return {
-    require: ['btnCheckbox', 'ngModel'],
-    controller: 'ButtonsController',
-    link: function (scope, element, attrs, ctrls) {
-      var buttonsCtrl = ctrls[0], ngModelCtrl = ctrls[1];
-
-      function getTrueValue() {
-        return getCheckboxValue(attrs.btnCheckboxTrue, true);
-      }
-
-      function getFalseValue() {
-        return getCheckboxValue(attrs.btnCheckboxFalse, false);
-      }
-
-      function getCheckboxValue(attributeValue, defaultValue) {
-        var val = scope.$eval(attributeValue);
-        return angular.isDefined(val) ? val : defaultValue;
-      }
-
-      //model -> UI
-      ngModelCtrl.$render = function () {
-        element.toggleClass(buttonsCtrl.activeClass, angular.equals(ngModelCtrl.$modelValue, getTrueValue()));
-      };
-
-      //ui->model
-      element.bind(buttonsCtrl.toggleEvent, function () {
-        scope.$apply(function () {
-          ngModelCtrl.$setViewValue(element.hasClass(buttonsCtrl.activeClass) ? getFalseValue() : getTrueValue());
-          ngModelCtrl.$render();
-        });
-      });
-    }
-  };
 });
