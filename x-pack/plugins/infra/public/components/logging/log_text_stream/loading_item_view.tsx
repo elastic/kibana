elastic/kibana

commit ae754ac60769288cd953660ab2ceee3e8286078b

Upgrades EUI to 9.x (#32009)
* Fixed a simple argument bug and removed infra date picker EUI types

* Fixes for EUI date picker types

* eui_8.0.0

* fix type errors in query_bar

* Small changes for EUI types

* Updates EUI to 9.0.0 and removes @types/react-datepicker as it now ships with EUI

* Updates to EUI 9.0.1 and removes duplicate types in infra eui.d.ts

* ts-ignore applied to ongoing type error with styled components and EUI

* Changes EuiProgress props to avoid TS errors

* Updates EUI 9.0 snapshots

* Updates kibana root snapshots for EUI 9.0 upgrade

* Update detail_panel.test.js for EUI changes

* Updated functioanl and unit tests to properly inspect EuiTableRowCell rendered values

* Fix docs_level_security_roles.js func tests

* Update EUI to 9.0.2

* Fixed failing snapshot for EUI icon default prop

x-pack/plugins/infra/public/components/logging/log_text_stream/loading_item_view.tsx
@@ -8,7 +8,6 @@ import { EuiButtonEmpty, EuiIcon, EuiProgress, EuiText } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import * as React from 'react';
 import styled from 'styled-components';
-
 import { RelativeTime } from './relative_time';
 
 interface LogTextStreamLoadingItemViewProps {
@@ -117,16 +116,28 @@ interface ProgressEntryProps {
 class ProgressEntry extends React.PureComponent<ProgressEntryProps, {}> {
   public render() {
     const { alignment, children, className, color, isLoading } = this.props;
+    const progressProps = {};
+    // NOTE: styled-components seems to make all props in EuiProgress required, so this
+    // style attribute hacking replaces styled-components here for now until that can be fixed
+    // see: https://github.com/elastic/eui/issues/1655
+    const alignmentStyle =
+      alignment === 'top' ? { top: 0, bottom: 'initial' } : { top: 'initial', bottom: 0 };
+
+    if (isLoading) {
+      // @ts-ignore
+      progressProps.max = 1;
+      // @ts-ignore
+      progressProps.value = 1;
+    }
 
     return (
       <ProgressEntryWrapper className={className}>
-        <AlignedProgress
-          alignment={alignment}
+        <EuiProgress
+          style={alignmentStyle}
           color={color}
-          max={isLoading ? undefined : 1}
           size="xs"
-          value={isLoading ? undefined : 1}
           position="absolute"
+          {...progressProps}
         />
         {children}
       </ProgressEntryWrapper>
@@ -143,11 +154,4 @@ const ProgressEntryWrapper = styled.div`
 
 const ProgressMessage = styled.div`
   padding: 8px 16px;
-`;
-
-const AlignedProgress = styled(EuiProgress).attrs<{
-  alignment: 'top' | 'bottom';
-}>({})`
-  top: ${props => (props.alignment === 'top' ? 0 : 'initial')};
-  bottom: ${props => (props.alignment === 'top' ? 'initial' : 0)};
 `;

commit 76eff0afab3e0a9fd273e625fc11c6ae4745dc0a

[Logs UI] Revert unintended inversion of loading state meaning (#33152)
This fixes a problem introduced with #32009, in which the meaning of the `isLoading` property for the `<ProgressEntry>` component was inverted.

x-pack/plugins/infra/public/components/logging/log_text_stream/loading_item_view.tsx
@@ -115,19 +115,11 @@ interface ProgressEntryProps {
 class ProgressEntry extends React.PureComponent<ProgressEntryProps, {}> {
   public render() {
     const { alignment, children, className, color, isLoading } = this.props;
-    const progressProps = {};
     // NOTE: styled-components seems to make all props in EuiProgress required, so this
     // style attribute hacking replaces styled-components here for now until that can be fixed
     // see: https://github.com/elastic/eui/issues/1655
     const alignmentStyle =
       alignment === 'top' ? { top: 0, bottom: 'initial' } : { top: 'initial', bottom: 0 };
-
-    if (isLoading) {
-      // @ts-ignore
-      progressProps.max = 1;
-      // @ts-ignore
-      progressProps.value = 1;
-    }
 
     return (
       <ProgressEntryWrapper className={className}>
@@ -136,7 +128,7 @@ class ProgressEntry extends React.PureComponent<ProgressEntryProps, {}> {
           color={color}
           size="xs"
           position="absolute"
-          {...progressProps}
+          {...(!isLoading ? { max: 1, value: 1 } : {})}
         />
         {children}
       </ProgressEntryWrapper>