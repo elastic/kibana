dc64b4c8b04aabebc1b3caa919457f941ff8b207
Sébastien <sebastien.loix@elastic.co>
2019-03-18 08:44:21 +0100

    [Rollup] Re-enable api integration tests + execute CI 20 times

--- test/scripts/jenkins_ci_group.sh
+++ test/scripts/jenkins_ci_group.sh
@@ -13,14 +13,17 @@ function report {
 
 trap report EXIT
 
-"$(FORCE_COLOR=0 yarn bin)/grunt" functionalTests:ensureAllTestsInCiGroup;
+source src/dev/ci_setup/checkout_sibling_es.sh
+
+# "$(FORCE_COLOR=0 yarn bin)/grunt" functionalTests:ensureAllTestsInCiGroup;
 
 node scripts/build --debug --oss;
 
 export TEST_BROWSER_HEADLESS=1
+export TEST_ES_FROM=${TEST_ES_FROM:-source}
 
-"$(FORCE_COLOR=0 yarn bin)/grunt" "run:functionalTests_ciGroup${CI_GROUP}";
+"$(FORCE_COLOR=0 yarn bin)/grunt" "run:functionalTests_ciGroup${CI_GROUP}" --from=source;
 
 if [ "$CI_GROUP" == "1" ]; then
-  "$(FORCE_COLOR=0 yarn bin)/grunt" run:pluginFunctionalTestsRelease;
+  "$(FORCE_COLOR=0 yarn bin)/grunt" run:pluginFunctionalTestsRelease --from=source;
 fi
