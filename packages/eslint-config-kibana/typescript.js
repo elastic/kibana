3779da17c7d8b4723bd25a0dc147bac3d5ab1d96
Tiago Costa <tiagoffcc@hotmail.com>
2019-03-19 20:39:28 +0000

    fix(NA): apply recommend eslint typescript rule.s

--- packages/eslint-config-kibana/typescript.js
+++ packages/eslint-config-kibana/typescript.js
@@ -1,20 +1,16 @@
-// NOTE: this is the configuration to apply the typescript eslint parser
+// NOTE: This is the configuration to apply the typescript eslint parser
 // in order to lint typescript files with eslint.
-// Some IDEs could not yet ran eslint with the correct extensions
+// Some IDEs could not be running eslint with the correct extensions yet
 // as this package was moved from typescript-eslint-parser to @typescript-eslint/parser
 //
-// For example in IntelliJ this would be fixed on 2019.1 to be launched soon.
-// In order to have it working now we should add the extensions `ts,tsx` to the option
+// As a matter of example in IntelliJ IDEA this would be fixed on 2019.1 release to be launched soon.
+// In order to have it working for now, we should add the extensions `ts,tsx` to the option
 // `eslint.additional.file.extensions` that can be found on Help > Find Action > Registry
-// More info: https://intellij-support.jetbrains.com/hc/en-us/community/posts/115000225170-ESLint-and-ts-Typescript-files
+// To read bore about it please visit
+// https://intellij-support.jetbrains.com/hc/en-us/community/posts/115000225170-ESLint-and-ts-Typescript-files
 
-const typescriptEslintRecommended = require('@typescript-eslint/eslint-plugin').configs.recommended;
+const typescriptEslintRecommendedRules = require('@typescript-eslint/eslint-plugin').configs.recommended.rules;
 const eslintConfigPrettierTypescriptEslintRules = require('eslint-config-prettier/@typescript-eslint').rules;
-
-// Remove every non @typescript-eslint rules from typescript eslint plugin recommended rules
-const typescriptEslintRecommendedSanitizedRules = Object.keys(typescriptEslintRecommended.rules)
-  .filter((key) => !!key.includes('@typescript-eslint'))
-  .reduce((obj, key) => ({ ...obj, [key]: typescriptEslintRecommended.rules[key] }), {});
 
 module.exports = {
   overrides: [
@@ -33,30 +29,14 @@ module.exports = {
       // For now we are using an workaround to create
       // those extended rules arrays
       rules: Object.assign(
-        typescriptEslintRecommendedSanitizedRules,
+        typescriptEslintRecommendedRules,
         {
-          "@typescript-eslint/adjacent-overload-signatures": "error",
-          "@typescript-eslint/array-type": "error",
-          "@typescript-eslint/ban-types": "error",
-          "@typescript-eslint/class-name-casing": "error",
-          "@typescript-eslint/indent": "error",
-          "@typescript-eslint/interface-name-prefix": "error",
-          "@typescript-eslint/member-delimiter-style": "error",
-          "@typescript-eslint/no-angle-bracket-type-assertion": "error",
-          "@typescript-eslint/no-array-constructor": "error",
-          "@typescript-eslint/no-empty-interface": "error",
-          "@typescript-eslint/no-inferrable-types": "error",
-          "@typescript-eslint/no-misused-new": "error",
-          "@typescript-eslint/no-namespace": "error",
-          "@typescript-eslint/no-non-null-assertion": "error",
-          "@typescript-eslint/no-object-literal-type-assertion": "error",
-          "@typescript-eslint/no-parameter-properties": "error",
-          "@typescript-eslint/no-triple-slash-reference": "error",
-          "@typescript-eslint/no-unused-vars": "warn",
-          "@typescript-eslint/no-var-requires": "error",
-          "@typescript-eslint/prefer-interface": "error",
-          "@typescript-eslint/prefer-namespace-keyword": "error",
-          "@typescript-eslint/type-annotation-spacing": "error"
+          "@typescript-eslint/camelcase": [ 'error', { properties: 'never' } ],
+          "@typescript-eslint/indent": [ 'error', 2, { SwitchCase: 1 } ],
+          "@typescript-eslint/no-unused-vars": [ 'error' ],
+          "@typescript-eslint/no-use-before-define": [ 'error', 'nofunc' ],
+          "@typescript-eslint/explicit-function-return-type": false,
+          "@typescript-eslint/no-explicit-any": false,
         },
         eslintConfigPrettierTypescriptEslintRules,
       )
