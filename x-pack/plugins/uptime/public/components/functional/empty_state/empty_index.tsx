1dfc4a8812f7a86ebe692476904ccf2d816d17a9
Justin Kambic <justin.kambic@elastic.co>
2019-03-20 14:10:08 -0400

    [Uptime] Change copy on empty state component (#33044)
    * Change copy on Uptime empty state component to use "collecting" instead of "logging".
    
    * Update jest snapshot for changed component.
    
    * Update empty prompt link to send users to Add Data tutorial.
    
    * Update empty state tests for new changes.

--- x-pack/plugins/uptime/public/components/functional/empty_state/empty_index.tsx
+++ x-pack/plugins/uptime/public/components/functional/empty_state/empty_index.tsx
@@ -42,7 +42,7 @@ export const EmptyIndex = ({ basePath }: EmptyIndexProps) => (
               <p>
                 <FormattedMessage
                   id="xpack.uptime.emptyState.configureHeartbeatToGetStartedMessage"
-                  defaultMessage="{configureHeartbeatLink} to start logging uptime data."
+                  defaultMessage="{configureHeartbeatLink} to start collecting uptime data."
                   values={{
                     configureHeartbeatLink: (
                       <EuiLink
