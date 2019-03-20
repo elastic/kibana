345bbf2fdef3d6a4595adeb84983403c75404447
FrankHassanabad <frank.hassanabad@elastic.co>
2019-03-19 22:32:24 -0600

    Updated per comments

--- x-pack/plugins/secops/public/components/timeline/body/renderers/auditd_ended_session_row_renderer.test.tsx
+++ x-pack/plugins/secops/public/components/timeline/body/renderers/auditd_ended_session_row_renderer.test.tsx
@@ -26,7 +26,7 @@ describe('auditdEndedSessionRowRenderer', () => {
   });
 
   test('renders correctly against snapshot', () => {
-    // I cannot and do not want to use the mocks for the snapshot tests as they are too heavy
+    // I cannot and do not want to use the BrowserFields mocks for the snapshot tests as they are too heavy
     const browserFields: BrowserFields = {};
     const children = auditdEndedSessionRowRenderer.renderRow({
       browserFields,
