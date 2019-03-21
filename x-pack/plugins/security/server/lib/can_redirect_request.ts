99de58daa27075a2a740df1c716eae1d1d21b1dc
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2019-03-21 16:56:33 +0100

    Typesciptify SAML Authentication provider. (#33559)

--- x-pack/plugins/security/server/lib/can_redirect_request.ts
+++ x-pack/plugins/security/server/lib/can_redirect_request.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { Request } from 'hapi';
 import { contains, get, has } from 'lodash';
 
 const ROUTE_TAG_API = 'api';
@@ -13,10 +14,9 @@ const KIBANA_VERSION_HEADER = 'kbn-version';
 /**
  * Checks whether we can reply to the request with redirect response. We can do that
  * only for non-AJAX and non-API requests.
- * @param {Hapi.Request} request HapiJS request instance to check redirection possibility for.
- * @returns {boolean}
+ * @param request HapiJS request instance to check redirection possibility for.
  */
-export function canRedirectRequest(request) {
+export function canRedirectRequest(request: Request) {
   const hasVersionHeader = has(request.raw.req.headers, KIBANA_VERSION_HEADER);
   const hasXsrfHeader = has(request.raw.req.headers, KIBANA_XSRF_HEADER);
 
