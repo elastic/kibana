633ec06488e140098239ee27370b1c8f552b7662
kobelb <brandon.kobel@elastic.co>
2019-03-21 13:41:01 -0700

    Adding JSDoc's for the Actions class and specifically the `allHack` action

--- x-pack/plugins/security/server/lib/authorization/actions/actions.ts
+++ x-pack/plugins/security/server/lib/authorization/actions/actions.ts
@@ -10,7 +10,16 @@ import { SavedObjectActions } from './saved_object';
 import { SpaceActions } from './space';
 import { UIActions } from './ui';
 
+/** Actions are used to create the "actions" that are associated with Elasticsearch's
+ * application privileges, and are used to perform the authorization checks implemented
+ * by the various `checkPrivilegesWithRequest` derivatives
+ */
 export class Actions {
+  /**
+   * The allHack action is used to differentiate the `all` privilege from the `read` privilege
+   * for those applications which register the same set of actions for both privileges. This is a
+   * temporary hack until we remove this assumption in the role management UI
+   */
   public readonly allHack = 'allHack:';
 
   public readonly api = new ApiActions();
