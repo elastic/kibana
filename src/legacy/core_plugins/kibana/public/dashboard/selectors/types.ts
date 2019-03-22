8c17ae9276762aa383268803f020b45be441c403
Matthew Kime <matt@mattki.me>
2019-03-22 12:32:40 -0500

    Update types.ts

--- src/legacy/core_plugins/kibana/public/dashboard/selectors/types.ts
+++ src/legacy/core_plugins/kibana/public/dashboard/selectors/types.ts
@@ -19,7 +19,6 @@
 
 import { EmbeddableMetadata, Filters, Query, RefreshConfig, TimeRange } from 'ui/embeddable';
 import { DashboardViewMode } from '../dashboard_view_mode';
-import { GridData } from '../types';
 
 export type DashboardViewMode = DashboardViewMode;
 export interface ViewState {
