69cd06d64a2d58237295ad5e9e4a8c10791e9e5c
maryia-lapata <mary.lopato@gmail.com>
2019-03-22 18:24:40 +0300

    Update unit test since wrapped React component haven't compiled yet

--- src/legacy/ui/public/agg_types/__tests__/buckets/date_histogram/_editor.js
+++ src/legacy/ui/public/agg_types/__tests__/buckets/date_histogram/_editor.js
@@ -108,7 +108,7 @@ describe('editor', function () {
 
       expect(params).to.have.property('field');
       expect(params.field).to.have.property('$el');
-      expect(params.field.modelValue()).to.be(field);
+      expect($scope.agg.params.field).to.be(field);
     });
 
     it('renders the interval editor', function () {
