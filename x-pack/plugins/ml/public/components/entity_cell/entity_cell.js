4dc110f28c87e8a1bc6044e9a4508223749170e7
Melissa Alvarez <melissa.alvarez@elastic.co>
2019-03-25 15:32:51 -0400

    Update entityCell design for table+InfluencerList

--- x-pack/plugins/ml/public/components/entity_cell/entity_cell.js
+++ x-pack/plugins/ml/public/components/entity_cell/entity_cell.js
@@ -17,6 +17,51 @@ import {
 } from '@elastic/eui';
 import { FormattedMessage, injectI18n } from '@kbn/i18n/react';
 
+
+function getAddFilter({ entityName, entityValue, filter, intl }) {
+  return (
+    <EuiToolTip
+      content={<FormattedMessage
+        id="xpack.ml.anomaliesTable.entityCell.addFilterTooltip"
+        defaultMessage="Add filter"
+      />}
+    >
+      <EuiButtonIcon
+        size="xs"
+        className="filter-button"
+        onClick={() => filter(entityName, entityValue, '+')}
+        iconType="plusInCircle"
+        aria-label={intl.formatMessage({
+          id: 'xpack.ml.anomaliesTable.entityCell.addFilterAriaLabel',
+          defaultMessage: 'Add filter'
+        })}
+      />
+    </EuiToolTip>
+  );
+}
+
+function getRemoveFilter({ entityName, entityValue, filter, intl }) {
+  return (
+    <EuiToolTip
+      content={<FormattedMessage
+        id="xpack.ml.anomaliesTable.entityCell.removeFilterTooltip"
+        defaultMessage="Remove filter"
+      />}
+    >
+      <EuiButtonIcon
+        size="xs"
+        className="filter-button"
+        onClick={() => filter(entityName, entityValue, '-')}
+        iconType="minusInCircle"
+        aria-label={intl.formatMessage({
+          id: 'xpack.ml.anomaliesTable.entityCell.removeFilterAriaLabel',
+          defaultMessage: 'Remove filter'
+        })}
+      />
+    </EuiToolTip>
+  );
+}
+
 /*
  * Component for rendering an entity in the anomalies table, displaying the value
  * of the entity, such as a partitioning or influencer field value, and optionally links for
@@ -32,66 +77,50 @@ export const EntityCell = injectI18n(function EntityCell({
   const valueText = (entityName !== 'mlcategory') ? entityValue : `mlcategory ${entityValue}`;
   const textStyle = { maxWidth: '100%' };
   const textWrapperClass = (wrapText ? 'field-value-long' : 'field-value-short');
-  return (
-    <EuiFlexGroup
-      direction="row"
-      alignItems="center"
-      gutterSize="none"
-    >
-      <EuiFlexItem grow={false} style={textStyle}>
-        <EuiText
-          size="xs"
-          className={textWrapperClass}
-        >
-          {valueText}
-        </EuiText>
-      </EuiFlexItem>
-      {filter !== undefined && entityName !== undefined && entityValue !== undefined &&
-        <EuiFlexItem grow={false}>
-          <EuiFlexGroup direction="row" alignItems="center" gutterSize="none">
-            <EuiFlexItem grow={false}>
-              <EuiToolTip
-                content={<FormattedMessage
-                  id="xpack.ml.anomaliesTable.entityCell.addFilterTooltip"
-                  defaultMessage="Add filter"
-                />}
-              >
-                <EuiButtonIcon
-                  size="xs"
-                  className="filter-button"
-                  onClick={() => filter(entityName, entityValue, '+')}
-                  iconType="plusInCircle"
-                  aria-label={intl.formatMessage({
-                    id: 'xpack.ml.anomaliesTable.entityCell.addFilterAriaLabel',
-                    defaultMessage: 'Add filter'
-                  })}
-                />
-              </EuiToolTip>
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <EuiToolTip
-                content={<FormattedMessage
-                  id="xpack.ml.anomaliesTable.entityCell.removeFilterTooltip"
-                  defaultMessage="Remove filter"
-                />}
-              >
-                <EuiButtonIcon
-                  size="xs"
-                  className="filter-button"
-                  onClick={() => filter(entityName, entityValue, '-')}
-                  iconType="minusInCircle"
-                  aria-label={intl.formatMessage({
-                    id: 'xpack.ml.anomaliesTable.entityCell.removeFilterAriaLabel',
-                    defaultMessage: 'Remove filter'
-                  })}
-                />
-              </EuiToolTip>
-            </EuiFlexItem>
-          </EuiFlexGroup>
+  const shouldDisplayIcons = (filter !== undefined && entityName !== undefined && entityValue !== undefined);
+
+  if (wrapText === true) {
+    return (
+      <div>
+        <span className={textWrapperClass}>{valueText}</span>
+        {shouldDisplayIcons &&
+          <React.Fragment>
+            {getAddFilter({ entityName, entityValue, filter, intl })}
+            {getRemoveFilter({ entityName, entityValue, filter, intl })}
+          </React.Fragment>
+        }
+      </div>
+    );
+  } else {
+    return (
+      <EuiFlexGroup
+        direction="row"
+        alignItems="center"
+        gutterSize="none"
+      >
+        <EuiFlexItem grow={false} style={textStyle}>
+          <EuiText
+            size="xs"
+            className={textWrapperClass}
+          >
+            {valueText}
+          </EuiText>
         </EuiFlexItem>
-      }
-    </EuiFlexGroup>
-  );
+        {shouldDisplayIcons &&
+          <EuiFlexItem grow={false}>
+            <EuiFlexGroup direction="row" alignItems="center" gutterSize="none">
+              <EuiFlexItem grow={false}>
+                {getAddFilter({ entityName, entityValue, filter, intl })}
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                {getRemoveFilter({ entityName, entityValue, filter, intl })}
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        }
+      </EuiFlexGroup>
+    );
+  }
 });
 
 EntityCell.WrappedComponent.propTypes = {
