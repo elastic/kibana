a989cc19418f00d3a35e6e37bfff4518a7dcf578
Angela Chuang <yi-chun.chuang@elastic.co>
2019-03-25 20:29:17 +0800

    isolate card items generator

--- x-pack/plugins/secops/public/components/page/network/kpi_network/index.tsx
+++ x-pack/plugins/secops/public/components/page/network/kpi_network/index.tsx
@@ -4,20 +4,13 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import {
-  // @ts-ignore
-  EuiCard,
-  EuiFlexGroup,
-  EuiFlexItem,
-} from '@elastic/eui';
-import { EuiLoadingSpinner } from '@elastic/eui';
-import numeral from '@elastic/numeral';
+import { EuiFlexGroup } from '@elastic/eui';
 import { get } from 'lodash/fp';
 import React from 'react';
 import { pure } from 'recompose';
 
+import { CardItem, CardItems, CardItemsComponent } from '../../../../components/card_items';
 import { KpiNetworkData } from '../../../../graphql/types';
-import { getEmptyTagValue } from '../../../empty_value';
 
 import * as i18n from './translations';
 
@@ -26,93 +19,45 @@ interface KpiNetworkProps {
   loading: boolean;
 }
 
-interface CardItemProps {
-  field: NestedCardItemProps[] | string;
-  description: string;
-  data?: KpiNetworkData;
-}
-
-interface NestedCardItemProps {
-  field: string;
-  description: string;
-  data?: KpiNetworkData;
-}
-
-interface CardItemListProps extends CardItemProps {
-  isLoading: boolean;
-  key: string;
-}
-
-const createTitle = (isLoading: boolean, field: string, data: KpiNetworkData) => {
-  const title: number = get(field, data);
-  return isLoading ? (
-    <EuiLoadingSpinner size="m" />
-  ) : title != null ? (
-    numeral(title).format('0,0')
-  ) : (
-    getEmptyTagValue()
-  );
-};
-
-const createMultipleTitles = (
-  isLoading: boolean,
-  field: NestedCardItemProps[],
-  data: KpiNetworkData
-) => {
-  return (
-    <div>
-      {field.map(content => {
-        const secondaryField: string = get('field', content);
-        return (
-          <EuiFlexGroup key={content.description} gutterSize="s" justifyContent="spaceBetween">
-            <EuiFlexItem grow={false}>{createTitle(isLoading, secondaryField, data)}</EuiFlexItem>
-            <EuiFlexItem grow={false}>{content.description}</EuiFlexItem>
-          </EuiFlexGroup>
-        );
-      })}
-    </div>
-  );
-};
-
-const CardItem = pure<CardItemListProps>(({ field, description, isLoading, data }) => {
-  return (
-    <EuiFlexItem key={description}>
+const fieldTitleMapping: Readonly<CardItems[]> = [
+  {
+    fields: [
       {
-        <EuiCard
-          title={
-            !Array.isArray(field)
-              ? createTitle(isLoading, field, data!)
-              : createMultipleTitles(isLoading, field, data!)
-          }
-          description={description}
-        />
-      }
-    </EuiFlexItem>
-  );
-});
-
-const fieldTitleMapping: CardItemProps[] = [
-  {
-    field: 'networkEvents',
-    description: i18n.NETWORK_EVENTS,
+        key: 'networkEvents',
+        description: i18n.NETWORK_EVENTS,
+        value: null,
+      },
+    ],
   },
   {
-    field: 'uniqueFlowId',
-    description: i18n.UNIQUE_ID,
-  },
-  {
-    field: 'activeAgents',
-    description: i18n.ACTIVE_AGENTS,
-  },
-  {
-    field: [
+    fields: [
       {
-        field: 'uniqueSourcePrivateIps',
+        key: 'uniqueFlowId',
+        description: i18n.UNIQUE_ID,
+        value: null,
+      },
+    ],
+  },
+  {
+    fields: [
+      {
+        key: 'activeAgents',
+        description: i18n.ACTIVE_AGENTS,
+        value: null,
+      },
+    ],
+  },
+  {
+    fields: [
+      {
+        key: 'uniqueSourcePrivateIps',
         description: i18n.UNIQUE_SOURCE_PRIVATE_IPS,
+        value: null,
       },
       {
-        field: 'uniqueDestinationPrivateIps',
+        key: 'uniqueDestinationPrivateIps',
         description: i18n.UNIQUE_DESTINATION_PRIVATE_IPS,
+        value: null,
       },
     ],
     description: i18n.UNIQUE_PRIVATE_IPS,
@@ -122,15 +67,17 @@ const fieldTitleMapping: CardItemProps[] = [
 export const KpiNetworkComponent = pure<KpiNetworkProps>(({ data, loading }) => {
   return (
     <EuiFlexGroup>
-      {fieldTitleMapping.map(card => (
-        <CardItem
-          key={`kpi-summary-${card.description}`}
+      {fieldTitleMapping.map((card, idx) => (
+        <CardItemsComponent
+          key={`kpi-network-summary-${card.description}-${idx}`}
           isLoading={loading}
           description={card.description}
-          field={card.field}
-          data={data}
+          fields={addValueToFields(card.fields, data)}
         />
       ))}
     </EuiFlexGroup>
   );
 });
+
+const addValueToFields = (fields: CardItem[], data: KpiNetworkData): CardItem[] =>
+  fields.map(field => ({ ...field, value: get(field.key, data) }));
--- x-pack/plugins/secops/server/graphql/types.ts
+++ x-pack/plugins/secops/server/graphql/types.ts
@@ -3321,7 +3321,13 @@ export namespace KpiNetworkDataResolvers {
 
     activeAgents?: ActiveAgentsResolver<number | null, TypeParent, Context>;
 
-    uniquePrivateIps?: UniquePrivateIpsResolver<number | null, TypeParent, Context>;
+    uniqueSourcePrivateIps?: UniqueSourcePrivateIpsResolver<number | null, TypeParent, Context>;
+
+    uniqueDestinationPrivateIps?: UniqueDestinationPrivateIpsResolver<
+      number | null,
+      TypeParent,
+      Context
+    >;
   }
 
   export type NetworkEventsResolver<
@@ -3339,7 +3345,12 @@ export namespace KpiNetworkDataResolvers {
     Parent = KpiNetworkData,
     Context = SecOpsContext
   > = Resolver<R, Parent, Context>;
-  export type UniquePrivateIpsResolver<
+  export type UniqueSourcePrivateIpsResolver<
+    R = number | null,
+    Parent = KpiNetworkData,
+    Context = SecOpsContext
+  > = Resolver<R, Parent, Context>;
+  export type UniqueDestinationPrivateIpsResolver<
     R = number | null,
     Parent = KpiNetworkData,
     Context = SecOpsContext
