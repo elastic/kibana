ecc67ecc0622515ec4bd43b2efae5936c12ecfc5
Josh Dover <me@joshdover.com>
2019-03-28 16:00:25 -0500

    Fix regex to select paths correctly. (#34106) (#34110)

--- src/dev/jest/config.js
+++ src/dev/jest/config.js
@@ -44,7 +44,7 @@ export default {
     '!packages/kbn-ui-framework/src/services/**/*/index.js',
   ],
   moduleNameMapper: {
-    '^plugins/(.*)/(.*)': '<rootDir>/src/legacy/core_plugins/$1/public/$2',
+    '^plugins/([^\/.]*)/(.*)': '<rootDir>/src/legacy/core_plugins/$1/public/$2',
     '^ui/(.*)': '<rootDir>/src/legacy/ui/public/$1',
     '^uiExports/(.*)': '<rootDir>/src/dev/jest/mocks/file_mock.js',
     '^test_utils/(.*)': '<rootDir>/src/test_utils/public/$1',
