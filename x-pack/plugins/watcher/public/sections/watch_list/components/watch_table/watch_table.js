b5a19e94a59b3d952de2066d6f34f880ce87af40
Liza Katz <liza.katz@elastic.co>
2019-03-28 14:52:30 +0200

    Move kbn tooltip directive to watcher

--- x-pack/plugins/watcher/public/sections/watch_list/components/watch_table/watch_table.js
+++ x-pack/plugins/watcher/public/sections/watch_list/components/watch_table/watch_table.js
@@ -7,6 +7,7 @@
 import _ from 'lodash';
 import { uiModules } from 'ui/modules';
 import 'plugins/watcher/directives/check_box';
+import 'plugins/watcher/directives/tooltip';
 import 'ui/sortable_column';
 import template from './watch_table.html';
 import 'plugins/watcher/components/watch_state_icon';
