5e6b81a3ca5b43604679cd014daadb322a00213f
Clint Andrew Hall <clint.hall@elastic.co>
2019-03-28 09:19:31 -0500

    [canvas] Color fixes + Storybook 5

--- x-pack/plugins/canvas/public/components/page_config/page_config.js
+++ x-pack/plugins/canvas/public/components/page_config/page_config.js
@@ -7,7 +7,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { EuiCard, EuiFormRow, EuiTitle, EuiSpacer, EuiSelect } from '@elastic/eui';
-import { ColorPickerMini } from '../color_picker_mini';
+import { WorkpadColorPicker } from '../workpad_color_picker';
 
 export const PageConfig = ({
   pageIndex,
@@ -24,7 +24,7 @@ export const PageConfig = ({
       </EuiTitle>
       <EuiSpacer size="m" />
       <EuiFormRow label="Background color">
-        <ColorPickerMini onChange={setBackground} value={background} />
+        <WorkpadColorPicker onChange={setBackground} value={background} />
       </EuiFormRow>
       {/* No need to show the transition for the first page because transitions occur when
         switching between pages (for example, when moving from the first page to the second
