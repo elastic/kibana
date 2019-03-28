1e564765102722fc61016f30134d83167c742c81
Maryia Lapata <mary.lopato@gmail.com>
2019-03-28 15:43:14 +0300

    [Vis: Default editor] EUIficate agg-select (#31892) (#34049)
    * EUIficate agg-select
    
    * Improve validation; update TS
    
    * Apply styles for helpLink
    
    * Fix unit test
    
    * Update functional tests
    
    * Adjust comboBox service to chose the item where the text mates exactly
    
    * Update vis page object
    
    * Add default value for agg
    
    * Move aggs grouping function to a separate file
    
    * Use labelAppend prop for help link node
    
    * Add watcher for aggType to manage to discard changes
    
    * Add default value for agg type title
    
    * Fix defining selected option when aggType is defined
    
    * Fix validation issues
    
    * Remove a bootstrap specific class
    
    * Change css selector in test
    
    * Update according to SASS guidelines
    
    * Update functinal comboBox service
    
    * Added check for undefined
    
    * Add jsdoc for groupAggregationsBy function
    
    * Add unit tests for groupAggregationsBy
    
    * Move setValidity invocation to DefaultEditorAggSelect component
    
    * Wrap setValidity into useEffect due to react warning when select is cleaned at the first time
    
    * Move help link definition to select component

---
+++ src/legacy/ui/public/vis/editors/default/components/default_editor_agg_select.tsx
@@ -0,0 +1,124 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { get, has, isFunction } from 'lodash';
+import React, { useEffect } from 'react';
+
+import { EuiComboBox, EuiFormRow, EuiLink } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { AggType } from 'ui/agg_types';
+import { AggConfig } from 'ui/vis/agg_config';
+import { documentationLinks } from '../../../../documentation_links/documentation_links';
+import { ComboBoxGroupedOption } from '../default_editor_utils';
+
+interface DefaultEditorAggSelectProps {
+  agg: AggConfig;
+  value: AggType;
+  setValue: (aggType: AggType) => void;
+  aggTypeOptions: AggType[];
+  isSubAggregation: boolean;
+  isSelectInvalid: boolean;
+  setTouched: () => void;
+  setValidity: (isValid: boolean) => void;
+}
+
+function DefaultEditorAggSelect({
+  agg = {},
+  value = { title: '' },
+  setValue,
+  aggTypeOptions = [],
+  isSelectInvalid,
+  isSubAggregation,
+  setTouched,
+  setValidity,
+}: DefaultEditorAggSelectProps) {
+  const isAggTypeDefined = value && Boolean(value.title);
+  const selectedOptions: ComboBoxGroupedOption[] = isAggTypeDefined
+    ? [{ label: value.title, value }]
+    : [];
+
+  const label = isSubAggregation ? (
+    <FormattedMessage
+      id="common.ui.vis.defaultEditor.aggSelect.subAggregationLabel"
+      defaultMessage="Sub aggregation"
+    />
+  ) : (
+    <FormattedMessage
+      id="common.ui.vis.defaultEditor.aggSelect.aggregationLabel"
+      defaultMessage="Aggregation"
+    />
+  );
+
+  let aggHelpLink = null;
+  if (has(agg, 'type.name')) {
+    aggHelpLink = get(documentationLinks, ['aggs', agg.type.name]);
+  }
+
+  const helpLink = isAggTypeDefined && aggHelpLink && (
+    <EuiLink
+      href={aggHelpLink}
+      target="_blank"
+      rel="noopener"
+      className="visEditorAggSelect__helpLink"
+    >
+      <FormattedMessage
+        id="common.ui.vis.defaultEditor.aggSelect.helpLinkLabel"
+        defaultMessage="{aggTitle} help"
+        values={{ aggTitle: isAggTypeDefined ? value.title : '' }}
+      />
+    </EuiLink>
+  );
+
+  useEffect(
+    () => {
+      if (isFunction(setValidity)) {
+        setValidity(isAggTypeDefined);
+      }
+    },
+    [isAggTypeDefined]
+  );
+
+  return (
+    <EuiFormRow
+      label={label}
+      labelAppend={helpLink}
+      isInvalid={isSelectInvalid}
+      fullWidth={true}
+      className="visEditorAggSelect__formRow"
+    >
+      <EuiComboBox
+        placeholder={i18n.translate('common.ui.vis.defaultEditor.aggSelect.selectAggPlaceholder', {
+          defaultMessage: 'Select an aggregation…',
+        })}
+        id={`visDefaultEditorAggSelect${agg.id}`}
+        options={aggTypeOptions}
+        selectedOptions={selectedOptions}
+        singleSelection={{ asPlainText: true }}
+        onChange={options => setValue(get(options, '0.value'))}
+        data-test-subj="defaultEditorAggSelect"
+        isClearable={false}
+        isInvalid={isSelectInvalid}
+        fullWidth={true}
+        onBlur={() => setTouched()}
+      />
+    </EuiFormRow>
+  );
+}
+
+export { DefaultEditorAggSelect };
