fb3776c624cc5afaf85e6c7a6128d065618473a5
Joel Griffith <joel@joelgriffith.net>
2019-03-29 08:33:39 -0700

    CSV panel actions for SavedSearch embedabbles (#33709)
    * Adding CSV panel actions for SavedSearch embedabbles

---
+++ x-pack/plugins/reporting/public/panel_actions/get_csv_panel_action.tsx
@@ -0,0 +1,120 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import dateMath from '@elastic/datemath';
+import { i18n } from '@kbn/i18n';
+
+import { ContextMenuAction, ContextMenuActionsRegistryProvider } from 'ui/embeddable';
+import { PanelActionAPI } from 'ui/embeddable/context_menu_actions/types';
+import { kfetch } from 'ui/kfetch';
+import { toastNotifications } from 'ui/notify';
+import { API_BASE_URL_V1 } from '../../common/constants';
+
+const API_BASE_URL = `${API_BASE_URL_V1}/generate/immediate/csv/saved-object/`;
+
+class GetCsvReportPanelAction extends ContextMenuAction {
+  constructor() {
+    super(
+      {
+        displayName: i18n.translate('xpack.reporting.dashboard.downloadCsvPanelTitle', {
+          defaultMessage: 'Download CSV',
+        }),
+        id: 'downloadCsvReport',
+        parentPanelId: 'mainMenu',
+      },
+      {
+        icon: 'document',
+      }
+    );
+  }
+
+  public async generateJobParams({ searchEmbeddable }: { searchEmbeddable: any }) {
+    const adapters = searchEmbeddable.getInspectorAdapters();
+    if (!adapters) {
+      return {};
+    }
+
+    if (adapters.requests.requests.length === 0) {
+      return {};
+    }
+
+    return searchEmbeddable.searchScope.searchSource.getSearchRequestBody();
+  }
+
+  // @TODO: Clean this up once we update SavedSearch's interface
+  // and location in the file-system. `viewMode` also has an enum
+  // buried inside of the dashboard folder we could use vs a bare string
+  public isVisible = (panelActionAPI: PanelActionAPI): boolean => {
+    const { embeddable, containerState } = panelActionAPI;
+
+    return (
+      containerState.viewMode !== 'edit' && !!embeddable && embeddable.hasOwnProperty('savedSearch')
+    );
+  };
+
+  // TODO: Need to expose some of the interface here from SavedSearch
+  // as well as pass things like columns into the API call
+  public onClick = async (panelActionAPI: PanelActionAPI) => {
+    const { embeddable } = panelActionAPI as any;
+    const {
+      timeRange: { from, to },
+    } = embeddable;
+
+    if (!embeddable) {
+      return;
+    }
+
+    const searchEmbeddable = embeddable;
+    const state = await this.generateJobParams({ searchEmbeddable });
+
+    const id = `search:${embeddable.savedSearch.id}`;
+    const filename = embeddable.savedSearch.title;
+    const timezone = embeddable.$rootScope.chrome.getUiSettingsClient().get('dateFormat:tz');
+    const fromTime = dateMath.parse(from);
+    const toTime = dateMath.parse(to);
+
+    if (!fromTime || !toTime) {
+      return this.onGenerationFail(
+        new Error(`Invalid time range: From: ${fromTime}, To: ${toTime}`)
+      );
+    }
+
+    const body = JSON.stringify({
+      timerange: {
+        min: fromTime.valueOf(),
+        max: toTime.valueOf(),
+        timezone,
+      },
+      state,
+    });
+
+    await kfetch({ method: 'POST', pathname: `${API_BASE_URL}${id}`, body }, { parseJson: false })
+      .then(r => r.text())
+      .then(csv => {
+        const blob = new Blob([csv], { type: 'text/csv' });
+        const a = window.document.createElement('a');
+        const downloadObject = window.URL.createObjectURL(blob);
+        a.href = downloadObject;
+        a.download = `${filename}.csv`;
+        a.click();
+        window.URL.revokeObjectURL(downloadObject);
+      })
+      .catch(this.onGenerationFail);
+  };
+
+  private onGenerationFail(error: Error) {
+    toastNotifications.addDanger({
+      title: i18n.translate('xpack.reporting.dashboard.failedCsvDownloadTitle', {
+        defaultMessage: `CSV download failed`,
+      }),
+      text: i18n.translate('xpack.reporting.dashboard.failedCsvDownloadMessage', {
+        defaultMessage: `We couldn't download your CSV at this time.`,
+      }),
+      'data-test-subj': 'downloadCsvFail',
+    });
+  }
+}
+
+ContextMenuActionsRegistryProvider.register(() => new GetCsvReportPanelAction());
