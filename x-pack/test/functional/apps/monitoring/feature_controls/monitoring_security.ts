61ed5b1834d52ad836139ece685c3f5ab2a94edc
kobelb <brandon.kobel@elastic.co>
2019-03-29 08:19:07 -0700

    Changing usages of expect.js to @kbn/expect

--- x-pack/test/functional/apps/monitoring/feature_controls/monitoring_security.ts
+++ x-pack/test/functional/apps/monitoring/feature_controls/monitoring_security.ts
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import expect from 'expect.js';
+import expect from '@kbn/expect';
 import { SecurityService } from 'x-pack/test/common/services';
 import { KibanaFunctionalTestDefaultProviders } from '../../../../types/providers';
 
