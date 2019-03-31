877f51174f179928a06a7df4c770e178f42f2712
Peter Schretlen <peter.schretlen@elastic.co>
2019-03-31 17:05:56 -0400

    migration step 3 - plugin definition shim

--- x-pack/plugins/notifications/init.ts
+++ x-pack/plugins/notifications/init.ts
@@ -4,25 +4,24 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Legacy } from 'kibana';
 import { Plugin } from './server/plugin';
-
 /**
  * Initialize the Action Service with various actions provided by X-Pack, when configured.
  *
  * @param server {Object} HapiJS server instance
  */
-export function init(legacyServer: Legacy.Server): void {
+export function init(server: any): void {
   const coreSetup = {
-    log: legacyServer.log,
-    config: legacyServer.config,
-    plugins: legacyServer.plugins,
+    ...server.newPlatform.setup.core,
+    log: server.log,
+    config: server.config,
+    plugins: server.plugins,
   };
 
   const notificationSetup = new Plugin().setup(coreSetup, {});
 
-  legacyServer.route(notificationSetup.getRoute());
+  server.route(notificationSetup.getRoute());
 
   // expose the notification service for other plugins
-  legacyServer.expose('notificationService', notificationSetup.getService());
+  server.expose('notificationService', notificationSetup.getService());
 }
