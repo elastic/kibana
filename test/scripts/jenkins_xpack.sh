67124e38913ac4d7e2241d391a13d28aa4884026
Matt Kime <matt@mattki.me>
2019-03-31 11:49:03 -0500

    xpack checks

--- test/scripts/jenkins_xpack.sh
+++ test/scripts/jenkins_xpack.sh
@@ -17,13 +17,13 @@ export TEST_BROWSER_HEADLESS=1
 
 echo " -> Running mocha tests"
 cd "$XPACK_DIR"
-yarn test
+node spawn "xpack mocha" yarn test
 echo ""
 echo ""
 
 echo " -> Running jest tests"
 cd "$XPACK_DIR"
-node scripts/jest --ci --no-cache --verbose
+node spawn "xpack jest" node scripts/jest --ci --no-cache --verbose
 echo ""
 echo ""
 
