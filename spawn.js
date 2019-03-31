0e2e8daf4dde74d66df6a73c81756a8f3ca1b5c2
Matt Kime <matt@mattki.me>
2019-03-30 22:36:11 -0500

    lets try firing off some checks

--- spawn.js
+++ spawn.js
@@ -19,7 +19,70 @@
 
 
 const { spawn } = require('child_process');
+const Octokit = require('@octokit/rest');
+const App = require('@octokit/app');
+const request = require('@octokit/request');
 
+
+const getInstallation = async function (jwt) {
+  return await request('GET /repos/:owner/:repo/installation', {
+    owner: 'elastic',
+    repo: 'kibana',
+    headers: {
+      authorization: `Bearer ${jwt}`,
+      accept: 'application/vnd.github.machine-man-preview+json'
+    }
+  });
+};
+
+const getClientWithAuth = async function () {
+  console.log('hi from script');
+  console.log('length', process.env.GITHUB_TOKEN.length);
+  const app = new App({
+    id: 26774,
+    privateKey: process.env.KIBANA_CI_REPORTER_KEY
+  });
+
+  const jwt = app.getSignedJsonWebToken();
+  const { data } = await request('GET /repos/:owner/:repo/installation', {
+    owner: 'elastic',
+    repo: 'kibana',
+    headers: {
+      authorization: `Bearer ${jwt}`,
+      accept: 'application/vnd.github.machine-man-preview+json'
+    }
+  });
+
+  const installationId = data.id;
+  const installationAccessToken = await app.getInstallationAccessToken({ installationId });
+
+  return new Octokit({ auth: `token ${installationAccessToken}` });
+  /*
+  const clientWithAuth = new Octokit({ auth: `token ${installationAccessToken}` });
+
+  console.log('creating check');
+  clientWithAuth.checks.create({
+    owner: 'elastic',
+    repo: 'kibana',
+    name: 'check name',
+    head_sha: process.env.ghprbActualCommit,
+    // head_sha: '7680ee538b1443fbb5f8d7a1e3c335bf477dbbdf',
+    details_url: 'http://www.google.com',
+    external_id: 'external id',
+    status: 'in_progress',
+    output: {
+      title: 'title',
+      summary: 'summary',
+      text: 'text',
+    },
+  }).then(({ headers: { 'x-ratelimit-limit': limit, 'x-ratelimit-remaining': remaining } }) => console.log(`limit: ${remaining} / ${limit}`));
+  console.log('check created');
+*/
+};
+
+// -------------
+
+const clientWithAuth = getClientWithAuth();
 const cmd = process.argv[2];
 const cmdArgs = process.argv.slice(3);
 const cmdSpawnConfig = { cwd: __dirname, stdio: 'inherit' };
@@ -27,8 +90,42 @@ const cmdSpawnConfig = { cwd: __dirname, stdio: 'inherit' };
 //todo check env vars
 //todo - fire api request
 //get title
+clientWithAuth.checks.create({
+  owner: 'elastic',
+  repo: 'kibana',
+  name: cmd,
+  head_sha: process.env.ghprbActualCommit,
+  // head_sha: '7680ee538b1443fbb5f8d7a1e3c335bf477dbbdf',
+  details_url: 'http://www.google.com',
+  external_id: 'external id',
+  status: 'in_progress',
+  output: {
+    title: `title ${cmd}`,
+    summary: `summary ${cmd}`,
+    text: `text ${cmd}`,
+  },
+}).then(({ headers: { 'x-ratelimit-limit': limit, 'x-ratelimit-remaining': remaining } }) => console.log(`limit: ${remaining} / ${limit}`));
+
 const ls = spawn(cmd, cmdArgs, cmdSpawnConfig);
 
 //todo - fire api request before exiting
 // determine success or failure
-ls.on('close', process.exit);
+ls.on('close', (code) => {
+  clientWithAuth.checks.create({
+    owner: 'elastic',
+    repo: 'kibana',
+    name: cmd,
+    head_sha: process.env.ghprbActualCommit,
+    // head_sha: '7680ee538b1443fbb5f8d7a1e3c335bf477dbbdf',
+    details_url: 'http://www.google.com',
+    external_id: 'external id',
+    //status: 'complete',
+    conclusion: code === 0 ? 'success' : 'failure',
+    output: {
+      title: `title ${cmd}`,
+      summary: `summary ${cmd}`,
+      text: `text ${cmd}`,
+    },
+  }).then(({ headers: { 'x-ratelimit-limit': limit, 'x-ratelimit-remaining': remaining } }) => console.log(`limit: ${remaining} / ${limit}`));
+
+  process.exit(code);});
