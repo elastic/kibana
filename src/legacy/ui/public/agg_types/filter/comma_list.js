7a25b895db9f083847cc64928d57729838f13aa9
Liza Katz <lizka.k@gmail.com>
2019-03-31 11:55:55 +0300

    New platform cleanup autoload - step 2 (#33807)
    * Explicit dependencies to doc_title
    
    * Explicit dependencies to
        - kbn_top_nav
        - saved_object_save_as_checkbox
    
    * Explicit dependencies to filter_bar
    
    * Explicit dependency to query_bar
    
    * Removed modules from autoloades that never
    used implicitly:
    - bound_to_config_obj
    - debounce
    - filter_manager
    - index_patterns
    - parse_query
    - persisted_log
    - timefilter
    
    * Removed modules from autoload (never used implicitly)
    - events
    
    * Explicit dependency to bind + typo fix
    
    * Explicit dependency to fancy_forms
    
    * Explicit dependency to $listen
    
    * Explicit dependency to timepicker
    
    * Moved kbn-infinite-scroll directive to doc_table
    
    * Explicit dependency on directives
    - css truncate
    - inequality
    
    * Explicit dependecy to input_focus directive
    
    * Explicit dependency to json-input directive
    
    * Explicit dependency on directives:
     - input_focus
     - paginate
    
    * Explicit dependency on paginated-selectable-list directive
    
    * Explicit dependency on saved-object-finder directive
    
    * Moved validate_json directive into agg_types
    
    * Don't include directives implicitly
    
    * Moved comma list
    Explicitly included sort_prefix_first filter
    
    * Deleted unused filter unique
    
    * Deleted unused filter startFrom
    
    * Removed unused filter matchAny
    
    * Moved filter trustAsHtml to kbn_doc_views
    Added explicit import to uriescape
    
    * Moved markdown filter to notify
    
    * Explicitly import label filter
    
    * Remove implicit autoload of filters

--- src/legacy/ui/public/agg_types/filter/comma_list.js
+++ src/legacy/ui/public/agg_types/filter/comma_list.js
@@ -17,11 +17,25 @@
  * under the License.
  */
 
-import { uiModules } from '../modules';
-const module = uiModules.get('kibana');
+import { uiModules } from 'ui/modules';
 
-// Simple filter to allow using ng-bind-html without explicitly calling $sce.trustAsHtml in a controller
-// (See http://goo.gl/mpj9o2)
-module.filter('trustAsHtml', function ($sce) {
-  return $sce.trustAsHtml;
-});
+import {
+  parseCommaSeparatedList,
+  formatListAsProse,
+} from '../../../../utils';
+
+uiModules
+  .get('kibana')
+  .filter('commaList', function () {
+  /**
+   * Angular filter that accepts either an array or a comma-separated string
+   * and outputs a comma-separated string for presentation.
+   *
+   * @param {String|Array} input - The comma-separated list or array
+   * @param {Boolean} inclusive - Should the list be joined with an "and"?
+   * @return {String}
+   */
+    return function (input, inclusive = false) {
+      return formatListAsProse(parseCommaSeparatedList(input), { inclusive });
+    };
+  });
