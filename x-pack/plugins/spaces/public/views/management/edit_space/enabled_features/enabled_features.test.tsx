5fe09be8464f02621b084e59a0f7a21af7d48466
Larry Gregory <larry.gregory@elastic.co>
2019-04-01 12:32:02 -0400

    Adds bulk toggle for showing/hiding features within a space

--- x-pack/plugins/spaces/public/views/management/edit_space/enabled_features/enabled_features.test.tsx
+++ x-pack/plugins/spaces/public/views/management/edit_space/enabled_features/enabled_features.test.tsx
@@ -4,39 +4,126 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { EuiLink } from '@elastic/eui';
 import React from 'react';
-import { shallowWithIntl } from 'test_utils/enzyme_helpers';
+import { mountWithIntl, shallowWithIntl } from 'test_utils/enzyme_helpers';
+import { Space } from 'x-pack/plugins/spaces/common/model/space';
+import { Feature } from 'x-pack/plugins/xpack_main/types';
+import { SectionPanel } from '../section_panel';
 import { EnabledFeatures } from './enabled_features';
+
+const features: Feature[] = [
+  {
+    id: 'feature-1',
+    name: 'Feature 1',
+    app: [],
+    privileges: {},
+  },
+  {
+    id: 'feature-2',
+    name: 'Feature 2',
+    app: [],
+    privileges: {},
+  },
+];
+
+const space: Space = {
+  id: 'my-space',
+  name: 'my space',
+  disabledFeatures: ['feature-1', 'feature-2'],
+};
+
+const uiCapabilities = {
+  navLinks: {},
+  management: {},
+  catalogue: {},
+  spaces: {
+    manage: true,
+  },
+};
 
 describe('EnabledFeatures', () => {
   it(`renders as expected`, () => {
     expect(
       shallowWithIntl<EnabledFeatures>(
         <EnabledFeatures
-          features={[
-            {
-              id: 'some-feature',
-              name: 'Some Feature',
-              icon: 'discoverApp',
-              app: [],
-              privileges: {},
-            },
-          ]}
-          space={{
-            id: 'my-space',
-          }}
-          uiCapabilities={{
-            navLinks: {},
-            management: {},
-            catalogue: {},
-            spaces: {
-              manage: true,
-            },
-          }}
+          features={features}
+          space={space}
+          uiCapabilities={uiCapabilities}
           intl={null as any}
           onChange={jest.fn()}
         />
       )
     ).toMatchSnapshot();
+  });
+
+  it('allows all features to be toggled on', () => {
+    const changeHandler = jest.fn();
+
+    const wrapper = mountWithIntl(
+      <EnabledFeatures
+        features={features}
+        space={space}
+        uiCapabilities={uiCapabilities}
+        intl={null as any}
+        onChange={changeHandler}
+      />
+    );
+
+    // expand section panel
+    wrapper
+      .find(SectionPanel)
+      .find(EuiLink)
+      .simulate('click');
+
+    // Click the "Change all" link
+    wrapper
+      .find('.spcFeatureChangeAllLink')
+      .first()
+      .simulate('click');
+
+    // Ask to show all features
+    wrapper.find('button[data-test-subj="spc-toggle-all-features-show"]').simulate('click');
+
+    expect(changeHandler).toBeCalledTimes(1);
+
+    const updatedSpace = changeHandler.mock.calls[0][0];
+
+    expect(updatedSpace.disabledFeatures).toEqual([]);
+  });
+
+  it('allows all features to be toggled off', () => {
+    const changeHandler = jest.fn();
+
+    const wrapper = mountWithIntl(
+      <EnabledFeatures
+        features={features}
+        space={space}
+        uiCapabilities={uiCapabilities}
+        intl={null as any}
+        onChange={changeHandler}
+      />
+    );
+
+    // expand section panel
+    wrapper
+      .find(SectionPanel)
+      .find(EuiLink)
+      .simulate('click');
+
+    // Click the "Change all" link
+    wrapper
+      .find('.spcFeatureChangeAllLink')
+      .first()
+      .simulate('click');
+
+    // Ask to hide all features
+    wrapper.find('button[data-test-subj="spc-toggle-all-features-hide"]').simulate('click');
+
+    expect(changeHandler).toBeCalledTimes(1);
+
+    const updatedSpace = changeHandler.mock.calls[0][0];
+
+    expect(updatedSpace.disabledFeatures).toEqual(['feature-1', 'feature-2']);
   });
 });
---
+++ x-pack/plugins/spaces/public/views/management/edit_space/enabled_features/toggle_all_features.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import { EuiContextMenuItem, EuiContextMenuPanel, EuiLink, EuiPopover } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { capitalize } from 'lodash';
+import React, { Component } from 'react';
+interface Props {
+  onChange: (visible: boolean) => void;
+  disabled?: boolean;
+}
+
+interface State {
+  isPopoverOpen: boolean;
+}
+
+export class ToggleAllFeatures extends Component<Props, State> {
+  public state = {
+    isPopoverOpen: false,
+  };
+
+  public render() {
+    const button = (
+      <EuiLink onClick={this.onButtonClick} className={'spcFeatureChangeAllLink'}>
+        <FormattedMessage
+          id="xpack.spaces.management.toggleAllFeaturesLink"
+          defaultMessage="(change all)"
+        />
+      </EuiLink>
+    );
+
+    const items = (['show', 'hide'] as Array<'show' | 'hide'>).map(item => {
+      return (
+        <EuiContextMenuItem
+          data-test-subj={`spc-toggle-all-features-${item}`}
+          key={item}
+          onClick={() => {
+            this.onSelect(item);
+          }}
+          disabled={this.props.disabled}
+        >
+          {capitalize(item)}
+        </EuiContextMenuItem>
+      );
+    });
+
+    return (
+      <EuiPopover
+        id={'changeAllFeatureVisibilityPopover'}
+        button={button}
+        isOpen={this.state.isPopoverOpen}
+        closePopover={this.closePopover}
+        panelPaddingSize="none"
+        anchorPosition="downLeft"
+      >
+        <EuiContextMenuPanel items={items} />
+      </EuiPopover>
+    );
+  }
+
+  private onSelect = (selection: 'show' | 'hide') => {
+    this.props.onChange(selection === 'show');
+    this.setState({ isPopoverOpen: false });
+  };
+
+  private onButtonClick = () => {
+    this.setState({
+      isPopoverOpen: !this.state.isPopoverOpen,
+    });
+  };
+
+  private closePopover = () => {
+    this.setState({
+      isPopoverOpen: false,
+    });
+  };
+}
