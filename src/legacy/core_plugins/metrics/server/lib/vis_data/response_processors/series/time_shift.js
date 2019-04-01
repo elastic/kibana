b61ad7ad46c2f5de44b1f25e9677e3415f734e2f
Alexey Antonov <alexwizp@gmail.com>
2019-04-01 19:14:02 +0300

    Timeseries Visual Builder - DST shift in historic data

--- src/legacy/core_plugins/metrics/server/lib/vis_data/response_processors/series/time_shift.js
+++ src/legacy/core_plugins/metrics/server/lib/vis_data/response_processors/series/time_shift.js
@@ -24,15 +24,21 @@ export default function timeShift(resp, panel, series) {
     if (/^([+-]?[\d]+)([shmdwMy]|ms)$/.test(series.offset_time)) {
       const matches = series.offset_time.match(/^([+-]?[\d]+)([shmdwMy]|ms)$/);
       if (matches) {
-        const offsetValue = matches[1];
+        const offsetValue = Number(matches[1]);
         const offsetUnit = matches[2];
+
         results.forEach(item => {
           if (_.startsWith(item.id, series.id)) {
-            item.data = item.data.map(row => [moment(row[0]).add(offsetValue, offsetUnit).valueOf(), row[1]]);
+            item.data = item.data.map(row => {
+              const timeWithOffset = row[0] + moment.duration(offsetValue, offsetUnit).valueOf();
+
+              return [timeWithOffset, row[1]];
+            });
           }
         });
       }
     }
+
     return next(results);
   };
 }
