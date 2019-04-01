7b50cf2f78c559cd87a4747306a8d4bc589e28d7
Thomas Neirynck <thomas@elastic.co>
2019-04-01 15:54:07 -0400

    add unit tests

---
+++ x-pack/plugins/maps/public/components/map/feature_tooltip.test.js
@@ -0,0 +1,103 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { shallowWithIntl } from 'test_utils/enzyme_helpers';
+
+import { FeatureTooltip } from './feature_tooltip';
+
+
+
+class MockTooltipProperty {
+  constructor(key, value, isFilterable) {
+    this._key = key;
+    this._value = value;
+    this._isFilterable = isFilterable;
+  }
+
+  isFilterable() {
+    return this._isFilterable;
+  }
+
+  getFilterAction() {
+    return () => {};
+  }
+
+  getHtmlDisplayValue() {
+    return this._value;
+  }
+
+  getPropertyName() {
+    return this._key;
+  }
+}
+
+const defaultProps = {
+  properties: [],
+  closeTooltip: () => {},
+  isFilterable: false,
+  showCloseButton: false
+};
+
+
+const mockTooltipProperties = [
+  new MockTooltipProperty('foo', 'bar', true),
+  new MockTooltipProperty('foo', 'bar', false)
+];
+
+describe('FeatureTooltip', () => {
+
+  test('default, no close', () => {
+    const component = shallowWithIntl(
+      <FeatureTooltip
+        {...defaultProps}
+      />
+    );
+
+    expect(component)
+      .toMatchSnapshot();
+  });
+
+  test('default, with close button', () => {
+    const component = shallowWithIntl(
+      <FeatureTooltip
+        {...defaultProps}
+        showCloseButton={true}
+      />
+    );
+
+    expect(component)
+      .toMatchSnapshot();
+  });
+
+  test('filterable', () => {
+    const component = shallowWithIntl(
+      <FeatureTooltip
+        {...defaultProps}
+        isFilterable={true}
+        properties={mockTooltipProperties}
+      />
+    );
+
+    expect(component)
+      .toMatchSnapshot();
+  });
+
+  test('not filterable', () => {
+    const component = shallowWithIntl(
+      <FeatureTooltip
+        {...defaultProps}
+        isFilterable={false}
+        properties={mockTooltipProperties}
+      />
+    );
+
+    expect(component)
+      .toMatchSnapshot();
+  });
+
+
+});
