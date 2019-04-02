2dcc37125bbbc625800eaea3a734909979b1fbbd
sulemanof <sul-dan@mail.ru>
2019-04-02 17:53:06 +0300

    EUIficate drop partials control

---
+++ src/legacy/ui/public/agg_types/controls/drop_partials.tsx
@@ -0,0 +1,62 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from 'react';
+
+import { EuiFormRow, EuiIconTip, EuiSwitch } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { AggParamEditorProps } from '../../vis/editors/default';
+
+function DropPartialsParamEditor({ agg, aggParam, value, setValue }: AggParamEditorProps<boolean>) {
+  if (agg.params.field.name !== agg.getIndexPattern().timeFieldName) {
+    return null;
+  }
+
+  const content = i18n.translate('common.ui.aggTypes.dropPartialBucketsTooltip', {
+    defaultMessage:
+      "Remove buckets that span time outside the time range so the histogram doesn't start and end with incomplete buckets.",
+  });
+
+  const label = (
+    <>
+      <FormattedMessage
+        id="common.ui.aggTypes.dropPartialBucketsLabel"
+        defaultMessage="Drop partial buckets"
+      />{' '}
+      <EuiIconTip position="right" content={content} type="questionInCircle" aria-label={content} />
+    </>
+  );
+
+  return (
+    <EuiFormRow
+      label={label}
+      fullWidth={true}
+      className="visEditorSidebar__aggParamFormRow dropPartialBuckets"
+    >
+      <EuiSwitch
+        checked={Boolean(value)}
+        data-test-subj={'dropPartialBucketsCheckbox'}
+        onChange={ev => setValue(ev.target.checked)}
+      />
+    </EuiFormRow>
+  );
+}
+
+export { DropPartialsParamEditor };
