43ffcc8d575cfd06af5e3607a6ab6ee36cf02619
Timothy Sullivan <tsullivan@elastic.co>
2019-04-03 14:37:35 -0700

    polish decorate range stats

--- x-pack/plugins/reporting/server/usage/decorate_range_stats.ts
+++ x-pack/plugins/reporting/server/usage/decorate_range_stats.ts
@@ -5,41 +5,50 @@
  */
 
 import { CSV_JOB_TYPE, PDF_JOB_TYPE, PNG_JOB_TYPE } from '../../common/constants';
-import { FeatureAvailabilityMap, RangeStats, ReportingFeature } from './';
+import { AvailableTotal, FeatureAvailabilityMap, RangeStats, ReportingFeature } from './';
+
+function getForFeature(
+  range: Partial<RangeStats>,
+  featureKey: ReportingFeature,
+  featureAvailability: FeatureAvailabilityMap,
+  additional?: any
+): AvailableTotal & typeof additional {
+  const isAvailable = (feature: ReportingFeature) =>
+    featureAvailability[feature] != null ? featureAvailability[feature] : false;
+  const jobType = range[featureKey] || { total: 0, ...additional };
+
+  // merge the additional stats for the jobType
+  type AdditionalType = { [K in keyof typeof additional]: K };
+  const filledAdditional: AdditionalType = {};
+  if (additional) {
+    Object.keys(additional).forEach(k => {
+      filledAdditional[k] = { ...additional[k], ...jobType[k] };
+    });
+  }
+
+  return {
+    available: isAvailable(featureKey),
+    total: jobType.total,
+    ...filledAdditional,
+  };
+}
 
 /*
  * Decorate range stats (stats for last day, last 7 days, etc) with feature
- * availability booleans
+ * availability booleans, and zero-filling for unused features
  */
 export const decorateRangeStats = (
-  range: RangeStats,
+  range: Partial<RangeStats> = {},
   featureAvailability: FeatureAvailabilityMap
 ): RangeStats => {
-  const isAvailable = (feature: ReportingFeature) => {
-    if (featureAvailability[feature] == null) {
-      throw new Error(`undefined available: ${featureAvailability[feature]}`);
-    }
-    return featureAvailability[feature] != null ? featureAvailability[feature] : false;
-  };
-
-  const overall = {
-    _all: range._all || 0,
-    status: range.status || { completed: 0 },
-  };
-
   return {
-    ...overall,
-    [CSV_JOB_TYPE]: {
-      ...range[CSV_JOB_TYPE],
-      available: isAvailable(CSV_JOB_TYPE),
-    },
-    [PNG_JOB_TYPE]: {
-      ...range[PNG_JOB_TYPE],
-      available: isAvailable(PNG_JOB_TYPE),
-    },
-    [PDF_JOB_TYPE]: {
-      ...range[PDF_JOB_TYPE],
-      available: isAvailable(PDF_JOB_TYPE),
-    },
+    _all: range._all || 0,
+    status: { completed: 0, failed: 0, ...range.status },
+    [CSV_JOB_TYPE]: getForFeature(range, CSV_JOB_TYPE, featureAvailability),
+    [PNG_JOB_TYPE]: getForFeature(range, PNG_JOB_TYPE, featureAvailability),
+    [PDF_JOB_TYPE]: getForFeature(range, PDF_JOB_TYPE, featureAvailability, {
+      app: { dashboard: 0, visualization: 0 },
+      layout: { preserve_layout: 0, print: 0 },
+    }),
   };
 };
