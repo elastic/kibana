2c5ab8f792f139685c83c439e6954529ffe18390
kobelb <brandon.kobel@elastic.co>
2019-04-04 16:02:14 -0700

    Updating imports

--- x-pack/test/functional/apps/machine_learning/feature_controls/ml_spaces.ts
+++ x-pack/test/functional/apps/machine_learning/feature_controls/ml_spaces.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import expect from '@kbn/expect';
-import { SpacesService } from 'x-pack/test/common/services';
+import { SpacesService } from '../../../../common/services';
 import { KibanaFunctionalTestDefaultProviders } from '../../../../types/providers';
 
 // tslint:disable:no-default-export
