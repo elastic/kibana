43f3ad5bf5a0e7b71af0b99d3c42dccd4cc9eca1
Matt Kime <matt@mattki.me>
2019-04-07 11:40:20 -0500

    implement xpack checks

--- test/scripts/jenkins_xpack.sh
+++ test/scripts/jenkins_xpack.sh
@@ -17,13 +17,13 @@ export TEST_BROWSER_HEADLESS=1
 
 echo " -> Running mocha tests"
 cd "$XPACK_DIR"
-yarn test
+yarn run github-checks-reporter "xpack:mocha" yarn test
 echo ""
 echo ""
 
 echo " -> Running jest tests"
 cd "$XPACK_DIR"
-node scripts/jest --ci --no-cache --verbose
+yarn run github-checks-reporter "xpack:jest" node scripts/jest --ci --no-cache --verbose
 echo ""
 echo ""
 
