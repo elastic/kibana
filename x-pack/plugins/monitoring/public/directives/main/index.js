1f6de332fbeebeaff94b36cdb5cd96e6035afa92
igoristic <igor.zaytsev.dev@gmail.com>
2019-04-08 16:51:44 -0400

    Swapped kui base class with eui equivalent (#34644)
    * Swapped kui base class with eui equivalent
    
    * Added the react component instead

--- x-pack/plugins/monitoring/public/directives/main/index.js
+++ x-pack/plugins/monitoring/public/directives/main/index.js
@@ -3,11 +3,55 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import {
+  EuiSelect,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiTitle
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
 import { get } from 'lodash';
 import { uiModules } from 'ui/modules';
 import template from './index.html';
 import { shortenPipelineHash } from '../../../common/formatting';
+
+const setOptions = (controller) => {
+  if (!controller.pipelineVersions || !controller.pipelineVersions.length || !controller.pipelineDropdownElement) {
+    return;
+  }
+
+  render(
+    <EuiFlexGroup>
+      <EuiFlexItem grow={false}>
+        <EuiTitle style={{ maxWidth: 400, lineHeight: '40px', overflow: 'hidden', whiteSpace: 'nowrap' }}>
+          <h2>{controller.pipelineId}</h2>
+        </EuiTitle>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiSelect
+          value={controller.pipelineHash}
+          options={controller.pipelineVersions.map((option) => {
+            return {
+              text: i18n.translate('xpack.monitoring.logstashNavigation.pipelineVersionDescription',
+                {
+                  defaultMessage: 'Version active {relativeLastSeen} and first seen {relativeFirstSeen}',
+                  values: {
+                    relativeLastSeen: option.relativeLastSeen,
+                    relativeFirstSeen: option.relativeFirstSeen
+                  }
+                }
+              ),
+              value: option.hash
+            };
+          })}
+          onChange={controller.onChangePipelineHash}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+    , controller.pipelineDropdownElement);
+};
 
 /*
  * Manage data and provide helper methods for the "main" directive's template
@@ -23,6 +67,11 @@ export class MonitoringMainController {
     this.inLogstash = false;
     this.inBeats = false;
     this.inApm = false;
+  }
+
+  dropdownLoadedHandler() {
+    this.pipelineDropdownElement = document.querySelector('#dropdown-elm');
+    setOptions(this);
   }
 
   // kick things off from the directive link function
@@ -113,8 +162,10 @@ uiModule.directive('monitoringMain', (breadcrumbs, license, kbnUrl, $injector) =
       Object.keys(setupObj.attributes).forEach(key => {
         attributes.$observe(key, () => controller.setup(getSetupObj()));
       });
+      scope.$on('$destroy', () => controller.pipelineDropdownElement && unmountComponentAtNode(controller.pipelineDropdownElement));
       scope.$watch('pageData.versions', versions => {
         controller.pipelineVersions = versions;
+        setOptions(controller);
       });
     }
   };
