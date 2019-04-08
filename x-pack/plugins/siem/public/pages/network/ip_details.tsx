ad518c52fc524d9659651ab7210e07cc70e0d58c
Michael Marcialis <michael.marcialis@elastic.co>
2019-04-08 13:52:39 -0400

    rm unused import

--- x-pack/plugins/siem/public/pages/network/ip_details.tsx
+++ x-pack/plugins/siem/public/pages/network/ip_details.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiLink, EuiSpacer } from '@elastic/eui';
+import { EuiLink } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import React from 'react';
 import { connect } from 'react-redux';
