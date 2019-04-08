769dca3352800b0c86ad975e5d7b906d66d75558
Chris Roberson <chrisronline@gmail.com>
2019-04-08 16:54:04 -0400

    Initial logic and tests for this

---
+++ x-pack/plugins/monitoring/server/lib/setup/collection/get_collection_status.js
@@ -0,0 +1,233 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { get, uniq } from 'lodash';
+import { METRICBEAT_INDEX_NAME_UNIQUE_TOKEN } from '../../../../common/constants';
+import { KIBANA_SYSTEM_ID, BEATS_SYSTEM_ID, LOGSTASH_SYSTEM_ID, APM_SYSTEM_ID } from '../../../../../xpack_main/common/constants';
+
+const getRecentMonitoringDocuments = async (req, indexPatterns, clusterUuid) => {
+  const filters = [
+    {
+      range: {
+        'timestamp': {
+          gte: 'now-30s',
+          lte: 'now'
+        }
+      }
+    }
+  ];
+
+  if (clusterUuid) {
+    filters.push({ term: { 'cluster_uuid': clusterUuid } });
+  }
+
+  const params = {
+    index: Object.values(indexPatterns),
+    size: 0,
+    ignoreUnavailable: true,
+    filterPath: [
+      'aggregations.indices.buckets'
+    ],
+    body: {
+      query: {
+        bool: {
+          filter: filters,
+        }
+      },
+      aggs: {
+        indices: {
+          terms: {
+            field: '_index',
+            size: 50,
+          },
+          aggs: {
+            es_uuids: {
+              terms: {
+                field: 'node_stats.node_id'
+              },
+              aggs: {
+                by_timestamp: {
+                  max: {
+                    field: 'timestamp'
+                  }
+                }
+              }
+            },
+            kibana_uuids: {
+              terms: {
+                field: 'kibana_stats.kibana.uuid'
+              },
+              aggs: {
+                by_timestamp: {
+                  max: {
+                    field: 'timestamp'
+                  }
+                }
+              }
+            },
+            beats_uuids: {
+              terms: {
+                field: 'beats_stats.beat.uuid'
+              },
+              aggs: {
+                by_timestamp: {
+                  max: {
+                    field: 'timestamp'
+                  }
+                }
+              }
+            },
+            logstash_uuids: {
+              terms: {
+                field: 'logstash_stats.logstash.uuid'
+              },
+              aggs: {
+                by_timestamp: {
+                  max: {
+                    field: 'timestamp'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const { callWithRequest } = req.server.plugins.elasticsearch.getCluster('monitoring');
+  return await callWithRequest(req, 'search', params);
+};
+
+function getUuidBucketName(productName) {
+  switch (productName) {
+    case 'elasticsearch':
+      return 'es_uuids';
+    case KIBANA_SYSTEM_ID:
+      return 'kibana_uuids';
+    case BEATS_SYSTEM_ID:
+    case APM_SYSTEM_ID:
+      return 'beats_uuids';
+    case LOGSTASH_SYSTEM_ID:
+      return 'logstash_uuids';
+  }
+}
+
+export const getCollectionStatus = async (req, indexPatterns, clusterUuid) => {
+  const PRODUCTS = [
+    { name: KIBANA_SYSTEM_ID },
+    { name: BEATS_SYSTEM_ID },
+    { name: LOGSTASH_SYSTEM_ID },
+    { name: APM_SYSTEM_ID, token: '-beats-' },
+    { name: 'elasticsearch', token: '-es-' },
+  ];
+
+  const recentDocuments = await getRecentMonitoringDocuments(req, indexPatterns, clusterUuid);
+  const indicesBuckets = get(recentDocuments, 'aggregations.indices.buckets');
+
+  const status = PRODUCTS.reduce((products, product) => {
+    const token = product.token || product.name;
+    const indexBuckets = indicesBuckets.filter(bucket => bucket.key.includes(token));
+    const uuidBucketName = getUuidBucketName(product.name);
+
+    const capabilities = {
+      totalUniqueInstanceCount: 0,
+      totalUniqueFullyMigratedCount: 0,
+      byUuid: {},
+    };
+
+    const fullyMigratedUuidsMap = {};
+    const internalCollectorsUuidsMap = {};
+    const partiallyMigratedUuidsMap = {};
+
+    // If there is no data, then they are a net new user
+    if (!indexBuckets || indexBuckets.length === 0) {
+      capabilities.totalUniqueInstanceCount = 0;
+    }
+    // If there is a single bucket, then they are fully migrated or fully on the internal collector
+    else if (indexBuckets.length === 1) {
+      const singleIndexBucket = indexBuckets[0];
+      const isFullyMigrated = singleIndexBucket.key.includes(METRICBEAT_INDEX_NAME_UNIQUE_TOKEN);
+
+      const map = isFullyMigrated ? fullyMigratedUuidsMap : internalCollectorsUuidsMap;
+      const uuidBuckets = get(singleIndexBucket, `${uuidBucketName}.buckets`, []);
+      for (const { key, by_timestamp: byTimestamp } of uuidBuckets) {
+        if (!map[key]) {
+          map[key] = { lastTimestamp: get(byTimestamp, 'value') };
+        }
+      }
+      capabilities.totalUniqueInstanceCount = Object.keys(map).length;
+      capabilities.totalUniqueFullyMigratedCount = Object.keys(fullyMigratedUuidsMap).length;
+      capabilities.byUuid = {
+        ...Object.keys(internalCollectorsUuidsMap).reduce((accum, uuid) => ({
+          ...accum,
+          [uuid]: { isInternalCollector: true, ...internalCollectorsUuidsMap[uuid] }
+        }), {}),
+        ...Object.keys(partiallyMigratedUuidsMap).reduce((accum, uuid) => ({
+          ...accum,
+          [uuid]: { isPartiallyMigrated: true, ...partiallyMigratedUuidsMap[uuid] }
+        }), {}),
+        ...Object.keys(fullyMigratedUuidsMap).reduce((accum, uuid) => ({
+          ...accum,
+          [uuid]: { isFullyMigrated: true, ...fullyMigratedUuidsMap[uuid] }
+        }), {}),
+      };
+    }
+    // If there are multiple buckets, they are partially upgraded assuming a single mb index exists
+    else {
+      const internalTimestamps = [];
+      for (const indexBucket of indexBuckets) {
+        const isFullyMigrated = indexBucket.key.includes(METRICBEAT_INDEX_NAME_UNIQUE_TOKEN);
+        const map = isFullyMigrated ? fullyMigratedUuidsMap : internalCollectorsUuidsMap;
+        const otherMap = !isFullyMigrated ? fullyMigratedUuidsMap : internalCollectorsUuidsMap;
+
+        const uuidBuckets = get(indexBucket, `${uuidBucketName}.buckets`, []);
+        for (const { key, by_timestamp: byTimestamp } of uuidBuckets) {
+          if (!map[key]) {
+            if (otherMap[key]) {
+              partiallyMigratedUuidsMap[key] = true;
+              delete otherMap[key];
+            }
+            else {
+              map[key] = true;
+            }
+          }
+          if (!isFullyMigrated) {
+            internalTimestamps.push(byTimestamp.value);
+          }
+        }
+      }
+
+      capabilities.totalUniqueInstanceCount = uniq([
+        ...Object.keys(internalCollectorsUuidsMap),
+        ...Object.keys(fullyMigratedUuidsMap),
+        ...Object.keys(partiallyMigratedUuidsMap)
+      ]).length;
+      capabilities.totalUniqueFullyMigratedCount = Object.keys(fullyMigratedUuidsMap).length;
+      capabilities.byUuid = {
+        ...Object.keys(internalCollectorsUuidsMap).reduce((accum, uuid) => ({
+          ...accum,
+          [uuid]: { isInternalCollector: true }
+        }), {}),
+        ...Object.keys(partiallyMigratedUuidsMap).reduce((accum, uuid) => ({
+          ...accum,
+          [uuid]: { isPartiallyMigrated: true, lastInternallyCollectedTimestamp: internalTimestamps[0] }
+        }), {}),
+        ...Object.keys(fullyMigratedUuidsMap).reduce((accum, uuid) => ({
+          ...accum,
+          [uuid]: { isFullyMigrated: true }
+        }), {}),
+      };
+    }
+
+    return {
+      ...products,
+      [product.name]: capabilities,
+    };
+  }, {});
+
+  return status;
+};
--- x-pack/plugins/monitoring/server/routes/api/v1/setup/cluster_setup_status.js
+++ x-pack/plugins/monitoring/server/routes/api/v1/setup/cluster_setup_status.js
@@ -3,49 +3,45 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
 import Joi from 'joi';
-import { getClustersFromRequest } from '../../../../lib/cluster/get_clusters_from_request';
 import { verifyMonitoringAuth } from '../../../../lib/elasticsearch/verify_monitoring_auth';
 import { handleError } from '../../../../lib/errors';
-import {
-  INDEX_PATTERN_FILEBEAT
-} from '../../../../../common/constants';
+import { getCollectionStatus } from '../../../../lib/setup/collection';
 import { getIndexPatterns } from '../../../../lib/cluster/get_index_patterns';
 
-export function clustersRoute(server) {
+export function clusterSetupStatusRoute(server) {
   /*
    * Monitoring Home
    * Route Init (for checking license and compatibility for multi-cluster monitoring
    */
   server.route({
     method: 'POST',
-    path: '/api/monitoring/v1/clusters',
+    path: '/api/monitoring/v1/setup/{clusterUuid}',
     config: {
       validate: {
+        params: Joi.object({
+          clusterUuid: Joi.string().required()
+        }),
         payload: Joi.object({
-          timeRange: Joi.object({
-            min: Joi.date().required(),
-            max: Joi.date().required()
-          }).required()
+          ccs: Joi.string().optional()
         })
       }
     },
     handler: async (req) => {
-      let clusters = [];
+      let setupCapabilities = null;
 
       // NOTE using try/catch because checkMonitoringAuth is expected to throw
       // an error when current logged-in user doesn't have permission to read
       // the monitoring data. `try/catch` makes it a little more explicit.
       try {
         await verifyMonitoringAuth(req);
-        const indexPatterns = getIndexPatterns(server, { INDEX_PATTERN_FILEBEAT });
-        clusters = await getClustersFromRequest(req, indexPatterns);
+        const indexPatterns = getIndexPatterns(server);
+        setupCapabilities = await getCollectionStatus(req, indexPatterns, req.params.clusterUuid);
       } catch (err) {
         throw handleError(err, req);
       }
 
-      return clusters;
+      return setupCapabilities;
     }
   });
 }
