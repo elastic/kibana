420b8064efd6a92feb1000ad0a277ffdd30129a6
Michael Marcialis <michael.marcialis@elastic.co>
2019-04-08 13:52:56 -0400

    add test for header_page

---
+++ x-pack/plugins/siem/public/components/header_page/index.test.tsx
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+
+import { HeaderPage } from './index';
+
+test('renders correctly', () => {
+  const HeaderPageComponent = shallow(<HeaderPage subtitle="My Subtitle" title="My Title" />);
+  expect(toJson(HeaderPageComponent)).toMatchSnapshot();
+});
