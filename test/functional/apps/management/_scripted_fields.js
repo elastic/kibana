26caecae049d09178eaf36771a4447df6eafb01c
Liza Katz <liza.katz@elastic.co>
2019-04-08 18:37:26 +0300

    Test for bug #33251

--- test/functional/apps/management/_scripted_fields.js
+++ test/functional/apps/management/_scripted_fields.js
@@ -73,6 +73,33 @@ export default function ({ getService, getPageObjects }) {
       });
     });
 
+
+    describe.only('testing regression for issue #33251', function describeIndexTests() {
+      const scriptedPainlessFieldName = 'ram_Pain_reg';
+
+      it('should create and edit scripted field', async function () {
+        await PageObjects.settings.navigateTo();
+        await PageObjects.settings.clickKibanaIndexPatterns();
+        await PageObjects.settings.clickIndexPatternLogstash();
+        const startingCount = parseInt(await PageObjects.settings.getScriptedFieldsTabCount());
+        await PageObjects.settings.clickScriptedFieldsTab();
+        await log.debug('add scripted field');
+        const script = `1`;
+        await PageObjects.settings.addScriptedField(scriptedPainlessFieldName, 'painless', 'number', null, '1', script);
+        await retry.try(async function () {
+          expect(parseInt(await PageObjects.settings.getScriptedFieldsTabCount())).to.be(startingCount + 1);
+        });
+
+        for (let i = 0; i < 3; i++) {
+          await PageObjects.settings.editScriptedField(scriptedPainlessFieldName);
+          const fieldSaveButton = await testSubjects.exists('fieldSaveButton');
+          expect(fieldSaveButton).to.be(true);
+          await PageObjects.settings.clickSaveScriptedField();
+        }
+      });
+    });
+
+
     describe('creating and using Painless numeric scripted fields', function describeIndexTests() {
       const scriptedPainlessFieldName = 'ram_Pain1';
 
