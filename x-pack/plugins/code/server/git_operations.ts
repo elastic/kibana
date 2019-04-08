aedf41ca5154f5681f6e0c51a74c454c339d3e7c
Fuyao Zhao <zfy0701@gmail.com>
2019-04-08 11:08:56 -0700

    [Code]: disable more eslint error due to nodegit

--- x-pack/plugins/code/server/git_operations.ts
+++ x-pack/plugins/code/server/git_operations.ts
@@ -4,6 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+/* eslint-disable @typescript-eslint/camelcase */
+
 import {
   Blame,
   Commit,
@@ -280,9 +282,7 @@ export class GitOperations {
       const patches = await diff.patches();
       for (const patch of patches) {
         const { total_deletions, total_additions } = patch.lineStats();
-        // eslint-disable-next-line @typescript-eslint/camelcase
         commitDiff.additions += total_additions;
-        // eslint-disable-next-line @typescript-eslint/camelcase
         commitDiff.deletions += total_deletions;
         if (patch.isAdded()) {
           const path = patch.newFile().path();
