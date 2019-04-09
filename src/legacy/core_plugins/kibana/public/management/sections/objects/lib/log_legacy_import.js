abe6055aa7940d2b07a675a2369d337a13840545
Mike Côté <mikecote@users.noreply.github.com>
2019-04-09 12:51:34 -0400

    Convert saved objects UI to use new import / export API (#33513) (#34802)
    * Initial work converting UI to use new server side APIs
    
    * Remove missed file
    
    * Fix jest tests
    
    * Code cleanup pt1
    
    * Fix file casing
    
    * Fix jest tests
    
    * Modify UI to support including nested references
    
    * Fix button layout
    
    * Connect includeReferencesDeep, remove references_missing_references logic
    
    * Fix broken tests
    
    * Cleanup
    
    * Display success notifications and auto close modals on export
    
    * More code cleanup
    
    * Log to server when user imports using legacy .json file
    
    * Final cleanup
    
    * Update test snapshots to match updated text
    
    * Apply PR feedback pt1
    
    * Remove isLoading and wasImportSuccessful state variables, use single status variable instead
    
    * Move business logic out of flyout component
    
    * Apply PR feedback
    
    * Update wordings

--- src/legacy/core_plugins/kibana/public/management/sections/objects/lib/log_legacy_import.js
+++ src/legacy/core_plugins/kibana/public/management/sections/objects/lib/log_legacy_import.js
@@ -17,20 +17,11 @@
  * under the License.
  */
 
-import { scanAllTypes } from '../scan_all_types';
+import { kfetch } from 'ui/kfetch';
 
-jest.mock('ui/chrome', () => ({
-  addBasePath: () => 'apiUrl',
-}));
-
-describe('scanAllTypes', () => {
-  it('should call the api', async () => {
-    const $http = {
-      post: jest.fn().mockImplementation(() => ([]))
-    };
-    const typesToInclude = ['index-pattern', 'dashboard'];
-
-    await scanAllTypes($http, typesToInclude);
-    expect($http.post).toBeCalledWith('apiUrl/export', { typesToInclude });
+export async function logLegacyImport() {
+  return await kfetch({
+    method: 'POST',
+    pathname: '/api/saved_objects/_log_legacy_import',
   });
-});
+}
