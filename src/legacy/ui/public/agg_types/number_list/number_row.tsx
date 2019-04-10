18c72d4655f988f9c92746e079b835859fc23de6
maryia-lapata <mary.lopato@gmail.com>
2019-04-10 13:22:52 +0300

    Add number row

---
+++ src/legacy/ui/public/agg_types/number_list/number_row.tsx
@@ -0,0 +1,65 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, { useState } from 'react';
+import { isUndefined } from 'lodash';
+
+import { EuiFieldNumber, EuiSpacer, EuiButton, EuiFlexGroup, EuiFlexItem, EuiToolTip, EuiButtonIcon } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { parseRange, Range } from '../../utils/range';
+
+interface NumberRowProps {
+  model: { id: string, value: number };
+  disableDelete: boolean;
+  labelledbyId: string;
+  onChange(): void;
+  onDelete(): void;
+}
+
+function NumberRow({ model: { id, value }, labelledbyId, onDelete, onChange }: NumberRowProps) {
+  
+
+  return (
+    <EuiFlexGroup key={index} responsive={false} alignItems="center">
+      <EuiFlexItem>
+        <EuiFieldNumber key={number.toString()}
+          aria-labelledby={labelledbyId}
+          onChange={onChangeValue}
+          value={number}
+          fullWidth={true}
+          min={numberRange.min}
+          max={numberRange.max}
+        />
+      </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiToolTip content="Remove this rank value">
+            <EuiButtonIcon
+              aria-label="Remove this rank value"
+              color="danger"
+              iconType="trash"
+              onClick={() => onDelete(index)}
+              disabled={numberList.length === 1}
+            />
+          </EuiToolTip>
+        </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+
+export { NumberRow };
