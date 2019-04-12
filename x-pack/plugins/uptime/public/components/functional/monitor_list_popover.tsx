b9109227c241624480d018450a2525ef4fd7d44f
Justin Kambic <justin.kambic@elastic.co>
2019-04-12 14:59:22 -0400

    Import settings values from context.

--- x-pack/plugins/uptime/public/components/functional/monitor_list_popover.tsx
+++ x-pack/plugins/uptime/public/components/functional/monitor_list_popover.tsx
@@ -5,24 +5,22 @@
  */
 
 import { EuiFlexGroup, EuiFlexItem, EuiLink } from '@elastic/eui';
-import { LatestMonitor } from '../../../common/graphql/types';
 import { EuiPopover, EuiButtonIcon, EuiToolTip } from '@elastic/eui';
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { get } from 'lodash';
+import { LatestMonitor } from '../../../common/graphql/types';
+import { UptimeSettingsContext } from '../../contexts';
 
 interface MonitorListPopoverProps {
   basePath: string;
-  dateRangeStart: string;
-  dateRangeEnd: string;
   monitor: LatestMonitor;
 }
 
-export const MonitorListPopover = ({ basePath, dateRangeStart, dateRangeEnd, monitor }: MonitorListPopoverProps) => {
+export const MonitorListPopover = ({ monitor }: MonitorListPopoverProps) => {
   // TODO: support basePath
   const [isPopoverVisible, setIsPopoverVisible] = useState<boolean>(false);
+  const { basePath, dateRangeStart, dateRangeEnd } = useContext(UptimeSettingsContext);
   const domain = get<string | null>(monitor, 'ping.url.domain', null);
-  const drs = 'now-15m';
-  const dre = 'now';
 
   return (
     <EuiPopover
@@ -42,7 +40,12 @@ export const MonitorListPopover = ({ basePath, dateRangeStart, dateRangeEnd, mon
       <EuiFlexGroup gutterSize="m" direction="column">
         <EuiFlexItem grow={false}>
           <EuiToolTip position="top" content={`${monitor.id.url} APM integration`}>
-            <EuiLink href={`${basePath}/app/apm#/services?kuery=${encodeURI(`url.domain: "${domain}"`)}&rangeFrom=${drs}&rangeTo=${dre}`} target="_blank">
+            <EuiLink
+              href={`${basePath}/app/apm#/services?kuery=${encodeURI(
+                `url.domain: "${domain}"`
+              )}&rangeFrom=${dateRangeStart}&rangeTo=${dateRangeEnd}`}
+              target="_blank"
+            >
               <EuiButtonIcon aria-label="WARNINGTHISAPM" iconType="apmApp" size="l" />
             </EuiLink>
           </EuiToolTip>
