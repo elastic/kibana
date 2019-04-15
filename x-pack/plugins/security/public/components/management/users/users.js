58f5aaaaccc5a34418dfc44a701f71dfa94dbb7a
Larry Gregory <lgregorydev@gmail.com>
2019-04-15 16:55:56 -0400

    Convert account screen to React/EUI (#30977)
    * WIP account management redesign
    
    * style updates
    
    * start implementing change password logic
    
    * restyle
    
    * remove api key management section
    
    * improved change password validation
    
    * first round of design edits
    
    * cleanup and testing
    
    * fix import
    
    * fix translations
    
    * fix error handling on user management page
    
    * consolidate password change logic
    
    * fix tests
    
    * happy linter, happy life
    
    * finish change password test
    
    * removes unused translations
    
    * fix typo in test
    
    * fix change password functional test
    
    * Design edits (#19)
    
    - Made `fullWidth`
    - Added a consistent password requirement help text
    - Use `title` for toast
    - Change username/email to us `dl`
    - Don’t use html elements in tests
    
    * clear password form on success
    
    * copy edits
    
    * fix handling of Change Password button
    
    * use encodeURIComponent for user supplied data

--- x-pack/plugins/security/public/components/management/users/users.js
+++ x-pack/plugins/security/public/components/management/users/users.js
@@ -20,6 +20,7 @@ import {
 import { toastNotifications } from 'ui/notify';
 import { ConfirmDelete } from './confirm_delete';
 import { injectI18n, FormattedMessage } from '@kbn/i18n/react';
+import { UserAPIClient } from '../../../lib/api';
 
 class UsersUI extends Component {
   constructor(props) {
@@ -44,19 +45,18 @@ class UsersUI extends Component {
     });
   };
   async loadUsers() {
-    const { apiClient } = this.props;
     try {
-      const users = await apiClient.getUsers();
+      const users = await UserAPIClient.getUsers();
       this.setState({ users });
     } catch (e) {
-      if (e.status === 403) {
+      if (e.body.statusCode === 403) {
         this.setState({ permissionDenied: true });
       } else {
         toastNotifications.addDanger(
           this.props.intl.formatMessage({
             id: 'xpack.security.management.users.fetchingUsersErrorMessage',
             defaultMessage: 'Error fetching users: {message}'
-          }, { message: e.data.message })
+          }, { message: e.body.message })
         );
       }
     }
@@ -88,7 +88,7 @@ class UsersUI extends Component {
   }
   render() {
     const { users, filter, permissionDenied, showDeleteConfirmation, selection } = this.state;
-    const { apiClient, intl } = this.props;
+    const { intl } = this.props;
     if (permissionDenied) {
       return (
         <div className="secUsersListingPage">
@@ -251,7 +251,6 @@ class UsersUI extends Component {
             {showDeleteConfirmation ? (
               <ConfirmDelete
                 onCancel={this.onCancelDelete}
-                apiClient={apiClient}
                 usersToDelete={selection.map((user) => user.username)}
                 callback={this.handleDelete}
               />
