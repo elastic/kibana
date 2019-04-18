ec4100d46fada5acb19e3062403daa6df55c1682
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-04-18 09:36:54 -0400

    Improves metadata component tests and removes giant snapshots

--- x-pack/plugins/apm/public/components/shared/MetadataTable/ErrorMetadata/__test__/ErrorMetadata.test.tsx
+++ x-pack/plugins/apm/public/components/shared/MetadataTable/ErrorMetadata/__test__/ErrorMetadata.test.tsx
@@ -9,6 +9,10 @@ import { ErrorMetadata } from '..';
 import { render, cleanup } from 'react-testing-library';
 import { APMError } from '../../../../../../typings/es_schemas/ui/APMError';
 import 'jest-dom/extend-expect';
+import {
+  expectTextsInDocument,
+  expectTextsNotInDocument
+} from '../../../../../utils/testHelpers';
 
 function getError() {
   return ({
@@ -21,9 +25,9 @@ function getError() {
     agent: { someKey: 'agent value' },
     url: { someKey: 'url value' },
     user: { someKey: 'user value' },
-    notIncluded: true,
+    notIncluded: 'not included value',
     error: {
-      notIncluded: true,
+      notIncluded: 'error not included value',
       custom: {
         someKey: 'custom value'
       }
@@ -31,55 +35,92 @@ function getError() {
   } as unknown) as APMError;
 }
 
-function expectNotInDocument(output: any, text: string) {
-  try {
-    output.getByText(text);
-  } catch (err) {
-    if (err.message.startsWith('Unable to find an element with the text:')) {
-      return;
-    } else {
-      throw err;
-    }
-  }
-
-  throw new Error(`Unexpected text found: ${text}`);
-}
-
 describe('ErrorMetadata', () => {
   afterEach(cleanup);
 
-  it('should render a transaction with all sections', () => {
+  it('should render a error with all sections', () => {
     const error = getError();
     const output = render(<ErrorMetadata error={error} />);
-    expect(output).toMatchSnapshot();
 
-    expect(output.getByText('Labels')).toBeInTheDocument();
-    expect(output.getByText('HTTP')).toBeInTheDocument();
-    expect(output.getByText('Host')).toBeInTheDocument();
-    expect(output.getByText('Container')).toBeInTheDocument();
-    expect(output.getByText('Service')).toBeInTheDocument();
-    expect(output.getByText('Process')).toBeInTheDocument();
-    expect(output.getByText('Agent')).toBeInTheDocument();
-    expect(output.getByText('URL')).toBeInTheDocument();
-    expect(output.getByText('User')).toBeInTheDocument();
-    expect(output.getByText('Custom')).toBeInTheDocument();
+    // sections
+    expectTextsInDocument(output, [
+      'Labels',
+      'HTTP',
+      'Host',
+      'Container',
+      'Service',
+      'Process',
+      'Agent',
+      'URL',
+      'User',
+      'Custom'
+    ]);
   });
 
-  it('should render a transaction with only the required sections', () => {
+  it('should render a error with all included dot notation keys', () => {
+    const error = getError();
+    const output = render(<ErrorMetadata error={error} />);
+
+    // included keys
+    expectTextsInDocument(output, [
+      'labels.someKey',
+      'http.someKey',
+      'host.someKey',
+      'container.someKey',
+      'service.someKey',
+      'process.someKey',
+      'agent.someKey',
+      'url.someKey',
+      'user.someKey',
+      'error.custom.someKey'
+    ]);
+
+    // excluded keys
+    expectTextsNotInDocument(output, ['notIncluded', 'error.notIncluded']);
+  });
+
+  it('should render a error with all included values', () => {
+    const error = getError();
+    const output = render(<ErrorMetadata error={error} />);
+
+    // included values
+    expectTextsInDocument(output, [
+      'labels value',
+      'http value',
+      'host value',
+      'container value',
+      'service value',
+      'process value',
+      'agent value',
+      'url value',
+      'user value',
+      'custom value'
+    ]);
+
+    // excluded values
+    expectTextsNotInDocument(output, [
+      'not included value',
+      'error not included value'
+    ]);
+  });
+
+  it('should render a error with only the required sections', () => {
     const error = {} as APMError;
     const output = render(<ErrorMetadata error={error} />);
-    expect(output).toMatchSnapshot();
 
-    expect(output.getByText('Labels')).toBeInTheDocument();
-    expect(output.getByText('User')).toBeInTheDocument();
+    // required sections should be found
+    expectTextsInDocument(output, ['Labels', 'User']);
 
-    expectNotInDocument(output, 'HTTP');
-    expectNotInDocument(output, 'Host');
-    expectNotInDocument(output, 'Container');
-    expectNotInDocument(output, 'Service');
-    expectNotInDocument(output, 'Process');
-    expectNotInDocument(output, 'Agent');
-    expectNotInDocument(output, 'URL');
-    expectNotInDocument(output, 'Custom');
+    // optional sections should NOT be found
+    expectTextsNotInDocument(output, [
+      'HTTP',
+      'Host',
+      'Container',
+      'Service',
+      'Process',
+      'Agent',
+      'URL',
+      'Custom'
+    ]);
   });
 });
--- x-pack/plugins/apm/public/components/shared/MetadataTable/TransactionMetadata/__test__/TransactionMetadata.test.tsx
+++ x-pack/plugins/apm/public/components/shared/MetadataTable/TransactionMetadata/__test__/TransactionMetadata.test.tsx
@@ -9,6 +9,10 @@ import { TransactionMetadata } from '..';
 import { render, cleanup } from 'react-testing-library';
 import { Transaction } from '../../../../../../typings/es_schemas/ui/Transaction';
 import 'jest-dom/extend-expect';
+import {
+  expectTextsInDocument,
+  expectTextsNotInDocument
+} from '../../../../../utils/testHelpers';
 
 function getTransaction() {
   return ({
@@ -21,28 +25,14 @@ function getTransaction() {
     agent: { someKey: 'agent value' },
     url: { someKey: 'url value' },
     user: { someKey: 'user value' },
-    notIncluded: true,
+    notIncluded: 'not included value',
     transaction: {
-      notIncluded: true,
+      notIncluded: 'transaction not included value',
       custom: {
         someKey: 'custom value'
       }
     }
   } as unknown) as Transaction;
-}
-
-function expectNotInDocument(output: any, text: string) {
-  try {
-    output.getByText(text);
-  } catch (err) {
-    if (err.message.startsWith('Unable to find an element with the text:')) {
-      return;
-    } else {
-      throw err;
-    }
-  }
-
-  throw new Error(`Unexpected text found: ${text}`);
 }
 
 describe('TransactionMetadata', () => {
@@ -51,35 +41,89 @@ describe('TransactionMetadata', () => {
   it('should render a transaction with all sections', () => {
     const transaction = getTransaction();
     const output = render(<TransactionMetadata transaction={transaction} />);
-    expect(output).toMatchSnapshot();
 
-    expect(output.getByText('Labels')).toBeInTheDocument();
-    expect(output.getByText('HTTP')).toBeInTheDocument();
-    expect(output.getByText('Host')).toBeInTheDocument();
-    expect(output.getByText('Container')).toBeInTheDocument();
-    expect(output.getByText('Service')).toBeInTheDocument();
-    expect(output.getByText('Process')).toBeInTheDocument();
-    expect(output.getByText('Agent')).toBeInTheDocument();
-    expect(output.getByText('URL')).toBeInTheDocument();
-    expect(output.getByText('User')).toBeInTheDocument();
-    expect(output.getByText('Custom')).toBeInTheDocument();
+    // sections
+    expectTextsInDocument(output, [
+      'Labels',
+      'HTTP',
+      'Host',
+      'Container',
+      'Service',
+      'Process',
+      'Agent',
+      'URL',
+      'User',
+      'Custom'
+    ]);
+  });
+
+  it('should render a transaction with all included dot notation keys', () => {
+    const transaction = getTransaction();
+    const output = render(<TransactionMetadata transaction={transaction} />);
+
+    // included keys
+    expectTextsInDocument(output, [
+      'labels.someKey',
+      'http.someKey',
+      'host.someKey',
+      'container.someKey',
+      'service.someKey',
+      'process.someKey',
+      'agent.someKey',
+      'url.someKey',
+      'user.someKey',
+      'transaction.custom.someKey'
+    ]);
+
+    // excluded keys
+    expectTextsNotInDocument(output, [
+      'notIncluded',
+      'transaction.notIncluded'
+    ]);
+  });
+
+  it('should render a transaction with all included values', () => {
+    const transaction = getTransaction();
+    const output = render(<TransactionMetadata transaction={transaction} />);
+
+    // included values
+    expectTextsInDocument(output, [
+      'labels value',
+      'http value',
+      'host value',
+      'container value',
+      'service value',
+      'process value',
+      'agent value',
+      'url value',
+      'user value',
+      'custom value'
+    ]);
+
+    // excluded values
+    expectTextsNotInDocument(output, [
+      'not included value',
+      'transaction not included value'
+    ]);
   });
 
   it('should render a transaction with only the required sections', () => {
     const transaction = {} as Transaction;
     const output = render(<TransactionMetadata transaction={transaction} />);
-    expect(output).toMatchSnapshot();
 
-    expect(output.getByText('Labels')).toBeInTheDocument();
-    expect(output.getByText('User')).toBeInTheDocument();
+    // required sections should be found
+    expectTextsInDocument(output, ['Labels', 'User']);
 
-    expectNotInDocument(output, 'HTTP');
-    expectNotInDocument(output, 'Host');
-    expectNotInDocument(output, 'Container');
-    expectNotInDocument(output, 'Service');
-    expectNotInDocument(output, 'Process');
-    expectNotInDocument(output, 'Agent');
-    expectNotInDocument(output, 'URL');
-    expectNotInDocument(output, 'Custom');
+    // optional sections should NOT be found
+    expectTextsNotInDocument(output, [
+      'HTTP',
+      'Host',
+      'Container',
+      'Service',
+      'Process',
+      'Agent',
+      'URL',
+      'Custom'
+    ]);
   });
 });
