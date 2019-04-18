2468edffac85a887f65a4e67c59734a480cbf46c
Matt Kime <matt@mattki.me>
2019-04-18 17:28:56 -0500

    move checks-reporter-with-killswitch to its own file

--- src/dev/ci_setup/setup.sh
+++ src/dev/ci_setup/setup.sh
@@ -186,15 +186,4 @@ fi
 ###
 export CHECKS_REPORTER_ACTIVE=true
 
-###
-### Implements github-checks-reporter kill switch when scripts are called from the command line
-### $* - all the arguments
-### ${*[@]:2} - all the arguments, minus the first one
-###
-function checks-reporter-with-killswitch() {
-  if [ "$CHECKS_REPORTER_ACTIVE" = true ] ; then
-    yarn run github-checks-reporter $*
-  else
-    ${*[@]:2}
-  fi
-}
+source functions.sh
