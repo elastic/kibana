62a81a08f2a78eb36267099f5639aa4c61ab418f
Matthew Kime <matt@mattki.me>
2019-04-18 22:41:30 -0500

    Update setup.sh

--- src/dev/ci_setup/setup.sh
+++ src/dev/ci_setup/setup.sh
@@ -198,7 +198,8 @@ function checks-reporter-with-killswitch() {
   if [ "$CHECKS_REPORTER_ACTIVE" = true ] ; then
     yarn run github-checks-reporter "$@"
   else
-    "${*[@]:2}"
+    arguments=("$@");
+    ${arguments[@]:1};
   fi
 }
 
