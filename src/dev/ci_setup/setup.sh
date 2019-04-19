f3d49edf7fc3ed535be738eb0e829a12cae5d765
Spencer <email@spalger.com>
2019-04-19 11:07:22 -0700

    [shellcheck] fix warnings (#35328)

--- src/dev/ci_setup/setup.sh
+++ src/dev/ci_setup/setup.sh
@@ -52,7 +52,7 @@ if [[ "$UNAME" = *"MINGW64_NT"* ]]; then
 fi
 echo " -- Running on OS: $OS"
 
-nodeVersion="$(cat $dir/.node-version)"
+nodeVersion="$(cat "$dir/.node-version")"
 nodeDir="$cacheDir/node/$nodeVersion"
 
 if [[ "$OS" == "win" ]]; then
@@ -66,7 +66,7 @@ fi
 echo " -- node: version=v${nodeVersion} dir=$nodeDir"
 
 echo " -- setting up node.js"
-if [ -x "$nodeBin/node" ] && [ "$($nodeBin/node --version)" == "v$nodeVersion" ]; then
+if [ -x "$nodeBin/node" ] && [ "$("$nodeBin/node" --version)" == "v$nodeVersion" ]; then
   echo " -- reusing node.js install"
 else
   if [ -d "$nodeDir" ]; then
@@ -78,8 +78,8 @@ else
   mkdir -p "$nodeDir"
   if [[ "$OS" == "win" ]]; then
     nodePkg="$nodeDir/${nodeUrl##*/}"
-    curl --silent -o $nodePkg $nodeUrl
-    unzip -qo $nodePkg -d $nodeDir
+    curl --silent -o "$nodePkg" "$nodeUrl"
+    unzip -qo "$nodePkg" -d "$nodeDir"
     mv "${nodePkg%.*}" "$nodeBin"
   else
     curl --silent "$nodeUrl" | tar -xz -C "$nodeDir" --strip-components=1
@@ -95,7 +95,7 @@ export PATH="$nodeBin:$PATH"
 ### downloading yarn
 ###
 yarnVersion="$(node -e "console.log(String(require('./package.json').engines.yarn || '').replace(/^[^\d]+/,''))")"
-npm install -g yarn@^${yarnVersion}
+npm install -g "yarn@^${yarnVersion}"
 
 ###
 ### setup yarn offline cache
