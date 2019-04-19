271e0283d8c5733c3cabda3f2c1dce5e3867bfaf
joe fleming <joe.fleming@gmail.com>
2019-04-19 15:24:12 -0700

    feat: add ungrouped arg to filters

--- x-pack/plugins/canvas/public/functions/filters.js
+++ x-pack/plugins/canvas/public/functions/filters.js
@@ -11,9 +11,18 @@ import { get } from 'lodash';
 import { getState } from '../state/store';
 import { getGlobalFilters } from '../state/selectors/workpad';
 
-function getFiltersByGroup(filters, groups = []) {
+function getFiltersByGroup(filters, groups = [], ungrouped = false) {
   if (!groups || groups.length === 0) {
-    return filters;
+    if (!ungrouped) {
+      return filters;
+    }
+
+    // remove all filters that belong to a group
+    return filters.filter(filter => {
+      const ast = fromExpression(filter);
+      const expGroups = get(ast, 'chain[0].arguments.filterGroup', []);
+      return expGroups.length === 0;
+    });
   }
 
   return filters.filter(filter => {
@@ -36,10 +45,16 @@ export const filters = () => ({
       help: 'The name of the filter group to use',
       multi: true,
     },
+    ungrouped: {
+      aliases: ['nogroup', 'nogroups'],
+      types: ['boolean'],
+      help: "Don't include filters that belong to groups",
+      default: false,
+    },
   },
   help: 'Collect element filters on the workpad, usually to provide them to a data source',
-  fn: (_, { group }) => {
-    const filterList = getFiltersByGroup(getGlobalFilters(getState()), group);
+  fn: (_, { group, ungrouped }) => {
+    const filterList = getFiltersByGroup(getGlobalFilters(getState()), group, ungrouped);
 
     if (filterList && filterList.length) {
       const filterExpression = filterList.join(' | ');
