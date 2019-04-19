144c2f0868d35cbd237e5e3b37d8ea49915232ca
Justin Kambic <justin.kambic@elastic.co>
2019-04-19 16:26:08 -0400

    Add functions for parsing supported URL parameters.

---
+++ x-pack/plugins/uptime/public/lib/helper/url_params/get_supported_url_params.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { parseIsPaused } from './parse_is_paused';
+import { parseAutorefreshInterval } from './parse_autorefresh_interval';
+import { CLIENT_DEFAULTS } from '../../../../common/constants';
+
+export interface UptimeUrlParams {
+  autorefreshInterval: number;
+  autorefreshIsPaused: boolean;
+  dateRangeStart: string;
+  dateRangeEnd: string;
+}
+
+const {
+  AUTOREFRESH_INTERVAL,
+  AUTOREFRESH_IS_PAUSED,
+  DATE_RANGE_START,
+  DATE_RANGE_END,
+} = CLIENT_DEFAULTS;
+
+export const getSupportedUrlParams = (params: {
+  [key: string]: string | undefined;
+}): UptimeUrlParams => {
+  const { autorefreshInterval, autorefreshIsPaused, rangeFrom, rangeTo } = params;
+
+  return {
+    autorefreshInterval: parseAutorefreshInterval(autorefreshInterval, AUTOREFRESH_INTERVAL),
+    autorefreshIsPaused: parseIsPaused(autorefreshIsPaused, AUTOREFRESH_IS_PAUSED),
+    dateRangeStart: rangeFrom || DATE_RANGE_START,
+    dateRangeEnd: rangeTo || DATE_RANGE_END,
+  };
+};
