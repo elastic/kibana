9d9359b9171343ce3728f2625871a8445d784c16
Yulong <allendragon@gmail.com>
2019-04-19 22:24:00 -0700

    [Code] pagination for history (#35329)

--- x-pack/plugins/code/public/components/main/commit_history.tsx
+++ x-pack/plugins/code/public/components/main/commit_history.tsx
@@ -5,6 +5,7 @@
  */
 
 import {
+  EuiButton,
   EuiFlexGroup,
   EuiFlexItem,
   EuiLoadingSpinner,
@@ -15,8 +16,12 @@ import {
 import _ from 'lodash';
 import moment from 'moment';
 import React from 'react';
+import { connect } from 'react-redux';
 import { CommitInfo } from '../../../model/commit';
 import { CommitLink } from '../diff_page/commit_link';
+import { RootState } from '../../reducers';
+import { hasMoreCommitsSelector, treeCommitsSelector } from '../../selectors';
+import { fetchMoreCommits } from '../../actions';
 
 const COMMIT_ID_LENGTH = 8;
 
@@ -75,20 +80,35 @@ export const CommitHistoryLoading = () => (
   </div>
 );
 
-export const CommitHistory = (props: {
+export const PageButtons = (props: {
+  loading?: boolean;
+  disabled: boolean;
+  onClick: () => void;
+}) => (
+  <EuiFlexGroup justifyContent="spaceAround">
+    <EuiFlexItem grow={false}>
+      <EuiButton
+        onClick={props.onClick}
+        iconType="arrowDown"
+        isLoading={props.loading}
+        isDisabled={props.disabled}
+        size="s"
+      >
+        More
+      </EuiButton>
+    </EuiFlexItem>
+  </EuiFlexGroup>
+);
+
+export const CommitHistoryComponent = (props: {
   commits: CommitInfo[];
   repoUri: string;
   header: React.ReactNode;
-  hideLoading?: boolean;
+  loadingCommits?: boolean;
+  showPagination?: boolean;
+  hasMoreCommit?: boolean;
+  fetchMoreCommits: any;
 }) => {
-  if (!props.commits) {
-    return (
-      <div className="codeContainer__commitMessages">
-        <h1>Commits</h1>
-        {!props.hideLoading && <CommitHistoryLoading />}
-      </div>
-    );
-  }
   const commits = _.groupBy(props.commits, commit => moment(commit.updated).format('YYYYMMDD'));
   const commitDates = Object.keys(commits).sort((a, b) => b.localeCompare(a)); // sort desc
   const commitList = commitDates.map(cd => (
@@ -103,6 +123,30 @@ export const CommitHistory = (props: {
     <div className="codeContainer__commitMessages">
       <div className="codeHeader__commit">{props.header}</div>
       {commitList}
+      {!props.showPagination && props.loadingCommits && <CommitHistoryLoading />}
+      {props.showPagination && (
+        <PageButtons
+          disabled={!props.hasMoreCommit || props.commits.length < 10}
+          onClick={() => props.fetchMoreCommits(props.repoUri)}
+          loading={props.loadingCommits}
+        />
+      )}
     </div>
   );
 };
+
+const mapStateToProps = (state: RootState) => ({
+  file: state.file.file,
+  commits: treeCommitsSelector(state) || [],
+  loadingCommits: state.file.loadingCommits,
+  hasMoreCommit: hasMoreCommitsSelector(state),
+});
+
+const mapDispatchToProps = {
+  fetchMoreCommits,
+};
+export const CommitHistory = connect(
+  mapStateToProps,
+  mapDispatchToProps
+  // @ts-ignore
+)(CommitHistoryComponent);
