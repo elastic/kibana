5147e0a9f6d8544532901448061bcb12b9d8f6e0
Aaron Caldwell <aaronjcaldwell@gmail.com>
2019-04-22 09:00:10 -0600

    Move file picker to separate component

---
+++ x-pack/plugins/file_upload/public/components/json_index_file_picker.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+
+import React from 'react';
+import {
+  EuiFilePicker,
+  EuiFormRow,
+} from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { parseFile } from '../util/file_parser';
+import { triggerIndexing } from '../util/indexing_service';
+
+export function JsonIndexFilePicker({
+  boolIndexData = false,
+  fileUploadMessage,
+  onFileUpload,
+  onFileRemove,
+  postParseJsonTransform,
+  onIndexAddSuccess,
+  onIndexAddError,
+  fileRef,
+  setFileRef,
+  parsedFile,
+  setParsedFile,
+  setIndexedFile,
+  preIndexTransform,
+  indexName,
+  indexDataType,
+}) {
+
+  return (
+    <EuiFormRow
+      label={(
+        <FormattedMessage
+          id="xpack.file_upload.filePickerLabel"
+          defaultMessage={
+            fileUploadMessage || 'Please select a Json file to upload'
+          }
+        />
+      )}
+    >
+      <EuiFilePicker
+        initialPromptText={(
+          <FormattedMessage
+            id="xpack.file_upload.filePicker"
+            defaultMessage="Upload file"
+          />
+        )}
+        onChange={async fileList => {
+          if (fileList.length === 0) { // Remove
+            setParsedFile(null);
+            onFileRemove && onFileRemove(fileRef);
+          } else if (fileList.length === 1) { // Parse & index file
+            const file = fileList[0];
+            setFileRef(file);
+            // Parse file
+            const parsedFileResult = await parseFile(
+              file, onFileUpload, postParseJsonTransform
+            );
+
+            // Immediately index file if flag set
+            if (file && boolIndexData) {
+              await triggerIndexing(parsedFile, preIndexTransform, indexName, indexDataType)
+                .then(
+                  resp => {
+                    if (resp.success) {
+                      setIndexedFile(parsedFile);
+                      onIndexAddSuccess && onIndexAddSuccess(resp);
+                    } else {
+                      setIndexedFile(null);
+                      onIndexAddError && onIndexAddError();
+                    }
+                  });
+            }
+
+            // Save parsed result
+            setParsedFile(parsedFileResult);
+          } else { // TODO: Support multiple file upload?
+            console.warn('Multiple file upload not currently supported');
+          }
+        }}
+      />
+    </EuiFormRow>
+  );
+}
