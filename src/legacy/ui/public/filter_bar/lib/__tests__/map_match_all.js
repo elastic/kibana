76687c0a0d3a7bc7b3e4702fbf4dced553c6f42e
Liza Katz <liza.katz@elastic.co>
2019-04-22 14:41:27 +0300

    Fixing filter bar lib tests 1

--- src/legacy/ui/public/filter_bar/lib/__tests__/map_match_all.js
+++ src/legacy/ui/public/filter_bar/lib/__tests__/map_match_all.js
@@ -19,19 +19,15 @@
 
 import expect from '@kbn/expect';
 import ngMock from 'ng_mock';
-import { FilterBarLibMapMatchAllProvider } from '../map_match_all';
+import { checkIsMatchAll } from '../map_match_all';
 
 describe('ui/filter_bar/lib', function () {
   describe('mapMatchAll()', function () {
-    let $rootScope;
-    let mapMatchAll;
     let filter;
 
 
     beforeEach(ngMock.module('kibana'));
-    beforeEach(ngMock.inject(function (Private, _$rootScope_) {
-      $rootScope = _$rootScope_;
-      mapMatchAll = Private(FilterBarLibMapMatchAllProvider);
+    beforeEach(ngMock.inject(function () {
       filter = {
         match_all: {},
         meta: {
@@ -44,19 +40,20 @@ describe('ui/filter_bar/lib', function () {
     describe('when given a filter that is not match_all', function () {
       it('filter is rejected', function (done) {
         delete filter.match_all;
-        mapMatchAll(filter).catch(result => {
+        checkIsMatchAll(filter).catch(result => {
           expect(result).to.be(filter);
           done();
         });
-        $rootScope.$apply();
       });
     });
 
     describe('when given a match_all filter', function () {
       let result;
-      beforeEach(function () {
-        mapMatchAll(filter).then(r => result = r);
-        $rootScope.$apply();
+      beforeEach(function (done) {
+        checkIsMatchAll(filter).then(r => {
+          result = r;
+          done();
+        });
       });
 
       it('key is set to meta field', function () {
