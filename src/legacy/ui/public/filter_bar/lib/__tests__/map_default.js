c19644cd1bf83d37a2d7a447a6b6ab7da665b0d7
Liza Katz <liza.katz@elastic.co>
2019-04-22 14:41:27 +0300

    Fixing filter bar lib tests 1

--- src/legacy/ui/public/filter_bar/lib/__tests__/map_default.js
+++ src/legacy/ui/public/filter_bar/lib/__tests__/map_default.js
@@ -18,28 +18,18 @@
  */
 
 import expect from '@kbn/expect';
-import ngMock from 'ng_mock';
-import { FilterBarLibMapDefaultProvider } from '../map_default';
+import { checkIsDefault } from '../map_default';
 
 describe('Filter Bar Directive', function () {
   describe('mapDefault()', function () {
 
-    let mapDefault;
-    let $rootScope;
-    beforeEach(ngMock.module('kibana'));
-    beforeEach(ngMock.inject(function (Private, _$rootScope_) {
-      $rootScope = _$rootScope_;
-      mapDefault = Private(FilterBarLibMapDefaultProvider);
-    }));
-
     it('should return the key and value for matching filters', function (done) {
       const filter = { query: { match_all: {} } };
-      mapDefault(filter).then(function (result) {
+      checkIsDefault(filter).then(function (result) {
         expect(result).to.have.property('key', 'query');
         expect(result).to.have.property('value', '{"match_all":{}}');
         done();
       });
-      $rootScope.$apply();
     });
 
     it('should work with undefined filter types', function (done) {
@@ -52,21 +42,19 @@ describe('Filter Bar Directive', function () {
           }
         }
       };
-      mapDefault(filter).then(function (result) {
+      checkIsDefault(filter).then(function (result) {
         expect(result).to.have.property('key', 'bool');
         expect(result).to.have.property('value', JSON.stringify(filter.bool));
         done();
       });
-      $rootScope.$apply();
     });
 
     it('should return undefined if there is no valid key', function (done) {
       const filter = { meta: {} };
-      mapDefault(filter).catch(function (result) {
+      checkIsDefault(filter).catch(function (result) {
         expect(result).to.be(filter);
         done();
       });
-      $rootScope.$apply();
     });
 
 
--- src/legacy/ui/public/filter_bar/lib/__tests__/map_missing.js
+++ src/legacy/ui/public/filter_bar/lib/__tests__/map_missing.js
@@ -17,39 +17,27 @@
  * under the License.
  */
 
-import ngMock from 'ng_mock';
 import expect from '@kbn/expect';
-import { FilterBarLibMapMissingProvider } from '../map_missing';
+import { checkIsMissing } from '../map_missing';
 
 describe('Filter Bar Directive', function () {
   describe('mapMissing()', function () {
 
-    let mapMissing;
-
-    let $rootScope;
-    beforeEach(ngMock.module('kibana'));
-    beforeEach(ngMock.inject(function (Private, _$rootScope_) {
-      $rootScope = _$rootScope_;
-      mapMissing = Private(FilterBarLibMapMissingProvider);
-    }));
-
     it('should return the key and value for matching filters', function (done) {
       const filter = { missing: { field: '_type' } };
-      mapMissing(filter).then(function (result) {
+      checkIsMissing(filter).then(function (result) {
         expect(result).to.have.property('key', '_type');
         expect(result).to.have.property('value', 'missing');
         done();
       });
-      $rootScope.$apply();
     });
 
     it('should return undefined for none matching', function (done) {
       const filter = { query: { match: { query: 'foo' } } };
-      mapMissing(filter).catch(function (result) {
+      checkIsMissing(filter).catch(function (result) {
         expect(result).to.be(filter);
         done();
       });
-      $rootScope.$apply();
     });
 
   });
