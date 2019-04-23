dc1a229b64c947d9908636696e979b3ae6ddf98d
Sébastien <sebastien.loix@elastic.co>
2019-04-23 18:46:32 +0200

    Move documentation to interface

--- x-pack/test_utils/testbed/testbed.ts
+++ x-pack/test_utils/testbed/testbed.ts
@@ -8,9 +8,9 @@ import { ComponentType, ReactWrapper } from 'enzyme';
 import { findTestSubject } from '../find_test_subject';
 import { reactRouterMock } from '../router_helpers';
 import { mountComponent, getJSXComponentWithProps } from './mount_component';
-import { TestBedConfig, TestBed, SetupFunc } from './types';
+import { TestBedConfig, TestBedOptions, TestBed, SetupFunc } from './types';
 
-const defaultOptions = {
+const defaultOptions: TestBedOptions = {
   memoryRouter: {
     wrapComponent: true,
   },
@@ -31,8 +31,9 @@ const defaultOptions = {
  */
 export const registerTestBed = <T extends string = string>(
   Component: ComponentType<any>,
-  { defaultProps = {}, options = defaultOptions, store = null }: TestBedConfig
+  config?: TestBedConfig
 ): SetupFunc<T> => {
+  const { defaultProps = {}, options = defaultOptions, store = null } = config || {};
   /**
    * In some cases, component have some logic that interacts with the react router
    * _before_ the component is mounted.(Class constructor() I'm looking at you :)
@@ -59,16 +60,6 @@ export const registerTestBed = <T extends string = string>(
      * ----------------------------------------------------------------
      */
 
-    /**
-     * Look for a data test subject in the component and return it.
-     * It is possible to target a nested test subject by separating it with a dot ('.');
-     *
-     * @param testSubject The data test subject to look for
-     *
-     * @example
-     * find('nameInput'); // if there is only 1 form, this is enough
-     * find('myForm.nameInput'); // if there are multiple forms, specify the test subject of the form
-     */
     const find: TestBed<T>['find'] = (testSubject: T) => {
       const testSubjectToArray = testSubject.split('.');
 
@@ -85,20 +76,9 @@ export const registerTestBed = <T extends string = string>(
       }, component);
     };
 
-    /**
-     * Look in the component if a data test subject exists, and return true or false
-     *
-     * @param testSubject The data test subject to look for
-     * @param count The number of times the subject needs to appear
-     */
     const exists: TestBed<T>['exists'] = (testSubject, count = 1) =>
       find(testSubject).length === count;
 
-    /**
-     * Update the props of the mounted component
-     *
-     * @param updatedProps The updated prop object
-     */
     const setProps: TestBed<T>['setProps'] = updatedProps => {
       if (options.memoryRouter.wrapComponent !== false) {
         throw new Error(
@@ -120,18 +100,6 @@ export const registerTestBed = <T extends string = string>(
      * ----------------------------------------------------------------
      */
 
-    /**
-     * Set the value of a form text input.
-     *
-     * In some cases, changing an input value triggers an HTTP request to validate
-     * the field. Even if we return immediately the response on the mock server we
-     * still need to wait until the next tick before the DOM updates.
-     * Setting isAsync to "true" takes care of that.
-     *
-     * @param input The form input. Can either be a data-test-subj or a reactWrapper
-     * @param value The value to set
-     * @param isAsync If set to true will return a Promise that resolves on the next "tick"
-     */
     const setInputValue: TestBed<T>['form']['setInputValue'] = (input, value, isAsync = false) => {
       const formInput = typeof input === 'string' ? find(input) : (input as ReactWrapper);
 
@@ -144,12 +112,6 @@ export const registerTestBed = <T extends string = string>(
       return new Promise(resolve => setTimeout(resolve));
     };
 
-    /**
-     * Select or unselect a form checkbox.
-     *
-     * @param dataTestSubject The test subject of the checkbox
-     * @param isChecked Defines if the checkobx is active or not
-     */
     const selectCheckBox: TestBed<T>['form']['selectCheckBox'] = (
       dataTestSubject,
       isChecked = true
@@ -157,18 +119,8 @@ export const registerTestBed = <T extends string = string>(
       find(dataTestSubject).simulate('change', { target: { checked: isChecked } });
     };
 
-    /**
-     * Toggle the EuiSwitch
-     */
     const toggleEuiSwitch: TestBed<T>['form']['toggleEuiSwitch'] = selectCheckBox; // Same API as "selectCheckBox"
 
-    /**
-     * The EUI ComboBox is a special input as it needs the ENTER key to be pressed
-     * in order to register the value set. This helpers automatically does that.
-     *
-     * @param comboBoxTestSubject The data test subject of the EuiComboBox
-     * @param value The value to set
-     */
     const setComboBoxValue: TestBed<T>['form']['setComboBoxValue'] = (
       comboBoxTestSubject,
       value
@@ -182,9 +134,6 @@ export const registerTestBed = <T extends string = string>(
       component.update();
     };
 
-    /**
-     * Get a list of the form error messages that are visible in the DOM of the component
-     */
     const getErrorsMessages: TestBed<T>['form']['getErrorsMessages'] = () => {
       const errorMessagesWrappers = component.find('.euiFormErrorText');
       return errorMessagesWrappers.map(err => err.text());
--- x-pack/test_utils/testbed/types.ts
+++ x-pack/test_utils/testbed/types.ts
@@ -26,18 +26,72 @@ export interface EuiTableMetaData {
 export interface TestBed<T> {
   /** The comonent under test */
   component: ReactWrapper;
+  /**
+   * Look in the component if a data test subject exists, and return true or false
+   *
+   * @param testSubject The data test subject to look for
+   * @param count The number of times the subject needs to appear in order to return "true"
+   */
   exists: (testSubject: T, count?: number) => boolean;
+  /**
+   * Look for a data test subject in the component and return it.
+   * It is possible to target a nested test subject by separating it with a dot ('.');
+   *
+   * @param testSubject The data test subject to look for
+   *
+   * @example
+   * find('nameInput'); // if there is only 1 form, this is enough
+   * find('myForm.nameInput'); // if there are multiple forms, specify the test subject of the form
+   */
   find: (testSubject: T) => ReactWrapper;
+  /**
+   * Update the props of the mounted component
+   *
+   * @param updatedProps The updated prop object
+   */
   setProps: (updatedProps: any) => void;
   form: {
+    /**
+     * Set the value of a form text input.
+     *
+     * In some cases, changing an input value triggers an HTTP request to validate
+     * the field. Even if we return immediately the response on the mock server we
+     * still need to wait until the next tick before the DOM updates.
+     * Setting isAsync to "true" takes care of that.
+     *
+     * @param input The form input. Can either be a data-test-subj or a reactWrapper
+     * @param value The value to set
+     * @param isAsync If set to true will return a Promise that resolves on the next "tick"
+     */
     setInputValue: (
       input: T | ReactWrapper,
       value: string,
       isAsync?: boolean
     ) => Promise<void> | void;
+    /**
+     * Select or unselect a form checkbox.
+     *
+     * @param dataTestSubject The test subject of the checkbox
+     * @param isChecked Defines if the checkobx is active or not
+     */
     selectCheckBox: (checkboxTestSubject: T, isChecked?: boolean) => void;
+    /**
+     * Toggle the EuiSwitch
+     *
+     * @param switchTestSubject The test subject of the EuiSwitch
+     */
     toggleEuiSwitch: (switchTestSubject: T) => void;
+    /**
+     * The EUI ComboBox is a special input as it needs the ENTER key to be pressed
+     * in order to register the value set. This helpers automatically does that.
+     *
+     * @param comboBoxTestSubject The data test subject of the EuiComboBox
+     * @param value The value to set
+     */
     setComboBoxValue: (comboBoxTestSubject: T, value: string) => void;
+    /**
+     * Get a list of the form error messages that are visible in the DOM of the component
+     */
     getErrorsMessages: () => string[];
   };
   table: {
@@ -51,7 +105,7 @@ export interface TestBedConfig {
   store: (() => Store) | Store | null;
 }
 
-interface TestBedOptions {
+export interface TestBedOptions {
   memoryRouter: {
     wrapComponent: boolean;
     initialEntries?: string[];
