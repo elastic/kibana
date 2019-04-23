6522e1f65728bcef6bdebdab54d4ced0ea2e5dd0
maryia-lapata <mary.lopato@gmail.com>
2019-04-23 18:05:02 +0300

    Add type for EditorConfig

--- src/legacy/ui/public/vis/editors/default/agg_param_editor_props.ts
+++ src/legacy/ui/public/vis/editors/default/agg_param_editor_props.ts
@@ -19,6 +19,7 @@
 
 import { AggParam } from '../../../agg_types';
 import { AggConfig } from '../../agg_config';
+import { EditorConfig } from '../config/types';
 
 // NOTE: we cannot export the interface with export { InterfaceName }
 // as there is currently a bug on babel typescript transform plugin for it
@@ -27,7 +28,7 @@ import { AggConfig } from '../../agg_config';
 export interface AggParamEditorProps<T> {
   agg: AggConfig;
   aggParam: AggParam;
-  editorConfig: any;
+  editorConfig: EditorConfig;
   indexedFields?: any[];
   isInvalid: boolean;
   value: T;
