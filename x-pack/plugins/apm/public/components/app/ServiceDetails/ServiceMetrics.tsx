1794c8b01c80697ffb23552f71438a1976996061
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-04-24 10:00:53 -0400

    [APM] useUrlParams as separate hook + removes redux 🎉 (#34792)
    * Exploring a useUrlParams hook separate from useLocation
    
    * Refactors url params hook into context + hook
    
    * Fixes unmount async bugs in some components
    
    * Rewrites datepicker tests
    
    * Fixes some import reference problems
    
    * Moves urlParams logic out of store and into context directory, updates references
    
    * Makes urlParams context update itself on location changes
    
    * Adds url params tests
    
    * Removed left-over debug additions
    
    * Small improvement for types based on review feedback :+1:
    
    * Converts url params to hooks for ErrorGroupDetails component
    
    * Converts url params to hooks for ServiceDetails component
    
    * Converts url params to hooks for ServiceOverview component
    
    * Converts url params to hooks for TraceOverview component
    
    * Converts url params to hooks for TransactionDetails component
    
    * Converts url params to hooks for KueryBar component (plus TS convert)
    
    * Cleanup removing redux
    
    * Fixes tests after removing redux
    
    * Updates type reference to use ui/ import
    
    * Moves selectors folder into public and deletes store folder
    
    * Removes unnecessary mountWithRouterAndStore
    
    * Correctly orders imports to satisfy lint gods
    
    * Removes unused imports

--- x-pack/plugins/apm/public/components/app/ServiceDetails/ServiceMetrics.tsx
+++ x-pack/plugins/apm/public/components/app/ServiceDetails/ServiceMetrics.tsx
@@ -18,7 +18,7 @@ import { useFetcher } from '../../../hooks/useFetcher';
 import { useServiceMetricCharts } from '../../../hooks/useServiceMetricCharts';
 import { useTransactionOverviewCharts } from '../../../hooks/useTransactionOverviewCharts';
 import { loadErrorDistribution } from '../../../services/rest/apm/error_groups';
-import { IUrlParams } from '../../../store/urlParams';
+import { IUrlParams } from '../../../context/UrlParamsContext/types';
 import { SyncChartGroup } from '../../shared/charts/SyncChartGroup';
 import { TransactionCharts } from '../../shared/charts/TransactionCharts';
 import { ErrorDistribution } from '../ErrorGroupDetails/Distribution';
