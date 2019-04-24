751e4a76f2fb748a196cbe5c823560d26761b4ae
Larry Gregory <larry.gregory@elastic.co>
2019-04-24 14:48:35 -0400

    fix import

--- x-pack/plugins/spaces/server/new_platform/index.ts
+++ x-pack/plugins/spaces/server/new_platform/index.ts
@@ -4,8 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Plugin } from './plugin';
-import { SpacesInitializerContext } from '../..';
+import { Plugin, SpacesInitializerContext } from './plugin';
 
 export function plugin(initializerContext: SpacesInitializerContext) {
   return new Plugin(initializerContext);
