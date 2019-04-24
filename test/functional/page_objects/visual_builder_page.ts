d0ba9379056a6fdebabc8db10e33c93b1e00b5f9
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2019-04-24 23:21:30 +0200

    fix tests

--- test/functional/page_objects/visual_builder_page.ts
+++ test/functional/page_objects/visual_builder_page.ts
@@ -37,6 +37,8 @@ export function VisualBuilderPageProvider({ getService, getPageObjects }: FtrPro
       await PageObjects.common.navigateToUrl('visualize', 'create?type=metrics');
       log.debug('Set absolute time range from "' + fromTime + '" to "' + toTime + '"');
       await PageObjects.timePicker.setAbsoluteRange(fromTime, toTime);
+      // TSVB bug: https://github.com/elastic/kibana/issues/35468
+      await PageObjects.common.sleep(5000);
     }
 
     public async checkTabIsLoaded(testSubj: string, name: string) {
