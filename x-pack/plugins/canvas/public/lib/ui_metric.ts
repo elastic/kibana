c2328d9ee12fc52bba16a2fbbd4f700b08a9186c
Corey Robertson <corey.robertson@elastic.co>
2019-04-26 21:23:50 -0400

    Add missing license

--- x-pack/plugins/canvas/public/lib/ui_metric.ts
+++ x-pack/plugins/canvas/public/lib/ui_metric.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
 import { fetch } from '../../common/lib/fetch';
 import { createUiMetricUri } from '../../../../common/ui_metric';
 
