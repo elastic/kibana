a10fa2de06d0e9e2b3fd9bfc956576d3a908736f
Clint Andrew Hall <clint.hall@elastic.co>
2019-04-29 12:13:57 -0400

    Convert to func

--- x-pack/plugins/canvas/canvas_plugin_src/functions/common/formatnumber.js
+++ x-pack/plugins/canvas/canvas_plugin_src/functions/common/formatnumber.js
@@ -6,24 +6,26 @@
 
 import numeral from '@elastic/numeral';
 
-export const formatnumber = () => ({
-  name: 'formatnumber',
-  type: 'string',
-  help: 'Turn a number into a string using a NumberJS format',
-  context: {
-    types: ['number'],
-  },
-  args: {
-    format: {
-      aliases: ['_'],
-      types: ['string'],
-      help: 'NumeralJS format string http://numeraljs.com/#format',
+export function formatnumber() {
+  return {
+    name: 'formatnumber',
+    type: 'string',
+    help: 'Turn a number into a string using a NumberJS format',
+    context: {
+      types: ['number'],
     },
-  },
-  fn: (context, args) => {
-    if (!args.format) {
-      return String(context);
-    }
-    return numeral(context).format(args.format);
-  },
-});
+    args: {
+      format: {
+        aliases: ['_'],
+        types: ['string'],
+        help: 'NumeralJS format string http://numeraljs.com/#format',
+      },
+    },
+    fn: (context, args) => {
+      if (!args.format) {
+        return String(context);
+      }
+      return numeral(context).format(args.format);
+    },
+  };
+}
--- x-pack/plugins/canvas/canvas_plugin_src/functions/common/string.js
+++ x-pack/plugins/canvas/canvas_plugin_src/functions/common/string.js
@@ -4,20 +4,22 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export const string = () => ({
-  name: 'string',
-  aliases: [],
-  type: 'string',
-  help:
-    'Output a string made of other strings. Mostly useful when combined with sub-expressions that output a string, ' +
-    ' or something castable to a string',
-  args: {
-    value: {
-      aliases: ['_'],
-      types: ['string'],
-      multi: true,
-      help: "One or more strings to join together. Don't forget spaces where needed!",
+export function string() {
+  return {
+    name: 'string',
+    aliases: [],
+    type: 'string',
+    help:
+      'Output a string made of other strings. Mostly useful when combined with sub-expressions that output a string, ' +
+      ' or something castable to a string',
+    args: {
+      value: {
+        aliases: ['_'],
+        types: ['string'],
+        multi: true,
+        help: "One or more strings to join together. Don't forget spaces where needed!",
+      },
     },
-  },
-  fn: (context, args) => args.value.join(''),
-});
+    fn: (context, args) => args.value.join(''),
+  };
+}
