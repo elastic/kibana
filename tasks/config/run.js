586b00e05ef7f93a885f8f29857ce33bc84f83e7
Matt Kime <matt@mattki.me>
2019-04-30 11:36:11 -0500

    lint lint

--- tasks/config/run.js
+++ tasks/config/run.js
@@ -65,10 +65,10 @@ module.exports = function (grunt) {
   const NODE = 'node';
   const scriptWithGithubChecks = ({ title, options, cmd, args }) => (
     process.env.CHECKS_REPORTER_ACTIVE === 'true' ? {
-        options,
-        cmd: 'yarn',
-        args: ['run', 'github-checks-reporter', title, cmd, ...args],
-      } : { options, cmd, args });
+      options,
+      cmd: 'yarn',
+      args: ['run', 'github-checks-reporter', title, cmd, ...args],
+    } : { options, cmd, args });
   const gruntTaskWithGithubChecks = (title, task) =>
     scriptWithGithubChecks({
       title,
