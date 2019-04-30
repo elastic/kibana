6be4e1a008904d7931d266599a77504a81698439
Chris Cowan <chris@chriscowan.us>
2019-04-30 12:49:30 -0700

    removing tsx extension; adding tests for createMetricLabel()

--- x-pack/plugins/infra/public/components/metrics_exploerer/create_metric_label.ts
+++ x-pack/plugins/infra/public/components/metrics_exploerer/create_metric_label.ts
@@ -4,10 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import {
-  MetricsExplorerAggregation,
-  MetricsExplorerMetric,
-} from '../../../server/routes/metrics_explorer/types';
+import { MetricsExplorerMetric } from '../../../server/routes/metrics_explorer/types';
 
 export const createMetricLabel = (metric: MetricsExplorerMetric) => {
   return `${metric.aggregation}(${metric.field || ''})`;
