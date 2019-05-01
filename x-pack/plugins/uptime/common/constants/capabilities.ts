a9037b986847741d121bccbbc8bf13e34908b69d
Justin Kambic <justin.kambic@elastic.co>
2019-05-01 11:21:47 -0400

    Add getter function for app availability for plugins that will be integrated with Uptime in the future. (#35543)

--- x-pack/plugins/uptime/common/constants/capabilities.ts
+++ x-pack/plugins/uptime/common/constants/capabilities.ts
@@ -4,8 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { CLIENT_DEFAULTS } from './client_defaults';
-export { INDEX_NAMES } from './index_names';
-export { INTEGRATED_SOLUTIONS } from './capabilities';
-export { PLUGIN } from './plugin';
-export { QUERY } from './query';
+export const INTEGRATED_SOLUTIONS = ['apm', 'infrastructure', 'logs'];
