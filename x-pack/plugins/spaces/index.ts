6d5f29deac5e36922a167888baa1e64c7520f95c
Peter <peter.schretlen@elastic.co>
2019-05-02 12:32:57 -0700

    enable security plugin in basic (#35892)
    enable security on file dataviz and import (ML plugin)
    
    update unit tests
    
    add api test coverage for security in basic
    
    move audit logging to standard+ license level

--- x-pack/plugins/spaces/index.ts
+++ x-pack/plugins/spaces/index.ts
@@ -135,7 +135,9 @@ export const spaces = (kibana: Record<string, any>) =>
 
       const config = server.config();
 
-      const spacesAuditLogger = new SpacesAuditLogger(config, new AuditLogger(server, 'spaces'));
+      const spacesAuditLogger = new SpacesAuditLogger(
+        new AuditLogger(server, 'spaces', config, xpackMainPlugin.info)
+      );
 
       server.expose('spacesClient', {
         getScopedClient: (request: Record<string, any>) => {
