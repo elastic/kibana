63150418a070fe4bba1c4de4f061384cc317544b
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-05-02 11:52:27 -0700

    adds index pattern back to Data->metrics

--- src/legacy/core_plugins/metrics/public/components/vis_editor.js
+++ src/legacy/core_plugins/metrics/public/components/vis_editor.js
@@ -205,7 +205,7 @@ class VisEditor extends Component {
 
   async componentDidMount() {
     this.props.renderComplete();
-    this.fetchIndexPatterns();
+    await this.fetchIndexPatterns();
   }
 
   componentDidUpdate(prevProps) {
