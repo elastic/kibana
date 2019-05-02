52ba601f9a3b1b8ce12dfba4961a3ee831fb3cf1
Alexey Antonov <alexwizp@gmail.com>
2019-05-02 13:41:09 +0300

    Have TSVB signal render-complete equals true on rendering (#35215)
    Fix: #16290

--- src/legacy/core_plugins/metrics/public/components/vis_editor_visualization.js
+++ src/legacy/core_plugins/metrics/public/components/vis_editor_visualization.js
@@ -253,10 +253,8 @@ class VisEditorVisualization extends Component {
           style={style}
           className="tvbEditorVisualization"
           data-shared-items-container
-          data-shared-item
           data-title={title}
           data-description={description}
-          data-render-complete="disabled"
           ref={this._visEl}
         />
         <div className="tvbEditor--hideForReporting">
