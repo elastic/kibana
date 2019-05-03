59908451a5d588ac76a2d259a1717e8cea540d48
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-05-03 08:24:58 -0700

    Adds QueryBar to Metric Vis Panel Options tab

--- src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/table/split_by_everything.js
+++ src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/table/split_by_everything.js
@@ -18,10 +18,14 @@
  */
 
 import _ from 'lodash';
+// import { buildEsQuery } from '@kbn/es-query';
 export default function splitByEverything(req, panel) {
   return next => doc => {
     panel.series.filter(c => !(c.aggregate_by && c.aggregate_function)).forEach(column => {
       if (column.filter) {
+        // TODO: replace these with the object created by buildEsQuery (see ../series/split_by_filters)?
+        // const builtEsQuery = buildEsQuery(indexPattern, [column.filter], [], esQueryConfig);
+        // _.set(doc, `aggs.pivot.aggs.${column.id}.filter`, builtEsQuery);
         _.set(doc, `aggs.pivot.aggs.${column.id}.filter.query_string.query`, column.filter);
         _.set(doc, `aggs.pivot.aggs.${column.id}.filter.query_string.analyze_wildcard`, true);
       } else {
