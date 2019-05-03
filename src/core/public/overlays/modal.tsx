00f3eb93c0e2b1ecd1e5098861e51b903d96d3e4
Stacey Gammon <gammon@elastic.co>
2019-05-03 16:39:54 -0400

    Add open modal core service

--- src/core/public/overlays/modal.tsx
+++ src/core/public/overlays/modal.tsx
@@ -19,7 +19,7 @@
 
 /* eslint-disable max-classes-per-file */
 
-import { EuiFlyout } from '@elastic/eui';
+import { EuiModal, EuiOverlayMask } from '@elastic/eui';
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { Subject } from 'rxjs';
@@ -27,23 +27,12 @@ import { I18nSetup } from '../i18n';
 import { OverlayRef } from './overlay_service';
 
 /**
- * A FlyoutRef is a reference to an opened flyout panel. It offers methods to
- * close the flyout panel again. If you open a flyout panel you should make
- * sure you call `close()` when it should be closed.
- * Since a flyout could also be closed by a user or from another flyout being
- * opened, you must bind to the `onClose` Promise on the FlyoutRef instance.
- * The Promise will resolve whenever the flyout was closed at which point you
- * should discard the FlyoutRef.
+ * A ModalRef is a reference to an opened modal. It offers methods to
+ * close the modal.
  *
  * @public
  */
-export class FlyoutRef implements OverlayRef {
-  /**
-   * An Promise that will resolve once this flyout is closed.
-   *
-   * Flyouts can close from user interaction, calling `close()` on the flyout
-   * reference or another call to `openFlyout()` replacing your flyout.
-   */
+export class ModalRef implements OverlayRef {
   public readonly onClose: Promise<void>;
 
   private closeSubject = new Subject<void>();
@@ -53,8 +42,8 @@ export class FlyoutRef implements OverlayRef {
   }
 
   /**
-   * Closes the referenced flyout if it's still open which in turn will
-   * resolve the `onClose` Promise. If the flyout had already been
+   * Closes the referenced modal if it's still open which in turn will
+   * resolve the `onClose` Promise. If the modal had already been
    * closed this method does nothing.
    */
   public close(): Promise<void> {
@@ -67,8 +56,8 @@ export class FlyoutRef implements OverlayRef {
 }
 
 /** @internal */
-export class FlyoutService {
-  private activeFlyout: FlyoutRef | null = null;
+export class ModalService {
+  private activeModal: ModalRef | null = null;
 
   constructor(private readonly targetDomElement: Element) {}
 
@@ -79,41 +68,43 @@ export class FlyoutService {
    * @param flyoutChildren - Mounts the children inside a flyout panel
    * @return {FlyoutRef} A reference to the opened flyout panel.
    */
-  public openFlyout = (
+  public openModal = (
     i18n: I18nSetup,
-    flyoutChildren: React.ReactNode,
-    flyoutProps: {
+    modalChildren: React.ReactNode,
+    modalProps: {
       closeButtonAriaLabel?: string;
       'data-test-subj'?: string;
     } = {}
-  ): FlyoutRef => {
+  ): ModalRef => {
     // If there is an active flyout session close it before opening a new one.
-    if (this.activeFlyout) {
-      this.activeFlyout.close();
+    if (this.activeModal) {
+      this.activeModal.close();
       this.cleanupDom();
     }
 
-    const flyout = new FlyoutRef();
+    const modal = new ModalRef();
 
-    // If a flyout gets closed through it's FlyoutRef, remove it from the dom
-    flyout.onClose.then(() => {
-      if (this.activeFlyout === flyout) {
+    // If a modal gets closed through it's ModalRef, remove it from the dom
+    modal.onClose.then(() => {
+      if (this.activeModal === modal) {
         this.cleanupDom();
       }
     });
 
-    this.activeFlyout = flyout;
+    this.activeModal = modal;
 
     render(
-      <i18n.Context>
-        <EuiFlyout {...flyoutProps} onClose={() => flyout.close()}>
-          {flyoutChildren}
-        </EuiFlyout>
-      </i18n.Context>,
+      <EuiOverlayMask>
+        <i18n.Context>
+          <EuiModal {...modalProps} onClose={() => modal.close()}>
+            {modalChildren}
+          </EuiModal>
+        </i18n.Context>
+      </EuiOverlayMask>,
       this.targetDomElement
     );
 
-    return flyout;
+    return modal;
   };
 
   /**
@@ -126,6 +117,6 @@ export class FlyoutService {
   private cleanupDom(): void {
     unmountComponentAtNode(this.targetDomElement);
     this.targetDomElement.innerHTML = '';
-    this.activeFlyout = null;
+    this.activeModal = null;
   }
 }
