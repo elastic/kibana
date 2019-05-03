b21fb79cba270a55121fd3fb94270f0b808d2aaa
James Gowdy <jgowdy@elastic.co>
2019-05-03 08:25:37 +0100

    [ML] Fields service for new job wizards - new

---
+++ x-pack/plugins/ml/server/client/elasticsearch_ml.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { CallClusterOptions } from 'src/legacy/core_plugins/elasticsearch';
+
+// TODO
+// this lib should similar to, or inherit from (if possible?) kibana/src/legacy/core_plugins/elasticsearch/index.d.ts
+// where all possible options are available
+// it should replace CallWithRequestType in kibana/x-pack/plugins/ml/server/models/annotation_service/annotation.ts
+/* eslint-disable */
+export interface CallWithRequestType {
+  (endpoint: string, params: any, options?: CallClusterOptions): Promise<any>;
+}
