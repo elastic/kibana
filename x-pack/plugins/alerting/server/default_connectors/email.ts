9ff7836dadcdd746fb158e6c680c9b3e158d098c
Mike Cote <mikecote@users.noreply.github.com>
2019-05-06 10:45:02 -0400

    Add params and connectorOptions for email

--- x-pack/plugins/alerting/server/default_connectors/email.ts
+++ x-pack/plugins/alerting/server/default_connectors/email.ts
@@ -4,13 +4,70 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import Joi from 'joi';
 import nodemailer from 'nodemailer';
+
+interface EmailConnectorOptions {
+  port?: number;
+  host: string;
+  auth: {
+    type?: string;
+    username: string;
+    password: string;
+  };
+}
+
+interface EmailParams {
+  from: string;
+  to: string[];
+  cc?: string[];
+  bcc?: string[];
+  subject: string;
+  text: string;
+  html?: string;
+}
 
 export const emailConnector = {
   id: 'email',
   name: 'E-mail',
-  async executor(connectorOptions: any, params: any) {
-    const transporter = nodemailer.createTransport(connectorOptions);
+  validate: {
+    params: Joi.object()
+      .keys({
+        from: Joi.string().required(),
+        to: Joi.array()
+          .items(Joi.string())
+          .min(1)
+          .required(),
+        cc: Joi.array()
+          .items(Joi.string())
+          .optional(),
+        bcc: Joi.array()
+          .items(Joi.string())
+          .optional(),
+        subject: Joi.string().required(),
+        text: Joi.string().required(),
+        html: Joi.strict().optional(),
+      })
+      .required(),
+    connectorOptions: Joi.object()
+      .keys({
+        port: Joi.number().optional(),
+        host: Joi.string().required(),
+        auth: Joi.object()
+          .keys({
+            type: Joi.string().optional(),
+            username: Joi.string().required(),
+            password: Joi.string().required(),
+          })
+          .required(),
+      })
+      .required(),
+  },
+  async executor(connectorOptions: EmailConnectorOptions, params: EmailParams) {
+    // @ts-ignore
+    const transporter = nodemailer.createTransport(connectorOptions, {
+      secure: true,
+    });
     await transporter.sendMail(params);
   },
 };
