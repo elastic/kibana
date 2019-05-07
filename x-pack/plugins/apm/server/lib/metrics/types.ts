4a07143b92e972f960a141d379f9b9872bad8cdf
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-05-07 10:07:53 +0200

    Simplify metric chart types

--- x-pack/plugins/apm/server/lib/metrics/types.ts
+++ x-pack/plugins/apm/server/lib/metrics/types.ts
@@ -5,61 +5,36 @@
  */
 
 import { ChartType, YUnit } from '../../../typings/timeseries';
-import { Coordinate } from '../../../typings/timeseries';
 
 export interface AggValue {
   value: number | null;
-}
-
-export interface TimeSeriesBucket {
-  key_as_string: string; // timestamp as string
-  key: number; // timestamp as epoch milliseconds
-  doc_count: number;
 }
 
 export interface MetricsKeys {
   [key: string]: AggValue;
 }
 
-interface SeriesDetails {
-  title: string;
-  color?: string;
-}
-
-type SeriesDetailsMap<T extends MetricsKeys> = {
-  [key in keyof T]: SeriesDetails
-};
-
 export interface ChartBase<T extends MetricsKeys> {
   title: string;
   key: string;
   type: ChartType;
   yUnit: YUnit;
-  transformValue?: (value: number) => number;
-  series: SeriesDetailsMap<T>;
-}
-
-export interface Chart<T extends MetricsKeys> {
-  title: ChartBase<T>['title'];
-  key: ChartBase<T>['key'];
-  type: ChartBase<T>['type'];
-  yUnit: ChartBase<T>['yUnit'];
-  totalHits: number;
-  series: Array<ChartSeries<T>>;
-}
-
-export type GenericMetricsChart = Chart<MetricsKeys>;
-
-export interface ChartSeries<T extends MetricsKeys> {
-  title: string;
-  key: keyof T;
-  type: ChartType;
-  overallValue: number | null;
-  data: Coordinate[];
+  series: {
+    [key in keyof T]: {
+      title: string;
+      color?: string;
+    }
+  };
 }
 
 export type MetricsAggs<T extends MetricsKeys> = {
   timeseriesData: {
-    buckets: Array<TimeSeriesBucket & T>;
+    buckets: Array<
+      {
+        key_as_string: string; // timestamp as string
+        key: number; // timestamp as epoch milliseconds
+        doc_count: number;
+      } & T
+    >;
   };
 } & T;
