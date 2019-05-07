ae2c038152752924f8b666817332f0b9cf735a47
Dario Gieselaar <dario.gieselaar@elastic.co>
2019-05-07 20:18:58 +0200

    [APM] Navigate to default transaction sample
    If the distribution data changes, and the currently selected transaction sample is no longer available, we navigate to the default sample of the latest distribution data.
    
    Previously, a displayed sample would "stick" if the new distribution data did not have a default sample.
    
    Readability improvements
    
    Use location primitives
    
    Simplifies hook logic in TransactionDetails/Distribution by only passing the necessary primitives (pathname, hash, search) to the history.replace() call rather than the full location object. Removes the need for memoization w/ useRef too.
    
    Fix lint error

--- x-pack/plugins/apm/public/components/app/TransactionDetails/index.tsx
+++ x-pack/plugins/apm/public/components/app/TransactionDetails/index.tsx
@@ -5,14 +5,12 @@
  */
 
 import { EuiPanel, EuiSpacer, EuiTitle, EuiHorizontalRule } from '@elastic/eui';
-import { i18n } from '@kbn/i18n';
 import _ from 'lodash';
 import React from 'react';
 import { useTransactionDetailsCharts } from '../../../hooks/useTransactionDetailsCharts';
 import { useTransactionDistribution } from '../../../hooks/useTransactionDistribution';
 import { useWaterfall } from '../../../hooks/useWaterfall';
 import { TransactionCharts } from '../../shared/charts/TransactionCharts';
-import { EmptyMessage } from '../../shared/EmptyMessage';
 import { ApmHeader } from '../../shared/ApmHeader';
 import { TransactionDistribution } from './Distribution';
 import { Transaction } from './Transaction';
@@ -58,23 +56,7 @@ export function TransactionDetails() {
 
       <EuiSpacer size="s" />
 
-      {!transaction ? (
-        <EmptyMessage
-          heading={i18n.translate(
-            'xpack.apm.transactionDetails.noTransactionTitle',
-            {
-              defaultMessage: 'No transaction sample available.'
-            }
-          )}
-          subheading={i18n.translate(
-            'xpack.apm.transactionDetails.noTransactionDescription',
-            {
-              defaultMessage:
-                'Try another time range, reset the search filter or select another bucket from the distribution histogram.'
-            }
-          )}
-        />
-      ) : (
+      {transaction && (
         <Transaction
           location={location}
           transaction={transaction}
