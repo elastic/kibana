60cd8911a98b7be1a7b47b7a454b0a9201747c0c
Joe Reuter <johannes.reuter@elastic.co>
2019-05-09 17:17:23 +0200

    add documentation

--- x-pack/plugins/lens/public/utils/native_renderer/native_renderer.tsx
+++ x-pack/plugins/lens/public/utils/native_renderer/native_renderer.tsx
@@ -31,6 +31,15 @@ function isShallowDifferent<T>(a: T, b: T): boolean {
   return false;
 }
 
+/**
+ * A component which takes care of providing a mountpoint for a generic render
+ * function which takes an html element and an optional props object.
+ * It also takes care of calling render again if the props object changes.
+ * By default the mountpoint element will be a div, this can be changed with the
+ * `tag` prop.
+ *
+ * @param props
+ */
 export function NativeRenderer<T>({ render, nativeProps, tag }: NativeRendererProps<T>) {
   const elementRef = useRef<Element | null>(null);
   const propsRef = useRef<T | null>(null);
