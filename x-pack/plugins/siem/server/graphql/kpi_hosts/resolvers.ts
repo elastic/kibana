e395b57a240ee0566cf5debc8a19908bebf02805
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2019-05-09 21:52:12 +0800

    Kpi host (#35065) (#36350)
    * init graphql
    
    * update KPIs
    
    * add charts
    
    * isolate charts
    
    * fix app crashes on screen resizing
    
    * gen types
    
    * rename variables
    
    * simplify flex groups, add icon var, rm loading
    
    * add icon names, change flex grows
    
    * rm overlay, enable axes, show first/last ticks
    
    * unabbreviate destination
    
    * update UI
    
    * update histogram data
    
    * customize chart axis
    
    * update x-axis for area chart
    
    * handle no data cases
    
    * update unit test
    
    * fix lint error
    
    fix type error
    
    fix unit itest
    
    * rename i18n var
    
    * fix response data type
    
    * add unit test for areachart
    
    * add unit test for barchart
    
    * fix UI
    
    * add more test case for barchart
    
    * fix for code review
    
    * gathering mock data in kpi_host integration test

---
+++ x-pack/plugins/siem/server/graphql/kpi_hosts/resolvers.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { SourceResolvers } from '../../graphql/types';
+import { AppResolverOf, ChildResolverOf } from '../../lib/framework';
+import { KpiHosts } from '../../lib/kpi_hosts';
+import { createOptions } from '../../utils/build_query/create_options';
+import { QuerySourceResolver } from '../sources/resolvers';
+
+export type QueryKpiHostsResolver = ChildResolverOf<
+  AppResolverOf<SourceResolvers.KpiHostsResolver>,
+  QuerySourceResolver
+>;
+
+export interface KpiHostsResolversDeps {
+  kpiHosts: KpiHosts;
+}
+
+export const createKpiHostsResolvers = (
+  libs: KpiHostsResolversDeps
+): {
+  Source: {
+    KpiHosts: QueryKpiHostsResolver;
+  };
+} => ({
+  Source: {
+    async KpiHosts(source, args, { req }, info) {
+      const options = { ...createOptions(source, args, info) };
+      return libs.kpiHosts.getKpiHosts(req, options);
+    },
+  },
+});
