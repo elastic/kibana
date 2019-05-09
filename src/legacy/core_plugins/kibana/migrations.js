c73ef194f9e6d7c1fdfe6292132804cb50e50ebd
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-05-09 14:12:26 -0700

    Migrates split filter items

--- src/legacy/core_plugins/kibana/migrations.js
+++ src/legacy/core_plugins/kibana/migrations.js
@@ -189,6 +189,7 @@ function transformFilterStringToQueryObject(doc) {
       });
       // migrate the series filters
       const series = get(visState, 'params.series') || [];
+      // TODO: the forEach body can be extracted into another function
       series.forEach((item) => {
         if (!item.filter) {
           // we don't need to transform anything if there isn't a filter at all
