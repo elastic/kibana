d2a0d569e9b7682ee81e2984a83e0b6a9265a4bb
Walter Rafelsberger <walter@elastic.co>
2019-05-09 10:00:05 +0200

    [ML] Data Frame: Optional index pattern creation. (#36020)
    Adds a checkbox to the data frame pivot wizard to optionally create an index pattern when creating the transform job. By default it's not enabled.

--- x-pack/plugins/ml/common/types/angular.ts
+++ x-pack/plugins/ml/common/types/angular.ts
@@ -4,6 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export type JobId = string;
-export type EsIndexName = string;
-export type IndexPatternTitle = string;
+export interface InjectorService {
+  get<T>(name: string, caller?: string): T;
+}
