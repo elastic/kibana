0c865a1d812a633d72b50d238b77c842e88f4a48
kobelb <brandon.kobel@elastic.co>
2019-05-09 17:41:02 -0700

    Using a regex to determine if the SSN is in the results
    Discover was showing the columns in different orders, which this test
    isn't concerned with, so we're using a regex now.

--- x-pack/test/functional/apps/security/field_level_security.js
+++ x-pack/test/functional/apps/security/field_level_security.js
@@ -97,8 +97,7 @@ export default function ({ getService, getPageObjects }) {
         expect(hitCount).to.be('2');
       });
       const rowData = await PageObjects.discover.getDocTableIndex(1);
-      expect(rowData).to
-        .be('customer_ssn:444.555.6666 customer_name:ABC Company customer_region:WEST _id:2 _type:_doc _index:flstest _score:0');
+      expect(rowData).to.match(/customer_ssn:444\.555\.6666/);
     });
 
     it('user customer2 should not see ssn', async function () {
@@ -110,7 +109,7 @@ export default function ({ getService, getPageObjects }) {
         expect(hitCount).to.be('2');
       });
       const rowData = await PageObjects.discover.getDocTableIndex(1);
-      expect(rowData).to.be('customer_name:ABC Company customer_region:WEST _id:2 _type:_doc _index:flstest _score:0');
+      expect(rowData).not.to.match(/customer_ssn:444\.555\.6666/);
     });
 
     after(async function () {
