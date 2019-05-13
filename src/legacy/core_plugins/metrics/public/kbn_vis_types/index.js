53116989a5433d728580cf5ff5c74db0a0c82d63
Alexey Antonov <alexwizp@gmail.com>
2019-05-13 13:51:42 +0300

    TSVB wrong default date field on default index-pattern
    Fix: #36264

--- src/legacy/core_plugins/metrics/public/kbn_vis_types/index.js
+++ src/legacy/core_plugins/metrics/public/kbn_vis_types/index.js
@@ -63,7 +63,7 @@ export default function MetricsVisProvider(Private, i18n) {
             fill: 0.5,
             stacked: 'none'
           }],
-        time_field: '@timestamp',
+        time_field: '',
         index_pattern: '',
         interval: 'auto',
         axis_position: 'left',
