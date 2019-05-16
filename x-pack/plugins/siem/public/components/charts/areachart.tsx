a9c2bd58ff4b2eeb5be32bb015b948c4f1fbb170
Angela Chuang <yi-chun.chuang@elastic.co>
2019-05-16 04:26:17 -0400

    replace areachart

--- x-pack/plugins/siem/public/components/charts/areachart.tsx
+++ x-pack/plugins/siem/public/components/charts/areachart.tsx
@@ -6,46 +6,84 @@
 
 import React from 'react';
 import { pure } from 'recompose';
-import styled from 'styled-components';
-import { EuiSeriesChart, EuiAreaSeries, EuiXAxis, EuiYAxis } from '@elastic/eui/lib/experimental';
+import {
+  Chart,
+  Axis,
+  AreaSeries,
+  ScaleType,
+  Position,
+  getAxisId,
+  getSpecId,
+} from '@elastic/charts';
+import '@elastic/charts/dist/style.css';
 import { AreaChartData, WrappedByAutoSizer, ChartHolder } from './common';
 import { AutoSizer } from '../auto_sizer';
+
+const seriesAreaStyle = (color: string) => ({
+  area: {
+    fill: color,
+    visible: true,
+    opacity: 0.4,
+  },
+  line: {
+    stroke: color,
+    strokeWidth: 1,
+    visible: true,
+  },
+  border: {
+    visible: false,
+    strokeWidth: 1,
+    stroke: color,
+  },
+  point: {
+    visible: false,
+    radius: 0.2,
+    stroke: color,
+    strokeWidth: 1,
+    opacity: 1,
+  },
+});
+
+const dateFormatter = (d: string) => {
+  return d.toLocaleString().split('T')[0];
+};
 
 export const AreaChartBaseComponent = pure<{
   data: AreaChartData[];
   width: number | null | undefined;
   height: number | null | undefined;
-}>(({ data, ...chartConfigs }) =>
-  chartConfigs.width && chartConfigs.height ? (
-    <SeriesChart
-      {...chartConfigs}
-      showDefaultAxis={false}
-      xType="ordinal"
-      data-test-subj="stat-area-chart"
-    >
-      {data.map(series => (
-        /**
-         * Placing ts-ignore here for fillOpacity
-         * */
-        // @ts-ignore
-        <EuiAreaSeries
-          key={`stat-items-areachart-${series.key}`}
-          name={series.key.replace('Histogram', '')}
-          // @ts-ignore
-          data={series.value}
-          fillOpacity={0.04}
-          color={series.color}
+}>(({ data, ...chartConfigs }) => {
+  return chartConfigs.width && chartConfigs.height ? (
+    <div style={{ height: chartConfigs.height, width: chartConfigs.width, position: 'relative' }}>
+      <Chart>
+        {data.map((series, idx) =>
+          series.value != null ? (
+            <AreaSeries
+              id={getSpecId(`area-${series.key}-${idx}`)}
+              key={`stat-items-areachart-${series.key}`}
+              name={series.key.replace('Histogram', '')}
+              data={series.value}
+              xScaleType={ScaleType.Ordinal}
+              yScaleType={ScaleType.Linear}
+              xAccessor="x"
+              yAccessors={['y']}
+              areaSeriesStyle={series.color ? seriesAreaStyle(series.color) : undefined}
+            />
+          ) : null
+        )}
+
+        <Axis
+          id={getAxisId(`group-${data[0].key}-x`)}
+          position={Position.Bottom}
+          showOverlappingTicks={false}
+          tickFormat={dateFormatter}
+          tickSize={0}
         />
-      ))}
-      {/*
-// @ts-ignore */}
-      <EuiXAxis tickFormat={timestamp => timestamp.split('T')[0]} />
-      {/*
-// @ts-ignore */}
-      <EuiYAxis />
-    </SeriesChart>
-  ) : null
-);
+        <Axis id={getAxisId(`group-${data[0].key}-y`)} position={Position.Left} tickSize={0} />
+      </Chart>
+    </div>
+  ) : null;
+});
 
 export const AreaChartWithCustomPrompt = pure<{
   data: AreaChartData[] | null | undefined;
@@ -77,9 +115,3 @@ export const AreaChart = pure<{ areaChart: AreaChartData[] | null | undefined }>
     </AutoSizer>
   )
 );
-
-const SeriesChart = styled(EuiSeriesChart)`
-  svg .rv-xy-plot__axis__ticks .rv-xy-plot__axis__tick:not(:first-child):not(:last-child) {
-    display: none;
-  }
-`;
