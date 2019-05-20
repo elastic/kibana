1e4573999ae05c353860078b7e3361205467b771
Joe Reuter <johannes.reuter@elastic.co>
2019-05-20 06:21:33 -0400

    switch to strictly typed kibana legacy object

--- x-pack/plugins/lens/index.ts
+++ x-pack/plugins/lens/index.ts
@@ -7,13 +7,13 @@
 import * as Joi from 'joi';
 import { Server } from 'hapi';
 import { resolve } from 'path';
+import { LegacyPluginInitializer } from 'src/legacy/types';
 
 import { PLUGIN_ID } from './common';
 
 const NOT_INTERNATIONALIZED_PRODUCT_NAME = 'Lens Visualizations';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const lens = (kibana: any) => {
+export const lens: LegacyPluginInitializer = kibana => {
   return new kibana.Plugin({
     id: PLUGIN_ID,
     configPrefix: `xpack.${PLUGIN_ID}`,
