e242a88c45f9e23e89f1a1d1b2824b385c86f07f
James Gowdy <jgowdy@elastic.co>
2019-05-21 22:04:36 +0100

    removing unused file

--- x-pack/plugins/ml/public/jobs/new_job_new/common/index.ts
+++ x-pack/plugins/ml/public/jobs/new_job_new/common/index.ts
@@ -4,6 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from './index_pattern_context';
 export * from './job_creator';
 export * from './job_runner';
