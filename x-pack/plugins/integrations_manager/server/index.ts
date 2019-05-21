73ca7e4c7447ade8f5169d5df2ade0049526e818
John Schulz <john.schulz@elastic.co>
2019-05-21 17:35:52 -0400

    WIP. Manger can make requests to Registry.
    Tough day today. Went sideways with https://elastic.slack.com/archives/CCJQY1NS0/p1558460667396200
    
    Not sure how server will make external requests or much about it's API. Put a quick skeleton up to show we can fetch data from registry service (optionally act on it) and respond.

--- x-pack/plugins/integrations_manager/server/index.ts
+++ x-pack/plugins/integrations_manager/server/index.ts
@@ -7,22 +7,92 @@
 import { PluginInitializerContext } from 'src/core/server';
 import { CoreSetup } from 'src/core/server';
 
-const IM_ROOT = '/api/integrations_manager';
+import http from 'http'
+import https from 'https'
+
+const ID = 'integrations_manager'
+const REGISTRY = process.env.REGISTRY || 'http://localhost:8080'
+const API_ROOT = `/api/${ID}`;
+
+// Manager public API paths (currently essentially a proxy to registry service)
+const routes = { 
+  reg_info: {
+    method: 'GET',
+    path: `${API_ROOT}/`,
+    options: {
+      tags: [`access:${ID}`],
+    },
+    handler: async req => {
+      const data = await requestJson(REGISTRY)
+      return data
+    }
+  },
+  reg_list: {
+    method: 'GET',
+    path: `${API_ROOT}/list`,
+    options: {
+      tags: [`access:${ID}`],
+    },
+    handler: async req => {
+      const data = await requestJson(`${REGISTRY}/list`)
+      return data
+    }
+  },
+  reg_package: {
+    method: 'GET',
+    path: `${API_ROOT}/package/{pkgkey}`,
+    options: {
+      tags: [`access:${ID}`],
+    },
+    handler: async req => {
+      const data = await requestJson(`${REGISTRY}/package/${req.params.pkgkey}`)
+      return data
+    }
+  },
+  reg_package_get: {
+    method: 'GET',
+    path: `${API_ROOT}/package/{pkgkey}/get`,
+    options: {
+      tags: [`access:${ID}`],
+    },
+    handler: async req => {
+      const url = `${REGISTRY}/package/${req.params.pkgkey}/get`
+      const data = await request(url)
+      return { meta: { WIP: true, url, length: data.length } }
+    }
+  }
+}
 
 class Plugin {
   public setup(core: CoreSetup) {
     const { server } = core.http;
-    server.route({
-      method: 'GET',
-      path: IM_ROOT,
-      options: {
-        tags: ['access:integrations_manager'],
-      },
-      handler: async req => {
-        return { message: 'INTEGRATIONS MANAGER WORKS' };
-      },
-    });
+    Object.values(routes).forEach((route) => server.route(route))
   }
+}
+
+function request(url) {
+  return new Promise((resolve, reject) => {
+    const lib = url.startsWith('https') ? https : http;
+
+    const request = lib.get(url, (response) => {
+      const body = [];
+      response.on('data', (chunk) => body.push(chunk));
+      response.on('end', () => resolve(body.join('')));
+    });
+
+    request.on('error', reject)
+  })
+}
+
+async function requestJson(url) {
+  try {
+    const json = await request(url)
+    const data = JSON.parse(json)
+    return data
+  } catch (e) {
+    throw e
+  }
+
 }
 
 export function plugin(initializerContext: PluginInitializerContext) {
