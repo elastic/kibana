0fb2e251a067d5a3ddae2a9a06c2db3079e66c49
Eli Perelman <eli@eliperelman.com>
2019-05-22 16:35:04 -0500

    Remove browser basePath service, move functionality into browser http service (#36611)
    * Remove browser basePath service, move functionality into browser http service
    
    * Update generated documentation for removal of browser basePath
    
    * Fix type interface for removal of basePath
    
    * Split IHttpService into separate setup and start interfaces
    
    * Rename appendToBasePath to prependBasePath, rename removeFromBasePath to removeBasePath

--- src/core/public/application/capabilities/capabilities_service.tsx
+++ src/core/public/application/capabilities/capabilities_service.tsx
@@ -20,11 +20,9 @@
 import { deepFreeze, RecursiveReadonly } from '../../utils/deep_freeze';
 import { MixedApp } from '../application_service';
 import { InjectedMetadataStart } from '../../injected_metadata';
-import { BasePathStart } from '../../base_path';
 
 interface StartDeps {
   apps: ReadonlyArray<MixedApp>;
-  basePath: BasePathStart;
   injectedMetadata: InjectedMetadataStart;
 }
 
@@ -74,7 +72,7 @@ export interface CapabilitiesStart {
  * Service that is responsible for UI Capabilities.
  */
 export class CapabilitiesService {
-  public async start({ apps, basePath, injectedMetadata }: StartDeps): Promise<CapabilitiesStart> {
+  public async start({ apps, injectedMetadata }: StartDeps): Promise<CapabilitiesStart> {
     const capabilities = deepFreeze(injectedMetadata.getCapabilities());
     const availableApps = apps.filter(app => capabilities.navLinks[app.id]);
 
