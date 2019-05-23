c31fad838ea7248fbe4c119b38f142517dcd3c13
Alexey Antonov <alexwizp@gmail.com>
2019-05-23 10:24:37 +0300

    [TSVB] Gauge/Top N/Metric panels show wrong data (#36502)
    Fix: #36501

--- src/legacy/core_plugins/metrics/common/get_last_value.test.js
+++ src/legacy/core_plugins/metrics/common/get_last_value.test.js
@@ -17,33 +17,27 @@
  * under the License.
  */
 
-import { expect } from 'chai';
-import getLastValue from '../get_last_value';
+import getLastValue from './get_last_value';
 
 describe('getLastValue(data)', () => {
-
-  it('returns data if data is not array', () => {
-    expect(getLastValue('foo')).to.equal('foo');
+  test('should returns data if data is not array', () => {
+    expect(getLastValue('foo')).toBe('foo');
   });
 
-  it('returns the last value', () => {
-    const data = [[1, 1]];
-    expect(getLastValue(data)).to.equal(1);
+  test('should returns the last value', () => {
+    expect(getLastValue([[1, 2]])).toBe(2);
   });
 
-  it('returns the second to last value if the last value is null (default)', () => {
-    const data = [[1, 4], [2, null]];
-    expect(getLastValue(data)).to.equal(4);
+  test('should returns the default value ', () => {
+    expect(getLastValue()).toBe(0);
   });
 
-  it('returns 0 if second to last is not defined (default)', () => {
-    const data = [[1, null], [2, null]];
-    expect(getLastValue(data)).to.equal(0);
+  test('should returns 0 if second to last is not defined (default)', () => {
+    expect(getLastValue([[1, null], [2, null]])).toBe(0);
   });
 
-  it('returns the N to last value if the last N-1 values are null (default)', () => {
-    const data = [[1, 4], [2, null], [3, null]];
-    expect(getLastValue(data, 3)).to.equal(4);
+  test('should allows to override the default value', () => {
+    expect(getLastValue(null, '-')).toBe('-');
   });
 });
 
