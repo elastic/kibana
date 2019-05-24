82fa749d208822eb03f00f7db815282eec402275
Artyom Gospodarsky <art.gospodarsky@gmail.com>
2019-05-24 16:16:54 +0300

    [TSVB] Get rid of default export on TSVB (#36872)
    The `import/no-default-export` has been turned on for TSVB.
    
    * Get rid of default export on TSVB
    * Change eslint rules lines

--- src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/series/__tests__/split_by_terms.js
+++ src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/series/__tests__/split_by_terms.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import splitByTerms from '../split_by_terms';
+import { splitByTerms } from '../split_by_terms';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
