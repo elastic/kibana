c0ca3b4fc04971d6da6accd4c5c5cf3d5d6fbb84
Alexey Antonov <alexwizp@gmail.com>
2019-05-27 17:45:14 +0300

    TSVB:  selecting the colour picker crashes Kibana (#37176) (#37182)
    Fix: #37115

--- src/legacy/core_plugins/metrics/public/components/custom_color_picker.js
+++ src/legacy/core_plugins/metrics/public/components/custom_color_picker.js
@@ -28,7 +28,7 @@ import CompactColor from 'react-color/lib/components/compact/CompactColor';
 import color from 'react-color/lib/helpers/color';
 import shallowCompare from 'react-addons-shallow-compare';
 
-export class CustomColorPickerUI extends Component {
+class CustomColorPickerUI extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
