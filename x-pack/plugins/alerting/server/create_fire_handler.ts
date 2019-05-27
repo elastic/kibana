72b461b5d3fa238530f441580eedbabb1ee633ae
Mike Cote <mikecote@users.noreply.github.com>
2019-05-27 13:53:43 -0400

    Use handlebars for templating strings at fire time

--- x-pack/plugins/alerting/server/create_fire_handler.ts
+++ x-pack/plugins/alerting/server/create_fire_handler.ts
@@ -4,8 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import Handlebars from 'handlebars';
 import { SavedObject } from 'src/legacy/server/saved_objects';
-import { State, Context } from './types';
+import { AlertAction, State, Context } from './types';
 import { ActionsPlugin } from '../../actions';
 
 interface CreateFireHandlerOptions {
@@ -15,14 +16,22 @@ interface CreateFireHandlerOptions {
 
 export function createFireHandler({ fireAction, alertSavedObject }: CreateFireHandlerOptions) {
   return async (actionGroup: string, context: Context, state: State) => {
-    const actions = alertSavedObject.attributes.actions.filter(
-      ({ group }: { group: string }) => group === actionGroup
-    );
+    const alertActions: AlertAction[] = alertSavedObject.attributes.actions;
+    const actions = alertActions.filter(({ group }: { group: string }) => group === actionGroup);
     for (const action of actions) {
+      const params: Record<string, any> = {};
+      for (const key of Object.keys(action.params)) {
+        const value = action.params[key];
+        if (typeof value !== 'string') {
+          params[key] = value;
+          continue;
+        }
+        const template = Handlebars.compile(value);
+        params[key] = template({ context, state });
+      }
       fireAction({
         id: action.id,
-        // TODO: Maybe not merge context & state here, but somewhere before
-        params: { ...context, ...state },
+        params,
       });
     }
   };
