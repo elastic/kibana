4a6cd797ebe5d43bd0a283a0dc400aaf498d93d4
Rashid Khan <github.finland@cspo.top>
2019-05-27 19:25:43 -0700

    Fix Canvas fullscreen
    Closes #34080
    
    For whatever reason, `Style.it` is unwrapping and dropping the root node of the component it wraps. No idea why. This simply wraps the tree up in a sacrificial `<div/>` for `Style.it` to eat.

--- x-pack/plugins/canvas/public/components/workpad/workpad.js
+++ x-pack/plugins/canvas/public/components/workpad/workpad.js
@@ -119,36 +119,38 @@ export class Workpad extends React.PureComponent {
               // NOTE: the data-shared-* attributes here are used for reporting
               return Style.it(
                 workpadCss,
-                <div
-                  className={`canvasWorkpad ${isFullscreen ? 'fullscreen' : ''}`}
-                  style={fsStyle}
-                  data-shared-items-count={totalElementCount}
-                >
-                  {isFullscreen && (
-                    <Shortcuts
-                      name="PRESENTATION"
-                      handler={this.keyHandler}
-                      targetNodeSelector="body"
-                      global
+                  <div>
+                    <div
+                    className={`canvasWorkpad ${isFullscreen ? 'fullscreen' : ''}`}
+                    style={fsStyle}
+                    data-shared-items-count={totalElementCount}
+                  >
+                    {isFullscreen && (
+                      <Shortcuts
+                        name="PRESENTATION"
+                        handler={this.keyHandler}
+                        targetNodeSelector="body"
+                        global
+                      />
+                    )}
+                    {pages.map((page, i) => (
+                      <WorkpadPage
+                        key={page.id}
+                        pageId={page.id}
+                        height={height}
+                        width={width}
+                        isSelected={i + 1 === selectedPageNumber}
+                        animation={getAnimation(i + 1)}
+                        onAnimationEnd={onTransitionEnd}
+                        registerLayout={registerLayout}
+                        unregisterLayout={unregisterLayout}
+                      />
+                    ))}
+                    <div
+                      className="canvasGrid"
+                      style={{ height, width, display: grid ? 'block' : 'none' }}
                     />
-                  )}
-                  {pages.map((page, i) => (
-                    <WorkpadPage
-                      key={page.id}
-                      pageId={page.id}
-                      height={height}
-                      width={width}
-                      isSelected={i + 1 === selectedPageNumber}
-                      animation={getAnimation(i + 1)}
-                      onAnimationEnd={onTransitionEnd}
-                      registerLayout={registerLayout}
-                      unregisterLayout={unregisterLayout}
-                    />
-                  ))}
-                  <div
-                    className="canvasGrid"
-                    style={{ height, width, display: grid ? 'block' : 'none' }}
-                  />
+                  </div>
                 </div>
               );
             }}
