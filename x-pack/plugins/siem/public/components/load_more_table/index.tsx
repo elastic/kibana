75ef3ddc376f0889c8d37eeea6231d475a5b4656
Frank Hassanabad <frank.hassanabad@elastic.co>
2019-05-28 13:10:53 -0600

    [SIEM] Fix state issues to avoid potential React update issues (#37261)
    Fixed prevState issues with React setState
      * This will avoid subtle bugs with the react update renderer
      * Enabled one lint rule from warn to error
      * Turned off the other linter issue as it has an open ticket against it.
      * https://github.com/elastic/ingest-dev/issues/468
    
    Summarize your PR. If it involves visual changes include a screenshot or gif.
    
    Use ~~strikethroughs~~ to remove checklist items you don't feel are applicable to this PR.
    
    ~~- [ ] This was checked for cross-browser compatibility, [including a check against IE11](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility)~~
    ~~- [ ] Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/master/packages/kbn-i18n/README.md)~~
    ~~- [ ] [Documentation](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#writing-documentation) was added for features that require explanation or tutorials~~
    ~~- [ ] [Unit or functional tests](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility) were updated or added to match the most common scenarios~~
    ~~- [ ] This was checked for [keyboard-only and screenreader accessibility](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Cross_browser_testing/Accessibility#Accessibility_testing_checklist)~~
    
    ~~- [ ] This was checked for breaking API changes and was [labeled appropriately](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#release-notes-process)~~
    ~~- [ ] This includes a feature addition or change that requires a release note and was [labeled appropriately](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#release-notes-process)~~

--- x-pack/plugins/siem/public/components/load_more_table/index.tsx
+++ x-pack/plugins/siem/public/components/load_more_table/index.tsx
@@ -282,17 +282,17 @@ export class LoadMoreTable<T, U, V, W, X, Y, Z, AA, AB> extends React.PureCompon
   }
 
   private onButtonClick = () => {
-    this.setState({
-      ...this.state,
-      isPopoverOpen: !this.state.isPopoverOpen,
-    });
+    this.setState(prevState => ({
+      ...prevState,
+      isPopoverOpen: !prevState.isPopoverOpen,
+    }));
   };
 
   private closePopover = () => {
-    this.setState({
-      ...this.state,
+    this.setState(prevState => ({
+      ...prevState,
       isPopoverOpen: false,
-    });
+    }));
   };
 }
 
