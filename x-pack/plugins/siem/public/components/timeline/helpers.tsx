c4be4d6b762340622718b4dba1f9061b97e2f88b
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2019-05-28 17:06:01 -0400

    fix dataprovider query when the filed is a timestamp

--- x-pack/plugins/siem/public/components/timeline/helpers.tsx
+++ x-pack/plugins/siem/public/components/timeline/helpers.tsx
@@ -4,43 +4,74 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { isEmpty, isNumber } from 'lodash/fp';
+import { isEmpty, isNumber, get } from 'lodash/fp';
 import { StaticIndexPattern } from 'ui/index_patterns';
 
 import { convertKueryToElasticSearchQuery, escapeQueryValue } from '../../lib/keury';
 
 import { DataProvider, DataProvidersAnd, EXISTS_OPERATOR } from './data_providers/data_provider';
+import { BrowserFields } from '../../containers/source';
 
-const buildQueryMatch = (dataProvider: DataProvider | DataProvidersAnd) =>
+const converTimestampFieldToQuery = (field: string, value: string | number) =>
+  `${field}: ${isNumber(value) ? value : new Date(value).valueOf()}`;
+
+const getBrowserFieldPath = (field: string) => {
+  const splitFields = field.split('.');
+  if (splitFields.length === 1) {
+    return `base.fields.${field}`;
+  }
+  return `${splitFields[0]}.fields.${field}`;
+};
+
+const checkIfFieldTypeIsDate = (field: string, browserFields: BrowserFields) => {
+  const pathBrowserField = getBrowserFieldPath(field);
+  const browserField = get(pathBrowserField, browserFields);
+  if (browserField != null && browserField.type === 'date') {
+    return true;
+  }
+  return false;
+};
+
+const buildQueryMatch = (
+  dataProvider: DataProvider | DataProvidersAnd,
+  browserFields: BrowserFields
+) =>
   `${dataProvider.excluded ? 'NOT ' : ''}${
     dataProvider.queryMatch.operator !== EXISTS_OPERATOR
-      ? `${dataProvider.queryMatch.field} : ${
-          isNumber(dataProvider.queryMatch.value)
-            ? dataProvider.queryMatch.value
-            : escapeQueryValue(dataProvider.queryMatch.value)
-        }`
+      ? checkIfFieldTypeIsDate(dataProvider.queryMatch.field, browserFields)
+        ? converTimestampFieldToQuery(dataProvider.queryMatch.field, dataProvider.queryMatch.value)
+        : `${dataProvider.queryMatch.field} : ${
+            isNumber(dataProvider.queryMatch.value)
+              ? dataProvider.queryMatch.value
+              : escapeQueryValue(dataProvider.queryMatch.value)
+          }`
       : `${dataProvider.queryMatch.field} ${EXISTS_OPERATOR}`
   }`.trim();
 
-const buildQueryForAndProvider = (dataAndProviders: DataProvidersAnd[]) =>
+const buildQueryForAndProvider = (
+  dataAndProviders: DataProvidersAnd[],
+  browserFields: BrowserFields
+) =>
   dataAndProviders
     .reduce((andQuery, andDataProvider) => {
       const prepend = (q: string) => `${q !== '' ? `${q} and ` : ''}`;
       return andDataProvider.enabled
-        ? `${prepend(andQuery)} ${buildQueryMatch(andDataProvider)}`
+        ? `${prepend(andQuery)} ${buildQueryMatch(andDataProvider, browserFields)}`
         : andQuery;
     }, '')
     .trim();
 
-export const buildGlobalQuery = (dataProviders: DataProvider[]) =>
+export const buildGlobalQuery = (dataProviders: DataProvider[], browserFields: BrowserFields) =>
   dataProviders
     .reduce((query, dataProvider: DataProvider) => {
       const prepend = (q: string) => `${q !== '' ? `${q} or ` : ''}`;
       return dataProvider.enabled
         ? `${prepend(query)}(
-        ${buildQueryMatch(dataProvider)}
+        ${buildQueryMatch(dataProvider, browserFields)}
         ${
-          dataProvider.and.length > 0 ? ` and ${buildQueryForAndProvider(dataProvider.and)}` : ''
+          dataProvider.and.length > 0
+            ? ` and ${buildQueryForAndProvider(dataProvider.and, browserFields)}`
+            : ''
         })`.trim()
         : query;
     }, '')
@@ -49,6 +80,7 @@ export const buildGlobalQuery = (dataProviders: DataProvider[]) =>
 export const combineQueries = (
   dataProviders: DataProvider[],
   indexPattern: StaticIndexPattern,
+  browserFields: BrowserFields,
   kqlQuery: string,
   kqlMode: string,
   start: number,
@@ -67,7 +99,8 @@ export const combineQueries = (
     return {
       filterQuery: convertKueryToElasticSearchQuery(
         `((${buildGlobalQuery(
-          dataProviders
+          dataProviders,
+          browserFields
         )}) and @timestamp >= ${start} and @timestamp <= ${end})`,
         indexPattern
       ),
@@ -75,10 +108,9 @@ export const combineQueries = (
   }
   const operatorKqlQuery = kqlMode === 'filter' ? 'and' : 'or';
   const postpend = (q: string) => `${!isEmpty(q) ? ` ${operatorKqlQuery} (${q})` : ''}`;
-  const globalQuery = `((${buildGlobalQuery(dataProviders)}${postpend(
+  const globalQuery = `((${buildGlobalQuery(dataProviders, browserFields)}${postpend(
     kqlQuery
   )}) and @timestamp >= ${start} and @timestamp <= ${end})`;
-
   return {
     filterQuery: convertKueryToElasticSearchQuery(globalQuery, indexPattern),
   };
