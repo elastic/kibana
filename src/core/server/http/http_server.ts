b8d604148bb0af7a2c8120d4b0f13f258ffa4217
restrry <restrry@gmail.com>
2019-05-29 11:22:12 +0200

    re-wording

--- src/core/server/http/http_server.ts
+++ src/core/server/http/http_server.ts
@@ -38,7 +38,7 @@ export interface HttpServerSetup {
   options: ServerOptions;
   registerRouter: (router: Router) => void;
   /**
-   * Define custom authentication and/or authorization mechanism for incoming requests.
+   * To define custom authentication and/or authorization mechanism for incoming requests.
    * A handler should return a state to associate with the incoming request.
    * The state can be retrieved later via http.auth.get(..)
    * Only one AuthenticationHandler can be registered.
@@ -48,7 +48,7 @@ export interface HttpServerSetup {
     cookieOptions: SessionStorageCookieOptions<T>
   ) => Promise<void>;
   /**
-   * Define custom logic to perform for incoming requests. Runs the handler before Auth
+   * To define custom logic to perform for incoming requests. Runs the handler before Auth
    * hook performs a check that user has access to requested resources, so it's the only
    * place when you can forward a request to another URL right on the server.
    * Can register any number of registerOnPostAuth, which are called in sequence
@@ -56,7 +56,7 @@ export interface HttpServerSetup {
    */
   registerOnPreAuth: (handler: OnPreAuthHandler) => void;
   /**
-   * Define custom logic to perform for incoming requests. Runs the handler after Auth hook
+   * To define custom logic to perform for incoming requests. Runs the handler after Auth hook
    * did make sure a user has access to the requested resource.
    * The auth state is available at stage via http.auth.get(..)
    * Can register any number of registerOnPreAuth, which are called in sequence
