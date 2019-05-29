8a43521b669f08b5f7451753147c4d02830ab2e0
Michael Marcialis <michael@marcial.is>
2019-05-29 11:07:40 -0400

    [SIEM] Update Empty Page Messages (#37251)
    * update EmptyPage comp and text on all pages
    
    * change proptype from `string` to `IconType`
    
    * update tests and snapshots
    
    * remove translation imports
    
    * restore `align-self` styles for IE11 centering
    
    * proper apostrophe and string in single quotes, per frank
    
    * update host and ip details
    
    * DRYing up host and network empty page message
    
    * fix localization
    
    * fixing localization again

--- x-pack/plugins/siem/public/pages/overview/overview.test.tsx
+++ x-pack/plugins/siem/public/pages/overview/overview.test.tsx
@@ -14,8 +14,6 @@ import { TestProviders } from '../../mock';
 import { MockedProvider } from 'react-apollo/test-utils';
 import { cloneDeep } from 'lodash/fp';
 
-import * as i18n from './translations';
-
 jest.mock('ui/documentation_links', () => ({
   documentationLinks: {
     kibana: 'http://www.example.com',
@@ -53,7 +51,7 @@ describe('Overview', () => {
       // Why => https://github.com/apollographql/react-apollo/issues/1711
       await new Promise(resolve => setTimeout(resolve));
       wrapper.update();
-      expect(wrapper.text()).toContain(i18n.SETUP_INSTRUCTIONS);
+      expect(wrapper.find('[data-test-subj="empty-page"]').exists()).toBe(true);
     });
 
     test('it DOES NOT render the Getting started text when an index is available', async () => {
@@ -68,7 +66,7 @@ describe('Overview', () => {
       // Why => https://github.com/apollographql/react-apollo/issues/1711
       await new Promise(resolve => setTimeout(resolve));
       wrapper.update();
-      expect(wrapper.text()).not.toContain(i18n.SETUP_INSTRUCTIONS);
+      expect(wrapper.find('[data-test-subj="empty-page"]').exists()).toBe(false);
     });
   });
 });
