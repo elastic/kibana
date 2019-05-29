c34baee4ae64f7e5d245e5aec25c5d0748d4eb25
Corey Robertson <corey.robertson@elastic.co>
2019-05-29 16:36:56 -0400

    Convert httpurl to TS and Jest

--- x-pack/plugins/canvas/common/lib/httpurl.ts
+++ x-pack/plugins/canvas/common/lib/httpurl.ts
@@ -7,6 +7,6 @@
 // A cheap regex to distinguish an HTTP URL string from a data URL string
 const httpurlRegex = /^https?:\/\/\S+(?:[0-9]+)?\/\S{1,}/;
 
-export function isValidHttpUrl(str) {
+export function isValidHttpUrl(str: string): boolean {
   return httpurlRegex.test(str);
 }
