elastic/kibana

commit 9907f48e3be71b69fb3d25b14bf545730ab36266

Rollup support for TSVB (#28762)
* Added a feature of rollup search on the UI side

Signed-off-by: Alexey Antonov <alexwizp@gmail.com>

* Rollup Feature - initial commit

* Revert "Added a feature of rollup search on the UI side"

This reverts commit 9568b0970b16f5102f50b748bb4d691a8612c2c2.

# Conflicts:
#	src/legacy/core_plugins/metrics/public/components/index_pattern.js

* Remove the 'label' property from the search strategies

* Changed search by strategy from the last

* add single search request

* rollup_search_strategy add base implementation of isViable method

* rollup_search_strategy add base implementation of isViable method -fix

* Changed requests due to search request type

* refactoring of import Base classes / remove '../../../../../../

* remove extra await

* rollup_search_strategy. Refactoring of isRollupJobExists method

* remove question

* Add support of annotations and table data

* skeleton for adding Search Strategy restrictions

* Add rollup search capabilities

* apply search strategy for annotations request

* set fields capabilities for rollup strategy

* add timezone, interval into SearchCapabilities

* Add fields from capabilities

* add timezone, interval into SearchCapabilities

* fix default timezone

* Merging of two Rollup Jobs was removed

* move getFieldsForWildcard to searchStrategy

* Fix TSVB search requests should have a timeout

# Conflicts:
#	src/legacy/core_plugins/metrics/server/lib/vis_data/get_annotations.js
#	src/legacy/core_plugins/metrics/server/lib/vis_data/series/get_request_params.js

* Add unit test

* apply getEsShardTimeout for annorations/get_request_params,  series/get_request_params

* rename metrics -> tsvb

* search_strategies_register refactoring: move 'add' method from class

* Add merge rollup capabilities with fields

* Add merge rollup capabilities with fields - small fixes

x-pack/plugins/rollup/server/routes/api/index_patterns.js
@@ -10,6 +10,7 @@ import { wrapEsError, wrapUnknownError } from '../../lib/error_wrappers';
 import { licensePreRoutingFactory } from'../../lib/license_pre_routing_factory';
 import indexBy from 'lodash/collection/indexBy';
 import { getCapabilitiesForRollupIndices } from '../../lib/map_capabilities';
+import { mergeCapabilitiesWithFields } from '../../lib/merge_capabilities_with_fields';
 import querystring from 'querystring';
 
 /**
@@ -57,7 +58,6 @@ export function registerFieldsForWildcardRoute(server) {
         const callWithRequest = callWithRequestFactory(server, request);
 
         const rollupFields = [];
-        const rollupFieldNames = [];
         const fieldsFromFieldCapsApi = indexBy(fields, 'name');
         const rollupIndexCapabilities = getCapabilitiesForRollupIndices(await callWithRequest('rollup.rollupIndexCapabilities', {
           indexPattern: rollupIndex
@@ -66,62 +66,10 @@ export function registerFieldsForWildcardRoute(server) {
         // Keep meta fields
         metaFields.forEach(field => fieldsFromFieldCapsApi[field] && rollupFields.push(fieldsFromFieldCapsApi[field]));
 
-        // Merge rollup capabilities information with field information
-        Object.keys(rollupIndexCapabilities).forEach(agg => {
-
-          // Field names of the aggregation
-          const fields = Object.keys(rollupIndexCapabilities[agg]);
-
-          // Default field information
-          const defaultField = {
-            name: null,
-            searchable: true,
-            aggregatable: true,
-            readFromDocValues: true,
-          };
-
-          // Date histogram agg only ever has one field defined, let date type overwrite a
-          // previous type if defined (such as number from max and min aggs).
-          if(agg === 'date_histogram') {
-            const timeFieldName = fields[0];
-            const fieldCapsKey = `${timeFieldName}.${agg}.timestamp`;
-            const newField = {
-              ...fieldsFromFieldCapsApi[fieldCapsKey],
-              ...defaultField,
-              name: timeFieldName,
-            };
-            const existingField = rollupFields.find(field => field.name === timeFieldName);
-
-            if(existingField) {
-              Object.assign(existingField, newField);
-            } else {
-              rollupFieldNames.push(timeFieldName);
-              rollupFields.push(newField);
-            }
-          }
-          // For all other aggs, filter out ones that have already been added to the field list
-          // because the same field can be part of multiple aggregations, but end consumption
-          // doesn't differentiate fields based on their aggregation abilities.
-          else {
-            rollupFields.push(
-              ...fields
-                .filter(field => !rollupFieldNames.includes(field))
-                .map(field => {
-                  // Expand each field into object format that end consumption expects.
-                  const fieldCapsKey = `${field}.${agg}.value`;
-                  rollupFieldNames.push(field);
-                  return {
-                    ...fieldsFromFieldCapsApi[fieldCapsKey],
-                    ...defaultField,
-                    name: field,
-                  };
-                })
-            );
-          }
-        });
+        const mergedRollupFields = mergeCapabilitiesWithFields(rollupIndexCapabilities, fieldsFromFieldCapsApi, rollupFields);
 
         return {
-          fields: rollupFields
+          fields: [ ...rollupFields, ...mergedRollupFields ]
         };
       } catch(err) {
         if (isEsError(err)) {

commit 90fdb149e094818c4393a050906f419d86b83fa8

Remove duplicate fields of rollup based index patterns (#37221)
Rollup fields were were merged twice with other fields, this caused duplicates. Fix of a bug introduced by #28762

x-pack/plugins/rollup/server/routes/api/index_patterns.js
@@ -69,7 +69,7 @@ export function registerFieldsForWildcardRoute(server) {
         const mergedRollupFields = mergeCapabilitiesWithFields(rollupIndexCapabilities, fieldsFromFieldCapsApi, rollupFields);
 
         return {
-          fields: [ ...rollupFields, ...mergedRollupFields ]
+          fields: mergedRollupFields
         };
       } catch(err) {
         if (isEsError(err)) {