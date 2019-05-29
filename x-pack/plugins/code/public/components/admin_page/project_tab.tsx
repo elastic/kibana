9b769b45aa2a2ab84d86345d18354bfd5dae96ec
Fuyao Zhao <zfy0701@gmail.com>
2019-05-29 09:38:49 -0700

    [Code] change suggested clone url

--- x-pack/plugins/code/public/components/admin_page/project_tab.tsx
+++ x-pack/plugins/code/public/components/admin_page/project_tab.tsx
@@ -161,7 +161,7 @@ class CodeProjectTab extends React.PureComponent<Props, State> {
                   value={this.state.repoURL}
                   onChange={this.onChange}
                   onBlur={this.updateIsInvalid}
-                  placeholder="https://github.com/elastic/elasticsearch"
+                  placeholder="https://github.com/elastic/eui"
                   aria-label="input project url"
                   data-test-subj="importRepositoryUrlInputBox"
                   isLoading={this.props.importLoading}
