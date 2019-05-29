621ad917bebfe54c67e71a6ad88044bfcde1fb8b
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2019-05-29 18:59:19 +0200

    skip all feature controls tests for Firefox

--- x-pack/test/functional/apps/infra/feature_controls/index.ts
+++ x-pack/test/functional/apps/infra/feature_controls/index.ts
@@ -7,7 +7,8 @@ import { KibanaFunctionalTestDefaultProviders } from '../../../../types/provider
 
 // eslint-disable-next-line import/no-default-export
 export default function({ loadTestFile }: KibanaFunctionalTestDefaultProviders) {
-  describe('feature controls', () => {
+  describe('feature controls', function() {
+    this.tags('skipFirefox');
     loadTestFile(require.resolve('./infrastructure_security'));
     loadTestFile(require.resolve('./infrastructure_spaces'));
     loadTestFile(require.resolve('./logs_security'));
