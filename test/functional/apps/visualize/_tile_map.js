b4197e216dd6b72d72c6fbeffa589ae9b39c7cf9
Nick Peihl <nick.peihl@elastic.co>
2019-05-30 16:37:58 -0700

    Skip tilemap zoom warning functional test on cloud (#37570)

--- test/functional/apps/visualize/_tile_map.js
+++ test/functional/apps/visualize/_tile_map.js
@@ -236,6 +236,8 @@ export default function ({ getService, getPageObjects }) {
     });
 
     describe('zoom warning behavior', function describeIndexTests() {
+      // Zoom warning is only applicable to OSS
+      this.tags(['skipCloud']);
 
       const waitForLoading = false;
       let zoomWarningEnabled;
