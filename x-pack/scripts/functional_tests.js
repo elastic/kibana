1fa46a4c8b381f3090cbba2b2a397457f488396a
spalger <email@spalger.com>
2019-05-31 14:11:32 -0700

    apply feedback

--- x-pack/scripts/functional_tests.js
+++ x-pack/scripts/functional_tests.js
@@ -10,7 +10,6 @@ require('@kbn/test').runTestsCli([
   require.resolve('../test/reporting/configs/chromium_functional.js'),
   require.resolve('../test/reporting/configs/generate_api'),
   require.resolve('../test/functional/config.js'),
-  // require.resolve('../test/functional/config.firefox.js'),
   require.resolve('../test/api_integration/config_security_basic.js'),
   require.resolve('../test/api_integration/config.js'),
   require.resolve('../test/plugin_api_integration/config.js'),
