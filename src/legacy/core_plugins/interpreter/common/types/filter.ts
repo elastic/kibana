aa81b07b4aeaec693bd9c8ead8b93bd5012c0d91
Luke Elmers <luke.elmers@elastic.co>
2019-05-31 15:50:47 -0600

    Convert interpreter filter type to TS.

--- src/legacy/core_plugins/interpreter/common/types/filter.ts
+++ src/legacy/core_plugins/interpreter/common/types/filter.ts
@@ -17,12 +17,29 @@
  * under the License.
  */
 
-export const filter = () => ({
-  name: 'filter',
+import { ExpressionType } from '../../types';
+
+const name = 'filter';
+
+/**
+ * Represents an object that is a Filter.
+ */
+export interface Filter {
+  type?: string;
+  value?: string;
+  column?: string;
+  and: Filter[];
+  to?: string;
+  from?: string;
+  query?: string | null;
+}
+
+export const filter = (): ExpressionType<typeof name, Filter> => ({
+  name,
   from: {
     null: () => {
       return {
-        type: 'filter',
+        type: name,
         // Any meta data you wish to pass along.
         meta: {},
         // And filters. If you need an "or", create a filter type for it.
