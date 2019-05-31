d4f114a054ac4379ae4fa684c40ce0bd5d58bc5e
Matt Apperson <me@mattapperson.com>
2019-05-31 14:19:05 -0400

    Project init

---
+++ x-pack/plugins/fleet/common/constants/index_names.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const INDEX_NAMES = {
+  FLEET: '.fleet',
+  EVENTS: '.fleet-agent-events-*',
+  EVENTS_ALIAS: '.fleet-agent-events',
+};
+
+export const POLICY_NAMES = {
+  EVENTS: '.fleet-agent-events-retention',
+};
