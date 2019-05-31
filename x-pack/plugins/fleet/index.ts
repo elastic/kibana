7d0acca53e1a3c1fe69a72f0da07442f90ed5246
Matt Apperson <me@mattapperson.com>
2019-05-31 14:19:05 -0400

    Project init

---
+++ x-pack/plugins/fleet/index.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import Joi from 'joi';
+import { resolve } from 'path';
+import { PLUGIN } from './common/constants';
+import { CONFIG_PREFIX } from './common/constants/plugin';
+import { i18n } from '../../../packages/kbn-i18n/src/index';
+//
+
+export const config = Joi.object({
+  enabled: Joi.boolean().default(true),
+}).default();
+
+export function beats(kibana: any) {
+  return new kibana.Plugin({
+    id: PLUGIN.ID,
+    require: ['kibana', 'elasticsearch', 'xpack_main'],
+    publicDir: resolve(__dirname, 'public'),
+    uiExports: {
+      app: {
+        title: 'Elastic Fleet',
+        description: i18n.translate('xpack.fleet.elasticFleetDescription', {
+          defaultMessage: 'Manage your elastic data ingestion stack',
+        }),
+        main: 'plugins/fleet/index',
+        icon: 'plugins/fleet/icon.svg',
+        euiIconType: 'apmApp',
+        order: 8000,
+      },
+    },
+    config: () => config,
+    configPrefix: CONFIG_PREFIX,
+    init(server: any) {},
+  });
+}
