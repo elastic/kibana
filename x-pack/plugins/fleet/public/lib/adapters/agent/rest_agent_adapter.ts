15804ed8c3a542ab63b6dc7fd574ffe8d1089b6c
Matt Apperson <me@mattapperson.com>
2019-05-31 22:03:53 -0400

    initial client libs

---
+++ x-pack/plugins/fleet/public/lib/adapters/agent/rest_agent_adapter.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { Agent } from '../../../../common/types/domain_data';
+import {
+  ReturnTypeGet,
+  ReturnTypeList,
+  ReturnTypeUpdate,
+} from '../../../../common/types/std_return_format';
+import { RestAPIAdapter } from '../rest_api/adapter_types';
+import { AgentAdapter } from './memory_agent_adapter';
+
+export class RestAgentAdapter extends AgentAdapter {
+  constructor(private readonly REST: RestAPIAdapter) {
+    super([]);
+  }
+
+  public async get(id: string): Promise<Agent | null> {
+    try {
+      return (await this.REST.get<ReturnTypeGet<Agent>>(`/api/fleet/agent/${id}`)).item;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  public async getWithToken(enrollmentToken: string): Promise<Agent | null> {
+    try {
+      return (await this.REST.get<ReturnTypeGet<Agent>>(
+        `/api/fleet/agent/unknown/${enrollmentToken}`
+      )).item;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  public async getAll(ESQuery?: string): Promise<Agent[]> {
+    try {
+      return (await this.REST.get<ReturnTypeList<Agent>>('/api/fleet/agents/all', { ESQuery }))
+        .list;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  public async getOnConfig(tagId: string): Promise<Agent[]> {
+    try {
+      return (await this.REST.get<ReturnTypeList<Agent>>(`/api/fleet/agents/tag/${tagId}`)).list;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  public async update(id: string, beatData: Partial<Agent>): Promise<boolean> {
+    await this.REST.put<ReturnTypeUpdate<Agent>>(`/api/fleet/agent/${id}`, beatData);
+    return true;
+  }
+}
---
+++ x-pack/plugins/fleet/public/lib/compose/memory.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { AutocompleteSuggestion } from 'ui/autocomplete_providers';
+import 'ui/autoload/all';
+// @ts-ignore: path dynamic for kibana
+import { management } from 'ui/management';
+// @ts-ignore: path dynamic for kibana
+import { uiModules } from 'ui/modules';
+// @ts-ignore: path dynamic for kibana
+import routes from 'ui/routes';
+// @ts-ignore: path dynamic for kibana
+import { MemoryAgentAdapter } from '../adapters/agent/memory_agents_adapter';
+import { KibanaFrameworkAdapter } from '../adapters/framework/kibana_framework_adapter';
+import { AgentsLib } from '../agent';
+import { FrameworkLib } from '../framework';
+import { FrontendLibs } from '../types';
+import { MemoryElasticsearchAdapter } from './../adapters/elasticsearch/memory';
+import { ElasticsearchLib } from './../elasticsearch';
+
+const onKibanaReady = uiModules.get('kibana').run;
+
+export function compose(
+  mockIsKueryValid: (kuery: string) => boolean,
+  mockKueryToEsQuery: (kuery: string) => string,
+  suggestions: AutocompleteSuggestion[]
+): FrontendLibs {
+  const esAdapter = new MemoryElasticsearchAdapter(
+    mockIsKueryValid,
+    mockKueryToEsQuery,
+    suggestions
+  );
+  const elasticsearchLib = new ElasticsearchLib(esAdapter);
+
+  const agents = new AgentsLib(new MemoryAgentAdapter([]), elasticsearchLib);
+
+  const pluginUIModule = uiModules.get('app/fleet');
+
+  const framework = new FrameworkLib(
+    new KibanaFrameworkAdapter(
+      pluginUIModule,
+      management,
+      routes,
+      () => '',
+      onKibanaReady,
+      null,
+      '7.0.0'
+    )
+  );
+  const libs: FrontendLibs = {
+    framework,
+    elasticsearch: elasticsearchLib,
+    agents,
+  };
+  return libs;
+}
