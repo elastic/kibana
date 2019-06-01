fa94efb9f88b5842465ae9a1ae6fec1ba7775b05
Mengwei Ding <mengwei.ding@gmail.com>
2019-06-01 14:34:50 -0700

    [Code] Nodegit clone/update job cancellation (#37755) (#37795)
    * [Code] Nodegit clone/update job cancellation
    
    * Update tests

--- x-pack/plugins/code/server/queue/cancellation_service.ts
+++ x-pack/plugins/code/server/queue/cancellation_service.ts
@@ -8,11 +8,30 @@ import { RepositoryUri } from '../../model';
 import { CancellationToken } from '../lib/esqueue';
 
 export class CancellationSerivce {
-  // TODO: Add clone/update cancellation map.
+  private cloneCancellationMap: Map<RepositoryUri, CancellationToken>;
+  private updateCancellationMap: Map<RepositoryUri, CancellationToken>;
   private indexCancellationMap: Map<RepositoryUri, CancellationToken>;
 
   constructor() {
+    this.cloneCancellationMap = new Map<RepositoryUri, CancellationToken>();
+    this.updateCancellationMap = new Map<RepositoryUri, CancellationToken>();
     this.indexCancellationMap = new Map<RepositoryUri, CancellationToken>();
+  }
+
+  public cancelCloneJob(repoUri: RepositoryUri) {
+    const token = this.cloneCancellationMap.get(repoUri);
+    if (token) {
+      token.cancel();
+      this.cloneCancellationMap.delete(repoUri);
+    }
+  }
+
+  public cancelUpdateJob(repoUri: RepositoryUri) {
+    const token = this.updateCancellationMap.get(repoUri);
+    if (token) {
+      token.cancel();
+      this.updateCancellationMap.delete(repoUri);
+    }
   }
 
   public cancelIndexJob(repoUri: RepositoryUri) {
@@ -21,6 +40,22 @@ export class CancellationSerivce {
       token.cancel();
       this.indexCancellationMap.delete(repoUri);
     }
+  }
+
+  public registerCloneJobToken(repoUri: RepositoryUri, cancellationToken: CancellationToken) {
+    const token = this.cloneCancellationMap.get(repoUri);
+    if (token) {
+      token.cancel();
+    }
+    this.cloneCancellationMap.set(repoUri, cancellationToken);
+  }
+
+  public registerUpdateJobToken(repoUri: RepositoryUri, cancellationToken: CancellationToken) {
+    const token = this.updateCancellationMap.get(repoUri);
+    if (token) {
+      token.cancel();
+    }
+    this.updateCancellationMap.set(repoUri, cancellationToken);
   }
 
   public registerIndexJobToken(repoUri: RepositoryUri, cancellationToken: CancellationToken) {
