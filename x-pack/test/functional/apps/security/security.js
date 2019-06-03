b6246aa4fc43c9bb53b0196e780fe409f1942093
Larry Gregory <larry.gregory@elastic.co>
2019-06-03 08:49:10 -0400

    remove newline

--- x-pack/test/functional/apps/security/security.js
+++ x-pack/test/functional/apps/security/security.js
@@ -42,7 +42,6 @@ export default function ({ getService, getPageObjects }) {
 
         const logoutMessage = await testSubjects.getVisibleText('loginInfoMessage');
         expect(logoutMessage).to.eql('You have logged out of Kibana.');
-
       });
     });
   });
