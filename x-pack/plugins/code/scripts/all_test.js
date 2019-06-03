9b2cd023134b4aa2a0df9ce0d2def5eda88864f6
Davey Holler <daveyholler@gmail.com>
2019-06-03 14:32:04 -0700

    Fixing a typo in all_test.js (#37684)

--- x-pack/plugins/code/scripts/all_test.js
+++ x-pack/plugins/code/scripts/all_test.js
@@ -8,5 +8,5 @@ const execa = require('execa'); // eslint-disable-line import/no-extraneous-depe
 
 execa.sync('node', [require.resolve('./jest')], { stdio: 'inherit' });
 execa.sync('node', [require.resolve('./mocha')], { stdio: 'inherit' });
-execa.sync('node', [require.resolve('./functional_jest')], { stdio: 'inherit' });
+execa.sync('node', [require.resolve('./functional_test')], { stdio: 'inherit' });
 execa.sync('node', [require.resolve('./api_integration_test')], { stdio: 'inherit' });
