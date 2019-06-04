50bb624e73ee00b5d90cd312b1fe50b5c314d968
Tim Roes <mail@timroes.de>
2019-06-04 15:55:17 +0200

    Fix more tests

--- src/legacy/ui/public/agg_types/__tests__/metrics/sibling_pipeline.js
+++ src/legacy/ui/public/agg_types/__tests__/metrics/sibling_pipeline.js
@@ -60,7 +60,7 @@ describe('sibling pipeline aggs', function () {
               id: '6',
               type: 'date_histogram',
               schema: 'bucket',
-              params: { field: '@timestamp' }
+              params: { field: '@timestamp', interval: '10s' }
             }
           };
 
