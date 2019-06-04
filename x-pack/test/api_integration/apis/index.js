f68a21386b22f549600dcf8f09f98b14bc9add9b
Larry Gregory <larry.gregory@elastic.co>
2019-06-04 07:47:45 -0400

    Feature Controls - automatically grant access to short urls (#37532)
    * automatically grant access to short urls
    
    * adds API integration tests
    
    * Update x-pack/test/api_integration/apis/short_urls/index.ts
    
    Co-Authored-By: Brandon Kobel <brandon.kobel@gmail.com>

--- x-pack/test/api_integration/apis/index.js
+++ x-pack/test/api_integration/apis/index.js
@@ -23,5 +23,6 @@ export default function ({ loadTestFile }) {
     loadTestFile(require.resolve('./apm'));
     loadTestFile(require.resolve('./siem'));
     loadTestFile(require.resolve('./code'));
+    loadTestFile(require.resolve('./short_urls'));
   });
 }
