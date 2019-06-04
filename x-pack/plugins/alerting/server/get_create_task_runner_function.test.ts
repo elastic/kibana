e9c0864a277583b263d798261987e20697b93798
Mike Cote <mikecote@users.noreply.github.com>
2019-06-04 14:32:45 -0400

    Fix get_create_task_runner_function.test.ts and make test pass

--- x-pack/plugins/alerting/server/get_create_task_runner_function.test.ts
+++ x-pack/plugins/alerting/server/get_create_task_runner_function.test.ts
@@ -19,16 +19,24 @@ const getCreateTaskRunnerFunctionParams = {
 
 beforeEach(() => jest.resetAllMocks());
 
+const mockedNow = new Date('2019-06-03T18:55:25.982Z');
+(global as any).Date = class Date extends global.Date {
+  static now() {
+    return mockedNow.getTime();
+  }
+};
+
 test('successfully executes the task', async () => {
+  const mockedLastRunAt = new Date('2019-06-03T18:55:20.982Z');
   const createTaskRunner = getCreateTaskRunnerFunction(getCreateTaskRunnerFunctionParams);
   const taskInstance = {
-    runAt: new Date(),
+    runAt: mockedLastRunAt,
     state: {
       previousRange: {
-        from: new Date(),
-        to: new Date(),
+        from: new Date(mockedLastRunAt.getTime() - 10000),
+        to: new Date(mockedLastRunAt.getTime() - 10000),
       },
-      scheduledRunAt: new Date(),
+      scheduledRunAt: mockedLastRunAt,
     },
     taskType: 'alerting:test',
     params: {
@@ -66,16 +74,43 @@ test('successfully executes the task', async () => {
   const runnerResult = await runner.run();
   expect(runnerResult).toMatchInlineSnapshot(`
 Object {
-  "runAt": 2019-06-03T18:55:20.982Z,
+  "runAt": 2019-06-03T18:55:30.982Z,
   "state": Object {
     "alertInstances": Object {},
     "alertTypeState": undefined,
     "previousRange": Object {
       "from": 2019-06-03T18:55:10.983Z,
-      "to": 2019-06-03T18:55:10.982Z,
+      "to": 2019-06-03T18:55:20.982Z,
     },
-    "scheduledRunAt": 2019-06-03T18:55:20.982Z,
+    "scheduledRunAt": 2019-06-03T18:55:30.982Z,
   },
+}
+`);
+  expect(getCreateTaskRunnerFunctionParams.alertType.execute).toMatchInlineSnapshot(`
+[MockFunction] {
+  "calls": Array [
+    Array [
+      Object {
+        "params": Object {
+          "bar": true,
+        },
+        "range": Object {
+          "from": 2019-06-03T18:55:10.983Z,
+          "to": 2019-06-03T18:55:20.982Z,
+        },
+        "services": Object {
+          "alertInstanceFactory": [Function],
+        },
+        "state": Object {},
+      },
+    ],
+  ],
+  "results": Array [
+    Object {
+      "type": "return",
+      "value": undefined,
+    },
+  ],
 }
 `);
 });
