cd4f860cd51110d6b79f724ef4f2f4a3e256ef8c
Garrett Spong <spong@users.noreply.github.com>
2019-06-04 10:22:24 -0600

    [SIEM] Fix IP Overview query to query for most recent Host (#37723)
    ## Summary
    
    As outlined in https://github.com/elastic/ingest-dev/issues/453, this PR fixes the `IP Overview` query to query for the most recent Host.
    
    Now the Host Data shown will be the most recent host for the given IP regardless of whether `Source` or `Destination` is selected in the FlowTargetSelect.
    
    ![image](https://user-images.githubusercontent.com/2946766/58731558-46fb5200-83ac-11e9-97d5-7db5dd8ac84e.png)
    
    
    ### Checklist
    
    Use ~~strikethroughs~~ to remove checklist items you don't feel are applicable to this PR.
    
    - [ ] ~This was checked for cross-browser compatibility, [including a check against IE11](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility)~
    - [ ] ~Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/master/packages/kbn-i18n/README.md)~
    - [ ] ~[Documentation](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#writing-documentation) was added for features that require explanation or tutorials~
    - [x] [Unit or functional tests](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility) were updated or added to match the most common scenarios
    - [ ] ~This was checked for [keyboard-only and screenreader accessibility](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Cross_browser_testing/Accessibility#Accessibility_testing_checklist)~
    
    ### For maintainers
    
    - [ ] ~This was checked for breaking API changes and was [labeled appropriately](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#release-notes-process)~
    - [ ] ~This includes a feature addition or change that requires a release note and was [labeled appropriately](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#release-notes-process)~

--- x-pack/plugins/siem/public/components/field_renderers/field_renderers.test.tsx
+++ x-pack/plugins/siem/public/components/field_renderers/field_renderers.test.tsx
@@ -114,9 +114,7 @@ describe('Field Renderers', () => {
     };
     test('it renders correctly against snapshot', () => {
       const wrapper = shallow(
-        <TestProviders>
-          {hostNameRenderer(mockData.complete.source!.host!, '10.10.10.10')}
-        </TestProviders>
+        <TestProviders>{hostNameRenderer(mockData.complete.host, '10.10.10.10')}</TestProviders>
       );
 
       expect(toJson(wrapper)).toMatchSnapshot();
@@ -124,9 +122,7 @@ describe('Field Renderers', () => {
 
     test('it renders emptyTagValue when non-matching IP is provided', () => {
       const wrapper = mount(
-        <TestProviders>
-          {hostNameRenderer(mockData.complete.source!.host!, '10.10.10.11')}
-        </TestProviders>
+        <TestProviders>{hostNameRenderer(mockData.complete.host, '10.10.10.11')}</TestProviders>
       );
       expect(wrapper.text()).toEqual(getEmptyValue());
     });
@@ -163,9 +159,7 @@ describe('Field Renderers', () => {
     };
     test('it renders correctly against snapshot', () => {
       const wrapper = shallow(
-        <TestProviders>
-          {hostNameRenderer(mockData.complete.source!.host!, '10.10.10.10')}
-        </TestProviders>
+        <TestProviders>{hostNameRenderer(mockData.complete.host, '10.10.10.10')}</TestProviders>
       );
 
       expect(toJson(wrapper)).toMatchSnapshot();
@@ -173,9 +167,7 @@ describe('Field Renderers', () => {
 
     test('it renders emptyTagValue when non-matching IP is provided', () => {
       const wrapper = mount(
-        <TestProviders>
-          {hostNameRenderer(mockData.complete.source!.host!, '10.10.10.11')}
-        </TestProviders>
+        <TestProviders>{hostNameRenderer(mockData.complete.host, '10.10.10.11')}</TestProviders>
       );
       expect(wrapper.text()).toEqual(getEmptyValue());
     });
