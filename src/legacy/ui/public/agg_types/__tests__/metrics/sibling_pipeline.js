cd39706dfffd2b8232a03e085d415ff700aeb6ad
Tim Roes <mail@timroes.de>
2019-06-05 09:56:56 +0200

    Fix more karma tests

--- src/legacy/ui/public/agg_types/__tests__/metrics/sibling_pipeline.js
+++ src/legacy/ui/public/agg_types/__tests__/metrics/sibling_pipeline.js
@@ -115,7 +115,7 @@ describe('sibling pipeline aggs', function () {
             id: '6',
             type: 'date_histogram',
             schema: 'bucket',
-            params: { field: '@timestamp' },
+            params: { field: '@timestamp', interval: '10s' },
           }
         });
         expect(aggDsl[metric.name].buckets_path).to.be('2-bucket>2-metric');
