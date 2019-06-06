b488ae5b480df4478a21d38232449aa9e5be61ad
Justin Kambic <justin.kambic@elastic.co>
2019-06-06 13:04:20 -0400

    Update link.

--- x-pack/plugins/uptime/public/components/functional/monitor_list.tsx
+++ x-pack/plugins/uptime/public/components/functional/monitor_list.tsx
@@ -124,7 +124,7 @@ export const MonitorListComponent = ({
                 locationName
               ) : (
                 <EuiLink
-                  href="https://www.elastic.co/guide/en/beats/heartbeat/current/add-observer-metadata.html"
+                  href="https://www.elastic.co/guide/en/beats/heartbeat/current/configuration-observer-options.html"
                   target="_blank"
                 >
                   {i18n.translate('xpack.uptime.monitorList.geoName.helpLinkAnnotation', {
