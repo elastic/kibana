0c52a16672be8ba923d52dd25c414228c875c1e3
Tudor Golubenco <tudor@elastic.co>
2019-06-06 21:32:30 +0200

    Update links to docs and website in the SIEM app

--- x-pack/plugins/siem/public/pages/overview/summary.tsx
+++ x-pack/plugins/siem/public/pages/overview/summary.tsx
@@ -39,7 +39,7 @@ export const Summary = pure(() => (
               </EuiLink>
             ),
             siemSolution: (
-              <EuiLink href="https://www.elastic.co/solutions/security-analytics" target="blank">
+              <EuiLink href="https://www.elastic.co/solutions/siem" target="blank">
                 <FormattedMessage
                   id="xpack.siem.overview.startedText.siemSolutionLinkText"
                   defaultMessage="Security Analytics"
