dcd26fa67e0b612f067b7d985107c6dcd778352c
Matt Kime <matt@mattki.me>
2019-06-09 11:05:13 -0500

    remove mock session storage usage

--- x-pack/plugins/xpack_main/public/services/__tests__/xpack_info_signature.js
+++ x-pack/plugins/xpack_main/public/services/__tests__/xpack_info_signature.js
@@ -5,33 +5,12 @@
  */
 
 import expect from '@kbn/expect';
-import ngMock from 'ng_mock';
-import sinon from 'sinon';
 import { xpackInfoSignature } from 'plugins/xpack_main/services/xpack_info_signature';
-import { mockWindow } from './_mock_window';
-
-const XPACK_INFO_SIG_KEY = 'xpackMain.infoSignature';
 
 describe('xpack_info_signature service', () => {
-  beforeEach(ngMock.module('kibana', () => {
-    sinon.stub(sessionStorage, 'getItem')
-      .callsFake(mockWindow.sessionStorage.getItem);
-    sinon.stub(sessionStorage, 'setItem')
-      .callsFake(mockWindow.sessionStorage.setItem);
-    sinon.stub(sessionStorage, 'removeItem')
-      .callsFake(mockWindow.sessionStorage.removeItem);
-  }));
-
-  afterEach(() => {
-    sessionStorage.getItem.restore();
-    sessionStorage.setItem.restore();
-    sessionStorage.removeItem.restore();
-  });
-
   it ('updates the stored xpack info signature', () => {
     const updatedXPackInfoSignature = 'foobar';
     xpackInfoSignature.set(updatedXPackInfoSignature);
-    expect(mockWindow.sessionStorage.getItem(XPACK_INFO_SIG_KEY)).to.be(updatedXPackInfoSignature);
     expect(xpackInfoSignature.get()).to.be(updatedXPackInfoSignature);
   });
 
@@ -39,9 +18,7 @@ describe('xpack_info_signature service', () => {
     const updatedXPackInfoSignature = 'foobar';
     xpackInfoSignature.set(updatedXPackInfoSignature);
     expect(xpackInfoSignature.get()).not.to.be(undefined);
-
     xpackInfoSignature.clear();
-    expect(mockWindow.sessionStorage.getItem(XPACK_INFO_SIG_KEY)).to.be(undefined);
-    expect(xpackInfoSignature.get()).to.be(undefined);
+    expect(xpackInfoSignature.get()).to.be(null);
   });
 });
