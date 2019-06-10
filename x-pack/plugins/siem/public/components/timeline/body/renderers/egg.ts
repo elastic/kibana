0a08c31ba67528cb60014e9fd2729917e23e9234
FrankHassanabad <frank.hassanabad@elastic.co>
2019-06-10 15:11:50 -0600

    Test chicken and egg riddle with deps checking

---
+++ x-pack/plugins/siem/public/components/timeline/body/renderers/egg.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { chicken } from './chicken';
+
+export const egg = 'egg';
+
+export const myRiddle = `what came first, the ${chicken} or the ${egg}`;
