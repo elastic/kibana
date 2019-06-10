77d4fac5635ac0fe500d207adf93ab6b4a0897d0
Spencer <email@spalger.com>
2019-06-10 08:35:23 -0700

    [6.8] [testFailureReporting] only run on master/version jobs (#38497) (#38549)

--- test/scripts/jenkins_ci_group.sh
+++ test/scripts/jenkins_ci_group.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
-function report {
-  if [[ -z "$PR_SOURCE_BRANCH" ]]; then
-    node src/dev/failed_tests/cli
-  else
-    echo "Failure issues not created on pull requests"
-
-  fi
-}
-
-trap report EXIT
+trap 'node "$KIBANA_DIR/src/dev/failed_tests/cli"' EXIT
 
 "$(FORCE_COLOR=0 yarn bin)/grunt" functionalTests:ensureAllTestsInCiGroup;
 
