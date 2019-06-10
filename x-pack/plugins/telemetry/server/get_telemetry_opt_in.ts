725b63b540f9031ef53cb2137a9773633efa7e04
Bamieh <ahmadbamieh@gmail.com>
2019-06-10 12:43:40 +0300

    js -> ts

--- x-pack/plugins/telemetry/server/get_telemetry_opt_in.ts
+++ x-pack/plugins/telemetry/server/get_telemetry_opt_in.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export async function getTelemetryOptIn(request) {
+export async function getTelemetryOptIn(request: any) {
   const isRequestingApplication = request.path.startsWith('/app');
 
   // Prevent interstitial screens (such as the space selector) from prompting for telemetry
