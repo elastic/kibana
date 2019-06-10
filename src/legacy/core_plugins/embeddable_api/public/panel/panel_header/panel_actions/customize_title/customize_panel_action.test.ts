6688fd6d8eae3e7d552c67afd6e80e79efbcf256
Stacey Gammon <gammon@elastic.co>
2019-06-10 18:04:03 -0400

    Embeddable API V2 (#37510) (#38602)
    * Embeddable API plugin
    
    * Expose new embeddableActions uiExport endpoint
    
    * Add missing getInjectedUIAppVars to Server type
    
    * Add jest tests
    
    * Most basic skeleton of the sample plugin to see if it still kills ci
    
    * strip even more out to see if it passes ci
    
    * It passed, put back init fn
    
    * add back in a uiExport to see if that is what is killing ci
    
    * Passed again, add back embeddableActions uiExports and require kibana line
    
    * Add everything back but the sass import
    
    * Found two bugs with customize panel title action, added jest test coverage and fixes
    
    * Functionally test embeddable explorer plugin
    
    * Addressing review feedback part 1
    
    * Simplify action context menu - remove support for nested actions/child panels, which was never exposed anyway.
    
    * More review feedback
    
    * Spread out orders to let developers inject their own actions in the middle.
    
    * Remove check for overwriting ApplyFilterAction
    
    * use createRegistry for EmbeddableFactoryRegistry
    
    * Add comment for getInheritedInput
    
    * Use kbn-es-query Filter types
    
    * Fix missed file after createRegistry switch over for EmbeddableFactoryRegistry
    
    * Use delete instead of setting to undefined
    
    * upgrade EUI to match kibana version
    
    * Add getIsContainer on base embeddable class
    
    * Run functional tests of sample plugin
    
    * move all tests classes outside __test__ and inside test_samples folder so not to potentially create issues with mocha
    
    * fix: 🐛 remove unused imports
    
    * fix: 🐛 set to undefined explicitly
    
    * chore: refactored getRoot functionality
    
    * fix: refactor miss from folder rename __test__ -> test_samples
    
    * fix: add eui switch to let the user indicate “hide this title” to improve ux.
    
    * fix: customize panel flout test
    
    * Try to fix issue with multiple rxjs bundles I hit before.
    
    See https://github.com/ReactiveX/rxjs/issues/3828
    
    * Use an rxjs polyfill to work around the issue of multiple rxjs bundles
    
    * chore: change customize panel flyout to modal post design feedback
    
    * capitalize Reset
    
    * fix: type errors in customize panel modal
    
    * fix: remove extra line added to prevent EUICallout overflow from spilling over panel edges
    
    The bug this was intended to fix doesn’t appear yet because dashboard isn’t using this, and it causes other issues, so for now, just remove.
    
    * Clean up some classes and SASS
    
    * Inline styles needed to get the containers to take up the full height of the app.
    
    * Use the same old style of error messaging in panels
    
    * chore: add untilEmbeddableLoaded tests, expose on interface, and use in embeddable_child_panel
    
    * Remove unused styles, add padding
    
    * Verify trigger Context containers an array of Filters for apply filter action
    
    * Remove panel.embeddableId param, use panel.explicitInput.id instead
    
    * Check `isCompatible` as well as the type guard needed for typescript.
    
    * executeTriggerActions should account for getHref
    
    * Remove unnecessary check for context being defined
    
    * use npStart instead of getNewPlatform
    
    * Conform to latest NP changes
    
    * Use new np_mocks in all the tests
    
    * Address review feedback on rxjs polyfill

---
+++ src/legacy/core_plugins/embeddable_api/public/panel/panel_header/panel_actions/customize_title/customize_panel_action.test.ts
@@ -0,0 +1,113 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import '../../../../np_core.test.mocks';
+
+import {
+  CONTACT_CARD_EMBEDDABLE,
+  ContactCardEmbeddableFactory,
+  HelloWorldContainer,
+  ContactCardEmbeddable,
+  ContactCardEmbeddableOutput,
+  ContactCardEmbeddableInput,
+} from '../../../../test_samples/index';
+
+import { Container, isErrorEmbeddable } from '../../../..';
+// @ts-ignore
+import { findTestSubject } from '@elastic/eui/lib/test';
+import { nextTick } from 'test_utils/enzyme_helpers';
+import { CustomizePanelTitleAction } from './customize_panel_action';
+import { createRegistry } from '../../../../create_registry';
+import { EmbeddableFactory } from '../../../../embeddables';
+
+let container: Container;
+let embeddable: ContactCardEmbeddable;
+
+function createHelloWorldContainer(input = { id: '123', panels: {} }) {
+  const embeddableFactories = createRegistry<EmbeddableFactory>();
+  embeddableFactories.set(CONTACT_CARD_EMBEDDABLE, new ContactCardEmbeddableFactory());
+  return new HelloWorldContainer(input, embeddableFactories);
+}
+
+beforeEach(async () => {
+  container = createHelloWorldContainer();
+  const contactCardEmbeddable = await container.addNewEmbeddable<
+    ContactCardEmbeddableInput,
+    ContactCardEmbeddableOutput,
+    ContactCardEmbeddable
+  >(CONTACT_CARD_EMBEDDABLE, {
+    id: 'robert',
+    firstName: 'Robert',
+    lastName: 'Baratheon',
+  });
+  if (isErrorEmbeddable(contactCardEmbeddable)) {
+    throw new Error('Error creating new hello world embeddable');
+  } else {
+    embeddable = contactCardEmbeddable;
+  }
+});
+
+test('Updates the embeddable title when given', async done => {
+  const getUserData = () => Promise.resolve({ title: 'What is up?' });
+  const customizePanelAction = new CustomizePanelTitleAction(getUserData);
+  expect(embeddable.getInput().title).toBeUndefined();
+  expect(embeddable.getTitle()).toBe('Hello Robert Baratheon');
+  await customizePanelAction.execute({ embeddable });
+  await nextTick();
+  expect(embeddable.getTitle()).toBe('What is up?');
+  expect(embeddable.getInput().title).toBe('What is up?');
+
+  // Recreating the container should preserve the custom title.
+  const containerClone = createHelloWorldContainer(container.getInput());
+  // Need to wait for the container to tell us the embeddable has been loaded.
+  const subscription = containerClone.getOutput$().subscribe(() => {
+    if (containerClone.getOutput().embeddableLoaded[embeddable.id]) {
+      expect(embeddable.getInput().title).toBe('What is up?');
+      subscription.unsubscribe();
+      done();
+    }
+  });
+});
+
+test('Empty string results in an empty title', async () => {
+  const getUserData = () => Promise.resolve({ title: '' });
+  const customizePanelAction = new CustomizePanelTitleAction(getUserData);
+  expect(embeddable.getInput().title).toBeUndefined();
+  expect(embeddable.getTitle()).toBe('Hello Robert Baratheon');
+
+  await customizePanelAction.execute({ embeddable });
+  await nextTick();
+  expect(embeddable.getTitle()).toBe('');
+});
+
+test('Undefined title results in the original title', async () => {
+  const getUserData = () => Promise.resolve({ title: 'hi' });
+  const customizePanelAction = new CustomizePanelTitleAction(getUserData);
+  expect(embeddable.getInput().title).toBeUndefined();
+  expect(embeddable.getTitle()).toBe('Hello Robert Baratheon');
+  await customizePanelAction.execute({ embeddable });
+  await nextTick();
+  expect(embeddable.getTitle()).toBe('hi');
+
+  await new CustomizePanelTitleAction(() => Promise.resolve({ title: undefined })).execute({
+    embeddable,
+  });
+  await nextTick();
+  expect(embeddable.getTitle()).toBe('Hello Robert Baratheon');
+});
