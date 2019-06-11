43160cdde2364eea3f6d5be392a40f77e13676fc
sulemanof <sul-dan@mail.ru>
2019-06-11 14:05:13 +0300

    Add unit tests

--- src/legacy/ui/public/agg_types/controls/date_ranges.test.tsx
+++ src/legacy/ui/public/agg_types/controls/date_ranges.test.tsx
@@ -18,7 +18,75 @@
  */
 
 import React from 'react';
-import { mount } from 'enzyme';
+import { mountWithIntl } from 'test_utils/enzyme_helpers';
 import { DateRangesParamEditor } from './date_ranges';
 
-describe('DateRangesParamEditor component', () => {});
+jest.mock('../../documentation_links', () => ({
+  getDocLink: jest.fn(),
+}));
+
+describe('DateRangesParamEditor component', () => {
+  let setValue: jest.Mock;
+  let setValidity: jest.Mock;
+  let setTouched: jest.Mock;
+  let defaultProps: any;
+
+  beforeEach(() => {
+    setValue = jest.fn();
+    setValidity = jest.fn();
+    setTouched = jest.fn();
+
+    defaultProps = {
+      agg: {},
+      aggParam: {
+        name: 'ranges',
+      },
+      value: [],
+      editorConfig: {},
+      showValidation: false,
+      setValue,
+      setValidity,
+      setTouched,
+    };
+  });
+
+  it('should add default range if there is an empty ininitial value', () => {
+    mountWithIntl(<DateRangesParamEditor {...defaultProps} />);
+
+    expect(setValue).toHaveBeenCalledWith([{}]);
+  });
+
+  it('should validate range values with date math', function() {
+    const component = mountWithIntl(<DateRangesParamEditor {...defaultProps} />);
+
+    // should allow empty values
+    expect(setValidity).toHaveBeenNthCalledWith(1, true);
+
+    component.setProps({ value: [{ from: 'hello, world' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(2, false);
+
+    component.setProps({ value: [{ from: 'now' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(3, true);
+
+    component.setProps({ value: [{ from: 'now', to: 'Hello, Dan! =)' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(4, false);
+
+    component.setProps({ value: [{ from: '2012-02-28' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(5, true);
+
+    component.setProps({ value: [{ from: 'now+-5w', to: 'now-3d' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(6, false);
+
+    component.setProps({ value: [{ from: 'now-3M/M' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(7, true);
+
+    component.setProps({ value: [{ from: '2012-02-31', to: 'now-3d' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(8, false);
+
+    component.setProps({ value: [{ to: '2012-05-31||-3M/M' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(9, true);
+
+    component.setProps({ value: [{ from: '5/5/2005+3d' }] });
+    expect(setValidity).toHaveBeenNthCalledWith(10, false);
+  });
+});
