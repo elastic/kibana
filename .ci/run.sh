13c362dae3080afd9dd9e8d5104e816ce4144a01
Tre' Seymour <wayne.seymour@elastic.co>
2019-06-11 15:54:15 -0600

    Temp drop sourced scripts. Trying to see my debug info.

--- .ci/run.sh
+++ .ci/run.sh
@@ -5,9 +5,9 @@ set -e
 # move to Kibana root
 cd "$(dirname "$0")/.."
 
-source src/dev/ci_setup/extract_bootstrap_cache.sh
-source src/dev/ci_setup/setup.sh
-source src/dev/ci_setup/checkout_sibling_es.sh
+#source src/dev/ci_setup/extract_bootstrap_cache.sh
+#source src/dev/ci_setup/setup.sh
+#source src/dev/ci_setup/checkout_sibling_es.sh
 
 echo "\n\t### JOB: ${JOB}"
 
