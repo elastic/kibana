69e4a1c5e3fff07ae9a27d12552d75e3dab686d4
Tre' Seymour <wayne.seymour@elastic.co>
2019-06-12 11:22:03 -0600

    Uncomment the startup scripts, run the intake job(s)

--- .ci/run.sh
+++ .ci/run.sh
@@ -5,30 +5,30 @@ set -e
 # move to Kibana root
 cd "$(dirname "$0")/.."
 
-#source src/dev/ci_setup/extract_bootstrap_cache.sh
-#source src/dev/ci_setup/setup.sh
-#source src/dev/ci_setup/checkout_sibling_es.sh
+source src/dev/ci_setup/extract_bootstrap_cache.sh
+source src/dev/ci_setup/setup.sh
+source src/dev/ci_setup/checkout_sibling_es.sh
 
 echo "\n\t### JOB: ${JOB}"
 
 case "$JOB" in
 kibana-intake)
-#  ./test/scripts/jenkins_unit.sh
-  echo "\n\t### kibana-intake"
+ ./test/scripts/jenkins_unit.sh
+  echo "\n\t### ACTUALLY RUNNING kibana-intake"
   ;;
 kibana-ciGroup*)
   export CI_GROUP="${JOB##kibana-ciGroup}"
 #  ./test/scripts/jenkins_ci_group.sh
-  echo "\n\t### kibana-ciGroup*"
+  echo "\n\t### NOT RUNNING kibana-ciGroup*"
   ;;
 x-pack-intake)
 #  ./test/scripts/jenkins_xpack.sh
-  echo "\n\t### x-pack-intake"
+  echo "\n\t### NOT RUNNING x-pack-intake"
   ;;
 x-pack-ciGroup*)
   export CI_GROUP="${JOB##x-pack-ciGroup}"
 #  ./test/scripts/jenkins_xpack_ci_group.sh
-  echo "\n\t### x-pack-ciGroup*"
+  echo "\n\t### NOT RUNNING x-pack-ciGroup*"
   ;;
 *)
   echo "JOB '$JOB' is not implemented."
