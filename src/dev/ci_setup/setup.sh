31ceb7dc207dacadc6b26a5dbd806d5d714068e0
Tre' Seymour <wayne.seymour@elastic.co>
2019-06-12 16:05:29 -0600

    Use node instead of jq, since it-aint-there :)

--- src/dev/ci_setup/setup.sh
+++ src/dev/ci_setup/setup.sh
@@ -33,7 +33,8 @@ export XPACK_DIR="$KIBANA_DIR/x-pack"
 parentDir="$(cd "$KIBANA_DIR/.."; pwd)"
 export PARENT_DIR="$parentDir"
 
-kbnBranch="$(jq -r .branch "$KIBANA_DIR/package.json")"
+# kbnBranch="$(jq -r .branch "$KIBANA_DIR/package.json")"
+kbnBranch="$(node -e "console.log(require('./package.json').branch)")"
 export KIBANA_PKG_BRANCH="$kbnBranch"
 
 echo " -- KIBANA_DIR='$KIBANA_DIR'"
