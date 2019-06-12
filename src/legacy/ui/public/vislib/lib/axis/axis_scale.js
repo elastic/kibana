affce12aac10dbc731a11de405df130f81ae6091
mmeshi <meir.meshi@gmail.com>
2019-06-12 12:25:43 +0300

    better way to manipulate axis to make room for labels

--- src/legacy/ui/public/vislib/lib/axis/axis_scale.js
+++ src/legacy/ui/public/vislib/lib/axis/axis_scale.js
@@ -155,12 +155,26 @@ export class AxisScale {
     return [Math.min(0, min), Math.max(0, max)];
   }
 
-  getRange(length) {
+  getDomain(length) {
+    const domain = this.getExtents();
     const pad = this.axisConfig.get('padForLabels');
+    if (pad > 0) {
+      const domainLength = domain[1] - domain[0];
+      const valuePerPixel = domainLength / length;
+      const padValue = valuePerPixel * pad;
+      if (domain[0] < 0) {
+        domain[0] -= padValue;
+      }
+      domain[1] += padValue;
+    }
+    return domain;
+  }
+
+  getRange(length) {
     if (this.axisConfig.isHorizontal()) {
-      return !this.axisConfig.get('scale.inverted') ? [0, length - pad] : [length - pad, 0];
+      return !this.axisConfig.get('scale.inverted') ? [0, length] : [length, 0];
     } else {
-      return this.axisConfig.get('scale.inverted') ? [0 + pad, length] : [length, 0 + pad];
+      return this.axisConfig.get('scale.inverted') ? [0, length] : [length, 0];
     }
   }
 
@@ -200,7 +214,7 @@ export class AxisScale {
   getScale(length) {
     const config = this.axisConfig;
     const scale = this.getD3Scale(config.getScaleType());
-    const domain = this.getExtents();
+    const domain = this.getDomain(length);
     const range = this.getRange(length);
     const padding = config.get('style.rangePadding');
     const outerPadding = config.get('style.rangeOuterPadding');
