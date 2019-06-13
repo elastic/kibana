efea27d186a7269773b1c41d7abb8f7e73b1a0bb
Nathan Reese <reese.nathan@gmail.com>
2019-06-13 11:54:31 -0600

    break out ToolsControl from ToolbarOverlay component

--- x-pack/plugins/maps/public/selectors/map_selectors.js
+++ x-pack/plugins/maps/public/selectors/map_selectors.js
@@ -183,7 +183,7 @@ export const getUniqueIndexPatternIds = createSelector(
     layerList.forEach(layer => {
       indexPatternIds.push(...layer.getIndexPatternIds());
     });
-    return _.uniq(indexPatternIds);
+    return _.uniq(indexPatternIds).sort();
   }
 );
 
