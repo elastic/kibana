4df9b5c9aa7cec8bcb5ce1f9fffa906c0ffea006
Aaron Caldwell <aaron.caldwell@elastic.co>
2019-06-14 09:52:36 -0600

    [File upload] Remove dynamic fields from mappings, code and telemetry test (#38902)
    * Remove dynamic fields from mappings, code and telemetry test
    
    * Add file-upload-telemetry to spaces and es archiver test mappings
    
    * Don't create telemetry saved object if none exists, create on first update instead
    
    * Back out es archiver mappings update

--- x-pack/plugins/file_upload/server/telemetry/telemetry.test.ts
+++ x-pack/plugins/file_upload/server/telemetry/telemetry.test.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { getTelemetry, incrementCounts, updateTelemetry } from './telemetry';
+import { getTelemetry, updateTelemetry } from './telemetry';
 
 const internalRepository = () => ({
   get: jest.fn(() => null),
@@ -32,13 +32,6 @@ function mockInit(getVal: any = { attributes: {} }): any {
 
 describe('file upload plugin telemetry', () => {
   describe('getTelemetry', () => {
-    it('should create new telemetry if no telemetry exists', async () => {
-      const internalRepo = mockInit({});
-      await getTelemetry(server, internalRepo);
-      expect(internalRepo.get.mock.calls.length).toBe(1);
-      expect(internalRepo.create.mock.calls.length).toBe(1);
-    });
-
     it('should get existing telemetry', async () => {
       const internalRepo = mockInit();
       await getTelemetry(server, internalRepo);
@@ -59,50 +52,6 @@ describe('file upload plugin telemetry', () => {
       expect(internalRepo.update.mock.calls.length).toBe(1);
       expect(internalRepo.get.mock.calls.length).toBe(1);
       expect(internalRepo.create.mock.calls.length).toBe(0);
-    });
-  });
-
-  describe('incrementCounts', () => {
-    const oldCounts = {
-      filesUploadedTotalCount: 3,
-      filesUploadedTypesTotalCounts: {
-        json: 1,
-        csv: 2,
-      },
-      filesUploadedByApp: {
-        maps: {
-          json: 1,
-          csv: 1,
-        },
-        ml: {
-          csv: 1,
-        },
-      },
-    };
-    const app = 'maps';
-    const fileType = 'json';
-
-    it('app, file and total count should increment by 1', async () => {
-      const newCounts: any = incrementCounts({ app, fileType, ...oldCounts });
-      expect(newCounts.filesUploadedTotalCount).toEqual(4);
-      expect(newCounts.filesUploadedTypesTotalCounts[fileType]).toEqual(2);
-      expect(newCounts.filesUploadedByApp[app][fileType]).toEqual(2);
-    });
-
-    it('total count should equal sum of all file type counts', async () => {
-      const newCounts = incrementCounts({ app, fileType, ...oldCounts });
-      const fileTypeCounts =
-        newCounts.filesUploadedTypesTotalCounts.json + newCounts.filesUploadedTypesTotalCounts.csv;
-      expect(newCounts.filesUploadedTotalCount).toEqual(fileTypeCounts);
-    });
-
-    it('total count should equal sum of all app counts', async () => {
-      const newCounts: any = incrementCounts({ app, fileType, ...oldCounts });
-      const fileAppCounts =
-        newCounts.filesUploadedByApp.maps.json +
-        newCounts.filesUploadedByApp.maps.csv +
-        newCounts.filesUploadedByApp.ml.csv;
-      expect(newCounts.filesUploadedTotalCount).toEqual(fileAppCounts);
     });
   });
 });
