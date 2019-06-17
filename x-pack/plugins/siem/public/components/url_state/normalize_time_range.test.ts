e054b3bac04c4206ee2f1b076bd9a4ef7ffca451
FrankHassanabad <frank.hassanabad@elastic.co>
2019-06-17 09:58:00 -0600

    Added more tests for relative date time

--- x-pack/plugins/siem/public/components/url_state/normalize_time_range.test.ts
+++ x-pack/plugins/siem/public/components/url_state/normalize_time_range.test.ts
@@ -5,10 +5,16 @@
  */
 
 import { normalizeTimeRange } from './normalize_time_range';
-import { URLTimeRange, AbsoluteTimeRange, isAbsoluteTimeRange } from '../../store/inputs/model';
+import {
+  URLTimeRange,
+  AbsoluteTimeRange,
+  isAbsoluteTimeRange,
+  RelativeTimeRange,
+  isRelativeTimeRange,
+} from '../../store/inputs/model';
 
 describe('#normalizeTimeRange', () => {
-  test('returns empty strings as 0', () => {
+  test('Absolute time range returns empty strings as 0', () => {
     const dateTimeRange: URLTimeRange = {
       kind: 'absolute',
       fromStr: undefined,
@@ -30,7 +36,7 @@ describe('#normalizeTimeRange', () => {
     }
   });
 
-  test('returns string with empty spaces as 0', () => {
+  test('Absolute time range returns string with empty spaces as 0', () => {
     const dateTimeRange: URLTimeRange = {
       kind: 'absolute',
       fromStr: undefined,
@@ -52,7 +58,7 @@ describe('#normalizeTimeRange', () => {
     }
   });
 
-  test('returns string date time as valid date with from and to as ISO strings', () => {
+  test('Absolute time range returns string date time as valid date with from and to as ISO strings', () => {
     const to = new Date('2019-04-28T23:05:28.405Z');
     const from = new Date('2019-05-28T23:05:28.405Z');
     const dateTimeRange: URLTimeRange = {
@@ -76,7 +82,7 @@ describe('#normalizeTimeRange', () => {
     }
   });
 
-  test('returns number as valid date with from and to as Epoch', () => {
+  test('Absolute time range returns number as valid date with from and to as Epoch', () => {
     const to = new Date('2019-04-28T23:05:28.405Z');
     const from = new Date('2019-05-28T23:05:28.405Z');
     const dateTimeRange: URLTimeRange = {
@@ -100,7 +106,7 @@ describe('#normalizeTimeRange', () => {
     }
   });
 
-  test('returns number as valid date with from and to as Epoch when the Epoch is a string', () => {
+  test('Absolute time range returns number as valid date with from and to as Epoch when the Epoch is a string', () => {
     const to = new Date('2019-04-28T23:05:28.405Z');
     const from = new Date('2019-05-28T23:05:28.405Z');
     const dateTimeRange: URLTimeRange = {
@@ -124,7 +130,7 @@ describe('#normalizeTimeRange', () => {
     }
   });
 
-  test('returns NaN with from and to when garbage is sent in', () => {
+  test('Absolute time range returns NaN with from and to when garbage is sent in', () => {
     const to = 'garbage';
     const from = 'garbage';
     const dateTimeRange: URLTimeRange = {
@@ -145,6 +151,146 @@ describe('#normalizeTimeRange', () => {
       expect(normalizeTimeRange<AbsoluteTimeRange>(dateTimeRange)).toEqual(expected);
     } else {
       throw new Error('Was expecting date time range to be a AbsoluteTimeRange');
+    }
+  });
+
+  test('Relative time range returns empty strings as 0', () => {
+    const dateTimeRange: URLTimeRange = {
+      kind: 'relative',
+      fromStr: '',
+      toStr: '',
+      from: '',
+      to: '',
+    };
+    if (isRelativeTimeRange(dateTimeRange)) {
+      const expected: RelativeTimeRange = {
+        kind: 'relative',
+        from: 0,
+        to: 0,
+        fromStr: '',
+        toStr: '',
+      };
+      expect(normalizeTimeRange<RelativeTimeRange>(dateTimeRange)).toEqual(expected);
+    } else {
+      throw new Error('Was expecting date time range to be a RelativeTimeRange');
+    }
+  });
+
+  test('Relative time range returns string with empty spaces as 0', () => {
+    const dateTimeRange: URLTimeRange = {
+      kind: 'relative',
+      fromStr: '',
+      toStr: '',
+      from: '  ',
+      to: '   ',
+    };
+    if (isRelativeTimeRange(dateTimeRange)) {
+      const expected: RelativeTimeRange = {
+        kind: 'relative',
+        from: 0,
+        to: 0,
+        fromStr: '',
+        toStr: '',
+      };
+      expect(normalizeTimeRange<RelativeTimeRange>(dateTimeRange)).toEqual(expected);
+    } else {
+      throw new Error('Was expecting date time range to be a RelativeTimeRange');
+    }
+  });
+
+  test('Relative time range returns string date time as valid date with from and to as ISO strings', () => {
+    const to = new Date('2019-04-28T23:05:28.405Z');
+    const from = new Date('2019-05-28T23:05:28.405Z');
+    const dateTimeRange: URLTimeRange = {
+      kind: 'relative',
+      fromStr: '',
+      toStr: '',
+      from: from.toISOString(),
+      to: to.toISOString(),
+    };
+    if (isRelativeTimeRange(dateTimeRange)) {
+      const expected: RelativeTimeRange = {
+        kind: 'relative',
+        from: from.valueOf(),
+        to: to.valueOf(),
+        fromStr: '',
+        toStr: '',
+      };
+      expect(normalizeTimeRange<RelativeTimeRange>(dateTimeRange)).toEqual(expected);
+    } else {
+      throw new Error('Was expecting date time range to be a AbsoluteTimeRange');
+    }
+  });
+
+  test('Relative time range returns number as valid date with from and to as Epoch', () => {
+    const to = new Date('2019-04-28T23:05:28.405Z');
+    const from = new Date('2019-05-28T23:05:28.405Z');
+    const dateTimeRange: URLTimeRange = {
+      kind: 'relative',
+      fromStr: '',
+      toStr: '',
+      from: from.valueOf(),
+      to: to.valueOf(),
+    };
+    if (isRelativeTimeRange(dateTimeRange)) {
+      const expected: RelativeTimeRange = {
+        kind: 'relative',
+        from: from.valueOf(),
+        to: to.valueOf(),
+        fromStr: '',
+        toStr: '',
+      };
+      expect(normalizeTimeRange<RelativeTimeRange>(dateTimeRange)).toEqual(expected);
+    } else {
+      throw new Error('Was expecting date time range to be a RelativeTimeRange');
+    }
+  });
+
+  test('Relative time range returns number as valid date with from and to as Epoch when the Epoch is a string', () => {
+    const to = new Date('2019-04-28T23:05:28.405Z');
+    const from = new Date('2019-05-28T23:05:28.405Z');
+    const dateTimeRange: URLTimeRange = {
+      kind: 'relative',
+      fromStr: '',
+      toStr: '',
+      from: `${from.valueOf()}`,
+      to: `${to.valueOf()}`,
+    };
+    if (isRelativeTimeRange(dateTimeRange)) {
+      const expected: RelativeTimeRange = {
+        kind: 'relative',
+        from: from.valueOf(),
+        to: to.valueOf(),
+        fromStr: '',
+        toStr: '',
+      };
+      expect(normalizeTimeRange<RelativeTimeRange>(dateTimeRange)).toEqual(expected);
+    } else {
+      throw new Error('Was expecting date time range to be a RelativeTimeRange');
+    }
+  });
+
+  test('Relative time range returns NaN with from and to when garbage is sent in', () => {
+    const to = 'garbage';
+    const from = 'garbage';
+    const dateTimeRange: URLTimeRange = {
+      kind: 'relative',
+      fromStr: '',
+      toStr: '',
+      from,
+      to,
+    };
+    if (isRelativeTimeRange(dateTimeRange)) {
+      const expected: RelativeTimeRange = {
+        kind: 'relative',
+        from: NaN,
+        to: NaN,
+        fromStr: '',
+        toStr: '',
+      };
+      expect(normalizeTimeRange<RelativeTimeRange>(dateTimeRange)).toEqual(expected);
+    } else {
+      throw new Error('Was expecting date time range to be a RelativeTimeRange');
     }
   });
 });
