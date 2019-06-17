64ae091915f44b4578570fb416c806b6f5136a0c
Alison Goryachev <alisonmllr20@gmail.com>
2019-06-17 15:00:27 -0400

    check permissions before loading license management actions

--- x-pack/plugins/license_management/common/constants/permissions.js
+++ x-pack/plugins/license_management/common/constants/permissions.js
@@ -4,7 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { PLUGIN } from './plugin';
-export { BASE_PATH } from './base_path';
-export { EXTERNAL_LINKS } from './external_links';
-export { APP_PERMISSION } from './permissions';
+export const APP_PERMISSION = 'cluster:manage';
