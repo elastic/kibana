9e5b686785ecb41d9c97413e5797300a5d2fbf90
streamich <streamich@gmail.com>
2019-06-18 12:52:34 +0200

    refactor: 💡 use new Map() in Embeddables

--- test/plugin_functional/plugins/kbn_tp_embeddable_explorer/public/app/app.tsx
+++ test/plugin_functional/plugins/kbn_tp_embeddable_explorer/public/app/app.tsx
@@ -19,15 +19,12 @@
 import { I18nContext } from 'ui/i18n';
 import { EuiTab } from '@elastic/eui';
 import React, { Component } from 'react';
-import {
-  IRegistry,
-  EmbeddableFactory,
-} from '../../../../../../src/legacy/core_plugins/embeddable_api/public';
+import { EmbeddableFactory } from '../../../../../../src/legacy/core_plugins/embeddable_api/public';
 import { ContactCardEmbeddableExample } from './hello_world_embeddable_example';
 import { HelloWorldContainerExample } from './hello_world_container_example';
 
 export interface AppProps {
-  embeddableFactories: IRegistry<EmbeddableFactory>;
+  embeddableFactories: Map<string, EmbeddableFactory>;
 }
 
 export class App extends Component<AppProps, { selectedTabId: string }> {
