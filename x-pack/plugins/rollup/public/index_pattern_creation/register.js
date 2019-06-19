071a52d4d4c83dc367c4376acfa8ed03b4d2c35b
Vadim Dalecky <streamich@users.noreply.github.com>
2019-06-19 22:21:15 +0200

    No common registry implementation (#39168)
    * refactor: 💡 use new Map() in Embeddables
    
    * refactor: 💡 remove IndexPatternCreationConfigRegistry
    
    * refactor: 💡 make indexPatternTypes registry semantic
    
    * fix: 🐛 fix TypeScript errors

--- x-pack/plugins/rollup/public/index_pattern_creation/register.js
+++ x-pack/plugins/rollup/public/index_pattern_creation/register.js
@@ -4,9 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { IndexPatternCreationConfigRegistry } from 'ui/management/index_pattern_creation';
+import { addIndexPatternType } from 'ui/management/index_pattern_creation';
 import { RollupIndexPatternCreationConfig } from './rollup_index_pattern_creation_config';
 
 export function initIndexPatternCreation() {
-  IndexPatternCreationConfigRegistry.register(() => RollupIndexPatternCreationConfig);
+  addIndexPatternType(RollupIndexPatternCreationConfig);
 }
