1c45809a50299637a189b0c72a963b16bd62b1b3
spalger <email@spalger.com>
2019-06-20 08:45:47 -0700

    simply test against ES 6.8.1, not a specific snapshot

--- src/dev/ci_setup/setup.sh
+++ src/dev/ci_setup/setup.sh
@@ -5,7 +5,7 @@ set -e
 ###
 ### keep until 6.8.2 snapshots are available
 ###
-export TEST_ES_BRANCH=6.8.1-58f9e974
+export TEST_ES_BRANCH=6.8.1
 
 dir="$(pwd)"
 cacheDir="${CACHE_DIR:-"$HOME/.kibana"}"
