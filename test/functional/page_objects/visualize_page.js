b1f1c96557880722e71ef3697294b85512caca0d
sulemanof <sul-dan@mail.ru>
2019-06-20 12:50:36 +0300

    EUIficate schema editors

--- test/functional/page_objects/visualize_page.js
+++ test/functional/page_objects/visualize_page.js
@@ -1012,10 +1012,6 @@ export function VisualizePageProvider({ getService, getPageObjects, updateBaseli
       return await markdown.getVisibleText();
     }
 
-    async clickColumns() {
-      await find.clickByCssSelector('div.schemaEditors > div > div > button:nth-child(2)');
-    }
-
     async getVisualizationRenderingCount() {
       const visualizationLoader = await testSubjects.find('visualizationLoader');
       const renderingCount = await visualizationLoader.getAttribute('data-rendering-count');
@@ -1245,9 +1241,7 @@ export function VisualizePageProvider({ getService, getPageObjects, updateBaseli
     }
 
     async clickSplitDirection(direction) {
-      const activeParamPanel = await find.byCssSelector('vis-editor-agg-params[aria-hidden="false"]');
-      const button = await testSubjects.findDescendant(`splitBy-${direction}`, activeParamPanel);
-      await button.click();
+      await testSubjects.clickByCssSelector(`#visEditorSplitBy__${direction}`);
     }
 
     async countNestedTables() {
