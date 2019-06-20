6470e4f207f16dee4407f69fac595dd8332043b3
Kerry Gallagher <k.gallagher.05@gmail.com>
2019-06-20 17:03:58 +0100

    Amend network traffic calculations to account for interfaces
    - Adds a network_traffic metric aggregation creator
    - Amends rx and tx to use this
    - Uses regular rate calculation for Kubernetes Pods

--- x-pack/legacy/plugins/infra/server/lib/snapshot/metric_aggregation_creators/rx.ts
+++ x-pack/legacy/plugins/infra/server/lib/snapshot/metric_aggregation_creators/rx.ts
@@ -5,12 +5,18 @@
  */
 
 import { InfraNodeType } from '../../../graphql/types';
-import { rate } from './rate';
+import { networkTraffic } from './network_traffic';
 
-const FIELDS = {
+const METRIC_FIELDS = {
   [InfraNodeType.host]: 'system.network.in.bytes',
   [InfraNodeType.pod]: 'kubernetes.pod.network.rx.bytes',
   [InfraNodeType.container]: 'docker.network.in.bytes',
 };
 
-export const rx = rate('rx', FIELDS);
+const INTERFACE_FIELDS = {
+  [InfraNodeType.host]: 'system.network.name',
+  [InfraNodeType.pod]: null,
+  [InfraNodeType.container]: 'docker.network.interface',
+};
+
+export const rx = networkTraffic('rx', METRIC_FIELDS, INTERFACE_FIELDS);
