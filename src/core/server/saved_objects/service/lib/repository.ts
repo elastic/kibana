0530155eb939daa7f47ab1c75344a0b69c28321d
Larry Gregory <larry.gregory@elastic.co>
2019-06-20 11:10:27 -0400

    assert that the repository cannot accept Symbol namespaces

--- src/core/server/saved_objects/service/lib/repository.ts
+++ src/core/server/saved_objects/service/lib/repository.ts
@@ -421,6 +421,10 @@ export class SavedObjectsRepository {
       throw new TypeError('options.fields must be an array');
     }
 
+    if (typeof namespace === 'symbol') {
+      throw new TypeError('options.namespace must not be a Symbol');
+    }
+
     const esOptions = {
       index: this.getIndicesForTypes(allowedTypes),
       size: perPage,
