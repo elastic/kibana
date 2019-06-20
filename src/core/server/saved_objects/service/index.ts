cb0d974b35673420f13755ad8c92a378e94e3315
Larry Gregory <larry.gregory@elastic.co>
2019-06-20 08:59:57 -0400

    fix build

--- src/core/server/saved_objects/service/index.ts
+++ src/core/server/saved_objects/service/index.ts
@@ -35,6 +35,7 @@ export interface SavedObjectsService<Request = any> {
 }
 
 export {
+  SavedObjectsRepository,
   ScopedSavedObjectsClientProvider,
   SavedObjectsClientWrapperFactory,
   SavedObjectsClientWrapperOptions,
