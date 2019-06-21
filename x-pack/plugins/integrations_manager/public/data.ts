4578f7f7f03bb1e6c45ec30a46ffeb0c595aa2d7
John Schulz <john.schulz@elastic.co>
2019-06-21 18:56:07 -0400

    Drop axios in favor of np.core.http. User can supply http client. (#39471)
    Might need to add back `headers` and `basePath` but this works for now.

--- x-pack/plugins/integrations_manager/public/data.ts
+++ x-pack/plugins/integrations_manager/public/data.ts
@@ -4,30 +4,27 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import axios from 'axios';
-import chrome from 'ui/chrome';
+import { npStart } from 'ui/new_platform';
+import { HttpHandler } from 'src/core/public/http';
 import { IntegrationInfo, IntegrationList } from '../common/types';
 import { API_INTEGRATIONS_INFO, API_INTEGRATIONS_LIST } from '../common/constants';
 
-export const fetch = axios.create({
-  withCredentials: true,
-  baseURL: chrome.getBasePath(),
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-    'kbn-xsrf': chrome.getXsrfToken(),
-  },
-  timeout: 30 * 1000,
-});
+let _fetch: HttpHandler = npStart.core.http.fetch;
+
+export function setClient(client: HttpHandler): void {
+  _fetch = client;
+}
 
 export async function getIntegrationsList(): Promise<IntegrationList> {
-  const response = await fetch(API_INTEGRATIONS_LIST);
-  const list: IntegrationList = response.data;
+  const path = API_INTEGRATIONS_LIST;
+  const list: IntegrationList = await _fetch(path);
+
   return list;
 }
 
 export async function getIntegrationInfoByKey(pkgkey: string): Promise<IntegrationInfo> {
-  const response = await fetch(API_INTEGRATIONS_INFO.replace('{pkgkey}', pkgkey));
-  const info: IntegrationInfo = response.data;
+  const path = API_INTEGRATIONS_INFO.replace('{pkgkey}', pkgkey);
+  const info: IntegrationInfo = await _fetch(path);
+
   return info;
 }
