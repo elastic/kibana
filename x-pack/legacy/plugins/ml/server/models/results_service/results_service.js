72ce3f61a432604980fb10d0f944e28ca50d3667
FrankHassanabad <frank.hassanabad@elastic.co>
2019-06-21 19:00:06 -0600

    Put code back that was accidently changed in another project

--- x-pack/legacy/plugins/ml/server/models/results_service/results_service.js
+++ x-pack/legacy/plugins/ml/server/models/results_service/results_service.js
@@ -77,6 +77,7 @@ export function resultsServiceProvider(callWithRequest) {
         }
       });
     }
+
     // Add in term queries for each of the specified criteria.
     criteriaFields.forEach((criteria) => {
       boolCriteria.push({
