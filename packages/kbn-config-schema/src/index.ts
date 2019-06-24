99c36d8fa6693ea3d85e2c23d4280f11ae54edb4
Mikhail Shustov <restrry@gmail.com>
2019-06-24 16:22:36 +0200

    support unknown keys for object type in @kbn/schema-config (#39448)
    * support unknown keys for object type in @kbn/schema-config
    
    * add test for children objects
    
    * update snapshot test
    
    * allowUnknowns as TypeOptions

--- packages/kbn-config-schema/src/index.ts
+++ packages/kbn-config-schema/src/index.ts
@@ -39,6 +39,7 @@ import {
   NumberOptions,
   NumberType,
   ObjectType,
+  ObjectTypeOptions,
   Props,
   RecordOfOptions,
   RecordOfType,
@@ -94,10 +95,7 @@ function maybe<V>(type: Type<V>): Type<V | undefined> {
   return new MaybeType(type);
 }
 
-function object<P extends Props>(
-  props: P,
-  options?: TypeOptions<{ [K in keyof P]: TypeOf<P[K]> }>
-): ObjectType<P> {
+function object<P extends Props>(props: P, options?: ObjectTypeOptions<P>): ObjectType<P> {
   return new ObjectType(props, options);
 }
 
