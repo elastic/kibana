18ebd85effca98b088bbc095f4d5e5c06bed0ea3
Catherine Liu <catherine.liu@elastic.co>
2019-06-25 12:31:48 -0700

    Fixed import

--- x-pack/legacy/plugins/canvas/canvas_plugin_src/functions/common/joinRows.ts
+++ x-pack/legacy/plugins/canvas/canvas_plugin_src/functions/common/joinRows.ts
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { ExpressionFunction } from '../../../../../../src/legacy/core_plugins/interpreter/types';
+import { ExpressionFunction } from 'src/legacy/core_plugins/interpreter/public';
 import { Datatable } from '../types';
 import { getFunctionHelp, getFunctionErrors } from '../../strings';
 
