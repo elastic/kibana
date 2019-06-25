3ede447bd93c4cba850e5d9d66ef5ac663c04224
James Gowdy <jgowdy@elastic.co>
2019-06-25 17:02:36 +0100

    removing unused include

--- x-pack/legacy/plugins/ml/server/lib/check_privileges/privileges.ts
+++ x-pack/legacy/plugins/ml/server/lib/check_privileges/privileges.ts
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Privileges } from '../../../common/types/privileges';
-
 export const mlPrivileges = {
   cluster: [
     'cluster:monitor/data_frame/get',
