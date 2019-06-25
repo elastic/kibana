45b1cc7ad2f09056565bb5d574228b510b1b695b
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-06-25 13:14:13 -0700

    removes commented out code, undoes comma deletion in kibana/index.js

--- src/legacy/core_plugins/kibana/index.js
+++ src/legacy/core_plugins/kibana/index.js
@@ -67,7 +67,6 @@ export default function (kibana) {
         'plugins/kibana/visualize/saved_visualizations/saved_visualization_register',
         'plugins/kibana/discover/saved_searches/saved_search_register',
         'plugins/kibana/dashboard/saved_dashboard/saved_dashboard_register',
-        // 'plugins/data/saved_queriesfilters/saved_queriesfilters_register'
       ],
       app: {
         id: 'kibana',
@@ -223,7 +222,7 @@ export default function (kibana) {
           getTitle(obj) {
             return `Advanced Settings [${obj.id}]`;
           },
-        }
+        },
       },
 
       savedObjectSchemas: {
