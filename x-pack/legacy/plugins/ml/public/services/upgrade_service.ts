13c9cdddeb4913cede29bc5061696827b4c077a1
James Gowdy <jgowdy@elastic.co>
2019-06-26 17:33:51 +0100

    [ML] Moving privileges checks to server (#39585)
    * [ML] Moving privileges checks to server
    
    * removing unused include
    
    * adding tests
    
    * adding general kibana type
    
    * adding coments and extra test
    
    * fixing type issue

--- x-pack/legacy/plugins/ml/public/services/upgrade_service.ts
+++ x-pack/legacy/plugins/ml/public/services/upgrade_service.ts
@@ -4,5 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export type Cluster = Record<string, boolean>;
-export type Privileges = Record<string, boolean>;
+let upgradeInProgress: boolean = false;
+
+export function setUpgradeInProgress(show: boolean) {
+  upgradeInProgress = show;
+}
+
+export function isUpgradeInProgress(): boolean {
+  return upgradeInProgress;
+}
