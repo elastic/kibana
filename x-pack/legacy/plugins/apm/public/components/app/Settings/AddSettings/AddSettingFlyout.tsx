c97a81b836d8332e586a25bd7bb15235916456a1
Casper Hübertz <casper@formgeist.com>
2019-06-26 22:06:09 +0200

    Copy edits on ListSettings page

--- x-pack/legacy/plugins/apm/public/components/app/Settings/AddSettings/AddSettingFlyout.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/Settings/AddSettings/AddSettingFlyout.tsx
@@ -9,7 +9,14 @@ import {
   EuiFlyoutBody,
   EuiFlyoutHeader,
   EuiPortal,
-  EuiTitle
+  EuiTitle,
+  EuiBetaBadge,
+  EuiText,
+  EuiHorizontalRule,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFlyoutFooter,
+  EuiButton
 } from '@elastic/eui';
 import React from 'react';
 import { AddSettingFlyoutBody } from './AddSettingFlyoutBody';
@@ -28,13 +35,42 @@ export function AddSettingsFlyout({ onClose, isOpen, onSubmit }: Props) {
     <EuiPortal>
       <EuiFlyout size="s" onClose={onClose} ownFocus={true}>
         <EuiFlyoutHeader hasBorder>
-          <EuiTitle>
-            <h2>Agent configuration</h2>
-          </EuiTitle>
+          <EuiFlexGroup alignItems="center">
+            <EuiFlexItem grow={false}>
+              <EuiTitle>
+                <h2>Create configuration</h2>
+              </EuiTitle>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiBetaBadge
+                label="Beta"
+                tooltipContent="This feature is not GA. Please help us by reporting any bugs."
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </EuiFlyoutHeader>
         <EuiFlyoutBody>
+          <EuiText>
+            <p>
+              Please note only sample rate configuration is supported in this
+              first version. We will extend support for central configuration in
+              future releases. As this feature is in beta, please beware of
+              bugs.
+            </p>
+          </EuiText>
+          <EuiHorizontalRule margin="m" />
           <AddSettingFlyoutBody onSubmit={onSubmit} />
         </EuiFlyoutBody>
+        {/* TODO: Add save submit function from AddSettingFlyoutBody */}
+        <EuiFlyoutFooter>
+          <EuiFlexGroup justifyContent="flexEnd">
+            <EuiFlexItem grow={false}>
+              <EuiButton fill isDisabled>
+                Save configuration
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlyoutFooter>
       </EuiFlyout>
     </EuiPortal>
   );
