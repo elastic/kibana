99768c3b65a10aaf816d079247bd7a07adcd2c9c
Casper Hübertz <casper@formgeist.com>
2019-06-27 14:52:59 +0200

    Adding main navigation bar for Home

--- x-pack/legacy/plugins/apm/public/components/app/Main/Home.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/Main/Home.tsx
@@ -8,9 +8,14 @@ import {
   EuiFlexGroup,
   EuiFlexItem,
   EuiTitle,
-  EuiButtonEmpty
+  EuiButtonEmpty,
+  EuiTab,
+  EuiTabs,
+  EuiSpacer
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
+import styled from 'styled-components';
+import theme from '@elastic/eui/dist/eui_theme_light.json';
 import React from 'react';
 import { ApmHeader } from '../../shared/ApmHeader';
 import { HistoryTabs, IHistoryTab } from '../../shared/HistoryTabs';
@@ -40,17 +45,48 @@ const SETTINGS_LINK_LABEL = i18n.translate('xpack.apm.settingsLinkLabel', {
   defaultMessage: 'Settings'
 });
 
+// TODO: I'm sure there's a nicer way to do this
+
+const MainNavigationBar = styled.div`
+  display: block;
+  margin-top: -${theme.euiSizeL};
+  margin-left: -${theme.euiSizeL};
+  height: 64px;
+  padding: 0 ${theme.euiSizeL};
+  background: ${theme.euiColorEmptyShade};
+  border-bottom: 1px solid ${theme.euiColorLightShade};
+`;
+
 export function Home() {
   return (
     <div>
+      <MainNavigationBar>
+        <EuiFlexGroup justifyContent="spaceBetween" alignItems="center">
+          <EuiFlexItem grow={false}>
+            {/* TODO: Tabs Nneeds to be hooked up with the appropiate routes */}
+            <EuiTabs display="condensed">
+              <EuiTab isSelected>Services</EuiTab>
+              <EuiTab>Traces</EuiTab>
+              <EuiTab>Settings</EuiTab>
+            </EuiTabs>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButtonEmpty size="s">Setup instructions</EuiButtonEmpty>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </MainNavigationBar>
+      <EuiSpacer />
       <ApmHeader>
         <EuiFlexGroup alignItems="center">
           <EuiFlexItem grow={false}>
             <EuiTitle size="l">
-              <h1>APM</h1>
+              {/* TODO: Needs to change depending on the selected route (Service or Traces). Settings has its own page header. */}
+              <h1>Services</h1>
             </EuiTitle>
           </EuiFlexItem>
-          <EuiFlexItem grow={false}>
+          {/* TODO: Removing the below links as they're replaced by the above */}
+
+          {/* <EuiFlexItem grow={false}>
             <APMLink path="/settings">
               <EuiButtonEmpty size="s" color="primary" iconType="gear">
                 {SETTINGS_LINK_LABEL}
@@ -59,9 +95,10 @@ export function Home() {
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
             <SetupInstructionsLink />
-          </EuiFlexItem>
+          </EuiFlexItem> */}
         </EuiFlexGroup>
       </ApmHeader>
+      {/* TODO: Replace the Services and Traces tabs with the main navigation bar above */}
       <HistoryTabs tabs={homeTabs} />
     </div>
   );
