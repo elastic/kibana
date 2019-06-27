2ef6484998e51ba3b0e93461a79823af5e6f230b
James Gowdy <jgowdy@elastic.co>
2019-06-27 15:27:51 +0100

    adding new tests

--- x-pack/legacy/plugins/ml/server/lib/check_privileges/check_privileges.test.ts
+++ x-pack/legacy/plugins/ml/server/lib/check_privileges/check_privileges.test.ts
@@ -46,6 +46,44 @@ const xpackMainPluginWithOutSecurity = {
   },
 };
 
+const xpackMainPluginWithOutSecurityBasicLicense = {
+  info: {
+    isAvailable: () => true,
+    feature: (f: string) => {
+      switch (f) {
+        case 'ml':
+          return { isEnabled: () => true };
+        case 'security':
+          return { isEnabled: () => false };
+      }
+    },
+    license: {
+      isOneOf: () => false,
+      isActive: () => true,
+      getType: () => 'basic',
+    },
+  },
+};
+
+const xpackMainPluginWithSecurityBasicLicense = {
+  info: {
+    isAvailable: () => true,
+    feature: (f: string) => {
+      switch (f) {
+        case 'ml':
+          return { isEnabled: () => true };
+        case 'security':
+          return { isEnabled: () => true };
+      }
+    },
+    license: {
+      isOneOf: () => false,
+      isActive: () => true,
+      getType: () => 'basic',
+    },
+  },
+};
+
 const mlIsEnabled = async () => true;
 // const mlIsntEnabled = async () => false;
 
@@ -211,6 +249,76 @@ describe('check_privileges', () => {
       expect(privileges.canStartStopDataFrameJob).toBe(false);
       done();
     });
+
+    test('ml_user privileges with security with basic license', async done => {
+      const callWithRequest = callWithRequestProvider('partialPrivileges');
+      const { getPrivileges } = privilegesProvider(
+        callWithRequest,
+        xpackMainPluginWithSecurityBasicLicense,
+        mlIsEnabled
+      );
+      const { privileges, upgradeInProgress } = await getPrivileges();
+      expect(upgradeInProgress).toBe(false);
+      expect(privileges.canGetJobs).toBe(false);
+      expect(privileges.canCreateJob).toBe(false);
+      expect(privileges.canDeleteJob).toBe(false);
+      expect(privileges.canOpenJob).toBe(false);
+      expect(privileges.canCloseJob).toBe(false);
+      expect(privileges.canForecastJob).toBe(false);
+      expect(privileges.canGetDatafeeds).toBe(false);
+      expect(privileges.canStartStopDatafeed).toBe(false);
+      expect(privileges.canUpdateJob).toBe(false);
+      expect(privileges.canUpdateDatafeed).toBe(false);
+      expect(privileges.canPreviewDatafeed).toBe(false);
+      expect(privileges.canGetCalendars).toBe(false);
+      expect(privileges.canCreateCalendar).toBe(false);
+      expect(privileges.canDeleteCalendar).toBe(false);
+      expect(privileges.canGetFilters).toBe(false);
+      expect(privileges.canCreateFilter).toBe(false);
+      expect(privileges.canDeleteFilter).toBe(false);
+      expect(privileges.canFindFileStructure).toBe(true);
+      expect(privileges.canGetDataFrameJobs).toBe(false);
+      expect(privileges.canDeleteDataFrameJob).toBe(false);
+      expect(privileges.canPreviewDataFrameJob).toBe(false);
+      expect(privileges.canCreateDataFrameJob).toBe(false);
+      expect(privileges.canStartStopDataFrameJob).toBe(false);
+      done();
+    });
+
+    test('full user with security with basic license', async done => {
+      const callWithRequest = callWithRequestProvider('fullPrivileges');
+      const { getPrivileges } = privilegesProvider(
+        callWithRequest,
+        xpackMainPluginWithSecurityBasicLicense,
+        mlIsEnabled
+      );
+      const { privileges, upgradeInProgress } = await getPrivileges();
+      expect(upgradeInProgress).toBe(false);
+      expect(privileges.canGetJobs).toBe(false);
+      expect(privileges.canCreateJob).toBe(false);
+      expect(privileges.canDeleteJob).toBe(false);
+      expect(privileges.canOpenJob).toBe(false);
+      expect(privileges.canCloseJob).toBe(false);
+      expect(privileges.canForecastJob).toBe(false);
+      expect(privileges.canGetDatafeeds).toBe(false);
+      expect(privileges.canStartStopDatafeed).toBe(false);
+      expect(privileges.canUpdateJob).toBe(false);
+      expect(privileges.canUpdateDatafeed).toBe(false);
+      expect(privileges.canPreviewDatafeed).toBe(false);
+      expect(privileges.canGetCalendars).toBe(false);
+      expect(privileges.canCreateCalendar).toBe(false);
+      expect(privileges.canDeleteCalendar).toBe(false);
+      expect(privileges.canGetFilters).toBe(false);
+      expect(privileges.canCreateFilter).toBe(false);
+      expect(privileges.canDeleteFilter).toBe(false);
+      expect(privileges.canFindFileStructure).toBe(true);
+      expect(privileges.canGetDataFrameJobs).toBe(true);
+      expect(privileges.canDeleteDataFrameJob).toBe(true);
+      expect(privileges.canPreviewDataFrameJob).toBe(true);
+      expect(privileges.canCreateDataFrameJob).toBe(true);
+      expect(privileges.canStartStopDataFrameJob).toBe(true);
+      done();
+    });
   });
 
   describe('getPrivileges() without security', () => {
@@ -316,6 +424,76 @@ describe('check_privileges', () => {
       expect(privileges.canPreviewDataFrameJob).toBe(false);
       expect(privileges.canCreateDataFrameJob).toBe(false);
       expect(privileges.canStartStopDataFrameJob).toBe(false);
+      done();
+    });
+
+    test('ml_user privileges without security with basic license', async done => {
+      const callWithRequest = callWithRequestProvider('partialPrivileges');
+      const { getPrivileges } = privilegesProvider(
+        callWithRequest,
+        xpackMainPluginWithOutSecurityBasicLicense,
+        mlIsEnabled
+      );
+      const { privileges, upgradeInProgress } = await getPrivileges();
+      expect(upgradeInProgress).toBe(false);
+      expect(privileges.canGetJobs).toBe(false);
+      expect(privileges.canCreateJob).toBe(false);
+      expect(privileges.canDeleteJob).toBe(false);
+      expect(privileges.canOpenJob).toBe(false);
+      expect(privileges.canCloseJob).toBe(false);
+      expect(privileges.canForecastJob).toBe(false);
+      expect(privileges.canGetDatafeeds).toBe(false);
+      expect(privileges.canStartStopDatafeed).toBe(false);
+      expect(privileges.canUpdateJob).toBe(false);
+      expect(privileges.canUpdateDatafeed).toBe(false);
+      expect(privileges.canPreviewDatafeed).toBe(false);
+      expect(privileges.canGetCalendars).toBe(false);
+      expect(privileges.canCreateCalendar).toBe(false);
+      expect(privileges.canDeleteCalendar).toBe(false);
+      expect(privileges.canGetFilters).toBe(false);
+      expect(privileges.canCreateFilter).toBe(false);
+      expect(privileges.canDeleteFilter).toBe(false);
+      expect(privileges.canFindFileStructure).toBe(true);
+      expect(privileges.canGetDataFrameJobs).toBe(true);
+      expect(privileges.canDeleteDataFrameJob).toBe(true);
+      expect(privileges.canPreviewDataFrameJob).toBe(true);
+      expect(privileges.canCreateDataFrameJob).toBe(true);
+      expect(privileges.canStartStopDataFrameJob).toBe(true);
+      done();
+    });
+
+    test('full user without security with basic license', async done => {
+      const callWithRequest = callWithRequestProvider('fullPrivileges');
+      const { getPrivileges } = privilegesProvider(
+        callWithRequest,
+        xpackMainPluginWithOutSecurityBasicLicense,
+        mlIsEnabled
+      );
+      const { privileges, upgradeInProgress } = await getPrivileges();
+      expect(upgradeInProgress).toBe(false);
+      expect(privileges.canGetJobs).toBe(false);
+      expect(privileges.canCreateJob).toBe(false);
+      expect(privileges.canDeleteJob).toBe(false);
+      expect(privileges.canOpenJob).toBe(false);
+      expect(privileges.canCloseJob).toBe(false);
+      expect(privileges.canForecastJob).toBe(false);
+      expect(privileges.canGetDatafeeds).toBe(false);
+      expect(privileges.canStartStopDatafeed).toBe(false);
+      expect(privileges.canUpdateJob).toBe(false);
+      expect(privileges.canUpdateDatafeed).toBe(false);
+      expect(privileges.canPreviewDatafeed).toBe(false);
+      expect(privileges.canGetCalendars).toBe(false);
+      expect(privileges.canCreateCalendar).toBe(false);
+      expect(privileges.canDeleteCalendar).toBe(false);
+      expect(privileges.canGetFilters).toBe(false);
+      expect(privileges.canCreateFilter).toBe(false);
+      expect(privileges.canDeleteFilter).toBe(false);
+      expect(privileges.canFindFileStructure).toBe(true);
+      expect(privileges.canGetDataFrameJobs).toBe(true);
+      expect(privileges.canDeleteDataFrameJob).toBe(true);
+      expect(privileges.canPreviewDataFrameJob).toBe(true);
+      expect(privileges.canCreateDataFrameJob).toBe(true);
+      expect(privileges.canStartStopDataFrameJob).toBe(true);
       done();
     });
   });
