641295ef1e309c0db626cea6f0db4acf7763ab8e
John Dorlus <jsdorlus@elastic.co>
2019-06-27 11:58:31 -0400

    Moved assertion to test file and changed function to licenseText()

--- x-pack/test/functional/apps/license_management/home_page.ts
+++ x-pack/test/functional/apps/license_management/home_page.ts
@@ -4,20 +4,24 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import expect from '@kbn/expect';
 import { KibanaFunctionalTestDefaultProviders } from '../../../types/providers';
 
 // eslint-disable-next-line import/no-default-export
 export default ({ getPageObjects, getService }: KibanaFunctionalTestDefaultProviders) => {
   const pageObjects = getPageObjects(['common', 'licenseManagement']);
+  const log = getService('log');
 
-  describe('Home page', function() {
+  describe('Home page', function () {
     this.tags('smoke');
     before(async () => {
       await pageObjects.common.navigateToApp('licenseManagement');
     });
 
     it('Loads the app', async () => {
-      await pageObjects.licenseManagement.appHasLoaded();
+      await log.debug('Checking for license header.');
+      const licenseText = await pageObjects.licenseManagement.licenseText();
+      expect(licenseText).to.be('Your Trial license is active');
     });
   });
 };
--- x-pack/test/functional/page_objects/license_management_page.ts
+++ x-pack/test/functional/page_objects/license_management_page.ts
@@ -4,20 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import expect from '@kbn/expect';
+
 import { KibanaFunctionalTestDefaultProviders } from '../../types/providers';
 
 export const LicenseManagementPageProvider = ({
   getService,
 }: KibanaFunctionalTestDefaultProviders) => {
   const testSubjects = getService('testSubjects');
-  const log = getService('log');
 
   return {
-    async appHasLoaded() {
-      await log.debug('Checking for license header.');
-      const licenseText = await testSubjects.getVisibleText('licenseText');
-      expect(licenseText).to.be('Your Trial license is active');
+    async licenseText() {
+      return await testSubjects.getVisibleText('licenseText');
     },
   };
 };
