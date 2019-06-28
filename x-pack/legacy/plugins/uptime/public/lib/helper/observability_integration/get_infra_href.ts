437fa8695bbd5efddacde2fd8b8f4991130c954e
Justin Kambic <justin.kambic@elastic.co>
2019-06-28 00:07:05 -0400

    Port infra url generators to use monitor summaries.

--- x-pack/legacy/plugins/uptime/public/lib/helper/observability_integration/get_infra_href.ts
+++ x-pack/legacy/plugins/uptime/public/lib/helper/observability_integration/get_infra_href.ts
@@ -4,31 +4,65 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { LatestMonitor } from '../../../../common/graphql/types';
+import { MonitorSummary } from '../../../../common/graphql/types';
 import { addBasePath } from './add_base_path';
-import { buildHref } from './build_href';
+import { buildHref, buildHrefFromList } from './build_href';
 
 export const getInfraContainerHref = (
-  monitor: LatestMonitor,
+  summary: MonitorSummary,
   basePath: string
-): string | undefined =>
-  buildHref(monitor, 'ping.container.id', containerId =>
-    addBasePath(basePath, `/app/infra#/link-to/container-detail/${encodeURIComponent(containerId)}`)
-  );
-
-export const getInfraKubernetesHref = (
-  monitor: LatestMonitor,
-  basePath: string
-): string | undefined =>
-  buildHref(monitor, 'ping.kubernetes.pod.uid', uid =>
-    addBasePath(basePath, `/app/infra#/link-to/pod-detail/${encodeURIComponent(uid)}`)
-  );
-
-export const getInfraIpHref = (monitor: LatestMonitor, basePath: string) =>
-  buildHref(monitor, 'ping.monitor.ip', ip => {
-    const expression = encodeURIComponent(`host.ip : ${ip}`);
+): string | undefined => {
+  const getHref = (value: string | string[]) => {
+    if (!Array.isArray(value)) {
+      return addBasePath(
+        basePath,
+        `/app/infra#/link-to/container-detail/${encodeURIComponent(value)}`
+      );
+    }
     return addBasePath(
       basePath,
-      `/app/infra#/infrastructure/inventory?waffleFilter=(expression:'${expression}',kind:kuery)`
+      `/app/infra#/link-to/container-detail/${encodeURIComponent(value[0])}`
     );
-  });
+  };
+  return buildHrefFromList(summary.state.checks || [], 'container.id', getHref);
+};
+
+export const getInfraKubernetesHref = (
+  summary: MonitorSummary,
+  basePath: string
+): string | undefined => {
+  const getHref = (value: string | string[]) => {
+    if (!Array.isArray(value)) {
+      return addBasePath(basePath, `/app/infra#/link-to/pod-detail/${encodeURIComponent(value)}`);
+    }
+    // TODO: this link should be updated to "OR" additional pods
+    return addBasePath(basePath, `/app/infra#/link-to/pod-detail/${encodeURIComponent(value[0])}`);
+  };
+
+  return buildHrefFromList(summary.state.checks || [], 'kubernetes.pod.uid', getHref);
+};
+
+export const getInfraIpHref = (summary: MonitorSummary, basePath: string) => {
+  const getHref = (value: string | string[]) => {
+    if (!Array.isArray(value)) {
+      const expression = encodeURIComponent(`host.ip : ${value}`);
+      return addBasePath(
+        basePath,
+        `/app/infra#/infrastructure/inventory?waffleFilter=(expression:'${expression}',kind:kuery)`
+      );
+    }
+    const ips = value.reduce(
+      (str: string, cur: string) => (!str ? `host.ip : ${cur}` : str + ` or host.ip : ${cur}`),
+      ''
+    );
+    return ips === ''
+      ? undefined
+      : addBasePath(
+          basePath,
+          `/app/infra#/infrastructure/inventory?waffleFilter=(expression:'${encodeURIComponent(
+            ips
+          )}',kind:kuery)`
+        );
+  };
+  return buildHrefFromList(summary.state.checks || [], 'monitor.ip', getHref);
+};
