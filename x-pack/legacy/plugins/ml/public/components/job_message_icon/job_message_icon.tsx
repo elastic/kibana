ad64db00b6504e0851a66dafbbd183746c263015
Melissa Alvarez <melissa.alvarez@elastic.co>
2019-06-28 14:57:52 -0400

    [ML] Adds icon to data frame messages expanded row tab (#39817)
    * Move jobIcon to shared components directory
    
    * use JobIcon component in DF message tab
    
    * Convert JobIcon to Typescript
    
    * move message types to common directory
    
    * Clearer type names. Fixed icon width.
    
    * fetch last 20 messages by default

--- x-pack/legacy/plugins/ml/public/components/job_message_icon/job_message_icon.tsx
+++ x-pack/legacy/plugins/ml/public/components/job_message_icon/job_message_icon.tsx
@@ -4,18 +4,19 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import React, { FC } from 'react';
 
-import PropTypes from 'prop-types';
-import React from 'react';
+import { EuiIcon, EuiToolTip } from '@elastic/eui';
+import { AuditMessageBase } from '../../../common/types/audit_message';
 
-import {
-  EuiIcon,
-  EuiToolTip,
-} from '@elastic/eui';
+interface Props {
+  message: AuditMessageBase;
+  showTooltip: boolean;
+}
 
 const [INFO, WARNING, ERROR] = ['info', 'warning', 'error'];
 
-export function JobIcon({ message, showTooltip = false }) {
+export const JobIcon: FC<Props> = ({ message, showTooltip = false }) => {
   if (message !== undefined) {
     let color = 'primary';
     const icon = 'alert';
@@ -30,21 +31,14 @@ export function JobIcon({ message, showTooltip = false }) {
 
     if (showTooltip) {
       return (
-        <EuiToolTip
-          position="bottom"
-          content={message.text}
-        >
+        <EuiToolTip position="bottom" content={message.text}>
           <EuiIcon type={icon} color={color} />
         </EuiToolTip>
       );
     } else {
-      return (<EuiIcon type={icon} color={color} />);
+      return <EuiIcon type={icon} color={color} />;
     }
   } else {
-    return (<span />);
+    return <span />;
   }
-}
-JobIcon.propTypes = {
-  message: PropTypes.object,
-  showTooltip: PropTypes.bool,
 };
