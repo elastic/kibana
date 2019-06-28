9aedb123fc7bf306734c9b83d9222665e8ba9af7
Andrew Cholakian <andrew@andrewvc.com>
2019-06-28 08:56:44 -0500

    Update snap

--- x-pack/legacy/plugins/uptime/public/components/functional/__tests__/monitor_charts.test.tsx
+++ x-pack/legacy/plugins/uptime/public/components/functional/__tests__/monitor_charts.test.tsx
@@ -11,12 +11,6 @@ import { MonitorChartsComponent } from '../monitor_charts';
 import { MonitorChart } from '../../../../common/graphql/types';
 
 describe('MonitorCharts component', () => {
-  beforeAll(() => {
-    moment.prototype.toLocaleString = jest.fn(() => '2019-06-21 15:29:26');
-    moment.prototype.from = jest.fn(() => 'a few moments ago');
-    moment.prototype.fromNow = jest.fn(() => 'a few moments ago');
-  });
-
   const chartResponse: { monitorChartsData: MonitorChart } = {
     monitorChartsData: {
       locationDurationLines: [
