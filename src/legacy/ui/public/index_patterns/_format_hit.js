62add874e1e2965ae3f4d16edd75f02491378110
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2019-06-29 07:53:41 +0200

    Extend formatHitProvider to format type 'text' - to get rid of angular markup

--- src/legacy/ui/public/index_patterns/_format_hit.js
+++ src/legacy/ui/public/index_patterns/_format_hit.js
@@ -26,21 +26,28 @@ const partialFormattedCache = new WeakMap();
 // returns a formatted version
 export function formatHitProvider(indexPattern, defaultFormat) {
 
-  function convert(hit, val, fieldName) {
+  function convert(hit, val, fieldName, type = 'html') {
     const field = indexPattern.fields.byName[fieldName];
-    if (!field) return defaultFormat.convert(val, 'html');
+    if (!field) return defaultFormat.convert(val, type);
     const parsedUrl = {
       origin: window.location.origin,
       pathname: window.location.pathname,
       basePath: indexPattern.fieldsFetcher.apiClient.basePath,
     };
-    return field.format.getConverterFor('html')(val, field, hit, parsedUrl);
+    return field.format.getConverterFor(type)(val, field, hit, parsedUrl);
   }
 
-  function formatHit(hit) {
-    const cached = formattedCache.get(hit);
-    if (cached) {
-      return cached;
+  function formatHit(hit, type = 'html') {
+    if(type === 'text') {
+      //formatHit of type text is for react components to get rid of <span ng-non-bindable>
+      //since it's currently just used at the discover's doc view table, caching is not necessary
+      //but this might change in the future, since react will take over
+      const flattened = indexPattern.flattenHit(hit);
+      const result = {};
+      for(const [key, value] of Object.entries(flattened)) {
+        result[key] = convert(hit, value, key, type);
+      }
+      return result;
     }
 
     // use and update the partial cache, but don't rewrite it.
