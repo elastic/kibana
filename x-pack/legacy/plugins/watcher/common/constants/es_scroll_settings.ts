c2a14e75f3603ace9c4121feaf34399409e0c202
CJ Cenizal <cj@cenizal.com>
2019-06-29 04:05:55 -0700

    EUIfy Watcher (#35301)

---
+++ x-pack/legacy/plugins/watcher/common/constants/es_scroll_settings.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const ES_SCROLL_SETTINGS: {
+  KEEPALIVE: string;
+  PAGE_SIZE: number;
+} = {
+  // How long to keep a scroll alive
+  KEEPALIVE: '30s',
+
+  // How many results to return per scroll response
+  PAGE_SIZE: 100,
+};
