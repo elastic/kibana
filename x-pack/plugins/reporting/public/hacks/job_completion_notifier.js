9a80712287b17ebb45cfadfc5eb8e0a76daaa76b
Joel Griffith <joel.griffith@elastic.co>
2019-07-01 12:16:22 -0700

    Fixing broken reporting URL (#39822) (#40047)

--- x-pack/plugins/reporting/public/hacks/job_completion_notifier.js
+++ x-pack/plugins/reporting/public/hacks/job_completion_notifier.js
@@ -65,8 +65,9 @@ uiModules.get('kibana')
       // the job completes, that way we don't give the user a toast to download their report if they can't.
       // NOTE: this should be looking at configuration rather than the existence of a navLink
       if (core.chrome.navLinks.has('kibana:management')) {
-        const managementUrl = core.chrome.navLinks.get('kibana:management').url;
-        const reportingSectionUrl = `${managementUrl}/kibana/reporting`;
+        const { baseUrl } = core.chrome.navLinks.get('kibana:management');
+        const reportingSectionUrl = `${baseUrl}/kibana/reporting`;
+
         seeReportLink = (
           <p>
             <FormattedMessage
