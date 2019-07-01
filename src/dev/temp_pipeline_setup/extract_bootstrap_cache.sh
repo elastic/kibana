e02155e3e38efd1aa6a4fda7485917a877f2e299
Tre' Seymour <wayne.seymour@elastic.co>
2019-07-01 11:21:44 -0600

    Revert the ci setup scripts and start with a fresh dir. Now that we have two approaches (tar || dockerfile), it's good to start fresh and set as many vars up-front.

--- src/dev/temp_pipeline_setup/extract_bootstrap_cache.sh
+++ src/dev/temp_pipeline_setup/extract_bootstrap_cache.sh
@@ -5,6 +5,8 @@ set -e
 BRANCH=${GIT_BRANCH/origin\/} # Maybe using `sed` is a better idea
 targetBranch="${PR_TARGET_BRANCH:-${BRANCH#*/}}"
 bootstrapCache="$JENKINS_HOME/.kibana/bootstrap_cache/master.tar"
+# targetBranch="${PR_TARGET_BRANCH:-${GIT_BRANCH#*/}}"
+# bootstrapCache="$HOME/.kibana/bootstrap_cache/$targetBranch.tar"
 
 ###
 ### Extract the bootstrap cache that we create in the packer_cache.sh script
