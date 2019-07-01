69eb99a44091d3072fdb8ab2a777e19225c113d5
Jen Huang <its.jenetic@gmail.com>
2019-07-01 14:50:21 -0700

    Extract `getManagedRepositoryName` to `lib/`

---
+++ x-pack/legacy/plugins/snapshot_restore/server/lib/get_managed_repository_name.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const getManagedRepositoryName = async (callWithInternalUser: any): Promise<string> => {
+  const { persistent, transient, defaults } = await callWithInternalUser('cluster.getSettings', {
+    filterPath: '*.*managed_repository',
+    flatSettings: true,
+    includeDefaults: true,
+  });
+  const { 'cluster.metadata.managed_repository': managedRepositoryName = undefined } = {
+    ...defaults,
+    ...persistent,
+    ...transient,
+  };
+  return managedRepositoryName;
+};
