b0fd3382cfdcbd178ab5c0d9b73e270fb56cb074
Tre' Seymour <wayne.seymour@elastic.co>
2019-07-01 11:35:26 -0600

    Add todo

--- src/dev/temp_pipeline_setup/extract_bootstrap_cache.sh
+++ src/dev/temp_pipeline_setup/extract_bootstrap_cache.sh
@@ -4,6 +4,7 @@ set -e
 
 BRANCH=${GIT_BRANCH/origin\/} # Maybe using `sed` is a better idea
 targetBranch="${PR_TARGET_BRANCH:-${BRANCH#*/}}"
+# TODO: Un-hardcode the bootstrapCache variable below.
 bootstrapCache="${CACHE_DIR}master.tar"
 # targetBranch="${PR_TARGET_BRANCH:-${GIT_BRANCH#*/}}"
 # bootstrapCache="$HOME/.kibana/bootstrap_cache/$targetBranch.tar"
