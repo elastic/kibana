f265a5667f76f98e2f1a8c60ea9a65528e692d1d
Justin Kambic <justin.kambic@elastic.co>
2019-07-02 17:23:45 -0400

    Import new tests to graphql describe block.

--- x-pack/test/api_integration/apis/uptime/graphql/index.js
+++ x-pack/test/api_integration/apis/uptime/graphql/index.js
@@ -21,8 +21,10 @@ export default function ({ getService, loadTestFile }) {
     loadTestFile(require.resolve('./monitor_charts'));
     loadTestFile(require.resolve('./monitor_list'));
     loadTestFile(require.resolve('./monitor_page_title'));
+    loadTestFile(require.resolve('./monitor_states'));
     loadTestFile(require.resolve('./monitor_status_bar'));
     loadTestFile(require.resolve('./ping_list'));
     loadTestFile(require.resolve('./snapshot'));
+    loadTestFile(require.resolve('./snapshot_histogram'));
   });
 }
