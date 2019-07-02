08a7de7ef09cae97823e5f9beeb7e96f29b8486d
Melissa Alvarez <melissa.alvarez@elastic.co>
2019-07-02 17:26:11 +0200

    use TransformTable for persistent pagination + sorting

--- x-pack/legacy/plugins/ml/public/data_frame/pages/job_management/components/job_list/preview_pane.tsx
+++ x-pack/legacy/plugins/ml/public/data_frame/pages/job_management/components/job_list/preview_pane.tsx
@@ -6,17 +6,13 @@
 
 import React, { FC, useEffect, useState } from 'react';
 import { i18n } from '@kbn/i18n';
-import { EuiInMemoryTable, EuiInMemoryTableProps, SortDirection } from '@elastic/eui';
+import { SortDirection } from '@elastic/eui';
 import { getFlattenedFields } from '../../../../components/source_index_preview/common';
 import { ml } from '../../../../../services/ml_api_service';
 
 import { DataFramePreviewRequest, useRefreshTransformList } from '../../../../common';
 import { DataFrameTransformWithId } from '../../../../common/job';
-
-interface CompressedTableProps extends EuiInMemoryTableProps {
-  compressed: boolean;
-  error: any;
-}
+import { TransformTable } from './transform_table';
 
 interface Column {
   field: string;
@@ -73,8 +69,11 @@ function getDataFromTransform(
 export const PreviewPane: FC<Props> = ({ transformConfig }) => {
   const [dataFramePreviewData, setDataFramePreviewData] = useState([]);
   const [columns, setColumns] = useState<Column[] | []>([]);
-  const [sort, setSort] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [sortField, setSortField] = useState<string>('');
+  const [sortDirection, setSortDirection] = useState<string>(SortDirection.ASC);
+  const [isLoading, setIsLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
   const { isRefresh } = useRefreshTransformList();
 
@@ -90,20 +89,15 @@ export const PreviewPane: FC<Props> = ({ transformConfig }) => {
           return {
             field: k,
             name: k,
-            sortable: false,
+            sortable: true,
             truncateText: true,
           };
         });
-        const sorting = {
-          sort: {
-            field: tableColumns[0].field,
-            direction: SortDirection.ASC,
-          },
-        };
 
         setDataFramePreviewData(resp.preview);
         setColumns(tableColumns);
-        setSort(sorting);
+        setSortField(tableColumns[0].field);
+        setSortDirection(SortDirection.ASC);
         setIsLoading(false);
       }
     } catch (error) {
@@ -129,19 +123,46 @@ export const PreviewPane: FC<Props> = ({ transformConfig }) => {
     }
   });
 
-  const CompressedTable = (EuiInMemoryTable as any) as FC<CompressedTableProps>;
+  const pagination = {
+    initialPageIndex: pageIndex,
+    initialPageSize: pageSize,
+    totalItemCount: dataFramePreviewData.length,
+    pageSizeOptions: [10, 20],
+    hidePerPageOptions: false,
+  };
+
+  const sorting = {
+    sort: {
+      field: sortField,
+      direction: sortDirection,
+    },
+  };
+
+  const onTableChange = ({
+    page = { index: 0, size: 10 },
+    sort = { field: columns[0].field, direction: SortDirection.ASC },
+  }: {
+    page: { index: number; size: number };
+    sort: { field: string; direction: string };
+  }) => {
+    const { index, size } = page;
+    setPageIndex(index);
+    setPageSize(size);
+
+    const { field, direction } = sort;
+    setSortField(field);
+    setSortDirection(direction);
+  };
 
   return (
-    <CompressedTable
+    <TransformTable
       loading={dataFramePreviewData.length === 0 && isLoading === true}
       compressed
       items={dataFramePreviewData}
       columns={columns}
-      pagination={{
-        initialPageSize: 5,
-        pageSizeOptions: [5, 10, 25],
-      }}
-      sorting={sort}
+      onChange={onTableChange}
+      pagination={pagination}
+      sorting={sorting}
       error={errorMessage}
     />
   );
