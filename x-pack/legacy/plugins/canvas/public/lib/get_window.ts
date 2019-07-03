3edd52d09985c7f9d1897275e9a8d810af52a6ed
Clint Andrew Hall <clint.hall@elastic.co>
2019-07-02 22:35:19 -0500

    Enable Canvas URL Parameters (#40061) (#40112)
    * Enable Canvas URL Parameters
    
    * Update to use time interval strings
    
    * Addressing feedback

--- x-pack/legacy/plugins/canvas/public/lib/get_window.ts
+++ x-pack/legacy/plugins/canvas/public/lib/get_window.ts
@@ -7,6 +7,6 @@
 // return window if it exists, otherwise just return an object literal
 const windowObj = {};
 
-export const getWindow = () => {
+export const getWindow = (): Window | {} => {
   return typeof window === 'undefined' ? windowObj : window;
 };
