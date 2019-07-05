4e2aa4deba565e6ec54abb2c1889e8eeccd473f3
spalger <email@spalger.com>
2019-07-05 07:34:55 -0700

    make elasticsearch.cluster optional, many instances don't have them

--- x-pack/test/ui_capabilities/common/types.ts
+++ x-pack/test/ui_capabilities/common/types.ts
@@ -22,7 +22,7 @@ export interface RoleKibanaPrivilege {
 export interface CustomRoleSpecification {
   name: string;
   elasticsearch?: {
-    cluster: string[];
+    cluster?: string[];
     indices: CustomRoleSpecificationElasticsearchIndices[];
   };
   kibana?: RoleKibanaPrivilege[];
