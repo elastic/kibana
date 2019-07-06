dfff4721dc06d7e345420f75c4086ed2fb018ccc
John Schulz <john.schulz@elastic.co>
2019-07-06 12:25:39 -0400

    Sort integrations list alphabetically closes #40468

--- x-pack/legacy/plugins/integrations_manager/server/integrations.ts
+++ x-pack/legacy/plugins/integrations_manager/server/integrations.ts
@@ -52,7 +52,15 @@ export async function handleGetList(req: Request) {
     integrationList.push(integration);
   }
 
-  return integrationList;
+  return integrationList.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    } else if (a.name < b.name) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
 }
 
 export async function handleGetInfo(req: PackageRequest) {
