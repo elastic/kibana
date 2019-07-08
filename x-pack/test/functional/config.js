b2c20b52cadba4dc3edbfcf5dd5853dc255614e0
John Dorlus <jsdorlus@elastic.co>
2019-07-08 13:04:26 -0400

    Revert "Added snapshot restore smoke test to ensure the app has loaded. (#39812)"
    This reverts commit 95c6158b4371fbd27971855107a5c390381a749e.

--- x-pack/test/functional/config.js
+++ x-pack/test/functional/config.js
@@ -28,8 +28,6 @@ import {
   RollupPageProvider,
   UptimePageProvider,
   LicenseManagementPageProvider,
-  IndexLifecycleManagementPageProvider,
-  SnapshotRestorePageProvider
 } from './page_objects';
 
 import {
@@ -113,9 +111,7 @@ export default async function ({ readConfigFile }) {
       resolve(__dirname, './apps/dev_tools'),
       resolve(__dirname, './apps/apm'),
       resolve(__dirname, './apps/index_patterns'),
-      resolve(__dirname, './apps/license_management'),
-      resolve(__dirname, './apps/index_lifecycle_management'),
-      resolve(__dirname, './apps/snapshot_restore')
+      resolve(__dirname, './apps/license_management')
     ],
 
     // define the name and providers for services that should be
@@ -182,9 +178,7 @@ export default async function ({ readConfigFile }) {
       code: CodeHomePageProvider,
       uptime: UptimePageProvider,
       rollup: RollupPageProvider,
-      licenseManagement: LicenseManagementPageProvider,
-      indexLifecycleManagement: IndexLifecycleManagementPageProvider,
-      snapshotRestore: SnapshotRestorePageProvider
+      licenseManagement: LicenseManagementPageProvider
     },
 
     servers: kibanaFunctionalConfig.get('servers'),
@@ -285,14 +279,6 @@ export default async function ({ readConfigFile }) {
       licenseManagement: {
         pathname: '/app/kibana',
         hash: '/management/elasticsearch/license_management',
-      },
-      indexLifecycleManagement: {
-        pathname: '/app/kibana',
-        hash: '/management/elasticsearch/index_lifecycle_management',
-      },
-      snapshotRestore: {
-        pathname: '/app/kibana',
-        hash: '/management/elasticsearch/snapshot_restore',
       },
       apm: {
         pathname: '/app/apm',
