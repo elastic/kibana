3e12da3eaace1bd0a8ddf7789c0a8aa6f846fe0b
Chris Cowan <chris@chriscowan.us>
2019-07-09 13:37:26 -0700

    fixed logic typo

--- x-pack/legacy/plugins/infra/public/utils/formatters/bytes.ts
+++ x-pack/legacy/plugins/infra/public/utils/formatters/bytes.ts
@@ -41,7 +41,7 @@ const BASES = {
 export const createBytesFormatter = (format: InfraWaffleMapDataFormat) => (bytes: number) => {
   const labels = LABELS[format];
   const base = BASES[format];
-  const value = InfraWaffleMapDataFormat.bitsDecimal ? bytes * 8 : bytes;
+  const value = format === InfraWaffleMapDataFormat.bitsDecimal ? bytes * 8 : bytes;
   // Use an exponetial equation to get the power to determine which label to use. If the power
   // is greater then the max label then use the max label.
   const power = Math.min(Math.floor(Math.log(Math.abs(value)) / Math.log(base)), labels.length - 1);
