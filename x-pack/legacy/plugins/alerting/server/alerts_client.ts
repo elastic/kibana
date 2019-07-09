3df15a891b9949fe650a90901595d14af6e69b18
Mike Cote <mikecote@users.noreply.github.com>
2019-07-09 15:07:39 -0400

    Fix ESLint errors

--- x-pack/legacy/plugins/alerting/server/alerts_client.ts
+++ x-pack/legacy/plugins/alerting/server/alerts_client.ts
@@ -200,7 +200,7 @@ export class AlertsClient {
           enabled: false,
           scheduledTaskId: null,
         },
-        { references: existingObject.references },
+        { references: existingObject.references }
       );
     }
   }
