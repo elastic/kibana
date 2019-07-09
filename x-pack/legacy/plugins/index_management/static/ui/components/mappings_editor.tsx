ca1b4eee4fe970d32f121240adbef3db0b5ad3ac
Sébastien <sebastien.loix@elastic.co>
2019-07-09 09:32:30 +0200

    Add prop to control validation error display + change i18n translate key

--- x-pack/legacy/plugins/index_management/static/ui/components/mappings_editor.tsx
+++ x-pack/legacy/plugins/index_management/static/ui/components/mappings_editor.tsx
@@ -4,13 +4,14 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useState, useEffect } from 'react';
-import { EuiCodeEditor } from '@elastic/eui';
+import React, { useState, useEffect, Fragment } from 'react';
+import { EuiCodeEditor, EuiSpacer, EuiCallOut } from '@elastic/eui';
 
 interface Props {
   setGetDataHandler: (handler: () => { isValid: boolean; data: Mappings }) => void;
   FormattedMessage: typeof ReactIntl.FormattedMessage;
   defaultValue?: Mappings;
+  areErrorsVisible?: boolean;
 }
 
 export interface Mappings {
@@ -20,11 +21,14 @@ export interface Mappings {
 export const MappingsEditor = ({
   setGetDataHandler,
   FormattedMessage,
+  areErrorsVisible = true,
   defaultValue = {},
 }: Props) => {
   const [mappings, setMappings] = useState<string>(JSON.stringify(defaultValue, null, 2));
+  const [error, setError] = useState<string | null>(null);
 
   const getFormData = () => {
+    setError(null);
     try {
       const parsed: Mappings = JSON.parse(mappings);
       return {
@@ -32,6 +36,7 @@ export const MappingsEditor = ({
         isValid: true,
       };
     } catch (e) {
+      setError(e.message);
       return {
         isValid: false,
         data: {},
@@ -44,31 +49,50 @@ export const MappingsEditor = ({
   }, [mappings]);
 
   return (
-    <EuiCodeEditor
-      mode="json"
-      theme="textmate"
-      width="100%"
-      value={mappings}
-      setOptions={{
-        showLineNumbers: false,
-        tabSize: 2,
-        maxLines: Infinity,
-      }}
-      editorProps={{
-        $blockScrolling: Infinity,
-      }}
-      showGutter={false}
-      minLines={6}
-      aria-label={
-        <FormattedMessage
-          id="xpack.idxMgmt.mappingsEditor"
-          defaultMessage="Index mappings editor"
-        />
-      }
-      onChange={(value: string) => {
-        setMappings(value);
-      }}
-      data-test-subj="mappingsEditor"
-    />
+    <Fragment>
+      <EuiCodeEditor
+        mode="json"
+        theme="textmate"
+        width="100%"
+        value={mappings}
+        setOptions={{
+          showLineNumbers: false,
+          tabSize: 2,
+          maxLines: Infinity,
+        }}
+        editorProps={{
+          $blockScrolling: Infinity,
+        }}
+        showGutter={false}
+        minLines={6}
+        aria-label={
+          <FormattedMessage
+            id="xpack.idxMgmt.mappingsEditor.mappingsEditorAriaLabel"
+            defaultMessage="Index mappings editor"
+          />
+        }
+        onChange={(value: string) => {
+          setMappings(value);
+        }}
+        data-test-subj="mappingsEditor"
+      />
+      {areErrorsVisible && error && (
+        <Fragment>
+          <EuiSpacer size="m" />
+          <EuiCallOut
+            title={
+              <FormattedMessage
+                id="xpack.idxMgmt.mappingsEditor.formatError"
+                defaultMessage="JSON format error"
+              />
+            }
+            color="danger"
+            iconType="alert"
+          >
+            <p data-test-subj="errors">{error}</p>
+          </EuiCallOut>
+        </Fragment>
+      )}
+    </Fragment>
   );
 };
