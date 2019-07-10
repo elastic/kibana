aa6ebf1457e35b334e77294e129003e0d61f9917
Joel Griffith <joel.griffith@elastic.co>
2019-07-10 08:51:31 -0700

    Fixing issues with typedef's missing

--- x-pack/legacy/plugins/reporting/server/lib/validate/validate_browser.ts
+++ x-pack/legacy/plugins/reporting/server/lib/validate/validate_browser.ts
@@ -3,6 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+// @ts-ignore no module definition
 import * as puppeteer from 'puppeteer-core';
 import { KbnServer, Logger } from '../../../types';
 import { CHROMIUM } from '../../browsers/browser_types';
