766020a37768cf65e44b1c3f50571d2ba29d0b79
Tre' Seymour <wayne.seymour@elastic.co>
2019-07-10 12:19:37 -0600

    Drop old start script for new, update docs

--- .ci/README.md
+++ .ci/README.md
@@ -1,7 +1,32 @@
 # Jenkins CI Pipelines
 
 ## Pipeline Unit Test Setup
+
+### Download and start local jenkins
+
+This will download, extract, and launch `Junkins` @ http://localhost:8080/  
+_From one shell window_
 ```
-cd KIBANA_PATH/.ci
-./test_pipeline.sh
+pushd KIBANA_PATH/.ci
+./bootsrap_pipeline_test.sh 
 ```
+
+### Prepare local Junkins for use
+
+*Grab the inital password*
+`pbcopy < ~/.jenkins/secrets/initialAdminPassword` _to use in Junkins ui_
+
+Using the gui
+
+ - Browse to http://localhost:8080/   
+ - Install the recommended plugins
+ - Create admin acct
+
+### Run the Pipeline Test
+_From a different shell window_
+```
+pushd KIBANA_PATH/.ci
+src/resources/jenkinsfile-runner/app/target/appassembler/bin/jenkinsfile-runner -w /tmp/jenkins -p /tmp/jenkins_home/plugins -f ~/foo/ -a "param1=Hello&param2=value2"
+```
+
+> Dont forget to kill the Junkins process when finished
---
+++ .ci/bootstrap_pipeline_test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+RESOURCES_DIR="./src/resources"
+WAR_PATH="${RESOURCES_DIR}/jenkins.war"
+TEMP_JUNKINS_BINARY="/tmp/jenkins"
+JENKINS_HOME="/tmp/jenkins_home"
+
+bootstrapJunkins () {
+  wget -r -O ${WAR_PATH} http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+  unzip -o ${WAR_PATH} -d ${TEMP_JUNKINS_BINARY}
+  java -jar ${WAR_PATH}
+}
+bootstrapJunkins
+
+# startJunkins () {
+#   ./app/target/appassembler/bin/jenkinsfile-runner -w ${TEMP_JUNKINS_BINARY} \
+#   -p ${JENKINS_HOME}/plugins -f ~/foo/ -a "param1=Hello&param2=value2"
+# }
+# startJunkins
+
+# ${RESOURCES_DIR}/jenkinsfile-runner/app/target/appassembler/bin/jenkinsfile-runner -w ${TEMP_JUNKINS_BINARY} -p ${TEMP_JUNKINS_BINARY}_home/plugins -f ~/foo/ -a "param1=Hello&param2=value2"
+# ./app/target/appassembler/bin/jenkinsfile-runner -w ${TEMP_JUNKINS_BINARY} -p ${JENKINS_HOME}/plugins -f ~/foo/ -a "param1=Hello&param2=value2"
