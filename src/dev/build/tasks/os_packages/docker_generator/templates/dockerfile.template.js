c6960d6f1d588038b4dda1c046d30cea9a762f80
Joel Griffith <joel.griffith@elastic.co>
2019-07-11 16:01:44 -0700

    Fixes an issue where headless_shell doesn't get properly closed (and thus zombied)

--- src/dev/build/tasks/os_packages/docker_generator/templates/dockerfile.template.js
+++ src/dev/build/tasks/os_packages/docker_generator/templates/dockerfile.template.js
@@ -32,7 +32,7 @@ function generator({ artifactTarball, versionTag, license, usePublicArtifact  })
   #
   # ** THIS IS AN AUTO-GENERATED FILE **
   #
-   
+
   ################################################################################
   # Build stage 0
   # Extract Kibana and make various file manipulations.
@@ -48,43 +48,46 @@ function generator({ artifactTarball, versionTag, license, usePublicArtifact  })
   # REF: https://docs.openshift.org/latest/creating_images/guidelines.html
   RUN chmod -R g=u /usr/share/kibana
   RUN find /usr/share/kibana -type d -exec chmod g+s {} \\;
-  
+
   ################################################################################
   # Build stage 1
   # Copy prepared files from the previous stage and complete the image.
   ################################################################################
   FROM centos:7
   EXPOSE 5601
-  
+
   # Add Reporting dependencies.
-  RUN yum update -y && yum install -y fontconfig freetype && yum clean all
-  
+  RUN yum update -y && yum install -y fontconfig freetype wget && yum clean all
+
+  # Add an init process
+  RUN wget -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.2/dumb-init_1.2.2_amd64 && chmod +x /usr/local/bin/dumb-init
+
   # Bring in Kibana from the initial stage.
   COPY --from=prep_files --chown=1000:0 /usr/share/kibana /usr/share/kibana
   WORKDIR /usr/share/kibana
   RUN ln -s /usr/share/kibana /opt/kibana
-  
+
   ENV ELASTIC_CONTAINER true
   ENV PATH=/usr/share/kibana/bin:$PATH
-  
+
   # Set some Kibana configuration defaults.
   COPY --chown=1000:0 config/kibana.yml /usr/share/kibana/config/kibana.yml
-  
+
   # Add the launcher/wrapper script. It knows how to interpret environment
   # variables and translate them to Kibana CLI options.
   COPY --chown=1000:0 bin/kibana-docker /usr/local/bin/
-  
+
   # Ensure gid 0 write permissions for OpenShift.
   RUN chmod g+ws /usr/share/kibana && \\
       find /usr/share/kibana -gid 0 -and -not -perm /g+w -exec chmod g+w {} \\;
-  
+
   # Provide a non-root user to run the process.
   RUN groupadd --gid 1000 kibana && \\
       useradd --uid 1000 --gid 1000 \\
         --home-dir /usr/share/kibana --no-create-home \\
         kibana
   USER kibana
-  
+
   LABEL org.label-schema.schema-version="1.0" \\
     org.label-schema.vendor="Elastic" \\
     org.label-schema.name="kibana" \\
@@ -92,7 +95,9 @@ function generator({ artifactTarball, versionTag, license, usePublicArtifact  })
     org.label-schema.url="https://www.elastic.co/products/kibana" \\
     org.label-schema.vcs-url="https://github.com/elastic/kibana" \\
     license="${ license }"
-  
+
+  ENTRYPOINT ["/usr/local/bin/dumb-init", "--"]
+
   CMD ["/usr/local/bin/kibana-docker"]
   `);
 }
