6366cf4fb1545caa7371f96027f58424478d9bfc
Christopher Davies <github@christophilus.com>
2019-07-11 15:25:57 -0400

    Add rudimentary smokescreen test for FieldIcon

---
+++ x-pack/legacy/plugins/lens/public/indexpattern_plugin/field_icon.test.tsx
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { shallow } from 'enzyme';
+import React from 'react';
+import { FieldIcon } from './field_icon';
+
+describe('FieldIcon', () => {
+  it('should render numeric icons', () => {
+    expect(shallow(<FieldIcon type="boolean" />)).toMatchInlineSnapshot(`
+      <EuiIcon
+        className="lnsFieldListPanel__fieldIcon lnsFieldListPanel__fieldIcon--boolean"
+        color="#F37020"
+        type="invert"
+      />
+    `);
+    expect(shallow(<FieldIcon type="date" />)).toMatchInlineSnapshot(`
+      <EuiIcon
+        className="lnsFieldListPanel__fieldIcon lnsFieldListPanel__fieldIcon--date"
+        color="#B0916F"
+        type="calendar"
+      />
+    `);
+    expect(shallow(<FieldIcon type="number" />)).toMatchInlineSnapshot(`
+      <EuiIcon
+        className="lnsFieldListPanel__fieldIcon lnsFieldListPanel__fieldIcon--number"
+        color="#1EA593"
+        type="number"
+      />
+    `);
+    expect(shallow(<FieldIcon type="string" />)).toMatchInlineSnapshot(`
+      <EuiIcon
+        className="lnsFieldListPanel__fieldIcon lnsFieldListPanel__fieldIcon--string"
+        color="#FCA5D3"
+        type="string"
+      />
+    `);
+  });
+});
