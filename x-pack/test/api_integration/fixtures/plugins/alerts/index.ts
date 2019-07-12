94be6d7be93bdf2c736aa1e14318d69b4d31e378
Patrick Mueller <pmuellr@gmail.com>
2019-07-11 23:40:15 -0400

    revert mistaken schema change to alerts

--- x-pack/test/api_integration/fixtures/plugins/alerts/index.ts
+++ x-pack/test/api_integration/fixtures/plugins/alerts/index.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import Joi from 'joi';
 import { schema } from '@kbn/config-schema';
 import { AlertExecutorOptions, AlertType } from '../../../../../legacy/plugins/alerting';
 import { ActionTypeExecutorOptions, ActionType } from '../../../../../legacy/plugins/actions';
@@ -137,9 +138,11 @@ export default function(kibana: any) {
         id: 'test.validation',
         name: 'Test: Validation',
         validate: {
-          params: schema.object({
-            param1: schema.string(),
-          }),
+          params: Joi.object()
+            .keys({
+              param1: Joi.string().required(),
+            })
+            .required(),
         },
         async executor({ services, params, state }: AlertExecutorOptions) {},
       };
