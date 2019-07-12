012dc81191baa59e4f64226429fe9bff2e0b03a3
Tre' Seymour <wayne.seymour@elastic.co>
2019-07-12 10:00:37 -0600

    Use the home var

--- .ci/bootstrap_pipeline_test.sh
+++ .ci/bootstrap_pipeline_test.sh
@@ -3,12 +3,11 @@
 RESOURCES_DIR="./src/resources"
 WAR_PATH="${RESOURCES_DIR}/jenkins.war"
 TEMP_JUNKINS_BINARY="/tmp/jenkins"
-JENKINS_HOME="/tmp/jenkins_home"
 
 bootstrapJunkins () {
   wget -r -O ${WAR_PATH} http://mirrors.jenkins.io/war-stable/latest/jenkins.war
   unzip -o ${WAR_PATH} -d ${TEMP_JUNKINS_BINARY}
-  java -jar ${WAR_PATH}
+  JENKINS_HOME="/tmp/jenkins_home" java -jar ${WAR_PATH}
 }
 bootstrapJunkins
 
