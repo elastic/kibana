857663280799efe9e8578364b7cc1f7e0632659d
Sébastien <sebastien.loix@elastic.co>
2019-07-12 17:10:50 +0200

    Add support for "array" data type

--- x-pack/legacy/plugins/index_management/static/ui/components/mappings_editor/components/properties_manager.tsx
+++ x-pack/legacy/plugins/index_management/static/ui/components/mappings_editor/components/properties_manager.tsx
@@ -34,7 +34,6 @@ export const PropertiesManager = ({ form, path = 'properties' }: Props) => {
                 />
               </li>
             ))}
-            <EuiSpacer size="s" />
             <EuiButton color="primary" onClick={addRow}>
               Add property
             </EuiButton>
