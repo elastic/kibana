3c9ad92283390d0ad6b821fdfec2548e6ced613f
Garrett Spong <garrett.spong@elastic.co>
2019-07-12 15:17:21 -0600

    One more link + snapshot fix

--- x-pack/legacy/plugins/siem/public/components/ml_popover/popover_description.tsx
+++ x-pack/legacy/plugins/siem/public/components/ml_popover/popover_description.tsx
@@ -16,7 +16,7 @@ export const PopoverDescription = React.memo(() => (
       defaultMessage="Run any of the Machine Learning jobs below to view anomalous events throughout the SIEM application. We’ve provided a few common detection jobs to get you started. If you wish to add your own custom jobs, simply create and tag them with “SIEM” from the {machineLearning} application for inclusion here."
       values={{
         machineLearning: (
-          <EuiLink href={`${chrome.getBasePath()}/app/ml`}>
+          <EuiLink href={`${chrome.getBasePath()}/app/ml`} target="_blank">
             <FormattedMessage
               id="xpack.siem.components.mlPopup.machineLearningLink"
               defaultMessage="Machine Learning"
