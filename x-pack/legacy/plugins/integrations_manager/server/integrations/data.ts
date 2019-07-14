f4ff475eaaaddf8076085fa4fdcd5a516ae51243
John Schulz <john.schulz@elastic.co>
2019-07-14 15:13:31 -0400

    Move getObjects and supporting functions from registry to integrations.

--- x-pack/legacy/plugins/integrations_manager/server/integrations/data.ts
+++ x-pack/legacy/plugins/integrations_manager/server/integrations/data.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { SavedObject } from 'src/core/server/saved_objects';
 import { SavedObjectsClientContract, ScopedClusterClient } from 'src/core/server/';
 import { SAVED_OBJECT_TYPE, AssetTypes, InstallationStatus } from '../../common/constants';
 import {
@@ -98,7 +99,7 @@ async function installObjects(
   asset: string
 ): Promise<AssetReference[]> {
   const filter = (entry: Registry.ArchiveEntry) => asset === Registry.pathParts(entry.path).type;
-  const toBeSavedObjects = await Registry.getObjects(pkgkey, filter);
+  const toBeSavedObjects = await getObjects(pkgkey, filter);
   const createResults = await client.bulkCreate<InstallationAttributes>(toBeSavedObjects, {
     overwrite: true,
   });
@@ -204,4 +205,80 @@ function createInstallableFrom<T>(from: T, savedObject?: Installation): Installa
 function assetUsesObjects(asset: string) {
   const values = Object.values(SavedObjectTypes);
   return values.includes(asset);
+}
+
+async function getObjects(
+  pkgkey: string,
+  filter = (entry: Registry.ArchiveEntry): boolean => true
+): Promise<SavedObject[]> {
+  // Create a Map b/c some values, especially index-patterns, are referenced multiple times
+  const objects: Map<string, SavedObject> = new Map();
+
+  // Get paths which match the given filter
+  const paths = await Registry.getArchiveInfo(`${pkgkey}.tar.gz`, filter);
+
+  // Get all objects which matched filter. Add them to the Map
+  const rootObjects = paths.map(getObject);
+  rootObjects.forEach(obj => objects.set(obj.id, obj));
+
+  // Each of those objects might have `references` property like [{id, type, name}]
+  for (const object of rootObjects) {
+    // For each of those objects
+    for (const reference of object.references) {
+      // Get the objects they reference. Call same function with a new filter
+      const referencedObjects = await getObjects(pkgkey, (entry: Registry.ArchiveEntry) => {
+        // Skip anything we've already stored
+        if (objects.has(reference.id)) return false;
+
+        // Is the archive entry the reference we want?
+        const { type, file } = Registry.pathParts(entry.path);
+        const isType = type === reference.type;
+        const isJson = file === `${reference.id}.json`;
+        return isType && isJson;
+      });
+
+      // Add referenced objects to the Map
+      referencedObjects.forEach(ro => objects.set(ro.id, ro));
+    }
+  }
+
+  // return the array of unique objects
+  return Array.from(objects.values());
+}
+
+// the assets from the registry are malformed
+// https://github.com/elastic/integrations-registry/issues/42
+function ensureJsonValues(obj: SavedObject) {
+  const { attributes } = obj;
+  if (
+    attributes.kibanaSavedObjectMeta &&
+    typeof attributes.kibanaSavedObjectMeta.searchSourceJSON !== 'string'
+  ) {
+    attributes.kibanaSavedObjectMeta.searchSourceJSON = JSON.stringify(
+      attributes.kibanaSavedObjectMeta.searchSourceJSON
+    );
+  }
+  ['optionsJSON', 'panelsJSON', 'uiStateJSON', 'visState']
+    .filter(key => typeof attributes[key] !== 'string')
+    .forEach(key => (attributes[key] = JSON.stringify(attributes[key])));
+  return obj;
+}
+
+function getObject(key: string) {
+  const buffer = Registry.getAsset(key);
+
+  // cache values are buffers. convert to string / JSON
+  const json = buffer.toString('utf8');
+  // convert that to an object & address issues with the formatting of some parts
+  const asset = ensureJsonValues(JSON.parse(json));
+
+  const { type, file } = Registry.pathParts(key);
+  const savedObject: SavedObject = {
+    type: asset.type || type,
+    id: asset.id || file.replace('.json', ''),
+    attributes: asset,
+    references: asset.references || [],
+  };
+
+  return savedObject;
 }
