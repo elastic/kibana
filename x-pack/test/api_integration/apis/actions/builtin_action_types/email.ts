b10a4d31697fdc2c7cce45f6930e83bfdee6f8c8
Patrick Mueller <pmuellr@gmail.com>
2019-07-15 11:43:22 -0400

    Change action config / params schema from joi to @kbn/config-schema (#40694)
    * Adds actionTypeConfig to AAD exclusion for action ESOs
    
    fixes https://github.com/elastic/kibana/issues/40177
    
    Prior to this, the `actionTypeConfig` was not excluded from AAD when using
    encrypted saved objects in actions.
    
    https://github.com/elastic/kibana/blob/d0da71c2b4b154fe2efe86b44869c06709c15d14/x-pack/legacy/plugins/actions/server/init.ts#L31-L35
    
    This caused a problem when updating values in the `actionTypeConfig`, as per
    issue https://github.com/elastic/kibana/issues/40177
    
    Also added `x-pack/test/functional/es_archives/actions/README.md` to explain
    how to get the id and encrypted value string, if this needs to be done again
    later, since it's a little tricky.
    
    * change alertings reference to actions archived action
    
    Alert happened to reuse the archived action, so it's reference to the
    action also had to be updated.

--- x-pack/test/api_integration/apis/actions/builtin_action_types/email.ts
+++ x-pack/test/api_integration/apis/actions/builtin_action_types/email.ts
@@ -56,6 +56,9 @@ export default function emailTest({ getService }: KibanaFunctionalTestDefaultPro
           actionTypeConfig: {
             from: 'bob@example.com',
             service: '__json',
+            host: null,
+            port: null,
+            secure: null,
           },
         },
         references: [],
