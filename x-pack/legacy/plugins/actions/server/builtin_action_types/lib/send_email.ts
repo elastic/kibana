88ac27295d979f87240cf2510708bd96033f1269
Patrick Mueller <pmuellr@gmail.com>
2019-07-15 17:59:15 -0400

    Renders email action message text as html from markdown - take 2 (#41187)
    * Renders email action message text as html from markdown
    
    * fix jest test
    
    * Fix refactor issue with previous PR in actions/email

--- x-pack/legacy/plugins/actions/server/builtin_action_types/lib/send_email.ts
+++ x-pack/legacy/plugins/actions/server/builtin_action_types/lib/send_email.ts
@@ -7,6 +7,10 @@
 // info on nodemailer: https://nodemailer.com/about/
 import nodemailer from 'nodemailer';
 
+import { default as MarkdownIt } from 'markdown-it';
+
+import { Services } from '../../types';
+
 // an email "service" which doesn't actually send, just returns what it would send
 export const JSON_TRANSPORT_SERVICE = '__json';
 
@@ -39,7 +43,7 @@ interface Content {
 }
 
 // send an email
-export async function sendEmail(options: SendEmailOptions): Promise<any> {
+export async function sendEmail(services: Services, options: SendEmailOptions): Promise<any> {
   const { transport, routing, content } = options;
   const { service, host, port, secure, user, password } = transport;
   const { from, to, cc, bcc } = routing;
@@ -64,6 +68,7 @@ export async function sendEmail(options: SendEmailOptions): Promise<any> {
   }
 
   const nodemailerTransport = nodemailer.createTransport(transportConfig);
+  const messageHTML = htmlFromMarkdown(services, message);
 
   const email = {
     // email routing
@@ -73,7 +78,7 @@ export async function sendEmail(options: SendEmailOptions): Promise<any> {
     bcc,
     // email content
     subject,
-    html: message,
+    html: messageHTML,
     text: message,
   };
 
@@ -88,4 +93,19 @@ export async function sendEmail(options: SendEmailOptions): Promise<any> {
   }
 
   return result;
+}
+
+// try rendering markdown to html, return markdown on any kind of error
+function htmlFromMarkdown(services: Services, markdown: string) {
+  try {
+    const md = MarkdownIt({
+      linkify: true,
+    });
+
+    return md.render(markdown);
+  } catch (err) {
+    services.log(['debug', 'actions'], `error rendering markdown to html: ${err.message}`);
+
+    return markdown;
+  }
 }
