7188ea1a27c5521b223626178678d67e677f46fa
John Dorlus <jsdorlus@elastic.co>
2019-07-15 23:15:34 +0000

    Added app load test for Remote Clusters. (#40932)

---
+++ x-pack/test/functional/apps/remote_clusters/home_page.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from '@kbn/expect';
+import { KibanaFunctionalTestDefaultProviders } from '../../../types/providers';
+
+// eslint-disable-next-line import/no-default-export
+export default ({ getPageObjects, getService }: KibanaFunctionalTestDefaultProviders) => {
+  const pageObjects = getPageObjects(['common', 'remoteClusters']);
+
+  describe('Home page', function() {
+    this.tags('smoke');
+    before(async () => {
+      await pageObjects.common.navigateToApp('remoteClusters');
+    });
+
+    it('Loads the app', async () => {
+      const remoteClusterButton = await pageObjects.remoteClusters.remoteClusterCreateButton();
+      expect(await remoteClusterButton.isDisplayed()).to.be(true);
+    });
+  });
+};
