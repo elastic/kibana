004e3b32b2d20088348068b553ce1883f177c680
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2019-07-15 18:28:42 +0200

    skip flaky test (#41112)

--- x-pack/test/functional/apps/code/explore_repository.ts
+++ x-pack/test/functional/apps/code/explore_repository.ts
@@ -217,7 +217,8 @@ export default function exploreRepositoryFunctionalTests({
         });
       });
 
-      it('click a breadcrumb should not affect the file tree', async () => {
+      // FLAKY https://github.com/elastic/kibana/issues/41112
+      it.skip('click a breadcrumb should not affect the file tree', async () => {
         log.debug('it goes to a deep node of file tree');
         const url = `${PageObjects.common.getHostPort()}/app/code#/github.com/elastic/TypeScript-Node-Starter/blob/master/src/models/User.ts`;
         await browser.get(url);
