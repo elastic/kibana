668994ddce8d6a2a2df9025d96501a6bda32f787
Michael Marcialis <michael.marcialis@elastic.co>
2019-07-15 11:53:24 -0400

    add loader to anomalies tables

--- x-pack/legacy/plugins/siem/public/components/ml/tables/anomalies_host_table.tsx
+++ x-pack/legacy/plugins/siem/public/components/ml/tables/anomalies_host_table.tsx
@@ -4,16 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useContext } from 'react';
 import { EuiPanel } from '@elastic/eui';
+import React, { useContext } from 'react';
+import styled, { css } from 'styled-components';
+
 import { useAnomaliesTableData } from '../anomaly/use_anomalies_table_data';
 import { HeaderPanel } from '../../header_panel';
 
 import * as i18n from './translations';
 import { getAnomaliesHostTableColumnsCurated } from './get_anomalies_host_table_columns';
 import { convertAnomaliesToHosts } from './convert_anomalies_to_hosts';
-import { BackgroundRefetch, BasicTableContainer } from '../../load_more_table';
-import { LoadingPanel } from '../../loading';
+import { Loader } from '../../loader';
 import { getIntervalFromAnomalies } from '../anomaly/get_interval_from_anomalies';
 import { getSizeFromAnomalies } from '../anomaly/get_size_from_anomalies';
 import { AnomaliesHostTableProps } from '../types';
@@ -56,34 +57,34 @@ export const AnomaliesHostTable = React.memo<AnomaliesHostTableProps>(
       pageSizeOptions: [5, 10, 20, 50],
       hidePerPageOptions: false,
     };
+
     if (!hasMlUserPermissions(capabilities)) {
       return null;
     } else {
       return (
-        <EuiPanel>
-          <BasicTableContainer>
-            {loading && (
-              <>
-                <BackgroundRefetch />
-                <LoadingPanel
-                  height="100%"
-                  width="100%"
-                  text={`${i18n.LOADING} ${i18n.ANOMALIES}`}
-                  position="absolute"
-                  zIndex={3}
-                  data-test-subj="anomalies-host-table-loading-panel"
-                />
-              </>
-            )}
-            <HeaderPanel
-              subtitle={`${i18n.SHOWING}: ${hosts.length.toLocaleString()} ${i18n.ANOMALIES}`}
-              title={i18n.ANOMALIES}
-            />
-            <BasicTable items={hosts} columns={columns} pagination={pagination} sorting={sorting} />
-          </BasicTableContainer>
-        </EuiPanel>
+        <Panel>
+          <HeaderPanel
+            subtitle={`${i18n.SHOWING}: ${hosts.length.toLocaleString()} ${i18n.ANOMALIES}`}
+            title={i18n.ANOMALIES}
+          />
+
+          <BasicTable items={hosts} columns={columns} pagination={pagination} sorting={sorting} />
+
+          {loading && <Loader overlay size="xl" />}
+        </Panel>
       );
     }
   },
   hostEquality
 );
+
+const Panel = styled(EuiPanel)<{ loading?: boolean }>`
+  ${props => css`
+    position: relative;
+
+    ${props.loading &&
+      `
+      overflow: hidden;
+    `}
+  `}
+`;
--- x-pack/legacy/plugins/siem/public/components/ml/tables/anomalies_network_table.tsx
+++ x-pack/legacy/plugins/siem/public/components/ml/tables/anomalies_network_table.tsx
@@ -4,15 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useContext } from 'react';
 import { EuiPanel } from '@elastic/eui';
+import React, { useContext } from 'react';
+import styled, { css } from 'styled-components';
 import { useAnomaliesTableData } from '../anomaly/use_anomalies_table_data';
 import { HeaderPanel } from '../../header_panel';
 
 import * as i18n from './translations';
 import { convertAnomaliesToNetwork } from './convert_anomalies_to_network';
-import { BackgroundRefetch, BasicTableContainer } from '../../load_more_table';
-import { LoadingPanel } from '../../loading';
+import { Loader } from '../../loader';
 import { AnomaliesNetworkTableProps } from '../types';
 import { getAnomaliesNetworkTableColumnsCurated } from './get_anomalies_network_table_columns';
 import { getIntervalFromAnomalies } from '../anomaly/get_interval_from_anomalies';
@@ -56,39 +56,39 @@ export const AnomaliesNetworkTable = React.memo<AnomaliesNetworkTableProps>(
       pageSizeOptions: [5, 10, 20, 50],
       hidePerPageOptions: false,
     };
+
     if (!hasMlUserPermissions(capabilities)) {
       return null;
     } else {
       return (
-        <EuiPanel>
-          <BasicTableContainer>
-            {loading && (
-              <>
-                <BackgroundRefetch />
-                <LoadingPanel
-                  height="100%"
-                  width="100%"
-                  text={`${i18n.LOADING} ${i18n.ANOMALIES}`}
-                  position="absolute"
-                  zIndex={3}
-                  data-test-subj="anomalies-network-table-loading-panel"
-                />
-              </>
-            )}
-            <HeaderPanel
-              subtitle={`${i18n.SHOWING}: ${networks.length.toLocaleString()} ${i18n.ANOMALIES}`}
-              title={i18n.ANOMALIES}
-            />
-            <BasicTable
-              items={networks}
-              columns={columns}
-              pagination={pagination}
-              sorting={sorting}
-            />
-          </BasicTableContainer>
-        </EuiPanel>
+        <Panel>
+          <HeaderPanel
+            subtitle={`${i18n.SHOWING}: ${networks.length.toLocaleString()} ${i18n.ANOMALIES}`}
+            title={i18n.ANOMALIES}
+          />
+
+          <BasicTable
+            items={networks}
+            columns={columns}
+            pagination={pagination}
+            sorting={sorting}
+          />
+
+          {loading && <Loader overlay size="xl" />}
+        </Panel>
       );
     }
   },
   networkEquality
 );
+
+const Panel = styled(EuiPanel)<{ loading?: boolean }>`
+  ${props => css`
+    position: relative;
+
+    ${props.loading &&
+      `
+      overflow: hidden;
+    `}
+  `}
+`;
