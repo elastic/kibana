7bf9c2e7e993e3a9dc3b54609900b76f9bdc8ef9
Jonathan Budzenski <jon@jbudz.me>
2019-07-16 15:19:37 -0500

    backwards compat __dirname

--- x-pack/scripts/jest.js
+++ x-pack/scripts/jest.js
@@ -4,5 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-require('@kbn/plugin-helpers').babelRegister();
-require('../dev-tools/jest').runJest();
+require('../../scripts/jest');
