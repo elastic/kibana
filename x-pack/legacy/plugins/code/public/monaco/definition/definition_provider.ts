1eea24cfb4e1c068f982f2d6225f67f8c6363abd
Yulong <yulong.huang@elastic.co>
2019-07-16 11:52:34 +0800

    [Code] don't register providerDefinition more than once (#41094)

--- x-pack/legacy/plugins/code/public/monaco/definition/definition_provider.ts
+++ x-pack/legacy/plugins/code/public/monaco/definition/definition_provider.ts
@@ -4,40 +4,44 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { DetailSymbolInformation, SymbolLocator } from '@elastic/lsp-extension';
-import { flatten } from 'lodash';
-import { editor, languages } from 'monaco-editor';
+import { DetailSymbolInformation } from '@elastic/lsp-extension';
+
 import { kfetch } from 'ui/kfetch';
 import { Location } from 'vscode-languageserver-types';
+import { monaco } from '../monaco';
 import { LspRestClient, TextDocumentMethods } from '../../../common/lsp_client';
 
-export function provideDefinition(monaco: any, model: editor.ITextModel, position: any) {
-  const lspClient = new LspRestClient('/api/code/lsp');
-  const lspMethods = new TextDocumentMethods(lspClient);
-  function handleLocation(location: Location): languages.Location {
-    return {
-      uri: monaco.Uri.parse(location.uri),
-      range: {
-        startLineNumber: location.range.start.line + 1,
-        startColumn: location.range.start.character + 1,
-        endLineNumber: location.range.end.line + 1,
-        endColumn: location.range.end.character + 1,
-      },
-    };
-  }
-
-  async function handleQname(qname: string) {
-    const res: any = await kfetch({ pathname: `/api/code/lsp/symbol/${qname}` });
-    if (res.symbols) {
-      return res.symbols.map((s: DetailSymbolInformation) =>
-        handleLocation(s.symbolInformation.location)
-      );
+export const definitionProvider: monaco.languages.DefinitionProvider = {
+  async provideDefinition(
+    model: monaco.editor.ITextModel,
+    position: monaco.Position,
+    token: monaco.CancellationToken
+  ): Promise<monaco.languages.Location[]> {
+    const lspClient = new LspRestClient('/api/code/lsp');
+    const lspMethods = new TextDocumentMethods(lspClient);
+    function handleLocation(location: Location): monaco.languages.Location {
+      return {
+        uri: monaco.Uri.parse(location.uri),
+        range: {
+          startLineNumber: location.range.start.line + 1,
+          startColumn: location.range.start.character + 1,
+          endLineNumber: location.range.end.line + 1,
+          endColumn: location.range.end.character + 1,
+        },
+      };
     }
-    return [];
-  }
 
-  return lspMethods.edefinition
-    .send({
+    async function handleQname(qname: string): Promise<monaco.languages.Location[]> {
+      const res: any = await kfetch({ pathname: `/api/code/lsp/symbol/${qname}` });
+      if (res.symbols) {
+        return res.symbols.map((s: DetailSymbolInformation) =>
+          handleLocation(s.symbolInformation.location)
+        );
+      }
+      return [];
+    }
+
+    const result = await lspMethods.edefinition.send({
       position: {
         line: position.lineNumber - 1,
         character: position.column - 1,
@@ -45,24 +49,23 @@ export function provideDefinition(monaco: any, model: editor.ITextModel, positio
       textDocument: {
         uri: model.uri.toString(),
       },
-    })
-    .then(
-      (result: SymbolLocator[]) => {
-        if (result) {
-          const locations = result.filter(l => l.location !== undefined);
-          if (locations.length > 0) {
-            return locations.map(l => handleLocation(l.location!));
-          } else {
-            return Promise.all(
-              result.filter(l => l.qname !== undefined).map(l => handleQname(l.qname!))
-            ).then(flatten);
+    });
+
+    if (result) {
+      const locations = result.filter(l => l.location !== undefined).map(l => l.location!);
+      if (locations.length > 0) {
+        return locations.map(handleLocation);
+      } else {
+        let qnameResults: monaco.languages.Location[] = [];
+        for (const l of result) {
+          if (l.qname) {
+            qnameResults = qnameResults.concat(await handleQname(l.qname));
           }
-        } else {
-          return [];
         }
-      },
-      (_: any) => {
-        return [];
+        return qnameResults;
       }
-    );
-}
+    }
+
+    return [];
+  },
+};
