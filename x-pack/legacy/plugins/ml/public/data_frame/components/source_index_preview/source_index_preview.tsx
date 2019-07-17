c220856272f560c25ad17a724abfb1611f959f8f
Walter Rafelsberger <walter@elastic.co>
2019-07-17 08:36:27 +0200

    [ML] Data frames: Fix source table for cells of type object. (#41234)
    - Fixes a possible crash of the data frames source index table.
    - While the source document field structure was already flattened to avoid displaying nested objects in table cells, this failed to catch if a field itself was of type object, for example geo fields.
    - Similar to how complex arrays were already handled, this adds a check to display a object badge with a tooltip if the cell value is of type object and not suitable to display the raw value.
    - This fixes an issue reported when the full raw object structure of tweets was used as a source index to create a data frame.

--- x-pack/legacy/plugins/ml/public/data_frame/components/source_index_preview/source_index_preview.tsx
+++ x-pack/legacy/plugins/ml/public/data_frame/components/source_index_preview/source_index_preview.tsx
@@ -245,7 +245,37 @@ export const SourceIndexPreview: React.SFC<Props> = React.memo(({ cellClick, que
               }
             )}
           >
-            <EuiBadge>array</EuiBadge>
+            <EuiBadge>
+              {i18n.translate(
+                'xpack.ml.dataframe.sourceIndexPreview.dataFrameSourceIndexArrayBadgeContent',
+                {
+                  defaultMessage: 'array',
+                }
+              )}
+            </EuiBadge>
+          </EuiToolTip>
+        );
+      } else if (typeof d === 'object' && d !== null) {
+        // If the cells data is an object, display a 'object' badge with a
+        // tooltip that explains that this type of field is not supported in this table.
+        return (
+          <EuiToolTip
+            content={i18n.translate(
+              'xpack.ml.dataframe.sourceIndexPreview.dataFrameSourceIndexObjectToolTipContent',
+              {
+                defaultMessage:
+                  'The full content of this object based column is available in the expanded row.',
+              }
+            )}
+          >
+            <EuiBadge>
+              {i18n.translate(
+                'xpack.ml.dataframe.sourceIndexPreview.dataFrameSourceIndexObjectBadgeContent',
+                {
+                  defaultMessage: 'object',
+                }
+              )}
+            </EuiBadge>
           </EuiToolTip>
         );
       }
