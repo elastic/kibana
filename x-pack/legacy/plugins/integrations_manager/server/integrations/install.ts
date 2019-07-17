de514e6ec3d87991ad419eb84b1424f2b14513d4
John Schulz <john.schulz@elastic.co>
2019-07-17 15:38:48 -0400

    Move getObjects (and related functions/types) to a separate file.
    40% of install.ts was for this one behavior. Moved elsewhere to make both files easier to scan.
    
    Did this in two separate commits to isolate the behavior change from the file structure change.

--- x-pack/legacy/plugins/integrations_manager/server/integrations/install.ts
+++ x-pack/legacy/plugins/integrations_manager/server/integrations/install.ts
@@ -9,9 +9,7 @@ import { SAVED_OBJECT_TYPE, ASSET_TYPE_INGEST_PIPELINE } from '../../common/cons
 import { AssetReference, AssetType, InstallationAttributes } from '../../common/types';
 import * as Registry from '../registry';
 import { CallESAsCurrentUser, assetUsesObjects, getInstallationObject } from './index';
-
-type ArchiveAsset = Pick<SavedObject, 'attributes' | 'migrationVersion' | 'references'>;
-type SavedObjectToBe = Required<SavedObjectsBulkCreateObject>;
+import { getObjects } from './get_objects';
 
 export async function installIntegration(options: {
   savedObjectsClient: SavedObjectsClientContract;
@@ -135,84 +133,4 @@ function toAssetReference({ id, type }: SavedObject) {
   const reference: AssetReference = { id, type };
 
   return reference;
-}
-
-async function getObjects(
-  pkgkey: string,
-  filter = (entry: Registry.ArchiveEntry): boolean => true
-): Promise<SavedObjectToBe[]> {
-  // Create a Map b/c some values, especially index-patterns, are referenced multiple times
-  const objects: Map<string, SavedObjectToBe> = new Map();
-
-  // Get paths which match the given filter
-  const paths = await Registry.getArchiveInfo(pkgkey, filter);
-
-  // Get all objects which matched filter. Add them to the Map
-  const rootObjects = paths.map(getObject);
-  rootObjects.forEach(obj => objects.set(obj.id, obj));
-
-  // Each of those objects might have `references` property like [{id, type, name}]
-  for (const object of rootObjects) {
-    // For each of those objects, if they have references
-    for (const reference of object.references) {
-      // Get the referenced objects. Call same function with a new filter
-      const referencedObjects = await getObjects(pkgkey, (entry: Registry.ArchiveEntry) => {
-        // Skip anything we've already stored
-        if (objects.has(reference.id)) return false;
-
-        // Is the archive entry the reference we want?
-        const { type, file } = Registry.pathParts(entry.path);
-        const isType = type === reference.type;
-        const isJson = file === `${reference.id}.json`;
-
-        return isType && isJson;
-      });
-
-      // Add referenced objects to the Map
-      referencedObjects.forEach(ro => objects.set(ro.id, ro));
-    }
-  }
-
-  // return the array of unique objects
-  return Array.from(objects.values());
-}
-
-// the assets from the registry are malformed
-// https://github.com/elastic/integrations-registry/issues/42
-function ensureJsonValues(obj: SavedObjectToBe) {
-  const { attributes } = obj;
-  if (
-    attributes.kibanaSavedObjectMeta &&
-    typeof attributes.kibanaSavedObjectMeta.searchSourceJSON !== 'string'
-  ) {
-    attributes.kibanaSavedObjectMeta.searchSourceJSON = JSON.stringify(
-      attributes.kibanaSavedObjectMeta.searchSourceJSON
-    );
-  }
-
-  ['optionsJSON', 'panelsJSON', 'uiStateJSON', 'visState']
-    .filter(key => typeof attributes[key] !== 'string')
-    .forEach(key => (attributes[key] = JSON.stringify(attributes[key])));
-
-  return obj;
-}
-
-function getObject(key: string) {
-  const buffer = Registry.getAsset(key);
-
-  // cache values are buffers. convert to string / JSON
-  const json = buffer.toString('utf8');
-  // convert that to an object & address issues with the formatting of some parts
-  const asset: ArchiveAsset = ensureJsonValues(JSON.parse(json));
-
-  const { type, file } = Registry.pathParts(key);
-  const savedObject: SavedObjectToBe = {
-    type,
-    id: file.replace('.json', ''),
-    attributes: asset.attributes,
-    references: asset.references || [],
-    migrationVersion: asset.migrationVersion || {},
-  };
-
-  return savedObject;
 }
