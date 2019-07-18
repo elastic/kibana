90819c2e995a22db66b2dd4f778acf50813a7ab2
wangqianliang <wangqianliang@live.com>
2019-07-18 21:48:28 +0800

    fix(code/frontend): fix file tree folder collapse

--- x-pack/legacy/plugins/code/public/sagas/route.ts
+++ x-pack/legacy/plugins/code/public/sagas/route.ts
@@ -4,12 +4,32 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { put, takeEvery, select } from 'redux-saga/effects';
+import { put, takeEvery, select, takeLatest } from 'redux-saga/effects';
 import { Action } from 'redux-actions';
-import { routeChange, Match, loadRepo, fetchRepoBranches } from '../actions';
-import { previousMatchSelector } from '../selectors';
+import {
+  routeChange,
+  Match,
+  loadRepo,
+  fetchRepoBranches,
+  fetchRepoCommits,
+  fetchRootRepoTree,
+} from '../actions';
+import { previousMatchSelector, repoUriSelector, revisionSelector } from '../selectors';
 import { routePathChange, repoChange, revisionChange, filePathChange } from '../actions/route';
 import * as ROUTES from '../components/routes';
+
+const MAIN_ROUTES = [ROUTES.MAIN, ROUTES.MAIN_ROOT];
+
+function* handleRepoOrRevisionChange() {
+  const repoUri = yield select(repoUriSelector);
+  const revision = yield select(revisionSelector);
+  yield put(fetchRepoCommits({ uri: repoUri, revision }));
+  yield put(fetchRootRepoTree({ uri: repoUri, revision }));
+}
+
+export function* watchRepoOrRevisionChange() {
+  yield takeLatest([String(repoChange), String(revisionChange)], handleRepoOrRevisionChange);
+}
 
 const getRepoFromMatch = (match: Match) =>
   `${match.params.resource}/${match.params.org}/${match.params.repo}`;
@@ -17,7 +37,10 @@ const getRepoFromMatch = (match: Match) =>
 function* handleRoute(action: Action<any>) {
   const currentMatch = action.payload;
   const previousMatch = yield select(previousMatchSelector);
-  if (currentMatch.path !== previousMatch.path) {
+  if (
+    currentMatch.path !== previousMatch.path &&
+    !(MAIN_ROUTES.includes(currentMatch.path) && MAIN_ROUTES.includes(previousMatch.path))
+  ) {
     yield put(routePathChange());
     if (currentMatch.path === ROUTES.MAIN) {
       const currentRepo = getRepoFromMatch(currentMatch);
