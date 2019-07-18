ab822ae933ae39b52998e793e9a3b06084330e5a
Artyom Gospodarsky <art.gospodarsky@gmail.com>
2019-07-18 12:59:33 +0300

    [TSVB] Introducing Timerange Data Mode for Metric Style Visualizations (#37185)
    * Add Time Range input to Panel on TSVB
    
    * Disable YesNo Component
    
    * Remove redundant translations
    
    * add timerange_data_modes
    
    * Fix reviews
    
    * BE part
    
    * add new ui restriction
    
    * add new ui restriction - ui part
    
    * override index pattern issue
    
    * Take TIME_RANGE_MODE_KEY out
    
    * Make TimeRange more stable
    
    * Small improvements
    
    * Small improvements - BE part
    
    * Small improvements - BE part
    
    * Small improvements - BE part
    
    * Bind uiRestrictions to UI of the Time Range
    
    * Get rid of context consumers
    
    * Remove console.log
    
    * Add translation and refactor a little
    
    * Pretty Panel config

--- src/legacy/core_plugins/metrics/common/timerange_data_modes.js
+++ src/legacy/core_plugins/metrics/common/timerange_data_modes.js
@@ -17,19 +17,28 @@
  * under the License.
  */
 
-import { dropLastBucket } from '../series/drop_last_bucket';
-import { isLastValueTimerangeMode } from '../../helpers/get_timerange_mode';
+/**
+ * Time Range data modes.
+ * @constant
+ * @public
+ */
+export const TIME_RANGE_DATA_MODES = {
+  /**
+   * Entire timerange mode will match all the documents selected in the
+   * timerange timepicker
+   */
+  ENTIRE_TIME_RANGE: 'entire_time_range',
 
-export function dropLastBucketFn(bucket, panel, series) {
-  return next => results => {
-    const shouldDropLastBucket = isLastValueTimerangeMode(panel);
+  /**
+   * Last value mode will match only the documents for the specified interval
+   * from the end of the timerange.
+   */
+  LAST_VALUE: 'last_value',
+};
 
-    if (shouldDropLastBucket) {
-      const fn = dropLastBucket({ aggregations: bucket }, panel, series);
-
-      return fn(next)(results);
-    }
-
-    return next(results);
-  };
-}
+/**
+ * Key for getting the Time Range mode from the Panel configuration object.
+ * @constant
+ * @public
+ */
+export const TIME_RANGE_MODE_KEY = 'time_range_mode';
