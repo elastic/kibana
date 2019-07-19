67e55aee48d003cab7fbee66a1045cf1d1f8e3fa
Michael Marcialis <michael.marcialis@elastic.co>
2019-07-19 16:38:08 -0400

    correct unused or misnamed translations

--- x-pack/legacy/plugins/siem/public/components/load_more_table/translations.ts
+++ x-pack/legacy/plugins/siem/public/components/load_more_table/translations.ts
@@ -6,18 +6,18 @@
 
 import { i18n } from '@kbn/i18n';
 
-export const LOADING = i18n.translate('xpack.siem.loadingMoreTable.loadingDescription', {
+export const LOADING = i18n.translate('xpack.siem.loadMoreTable.loading', {
   defaultMessage: 'Loading…',
 });
 
-export const LOAD_MORE = i18n.translate('xpack.siem.loadingMoreTable.loadMoreDescription', {
+export const LOAD_MORE = i18n.translate('xpack.siem.loadMoreTable.loadMore', {
   defaultMessage: 'Load more',
 });
 
-export const SHOWING = i18n.translate('xpack.siem.loadingMoreTable.showing', {
+export const SHOWING = i18n.translate('xpack.siem.loadMoreTable.showing', {
   defaultMessage: 'Showing',
 });
 
-export const ROWS = i18n.translate('xpack.siem.loadingMoreTable.rows', {
+export const ROWS = i18n.translate('xpack.siem.loadMoreTable.rows', {
   defaultMessage: 'Rows per page',
 });
--- x-pack/legacy/plugins/siem/public/components/paginated_table/translations.ts
+++ x-pack/legacy/plugins/siem/public/components/paginated_table/translations.ts
@@ -6,26 +6,18 @@
 
 import { i18n } from '@kbn/i18n';
 
-export const LOADING = i18n.translate('xpack.siem.loadingMoreTable.loadingDescription', {
-  defaultMessage: 'Loading…',
-});
-
-export const LOAD_MORE = i18n.translate('xpack.siem.loadingMoreTable.loadMoreDescription', {
-  defaultMessage: 'Load More',
-});
-
-export const SHOWING = i18n.translate('xpack.siem.loadingMoreTable.showing', {
+export const SHOWING = i18n.translate('xpack.siem.paginatedTable.showing', {
   defaultMessage: 'Showing',
 });
 
-export const ROWS = i18n.translate('xpack.siem.loadingMoreTable.rows', {
+export const ROWS = i18n.translate('xpack.siem.paginatedTable.rows', {
   defaultMessage: 'Rows per page',
 });
 
-export const TOAST_TITLE = i18n.translate('xpack.siem.unableToLoadMoreResults.title', {
+export const TOAST_TITLE = i18n.translate('xpack.siem.paginatedTable.toastTitle', {
   defaultMessage: ' - too many results',
 });
 
-export const TOAST_TEXT = i18n.translate('xpack.siem.unableToLoadMoreResults.text', {
+export const TOAST_TEXT = i18n.translate('xpack.siem.paginatedTable.toastText', {
   defaultMessage: 'Narrow your query to better filter the results',
 });
