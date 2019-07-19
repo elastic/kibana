d008475786d1be625b61cb41a6a055714ca37202
stephmilovic <stephanie.milovic@elastic.co>
2019-07-19 14:44:03 -0600

    working on clearing errs

--- x-pack/legacy/plugins/siem/public/components/open_timeline/timelines_table/index.tsx
+++ x-pack/legacy/plugins/siem/public/components/open_timeline/timelines_table/index.tsx
@@ -145,6 +145,17 @@ export const TimelinesTable = pure<TimelinesTableProps>(
       onSelectionChange,
     };
 
+    console.log(
+      'getTimelinesTableColumns',
+      getTimelinesTableColumns({
+        deleteTimelines,
+        itemIdToExpandedNotesRowMap,
+        onOpenTimeline,
+        onToggleShowNotes,
+        showExtendedColumnsAndActions,
+      })[8]
+    );
+
     return (
       <TimelinesTableContainer data-test-subj="timelines-table-container">
         <EuiBasicTable
