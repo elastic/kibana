a5a2e5aab72559648d94679cd8ad72ca361d8edb
Thomas Neirynck <thomas@elastic.co>
2019-07-19 15:03:39 -0400

    [Maps] Coerce term join values to string (#41503)

--- x-pack/legacy/plugins/maps/public/shared/layers/joins/left_inner_join.test.js
+++ x-pack/legacy/plugins/maps/public/shared/layers/joins/left_inner_join.test.js
@@ -18,18 +18,21 @@ jest.mock('ui/timefilter/timefilter', () => {});
 jest.mock('../vector_layer', () => {});
 
 
+const rightSource = {
+  id: 'd3625663-5b34-4d50-a784-0d743f676a0c',
+  indexPatternId: '90943e30-9a47-11e8-b64d-95841ca0b247',
+  indexPatternTitle: 'kibana_sample_data_logs',
+  term: 'geo.dest',
+};
+
 const leftJoin = new LeftInnerJoin({
   leftField: 'iso2',
-  right: {
-    id: 'd3625663-5b34-4d50-a784-0d743f676a0c',
-    indexPatternId: '90943e30-9a47-11e8-b64d-95841ca0b247',
-    indexPatternTitle: 'kibana_sample_data_logs',
-    term: 'geo.dest',
-  }
+  right: rightSource
 });
+const COUNT_PROPERTY_NAME = '__kbnjoin__count_groupby_kibana_sample_data_logs.geo.dest';
 
 describe('joinPropertiesToFeature', () => {
-  const COUNT_PROPERTY_NAME = '__kbnjoin__count_groupby_kibana_sample_data_logs.geo.dest';
+
 
   it('Should add join property to features in feature collection', () => {
     const feature = {
@@ -67,4 +70,73 @@ describe('joinPropertiesToFeature', () => {
       iso2: 'CN',
     });
   });
+
+  it('Should coerce to string before joining', () => {
+
+    const leftJoin = new LeftInnerJoin({
+      leftField: 'zipcode',
+      right: rightSource
+    });
+
+    const feature = {
+      properties: {
+        zipcode: 40204
+      }
+    };
+    const propertiesMap = new Map();
+    propertiesMap.set('40204', { [COUNT_PROPERTY_NAME]: 61 });
+
+    leftJoin.joinPropertiesToFeature(feature, propertiesMap, [{
+      propertyKey: COUNT_PROPERTY_NAME
+    }]);
+    expect(feature.properties).toEqual({
+      zipcode: 40204,
+      [COUNT_PROPERTY_NAME]: 61,
+    });
+  });
+
+  it('Should handle undefined values', () => {
+
+    const feature = {//this feature does not have the iso2 field
+      properties: {
+        zipcode: 40204
+      }
+    };
+    const propertiesMap = new Map();
+    propertiesMap.set('40204', { [COUNT_PROPERTY_NAME]: 61 });
+
+    leftJoin.joinPropertiesToFeature(feature, propertiesMap, [{
+      propertyKey: COUNT_PROPERTY_NAME
+    }]);
+    expect(feature.properties).toEqual({
+      zipcode: 40204
+    });
+  });
+
+  it('Should handle falsy values', () => {
+
+    const leftJoin = new LeftInnerJoin({
+      leftField: 'code',
+      right: rightSource
+    });
+
+    const feature = {
+      properties: {
+        code: 0
+      }
+    }
+    ;
+    const propertiesMap = new Map();
+    propertiesMap.set('0', { [COUNT_PROPERTY_NAME]: 61 });
+
+    leftJoin.joinPropertiesToFeature(feature, propertiesMap, [{
+      propertyKey: COUNT_PROPERTY_NAME
+    }]);
+    expect(feature.properties).toEqual({
+      code: 0,
+      [COUNT_PROPERTY_NAME]: 61
+    });
+  });
+
+
 });
