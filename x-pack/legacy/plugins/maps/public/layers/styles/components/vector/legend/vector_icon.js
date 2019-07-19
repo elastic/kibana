284ffd12829ab99f94bdebcd7cc38902760c6041
Nathan Reese <reese.nathan@gmail.com>
2019-07-19 13:56:01 -0600

    reword comment

--- x-pack/legacy/plugins/maps/public/layers/styles/components/vector/legend/vector_icon.js
+++ x-pack/legacy/plugins/maps/public/layers/styles/components/vector/legend/vector_icon.js
@@ -89,7 +89,7 @@ function extractColorFromStyleProperty(colorStyleProperty, defaultColor) {
     return colorStyleProperty.options.color;
   }
 
-  // Do not showing dynamic color unless configuration is complete
+  // Do not use dynamic color unless configuration is complete
   if (!colorStyleProperty.options.field || !colorStyleProperty.options.field.name) {
     return defaultColor;
   }
