efcb1182c92b12a1d4ba2c8aa90340d44574a9b7
Sébastien <sebastien.loix@elastic.co>
2019-07-19 10:23:34 +0200

    Initial commit

---
+++ src/plugins/elasticsearch_ui_shared/static/forms/hook_form_lib/hooks/use_field.ts
@@ -0,0 +1,427 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { useState, useEffect, useRef } from 'react';
+
+import {
+  Form,
+  Field,
+  FieldConfig,
+  FieldValidateResponse,
+  ValidationConfig,
+  ValidationError,
+} from '../types';
+import { fieldFormatters } from '../lib';
+import { FIELD_TYPES, VALIDATION_TYPES } from '../constants';
+
+const { toInt } = fieldFormatters;
+
+/**
+ * Helpers to decide which message to output from a validation.
+ *
+ * A default message can be declared on our validators, but this message can be overriden
+ * in the configuration of the field "validations".
+ * A message can _also_ be a function that receives the error being thrown.
+ *
+ * @param validation The validation being executed
+ * @param validationResult The validation result
+ */
+const getValidationErrorWithMessage = (
+  validation: Partial<ValidationConfig>,
+  validationResult: ValidationError
+) => {
+  const message =
+    typeof validation.message !== 'undefined' ? validation.message : validationResult.message;
+
+  return {
+    ...validationResult,
+    message: typeof message === 'function' ? message(validationResult) : message,
+  };
+};
+
+export const useField = (form: Form, path: string, config: FieldConfig = {}) => {
+  const {
+    defaultValue = '',
+    label = '',
+    helpText = '',
+    type = FIELD_TYPES.TEXT,
+    validations = [],
+    formatters = [],
+    fieldsToValidateOnChange = [path],
+    isValidationAsync = false,
+    errorDisplayDelay = form.options.errorDisplayDelay,
+  } = config;
+
+  const {
+    serializer = (value: unknown) => value,
+    deSerializer = (value: unknown) => value,
+  } = config;
+
+  const [value, setStateValue] = useState(
+    typeof defaultValue === 'function' ? deSerializer(defaultValue()) : deSerializer(defaultValue)
+  );
+  const [errors, setErrors] = useState<ValidationError[]>([]);
+  const [isPristine, setPristine] = useState(true);
+  const [isValidating, setValidating] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const validateCounter = useRef(0);
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const setDefaultFormatter = () => {
+    if (formatters.length > 0) {
+      return;
+    }
+
+    if (type === FIELD_TYPES.NUMBER) {
+      formatters.push(toInt);
+    }
+  };
+
+  // -- INIT
+  // ----------------------------------
+  setDefaultFormatter();
+
+  // -- HELPERS
+  // ----------------------------------
+
+  /**
+   * Filter an array of errors with specific validation type on them
+   *
+   * @param _errors The array of errors to filter
+   * @param validationType The validation type to filter out
+   */
+  const filterErrors = (
+    _errors: ValidationError[],
+    validationTypeToFilterOut: string | string[] = VALIDATION_TYPES.FIELD
+  ): ValidationError[] => {
+    const validationTypeToArray = Array.isArray(validationTypeToFilterOut)
+      ? (validationTypeToFilterOut as string[])
+      : ([validationTypeToFilterOut] as string[]);
+
+    return _errors.filter(error =>
+      validationTypeToArray.every(_type => error.validationType !== _type)
+    );
+  };
+
+  const runFormatters = (input: unknown): unknown => {
+    const isEmptyString = typeof input === 'string' && input.trim() === '';
+
+    if (isEmptyString) {
+      return input;
+    }
+    return formatters.reduce((output, formatter) => formatter(output), input);
+  };
+
+  const onValueChange = () => {
+    if (isPristine) {
+      setPristine(false);
+    }
+    setIsUpdating(true);
+
+    if (debounceTimeout.current) {
+      clearTimeout(debounceTimeout.current);
+    }
+
+    debounceTimeout.current = setTimeout(() => {
+      setIsUpdating(false);
+    }, errorDisplayDelay);
+  };
+
+  const validateSync = ({
+    formData,
+    value: valueToValidate,
+    validationTypeToValidate,
+  }: {
+    formData: any;
+    value: unknown;
+    validationTypeToValidate?: string;
+  }): ValidationError[] => {
+    const validationErrors: ValidationError[] = [];
+    let skip = false;
+
+    const runValidation = ({
+      validator,
+      exitOnFail,
+      type: validationType = VALIDATION_TYPES.FIELD,
+    }: ValidationConfig) => {
+      if (
+        skip ||
+        (typeof validationTypeToValidate !== 'undefined' &&
+          validationType !== validationTypeToValidate)
+      ) {
+        return;
+      }
+      let validationResult;
+
+      try {
+        validationResult = validator({
+          value: (valueToValidate as unknown) as string,
+          errors: validationErrors,
+          formData,
+          path,
+        });
+
+        if (validationResult && exitOnFail !== false) {
+          throw validationResult;
+        }
+      } catch (error) {
+        // If an error is thrown, skip the rest of the validations
+        skip = true;
+        validationResult = error;
+      }
+
+      return validationResult;
+    };
+
+    // Execute each validations for the field sequencially
+    validations.forEach(validation => {
+      const validationResult = runValidation(validation);
+
+      if (validationResult) {
+        const error = getValidationErrorWithMessage(validation, validationResult);
+        validationErrors.push({
+          ...error,
+          validationType: validation.type || VALIDATION_TYPES.FIELD,
+        });
+      }
+    });
+
+    return validationErrors;
+  };
+
+  const validateAsync = async ({
+    formData,
+    value: valueToValidate,
+    validationTypeToValidate,
+  }: {
+    formData: any;
+    value: unknown;
+    validationTypeToValidate?: string;
+  }): Promise<ValidationError[]> => {
+    const validationErrors: ValidationError[] = [];
+    let skip = false;
+
+    // By default, for fields that have an asynchronous validation
+    // we will clear the errors as soon as the field value changes.
+    clearErrors([VALIDATION_TYPES.FIELD, VALIDATION_TYPES.ASYNC]);
+
+    const runValidation = async ({
+      validator,
+      exitOnFail,
+      type: validationType = VALIDATION_TYPES.FIELD,
+    }: ValidationConfig) => {
+      if (
+        skip ||
+        (typeof validationTypeToValidate !== 'undefined' &&
+          validationType !== validationTypeToValidate)
+      ) {
+        return;
+      }
+      let validationResult;
+
+      try {
+        validationResult = await validator({
+          value: (valueToValidate as unknown) as string,
+          errors: validationErrors,
+          formData,
+          path,
+        });
+
+        if (validationResult && exitOnFail !== false) {
+          throw validationResult;
+        }
+      } catch (error) {
+        // If an error is thrown, skip the rest of the validations
+        skip = true;
+        validationResult = error;
+      }
+
+      return validationResult;
+    };
+
+    // Sequencially execute all the validations for the field
+    await validations.reduce(
+      (promise, validation) =>
+        promise.then(async () => {
+          const validationResult = await runValidation(validation);
+
+          if (validationResult) {
+            const error = getValidationErrorWithMessage(validation, validationResult);
+            validationErrors.push({
+              ...error,
+              validationType: validation.type || VALIDATION_TYPES.FIELD,
+            });
+          }
+        }),
+      Promise.resolve()
+    );
+
+    return validationErrors;
+  };
+
+  // -- API
+  // ----------------------------------
+  const clearErrors: Field['clearErrors'] = (validationType = VALIDATION_TYPES.FIELD) => {
+    setErrors(previousErrors => filterErrors(previousErrors, validationType));
+  };
+
+  /**
+   * Validate a form field, running all its validations.
+   * If a validationType is provided then only that validation will be executed,
+   * skipping the other type of validation that might exist.
+   */
+  const validate: Field['validate'] = (validationData = {}) => {
+    const {
+      formData = form.__getFormData({ unflatten: false }),
+      value: valueToValidate = value,
+      validationType,
+    } = validationData;
+
+    setValidating(true);
+
+    // By the time our validate function has reached completion, it’s possible
+    // that validate() will have been called again. If this is the case, we need
+    // to ignore the results of this invocation and only use the results of
+    // the most recent invocation to update the error state for a field
+    const validateIteration = ++validateCounter.current;
+
+    const onValidationResult = (validationErrors: ValidationError[]): FieldValidateResponse => {
+      if (validateIteration === validateCounter.current) {
+        // This is the most recent invocation
+        setValidating(false);
+        // Update the errors array
+        setErrors(previousErrors => {
+          // First filter out the validation type we are currently validating
+          const filteredErrors = filterErrors(previousErrors, validationType);
+          return [...filteredErrors, ...validationErrors];
+        });
+      }
+      return {
+        isValid: validationErrors.length === 0,
+        errors: validationErrors,
+      };
+    };
+
+    if (isValidationAsync) {
+      return validateAsync({
+        formData,
+        value: valueToValidate,
+        validationTypeToValidate: validationType,
+      }).then(onValidationResult);
+    } else {
+      const validationErrors = validateSync({
+        formData,
+        value: valueToValidate,
+        validationTypeToValidate: validationType,
+      });
+      return onValidationResult(validationErrors);
+    }
+  };
+
+  /**
+   * Handler to change the field value
+   *
+   * @param newValue The new value to assign to the field
+   */
+  const setValue: Field['setValue'] = newValue => {
+    onValueChange();
+
+    const formattedValue = runFormatters(newValue);
+    setStateValue(formattedValue);
+
+    // Update the form data observable
+    form.__updateFormDataAt(path, getOutputValue(formattedValue));
+  };
+
+  /**
+   * Form <input /> "onChange" event handler
+   *
+   * @param event Form input change event
+   */
+  const onChange: Field['onChange'] = event => {
+    const newValue = {}.hasOwnProperty.call(event!.target, 'checked')
+      ? event.target.checked
+      : event.target.value;
+
+    setValue(newValue);
+  };
+
+  const getOutputValue: Field['getOutputValue'] = (rawValue = value) => serializer(rawValue);
+
+  /**
+   * As we can have multiple validation types (FIELD, ASYNC, ARRAY_ITEM), this
+   * method allows us to retrieve error messages for certain types of validation.
+   *
+   * For example, if we want to validation error messages to be displayed when the user clicks the "save" button
+   * _but_ in caase of an asynchronous validation (for ex. an HTTP request that would validate an index name) we
+   * want to immediately display the error message, we would have 2 types of validation: FIELD & ASYNC
+   *
+   * @param validationType The validation type to return error messages from
+   */
+  const getErrorsMessages: Field['getErrorsMessages'] = (
+    validationType = VALIDATION_TYPES.FIELD
+  ) => {
+    const errorMessages = errors.reduce((messages, error) => {
+      if (
+        error.validationType === validationType ||
+        (validationType === VALIDATION_TYPES.FIELD &&
+          !{}.hasOwnProperty.call(error, 'validationType'))
+      ) {
+        return messages ? `${messages}, ${error.message}` : (error.message as string);
+      }
+      return messages;
+    }, '');
+
+    return errorMessages ? errorMessages : null;
+  };
+
+  // -- EFFECTS
+  // ----------------------------------
+  useEffect(() => {
+    if (isPristine) {
+      // Avoid validate on mount
+      return;
+    }
+    form.__validateFields(fieldsToValidateOnChange);
+  }, [value]);
+
+  const field: Field = {
+    path,
+    label,
+    helpText,
+    value,
+    errors,
+    type,
+    form,
+    isPristine,
+    isValidating,
+    isUpdating,
+    validate,
+    setErrors,
+    clearErrors,
+    setValue,
+    onChange,
+    getOutputValue,
+    getErrorsMessages,
+  };
+
+  form.__addField(field);
+
+  return field;
+};
---
+++ src/plugins/elasticsearch_ui_shared/static/forms/hook_form_lib/hooks/use_form.ts
@@ -0,0 +1,212 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { useState, useRef } from 'react';
+
+import { Form, FormData, FieldConfig, FieldsMap, FormConfig } from '../types';
+import { getAt, mapFormFields, unflattenObject, Subject } from '../lib';
+
+const DEFAULT_ERROR_DISPLAY_TIMEOUT = 500;
+
+export const useForm = <T = FormData>({
+  onSubmit,
+  schema,
+  defaultValue = {},
+  serializer = (data: any) => data,
+  deSerializer = (data: any) => data,
+  options = { errorDisplayDelay: DEFAULT_ERROR_DISPLAY_TIMEOUT, stripEmptyFields: true },
+}: FormConfig<T>): { form: Form<T> } => {
+  const defaultValueDeSerialized =
+    Object.keys(defaultValue).length === 0 ? defaultValue : deSerializer(defaultValue);
+  const [isSubmitted, setSubmitted] = useState(false);
+  const [isSubmitting, setSubmitting] = useState(false);
+  const [isValid, setIsValid] = useState(true);
+  const fieldsRefs = useRef<FieldsMap>({});
+
+  // formData$ is an observable we can subscribe to in order to receive live
+  // update of the raw form data. As an observable it does not trigger any React
+  // render().
+  // The <FormDataProvider> component is the one in charge of reading this observable
+  // and updating its state to trigger the necessary view render.
+  const formData$ = useRef<Subject<T>>(new Subject<T>({} as T));
+
+  // -- HELPERS
+  // ----------------------------------
+  const fieldsToArray = () => Object.values(fieldsRefs.current);
+
+  const stripEmptyFields = (fields: FieldsMap): FieldsMap => {
+    if (options.stripEmptyFields) {
+      return Object.entries(fields).reduce(
+        (acc, [key, field]) => {
+          if (field.value !== '') {
+            acc[key] = field;
+          }
+          return acc;
+        },
+        {} as FieldsMap
+      );
+    }
+    return fields;
+  };
+
+  // -- API
+  // ----------------------------------
+  const getFormData: Form<T>['__getFormData'] = (getDataOptions = { unflatten: true }) =>
+    getDataOptions.unflatten
+      ? (unflattenObject(
+          mapFormFields(stripEmptyFields(fieldsRefs.current), field => field.getOutputValue())
+        ) as T)
+      : Object.entries(fieldsRefs.current).reduce(
+          (acc, [key, field]) => ({
+            ...acc,
+            [key]: field.getOutputValue(),
+          }),
+          {} as T
+        );
+
+  const updateFormDataAt: Form<T>['__updateFormDataAt'] = (path, value) => {
+    const currentFormData = formData$.current.value;
+    formData$.current.next({ ...currentFormData, [path]: value });
+    return formData$.current.value;
+  };
+
+  const validateFields: Form<T>['__validateFields'] = async fieldNames => {
+    const fieldsToValidate = fieldNames
+      ? fieldNames.map(name => fieldsRefs.current[name]).filter(field => field !== undefined)
+      : fieldsToArray().filter(field => field.isPristine);
+
+    const formData = getFormData({ unflatten: false });
+
+    await Promise.all(fieldsToValidate.map(field => field.validate({ formData })));
+
+    const isFormValid = fieldsToArray().every(
+      field => field.getErrorsMessages() === null && !field.isValidating
+    );
+    setIsValid(isFormValid);
+
+    return isFormValid;
+  };
+
+  const addField: Form<T>['__addField'] = field => {
+    fieldsRefs.current[field.path] = field;
+
+    // Only update the formData if the path does not exist (= it is the _first_ time
+    // the field is added), to avoid entering an infinite loop when the form is re-rendered.
+    if (!{}.hasOwnProperty.call(formData$.current.value, field.path)) {
+      updateFormDataAt(field.path, field.getOutputValue());
+    }
+  };
+
+  const removeField: Form<T>['__removeField'] = _fieldNames => {
+    const fieldNames = Array.isArray(_fieldNames) ? _fieldNames : [_fieldNames];
+    const currentFormData = { ...formData$.current.value } as FormData;
+
+    fieldNames.forEach(name => {
+      delete fieldsRefs.current[name];
+      delete currentFormData[name];
+    });
+    formData$.current.next(currentFormData as T);
+
+    // Wait next tick to make sure all the fields have their
+    // values updated after the DOM has been updated
+    setTimeout(() => {
+      validateFields();
+    });
+  };
+
+  /**
+   * Remove all fields whose path starts with the pattern provided.
+   * Usefull for removing all the elements of an Array
+   * for example (e.g pattern = "colors." => "colors.0, colors.1" would be removed.)
+   *
+   * @param pattern The path pattern to match
+   */
+  const removeFieldsStartingWith: Form<T>['__removeFieldsStartingWith'] = pattern => {
+    Object.keys(fieldsRefs.current).forEach(key => {
+      if (key.startsWith(pattern)) {
+        delete fieldsRefs.current[key];
+      }
+    });
+
+    // As removing a field implies that it is also removed from the DOM
+    // we need to first wait for the the DOM to be updated _before_ validating the form fields.
+    setTimeout(() => {
+      validateFields();
+    });
+  };
+  const setFieldValue: Form<T>['setFieldValue'] = (fieldName, value) => {
+    fieldsRefs.current[fieldName].setValue(value);
+  };
+
+  const setFieldErrors: Form<T>['setFieldErrors'] = (fieldName, errors) => {
+    fieldsRefs.current[fieldName].setErrors(errors);
+  };
+
+  const getFieldDefaultValue: Form['__getFieldDefaultValue'] = fieldName =>
+    getAt(fieldName, defaultValueDeSerialized, false);
+
+  const readFieldConfigFromSchema: Form<T>['__readFieldConfigFromSchema'] = fieldName => {
+    const config = (getAt(fieldName, schema ? schema : {}, false) as FieldConfig) || {};
+
+    return config;
+  };
+
+  const onSubmitForm: Form<T>['onSubmit'] = async e => {
+    if (e) {
+      e.preventDefault();
+    }
+
+    setSubmitting(true);
+    setSubmitted(true); // User has attempted to submit the form at least once
+
+    const isFormValid = await validateFields();
+    const formData = serializer(getFormData() as T);
+
+    if (onSubmit) {
+      await onSubmit(formData, isFormValid);
+    }
+
+    setSubmitting(false);
+
+    return { data: formData, isValid: isFormValid };
+  };
+
+  const form: Form<T> = {
+    isSubmitted,
+    isSubmitting,
+    isValid,
+    options,
+    onSubmit: onSubmitForm,
+    setFieldValue,
+    setFieldErrors,
+    __formData$: formData$,
+    __getFormData: getFormData,
+    __updateFormDataAt: updateFormDataAt,
+    __readFieldConfigFromSchema: readFieldConfigFromSchema,
+    __addField: addField,
+    __removeField: removeField,
+    __validateFields: validateFields,
+    __removeFieldsStartingWith: removeFieldsStartingWith,
+    __getFieldDefaultValue: getFieldDefaultValue,
+  };
+
+  return {
+    form,
+  };
+};
