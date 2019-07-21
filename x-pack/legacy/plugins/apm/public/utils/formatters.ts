255ea61610f4a8ffe9a6b8e53b7dd5ec2ff267d0
Bart Van Remortele <bartvanremortele@gmail.com>
2019-07-21 16:57:02 +0200

    Add time formatting support for very high durations (minutes and hours) to x-pack

--- x-pack/legacy/plugins/apm/public/utils/formatters.ts
+++ x-pack/legacy/plugins/apm/public/utils/formatters.ts
@@ -9,6 +9,8 @@ import { i18n } from '@kbn/i18n';
 import { memoize } from 'lodash';
 import { NOT_AVAILABLE_LABEL } from '../../common/i18n';
 
+const HOURS_CUT_OFF = 3600000000; // 1 hour (in microseconds)
+const MINUTES_CUT_OFF = 60000000; // 1 minute (in microseconds)
 const SECONDS_CUT_OFF = 10 * 1000000; // 10 seconds (in microseconds)
 const MILLISECONDS_CUT_OFF = 10 * 1000; // 10 milliseconds (in microseconds)
 const SPACE = ' ';
@@ -22,6 +24,38 @@ type FormatterValue = number | undefined | null;
 interface FormatterOptions {
   withUnit?: boolean;
   defaultValue?: string;
+}
+
+export function asHours(
+  value: FormatterValue,
+  { withUnit = true, defaultValue = NOT_AVAILABLE_LABEL }: FormatterOptions = {}
+) {
+  if (value == null) {
+    return defaultValue;
+  }
+  const hoursLabel =
+    SPACE +
+    i18n.translate('xpack.apm.formatters.hoursTimeUnitLabel', {
+      defaultMessage: 'h'
+    });
+  const formatted = asDecimal(value / 3600000000);
+  return `${formatted}${withUnit ? hoursLabel : ''}`;
+}
+
+export function asMinutes(
+  value: FormatterValue,
+  { withUnit = true, defaultValue = NOT_AVAILABLE_LABEL }: FormatterOptions = {}
+) {
+  if (value == null) {
+    return defaultValue;
+  }
+  const minutesLabel =
+    SPACE +
+    i18n.translate('xpack.apm.formatters.minutesTimeUnitLabel', {
+      defaultMessage: 'm'
+    });
+  const formatted = asDecimal(value / 60000000);
+  return `${formatted}${withUnit ? minutesLabel : ''}`;
 }
 
 export function asSeconds(
@@ -81,6 +115,10 @@ type TimeFormatter = (
 export const getTimeFormatter: TimeFormatter = memoize((max: number) => {
   const unit = timeUnit(max);
   switch (unit) {
+    case 'h':
+      return asHours;
+    case 'm':
+      return asMinutes;
     case 's':
       return asSeconds;
     case 'ms':
@@ -91,7 +129,11 @@ export const getTimeFormatter: TimeFormatter = memoize((max: number) => {
 });
 
 export function timeUnit(max: number) {
-  if (max > SECONDS_CUT_OFF) {
+  if (max > HOURS_CUT_OFF) {
+    return 'h';
+  } else if (max > MINUTES_CUT_OFF) {
+    return 'm';
+  } else if (max > SECONDS_CUT_OFF) {
     return 's';
   } else if (max > MILLISECONDS_CUT_OFF) {
     return 'ms';
