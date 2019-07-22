f32c829c63c46b5f63a113640b0ce2321d98d462
Matt Apperson <me@mattapperson.com>
2019-07-22 17:04:57 -0400

    Add shell ingest plugin

---
+++ x-pack/legacy/plugins/ingest/common/types/io_ts.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import * as t from 'io-ts';
+import { either } from 'fp-ts/lib/Either';
+export type DateType = t.Type<Date, string, unknown>;
+
+export const DateFromString: DateType = new t.Type<Date, string, unknown>(
+  'DateFromString',
+  (u): u is Date => u instanceof Date,
+  (u, c) =>
+    either.chain(t.string.validate(u, c), s => {
+      const d = new Date(s);
+      return isNaN(d.getTime()) ? t.failure(u, c) : t.success(d);
+    }),
+  a => a.toISOString()
+);
