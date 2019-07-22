4f9e23e0847dcf6b4f300d2ff2bf95be2467ac71
Matt Apperson <me@mattapperson.com>
2019-07-22 17:04:57 -0400

    Add shell ingest plugin

---
+++ x-pack/legacy/plugins/ingest/common/constants/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export { INDEX_NAMES } from './index_names';
+export { PLUGIN } from './plugin';
+export const BASE_PATH = '/ingest';
