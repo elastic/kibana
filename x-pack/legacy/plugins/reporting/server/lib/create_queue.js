e0d7511366260745aaf29ebc6abb3f96a0cfaa63
Timothy Sullivan <tsullivan@elastic.co>
2019-07-22 17:54:49 -0700

    move some code around in create_queue

--- x-pack/legacy/plugins/reporting/server/lib/create_queue.js
+++ x-pack/legacy/plugins/reporting/server/lib/create_queue.js
@@ -7,28 +7,25 @@
 import { Esqueue } from './esqueue';
 import { createWorkerFactory } from './create_worker';
 import { oncePerServer } from './once_per_server';
-import { createTaggedLogger } from './create_tagged_logger';
-
-const dateSeparator = '.';
+import { createTaggedLogger } from './create_tagged_logger'; // TODO remove createTaggedLogger once esqueue is removed
 
 function createQueueFn(server) {
   const queueConfig = server.config().get('xpack.reporting.queue');
   const index = server.config().get('xpack.reporting.index');
-  const createWorker = createWorkerFactory(server);
 
-  const logger = createTaggedLogger(server, ['reporting', 'esqueue']);
   const queueOptions = {
     interval: queueConfig.indexInterval,
     timeout: queueConfig.timeout,
-    dateSeparator: dateSeparator,
+    dateSeparator: '.',
     client: server.plugins.elasticsearch.getCluster('admin'),
-    logger,
+    logger: createTaggedLogger(server, ['reporting', 'esqueue']),
   };
 
   const queue = new Esqueue(index, queueOptions);
 
   if (queueConfig.pollEnabled) {
     // create workers to poll the index for idle jobs waiting to be claimed and executed
+    const createWorker = createWorkerFactory(server);
     createWorker(queue);
   } else {
     logger(
