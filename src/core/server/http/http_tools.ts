bc16491a236200fad51039c3d2aef8a16f434b8e
Peter Schretlen <peter.schretlen@elastic.co>
2019-07-23 17:11:43 -0400

    [7.3] revert configurable global socket timeouts (#31603)

--- src/core/server/http/http_tools.ts
+++ src/core/server/http/http_tools.ts
@@ -79,30 +79,11 @@ export function getServerOptions(config: HttpConfig, { configureTLS = true } = {
   return options;
 }
 
-export function getListenerOptions(config: HttpConfig) {
-  return {
-    keepaliveTimeout: config.keepaliveTimeout,
-    socketTimeout: config.socketTimeout,
-  };
-}
+export function createServer(options: ServerOptions) {
+  const server = new Server(options);
 
-interface ListenerOptions {
-  keepaliveTimeout: number;
-  socketTimeout: number;
-}
-
-export function createServer(serverOptions: ServerOptions, listenerOptions: ListenerOptions) {
-  const server = new Server(serverOptions);
-
-  server.listener.keepAliveTimeout = listenerOptions.keepaliveTimeout;
-  server.listener.setTimeout(listenerOptions.socketTimeout);
-  server.listener.on('timeout', socket => {
-    if (socket.writable) {
-      socket.end(Buffer.from('HTTP/1.1 408 Request Timeout\r\n\r\n', 'ascii'));
-    } else {
-      socket.destroy();
-    }
-  });
+  // Revert to previous 120 seconds keep-alive timeout in Node < 8.
+  server.listener.keepAliveTimeout = 120e3;
   server.listener.on('clientError', (err, socket) => {
     if (socket.writable) {
       socket.end(Buffer.from('HTTP/1.1 400 Bad Request\r\n\r\n', 'ascii'));
