6a21f45b8bf0a519aab6a3ca23ff17b4bd1d0197
James Gowdy <jgowdy@elastic.co>
2019-07-24 15:39:10 +0100

    adding a comment

--- x-pack/legacy/plugins/ml/public/jobs/new_job_new/pages/components/pick_fields_step/components/split_cards/animate_split_hook.ts
+++ x-pack/legacy/plugins/ml/public/jobs/new_job_new/pages/components/pick_fields_step/components/split_cards/animate_split_hook.ts
@@ -8,6 +8,11 @@ import { useEffect, useState } from 'react';
 
 export const ANIMATION_SWITCH_DELAY_MS = 1000;
 
+// custom hook to enable the card split animation of the cards 1 second after the component has been rendered
+// then switching to a step which contains the cards, the animation shouldn't play, instead
+// the cards should be initially rendered in the split state.
+// all subsequent changes to the split should be animated.
+
 export function useAnimateSplit() {
   const [animateSplit, setAnimateSplit] = useState(false);
   useEffect(() => {
