bcb1f9657539d50650b6b2c7c0d9102bf08d6873
Thomas Neirynck <thomas@elastic.co>
2019-07-25 17:46:56 -0400

    fix typo

--- x-pack/legacy/plugins/maps/public/layers/styles/vector_style_defaults.js
+++ x-pack/legacy/plugins/maps/public/layers/styles/vector_style_defaults.js
@@ -12,7 +12,7 @@ const DEFAULT_COLORS = ['#e6194b', '#3cb44b', '#ffe119', '#f58231', '#911eb4'];
 const DEFAULT_ICON = 'airfield';
 
 export const ABSOLUTE_MIN_SIZE = 0;
-export const DEFAULT_MIN_SIZE = 0;
+export const DEFAULT_MIN_SIZE = 1;
 export const DEFAULT_MAX_SIZE = 64;
 
 export function getDefaultProperties(mapColors = []) {
