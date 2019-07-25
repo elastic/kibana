5f77325ae96e10d10d38c64f812c8ce39d639949
Kerry Gallagher <k.gallagher.05@gmail.com>
2019-07-25 16:04:35 +0100

    Begin styling adjustments to settings page
    * Implements use of EUI Panels
    * Centers page content

--- x-pack/legacy/plugins/infra/public/components/source_configuration/source_configuration_settings.tsx
+++ x-pack/legacy/plugins/infra/public/components/source_configuration/source_configuration_settings.tsx
@@ -4,7 +4,18 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiButton, EuiCallOut, EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiCallOut,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPanel,
+  EuiSpacer,
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+} from '@elastic/eui';
 import { FormattedMessage, injectI18n, InjectedIntl } from '@kbn/i18n/react';
 import React, { useCallback, useContext, useMemo } from 'react';
 
@@ -75,75 +86,90 @@ export const SourceConfigurationSettings = injectI18n(
 
     return (
       <>
-        <NameConfigurationPanel
-          isLoading={isLoading}
-          nameFieldProps={indicesConfigurationProps.name}
-          readOnly={!isWriteable}
-        />
-        <EuiSpacer />
-        <IndicesConfigurationPanel
-          isLoading={isLoading}
-          logAliasFieldProps={indicesConfigurationProps.logAlias}
-          metricAliasFieldProps={indicesConfigurationProps.metricAlias}
-          readOnly={!isWriteable}
-        />
-        <EuiSpacer />
-        <FieldsConfigurationPanel
-          containerFieldProps={indicesConfigurationProps.containerField}
-          hostFieldProps={indicesConfigurationProps.hostField}
-          isLoading={isLoading}
-          podFieldProps={indicesConfigurationProps.podField}
-          readOnly={!isWriteable}
-          tiebreakerFieldProps={indicesConfigurationProps.tiebreakerField}
-          timestampFieldProps={indicesConfigurationProps.timestampField}
-        />
-        <EuiSpacer />
-        <LogColumnsConfigurationPanel
-          addLogColumn={addLogColumn}
-          moveLogColumn={moveLogColumn}
-          availableFields={availableFields}
-          isLoading={isLoading}
-          logColumnConfiguration={logColumnConfigurationProps}
-        />
-
-        {errors.length > 0 ? (
-          <>
-            <EuiCallOut color="danger">
-              <ul>
-                {errors.map((error, errorIndex) => (
-                  <li key={errorIndex}>{error}</li>
-                ))}
-              </ul>
-            </EuiCallOut>
-            <EuiSpacer size="m" />
-          </>
-        ) : null}
-
-        <EuiFlexGroup>
-          <EuiFlexItem />
-          {isWriteable && (
-            <EuiFlexItem grow={false}>
-              {isLoading ? (
-                <EuiButton color="primary" isLoading fill>
-                  Loading
-                </EuiButton>
-              ) : (
-                <EuiButton
-                  data-test-subj="updateSourceConfigurationButton"
-                  color="primary"
-                  isDisabled={!isFormDirty || !isFormValid}
-                  fill
-                  onClick={persistUpdates}
-                >
-                  <FormattedMessage
-                    id="xpack.infra.sourceConfiguration.updateSourceConfigurationButtonLabel"
-                    defaultMessage="Update Source"
+        <EuiPage>
+          <EuiPageBody>
+            <EuiPageContent verticalPosition="center" horizontalPosition="center">
+              <EuiPageContentBody>
+                <EuiPanel paddingSize="l">
+                  <NameConfigurationPanel
+                    isLoading={isLoading}
+                    nameFieldProps={indicesConfigurationProps.name}
+                    readOnly={!isWriteable}
                   />
-                </EuiButton>
-              )}
-            </EuiFlexItem>
-          )}
-        </EuiFlexGroup>
+                </EuiPanel>
+                <EuiSpacer />
+                <EuiPanel paddingSize="l">
+                  <IndicesConfigurationPanel
+                    isLoading={isLoading}
+                    logAliasFieldProps={indicesConfigurationProps.logAlias}
+                    metricAliasFieldProps={indicesConfigurationProps.metricAlias}
+                    readOnly={!isWriteable}
+                  />
+                </EuiPanel>
+                <EuiSpacer />
+                <EuiPanel paddingSize="l">
+                  <FieldsConfigurationPanel
+                    containerFieldProps={indicesConfigurationProps.containerField}
+                    hostFieldProps={indicesConfigurationProps.hostField}
+                    isLoading={isLoading}
+                    podFieldProps={indicesConfigurationProps.podField}
+                    readOnly={!isWriteable}
+                    tiebreakerFieldProps={indicesConfigurationProps.tiebreakerField}
+                    timestampFieldProps={indicesConfigurationProps.timestampField}
+                  />
+                </EuiPanel>
+                <EuiSpacer />
+                <EuiPanel paddingSize="l">
+                  <LogColumnsConfigurationPanel
+                    addLogColumn={addLogColumn}
+                    moveLogColumn={moveLogColumn}
+                    availableFields={availableFields}
+                    isLoading={isLoading}
+                    logColumnConfiguration={logColumnConfigurationProps}
+                  />
+                </EuiPanel>
+                {errors.length > 0 ? (
+                  <>
+                    <EuiCallOut color="danger">
+                      <ul>
+                        {errors.map((error, errorIndex) => (
+                          <li key={errorIndex}>{error}</li>
+                        ))}
+                      </ul>
+                    </EuiCallOut>
+                    <EuiSpacer size="m" />
+                  </>
+                ) : null}
+                <EuiSpacer size="m" />
+                <EuiFlexGroup>
+                  <EuiFlexItem />
+                  {isWriteable && (
+                    <EuiFlexItem grow={false}>
+                      {isLoading ? (
+                        <EuiButton color="primary" isLoading fill>
+                          Loading
+                        </EuiButton>
+                      ) : (
+                        <EuiButton
+                          data-test-subj="updateSourceConfigurationButton"
+                          color="primary"
+                          isDisabled={!isFormDirty || !isFormValid}
+                          fill
+                          onClick={persistUpdates}
+                        >
+                          <FormattedMessage
+                            id="xpack.infra.sourceConfiguration.updateSourceConfigurationButtonLabel"
+                            defaultMessage="Update Source"
+                          />
+                        </EuiButton>
+                      )}
+                    </EuiFlexItem>
+                  )}
+                </EuiFlexGroup>
+              </EuiPageContentBody>
+            </EuiPageContent>
+          </EuiPageBody>
+        </EuiPage>
       </>
     );
   }
