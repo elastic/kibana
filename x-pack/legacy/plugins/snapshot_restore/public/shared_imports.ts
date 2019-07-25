98e0c020b7022afbf7caf4fa3a96d8931b8cc170
CJ Cenizal <cj@cenizal.com>
2019-07-24 18:09:49 -0700

    Rename plugin to es_ui_shared.

--- x-pack/legacy/plugins/snapshot_restore/public/shared_imports.ts
+++ x-pack/legacy/plugins/snapshot_restore/public/shared_imports.ts
@@ -10,4 +10,4 @@ export {
   UseRequestConfig,
   sendRequest,
   useRequest,
-} from '../../../../../src/plugins/elasticsearch_ui_shared/public';
+} from '../../../../../src/plugins/es_ui_shared/public';
