db244c9d8fa4350286fdbc686b97949e5fb06298
John Schulz <john.schulz@elastic.co>
2019-07-26 17:35:40 -0400

    Add Hero image. Vertically center copy and push image to bottom.
    Line-height, color and some other items are not to spec yet.
    
    Using inline styles while we're iterating. Will extract to CSS / Sass soon.

--- x-pack/legacy/plugins/integrations_manager/public/screens/home.tsx
+++ x-pack/legacy/plugins/integrations_manager/public/screens/home.tsx
@@ -4,7 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React, { useState, useEffect } from 'react';
-import { EuiPage, EuiPageBody, EuiTitle } from '@elastic/eui';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPage,
+  EuiPageBody,
+  // @ts-ignore (elastic/eui#1557) & (elastic/eui#1262) EuiImage is not exported yet
+  EuiImage,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
 import { PLUGIN } from '../../common/constants';
 import { IntegrationsGroupedByStatus } from '../../common/types';
 import { IntegrationsGridByStatus } from '../components/integration_list_grid';
@@ -25,13 +34,50 @@ export function Home() {
   }, []);
 
   return (
-    <EuiPage restrictWidth={1200}>
-      <EuiPageBody>
-        <EuiTitle size="l">
-          <h1>Add Your Data</h1>
-        </EuiTitle>
-        {map ? <IntegrationsGridByStatus map={map} /> : null}
-      </EuiPageBody>
-    </EuiPage>
+    <>
+      <EuiPage style={{ backgroundColor: 'white', paddingBottom: '8px' }}>
+        <EuiPageBody restrictWidth={1200}>
+          <Header />
+        </EuiPageBody>
+      </EuiPage>
+      <EuiPage>
+        <EuiPageBody restrictWidth={1200}>
+          {map ? <IntegrationsGridByStatus map={map} /> : null}
+        </EuiPageBody>
+      </EuiPage>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <EuiFlexGroup>
+      <EuiFlexItem grow={1}>
+        <EuiFlexGroup alignItems="center">
+          <EuiFlexItem>
+            <EuiTitle size="l">
+              <h1>Add Your Data</h1>
+            </EuiTitle>
+            <EuiText
+              style={{
+                color: '#69707D', // euiColorDarkShade
+              }}
+            >
+              Some creative copy about integrations goes here.
+            </EuiText>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlexItem>
+      <EuiFlexItem grow={1}>
+        <EuiFlexGroup justifyContent="flexEnd">
+          <EuiImage
+            size="l"
+            alt="Illustration of computer"
+            url="/plugins/integrations_manager/assets/illustration-kibana-getting-started@2x.png"
+            style={{ width: '475px', height: '273px' }}
+          />
+        </EuiFlexGroup>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 }
