d6fb894f6acea9c1ae7a6f80cf39ae470eb6e951
Timothy Sullivan <tsullivan@elastic.co>
2019-07-26 15:51:26 -0700

    fix constants import

--- x-pack/legacy/plugins/reporting/export_types/common/lib/screenshots.js
+++ x-pack/legacy/plugins/reporting/export_types/common/lib/screenshots.js
@@ -9,7 +9,7 @@ import { first, tap, mergeMap } from 'rxjs/operators';
 import fs from 'fs';
 import getPort from 'get-port';
 import { promisify } from 'util';
-import { PLUGIN_ID } from '../../common/constants';
+import { PLUGIN_ID } from '../../../common/constants';
 import { LevelLogger } from '../../../server/lib/level_logger';
 import { i18n } from '@kbn/i18n';
 
