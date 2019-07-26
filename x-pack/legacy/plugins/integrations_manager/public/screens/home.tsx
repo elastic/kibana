c76e5da191894554ae5f41b4ba99514f9b13f0dd
John Schulz <john.schulz@elastic.co>
2019-07-26 17:35:40 -0400

    Add Hero image. Vertically center copy and push image to bottom.
    Line-height, color and some other items are not to spec yet.
    
    Using inline styles while we're iterating. Will extract to CSS / Sass soon.

--- x-pack/legacy/plugins/integrations_manager/public/screens/home.tsx
+++ x-pack/legacy/plugins/integrations_manager/public/screens/home.tsx
@@ -4,8 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React, { useState, useEffect } from 'react';
-import { EuiPage, EuiPageBody, EuiTitle } from '@elastic/eui';
-import { PLUGIN } from '../../common/constants';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  // @ts-ignore (elastic/eui#1557) & (elastic/eui#1262) EuiImage is not exported yet
+  EuiImage,
+  EuiPage,
+  EuiPageBody,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
+import { PLUGIN, STATUS_INSTALLED, STATUS_NOT_INSTALLED } from '../../common/constants';
 import { IntegrationsGroupedByStatus } from '../../common/types';
 import { IntegrationsGridByStatus } from '../components/integration_list_grid';
 import { getIntegrationsGroupedByStatus } from '../data';
@@ -16,8 +25,8 @@ export function Home() {
   useBreadcrumbs([{ text: PLUGIN.TITLE, href: linkToListView() }]);
 
   const [map, setMap] = useState<IntegrationsGroupedByStatus>({
-    installed: [],
-    not_installed: [],
+    [STATUS_INSTALLED]: [],
+    [STATUS_NOT_INSTALLED]: [],
   });
 
   useEffect(() => {
@@ -25,13 +34,61 @@ export function Home() {
   }, []);
 
   return (
-    <EuiPage restrictWidth={1200}>
-      <EuiPageBody>
+    <>
+      <EuiPage style={{ backgroundColor: 'white', paddingBottom: '8px' }}>
+        <EuiPageBody restrictWidth={1200}>
+          <Header />
+        </EuiPageBody>
+      </EuiPage>
+      <EuiPage>
+        <EuiPageBody restrictWidth={1200}>
+          <IntegrationsGridByStatus map={map} />
+        </EuiPageBody>
+      </EuiPage>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <EuiFlexGroup>
+      <EuiFlexItem grow={1}>
+        <HeroCopy />
+      </EuiFlexItem>
+      <EuiFlexItem grow={1}>
+        <HeroImage />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+
+function HeroCopy() {
+  return (
+    <EuiFlexGroup alignItems="center">
+      <EuiFlexItem>
         <EuiTitle size="l">
           <h1>Add Your Data</h1>
         </EuiTitle>
-        {map ? <IntegrationsGridByStatus map={map} /> : null}
-      </EuiPageBody>
-    </EuiPage>
+        <EuiText
+          style={{
+            color: '#69707D', // euiColorDarkShade
+          }}
+        >
+          Some creative copy about integrations goes here.
+        </EuiText>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+
+function HeroImage() {
+  return (
+    <EuiFlexGroup justifyContent="flexEnd">
+      <EuiImage
+        alt="Illustration of computer"
+        url="/plugins/integrations_manager/assets/illustration-kibana-getting-started@2x.png"
+        style={{ width: '475px', height: '273px' }}
+      />
+    </EuiFlexGroup>
   );
 }
