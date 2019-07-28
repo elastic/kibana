a9ca13c25fe7d043547ab932838ca52fab463f66
Liza Katz <liza.katz@elastic.co>
2019-07-28 14:46:56 +0300

    Fix merge

--- src/legacy/core_plugins/data/public/index.ts
+++ src/legacy/core_plugins/data/public/index.ts
@@ -90,7 +90,12 @@ export { ExpressionRenderer, ExpressionRendererProps, ExpressionRunner } from '.
 /** @public types */
 export { IndexPattern, StaticIndexPattern, StaticIndexPatternField, Field } from './index_patterns';
 export { Query } from './query';
-export { FilterManager, FilterStateManager, uniqFilters } from './filter/filter_manager';
+export {
+  FilterManager,
+  FilterStateManager,
+  uniqFilters,
+  onlyDisabledFiltersChanged,
+} from './filter/filter_manager';
 
 /** @public static code */
 export { dateHistogramInterval } from '../common/date_histogram_interval';
