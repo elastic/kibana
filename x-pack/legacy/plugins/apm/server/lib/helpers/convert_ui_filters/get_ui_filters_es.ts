edd22486d73f970d1a08ecbc5d749aafaaee1adb
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-07-28 21:11:32 +0200

    [APM] Local filters preparation: move uiFilters encoding to server and update routes (#41233)

--- x-pack/legacy/plugins/apm/server/lib/helpers/convert_ui_filters/get_ui_filters_es.ts
+++ x-pack/legacy/plugins/apm/server/lib/helpers/convert_ui_filters/get_ui_filters_es.ts
@@ -4,15 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { UIFilters } from '../../../typings/ui-filters';
+import { Server } from 'hapi';
+import { ESFilter } from 'elasticsearch';
+import { UIFilters } from '../../../../typings/ui-filters';
 import { getEnvironmentUiFilterES } from './get_environment_ui_filter_es';
 import { getKueryUiFilterES } from './get_kuery_ui_filter_es';
 
-export async function getUiFiltersES(uiFilters: UIFilters): Promise<string> {
-  const kuery = await getKueryUiFilterES(uiFilters.kuery);
+export async function getUiFiltersES(server: Server, uiFilters: UIFilters) {
+  const kuery = await getKueryUiFilterES(server, uiFilters.kuery);
   const environment = getEnvironmentUiFilterES(uiFilters.environment);
 
-  const filters = [kuery, environment].filter(filter => !!filter);
-
-  return encodeURIComponent(JSON.stringify(filters));
+  // remove undefined items from list
+  const filters = [kuery, environment].filter(filter => !!filter) as ESFilter[];
+  return filters;
 }
