20d2a350adbc920efa27187a7355ad16b0d70cad
Michael Marcialis <michael.marcialis@elastic.co>
2019-07-29 11:08:47 -0400

    correct translation casing and cleanup

--- x-pack/legacy/plugins/siem/public/components/page/network/ip_overview/translations.ts
+++ x-pack/legacy/plugins/siem/public/components/page/network/ip_overview/translations.ts
@@ -6,10 +6,6 @@
 
 import { i18n } from '@kbn/i18n';
 
-export const LAST_BEAT = i18n.translate('xpack.siem.network.ipDetails.ipOverview.lastBeatTitle', {
-  defaultMessage: 'Last Beat',
-});
-
 export const LOCATION = i18n.translate('xpack.siem.network.ipDetails.ipOverview.locationTitle', {
   defaultMessage: 'Location',
 });
@@ -17,23 +13,23 @@ export const LOCATION = i18n.translate('xpack.siem.network.ipDetails.ipOverview.
 export const AUTONOMOUS_SYSTEM = i18n.translate(
   'xpack.siem.network.ipDetails.ipOverview.autonomousSystemTitle',
   {
-    defaultMessage: 'Autonomous System',
+    defaultMessage: 'Autonomous system',
   }
 );
 
 export const MAX_ANOMALY_SCORE_BY_JOB = i18n.translate(
   'xpack.siem.network.ipDetails.ipOverview.maxAnomalyScoreByJobTitle',
   {
-    defaultMessage: 'Max Anomaly Score By Job',
+    defaultMessage: 'Max anomaly score by job',
   }
 );
 
 export const FIRST_SEEN = i18n.translate('xpack.siem.network.ipDetails.ipOverview.firstSeenTitle', {
-  defaultMessage: 'First Seen',
+  defaultMessage: 'First seen',
 });
 
 export const LAST_SEEN = i18n.translate('xpack.siem.network.ipDetails.ipOverview.lastSeenTitle', {
-  defaultMessage: 'Last Seen',
+  defaultMessage: 'Last seen',
 });
 
 export const HOST_ID = i18n.translate('xpack.siem.network.ipDetails.ipOverview.hostIdTitle', {
@@ -41,7 +37,7 @@ export const HOST_ID = i18n.translate('xpack.siem.network.ipDetails.ipOverview.h
 });
 
 export const HOST_NAME = i18n.translate('xpack.siem.network.ipDetails.ipOverview.hostNameTitle', {
-  defaultMessage: 'Host Name',
+  defaultMessage: 'Host name',
 });
 
 export const WHOIS = i18n.translate('xpack.siem.network.ipDetails.ipOverview.whoIsTitle', {
@@ -51,36 +47,11 @@ export const WHOIS = i18n.translate('xpack.siem.network.ipDetails.ipOverview.who
 export const VIEW_WHOIS = i18n.translate('xpack.siem.network.ipDetails.ipOverview.viewWhoisTitle', {
   defaultMessage: 'iana.org',
 });
-export const VIEW_VIRUS_TOTAL = i18n.translate(
-  'xpack.siem.network.ipDetails.ipOverview.viewVirusTotalTitle.',
-  {
-    defaultMessage: 'virustotal.com',
-  }
-);
-export const VIEW_TALOS_INTELLIGENCE = i18n.translate(
-  'xpack.siem.network.ipDetails.ipOverview.viewTalosIntelligenceTitle',
-  {
-    defaultMessage: 'talosIntelligence.com',
-  }
-);
 
 export const REPUTATION = i18n.translate(
   'xpack.siem.network.ipDetails.ipOverview.ipReputationTitle',
   {
     defaultMessage: 'Reputation',
-  }
-);
-
-export const AS_SOURCE = i18n.translate(
-  'xpack.siem.network.ipDetails.ipOverview.asSourceDropDownOptionLabel',
-  {
-    defaultMessage: 'As Source',
-  }
-);
-export const AS_DESTINATION = i18n.translate(
-  'xpack.siem.network.ipDetails.ipOverview.asDestinationDropDownOptionLabel',
-  {
-    defaultMessage: 'As Destination',
   }
 );
 
