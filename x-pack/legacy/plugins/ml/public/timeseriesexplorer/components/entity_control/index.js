6c36fa103aef1d5c8fbfc84edd6aaeeb02255cbd
Walter Rafelsberger <walter@elastic.co>
2019-07-29 18:51:20 +0200

    [ML] Migrates single metric viewer to React. (#41739)
    Migrates the overall page of Single Metric Viewer to React.

--- x-pack/legacy/plugins/ml/public/timeseriesexplorer/components/entity_control/index.js
+++ x-pack/legacy/plugins/ml/public/timeseriesexplorer/components/entity_control/index.js
@@ -4,4 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { InfluencersList } from './influencers_list';
+export { EntityControl } from './entity_control';
--- x-pack/legacy/plugins/ml/public/timeseriesexplorer/components/timeseriesexplorer_no_chart_data/index.js
+++ x-pack/legacy/plugins/ml/public/timeseriesexplorer/components/timeseriesexplorer_no_chart_data/index.js
@@ -4,4 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { InfluencersList } from './influencers_list';
+export { TimeseriesexplorerNoChartData } from './timeseriesexplorer_no_chart_data';
