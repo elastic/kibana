elastic/kibana

commit 6e7e60d49a7701dfd14ef4c8a2463e99df83da1a

Replace basePath.prepend in routes w/plain string.

x-pack/legacy/plugins/integrations_manager/public/routes.tsx
@@ -6,7 +6,6 @@
 
 import React from 'react';
 import { generatePath, Route } from 'react-router-dom';
-import { npStart } from 'ui/new_platform';
 import { Detail } from './screens/detail';
 import { Home } from './screens/home';
 import { PLUGIN } from '../common/constants';
@@ -26,9 +25,8 @@ interface DetailMatch {
   };
 }
 
-const { prepend } = npStart.core.http.basePath;
 // include '#' because we're using HashRouter
-const prependRoot = (path: string) => prepend(patterns.APP_ROOT + '#' + path);
+const prependRoot = (path: string) => `${patterns.APP_ROOT}#${path}`;
 
 // TODO: get this from server/integrations/handlers.ts (move elsewhere?)
 // seems like part of the name@version change

commit af742b853d323f67d38359099720c58139b510d1

Move link generators into to new useLinks hook.
6e7e60d broke the paths because I forgot what `basePath` actually does. This moves the functions into a hook.

Usage is nearly identical in views.

Also adds a path function for assets. I didn't realize those were broken because I was running with `--no-base-path`

x-pack/legacy/plugins/integrations_manager/public/components/integration_card.tsx
@@ -5,7 +5,7 @@
  */
 import React from 'react';
 import { EuiCard, EuiIcon, ICON_TYPES } from '@elastic/eui';
-import { linkToDetailView } from '../routes';
+import { useLinks } from '../hooks';
 import { IntegrationListItem, IntegrationInfo } from '../../common/types';
 
 type IntegrationCardProps = IntegrationListItem | IntegrationInfo;
@@ -16,7 +16,8 @@ export function IntegrationCard({
   version,
   icon: iconUrl,
 }: IntegrationCardProps) {
-  const url = linkToDetailView({ name, version });
+  const links = useLinks();
+  const url = links.toDetailView({ name, version });
 
   // TODO: Need title or something which uses correct capitalization (e.g. PostgreSQL)
   const title = description.split(' ')[0];

x-pack/legacy/plugins/integrations_manager/public/hooks/index.ts
@@ -4,5 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { useCore } from './core';
 export { useBreadcrumbs } from './breadcrumbs';
+export { useCore } from './core';
+export { useLinks } from './links';

x-pack/legacy/plugins/integrations_manager/public/hooks/links.tsx
@@ -1,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { generatePath } from 'react-router-dom';
+import { PLUGIN } from '../../common/constants';
+import { patterns } from '../routes';
+import { useCore } from './index';
+
+// TODO: get this from server/integrations/handlers.ts (move elsewhere?)
+// seems like part of the name@version change
+interface DetailParams {
+  name: string;
+  version: string;
+}
+
+function addBasePath(path: string) {
+  const { http } = useCore();
+  return http.basePath.prepend(path);
+}
+
+function appRoot(path: string) {
+  // include '#' because we're using HashRouter
+  return addBasePath(patterns.APP_ROOT + '#' + path);
+}
+
+export function useLinks() {
+  return {
+    toAssets: (path: string) => addBasePath(`/plugins/${PLUGIN.ID}/assets/${path}`),
+    toListView: () => appRoot(patterns.LIST_VIEW),
+    toDetailView: ({ name, version }: DetailParams) =>
+      appRoot(generatePath(patterns.DETAIL_VIEW, { pkgkey: `${name}-${version}` })),
+  };
+}

x-pack/legacy/plugins/integrations_manager/public/routes.tsx
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { generatePath, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { Detail } from './screens/detail';
 import { Home } from './screens/home';
 import { PLUGIN } from '../common/constants';
@@ -25,16 +25,6 @@ interface DetailMatch {
   };
 }
 
-// include '#' because we're using HashRouter
-const prependRoot = (path: string) => `${patterns.APP_ROOT}#${path}`;
-
-// TODO: get this from server/integrations/handlers.ts (move elsewhere?)
-// seems like part of the name@version change
-interface DetailParams {
-  name: string;
-  version: string;
-}
-
 export const routes = [
   <Route key="home" path={patterns.LIST_VIEW} exact={true} component={Home} />,
   <Route
@@ -44,9 +34,3 @@ export const routes = [
     render={(props: DetailMatch) => <Detail package={props.match.params.pkgkey} />}
   />,
 ];
-
-// linkTo* are for EUILink and other places which need a full path
-export const linkToListView = () => prependRoot(patterns.LIST_VIEW);
-
-export const linkToDetailView = ({ name, version }: DetailParams) =>
-  prependRoot(generatePath(patterns.DETAIL_VIEW, { pkgkey: `${name}-${version}` }));

x-pack/legacy/plugins/integrations_manager/public/screens/detail.tsx
@@ -8,8 +8,7 @@ import { EuiPage, EuiPageBody, EuiSpacer, EuiTitle } from '@elastic/eui';
 import { PLUGIN } from '../../common/constants';
 import { IntegrationInfo } from '../../common/types';
 import { getIntegrationInfoByKey } from '../data';
-import { useBreadcrumbs } from '../hooks';
-import { linkToListView } from '../routes';
+import { useBreadcrumbs, useLinks } from '../hooks';
 
 export function Detail(props: { package: string }) {
   const [info, setInfo] = useState<IntegrationInfo | null>(null);
@@ -27,8 +26,8 @@ function InfoPanel(info: IntegrationInfo) {
   const { description, version } = info;
   // TODO: Need title or something which uses correct capitalization (e.g. PostgreSQL)
   const title = description.split(' ')[0];
-
-  useBreadcrumbs([{ text: PLUGIN.TITLE, href: linkToListView() }, { text: title }]);
+  const links = useLinks();
+  useBreadcrumbs([{ text: PLUGIN.TITLE, href: links.toListView() }, { text: title }]);
 
   return (
     <EuiPage restrictWidth={1200}>

x-pack/legacy/plugins/integrations_manager/public/screens/home.tsx
@@ -18,11 +18,11 @@ import { PLUGIN, STATUS_INSTALLED, STATUS_NOT_INSTALLED } from '../../common/con
 import { IntegrationsGroupedByStatus } from '../../common/types';
 import { IntegrationsGridByStatus } from '../components/integration_list_grid';
 import { getIntegrationsGroupedByStatus } from '../data';
-import { useBreadcrumbs } from '../hooks';
-import { linkToListView } from '../routes';
+import { useBreadcrumbs, useLinks } from '../hooks';
 
 export function Home() {
-  useBreadcrumbs([{ text: PLUGIN.TITLE, href: linkToListView() }]);
+  const links = useLinks();
+  useBreadcrumbs([{ text: PLUGIN.TITLE, href: links.toListView() }]);
 
   const [map, setMap] = useState<IntegrationsGroupedByStatus>({
     [STATUS_INSTALLED]: [],
@@ -82,11 +82,12 @@ function HeroCopy() {
 }
 
 function HeroImage() {
+  const links = useLinks();
   return (
     <EuiFlexGroup justifyContent="flexEnd">
       <EuiImage
         alt="Illustration of computer"
-        url="/plugins/integrations_manager/assets/illustration_kibana_getting_started@2x.png"
+        url={links.toAssets('illustration_kibana_getting_started@2x.png')}
         style={{ width: '475px', height: '273px' }}
       />
     </EuiFlexGroup>