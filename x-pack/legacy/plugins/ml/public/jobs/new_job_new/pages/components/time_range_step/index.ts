53c530c0124131c8f22f0ba2c1706731c299bc8d
James Gowdy <jgowdy@elastic.co>
2019-07-31 14:11:06 +0100

    [ML] Merging job wizards feature branch to master (#42339)
    * [ML] Adding new job wizards services (#40718)
    
    * [ML] Adding new job wizards services
    
    * adding ts-ignore for incomplte kibana timefilter type
    
    * [ML] New job wizards UI (#40748)
    
    * [ML] New job wizards UI
    
    * small refactor
    
    * fixing type problems
    
    * updating snapshots
    
    * updating translations ids for temporary new job links
    
    * updating translation ids
    
    * fixing population by field selection
    
    * small refactor to by field select
    
    * moving component descriptions to separate files
    
    * moving chart grid component into own file
    
    * fixing event rate count detector
    
    * fixing decimal place rounding
    
    * changes based on review
    
    * more changes based on review
    
    * fixing test
    
    * [ML] Adding created_by to job (#41015)
    
    * [ML] Adding json preview (#41078)
    
    * [ML] Adding job json preview
    
    * fix typo
    
    * [ML] Fixing view results button (#41077)
    
    * [ML] Adding generic custom settings functions to job creator (#41046)
    
    * [ML] Adding generic custom settings functions to job creator
    
    * fixing typo
    
    * [ML] Adding advanced section to job details step (#41157)
    
    * [ML] Adding advanced section to job details step
    
    * disabling model plot for population
    
    * changes based on review
    
    * updating include paths
    
    * [ML] Adding additional settings section to job details step (#41339)
    
    * [ML] Adding additional settings section to job details step
    
    * changes based on review
    
    * [ML] Adding basic job validation (#41459)
    
    * [ML] Adding basic job validation
    
    * removing observable
    
    * small refactor
    
    * comments and refactors
    
    * disabling create job button
    
    * adding duplicate job and group id checks
    
    * disabling next on invalid wizard step
    
    * changes based on review
    
    * removing unused includes
    
    * [ML] Adding advanced job validation (#41634)
    
    * [ML] Adding advanced job validation
    
    * clean up
    
    * function rename
    
    * updating tests
    
    * show some previous step info
    
    * removing . from validation titles
    
    * fixes advanced validation duration check
    
    * removing include
    
    * [ML] Changing wizard layout (#41802)
    
    * [ML] Changing wizard layout
    
    * adding a comment
    
    * fixing wizard step link issue
    
    * removing greyed out controls in summary
    
    * filtering out basic validation results
    
    * adding wizards steps back into summary
    
    * moving idFilterList
    
    * removing unused includes
    
    * [ML] Adding date picker to time range step (#41978)
    
    * [ML] Adding date picker to time range step
    
    * adding comments
    
    * sorting influencers
    
    * removing euiSize for width
    
    * [ML] Displaying job creation errors (#42165)
    
    * [ML] Displaying job creation errors
    
    * removing unnecessary try/catch
    
    * [ML] Updating index pattern type (#42269)
    
    * [ML] Updating index pattern type
    
    * changes based on review

--- x-pack/legacy/plugins/ml/public/jobs/new_job_new/pages/components/time_range_step/index.ts
+++ x-pack/legacy/plugins/ml/public/jobs/new_job_new/pages/components/time_range_step/index.ts
@@ -4,16 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-
-
-
-export const VALIDATION_STATUS = {
-  ERROR: 'error',
-  INFO: 'info',
-  SUCCESS: 'success',
-  WARNING: 'warning'
-};
-
-export const SKIP_BUCKET_SPAN_ESTIMATION = true;
-
-export const ALLOWED_DATA_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+export { TimeRangeStep } from './time_range';
