784962b87a945e60a01733f0f002b8c5d0852968
Tre' Seymour <wayne.seymour@elastic.co>
2019-08-02 13:26:23 -0600

    Fixup broken file paths (_that reach outside_of_kbn-test_), found while running yarn script for functional_tests_server, after refactoring ftr under kbn-test.

--- packages/kbn-test/src/functional_test_runner/lib/mocha/reporter/reporter.js
+++ packages/kbn-test/src/functional_test_runner/lib/mocha/reporter/reporter.js
@@ -23,12 +23,12 @@ import Mocha from 'mocha';
 import { ToolingLogTextWriter } from '@kbn/dev-utils';
 import moment from 'moment';
 
-import { setupJUnitReportGeneration } from '../../../../dev';
+import { setupJUnitReportGeneration } from '../../../../../../../src/dev';
 import * as colors from './colors';
 import * as symbols from './symbols';
 import { ms } from './ms';
 import { writeEpilogue } from './write_epilogue';
-import { recordLog, snapshotLogsForRunnable } from '../../../../dev/mocha/log_cache';
+import { recordLog, snapshotLogsForRunnable } from '../../../../../../../src/dev/mocha/log_cache';
 
 export function MochaReporterProvider({ getService }) {
   const log = getService('log');
@@ -60,7 +60,9 @@ export function MochaReporterProvider({ getService }) {
 
     onStart = () => {
       if (config.get('mochaReporter.captureLogOutput')) {
-        log.warning('debug logs are being captured, only error logs will be written to the console');
+        log.warning(
+          'debug logs are being captured, only error logs will be written to the console'
+        );
 
         reporterCaptureStartTime = moment();
         originalLogWriters = log.getWriters();
@@ -68,47 +70,47 @@ export function MochaReporterProvider({ getService }) {
         log.setWriters([
           new ToolingLogTextWriter({
             level: 'error',
-            writeTo: process.stdout
+            writeTo: process.stdout,
           }),
           new ToolingLogTextWriter({
             level: 'debug',
             writeTo: {
-              write: (line) => {
+              write: line => {
                 // if the current runnable is a beforeEach hook then
                 // `runner.suite` is set to the suite that defined the
                 // hook, rather than the suite executing, so instead we
                 // grab the suite from the test, but that's only available
                 // when we are doing something test specific, so for global
                 // hooks we fallback to `runner.suite`
-                const currentSuite = this.runner.test
-                  ? this.runner.test.parent
-                  : this.runner.suite;
+                const currentSuite = this.runner.test ? this.runner.test.parent : this.runner.suite;
 
                 // We are computing the difference between the time when this
                 // reporter has started and the time when each line are being
                 // logged in order to be able to label the test results log lines
                 // with this relative time information
                 const diffTimeSinceStart = moment().diff(reporterCaptureStartTime);
-                const readableDiffTimeSinceStart = `[${moment(diffTimeSinceStart).format('HH:mm:ss')}] `;
+                const readableDiffTimeSinceStart = `[${moment(diffTimeSinceStart).format(
+                  'HH:mm:ss'
+                )}] `;
 
                 recordLog(currentSuite, `${readableDiffTimeSinceStart} ${line}`);
-              }
-            }
-          })
+              },
+            },
+          }),
         ]);
       }
 
       log.write('');
-    }
+    };
 
     onHookStart = hook => {
       log.write(`-> ${colors.suite(hook.title)}`);
       log.indent(2);
-    }
+    };
 
     onHookEnd = () => {
       log.indent(-2);
-    }
+    };
 
     onSuiteStart = suite => {
       if (!suite.root) {
@@ -116,34 +118,34 @@ export function MochaReporterProvider({ getService }) {
       }
 
       log.indent(2);
-    }
+    };
 
     onSuiteEnd = () => {
       if (log.indent(-2) === 0) {
         log.write();
       }
-    }
+    };
 
     onTestStart = test => {
       log.write(`-> ${test.title}`);
       log.indent(2);
-    }
+    };
 
-    onTestEnd = (test) => {
+    onTestEnd = test => {
       snapshotLogsForRunnable(test);
       log.indent(-2);
-    }
+    };
 
     onPending = test => {
       log.write('-> ' + colors.pending(test.title));
       log.indent(2);
-    }
+    };
 
     onPass = test => {
       const time = colors.speed(test.speed, ` (${ms(test.duration)})`);
       const pass = colors.pass(`${symbols.ok} pass`);
       log.write(`- ${pass} ${time} "${test.fullTitle()}"`);
-    }
+    };
 
     onFail = runnable => {
       // NOTE: this is super gross
@@ -155,7 +157,7 @@ export function MochaReporterProvider({ getService }) {
       //
       let output = '';
       const realLog = console.log;
-      console.log = (...args) => output += `${format(...args)}\n`;
+      console.log = (...args) => (output += `${format(...args)}\n`);
       try {
         Mocha.reporters.Base.list([runnable]);
       } finally {
@@ -164,21 +166,21 @@ export function MochaReporterProvider({ getService }) {
 
       log.write(
         `- ${colors.fail(`${symbols.err} fail: "${runnable.fullTitle()}"`)}` +
-        '\n' +
-        output
-          .split('\n')
-          // drop the first two lines, (empty + test title)
-          .slice(2)
-          // move leading colors behind leading spaces
-          .map(line => line.replace(/^((?:\[.+m)+)(\s+)/, '$2$1'))
-          .map(line => ` ${line}`)
-          .join('\n')
+          '\n' +
+          output
+            .split('\n')
+            // drop the first two lines, (empty + test title)
+            .slice(2)
+            // move leading colors behind leading spaces
+            .map(line => line.replace(/^((?:\[.+m)+)(\s+)/, '$2$1'))
+            .map(line => ` ${line}`)
+            .join('\n')
       );
 
       // failed hooks trigger the `onFail(runnable)` callback, so we snapshot the logs for
       // them here. Tests will re-capture the snapshot in `onTestEnd()`
       snapshotLogsForRunnable(runnable);
-    }
+    };
 
     onEnd = () => {
       if (originalLogWriters) {
@@ -186,6 +188,6 @@ export function MochaReporterProvider({ getService }) {
       }
 
       writeEpilogue(log, this.stats);
-    }
+    };
   };
 }
