392b8e16f783aa44c5df0930a1dcba66f2714537
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2019-08-03 19:08:59 +0800

    [SIEM] Add chart interactions - update date picker after brush selection on charts (#42440)
    * add click and brush events
    
    * replace setAbsoluteRangeDatePicker
    
    * fix type error
    
    * remove a not necessary export

--- x-pack/legacy/plugins/siem/public/pages/hosts/host_details.tsx
+++ x-pack/legacy/plugins/siem/public/pages/hosts/host_details.tsx
@@ -151,6 +151,11 @@ const HostDetailsComponent = pure<HostDetailsComponentProps>(
                             refetch={refetch}
                             setQuery={setQuery}
                             to={to}
+                            narrowDateRange={(min: number, max: number) => {
+                              setTimeout(() => {
+                                setAbsoluteRangeDatePicker({ id: 'global', from: min, to: max });
+                              }, 500);
+                            }}
                           />
                         )}
                       </KpiHostDetailsQuery>
