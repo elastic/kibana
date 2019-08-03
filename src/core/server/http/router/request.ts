d394ded2ac71227312fd6f4bdb36940bbb9840d3
Mikhail Shustov <restrry@gmail.com>
2019-08-03 09:58:35 +0200

    Document HTTP service (#42331)
    * improving naming, add TSDoc
    
    * re-genereate docs
    
    * use response name in migration guide
    
    * Apply suggestions from code review
    
    Co-Authored-By: Josh Dover <me@joshdover.com>
    
    * place docs near the related code
    
    * re-generate docs
    
    * mark code as example to reduce noise in http-service.md

--- src/core/server/http/router/request.ts
+++ src/core/server/http/router/request.ts
@@ -39,15 +39,21 @@ export interface KibanaRequestRoute {
 }
 
 /**
+ * @deprecated
+ * `hapi` request object, supported during migration process only for backward compatibility.
+ * @public
+ */
+export interface LegacyRequest extends Request {} // eslint-disable-line @typescript-eslint/no-empty-interface
+
+/**
  * Kibana specific abstraction for an incoming request.
  * @public
- * */
+ */
 export class KibanaRequest<Params = unknown, Query = unknown, Body = unknown> {
   /**
    * Factory for creating requests. Validates the request before creating an
    * instance of a KibanaRequest.
    * @internal
-   *
    */
   public static from<P extends ObjectType, Q extends ObjectType, B extends ObjectType>(
     req: Request,
@@ -68,6 +74,7 @@ export class KibanaRequest<Params = unknown, Query = unknown, Body = unknown> {
    * Validates the different parts of a request based on the schemas defined for
    * the route. Builds up the actual params, query and body object that will be
    * received in the route handler.
+   * @internal
    */
   private static validate<P extends ObjectType, Q extends ObjectType, B extends ObjectType>(
     req: Request,
@@ -102,8 +109,9 @@ export class KibanaRequest<Params = unknown, Query = unknown, Body = unknown> {
 
     return { query, params, body };
   }
-
+  /** a WHATWG URL standard object. */
   public readonly url: Url;
+  /** matched route details */
   public readonly route: RecursiveReadonly<KibanaRequestRoute>;
   /**
    * Readonly copy of incoming request headers.
@@ -153,14 +161,14 @@ export class KibanaRequest<Params = unknown, Query = unknown, Body = unknown> {
  * Returns underlying Hapi Request
  * @internal
  */
-export const ensureRawRequest = (request: KibanaRequest | Request) =>
+export const ensureRawRequest = (request: KibanaRequest | LegacyRequest) =>
   isKibanaRequest(request) ? request[requestSymbol] : request;
 
 function isKibanaRequest(request: unknown): request is KibanaRequest {
   return request instanceof KibanaRequest;
 }
 
-function isRequest(request: any): request is Request {
+function isRequest(request: any): request is LegacyRequest {
   try {
     return request.raw.req && typeof request.raw.req === 'object';
   } catch {
@@ -172,6 +180,6 @@ function isRequest(request: any): request is Request {
  * Checks if an incoming request either KibanaRequest or Legacy.Request
  * @internal
  */
-export function isRealRequest(request: unknown): request is KibanaRequest | Request {
+export function isRealRequest(request: unknown): request is KibanaRequest | LegacyRequest {
   return isKibanaRequest(request) || isRequest(request);
 }
