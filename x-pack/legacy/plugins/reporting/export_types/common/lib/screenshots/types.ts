c0fc137f0ae26e950a470dc6953e8b864b8e69d6
Timothy Sullivan <tsullivan@elastic.co>
2019-08-05 14:58:58 -0700

    this is for later

--- x-pack/legacy/plugins/reporting/export_types/common/lib/screenshots/types.ts
+++ x-pack/legacy/plugins/reporting/export_types/common/lib/screenshots/types.ts
@@ -4,18 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import * as Rx from 'rxjs';
 import { ElementPosition } from '../../../../types';
 import { HeadlessChromiumDriver as HeadlessBrowser } from '../../../../server/browsers/chromium/driver';
 import { LayoutInstance } from '../../layouts/layout';
 
 export interface ScreenshotObservableOpts {
-  browserDriver$: {
-    driver$: Rx.Observable<HeadlessBrowser>;
-    consoleMessage$: Rx.Observable<string>;
-    message$: Rx.Observable<string>;
-    exit$: Rx.Observable<never>;
-  };
   url: string;
   conditionalHeaders: any;
   layout: LayoutInstance;
