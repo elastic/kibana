b46a628524461953f3dbce69418225da8edb5c6e
Tim Sullivan <tsullivan@users.noreply.github.com>
2019-08-06 12:19:29 -0700

    [Reporting/Refactor] Break up screenshots.ts, consolidate logging (#42304)
    * [Reporting] Sanitize 409 error log message
    
    * [Reporting] Break up screenshots libs
    
    * Revert "[Reporting] Sanitize 409 error log message"
    
    This reverts commit 61998b5847f5dcbdce98c9db01a9bf1152f254f1.
    
    * [Reporting] Sanitize 409 error log message
    
    * this is for later

--- x-pack/legacy/plugins/reporting/export_types/printable_pdf/server/lib/generate_pdf.js
+++ x-pack/legacy/plugins/reporting/export_types/printable_pdf/server/lib/generate_pdf.js
@@ -33,7 +33,8 @@ function generatePdfObservableFn(server) {
 
   const urlScreenshotsObservable = (urls, conditionalHeaders, layout, browserTimezone) => {
     return Rx.from(urls).pipe(
-      mergeMap(url => screenshotsObservable(url, conditionalHeaders, layout, browserTimezone),
+      mergeMap(
+        url => screenshotsObservable({ url, conditionalHeaders, layout, browserTimezone }),
         captureConcurrency
       )
     );
