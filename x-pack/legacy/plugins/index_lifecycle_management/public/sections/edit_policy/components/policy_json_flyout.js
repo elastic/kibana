ab0ffb93ea553e2cf61bfb2e819f95864d02ef2d
CJ Cenizal <cj@cenizal.com>
2019-08-07 08:22:16 -0700

    Copy endpoint. Use EuiCopy component. Add description text. Update title and endpoint when there is no name specified.

--- x-pack/legacy/plugins/index_lifecycle_management/public/sections/edit_policy/components/policy_json_flyout.js
+++ x-pack/legacy/plugins/index_lifecycle_management/public/sections/edit_policy/components/policy_json_flyout.js
@@ -9,16 +9,18 @@ import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
 import PropTypes from 'prop-types';
 import { toastNotifications } from 'ui/notify';
-import copy from 'copy-to-clipboard';
 
 import {
   EuiButton,
   EuiCodeEditor,
+  EuiCopy,
+  EuiFlyout,
   EuiFlyoutBody,
   EuiFlyoutFooter,
-  EuiFlyout,
   EuiFlyoutHeader,
   EuiPortal,
+  EuiSpacer,
+  EuiText,
   EuiTitle,
 } from '@elastic/eui';
 
@@ -36,20 +38,10 @@ export class PolicyJsonFlyout extends PureComponent {
     }, null, 2);
   }
 
-  copyToClipboard(lifecycle) {
-    copy(this.getEsJson(lifecycle));
-    toastNotifications.add(i18n.translate(
-      'xpack.indexLifecycleMgmt.editPolicy.policyJsonFlyout.copiedToClipboardMessage',
-      {
-        defaultMessage: 'JSON copied to clipboard'
-      }
-    ));
-  }
-
   render() {
     const { lifecycle, close, policyName } = this.props;
-    const endpoint = policyName ? `PUT _ilm/policy/${policyName}\n` : '';
-    const jsonString = `${endpoint}${this.getEsJson(lifecycle)}`;
+    const endpoint = `PUT _ilm/policy/${policyName || '{policyName}'}\n`;
+    const request = `${endpoint}${this.getEsJson(lifecycle)}`;
 
     return (
       <EuiPortal>
@@ -57,22 +49,40 @@ export class PolicyJsonFlyout extends PureComponent {
           <EuiFlyoutHeader>
             <EuiTitle>
               <h2>
-                <FormattedMessage
-                  id="xpack.indexLifecycleMgmt.policyJsonFlyout.title"
-                  defaultMessage="JSON for index lifecycle policy {policyName}"
-                  values={{ policyName }}
-                />
+                {policyName ? (
+                  <FormattedMessage
+                    id="xpack.indexLifecycleMgmt.policyJsonFlyout.namedTitle"
+                    defaultMessage="Request for {policyName}"
+                    values={{ policyName }}
+                  />
+                ) : (
+                  <FormattedMessage
+                    id="xpack.indexLifecycleMgmt.policyJsonFlyout.unnamedTitle"
+                    defaultMessage="Request"
+                  />
+                )}
               </h2>
             </EuiTitle>
           </EuiFlyoutHeader>
 
           <EuiFlyoutBody>
+            <EuiText>
+              <p>
+                <FormattedMessage
+                  id="xpack.indexLifecycleMgmt.policyJsonFlyout.descriptionText"
+                  defaultMessage="This is the underlying request to Elasticsearch that will create or update this index lifecycle policy."
+                />
+              </p>
+            </EuiText>
+
+            <EuiSpacer />
+
             <EuiCodeEditor
               mode="json"
               theme="textmate"
               isReadOnly
               setOptions={{ maxLines: Infinity, useWorker: false }}
-              value={jsonString}
+              value={request}
               editorProps={{
                 $blockScrolling: Infinity
               }}
@@ -80,12 +90,26 @@ export class PolicyJsonFlyout extends PureComponent {
           </EuiFlyoutBody>
 
           <EuiFlyoutFooter>
-            <EuiButton onClick={() => this.copyToClipboard(lifecycle)}>
-              <FormattedMessage
-                id="xpack.indexLifecycleMgmt.policyJsonFlyout.copyToClipboardButton"
-                defaultMessage="Copy to clipboard"
-              />
-            </EuiButton>
+            <EuiCopy textToCopy={request}>
+              {copy => (
+                <EuiButton
+                  onClick={() => {
+                    copy();
+                    toastNotifications.add(i18n.translate(
+                      'xpack.indexLifecycleMgmt.editPolicy.policyJsonFlyout.copiedToClipboardMessage',
+                      {
+                        defaultMessage: 'Request copied to clipboard'
+                      }
+                    ));
+                  }}
+                >
+                  <FormattedMessage
+                    id="xpack.indexLifecycleMgmt.policyJsonFlyout.copyToClipboardButton"
+                    defaultMessage="Copy to clipboard"
+                  />
+                </EuiButton>
+              )}
+            </EuiCopy>
           </EuiFlyoutFooter>
         </EuiFlyout>
       </EuiPortal>
