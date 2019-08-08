588a50b7073b45d3ad96d2b57915ae769222e970
streamich <streamich@gmail.com>
2019-08-08 22:20:00 +0200

    test: 💍 try fixing functional test on CI
    These tests pass locally, but fail on CI. This is a stab to fix it on
    CI.

--- test/plugin_functional/plugins/kbn_tp_embeddable_explorer/public/index.ts
+++ test/plugin_functional/plugins/kbn_tp_embeddable_explorer/public/index.ts
@@ -27,24 +27,37 @@ import {
   ContactCardEmbeddableFactory,
 } from '../../../../../src/legacy/core_plugins/embeddable_api/public/np_ready/public/lib/test_samples';
 
-const helloWorldAction = new HelloWorldAction(npStart.core.overlays);
-const sayHelloAction = new SayHelloAction(alert);
-const sendMessageAction = new SendMessageAction(npStart.core.overlays);
-const helloWorldEmbeddableFactory = new HelloWorldEmbeddableFactory();
-const contactCardEmbeddableFactory = new ContactCardEmbeddableFactory(
-  {},
-  start.executeTriggerActions,
-  npStart.core.overlays
-);
-
-start.registerAction(helloWorldAction);
-start.registerAction(sayHelloAction);
-start.registerAction(sendMessageAction);
-start.registerEmbeddableFactory(helloWorldEmbeddableFactory.type, helloWorldEmbeddableFactory);
-start.registerEmbeddableFactory(contactCardEmbeddableFactory.type, contactCardEmbeddableFactory);
-
 import { Plugin as EmbeddableExplorer } from './plugin';
 import { createShim } from './shim';
 
-const embeddableExplorer = new EmbeddableExplorer();
-embeddableExplorer.start(createShim());
+const onCoreReady = (callback: () => void) => {
+  if (npStart.core) {
+    console.log('CORE READY'); // eslint-disable-line
+    callback();
+    return;
+  }
+
+  console.log('WAITING FOR CORE'); // eslint-disable-line
+  setTimeout(() => onCoreReady(callback), 10);
+};
+
+onCoreReady(() => {
+  const helloWorldAction = new HelloWorldAction(npStart.core.overlays);
+  const sayHelloAction = new SayHelloAction(alert);
+  const sendMessageAction = new SendMessageAction(npStart.core.overlays);
+  const helloWorldEmbeddableFactory = new HelloWorldEmbeddableFactory();
+  const contactCardEmbeddableFactory = new ContactCardEmbeddableFactory(
+    {},
+    start.executeTriggerActions,
+    npStart.core.overlays
+  );
+
+  start.registerAction(helloWorldAction);
+  start.registerAction(sayHelloAction);
+  start.registerAction(sendMessageAction);
+  start.registerEmbeddableFactory(helloWorldEmbeddableFactory.type, helloWorldEmbeddableFactory);
+  start.registerEmbeddableFactory(contactCardEmbeddableFactory.type, contactCardEmbeddableFactory);
+
+  const embeddableExplorer = new EmbeddableExplorer();
+  embeddableExplorer.start(createShim());
+});
