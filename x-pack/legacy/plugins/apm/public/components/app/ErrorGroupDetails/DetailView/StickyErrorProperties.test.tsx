251ccd973e3b5c903e453b105a742c7e491ac547
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-08-08 18:20:50 +0200

    Add tests for `url.full` and `error.page.url`

--- x-pack/legacy/plugins/apm/public/components/app/ErrorGroupDetails/DetailView/StickyErrorProperties.test.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/ErrorGroupDetails/DetailView/StickyErrorProperties.test.tsx
@@ -4,12 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
 import { APMError } from '../../../../../typings/es_schemas/ui/APMError';
 import { Transaction } from '../../../../../typings/es_schemas/ui/Transaction';
 import { IStickyProperty } from '../../../shared/StickyProperties';
 import { StickyErrorProperties } from './StickyErrorProperties';
+import {
+  ERROR_PAGE_URL,
+  URL_FULL
+} from '../../../../../common/elasticsearch_fieldnames';
 
 describe('StickyErrorProperties', () => {
   it('should render StickyProperties', () => {
@@ -44,27 +48,43 @@ describe('StickyErrorProperties', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('url.full', () => {
+    const error = {
+      agent: { name: 'nodejs' },
+      url: { full: 'myFullUrl' }
+    } as APMError;
+
+    const wrapper = shallow(
+      <StickyErrorProperties error={error} transaction={undefined} />
+    );
+    const urlValue = getValueByFieldName(wrapper, URL_FULL);
+    expect(urlValue).toBe('myFullUrl');
+  });
+
+  it('error.page.url', () => {
+    const error = {
+      agent: { name: 'rum-js' },
+      error: { page: { url: 'myPageUrl' } }
+    } as APMError;
+
+    const wrapper = shallow(
+      <StickyErrorProperties error={error} transaction={undefined} />
+    );
+    const urlValue = getValueByFieldName(wrapper, ERROR_PAGE_URL);
+    expect(urlValue).toBe('myPageUrl');
+  });
+
   describe('error.exception.handled', () => {
-    function getIsHandledValue(error: APMError) {
-      const wrapper = shallow(
-        <StickyErrorProperties error={error} transaction={undefined} />
-      );
-
-      const stickyProps = wrapper.prop('stickyProperties') as IStickyProperty[];
-      const handledProp = stickyProps.find(
-        prop => prop.fieldName === 'error.exception.handled'
-      );
-
-      return handledProp && handledProp.val;
-    }
-
     it('should should render "true"', () => {
       const error = {
         agent: { name: 'nodejs' },
         error: { exception: [{ handled: true }] }
       } as APMError;
-      const isHandledValue = getIsHandledValue(error);
-      expect(isHandledValue).toBe('true');
+      const wrapper = shallow(
+        <StickyErrorProperties error={error} transaction={undefined} />
+      );
+      const value = getValueByFieldName(wrapper, 'error.exception.handled');
+      expect(value).toBe('true');
     });
 
     it('should should render "false"', () => {
@@ -72,14 +92,26 @@ describe('StickyErrorProperties', () => {
         agent: { name: 'nodejs' },
         error: { exception: [{ handled: false }] }
       } as APMError;
-      const isHandledValue = getIsHandledValue(error);
-      expect(isHandledValue).toBe('false');
+      const wrapper = shallow(
+        <StickyErrorProperties error={error} transaction={undefined} />
+      );
+      const value = getValueByFieldName(wrapper, 'error.exception.handled');
+      expect(value).toBe('false');
     });
 
     it('should should render "N/A"', () => {
       const error = { agent: { name: 'nodejs' } } as APMError;
-      const isHandledValue = getIsHandledValue(error);
-      expect(isHandledValue).toBe('N/A');
+      const wrapper = shallow(
+        <StickyErrorProperties error={error} transaction={undefined} />
+      );
+      const value = getValueByFieldName(wrapper, 'error.exception.handled');
+      expect(value).toBe('N/A');
     });
   });
 });
+
+function getValueByFieldName(wrapper: ShallowWrapper, fieldName: string) {
+  const stickyProps = wrapper.prop('stickyProperties') as IStickyProperty[];
+  const prop = stickyProps.find(p => p.fieldName === fieldName);
+  return prop && prop.val;
+}
