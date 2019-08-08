77766256eabe3e91dfd84f7c8575826dee561664
maryia-lapata <mary.lopato@gmail.com>
2019-08-08 15:20:08 +0300

    Update agg_params_helper.test.ts

--- src/legacy/ui/public/vis/editors/default/components/agg_params_helper.test.ts
+++ src/legacy/ui/public/vis/editors/default/components/agg_params_helper.test.ts
@@ -37,7 +37,7 @@ jest.mock('ui/agg_types', () => ({
     },
   },
 }));
-jest.mock('../default_editor_utils', () => ({
+jest.mock('../utils', () => ({
   groupAndSortBy: jest.fn(() => ['indexedFields']),
 }));
 
