31dd2bd41c55844810d38d23baefd8be43f9fa50
spalger <email@spalger.com>
2019-08-13 15:56:36 -0700

    set `PERCY_BRANCH` and `PERCY_TARGET_BRANCH`

--- src/dev/ci_setup/get_percy_env.js
+++ src/dev/ci_setup/get_percy_env.js
@@ -23,22 +23,34 @@ const pkg = require('../../../package.json');
 const { stdout: commit } = execa.sync('git', ['rev-parse', 'HEAD']);
 const shortCommit = commit.slice(0, 8);
 
-const isPr = process.env.JOB_NAME.includes('elastic+kibana+pull-request');
-let branch = process.env.PR_TARGET_BRANCH;
+if (!process.env.JOB_NAME) {
+  throw new Error('getPercyEnv: JOB_NAME environment variable required');
+}
 
-if (!isPr) {
+const isPr = process.env.JOB_NAME.includes('elastic+kibana+pull-request');
+if (isPr && !(process.env.PR_TARGET_BRANCH && process.env.PR_SOURCE_BRANCH)) {
+  throw new Error('Unable to determine percy environment in prs without [PR_TARGET_BRANCH] and [PR_SOURCE_BRANCH] environment variables');
+}
+
+let branch;
+if (isPr) {
+  branch = process.env.PR_SOURCE_BRANCH;
+} else {
   if (!process.env.branch_specifier) {
-    throw new Error('Unable to determine PERCY_BRANCH without a [branch_specifier]');
+    throw new Error('getPercyEnv: branch_specifier environment variable required');
   }
 
-  [, branch] = process.env.branch_specifier.split('refs/heads/');
+  branch = process.env.branch_specifier.split('refs/heads/')[1];
+
   if (!branch) {
     throw new Error(
-      `Unable to determine PERCY_BRANCH from [branch_specifier=${process.env.branch_specifier}], expected it to start with [refs/heads/]`
+      `getPercyEnv: branch_specifier [${process.env.branch_specifier}] must start with [refs/heads/]`
     );
   }
 }
 
 console.log(`export PERCY_PARALLEL_TOTAL=2;`);
-console.log(`export PERCY_PARALLEL_NONCE="${shortCommit}/${isPr ? 'PR' : pkg.branch}/${process.env.BUILD_ID}";`);
+console.log(`export PERCY_PARALLEL_NONCE="${shortCommit}/${isPr ? 'PR' : branch}/${process.env.BUILD_ID}";`);
 console.log(`export PERCY_BRANCH="${branch}";`);
+// percy snapshots always target pkg.branch, so that feature branches can be based on master/7.x/etc.
+console.log(`export PERCY_TARGET_BRANCH="${isPr ? process.env.PR_TARGET_BRANCH : pkg.branch}";`);
