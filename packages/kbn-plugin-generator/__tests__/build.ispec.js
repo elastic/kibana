c347097af314a20ff02ac8c99f66d5d448e537d3
Tre' Seymour <wayne.seymour@elastic.co>
2019-08-13 14:13:17 -0600

    Add first test + confs for babel and jest, and add yarn script.

---
+++ packages/kbn-plugin-generator/__tests__/build.ispec.js
@@ -0,0 +1,48 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/* eslint-disable no-restricted-syntax */
+import { spawn } from 'child_process';
+import { resolve } from 'path';
+import { promises } from 'fs';
+import { snakeCase } from 'lodash';
+
+const ROOT_DIR = resolve(__dirname, '../../../');
+
+describe('build a plugin', () => {
+  const pluginName = 'ispec-plugin';
+  const snakeCased = snakeCase(pluginName);
+  // eslint-disable-next-line no-undef
+  beforeAll(done => {
+    const create = spawn(process.execPath, ['scripts/generate_plugin.js', pluginName], {
+      cwd: ROOT_DIR,
+    });
+    create.stdout.on('data', () => {
+      create.stdin.write('\n');
+    });
+    create.on('close', done);
+  }, 150000);
+
+  it(`should have created a plugin in a directory named ${snakeCased}`, async () => {
+    const generatedPath = resolve(ROOT_DIR, `plugins/${snakeCased}`);
+    const stats = await promises.stat(generatedPath);
+    // eslint-disable-next-line no-undef
+    expect(stats.isDirectory()).toBe(true);
+  });
+});
