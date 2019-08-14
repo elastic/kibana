f3391bd05c967267a6c9c525765bb26a20d327ec
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-08-14 22:26:57 +0200

    [7.x] [APM] Fix missing RUM url (#42940) (#43021)
    * [APM] Fix missing RUM url (#42940)
    
    * [APM] Fix missing RUM url
    
    * Reduce to single conditional branch
    
    * Fix test
    
    * Add tests for `url.full` and `error.page.url`
    
    # Conflicts:
    #	x-pack/legacy/plugins/apm/public/components/app/ErrorGroupDetails/DetailView/StickyErrorProperties.test.tsx
    #	x-pack/legacy/plugins/apm/public/components/app/ErrorGroupDetails/DetailView/StickyErrorProperties.tsx
    
    * Fix missing import and `isHandled`

--- x-pack/legacy/plugins/apm/public/components/app/ErrorGroupDetails/DetailView/StickyErrorProperties.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/ErrorGroupDetails/DetailView/StickyErrorProperties.tsx
@@ -5,6 +5,7 @@
  */
 
 import { i18n } from '@kbn/i18n';
+import { isBoolean } from 'lodash';
 import React, { Fragment } from 'react';
 import { idx } from '@kbn/elastic-idx';
 import {
@@ -12,13 +13,15 @@ import {
   HTTP_REQUEST_METHOD,
   TRANSACTION_ID,
   URL_FULL,
-  USER_ID
+  USER_ID,
+  ERROR_PAGE_URL
 } from '../../../../../common/elasticsearch_fieldnames';
 import { NOT_AVAILABLE_LABEL } from '../../../../../common/i18n';
 import { APMError } from '../../../../../typings/es_schemas/ui/APMError';
 import { Transaction } from '../../../../../typings/es_schemas/ui/Transaction';
 import { StickyProperties } from '../../../shared/StickyProperties';
 import { TransactionLink } from '../../../shared/Links/apm/TransactionLink';
+import { isRumAgentName } from '../../../../../common/agent_name';
 
 interface Props {
   error: APMError;
@@ -47,6 +50,19 @@ function TransactionLinkWrapper({
 }
 
 export function StickyErrorProperties({ error, transaction }: Props) {
+  const isHandled = idx(error, _ => _.error.exception[0].handled);
+  const isRumAgent = isRumAgentName(error.agent.name);
+
+  const { urlFieldName, urlValue } = isRumAgent
+    ? {
+        urlFieldName: ERROR_PAGE_URL,
+        urlValue: idx(error, _ => _.error.page.url)
+      }
+    : {
+        urlFieldName: URL_FULL,
+        urlValue: idx(error, _ => _.url.full)
+      };
+
   const stickyProperties = [
     {
       fieldName: '@timestamp',
@@ -57,12 +73,9 @@ export function StickyErrorProperties({ error, transaction }: Props) {
       width: '50%'
     },
     {
-      fieldName: URL_FULL,
+      fieldName: urlFieldName,
       label: 'URL',
-      val:
-        idx(error, _ => _.context.page.url) ||
-        idx(error, _ => _.url.full) ||
-        NOT_AVAILABLE_LABEL,
+      val: urlValue || NOT_AVAILABLE_LABEL,
       truncated: true,
       width: '50%'
     },
@@ -79,9 +92,7 @@ export function StickyErrorProperties({ error, transaction }: Props) {
       label: i18n.translate('xpack.apm.errorGroupDetails.handledLabel', {
         defaultMessage: 'Handled'
       }),
-      val:
-        String(idx(error, _ => _.error.exception[0].handled)) ||
-        NOT_AVAILABLE_LABEL,
+      val: isBoolean(isHandled) ? String(isHandled) : NOT_AVAILABLE_LABEL,
       width: '25%'
     },
     {
