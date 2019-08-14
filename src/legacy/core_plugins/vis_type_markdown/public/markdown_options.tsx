47b5dffa6ed513ef991de8b4450cc1a7c0d689a2
maryia-lapata <mary.lopato@gmail.com>
2019-08-14 13:20:11 +0300

    Textarea fills the entire height of the sidebar

--- src/legacy/core_plugins/vis_type_markdown/public/markdown_options.tsx
+++ src/legacy/core_plugins/vis_type_markdown/public/markdown_options.tsx
@@ -21,10 +21,8 @@ import React, { useCallback } from 'react';
 import {
   EuiPanel,
   EuiTitle,
-  EuiSpacer,
   EuiLink,
   EuiIcon,
-  EuiFormRow,
   EuiTextArea,
   EuiFlexGroup,
   EuiFlexItem,
@@ -43,38 +41,43 @@ function MarkdownOptions({ stateParams, setValue }: VisOptionsProps<MarkdownVisP
 
   return (
     <EuiPanel paddingSize="s">
-      <EuiFlexGroup gutterSize="none" justifyContent="spaceBetween" alignItems="baseline">
+      <EuiFlexGroup direction="column" gutterSize="m" className="mkdEditor">
         <EuiFlexItem grow={false}>
-          <EuiTitle size="xs">
-            <h2>Markdown</h2>
-          </EuiTitle>
+          <EuiFlexGroup gutterSize="none" justifyContent="spaceBetween" alignItems="baseline">
+            <EuiFlexItem grow={false}>
+              <EuiTitle size="xs">
+                <h2>Markdown</h2>
+              </EuiTitle>
+            </EuiFlexItem>
+
+            <EuiFlexItem grow={false}>
+              <EuiText size="xs">
+                <EuiLink
+                  href="https://help.github.com/articles/github-flavored-markdown/"
+                  target="_blank"
+                >
+                  <FormattedMessage
+                    id="visTypeMarkdown.params.helpLinkLabel"
+                    defaultMessage="Help"
+                  />{' '}
+                  <EuiIcon type="popout" size="s" />
+                </EuiLink>
+              </EuiText>
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </EuiFlexItem>
 
-        <EuiFlexItem grow={false}>
-          <EuiText size="xs">
-            <EuiLink
-              href="https://help.github.com/articles/github-flavored-markdown/"
-              target="_blank"
-            >
-              <FormattedMessage id="visTypeMarkdown.params.helpLinkLabel" defaultMessage="Help" />{' '}
-              <EuiIcon type="popout" size="s" />
-            </EuiLink>
-          </EuiText>
+        <EuiFlexItem>
+          <EuiTextArea
+            id="markdownVisInput"
+            value={stateParams.markdown}
+            onChange={({ target: { value } }) => onMarkdownUpdate(value)}
+            fullWidth={true}
+            data-test-subj="markdownTextarea"
+            // resize="none"
+          />
         </EuiFlexItem>
       </EuiFlexGroup>
-      <EuiSpacer size="s" />
-
-      <EuiFormRow fullWidth={true} compressed>
-        <EuiTextArea
-          id="markdownVisInput"
-          value={stateParams.markdown}
-          onChange={({ target: { value } }) => onMarkdownUpdate(value)}
-          rows={20}
-          fullWidth={true}
-          data-test-subj="markdownTextarea"
-          // resize="none"
-        />
-      </EuiFormRow>
     </EuiPanel>
   );
 }
