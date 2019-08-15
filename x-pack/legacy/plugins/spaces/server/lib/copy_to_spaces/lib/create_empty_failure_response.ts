6b53969c1fceef3752a7b849bcfb776efce6d42a
Larry Gregory <larry.gregory@elastic.co>
2019-08-15 15:39:09 -0400

    improve error handling when thrown from wrapped Saved Objects Client

--- x-pack/legacy/plugins/spaces/server/lib/copy_to_spaces/lib/create_empty_failure_response.ts
+++ x-pack/legacy/plugins/spaces/server/lib/copy_to_spaces/lib/create_empty_failure_response.ts
@@ -3,14 +3,23 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
+import Boom from 'boom';
 import { SavedObjectsImportError } from 'src/core/server';
 import { CopyToSpaceError } from '../types';
 
 export const createEmptyFailureResponse = (
-  errors?: Array<SavedObjectsImportError | CopyToSpaceError>
-) => ({
-  success: false,
-  successCount: 0,
-  errors,
-});
+  errors?: Array<SavedObjectsImportError | CopyToSpaceError | Boom>
+) => {
+  const errorMessages = (errors || []).map(error => {
+    if (Boom.isBoom(error as any)) {
+      return (error as Boom).output.payload;
+    }
+    return error;
+  });
+
+  return {
+    success: false,
+    successCount: 0,
+    errors: errorMessages,
+  };
+};
