705577a8215973080b7efdbd5d5816ba92de0a30
Zacqary Xeper <zacqary.xeper@elastic.co>
2019-08-15 14:26:50 -0500

    [Logs UI] Add analyis setup landing screen

--- x-pack/legacy/plugins/infra/public/pages/logs/analysis/page_setup_content.tsx
+++ x-pack/legacy/plugins/infra/public/pages/logs/analysis/page_setup_content.tsx
@@ -5,12 +5,69 @@
  */
 
 import React from 'react';
-
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiPageContentBody,
+  EuiText,
+  EuiTitle,
+  EuiSpacer,
+  EuiButton,
+} from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+import euiStyled from '../../../../../../common/eui_styled_components';
 import { useTrackPageview } from '../../../hooks/use_track_metric';
 
 export const AnalysisSetupContent = () => {
   useTrackPageview({ app: 'infra_logs', path: 'analysis_setup' });
   useTrackPageview({ app: 'infra_logs', path: 'analysis_setup', delay: 15000 });
 
-  return <div>Setup</div>;
+  return (
+    <AnalysisSetupPage>
+      <EuiPageBody>
+        <AnalysisPageContent
+          verticalPosition="center"
+          horizontalPosition="center"
+          data-test-subj="analysisSetupContent"
+          style={{ maxWidth: '518px' }}
+        >
+          <EuiPageContentHeader>
+            <EuiPageContentHeaderSection>
+              <EuiTitle size="m">
+                <h3>
+                  <FormattedMessage
+                    id="xpack.infra.analysisSetup.analysisSetupTitle"
+                    defaultMessage="Enable Machine Learning analysis"
+                  />
+                </h3>
+              </EuiTitle>
+            </EuiPageContentHeaderSection>
+          </EuiPageContentHeader>
+          <EuiPageContentBody>
+            <EuiText size="s">
+              <FormattedMessage
+                id="xpack.infra.analysisSetup.analysisSetupDescription"
+                defaultMessage="Use Machine Learning to automatically categorize log messages, and detect anomalous event counts and rare log messages."
+              />
+            </EuiText>
+            <EuiSpacer size="m" />
+
+            <EuiButton fill iconSide="right" iconType="check">
+              Create ML Job
+            </EuiButton>
+          </EuiPageContentBody>
+        </AnalysisPageContent>
+      </EuiPageBody>
+    </AnalysisSetupPage>
+  );
 };
+
+// https://github.com/elastic/eui/issues/2232
+const AnalysisPageContent = euiStyled(EuiPageContent)<{ style: object }>``;
+
+const AnalysisSetupPage = euiStyled(EuiPage)`
+  height: 100%;
+`;
