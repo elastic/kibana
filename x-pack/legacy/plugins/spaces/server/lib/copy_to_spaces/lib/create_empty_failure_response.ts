633085897e2e8ea8e2c9c25b414f42639c57c829
Larry Gregory <larry.gregory@elastic.co>
2019-08-15 16:11:05 -0400

    make TS happy

--- x-pack/legacy/plugins/spaces/server/lib/copy_to_spaces/lib/create_empty_failure_response.ts
+++ x-pack/legacy/plugins/spaces/server/lib/copy_to_spaces/lib/create_empty_failure_response.ts
@@ -3,18 +3,20 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import Boom from 'boom';
+import Boom, { Payload } from 'boom';
 import { SavedObjectsImportError } from 'src/core/server';
 import { CopyToSpaceError } from '../types';
 
 export const createEmptyFailureResponse = (
   errors?: Array<SavedObjectsImportError | CopyToSpaceError | Boom>
 ) => {
-  const errorMessages = (errors || []).map(error => {
+  const errorMessages: Array<SavedObjectsImportError | CopyToSpaceError | Payload> = (
+    errors || []
+  ).map(error => {
     if (Boom.isBoom(error as any)) {
-      return (error as Boom).output.payload;
+      return (error as Boom).output.payload as Payload;
     }
-    return error;
+    return error as SavedObjectsImportError | CopyToSpaceError;
   });
 
   return {
--- x-pack/legacy/plugins/spaces/server/lib/copy_to_spaces/types.ts
+++ x-pack/legacy/plugins/spaces/server/lib/copy_to_spaces/types.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { Payload } from 'boom';
 import { SavedObjectsImportError } from 'src/core/server';
 
 export interface CopyOptions {
@@ -38,6 +39,6 @@ export interface CopyResponse {
   [spaceId: string]: {
     success: boolean;
     successCount: number;
-    errors?: Array<SavedObjectsImportError | CopyToSpaceError>;
+    errors?: Array<SavedObjectsImportError | CopyToSpaceError | Payload>;
   };
 }
