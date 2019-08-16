44556d17b83aef2e9aadd03a076865ee7e8435b9
Mike Place <mike.place@elastic.co>
2019-08-16 14:54:17 +0200

    Better handling of Monitoring in Elastic Cloud

---
+++ x-pack/legacy/plugins/monitoring/public/components/no_data/blurbs/cloud_deployment.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { Fragment } from 'react';
+import { EuiTitle, EuiText, EuiTextColor, EuiLink } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+
+export const CloudDeployment = () => {
+  return (
+    <Fragment>
+      <EuiTitle size="l">
+        <h2>
+          <FormattedMessage
+            id="xpack.monitoring.noData.blurbs.cloudDeploymentTitle"
+            defaultMessage="Your monitoring data is not available here."
+          />
+        </h2>
+      </EuiTitle>
+      <EuiTextColor color="subdued">
+        <EuiText>
+          <p>
+            <FormattedMessage
+              id="xpack.monitoring.noData.blurbs.cloudDeploymentDescription"
+              defaultMessage="Please return to your "
+            />
+            <EuiLink href="https://cloud.elastic.co/deployments" target="_blank">
+              cloud dashboard.
+            </EuiLink>
+          </p>
+        </EuiText>
+      </EuiTextColor>
+    </Fragment>
+  );
+};
--- x-pack/legacy/plugins/monitoring/public/components/no_data/reasons/__tests__/reason_found.test.js
+++ x-pack/legacy/plugins/monitoring/public/components/no_data/reasons/__tests__/reason_found.test.js
@@ -39,6 +39,19 @@ describe('ReasonFound', () => {
     expect(component).toMatchSnapshot();
   });
 
+  test('should load ExplainExportersCloud component', () => {
+    const component = renderWithIntl(
+    <ReasonFound
+      reason={{
+        property: 'xpack.monitoring.exporters.cloud_enabled',
+          data: 'false',
+          context: 'fakeContext'
+      }}
+      enabled={enabler}
+    />
+  );
+})
+
   test('should load ExplainPluginEnabled component', () => {
     const component = renderWithIntl(
       <ReasonFound
