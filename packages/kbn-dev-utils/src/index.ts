5d73f4027d08a07020533050901908d187ce6df7
Spencer <email@spalger.com>
2019-08-16 07:13:54 -0700

    Move `src/dev/run` to @kbn/dev-utils (#43387)
    * move src/dev/run into `@kbn/dev-utils`
    
    * forgot to save a replacement
    
    # Conflicts:
    #	src/dev/renovate/run_build_renovate_config_cli.ts

--- packages/kbn-dev-utils/src/index.ts
+++ packages/kbn-dev-utils/src/index.ts
@@ -21,3 +21,4 @@ export { withProcRunner } from './proc_runner';
 export { ToolingLog, ToolingLogTextWriter, pickLevelFromFlags } from './tooling_log';
 export { createAbsolutePathSerializer } from './serializers';
 export { CA_CERT_PATH, ES_KEY_PATH, ES_CERT_PATH } from './certs';
+export { run, createFailError, createFlagError, combineErrors, isFailError } from './run';
