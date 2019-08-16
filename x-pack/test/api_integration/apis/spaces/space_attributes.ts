59060215af476a5a2ae9d7cacb0af2412e9c0fb8
Larry Gregory <larry.gregory@elastic.co>
2019-08-16 13:05:25 -0400

    allow for mixed case hex codes

---
+++ x-pack/test/api_integration/apis/spaces/space_attributes.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from '@kbn/expect/expect.js';
+import { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function ({ getService }: FtrProviderContext) {
+  const supertest = getService('supertest');
+
+  describe('space attributes', () => {
+
+    it('should allow a space to be created with a mixed-case hex color code', async () => {
+      await supertest
+        .post('/api/spaces/space')
+        .set('kbn-xsrf', 'xxx')
+        .send({
+          id: 'api-test-space',
+          name: 'api test space',
+          disabledFeatures: [],  
+          color: '#aaBB78'
+        })
+        .expect(200)
+        .then((response: Record<string, any>) => {
+          expect(response.body).to.eql({
+            id: 'api-test-space',
+            name: 'api test space',
+            disabledFeatures: [],
+            color: '#aaBB78'
+          });
+        });
+    });  
+  });
+}
