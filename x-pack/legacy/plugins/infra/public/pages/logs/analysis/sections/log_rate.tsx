a32fa03ed2a0a33f9d3d3c35a1205e9808bafb0f
Kerry Gallagher <k.gallagher.05@gmail.com>
2019-08-16 15:33:22 +0100

    Allow ability to toggle model bounds

--- x-pack/legacy/plugins/infra/public/pages/logs/analysis/sections/log_rate.tsx
+++ x-pack/legacy/plugins/infra/public/pages/logs/analysis/sections/log_rate.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useMemo, useCallback } from 'react';
+import React, { useMemo, useCallback, useState } from 'react';
 import { i18n } from '@kbn/i18n';
 import { first, last } from 'lodash';
 import moment from 'moment';
@@ -26,6 +26,7 @@ import {
   EuiLoadingChart,
   EuiSpacer,
   EuiEmptyPrompt,
+  EuiCheckbox,
 } from '@elastic/eui';
 import { GetLogEntryRateSuccessResponsePayload } from '../../../../../common/http_api/log_analysis/results/log_entry_rate';
 import { useLogEntryRateGraphData } from '../../../../containers/logs/log_analysis/log_analysis_graph_data/log_entry_rate';
@@ -50,6 +51,10 @@ export const LogRateResults = ({
     'xpack.infra.logs.analysis.logRateSectionLoadingAriaLabel',
     { defaultMessage: 'Loading log rate results' }
   );
+  const showModelBoundsLabel = i18n.translate(
+    'xpack.infra.logs.analysis.logRateSectionModelBoundsCheckboxLabel',
+    { defaultMessage: 'Show model bounds' }
+  );
 
   const { areaSeries, lineSeries } = useLogEntryRateGraphData({
     data: results,
@@ -73,6 +78,8 @@ export const LogRateResults = ({
       [dateFormat]
     ),
   };
+
+  const [isShowingModelBounds, setIsShowingModelBounds] = useState<boolean>(true);
 
   return (
     <>
@@ -105,77 +112,94 @@ export const LogRateResults = ({
           }
         />
       ) : (
-        <div style={{ height: 400, width: '100%' }}>
-          <Chart className="log-entry-rate-chart">
-            <Axis
-              id={getAxisId('timestamp')}
-              title={i18n.translate('xpack.infra.logs.analysis.logRateSectionXaxisTitle', {
-                defaultMessage: 'Time',
-              })}
-              position="bottom"
-              showOverlappingTicks
-              tickFormat={dateFormatter}
-            />
-            <Axis
-              id={getAxisId('values')}
-              title={i18n.translate('xpack.infra.logs.analysis.logRateSectionYaxisTitle', {
-                defaultMessage: 'Log entries',
-              })}
-              position="left"
-              tickFormat={value => Number(value).toFixed(0)}
-            />
-            <AreaSeries
-              id={areaSpecId}
-              name={i18n.translate('xpack.infra.logs.analysis.logRateSectionAreaSeriesName', {
-                defaultMessage: 'Expected',
-              })}
-              xScaleType="time"
-              yScaleType="linear"
-              xAccessor="x"
-              yAccessors={['max']}
-              y0Accessors={['min']}
-              data={areaSeries}
-              yScaleToDataExtent
-              curve={2}
-              areaSeriesStyle={
-                !isDarkMode()
-                  ? {
-                      line: { stroke: areaSeriesColour },
-                      area: { fill: areaSeriesColour, visible: true, opacity: 0.8 },
-                    }
-                  : undefined
-              }
-              customSeriesColors={
-                !isDarkMode() ? getColorsMap(areaSeriesColour, areaSpecId) : undefined
-              }
-            />
-            <LineSeries
-              id={lineSpecId}
-              name={i18n.translate('xpack.infra.logs.analysis.logRateSectionLineSeriesName', {
-                defaultMessage: 'Log entries (avg)',
-              })}
-              xScaleType="time"
-              yScaleType="linear"
-              xAccessor={0}
-              yAccessors={[1]}
-              data={lineSeries}
-              yScaleToDataExtent
-              curve={2}
-              lineSeriesStyle={
-                !isDarkMode()
-                  ? {
-                      line: { stroke: lineSeriesColour },
-                      point: { radius: 2, fill: lineSeriesColour },
-                    }
-                  : undefined
-              }
-              customSeriesColors={
-                !isDarkMode() ? getColorsMap(lineSeriesColour, lineSpecId) : undefined
-              }
-            />
-            <Settings tooltip={tooltipProps} theme={getChartTheme()} />
-          </Chart>
-        </div>
+        <>
+          <EuiFlexGroup justifyContent="flexEnd">
+            <EuiFlexItem grow={false}>
+              <EuiCheckbox
+                id={'showModelBoundsCheckbox'}
+                label={showModelBoundsLabel}
+                aria-label={showModelBoundsLabel}
+                checked={isShowingModelBounds}
+                onChange={e => {
+                  setIsShowingModelBounds(e.target.checked);
+                }}
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+          <div style={{ height: 400, width: '100%' }}>
+            <Chart className="log-entry-rate-chart">
+              <Axis
+                id={getAxisId('timestamp')}
+                title={i18n.translate('xpack.infra.logs.analysis.logRateSectionXaxisTitle', {
+                  defaultMessage: 'Time',
+                })}
+                position="bottom"
+                showOverlappingTicks
+                tickFormat={dateFormatter}
+              />
+              <Axis
+                id={getAxisId('values')}
+                title={i18n.translate('xpack.infra.logs.analysis.logRateSectionYaxisTitle', {
+                  defaultMessage: 'Log entries',
+                })}
+                position="left"
+                tickFormat={value => Number(value).toFixed(0)}
+              />
+              {isShowingModelBounds ? (
+                <AreaSeries
+                  id={areaSpecId}
+                  name={i18n.translate('xpack.infra.logs.analysis.logRateSectionAreaSeriesName', {
+                    defaultMessage: 'Expected',
+                  })}
+                  xScaleType="time"
+                  yScaleType="linear"
+                  xAccessor="x"
+                  yAccessors={['max']}
+                  y0Accessors={['min']}
+                  data={areaSeries}
+                  yScaleToDataExtent
+                  curve={2}
+                  areaSeriesStyle={
+                    !isDarkMode()
+                      ? {
+                          line: { stroke: areaSeriesColour },
+                          area: { fill: areaSeriesColour, visible: true, opacity: 0.8 },
+                        }
+                      : undefined
+                  }
+                  customSeriesColors={
+                    !isDarkMode() ? getColorsMap(areaSeriesColour, areaSpecId) : undefined
+                  }
+                />
+              ) : null}
+              <LineSeries
+                id={lineSpecId}
+                name={i18n.translate('xpack.infra.logs.analysis.logRateSectionLineSeriesName', {
+                  defaultMessage: 'Log entries (avg)',
+                })}
+                xScaleType="time"
+                yScaleType="linear"
+                xAccessor={0}
+                yAccessors={[1]}
+                data={lineSeries}
+                yScaleToDataExtent
+                curve={2}
+                lineSeriesStyle={
+                  !isDarkMode()
+                    ? {
+                        line: { stroke: lineSeriesColour },
+                        point: { radius: 2, fill: lineSeriesColour },
+                      }
+                    : undefined
+                }
+                customSeriesColors={
+                  !isDarkMode() ? getColorsMap(lineSeriesColour, lineSpecId) : undefined
+                }
+              />
+              <Settings tooltip={tooltipProps} theme={getChartTheme()} />
+            </Chart>
+          </div>
+        </>
       )}
     </>
   );
