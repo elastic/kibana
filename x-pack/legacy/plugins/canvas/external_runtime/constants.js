eb591140d2489f580c441fdb37df90c32b9ca4a0
spalger <email@spalger.com>
2019-08-19 10:52:04 -0700

    migrate external_runtime script to dev/run

--- x-pack/legacy/plugins/canvas/external_runtime/constants.js
+++ x-pack/legacy/plugins/canvas/external_runtime/constants.js
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 const RUNTIME_NAME = 'canvas_external_runtime';
--- x-pack/legacy/plugins/canvas/scripts/external_runtime.js
+++ x-pack/legacy/plugins/canvas/scripts/external_runtime.js
@@ -6,10 +6,14 @@
 
 const fs = require('fs');
 const path = require('path');
+const { pipeline } = require('stream');
+const { promisify } = require('util');
+
 const del = require('del');
-const devUtils = require('@kbn/dev-utils');
+const { run } = require('@kbn/dev-utils');
 const execa = require('execa');
-const getopts = require('getopts');
+
+const asyncPipeline = promisify(pipeline);
 
 const {
   RUNTIME_SRC,
@@ -18,97 +22,87 @@ const {
   RUNTIME_OUTPUT,
 } = require('../external_runtime/constants');
 
-const log = new devUtils.ToolingLog({
-  level: 'info',
-  writeTo: process.stdout,
-});
+run(
+  async ({ log, flags }) => {
+    const webpackConfig = path.resolve(RUNTIME_SRC, 'webpack.config.js');
 
-const unknownFlags = [];
-const flags = getopts(process.argv, {
-  boolean: ['run', 'clean', 'help', 'stats', 'dev'],
-  unknown(name) {
-    unknownFlags.push(name);
-  },
-});
+    const clean = () => {
+      log.info('Deleting previous build.');
+      del.sync([RUNTIME_OUTPUT], { force: true });
+    };
 
-if (unknownFlags.length) {
-  log.error(`Unknown flag(s): ${unknownFlags.join(', ')}`);
-  flags.help = true;
-  process.exitCode = 1;
-}
+    if (flags.clean) {
+      clean();
+    }
 
-if (flags.help) {
-  log.info(`
-    Build script for the Canvas External Runtime.
+    const options = {
+      cwd: KIBANA_ROOT,
+      stdio: ['ignore', 'inherit', 'inherit'],
+      buffer: false,
+    };
 
-    --run       Run a server with the runtime
-    --dev       Build and/or create stats in development mode.
-    --stats     Output Webpack statistics to a stats.json file.
-    --clean     Delete the existing build
-    --help      Show this message
-  `);
-  process.exit();
-}
+    const env = {};
 
-const webpackConfig = path.resolve(RUNTIME_SRC, 'webpack.config.js');
+    if (!flags.dev) {
+      env.NODE_ENV = 'production';
+    }
 
-const clean = () => {
-  log.info('Deleting previous build.');
-  del.sync([RUNTIME_OUTPUT], { force: true });
-};
+    if (flags.run) {
+      log.info('Starting Webpack Dev Server...');
+      execa.sync(
+        'yarn',
+        [
+          'webpack-dev-server',
+          '--config',
+          webpackConfig,
+          '--progress',
+          '--hide-modules',
+          '--display-entrypoints',
+          'false',
+          '--content-base',
+          RUNTIME_SRC,
+        ],
+        options
+      );
+      return;
+    }
 
-if (flags.clean) {
-  clean();
-}
+    if (flags.stats) {
+      log.info('Writing Webpack stats...');
+      const output = execa(
+        require.resolve('webpack/bin/webpack'),
+        ['--config', webpackConfig, '--profile', '--json'],
+        {
+          ...options,
+          env,
+          stdio: ['ignore', 'pipe', 'inherit'],
+        }
+      );
+      await asyncPipeline(output.stdout, fs.createWriteStream(STATS_OUTPUT));
+      log.success('...output written to', STATS_OUTPUT);
+      return;
+    }
 
-const options = {
-  cwd: KIBANA_ROOT,
-  stdio: ['ignore', 'inherit', 'inherit'],
-  buffer: false,
-};
-
-const env = {};
-
-if (!flags.dev) {
-  env.NODE_ENV = 'production';
-}
-
-if (flags.run) {
-  log.info('Starting Webpack Dev Server...');
-  execa.sync(
-    'yarn',
-    [
-      'webpack-dev-server',
-      '--config',
-      webpackConfig,
-      '--progress',
-      '--hide-modules',
-      '--display-entrypoints',
-      'false',
-      '--content-base',
-      RUNTIME_SRC,
-    ],
-    options
-  );
-} else if (flags.stats) {
-  log.info('Writing Webpack stats...');
-  const output = execa(
-    './node_modules/.bin/webpack',
-    ['--config', webpackConfig, '--profile', '--json'],
-    {
+    clean();
+    log.info('Building Canvas External Runtime...');
+    execa.sync('yarn', ['webpack', '--config', webpackConfig, '--hide-modules'], {
       ...options,
       env,
-      stdio: ['ignore', 'pipe', 'inherit'],
-    }
-  );
-  output.then(() => log.success('...output written to', STATS_OUTPUT));
-  output.stdout.pipe(fs.createWriteStream(STATS_OUTPUT));
-} else {
-  clean();
-  log.info('Building Canvas External Runtime...');
-  execa.sync('yarn', ['webpack', '--config', webpackConfig, '--hide-modules'], {
-    ...options,
-    env,
-  });
-  log.success('...runtime built!');
-}
+    });
+    log.success('...runtime built!');
+  },
+  {
+    description: `
+      Build script for the Canvas External Runtime.
+    `,
+    flags: {
+      boolean: ['run', 'clean', 'help', 'stats', 'dev'],
+      help: `
+        --run              Run a server with the runtime
+        --dev              Build and/or create stats in development mode.
+        --stats            Output Webpack statistics to a stats.json file.
+        --clean            Delete the existing build
+      `,
+    },
+  }
+);
