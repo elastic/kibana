0f37050b5f98b713ffac2620b7bcf501020be9e0
stephmilovic <stephanie.milovic@elastic.co>
2019-08-19 15:10:29 -0600

    more url state tests

--- x-pack/legacy/plugins/siem/cypress/integration/lib/url_state/index.ts
+++ x-pack/legacy/plugins/siem/cypress/integration/lib/url_state/index.ts
@@ -6,6 +6,9 @@
 export const ABSOLUTE_DATE_RANGE = {
   url:
     '/app/siem#/network/?_g=()&kqlQuery=(filterQuery:!n,queryLocation:network.page,type:page)&timerange=(global:(linkTo:!(timeline),timerange:(from:1564689809186,kind:absolute,to:1564691609186)),timeline:(linkTo:!(global),timerange:(from:1564689809186,kind:absolute,to:1564691609186)))',
+
+  urlUnlinked:
+    '/app/siem#/network/?_g=()&kqlQuery=(filterQuery:!n,queryLocation:network.page,type:page)&timerange=(global:(linkTo:!(),timerange:(from:1564689809186,kind:absolute,to:1564691609186)),timeline:(linkTo:!(),timerange:(from:1564776209186,kind:absolute,to:1564779809186)))',
   startTime: '1564689809186',
   endTime: '1564691609186',
   startTimeTyped: '2019-08-01 14:03:29.186',
@@ -18,11 +21,22 @@ export const ABSOLUTE_DATE_RANGE = {
   newEndTime: '1564693409186',
   newEndTimeTyped: '2019-08-01 15:03:29.186',
   newEndTimeFormat: '2019-08-01T21:03:29.186Z',
+  startTimeTimeline: '1564776209186',
+  endTimeTimeline: '1564779809186',
+  startTimeTypedTimeline: '2019-08-02 14:03:29.186',
+  startTimeFormatTimeline: '2019-08-02T20:03:29.186Z',
+  endTimeTypedTimeline: '2019-08-02 21:03:29.186',
+  endTimeFormatTimeline: '2019-08-02T21:03:29.186Z',
 };
 export const DATE_PICKER_START_DATE_POPOVER_BUTTON =
-  '[data-test-subj="superDatePickerstartDatePopoverButton"]';
+  '[data-test-subj="globalDatePicker"] [data-test-subj="superDatePickerstartDatePopoverButton"]';
 export const DATE_PICKER_END_DATE_POPOVER_BUTTON =
-  '[data-test-subj="superDatePickerendDatePopoverButton"]';
+  '[data-test-subj="globalDatePicker"] [data-test-subj="superDatePickerendDatePopoverButton"]';
+export const DATE_PICKER_START_DATE_POPOVER_BUTTON_TIMELINE =
+  '[data-test-subj="timeline-properties"] [data-test-subj="superDatePickerstartDatePopoverButton"]';
+export const DATE_PICKER_END_DATE_POPOVER_BUTTON_TIMELINE =
+  '[data-test-subj="timeline-properties"] [data-test-subj="superDatePickerendDatePopoverButton"]';
 export const DATE_PICKER_ABSOLUTE_TAB = '[data-test-subj="superDatePickerAbsoluteTab"]';
-export const DATE_PICKER_APPLY_BUTTON = 'button[data-test-subj="superDatePickerApplyTimeButton"]';
+export const DATE_PICKER_APPLY_BUTTON =
+  '[data-test-subj="globalDatePicker"] button[data-test-subj="superDatePickerApplyTimeButton"]';
 export const DATE_PICKER_ABSOLUTE_INPUT = '[data-test-subj="superDatePickerAbsoluteDateInput"]';
--- x-pack/legacy/plugins/siem/cypress/integration/smoke_tests/url_state/url_state.spec.ts
+++ x-pack/legacy/plugins/siem/cypress/integration/smoke_tests/url_state/url_state.spec.ts
@@ -7,71 +7,99 @@
 import { logout } from '../../lib/logout';
 import {
   ABSOLUTE_DATE_RANGE,
-  DATE_PICKER_START_DATE_POPOVER_BUTTON,
-  DATE_PICKER_END_DATE_POPOVER_BUTTON,
+  DATE_PICKER_ABSOLUTE_INPUT,
   DATE_PICKER_ABSOLUTE_TAB,
   DATE_PICKER_APPLY_BUTTON,
-  DATE_PICKER_ABSOLUTE_INPUT,
+  DATE_PICKER_END_DATE_POPOVER_BUTTON,
+  DATE_PICKER_END_DATE_POPOVER_BUTTON_TIMELINE,
+  DATE_PICKER_START_DATE_POPOVER_BUTTON,
+  DATE_PICKER_START_DATE_POPOVER_BUTTON_TIMELINE,
 } from '../../lib/url_state';
 import { loginAndWaitForPage } from '../../lib/util/helpers';
+import { toggleTimelineVisibility } from '../../lib/timeline/helpers';
 
 describe('top-level navigation common to all pages in the SIEM app', () => {
   afterEach(() => {
     logout();
   });
 
-  it('properly sets the start and end dates from the url', () => {
+  it('sets the global start and end dates from the url', () => {
     loginAndWaitForPage(ABSOLUTE_DATE_RANGE.url);
 
     cy.get(DATE_PICKER_START_DATE_POPOVER_BUTTON)
-      .first()
       .invoke('attr', 'title')
       .should('eq', ABSOLUTE_DATE_RANGE.startTimeFormat);
 
     cy.get(DATE_PICKER_END_DATE_POPOVER_BUTTON)
-      .first()
       .invoke('attr', 'title')
       .should('eq', ABSOLUTE_DATE_RANGE.endTimeFormat);
   });
 
-  it('properly sets the url state when start and end date are set', () => {
+  it('sets the url state when start and end date are set', () => {
     loginAndWaitForPage(ABSOLUTE_DATE_RANGE.url);
 
-    cy.get(DATE_PICKER_START_DATE_POPOVER_BUTTON)
-      .first()
-      .click({ force: true });
+    cy.get(DATE_PICKER_START_DATE_POPOVER_BUTTON).click({ force: true });
 
     cy.get(DATE_PICKER_ABSOLUTE_TAB)
       .first()
       .click({ force: true });
 
-    cy.get(DATE_PICKER_ABSOLUTE_INPUT).type(
+    cy.get(DATE_PICKER_ABSOLUTE_INPUT, { timeout: 3000 }).type(
       `{selectall}{backspace}${ABSOLUTE_DATE_RANGE.newStartTimeTyped}`
     );
 
-    cy.get(DATE_PICKER_APPLY_BUTTON)
-      .first()
-      .click({ force: true });
+    cy.get(DATE_PICKER_APPLY_BUTTON).click({ force: true });
 
-    cy.get(DATE_PICKER_END_DATE_POPOVER_BUTTON)
-      .first()
-      .click({ force: true });
+    cy.get(DATE_PICKER_END_DATE_POPOVER_BUTTON).click({ force: true });
 
     cy.get(DATE_PICKER_ABSOLUTE_TAB)
       .first()
       .click({ force: true });
 
-    cy.get(DATE_PICKER_ABSOLUTE_INPUT).type(
+    cy.get(DATE_PICKER_ABSOLUTE_INPUT, { timeout: 3000 }).type(
       `{selectall}{backspace}${ABSOLUTE_DATE_RANGE.newEndTimeTyped}`
     );
 
-    cy.get(DATE_PICKER_APPLY_BUTTON)
-      .first()
-      .click({ force: true });
+    cy.get(DATE_PICKER_APPLY_BUTTON).click({ force: true });
 
     cy.url().should(
       'include',
       `(global:(linkTo:!(timeline),timerange:(from:${ABSOLUTE_DATE_RANGE.newStartTime},kind:absolute,to:${ABSOLUTE_DATE_RANGE.newEndTime}))`
     );
+  });
+
+  it('sets the timeline start and end dates from the url when locked to global time', () => {
+    loginAndWaitForPage(ABSOLUTE_DATE_RANGE.url);
+    toggleTimelineVisibility();
+
+    cy.get(DATE_PICKER_START_DATE_POPOVER_BUTTON_TIMELINE)
+      .invoke('attr', 'title')
+      .should('eq', ABSOLUTE_DATE_RANGE.startTimeFormat);
+
+    cy.get(DATE_PICKER_END_DATE_POPOVER_BUTTON_TIMELINE)
+      .invoke('attr', 'title')
+      .should('eq', ABSOLUTE_DATE_RANGE.endTimeFormat);
+  });
+
+  it('sets the timeline start and end dates independently of the global start and end dates when times are unlocked', () => {
+    loginAndWaitForPage(ABSOLUTE_DATE_RANGE.urlUnlinked);
+
+    cy.get(DATE_PICKER_START_DATE_POPOVER_BUTTON)
+      .invoke('attr', 'title')
+      .should('eq', ABSOLUTE_DATE_RANGE.startTimeFormat);
+
+    cy.get(DATE_PICKER_END_DATE_POPOVER_BUTTON)
+      .invoke('attr', 'title')
+      .should('eq', ABSOLUTE_DATE_RANGE.endTimeFormat);
+
+    toggleTimelineVisibility();
+
+    cy.get(DATE_PICKER_START_DATE_POPOVER_BUTTON_TIMELINE)
+      .invoke('attr', 'title')
+      .should('eq', ABSOLUTE_DATE_RANGE.startTimeFormatTimeline);
+
+    cy.get(DATE_PICKER_END_DATE_POPOVER_BUTTON_TIMELINE)
+      .invoke('attr', 'title')
+      .should('eq', ABSOLUTE_DATE_RANGE.endTimeFormatTimeline);
   });
 });
