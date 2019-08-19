4908922d3b6ec3704c09984498ecb3796f9ca807
Zacqary Xeper <zacqary.xeper@elastic.co>
2019-08-19 11:39:05 -0500

    Add toggleable default message

--- x-pack/legacy/plugins/infra/public/pages/logs/analysis/page_setup_content.tsx
+++ x-pack/legacy/plugins/infra/public/pages/logs/analysis/page_setup_content.tsx
@@ -4,9 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useState, useMemo } from 'react';
-import moment, { Moment } from 'moment';
+import React, { useState } from 'react';
 import {
+  EuiButtonEmpty,
   EuiPage,
   EuiPageBody,
   EuiPageContent,
@@ -16,11 +16,6 @@ import {
   EuiText,
   EuiTitle,
   EuiSpacer,
-  EuiButton,
-  EuiDescribedFormGroup,
-  EuiFormRow,
-  EuiDatePicker,
-  EuiFlexGroup,
   EuiCallOut,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
@@ -28,40 +23,18 @@ import { FormattedMessage } from '@kbn/i18n/react';
 import euiStyled from '../../../../../../common/eui_styled_components';
 import { useTrackPageview } from '../../../hooks/use_track_metric';
 
+import { AnalysisSetupTimerangeForm } from './analysis_setup_timerange_form';
+import { CreateMLJobsButton } from './create_ml_jobs_button';
+
 interface AnalysisSetupContentProps {
-  setupMlModule: (startTime: number | undefined, endTime: number | undefined) => Promise<any>;
+  setupMlModule: (startTime?: number | undefined, endTime?: number | undefined) => Promise<any>;
   isSettingUp: boolean;
   didSetupFail: boolean;
 }
 
-const startTimeLabel = i18n.translate('xpack.infra.analysisSetup.startTimeLabel', {
-  defaultMessage: 'Start time',
-});
-const endTimeLabel = i18n.translate('xpack.infra.analysisSetup.endTimeLabel', {
-  defaultMessage: 'End time',
-});
-const startTimeDefaultDescription = i18n.translate(
-  'xpack.infra.analysisSetup.startTimeDefaultDescription',
-  {
-    defaultMessage: 'Start of log indices',
-  }
-);
-const endTimeDefaultDescription = i18n.translate(
-  'xpack.infra.analysisSetup.endTimeDefaultDescription',
-  {
-    defaultMessage: 'Indefinitely',
-  }
-);
 const errorTitle = i18n.translate('xpack.infra.analysisSetup.errorTitle', {
   defaultMessage: 'Sorry, there was an error setting up Machine Learning',
 });
-
-function selectedDateToParam(selectedDate: Moment | null) {
-  if (selectedDate) {
-    return selectedDate.valueOf(); // To ms unix timestamp
-  }
-  return undefined;
-}
 
 export const AnalysisSetupContent: React.FunctionComponent<AnalysisSetupContentProps> = ({
   setupMlModule,
@@ -71,14 +44,7 @@ export const AnalysisSetupContent: React.FunctionComponent<AnalysisSetupContentP
   useTrackPageview({ app: 'infra_logs', path: 'analysis_setup' });
   useTrackPageview({ app: 'infra_logs', path: 'analysis_setup', delay: 15000 });
 
-  const [startTime, setStartTime] = useState<Moment | null>(null);
-  const [endTime, setEndTime] = useState<Moment | null>(null);
-
-  const now = useMemo(() => moment(), []);
-  const selectedEndTimeIsToday = !endTime || endTime.isSame(now, 'day');
-
-  const onClickCreateJob = () =>
-    setupMlModule(selectedDateToParam(startTime), selectedDateToParam(endTime));
+  const [showTimeRangeForm, setShowTimeRangeForm] = useState(false);
 
   return (
     <AnalysisSetupPage>
@@ -104,85 +70,36 @@ export const AnalysisSetupContent: React.FunctionComponent<AnalysisSetupContentP
             <EuiText size="s">
               <FormattedMessage
                 id="xpack.infra.analysisSetup.analysisSetupDescription"
-                defaultMessage="Use Machine Learning to automatically detect anomalous log rate counts"
+                defaultMessage="Use Machine Learning to automatically detect anomalous log rate counts."
               />
             </EuiText>
-            <EuiSpacer size="l" />
-            <EuiDescribedFormGroup
-              idAria="timeRange"
-              title={
-                <FormattedMessage
-                  id="xpack.infra.analysisSetup.timeRangeTitle"
-                  defaultMessage="Choose a time range"
+            {showTimeRangeForm ? (
+              <>
+                <EuiSpacer size="l" />
+                <AnalysisSetupTimerangeForm
+                  isSettingUp={isSettingUp}
+                  setupMlModule={setupMlModule}
                 />
-              }
-              description={
-                <FormattedMessage
-                  id="xpack.infra.analysisSetup.timeRangeDescription"
-                  defaultMessage="By default, Machine Learning analyzes log messages from the start of your log indices and continues indefinitely. You can specify a different date to begin, to end, or both."
-                />
-              }
-            >
-              <EuiFormRow
-                describedByIds={['timeRange']}
-                error={false}
-                fullWidth
-                isInvalid={false}
-                label={startTimeLabel}
-              >
-                <EuiFlexGroup gutterSize="s">
-                  <EuiDatePicker
-                    showTimeSelect
-                    selected={startTime}
-                    onChange={setStartTime}
-                    placeholder={startTimeDefaultDescription}
-                    maxDate={now}
-                    onClear={() => setStartTime(null)}
-                  />
-                </EuiFlexGroup>
-              </EuiFormRow>
-              <EuiFormRow
-                describedByIds={['timeRange']}
-                error={false}
-                fullWidth
-                isInvalid={false}
-                label={endTimeLabel}
-              >
-                <EuiFlexGroup gutterSize="s">
-                  <EuiDatePicker
-                    showTimeSelect
-                    selected={endTime}
-                    onChange={setEndTime}
-                    placeholder={endTimeDefaultDescription}
-                    openToDate={now}
-                    minDate={now}
-                    minTime={
-                      selectedEndTimeIsToday
-                        ? now
-                        : moment()
-                            .hour(0)
-                            .minutes(0)
-                    }
-                    maxTime={moment()
-                      .hour(23)
-                      .minutes(59)}
-                    onClear={() => setEndTime(null)}
-                  />
-                </EuiFlexGroup>
-              </EuiFormRow>
-              <EuiButton
-                fill
-                isLoading={isSettingUp}
-                iconSide="right"
-                iconType="check"
-                onClick={onClickCreateJob}
-              >
-                <FormattedMessage
-                  id="xpack.infra.analysisSetup.createMlJobButton"
-                  defaultMessage="Create ML Job"
-                />
-              </EuiButton>
-            </EuiDescribedFormGroup>
+              </>
+            ) : (
+              <>
+                <EuiSpacer size="m" />
+                <ByDefaultText>
+                  <FormattedMessage
+                    id="xpack.infra.analysisSetup.timeRangeByDefault"
+                    defaultMessage="By default, we'll analyze all past and future log messages in your logs indices."
+                  />{' '}
+                  <EuiButtonEmpty onClick={() => setShowTimeRangeForm(true)}>
+                    <FormattedMessage
+                      id="xpack.infra.analysisSetup.configureTimeRange"
+                      defaultMessage="Configure time range?"
+                    />
+                  </EuiButtonEmpty>
+                </ByDefaultText>
+                <EuiSpacer size="l" />
+                <CreateMLJobsButton isLoading={isSettingUp} onClick={() => setupMlModule()} />
+              </>
+            )}
             {didSetupFail && (
               <EuiCallOut color="danger" iconType="alert" title={errorTitle}>
                 <EuiText>
@@ -207,4 +124,16 @@ const AnalysisPageContent = euiStyled(EuiPageContent)`
 
 const AnalysisSetupPage = euiStyled(EuiPage)`
   height: 100%;
+`;
+
+const ByDefaultText = euiStyled(EuiText).attrs({ size: 's' })`
+  & .euiButtonEmpty {
+    font-size: inherit;
+    line-height: inherit;
+    height: initial;
+  }
+
+  & .euiButtonEmpty__content {
+    padding: 0;
+  }
 `;
