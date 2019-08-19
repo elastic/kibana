fee15cdb0fa280e1297f92dd8b24b00a0ef210ec
Gidi Morris <github@gidi.io>
2019-08-19 16:29:45 +0100

    feat(webhook-action): Defined the advanced configurations for proxy and custom timeouts

--- x-pack/legacy/plugins/actions/server/builtin_action_types/webhook.test.ts
+++ x-pack/legacy/plugins/actions/server/builtin_action_types/webhook.test.ts
@@ -46,6 +46,9 @@ describe('config validation', () => {
     method: 'get',
     path: null,
     headers: null,
+    proxy: null,
+    connection_timeout: null,
+    read_timeout: null,
   };
 
   test('config validation passes when only required fields are provided', () => {
@@ -155,5 +158,105 @@ describe('config validation', () => {
 - [headers.0]: expected value of type [object] but got [string]
 - [headers.1]: expected value to equal [null] but got [application/json]"
 `);
+  });
+
+  test('config validation passes when a valid proxy is provided', () => {
+    const config: Record<string, any> = {
+      host: 'mylisteningserver',
+      port: 9200,
+      proxy: {
+        host: 'localhost',
+        port: 9200,
+      },
+    };
+    expect(validateConfig(actionType, config)).toEqual({
+      ...defaultValues,
+      ...config,
+    });
+  });
+
+  test('config validation passes when a proxy ommits a port', () => {
+    const config: Record<string, any> = {
+      host: 'mylisteningserver',
+      port: 9200,
+      proxy: {
+        host: 'localhost',
+      },
+    };
+    expect(validateConfig(actionType, config)).toEqual({
+      ...defaultValues,
+      ...config,
+      proxy: {
+        host: 'localhost',
+        port: null,
+      },
+    });
+  });
+
+  test('should validate and throw error when a proxy is specified but has no host', () => {
+    const config: Record<string, any> = {
+      host: 'mylisteningserver',
+      port: 9200,
+      proxy: {
+        port: 8080,
+      },
+    };
+    expect(() => {
+      validateConfig(actionType, config);
+    }).toThrowErrorMatchingInlineSnapshot(`
+"error validating action type config: [proxy]: types that failed validation:
+- [proxy.0.host]: expected value of type [string] but got [undefined]
+- [proxy.1]: expected value to equal [null] but got [[object Object]]"
+`);
+  });
+
+  test('config validation passes when a valid connection_timeout is provided', () => {
+    const config: Record<string, any> = {
+      host: 'mylisteningserver',
+      port: 9200,
+      connection_timeout: '10s',
+    };
+    expect(validateConfig(actionType, config)).toEqual({
+      ...defaultValues,
+      ...config,
+    });
+  });
+
+  test('ashould validate and throw error when an invalid connection_timeout is specified', () => {
+    const config: Record<string, any> = {
+      host: 'mylisteningserver',
+      port: 9200,
+      connection_timeout: '10 seconds',
+    };
+    expect(() => {
+      validateConfig(actionType, config);
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"error validating action type config: [connection_timeout]: Invalid interval \\"10 seconds\\". Intervals must be of the form {number}m. Example: 5m."`
+    );
+  });
+
+  test('config validation passes when a valid read_timeout is provided', () => {
+    const config: Record<string, any> = {
+      host: 'mylisteningserver',
+      port: 9200,
+      read_timeout: '10s',
+    };
+    expect(validateConfig(actionType, config)).toEqual({
+      ...defaultValues,
+      ...config,
+    });
+  });
+
+  test('ashould validate and throw error when an invalid read_timeout is specified', () => {
+    const config: Record<string, any> = {
+      host: 'mylisteningserver',
+      port: 9200,
+      read_timeout: '10 seconds',
+    };
+    expect(() => {
+      validateConfig(actionType, config);
+    }).toThrowErrorMatchingInlineSnapshot(
+      `"error validating action type config: [read_timeout]: Invalid interval \\"10 seconds\\". Intervals must be of the form {number}m. Example: 5m."`
+    );
   });
 });
--- x-pack/legacy/plugins/actions/server/builtin_action_types/webhook.ts
+++ x-pack/legacy/plugins/actions/server/builtin_action_types/webhook.ts
@@ -7,34 +7,68 @@
 import { schema, TypeOf } from '@kbn/config-schema';
 import { nullableType } from './lib/nullable';
 import { ActionType, ActionTypeExecutorOptions, ActionTypeExecutorResult } from '../types';
+import { assertValidInterval } from '../../../task_manager/lib/intervals';
 
+// Generic Schemas
 const PORT_MAX = 256 * 256 - 1;
+const PortSchema = schema.number({ min: 1, max: PORT_MAX });
 
 // config definition
 export type ActionTypeConfigType = TypeOf<typeof ConfigSchema>;
 
 const HeadersSchema = schema.recordOf(schema.string(), schema.string());
-const ConfigSchema = schema.object({
+const ProxySchema = schema.object({
   host: schema.string(),
-  port: schema.number({ min: 1, max: PORT_MAX }),
-  scheme: schema.oneOf([schema.literal('http'), schema.literal('https')], {
-    defaultValue: 'http',
-  }),
-  method: schema.oneOf(
-    [
-      schema.literal('head'),
-      schema.literal('get'),
-      schema.literal('post'),
-      schema.literal('put'),
-      schema.literal('delete'),
-    ],
-    {
-      defaultValue: 'get',
-    }
-  ),
-  path: nullableType(schema.string()),
-  headers: nullableType(HeadersSchema),
+  port: nullableType(PortSchema),
 });
+
+const ConfigSchema = schema.object(
+  {
+    host: schema.string(),
+    port: PortSchema,
+    scheme: schema.oneOf([schema.literal('http'), schema.literal('https')], {
+      defaultValue: 'http',
+    }),
+    method: schema.oneOf(
+      [
+        schema.literal('head'),
+        schema.literal('get'),
+        schema.literal('post'),
+        schema.literal('put'),
+        schema.literal('delete'),
+      ],
+      {
+        defaultValue: 'get',
+      }
+    ),
+    path: nullableType(schema.string()),
+    headers: nullableType(HeadersSchema),
+    proxy: nullableType(ProxySchema),
+    connection_timeout: nullableType(schema.string()),
+    read_timeout: nullableType(schema.string()),
+  },
+  { validate: validateConfig }
+);
+
+function validateConfig(configObject: any): string | void {
+  const config: ActionTypeConfigType = configObject;
+
+  const { read_timeout: readTimeout, connection_timeout: connectionTimeout } = config;
+  try {
+    if (readTimeout != null) {
+      assertValidInterval(readTimeout);
+    }
+  } catch (e) {
+    return `[read_timeout]: ${e.message}`;
+  }
+  try {
+    if (connectionTimeout != null) {
+      assertValidInterval(connectionTimeout);
+    }
+  } catch (e) {
+    return `[connection_timeout]: ${e.message}`;
+  }
+}
 
 // secrets definition
 export type ActionTypeSecretsType = TypeOf<typeof SecretsSchema>;
