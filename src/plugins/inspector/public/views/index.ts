7f60212bbd1da2c4ffca69d88d4a55b2d7af0db8
Alexey Antonov <alexwizp@gmail.com>
2019-08-19 16:54:28 +0800

    [Inspector Views] [Request View] - Migrate inspector_views to new platform (#43191)
    * [Inspector Views] [Request View] - Migrate inspector_views to new platform
    
    * fix i18n keys
    
    * rename scss files
    
    * fix PR comments

--- src/plugins/inspector/public/views/index.ts
+++ src/plugins/inspector/public/views/index.ts
@@ -17,24 +17,4 @@
  * under the License.
  */
 
-import React from 'react';
-import {
-  EuiCodeBlock,
-} from '@elastic/eui';
-
-function RequestDetailsResponse(props) {
-  return (
-    <EuiCodeBlock
-      language="json"
-      paddingSize="s"
-      isCopyable
-      data-test-subj="inspectorResponseBody"
-    >
-      { JSON.stringify(props.request.response.json, null, 2) }
-    </EuiCodeBlock>
-  );
-}
-
-RequestDetailsResponse.shouldShow = (request) => request.response && request.response.json;
-
-export { RequestDetailsResponse };
+export { RequestsView } from './requests';
--- src/plugins/inspector/public/views/requests/components/types.ts
+++ src/plugins/inspector/public/views/requests/components/types.ts
@@ -16,51 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import { Request } from '../../../adapters/request/types';
 
-/**
- * The status a request can have.
- */
-export enum RequestStatus {
-  /**
-   * The request hasn't finished yet.
-   */
-  PENDING,
-  /**
-   * The request has successfully finished.
-   */
-  OK,
-  /**
-   * The request failed.
-   */
-  ERROR,
-}
-
-export interface Request extends RequestParams {
-  id: string;
-  name: string;
-  json?: object;
-  response?: Response;
-  startTime: number;
-  stats?: RequestStatistics;
-  status: RequestStatus;
-  time?: number;
-}
-
-export interface RequestParams {
-  id?: string;
-  description?: string;
-}
-
-export interface RequestStatistics {
-  [key: string]: RequestStatistic;
-}
-
-export interface RequestStatistic {
-  label: string;
-  description?: string;
-  value: any;
-}
-
-export interface Response {
-  json?: object;
+export interface RequestDetailsProps {
+  request: Request;
 }
