10aeb2b53c61f17b1ad995d188c89a65d26a6536
chrisronline <chrisronline@gmail.com>
2019-08-19 15:28:01 -0400

    Remove manual checks for logstash, beats, apm and kibana

--- x-pack/legacy/plugins/monitoring/public/components/metricbeat_migration/instruction_steps/logstash/disable_internal_collection_instructions.js
+++ x-pack/legacy/plugins/monitoring/public/components/metricbeat_migration/instruction_steps/logstash/disable_internal_collection_instructions.js
@@ -8,9 +8,6 @@ import React, { Fragment } from 'react';
 import {
   EuiSpacer,
   EuiCodeBlock,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiButton,
   EuiCallOut,
   EuiText
 } from '@elastic/eui';
@@ -20,12 +17,7 @@ import { Monospace } from '../components/monospace';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { statusTitle } from './common_logstash_instructions';
 
-export function getLogstashInstructionsForDisablingInternalCollection(product, meta, {
-  checkForMigrationStatus,
-  checkingMigrationStatus,
-  hasCheckedStatus,
-  autoCheckIntervalInMs,
-}) {
+export function getLogstashInstructionsForDisablingInternalCollection(product, meta) {
   const disableInternalCollectionStep = {
     title: i18n.translate('xpack.monitoring.metricbeatMigration.logstashInstructions.disableInternalCollection.title', {
       defaultMessage: 'Disable internal collection of Logstash monitoring metrics'
@@ -67,100 +59,51 @@ export function getLogstashInstructionsForDisablingInternalCollection(product, m
 
   let migrationStatusStep = null;
   if (!product || !product.isFullyMigrated) {
-    let status = null;
-    if (hasCheckedStatus) {
-      let lastInternallyCollectedMessage = '';
-      // It is possible that, during the migration steps, products are not reporting
-      // monitoring data for a period of time outside the window of our server-side check
-      // and this is most likely temporary so we want to be defensive and not error out
-      // and hopefully wait for the next check and this state will be self-corrected.
-      if (product) {
-        const lastInternallyCollectedTimestamp = product.lastInternallyCollectedTimestamp || product.lastTimestamp;
-        const secondsSinceLastInternalCollectionLabel =
-          formatTimestampToDuration(lastInternallyCollectedTimestamp, CALCULATE_DURATION_SINCE);
-        lastInternallyCollectedMessage = (<FormattedMessage
-          id="xpack.monitoring.metricbeatMigration.logstashInstructions.disableInternalCollection.partiallyMigratedStatusDescription"
-          defaultMessage="Last internal collection occurred {secondsSinceLastInternalCollectionLabel} ago."
-          values={{
-            secondsSinceLastInternalCollectionLabel,
-          }}
-        />);
-      }
-
-      status = (
-        <Fragment>
-          <EuiSpacer size="m"/>
-          <EuiCallOut
-            size="s"
-            color="warning"
-            title={i18n.translate('xpack.monitoring.metricbeatMigration.logstashInstructions.partiallyMigratedStatusTitle',
-              {
-                defaultMessage: `We still see data coming from internal collection of Logstash.`
-              }
-            )}
-          >
-            <p>
-              <FormattedMessage
-                id="xpack.monitoring.metricbeatMigration.logstashInstructions.partiallyMigratedStatusDescription"
-                defaultMessage="Note that it can take up to {secondsAgo} seconds to detect, but
-                we will continuously check every {timePeriod} seconds in the background."
-                values={{
-                  secondsAgo: meta.secondsAgo,
-                  timePeriod: autoCheckIntervalInMs / 1000,
-                }}
-              />
-            </p>
-            <p>
-              {lastInternallyCollectedMessage}
-            </p>
-          </EuiCallOut>
-        </Fragment>
-      );
-    }
-
-    let buttonLabel;
-    if (checkingMigrationStatus) {
-      buttonLabel = i18n.translate(
-        'xpack.monitoring.metricbeatMigration.logstashInstructions.disableInternalCollection.checkingStatusButtonLabel',
-        {
-          defaultMessage: 'Checking...'
-        }
-      );
-    } else {
-      buttonLabel = i18n.translate(
-        'xpack.monitoring.metricbeatMigration.logstashInstructions.disableInternalCollection.checkStatusButtonLabel',
-        {
-          defaultMessage: 'Check'
-        }
-      );
+    let lastInternallyCollectedMessage = '';
+    // It is possible that, during the migration steps, products are not reporting
+    // monitoring data for a period of time outside the window of our server-side check
+    // and this is most likely temporary so we want to be defensive and not error out
+    // and hopefully wait for the next check and this state will be self-corrected.
+    if (product) {
+      const lastInternallyCollectedTimestamp = product.lastInternallyCollectedTimestamp || product.lastTimestamp;
+      const secondsSinceLastInternalCollectionLabel =
+        formatTimestampToDuration(lastInternallyCollectedTimestamp, CALCULATE_DURATION_SINCE);
+      lastInternallyCollectedMessage = (<FormattedMessage
+        id="xpack.monitoring.metricbeatMigration.logstashInstructions.disableInternalCollection.partiallyMigratedStatusDescription"
+        defaultMessage="Last internal collection occurred {secondsSinceLastInternalCollectionLabel} ago."
+        values={{
+          secondsSinceLastInternalCollectionLabel,
+        }}
+      />);
     }
 
     migrationStatusStep = {
       title: statusTitle,
       status: 'incomplete',
       children: (
-        <Fragment>
-          <EuiFlexGroup alignItems="center">
-            <EuiFlexItem>
-              <EuiText>
-                <p>
-                  {i18n.translate(
-                    'xpack.monitoring.metricbeatMigration.logstashInstructions.disableInternalCollection.statusDescription',
-                    {
-                      defaultMessage: 'Check that no documents are coming from internal collection.'
-                    }
-                  )}
-                </p>
-              </EuiText>
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <EuiButton onClick={checkForMigrationStatus} isDisabled={checkingMigrationStatus}>
-                {buttonLabel}
-              </EuiButton>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-          {status}
-        </Fragment>
+        <EuiCallOut
+          size="s"
+          color="warning"
+          title={i18n.translate('xpack.monitoring.metricbeatMigration.logstashInstructions.partiallyMigratedStatusTitle',
+            {
+              defaultMessage: `We still see data coming from internal collection of Logstash.`
+            }
+          )}
+        >
+          <p>
+            <FormattedMessage
+              id="xpack.monitoring.metricbeatMigration.logstashInstructions.partiallyMigratedStatusDescription"
+              defaultMessage="Note that it can take up to {secondsAgo} seconds to detect, but
+              we will continuously check in the background."
+              values={{
+                secondsAgo: meta.secondsAgo,
+              }}
+            />
+          </p>
+          <p>
+            {lastInternallyCollectedMessage}
+          </p>
+        </EuiCallOut>
       )
     };
   }
