ca986f25c6331dd859adf8c1c3a321ced0ba2dab
Christopher Davies <github@christophilus.com>
2019-08-20 13:26:45 -0400

    FIx table_list_view deleteItems

--- src/legacy/core_plugins/kibana/public/table_list_view/table_list_view.js
+++ src/legacy/core_plugins/kibana/public/table_list_view/table_list_view.js
@@ -116,7 +116,8 @@ class TableListViewUi extends React.Component {
       isDeletingItems: true
     });
     try {
-      await this.props.deleteItems(this.state.selectedIds);
+      const itemsById = _.indexBy(this.state.items, 'id');
+      await this.props.deleteItems(this.state.selectedIds.map(id => itemsById[id]));
     } catch (error) {
       toastNotifications.addDanger({
         title: (
