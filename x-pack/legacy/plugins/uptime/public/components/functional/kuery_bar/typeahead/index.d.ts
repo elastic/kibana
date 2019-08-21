796221fe4a465317aa8a389143871fa35a52231f
Justin Kambic <justin.kambic@elastic.co>
2019-08-21 15:32:40 -0400

    Add KueryBar component to Uptime app.

---
+++ x-pack/legacy/plugins/uptime/public/components/functional/kuery_bar/typeahead/index.d.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { Component } from 'react';
+
+interface TypeaheadProps {
+  onChange: (inputValue: string, selectionStart: number) => void;
+  onSubmit: (inputValue: string) => void;
+  suggestions: unknown[];
+  queryExample: string;
+  initialValue?: string;
+  isLoading?: boolean;
+  disabled?: boolean;
+}
+
+export class Typeahead extends React.Component<TypeaheadProps> {
+  incrementIndex(currentIndex: any): void;
+
+  decrementIndex(currentIndex: any): void;
+
+  onKeyUp(event: any): void;
+
+  onKeyDown(event: any): void;
+
+  selectSuggestion(suggestion: any): void;
+
+  onClickOutside(): void;
+
+  onChangeInputValue(event: any): void;
+
+  onClickInput(event: any): void;
+
+  onClickSuggestion(suggestion: any): void;
+
+  onMouseEnterSuggestion(index: any): void;
+
+  onSubmit(): void;
+
+  render(): any;
+}
