c97182163f315f4fef95069ea6f1c7c9e56e70b0
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2019-08-21 11:11:52 -0700

    WIP- hybrid index pattern tests

---
+++ x-pack/test/functional/apps/visualize/hybrid_visualization.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import expect from '@kbn/expect';
+import { KibanaFunctionalTestDefaultProviders } from '../../../../types/providers';
+
+export default function({ getPageObjects, getService }: KibanaFunctionalTestDefaultProviders) {
+  const esArchiver = getService('esArchiver');
+  const PageObjects = getPageObjects(['common', 'visualize', 'timePicker']);
+  const log = getService('log');
+  const inspector = getService('inspector');
+  const retry = getService('retry');
+  const testSubjects = getService('testSubjects');
+
+  describe('hybrid', () => {
+    before(async () => {
+      log.debug('loading hybrid test data');
+      await esArchiver.load('hybrid');
+    });
+
+    after(async () => {
+      await esArchiver.unload('hybrid');
+    });
+
+    it('hybrid visualization', async () => {
+      log.debug('navigateToApp visualize');
+      await PageObjects.visualize.navigateToNewVisualization();
+    });
+  });
+}
