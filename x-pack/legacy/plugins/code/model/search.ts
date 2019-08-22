027bfe4c07a7cb76a3ce86068551e61590689385
Mengwei Ding <mengwei.ding@gmail.com>
2019-08-22 13:56:29 -0700

    [Code] Add a resolveSnippets API to resolve APM stacktrace item to source code

--- x-pack/legacy/plugins/code/model/search.ts
+++ x-pack/legacy/plugins/code/model/search.ts
@@ -109,6 +109,17 @@ export interface SymbolSearchRequest extends SearchRequest {
   repoScope?: RepositoryUri[];
 }
 
+export interface CodeIntegrationRequest {
+  repoUri: RepositoryUri;
+  revision?: string;
+}
+
+export interface ResolveSnippetsIntegrationRequest extends CodeIntegrationRequest {
+  filePath: string;
+  lineNumStart: number;
+  lineNumEnd?: number;
+}
+
 // The base interface of any kind of search result.
 export interface SearchResult {
   total: number;
