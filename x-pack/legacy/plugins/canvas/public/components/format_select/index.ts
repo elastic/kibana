79cea1f906fa4d1b4d77fe6d80d7cdd711542d54
Catherine Liu <catherine.liu@elastic.co>
2019-08-22 12:45:36 -0700

    [Canvas] Added formatnumber/formatdate UIs to sidebar (#43059) (#43801)
    * Added formatnumber and formatdate transform UIs
    
    * Added rounddate transform
    
    * Changed default custom format
    
    * Changed to UTC date
    
    * Fixed ts error
    
    * Fixed help text
    
    * Added type def for arguments
    
    * Added types for tranforms
    
    * Added snapshots
    
    * Fixed prop

--- x-pack/legacy/plugins/canvas/public/components/format_select/index.ts
+++ x-pack/legacy/plugins/canvas/public/components/format_select/index.ts
@@ -4,9 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { formatdate } from './formatdate';
-import { formatnumber } from './formatnumber';
-import { rounddate } from './rounddate';
-import { sort } from './sort';
-
-export const transformSpecs = [formatdate, formatnumber, rounddate, sort];
+export { FormatSelect } from './format_select';
