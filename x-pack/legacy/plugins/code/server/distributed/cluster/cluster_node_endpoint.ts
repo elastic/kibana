73f753ada801f70357ecb8c9f08674a9935a62f1
Yang Yang <yang.yang@elastic.co>
2019-08-23 09:52:08 +0800

    [Code] refactored the queue to process tasks belong to local node only. (#42601)
    * [Code] a MVP implementation to support cluster mode code deployment with manually configured static routing table.
    
    1. add a ClusterNodeAdapter
    2. add the abstraction of ClusterService to manage cluster state and support event listening.
    3. add the abstraction of ClusterMembershipService to manage the members in the cluster.
    4. add the abstraction of ResourceScheduler to allocate resources to nodes, and implement a hash based resource allocation policy.
    5. added ResourceSchedulerService to watch the change of cluster states and trigger resource allocations.
    6. support request handling based on the routing table.
    
    Fixed jobs filtering in Worker._getPendingJobs().
    As Array.filter() doesn't support async callbacks, rewrite with a loop.

---
+++ x-pack/legacy/plugins/code/server/distributed/cluster/cluster_node_endpoint.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { Request } from 'hapi';
+import { LocalEndpoint } from '../local_endpoint';
+import { CodeNode } from './code_nodes';
+
+export class ClusterNodeEndpoint extends LocalEndpoint {
+  constructor(
+    public readonly httpRequest: Request,
+    public readonly resource: string,
+    public readonly codeNode: CodeNode
+  ) {
+    super(httpRequest, resource);
+  }
+}
