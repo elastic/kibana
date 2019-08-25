ade66adbf4d19616fff2ab8df2c0f7c1f48fc92d
Kerry Gallagher <k.gallagher.05@gmail.com>
2019-08-25 11:31:14 +0100

    [LogsUI] Add analysis results screen (#43471) (#43932)
    * Add empty analysis tab
    
    * Add ml capabilities check
    
    * Add job status checking functionality
    
    * Add a loading page for the job status check
    
    * Change types / change method for deriving space ID / change setup requirement filtering check
    
    * Use new structure
    
    * Add a loading page
    
    * Initial timeRange URL state hookup
    
    * Hook up params to data fetching
    
    * Fleshing out EUI structure
    
    * Change tab syntax
    
    * i18n translate message prop
    
    * Fix import
    
    * Add structural visual components
    
    * Split section in to independent component
    
    * Real loading and no data states
    
    * Add initial chart rendering (WIP)
    
    * Tick formatting for x axis
    
    * Add series styling, tickFormatter etc
    
    * Base bucketDuration on time range for a sensible number of data points (naieve version)
    
    * Add auto refresh
    
    * Adjust bucketDuration algorithm
    
    * Add some dark theme support
    
    * Call the functions
    
    * Extract chart helpers
    
    * Amend io-ts types
    
    * i18n translations
    
    * Add types for graph data
    
    * Allow ability to toggle model bounds
    
    * Add anomaly series
    
    * Format date correctly
    
    * Add anomalies detected text
    
    * Simplify syntax
    
    * Update title
    
    * Render panel within a page
    
    * Add ability to switch between chart and table view
    
    * Fix typechecking errors
    
    * Add a Beta badge to the analysis tab

---
+++ x-pack/legacy/plugins/infra/public/pages/logs/analysis/sections/log_rate/index.tsx
@@ -0,0 +1,92 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { useState } from 'react';
+import { i18n } from '@kbn/i18n';
+import {
+  EuiTitle,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiLoadingChart,
+  EuiSpacer,
+  EuiEmptyPrompt,
+} from '@elastic/eui';
+import { GetLogEntryRateSuccessResponsePayload } from '../../../../../../common/http_api/log_analysis/results/log_entry_rate';
+import { ViewSwitcher } from './view_switcher';
+import { ChartView } from './chart';
+import { TableView } from './table';
+
+export enum ViewMode {
+  chart = 'chart',
+  table = 'table',
+}
+
+export const LogRateResults = ({
+  isLoading,
+  results,
+}: {
+  isLoading: boolean;
+  results: GetLogEntryRateSuccessResponsePayload['data'] | null;
+}) => {
+  const title = i18n.translate('xpack.infra.logs.analysis.logRateSectionTitle', {
+    defaultMessage: 'Log entry anomalies',
+  });
+
+  const loadingAriaLabel = i18n.translate(
+    'xpack.infra.logs.analysis.logRateSectionLoadingAriaLabel',
+    { defaultMessage: 'Loading log rate results' }
+  );
+
+  const [viewMode, setViewMode] = useState<ViewMode>(ViewMode.chart);
+
+  return (
+    <>
+      <EuiTitle size="m" aria-label={title}>
+        <h2>{title}</h2>
+      </EuiTitle>
+      <EuiSpacer size="l" />
+      {isLoading ? (
+        <EuiFlexGroup justifyContent="center">
+          <EuiFlexItem grow={false}>
+            <EuiLoadingChart size="xl" aria-label={loadingAriaLabel} />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      ) : !results || (results && results.histogramBuckets && !results.histogramBuckets.length) ? (
+        <EuiEmptyPrompt
+          title={
+            <h2>
+              {i18n.translate('xpack.infra.logs.analysis.logRateSectionNoDataTitle', {
+                defaultMessage: 'There is no data to display.',
+              })}
+            </h2>
+          }
+          titleSize="m"
+          body={
+            <p>
+              {i18n.translate('xpack.infra.logs.analysis.logRateSectionNoDataBody', {
+                defaultMessage: 'Try adjusting your time range',
+              })}
+            </p>
+          }
+        />
+      ) : (
+        <>
+          <EuiFlexGroup>
+            <EuiFlexItem grow={true}>
+              <ViewSwitcher selectedView={viewMode} onChange={id => setViewMode(id as ViewMode)} />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+          <EuiSpacer size="l" />
+          {viewMode === ViewMode.chart ? (
+            <ChartView data={results} />
+          ) : (
+            <TableView data={results} />
+          )}
+        </>
+      )}
+    </>
+  );
+};
