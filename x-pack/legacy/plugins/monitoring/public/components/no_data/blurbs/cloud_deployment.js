898f52dd2caa561874895a8b507be63f84791e91
Mike Place <mike.place@elastic.co>
2019-08-26 12:10:31 +0200

    Internationalization fix to avoid duplicates

--- x-pack/legacy/plugins/monitoring/public/components/no_data/blurbs/cloud_deployment.js
+++ x-pack/legacy/plugins/monitoring/public/components/no_data/blurbs/cloud_deployment.js
@@ -31,7 +31,7 @@ export const CloudDeployment = () => {
               cloud dashboard.
             </EuiLink>{' '}
             <FormattedMessage
-              id="xpack.monitoring.noData.blurbs.cloudDeploymentDescription"
+              id="xpack.monitoring.noData.blurbs.cloudDeploymentDescriptionMore"
               defaultMessage="For more information on Monitoring in Elastic Cloud, please see "
             />
             <EuiLink href="https://www.elastic.co/guide/en/cloud/current/ec-enable-monitoring.html" target="_blank">
