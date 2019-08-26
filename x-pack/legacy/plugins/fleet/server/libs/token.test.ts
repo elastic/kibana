9412de66328541ccb601aa39dd4f28fe6ab3aae0
Nicolas Chaulet <n.chaulet@gmail.com>
2019-08-26 15:54:23 -0400

    [Ingest]  fleet add domain token lib (#43526)

---
+++ x-pack/legacy/plugins/fleet/server/libs/token.test.ts
@@ -0,0 +1,135 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { sign } from 'jsonwebtoken';
+import { createHmac } from 'crypto';
+import { TokenLib } from './token';
+import { FrameworkLib } from './framework';
+import { MemoryTokenAdapter } from './adapters/tokens/memory';
+import { FrameworkAdapter } from './adapters/framework/default';
+import { TokenType } from './adapters/tokens/adapter_types';
+
+jest.mock('./framework');
+
+function generateJWTToken(): string {
+  return sign(
+    {
+      config: {
+        id: 'configId',
+        sharedId: 'sharedId',
+      },
+      type: TokenType.ENROLMENT_TOKEN,
+    },
+    'mockedEncryptionKey'
+  );
+}
+
+function hashJWTToken(token: string) {
+  return createHmac('sha512', 'mockedEncryptionKey')
+    .update(token)
+    .digest('hex');
+}
+
+describe('Token Lib', () => {
+  describe('verify', () => {
+    it('should verify a valid token', async () => {
+      const tokenAdapter = new MemoryTokenAdapter();
+      const token = generateJWTToken();
+      const tokenHash = hashJWTToken(token);
+      tokenAdapter.create({
+        type: TokenType.ENROLMENT_TOKEN,
+        active: true,
+        tokenHash,
+        config: { id: 'configId', sharedId: 'sharedId' },
+      });
+      const tokens = new TokenLib(tokenAdapter, new FrameworkLib({} as FrameworkAdapter));
+
+      const res = await tokens.verify(token);
+
+      expect(res).toMatchObject({
+        valid: true,
+      });
+    });
+
+    it('should not verify a inactive token', async () => {
+      const tokenAdapter = new MemoryTokenAdapter();
+      const token = generateJWTToken();
+      const tokenHash = hashJWTToken(token);
+      tokenAdapter.create({
+        type: TokenType.ENROLMENT_TOKEN,
+        active: false,
+        tokenHash,
+        config: { id: 'configId', sharedId: 'sharedId' },
+      });
+      const tokens = new TokenLib(tokenAdapter, new FrameworkLib({} as FrameworkAdapter));
+
+      const res = await tokens.verify(token);
+
+      expect(res).toMatchObject({
+        valid: false,
+        reason: 'Token is not active',
+      });
+    });
+
+    it('should not verify a token not persisted', async () => {
+      const tokenAdapter = new MemoryTokenAdapter();
+      const token = generateJWTToken();
+      const tokens = new TokenLib(tokenAdapter, new FrameworkLib({} as FrameworkAdapter));
+
+      const res = await tokens.verify(token);
+
+      expect(res).toMatchObject({
+        valid: false,
+        reason: 'Token not found',
+      });
+    });
+
+    it('should not verify invalid token', async () => {
+      const tokenAdapter = new MemoryTokenAdapter();
+      const tokens = new TokenLib(tokenAdapter, new FrameworkLib({} as FrameworkAdapter));
+
+      const res = await tokens.verify('iamnotavalidtoken');
+
+      expect(res).toMatchObject({
+        valid: false,
+        reason: 'jwt malformed',
+      });
+    });
+  });
+
+  describe('generateEnrolmentToken', () => {
+    it('should generate a valid token', async () => {
+      const tokenAdapter = new MemoryTokenAdapter();
+      const tokens = new TokenLib(tokenAdapter, new FrameworkLib({} as FrameworkAdapter));
+
+      const token = await tokens.generateEnrolmentToken({
+        id: 'config_id',
+        sharedId: 'config_shared_id',
+      });
+
+      expect(token).toBeDefined();
+    });
+
+    it('should persit the generated token', async () => {
+      const tokenAdapter = new MemoryTokenAdapter();
+      const tokens = new TokenLib(tokenAdapter, new FrameworkLib({} as FrameworkAdapter));
+
+      const token = await tokens.generateEnrolmentToken({
+        id: 'config_id',
+        sharedId: 'config_shared_id',
+      });
+
+      const tokenHash = hashJWTToken(token);
+      const persistedToken = await tokenAdapter.getByTokenHash(tokenHash);
+
+      expect(persistedToken).toMatchObject({
+        tokenHash,
+        config_id: 'config_id',
+        config_shared_id: 'config_shared_id',
+      });
+    });
+  });
+});
--- x-pack/legacy/plugins/fleet/server/libs/token.ts
+++ x-pack/legacy/plugins/fleet/server/libs/token.ts
@@ -4,14 +4,111 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { TokenVerificationResponse } from './adapters/tokens/adapter_types';
+import { sign as signToken, verify as verifyToken } from 'jsonwebtoken';
+import { createHmac } from 'crypto';
+import { TokenVerificationResponse, TokenType } from './adapters/tokens/adapter_types';
+import { TokenAdapter } from './adapters/tokens/adapter_types';
+import { FrameworkLib } from './framework';
+
+interface JWTToken {
+  config: { id: string; sharedId: string };
+  type: TokenType;
+}
 
 export class TokenLib {
-  public async verify(token: any): Promise<TokenVerificationResponse> {
-    throw new Error('Not implemented');
+  constructor(
+    private readonly adapter: TokenAdapter,
+    private readonly frameworkLib: FrameworkLib
+  ) {}
+
+  /**
+   * Verify if a token is valid
+   * @param token
+   */
+  public async verify(token: string): Promise<TokenVerificationResponse> {
+    try {
+      const decodedToken = this._verifyJWTToken(token);
+
+      await this._verifyPersistedToken(token);
+
+      return {
+        valid: true,
+        token: {
+          config: decodedToken.config,
+        },
+      };
+    } catch (error) {
+      return {
+        valid: false,
+        reason: error.message,
+      };
+    }
   }
 
   public async generateAccessToken(token: any): Promise<string> {
     throw new Error('Not implemented');
+  }
+
+  /**
+   * Generate a new enrolment token for a config
+   * @param config
+   * @param expire
+   */
+  public async generateEnrolmentToken(
+    config: { id: string; sharedId: string },
+    expire: string = '24h'
+  ): Promise<string> {
+    const encryptionKey = this.frameworkLib.getSetting('encryptionKey');
+    const token = signToken(
+      {
+        type: TokenType.ENROLMENT_TOKEN,
+        config,
+      },
+      encryptionKey,
+      {
+        expiresIn: expire,
+      }
+    );
+    const tokenHash = await this._hashJWTToken(token);
+
+    await this.adapter.create({
+      active: true,
+      type: TokenType.ENROLMENT_TOKEN,
+      tokenHash,
+      config,
+    });
+
+    return token;
+  }
+
+  private _verifyJWTToken(token: string): JWTToken {
+    const encryptionKey = this.frameworkLib.getSetting('encryptionKey');
+    const decodedToken = verifyToken(token, encryptionKey) as JWTToken;
+
+    if (decodedToken.type !== TokenType.ENROLMENT_TOKEN) {
+      throw new Error('Not a valid token type');
+    }
+
+    return decodedToken;
+  }
+
+  private async _verifyPersistedToken(token: string) {
+    const tokenHash = await this._hashJWTToken(token);
+    const persistedToken = await this.adapter.getByTokenHash(tokenHash);
+    if (!persistedToken) {
+      throw new Error('Token not found');
+    }
+
+    if (persistedToken.active === false) {
+      throw new Error('Token is not active');
+    }
+  }
+
+  private async _hashJWTToken(token: string): Promise<string> {
+    const encryptionKey = this.frameworkLib.getSetting('encryptionKey');
+
+    const hmac = createHmac('sha512', encryptionKey);
+
+    return hmac.update(token).digest('hex');
   }
 }
