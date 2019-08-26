747389dcefd6cc0094d6677351b66bd3134e9bb5
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2019-08-26 19:17:32 +0200

    Adapt link to context in discover table

--- src/legacy/core_plugins/kibana/public/discover/doc_table/components/table_row.js
+++ src/legacy/core_plugins/kibana/public/discover/doc_table/components/table_row.js
@@ -109,7 +109,7 @@ module.directive('kbnTableRow', function ($compile, $httpParamSerializer, kbnUrl
       };
 
       $scope.getContextAppHref = () => {
-        const path = kbnUrl.eval('#/context/{{ indexPattern }}/{{ anchorType }}/{{ anchorId }}', {
+        const path = kbnUrl.eval('#/context/{{ indexPattern }}/{{ anchorId }}', {
           anchorId: $scope.row._id,
           anchorType: $scope.row._type,
           indexPattern: $scope.indexPattern.id,
