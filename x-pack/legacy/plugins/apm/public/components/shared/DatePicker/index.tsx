9527d4c36fa264841335ea5ba28c7c4e87fe213d
Miqayel Ohanjanyan <ohanjanyan.miqayel@gmail.com>
2019-08-27 16:11:18 +0400

    Add commonly used ranges to apm date-picker

--- x-pack/legacy/plugins/apm/public/components/shared/DatePicker/index.tsx
+++ x-pack/legacy/plugins/apm/public/components/shared/DatePicker/index.tsx
@@ -14,6 +14,16 @@ import { useUrlParams } from '../../../hooks/useUrlParams';
 export function DatePicker() {
   const location = useLocation();
   const { urlParams, refreshTimeRange } = useUrlParams();
+  const commonlyUsedRanges = [
+    { start: 'now-15m', end: 'now', label: 'Last 15 minutes' },
+    { start: 'now-30m', end: 'now', label: 'Last 30 minutes' },
+    { start: 'now-1h', end: 'now', label: 'Last 1 hour' },
+    { start: 'now-24h', end: 'now', label: 'Last 24 hours' },
+    { start: 'now-7d', end: 'now', label: 'Last 7 days' },
+    { start: 'now-30d', end: 'now', label: 'Last 30 days' },
+    { start: 'now-90d', end: 'now', label: 'Last 90 days' },
+    { start: 'now-1y', end: 'now', label: 'Last 1 year' }
+  ];
 
   function updateUrl(nextQuery: {
     rangeFrom?: string;
@@ -58,6 +68,7 @@ export function DatePicker() {
       }}
       onRefreshChange={onRefreshChange}
       showUpdateButton={true}
+      commonlyUsedRanges={commonlyUsedRanges}
     />
   );
 }
