28187968d1e3f6132e9d78ce905e2db52c660311
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2019-08-27 12:00:47 -0700

    Reset dirty saved query on reload (#43927)
    * Clears changes a loaded saved query before loading another one
    
    * Adds general functional test to ensure changes to a saved query are discarded on reloading it
    
    * Moves resetting a dirty saved query to the app controllers in Discover, Visualize and Dashboard

--- src/legacy/core_plugins/kibana/public/discover/controllers/discover.js
+++ src/legacy/core_plugins/kibana/public/discover/controllers/discover.js
@@ -923,7 +923,7 @@ function discoverController(
   };
 
   $scope.onSavedQueryUpdated = savedQuery => {
-    $scope.savedQuery = savedQuery;
+    $scope.savedQuery = { ...savedQuery };
   };
 
   $scope.onClearSavedQuery = () => {
