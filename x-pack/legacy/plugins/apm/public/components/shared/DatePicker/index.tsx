e0926ea5a28c4af33cae1497a00f5b03fc2b6b0e
Miqayel Ohanjanyan <ohanjanyan.miqayel@gmail.com>
2019-08-27 17:10:58 +0400

    Apm date-picker internationalization

--- x-pack/legacy/plugins/apm/public/components/shared/DatePicker/index.tsx
+++ x-pack/legacy/plugins/apm/public/components/shared/DatePicker/index.tsx
@@ -5,6 +5,7 @@
  */
 
 import { EuiSuperDatePicker } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
 import React from 'react';
 import { fromQuery, toQuery } from '../Links/url_helpers';
 import { history } from '../../../utils/history';
@@ -15,14 +16,62 @@ export function DatePicker() {
   const location = useLocation();
   const { urlParams, refreshTimeRange } = useUrlParams();
   const commonlyUsedRanges = [
-    { start: 'now-15m', end: 'now', label: 'Last 15 minutes' },
-    { start: 'now-30m', end: 'now', label: 'Last 30 minutes' },
-    { start: 'now-1h', end: 'now', label: 'Last 1 hour' },
-    { start: 'now-24h', end: 'now', label: 'Last 24 hours' },
-    { start: 'now-7d', end: 'now', label: 'Last 7 days' },
-    { start: 'now-30d', end: 'now', label: 'Last 30 days' },
-    { start: 'now-90d', end: 'now', label: 'Last 90 days' },
-    { start: 'now-1y', end: 'now', label: 'Last 1 year' }
+    {
+      start: 'now-15m',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last15MinutesLabel', {
+        defaultMessage: 'Last 15 minutes'
+      })
+    },
+    {
+      start: 'now-30m',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last30MinutesLabel', {
+        defaultMessage: 'Last 30 minutes'
+      })
+    },
+    {
+      start: 'now-1h',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last1HourLabel', {
+        defaultMessage: 'Last 1 hour'
+      })
+    },
+    {
+      start: 'now-24h',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last24HoursLabel', {
+        defaultMessage: 'Last 24 hours'
+      })
+    },
+    {
+      start: 'now-7d',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last7DaysLabel', {
+        defaultMessage: 'Last 7 days'
+      })
+    },
+    {
+      start: 'now-30d',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last30DaysLabel', {
+        defaultMessage: 'Last 30 days'
+      })
+    },
+    {
+      start: 'now-90d',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last90DaysLabel', {
+        defaultMessage: 'Last 90 days'
+      })
+    },
+    {
+      start: 'now-1y',
+      end: 'now',
+      label: i18n.translate('xpack.apm.datePicker.last1YearLabel', {
+        defaultMessage: 'Last 1 year'
+      })
+    }
   ];
 
   function updateUrl(nextQuery: {
