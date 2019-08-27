880e5c6dd3b6326d533b90ccbeefebf48ca9ad9e
Tre' Seymour <wayne.seymour@elastic.co>
2019-08-27 08:40:15 -0600

    Move conf and task to config per review

--- tasks/config/availabletasks.js
+++ tasks/config/availabletasks.js
@@ -17,28 +17,19 @@
  * under the License.
  */
 
-require('./src/setup_node_env');
-
 module.exports = function (grunt) {
-  // set the config once before calling load-grunt-config
-  // and once during so that we have access to it via
-  // grunt.config.get() within the config files
   const config = {
-    root: __dirname,
+    availabletasks: {
+      tasks: {
+        options: {
+          filter: 'exclude',
+          tasks: ['availabletasks', 'tasks']
+        }
+      }
+    }
   };
-
+  grunt.registerTask('tasks', ['availabletasks']);
   grunt.config.merge(config);
 
-  // load plugins
-  require('load-grunt-config')(grunt, {
-    configPath: __dirname + '/tasks/config',
-    init: true,
-    config: config,
-    loadGruntTasks: {
-      pattern: ['grunt-*', '@*/grunt-*', 'gruntify-*', '@*/gruntify-*']
-    }
-  });
-
-  // load task definitions
-  grunt.task.loadTasks('tasks');
+  return config;
 };
