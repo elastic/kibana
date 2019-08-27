180e17534497c34919d4d8c1e3e85a358714657e
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-08-27 12:01:20 -0700

    Adds savedQuery methods to map_controller

--- x-pack/legacy/plugins/maps/public/angular/map_controller.js
+++ x-pack/legacy/plugins/maps/public/angular/map_controller.js
@@ -55,7 +55,7 @@ import {
 import { FilterStateStore } from '@kbn/es-query';
 import { setup as data } from '../../../../../../src/legacy/core_plugins/data/public/legacy';
 
-// const { savedQueryService } = data.search.services;
+const { savedQueryService } = data.search.services;
 
 const REACT_ANCHOR_DOM_ELEMENT_ID = 'react-maps-root';
 
@@ -126,58 +126,71 @@ app.controller('GisMapController', ($scope, $route, kbnUrl, localStorage, AppSta
     globalState: globalState,
   });
   /* Saved Queries */
+  $scope.showSaveQuery = capabilities.get().maps.saveQuery;
+
+  $scope.$watch(() => capabilities.get().maps.saveQuery, (newCapability) => {
+    $scope.showSaveQuery = newCapability;
+  });
+
   $scope.onQuerySaved = savedQuery => {
     $scope.savedQuery = savedQuery;
   };
 
   $scope.onSavedQueryUpdated = savedQuery => {
-    $scope.savedQuery = savedQuery;
+    $scope.savedQuery = { ...savedQuery };
   };
 
   $scope.onClearSavedQuery = () => {
-    $scope.savedQuery = undefined;
-  };
-
-  const updateStateFromSavedQuery = (savedQuery) => {
-    // use the items from the saved query as the scope variables,
-    // then sync the app and global state,
-    // then dispatch set Query
-    $scope.query = savedQuery.attributes.query;
-    $scope.filters = savedQuery.attributes.filters || [];
-    if (savedQuery.attributes.timefilter) {
-      $scope.time = {
-        from: savedQuery.attributes.timefilter.timeFrom,
-        to: savedQuery.attributes.timefilter.timeTo,
-      };
-      if (savedQuery.attributes.timefilter.refreshInterval) {
-        $scope.refreshInterval = savedQuery.attributes.timefilter.refreshInterval;
-      }
-    }
-    syncAppAndGlobalState();
-    dispatchSetQuery();
-  };
-  $scope.$watch('savedQuery', (newSavedQuery, oldSavedQuery) => {
-    if (!newSavedQuery) {
-      $state.savedQuery = undefined;
-      $scope.query = {
+    delete $scope.savedQuery;
+    delete $state.savedQuery;
+    onQueryChange({
+      filters: [],
+      query: {
         query: '',
         language: localStorage.get('kibana.userQueryLanguage')
-      };
-      syncAppAndGlobalState();
-      dispatchSetQuery();
-    } else {
-      $state.savedQuery = newSavedQuery.id;
-      if (newSavedQuery.id === (oldSavedQuery && oldSavedQuery.id)) {
-        updateStateFromSavedQuery(newSavedQuery);
-      }
+      },
+    });
+  };
+
+  async function updateStateFromSavedQuery(savedQuery) {
+    // use the items from the saved query as the arguments to onQueryChange
+    // onQueryChange syncs the app with the global state, taking the args given and setting those to $scope and $state variables.
+    // For the refresh config, we don't need an 'isPaused' because the app will take the global setting for us.
+    // onQueryChange then also does the dispatching, so we don't need to worry about that either,
+    await onQueryChange({
+      filters: savedQuery.attributes.filters || [],
+      query: savedQuery.attributes.query,
+      time: savedQuery.attributes.timefilter ?
+        {
+          from: savedQuery.attributes.timefilter.timeFrom,
+          to: savedQuery.attributes.timefilter.timeTo
+        } : undefined
+    });
+    if (savedQuery.attributes.timefilter && savedQuery.attributes.timefilter.refreshInterval) {
+      // I need to set the refreshInterval but don't know where to get isPaused from
+      $scope.refreshConfig.interval = savedQuery.attributes.timefilter.refreshInterval;
+    }
+  }
+
+  $scope.$watch('savedQuery', (newSavedQuery, oldSavedQuery) => {
+    if (!newSavedQuery) return;
+    $state.savedQuery = newSavedQuery.id;
+
+    if (newSavedQuery.id === (oldSavedQuery && oldSavedQuery.id)) {
+      updateStateFromSavedQuery(newSavedQuery);
     }
   });
-
-  $scope.$watch(() => $state.savedQuery, newSavedQueryId => {
+  $scope.$watch('state.savedQuery', newSavedQueryId => {
     if (!newSavedQueryId) {
       $scope.savedQuery = undefined;
       return;
     }
+    savedQueryService.getSavedQuery(newSavedQueryId).then((savedQuery) => {
+      $scope.$evalAsync(() => {
+        $scope.savedQuery = savedQuery;
+        updateStateFromSavedQuery(savedQuery);
+      });
+    });
   });
 
   async function onQueryChange({ filters, query, time }) {
@@ -444,7 +457,6 @@ app.controller('GisMapController', ($scope, $route, kbnUrl, localStorage, AppSta
   timefilter.disableTimeRangeSelector();
   timefilter.disableAutoRefreshSelector();
   $scope.showDatePicker = true; // used by query-bar directive to enable timepikcer in query bar
-  $scope.showSavedQuery = true; //used by saved queries to show the saved query management component
   $scope.topNavMenu = [{
     id: 'full-screen',
     label: i18n.translate('xpack.maps.mapController.fullScreenButtonLabel', {
