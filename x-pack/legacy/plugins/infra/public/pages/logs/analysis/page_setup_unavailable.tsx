eae56fea4a24a2b7a8437e328c9f181ed76ed1a5
Zacqary Xeper <zacqary.xeper@elastic.co>
2019-08-27 15:51:25 -0500

    Add missing unavaiablable page file

---
+++ x-pack/legacy/plugins/infra/public/pages/logs/analysis/page_setup_unavailable.tsx
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { EuiEmptyPrompt, EuiLink, EuiButton } from '@elastic/eui';
+
+export const AnalysisSetupUnavailable: React.FunctionComponent<{}> = () => (
+  <EuiEmptyPrompt
+    title={
+      <h2>
+        <FormattedMessage
+          id="xpack.infra.logs.analysisPage.unavailable.mLDisabledTitle"
+          defaultMessage="Analysis requires Machine Learning to be available"
+        />
+      </h2>
+    }
+    body={
+      <p>
+        <FormattedMessage
+          id="xpack.infra.logs.analysisPage.unavailable.mlDisabledBody"
+          defaultMessage="Check the {machineLearningAppLink} for more information."
+          values={{
+            machineLearningAppLink: (
+              <EuiLink href="ml" target="_blank">
+                <FormattedMessage
+                  id="xpack.infra.logs.analysisPage.unavailable.mlAppLink"
+                  defaultMessage="Machine Learning app"
+                />
+              </EuiLink>
+            ),
+          }}
+        />
+      </p>
+    }
+    actions={
+      <EuiButton target="_blank" href="ml" color="primary" fill>
+        {i18n.translate('xpack.infra.logs.analysisPage.unavailable.mlAppButton', {
+          defaultMessage: 'Open Machine Learning',
+        })}
+      </EuiButton>
+    }
+  />
+);
