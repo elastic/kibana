5917cd375f7e66a1a24582b64ee20c41b2a2619e
Ahmad Bamieh <ahmadbamieh@gmail.com>
2019-08-27 13:34:56 +0300

    Telemetry/opt in welcome screen (#42110)
    * refactor opt_in_message
    
    * welcome optin card
    
    * finish optin in welcome screen
    
    * add  steps
    
    * disable  current step
    
    * remove checkbox option
    
    * add metrics
    
    * fix  typescript checks
    
    * hide in oss
    
    * update snapshots
    
    * only require TelemetryOptInProvider if telemetry is enabled
    
    * pass telemetry description from service
    
    * remove x-pack import
    
    * remove x-pack import
    
    * update image
    
    * update per design feedback
    
    * update props
    
    * fix in oss
    
    * await before moving to next screen
    
    * utlize bannerId in telemtry provider
    
    * keep export in 1 line
    
    * ts-ignore banner import
    
    * add test
    
    * update tests
    
    * update translations
    
    * update home tests
    
    * remove extra license header
    
    * showTelemetryOptIn -> shouldShowTelemetryOptIn
    
    * remote extra EuiTexts
    
    * update jest snapshot
    
    * unencrypted telemetry example

--- x-pack/legacy/plugins/telemetry/public/components/index.ts
+++ x-pack/legacy/plugins/telemetry/public/components/index.ts
@@ -4,5 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+// @ts-ignore
 export { TelemetryForm } from './telemetry_form';
 export { OptInExampleFlyout } from './opt_in_details_component';
+export { OptInBanner } from './opt_in_banner_component';
+export { OptInMessage } from './opt_in_message';
