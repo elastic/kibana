962a377835576c8441fcfee87c14a6026623e9cc
Patrick Mueller <pmuellr@gmail.com>
2019-08-27 09:41:04 -0400

    Fixes alert mustache templating with arrays
    fixes https://github.com/elastic/kibana/issues/44057
    
    Prior to this fix, values inside an array were not subject to the
    mustache transform.

--- x-pack/legacy/plugins/alerting/server/lib/transform_action_params.ts
+++ x-pack/legacy/plugins/alerting/server/lib/transform_action_params.ts
@@ -5,19 +5,13 @@
  */
 
 import Mustache from 'mustache';
-import { isPlainObject } from 'lodash';
+import { isString, cloneDeep } from 'lodash';
 import { AlertActionParams, State, Context } from '../types';
 
 export function transformActionParams(params: AlertActionParams, state: State, context: Context) {
-  const result: AlertActionParams = {};
-  for (const [key, value] of Object.entries(params)) {
-    if (isPlainObject(value)) {
-      result[key] = transformActionParams(value as AlertActionParams, state, context);
-    } else if (typeof value !== 'string') {
-      result[key] = value;
-    } else {
-      result[key] = Mustache.render(value, { context, state });
-    }
-  }
-  return result;
+  return cloneDeep(params, value => {
+    if (!isString(value)) return;
+
+    return Mustache.render(value, { context, state });
+  });
 }
