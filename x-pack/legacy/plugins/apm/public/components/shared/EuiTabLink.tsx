bc1ac49f552b02a27c867c7973494629fdb0b730
Dario Gieselaar <dario.gieselaar@elastic.co>
2019-08-28 13:39:02 +0200

    Add comment clarifying override of EUITab styling

--- x-pack/legacy/plugins/apm/public/components/shared/EuiTabLink.tsx
+++ x-pack/legacy/plugins/apm/public/components/shared/EuiTabLink.tsx
@@ -9,6 +9,11 @@ import { EuiTab } from '@elastic/eui';
 import theme from '@elastic/eui/dist/eui_theme_light.json';
 import { px, unit } from '../../style/variables';
 
+// We need to remove padding and add it to the link,
+// to prevent the user from clicking in the tab, but outside of the link
+// We also need to override the color here to subdue the color of the link
+// when not selected
+
 const EuiTabLink = styled(EuiTab)`
   padding: 0;
   a {
