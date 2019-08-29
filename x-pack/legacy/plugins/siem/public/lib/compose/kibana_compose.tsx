37c4f68a63264f46c2a1f3dae8edeffcc2a5675f
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2019-08-29 18:30:15 -0400

    Re-Bootstrap siem app (#43421) (#44391)
    * Change the bootstrap of the app to allow embeddable
    
    * add embeddable library
    
    * make sure to use new platform to get kibana version
    
    * review

--- x-pack/legacy/plugins/siem/public/lib/compose/kibana_compose.tsx
+++ x-pack/legacy/plugins/siem/public/lib/compose/kibana_compose.tsx
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { ILocationProvider } from 'angular';
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import ApolloClient from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
@@ -12,13 +13,8 @@ import 'ui/autoload/all';
 import chrome from 'ui/chrome';
 // @ts-ignore: path dynamic for kibana
 import { uiModules } from 'ui/modules';
-import uiRoutes from 'ui/routes';
-// @ts-ignore: path dynamic for kibana
-import { timezoneProvider } from 'ui/vis/lib/timezone';
 
 import introspectionQueryResultData from '../../graphql/introspection.json';
-import { AppKibanaFrameworkAdapter } from '../adapters/framework/kibana_framework_adapter';
-import { AppKibanaObservableApiAdapter } from '../adapters/observable_api/kibana_observable_api';
 import { AppFrontendLibs } from '../lib';
 import { getLinks } from './helpers';
 
@@ -28,11 +24,6 @@ export function compose(): AppFrontendLibs {
     fragmentMatcher: new IntrospectionFragmentMatcher({
       introspectionQueryResultData,
     }),
-  });
-
-  const observableApi = new AppKibanaObservableApiAdapter({
-    basePath: chrome.getBasePath(),
-    xsrfToken: chrome.getXsrfToken(),
   });
 
   const graphQLOptions = {
@@ -45,12 +36,19 @@ export function compose(): AppFrontendLibs {
 
   const appModule = uiModules.get('app/siem');
 
-  const framework = new AppKibanaFrameworkAdapter(appModule, uiRoutes, timezoneProvider);
+  // disable angular's location provider
+  appModule.config(($locationProvider: ILocationProvider) => {
+    $locationProvider.html5Mode({
+      enabled: false,
+      requireBase: false,
+      rewriteLinks: false,
+    });
+  });
+
+  // const framework = new AppKibanaFrameworkAdapter(appModule, uiRoutes, timezoneProvider);
 
   const libs: AppFrontendLibs = {
     apolloClient,
-    framework,
-    observableApi,
   };
   return libs;
 }
