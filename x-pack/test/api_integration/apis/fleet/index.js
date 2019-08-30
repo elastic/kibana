d44a561df5da44e249f3583626b583f8214dfcd7
Nicolas Chaulet <n.chaulet@gmail.com>
2019-08-30 14:18:45 -0400

    [Ingest] Elastic Fleet agent endpoints (#44319)

---
+++ x-pack/test/api_integration/apis/fleet/index.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export default function loadTests({ loadTestFile }) {
+  describe('Fleet Endpoints', () => {
+    loadTestFile(require.resolve('./delete_agent'));
+    loadTestFile(require.resolve('./list_agent'));
+    loadTestFile(require.resolve('./enroll_agent'));
+  });
+}
