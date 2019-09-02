66da955daf8e927e51ad39aee772d0bf866f6351
James Gowdy <jgowdy@elastic.co>
2019-09-02 16:09:45 +0100

    [ML] Fixing wizard charts repeated loading (#44409)
    * [ML] Caching wizard chart data
    
    * using shared chart settings hook
    
    * fixing settings hook and switching to memoize-one
    
    * removing custom hook

--- x-pack/legacy/plugins/ml/public/jobs/new_job_new/pages/components/pick_fields_step/components/single_metric_view/metric_selection_summary.tsx
+++ x-pack/legacy/plugins/ml/public/jobs/new_job_new/pages/components/pick_fields_step/components/single_metric_view/metric_selection_summary.tsx
@@ -10,82 +10,58 @@ import {
   SingleMetricJobCreator,
   isSingleMetricJobCreator,
 } from '../../../../../common/job_creator';
+import { Results, ModelItem, Anomaly } from '../../../../../common/results_loader';
 import { LineChartData } from '../../../../../common/chart_loader';
-import { AggSelect, DropDownLabel, DropDownProps, createLabel } from '../agg_select';
-import { newJobCapsService } from '../../../../../../../services/new_job_capabilities_service';
-import { AggFieldPair } from '../../../../../../../../common/types/fields';
 import { AnomalyChart, CHART_TYPE } from '../../../charts/anomaly_chart';
 import { getChartSettings } from '../../../charts/common/settings';
 import { mlMessageBarService } from '../../../../../../../components/messagebar/messagebar_service';
 
-interface Props {
-  setIsValid: (na: boolean) => void;
-}
-
 const DTR_IDX = 0;
 
-export const SingleMetricDetectors: FC<Props> = ({ setIsValid }) => {
-  const {
-    jobCreator: jc,
-    jobCreatorUpdate,
-    jobCreatorUpdated,
-    chartLoader,
-    chartInterval,
-  } = useContext(JobCreatorContext);
+export const SingleMetricDetectorsSummary: FC = () => {
+  const { jobCreator: jc, chartLoader, resultsLoader, chartInterval } = useContext(
+    JobCreatorContext
+  );
 
   if (isSingleMetricJobCreator(jc) === false) {
     return null;
   }
   const jobCreator = jc as SingleMetricJobCreator;
 
-  const { fields } = newJobCapsService;
-  const [selectedOptions, setSelectedOptions] = useState<DropDownProps>([
-    { label: createLabel(jobCreator.aggFieldPair) },
-  ]);
-  const [aggFieldPair, setAggFieldPair] = useState<AggFieldPair | null>(jobCreator.aggFieldPair);
   const [lineChartsData, setLineChartData] = useState<LineChartData>({});
   const [loadingData, setLoadingData] = useState(false);
-  const [start, setStart] = useState(jobCreator.start);
-  const [end, setEnd] = useState(jobCreator.end);
+  const [modelData, setModelData] = useState<ModelItem[]>([]);
+  const [anomalyData, setAnomalyData] = useState<Anomaly[]>([]);
 
-  function detectorChangeHandler(selectedOptionsIn: DropDownLabel[]) {
-    setSelectedOptions(selectedOptionsIn);
-    if (selectedOptionsIn.length) {
-      const option = selectedOptionsIn[0];
-      if (typeof option !== 'undefined') {
-        setAggFieldPair({ agg: option.agg, field: option.field });
-      } else {
-        setAggFieldPair(null);
-      }
+  function setResultsWrapper(results: Results) {
+    const model = results.model[DTR_IDX];
+    if (model !== undefined) {
+      setModelData(model);
+    }
+    const anomalies = results.anomalies[DTR_IDX];
+    if (anomalies !== undefined) {
+      setAnomalyData(anomalies);
     }
   }
 
   useEffect(() => {
-    if (aggFieldPair !== null) {
-      jobCreator.setDetector(aggFieldPair.agg, aggFieldPair.field);
-      jobCreatorUpdate();
-      loadChart();
-      setIsValid(aggFieldPair !== null);
-    }
-  }, [aggFieldPair]);
-
-  useEffect(() => {
-    if (jobCreator.start !== start || jobCreator.end !== end) {
-      setStart(jobCreator.start);
-      setEnd(jobCreator.end);
-      loadChart();
-    }
-  }, [jobCreatorUpdated]);
+    // subscribe to progress and results
+    const subscription = resultsLoader.subscribeToResults(setResultsWrapper);
+    loadChart();
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
 
   async function loadChart() {
-    if (aggFieldPair !== null) {
+    if (jobCreator.aggFieldPair !== null) {
       setLoadingData(true);
       try {
         const cs = getChartSettings(jobCreator, chartInterval);
         const resp: LineChartData = await chartLoader.loadLineCharts(
           jobCreator.start,
           jobCreator.end,
-          [aggFieldPair],
+          [jobCreator.aggFieldPair],
           null,
           null,
           cs.intervalMs
@@ -103,19 +79,13 @@ export const SingleMetricDetectors: FC<Props> = ({ setIsValid }) => {
 
   return (
     <Fragment>
-      <AggSelect
-        fields={fields}
-        changeHandler={detectorChangeHandler}
-        selectedOptions={selectedOptions}
-        removeOptions={[]}
-      />
       {(lineChartsData[DTR_IDX] !== undefined || loadingData === true) && (
         <Fragment>
           <AnomalyChart
             chartType={CHART_TYPE.LINE}
             chartData={lineChartsData[DTR_IDX]}
-            modelData={[]}
-            anomalyData={[]}
+            modelData={modelData}
+            anomalyData={anomalyData}
             height="300px"
             width="100%"
             loading={loadingData}
