4f886a79cb1e56d35a638b4221b1f7aaf5120ff8
Liza K <liza.katz@elastic.co>
2019-09-02 13:42:34 +0300

    Fixed faulty import of data plugin

--- src/legacy/ui/public/vis/vis_filters/vis_filters.js
+++ src/legacy/ui/public/vis/vis_filters/vis_filters.js
@@ -20,7 +20,8 @@
 import _ from 'lodash';
 import { pushFilterBarFilters } from '../push_filters';
 import { onBrushEvent } from './brush_event';
-import { uniqFilters, setup as data } from '../../../../core_plugins/data/public';
+import { uniqFilters } from '../../../../core_plugins/data/public';
+import { setup as data } from '../../../../core_plugins/data/public/legacy';
 import { toggleFilterNegated } from '@kbn/es-query';
 /**
  * For terms aggregations on `__other__` buckets, this assembles a list of applicable filter
