46783ac885e9531ba5221b00d5972ecf082f80ca
Robert Oskamp <robert.oskamp@elastic.co>
2019-09-03 13:03:17 +0200

    Add enhanced job list validations to 'create single metric job' test

--- x-pack/test/functional/apps/machine_learning/create_single_metric_job.ts
+++ x-pack/test/functional/apps/machine_learning/create_single_metric_job.ts
@@ -13,6 +13,12 @@ export default function({ getService }: FtrProviderContext) {
   const ml = getService('ml');
 
   const jobId = `fq_single_1_${Date.now()}`;
+  const jobDescription =
+    'Create single metric job based on the farequote dataset with 30m bucketspan and mean(responsetime)';
+  const jobGroups = ['automated', 'farequote', 'single-metric'];
+  const aggAndFieldIdentifier = 'Mean(responsetime)';
+  const bucketSpan = '30m';
+  const memoryLimit = '15MB';
 
   describe('single metric job creation', function() {
     this.tags('smoke');
@@ -58,14 +64,12 @@ export default function({ getService }: FtrProviderContext) {
     });
 
     it('selects field and aggregation', async () => {
-      const identifier = 'Mean(responsetime)';
       await ml.jobWizardCommon.assertAggAndFieldInputExists();
-      await ml.jobWizardCommon.selectAggAndField(identifier);
-      await ml.jobWizardCommon.assertAggAndFieldSelection(identifier);
+      await ml.jobWizardCommon.selectAggAndField(aggAndFieldIdentifier);
+      await ml.jobWizardCommon.assertAggAndFieldSelection(aggAndFieldIdentifier);
     });
 
     it('inputs the bucket span', async () => {
-      const bucketSpan = '30m';
       await ml.jobWizardCommon.assertBucketSpanInputExists();
       await ml.jobWizardCommon.setBucketSpan(bucketSpan);
       await ml.jobWizardCommon.assertBucketSpanValue(bucketSpan);
@@ -83,15 +87,12 @@ export default function({ getService }: FtrProviderContext) {
     });
 
     it('inputs the job description', async () => {
-      const jobDescription =
-        'Create single metric job based on the farequote dataset with 30m bucketspan and mean(responsetime)';
       await ml.jobWizardCommon.assertJobDescriptionInputExists();
       await ml.jobWizardCommon.setJobDescription(jobDescription);
       await ml.jobWizardCommon.assertJobDescriptionValue(jobDescription);
     });
 
     it('inputs job groups', async () => {
-      const jobGroups = ['automated', 'farequote', 'single-metric'];
       await ml.jobWizardCommon.assertJobGroupInputExists();
       for (const jobGroup of jobGroups) {
         await ml.jobWizardCommon.addJobGroup(jobGroup);
@@ -114,7 +115,6 @@ export default function({ getService }: FtrProviderContext) {
     });
 
     it('inputs the model memory limit', async () => {
-      const memoryLimit = '15MB';
       await ml.jobWizardCommon.assertModelMemoryLimitInputExists();
       await ml.jobWizardCommon.setModelMemoryLimit(memoryLimit);
       await ml.jobWizardCommon.assertModelMemoryLimitValue(memoryLimit);
@@ -138,9 +138,56 @@ export default function({ getService }: FtrProviderContext) {
     it('displays the created job in the job list', async () => {
       await ml.navigation.navigateToMl();
       await ml.navigation.navigateToJobManagement();
-      await ml.jobManagement.filterJobsTable(jobId);
-      const jobRow = await ml.jobManagement.getJobRowByJobId(jobId);
-      expect(jobRow).to.not.be(null);
+
+      await ml.jobTable.waitForJobsToLoad();
+      await ml.jobTable.filterWithSearchString(jobId);
+      const rows = await ml.jobTable.parseJobTable();
+      expect(rows.filter(row => row.id === jobId)).to.have.length(1);
+    });
+
+    it('displays details for the created job in the job list', async () => {
+      const rows = await ml.jobTable.parseJobTable();
+      const job = rows.filter(row => row.id === jobId)[0];
+      expect(job.description).to.eql(jobDescription);
+      expect(job.jobGroups).to.eql(jobGroups);
+      expect(job.recordCount).to.eql('2,399');
+      expect(job.memoryStatus).to.eql('ok');
+      expect(job.jobState).to.eql('closed');
+      expect(job.datafeedState).to.eql('stopped');
+      expect(job.latestTimestamp).to.eql('2016-02-12 00:56:59');
+
+      const countDetails = await ml.jobTable.parseJobCounts(jobId);
+      const counts = countDetails.counts;
+      expect(counts.job_id).to.eql(jobId);
+      expect(counts.processed_record_count).to.eql('2,399');
+      expect(counts.processed_field_count).to.eql('4,798');
+      expect(counts.input_bytes).to.eql('180.6 KB');
+      expect(counts.input_field_count).to.eql('4,798');
+      expect(counts.invalid_date_count).to.eql('0');
+      expect(counts.missing_field_count).to.eql('0');
+      expect(counts.out_of_order_timestamp_count).to.eql('0');
+      expect(counts.empty_bucket_count).to.eql('0');
+      expect(counts.sparse_bucket_count).to.eql('0');
+      expect(counts.bucket_count).to.eql('239');
+      expect(counts.earliest_record_timestamp).to.eql('2016-02-07 01:02:50');
+      expect(counts.latest_record_timestamp).to.eql('2016-02-12 00:56:59');
+      expect(counts).to.have.property('last_data_time');
+      expect(counts.input_record_count).to.eql('2,399');
+      expect(counts.latest_bucket_timestamp).to.eql('2016-02-12 00:30:00');
+
+      const modelSizeStats = countDetails.modelSizeStats;
+      expect(modelSizeStats.job_id).to.eql(jobId);
+      expect(modelSizeStats.result_type).to.eql('model_size_stats');
+      expect(modelSizeStats.model_bytes).to.eql('47.6 KB');
+      expect(modelSizeStats.model_bytes_exceeded).to.eql('0');
+      expect(modelSizeStats.model_bytes_memory_limit).to.eql('15728640');
+      expect(modelSizeStats.total_by_field_count).to.eql('3');
+      expect(modelSizeStats.total_over_field_count).to.eql('0');
+      expect(modelSizeStats.total_partition_field_count).to.eql('2');
+      expect(modelSizeStats.bucket_allocation_failures_count).to.eql('0');
+      expect(modelSizeStats.memory_status).to.eql('ok');
+      expect(modelSizeStats).to.have.property('log_time');
+      expect(modelSizeStats.timestamp).to.eql('2016-02-12 00:00:00');
     });
   });
 }
