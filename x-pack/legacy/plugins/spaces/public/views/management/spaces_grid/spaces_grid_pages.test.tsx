99cfd1694302eef5cfe369f04978708d81ffab8a
Larry Gregory <larry.gregory@elastic.co>
2019-09-05 07:12:42 -0400

    consolidate mock

--- x-pack/legacy/plugins/spaces/public/views/management/spaces_grid/spaces_grid_pages.test.tsx
+++ x-pack/legacy/plugins/spaces/public/views/management/spaces_grid/spaces_grid_pages.test.tsx
@@ -6,19 +6,8 @@
 jest.mock('ui/kfetch', () => ({
   kfetch: () => Promise.resolve([]),
 }));
-jest.mock('../../../../../xpack_main/public/services/xpack_info', () => {
-  return {
-    xpackInfo: {
-      get: jest.fn().mockImplementation((key: string) => {
-        if (key === 'features.security.showLinks') {
-          return true;
-        }
-        throw new Error(`unexpected key: ${key}`);
-      }),
-    },
-  };
-});
 import '../../../__mocks__/ui_capabilities';
+import '../../../__mocks__/xpack_info';
 import React from 'react';
 import { mountWithIntl, shallowWithIntl } from 'test_utils/enzyme_helpers';
 import { SpaceAvatar } from '../../../components';
