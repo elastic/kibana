5a9d01a592bb910e40d6e1dcd8cb42ac257631d3
Robert Oskamp <robert.oskamp@elastic.co>
2019-09-05 12:41:23 +0200

    [ML] Add enhanced job list validations to `create single metric job` test (#44651) (#44857)
    Backports the following commits to 7.4:
     - [ML] Add enhanced job list validations to `create single metric job` test  (#44651)

--- x-pack/legacy/plugins/ml/public/jobs/jobs_list/components/job_details/job_details_pane.js
+++ x-pack/legacy/plugins/ml/public/jobs/jobs_list/components/job_details/job_details_pane.js
@@ -47,7 +47,7 @@ function Section({ section }) {
   return (
     <React.Fragment>
       <EuiTitle size="xs"><h4>{section.title}</h4></EuiTitle>
-      <div className="job-section">
+      <div className="job-section"  data-test-subj={section.id}>
         <EuiTable compressed={true}>
           <EuiTableBody>
             { section.items.map((item, i) => (<SectionItem item={item} key={i} />)) }
@@ -78,7 +78,7 @@ export class JobDetailsPane  extends Component {
     return (
       <React.Fragment>
         <EuiSpacer size="s" />
-        <div className="row" time={time}>
+        <div className="row" time={time}  data-test-subj={this.props['data-test-subj']}>
           <div className="col-md-6">
             {
               sections
@@ -100,5 +100,6 @@ export class JobDetailsPane  extends Component {
 }
 JobDetailsPane.propTypes = {
   sections: PropTypes.array.isRequired,
+  'data-test-subj': PropTypes.string,
 };
 
