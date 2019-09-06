74ac217a426f1c0dd583aee49df9412b62c69d5c
Catherine Liu <catherine.liu@elastic.co>
2019-09-06 12:02:32 -0700

    Added i18n to HelpMenu and KeyboardShortcutDocs components

--- x-pack/legacy/plugins/canvas/i18n/components.ts
+++ x-pack/legacy/plugins/canvas/i18n/components.ts
@@ -5,12 +5,47 @@
  */
 
 import { i18n } from '@kbn/i18n';
+import { CANVAS } from './constants';
 
 export const ComponentStrings = {
   AddEmbeddableFlyout: {
     getNoItemsText: () =>
       i18n.translate('xpack.canvas.embedObject.noMatchingObjectsMessage', {
         defaultMessage: 'No matching objects found.',
+      }),
+  },
+  HelpMenu: {
+    getHelpMenuDescription: () =>
+      i18n.translate('xpack.canvas.helpMenu.description', {
+        defaultMessage: 'For {CANVAS} specific information',
+        values: {
+          CANVAS,
+        },
+      }),
+    getDocumentationLinkLabel: () =>
+      i18n.translate('xpack.canvas.helpMenu.documentationLinkLabel', {
+        defaultMessage: '{CANVAS} documentation',
+        values: {
+          CANVAS,
+        },
+      }),
+    getKeyboardShortcutsLinkLabel: () =>
+      i18n.translate('xpack.canvas.helpMenu.keyboardShortcutsLinkLabel', {
+        defaultMessage: 'Keyboard Shortcuts',
+      }),
+  },
+  KeyboardShortcutsDoc: {
+    getTitle: () =>
+      i18n.translate('xpack.canvas.keyboardShortcutsDoc.flyoutHeaderTitle', {
+        defaultMessage: 'Keyboard Shortcuts',
+      }),
+    getShortcutSeparator: () =>
+      i18n.translate('xpack.canvas.keyboardShortcutsDoc.shortcutListSeparator', {
+        defaultMessage: 'or',
+      }),
+    getFlyoutCloseButtonAriaLabel: () =>
+      i18n.translate('xpack.canvas.keyboardShortcutsDoc.flyout.closeButtonAriaLabel', {
+        defaultMessage: 'Closes keyboard shortcuts reference',
       }),
   },
 };
