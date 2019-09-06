edfd343eabd5def2b0bfd67e60b696fa0cce349a
Justin Kambic <justin.kambic@elastic.co>
2019-09-06 10:37:04 -0400

    Add files to use for new platform shim.

---
+++ x-pack/legacy/plugins/uptime/server/plugin.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { PluginInitializerContext, InternalCoreSetup } from 'src/core/server';
+import { initServerWithKibana } from './kibana.index';
+
+export function plugin(initializerContext: PluginInitializerContext) {
+  return new Plugin();
+}
+
+export class Plugin {
+  public setup(core: InternalCoreSetup) {
+    initServerWithKibana(core);
+  }
+}
