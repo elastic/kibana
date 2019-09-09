ab68e750e4d114b5ef391b4f62392360160694ce
sainthkh <sainthkh@naver.com>
2019-09-09 17:45:16 +0900

    Prettierize query_params.js

--- test/api_integration/apis/index_patterns/fields_for_time_pattern_route/query_params.js
+++ test/api_integration/apis/index_patterns/fields_for_time_pattern_route/query_params.js
@@ -27,34 +27,29 @@ export default function ({ getService }) {
     before(() => esArchiver.load('index_patterns/daily_index'));
     after(() => esArchiver.unload('index_patterns/daily_index'));
 
-    it('requires `pattern` and `look_back` query params', () => (
+    it('requires `pattern` and `look_back` query params', () =>
       supertest
         .get('/api/index_patterns/_fields_for_time_pattern')
         .query({ pattern: null })
         .expect(400)
         .then(resp => {
           expect(resp.body.validation).to.eql({
-            keys: [
-              'pattern',
-              'look_back'
-            ],
-            source: 'query'
+            keys: ['pattern', 'look_back'],
+            source: 'query',
           });
-        })
-    ));
+        }));
 
-    it('supports `meta_fields` query param', () => (
+    it('supports `meta_fields` query param', () =>
       supertest
         .get('/api/index_patterns/_fields_for_time_pattern')
         .query({
           pattern: '[logs-]YYYY.MM.DD',
           look_back: 1,
-          meta_fields: JSON.stringify(['a'])
+          meta_fields: JSON.stringify(['a']),
         })
-        .expect(200)
-    ));
+        .expect(200));
 
-    it('requires `look_back` to be a number', () => (
+    it('requires `look_back` to be a number', () =>
       supertest
         .get('/api/index_patterns/_fields_for_time_pattern')
         .query({
@@ -64,10 +59,9 @@ export default function ({ getService }) {
         .expect(400)
         .then(resp => {
           expect(resp.body.message).to.contain('"look_back" must be a number');
-        })
-    ));
+        }));
 
-    it('requires `look_back` to be greater than one', () => (
+    it('requires `look_back` to be greater than one', () =>
       supertest
         .get('/api/index_patterns/_fields_for_time_pattern')
         .query({
@@ -77,7 +71,6 @@ export default function ({ getService }) {
         .expect(400)
         .then(resp => {
           expect(resp.body.message).to.contain('"look_back" must be larger than or equal to 1');
-        })
-    ));
+        }));
   });
 }
