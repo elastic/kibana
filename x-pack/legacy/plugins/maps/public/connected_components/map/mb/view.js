e7d1e920c9037fc55d5019a3146981201bdc2102
Nick Peihl <nick.peihl@elastic.co>
2019-09-08 20:54:46 -0500

    eslint appeasement

--- x-pack/legacy/plugins/maps/public/connected_components/map/mb/view.js
+++ x-pack/legacy/plugins/maps/public/connected_components/map/mb/view.js
@@ -46,9 +46,6 @@ const TOOLTIP_TYPE = {
   LOCKED: 'LOCKED'
 };
 
-// eslint-disable-next-line max-len,camelcase
-const TRANSPARENT_1x1_BASE64_URI = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQYV2NgAAIAAAUAAarVyFEAAAAASUVORK5CYII=';
-
 export class MBMapContainer extends React.Component {
 
   state = {
@@ -397,7 +394,8 @@ export class MBMapContainer extends React.Component {
       });
       mbMap.on('load', () => {
         emptyImage = new Image();
-        emptyImage.src = TRANSPARENT_1x1_BASE64_URI;
+        // eslint-disable-next-line max-len
+        emptyImage.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQYV2NgAAIAAAUAAarVyFEAAAAASUVORK5CYII=';
         emptyImage.crossOrigin = 'anonymous';
         resolve(mbMap);
       });
