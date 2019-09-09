f325fceb8347f5553a64b9c0db2c06a34a67a8ea
Kerry Gallagher <k.gallagher.05@gmail.com>
2019-09-09 12:33:30 +0100

    Remove now unused fragments

--- x-pack/legacy/plugins/infra/public/pages/logs/analysis/setup/steps/setup_process.tsx
+++ x-pack/legacy/plugins/infra/public/pages/logs/analysis/setup/steps/setup_process.tsx
@@ -32,51 +32,47 @@ export const SetupProcess: React.FunctionComponent<Props> = ({
   indexPattern,
 }: Props) => {
   return (
-    <>
-      <StepText>
-        {isSettingUp || isRetrying ? (
-          <EuiFlexGroup alignItems="center">
-            <EuiFlexItem grow={false}>
-              <EuiLoadingSpinner size="xl" />
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <FormattedMessage
-                id="xpack.infra.analysisSetup.steps.setupProcess.loadingText"
-                defaultMessage="Creating ML jobs..."
-              />
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        ) : didSetupFail ? (
-          <>
+    <StepText>
+      {isSettingUp || isRetrying ? (
+        <EuiFlexGroup alignItems="center">
+          <EuiFlexItem grow={false}>
+            <EuiLoadingSpinner size="xl" />
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
             <FormattedMessage
-              id="xpack.infra.analysisSetup.steps.setupProcess.failureText"
-              defaultMessage="Something went wrong creating the necessary Machine Learning jobs.
-              Please ensure your configured logs indices ({indexPattern}) exist."
-              values={{
-                indexPattern,
-              }}
+              id="xpack.infra.analysisSetup.steps.setupProcess.loadingText"
+              defaultMessage="Creating ML jobs..."
             />
-            <EuiSpacer />
-            <EuiButton fill onClick={retry} disabled={isRetrying}>
-              <FormattedMessage
-                id="xpack.infra.analysisSetup.steps.setupProcess.tryAgainButton"
-                defaultMessage="Try again"
-              />
-            </EuiButton>
-          </>
-        ) : hasCompletedSetup ? (
-          <EuiButton fill onClick={viewResults}>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      ) : didSetupFail ? (
+        <>
+          <FormattedMessage
+            id="xpack.infra.analysisSetup.steps.setupProcess.failureText"
+            defaultMessage="Something went wrong creating the necessary Machine Learning jobs.
+            Please ensure your configured logs indices ({indexPattern}) exist."
+            values={{
+              indexPattern,
+            }}
+          />
+          <EuiSpacer />
+          <EuiButton fill onClick={retry} disabled={isRetrying}>
             <FormattedMessage
-              id="xpack.infra.analysisSetup.steps.setupProcess.viewResultsButton"
-              defaultMessage="View results"
+              id="xpack.infra.analysisSetup.steps.setupProcess.tryAgainButton"
+              defaultMessage="Try again"
             />
           </EuiButton>
-        ) : (
-          <>
-            <CreateMLJobsButton isDisabled={isSettingUp} onClick={setup} />
-          </>
-        )}
-      </StepText>
-    </>
+        </>
+      ) : hasCompletedSetup ? (
+        <EuiButton fill onClick={viewResults}>
+          <FormattedMessage
+            id="xpack.infra.analysisSetup.steps.setupProcess.viewResultsButton"
+            defaultMessage="View results"
+          />
+        </EuiButton>
+      ) : (
+        <CreateMLJobsButton isDisabled={isSettingUp} onClick={setup} />
+      )}
+    </StepText>
   );
 };
