014f072078a1de3afa1013a39ef1a78024065776
Larry Gregory <lgregorydev@gmail.com>
2019-09-09 10:34:59 -0400

    Revert "Export saved objects based on search criteria (#44723) (#45004)"
    This reverts commit 3eb888bd8a2697616df42ac4b92c2203eb6207a6.

--- src/legacy/core_plugins/kibana/public/management/sections/objects/lib/fetch_export_by_type.js
+++ src/legacy/core_plugins/kibana/public/management/sections/objects/lib/fetch_export_by_type.js
@@ -17,18 +17,15 @@
  * under the License.
  */
 
-export * from './fetch_export_by_type';
-export * from './fetch_export_objects';
-export * from './in_app_url';
-export * from './get_relationships';
-export * from './get_saved_object_counts';
-export * from './get_saved_object_label';
-export * from './import_file';
-export * from './import_legacy_file';
-export * from './parse_query';
-export * from './resolve_import_errors';
-export * from './resolve_saved_objects';
-export * from './log_legacy_import';
-export * from './process_import_response';
-export * from './get_default_title';
-export * from './find_objects';
+import { kfetch } from 'ui/kfetch';
+
+export async function fetchExportByType(types, includeReferencesDeep = false) {
+  return await kfetch({
+    method: 'POST',
+    pathname: '/api/saved_objects/_export',
+    body: JSON.stringify({
+      type: types,
+      includeReferencesDeep,
+    }),
+  });
+}
