672edc8af0c7264e24498ef7500ce482d3d9b138
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2019-09-09 13:11:50 +0200

    Add directory for api integration tests.

---
+++ x-pack/test/api_integration/apis/integrations_manager/list.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function({ getService }: FtrProviderContext) {
+  describe('list', () => {
+    it('lists all packages from the registry', () => {
+      expect(true).to.be(false);
+    });
+  });
+}
