3798674043f83be34540f8af155905bad5736deb
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2019-09-10 16:47:28 +0200

    De-angularize and EUI-ficate Discover Context control elements (#44474)
    * Create ActionBar component, merges loading button and size picker
    
    * Use react component, remove angular code
    
    * Migrate constants + state to typescript
    
    * Remove unused increaseCount functions + tests
    
    * Add jest test

--- src/legacy/core_plugins/kibana/public/context/components/action_bar/index.ts
+++ src/legacy/core_plugins/kibana/public/context/components/action_bar/index.ts
@@ -17,9 +17,4 @@
  * under the License.
  */
 
-import { createInitialQueryParametersState } from './state';
-
-
-export const MAX_CONTEXT_SIZE = 10000; // Elasticsearch's default maximum size limit
-export const MIN_CONTEXT_SIZE = 0;
-export const QUERY_PARAMETER_KEYS = Object.keys(createInitialQueryParametersState());
+import './action_bar_directive';
