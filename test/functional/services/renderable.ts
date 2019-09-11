5c3cd1bdd7ad6e1633c45e3756628cf1c910e26b
Dmitry Lemeshko <dzmitry.lemechko@elastic.co>
2019-09-11 20:30:44 +0200

    Functional tests: convert more test/services to TS (#45176) (#45385)
    * convert more test/services to TS
    
    * Update test/functional/services/combo_box.ts
    
    Co-Authored-By: Tre' <wayne.seymour@elastic.co>
    
    * Update test/functional/services/combo_box.ts
    
    Co-Authored-By: Tre' <wayne.seymour@elastic.co>
    
    * Update test/functional/services/combo_box.ts
    
    Co-Authored-By: Tre' <wayne.seymour@elastic.co>
    
    * fix lint error

--- test/functional/services/renderable.ts
+++ test/functional/services/renderable.ts
@@ -17,17 +17,18 @@
  * under the License.
  */
 
+import { FtrProviderContext } from '../ftr_provider_context';
+
 const RENDER_COMPLETE_SELECTOR = '[data-render-complete="true"]';
 const RENDER_COMPLETE_PENDING_SELECTOR = '[data-render-complete="false"]';
 const DATA_LOADING_SELECTOR = '[data-loading]';
 
-export function RenderableProvider({ getService }) {
+export function RenderableProvider({ getService }: FtrProviderContext) {
   const log = getService('log');
   const retry = getService('retry');
   const find = getService('find');
 
-  return new class Renderable {
-
+  class Renderable {
     /**
      * This method waits for a certain number of objects to finish rendering and loading, which is indicated
      * by a couple tags. The RENDER_COMPLETE_SELECTOR indicates that it's done initially loading up. Some
@@ -35,7 +36,7 @@ export function RenderableProvider({ getService }) {
      * return if any of those tags are found.
      * @param count {Number} Number of RENDER_COMPLETE_SELECTORs to wait for.
      */
-    async waitForRender(count = 1) {
+    public async waitForRender(count: number = 1): Promise<void> {
       log.debug(`Renderable.waitForRender for ${count} elements`);
       await retry.try(async () => {
         const completedElements = await find.allByCssSelector(RENDER_COMPLETE_SELECTOR);
@@ -46,8 +47,10 @@ export function RenderableProvider({ getService }) {
             const title = await pendingElement.getAttribute('data-title');
             pendingElementNames.push(title);
           }
-          throw new Error(`${completedElements.length} elements completed rendering, still waiting on a total of ${count}
-            specifically:\n${pendingElementNames.join('\n')}`);
+          throw new Error(`${
+            completedElements.length
+          } elements completed rendering, still waiting on a total of ${count}
+                specifically:\n${pendingElementNames.join('\n')}`);
         }
 
         const stillLoadingElements = await find.allByCssSelector(DATA_LOADING_SELECTOR, 1000);
@@ -56,5 +59,7 @@ export function RenderableProvider({ getService }) {
         }
       });
     }
-  };
+  }
+
+  return new Renderable();
 }
