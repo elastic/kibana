9cd2f6e0b35c9781e7cbdff42db4cfb6561df231
Robert Oskamp <robert.oskamp@elastic.co>
2019-09-11 16:57:23 +0200

    [ML] Add multi metric job wizard test (#45279)
    This PR adds functional UI tests to create a machine learning job using the multi metric wizard.

--- x-pack/test/functional/apps/machine_learning/create_multi_metric_job.ts
+++ x-pack/test/functional/apps/machine_learning/create_multi_metric_job.ts
@@ -12,15 +12,16 @@ export default function({ getService }: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
   const ml = getService('ml');
 
-  const jobId = `fq_single_1_${Date.now()}`;
+  const jobId = `fq_multi_1_${Date.now()}`;
   const jobDescription =
-    'Create single metric job based on the farequote dataset with 30m bucketspan and mean(responsetime)';
-  const jobGroups = ['automated', 'farequote', 'single-metric'];
-  const aggAndFieldIdentifier = 'Mean(responsetime)';
-  const bucketSpan = '30m';
-  const memoryLimit = '15MB';
+    'Create multi metric job based on the farequote dataset with 15m bucketspan and min/max/mean(responsetime) split by airline';
+  const jobGroups = ['automated', 'farequote', 'multi-metric'];
+  const aggAndFieldIdentifiers = ['Min(responsetime)', 'Max(responsetime)', 'Mean(responsetime)'];
+  const splitField = 'airline';
+  const bucketSpan = '15m';
+  const memoryLimit = '20MB';
 
-  describe('single metric job creation', function() {
+  describe('multi metric job creation', function() {
     this.tags(['smoke', 'mlqa']);
     before(async () => {
       await esArchiver.loadIfNeeded('ml/farequote');
@@ -46,7 +47,7 @@ export default function({ getService }: FtrProviderContext) {
     });
 
     it('loads the single metric job wizard page', async () => {
-      await ml.jobTypeSelection.selectSingleMetricJob();
+      await ml.jobTypeSelection.selectMultiMetricJob();
     });
 
     it('displays the time range step', async () => {
@@ -63,10 +64,29 @@ export default function({ getService }: FtrProviderContext) {
       await ml.jobWizardCommon.assertPickFieldsSectionExists();
     });
 
-    it('selects field and aggregation', async () => {
-      await ml.jobWizardCommon.assertAggAndFieldInputExists();
-      await ml.jobWizardCommon.selectAggAndField(aggAndFieldIdentifier);
-      await ml.jobWizardCommon.assertAggAndFieldSelection(aggAndFieldIdentifier);
+    it('selects detectors and displays detector previews', async () => {
+      for (const [index, aggAndFieldIdentifier] of aggAndFieldIdentifiers.entries()) {
+        await ml.jobWizardCommon.assertAggAndFieldInputExists();
+        await ml.jobWizardCommon.selectAggAndField(aggAndFieldIdentifier);
+        await ml.jobWizardCommon.assertDetectorPreviewExists(aggAndFieldIdentifier, index, 'LINE');
+      }
+    });
+
+    it('inputs the split field and displays split cards', async () => {
+      await ml.jobWizardCommon.assertMultiMetricSplitFieldInputExists();
+      await ml.jobWizardCommon.selectMultiMetricSplitField(splitField);
+      await ml.jobWizardCommon.assertMultiMetricSplitFieldSelection(splitField);
+
+      await ml.jobWizardCommon.assertDetectorSplitExists(splitField);
+      await ml.jobWizardCommon.assertDetectorSplitFrontCardTitle('AAL');
+      await ml.jobWizardCommon.assertDetectorSplitNumberOfBackCards(9);
+
+      await ml.jobWizardCommon.assertInfluencerSelection([splitField]);
+    });
+
+    it('displays the influencer field', async () => {
+      await ml.jobWizardCommon.assertInfluencerInputExists();
+      await ml.jobWizardCommon.assertInfluencerSelection([splitField]);
     });
 
     it('inputs the bucket span', async () => {
@@ -150,43 +170,43 @@ export default function({ getService }: FtrProviderContext) {
         id: jobId,
         description: jobDescription,
         jobGroups,
-        recordCount: '2,399',
+        recordCount: '86,274',
         memoryStatus: 'ok',
         jobState: 'closed',
         datafeedState: 'stopped',
-        latestTimestamp: '2016-02-11 23:56:59',
+        latestTimestamp: '2016-02-11 23:59:54',
       };
       await ml.jobTable.assertJobRowFields(jobId, expectedRow);
 
       const expectedCounts = {
         job_id: jobId,
-        processed_record_count: '2,399',
-        processed_field_count: '4,798',
-        input_bytes: '180.6 KB',
-        input_field_count: '4,798',
+        processed_record_count: '86,274',
+        processed_field_count: '172,548',
+        input_bytes: '6.4 MB',
+        input_field_count: '172,548',
         invalid_date_count: '0',
         missing_field_count: '0',
         out_of_order_timestamp_count: '0',
         empty_bucket_count: '0',
         sparse_bucket_count: '0',
-        bucket_count: '239',
-        earliest_record_timestamp: '2016-02-07 00:02:50',
-        latest_record_timestamp: '2016-02-11 23:56:59',
-        input_record_count: '2,399',
-        latest_bucket_timestamp: '2016-02-11 23:30:00',
+        bucket_count: '479',
+        earliest_record_timestamp: '2016-02-07 00:00:00',
+        latest_record_timestamp: '2016-02-11 23:59:54',
+        input_record_count: '86,274',
+        latest_bucket_timestamp: '2016-02-11 23:45:00',
       };
       const expectedModelSizeStats = {
         job_id: jobId,
         result_type: 'model_size_stats',
-        model_bytes: '47.6 KB',
+        model_bytes: '1.8 MB',
         model_bytes_exceeded: '0',
-        model_bytes_memory_limit: '15728640',
-        total_by_field_count: '3',
+        model_bytes_memory_limit: '20971520',
+        total_by_field_count: '59',
         total_over_field_count: '0',
-        total_partition_field_count: '2',
+        total_partition_field_count: '58',
         bucket_allocation_failures_count: '0',
         memory_status: 'ok',
-        timestamp: '2016-02-11 23:00:00',
+        timestamp: '2016-02-11 23:30:00',
       };
       await ml.jobTable.assertJobRowDetailsCounts(jobId, expectedCounts, expectedModelSizeStats);
     });
