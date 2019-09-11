c67edc55c8553f0ddd77423b21cbf2f247b83db7
art.gospodarsky <art.gospodarsky@gmail.com>
2019-09-11 15:11:45 +0300

    Convert getHighlightRequest() to TS

--- src/legacy/core_plugins/kibana/common/highlight/highlight_request.ts
+++ src/legacy/core_plugins/kibana/common/highlight/highlight_request.ts
@@ -21,15 +21,15 @@ import { highlightTags } from './highlight_tags';
 
 const FRAGMENT_SIZE = Math.pow(2, 31) - 1; // Max allowed value for fragment_size (limit of a java int)
 
-export function getHighlightRequest(query, getConfig) {
+export function getHighlightRequest(query: any, getConfig: Function) {
   if (!getConfig('doc_table:highlight')) return;
 
   return {
     pre_tags: [highlightTags.pre],
     post_tags: [highlightTags.post],
     fields: {
-      '*': {}
+      '*': {},
     },
-    fragment_size: FRAGMENT_SIZE
+    fragment_size: FRAGMENT_SIZE,
   };
 }
