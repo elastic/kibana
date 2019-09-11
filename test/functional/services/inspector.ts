d4c28ecef67a57f90ef24cbf4aebf2f21ccdea55
Dmitry Lemeshko <dzmitry.lemechko@elastic.co>
2019-09-11 20:31:28 +0200

    Functional tests: convert more test/services to TS (#45176) (#45396)
    * convert more test/services to TS
    
    * Update test/functional/services/combo_box.ts
    
    Co-Authored-By: Tre' <wayne.seymour@elastic.co>
    
    * Update test/functional/services/combo_box.ts
    
    Co-Authored-By: Tre' <wayne.seymour@elastic.co>
    
    * Update test/functional/services/combo_box.ts
    
    Co-Authored-By: Tre' <wayne.seymour@elastic.co>
    
    * fix lint error

--- test/functional/services/inspector.ts
+++ test/functional/services/inspector.ts
@@ -18,8 +18,9 @@
  */
 
 import expect from '@kbn/expect';
+import { FtrProviderContext } from '../ftr_provider_context';
 
-export function InspectorProvider({ getService }) {
+export function InspectorProvider({ getService }: FtrProviderContext) {
   const log = getService('log');
   const retry = getService('retry');
   const renderable = getService('renderable');
@@ -27,27 +28,36 @@ export function InspectorProvider({ getService }) {
   const testSubjects = getService('testSubjects');
   const find = getService('find');
 
-  return new class Inspector {
-    async getIsEnabled() {
+  class Inspector {
+    private async getIsEnabled(): Promise<boolean> {
       const ariaDisabled = await testSubjects.getAttribute('openInspectorButton', 'aria-disabled');
       return ariaDisabled !== 'true';
     }
 
-    async expectIsEnabled() {
+    /**
+     * Asserts that inspector is enabled
+     */
+    public async expectIsEnabled(): Promise<void> {
       await retry.try(async () => {
         const isEnabled = await this.getIsEnabled();
         expect(isEnabled).to.be(true);
       });
     }
 
-    async expectIsNotEnabled() {
+    /**
+     * Asserts that inspector is disabled
+     */
+    public async expectIsNotEnabled(): Promise<void> {
       await retry.try(async () => {
         const isEnabled = await this.getIsEnabled();
         expect(isEnabled).to.be(false);
       });
     }
 
-    async open() {
+    /**
+     * Opens inspector panel
+     */
+    public async open(): Promise<void> {
       log.debug('Inspector.open');
       const isOpen = await testSubjects.exists('inspectorPanel');
       if (!isOpen) {
@@ -58,7 +68,10 @@ export function InspectorProvider({ getService }) {
       }
     }
 
-    async close() {
+    /**
+     * Closes inspector panel
+     */
+    public async close(): Promise<void> {
       log.debug('Close Inspector');
       let isOpen = await testSubjects.exists('inspectorPanel');
       if (isOpen) {
@@ -72,13 +85,21 @@ export function InspectorProvider({ getService }) {
       }
     }
 
-    async expectTableData(expectedData) {
+    /**
+     * Asserts data on inspector panel
+     * @param expectedData
+     */
+    public async expectTableData(expectedData: string[][]): Promise<void> {
       await log.debug(`Inspector.expectTableData(${expectedData.join(',')})`);
       const data = await this.getTableData();
       expect(data).to.eql(expectedData);
     }
 
-    async setTablePageSize(size) {
+    /**
+     * Sets table page size
+     * @param size rows count
+     */
+    public async setTablePageSize(size: number): Promise<void> {
       const panel = await testSubjects.find('inspectorPanel');
       await find.clickByButtonText('Rows per page: 20', panel);
       // The buttons for setting table page size are in a popover element. This popover
@@ -88,27 +109,43 @@ export function InspectorProvider({ getService }) {
       await find.clickByButtonText(`${size} rows`, tableSizesPopover);
     }
 
-    async getTableData() {
+    /**
+     * Returns table data in nested array format
+     */
+    public async getTableData(): Promise<string[][]> {
       // TODO: we should use datat-test-subj=inspectorTable as soon as EUI supports it
       const inspectorPanel = await testSubjects.find('inspectorPanel');
       const tableBody = await retry.try(async () => inspectorPanel.findByTagName('tbody'));
       const $ = await tableBody.parseDomContent();
-      return $('tr').toArray().map(tr => {
-        return $(tr).find('td').toArray().map(cell => {
-          // if this is an EUI table, filter down to the specific cell content
-          // otherwise this will include mobile-specific header information
-          const euiTableCellContent = $(cell).find('.euiTableCellContent');
+      return $('tr')
+        .toArray()
+        .map(tr => {
+          return $(tr)
+            .find('td')
+            .toArray()
+            .map(cell => {
+              // if this is an EUI table, filter down to the specific cell content
+              // otherwise this will include mobile-specific header information
+              const euiTableCellContent = $(cell).find('.euiTableCellContent');
 
-          if (euiTableCellContent.length > 0) {
-            return $(cell).find('.euiTableCellContent').text().trim();
-          } else {
-            return $(cell).text().trim();
-          }
+              if (euiTableCellContent.length > 0) {
+                return $(cell)
+                  .find('.euiTableCellContent')
+                  .text()
+                  .trim();
+              } else {
+                return $(cell)
+                  .text()
+                  .trim();
+              }
+            });
         });
-      });
     }
 
-    async getTableHeaders() {
+    /**
+     * Returns table headers
+     */
+    public async getTableHeaders(): Promise<string[]> {
       log.debug('Inspector.getTableHeaders');
       // TODO: we should use datat-test-subj=inspectorTable as soon as EUI supports it
       const dataTableHeader = await retry.try(async () => {
@@ -116,21 +153,37 @@ export function InspectorProvider({ getService }) {
         return await inspectorPanel.findByTagName('thead');
       });
       const $ = await dataTableHeader.parseDomContent();
-      return $('th span.euiTableCellContent__text').toArray()
-        .map(cell => $(cell).text().trim());
+      return $('th span.euiTableCellContent__text')
+        .toArray()
+        .map(cell =>
+          $(cell)
+            .text()
+            .trim()
+        );
     }
 
-    async expectTableHeaders(expected) {
+    /**
+     * Asserts table headers
+     * @param expected expected headers
+     */
+    public async expectTableHeaders(expected: string[]): Promise<void> {
       await retry.try(async () => {
         const headers = await this.getTableHeaders();
         expect(headers).to.eql(expected);
       });
     }
 
-    async filterForTableCell(column, row) {
+    /**
+     * Filters table for value by clicking specified cell
+     * @param column column index
+     * @param row row index
+     */
+    public async filterForTableCell(column: string, row: string): Promise<void> {
       await retry.try(async () => {
         const table = await testSubjects.find('inspectorTable');
-        const cell = await table.findByCssSelector(`tbody tr:nth-child(${row}) td:nth-child(${column})`);
+        const cell = await table.findByCssSelector(
+          `tbody tr:nth-child(${row}) td:nth-child(${column})`
+        );
         await cell.moveMouseTo();
         const filterBtn = await testSubjects.findDescendant('filterForInspectorCellValue', cell);
         await filterBtn.click();
@@ -138,10 +191,17 @@ export function InspectorProvider({ getService }) {
       await renderable.waitForRender();
     }
 
-    async filterOutTableCell(column, row) {
+    /**
+     * Filters out table by clicking specified cell
+     * @param column column index
+     * @param row row index
+     */
+    public async filterOutTableCell(column: string, row: string): Promise<void> {
       await retry.try(async () => {
         const table = await testSubjects.find('inspectorTable');
-        const cell = await table.findByCssSelector(`tbody tr:nth-child(${row}) td:nth-child(${column})`);
+        const cell = await table.findByCssSelector(
+          `tbody tr:nth-child(${row}) td:nth-child(${column})`
+        );
         await cell.moveMouseTo();
         const filterBtn = await testSubjects.findDescendant('filterOutInspectorCellValue', cell);
         await filterBtn.click();
@@ -149,28 +209,43 @@ export function InspectorProvider({ getService }) {
       await renderable.waitForRender();
     }
 
-    async openInspectorView(viewId) {
+    /**
+     * Opens inspector view
+     * @param viewId
+     */
+    public async openInspectorView(viewId: string): Promise<void> {
       log.debug(`Open Inspector view ${viewId}`);
       await testSubjects.click('inspectorViewChooser');
       await testSubjects.click(viewId);
     }
 
-    async openInspectorRequestsView() {
+    /**
+     * Opens inspector requests view
+     */
+    public async openInspectorRequestsView(): Promise<void> {
       await this.openInspectorView('inspectorViewChooserRequests');
     }
 
-    async getRequestNames() {
+    /**
+     * Returns request name as the comma-separated string
+     */
+    public async getRequestNames(): Promise<string> {
       await this.openInspectorRequestsView();
       const requestChooserExists = await testSubjects.exists('inspectorRequestChooser');
       if (requestChooserExists) {
         await testSubjects.click('inspectorRequestChooser');
         const menu = await testSubjects.find('inspectorRequestChooserMenuPanel');
         const requestNames = await menu.getVisibleText();
-        return requestNames.trim().split('\n').join(',');
+        return requestNames
+          .trim()
+          .split('\n')
+          .join(',');
       }
 
       const singleRequest = await testSubjects.find('inspectorRequestName');
       return await singleRequest.getVisibleText();
     }
-  };
+  }
+
+  return new Inspector();
 }
