e26cc3de7e3af74b72234e04f38265aee09a3c90
Mengwei Ding <mengwei.ding@gmail.com>
2019-09-12 14:22:30 -0700

    frontend metric code

---
+++ x-pack/legacy/plugins/code/public/lib/usage_collector/index.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+// @ts-ignore
+import {
+  createUiStatsReporter,
+  METRIC_TYPE,
+} from '../../../../../../../src/legacy/core_plugins/ui_metric/public';
+import { APP_USAGE_TYPE } from '../../../common/constants';
+
+export const trackUiAction = createUiStatsReporter(APP_USAGE_TYPE);
+export { METRIC_TYPE };
