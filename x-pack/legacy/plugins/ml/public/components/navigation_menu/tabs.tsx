a90916dfab4a5fd8033912f5e563391c7f75bee9
Walter Rafelsberger <walter@elastic.co>
2019-09-13 12:40:35 +0200

    [ML] Review feedback: Add main tab ids back in.

--- x-pack/legacy/plugins/ml/public/components/navigation_menu/tabs.tsx
+++ x-pack/legacy/plugins/ml/public/components/navigation_menu/tabs.tsx
@@ -19,6 +19,10 @@ interface Props {
 
 export function getTabs(tabId: TabId, disableLinks: boolean): Tab[] {
   const TAB_MAP: Partial<Record<TabId, Tab[]>> = {
+    // overview: [],
+    datavisualizer: [],
+    data_frames: [],
+    data_frame_analytics: [],
     anomaly_detection: [
       {
         id: 'jobs',
