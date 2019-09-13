19efdadbe74ac4bfdbb68c1adeed44f51772dec1
Lee Drengenberg <lee.drengenberg@elastic.co>
2019-09-13 15:34:35 -0500

    [Reporting] skip flaky reporting tests (#45659)
    * Add option for Internet Explorer IE11
    
    * WIP trying to figure out IE options/capabilities
    
    * more attempts to get IE tests working
    
    * more work, but still can't set font size in Dev Tools Console
    
    * two changes for IE
    
    * revert skip in console test
    
    * this change *appears* to set absolute times but they don't stick
    
    * Shakespeare passing on all 3 browsers
    
    * add config.ie.js, change 'internet explorer' to 'ie'
    
    * increase notifications:lifetime so ew have a chance to see the message
    
    * correct path with this missing slash
    
    * skip flaky tests issues #45499 #42725
    
    * revert these files to master
    
    * revert to master
    
    (cherry picked from commit d9cd4e614efb6510a8a48dd773e0aef0e0d1bcac)

--- x-pack/test/reporting/functional/reporting.js
+++ x-pack/test/reporting/functional/reporting.js
@@ -188,7 +188,8 @@ export default function ({ getService, getPageObjects }) {
         });
       });
 
-      describe('Print PNG button', () => {
+      // FLAKY: https://github.com/elastic/kibana/issues/43131
+      describe.skip('Print PNG button', () => {
         it('is not available if new', async () => {
           await PageObjects.common.navigateToApp('dashboard');
           await PageObjects.dashboard.clickNewDashboard();
