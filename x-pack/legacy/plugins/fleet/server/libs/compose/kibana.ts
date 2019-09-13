66aa3f95801891e3413102457e9759adc57139d4
Nicolas Chaulet <n.chaulet@gmail.com>
2019-09-13 08:18:37 -0400

    [Ingest] Add policy to the fleet checkin API (#45374)

--- x-pack/legacy/plugins/fleet/server/libs/compose/kibana.ts
+++ x-pack/legacy/plugins/fleet/server/libs/compose/kibana.ts
@@ -12,6 +12,8 @@ import { AgentAdapter } from '../adapters/agent/default';
 import { SODatabaseAdapter } from '../adapters/saved_objets_database/default';
 import { TokenAdapter } from '../adapters/tokens/default';
 import { FrameworkAdapter } from '../adapters/framework/default';
+import { PolicyLib } from '../policy';
+import { InMemoryPolicyAdapter } from '../adapters/policy/in_memory';
 
 export function compose(server: any): FleetServerLib {
   const soDatabaseAdapter = new SODatabaseAdapter(
@@ -21,14 +23,18 @@ export function compose(server: any): FleetServerLib {
   const agentAdapter = new AgentAdapter(soDatabaseAdapter);
   const tokenAdapter = new TokenAdapter(soDatabaseAdapter);
   const frameworkAdapter = new FrameworkAdapter(server);
+  // TODO replace with real adapter when ingest plugin exists
+  const policyAdapter = new InMemoryPolicyAdapter();
 
   const framework = new FrameworkLib(frameworkAdapter);
 
+  const policies = new PolicyLib(policyAdapter);
   const tokens = new TokenLib(tokenAdapter, framework);
-  const agents = new AgentLib(agentAdapter, tokens);
+  const agents = new AgentLib(agentAdapter, tokens, policies);
 
   return {
     agents,
     tokens,
+    policies,
   };
 }
