f5e91da99cf4c430e8ff63d92742645fbf7d9cec
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2019-09-13 07:22:27 -0400

    [SIEM] Fix paginated table state when switching tab (#45487)
    * Keep state of host/netwrok table when switching tab
    
    * keep state in the event viewer when switching tab in host page
    
    * refactor test to ensure activePage updates from prop change
    
    * rm comments
    
    * add/fix unit test
    
    * fix i18n
    
    * need to rm weird files
    
    * rm weird files
    
    * fix inspect button for real + loading view between tabs
    
    * cleanups
    
    * commented by mistake
    
    * fix logic loading
    
    * review I
    
    * review II
    
    * review III
    
    * review IV

--- x-pack/legacy/plugins/siem/public/pages/hosts/details/body.tsx
+++ x-pack/legacy/plugins/siem/public/pages/hosts/details/body.tsx
@@ -19,6 +19,7 @@ import { getFilterQuery, type, makeMapStateToProps } from './utils';
 const HostDetailsBodyComponent = React.memo<HostDetailsBodyComponentProps>(
   ({
     children,
+    deleteQuery,
     filterQueryExpression,
     from,
     isInitializing,
@@ -33,6 +34,7 @@ const HostDetailsBodyComponent = React.memo<HostDetailsBodyComponentProps>(
           indicesExistOrDataTemporarilyUnavailable(indicesExist) ? (
             <>
               {children({
+                deleteQuery,
                 endDate: to,
                 filterQuery: getFilterQuery(detailName, filterQueryExpression, indexPattern),
                 kqlQueryExpression: getHostDetailsEventsKqlQueryExpression({
