df114485a8cf0d6f5ffee1b350fbf9ee4cca2262
sainthkh <sainthkh@naver.com>
2019-09-14 11:28:52 +0900

    Prettierize params.js

--- test/api_integration/apis/index_patterns/fields_for_wildcard_route/params.js
+++ test/api_integration/apis/index_patterns/fields_for_wildcard_route/params.js
@@ -26,41 +26,37 @@ export default function ({ getService }) {
     before(() => esArchiver.load('index_patterns/basic_index'));
     after(() => esArchiver.unload('index_patterns/basic_index'));
 
-    it('requires a pattern query param', () => (
+    it('requires a pattern query param', () =>
       supertest
         .get('/api/index_patterns/_fields_for_wildcard')
         .query({})
-        .expect(400)
-    ));
+        .expect(400));
 
-    it('accepts a JSON formatted meta_fields query param', () => (
+    it('accepts a JSON formatted meta_fields query param', () =>
       supertest
         .get('/api/index_patterns/_fields_for_wildcard')
         .query({
           pattern: '*',
-          meta_fields: JSON.stringify(['meta'])
+          meta_fields: JSON.stringify(['meta']),
         })
-        .expect(200)
-    ));
+        .expect(200));
 
-    it('rejects a comma-separated list of meta_fields', () => (
+    it('rejects a comma-separated list of meta_fields', () =>
       supertest
         .get('/api/index_patterns/_fields_for_wildcard')
         .query({
           pattern: '*',
-          meta_fields: 'foo,bar'
+          meta_fields: 'foo,bar',
         })
-        .expect(400)
-    ));
+        .expect(400));
 
-    it('rejects unexpected query params', () => (
+    it('rejects unexpected query params', () =>
       supertest
         .get('/api/index_patterns/_fields_for_wildcard')
         .query({
           pattern: chance.word(),
           [chance.word()]: chance.word(),
         })
-        .expect(400)
-    ));
+        .expect(400));
   });
 }
