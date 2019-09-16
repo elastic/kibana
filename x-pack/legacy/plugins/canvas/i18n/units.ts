8bb1efab7ccc0cb89b3d21a1afc7683811c35326
Poff Poffenberger <poffdeluxe@gmail.com>
2019-09-16 10:32:13 -0500

    Changing plural format

--- x-pack/legacy/plugins/canvas/i18n/units.ts
+++ x-pack/legacy/plugins/canvas/i18n/units.ts
@@ -9,17 +9,17 @@ export const UnitStrings = {
   time: {
     getSecondsText: (seconds: number) =>
       i18n.translate('xpack.canvas.units.time.seconds', {
-        defaultMessage: '{seconds} {seconds, plural, one {second} other {seconds}}',
+        defaultMessage: '{seconds, plural, one {# second} other {# seconds}',
         values: { seconds },
       }),
     getMinutesText: (minutes: number) =>
       i18n.translate('xpack.canvas.units.time.minutes', {
-        defaultMessage: '{minutes} {minutes, plural, one {minute} other {minutes}}',
+        defaultMessage: '{minutes, plural, one {# minute} other {# minutes}',
         values: { minutes },
       }),
     getHoursText: (hours: number) =>
       i18n.translate('xpack.canvas.units.time.hours', {
-        defaultMessage: '{hours} {hours, plural, one {hour} other {hours}}',
+        defaultMessage: '{hours, plural, one {# hour} other {# hours}',
         values: { hours },
       }),
     getDaysText: (days: number) =>
