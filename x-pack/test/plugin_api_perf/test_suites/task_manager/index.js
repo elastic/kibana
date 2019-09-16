413399ce7a3e28c6bfdb91e42b7d2b2f5bf25005
Gidi Morris <github@gidi.io>
2019-09-16 12:54:29 +0100

    feat(task-manager-perf): Intorduced task manager stress test

---
+++ x-pack/test/plugin_api_perf/test_suites/task_manager/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export default function ({ loadTestFile }) {
+  describe.skip('task_manager_perf', function taskManagerSuite() {
+    this.tags('ciGroup2');
+    loadTestFile(require.resolve('./task_manager_perf_integration'));
+  });
+}
---
+++ x-pack/test/plugin_api_perf/test_suites/task_manager/task_manager_perf_integration.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from '@kbn/expect';
+import url from 'url';
+import supertestAsPromised from 'supertest-as-promised';
+
+export default function ({ getService }) {
+  const log = getService('log');
+  const config = getService('config');
+  const supertest = supertestAsPromised(url.format(config.get('servers.kibana')));
+
+  describe('stressing task manager', () => {
+    beforeEach(() =>
+      supertest
+        .delete('/api/sample_tasks')
+        .set('kbn-xsrf', 'xxx')
+        .expect(200)
+    );
+
+    it('should run 10 tasks every second for a minute', async () => {
+      const stressTestResult = await supertest
+        .post('/api/perf_tasks')
+        .set('kbn-xsrf', 'xxx')
+        .send({ tasksToSpawn: 10, durationInSeconds: 60 })
+        .expect(200)
+        .then(response => response.body);
+
+      log.debug(`Stress Test Result: ${JSON.stringify(stressTestResult)}`);
+
+      expect(stressTestResult.runningAverageTasks).to.be.greaterThan(0);
+      expect(stressTestResult.runningAverageLeadTime).to.be.greaterThan(0);
+    });
+  });
+}
