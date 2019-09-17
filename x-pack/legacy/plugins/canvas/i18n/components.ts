de27409ba88cf4e1e91509b236cd581a0461b3bd
Poff Poffenberger <poffdeluxe@gmail.com>
2019-09-17 15:44:41 -0500

    App i18n

--- x-pack/legacy/plugins/canvas/i18n/components.ts
+++ x-pack/legacy/plugins/canvas/i18n/components.ts
@@ -8,6 +8,23 @@ import { i18n } from '@kbn/i18n';
 import { CANVAS, JSON, KIBANA, PDF, POST, URL } from './constants';
 
 export const ComponentStrings = {
+  App: {
+    getLoadErrorTitle: () =>
+      i18n.translate('xpack.canvas.app.loadErrorTitle', {
+        defaultMessage: 'Canvas failed to load :(',
+      }),
+    getLoadErrorMessage: (error: string) =>
+      i18n.translate('xpack.canvas.app.loadErrorMessage', {
+        defaultMessage: 'Message: {error}',
+        values: {
+          error,
+        },
+      }),
+    getLoadingMessage: () =>
+      i18n.translate('xpack.canvas.app.loadingMessage', {
+        defaultMessage: 'Canvas is loading',
+      }),
+  },
   AddEmbeddableFlyout: {
     getNoItemsText: () =>
       i18n.translate('xpack.canvas.embedObject.noMatchingObjectsMessage', {
