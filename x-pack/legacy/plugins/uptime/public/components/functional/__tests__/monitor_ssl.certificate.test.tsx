53e2400eacabb0635ac2b3b04a3d7f7928e03e09
shahzad <shahzad31comp@gmail.com>
2019-09-17 15:19:38 +0500

    updat test snaps

--- x-pack/legacy/plugins/uptime/public/components/functional/__tests__/monitor_ssl.certificate.test.tsx
+++ x-pack/legacy/plugins/uptime/public/components/functional/__tests__/monitor_ssl.certificate.test.tsx
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import moment from 'moment';
 import { renderWithIntl } from 'test_utils/enzyme_helpers';
 import { PingTls } from '../../../../common/graphql/types';
 import { MonitorSSLCertificate } from '../monitor_status_bar/monitor_ssl_certificate';
@@ -13,12 +14,24 @@ describe('MonitorStatusBar component', () => {
   let monitorTls: PingTls;
 
   beforeEach(() => {
+    const dateInTwoMonths = moment()
+      .add(2, 'month')
+      .toString();
+
     monitorTls = {
-      certificate_not_valid_after: '2019-11-03T02:02:21.000Z',
+      certificate_not_valid_after: dateInTwoMonths,
     };
   });
 
   it('renders', () => {
+    const component = renderWithIntl(<MonitorSSLCertificate tls={monitorTls} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders null if invalid date', () => {
+    monitorTls = {
+      certificate_not_valid_after: 'i am so invalid date',
+    };
     const component = renderWithIntl(<MonitorSSLCertificate tls={monitorTls} />);
     expect(component).toMatchSnapshot();
   });
