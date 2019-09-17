736855bf8ec592a165b63defb49d9afc0344880d
John Dorlus <jsdorlus@elastic.co>
2019-09-16 21:33:28 -0400

    Fixing the button that's being searched for.

--- test/functional/page_objects/settings_page.js
+++ test/functional/page_objects/settings_page.js
@@ -322,7 +322,7 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
     async clickOptionalAddNewButton(isStandardIndexPattern) {
       if (await testSubjects.isDisplayed('createIndexPatternButton')) {
         await testSubjects.click('createIndexPatternButton');
-        if (await testSubjects.isDisplayed('createRollupIndexPatternButton')) {
+        if (await testSubjects.isDisplayed('createStandardIndexPatternButton')) {
           if (isStandardIndexPattern) {
             await this.clickIndexPatternType('Standard');
           } else {
