2f469e2ef235c6ccf2e0d502cc106e335b0a23b1
John Gress <john.gress@logrhythm.com>
2019-09-17 17:17:43 -0600

    Load dashboards for kibana on startup.

--- scripts/exportAssets.py
+++ scripts/exportAssets.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python
 import os
+import requests
 import sys
 import argparse
 import json
-from util import ElasticsearchUtil
-from util import LogConfig
 from util import Utility
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 EXPORT_LOG = "/tmp/ExportAssets.log"
-
-esUtil = ElasticsearchUtil(EXPORT_LOG)
-logger = LogConfig(log_file=EXPORT_LOG)
-logging, rotating_handler = logger.configure_and_return_logging()
 UTIL = Utility(log_file=EXPORT_LOG)
+logging, rotating_handler = UTIL.get_logging()
+logger = logging.getLogger()
 
 OUTPUT_DIR = os.path.dirname(os.path.realpath(__file__)) 
 
-INDEX = esUtil.KIBANA_INDEX
 TYPE = None
-ID = None
+TITLE = None
 
 DASHBOARD = "dashboard"
 VISUALIZATION = "visualization"
@@ -26,19 +24,64 @@ SEARCH = "search"
 
 FILE_PATHS_AND_CONTENTS = {}
 
+def check_elasticsearch_health():
+    try:
+        url = 'http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=60s'
+        session = requests.Session()
+        response = session.get(url)
+        if not response.ok:
+            response.raise_for_status()
 
-def get_asset(es_index, es_type, es_id):
-    ignored, doc_exists = esUtil.function_with_timeout(esUtil.ES_QUERY_TIMEOUT,
-                                                       esUtil.document_exists,
-                                                       es_index,
-                                                       es_type,
-                                                       es_id)
-    if doc_exists:
-        success, ret = esUtil.get_document(es_index, es_type, es_id)
-        logging.info("GET RETURNS: " + "\n" + UTIL.pretty_format(ret))
-        return True, ret
-    else:
-        logging.info("No such thing as " + es_id + " in /" + es_index + "/" + es_type + "/")
+        logger.info("elasticsearch status: " + "\n" + UTIL.pretty_format(response.json()))
+        return True
+
+    except Exception as err:
+        logger.warning("[loadAssets.py] Caught HTTP exception: {0}".format(err))
+        return False
+
+def get_search_title_json(es_type, es_title):
+    esField = es_type + '.title'
+      
+    query = {
+        "query": {
+            "match_phrase": {
+                esField : es_title
+            }
+        }
+    }
+    return query
+   
+def get_search_id_json(es_type, es_id):
+    query = {
+        "query": {
+            "match_phrase": {
+                "_id" : es_type + ":" + es_id
+            }
+        }
+    }
+    return query
+   
+def get_asset(es_query):
+    try:
+        url = 'http://localhost:9200/.kibana/_search'
+      
+        session = requests.Session()
+        # Retry on Internal Server Error (500) and Service Unavailable (503)
+        retries = Retry(total=5, backoff_factor=0.3, status_forcelist=[ 500, 503])
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        esQuery = json.dumps(es_query)
+        headers = { 'Content-type': 'application/json' }
+        response = session.get(url, headers=headers, data=esQuery)
+
+        if not response.ok:
+            response.raise_for_status()
+
+        logger.info("GET RETURNS: " + "\n" + UTIL.pretty_format(response.json()))
+        return True, response.text
+
+    except Exception as err:
+        logger.info("[getEs.py] Caught HTTP exception: {0}".format(err))
         return False, "Not found"
 
 def print_error_and_usage(argParser, error):
@@ -65,12 +108,12 @@ def get_full_path(asset_id):
         return OUTPUT_DIR + "/" + get_filename(asset_id)
 
 def strip_metadata(json_string):
-    ob = json.loads(json.dumps(json_string))
+    ob = json.loads(json_string)["hits"]["hits"][0]
     return UTIL.safe_list_read(list_ob=ob, key='_source')
 
 def get_dashboard_panels(panels_str):
     panels_with_type = {}
-    db_panels_json = json.loads(UTIL.remove_all_char(string=panels_str, to_remove="\\"))
+    db_panels_json = panels_str
     for index, panel in enumerate(db_panels_json):
         panel_id = UTIL.safe_list_read(list_ob=db_panels_json[index], key='id')
         panel_type = UTIL.safe_list_read(list_ob=db_panels_json[index], key='type')
@@ -84,14 +127,15 @@ def export_all_files(asset_dict=FILE_PATHS_AND_CONTENTS):
 
 def get_all_dashboard_content_from_ES(dashboard_raw):
     global FILE_PATHS_AND_CONTENTS
-    global INDEX
     db_json = UTIL.make_json(dashboard_raw)
-    db_panels_raw = UTIL.safe_list_read(list_ob=db_json, key='panelsJSON')
+    db_panels_raw = UTIL.safe_list_read(list_ob=db_json, key='references')
     panels_with_type = get_dashboard_panels(db_panels_raw)
     for panel_id, panel_type in panels_with_type.iteritems():
-        success, ret = get_asset(INDEX, panel_type, panel_id)
+        es_query = get_search_id_json(panel_type, panel_id)
+        success, asset_raw = get_asset(es_query)
+        asset_id = panel_type + ":" + panel_id
         if success:
-            FILE_PATHS_AND_CONTENTS[get_full_path(panel_id)] = strip_metadata(ret)
+            add_asset_to_output_dict(asset_raw=asset_raw, asset_id=asset_id)
         else:
             print "ERROR: Failed to get asset " + panel_id + " needed by dashboard."
 
@@ -118,33 +162,40 @@ def main(argv):
     global OUTPUT_DIR
     global FILE_PATHS_AND_CONTENTS
     global TYPE
-    global ID
+    global TITLE
 
     if args.dash_name:
         TYPE = DASHBOARD
-        ID = args.dash_name
+        TITLE = args.dash_name
     elif args.viz_name:
         TYPE = VISUALIZATION
-        ID = args.viz_name
+        TITLE = args.viz_name
     elif args.search_name:
         TYPE = SEARCH
-        ID = args.search_name
+        TITLE = args.search_name
 
     if args.directory:
         OUTPUT_DIR = args.directory
 
-    success, asset_raw = get_asset(es_index=INDEX, es_type=TYPE, es_id=ID)
+    es_ok = check_elasticsearch_health()
+    if es_ok:
+        es_query = get_search_title_json(TYPE, TITLE)
+        success, asset_raw = get_asset(es_query)
 
-
-    if success:
-        add_asset_to_output_dict(asset_raw=asset_raw, asset_id=ID)
-        if TYPE == DASHBOARD:
-            get_all_dashboard_content_from_ES(dashboard_raw=UTIL.safe_list_read(list_ob=FILE_PATHS_AND_CONTENTS, key=get_full_path(asset_id=ID)))
-        export_all_files()
+        if success:
+            ob = json.loads(asset_raw)["hits"]["hits"][0]
+            asset_id = ob["_id"]
+            add_asset_to_output_dict(asset_raw=asset_raw, asset_id=asset_id)
+            if TYPE == DASHBOARD:
+                get_all_dashboard_content_from_ES(dashboard_raw=UTIL.safe_list_read(list_ob=FILE_PATHS_AND_CONTENTS, key=get_full_path(asset_id=asset_id)))
+            export_all_files()
+        else:
+            print "ERROR:   Did not find any " + TYPE + " named " + TITLE + " in Elasticsearch."
+            sys.exit(2)
     else:
-        print "ERROR:   Did not find any " + TYPE + " named " + ID + " in Elasticsearch."
-        sys.exit(2)
+      print "elasticsearch is not ready\n"
+      sys.exit(1)
 
 
 if __name__ == '__main__':
-     main(sys.argv[1:])
+    main(sys.argv[1:])
--- scripts/loadAssets.py
+++ scripts/loadAssets.py
@@ -2,116 +2,119 @@
 #
 # Author: Craig Cogdill
 
+import sys
 import json
+import requests
 import time
-from elasticsearch import Elasticsearch
 from os import listdir
 from os.path import isfile, join, splitext
-from util import ElasticsearchUtil
 from util import Utility
-from util import LogConfig
-es = Elasticsearch(max_retries=5, retry_on_timeout=True)
-esUtil = ElasticsearchUtil()
-logConfig = LogConfig()
-logging, rotating_handler = logConfig.configure_and_return_logging()
-logger = logging.getLogger()
-logger.addHandler(rotating_handler)
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 UTIL = Utility()
+logging, rotating_handler = UTIL.get_logging()
+logger = logging.getLogger()
 
-resources = "/usr/local/kibana-" + esUtil.KIBANA_VERSION + "-linux-x64/resources"
-dashboards_path = resources + "/dashboards"
-visualizations_path = resources + "/visualizations"
-searches_path = resources + "/searches"
+resources = "/usr/local/kibana-7.2.0-linux-x64/resources"
 
-dashboard_type = "dashboard"
-visualization_type = "visualization"
-search_type = "search"
+def check_elasticsearch_health():
+    try:
+        url = 'http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=60s'
+        session = requests.Session()
+        response = session.get(url)
+        if not response.ok:
+            response.raise_for_status()
 
-always_update_whitelist = ["Capture-Dashboard.json"]
+        logger.info("elasticsearch status: " + "\n" + UTIL.pretty_format(response.json()))
+        return True
 
-def create_document_from_file(es_index, es_type, es_id, path_to_updated_json):
+    except Exception as err:
+        logger.warning("[loadAssets.py] Caught HTTP exception: {0}".format(err))
+        return False
+
+def set_default_index_pattern():
+    try:
+        url = 'http://localhost:9200/.kibana/_update/config:7.2.0'
+
+        postJson = {
+            "doc": {
+                "config": {
+                    "defaultIndex" : "361f5c00-b47c-11e9-86a0-cd3d7bf2f81b"
+                }
+            }
+        }
+
+        session = requests.Session()
+        # Retry on Internal Server Error (500) and Service Unavailable (503)
+        retries = Retry(total=5, backoff_factor=0.3, status_forcelist=[ 500, 503])
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        esJson = json.dumps(postJson)
+        headers = { 'Content-type': 'application/json' }
+        response = session.post(url, headers=headers, data=esJson)
+
+        if not response.ok:
+            response.raise_for_status()
+
+        logger.info("Successfully set index pattern:\n" + UTIL.pretty_format(response.json()))
+        return True
+
+    except Exception as err:
+        logger.warning("[loadAssets.py] Failed to set default index pattern: {0}".format(err) \
+            + "\n\turl: {0}".format(url) + "\n\tpostJson: {0}".format(postJson))
+        return False
+
+def put_document_from_file(es_id, path_to_updated_json):
     content = UTIL.read_json_from_file(path_to_updated_json)
-    return esUtil.function_with_timeout(esUtil.STARTUP_TIMEOUT,
-                                      esUtil.create_document,
-                                          es_index,
-                                          es_type,
-                                          es_id,
-                                          content)
+    try:
+        url = 'http://localhost:9200/.kibana/_doc/' + es_id
+      
+        session = requests.Session()
+        # Retry on Internal Server Error (500) and Service Unavailable (503)
+        retries = Retry(total=5, backoff_factor=0.3, status_forcelist=[ 500, 503])
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        esJson = json.dumps(content)
+        headers = { 'Content-type': 'application/json' }
+        response = session.put(url, headers=headers, data=esJson)
+
+        if not response.ok:
+            response.raise_for_status()
+
+        logger.info("PUT RETURNS: " + "\n" + UTIL.pretty_format(response.json()))
+        return True, response.text
+
+    except Exception as err:
+        logger.warning("[loadAssets.py] Caught HTTP exception: {0}".format(err))
+        return False, "Not found"
 
 def get_es_id(filename):
     return splitext(filename)[0]
 
-def get_version_of_file(file):
-    file_json = UTIL.read_json_from_file(file)
-    version_from_file = UTIL.safe_list_read(file_json, 'version')
-    return version_from_file
-
-def update_existing_document(es_index, es_type, es_id, path_to_updated_json):
-    deleted, del_ret_val = esUtil.function_with_timeout(esUtil.ES_QUERY_TIMEOUT,
-                                                      esUtil.delete_document,
-                                                        es_index,
-                                                        es_type,
-                                                        es_id)
-    logger.info("Delete returns: " + str(json.dumps(del_ret_val)))
-    created, create_ret_val = create_document_from_file(es_index,
-                                                      es_type,
-                                                      es_id,
-                                                      path_to_updated_json)
-    logger.info("Create returns: " + str(create_ret_val))
-
-def es_version_is_outdated(es_index, es_type, es_id, full_file_path):
-    get_response_json = esUtil.get_request_as_json(es_index, es_type, es_id)
-    version_of_disk_file = get_version_of_file(full_file_path)
-    es_file_source = UTIL.safe_list_read(get_response_json, '_source')
-    version_of_es_file = UTIL.safe_list_read(es_file_source, 'version')
-    return version_of_disk_file > version_of_es_file, version_of_disk_file, version_of_es_file
-
-def load_assets(es_index, es_type, path_to_files, files):
+def load_assets(path_to_files, files):
     for file in files:
         logger.info("--------- " + file + " ---------")
         full_file_path = path_to_files + "/" + file
         es_id = get_es_id(file)
-        ignored, asset_exists = esUtil.function_with_timeout(esUtil.ES_QUERY_TIMEOUT,
-                                      esUtil.document_exists,
-                                          es_index,
-                                          es_type,
-                                          es_id)
-        if asset_exists:
-            if file in always_update_whitelist:
-                logger.info("File \""+str(file)+"\" should always be updated.")
-                update_existing_document(es_index, es_type, es_id, full_file_path)
-            else:
-                es_outdated, version_of_disk_file, version_of_es_file = es_version_is_outdated(es_index,
-                                                                                             es_type,
-                                                                                             es_id,
-                                                                                             full_file_path)
-                if es_outdated:
-                    logger.info("File \"" + str(file) + "\" is outdated and requires update from version " + str(version_of_es_file) +
-                             " to version " + str(version_of_disk_file) + ". Updating it now...")
-                    update_existing_document(es_index, es_type, es_id, full_file_path)
-                else:
-                    logger.info("Current version of file \"" + str(file) + "\" in Elasticsearch is up-to-date. Version: " + str(version_of_es_file))
-        else:
-            logger.info("File \"" + str(file) + "\" doesn't exist in Elasticsearch. Creating it now...")
-            created, created_ret = create_document_from_file(es_index, es_type, es_id, full_file_path)
-            logger.info("Create returns: " + created_ret)
+        created, created_ret = put_document_from_file(es_id, full_file_path)
+        if not created:
+            logger.warning("Failed to load kibana object: " + file)
 
 
 # ----------------- MAIN -----------------
 def main():
 
-    # Create arrays of the filenames in each of the resource dirs
-    dashboards = [filename for filename in listdir(dashboards_path) if isfile(join(dashboards_path, filename))]
-    visualizations = [filename for filename in listdir(visualizations_path) if isfile(join(visualizations_path, filename))]
-    searches = [filename for filename in listdir(searches_path) if isfile(join(searches_path, filename))]
+    es_ok = check_elasticsearch_health()
+    if es_ok:
+        # Create arrays of the filenames in the resources directory
+        objects = [filename for filename in listdir(resources) if isfile(join(resources, filename))]
+        # Load all the artifacts appropriately
+        load_assets(resources, objects)
+        set_default_index_pattern()
+    else:
+      print "elasticsearch is not ready\n"
+      sys.exit(1)
 
-    # Load all the artifacts appropriately
-    logger.info("================================== DASHBOARDS ==================================")
-    load_assets(esUtil.KIBANA_INDEX, dashboard_type, dashboards_path, dashboards)
-    logger.info("================================== VISUALIZATIONS ==================================")
-    load_assets(esUtil.KIBANA_INDEX, visualization_type, visualizations_path, visualizations)
-    logger.info("================================== SEARCHES ==================================")
-    load_assets(esUtil.KIBANA_INDEX, search_type, searches_path, searches)
 
 if __name__ == '__main__':
     main()
