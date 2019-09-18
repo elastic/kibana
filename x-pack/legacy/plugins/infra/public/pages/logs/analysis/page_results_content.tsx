2a1e36b08a91f99684e237e357d07d870571b5e4
Felix Stürmer <stuermer@weltenwort.de>
2019-09-18 21:03:15 +0200

    Poll for job status on results page

--- x-pack/legacy/plugins/infra/public/pages/logs/analysis/page_results_content.tsx
+++ x-pack/legacy/plugins/infra/public/pages/logs/analysis/page_results_content.tsx
@@ -19,20 +19,24 @@ import {
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
 import moment from 'moment';
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useContext, useMemo, useState } from 'react';
 
 import euiStyled from '../../../../../../common/eui_styled_components';
 import { TimeRange } from '../../../../common/http_api/shared/time_range';
 import { bucketSpan } from '../../../../common/log_analysis';
 import { LoadingPage } from '../../../components/loading_page';
 import {
+  LogAnalysisJobs,
   StringTimeRange,
   useLogAnalysisResults,
   useLogAnalysisResultsUrlState,
 } from '../../../containers/logs/log_analysis';
+import { useInterval } from '../../../hooks/use_interval';
 import { useTrackPageview } from '../../../hooks/use_track_metric';
 import { FirstUseCallout } from './first_use';
 import { LogRateResults } from './sections/log_rate';
+
+const JOB_STATUS_POLLING_INTERVAL = 10000;
 
 export const AnalysisResultsContent = ({
   sourceId,
@@ -136,6 +140,12 @@ export const AnalysisResultsContent = ({
       }
     }
   }, [logEntryRate]);
+
+  const { fetchJobStatus, jobStatus } = useContext(LogAnalysisJobs.Context);
+
+  useInterval(() => {
+    fetchJobStatus();
+  }, JOB_STATUS_POLLING_INTERVAL);
 
   return (
     <>
