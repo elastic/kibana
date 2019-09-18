5019f995716ada98c9d0a3b03ab89be87bc3d52c
Dmitrii Arnautov <dmitrii.arnautov@elastic.co>
2019-09-18 18:26:32 +0200

    [ML] replace css classes with Eui components

--- x-pack/legacy/plugins/ml/public/components/anomalies_table/severity_cell/severity_cell.tsx
+++ x-pack/legacy/plugins/ml/public/components/anomalies_table/severity_cell/severity_cell.tsx
@@ -5,7 +5,7 @@
  */
 
 import React, { FC, memo } from 'react';
-import { EuiHealth } from '@elastic/eui';
+import { EuiHealth, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { MULTI_BUCKET_IMPACT } from '../../../../common/constants/multi_bucket_impact';
 import { getSeverityColor } from '../../../../common/util/anomaly_utils';
 
@@ -30,17 +30,17 @@ export const SeverityCell: FC<SeverityCellProps> = memo(({ score, multiBucketImp
   const color = getSeverityColor(score);
   const isMultiBucket = multiBucketImpact >= MULTI_BUCKET_IMPACT.MEDIUM;
   return isMultiBucket ? (
-    <div className="euiFlexGroup euiFlexGroup--gutterExtraSmall euiFlexGroup--alignItemsCenter euiFlexGroup--directionRow">
-      <div className="euiFlexItem euiFlexItem--flexGrowZero">
+    <EuiFlexGroup>
+      <EuiFlexItem>
         <svg width="16" height="16" viewBox="-2 -2 20 20" fill={color}>
           <path
             d="M-6.708203932499369,-2.23606797749979H-2.23606797749979V-6.708203932499369H2.23606797749979V-2.23606797749979H6.708203932499369V2.23606797749979H2.23606797749979V6.708203932499369H-2.23606797749979V2.23606797749979H-6.708203932499369Z"
             transform="translate(8,8)"
           ></path>
         </svg>
-      </div>
-      <span className="euiFlexItem euiFlexItem--flexGrowZero">{severity}</span>
-    </div>
+      </EuiFlexItem>
+      <EuiFlexItem>{severity}</EuiFlexItem>
+    </EuiFlexGroup>
   ) : (
     <EuiHealth color={color}>{severity}</EuiHealth>
   );
