9fb4ba2fdc5f055b9e19d1fb0f8464d1903668d2
Dmitrii Arnautov <dmitrii.arnautov@elastic.co>
2019-09-18 13:30:37 +0200

    [ML] severity cell with multi-bucket impact support

---
+++ x-pack/legacy/plugins/ml/public/components/anomalies_table/severity_cell/severity_cell.test.tsx
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { cleanup, render } from 'react-testing-library';
+import { SeverityCell } from './severity_cell';
+
+describe('SeverityCell', () => {
+  afterEach(cleanup);
+
+  test('should render a single-bucket marker with rounded severity score', () => {
+    const props = {
+      score: 75.2,
+      multiBucketImpact: -2,
+    };
+    const { container } = render(<SeverityCell {...props} />);
+    expect(container.textContent).toBe('75');
+    expect(container.getElementsByTagName('svg').item(0).style.fill).toBe('#fe5050');
+  });
+
+  test('should render a multi-bucket marker with low severity score', () => {
+    const props = {
+      score: 0.8,
+      multiBucketImpact: 4,
+    };
+    const { container } = render(<SeverityCell {...props} />);
+    expect(container.textContent).toBe('< 1');
+    expect(
+      container
+        .getElementsByTagName('svg')
+        .item(0)
+        .getAttribute('fill')
+    ).toBe('#d2e9f7');
+  });
+});
