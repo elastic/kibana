b1b7bc435ac82338013ab5fe1cbfd3c0d4f3abab
FrankHassanabad <frank.hassanabad@elastic.co>
2019-09-18 14:47:46 -0600

    [SIEM] Changed job links have the job configuration in the search on clicks

--- x-pack/legacy/plugins/siem/public/components/ml_popover/jobs_table/job_switch.test.tsx
+++ x-pack/legacy/plugins/siem/public/components/ml_popover/jobs_table/job_switch.test.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import * as React from 'react';
 
@@ -12,11 +12,61 @@ import { isChecked, isFailure, isJobLoading, JobSwitch } from './job_switch';
 import { mockOpenedJob } from '../__mocks__/api';
 
 describe('JobSwitch', () => {
+  let onJobStateChangeMock = jest.fn();
+  beforeEach(() => {
+    onJobStateChangeMock = jest.fn();
+  });
+
   test('renders correctly against snapshot', () => {
     const wrapper = shallow(
       <JobSwitch job={mockOpenedJob} isSummaryLoading={false} onJobStateChange={jest.fn()} />
     );
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('should call onJobStateChange when the switch is clicked to a a true/open', () => {
+    const wrapper = mount(
+      <JobSwitch
+        isSummaryLoading={false}
+        job={mockOpenedJob}
+        onJobStateChange={onJobStateChangeMock}
+      />
+    );
+
+    wrapper
+      .find('[data-test-subj="job-switch"] input')
+      .first()
+      .simulate('change', {
+        target: { checked: true },
+      });
+
+    expect(onJobStateChangeMock.mock.calls[0]).toEqual([
+      'siem-api-rare_process_linux_ecs',
+      1562870521264,
+      true,
+    ]);
+  });
+
+  test('should have a switch when it is not in the loading state', () => {
+    const wrapper = mount(
+      <JobSwitch
+        isSummaryLoading={false}
+        job={mockOpenedJob}
+        onJobStateChange={onJobStateChangeMock}
+      />
+    );
+    expect(wrapper.find('[data-test-subj="job-switch"]').exists()).toBe(true);
+  });
+
+  test('should not have a switch when it is in the loading state', () => {
+    const wrapper = mount(
+      <JobSwitch
+        isSummaryLoading={true}
+        job={mockOpenedJob}
+        onJobStateChange={onJobStateChangeMock}
+      />
+    );
+    expect(wrapper.find('[data-test-subj="job-switch"]').exists()).toBe(false);
   });
 
   describe('isChecked', () => {
--- x-pack/legacy/plugins/siem/public/components/ml_popover/jobs_table/jobs_table.test.tsx
+++ x-pack/legacy/plugins/siem/public/components/ml_popover/jobs_table/jobs_table.test.tsx
@@ -4,14 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import * as React from 'react';
 import { JobsTable } from './jobs_table';
 import { mockJobsSummaryResponse } from '../__mocks__/api';
 
 describe('JobsTable', () => {
-  const onJobStateChangeMock = jest.fn();
+  let onJobStateChangeMock = jest.fn();
+  beforeEach(() => {
+    onJobStateChangeMock = jest.fn();
+  });
 
   test('renders correctly against snapshot', () => {
     const wrapper = shallow(
@@ -22,5 +25,64 @@ describe('JobsTable', () => {
       />
     );
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('should render the hyper link which points specifically to the job id', () => {
+    const wrapper = mount(
+      <JobsTable
+        isLoading={true}
+        jobs={mockJobsSummaryResponse}
+        onJobStateChange={onJobStateChangeMock}
+      />
+    );
+    expect(
+      wrapper
+        .find('[data-test-subj="jobs-table-link"]')
+        .first()
+        .props().href
+    ).toEqual('/test/base/path/app/ml#/jobs?mlManagement=(jobId:rc-rare-process-windows-5)');
+  });
+
+  test('should call onJobStateChange when the switch is clicked to a a true/open', () => {
+    const wrapper = mount(
+      <JobsTable
+        isLoading={false}
+        jobs={mockJobsSummaryResponse}
+        onJobStateChange={onJobStateChangeMock}
+      />
+    );
+    wrapper
+      .find('[data-test-subj="job-switch"] input')
+      .first()
+      .simulate('change', {
+        target: { checked: true },
+      });
+    expect(onJobStateChangeMock.mock.calls[0]).toEqual([
+      'rc-rare-process-windows-5',
+      1561402325194,
+      true,
+    ]);
+  });
+
+  test('should have a switch when it is not in the loading state', () => {
+    const wrapper = mount(
+      <JobsTable
+        isLoading={false}
+        jobs={mockJobsSummaryResponse}
+        onJobStateChange={onJobStateChangeMock}
+      />
+    );
+    expect(wrapper.find('[data-test-subj="job-switch"]').exists()).toBe(true);
+  });
+
+  test('should not have a switch when it is in the loading state', () => {
+    const wrapper = mount(
+      <JobsTable
+        isLoading={true}
+        jobs={mockJobsSummaryResponse}
+        onJobStateChange={onJobStateChangeMock}
+      />
+    );
+    expect(wrapper.find('[data-test-subj="job-switch"]').exists()).toBe(false);
   });
 });
