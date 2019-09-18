b04fa4e42b959f33f95924fde040c39f01a9d349
Walter Rafelsberger <walter@elastic.co>
2019-09-18 18:28:53 +0200

    [transform] Fix translations.

--- x-pack/legacy/plugins/transform/public/app/services/text/text.ts
+++ x-pack/legacy/plugins/transform/public/app/services/text/text.ts
@@ -10,41 +10,17 @@ class TextService {
   public init(i18n: any): void {
     this.i18n = i18n;
     this.breadcrumbs = {
-      home: i18n.translate('xpack.snapshotRestore.home.breadcrumbTitle', {
-        defaultMessage: 'Snapshot and Restore',
+      home: i18n.translate('xpack.transform.home.breadcrumbTitle', {
+        defaultMessage: 'Transforms',
       }),
-      snapshots: i18n.translate('xpack.snapshotRestore.snapshots.breadcrumbTitle', {
-        defaultMessage: 'Snapshots',
-      }),
-      repositories: i18n.translate('xpack.snapshotRestore.repositories.breadcrumbTitle', {
-        defaultMessage: 'Repositories',
-      }),
-      policies: i18n.translate('xpack.snapshotRestore.policies.breadcrumbTitle', {
-        defaultMessage: 'Policies',
-      }),
-      restore_status: i18n.translate('xpack.snapshotRestore.restoreStatus.breadcrumbTitle', {
-        defaultMessage: 'Restore Status',
-      }),
-      repositoryAdd: i18n.translate('xpack.snapshotRestore.addRepository.breadcrumbTitle', {
-        defaultMessage: 'Add repository',
-      }),
-      repositoryEdit: i18n.translate('xpack.snapshotRestore.editRepository.breadcrumbTitle', {
-        defaultMessage: 'Edit repository',
-      }),
-      restoreSnapshot: i18n.translate('xpack.snapshotRestore.restoreSnapshot.breadcrumbTitle', {
-        defaultMessage: 'Restore snapshot',
-      }),
-      policyAdd: i18n.translate('xpack.snapshotRestore.addPolicy.breadcrumbTitle', {
-        defaultMessage: 'Add policy',
-      }),
-      policyEdit: i18n.translate('xpack.snapshotRestore.editPolicy.breadcrumbTitle', {
-        defaultMessage: 'Edit policy',
+      snapshots: i18n.translate('xpack.transform.createTransform.breadcrumbTitle', {
+        defaultMessage: 'Create transform',
       }),
     };
   }
 
   public getSizeNotationHelpText() {
-    return this.i18n.translate('xpack.snapshotRestore.repositoryForm.sizeNotationPlaceholder', {
+    return this.i18n.translate('xpack.transform.transformForm.sizeNotationPlaceholder', {
       defaultMessage: 'Examples: {example1}, {example2}, {example3}, {example4}',
       values: {
         example1: '1g',
