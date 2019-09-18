4d6f3081dccb5cba97be04349efbf81838107332
Dmitrii Arnautov <dmitrii.arnautov@elastic.co>
2019-09-18 13:30:37 +0200

    [ML] severity cell with multi-bucket impact support

---
+++ x-pack/legacy/plugins/ml/public/components/anomalies_table/severity_cell/severity_cell.tsx
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { FC, memo } from 'react';
+import { EuiHealth } from '@elastic/eui';
+import { getSeverityColor } from '../../../../common/util/anomaly_utils';
+
+interface SeverityCellProps {
+  /**
+   * Severity score.
+   */
+  score: number;
+  /**
+   * Multi-bucket impact score from –5 to 5.
+   * Anomalies with a multi_bucket_impact value of greater than or equal
+   * to 2 are considered multi-bucket impact anomalies.
+   */
+  multiBucketImpact: number;
+}
+
+/**
+ * Renders anomaly severity score with single or multi-bucket impact marker.
+ */
+export const SeverityCell: FC<SeverityCellProps> = memo(({ score, multiBucketImpact }) => {
+  const severity = score >= 1 ? Math.floor(score) : '< 1';
+  const color = getSeverityColor(score);
+  const isMultiBucket = multiBucketImpact >= 2;
+  return isMultiBucket ? (
+    <div className="euiFlexGroup euiFlexGroup--gutterExtraSmall euiFlexGroup--alignItemsCenter euiFlexGroup--directionRow">
+      <div className="euiFlexItem euiFlexItem--flexGrowZero">
+        <svg width="16" height="16" viewBox="-2 -2 20 20" fill={color}>
+          <path
+            d="M-6.708203932499369,-2.23606797749979H-2.23606797749979V-6.708203932499369H2.23606797749979V-2.23606797749979H6.708203932499369V2.23606797749979H2.23606797749979V6.708203932499369H-2.23606797749979V2.23606797749979H-6.708203932499369Z"
+            transform="translate(8,8)"
+          ></path>
+        </svg>
+      </div>
+      <span className="euiFlexItem euiFlexItem--flexGrowZero">{severity}</span>
+    </div>
+  ) : (
+    <EuiHealth color={color}>{severity}</EuiHealth>
+  );
+});
