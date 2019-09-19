87a571d86d2f0adac2d69583cb1b1dffc57ee8a2
Nathan Reese <reese.nathan@gmail.com>
2019-09-19 12:02:51 -0600

    add unit test for on click handler

--- x-pack/legacy/plugins/maps/public/connected_components/map/mb/tooltip_control/tool_control.test.js
+++ x-pack/legacy/plugins/maps/public/connected_components/map/mb/tooltip_control/tool_control.test.js
@@ -4,10 +4,19 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import sinon from 'sinon';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import { TooltipControl, TOOLTIP_TYPE } from './tooltip_control';
 
+const layerId = 'tfi3f';
+const mbLayerId = 'tfi3f_circle';
+const mockLayer = {
+  getMbLayerIds: () => { return [mbLayerId]; },
+  getId: () => { return layerId; },
+  canShowTooltip: () => { return true; },
+};
+let featuresAtLocation = [];
 const mockMbMapBounds = {
   west: -180,
   east: 180,
@@ -37,14 +46,14 @@ const mockMBMap = {
     };
   },
   getLayer: () => {},
-  queryRenderedFeatures: () => {},
+  queryRenderedFeatures: () => { return featuresAtLocation; },
 };
 
 const defaultProps = {
   mbMap: mockMBMap,
   clearTooltipState: () => {},
   setTooltipState: () => {},
-  layerList: [],
+  layerList: [mockLayer],
   isDrawingFilter: false,
   addFilters: () => {},
   geoFields: [{  }],
@@ -56,7 +65,7 @@ const hoverTooltipState = {
   features: [
     {
       id: 1,
-      layerId: 'tfi3f',
+      layerId: layerId,
       geometry: {},
     }
   ]
@@ -101,6 +110,85 @@ describe('TooltipControl', () => {
         );
 
         expect(component).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('onClick', () => {
+    const mockMapMouseEvent = {
+      point: { x: 0, y: 0 },
+      lngLat: { lng: 0, lat: 0 },
+    };
+    const setTooltipStateStub = sinon.stub();
+    const clearTooltipStateStub = sinon.stub();
+
+    beforeEach(() => {
+      setTooltipStateStub.reset();
+      clearTooltipStateStub.reset();
+    });
+
+    test('should ignore clicks when map is in drawing mode', () => {
+      mount(
+        <TooltipControl
+          {...defaultProps}
+          clearTooltipState={clearTooltipStateStub}
+          setTooltipState={setTooltipStateStub}
+          isDrawingFilter={true}
+        />
+      );
+
+      mockMbMapHandlers.click(mockMapMouseEvent);
+
+      sinon.assert.notCalled(clearTooltipStateStub);
+      sinon.assert.notCalled(setTooltipStateStub);
+    });
+
+    test('should clear tooltip state when there are no features at clicked location', () => {
+      featuresAtLocation = [];
+      mount(
+        <TooltipControl
+          {...defaultProps}
+          clearTooltipState={clearTooltipStateStub}
+          setTooltipState={setTooltipStateStub}
+        />
+      );
+
+      mockMbMapHandlers.click(mockMapMouseEvent);
+
+      sinon.assert.calledOnce(clearTooltipStateStub);
+      sinon.assert.notCalled(setTooltipStateStub);
+    });
+
+    test('should set tooltip state when there are features at clicked location', () => {
+      featuresAtLocation = [
+        {
+          geometry: {
+            type: 'Point',
+            coordinates: [ 100, 30 ]
+          },
+          layer: {
+            id: mbLayerId
+          },
+          properties: {
+            __kbn__feature_id__: 1
+          }
+        }
+      ];
+      mount(
+        <TooltipControl
+          {...defaultProps}
+          clearTooltipState={clearTooltipStateStub}
+          setTooltipState={setTooltipStateStub}
+        />
+      );
+
+      mockMbMapHandlers.click(mockMapMouseEvent);
+
+      sinon.assert.notCalled(clearTooltipStateStub);
+      sinon.assert.calledWith(setTooltipStateStub, {
+        features: [{ geometry: { coordinates: [100, 30], type: 'Point' }, id: 1, layerId: 'tfi3f' }],
+        location: [100, 30],
+        type: 'LOCKED'
       });
     });
   });
