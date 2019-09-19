b48fcfb7749920ded96927623afb3388d489fee7
Patryk Kopyciński <contact@patrykkopycinski.com>
2019-09-19 15:04:58 +0200

    Fix typo

--- x-pack/legacy/plugins/siem/public/pages/hosts/navigation/events_query_tab_body.tsx
+++ x-pack/legacy/plugins/siem/public/pages/hosts/navigation/events_query_tab_body.tsx
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import React from 'react';
 import { StatefulEventsViewer } from '../../../components/events_viewer';
 import { HostsComponentsQueryProps } from './types';
 
@@ -15,7 +16,7 @@ export const EventsQueryTabBody = ({
   const HOSTS_PAGE_TIMELINE_ID = 'hosts-page';
 
   return (
-    <StatefulEventsViewer<>
+    <StatefulEventsViewer
       end={endDate}
       id={HOSTS_PAGE_TIMELINE_ID}
       kqlQueryExpression={kqlQueryExpression}
