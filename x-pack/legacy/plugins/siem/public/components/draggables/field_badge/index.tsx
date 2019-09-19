e2a55e6c47ff9765ac0755fccaf5076750e41544
Michael Marcialis <michael.marcialis@elastic.co>
2019-09-19 15:36:48 -0400

    inline dynamic width

--- x-pack/legacy/plugins/siem/public/components/draggables/field_badge/index.tsx
+++ x-pack/legacy/plugins/siem/public/components/draggables/field_badge/index.tsx
@@ -9,8 +9,8 @@ import * as React from 'react';
 import { pure } from 'recompose';
 import styled, { css } from 'styled-components';
 
-const Field = styled.div<{ fieldWidth?: string }>`
-  ${({ fieldWidth, theme }) => css`
+const Field = styled.div`
+  ${({ theme }) => css`
     background-color: ${theme.eui.euiColorEmptyShade};
     border: ${theme.eui.euiBorderThin};
     box-shadow: 0 2px 2px -1px ${rgba(theme.eui.euiColorMediumShade, 0.3)},
@@ -19,7 +19,6 @@ const Field = styled.div<{ fieldWidth?: string }>`
     font-weight: ${theme.eui.euiFontWeightSemiBold};
     line-height: ${theme.eui.euiLineHeight};
     padding: ${theme.eui.paddingSizes.xs};
-    width: ${fieldWidth};
   `}
 `;
 Field.displayName = 'Field';
@@ -27,9 +26,13 @@ Field.displayName = 'Field';
 /**
  * Renders a field (e.g. `event.action`) as a draggable badge
  */
+
+// Passing the styles directly to the component because the width is
+// being calculated and is recommended by Styled Components for performance
+// https://github.com/styled-components/styled-components/issues/134#issuecomment-312415291
 export const DraggableFieldBadge = pure<{ fieldId: string; fieldWidth?: string }>(
   ({ fieldId, fieldWidth }) => (
-    <Field data-test-subj="field" fieldWidth={fieldWidth}>
+    <Field data-test-subj="field" style={{ width: fieldWidth }}>
       {fieldId}
     </Field>
   )
