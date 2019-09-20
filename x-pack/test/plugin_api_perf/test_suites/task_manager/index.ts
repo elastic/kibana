e3743c3c7d0e57bfe07eb27065f7c572347c4c17
Gidi Morris <github@gidi.io>
2019-09-20 14:27:24 +0100

    test(task=manager): Added comment explaining the reason for theeeeeee test being marked as skip

--- x-pack/test/plugin_api_perf/test_suites/task_manager/index.ts
+++ x-pack/test/plugin_api_perf/test_suites/task_manager/index.ts
@@ -5,6 +5,14 @@
  */
 
 export default function({ loadTestFile }: { loadTestFile: (file: string) => void }) {
+  /*
+   * This test provides a test framework for spawning multiple concurrent tasks in
+   * Task Manager and measuring how well Task Manager performs in claiming, processing
+   * and finalising these tasks.
+   *
+   * We keep this test disabled as it is used on an ad hoc basis and we feel it is
+   * worth keeping around for future use, rather than being rewritten time and time again.
+   */
   describe.skip('task_manager_perf', function taskManagerSuite() {
     this.tags('ciGroup2');
     loadTestFile(require.resolve('./task_manager_perf_integration'));
