0ab2e20d64f2d4a0a5ac082e1f30b4ed5372f0f6
John Dorlus <jsdorlus@elastic.co>
2019-09-20 20:49:26 -0400

    Reverting the script changes. Did not work.

--- test/scripts/jenkins_build_kibana.sh
+++ test/scripts/jenkins_build_kibana.sh
@@ -6,7 +6,7 @@ echo " -> downloading es snapshot"
 node scripts/es snapshot --license=oss --download-only;
 
 echo " -> Ensuring all functional tests are in a ciGroup"
-# yarn run grunt functionalTests:ensureAllTestsInCiGroup;
+yarn run grunt functionalTests:ensureAllTestsInCiGroup;
 
 echo " -> building and extracting OSS Kibana distributable for use in functional tests"
 node scripts/build --debug --oss
