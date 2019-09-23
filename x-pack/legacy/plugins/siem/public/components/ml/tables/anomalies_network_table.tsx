39933e1e4f609f39098e4fcee4b559879e81ada4
Patryk Kopyciński <contact@patrykkopycinski.com>
2019-09-23 12:27:01 +0200

    Revert changes to anomalies_network_table

--- x-pack/legacy/plugins/siem/public/components/ml/tables/anomalies_network_table.tsx
+++ x-pack/legacy/plugins/siem/public/components/ml/tables/anomalies_network_table.tsx
@@ -5,7 +5,7 @@
  */
 
 import { EuiPanel } from '@elastic/eui';
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { useAnomaliesTableData } from '../anomaly/use_anomalies_table_data';
 import { HeaderPanel } from '../../header_panel';
@@ -17,6 +17,8 @@ import { AnomaliesNetworkTableProps } from '../types';
 import { getAnomaliesNetworkTableColumnsCurated } from './get_anomalies_network_table_columns';
 import { getIntervalFromAnomalies } from '../anomaly/get_interval_from_anomalies';
 import { getSizeFromAnomalies } from '../anomaly/get_size_from_anomalies';
+import { hasMlUserPermissions } from '../permissions/has_ml_user_permissions';
+import { MlCapabilitiesContext } from '../permissions/ml_capabilities_provider';
 import { BasicTable } from './basic_table';
 import { networkEquality } from './network_equality';
 import { getCriteriaFromNetworkType } from '../criteria/get_criteria_from_network_type';
@@ -30,6 +32,7 @@ const sorting = {
 
 export const AnomaliesNetworkTable = React.memo<AnomaliesNetworkTableProps>(
   ({ startDate, endDate, narrowDateRange, skip, ip, type, flowTarget }): JSX.Element | null => {
+    const capabilities = useContext(MlCapabilitiesContext);
     const [loading, tableData] = useAnomaliesTableData({
       startDate,
       endDate,
@@ -54,29 +57,33 @@ export const AnomaliesNetworkTable = React.memo<AnomaliesNetworkTableProps>(
       hidePerPageOptions: false,
     };
 
-    return (
-      <Panel loading={{ loading }}>
-        <HeaderPanel
-          subtitle={`${i18n.SHOWING}: ${pagination.totalItemCount.toLocaleString()} ${i18n.UNIT(
-            pagination.totalItemCount
-          )}`}
-          title={i18n.ANOMALIES}
-          tooltip={i18n.TOOLTIP}
-        />
+    if (!hasMlUserPermissions(capabilities)) {
+      return null;
+    } else {
+      return (
+        <Panel loading={{ loading }}>
+          <HeaderPanel
+            subtitle={`${i18n.SHOWING}: ${pagination.totalItemCount.toLocaleString()} ${i18n.UNIT(
+              pagination.totalItemCount
+            )}`}
+            title={i18n.ANOMALIES}
+            tooltip={i18n.TOOLTIP}
+          />
 
-        <BasicTable
-          columns={columns}
-          compressed
-          items={networks}
-          pagination={pagination}
-          sorting={sorting}
-        />
+          <BasicTable
+            columns={columns}
+            compressed
+            items={networks}
+            pagination={pagination}
+            sorting={sorting}
+          />
 
-        {loading && (
-          <Loader data-test-subj="anomalies-network-table-loading-panel" overlay size="xl" />
-        )}
-      </Panel>
-    );
+          {loading && (
+            <Loader data-test-subj="anomalies-network-table-loading-panel" overlay size="xl" />
+          )}
+        </Panel>
+      );
+    }
   },
   networkEquality
 );
