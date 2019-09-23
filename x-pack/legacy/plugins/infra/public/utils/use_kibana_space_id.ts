801420e3720534249f2523033c1521da67c1ac76
Felix Stürmer <stuermer@weltenwort.de>
2019-09-23 20:05:31 +0200

    Adapt to older io-ts version in this branch

--- x-pack/legacy/plugins/infra/public/utils/use_kibana_space_id.ts
+++ x-pack/legacy/plugins/infra/public/utils/use_kibana_space_id.ts
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { fold } from 'fp-ts/lib/Either';
-import { pipe } from 'fp-ts/lib/pipeable';
 import * as rt from 'io-ts';
 
 import { useKibanaInjectedVar } from './use_kibana_injected_var';
@@ -13,10 +11,9 @@ import { useKibanaInjectedVar } from './use_kibana_injected_var';
 export const useKibanaSpaceId = (): string => {
   const activeSpace = useKibanaInjectedVar('activeSpace');
 
-  return pipe(
-    activeSpaceRT.decode(activeSpace),
-    fold(() => 'default', decodedActiveSpace => decodedActiveSpace.space.id)
-  );
+  return activeSpaceRT
+    .decode(activeSpace)
+    .fold(() => 'default', decodedActiveSpace => decodedActiveSpace.space.id);
 };
 
 const activeSpaceRT = rt.type({
