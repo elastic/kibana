7c612b1f364f4e84843ee4a38e686b56675f19fe
shahzad <shahzad31comp@gmail.com>
2019-09-23 15:48:59 +0500

    update expander row

--- x-pack/legacy/plugins/uptime/public/components/functional/monitor_list/monitor_list_drawer.tsx
+++ x-pack/legacy/plugins/uptime/public/components/functional/monitor_list/monitor_list_drawer.tsx
@@ -5,13 +5,26 @@
  */
 
 import React, { useContext } from 'react';
-import { EuiText, EuiHealth, EuiLink, EuiSpacer } from '@elastic/eui';
+import {
+  EuiText,
+  EuiHealth,
+  EuiLink,
+  EuiSpacer,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButton,
+} from '@elastic/eui';
+import { get } from 'lodash';
+import styled from 'styled-components';
 import { MonitorSummary, CheckMonitor } from '../../../../common/graphql/types';
-import { CheckList } from './check_list';
 import { toCondensedCheck } from './to_condensed_check';
 import { CondensedCheckList } from './condensed_check_list';
 import { CLIENT_DEFAULTS } from '../../../../common/constants';
 import { UptimeSettingsContext } from '../../../contexts';
+
+const ContainerDiv = styled.div`
+  padding: 10px;
+`;
 
 interface MonitorListDrawerProps {
   summary?: MonitorSummary;
@@ -37,22 +50,35 @@ export const MonitorListDrawer = ({ summary }: MonitorListDrawerProps) => {
       downLocations.push(check.monitor);
     }
   });
-  if (summary.state.checks.length < CLIENT_DEFAULTS.CONDENSED_CHECK_LIMIT) {
+  const displayMonitorStatus = (locations: CheckMonitor[], color: string) => {
     return (
       <>
-        <EuiLink href={summary.state.url.full}>{summary.state.url.full}</EuiLink>
-        <EuiSpacer size="s" />
-        <EuiText>
-          <EuiHealth color={danger} />
-          Down in {downLocations.map(location => (location.name || location.ip) + ',')}
+        <EuiText size="s">
+          <EuiHealth color={color} />
+          Up in{' '}
+          {locations.map((location, index) => (index ? ', ' : '') + (location.name || location.ip))}
         </EuiText>
         <EuiSpacer size="s" />
-        <EuiText>
-          <EuiHealth color={success} />
-          Up in {upLocations.map(location => (location.name || location.ip) + ',')}
-        </EuiText>
-        {/* <CheckList checks={summary.state.checks} /> */}
       </>
+    );
+  };
+  if (summary.state.checks.length < CLIENT_DEFAULTS.CONDENSED_CHECK_LIMIT) {
+    const monitorUrl: string | undefined = get(summary.state.url, 'full', undefined);
+
+    return (
+      <ContainerDiv>
+        <EuiFlexGroup>
+          <EuiFlexItem>
+            <EuiLink href={monitorUrl}>{monitorUrl}</EuiLink>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiButton size="s">Actions</EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiSpacer size="s" />
+        {downLocations.length > 0 && displayMonitorStatus(downLocations, danger)}
+        {upLocations.length > 0 && displayMonitorStatus(upLocations, success)}
+      </ContainerDiv>
     );
   } else {
     return <CondensedCheckList condensedChecks={toCondensedCheck(summary.state.checks)} />;
