79f0b09a27a9e9125f96b06f78ab97f7cf9de779
spalger <email@spalger.com>
2019-09-24 17:11:20 -0700

    move to @hapi org packages

--- packages/eslint-config-kibana/.eslintrc.js
+++ packages/eslint-config-kibana/.eslintrc.js
@@ -26,6 +26,50 @@ module.exports = {
           from: 'x-pack',
           toRelative: 'x-pack',
         },
+        {
+          from: 'boom',
+          to: '@hapi/boom',
+        },
+        {
+          from: 'good-squeeze',
+          to: '@hapi/good-squeeze',
+        },
+        {
+          from: 'h2o2',
+          to: '@hapi/h2o2',
+        },
+        {
+          from: 'hapi',
+          to: '@hapi/hapi',
+        },
+        {
+          from: 'hapi-auth-cookie',
+          to: '@hapi/cookie',
+        },
+        {
+          from: 'hoek',
+          to: '@hapi/hoek',
+        },
+        {
+          from: 'joi',
+          toRelative: '@hapi/joi',
+        },
+        {
+          from: 'inert',
+          to: '@hapi/inert',
+        },
+        {
+          from: 'oppsy',
+          to: '@hapi/oppsy',
+        },
+        {
+          from: 'podium',
+          to: '@hapi/podium',
+        },
+        {
+          from: 'vision',
+          to: '@hapi/vision',
+        }
       ],
     ],
   }
