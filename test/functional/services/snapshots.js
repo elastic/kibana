0c33a88240ebf182a95219e2cb31f011af747462
Spencer <email@spalger.com>
2019-09-26 13:41:42 -0700

    [7.3] Fix disappearing screenshots in CI (#46382) (#46666)
    # Conflicts:
    #	test/functional/services/failure_debugging.ts
    #	test/functional/services/screenshots.ts
    #	test/functional/services/snapshots.ts

--- test/functional/services/snapshots.js
+++ test/functional/services/snapshots.js
@@ -32,7 +32,10 @@ export async function SnapshotsProvider({ getService }) {
 
   const SESSION_DIRECTORY = resolve(config.get('snapshots.directory'), 'session');
   const BASELINE_DIRECTORY = resolve(config.get('snapshots.directory'), 'baseline');
-  await del([SESSION_DIRECTORY]);
+
+  if (process.env.CI !== 'true') {
+    await del([SESSION_DIRECTORY]);
+  }
 
   class Snapshots {
 
