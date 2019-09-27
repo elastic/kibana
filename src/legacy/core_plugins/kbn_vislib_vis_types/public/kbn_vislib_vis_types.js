0eed9a869269b14ab08dc94c7058437668951908
Matt Kime <matt@mattki.me>
2019-09-27 10:46:23 -0500

    fix merge mistake

--- src/legacy/core_plugins/kbn_vislib_vis_types/public/kbn_vislib_vis_types.js
+++ src/legacy/core_plugins/kbn_vislib_vis_types/public/kbn_vislib_vis_types.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import { setup as visualizations } from '../../visualizations/public/legacy';
 
 import histogramVisTypeProvider from './histogram';
 import lineVisTypeProvider from './line';
