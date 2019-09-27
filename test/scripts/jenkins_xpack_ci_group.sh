84247b3c81da87dc6623a8fc298d047b0657527e
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2019-09-27 18:44:05 +0300

    Revert "run ciGroup12 30x times"
    This reverts commit 08fac11139a92f7b4e5e27d4fe2e2d23d34aa802.

--- test/scripts/jenkins_xpack_ci_group.sh
+++ test/scripts/jenkins_xpack_ci_group.sh
@@ -13,17 +13,17 @@ export TEST_BROWSER_HEADLESS=1
 if [[ -z "$IS_PIPELINE_JOB" ]] ; then
   echo " -> Ensuring all functional tests are in a ciGroup"
   cd "$XPACK_DIR"
-  # node scripts/functional_tests --assert-none-excluded \
-  #   --include-tag ciGroup1 \
-  #   --include-tag ciGroup2 \
-  #   --include-tag ciGroup3 \
-  #   --include-tag ciGroup4 \
-  #   --include-tag ciGroup5 \
-  #   --include-tag ciGroup6 \
-  #   --include-tag ciGroup7 \
-  #   --include-tag ciGroup8 \
-  #   --include-tag ciGroup9 \
-  #   --include-tag ciGroup10
+  node scripts/functional_tests --assert-none-excluded \
+    --include-tag ciGroup1 \
+    --include-tag ciGroup2 \
+    --include-tag ciGroup3 \
+    --include-tag ciGroup4 \
+    --include-tag ciGroup5 \
+    --include-tag ciGroup6 \
+    --include-tag ciGroup7 \
+    --include-tag ciGroup8 \
+    --include-tag ciGroup9 \
+    --include-tag ciGroup10
 fi
 
 cd "$KIBANA_DIR"
