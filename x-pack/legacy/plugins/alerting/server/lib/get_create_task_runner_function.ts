522eaa902343b61e991406d35b26cd668cd0bc0b
Mike Cote <mikecote@users.noreply.github.com>
2019-09-27 13:38:35 -0400

    Rename variable in task runner

--- x-pack/legacy/plugins/alerting/server/lib/get_create_task_runner_function.ts
+++ x-pack/legacy/plugins/alerting/server/lib/get_create_task_runner_function.ts
@@ -74,7 +74,7 @@ export function getCreateTaskRunnerFunction({
         const services = getServices(fakeRequest);
         // Ensure API key is still valid and user has access
         const {
-          attributes: { alertTypeParams, actions, interval, throttle, muted, mutedInstanceIds },
+          attributes: { alertTypeParams, actions, interval, throttle, muteAll, mutedInstanceIds },
           references,
         } = await services.savedObjectsClient.get<RawAlert>('alert', alertId);
 
@@ -128,7 +128,7 @@ export function getCreateTaskRunnerFunction({
           Object.keys(alertInstances).map(alertInstanceId => {
             const alertInstance = alertInstances[alertInstanceId];
             if (alertInstance.hasScheduledActions(throttle)) {
-              if (muted || mutedInstanceIds.includes(alertInstanceId)) {
+              if (muteAll || mutedInstanceIds.includes(alertInstanceId)) {
                 return;
               }
               const { actionGroup, context, state } = alertInstance.getSechduledActionOptions()!;
