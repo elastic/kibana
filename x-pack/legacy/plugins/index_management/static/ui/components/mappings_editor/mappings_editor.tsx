a17b470da7cf6cd12344b589fe68a21fe922337a
Sébastien <sebastien.loix@elastic.co>
2019-09-27 11:11:57 +0200

    Reset before refactor to v3

--- x-pack/legacy/plugins/index_management/static/ui/components/mappings_editor/mappings_editor.tsx
+++ x-pack/legacy/plugins/index_management/static/ui/components/mappings_editor/mappings_editor.tsx
@@ -4,14 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useEffect, useRef, useCallback } from 'react';
+import React from 'react';
+// import React, { useEffect, useRef, useCallback } from 'react';
 
-import {
-  ConfigurationForm,
-  PropertiesProvider,
-  DocumentFields,
-  DocumentFieldsState,
-} from './components';
+// import {
+//   ConfigurationForm,
+//   PropertiesProvider,
+//   DocumentFields,
+//   DocumentFieldsState,
+// } from './components';
 
 interface Props {
   setGetDataHandler: (handler: GetMappingsDataHandler) => void;
@@ -20,7 +21,7 @@ interface Props {
 }
 
 type GetMappingsDataHandler = () => Promise<{ isValid: boolean; mappings: Mappings }>;
-type GetConfigFormDataHandler = () => Promise<{ isValid: boolean; data: Record<string, unknown> }>;
+// type GetConfigFormDataHandler = () => Promise<{ isValid: boolean; data: Record<string, unknown> }>;
 
 export interface State {
   isValid: boolean;
@@ -32,49 +33,51 @@ export type Mappings = Record<string, any>;
 
 export const MappingsEditor = React.memo(
   ({ setGetDataHandler, onStateUpdate, defaultValue = {} }: Props) => {
-    const properties = useRef<Record<string, unknown>>({});
-    const getConfigurationFormData = useRef<GetConfigFormDataHandler>(() =>
-      Promise.resolve({ isValid: true, data: defaultValue })
-    );
+    // const properties = useRef<Record<string, unknown>>({});
+    // const getConfigurationFormData = useRef<GetConfigFormDataHandler>(() =>
+    //   Promise.resolve({ isValid: true, data: defaultValue })
+    // );
 
-    useEffect(() => {
-      setGetDataHandler(async () => {
-        const { isValid, data: configFormData } = await getConfigurationFormData.current();
+    // useEffect(() => {
+    //   setGetDataHandler(async () => {
+    //     const { isValid, data: configFormData } = await getConfigurationFormData.current();
 
-        return { isValid, mappings: { ...configFormData, properties: properties.current } };
-      });
-    }, []);
+    //     return { isValid, mappings: { ...configFormData, properties: properties.current } };
+    //   });
+    // }, []);
 
-    const setGetConfigurationFormDataHandler = useCallback((handler: GetConfigFormDataHandler) => {
-      getConfigurationFormData.current = handler;
-    }, []);
+    // const setGetConfigurationFormDataHandler = useCallback((handler: GetConfigFormDataHandler) => {
+    //   getConfigurationFormData.current = handler;
+    // }, []);
 
-    const onConfigFormValidityChange = useCallback(
-      (isValid: boolean) => onStateUpdate({ isValid }),
-      []
-    );
+    // const onConfigFormValidityChange = useCallback(
+    //   (isValid: boolean) => onStateUpdate({ isValid }),
+    //   []
+    // );
 
-    const onDocumentFieldsUpdate = useCallback((docFieldsState: DocumentFieldsState) => {
-      properties.current = docFieldsState.properties;
+    // const onDocumentFieldsUpdate = useCallback((docFieldsState: DocumentFieldsState) => {
+    //   properties.current = docFieldsState.properties;
 
-      onStateUpdate({
-        isEditingProperty: docFieldsState.isEditing,
-      });
-    }, []);
+    //   onStateUpdate({
+    //     isEditingProperty: docFieldsState.isEditing,
+    //   });
+    // }, []);
 
     return (
       <div className="mappings-editor">
         {/* Global Mappings configuration */}
-        <ConfigurationForm
+        <h2>Global configuration</h2>
+        {/* <ConfigurationForm
           setGetDataHandler={setGetConfigurationFormDataHandler}
           onValidityChange={onConfigFormValidityChange}
           defaultValue={defaultValue}
-        />
+        /> */}
 
         {/* Document fields */}
-        <PropertiesProvider defaultValue={defaultValue.properties}>
+        <h2>Document Fields</h2>
+        {/* <PropertiesProvider defaultValue={defaultValue.properties}>
           <DocumentFields onUpdate={onDocumentFieldsUpdate} />
-        </PropertiesProvider>
+        </PropertiesProvider> */}
       </div>
     );
   }
