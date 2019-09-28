2d9d3c0999c08ae7640af9afe02f27085a119485
Mengwei Ding <mengwei.ding@gmail.com>
2019-09-27 17:01:35 -0700

    remove joi config

--- x-pack/legacy/plugins/code/index.ts
+++ x-pack/legacy/plugins/code/index.ts
@@ -5,16 +5,12 @@
  */
 
 import { RequestQuery, ResponseToolkit, RouteOptions, ServerRoute } from 'hapi';
-import JoiNamespace from 'joi';
 import { Legacy } from 'kibana';
-import moment from 'moment';
 import { resolve } from 'path';
 import { CoreSetup } from 'src/core/server';
 
 import { APP_TITLE } from './common/constants';
 import { codePlugin } from './server';
-import { DEFAULT_WATERMARK_LOW_PERCENTAGE } from './server/disk_watermark';
-import { LanguageServers, LanguageServersDeveloping } from './server/lsp/language_servers';
 
 export type RequestFacade = Legacy.Request;
 export type RequestQueryFacade = RequestQuery;
@@ -44,86 +40,6 @@ export const code = (kibana: any) =>
         };
       },
       hacks: ['plugins/code/hacks/toggle_app_link_in_nav'],
-    },
-    config(Joi: typeof JoiNamespace) {
-      const langSwitches: any = {};
-      LanguageServers.forEach(lang => {
-        langSwitches[lang.name] = Joi.object({
-          enabled: Joi.boolean().default(true),
-        });
-      });
-      LanguageServersDeveloping.forEach(lang => {
-        langSwitches[lang.name] = Joi.object({
-          enabled: Joi.boolean().default(false),
-        });
-      });
-      return Joi.object({
-        ui: Joi.object({
-          enabled: Joi.boolean().default(true),
-        }).default(),
-        enabled: Joi.boolean().default(true),
-        queueIndex: Joi.string().default('.code_internal-worker-queue'),
-        // 1 hour by default.
-        queueTimeoutMs: Joi.number().default(moment.duration(1, 'hour').asMilliseconds()),
-        // The frequency which update scheduler executes. 1 minute by default.
-        updateFrequencyMs: Joi.number().default(moment.duration(1, 'minute').asMilliseconds()),
-        // The frequency which index scheduler executes. 1 day by default.
-        indexFrequencyMs: Joi.number().default(moment.duration(1, 'day').asMilliseconds()),
-        // The frequency which each repo tries to update. 5 minutes by default.
-        updateRepoFrequencyMs: Joi.number().default(moment.duration(5, 'minute').asMilliseconds()),
-        // The frequency which each repo tries to index. 1 day by default.
-        indexRepoFrequencyMs: Joi.number().default(moment.duration(1, 'day').asMilliseconds()),
-        // whether we want to show more logs
-        verbose: Joi.boolean().default(false),
-        lsp: Joi.object({
-          ...langSwitches,
-          // timeout of a request
-          requestTimeoutMs: Joi.number().default(moment.duration(10, 'second').asMilliseconds()),
-          // if we want the language server run in seperately
-          detach: Joi.boolean().default(false),
-          // enable oom_score_adj on linux
-          oomScoreAdj: Joi.boolean().default(true),
-        }).default(),
-        repos: Joi.array().default([]),
-        security: Joi.object({
-          enableMavenImport: Joi.boolean().default(true),
-          enableGradleImport: Joi.boolean().default(false),
-          installGoDependency: Joi.boolean().default(false),
-          installNodeDependency: Joi.boolean().default(true),
-          gitHostWhitelist: Joi.array()
-            .items(Joi.string())
-            .default([
-              'github.com',
-              'gitlab.com',
-              'bitbucket.org',
-              'gitbox.apache.org',
-              'eclipse.org',
-            ]),
-          gitProtocolWhitelist: Joi.array()
-            .items(Joi.string())
-            .default(['https', 'git', 'ssh']),
-          enableGitCertCheck: Joi.boolean().default(true),
-        }).default(),
-        disk: Joi.object({
-          thresholdEnabled: Joi.bool().default(true),
-          watermarkLow: Joi.string().default(`${DEFAULT_WATERMARK_LOW_PERCENTAGE}%`),
-        }).default(),
-        maxWorkspace: Joi.number().default(5), // max workspace folder for each language server
-        enableGlobalReference: Joi.boolean().default(false), // Global reference as optional feature for now
-        enableCommitIndexing: Joi.boolean().default(false),
-        codeNodeUrl: Joi.string(),
-        clustering: Joi.object({
-          enabled: Joi.bool().default(false),
-          codeNodes: Joi.array()
-            .items(
-              Joi.object({
-                id: Joi.string(),
-                address: Joi.string(),
-              })
-            )
-            .default([]),
-        }).default(),
-      }).default();
     },
     async init(server: ServerFacade) {
       // @ts-ignore
