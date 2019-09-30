ca3f1fe66b6dd49215d6e507d45ded2d4f22672b
sulemanof <sul-dan@mail.ru>
2019-09-30 18:34:01 +0300

    Remove unused translations

--- src/legacy/core_plugins/vis_type_metric/public/components/metric_vis_options.tsx
+++ src/legacy/core_plugins/vis_type_metric/public/components/metric_vis_options.tsx
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { EuiPanel, EuiSpacer, EuiTitle } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
