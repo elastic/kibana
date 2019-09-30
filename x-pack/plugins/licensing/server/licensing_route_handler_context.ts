c328b4ef0327ea01698c781d08466545086a498a
Josh Dover <me@joshdover.com>
2019-09-30 13:38:09 -0500

    Update licensing context types

--- x-pack/plugins/licensing/server/licensing_route_handler_context.ts
+++ x-pack/plugins/licensing/server/licensing_route_handler_context.ts
@@ -4,14 +4,14 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { RequestHandlerContext, IContextProvider } from 'src/core/server';
+import { IContextProvider, RequestHandler } from 'src/core/server';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { ILicense } from './types';
 
 export function createRouteHandlerContext(
   license$: Observable<ILicense>
-): IContextProvider<RequestHandlerContext, 'licensing'> {
+): IContextProvider<RequestHandler<any, any, any>, 'licensing'> {
   return async function licensingRouteHandlerContext() {
     const license = await license$.pipe(take(1)).toPromise();
     return { license };
