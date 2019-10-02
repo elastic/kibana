184d09abc5c11a580334f801a710a2d19413dde5
Flávio Franco Knob <ffknob@gmail.com>
2019-10-02 11:46:39 -0300

    Changed the 'local variables' style in Stacktrace to use Eui, and not anymore the inline styling. It looks a little different now, I choose to get rid of the "VariablesToggle" and use EuiAccordion instead.

--- x-pack/legacy/plugins/apm/public/components/shared/Stacktrace/Variables.tsx
+++ x-pack/legacy/plugins/apm/public/components/shared/Stacktrace/Variables.tsx
@@ -4,10 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import {
-  EuiSpacer,
-  EuiAccordion
-} from '@elastic/eui';
+import { EuiSpacer, EuiAccordion } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
 import { IStackframe } from '../../../../typings/es_schemas/raw/fields/Stackframe';
@@ -32,8 +29,11 @@ export class Variables extends React.Component<Props> {
 
     return (
       <React.Fragment>
-        <EuiSpacer/>
-        <EuiAccordion id="local-variables" buttonContent={ this.localVariablesToogleButtonLabel() }>       
+        <EuiSpacer />
+        <EuiAccordion
+          id="local-variables"
+          buttonContent={this.localVariablesToogleButtonLabel()}
+        >
           <React.Fragment>
             <DottedKeyValueTable data={this.props.vars} maxDepth={5} />
           </React.Fragment>
