29b599c3626473494eb66c5fc04ef676ec31b2be
Caue Marcondes <caue.marcondes@elastic.co>
2019-10-02 14:56:17 +0200

    Changing status code colors on trace summary

--- x-pack/legacy/plugins/apm/public/components/shared/Summary/HttpInfoSummaryItem/__test__/HttpInfoSummaryItem.test.tsx
+++ x-pack/legacy/plugins/apm/public/components/shared/Summary/HttpInfoSummaryItem/__test__/HttpInfoSummaryItem.test.tsx
@@ -6,16 +6,16 @@
 
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { palettes } from '@elastic/eui';
-import { HttpInfoSummaryItem } from './';
-import * as exampleTransactions from '../__fixtures__/transactions';
+import theme from '@elastic/eui/dist/eui_theme_light.json';
+import { HttpInfoSummaryItem } from '../';
+import * as exampleTransactions from '../../__fixtures__/transactions';
 
 describe('HttpInfoSummaryItem', () => {
   describe('render', () => {
     const transaction = exampleTransactions.httpOk;
     const url = 'https://example.com';
     const method = 'get';
-    const props = { transaction, url, method, status: 200 };
+    const props = { transaction, url, method, status: 100 };
 
     it('renders', () => {
       expect(() =>
@@ -23,12 +23,23 @@ describe('HttpInfoSummaryItem', () => {
       ).not.toThrowError();
     });
 
-    describe('with status code 200', () => {
+    describe('with status code 100', () => {
       it('shows a success color', () => {
         const wrapper = mount(<HttpInfoSummaryItem {...props} />);
 
         expect(wrapper.find('HttpStatusBadge EuiBadge').prop('color')).toEqual(
-          palettes.euiPaletteForStatus.colors[0]
+          theme.euiColorDarkShade
+        );
+      });
+    });
+
+    describe('with status code 200', () => {
+      it('shows a success color', () => {
+        const p = { ...props, status: 200 };
+        const wrapper = mount(<HttpInfoSummaryItem {...p} />);
+
+        expect(wrapper.find('HttpStatusBadge EuiBadge').prop('color')).toEqual(
+          theme.euiColorSecondary
         );
       });
     });
@@ -40,7 +51,7 @@ describe('HttpInfoSummaryItem', () => {
         const wrapper = mount(<HttpInfoSummaryItem {...p} />);
 
         expect(wrapper.find('HttpStatusBadge EuiBadge').prop('color')).toEqual(
-          palettes.euiPaletteForStatus.colors[4]
+          theme.euiColorDarkShade
         );
       });
     });
@@ -52,7 +63,7 @@ describe('HttpInfoSummaryItem', () => {
         const wrapper = mount(<HttpInfoSummaryItem {...p} />);
 
         expect(wrapper.find('HttpStatusBadge EuiBadge').prop('color')).toEqual(
-          palettes.euiPaletteForStatus.colors[9]
+          theme.euiColorWarning
         );
       });
     });
@@ -64,7 +75,7 @@ describe('HttpInfoSummaryItem', () => {
         const wrapper = mount(<HttpInfoSummaryItem {...p} />);
 
         expect(wrapper.find('HttpStatusBadge EuiBadge').prop('color')).toEqual(
-          palettes.euiPaletteForStatus.colors[9]
+          theme.euiColorDanger
         );
       });
     });
