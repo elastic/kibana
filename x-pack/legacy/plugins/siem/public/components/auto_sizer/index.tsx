5efd2fc2d02532fb976ecd2baa926883be24a64e
Steph Milovic <stephanie.milovic@elastic.co>
2019-10-01 22:49:50 -0600

    revert autosizer to class

--- x-pack/legacy/plugins/siem/public/components/auto_sizer/index.tsx
+++ x-pack/legacy/plugins/siem/public/components/auto_sizer/index.tsx
@@ -5,7 +5,7 @@
  */
 
 import isEqual from 'lodash/fp/isEqual';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 
 interface Measurement {
@@ -20,155 +20,163 @@ interface Measurements {
 }
 
 interface AutoSizerProps {
+  detectAnyWindowResize?: boolean;
   bounds?: boolean;
+  content?: boolean;
+  onResize?: (size: Measurements) => void;
   children: (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     args: { measureRef: (instance: HTMLElement | null) => any } & Measurements
-  ) => React.ReactElement;
-  content?: boolean;
-  detectAnyWindowResize?: boolean;
-  onResize?: (size: Measurements) => void;
+  ) => React.ReactNode;
 }
 
-/**
- * Returning a new object reference guarantees that a before-and-after
- * equivalence check will always be false, resulting in a re-render, even
- * when multiple calls to forceUpdate are batched.
- */
-const useForceUpdate = () => {
-  const [, dispatch] = useState<{}>(Object.create(null));
-  const memoizedDispatch = useCallback((): void => {
-    dispatch(Object.create(null));
-  }, [dispatch]);
-  return memoizedDispatch;
-};
-export const AutoSizer = React.memo<AutoSizerProps>(
-  ({ bounds = false, children, content = true, detectAnyWindowResize, onResize }) => {
-    const element = useRef<HTMLElement | null>(null);
-    const resizeObserver = useRef<ResizeObserver | null>(null);
-    const windowWidth = useRef(-1);
-    const boundsMeasurement = useRef<Measurement>({
-      height: 0,
-      width: 0,
-    });
-    const contentMeasurement = useRef<Measurement>({
-      height: 0,
-      width: 0,
-    });
-    const windowMeasurement = useRef<Measurement>({
-      height: 0,
-      width: 0,
-    });
-    const forceUpdate = useForceUpdate();
+interface AutoSizerState {
+  boundsMeasurement: Measurement;
+  contentMeasurement: Measurement;
+  windowMeasurement: Measurement;
+}
 
-    const storeRef = (htmlElement: HTMLElement | null) => {
-      if (element.current && resizeObserver.current) {
-        resizeObserver.current.unobserve(element.current);
-      }
+/** A hard-fork of the `infra` `AutoSizer` ಠ_ಠ */
+export class AutoSizer extends React.PureComponent<AutoSizerProps, AutoSizerState> {
+  public element: HTMLElement | null = null;
+  public resizeObserver: ResizeObserver | null = null;
+  public windowWidth: number = -1;
 
-      if (htmlElement && resizeObserver.current) {
-        resizeObserver.current.observe(htmlElement);
-      }
+  public readonly state = {
+    boundsMeasurement: {
+      height: void 0,
+      width: void 0,
+    },
+    contentMeasurement: {
+      height: void 0,
+      width: void 0,
+    },
+    windowMeasurement: {
+      height: void 0,
+      width: void 0,
+    },
+  };
 
-      element.current = htmlElement;
-    };
-
-    const measure = (entry: ResizeObserverEntry | null) => {
-      if (!element.current) {
-        return;
-      }
-      const boundsRect = bounds ? element.current.getBoundingClientRect() : null;
-
-      const boundsMeasurementNow: Measurement = boundsRect
-        ? {
-            height: element.current.getBoundingClientRect().height,
-            width: element.current.getBoundingClientRect().width,
-          }
-        : boundsMeasurement.current;
-
-      const windowMeasurementNow: Measurement = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-
-      if (
-        detectAnyWindowResize &&
-        boundsMeasurementNow &&
-        boundsMeasurementNow.width &&
-        windowWidth.current !== -1 &&
-        windowWidth.current > window.innerWidth
-      ) {
-        const gap = windowWidth.current - window.innerWidth;
-        boundsMeasurementNow.width = boundsMeasurementNow.width - gap;
-      }
-      windowWidth.current = window.innerWidth;
-      const contentRect = content && entry ? entry.contentRect : null;
-
-      const contentMeasurementNow: Measurement =
-        contentRect && entry
-          ? {
-              height: entry.contentRect.height,
-              width: entry.contentRect.width,
-            }
-          : contentMeasurement.current;
-
-      if (
-        isEqual(boundsMeasurementNow, boundsMeasurement.current) &&
-        isEqual(contentMeasurementNow, contentMeasurement.current) &&
-        isEqual(windowMeasurementNow, windowMeasurement.current)
-      ) {
-        return;
-      }
-
-      requestAnimationFrame(() => {
-        if (!resizeObserver) {
-          return;
-        }
-
-        boundsMeasurement.current = boundsMeasurementNow;
-        contentMeasurement.current = contentMeasurementNow;
-        windowMeasurement.current = windowMeasurementNow;
-        forceUpdate();
-        if (onResize) {
-          onResize({
-            bounds: boundsMeasurementNow,
-            content: contentMeasurementNow,
-            windowMeasurement: windowMeasurementNow,
-          });
+  constructor(props: AutoSizerProps) {
+    super(props);
+    if (this.props.detectAnyWindowResize) {
+      window.addEventListener('resize', this.updateMeasurement);
+    }
+    this.resizeObserver = new ResizeObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.target === this.element) {
+          this.measure(entry);
         }
       });
-    };
-
-    useEffect(() => {
-      if (detectAnyWindowResize) {
-        window.addEventListener('resize', () => measure(null));
-      }
-      resizeObserver.current = new ResizeObserver(entries => {
-        entries.forEach(entry => {
-          if (entry.target === element.current) {
-            measure(entry);
-          }
-        });
-      });
-
-      return () => {
-        if (resizeObserver && resizeObserver.current) {
-          resizeObserver.current.disconnect();
-          resizeObserver.current = null;
-        }
-        if (detectAnyWindowResize) {
-          window.removeEventListener('resize', () => measure(null));
-        }
-      };
-    }, []);
-
-    return children({
-      bounds: boundsMeasurement.current,
-      content: contentMeasurement.current,
-      windowMeasurement: windowMeasurement.current,
-      measureRef: storeRef,
     });
   }
-);
 
-AutoSizer.displayName = 'AutoSizer';
+  public componentWillUnmount() {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+      this.resizeObserver = null;
+    }
+    if (this.props.detectAnyWindowResize) {
+      window.removeEventListener('resize', this.updateMeasurement);
+    }
+  }
+
+  public measure = (entry: ResizeObserverEntry | null) => {
+    if (!this.element) {
+      return;
+    }
+
+    const { content = true, bounds = false } = this.props;
+    const {
+      boundsMeasurement: previousBoundsMeasurement,
+      contentMeasurement: previousContentMeasurement,
+      windowMeasurement: previousWindowMeasurement,
+    } = this.state;
+
+    const boundsRect = bounds ? this.element.getBoundingClientRect() : null;
+    const boundsMeasurement = boundsRect
+      ? {
+          height: this.element.getBoundingClientRect().height,
+          width: this.element.getBoundingClientRect().width,
+        }
+      : previousBoundsMeasurement;
+    const windowMeasurement: Measurement = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    if (
+      this.props.detectAnyWindowResize &&
+      boundsMeasurement &&
+      boundsMeasurement.width &&
+      this.windowWidth !== -1 &&
+      this.windowWidth > window.innerWidth
+    ) {
+      const gap = this.windowWidth - window.innerWidth;
+      boundsMeasurement.width = boundsMeasurement.width - gap;
+    }
+    this.windowWidth = window.innerWidth;
+    const contentRect = content && entry ? entry.contentRect : null;
+    const contentMeasurement =
+      contentRect && entry
+        ? {
+            height: entry.contentRect.height,
+            width: entry.contentRect.width,
+          }
+        : previousContentMeasurement;
+
+    if (
+      isEqual(boundsMeasurement, previousBoundsMeasurement) &&
+      isEqual(contentMeasurement, previousContentMeasurement) &&
+      isEqual(windowMeasurement, previousWindowMeasurement)
+    ) {
+      return;
+    }
+
+    requestAnimationFrame(() => {
+      if (!this.resizeObserver) {
+        return;
+      }
+
+      this.setState({ boundsMeasurement, contentMeasurement, windowMeasurement });
+
+      if (this.props.onResize) {
+        this.props.onResize({
+          bounds: boundsMeasurement,
+          content: contentMeasurement,
+          windowMeasurement,
+        });
+      }
+    });
+  };
+
+  public render() {
+    const { children } = this.props;
+    const { boundsMeasurement, contentMeasurement, windowMeasurement } = this.state;
+
+    return children({
+      bounds: boundsMeasurement,
+      content: contentMeasurement,
+      windowMeasurement,
+      measureRef: this.storeRef,
+    });
+  }
+
+  private updateMeasurement = () => {
+    window.setTimeout(() => {
+      this.measure(null);
+    }, 0);
+  };
+
+  private storeRef = (element: HTMLElement | null) => {
+    if (this.element && this.resizeObserver) {
+      this.resizeObserver.unobserve(this.element);
+    }
+
+    if (element && this.resizeObserver) {
+      this.resizeObserver.observe(element);
+    }
+
+    this.element = element;
+  };
+}
