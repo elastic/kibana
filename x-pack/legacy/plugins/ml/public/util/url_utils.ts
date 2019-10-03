c5c35c538fe4a4c79d8aad0cb419bbe21819ac2f
Pete Harverson <pete@elastic.co>
2019-10-03 15:31:33 +0100

    [ML] Edits to url_utils following review

--- x-pack/legacy/plugins/ml/public/util/url_utils.ts
+++ x-pack/legacy/plugins/ml/public/util/url_utils.ts
@@ -9,13 +9,13 @@
  */
 
 /**
- *
- * @param {string} url - a relative or root relative url.  If a relative path is given then the
+ * Converts a relative URL to an absolute URL.
+ * @param url - a relative or root relative url.  If a relative path is given then the
  * absolute url returned will depend on the current page where this function is called from. For example
  * if you are on page "http://www.mysite.com/shopping/kids" and you pass this function "adults", you would get
  * back "http://www.mysite.com/shopping/adults".  If you passed this function a root relative path, or one that
  * starts with a "/", for example "/account/cart", you would get back "http://www.mysite.com/account/cart".
- * @return {string} the relative url transformed into an absolute url
+ * @returns the relative url transformed into an absolute url
  */
 export function relativeToAbsolute(url: string) {
   // convert all link urls to absolute urls
@@ -27,10 +27,10 @@ export function relativeToAbsolute(url: string) {
 /**
  * Returns whether the supplied String represents a web URL
  * i.e. whether it starts with http:// or https://
- * @param {string} url URL to test
- * @return {boolean} true if it starts with http:// or https://, false otherwise
+ * @param url URL to test
+ * @returns true if it starts with http:// or https://, false otherwise
  */
-export function isWebUrl(url: string) {
+export function isWebUrl(url: string): boolean {
   const absoluteUrl = relativeToAbsolute(url);
-  return absoluteUrl.startsWith('http://') || absoluteUrl.startsWith('https://');
+  return /^https?:\/\//.test(absoluteUrl);
 }
