c32984e63b2ac11ac4feb295132b07d85713b01d
Dario Gieselaar <dario.gieselaar@elastic.co>
2019-10-03 17:41:45 +0200

    [APM] JVM List view & JVM metrics page (#46779)
    * [APM] JVM List view & JVM metrics page
    
    Closes #43765.
    
    * Add service node metadata
    
    * Use service node terminology
    
    * Use asPercent for CPU
    
    * Tooltip for truncated elements
    
    * Add processor.event filters
    
    * Use service.node.name
    
    * Sort client-side only
    
    * Add processor.event filter to service nodes projection

--- x-pack/legacy/plugins/apm/common/projections/service_nodes.ts
+++ x-pack/legacy/plugins/apm/common/projections/service_nodes.ts
@@ -5,23 +5,21 @@
  */
 
 import { Setup } from '../../server/lib/helpers/setup_request';
-import { SERVICE_NAME, PROCESSOR_EVENT } from '../elasticsearch_fieldnames';
+import {
+  SERVICE_NAME,
+  SERVICE_NODE_NAME,
+  PROCESSOR_EVENT
+} from '../elasticsearch_fieldnames';
 import { rangeFilter } from '../../server/lib/helpers/range_filter';
 
-export function getMetricsProjection({
+export function getServiceNodesProjection({
   setup,
-  serviceName,
-  serviceNodeName
+  serviceName
 }: {
   setup: Setup;
   serviceName: string;
-  serviceNodeName?: string;
 }) {
   const { start, end, uiFiltersES, config } = setup;
-
-  const serviceNodeNameFilters = serviceNodeName
-    ? [{ term: { [SERVICE_NAME]: serviceName } }]
-    : [];
 
   return {
     index: config.get<string>('apm_oss.metricsIndices'),
@@ -30,13 +28,17 @@ export function getMetricsProjection({
         bool: {
           filter: [
             { term: { [SERVICE_NAME]: serviceName } },
-            ...serviceNodeNameFilters,
             { term: { [PROCESSOR_EVENT]: 'metric' } },
-            {
-              range: rangeFilter(start, end)
-            },
+            { range: rangeFilter(start, end) },
             ...uiFiltersES
           ]
+        }
+      },
+      aggs: {
+        nodes: {
+          terms: {
+            field: SERVICE_NODE_NAME
+          }
         }
       }
     }
