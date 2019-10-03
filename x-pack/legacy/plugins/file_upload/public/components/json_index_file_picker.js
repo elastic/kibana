13dead5f5b4aedb1cae27bf92ba52ba2aa7ace07
Aaron Caldwell <aaron.caldwell@elastic.co>
2019-10-03 07:24:11 -0600

    Revert "Fix issue where subsequent index name is wiped out when previous file cancelled" (covered in separate PR)
    This reverts commit 0688e73ffce56c062a8b21ba27bf59d1f5ebfc7f.

--- x-pack/legacy/plugins/file_upload/public/components/json_index_file_picker.js
+++ x-pack/legacy/plugins/file_upload/public/components/json_index_file_picker.js
@@ -48,10 +48,27 @@ export class JsonIndexFilePicker extends Component {
       return;
     }
     const file = fileArr[0];
+    let initIndexName;
+    try {
+      initIndexName = this._getIndexName(file);
+    } catch (error) {
+      this.setState({
+        fileUploadError: i18n.translate('xpack.fileUpload.jsonIndexFilePicker.errorGettingIndexName', {
+          defaultMessage: 'Error retrieving index name: {errorMessage}',
+          values: {
+            errorMessage: error.message
+          }
+        })
+      });
+      return;
+    }
+
+    this.props.setIndexName(initIndexName);
+
     this.setState({ fileParseActive: true }, () => this._parseFile(file));
   };
 
-  _getDefaultIndexName({ name, size }) {
+  _getIndexName({ name, size }) {
     if (!name) {
       throw new Error(i18n.translate('xpack.fileUpload.jsonIndexFilePicker.noFileNameError', {
         defaultMessage: 'No file name provided'
@@ -81,25 +98,6 @@ export class JsonIndexFilePicker extends Component {
 
     return splitNameArr[0];
   }
-
-  _setIndexName(file) {
-    let initIndexName;
-    try {
-      initIndexName = this._getDefaultIndexName(file);
-    } catch (error) {
-      this.setState({
-        fileUploadError: i18n.translate('xpack.fileUpload.jsonIndexFilePicker.errorGettingIndexName', {
-          defaultMessage: 'Error retrieving index name: {errorMessage}',
-          values: {
-            errorMessage: error.message
-          }
-        })
-      });
-      return;
-    }
-    this.props.setIndexName(initIndexName);
-  }
-
 
   setFileProgress = ({ bytesProcessed, totalBytes }) => {
     const percentageProcessed = parseInt((100 * bytesProcessed) / totalBytes);
@@ -145,13 +143,9 @@ export class JsonIndexFilePicker extends Component {
       resetFileAndIndexSettings();
       return;
     }
-
-    this._setIndexName(file);
     setFileRef(file);
     setParsedFile(parsedFileResult);
   }
-
-
 
   render() {
     const {
