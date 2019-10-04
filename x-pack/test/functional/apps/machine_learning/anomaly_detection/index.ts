a2f63aa573188a7ff19c361930d97b4e19e39205
Robert Oskamp <robert.oskamp@elastic.co>
2019-10-04 14:39:53 +0200

    Add saved search based job creation tests

--- x-pack/test/functional/apps/machine_learning/anomaly_detection/index.ts
+++ x-pack/test/functional/apps/machine_learning/anomaly_detection/index.ts
@@ -10,5 +10,6 @@ export default function({ loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./single_metric_job'));
     loadTestFile(require.resolve('./multi_metric_job'));
     loadTestFile(require.resolve('./population_job'));
+    loadTestFile(require.resolve('./saved_search_job'));
   });
 }
