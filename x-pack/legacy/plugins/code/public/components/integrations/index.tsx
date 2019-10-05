67d026e77f6ab0b16af4fd7ce96c6f92483b5d39
Ryland Herrick <ryalnd@gmail.com>
2019-10-04 20:26:55 -0500

    [Code] Code Integrator Component (#47180)
    * Add CodeIntegrator component
    
    * Abstracts shared ImportModal component
    * Uses placeholder data and callbacks in lieu of real integration

--- x-pack/legacy/plugins/code/public/components/integrations/index.tsx
+++ x-pack/legacy/plugins/code/public/components/integrations/index.tsx
@@ -11,11 +11,17 @@ import { CodeBlock } from '../codeblock/codeblock';
 import { history } from '../../utils/url';
 import { FrameHeader } from './frame_header';
 import { RepoTitle } from './repo_title';
+import { CodeIntegrator } from './code_integrator';
 import { externalFileURI } from './helpers';
-import { frames, results } from './data';
+import { frames, Frame, repos, results } from './data';
+
+const associateToService = (frame: Frame) => (repo: string) =>
+  alert(`repo ${repo} associated with service ${JSON.stringify(frame)}`);
+
+const handleImport = (repo: string) => alert(`import done: ${repo}`);
 
 export const Integrations = () => (
-  <div className="codeContainer__root integrations__container">
+  <div className="codeContainer__root codeIntegrations__container">
     {frames.map(frame => {
       const { fileName, lineNumber } = frame;
       const key = `${fileName}#L${lineNumber}`;
@@ -27,7 +33,7 @@ export const Integrations = () => (
         const fileUrl = externalFileURI(uri, filePath);
 
         return (
-          <div key={key} className="integrations__frame">
+          <div key={key} className="codeIntegrations__frame">
             <RepoTitle uri={snippet.uri} />
             <CodeBlock
               content={content}
@@ -46,13 +52,18 @@ export const Integrations = () => (
       }
 
       return (
-        <div key={key} className="integrations__frame">
+        <div key={key} className="codeIntegrations__frame">
           <EuiFlexGroup justifyContent="spaceBetween" gutterSize="none" alignItems="center">
-            <EuiText size="s" className="integrations__code">
+            <EuiText size="s" className="codeIntegrations__code">
               <span>{fileName}</span>
-              <span className="integrations__preposition">at</span>
+              <span className="codeIntegrations__preposition">at</span>
               <span>line {lineNumber}</span>
             </EuiText>
+            <CodeIntegrator
+              onRepoSelect={associateToService(frame)}
+              onImportSuccess={handleImport}
+              repos={repos}
+            />
           </EuiFlexGroup>
         </div>
       );
