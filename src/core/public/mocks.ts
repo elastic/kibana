4f4b0946d18accd05f962c52cb0ba57319e6a283
Nathan L Smith <smith@nlsmith.com>
2019-10-07 09:26:24 -0500

    revert mocks change

--- src/core/public/mocks.ts
+++ src/core/public/mocks.ts
@@ -18,12 +18,11 @@
  */
 import { applicationServiceMock } from './application/application_service.mock';
 import { chromeServiceMock } from './chrome/chrome_service.mock';
-import { CoreSetup, PluginInitializerContext, CoreStart } from '.';
+import { CoreSetup, CoreStart, PluginInitializerContext } from '.';
 import { docLinksServiceMock } from './doc_links/doc_links_service.mock';
 import { fatalErrorsServiceMock } from './fatal_errors/fatal_errors_service.mock';
 import { httpServiceMock } from './http/http_service.mock';
 import { i18nServiceMock } from './i18n/i18n_service.mock';
-import { injectedMetadataServiceMock } from './injected_metadata/injected_metadata_service.mock';
 import { notificationServiceMock } from './notifications/notifications_service.mock';
 import { overlayServiceMock } from './overlays/overlay_service.mock';
 import { uiSettingsServiceMock } from './ui_settings/ui_settings_service.mock';
