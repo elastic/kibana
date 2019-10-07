6a00f78159215539118b5dead1601933804df4e4
Nathan Reese <reese.nathan@gmail.com>
2019-10-07 13:46:37 -0600

    [Maps] avoid resizing tooltip while loading next feature

--- x-pack/legacy/plugins/maps/public/connected_components/map/feature_properties.js
+++ x-pack/legacy/plugins/maps/public/connected_components/map/feature_properties.js
@@ -13,7 +13,6 @@ import {
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
-
 export class FeatureProperties extends React.Component {
 
   state = {
@@ -55,6 +54,14 @@ export class FeatureProperties extends React.Component {
       properties: undefined,
       loadPropertiesErrorMsg: undefined,
     });
+
+    // Preserve current properties width/height so they can be used while rendering loading indicator.
+    if (this.state.properties && this.node) {
+      this.setState({
+        prevWidth: this.node.clientWidth,
+        prevHeight: this.node.clientHeight
+      });
+    }
 
     let properties;
     try {
@@ -130,8 +137,15 @@ export class FeatureProperties extends React.Component {
       const loadingMsg = i18n.translate('xpack.maps.tooltip.loadingMsg', {
         defaultMessage: 'Loading'
       });
+      // Use width/height of last viewed properties while displaying loading status
+      // to avoid resizing component during loading phase and bouncing tooltip container around
+      const style = {};
+      if (this.state.prevWidth && this.state.prevHeight) {
+        style.width = this.state.prevWidth;
+        style.height = this.state.prevHeight;
+      }
       return (
-        <EuiTextAlign textAlign="center">
+        <EuiTextAlign textAlign="center" style={style}>
           <EuiLoadingSpinner size="m"/>
           {loadingMsg}
         </EuiTextAlign>
@@ -161,7 +175,12 @@ export class FeatureProperties extends React.Component {
     });
 
     return (
-      <table className="mapFeatureTooltip_table">
+      <table
+        className="mapFeatureTooltip_table"
+        ref={node => {
+          this.node = node;
+        }}
+      >
         <tbody>
           {rows}
         </tbody>
