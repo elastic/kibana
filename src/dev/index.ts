ef6727cc52ce7de5afab6937d36540199b29bc0e
Spencer <email@spalger.com>
2019-10-06 22:49:28 -0700

    [7.x] Update gulp related packages (major) (#46665 and #47421) (#47409)
    * Update gulp related packages (major) (#46665)
    
    * Update gulp related packages
    
    * ts-ify and gulp4-ify x-pack tasks, remove unused canvas tasks
    
    * remove unnecessary gulp.TaskFunction usage
    
    * fix old references
    
    * update renovate config
    
    * move constants into helpers directory
    
    * typo
    
    * compact tasks a bit, remove unnecessary paths
    
    * fix build directories
    
    * deprecate testonly task
    
    * rather than justifying an unjustifiable ts-ignore, ts-ify the imported module
    
    * update renovate config
    
    * update browser download tests to mock axios
    
    * add root index.d.ts to tsconfig
    
    * export BrowserType
    
    * remove unnecessary `@ts-ignore`
    
    * use consistent casing
    
    * correct import for createAutoJUnitReporter
    
    * Update gulp related packages (#47421)
    
    (cherry picked from commit 35751f9828ecf4be855d1cbe6dc2021e55c2830e)

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -17,18 +17,9 @@
  * under the License.
  */
 
-export { runTestsCli, startServersCli } from './functional_tests/cli';
-
-export { runTests, startServers } from './functional_tests/tasks';
-
-export { OPTIMIZE_BUNDLE_DIR, KIBANA_ROOT } from './functional_tests/lib/paths';
-
-export { esTestConfig, createEsTestCluster } from './es';
-
-export { kbnTestConfig, kibanaServerTestUser, kibanaTestUser, adminTestUser } from './kbn';
-
-export { setupUsers, DEFAULT_SUPERUSER_PASS } from './functional_tests/lib/auth';
-
-export { readConfigFile } from './functional_test_runner/lib/config/read_config_file';
-
-export { runFtrCli } from './functional_test_runner/cli';
+// @ts-ignore not typed yet
+export { createAutoJUnitReporter } from './auto_junit_reporter';
+// @ts-ignore not typed yet
+export { setupJUnitReportGeneration } from './junit_report_generation';
+// @ts-ignore not typed yet
+export { runMochaCli } from './run_mocha_cli';
--- x-pack/legacy/plugins/reporting/server/browsers/download/util.ts
+++ x-pack/legacy/plugins/reporting/server/browsers/download/util.ts
@@ -4,33 +4,30 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { Readable } from 'stream';
+
 /**
  * Log a message if the DEBUG environment variable is set
- * @param  {...any} args
- * @return {undefined}
  */
-export function log(...args) {
+export function log(...args: any[]) {
   if (process.env.DEBUG) {
+    // allow console log since this is off by default and only for debugging
+    // eslint-disable-next-line no-console
     console.log(...args);
   }
 }
 
 /**
  * Iterate an array asynchronously and in parallel
- * @param  {Array} array
- * @param  {Function} asyncFn
- * @return {Promise}
  */
-export function asyncMap(array, asyncFn) {
+export function asyncMap<T, T2>(array: T[], asyncFn: (x: T) => T2): Promise<T2[]> {
   return Promise.all(array.map(asyncFn));
 }
 
 /**
  * Wait for a readable stream to end
- * @param  {Stream.Readable} stream
- * @return {Promise<undefined>}
  */
-export function readableEnd(stream) {
+export function readableEnd(stream: Readable) {
   return new Promise((resolve, reject) => {
     stream.on('error', reject).on('end', resolve);
   });
