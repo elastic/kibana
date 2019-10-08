aa7d66d4929d14e98dc48f7c9d940892ea5eb151
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2019-10-08 14:00:22 +0200

    run FF 20x times

--- test/scripts/jenkins_xpack_build_kibana.sh
+++ test/scripts/jenkins_xpack_build_kibana.sh
@@ -8,17 +8,17 @@ node scripts/es snapshot --download-only;
 
 echo " -> Ensuring all functional tests are in a ciGroup"
 cd "$XPACK_DIR"
-node scripts/functional_tests --assert-none-excluded \
-  --include-tag ciGroup1 \
-  --include-tag ciGroup2 \
-  --include-tag ciGroup3 \
-  --include-tag ciGroup4 \
-  --include-tag ciGroup5 \
-  --include-tag ciGroup6 \
-  --include-tag ciGroup7 \
-  --include-tag ciGroup8 \
-  --include-tag ciGroup9 \
-  --include-tag ciGroup10
+# node scripts/functional_tests --assert-none-excluded \
+#   --include-tag ciGroup1 \
+#   --include-tag ciGroup2 \
+#   --include-tag ciGroup3 \
+#   --include-tag ciGroup4 \
+#   --include-tag ciGroup5 \
+#   --include-tag ciGroup6 \
+#   --include-tag ciGroup7 \
+#   --include-tag ciGroup8 \
+#   --include-tag ciGroup9 \
+#   --include-tag ciGroup10
 
 echo " -> building and extracting default Kibana distributable for use in functional tests"
 cd "$KIBANA_DIR"
