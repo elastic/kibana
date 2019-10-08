9559186482c0b1c106af7ac03f779cd5b645d354
Flávio Knob <ffknob@gmail.com>
2019-10-08 08:30:47 -0300

    brought back some of the styling code so it looks more like how it used to be

--- x-pack/legacy/plugins/apm/public/components/shared/Stacktrace/Variables.tsx
+++ x-pack/legacy/plugins/apm/public/components/shared/Stacktrace/Variables.tsx
@@ -4,11 +4,28 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiSpacer, EuiAccordion } from '@elastic/eui';
+import theme from '@elastic/eui/dist/eui_theme_light.json';
+import styled from 'styled-components';
+import { EuiHorizontalRule, EuiAccordion } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
+import {
+  borderRadius,
+  fontFamily,
+  px,
+  unit,
+  units
+} from '../../../style/variables';
 import { IStackframe } from '../../../../typings/es_schemas/raw/fields/Stackframe';
 import { DottedKeyValueTable } from '../DottedKeyValueTable';
+
+const VariablesContainer = styled.div`
+  background: ${theme.euiColorEmptyShade};
+  border-top: 1px solid ${theme.euiColorLightShade};
+  border-radius: 0 0 ${borderRadius} ${borderRadius};
+  padding: ${px(units.half)} ${px(unit)};
+  font-family: ${fontFamily};
+`;
 
 interface Props {
   vars: IStackframe['vars'];
@@ -22,18 +39,20 @@ export class Variables extends React.Component<Props> {
 
     return (
       <React.Fragment>
-        <EuiSpacer />
-        <EuiAccordion
-          id="local-variables"
-          buttonContent={i18n.translate(
-            'xpack.apm.stacktraceTab.localVariablesToogleButtonLabel',
-            { defaultMessage: 'Local variables' }
-          )}
-        >
-          <React.Fragment>
-            <DottedKeyValueTable data={this.props.vars} maxDepth={5} />
-          </React.Fragment>
-        </EuiAccordion>
+        <EuiHorizontalRule margin="none" />
+        <VariablesContainer>
+          <EuiAccordion
+            id="local-variables"
+            buttonContent={i18n.translate(
+              'xpack.apm.stacktraceTab.localVariablesToogleButtonLabel',
+              { defaultMessage: 'Local variables' }
+            )}
+          >
+            <React.Fragment>
+              <DottedKeyValueTable data={this.props.vars} maxDepth={5} />
+            </React.Fragment>
+          </EuiAccordion>
+        </VariablesContainer>
       </React.Fragment>
     );
   }
