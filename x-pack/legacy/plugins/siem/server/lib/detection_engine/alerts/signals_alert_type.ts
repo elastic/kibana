149cbd210ba2d8da38c3c3a838eceb0bf2e31360
Devin Hurley <devin.hurley@elastic.co>
2019-10-09 12:16:35 -0400

    fix leftover merge conflicts from rebase

--- x-pack/legacy/plugins/siem/server/lib/detection_engine/alerts/signals_alert_type.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/alerts/signals_alert_type.ts
@@ -19,191 +19,191 @@ import { buildEventsScrollQuery } from './build_events_query';
 // search interface
 import { SearchResponse, SearchHit, SignalHit } from '../../types';
 
-export const signalsAlertType: AlertType = {
-  id: SIGNALS_ID,
-  name: 'SIEM Signals',
-  actionGroups: ['default'],
-  validate: {
-    params: schema.object({
-      description: schema.string(),
-      from: schema.string(),
-      filter: schema.maybe(schema.object({}, { allowUnknowns: true })),
-      id: schema.number(),
-      index: schema.arrayOf(schema.string()),
-      kql: schema.maybe(schema.string({ defaultValue: undefined })),
-      maxSignals: schema.number({ defaultValue: 100 }),
-      name: schema.string(),
-      severity: schema.number(),
-      to: schema.string(),
-      type: schema.string(),
-      references: schema.arrayOf(schema.string(), { defaultValue: [] }),
-      scrollSize: schema.maybe(schema.number()),
-      scrollLock: schema.maybe(schema.string()),
-    }),
-  },
-  // TODO: Type the params as it is all filled with any
-  async executor({ services, params, state }: AlertExecutorOptions) {
-    const instance = services.alertInstanceFactory('siem-signals');
+export const signalsAlertType = ({ logger }: { logger: Logger }): AlertType => {
+  return {
+    id: SIGNALS_ID,
+    name: 'SIEM Signals',
+    actionGroups: ['default'],
+    validate: {
+      params: schema.object({
+        description: schema.string(),
+        from: schema.string(),
+        filter: schema.maybe(schema.object({}, { allowUnknowns: true })),
+        id: schema.number(),
+        index: schema.arrayOf(schema.string()),
+        kql: schema.maybe(schema.string({ defaultValue: undefined })),
+        maxSignals: schema.number({ defaultValue: 100 }),
+        name: schema.string(),
+        severity: schema.number(),
+        to: schema.string(),
+        type: schema.string(),
+        references: schema.arrayOf(schema.string(), { defaultValue: [] }),
+        scrollSize: schema.maybe(schema.number()),
+        scrollLock: schema.maybe(schema.string()),
+      }),
+    },
+    // TODO: Type the params as it is all filled with any
+    async executor({ services, params, state }: AlertExecutorOptions) {
+      const instance = services.alertInstanceFactory('siem-signals');
 
-    const {
-      description,
-      filter,
-      from,
-      id,
-      index,
-      kql,
-      maxSignals,
-      name,
-      references,
-      severity,
-      to,
-      type,
-      scrollSize,
-      scrollLock,
-    } = params;
-
-    const indexPatterns = index.map((element: string) => `"${element}"`).join(',');
-    const refs = references.map((element: string) => `"${element}"`).join(',');
-    const scroll = scrollLock ? scrollLock : '1m';
-    const size = scrollSize ? scrollSize : 400;
-
-    // TODO: Turn these options being sent in into a template for the alert type
-    const noReIndex = buildEventsScrollQuery({
-      index,
-      from,
-      to,
-      kql,
-      filter,
-      size,
-      scroll,
-    });
-
-    const reIndex = buildEventsReIndex({
-      index,
-      from,
-      kql,
-      to,
-      // TODO: Change this out once we have solved
-      // https://github.com/elastic/kibana/issues/47002
-      signalsIndex: process.env.SIGNALS_INDEX || '.siem-signals-10-01-2019',
-      severity,
-      description,
-      name,
-      timeDetected: Date.now(),
-      filter,
-      maxDocs: maxSignals,
-      ruleRevision: 1,
-      id,
-      type,
-      references,
-    });
-
-    // format scroll search result for signals index.
-    const buildBulkBody = (doc: SearchHit): SignalHit => ({
-      ...doc._source,
-      signal: {
-        rule_revision: 1,
-        rule_id: id,
-        rule_type: type,
-        parent: {
-          id: doc._id,
-          type: 'event',
-          depth: 1,
-        },
+      const {
+        description,
+        filter,
+        from,
+        id,
+        index,
+        kql,
+        maxSignals,
         name,
+        references,
+        severity,
+        to,
+        type,
+        scrollSize,
+        scrollLock,
+      } = params;
+
+      const indexPatterns = index.map((element: string) => `"${element}"`).join(',');
+      const refs = references.map((element: string) => `"${element}"`).join(',');
+      const scroll = scrollLock ? scrollLock : '1m';
+      const size = scrollSize ? scrollSize : 400;
+
+      // TODO: Turn these options being sent in into a template for the alert type
+      const noReIndex = buildEventsScrollQuery({
+        index,
+        from,
+        to,
+        kql,
+        filter,
+        size,
+        scroll,
+      });
+
+      const reIndex = buildEventsReIndex({
+        index,
+        from,
+        kql,
+        to,
+        // TODO: Change this out once we have solved
+        // https://github.com/elastic/kibana/issues/47002
+        signalsIndex: process.env.SIGNALS_INDEX || '.siem-signals-10-01-2019',
         severity,
         description,
-        time_detected: Date.now(),
-        index_patterns: indexPatterns,
-        references: refs,
-      },
-    });
-
-    async function bulkIndex(someResult: SearchResponse<object>): Promise<boolean> {
-      services.log(['info', 'SIEM'], '[+] starting bulk insertion');
-      const bulkBody = someResult.hits.hits.flatMap((doc: SearchHit) => [
-        {
-          index: { _index: process.env.SIGNALS_INDEX || '.siem-signals-10-01-2019', _id: doc._id },
-        },
-        buildBulkBody(doc),
-      ]);
-      await services.callCluster('bulk', {
-        refresh: true,
-        body: bulkBody,
+        name,
+        timeDetected: Date.now(),
+        filter,
+        maxDocs: maxSignals,
+        ruleRevision: 1,
+        id,
+        type,
+        references,
       });
-      while (true) {
-        let nextScrollResult;
-        try {
-          nextScrollResult = await services.callCluster('scroll', {
-            scroll,
-            scrollId: someResult._scroll_id,
-          });
-          if (nextScrollResult.hits.hits.length === 0) {
-            // reached end of scroll
-            services.log(['info', 'SIEM'], `Returning concatenated result`);
-            return true;
+
+      // format scroll search result for signals index.
+      const buildBulkBody = (doc: SearchHit): SignalHit => ({
+        ...doc._source,
+        signal: {
+          rule_revision: 1,
+          rule_id: id,
+          rule_type: type,
+          parent: {
+            id: doc._id,
+            type: 'event',
+            depth: 1,
+          },
+          name,
+          severity,
+          description,
+          time_detected: Date.now(),
+          index_patterns: indexPatterns,
+          references: refs,
+        },
+      });
+
+      async function bulkIndex(someResult: SearchResponse<object>): Promise<boolean> {
+        logger.info('[+] starting bulk insertion');
+        const bulkBody = someResult.hits.hits.flatMap((doc: SearchHit) => [
+          {
+            index: {
+              _index: process.env.SIGNALS_INDEX || '.siem-signals-10-01-2019',
+              _id: doc._id,
+            },
+          },
+          buildBulkBody(doc),
+        ]);
+        await services.callCluster('bulk', {
+          refresh: true,
+          body: bulkBody,
+        });
+        while (true) {
+          let nextScrollResult;
+          try {
+            nextScrollResult = await services.callCluster('scroll', {
+              scroll,
+              scrollId: someResult._scroll_id,
+            });
+            if (nextScrollResult.hits.hits.length === 0) {
+              // reached end of scroll
+              logger.info(`Returning concatenated result`);
+              return true;
+            }
+          } catch (exc) {
+            logger.info(`[-] nextScroll threw an error ${JSON.stringify(exc)}`);
           }
-        } catch (exc) {
-          services.log(['error', 'SIEM'], `[-] nextScroll threw an error ${JSON.stringify(exc)}`);
-        }
-        let bulkResponse;
-        try {
-          bulkResponse = await services.callCluster('bulk', {
-            refresh: true,
-            body: nextScrollResult.hits.hits.flatMap((doc: SearchHit) => [
-              {
-                index: {
-                  _index: process.env.SIGNALS_INDEX || '.siem-signals-10-01-2019',
-                  _id: doc._id,
+          let bulkResponse;
+          try {
+            bulkResponse = await services.callCluster('bulk', {
+              refresh: true,
+              body: nextScrollResult.hits.hits.flatMap((doc: SearchHit) => [
+                {
+                  index: {
+                    _index: process.env.SIGNALS_INDEX || '.siem-signals-10-01-2019',
+                    _id: doc._id,
+                  },
                 },
-              },
-              buildBulkBody(doc),
-            ]),
-          });
-        } catch (exc) {
-          services.log(['error', 'SIEM'], `[-] bulkResponse threw an error ${JSON.stringify(exc)}`);
-        }
-        if (bulkResponse.errors) {
-          services.log(
-            ['error', 'SIEM'],
-            `[-] bulkResponse had errors: ${JSON.stringify(bulkResponse.errors, null, 2)}}`
-          );
-          return false;
+                buildBulkBody(doc),
+              ]),
+            });
+          } catch (exc) {
+            logger.error(`[-] bulkResponse threw an error ${JSON.stringify(exc)}`);
+          }
+          if (bulkResponse.errors) {
+            logger.error(
+              `[-] bulkResponse had errors: ${JSON.stringify(bulkResponse.errors, null, 2)}}`
+            );
+            return false;
+          }
         }
       }
-    }
 
-    try {
-      services.log(['info', 'SIEM'], 'Starting SIEM signal job');
+      try {
+        logger.info('Starting SIEM signal job');
 
-      // TODO: Comment this in eventually and use this for manual insertion of the
-      // signals instead of the ReIndex() api
+        // TODO: Comment this in eventually and use this for manual insertion of the
+        // signals instead of the ReIndex() api
 
-      if (process.env.USE_SCROLL_BULK_INDEX === 'USE_SCROLL_BULK_INDEX') {
-        services.log(['info', 'SIEM'], `[+] Initial search call`);
-        const noReIndexResult = await services.callCluster('search', noReIndex);
-        services.log(
-          ['info', 'SIEM'],
-          `Total docs to reindex: ${noReIndexResult.hits.total.value}`
-        );
-        const bulkIndexResult = await bulkIndex(noReIndexResult);
-        if (bulkIndexResult) {
-          services.log(['info', 'SIEM'], 'Finished SIEM signal job');
+        if (process.env.USE_SCROLL_BULK_INDEX === 'USE_SCROLL_BULK_INDEX') {
+          logger.info(`[+] Initial search call`);
+          const noReIndexResult = await services.callCluster('search', noReIndex);
+          logger.info(`Total docs to reindex: ${noReIndexResult.hits.total.value}`);
+          const bulkIndexResult = await bulkIndex(noReIndexResult);
+          if (bulkIndexResult) {
+            logger.info('Finished SIEM signal job');
+          } else {
+            logger.info('Error processing SIEM signal job');
+          }
         } else {
-          services.log(['error', 'SIEM'], 'Error processing SIEM signal job');
-        }
-      } else {
-        // eslint-disable-next-line
-        const result = await services.callCluster('reindex', reIndex);
+          // eslint-disable-next-line
+          const result = await services.callCluster('reindex', reIndex);
 
-        // TODO: Error handling here and writing of any errors that come back from ES by
-        services.log(['info', 'SIEM'], `Result of reindex: ${JSON.stringify(result, null, 2)}`);
+          // TODO: Error handling here and writing of any errors that come back from ES by
+          logger.info(`Result of reindex: ${JSON.stringify(result, null, 2)}`);
+        }
+      } catch (err) {
+        // TODO: Error handling and writing of errors into a signal that has error
+        // handling/conditions
+        logger.info(`You encountered an error of: ${err.message}`);
       }
-    } catch (err) {
-      // TODO: Error handling and writing of errors into a signal that has error
-      // handling/conditions
-      services.log(['error', 'SIEM'], `You encountered an error of: ${err.message}`);
-    }
 
       // Schedule the default action which is nothing if it's a plain signal.
       instance.scheduleActions('default');
