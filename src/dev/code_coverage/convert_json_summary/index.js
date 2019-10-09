3bae19a4a0da457278b7c7f9a37aba72684d23eb
Tre' Seymour <wayne.seymour@elastic.co>
2019-10-09 12:10:58 -0600

    Print total

--- src/dev/code_coverage/convert_json_summary/index.js
+++ src/dev/code_coverage/convert_json_summary/index.js
@@ -1,11 +1,24 @@
 import { resolve } from 'path';
+import { createReadStream } from 'fs';
 
 console.log('\n### Converting json-summary output to elastic search import format');
 
 const kibanaRoot = resolve(__dirname, '../../../..');
 const origCoveragePath = resolve(kibanaRoot, './target/kibana-coverage/mocha/coverage-summary.json');
-console.log(`\n### origCoveragePath: \n\t${origCoveragePath}`);
+// console.log(`\n### origCoveragePath: \n\t${origCoveragePath}`);
+// const origCoverage = require(origCoveragePath);
+// console.log(`\n### origCoverage: \n\t${origCoverage}`);
 
-const origCoverage = require(origCoveragePath);
+import { fromEventPattern } from 'rxjs';
+import { map, mergeAll, mergeMap } from 'rxjs/operators';
+import Oboe from 'oboe';
 
-console.log(`\n### origCoverage: \n\t${origCoverage}`);
+
+// create another oboe instance
+Oboe(createReadStream(origCoveragePath))
+  .on('node', {
+    '!.total': total => {
+      console.log(`\n### Total Coverage: \n\t${JSON.stringify(total, null, 2)}`);
+    },
+  })
+  .on('done', () => console.log('\n### done'));
