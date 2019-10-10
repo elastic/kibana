3c5c4b0d2ba49c7bf945399a34a1541a0cc323d1
Poff Poffenberger <poffdeluxe@gmail.com>
2019-10-10 09:43:43 -0500

    Fixing weird merge

--- x-pack/legacy/plugins/canvas/i18n/errors.ts
+++ x-pack/legacy/plugins/canvas/i18n/errors.ts
@@ -8,7 +8,6 @@ import { i18n } from '@kbn/i18n';
 import { CANVAS, JSON } from './constants';
 
 export const ErrorStrings = {
-
   actionsElements: {
     getInvalidArgIndexErrorMessage: (index: string) =>
       i18n.translate('xpack.canvas.error.actionsElements.invaludArgIndexErrorMessage', {
@@ -64,20 +63,6 @@ export const ErrorStrings = {
           JSON,
         },
       }),
-    getDeleteFailureErrorMessage: () =>
-      i18n.translate('xpack.canvas.error.workpadLoader.deleteFailureErrorMessage', {
-        defaultMessage: `Couldn't delete all workpads`,
-      }),
-    getFindFailureErrorMessage: () =>
-      i18n.translate('xpack.canvas.error.workpadLoader.findFailureErrorMessage', {
-        defaultMessage: `Couldn't find workpad`,
-      }),
-    getUploadFailureErrorMessage: () =>
-      i18n.translate('xpack.canvas.error.workpadLoader.uploadFailureErrorMessage', {
-        defaultMessage: `Couldn't upload workpad`,
-      }),
-  },
-  WorkpadFileUpload: {
     getFileUploadFailureWithFileNameErrorMessage: (fileName: string) =>
       i18n.translate('xpack.canvas.errors.workpadUpload.fileUploadFileWithFileNameErrorMessage', {
         defaultMessage: `Couldn't upload '{fileName}'`,
@@ -92,13 +77,6 @@ export const ErrorStrings = {
           defaultMessage: `Couldn't upload file`,
         }
       ),
-    getAcceptJSONOnlyErrorMessage: () =>
-      i18n.translate('xpack.canvas.error.workpadUpload.acceptJSONOnlyErrorMessage', {
-        defaultMessage: 'Only {JSON} files are accepted',
-        values: {
-          JSON,
-        },
-      }),
     getMissingPropertiesErrorMessage: () =>
       i18n.translate('xpack.canvas.error.workpadUpload.missingPropertiesErrorMessage', {
         defaultMessage:
