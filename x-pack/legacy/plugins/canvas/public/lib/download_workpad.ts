3632bd62f40aadf5ba2fdd12ca0776d36d89ce05
Poff Poffenberger <poffdeluxe@gmail.com>
2019-10-10 09:28:30 -0500

    [Canvas] i18n for all known canvas errors (#46945)
    * i18n for all known canvas errors
    
    * Removing error message not seen by users

--- x-pack/legacy/plugins/canvas/public/lib/download_workpad.ts
+++ x-pack/legacy/plugins/canvas/public/lib/download_workpad.ts
@@ -4,15 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import fileSaver from 'file-saver';
+import { ErrorStrings } from '../../i18n';
+// @ts-ignore untyped local
 import { notify } from './notify';
+// @ts-ignore untyped local
 import * as workpadService from './workpad_service';
 
-export const downloadWorkpad = async workpadId => {
+const { downloadWorkpad: strings } = ErrorStrings;
+
+export const downloadWorkpad = async (workpadId: string) => {
   try {
     const workpad = await workpadService.get(workpadId);
     const jsonBlob = new Blob([JSON.stringify(workpad)], { type: 'application/json' });
     fileSaver.saveAs(jsonBlob, `canvas-workpad-${workpad.name}-${workpad.id}.json`);
   } catch (err) {
-    notify.error(err, { title: `Couldn't download workpad` });
+    notify.error(err, { title: strings.getDownloadFailureErrorMessage() });
   }
 };
