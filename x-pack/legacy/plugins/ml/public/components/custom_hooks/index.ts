7512ed8ddfc27537f2273e25df9778556df9ec65
Dima Arnautov <dmitrii.arnautov@elastic.co>
2019-10-10 12:14:12 +0200

    [ML] Converts Recognizer job page to React (#47429)
    * [ML] wip recognize job
    
    * [ML] error handling
    
    * [ML] layout enhancements
    
    * [ML] errors handling
    
    * [ML] fix badges alignment and error message
    
    * [ML] components split
    
    * [ML] load module from the component, reset support
    
    * [ML] validators
    
    * [ML] check view route and resolver
    
    * [ML] validation
    
    * [ML] remove angular code
    
    * [ML] change layout, fix patterns
    
    * [ML] job response fix
    
    * [ML] directive test
    
    * [ML] directive test
    
    * [ML] remove lookbehind regexp
    
    * [ML] fix IE11 layout issues
    
    * [ML] remove form tag, refactor to formState
    
    * [ML] aria labels, remove unused i18n
    
    * [ML] align kibana objects and reset button
    
    * [ML] column layout for jobs response
    
    * [ML] align icons and buttons
    
    * [ML] check existing kibana objects
    
    * [ML] don't display kibana objects panel if empty
    
    * [ML] result and reset button order
    
    * [ML] add EuiHorizontalRule
    
    * [ML] use time range and full index dat
    
    * [ML] no check icon for existing objects
    
    * [ML] toast for exception during a jobs setup
    
    * [ML] disable panels growth, advanced settings with described form
    
    * [ML] fix timeRange for results url
    
    * [ML] fix i18n
    
    * [ML] use EuiSwitch
    
    * [ML] PR remarks
    
    * [ML] job settings form component
    
    * [ML] i18n
    
    * [ML] onChange fix
    
    * [ML] custom hook for partial state update
    
    * [ML] jobGroups update
    
    * [ML] PR remarks
    
    * [ML] fix imports

--- x-pack/legacy/plugins/ml/public/components/custom_hooks/index.ts
+++ x-pack/legacy/plugins/ml/public/components/custom_hooks/index.ts
@@ -4,9 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-
-
-import './create_job_controller';
-import './create_job_service';
-import 'plugins/ml/services/mapping_service';
-import 'plugins/ml/components/job_group_select';
+export { usePartialState } from './use_partial_state';
