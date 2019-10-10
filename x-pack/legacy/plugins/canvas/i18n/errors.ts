296903473e5001c89d44eb372672bfb5875d9281
Poff Poffenberger <poffdeluxe@gmail.com>
2019-10-10 09:28:30 -0500

    [Canvas] i18n for all known canvas errors (#46945)
    * i18n for all known canvas errors
    
    * Removing error message not seen by users

--- x-pack/legacy/plugins/canvas/i18n/errors.ts
+++ x-pack/legacy/plugins/canvas/i18n/errors.ts
@@ -8,10 +8,61 @@ import { i18n } from '@kbn/i18n';
 import { CANVAS, JSON } from './constants';
 
 export const ErrorStrings = {
-  WorkpadLoader: {
-    getCloneFailureErrorMessage: () =>
-      i18n.translate('xpack.canvas.error.workpadLoader.cloneFailureErrorMessage', {
-        defaultMessage: `Couldn't clone workpad`,
+
+  actionsElements: {
+    getInvalidArgIndexErrorMessage: (index: string) =>
+      i18n.translate('xpack.canvas.error.actionsElements.invaludArgIndexErrorMessage', {
+        defaultMessage: 'Invalid argument index: {index}',
+        values: {
+          index,
+        },
+      }),
+  },
+  downloadWorkpad: {
+    getDownloadFailureErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.downloadWorkpad.downloadFailureErrorMessage', {
+        defaultMessage: "Couldn't download workpad",
+      }),
+  },
+  esPersist: {
+    getSaveFailureTitle: () =>
+      i18n.translate('xpack.canvas.error.esPersist.saveFailureTitle', {
+        defaultMessage: "Couldn't save your changes to Elasticsearch",
+      }),
+    getTooLargeErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.esPersist.tooLargeErrorMessage', {
+        defaultMessage:
+          'The server gave a response that the workpad data was too large. This usually means uploaded image assets that are too large for Kibana or a proxy. Try removing some assets in the asset manager.',
+      }),
+    getUpdateFailureTitle: () =>
+      i18n.translate('xpack.canvas.error.esPersist.updateFailureTitle', {
+        defaultMessage: "Couldn't update workpad",
+      }),
+  },
+  esService: {
+    getDefaultIndexFetchErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.esService.defaultIndexFetchErrorMessage', {
+        defaultMessage: "Couldn't fetch default index",
+      }),
+    getFieldsFetchErrorMessage: (index: string) =>
+      i18n.translate('xpack.canvas.error.esService.fieldsFetchErrorMessage', {
+        defaultMessage: "Couldn't fetch Elasticsearch fields for '{index}'",
+        values: {
+          index,
+        },
+      }),
+    getIndicesFetchErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.esService.indicesFetchErrorMessage', {
+        defaultMessage: "Couldn't fetch Elasticsearch indices",
+      }),
+  },
+  WorkpadFileUpload: {
+    getAcceptJSONOnlyErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.workpadUpload.acceptJSONOnlyErrorMessage', {
+        defaultMessage: 'Only {JSON} files are accepted',
+        values: {
+          JSON,
+        },
       }),
     getDeleteFailureErrorMessage: () =>
       i18n.translate('xpack.canvas.error.workpadLoader.deleteFailureErrorMessage', {
@@ -56,6 +107,34 @@ export const ErrorStrings = {
           CANVAS,
           JSON,
         },
+      }),
+  },
+  WorkpadLoader: {
+    getCloneFailureErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.workpadLoader.cloneFailureErrorMessage', {
+        defaultMessage: `Couldn't clone workpad`,
+      }),
+    getDeleteFailureErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.workpadLoader.deleteFailureErrorMessage', {
+        defaultMessage: `Couldn't delete all workpads`,
+      }),
+    getFindFailureErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.workpadLoader.findFailureErrorMessage', {
+        defaultMessage: `Couldn't find workpad`,
+      }),
+    getUploadFailureErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.workpadLoader.uploadFailureErrorMessage', {
+        defaultMessage: `Couldn't upload workpad`,
+      }),
+  },
+  workpadRoutes: {
+    getCreateFailureErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.workpadRoutes.createFailureErrorMessage', {
+        defaultMessage: "Couldn't create workpad",
+      }),
+    getLoadFailureErrorMessage: () =>
+      i18n.translate('xpack.canvas.error.workpadRoutes.loadFailureErrorMessage', {
+        defaultMessage: "Couldn't load workpad with ID",
       }),
   },
 };
