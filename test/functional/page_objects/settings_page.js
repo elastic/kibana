ca435ef6f79293bdc4f6d6baf508ecd0c0625d3c
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2019-10-10 12:32:27 +0200

    Add method to detect oss in settings pages
    - if oss the license management link is missing

--- test/functional/page_objects/settings_page.js
+++ test/functional/page_objects/settings_page.js
@@ -666,6 +666,10 @@ export function SettingsPageProvider({ getService, getPageObjects }) {
       await checkBox.click();
       return await this.canSavedObjectsBeDeleted();
     }
+    async isOss() {
+      const hasLicenceManagement = await testSubjects.exists('license_management');
+      return !hasLicenceManagement;
+    }
   }
 
   return new SettingsPage();
