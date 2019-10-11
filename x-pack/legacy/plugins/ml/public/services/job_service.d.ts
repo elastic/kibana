a3fd2572fd3b37ea09ef4e3f54cca86176c831d5
James Gowdy <jgowdy@elastic.co>
2019-10-11 17:07:17 +0100

    [ML] Adding datafeed preview to advanced wizard

--- x-pack/legacy/plugins/ml/public/services/job_service.d.ts
+++ x-pack/legacy/plugins/ml/public/services/job_service.d.ts
@@ -4,6 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { CombinedJob } from '../jobs/new_job_new/common/job_creator/configs';
+
 export interface ExistingJobsAndGroups {
   jobIds: string[];
   groupIds: string[];
@@ -31,6 +33,7 @@ declare interface JobService {
   ): Promise<any>;
   createResultsUrl(jobId: string[], start: number, end: number, location: string): string;
   getJobAndGroupIds(): ExistingJobsAndGroups;
+  searchPreview(job: CombinedJob): Promise<any>;
 }
 
 export const mlJobService: JobService;
