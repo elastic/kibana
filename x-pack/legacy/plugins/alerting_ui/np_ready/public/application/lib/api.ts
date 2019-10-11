6821358db585735642e3c180be4ccd66b88e4830
Mike Cote <mikecote@users.noreply.github.com>
2019-10-11 08:58:03 -0400

    Cleanup api.ts

--- x-pack/legacy/plugins/alerting_ui/np_ready/public/application/lib/api.ts
+++ x-pack/legacy/plugins/alerting_ui/np_ready/public/application/lib/api.ts
@@ -3,78 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { Option } from 'fp-ts/lib/Option';
 import { HttpServiceBase } from 'kibana/public';
-import { useRequestNp } from '../../../../public/shared_imports';
 import { BASE_ACTION_API_PATH } from '../constants';
-
-import { ActionType } from '../../../../../actions/server';
-
-export interface RequestData<T> {
-  isLoading: boolean;
-  data: Option<T>;
-  sendRequest?: any;
-}
-
-export interface RequestStatus<T, E> {
-  isLoading: boolean;
-  error?: E;
-  data?: T;
-  sendRequest?: any;
-}
-
-export interface LoadActionTypesResponse {
-  data: ActionTypesResponse[];
-}
-
-export interface LoadActionTypesError {
-  status: number;
-}
-export type LoadActionTypesErrorResponse = LoadActionTypesError | LoadActionTypesError[];
-
-export function hasReceivedAErrorCode(
-  errorOrErrors: any
-): errorOrErrors is LoadActionTypesErrorResponse {
-  const errors = Array.isArray(errorOrErrors) ? errorOrErrors : [errorOrErrors];
-  const firstError = errors.find((error: any) => {
-    if (error) {
-      return [403, 404].includes(error.status);
-    }
-
-    return false;
-  });
-
-  if (firstError) {
-    return true;
-  }
-  return false;
-}
-
-export interface ActionTypesResponse extends ActionType {
-  id: string;
-  name: string;
-}
-export interface LoadActionTypese {
-  page: number;
-  perPage: number;
-  total: number;
-  data: ActionTypesResponse[];
-}
-
-export interface ActionTypesApi {
-  loadActionTypes: (pollIntervalMs: number) => RequestData<LoadActionTypesResponse>;
-}
-
-export function loadActionTypes(http: HttpServiceBase, pollIntervalMs?: number) {
-  return useRequestNp(http, {
-    path: `${BASE_ACTION_API_PATH}/types`,
-    method: 'get',
-    pollIntervalMs,
-    deserializer: (response: { data?: any[]; error?: any }) => {
-      return response;
-    },
-  });
-}
 
 export interface Action {
   id: string;
