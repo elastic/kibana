d503b7268a2679f049dc250aefc96d6b90d7420f
Andrew Goldstein <andrew-goldstein@users.noreply.github.com>
2019-10-11 08:42:58 -0600

    [SIEM] Endgame events on the SIEM Overview page (#47774) (#47904)
    ## Summary
    
    * Adds Endgame events to the SIEM Overview page, per the following screenshot:
    
    <img width="1680" alt="overview-chrome" src="https://user-images.githubusercontent.com/4459398/66524250-26a47800-eaaf-11e9-8ff9-311c031e5d00.png">
    
    * Adds `endgame-*` to the default SIEM index pattern, per the following screenshot:
    
    <img width="1665" alt="siem-advanced-settings" src="https://user-images.githubusercontent.com/4459398/66524300-45a30a00-eaaf-11e9-93c3-dce74917e73a.png">
    
    RELEASE NOTE: To view Endgame events in existing SIEM deployments, you must manually add `endgame-*` to the SIEM index pattern in `Kibana Management > Advanced Settings > SIEM > Elasticsearch indices`. Also note that the `Reset to default` feature for this setting in the Advanced Settings page now includes `endgame-*`.
    
    * Adds the GraphQL plumbing for rendering Endgame data in the Timeline via row renderers (in an upcoming PR), with the introduction of the following fields:
    
    ```
    dns.question.name
    dns.question.type
    dns.resolved_ip
    dns.response_code
    endgame.exit_code
    endgame.file_name
    endgame.file_path
    endgame.logon_type
    endgame.parent_process_name
    endgame.pid
    endgame.process_name
    endgame.subject_domain_name
    endgame.subject_logon_id
    endgame.subject_user_name
    endgame.target_domain_name
    endgame.target_logon_id
    endgame.target_user_name
    event.code
    file.name
    process.hash.md5
    process.hash.sha1
    process.hash.sha256
    user.domain
    winlog.event_id
    ```
    
    ## Testing
    
    ### Cypress
    The `smoke_tests/overview/overview.spec.ts` Cypress test was updated to include the new counts on the Overview page, per the screenshot below:
    
    ![cypress-overview-spec](https://user-images.githubusercontent.com/4459398/66529142-8c98fb80-eabf-11e9-800e-a0d9e1e51d6d.png)
    
    ### API Integration test
    
    The Overview page API integration test `xpack/test/api_integration/apis/siem/overview_host.ts` was updated to include counts of mock Endgame data added to `test/functional/es_archives/auditbeat/overview/data.json.gz`
    
    ### Unit tests
    
    Overview page unit tests were updated to include the new Endgame event counts
    
    ### Desk testing
    
    * Desk tested by hand-editing `components/page/overview/overview_host/index.tsx` and setting the `endDate` and `startDate` values below to a fixed datetime:
    
    ```
    <OverviewHostQuery endDate={endDate} sourceId="default" startDate={startDate}>
    ```
    
    The counts shown on the overview page where then compared to the counts shown in the timeline in the same date period, to verify the counts match 1:1.
    
    * The additional fields mentioned above in this PR (e.g. `dns.question.name`,`endgame.target_domain_name`) that are now being requested via GraphQL can be seen via the Timeline Inspect (query) feature:
    
    1) Enter `event.module: endgame` in the Timeline KQL bar. (Adjust the date range if necessary.)
    2) After Endgame events are displayed in the timeline, click the Inspect button in the Timeline settings gear.
    
    The additional fields (and values) will be included in the Inspect query Request / Response tabs.
    
    ### Cross-browser dark/light testing
    #### Firefox
    
    <img width="1680" alt="overview-firefox" src="https://user-images.githubusercontent.com/4459398/66524773-9c5d1380-eab0-11e9-9383-c155872881b0.png">
    
    #### Safari
    
    <img width="1680" alt="overview-safari" src="https://user-images.githubusercontent.com/4459398/66524790-a54de500-eab0-11e9-9786-aa7dbe18c1bf.png">
    
    #### IE11
    
    This PR was *not* tested in IE11 due to the current blocker with `react-reverse-portal`
    
    https://github.com/elastic/siem-team/issues/465
    https://github.com/elastic/ecs-dev/issues/178

--- x-pack/legacy/plugins/siem/public/graphql/types.ts
+++ x-pack/legacy/plugins/siem/public/graphql/types.ts
@@ -539,6 +539,8 @@ export interface AuthenticationItem {
 }
 
 export interface UserEcsFields {
+  domain?: Maybe<string[]>;
+
   id?: Maybe<string[]>;
 
   name?: Maybe<string[]>;
@@ -687,6 +689,10 @@ export interface Ecs {
 
   destination?: Maybe<DestinationEcsFields>;
 
+  dns?: Maybe<DnsEcsFields>;
+
+  endgame?: Maybe<EndgameEcsFields>;
+
   event?: Maybe<EventEcsFields>;
 
   geo?: Maybe<GeoEcsFields>;
@@ -712,6 +718,8 @@ export interface Ecs {
   message?: Maybe<string[]>;
 
   user?: Maybe<UserEcsFields>;
+
+  winlog?: Maybe<WinlogEcsFields>;
 
   process?: Maybe<ProcessEcsFields>;
 
@@ -774,10 +782,54 @@ export interface DestinationEcsFields {
   packets?: Maybe<number[]>;
 }
 
+export interface DnsEcsFields {
+  question?: Maybe<DnsQuestionData>;
+
+  resolved_ip?: Maybe<string[]>;
+
+  response_code?: Maybe<string[]>;
+}
+
+export interface DnsQuestionData {
+  name?: Maybe<string[]>;
+
+  type?: Maybe<string[]>;
+}
+
+export interface EndgameEcsFields {
+  exit_code?: Maybe<number[]>;
+
+  file_name?: Maybe<string[]>;
+
+  file_path?: Maybe<string[]>;
+
+  logon_type?: Maybe<number[]>;
+
+  parent_process_name?: Maybe<string[]>;
+
+  pid?: Maybe<number[]>;
+
+  process_name?: Maybe<string[]>;
+
+  subject_domain_name?: Maybe<string[]>;
+
+  subject_logon_id?: Maybe<string[]>;
+
+  subject_user_name?: Maybe<string[]>;
+
+  target_domain_name?: Maybe<string[]>;
+
+  target_logon_id?: Maybe<string[]>;
+
+  target_user_name?: Maybe<string[]>;
+}
+
 export interface EventEcsFields {
   action?: Maybe<string[]>;
 
   category?: Maybe<string[]>;
+
+  code?: Maybe<string[]>;
 
   created?: Maybe<string[]>;
 
@@ -1042,7 +1094,13 @@ export interface UrlEcsFields {
   password?: Maybe<string[]>;
 }
 
+export interface WinlogEcsFields {
+  event_id?: Maybe<number[]>;
+}
+
 export interface ProcessEcsFields {
+  hash?: Maybe<ProcessHashData>;
+
   pid?: Maybe<number[]>;
 
   name?: Maybe<string[]>;
@@ -1060,6 +1118,14 @@ export interface ProcessEcsFields {
   working_directory?: Maybe<string[]>;
 }
 
+export interface ProcessHashData {
+  md5?: Maybe<string[]>;
+
+  sha1?: Maybe<string[]>;
+
+  sha256?: Maybe<string[]>;
+}
+
 export interface Thread {
   id?: Maybe<number[]>;
 
@@ -1067,6 +1133,8 @@ export interface Thread {
 }
 
 export interface FileFields {
+  name?: Maybe<string[]>;
+
   path?: Maybe<string[]>;
 
   target_path?: Maybe<string[]>;
@@ -1592,6 +1660,20 @@ export interface OverviewHostData {
   auditbeatProcess?: Maybe<number>;
 
   auditbeatUser?: Maybe<number>;
+
+  endgameDns?: Maybe<number>;
+
+  endgameFile?: Maybe<number>;
+
+  endgameImageLoad?: Maybe<number>;
+
+  endgameNetwork?: Maybe<number>;
+
+  endgameProcess?: Maybe<number>;
+
+  endgameRegistry?: Maybe<number>;
+
+  endgameSecurity?: Maybe<number>;
 
   filebeatSystemModule?: Maybe<number>;
 
@@ -3414,6 +3496,20 @@ export namespace GetOverviewHostQuery {
 
     auditbeatUser: Maybe<number>;
 
+    endgameDns: Maybe<number>;
+
+    endgameFile: Maybe<number>;
+
+    endgameImageLoad: Maybe<number>;
+
+    endgameNetwork: Maybe<number>;
+
+    endgameProcess: Maybe<number>;
+
+    endgameRegistry: Maybe<number>;
+
+    endgameSecurity: Maybe<number>;
+
     filebeatSystemModule: Maybe<number>;
 
     winlogbeat: Maybe<number>;
@@ -3843,6 +3939,10 @@ export namespace GetTimelineQuery {
 
     destination: Maybe<Destination>;
 
+    dns: Maybe<Dns>;
+
+    endgame: Maybe<Endgame>;
+
     geo: Maybe<__Geo>;
 
     suricata: Maybe<Suricata>;
@@ -3856,6 +3956,8 @@ export namespace GetTimelineQuery {
     url: Maybe<Url>;
 
     user: Maybe<User>;
+
+    winlog: Maybe<Winlog>;
 
     process: Maybe<Process>;
 
@@ -3912,6 +4014,8 @@ export namespace GetTimelineQuery {
     action: Maybe<string[]>;
 
     category: Maybe<string[]>;
+
+    code: Maybe<string[]>;
 
     created: Maybe<string[]>;
 
@@ -4002,6 +4106,8 @@ export namespace GetTimelineQuery {
 
   export type File = {
     __typename?: 'FileFields';
+
+    name: Maybe<string[]>;
 
     path: Maybe<string[]>;
 
@@ -4100,6 +4206,54 @@ export namespace GetTimelineQuery {
     region_iso_code: Maybe<string[]>;
 
     region_name: Maybe<string[]>;
+  };
+
+  export type Dns = {
+    __typename?: 'DnsEcsFields';
+
+    question: Maybe<Question>;
+
+    resolved_ip: Maybe<string[]>;
+
+    response_code: Maybe<string[]>;
+  };
+
+  export type Question = {
+    __typename?: 'DnsQuestionData';
+
+    name: Maybe<string[]>;
+
+    type: Maybe<string[]>;
+  };
+
+  export type Endgame = {
+    __typename?: 'EndgameEcsFields';
+
+    exit_code: Maybe<number[]>;
+
+    file_name: Maybe<string[]>;
+
+    file_path: Maybe<string[]>;
+
+    logon_type: Maybe<number[]>;
+
+    parent_process_name: Maybe<string[]>;
+
+    pid: Maybe<number[]>;
+
+    process_name: Maybe<string[]>;
+
+    subject_domain_name: Maybe<string[]>;
+
+    subject_logon_id: Maybe<string[]>;
+
+    subject_user_name: Maybe<string[]>;
+
+    target_domain_name: Maybe<string[]>;
+
+    target_logon_id: Maybe<string[]>;
+
+    target_user_name: Maybe<string[]>;
   };
 
   export type __Geo = {
@@ -4255,11 +4409,21 @@ export namespace GetTimelineQuery {
   export type User = {
     __typename?: 'UserEcsFields';
 
+    domain: Maybe<string[]>;
+
     name: Maybe<string[]>;
+  };
+
+  export type Winlog = {
+    __typename?: 'WinlogEcsFields';
+
+    event_id: Maybe<number[]>;
   };
 
   export type Process = {
     __typename?: 'ProcessEcsFields';
+
+    hash: Maybe<Hash>;
 
     pid: Maybe<number[]>;
 
@@ -4276,6 +4440,16 @@ export namespace GetTimelineQuery {
     working_directory: Maybe<string[]>;
   };
 
+  export type Hash = {
+    __typename?: 'ProcessHashData';
+
+    md5: Maybe<string[]>;
+
+    sha1: Maybe<string[]>;
+
+    sha256: Maybe<string[]>;
+  };
+
   export type Zeek = {
     __typename?: 'ZeekEcsFields';
 
@@ -4285,7 +4459,7 @@ export namespace GetTimelineQuery {
 
     notice: Maybe<Notice>;
 
-    dns: Maybe<Dns>;
+    dns: Maybe<_Dns>;
 
     http: Maybe<_Http>;
 
@@ -4326,7 +4500,7 @@ export namespace GetTimelineQuery {
     peer_descr: Maybe<string[]>;
   };
 
-  export type Dns = {
+  export type _Dns = {
     __typename?: 'ZeekDnsData';
 
     AA: Maybe<boolean[]>;
