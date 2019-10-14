8b2dde650d444d8b84fdf7285de7871d8a4c06fe
Angela Chuang <yi-chun.chuang@elastic.co>
2019-10-14 23:26:18 +0100

    revert not necessary change

--- x-pack/legacy/plugins/siem/public/components/link_to/redirect_to_network.tsx
+++ x-pack/legacy/plugins/siem/public/components/link_to/redirect_to_network.tsx
@@ -8,8 +8,6 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 import { RedirectWrapper } from './redirect_wrapper';
-// import { NetworkRouteType } from '../../pages/network/navigation/types';
-// import { SiemPageName } from '../../pages/home/home_navigations';
 
 export type NetworkComponentProps = RouteComponentProps<{
   detailName: string;
@@ -21,22 +19,8 @@ export const RedirectToNetworkPage = ({
     params: { detailName },
   },
   location: { search },
-}: NetworkComponentProps) => {
-  const defaultSelectedTab = 'ips';
-  const selectedTab = detailName ? detailName : defaultSelectedTab;
-  const to = `/network/${selectedTab}${search}`;
-  return <RedirectWrapper to={to} />;
-};
-
-export const RedirectToIpDetailsPage = ({
-  match: {
-    params: { detailName },
-  },
-  location: { search },
-}: NetworkComponentProps) => {
-  return (
-    <RedirectWrapper to={detailName ? `/network/ip/${detailName}${search}` : `/network${search}`} />
-  );
-};
+}: NetworkComponentProps) => (
+  <RedirectWrapper to={detailName ? `/network/ip/${detailName}${search}` : `/network${search}`} />
+);
 
 export const getNetworkUrl = () => '#/link-to/network';
