189bf84ce9d5628d2687538b8583312759998797
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-10-15 13:26:00 +0100

    fix some memory issues

--- x-pack/legacy/plugins/apm/cypress/ci/run.sh
+++ x-pack/legacy/plugins/apm/cypress/ci/run.sh
@@ -8,6 +8,7 @@ echo "1/4 Install dependencies..."
 source src/dev/ci_setup/setup_env.sh true
 yarn kbn bootstrap
 cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.dev.yml
+echo 'elasticsearch:' >> config/kibana.dev.yml
 cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.yml
 npm install -g wait-on
 
@@ -19,7 +20,8 @@ node ingest-data/replay.js --server-url http://localhost:8200 --secret-token abc
 
 echo "3/4 Start Kibana..."
 popd
-nohup node scripts/kibana > kibana.log 2>&1 &
+## Might help to avoid FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
+nohup NODE_OPTIONS=--max-old-space-size=4096 node scripts/kibana > kibana.log 2>&1 &
 
 echo "4/4 Run cypress tests..."
 pushd ${CYPRESS_DIR}
