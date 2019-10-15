52f7e4cc2d51b9cc4d7d0c592dc63b85891ca879
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-10-15 12:53:49 +0100

    fix typo

--- x-pack/legacy/plugins/apm/cypress/ci/run.sh
+++ x-pack/legacy/plugins/apm/cypress/ci/run.sh
@@ -4,16 +4,17 @@ set -ex
 CYPRESS_DIR="x-pack/legacy/plugins/apm/cypress"
 
 echo "1/4 Install dependencies..."
+# shellcheck disable=SC1091
 source src/dev/ci_setup/setup_env.sh true
 yarn kbn bootstrap
-cp ${CYPRESS_DIR}/kibana.dev.yml config/kibana.dev.yml
-cp ${CYPRESS_DIR}/kibana.dev.yml config/kibana.yml
+cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.dev.yml
+cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.yml
 npm install -g wait-on
 
 echo "2/4 Ingest test data..."
 pushd ${CYPRESS_DIR}
 yarn install
-curl https://storage.googleapis.com/apm-ui-e2e-static-data/events.json --output events.json
+curl https://storage.googleapis.com/apm-ui-e2e-static-data/events.json --output ingest-data/events.json
 node ingest-data/replay.js --server-url http://localhost:8200 --secret-token abcd --events ./events.json
 
 echo "3/4 Start Kibana..."
