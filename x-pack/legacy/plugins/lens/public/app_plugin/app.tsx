fd9f4a4cd690e62be3970917645ee00ad5f01dce
Brandon Kobel <brandon.kobel@elastic.co>
2019-10-15 15:28:49 -0700

    Removing the lens feature, using visualize instead (#48041) (#48272)
    * Removing the lens feature, using visualize instead
    
    * Updating /api/features test because lens isn't a feature anymore
    
    * Updating capability from lens to visualize in test
    
    * Fixing api integration test

--- x-pack/legacy/plugins/lens/public/app_plugin/app.tsx
+++ x-pack/legacy/plugins/lens/public/app_plugin/app.tsx
@@ -153,7 +153,7 @@ export function App({
   // Can save if the frame has told us what it has, and there is either:
   // a) No saved doc
   // b) A saved doc that differs from the frame state
-  const isSaveable = state.isDirty && (core.application.capabilities.lens.save as boolean);
+  const isSaveable = state.isDirty && (core.application.capabilities.visualize.save as boolean);
 
   const onError = useCallback(
     (e: { message: string }) =>
@@ -229,7 +229,7 @@ export function App({
               showDatePicker={true}
               showQueryBar={true}
               showFilterBar={true}
-              showSaveQuery={core.application.capabilities.lens.saveQuery as boolean}
+              showSaveQuery={core.application.capabilities.visualize.saveQuery as boolean}
               savedQuery={state.savedQuery}
               onSaved={savedQuery => {
                 setState(s => ({ ...s, savedQuery }));
