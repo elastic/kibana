c1a3e22759d68941655d6ae4a0b10ec7f9b58338
Candace Park <park@endgame.com>
2019-10-16 17:49:18 -0400

    wip integrating search bar

---
+++ x-pack/plugins/endgame/public/components/search_bar.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { Component } from 'react';
+
+import { EuiSearchBar } from '@elastic/eui';
+
+interface SearchBarProps {
+  searchItems: [];
+  defaultFields: string[];
+  updateOnChange: Function;
+}
+
+interface SearchBarState {
+  currentResult: {};
+}
+
+export class SearchBar extends Component<SearchBarProps, SearchBarState> {
+  private defaultOnChange = ({ query }: { query: string }) => {
+    const { searchItems, defaultFields, updateOnChange } = this.props;
+
+    const result = EuiSearchBar.Query.execute(query, searchItems, {
+      defaultFields,
+    });
+    this.setState({ currentResult: result });
+    updateOnChange({ updatedResult: result });
+  };
+
+  public render() {
+    return (
+      <EuiSearchBar
+        defaultQuery={EuiSearchBar.Query.MATCH_ALL}
+        box={{
+          placeholder: 'try _source.host.ip:"10.11.12.13"',
+          incremental: false,
+          filters: [],
+        }}
+        onChange={this.defaultOnChange}
+      />
+    );
+  }
+}
