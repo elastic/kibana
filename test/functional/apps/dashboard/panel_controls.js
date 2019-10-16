d8dab891b2f97fc279d10dd4c61358ec27b942e7
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2019-10-17 01:09:52 +0200

    fix panel_controls test

--- test/functional/apps/dashboard/panel_controls.js
+++ test/functional/apps/dashboard/panel_controls.js
@@ -37,15 +37,7 @@ export default function ({ getService, getPageObjects }) {
 
     before(async function () {
       await PageObjects.dashboard.initTests();
-      await browser.refresh();
-      await PageObjects.header.awaitKibanaChrome();
-
-      // This flip between apps fixes the url so state is preserved when switching apps in test mode.
-      // Without this flip the url in test mode looks something like
-      // "http://localhost:5620/app/kibana?_t=1486069030837#/dashboard?_g=...."
-      // after the initial flip, the url will look like this: "http://localhost:5620/app/kibana#/dashboard?_g=...."
-      await PageObjects.header.clickVisualize();
-      await PageObjects.header.clickDashboard();
+      await PageObjects.dashboard.preserveCrossAppState();
     });
 
     after(async function () {
