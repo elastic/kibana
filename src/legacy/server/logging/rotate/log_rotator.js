057f997b8046c59bd2cef966e9a279d7460f5d86
Tiago Costa <tiagoffcc@hotmail.com>
2019-10-16 22:06:25 +0100

    chore(NA): base lifecycle methods and logic

--- src/legacy/server/logging/rotate/log_rotator.js
+++ src/legacy/server/logging/rotate/log_rotator.js
@@ -17,6 +17,9 @@
  * under the License.
  */
 
+import fs from 'fs';
+import { throttle } from 'lodash';
+
 export class LogRotator {
   constructor(config, logInterceptor) {
     this.logInterceptor = logInterceptor;
@@ -25,15 +28,104 @@ export class LogRotator {
     this.everyBytes = config.get('logging.rotate.everyBytes');
     this.keepFiles = config.get('logging.rotate.keepFiles');
     this.onStartup = config.get('logging.rotate.onStartup');
+    this.running = false;
+    this.logFileSize = 0;
+
+    throttle(this._rotate, 5000);
   }
 
-  start() {}
+  start() {
+    if (this.running) {
+      return;
+    }
 
-  stop() {}
+    // create exit listener for cleanup purposes
+    this._createExitListener();
 
-  _shouldRotate() {}
+    // init log file size monitor
+    this._startLogFileSizeMonitor();
 
-  _rotate() {}
+    // init log file interval monitor
+    this._startLogFileIntervalMonitor();
+
+    // call rotate in case we should try
+    // to rotate on startup
+    this._callRotateOnStartup();
+
+    this.running = true;
+  }
+
+  stop() {
+    if (!this.running) {
+      return;
+    }
+
+    // cleanup exit listener
+    this._deleteExitListener();
+
+    // stop log file size monitor
+    this._stopLogFileSizeMonitor();
+
+    // stop log file interval monitor
+    this._stopLogFileIntervalMonitor();
+
+    this.running = false;
+  }
+
+  _startLogFileSizeMonitor() {
+
+  }
+
+  _logFileSizeMonitorHandle() {
+
+  }
+
+  _stopLogFileSizeMonitor() {
+
+  }
+
+  _startLogFileIntervalMonitor() {
+
+  }
+
+  _logFileIntervalMonitorHandle() {
+
+  }
+
+  _stopLogFileIntervalMonitor() {
+
+  }
+
+  _createExitListener() {
+    process.on('exit', this.stop());
+  }
+
+  _deleteExitListener() {
+    process.removeListener('exit', this.stop());
+  }
+
+  _getLogFileSize() {
+    const logFileStats = fs.statSync(this.logFilePath);
+    return logFileStats.size;
+  }
+
+  _callRotateOnStartup() {
+    if (this.onStartup) {
+      this._rotate();
+    }
+  }
+
+  _shouldRotate() {
+    return false;
+  }
+
+  _rotate() {
+    if (!this._shouldRotate()) {
+      return;
+    }
+
+    this._rotateNow();
+  }
 
   _rotateNow() {
     // rename old
