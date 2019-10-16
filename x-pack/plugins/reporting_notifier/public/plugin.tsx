69edf32756f3cb64e43a8f1d50180ece56e28634
Timothy Sullivan <tsullivan@elastic.co>
2019-10-16 12:44:10 -0700

    remove type annos and condense

--- x-pack/plugins/reporting_notifier/public/plugin.tsx
+++ x-pack/plugins/reporting_notifier/public/plugin.tsx
@@ -17,7 +17,7 @@ import {
   JOB_COMPLETION_NOTIFICATIONS_POLLER_CONFIG,
   JOB_COMPLETION_NOTIFICATIONS_SESSION_KEY,
 } from '../constants';
-import { JobId, JobStatusBuckets } from '../index.d';
+import { JobId, JobStatusBuckets, NotificationsService } from '../index.d';
 import { getGeneralErrorToast } from './components';
 import { ReportingNotifierStreamHandler as StreamHandler } from './lib/stream_handler';
 
@@ -28,6 +28,23 @@ const {
 function getStored(): JobId[] {
   const sessionValue = sessionStorage.getItem(JOB_COMPLETION_NOTIFICATIONS_SESSION_KEY);
   return sessionValue ? JSON.parse(sessionValue) : [];
+}
+
+function handleError(
+  notifications: NotificationsService,
+  err: Error
+): Rx.Observable<JobStatusBuckets> {
+  // show general toast, log the error and resume
+  notifications.toasts.addDanger(
+    getGeneralErrorToast(
+      i18n.translate('xpack.reportingNotifier.pollingErrorMessage', {
+        defaultMessage: 'Reporting notifier error!',
+      }),
+      err
+    )
+  );
+  window.console.error(err);
+  return Rx.of({ completed: [], failed: [] });
 }
 
 export class ReportingNotifierPublicPlugin implements Plugin<any, any> {
@@ -47,26 +64,10 @@ export class ReportingNotifierPublicPlugin implements Plugin<any, any> {
 
     this.poller$ = Rx.timer(0, JOBS_REFRESH_INTERVAL).pipe(
       map(() => getStored()), // Read all pending job IDs from session storage
-      filter((storedJobs: JobId[]) => storedJobs.length > 0), // stop the pipeline here if there are none pending
-      mergeMap((storedJobs: JobId[]) => {
-        return streamHandler.findChangedStatusJobs(storedJobs); // look up the latest status of all pending jobs on the server
-      }),
-      mergeMap(({ completed, failed }: JobStatusBuckets) => {
-        return streamHandler.showNotifications({ completed, failed });
-      }),
-      catchError(err => {
-        // show general toast, log the error and resume
-        notifications.toasts.addDanger(
-          getGeneralErrorToast(
-            i18n.translate('xpack.reportingNotifier.pollingErrorMessage', {
-              defaultMessage: 'Reporting notifier error!',
-            }),
-            err
-          )
-        );
-        window.console.error(err);
-        return Rx.of({ completed: [], failed: [] });
-      })
+      filter(storedJobs => storedJobs.length > 0), // stop the pipeline here if there are none pending
+      mergeMap(storedJobs => streamHandler.findChangedStatusJobs(storedJobs)), // look up the latest status of all pending jobs on the server
+      mergeMap(({ completed, failed }) => streamHandler.showNotifications({ completed, failed })),
+      catchError(err => handleError(notifications, err))
     );
 
     this.poller$.subscribe();
