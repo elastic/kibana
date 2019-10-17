85067259b7e2db4ab88b9e097a2b483c41b7d121
neptunian <sandra.gonzales@elastic.co>
2019-10-17 14:38:23 -0400

    fix ie11 issues with nested flex items, change radius to use eui variable

--- x-pack/legacy/plugins/integrations_manager/public/screens/detail/screenshots.tsx
+++ x-pack/legacy/plugins/integrations_manager/public/screens/detail/screenshots.tsx
@@ -19,11 +19,16 @@ export function Screenshots(props: ScreenshotProps) {
   const { images } = props;
   // for now, just get first image
   const src = toImage(images[0].src);
-  const ScreenshotsContainer = styled.div`
+  const ScreenshotsContainer = styled(EuiFlexGroup)`
     background: linear-gradient(360deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0) 100%),
       ${theme.eui.euiColorPrimary};
     padding: 32px 64px 56px 64px;
-    border-radius: 4px;
+    flex: 0 0 auto;
+    border-radius: ${theme.eui.euiBorderRadius};
+  `;
+  // fixes ie11 problems with nested flex items
+  const NestedEuiFlexItem = styled(EuiFlexItem)`
+    flex: 0 0 auto !important;
   `;
   return (
     <Fragment>
@@ -31,18 +36,14 @@ export function Screenshots(props: ScreenshotProps) {
         <h3>Screenshots</h3>
       </EuiTitle>
       <EuiSpacer size="m" />
-      <ScreenshotsContainer>
-        <EuiFlexGroup gutterSize="none" direction="column" alignItems="center">
-          <EuiFlexItem>
-            <EuiText color="ghost">
-              We need image descriptions to be returned in the response
-            </EuiText>
-            <EuiSpacer />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiImage url={src} alt="image" size="l" allowFullScreen />
-          </EuiFlexItem>
-        </EuiFlexGroup>
+      <ScreenshotsContainer gutterSize="none" direction="column" alignItems="center">
+        <NestedEuiFlexItem>
+          <EuiText color="ghost">We need image descriptions to be returned in the response</EuiText>
+          <EuiSpacer />
+        </NestedEuiFlexItem>
+        <NestedEuiFlexItem>
+          <EuiImage url={src} alt="image" size="l" allowFullScreen />
+        </NestedEuiFlexItem>
       </ScreenshotsContainer>
     </Fragment>
   );
