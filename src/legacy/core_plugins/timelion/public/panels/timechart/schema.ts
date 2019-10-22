a25afa1dc859a06d5df58d29585f3411bf5a4469
Joe Reuter <johannes.reuter@elastic.co>
2019-10-22 15:40:06 +0200

    Timelion: Show correct value in legend for step charts (#47863) (#48891)

--- src/legacy/core_plugins/timelion/public/panels/timechart/schema.ts
+++ src/legacy/core_plugins/timelion/public/panels/timechart/schema.ts
@@ -225,7 +225,6 @@ export function timechartFn(dependencies: TimelionVisualizationDependencies) {
           }
 
           let i;
-          let j;
           const dataset = plot.getData();
           if (legendCaption) {
             legendCaption.text(
@@ -236,21 +235,23 @@ export function timechartFn(dependencies: TimelionVisualizationDependencies) {
           }
           for (i = 0; i < dataset.length; ++i) {
             const series = dataset[i];
+            const useNearestPoint = series.lines.show && !series.lines.steps;
             const precision = _.get(series, '_meta.precision', 2);
 
             if (series._hide) continue;
 
-            // Nearest point
-            for (j = 0; j < series.data.length; ++j) {
-              if (series.data[j][0] > pos.x) break;
-            }
+            const currentPoint = series.data.find((point: any, index: number) => {
+              if (index + 1 === series.data.length) {
+                return true;
+              }
+              if (useNearestPoint) {
+                return pos.x - point[0] < series.data[index + 1][0] - pos.x;
+              } else {
+                return pos.x < series.data[index + 1][0];
+              }
+            });
 
-            let y;
-            try {
-              y = series.data[j][1];
-            } catch (e) {
-              y = null;
-            }
+            const y = currentPoint[1];
 
             if (y != null) {
               let label = y.toFixed(precision);
