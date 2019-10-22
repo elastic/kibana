e4a959194791f9de5e3ee36731bd6fb6406b3693
Joe Reuter <johannes.reuter@elastic.co>
2019-10-22 14:58:09 +0200

    Pass raw values and format afterwards (#48090)

--- src/legacy/ui/public/utils/ip_range.ts
+++ src/legacy/ui/public/utils/ip_range.ts
@@ -17,28 +17,15 @@
  * under the License.
  */
 
-import moment from 'moment';
+import { IpRangeKey } from '../agg_types/buckets/ip_range';
 
-export const dateRange = {
-  toString: function (range, format) {
-    if (!range.from) {
-      return 'Before ' + format(range.to);
-    } else if (!range.to) {
-      return 'After ' + format(range.from);
-    } else {
-      return format(range.from) + ' to ' + format(range.to);
+export const ipRange = {
+  toString(range: IpRangeKey, format: (val: any) => string) {
+    if (range.type === 'mask') {
+      return format(range.mask);
     }
+    const from = range.from ? format(range.from) : '-Infinity';
+    const to = range.to ? format(range.to) : 'Infinity';
+    return `${from} to ${to}`;
   },
-  parse: function (rangeString, format) {
-    let chunks = rangeString.split(' to ');
-    if (chunks.length === 2) return { from: moment(chunks[0], format), to: moment(chunks[1], format) };
-
-    chunks = rangeString.split('Before ');
-    if (chunks.length === 2) return { to: moment(chunks[1], format) };
-
-    chunks = rangeString.split('After ');
-    if (chunks.length === 2) return { from: moment(chunks[1], format) };
-
-    throw new Error('Error attempting to parse date range: ' + rangeString);
-  }
 };
