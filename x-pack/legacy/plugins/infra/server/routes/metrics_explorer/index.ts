b9413b21f25861a53c3fa67b1bf4362241275593
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-10-23 13:37:15 -0400

    WIP continues

--- x-pack/legacy/plugins/infra/server/routes/metrics_explorer/index.ts
+++ x-pack/legacy/plugins/infra/server/routes/metrics_explorer/index.ts
@@ -5,41 +5,45 @@
  */
 
 import { boomify } from 'boom';
+import { schema } from '@kbn/config-schema';
 import { InfraBackendLibs } from '../../lib/infra_types';
 import { getGroupings } from './lib/get_groupings';
 import { populateSeriesWithTSVBData } from './lib/populate_series_with_tsvb_data';
 import { metricsExplorerSchema } from './schema';
-import { MetricsExplorerResponse, MetricsExplorerWrappedRequest } from './types';
+import { MetricsExplorerResponse, MetricsExplorerRequestBody } from './types';
+
+// NP_TODO: need to replace all of this with real types
+const escapeHatch = schema.object({}, { allowUnknowns: true });
+type EscapeHatch = typeof escapeHatch;
 
 export const initMetricExplorerRoute = (libs: InfraBackendLibs) => {
   const { framework } = libs;
   const { callWithRequest } = framework;
 
-  // framework.registerRoute<MetricsExplorerWrappedRequest, Promise<MetricsExplorerResponse>>({
-  //   method: 'post',
-  //   path: '/api/infra/metrics_explorer',
-  //   options: {
-  //     validate: {
-  //       payload: metricsExplorerSchema,
-  //     },
-  //   },
-  //   handler: async req => {
-  //     try {
-  //       const search = <Aggregation>(searchOptions: object) =>
-  //         callWithRequest<{}, Aggregation>(req, 'search', searchOptions);
-  //       const options = req.payload;
-  //       // First we get the groupings from a composite aggregation
-  //       const response = await getGroupings(search, options);
+  framework.router.post<EscapeHatch, EscapeHatch, EscapeHatch>(
+    {
+      path: '/api/infra/metrics_explorer',
+      validate: {
+        body: escapeHatch,
+      },
+    },
+    async (context, request, response) => {
+      try {
+        const search = <Aggregation>(searchOptions: object) =>
+          callWithRequest<{}, Aggregation>(request, 'search', searchOptions);
+        const options = request.payload;
+        // First we get the groupings from a composite aggregation
+        const groupings = await getGroupings(search, options);
 
-  //       // Then we take the results and fill in the data from TSVB with the
-  //       // user's custom metrics
-  //       const seriesWithMetrics = await Promise.all(
-  //         response.series.map(populateSeriesWithTSVBData(req, options, framework))
-  //       );
-  //       return { ...response, series: seriesWithMetrics };
-  //     } catch (error) {
-  //       throw boomify(error);
-  //     }
-  //   },
-  // });
+        // Then we take the results and fill in the data from TSVB with the
+        // user's custom metrics
+        const seriesWithMetrics = await Promise.all(
+          groupings.series.map(populateSeriesWithTSVBData(request, options, framework))
+        );
+        response.ok({ body: { ...response, series: seriesWithMetrics } });
+      } catch (error) {
+        throw boomify(error);
+      }
+    }
+  );
 };
