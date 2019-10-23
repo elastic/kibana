d49a8a17fa65a148c6d76ad57ed6b3cbfe0f3579
streamich <streamich@gmail.com>
2019-10-23 13:47:47 +0200

    fix: 🐛 fix Lens import after merge with master

--- x-pack/legacy/plugins/lens/public/indexpattern_plugin/plugin.tsx
+++ x-pack/legacy/plugins/lens/public/indexpattern_plugin/plugin.tsx
@@ -11,7 +11,7 @@ import { Storage } from 'ui/storage';
 import { npSetup, npStart } from 'ui/new_platform';
 import { getIndexPatternDatasource } from './indexpattern';
 import { renameColumns } from './rename_columns';
-import { calculateFilterRatio } from './filter_ratio';
+import { autoDate } from './auto_date';
 import { ExpressionsSetup } from '../../../../../../src/plugins/expressions/public';
 
 // TODO these are intermediary types because interpreter is not typed yet
@@ -28,7 +28,7 @@ class IndexPatternDatasourcePlugin {
 
   setup(core: CoreSetup, { expressions }: IndexPatternDatasourceSetupPlugins) {
     expressions.registerFunction(renameColumns);
-    expressions.registerFunction(calculateFilterRatio);
+    expressions.registerFunction(autoDate);
   }
 
   stop() {}
