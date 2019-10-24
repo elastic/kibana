491914c4b50bbc2fb900a813f7fb088945a53902
Sébastien <sebastien.loix@elastic.co>
2019-10-24 14:12:32 +0200

    Add documentation for each type in flyout

--- x-pack/legacy/plugins/index_management/public/components/mappings_editor/components/document_fields/fields/edit_field/edit_field.tsx
+++ x-pack/legacy/plugins/index_management/public/components/mappings_editor/components/document_fields/fields/edit_field/edit_field.tsx
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React, { useEffect } from 'react';
+import { i18n } from '@kbn/i18n';
 import {
   EuiFlyout,
   EuiFlyoutHeader,
@@ -18,10 +19,11 @@ import {
   EuiSpacer,
 } from '@elastic/eui';
 
-import { useForm, Form } from '../../../../shared_imports';
+import { useForm, Form, FormDataProvider } from '../../../../shared_imports';
 import { useDispatch } from '../../../../mappings_state';
-import { Field, NormalizedField } from '../../../../types';
-import { fieldSerializer, fieldDeserializer } from '../../../../lib';
+import { TYPE_DEFINITION } from '../../../../constants';
+import { Field, NormalizedField, MainType, SubType } from '../../../../types';
+import { fieldSerializer, fieldDeserializer, getTypeDocLink } from '../../../../lib';
 import { UpdateFieldProvider, UpdateFieldFunc } from './update_field_provider';
 import { EditFieldHeaderForm } from './edit_field_header_form';
 import { EditFieldSection } from './edit_field_section';
@@ -73,50 +75,98 @@ export const EditField = React.memo(({ field }: Props) => {
   return (
     <UpdateFieldProvider>
       {updateField => (
-        <EuiFlyout
-          data-test-subj="mappingsEditorFieldEdit"
-          onClose={exitEdit}
-          aria-labelledby="mappingsEditorFieldEditTitle"
-          size="m"
-          maxWidth={720}
+        <Form
+          form={form}
+          style={{ padding: '20px 0' }}
+          FormWrapper={formWrapper}
+          onSubmit={getSubmitForm(updateField)}
         >
-          <EuiFlyoutHeader>
-            <EuiTitle size="m">
-              <h2>Edit field '{field.source.name}'</h2>
-            </EuiTitle>
-            <EuiCode>{field.path}</EuiCode>
-          </EuiFlyoutHeader>
+          <FormDataProvider pathsToWatch={['type', 'subType']}>
+            {({ type, subType }) => {
+              const typeDefinition = TYPE_DEFINITION[type as MainType];
+              const subTypeDefinition = TYPE_DEFINITION[subType as SubType];
 
-          <EuiFlyoutBody>
-            <Form
-              form={form}
-              style={{ padding: '20px 0' }}
-              FormWrapper={formWrapper}
-              onSubmit={getSubmitForm(updateField)}
-            >
-              <EditFieldSection title="Field definition">
-                <EditFieldHeaderForm defaultValue={field.source} isMultiField={isMultiField} />
-              </EditFieldSection>
+              if (typeDefinition === undefined) {
+                return null;
+              }
 
-              <EditFieldSection>
-                <p>Here will come the form for the parameters....</p>
-              </EditFieldSection>
-            </Form>
-          </EuiFlyoutBody>
+              const linkDocumentation = getTypeDocLink(subType) || getTypeDocLink(type);
 
-          <EuiFlyoutFooter>
-            <EuiFlexGroup justifyContent="flexEnd">
-              <EuiFlexItem grow={false}>
-                <EuiButtonEmpty onClick={cancel}>Cancel</EuiButtonEmpty>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiButton fill onClick={getSubmitForm(updateField)} type="submit">
-                  Update
-                </EuiButton>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiFlyoutFooter>
-        </EuiFlyout>
+              return (
+                <EuiFlyout
+                  data-test-subj="mappingsEditorFieldEdit"
+                  onClose={exitEdit}
+                  aria-labelledby="mappingsEditorFieldEditTitle"
+                  size="m"
+                  maxWidth={720}
+                >
+                  <EuiFlyoutHeader>
+                    <EuiTitle size="m">
+                      <h2>Edit field '{field.source.name}'</h2>
+                    </EuiTitle>
+                    <EuiFlexGroup justifyContent="spaceBetween">
+                      <EuiFlexItem grow={false}>
+                        <EuiCode>{field.path}</EuiCode>
+                      </EuiFlexItem>
+
+                      {/* Type documentation link */}
+                      {linkDocumentation && (
+                        <EuiFlexItem grow={false}>
+                          <EuiButtonEmpty
+                            size="s"
+                            flush="right"
+                            href={linkDocumentation}
+                            target="_blank"
+                            iconType="help"
+                          >
+                            {i18n.translate(
+                              'xpack.idxMgmt.mappingsEditor.editField.typeDocumentation',
+                              {
+                                defaultMessage: '{type} documentation',
+                                values: {
+                                  type: subTypeDefinition
+                                    ? subTypeDefinition.label
+                                    : typeDefinition.label,
+                                },
+                              }
+                            )}
+                          </EuiButtonEmpty>
+                        </EuiFlexItem>
+                      )}
+                    </EuiFlexGroup>
+                  </EuiFlyoutHeader>
+
+                  <EuiFlyoutBody>
+                    <EditFieldSection title="Field definition">
+                      <EditFieldHeaderForm
+                        type={type}
+                        defaultValue={field.source}
+                        isMultiField={isMultiField}
+                      />
+                    </EditFieldSection>
+
+                    <EditFieldSection>
+                      <p>Here will come the form for the parameters....</p>
+                    </EditFieldSection>
+                  </EuiFlyoutBody>
+
+                  <EuiFlyoutFooter>
+                    <EuiFlexGroup justifyContent="flexEnd">
+                      <EuiFlexItem grow={false}>
+                        <EuiButtonEmpty onClick={cancel}>Cancel</EuiButtonEmpty>
+                      </EuiFlexItem>
+                      <EuiFlexItem grow={false}>
+                        <EuiButton fill onClick={getSubmitForm(updateField)} type="submit">
+                          Update
+                        </EuiButton>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiFlyoutFooter>
+                </EuiFlyout>
+              );
+            }}
+          </FormDataProvider>
+        </Form>
       )}
     </UpdateFieldProvider>
   );
--- x-pack/legacy/plugins/index_management/public/components/mappings_editor/components/document_fields/fields/edit_field/edit_field_header_form.tsx
+++ x-pack/legacy/plugins/index_management/public/components/mappings_editor/components/document_fields/fields/edit_field/edit_field_header_form.tsx
@@ -7,120 +7,104 @@
 import React, { ChangeEvent } from 'react';
 import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
-import {
-  FormDataProvider,
-  SelectField,
-  UseField,
-  FieldConfig,
-  FieldHook,
-} from '../../../../shared_imports';
-import { MainType, ParameterName, Field } from '../../../../types';
+import { SelectField, UseField, FieldConfig, FieldHook } from '../../../../shared_imports';
+import { MainType, SubType, ParameterName, Field } from '../../../../types';
 import {
   TYPE_DEFINITION,
   FIELD_TYPES_OPTIONS,
   MULTIFIELD_TYPES_OPTIONS,
   PARAMETERS_DEFINITION,
 } from '../../../../constants';
+
 import { NameParameter } from '../../field_parameters';
 
 const getFieldConfig = (param: ParameterName): FieldConfig =>
   PARAMETERS_DEFINITION[param].fieldConfig || {};
 
 interface Props {
-  isMultiField: boolean;
+  type: MainType;
   defaultValue: Field;
+  isMultiField: boolean;
 }
 
-export const EditFieldHeaderForm = ({ isMultiField, defaultValue }: Props) => {
+export const EditFieldHeaderForm = React.memo(({ type, defaultValue, isMultiField }: Props) => {
+  const typeDefinition = TYPE_DEFINITION[type];
+  const hasSubType = typeDefinition.subTypes !== undefined;
+
+  const subTypeOptions = hasSubType
+    ? typeDefinition
+        .subTypes!.types.map(_subType => TYPE_DEFINITION[_subType])
+        .map(_subType => ({ value: _subType.value, text: _subType.label }))
+    : undefined;
+
+  const defaultValueSubType = hasSubType
+    ? typeDefinition.subTypes!.types.includes(defaultValue.type as SubType)
+      ? defaultValue.type // we use the default value provided
+      : typeDefinition.subTypes!.types[0] // we set the first item from the subType array
+    : undefined;
+
   const onTypeChange = (field: FieldHook) => (e: ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.currentTarget;
-    const typeDefinition = TYPE_DEFINITION[value as MainType];
-    const hasSubType = typeDefinition.subTypes !== undefined;
+    const nextTypeDefinition = TYPE_DEFINITION[value as MainType];
+    const nextHasSubType = nextTypeDefinition.subTypes !== undefined;
     const subTypeField = field.form.getFields().subType;
 
-    if (hasSubType && subTypeField) {
+    if (nextHasSubType && subTypeField) {
       /**
        * We need to manually set the subType field value because if we edit a field type that already has a subtype
        * (e.g. "numeric" with subType "float"), and we change the type to another one that also has subTypes (e.g. "range").
        * If we then immediately click the "update" button, _without_ changing the subType, the old value of subType
        * is maintained.
        */
-      subTypeField.setValue(typeDefinition.subTypes!.types[0]);
+      subTypeField.setValue(nextTypeDefinition.subTypes!.types[0]);
     }
 
     field.setValue(value);
   };
 
   return (
-    <FormDataProvider pathsToWatch="type">
-      {formData => {
-        const type = formData.type as MainType;
+    <EuiFlexGroup gutterSize="s">
+      {/* Field name */}
+      <EuiFlexItem>
+        <NameParameter />
+      </EuiFlexItem>
 
-        if (!type) {
-          return null;
-        }
+      {/* Field type */}
+      <EuiFlexItem>
+        <UseField path="type" config={getFieldConfig('type')}>
+          {field => (
+            <SelectField
+              field={field}
+              euiFieldProps={{
+                options: isMultiField ? MULTIFIELD_TYPES_OPTIONS : FIELD_TYPES_OPTIONS,
+                hasNoInitialSelection: true,
+                onChange: onTypeChange(field),
+              }}
+            />
+          )}
+        </UseField>
+      </EuiFlexItem>
 
-        const typeDefinition = TYPE_DEFINITION[type];
-        const hasSubType = typeDefinition.subTypes !== undefined;
-
-        const subTypeOptions = hasSubType
-          ? typeDefinition
-              .subTypes!.types.map(subType => TYPE_DEFINITION[subType])
-              .map(subType => ({ value: subType.value, text: subType.label }))
-          : undefined;
-
-        const defaultValueSubType = hasSubType
-          ? defaultValue.subType === undefined || type !== defaultValue.type
-            ? typeDefinition.subTypes!.types[0]
-            : defaultValue.subType
-          : undefined;
-
-        return (
-          <EuiFlexGroup gutterSize="s">
-            {/* Field name */}
-            <EuiFlexItem>
-              <NameParameter />
-            </EuiFlexItem>
-
-            {/* Field type */}
-            <EuiFlexItem>
-              <UseField path="type" config={getFieldConfig('type')}>
-                {field => (
-                  <SelectField
-                    field={field}
-                    euiFieldProps={{
-                      options: isMultiField ? MULTIFIELD_TYPES_OPTIONS : FIELD_TYPES_OPTIONS,
-                      hasNoInitialSelection: true,
-                      onChange: onTypeChange(field),
-                    }}
-                  />
-                )}
-              </UseField>
-            </EuiFlexItem>
-
-            {/* Field sub type (if any) */}
-            {hasSubType && (
-              <EuiFlexItem grow={false}>
-                <UseField
-                  path="subType"
-                  defaultValue={defaultValueSubType}
-                  config={{
-                    ...getFieldConfig('type'),
-                    label: typeDefinition.subTypes!.label,
-                  }}
-                  component={SelectField}
-                  componentProps={{
-                    euiFieldProps: {
-                      options: subTypeOptions,
-                      hasNoInitialSelection: false,
-                    },
-                  }}
-                />
-              </EuiFlexItem>
-            )}
-          </EuiFlexGroup>
-        );
-      }}
-    </FormDataProvider>
+      {/* Field sub type (if any) */}
+      {hasSubType && (
+        <EuiFlexItem grow={false}>
+          <UseField
+            path="subType"
+            config={{
+              ...getFieldConfig('type'),
+              label: typeDefinition.subTypes!.label,
+            }}
+            defaultValue={defaultValueSubType}
+            component={SelectField}
+            componentProps={{
+              euiFieldProps: {
+                options: subTypeOptions,
+                hasNoInitialSelection: false,
+              },
+            }}
+          />
+        </EuiFlexItem>
+      )}
+    </EuiFlexGroup>
   );
-};
+});
