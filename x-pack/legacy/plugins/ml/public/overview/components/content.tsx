c463cce765a9877fa52442f95ba26dd6a25ed8a7
Melissa Alvarez <melissa.alvarez@elastic.co>
2019-10-24 14:56:04 -0400

    [ML] Overview: ensure proper permissions check for empty prompt 'Create job' buttons (#49067) (#49215)
    * disabled overview empty prompt create job button if no permissions
    
    * permission check from overviewPage main component. add mlNodes check to resolver
    
    * remove period from overview empty prompt title

--- x-pack/legacy/plugins/ml/public/overview/components/content.tsx
+++ x-pack/legacy/plugins/ml/public/overview/components/content.tsx
@@ -9,15 +9,23 @@ import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { AnomalyDetectionPanel } from './anomaly_detection_panel';
 import { AnalyticsPanel } from './analytics_panel/';
 
+interface Props {
+  createAnomalyDetectionJobDisabled: boolean;
+  createAnalyticsJobDisabled: boolean;
+}
+
 // Fetch jobs and determine what to show
-export const OverviewContent: FC = () => (
+export const OverviewContent: FC<Props> = ({
+  createAnomalyDetectionJobDisabled,
+  createAnalyticsJobDisabled,
+}) => (
   <EuiFlexItem grow={3}>
     <EuiFlexGroup direction="column">
       <EuiFlexItem grow={false}>
-        <AnomalyDetectionPanel />
+        <AnomalyDetectionPanel jobCreationDisabled={createAnomalyDetectionJobDisabled} />
       </EuiFlexItem>
       <EuiFlexItem grow={false}>
-        <AnalyticsPanel />
+        <AnalyticsPanel jobCreationDisabled={createAnalyticsJobDisabled} />
       </EuiFlexItem>
     </EuiFlexGroup>
   </EuiFlexItem>
