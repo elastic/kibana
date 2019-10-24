4914baa19bd48bef0678def8ccad9531dc2c423f
chrisronline <chrisronline@gmail.com>
2019-10-24 13:11:47 -0400

    Ensure cloud cannot see setup mode

--- x-pack/legacy/plugins/monitoring/public/lib/setup_mode.js
+++ x-pack/legacy/plugins/monitoring/public/lib/setup_mode.js
@@ -7,6 +7,7 @@
 import { ajaxErrorHandlersProvider } from './ajax_error_handler';
 import { get, contains } from 'lodash';
 import chrome from 'ui/chrome';
+import { i18n } from '@kbn/i18n';
 
 function isOnPage(hash) {
   return contains(window.location.hash, hash);
@@ -139,15 +140,17 @@ export const setSetupModeMenuItem = () => {
   }
 
   const globalState = angularState.injector.get('globalState');
-  const navItems = globalState.inSetupMode
-    ? []
-    : [{
+  const navItems = [];
+  if (!globalState.inSetupMode && !chrome.getInjected('isOnCloud')) {
+    navItems.push({
       id: 'enter',
-      label: 'Enter Setup Mode',
-      description: 'Enter setup',
+      label: i18n.translate('xpack.monitoring.setupMode.enter', {
+        defaultMessage: 'Enter Setup Mode'
+      }),
       run: () => toggleSetupMode(true),
       testId: 'enterSetupMode'
-    }];
+    });
+  }
 
   angularState.scope.topNavMenu = [...navItems];
   // LOL angular
