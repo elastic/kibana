6937c587c9920eaa954c09ac27a42bb6fa9205fe
Seth Perry <seth.perry@logrhythm.com>
2019-10-24 12:22:32 -0600

    Pulling in new react conversion changes (#123)
    * pulling in new react conversion changes
    
    * fix incorrect import
    
    * fix typescript errors

---
+++ src/netmon/components/navbar.tsx
@@ -0,0 +1,92 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, { useEffect, useState } from 'react';
+import { SnackbarProvider } from 'notistack';
+import { makeStyles } from '@material-ui/core/styles';
+import { AuthContext, AuthContextValue } from '@logrhythm/nm-web-shared/contexts/auth_context';
+import {
+  BlockingProcessContext,
+  BlockingProcessContextState,
+} from '@logrhythm/nm-web-shared/contexts/blocking_process_context';
+import BlockingProcessModal from '@logrhythm/nm-web-shared/components/blocking_process/blocking_process_modal';
+import { Navbar } from '@logrhythm/nm-web-shared/components/navigation/navbar/navbar';
+import Auth from '@logrhythm/nm-web-shared/services/auth';
+import NotificationHandler from './notification_handler';
+
+const useStyles = makeStyles(
+  {
+    snackbar: {
+      maxWidth: '20vw',
+      '& > div': {
+        borderRadius: 0,
+        font: '400 100%/1.4 Ubuntu,Tahoma,sans-serif',
+        flexWrap: 'nowrap',
+      },
+      '& a': {
+        textDecoration: 'underline !important',
+      },
+    },
+  },
+  { name: 'Navbar' }
+);
+
+const LogRhythmNavbar = () => {
+  const classes = useStyles();
+
+  const [authState, setAuthState] = useState<AuthContextValue>(undefined);
+
+  const [blockingProcessMsg, setBlockingProcessMsg] = useState<string>('');
+  const blockingProcessContextState: BlockingProcessContextState = {
+    message: blockingProcessMsg,
+    block: setBlockingProcessMsg,
+    unblock: () => setBlockingProcessMsg(''),
+  };
+
+  useEffect(() => {
+    const unsub = Auth.subscribe(setAuthState);
+
+    Auth.getCurrentUser();
+
+    return unsub;
+  }, []);
+
+  if (authState === undefined) {
+    return null;
+  }
+
+  return (
+    <AuthContext.Provider value={[authState, setAuthState]}>
+      <BlockingProcessContext.Provider value={blockingProcessContextState}>
+        <SnackbarProvider
+          maxSnack={7}
+          classes={{ root: classes.snackbar }}
+          autoHideDuration={3000}
+          hideIconVariant={true}
+        >
+          <Navbar />
+          <NotificationHandler />
+          <BlockingProcessModal />
+        </SnackbarProvider>
+      </BlockingProcessContext.Provider>
+    </AuthContext.Provider>
+  );
+};
+
+export default LogRhythmNavbar; // eslint-disable-line
--- src/netmon/components/save_rule/save_rule.tsx
+++ src/netmon/components/save_rule/save_rule.tsx
@@ -33,7 +33,7 @@ import {
 import { convertQuery } from '@logrhythm/nm-web-shared/services/query_mapping';
 import {
   getTriggerCount,
-  save as saveQueryRule,
+  saveQueryRule,
   QueryRule,
 } from '@logrhythm/nm-web-shared/services/query_rules';
 import { SaveRuleForm, SaveRuleFormDataValidation } from './save_rule_form';
@@ -43,6 +43,7 @@ const validateForm = (value: QueryRule | null): SaveRuleFormDataValidation | nul
     id: !!value && !!value.id,
     severity: !!value && _.contains(['low', 'medium', 'high'], value.severity),
     query: !!value && !!value.query,
+    enabled: true,
   };
 
   return _.every(Object.values(validation), v => v) ? null : validation;
@@ -88,6 +89,7 @@ const reducer = (state: SaveRuleState, action: SaveRuleAction): SaveRuleState =>
           id: '',
           severity: '',
           query: action.query,
+          enabled: true,
         },
         saveSuccess: null,
         triggerCount: null,
