299aa9af1239d8c1e0e65362001611ba9f2b329a
Sébastien <sebastien.loix@elastic.co>
2019-10-25 15:10:11 +0200

    Add basic fields for text type

--- x-pack/legacy/plugins/index_management/public/components/mappings_editor/components/document_fields/fields/edit_field/edit_field_form_row.tsx
+++ x-pack/legacy/plugins/index_management/public/components/mappings_editor/components/document_fields/fields/edit_field/edit_field_form_row.tsx
@@ -8,7 +8,7 @@ import React, { useState } from 'react';
 
 import { EuiFlexGroup, EuiFlexItem, EuiTitle, EuiText, EuiSwitch } from '@elastic/eui';
 
-import { ToggleField, UseField, FieldHook, FieldConfig } from '../../../../shared_imports';
+import { ToggleField, UseField, FieldConfig, FormDataProvider } from '../../../../shared_imports';
 
 import { PARAMETERS_DEFINITION } from '../../../../constants';
 import { ParameterName } from '../../../../types';
@@ -30,89 +30,94 @@ const PADDING_LEFT_NO_TOGGLE = '66px';
 const getFieldConfig = (param: ParameterName): FieldConfig =>
   PARAMETERS_DEFINITION[param].fieldConfig || {};
 
-export const EditFieldFormRow = ({
-  title,
-  description,
-  ariaId,
-  withToggle = true,
-  direction = 'row',
-  formFieldPath,
-  children,
-}: Props) => {
-  const initialVisibleState =
-    formFieldPath === undefined
-      ? !Boolean(withToggle)
-      : (getFieldConfig(formFieldPath).defaultValue! as boolean);
+export const EditFieldFormRow = React.memo(
+  ({
+    title,
+    description,
+    ariaId,
+    withToggle = true,
+    direction = 'row',
+    formFieldPath,
+    children,
+  }: Props) => {
+    const initialVisibleState =
+      formFieldPath === undefined
+        ? !Boolean(withToggle)
+        : (getFieldConfig(formFieldPath).defaultValue! as boolean);
 
-  const [isContentVisible, setIsContentVisible] = useState<boolean>(initialVisibleState);
+    const [isContentVisible, setIsContentVisible] = useState<boolean>(initialVisibleState);
 
-  const isChildrenFunction = typeof children === 'function';
+    const isChildrenFunction = typeof children === 'function';
 
-  const onToggle = () => {
-    setIsContentVisible(!isContentVisible);
-  };
+    const onToggle = () => {
+      setIsContentVisible(!isContentVisible);
+    };
 
-  const onFormToggleChange = (field: FieldHook) => (e: React.ChangeEvent<HTMLInputElement>) => {
-    const isSelected = e.target.checked;
+    const renderToggleInput = () =>
+      formFieldPath === undefined ? (
+        <EuiSwitch checked={isContentVisible} onChange={onToggle} data-test-subj="input" />
+      ) : (
+        <UseField
+          path={formFieldPath}
+          component={ToggleField}
+          config={getFieldConfig(formFieldPath)}
+        />
+      );
 
-    // We both set the form field value + set the isContentVisible
-    field.setValue(isSelected);
-    setIsContentVisible(isSelected);
-  };
-
-  const renderToggleInput = () =>
-    formFieldPath === undefined ? (
-      <EuiSwitch checked={isContentVisible} onChange={onToggle} data-test-subj="input" />
-    ) : (
-      <UseField path={formFieldPath} component={ToggleField} config={getFieldConfig(formFieldPath)}>
-        {field => (
-          <ToggleField field={field} euiFieldProps={{ onChange: onFormToggleChange(field) }} />
+    const renderContent = () => (
+      <EuiFlexGroup className="mappings-editor__edit-field__formRow">
+        {withToggle && (
+          <EuiFlexItem grow={false} className="mappingsEditor__editFieldFormRow__toggle">
+            {renderToggleInput()}
+          </EuiFlexItem>
         )}
-      </UseField>
+        <EuiFlexItem>
+          <EuiFlexGroup direction={direction}>
+            {(title || description) && (
+              <EuiFlexItem
+                style={{
+                  paddingLeft: withToggle === false ? PADDING_LEFT_NO_TOGGLE : undefined,
+                }}
+              >
+                {title && (
+                  <EuiTitle
+                    id={`${ariaId}-title`}
+                    size="xs"
+                    className="mappings-editor__edit-field__formRow__title"
+                  >
+                    {title}
+                  </EuiTitle>
+                )}
+                {description && (
+                  <EuiText id={ariaId} size="s" color="subdued">
+                    {description}
+                  </EuiText>
+                )}
+              </EuiFlexItem>
+            )}
+            {(isContentVisible || isChildrenFunction) && (
+              <EuiFlexItem
+                style={{
+                  paddingLeft: withToggle === false ? PADDING_LEFT_NO_TOGGLE : undefined,
+                }}
+              >
+                {isChildrenFunction ? (children as ChildrenFunc)(isContentVisible) : children}
+              </EuiFlexItem>
+            )}
+          </EuiFlexGroup>
+        </EuiFlexItem>
+      </EuiFlexGroup>
     );
 
-  return (
-    <EuiFlexGroup className="mappings-editor__edit-field__formRow">
-      {withToggle && (
-        <EuiFlexItem grow={false} className="mappingsEditor__editFieldFormRow__toggle">
-          {renderToggleInput()}
-        </EuiFlexItem>
-      )}
-      <EuiFlexItem>
-        <EuiFlexGroup direction={direction}>
-          {(title || description) && (
-            <EuiFlexItem
-              style={{
-                paddingLeft: withToggle === false ? PADDING_LEFT_NO_TOGGLE : undefined,
-              }}
-            >
-              {title && (
-                <EuiTitle
-                  id={`${ariaId}-title`}
-                  size="s"
-                  className="mappings-editor__edit-field__formRow__title"
-                >
-                  {title}
-                </EuiTitle>
-              )}
-              {description && (
-                <EuiText id={ariaId} size="s" color="subdued">
-                  {description}
-                </EuiText>
-              )}
-            </EuiFlexItem>
-          )}
-          {(isContentVisible || isChildrenFunction) && (
-            <EuiFlexItem
-              style={{
-                paddingLeft: withToggle === false ? PADDING_LEFT_NO_TOGGLE : undefined,
-              }}
-            >
-              {isChildrenFunction ? (children as ChildrenFunc)(isContentVisible) : children}
-            </EuiFlexItem>
-          )}
-        </EuiFlexGroup>
-      </EuiFlexItem>
-    </EuiFlexGroup>
-  );
-};
+    return formFieldPath ? (
+      <FormDataProvider pathsToWatch={formFieldPath}>
+        {formData => {
+          setIsContentVisible(formData[formFieldPath]);
+          return renderContent();
+        }}
+      </FormDataProvider>
+    ) : (
+      renderContent()
+    );
+  }
+);
