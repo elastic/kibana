804aadbb0b3f387142fcb591d13327196d769169
Joe Reuter <johannes.reuter@elastic.co>
2019-10-25 12:35:13 +0200

    Kibana app migration: Centralize home dependencies (#48618) (#49294)

--- src/legacy/core_plugins/kibana/public/home/components/tutorial/tutorial.js
+++ src/legacy/core_plugins/kibana/public/home/components/tutorial/tutorial.js
@@ -37,7 +37,7 @@ import {
 import * as StatusCheckStates from './status_check_states';
 import { injectI18n, FormattedMessage } from '@kbn/i18n/react';
 import { i18n } from '@kbn/i18n';
-import chrome from 'ui/chrome';
+import { getServices } from '../../kibana_services';
 
 const INSTRUCTIONS_TYPE = {
   ELASTIC_CLOUD: 'elasticCloud',
@@ -94,7 +94,7 @@ class TutorialUi extends React.Component {
       });
     }
 
-    chrome.breadcrumbs.set([
+    getServices().chrome.setBreadcrumbs([
       {
         text: homeTitle,
         href: '#/home'
