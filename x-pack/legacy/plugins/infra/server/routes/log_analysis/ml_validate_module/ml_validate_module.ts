6ccbd499d0182fe4a2c5334aa6d783a28f934227
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2019-10-25 16:26:18 +0200

    Validate indexPattern and timestamp fields

--- x-pack/legacy/plugins/infra/server/routes/log_analysis/ml_validate_module/ml_validate_module.ts
+++ x-pack/legacy/plugins/infra/server/routes/log_analysis/ml_validate_module/ml_validate_module.ts
@@ -15,6 +15,7 @@ import {
   LOG_ANALYSIS_VALIDATE_ML_MODULE_PATH,
   validateMlModuleRequestPayloadRT,
   validateMlModuleResponsePayloadRT,
+  ValidateMlModuleError,
 } from '../../../../common/http_api';
 
 import { throwErrors } from '../../../../common/runtime_types';
@@ -29,18 +30,41 @@ export const initMlValidateModuleRoute = ({ framework }: InfraBackendLibs) => {
         fold(throwErrors(Boom.badRequest), identity)
       );
 
-      // TODO Implement
+      const errors: ValidateMlModuleError[] = [];
+      const { timestamp, indexPatternName } = payload.data;
+      // 1. Check that any of the indices exist:
+      const indices = await framework.callWithRequest(req, 'indices.get', {
+        index: indexPatternName,
+      });
+
+      const indexNames = Object.keys(indices);
+
+      if (indexNames.length === 0) {
+        errors.push({
+          field: 'indexPatternName',
+          message: `No indices match the pattern \`${indexPatternName}\`.`,
+        });
+      } else {
+        for (const index of indexNames) {
+          const metadata = indices[index];
+          if (!metadata.mappings.properties[timestamp]) {
+            errors.push({
+              field: 'timestamp',
+              message: `Index \`${index}\` has no field \`${timestamp}\`. Ensure the "Timestamp" field in your settings exists in all indices.`,
+            });
+          } else if (metadata.mappings.properties[timestamp].type !== 'date') {
+            errors.push({
+              field: 'timestamp',
+              message: `Field \`${timestamp}\` in index \`${index}\` is not of type \`date\`. Ensure the "Timestamp" field in your settings has \`date\` type.`,
+            });
+          }
+        }
+      }
 
       return res.response(
         validateMlModuleResponsePayloadRT.encode({
           data: {
-            errors: [
-              { field: 'timestamp', message: `Timestamp: ${payload.data.timestamp}` },
-              {
-                field: 'indexPatternName',
-                message: `indexPatternName: ${payload.data.indexPatternName}`,
-              },
-            ],
+            errors,
           },
         })
       );
