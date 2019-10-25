07c6d9bfa2be697888c522d16a97409e358ec233
Brian Seeders <brian.seeders@elastic.co>
2019-10-25 12:57:46 -0400

    Re-add pipeline for flaky test runner job (#48781)
    * Revert "Revert "Add pipeline for flaky test runner job (#46740)""
    
    This reverts commit 7d96a13fad5337f262743b51d5bdd6103d2c51a4.
    
    Also reconcile changes to Jenkinsfile since original revert happened
    
    * Fix param parsing and add missed change
    
    * Add missing variable

--- src/dev/precommit_hook/casing_check_config.js
+++ src/dev/precommit_hook/casing_check_config.js
@@ -43,7 +43,7 @@ export const IGNORE_FILE_GLOBS = [
   '**/{webpackShims,__mocks__}/**/*',
   'x-pack/docs/**/*',
   'src/dev/tslint/rules/*',
-  'Jenkinsfile',
+  '**/Jenkinsfile*',
   'Dockerfile*',
 
   // filename must match language code which requires capital letters
