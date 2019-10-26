0bd8a6d7b90499b45b638a5f6a5954f77f273f49
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2019-10-26 12:22:09 +0200

    Fix merge conflicts (#48457)

--- src/legacy/core_plugins/kibana/public/discover/context/query/actions.js
+++ src/legacy/core_plugins/kibana/public/discover/context/query/actions.js
@@ -26,7 +26,7 @@ import { fetchAnchorProvider } from '../api/anchor';
 import { fetchContextProvider } from '../api/context';
 import { QueryParameterActionsProvider } from '../query_parameters';
 import { FAILURE_REASONS, LOADING_STATUS } from './constants';
-import { MarkdownSimple } from '../../../../kibana_react/public';
+import { MarkdownSimple } from '../../../../../kibana_react/public';
 
 export function QueryActionsProvider(Private, Promise) {
   const fetchAnchor = Private(fetchAnchorProvider);
