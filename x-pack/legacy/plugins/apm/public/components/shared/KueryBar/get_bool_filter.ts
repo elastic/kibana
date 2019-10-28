5dcba8c58cbf90f88cbb6d3dafae8a12fc1503ba
Dario Gieselaar <dario.gieselaar@elastic.co>
2019-10-28 10:21:39 +0100

    Remove missing labels

--- x-pack/legacy/plugins/apm/public/components/shared/KueryBar/get_bool_filter.ts
+++ x-pack/legacy/plugins/apm/public/components/shared/KueryBar/get_bool_filter.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { ESFilter } from 'elasticsearch';
+import { ESFilter } from '../../../../typings/elasticsearch';
 import {
   TRANSACTION_TYPE,
   ERROR_GROUP_ID,
