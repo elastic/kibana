2285de7f88f0f7eb8df41dca8c4312e442a56259
Court Ewing <court@epixa.com>
2019-10-28 11:40:27 -0400

    Remove experimental code app (#49404)
    * Fix erroneous code plugin import in apm
    
    * Stop running legacy code plugin
    
    * Stop testing code app and apis
    
    * Remove code plugin source and deprecate config
    
    * Remove code plugin docs
    
    * Remove xpack.code translations
    
    * Remove code import on api_integration
    
    * Remove code privilege from security test
    
    * remove two more mentions of code
    
    * remove code es_archives
    
    * remove code doc images

--- x-pack/test/functional/config.js
+++ x-pack/test/functional/config.js
@@ -44,7 +44,6 @@ export default async function ({ readConfigFile }) {
       resolve(__dirname, './apps/status_page'),
       resolve(__dirname, './apps/timelion'),
       resolve(__dirname, './apps/upgrade_assistant'),
-      resolve(__dirname, './apps/code'),
       resolve(__dirname, './apps/visualize'),
       resolve(__dirname, './apps/uptime'),
       resolve(__dirname, './apps/saved_objects_management'),
