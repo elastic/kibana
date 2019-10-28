bb4ec159e7ad3dc5cf6350c980b0a1799517afbe
Nathan L Smith <smith@nlsmith.com>
2019-10-28 18:35:32 -0500

    [APM] Performance comparison charts by browser
    Show a chart with average page load broken down by user agent name on the RUM overview.
    
    I tested this by setting up a RUM agent on a sample app on my test cloud cluster.
    
    On the internal APM dev cluster you'll need to set the time range to about 90 days then look at the "client" app and find the time range where there are requests. Unfortunately with this sample data the only series you see is "Other" because there aren't a lot of requests.
    
    Fixes #43342

---
+++ x-pack/legacy/plugins/apm/public/components/shared/charts/TransactionCharts/BrowserLineChart.test.tsx
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { BrowserLineChart } from './BrowserLineChart';
+
+describe('BrowserLineChart', () => {
+  describe('render', () => {
+    it('renders', () => {
+      const props = { series: [] };
+
+      expect(() => shallow(<BrowserLineChart {...props} />)).not.toThrowError();
+    });
+  });
+});
