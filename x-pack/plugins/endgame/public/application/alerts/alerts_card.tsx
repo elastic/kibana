93ae554ff6c6ed76223a5ada8a15058f4dcd9a9b
Paul Tavares <paul.tavares@elastic.co>
2019-10-29 15:27:26 -0400

    [FE] Alerts + Endpoints card converted to functional components

--- x-pack/plugins/endgame/public/application/alerts/alerts_card.tsx
+++ x-pack/plugins/endgame/public/application/alerts/alerts_card.tsx
@@ -4,22 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { Component } from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { EuiCard } from '@elastic/eui';
 import { routePathsById } from '../../common/route_paths';
 
-class AlertsCard extends Component<RouteComponentProps> {
-  render() {
-    return (
-      <EuiCard
-        layout="horizontal"
-        title="Alerts and Response"
-        description="View and follow up on alerts"
-        onClick={() => this.props.history.push(routePathsById.alerts.path)}
-      />
-    );
-  }
+function AlertsCard(props: RouteComponentProps) {
+  return (
+    <EuiCard
+      layout="horizontal"
+      title="Alerts and Response"
+      description="View and follow up on alerts"
+      onClick={() => props.history.push(routePathsById.alerts.path)}
+    />
+  );
 }
 
 export const AlertsCardShowcase = withRouter(AlertsCard);
--- x-pack/plugins/endgame/public/application/endpoints/enpoints_card.tsx
+++ x-pack/plugins/endgame/public/application/endpoints/enpoints_card.tsx
@@ -4,22 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { Component } from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { EuiCard } from '@elastic/eui';
 import { routePathsById } from '../../common/route_paths';
 
-class EndpointsCard extends Component<RouteComponentProps> {
-  render() {
-    return (
-      <EuiCard
-        layout="horizontal"
-        title="Endpoint Management"
-        description="View and manage the endpoints"
-        onClick={() => this.props.history.push(routePathsById.endpoints.path)}
-      />
-    );
-  }
+function EndpointsCard(props: RouteComponentProps) {
+  return (
+    <EuiCard
+      layout="horizontal"
+      title="Endpoint Management"
+      description="View and manage the endpoints"
+      onClick={() => props.history.push(routePathsById.endpoints.path)}
+    />
+  );
 }
 
 export const EndpointsCardShowcase = withRouter(EndpointsCard);
