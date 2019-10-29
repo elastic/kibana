c1b5e12f8ba724111e77b8758a953457d6c129d1
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2019-10-28 21:12:12 -0700

    Added newsfeed button to navigation controls on the right side

--- x-pack/plugins/newsfeed/public/plugin.tsx
+++ x-pack/plugins/newsfeed/public/plugin.tsx
@@ -4,12 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import ReactDOM from 'react-dom';
+import React from 'react';
 import {
   PluginInitializerContext,
   CoreSetup,
   CoreStart,
   Plugin,
 } from '../../../../src/core/public';
+import { MailNavButton } from './components/spaces_header_nav_button';
 
 export type Setup = void;
 export type Start = void;
@@ -19,5 +22,15 @@ export class NewsfeedPublicPlugin implements Plugin<Setup, Start> {
 
   public setup(core: CoreSetup): Setup {}
 
-  public start(core: CoreStart): Start {}
+  public start(core: CoreStart): Start {
+    function mount(targetDomElement: HTMLElement) {
+      ReactDOM.render(<MailNavButton />, targetDomElement);
+      return () => ReactDOM.unmountComponentAtNode(targetDomElement);
+    }
+
+    core.chrome.navControls.registerRight({
+      order: 1000,
+      mount,
+    });
+  }
 }
