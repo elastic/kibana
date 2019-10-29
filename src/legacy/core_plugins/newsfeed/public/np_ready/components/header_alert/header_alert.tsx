55445b66973540a90e508aa1964273dc75e8eb2e
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2019-10-29 16:42:49 -0700

    Moved newsfeed plugin to OSS and added for the styles purpose new folder for legacy plugin 'newsfeed' with the same id to support this

--- src/legacy/core_plugins/newsfeed/public/np_ready/components/header_alert/header_alert.tsx
+++ src/legacy/core_plugins/newsfeed/public/np_ready/components/header_alert/header_alert.tsx
@@ -1,14 +1,27 @@
 /*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import { EuiFlexGroup, EuiFlexItem, EuiBadgeProps, EuiI18n } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiI18n } from '@elastic/eui';
 
 interface IEuiHeaderAlertProps {
   action: JSX.Element;
@@ -49,7 +62,7 @@ export const EuiHeaderAlert = ({
             <EuiFlexItem grow={false}>{badgeContent}</EuiFlexItem>
           </EuiFlexGroup>
 
-          <div className="euiHeaderAlert__title">{title}</div>
+          <div className="test_style">{title}</div>
           <div className="euiHeaderAlert__text">{text}</div>
           <div className="euiHeaderAlert__action euiLink">{action}</div>
         </div>
--- src/plugins/newsfeed/public/components/flyout_list.tsx
+++ src/plugins/newsfeed/public/components/flyout_list.tsx
@@ -1,7 +1,20 @@
 /*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 import React, { useCallback, useContext } from 'react';
 import {
@@ -19,7 +32,7 @@ import {
   EuiBadge,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { EuiHeaderAlert } from './header_alert/header_alert';
+import { EuiHeaderAlert } from '../../../../legacy/core_plugins/newsfeed/public/np_ready/components/header_alert/header_alert';
 import { NewsfeedContext } from './newsfeed_header_nav_button';
 
 export const NewsfeedFlyout = () => {
@@ -32,7 +45,7 @@ export const NewsfeedFlyout = () => {
           <h2 id="flyoutSmallTitle">What's new</h2>
         </EuiTitle>
       </EuiFlyoutHeader>
-      <EuiFlyoutBody>
+      <EuiFlyoutBody className={'kbnNews__flyoutAlerts'}>
         <EuiHeaderAlert
           title="Control access to features"
           text="Show or hide applications and features per space in Kibana."
