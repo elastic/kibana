ddebf904929963e9aef592927acbd428a5349ff8
spalger <email@spalger.com>
2019-10-29 06:30:23 -0700

    update a11y test script for pipelines

--- test/scripts/jenkins_accessibility.sh
+++ test/scripts/jenkins_accessibility.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
 set -e
-trap 'node "$KIBANA_DIR/src/dev/failed_tests/cli"' EXIT
 
-node scripts/build --debug --oss;
-linuxBuild="$(find "$KIBANA_DIR/target" -name 'kibana-oss-*-linux-x86_64.tar.gz')"
-installDir="$PARENT_DIR/install/kibana"
-mkdir -p "$installDir"
-tar -xzf "$linuxBuild" -C "$installDir" --strip=1
+if [[ -n "$IS_PIPELINE_JOB" ]] ; then
+  source src/dev/ci_setup/setup_env.sh
+fi
+
+if [[ -z "$IS_PIPELINE_JOB" ]] ; then
+  echo " -> building and extracting default Kibana distributable for use in functional tests"
+  node scripts/build --debug --no-oss
+
+  linuxBuild="$(find "$KIBANA_DIR/target" -name 'kibana-*-linux-x86_64.tar.gz')"
+  installDir="$PARENT_DIR/install/kibana"
+
+  mkdir -p "$installDir"
+  tar -xzf "$linuxBuild" -C "$installDir" --strip=1
+
+  export KIBANA_INSTALL_DIR="$installDir"
+else
+  installDir="$PARENT_DIR/install/kibana"
+  destDir="${installDir}-${CI_WORKER_NUMBER}"
+  cp -R "$installDir" "$destDir"
+
+  export KIBANA_INSTALL_DIR="$destDir"
+fi
 
 export TEST_BROWSER_HEADLESS=1
 
