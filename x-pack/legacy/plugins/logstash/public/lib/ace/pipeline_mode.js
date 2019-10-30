52adce9f882a611ec3bd91ea4f6317b6c3dea50d
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2019-10-29 21:04:03 -0500

    Adding a PipelineMode for logstash Ace editor

---
+++ x-pack/legacy/plugins/logstash/public/lib/ace/pipeline_mode.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import ace from 'ace';
+import {
+  PipelineHighlightRules
+} from './pipeline_highlight_rules';
+
+const AceTokenizer = ace.acequire('ace/tokenizer').Tokenizer;
+const TextMode = ace.acequire('ace/mode/text').Mode;
+
+export class PipelineMode extends TextMode {
+  constructor() {
+    super();
+    this.$tokenizer = new AceTokenizer(new PipelineHighlightRules().getRules());
+    this.HighlightRules = PipelineHighlightRules;
+  }
+}
