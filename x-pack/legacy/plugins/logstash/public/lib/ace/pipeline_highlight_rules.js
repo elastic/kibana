d6e319c1dece8b24746296fc72b7795fb58bfe3f
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2019-10-30 09:43:25 -0500

    adding logtash keywords for filter, input and output

--- x-pack/legacy/plugins/logstash/public/lib/ace/pipeline_highlight_rules.js
+++ x-pack/legacy/plugins/logstash/public/lib/ace/pipeline_highlight_rules.js
@@ -10,6 +10,111 @@ const {
   TextHighlightRules
 } = ace.acequire('ace/mode/text_highlight_rules');
 
+const LOGSTASH_KEYWORDS = [
+  'aggregate',
+  'alter',
+  'bytes',
+  'cidr',
+  'cipher',
+  'clone',
+  'csv',
+  'date',
+  'de_dot',
+  'dissect',
+  'dns',
+  'drop',
+  'elapsed',
+  'elasticsearch',
+  'environment',
+  'extractnumbers',
+  'fingerprint',
+  'geoip',
+  'grok',
+  'http',
+  'i18n',
+  'java_uuid',
+  'jdbc_static',
+  'jdbc_streaming',
+  'json',
+  'json_encode',
+  'kv',
+  'memcached',
+  'metricize',
+  'metrics',
+  'mutate',
+  'prune',
+  'range',
+  'ruby',
+  'sleep',
+  'split',
+  'syslog_pri',
+  'threats_classifier',
+  'throttle',
+  'tld',
+  'translate',
+  'truncate',
+  'urldecode',
+  'useragent',
+  'uuid',
+  'xml'
+];
+
+const LOGSTASH_INPUT_OUTPUT = [
+  'azure_event_hubs',
+  'beats',
+  'cloudwatch',
+  'couchdb_changes',
+  'dead_letter_queue',
+  'elasticsearch',
+  'exec',
+  'file',
+  'ganglia',
+  'gelf',
+  'generator',
+  'github',
+  'google_cloud_storage',
+  'google_pubsub',
+  'graphite',
+  'heartbeat',
+  'http',
+  'http_poller',
+  'imap',
+  'irc',
+  'java_generator',
+  'java_stdin',
+  'jdbc',
+  'jms',
+  'jmx',
+  'kafka',
+  'kinesis',
+  'log4j',
+  'lumberjack',
+  'meetup',
+  'pipe',
+  'puppet_facter',
+  'rabbitmq',
+  'redis',
+  'relp',
+  'rss',
+  's3',
+  'salesforce',
+  'snmp',
+  'snmptrap',
+  'sqlite',
+  'sqs',
+  'stdin',
+  'stomp',
+  'syslog',
+  'tcp',
+  'twitter',
+  'udp',
+  'unix',
+  'varnishlog',
+  'websocket',
+  'wmi',
+  'xmpp'
+];
+
 export function PipelineHighlightRules() {
   this.name = 'PipelineHighlightRules';
   this.$rules = {
@@ -20,7 +125,11 @@ export function PipelineHighlightRules() {
       },
       {
         token: 'entity.name.function',
-        regex: 'mutate|grok'
+        regex: LOGSTASH_KEYWORDS.join('|')
+      },
+      {
+        token: 'entity.name.function',
+        regex: LOGSTASH_INPUT_OUTPUT.join('|')
       },
       {
         token: 'storage.type',
@@ -29,6 +138,10 @@ export function PipelineHighlightRules() {
       {
         token: 'string',
         regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'
+      },
+      {
+        token: 'constant.numeric',
+        regex: '\\d+'
       },
       {
         token: 'keyword.operator',
