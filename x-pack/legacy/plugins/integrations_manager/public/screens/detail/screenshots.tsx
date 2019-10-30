34e10ebde8dd6c09826fc3e76fc6feb9d9029a68
neptunian <sandra.gonzales@elastic.co>
2019-10-30 16:36:12 -0400

    hardcode image width for ie11

--- x-pack/legacy/plugins/integrations_manager/public/screens/detail/screenshots.tsx
+++ x-pack/legacy/plugins/integrations_manager/public/screens/detail/screenshots.tsx
@@ -55,11 +55,16 @@ export function Screenshots(props: ScreenshotProps) {
           </NestedEuiFlexItem>
         )}
         <NestedEuiFlexItem>
+          { /* by default EuiImage sets width to 100% and Figure to 22.5rem for large images,
+              set image to same width
+            */
+         }
           <EuiImage
             url={toImage(image.src)}
             alt="screenshot image preview"
             size="l"
             allowFullScreen
+            style={{'width': '22.5rem'}}
           />
         </NestedEuiFlexItem>
       </ScreenshotsContainer>
