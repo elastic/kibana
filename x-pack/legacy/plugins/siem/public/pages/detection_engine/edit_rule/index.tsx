9b3d06cfcbfea794604a1c2c168f3190b673c0a2
Michael Marcialis <michael.marcialis@elastic.co>
2019-10-31 15:49:36 -0400

    rough out edit layout

--- x-pack/legacy/plugins/siem/public/pages/detection_engine/edit_rule/index.tsx
+++ x-pack/legacy/plugins/siem/public/pages/detection_engine/edit_rule/index.tsx
@@ -4,12 +4,53 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiButton } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPanel,
+  EuiSpacer,
+  EuiTabbedContent,
+} from '@elastic/eui';
 import React from 'react';
 
 import { HeaderPage } from '../../../components/header_page';
+import { HeaderSection } from '../../../components/header_section';
 import { SpyRoute } from '../../../utils/route/spy_routes';
 import * as i18n from './translations';
+
+const Define = React.memo(() => (
+  <>
+    <EuiSpacer />
+
+    <EuiPanel>
+      <HeaderSection title="Define rule" />
+    </EuiPanel>
+  </>
+));
+Define.displayName = 'Define';
+
+const About = React.memo(() => (
+  <>
+    <EuiSpacer />
+
+    <EuiPanel>
+      <HeaderSection title="About rule" />
+    </EuiPanel>
+  </>
+));
+About.displayName = 'About';
+
+const Schedule = React.memo(() => (
+  <>
+    <EuiSpacer />
+
+    <EuiPanel>
+      <HeaderSection title="Schedule rule" />
+    </EuiPanel>
+  </>
+));
+Schedule.displayName = 'Schedule';
 
 export const EditRuleComponent = React.memo(() => {
   return (
@@ -19,13 +60,58 @@ export const EditRuleComponent = React.memo(() => {
           href: '#detection-engine/rules/rule-details',
           text: 'Back to automated exfiltration',
         }}
-        border
         title={i18n.PAGE_TITLE}
       >
-        <EuiButton fill href="#/detection-engine/rules/rule-details" iconType="save">
-          {'Save changes'}
-        </EuiButton>
+        <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
+          <EuiFlexItem grow={false}>
+            <EuiButton href="#/detection-engine/rules/rule-details" iconType="cross">
+              {'Cancel'}
+            </EuiButton>
+          </EuiFlexItem>
+
+          <EuiFlexItem grow={false}>
+            <EuiButton fill href="#/detection-engine/rules/rule-details" iconType="save">
+              {'Save changes'}
+            </EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
       </HeaderPage>
+
+      <EuiTabbedContent
+        tabs={[
+          {
+            id: 'tabDefine',
+            name: 'Define',
+            content: <Define />,
+          },
+          {
+            id: 'tabAbout',
+            name: 'About',
+            content: <About />,
+          },
+          {
+            id: 'tabSchedule',
+            name: 'Schedule',
+            content: <Schedule />,
+          },
+        ]}
+      />
+
+      <EuiSpacer />
+
+      <EuiFlexGroup alignItems="center" gutterSize="s" justifyContent="flexEnd" responsive={false}>
+        <EuiFlexItem grow={false}>
+          <EuiButton href="#/detection-engine/rules/rule-details" iconType="cross">
+            {'Cancel'}
+          </EuiButton>
+        </EuiFlexItem>
+
+        <EuiFlexItem grow={false}>
+          <EuiButton fill href="#/detection-engine/rules/rule-details" iconType="save">
+            {'Save changes'}
+          </EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
 
       <SpyRoute />
     </>
