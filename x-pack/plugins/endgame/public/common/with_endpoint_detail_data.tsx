1be82453abd04f6cd465ee2c282c67032a45c102
Paul Tavares <paul.tavares@elastic.co>
2019-10-31 09:53:36 -0400

    Endpoint List and Endpoint Detail higher order components
    - Added two higher order components that handle retrieving API data
      for endpoints (list + detail) and passing that as a prop to the
      wrapped component

---
+++ x-pack/plugins/endgame/public/common/with_endpoint_detail_data.tsx
@@ -0,0 +1,68 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { PureComponent, ComponentType } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+import { EndgameAppContext } from './app_context';
+
+/**
+ * Creates a higher order component around the given component on input and passes a prop named
+ * `endpoint` when that component is rendered. `endpoint` is an `object` (or `null`) containing
+ * the information about the given endpoint.
+ * The higher order component needs to be given the props from the `react-router-dom` `Route`
+ * match - specifically `props.match.params.id` should be the ID of the endpoint to be retrieved.
+ *
+ * @param {ComponentType} C
+ *
+ * @example
+ *
+ * const TestComponent = withEndpointDetailData(function({endpoint}){
+ *   return endpointData ?
+ *    (<div>{JSON.stringify(endpoint, null, 4)}</div>) :
+ *    (<div>no endpoint data yet</div>);
+ * })
+ * //
+ * //.... somewhere else
+ * //
+ * <Route path="/endpoint/view/:id" exact component={TestComponent} />
+ */
+export function withEndpointDetailData<P extends { endpoint: null | object } & RouteComponentProps>(
+  C: ComponentType<P>
+) {
+  return class ComponentWithEndpointsData extends PureComponent<P, { endpoint: null | object }> {
+    static contextType = EndgameAppContext;
+
+    context!: React.ContextType<typeof EndgameAppContext>;
+
+    state = {
+      endpoint: null,
+    };
+
+    render() {
+      const { children, ...props } = this.props;
+
+      return (
+        // FIXME: need help on this.... What am I missing in the TS generic to avoid this error?
+        <C {...props} endpoint={this.state.endpoint}>
+          {children}
+        </C>
+      );
+    }
+
+    async componentDidMount() {
+      // Load some API data for this component
+      const endpoint = await this.context.appContext.core.http
+        // FIXME: need help fixing issue below with `id` and TS validation
+        .get(`${this.context.apiPrefixPath}/endpoints/${this.props.match.params.id}`)
+        .catch((e: Error) => {
+          console.error(e); //eslint-disable-line
+          return Promise.resolve([]);
+        });
+      this.setState({ endpoint });
+    }
+  };
+}
---
+++ x-pack/plugins/endgame/public/common/with_endpoints_list_data.tsx
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { PureComponent, ComponentType } from 'react';
+import { EndgameAppContext } from './app_context';
+
+/**
+ * Creates a higher order component around the given component on input and passes a prop named
+ * `endpoints` when that component is rendered. `endpoints` is an `array<Object>` containing
+ * the list of endpoints.
+ *
+ * @param {ComponentType} C
+ */
+export function withEndPointsListData<P extends { endpoints: object[] }>(C: ComponentType<P>) {
+  return class ComponentWithEndpointsData extends PureComponent<P, { endpoints: object[] }> {
+    static contextType = EndgameAppContext;
+
+    context!: React.ContextType<typeof EndgameAppContext>;
+
+    state = {
+      endpoints: [],
+    };
+
+    render() {
+      const { children, ...props } = this.props;
+
+      return (
+        // FIXME: need help on this.... What am I missing in the TS generic to avoid this error?
+        <C {...props} endpoints={this.state.endpoints}>
+          {children}
+        </C>
+      );
+    }
+
+    async componentDidMount() {
+      // Load some API data for this component
+      const endpoints = await this.context.appContext.core.http
+        .get(`${this.context.apiPrefixPath}/endpoints`)
+        .catch((e: Error) => {
+          console.error(e); //eslint-disable-line
+          return Promise.resolve({ hits: { hits: [] } });
+        });
+
+      this.setState({ endpoints: endpoints.hits.hits });
+    }
+  };
+}
