68fd1d844e2d129ef0f239293f90189838b3a589
spalger <email@spalger.com>
2019-10-30 22:30:27 -0700

    don't print initial autofix output

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -6,6 +6,6 @@ export TEST_BROWSER_HEADLESS=1
 
 ## TODO: remove before merging and commit auto fixed source
 echo " -- autofixing eslint violations"
-node scripts/eslint --fix || true
+node scripts/eslint --fix > /dev/null || true
 
 "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
