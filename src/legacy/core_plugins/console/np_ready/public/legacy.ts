e238a5dcc2ad62d7a383d2b1831eaae0234c42eb
Joe Reuter <johannes.reuter@elastic.co>
2019-10-31 10:33:12 +0100

    fix other bugs

--- src/legacy/core_plugins/console/np_ready/public/legacy.ts
+++ src/legacy/core_plugins/console/np_ready/public/legacy.ts
@@ -30,8 +30,8 @@ import uiRoutes from 'ui/routes';
 import { DOC_LINK_VERSION } from 'ui/documentation_links';
 import { I18nContext } from 'ui/i18n';
 import { ResizeChecker } from 'ui/resize_checker';
-import 'ui/autoload/styles';
 /* eslint-enable @kbn/eslint/no-restricted-paths */
+
 import template from '../../public/quarantined/index.html';
 import { App, AppUnmount, NotificationsSetup } from '../../../../../core/public';
 
