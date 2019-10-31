dc5fb6e39bce9bb92307e7cc43a5b464f3594b6a
Timothy Sullivan <tsullivan@elastic.co>
2019-10-31 14:08:37 -0700

    polish the code changes

--- src/plugins/newsfeed/public/index.ts
+++ src/plugins/newsfeed/public/index.ts
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import { PluginInitializerContext } from '../../../../src/core/public';
+import { PluginInitializerContext } from 'src/core/public';
 import { NewsfeedPublicPlugin } from './plugin';
 
 export function plugin(initializerContext: PluginInitializerContext) {
