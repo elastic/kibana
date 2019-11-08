7b003cd75d022bdacb05a7dd29a7212881846a34
John Schulz <john.schulz@elastic.co>
2019-11-08 13:50:46 -0500

    Use NP feature_catalogue.register

--- x-pack/legacy/plugins/integrations_manager/public/register_feature.ts
+++ x-pack/legacy/plugins/integrations_manager/public/register_feature.ts
@@ -4,20 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import {
-  FeatureCatalogueCategory,
-  FeatureCatalogueRegistryProvider,
-} from 'ui/registry/feature_catalogue';
+import { npSetup } from 'ui/new_platform';
+import { FeatureCatalogueCategory } from 'ui/registry/feature_catalogue';
 import { PLUGIN } from '../common/constants';
 import { patterns } from './routes';
 
 // This defines what shows up in the registry found at /app/kibana#/home and /app/kibana#/home/feature_directory
-FeatureCatalogueRegistryProvider.register(() => ({
-  id: PLUGIN.ID,
-  title: PLUGIN.TITLE,
-  description: PLUGIN.DESCRIPTION,
-  icon: PLUGIN.ICON,
-  path: patterns.APP_ROOT,
-  showOnHomePage: true,
-  category: FeatureCatalogueCategory.DATA,
-}));
+if (npSetup.plugins.feature_catalogue) {
+  npSetup.plugins.feature_catalogue.register({
+    id: PLUGIN.ID,
+    title: PLUGIN.TITLE,
+    description: PLUGIN.DESCRIPTION,
+    icon: PLUGIN.ICON,
+    path: patterns.APP_ROOT,
+    showOnHomePage: true,
+    category: FeatureCatalogueCategory.DATA,
+  });
+}
