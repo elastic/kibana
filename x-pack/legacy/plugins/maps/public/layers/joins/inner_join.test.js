c2648cc4d9ad9ec79350a3bba11e8fae2b932181
Thomas Neirynck <thomas@elastic.co>
2019-11-09 11:30:57 -0500

    fix test

--- x-pack/legacy/plugins/maps/public/layers/joins/inner_join.test.js
+++ x-pack/legacy/plugins/maps/public/layers/joins/inner_join.test.js
@@ -29,7 +29,7 @@ const rightSource = {
 const mockSource = {
   getInspectorAdapters() {
   },
-  createField(name) {
+  createField({ fieldName: name }) {
     return {
       getName() {
         return name;
