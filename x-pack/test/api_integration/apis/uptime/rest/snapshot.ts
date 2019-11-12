438ca0b12bc37dfcbee58248931ac7042444d3d4
Justin Kambic <justin.kambic@elastic.co>
2019-11-12 16:13:09 -0500

    Update API test fixtures to match new snapshot count REST response shape.

--- x-pack/test/api_integration/apis/uptime/rest/snapshot.ts
+++ x-pack/test/api_integration/apis/uptime/rest/snapshot.ts
@@ -14,39 +14,39 @@ export default function({ getService }: FtrProviderContext) {
     let dateRangeStart = '2019-01-28T17:40:08.078Z';
     let dateRangeEnd = '2025-01-28T19:00:16.078Z';
 
-    it('snapshot count another', async () => {
-      const res = await supertest.get(
+    it('will fetch the full set of snapshot counts', async () => {
+      const apiResponse = await supertest.get(
         `/api/uptime/snapshot/count?dateRangeStart=${dateRangeStart}&dateRangeEnd=${dateRangeEnd}`
       );
-      expectFixtureEql(res, 'snapshot');
+      expectFixtureEql(apiResponse.body, 'snapshot');
     });
 
     it('will fetch a monitor snapshot filtered by down status', async () => {
       const filters = `{"bool":{"must":[{"match":{"monitor.status":{"query":"down","operator":"and"}}}]}}`;
       const statusFilter = 'down';
-      const res = await supertest.get(
+      const apiResponse = await supertest.get(
         `/api/uptime/snapshot/count?dateRangeStart=${dateRangeStart}&dateRangeEnd=${dateRangeEnd}&filters=${filters}&statusFilter=${statusFilter}`
       );
-      expectFixtureEql(res, 'snapshot_filtered_by_down');
+      expectFixtureEql(apiResponse.body, 'snapshot_filtered_by_down');
     });
 
     it('will fetch a monitor snapshot filtered by up status', async () => {
       const filters = `{"bool":{"must":[{"match":{"monitor.status":{"query":"up","operator":"and"}}}]}}`;
       const statusFilter = 'up';
-      const res = await supertest.get(
+      const apiResponse = await supertest.get(
         `/api/uptime/snapshot/count?dateRangeStart=${dateRangeStart}&dateRangeEnd=${dateRangeEnd}&filters=${filters}&statusFilter=${statusFilter}`
       );
-      expectFixtureEql(res, 'snapshot_filtered_by_up');
+      expectFixtureEql(apiResponse.body, 'snapshot_filtered_by_up');
     });
 
     it('returns a null snapshot when no data is present', async () => {
       dateRangeStart = '2019-01-25T04:30:54.740Z';
       dateRangeEnd = '2025-01-28T04:50:54.740Z';
       const filters = `{"bool":{"must":[{"match":{"monitor.status":{"query":"down","operator":"and"}}}]}}`;
-      const res = await supertest.get(
+      const apiResponse = await supertest.get(
         `/api/uptime/snapshot/count?dateRangeStart=${dateRangeStart}&dateRangeEnd=${dateRangeEnd}&filters=${filters}`
       );
-      expectFixtureEql(res, 'snapshot_empty');
+      expectFixtureEql(apiResponse.body, 'snapshot_empty');
     });
   });
 }
