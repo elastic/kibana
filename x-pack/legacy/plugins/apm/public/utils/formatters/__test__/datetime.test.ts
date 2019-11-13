52b88390077dd02eeee2fbc80bcaecf8d1056f72
cauemarcondes <caue.marcondes@elastic.co>
2019-11-13 09:38:39 +0100

    Adding timezone to tests

--- x-pack/legacy/plugins/apm/public/utils/formatters/__test__/datetime.test.ts
+++ x-pack/legacy/plugins/apm/public/utils/formatters/__test__/datetime.test.ts
@@ -8,6 +8,10 @@ import { asRelativeDateTimeRange, asAbsoluteDateTime } from '../datetime';
 
 describe('date time formatters', () => {
   describe('asRelativeDateTimeRange', () => {
+    beforeAll(() => {
+      moment.tz.setDefault('Europe/Amsterdam');
+    });
+    afterAll(() => moment.tz.setDefault(''));
     const formatDateToTimezone = (dateTimeString: string) =>
       new Date(dateTimeString).getTime();
 
