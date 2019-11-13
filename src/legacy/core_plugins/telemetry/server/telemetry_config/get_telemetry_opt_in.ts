2c126882e05013b429a603bbb3e8f643706b1403
Ahmad Bamieh <ahmadbamieh@gmail.com>
2019-11-13 07:51:32 +0200

    update test case

--- src/legacy/core_plugins/telemetry/server/telemetry_config/get_telemetry_opt_in.ts
+++ src/legacy/core_plugins/telemetry/server/telemetry_config/get_telemetry_opt_in.ts
@@ -24,7 +24,7 @@ interface GetTelemetryOptInConfig {
   telemetrySavedObject: TelemetrySavedObject;
   currentKibanaVersion: string;
   allowChangingOptInStatus: boolean;
-  configTelemetryOptIn: boolean;
+  configTelemetryOptIn: boolean | null;
 }
 
 type GetTelemetryOptIn = (config: GetTelemetryOptInConfig) => null | boolean;
