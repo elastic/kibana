5f8b5aad400ba218577fcf2a115c08d2ab45e2d9
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2019-11-13 18:48:54 +0100

    Implement pipeline rewriting.

--- x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.test.ts
+++ x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.test.ts
@@ -4,6 +4,29 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-test('the file exists', () => {
-  expect(true).toBe(true);
+import { rewriteIngestPipeline } from './ingest_pipelines';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+test('a pipeline with pipeline references is correctly rewritten', () => {
+  const input = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_input.json')
+  ).toString('utf-8');
+  const output = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_output.json')
+  ).toString('utf-8');
+
+  const substitutions = [
+    {
+      source: 'pipeline-json',
+      target: 'new-pipeline-json',
+      templateFunction: 'IngestPipeline',
+    },
+    {
+      source: 'pipeline-plaintext',
+      target: 'new-pipeline-plaintext',
+      templateFunction: 'IngestPipeline',
+    },
+  ];
+  expect(rewriteIngestPipeline(input, substitutions)).toBe(output);
 });
--- x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.ts
+++ x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.ts
@@ -3,3 +3,21 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+
+export function rewriteIngestPipeline(
+  pipeline: string,
+  substitutions: Array<{
+    source: string;
+    target: string;
+    templateFunction: string;
+  }>
+): string {
+  substitutions.forEach(sub => {
+    const { source, target, templateFunction } = sub;
+    // This reimplements the golang text/template expression {{TemplateFunction 'some-param'}}
+    const match = `{{\\s?${templateFunction}\\s+'${source}'\\s?}}`;
+    const regex = new RegExp(match);
+    pipeline = pipeline.replace(regex, target);
+  });
+  return pipeline;
+}
