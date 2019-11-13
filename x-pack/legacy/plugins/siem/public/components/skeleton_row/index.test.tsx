d98fcd33b593bc9da68a0a2ef4f869fd571f13e5
Patryk Kopyciński <contact@patrykkopycinski.com>
2019-11-13 11:14:30 +0100

    Reenable unit tests

--- x-pack/legacy/plugins/siem/public/components/skeleton_row/index.test.tsx
+++ x-pack/legacy/plugins/siem/public/components/skeleton_row/index.test.tsx
@@ -51,9 +51,8 @@ describe('SkeletonRow', () => {
     expect(siemSkeletonRow).toHaveStyleRule('padding', '10px');
     expect(siemSkeletonRow.props().style!.width).toBe('auto');
     expect(siemSkeletonRowCell).toHaveStyleRule('background-color', 'red');
-    // FIXME: doesn't work with current version of styled-component
-    // expect(siemSkeletonRowCell).toHaveStyleRule('margin-left', '10px', {
-    //   modifier: '& + &',
-    // });
+    expect(siemSkeletonRowCell).toHaveStyleRule('margin-left', '10px', {
+      modifier: '& + &',
+    });
   });
 });
