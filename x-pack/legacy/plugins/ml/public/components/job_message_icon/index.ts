a63573d32fded9f51c711d2423b4e503b27532af
Dima Arnautov <dmitrii.arnautov@elastic.co>
2019-11-14 09:55:07 +0100

    [ML] job messages component

--- x-pack/legacy/plugins/ml/public/components/job_message_icon/index.ts
+++ x-pack/legacy/plugins/ml/public/components/job_message_icon/index.ts
@@ -4,5 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-
 export { JobIcon } from './job_message_icon';
---
+++ x-pack/legacy/plugins/ml/public/jobs/jobs_list/components/job_details/job_messages_pane.tsx
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { FC, useEffect, useState } from 'react';
+
+import { ml } from '../../../../services/ml_api_service';
+import { JobMessages } from '../../../../components/job_messages';
+import { JobMessage } from '../../../../../common/types/audit_message';
+
+interface JobMessagesPaneProps {
+  jobId: string;
+}
+
+export const JobMessagesPane: FC<JobMessagesPaneProps> = ({ jobId }) => {
+  const [messages, setMessages] = useState<JobMessage[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    ml.jobs
+      .jobAuditMessages(jobId)
+      .then(response => {
+        setIsLoading(false);
+        setMessages(response);
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setErrorMessage(error);
+        // eslint-disable-next-line no-console
+        console.log('Job messages could not be loaded', error);
+      });
+  });
+
+  return <JobMessages messages={messages} loading={isLoading} error={errorMessage} />;
+};
