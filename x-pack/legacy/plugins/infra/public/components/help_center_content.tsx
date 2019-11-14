9ed8be7eb3b5ee564a22f69dbcb886f36de256fd
cchaos <caroline.horn@elastic.co>
2019-11-14 16:37:25 -0500

    Updated Logs and Metrics usage

--- x-pack/legacy/plugins/infra/public/components/help_center_content.tsx
+++ x-pack/legacy/plugins/infra/public/components/help_center_content.tsx
@@ -4,29 +4,24 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiLink } from '@elastic/eui';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import chrome from 'ui/chrome';
 
 interface HelpCenterContentProps {
   feedbackLink: string;
-  feedbackLinkText: string;
+  appName: string;
 }
-
-const Content: React.FC<HelpCenterContentProps> = ({ feedbackLink, feedbackLinkText }) => (
-  <EuiLink href={feedbackLink} target="_blank" rel="noopener">
-    {feedbackLinkText}
-  </EuiLink>
-);
 
 export class HelpCenterContent extends React.Component<HelpCenterContentProps> {
   public componentDidMount = () => {
-    chrome.helpExtension.set(domElement => {
-      ReactDOM.render(<Content {...this.props} />, domElement);
-      return () => {
-        ReactDOM.unmountComponentAtNode(domElement);
-      };
+    chrome.helpExtension.set({
+      appName: this.props.appName,
+      links: [
+        {
+          linkType: 'discuss',
+          href: this.props.feedbackLink,
+        },
+      ],
     });
   };
 
