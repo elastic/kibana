c76483c10eb7eca812d08d7ab256e16dd4c0ba12
miukimiu <elizabet.oliveira@elastic.co>
2019-11-14 17:07:50 +0000

    Removing EuiCode from inside EuiPanel

--- x-pack/legacy/plugins/grokdebugger/public/components/event_output/event_output.js
+++ x-pack/legacy/plugins/grokdebugger/public/components/event_output/event_output.js
@@ -5,38 +5,32 @@
  */
 
 import React from 'react';
-import {
-  EuiFormRow,
-  EuiPanel,
-  EuiCodeEditor
-} from '@elastic/eui';
+import { EuiFormRow, EuiCodeEditor } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 
 export function EventOutput({ value }) {
   return (
     <EuiFormRow
-      label={(
+      label={
         <FormattedMessage
           id="xpack.grokDebugger.structuredDataLabel"
           defaultMessage="Structured Data"
         />
-      )}
+      }
       fullWidth
       data-test-subj="aceEventOutput"
     >
-      <EuiPanel paddingSize="s">
-        <EuiCodeEditor
-          mode="json"
-          isReadOnly
-          width="100%"
-          height="340px"
-          value={JSON.stringify(value, null, 2)}
-          setOptions={{
-            highlightActiveLine: false,
-            highlightGutterLine: false,
-          }}
-        />
-      </EuiPanel>
+      <EuiCodeEditor
+        mode="json"
+        isReadOnly
+        width="100%"
+        height="340px"
+        value={JSON.stringify(value, null, 2)}
+        setOptions={{
+          highlightActiveLine: false,
+          highlightGutterLine: false,
+        }}
+      />
     </EuiFormRow>
   );
 }
