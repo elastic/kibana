3a152894a684e9b2e2048a02e0942b8a1bfbbc64
neptunian <sandra.gonzales@elastic.co>
2019-11-13 19:48:27 -0500

    cleanup assets, filter assets for those currently supported

--- x-pack/legacy/plugins/integrations_manager/public/constants.ts
+++ x-pack/legacy/plugins/integrations_manager/public/constants.ts
@@ -4,41 +4,36 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import { IconType } from '@elastic/eui';
-import { AssetType, ServiceName } from '../common/types';
+import { KibanaAssetType, AssetType, ServiceName } from '../common/types';
+
+export const DisplayedAssets: Record<ServiceName, AssetType[]> = {
+  kibana: ['index-pattern', 'visualization', 'search', 'dashboard'],
+  elasticsearch: ['index-template', 'ingest-pipeline', 'ilm-policy'],
+};
 
 export const AssetTitleMap: Record<AssetType, string> = {
-  config: 'Config',
   dashboard: 'Dashboard',
-  'index-pattern': 'Index Pattern',
+  'ilm-policy': 'ILM Policy',
   'ingest-pipeline': 'Ingest Pipeline',
+  'index-pattern': 'Index Pattern',
+  'index-template': 'Index Template',
   search: 'Saved Search',
-  'timelion-sheet': 'Timelion Sheet',
   visualization: 'Visualization',
 };
 
 export const ServiceTitleMap: Record<ServiceName, string> = {
   elasticsearch: 'Elasticsearch',
-  filebeat: 'Filebeat',
   kibana: 'Kibana',
-  metricbeat: 'Metricbeat',
 };
 
-export const AssetIcons: Record<AssetType, IconType> = {
-  config: 'advancedSettingsApp',
-  visualization: 'visualizeApp',
+export const AssetIcons: Record<KibanaAssetType, IconType> = {
   dashboard: 'dashboardApp',
-  search: 'searchProfilerApp',
   'index-pattern': 'indexPatternApp',
-  // spaces: 'spacesApp',
-  'ingest-pipeline': 'pipelineApp',
-  // 'index-template': 'indexManagementApp'
-  // 'ilm-policy': 'reportingApp',
-  'timelion-sheet': 'timelionApp',
+  search: 'searchProfilerApp',
+  visualization: 'visualizeApp',
 };
 
 export const ServiceIcons: Record<ServiceName, IconType> = {
   elasticsearch: 'logoElasticsearch',
-  filebeat: 'logoBeats',
   kibana: 'logoKibana',
-  metricbeat: 'logoBeats',
 };
