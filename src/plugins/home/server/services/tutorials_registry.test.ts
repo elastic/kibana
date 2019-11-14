628e410a5d430fddf03bbdddb31ca13986231c1e
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-11-14 16:06:37 -0700

    Adds test for route

--- src/plugins/home/server/services/tutorials_registry.test.ts
+++ src/plugins/home/server/services/tutorials_registry.test.ts
@@ -20,6 +20,9 @@
 import { TutorialsRegistry } from './tutorials_registry';
 import { coreMock } from '../../../../core/server/mocks';
 import { CoreSetup } from '../../../../core/server';
+import { httpServiceMock, httpServerMock } from '../../../../../src/core/server/mocks';
+import { IRouter } from 'kibana/server';
+
 import {
   TutorialProvider,
   TutorialSchema,
@@ -27,7 +30,7 @@ import {
   ScopedTutorialContextFactory,
 } from './tutorials_registry_types';
 
-const TEST_TUTORIAL: TutorialSchema = {
+const INVALID_TUTORIAL: TutorialSchema = {
   id: 'test',
   category: 'logging' as TutorialsCategory,
   name: '',
@@ -69,16 +72,26 @@ const VALID_TUTORIAL: TutorialSchema = {
   savedObjects: [],
   savedObjectsInstallMsg: 'testMsg',
 };
-const testTutorialProvider = TEST_TUTORIAL;
-const testTutorialProvider2 = VALID_TUTORIAL;
+const invalidTutorialProvider = INVALID_TUTORIAL;
+const validTutorialProvider = VALID_TUTORIAL;
 
 describe('TutorialsRegistry', () => {
   let mockCoreSetup: MockedKeys<CoreSetup>;
   let testProvider: TutorialProvider;
   let testScopedTutorialContextFactory: ScopedTutorialContextFactory;
 
-  beforeEach(() => {
-    mockCoreSetup = coreMock.createSetup();
+  describe('GET /api/kibana/home/NP_tutorials', () => {
+    let routerMock: jest.Mocked<IRouter>;
+
+    beforeEach(() => {
+      mockCoreSetup = coreMock.createSetup();
+      routerMock = httpServiceMock.createRouter();
+    });
+
+    test('has a router that retrieves registered tutorials', () => {
+      const mockResponse = httpServerMock.createResponseFactory();
+      expect(mockResponse.ok.mock.calls).toMatchInlineSnapshot(`Array []`);
+    });
   });
 
   describe('setup', () => {
@@ -87,27 +100,31 @@ describe('TutorialsRegistry', () => {
       expect(setup).toHaveProperty('registerTutorial');
       expect(setup).toHaveProperty('addScopedTutorialContextFactory');
     });
+
     test('registerTutorial throws when registering a tutorial with an invalid schema', () => {
       const setup = new TutorialsRegistry().setup(mockCoreSetup);
-      testProvider = ({}) => testTutorialProvider;
+      testProvider = ({}) => invalidTutorialProvider;
       expect(() => setup.registerTutorial(testProvider)).toThrowErrorMatchingInlineSnapshot(
         `"Unable to register tutorial spec because its invalid. ValidationError: child \\"name\\" fails because [\\"name\\" is not allowed to be empty]"`
       );
     });
+
     test('registerTutorial registers a tutorial with a valid schema', () => {
       const setup = new TutorialsRegistry().setup(mockCoreSetup);
-      testProvider = ({}) => testTutorialProvider2;
+      testProvider = ({}) => validTutorialProvider;
       expect(() => setup.registerTutorial(testProvider)).not.toThrowError();
     });
+
     test('addScopedTutorialContextFactory throws when given a scopedTutorialContextFactory that is not a function', () => {
       const setup = new TutorialsRegistry().setup(mockCoreSetup);
-      const testItem = {};
+      const testItem = {} as TutorialProvider;
       expect(() =>
         setup.addScopedTutorialContextFactory(testItem)
       ).toThrowErrorMatchingInlineSnapshot(
         `"Unable to add scoped(request) context factory because you did not provide a function"`
       );
     });
+
     test('addScopedTutorialContextFactory adds a scopedTutorialContextFactory when given a function', () => {
       const setup = new TutorialsRegistry().setup(mockCoreSetup);
       testScopedTutorialContextFactory = ({}) => 'string';
@@ -116,6 +133,7 @@ describe('TutorialsRegistry', () => {
       ).not.toThrowError();
     });
   });
+
   describe('start', () => {
     test('exposes proper contract', () => {
       const start = new TutorialsRegistry().start();
