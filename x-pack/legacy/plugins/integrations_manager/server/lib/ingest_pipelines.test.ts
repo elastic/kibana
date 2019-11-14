19a574662277197abee87c83b9081c662220b487
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2019-11-14 16:35:53 +0100

    40752 rewrite ingest pipeline (#50627)
    * Add directory structure for server/lib.
    
    * 'tests' seems to be more common than 'test'
    
    * Make CI happy
    
    * Implement pipeline rewriting.
    
    * Add more testcases
    
    * For posterity (comment change)
    
    * Allow beats-style template delimiters
    
    * Be more succinct
    
    * Document better

--- x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.test.ts
+++ x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.test.ts
@@ -4,6 +4,102 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-test('the file exists', () => {
-  expect(true).toBe(true);
+import { rewriteIngestPipeline } from './ingest_pipelines';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+test('a json-format pipeline with pipeline references is correctly rewritten', () => {
+  const inputStandard = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_input_standard.json'),
+    'utf-8'
+  );
+  const inputBeats = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_input_beats.json'),
+    'utf-8'
+  );
+  const output = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_output.json')
+  ).toString('utf-8');
+
+  const substitutions = [
+    {
+      source: 'pipeline-json',
+      target: 'new-pipeline-json',
+      templateFunction: 'IngestPipeline',
+    },
+    {
+      source: 'pipeline-plaintext',
+      target: 'new-pipeline-plaintext',
+      templateFunction: 'IngestPipeline',
+    },
+  ];
+  expect(rewriteIngestPipeline(inputStandard, substitutions)).toBe(output);
+  expect(rewriteIngestPipeline(inputBeats, substitutions)).toBe(output);
+});
+
+test('a yml-format pipeline with pipeline references is correctly rewritten', () => {
+  const inputStandard = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_input_standard.yml')
+  ).toString('utf-8');
+  const inputBeats = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_input_beats.yml')
+  ).toString('utf-8');
+  const output = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/real_output.yml')
+  ).toString('utf-8');
+
+  const substitutions = [
+    {
+      source: 'pipeline-json',
+      target: 'new-pipeline-json',
+      templateFunction: 'IngestPipeline',
+    },
+    {
+      source: 'pipeline-plaintext',
+      target: 'new-pipeline-plaintext',
+      templateFunction: 'IngestPipeline',
+    },
+  ];
+  expect(rewriteIngestPipeline(inputStandard, substitutions)).toBe(output);
+  expect(rewriteIngestPipeline(inputBeats, substitutions)).toBe(output);
+});
+
+test('a json-format pipeline with no pipeline references stays unchanged', () => {
+  const input = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/no_replacement.json')
+  ).toString('utf-8');
+
+  const substitutions = [
+    {
+      source: 'pipeline-json',
+      target: 'new-pipeline-json',
+      templateFunction: 'IngestPipeline',
+    },
+    {
+      source: 'pipeline-plaintext',
+      target: 'new-pipeline-plaintext',
+      templateFunction: 'IngestPipeline',
+    },
+  ];
+  expect(rewriteIngestPipeline(input, substitutions)).toBe(input);
+});
+
+test('a yml-format pipeline with no pipeline references stays unchanged', () => {
+  const input = readFileSync(
+    path.join(__dirname, '/tests/ingest_pipelines/no_replacement.yml')
+  ).toString('utf-8');
+
+  const substitutions = [
+    {
+      source: 'pipeline-json',
+      target: 'new-pipeline-json',
+      templateFunction: 'IngestPipeline',
+    },
+    {
+      source: 'pipeline-plaintext',
+      target: 'new-pipeline-plaintext',
+      templateFunction: 'IngestPipeline',
+    },
+  ];
+  expect(rewriteIngestPipeline(input, substitutions)).toBe(input);
 });
--- x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.ts
+++ x-pack/legacy/plugins/integrations_manager/server/lib/ingest_pipelines.ts
@@ -3,3 +3,28 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+
+export function rewriteIngestPipeline(
+  pipeline: string,
+  substitutions: Array<{
+    source: string;
+    target: string;
+    templateFunction: string;
+  }>
+): string {
+  substitutions.forEach(sub => {
+    const { source, target, templateFunction } = sub;
+    // This fakes the use of the golang text/template expression {{SomeTemplateFunction 'some-param'}}
+    // cf. https://github.com/elastic/beats/blob/master/filebeat/fileset/fileset.go#L294
+
+    // "Standard style" uses '{{' and '}}' as delimiters
+    const matchStandardStyle = `{{\\s?${templateFunction}\\s+['"]${source}['"]\\s?}}`;
+    // "Beats style" uses '{<' and '>}' as delimiters because this is current practice in the beats project
+    const matchBeatsStyle = `{<\\s?${templateFunction}\\s+['"]${source}['"]\\s?>}`;
+
+    const regexStandardStyle = new RegExp(matchStandardStyle);
+    const regexBeatsStyle = new RegExp(matchBeatsStyle);
+    pipeline = pipeline.replace(regexStandardStyle, target).replace(regexBeatsStyle, target);
+  });
+  return pipeline;
+}
