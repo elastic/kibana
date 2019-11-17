d8ebf7396f9829f80aadebb79fa4a7cb08307a7c
James Gowdy <jgowdy@elastic.co>
2019-11-17 14:31:58 +0000

    [ML] Adding ML node warning to overview and analytics pages (#50766) (#50867)

--- x-pack/legacy/plugins/ml/public/data_frame_analytics/pages/analytics_management/route.ts
+++ x-pack/legacy/plugins/ml/public/data_frame_analytics/pages/analytics_management/route.ts
@@ -9,6 +9,8 @@ import uiRoutes from 'ui/routes';
 // @ts-ignore
 import { checkFullLicense } from '../../../license/check_license';
 import { checkGetJobsPrivilege } from '../../../privilege/check_privilege';
+import { loadMlServerInfo } from '../../../services/ml_server_info';
+import { getMlNodeCount } from '../../../ml_nodes_check/check_ml_nodes';
 import {
   loadCurrentIndexPattern,
   loadCurrentSavedSearch,
@@ -26,5 +28,7 @@ uiRoutes.when('/data_frame_analytics/?', {
     privileges: checkGetJobsPrivilege,
     indexPattern: loadCurrentIndexPattern,
     savedSearch: loadCurrentSavedSearch,
+    mlNodeCount: getMlNodeCount,
+    loadMlServerInfo,
   },
 });
