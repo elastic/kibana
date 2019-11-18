66038f5cfd6b3790c980e02a3341e14ecb9b26e4
Chris Roberson <chrisronline@gmail.com>
2019-11-18 10:24:53 -0500

    [Monitoring] Improve permissions required around setup mode (#50421)
    * Add error messages when setup mode is not enabled, disable it for users without the necessary permissions, and change one query to relax the privilege requirements
    
    * Fix default value
    
    * PR feedback
    
    * Forgot to update this part
    
    * Fix tests

--- x-pack/legacy/plugins/monitoring/public/lib/setup_mode.js
+++ x-pack/legacy/plugins/monitoring/public/lib/setup_mode.js
@@ -7,6 +7,7 @@
 import { ajaxErrorHandlersProvider } from './ajax_error_handler';
 import { get, contains } from 'lodash';
 import chrome from 'ui/chrome';
+import { toastNotifications } from 'ui/notify';
 import { i18n } from '@kbn/i18n';
 
 function isOnPage(hash) {
@@ -81,7 +82,26 @@ export const updateSetupModeData = async (uuid, fetchWithoutClusterUuid = false)
   const oldData = setupModeState.data;
   const data = await fetchCollectionData(uuid, fetchWithoutClusterUuid);
   setupModeState.data = data;
-  if (chrome.getInjected('isOnCloud')) {
+
+  const isCloud = chrome.getInjected('isOnCloud');
+  const hasPermissions = get(data, '_meta.hasPermissions', false);
+  if (isCloud || !hasPermissions) {
+    const text = !hasPermissions
+      ? i18n.translate('xpack.monitoring.setupMode.notAvailablePermissions', {
+        defaultMessage: 'You do not have the necessary permissions to do this.'
+      })
+      : i18n.translate('xpack.monitoring.setupMode.notAvailableCloud', {
+        defaultMessage: 'This feature is not available on cloud.'
+      });
+
+    angularState.scope.$evalAsync(() => {
+      toastNotifications.addDanger({
+        title: i18n.translate('xpack.monitoring.setupMode.notAvailableTitle', {
+          defaultMessage: 'Setup mode is not available'
+        }),
+        text,
+      });
+    });
     return toggleSetupMode(false); // eslint-disable-line no-use-before-define
   }
   notifySetupModeDataChange(oldData);
