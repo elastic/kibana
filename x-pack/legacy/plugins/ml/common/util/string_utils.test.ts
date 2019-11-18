5a27d773e18b2aa09d34291f03fbb932f795638a
James Gowdy <jgowdy@elastic.co>
2019-11-18 10:27:36 +0000

    converting tests to jest

--- x-pack/legacy/plugins/ml/common/util/string_utils.test.ts
+++ x-pack/legacy/plugins/ml/common/util/string_utils.test.ts
@@ -4,25 +4,24 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import expect from '@kbn/expect';
-import { renderTemplate } from '../string_utils';
+import { renderTemplate } from './string_utils';
 
 describe('ML - string utils', () => {
   describe('renderTemplate', () => {
-    it('returns plain string', () => {
+    test('returns plain string', () => {
       const templateString = 'plain string';
       const result = renderTemplate(templateString);
-      expect(result).to.be(result);
+      expect(result).toBe(result);
     });
-    it('returns rendered template with one replacement', () => {
+    test('returns rendered template with one replacement', () => {
       const templateString = 'string with {{one}} replacement';
       const result = renderTemplate(templateString, { one: '1' });
-      expect(result).to.be('string with 1 replacement');
+      expect(result).toBe('string with 1 replacement');
     });
-    it('returns rendered template with two replacements', () => {
+    test('returns rendered template with two replacements', () => {
       const templateString = 'string with {{one}} replacement, and a {{two}} one.';
       const result = renderTemplate(templateString, { one: '1', two: '2nd' });
-      expect(result).to.be('string with 1 replacement, and a 2nd one.');
+      expect(result).toBe('string with 1 replacement, and a 2nd one.');
     });
   });
 });
