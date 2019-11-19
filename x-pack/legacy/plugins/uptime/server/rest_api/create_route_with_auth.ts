03147755761f8c3c56514a13c1fc83a1bc867ee3
Justin Kambic <justin.kambic@elastic.co>
2019-11-19 17:59:33 -0500

    Move a REST endpoint and the GQL endpoint to NP routing.

--- x-pack/legacy/plugins/uptime/server/rest_api/create_route_with_auth.ts
+++ x-pack/legacy/plugins/uptime/server/rest_api/create_route_with_auth.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import Boom from 'boom';
 import { UMServerLibs } from '../lib/lib';
 import { UMRestApiRouteCreator, UMServerRoute } from './types';
 
@@ -13,17 +12,18 @@ export const createRouteWithAuth = (
   routeCreator: UMRestApiRouteCreator
 ): UMServerRoute => {
   const restRoute = routeCreator(libs);
-  const { handler, method, path, options } = restRoute;
-  const authHandler = async (request: any, h: any) => {
-    if (libs.auth.requestIsValid(request)) {
-      return await handler(request, h);
-    }
-    return Boom.badRequest();
-  };
+  const { handler, method, path, options, ...rest } = restRoute;
+  // const authHandler = async (request: any, h: any) => {
+  // if (libs.auth.requestIsValid(request)) {
+  //  return await handler(request, h);
+  // }
+  // return Boom.badRequest();
+  // };
   return {
     method,
     path,
     options,
-    handler: authHandler,
+    handler, // : authHandler,
+    ...rest,
   };
 };
