13634ef1bd8a869c4f1a84733ca14c895559c293
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-11-19 07:07:02 +0100

    Revert ProcessorEvent back to type

--- x-pack/legacy/plugins/apm/common/processor_event.ts
+++ x-pack/legacy/plugins/apm/common/processor_event.ts
@@ -4,8 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export enum ProcessorEvent {
-  transaction = 'transaction',
-  error = 'error',
-  metric = 'metric'
-}
+export type ProcessorEvent = 'transaction' | 'error' | 'metric';
