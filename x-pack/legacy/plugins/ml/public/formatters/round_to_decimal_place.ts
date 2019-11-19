a93b98e97db092ab426a07e840034f001cebb654
James Gowdy <jgowdy@elastic.co>
2019-11-19 09:10:03 +0000

    converting round_to_decimal_place

--- x-pack/legacy/plugins/ml/public/formatters/round_to_decimal_place.ts
+++ x-pack/legacy/plugins/ml/public/formatters/round_to_decimal_place.ts
@@ -4,12 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-
-export function roundToDecimalPlace(num, dp = 2) {
+export function roundToDecimalPlace(num: number, dp: number = 2) {
   if (Math.abs(num) < Math.pow(10, -dp)) {
-    return Number.parseFloat(num).toExponential(2);
-  } else {
-    const m = Math.pow(10, dp);
-    return Math.round(num * m) / m;
+    return Number.parseFloat(String(num)).toExponential(2);
   }
+  const m = Math.pow(10, dp);
+  return Math.round(num * m) / m;
 }
