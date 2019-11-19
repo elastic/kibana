afcfe025bd523bc54799d114ef4ac67e8f04ec7a
Aaron Caldwell <aaron.caldwell@elastic.co>
2019-11-18 20:35:59 -0700

    Move startup logic over to server plugin file and call from index.js

--- x-pack/legacy/plugins/file_upload/index.js
+++ x-pack/legacy/plugins/file_upload/index.js
@@ -3,10 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { mirrorPluginStatus } from '../../server/lib/mirror_plugin_status';
-import { getImportRouteHandler } from './server/routes/file_upload';
-import { getTelemetry, initTelemetry } from './server/telemetry/telemetry';
-import { MAX_BYTES } from './common/constants/file_import';
+import { FileUploadPlugin } from './server/plugin';
 import { mappings } from './mappings';
 
 export const fileUpload = kibana => {
@@ -24,31 +21,27 @@ export const fileUpload = kibana => {
     },
 
     init(server) {
-      const elasticsearchPlugin = server.plugins.elasticsearch;
-      const getSavedObjectsRepository = server.savedObjects.getSavedObjectsRepository;
-      const xpackMainPlugin = server.plugins.xpack_main;
-      const makeUsageCollector = server.usage.collectorSet.makeUsageCollector;
+      const coreSetup = server.newPlatform.setup.core;
+      const pluginsSetup = {};
 
-      mirrorPluginStatus(xpackMainPlugin, this);
-
-      // Set up route
-      server.route({
-        method: 'POST',
-        path: '/api/fileupload/import',
-        handler: getImportRouteHandler(elasticsearchPlugin, getSavedObjectsRepository),
-        config: {
-          payload: { maxBytes: MAX_BYTES },
+      // legacy dependencies
+      const __LEGACY = {
+        route: server.route.bind(server),
+        plugins: {
+          elasticsearch: server.plugins.elasticsearch,
+        },
+        savedObjects: {
+          getSavedObjectsRepository:
+            server.savedObjects.getSavedObjectsRepository
+        },
+        usage: {
+          collectorSet: {
+            makeUsageCollector: server.usage.collectorSet.makeUsageCollector
+          }
         }
-      });
+      };
 
-      // Make usage collector
-      const TELEMETRY_TYPE = 'fileUploadTelemetry';
-      makeUsageCollector({
-        type: TELEMETRY_TYPE,
-        isReady: () => true,
-        fetch: async () =>
-          (await getTelemetry(elasticsearchPlugin, getSavedObjectsRepository)) || initTelemetry()
-      });
+      new FileUploadPlugin().setup(coreSetup, pluginsSetup, __LEGACY);
     }
   });
 };
