82bfd5244ec83a45ccf1a20d27fee7fe99a53b5b
patrykkopycinski <contact@patrykkopycinski.com>
2019-11-20 19:29:09 +0100

    Update setup_test.js

--- x-pack/dev-tools/jest/setup/setup_test.js
+++ x-pack/dev-tools/jest/setup/setup_test.js
@@ -4,4 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+/*
+  Global import, so we don't need to remember to import the lib in each file 
+  https://www.npmjs.com/package/jest-styled-components#global-installation
+*/
+
 import 'jest-styled-components';
