d03553971fb4342c45a8dc25473afbeca93af8f5
Jean-Louis Leysens <jloleysens@gmail.com>
2019-11-20 13:17:54 +0100

    Upgrade Assistant to New Platform Ready (#50163)
    * - Public NP Shim done
    - Reviving app + tests wip
    
    * Update components to consume via context and props instead of imports (tests working again)
    
    * Moved server files to np_ready dir, updated paths and introduced ServerShim and RequestShim
    
    * Removed unused comments
    Fixed use of legacy server components
    
    * Fix typings and add app context where needed
    
    * getSavedObjectsClient may be null, specifically during tests
    
    * Move styles for _index.scss file -> more specific file
    
    * Fix typings issue [skip ci]
    
    * Migrate to NP router

--- x-pack/legacy/plugins/upgrade_assistant/server/np_ready/routes/__mocks__/request.mock.ts
+++ x-pack/legacy/plugins/upgrade_assistant/server/np_ready/routes/__mocks__/request.mock.ts
@@ -3,5 +3,10 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
-export { plugin } from './np_ready';
+export const createRequestMock = (opts?: {
+  headers?: any;
+  params?: Record<string, any>;
+  payload?: Record<string, any>;
+}) => {
+  return Object.assign({ headers: {} }, opts || {});
+};
