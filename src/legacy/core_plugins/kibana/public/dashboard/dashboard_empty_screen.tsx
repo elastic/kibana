34fa658441fc9863c78aa301d5b7b29429d48bb3
Maja Grubic <maja.grubic@elastic.co>
2019-11-20 16:04:52 +0000

    Adding string constants

--- src/legacy/core_plugins/kibana/public/dashboard/dashboard_empty_screen.tsx
+++ src/legacy/core_plugins/kibana/public/dashboard/dashboard_empty_screen.tsx
@@ -17,9 +17,9 @@
  * under the License.
  */
 import React from 'react';
-import { i18n } from '@kbn/i18n';
 import { I18nProvider, FormattedMessage } from '@kbn/i18n/react';
-import { EuiIcon, EuiI18n, EuiLink } from '@elastic/eui';
+import { EuiIcon, EuiLink } from '@elastic/eui';
+import * as constants from './dashboard_empty_screen_constants';
 
 export interface Props {
   showLinkToVisualize: boolean;
@@ -27,95 +27,63 @@ export interface Props {
 }
 
 export function DashboardEmptyScreen({ showLinkToVisualize, onLinkClick }: Props) {
-  const fillDashboardTitle = i18n.translate('kbn.dashboard.fillDashboardTitle', {
-    defaultMessage: 'This dashboard is empty. Let\u2019s fill it up!',
-  });
-  const visualizeAppLinkTest = i18n.translate('kbn.dashboard.visitVisualizeAppLinkText', {
-    defaultMessage: 'visit the Visualize app',
-  });
   const linkToVisualizeParagraph = (
     <p className="linkToVisualizeParagraph">
       <FormattedMessage
-        id="kbn.dashboard.addVisualizationDescription3'"
+        id="kbn.dashboard.addVisualizationDescription3"
         defaultMessage="If you haven't set up any visualizations yet, {visualizeAppLink} to create your first visualization"
         values={{
           visualizeAppLink: (
             <a className="euiLink" href="#/visualize">
-              {visualizeAppLinkTest}
+              {constants.visualizeAppLinkTest}
             </a>
           ),
         }}
       />
     </p>
   );
+  const paragraph = (
+    description1: string,
+    description2: string,
+    linkText: string,
+    ariaLabel: string,
+    dataTestSubj?: string
+  ) => {
+    return (
+      <p>
+        <span>
+          {description1}
+          <EuiLink onClick={onLinkClick} aria-label={ariaLabel} data-test-subj={dataTestSubj || ''}>
+            {linkText}
+          </EuiLink>
+          {description2}
+        </span>
+      </p>
+    );
+  };
   const addVisualizationParagraph = (
     <React.Fragment>
-      <p>
-        <EuiI18n
-          tokens={[
-            'kbn.dashboard.addVisualizationDescription1',
-            'kbn.dashboard.addVisualizationDescription2',
-            'kbn.dashboard.addVisualizationLinkText',
-            'kbn.dashboard.addVisualizationLinkAriaLabel',
-          ]}
-          defaults={[
-            'Click the ',
-            ' button in the menu bar above to add a visualization to the dashboard.',
-            'Add',
-            'Add visualization',
-          ]}
-        >
-          {([description1, description2, label, ariaLabel]: string[]) => (
-            <span>
-              {description1}
-              <EuiLink
-                onClick={onLinkClick}
-                aria-label={ariaLabel}
-                data-test-subj={'emptyDashboardAddPanelButton'}
-              >
-                {label}
-              </EuiLink>
-              {description2}
-            </span>
-          )}
-        </EuiI18n>
-      </p>
+      {paragraph(
+        constants.addVisualizationDescription1,
+        constants.addVisualizationDescription2,
+        constants.addVisualizationLinkText,
+        constants.addVisualizationLinkAriaLabel,
+        'emptyDashboardAddPanelButton'
+      )}
       {linkToVisualizeParagraph}
     </React.Fragment>
   );
-  const enterEditModeParagraph = (
-    <p>
-      <EuiI18n
-        tokens={[
-          'kbn.dashboard.howToStartWorkingOnNewDashboardDescription1',
-          'kbn.dashboard.howToStartWorkingOnNewDashboardDescription2',
-          'kbn.dashboard.howToStartWorkingOnNewDashboardEditLinkText',
-          'kbn.dashboard.howToStartWorkingOnNewDashboardEditLinkAriaLabel',
-        ]}
-        defaults={[
-          'Click the ',
-          ' button in the menu bar above to start working on your new dashboard.',
-          'Edit',
-          'Edit dashboard',
-        ]}
-      >
-        {([description1, description2, label, ariaLabel]: string[]) => (
-          <span>
-            {description1}
-            <EuiLink onClick={onLinkClick} aria-label={ariaLabel}>
-              {label}
-            </EuiLink>
-            {description2}
-          </span>
-        )}
-      </EuiI18n>
-    </p>
+  const enterEditModeParagraph = paragraph(
+    constants.howToStartWorkingOnNewDashboardDescription1,
+    constants.howToStartWorkingOnNewDashboardDescription2,
+    constants.howToStartWorkingOnNewDashboardEditLinkText,
+    constants.howToStartWorkingOnNewDashboardEditLinkAriaLabel
   );
   return (
     <I18nProvider>
       <React.Fragment>
         <EuiIcon type="dashboardApp" size="xxl" color="subdued" />
-        <h2>{fillDashboardTitle}</h2>
+        <h2>{constants.fillDashboardTitle}</h2>
         {showLinkToVisualize ? addVisualizationParagraph : enterEditModeParagraph}
       </React.Fragment>
     </I18nProvider>
