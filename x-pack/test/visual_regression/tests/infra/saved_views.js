1538a59831353218b096c2039d5b19a94326999b
liza-mae <liza.dayoub@elastic.co>
2019-11-20 12:17:52 -0700

    Re-enable infra visual tests

--- x-pack/test/visual_regression/tests/infra/saved_views.js
+++ x-pack/test/visual_regression/tests/infra/saved_views.js
@@ -7,17 +7,24 @@ import { DATES } from '../../../functional/apps/infra/constants';
 const DATE_WITH_DATA = DATES.metricsAndLogs.hosts.withData;
 
 export default function ({ getPageObjects, getService }) {
-  const PageObjects = getPageObjects(['common', 'infraHome', 'infraMetricExplorer']);
+  const PageObjects = getPageObjects(['common', 'header', 'infraHome', 'infraMetricExplorer', 'timePicker']);
   const visualTesting = getService('visualTesting');
   const esArchiver = getService('esArchiver');
+  const find = getService('find');
 
   describe('saved views', () => {
-    before(() => esArchiver.load('infra/metrics_and_logs'));
+
+    before(async function () {
+      esArchiver.load('infra/metrics_and_logs');
+      await PageObjects.common.navigateToApp('infraOps');
+      await PageObjects.infraHome.goToTime(DATE_WITH_DATA);
+      await PageObjects.header.awaitGlobalLoadingIndicatorHidden();
+    });
+
     after(() => esArchiver.unload('infra/metrics_and_logs'));
+
     describe('Inverntory Test save functionality', () => {
       it('should have save and load controls', async () => {
-        await PageObjects.common.navigateToApp('infraOps');
-        await PageObjects.infraHome.goToTime(DATE_WITH_DATA);
         await PageObjects.infraHome.getSaveViewButton();
         await PageObjects.infraHome.getLoadViewsButton();
         await visualTesting.snapshot();
@@ -25,6 +32,7 @@ export default function ({ getPageObjects, getService }) {
 
       it('should open flyout list', async () => {
         await PageObjects.infraHome.openSaveViewsFlyout();
+        await find.waitForDeletedByCssSelector('.euiBasicTable-loading', 5000);
         await visualTesting.snapshot();
         await PageObjects.infraHome.closeSavedViewFlyout();
       });
@@ -36,19 +44,30 @@ export default function ({ getPageObjects, getService }) {
 
       it('should be able to enter a view name', async () => {
         await PageObjects.infraHome.openEnterViewNameAndSave();
+        await PageObjects.header.awaitGlobalLoadingIndicatorHidden();
         await visualTesting.snapshot();
       });
 
       it('should see a saved view in list', async () => {
         await PageObjects.infraHome.openSaveViewsFlyout();
+        await find.waitForDeletedByCssSelector('.euiBasicTable-loading', 5000);
         await visualTesting.snapshot();
       });
     });
 
     describe('Metric Explorer Test save functionality', () => {
-      it('should have save and load controls', async () => {
+
+      const fromTime = '2018-10-16 09:00:00.000';
+      const toTime = '2018-10-18 19:00:00.000';
+
+      before(async function () {
         await PageObjects.common.navigateToApp('infraOps');
         await PageObjects.infraHome.goToMetricExplorer();
+        await PageObjects.timePicker.setAbsoluteRange(fromTime, toTime);
+        await PageObjects.header.awaitGlobalLoadingIndicatorHidden();
+      });
+
+      it('should have save and load controls', async () => {
         await PageObjects.infraMetricExplorer.getSaveViewButton();
         await PageObjects.infraMetricExplorer.getLoadViewsButton();
         await visualTesting.snapshot();
@@ -56,6 +75,7 @@ export default function ({ getPageObjects, getService }) {
 
       it('should open flyout list', async () => {
         await PageObjects.infraMetricExplorer.openSaveViewsFlyout();
+        await find.waitForDeletedByCssSelector('.euiBasicTable-loading', 5000);
         await visualTesting.snapshot();
         await PageObjects.infraMetricExplorer.closeSavedViewFlyout();
       });
@@ -72,6 +92,7 @@ export default function ({ getPageObjects, getService }) {
 
       it('should see a saved view in list', async () => {
         await PageObjects.infraMetricExplorer.openSaveViewsFlyout();
+        await find.waitForDeletedByCssSelector('.euiBasicTable-loading', 5000);
         await visualTesting.snapshot();
       });
     });
