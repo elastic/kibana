be624acbdc8b1e1a11928017d8da9e4e6aa47aac
Brian Seeders <brian.seeders@elastic.co>
2019-11-22 15:51:50 -0500

    A bunch of cleanup and some comments

--- vars/githubPr.groovy
+++ vars/githubPr.groovy
@@ -1,39 +1,59 @@
+/**
+  Wraps the main/important part of a job, executes it, and then publishes a comment to GitHub with the status.
+
+  It will check for the existence of GHPRB env variables before doing any actual PR work,
+    so it can be used to wrap code that is executed in both PR and non-PR contexts.
+
+  Inside the comment, it will hide a JSON blob containing build data (status, etc).
+
+  Then, the next time it posts a comment, it will:
+    1. Read the previous comment and parse the json
+    2. Create a new comment, add a summary of up to 5 previous builds to it, and append this build's data to the hidden JSON
+    3. Delete the old comment
+
+  So, there is only ever one build status comment on a PR at any given time, the most recent one.
+*/
+def withDefaultPrComments(closure) {
+  catchError {
+    catchError {
+      closure()
+    }
+
+    if (!isPr()) {
+      return
+    }
+
+    def status = buildUtils.getBuildStatus()
+    if (status == "ABORTED") {
+      return;
+    }
+
+    def lastComment = getLatestBuildComment()
+    def info = getLatestBuildInfo(lastComment) ?: [:]
+    info.builds = (info.builds ?: []).takeRight(5) // Rotate out old builds
+
+    def message = getNextCommentMessage(info)
+    postComment(message)
+
+    if (lastComment) {
+      deleteComment(lastComment.id)
+    }
+  }
+}
+
+// Checks whether or not this currently executing build was triggered via a PR in the elastic/kibana repo
 def isPr() {
   return !!(env.ghprbPullId && env.ghprbPullLink && env.ghprbPullLink =~ /\/elastic\/kibana\//)
 }
 
-def getCommitHash() {
-  return env.ghprbActualCommit
-}
-
-def postComment(message) {
-  if (!isPr()) {
-    error "Trying to post a GitHub PR comment on a non-PR or non-elastic PR build"
-  }
-
-  withCredentials([
-    string(credentialsId: '2a9602aa-ab9f-4e52-baf3-b71ca88469c7', variable: 'GITHUB_TOKEN'),
-  ]) {
-    return githubApi.post("repos/elastic/kibana/issues/${env.ghprbPullId}/comments", [ body: message ])
-  }
-}
-
-def getComments() {
-  withCredentials([
-    string(credentialsId: '2a9602aa-ab9f-4e52-baf3-b71ca88469c7', variable: 'GITHUB_TOKEN'),
-  ]) {
-    return githubApi.get("repos/elastic/kibana/issues/${env.ghprbPullId}/comments")
-  }
-}
-
-def getLatestPipelineComment() {
+def getLatestBuildComment() {
   return getComments()
     .reverse()
     .find { it.user.login == 'elasticmachine' && it.body =~ /<!--PIPELINE/ }
 }
 
-def getPipelineInfoFromComment(comment) {
-  def matches = comment =~ /(?ms)<!--PIPELINE(.*?)PIPELINE-->/
+def getBuildInfoFromComment(commentText) {
+  def matches = commentText =~ /(?ms)<!--PIPELINE(.*?)PIPELINE-->/
   if (!matches || !matches[0]) {
     return null
   }
@@ -41,18 +61,12 @@ def getPipelineInfoFromComment(comment) {
   return toJSON(matches[0][1].trim())
 }
 
-def getLatestPipelineInfo() {
-  return getLatestPipelineInfo(getLatestPipelineComment())
+def getLatestBuildInfo() {
+  return getLatestBuildInfo(getLatestBuildComment())
 }
 
-def getLatestPipelineInfo(comment) {
-  def info
-
-  if (comment) {
-    info = getPipelineInfoFromComment(comment)
-  }
-
-  return info ?: [ builds: [] ]
+def getLatestBuildInfo(comment) {
+  return comment ? getBuildInfoFromComment(comment) : null
 }
 
 def createBuildInfo() {
@@ -62,16 +76,6 @@ def createBuildInfo() {
     number: env.BUILD_NUMBER,
     commit: getCommitHash()
   ]
-}
-
-def deleteComment(commentId) {
-  withCredentials([
-    string(credentialsId: '2a9602aa-ab9f-4e52-baf3-b71ca88469c7', variable: 'GITHUB_TOKEN'),
-  ]) {
-    // TODO - verification that this is an elasticmachine comment or similar?
-    def path = "repos/elastic/kibana/issues/comments/${commentId}"
-    return githubApi([ path: path ], [ method: "DELETE" ])
-  }
 }
 
 def getHistoryText(builds) {
@@ -93,64 +97,77 @@ def getHistoryText(builds) {
   return "### History\n${list}"
 }
 
-def withDefaultPrComments(closure) {
-  catchError {
-    catchError {
-      closure()
-    }
+def getNextCommentMessage(previousCommentInfo = [:]) {
+  info = previousCommentInfo ?: [:]
+  info.builds = previousCommentInfo.builds ?: []
 
-    if (!isPr()) {
-      return
-    }
+  def messages = []
 
-    def status = buildUtils.getBuildStatus()
-    if (status == "ABORTED") {
-      return;
-    }
-
-    def lastComment = getLatestPipelineComment()
-    def info = getLatestPipelineInfo(lastComment)
-    info.builds = (info.builds ?: []).takeRight(5) // Rotate out old builds
-
-    def messages = []
-
-    if (status == 'SUCCESS') {
-      messages << """
-        ## :green_heart: Build Succeeded
-        * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
-        * Commit: ${getCommitHash()}
-      """
-    } else {
-      messages << """
-        ## :broken_heart: Build Failed
-        * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
-        * Commit: ${getCommitHash()}
-      """
-    }
-
-    if (info.builds && info.builds.size() > 0) {
-      messages << getHistoryText(info.builds)
-    }
-
-    messages << "To update your PR or re-run it, just comment with:\n`@elasticmachine merge upstream`"
-
-    info.builds << createBuildInfo()
-
+  if (buildUtils.getBuildStatus() == 'SUCCESS') {
     messages << """
-      <!--PIPELINE
-      ${toJSON(info).toString()}
-      PIPELINE-->
+      ## :green_heart: Build Succeeded
+      * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
+      * Commit: ${getCommitHash()}
     """
-
-    def message = messages
-      .findAll { !!it } // No blank strings
-      .collect { it.stripIndent().trim() }
-      .join("\n\n")
-
-    postComment(message)
-
-    if (lastComment) {
-      deleteComment(lastComment.id)
-    }
+  } else {
+    messages << """
+      ## :broken_heart: Build Failed
+      * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
+      * Commit: ${getCommitHash()}
+    """
   }
+
+  if (info.builds && info.builds.size() > 0) {
+    messages << getHistoryText(info.builds)
+  }
+
+  messages << "To update your PR or re-run it, just comment with:\n`@elasticmachine merge upstream`"
+
+  info.builds << createBuildInfo()
+
+  messages << """
+    <!--PIPELINE
+    ${toJSON(info).toString()}
+    PIPELINE-->
+  """
+
+  return messages
+    .findAll { !!it } // No blank strings
+    .collect { it.stripIndent().trim() }
+    .join("\n\n")
+}
+
+def withGithubCredentials(closure) {
+  withCredentials([
+    string(credentialsId: '2a9602aa-ab9f-4e52-baf3-b71ca88469c7', variable: 'GITHUB_TOKEN'),
+  ]) {
+    closure()
+  }
+}
+
+def postComment(message) {
+  if (!isPr()) {
+    error "Trying to post a GitHub PR comment on a non-PR or non-elastic PR build"
+  }
+
+  withGithubCredentials {
+    return githubApi.post("repos/elastic/kibana/issues/${env.ghprbPullId}/comments", [ body: message ])
+  }
+}
+
+def getComments() {
+  withGithubCredentials {
+    return githubApi.get("repos/elastic/kibana/issues/${env.ghprbPullId}/comments")
+  }
+}
+
+def deleteComment(commentId) {
+  withGithubCredentials {
+    def path = "repos/elastic/kibana/issues/comments/${commentId}"
+    return githubApi([ path: path ], [ method: "DELETE" ])
+  }
+}
+
+def getCommitHash() {
+  return env.ghprbActualCommit
 }
