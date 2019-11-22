b63f66d4e9e9f1d2f0e04a01006a9f548291d4cb
Patryk Kopyciński <contact@patrykkopycinski.com>
2019-11-22 17:12:21 +0100

    cleanup

--- x-pack/legacy/plugins/siem/server/lib/compose/kibana.ts
+++ x-pack/legacy/plugins/siem/server/lib/compose/kibana.ts
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Server } from 'hapi';
-
 import { EnvironmentMode } from 'src/core/server';
 import { ServerFacade } from '../../types';
 import { Anomalies } from '../anomalies';
