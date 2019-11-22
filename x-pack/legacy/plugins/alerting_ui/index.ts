a0560ddda7f66ed66ed20a74efb51b27a0f2b0a2
Mike Cote <mikecote@users.noreply.github.com>
2019-11-22 13:18:23 -0500

    Remove the test alert type that was in the init function

--- x-pack/legacy/plugins/alerting_ui/index.ts
+++ x-pack/legacy/plugins/alerting_ui/index.ts
@@ -31,44 +31,5 @@ export function alertingUI(kibana: any) {
       hacks: ['plugins/alerting_ui/hacks/register'],
       managementSections: ['plugins/alerting_ui'],
     },
-    // TODO: Remove init
-    init(server: any) {
-      server.plugins.alerting.setup.registerType({
-        id: 'test.always-firing',
-        name: 'Test: Always Firing',
-        actionGroups: ['default', 'other'],
-        async executor({ services, params, state }: any) {
-          let group = 'default';
-
-          if (params.groupsToScheduleActionsInSeries) {
-            const index = state.groupInSeriesIndex || 0;
-            group = params.groupsToScheduleActionsInSeries[index];
-          }
-
-          if (group) {
-            services
-              .alertInstanceFactory('1')
-              .replaceState({ instanceStateValue: true })
-              .scheduleActions(group, {
-                instanceContextValue: true,
-              });
-          }
-          await services.callCluster('index', {
-            index: params.index,
-            refresh: 'wait_for',
-            body: {
-              state,
-              params,
-              reference: params.reference,
-              source: 'alert:test.always-firing',
-            },
-          });
-          return {
-            globalStateValue: true,
-            groupInSeriesIndex: (state.groupInSeriesIndex || 0) + 1,
-          };
-        },
-      });
-    },
   });
 }
