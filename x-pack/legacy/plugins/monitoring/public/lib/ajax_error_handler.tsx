6928486bb469aee6077169b572b679672bcf77b8
chrisronline <chrisronline@gmail.com>
2019-11-22 16:30:29 -0500

    Fix type issue

--- x-pack/legacy/plugins/monitoring/public/lib/ajax_error_handler.tsx
+++ x-pack/legacy/plugins/monitoring/public/lib/ajax_error_handler.tsx
@@ -7,24 +7,19 @@
 import React from 'react';
 import { contains } from 'lodash';
 import { toastNotifications } from 'ui/notify';
+// @ts-ignore
 import { formatMsg } from 'ui/notify/lib';
-import {
-  EuiButton,
-  EuiSpacer,
-  EuiText,
-} from '@elastic/eui';
+import { EuiButton, EuiSpacer, EuiText } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { toMountPoint } from '../../../../../../src/plugins/kibana_react/public';
 
-export function formatMonitoringError(err) {
+export function formatMonitoringError(err: any) {
   // TODO: We should stop using Boom for errors and instead write a custom handler to return richer error objects
   // then we can do better messages, such as highlighting the Cluster UUID instead of requiring it be part of the message
   if (err.status && err.status !== -1 && err.data) {
     return (
       <EuiText>
-        <p>
-          { err.data.message }
-        </p>
+        <p>{err.data.message}</p>
         <EuiText size="xs">
           <FormattedMessage
             id="xpack.monitoring.ajaxErrorHandler.httpErrorMessage"
@@ -39,36 +34,34 @@ export function formatMonitoringError(err) {
   return formatMsg(err);
 }
 
-export function ajaxErrorHandlersProvider($injector) {
+export function ajaxErrorHandlersProvider($injector: any) {
   const kbnUrl = $injector.get('kbnUrl');
 
-  return (err) => {
+  return (err: any) => {
     if (err.status === 403) {
       // redirect to error message view
       kbnUrl.redirect('access-denied');
-    } else if (err.status === 404 && !contains(window.location.hash, 'no-data')) { // pass through if this is a 404 and we're already on the no-data page
+    } else if (err.status === 404 && !contains(window.location.hash, 'no-data')) {
+      // pass through if this is a 404 and we're already on the no-data page
       toastNotifications.addDanger({
         title: toMountPoint(
           <FormattedMessage
             id="xpack.monitoring.ajaxErrorHandler.requestFailedNotificationTitle"
             defaultMessage="Monitoring Request Failed"
-          />),
+          />
+        ),
         text: toMountPoint(
           <div>
-            { formatMonitoringError(err) }
+            {formatMonitoringError(err)}
             <EuiSpacer />
-            <EuiButton
-              size="s"
-              color="danger"
-              onClick={() => window.location.reload()}
-            >
+            <EuiButton size="s" color="danger" onClick={() => window.location.reload()}>
               <FormattedMessage
                 id="xpack.monitoring.ajaxErrorHandler.requestFailedNotification.retryButtonLabel"
                 defaultMessage="Retry"
               />
             </EuiButton>
           </div>
-        )
+        ),
       });
     } else {
       toastNotifications.addDanger({
@@ -76,8 +69,9 @@ export function ajaxErrorHandlersProvider($injector) {
           <FormattedMessage
             id="xpack.monitoring.ajaxErrorHandler.requestErrorNotificationTitle"
             defaultMessage="Monitoring Request Error"
-          />),
-        text: toMountPoint(formatMonitoringError(err))
+          />
+        ),
+        text: toMountPoint(formatMonitoringError(err)),
       });
     }
 
