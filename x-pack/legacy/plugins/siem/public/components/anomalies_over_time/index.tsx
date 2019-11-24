a1875b02eac8806b12787b6322522e65a79d3369
Patryk Kopyciński <contact@patrykkopycinski.com>
2019-11-24 19:40:04 +0100

    fix imports

--- x-pack/legacy/plugins/siem/public/components/anomalies_over_time/index.tsx
+++ x-pack/legacy/plugins/siem/public/components/anomalies_over_time/index.tsx
@@ -6,17 +6,21 @@
 
 import React from 'react';
 
+import { MatrixHistogramBasicProps } from '../matrix_histogram/types';
+import { MatrixOverTimeHistogramData } from '../../graphql/types';
+import { MatrixHistogram } from '../matrix_histogram';
 import * as i18n from './translation';
-import { MatrixOverTimeHistogram, MatrixOverTimeBasicProps } from '../matrix_over_time';
 
-export const AnomaliesOverTimeHistogram = (props: MatrixOverTimeBasicProps) => {
+export const AnomaliesOverTimeHistogram = (
+  props: MatrixHistogramBasicProps<MatrixOverTimeHistogramData>
+) => {
   const dataKey = 'anomaliesOverTime';
   const { totalCount } = props;
   const subtitle = `${i18n.SHOWING}: ${totalCount.toLocaleString()} ${i18n.UNIT(totalCount)}`;
   const { ...matrixOverTimeProps } = props;
 
   return (
-    <MatrixOverTimeHistogram
+    <MatrixHistogram
       title={i18n.ANOMALIES_COUNT_FREQUENCY_BY_ACTION}
       subtitle={subtitle}
       dataKey={dataKey}
