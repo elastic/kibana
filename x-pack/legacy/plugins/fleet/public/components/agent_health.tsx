38d88f6869f54c44ff6351882fed6571cb4d95fa
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2019-11-25 17:44:06 -0500

    [Fleet] Move agent status server side and API to get aggregated status for a policy

--- x-pack/legacy/plugins/fleet/public/components/agent_health.tsx
+++ x-pack/legacy/plugins/fleet/public/components/agent_health.tsx
@@ -6,11 +6,6 @@
 import React from 'react';
 import { FormattedMessage, FormattedRelative } from '@kbn/i18n/react';
 import { EuiHealth, EuiToolTip } from '@elastic/eui';
-import {
-  AGENT_TYPE_PERMANENT,
-  AGENT_TYPE_TEMPORARY,
-  AGENT_POLLING_THRESHOLD_MS,
-} from '../../common/constants';
 import { Agent } from '../../common/types/domain_data';
 
 interface Props {
@@ -40,41 +35,29 @@ const Status = {
   ),
   Error: (
     <EuiHealth color="danger">
-      <FormattedMessage id="xpack.fleet.agentHealth.errorStatusText" defaultMessage="Offline" />
+      <FormattedMessage id="xpack.fleet.agentHealth.errorStatusText" defaultMessage="Error" />
     </EuiHealth>
   ),
 };
 
-export const AgentHealth: React.SFC<Props> = ({ agent }) => {
-  // TODO: Use agent events as part of health calculation once we have them;
-  // until then, we just use last check in time
-  const { type, last_checkin: lastCheckIn } = agent;
-  const msLastCheckIn = new Date(lastCheckIn || 0).getTime();
-  const msSinceLastCheckIn = new Date().getTime() - msLastCheckIn;
-  const intervalsSinceLastCheckIn = Math.floor(msSinceLastCheckIn / AGENT_POLLING_THRESHOLD_MS);
-
-  let status: React.ReactElement = Status.Online;
-
-  if (!agent.active) {
-    status = Status.Inactive;
-  } else {
-    switch (type) {
-      case AGENT_TYPE_PERMANENT:
-        if (intervalsSinceLastCheckIn >= 4) {
-          status = Status.Error;
-          break;
-        }
-        if (intervalsSinceLastCheckIn >= 2) {
-          status = Status.Warning;
-          break;
-        }
-      case AGENT_TYPE_TEMPORARY:
-        if (intervalsSinceLastCheckIn >= 3) {
-          status = Status.Offline;
-          break;
-        }
-    }
+function getStatusComponent(agent: Agent): React.ReactElement {
+  switch (agent.status) {
+    case 'error':
+      return Status.Error;
+    case 'inactive':
+      return Status.Inactive;
+    case 'offline':
+      return Status.Offline;
+    case 'warning':
+      return Status.Warning;
+    default:
+      return Status.Online;
   }
+}
+
+export const AgentHealth: React.SFC<Props> = ({ agent }) => {
+  const { last_checkin: lastCheckIn } = agent;
+  const msLastCheckIn = new Date(lastCheckIn || 0).getTime();
 
   return (
     <EuiToolTip
@@ -96,7 +79,7 @@ export const AgentHealth: React.SFC<Props> = ({ agent }) => {
         )
       }
     >
-      {status}
+      {getStatusComponent(agent)}
     </EuiToolTip>
   );
 };
--- x-pack/legacy/plugins/fleet/server/libs/agent.ts
+++ x-pack/legacy/plugins/fleet/server/libs/agent.ts
@@ -20,6 +20,7 @@ import { PolicyLib } from './policy';
 import { FullPolicyFile } from '../repositories/policies/types';
 import { FrameworkUser } from '../adapters/framework/adapter_types';
 import { AgentEventsRepository, AgentEvent } from '../repositories/agent_events/types';
+import { AgentStatusHelper } from './agent_status_helper';
 
 export class AgentLib {
   constructor(
@@ -269,10 +270,33 @@ export class AgentLib {
     return action;
   }
 
+  public async getAgentsStatusForPolicy(user: FrameworkUser, policyId: string) {
+    const [all, error, offline] = await Promise.all(
+      [
+        undefined,
+        AgentStatusHelper.buildKueryForErrorAgents(),
+        AgentStatusHelper.buildKueryForOfflineAgents(),
+      ].map(kuery => {
+        return this.agentsRepository.listForPolicy(user, policyId, {
+          perPage: 0,
+          kuery,
+        });
+      })
+    );
+
+    return {
+      total: all.total,
+      online: all.total - error.total - offline.total,
+      error: error.total,
+      offline: offline.total,
+    };
+  }
+
   /**
    * List agents
    *
    * @param sortOptions
+   *
    * @param page
    * @param perPage
    */
