1b8bf51dc476692a2108920b6d8a350fe4fcaf10
Brian Seeders <brian.seeders@elastic.co>
2019-11-26 12:32:59 -0500

    Re-add failed_tests cli

--- test/scripts/jenkins_test_setup.sh
+++ test/scripts/jenkins_test_setup.sh
@@ -2,6 +2,9 @@ set -e
 
 function post_work() {
   set +e
+  if [[ -z "$IS_PIPELINE_JOB" ]] ; then
+    node "$KIBANA_DIR/src/dev/failed_tests/cli"
+  fi
 
   if [[ -z "$REMOVE_KIBANA_INSTALL_DIR" && -z "$KIBANA_INSTALL_DIR" && -d "$KIBANA_INSTALL_DIR" ]]; then
     rm -rf "$REMOVE_KIBANA_INSTALL_DIR"
