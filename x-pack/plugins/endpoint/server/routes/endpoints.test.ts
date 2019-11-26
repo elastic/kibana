68d32853b61c21a501100b99f6c4dfef02c41fb0
nnamdifrankie <franklin.ejoh@elastic.co>
2019-11-26 00:19:39 -0500

    IS-21:more type checks

--- x-pack/plugins/endpoint/server/routes/endpoints.test.ts
+++ x-pack/plugins/endpoint/server/routes/endpoints.test.ts
@@ -3,15 +3,26 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { IRouter, KibanaResponseFactory, RequestHandlerContext } from 'kibana/server';
+import {
+  IRouter,
+  KibanaResponseFactory,
+  RequestHandler,
+  RequestHandlerContext,
+  RouteConfig,
+} from 'kibana/server';
 import { httpServiceMock } from '../../../../../src/core/server/http/http_service.mock';
 import { httpServerMock } from '../../../../../src/core/server/http/http_server.mocks';
 import { registerEndpointRoutes } from './endpoints';
 import { EndpointRequestContext } from '../handlers/endpoint_handler';
+import { SearchResponse } from 'elasticsearch';
+import { EndpointData } from '../types';
+import { ObjectType } from '@kbn/config-schema';
 
 describe('endpoints route test', () => {
   let routerMock: jest.Mocked<IRouter>;
   let mockResponse: jest.Mocked<KibanaResponseFactory>;
+  let routeHandler: RequestHandler<ObjectType<any>, ObjectType<any>, ObjectType<any>>;
+  let routeConfig: RouteConfig<ObjectType<any>, ObjectType<any>, ObjectType<any>>;
 
   beforeEach(() => {
     routerMock = httpServiceMock.createRouter();
@@ -23,14 +34,18 @@ describe('endpoints route test', () => {
       body: {},
       params: { id: 'endpoint_id' },
     });
+    const response: SearchResponse<EndpointData> = ({
+      id: 'endpoint_id',
+    } as unknown) as SearchResponse<EndpointData>;
     const endpointHandler: jest.Mocked<EndpointRequestContext> = {
-      findEndpoint: jest.fn((endpointId: string) => ({ id: endpointId })),
+      findEndpoint: jest.fn(),
+      findLatestOfAllEndpoints: jest.fn(),
     };
+    endpointHandler.findEndpoint.mockReturnValue(Promise.resolve(response));
     registerEndpointRoutes(routerMock, endpointHandler);
-    const [routeConfig, routeHandler] = routerMock.get.mock.calls.find(([{ path }]) =>
+    [routeConfig, routeHandler] = routerMock.get.mock.calls.find(([{ path }]) =>
       path.startsWith('/api/endpoint/endpoints')
-    );
-
+    )!;
     await routeHandler(({} as unknown) as RequestHandlerContext, mockRequest, mockResponse);
     expect(routeConfig.options).toEqual({ authRequired: true });
     expect(mockResponse.ok).toBeCalled();
@@ -42,13 +57,18 @@ describe('endpoints route test', () => {
       body: {},
       params: {},
     });
+    const response: SearchResponse<EndpointData> = ({
+      id: 'all',
+    } as unknown) as SearchResponse<EndpointData>;
     const endpointHandler: jest.Mocked<EndpointRequestContext> = {
-      findLatestOfAllEndpoints: jest.fn(() => ({ id: 'all' })),
+      findEndpoint: jest.fn(),
+      findLatestOfAllEndpoints: jest.fn(),
     };
+    endpointHandler.findLatestOfAllEndpoints.mockReturnValue(Promise.resolve(response));
     registerEndpointRoutes(routerMock, endpointHandler);
-    const [routeConfig, routeHandler] = routerMock.get.mock.calls.find(
+    [routeConfig, routeHandler] = routerMock.get.mock.calls.find(
       ([{ path }]) => path === '/api/endpoint/endpoints'
-    );
+    )!;
     await routeHandler(({} as unknown) as RequestHandlerContext, mockRequest, mockResponse);
     expect(routeConfig.options).toEqual({ authRequired: true });
     expect(mockResponse.ok).toBeCalled();
