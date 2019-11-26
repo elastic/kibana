43a9158a3e8724e3303f43153222916e94cf6aa0
Anton Dosov <anton.dosov@elastic.co>
2019-11-26 14:22:34 +0100

    Typescriptify and shim kbn_tp_run_pipeline test plugin (#50645)
    * Typscriptify and shim kbn_tp_run_pipeline test plugin
    
    * fix imports to not re-export ‘legacy’ from root of plugin

--- test/interpreter_functional/plugins/kbn_tp_run_pipeline/public/np_ready/index.ts
+++ test/interpreter_functional/plugins/kbn_tp_run_pipeline/public/np_ready/index.ts
@@ -17,20 +17,12 @@
  * under the License.
  */
 
-import { PluginInitializerContext } from '../../../core/public';
-import { ExpressionsPublicPlugin } from './plugin';
+import { PluginInitializer, PluginInitializerContext } from 'src/core/public';
+import { Plugin, StartDeps } from './plugin';
+export { StartDeps };
 
-export { ExpressionsPublicPlugin as Plugin };
-
-export * from './plugin';
-export * from './types';
-export * from '../common';
-export { interpreterProvider, ExpressionInterpret } from './interpreter_provider';
-export { ExpressionRenderer, ExpressionRendererProps } from './expression_renderer';
-export { ExpressionDataHandler } from './execute';
-
-export { RenderResult, ExpressionRenderHandler } from './render';
-
-export function plugin(initializerContext: PluginInitializerContext) {
-  return new ExpressionsPublicPlugin(initializerContext);
-}
+export const plugin: PluginInitializer<void, void, {}, StartDeps> = (
+  initializerContext: PluginInitializerContext
+) => {
+  return new Plugin(initializerContext);
+};
