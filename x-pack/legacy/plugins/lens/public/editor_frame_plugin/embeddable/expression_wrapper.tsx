9a3282f3ef806513bf1aecf6548a82b9763a39e0
Anton Dosov <anton.dosov@elastic.co>
2019-11-26 12:18:48 +0100

    Render error inline for lens emedbale as it was previosly

--- x-pack/legacy/plugins/lens/public/editor_frame_plugin/embeddable/expression_wrapper.tsx
+++ x-pack/legacy/plugins/lens/public/editor_frame_plugin/embeddable/expression_wrapper.tsx
@@ -50,6 +50,7 @@ export function ExpressionWrapper({
             padding="m"
             expression={expression}
             searchContext={{ ...context, type: 'kibana_context' }}
+            renderError={error => <div>{error}</div>}
           />
         </div>
       )}
