36e2a5435c4f57e79849cbb1846644cbd9f158dc
ruflin <spam@ruflin.com>
2019-11-27 11:42:23 +0100

    always encode

--- x-pack/legacy/plugins/epm/server/lib/agent/agent.ts
+++ x-pack/legacy/plugins/epm/server/lib/agent/agent.ts
@@ -7,21 +7,30 @@
 import Mustache from 'mustache';
 
 interface Manifest {
-  vars: object[];
+  vars: VarsEntry[];
 }
 
+interface VarsEntry {
+  name: string;
+  default: string;
+}
 /**
  * This takes a manifest object as input and merges it with the input template.
  * It returns the resolved template as a string.
  */
 export function createInput(manifest: Manifest, inputTemplate: string): string {
-  const view = {};
+  const view: any = {};
 
   for (const v of manifest.vars) {
-    console.log(v.name);
     view[v.name] = v.default;
   }
 
+  // This disables escaping. All our configs should be never escaped.
+  // Otherwise the creator would have to prefix all variables with &
+  // which is not very user friendly.
+  Mustache.escape = function(v) {
+    return v;
+  };
   // Mustache.parse(inputTemplate);
   return Mustache.render(inputTemplate, view);
 }
