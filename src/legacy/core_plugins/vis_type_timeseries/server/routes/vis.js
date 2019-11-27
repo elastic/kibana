b485d56eaa394797c86d7ba6955485d0c9413ff7
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-11-27 16:36:09 -0700

    [Route audit] adds initial Joi validation schema to payload

--- src/legacy/core_plugins/vis_type_timeseries/server/routes/vis.js
+++ src/legacy/core_plugins/vis_type_timeseries/server/routes/vis.js
@@ -19,11 +19,87 @@
 
 import { getVisData } from '../lib/get_vis_data';
 import Boom from 'boom';
+import Joi from 'joi';
 
+// The request payload validation could use additional work on default values, allowed values, required vs optional etc.
+const requestPayload = {
+  payload: Joi.object({
+    filters: Joi.array().allow(null),
+    panels: Joi.array().items(Joi.object({
+      annotations: Joi.array().items(Joi.object({
+        color: Joi.string(),
+        fields: Joi.string(),
+        icon: Joi.string(),
+        id: Joi.string(),
+        ignore_global_filters: Joi.number().integer(), // allow only 0 and 1
+        ignore_panel_filters: Joi.number().integer(), // allow only 0 and 1
+        index_pattern: Joi.string(),
+        query_string: Joi.object({
+          language: Joi.string().allow(''),
+          query: Joi.string().allow(''),
+        }),
+        template: Joi.string.allow(null, ''),
+        time_field: Joi.string(),
+      })),
+      axis_formatter: Joi.string(),
+      axis_position: Joi.string(),
+      axis_scale: Joi.string(),
+      background_color_rules: Joi.object({
+        id: Joi.string(),
+      }),
+      default_index_pattern: Joi.string(),
+      default_timefield: Joi.string(),
+      id: Joi.string(),
+      index_pattern: Joi.string().optional(),
+      interval: Joi.string(),
+      isModelInvalid: Joi.boolean(),
+      legend_position: Joi.string(),
+      series: Joi.array()
+        .items(
+          Joi.object({
+            axis_position: Joi.string(),
+            chart_type: Joi.string(),
+            color: Joi.string().optional(),
+            fill: Joi.number(),
+            formatter: Joi.string(),
+            id: Joi.string(),
+            label: Joi.string().allow('', null),
+            line_width: Joi.string(),
+            metrics: Joi.array()
+              .items(
+                Joi.object({
+                  id: Joi.string(),
+                  numerator: Joi.string(),
+                  type: Joi.string(),
+                })
+              ),
+            point_size: Joi.string(),
+            separate_axis: Joi.number().integer(),
+            split_mode: Joi.string(),
+            stacked: Joi.string(),
+          }),
+        ),
+      show_grid: Joi.number().integer(),
+      show_legend: Joi.number().integer(),
+      time_field: Joi.string(),
+      type: Joi.string(),
+    })),
+    query: Joi.array().allow(null),
+    state: Joi.object({}),
+    timerange: Joi.object({
+      timezone: Joi.string(),
+      min: Joi.string(),
+      max: Joi.string()
+    })
+  })
+};
 export const visDataRoutes = server => {
   server.route({
     path: '/api/metrics/vis/data',
     method: 'POST',
+    options: {
+      validate: requestPayload,
+    },
     handler: async req => {
       try {
         return await getVisData(req);
