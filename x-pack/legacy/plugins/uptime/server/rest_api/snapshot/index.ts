dc8edf723ba189b4f64af5dd110d24ad803e2c7c
Justin Kambic <justin.kambic@elastic.co>
2019-11-26 19:44:46 -0500

    [Uptime] Update snapshot counts (#48035)
    * Add snapshot count function that uses monitor iterator class.
    
    * Add js-doc comment.
    
    * Add snapshot count route.
    
    * Start adding snapshot state management.
    
    * Commit changes that were missed in previous.
    
    * Finish implementing snapshot count redux code.
    
    * Add basePath setter and type to ui state.
    
    * Dispatch basePath set action on app render.
    
    * Replace GQL-powered Snapshot export with Redux/Rest-powered version.
    
    * Extract presentational element to dedicated component.
    
    * Update broken test.
    
    * Rename action.
    
    * Add comments to clarify adapter function.
    
    * Remove obsolete code.
    
    * Add ui state field to store for tracking when app refreshes.
    
    * Make snapshot component refresh on global refresh via redux state.
    
    * Remove obsolete @ts-igore.
    
    * Alphabetize imports.
    
    * Port functional test fixture to REST test.
    
    * Delete snapshot GQL query.
    
    * Update API test fixtures to match new snapshot count REST response shape.
    
    * Update Snapshot count type check to be stricter.
    
    * Add tests for Snapshot API call.
    
    * Rename new test file from tsx to ts, it has no JSX.
    
    * Add tests for snapshot reducer.
    
    * Add tests for UI reducer.
    
    * Add tests for selectors.
    
    * Delete unused test file.
    
    * Move snapshot getter and map/reduce logic to dedicated helper function.
    
    * Add test for snapshot helper function.
    
    * Export type from module.
    
    * Rename outdated snapshot file.
    
    * Add action creator for fetch success.
    
    * Reorganize ui actions file.
    
    * Update snapshot effect to put error when input params are not valid.
    
    * Simplify typing code for a function.
    
    * Simplify snapshot count reduction.
    
    * Rename a function.
    
    * Rewrite a function to increase code clarity.
    
    * Remove duplicated interface.
    
    * Add very high ceiling for snapshot count iteration.
    
    * Update broken test assertion.

--- x-pack/legacy/plugins/uptime/server/rest_api/snapshot/index.ts
+++ x-pack/legacy/plugins/uptime/server/rest_api/snapshot/index.ts
@@ -4,5 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { fetchPage, MonitorGroups, MonitorLocCheckGroup, MonitorGroupsPage } from './fetch_page';
-export { MonitorGroupIterator } from './monitor_group_iterator';
+export { createGetSnapshotCount } from './get_snapshot_count';
