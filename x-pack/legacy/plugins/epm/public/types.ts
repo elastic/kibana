dd30a4806062335b632731e3265b3485115f040e
neptunian <sandra.gonzales@elastic.co>
2019-11-27 13:00:44 -0500

    add comment about InstallStatus type overlapping InstallationStatus

--- x-pack/legacy/plugins/epm/public/types.ts
+++ x-pack/legacy/plugins/epm/public/types.ts
@@ -4,6 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+ // Separate install status type for the UI, which overlaps with common/types/InstallationStatus,
+ // look into finding a way to combine them?
 export enum InstallStatus {
   installed = 'installed',
   notInstalled = 'not_installed',
