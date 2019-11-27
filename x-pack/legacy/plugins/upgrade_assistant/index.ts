78de7afab44ffba859ddf465c403e7c221f86f73
Ahmad Bamieh <ahmadbamieh@gmail.com>
2019-11-27 02:56:10 +0200

    fix upgrade_assistant code

--- x-pack/legacy/plugins/upgrade_assistant/index.ts
+++ x-pack/legacy/plugins/upgrade_assistant/index.ts
@@ -8,7 +8,7 @@ import Joi from 'joi';
 import { Legacy } from 'kibana';
 import { resolve } from 'path';
 import mappings from './mappings.json';
-import { plugin } from './server/np_ready';
+import { plugin } from './server';
 
 export function upgradeAssistant(kibana: any) {
   const publicSrc = resolve(__dirname, 'public');
