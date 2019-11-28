676f2beac159f24cadeb788f050782cf6cb1a01c
Avinar-24 <artyom_gospodarsky@epam.com>
2019-11-28 19:20:32 +0300

    Fix type error

--- src/plugins/data/public/types.ts
+++ src/plugins/data/public/types.ts
@@ -25,13 +25,14 @@ import { ISearchSetup, ISearchStart } from './search';
 import { IGetSuggestions } from './suggestions_provider/types';
 import { QuerySetup, QueryStart } from './query';
 import { IndexPatternSelectProps } from './ui/index_pattern_select';
-import { IndexPatternsStart } from './index_patterns';
+import { IndexPatternsStart, IndexPatternsSetup } from './index_patterns';
 
 export interface DataPublicPluginSetup {
   autocomplete: AutocompletePublicPluginSetup;
   search: ISearchSetup;
   fieldFormats: FieldFormatsSetup;
   query: QuerySetup;
+  indexPatterns?: IndexPatternsSetup;
 }
 
 export interface DataPublicPluginStart {
