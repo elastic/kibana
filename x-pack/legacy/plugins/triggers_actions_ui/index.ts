f6daeb3d5ea8c281fa3ce7393015ce5684cbd25a
Mike Cote <mikecote@users.noreply.github.com>
2019-11-28 15:09:59 -0500

    Rename plugin id

--- x-pack/legacy/plugins/triggers_actions_ui/index.ts
+++ x-pack/legacy/plugins/triggers_actions_ui/index.ts
@@ -10,7 +10,7 @@ import { resolve } from 'path';
 
 export function triggersActionsUI(kibana: any) {
   return new kibana.Plugin({
-    id: 'triggers_actions_ui',
+    id: 'triggersActionsUI',
     configPrefix: 'xpack.triggers_actions_ui',
     publicDir: resolve(__dirname, 'public'),
     require: ['kibana', 'actions'],
