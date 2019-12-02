0108b50cb6c52acb199168b6c6089763e486c140
Christiane Heiligers <christiane.heiligers@elastic.co>
2019-12-02 16:37:52 -0700

    Adds more validation items to the TSVB payload

--- src/legacy/core_plugins/vis_type_timeseries/server/routes/vis.js
+++ src/legacy/core_plugins/vis_type_timeseries/server/routes/vis.js
@@ -44,21 +44,43 @@ const requestPayload = {
       axis_formatter: Joi.string(),
       axis_position: Joi.string(),
       axis_scale: Joi.string(),
+      axis_min: Joi.number(),
+      axis_max: Joi.number(),
       bar_color_rules: Joi.array().allow(null),
-      background_color_rules: Joi.object({
+      background_color: Joi.string().optional(),
+      background_color_rules: Joi.array().items(Joi.object({
         id: Joi.string(),
-      }),
+      })),
       default_index_pattern: Joi.string(),
       default_timefield: Joi.string(),
       drilldown_url: Joi.string().allow(''),
       drop_last_bucket: Joi.number().integer(),
+      filter: Joi.any(),
+      // Gauge
+      gauge_color_rules: Joi.array().items(
+        Joi.object({
+          gauge: Joi.string(),
+          id: Joi.string(),
+          operator: Joi.string(),
+          value: Joi.number(),
+        })
+      ),
+      gauge_width: Joi.any(),
+      gauge_inner_width: Joi.any(),
+      gauge_style: Joi.any(),
+      gauge_max: Joi.any(),
+      // general
       id: Joi.string(),
+      ignore_global_filters: Joi.number(),
       index_pattern: Joi.string().optional(),
       interval: Joi.string(),
       isModelInvalid: Joi.boolean(),
+      // unknown
       legend_position: Joi.string(),
+      // table ??
       pivot_id: Joi.string(),
       pivot_label: Joi.string(),
+      // general
       series: Joi.array()
         .items(
           Joi.object({
@@ -67,39 +89,68 @@ const requestPayload = {
             color: Joi.string(),
             color_rules: Joi.array(),
             fill: Joi.number(),
+            filter: Joi.object({
+              query: Joi.string().allow(''),
+              language: Joi.string(),
+            }),
             formatter: Joi.string(),
+            hidden: Joi.boolean().optional(),
             id: Joi.string(),
             label: Joi.string().allow('', null),
             line_width: Joi.number(),
             metrics: Joi.array()
               .items(
                 Joi.object({
+                  field: Joi.string().allow(null),
                   id: Joi.string(),
+                  metric_agg: Joi.string(),
                   numerator: Joi.string(),
+                  sigma: Joi.string().allow(''),
                   type: Joi.string(),
-                  field: Joi.string(),
-                  sigma: Joi.string().allow('')
                 })
               ),
+            offset_time: Joi.string().allow(''),
+            override_index_pattern: Joi.number().optional(),
             point_size: Joi.number(),
-            separate_axis: Joi.number().integer(),
-            seperate_axis: Joi.number().integer(),
+            separate_axis: Joi.number().integer(), // Timeseries, Gauge
+            seperate_axis: Joi.number().integer(), // Table
+            series_index_pattern: Joi.string().allow(''),
+            series_time_field: Joi.string().allow(''),
+            series_interval: Joi.string(),
+            series_drop_last_bucket: Joi.number().integer(),
+            split_color_mode: Joi.string(),
+            split_filters: Joi.array().items(Joi.object({
+              id: Joi.string().allow('', null),
+              color: Joi.string(),
+              filter: Joi.object({
+                language: Joi.string(),
+                query: Joi.string().allow('')
+              })
+            })),
             split_mode: Joi.string(),
             stacked: Joi.string(),
             terms_field: Joi.string(),
             terms_order_by: Joi.string(),
-            split_color_mode: Joi.string(),
-            offset_time: Joi.string().allow(''),
-            value_template: Joi.string().allow(''),
+            terms_size: Joi.string(),
+            terms_direction: Joi.string(),
+            time_range_mode: Joi.string().allow(null),
             trend_arrows: Joi.number(),
+            value_template: Joi.string().allow(''),
           }),
         ),
       show_grid: Joi.number().integer(),
       show_legend: Joi.number().integer(),
-      time_field: Joi.string(),
-      type: Joi.string(),
+      time_field: Joi.string().allow(null),
+      time_range_mode: Joi.string().optional(),
+      type: Joi.string().required(),
     })),
-    query: Joi.array().allow(null),
+    // general
+    query: Joi.array().items(
+      Joi.object({
+        language: Joi.string(),
+        query: Joi.string().allow('')
+      })
+    ).allow(null),
     state: Joi.object({}),
     timerange: Joi.object({
       timezone: Joi.string(),
