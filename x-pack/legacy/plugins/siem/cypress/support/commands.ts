b51e357012e0533bef8c59d2334512348f64ac71
Patryk Kopyciński <contact@patrykkopycinski.com>
2019-12-02 16:06:43 +0100

    cleanup

--- x-pack/legacy/plugins/siem/cypress/support/commands.ts
+++ x-pack/legacy/plugins/siem/cypress/support/commands.ts
@@ -13,17 +13,40 @@ import yaml from 'js-yaml';
 
 const KIBANA_DEV_YML_PATH = '../../../../config/kibana.dev.yml';
 
-Cypress.Commands.add('logout', () => {
-  cy.request({
-    method: 'GET',
-    url: `${Cypress.config().baseUrl}/logout`,
-  }).then(response => {
-    expect(response.status).to.eq(200);
-  });
-});
+type logout = typeof logout;
+type loginRequest = typeof loginRequest;
+type loginViaEnvironmentCredentials = typeof loginViaEnvironmentCredentials;
+type loginViaConfig = typeof loginViaConfig;
+type login = typeof login;
+type visitSiem = typeof visitSiem;
 
-Cypress.Commands.add('loginRequest', (username, password) => {
-  cy.request({
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  module Cypress {
+    interface Chainable {
+      logout: logout;
+      loginRequest: loginRequest;
+      loginViaEnvironmentCredentials: loginViaEnvironmentCredentials;
+      loginViaConfig: loginViaConfig;
+      login: login;
+      visitSiem: visitSiem;
+    }
+  }
+}
+
+export function logout() {
+  return cy
+    .request({
+      method: 'GET',
+      url: `${Cypress.config().baseUrl}/logout`,
+    })
+    .then(response => {
+      expect(response.status).to.eq(200);
+    });
+}
+
+export function loginRequest(username: string, password: string) {
+  return cy.request({
     body: {
       username,
       password,
@@ -32,20 +55,23 @@ Cypress.Commands.add('loginRequest', (username, password) => {
     method: 'POST',
     url: `${Cypress.config().baseUrl}/api/security/v1/login`,
   });
-});
+}
 
-Cypress.Commands.add('loginViaEnvironmentCredentials', () => {
-  cy.loginRequest(Cypress.env('ELASTICSEARCH_USERNAME'), Cypress.env('ELASTICSEARCH_PASSWORD'));
-});
+export function loginViaEnvironmentCredentials() {
+  return cy.loginRequest(
+    Cypress.env('ELASTICSEARCH_USERNAME'),
+    Cypress.env('ELASTICSEARCH_PASSWORD')
+  );
+}
 
-Cypress.Commands.add('loginViaConfig', () => {
-  cy.readFile(KIBANA_DEV_YML_PATH).then(kibanaDevYml => {
+export function loginViaConfig() {
+  return cy.readFile(KIBANA_DEV_YML_PATH).then(kibanaDevYml => {
     const config = yaml.safeLoad(kibanaDevYml);
     cy.loginRequest(config.elasticsearch.username, config.elasticsearch.password);
   });
-});
+}
 
-Cypress.Commands.add('login', () => {
+export function login() {
   if (
     Cypress.env('ELASTICSEARCH_USERNAME') != null &&
     Cypress.env('ELASTICSEARCH_PASSWORD') != null
@@ -54,11 +80,18 @@ Cypress.Commands.add('login', () => {
   } else {
     cy.loginViaConfig();
   }
-});
+}
 
-Cypress.Commands.add('visitSiem', url => {
+export function visitSiem(url: string) {
   cy.login();
   cy.visit(`${Cypress.config().baseUrl}${url}`);
   cy.viewport('macbook-15');
   cy.contains('a', 'SIEM', { timeout: 30000 });
-});
+}
+
+Cypress.Commands.add('logout', logout);
+Cypress.Commands.add('loginRequest', loginRequest);
+Cypress.Commands.add('loginViaEnvironmentCredentials', loginViaEnvironmentCredentials);
+Cypress.Commands.add('loginViaConfig', loginViaConfig);
+Cypress.Commands.add('login', login);
+Cypress.Commands.add('visitSiem', visitSiem);
