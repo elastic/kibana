1b99053e8d6f6e098636a478ca4e04c73e50ab91
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2019-12-02 17:55:15 -0500

    fix cherry pick imports

--- x-pack/legacy/plugins/siem/public/components/navigation/types.ts
+++ x-pack/legacy/plugins/siem/public/components/navigation/types.ts
@@ -4,7 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { esFilters, Query } from '../../../../../../../src/plugins/data/public';
+import { Filter } from '@kbn/es-query';
+import { Query } from '../../../../../../../src/plugins/data/public';
 import { HostsTableType } from '../../store/hosts/model';
 import { UrlInputsModel } from '../../store/inputs/model';
 import { CONSTANTS, UrlStateType } from '../url_state/constants';
@@ -22,7 +23,7 @@ export interface SiemNavigationComponentProps {
   tabName: HostsTableType | undefined;
   urlState: {
     [CONSTANTS.appQuery]?: Query;
-    [CONSTANTS.filters]?: esFilters.Filter[];
+    [CONSTANTS.filters]?: Filter[];
     [CONSTANTS.savedQuery]?: string;
     [CONSTANTS.timerange]: UrlInputsModel;
     [CONSTANTS.timeline]: Timeline;
