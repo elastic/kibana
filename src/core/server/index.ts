5a13a20076076705bfe618b234d3a4ac8e3466dd
Rudolf Meijering <skaapgif@gmail.com>
2019-12-02 18:45:07 +0100

    Split legacy plugin discovery, expose SavedObjects scopedClient, wrappers, repository (#48882) (#51978)
    * Split legacy plugin discovery, expose internal SavedObjectsClient
    
    * Expose internal SavedObjectsClient to plugins
    
    * Add more documentation
    
    * Expose client wrappers, repository, scoped client from SavedObjects
    
    * Remove unused onBeforeWrite
    
    * Refactor Service / Repository for testability
    
    * Bind exposed clientProvider methods
    
    * Fix eArchiver's KibanaMigrator
    
    * Cleanup
    
    * Use APICaller type
    
    * Expose SavedObjectsServiceStart to plugins
    
    * API documentation
    
    * Rename API methods to be verbs

--- src/core/server/index.ts
+++ src/core/server/index.ts
@@ -45,6 +45,7 @@ import { PluginsServiceSetup, PluginsServiceStart, PluginOpaqueId } from './plug
 import { ContextSetup } from './context';
 import { IUiSettingsClient, UiSettingsServiceSetup } from './ui_settings';
 import { SavedObjectsClientContract } from './saved_objects/types';
+import { SavedObjectsServiceSetup, SavedObjectsServiceStart } from './saved_objects';
 
 export { bootstrap } from './bootstrap';
 export { ConfigPath, ConfigService, EnvironmentMode, PackageInfo } from './config';
@@ -149,6 +150,7 @@ export {
   SavedObjectsClientProviderOptions,
   SavedObjectsClientWrapperFactory,
   SavedObjectsClientWrapperOptions,
+  SavedObjectsClientFactory,
   SavedObjectsCreateOptions,
   SavedObjectsErrorHelpers,
   SavedObjectsExportOptions,
@@ -170,7 +172,13 @@ export {
   SavedObjectsLegacyService,
   SavedObjectsUpdateOptions,
   SavedObjectsUpdateResponse,
+  SavedObjectsServiceStart,
+  SavedObjectsServiceSetup,
   SavedObjectsDeleteOptions,
+  ISavedObjectsRepository,
+  SavedObjectsRepository,
+  SavedObjectsDeleteByNamespaceOptions,
+  SavedObjectsIncrementCounterOptions,
 } from './saved_objects';
 
 export {
@@ -238,6 +246,8 @@ export interface CoreSetup {
   elasticsearch: ElasticsearchServiceSetup;
   /** {@link HttpServiceSetup} */
   http: HttpServiceSetup;
+  /** {@link SavedObjectsServiceSetup} */
+  savedObjects: SavedObjectsServiceSetup;
   /** {@link UiSettingsServiceSetup} */
   uiSettings: UiSettingsServiceSetup;
 }
@@ -247,6 +257,9 @@ export interface CoreSetup {
  *
  * @public
  */
-export interface CoreStart {} // eslint-disable-line @typescript-eslint/no-empty-interface
+export interface CoreStart {
+  /** {@link SavedObjectsServiceStart} */
+  savedObjects: SavedObjectsServiceStart;
+}
 
 export { ContextSetup, PluginsServiceSetup, PluginsServiceStart, PluginOpaqueId };
