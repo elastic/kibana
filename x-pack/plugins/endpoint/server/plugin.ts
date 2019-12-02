0d34dacfab91fadc748ef92d2484dd6883299444
oatkiller <robert.austin@elastic.co>
2019-12-02 08:17:26 -0500

    fixing typecheck

--- x-pack/plugins/endpoint/server/plugin.ts
+++ x-pack/plugins/endpoint/server/plugin.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Plugin, CoreSetup, CoreStart } from 'kibana/server';
+import { Plugin, CoreSetup } from 'kibana/server';
 import { PluginSetupContract as FeaturesPluginSetupContract } from '../../features/server';
 import { addRoutes } from './routes';
 
