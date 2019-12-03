b0437874b5da0f30545f508f814772ed882d381d
Mike Cote <mikecote@users.noreply.github.com>
2019-12-03 13:58:18 -0500

    Use unique connector names to allow re-running tests

--- x-pack/test/functional/apps/triggers_actions_ui/connectors.ts
+++ x-pack/test/functional/apps/triggers_actions_ui/connectors.ts
@@ -4,10 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import uuid from 'uuid';
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 const ENTER_KEY = '\uE007';
+
+function generateUniqueKey() {
+  return uuid.v4().replace(/-/g, '');
+}
 
 export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const testSubjects = getService('testSubjects');
@@ -20,6 +25,8 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     });
 
     it('should create a connector', async () => {
+      const connectorName = generateUniqueKey();
+
       await pageObjects.triggersActionsUI.createActionConnector();
 
       const serverLogCard = await testSubjects.find('.server-log-card');
@@ -28,7 +35,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const nameInput = await testSubjects.find('nameInput');
       await nameInput.click();
       await nameInput.clearValue();
-      await nameInput.type('My server log connector');
+      await nameInput.type(connectorName);
 
       const saveButton = await find.byCssSelector(
         '[data-test-subj="saveActionButton"]:not(disabled)'
@@ -38,13 +45,16 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const toastMessage = await find.byCssSelector(
         '[data-test-subj="euiToastHeader"] .euiToastHeader__title'
       );
-      expect(await toastMessage.getVisibleText()).to.eql(`Created 'My server log connector'`);
+      expect(await toastMessage.getVisibleText()).to.eql(`Created '${connectorName}'`);
 
       const closeButton = await testSubjects.find('toastCloseButton');
       await closeButton.click();
     });
 
     it('should edit a connector', async () => {
+      const connectorName = generateUniqueKey();
+      const updatedConnectorName = `${connectorName}updated`;
+
       await pageObjects.triggersActionsUI.createActionConnector();
 
       const serverLogCard = await testSubjects.find('.server-log-card');
@@ -53,7 +63,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const nameInput = await testSubjects.find('nameInput');
       await nameInput.click();
       await nameInput.clearValue();
-      await nameInput.type('My server log connector to update');
+      await nameInput.type(connectorName);
 
       const saveButton = await find.byCssSelector(
         '[data-test-subj="saveActionButton"]:not(disabled)'
@@ -66,7 +76,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const searchBox = await find.byCssSelector('[data-test-subj="actionsList"] .euiFieldSearch');
       await searchBox.click();
       await searchBox.clearValue();
-      await searchBox.type('My server log connector to update');
+      await searchBox.type(connectorName);
       await searchBox.pressKeys(ENTER_KEY);
 
       const rowsBeforeDelete = await testSubjects.findAll('connectors-row');
@@ -77,7 +87,8 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
       const nameInputToUpdate = await testSubjects.find('nameInput');
       await nameInputToUpdate.click();
-      await nameInputToUpdate.type(' is now updated');
+      await nameInputToUpdate.clearValue();
+      await nameInputToUpdate.type(updatedConnectorName);
 
       const saveEditButton = await find.byCssSelector(
         '[data-test-subj="saveActionButton"]:not(disabled)'
@@ -87,15 +98,15 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const toastMessage = await find.byCssSelector(
         '[data-test-subj="euiToastHeader"] .euiToastHeader__title'
       );
-      expect(await toastMessage.getVisibleText()).to.eql(
-        `Updated 'My server log connector to update is now updated'`
-      );
+      expect(await toastMessage.getVisibleText()).to.eql(`Updated '${updatedConnectorName}'`);
 
       const closeToastUpdatedButton = await testSubjects.find('toastCloseButton');
       await closeToastUpdatedButton.click();
     });
 
     it('should delete a connector', async () => {
+      const connectorName = generateUniqueKey();
+
       await pageObjects.triggersActionsUI.createActionConnector();
 
       const serverLogCard = await testSubjects.find('.server-log-card');
@@ -104,7 +115,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const nameInput = await testSubjects.find('nameInput');
       await nameInput.click();
       await nameInput.clearValue();
-      await nameInput.type('My server log connector to delete');
+      await nameInput.type(connectorName);
 
       const saveButton = await find.byCssSelector(
         '[data-test-subj="saveActionButton"]:not(disabled)'
@@ -119,7 +130,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const searchBox = await find.byCssSelector('[data-test-subj="actionsList"] .euiFieldSearch');
       await searchBox.click();
       await searchBox.clearValue();
-      await searchBox.type('My server log connector to delete');
+      await searchBox.type(connectorName);
       await searchBox.pressKeys(ENTER_KEY);
 
       const rowsBeforeDelete = await testSubjects.findAll('connectors-row');
