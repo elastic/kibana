5240ea3fa44a802344db304024dbb5d08dd866fc
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-12-04 10:51:58 +0000

    force the build

--- x-pack/legacy/plugins/apm/cypress/ci/prepare-kibana.sh
+++ x-pack/legacy/plugins/apm/cypress/ci/prepare-kibana.sh
@@ -3,7 +3,7 @@ set -e
 
 CYPRESS_DIR="x-pack/legacy/plugins/apm/cypress"
 
-echo "1/3 Install dependencies..."
+echo "1/3 Install dependencies ..."
 # shellcheck disable=SC1091
 source src/dev/ci_setup/setup_env.sh true
 yarn kbn bootstrap
@@ -11,13 +11,13 @@ cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.dev.yml
 echo 'elasticsearch:' >> config/kibana.dev.yml
 cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.yml
 
-echo "2/3 Ingest test data..."
+echo "2/3 Ingest test data ..."
 pushd ${CYPRESS_DIR}
 yarn install
 curl --silent https://storage.googleapis.com/apm-ui-e2e-static-data/events.json --output ingest-data/events.json
 node ingest-data/replay.js --server-url http://localhost:8200 --secret-token abcd --events ./events.json > ingest-data.log
 
-echo "3/3 Start Kibana..."
+echo "3/3 Start Kibana ..."
 popd
 ## Might help to avoid FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
 export NODE_OPTIONS="--max-old-space-size=4096"
