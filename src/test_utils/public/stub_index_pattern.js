acbf9cf5a39cc544ee4e1835b3fc438aca8a4f71
Liza K <liza.katz@elastic.co>
2019-12-04 08:51:03 +0200

    fix jest tests

--- src/test_utils/public/stub_index_pattern.js
+++ src/test_utils/public/stub_index_pattern.js
@@ -21,18 +21,23 @@ import sinon from 'sinon';
 // TODO: We should not be importing from the data plugin directly here; this is only necessary
 // because it is one of the few places that we need to access the IndexPattern class itself, rather
 // than just the type. Doing this as a temporary measure; it will be left behind when migrating to NP.
+
 import {
   FieldList,
-} from 'ui/index_patterns';
-import {
   FIELD_FORMAT_IDS,
   IndexPattern,
   indexPatterns,
 } from '../../plugins/data/public';
 
+import { setFieldFormats } from '../../plugins/data/public/services';
+
+setFieldFormats({
+  getDefaultInstance: () => {},
+});
+
 import { getFieldFormatsRegistry } from './stub_field_formats';
 
-export default  function StubIndexPattern(pattern, getConfig, timeField, fields, uiSettings) {
+export default function StubIndexPattern(pattern, getConfig, timeField, fields, uiSettings) {
   const registeredFieldFormats = getFieldFormatsRegistry(uiSettings);
 
   this.id = pattern;
