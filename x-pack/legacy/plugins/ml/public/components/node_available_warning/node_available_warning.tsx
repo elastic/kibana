76662c1669c6d368e637730a73d9d828a9146684
James Gowdy <jgowdy@elastic.co>
2019-12-04 15:43:03 +0000

    fixing conflicts (#52179)

--- x-pack/legacy/plugins/ml/public/components/node_available_warning/node_available_warning.tsx
+++ x-pack/legacy/plugins/ml/public/components/node_available_warning/node_available_warning.tsx
@@ -8,8 +8,8 @@ import React, { Fragment, FC } from 'react';
 
 import { EuiCallOut, EuiLink, EuiSpacer } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { mlNodesAvailable, permissionToViewMlNodeCount } from '../../../../ml_nodes_check';
-import { getCloudDeploymentId, isCloud } from '../../../../services/ml_server_info';
+import { mlNodesAvailable, permissionToViewMlNodeCount } from '../../ml_nodes_check';
+import { getCloudDeploymentId, isCloud } from '../../services/ml_server_info';
 
 export const NodeAvailableWarning: FC = () => {
   if (mlNodesAvailable() === true || permissionToViewMlNodeCount() === false) {
