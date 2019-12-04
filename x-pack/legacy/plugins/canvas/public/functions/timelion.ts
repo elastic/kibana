0cfa9c98607c851a1d77e9132d822aad9d2979da
Joe Reuter <johannes.reuter@elastic.co>
2019-12-04 12:35:15 +0100

    respect timezone setting

--- x-pack/legacy/plugins/canvas/public/functions/timelion.ts
+++ x-pack/legacy/plugins/canvas/public/functions/timelion.ts
@@ -5,8 +5,10 @@
  */
 
 import { flatten } from 'lodash';
+import moment from 'moment-timezone';
 import chrome from 'ui/chrome';
 import { npStart } from 'ui/new_platform';
+import { TimeRange } from 'src/plugins/data/common';
 import { ExpressionFunction, DatatableRow } from 'src/plugins/expressions/public';
 import { fetch } from '../../common/lib/fetch';
 // @ts-ignore untyped local
@@ -20,6 +22,26 @@ interface Arguments {
   from: string;
   to: string;
   timezone: string;
+}
+
+/**
+ * This function parses a given time range containing date math
+ * and returns ISO dates. Parsing is done respecting the given time zone.
+ * @param timeRange time range to parse
+ * @param timeZone time zone to do the parsing in
+ */
+function parseDateMath(timeRange: TimeRange, timeZone: string) {
+  // the datemath plugin always parses dates by using the current default moment time zone.
+  // to use the configured time zone, we are switching just for the bounds calculation.
+  const defaultTimezone = moment().zoneName();
+  moment.tz.setDefault(timeZone);
+
+  const parsedRange = npStart.plugins.data.query.timefilter.timefilter.calculateBounds(timeRange);
+
+  // reset default moment timezone
+  moment.tz.setDefault(defaultTimezone);
+
+  return parsedRange;
 }
 
 export function timelion(): ExpressionFunction<'timelion', Filter, Arguments, Promise<Datatable>> {
@@ -66,10 +88,7 @@ export function timelion(): ExpressionFunction<'timelion', Filter, Arguments, Pr
       const timeFilter = context.and.find(and => and.type === 'time');
       const range = timeFilter
         ? { min: timeFilter.from, max: timeFilter.to }
-        : npStart.plugins.data.query.timefilter.timefilter.calculateBounds({
-            from: args.from,
-            to: args.to,
-          });
+        : parseDateMath({ from: args.from, to: args.to }, args.timezone);
 
       const body = {
         extended: {
