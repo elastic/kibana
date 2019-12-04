1240d02c9d72bbedbd8b24aefecf5a54772d109a
Cauê Marcondes <55978943+cauemarcondes@users.noreply.github.com>
2019-12-04 14:08:48 +0100

    [APM] Make it possible to link directly to a trace with just the trace.id (#51450)
    * create new api to fetch root transaction by trace id
    
    * redirecting trace to transaction
    
    * redirecting trace to transaction
    
    * redirecting trace to transaction
    
    * testing external link
    
    * testing external link
    
    * testing external link
    
    * testing external link
    
    * changing route name
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    * fixing merge conflicts
    
    * adding rangeFrom and to, into the url query param
    
    * removing convertedValue from duration formatter
    
    * refactoring ES query and tracelink component
    
    * pr comments
    
    * refactoring link

---
+++ x-pack/legacy/plugins/apm/public/components/shared/Links/apm/ExternalLinks.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import url from 'url';
+
+export const getTraceUrl = ({
+  traceId,
+  rangeFrom,
+  rangeTo
+}: {
+  traceId: string;
+  rangeFrom: string;
+  rangeTo: string;
+}) => {
+  return url.format({
+    pathname: `/link-to/trace/${traceId}`,
+    query: { rangeFrom, rangeTo }
+  });
+};
