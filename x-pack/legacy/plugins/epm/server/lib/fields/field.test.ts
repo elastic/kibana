2e230bf70186a8d24761adec92546a94626692b7
Nicolas Ruflin <spam@ruflin.com>
2019-12-05 14:16:59 +0100

    [EPM] Move golden files generation over to jest snapshot (#52203)
    * [EPM] Move golden files generation over to jest snapshot
    
    I initially used my own implementation to write the generated files. It runs out jest has a feature to write snapshots which simplifies the code a lot.
    
    I added a loop with an additional test file so in the future we can just keep adding test files without having to modify the test code.
    
    To updated the snapshots, the param `-u` has to be used:
    
    ```
    node scripts/jest legacy/plugins/epm/server/lib/fields/field.test.ts -u
    ```

--- x-pack/legacy/plugins/epm/server/lib/fields/field.test.ts
+++ x-pack/legacy/plugins/epm/server/lib/fields/field.test.ts
@@ -4,29 +4,32 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { readFileSync, writeFileSync } from 'fs';
-import { safeLoad } from 'js-yaml';
+import { readFileSync } from 'fs';
 import path from 'path';
+import { safeLoad } from 'js-yaml';
 import { Field, processFields } from './field';
+import glob from 'glob';
+
+// Add our own serialiser to just do JSON.stringify
+expect.addSnapshotSerializer({
+  print(val) {
+    return JSON.stringify(val, null, 2);
+  },
+
+  test(val) {
+    return val;
+  },
+});
 
 test('tests loading fields.yml', () => {
-  const fieldsYML = readFileSync(path.join(__dirname, '/tests/base.yml'), 'utf-8');
+  // Find all .yml files to run tests on
+  const files = glob.sync(path.join(__dirname, '/tests/*.yml'));
+  for (const file of files) {
+    const fieldsYML = readFileSync(file, 'utf-8');
+    const fields: Field[] = safeLoad(fieldsYML);
+    processFields(fields);
 
-  const fields: Field[] = safeLoad(fieldsYML);
-
-  processFields(fields);
-
-  // Convert it json for easier comparison of the output
-  const json = JSON.stringify(fields, null, 2);
-  const generatedFile = path.join(__dirname, './tests/base.fields.generate.json');
-
-  // Regenerate the file if `-generate` flag is used
-  if (process.argv.includes('-generate')) {
-    writeFileSync(generatedFile, json);
+    // Check that content file and generated file are equal
+    expect(fields).toMatchSnapshot(path.basename(file));
   }
-
-  const jsonData = readFileSync(generatedFile, 'utf-8');
-
-  // Check that content file and generated file are equal
-  expect(jsonData).toBe(json);
 });
