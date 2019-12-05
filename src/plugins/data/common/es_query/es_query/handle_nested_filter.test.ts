af4e53e1f8187b24444bfbcd5530597d428bb421
Matthew Bargar <mbargar@gmail.com>
2019-12-05 16:19:26 -0500

    Convert test from pre-merge to jest

--- src/plugins/data/common/es_query/es_query/handle_nested_filter.test.ts
+++ src/plugins/data/common/es_query/es_query/handle_nested_filter.test.ts
@@ -17,67 +17,75 @@
  * under the License.
  */
 
+import { handleNestedFilter } from './handle_nested_filter';
+import { fields } from '../../index_patterns/mocks';
+import { buildPhraseFilter, buildQueryFilter } from '../filters';
+import { IFieldType, IIndexPattern } from '../../index_patterns';
 
-import { handleNestedFilter } from '../handle_nested_filter';
-import {  indexPatternResponse as indexPattern  } from '../../__fixtures__/index_pattern_response';
-import { buildPhraseFilter, buildQueryFilter } from '../../filters';
-import expect from '@kbn/expect';
+describe('handleNestedFilter', function() {
+  const indexPattern: IIndexPattern = ({
+    id: 'logstash-*',
+    fields,
+  } as unknown) as IIndexPattern;
 
-describe('handleNestedFilter', function () {
-
-  it('should return the filter\'s query wrapped in nested query if the target field is nested', () => {
+  it("should return the filter's query wrapped in nested query if the target field is nested", () => {
     const field = getField('nestedField.child');
-    const filter = buildPhraseFilter(field, 'foo', indexPattern);
+    const filter = buildPhraseFilter(field!, 'foo', indexPattern);
     const result = handleNestedFilter(filter, indexPattern);
-    expect(result).to.eql({
+    expect(result).toEqual({
+      meta: {
+        index: 'logstash-*',
+      },
       nested: {
         path: 'nestedField',
         query: {
           match_phrase: {
-            'nestedField.child': 'foo'
-          }
-        }
-      }
-    }
-    );
+            'nestedField.child': 'foo',
+          },
+        },
+      },
+    });
   });
 
   it('should return filter untouched if it does not target a nested field', () => {
     const field = getField('extension');
-    const filter = buildPhraseFilter(field, 'jpg', indexPattern);
+    const filter = buildPhraseFilter(field!, 'jpg', indexPattern);
     const result = handleNestedFilter(filter, indexPattern);
-    expect(result).to.be(filter);
+    expect(result).toBe(filter);
   });
 
   it('should return filter untouched if it does not target a field from the given index pattern', () => {
     const field = { ...getField('extension'), name: 'notarealfield' };
-    const filter = buildPhraseFilter(field, 'jpg', indexPattern);
+    const filter = buildPhraseFilter(field as IFieldType, 'jpg', indexPattern);
     const result = handleNestedFilter(filter, indexPattern);
-    expect(result).to.be(filter);
+    expect(result).toBe(filter);
   });
 
   it('should return filter untouched if no index pattern is provided', () => {
     const field = getField('extension');
-    const filter = buildPhraseFilter(field, 'jpg', indexPattern);
+    const filter = buildPhraseFilter(field!, 'jpg', indexPattern);
     const result = handleNestedFilter(filter);
-    expect(result).to.be(filter);
+    expect(result).toBe(filter);
   });
 
   it('should return the filter untouched if a target field cannot be determined', () => {
     // for example, we don't support query_string queries
-    const filter = buildQueryFilter({
-      query: {
-        query_string: {
-          query: 'response:200'
-        }
-      }
-    }, indexPattern.id);
+    const filter = buildQueryFilter(
+      {
+        query: {
+          query_string: {
+            query: 'response:200',
+          },
+        },
+      },
+      'logstash-*',
+      'foo'
+    );
     const result = handleNestedFilter(filter);
-    expect(result).to.be(filter);
+    expect(result).toBe(filter);
   });
+
+  function getField(name: string) {
+    return indexPattern.fields.find(field => field.name === name);
+  }
 });
-
-
-function getField(name) {
-  return indexPattern.fields.find(field => field.name === name);
-}
