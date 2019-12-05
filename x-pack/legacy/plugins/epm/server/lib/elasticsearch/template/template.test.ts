22bda3af6640ea34ebb29070c03fda6f301ffb9c
ruflin <spam@ruflin.com>
2019-12-05 10:44:31 +0100

    fix import

--- x-pack/legacy/plugins/epm/server/lib/elasticsearch/template/template.test.ts
+++ x-pack/legacy/plugins/epm/server/lib/elasticsearch/template/template.test.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { getTemplate, generateMappings } from './template';
 import { readFileSync } from 'fs';
 import path from 'path';
 import { safeLoad } from 'js-yaml';
