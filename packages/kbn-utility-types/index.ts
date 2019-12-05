8d0d09c736cd04914ad7d64e66fee895de534938
nickofthyme <nick.ryan.partridge@gmail.com>
2019-12-05 14:14:28 -0600

    Add Required, Optional to kbn-utility-types

--- packages/kbn-utility-types/index.ts
+++ packages/kbn-utility-types/index.ts
@@ -18,7 +18,7 @@
  */
 
 import { PromiseType } from 'utility-types';
-export { $Values } from 'utility-types';
+export { $Values, Required, Optional } from 'utility-types';
 
 /**
  * Returns wrapped type of a promise.
