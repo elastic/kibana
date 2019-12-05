c6388d37a0c7b3468e3efb1fa9a896484894d167
Alison Goryachev <alisonmllr20@gmail.com>
2019-12-05 15:12:04 -0500

    add unit test for deNormalize()

--- x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/lib/utils.test.ts
+++ x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/lib/utils.test.ts
@@ -6,9 +6,76 @@
 
 jest.mock('../constants', () => ({ MAIN_DATA_TYPE_DEFINITION: {} }));
 
-import { isStateValid } from './utils';
+import uuid from 'uuid';
+import { isStateValid, deNormalize } from './utils';
+import { NormalizedFields, NormalizedField } from '../types';
+
+const createField = ({
+  id,
+  nestedDepth = 0,
+  path,
+  source,
+  isMultiField = false,
+  canHaveChildFields = false,
+}: NormalizedField) => {
+  return {
+    id,
+    nestedDepth,
+    path,
+    source,
+    isMultiField,
+    canHaveChildFields,
+  };
+};
 
 describe('utils', () => {
+  describe('deNormalize()', () => {
+    const rootField1Id = uuid.v4();
+    const fieldName = 'my_field';
+    const source = {
+      doc_values: true,
+      index: true,
+      name: fieldName,
+      null_value: 'true',
+      store: false,
+      type: 'boolean',
+    };
+    const fieldsState = {
+      rootLevelFields: [rootField1Id],
+      byId: {
+        [rootField1Id]: {
+          id: rootField1Id,
+          nestedDepth: 0,
+          path: fieldName,
+          source,
+          isMultiField: false,
+          canHaveChildFields: false,
+        },
+      },
+      aliases: {},
+    } as NormalizedFields;
+
+    it('handles base case', () => {
+      const { name, ...result } = source;
+      expect(deNormalize(fieldsState)).toEqual({
+        [fieldName]: result,
+      });
+    });
+
+    // it('handles alias field', () => {
+    //   const { name, ...result } = source;
+    //   expect(deNormalize(fieldsState)).toEqual({
+    //     [fieldName]: result,
+    //   });
+    // });
+
+    // it('handles child fields', () => {
+    //   const { name, ...result } = source;
+    //   expect(deNormalize(fieldsState)).toEqual({
+    //     [fieldName]: result,
+    //   });
+    // });
+  });
   describe('isStateValid()', () => {
     let components: any;
     it('handles base case', () => {
