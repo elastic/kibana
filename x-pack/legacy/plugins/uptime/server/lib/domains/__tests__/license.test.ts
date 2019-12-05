296c7b322af67be454478b23e51e7e603503ef85
Justin Kambic <justin.kambic@elastic.co>
2019-12-05 13:28:54 -0500

    Remove usages of Boom.

--- x-pack/legacy/plugins/uptime/server/lib/domains/__tests__/license.test.ts
+++ x-pack/legacy/plugins/uptime/server/lib/domains/__tests__/license.test.ts
@@ -4,38 +4,37 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { ILicense } from '../../../../../../../plugins/licensing/server';
 import { licenseCheck } from '../license';
-import { RequestHandlerContext } from 'kibana/server';
 
 describe('license check', () => {
-  let mockContext: RequestHandlerContext;
-  const isOneOf = jest.fn();
+  let mockLicense: Pick<ILicense, 'isActive' | 'isOneOf'>;
 
   it('throws for null license', () => {
-    // @ts-ignore there's a null check in this function
-    mockContext = { licensing: { license: null } };
-    expect(() => licenseCheck(mockContext)).toThrowErrorMatchingSnapshot();
+    expect(licenseCheck(null)).toMatchSnapshot();
   });
 
   it('throws for unsupported license type', () => {
-    isOneOf.mockReturnValue(false);
-    expect(() =>
-      // @ts-ignore it needs to throw if the license is not supported
-      licenseCheck({ licensing: { license: { isOneOf } } })
-    ).toThrowErrorMatchingSnapshot();
+    mockLicense = {
+      isOneOf: jest.fn().mockReturnValue(false),
+      isActive: false,
+    };
+    expect(licenseCheck(mockLicense)).toMatchSnapshot();
   });
 
   it('throws for inactive license', () => {
-    isOneOf.mockReturnValue(true);
-    expect(() =>
-      // @ts-ignore it needs to throw if !isActive
-      licenseCheck({ licensing: { license: { isActive: false, isOneOf } } })
-    ).toThrowErrorMatchingSnapshot();
+    mockLicense = {
+      isOneOf: jest.fn().mockReturnValue(true),
+      isActive: false,
+    };
+    expect(licenseCheck(mockLicense)).toMatchSnapshot();
   });
 
-  it('returns true for a valid license', () => {
-    isOneOf.mockReturnValue(true);
-    // @ts-ignore license type needs to be non-null
-    expect(licenseCheck({ licensing: { license: { isActive: true, isOneOf } } })).toBe(true);
+  it('returns result for a valid license', () => {
+    mockLicense = {
+      isOneOf: jest.fn().mockReturnValue(true),
+      isActive: true,
+    };
+    expect(licenseCheck(mockLicense)).toMatchSnapshot();
   });
 });
--- x-pack/legacy/plugins/uptime/server/lib/domains/license.ts
+++ x-pack/legacy/plugins/uptime/server/lib/domains/license.ts
@@ -4,20 +4,36 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import Boom from 'boom';
-import { RequestHandlerContext } from 'kibana/server';
+import { ILicense } from '../../../../../../plugins/licensing/server';
 
-export type UMLicenseCheck = (context: RequestHandlerContext) => boolean;
+export interface UMLicenseStatusResponse {
+  statusCode: number;
+  message?: string;
+}
+export type UMLicenseCheck = (
+  license: Pick<ILicense, 'isActive' | 'isOneOf'> | null
+) => UMLicenseStatusResponse;
 
-export const licenseCheck: UMLicenseCheck = ({ licensing: { license } }) => {
+export const licenseCheck: UMLicenseCheck = license => {
   if (license === null) {
-    throw Boom.badRequest('Missing license information');
+    return {
+      message: 'Missing license information',
+      statusCode: 400,
+    };
   }
   if (!license.isOneOf(['basic', 'standard', 'gold', 'platinum', 'trial'])) {
-    throw Boom.forbidden('License not supported');
+    return {
+      message: 'License not supported',
+      statusCode: 401,
+    };
   }
   if (license.isActive === false) {
-    throw Boom.forbidden('License not active');
+    return {
+      message: 'License not active',
+      statusCode: 403,
+    };
   }
-  return true;
+  return {
+    statusCode: 200,
+  };
 };
