0ec9a4cb62e30fb0f8d5572349824852f9d43369
FrankHassanabad <frank.hassanabad@elastic.co>
2019-12-05 22:04:53 -0700

    Small change for users to get back the index name if it exists

--- x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/index/read_index_route.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/index/read_index_route.ts
@@ -5,11 +5,11 @@
  */
 
 import Hapi from 'hapi';
+import Boom from 'boom';
 
 import { DETECTION_ENGINE_INDEX_URL } from '../../../../../common/constants';
 import { ServerFacade, RequestFacade } from '../../../../types';
 import { transformError, getIndex, callWithRequestFactory } from '../utils';
-import { readIndex } from '../../index/read_index';
 import { getIndexExists } from '../../index/get_index_exists';
 
 export const createReadIndexRoute = (server: ServerFacade): Hapi.ServerRoute => {
@@ -28,19 +28,22 @@ export const createReadIndexRoute = (server: ServerFacade): Hapi.ServerRoute => 
       try {
         const index = getIndex(request, server);
         const callWithRequest = callWithRequestFactory(request);
-        // head request is used for if you want to get if the index exists
-        // or not and it will return a content-length: 0 along with either a 200 or 404
-        // depending on if the index exists or not.
-        if (request.method.toLowerCase() === 'head') {
-          const indexExists = await getIndexExists(callWithRequest, index);
-          if (indexExists) {
+        const indexExists = await getIndexExists(callWithRequest, index);
+        if (indexExists) {
+          // head request is used for if you want to get if the index exists
+          // or not and it will return a content-length: 0 along with either a 200 or 404
+          // depending on if the index exists or not.
+          if (request.method.toLowerCase() === 'head') {
             return headers.response().code(200);
           } else {
-            return headers.response().code(404);
+            return headers.response({ name: index }).code(200);
           }
         } else {
-          const indexSettings = await readIndex(callWithRequest, index);
-          return indexSettings;
+          if (request.method.toLowerCase() === 'head') {
+            return headers.response().code(404);
+          } else {
+            return new Boom('An index for this space does not exist', { statusCode: 404 });
+          }
         }
       } catch (err) {
         return transformError(err);
