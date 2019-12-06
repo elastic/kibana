c29d98caf3b03ab297ad09d646fab32571671624
bhavyarm <bhavya@elastic.co>
2019-12-06 10:04:50 -0500

    console changes

--- src/legacy/core_plugins/console/np_ready/public/application/containers/editor/legacy/console_editor/editor.tsx
+++ src/legacy/core_plugins/console/np_ready/public/application/containers/editor/legacy/console_editor/editor.tsx
@@ -172,6 +172,9 @@ function EditorUI({ previousStateLocation = 'stored' }: EditorProps) {
               <button
                 onClick={sendCurrentRequestToES}
                 data-test-subj="sendRequestButton"
+                aria-label={i18n.translate('console.sendRequestButtonTooltip', {
+                  defaultMessage: 'click to send request',
+                })}
                 className="conApp__editorActionButton conApp__editorActionButton--success"
               >
                 <EuiIcon type="play" />
--- test/accessibility/config.ts
+++ test/accessibility/config.ts
@@ -28,13 +28,12 @@ export default async function({ readConfigFile }: FtrConfigProviderContext) {
     ...functionalConfig.getAll(),
 
     testFiles: [
-      // require.resolve('./apps/discover'),
-      // require.resolve('./apps/dashboard'),
+      require.resolve('./apps/discover'),
+      require.resolve('./apps/dashboard'),
       require.resolve('./apps/visualize'),
-      // require.resolve('./apps/management'),
-      // require.resolve('./apps/console'),
-
-      // require.resolve('./apps/home'),
+      require.resolve('./apps/management'),
+      require.resolve('./apps/console'),
+      require.resolve('./apps/home'),
     ],
     pageObjects,
     services,
