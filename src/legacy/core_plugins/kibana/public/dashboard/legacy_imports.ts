elastic/kibana

commit 217608d11e2196a9e384f9ef679af7800e3a5969

[State Management] Typescripify, jestify, simplify state_hashing and state_storage (#51835)
The hashUrl and unhashUrl functions no longer rely on states being provided as an argument, therefore getUnhashableStates/getUnhashableStatesProvider have been removed.

src/legacy/core_plugins/kibana/public/discover/kibana_services.ts
@@ -74,8 +74,6 @@ export { subscribeWithScope } from 'ui/utils/subscribe_with_scope';
 // @ts-ignore
 export { timezoneProvider } from 'ui/vis/lib/timezone';
 // @ts-ignore
-export { getUnhashableStatesProvider } from 'ui/state_management/state_hashing';
-// @ts-ignore
 export { tabifyAggResponse } from 'ui/agg_response/tabify';
 // @ts-ignore
 export { vislibSeriesResponseHandlerProvider } from 'ui/vis/response_handlers/vislib';

src/legacy/core_plugins/kibana/public/visualize/editor/editor.js
@@ -39,7 +39,6 @@ import {
   getServices,
   angular,
   absoluteToParsedUrl,
-  getUnhashableStatesProvider,
   KibanaParsedUrl,
   migrateLegacyQuery,
   SavedObjectSaveModal,
@@ -166,7 +165,6 @@ function VisEditor(
   localStorage,
 ) {
   const queryFilter = Private(FilterBarQueryFilterProvider);
-  const getUnhashableStates = Private(getUnhashableStatesProvider);
 
   // Retrieve the resolved SavedVis instance.
   const savedVis = $route.current.locals.savedVis;
@@ -250,7 +248,7 @@ function VisEditor(
         anchorElement,
         allowEmbed: true,
         allowShortUrl: capabilities.visualize.createShortUrl,
-        shareableUrl: unhashUrl(window.location.href, getUnhashableStates()),
+        shareableUrl: unhashUrl(window.location.href),
         objectId: savedVis.id,
         objectType: 'visualization',
         sharingData: {

src/legacy/core_plugins/kibana/public/visualize/kibana_services.ts
@@ -96,8 +96,6 @@ export { getFromSavedObject } from 'ui/index_patterns';
 export { PersistedState } from 'ui/persisted_state';
 // @ts-ignore
 export { VisEditorTypesRegistryProvider } from 'ui/registry/vis_editor_types';
-// @ts-ignore
-export { getUnhashableStatesProvider } from 'ui/state_management/state_hashing';
 export { showSaveModal } from 'ui/saved_objects/show_saved_object_save_modal';
 export { stateMonitorFactory } from 'ui/state_management/state_monitor_factory';
 export { absoluteToParsedUrl } from 'ui/url/absolute_to_parsed_url';

src/legacy/ui/public/chrome/api/sub_url_hooks.js
@@ -20,18 +20,16 @@
 import url from 'url';
 
 import {
-  getUnhashableStatesProvider,
   unhashUrl,
 } from '../../state_management/state_hashing';
 
 export function registerSubUrlHooks(angularModule, internals) {
   angularModule.run(($rootScope, Private, $location) => {
-    const getUnhashableStates = Private(getUnhashableStatesProvider);
     const subUrlRouteFilter = Private(SubUrlRouteFilterProvider);
 
     function updateSubUrls() {
       const urlWithHashes = window.location.href;
-      const urlWithStates = unhashUrl(urlWithHashes, getUnhashableStates());
+      const urlWithStates = unhashUrl(urlWithHashes);
       internals.trackPossibleSubUrl(urlWithStates);
     }
 

src/legacy/ui/public/state_management/state.js
@@ -37,10 +37,12 @@ import { createLegacyClass } from '../utils/legacy_class';
 import { callEach } from '../utils/function';
 
 import {
-  createStateHash,
   HashedItemStoreSingleton,
-  isStateHash,
 } from './state_storage';
+import {
+  createStateHash,
+  isStateHash
+} from './state_hashing';
 
 export function StateProvider(Private, $rootScope, $location, stateManagementConfig, config, kbnUrl, $injector) {
   const Events = Private(EventsProvider);
@@ -293,9 +295,7 @@ export function StateProvider(Private, $rootScope, $location, stateManagementCon
 
     // We need to strip out Angular-specific properties.
     const json = angular.toJson(state);
-    const hash = createStateHash(json, hash => {
-      return this._hashedItemStore.getItem(hash);
-    });
+    const hash = createStateHash(json);
     const isItemSet = this._hashedItemStore.setItem(hash, json);
 
     if (isItemSet) {

src/legacy/ui/public/state_management/__tests__/state.js
@@ -26,12 +26,10 @@ import { toastNotifications } from '../../notify';
 import * as FatalErrorNS from '../../notify/fatal_error';
 import { StateProvider } from '../state';
 import {
-  unhashQueryString,
-} from '../state_hashing';
-import {
   createStateHash,
   isStateHash,
-} from '../state_storage';
+  unhashQuery
+} from '../state_hashing';
 import { HashedItemStore } from '../state_storage/hashed_item_store';
 import { StubBrowserStorage } from 'test_utils/stub_browser_storage';
 import { EventsProvider } from '../../events';
@@ -60,9 +58,7 @@ describe('State Management', () => {
         const hashedItemStore = new HashedItemStore(store);
         const state = new State(param, initial, hashedItemStore);
 
-        const getUnhashedSearch = state => {
-          return unhashQueryString($location.search(), [ state ]);
-        };
+        const getUnhashedSearch = () => unhashQuery($location.search());
 
         return { store, hashedItemStore, state, getUnhashedSearch };
       };

src/legacy/ui/public/state_management/state_hashing/hash_unhash_url.test.ts
@@ -1,0 +1,286 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { mockSessionStorage } from '../state_storage/mock';
+import { HashedItemStore } from '../state_storage/hashed_item_store';
+import { hashUrl, unhashUrl } from './hash_unhash_url';
+
+describe('hash unhash url', () => {
+  beforeEach(() => {
+    mockSessionStorage.clear();
+    mockSessionStorage.setStubbedSizeLimit(5000000);
+  });
+
+  describe('hash url', () => {
+    describe('does nothing', () => {
+      it('if missing input', () => {
+        expect(() => {
+          // @ts-ignore
+          hashUrl();
+        }).not.toThrowError();
+      });
+
+      it('if url is empty', () => {
+        const url = '';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if just a host and port', () => {
+        const url = 'https://localhost:5601';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if just a path', () => {
+        const url = 'https://localhost:5601/app/kibana';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if just a path and query', () => {
+        const url = 'https://localhost:5601/app/kibana?foo=bar';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if empty hash with query', () => {
+        const url = 'https://localhost:5601/app/kibana?foo=bar#';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if query parameter matches and there is no hash', () => {
+        const url = 'https://localhost:5601/app/kibana?testParam=(yes:!t)';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it(`if query parameter matches and it's before the hash`, () => {
+        const url = 'https://localhost:5601/app/kibana?testParam=(yes:!t)';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if empty hash without query', () => {
+        const url = 'https://localhost:5601/app/kibana#';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if hash is just a path', () => {
+        const url = 'https://localhost:5601/app/kibana#/discover';
+        expect(hashUrl(url)).toBe(url);
+      });
+
+      it('if hash does not have matching query string vals', () => {
+        const url = 'https://localhost:5601/app/kibana#/discover?foo=bar';
+        expect(hashUrl(url)).toBe(url);
+      });
+    });
+
+    describe('replaces expanded state with hash', () => {
+      it('if uses single state param', () => {
+        const stateParamKey = '_g';
+        const stateParamValue = '(yes:!t)';
+        const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey}=${stateParamValue}`;
+        const result = hashUrl(url);
+        expect(result).toMatchInlineSnapshot(
+          `"https://localhost:5601/app/kibana#/discover?foo=bar&_g=h@4e60e02"`
+        );
+        expect(mockSessionStorage.getItem('kbn.hashedItemsIndex.v1')).toBeTruthy();
+        expect(mockSessionStorage.getItem('h@4e60e02')).toEqual(JSON.stringify({ yes: true }));
+      });
+
+      it('if uses multiple states params', () => {
+        const stateParamKey1 = '_g';
+        const stateParamValue1 = '(yes:!t)';
+        const stateParamKey2 = '_a';
+        const stateParamValue2 = '(yes:!f)';
+        const stateParamKey3 = '_b';
+        const stateParamValue3 = '(yes:!f)';
+        const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey1}=${stateParamValue1}&${stateParamKey2}=${stateParamValue2}&${stateParamKey3}=${stateParamValue3}`;
+        const result = hashUrl(url);
+        expect(result).toMatchInlineSnapshot(
+          `"https://localhost:5601/app/kibana#/discover?foo=bar&_g=h@4e60e02&_a=h@61fa078&_b=(yes:!f)"`
+        );
+        expect(mockSessionStorage.getItem('h@4e60e02')).toEqual(JSON.stringify({ yes: true }));
+        expect(mockSessionStorage.getItem('h@61fa078')).toEqual(JSON.stringify({ yes: false }));
+        if (!HashedItemStore.PERSISTED_INDEX_KEY) {
+          // This is very brittle and depends upon HashedItemStore implementation details,
+          // so let's protect ourselves from accidentally breaking this test.
+          throw new Error('Missing HashedItemStore.PERSISTED_INDEX_KEY');
+        }
+        expect(mockSessionStorage.getItem(HashedItemStore.PERSISTED_INDEX_KEY)).toBeTruthy();
+        expect(mockSessionStorage.length).toBe(3);
+      });
+
+      it('hashes only whitelisted properties', () => {
+        const stateParamKey1 = '_g';
+        const stateParamValue1 = '(yes:!t)';
+        const stateParamKey2 = '_a';
+        const stateParamValue2 = '(yes:!f)';
+        const stateParamKey3 = '_someother';
+        const stateParamValue3 = '(yes:!f)';
+        const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey1}=${stateParamValue1}&${stateParamKey2}=${stateParamValue2}&${stateParamKey3}=${stateParamValue3}`;
+        const result = hashUrl(url);
+        expect(result).toMatchInlineSnapshot(
+          `"https://localhost:5601/app/kibana#/discover?foo=bar&_g=h@4e60e02&_a=h@61fa078&_someother=(yes:!f)"`
+        );
+
+        expect(mockSessionStorage.length).toBe(3); // 2 hashes + HashedItemStoreSingleton.PERSISTED_INDEX_KEY
+      });
+    });
+
+    it('throws error if unable to hash url', () => {
+      const stateParamKey1 = '_g';
+      const stateParamValue1 = '(yes:!t)';
+      mockSessionStorage.setStubbedSizeLimit(1);
+
+      const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey1}=${stateParamValue1}`;
+      expect(() => hashUrl(url)).toThrowError();
+    });
+  });
+
+  describe('unhash url', () => {
+    describe('does nothing', () => {
+      it('if missing input', () => {
+        expect(() => {
+          // @ts-ignore
+        }).not.toThrowError();
+      });
+
+      it('if just a host and port', () => {
+        const url = 'https://localhost:5601';
+        expect(unhashUrl(url)).toBe(url);
+      });
+
+      it('if just a path', () => {
+        const url = 'https://localhost:5601/app/kibana';
+        expect(unhashUrl(url)).toBe(url);
+      });
+
+      it('if just a path and query', () => {
+        const url = 'https://localhost:5601/app/kibana?foo=bar';
+        expect(unhashUrl(url)).toBe(url);
+      });
+
+      it('if empty hash with query', () => {
+        const url = 'https://localhost:5601/app/kibana?foo=bar#';
+        expect(unhashUrl(url)).toBe(url);
+      });
+
+      it('if empty hash without query', () => {
+        const url = 'https://localhost:5601/app/kibana#';
+        expect(unhashUrl(url)).toBe(url);
+      });
+
+      it('if hash is just a path', () => {
+        const url = 'https://localhost:5601/app/kibana#/discover';
+        expect(unhashUrl(url)).toBe(url);
+      });
+
+      it('if hash does not have matching query string vals', () => {
+        const url = 'https://localhost:5601/app/kibana#/discover?foo=bar';
+        expect(unhashUrl(url)).toBe(url);
+      });
+
+      it("if hash has matching query, but it isn't hashed", () => {
+        const stateParamKey = '_g';
+        const stateParamValue = '(yes:!t)';
+        const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey}=${stateParamValue}`;
+        expect(unhashUrl(url)).toBe(url);
+      });
+    });
+
+    describe('replaces expanded state with hash', () => {
+      it('if uses single state param', () => {
+        const stateParamKey = '_g';
+        const stateParamValueHashed = 'h@4e60e02';
+        const state = { yes: true };
+        mockSessionStorage.setItem(stateParamValueHashed, JSON.stringify(state));
+
+        const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey}=${stateParamValueHashed}`;
+        const result = unhashUrl(url);
+        expect(result).toMatchInlineSnapshot(
+          `"https://localhost:5601/app/kibana#/discover?foo=bar&_g=(yes:!t)"`
+        );
+      });
+
+      it('if uses multiple state param', () => {
+        const stateParamKey1 = '_g';
+        const stateParamValueHashed1 = 'h@4e60e02';
+        const state1 = { yes: true };
+
+        const stateParamKey2 = '_a';
+        const stateParamValueHashed2 = 'h@61fa078';
+        const state2 = { yes: false };
+
+        mockSessionStorage.setItem(stateParamValueHashed1, JSON.stringify(state1));
+        mockSessionStorage.setItem(stateParamValueHashed2, JSON.stringify(state2));
+
+        const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey1}=${stateParamValueHashed1}&${stateParamKey2}=${stateParamValueHashed2}`;
+        const result = unhashUrl(url);
+        expect(result).toMatchInlineSnapshot(
+          `"https://localhost:5601/app/kibana#/discover?foo=bar&_g=(yes:!t)&_a=(yes:!f)"`
+        );
+      });
+
+      it('unhashes only whitelisted properties', () => {
+        const stateParamKey1 = '_g';
+        const stateParamValueHashed1 = 'h@4e60e02';
+        const state1 = { yes: true };
+
+        const stateParamKey2 = '_a';
+        const stateParamValueHashed2 = 'h@61fa078';
+        const state2 = { yes: false };
+
+        const stateParamKey3 = '_someother';
+        const stateParamValueHashed3 = 'h@61fa078';
+        const state3 = { yes: false };
+
+        mockSessionStorage.setItem(stateParamValueHashed1, JSON.stringify(state1));
+        mockSessionStorage.setItem(stateParamValueHashed2, JSON.stringify(state2));
+        mockSessionStorage.setItem(stateParamValueHashed3, JSON.stringify(state3));
+
+        const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey1}=${stateParamValueHashed1}&${stateParamKey2}=${stateParamValueHashed2}&${stateParamKey3}=${stateParamValueHashed3}`;
+        const result = unhashUrl(url);
+        expect(result).toMatchInlineSnapshot(
+          `"https://localhost:5601/app/kibana#/discover?foo=bar&_g=(yes:!t)&_a=(yes:!f)&_someother=h@61fa078"`
+        );
+      });
+    });
+
+    it('throws error if unable to restore the url', () => {
+      const stateParamKey1 = '_g';
+      const stateParamValueHashed1 = 'h@4e60e02';
+
+      const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey1}=${stateParamValueHashed1}`;
+      expect(() => unhashUrl(url)).toThrowErrorMatchingInlineSnapshot(
+        `"Unable to completely restore the URL, be sure to use the share functionality."`
+      );
+    });
+  });
+
+  describe('hash unhash url integration', () => {
+    it('hashing and unhashing url should produce the same result', () => {
+      const stateParamKey1 = '_g';
+      const stateParamValue1 = '(yes:!t)';
+      const stateParamKey2 = '_a';
+      const stateParamValue2 = '(yes:!f)';
+      const stateParamKey3 = '_someother';
+      const stateParamValue3 = '(yes:!f)';
+      const url = `https://localhost:5601/app/kibana#/discover?foo=bar&${stateParamKey1}=${stateParamValue1}&${stateParamKey2}=${stateParamValue2}&${stateParamKey3}=${stateParamValue3}`;
+      const result = unhashUrl(hashUrl(url));
+      expect(url).toEqual(result);
+    });
+  });
+});

commit 881c836f9489a122cb1d1e2e0d32b4d9c2289f91

[State Management] Move url state_hashing utils to kibana_utils (#52280)
Part of #44151,
Continuation of #51835,
Just moves existing state related url utils to kibana_utils plugin

Also fixes small regression introduced in #51835,
When sharing hashed url directly it should show error toast instead of full page fatal error

src/legacy/core_plugins/kibana/public/dashboard/legacy_imports.ts
@@ -63,6 +63,6 @@ export { confirmModalFactory } from 'ui/modals/confirm_modal';
 export { configureAppAngularModule } from 'ui/legacy_compat';
 export { stateMonitorFactory, StateMonitor } from 'ui/state_management/state_monitor_factory';
 export { ensureDefaultIndexPattern } from 'ui/legacy_compat';
-export { unhashUrl } from 'ui/state_management/state_hashing';
+export { unhashUrl } from '../../../../../plugins/kibana_utils/public';
 export { IInjector } from 'ui/chrome';
 export { SavedObjectFinder } from 'ui/saved_objects/components/saved_object_finder';

src/legacy/core_plugins/kibana/public/discover/kibana_services.ts
@@ -78,7 +78,7 @@ export { tabifyAggResponse } from 'ui/agg_response/tabify';
 // @ts-ignore
 export { vislibSeriesResponseHandlerProvider } from 'ui/vis/response_handlers/vislib';
 export { ensureDefaultIndexPattern } from 'ui/legacy_compat';
-export { unhashUrl } from 'ui/state_management/state_hashing';
+export { unhashUrl } from '../../../../../plugins/kibana_utils/public';
 
 // EXPORT types
 export { Vis } from 'ui/vis';

src/legacy/core_plugins/kibana/public/visualize/editor/editor.js
@@ -45,7 +45,7 @@ import {
   showSaveModal,
   stateMonitorFactory,
   subscribeWithScope,
-  unhashUrl,
+  unhashUrl
 } from '../kibana_services';
 
 const {

src/legacy/core_plugins/kibana/public/visualize/kibana_services.ts
@@ -103,7 +103,7 @@ export { KibanaParsedUrl } from 'ui/url/kibana_parsed_url';
 export { migrateLegacyQuery } from 'ui/utils/migrate_legacy_query';
 export { subscribeWithScope } from 'ui/utils/subscribe_with_scope';
 export { SavedObjectSaveModal } from 'ui/saved_objects/components/saved_object_save_modal';
-export { unhashUrl } from 'ui/state_management/state_hashing';
+export { unhashUrl } from '../../../../../plugins/kibana_utils/public';
 export {
   Container,
   Embeddable,

src/legacy/ui/public/chrome/api/sub_url_hooks.js
@@ -19,9 +19,8 @@
 
 import url from 'url';
 
-import {
-  unhashUrl,
-} from '../../state_management/state_hashing';
+import { unhashUrl } from '../../../../../plugins/kibana_utils/public';
+import { toastNotifications } from '../../notify/toasts';
 
 export function registerSubUrlHooks(angularModule, internals) {
   angularModule.run(($rootScope, Private, $location) => {
@@ -29,8 +28,14 @@ export function registerSubUrlHooks(angularModule, internals) {
 
     function updateSubUrls() {
       const urlWithHashes = window.location.href;
-      const urlWithStates = unhashUrl(urlWithHashes);
-      internals.trackPossibleSubUrl(urlWithStates);
+      let urlWithStates;
+      try {
+        urlWithStates = unhashUrl(urlWithHashes);
+      } catch (e) {
+        toastNotifications.addDanger(e.message);
+      }
+
+      internals.trackPossibleSubUrl(urlWithStates || urlWithHashes);
     }
 
     function onRouteChange($event) {

src/legacy/ui/public/state_management/state.js
@@ -35,11 +35,7 @@ import { fatalError, toastNotifications } from '../notify';
 import './config_provider';
 import { createLegacyClass } from '../utils/legacy_class';
 import { callEach } from '../utils/function';
-import { hashedItemStore } from '../../../../plugins/kibana_utils/public';
-import {
-  createStateHash,
-  isStateHash
-} from './state_hashing';
+import { hashedItemStore, isStateHash, createStateHash } from '../../../../plugins/kibana_utils/public';
 
 export function StateProvider(Private, $rootScope, $location, stateManagementConfig, config, kbnUrl, $injector) {
   const Events = Private(EventsProvider);

src/plugins/kibana_utils/public/index.ts
@@ -28,3 +28,5 @@ export * from './errors';
 export * from './field_mapping';
 export * from './storage';
 export * from './storage/hashed_item_store';
+export * from './state_management/state_hash';
+export * from './state_management/url';

src/plugins/kibana_utils/public/state_management/state_hash/index.ts
@@ -17,5 +17,4 @@
  * under the License.
  */
 
-export { hashUrl, unhashUrl, hashQuery, unhashQuery } from './hash_unhash_url';
-export { createStateHash, isStateHash } from './state_hash';
+export * from './state_hash';

src/plugins/kibana_utils/public/state_management/state_hash/state_hash.ts
@@ -18,7 +18,7 @@
  */
 
 import { Sha256 } from '../../../../../core/public/utils';
-import { hashedItemStore } from '../../../../../plugins/kibana_utils/public';
+import { hashedItemStore } from '../../storage/hashed_item_store';
 
 // This prefix is used to identify hash strings that have been encoded in the URL.
 const HASH_PREFIX = 'h@';

src/plugins/kibana_utils/public/state_management/url/hash_unhash_url.ts
@@ -22,8 +22,8 @@ import rison, { RisonObject } from 'rison-node';
 import { stringify as stringifyQueryString } from 'querystring';
 import encodeUriQuery from 'encode-uri-query';
 import { format as formatUrl, parse as parseUrl } from 'url';
-import { hashedItemStore } from '../../../../../plugins/kibana_utils/public';
-import { createStateHash, isStateHash } from './state_hash';
+import { hashedItemStore } from '../../storage/hashed_item_store';
+import { createStateHash, isStateHash } from '../state_hash';
 
 export type IParsedUrlQuery = Record<string, any>;
 
@@ -98,7 +98,7 @@ export function retrieveState(stateHash: string): RisonObject {
   const json = hashedItemStore.getItem(stateHash);
   const throwUnableToRestoreUrlError = () => {
     throw new Error(
-      i18n.translate('common.ui.stateManagement.unableToRestoreUrlErrorMessage', {
+      i18n.translate('kibana_utils.stateManagement.url.unableToRestoreUrlErrorMessage', {
         defaultMessage:
           'Unable to completely restore the URL, be sure to use the share functionality.',
       })
@@ -125,7 +125,7 @@ export function persistState(state: RisonObject): string {
   if (isItemSet) return hash;
   // If we ran out of space trying to persist the state, notify the user.
   const message = i18n.translate(
-    'common.ui.stateManagement.unableToStoreHistoryInSessionErrorMessage',
+    'kibana_utils.stateManagement.url.unableToStoreHistoryInSessionErrorMessage',
     {
       defaultMessage:
         'Kibana is unable to store history items in your session ' +

src/plugins/kibana_utils/public/state_management/url/index.ts
@@ -17,25 +17,4 @@
  * under the License.
  */
 
-import chrome from 'ui/chrome';
-import { hashUrl } from '../../../../plugins/kibana_utils/public';
-import uiRoutes from 'ui/routes';
-import { fatalError } from 'ui/notify';
-
-uiRoutes.enable();
-uiRoutes
-  .when('/', {
-    resolve: {
-      url: function (AppState, globalState, $window) {
-        const redirectUrl = chrome.getInjected('redirectUrl');
-        try {
-          const hashedUrl = hashUrl(redirectUrl);
-          const url = chrome.addBasePath(hashedUrl);
-
-          $window.location = url;
-        } catch (e) {
-          fatalError(e);
-        }
-      }
-    }
-  });
+export * from './hash_unhash_url';

src/legacy/ui/public/state_management/__tests__/state.js
@@ -26,11 +26,11 @@ import { toastNotifications } from '../../notify';
 import * as FatalErrorNS from '../../notify/fatal_error';
 import { StateProvider } from '../state';
 import {
+  unhashQuery,
   createStateHash,
   isStateHash,
-  unhashQuery
-} from '../state_hashing';
-import { HashedItemStore } from '../../../../../plugins/kibana_utils/public';
+  HashedItemStore
+} from '../../../../../plugins/kibana_utils/public';
 import { StubBrowserStorage } from 'test_utils/stub_browser_storage';
 import { EventsProvider } from '../../events';
 

src/plugins/kibana_utils/public/state_management/state_hash/state_hash.test.ts
@@ -18,9 +18,8 @@
  */
 
 import { encode as encodeRison } from 'rison-node';
-// eslint-disable-next-line @kbn/eslint/no-restricted-paths
-import { mockStorage } from '../../../../../plugins/kibana_utils/public/storage/hashed_item_store/mock';
-import { createStateHash, isStateHash } from '../state_hashing';
+import { mockStorage } from '../../storage/hashed_item_store/mock';
+import { createStateHash, isStateHash } from './state_hash';
 
 describe('stateHash', () => {
   beforeEach(() => {

src/plugins/kibana_utils/public/state_management/url/hash_unhash_url.test.ts
@@ -17,9 +17,8 @@
  * under the License.
  */
 
-// eslint-disable-next-line @kbn/eslint/no-restricted-paths
-import { mockStorage } from '../../../../../plugins/kibana_utils/public/storage/hashed_item_store/mock';
-import { HashedItemStore } from '../../../../../plugins/kibana_utils/public';
+import { mockStorage } from '../../storage/hashed_item_store/mock';
+import { HashedItemStore } from '../../storage/hashed_item_store';
 import { hashUrl, unhashUrl } from './hash_unhash_url';
 
 describe('hash unhash url', () => {

test/typings/encode_uri_query.d.ts
@@ -17,25 +17,8 @@
  * under the License.
  */
 
-import chrome from 'ui/chrome';
-import { hashUrl } from '../../../../plugins/kibana_utils/public';
-import uiRoutes from 'ui/routes';
-import { fatalError } from 'ui/notify';
-
-uiRoutes.enable();
-uiRoutes
-  .when('/', {
-    resolve: {
-      url: function (AppState, globalState, $window) {
-        const redirectUrl = chrome.getInjected('redirectUrl');
-        try {
-          const hashedUrl = hashUrl(redirectUrl);
-          const url = chrome.addBasePath(hashedUrl);
-
-          $window.location = url;
-        } catch (e) {
-          fatalError(e);
-        }
-      }
-    }
-  });
+declare module 'encode-uri-query' {
+  function encodeUriQuery(query: string, usePercentageSpace?: boolean): string;
+  // eslint-disable-next-line import/no-default-export
+  export default encodeUriQuery;
+}

test/typings/rison_node.d.ts
@@ -17,25 +17,23 @@
  * under the License.
  */
 
-import chrome from 'ui/chrome';
-import { hashUrl } from '../../../../plugins/kibana_utils/public';
-import uiRoutes from 'ui/routes';
-import { fatalError } from 'ui/notify';
+declare module 'rison-node' {
+  export type RisonValue = null | boolean | number | string | RisonObject | RisonArray;
 
-uiRoutes.enable();
-uiRoutes
-  .when('/', {
-    resolve: {
-      url: function (AppState, globalState, $window) {
-        const redirectUrl = chrome.getInjected('redirectUrl');
-        try {
-          const hashedUrl = hashUrl(redirectUrl);
-          const url = chrome.addBasePath(hashedUrl);
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface RisonArray extends Array<RisonValue> {}
 
-          $window.location = url;
-        } catch (e) {
-          fatalError(e);
-        }
-      }
-    }
-  });
+  export interface RisonObject {
+    [key: string]: RisonValue;
+  }
+
+  export const decode: (input: string) => RisonValue;
+
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  export const decode_object: (input: string) => RisonObject;
+
+  export const encode: <Input extends RisonValue>(input: Input) => string;
+
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  export const encode_object: <Input extends RisonObject>(input: Input) => string;
+}