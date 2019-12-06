159a3a177c8e4a2e04b7c07c94f4c1dd9dff7569
Nathan Reese <reese.nathan@gmail.com>
2019-12-06 10:53:30 -0700

    [Maps] use style metadata to calculate symbolization bands (#51713) (#52401)
    * [Maps] use style metadata to calculate symbolization bands
    
    * only update style meta when fields change
    
    * load join source style meta
    
    * use style meta data request to populate range
    
    * apply source filter to style meta request
    
    * fix heatmap
    
    * only use style meta range if field supports field meta
    
    * add fieldMetaOptions to style prperty descriptor and add migration script
    
    * add UI for setting fieldMetaOptions.isEnabled
    
    * clean up
    
    * review feedback
    
    * fix can_skip_fetch tests
    
    * review feedback
    
    * only show field meta popover for fields that support field meta
    
    * avoid duplicate fields re-fetching style meta
    
    * clean up problems when first creating grid source
    
    * update text for enabling field meta toggle
    
    * provide UI for setting sigma
    
    * allow users to include global time in style meta request
    
    * update SIEM saved objects
    
    * add less than and greater than symbols when styling by field stats
    
    * fix functional tests
    
    * review feedback
    
    * add support for date fields
    
    * review feedback
    
    * only show less then and greater then in legend when values will be outside of std range
    
    * unnest VectorStyle._getFieldRange
    
    * remove unused function
    
    * only show style isTimeAware switch when style fields use field meta

--- x-pack/legacy/plugins/maps/public/layers/styles/vector/vector_style_defaults.js
+++ x-pack/legacy/plugins/maps/public/layers/styles/vector/vector_style_defaults.js
@@ -16,8 +16,9 @@ const DEFAULT_ICON = 'airfield';
 
 export const DEFAULT_MIN_SIZE = 1;
 export const DEFAULT_MAX_SIZE = 64;
+export const DEFAULT_SIGMA = 3;
 
-export const vectorStyles = {
+export const VECTOR_STYLES = {
   SYMBOL: 'symbol',
   FILL_COLOR: 'fillColor',
   LINE_COLOR: 'lineColor',
@@ -29,7 +30,7 @@ export const vectorStyles = {
 export function getDefaultProperties(mapColors = []) {
   return {
     ...getDefaultStaticProperties(mapColors),
-    [vectorStyles.SYMBOL]: {
+    [VECTOR_STYLES.SYMBOL]: {
       options: {
         symbolizeAs: SYMBOLIZE_AS_CIRCLE,
         symbolId: DEFAULT_ICON,
@@ -48,31 +49,31 @@ export function getDefaultStaticProperties(mapColors = []) {
 
 
   return {
-    [vectorStyles.FILL_COLOR]: {
+    [VECTOR_STYLES.FILL_COLOR]: {
       type: VectorStyle.STYLE_TYPE.STATIC,
       options: {
         color: nextFillColor,
       }
     },
-    [vectorStyles.LINE_COLOR]: {
+    [VECTOR_STYLES.LINE_COLOR]: {
       type: VectorStyle.STYLE_TYPE.STATIC,
       options: {
         color: nextLineColor
       }
     },
-    [vectorStyles.LINE_WIDTH]: {
+    [VECTOR_STYLES.LINE_WIDTH]: {
       type: VectorStyle.STYLE_TYPE.STATIC,
       options: {
         size: 1
       }
     },
-    [vectorStyles.ICON_SIZE]: {
+    [VECTOR_STYLES.ICON_SIZE]: {
       type: VectorStyle.STYLE_TYPE.STATIC,
       options: {
         size: DEFAULT_ICON_SIZE
       }
     },
-    [vectorStyles.ICON_ORIENTATION]: {
+    [VECTOR_STYLES.ICON_ORIENTATION]: {
       type: VectorStyle.STYLE_TYPE.STATIC,
       options: {
         orientation: 0
@@ -83,40 +84,60 @@ export function getDefaultStaticProperties(mapColors = []) {
 
 export function getDefaultDynamicProperties() {
   return {
-    [vectorStyles.FILL_COLOR]: {
+    [VECTOR_STYLES.FILL_COLOR]: {
       type: VectorStyle.STYLE_TYPE.DYNAMIC,
       options: {
         color: COLOR_GRADIENTS[0].value,
         field: undefined,
+        fieldMetaOptions: {
+          isEnabled: true,
+          sigma: DEFAULT_SIGMA,
+        }
       }
     },
-    [vectorStyles.LINE_COLOR]: {
+    [VECTOR_STYLES.LINE_COLOR]: {
       type: VectorStyle.STYLE_TYPE.DYNAMIC,
       options: {
         color: COLOR_GRADIENTS[0].value,
         field: undefined,
+        fieldMetaOptions: {
+          isEnabled: true,
+          sigma: DEFAULT_SIGMA,
+        }
       }
     },
-    [vectorStyles.LINE_WIDTH]: {
+    [VECTOR_STYLES.LINE_WIDTH]: {
       type: VectorStyle.STYLE_TYPE.DYNAMIC,
       options: {
         minSize: DEFAULT_MIN_SIZE,
         maxSize: DEFAULT_MAX_SIZE,
         field: undefined,
+        fieldMetaOptions: {
+          isEnabled: true,
+          sigma: DEFAULT_SIGMA,
+        }
       }
     },
-    [vectorStyles.ICON_SIZE]: {
+    [VECTOR_STYLES.ICON_SIZE]: {
       type: VectorStyle.STYLE_TYPE.DYNAMIC,
       options: {
         minSize: DEFAULT_MIN_SIZE,
         maxSize: DEFAULT_MAX_SIZE,
         field: undefined,
+        fieldMetaOptions: {
+          isEnabled: true,
+          sigma: DEFAULT_SIGMA,
+        }
       }
     },
-    [vectorStyles.ICON_ORIENTATION]: {
+    [VECTOR_STYLES.ICON_ORIENTATION]: {
       type: VectorStyle.STYLE_TYPE.STATIC,
       options: {
         field: undefined,
+        fieldMetaOptions: {
+          isEnabled: true,
+          sigma: DEFAULT_SIGMA,
+        }
       }
     },
   };
