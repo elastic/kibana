9126d537d0a9559260848cb37500420b6cbac77d
Aaron Caldwell <aaron.caldwell@elastic.co>
2019-12-06 11:43:04 -0700

    Add index and general maps plugin template

--- x-pack/legacy/plugins/maps/public/index.ts
+++ x-pack/legacy/plugins/maps/public/index.ts
@@ -16,19 +16,28 @@ import 'uiExports/embeddableActions';
 import 'ui/agg_types';
 
 import 'ui/kbn_top_nav';
+// @ts-ignore
 import { uiModules } from 'ui/modules';
 import 'ui/autoload/all';
 import 'react-vis/dist/style.css';
 
 import './angular/services/gis_map_saved_object_loader';
 import './angular/map_controller';
+// @ts-ignore
+import { MapsPlugin } from './plugin';
+// @ts-ignore
 import { MapListing } from './components/map_listing';
+// @ts-ignore
 import { initRoutes } from './routes';
 
 const app = uiModules.get('app/maps', ['ngRoute', 'react']);
 
-app.directive('mapListing', function (reactDirective) {
+app.directive('mapListing', function(reactDirective: any) {
   return reactDirective(wrapInI18nContext(MapListing));
 });
 
 initRoutes();
+
+export function plugin() {
+  return new MapsPlugin();
+}
---
+++ x-pack/legacy/plugins/maps/public/plugin.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { Plugin, CoreStart } from 'src/core/public';
+
+/**
+ * These are the interfaces with your public contracts. You should export these
+ * for other plugins to use in _their_ `SetupDeps`/`StartDeps` interfaces.
+ * @public
+ */
+export type MapsPluginSetup = ReturnType<MapsPlugin['setup']>;
+export type MapsPluginStart = ReturnType<MapsPlugin['start']>;
+
+/** @internal */
+export class MapsPlugin implements Plugin<MapsPluginSetup, MapsPluginStart> {
+  public setup() {}
+
+  public start(core: CoreStart) {}
+}
