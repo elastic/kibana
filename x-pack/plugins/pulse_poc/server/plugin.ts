0b8d10f213855e5db076a0b3e598d9b8908054e1
Court Ewing <court@epixa.com>
2019-12-07 15:10:42 -0500

    Base service with receiving_telemetry check

---
+++ x-pack/plugins/pulse_poc/server/plugin.ts
@@ -0,0 +1,81 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
+import { schema } from '@kbn/config-schema';
+import { CoreSetup, PluginInitializerContext } from 'src/core/server';
+
+export class PulsePocPlugin {
+  private channels = readdirSync(resolve(__dirname, 'channels'))
+    .filter((fileName: string) => !fileName.startsWith('.'))
+    .map((channelName: string) => {
+      const channelPath = resolve(__dirname, 'channels', channelName);
+      const checks = readdirSync(channelPath)
+        .filter((fileName: string) => fileName.startsWith('check_'))
+        .map((fileName: string) => {
+          const id = fileName.slice(6, -3);
+          const checkFilePath = resolve(channelPath, fileName);
+          const check = require(checkFilePath).check;
+          return { id, check };
+        });
+      return {
+        id: channelName,
+        checks,
+      };
+    });
+
+  constructor(private readonly initializerContext: PluginInitializerContext) {}
+
+  public async setup(core: CoreSetup) {
+    const router = core.http.createRouter();
+
+    router.post(
+      {
+        path: '/api/pulse_poc/intake',
+        validate: {
+          params: schema.object({
+            channels: schema.arrayOf(schema.object({})),
+          }),
+        },
+      },
+      async (context, request, response) => {
+        return response.ok();
+      }
+    );
+
+    router.get(
+      {
+        path: '/api/pulse_poc/instructions/{deploymentId}',
+        validate: {
+          query: schema.object({
+            deploymentId: schema.string(),
+          }),
+        },
+      },
+      async (context, request, response) => {
+        const { deploymentId } = request.query;
+        const es = context.core.elasticsearch.adminClient;
+
+        const allChannelCheckResults = this.channels.map(async channel => {
+          const channelChecks = channel.checks.map(check => check.check(es, deploymentId));
+          const checkResults = await Promise.all(channelChecks);
+          const instructions = checkResults.filter((value: any) => Boolean(value));
+          return {
+            id: channel.id,
+            instructions,
+          };
+        });
+        const channels = await Promise.all(allChannelCheckResults);
+        return response.ok({ body: { channels } });
+      }
+    );
+  }
+
+  public start() {}
+
+  public stop() {}
+}
