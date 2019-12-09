a2806d1453024d0da7468985e6fb4333fd38bef5
Robert Oskamp <robert.oskamp@elastic.co>
2019-12-09 09:52:09 +0100

    Remove .only from anomaly detection suite

--- x-pack/test/functional/apps/machine_learning/anomaly_detection/index.ts
+++ x-pack/test/functional/apps/machine_learning/anomaly_detection/index.ts
@@ -6,8 +6,7 @@
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function({ loadTestFile }: FtrProviderContext) {
-  // eslint-disable-next-line ban/ban
-  describe.only('anomaly detection', function() {
+  describe('anomaly detection', function() {
     this.tags(['skipFirefox']);
 
     loadTestFile(require.resolve('./single_metric_job'));
