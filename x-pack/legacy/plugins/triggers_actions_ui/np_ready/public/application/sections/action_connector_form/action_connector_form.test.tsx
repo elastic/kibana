0c4bbe8c2645549fde94cc43d3c2e090848054df
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2019-12-09 21:39:59 -0800

    Added connector form unit tests

---
+++ x-pack/legacy/plugins/triggers_actions_ui/np_ready/public/application/sections/action_connector_form/action_connector_form.test.tsx
@@ -0,0 +1,95 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import * as React from 'react';
+import { mountWithIntl } from 'test_utils/enzyme_helpers';
+import { setAppDependencies } from '../../app_dependencies';
+import { coreMock } from '../../../../../../../../../src/core/public/mocks';
+import { ReactWrapper } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import { ConnectorAddFlyout } from './connector_add_flyout';
+import { ActionsConnectorsContext } from '../../context/actions_connectors_context';
+import { actionTypeRegistryMock } from '../../action_type_registry.mock';
+import { ValidationResult } from '../../../types';
+jest.mock('../../context/actions_connectors_context');
+const actionTypeRegistry = actionTypeRegistryMock.create();
+
+describe('action_connector_form', () => {
+  let wrapper: ReactWrapper<any>;
+
+  beforeEach(async () => {
+    const deps = {
+      core: coreMock.createStart(),
+      plugins: {
+        capabilities: {
+          get() {
+            return {
+              actions: {
+                delete: true,
+                save: true,
+                show: true,
+              },
+            };
+          },
+        },
+      } as any,
+      actionTypeRegistry: actionTypeRegistry as any,
+      alertTypeRegistry: {} as any,
+    };
+    const AppDependenciesProvider = setAppDependencies(deps);
+
+    await act(async () => {
+      wrapper = mountWithIntl(
+        <AppDependenciesProvider value={deps}>
+          <ActionsConnectorsContext.Provider
+            value={{
+              addFlyoutVisible: true,
+              setAddFlyoutVisibility: state => {},
+              editFlyoutVisible: false,
+              setEditFlyoutVisibility: state => {},
+              actionTypesIndex: { 'my-action-type': { id: 'my-action-type', name: 'test' } },
+              reloadConnectors: () => {
+                return new Promise<void>(() => {});
+              },
+            }}
+          >
+            <ConnectorAddFlyout />
+          </ActionsConnectorsContext.Provider>
+        </AppDependenciesProvider>
+      );
+    });
+
+    await waitForRender(wrapper);
+  });
+
+  it('renders action_connector_form', () => {
+    const actionType = {
+      id: 'my-action-type',
+      iconClass: 'test',
+      selectMessage: 'test',
+      validateConnector: (): ValidationResult => {
+        return { errors: {} };
+      },
+      validateParams: (): ValidationResult => {
+        const validationResult = { errors: {} };
+        return validationResult;
+      },
+      actionConnectorFields: null,
+      actionParamsFields: null,
+    };
+    actionTypeRegistry.get.mockReturnValueOnce(actionType);
+    actionTypeRegistry.has.mockReturnValue(true);
+
+    const connectorNameField = wrapper.find('[data-test-subj="nameInput"]');
+    expect(connectorNameField.exists()).toBeTruthy();
+    expect(connectorNameField.first().prop('value')).toBe('action-connector');
+  });
+});
+
+async function waitForRender(wrapper: ReactWrapper<any, any>) {
+  await Promise.resolve();
+  await Promise.resolve();
+  wrapper.update();
+}
