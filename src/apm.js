7bddbef3b0af259bbb8b831569b35cacdfe12b9d
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2019-12-10 12:50:11 +0100

    remove config/apm.js and address review

--- src/apm.js
+++ src/apm.js
@@ -17,29 +17,68 @@
  * under the License.
  */
 
-const { existsSync } = require('fs');
 const { join } = require('path');
+const { readFileSync } = require('fs');
+const { execSync } = require('child_process');
+const merge = require('lodash.merge');
 const { name, version, build } = require('../package.json');
 
-/**
- * flag to disable APM support on all kibana builds by default
- */
-const isKibanaDistributable = Boolean(build && build.distributable === true);
+function gitRev() {
+  try {
+    return execSync('git rev-parse --short HEAD', { encoding: 'utf-8' }).trim();
+  } catch (e) {
+    return null;
+  }
+}
+
+function devConfig() {
+  try {
+    const apmDevConfigPath = join(__dirname, '..', 'config', 'apm.dev.js');
+    return require(apmDevConfigPath); // eslint-disable-line import/no-dynamic-require
+  } catch (e) {
+    return {};
+  }
+}
+
+const apmConfig = merge(
+  {
+    active: false,
+    serverUrl: 'https://f1542b814f674090afd914960583265f.apm.us-central1.gcp.cloud.es.io:443',
+    // The secretToken below is intended to be hardcoded in this file even though
+    // it makes it public. This is not a security/privacy issue. Normally we'd
+    // instead disable the need for a secretToken in the APM Server config where
+    // the data is transmitted to, but due to how it's being hosted, it's easier,
+    // for now, to simply leave it in.
+    secretToken: 'R0Gjg46pE9K9wGestd',
+    globalLabels: {},
+    breakdownMetrics: true,
+    centralConfig: false,
+    logUncaughtExceptions: true,
+  },
+  devConfig()
+);
+
+try {
+  const filename = join(__dirname, '..', 'data', 'uuid');
+  apmConfig.globalLabels.kibana_uuid = readFileSync(filename, 'utf-8');
+} catch (e) {} // eslint-disable-line no-empty
+
+const rev = gitRev();
+if (rev !== null) apmConfig.globalLabels.git_rev = rev;
 
 function getConfig(serviceName) {
-  const config = {
-    serviceName: `${serviceName}-${version.replace(/\./g, '_')}`,
+  return {
+    ...apmConfig,
+    ...{
+      serviceName: `${serviceName}-${version.replace(/\./g, '_')}`,
+    },
   };
-
-  const configPath = join(__dirname, '..', 'config', 'apm.js');
-  if (existsSync(configPath)) {
-    const configFile = require(configPath); // eslint-disable-line import/no-dynamic-require
-    Object.assign(config, configFile);
-  } else {
-    config.active = false;
-  }
-  return config;
 }
+
+/**
+ * Flag to disable APM RUM support on all kibana builds by default
+ */
+const isKibanaDistributable = Boolean(build && build.distributable === true);
 
 module.exports = function(serviceName = name) {
   if (process.env.kbnWorkerType === 'optmzr') return;
