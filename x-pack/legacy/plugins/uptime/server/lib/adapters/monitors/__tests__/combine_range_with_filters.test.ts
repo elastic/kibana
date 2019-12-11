f3bcbc41b387ad93e5e12eaf563b459e1f7e310a
Justin Kambic <justin.kambic@elastic.co>
2019-12-10 20:40:30 -0500

    Finish working on adapter/helper tests.

---
+++ x-pack/legacy/plugins/uptime/server/lib/adapters/monitors/__tests__/combine_range_with_filters.test.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { combineRangeWithFilters } from '../combine_range_with_filters';
+
+describe('combineRangeWithFilters', () => {
+  it('combines filters that have no filter clause', () => {
+    expect(
+      combineRangeWithFilters(
+        'now-15m',
+        'now',
+        '{"bool":{"should":[{"match":{"url.port":80}}],"minimum_should_match":1}}'
+      )
+    ).toMatchSnapshot();
+  });
+
+  it('combines query with filter object', () => {
+    expect(
+      combineRangeWithFilters(
+        'now-15m',
+        'now',
+        '{"bool":{"filter":{"term":{"field":"monitor.id"}},"should":[{"match":{"url.port":80}}],"minimum_should_match":1}}'
+      )
+    ).toMatchSnapshot();
+  });
+
+  it('combines query with filter list', () => {
+    expect(
+      combineRangeWithFilters(
+        'now-15m',
+        'now',
+        '{"bool":{"filter":[{"field":"monitor.id"}],"should":[{"match":{"url.port":80}}],"minimum_should_match":1}}'
+      )
+    ).toMatchSnapshot();
+  });
+});
