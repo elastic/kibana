4aaaa1d830658c1c6abf9c2a94a2e39823c46456
Nicolas Ruflin <spam@ruflin.com>
2019-12-11 14:21:10 +0100

    Update x-pack/legacy/plugins/epm/common/types.ts
    Co-Authored-By: John Schulz <github.com@jfsiii.org>

--- x-pack/legacy/plugins/epm/common/types.ts
+++ x-pack/legacy/plugins/epm/common/types.ts
@@ -122,7 +122,7 @@ export interface Dataset {
   ingeset_pipeline: string;
   vars: object[];
   type: string;
-  // This is for convenient and not in the output from the registry. When creating a dataset, this info should be added.
+  // This is for convenience and not in the output from the registry. When creating a dataset, this info should be added.
   package: string;
 }
 
