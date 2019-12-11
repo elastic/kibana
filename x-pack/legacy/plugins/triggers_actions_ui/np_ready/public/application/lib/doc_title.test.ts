8ecccba6c055dae69e8c54674d6a4bea7b524b1f
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2019-12-11 10:19:12 -0800

    Added doc_title and get_time_units unit tests

---
+++ x-pack/legacy/plugins/triggers_actions_ui/np_ready/public/application/lib/doc_title.test.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import { docTitleService } from './doc_title';
+import { coreMock } from '../../../../../../../../src/core/public/mocks';
+
+describe('doc_title', () => {
+  test('if change calls set proper breadcrumb title ', async () => {
+    const core = coreMock.createStart();
+    const spy = jest.spyOn(core.chrome.docTitle, 'change');
+    docTitleService.init(spy);
+    docTitleService.setTitle('home');
+    docTitleService.setTitle('alerts');
+    docTitleService.setTitle('connectors');
+    expect(core.chrome.docTitle).toMatchInlineSnapshot(`
+    Object {
+      "__legacy": Object {
+        "setBaseTitle": [MockFunction],
+      },
+      "change": [MockFunction] {
+        "calls": Array [
+          Array [
+            "Alerts and actions",
+          ],
+          Array [
+            "Alerts",
+          ],
+          Array [
+            "Connectors",
+          ],
+        ],
+        "results": Array [
+          Object {
+            "type": "return",
+            "value": undefined,
+          },
+          Object {
+            "type": "return",
+            "value": undefined,
+          },
+          Object {
+            "type": "return",
+            "value": undefined,
+          },
+        ],
+      },
+      "reset": [MockFunction],
+    }
+    `);
+  });
+});
