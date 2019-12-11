c2c11b90d68bbe351e7fab2710c1c931c0d40a9c
Alexey Antonov <alexwizp@gmail.com>
2019-12-11 10:28:54 +0300

    [ui/public/utils] Move items into ui/vis (#52615)
    * [ui/public/utils] Move items into ui/vis
    
    * fix PR comments

--- src/legacy/ui/public/vis/map/decode_geo_hash.test.ts
+++ src/legacy/ui/public/vis/map/decode_geo_hash.test.ts
@@ -17,27 +17,18 @@
  * under the License.
  */
 
-import { geohashColumns, decodeGeoHash } from '../decode_geo_hash';
+import { geohashColumns, decodeGeoHash } from './decode_geo_hash';
 
-test('geohashColumns', function () {
+test('geohashColumns', () => {
   expect(geohashColumns(1)).toBe(8);
   expect(geohashColumns(2)).toBe(8 * 4);
   expect(geohashColumns(3)).toBe(8 * 4 * 8);
   expect(geohashColumns(4)).toBe(8 * 4 * 8 * 4);
 });
 
-test('decodeGeoHash', function () {
+test('decodeGeoHash', () => {
   expect(decodeGeoHash('drm3btev3e86')).toEqual({
-    latitude: [
-      41.119999922811985,
-      41.12000009045005,
-      41.12000000663102,
-    ],
-    longitude: [
-      -71.34000029414892,
-      -71.3399999588728,
-      -71.34000012651086,
-    ],
+    latitude: [41.119999922811985, 41.12000009045005, 41.12000000663102],
+    longitude: [-71.34000029414892, -71.3399999588728, -71.34000012651086],
   });
 });
-
