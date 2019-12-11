fce4f82ad46159cab82cb680385cbecbee5102ed
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2019-12-11 17:27:17 +0100

    [EPM] Data source integration tests (#52542)
    * Add fixtures for data source integration test.
    
    * Move test setup to beforeEach
    
    * Add test for datasource creation
    
    * Handle pipelines in yml format.
    
    * Make integration test for adding a data source pass.

---
+++ x-pack/test/epm_api_integration/apis/data_sources.ts
@@ -0,0 +1,105 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+/* eslint-disable no-console */
+/* tslint:disable */
+
+import { readFileSync } from 'fs';
+import path from 'path';
+import expect from '@kbn/expect';
+import ServerMock from 'mock-http-server';
+import { FtrProviderContext } from '../../api_integration/ftr_provider_context';
+
+export default function({ getService }: FtrProviderContext) {
+  const supertest = getService('supertest');
+  describe('data source installation', () => {
+    const registryMock = new ServerMock({ host: 'localhost', port: 6666 });
+    beforeEach(async () => {
+      registryMock.start(() => {});
+      const packageResponse = readFileSync(
+        path.join(__dirname, '/fixtures/packages/package/yamlpipeline_1.0.0')
+      ).toString();
+      const fileResponse = readFileSync(
+        path.join(__dirname, '/fixtures/packages/epr/yamlpipeline_1.0.0.tar.gz')
+      );
+      registryMock.on({
+        method: 'GET',
+        path: '/package/yamlpipeline-1.0.0',
+        reply: {
+          status: 200,
+          headers: { 'content-type': 'application/json' },
+          body: packageResponse,
+        },
+      });
+
+      registryMock.on({
+        method: 'GET',
+        path: '/epr/yamlpipeline/yamlpipeline-1.0.0.tar.gz',
+        reply: {
+          status: 200,
+          headers: { 'content-type': 'application/gzip' },
+          body: fileResponse,
+        },
+      });
+
+      const installPackage = async () => {
+        const response = await supertest
+          .get('/api/epm/install/yamlpipeline-1.0.0')
+          .set('kbn-xsrf', 'xxx')
+          .expect(200);
+        return response.body;
+      };
+      // commment for debugging
+      await installPackage();
+
+      // uncomment for debugging
+      // const packageInstallResponse = await installPackage();
+      // console.log('packageInstallResponse is: ', packageInstallResponse);
+      // console.log(
+      //   'requests are',
+      //   registryMock.requests().map(r => r.url)
+      // );
+    });
+    afterEach(() => {
+      registryMock.stop(() => {});
+    });
+    it('test setup works', async () => {
+      const readPackageSavedObject = async () => {
+        const response = await supertest
+          .get('/api/saved_objects/epm-package/yamlpipeline-1.0.0')
+          .expect(200);
+        return response.body;
+      };
+      const savedObjectResponse = await readPackageSavedObject();
+      expect(savedObjectResponse.id).to.be('yamlpipeline-1.0.0');
+    });
+    it('works with a package containing only yml format ingest pipelines', async () => {
+      const createDataSource = async () => {
+        const response = await supertest
+          .get('/api/epm/datasource/install/yamlpipeline-1.0.0')
+          .expect(200);
+        return response.body;
+      };
+
+      const readDataSourceSavedObject = async () => {
+        const response = await supertest
+          .get('/api/saved_objects/epm-datasource/yamlpipeline-1.0.0')
+          .expect(200);
+        return response.body;
+      };
+
+      // comment for debugging
+      await createDataSource();
+
+      // uncomment for debugging
+      // const createDataSourceResponse = await createDataSource();
+      // console.log('createDataSourceResponse is ', createDataSourceResponse);
+
+      const readDataSourceSavedObjectResponse = await readDataSourceSavedObject();
+      expect(readDataSourceSavedObjectResponse.id).to.be('yamlpipeline-1.0.0');
+    });
+  });
+}
--- x-pack/test/epm_api_integration/apis/index.js
+++ x-pack/test/epm_api_integration/apis/index.js
@@ -11,5 +11,6 @@ export default function ({ loadTestFile }) {
     loadTestFile(require.resolve('./file'));
     loadTestFile(require.resolve('./template'));
     loadTestFile(require.resolve('./ilm'));
+    loadTestFile(require.resolve('./data_sources'));
   });
 }
