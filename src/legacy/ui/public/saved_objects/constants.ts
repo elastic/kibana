85aea35c944abf3bb8d315cb599d0e19ce58ed1f
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2019-12-12 17:35:22 +0100

    Deangularize and typescriptify ui/saved_object (#51562)

--- src/legacy/ui/public/saved_objects/constants.ts
+++ src/legacy/ui/public/saved_objects/constants.ts
@@ -16,15 +16,25 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import { i18n } from '@kbn/i18n';
 
-export interface SaveOptions {
-  confirmOverwrite: boolean;
-  isTitleDuplicateConfirmed: boolean;
-  onTitleDuplicate: () => void;
-}
-
-export interface SavedObject {
-  save: (saveOptions: SaveOptions) => Promise<string>;
-  copyOnSave: boolean;
-  id?: string;
-}
+/**
+ * An error message to be used when the user rejects a confirm overwrite.
+ * @type {string}
+ */
+export const OVERWRITE_REJECTED = i18n.translate(
+  'common.ui.savedObjects.overwriteRejectedDescription',
+  {
+    defaultMessage: 'Overwrite confirmation was rejected',
+  }
+);
+/**
+ * An error message to be used when the user rejects a confirm save with duplicate title.
+ * @type {string}
+ */
+export const SAVE_DUPLICATE_REJECTED = i18n.translate(
+  'common.ui.savedObjects.saveDuplicateRejectedDescription',
+  {
+    defaultMessage: 'Save with duplicate title confirmation was rejected',
+  }
+);
