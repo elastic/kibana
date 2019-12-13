20ad523cf76c6d1c533bdf06201c083737b530cc
Jonathan Budzenski <jon@budzenski.me>
2019-12-12 23:50:34 -0600

    Support api_integration/kibana/stats against remote hosts
    The stats API integration tests run under the assumption of a fixed
    kibana.yml.  This is the case when running against a
    default FTR configuration, but not when running against remote servers.
    This relaxes a few assertions and conditionally runs in cases that can't be
    relaxed.
    
    Resolves #37386

--- x-pack/test/api_integration/apis/kibana/stats/stats.js
+++ x-pack/test/api_integration/apis/kibana/stats/stats.js
@@ -9,26 +9,36 @@ import expect from '@kbn/expect';
 export default function ({ getService }) {
   const supertestNoAuth = getService('supertestWithoutAuth');
   const supertest = getService('supertest');
+  const config = getService('config');
 
   describe('/api/stats', () => {
     describe('operational stats and usage stats', () => {
 
+      // lazy check for uuid for test runs against preexisting services
+      function isUUID(uuid) {
+        return typeof uuid === 'string' && uuid.length === 36;
+      }
+
       describe('no auth', () => {
-        it('should return 200 and stats for no extended', async () => {
+        // depends on kibana.yml setting status.allowAnonymous
+        // skip this test when running against a remote host, as we can't
+        // validate the status of this setting
+        const host = config.get('servers.kibana.host');
+        const ifLocalhost = host.includes('localhost') ? it : it.skip;
+        ifLocalhost('should return 200 and stats for no extended', async () => {
           const { body } = await supertestNoAuth
             .get('/api/stats')
             .expect(200);
-          expect(body.kibana.uuid).to.eql('5b2de169-2785-441b-ae8c-186a1936b17d');
+          expect(isUUID(body.kibana.uuid)).to.be.ok();
           expect(body.process.uptime_ms).to.be.greaterThan(0);
           expect(body.os.uptime_ms).to.be.greaterThan(0);
           expect(body.usage).to.be(undefined);
         });
 
-        // FLAKY: https://github.com/elastic/kibana/issues/29254
-        it.skip('should return 401 for extended', async () => {
+        it('should return 401 for extended', async () => {
           await supertestNoAuth
             .get('/api/stats?extended')
-            .expect(401); // unauthorized
+            .expect(401);
         });
       });
 
@@ -37,7 +47,7 @@ export default function ({ getService }) {
           const { body } = await supertest
             .get('/api/stats')
             .expect(200);
-          expect(body.kibana.uuid).to.eql('5b2de169-2785-441b-ae8c-186a1936b17d');
+          expect(isUUID(body.kibana.uuid)).to.be.ok();
           expect(body.process.uptime_ms).to.be.greaterThan(0);
           expect(body.os.uptime_ms).to.be.greaterThan(0);
         });
@@ -46,7 +56,7 @@ export default function ({ getService }) {
           const { body } = await supertest
             .get('/api/stats?extended')
             .expect(200);
-          expect(body.kibana.uuid).to.eql('5b2de169-2785-441b-ae8c-186a1936b17d');
+          expect(isUUID(body.kibana.uuid)).to.be.ok();
           expect(body.process.uptime_ms).to.be.greaterThan(0);
           expect(body.os.uptime_ms).to.be.greaterThan(0);
           expect(body.usage.kibana.index).to.be('.kibana');
@@ -57,7 +67,7 @@ export default function ({ getService }) {
           const { body } = await supertest
             .get('/api/stats?extended&legacy')
             .expect(200);
-          expect(body.kibana.uuid).to.eql('5b2de169-2785-441b-ae8c-186a1936b17d');
+          expect(isUUID(body.kibana.uuid)).to.be.ok();
           expect(body.process.uptime_ms).to.be.greaterThan(0);
           expect(body.os.uptime_ms).to.be.greaterThan(0);
           expect(body.usage.index).to.be('.kibana');
