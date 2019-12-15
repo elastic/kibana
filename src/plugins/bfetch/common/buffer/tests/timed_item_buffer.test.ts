7f528f660d98a6ae257d3cb618ad0f03bda0e181
streamich <streamich@gmail.com>
2019-12-16 00:41:33 +0100

    test: 💍 add TimedItemBuffer tests

---
+++ src/plugins/bfetch/common/buffer/tests/timed_item_buffer.test.ts
@@ -0,0 +1,105 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { TimedItemBuffer } from '../timed_item_buffer';
+import { runItemBufferTests } from './run_item_buffer_tests';
+import { ItemBuffer } from '../item_buffer';
+
+describe('TimedItemBuffer', () => {
+  runItemBufferTests((TimedItemBuffer as unknown) as new () => ItemBuffer<any>);
+
+  test('does not do unnecessary flushes', async () => {
+    const onflush = jest.fn();
+    const buf = new TimedItemBuffer({
+      onflush,
+      maxItemAge: 3,
+    });
+
+    expect(onflush).toHaveBeenCalledTimes(0);
+    buf.write(0);
+    expect(onflush).toHaveBeenCalledTimes(0);
+    buf.flush();
+    expect(onflush).toHaveBeenCalledTimes(1);
+  });
+
+  test('does not do extra flush after timeout if buffer was flushed during timeout wait', async () => {
+    const onflush = jest.fn();
+    const buf = new TimedItemBuffer({
+      onflush,
+      maxItemAge: 10,
+    });
+
+    buf.write(0);
+    await new Promise(r => setTimeout(r, 3));
+    buf.flush();
+    await new Promise(r => setTimeout(r, 11));
+
+    expect(onflush).toHaveBeenCalledTimes(1);
+  });
+
+  test('flushes buffer automatically after timeout reached', async () => {
+    const onflush = jest.fn();
+    const buf = new TimedItemBuffer({
+      onflush,
+      maxItemAge: 2,
+    });
+
+    buf.write(1);
+    buf.write(2);
+    expect(onflush).toHaveBeenCalledTimes(0);
+
+    await new Promise(r => setTimeout(r, 3));
+    expect(onflush).toHaveBeenCalledTimes(1);
+    expect(onflush).toHaveBeenCalledWith([1, 2]);
+  });
+
+  test('does not call flush after timeout if flush was triggered because buffer size reached', async () => {
+    const onflush = jest.fn();
+    const buf = new TimedItemBuffer({
+      onflush,
+      flushOnMaxItems: 2,
+      maxItemAge: 2,
+    });
+
+    buf.write(1);
+    buf.write(2);
+
+    expect(onflush).toHaveBeenCalledTimes(1);
+    await new Promise(r => setTimeout(r, 3));
+    expect(onflush).toHaveBeenCalledTimes(1);
+  });
+
+  test('does not automatically flush if `.clear()` was called', async () => {
+    const onflush = jest.fn();
+    const buf = new TimedItemBuffer({
+      onflush,
+      flushOnMaxItems: 25,
+      maxItemAge: 5,
+    });
+
+    buf.write(1);
+    buf.write(2);
+    await new Promise(r => setImmediate(r));
+    buf.clear();
+
+    expect(onflush).toHaveBeenCalledTimes(0);
+    await new Promise(r => setTimeout(r, 6));
+    expect(onflush).toHaveBeenCalledTimes(0);
+  });
+});
