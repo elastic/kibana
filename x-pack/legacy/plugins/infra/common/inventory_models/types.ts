e4c2a6cff985d934c5579159bddf6177c69c9296
Phillip Burch <phillip.burch@live.com>
2019-12-16 10:18:21 -0600

    Fix type

--- x-pack/legacy/plugins/infra/common/inventory_models/types.ts
+++ x-pack/legacy/plugins/infra/common/inventory_models/types.ts
@@ -320,7 +320,7 @@ export interface InventoryMetrics {
 export interface InventoryModel {
   id: string;
   displayName: string;
-  requiredDataset: string;
+  requiredModule: string;
   fields: {
     id: string;
     name: string;
