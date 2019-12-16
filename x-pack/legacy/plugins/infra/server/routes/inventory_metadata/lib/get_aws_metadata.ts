b531b6178465fa439f59cd3713610904d7a31dbb
Phillip Burch <phillip.burch@live.com>
2019-12-16 06:26:08 -0600

    Update x-pack/legacy/plugins/infra/server/routes/inventory_metadata/lib/get_aws_metadata.ts
    Co-Authored-By: Chris Cowan <chris@chriscowan.us>

--- x-pack/legacy/plugins/infra/server/routes/inventory_metadata/lib/get_aws_metadata.ts
+++ x-pack/legacy/plugins/infra/server/routes/inventory_metadata/lib/get_aws_metadata.ts
@@ -36,7 +36,7 @@ export const getAWSMetadata = async (
     body: {
       query: {
         bool: {
-          must: [{ match: { 'event.dataset': model.requiredDataset } }],
+          must: [{ match: { 'event.module': model.requiredDataset } }],
         },
       },
       size: 0,
