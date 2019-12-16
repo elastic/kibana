f182158eb854d3499eb6a9c45db53bdfd9c9fab9
Joe Portner <5295965+jportner@users.noreply.github.com>
2019-12-16 09:49:52 -0500

    Generate new certificates and keys for kbn-dev-utils
    Added Kibana certificate/keypair, and also added P12 formats for
    both Kibana and Elasticsearch.

--- packages/kbn-dev-utils/src/index.ts
+++ packages/kbn-dev-utils/src/index.ts
@@ -25,7 +25,19 @@ export {
   ToolingLogCollectingWriter,
 } from './tooling_log';
 export { createAbsolutePathSerializer } from './serializers';
-export { CA_CERT_PATH, ES_KEY_PATH, ES_CERT_PATH } from './certs';
+export {
+  CA_CERT_PATH,
+  ES_KEY_PATH,
+  ES_CERT_PATH,
+  ES_P12_PATH,
+  ES_P12_PASSWORD,
+  ES_EMPTYPASSWORD_P12_PATH,
+  ES_NOPASSWORD_P12_PATH,
+  KBN_KEY_PATH,
+  KBN_CERT_PATH,
+  KBN_P12_PATH,
+  KBN_P12_PASSWORD,
+} from './certs';
 export { run, createFailError, createFlagError, combineErrors, isFailError, Flags } from './run';
 export { REPO_ROOT } from './constants';
 export { KbnClient } from './kbn_client';
