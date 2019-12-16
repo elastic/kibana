180d17d72777a603497f2b0989c97b305a75c6b7
spalger <spalger@users.noreply.github.com>
2019-12-16 10:34:53 -0700

    update other changes css-loader config options

--- x-pack/legacy/plugins/canvas/shareable_runtime/webpack.config.js
+++ x-pack/legacy/plugins/canvas/shareable_runtime/webpack.config.js
@@ -99,10 +99,11 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
               localsConvention: 'camelCaseOnly',
               sourceMap: !isProd,
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]',
+              },
             },
           },
           {
