b6e77eb98ed34dba7811ee78386bebaa076ae141
Brittany Joiner <brittanyjoiner15@gmail.com>
2019-12-16 13:02:04 -0500

    adding span sync badge on waterfall and flyout

---
+++ x-pack/legacy/plugins/apm/public/components/app/TransactionDetails/WaterfallWithSummmary/WaterfallContainer/Waterfall/SyncBadge.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { EuiBadge } from '@elastic/eui';
+import React from 'react';
+import styled from 'styled-components';
+import { px, units } from '../../../../../../style/variables';
+
+const SpanBadge = styled(EuiBadge)`
+  display: inline-block;
+  margin-right: ${px(units.quarter)};
+`;
+
+interface SyncBadgeProps {
+  sync?: boolean;
+}
+
+export function SyncBadge({ sync }: SyncBadgeProps) {
+  switch (sync) {
+    case true:
+      return <SpanBadge>{'blocking'}</SpanBadge>;
+    case false:
+      return <SpanBadge>{'async'}</SpanBadge>;
+    default:
+      return null;
+  }
+}
