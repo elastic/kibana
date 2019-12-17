1bb1eef1c55d576bd22dfef872efcf37688bafa5
shahzad <shahzad31comp@gmail.com>
2019-12-17 17:34:54 +0100

    fix type

--- x-pack/legacy/plugins/uptime/public/components/functional/monitor_list/monitor_list_actions_popover.tsx
+++ x-pack/legacy/plugins/uptime/public/components/functional/monitor_list/monitor_list_actions_popover.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiButtonIcon, EuiPopover, EuiButton } from '@elastic/eui';
+import { EuiPopover, EuiButton } from '@elastic/eui';
 import React, { useContext } from 'react';
 import { i18n } from '@kbn/i18n';
 import { get } from 'lodash';
