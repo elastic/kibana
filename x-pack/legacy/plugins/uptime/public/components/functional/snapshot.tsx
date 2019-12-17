ec1c3ff28690bf9c022f6a60db32b6e8f73308ab
shahzad <shahzad31comp@gmail.com>
2019-12-17 21:57:13 +0100

    ignore typcehck

--- x-pack/legacy/plugins/uptime/public/components/functional/snapshot.tsx
+++ x-pack/legacy/plugins/uptime/public/components/functional/snapshot.tsx
@@ -124,8 +124,8 @@ const mapDispatchToProps = (dispatch: any) => ({
   },
 });
 
-// @ts-ignore connect is expecting null | undefined for some reason
 export const Snapshot = connect<StoreProps, DispatchProps, OwnProps>(
+  // @ts-ignore connect is expecting null | undefined for some reason
   mapStateToProps,
   mapDispatchToProps
 )(Container);
