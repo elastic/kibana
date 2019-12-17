71defb400d4d109d3fffbd99c49688c89462e542
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2019-12-17 10:43:47 +0100

    Migrate import from 'kibana/server' to 'kibana/public'

--- x-pack/legacy/plugins/graph/public/angular/services/saved_workspace_references.ts
+++ x-pack/legacy/plugins/graph/public/angular/services/saved_workspace_references.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { SavedObjectAttributes, SavedObjectReference } from 'kibana/server';
+import { SavedObjectAttributes, SavedObjectReference } from 'kibana/public';
 import { SavedWorkspace } from './saved_workspace';
 
 export function extractReferences({
