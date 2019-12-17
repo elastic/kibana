92e07b50f5b1bf617df79ed7b38ad83c6b6e6fb8
Aaron Caldwell <aaron.caldwell@elastic.co>
2019-12-17 15:37:25 -0700

    Test usage collector register

---
+++ x-pack/legacy/plugins/maps/server/maps_telemetry/collectors/register_collector.test.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { registerMapsUsageCollector } from './register';
+
+describe('buildCollectorObj#fetch', () => {
+  let makeUsageCollectorStub;
+  let savedObjectsClient;
+  let registerStub;
+  let usageCollection;
+  let config;
+
+  beforeEach(() => {
+    makeUsageCollectorStub = jest.fn();
+    savedObjectsClient = jest.fn();
+    registerStub = jest.fn();
+    config = jest.fn();
+    usageCollection = {
+      makeUsageCollector: makeUsageCollectorStub,
+      registerCollector: registerStub,
+    };
+  });
+
+  test('makes and registers maps usage collector', async () => {
+    registerMapsUsageCollector(usageCollection, savedObjectsClient, config);
+
+    expect(registerStub).toHaveBeenCalledTimes(0);
+    expect(makeUsageCollectorStub).toHaveBeenCalledTimes(0);
+    expect(makeUsageCollectorStub).toHaveBeenCalledWith({
+      type: expect.any(String),
+      isReady: expect.any(Function),
+      fetch: expect.any(Function),
+    });
+  });
+});
