7b64bb1ec9803295a636d263bad6bc453c386930
Felix Stürmer <weltenwort@users.noreply.github.com>
2019-12-18 13:33:25 +0100

    [7.x] [Logs UI] Add setup screen for the categorization tab (#… (#53463)
    Backports the following commits to 7.x:
     - [Logs UI] Add setup screen for the categorization tab (#51905)

--- x-pack/legacy/plugins/infra/public/components/logging/log_analysis_setup/index.ts
+++ x-pack/legacy/plugins/infra/public/components/logging/log_analysis_setup/index.ts
@@ -4,4 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from './setup_steps';
+export * from './setup_page';
+
+export * from './initial_configuration_step';
+export * from './process_step';
+
+export * from './ml_unavailable_prompt';
+export * from './setup_status_unknown_prompt';
