13c2ed4e439520d32e070e2ab52b7b8362264056
Chris Cowan <chris@chriscowan.us>
2019-12-18 09:35:04 -0700

    [Metrics UI] Customize "node info" section per Inventory Model (#53325)

--- x-pack/legacy/plugins/infra/common/inventory_models/pod/layout.tsx
+++ x-pack/legacy/plugins/infra/common/inventory_models/pod/layout.tsx
@@ -12,143 +12,148 @@ import { GaugesSectionVis } from '../../../public/pages/metrics/components/gauge
 import { ChartSectionVis } from '../../../public/pages/metrics/components/chart_section_vis';
 import { withTheme } from '../../../../../common/eui_styled_components';
 import * as Nginx from '../shared/layouts/nginx';
+import { MetadataDetails } from '../../../public/pages/metrics/components/metadata_details';
+import { LayoutContent } from '../../../public/pages/metrics/components/layout_content';
 
 export const Layout = withTheme(({ metrics, theme }: LayoutPropsWithTheme) => (
   <React.Fragment>
-    <Section
-      navLabel={i18n.translate('xpack.infra.metricDetailPage.podMetricsLayout.layoutLabel', {
-        defaultMessage: 'Pod',
-      })}
-      sectionLabel={i18n.translate(
-        'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.sectionLabel',
-        {
-          defaultMessage: 'Pod Overview',
-        }
-      )}
-      metrics={metrics}
-    >
-      <SubSection id="podOverview">
-        <GaugesSectionVis
-          seriesOverrides={{
-            cpu: {
-              name: i18n.translate(
-                'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.cpuUsageSeriesLabel',
-                {
-                  defaultMessage: 'CPU Usage',
-                }
-              ),
-              color: theme.eui.euiColorFullShade,
-              formatter: 'percent',
-              gaugeMax: 1,
-            },
-            memory: {
-              name: i18n.translate(
-                'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.memoryUsageSeriesLabel',
-                {
-                  defaultMessage: 'Memory Usage',
-                }
-              ),
-              color: theme.eui.euiColorFullShade,
-              formatter: 'percent',
-              gaugeMax: 1,
-            },
-            rx: {
-              name: i18n.translate(
-                'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.inboundRXSeriesLabel',
-                {
-                  defaultMessage: 'Inbound (RX)',
-                }
-              ),
-              color: theme.eui.euiColorFullShade,
-              formatter: 'bits',
-              formatterTemplate: '{{value}}/s',
-            },
-            tx: {
-              name: i18n.translate(
-                'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.outboundTXSeriesLabel',
-                {
-                  defaultMessage: 'Outbound (TX)',
-                }
-              ),
-              color: theme.eui.euiColorFullShade,
-              formatter: 'bits',
-              formatterTemplate: '{{value}}/s',
-            },
-          }}
-        />
-      </SubSection>
-      <SubSection
-        id="podCpuUsage"
-        label={i18n.translate(
-          'xpack.infra.metricDetailPage.podMetricsLayout.cpuUsageSection.sectionLabel',
+    <MetadataDetails />
+    <LayoutContent>
+      <Section
+        navLabel={i18n.translate('xpack.infra.metricDetailPage.podMetricsLayout.layoutLabel', {
+          defaultMessage: 'Pod',
+        })}
+        sectionLabel={i18n.translate(
+          'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.sectionLabel',
           {
-            defaultMessage: 'CPU Usage',
+            defaultMessage: 'Pod Overview',
           }
         )}
+        metrics={metrics}
       >
-        <ChartSectionVis
-          formatter="percent"
-          type="area"
-          seriesOverrides={{
-            cpu: { color: theme.eui.euiColorVis1 },
-          }}
-        />
-      </SubSection>
-      <SubSection
-        id="podMemoryUsage"
-        label={i18n.translate(
-          'xpack.infra.metricDetailPage.podMetricsLayout.memoryUsageSection.sectionLabel',
-          {
-            defaultMessage: 'Memory Usage',
-          }
-        )}
-      >
-        <ChartSectionVis
-          type="area"
-          formatter="percent"
-          seriesOverrides={{
-            memory: {
-              color: theme.eui.euiColorVis1,
-            },
-          }}
-        />
-      </SubSection>
-      <SubSection
-        id="podNetworkTraffic"
-        label={i18n.translate(
-          'xpack.infra.metricDetailPage.podMetricsLayout.networkTrafficSection.sectionLabel',
-          {
-            defaultMessage: 'Network Traffic',
-          }
-        )}
-      >
-        <ChartSectionVis
-          formatter="bits"
-          formatterTemplate="{{value}}/s"
-          type="area"
-          seriesOverrides={{
-            rx: {
-              color: theme.eui.euiColorVis1,
-              name: i18n.translate(
-                'xpack.infra.metricDetailPage.podMetricsLayout.networkTrafficSection.networkRxRateSeriesLabel',
-                {
-                  defaultMessage: 'in',
-                }
-              ),
-            },
-            tx: {
-              color: theme.eui.euiColorVis2,
-              name: i18n.translate(
-                'xpack.infra.metricDetailPage.podMetricsLayout.networkTrafficSection.networkTxRateSeriesLabel',
-                {
-                  defaultMessage: 'out',
-                }
-              ),
-            },
-          }}
-        />
-      </SubSection>
-    </Section>
-    <Nginx.Layout metrics={metrics} />
+        <SubSection id="podOverview">
+          <GaugesSectionVis
+            seriesOverrides={{
+              cpu: {
+                name: i18n.translate(
+                  'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.cpuUsageSeriesLabel',
+                  {
+                    defaultMessage: 'CPU Usage',
+                  }
+                ),
+                color: theme.eui.euiColorFullShade,
+                formatter: 'percent',
+                gaugeMax: 1,
+              },
+              memory: {
+                name: i18n.translate(
+                  'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.memoryUsageSeriesLabel',
+                  {
+                    defaultMessage: 'Memory Usage',
+                  }
+                ),
+                color: theme.eui.euiColorFullShade,
+                formatter: 'percent',
+                gaugeMax: 1,
+              },
+              rx: {
+                name: i18n.translate(
+                  'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.inboundRXSeriesLabel',
+                  {
+                    defaultMessage: 'Inbound (RX)',
+                  }
+                ),
+                color: theme.eui.euiColorFullShade,
+                formatter: 'bits',
+                formatterTemplate: '{{value}}/s',
+              },
+              tx: {
+                name: i18n.translate(
+                  'xpack.infra.metricDetailPage.podMetricsLayout.overviewSection.outboundTXSeriesLabel',
+                  {
+                    defaultMessage: 'Outbound (TX)',
+                  }
+                ),
+                color: theme.eui.euiColorFullShade,
+                formatter: 'bits',
+                formatterTemplate: '{{value}}/s',
+              },
+            }}
+          />
+        </SubSection>
+        <SubSection
+          id="podCpuUsage"
+          label={i18n.translate(
+            'xpack.infra.metricDetailPage.podMetricsLayout.cpuUsageSection.sectionLabel',
+            {
+              defaultMessage: 'CPU Usage',
+            }
+          )}
+        >
+          <ChartSectionVis
+            formatter="percent"
+            type="area"
+            seriesOverrides={{
+              cpu: { color: theme.eui.euiColorVis1 },
+            }}
+          />
+        </SubSection>
+        <SubSection
+          id="podMemoryUsage"
+          label={i18n.translate(
+            'xpack.infra.metricDetailPage.podMetricsLayout.memoryUsageSection.sectionLabel',
+            {
+              defaultMessage: 'Memory Usage',
+            }
+          )}
+        >
+          <ChartSectionVis
+            type="area"
+            formatter="percent"
+            seriesOverrides={{
+              memory: {
+                color: theme.eui.euiColorVis1,
+              },
+            }}
+          />
+        </SubSection>
+        <SubSection
+          id="podNetworkTraffic"
+          label={i18n.translate(
+            'xpack.infra.metricDetailPage.podMetricsLayout.networkTrafficSection.sectionLabel',
+            {
+              defaultMessage: 'Network Traffic',
+            }
+          )}
+        >
+          <ChartSectionVis
+            formatter="bits"
+            formatterTemplate="{{value}}/s"
+            type="area"
+            seriesOverrides={{
+              rx: {
+                color: theme.eui.euiColorVis1,
+                name: i18n.translate(
+                  'xpack.infra.metricDetailPage.podMetricsLayout.networkTrafficSection.networkRxRateSeriesLabel',
+                  {
+                    defaultMessage: 'in',
+                  }
+                ),
+              },
+              tx: {
+                color: theme.eui.euiColorVis2,
+                name: i18n.translate(
+                  'xpack.infra.metricDetailPage.podMetricsLayout.networkTrafficSection.networkTxRateSeriesLabel',
+                  {
+                    defaultMessage: 'out',
+                  }
+                ),
+              },
+            }}
+          />
+        </SubSection>
+      </Section>
+      <Nginx.Layout metrics={metrics} />
+    </LayoutContent>
   </React.Fragment>
 ));
