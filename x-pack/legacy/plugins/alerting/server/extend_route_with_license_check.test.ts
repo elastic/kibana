c2515cf1f2879d479842dfc7dca5a70566f591c6
Yuliia Naumenko <jo.naumenko@gmail.com>
2019-12-19 11:31:31 -0800

    Update x-pack/legacy/plugins/alerting/server/extend_route_with_license_check.test.ts
    Co-Authored-By: Gidi Meir Morris <github@gidi.io>

--- x-pack/legacy/plugins/alerting/server/extend_route_with_license_check.test.ts
+++ x-pack/legacy/plugins/alerting/server/extend_route_with_license_check.test.ts
@@ -20,7 +20,7 @@ describe('extendRouteWithLicenseCheck', () => {
       );
       expect(newRoute.config.pre.length > 0);
     });
-    test('extends route object with license check uder config.pro, if config property not exists', () => {
+    test('extends route object with license check under config.pre, if config property not exists', () => {
       const newRoute = extendRouteWithLicenseCheck({ someProperty: 'test' }, licenseState);
       expect(newRoute.config.pre.length > 0);
     });
