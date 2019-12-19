b0ead5e026f570cf9318f774acc1fa5a91fdf23d
FrankHassanabad <frank.hassanabad@elastic.co>
2019-12-18 20:58:39 -0700

    text addition

--- x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/schemas/query_rules_bulk_schema.test.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/schemas/query_rules_bulk_schema.test.ts
@@ -7,6 +7,9 @@
 import { queryRulesBulkSchema } from './query_rules_bulk_schema';
 import { UpdateRuleAlertParamsRest } from '../../rules/types';
 
+// only the basics of testing are here.
+// see: query_rules_bulk_schema.test.ts for the bulk of the validation tests
+// this this just wraps queryRulesSchema in an array
 describe('query_rules_bulk_schema', () => {
   test('can take an empty array and validate it', () => {
     expect(
