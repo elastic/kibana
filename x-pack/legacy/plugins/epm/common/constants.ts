0b15c182b3a2bae8a1e29e52a8df8c7f4d41bada
John Schulz <john.schulz@elastic.co>
2019-12-20 09:28:50 -0500

    Delete EPM's mapping of datasources saved object. Ingest handles that.

--- x-pack/legacy/plugins/epm/common/constants.ts
+++ x-pack/legacy/plugins/epm/common/constants.ts
@@ -18,4 +18,6 @@ export const PLUGIN = {
 };
 
 export const SAVED_OBJECT_TYPE_PACKAGES = 'epm-package';
-export const SAVED_OBJECT_TYPE_DATASOURCES = 'datasources'; // From Ingest
+// This is actually controled by Ingest
+// Ultimately, EPM should a) import this or b) not know about it at all
+export const SAVED_OBJECT_TYPE_DATASOURCES = 'datasources';
