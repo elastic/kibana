26021d8632369e7e402bc3ec87bb28830395e9a7
kevinlog <kevin.logan@elastic.co>
2019-12-20 07:55:16 -0500

    fix type check

--- x-pack/plugins/endpoint/public/plugin.ts
+++ x-pack/plugins/endpoint/public/plugin.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Plugin, CoreSetup } from 'kibana/public';
+import { Plugin, CoreSetup, AppMountParameters } from 'kibana/public';
 import { IEmbeddableSetup } from 'src/plugins/embeddable/public';
 import { i18n } from '@kbn/i18n';
 import { ResolverEmbeddableFactory } from './embeddables/resolver';
@@ -32,7 +32,7 @@ export class EndpointPlugin
       title: i18n.translate('xpack.endpoint.pluginTitle', {
         defaultMessage: 'Endpoint',
       }),
-      async mount(params) {
+      async mount(params: AppMountParameters) {
         const [coreStart] = await core.getStartServices();
         const { renderApp } = await import('./applications/endpoint');
         return renderApp(coreStart, params);
