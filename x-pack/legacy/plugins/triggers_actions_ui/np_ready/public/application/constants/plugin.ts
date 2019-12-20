db143438876ee3736f0baa00455507de68f1bd3f
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2019-12-20 10:40:55 -0800

    Fixed language check failing

--- x-pack/legacy/plugins/triggers_actions_ui/np_ready/public/application/constants/plugin.ts
+++ x-pack/legacy/plugins/triggers_actions_ui/np_ready/public/application/constants/plugin.ts
@@ -7,7 +7,7 @@
 export const PLUGIN = {
   ID: 'triggers_actions_ui',
   getI18nName: (i18n: any): string => {
-    return i18n.translate('xpack.triggers_actions_ui.appName', {
+    return i18n.translate('xpack.triggersActionsUI.appName', {
       defaultMessage: 'Alerts and Actions',
     });
   },
