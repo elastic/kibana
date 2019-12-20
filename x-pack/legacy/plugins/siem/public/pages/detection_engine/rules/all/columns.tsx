0a574034b50e8830c7b2a3b48bfef641eda5cf5a
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2019-12-20 12:49:00 -0500

    [SIEM] [DETECTION ENGINE] Details and Edit view for a rule (#53252) (#53698)
    * re-structure detection engine + change routing name
    
    * add editing/details feature for a rule
    add feature to not edit immutable rule
    
    * review I
    
    * review II
    
    * change constant
    
    * review III

---
+++ x-pack/legacy/plugins/siem/public/pages/detection_engine/rules/all/columns.tsx
@@ -0,0 +1,169 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { EuiBadge, EuiHealth, EuiIconTip, EuiLink, EuiTextColor } from '@elastic/eui';
+import * as H from 'history';
+import React from 'react';
+import euiLightVars from '@elastic/eui/dist/eui_theme_light.json';
+import { getEmptyTagValue } from '../../../../components/empty_value';
+import {
+  deleteRulesAction,
+  duplicateRuleAction,
+  editRuleAction,
+  exportRulesAction,
+  runRuleAction,
+} from './actions';
+
+import { Action } from './reducer';
+import { TableData } from '../types';
+import * as i18n from '../translations';
+import { PreferenceFormattedDate } from '../../../../components/formatted_date';
+import { RuleSwitch } from '../components/rule_switch';
+
+const getActions = (dispatch: React.Dispatch<Action>, kbnVersion: string, history: H.History) => [
+  {
+    description: i18n.EDIT_RULE_SETTINGS,
+    icon: 'visControls',
+    name: i18n.EDIT_RULE_SETTINGS,
+    onClick: (rowItem: TableData) => editRuleAction(rowItem.sourceRule, history),
+    enabled: (rowItem: TableData) => !rowItem.sourceRule.immutable,
+  },
+  {
+    description: i18n.RUN_RULE_MANUALLY,
+    icon: 'play',
+    name: i18n.RUN_RULE_MANUALLY,
+    onClick: runRuleAction,
+    enabled: () => false,
+  },
+  {
+    description: i18n.DUPLICATE_RULE,
+    icon: 'copy',
+    name: i18n.DUPLICATE_RULE,
+    onClick: (rowItem: TableData) => duplicateRuleAction(rowItem.sourceRule, dispatch, kbnVersion),
+  },
+  {
+    description: i18n.EXPORT_RULE,
+    icon: 'exportAction',
+    name: i18n.EXPORT_RULE,
+    onClick: (rowItem: TableData) => exportRulesAction([rowItem.sourceRule], dispatch),
+  },
+  {
+    description: i18n.DELETE_RULE,
+    icon: 'trash',
+    name: i18n.DELETE_RULE,
+    onClick: (rowItem: TableData) => deleteRulesAction([rowItem.id], dispatch, kbnVersion),
+  },
+];
+
+// Michael: Are we able to do custom, in-table-header filters, as shown in my wireframes?
+export const getColumns = (
+  dispatch: React.Dispatch<Action>,
+  kbnVersion: string,
+  history: H.History
+) => [
+  {
+    field: 'rule',
+    name: i18n.COLUMN_RULE,
+    render: (value: TableData['rule']) => <EuiLink href={value.href}>{value.name}</EuiLink>,
+    truncateText: true,
+    width: '24%',
+  },
+  {
+    field: 'method',
+    name: i18n.COLUMN_METHOD,
+    truncateText: true,
+  },
+  {
+    field: 'severity',
+    name: i18n.COLUMN_SEVERITY,
+    render: (value: TableData['severity']) => (
+      <EuiHealth
+        color={
+          value === 'low'
+            ? euiLightVars.euiColorVis0
+            : value === 'medium'
+            ? euiLightVars.euiColorVis5
+            : value === 'high'
+            ? euiLightVars.euiColorVis7
+            : euiLightVars.euiColorVis9
+        }
+      >
+        {value}
+      </EuiHealth>
+    ),
+    truncateText: true,
+  },
+  {
+    field: 'lastCompletedRun',
+    name: i18n.COLUMN_LAST_COMPLETE_RUN,
+    render: (value: TableData['lastCompletedRun']) => {
+      return value == null ? (
+        getEmptyTagValue()
+      ) : (
+        <PreferenceFormattedDate value={new Date(value)} />
+      );
+    },
+    sortable: true,
+    truncateText: true,
+    width: '16%',
+  },
+  {
+    field: 'lastResponse',
+    name: i18n.COLUMN_LAST_RESPONSE,
+    render: (value: TableData['lastResponse']) => {
+      return value == null ? (
+        getEmptyTagValue()
+      ) : (
+        <>
+          {value.type === 'Fail' ? (
+            <EuiTextColor color="danger">
+              {value.type} <EuiIconTip content={value.message} type="iInCircle" />
+            </EuiTextColor>
+          ) : (
+            <EuiTextColor color="secondary">{value.type}</EuiTextColor>
+          )}
+        </>
+      );
+    },
+    truncateText: true,
+  },
+  {
+    field: 'tags',
+    name: i18n.COLUMN_TAGS,
+    render: (value: TableData['tags']) => (
+      <div>
+        <>
+          {value.map((tag, i) => (
+            <EuiBadge color="hollow" key={i}>
+              {tag}
+            </EuiBadge>
+          ))}
+        </>
+      </div>
+    ),
+    truncateText: true,
+    width: '20%',
+  },
+  {
+    align: 'center',
+    field: 'activate',
+    name: i18n.COLUMN_ACTIVATE,
+    render: (value: TableData['activate'], item: TableData) => (
+      <RuleSwitch
+        dispatch={dispatch}
+        id={item.id}
+        enabled={item.activate}
+        isLoading={item.isLoading}
+      />
+    ),
+    sortable: true,
+    width: '85px',
+  },
+  {
+    actions: getActions(dispatch, kbnVersion, history),
+    width: '40px',
+  },
+];
