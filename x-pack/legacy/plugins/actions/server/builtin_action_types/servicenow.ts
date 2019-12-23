f535c0d7cd5bca0b16a5e7bccca3a3e2b244841c
stephmilovic <stephanie.milovic@elastic.co>
2019-12-23 15:31:25 -0600

    can now post to service now via kibana action

--- x-pack/legacy/plugins/actions/server/builtin_action_types/servicenow.ts
+++ x-pack/legacy/plugins/actions/server/builtin_action_types/servicenow.ts
@@ -7,11 +7,6 @@
 import { curry } from 'lodash';
 import { i18n } from '@kbn/i18n';
 import { schema, TypeOf } from '@kbn/config-schema';
-import { IncomingWebhook, IncomingWebhookResult } from '@serviceNow/webhook';
-import { pipe } from 'fp-ts/lib/pipeable';
-import { map, getOrElse } from 'fp-ts/lib/Option';
-import { getRetryAfterIntervalFromHeaders } from './lib/http_rersponse_retry_header';
-
 import {
   ActionType,
   ActionTypeExecutorOptions,
@@ -19,6 +14,7 @@ import {
   ExecutorType,
 } from '../types';
 import { ActionsConfigurationUtilities } from '../actions_config';
+import { postServiceNow } from './lib/post_servicenow';
 
 // config definition
 export type ConfigType = TypeOf<typeof ConfigSchema>;
@@ -53,7 +49,7 @@ function validateConfig(
 export type SecretsType = TypeOf<typeof SecretsSchema>;
 const SecretsSchemaProps = {
   password: schema.string(),
-  user: schema.string(),
+  username: schema.string(),
 };
 
 const SecretsSchema = schema.object(SecretsSchemaProps);
@@ -62,9 +58,9 @@ function validateSecrets(
   configurationUtilities: ActionsConfigurationUtilities,
   secrets: SecretsType
 ) {
-  if (secrets.user == null) {
+  if (secrets.username == null) {
     return i18n.translate('xpack.actions.builtin.servicenow.servicenowApiUserError', {
-      defaultMessage: 'error configuring servicenow action: no secrets [user] provided',
+      defaultMessage: 'error configuring servicenow action: no secrets [username] provided',
     });
   }
   if (secrets.password == null) {
@@ -117,132 +113,58 @@ async function serviceNowExecutor(
   const secrets = execOptions.secrets as SecretsType;
   const params = execOptions.params as ParamsType;
 
-  let result: IncomingWebhookResult;
-  const { apiUrl } = config;
-  const { user, password } = secrets;
-  const { comments, short_description } = params;
   const headers = {
+    Accept: 'application/json',
     'Content-Type': 'application/json',
   };
-  console.log('HOORAYHOORAYHOORAY WE ARE HERE!')
-  return 'HOORAY WE ARE HERE!';
+  let response;
+  try {
+    response = await postServiceNow({ apiUrl: config.apiUrl, data: params, headers, secrets });
+  } catch (err) {
+    const message = i18n.translate('xpack.actions.builtin.servicenow.postingErrorMessage', {
+      defaultMessage: 'error posting servicenow event',
+    });
+    return {
+      status: 'error',
+      actionId,
+      message,
+      serviceMessage: err.message,
+    };
+  }
+  if (response.status === 201) {
+    return {
+      status: 'ok',
+      actionId,
+      data: response.data,
+    };
+  }
 
-  //
-  //
-  // try {
-  //   const webhook = new IncomingWebhook(apiUrl);
-  //   result = await webhook.send(message);
-  // } catch (err) {
-  //   if (err.original == null || err.original.response == null) {
-  //     return serviceErrorResult(actionId, err.message);
-  //   }
-  //
-  //   const { status, statusText, headers } = err.original.response;
-  //
-  //   // special handling for 5xx
-  //   if (status >= 500) {
-  //     return retryResult(actionId, err.message);
-  //   }
-  //
-  //   // special handling for rate limiting
-  //   if (status === 429) {
-  //     return pipe(
-  //       getRetryAfterIntervalFromHeaders(headers),
-  //       map(retry => retryResultSeconds(actionId, err.message, retry)),
-  //       getOrElse(() => retryResult(actionId, err.message))
-  //     );
-  //   }
-  //
-  //   const errMessage = i18n.translate(
-  //     'xpack.actions.builtin.serviceNow.unexpectedHttpResponseErrorMessage',
-  //     {
-  //       defaultMessage: 'unexpected http response from serviceNow: {httpStatus} {httpStatusText}',
-  //       values: {
-  //         httpStatus: status,
-  //         httpStatusText: statusText,
-  //       },
-  //     }
-  //   );
-  //   return errorResult(actionId, errMessage);
-  // }
-  //
-  // if (result == null) {
-  //   const errMessage = i18n.translate(
-  //     'xpack.actions.builtin.serviceNow.unexpectedNullResponseErrorMessage',
-  //     {
-  //       defaultMessage: 'unexpected null response from serviceNow',
-  //     }
-  //   );
-  //   return errorResult(actionId, errMessage);
-  // }
-  //
-  // if (result.text !== 'ok') {
-  //   return serviceErrorResult(actionId, result.text);
-  // }
-  //
-  // return successResult(actionId, result);
-}
-
-function successResult(actionId: string, data: any): ActionTypeExecutorResult {
-  return { status: 'ok', data, actionId };
-}
-
-function errorResult(actionId: string, message: string): ActionTypeExecutorResult {
-  return {
-    status: 'error',
-    message,
-    actionId,
-  };
-}
-function serviceErrorResult(actionId: string, serviceMessage: string): ActionTypeExecutorResult {
-  const errMessage = i18n.translate('xpack.actions.builtin.serviceNow.errorPostingErrorMessage', {
-    defaultMessage: 'error posting serviceNow message',
-  });
-  return {
-    status: 'error',
-    message: errMessage,
-    actionId,
-    serviceMessage,
-  };
-}
-
-function retryResult(actionId: string, message: string): ActionTypeExecutorResult {
-  const errMessage = i18n.translate(
-    'xpack.actions.builtin.serviceNow.errorPostingRetryLaterErrorMessage',
-    {
-      defaultMessage: 'error posting a serviceNow message, retry later',
-    }
-  );
-  return {
-    status: 'error',
-    message: errMessage,
-    retry: true,
-    actionId,
-  };
-}
-
-function retryResultSeconds(
-  actionId: string,
-  message: string,
-  retryAfter: number
-): ActionTypeExecutorResult {
-  const retryEpoch = Date.now() + retryAfter * 1000;
-  const retry = new Date(retryEpoch);
-  const retryString = retry.toISOString();
-  const errMessage = i18n.translate(
-    'xpack.actions.builtin.serviceNow.errorPostingRetryDateErrorMessage',
-    {
-      defaultMessage: 'error posting a serviceNow message, retry at {retryString}',
+  if (response.status === 429 || response.status >= 500) {
+    const message = i18n.translate('xpack.actions.builtin.servicenow.postingRetryErrorMessage', {
+      defaultMessage: 'error posting servicenow event: http status {status}, retry later',
       values: {
-        retryString,
+        status: response.status,
       },
-    }
-  );
+    });
+
+    return {
+      status: 'error',
+      actionId,
+      message,
+      retry: true,
+    };
+  }
+
+  const message = i18n.translate('xpack.actions.builtin.servicenow.postingUnexpectedErrorMessage', {
+    defaultMessage: 'error posting servicenow event: unexpected status {status}',
+    values: {
+      status: response.status,
+    },
+  });
+
   return {
     status: 'error',
-    message: errMessage,
-    retry,
     actionId,
-    serviceMessage: message,
+    message,
   };
 }
