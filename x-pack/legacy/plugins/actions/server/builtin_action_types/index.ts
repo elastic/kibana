5f92caee7a3eaa12a7d19968ddfd4d8768e08170
stephmilovic <stephanie.milovic@elastic.co>
2019-12-23 11:45:22 -0600

    working on servicenow action

--- x-pack/legacy/plugins/actions/server/builtin_action_types/index.ts
+++ x-pack/legacy/plugins/actions/server/builtin_action_types/index.ts
@@ -8,26 +8,28 @@ import { ActionTypeRegistry } from '../action_type_registry';
 import { ActionsConfigurationUtilities } from '../actions_config';
 import { Logger } from '../../../../../../src/core/server';
 
-import { getActionType as getServerLogActionType } from './server_log';
-import { getActionType as getSlackActionType } from './slack';
 import { getActionType as getEmailActionType } from './email';
 import { getActionType as getIndexActionType } from './es_index';
 import { getActionType as getPagerDutyActionType } from './pagerduty';
+import { getActionType as getServerLogActionType } from './server_log';
+import { getActionType as getServiceNowActionType } from './servicenow';
+import { getActionType as getSlackActionType } from './slack';
 import { getActionType as getWebhookActionType } from './webhook';
 
 export function registerBuiltInActionTypes({
-  logger,
-  actionTypeRegistry,
   actionsConfigUtils: configurationUtilities,
+  actionTypeRegistry,
+  logger,
 }: {
-  logger: Logger;
-  actionTypeRegistry: ActionTypeRegistry;
   actionsConfigUtils: ActionsConfigurationUtilities;
+  actionTypeRegistry: ActionTypeRegistry;
+  logger: Logger;
 }) {
-  actionTypeRegistry.register(getServerLogActionType({ logger }));
-  actionTypeRegistry.register(getSlackActionType({ configurationUtilities }));
   actionTypeRegistry.register(getEmailActionType({ logger, configurationUtilities }));
   actionTypeRegistry.register(getIndexActionType({ logger }));
   actionTypeRegistry.register(getPagerDutyActionType({ logger, configurationUtilities }));
+  actionTypeRegistry.register(getServerLogActionType({ logger }));
+  actionTypeRegistry.register(getServiceNowActionType({ configurationUtilities }));
+  actionTypeRegistry.register(getSlackActionType({ configurationUtilities }));
   actionTypeRegistry.register(getWebhookActionType({ logger, configurationUtilities }));
 }
