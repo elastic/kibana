6c555d854cc1eca4a59e11fbe1ddcbfa2864cce2
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-12-23 13:15:57 -0500

    Unshims NP server plugin

--- x-pack/plugins/infra/server/plugin.ts
+++ x-pack/plugins/infra/server/plugin.ts
@@ -4,30 +4,134 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Plugin, PluginInitializerContext } from 'src/core/server';
+import { CoreSetup, PluginInitializerContext } from 'src/core/server';
+import { Server } from 'hapi';
+import { schema, TypeOf } from '@kbn/config-schema';
+import { initInfraServer } from './infra_server';
+import { InfraBackendLibs, InfraDomainLibs } from './lib/infra_types';
+import { FrameworkFieldsAdapter } from './lib/adapters/fields/framework_fields_adapter';
+import { KibanaFramework } from './lib/adapters/framework/kibana_framework_adapter';
+import { InfraKibanaLogEntriesAdapter } from './lib/adapters/log_entries/kibana_log_entries_adapter';
+import { KibanaMetricsAdapter } from './lib/adapters/metrics/kibana_metrics_adapter';
+import { InfraElasticsearchSourceStatusAdapter } from './lib/adapters/source_status';
+import { InfraFieldsDomain } from './lib/domains/fields_domain';
+import { InfraLogEntriesDomain } from './lib/domains/log_entries_domain';
+import { InfraMetricsDomain } from './lib/domains/metrics_domain';
+import { InfraLogAnalysis } from './lib/log_analysis';
+import { InfraSnapshot } from './lib/snapshot';
+import { InfraSourceStatus } from './lib/source_status';
+import { InfraSources } from './lib/sources';
+import { InfraServerPluginDeps } from './lib/adapters/framework';
+import { METRICS_FEATURE, LOGS_FEATURE } from './features';
+import { UsageCollector } from './usage/usage_collector';
+import { InfraStaticSourceConfiguration } from './lib/sources/types';
 
-export class InfraPlugin implements Plugin<InfraSetup> {
-  private readonly initContext: PluginInitializerContext;
+export const config = {
+  schema: schema.object({
+    enabled: schema.maybe(schema.boolean()),
+    query: schema.object({
+      partitionSize: schema.maybe(schema.number()),
+      partitionFactor: schema.maybe(schema.number()),
+    }),
+  }),
+};
 
-  constructor(initContext: PluginInitializerContext) {
-    this.initContext = initContext;
-  }
+export type InfraConfig = TypeOf<typeof config.schema>;
 
-  public setup() {
-    return {
-      __legacy: {
-        config: this.initContext.config,
-      },
-    };
-  }
-
-  public start() {}
-  public stop() {}
+export interface KbnServer extends Server {
+  usage: any;
 }
 
-export interface InfraSetup {
-  /** @deprecated */
-  __legacy: {
-    config: PluginInitializerContext['config'];
-  };
+export interface InfraPluginSetup {
+  defineInternalSourceConfiguration: (
+    sourceId: string,
+    sourceProperties: InfraStaticSourceConfiguration
+  ) => void;
+}
+
+const DEFAULT_CONFIG: InfraConfig = {
+  enabled: true,
+  query: {
+    partitionSize: 75,
+    partitionFactor: 1.2,
+  },
+};
+
+export class InfraServerPlugin {
+  public config: InfraConfig = DEFAULT_CONFIG;
+  public libs: InfraBackendLibs | undefined;
+
+  constructor(context: PluginInitializerContext) {
+    const config$ = context.config.create<InfraConfig>();
+    config$.subscribe(configValue => {
+      this.config = {
+        ...DEFAULT_CONFIG,
+        enabled: configValue.enabled,
+        query: {
+          ...DEFAULT_CONFIG.query,
+          ...configValue.query,
+        },
+      };
+    });
+  }
+
+  getLibs() {
+    if (!this.libs) {
+      throw new Error('libs not set up yet');
+    }
+    return this.libs;
+  }
+
+  setup(core: CoreSetup, plugins: InfraServerPluginDeps) {
+    const framework = new KibanaFramework(core, this.config, plugins);
+    const sources = new InfraSources({
+      config: this.config,
+    });
+    const sourceStatus = new InfraSourceStatus(
+      new InfraElasticsearchSourceStatusAdapter(framework),
+      {
+        sources,
+      }
+    );
+    const snapshot = new InfraSnapshot({ sources, framework });
+    const logAnalysis = new InfraLogAnalysis({ framework });
+
+    // TODO: separate these out individually and do away with "domains" as a temporary group
+    const domainLibs: InfraDomainLibs = {
+      fields: new InfraFieldsDomain(new FrameworkFieldsAdapter(framework), {
+        sources,
+      }),
+      logEntries: new InfraLogEntriesDomain(new InfraKibanaLogEntriesAdapter(framework), {
+        sources,
+      }),
+      metrics: new InfraMetricsDomain(new KibanaMetricsAdapter(framework)),
+    };
+
+    this.libs = {
+      configuration: this.config,
+      framework,
+      logAnalysis,
+      snapshot,
+      sources,
+      sourceStatus,
+      ...domainLibs,
+    };
+
+    plugins.features.registerFeature(METRICS_FEATURE);
+    plugins.features.registerFeature(LOGS_FEATURE);
+
+    initInfraServer(this.libs);
+
+    // Telemetry
+    UsageCollector.registerUsageCollector(plugins.usageCollection);
+
+    return {
+      defineInternalSourceConfiguration(sourceId, sourceProperties) {
+        sources.defineInternalSourceConfiguration(sourceId, sourceProperties);
+      },
+    } as InfraPluginSetup;
+  }
+
+  start() {}
+  stop() {}
 }
