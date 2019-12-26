e76c7e003b310b801ab547c58b29ed041a687e53
kevinlog <kevin.logan@elastic.co>
2019-12-26 15:32:35 -0500

    linting

--- x-pack/plugins/endpoint/public/applications/endpoint/index.tsx
+++ x-pack/plugins/endpoint/public/applications/endpoint/index.tsx
@@ -8,18 +8,17 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { CoreStart, AppMountParameters } from 'kibana/public';
 import { I18nProvider, FormattedMessage } from '@kbn/i18n/react';
-import { History, createBrowserHistory, createHashHistory } from 'history';
+import { History, createHashHistory } from 'history';
 import { Route, Router, Switch } from 'react-router-dom';
 
 /**
  * This module will be loaded asynchronously to reduce the bundle size of your plugin's main bundle.
  */
-export function renderApp(coreStart: CoreStart, { appBasePath, element }: AppMountParameters) {
+export function renderApp(coreStart: CoreStart, { element }: AppMountParameters) {
   coreStart.http.get('/api/endpoint/hello-world');
 
-  const history = createHashHistory();
-  // const history = createBrowserHistory({ basename: appBasePath })
-  ReactDOM.render(<AppRoot history={history} />, element);
+  const history = createHashHistory()
+  ReactDOM.render(<AppRoot history={history}/>, element);
 
   return () => {
     ReactDOM.unmountComponentAtNode(element);
@@ -30,33 +29,22 @@ interface RouterProps {
   history: History;
 }
 
-const AppRoot: React.FC<RouterProps> = React.memo(({ history }) => (
+const AppRoot: React.FC<RouterProps> = React.memo(({history}) => (
   <I18nProvider>
-    <Router history={history}>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" render={() => (
             <h1 data-test-subj="welcomeTitle">
               <FormattedMessage id="xpack.endpoint.welcomeTitle" defaultMessage="Hello World" />
             </h1>
-          )}
-        />
-        <Route
-          path="/management"
-          render={() => (
-            <h1 data-test-subj="management">
+          )} />
+          <Route path="/management" render={() => (
+            <h1 data-test-subj="management ">
               <FormattedMessage id="xpack.endpoint.management" defaultMessage="Manage Endpoints" />
             </h1>
-          )}
-        />
-        <Route
-          render={() => (
-            <FormattedMessage id="xpack.endpoint.notFound" defaultMessage="Page Not Found" />
-          )}
-        />
-      </Switch>
+          )} />
+          <Route render={() => <FormattedMessage id="xpack.endpoint.notFound" defaultMessage="Page Not Found" />} />
+        </Switch>
     </Router>
   </I18nProvider>
 ));
