7c96d36b344ebf6f7a9b5f967beaa32faf4101e8
Dario Gieselaar <dario.gieselaar@elastic.co>
2019-12-27 23:05:07 +0100

    [APM] Runtime service maps

--- x-pack/legacy/plugins/apm/public/components/app/ServiceMap/index.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/ServiceMap/index.tsx
@@ -5,13 +5,20 @@
  */
 
 import theme from '@elastic/eui/dist/eui_theme_light.json';
-import React from 'react';
-import { useFetcher } from '../../../hooks/useFetcher';
+import React, { useMemo, useEffect, useState } from 'react';
+import { ServiceMapAPIResponse } from '../../../../server/lib/service_map/get_service_map';
+import {
+  Connection,
+  ServiceConnectionNode,
+  ConnectionNode
+} from '../../../../common/service_map';
 import { useLicense } from '../../../hooks/useLicense';
 import { useUrlParams } from '../../../hooks/useUrlParams';
 import { Controls } from './Controls';
 import { Cytoscape } from './Cytoscape';
 import { PlatinumLicensePrompt } from './PlatinumLicensePrompt';
+import { useCallApmApi } from '../../../hooks/useCallApmApi';
+import { useDeepObjectIdentity } from '../../../hooks/useDeepObjectIdentity';
 
 interface ServiceMapProps {
   serviceName?: string;
@@ -37,25 +44,146 @@ ${theme.euiColorLightShade}`,
   margin: `-${theme.gutterTypes.gutterLarge}`
 };
 
+const MAX_REQUESTS = 5;
+
+function getConnectionNodeId(
+  node: ConnectionNode,
+  destMap: Record<string, ServiceConnectionNode> = {}
+): string {
+  if ('destination.address' in node) {
+    const mapped = destMap[node['destination.address']];
+    return mapped
+      ? getConnectionNodeId(mapped, destMap)
+      : node['destination.address'];
+  }
+  return node['service.name'];
+}
+
+function getEdgeId(source: ConnectionNode, destination: ConnectionNode) {
+  return `${getConnectionNodeId(source)}~${getConnectionNodeId(destination)}`;
+}
+
 export function ServiceMap({ serviceName }: ServiceMapProps) {
-  const {
-    urlParams: { start, end }
-  } = useUrlParams();
+  const { urlParams, uiFilters } = useUrlParams();
 
-  const { data } = useFetcher(
-    callApmApi => {
-      if (start && end) {
-        return callApmApi({
-          pathname: '/api/apm/service-map',
-          params: { query: { start, end } }
-        });
-      }
-    },
-    [start, end]
-  );
+  const callApmApi = useCallApmApi();
 
-  const elements = Array.isArray(data) ? data : [];
+  const params = useDeepObjectIdentity({
+    start: urlParams.start,
+    end: urlParams.end,
+    environment: urlParams.environment,
+    serviceName,
+    uiFilters: {
+      ...uiFilters,
+      environment: undefined
+    }
+  });
+
+  const [responses, setResponses] = useState<ServiceMapAPIResponse[]>([]);
+
+  const getNext = (input: { reset?: boolean; after?: string | undefined }) => {
+    const { start, end, uiFilters: strippedUiFilters, ...query } = params;
+
+    if (input.reset) {
+      setResponses([]);
+    }
+
+    if (start && end) {
+      callApmApi({
+        pathname: '/api/apm/service-map',
+        params: {
+          query: {
+            ...query,
+            start,
+            end,
+            uiFilters: JSON.stringify(strippedUiFilters),
+            after: input.after
+          }
+        }
+      }).then(data => {
+        setResponses(resp => resp.concat(data));
+
+        const shouldGetNext = responses.length + 1 < MAX_REQUESTS && data.after;
+
+        if (shouldGetNext) {
+          getNext({ after: data.after });
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    getNext({ reset: true });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params]);
+
+  const elements = useMemo(() => {
+    const destMap = responses.reduce((prev, response) => {
+      return {
+        ...prev,
+        ...response.destinationMap
+      };
+    }, {} as Record<string, ServiceConnectionNode>);
+
+    const nodesById = responses
+      .flatMap(response => response.connections)
+      .reduce((acc, connection) => {
+        const sourceId = getConnectionNodeId(connection.source, destMap);
+        const destId = getConnectionNodeId(connection.destination, destMap);
+
+        return {
+          ...acc,
+          [sourceId]: destMap[sourceId] || connection.source,
+          [destId]: destMap[destId] || connection.destination
+        };
+      }, {} as Record<string, ConnectionNode>);
+
+    const edgesById = responses
+      .flatMap(response => response.connections)
+      .reduce((acc, connection) => {
+        const source =
+          nodesById[getConnectionNodeId(connection.source, destMap)];
+        const destination =
+          nodesById[getConnectionNodeId(connection.destination, destMap)];
+
+        if (source === destination) {
+          return acc;
+        }
+
+        return {
+          ...acc,
+          [getEdgeId(source, destination)]: {
+            source,
+            destination
+          }
+        };
+      }, {} as Record<string, Connection>);
+
+    return [
+      ...(Object.values(nodesById) as ConnectionNode[]).map(node => {
+        return {
+          group: 'nodes' as const,
+          data: {
+            id: getConnectionNodeId(node, destMap),
+            ...('agent.name' in node ? { agentName: node['agent.name'] } : {})
+          }
+        };
+      }),
+      ...(Object.values(edgesById) as Connection[]).map(connection => {
+        return {
+          group: 'edges' as const,
+          data: {
+            id: getEdgeId(connection.source, connection.destination),
+            source: getConnectionNodeId(connection.source, destMap),
+            target: getConnectionNodeId(connection.destination, destMap)
+          }
+        };
+      })
+    ];
+  }, [responses]);
+
   const license = useLicense();
+
   const isValidPlatinumLicense =
     license?.isActive &&
     (license?.type === 'platinum' || license?.type === 'trial');
