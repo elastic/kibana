6ad982da894f75733f33f32d2dddaf7add05f7f1
John Dorlus <jsdorlus@elastic.co>
2019-12-26 22:24:58 -0500

    Removed the jest tests for repository details until we get jest fixed.

--- x-pack/legacy/plugins/snapshot_restore/__jest__/client_integration/repository_detail.test.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-// TODO: Add Component Integration test for cleanup repository
-// TODO: Add component integration test for repository verification.
-// TODO: Add component integration test for repository detail.
