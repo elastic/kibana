b9a67a3e0b20af5fcdd0da709fa49c8a0a0406ef
Anton Dosov <anton.dosov@elastic.co>
2019-12-28 08:10:29 +0300

    Revert "Apply suggestions from code review"
    This reverts commit 18bc3789f27b52c67dae285e9e3bfdf652a618dc.

--- src/plugins/kibana_utils/public/state_management/url/url_tracker.ts
+++ src/plugins/kibana_utils/public/state_management/url/url_tracker.ts
@@ -50,8 +50,8 @@ export function createUrlTracker(key: string, storage: Storage = sessionStorage)
 }
 
 export function useUrlTracker(
-  key: string,
-  history?: History,
+  appInstanceId: string,
+  history: History,
   shouldRestoreUrl: (urlToRestore: string) => boolean = () => true,
   storage: Storage = sessionStorage
 ) {
