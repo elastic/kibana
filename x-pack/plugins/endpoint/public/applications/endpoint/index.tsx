bab46fb9a85799ccf4fe91eab2ab8c6f84b939d7
Kevin Logan <56395104+kevinlog@users.noreply.github.com>
2020-01-02 15:58:45 -0500

    [Endpoint] add react router to endpoint app (#53808)
    * add react router to endpoint app
    
    * linting
    
    * linting
    
    * linting
    
    * correct tests
    
    * change history from hash to browser, add new test util
    
    * remove default values in helper functions
    
    * fix type check, use FunctionComponent as oppsed to FC
    
    * use BrowserRouter component
    
    * use BrowserRouter component lin
    
    * add comments to test framework, change function name to include browserHistory
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/endpoint/public/applications/endpoint/index.tsx
+++ x-pack/plugins/endpoint/public/applications/endpoint/index.tsx
@@ -8,24 +8,55 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { CoreStart, AppMountParameters } from 'kibana/public';
 import { I18nProvider, FormattedMessage } from '@kbn/i18n/react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 /**
  * This module will be loaded asynchronously to reduce the bundle size of your plugin's main bundle.
  */
-export function renderApp(coreStart: CoreStart, { element }: AppMountParameters) {
+export function renderApp(coreStart: CoreStart, { appBasePath, element }: AppMountParameters) {
   coreStart.http.get('/api/endpoint/hello-world');
 
-  ReactDOM.render(<AppRoot />, element);
+  ReactDOM.render(<AppRoot basename={appBasePath} />, element);
 
   return () => {
     ReactDOM.unmountComponentAtNode(element);
   };
 }
 
-const AppRoot = React.memo(() => (
+interface RouterProps {
+  basename: string;
+}
+
+const AppRoot: React.FunctionComponent<RouterProps> = React.memo(({ basename }) => (
   <I18nProvider>
-    <h1 data-test-subj="welcomeTitle">
-      <FormattedMessage id="xpack.endpoint.welcomeTitle" defaultMessage="Hello World" />
-    </h1>
+    <BrowserRouter basename={basename}>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <h1 data-test-subj="welcomeTitle">
+              <FormattedMessage id="xpack.endpoint.welcomeTitle" defaultMessage="Hello World" />
+            </h1>
+          )}
+        />
+        <Route
+          path="/management"
+          render={() => (
+            <h1 data-test-subj="endpointManagement">
+              <FormattedMessage
+                id="xpack.endpoint.endpointManagement"
+                defaultMessage="Manage Endpoints"
+              />
+            </h1>
+          )}
+        />
+        <Route
+          render={() => (
+            <FormattedMessage id="xpack.endpoint.notFound" defaultMessage="Page Not Found" />
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
   </I18nProvider>
 ));
