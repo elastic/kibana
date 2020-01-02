f933eabb508b52bc2f6a103028df085cfdf4395b
Gloria Hornero <snootchie.boochies@gmail.com>
2020-01-02 20:59:45 +0100

    fixes

--- x-pack/legacy/plugins/siem/cypress/integration/smoke_tests/url_state/url_state.spec.ts
+++ x-pack/legacy/plugins/siem/cypress/integration/smoke_tests/url_state/url_state.spec.ts
@@ -266,7 +266,9 @@ describe('url state', () => {
     cy.get(TIMELINE_TITLE, { timeout: 5000 }).type(bestTimelineName);
     cy.url().should('include', 'timeline=');
     cy.visit(
-      `/app/siem#/timelines?timerange=(global:(linkTo:!(),timerange:(from:1576754930819,kind:absolute,to:1565360777369)),timeline:(linkTo:!(),timerange:(from:1576754930819,kind:absolute,to:1565360777369)))`
+      `${
+        Cypress.config().baseUrl
+      }/app/siem#/timelines?timerange=(global:(linkTo:!(),timerange:(from:1576754930819,kind:absolute,to:1565360777369)),timeline:(linkTo:!(),timerange:(from:1576754930819,kind:absolute,to:1565360777369)))`
     ).then(() => cy.get(TIMELINE_TITLE).should('have.attr', 'value', bestTimelineName));
   });
 });
