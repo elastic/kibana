a6caa0c875bbf433f3bf671e317684ad612a95cf
Justin Kambic <justin.kambic@elastic.co>
2020-01-03 16:44:55 -0500

    Re-add feature registration code.

---
+++ x-pack/plugins/uptime/server/register_feature.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { i18n } from '@kbn/i18n';
+import { PluginSetupContract } from '../../features/server';
+import { PLUGIN } from '../../../legacy/plugins/uptime/common/constants';
+
+export const registerFeature = (features: PluginSetupContract) => {
+  features.registerFeature({
+    id: PLUGIN.ID,
+    name: i18n.translate('xpack.uptime.featureRegistry.uptimeFeatureName', {
+      defaultMessage: 'Uptime',
+    }),
+    navLinkId: PLUGIN.ID,
+    icon: 'uptimeApp',
+    app: ['uptime', 'kibana'],
+    catalogue: ['uptime'],
+    privileges: {
+      all: {
+        api: ['uptime'],
+        savedObject: {
+          all: [],
+          read: [],
+        },
+        ui: ['save'],
+      },
+      read: {
+        api: ['uptime'],
+        savedObject: {
+          all: [],
+          read: [],
+        },
+        ui: [],
+      },
+    },
+  });
+};
