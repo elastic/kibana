c700ad341bf4033a7650f8d2f680531cc207994d
Patryk Kopyciński <contact@patrykkopycinski.com>
2020-01-04 18:32:59 +0100

    cleanup

--- x-pack/legacy/plugins/siem/public/components/drag_and_drop/droppable_wrapper.tsx
+++ x-pack/legacy/plugins/siem/public/components/drag_and_drop/droppable_wrapper.tsx
@@ -36,43 +36,43 @@ const ReactDndDropTarget = styled.div<{ isDraggingOver: boolean; height: string 
       }
     }
     .drop-and-provider-timeline:hover {
-      background-color: ${rgba(props.theme.eui.euiColorSuccess, 0.3)};
+        background-color: ${rgba(props.theme.eui.euiColorSuccess, 0.3)};
     }
-    > div.timeline-drop-area-empty {
+  > div.timeline-drop-area-empty {
+     color: ${props.theme.eui.euiColorSuccess};
+     background-color: ${rgba(props.theme.eui.euiColorSuccess, 0.2)};
+
+     & .euiTextColor--subdued {
       color: ${props.theme.eui.euiColorSuccess};
-      background-color: ${rgba(props.theme.eui.euiColorSuccess, 0.2)};
-
-      & .euiTextColor--subdued {
-        color: ${props.theme.eui.euiColorSuccess};
-      }
+     }
+  }
+  > div.timeline-drop-area {
+    background-color: ${rgba(props.theme.eui.euiColorSuccess, 0.2)};
+    .provider-item-filter-container div:first-child{
+      /* Override dragNdrop beautiful so we do not have our droppable moving around for no good reason */
+      transform: none !important;
     }
-    > div.timeline-drop-area {
-      background-color: ${rgba(props.theme.eui.euiColorSuccess, 0.2)};
-      .provider-item-filter-container div:first-child {
-        /* Override dragNdrop beautiful so we do not have our droppable moving around for no good reason */
-        transform: none !important;
-      }
-      .drop-and-provider-timeline {
-        display: block !important;
-        + div {
-          display: none;
-        }
-      }
-
-      & .euiFormHelpText {
-        color: ${props.theme.eui.euiColorSuccess};
-      }
-    }
-    .flyout-overlay {
-      .euiPanel {
-        background-color: ${props.theme.eui.euiColorLightShade};
-      }
+    .drop-and-provider-timeline {
+      display: block !important;
       + div {
-        /* Override dragNdrop beautiful so we do not have our droppable moving around for no good reason */
-        display: none !important;
+        display: none;
       }
     }
-        `
+
+    & .euiFormHelpText {
+      color: ${props.theme.eui.euiColorSuccess};
+    }
+  }
+  .flyout-overlay {
+    .euiPanel {
+      background-color: ${props.theme.eui.euiColorLightShade};
+    }
+    + div {
+      /* Override dragNdrop beautiful so we do not have our droppable moving around for no good reason */
+      display: none !important;
+    }
+  }
+  `
       : ''}
   > div.timeline-drop-area {
     .drop-and-provider-timeline {
