87cc67a0c286fffbc95fe7b4d972dc0c13717d1c
Patryk Kopyciński <contact@patrykkopycinski.com>
2020-01-05 17:36:39 +0100

    Fix lint

--- x-pack/legacy/plugins/siem/public/graphql/types.tsx
+++ x-pack/legacy/plugins/siem/public/graphql/types.tsx
@@ -2,7 +2,7 @@
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
-*/
+ */
 
 /* tslint:disable */
 /* eslint-disable */
