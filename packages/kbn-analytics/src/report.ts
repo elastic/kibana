fe01c92dedae998596c8c1e5008ce910d74e93cb
Ahmad Bamieh <ahmadbamieh@gmail.com>
2020-01-06 21:13:55 +0200

    fix tests

--- packages/kbn-analytics/src/report.ts
+++ packages/kbn-analytics/src/report.ts
@@ -78,6 +78,7 @@ export class ReportManager {
   }
   assignReports(newMetrics: Metric | Metric[]) {
     wrapArray(newMetrics).forEach(newMetric => this.assignReport(this.report, newMetric));
+    return { report: this.report };
   }
   static createMetricKey(metric: Metric): string {
     switch (metric.type) {
@@ -101,7 +102,7 @@ export class ReportManager {
       case METRIC_TYPE.USER_AGENT: {
         const { appName, type, userAgent } = metric;
         if (userAgent) {
-          this.report.userAgent = {
+          report.userAgent = {
             [key]: {
               key,
               appName,
@@ -110,15 +111,16 @@ export class ReportManager {
             },
           };
         }
+
         return;
       }
       case METRIC_TYPE.CLICK:
       case METRIC_TYPE.LOADED:
       case METRIC_TYPE.COUNT: {
         const { appName, type, eventName, count } = metric;
-        this.report.uiStatsMetrics = this.report.uiStatsMetrics || {};
-        const existingStats = (this.report.uiStatsMetrics[key] || {}).stats;
-        this.report.uiStatsMetrics[key] = {
+        report.uiStatsMetrics = report.uiStatsMetrics || {};
+        const existingStats = (report.uiStatsMetrics[key] || {}).stats;
+        report.uiStatsMetrics[key] = {
           key,
           appName,
           eventName,
