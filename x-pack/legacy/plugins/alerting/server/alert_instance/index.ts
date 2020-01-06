447c15d0d2be5342c65d9afc7204db77903582fb
Gidi Meir Morris <github@gidi.io>
2020-01-06 17:17:38 +0000

    allows Alerts to recover gracefully from Executor errors (#53688) (#54018)
    Prevents errors in Alert Executors from forcing their underlying tasks into a zombie state.

--- x-pack/legacy/plugins/alerting/server/alert_instance/index.ts
+++ x-pack/legacy/plugins/alerting/server/alert_instance/index.ts
@@ -4,6 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { parseDuration, getDurationSchema } from './parse_duration';
-export { LicenseState } from './license_state';
-export { validateAlertTypeParams } from './validate_alert_type_params';
+export { AlertInstance } from './alert_instance';
+export { createAlertInstanceFactory } from './create_alert_instance_factory';
