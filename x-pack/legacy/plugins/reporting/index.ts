4e7e44871e6b707ab880f75ad4b03d10b09f0e98
Timothy Sullivan <tsullivan@elastic.co>
2020-01-07 14:10:48 -0700

    Cosmetic

--- x-pack/legacy/plugins/reporting/index.ts
+++ x-pack/legacy/plugins/reporting/index.ts
@@ -63,7 +63,7 @@ export const reporting = (kibana: any) => {
       embeddableActions: ['plugins/reporting/panel_actions/get_csv_panel_action'],
       home: ['plugins/reporting/register_feature'],
       managementSections: ['plugins/reporting/views/management'],
-      injectDefaultVars(server: ServerFacade, options?: ReportingConfigOptions) {
+      injectDefaultVars(server: Legacy.Server, options?: ReportingConfigOptions) {
         const config = server.config();
         return {
           reportingPollConfig: options ? options.poll : {},
