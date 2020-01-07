677670b929613cda90f6ac39f0b10cd7a99a3d55
patrykkopycinski <patryk.kopycinski@elastic.co>
2020-01-07 18:02:32 +0100

    [SIEM] Enable eslint prefer-template rule (#53983)
    * [SIEM] Enable eslint prefer-template rule
    
    * cleanup
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -737,8 +737,7 @@ module.exports = {
         'prefer-promise-reject-errors': 'error',
         'prefer-rest-params': 'error',
         'prefer-spread': 'error',
-        // This style will be turned on after most bugs are fixed
-        // 'prefer-template': 'warn',
+        'prefer-template': 'error',
         'react/boolean-prop-naming': 'error',
         'react/button-has-type': 'error',
         'react/forbid-dom-props': 'error',
