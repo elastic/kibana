8a809d493c464c00f5d3fb7784e54f6a27cf4e39
Shaunak Kashyap <ycombinator@gmail.com>
2020-01-08 07:01:31 -0800

    Adding placeholder for aggregating functionbeat telemetry per cluster

--- x-pack/legacy/plugins/monitoring/server/telemetry_collection/get_beats_stats.js
+++ x-pack/legacy/plugins/monitoring/server/telemetry_collection/get_beats_stats.js
@@ -138,6 +138,18 @@ export function processResults(
         }
       }
 
+      const functionbeatState = get(hit, '_source.beats_state.state.functionbeat');
+      if (functionbeatState !== undefined) {
+        if (!clusters[clusterUuid].hasOwnProperty('functionbeat')) {
+          clusters[clusterUuid].functionbeat = {
+            // TODO
+          };
+        }
+        // const clusterFb = clusters[clusterUuid].functionbeat;
+
+        // TODO
+      }
+
       const stateHost = get(hit, '_source.beats_state.state.host');
       if (stateHost !== undefined) {
         const hostMap = clusterArchitectureMaps[clusterUuid];
