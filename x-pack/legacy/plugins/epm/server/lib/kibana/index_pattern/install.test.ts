88a1d8c8ffdfe08f6cba8599c0a1acc0c4e65726
neptunian <sandra.gonzales@elastic.co>
2020-01-08 10:12:10 -0500

    use yml files for tests

--- x-pack/legacy/plugins/epm/server/lib/kibana/index_pattern/install.test.ts
+++ x-pack/legacy/plugins/epm/server/lib/kibana/index_pattern/install.test.ts
@@ -3,10 +3,11 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
+import path from 'path';
+import { readFileSync } from 'fs';
+import { safeLoad } from 'js-yaml';
 import { flattenFields, dedupFields } from './install';
-import { unflattenedFieldsArray } from './tests/unflattened_fields_array';
-import { duplicatedFieldsArray } from './tests/duplicated_fields_array';
+import { Field } from '../../fields/field';
 
 // Add our own serialiser to just do JSON.stringify
 expect.addSnapshotSerializer({
@@ -19,12 +20,23 @@ expect.addSnapshotSerializer({
   },
 });
 
-test('flattenFields function', () => {
-  const flattened = flattenFields(unflattenedFieldsArray);
+const loadYamlFile = (filePath: string) => {
+  const ymlPath = path.join(__dirname, filePath);
+  const fieldsYML = readFileSync(ymlPath, 'utf-8');
+  const fields: Field[] = safeLoad(fieldsYML);
+  return fields;
+};
+const fields = loadYamlFile('./tests/nginx.fields.yml');
+const accessEcsFields = loadYamlFile('./tests/nginx.access.ecs.yml');
+const errorEcsFields = loadYamlFile('./tests/nginx.error.ecs.yml');
+
+test('flattenFields function recurseively flattens nested fields and renames name property with path', () => {
+  const flattened = flattenFields(fields);
   expect(flattened).toMatchSnapshot('flattenFields');
 });
 
-test('dedupFields function', () => {
-  const deduped = dedupFields(duplicatedFieldsArray);
+test('dedupFields function remove duplicated fields when parsing multiple files', () => {
+  const allFields = accessEcsFields.concat(errorEcsFields);
+  const deduped = dedupFields(allFields);
   expect(deduped).toMatchSnapshot('dedupFields');
 });
