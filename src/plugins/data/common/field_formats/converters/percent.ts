36a2140fb0f06596ee498493878faa461b6eb0a9
Marta Bondyra <marta.bondyra@gmail.com>
2020-01-09 16:11:47 +0100

    Update percent.ts

--- src/plugins/data/common/field_formats/converters/percent.ts
+++ src/plugins/data/common/field_formats/converters/percent.ts
@@ -23,7 +23,6 @@ import { TextContextTypeConvert, FIELD_FORMAT_IDS } from '../types';
 export class PercentFormat extends NumeralFormat {
   static id = FIELD_FORMAT_IDS.PERCENT;
   static title = 'Percentage';
-  static allowsNumericalAggregations = true;
 
   id = PercentFormat.id;
   title = PercentFormat.title;
