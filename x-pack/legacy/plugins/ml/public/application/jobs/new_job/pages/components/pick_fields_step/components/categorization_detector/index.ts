8b34e77da0f6f267afe6afebe26906fd11793692
James Gowdy <jgowdy@elastic.co>
2020-01-09 15:21:40 +0000

    [ML] Categorization wizard (#53009)
    * [ML] Categorization wizard
    
    * fixing js prettier issues
    
    * adding basic category field validation
    
    * adding rare or count selection
    
    * fixing types
    
    * category examples changes
    
    * improving results search
    
    * adding analyzer editing
    
    * improving callout
    
    * updating callout text
    
    * fixing import path
    
    * resetting cat analyser json on flyout open
    
    * disabling model plot by default
    
    * minor refactoring
    
    * fixing types
    
    * hide estimate bucket span
    
    * setting default bucket span
    
    * removing ml_classic workaround
    
    * changing style of detector selection
    
    * fixing convert to advanced issue
    
    * removing sparse data checkbox
    
    * changes based on review
    
    * use default mml
    
    * fixing job cloning
    
    * changes based on review
    
    * removing categorization_analyzer from job if it is same as default
    
    * fixing translations
    
    * disabling model plot for rare jobs
    
    * removing console.error in useResolver

--- x-pack/legacy/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/categorization_detector/index.ts
+++ x-pack/legacy/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/categorization_detector/index.ts
@@ -3,6 +3,4 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
-export { ResultsLoader, Results, ModelItem, Anomaly } from './results_loader';
-export { CategorizationExamplesLoader, CategoryExample } from './categorization_examples_loader';
+export { CategorizationDetector } from './categorization_detector';
