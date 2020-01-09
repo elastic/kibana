6a1e2137c4f10b23d70ef6eebe71ccef4acdece4
Jean-Louis Leysens <jloleysens@gmail.com>
2020-01-09 16:04:15 +0100

    Document TextObject interface

--- src/legacy/core_plugins/console/common/text_object.ts
+++ src/legacy/core_plugins/console/common/text_object.ts
@@ -19,9 +19,30 @@
 
 export const textObjectTypeName = 'text-object';
 
+/**
+ * Describes the shape of persisted objects that contain information about the current text in the
+ * text editor.
+ */
 export interface TextObject {
-  createdAt: number;
-  updatedAt: number;
-  text: string;
+  /**
+   * An ID that uniquely identifies this object.
+   */
   id: string;
+
+  /**
+   * UNIX timestamp of when the object was created.
+   */
+  createdAt: number;
+
+  /**
+   * UNIX timestamp of when the object was last updated.
+   */
+  updatedAt: number;
+
+  /**
+   * Text value input by the user.
+   *
+   * Used to re-populate a text editor buffer.
+   */
+  text: string;
 }
