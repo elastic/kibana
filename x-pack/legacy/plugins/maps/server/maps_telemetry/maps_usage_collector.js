9fb2d7f9b4d2d96d3b9ae09110e85c51628bfe9e
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-01-09 15:23:21 -0700

    [7.5] [Maps] Fix regression preventing maps telemetry from populating & remove task manager logic (#52834) (#54055)
    * [Maps] Fix regression preventing maps telemetry from populating & remove task manager logic (#52834)
    
    * Remove task logic. Remove server refs and revise for np. Migrate a few files to ts
    
    * Remove unused reference
    
    * Update mappings
    
    * Test usage collector register
    
    * Update api integration tests to include maps now that telemetry is 'normal' (not using task mgr state)
    
    * Update integration test to use stack stats
    
    * Update integration test to look for 'maps-telemetry' instead of 'maps'
    
    * Update jest test to reflect calls to register
    
    * Follow the same pattern as other int tests and test reliable nested attribute
    
    * Back out np-related changes for separate PR
    
    * timeCaptured hasn't changed but for some reason stopped working. Getting iso string fixes issue
    
    * Back out file shuffling for separate PR
    
    * Remove mappings updates (handled in separate PR)
    
    * Review feedback. Move telemetry type constant to constants file
    
    * Consolidate imports
    
    * Linting fix
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    * Revise test for 7.5 (pre-NP changes) compat
    
    * Linting
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/legacy/plugins/maps/server/maps_telemetry/maps_usage_collector.js
+++ x-pack/legacy/plugins/maps/server/maps_telemetry/maps_usage_collector.js
@@ -4,85 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import _ from 'lodash';
-import { TASK_ID, scheduleTask, registerMapsTelemetryTask } from './telemetry_task';
+import { getMapsTelemetry } from './maps_telemetry';
+import { TELEMETRY_TYPE } from '../../common/constants';
 
 export function initTelemetryCollection(server) {
-  registerMapsTelemetryTask(server);
-  scheduleTask(server);
-  registerMapsUsageCollector(server);
-}
+  const mapsUsageCollector = server.usage.collectorSet.makeUsageCollector({
+    type: TELEMETRY_TYPE,
+    isReady: () => true,
+    fetch: async () => await getMapsTelemetry(server),
+  });
 
-async function isTaskManagerReady(server) {
-  const result = await fetch(server);
-  return result !== null;
-}
-
-async function fetch(server) {
-  let docs;
-  const taskManager = server.plugins.task_manager;
-
-  if (!taskManager) {
-    return null;
-  }
-
-  try {
-    ({ docs } = await taskManager.fetch({
-      query: {
-        bool: {
-          filter: {
-            term: {
-              _id: `task:${TASK_ID}`,
-            },
-          },
-        },
-      },
-    }));
-  } catch (err) {
-    const errMessage = err && err.message ? err.message : err.toString();
-    /*
-     * The usage service WILL to try to fetch from this collector before the task manager has been initialized, because the task manager
-     * has to wait for all plugins to initialize first.
-     * It's fine to ignore it as next time around it will be initialized (or it will throw a different type of error)
-     */
-    if (errMessage.indexOf('NotInitialized') >= 0) {
-      return null;
-    } else {
-      throw err;
-    }
-  }
-
-  return docs;
-}
-
-export function buildCollectorObj(server) {
-  let isCollectorReady = false;
-  async function determineIfTaskManagerIsReady() {
-    let isReady = false;
-    try {
-      isReady = await isTaskManagerReady(server);
-    } catch (err) {} // eslint-disable-line
-
-    if (isReady) {
-      isCollectorReady = true;
-    } else {
-      setTimeout(determineIfTaskManagerIsReady, 500);
-    }
-  }
-  determineIfTaskManagerIsReady();
-
-  return {
-    type: 'maps',
-    isReady: () => isCollectorReady,
-    fetch: async () => {
-      const docs = await fetch(server);
-      return _.get(docs, '[0].state.stats');
-    },
-  };
-}
-
-export function registerMapsUsageCollector(server) {
-  const collectorObj = buildCollectorObj(server);
-  const mapsUsageCollector = server.usage.collectorSet.makeUsageCollector(collectorObj);
   server.usage.collectorSet.register(mapsUsageCollector);
 }
