2918c4555b4910fbe16ef5f520dfab7f7cbd61d2
neptunian <sandra.gonzales@elastic.co>
2020-01-09 15:39:11 -0500

    fix test

--- x-pack/legacy/plugins/epm/server/lib/elasticsearch/index.test.ts
+++ x-pack/legacy/plugins/epm/server/lib/elasticsearch/index.test.ts
@@ -10,7 +10,7 @@ import { getDatasetAssetBaseName } from './index';
 test('getBaseName', () => {
   const dataset: Dataset = {
     name: 'bar',
-    packageName: 'foo',
+    package: 'foo',
     type: 'logs',
   } as Dataset;
   const name = getDatasetAssetBaseName(dataset);
