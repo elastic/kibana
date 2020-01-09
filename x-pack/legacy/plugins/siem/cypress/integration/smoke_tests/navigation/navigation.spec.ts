1da4caebf72852d61c1be93f24762d1430136361
Gloria Hornero <snootchie.boochies@gmail.com>
2020-01-09 12:49:10 +0100

    fixes the type check failure

--- x-pack/legacy/plugins/siem/cypress/integration/smoke_tests/navigation/navigation.spec.ts
+++ x-pack/legacy/plugins/siem/cypress/integration/smoke_tests/navigation/navigation.spec.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { OVERVIEW_PAGE, TIMELINES_PAGE } from '../../lib/urls';
+import { TIMELINES_PAGE } from '../../lib/urls';
 import {
   NAVIGATION_HOSTS,
   NAVIGATION_NETWORK,
