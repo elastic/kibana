c94edc4c8538364f902c38ae4b3ca3fa741a9ad9
James Gowdy <jgowdy@elastic.co>
2020-01-09 16:47:36 +0000

    [ML] Categorization wizard (#53009) (#54353)
    * [ML] Categorization wizard
    
    * fixing js prettier issues
    
    * adding basic category field validation
    
    * adding rare or count selection
    
    * fixing types
    
    * category examples changes
    
    * improving results search
    
    * adding analyzer editing
    
    * improving callout
    
    * updating callout text
    
    * fixing import path
    
    * resetting cat analyser json on flyout open
    
    * disabling model plot by default
    
    * minor refactoring
    
    * fixing types
    
    * hide estimate bucket span
    
    * setting default bucket span
    
    * removing ml_classic workaround
    
    * changing style of detector selection
    
    * fixing convert to advanced issue
    
    * removing sparse data checkbox
    
    * changes based on review
    
    * use default mml
    
    * fixing job cloning
    
    * changes based on review
    
    * removing categorization_analyzer from job if it is same as default
    
    * fixing translations
    
    * disabling model plot for rare jobs
    
    * removing console.error in useResolver

--- x-pack/legacy/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/categorization_view/settings.tsx
+++ x-pack/legacy/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/categorization_view/settings.tsx
@@ -4,27 +4,23 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { Fragment, FC } from 'react';
+import React, { FC } from 'react';
 import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
 import { BucketSpan } from '../bucket_span';
-import { Influencers } from '../influencers';
 
 interface Props {
   setIsValid: (proceed: boolean) => void;
 }
 
-export const PopulationSettings: FC<Props> = ({ setIsValid }) => {
+export const CategorizationSettings: FC<Props> = ({ setIsValid }) => {
   return (
-    <Fragment>
+    <>
       <EuiFlexGroup gutterSize="xl">
         <EuiFlexItem>
-          <BucketSpan setIsValid={setIsValid} />
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <Influencers />
+          <BucketSpan setIsValid={setIsValid} hideEstimateButton={true} />
         </EuiFlexItem>
       </EuiFlexGroup>
-    </Fragment>
+    </>
   );
 };
--- x-pack/legacy/plugins/ml/server/models/job_service/new_job/index.ts
+++ x-pack/legacy/plugins/ml/server/models/job_service/new_job/index.ts
@@ -5,3 +5,4 @@
  */
 
 export { newJobChartsProvider } from './charts';
+export { categorizationExamplesProvider } from './categorization';
