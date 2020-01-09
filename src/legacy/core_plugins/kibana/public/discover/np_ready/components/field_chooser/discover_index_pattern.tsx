551f751fc2f1fa1b9231ca452358e34c5cea9f2d
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-01-09 06:40:15 +0100

    Improve DiscoverIndexPattern code

--- src/legacy/core_plugins/kibana/public/discover/np_ready/components/field_chooser/discover_index_pattern.tsx
+++ src/legacy/core_plugins/kibana/public/discover/np_ready/components/field_chooser/discover_index_pattern.tsx
@@ -38,17 +38,6 @@ export interface DiscoverIndexPatternProps {
 }
 
 /**
- * At initial rendering the angular directive the selectedIndexPattern prop is undefined
- * this wrapper component should be removed once all is deangularized
- */
-export function DiscoverIndexPatternWrapper(props: DiscoverIndexPatternProps) {
-  if (!props.selectedIndexPattern || !Array.isArray(props.indexPatternList)) {
-    return null;
-  }
-  return <DiscoverIndexPattern {...props} />;
-}
-
-/**
  * Component allows you to select an index pattern in discovers side bar
  */
 export function DiscoverIndexPattern({
@@ -60,12 +49,15 @@ export function DiscoverIndexPattern({
     id: entity.id,
     title: entity.attributes!.title,
   }));
-  const { id: selectedId, attributes } = selectedIndexPattern;
+  const { id: selectedId, attributes } = selectedIndexPattern || {};
 
   const [selected, setSelected] = useState({
     id: selectedId,
     title: attributes ? attributes.title : '',
   });
+  if (!selectedId) {
+    return null;
+  }
 
   return (
     <div className="indexPattern__container">
