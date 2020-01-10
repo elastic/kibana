030a249dc91f4b821f4b4c9bde0d0a621ac9cd7a
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2020-01-10 15:15:20 -0500

    [Fleet] Fix api key creation

--- x-pack/legacy/plugins/fleet/server/libs/policy_update.ts
+++ x-pack/legacy/plugins/fleet/server/libs/policy_update.ts
@@ -12,10 +12,6 @@ export function makePolicyUpdateHandler(libs: FleetServerLib) {
     const internalUser = libs.framework.getInternalUser();
 
     if (action === 'created') {
-      if (policyId === 'default') {
-        // TODO wait for #53111 to be fixed
-        return;
-      }
       await libs.apiKeys.generateEnrollmentApiKey(internalUser, {
         policyId,
       });
