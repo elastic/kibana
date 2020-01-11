ff1223242463bea255a37b2df74d6ecd1c3aa5b5
Walter Rafelsberger <walter@elastic.co>
2020-01-11 19:42:57 +0100

    [ML] Fix clearing appState in single metric viewer when changing jobs.

--- x-pack/legacy/plugins/ml/public/application/routing/routes/timeseriesexplorer.tsx
+++ x-pack/legacy/plugins/ml/public/application/routing/routes/timeseriesexplorer.tsx
@@ -4,22 +4,19 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { isEqual } from 'lodash';
 import React, { FC, useCallback, useEffect, useState } from 'react';
 import { usePrevious } from 'react-use';
-import { i18n } from '@kbn/i18n';
 import moment from 'moment';
-
 // @ts-ignore
 import queryString from 'query-string';
+
+import { i18n } from '@kbn/i18n';
 
 import { timefilter } from 'ui/timefilter';
 
 import { MlJobWithTimeRange } from '../../../../common/types/jobs';
 
-import { MlRoute, PageLoader, PageProps } from '../router';
-import { useRefresh } from '../use_refresh';
-import { useResolver } from '../use_resolver';
-import { basicResolvers } from '../resolvers';
 import { TimeSeriesExplorer } from '../../timeseriesexplorer';
 import { getDateFormatTz, TimeRangeBounds } from '../../explorer/explorer_utils';
 import { ml } from '../../services/ml_api_service';
@@ -33,6 +30,11 @@ import {
 import { useUrlState } from '../../util/url_state';
 import { useTableInterval } from '../../components/controls/select_interval';
 import { useTableSeverity } from '../../components/controls/select_severity';
+
+import { MlRoute, PageLoader, PageProps } from '../router';
+import { useRefresh } from '../use_refresh';
+import { useResolver } from '../use_resolver';
+import { basicResolvers } from '../resolvers';
 import { ANOMALY_DETECTION_BREADCRUMB, ML_BREADCRUMB } from '../breadcrumbs';
 
 export const timeSeriesExplorerRoute: MlRoute = {
@@ -118,12 +120,36 @@ const TimeSeriesExplorerUrlStateManager: FC<TimeSeriesExplorerUrlStateManager> =
   }
 
   const selectedJobIds = globalState?.ml?.jobIds;
-  const selectedDetectorIndex = +appState?.mlTimeSeriesExplorer?.detectorIndex || 0;
-  const selectedEntities = appState?.mlTimeSeriesExplorer?.entities;
-  const selectedForecastId = appState?.mlTimeSeriesExplorer?.forecastId;
-  const zoom = appState?.mlTimeSeriesExplorer?.zoom;
+  // Sort selectedJobIds so we can be sure comparison works when stringifying.
+  if (Array.isArray(selectedJobIds)) {
+    selectedJobIds.sort();
+  }
 
-  const selectedJob = mlJobService.getJob(selectedJobIds[0]);
+  // When changing jobs we'll clear appState (detectorIndex, entities, forecastId).
+  // To retore settings from the URL on initial load we also need to check against
+  // `previousSelectedJobIds` to avoid wiping appState.
+  const previousSelectedJobIds = usePrevious(selectedJobIds);
+  const isJobChange = !isEqual(previousSelectedJobIds, selectedJobIds);
+
+  // Use a side effect to clear appState when changing jobs.
+  useEffect(() => {
+    if (selectedJobIds !== undefined && previousSelectedJobIds !== undefined) {
+      setLastRefresh(Date.now());
+      appStateHandler(APP_STATE_ACTION.CLEAR);
+    }
+  }, [JSON.stringify(selectedJobIds)]);
+
+  // Next we get globalState and appState information to pass it on as props later.
+  // If a job change is going on, we fall back to defaults (as if appState was already cleard),
+  // otherwise the page could break.
+  const selectedDetectorIndex = isJobChange
+    ? 0
+    : +appState?.mlTimeSeriesExplorer?.detectorIndex || 0;
+  const selectedEntities = isJobChange ? undefined : appState?.mlTimeSeriesExplorer?.entities;
+  const selectedForecastId = isJobChange ? undefined : appState?.mlTimeSeriesExplorer?.forecastId;
+  const zoom = isJobChange ? undefined : appState?.mlTimeSeriesExplorer?.zoom;
+
+  const selectedJob = selectedJobIds && mlJobService.getJob(selectedJobIds[0]);
 
   let autoZoomDuration: number | undefined;
   if (selectedJobIds !== undefined && selectedJobIds.length === 1 && selectedJob !== undefined) {
@@ -171,20 +197,6 @@ const TimeSeriesExplorerUrlStateManager: FC<TimeSeriesExplorerUrlStateManager> =
     },
     [JSON.stringify([appState, globalState])]
   );
-
-  // When changing jobs we'll clear appState (detectorIndex, entities, forecastId).
-  // To retore settings from the URL on initial load we also need to check against
-  // `previousSelectedJobIds` to avoid wiping appState.
-  if (Array.isArray(selectedJobIds)) {
-    selectedJobIds.sort();
-  }
-  const previousSelectedJobIds = usePrevious(selectedJobIds);
-  useEffect(() => {
-    if (selectedJobIds !== undefined && previousSelectedJobIds !== undefined) {
-      setLastRefresh(Date.now());
-      appStateHandler(APP_STATE_ACTION.CLEAR);
-    }
-  }, [JSON.stringify(selectedJobIds)]);
 
   const boundsMinMs = bounds?.min?.valueOf();
   const boundsMaxMs = bounds?.max?.valueOf();
