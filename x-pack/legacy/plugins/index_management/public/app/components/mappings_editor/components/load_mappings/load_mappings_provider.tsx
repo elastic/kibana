54c0e3578923937e8ab198ec80d2e0e047553e7e
CJ Cenizal <cj@cenizal.com>
2020-01-11 06:59:14 -0800

    Clarify the loading JSON will overwrite existing mappings and that you have to drop invalid options to proceed.

--- x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/components/load_mappings/load_mappings_provider.tsx
+++ x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/components/load_mappings/load_mappings_provider.tsx
@@ -40,17 +40,17 @@ interface State {
 type ModalView = 'json' | 'validationResult';
 
 const getTexts = (view: ModalView, totalErrors = 0) => ({
-  modalTitle: i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModal.title', {
-    defaultMessage: 'Load mappings from JSON',
+  modalTitle: i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModalTitle', {
+    defaultMessage: 'Load JSON',
   }),
   buttons: {
     confirm:
       view === 'json'
         ? i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModal.loadButtonLabel', {
-            defaultMessage: 'Load',
+            defaultMessage: 'Load and overwrite',
           })
         : i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModal.acceptWarningLabel', {
-            defaultMessage: 'OK',
+            defaultMessage: 'Drop invalid options',
           }),
     cancel:
       view === 'json'
@@ -65,15 +65,11 @@ const getTexts = (view: ModalView, totalErrors = 0) => ({
     label: i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModal.jsonEditorLabel', {
       defaultMessage: 'Mappings object',
     }),
-    helpText: i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModal.jsonEditorHelpText', {
-      defaultMessage:
-        'Provide the complete mappings object with both the configuration and the properties.',
-    }),
   },
   validationErrors: {
     title: i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModal.validationErrorTitle', {
       defaultMessage:
-        '{totalErrors} {totalErrors, plural, one {error} other {errors}} detected in the mappings object',
+        '{totalErrors} {totalErrors, plural, one {invalid option} other {invalid options}} detected in the mappings object',
       values: {
         totalErrors,
       },
@@ -82,7 +78,7 @@ const getTexts = (view: ModalView, totalErrors = 0) => ({
       'xpack.idxMgmt.mappingsEditor.loadJsonModal.validationErrorDescription',
       {
         defaultMessage:
-          'The mappings provided contains some errors. You can safely ignore them: the configuration, field or parameter containing an error will be discarded before loading the JSON object in the editor.',
+          'The mappings object contains some invalid options. You can continue to load the mappings object if you drop these invalid options.',
       }
     ),
   },
@@ -93,7 +89,7 @@ const getErrorMessage = (error: MappingsValidationError) => {
     case 'ERR_CONFIG': {
       return (
         <FormattedMessage
-          id="xpack.idxMgmt.mappingsEditor.loadJsonModal.validationError.configuration"
+          id="xpack.idxMgmt.mappingsEditor.loadJsonModal.validationError.configurationMessage"
           defaultMessage="The {configName} configuration is invalid."
           values={{
             configName: <code>{error.configName}</code>,
@@ -104,7 +100,7 @@ const getErrorMessage = (error: MappingsValidationError) => {
     case 'ERR_FIELD': {
       return (
         <FormattedMessage
-          id="xpack.idxMgmt.mappingsEditor.loadJsonModal.validationError.field"
+          id="xpack.idxMgmt.mappingsEditor.loadJsonModal.validationError.fieldMessage"
           defaultMessage="The {fieldPath} field is invalid."
           values={{
             fieldPath: <code>{error.fieldPath}</code>,
@@ -115,7 +111,7 @@ const getErrorMessage = (error: MappingsValidationError) => {
     case 'ERR_PARAMETER': {
       return (
         <FormattedMessage
-          id="xpack.idxMgmt.mappingsEditor.loadJsonModal.validationError.parameter"
+          id="xpack.idxMgmt.mappingsEditor.loadJsonModal.validationError.parameterMessage"
           defaultMessage="The {paramName} parameter on field {fieldPath} is invalid."
           values={{
             paramName: <code>{error.paramName}</code>,
@@ -228,9 +224,17 @@ export const LoadMappingsProvider = ({ onJson, children }: Props) => {
             {view === 'json' ? (
               // The CSS override for the EuiCodeEditor requires a parent .application css class
               <div className="application">
+                <EuiText color="subdued">
+                  {i18n.translate('xpack.idxMgmt.mappingsEditor.loadJsonModal.jsonEditorHelpText', {
+                    defaultMessage:
+                      'Provide a mappings object, e.g. the object assigned to an index\'s "mappings" property. Existing mappings and configuration will be overwritten.',
+                  })}
+                </EuiText>
+
+                <EuiSpacer size="m" />
+
                 <JsonEditor
                   label={i18nTexts.editor.label}
-                  helpText={i18nTexts.editor.helpText}
                   onUpdate={onJsonUpdate}
                   defaultValue={state.json?.unparsed}
                   euiCodeEditorProps={{
