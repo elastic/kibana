a78ebcdfc28b44c4b68263d9daa92136108dc40a
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2020-01-11 20:14:31 -0800

    Changed ci group and disabled plugin

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/index.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/index.ts
@@ -8,7 +8,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default ({ loadTestFile, getService }: FtrProviderContext) => {
   describe('Actions and Triggers app', function() {
-    this.tags('ciGroup3');
+    this.tags(['ciGroup2', 'skipFirefox']);
     loadTestFile(require.resolve('./home_page'));
     loadTestFile(require.resolve('./connectors'));
     loadTestFile(require.resolve('./alerts'));
