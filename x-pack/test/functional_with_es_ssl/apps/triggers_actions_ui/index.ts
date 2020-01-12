7352c2091a7e7a1e99081f033fe7ce2f3f654da6
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2020-01-11 21:34:15 -0800

    changed config setting to root

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/index.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/index.ts
@@ -8,7 +8,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default ({ loadTestFile, getService }: FtrProviderContext) => {
   describe('Actions and Triggers app', function() {
-    this.tags(['ciGroup2', 'skipFirefox']);
+    this.tags(['ciGroup3', 'skipFirefox']);
     loadTestFile(require.resolve('./home_page'));
     loadTestFile(require.resolve('./connectors'));
     loadTestFile(require.resolve('./alerts'));
