e771da1b90320dccff6f19e7b64e6f05d5723a90
Sébastien <sebastien.loix@elastic.co>
2020-01-13 13:06:02 +0530

    Make the searchBox "sticky"

--- x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/components/document_fields/document_fields_header.tsx
+++ x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/components/document_fields/document_fields_header.tsx
@@ -3,7 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
 import { EuiText, EuiLink, EuiFlexGroup, EuiFlexItem, EuiFieldSearch } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
@@ -14,7 +15,34 @@ interface Props {
   onSearchChange(value: string): void;
 }
 
+// The height of the Kibana Nav bar
+const NAVBAR_HEIGHT = 48;
+
 export const DocumentFieldsHeader = React.memo(({ searchValue, onSearchChange }: Props) => {
+  const searchBox = useRef<HTMLDivElement | null>(null);
+  // We initially hardcode the height, but we will update it with the DOM value
+  const [searchBoxHeight, setSearchBoxHeight] = useState(40);
+  const [isSearchBoxSticky, setIsSearchBoxSticky] = useState(false);
+
+  const handleScroll = () => {
+    const isSticky = searchBox.current!.getBoundingClientRect().top - NAVBAR_HEIGHT <= 0;
+    setIsSearchBoxSticky(isSticky);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (searchBox.current !== null) {
+      setSearchBoxHeight(searchBox.current.getBoundingClientRect().height);
+    }
+  }, [searchBox]);
+
   return (
     <EuiFlexGroup justifyContent="spaceBetween">
       <EuiFlexItem>
@@ -35,24 +63,33 @@ export const DocumentFieldsHeader = React.memo(({ searchValue, onSearchChange }:
         </EuiText>
       </EuiFlexItem>
 
-      <EuiFlexItem grow={false}>
-        <EuiFieldSearch
-          style={{ minWidth: '350px' }}
-          placeholder={i18n.translate(
-            'xpack.idxMgmt.mappingsEditor.documentFields.searchFieldsPlaceholder',
-            {
-              defaultMessage: 'Search fields',
-            }
-          )}
-          value={searchValue}
-          onChange={e => onSearchChange(e.target.value)}
-          aria-label={i18n.translate(
-            'xpack.idxMgmt.mappingsEditor.documentFields.searchFieldsAriaLabel',
-            {
-              defaultMessage: 'Search mapped fields',
-            }
-          )}
-        />
+      {/* We set the height to avoid a UI jump when going "sticky" */}
+      <EuiFlexItem grow={false} style={{ height: `${searchBoxHeight}px` }}>
+        <div ref={searchBox} className="mappingsEditor__documentFields__searchBox">
+          <div
+            className={classNames('mappingsEditor__documentFields__searchBox__inner', {
+              'mappingsEditor__documentFields__searchBox__inner--is-sticky': isSearchBoxSticky,
+            })}
+          >
+            <EuiFieldSearch
+              style={{ minWidth: '350px' }}
+              placeholder={i18n.translate(
+                'xpack.idxMgmt.mappingsEditor.documentFields.searchFieldsPlaceholder',
+                {
+                  defaultMessage: 'Search fields',
+                }
+              )}
+              value={searchValue}
+              onChange={e => onSearchChange(e.target.value)}
+              aria-label={i18n.translate(
+                'xpack.idxMgmt.mappingsEditor.documentFields.searchFieldsAriaLabel',
+                {
+                  defaultMessage: 'Search mapped fields',
+                }
+              )}
+            />
+          </div>
+        </div>
       </EuiFlexItem>
     </EuiFlexGroup>
   );
