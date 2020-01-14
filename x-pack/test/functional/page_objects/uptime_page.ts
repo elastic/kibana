f119e08d3ebf6b6442f846c516f76787fa7c6f5b
Justin Kambic <justin.kambic@elastic.co>
2020-01-14 12:22:08 -0500

    Increase timeout for url checks.

--- x-pack/test/functional/page_objects/uptime_page.ts
+++ x-pack/test/functional/page_objects/uptime_page.ts
@@ -51,7 +51,7 @@ export function UptimePageProvider({ getPageObjects, getService }: FtrProviderCo
       await Promise.all(monitorIdsToCheck.map(id => uptimeService.monitorPageLinkExists(id)));
     }
 
-    public async pageUrlContains(value: string, expected: boolean = true, timeout: number = 1000) {
+    public async pageUrlContains(value: string, expected: boolean = true, timeout: number = 2000) {
       const limit = Date.now().valueOf() + timeout;
       let result: boolean;
       while ((result = await uptimeService.urlContains(value)) !== expected) {
@@ -67,17 +67,6 @@ export function UptimePageProvider({ getPageObjects, getService }: FtrProviderCo
       } else if (direction === 'prev') {
         await uptimeService.goToPreviousPage();
       }
-      // await new Promise(r => setTimeout(r, 1000 * 60 * 60));
-      // await new Promise((res, rej) => {
-      //   const limit = Date.now().valueOf() + maxTimeout;
-      //   const interval = setInterval(async () => {
-      //     const exists = await uptimeService.assertExists(elementToCheck);
-      //     if (exists) {
-      //       clearInterval(interval);
-      //       res();
-      //     }
-      //   }, 50);
-      // });
     }
 
     public async setStatusFilter(value: 'up' | 'down') {
