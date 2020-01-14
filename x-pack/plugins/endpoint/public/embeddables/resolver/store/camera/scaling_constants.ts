e8cc49e981f5324587ba7b7bdaf9e5186c68f13d
Davis Plumlee <davis.plumlee@elastic.co>
2020-01-14 15:20:43 -0700

    changes zoom and scale source of calculations

---
+++ x-pack/plugins/endpoint/public/embeddables/resolver/store/camera/scaling_constants.ts
@@ -0,0 +1,9 @@
+/**
+ * The minimum allowed value for the camera scale. This is the least scale that we will ever render something at.
+ */
+export const minimum = 0.1;
+
+/**
+ * The maximum allowed value for the camera scale. This is greatest scale that we will ever render something at.
+ */
+export const maximum = 6;
