2c66ffb3b8d609a638aec0ea73a6b49af156cab6
Chandler Prall <chandler.prall@gmail.com>
2020-01-14 16:53:06 -0700

    [7.x] Backport/7.x/pr 54042 (#54799)
    * Upgraded EUI to 18.0.0
    
    * Fix breaks from `palette._.colors` changes
    
    * snapshots
    
    * Updated hard coded hex color codes in tests, fixed TS errors
    
    * Updated a functional test's selector; added (BSD-3-Clause AND Apache-2.0) to license checker whitelist
    
    * Functional test selector update
    
    * Updated vega browser-ci tests for palette changes
    
    * One more location for EUI package number update and yarn lock
    
    * Fixed lurking [but introduced] TypeScript logic bug
    
    * Swap a prop definition for the same value but tied closer to its source
    
    * updates for backport
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>

--- x-pack/legacy/plugins/lens/public/indexpattern_plugin/lens_field_icon.test.tsx
+++ x-pack/legacy/plugins/lens/public/indexpattern_plugin/lens_field_icon.test.tsx
@@ -20,10 +20,10 @@ test('LensFieldIcon renders properly', () => {
 
 test('LensFieldIcon getColorForDataType for a valid type', () => {
   const color = getColorForDataType('date');
-  expect(color).toEqual('#B0916F');
+  expect(color).toEqual('#F19F58');
 });
 
 test('LensFieldIcon getColorForDataType for an invalid type', () => {
   const color = getColorForDataType('invalid');
-  expect(color).toEqual('#1EA593');
+  expect(color).toEqual('#5BBAA0');
 });
