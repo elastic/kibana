cf69901043df54a047c4c010c52970e7236e944c
Melissa Alvarez <melissa.alvarez@elastic.co>
2020-01-15 16:51:30 -0500

    ensure bool.must exists in the cloned searchQuery

--- x-pack/legacy/plugins/ml/public/application/data_frame_analytics/pages/analytics_exploration/components/regression_exploration/use_explore_data.ts
+++ x-pack/legacy/plugins/ml/public/application/data_frame_analytics/pages/analytics_exploration/components/regression_exploration/use_explore_data.ts
@@ -94,7 +94,10 @@ export const useExploreData = (
               field: resultsField,
             },
           };
-        } else if (isResultsSearchBoolQuery(searchQueryClone)) {
+        } else if (
+          isResultsSearchBoolQuery(searchQueryClone) &&
+          Array.isArray(searchQueryClone.bool.must)
+        ) {
           searchQueryClone.bool.must.push({
             exists: {
               field: resultsField,
