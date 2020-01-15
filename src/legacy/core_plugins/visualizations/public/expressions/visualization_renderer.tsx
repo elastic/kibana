43ca891c971578f1c2f31f5321621414aa81ade4
Peter Pisljar <peter.pisljar@gmail.com>
2020-01-15 13:53:56 -0500

    visualize embeddable to visualizations plugin (#54840)

--- src/legacy/core_plugins/visualizations/public/expressions/visualization_renderer.tsx
+++ src/legacy/core_plugins/visualizations/public/expressions/visualization_renderer.tsx
@@ -21,8 +21,8 @@ import chrome from 'ui/chrome';
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 // @ts-ignore
-import { Vis } from '../../../../ui/public/visualize/loader/vis';
-import { Visualization } from '../../../visualizations/public/np_ready/public/components';
+import { Vis } from './vis';
+import { Visualization } from '../np_ready/public/components';
 
 export const visualization = () => ({
   name: 'visualization',
