d5eb7e54b98d955853ece059a6105cd5d7d329c1
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2020-01-15 17:57:50 +0100

    Implement `extendDatemath()`
    The function takes a dateMath expression and extends it in the specified
    direction. This is useful to extend the `startDate` and `endDate` when
    the user reaches the first or last pages respectively and wants to see
    more log entries.

--- x-pack/legacy/plugins/infra/public/utils/datemath.test.ts
+++ x-pack/legacy/plugins/infra/public/utils/datemath.test.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { isValidDatemath, datemathToEpochMillis } from './datemath';
+import { isValidDatemath, datemathToEpochMillis, extendDatemath, convertDate } from './datemath';
 import sinon from 'sinon';
 
 describe('isValidDatemath()', () => {
@@ -44,5 +44,294 @@ describe('datemathToEpochMillis()', () => {
 
   it('Returns the current timestamp when `now`', () => {
     expect(datemathToEpochMillis('now')).toEqual(Date.now());
+  });
+});
+
+describe('extendDatemath()', () => {
+  it('Returns `undefined` for invalid values', () => {
+    expect(extendDatemath('')).toBeUndefined();
+  });
+
+  it('Keeps `"now"` stable', () => {
+    expect(extendDatemath('now')).toEqual({ value: 'now' });
+    expect(extendDatemath('now', 'before')).toEqual({ value: 'now' });
+    expect(extendDatemath('now', 'after')).toEqual({ value: 'now' });
+  });
+
+  describe('moving before', () => {
+    describe('with a negative operator', () => {
+      it('doubles miliseconds', () => {
+        expect(extendDatemath('now-250ms')).toEqual({
+          value: 'now-500ms',
+          diffAmount: 250,
+          diffUnit: 'ms',
+        });
+      });
+
+      it('doubles seconds', () => {
+        expect(extendDatemath('now-10s')).toEqual({
+          value: 'now-20s',
+          diffAmount: 10,
+          diffUnit: 's',
+        });
+      });
+
+      it('doubles minutes when amount is low', () => {
+        expect(extendDatemath('now-1m')).toEqual({ value: 'now-2m', diffAmount: 1, diffUnit: 'm' });
+        expect(extendDatemath('now-2m')).toEqual({ value: 'now-4m', diffAmount: 2, diffUnit: 'm' });
+        expect(extendDatemath('now-3m')).toEqual({ value: 'now-6m', diffAmount: 3, diffUnit: 'm' });
+      });
+
+      it('adds half the minutes when the amount is high', () => {
+        expect(extendDatemath('now-20m')).toEqual({
+          value: 'now-30m',
+          diffAmount: 10,
+          diffUnit: 'm',
+        });
+      });
+
+      it('Adds half an hour when the amount is one hour', () => {
+        expect(extendDatemath('now-1h')).toEqual({
+          value: 'now-90m',
+          diffAmount: 30,
+          diffUnit: 'm',
+        });
+      });
+
+      it('Adds one hour when the amount more than one hour', () => {
+        expect(extendDatemath('now-2h')).toEqual({
+          value: 'now-3h',
+          diffAmount: 1,
+          diffUnit: 'h',
+        });
+      });
+
+      it('Adds one hour when the amount is one day', () => {
+        expect(extendDatemath('now-1d')).toEqual({
+          value: 'now-25h',
+          diffAmount: 1,
+          diffUnit: 'h',
+        });
+      });
+
+      it('Adds one day when the amount is more than one day', () => {
+        expect(extendDatemath('now-2d')).toEqual({
+          value: 'now-3d',
+          diffAmount: 1,
+          diffUnit: 'd',
+        });
+        expect(extendDatemath('now-3d')).toEqual({
+          value: 'now-4d',
+          diffAmount: 1,
+          diffUnit: 'd',
+        });
+      });
+
+      it('Adds one day when the amount is one week', () => {
+        expect(extendDatemath('now-1w')).toEqual({
+          value: 'now-8d',
+          diffAmount: 1,
+          diffUnit: 'd',
+        });
+      });
+
+      it('Adds one week when the amount is more than one week', () => {
+        expect(extendDatemath('now-2w')).toEqual({
+          value: 'now-3w',
+          diffAmount: 1,
+          diffUnit: 'w',
+        });
+      });
+
+      it('Adds one week when the amount is one month', () => {
+        expect(extendDatemath('now-1M')).toEqual({
+          value: 'now-5w',
+          diffAmount: 1,
+          diffUnit: 'w',
+        });
+      });
+
+      it('Adds one month when the amount is more than one month', () => {
+        expect(extendDatemath('now-2M')).toEqual({
+          value: 'now-3M',
+          diffAmount: 1,
+          diffUnit: 'M',
+        });
+      });
+
+      it('Adds one month when the amount is one year', () => {
+        expect(extendDatemath('now-1y')).toEqual({
+          value: 'now-13M',
+          diffAmount: 1,
+          diffUnit: 'M',
+        });
+      });
+
+      it('Adds one year when the amount is in years', () => {
+        expect(extendDatemath('now-2y')).toEqual({
+          value: 'now-3y',
+          diffAmount: 1,
+          diffUnit: 'y',
+        });
+      });
+    });
+
+    describe('with a positive Operator', () => {
+      it('Halves miliseconds', () => {
+        expect(extendDatemath('now+250ms')).toEqual({
+          value: 'now+125ms',
+          diffAmount: 125,
+          diffUnit: 'ms',
+        });
+      });
+
+      it('Halves seconds', () => {
+        expect(extendDatemath('now+10s')).toEqual({
+          value: 'now+5s',
+          diffAmount: 5,
+          diffUnit: 's',
+        });
+      });
+
+      it('Halves minutes when the amount is low', () => {
+        expect(extendDatemath('now+2m')).toEqual({ value: 'now+1m', diffAmount: 1, diffUnit: 'm' });
+        expect(extendDatemath('now+4m')).toEqual({ value: 'now+2m', diffAmount: 2, diffUnit: 'm' });
+        expect(extendDatemath('now+6m')).toEqual({ value: 'now+3m', diffAmount: 3, diffUnit: 'm' });
+      });
+
+      it('Decreases minutes in half ammounts when the amount is high', () => {
+        expect(extendDatemath('now+30m')).toEqual({
+          value: 'now+20m',
+          diffAmount: 10,
+          diffUnit: 'm',
+        });
+      });
+
+      it('Decreases half an hour when the amount is one hour', () => {
+        expect(extendDatemath('now+1h')).toEqual({
+          value: 'now+30m',
+          diffAmount: 30,
+          diffUnit: 'm',
+        });
+      });
+
+      it('Removes one hour when the amount is one day', () => {
+        expect(extendDatemath('now+1d')).toEqual({
+          value: 'now+23h',
+          diffAmount: 1,
+          diffUnit: 'h',
+        });
+      });
+
+      it('Removes one day when the amount is more than one day', () => {
+        expect(extendDatemath('now+2d')).toEqual({
+          value: 'now+1d',
+          diffAmount: 1,
+          diffUnit: 'd',
+        });
+        expect(extendDatemath('now+3d')).toEqual({
+          value: 'now+2d',
+          diffAmount: 1,
+          diffUnit: 'd',
+        });
+      });
+
+      it('Removes one day when the amount is one week', () => {
+        expect(extendDatemath('now+1w')).toEqual({
+          value: 'now+6d',
+          diffAmount: 1,
+          diffUnit: 'd',
+        });
+      });
+
+      it('Removes one week when the amount is more than one week', () => {
+        expect(extendDatemath('now+2w')).toEqual({
+          value: 'now+1w',
+          diffAmount: 1,
+          diffUnit: 'w',
+        });
+      });
+
+      it('Removes one week when the amount is one month', () => {
+        expect(extendDatemath('now+1M')).toEqual({
+          value: 'now+3w',
+          diffAmount: 1,
+          diffUnit: 'w',
+        });
+      });
+
+      it('Removes one month when the amount is more than one month', () => {
+        expect(extendDatemath('now+2M')).toEqual({
+          value: 'now+1M',
+          diffAmount: 1,
+          diffUnit: 'M',
+        });
+      });
+
+      it('Removes one month when the amount is one year', () => {
+        expect(extendDatemath('now+1y')).toEqual({
+          value: 'now+11M',
+          diffAmount: 1,
+          diffUnit: 'M',
+        });
+      });
+
+      it('Adds one year when the amount is in years', () => {
+        expect(extendDatemath('now+2y')).toEqual({
+          value: 'now+1y',
+          diffAmount: 1,
+          diffUnit: 'y',
+        });
+      });
+    });
+  });
+});
+
+describe('convertDate()', () => {
+  it('returns same value if units are the same', () => {
+    expect(convertDate(1, 'h', 'h')).toEqual(1);
+  });
+
+  it('converts from big units to small units', () => {
+    expect(convertDate(1, 's', 'ms')).toEqual(1000);
+    expect(convertDate(1, 'm', 'ms')).toEqual(60000);
+    expect(convertDate(1, 'h', 'ms')).toEqual(3600000);
+    expect(convertDate(1, 'd', 'ms')).toEqual(86400000);
+    expect(convertDate(1, 'M', 'ms')).toEqual(2592000000);
+    expect(convertDate(1, 'y', 'ms')).toEqual(31536000000);
+  });
+
+  it('converts from small units to big units', () => {
+    expect(convertDate(1000, 'ms', 's')).toEqual(1);
+    expect(convertDate(60000, 'ms', 'm')).toEqual(1);
+    expect(convertDate(3600000, 'ms', 'h')).toEqual(1);
+    expect(convertDate(86400000, 'ms', 'd')).toEqual(1);
+    expect(convertDate(2592000000, 'ms', 'M')).toEqual(1);
+    expect(convertDate(31536000000, 'ms', 'y')).toEqual(1);
+  });
+
+  it('Handles days to years', () => {
+    expect(convertDate(1, 'y', 'd')).toEqual(365);
+    expect(convertDate(365, 'd', 'y')).toEqual(1);
+  });
+
+  it('Handles years to months', () => {
+    expect(convertDate(1, 'y', 'M')).toEqual(12);
+    expect(convertDate(12, 'M', 'y')).toEqual(1);
+  });
+
+  it('Handles days to months', () => {
+    expect(convertDate(1, 'M', 'd')).toEqual(30);
+    expect(convertDate(30, 'd', 'M')).toEqual(1);
+  });
+
+  it('Handles days to weeks', () => {
+    expect(convertDate(1, 'w', 'd')).toEqual(7);
+    expect(convertDate(7, 'd', 'w')).toEqual(1);
+  });
+
+  it('Handles weeks to years', () => {
+    expect(convertDate(1, 'y', 'w')).toEqual(52);
+    expect(convertDate(52, 'w', 'y')).toEqual(1);
   });
 });
--- x-pack/legacy/plugins/infra/public/utils/datemath.ts
+++ x-pack/legacy/plugins/infra/public/utils/datemath.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import dateMath from '@elastic/datemath';
+import dateMath, { Unit } from '@elastic/datemath';
 
 export function isValidDatemath(value: string): boolean {
   const parsedValue = dateMath.parse(value);
@@ -17,4 +17,191 @@ export function datemathToEpochMillis(value: string): number | null {
     return null;
   }
   return parsedValue.valueOf();
+}
+
+type DatemathExtension =
+  | {
+      value: string;
+      diffUnit: Unit;
+      diffAmount: number;
+    }
+  | { value: 'now' }
+  | undefined;
+
+const datemathNowExpression = /(\+|\-)(\d+)(ms|s|m|h|d|w|M|y)$/;
+
+export function extendDatemath(
+  value: string,
+  direction: 'before' | 'after' = 'before'
+): DatemathExtension {
+  if (!isValidDatemath(value)) {
+    return undefined;
+  }
+
+  if (value === 'now') {
+    return { value: 'now' };
+  }
+
+  if (value.startsWith('now')) {
+    const [, operator, amount, unit] = datemathNowExpression.exec(value) || [];
+    if (!operator || !amount || !unit) {
+      return undefined;
+    }
+
+    const parsedAmount = parseInt(amount, 10);
+    let newUnit: Unit = unit as Unit;
+    let newAmount: number;
+
+    switch (unit) {
+      case 'ms':
+      case 's':
+        newAmount =
+          operator === '-' && direction === 'before'
+            ? parsedAmount * 2
+            : Math.floor(parsedAmount / 2);
+        break;
+      case 'm':
+        let ratio;
+        if (operator === '-' && direction === 'before') {
+          ratio = parsedAmount >= 10 ? 0.5 : 1;
+          newAmount = parsedAmount + parsedAmount * ratio;
+        } else {
+          newAmount =
+            parsedAmount >= 10 ? Math.floor(parsedAmount / 1.5) : parsedAmount - parsedAmount * 0.5;
+        }
+        break;
+      case 'h':
+        if (parsedAmount === 1) {
+          newAmount = operator === '-' && direction === 'before' ? 90 : 30;
+          newUnit = 'm';
+        } else {
+          newAmount =
+            operator === '-' && direction === 'before' ? parsedAmount + 1 : parsedAmount - 1;
+        }
+        break;
+      case 'd':
+        if (parsedAmount === 1) {
+          newAmount = operator === '-' && direction === 'before' ? 25 : 23;
+          newUnit = 'h';
+        } else {
+          newAmount =
+            operator === '-' && direction === 'before' ? parsedAmount + 1 : parsedAmount - 1;
+        }
+        break;
+
+      case 'w':
+        if (parsedAmount === 1) {
+          newAmount = operator === '-' && direction === 'before' ? 8 : 6;
+          newUnit = 'd';
+        } else {
+          newAmount =
+            operator === '-' && direction === 'before' ? parsedAmount + 1 : parsedAmount - 1;
+        }
+        break;
+
+      case 'M':
+        if (parsedAmount === 1) {
+          newAmount = operator === '-' && direction === 'before' ? 5 : 3;
+          newUnit = 'w';
+        } else {
+          newAmount =
+            operator === '-' && direction === 'before' ? parsedAmount + 1 : parsedAmount - 1;
+        }
+        break;
+
+      case 'y':
+        if (parsedAmount === 1) {
+          newAmount = operator === '-' && direction === 'before' ? 13 : 11;
+          newUnit = 'M';
+        } else {
+          newAmount =
+            operator === '-' && direction === 'before' ? parsedAmount + 1 : parsedAmount - 1;
+        }
+        break;
+
+      default:
+        throw new TypeError('Unhandled datemath unit');
+    }
+
+    return {
+      value: `now${operator}${newAmount}${newUnit}`,
+      diffUnit: newUnit,
+      diffAmount:
+        newUnit !== unit
+          ? Math.abs(newAmount - convertDate(parsedAmount, unit, newUnit))
+          : Math.abs(newAmount - parsedAmount),
+    };
+  }
+
+  return undefined;
+}
+
+const CONVERSION_RATIOS: Record<string, Array<[Unit, number]>> = {
+  wy: [
+    ['w', 52], // 1 year = 52 weeks
+    ['y', 1],
+  ],
+  w: [
+    ['ms', 1000],
+    ['s', 60],
+    ['m', 60],
+    ['h', 24],
+    ['d', 7], // 1 week = 7 days
+    ['w', 4], // 1 month = 4 weeks = 28 days
+    ['M', 12], // 1 year = 12 months = 52 weeks = 364 days
+    ['y', 1],
+  ],
+  M: [
+    ['ms', 1000],
+    ['s', 60],
+    ['m', 60],
+    ['h', 24],
+    ['d', 30], // 1 month = 30 days
+    ['M', 12], // 1 year = 12 months = 360 days
+    ['y', 1],
+  ],
+  default: [
+    ['ms', 1000],
+    ['s', 60],
+    ['m', 60],
+    ['h', 24],
+    ['d', 365], // 1 year = 365 days
+    ['y', 1],
+  ],
+};
+
+export function convertDate(value: number, from: Unit, to: Unit): number {
+  if (from === to) {
+    return value;
+  }
+
+  let ratios;
+  if ((from === 'y' && to === 'w') || (from === 'w' && to === 'y')) {
+    ratios = CONVERSION_RATIOS.wy;
+  } else if (from === 'w' || to === 'w') {
+    ratios = CONVERSION_RATIOS.w;
+  } else if (from === 'M' || to === 'M') {
+    ratios = CONVERSION_RATIOS.M;
+  } else {
+    ratios = CONVERSION_RATIOS.default;
+  }
+
+  let convertedValue = value;
+
+  const fromIdx = ratios.findIndex(ratio => ratio[0] === from);
+  const toIdx = ratios.findIndex(ratio => ratio[0] === to);
+
+  if (fromIdx > toIdx) {
+    // `from` is the bigger unit. Multiply the value
+    for (let i = toIdx; i < fromIdx; i++) {
+      convertedValue *= ratios[i][1];
+    }
+  } else {
+    // `from` is the smaller unit. Divide the value
+    for (let i = fromIdx; i < toIdx; i++) {
+      convertedValue /= ratios[i][1];
+    }
+  }
+
+  return convertedValue;
 }
