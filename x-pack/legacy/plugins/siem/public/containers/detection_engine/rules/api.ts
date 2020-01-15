c076499b3d83730e8c56e3deefb9001aaa4517de
Garrett Spong <spong@users.noreply.github.com>
2020-01-14 22:34:05 -0700

    [SIEM][Detection Engine] Adds actions to Rule Details (#54828) (#54868)
    This PR adds the following actions to the `Rule Details` page via the `RuleActionsOverflow` component (which is permission-aware):
    * Duplicate
    * Export
    * Delete
    
    Additional fixes include:
    * Fixes duplication action (recent regression as part of status update additions)
    * i18n of `Duplicate` postfix when duplicating rules
    * Adds success toast when duplication is a success
    * Enabled `Edit Index Patterns` batch action
    * Removes unused `Run Rule Manually` action
    
    Rule Details Actions:
    ![image](https://user-images.githubusercontent.com/2946766/72385375-9c3a6880-36dc-11ea-8249-4ae92eb72dd1.png)
    
    Edit Index Patterns Batch Action:
    ![image](https://user-images.githubusercontent.com/2946766/72385468-c5f38f80-36dc-11ea-93c8-b70e4982f01a.png)
    
    Use ~~strikethroughs~~ to remove checklist items you don't feel are applicable to this PR.
    
    - [X] This was checked for cross-browser compatibility, [including a check against IE11](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility)
    - [x] Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/master/packages/kbn-i18n/README.md)
    - [ ] ~[Documentation](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#writing-documentation) was added for features that require explanation or tutorials~
    - [x] [Unit or functional tests](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility) were updated or added to match the most common scenarios
    - [ ] ~This was checked for [keyboard-only and screenreader accessibility](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Cross_browser_testing/Accessibility#Accessibility_testing_checklist)~
    
    - [ ] ~This was checked for breaking API changes and was [labeled appropriately](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#release-notes-process)~
    - [ ] ~This includes a feature addition or change that requires a release note and was [labeled appropriately](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#release-notes-process)~
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/legacy/plugins/siem/public/containers/detection_engine/rules/api.ts
+++ x-pack/legacy/plugins/siem/public/containers/detection_engine/rules/api.ts
@@ -191,7 +191,7 @@ export const duplicateRules = async ({ rules }: DuplicateRulesProps): Promise<Ru
       },
       body: JSON.stringify({
         ...rule,
-        name: `${rule.name} [Duplicate]`,
+        name: `${rule.name} [${i18n.DUPLICATE}]`,
         created_at: undefined,
         created_by: undefined,
         id: undefined,
@@ -200,6 +200,10 @@ export const duplicateRules = async ({ rules }: DuplicateRulesProps): Promise<Ru
         updated_by: undefined,
         enabled: rule.enabled,
         immutable: false,
+        last_success_at: undefined,
+        last_success_message: undefined,
+        status: undefined,
+        status_date: undefined,
       }),
     })
   );
