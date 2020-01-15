384f7e23e16c9761819db1ddc033fe11cb670e4c
neptunian <sandra.gonzales@elastic.co>
2020-01-15 18:14:06 -0500

    group tests

--- x-pack/legacy/plugins/epm/server/lib/kibana/index_pattern/install.test.ts
+++ x-pack/legacy/plugins/epm/server/lib/kibana/index_pattern/install.test.ts
@@ -55,82 +55,96 @@ describe('creating index patterns from yaml fields', () => {
     expect(transformField({ name: 'testField', type: 'invalid' }).type).toBe(undefined);
   });
 
-  test('transformField changes values based on other values', () => {
-    expect(transformField({ name: 'testField' }).count).toBe(0);
-    expect(transformField({ name: 'testField', count: 4 }).count).toBe(4);
+  describe('transformField changes values based on other values', () => {
+    test('count', () => {
+      expect(transformField({ name: 'testField' }).count).toBe(0);
+      expect(transformField({ name: 'testField', count: 4 }).count).toBe(4);
+    });
 
-    // searchable
-    expect(transformField({ name: 'testField' }).searchable).toBe(true);
-    expect(transformField({ name: 'testField', searchable: true }).searchable).toBe(true);
-    expect(transformField({ name: 'testField', searchable: false }).searchable).toBe(false);
-    expect(transformField({ name: 'testField', type: 'binary' }).searchable).toBe(false);
-    expect(transformField({ name: 'testField', searchable: true, type: 'binary' }).searchable).toBe(
-      false
-    );
+    test('searchable', () => {
+      expect(transformField({ name: 'testField' }).searchable).toBe(true);
+      expect(transformField({ name: 'testField', searchable: true }).searchable).toBe(true);
+      expect(transformField({ name: 'testField', searchable: false }).searchable).toBe(false);
+      expect(transformField({ name: 'testField', type: 'binary' }).searchable).toBe(false);
+      expect(
+        transformField({ name: 'testField', searchable: true, type: 'binary' }).searchable
+      ).toBe(false);
+    });
 
-    // aggregatable
-    expect(transformField({ name: 'testField' }).aggregatable).toBe(true);
-    expect(transformField({ name: 'testField', aggregatable: true }).aggregatable).toBe(true);
-    expect(transformField({ name: 'testField', aggregatable: false }).aggregatable).toBe(false);
-    expect(transformField({ name: 'testField', type: 'binary' }).aggregatable).toBe(false);
-    expect(
-      transformField({ name: 'testField', aggregatable: true, type: 'binary' }).aggregatable
-    ).toBe(false);
-    expect(transformField({ name: 'testField', type: 'keyword' }).aggregatable).toBe(true);
-    expect(
-      transformField({ name: 'testField', aggregatable: true, type: 'text' }).aggregatable
-    ).toBe(false);
-    expect(transformField({ name: 'testField', type: 'text' }).aggregatable).toBe(false);
+    test('aggregatable', () => {
+      expect(transformField({ name: 'testField' }).aggregatable).toBe(true);
+      expect(transformField({ name: 'testField', aggregatable: true }).aggregatable).toBe(true);
+      expect(transformField({ name: 'testField', aggregatable: false }).aggregatable).toBe(false);
+      expect(transformField({ name: 'testField', type: 'binary' }).aggregatable).toBe(false);
+      expect(
+        transformField({ name: 'testField', aggregatable: true, type: 'binary' }).aggregatable
+      ).toBe(false);
+      expect(transformField({ name: 'testField', type: 'keyword' }).aggregatable).toBe(true);
+      expect(
+        transformField({ name: 'testField', aggregatable: true, type: 'text' }).aggregatable
+      ).toBe(false);
+      expect(transformField({ name: 'testField', type: 'text' }).aggregatable).toBe(false);
+    });
 
-    // analyzed
-    expect(transformField({ name: 'testField' }).analyzed).toBe(false);
-    expect(transformField({ name: 'testField', analyzed: true }).analyzed).toBe(true);
-    expect(transformField({ name: 'testField', analyzed: false }).analyzed).toBe(false);
-    expect(transformField({ name: 'testField', type: 'binary' }).analyzed).toBe(false);
-    expect(transformField({ name: 'testField', analyzed: true, type: 'binary' }).analyzed).toBe(
-      false
-    );
+    test('analyzed', () => {
+      expect(transformField({ name: 'testField' }).analyzed).toBe(false);
+      expect(transformField({ name: 'testField', analyzed: true }).analyzed).toBe(true);
+      expect(transformField({ name: 'testField', analyzed: false }).analyzed).toBe(false);
+      expect(transformField({ name: 'testField', type: 'binary' }).analyzed).toBe(false);
+      expect(transformField({ name: 'testField', analyzed: true, type: 'binary' }).analyzed).toBe(
+        false
+      );
+    });
 
-    // doc_values always set to true except for meta fields
-    expect(transformField({ name: 'testField' }).doc_values).toBe(true);
-    expect(transformField({ name: 'testField', doc_values: true }).doc_values).toBe(true);
-    expect(transformField({ name: 'testField', doc_values: false }).doc_values).toBe(false);
-    expect(transformField({ name: 'testField', script: 'doc[]' }).doc_values).toBe(false);
-    expect(
-      transformField({ name: 'testField', doc_values: true, script: 'doc[]' }).doc_values
-    ).toBe(false);
-    expect(transformField({ name: 'testField', type: 'binary' }).doc_values).toBe(false);
-    expect(transformField({ name: 'testField', doc_values: true, type: 'binary' }).doc_values).toBe(
-      true
-    );
+    test('doc_values always set to true except for meta fields', () => {
+      expect(transformField({ name: 'testField' }).doc_values).toBe(true);
+      expect(transformField({ name: 'testField', doc_values: true }).doc_values).toBe(true);
+      expect(transformField({ name: 'testField', doc_values: false }).doc_values).toBe(false);
+      expect(transformField({ name: 'testField', script: 'doc[]' }).doc_values).toBe(false);
+      expect(
+        transformField({ name: 'testField', doc_values: true, script: 'doc[]' }).doc_values
+      ).toBe(false);
+      expect(transformField({ name: 'testField', type: 'binary' }).doc_values).toBe(false);
+      expect(
+        transformField({ name: 'testField', doc_values: true, type: 'binary' }).doc_values
+      ).toBe(true);
+    });
 
-    // enabled - only applies to objects (and only if set)
-    expect(transformField({ name: 'testField', type: 'binary', enabled: false }).enabled).toBe(
-      undefined
-    );
-    expect(transformField({ name: 'testField', type: 'binary', enabled: true }).enabled).toBe(
-      undefined
-    );
-    expect(transformField({ name: 'testField', type: 'object', enabled: true }).enabled).toBe(true);
-    expect(transformField({ name: 'testField', type: 'object', enabled: false }).enabled).toBe(
-      false
-    );
-    expect(transformField({ name: 'testField', type: 'object', enabled: false }).doc_values).toBe(
-      false
-    );
+    test('enabled - only applies to objects (and only if set)', () => {
+      expect(transformField({ name: 'testField', type: 'binary', enabled: false }).enabled).toBe(
+        undefined
+      );
+      expect(transformField({ name: 'testField', type: 'binary', enabled: true }).enabled).toBe(
+        undefined
+      );
+      expect(transformField({ name: 'testField', type: 'object', enabled: true }).enabled).toBe(
+        true
+      );
+      expect(transformField({ name: 'testField', type: 'object', enabled: false }).enabled).toBe(
+        false
+      );
+      expect(transformField({ name: 'testField', type: 'object', enabled: false }).doc_values).toBe(
+        false
+      );
+    });
 
-    // indexed
-    expect(transformField({ name: 'testField', type: 'binary' }).indexed).toBe(false);
-    expect(transformField({ name: 'testField', index: true, type: 'binary' }).indexed).toBe(false);
+    test('indexed', () => {
+      expect(transformField({ name: 'testField', type: 'binary' }).indexed).toBe(false);
+      expect(transformField({ name: 'testField', index: true, type: 'binary' }).indexed).toBe(
+        false
+      );
+    });
 
-    // script, scripted
-    expect(transformField({ name: 'testField' }).scripted).toBe(false);
-    expect(transformField({ name: 'testField' }).script).toBe(undefined);
-    expect(transformField({ name: 'testField', script: 'doc[]' }).scripted).toBe(true);
-    expect(transformField({ name: 'testField', script: 'doc[]' }).script).toBe('doc[]');
+    test('script, scripted', () => {
+      expect(transformField({ name: 'testField' }).scripted).toBe(false);
+      expect(transformField({ name: 'testField' }).script).toBe(undefined);
+      expect(transformField({ name: 'testField', script: 'doc[]' }).scripted).toBe(true);
+      expect(transformField({ name: 'testField', script: 'doc[]' }).script).toBe('doc[]');
+    });
 
-    // language
-    expect(transformField({ name: 'testField' }).lang).toBe(undefined);
-    expect(transformField({ name: 'testField', script: 'doc[]' }).lang).toBe('painless');
+    test('language', () => {
+      expect(transformField({ name: 'testField' }).lang).toBe(undefined);
+      expect(transformField({ name: 'testField', script: 'doc[]' }).lang).toBe('painless');
+    });
   });
 });
