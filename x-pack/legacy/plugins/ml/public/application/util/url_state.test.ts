47506c182bc1688e33a52862173af3e8f5708214
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-01-15 14:08:11 +0100

    [ML] condition for rison decode in getUrlState

---
+++ x-pack/legacy/plugins/ml/public/application/util/url_state.test.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { getUrlState } from './url_state';
+
+describe('getUrlState', () => {
+  test('properly docode url with _g and _a', () => {
+    expect(
+      getUrlState(
+        "?_a=(mlExplorerFilter:(),mlExplorerSwimlane:(viewByFieldName:action),query:(query_string:(analyze_wildcard:!t,query:'*')))&_g=(ml:(jobIds:!(dec-2)),refreshInterval:(display:Off,pause:!f,value:0),time:(from:'2019-01-01T00:03:40.000Z',mode:absolute,to:'2019-08-30T11:55:07.000Z'))&savedSearchId=571aaf70-4c88-11e8-b3d7-01146121b73d"
+      )
+    ).toEqual({
+      _a: {
+        mlExplorerFilter: {},
+        mlExplorerSwimlane: {
+          viewByFieldName: 'action',
+        },
+        query: {
+          query_string: {
+            analyze_wildcard: true,
+            query: '*',
+          },
+        },
+      },
+      _g: {
+        ml: {
+          jobIds: ['dec-2'],
+        },
+        refreshInterval: {
+          display: 'Off',
+          pause: false,
+          value: 0,
+        },
+        time: {
+          from: '2019-01-01T00:03:40.000Z',
+          mode: 'absolute',
+          to: '2019-08-30T11:55:07.000Z',
+        },
+      },
+      savedSearchId: '571aaf70-4c88-11e8-b3d7-01146121b73d',
+    });
+  });
+});
