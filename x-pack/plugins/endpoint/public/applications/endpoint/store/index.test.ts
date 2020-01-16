eaa7700d180420524a53548d88f96275b2fb4945
Paul Tavares <paul.tavares@elastic.co>
2020-01-16 16:41:52 -0500

    Added test for app level global store

---
+++ x-pack/plugins/endpoint/public/applications/endpoint/store/index.test.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { appStoreFactory } from './index';
+import { coreMock } from '../../../../../../../src/core/public/mocks';
+
+describe('endpoint app store', () => {
+  test('it creates global app store', () => {
+    const coreStart = coreMock.createStart({ basePath: '/mock' });
+    const store = appStoreFactory(coreStart);
+
+    expect(store).toBeDefined();
+    expect(store.dispatch).toBeInstanceOf(Function);
+  });
+});
