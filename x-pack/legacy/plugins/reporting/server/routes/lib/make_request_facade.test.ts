b4dcefb1989da9f1f4636dd2212ab638efaf5540
Timothy Sullivan <tsullivan@elastic.co>
2020-01-15 21:07:20 -0700

    update test snapshot

--- x-pack/legacy/plugins/reporting/server/routes/lib/make_request_facade.test.ts
+++ x-pack/legacy/plugins/reporting/server/routes/lib/make_request_facade.test.ts
@@ -7,36 +7,56 @@
 import { Legacy } from 'kibana';
 import { makeRequestFacade } from './make_request_facade';
 
-test('makeRequestFacade', () => {
-  const originalRequest = ({
-    getBasePath: () => 'basebase',
-    params: {
-      param1: 123,
-    },
-    payload: {
-      payload1: 123,
-    },
-    headers: {
-      user: 123,
-    },
-  } as unknown) as Legacy.Request;
+describe('makeRequestFacade', () => {
+  test('creates a default object', () => {
+    const originalRequest = ({
+      getBasePath: () => 'basebase',
+      params: {
+        param1: 123,
+      },
+      payload: {
+        payload1: 123,
+      },
+      headers: {
+        user: 123,
+      },
+    } as unknown) as Legacy.Request;
 
-  expect(makeRequestFacade(originalRequest)).toMatchInlineSnapshot(`
-    Object {
-      "getBasePath": [Function],
-      "getSavedObjectsClient": undefined,
-      "headers": Object {
-        "user": 123,
+    expect(makeRequestFacade(originalRequest)).toMatchInlineSnapshot(`
+      Object {
+        "getBasePath": [Function],
+        "getRawRequest": [Function],
+        "getSavedObjectsClient": undefined,
+        "headers": Object {
+          "user": 123,
+        },
+        "params": Object {
+          "param1": 123,
+        },
+        "payload": Object {
+          "payload1": 123,
+        },
+        "pre": undefined,
+        "query": undefined,
+        "route": undefined,
+      }
+    `);
+  });
+
+  test('getRawRequest', () => {
+    const originalRequest = ({
+      getBasePath: () => 'basebase',
+      params: {
+        param1: 123,
       },
-      "params": Object {
-        "param1": 123,
+      payload: {
+        payload1: 123,
       },
-      "payload": Object {
-        "payload1": 123,
+      headers: {
+        user: 123,
       },
-      "pre": undefined,
-      "query": undefined,
-      "route": undefined,
-    }
-  `);
+    } as unknown) as Legacy.Request;
+
+    expect(makeRequestFacade(originalRequest).getRawRequest()).toBe(originalRequest);
+  });
 });
