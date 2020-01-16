1d7f3011ad26c0f974415be11c6a1f2b15c018e9
Jean-Louis Leysens <jloleysens@gmail.com>
2020-01-16 13:43:09 +0100

    Fix type issue with TimeBuckets export

--- x-pack/plugins/watcher/public/legacy/time_buckets.d.ts
+++ x-pack/plugins/watcher/public/legacy/time_buckets.d.ts
@@ -4,4 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export declare const MANAGEMENT_BREADCRUMB: { text: string; href?: string };
+export declare const TimeBuckets: any;
