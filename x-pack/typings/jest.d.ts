979c64660e05ad90ce476ad5bc0856bbcda1f664
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2020-01-16 14:51:53 +0100

    Remove redundant `x-pack/typings`.

--- x-pack/typings/jest.d.ts
+++ x-pack/typings/jest.d.ts
@@ -4,8 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// https://github.com/styled-components/jest-styled-components/issues/264
+type MockedKeys<T> = { [P in keyof T]: jest.Mocked<Writable<T[P]>> };
 
+type DeeplyMockedKeys<T> = {
+  [P in keyof T]: T[P] extends (...args: any[]) => any
+    ? jest.MockInstance<ReturnType<T[P]>, Parameters<T[P]>>
+    : DeeplyMockedKeys<T[P]>;
+} &
+  T;
+
+// https://github.com/styled-components/jest-styled-components/issues/264
 declare namespace jest {
   interface AsymmetricMatcher {
     $$typeof: Symbol; //eslint-disable-line
