24ac0323cd4481c00e947c7dde331af1adadec29
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2020-01-16 13:17:38 +0000

    Color blind palette (#54918) (#54952)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/legacy/plugins/maps/public/layers/styles/color_utils.js
+++ x-pack/legacy/plugins/maps/public/layers/styles/color_utils.js
@@ -97,36 +97,6 @@ const COLOR_PALETTES_CONFIGS = [
     id: 'palette_0',
     colors: DEFAULT_FILL_COLORS.slice(0, COLOR_PALETTE_MAX_SIZE),
   },
-  {
-    id: 'palette_1',
-    colors: [
-      '#a6cee3',
-      '#1f78b4',
-      '#b2df8a',
-      '#33a02c',
-      '#fb9a99',
-      '#e31a1c',
-      '#fdbf6f',
-      '#ff7f00',
-      '#cab2d6',
-      '#6a3d9a',
-    ],
-  },
-  {
-    id: 'palette_2',
-    colors: [
-      '#8dd3c7',
-      '#ffffb3',
-      '#bebada',
-      '#fb8072',
-      '#80b1d3',
-      '#fdb462',
-      '#b3de69',
-      '#fccde5',
-      '#d9d9d9',
-      '#bc80bd',
-    ],
-  },
 ];
 
 export function getColorPalette(paletteId) {
