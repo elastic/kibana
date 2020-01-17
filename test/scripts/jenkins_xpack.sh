8428d3bbd21d57e1b256a55fb806f8d14a46df18
Brian Seeders <brian.seeders@elastic.co>
2020-01-17 16:42:10 -0500

    Fix intake test setup

--- test/scripts/jenkins_xpack.sh
+++ test/scripts/jenkins_xpack.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
-
-export TEST_BROWSER_HEADLESS=1
+source test/scripts/jenkins_test_setup.sh
 
 if [[ -z "$CODE_COVERAGE" ]] ; then
   echo " -> Running mocha tests"
@@ -10,26 +8,26 @@ if [[ -z "$CODE_COVERAGE" ]] ; then
   checks-reporter-with-killswitch "X-Pack Karma Tests" yarn test:browser
   echo ""
   echo ""
-    
+
   echo " -> Running jest tests"
   cd "$XPACK_DIR"
   checks-reporter-with-killswitch "X-Pack Jest" node scripts/jest --ci --verbose
   echo ""
   echo ""
-    
+
   echo " -> Running SIEM cyclic dependency test"
   cd "$XPACK_DIR"
   checks-reporter-with-killswitch "X-Pack SIEM cyclic dependency test" node legacy/plugins/siem/scripts/check_circular_deps
   echo ""
   echo ""
-    
+
   # FAILING: https://github.com/elastic/kibana/issues/44250
   # echo " -> Running jest contracts tests"
   # cd "$XPACK_DIR"
   # SLAPSHOT_ONLINE=true CONTRACT_ONLINE=true node scripts/jest_contract.js --ci --verbose
   # echo ""
   # echo ""
-    
+
   # echo " -> Running jest integration tests"
   # cd "$XPACK_DIR"
   # node scripts/jest_integration --ci --verbose
