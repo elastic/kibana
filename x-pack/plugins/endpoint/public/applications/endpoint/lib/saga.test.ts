5a5bade8be51c2d93a3535dc9952e7ca47309dc0
Paul Tavares <56442535+paul-tavares@users.noreply.github.com>
2020-01-21 14:21:33 -0500

    [Endpoint] Fix saga to start only after store is created and stopped on app unmount (#55245)
    - added `stop()`/`start()` methods to the Saga Middleware creator factory
    - adjust tests based on changes
    - changed application `renderApp` to stop sagas when react app is unmounted

--- x-pack/plugins/endpoint/public/applications/endpoint/lib/saga.test.ts
+++ x-pack/plugins/endpoint/public/applications/endpoint/lib/saga.test.ts
@@ -3,18 +3,21 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { createSagaMiddleware, SagaContext } from './index';
-import { applyMiddleware, createStore, Reducer } from 'redux';
+
+import { createSagaMiddleware, SagaContext, SagaMiddleware } from './index';
+import { applyMiddleware, createStore, Reducer, Store } from 'redux';
 
 describe('saga', () => {
   const INCREMENT_COUNTER = 'INCREMENT';
   const DELAYED_INCREMENT_COUNTER = 'DELAYED INCREMENT COUNTER';
   const STOP_SAGA_PROCESSING = 'BREAK ASYNC ITERATOR';
 
-  const sleep = (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
+  const sleep = (ms = 10) => new Promise(resolve => setTimeout(resolve, ms));
+  let store: Store;
   let reducerA: Reducer;
   let sideAffect: (a: unknown, s: unknown) => void;
   let sagaExe: (sagaContext: SagaContext) => Promise<void>;
+  let sagaExeReduxMiddleware: SagaMiddleware;
 
   beforeEach(() => {
     reducerA = jest.fn((prevState = { count: 0 }, { type }) => {
@@ -47,53 +50,63 @@ describe('saga', () => {
         }
       }
     });
+
+    sagaExeReduxMiddleware = createSagaMiddleware(sagaExe);
+    store = createStore(reducerA, applyMiddleware(sagaExeReduxMiddleware));
   });
 
-  test('it returns Redux Middleware from createSagaMiddleware()', () => {
-    const sagaMiddleware = createSagaMiddleware(async () => {});
-    expect(sagaMiddleware).toBeInstanceOf(Function);
+  afterEach(() => {
+    sagaExeReduxMiddleware.stop();
   });
+
   test('it does nothing if saga is not started', () => {
-    const store = createStore(reducerA, applyMiddleware(createSagaMiddleware(sagaExe)));
-    expect(store.getState().count).toEqual(0);
-    expect(reducerA).toHaveBeenCalled();
-    expect(sagaExe).toHaveBeenCalled();
-    expect(sideAffect).not.toHaveBeenCalled();
-    expect(store.getState()).toEqual({ count: 0 });
+    expect(sagaExe).not.toHaveBeenCalled();
   });
-  test('it updates store once running', async () => {
-    const sagaMiddleware = createSagaMiddleware(sagaExe);
-    const store = createStore(reducerA, applyMiddleware(sagaMiddleware));
 
+  test('it can dispatch store actions once running', async () => {
+    sagaExeReduxMiddleware.start();
     expect(store.getState()).toEqual({ count: 0 });
     expect(sagaExe).toHaveBeenCalled();
 
     store.dispatch({ type: DELAYED_INCREMENT_COUNTER });
     expect(store.getState()).toEqual({ count: 0 });
 
-    await sleep(100);
+    await sleep();
 
     expect(sideAffect).toHaveBeenCalled();
     expect(store.getState()).toEqual({ count: 1 });
   });
-  test('it stops processing if break out of loop', async () => {
-    const sagaMiddleware = createSagaMiddleware(sagaExe);
-    const store = createStore(reducerA, applyMiddleware(sagaMiddleware));
 
+  test('it stops processing if break out of loop', async () => {
+    sagaExeReduxMiddleware.start();
     store.dispatch({ type: DELAYED_INCREMENT_COUNTER });
-    await sleep(100);
+    await sleep();
 
     expect(store.getState()).toEqual({ count: 1 });
     expect(sideAffect).toHaveBeenCalledTimes(2);
 
     store.dispatch({ type: STOP_SAGA_PROCESSING });
-    await sleep(100);
+    await sleep();
+
+    store.dispatch({ type: DELAYED_INCREMENT_COUNTER });
+    await sleep();
+
+    expect(store.getState()).toEqual({ count: 1 });
+    expect(sideAffect).toHaveBeenCalledTimes(2);
+  });
+
+  test('it stops saga middleware when stop() is called', async () => {
+    sagaExeReduxMiddleware.start();
+    store.dispatch({ type: DELAYED_INCREMENT_COUNTER });
+    await sleep();
 
     expect(store.getState()).toEqual({ count: 1 });
     expect(sideAffect).toHaveBeenCalledTimes(2);
 
+    sagaExeReduxMiddleware.stop();
+
     store.dispatch({ type: DELAYED_INCREMENT_COUNTER });
-    await sleep(100);
+    await sleep();
 
     expect(store.getState()).toEqual({ count: 1 });
     expect(sideAffect).toHaveBeenCalledTimes(2);
