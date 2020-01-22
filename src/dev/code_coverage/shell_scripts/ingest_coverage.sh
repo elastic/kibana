96ff35701a801e9ef984444b733611723ed637c1
Tre' Seymour <wayne.seymour@elastic.co>
2020-01-22 14:47:44 -0700

    Refactor inline ingestion to it's own file.

---
+++ src/dev/code_coverage/shell_scripts/ingest_coverage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "###  Ingesting Code Coverage"
+
+
+BUILD_ID=$1
+export BUILD_ID
+
+ES_HOST=https://super:changeme@87883974a2984732b6cb134e5c7e240f.us-east-1.aws.staging.foundit.no:9243
+export ES_HOST
+
+TIME_STAMP=$(date -u +%Y%m%d_%H%M%SZ)
+export TIME_STAMP
+
+for x in jest functional mocha
+do
+  echo "### Ingesting coverage for ${x}"
+  node scripts/ingest_coverage.js --verbose --path target/kibana-coverage/${x}-combined/coverage-summary.json || echo "### target/kibana-coverage/${x}-combined/coverage-summary.json not found?"
+done
