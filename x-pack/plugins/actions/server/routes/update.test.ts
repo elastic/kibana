ad7c03b2eaec96b5090808a5c9c67b14f94640c9
Gidi Meir Morris <github@gidi.io>
2020-01-22 14:37:56 +0000

    removed dead code

--- x-pack/plugins/actions/server/routes/update.test.ts
+++ x-pack/plugins/actions/server/routes/update.test.ts
@@ -3,50 +3,6 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
-// it('calls the update function with proper parameters', async () => {
-//   const request = {
-//     method: 'PUT',
-//     url: '/api/action/1',
-//     payload: {
-//       name: 'My name',
-//       config: { foo: true },
-//     },
-//   };
-//   const updateResult: ActionResult = {
-//     id: '1',
-//     actionTypeId: 'my-action-type-id',
-//     name: 'My name',
-//     config: { foo: true },
-//   };
-
-//   actionsClient.update.mockResolvedValueOnce(updateResult);
-//   const { payload, statusCode } = await server.inject(request);
-//   expect(statusCode).toBe(200);
-//   const response = JSON.parse(payload);
-//   expect(response).toEqual({
-//     id: '1',
-//     actionTypeId: 'my-action-type-id',
-//     name: 'My name',
-//     config: { foo: true },
-//   });
-//   expect(actionsClient.update).toHaveBeenCalledTimes(1);
-//   expect(actionsClient.update.mock.calls[0]).toMatchInlineSnapshot(`
-//     Array [
-//       Object {
-//         "action": Object {
-//           "config": Object {
-//             "foo": true,
-//           },
-//           "name": "My name",
-//           "secrets": Object {},
-//         },
-//         "id": "1",
-//       },
-//     ]
-//   `);
-// });
-
 import { updateActionRoute } from './update';
 import { mockRouter, RouterMock } from '../../../../../src/core/server/http/router/router.mock';
 import { mockLicenseState } from '../lib/license_state.mock';
