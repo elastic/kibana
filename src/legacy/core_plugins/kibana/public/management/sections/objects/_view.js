5639313c36bd2431c65e479ab301be4e4e671fde
Liza K <liza.katz@elastic.co>
2020-01-23 14:16:14 +0200

    Delete legacy saved objects client provider

--- src/legacy/core_plugins/kibana/public/management/sections/objects/_view.js
+++ src/legacy/core_plugins/kibana/public/management/sections/objects/_view.js
@@ -28,9 +28,9 @@ import uiRoutes from 'ui/routes';
 import { uiModules } from 'ui/modules';
 import { fatalError, toastNotifications } from 'ui/notify';
 import 'ui/accessibility/kbn_ui_ace_keyboard_mode';
-import { SavedObjectsClientProvider } from 'ui/saved_objects';
 import { isNumeric } from './lib/numeric';
 import { canViewInApp } from './lib/in_app_url';
+import { npStart } from 'ui/new_platform';
 
 import { castEsToKbnFieldTypeName } from '../../../../../../../plugins/data/public';
 
@@ -56,12 +56,11 @@ uiModules
         $location,
         $window,
         $rootScope,
-        Private,
         uiCapabilities
       ) {
         const serviceObj = savedObjectManagementRegistry.get($routeParams.service);
         const service = $injector.get(serviceObj.service);
-        const savedObjectsClient = Private(SavedObjectsClientProvider);
+        const savedObjectsClient = npStart.core.savedObjects.client;
 
         /**
          * Creates a field definition and pushes it to the memo stack. This function
