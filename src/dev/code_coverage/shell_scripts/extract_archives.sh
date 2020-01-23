a7f3e4aabf3e8de7ee2b7d59b934f6181a88b06f
Tre' Seymour <wayne.seymour@elastic.co>
2020-01-23 13:06:56 -0700

    Fixup path for artifact extraction.

--- src/dev/code_coverage/shell_scripts/extract_archives.sh
+++ src/dev/code_coverage/shell_scripts/extract_archives.sh
@@ -10,6 +10,6 @@ mkdir -p $EXTRACT_DIR
 echo "### Extracting downloaded artifacts"
 # TODO-TRE: Prolly need to update the numbered kibana-xpack-tests-NUMBER later.
 for x in kibana-intake kibana-oss-tests kibana-xpack-tests-1 x-pack-intake; do
-  tar -xzf $DOWNLOAD_DIR/kibana-coverage/${x}/kibana-coverage.tar.gz -C $EXTRACT_DIR
+  tar -xzf $DOWNLOAD_DIR/coverage/${x}/kibana-coverage.tar.gz -C $EXTRACT_DIR
 done
 
