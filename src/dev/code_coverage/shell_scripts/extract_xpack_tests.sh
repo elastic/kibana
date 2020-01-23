f41073924e51ccd51559b60d3ed5c8a11b56849d
Tre' Seymour <wayne.seymour@elastic.co>
2020-01-23 11:49:02 -0700

    Fixup xpack tests extraction and list after.

--- src/dev/code_coverage/shell_scripts/extract_xpack_tests.sh
+++ src/dev/code_coverage/shell_scripts/extract_xpack_tests.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-echo "### Extracting kibana-xpack-tests"
-for i in {1..3}; do
-  tar -xzf /tmp/downloaded_coverage/coverage/kibana-xpack-tests-${i}/kibana-coverage.tar.gz -C /tmp/extracted_coverage
-done
+DOWNLOAD_DIR=/tmp/downloaded_coverage
+EXTRACT_DIR=/tmp/extracted_coverage
 
-tar -xzf /tmp/downloaded_coverage/coverage/kibana-xpack-tests-1/kibana-coverage.tar.gz -C /tmp/extracted_coverage
+echo "### Extracting kibana-xpack-tests"
+#for i in {1..3}; do
+#  tar -xzf $DOWNLOAD_DIR/coverage/kibana-xpack-tests-${i}/kibana-coverage.tar.gz -C $EXTRACT_DIR
+#done
+
+# TODO-TRE: Uncomment the loop above, and remove the line below
+tar -xzf $DOWNLOAD_DIR/coverage/kibana-xpack-tests-1/kibana-coverage.tar.gz -C $EXTRACT_DIR
