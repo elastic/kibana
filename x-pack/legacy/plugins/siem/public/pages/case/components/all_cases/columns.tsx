90926e3fc9ef8c236b1aebd210b56216715dbb4d
stephmilovic <stephanie.milovic@elastic.co>
2020-01-23 15:44:02 -0700

    pretty up table

--- x-pack/legacy/plugins/siem/public/pages/case/components/all_cases/columns.tsx
+++ x-pack/legacy/plugins/siem/public/pages/case/components/all_cases/columns.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React from 'react';
-import { EuiBadge, EuiTableFieldDataColumnType } from '@elastic/eui';
+import { EuiBadge, EuiTableFieldDataColumnType, EuiTableComputedColumnType } from '@elastic/eui';
 import { getEmptyTagValue } from '../../../../components/empty_value';
 import { CaseSavedObject } from '../../../../containers/case/types';
 import { FormattedRelativePreferenceDate } from '../../../../components/formatted_date';
@@ -12,22 +12,20 @@ import { CaseDetailsLink } from '../../../../components/links';
 import { TruncatableText } from '../../../../components/truncatable_text';
 import * as i18n from './translations';
 
-export type CasesColumns = EuiTableFieldDataColumnType<CaseSavedObject>;
+export type CasesColumns =
+  | EuiTableFieldDataColumnType<CaseSavedObject>
+  | EuiTableComputedColumnType<CaseSavedObject>;
 
 const renderStringField = (field: string) => (field != null ? field : getEmptyTagValue());
 
 export const getCasesColumns = (): CasesColumns[] => [
   {
-    field: 'attributes.title',
     name: i18n.CASE_TITLE,
-    render: title => renderStringField(title),
-  },
-  {
-    field: 'id',
-    name: i18n.CASE_ID,
-    render: id => {
-      if (id != null) {
-        return <CaseDetailsLink detailName={id} />;
+    render: (theCase: CaseSavedObject) => {
+      if (theCase.id != null && theCase.attributes!.title) {
+        return (
+          <CaseDetailsLink detailName={theCase.id}>{theCase.attributes.title}</CaseDetailsLink>
+        );
       }
       return getEmptyTagValue();
     },
@@ -35,11 +33,11 @@ export const getCasesColumns = (): CasesColumns[] => [
   {
     field: 'attributes.tags',
     name: i18n.TAGS,
-    render: tags => {
+    render: (tags: CaseSavedObject['attributes']['tags']) => {
       if (tags != null && tags.length > 0) {
         return (
           <TruncatableText>
-            {tags.map((tag, i) => (
+            {tags.map((tag: string, i: number) => (
               <EuiBadge color="hollow" key={`${tag}-${i}`}>
                 {tag}
               </EuiBadge>
@@ -55,7 +53,7 @@ export const getCasesColumns = (): CasesColumns[] => [
     field: 'attributes.created_at',
     name: i18n.CREATED_AT,
     sortable: true,
-    render: createdAt => {
+    render: (createdAt: CaseSavedObject['attributes']['created_at']) => {
       if (createdAt != null) {
         return <FormattedRelativePreferenceDate value={createdAt} />;
       }
@@ -65,13 +63,14 @@ export const getCasesColumns = (): CasesColumns[] => [
   {
     field: 'attributes.created_by.username',
     name: i18n.CREATED_BY,
-    render: createdBy => renderStringField(createdBy),
+    render: (createdBy: CaseSavedObject['attributes']['created_by']['username']) =>
+      renderStringField(createdBy),
   },
   {
     field: 'updated_at',
     name: i18n.LAST_UPDATED,
     sortable: true,
-    render: updatedAt => {
+    render: (updatedAt: CaseSavedObject['updated_at']) => {
       if (updatedAt != null) {
         return <FormattedRelativePreferenceDate value={updatedAt} />;
       }
@@ -82,6 +81,6 @@ export const getCasesColumns = (): CasesColumns[] => [
     field: 'attributes.state',
     name: i18n.STATE,
     sortable: true,
-    render: state => renderStringField(state),
+    render: (state: CaseSavedObject['attributes']['state']) => renderStringField(state),
   },
 ];
