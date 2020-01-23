elastic/kibana

commit fe15d6ecbf83cddcfd86aef87f9dfcea9eb0aa30

[Logs / Metrics UI] Client side NP migration (#52867)

x-pack/legacy/plugins/infra/public/pages/infrastructure/snapshot/index.tsx
@@ -8,8 +8,6 @@ import { EuiButton, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
 import React, { useContext } from 'react';
-import { UICapabilities } from 'ui/capabilities';
-import { injectUICapabilities } from 'ui/capabilities/react';
 import { SnapshotPageContent } from './page_content';
 import { SnapshotToolbar } from './toolbar';
 
@@ -27,15 +25,11 @@ import { Source } from '../../../containers/source';
 import { WithWaffleFilterUrlState } from '../../../containers/waffle/with_waffle_filters';
 import { WithWaffleOptionsUrlState } from '../../../containers/waffle/with_waffle_options';
 import { WithWaffleTimeUrlState } from '../../../containers/waffle/with_waffle_time';
-import { WithKibanaChrome } from '../../../containers/with_kibana_chrome';
 import { useTrackPageview } from '../../../hooks/use_track_metric';
+import { useKibana } from '../../../../../../../../src/plugins/kibana_react/public';
 
-interface SnapshotPageProps {
-  uiCapabilities: UICapabilities;
-}
-
-export const SnapshotPage = injectUICapabilities((props: SnapshotPageProps) => {
-  const { uiCapabilities } = props;
+export const SnapshotPage = () => {
+  const uiCapabilities = useKibana().services.application?.capabilities;
   const {
     createDerivedIndexPattern,
     hasFailedLoadingSource,
@@ -44,7 +38,7 @@ export const SnapshotPage = injectUICapabilities((props: SnapshotPageProps) => {
     loadSource,
     metricIndicesExist,
   } = useContext(Source.Context);
-
+  const basePath = useKibana().services.http?.basePath || '';
   useTrackPageview({ app: 'infra_metrics', path: 'inventory' });
   useTrackPageview({ app: 'infra_metrics', path: 'inventory', delay: 15000 });
 
@@ -73,49 +67,44 @@ export const SnapshotPage = injectUICapabilities((props: SnapshotPageProps) => {
       ) : hasFailedLoadingSource ? (
         <SourceErrorPage errorMessage={loadSourceFailureMessage || ''} retry={loadSource} />
       ) : (
-        <WithKibanaChrome>
-          {({ basePath }) => (
-            <NoIndices
-              title={i18n.translate('xpack.infra.homePage.noMetricsIndicesTitle', {
-                defaultMessage: "Looks like you don't have any metrics indices.",
-              })}
-              message={i18n.translate('xpack.infra.homePage.noMetricsIndicesDescription', {
-                defaultMessage: "Let's add some!",
-              })}
-              actions={
-                <EuiFlexGroup>
-                  <EuiFlexItem>
-                    <EuiButton
-                      href={`${basePath}/app/kibana#/home/tutorial_directory/metrics`}
-                      color="primary"
-                      fill
-                      data-test-subj="infrastructureViewSetupInstructionsButton"
-                    >
-                      {i18n.translate(
-                        'xpack.infra.homePage.noMetricsIndicesInstructionsActionLabel',
-                        { defaultMessage: 'View setup instructions' }
-                      )}
-                    </EuiButton>
-                  </EuiFlexItem>
-                  {uiCapabilities.infrastructure.configureSource ? (
-                    <EuiFlexItem>
-                      <ViewSourceConfigurationButton
-                        data-test-subj="configureSourceButton"
-                        hrefBase={ViewSourceConfigurationButtonHrefBase.infrastructure}
-                      >
-                        {i18n.translate('xpack.infra.configureSourceActionLabel', {
-                          defaultMessage: 'Change source configuration',
-                        })}
-                      </ViewSourceConfigurationButton>
-                    </EuiFlexItem>
-                  ) : null}
-                </EuiFlexGroup>
-              }
-              data-test-subj="noMetricsIndicesPrompt"
-            />
-          )}
-        </WithKibanaChrome>
+        <NoIndices
+          title={i18n.translate('xpack.infra.homePage.noMetricsIndicesTitle', {
+            defaultMessage: "Looks like you don't have any metrics indices.",
+          })}
+          message={i18n.translate('xpack.infra.homePage.noMetricsIndicesDescription', {
+            defaultMessage: "Let's add some!",
+          })}
+          actions={
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                <EuiButton
+                  href={`${basePath}/app/kibana#/home/tutorial_directory/metrics`}
+                  color="primary"
+                  fill
+                  data-test-subj="infrastructureViewSetupInstructionsButton"
+                >
+                  {i18n.translate('xpack.infra.homePage.noMetricsIndicesInstructionsActionLabel', {
+                    defaultMessage: 'View setup instructions',
+                  })}
+                </EuiButton>
+              </EuiFlexItem>
+              {uiCapabilities?.infrastructure?.configureSource ? (
+                <EuiFlexItem>
+                  <ViewSourceConfigurationButton
+                    data-test-subj="configureSourceButton"
+                    hrefBase={ViewSourceConfigurationButtonHrefBase.infrastructure}
+                  >
+                    {i18n.translate('xpack.infra.configureSourceActionLabel', {
+                      defaultMessage: 'Change source configuration',
+                    })}
+                  </ViewSourceConfigurationButton>
+                </EuiFlexItem>
+              ) : null}
+            </EuiFlexGroup>
+          }
+          data-test-subj="noMetricsIndicesPrompt"
+        />
       )}
     </ColumnarPage>
   );
-});
+};

x-pack/legacy/plugins/infra/public/pages/logs/stream/page_no_indices_content.tsx
@@ -9,66 +9,53 @@ import { i18n } from '@kbn/i18n';
 
 import React from 'react';
 
-import { UICapabilities } from 'ui/capabilities';
-import { injectUICapabilities } from 'ui/capabilities/react';
 import { NoIndices } from '../../../components/empty_states/no_indices';
-import { WithKibanaChrome } from '../../../containers/with_kibana_chrome';
 import {
   ViewSourceConfigurationButton,
   ViewSourceConfigurationButtonHrefBase,
 } from '../../../components/source_configuration';
+import { useKibana } from '../../../../../../../../src/plugins/kibana_react/public';
 
-interface LogsPageNoIndicesContentProps {
-  uiCapabilities: UICapabilities;
-}
-
-export const LogsPageNoIndicesContent = injectUICapabilities(
-  (props: LogsPageNoIndicesContentProps) => {
-    const { uiCapabilities } = props;
-
-    return (
-      <WithKibanaChrome>
-        {({ basePath }) => (
-          <NoIndices
-            data-test-subj="noLogsIndicesPrompt"
-            title={i18n.translate('xpack.infra.logsPage.noLoggingIndicesTitle', {
-              defaultMessage: "Looks like you don't have any logging indices.",
-            })}
-            message={i18n.translate('xpack.infra.logsPage.noLoggingIndicesDescription', {
-              defaultMessage: "Let's add some!",
-            })}
-            actions={
-              <EuiFlexGroup>
-                <EuiFlexItem>
-                  <EuiButton
-                    href={`${basePath}/app/kibana#/home/tutorial_directory/logging`}
-                    color="primary"
-                    fill
-                    data-test-subj="logsViewSetupInstructionsButton"
-                  >
-                    {i18n.translate(
-                      'xpack.infra.logsPage.noLoggingIndicesInstructionsActionLabel',
-                      { defaultMessage: 'View setup instructions' }
-                    )}
-                  </EuiButton>
-                </EuiFlexItem>
-                {uiCapabilities.logs.configureSource ? (
-                  <EuiFlexItem>
-                    <ViewSourceConfigurationButton
-                      data-test-subj="configureSourceButton"
-                      hrefBase={ViewSourceConfigurationButtonHrefBase.logs}
-                    >
-                      {i18n.translate('xpack.infra.configureSourceActionLabel', {
-                        defaultMessage: 'Change source configuration',
-                      })}
-                    </ViewSourceConfigurationButton>
-                  </EuiFlexItem>
-                ) : null}
-              </EuiFlexGroup>
-            }
-          />
-        )}
-      </WithKibanaChrome>
-    );
-  }
-);
+export const LogsPageNoIndicesContent = () => {
+  const basePath = useKibana().services.http?.basePath || '';
+  const uiCapabilities = useKibana().services.application?.capabilities;
+  return (
+    <NoIndices
+      data-test-subj="noLogsIndicesPrompt"
+      title={i18n.translate('xpack.infra.logsPage.noLoggingIndicesTitle', {
+        defaultMessage: "Looks like you don't have any logging indices.",
+      })}
+      message={i18n.translate('xpack.infra.logsPage.noLoggingIndicesDescription', {
+        defaultMessage: "Let's add some!",
+      })}
+      actions={
+        <EuiFlexGroup>
+          <EuiFlexItem>
+            <EuiButton
+              href={`${basePath}/app/kibana#/home/tutorial_directory/logging`}
+              color="primary"
+              fill
+              data-test-subj="logsViewSetupInstructionsButton"
+            >
+              {i18n.translate('xpack.infra.logsPage.noLoggingIndicesInstructionsActionLabel', {
+                defaultMessage: 'View setup instructions',
+              })}
+            </EuiButton>
+          </EuiFlexItem>
+          {uiCapabilities?.logs?.configureSource ? (
+            <EuiFlexItem>
+              <ViewSourceConfigurationButton
+                data-test-subj="configureSourceButton"
+                hrefBase={ViewSourceConfigurationButtonHrefBase.logs}
+              >
+                {i18n.translate('xpack.infra.configureSourceActionLabel', {
+                  defaultMessage: 'Change source configuration',
+                })}
+              </ViewSourceConfigurationButton>
+            </EuiFlexItem>
+          ) : null}
+        </EuiFlexGroup>
+      }
+    />
+  );
+};

x-pack/legacy/plugins/infra/public/pages/metrics/components/invalid_node.tsx
@@ -9,70 +9,67 @@ import { FormattedMessage } from '@kbn/i18n/react';
 import React from 'react';
 
 import euiStyled from '../../../../../../common/eui_styled_components';
-import { WithKibanaChrome } from '../../../containers/with_kibana_chrome';
 import {
   ViewSourceConfigurationButton,
   ViewSourceConfigurationButtonHrefBase,
 } from '../../../components/source_configuration';
+import { useKibana } from '../../../../../../../../src/plugins/kibana_react/public';
 
 interface InvalidNodeErrorProps {
   nodeName: string;
 }
 
 export const InvalidNodeError: React.FunctionComponent<InvalidNodeErrorProps> = ({ nodeName }) => {
+  const basePath = useKibana().services.http?.basePath || '';
   return (
-    <WithKibanaChrome>
-      {({ basePath }) => (
-        <CenteredEmptyPrompt
-          title={
-            <h2>
+    <CenteredEmptyPrompt
+      title={
+        <h2>
+          <FormattedMessage
+            id="xpack.infra.metrics.invalidNodeErrorTitle"
+            defaultMessage="Looks like {nodeName} isn't collecting any metrics data"
+            values={{
+              nodeName,
+            }}
+          />
+        </h2>
+      }
+      body={
+        <p>
+          <FormattedMessage
+            id="xpack.infra.metrics.invalidNodeErrorDescription"
+            defaultMessage="Double check your configuration"
+          />
+        </p>
+      }
+      actions={
+        <EuiFlexGroup>
+          <EuiFlexItem>
+            <EuiButton
+              href={`${basePath}/app/kibana#/home/tutorial_directory/metrics`}
+              color="primary"
+              fill
+            >
               <FormattedMessage
-                id="xpack.infra.metrics.invalidNodeErrorTitle"
-                defaultMessage="Looks like {nodeName} isn't collecting any metrics data"
-                values={{
-                  nodeName,
-                }}
+                id="xpack.infra.homePage.noMetricsIndicesInstructionsActionLabel"
+                defaultMessage="View setup instructions"
               />
-            </h2>
-          }
-          body={
-            <p>
+            </EuiButton>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <ViewSourceConfigurationButton
+              data-test-subj="configureSourceButton"
+              hrefBase={ViewSourceConfigurationButtonHrefBase.infrastructure}
+            >
               <FormattedMessage
-                id="xpack.infra.metrics.invalidNodeErrorDescription"
-                defaultMessage="Double check your configuration"
+                id="xpack.infra.configureSourceActionLabel"
+                defaultMessage="Change source configuration"
               />
-            </p>
-          }
-          actions={
-            <EuiFlexGroup>
-              <EuiFlexItem>
-                <EuiButton
-                  href={`${basePath}/app/kibana#/home/tutorial_directory/metrics`}
-                  color="primary"
-                  fill
-                >
-                  <FormattedMessage
-                    id="xpack.infra.homePage.noMetricsIndicesInstructionsActionLabel"
-                    defaultMessage="View setup instructions"
-                  />
-                </EuiButton>
-              </EuiFlexItem>
-              <EuiFlexItem>
-                <ViewSourceConfigurationButton
-                  data-test-subj="configureSourceButton"
-                  hrefBase={ViewSourceConfigurationButtonHrefBase.infrastructure}
-                >
-                  <FormattedMessage
-                    id="xpack.infra.configureSourceActionLabel"
-                    defaultMessage="Change source configuration"
-                  />
-                </ViewSourceConfigurationButton>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          }
-        />
-      )}
-    </WithKibanaChrome>
+            </ViewSourceConfigurationButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      }
+    />
   );
 };
 

x-pack/legacy/plugins/infra/public/pages/infrastructure/index.tsx
@@ -8,8 +8,6 @@ import { i18n } from '@kbn/i18n';
 
 import React from 'react';
 import { Route, RouteComponentProps, Switch } from 'react-router-dom';
-import { UICapabilities } from 'ui/capabilities';
-import { injectUICapabilities } from 'ui/capabilities/react';
 
 import { DocumentTitle } from '../../components/document_title';
 import { HelpCenterContent } from '../../components/help_center_content';
@@ -25,13 +23,11 @@ import { SnapshotPage } from './snapshot';
 import { SettingsPage } from '../shared/settings';
 import { AppNavigation } from '../../components/navigation/app_navigation';
 import { SourceLoadingPage } from '../../components/source_loading_page';
+import { useKibana } from '../../../../../../../src/plugins/kibana_react/public';
 
-interface InfrastructurePageProps extends RouteComponentProps {
-  uiCapabilities: UICapabilities;
-}
-
-export const InfrastructurePage = injectUICapabilities(
-  ({ match, uiCapabilities }: InfrastructurePageProps) => (
+export const InfrastructurePage = ({ match }: RouteComponentProps) => {
+  const uiCapabilities = useKibana().services.application?.capabilities;
+  return (
     <Source.Provider sourceId="default">
       <ColumnarPage>
         <DocumentTitle
@@ -55,7 +51,7 @@ export const InfrastructurePage = injectUICapabilities(
               }),
             },
           ]}
-          readOnlyBadge={!uiCapabilities.infrastructure.save}
+          readOnlyBadge={!uiCapabilities?.infrastructure?.save}
         />
 
         <AppNavigation
@@ -114,5 +110,5 @@ export const InfrastructurePage = injectUICapabilities(
         </Switch>
       </ColumnarPage>
     </Source.Provider>
-  )
-);
+  );
+};

x-pack/legacy/plugins/infra/public/pages/logs/index.tsx
@@ -7,8 +7,6 @@
 import { i18n } from '@kbn/i18n';
 import React from 'react';
 import { Route, RouteComponentProps, Switch } from 'react-router-dom';
-import { UICapabilities } from 'ui/capabilities';
-import { injectUICapabilities } from 'ui/capabilities/react';
 
 import { DocumentTitle } from '../../components/document_title';
 import { HelpCenterContent } from '../../components/help_center_content';
@@ -27,14 +25,12 @@ import {
 } from '../../containers/logs/log_analysis';
 import { useSourceId } from '../../containers/source_id';
 import { RedirectWithQueryParams } from '../../utils/redirect_with_query_params';
+import { useKibana } from '../../../../../../..//src/plugins/kibana_react/public';
 import { LogEntryCategoriesPage } from './log_entry_categories';
 import { LogEntryRatePage } from './log_entry_rate';
 
-interface LogsPageProps extends RouteComponentProps {
-  uiCapabilities: UICapabilities;
-}
-
-export const LogsPage = injectUICapabilities(({ match, uiCapabilities }: LogsPageProps) => {
+export const LogsPage = ({ match }: RouteComponentProps) => {
+  const uiCapabilities = useKibana().services.application?.capabilities;
   const [sourceId] = useSourceId();
   const source = useSource({ sourceId });
   const logAnalysisCapabilities = useLogAnalysisCapabilities();
@@ -83,7 +79,7 @@ export const LogsPage = injectUICapabilities(({ match, uiCapabilities }: LogsPag
                 text: pageTitle,
               },
             ]}
-            readOnlyBadge={!uiCapabilities.logs.save}
+            readOnlyBadge={!uiCapabilities?.logs?.save}
           />
           {source.isLoadingSource ||
           (!source.isLoadingSource &&
@@ -124,7 +120,7 @@ export const LogsPage = injectUICapabilities(({ match, uiCapabilities }: LogsPag
       </LogAnalysisCapabilities.Context.Provider>
     </Source.Context.Provider>
   );
-});
+};
 
 const pageTitle = i18n.translate('xpack.infra.header.logsTitle', {
   defaultMessage: 'Logs',

commit 4bd329e5895fc73d575100d9e4a1d1ff81212d73

[Logs UI] Fix base path in tutorial URLs (#55459)
This fixes the way the base path is prepended to several tutorial URLs, which broke with #52867.

fixes #55350

x-pack/legacy/plugins/infra/public/pages/infrastructure/snapshot/index.tsx
@@ -6,8 +6,9 @@
 
 import { EuiButton, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-
+import { identity } from 'fp-ts/lib/function';
 import React, { useContext } from 'react';
+
 import { SnapshotPageContent } from './page_content';
 import { SnapshotToolbar } from './toolbar';
 
@@ -38,9 +39,10 @@ export const SnapshotPage = () => {
     loadSource,
     metricIndicesExist,
   } = useContext(Source.Context);
-  const basePath = useKibana().services.http?.basePath || '';
   useTrackPageview({ app: 'infra_metrics', path: 'inventory' });
   useTrackPageview({ app: 'infra_metrics', path: 'inventory', delay: 15000 });
+
+  const prependBasePath = useKibana().services.http?.basePath.prepend ?? identity;
 
   return (
     <ColumnarPage>
@@ -78,7 +80,7 @@ export const SnapshotPage = () => {
             <EuiFlexGroup>
               <EuiFlexItem>
                 <EuiButton
-                  href={`${basePath}/app/kibana#/home/tutorial_directory/metrics`}
+                  href={prependBasePath('/app/kibana#/home/tutorial_directory/metrics')}
                   color="primary"
                   fill
                   data-test-subj="infrastructureViewSetupInstructionsButton"

x-pack/legacy/plugins/infra/public/pages/logs/stream/page_no_indices_content.tsx
@@ -6,19 +6,24 @@
 
 import { EuiButton, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-
+import { identity } from 'fp-ts/lib/function';
 import React from 'react';
 
+import { useKibana } from '../../../../../../../../src/plugins/kibana_react/public';
 import { NoIndices } from '../../../components/empty_states/no_indices';
 import {
   ViewSourceConfigurationButton,
   ViewSourceConfigurationButtonHrefBase,
 } from '../../../components/source_configuration';
-import { useKibana } from '../../../../../../../../src/plugins/kibana_react/public';
 
 export const LogsPageNoIndicesContent = () => {
-  const basePath = useKibana().services.http?.basePath || '';
-  const uiCapabilities = useKibana().services.application?.capabilities;
+  const {
+    services: { application, http },
+  } = useKibana<{}>();
+
+  const canConfigureSource = application?.capabilities?.logs?.configureSource ? true : false;
+  const prependBasePath = http?.basePath.prepend ?? identity;
+
   return (
     <NoIndices
       data-test-subj="noLogsIndicesPrompt"
@@ -32,7 +37,7 @@ export const LogsPageNoIndicesContent = () => {
         <EuiFlexGroup>
           <EuiFlexItem>
             <EuiButton
-              href={`${basePath}/app/kibana#/home/tutorial_directory/logging`}
+              href={prependBasePath('/app/kibana#/home/tutorial_directory/logging')}
               color="primary"
               fill
               data-test-subj="logsViewSetupInstructionsButton"
@@ -42,7 +47,7 @@ export const LogsPageNoIndicesContent = () => {
               })}
             </EuiButton>
           </EuiFlexItem>
-          {uiCapabilities?.logs?.configureSource ? (
+          {canConfigureSource ? (
             <EuiFlexItem>
               <ViewSourceConfigurationButton
                 data-test-subj="configureSourceButton"

x-pack/legacy/plugins/infra/public/pages/metrics/components/invalid_node.tsx
@@ -6,6 +6,7 @@
 
 import { EuiButton, EuiEmptyPrompt, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
+import { identity } from 'fp-ts/lib/function';
 import React from 'react';
 
 import euiStyled from '../../../../../../common/eui_styled_components';
@@ -20,7 +21,8 @@ interface InvalidNodeErrorProps {
 }
 
 export const InvalidNodeError: React.FunctionComponent<InvalidNodeErrorProps> = ({ nodeName }) => {
-  const basePath = useKibana().services.http?.basePath || '';
+  const prependBasePath = useKibana().services.http?.basePath.prepend ?? identity;
+
   return (
     <CenteredEmptyPrompt
       title={
@@ -46,7 +48,7 @@ export const InvalidNodeError: React.FunctionComponent<InvalidNodeErrorProps> = 
         <EuiFlexGroup>
           <EuiFlexItem>
             <EuiButton
-              href={`${basePath}/app/kibana#/home/tutorial_directory/metrics`}
+              href={prependBasePath('/app/kibana#/home/tutorial_directory/metrics')}
               color="primary"
               fill
             >