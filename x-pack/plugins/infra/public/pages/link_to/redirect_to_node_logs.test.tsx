eab2091afb54462c74007f59856fe483ea855e1f
Kerry Gallagher <k.gallagher.05@gmail.com>
2020-01-24 14:42:38 +0000

    Functional test amends

--- x-pack/plugins/infra/public/pages/link_to/redirect_to_node_logs.test.tsx
+++ x-pack/plugins/infra/public/pages/link_to/redirect_to_node_logs.test.tsx
@@ -73,7 +73,7 @@ describe('RedirectToNodeLogs component', () => {
 
     expect(component).toMatchInlineSnapshot(`
       <Redirect
-        to="/logs?sourceId=default&logFilter=(expression:'HOST_FIELD:%20HOST_NAME',kind:kuery)&logPosition=(position:(tiebreaker:0,time:1550671089404))"
+        to="/logs?sourceId=default&logPosition=(position:(tiebreaker:0,time:1550671089404))&logFilter=(expression:'HOST_FIELD:%20HOST_NAME',kind:kuery)"
       />
     `);
   });
@@ -89,7 +89,7 @@ describe('RedirectToNodeLogs component', () => {
 
     expect(component).toMatchInlineSnapshot(`
       <Redirect
-        to="/logs?sourceId=default&logFilter=(expression:'(HOST_FIELD:%20HOST_NAME)%20and%20(FILTER_FIELD:FILTER_VALUE)',kind:kuery)&logPosition=(position:(tiebreaker:0,time:1550671089404))"
+        to="/logs?sourceId=default&logPosition=(position:(tiebreaker:0,time:1550671089404))&logFilter=(expression:'(HOST_FIELD:%20HOST_NAME)%20and%20(FILTER_FIELD:FILTER_VALUE)',kind:kuery)"
       />
     `);
   });
