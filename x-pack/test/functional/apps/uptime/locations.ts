c51394a62e82227e8a8f4a823878f22dff01c3fa
Justin Kambic <justin.kambic@elastic.co>
2020-01-27 14:47:57 -0500

    Rename test.

--- x-pack/test/functional/apps/uptime/locations.ts
+++ x-pack/test/functional/apps/uptime/locations.ts
@@ -14,48 +14,46 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     const start = new Date().toISOString();
     const end = new Date().toISOString();
 
-    describe('when data pfewfewresent', async () => {
-      const MONITOR_ID = 'location-testing-id';
-      before(async () => {
-        const numIps = 2;
-        const checksPerMonitor = 5;
-        const scheduleEvery = 10000; // fake monitor checks every 10s
+    const MONITOR_ID = 'location-testing-id';
+    before(async () => {
+      const numIps = 2;
+      const checksPerMonitor = 5;
+      const scheduleEvery = 10000; // fake monitor checks every 10s
 
-        /**
-         * This mogrify function will strip the documents of their location
-         * data (but preserve their locaion name), which is necessary for
-         * this test to work as desired.
-         * @param d current document
-         */
-        const mogrifyNoLocation = (d: any) => {
-          if (d.observer?.geo?.location) {
-            d.observer.geo.location = undefined;
-          }
-          return d;
-        };
-        await makeChecksWithStatus(
-          getService('legacyEs'),
-          MONITOR_ID,
-          checksPerMonitor,
-          numIps,
-          scheduleEvery,
-          {
-            geo: { location: 'g' },
-            summary: {
-              state: {
-                monitor: { monitor_id: MONITOR_ID },
-              },
+      /**
+       * This mogrify function will strip the documents of their location
+       * data (but preserve their locaion name), which is necessary for
+       * this test to work as desired.
+       * @param d current document
+       */
+      const mogrifyNoLocation = (d: any) => {
+        if (d.observer?.geo?.location) {
+          d.observer.geo.location = undefined;
+        }
+        return d;
+      };
+      await makeChecksWithStatus(
+        getService('legacyEs'),
+        MONITOR_ID,
+        checksPerMonitor,
+        numIps,
+        scheduleEvery,
+        {
+          geo: { location: 'g' },
+          summary: {
+            state: {
+              monitor: { monitor_id: MONITOR_ID },
             },
           },
-          'up',
-          mogrifyNoLocation
-        );
-      });
+        },
+        'up',
+        mogrifyNoLocation
+      );
+    });
 
-      it('has stuff i want it to have', async () => {
-        await pageObjects.uptime.loadDataAndGoToMonitorPage(start, end, MONITOR_ID);
-        await pageObjects.uptime.locationMissingIsDisplayed();
-      });
+    it('renders the location missing popover when monitor has location name, but no geo data', async () => {
+      await pageObjects.uptime.loadDataAndGoToMonitorPage(start, end, MONITOR_ID);
+      await pageObjects.uptime.locationMissingIsDisplayed();
     });
   });
 };
