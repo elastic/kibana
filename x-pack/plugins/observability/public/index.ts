dfcaaa0bc21c1f6a1256ce8f5da69f4f2e1dcbe7
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2020-01-27 15:15:15 -0500

    Completes fleshing out basic observability NP plugin

--- x-pack/plugins/observability/public/index.ts
+++ x-pack/plugins/observability/public/index.ts
@@ -4,6 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { PluginInitializerContext, PluginInitializer } from 'kibana/public';
+import { Plugin, ClientSetup, ClientStart } from './plugin';
+
+export const plugin: PluginInitializer<ClientSetup, ClientStart> = (
+  context: PluginInitializerContext
+) => {
+  return new Plugin(context);
+};
+
 export {
   useTrackPageview,
   useUiTracker,
