60c7a674627d3b6fbb3085d07dbee1b5c0899d72
Jen Huang <its.jenetic@gmail.com>
2020-01-28 12:57:41 -0800

    Fix app routing

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/constants/index.ts
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/constants/index.ts
@@ -3,13 +3,14 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import { PLUGIN_ID } from '../../../../common';
 export {
   PLUGIN_ID,
   EPM_API_ROUTES,
   DEFAULT_AGENT_CONFIG_ID,
   AGENT_CONFIG_SAVED_OBJECT_TYPE,
 } from '../../../../common';
-export const BASE_PATH = '/app/ingest';
+export const BASE_PATH = `/app/${PLUGIN_ID}`;
 export const EPM_PATH = '/epm';
 export const AGENT_CONFIG_PATH = '/configs';
 export const AGENT_CONFIG_DETAILS_PATH = '/configs/';
