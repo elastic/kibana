6c83829ac11cf41083215a31695a27a868472666
Jen Huang <its.jenetic@gmail.com>
2020-01-27 16:34:49 -0800

    Adjust ingest manager base API route

--- x-pack/plugins/ingest_manager/common/constants/routes.ts
+++ x-pack/plugins/ingest_manager/common/constants/routes.ts
@@ -3,10 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { PLUGIN_ID } from './plugin';
-
 // Base API paths
-export const API_ROOT = `/api/${PLUGIN_ID}`;
+export const API_ROOT = `/api/ingest_manager`;
 export const DATASOURCE_API_ROOT = `${API_ROOT}/datasources`;
 export const AGENT_CONFIG_API_ROOT = `${API_ROOT}/agent_configs`;
 export const EPM_API_ROOT = `${API_ROOT}/epm`;
@@ -32,10 +30,10 @@ export const DATASOURCE_API_ROUTES = {
 // Agent config API routes
 export const AGENT_CONFIG_API_ROUTES = {
   LIST_PATTERN: `${AGENT_CONFIG_API_ROOT}`,
-  INFO_PATTERN: `${AGENT_CONFIG_API_ROOT}/{agentConfig}`,
+  INFO_PATTERN: `${AGENT_CONFIG_API_ROOT}/{agentConfigId}`,
   CREATE_PATTERN: `${AGENT_CONFIG_API_ROOT}`,
-  UPDATE_PATTERN: `${AGENT_CONFIG_API_ROOT}/{agentConfig}`,
-  DELETE_PATTERN: `${AGENT_CONFIG_API_ROOT}/{agentConfig}`,
+  UPDATE_PATTERN: `${AGENT_CONFIG_API_ROOT}/{agentConfigId}`,
+  DELETE_PATTERN: `${AGENT_CONFIG_API_ROOT}/{agentConfigId}`,
 };
 
 // Fleet API routes
