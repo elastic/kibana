370b15eec714777e2f061841077b31a743712499
Tre' Seymour <wayne.seymour@elastic.co>
2020-01-29 13:17:35 -0700

    Drop unneeded code.

--- src/dev/code_coverage/shell_scripts/ingest_coverage.sh
+++ src/dev/code_coverage/shell_scripts/ingest_coverage.sh
@@ -11,16 +11,12 @@ export BUILD_ID
 ES_HOST=https://super:changeme@142fea2d3047486e925eb8b223559cae.europe-west1.gcp.cloud.es.io:9243
 export ES_HOST
 
-#TIME_STAMP=$(date -u +%Y%m%d_%H%M%SZ)
-#export TIME_STAMP
-
 for x in jest functional mocha; do
   echo "### Ingesting coverage for ${x}"
 
   COVERAGE_SUMMARY_FILE=target/kibana-coverage/${x}-combined/coverage-summary.json
 
   node scripts/ingest_coverage.js --verbose --path ${COVERAGE_SUMMARY_FILE}
-  echo "### pwd: $(pwd)"
 done
 
 echo "###  Ingesting Code Coverage - Complete"
