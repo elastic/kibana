86cc6352b5f84d81fd8e406f9b0762f97c665e28
Sébastien Loix <sebastien.loix@elastic.co>
2020-01-29 12:35:30 +0530

    [Index template] Fix editor should support mappings types (#55804)

--- x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/lib/extract_mappings_definition.ts
+++ x-pack/legacy/plugins/index_management/public/app/components/mappings_editor/lib/extract_mappings_definition.ts
@@ -6,15 +6,15 @@
 import { isPlainObject } from 'lodash';
 
 import { GenericObject } from '../types';
-import {
-  validateMappingsConfiguration,
-  mappingsConfigurationSchemaKeys,
-} from './mappings_validator';
+import { validateMappingsConfiguration, VALID_MAPPINGS_PARAMETERS } from './mappings_validator';
 
-const ALLOWED_PARAMETERS = [...mappingsConfigurationSchemaKeys, 'dynamic_templates', 'properties'];
+interface MappingsWithType {
+  type?: string;
+  mappings: GenericObject;
+}
 
 const isMappingDefinition = (obj: GenericObject): boolean => {
-  const areAllKeysValid = Object.keys(obj).every(key => ALLOWED_PARAMETERS.includes(key));
+  const areAllKeysValid = Object.keys(obj).every(key => VALID_MAPPINGS_PARAMETERS.includes(key));
 
   if (!areAllKeysValid) {
     return false;
@@ -31,6 +31,29 @@ const isMappingDefinition = (obj: GenericObject): boolean => {
   // we can assume that the mapping is declared at root level (no types)
   return isConfigurationValid && isPropertiesValid && isDynamicTemplatesValid;
 };
+
+const getMappingsDefinitionWithType = (mappings: GenericObject): MappingsWithType[] => {
+  if (isMappingDefinition(mappings)) {
+    // No need to go any further
+    return [{ mappings }];
+  }
+
+  // At this point there must be one or more type mappings
+  const typedMappings = Object.entries(mappings).reduce(
+    (acc: Array<{ type: string; mappings: GenericObject }>, [type, value]) => {
+      if (isMappingDefinition(value)) {
+        acc.push({ type, mappings: value as GenericObject });
+      }
+      return acc;
+    },
+    []
+  );
+
+  return typedMappings;
+};
+
+export const doMappingsHaveType = (mappings: GenericObject = {}): boolean =>
+  getMappingsDefinitionWithType(mappings).filter(({ type }) => type !== undefined).length > 0;
 
 /**
  * 5.x index templates can be created with multiple types.
@@ -72,19 +95,10 @@ const isMappingDefinition = (obj: GenericObject): boolean => {
  *
  * @param mappings The mappings object to validate
  */
-export const extractMappingsDefinition = (mappings: GenericObject = {}): GenericObject | null => {
-  if (isMappingDefinition(mappings)) {
-    // No need to go any further
-    return mappings;
-  }
-
-  // At this point there must be one or more type mappings
-  const typedMappings = Object.values(mappings).reduce((acc: GenericObject[], value) => {
-    if (isMappingDefinition(value)) {
-      acc.push(value as GenericObject);
-    }
-    return acc;
-  }, []);
+export const extractMappingsDefinition = (
+  mappings: GenericObject = {}
+): MappingsWithType | null => {
+  const typedMappings = getMappingsDefinitionWithType(mappings);
 
   // If there are no typed mappings found this means that one of the type must did not pass
   // the "isMappingDefinition()" validation.
