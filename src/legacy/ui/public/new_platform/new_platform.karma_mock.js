0d03ade9eafc5862bdc6da0a800862a2d7ad9656
Joe Reuter <johannes.reuter@elastic.co>
2020-01-30 09:25:47 +0100

    NP Migration: Rollup plugin (#53503)
    * Start shimming rollup plugin
    
    * continued shimming rollup ui
    
    * Remove unnecessarily return
    
    * Register management section
    
    * Replace ui/chrome
    
    * Replace ui/documentation_links
    
    * Replace ui/kfetch and ui/courier
    
    * Start shimming rollup plugin
    
    * continued shimming rollup ui
    
    * Remove unnecessarily return
    
    * Register management section
    
    * Replace ui/chrome
    
    * Replace ui/documentation_links
    
    * Replace ui/kfetch and ui/courier
    
    * Replace ui/notify
    
    * Move ui/ imports to legacy_imports.ts
    
    * Update NP mock for management
    
    * Refactoring
    
    * Read body from error object
    
    * Update setup_environment.js
    
    * Update unit tests
    
    * Get rid of injectI18n
    
    * Replace npStart and npSetup usage to services
    
    * Import search strategy stuff from the top level of the data plugin
    
    * Update unit tests
    
    * Do not prepend the url
    
    * Fix merge conflicts
    
    * Refactoring
    
    * Revert removal of setUserHasLeftApp
    
    * Export getSearchErrorType
    
    * Remove extra wrapper - Router
    
    * Fix cause prop.
    
    * Leave just static imports in legacy_imports.js
    
    * Add TS
    
    * Pass statusCode instead of statusText
    
    * Move template in a separate file
    
    * Move app register to setup
    
    * Add karma mock for management setup
    
    * Add EditorConfigProviderRegistry export
    
    Co-authored-by: Maryia Lapata <mary.lopato@gmail.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/legacy/ui/public/new_platform/new_platform.karma_mock.js
+++ src/legacy/ui/public/new_platform/new_platform.karma_mock.js
@@ -147,6 +147,13 @@ export const npSetup = {
         useChartsTheme: sinon.fake(),
       },
     },
+    management: {
+      sections: {
+        getSection: () => ({
+          registerApp: sinon.fake(),
+        }),
+      },
+    },
   },
 };
 
@@ -165,6 +172,11 @@ export const npStart = {
           register: sinon.fake(),
           deregister: sinon.fake(),
           hasItem: sinon.fake(),
+        }),
+      },
+      sections: {
+        getSection: () => ({
+          registerApp: sinon.fake(),
         }),
       },
     },
--- src/legacy/ui/public/new_platform/new_platform.ts
+++ src/legacy/ui/public/new_platform/new_platform.ts
@@ -32,7 +32,7 @@ import { DevToolsSetup, DevToolsStart } from '../../../../plugins/dev_tools/publ
 import { KibanaLegacySetup, KibanaLegacyStart } from '../../../../plugins/kibana_legacy/public';
 import { HomePublicPluginSetup, HomePublicPluginStart } from '../../../../plugins/home/public';
 import { SharePluginSetup, SharePluginStart } from '../../../../plugins/share/public';
-import { ManagementStart } from '../../../../plugins/management/public';
+import { ManagementSetup, ManagementStart } from '../../../../plugins/management/public';
 import { BfetchPublicSetup, BfetchPublicStart } from '../../../../plugins/bfetch/public';
 import { UsageCollectionSetup } from '../../../../plugins/usage_collection/public';
 import {
@@ -54,6 +54,7 @@ export interface PluginsSetup {
   kibana_legacy: KibanaLegacySetup;
   share: SharePluginSetup;
   usageCollection: UsageCollectionSetup;
+  management: ManagementSetup;
 }
 
 export interface PluginsStart {
