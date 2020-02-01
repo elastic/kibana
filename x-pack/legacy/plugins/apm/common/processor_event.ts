7b65f6a00c9a876682ca469eb3775d7614339edd
zufishanali <zali30@myseneca.ca>
2020-01-31 20:36:22 -0500

    Update processor_event.ts

--- x-pack/legacy/plugins/apm/common/processor_event.ts
+++ x-pack/legacy/plugins/apm/common/processor_event.ts
@@ -4,11 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
 export enum ProcessorEvent {
   transaction = 'transaction',
   error = 'error',
