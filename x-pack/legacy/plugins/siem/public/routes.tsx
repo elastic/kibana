112a64bd4f00c6fc5cfdf775c00e4594d427dead
Patryk Kopyciński <contact@patrykkopycinski.com>
2020-02-02 16:08:09 +0100

    add why-did-you-render

--- x-pack/legacy/plugins/siem/public/routes.tsx
+++ x-pack/legacy/plugins/siem/public/routes.tsx
@@ -12,6 +12,16 @@ import { NotFoundPage } from './pages/404';
 import { HomePage } from './pages/home';
 import { ManageRoutesSpy } from './utils/route/manage_spy_routes';
 
+/* Uncomment only during debugging */
+// const whyDidYouRender = require('@welldone-software/why-did-you-render'); // eslint-disable-line
+// whyDidYouRender(React, {
+//   exclude: [/^ColumnHeaders/, /^Connect/, /^BarValuesComponent/, /^BarGeometries/],
+//   trackAllPureComponents: true,
+//   trackHooks: false,
+//   collapseGroups: true,
+//   // logOnDifferentValues: true,
+// });
+
 interface RouterProps {
   history: History;
 }
