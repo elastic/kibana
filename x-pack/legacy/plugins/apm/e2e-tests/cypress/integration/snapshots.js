29448f80fb751d12803b9d953a19854baee747e0
Manuel de la Peña <mdelapenya@gmail.com>
2020-02-04 00:03:29 +0100

    chore: use cypres + webpack + cucumber scaffolding
    See https://github.com/TheBrainFamily/cypress-cucumber-webpack-typescript-example

---
+++ x-pack/legacy/plugins/apm/e2e-tests/cypress/integration/snapshots.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+module.exports = {
+  'When clicking opbeans-go service': {
+    'transaction duration charts': {
+      'should have correct y-axis ticks': {
+        '1': '3.7 min',
+        '2': '1.8 min',
+        '3': '0.0 min'
+      }
+    }
+  },
+  __version: '3.4.1'
+};
