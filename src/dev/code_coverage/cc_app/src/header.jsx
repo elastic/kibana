2336dc4a58f49e9b0fd624e5d168330518e85a8e
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-04 14:23:45 -0700

    Add the logo to the header

--- src/dev/code_coverage/cc_app/src/header.jsx
+++ src/dev/code_coverage/cc_app/src/header.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-
+import logo from './logo_kibana_32_color.svg'
 export default function Header({ url }) {
   return (
     <header className="App-header">
-      {/*<img src={logo} className="App-logo" alt="logo"/>*/}
       <a
         className="App-link"
         href={url}
         target="_blank"
         rel="noopener noreferrer"
       >
-        Build Stats Dashboard
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{ height: 20, width: 20}}
+        />
+
+        Build Stats
       </a>
     </header>
   );
