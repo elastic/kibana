87a1784f2cbad834aeb2ab2904d575898c1209fd
John Schulz <john.schulz@elastic.co>
2020-02-04 12:50:40 -0500

    Don't create BASE_URL value based on PLUGIN_ID

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/constants/index.ts
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/constants/index.ts
@@ -3,14 +3,18 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { PLUGIN_ID } from '../../../../common';
+
 export {
   PLUGIN_ID,
   EPM_API_ROUTES,
   DEFAULT_AGENT_CONFIG_ID,
   AGENT_CONFIG_SAVED_OBJECT_TYPE,
 } from '../../../../common';
-export const BASE_PATH = `/app/${PLUGIN_ID}`;
+
+// intially tried `/app/${PLUGIN_ID}` but that returned `/app/undefined`
+// Could be due to import/import()/export or something else
+// thankfully, we're not likely to change our id and it's only one place to update
+export const BASE_PATH = '/app/ingestManager';
 export const EPM_PATH = '/epm';
 export const AGENT_CONFIG_PATH = '/configs';
 export const AGENT_CONFIG_DETAILS_PATH = '/configs/';
