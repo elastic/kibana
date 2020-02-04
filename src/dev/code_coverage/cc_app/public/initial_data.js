78049e41f66d84d72f85563d0532159f5780223c
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-04 16:22:01 -0700

    A lil more readable, ide-wise :)

--- src/dev/code_coverage/cc_app/public/initial_data.js
+++ src/dev/code_coverage/cc_app/public/initial_data.js
@@ -33,10 +33,10 @@ window.initialData = {
     },
   ],
   buildStats: {
-    url:
-      'https://build-stats.elastic.co/app/kibana#/dashboard/02b9d310-9513-11e8-a9c0-db5f285c257f?_g=(refreshInterval%3A(pause%3A!f%2Cvalue%3A10000)%2Ctime%3A(from%3Anow%2Fd%2Cmode%3Aquick%2Cto%3Anow%2Fd))',
+    url: `
+https://build-stats.elastic.co/app/kibana#/dashboard/02b9d310-9513-11e8-a9c0-db5f285c257f?_g=
+(refreshInterval%3A(pause%3A!f%2Cvalue%3A10000)%2Ctime%3A(from%3Anow%2Fd%2Cmode%3Aquick%2Cto%3Anow%2Fd))
+`,
   },
-  historicalItems: {
-    a: 'abc',
-  },
+  historicalItems: {},
 };
