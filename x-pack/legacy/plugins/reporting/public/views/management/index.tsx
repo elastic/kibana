88bf856947812726cc0b3fc8c2003e60adea638b
Joel Griffith <joel.griffith@elastic.co>
2020-02-04 13:48:48 -0800

    WIP: moving over to better license checks for np

--- x-pack/legacy/plugins/reporting/public/views/management/index.tsx
+++ x-pack/legacy/plugins/reporting/public/views/management/index.tsx
@@ -10,35 +10,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { I18nContext } from 'ui/i18n';
 import { npSetup } from 'ui/new_platform';
+import {
+  LicensingPluginSetup,
+  LICENSE_CHECK_STATE,
+} from '../../../../../../plugins/licensing/public';
 import { ReportListing } from '../../components/report_listing';
 
 const management = npSetup.plugins.management;
+// @ts-ignore TS doesn't have types for this due to breaking OS builds
+const licensing = npSetup.plugins.licensing as LicensingPluginSetup;
 const title = i18n.translate('xpack.reporting.management.reportingTitle', {
   defaultMessage: 'Reporting',
 });
 
-// TODO: Get these dang license checks in pronto-tonto
-const testSection = management.sections.getSection('kibana');
+licensing.license$.subscribe(license => {
+  const { state, message } = license.check('reporting', 'gold');
+  const isAvailable = state === LICENSE_CHECK_STATE.Valid;
 
-testSection!.registerApp({
-  id: 'test-management',
-  title,
-  order: 15,
-  mount(params) {
-    params.setBreadcrumbs([{ text: 'Boot licker' }]);
-    ReactDOM.render(
-      <I18nContext>
-        <ReportListing
-          badLicenseMessage={xpackInfo.get('features.reporting.management.message')}
-          showLinks={xpackInfo.get('features.reporting.management.showLinks')}
-          enableLinks={xpackInfo.get('features.reporting.management.enableLinks')}
-        />
-      </I18nContext>,
-      params.element
-    );
+  if (isAvailable) {
+    const kibanaSection = management.sections.getSection('kibana');
+    kibanaSection!.registerApp({
+      id: 'test-management',
+      title,
+      order: 15,
+      mount(params) {
+        params.setBreadcrumbs([{ text: 'Boot licker' }]);
+        ReactDOM.render(
+          <I18nContext>
+            <ReportListing
+              badLicenseMessage={message || ''}
+              showLinks={xpackInfo.get('features.reporting.management.showLinks')}
+              enableLinks={xpackInfo.get('features.reporting.management.enableLinks')}
+            />
+          </I18nContext>,
+          params.element
+        );
 
-    return () => {
-      ReactDOM.unmountComponentAtNode(params.element);
-    };
-  },
+        return () => {
+          ReactDOM.unmountComponentAtNode(params.element);
+        };
+      },
+    });
+  }
 });
