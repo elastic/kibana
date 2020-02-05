cefb12069650e6f9ca381d7105589abdbd7073ed
Mike Cote <mikecote@users.noreply.github.com>
2020-02-05 14:16:53 -0500

    Add other part to plugin.test.ts

--- x-pack/plugins/actions/server/plugin.test.ts
+++ x-pack/plugins/actions/server/plugin.test.ts
@@ -9,7 +9,77 @@ import { coreMock } from '../../../../src/core/server/mocks';
 import { licensingMock } from '../../licensing/server/mocks';
 import { encryptedSavedObjectsMock } from '../../encrypted_saved_objects/server/mocks';
 
+/**
+ * HACK: These tests were put together to ensuire the routeHandlerContext and the function
+ * "getActionsClientWithRequest" throws when needed. There's a lot of blockers for writing
+ * a proper test like misisng plugin start/setup mocks for taskManager and event log.
+ * This test contains what is needed to get to the necessary function within setup() and start().
+ */
 describe('Actions Plugin', () => {
+  describe('setup()', () => {
+    describe('routeHandlerContext.getActionsClient()', () => {
+      it('should not throw error when ESO plugin not using a generated key', async () => {
+        const context = coreMock.createPluginInitializerContext();
+        const plugin = new ActionsPlugin(context);
+
+        const coreSetup = coreMock.createSetup();
+        await plugin.setup(coreSetup, {
+          taskManager: {
+            registerTaskDefinitions: jest.fn(),
+          } as any,
+          encryptedSavedObjects: {
+            ...encryptedSavedObjectsMock.createSetup(),
+            usingEphemeralEncryptionKey: false,
+          },
+          licensing: licensingMock.createSetup(),
+          event_log: {
+            getLogger: jest.fn(),
+            registerProviderActions: jest.fn(),
+          } as any,
+        });
+
+        expect(coreSetup.http.registerRouteHandlerContext).toHaveBeenCalledTimes(1);
+        const handler = coreSetup.http.registerRouteHandlerContext.mock.calls[0];
+        expect(handler[0]).toEqual('actions');
+
+        const actionsContextHandler = await (handler[1](
+          {
+            core: coreMock.createStart(),
+          } as any,
+          {} as any,
+          {} as any
+        ) as any)!;
+        actionsContextHandler.getActionsClient();
+      });
+
+      it('should throw error when ESO plugin using a generated key', async () => {
+        const context = coreMock.createPluginInitializerContext();
+        const plugin = new ActionsPlugin(context);
+
+        const coreSetup = coreMock.createSetup();
+        await plugin.setup(coreSetup, {
+          taskManager: {
+            registerTaskDefinitions: jest.fn(),
+          } as any,
+          encryptedSavedObjects: encryptedSavedObjectsMock.createSetup(),
+          licensing: licensingMock.createSetup(),
+          event_log: {
+            getLogger: jest.fn(),
+            registerProviderActions: jest.fn(),
+          } as any,
+        });
+
+        expect(coreSetup.http.registerRouteHandlerContext).toHaveBeenCalledTimes(1);
+        const handler = coreSetup.http.registerRouteHandlerContext.mock.calls[0];
+        expect(handler[0]).toEqual('actions');
+
+        const actionsContextHandler = await (handler[1]({} as any, {} as any, {} as any) as any)!;
+        expect(() => actionsContextHandler.getActionsClient()).toThrowErrorMatchingInlineSnapshot(
+          `"Unable to create actions client due to the Encrypted Saved Objects plugin using an ephemeral encryption key. Please set xpack.encryptedSavedObjects.encryptionKey in kibana.yml"`
+        );
+      });
+    });
+  });
   describe('start()', () => {
     describe('getActionsClientWithRequest()', () => {
       it('should not throw error when ESO plugin not using a generated key', async () => {
@@ -43,7 +113,7 @@ describe('Actions Plugin', () => {
         const context = coreMock.createPluginInitializerContext();
         const plugin = new ActionsPlugin(context);
 
-        const esoPluginSetup = encryptedSavedObjectsMock.createSetup()
+        const esoPluginSetup = encryptedSavedObjectsMock.createSetup();
         await plugin.setup(coreMock.createSetup(), {
           taskManager: {
             registerTaskDefinitions: jest.fn(),
