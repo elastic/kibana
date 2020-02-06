2b17955ef92747f6768142db341b498690d16979
John Gress <john.gress@logrhythm.com>
2020-02-06 13:05:26 -0700

    A better way to allow root builds.

--- src/setup_node_env/root/index.js
+++ src/setup_node_env/root/index.js
@@ -17,8 +17,7 @@
  * under the License.
  */
 
-//var force = require('./force')(process.argv);
-var force = 1;
+var force = require('./force')(process.argv);
 
 var uid = process.getuid && process.getuid();
 var isRoot = require('./is_root')(uid);
