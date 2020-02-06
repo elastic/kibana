ac82c35f2ec361e65e331b83293625b0b6d4e648
Luke Elmers <luke.elmers@elastic.co>
2020-02-06 16:44:59 -0700

    Clean up legacy dependencies in agg types.

--- src/legacy/core_plugins/data/public/search/aggs/utils.ts
+++ src/legacy/core_plugins/data/public/search/aggs/utils.ts
@@ -68,3 +68,38 @@ function _parseWithBase(value: string, baseInterval: string) {
     return false;
   }
 }
+
+// An inlined version of angular.toJSON()
+// source: https://github.com/angular/angular.js/blob/master/src/Angular.js#L1312
+// @internal
+export function toAngularJSON(obj: any, pretty?: any): string {
+  if (obj === undefined) return '';
+  if (typeof pretty === 'number') {
+    pretty = pretty ? 2 : null;
+  }
+  return JSON.stringify(obj, toJsonReplacer, pretty);
+}
+
+function isWindow(obj: any) {
+  return obj && obj.window === obj;
+}
+
+function isScope(obj: any) {
+  return obj && obj.$evalAsync && obj.$watch;
+}
+
+function toJsonReplacer(key: any, value: any) {
+  let val = value;
+
+  if (typeof key === 'string' && key.charAt(0) === '$' && key.charAt(1) === '$') {
+    val = undefined;
+  } else if (isWindow(value)) {
+    val = '$WINDOW';
+  } else if (value && window.document === value) {
+    val = '$DOCUMENT';
+  } else if (isScope(value)) {
+    val = '$SCOPE';
+  }
+
+  return val;
+}
