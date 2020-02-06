3746cb681687ecedd420531a4cfb0580bdcf39c5
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-06 14:21:18 -0700

    Clear the outfile first.

--- src/dev/code_coverage/shell_scripts/query_app_inital_data.sh
+++ src/dev/code_coverage/shell_scripts/query_app_inital_data.sh
@@ -13,14 +13,20 @@ export PAST_BUILD_ID
 OUT_FILE='bootstrapped.txt'
 export OUT_FILE
 
-queryDataStore () {
+clearOutFile() {
+  echo "### Clearing the 'outfile', ${OUTFILE}"
+  touch $OUT_FILE
+  echo "" >$OUT_FILE
+}
+
+queryDataStore() {
+  clearOutFile
+
   # Sink errors to /dev/null...THEE BITBUCKET :)
   for x in `seq ${PAST_BUILD_ID} ${CURRENT_BUILD_ID}`
   do
     gsutil ls -a ${BUCKET}/${x} 2>/dev/null | grep Z 1>> $OUT_FILE
   done
+  echo "### Querying App Initial Data - Complete"
 }
 queryDataStore
-
-
-echo "### Querying App Initial Data - Complete"
