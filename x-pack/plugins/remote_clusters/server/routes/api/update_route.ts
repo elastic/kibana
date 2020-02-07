c9d0c5db05abfa3fdebe8b14129feb239a38e82f
Alison Goryachev <alisonmllr20@gmail.com>
2020-02-06 22:55:41 -0500

    remove todo comment

--- x-pack/plugins/remote_clusters/server/routes/api/update_route.ts
+++ x-pack/plugins/remote_clusters/server/routes/api/update_route.ts
@@ -17,7 +17,6 @@ import { licensePreRoutingFactory } from '../../lib/license_pre_routing_factory'
 import { isEsError } from '../../lib/is_es_error';
 
 export const register = (deps: RouteDependencies): void => {
-  // TODO there are other settings that can be specified for a remote cluster via console/API that I think might cause issues when editing
   const updateHandler: RequestHandler<any, any, any> = async (ctx, request, response) => {
     try {
       const callAsCurrentUser = ctx.core.elasticsearch.dataClient.callAsCurrentUser;
