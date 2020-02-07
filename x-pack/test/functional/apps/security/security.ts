b7999aab60789e1ef34132f537537445aa7a54f5
Larry Gregory <larry.gregory@elastic.co>
2020-02-07 10:58:42 -0500

    fix logout url in nav control service

--- x-pack/test/functional/apps/security/security.ts
+++ x-pack/test/functional/apps/security/security.ts
@@ -5,11 +5,15 @@
  */
 
 import expect from '@kbn/expect';
+import { parse } from 'url';
+import { FtrProviderContext } from '../../ftr_provider_context';
 
-export default function({ getService, getPageObjects }) {
+export default function({ getService, getPageObjects }: FtrProviderContext) {
+  const browser = getService('browser');
   const esArchiver = getService('esArchiver');
-  const PageObjects = getPageObjects(['security']);
+  const PageObjects = getPageObjects(['security', 'spaceSelector']);
   const testSubjects = getService('testSubjects');
+  const spaces = getService('spaces');
 
   describe('Security', function() {
     this.tags('smoke');
@@ -45,6 +49,37 @@ export default function({ getService, getPageObjects }) {
 
         const logoutMessage = await testSubjects.getVisibleText('loginInfoMessage');
         expect(logoutMessage).to.eql('You have logged out of Kibana.');
+      });
+
+      describe('within a non-default space', async () => {
+        before(async () => {
+          await PageObjects.security.forceLogout();
+
+          await spaces.create({
+            id: 'some-space',
+            name: 'Some non-default space',
+            disabledFeatures: [],
+          });
+        });
+
+        after(async () => {
+          await spaces.delete('some-space');
+        });
+
+        it('logging out of a non-default space redirects to the login page at the server root', async () => {
+          await PageObjects.security.login(null, null, {
+            expectSpaceSelector: true,
+          });
+
+          await PageObjects.spaceSelector.clickSpaceCard('some-space');
+          await PageObjects.spaceSelector.expectHomePage('some-space');
+
+          await PageObjects.security.logout();
+
+          const currentUrl = await browser.getCurrentUrl();
+          const url = parse(currentUrl);
+          expect(url.pathname).to.eql('/login');
+        });
       });
     });
   });
