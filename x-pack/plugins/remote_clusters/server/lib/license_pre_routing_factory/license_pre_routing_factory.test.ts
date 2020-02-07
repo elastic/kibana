18a36f3094f65bb748071495848af079f05c0bb1
Alison Goryachev <alisonmllr20@gmail.com>
2020-02-06 22:16:09 -0500

    fix TS

--- x-pack/plugins/remote_clusters/server/lib/license_pre_routing_factory/license_pre_routing_factory.test.ts
+++ x-pack/plugins/remote_clusters/server/lib/license_pre_routing_factory/license_pre_routing_factory.test.ts
@@ -7,21 +7,29 @@
 import expect from '@kbn/expect';
 import { kibanaResponseFactory } from '../../../../../../src/core/server';
 import { licensePreRoutingFactory } from '../license_pre_routing_factory';
+import { LicenseStatus } from '../../types';
 
 describe('licensePreRoutingFactory()', () => {
-  let mockDeps;
-  let licenseStatus;
+  let mockDeps: any;
+  let mockContext: any;
+  let licenseStatus: LicenseStatus;
 
   beforeEach(() => {
     mockDeps = { getLicenseStatus: () => licenseStatus };
+    mockContext = {
+      core: {},
+      actions: {},
+      licensing: {},
+    };
   });
 
   describe('status is not valid', () => {
     it('replies with 403', () => {
       licenseStatus = { valid: false };
-      const routeWithLicenseCheck = licensePreRoutingFactory(mockDeps, () => {});
-      const stubRequest = {};
-      const response = routeWithLicenseCheck({}, stubRequest, kibanaResponseFactory);
+      const stubRequest: any = {};
+      const stubHandler: any = () => {};
+      const routeWithLicenseCheck = licensePreRoutingFactory(mockDeps, stubHandler);
+      const response: any = routeWithLicenseCheck(mockContext, stubRequest, kibanaResponseFactory);
       expect(response.status).to.be(403);
     });
   });
@@ -29,9 +37,10 @@ describe('licensePreRoutingFactory()', () => {
   describe('status is valid', () => {
     it('replies with nothing', () => {
       licenseStatus = { valid: true };
-      const routeWithLicenseCheck = licensePreRoutingFactory(mockDeps, () => null);
-      const stubRequest = {};
-      const response = routeWithLicenseCheck({}, stubRequest, kibanaResponseFactory);
+      const stubRequest: any = {};
+      const stubHandler: any = () => null;
+      const routeWithLicenseCheck = licensePreRoutingFactory(mockDeps, stubHandler);
+      const response = routeWithLicenseCheck(mockContext, stubRequest, kibanaResponseFactory);
       expect(response).to.be(null);
     });
   });
