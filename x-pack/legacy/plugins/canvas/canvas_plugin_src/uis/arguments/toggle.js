1f70cf31479e8aeefd38028e3cc5bdae499c4737
Maggie Ghamry <maggie.ghamry@elastic.co>
2020-02-07 16:03:47 -0500

    Update toggle.js
    Update to prevent text for "Open links in new tab from wrapping" - the example from the horizontal bar chart is quite differently, and reads from "axisConfig" - when I changed the argType to "axisConfig", the layout was the same, but I'll need some input on which specific styles to add to the "ToggleArgInput" - I think this is where the style updates need to occur to get the toggle to stay on the same line, but may be wrong.

--- x-pack/legacy/plugins/canvas/canvas_plugin_src/uis/arguments/toggle.js
+++ x-pack/legacy/plugins/canvas/canvas_plugin_src/uis/arguments/toggle.js
@@ -19,18 +19,21 @@ const ToggleArgInput = ({ onValueChange, argValue, argId, renderError, typeInsta
     return null;
   }
   return (
-    <EuiFormRow display="columnCompressedSwitch">
-      <EuiSwitch
-        compressed
-        id={argId}
-        checked={argValue}
-        onChange={handleChange}
-        className="canvasArg__switch"
-        aria-label={typeInstance.displayName}
-        label=""
-        showLabel={false}
-      />
-    </EuiFormRow>
+    <div>
+      <EuiFormRow display="rowCompressed">
+        <EuiSwitch
+          compressed
+          id={argId}
+          checked={argValue}
+          onChange={handleChange}
+          className="canvasArg__form"
+          aria-label={typeInstance.displayName}
+          resize="none"
+          label=""
+          showLabel={false}
+        />
+      </EuiFormRow>
+    </div>
   );
 };
 
@@ -45,6 +48,6 @@ export const toggle = () => ({
   name: 'toggle',
   displayName: strings.getDisplayName(),
   help: strings.getHelp(),
-  simpleTemplate: templateFromReactComponent(ToggleArgInput),
+  template: templateFromReactComponent(ToggleArgInput),
   default: 'false',
 });
