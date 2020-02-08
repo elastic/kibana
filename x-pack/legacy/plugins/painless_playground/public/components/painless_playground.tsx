27e5cacaa0f2a2447db01b627ce212a33a3aca97
CJ Cenizal <cj@cenizal.com>
2020-02-07 16:33:09 -0800

    Live-update output as the user enters input for a faster iteration loop. Use heart script as the example script.

--- x-pack/legacy/plugins/painless_playground/public/components/painless_playground.tsx
+++ x-pack/legacy/plugins/painless_playground/public/components/painless_playground.tsx
@@ -3,7 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { debounce } from 'lodash';
 import {
   EuiCodeBlock,
   EuiFlexGroup,
@@ -21,41 +22,93 @@ import { Output } from './output';
 import { MainControls } from './main_controls';
 import { Editor } from './editor';
 
+let _mostRecentRequestId = 0;
+
+const submit = async (code, context, contextSetup, executeCode, setResponse) => {
+  // Prevent an older request that resolves after a more recent request from clobbering it.
+  // We store the resulting ID in this closure for comparison when the request resolves.
+  const requestId = ++_mostRecentRequestId;
+
+  try {
+    localStorage.setItem('painlessPlaygroundCode', code);
+    localStorage.setItem('painlessPlaygroundContext', context);
+    localStorage.setItem('painlessPlaygroundContextSetup', JSON.stringify(contextSetup));
+    const res = await executeCode(buildRequestPayload(code, context, contextSetup));
+
+    if (_mostRecentRequestId === requestId) {
+      setResponse(res);
+    }
+  } catch (error) {
+    if (_mostRecentRequestId === requestId) {
+      setResponse({
+        error,
+      });
+    }
+  }
+};
+
+const debouncedSubmit = debounce(submit, 800);
+
+// Render a heart as an example.
+const exampleScript = `
+def result = '';
+int charCount = 0;
+
+int n = 10;
+int threshold = n*n/2;
+int dimension = 3*n/2;
+
+for (int i = -dimension; i <= n; i++) {
+  int a = -n/2-i;
+
+  for (int j = -dimension; j <= dimension; j++) {
+    int b = n/2-j;
+    int c = -n/2-j;
+
+    def isHeartVentricles = (Math.abs(i) + Math.abs(j) < n);
+    def isRightAtrium = ((a * a) + (b * b) <= threshold);
+    def isLeftAtrium =  ((a * a) + (c * c) <= threshold);
+
+    if (isHeartVentricles || isRightAtrium || isLeftAtrium) {
+      result += "* ";
+    } else {
+      result += ". ";
+    }
+
+    // Make sure the heart doesn't deform as the container changes width.
+    charCount++;
+    if (charCount % 31 === 0) {
+      result += "\\\\n";
+    }
+  }
+}
+
+return result;
+`;
+
 export function PainlessPlayground({
   executeCode,
 }: {
   executeCode: (payload: Request) => Promise<Response>;
 }) {
-  const [code, setCode] = useState(
-    getFromLocalStorage('painlessPlaygroundCode', 'return "Hello painless world!"')
-  );
+  const [code, setCode] = useState(getFromLocalStorage('painlessPlaygroundCode', exampleScript));
+
   const [response, setResponse] = useState<Response>({});
+
   const [context, setContext] = useState(
     getFromLocalStorage('painlessPlaygroundContext', 'painless_test_without_params')
   );
+
   const [contextSetup, setContextSetup] = useState(
     getFromLocalStorage('painlessPlaygroundContextSetup', {}, true)
   );
 
   const [showRequestFlyout, setShowRequestFlyout] = useState(false);
 
-  const buildRequestPayloadPreview = () => buildRequestPayload(code, context, contextSetup);
-
-  const submit = async () => {
-    try {
-      localStorage.setItem('painlessPlaygroundCode', code);
-      localStorage.setItem('painlessPlaygroundContext', context);
-      localStorage.setItem('painlessPlaygroundContextSetup', JSON.stringify(contextSetup));
-      const res = await executeCode(buildRequestPayloadPreview());
-      setResponse(res);
-    } catch (e) {
-      setResponse({
-        error: {
-          message: e.message,
-        },
-      });
-    }
-  };
+  // Live-update the output as the user changes the input code.
+  useEffect(() => {
+    debouncedSubmit(code, context, contextSetup, executeCode, setResponse);
+  }, [code, context, contextSetup, executeCode]);
 
   const toggleViewRequestFlyout = () => {
     setShowRequestFlyout(!showRequestFlyout);
@@ -88,7 +141,7 @@ export function PainlessPlayground({
       </EuiFlexGroup>
 
       <MainControls
-        submit={submit}
+        submit={() => submit(code, context, contextSetup, executeCode, setResponse)}
         disabled={code.trim() === ''}
         toggleFlyout={toggleViewRequestFlyout}
         isFlyoutOpen={showRequestFlyout}
@@ -100,14 +153,14 @@ export function PainlessPlayground({
             <EuiTitle>
               <h3>
                 {i18n.translate('xpack.painless_playground.flyoutTitle', {
-                  defaultMessage: 'Console Request',
+                  defaultMessage: 'Test script request',
                 })}
               </h3>
             </EuiTitle>
             <EuiSpacer size="s" />
             <EuiCodeBlock language="json" paddingSize="s" isCopyable>
               {'POST _scripts/painless/_execute\n'}
-              {formatJson(buildRequestPayloadPreview())}
+              {formatJson(buildRequestPayload(code, context, contextSetup))}
             </EuiCodeBlock>
           </EuiPageContent>
         </EuiFlyout>
