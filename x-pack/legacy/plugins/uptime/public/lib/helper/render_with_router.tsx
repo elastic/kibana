1a47ed9017a09a4df4ba0b558c2f1cb7e2a08ff2
shahzad <shahzad31comp@gmail.com>
2020-02-10 16:32:20 +0100

    PR feedback

--- x-pack/legacy/plugins/uptime/public/lib/helper/render_with_router.tsx
+++ x-pack/legacy/plugins/uptime/public/lib/helper/render_with_router.tsx
@@ -4,40 +4,36 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import { Router } from 'react-router-dom';
 import { MemoryHistory } from 'history/createMemoryHistory';
 import { createMemoryHistory } from 'history';
 import { mountWithIntl, renderWithIntl, shallowWithIntl } from 'test_utils/enzyme_helpers';
 
-export const renderWithRouter = (Component: any, customHistory?: MemoryHistory) => {
-  if (customHistory) {
-    return renderWithIntl(<Router history={customHistory}>{Component}</Router>);
-  }
-  const history = createMemoryHistory();
-  history.location.key = 'TestKeyForTesting';
-
-  return renderWithIntl(<Router history={history}>{Component}</Router>);
-};
-
-export const shallowWithRouter = (Component: any, customHistory?: MemoryHistory) => {
-  if (customHistory) {
-    return shallowWithIntl(<Router history={customHistory}>{Component}</Router>);
-  }
-  const history = createMemoryHistory();
-  history.location.key = 'TestKeyForTesting';
-
-  return shallowWithIntl(<Router history={history}>{Component}</Router>);
-};
-
-export const mountWithRouter = (Component: any, customHistory?: MemoryHistory) => {
+const helperWithRouter: <R>(
+  helper: (node: ReactElement) => R,
+  component: ReactElement,
+  customHistory?: MemoryHistory
+) => R = (helper, component, customHistory) => {
   if (customHistory) {
     customHistory.location.key = 'TestKeyForTesting';
-    return mountWithIntl(<Router history={customHistory}>{Component}</Router>);
+    return helper(<Router history={customHistory}>{component}</Router>);
   }
   const history = createMemoryHistory();
   history.location.key = 'TestKeyForTesting';
 
-  return mountWithIntl(<Router history={history}>{Component}</Router>);
+  return helper(<Router history={history}>{component}</Router>);
+};
+
+export const renderWithRouter = (component: ReactElement, customHistory?: MemoryHistory) => {
+  return helperWithRouter(renderWithIntl, component, customHistory);
+};
+
+export const shallowWithRouter = (component: ReactElement, customHistory?: MemoryHistory) => {
+  return helperWithRouter(shallowWithIntl, component, customHistory);
+};
+
+export const mountWithRouter = (component: ReactElement, customHistory?: MemoryHistory) => {
+  return helperWithRouter(mountWithIntl, component, customHistory);
 };
