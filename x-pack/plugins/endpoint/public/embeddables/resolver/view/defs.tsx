af1af1809830e151b57109d91c4587a6e9fba1c6
Brent Kimmel <bkimmel@endgame.com>
2020-02-10 17:00:54 -0500

    working WIP

---
+++ x-pack/plugins/endpoint/public/embeddables/resolver/view/defs.tsx
@@ -0,0 +1,99 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { memo } from 'react';
+
+/*
+*Next steps: 
+    1. Arbitrage of colors from EUI pallette
+    2. Use a color library
+*/
+enum ColorTypes {
+  OK = '#0068ff',
+  OKDarker = '#152c51',
+  /* TODO: define other colors by meaning like so
+  Warning = 'hsl(62, 100%, 51%)',
+  Danger = 'hsl(0, 85%, 57%)',
+  Attention = 'hsl(32, 100%, 51%)',
+  Enabled = 'hsl(0, 0%, 100%)',
+  Disabled = 'hsl(0, 0%, 85%)',
+  */
+}
+
+// PaintServers: Where color pallettes and other similar concerns are exposed to the component
+const PaintServers = memo(() => (
+  <>
+    <linearGradient
+      id="OK_userSpaceNWtoSE_Solid"
+      x1="-50"
+      y1="-50"
+      x2="50"
+      y2="50"
+      spreadMethod="pad"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop offset="0%" stopColor={ColorTypes.OK} stopOpacity="1" />
+      <stop offset="100%" stopColor={ColorTypes.OK} stopOpacity="1" />
+    </linearGradient>
+    <linearGradient
+      id="OKDarker_userSpaceNWtoSE_Solid"
+      x1="-50"
+      y1="-50"
+      x2="50"
+      y2="50"
+      spreadMethod="pad"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop offset="0%" stopColor={ColorTypes.OKDarker} stopOpacity="1" />
+      <stop offset="100%" stopColor={ColorTypes.OKDarker} stopOpacity="1" />
+    </linearGradient>
+  </>
+));
+
+// SymbolsAndShapes: defs that define shapes, masks and other spatial elements
+const SymbolsAndShapes = memo(() => (
+  <>
+    <symbol id="node_icon_curve" viewBox="0 0 108.6889 29.31389">
+      <g transform="translate(-52.244835,-115.27758)">
+        <g
+          fill="url(#OKDarker_userSpaceNWtoSE_Solid)"
+          fillOpacity="1"
+          paintOrder="normal"
+          strokeWidth="0.79375"
+          strokeMiterlimit="4"
+        >
+          <g transform="matrix(0.26458333,0,0,0.26458333,53.672616,-135.2738)">
+            <path
+              strokeWidth="3.00000004"
+              stroke="url(#OK_userSpaceNWtoSE_Solid)"
+              strokeLinecap="butt"
+              strokeLinejoin="round"
+              strokeOpacity="0.94117647"
+              d="M 3,1014.0043 C 1.9285712,955.24249 2.5,954.53017 50,955.42048 h 300 c 47.14282,-1.33548 47.71427,-0.62325 47,58.58382"
+            />
+            <path
+              strokeWidth="3.00000004"
+              stroke="url(#OK_userSpaceNWtoSE_Solid)"
+              strokeLinecap="butt"
+              strokeLinejoin="round"
+              strokeOpacity="0.94117647"
+              d="m 397,1002.3622 c 1.07142,47.1428 0.5,47.7143 -47,47 H 50 c -47.1428203,1.0714 -47.71427,0.4994 -47,-47"
+            />
+          </g>
+        </g>
+      </g>
+    </symbol>
+  </>
+));
+
+export const SymbolDefinitions = memo(() => (
+  <svg className="resolver_defs">
+    <defs>
+      <PaintServers />
+      <SymbolsAndShapes />
+    </defs>
+  </svg>
+));
