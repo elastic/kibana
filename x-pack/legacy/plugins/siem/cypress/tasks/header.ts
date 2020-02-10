f746414d3ee750b672af1a8704f01a4b94950918
Gloria Hornero <snootchie.boochies@gmail.com>
2020-02-10 14:16:48 +0100

    fixes events viewer tests

--- x-pack/legacy/plugins/siem/cypress/tasks/header.ts
+++ x-pack/legacy/plugins/siem/cypress/tasks/header.ts
@@ -4,6 +4,15 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { KQL_INPUT } from '../screens/header';
+import { DEFAULT_TIMEOUT } from '../tasks/login';
+
 export const navigateFromHeaderTo = (page: string) => {
   cy.get(page).click({ force: true });
+};
+
+export const clearSearchBar = () => {
+  cy.get(KQL_INPUT, { DEFAULT_TIMEOUT })
+    .clear()
+    .type('{enter}');
 };
