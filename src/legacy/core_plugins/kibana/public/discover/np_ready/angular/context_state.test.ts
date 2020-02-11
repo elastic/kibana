cd274ed64c8c6448d69e4048aacf28cd9421e018
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-02-11 05:12:51 +0100

    Add basic test

---
+++ src/legacy/core_plugins/kibana/public/discover/np_ready/angular/context_state.test.ts
@@ -0,0 +1,45 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { getState } from './context_state';
+
+describe('Discover Context State', () => {
+  test('getState', () => {
+    const state = getState('4', 'time');
+    expect(state.appState.getState()).toMatchInlineSnapshot(`
+      Object {
+        "columns": Array [
+          "_source",
+        ],
+        "filters": Array [],
+        "predecessorCount": 4,
+        "sort": Array [
+          "time",
+          "desc",
+        ],
+        "successorCount": 4,
+      }
+    `);
+    expect(state.globalState.getState()).toMatchInlineSnapshot(`null`);
+    expect(state.start).toBeDefined();
+    expect(state.stop).toBeDefined();
+    expect(state.getGlobalFilters()).toStrictEqual([]);
+    expect(state.getAppFilters()).toStrictEqual([]);
+  });
+});
