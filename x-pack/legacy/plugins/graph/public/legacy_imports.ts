5d3922fb9ec0b3e55df8b64cab503e8cc3443667
Maryia Lapata <mary.lopato@gmail.com>
2020-02-11 12:59:56 +0300

    Remove confirm modal directive and factory (#56846) (#57291)
    * Graph: replace confirmModal
    
    * Remove confirmModal from visualize
    
    * Remove confirmModal from dashboard
    
    * Remove confirm_modal
    
    * Remove confirmModalPromises
    
    * Replace confirmModal
    
    * FIx TS
    
    * Add data-test-subj for graph confirm modal
    
    * Update public.api.md
    
    * Remove unused translation
    
    * Update mock test

--- x-pack/legacy/plugins/graph/public/legacy_imports.ts
+++ x-pack/legacy/plugins/graph/public/legacy_imports.ts
@@ -10,8 +10,6 @@ export { SavedObject, SavedObjectKibanaServices } from 'ui/saved_objects/types';
 // @ts-ignore
 export { createTopNavDirective, createTopNavHelper } from 'ui/kbn_top_nav/kbn_top_nav';
 // @ts-ignore
-export { confirmModalFactory } from 'ui/modals/confirm_modal';
-// @ts-ignore
 export { addAppRedirectMessageToUrl } from 'ui/notify';
 export { createSavedObjectClass } from 'ui/saved_objects/saved_object';
 export { configureAppAngularModule } from '../../../../../src/plugins/kibana_legacy/public';
