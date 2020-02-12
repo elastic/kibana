2abc3f57930f7b9e761afb512d222b3684f200f8
Mikhail Shustov <restrry@gmail.com>
2020-02-12 09:04:50 +0100

    [NP] Add meta-data and timestamp formatting for Log Record (#56982)
    * log meta in pattern layout
    
    * address pgayvallet comment
    
    * add conversion patterns for timestamp
    
    * use comparison instead of inline snapshots
    
    * logs. use elasticsearch and LP compatible timezone format in json layout
    
    * use regexp groups and dot separator for ms. as in default iso format
    
    * use America/New_York timezone as default as it set in guess anyway
    
    * fix APM tests. they need to reset timezone locally and restore it after
    
    * fix logging tests

--- src/core/server/logging/layouts/pattern_layout.test.ts
+++ src/core/server/logging/layouts/pattern_layout.test.ts
@@ -20,8 +20,9 @@
 import { stripAnsiSnapshotSerializer } from '../../../test_helpers/strip_ansi_snapshot_serializer';
 import { LogLevel } from '../log_level';
 import { LogRecord } from '../log_record';
-import { PatternLayout } from './pattern_layout';
+import { PatternLayout, patternSchema } from './pattern_layout';
 
+const timestamp = new Date(Date.UTC(2012, 1, 1, 14, 30, 22, 11));
 const records: LogRecord[] = [
   {
     context: 'context-1',
@@ -32,42 +33,42 @@ const records: LogRecord[] = [
     },
     level: LogLevel.Fatal,
     message: 'message-1',
-    timestamp: new Date(Date.UTC(2012, 1, 1)),
+    timestamp,
     pid: 5355,
   },
   {
     context: 'context-2',
     level: LogLevel.Error,
     message: 'message-2',
-    timestamp: new Date(Date.UTC(2012, 1, 1)),
+    timestamp,
     pid: 5355,
   },
   {
     context: 'context-3',
     level: LogLevel.Warn,
     message: 'message-3',
-    timestamp: new Date(Date.UTC(2012, 1, 1)),
+    timestamp,
     pid: 5355,
   },
   {
     context: 'context-4',
     level: LogLevel.Debug,
     message: 'message-4',
-    timestamp: new Date(Date.UTC(2012, 1, 1)),
+    timestamp,
     pid: 5355,
   },
   {
     context: 'context-5',
     level: LogLevel.Info,
     message: 'message-5',
-    timestamp: new Date(Date.UTC(2012, 1, 1)),
+    timestamp,
     pid: 5355,
   },
   {
     context: 'context-6',
     level: LogLevel.Trace,
     message: 'message-6',
-    timestamp: new Date(Date.UTC(2012, 1, 1)),
+    timestamp,
     pid: 5355,
   },
 ];
@@ -118,6 +119,45 @@ test('`format()` correctly formats record with custom pattern.', () => {
   }
 });
 
+test('`format()` correctly formats record with meta data.', () => {
+  const layout = new PatternLayout();
+
+  expect(
+    layout.format({
+      context: 'context-meta',
+      level: LogLevel.Debug,
+      message: 'message-meta',
+      timestamp,
+      pid: 5355,
+      meta: {
+        from: 'v7',
+        to: 'v8',
+      },
+    })
+  ).toBe('[2012-02-01T14:30:22.011Z][DEBUG][context-meta][{"from":"v7","to":"v8"}] message-meta');
+
+  expect(
+    layout.format({
+      context: 'context-meta',
+      level: LogLevel.Debug,
+      message: 'message-meta',
+      timestamp,
+      pid: 5355,
+      meta: {},
+    })
+  ).toBe('[2012-02-01T14:30:22.011Z][DEBUG][context-meta][{}] message-meta');
+
+  expect(
+    layout.format({
+      context: 'context-meta',
+      level: LogLevel.Debug,
+      message: 'message-meta',
+      timestamp,
+      pid: 5355,
+    })
+  ).toBe('[2012-02-01T14:30:22.011Z][DEBUG][context-meta] message-meta');
+});
+
 test('`format()` correctly formats record with highlighting.', () => {
   const layout = new PatternLayout(undefined, true);
 
@@ -132,4 +172,161 @@ test('allows specifying the PID in custom pattern', () => {
   for (const record of records) {
     expect(layout.format(record)).toMatchSnapshot();
   }
+});
+
+test('`format()` allows specifying pattern with meta.', () => {
+  const layout = new PatternLayout('{context}-{meta}-{message}');
+  const record = {
+    context: 'context',
+    level: LogLevel.Debug,
+    message: 'message',
+    timestamp,
+    pid: 5355,
+    meta: {
+      from: 'v7',
+      to: 'v8',
+    },
+  };
+  expect(layout.format(record)).toBe('context-[{"from":"v7","to":"v8"}]-message');
+});
+
+describe('format', () => {
+  describe('timestamp', () => {
+    const record = {
+      context: 'context',
+      level: LogLevel.Debug,
+      message: 'message',
+      timestamp,
+      pid: 5355,
+    };
+    it('uses ISO8601 as default', () => {
+      const layout = new PatternLayout();
+
+      expect(layout.format(record)).toBe('[2012-02-01T14:30:22.011Z][DEBUG][context] message');
+    });
+
+    describe('supports specifying a predefined format', () => {
+      it('ISO8601', () => {
+        const layout = new PatternLayout('[{timestamp{ISO8601}}][{context}]');
+
+        expect(layout.format(record)).toBe('[2012-02-01T14:30:22.011Z][context]');
+      });
+
+      it('ISO8601_TZ', () => {
+        const layout = new PatternLayout('[{timestamp{ISO8601_TZ}}][{context}]');
+
+        expect(layout.format(record)).toBe('[2012-02-01T09:30:22.011-05:00][context]');
+      });
+
+      it('ABSOLUTE', () => {
+        const layout = new PatternLayout('[{timestamp{ABSOLUTE}}][{context}]');
+
+        expect(layout.format(record)).toBe('[09:30:22.011][context]');
+      });
+
+      it('UNIX', () => {
+        const layout = new PatternLayout('[{timestamp{UNIX}}][{context}]');
+
+        expect(layout.format(record)).toBe('[1328106622][context]');
+      });
+
+      it('UNIX_MILLIS', () => {
+        const layout = new PatternLayout('[{timestamp{UNIX_MILLIS}}][{context}]');
+
+        expect(layout.format(record)).toBe('[1328106622011][context]');
+      });
+    });
+
+    describe('supports specifying a predefined format and timezone', () => {
+      it('ISO8601', () => {
+        const layout = new PatternLayout('[{timestamp{ISO8601}{America/Los_Angeles}}][{context}]');
+
+        expect(layout.format(record)).toBe('[2012-02-01T14:30:22.011Z][context]');
+      });
+
+      it('ISO8601_TZ', () => {
+        const layout = new PatternLayout(
+          '[{timestamp{ISO8601_TZ}{America/Los_Angeles}}][{context}]'
+        );
+
+        expect(layout.format(record)).toBe('[2012-02-01T06:30:22.011-08:00][context]');
+      });
+
+      it('ABSOLUTE', () => {
+        const layout = new PatternLayout('[{timestamp{ABSOLUTE}{America/Los_Angeles}}][{context}]');
+
+        expect(layout.format(record)).toBe('[06:30:22.011][context]');
+      });
+
+      it('UNIX', () => {
+        const layout = new PatternLayout('[{timestamp{UNIX}{America/Los_Angeles}}][{context}]');
+
+        expect(layout.format(record)).toBe('[1328106622][context]');
+      });
+
+      it('UNIX_MILLIS', () => {
+        const layout = new PatternLayout(
+          '[{timestamp{UNIX_MILLIS}{America/Los_Angeles}}][{context}]'
+        );
+
+        expect(layout.format(record)).toBe('[1328106622011][context]');
+      });
+    });
+    it('formats several conversions patterns correctly', () => {
+      const layout = new PatternLayout(
+        '[{timestamp{ABSOLUTE}{America/Los_Angeles}}][{context}][{timestamp{UNIX}}]'
+      );
+
+      expect(layout.format(record)).toBe('[06:30:22.011][context][1328106622]');
+    });
+  });
+});
+
+describe('schema', () => {
+  describe('pattern', () => {
+    describe('{timestamp}', () => {
+      it('does not fail when {timestamp} not present', () => {
+        expect(patternSchema.validate('')).toBe('');
+        expect(patternSchema.validate('{pid}')).toBe('{pid}');
+      });
+
+      it('does not fail on {timestamp} without params', () => {
+        expect(patternSchema.validate('{timestamp}')).toBe('{timestamp}');
+        expect(patternSchema.validate('{timestamp}}')).toBe('{timestamp}}');
+        expect(patternSchema.validate('{{timestamp}}')).toBe('{{timestamp}}');
+      });
+
+      it('does not fail on {timestamp} with predefined date format', () => {
+        expect(patternSchema.validate('{timestamp{ISO8601}}')).toBe('{timestamp{ISO8601}}');
+      });
+
+      it('does not fail on {timestamp} with predefined date format and valid timezone', () => {
+        expect(patternSchema.validate('{timestamp{ISO8601_TZ}{Europe/Berlin}}')).toBe(
+          '{timestamp{ISO8601_TZ}{Europe/Berlin}}'
+        );
+      });
+
+      it('fails on {timestamp} with unknown date format', () => {
+        expect(() =>
+          patternSchema.validate('{timestamp{HH:MM:SS}}')
+        ).toThrowErrorMatchingInlineSnapshot(
+          `"Date format expected one of ISO8601, ISO8601_TZ, ABSOLUTE, UNIX, UNIX_MILLIS, but given: HH:MM:SS"`
+        );
+      });
+
+      it('fails on {timestamp} with predefined date format and invalid timezone', () => {
+        expect(() =>
+          patternSchema.validate('{timestamp{ISO8601_TZ}{Europe/Kibana}}')
+        ).toThrowErrorMatchingInlineSnapshot(`"Unknown timezone: Europe/Kibana"`);
+      });
+
+      it('validates several {timestamp} in pattern', () => {
+        expect(() =>
+          patternSchema.validate('{timestamp{ISO8601_TZ}{Europe/Berlin}}{message}{timestamp{HH}}')
+        ).toThrowErrorMatchingInlineSnapshot(
+          `"Date format expected one of ISO8601, ISO8601_TZ, ABSOLUTE, UNIX, UNIX_MILLIS, but given: HH"`
+        );
+      });
+    });
+  });
 });
