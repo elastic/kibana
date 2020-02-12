83c803d952d507c041a401d387fa1a77cee8cfb4
Tiago Costa <tiagoffcc@hotmail.com>
2020-02-12 17:43:35 +0000

    chore(NA): add newly build dist

--- packages/kbn-pm/dist/index.js
+++ packages/kbn-pm/dist/index.js
@@ -108562,7 +108562,9 @@ __webpack_require__.r(__webpack_exports__);
  * to Kibana itself.
  */
 
-const isKibanaDep = depVersion => depVersion.includes('../../packages/');
+const isKibanaDep = depVersion => // For ../kibana-extra/ directory (legacy only)
+depVersion.includes('../../kibana/packages/') || // For plugins/ directory
+depVersion.includes('../../packages/');
 /**
  * This prepares the dependencies for an _external_ project.
  */
