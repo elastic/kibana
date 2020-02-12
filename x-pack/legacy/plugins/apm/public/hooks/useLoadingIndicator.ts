680e423e104ca1a7f9502d79ac8cfb4fbefc4883
Dario Gieselaar <dario.gieselaar@elastic.co>
2020-02-12 13:33:21 +0100

    Unset loading indicator if component unmounts

--- x-pack/legacy/plugins/apm/public/hooks/useLoadingIndicator.ts
+++ x-pack/legacy/plugins/apm/public/hooks/useLoadingIndicator.ts
@@ -4,13 +4,19 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { useContext, useMemo } from 'react';
+import { useContext, useMemo, useEffect } from 'react';
 import { LoadingIndicatorContext } from '../context/LoadingIndicatorContext';
 import { useComponentId } from './useComponentId';
 
 export function useLoadingIndicator() {
   const { dispatchStatus } = useContext(LoadingIndicatorContext);
   const id = useComponentId();
+
+  useEffect(() => {
+    return () => {
+      dispatchStatus({ id, isLoading: false });
+    };
+  }, [dispatchStatus, id]);
 
   return useMemo(() => {
     return {
