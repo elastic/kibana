2a69b056c895b65c8474e238d15d6a32df5d9e80
Lee Drengenberg <lee.drengenberg@elastic.co>
2020-02-12 10:48:29 -0600

    remove for loop

--- src/dev/code_coverage/shell_scripts/extract_xpack_tests.sh
+++ src/dev/code_coverage/shell_scripts/extract_xpack_tests.sh
@@ -4,6 +4,4 @@ DOWNLOAD_DIR=/tmp/downloaded_coverage
 EXTRACT_DIR=/tmp/extracted_coverage
 
 echo "### Extracting kibana-xpack-tests"
-for i in {1..3}; do
-  tar -xzf $DOWNLOAD_DIR/coverage/kibana-xpack-tests-${i}/kibana-coverage.tar.gz -C $EXTRACT_DIR
-done
+tar -xzf $DOWNLOAD_DIR/coverage/kibana-xpack-tests/kibana-coverage.tar.gz -C $EXTRACT_DIR
