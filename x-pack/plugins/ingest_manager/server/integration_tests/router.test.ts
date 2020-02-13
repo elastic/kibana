25124c6cfd9a957689a47898a06f24ef6cda68fb
John Schulz <john.schulz@elastic.co>
2020-02-13 13:51:14 -0500

    No more unknown key errors. Timeouts now

--- x-pack/plugins/ingest_manager/server/integration_tests/router.test.ts
+++ x-pack/plugins/ingest_manager/server/integration_tests/router.test.ts
@@ -8,46 +8,42 @@ import * as kbnTestServer from '../../../../../src/test_utils/kbn_server';
 // import { IngestManagerConfigType } from '../../common/types';
 
 describe('ingestManager', () => {
-  describe('GET /ingest_manager/agent_configs', () => {
+  describe('manager enabled (no EPM, no Fleet)', () => {
     let root: ReturnType<typeof kbnTestServer.createRoot>;
     beforeAll(async () => {
       const ingestManagerConfig = {
         enabled: true,
-        epm: { enabled: true },
+        epm: { enabled: false },
         fleet: { enabled: false },
       };
-      root = kbnTestServer.createRoot({
-        oss: false,
-        xpack: ingestManagerConfig,
-      });
+      root = kbnTestServer.createRoot(
+        {
+          xpack: { ingestManager: ingestManagerConfig },
+        },
+        {
+          oss: false,
+        }
+      );
       await root.setup();
       await root.start();
     }, 30000);
 
     afterAll(async () => await root.shutdown());
-    it('validates given text', async () => {
-      const response = await kbnTestServer.request
-        .get(root, '/ingest_manager/agent_configs')
-        .query({ text: 'input string'.repeat(100) })
-        .expect(400);
 
-      expect(response.body).toHaveProperty('message');
+    it('has agent config API', async () => {
+      await kbnTestServer.request.get(root, '/api/ingest_manager/agent_configs').expect(200);
     });
 
-    it('formats given text', async () => {
-      const response = await kbnTestServer.request
-        .get(root, '/ingest_manager/agent_configs')
-        .query({ text: 'input string' })
-        .expect(200);
-
-      expect(response.text).toBe('...');
+    it('has datasources api', async () => {
+      await kbnTestServer.request.get(root, '/api/ingest_manager/datasources').expect(200);
     });
 
-    it('returns BadRequest if passed string contains banned symbols', async () => {
-      await kbnTestServer.request
-        .get(root, '/ingest_manager/agent_configs')
-        .query({ text: '<script>' })
-        .expect(400, 'Text cannot contain unescaped HTML markup.');
+    it('does not have EPM api', async () => {
+      await kbnTestServer.request.get(root, '/api/ingest_manager/epm').expect(404);
+    });
+
+    it('does not have Fleet api', async () => {
+      await kbnTestServer.request.get(root, '/api/ingest_manager/fleet').expect(404);
     });
   });
 });
