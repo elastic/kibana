b845f710ff3677f56621d7622539d0934d2e32e1
Shahzad <shahzad.muhammad@elastic.co>
2020-02-13 18:50:10 +0100

    [Uptime] Added tests for pages (#56736) (#57554)
    * added test for pages
    
    * fixed types
    
    * update types
    
    * update snap
    
    * PR feedback
    
    * PR feedback

--- x-pack/legacy/plugins/uptime/public/lib/helper/render_with_router.tsx
+++ x-pack/legacy/plugins/uptime/public/lib/helper/render_with_router.tsx
@@ -4,18 +4,36 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React from 'react';
+import React, { ReactElement } from 'react';
 
 import { Router } from 'react-router-dom';
 import { MemoryHistory } from 'history/createMemoryHistory';
 import { createMemoryHistory } from 'history';
+import { mountWithIntl, renderWithIntl, shallowWithIntl } from 'test_utils/enzyme_helpers';
 
-export const renderWithRouter = (Component: any, customHistory?: MemoryHistory) => {
+const helperWithRouter: <R>(
+  helper: (node: ReactElement) => R,
+  component: ReactElement,
+  customHistory?: MemoryHistory
+) => R = (helper, component, customHistory) => {
   if (customHistory) {
-    return <Router history={customHistory}>{Component}</Router>;
+    customHistory.location.key = 'TestKeyForTesting';
+    return helper(<Router history={customHistory}>{component}</Router>);
   }
   const history = createMemoryHistory();
   history.location.key = 'TestKeyForTesting';
 
-  return <Router history={history}>{Component}</Router>;
+  return helper(<Router history={history}>{component}</Router>);
+};
+
+export const renderWithRouter = (component: ReactElement, customHistory?: MemoryHistory) => {
+  return helperWithRouter(renderWithIntl, component, customHistory);
+};
+
+export const shallowWithRouter = (component: ReactElement, customHistory?: MemoryHistory) => {
+  return helperWithRouter(shallowWithIntl, component, customHistory);
+};
+
+export const mountWithRouter = (component: ReactElement, customHistory?: MemoryHistory) => {
+  return helperWithRouter(mountWithIntl, component, customHistory);
 };
