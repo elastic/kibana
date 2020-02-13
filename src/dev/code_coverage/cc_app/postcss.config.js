d8ae840355a6025b242e7d0679377d2b2e2e1045
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-13 10:06:16 -0700

    Drop the app, and all it's supporting scripts.

--- src/dev/code_coverage/cc_app/postcss.config.js
+++
@@ -1,7 +0,0 @@
- const tailwindcss = require('tailwindcss');
- module.exports = {
-     plugins: [
-         tailwindcss('./tailwind.js'),
-         require('autoprefixer'),
-     ],
- };
--- src/dev/code_coverage/node_scripts/populate_cc_app/index.js
+++
@@ -1,5 +0,0 @@
-require('../../../../../src/setup_node_env');
-
-const [ , , buildNumber, srcFile, destFile ] = process.argv;
-
-require('./bootstrap_app_initial_data').populate(buildNumber, srcFile, destFile);
