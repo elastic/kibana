d00fca393966388bb8318098352614759fc61f7c
spalger <spalger@users.noreply.github.com>
2020-02-14 11:44:42 -0700

    Revert "skip settings tests (#57608)"
    This reverts commit 64625b282cc0042fcec96459bec6376c00db4451.

--- x-pack/test/functional/apps/index_patterns/feature_controls/index_patterns_spaces.ts
+++ x-pack/test/functional/apps/index_patterns/feature_controls/index_patterns_spaces.ts
@@ -52,8 +52,7 @@ export default function({ getPageObjects, getService }: FtrProviderContext) {
       });
     });
 
-    // https://github.com/elastic/kibana/issues/57601
-    describe.skip('space with Index Patterns disabled', () => {
+    describe('space with Index Patterns disabled', () => {
       before(async () => {
         // we need to load the following in every situation as deleting
         // a space deletes all of the associated saved objects
