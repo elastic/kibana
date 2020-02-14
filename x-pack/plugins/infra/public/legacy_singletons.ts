a92e367fd8f1baccfda4ba7c06722814b6847d0b
Kerry Gallagher <k.gallagher.05@gmail.com>
2020-02-14 13:42:54 +0000

    Remove setup from legacy_singletons

--- x-pack/plugins/infra/public/legacy_singletons.ts
+++ x-pack/plugins/infra/public/legacy_singletons.ts
@@ -3,17 +3,12 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { CoreSetup, CoreStart } from 'kibana/public';
+import { CoreStart } from 'kibana/public';
 
-let npSetup: CoreSetup;
 let npStart: CoreStart;
-
-export function registerSetupSingleton(setup: CoreSetup) {
-  npSetup = setup;
-}
 
 export function registerStartSingleton(start: CoreStart) {
   npStart = start;
 }
 
-export { npSetup, npStart };
+export { npStart };
