5742eaab97f559e11c97d4a11d12123af8ab5965
Patrick Mueller <pmuellr@gmail.com>
2020-02-16 20:10:35 -0500

    change id of x-pack event_log plugin to eventLog (#57612)
    change id of x-pack event_log plugin to eventLog

--- x-pack/plugins/event_log/server/plugin.ts
+++ x-pack/plugins/event_log/server/plugin.ts
@@ -22,7 +22,8 @@ import { createEsContext, EsContext } from './es';
 
 export type PluginClusterClient = Pick<ClusterClient, 'callAsInternalUser' | 'asScoped'>;
 
-const PROVIDER = 'event_log';
+const PROVIDER = 'eventLog';
+
 const ACTIONS = {
   starting: 'starting',
   stopping: 'stopping',
@@ -88,7 +89,7 @@ export class Plugin implements CorePlugin<IEventLogService> {
     // will log the event after initialization
     this.eventLogger.logEvent({
       event: { action: ACTIONS.starting },
-      message: 'event_log starting',
+      message: 'eventLog starting',
     });
   }
 
@@ -101,7 +102,7 @@ export class Plugin implements CorePlugin<IEventLogService> {
     // when Kibana is actuaelly stopping, as it's written asynchronously
     this.eventLogger.logEvent({
       event: { action: ACTIONS.stopping },
-      message: 'event_log stopping',
+      message: 'eventLog stopping',
     });
   }
 }
