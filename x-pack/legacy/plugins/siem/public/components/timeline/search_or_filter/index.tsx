2c7fba2b3ced2562d1cf1da0ca63883621ab4609
patrykkopycinski <patryk.kopycinski@elastic.co>
2020-02-17 11:54:35 +0100

    Bump redux dependencies (#53348) (#57776)

--- x-pack/legacy/plugins/siem/public/components/timeline/search_or_filter/index.tsx
+++ x-pack/legacy/plugins/siem/public/components/timeline/search_or_filter/index.tsx
@@ -6,7 +6,7 @@
 
 import { getOr, isEqual } from 'lodash/fp';
 import React, { useCallback } from 'react';
-import { connect } from 'react-redux';
+import { connect, ConnectedProps } from 'react-redux';
 import { Dispatch } from 'redux';
 
 import { Filter, IIndexPattern } from '../../../../../../../../src/plugins/data/public';
@@ -22,8 +22,7 @@ import {
 } from '../../../store';
 import { timelineActions } from '../../../store/actions';
 import { KqlMode, timelineDefaults, TimelineModel, EventType } from '../../../store/timeline/model';
-import { DispatchUpdateReduxTime, dispatchUpdateReduxTime } from '../../super_date_picker';
-import { DataProvider } from '../data_providers/data_provider';
+import { dispatchUpdateReduxTime } from '../../super_date_picker';
 import { SearchOrFilter } from './search_or_filter';
 
 interface OwnProps {
@@ -32,45 +31,7 @@ interface OwnProps {
   timelineId: string;
 }
 
-interface StateReduxProps {
-  dataProviders: DataProvider[];
-  eventType: EventType;
-  filters: Filter[];
-  filterQuery: KueryFilterQuery;
-  filterQueryDraft: KueryFilterQuery;
-  from: number;
-  fromStr: string;
-  isRefreshPaused: boolean;
-  kqlMode: KqlMode;
-  refreshInterval: number;
-  savedQueryId: string | null;
-  to: number;
-  toStr: string;
-}
-
-interface DispatchProps {
-  applyKqlFilterQuery: ({
-    id,
-    filterQuery,
-  }: {
-    id: string;
-    filterQuery: SerializedFilterQuery;
-  }) => void;
-  updateEventType: ({ id, eventType }: { id: string; eventType: EventType }) => void;
-  updateKqlMode: ({ id, kqlMode }: { id: string; kqlMode: KqlMode }) => void;
-  setKqlFilterQueryDraft: ({
-    id,
-    filterQueryDraft,
-  }: {
-    id: string;
-    filterQueryDraft: KueryFilterQuery;
-  }) => void;
-  setSavedQueryId: ({ id, savedQueryId }: { id: string; savedQueryId: string | null }) => void;
-  setFilters: ({ id, filters }: { id: string; filters: Filter[] }) => void;
-  updateReduxTime: DispatchUpdateReduxTime;
-}
-
-type Props = OwnProps & StateReduxProps & DispatchProps;
+type Props = OwnProps & PropsFromRedux;
 
 const StatefulSearchOrFilterComponent = React.memo<Props>(
   ({
@@ -217,17 +178,17 @@ const makeMapStateToProps = () => {
     return {
       dataProviders: timeline.dataProviders,
       eventType: timeline.eventType ?? 'raw',
-      filterQuery: getKqlFilterQuery(state, timelineId),
-      filterQueryDraft: getKqlFilterQueryDraft(state, timelineId),
-      filters: timeline.filters,
+      filterQuery: getKqlFilterQuery(state, timelineId)!,
+      filterQueryDraft: getKqlFilterQueryDraft(state, timelineId)!,
+      filters: timeline.filters!,
       from: input.timerange.from,
-      fromStr: input.timerange.fromStr,
+      fromStr: input.timerange.fromStr!,
       isRefreshPaused: policy.kind === 'manual',
       kqlMode: getOr('filter', 'kqlMode', timeline),
       refreshInterval: policy.duration,
       savedQueryId: getOr(null, 'savedQueryId', timeline),
       to: input.timerange.to,
-      toStr: input.timerange.toStr,
+      toStr: input.timerange.toStr!,
     };
   };
   return mapStateToProps;
@@ -265,7 +226,8 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
   updateReduxTime: dispatchUpdateReduxTime(dispatch),
 });
 
-export const StatefulSearchOrFilter = connect(
-  makeMapStateToProps,
-  mapDispatchToProps
-)(StatefulSearchOrFilterComponent);
+export const connector = connect(makeMapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export const StatefulSearchOrFilter = connector(StatefulSearchOrFilterComponent);
