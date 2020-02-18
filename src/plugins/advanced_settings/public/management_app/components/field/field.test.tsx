ac65d8f5e02f925dab292d0e79d50dccd13ca0e3
Matt Kime <matt@mattki.me>
2020-02-18 12:42:57 -0600

    revert removal of ts-ignore

--- src/plugins/advanced_settings/public/management_app/components/field/field.test.tsx
+++ src/plugins/advanced_settings/public/management_app/components/field/field.test.tsx
@@ -25,7 +25,8 @@ import { FieldSetting } from '../../types';
 import { UiSettingsType, StringValidation } from '../../../../../../core/public';
 import { notificationServiceMock, docLinksServiceMock } from '../../../../../../core/public/mocks';
 
-import { findTestSubject } from '@elastic/eui';
+// @ts-ignore
+import { findTestSubject } from '@elastic/eui/lib/test';
 import { Field } from './field';
 
 jest.mock('ui/notify', () => ({
