17ba0c619e400db4a0664dacac63cf4380c0ea82
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-18 12:13:36 -0700

    Use fake data.

--- src/dev/code_coverage/shell_scripts/ingest_coverage.sh
+++ src/dev/code_coverage/shell_scripts/ingest_coverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "### Ingesting Code Coverage"
+echo ""
 
 
 BUILD_ID=$1
@@ -15,9 +16,11 @@ export STATIC_SITE_URL_BASE
 for x in jest functional mocha; do
   echo "### Ingesting coverage for ${x}"
 
-  COVERAGE_SUMMARY_FILE=target/kibana-coverage/${x}-combined/coverage-summary.json
+#  COVERAGE_SUMMARY_FILE=target/kibana-coverage/${x}-combined/coverage-summary.json
+  COVERAGE_SUMMARY_FILE=temp-target/kibana-coverage/mocha/coverage-summary-fake-${x}.json
 
   node scripts/ingest_coverage.js --verbose --path ${COVERAGE_SUMMARY_FILE}
 done
 
 echo "###  Ingesting Code Coverage - Complete"
+echo ""
