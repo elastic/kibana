0272a14ed488f3274524f8e63821b73bcfb45a31
Jean-Louis Leysens <jloleysens@gmail.com>
2020-02-18 16:39:49 +0100

    Include stylesheets in public

--- x-pack/plugins/upgrade_assistant/public/index.ts
+++ x-pack/plugins/upgrade_assistant/public/index.ts
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
+import './index.scss';
 import { UpgradeAssistantUIPlugin } from './plugin';
 
 export const plugin = () => {
