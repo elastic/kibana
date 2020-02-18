0845060095dac98f746be11e1efdaecc8c17ac86
streamich <streamich@gmail.com>
2020-02-18 15:19:56 +0100

    feat: 🎸 annotate with comments Trigger and add createContext(

--- src/plugins/ui_actions/public/triggers/trigger.ts
+++ src/plugins/ui_actions/public/triggers/trigger.ts
@@ -17,8 +17,38 @@
  * under the License.
  */
 
-export interface Trigger {
+/**
+ * `Trigger` specifies a named anchor to which `Action` can be attached. When
+ * `Trigger` is being *called* it creates a `Context` object and passes it to
+ * the `execute` method of an `Action`.
+ *
+ * More than one action can be attached to a single trigger, in which case when
+ * trigger is *called* it first displays a context menu for user to pick a
+ * single action to execute.
+ */
+export interface Trigger<
+  Context extends object = object,
+  CreateContextParams extends object = object
+> {
+  /**
+   * Unique name of the trigger as identified in `ui_actions` plugin trigger
+   * registry, such as "SELECT_RANGE_TRIGGER" or "VALUE_CLICK_TRIGGER".
+   */
   id: string;
+
+  /**
+   * User friendly name of the trigger.
+   */
   title?: string;
+
+  /**
+   * A longer user friendly description of the trigger.
+   */
   description?: string;
+
+  /**
+   * Method called to create trigger `Context`, which is passed to `Action`.
+   * If omitted, an identity function is used.
+   */
+  createContext?: (params: CreateContextParams) => Context;
 }
