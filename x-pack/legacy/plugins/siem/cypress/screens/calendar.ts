9d37535b6437f9716d530052b8c091de64f4fa96
MadameSheema <snootchie.boochies@gmail.com>
2020-02-19 13:08:27 +0100

    refactors 'url state' tests (#57981)

---
+++ x-pack/legacy/plugins/siem/cypress/screens/calendar.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const DATE_PICKER_START_DATE_POPOVER_BUTTON =
+  'div[data-test-subj="globalDatePicker"] button[data-test-subj="superDatePickerstartDatePopoverButton"]';
+export const DATE_PICKER_END_DATE_POPOVER_BUTTON =
+  '[data-test-subj="globalDatePicker"] [data-test-subj="superDatePickerendDatePopoverButton"]';
+export const DATE_PICKER_ABSOLUTE_TAB = '[data-test-subj="superDatePickerAbsoluteTab"]';
+export const DATE_PICKER_APPLY_BUTTON =
+  '[data-test-subj="globalDatePicker"] button[data-test-subj="querySubmitButton"]';
+export const DATE_PICKER_APPLY_BUTTON_TIMELINE =
+  '[data-test-subj="timeline-properties"] button[data-test-subj="superDatePickerApplyTimeButton"]';
+export const DATE_PICKER_ABSOLUTE_INPUT = '[data-test-subj="superDatePickerAbsoluteDateInput"]';
+export const DATE_PICKER_START_DATE_POPOVER_BUTTON_TIMELINE =
+  '[data-test-subj="timeline-properties"] [data-test-subj="superDatePickerstartDatePopoverButton"]';
+
+export const DATE_PICKER_END_DATE_POPOVER_BUTTON_TIMELINE =
+  '[data-test-subj="timeline-properties"] [data-test-subj="superDatePickerendDatePopoverButton"]';
