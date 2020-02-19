26c3ca6776cc5bb3934b474d6b4e6ef46f845cf6
oatkiller <robert.austin@elastic.co>
2020-02-18 20:06:50 -0500

    Removed sample code
    And created issue: https://github.com/elastic/endpoint-app-team/issues/192

--- x-pack/plugins/endpoint/public/applications/endpoint/store/alerts/middleware.ts
+++ x-pack/plugins/endpoint/public/applications/endpoint/store/alerts/middleware.ts
@@ -8,49 +8,17 @@ import { HttpFetchQuery } from 'kibana/public';
 import { AlertResultList } from '../../../../../common/types';
 import { AppAction } from '../action';
 import { MiddlewareFactory, AlertListState } from '../../types';
-import { isOnAlertPage, queryParams } from './selectors';
+import { isOnAlertPage, paginationDataFromUrl } from './selectors';
 
 export const alertMiddlewareFactory: MiddlewareFactory<AlertListState> = coreStart => {
   return api => next => async (action: AppAction) => {
     next(action);
     const state = api.getState();
     if (action.type === 'userChangedUrl' && isOnAlertPage(state)) {
-      const response: AlertResultList = {
-        total: 1,
-        request_page_size: 1,
-        request_page_index: 0,
-        result_from_index: 0,
-        alerts: [
-          {
-            '@timestamp': new Date().toString(),
-            agent: { id: '', version: '' },
-            event: {
-              action: '',
-            },
-            file_classification: {
-              malware_classification: {
-                score: 1,
-              },
-            },
-            host: {
-              hostname: '',
-              ip: '',
-              os: {
-                name: '',
-              },
-            },
-            thread: {},
-          },
-        ],
-      };
-      api.dispatch({ type: 'serverReturnedAlertsData', payload: response });
-      /*
-       * TODO dont commit this file
       const response: AlertResultList = await coreStart.http.get(`/api/endpoint/alerts`, {
         query: paginationDataFromUrl(state) as HttpFetchQuery,
       });
       api.dispatch({ type: 'serverReturnedAlertsData', payload: response });
-      */
     }
   };
 };
