e098c247add5b115329a018d1582549f5d772e62
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-18 20:03:44 -0700

    Now we hot reload with auto-built css, thanks to: https://www.npmjs.com/package/react-scripts-tailwindcss

--- src/dev/code_coverage/cc_app/postcss.config.js
+++ src/dev/code_coverage/cc_app/postcss.config.js
@@ -1,7 +1,7 @@
  const tailwindcss = require('tailwindcss');
  module.exports = {
      plugins: [
-         tailwindcss('./tailwind.js'),
+         tailwindcss('./tailwind.config.js'),
          require('autoprefixer'),
      ],
  };
