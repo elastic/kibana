2d920bd0ba0a26869dfb2b9f11f1125289f61e65
Luke Elmers <luke.elmers@elastic.co>
2020-02-18 17:00:21 -0700

    Change shimmed expressions utils to import createAggConfigs internally.

--- src/legacy/core_plugins/data/public/search/expressions/utils.ts
+++ src/legacy/core_plugins/data/public/search/expressions/utils.ts
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import { createAggConfigs } from 'ui/agg_types';
+import { getSearchServiceShim } from '../../services';
 import { IAggConfig } from '../aggs/types';
 import { KibanaDatatableColumnMeta } from '../../../../../../plugins/expressions/common/expression_types';
 import { IndexPattern } from '../../../../../../plugins/data/public';
@@ -41,7 +41,8 @@ export const deserializeAggConfig = ({
   aggConfigParams,
   indexPattern,
 }: DeserializeAggConfigParams) => {
-  const aggConfigs = createAggConfigs(indexPattern);
+  const { aggs } = getSearchServiceShim();
+  const aggConfigs = aggs.createAggConfigs(indexPattern);
   const aggConfig = aggConfigs.createAggConfig({
     enabled: true,
     type,
