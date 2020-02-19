252ff405e2e24716b737727f9d2677fc2b3f2948
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2020-02-19 18:22:14 +0100

    remove NODE_ENV=test

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -5,7 +5,6 @@ source test/scripts/jenkins_test_setup.sh
 if [[ -z "$CODE_COVERAGE" ]] ; then
   "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
 else
-  echo "NODE_ENV=$NODE_ENV"
   echo " -> Running jest tests with coverage"
   node scripts/jest --ci --verbose --coverage
   echo ""
