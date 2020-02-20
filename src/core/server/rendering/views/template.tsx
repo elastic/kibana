47bb498710ee5b87e40e4f198779c3b00808dc62
Dave Snider <dave.snider@gmail.com>
2020-02-20 10:42:58 -0800

    words

--- src/core/server/rendering/views/template.tsx
+++ src/core/server/rendering/views/template.tsx
@@ -100,7 +100,7 @@ export const Template: FunctionComponent<Props> = ({
                 'Kibana did not load properly. Check the server output for more information.',
             })}
           >
-            {i18n('core.ui.welcomeMessage', { defaultMessage: 'Loading Kibana' })}
+            {i18n('core.ui.welcomeMessage', { defaultMessage: 'Loading' })}
           </div>
         </div>
 
