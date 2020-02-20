40e92d787907eb12c76ce21000e3f0b3ef41ecea
Gloria Hornero <snootchie.boochies@gmail.com>
2020-02-20 20:41:54 +0100

    fixes type check

--- x-pack/legacy/plugins/siem/cypress/screens/hosts/events.ts
+++ x-pack/legacy/plugins/siem/cypress/screens/hosts/events.ts
@@ -6,13 +6,15 @@
 
 export const CLOSE_MODAL = '[data-test-subj="modal-inspect-close"]';
 
-export const EVENTS_VIEWER_FIELDS_BUTTON = `${EVENTS_VIEWER_PANEL} [data-test-subj="show-field-browser-gear"]`;
+export const EVENTS_VIEWER_FIELDS_BUTTON =
+  '[data-test-subj="events-viewer-panel"] [data-test-subj="show-field-browser-gear"]';
 
 export const EVENTS_VIEWER_PANEL = '[data-test-subj="events-viewer-panel"]';
 
 export const FIELDS_BROWSER_CONTAINER = '[data-test-subj="fields-browser-container"]';
 
-export const HEADER_SUBTITLE = `${EVENTS_VIEWER_PANEL} [data-test-subj="header-panel-subtitle"]`;
+export const HEADER_SUBTITLE =
+  '[data-test-subj="events-viewer-panel"] [data-test-subj="header-panel-subtitle"]';
 
 export const HOST_GEO_CITY_NAME_CHECKBOX = '[data-test-subj="field-host.geo.city_name-checkbox"]';
 
@@ -25,12 +27,16 @@ export const HOST_GEO_COUNTRY_NAME_HEADER = '[data-test-subj="header-text-host.g
 
 export const INSPECT_MODAL = '[data-test-subj="modal-inspect-euiModal"]';
 
-export const INSPECT_QUERY = `${EVENTS_VIEWER_PANEL} [data-test-subj="inspect-icon-button"]`;
+export const INSPECT_QUERY =
+  '[data-test-subj="events-viewer-panel"] [data-test-subj="inspect-icon-button"]';
 
-export const LOCAL_EVENTS_COUNT = `${EVENTS_VIEWER_PANEL} [data-test-subj="local-events-count"]`;
+export const LOCAL_EVENTS_COUNT =
+  '[data-test-subj="events-viewer-panel"] [data-test-subj="local-events-count"';
 
-export const LOAD_MORE = `${EVENTS_VIEWER_PANEL} [data-test-subj="TimelineMoreButton"]`;
+export const LOAD_MORE =
+  '[data-test-subj="events-viewer-panel"] [data-test-subj="TimelineMoreButton"';
 
-export const RESET_FIELDS = `${EVENTS_VIEWER_PANEL} [data-test-subj="reset-fields"]`;
+export const RESET_FIELDS =
+  '[data-test-subj="events-viewer-panel"] [data-test-subj="reset-fields"]';
 
 export const SERVER_SIDE_EVENT_COUNT = '[data-test-subj="server-side-event-count"]';
