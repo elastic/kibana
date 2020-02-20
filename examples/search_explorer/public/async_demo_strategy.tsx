9337dead21f7d0d941ec0816fb2702881939a6a7
Lukas Olson <olson.lukas@gmail.com>
2020-02-20 14:55:55 -0700

    Fix error with setting state

--- examples/search_explorer/public/async_demo_strategy.tsx
+++ examples/search_explorer/public/async_demo_strategy.tsx
@@ -64,7 +64,7 @@ export class AsyncDemoStrategy extends React.Component<Props, State> {
 
   renderDemo = () => {
     const request: IAsyncDemoRequest = {
-      fibonacciNumbers: 5,
+      fibonacciNumbers: this.state.fibonacciNumbers,
     };
     return (
       <React.Fragment>
