206cb6a36ad53f0afa99c78b95716a3da431f3c5
FrankHassanabad <frank.hassanabad@elastic.co>
2020-02-20 07:47:47 -0700

    Added more io-ts

--- x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/schemas/output/create_rules_schema_output.test.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/schemas/output/create_rules_schema_output.test.ts
@@ -6,33 +6,50 @@
 
 import * as t from 'io-ts';
 import { PathReporter } from 'io-ts/lib/PathReporter';
-import { right, fold } from 'fp-ts/lib/Either';
+import { right, fold, left, Either } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 
 import { createRulesSchema, CreateRulesSchema } from './create_rules_schema_output';
 
+export const ANCHOR_DATE = '2020-02-20T03:57:54.037Z';
+
+interface Message {
+  errors?: t.Errors;
+  schema?: CreateRulesSchema;
+}
+
+const onLeft = (errors: t.Errors): Message => {
+  return { errors };
+};
+
+const onRight = (schema: CreateRulesSchema): Message => {
+  return { schema };
+};
+
+const foldLeftRight = fold(onLeft, onRight);
+
 describe('output create_rules_schema', () => {
   test('it should validate common output message', () => {
-    const outputRule: CreateRulesSchema = {
-      created_at: new Date(),
-      updated_at: new Date(),
+    const decoded = createRulesSchema.decode({
+      created_at: new Date(ANCHOR_DATE).toISOString(),
+      updated_at: new Date(ANCHOR_DATE).toISOString(),
       created_by: 'elastic',
-    };
-    const either = createRulesSchema.decode({
-      created_at: new Date().toISOString(),
-      updated_at: new Date().toISOString(),
-      created_by: 'elastic',
+      description: 'some description',
+      extra: 'string',
     });
-    // failure handler
-    const onLeft = (errors: t.Errors): string => `${errors.length} error(s) found`;
-    // success handler
-    const onRight = (s: CreateRulesSchema) => `No errors: ${s.created_at}`;
 
-    const message = pipe(either, fold(onLeft, onRight));
-    expect(message).toEqual('');
-    // const value = right(either);
-    // expect(value).toEqual({});
-    expect(true).toEqual(false);
-    // expect(PathReporter.report(either)).toEqual(['No errors!']);
+    const message = pipe(decoded, foldLeftRight);
+    const expected: CreateRulesSchema = {
+      created_at: new Date(ANCHOR_DATE),
+      updated_at: new Date(ANCHOR_DATE),
+      created_by: 'elastic',
+      description: 'some description',
+    };
+
+    if (message.errors != null) {
+      expect(PathReporter.report(left(message.errors))).toEqual('');
+    } else {
+      expect(message.schema).toEqual(expected);
+    }
   });
 });
--- x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/schemas/output/create_rules_schema_output.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/schemas/output/create_rules_schema_output.ts
@@ -16,6 +16,55 @@ export const createRulesSchema = t.type({
   created_at: DateFromISOString,
   updated_at: DateFromISOString,
   created_by: t.string,
+  description: t.string,
 });
 
 export type CreateRulesSchema = t.TypeOf<typeof createRulesSchema>;
+
+/*
+"description": "Identifies Windows programs run from unexpected parent processes. This could indicate masquerading or other strange activity on a system.",
+"enabled": false,
+"false_positives": [],
+"from": "now-6m",
+"id": "ff043f57-adf0-4cbc-8752-efc85b90de3d",
+"immutable": true,
+"index": [
+  "winlogbeat-*"
+],
+"interval": "5m",
+"rule_id": "35df0dd8-092d-4a83-88c1-5151a804f31b",
+"language": "kuery",
+"output_index": ".siem-signals-hassanabad-frank-default",
+"max_signals": 100,
+"risk_score": 47,
+"name": "Unusual Parent-Child Relationship ",
+"query": "   event.action:\"Process Create (rule: ProcessCreate)\" and process.parent.executable:* and   (      (process.name:\"smss.exe\" and not process.parent.name:(\"System\" or \"smss.exe\")) or       (process.name:\"csrss.exe\" and not process.parent.name:(\"smss.exe\" or \"svchost.exe\")) or       (process.name:\"wininit.exe\" and not process.parent.name:\"smss.exe\") or       (process.name:\"winlogon.exe\" and not process.parent.name:\"smss.exe\") or       (process.name:\"lsass.exe\" and not process.parent.name:\"wininit.exe\") or       (process.name:\"LogonUI.exe\" and not process.parent.name:(\"winlogon.exe\" or \"wininit.exe\")) or       (process.name:\"services.exe\" and not process.parent.name:\"wininit.exe\") or       (process.name:\"svchost.exe\" and not process.parent.name:(\"services.exe\" or \"MsMpEng.exe\")) or      (process.name:\"spoolsv.exe\" and not process.parent.name:\"services.exe\") or       (process.name:\"taskhost.exe\" and not process.parent.name:(\"services.exe\" or \"svchost.exe\")) or       (process.name:\"taskhostw.exe\" and not process.parent.name:(\"services.exe\" or \"svchost.exe\")) or       (process.name:\"userinit.exe\" and not process.parent.name:(\"dwm.exe\" or \"winlogon.exe\"))    )",
+"references": [],
+"severity": "medium",
+"updated_by": "elastic_kibana",
+"tags": [
+  "Elastic",
+  "Windows"
+],
+"to": "now",
+"type": "query",
+"threat": [
+  {
+    "framework": "MITRE ATT&CK",
+    "tactic": {
+      "id": "TA0004",
+      "name": "Privilege Escalation",
+      "reference": "https://attack.mitre.org/tactics/TA0004/"
+    },
+    "technique": [
+      {
+        "id": "T1093",
+        "name": "Process Hollowing",
+        "reference": "https://attack.mitre.org/techniques/T1093/"
+      }
+    ]
+  }
+],
+"version": 1
+}
+*/
