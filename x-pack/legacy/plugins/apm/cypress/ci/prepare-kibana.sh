05e22f8a6d82daf69ee72cf7632e87dc2b361456
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-02-21 10:16:52 +0000

    ci(jenkins): use the customised kibana.dev.yml file

--- x-pack/legacy/plugins/apm/cypress/ci/prepare-kibana.sh
+++ x-pack/legacy/plugins/apm/cypress/ci/prepare-kibana.sh
@@ -7,9 +7,6 @@ echo "1/3 Install dependencies ..."
 # shellcheck disable=SC1091
 source src/dev/ci_setup/setup_env.sh true
 yarn kbn bootstrap
-cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.dev.yml
-echo 'elasticsearch:' >> config/kibana.dev.yml
-cp ${CYPRESS_DIR}/ci/kibana.dev.yml config/kibana.yml
 
 echo "2/3 Ingest test data ..."
 pushd ${CYPRESS_DIR}
@@ -21,4 +18,5 @@ echo "3/3 Start Kibana ..."
 popd
 ## Might help to avoid FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
 export NODE_OPTIONS="--max-old-space-size=4096"
-nohup node scripts/kibana --no-base-path --csp.strict=false --optimize.watch=false> kibana.log 2>&1 &
+nohup node scripts/kibana --config=${CYPRESS_DIR}/ci/kibana.yml --no-base-path --csp.strict=false --optimize.watch=false> kibana.log 2>&1 &
+echo $! > ${CYPRESS_DIR}/kibana_pid.txt
