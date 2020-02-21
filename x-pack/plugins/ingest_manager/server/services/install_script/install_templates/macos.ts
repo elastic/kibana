d7daa8bbcc82bef9f9bf33e1756e434edd149f9a
John Schulz <john.schulz@elastic.co>
2020-02-21 10:01:51 -0500

    Use a more specific path to the agent script

--- x-pack/plugins/ingest_manager/server/services/install_script/install_templates/macos.ts
+++ x-pack/plugins/ingest_manager/server/services/install_script/install_templates/macos.ts
@@ -4,10 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { resolve } from 'path';
 import { InstallTemplateFunction } from './types';
 
+const PROJECT_ROOT = resolve(__dirname, '../../../../');
 export const macosInstallTemplate: InstallTemplateFunction = variables => `#!/bin/sh
 
-eval "node scripts/dev_agent --enrollmentApiKey=$API_KEY --kibanaUrl=${variables.kibanaUrl}"
+eval "node ${PROJECT_ROOT}/scripts/dev_agent --enrollmentApiKey=$API_KEY --kibanaUrl=${variables.kibanaUrl}"
 
 `;
