40fcf7e69e83cdba7cade5cbd50336a5db262f4b
Sébastien <sebastien.loix@elastic.co>
2020-02-21 11:49:21 +0530

    Remove unnecessary legacy config

--- x-pack/legacy/plugins/index_management/index.ts
+++ x-pack/legacy/plugins/index_management/index.ts
@@ -8,22 +8,5 @@ export function indexManagement(kibana: any) {
   return new kibana.Plugin({
     id: 'index_management',
     configPrefix: 'xpack.index_management',
-    // TODO: Remove once All dependent apps have migrated to NP
-    config(Joi: any) {
-      return Joi.object({
-        // display menu item
-        ui: Joi.object({
-          enabled: Joi.boolean().default(true),
-        }).default(),
-
-        // enable plugin
-        enabled: Joi.boolean().default(true),
-      }).default();
-    },
-    isEnabled(config: any) {
-      return (
-        config.get('xpack.index_management.enabled') && config.get('xpack.index_management.enabled')
-      );
-    },
   });
 }
