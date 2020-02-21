733d5abbeb8ed7fd923a1a7933aa1d6963bfb885
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-21 14:33:49 -0700

    Drop build dir.

---
+++ src/dev/code_coverage/cc_app/build/precache-manifest.bfefd7c03ec635f43b3ea97c133c929f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ac7572f576ae27b4272da5fe6ec474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40e7958c65e83ee3b9bd",
+    "url": "/static/css/main.19115a7b.chunk.css"
+  },
+  {
+    "revision": "c1d8bc4e519cb036a799",
+    "url": "/static/js/2.b21a70ae.chunk.js"
+  },
+  {
+    "revision": "40e7958c65e83ee3b9bd",
+    "url": "/static/js/main.2ac17b1f.chunk.js"
+  },
+  {
+    "revision": "cdec5b5861ae9b8490c4",
+    "url": "/static/js/runtime-main.f73b9df7.js"
+  },
+  {
+    "revision": "752a955971cde59cee29773b0ab7bd80",
+    "url": "/static/media/logo_kibana_32_color.752a9559.svg"
+  }
+]);
