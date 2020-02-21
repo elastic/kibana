537ffa77dd79ed833d421510938c46389b8a1f60
Andrew Cholakian <andrew@andrewvc.com>
2020-02-20 18:39:50 -0600

    Add high card test

--- x-pack/test/api_integration/apis/uptime/rest/snapshot.ts
+++ x-pack/test/api_integration/apis/uptime/rest/snapshot.ts
@@ -8,7 +8,7 @@ import { expectFixtureEql } from '../graphql/helpers/expect_fixture_eql';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 import { makeChecksWithStatus, getChecksDateRange } from '../graphql/helpers/make_checks';
 
-export default function({ getService }: FtrProviderContext) {
+export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
 
   describe('snapshot count', () => {
@@ -26,77 +26,87 @@ export default function({ getService }: FtrProviderContext) {
     });
 
     describe('when data is present', async () => {
-      const numUpMonitors = 10;
-      const numDownMonitors = 7;
-      const numIps = 2;
-      const checksPerMonitor = 5;
-      const scheduleEvery = 10000; // fake monitor checks every 10s
-      let dateRange: { start: string; end: string };
+      [true, false].forEach(async (highCardinality: boolean) => {
+        const cardinality = highCardinality ? 'high' : 'low';
+        const cardMultiplier = highCardinality ? 200 : 1;
+        const cardSnapshotSuffix = highCardinality ? '_high_card' : '';
 
-      [true, false].forEach(async (includeTimespan: boolean) => {
-        describe(`with timespans ${includeTimespan ? 'included' : 'missing'}`, async () => {
-          before(async () => {
-            const promises: Array<Promise<any>> = [];
+        describe(`with ${cardinality} cardinality`, async () => {
+          const numUpMonitors = 10 * cardMultiplier;
+          const numDownMonitors = 7 & cardMultiplier;
+          const numIps = 2;
+          const checksPerMonitor = 5;
+          const scheduleEvery = 10000; // fake monitor checks every 10s
+          let dateRange: { start: string; end: string };
 
-            // When includeTimespan is false we have to remove the values there.
-            let mogrify = (d: any) => d;
-            if ((includeTimespan = false)) {
-              mogrify = (d: any): any => {
-                d.monitor.delete('timespan');
-                return d;
-              };
-            }
+          [true, false].forEach(async (includeTimespan: boolean) => {
+            describe(`with timespans ${includeTimespan ? 'included' : 'missing'}`, async () => {
+              before(async () => {
+                const promises: Array<Promise<any>> = [];
 
-            const makeMonitorChecks = async (monitorId: string, status: 'up' | 'down') => {
-              return makeChecksWithStatus(
-                getService('legacyEs'),
-                monitorId,
-                checksPerMonitor,
-                numIps,
-                scheduleEvery,
-                {},
-                status,
-                mogrify
-              );
-            };
+                // When includeTimespan is false we have to remove the values there.
+                let mogrify = (d: any) => d;
+                if ((includeTimespan = false)) {
+                  mogrify = (d: any): any => {
+                    d.monitor.delete('timespan');
+                    return d;
+                  };
+                }
 
-            for (let i = 0; i < numUpMonitors; i++) {
-              promises.push(makeMonitorChecks(`up-${i}`, 'up'));
-            }
-            for (let i = 0; i < numDownMonitors; i++) {
-              promises.push(makeMonitorChecks(`down-${i}`, 'down'));
-            }
+                const makeMonitorChecks = async (monitorId: string, status: 'up' | 'down') => {
+                  return makeChecksWithStatus(
+                    getService('legacyEs'),
+                    monitorId,
+                    checksPerMonitor,
+                    numIps,
+                    scheduleEvery,
+                    {},
+                    status,
+                    mogrify
+                  );
+                };
 
-            const allResults = await Promise.all(promises);
-            dateRange = getChecksDateRange(allResults);
-          });
+                for (let i = 0; i < numUpMonitors; i++) {
+                  promises.push(makeMonitorChecks(`up-${i}`, 'up'));
+                }
+                for (let i = 0; i < numDownMonitors; i++) {
+                  promises.push(makeMonitorChecks(`down-${i}`, 'down'));
+                }
 
-          it('will count all statuses correctly', async () => {
-            const apiResponse = await supertest.get(
-              `/api/uptime/snapshot/count?dateRangeStart=${dateRange.start}&dateRangeEnd=${dateRange.end}`
-            );
+                const allResults = await Promise.all(promises);
+                dateRange = getChecksDateRange(allResults);
+              });
 
-            expectFixtureEql(apiResponse.body, 'snapshot');
-          });
+              it('will count all statuses correctly', async () => {
+                const apiResponse = await supertest.get(
+                  `/api/uptime/snapshot/count?dateRangeStart=${dateRange.start}&dateRangeEnd=${dateRange.end}`
+                );
 
-          it('will fetch a monitor snapshot filtered by down status', async () => {
-            const statusFilter = 'down';
-            const apiResponse = await supertest.get(
-              `/api/uptime/snapshot/count?dateRangeStart=${dateRange.start}&dateRangeEnd=${dateRange.end}&statusFilter=${statusFilter}`
-            );
+                expectFixtureEql(apiResponse.body, 'snapshot' + cardSnapshotSuffix);
+              });
 
-            expectFixtureEql(apiResponse.body, 'snapshot_filtered_by_down');
-          });
+              it('will fetch a monitor snapshot filtered by down status', async () => {
+                const statusFilter = 'down';
+                const apiResponse = await supertest.get(
+                  `/api/uptime/snapshot/count?dateRangeStart=${dateRange.start}&dateRangeEnd=${dateRange.end}&statusFilter=${statusFilter}`
+                );
 
-          it('will fetch a monitor snapshot filtered by up status', async () => {
-            const statusFilter = 'up';
-            const apiResponse = await supertest.get(
-              `/api/uptime/snapshot/count?dateRangeStart=${dateRange.start}&dateRangeEnd=${dateRange.end}&statusFilter=${statusFilter}`
-            );
-            expectFixtureEql(apiResponse.body, 'snapshot_filtered_by_up');
+                expectFixtureEql(apiResponse.body, 'snapshot_filtered_by_down' + cardSnapshotSuffix);
+              });
+
+              it('will fetch a monitor snapshot filtered by up status', async () => {
+                const statusFilter = 'up';
+                const apiResponse = await supertest.get(
+                  `/api/uptime/snapshot/count?dateRangeStart=${dateRange.start}&dateRangeEnd=${dateRange.end}&statusFilter=${statusFilter}`
+                );
+                expectFixtureEql(apiResponse.body, 'snapshot_filtered_by_up' + cardSnapshotSuffix);
+              });
+            });
           });
         });
+
       });
+
     });
   });
 }
