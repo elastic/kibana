4604e1ca932bae5037bbffeaf91d3c6f244f4717
Jean-Louis Leysens <jloleysens@gmail.com>
2020-02-21 11:00:56 +0100

    Restore previous auto-submit request behaviour

--- x-pack/legacy/plugins/painless_lab/public/hooks/use_submit_code.ts
+++ x-pack/legacy/plugins/painless_lab/public/hooks/use_submit_code.ts
@@ -11,12 +11,12 @@ import { Response } from '../common/types';
 import { buildRequestPayload } from '../lib/helpers';
 import { executeCode } from '../lib/execute_code';
 
-const WAIT_MS = 800;
+const DEBOUNCE_MS = 800;
 
 export const useSubmitCode = (http: HttpSetup) => {
   // .then off the same promise reference to enforce sequential
   // execution
-  const promiseChainRef = useRef(Promise.resolve());
+  const currentRequestIdRef = useRef(0);
   const [response, setResponse] = useState<null | Response>(null);
   const [inProgress, setInProgress] = useState(false);
 
@@ -25,27 +25,35 @@ export const useSubmitCode = (http: HttpSetup) => {
       async (code: string, context: string, contextSetup: Record<string, any>) => {
         setInProgress(true);
 
+        // Prevent an older request that resolves after a more recent request from clobbering it.
+        // We store the resulting ID in this closure for comparison when the request resolves.
+        const requestId = ++currentRequestIdRef.current;
+
         try {
           localStorage.setItem('painlessLabCode', code);
           localStorage.setItem('painlessLabContext', context);
           localStorage.setItem('painlessLabContextSetup', JSON.stringify(contextSetup));
-          const result = await promiseChainRef.current.then(() =>
-            executeCode(http, buildRequestPayload(code, context, contextSetup))
-          );
-          setResponse(result);
+          const result = await executeCode(http, buildRequestPayload(code, context, contextSetup));
+
+          if (currentRequestIdRef.current === requestId) {
+            setResponse(result);
+            setInProgress(false);
+          }
+          // else ignore this response...
         } catch (error) {
-          setResponse({
-            result: undefined,
-            error,
-          });
-        } finally {
-          setInProgress(false);
+          if (currentRequestIdRef.current === requestId) {
+            setResponse({
+              error,
+            });
+            setInProgress(false);
+          }
+          // else ignore this response...
         }
       },
-      WAIT_MS,
+      DEBOUNCE_MS,
       { trailing: true }
     ),
-    []
+    [http]
   );
 
   return {
