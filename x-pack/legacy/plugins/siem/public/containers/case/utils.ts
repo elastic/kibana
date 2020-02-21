7c0b8d1407f367dae0f0620dade41f5670c1313c
stephmilovic <stephanie.milovic@elastic.co>
2020-02-21 15:56:41 -0700

    bugfixing

--- x-pack/legacy/plugins/siem/public/containers/case/utils.ts
+++ x-pack/legacy/plugins/siem/public/containers/case/utils.ts
@@ -4,14 +4,27 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { camelCase, isObject, set } from 'lodash';
+import { camelCase, isArray, isObject, set } from 'lodash';
 import { AllCases, AllCasesSnake, Case, CaseSnake } from './types';
 
 export const getTypedPayload = <T>(a: unknown): T => a as T;
 
+export const convertArrayToCamelCase = (arrayOfSnakes: unknown[]): unknown[] =>
+  arrayOfSnakes.reduce((acc: unknown[], value) => {
+    if (isArray(value)) {
+      return [...acc, convertArrayToCamelCase(value)];
+    } else if (isObject(value)) {
+      return [...acc, convertToCamelCase(value)];
+    } else {
+      return [...acc, value];
+    }
+  }, []);
+
 export const convertToCamelCase = <T, U extends {}>(snakeCase: T): U =>
   Object.entries(snakeCase).reduce((acc, [key, value]) => {
-    if (isObject(value)) {
+    if (isArray(value)) {
+      set(acc, camelCase(key), convertArrayToCamelCase(value));
+    } else if (isObject(value)) {
       set(acc, camelCase(key), convertToCamelCase(value));
     } else {
       set(acc, camelCase(key), value);
