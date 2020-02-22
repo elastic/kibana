cc80dc3508bf39e4466c2d5b287f91ad696aa2e7
Christos Nasikas <christos.nasikas@elastic.co>
2020-02-22 20:03:30 +0200

    Create wrappers

---
+++ x-pack/legacy/plugins/siem/public/pages/case/components/wrappers/index.tsx
@@ -0,0 +1,17 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import styled, { css } from 'styled-components';
+
+export const WhitePageWrapper = styled.div`
+  ${({ theme }) => css`
+    background-color: ${theme.eui.euiColorEmptyShade};
+    border-top: ${theme.eui.euiBorderThin};
+    height: 100%;
+    min-height: 100vh;
+  `}
+`;
+
