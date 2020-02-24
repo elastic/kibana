fd35ee3e4897fed6767272fc17dc82a7e39f551f
streamich <streamich@gmail.com>
2020-02-24 22:22:48 +0100

    ci: 🎡 move Storybook build tests to different branch

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -4,13 +4,6 @@ source test/scripts/jenkins_test_setup.sh
 
 if [[ -z "$CODE_COVERAGE" ]] ; then
   "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
-else
-  echo " -> Running jest tests with coverage"
-  node scripts/jest --ci --verbose --coverage
-  echo ""
-  echo ""
-  echo " -> Running mocha tests with coverage"
-  yarn run grunt "test:mochaCoverage";
   echo ""
   echo ""
   # When moving these somewhere also update packages/kbn-storybook/README.md
@@ -19,6 +12,13 @@ else
   yarn storybook canvas --site
   yarn storybook drilldowns --site
   yarn storybook embeddable --site
+else
+  echo " -> Running jest tests with coverage"
+  node scripts/jest --ci --verbose --coverage
+  echo ""
+  echo ""
+  echo " -> Running mocha tests with coverage"
+  yarn run grunt "test:mochaCoverage";
   echo ""
   echo ""
 fi
