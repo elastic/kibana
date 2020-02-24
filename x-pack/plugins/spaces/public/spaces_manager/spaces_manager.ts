3ffc3c276dbd8d835a92ccfaeb4a438ddd0557d8
Larry Gregory <larry.gregory@elastic.co>
2020-02-24 14:26:47 -0500

    remove unused imports

--- x-pack/plugins/spaces/public/spaces_manager/spaces_manager.ts
+++ x-pack/plugins/spaces/public/spaces_manager/spaces_manager.ts
@@ -9,9 +9,7 @@ import { HttpSetup } from 'src/core/public';
 import { SavedObjectsManagementRecord } from 'src/legacy/core_plugins/management/public';
 import { Space } from '../../common/model/space';
 import { GetSpacePurpose } from '../../common/model/types';
-import { ENTER_SPACE_PATH } from '../../common/constants';
 import { CopySavedObjectsToSpaceResponse } from '../copy_saved_objects_to_space/types';
-import { addSpaceIdToPath } from '../../common';
 
 export class SpacesManager {
   private activeSpace$: BehaviorSubject<Space | null> = new BehaviorSubject<Space | null>(null);
