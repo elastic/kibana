354c75da6aca834ce0f7cf78bea0d9e073f7afd5
spalger <spalger@users.noreply.github.com>
2020-02-25 14:59:22 -0700

    focus tests on jest integration

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -2,15 +2,4 @@
 
 source test/scripts/jenkins_test_setup.sh
 
-if [[ -z "$CODE_COVERAGE" ]] ; then
-  "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
-else
-  echo " -> Running jest tests with coverage"
-  node scripts/jest --ci --verbose --coverage
-  echo ""
-  echo ""
-  echo " -> Running mocha tests with coverage"
-  yarn run grunt "test:mochaCoverage";
-  echo ""
-  echo ""
-fi
+"$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
