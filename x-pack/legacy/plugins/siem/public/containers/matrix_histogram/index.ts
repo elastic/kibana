672170b9f96c83f576a1ed053a35fb8985cde875
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2020-02-25 14:24:48 +0000

    [7.x] [SIEM] Remove additional props for matrix histogram (#54979) (#58457)
    * [SIEM] Remove additional props for matrix histogram (#54979)
    
    * update DNS histogram
    
    * fix indent
    
    * hide dropdown if only one option provided
    
    * update DNS histogram
    
    * fix types
    
    * wip
    
    * isolate matrix histogram on server side
    
    * fix type for graphql
    
    * fix types
    
    * fix tests
    
    * fix types
    
    * fix types
    
    * add unit test
    
    * add unit test
    
    * fix types
    
    * split histogram configs to an object
    
    * an idea to simplify more
    
    * add unit test
    
    * remove updateDateRange passing down to matrixHistogram
    
    * change histogramType to enum
    
    * handle the case which config not available
    
    * fix review
    
    * fix review II
    
    * fix parser for dns histogram
    
    * revert change
    
    * isolate parsers
    
    * fix unit
    
    Co-authored-by: Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
    
    * fix syntex
    
    Co-authored-by: Xavier Mouligneau <189600+XavierM@users.noreply.github.com>

--- x-pack/legacy/plugins/siem/public/containers/matrix_histogram/index.ts
+++ x-pack/legacy/plugins/siem/public/containers/matrix_histogram/index.ts
@@ -3,12 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { getOr } from 'lodash/fp';
-import { useEffect, useRef, useState } from 'react';
-import {
-  MatrixHistogramDataTypes,
-  MatrixHistogramQueryProps,
-} from '../../components/matrix_histogram/types';
+import { useEffect, useState, useRef } from 'react';
+import { MatrixHistogramQueryProps } from '../../components/matrix_histogram/types';
 import { DEFAULT_INDEX_KEY } from '../../../common/constants';
 import { useStateToaster } from '../../components/toasters';
 import { errorToToaster } from '../../components/ml/api/error_to_toaster';
@@ -16,20 +12,15 @@ import { useUiSetting$ } from '../../lib/kibana';
 import { createFilter } from '../helpers';
 import { useApolloClient } from '../../utils/apollo_context';
 import { inputsModel } from '../../store';
-import { GetMatrixHistogramQuery } from '../../graphql/types';
+import { MatrixHistogramGqlQuery } from './index.gql_query';
+import { GetMatrixHistogramQuery, MatrixOverTimeHistogramData } from '../../graphql/types';
 
 export const useQuery = <Hit, Aggs, TCache = object>({
-  dataKey,
   endDate,
   errorMessage,
   filterQuery,
-  isAlertsHistogram = false,
-  isAnomaliesHistogram = false,
-  isAuthenticationsHistogram = false,
-  isEventsHistogram = false,
-  isDnsHistogram = false,
+  histogramType,
   isInspected,
-  query,
   stackByField,
   startDate,
 }: MatrixHistogramQueryProps) => {
@@ -37,30 +28,25 @@ export const useQuery = <Hit, Aggs, TCache = object>({
   const [, dispatchToaster] = useStateToaster();
   const refetch = useRef<inputsModel.Refetch>();
   const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState<MatrixHistogramDataTypes[] | null>(null);
+  const [data, setData] = useState<MatrixOverTimeHistogramData[] | null>(null);
   const [inspect, setInspect] = useState<inputsModel.InspectQuery | null>(null);
-  const [totalCount, setTotalCount] = useState(-1);
+  const [totalCount, setTotalCount] = useState<number>(-1);
   const apolloClient = useApolloClient();
 
-  const matrixHistogramVariables: GetMatrixHistogramQuery.Variables = {
-    filterQuery: createFilter(filterQuery),
-    sourceId: 'default',
-    timerange: {
-      interval: '12h',
-      from: startDate!,
-      to: endDate!,
-    },
-    defaultIndex,
-    inspect: isInspected,
-    stackByField,
-    isAlertsHistogram,
-    isAnomaliesHistogram,
-    isAuthenticationsHistogram,
-    isDnsHistogram,
-    isEventsHistogram,
-  };
-
   useEffect(() => {
+    const matrixHistogramVariables: GetMatrixHistogramQuery.Variables = {
+      filterQuery: createFilter(filterQuery),
+      sourceId: 'default',
+      timerange: {
+        interval: '12h',
+        from: startDate!,
+        to: endDate!,
+      },
+      defaultIndex,
+      inspect: isInspected,
+      stackByField,
+      histogramType,
+    };
     let isSubscribed = true;
     const abortCtrl = new AbortController();
     const abortSignal = abortCtrl.signal;
@@ -70,7 +56,7 @@ export const useQuery = <Hit, Aggs, TCache = object>({
       setLoading(true);
       return apolloClient
         .query<GetMatrixHistogramQuery.Query, GetMatrixHistogramQuery.Variables>({
-          query,
+          query: MatrixHistogramGqlQuery,
           fetchPolicy: 'network-only',
           variables: matrixHistogramVariables,
           context: {
@@ -82,13 +68,10 @@ export const useQuery = <Hit, Aggs, TCache = object>({
         .then(
           result => {
             if (isSubscribed) {
-              const isDataKeyAnArray = Array.isArray(dataKey);
-              const rootDataKey = isDataKeyAnArray ? dataKey[0] : `${dataKey}`;
-              const histogramDataKey = isDataKeyAnArray ? dataKey[1] : `matrixHistogramData`;
-              const source = getOr({}, `data.source.${rootDataKey}`, result);
-              setData(getOr([], histogramDataKey, source));
-              setTotalCount(getOr(-1, 'totalCount', source));
-              setInspect(getOr(null, 'inspect', source));
+              const source = result?.data?.source?.MatrixHistogram ?? {};
+              setData(source?.matrixHistogramData ?? []);
+              setTotalCount(source?.totalCount ?? -1);
+              setInspect(source?.inspect ?? null);
               setLoading(false);
             }
           },
@@ -97,8 +80,8 @@ export const useQuery = <Hit, Aggs, TCache = object>({
               setData(null);
               setTotalCount(-1);
               setInspect(null);
-              errorToToaster({ title: errorMessage, error, dispatchToaster });
               setLoading(false);
+              errorToToaster({ title: errorMessage, error, dispatchToaster });
             }
           }
         );
@@ -111,13 +94,14 @@ export const useQuery = <Hit, Aggs, TCache = object>({
     };
   }, [
     defaultIndex,
-    query,
+    errorMessage,
     filterQuery,
+    histogramType,
     isInspected,
-    isDnsHistogram,
     stackByField,
     startDate,
     endDate,
+    data,
   ]);
 
   return { data, loading, inspect, totalCount, refetch: refetch.current };
