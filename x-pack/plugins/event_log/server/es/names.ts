29439789abc05d05f7a12305397f23469a3499d9
Mike Côté <mikecote@users.noreply.github.com>
2020-02-25 18:18:27 -0500

    Add migration support to the event log (#58010)
    * Initial work
    
    * Add missing file
    
    * Add tests where missing
    
    * Add kibana version to esNames
    
    * Share ILM policy across versions

--- x-pack/plugins/event_log/server/es/names.ts
+++ x-pack/plugins/event_log/server/es/names.ts
@@ -4,25 +4,31 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-const EVENT_LOG_NAME_SUFFIX = '-event-log';
+import xPackage from '../../../../package.json';
+
+const EVENT_LOG_NAME_SUFFIX = `-event-log`;
+const EVENT_LOG_VERSION_SUFFIX = `-${xPackage.version}`;
 
 export interface EsNames {
   base: string;
   alias: string;
   ilmPolicy: string;
   indexPattern: string;
+  indexPatternWithVersion: string;
   initialIndex: string;
   indexTemplate: string;
 }
 
 export function getEsNames(baseName: string): EsNames {
   const eventLogName = `${baseName}${EVENT_LOG_NAME_SUFFIX}`;
+  const eventLogNameWithVersion = `${eventLogName}${EVENT_LOG_VERSION_SUFFIX}`;
   return {
     base: baseName,
-    alias: eventLogName,
+    alias: eventLogNameWithVersion,
     ilmPolicy: `${eventLogName}-policy`,
     indexPattern: `${eventLogName}-*`,
-    initialIndex: `${eventLogName}-000001`,
-    indexTemplate: `${eventLogName}-template`,
+    indexPatternWithVersion: `${eventLogNameWithVersion}-*`,
+    initialIndex: `${eventLogNameWithVersion}-000001`,
+    indexTemplate: `${eventLogNameWithVersion}-template`,
   };
 }
