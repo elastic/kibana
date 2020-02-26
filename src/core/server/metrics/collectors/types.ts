0d9b91bb360d3027be8787de7879745e70be3c5e
pgayvallet <pierre.gayvallet@elastic.co>
2020-02-26 12:50:42 +0100

    create base service and collectors

---
+++ src/core/server/metrics/collectors/types.ts
@@ -0,0 +1,99 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/** Base interface for all metrics gatherers */
+export interface MetricsCollector<T> {
+  collect(): Promise<T>;
+}
+
+/**
+ * Process related metrics
+ * @public
+ */
+export interface OpsProcessMetrics {
+  /** process memory usage */
+  memory: {
+    heap: {
+      total_in_bytes: number;
+      used_in_bytes: number;
+      size_limit: number;
+    };
+    resident_set_size_in_bytes: number;
+  };
+  event_loop_delay: number;
+  /** pid of the kibana process */
+  pid: number;
+  /** uptime of the kibana process */
+  uptime_in_millis: number;
+}
+
+/**
+ * OS related metrics
+ * @public
+ */
+export interface OpsOsMetrics {
+  /** The os platform */
+  platform: string;
+  /** The os platform release, prefixed by the platform name */
+  platformRelease: string;
+  /** The os distrib. Only present for linux platforms */
+  distro?: string;
+  /** The os distrib release, prefixed by the os distrib. Only present for linux platforms */
+  distroRelease?: string;
+  /** cpu load metrics */
+  load: {
+    /** load for last minute */
+    '1m': number;
+    /** load for last 5 minutes */
+    '5m': number;
+    /** load for last 15 minutes */
+    '15m': number;
+  };
+  /** system memory usage metrics */
+  memory: {
+    /** total memory available */
+    total_in_bytes: number;
+    /** current free memory */
+    free_in_bytes: number;
+    /** current used memory */
+    used_in_bytes: number;
+  };
+  /** the OS uptime */
+  uptime_in_millis: number;
+}
+
+/**
+ * server related metrics
+ * @public
+ */
+export interface OpsServerMetrics {
+  /** server response time stats */
+  response_times: {
+    avg_in_millis: number;
+    max_in_millis: number;
+  };
+  /** server requests stats */
+  requests: {
+    disconnects: number;
+    total: number;
+    statusCodes: Record<number, number>;
+  };
+  /** number of current concurrent connections to the server */
+  concurrent_connections: number;
+}
---
+++ src/core/server/metrics/types.ts
@@ -0,0 +1,48 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Observable } from 'rxjs';
+import { OpsProcessMetrics, OpsOsMetrics, OpsServerMetrics } from './collectors';
+
+export interface MetricsServiceSetup {
+  getMetrics$: () => Observable<OpsMetrics>;
+}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface MetricsServiceStart {}
+
+export type InternalMetricsServiceSetup = MetricsServiceSetup;
+export type InternalMetricsServiceStart = MetricsServiceStart;
+
+/**
+ * TODO documentation
+ *
+ * @public
+ */
+export interface OpsMetrics {
+  /** Process related metrics */
+  process: OpsProcessMetrics;
+  /** OS related metrics */
+  os: OpsOsMetrics;
+  /** server response time stats */
+  response_times: OpsServerMetrics['response_times'];
+  /** server requests stats */
+  requests: OpsServerMetrics['requests'];
+  /** number of current concurrent connections to the server */
+  concurrent_connections: OpsServerMetrics['concurrent_connections'];
+}
