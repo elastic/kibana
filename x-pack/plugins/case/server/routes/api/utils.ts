256811aa3614db1379c650106a537ab46eadd08f
Christos Nasikas <christos.nasikas@elastic.co>
2020-02-26 10:24:59 +0200

    Support http status codes

--- x-pack/plugins/case/server/routes/api/utils.ts
+++ x-pack/plugins/case/server/routes/api/utils.ts
@@ -65,7 +65,8 @@ export const transformNewComment = ({
 });
 
 export function wrapError(error: any): CustomHttpResponseOptions<ResponseError> {
-  const boom = isBoom(error) ? error : boomify(error);
+  const options = { statusCode: error.statusCode ?? 500 };
+  const boom = isBoom(error) ? error : boomify(error, options);
   return {
     body: boom,
     headers: boom.output.headers,
