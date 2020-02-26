2f09fe927bc48e9129522e6c22bd6e3aca8c4a75
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2020-02-25 21:59:44 -0500

    [SIEM] [Detections] Refactor the all rules page (#58428)
    * Refactor the all rules page to be easier to test
    
    * review with Garrett
    
    * bring back utility bar under condition
    
    * fix bugs tags and allow switch to show loading when enable/disable rule
    
    * fix rules selection when trigerring new rules
    
    * fix imports/exports can only use rule_id as learned today
    
    * review I

--- x-pack/legacy/plugins/siem/public/pages/detection_engine/rules/all/reducer.ts
+++ x-pack/legacy/plugins/siem/public/pages/detection_engine/rules/all/reducer.ts
@@ -4,34 +4,30 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { EuiBasicTable } from '@elastic/eui';
 import {
   FilterOptions,
   PaginationOptions,
   Rule,
 } from '../../../../containers/detection_engine/rules';
-import { TableData } from '../types';
-import { formatRules } from './helpers';
 
+type LoadingRuleAction = 'duplicate' | 'enable' | 'disable' | 'export' | 'delete' | null;
 export interface State {
-  isLoading: boolean;
-  rules: Rule[];
-  selectedItems: TableData[];
-  pagination: PaginationOptions;
+  exportRuleIds: string[];
   filterOptions: FilterOptions;
-  refreshToggle: boolean;
-  tableData: TableData[];
-  exportPayload?: Rule[];
+  loadingRuleIds: string[];
+  loadingRulesAction: LoadingRuleAction;
+  pagination: PaginationOptions;
+  rules: Rule[];
+  selectedRuleIds: string[];
 }
 
 export type Action =
-  | { type: 'refresh' }
-  | { type: 'loading'; isLoading: boolean }
-  | { type: 'deleteRules'; rules: Rule[] }
-  | { type: 'duplicate'; rule: Rule }
-  | { type: 'setExportPayload'; exportPayload?: Rule[] }
-  | { type: 'setSelected'; selectedItems: TableData[] }
-  | { type: 'updateLoading'; ids: string[]; isLoading: boolean }
-  | { type: 'updateRules'; rules: Rule[]; pagination?: PaginationOptions }
+  | { type: 'exportRuleIds'; ids: string[] }
+  | { type: 'loadingRuleIds'; ids: string[]; actionType: LoadingRuleAction }
+  | { type: 'selectedRuleIds'; ids: string[] }
+  | { type: 'setRules'; rules: Rule[] }
+  | { type: 'updateRules'; rules: Rule[] }
   | { type: 'updatePagination'; pagination: Partial<PaginationOptions> }
   | {
       type: 'updateFilterOptions';
@@ -40,53 +36,70 @@ export type Action =
     }
   | { type: 'failure' };
 
-export const allRulesReducer = (state: State, action: Action): State => {
+export const allRulesReducer = (
+  tableRef: React.MutableRefObject<EuiBasicTable<unknown> | undefined>
+) => (state: State, action: Action): State => {
   switch (action.type) {
-    case 'refresh': {
+    case 'exportRuleIds': {
       return {
         ...state,
-        refreshToggle: !state.refreshToggle,
+        loadingRuleIds: action.ids,
+        loadingRulesAction: 'export',
+        exportRuleIds: action.ids,
+      };
+    }
+    case 'loadingRuleIds': {
+      return {
+        ...state,
+        loadingRuleIds: action.actionType == null ? [] : [...state.loadingRuleIds, ...action.ids],
+        loadingRulesAction: action.actionType,
+      };
+    }
+    case 'selectedRuleIds': {
+      return {
+        ...state,
+        selectedRuleIds: action.ids,
+      };
+    }
+    case 'setRules': {
+      if (
+        tableRef != null &&
+        tableRef.current != null &&
+        tableRef.current.changeSelection != null
+      ) {
+        tableRef.current.changeSelection([]);
+      }
+
+      return {
+        ...state,
+        rules: action.rules,
+        selectedRuleIds: [],
+        loadingRuleIds: [],
+        loadingRulesAction: null,
       };
     }
     case 'updateRules': {
-      // If pagination included, this was a hard refresh
-      if (action.pagination) {
+      if (state.rules != null) {
+        const ruleIds = state.rules.map(r => r.id);
+        const updatedRules = action.rules.reduce((rules, updatedRule) => {
+          let newRules = rules;
+          if (ruleIds.includes(updatedRule.id)) {
+            newRules = newRules.map(r => (updatedRule.id === r.id ? updatedRule : r));
+          } else {
+            newRules = [...newRules, updatedRule];
+          }
+          return newRules;
+        }, state.rules);
+        const updatedRuleIds = action.rules.map(r => r.id);
+        const newLoadingRuleIds = state.loadingRuleIds.filter(id => !updatedRuleIds.includes(id));
         return {
           ...state,
-          rules: action.rules,
-          pagination: action.pagination,
-          tableData: formatRules(action.rules),
+          rules: updatedRules,
+          loadingRuleIds: newLoadingRuleIds,
+          loadingRulesAction: newLoadingRuleIds.length === 0 ? null : state.loadingRulesAction,
         };
       }
-
-      const ruleIds = state.rules.map(r => r.rule_id);
-      const updatedRules = action.rules.reverse().reduce((rules, updatedRule) => {
-        let newRules = rules;
-        if (ruleIds.includes(updatedRule.rule_id)) {
-          newRules = newRules.map(r => (updatedRule.rule_id === r.rule_id ? updatedRule : r));
-        } else {
-          newRules = [...newRules, updatedRule];
-        }
-        return newRules;
-      }, state.rules);
-
-      // Update enabled on selectedItems so that batch actions show correct available actions
-      const updatedRuleIdToState = action.rules.reduce<Record<string, boolean>>(
-        (acc, r) => ({ ...acc, [r.id]: r.enabled }),
-        {}
-      );
-      const updatedSelectedItems = state.selectedItems.map(selectedItem =>
-        Object.keys(updatedRuleIdToState).includes(selectedItem.id)
-          ? { ...selectedItem, activate: updatedRuleIdToState[selectedItem.id] }
-          : selectedItem
-      );
-
-      return {
-        ...state,
-        rules: updatedRules,
-        tableData: formatRules(updatedRules),
-        selectedItems: updatedSelectedItems,
-      };
+      return state;
     }
     case 'updatePagination': {
       return {
@@ -110,49 +123,10 @@ export const allRulesReducer = (state: State, action: Action): State => {
         },
       };
     }
-    case 'deleteRules': {
-      const deletedRuleIds = action.rules.map(r => r.rule_id);
-      const updatedRules = state.rules.reduce<Rule[]>(
-        (rules, rule) => (deletedRuleIds.includes(rule.rule_id) ? rules : [...rules, rule]),
-        []
-      );
-      return {
-        ...state,
-        rules: updatedRules,
-        tableData: formatRules(updatedRules),
-        refreshToggle: !state.refreshToggle,
-      };
-    }
-    case 'setSelected': {
-      return {
-        ...state,
-        selectedItems: action.selectedItems,
-      };
-    }
-    case 'updateLoading': {
-      return {
-        ...state,
-        rules: state.rules,
-        tableData: formatRules(state.rules, action.ids),
-      };
-    }
-    case 'loading': {
-      return {
-        ...state,
-        isLoading: action.isLoading,
-      };
-    }
     case 'failure': {
       return {
         ...state,
-        isLoading: false,
         rules: [],
-      };
-    }
-    case 'setExportPayload': {
-      return {
-        ...state,
-        exportPayload: [...(action.exportPayload ?? [])],
       };
     }
     default:
--- x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/rules/utils.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/rules/utils.ts
@@ -4,8 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { pickBy } from 'lodash/fp';
-import { Dictionary } from 'lodash';
+import { pickBy, countBy } from 'lodash/fp';
 import { SavedObject } from 'kibana/server';
 import uuid from 'uuid';
 import { INTERNAL_IDENTIFIER } from '../../../../../common/constants';
@@ -18,7 +17,7 @@ import {
   isRuleStatusFindTypes,
   isRuleStatusSavedObjectType,
 } from '../../rules/types';
-import { OutputRuleAlertRest, ImportRuleAlertRest } from '../../types';
+import { OutputRuleAlertRest, ImportRuleAlertRest, RuleAlertParamsRest } from '../../types';
 import {
   createBulkErrorObject,
   BulkError,
@@ -224,10 +223,14 @@ export const transformOrImportError = (
   }
 };
 
-export const getDuplicates = (lodashDict: Dictionary<number>): string[] => {
-  const hasDuplicates = Object.values(lodashDict).some(i => i > 1);
+export const getDuplicates = (ruleDefinitions: RuleAlertParamsRest[], by: 'rule_id'): string[] => {
+  const mappedDuplicates = countBy(
+    by,
+    ruleDefinitions.filter(r => r[by] != null)
+  );
+  const hasDuplicates = Object.values(mappedDuplicates).some(i => i > 1);
   if (hasDuplicates) {
-    return Object.keys(lodashDict).filter(key => lodashDict[key] > 1);
+    return Object.keys(mappedDuplicates).filter(key => mappedDuplicates[key] > 1);
   }
   return [];
 };
