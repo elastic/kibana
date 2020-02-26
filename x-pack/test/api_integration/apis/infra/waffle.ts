b21849ef6c5b85fee11bdea42935439a1c0f4f1a
Chris Cowan <chris@chriscowan.us>
2020-02-26 13:59:15 -0700

    Trying to fix the tests... again

--- x-pack/test/api_integration/apis/infra/waffle.ts
+++ x-pack/test/api_integration/apis/infra/waffle.ts
@@ -281,7 +281,7 @@ export default function({ getService }: FtrProviderContext) {
               name: 'cpu',
               value: 0.0032,
               max: 0.0038333333333333336,
-              avg: 0.002794444444444444,
+              avg: 0.0027944444444444444,
             });
             const secondNode = nodes[1];
             expect(secondNode).to.have.property('path');
@@ -293,7 +293,7 @@ export default function({ getService }: FtrProviderContext) {
               name: 'cpu',
               value: 0.0032,
               max: 0.0038333333333333336,
-              avg: 0.002794444444444444,
+              avg: 0.0027944444444444444,
             });
           }
         });
