44d220dbc58568b951c5ed3ec58a382dbe31cf78
Chris Cowan <chris@chriscowan.us>
2020-02-26 07:59:01 -0700

    Fixing type_check issues

--- x-pack/plugins/infra/server/lib/adapters/fields/framework_fields_adapter.ts
+++ x-pack/plugins/infra/server/lib/adapters/fields/framework_fields_adapter.ts
@@ -4,28 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { startsWith, uniq, first } from 'lodash';
 import { RequestHandlerContext } from 'src/core/server';
-import { InfraDatabaseSearchResponse } from '../framework';
 import { KibanaFramework } from '../framework/kibana_framework_adapter';
 import { FieldsAdapter, IndexFieldDescriptor } from './adapter_types';
-import { getAllowedListForPrefix } from '../../../../common/ecs_allowed_list';
-import { getAllCompositeData } from '../../../utils/get_all_composite_data';
-import { createAfterKeyHandler } from '../../../utils/create_afterkey_handler';
-
-interface Bucket {
-  key: { dataset: string };
-  doc_count: number;
-}
-
-interface DataSetResponse {
-  datasets: {
-    buckets: Bucket[];
-    after_key: {
-      dataset: string;
-    };
-  };
-}
 
 export class FrameworkFieldsAdapter implements FieldsAdapter {
   private framework: KibanaFramework;
