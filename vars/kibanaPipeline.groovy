0725bfb8d7acc464c9adce99bae0563c69f75f5b
Tre' Seymour <wayne.seymour@elastic.co>
2020-02-26 15:17:46 -0700

    copy over the built app

--- vars/kibanaPipeline.groovy
+++ vars/kibanaPipeline.groovy
@@ -210,24 +210,34 @@ def uploadCoverageStaticSite(timestamp) {
 }
 
 def uploadCoverageStaticSite_PROD(timestamp) {
-  def ARTIFACT_PATTERNS = [
-    'target/kibana-coverage/functional-combined',
-    'target/kibana-coverage/jest-combined',
-    'target/kibana-coverage/mocha-combined',
-    'src/dev/code_coverage/live_coverage_app'
-  ]
+  def uploadPrefix = "gs://elastic-bekitzur-kibana-coverage-live/jobs/${env.JOB_NAME}/${timestamp}/"
 
-  def uploadPrefix = "gs://elastic-bekitzur-kibana-coverage-live/jobs/${env.JOB_NAME}/${BUILD_NUMBER}/${timestamp}/"
+  uploadWithVault(uploadPrefix, 'src/dev/code_coverage/404.html')
+  uploadWithVault(uploadPrefix, 'src/dev/code_coverage/live_coverage_app')
 
-  ARTIFACT_PATTERNS.each { pattern ->
-    withGcpServiceAccount.fromVaultSecret('secret/gce/elastic-bekitzur/service-account/kibana', 'value') {
-      sh """
-        gsutil -m cp -r -a public-read -z js,css,html ${pattern} '${uploadPrefix}'
+  def dataUploadPrefix = uploadPrefix + 'live_coverage_app/'
+  uploadCoverageStaticSiteData(dataUploadPrefix)
+}
+
+def uploadWithVault(prefix, x) {
+  def vaultSecret = 'secret/gce/elastic-bekitzur/service-account/kibana'
+
+  withGcpServiceAccount.fromVaultSecret(vaultSecret, 'value') {
+    sh """
+        gsutil -m cp -r -a public-read -z js,css,html ${x} '${prefix}'
       """
-    }
   }
 }
 
+def uploadCoverageStaticSiteData(prefix) {
+  [
+    'target/kibana-coverage/functional-combined',
+    'target/kibana-coverage/jest-combined',
+    'target/kibana-coverage/mocha-combined',
+  ].each { x ->
+    uploadWithVault(prefix, x)
+  }
+}
 
 def withGcsArtifactUpload(workerName, closure) {
   def uploadPrefix = "kibana-ci-artifacts/jobs/${env.JOB_NAME}/${BUILD_NUMBER}/${workerName}"
