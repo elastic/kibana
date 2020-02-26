899d22197240c4df392b13e8dcffd807a681da27
Thomas Neirynck <thomas@elastic.co>
2020-02-25 22:57:35 -0500

    trim definition to minimum

--- x-pack/legacy/plugins/maps/public/layers/sources/source.d.ts
+++ x-pack/legacy/plugins/maps/public/layers/sources/source.d.ts
@@ -8,35 +8,11 @@ import { ISourceDescriptor } from '../../../common/descriptor_types';
 import { ILayer } from '../layer';
 
 export interface ISource {
-  destroy(): void;
-
-  cloneDescriptor(): ISourceDescriptor;
-
-  supportsFitToBounds(): Promise<boolean>;
-
-  getImmutableProperties(): Promise<unknown[]>;
-
-  getInspectorAdapters(): unknown;
-
   createDefaultLayer(): ILayer;
-
-  getDisplayName(): string;
 }
 
 export class AbstractSource implements ISource {
   constructor(sourceDescriptor: ISourceDescriptor, inspectorAdapters: object);
 
-  cloneDescriptor(): ISourceDescriptor;
-
   createDefaultLayer(): ILayer;
-
-  destroy(): void;
-
-  getDisplayName(): string;
-
-  getImmutableProperties(): Promise<unknown[]>;
-
-  getInspectorAdapters(): unknown;
-
-  supportsFitToBounds(): Promise<boolean>;
 }
