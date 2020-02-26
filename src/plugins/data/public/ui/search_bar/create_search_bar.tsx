c8162815b66b9ee6c394033fcd73dbaec9ee6864
Liza K <liza.katz@elastic.co>
2020-02-26 14:57:17 +0200

    removed useCallback

--- src/plugins/data/public/ui/search_bar/create_search_bar.tsx
+++ src/plugins/data/public/ui/search_bar/create_search_bar.tsx
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { CoreStart } from 'src/core/public';
 import { IStorageWrapper } from 'src/plugins/kibana_utils/public';
 import { KibanaContextProvider } from '../../../../kibana_react/public';
