5db064fda9282c850a40aacff8d038ccfa376f6e
Thomas Neirynck <thomas@elastic.co>
2020-02-25 22:51:12 -0500

    add constructor to al

--- x-pack/legacy/plugins/maps/public/layers/layer.d.ts
+++ x-pack/legacy/plugins/maps/public/layers/layer.d.ts
@@ -3,10 +3,19 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import { ILayerDescriptor } from '../../common/descriptor_types';
+import { ISource } from './sources/source';
+
 export interface ILayer {
   destroy(): void;
 }
 
+interface ILayerArguments {
+  layerDescriptor: ILayerDescriptor;
+  source: ISource;
+}
+
 export class AbstractLayer implements ILayer {
+  constructor(layerArguments: ILayerArguments);
   destroy(): void;
 }
