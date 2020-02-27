780bbaf020f24076beda4457365d9776f70d3aa8
Ryland Herrick <ryalnd@gmail.com>
2020-02-27 17:31:01 -0600

    Refactor signals API tests to use core mocks
    * Simplifies our mocking verbosity by leveraging core mocks
    * Simplifies test setup by isolating a reference to our fetch mock
    * Abstracts response structure to pure helper functions
    
    The try/catch tests had some false positives in that nothing would be
    asserted if the code did not throw an error. These proved to be masking
    a gap in coverage for our get/create signal index requests, which do not
    leverage `throwIfNotOk` but instead rely on the fetch to throw an error;
    once that behavior is verified we can update those tests to have our
    fetchMock throw errors, and we should be all set.

--- x-pack/legacy/plugins/siem/public/containers/detection_engine/signals/api.test.ts
+++ x-pack/legacy/plugins/siem/public/containers/detection_engine/signals/api.test.ts
@@ -21,70 +21,44 @@ import {
 } from './api';
 import { ToasterErrors } from '../../../components/ml/api/throw_if_not_ok';
 import { PostSignalError, SignalIndexError } from './types';
+import { coreMock } from '../../../../../../../../src/core/public/mocks';
 
 const abortCtrl = new AbortController();
 const mockKibanaServices = KibanaServices.get as jest.Mock;
 jest.mock('../../../lib/kibana');
 
-const mockfetchSuccess = (body: unknown, fetchMock?: jest.Mock) => {
-  if (fetchMock) {
-    mockKibanaServices.mockImplementation(() => ({
-      http: {
-        fetch: fetchMock,
-      },
-    }));
-  } else {
-    mockKibanaServices.mockImplementation(() => ({
-      http: {
-        fetch: () => ({
-          response: {
-            ok: true,
-            message: 'success',
-            text: 'success',
-          },
-          body,
-        }),
-      },
-    }));
-  }
-};
+const mockServices = coreMock.createStart();
+mockKibanaServices.mockReturnValue(mockServices);
+const fetchMock = mockServices.http.fetch;
 
-const mockfetchError = () => {
-  mockKibanaServices.mockImplementation(() => ({
-    http: {
-      fetch: () => ({
-        response: {
-          ok: false,
-          text: () =>
-            JSON.stringify({
-              message: 'super mega error, it is not that bad',
-            }),
-        },
-        body: null,
+const successResponse = (body: unknown) => ({
+  response: {
+    ok: true,
+    message: 'success',
+    text: 'success',
+  },
+  body,
+});
+
+const failResponse = () => ({
+  response: {
+    ok: false,
+    text: () =>
+      JSON.stringify({
+        message: 'super mega error, it is not that bad',
       }),
-    },
-  }));
-};
+  },
+  body: null,
+});
 
 describe('Detections Signals API', () => {
-  const fetchMock = jest.fn();
-
   describe('fetchQuerySignals', () => {
     beforeEach(() => {
-      mockKibanaServices.mockClear();
       fetchMock.mockClear();
-      fetchMock.mockImplementation(() => ({
-        response: {
-          ok: true,
-          message: 'success',
-          text: 'success',
-        },
-        body: signalsMock,
-      }));
+      fetchMock.mockResolvedValue(successResponse(signalsMock));
     });
-    test('check parameter url, body', async () => {
-      mockfetchSuccess(null, fetchMock);
 
+    test('check parameter url, body', async () => {
       await fetchQuerySignals({ query: mockSignalsQuery, signal: abortCtrl.signal });
       expect(fetchMock).toHaveBeenCalledWith('/api/detection_engine/signals/search', {
         asResponse: true,
@@ -94,41 +68,35 @@ describe('Detections Signals API', () => {
         signal: abortCtrl.signal,
       });
     });
+
     test('happy path', async () => {
-      mockfetchSuccess(signalsMock);
       const signalsResp = await fetchQuerySignals({
         query: mockSignalsQuery,
         signal: abortCtrl.signal,
       });
       expect(signalsResp).toEqual(signalsMock);
     });
+
     test('unhappy path', async () => {
-      mockfetchError();
+      fetchMock.mockResolvedValue(failResponse());
+      let error;
       try {
         await fetchQuerySignals({ query: mockSignalsQuery, signal: abortCtrl.signal });
       } catch (exp) {
-        expect(exp).toBeInstanceOf(ToasterErrors);
-        expect(exp.message).toEqual('super mega error, it is not that bad');
+        error = exp;
       }
+      expect(error).toBeInstanceOf(ToasterErrors);
+      expect(error.message).toEqual('super mega error, it is not that bad');
     });
   });
 
   describe('updateSignalStatus', () => {
     beforeEach(() => {
-      mockKibanaServices.mockClear();
       fetchMock.mockClear();
-      fetchMock.mockImplementation(() => ({
-        response: {
-          ok: true,
-          message: 'success',
-          text: 'success',
-        },
-        body: {},
-      }));
+      fetchMock.mockResolvedValue(successResponse({}));
     });
-    test('check parameter url, body when closing a signal', async () => {
-      mockfetchSuccess(null, fetchMock);
 
+    test('check parameter url, body when closing a signal', async () => {
       await updateSignalStatus({
         query: mockStatusSignalQuery,
         signal: abortCtrl.signal,
@@ -142,9 +110,8 @@ describe('Detections Signals API', () => {
         signal: abortCtrl.signal,
       });
     });
-    test('check parameter url, body when opening a signal', async () => {
-      mockfetchSuccess(null, fetchMock);
 
+    test('check parameter url, body when opening a signal', async () => {
       await updateSignalStatus({
         query: mockStatusSignalQuery,
         signal: abortCtrl.signal,
@@ -158,8 +125,8 @@ describe('Detections Signals API', () => {
         signal: abortCtrl.signal,
       });
     });
+
     test('happy path', async () => {
-      mockfetchSuccess({});
       const signalsResp = await updateSignalStatus({
         query: mockStatusSignalQuery,
         signal: abortCtrl.signal,
@@ -167,8 +134,10 @@ describe('Detections Signals API', () => {
       });
       expect(signalsResp).toEqual({});
     });
+
     test('unhappy path', async () => {
-      mockfetchError();
+      fetchMock.mockResolvedValue(failResponse());
+      let error;
       try {
         await updateSignalStatus({
           query: mockStatusSignalQuery,
@@ -176,61 +145,54 @@ describe('Detections Signals API', () => {
           status: 'open',
         });
       } catch (exp) {
-        expect(exp).toBeInstanceOf(ToasterErrors);
-        expect(exp.message).toEqual('super mega error, it is not that bad');
+        error = exp;
       }
+      expect(error).toBeInstanceOf(ToasterErrors);
+      expect(error.message).toEqual('super mega error, it is not that bad');
     });
   });
 
   describe('getSignalIndex', () => {
     beforeEach(() => {
-      mockKibanaServices.mockClear();
       fetchMock.mockClear();
-      fetchMock.mockImplementation(() => mockSignalIndex);
+      fetchMock.mockResolvedValue(mockSignalIndex);
     });
-    test('check parameter url', async () => {
-      mockfetchSuccess(null, fetchMock);
 
+    test('check parameter url', async () => {
       await getSignalIndex({ signal: abortCtrl.signal });
       expect(fetchMock).toHaveBeenCalledWith('/api/detection_engine/index', {
         method: 'GET',
         signal: abortCtrl.signal,
       });
     });
+
     test('happy path', async () => {
-      mockfetchSuccess(null, fetchMock);
       const signalsResp = await getSignalIndex({
         signal: abortCtrl.signal,
       });
       expect(signalsResp).toEqual(mockSignalIndex);
     });
+
     test('unhappy path', async () => {
-      mockfetchError();
+      fetchMock.mockResolvedValue(failResponse());
+      let error;
       try {
         await getSignalIndex({ signal: abortCtrl.signal });
       } catch (exp) {
-        expect(exp).toBeInstanceOf(SignalIndexError);
-        expect(exp.message).toEqual('super mega error, it is not that bad');
+        error = exp;
       }
+      expect(error).toBeInstanceOf(SignalIndexError);
+      expect(error.message).toEqual('super mega error, it is not that bad');
     });
   });
 
   describe('getUserPrivilege', () => {
     beforeEach(() => {
-      mockKibanaServices.mockClear();
       fetchMock.mockClear();
-      fetchMock.mockImplementation(() => ({
-        response: {
-          ok: true,
-          message: 'success',
-          text: 'success',
-        },
-        body: mockUserPrivilege,
-      }));
+      fetchMock.mockResolvedValue(successResponse(mockUserPrivilege));
     });
-    test('check parameter url', async () => {
-      mockfetchSuccess(null, fetchMock);
 
+    test('check parameter url', async () => {
       await getUserPrivilege({ signal: abortCtrl.signal });
       expect(fetchMock).toHaveBeenCalledWith('/api/detection_engine/privileges', {
         asResponse: true,
@@ -238,54 +200,59 @@ describe('Detections Signals API', () => {
         signal: abortCtrl.signal,
       });
     });
+
     test('happy path', async () => {
-      mockfetchSuccess(mockUserPrivilege);
       const signalsResp = await getUserPrivilege({
         signal: abortCtrl.signal,
       });
       expect(signalsResp).toEqual(mockUserPrivilege);
     });
+
     test('unhappy path', async () => {
-      mockfetchError();
+      fetchMock.mockResolvedValue(failResponse());
+      let error;
       try {
         await getUserPrivilege({ signal: abortCtrl.signal });
       } catch (exp) {
-        expect(exp).toBeInstanceOf(ToasterErrors);
-        expect(exp.message).toEqual('super mega error, it is not that bad');
+        error = exp;
       }
+
+      expect(error).toBeInstanceOf(ToasterErrors);
+      expect(error.message).toEqual('super mega error, it is not that bad');
     });
   });
 
   describe('createSignalIndex', () => {
     beforeEach(() => {
-      mockKibanaServices.mockClear();
       fetchMock.mockClear();
-      fetchMock.mockImplementation(() => mockSignalIndex);
+      fetchMock.mockResolvedValue(mockSignalIndex);
     });
-    test('check parameter url', async () => {
-      mockfetchSuccess(null, fetchMock);
 
+    test('check parameter url', async () => {
       await createSignalIndex({ signal: abortCtrl.signal });
       expect(fetchMock).toHaveBeenCalledWith('/api/detection_engine/index', {
         method: 'POST',
         signal: abortCtrl.signal,
       });
     });
+
     test('happy path', async () => {
-      mockfetchSuccess(null, fetchMock);
       const signalsResp = await createSignalIndex({
         signal: abortCtrl.signal,
       });
       expect(signalsResp).toEqual(mockSignalIndex);
     });
+
     test('unhappy path', async () => {
-      mockfetchError();
+      fetchMock.mockResolvedValue(failResponse());
+      let error;
       try {
         await createSignalIndex({ signal: abortCtrl.signal });
       } catch (exp) {
-        expect(exp).toBeInstanceOf(PostSignalError);
-        expect(exp.message).toEqual('super mega error, it is not that bad');
+        error = exp;
       }
+      expect(error).toBeInstanceOf(PostSignalError);
+      expect(error.message).toEqual('super mega error, it is not that bad');
     });
   });
 });
