3e97357c11b6f2f458f6c564de734f21112a4a7d
spalger <spalger@users.noreply.github.com>
2020-02-27 08:41:34 -0700

    run all tests again

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -2,4 +2,15 @@
 
 source test/scripts/jenkins_test_setup.sh
 
-"$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
+if [[ -z "$CODE_COVERAGE" ]] ; then
+  "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
+else
+  echo " -> Running jest tests with coverage"
+  node scripts/jest --ci --verbose --coverage
+  echo ""
+  echo ""
+  echo " -> Running mocha tests with coverage"
+  yarn run grunt "test:mochaCoverage";
+  echo ""
+  echo ""
+fi
