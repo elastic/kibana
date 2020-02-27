73b7aa81d891e94988b18f7f8c9765936df976da
Nathan Reese <reese.nathan@gmail.com>
2020-02-27 15:54:15 -0700

    [Maps] source descriptor types

--- x-pack/legacy/plugins/maps/common/descriptor_types.d.ts
+++ x-pack/legacy/plugins/maps/common/descriptor_types.d.ts
@@ -4,17 +4,74 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export interface ISourceDescriptor {
+import { AGG_TYPE, GRID_RESOLUTION, RENDER_AS } from './constants';
+
+export type SourceDescriptor =
+  | XYZTMSSourceDescriptor
+  | EMSTMSSourceDescriptor
+  | EMSFileSourceDescriptor
+  | ESGeoGridSourceDescriptor
+  | ESSearchSourceDescriptor
+  | ESPewPewSource;
+
+export interface XYZTMSSourceDescriptor {
   id: string;
   type: string;
-}
-
-export interface IXYZTMSSourceDescriptor extends ISourceDescriptor {
   urlTemplate: string;
 }
 
-export interface ILayerDescriptor {
-  sourceDescriptor: ISourceDescriptor;
+export interface EMSTMSSourceDescriptor {
+  id?: string; // EMS TMS layer id. Used when !isAutoSelect
+  type: string;
+  isAutoSelect: boolean;
+}
+
+export interface EMSFileSourceDescriptor {
+  id: string; // EMS file id
+  type: string;
+  tooltipProperties: string[];
+}
+
+export interface ESSourceDescriptor {
+  id: string; // UUID
+  type: string;
+  indexPatternId: string;
+  geoField: string;
+}
+
+export interface AggDescriptor {
+  field?: string; // count aggregation does not require field. All other aggregation types do
+  label?: string;
+  type: AGG_TYPE;
+}
+
+export type ESGeoGridSourceDescriptor = ESSourceDescriptor & {
+  requestType: RENDER_AS;
+  resolution: GRID_RESOLUTION;
+  metrics: AggDescriptor[];
+};
+
+export interface ESPewPewSource {
+  id: string; // UUID
+  type: string;
+  indexPatternId: string;
+  sourceGeoField: string;
+  destGeoField: string;
+  metrics: AggDescriptor[];
+}
+
+export type ESSearchSourceDescriptor = ESSourceDescriptor & {
+  filterByMapBounds: boolean;
+  tooltipProperties: string[];
+  sortField: string;
+  sortOrder: string;
+  useTopHits: boolean;
+  topHitsSplitField: string;
+  topHitsSize: number;
+};
+
+export interface LayerDescriptor {
+  sourceDescriptor: SourceDescriptor;
   id: string;
   label?: string;
 }
