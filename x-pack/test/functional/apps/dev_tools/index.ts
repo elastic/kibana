e772d911fbd7f07c4833d80a9f2acdeb4178a302
LeeDr <lee.drengenberg@elastic.co>
2020-02-28 13:27:31 -0600

    rebalance x-pack groups

--- x-pack/test/functional/apps/dev_tools/index.ts
+++ x-pack/test/functional/apps/dev_tools/index.ts
@@ -7,7 +7,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function({ loadTestFile }: FtrProviderContext) {
   describe('console', function() {
-    this.tags('ciGroup3');
+    this.tags('10');
 
     loadTestFile(require.resolve('./feature_controls'));
     loadTestFile(require.resolve('./searchprofiler_editor'));
