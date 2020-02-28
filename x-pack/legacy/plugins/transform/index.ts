22d2fb50afc8f5bee215d6331b543b27cc5fdae6
Walter Rafelsberger <walter@elastic.co>
2020-02-28 09:34:42 +0100

    [ML] Fix imports.

--- x-pack/legacy/plugins/transform/index.ts
+++ x-pack/legacy/plugins/transform/index.ts
@@ -4,10 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Legacy } from 'kibana';
 import { resolve } from 'path';
-
-import { PluginInitializerContext } from 'src/core/server';
 
 import { PLUGIN } from './common/constants';
 
