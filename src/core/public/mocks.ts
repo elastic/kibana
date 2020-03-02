7972d4f0ab3ffdd4f2c18bab92fa9d543e14c80f
Josh Dover <me@joshdover.com>
2020-03-02 13:31:22 -0700

    Fix imports in mocks file

--- src/core/public/mocks.ts
+++ src/core/public/mocks.ts
@@ -18,9 +18,13 @@
  */
 import { createMemoryHistory } from 'history';
 
+// Only import types from '.' to avoid triggering default Jest mocks.
+import { CoreContext, PluginInitializerContext, AppMountParameters } from '.';
+// Import values from their individual modules instead.
+import { ScopedHistory } from './application';
+
 import { applicationServiceMock } from './application/application_service.mock';
 import { chromeServiceMock } from './chrome/chrome_service.mock';
-import { CoreContext, PluginInitializerContext, AppMountParameters, ScopedHistory } from '.';
 import { docLinksServiceMock } from './doc_links/doc_links_service.mock';
 import { fatalErrorsServiceMock } from './fatal_errors/fatal_errors_service.mock';
 import { httpServiceMock } from './http/http_service.mock';
