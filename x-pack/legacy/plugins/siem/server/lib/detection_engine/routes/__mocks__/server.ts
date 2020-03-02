3254c49e495ae7d2934f98c481a7b2ea1886d637
Ryland Herrick <ryalnd@gmail.com>
2020-03-02 09:09:41 -0600

    Remove need for a route spy
    Instead of mocking certain router methods with the same spy and then
    retrieving its calls, we can simply iterate over the calls of the router
    methods we care about. This is a little less logic and a little more
    straightforward.

--- x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/__mocks__/server.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/__mocks__/server.ts
@@ -20,66 +20,33 @@ interface Route {
   config: RouteConfig<unknown, unknown, unknown, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
   handler: RequestHandler;
 }
+const getRoute = (routerMock: MockServer['router']): Route => {
+  const routeCalls = [
+    ...routerMock.get.mock.calls,
+    ...routerMock.post.mock.calls,
+    ...routerMock.put.mock.calls,
+    ...routerMock.patch.mock.calls,
+    ...routerMock.delete.mock.calls,
+  ];
 
-const buildRoutingTools = () => {
-  const mock = httpServiceMock.createRouter();
-  const spy = jest.fn();
+  const [route] = routeCalls;
+  if (!route) {
+    throw new Error('No route registered!');
+  }
 
-  mock.get.mockImplementation(spy);
-  mock.post.mockImplementation(spy);
-  mock.patch.mockImplementation(spy);
-  mock.put.mockImplementation(spy);
-  mock.delete.mockImplementation(spy);
-
-  const getRoute = (): Route => {
-    const [route] = spy.mock.calls;
-    if (!route) {
-      throw new Error('No route registered!');
-    }
-
-    const [config, handler] = route;
-    return { config, handler };
-  };
-
-  return { routerMock: mock, getRoute };
+  const [config, handler] = route;
+  return { config, handler };
 };
 
 const buildResultMock = () => ({ ok: jest.fn(x => x), badRequest: jest.fn(x => x) });
 
 class MockServer {
-  public router: ReturnType<typeof httpServiceMock.createRouter>;
-
   constructor(
-    private routing = buildRoutingTools(),
+    public router = httpServiceMock.createRouter(),
     private responseMock = responseFactoryMock.create(),
     private contextMock = requestContextMock.create(),
     private resultMock = buildResultMock()
-  ) {
-    this.router = this.routing.routerMock;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private maybeValidate(part: any, validator?: any): any {
-    return typeof validator === 'function' ? validator(part, this.resultMock) : part;
-  }
-
-  private validateRequest(request: KibanaRequest): KibanaRequest {
-    const validations = this.routing.getRoute().config.validate;
-
-    if (!validations) {
-      return request;
-    }
-
-    const validatedRequest = requestMock.create({
-      path: request.route.path,
-      method: request.route.method,
-      body: this.maybeValidate(request.body, validations.body),
-      query: this.maybeValidate(request.query, validations.query),
-      params: this.maybeValidate(request.params, validations.params),
-    });
-
-    return validatedRequest;
-  }
+  ) {}
 
   public validate(request: KibanaRequest) {
     this.validateRequest(request);
@@ -93,8 +60,34 @@ class MockServer {
       throw new Error(`Request was rejected with message: '${rejection}'`);
     }
 
-    await this.routing.getRoute().handler(context, validatedRequest, this.responseMock);
+    await this.getRoute().handler(context, validatedRequest, this.responseMock);
     return responseAdapter(this.responseMock);
+  }
+
+  private getRoute(): Route {
+    return getRoute(this.router);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private maybeValidate(part: any, validator?: any): any {
+    return typeof validator === 'function' ? validator(part, this.resultMock) : part;
+  }
+
+  private validateRequest(request: KibanaRequest): KibanaRequest {
+    const validations = this.getRoute().config.validate;
+    if (!validations) {
+      return request;
+    }
+
+    const validatedRequest = requestMock.create({
+      path: request.route.path,
+      method: request.route.method,
+      body: this.maybeValidate(request.body, validations.body),
+      query: this.maybeValidate(request.query, validations.query),
+      params: this.maybeValidate(request.params, validations.params),
+    });
+
+    return validatedRequest;
   }
 }
 
