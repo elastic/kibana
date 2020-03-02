26718be03bbf1e0e87ee9a1b61a7faf7091f1285
Christos Nasikas <christos.nasikas@elastic.co>
2020-03-02 15:07:33 +0200

    Create field mapping row

---
+++ x-pack/legacy/plugins/siem/public/pages/case/components/configure_cases/field_mapping_row.tsx
@@ -0,0 +1,83 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { useState, useCallback, useMemo } from 'react';
+import { EuiFlexItem, EuiFlexGroup, EuiSuperSelect, EuiIcon } from '@elastic/eui';
+
+interface ThirdPartyField {
+  value: string;
+  label: string;
+}
+interface RowProps {
+  siemField: string;
+  thirdPartyFields: ThirdPartyField[];
+}
+
+const editUpdateOptions = [
+  {
+    value: 'nothing',
+    inputDisplay: <span>{'Nothing'}</span>,
+    'data-test-subj': 'edit-update-option-nothing',
+  },
+  {
+    value: 'overwrite',
+    inputDisplay: <span>{'Overwrite'}</span>,
+    'data-test-subj': 'edit-update-option-overwrite',
+  },
+  {
+    value: 'append',
+    inputDisplay: <span>{'Append'}</span>,
+    'data-test-subj': 'edit-update-option-append',
+  },
+];
+
+const FieldMappingRowComponent: React.FC<RowProps> = ({ siemField, thirdPartyFields }) => {
+  const [selectedEditUpdate, selectEditUpdate] = useState(editUpdateOptions[0].value);
+  const onEditUpdateChange = useCallback(option => selectEditUpdate(option), [selectedEditUpdate]);
+
+  const thirdPartyOptions = useMemo(
+    () =>
+      thirdPartyFields.map(field => ({
+        value: field.value,
+        inputDisplay: <span>{field.label}</span>,
+      })),
+    [thirdPartyFields]
+  );
+
+  const [selectedThirdParty, selectThirdParty] = useState(thirdPartyOptions[0].value);
+  const onThirdPartyChange = useCallback(option => selectThirdParty(option), [selectedThirdParty]);
+
+  return (
+    <EuiFlexGroup alignItems="center">
+      <EuiFlexItem>
+        <EuiFlexGroup component="span" justifyContent="spaceBetween">
+          <EuiFlexItem component="span" grow={false}>
+            {siemField}
+          </EuiFlexItem>
+          <EuiFlexItem component="span" grow={false}>
+            <EuiIcon type="sortRight" />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiSuperSelect
+          options={thirdPartyOptions}
+          valueOfSelected={selectedThirdParty}
+          onChange={onThirdPartyChange}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiSuperSelect
+          options={editUpdateOptions}
+          valueOfSelected={selectedEditUpdate}
+          onChange={onEditUpdateChange}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
+
+export const FieldMappingRow = React.memo(FieldMappingRowComponent);
