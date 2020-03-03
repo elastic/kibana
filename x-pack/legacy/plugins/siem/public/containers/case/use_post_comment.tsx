5c8e98a74bfc5f00c4dc5e98dd8a795d26b0a118
Steph Milovic <stephanie.milovic@elastic.co>
2020-03-03 11:39:27 -0700

    [7.x] [SIEM] [Case] Comments to case view (#58315) (#59098)

--- x-pack/legacy/plugins/siem/public/containers/case/use_post_comment.tsx
+++ x-pack/legacy/plugins/siem/public/containers/case/use_post_comment.tsx
@@ -8,23 +8,24 @@ import { Dispatch, SetStateAction, useEffect, useReducer, useState } from 'react
 import { useStateToaster } from '../../components/toasters';
 import { errorToToaster } from '../../components/ml/api/error_to_toaster';
 import * as i18n from './translations';
-import { FETCH_FAILURE, FETCH_INIT, FETCH_SUCCESS, POST_NEW_CASE } from './constants';
-import { Case, NewCase } from './types';
-import { createCase } from './api';
+import { FETCH_FAILURE, FETCH_INIT, FETCH_SUCCESS, POST_NEW_COMMENT } from './constants';
+import { Comment, NewComment } from './types';
+import { createComment } from './api';
 import { getTypedPayload } from './utils';
 
-interface NewCaseState {
-  data: NewCase;
-  newCase?: Case;
+interface NewCommentState {
+  data: NewComment;
+  newComment?: Comment;
   isLoading: boolean;
   isError: boolean;
+  caseId: string;
 }
 interface Action {
   type: string;
-  payload?: NewCase | Case;
+  payload?: NewComment | Comment;
 }
 
-const dataFetchReducer = (state: NewCaseState, action: Action): NewCaseState => {
+const dataFetchReducer = (state: NewCommentState, action: Action): NewCommentState => {
   switch (action.type) {
     case FETCH_INIT:
       return {
@@ -32,19 +33,19 @@ const dataFetchReducer = (state: NewCaseState, action: Action): NewCaseState => 
         isLoading: true,
         isError: false,
       };
-    case POST_NEW_CASE:
+    case POST_NEW_COMMENT:
       return {
         ...state,
         isLoading: false,
         isError: false,
-        data: getTypedPayload<NewCase>(action.payload),
+        data: getTypedPayload<NewComment>(action.payload),
       };
     case FETCH_SUCCESS:
       return {
         ...state,
         isLoading: false,
         isError: false,
-        newCase: getTypedPayload<Case>(action.payload),
+        newComment: getTypedPayload<Comment>(action.payload),
       };
     case FETCH_FAILURE:
       return {
@@ -56,32 +57,32 @@ const dataFetchReducer = (state: NewCaseState, action: Action): NewCaseState => 
       throw new Error();
   }
 };
-const initialData: NewCase = {
-  description: '',
-  isNew: false,
-  tags: [],
-  title: '',
+const initialData: NewComment = {
+  comment: '',
 };
 
-export const usePostCase = (): [NewCaseState, Dispatch<SetStateAction<NewCase>>] => {
+export const usePostComment = (
+  caseId: string
+): [NewCommentState, Dispatch<SetStateAction<NewComment>>] => {
   const [state, dispatch] = useReducer(dataFetchReducer, {
     isLoading: false,
     isError: false,
+    caseId,
     data: initialData,
   });
   const [formData, setFormData] = useState(initialData);
   const [, dispatchToaster] = useStateToaster();
 
   useEffect(() => {
-    dispatch({ type: POST_NEW_CASE, payload: formData });
+    dispatch({ type: POST_NEW_COMMENT, payload: formData });
   }, [formData]);
 
   useEffect(() => {
-    const postCase = async () => {
+    const postComment = async () => {
       dispatch({ type: FETCH_INIT });
       try {
         const { isNew, ...dataWithoutIsNew } = state.data;
-        const response = await createCase(dataWithoutIsNew);
+        const response = await createComment(dataWithoutIsNew, state.caseId);
         dispatch({ type: FETCH_SUCCESS, payload: response });
       } catch (error) {
         errorToToaster({ title: i18n.ERROR_TITLE, error, dispatchToaster });
@@ -89,7 +90,7 @@ export const usePostCase = (): [NewCaseState, Dispatch<SetStateAction<NewCase>>]
       }
     };
     if (state.data.isNew) {
-      postCase();
+      postComment();
     }
   }, [state.data.isNew]);
   return [state, setFormData];
