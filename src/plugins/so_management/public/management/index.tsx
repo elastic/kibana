ff0813c050c3e8351cfd57cad5b05c757f77d1d1
pgayvallet <pierre.gayvallet@elastic.co>
2020-03-03 11:46:50 +0100

    create client side plugin + register dummy management section

---
+++ src/plugins/so_management/public/management/index.tsx
@@ -0,0 +1,63 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import { i18n } from '@kbn/i18n';
+import { I18nProvider } from '@kbn/i18n/react';
+import { ManagementSetup } from '../../../management/public';
+
+interface RegisterOptions {
+  sections: ManagementSetup['sections'];
+}
+
+const title = i18n.translate('kbn.management.objects.savedObjectsSectionLabel', {
+  defaultMessage: 'Saved Objects XXX',
+});
+
+export const registerManagementSection = ({ sections }: RegisterOptions) => {
+  const kibanaSection = sections.getSection('kibana');
+  if (!kibanaSection) {
+    throw new Error('`kibana` management section not found.');
+  }
+  kibanaSection.registerApp({
+    id: 'toto', // 'objects',
+    title,
+    order: 10,
+    mount: async ({ element, basePath, setBreadcrumbs }) => {
+      ReactDOM.render(
+        <I18nProvider>
+          <HashRouter basename={basePath}>
+            <Switch>
+              <Route path={['/']}>
+                <div> Hello world </div>
+              </Route>
+            </Switch>
+          </HashRouter>
+        </I18nProvider>,
+        element
+      );
+
+      return () => {
+        ReactDOM.unmountComponentAtNode(element);
+      };
+    },
+  });
+};
---
+++ src/plugins/so_management/public/plugin.ts
@@ -0,0 +1,40 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { CoreSetup, CoreStart, Plugin } from 'src/core/public';
+import { ManagementSetup } from '../../management/public';
+import { registerManagementSection } from './management';
+
+interface SetupDependencies {
+  management: ManagementSetup;
+}
+
+export class SavedObjectsManagementPlugin implements Plugin<{}, {}, SetupDependencies> {
+  public setup(core: CoreSetup, { management }: SetupDependencies) {
+    registerManagementSection({
+      sections: management.sections,
+    });
+
+    return {};
+  }
+
+  public start(core: CoreStart) {
+    return {};
+  }
+}
