c09f855f2d1b0c2e2ea31e0526796533badeb45a
Tre' Seymour <wayne.seymour@elastic.co>
2020-03-03 09:52:05 -0700

    Add the assertions.

--- src/dev/code_coverage/ingest_coverage/integration_tests/ingest_coverage.test.js
+++ src/dev/code_coverage/ingest_coverage/integration_tests/ingest_coverage.test.js
@@ -17,30 +17,35 @@
  * under the License.
  */
 
-
 import expect from '@kbn/expect';
-import { spawn } from "child_process";
-import { resolve } from "path";
+import { spawn } from 'child_process';
+import { resolve } from 'path';
 
 const ROOT_DIR = resolve(__dirname, '../../../../..');
-const MOCKS_DIR = resolve(__dirname, './mocks')
+const MOCKS_DIR = resolve(__dirname, './mocks');
+const regexes = {
+  staticHostIncluded: /https:\/\/kibana-coverage\.elastic\.dev/,
+  jobNameIncluded: /jobs\/elastic\+kibana\+code-coverage/,
+  timeStampIncluded: /\d{4}-\d{2}-\d{2}T\d{2}.*\d{2}.*\d{2}Z/,
+  folderStructureIncluded: /live_cc_app\/coverage_data/,
+  endsInDotHtml: /.html$/,
+};
 
-describe('Ingest Coverage to Cluster', () => {
+describe('Ingesting Coverage to Cluster', () => {
+  const chunks = [];
+
+  function onData (x) {
+    chunks.push(x + '');
+  }
 
   beforeAll(done => {
-    const coverageSummaryPath = resolve(MOCKS_DIR, 'jest-combined/coverage-summary.json');
+    const coverageSummaryPath = resolve(MOCKS_DIR, 'jest-combined/coverage-summary-NO-total.json');
     const args = [
       'scripts/ingest_coverage.js',
-      '--verbose',
+      '--debug',
       '--path',
-      coverageSummaryPath
+      coverageSummaryPath,
     ];
-
-    // BUILD_ID=407
-    // ES_HOST=https://super:changeme@142fea2d3047486e925eb8b223559cae.europe-west1.gcp.cloud.es.io:9243
-    // STATIC_SITE_URL_BASE=https://kibana-coverage.elastic.dev/jobs/elastic+kibana+code-coverage
-    // TIME_STAMP=2020-03-02T21:11:47Z
-    // CI_RUN_URL=https://kibana-ci.elastic.co/job/elastic+kibana+code-coverage/407/
     const create = spawn(process.execPath, args, {
       cwd: ROOT_DIR,
       env: {
@@ -50,20 +55,30 @@ describe('Ingest Coverage to Cluster', () => {
         TIME_STAMP: '2020-03-02T21:11:47Z',
         ES_HOST: 'https://super:changeme@142fea2d3047486e925eb8b223559cae.europe-west1.gcp.cloud.es.io:9243',
         NODE_ENV: 'integration_test',
-        // NODE_ENV: 'prod',
-      }
+      },
     });
-    create.stdout.on('data', function onData(x) {
 
-      console.log(`\n### x: \n\t${x}`);
-
-
-    });
+    create.stdout.on('data', onData);
     create.on('close', done);
   });
 
-
-  it('should blah blah blah', function() {
-    expect(true).to.be(true);
+  it('should result in every posted item having a static site url that meets certain requirements, tested via regex', function() {
+    chunks
+      .filter(x => x.includes('staticSiteUrl'))
+      .map(x => x.split('\n')
+        .reduce(getUrlLine)
+      )
+      .forEach(urlLine => Object.entries(regexes)
+        .forEach(reList =>
+          expect(reList[1].test(urlLine)).to.be(true)
+        )
+      );
   });
 });
+
+function getUrlLine(acc, item) {
+  if (item != '') {
+    return item;
+  }
+  return acc;
+}
