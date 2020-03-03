83d863ce187bb544c8a317739169733db82542a2
Nathan L Smith <smith@nlsmith.com>
2020-03-03 09:33:28 -0600

    more

--- x-pack/legacy/plugins/apm/public/components/app/TransactionDetails/WaterfallWithSummmary/WaterfallContainer/MiniWaterfall/SelectionText.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/TransactionDetails/WaterfallWithSummmary/WaterfallContainer/MiniWaterfall/SelectionText.tsx
@@ -4,9 +4,18 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React from 'react';
+import { EuiLink, EuiText } from '@elastic/eui';
+import lightTheme from '@elastic/eui/dist/eui_theme_light.json';
+import { i18n } from '@kbn/i18n';
+import React, { CSSProperties } from 'react';
+import styled from 'styled-components';
 import { Selection } from './';
-import { EuiText } from '@elastic/eui';
+
+const Text = styled(EuiText)`
+  font-size: ${lightTheme.euiFontSizeXS};
+  margin-bottom: ${lightTheme.spacerSizes.s};
+  text-align: right;
+`;
 
 interface SelectionTextProps {
   selection: Selection;
@@ -17,24 +26,32 @@ export function SelectionText({
   selection,
   resetSelection
 }: SelectionTextProps) {
+  const start = selection[0] && (selection[0] / 100).toFixed(0);
+  const end = selection[1] && (selection[1] / 100).toFixed(0);
+  const style: CSSProperties = {
+    visibility: selection[0] ? 'visible' : 'hidden'
+  };
+
   return (
-    // <div
-    //   style={{
-    //     visibility: selection[0] ? 'visible' : 'hidden',
-    //     // fontSize: '11px',
-    //     // color: 'grey',
-    //     textAlign: 'right',
-    //     marginBottom: 16
-    //   }}
-    // >
-    <EuiText color="subdued">
-      Selected {selection[0] && (selection[0] / 100).toFixed(0)}
-      &thinsp;&ndash;&thinsp;
-      {selection[1] && (selection[1] / 100).toFixed(0)} ms |{' '}
-      <button onClick={resetSelection} style={{ color: 'blue' }}>
-        Reset
-      </button>
-    </EuiText>
-    //    </div>
+    <span style={style}>
+      <Text color="subdued">
+        {i18n.translate(
+          'xpack.apm.transactionDetails.miniWaterfall.selectionText',
+          {
+            defaultMessage: 'Selected {start} – {end} ms',
+            values: { start, end }
+          }
+        )}
+        |{' '}
+        <EuiLink onClick={resetSelection}>
+          {i18n.translate(
+            'xpack.apm.transactionDetails.miniWaterfall.resetButtonText',
+            {
+              defaultMessage: 'Reset'
+            }
+          )}
+        </EuiLink>
+      </Text>
+    </span>
   );
 }
