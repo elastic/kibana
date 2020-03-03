8748bb35a2b0da6c930166ec3f1b769152b6aca7
Liza K <liza.katz@elastic.co>
2020-03-03 16:28:07 +0200

    index file for suggestion component

--- src/plugins/data/public/ui/typeahead/index.ts
+++ src/plugins/data/public/ui/typeahead/index.ts
@@ -17,21 +17,4 @@
  * under the License.
  */
 
-export { SuggestionsComponent } from './typeahead';
-export { IndexPatternSelect } from './index_pattern_select';
-export { FilterBar } from './filter_bar';
-export { QueryStringInput } from './query_string_input/query_string_input';
-export { SearchBar, SearchBarProps, StatefulSearchBarProps } from './search_bar';
-
-// @internal
-export {
-  ShardFailureOpenModalButton,
-  ShardFailureRequest,
-  ShardFailureResponse,
-} from './shard_failure_modal';
-
-// @internal
-export { SavedQueryManagementComponent } from './saved_query_management';
-
-// @internal
-export { SaveQueryForm, SavedQueryMeta } from './saved_query_form';
+export { SuggestionsComponent } from './suggestions_component';
