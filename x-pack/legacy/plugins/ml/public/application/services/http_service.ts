7646aa13128cd32653c2a9f929f3c8b8dac92d3b
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-03-04 16:13:44 +0100

    [ML] use kibana http

--- x-pack/legacy/plugins/ml/public/application/services/http_service.ts
+++ x-pack/legacy/plugins/ml/public/application/services/http_service.ts
@@ -4,68 +4,66 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// service for interacting with the server
+import { Observable } from 'rxjs';
 
-import { fromFetch } from 'rxjs/fetch';
-import { from, Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-
-import { getXSRF } from '../util/dependency_cache';
-
-export interface HttpOptions {
-  url?: string;
-}
+import { getHttp } from '../util/dependency_cache';
 
 function getResultHeaders(headers: HeadersInit): HeadersInit {
   return {
     asSystemRequest: false,
     'Content-Type': 'application/json',
-    'kbn-version': getXSRF(),
     ...headers,
   } as HeadersInit;
 }
 
-export function http(options: any) {
-  return new Promise((resolve, reject) => {
-    if (options && options.url) {
-      let url = '';
-      url = url + (options.url || '');
-      const headers = getResultHeaders(options.headers ?? {});
+interface HttpOptions {
+  url: string;
+  method: string;
+  headers?: any;
+  data?: any;
+}
 
-      const allHeaders =
-        options.headers === undefined ? headers : { ...options.headers, ...headers };
-      const body = options.data === undefined ? null : JSON.stringify(options.data);
+/**
+ * Function for making HTTP requests to Kibana's backend.
+ * Wrapper for Kibana's HttpHandler.
+ */
+export async function http(options: HttpOptions) {
+  if (!options?.url) {
+    throw new Error('URL is missing');
+  }
 
-      const payload: RequestInit = {
-        method: options.method || 'GET',
-        headers: allHeaders,
-        credentials: 'same-origin',
-      };
+  try {
+    let url = '';
+    url = url + (options.url || '');
+    const headers = getResultHeaders(options.headers ?? {});
 
-      if (body !== null) {
-        payload.body = body;
-      }
+    const allHeaders = options.headers === undefined ? headers : { ...options.headers, ...headers };
+    const body = options.data === undefined ? null : JSON.stringify(options.data);
 
-      fetch(url, payload)
-        .then(resp => {
-          resp
-            .json()
-            .then(resp.ok === true ? resolve : reject)
-            .catch(resp.ok === true ? resolve : reject);
-        })
-        .catch(resp => {
-          reject(resp);
-        });
-    } else {
-      reject();
+    const payload: RequestInit = {
+      method: options.method || 'GET',
+      headers: allHeaders,
+      credentials: 'same-origin',
+    };
+
+    if (body !== null) {
+      payload.body = body;
     }
-  });
+
+    return await getHttp().fetch(url, payload);
+  } catch (e) {
+    throw new Error(e);
+  }
 }
 
 interface RequestOptions extends RequestInit {
   body: BodyInit | any;
 }
 
+/**
+ * Function for making HTTP requests to Kibana's backend which returns an Observable
+ * with request cancellation support.
+ */
 export function http$<T>(url: string, options: RequestOptions): Observable<T> {
   const requestInit: RequestInit = {
     ...options,
@@ -75,13 +73,62 @@ export function http$<T>(url: string, options: RequestOptions): Observable<T> {
     headers: getResultHeaders(options.headers ?? {}),
   };
 
-  return fromFetch(url, requestInit).pipe(
-    switchMap(response => {
-      if (response.ok) {
-        return from(response.json() as Promise<T>);
-      } else {
-        throw new Error(String(response.status));
+  return kibanaFromFetch<T>(url, requestInit);
+}
+
+/**
+ * Adjusted rxjs fromFetch to use Kibana's HttpHandler.
+ */
+export function kibanaFromFetch<T>(input: string, init?: RequestInit): Observable<T> {
+  return new Observable<T>(subscriber => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    let outerSignalHandler: () => void;
+    let abortable = true;
+    let unsubscribed = false;
+
+    let perSubscriberInit: RequestInit;
+    if (init) {
+      // If a signal is provided, just have it teardown. It's a cancellation token, basically.
+      if (init.signal) {
+        if (init.signal.aborted) {
+          controller.abort();
+        } else {
+          outerSignalHandler = () => {
+            if (!signal.aborted) {
+              controller.abort();
+            }
+          };
+          init.signal.addEventListener('abort', outerSignalHandler);
+        }
       }
-    })
-  );
+      // init cannot be mutated or reassigned as it's closed over by the
+      // subscriber callback and is shared between subscribers.
+      perSubscriberInit = { ...init, signal };
+    } else {
+      perSubscriberInit = { signal };
+    }
+
+    getHttp()
+      .fetch(input, perSubscriberInit)
+      .then(response => {
+        abortable = false;
+        subscriber.next(response);
+        subscriber.complete();
+      })
+      .catch(err => {
+        abortable = false;
+        if (!unsubscribed) {
+          // Only forward the error if it wasn't an abort.
+          subscriber.error(err);
+        }
+      });
+
+    return () => {
+      unsubscribed = true;
+      if (abortable) {
+        controller.abort();
+      }
+    };
+  });
 }
