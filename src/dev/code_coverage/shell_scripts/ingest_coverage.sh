56f159bdcae2d20751ae43c879fb968e3e41ff62
Tre' Seymour <wayne.seymour@elastic.co>
2020-03-04 10:57:43 -0700

    Prepare to use vault for the es host url, user, and pass.

--- src/dev/code_coverage/shell_scripts/ingest_coverage.sh
+++ src/dev/code_coverage/shell_scripts/ingest_coverage.sh
@@ -12,6 +12,11 @@ export CI_RUN_URL
 echo "### debug CI_RUN_URL: ${CI_RUN_URL}"
 
 ES_HOST=https://super:changeme@142fea2d3047486e925eb8b223559cae.europe-west1.gcp.cloud.es.io:9243
+#PATH_TO_SECRET='path/to/secret'
+#HOST_FROM_VAULT=$(vault read -field=host ${PATH_TO_SECRET})
+#USER_FROM_VAULT=$(vault read -field=user ${PATH_TO_SECRET})
+#PASS_FROM_VAULT=$(vault read -field=pass ${PATH_TO_SECRET})
+#ES_HOST="https://${USER_FROM_VAULT}:${PASS_FROM_VAULT}/${HOST_FROM_VAULT}"
 export ES_HOST
 
 STATIC_SITE_URL_BASE='https://kibana-coverage.elastic.dev/jobs/elastic+kibana+code-coverage'
@@ -20,7 +25,6 @@ export STATIC_SITE_URL_BASE
 for x in jest functional mocha; do
   echo "### Ingesting coverage for ${x}"
 
-#  COVERAGE_SUMMARY_FILE=temp-target/kibana-coverage/mocha/coverage-summary-fake-${x}.json ## FAKE DATA
   COVERAGE_SUMMARY_FILE=target/kibana-coverage/${x}-combined/coverage-summary.json
 
   node scripts/ingest_coverage.js --verbose --path ${COVERAGE_SUMMARY_FILE}
