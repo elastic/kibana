4e710ad47b7636f9a6e95f82d3df5329a8a79256
Christos Nasikas <christos.nasikas@elastic.co>
2020-03-04 20:39:31 +0200

    Create new types

--- x-pack/plugins/actions/server/builtin_action_types/servicenow/types.ts
+++ x-pack/plugins/actions/server/builtin_action_types/servicenow/types.ts
@@ -6,7 +6,14 @@
 
 import { TypeOf } from '@kbn/config-schema';
 
-import { ConfigSchema, SecretsSchema, ParamsSchema } from './schema';
+import {
+  ConfigSchema,
+  SecretsSchema,
+  ParamsSchema,
+  CasesConfigurationSchema,
+  MapsSchema,
+  CommentSchema,
+} from './schema';
 
 // config definition
 export type ConfigType = TypeOf<typeof ConfigSchema>;
@@ -14,4 +21,12 @@ export type ConfigType = TypeOf<typeof ConfigSchema>;
 // secrets definition
 export type SecretsType = TypeOf<typeof SecretsSchema>;
 
-export type ParamsType = TypeOf<typeof ParamsSchema>;
+export type ParamsType = TypeOf<typeof ParamsSchema> & {
+  [key: string]: any;
+};
+
+export type CasesConfigurationType = TypeOf<typeof CasesConfigurationSchema>;
+export type MapsType = TypeOf<typeof MapsSchema>;
+export type CommentType = TypeOf<typeof CommentSchema>;
+
+export type FinalMapping = Map<string, any>;
