8a48ccf20b3e0fd99550062176e77949cce09f4e
Nathan Reese <reese.nathan@gmail.com>
2020-03-03 18:25:48 -0700

    [Maps] source descriptor types (#58791) (#59230)
    * [Maps] source descriptor types
    
    * make SORT_ORDER an enum
    
    * fix type error
    
    * finish defining descriptors for all sources
    
    * fill out layer descriptor
    
    * fix type
    
    * make some properties optional to avoid type explosions
    
    * make type optional
    
    * nest types a bit more so they better match class structor
    
    * in progress work from pairing with Thomas
    
    * one more thing
    
    * add unit test (#35)
    
    * add functions removed from fields typescript converstion
    
    * move joins from VectorTileLayer constructor to VectorLayer constructor, add mock to fix map_selectors test
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Thomas Neirynck <thomas@elastic.co>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Thomas Neirynck <thomas@elastic.co>

--- x-pack/legacy/plugins/maps/public/layers/sources/es_source.d.ts
+++ x-pack/legacy/plugins/maps/public/layers/sources/es_source.d.ts
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export const GRID_RESOLUTION = {
-  COARSE: 'COARSE',
-  FINE: 'FINE',
-  MOST_FINE: 'MOST_FINE',
-};
+import { AbstractVectorSource } from './vector_source';
+
+export class AbstractESSource extends AbstractVectorSource {}
