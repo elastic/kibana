5af92be681269f6a44d5cb6d08e5f7d2a48d622b
Christos Nasikas <christos.nasikas@elastic.co>
2020-03-04 20:40:19 +0200

    Create helpers

---
+++ x-pack/plugins/actions/server/builtin_action_types/servicenow/helpers.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { SUPPORTED_SOURCE_FIELDS } from './constants';
+import { MapsType, FinalMapping, ParamsType } from './types';
+
+export const sanitizeMapping = (fields: string[], mapping: MapsType[]): MapsType[] => {
+  // Prevent prototype pollution and remove unsupported fields
+  return mapping.filter(
+    m => m.source !== '__proto__' && m.target !== '__proto__' && fields.includes(m.source)
+  );
+};
+
+export const buildMap = (mapping: MapsType[]): FinalMapping => {
+  // Maybe redundant as Map is safe against prototype pollution
+  const sanitizedMap = sanitizeMapping(SUPPORTED_SOURCE_FIELDS, mapping);
+  const fieldsMap = new Map();
+
+  for (const field of sanitizedMap) {
+    const { source, target, onEditAndUpdate } = field;
+    fieldsMap.set(source, { target, onEditAndUpdate });
+    fieldsMap.set(target, { target: source, onEditAndUpdate });
+  }
+
+  return fieldsMap;
+};
+
+interface KeyAny {
+  [key: string]: any;
+}
+
+export const mapParams = (params: any, mapping: FinalMapping) => {
+  return Object.keys(params).reduce((prev: KeyAny, curr: string): KeyAny => {
+    const field = mapping.get(curr);
+    if (field) {
+      prev[field.target] = params[curr];
+    }
+    return prev;
+  }, {} as KeyAny);
+};
