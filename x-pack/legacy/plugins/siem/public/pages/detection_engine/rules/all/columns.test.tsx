f6c8e6aa03e1f0259331e0af4039e076695e893a
Patryk Kopyciński <contact@patrykkopycinski.com>
2020-03-04 15:15:23 +0100

    [SIEM] Fix rule delete/duplicate actions

---
+++ x-pack/legacy/plugins/siem/public/pages/detection_engine/rules/all/columns.test.tsx
@@ -0,0 +1,73 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import uuid from 'uuid';
+import { createMemoryHistory } from 'history';
+
+const history = createMemoryHistory();
+
+import { mockRule } from './__mocks__/mock';
+import { getActions } from './columns';
+
+jest.mock('./actions', () => ({
+  __esModule: true, // this property makes it work
+  duplicateRulesAction: jest.fn(),
+  deleteRulesAction: jest.fn(),
+}));
+
+import { duplicateRulesAction, deleteRulesAction } from './actions';
+
+describe('AllRulesTable Columns', () => {
+  describe('getActions', () => {
+    const rule = mockRule(uuid.v4());
+    let actions: ReturnType<typeof getActions>;
+    let results: string[] = [];
+    const dispatch = jest.fn();
+    const dispatchToaster = jest.fn();
+    const reFetchRules = jest.fn();
+
+    beforeEach(() => {
+      results = [];
+
+      reFetchRules.mockReturnValue(
+        new Promise(resolve => {
+          results.push('reFetchRules');
+          resolve();
+        })
+      );
+    });
+
+    test('duplicate rule onClick should call refetch after the rule is duplicated', async () => {
+      (duplicateRulesAction as jest.Mock).mockReturnValue(
+        new Promise(resolve =>
+          setTimeout(() => {
+            results.push('duplicateRulesAction');
+            resolve();
+          }, 500)
+        )
+      );
+
+      actions = getActions(dispatch, dispatchToaster, history, reFetchRules);
+      await actions[1].onClick(rule);
+      expect(results).toEqual(['reFetchRules', 'duplicateRulesAction']);
+    });
+
+    test('delete rule onClick should call refetch after the rule is deleted', async () => {
+      (deleteRulesAction as jest.Mock).mockReturnValue(
+        new Promise(resolve =>
+          setTimeout(() => {
+            results.push('deleteRulesAction');
+            resolve();
+          }, 500)
+        )
+      );
+
+      actions = getActions(dispatch, dispatchToaster, history, reFetchRules);
+      await actions[3].onClick(rule);
+      expect(results).toEqual(['reFetchRules', 'deleteRulesAction']);
+    });
+  });
+});
