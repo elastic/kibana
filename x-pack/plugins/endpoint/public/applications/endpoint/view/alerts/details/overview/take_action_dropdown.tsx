42902123313221e0b9651b34ccc0d301eb1be166
Davis Plumlee <davis.plumlee@elastic.co>
2020-03-04 14:52:33 -0500

    switches to buttons

--- x-pack/plugins/endpoint/public/applications/endpoint/view/alerts/details/overview/take_action_dropdown.tsx
+++ x-pack/plugins/endpoint/public/applications/endpoint/view/alerts/details/overview/take_action_dropdown.tsx
@@ -7,51 +7,48 @@
 import React, { memo, useMemo, useState } from 'react';
 import { EuiPopover, EuiFormRow, EuiButton, EuiButtonEmpty } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
-import styled from 'styled-components';
 
-export const TakeActionDropdown = styled(
-  memo(({ className }: { className?: string }) => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+export const TakeActionDropdown = memo(() => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const TakeActionButton = useMemo(() => {
-      return (
-        <EuiButton
-          iconType="arrowDown"
-          iconSide="right"
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-        >
-          <FormattedMessage
-            id="xpack.endpoint.application.endpoint.alertDetails.takeAction.title"
-            defaultMessage="Take Action"
-          />
-        </EuiButton>
-      );
-    }, [isDropdownOpen]);
-
+  const TakeActionButton = useMemo(() => {
     return (
-      <EuiPopover
-        button={TakeActionButton}
-        isOpen={isDropdownOpen}
-        closePopover={() => setIsDropdownOpen(false)}
+      <EuiButton
+        iconType="arrowDown"
+        iconSide="right"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
       >
-        <EuiFormRow>
-          <EuiButtonEmpty color="text" onClick={() => {}} iconType="folderCheck">
-            <FormattedMessage
-              id="xpack.endpoint.application.endpoint.alertDetails.takeAction.close"
-              defaultMessage="Close Alert"
-            />
-          </EuiButtonEmpty>
-        </EuiFormRow>
-
-        <EuiFormRow>
-          <EuiButtonEmpty color="text" onClick={() => {}} iconType="listAdd">
-            <FormattedMessage
-              id="xpack.endpoint.application.endpoint.alertDetails.takeAction.whitelist"
-              defaultMessage="Whitelist..."
-            />
-          </EuiButtonEmpty>
-        </EuiFormRow>
-      </EuiPopover>
+        <FormattedMessage
+          id="xpack.endpoint.application.endpoint.alertDetails.takeAction.title"
+          defaultMessage="Take Action"
+        />
+      </EuiButton>
     );
-  })
-)``;
+  }, [isDropdownOpen]);
+
+  return (
+    <EuiPopover
+      button={TakeActionButton}
+      isOpen={isDropdownOpen}
+      closePopover={() => setIsDropdownOpen(false)}
+    >
+      <EuiFormRow>
+        <EuiButtonEmpty color="text" onClick={() => {}} iconType="folderCheck">
+          <FormattedMessage
+            id="xpack.endpoint.application.endpoint.alertDetails.takeAction.close"
+            defaultMessage="Close Alert"
+          />
+        </EuiButtonEmpty>
+      </EuiFormRow>
+
+      <EuiFormRow>
+        <EuiButtonEmpty color="text" onClick={() => {}} iconType="listAdd">
+          <FormattedMessage
+            id="xpack.endpoint.application.endpoint.alertDetails.takeAction.whitelist"
+            defaultMessage="Whitelist..."
+          />
+        </EuiButtonEmpty>
+      </EuiFormRow>
+    </EuiPopover>
+  );
+});
