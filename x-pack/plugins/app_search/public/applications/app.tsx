16cfd0f5453b94e3f5a528c771da3eb09752ab73
Constance Chen <constance.chen.3@gmail.com>
2020-03-03 16:43:31 -0800

    Initial App Search in Kibana plugin work
    - Initializes a new platform plugin that ships out of the box w/ x-pack
    - Contains a very basic front-end that shows AS engines, error states, or a Setup Guide
    - Contains a very basic server that remotely calls the AS internal engines API and returns results

---
+++ x-pack/plugins/app_search/public/applications/app.tsx
@@ -0,0 +1,17 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CoreStart, AppMountParams } from 'src/core/public';
+import { ClientConfigType } from '../plugin';
+
+import { Main } from './components/main';
+
+export const renderApp = (core: CoreStart, params: AppMountParams, config: ClientConfigType) => {
+  ReactDOM.render(<Main http={core.http} appSearchUrl={config.host} />, params.element);
+  return () => ReactDOM.unmountComponentAtNode(params.element);
+};
