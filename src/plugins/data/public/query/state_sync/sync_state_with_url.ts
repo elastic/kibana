3dac39af86e1a0748877830a2fe760b73b23376d
sulemanof <sul-dan@mail.ru>
2020-03-05 18:35:50 +0300

    Update state syncing with url

--- src/plugins/data/public/query/state_sync/sync_state_with_url.ts
+++ src/plugins/data/public/query/state_sync/sync_state_with_url.ts
@@ -85,10 +85,10 @@ export const syncQueryStateWithUrl = (
     stateContainer: {
       ...globalQueryStateContainer,
       set: state => {
-        if (typeof state !== 'object' || state === null) {
-          return;
+        if (state) {
+          // syncState utils requires to handle incoming "null" value
+          globalQueryStateContainer.set(state);
         }
-        globalQueryStateContainer.set(state);
       },
     },
     storageKey: GLOBAL_STATE_STORAGE_KEY,
