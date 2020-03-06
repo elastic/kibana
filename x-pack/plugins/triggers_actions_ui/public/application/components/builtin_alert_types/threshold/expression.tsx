0f728d78587d42df6b3c25d52a0b0f4e2a6db6b3
Patrick Mueller <pmuellr@gmail.com>
2020-03-06 12:18:07 -0500

    [Alerting] replace index threshold graph usage of watcher APIs with new API (#59385) (#59557)
    Changes the alerting UI to use the new time series query HTTP endpoint provided by the builtin index threshold alertType; previously it used a watcher HTTP endpoint.
    
    This is part of the ongoing index threshold work tracked in https://github.com/elastic/kibana/issues/53041

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_alert_types/threshold/expression.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_alert_types/threshold/expression.tsx
@@ -62,6 +62,7 @@ const expressionFieldsWithValidation = [
 
 interface IndexThresholdProps {
   alertParams: IndexThresholdAlertParams;
+  alertInterval: string;
   setAlertParams: (property: string, value: any) => void;
   setAlertProperty: (key: string, value: any) => void;
   errors: { [key: string]: string[] };
@@ -70,6 +71,7 @@ interface IndexThresholdProps {
 
 export const IndexThresholdAlertTypeExpression: React.FunctionComponent<IndexThresholdProps> = ({
   alertParams,
+  alertInterval,
   setAlertParams,
   setAlertProperty,
   errors,
@@ -454,6 +456,7 @@ export const IndexThresholdAlertTypeExpression: React.FunctionComponent<IndexThr
         <Fragment>
           <ThresholdVisualization
             alertParams={alertParams}
+            alertInterval={alertInterval}
             aggregationTypes={builtInAggregationTypes}
             comparators={builtInComparators}
             alertsContext={alertsContext}
