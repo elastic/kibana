46d7258a9db260e4f4d33e641fd7aa5c05407eec
Christos Nasikas <christos.nasikas@elastic.co>
2020-03-06 11:56:19 +0200

    Show flyout

--- x-pack/legacy/plugins/siem/public/pages/case/components/configure_cases/connectors.tsx
+++ x-pack/legacy/plugins/siem/public/pages/case/components/configure_cases/connectors.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useMemo } from 'react';
+import React, { useState } from 'react';
 import {
   EuiDescribedFormGroup,
   EuiFormRow,
@@ -26,6 +26,12 @@ const EuiIconExtended = styled(EuiIcon)`
   margin-right: 13px;
 `;
 
+import {
+  ActionsConnectorsContextProvider,
+  ConnectorAddFlyout,
+} from '../../../../../../../../plugins/triggers_actions_ui/public';
+import { useKibana } from '../../../../lib/kibana';
+
 const EuiFormRowExtended = styled(EuiFormRow)`
   .euiFormRow__labelWrapper {
     .euiFormRow__label {
@@ -34,58 +40,44 @@ const EuiFormRowExtended = styled(EuiFormRow)`
   }
 `;
 
-interface Props {
-  connectors: Connector[];
-}
-
-const noConnectorOption = {
-  value: 'no-connector',
-  inputDisplay: (
-    <>
-      <EuiIconExtended type="minusInCircle" size={ICON_SIZE} />
-      <span>{i18n.NO_CONNECTOR}</span>
-    </>
-  ),
-  'data-test-subj': 'no-connector',
-};
-
-const ConnectorsComponent: React.FC<Props> = ({ connectors }) => {
-  let connectorsAsOptions = useMemo(
-    () =>
-      connectors.map(c => ({
-        value: c.id,
-        inputDisplay: (
-          <>
-            <EuiIconExtended type="logoWebhook" size={ICON_SIZE} />
-            <span>{c.name}</span>
-          </>
-        ),
-        'data-test-subj': 'servicenow-connector',
-      })),
-    [connectors]
-  );
-
-  connectorsAsOptions = [noConnectorOption, ...connectorsAsOptions];
+const ConnectorsComponent: React.FC = () => {
+  const { http, triggers_actions_ui, notifications, application } = useKibana().services;
+  const [addFlyoutVisible, setAddFlyoutVisibility] = useState<boolean>(false);
 
   const dropDownLabel = (
     <EuiFlexGroup justifyContent="spaceBetween">
       <EuiFlexItem grow={false}>{i18n.INCIDENT_MANAGEMENT_SYSTEM_LABEL}</EuiFlexItem>
       <EuiFlexItem grow={false}>
-        <EuiLink>{i18n.ADD_NEW_CONNECTOR}</EuiLink>
+        <EuiLink onClick={() => setAddFlyoutVisibility(true)}>{i18n.ADD_NEW_CONNECTOR}</EuiLink>
       </EuiFlexItem>
     </EuiFlexGroup>
   );
 
   return (
-    <EuiDescribedFormGroup
-      fullWidth
-      title={<h3>{i18n.INCIDENT_MANAGEMENT_SYSTEM_TITLE}</h3>}
-      description={i18n.INCIDENT_MANAGEMENT_SYSTEM_DESC}
-    >
-      <EuiFormRowExtended fullWidth label={dropDownLabel}>
-        <ConnectorsDropdown connectors={connectorsAsOptions} />
-      </EuiFormRowExtended>
-    </EuiDescribedFormGroup>
+    <>
+      <EuiDescribedFormGroup
+        fullWidth
+        title={<h3>{i18n.INCIDENT_MANAGEMENT_SYSTEM_TITLE}</h3>}
+        description={i18n.INCIDENT_MANAGEMENT_SYSTEM_DESC}
+      >
+        <EuiFormRowExtended fullWidth label={dropDownLabel}>
+          <ConnectorsDropdown />
+        </EuiFormRowExtended>
+      </EuiDescribedFormGroup>
+      <ActionsConnectorsContextProvider
+        value={{
+          http,
+          actionTypeRegistry: triggers_actions_ui.actionTypeRegistry,
+          toastNotifications: notifications.toasts,
+          capabilities: application.capabilities,
+        }}
+      >
+        <ConnectorAddFlyout
+          addFlyoutVisible={addFlyoutVisible}
+          setAddFlyoutVisibility={setAddFlyoutVisibility}
+        />
+      </ActionsConnectorsContextProvider>
+    </>
   );
 };
 
