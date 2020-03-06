d759ac919ea576bf8dd01948ca936a68b813e9ae
Walter Rafelsberger <walter@elastic.co>
2020-03-06 13:03:11 +0100

    [ML] Fix http routes.

--- x-pack/plugins/transform/public/app/hooks/use_api.ts
+++ x-pack/plugins/transform/public/app/hooks/use_api.ts
@@ -5,6 +5,7 @@
  */
 
 import { TransformId, TransformEndpointRequest, TransformEndpointResult } from '../../../common';
+import { API_BASE_PATH } from '../../../common/constants';
 
 import { useAppDependencies } from '../app_dependencies';
 import { PreviewRequestBody } from '../common';
@@ -14,52 +15,49 @@ import { EsIndex } from './use_api_types';
 export const useApi = () => {
   const { http } = useAppDependencies();
 
-  const basePath = http.basePath.prepend('/api/transform');
-  const indicesBasePath = http.basePath.prepend('/api');
-
   return {
     getTransforms(transformId?: TransformId): Promise<any> {
       const transformIdString = transformId !== undefined ? `/${transformId}` : '';
-      return http.get(`${basePath}/transforms${transformIdString}`);
+      return http.get(`${API_BASE_PATH}transforms${transformIdString}`);
     },
     getTransformsStats(transformId?: TransformId): Promise<any> {
       if (transformId !== undefined) {
-        return http.get(`${basePath}/transforms/${transformId}/_stats`);
+        return http.get(`${API_BASE_PATH}transforms/${transformId}/_stats`);
       }
 
-      return http.get(`${basePath}/transforms/_stats`);
+      return http.get(`${API_BASE_PATH}transforms/_stats`);
     },
     createTransform(transformId: TransformId, transformConfig: any): Promise<any> {
-      return http.put(`${basePath}/transforms/${transformId}`, {
+      return http.put(`${API_BASE_PATH}transforms/${transformId}`, {
         body: JSON.stringify(transformConfig),
       });
     },
     deleteTransforms(transformsInfo: TransformEndpointRequest[]): Promise<TransformEndpointResult> {
-      return http.post(`${basePath}/delete_transforms`, {
+      return http.post(`${API_BASE_PATH}delete_transforms`, {
         body: JSON.stringify(transformsInfo),
       });
     },
     getTransformsPreview(obj: PreviewRequestBody): Promise<any> {
-      return http.post(`${basePath}/transforms/_preview`, { body: JSON.stringify(obj) });
+      return http.post(`${API_BASE_PATH}transforms/_preview`, { body: JSON.stringify(obj) });
     },
     startTransforms(transformsInfo: TransformEndpointRequest[]): Promise<TransformEndpointResult> {
-      return http.post(`${basePath}/start_transforms`, {
+      return http.post(`${API_BASE_PATH}start_transforms`, {
         body: JSON.stringify(transformsInfo),
       });
     },
     stopTransforms(transformsInfo: TransformEndpointRequest[]): Promise<TransformEndpointResult> {
-      return http.post(`${basePath}/stop_transforms`, {
+      return http.post(`${API_BASE_PATH}stop_transforms`, {
         body: JSON.stringify(transformsInfo),
       });
     },
     getTransformAuditMessages(transformId: TransformId): Promise<any> {
-      return http.get(`${basePath}/transforms/${transformId}/messages`);
+      return http.get(`${API_BASE_PATH}transforms/${transformId}/messages`);
     },
     esSearch(payload: any): Promise<any> {
-      return http.post(`${basePath}/es_search`, { body: JSON.stringify(payload) });
+      return http.post(`${API_BASE_PATH}es_search`, { body: JSON.stringify(payload) });
     },
     getIndices(): Promise<EsIndex[]> {
-      return http.get(`${indicesBasePath}/index_management/indices`);
+      return http.get(`/api/index_management/indices`);
     },
   };
 };
