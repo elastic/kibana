9cf6b41b34b493fbee85d0c3cd26a31b848cae85
Chris Queen <chrisqueen10@gmail.com>
2020-03-06 23:01:25 -0500

    The 'filter' parameter passed into the mapFilter function is now checked for nullish-ness via optional chaining.

--- src/plugins/data/public/query/filter_manager/lib/compare_filters.ts
+++ src/plugins/data/public/query/filter_manager/lib/compare_filters.ts
@@ -44,9 +44,9 @@ const mapFilter = (
 ) => {
   const cleaned: FilterMeta = omit(filter, excludedAttributes);
 
-  if (comparators.negate) cleaned.negate = filter.meta && Boolean(filter.meta.negate);
-  if (comparators.disabled) cleaned.disabled = filter.meta && Boolean(filter.meta.disabled);
-  if (comparators.disabled) cleaned.alias = filter.meta?.alias;
+  if (comparators.negate) cleaned.negate = filter?.meta && Boolean(filter?.meta.negate);
+  if (comparators.disabled) cleaned.disabled = filter?.meta && Boolean(filter?.meta.disabled);
+  if (comparators.disabled) cleaned.alias = filter?.meta?.alias;
 
   return cleaned;
 };
