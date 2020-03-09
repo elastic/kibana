86885c45eed7e5f716e459562ad9e8b0aa408474
Anton Dosov <anton.dosov@elastic.co>
2020-03-09 15:43:16 +0100

    fix ConfigurableBaseConfig type

--- src/plugins/ui_actions/public/index.ts
+++ src/plugins/ui_actions/public/index.ts
@@ -35,7 +35,7 @@ export {
   ActionContract as UiActionsActionContract,
 } from './actions';
 export { buildContextMenuForActions, contextMenuSeparatorAction } from './context_menu';
-export { CollectConfigProps, Presentable, Configurable } from './util';
+export { CollectConfigProps, Presentable, Configurable, ConfigurableBaseConfig } from './util';
 export { Trigger, TriggerContext } from './triggers';
 export { TriggerContextMapping, TriggerId, ActionContextMapping, ActionType } from './types';
 export { ActionByType } from './actions';
