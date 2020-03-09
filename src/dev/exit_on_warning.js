e8a1e386c5c1f5872202f013526cd005bff65b27
Thomas Watson <w@tson.dk>
2020-03-09 15:28:02 +0100

    Also run during CI

--- src/dev/exit_on_warning.js
+++ src/dev/exit_on_warning.js
@@ -17,7 +17,11 @@
  * under the License.
  */
 
-require('../src/dev/exit_on_warning');
-require('../src/apm')(process.env.ELASTIC_APM_PROXY_SERVICE_NAME || 'kibana-proxy');
-require('../src/setup_node_env');
-require('../src/cli/cli');
+process.on('warning', function(warn) {
+  console.error('Node.js process-warning detected:');
+  console.error();
+  console.error(warn.stack);
+  console.error();
+  console.error('Terminating process...');
+  process.exit(1);
+});
