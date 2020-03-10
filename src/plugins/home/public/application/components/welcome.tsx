7b95246cddfb9eb3ff15e8ac2904e33cb52505f2
Dave Snider <dave.snider@gmail.com>
2020-03-10 08:56:53 -0700

    logo for alex

--- src/plugins/home/public/application/components/welcome.tsx
+++ src/plugins/home/public/application/components/welcome.tsx
@@ -137,7 +137,7 @@ export class Welcome extends React.Component<Props> {
             <div className="homWelcome__content eui-textCenter">
               <EuiSpacer size="xl" />
               <span className="homWelcome__logo">
-                <EuiIcon type="logoKibana" size="xxl" />
+                <EuiIcon type="logoElastic" size="xxl" />
               </span>
               <EuiTitle size="l" className="homWelcome__title">
                 <h1>
