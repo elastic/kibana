08fa89cb207687ea41b354ff80e52cad36beb182
Paul Tavares <paul.tavares@elastic.co>
2020-03-10 09:02:15 -0400

    Removed stats and added header summary

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/agent_config/details_page/index.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/agent_config/details_page/index.tsx
@@ -3,10 +3,10 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useCallback, useMemo, useState } from 'react';
 import { Redirect, useRouteMatch } from 'react-router-dom';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n/react';
+import { FormattedMessage, FormattedDate } from '@kbn/i18n/react';
 import {
   EuiFlexGroup,
   EuiFlexItem,
@@ -14,47 +14,172 @@ import {
   EuiText,
   EuiSpacer,
   EuiTitle,
-  EuiHealth,
   EuiButton,
   EuiButtonEmpty,
   EuiEmptyPrompt,
-  EuiBadge,
+  EuiI18nNumber,
+  EuiStat,
 } from '@elastic/eui';
+import { Props as EuiTabProps } from '@elastic/eui/src/components/tabs/tab';
 import { useGetOneAgentConfig, useLink } from '../../../hooks';
-import { AGENT_CONFIG_DETAILS_PATH } from '../../../constants';
+import { AGENT_CONFIG_DETAILS_PATH, AGENT_CONFIG_PATH } from '../../../constants';
 import { Datasource } from '../../../types';
 import { Loading } from '../../../components';
-import { ConnectedLink } from '../../fleet/components';
 import { WithHeaderLayout } from '../../../layouts';
-import { AgentConfigDeleteProvider } from '../components';
 import { ConfigRefreshContext, useGetAgentStatus, AgentStatusRefreshContext } from './hooks';
-import { DatasourcesTable, DonutChart, EditConfigFlyout } from './components';
+import { DatasourcesTable, EditConfigFlyout } from './components';
 
 export const AgentConfigDetailsPage: React.FunctionComponent = () => {
   const {
     params: { configId },
+    // path: currentRoutePath,
   } = useRouteMatch<{ configId: string }>();
   const agentConfigRequest = useGetOneAgentConfig(configId);
   const agentConfig = agentConfigRequest.data ? agentConfigRequest.data.item : null;
   const { isLoading, error, sendRequest: refreshAgentConfig } = agentConfigRequest;
-  const [redirectToAgentConfigList, setRedirectToAgentConfigsList] = useState<boolean>(false);
+  const [redirectToAgentConfigList /* , setRedirectToAgentConfigsList*/] = useState<boolean>(false);
   const agentStatusRequest = useGetAgentStatus(configId);
   const {
-    isLoading: agentStatusIsLoading,
-    error: agentStatusError,
+    // isLoading: agentStatusIsLoading,
+    // error: agentStatusError,
     refreshAgentStatus,
   } = agentStatusRequest;
   const agentStatus = agentStatusRequest.data?.results;
 
   const ADD_DATASOURCE_URI = useLink(`${AGENT_CONFIG_DETAILS_PATH}${configId}/add-datasource`);
+  const AGENT_CONFIG_LIST_URI = useLink(AGENT_CONFIG_PATH);
 
   // Flyout states
   const [isEditConfigFlyoutOpen, setIsEditConfigFlyoutOpen] = useState<boolean>(false);
 
-  const refreshData = () => {
+  const refreshData = useCallback(() => {
     refreshAgentConfig();
     refreshAgentStatus();
-  };
+  }, [refreshAgentConfig, refreshAgentStatus]);
+
+  const headerLeftContent = useMemo(
+    () => (
+      <React.Fragment>
+        <EuiFlexGroup justifyContent="spaceBetween">
+          <EuiFlexItem grow={false}>
+            <EuiFlexGroup gutterSize="s" alignItems="center">
+              <EuiFlexItem grow={false}>
+                <EuiButtonEmpty iconType="arrowLeft" href={AGENT_CONFIG_LIST_URI} flush="left">
+                  <FormattedMessage
+                    id="xpack.ingestManager.configDetails.viewAgentListTitle"
+                    defaultMessage="View all agent configurations"
+                  />
+                </EuiButtonEmpty>
+                <EuiTitle size="l">
+                  <h1>
+                    {(agentConfig && agentConfig.name) || (
+                      <FormattedMessage
+                        id="xpack.ingestManager.configDetails.configDetailsTitle"
+                        defaultMessage="Config '{id}'"
+                        values={{
+                          id: configId,
+                        }}
+                      />
+                    )}
+                  </h1>
+                </EuiTitle>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            {agentConfig && agentConfig.description ? (
+              <Fragment>
+                <EuiSpacer size="s" />
+                <EuiText color="subdued">{agentConfig.description}</EuiText>
+              </Fragment>
+            ) : null}
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiSpacer size="l" />
+      </React.Fragment>
+    ),
+    [AGENT_CONFIG_LIST_URI, agentConfig, configId]
+  );
+
+  const headerRightContent = useMemo(
+    () => (
+      <EuiFlexGroup justifyContent={'flexEnd'} direction="row">
+        {[
+          {
+            label: i18n.translate('xpack.ingestManager.configDetails.summary.revision', {
+              defaultMessage: 'Revision',
+            }),
+            content: '999', // FIXME: implement version - see: https://github.com/elastic/kibana/issues/56750
+          },
+          {
+            label: i18n.translate('xpack.ingestManager.configDetails.summary.datasources', {
+              defaultMessage: 'Data sources',
+            }),
+            content: (
+              <EuiI18nNumber
+                value={
+                  (agentConfig && agentConfig.datasources && agentConfig.datasources.length) || 0
+                }
+              />
+            ),
+          },
+          {
+            label: i18n.translate('xpack.ingestManager.configDetails.summary.usedBy', {
+              defaultMessage: 'Used by',
+            }),
+            content: <EuiI18nNumber value={(agentStatus && agentStatus.total) || 0} />,
+          },
+          {
+            label: i18n.translate('xpack.ingestManager.configDetails.summary.lastUpdated', {
+              defaultMessage: 'Last updated on',
+            }),
+            content:
+              (agentConfig && (
+                <FormattedDate
+                  value={agentConfig?.updated_on}
+                  year="numeric"
+                  month="short"
+                  day="2-digit"
+                />
+              )) ||
+              '',
+          },
+        ].map(item => (
+          <EuiFlexItem grow={false}>
+            <EuiStat
+              titleSize="xs"
+              textAlign="right"
+              title={item.content}
+              description={item.label}
+            />
+          </EuiFlexItem>
+        ))}
+      </EuiFlexGroup>
+    ),
+    [agentConfig, agentStatus]
+  );
+
+  const headerTabs = useMemo((): EuiTabProps[] => {
+    return [
+      {
+        id: 'datasources',
+        name: i18n.translate('xpack.ingestManager.configDetails.subTabs.datasouces', {
+          defaultMessage: 'Data sources',
+        }),
+        isSelected: true,
+      },
+      {
+        id: 'yamlFile',
+        name: i18n.translate('xpack.ingestManager.configDetails.subTabs.yamlFile', {
+          defaultMessage: 'YAML File',
+        }),
+      },
+      {
+        id: 'settings',
+        name: i18n.translate('xpack.ingestManager.configDetails.subTabs.settings', {
+          defaultMessage: 'Settings',
+        }),
+      },
+    ];
+  }, []);
 
   if (redirectToAgentConfigList) {
     return <Redirect to="/" />;
@@ -67,6 +192,7 @@ export const AgentConfigDetailsPage: React.FunctionComponent = () => {
   if (error) {
     return (
       <WithHeaderLayout>
+        {/* TODO: use Error component from top-level `components/` */}
         <EuiCallOut
           title={i18n.translate('xpack.ingestManager.configDetails.unexceptedErrorTitle', {
             defaultMessage: 'An error happened while loading the config',
@@ -100,75 +226,9 @@ export const AgentConfigDetailsPage: React.FunctionComponent = () => {
     <ConfigRefreshContext.Provider value={{ refresh: refreshAgentConfig }}>
       <AgentStatusRefreshContext.Provider value={{ refresh: refreshAgentStatus }}>
         <WithHeaderLayout
-          leftColumn={
-            <React.Fragment>
-              <EuiFlexGroup justifyContent="spaceBetween">
-                <EuiFlexItem grow={false}>
-                  <EuiFlexGroup gutterSize="s" alignItems="center">
-                    <EuiFlexItem grow={false}>
-                      <EuiTitle size="l">
-                        <h1>
-                          {agentConfig.name || (
-                            <FormattedMessage
-                              id="xpack.ingestManager.configDetails.configDetailsTitle"
-                              defaultMessage="Config '{id}'"
-                              values={{
-                                id: configId,
-                              }}
-                            />
-                          )}
-                        </h1>
-                      </EuiTitle>
-                    </EuiFlexItem>
-                    {agentConfig.name ? (
-                      <EuiFlexItem grow={false}>
-                        <EuiBadge>{agentConfig.name}</EuiBadge>
-                      </EuiFlexItem>
-                    ) : null}
-                  </EuiFlexGroup>
-                  {agentConfig.description ? (
-                    <Fragment>
-                      <EuiSpacer size="s" />
-                      <EuiText color="subdued">{agentConfig.description}</EuiText>
-                    </Fragment>
-                  ) : null}
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  <EuiFlexGroup gutterSize="s">
-                    <EuiFlexItem grow={false}>
-                      <EuiButton onClick={() => setIsEditConfigFlyoutOpen(true)} iconType="pencil">
-                        <FormattedMessage
-                          id="xpack.ingestManager.configDetails.editConfigButtonLabel"
-                          defaultMessage="Edit config"
-                        />
-                      </EuiButton>
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={false}>
-                      <AgentConfigDeleteProvider>
-                        {deleteConfigsPrompt => (
-                          <EuiButtonEmpty
-                            color="danger"
-                            onClick={() => {
-                              deleteConfigsPrompt([configId], () => {
-                                setRedirectToAgentConfigsList(true);
-                              });
-                            }}
-                            disabled={agentConfig.is_default}
-                          >
-                            <FormattedMessage
-                              id="xpack.ingestManager.configDetails.deleteConfigButtonLabel"
-                              defaultMessage="Delete"
-                            />
-                          </EuiButtonEmpty>
-                        )}
-                      </AgentConfigDeleteProvider>
-                    </EuiFlexItem>
-                  </EuiFlexGroup>
-                </EuiFlexItem>
-              </EuiFlexGroup>
-              <EuiSpacer size="l" />
-            </React.Fragment>
-          }
+          leftColumn={headerLeftContent}
+          rightColumn={headerRightContent}
+          tabs={headerTabs}
         >
           {isEditConfigFlyoutOpen ? (
             <EditConfigFlyout
@@ -179,141 +239,6 @@ export const AgentConfigDetailsPage: React.FunctionComponent = () => {
               agentConfig={agentConfig}
             />
           ) : null}
-          <EuiTitle size="m">
-            <h3>
-              <FormattedMessage
-                id="xpack.ingestManager.configDetails.agentsSummaryTitle"
-                defaultMessage="Enrolled agents"
-              />
-            </h3>
-          </EuiTitle>
-          <EuiSpacer size="l" />
-          {agentStatusIsLoading || !agentStatus ? (
-            <Loading />
-          ) : agentStatusError ? (
-            <FormattedMessage
-              id="xpack.ingestManager.configDetails.agentStatusNotFoundErrorTitle"
-              defaultMessage="Unable to load enrolled agents status"
-            />
-          ) : (
-            <EuiFlexGroup gutterSize="xl">
-              <EuiFlexItem grow={false}>
-                <EuiTitle size="xs">
-                  <h5>
-                    <FormattedMessage
-                      id="xpack.ingestManager.configDetails.agentsTotalTitle"
-                      defaultMessage="Total"
-                    />
-                  </h5>
-                </EuiTitle>
-                <EuiFlexGroup alignItems="center">
-                  <EuiFlexItem grow={false}>
-                    <EuiTitle size="l">
-                      <span>{agentStatus.total}</span>
-                    </EuiTitle>
-                  </EuiFlexItem>
-                  <EuiFlexItem grow={false}>
-                    {/* TODO: Make this link to filtered agents list and change to real agent count */}
-                    <ConnectedLink color="primary" path={`/fleet/agents`}>
-                      <FormattedMessage
-                        id="xpack.ingestManager.configDetails.viewAgentsLinkText"
-                        defaultMessage="View agents"
-                      />
-                    </ConnectedLink>
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiTitle size="xs">
-                  <h5>
-                    <FormattedMessage
-                      id="xpack.ingestManager.configDetails.eventsTitle"
-                      defaultMessage="Events"
-                    />
-                  </h5>
-                </EuiTitle>
-                <EuiFlexGroup alignItems="center">
-                  <EuiFlexItem grow={false}>
-                    <EuiTitle size="l">
-                      <span>{agentStatus.events}</span>
-                    </EuiTitle>
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiTitle size="xs">
-                  <h5>
-                    <FormattedMessage
-                      id="xpack.ingestManager.configDetails.agentsStatusTitle"
-                      defaultMessage="Status"
-                    />
-                  </h5>
-                </EuiTitle>
-                <EuiFlexGroup alignItems="center">
-                  <EuiFlexItem grow={false}>
-                    <DonutChart
-                      height={150}
-                      width={150}
-                      data={
-                        agentStatus.total === 0
-                          ? {
-                              online: 0,
-                              offline: 1,
-                              error: 0,
-                            }
-                          : {
-                              online: agentStatus.online,
-                              offline: agentStatus.offline,
-                              error: agentStatus.error,
-                            }
-                      }
-                    />
-                  </EuiFlexItem>
-                  <EuiFlexItem grow={false}>
-                    <EuiHealth color="success">
-                      <FormattedMessage
-                        id="xpack.ingestManager.configDetails.onlineAgentsCountText"
-                        defaultMessage="{count} online"
-                        values={{
-                          count: agentStatus.online,
-                        }}
-                      />
-                    </EuiHealth>
-                    <EuiSpacer size="s" />
-                    <EuiHealth color="subdued">
-                      <FormattedMessage
-                        id="xpack.ingestManager.configDetails.offlineAgentsCountText"
-                        defaultMessage="{count} offline"
-                        values={{
-                          count: agentStatus.offline,
-                        }}
-                      />
-                    </EuiHealth>
-                    <EuiSpacer size="s" />
-                    <EuiHealth color="danger">
-                      <FormattedMessage
-                        id="xpack.ingestManager.configDetails.errorAgentsCountText"
-                        defaultMessage="{count} error"
-                        values={{
-                          count: agentStatus.error,
-                        }}
-                      />
-                    </EuiHealth>
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          )}
-          <EuiSpacer size="xl" />
-          <EuiTitle size="m">
-            <h3>
-              <FormattedMessage
-                id="xpack.ingestManager.configDetails.datasourcesTableTitle"
-                defaultMessage="Data sources"
-              />
-            </h3>
-          </EuiTitle>
-          <EuiSpacer size="l" />
           <DatasourcesTable
             datasources={agentConfig.datasources as Datasource[]}
             message={
@@ -331,7 +256,7 @@ export const AgentConfigDetailsPage: React.FunctionComponent = () => {
                     <EuiButton fill iconType="plusInCircle" href={ADD_DATASOURCE_URI}>
                       <FormattedMessage
                         id="xpack.ingestManager.configDetails.addDatasourceButtonText"
-                        defaultMessage="Add a data source"
+                        defaultMessage="Create data source"
                       />
                     </EuiButton>
                   }
@@ -340,10 +265,10 @@ export const AgentConfigDetailsPage: React.FunctionComponent = () => {
             }
             search={{
               toolsRight: [
-                <EuiButton fill iconType="plusInCircle" href={ADD_DATASOURCE_URI}>
+                <EuiButton iconType="plusInCircle" href={ADD_DATASOURCE_URI}>
                   <FormattedMessage
                     id="xpack.ingestManager.configDetails.addDatasourceButtonText"
-                    defaultMessage="Add a data source"
+                    defaultMessage="Create data source"
                   />
                 </EuiButton>,
               ],
