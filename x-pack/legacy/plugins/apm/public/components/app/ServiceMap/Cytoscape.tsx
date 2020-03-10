72c287158e934bea5e2e555901288874d621b71d
Oliver Gupte <olivergupte@gmail.com>
2020-03-10 00:07:50 -0700

    - adds primary color highlighting to connected edges of the focused service - fixes z-indexing issues with overlapping edges when hovering on a service node - always centers on the focused service node after layout reflows

--- x-pack/legacy/plugins/apm/public/components/app/ServiceMap/Cytoscape.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/ServiceMap/Cytoscape.tsx
@@ -111,18 +111,23 @@ export function Cytoscape({
   const dataHandler = useCallback<cytoscape.EventHandler>(
     event => {
       if (cy) {
+        cy.edges().removeClass('highlight');
         // Add the "primary" class to the node if its id matches the serviceName.
         if (cy.nodes().length > 0 && serviceName) {
           cy.nodes().removeClass('primary');
           cy.getElementById(serviceName).addClass('primary');
         }
-
         if (event.cy.elements().length > 0) {
           const selectedRoots = selectRoots(event.cy);
           const layout = cy.layout(
             getLayoutOptions(selectedRoots, height, width)
           );
           layout.one('layoutstop', () => {
+            if (serviceName) {
+              const focusedNode = cy.getElementById(serviceName);
+              cy.center(focusedNode);
+              focusedNode.connectedEdges().addClass('highlight');
+            }
             // show elements after layout is applied
             cy.elements().removeClass('invisible');
           });
