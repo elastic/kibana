129bb4e7905d69b974c1806035d5dc2da112547b
Patryk Kopycinski <contact@patrykkopycinski.com>
2020-03-12 00:48:08 +0100

    fix unnecessary rerenders

--- x-pack/legacy/plugins/siem/public/components/flyout/index.tsx
+++ x-pack/legacy/plugins/siem/public/components/flyout/index.tsx
@@ -85,7 +85,13 @@ const DEFAULT_TIMELINE_BY_ID = {};
 const mapStateToProps = (state: State, { timelineId }: OwnProps) => {
   const timelineById: TimelineById =
     timelineSelectors.timelineByIdSelector(state) ?? DEFAULT_TIMELINE_BY_ID;
-  const dataProviders = timelineById[timelineId]?.dataProviders ?? DEFAULT_DATA_PROVIDERS;
+  /*
+    In case timelineById[timelineId]?.dataProviders is an empty array it will cause unnecessary rerender
+    of StatefulTimeline which can be expensive, so to avoid that return DEFAULT_DATA_PROVIDERS
+  */
+  const dataProviders = timelineById[timelineId]?.dataProviders.length
+    ? timelineById[timelineId]?.dataProviders
+    : DEFAULT_DATA_PROVIDERS;
   const show = timelineById[timelineId]?.show ?? false;
   const width = timelineById[timelineId]?.width ?? DEFAULT_TIMELINE_WIDTH;
 
