5e29bf4293cbe1af365798d0b39d5f186e81da87
CJ Cenizal <cj@cenizal.com>
2020-03-10 18:07:35 -0700

    Replace original request.ts with np_ready_request.ts.

--- x-pack/plugins/index_management/public/shared_imports.ts
+++ x-pack/plugins/index_management/public/shared_imports.ts
@@ -13,7 +13,7 @@ export {
   Forms,
   extractQueryParams,
   GlobalFlyout,
-} from '../../../../src/plugins/es_ui_shared/public/';
+} from '../../../../src/plugins/es_ui_shared/public';
 
 export {
   FormSchema,
