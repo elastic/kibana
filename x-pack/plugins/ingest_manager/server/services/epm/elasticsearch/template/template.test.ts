af1664406d514d84f6875596ca1b0aeec8ee46d8
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2020-03-11 14:30:15 +0100

    Adjust tests

--- x-pack/plugins/ingest_manager/server/services/epm/elasticsearch/template/template.test.ts
+++ x-pack/plugins/ingest_manager/server/services/epm/elasticsearch/template/template.test.ts
@@ -7,8 +7,9 @@
 import { readFileSync } from 'fs';
 import { safeLoad } from 'js-yaml';
 import path from 'path';
-import { Field, processFields } from '../../fields/field';
+import { Field } from '../../fields/field';
 import { generateMappings, getTemplate } from './template';
+import { flattenAndPreprocessFields } from './install';
 
 // Add our own serialiser to just do JSON.stringify
 expect.addSnapshotSerializer({
@@ -28,15 +29,38 @@ test('get template', () => {
   expect(template.index_patterns).toStrictEqual([`${templateName}-*`]);
 });
 
-test('tests loading fields.yml', () => {
-  // Load fields.yml file
+test('tests loading base.yml', () => {
   const ymlPath = path.join(__dirname, '../../fields/tests/base.yml');
   const fieldsYML = readFileSync(ymlPath, 'utf-8');
   const fields: Field[] = safeLoad(fieldsYML);
 
-  processFields(fields);
-  const mappings = generateMappings(fields);
+  const flattenedFields = flattenAndPreprocessFields(fields);
+  const mappings = generateMappings(flattenedFields);
   const template = getTemplate('logs', 'foo', mappings);
+
+  expect(template).toMatchSnapshot(path.basename(ymlPath));
+});
+
+test('tests loading coredns.logs.yml', () => {
+  const ymlPath = path.join(__dirname, '../../fields/tests/coredns.logs.yml');
+  const fieldsYML = readFileSync(ymlPath, 'utf-8');
+  const fields: Field[] = safeLoad(fieldsYML);
+
+  const flattenedFields = flattenAndPreprocessFields(fields);
+  const mappings = generateMappings(flattenedFields);
+  const template = getTemplate('logs', 'foo', mappings);
+
+  expect(template).toMatchSnapshot(path.basename(ymlPath));
+});
+
+test('tests loading system.yml', () => {
+  const ymlPath = path.join(__dirname, '../../fields/tests/system.yml');
+  const fieldsYML = readFileSync(ymlPath, 'utf-8');
+  const fields: Field[] = safeLoad(fieldsYML);
+
+  const flattenedFields = flattenAndPreprocessFields(fields);
+  const mappings = generateMappings(flattenedFields);
+  const template = getTemplate('metrics', 'whatsthis', mappings);
 
   expect(template).toMatchSnapshot(path.basename(ymlPath));
 });
