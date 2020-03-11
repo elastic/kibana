42e9f52000c307c37826fe3073e1f8e5ab66b9ef
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-03-11 17:43:17 -0400

    Adding new datastreams file

---
+++ x-pack/plugins/endpoint/server/data_stream.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import { EPM_DATA_STREAM } from '../../ingest_manager/common';
+
+export async function getDataStreamName(packageName: string, version?: string) {
+  return null;
+}
