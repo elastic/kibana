451d1e2305c36118b23fd3b4bc42dcadf3a20d14
Srikanth Suresh <4140019+srikwit@users.noreply.github.com>
2020-03-12 10:07:00 +0300

    Updating href for source-only repository documentation
    The current href for the documentation for:
    "Creates source-only snapshots that take up to 50% less space." is 
    href={documentationLinksService.getRepositoryTypeDocUrl(REPOSITORY_TYPES.source)}
    which resolves to:
      source = 'modules-snapshots.html#_source_only_repository',
    However, the actual documentation is found at:
    snapshots-register-repository.html#snapshots-source-only-repository

--- x-pack/plugins/snapshot_restore/public/application/constants/index.ts
+++ x-pack/plugins/snapshot_restore/public/application/constants/index.ts
@@ -17,7 +17,7 @@ export enum REPOSITORY_DOC_PATHS {
   default = 'modules-snapshots.html',
   fs = 'modules-snapshots.html#_shared_file_system_repository',
   url = 'modules-snapshots.html#_read_only_url_repository',
-  source = 'modules-snapshots.html#_source_only_repository',
+  source = 'snapshots-register-repository.html#snapshots-source-only-repository',
   s3 = 'repository-s3.html',
   hdfs = 'repository-hdfs.html',
   azure = 'repository-azure.html',
