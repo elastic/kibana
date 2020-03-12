e800438f7671791e1be61ebac7d4ae7302c39aba
Tre' Seymour <wayne.seymour@elastic.co>
2020-03-12 13:23:47 -0600

    Use vault for ES HOST info

--- src/dev/code_coverage/shell_scripts/ingest_coverage.sh
+++ src/dev/code_coverage/shell_scripts/ingest_coverage.sh
@@ -11,12 +11,12 @@ CI_RUN_URL=$2
 export CI_RUN_URL
 echo "### debug CI_RUN_URL: ${CI_RUN_URL}"
 
-ES_HOST=https://super:changeme@142fea2d3047486e925eb8b223559cae.europe-west1.gcp.cloud.es.io:9243
-#PATH_TO_SECRET='secret/kibana-issues/prod/coverage/elasticsearch'
-#HOST_FROM_VAULT=$(vault read -field=host ${PATH_TO_SECRET})
-#USER_FROM_VAULT=$(vault read -field=user ${PATH_TO_SECRET})
-#PASS_FROM_VAULT=$(vault read -field=pass ${PATH_TO_SECRET})
-#ES_HOST="https://${USER_FROM_VAULT}:${PASS_FROM_VAULT}/${HOST_FROM_VAULT}"
+#ES_HOST=https://super:changeme@142fea2d3047486e925eb8b223559cae.europe-west1.gcp.cloud.es.io:9243
+PATH_TO_SECRET='secret/kibana-issues/prod/coverage/elasticsearch'
+HOST_FROM_VAULT=$(vault read -field=host ${PATH_TO_SECRET})
+USER_FROM_VAULT=$(vault read -field=username ${PATH_TO_SECRET})
+PASS_FROM_VAULT=$(vault read -field=password ${PATH_TO_SECRET})
+ES_HOST="https://${USER_FROM_VAULT}:${PASS_FROM_VAULT}/${HOST_FROM_VAULT}"
 export ES_HOST
 
 STATIC_SITE_URL_BASE='https://kibana-coverage.elastic.dev'
