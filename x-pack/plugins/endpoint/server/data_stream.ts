e89f6755f4434acfe301dd45e4ecf3e14ca30677
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-03-12 13:06:43 -0400

    Adding datastream get request

--- x-pack/plugins/endpoint/server/data_stream.ts
+++ x-pack/plugins/endpoint/server/data_stream.ts
@@ -3,8 +3,29 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { EPM_DATA_STREAM } from '../../ingest_manager/common';
+import fetch from 'node-fetch';
+import * as url from 'url';
+import Boom from 'boom';
+import { EPM_DATA_STREAM, GetDataStreamResponse } from '../../ingest_manager/common';
 
-export async function getDataStreamName(packageName: string, version?: string) {
-  return null;
+export async function getDataStreamName(
+  packageName: string,
+  datasetPath: string,
+  version?: string
+): Promise<GetDataStreamResponse> {
+  try {
+    const streamURL = new url.URL(packageName, EPM_DATA_STREAM);
+    streamURL.searchParams.append('datasetPath', datasetPath);
+    if (version) {
+      streamURL.searchParams.append('version', version);
+    }
+    const response = await fetch(streamURL.toString());
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Boom(response.statusText, { statusCode: response.status });
+    }
+  } catch (e) {
+    throw Boom.boomify(e);
+  }
 }
