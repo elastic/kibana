b0c70d280b958ac5ec7256938177fb2204601845
Kerry Gallagher <k.gallagher.05@gmail.com>
2020-03-12 12:45:13 +0000

    Add kibana.json file

--- x-pack/plugins/grokdebugger/public/register_feature.ts
+++ x-pack/plugins/grokdebugger/public/register_feature.ts
@@ -6,7 +6,7 @@
 
 import { i18n } from '@kbn/i18n';
 import { npSetup } from 'ui/new_platform';
-import { FeatureCatalogueCategory } from '../../../../../src/plugins/home/public';
+import { FeatureCatalogueCategory } from '../../../../src/plugins/home/public';
 
 const {
   plugins: { home },
--- x-pack/plugins/grokdebugger/server/lib/register_license_checker/register_license_checker.js
+++ x-pack/plugins/grokdebugger/server/lib/register_license_checker/register_license_checker.js
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { mirrorPluginStatus } from '../../../../../server/lib/mirror_plugin_status';
+import { mirrorPluginStatus } from '../../../../../legacy/server/lib/mirror_plugin_status';
 import { checkLicense } from '../check_license';
 import { PLUGIN } from '../../../common/constants';
 
