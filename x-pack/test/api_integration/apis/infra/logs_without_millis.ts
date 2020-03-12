4586cdbcfe99adc88d5472ea8e81dd087664812f
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2020-03-12 11:25:25 +0100

    Restore logs_without_millis test

--- x-pack/test/api_integration/apis/infra/logs_without_millis.ts
+++ x-pack/test/api_integration/apis/infra/logs_without_millis.ts
@@ -17,6 +17,9 @@ import {
   LOG_ENTRIES_SUMMARY_PATH,
   logEntriesSummaryRequestRT,
   logEntriesSummaryResponseRT,
+  LOG_ENTRIES_PATH,
+  logEntriesRequestRT,
+  logEntriesResponseRT,
 } from '../../../../plugins/infra/common/http_api/log_entries';
 
 const COMMON_HEADERS = {
@@ -30,6 +33,10 @@ const LATEST_KEY_WITH_DATA = {
   time: new Date('2019-01-06T23:59:23.000Z').valueOf(),
   tiebreaker: 2,
 };
+const KEY_WITHIN_DATA_RANGE = {
+  time: new Date('2019-01-06T00:00:00.000Z').valueOf(),
+  tiebreaker: 0,
+};
 
 export default function({ getService }: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
@@ -39,33 +46,84 @@ export default function({ getService }: FtrProviderContext) {
     before(() => esArchiver.load('infra/logs_without_epoch_millis'));
     after(() => esArchiver.unload('infra/logs_without_epoch_millis'));
 
-    it('logSummaryBetween should return non-empty buckets', async () => {
-      const startTimestamp = EARLIEST_KEY_WITH_DATA.time;
-      const endTimestamp = LATEST_KEY_WITH_DATA.time + 1; // the interval end is exclusive
-      const bucketSize = Math.ceil((endTimestamp - startTimestamp) / 10);
+    describe('/log_entries/summary', () => {
+      it('returns non-empty buckets', async () => {
+        const startTimestamp = EARLIEST_KEY_WITH_DATA.time;
+        const endTimestamp = LATEST_KEY_WITH_DATA.time + 1; // the interval end is exclusive
+        const bucketSize = Math.ceil((endTimestamp - startTimestamp) / 10);
 
-      const { body } = await supertest
-        .post(LOG_ENTRIES_SUMMARY_PATH)
-        .set(COMMON_HEADERS)
-        .send(
-          logEntriesSummaryRequestRT.encode({
-            sourceId: 'default',
-            startTimestamp,
-            endTimestamp,
-            bucketSize,
-            query: null,
-          })
-        )
-        .expect(200);
+        const { body } = await supertest
+          .post(LOG_ENTRIES_SUMMARY_PATH)
+          .set(COMMON_HEADERS)
+          .send(
+            logEntriesSummaryRequestRT.encode({
+              sourceId: 'default',
+              startTimestamp,
+              endTimestamp,
+              bucketSize,
+              query: null,
+            })
+          )
+          .expect(200);
 
-      const logSummaryResponse = pipe(
-        logEntriesSummaryResponseRT.decode(body),
-        fold(throwErrors(createPlainError), identity)
-      );
+        const logSummaryResponse = pipe(
+          logEntriesSummaryResponseRT.decode(body),
+          fold(throwErrors(createPlainError), identity)
+        );
 
-      expect(
-        logSummaryResponse.data.buckets.filter((bucket: any) => bucket.entriesCount > 0)
-      ).to.have.length(2);
+        expect(
+          logSummaryResponse.data.buckets.filter((bucket: any) => bucket.entriesCount > 0)
+        ).to.have.length(2);
+      });
+    });
+
+    describe('/log_entries/entries', () => {
+      it('returns log entries', async () => {
+        const startTimestamp = EARLIEST_KEY_WITH_DATA.time;
+        const endTimestamp = LATEST_KEY_WITH_DATA.time + 1; // the interval end is exclusive
+
+        const { body } = await supertest
+          .post(LOG_ENTRIES_PATH)
+          .set(COMMON_HEADERS)
+          .send(
+            logEntriesRequestRT.encode({
+              sourceId: 'default',
+              startTimestamp,
+              endTimestamp,
+            })
+          )
+          .expect(200);
+
+        const logEntriesResponse = pipe(
+          logEntriesResponseRT.decode(body),
+          fold(throwErrors(createPlainError), identity)
+        );
+        expect(logEntriesResponse.data.entries).to.have.length(2);
+      });
+
+      it('returns log entries when centering around a point', async () => {
+        const startTimestamp = EARLIEST_KEY_WITH_DATA.time;
+        const endTimestamp = LATEST_KEY_WITH_DATA.time + 1; // the interval end is exclusive
+
+        const { body } = await supertest
+          .post(LOG_ENTRIES_PATH)
+          .set(COMMON_HEADERS)
+          .send(
+            logEntriesRequestRT.encode({
+              sourceId: 'default',
+              startTimestamp,
+              endTimestamp,
+              center: KEY_WITHIN_DATA_RANGE,
+            })
+          )
+          .expect(200);
+
+        const logEntriesResponse = pipe(
+          logEntriesResponseRT.decode(body),
+          fold(throwErrors(createPlainError), identity)
+        );
+        expect(logEntriesResponse.data.entries).to.have.length(2);
+      });
     });
   });
 }
