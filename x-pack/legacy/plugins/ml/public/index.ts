6356d3814456b85e9e89dcfd2c6d2ba30f602202
spalger <spalger@users.noreply.github.com>
2020-03-11 18:55:16 -0700

    remove import that was for testing

--- x-pack/legacy/plugins/ml/public/index.ts
+++ x-pack/legacy/plugins/ml/public/index.ts
@@ -4,9 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import acorn from 'acorn';
-acorn.full();
-
 import { PluginInitializer } from '../../../../../src/core/public';
 import { MlPlugin, Setup, Start } from './plugin';
 
