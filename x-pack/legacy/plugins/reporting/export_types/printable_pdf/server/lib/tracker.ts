0aa13e6f2e2cdf79291371422f0a486661b74aa5
Timothy Sullivan <tsullivan@elastic.co>
2020-03-12 14:46:12 -0700

    fix typescript

--- x-pack/legacy/plugins/reporting/export_types/printable_pdf/server/lib/tracker.ts
+++ x-pack/legacy/plugins/reporting/export_types/printable_pdf/server/lib/tracker.ts
@@ -25,49 +25,53 @@ interface PdfTracker {
 const SPANTYPE_SETUP = 'setup';
 const SPANTYPE_OUTPUT = 'output';
 
+interface ApmSpan {
+  end: () => void;
+}
+
 export function getTracker(): PdfTracker {
   const apmTrans = apm.startTransaction('reporting generate_pdf', 'reporting');
 
-  let apmLayout: Date;
-  let apmScreenshots: any;
-  let apmSetup: any;
-  let apmAddImage: any;
-  let apmCompilePdf: any;
-  let apmGetBuffer: any;
+  let apmLayout: ApmSpan | null = null;
+  let apmScreenshots: ApmSpan | null = null;
+  let apmSetup: ApmSpan | null = null;
+  let apmAddImage: ApmSpan | null = null;
+  let apmCompilePdf: ApmSpan | null = null;
+  let apmGetBuffer: ApmSpan | null = null;
 
   return {
     startLayout() {
-      apmLayout = apmTrans?.startSpan('create_layout', SPANTYPE_SETUP);
+      apmLayout = apmTrans?.startSpan('create_layout', SPANTYPE_SETUP) || null;
     },
     endLayout() {
       if (apmLayout) apmLayout.end();
     },
     startScreenshots() {
-      apmScreenshots = apmTrans?.startSpan('screenshots_pipeline', SPANTYPE_SETUP);
+      apmScreenshots = apmTrans?.startSpan('screenshots_pipeline', SPANTYPE_SETUP) || null;
     },
     endScreenshots() {
       if (apmScreenshots) apmScreenshots.end();
     },
     startSetup() {
-      apmSetup = apmTrans?.startSpan('setup_pdf', SPANTYPE_SETUP);
+      apmSetup = apmTrans?.startSpan('setup_pdf', SPANTYPE_SETUP) || null;
     },
     endSetup() {
       if (apmSetup) apmSetup.end();
     },
     startAddImage() {
-      apmAddImage = apmTrans?.startSpan('add_pdf_image', SPANTYPE_OUTPUT);
+      apmAddImage = apmTrans?.startSpan('add_pdf_image', SPANTYPE_OUTPUT) || null;
     },
     endAddImage() {
       if (apmAddImage) apmAddImage.end();
     },
     startCompile() {
-      apmCompilePdf = apmTrans?.startSpan('compile_pdf', SPANTYPE_OUTPUT);
+      apmCompilePdf = apmTrans?.startSpan('compile_pdf', SPANTYPE_OUTPUT) || null;
     },
     endCompile() {
       if (apmCompilePdf) apmCompilePdf.end();
     },
     startGetBuffer() {
-      apmGetBuffer = apmTrans?.startSpan('get_buffer', SPANTYPE_OUTPUT);
+      apmGetBuffer = apmTrans?.startSpan('get_buffer', SPANTYPE_OUTPUT) || null;
     },
     endGetBuffer() {
       if (apmGetBuffer) apmGetBuffer.end();
