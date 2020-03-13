7d8d0cc77d138616269c23e8ac670bd465039e5e
Jean-Louis Leysens <jloleysens@gmail.com>
2020-03-13 17:42:39 +0100

    Removed unused error wrapping code

--- x-pack/legacy/plugins/cross_cluster_replication/server/np_ready/lib/error_wrappers/index.js
+++ x-pack/legacy/plugins/cross_cluster_replication/server/np_ready/lib/error_wrappers/index.js
@@ -4,6 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { wrapCustomError } from './wrap_custom_error';
 export { wrapEsError } from './wrap_es_error';
-export { wrapUnknownError } from './wrap_unknown_error';
