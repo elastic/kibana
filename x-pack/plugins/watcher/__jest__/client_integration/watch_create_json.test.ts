f75d81ae8768ed794fbf9a099c971091b84f3f17
Jimmy Kuang <jimmy@elastic.co>
2020-03-12 17:27:55 -0700

    Removed duplicate comments

--- x-pack/plugins/watcher/__jest__/client_integration/watch_create_json.test.ts
+++ x-pack/plugins/watcher/__jest__/client_integration/watch_create_json.test.ts
@@ -4,12 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 import '../../../es_ui_shared/console_lang/mocks';
 
 import { act } from 'react-dom/test-utils';
