362c66637fd95e28348b5b94020a7a9f3ab315d3
James Gowdy <jgowdy@elastic.co>
2020-03-13 12:38:21 +0000

    updating paths and commented code

--- x-pack/plugins/ml/shared_imports.ts
+++ x-pack/plugins/ml/shared_imports.ts
@@ -4,9 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { XJsonMode } from '../../es_ui_shared/console_lang/ace/modes/x_json';
+export { XJsonMode } from '../../../src/plugins/es_ui_shared/console_lang/ace/modes/x_json';
 
 export {
   collapseLiteralStrings,
   expandLiteralStrings,
-} from '../../../../src/plugins/es_ui_shared/console_lang/lib';
+} from '../../../src/plugins/es_ui_shared/console_lang/lib';
