28c9afaa60c891ba7e209592ed0474274c6265c1
Oliver Gupte <ogupte@users.noreply.github.com>
2020-03-13 10:15:43 -0700

    [APM] Service maps fix for service name/env filtering & centering (#59726)
    * - adds primary color highlighting to connected edges of the focused service
    - fixes z-indexing issues with overlapping edges when hovering on a service node
    - always centers on the focused service node after layout reflows
    
    * re-centers the already focused service node when the focus button is clicked
    
    * remove environemnt filter when querying for sample trace ids
    
    * - fixes missing query params in the generated hrefs for details and focused service navigation
    - fix layout bug by passing undefined for roots (instead of []) in service maps layout when no roots are found
    
    * Revert "remove environemnt filter when querying for sample trace ids"
    
    This reverts commit d482aa124b1f2c47da01d4386c2b88108bc94275.
    
    * Fixes extra prop from a merge conflict

--- x-pack/legacy/plugins/apm/public/components/app/ServiceMap/Popover/index.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/ServiceMap/Popover/index.tsx
@@ -86,6 +86,14 @@ export function Popover({ focusedServiceName }: PopoverProps) {
     }
   }, [popoverRef, x, y]);
 
+  const centerSelectedNode = useCallback(() => {
+    if (cy) {
+      cy.center(cy.getElementById(selectedNodeServiceName));
+    }
+  }, [cy, selectedNodeServiceName]);
+
+  const isAlreadyFocused = focusedServiceName === selectedNodeServiceName;
+
   return (
     <EuiPopover
       anchorPosition={'upCenter'}
@@ -98,10 +106,9 @@ export function Popover({ focusedServiceName }: PopoverProps) {
       <Contents
         isService={isService}
         label={label}
-        onFocusClick={deselect}
+        onFocusClick={isAlreadyFocused ? centerSelectedNode : deselect}
         selectedNodeData={selectedNodeData}
         selectedNodeServiceName={selectedNodeServiceName}
-        focusedServiceName={focusedServiceName}
       />
     </EuiPopover>
   );
