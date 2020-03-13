1017a4668b4fdcdea0179f439e7ee88924bd528d
spalger <spalger@users.noreply.github.com>
2020-03-13 09:18:45 -0700

    Revert "Using re2 for Timelion regular expressions (#55208) (#60059)"
    This reverts commit 29f1dd54832fe82848d6f4edb0d4eeaea9472ab2.

--- src/dev/build/lib/index.js
+++ src/dev/build/lib/index.js
@@ -33,6 +33,5 @@ export {
   deleteEmptyFolders,
   compress,
 } from './fs';
-export { download } from './download';
 export { scanDelete } from './scan_delete';
 export { scanCopy } from './scan_copy';
