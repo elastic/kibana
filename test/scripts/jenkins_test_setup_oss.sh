24ba54bdf2bb84340e07409c38f1765c5d856bc0
Brian Seeders <brian.seeders@elastic.co>
2020-03-16 17:44:02 -0400

    Skip build stuff

--- test/scripts/jenkins_test_setup_oss.sh
+++ test/scripts/jenkins_test_setup_oss.sh
@@ -2,10 +2,10 @@
 
 source test/scripts/jenkins_test_setup.sh
 
-if [[ -z "$CODE_COVERAGE" ]] ; then
-  installDir="$(realpath $KIBANA_DIR/build/oss/kibana-*-SNAPSHOT-linux-x86_64)"
-  destDir=${installDir}-${CI_PARALLEL_PROCESS_NUMBER}
-  cp -R "$installDir" "$destDir"
+# if [[ -z "$CODE_COVERAGE" ]] ; then
+#   installDir="$(realpath $KIBANA_DIR/build/oss/kibana-*-SNAPSHOT-linux-x86_64)"
+#   destDir=${installDir}-${CI_PARALLEL_PROCESS_NUMBER}
+#   cp -R "$installDir" "$destDir"
 
-  export KIBANA_INSTALL_DIR="$destDir"
-fi
+#   export KIBANA_INSTALL_DIR="$destDir"
+# fi
