elastic/kibana

commit 07a7caaf2f789c0b33907691ee2588872ec9558a

[Logs UI] Add expandable rows with category examples (#54586)
Make the category rows expandable to show actual log messages samples from the categories during the selected time frame.

x-pack/plugins/infra/public/pages/logs/log_entry_rate/sections/anomalies/table.tsx
@@ -4,19 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiBasicTable, EuiButtonIcon } from '@elastic/eui';
+import { EuiBasicTable } from '@elastic/eui';
 import { RIGHT_ALIGNMENT } from '@elastic/eui/lib/services';
 import { i18n } from '@kbn/i18n';
 import React, { useCallback, useMemo, useState } from 'react';
 
+import { euiStyled } from '../../../../../../../observability/public';
 import { TimeRange } from '../../../../../../common/http_api/shared/time_range';
 import {
   formatAnomalyScore,
   getFriendlyNameForPartitionId,
 } from '../../../../../../common/log_analysis';
+import { RowExpansionButton } from '../../../../../components/basic_table';
 import { LogEntryRateResults } from '../../use_log_entry_rate_results';
 import { AnomaliesTableExpandedRow } from './expanded_row';
-import { euiStyled } from '../../../../../../../observability/public';
 
 interface TableItem {
   id: string;
@@ -30,14 +31,6 @@ interface SortingOptions {
     direction: 'asc' | 'desc';
   };
 }
-
-const collapseAriaLabel = i18n.translate('xpack.infra.logs.analysis.anomaliesTableCollapseLabel', {
-  defaultMessage: 'Collapse',
-});
-
-const expandAriaLabel = i18n.translate('xpack.infra.logs.analysis.anomaliesTableExpandLabel', {
-  defaultMessage: 'Expand',
-});
 
 const partitionColumnName = i18n.translate(
   'xpack.infra.logs.analysis.anomaliesTablePartitionColumnName',
@@ -106,29 +99,34 @@ export const AnomaliesTable: React.FunctionComponent<{
     return sorting.sort.direction === 'asc' ? sortedItems : sortedItems.reverse();
   }, [tableItems, sorting]);
 
-  const toggleExpandedItems = useCallback(
+  const expandItem = useCallback(
+    item => {
+      const newItemIdToExpandedRowMap = {
+        ...itemIdToExpandedRowMap,
+        [item.id]: (
+          <AnomaliesTableExpandedRow
+            partitionId={item.partitionId}
+            results={results}
+            topAnomalyScore={item.topAnomalyScore}
+            setTimeRange={setTimeRange}
+            timeRange={timeRange}
+            jobId={jobId}
+          />
+        ),
+      };
+      setItemIdToExpandedRowMap(newItemIdToExpandedRowMap);
+    },
+    [itemIdToExpandedRowMap, jobId, results, setTimeRange, timeRange]
+  );
+
+  const collapseItem = useCallback(
     item => {
       if (itemIdToExpandedRowMap[item.id]) {
         const { [item.id]: toggledItem, ...remainingExpandedRowMap } = itemIdToExpandedRowMap;
         setItemIdToExpandedRowMap(remainingExpandedRowMap);
-      } else {
-        const newItemIdToExpandedRowMap = {
-          ...itemIdToExpandedRowMap,
-          [item.id]: (
-            <AnomaliesTableExpandedRow
-              partitionId={item.partitionId}
-              results={results}
-              topAnomalyScore={item.topAnomalyScore}
-              setTimeRange={setTimeRange}
-              timeRange={timeRange}
-              jobId={jobId}
-            />
-          ),
-        };
-        setItemIdToExpandedRowMap(newItemIdToExpandedRowMap);
       }
     },
-    [itemIdToExpandedRowMap, jobId, results, setTimeRange, timeRange]
+    [itemIdToExpandedRowMap]
   );
 
   const columns = [
@@ -150,10 +148,11 @@ export const AnomaliesTable: React.FunctionComponent<{
       width: '40px',
       isExpander: true,
       render: (item: TableItem) => (
-        <EuiButtonIcon
-          onClick={() => toggleExpandedItems(item)}
-          aria-label={itemIdToExpandedRowMap[item.id] ? collapseAriaLabel : expandAriaLabel}
-          iconType={itemIdToExpandedRowMap[item.id] ? 'arrowUp' : 'arrowDown'}
+        <RowExpansionButton
+          isExpanded={item.id in itemIdToExpandedRowMap}
+          item={item.id}
+          onExpand={expandItem}
+          onCollapse={collapseItem}
         />
       ),
     },

x-pack/plugins/infra/common/http_api/log_analysis/results/log_entry_categories.ts
@@ -72,9 +72,16 @@ export const logEntryCategoryHistogramRT = rt.type({
 
 export type LogEntryCategoryHistogram = rt.TypeOf<typeof logEntryCategoryHistogramRT>;
 
+export const logEntryCategoryDatasetRT = rt.type({
+  name: rt.string,
+  maximumAnomalyScore: rt.number,
+});
+
+export type LogEntryCategoryDataset = rt.TypeOf<typeof logEntryCategoryDatasetRT>;
+
 export const logEntryCategoryRT = rt.type({
   categoryId: rt.number,
-  datasets: rt.array(rt.string),
+  datasets: rt.array(logEntryCategoryDatasetRT),
   histograms: rt.array(logEntryCategoryHistogramRT),
   logEntryCount: rt.number,
   maximumAnomalyScore: rt.number,

x-pack/plugins/infra/common/http_api/log_analysis/results/log_entry_category_examples.ts
@@ -1,0 +1,75 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import * as rt from 'io-ts';
+
+import {
+  badRequestErrorRT,
+  forbiddenErrorRT,
+  timeRangeRT,
+  routeTimingMetadataRT,
+} from '../../shared';
+
+export const LOG_ANALYSIS_GET_LOG_ENTRY_CATEGORY_EXAMPLES_PATH =
+  '/api/infra/log_analysis/results/log_entry_category_examples';
+
+/**
+ * request
+ */
+
+export const getLogEntryCategoryExamplesRequestPayloadRT = rt.type({
+  data: rt.type({
+    // the category to fetch the examples for
+    categoryId: rt.number,
+    // the number of examples to fetch
+    exampleCount: rt.number,
+    // the id of the source configuration
+    sourceId: rt.string,
+    // the time range to fetch the category examples from
+    timeRange: timeRangeRT,
+  }),
+});
+
+export type GetLogEntryCategoryExamplesRequestPayload = rt.TypeOf<
+  typeof getLogEntryCategoryExamplesRequestPayloadRT
+>;
+
+/**
+ * response
+ */
+
+const logEntryCategoryExampleRT = rt.type({
+  dataset: rt.string,
+  message: rt.string,
+  timestamp: rt.number,
+});
+
+export type LogEntryCategoryExample = rt.TypeOf<typeof logEntryCategoryExampleRT>;
+
+export const getLogEntryCategoryExamplesSuccessReponsePayloadRT = rt.intersection([
+  rt.type({
+    data: rt.type({
+      examples: rt.array(logEntryCategoryExampleRT),
+    }),
+  }),
+  rt.partial({
+    timing: routeTimingMetadataRT,
+  }),
+]);
+
+export type GetLogEntryCategoryExamplesSuccessResponsePayload = rt.TypeOf<
+  typeof getLogEntryCategoryExamplesSuccessReponsePayloadRT
+>;
+
+export const getLogEntryCategoryExamplesResponsePayloadRT = rt.union([
+  getLogEntryCategoryExamplesSuccessReponsePayloadRT,
+  badRequestErrorRT,
+  forbiddenErrorRT,
+]);
+
+export type GetLogEntryCategoryExamplesReponsePayload = rt.TypeOf<
+  typeof getLogEntryCategoryExamplesResponsePayloadRT
+>;

x-pack/plugins/infra/common/log_analysis/job_parameters.ts
@@ -28,3 +28,10 @@ export const jobSourceConfigurationRT = rt.type({
 });
 
 export type JobSourceConfiguration = rt.TypeOf<typeof jobSourceConfigurationRT>;
+
+export const jobCustomSettingsRT = rt.partial({
+  job_revision: rt.number,
+  logs_source_config: rt.partial(jobSourceConfigurationRT.props),
+});
+
+export type JobCustomSettings = rt.TypeOf<typeof jobCustomSettingsRT>;

x-pack/plugins/infra/common/log_analysis/log_analysis_results.ts
@@ -44,3 +44,10 @@ export const formatAnomalyScore = (score: number) => {
 export const getFriendlyNameForPartitionId = (partitionId: string) => {
   return partitionId !== '' ? partitionId : 'unknown';
 };
+
+export const compareDatasetsByMaximumAnomalyScore = <
+  Dataset extends { maximumAnomalyScore: number }
+>(
+  firstDataset: Dataset,
+  secondDataset: Dataset
+) => firstDataset.maximumAnomalyScore - secondDataset.maximumAnomalyScore;

x-pack/plugins/infra/public/components/basic_table/index.ts
@@ -4,7 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from './log_entry_categories';
-export * from './log_entry_category_datasets';
-export * from './log_entry_category_examples';
-export * from './log_entry_rate';
+export * from './row_expansion_button';

x-pack/plugins/infra/public/components/basic_table/row_expansion_button.tsx
@@ -1,0 +1,44 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { EuiButtonIcon } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import React, { useCallback } from 'react';
+
+export const RowExpansionButton = <Item extends any>({
+  isExpanded,
+  item,
+  onCollapse,
+  onExpand,
+}: {
+  isExpanded: boolean;
+  item: Item;
+  onCollapse: (item: Item) => void;
+  onExpand: (item: Item) => void;
+}) => {
+  const handleClick = useCallback(() => (isExpanded ? onCollapse(item) : onExpand(item)), [
+    isExpanded,
+    item,
+    onCollapse,
+    onExpand,
+  ]);
+
+  return (
+    <EuiButtonIcon
+      onClick={handleClick}
+      aria-label={isExpanded ? collapseAriaLabel : expandAriaLabel}
+      iconType={isExpanded ? 'arrowUp' : 'arrowDown'}
+    />
+  );
+};
+
+const collapseAriaLabel = i18n.translate('xpack.infra.table.collapseRowLabel', {
+  defaultMessage: 'Collapse',
+});
+
+const expandAriaLabel = i18n.translate('xpack.infra.table.expandRowLabel', {
+  defaultMessage: 'Expand',
+});

x-pack/plugins/infra/public/components/formatted_time.tsx
@@ -17,8 +17,10 @@ const getFormattedTime = (
   return userFormat ? moment(time).format(userFormat) : moment(time).format(fallbackFormat);
 };
 
+export type TimeFormat = 'dateTime' | 'time';
+
 interface UseFormattedTimeOptions {
-  format?: 'dateTime' | 'time';
+  format?: TimeFormat;
   fallbackFormat?: string;
 }
 

commit 6cd888f75fefc6d3ec4405f7b937069e5ea9bf75

[Logs UI] Fix log rate table row expansion (#60096)
This fixes the log rate table row expansion button, which broke in #54586 during a refactoring.

x-pack/plugins/infra/public/pages/logs/log_entry_rate/sections/anomalies/table.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiBasicTable } from '@elastic/eui';
+import { EuiBasicTable, EuiBasicTableColumn } from '@elastic/eui';
 import { RIGHT_ALIGNMENT } from '@elastic/eui/lib/services';
 import { i18n } from '@kbn/i18n';
 import React, { useCallback, useMemo, useState } from 'react';
@@ -20,8 +20,8 @@ import { LogEntryRateResults } from '../../use_log_entry_rate_results';
 import { AnomaliesTableExpandedRow } from './expanded_row';
 
 interface TableItem {
-  id: string;
-  partition: string;
+  partitionName: string;
+  partitionId: string;
   topAnomalyScore: number;
 }
 
@@ -55,11 +55,10 @@ export const AnomaliesTable: React.FunctionComponent<{
   const tableItems: TableItem[] = useMemo(() => {
     return Object.entries(results.partitionBuckets).map(([key, value]) => {
       return {
-        // Note: EUI's table expanded rows won't work with a key of '' in itemIdToExpandedRowMap, so we have to use the friendly name here
-        id: getFriendlyNameForPartitionId(key),
         // The real ID
         partitionId: key,
-        partition: getFriendlyNameForPartitionId(key),
+        // Note: EUI's table expanded rows won't work with a key of '' in itemIdToExpandedRowMap, so we have to use the friendly name here
+        partitionName: getFriendlyNameForPartitionId(key),
         topAnomalyScore: formatAnomalyScore(value.topAnomalyScore),
       };
     });
@@ -91,8 +90,8 @@ export const AnomaliesTable: React.FunctionComponent<{
 
   const sortedTableItems = useMemo(() => {
     let sortedItems: TableItem[] = [];
-    if (sorting.sort.field === 'partition') {
-      sortedItems = tableItems.sort((a, b) => (a.partition > b.partition ? 1 : -1));
+    if (sorting.sort.field === 'partitionName') {
+      sortedItems = tableItems.sort((a, b) => (a.partitionId > b.partitionId ? 1 : -1));
     } else if (sorting.sort.field === 'topAnomalyScore') {
       sortedItems = tableItems.sort((a, b) => a.topAnomalyScore - b.topAnomalyScore);
     }
@@ -100,10 +99,10 @@ export const AnomaliesTable: React.FunctionComponent<{
   }, [tableItems, sorting]);
 
   const expandItem = useCallback(
-    item => {
+    (item: TableItem) => {
       const newItemIdToExpandedRowMap = {
         ...itemIdToExpandedRowMap,
-        [item.id]: (
+        [item.partitionName]: (
           <AnomaliesTableExpandedRow
             partitionId={item.partitionId}
             results={results}
@@ -120,18 +119,21 @@ export const AnomaliesTable: React.FunctionComponent<{
   );
 
   const collapseItem = useCallback(
-    item => {
-      if (itemIdToExpandedRowMap[item.id]) {
-        const { [item.id]: toggledItem, ...remainingExpandedRowMap } = itemIdToExpandedRowMap;
+    (item: TableItem) => {
+      if (itemIdToExpandedRowMap[item.partitionName]) {
+        const {
+          [item.partitionName]: toggledItem,
+          ...remainingExpandedRowMap
+        } = itemIdToExpandedRowMap;
         setItemIdToExpandedRowMap(remainingExpandedRowMap);
       }
     },
     [itemIdToExpandedRowMap]
   );
 
-  const columns = [
+  const columns: Array<EuiBasicTableColumn<TableItem>> = [
     {
-      field: 'partition',
+      field: 'partitionName',
       name: partitionColumnName,
       sortable: true,
       truncateText: true,
@@ -149,8 +151,8 @@ export const AnomaliesTable: React.FunctionComponent<{
       isExpander: true,
       render: (item: TableItem) => (
         <RowExpansionButton
-          isExpanded={item.id in itemIdToExpandedRowMap}
-          item={item.id}
+          isExpanded={item.partitionName in itemIdToExpandedRowMap}
+          item={item}
           onExpand={expandItem}
           onCollapse={collapseItem}
         />
@@ -161,7 +163,7 @@ export const AnomaliesTable: React.FunctionComponent<{
   return (
     <StyledEuiBasicTable
       items={sortedTableItems}
-      itemId="id"
+      itemId="partitionName"
       itemIdToExpandedRowMap={itemIdToExpandedRowMap}
       isExpandable={true}
       hasActions={true}