c41038facdedf350d5514cf2ee5b0991b1d43189
Alison Goryachev <alison.goryachev@elastic.co>
2020-03-16 15:46:17 -0400

    [Remote clusters] Add support for proxy mode (#59221)

--- x-pack/plugins/remote_clusters/public/application/services/validate_address.test.js
+++ x-pack/plugins/remote_clusters/public/application/services/validate_address.test.js
@@ -4,75 +4,75 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { isSeedNodeValid, isSeedNodePortValid } from './validate_seed_node';
+import { isAddressValid, isPortValid } from './validate_address';
 
-describe('Validate seed node', () => {
+describe('Validate address', () => {
   describe('isSeedNodeValid', () => {
     describe('rejects', () => {
       it('adjacent periods', () => {
-        expect(isSeedNodeValid('a..b')).toBe(false);
+        expect(isAddressValid('a..b')).toBe(false);
       });
 
       it('underscores', () => {
-        expect(isSeedNodeValid('____')).toBe(false);
+        expect(isAddressValid('____')).toBe(false);
       });
 
       ['/', '\\', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '=', '+', '?'].forEach(char => {
         it(char, () => {
-          expect(isSeedNodeValid(char)).toBe(false);
+          expect(isAddressValid(char)).toBe(false);
         });
       });
     });
 
     describe('accepts', () => {
       it('uppercase letters', () => {
-        expect(isSeedNodeValid('A.B.C.D')).toBe(true);
+        expect(isAddressValid('A.B.C.D')).toBe(true);
       });
 
       it('lowercase letters', () => {
-        expect(isSeedNodeValid('a')).toBe(true);
+        expect(isAddressValid('a')).toBe(true);
       });
 
       it('numbers', () => {
-        expect(isSeedNodeValid('56546354')).toBe(true);
+        expect(isAddressValid('56546354')).toBe(true);
       });
 
       it('dashes', () => {
-        expect(isSeedNodeValid('----')).toBe(true);
+        expect(isAddressValid('----')).toBe(true);
       });
 
       it('many parts', () => {
-        expect(isSeedNodeValid('abcd.efgh.ijkl.mnop.qrst.uvwx.yz')).toBe(true);
+        expect(isAddressValid('abcd.efgh.ijkl.mnop.qrst.uvwx.yz')).toBe(true);
       });
     });
   });
 
-  describe('isSeedNodePortValid', () => {
+  describe('isPortValid', () => {
     describe('rejects', () => {
       it('missing port', () => {
-        expect(isSeedNodePortValid('abcd')).toBe(false);
+        expect(isPortValid('abcd')).toBe(false);
       });
 
       it('empty port', () => {
-        expect(isSeedNodePortValid('abcd:')).toBe(false);
+        expect(isPortValid('abcd:')).toBe(false);
       });
 
       it('letters', () => {
-        expect(isSeedNodePortValid('ab:cd')).toBe(false);
+        expect(isPortValid('ab:cd')).toBe(false);
       });
 
       it('non-numbers', () => {
-        expect(isSeedNodePortValid('ab:5 0')).toBe(false);
+        expect(isPortValid('ab:5 0')).toBe(false);
       });
 
       it('multiple ports', () => {
-        expect(isSeedNodePortValid('ab:cd:9000')).toBe(false);
+        expect(isPortValid('ab:cd:9000')).toBe(false);
       });
     });
 
     describe('accepts', () => {
       it('a single numeric port, even beyond the standard port range', () => {
-        expect(isSeedNodePortValid('abcd:100000000')).toBe(true);
+        expect(isPortValid('abcd:100000000')).toBe(true);
       });
     });
   });
