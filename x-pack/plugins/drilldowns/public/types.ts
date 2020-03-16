77cc530b74f1a5ec32a6884ae4d48432f86c93db
streamich <streamich@gmail.com>
2020-03-16 11:05:28 +0100

    style: 💄 remove AnyDrilldown type

--- x-pack/plugins/drilldowns/public/types.ts
+++ x-pack/plugins/drilldowns/public/types.ts
@@ -8,11 +8,11 @@ import { AdvancedUiActionsActionFactoryDefinition as ActionFactoryDefinition } f
 
 export interface Drilldown<
   Config extends object = object,
-  FactoryContext extends object = object,
+  CreationContext extends object = object,
   ExecutionContext extends object = object
 >
   extends Pick<
-    ActionFactoryDefinition<Config, FactoryContext, ExecutionContext>,
+    ActionFactoryDefinition<Config, CreationContext, ExecutionContext>,
     'id' | 'createConfig' | 'CollectConfig' | 'isConfigValid' | 'getDisplayName'
   > {
   /**
@@ -27,6 +27,20 @@ export interface Drilldown<
   euiIcon?: string;
 
   /**
+   * Whether this drilldown should be considered for execution given `config`
+   * and `context`. When multiple drilldowns are attached to the same trigger
+   * user is presented with a context menu to pick on drilldown to execute. If
+   * this method returns `true` this trigger will appear in the context menu
+   * list, if `false`, it will not be presented to the user. If `doExecute` is
+   * not implemented, this drilldown will always be show to the user.
+   *
+   * @param config Config object that user configured this drilldown with.
+   * @param context Object that represents context in which the underlying
+   *  `UIAction` of this drilldown is being executed in.
+   */
+  doExecute?(config: Config, context: ExecutionContext): Promise<boolean>;
+
+  /**
    * Implements the "navigation" action when user clicks something in the UI and
    * instance of this drilldown is triggered.
    *
@@ -36,5 +50,3 @@ export interface Drilldown<
    */
   execute(config: Config, context: ExecutionContext): void;
 }
-
-export type AnyDrilldown = Drilldown<any, any, any>;
