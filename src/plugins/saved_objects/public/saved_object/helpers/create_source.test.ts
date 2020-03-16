2c0184520f1ad0b3159a194ccaf0c66f41b24c1d
maryia-lapata <mary.lopato@gmail.com>
2020-03-16 16:20:44 +0300

    Add unit tests for saveWithConfirmation

---
+++ src/plugins/saved_objects/public/saved_object/helpers/create_source.test.ts
@@ -0,0 +1,95 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+jest.mock('./confirm_modal_promise');
+
+import { SavedObjectAttributes, SavedObjectsCreateOptions, OverlayStart } from 'kibana/public';
+import { SavedObjectsClientContract } from '../../../../../core/public';
+import { saveWithConfirmation } from './create_source';
+import { confirmModalPromise } from './confirm_modal_promise';
+import { OVERWRITE_REJECTED } from '../../constants';
+
+describe('saveWithConfirmation', () => {
+  const savedObjectsClient: SavedObjectsClientContract = {} as SavedObjectsClientContract;
+  const overlays: OverlayStart = {} as OverlayStart;
+  const source: SavedObjectAttributes = {} as SavedObjectAttributes;
+  const options: SavedObjectsCreateOptions = {} as SavedObjectsCreateOptions;
+  const savedObject = {
+    getEsType: () => 'test type',
+    title: 'test title',
+    displayName: 'test display name',
+  };
+
+  beforeEach(() => {
+    savedObjectsClient.create = jest.fn();
+  });
+
+  test('should call create of savedObjectsClient', async () => {
+    await saveWithConfirmation(source, savedObject, options, { savedObjectsClient, overlays });
+    expect(savedObjectsClient.create).toHaveBeenCalledWith(
+      savedObject.getEsType(),
+      source,
+      options
+    );
+  });
+
+  test('should call confirmModalPromise when such record exists', async () => {
+    savedObjectsClient.create.mockImplementation((type, src, { overwrite }) =>
+      overwrite ? Promise.resolve() : Promise.reject({ res: { status: 409 } })
+    );
+    confirmModalPromise.mockReturnValue(Promise.resolve());
+
+    await saveWithConfirmation(source, savedObject, options, { savedObjectsClient, overlays });
+    expect(confirmModalPromise).toHaveBeenCalledWith(
+      expect.any(String),
+      expect.any(String),
+      expect.any(String),
+      overlays
+    );
+  });
+
+  test('should call create of savedObjectsClient when overwriting confirmed', async () => {
+    savedObjectsClient.create.mockImplementation((type, src, { overwrite }) => {
+      if (overwrite) {
+        return Promise.resolve();
+      }
+      return Promise.reject({ res: { status: 409 } });
+    });
+    confirmModalPromise.mockReturnValue(Promise.resolve());
+
+    await saveWithConfirmation(source, savedObject, options, { savedObjectsClient, overlays });
+    expect(savedObjectsClient.create).toHaveBeenLastCalledWith(savedObject.getEsType(), source, {
+      overwrite: true,
+      ...options,
+    });
+  });
+
+  test('should reject when overwriting denied', async () => {
+    savedObjectsClient.create.mockReturnValue(Promise.reject({ res: { status: 409 } }));
+    confirmModalPromise.mockReturnValue(Promise.reject());
+
+    expect.assertions(1);
+    await expect(
+      saveWithConfirmation(source, savedObject, options, {
+        savedObjectsClient,
+        overlays,
+      })
+    ).rejects.toThrow(OVERWRITE_REJECTED);
+  });
+});
