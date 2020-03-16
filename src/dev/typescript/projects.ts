cd492203ed3ebca837b85944ffcb2d6215cdc521
Gidi Meir Morris <github@gidi.io>
2020-03-16 13:00:34 +0000

    removed unneccesery ts project

--- src/dev/typescript/projects.ts
+++ src/dev/typescript/projects.ts
@@ -47,9 +47,6 @@ export const PROJECTS = [
   ...glob
     .sync('test/plugin_functional/plugins/*/tsconfig.json', { cwd: REPO_ROOT })
     .map(path => new Project(resolve(REPO_ROOT, path))),
-  ...glob
-    .sync('x-pack/test/functional_with_es_ssl/fixtures/plugins/*/tsconfig.json', { cwd: REPO_ROOT })
-    .map(path => new Project(resolve(REPO_ROOT, path))),
 ];
 
 export function filterProjectsByFlag(projectFlag?: string) {
