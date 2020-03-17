68e8a9764a588a1cd5a6bec00d39d479da211772
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2020-03-17 08:45:38 -0400

    review III

--- x-pack/legacy/plugins/siem/public/components/link_to/helpers.test.ts
+++ x-pack/legacy/plugins/siem/public/components/link_to/helpers.test.ts
@@ -13,9 +13,6 @@ describe('appendSearch', () => {
   test('should return empty string if parameter is undefined', () => {
     expect(appendSearch(undefined)).toEqual('');
   });
-  test('should return empty string if  parameter is null', () => {
-    expect(appendSearch(null)).toEqual('');
-  });
   test('should return parameter if  parameter is defined', () => {
     expect(appendSearch('helloWorld')).toEqual('helloWorld');
   });
