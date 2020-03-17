b10660270a1a4a0c40968bbc29af54ad490a8c4f
Tre' Seymour <wayne.seymour@elastic.co>
2020-03-17 10:56:35 -0600

    Fixup up format and DO NOT print anything vault related.

--- src/dev/code_coverage/shell_scripts/ingest_coverage.sh
+++ src/dev/code_coverage/shell_scripts/ingest_coverage.sh
@@ -12,10 +12,8 @@ export CI_RUN_URL
 echo "### debug CI_RUN_URL: ${CI_RUN_URL}"
 
 #ES_HOST=https://super:changeme@142fea2d3047486e925eb8b223559cae.europe-west1.gcp.cloud.es.io:9243
-ES_HOST="https://${USER_FROM_VAULT}:${PASS_FROM_VAULT}/${HOST_FROM_VAULT}"
+ES_HOST="https://${USER_FROM_VAULT}:${PASS_FROM_VAULT}@${HOST_FROM_VAULT}"
 export ES_HOST
-echo "### ES_HOST: ..."
-echo $ES_HOST
 
 STATIC_SITE_URL_BASE='https://kibana-coverage.elastic.dev'
 export STATIC_SITE_URL_BASE
