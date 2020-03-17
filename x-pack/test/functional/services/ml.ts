baaca890af818344ac3991cec68f307716839424
Robert Oskamp <robert.oskamp@elastic.co>
2020-03-17 17:35:41 +0100

    Add needed service methods

--- x-pack/test/functional/services/ml.ts
+++ x-pack/test/functional/services/ml.ts
@@ -16,6 +16,7 @@ import {
   MachineLearningDataFrameAnalyticsCreationProvider,
   MachineLearningDataFrameAnalyticsTableProvider,
   MachineLearningDataVisualizerProvider,
+  MachineLearningDataVisualizerFileBasedProvider,
   MachineLearningDataVisualizerIndexBasedProvider,
   MachineLearningJobManagementProvider,
   MachineLearningJobSelectionProvider,
@@ -48,6 +49,7 @@ export function MachineLearningProvider(context: FtrProviderContext) {
   );
   const dataFrameAnalyticsTable = MachineLearningDataFrameAnalyticsTableProvider(context);
   const dataVisualizer = MachineLearningDataVisualizerProvider(context);
+  const dataVisualizerFileBased = MachineLearningDataVisualizerFileBasedProvider(context, common);
   const dataVisualizerIndexBased = MachineLearningDataVisualizerIndexBasedProvider(context);
   const jobManagement = MachineLearningJobManagementProvider(context, api);
   const jobSelection = MachineLearningJobSelectionProvider(context);
@@ -75,6 +77,7 @@ export function MachineLearningProvider(context: FtrProviderContext) {
     dataFrameAnalyticsCreation,
     dataFrameAnalyticsTable,
     dataVisualizer,
+    dataVisualizerFileBased,
     dataVisualizerIndexBased,
     jobManagement,
     jobSelection,
