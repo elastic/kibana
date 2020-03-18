510a0734c14beded8568e168480fa683a44ce794
nnamdifrankie <franklin.ejoh@elastic.co>
2020-03-18 19:24:02 -0400

    EMT-248: fix formatting

--- x-pack/plugins/ingest_manager/server/types/rest_spec/agent.ts
+++ x-pack/plugins/ingest_manager/server/types/rest_spec/agent.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { schema} from '@kbn/config-schema';
+import { schema } from '@kbn/config-schema';
 import { AckEventSchema, AgentEventSchema, AgentTypeSchema, NewAgentActionSchema } from '../models';
 
 export const GetAgentsRequestSchema = {
