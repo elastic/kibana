da0876d5ee60e0d6a7092e71fb66408184af87a0
Daniil Suleiman <31325372+sulemanof@users.noreply.github.com>
2020-03-18 10:25:30 +0300

    [NP] Use local helper shortenDottedString for discover (#60271)
    * Move shortenDottedString into kibana_utils
    
    * Move  helper back to data utils
    
    * Use local helper for discover
    
    * Clean up

--- src/legacy/core_plugins/kibana/public/discover/np_ready/helpers/shorten_dotted_string.ts
+++ src/legacy/core_plugins/kibana/public/discover/np_ready/helpers/shorten_dotted_string.ts
@@ -22,10 +22,5 @@ const DOT_PREFIX_RE = /(.).+?\./g;
 /**
  * Convert a dot.notated.string into a short
  * version (d.n.string)
- *
- * @param {string} str - the long string to convert
- * @return {string}
  */
-export function shortenDottedString(input) {
-  return typeof input !== 'string' ? input : input.replace(DOT_PREFIX_RE, '$1.');
-}
+export const shortenDottedString = (input: string) => input.replace(DOT_PREFIX_RE, '$1.');
