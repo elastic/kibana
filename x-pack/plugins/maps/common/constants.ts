49a8d6943e0952e1a68bccd1f67ed598fa4bfa3c
Nathan Reese <reese.nathan@gmail.com>
2020-03-18 14:46:03 -0600

    updated blended layer to use vector style descriptor type

--- x-pack/plugins/maps/common/constants.ts
+++ x-pack/plugins/maps/common/constants.ts
@@ -68,7 +68,7 @@ export enum FIELD_ORIGIN {
   JOIN = 'join',
 }
 
-export const SOURCE_DATA_ID_ORIGIN = 'source';
+export const SOURCE_DATA_ID_ORIGIN = FIELD_ORIGIN.SOURCE;
 export const META_ID_ORIGIN_SUFFIX = 'meta';
 export const SOURCE_META_ID_ORIGIN = `${SOURCE_DATA_ID_ORIGIN}_${META_ID_ORIGIN_SUFFIX}`;
 export const FORMATTERS_ID_ORIGIN_SUFFIX = 'formatters';
