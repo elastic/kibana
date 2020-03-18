b36b8d75d0df8d9ca474bc1e6744be965c00e171
Daniil Suleiman <31325372+sulemanof@users.noreply.github.com>
2020-03-18 18:28:22 +0300

    [NP] Cutover ensureDefaultIndexPattern to kibana_utils (#59895)
    * Cutover ensure_default_index_pattern to kibana_utils
    
    * Fix conflicts
    
    * Proper name for argument
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/plugins/kibana_utils/public/history/ensure_default_index_pattern.tsx
+++ src/plugins/kibana_utils/public/history/ensure_default_index_pattern.tsx
@@ -18,14 +18,13 @@
  */
 
 import { contains } from 'lodash';
-import { IRootScopeService } from 'angular';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { History } from 'history';
 import { i18n } from '@kbn/i18n';
-import { I18nProvider } from '@kbn/i18n/react';
 import { EuiCallOut } from '@elastic/eui';
 import { CoreStart } from 'kibana/public';
 import { DataPublicPluginStart } from 'src/plugins/data/public';
+import { toMountPoint } from '../../../kibana_react/public';
 
 let bannerId: string;
 let timeoutId: NodeJS.Timeout | undefined;
@@ -39,18 +38,17 @@ let timeoutId: NodeJS.Timeout | undefined;
  * resolve to wait for the URL change to happen.
  */
 export async function ensureDefaultIndexPattern(
-  newPlatform: CoreStart,
+  core: CoreStart,
   data: DataPublicPluginStart,
-  $rootScope: IRootScopeService,
-  kbnUrl: any
+  history: History
 ) {
   const patterns = await data.indexPatterns.getIds();
-  let defaultId = newPlatform.uiSettings.get('defaultIndex');
+  let defaultId = core.uiSettings.get('defaultIndex');
   let defined = !!defaultId;
   const exists = contains(patterns, defaultId);
 
   if (defined && !exists) {
-    newPlatform.uiSettings.remove('defaultIndex');
+    core.uiSettings.remove('defaultIndex');
     defaultId = defined = false;
   }
 
@@ -61,10 +59,9 @@ export async function ensureDefaultIndexPattern(
   // If there is any index pattern created, set the first as default
   if (patterns.length >= 1) {
     defaultId = patterns[0];
-    newPlatform.uiSettings.set('defaultIndex', defaultId);
+    core.uiSettings.set('defaultIndex', defaultId);
   } else {
-    const canManageIndexPatterns =
-      newPlatform.application.capabilities.management.kibana.index_patterns;
+    const canManageIndexPatterns = core.application.capabilities.management.kibana.index_patterns;
     const redirectTarget = canManageIndexPatterns ? '/management/kibana/index_pattern' : '/home';
 
     if (timeoutId) {
@@ -73,31 +70,27 @@ export async function ensureDefaultIndexPattern(
 
     // Avoid being hostile to new users who don't have an index pattern setup yet
     // give them a friendly info message instead of a terse error message
-    bannerId = newPlatform.overlays.banners.replace(bannerId, (element: HTMLElement) => {
-      ReactDOM.render(
-        <I18nProvider>
-          <EuiCallOut
-            color="warning"
-            iconType="iInCircle"
-            title={i18n.translate('kibana_legacy.indexPattern.bannerLabel', {
-              defaultMessage:
-                "In order to visualize and explore data in Kibana, you'll need to create an index pattern to retrieve data from Elasticsearch.",
-            })}
-          />
-        </I18nProvider>,
-        element
-      );
-      return () => ReactDOM.unmountComponentAtNode(element);
-    });
+    bannerId = core.overlays.banners.replace(
+      bannerId,
+      toMountPoint(
+        <EuiCallOut
+          color="warning"
+          iconType="iInCircle"
+          title={i18n.translate('kibana_utils.indexPattern.bannerLabel', {
+            defaultMessage:
+              "In order to visualize and explore data in Kibana, you'll need to create an index pattern to retrieve data from Elasticsearch.",
+          })}
+        />
+      )
+    );
 
     // hide the message after the user has had a chance to acknowledge it -- so it doesn't permanently stick around
     timeoutId = setTimeout(() => {
-      newPlatform.overlays.banners.remove(bannerId);
+      core.overlays.banners.remove(bannerId);
       timeoutId = undefined;
     }, 15000);
 
-    kbnUrl.change(redirectTarget);
-    $rootScope.$digest();
+    history.push(redirectTarget);
 
     // return never-resolving promise to stop resolving and wait for the url change
     return new Promise(() => {});
