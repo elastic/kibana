56be263365cdbf248984ca1d1640a92e19ec7031
Alexey Antonov <alexwizp@gmail.com>
2020-03-18 18:06:59 +0300

    [Visualize] Duplicated query filters in es request (#60106)
    * [Visualize] Duplicated query filters in es request
    
    Closes: #59630
    
    * Fix CI
    
    * fix CI
    
    * move uniq_filters to common
    
    * fix scripts/check_published_api_changes
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/plugins/data/public/query/state_sync/connect_to_query_state.ts
+++ src/plugins/data/public/query/state_sync/connect_to_query_state.ts
@@ -21,10 +21,9 @@ import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import _ from 'lodash';
 import { BaseStateContainer } from '../../../../kibana_utils/public';
-import { COMPARE_ALL_OPTIONS, compareFilters } from '../filter_manager/lib/compare_filters';
 import { QuerySetup, QueryStart } from '../query_service';
 import { QueryState, QueryStateChange } from './types';
-import { FilterStateStore } from '../../../common/es_query/filters';
+import { FilterStateStore, COMPARE_ALL_OPTIONS, compareFilters } from '../../../common';
 
 /**
  * Helper to setup two-way syncing of global data and a state container
