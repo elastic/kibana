36a75703079b3aeecceebc668f20d04020fe3672
stephmilovic <stephanie.milovic@elastic.co>
2020-03-18 13:32:03 -0600

    add state one more place

--- x-pack/legacy/plugins/siem/public/utils/route/spy_routes.tsx
+++ x-pack/legacy/plugins/siem/public/utils/route/spy_routes.tsx
@@ -39,12 +39,13 @@ export const SpyRouteComponent = memo<SpyRouteProps & { location: H.Location }>(
           dispatch({
             type: 'updateRouteWithOutSearch',
             route: {
-              pageName,
               detailName,
-              tabName,
-              pathName: pathname,
-              history,
               flowTarget,
+              history,
+              pageName,
+              pathName: pathname,
+              state,
+              tabName,
             },
           });
           setIsInitializing(false);
@@ -52,14 +53,14 @@ export const SpyRouteComponent = memo<SpyRouteProps & { location: H.Location }>(
           dispatch({
             type: 'updateRoute',
             route: {
-              pageName,
               detailName,
-              tabName,
+              flowTarget,
+              history,
+              pageName,
+              pathName: pathname,
               search,
               state,
-              pathName: pathname,
-              history,
-              flowTarget,
+              tabName,
             },
           });
         }
@@ -68,14 +69,14 @@ export const SpyRouteComponent = memo<SpyRouteProps & { location: H.Location }>(
           dispatch({
             type: 'updateRoute',
             route: {
-              pageName,
               detailName,
-              tabName,
-              search,
-              pathName: pathname,
-              history,
               flowTarget,
+              history,
+              pageName,
+              pathName: pathname,
+              search,
               state,
+              tabName,
             },
           });
         }
