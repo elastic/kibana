32edf741012d5594200d2b60342d529ad09fc8ae
Robert Oskamp <robert.oskamp@elastic.co>
2020-03-19 10:53:32 +0100

    [ML] Add functional tests for file data visualizer (#60413) (#60600)
    This PR adds basic functional tests for the file data visualizer, covering a file import and error messages for non-log files. It also moves the file input path handling to a common location in order to avoid code duplication.

--- test/functional/page_objects/common_page.ts
+++ test/functional/page_objects/common_page.ts
@@ -508,6 +508,12 @@ export function CommonPageProvider({ getService, getPageObjects }: FtrProviderCo
         }
       });
     }
+
+    async setFileInputPath(path: string) {
+      log.debug(`Setting the path '${path}' on the file input`);
+      const input = await find.byCssSelector('.euiFilePicker__input');
+      await input.type(path);
+    }
   }
 
   return new CommonPage();
