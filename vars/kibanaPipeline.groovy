b5647e2189a8e168e86a9403f00083db861d46cf
Gloria Hornero <snootchie.boochies@gmail.com>
2020-03-19 16:22:29 +0100

    uploads only the .png files

--- vars/kibanaPipeline.groovy
+++ vars/kibanaPipeline.groovy
@@ -97,7 +97,7 @@ def uploadCoverageArtifacts(prefix, pattern) {
 def withGcsArtifactUpload(workerName, closure) {
   def uploadPrefix = "kibana-ci-artifacts/jobs/${env.JOB_NAME}/${BUILD_NUMBER}/${workerName}"
   def ARTIFACT_PATTERNS = [
-    'target/kibana-*/**/*',
+    'target/kibana-*/**/*.png',
     'target/junit/**/*',
     'test/**/screenshots/**/*.png',
     'test/functional/failure_debug/html/*.html',
