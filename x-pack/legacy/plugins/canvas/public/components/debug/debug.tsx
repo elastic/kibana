eca30ec7c656581cc1d2781935a43bc4e9bb457f
Tim Schnell <timductive@gmail.com>
2020-03-20 16:48:20 -0500

    updating function and typescripting

--- x-pack/legacy/plugins/canvas/public/components/debug/debug.tsx
+++ x-pack/legacy/plugins/canvas/public/components/debug/debug.tsx
@@ -8,15 +8,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { EuiCode } from '@elastic/eui';
 
-function LimitRows(key, value) {
-  // Filtering out properties
+const LimitRows = (key: string, value: any) => {
   if (key === 'rows') {
     return value.slice(0, 99);
   }
   return value;
-}
+};
 
-export const Debug = ({ payload }) => (
+export const Debug = ({ payload }: any) => (
   <EuiCode className="canvasDebug">
     <pre className="canvasDebug__content">{JSON.stringify(payload, LimitRows, 2)}</pre>
   </EuiCode>
