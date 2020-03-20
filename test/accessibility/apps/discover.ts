de76b51805f8fbc7a0ab015fbe5d8c7810510978
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-03-20 09:47:24 +0100

    Unskip a11y test

--- test/accessibility/apps/discover.ts
+++ test/accessibility/apps/discover.ts
@@ -121,7 +121,7 @@ export default function({ getService, getPageObjects }: FtrProviderContext) {
       await a11y.testAppSnapshot();
     });
 
-    it.skip('Add more fields from sidebar', async () => {
+    it('Add more fields from sidebar', async () => {
       for (const [columnName, value] of TEST_FILTER_COLUMN_NAMES) {
         await PageObjects.discover.clickFieldListItem(columnName);
         await PageObjects.discover.clickFieldListPlusFilter(columnName, value);
