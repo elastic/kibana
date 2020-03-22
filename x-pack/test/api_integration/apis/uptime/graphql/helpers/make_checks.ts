2d5d07023c8f5f1e809fbf87a4e5e15f2b36df27
Andrew Cholakian <andrew@andrewvc.com>
2020-03-22 13:04:28 -0500

    Fix tests to use heartbeat-7 instead of heartbeat-8

--- x-pack/test/api_integration/apis/uptime/graphql/helpers/make_checks.ts
+++ x-pack/test/api_integration/apis/uptime/graphql/helpers/make_checks.ts
@@ -7,7 +7,7 @@
 import uuid from 'uuid';
 import { merge, flattenDeep } from 'lodash';
 
-const INDEX_NAME = 'heartbeat-8-generated-test';
+const INDEX_NAME = 'heartbeat-7-generated-test';
 
 export const makePing = async (
   es: any,
