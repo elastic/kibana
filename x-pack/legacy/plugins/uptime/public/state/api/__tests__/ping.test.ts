0370e861756ed3e07d7c417752958d42c6c89bff
Justin Kambic <justin.kambic@elastic.co>
2020-03-23 17:08:47 -0400

    Add test for new helper function.

---
+++ x-pack/legacy/plugins/uptime/public/state/api/__tests__/ping.test.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { mergeParams } from '../ping';
+
+describe('mergeParams', () => {
+  describe('mergeParams', () => {
+    it('returns the guaranteed params if there are no falsy params', () => {
+      expect(mergeParams({ foo: 'bar' }, {})).toMatchInlineSnapshot();
+    });
+
+    it('adds truthy params while ignoring falsy params', () => {
+      expect(
+        mergeParams({ young: 'star' }, { h: 1, he: 2, li: undefined })
+      ).toMatchInlineSnapshot();
+    });
+  });
+});
