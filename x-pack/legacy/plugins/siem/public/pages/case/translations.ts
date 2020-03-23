21fd8887d4e6988668f0f4479760f3c1c7b5d20f
Christos Nasikas <christos.nasikas@elastic.co>
2020-03-23 21:14:28 +0200

    Change title

--- x-pack/legacy/plugins/siem/public/pages/case/translations.ts
+++ x-pack/legacy/plugins/siem/public/pages/case/translations.ts
@@ -130,7 +130,7 @@ export const CONFIGURE_CASES_PAGE_TITLE = i18n.translate(
 );
 
 export const CONFIGURE_CASES_BUTTON = i18n.translate('xpack.siem.case.configureCasesButton', {
-  defaultMessage: 'Edit third-party connection',
+  defaultMessage: 'Edit external connection',
 });
 
 export const ADD_COMMENT = i18n.translate('xpack.siem.case.caseView.comment.addComment', {
