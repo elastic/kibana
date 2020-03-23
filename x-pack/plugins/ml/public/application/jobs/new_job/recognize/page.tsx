0e3c9e1f384208ffea6ff9a18b3c52faf3877ed7
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-03-23 13:11:38 +0100

    [ML] disable estimateModelMemory

--- x-pack/plugins/ml/public/application/jobs/new_job/recognize/page.tsx
+++ x-pack/plugins/ml/public/application/jobs/new_job/recognize/page.tsx
@@ -172,7 +172,7 @@ export const Page: FC<PageProps> = ({ moduleId, existingGroupIds }) => {
         startDatafeed: startDatafeedAfterSave,
         ...(jobOverridesPayload !== null ? { jobOverrides: jobOverridesPayload } : {}),
         ...resultTimeRange,
-        estimateModelMemory: true,
+        estimateModelMemory: false,
       });
       const { datafeeds: datafeedsResponse, jobs: jobsResponse, kibana: kibanaResponse } = response;
 
