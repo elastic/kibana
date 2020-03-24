5ea89d20a4a9077e97e296433614e9a63894af23
patrykkopycinski <patryk.kopycinski@elastic.co>
2020-03-25 00:49:10 +0100

    [SIEM] Add rule notifications (#59004)
    ## Summary
    
    Allow defining notifications that will trigger whenever the rule created new signals.
    
    Requires:
    - https://github.com/elastic/kibana/pull/58395
    - https://github.com/elastic/kibana/pull/58964
    - https://github.com/elastic/kibana/pull/60832
    
    
    ![Screenshot 2020-03-02 at 10 19 18](https://user-images.githubusercontent.com/5188868/75662390-4fe8bf00-5c6f-11ea-943f-591367348b91.png)
    
    ![Screenshot 2020-03-02 at 10 13 00](https://user-images.githubusercontent.com/5188868/75662421-5e36db00-5c6f-11ea-9317-d158cddf4344.png)
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [ ] Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/master/packages/kbn-i18n/README.md)
    - [ ] [Documentation](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#writing-documentation) was added for features that require explanation or tutorials
    - [ ] [Unit or functional tests](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility) were updated or added to match the most common scenarios
    - [ ] This was checked for [keyboard-only and screenreader accessibility](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Cross_browser_testing/Accessibility#Accessibility_testing_checklist)
    - [ ] This renders correctly on smaller devices using a responsive layout. (You can test this [in your browser](https://www.browserstack.com/guide/responsive-testing-on-local-server)
    - [ ] This was checked for cross-browser compatibility, [including a check against IE11](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility)
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled appropriately](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#release-notes-process)

--- x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/__mocks__/request_responses.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/routes/__mocks__/request_responses.ts
@@ -335,7 +335,7 @@ export const createRuleWithActionsRequest = () => {
         {
           group: 'default',
           id: '99403909-ca9b-49ba-9d7a-7e5320e68d05',
-          params: { message: 'Rule generated {{state.signalsCount}} signals' },
+          params: { message: 'Rule generated {{state.signals_count}} signals' },
           action_type_id: '.slack',
         },
       ],
@@ -668,7 +668,8 @@ export const getNotificationResult = (): RuleNotificationAlertType => ({
     {
       actionTypeId: '.slack',
       params: {
-        message: 'Rule generated {{state.signalsCount}} signals\n\n{{rule.name}}\n{{resultsLink}}',
+        message:
+          'Rule generated {{state.signals_count}} signals\n\n{{context.rule.name}}\n{{{context.results_link}}}',
       },
       group: 'default',
       id: '99403909-ca9b-49ba-9d7a-7e5320e68d05',
