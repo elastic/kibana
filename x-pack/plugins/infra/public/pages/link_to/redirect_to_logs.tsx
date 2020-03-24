0d8ae36796930f420d58d6513d61d26f76bfb09b
Zacqary Xeper <zacqary.xeper@elastic.co>
2020-03-24 16:12:57 -0500

    Import fix

--- x-pack/plugins/infra/public/pages/link_to/redirect_to_logs.tsx
+++ x-pack/plugins/infra/public/pages/link_to/redirect_to_logs.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import compose from 'lodash/fp/compose';
+import { compose } from 'lodash';
 import React from 'react';
 import { match as RouteMatch, Redirect, RouteComponentProps } from 'react-router-dom';
 
