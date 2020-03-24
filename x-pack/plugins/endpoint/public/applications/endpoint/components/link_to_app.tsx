5c293a274a55fc8c8002d4e3c3a9b53354c3c517
Paul Tavares <paul.tavares@elastic.co>
2020-03-24 09:59:35 -0400

    added exported LinkToAppProps type

--- x-pack/plugins/endpoint/public/applications/endpoint/components/link_to_app.tsx
+++ x-pack/plugins/endpoint/public/applications/endpoint/components/link_to_app.tsx
@@ -9,25 +9,27 @@ import { EuiLink } from '@elastic/eui';
 import { EuiLinkProps } from '@elastic/eui';
 import { useNavigateToAppEventHandler } from '../hooks/use_navigate_to_app_event_handler';
 
+export type LinkToAppProps = EuiLinkProps & {
+  /** the app id - normally the value of the `id` in that plugin's `kibana.json`  */
+  appId: string;
+  /** Any app specic path (route) */
+  appPath?: string;
+  appState?: any;
+  onClick?: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+};
+
 /**
  * An `EuiLink` that will use Kibana's `.application.navigateToApp()` to redirect the user to the
  * a given app without causing a full browser refresh
  */
-export const LinkToApp = memo<
-  EuiLinkProps & {
-    /** the app id - normally the value of the `id` in that plugin's `kibana.json`  */
-    appId: string;
-    /** Any app specic path (route) */
-    appPath?: string;
-    appState?: any;
-    onClick?: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+export const LinkToApp = memo<LinkToAppProps>(
+  ({ appId, appPath: path, appState: state, onClick, children, ...otherProps }) => {
+    const handleOnClick = useNavigateToAppEventHandler(appId, { path, state, onClick });
+    return (
+      // eslint-disable-next-line @elastic/eui/href-or-on-click
+      <EuiLink {...otherProps} onClick={handleOnClick}>
+        {children}
+      </EuiLink>
+    );
   }
->(({ appId, appPath: path, appState: state, onClick, children, ...otherProps }) => {
-  const handleOnClick = useNavigateToAppEventHandler(appId, { path, state, onClick });
-  return (
-    // eslint-disable-next-line @elastic/eui/href-or-on-click
-    <EuiLink {...otherProps} onClick={handleOnClick}>
-      {children}
-    </EuiLink>
-  );
-});
+);
