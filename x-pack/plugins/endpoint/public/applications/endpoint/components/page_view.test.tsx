a356c20da8b0fcce830247658d0f416180a112ed
Paul Tavares <paul.tavares@elastic.co>
2020-03-24 16:14:51 -0400

    Updated PageView tests to use wrappingComponent

--- x-pack/plugins/endpoint/public/applications/endpoint/components/page_view.test.tsx
+++ x-pack/plugins/endpoint/public/applications/endpoint/components/page_view.test.tsx
@@ -10,84 +10,53 @@ import { PageView } from './page_view';
 import { EuiThemeProvider } from '../../../../../../legacy/common/eui_styled_components';
 
 describe('PageView component', () => {
+  const render = (ui: Parameters<typeof mount>[0]) =>
+    mount(ui, { wrappingComponent: EuiThemeProvider });
+
   it('should display only body if not header props used', () => {
-    expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView>body content</PageView>
-        </EuiThemeProvider>
-      )
-    ).toMatchSnapshot();
+    expect(render(<PageView>body content</PageView>)).toMatchSnapshot();
   });
   it('should display header left and right', () => {
     expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView headerLeft="page title" headerRight="right side actions">
-            body content
-          </PageView>
-        </EuiThemeProvider>
+      render(
+        <PageView headerLeft="page title" headerRight="right side actions">
+          body content
+        </PageView>
       )
     ).toMatchSnapshot();
   });
   it('should display only header left', () => {
-    expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView headerLeft="page title">body content</PageView>
-        </EuiThemeProvider>
-      )
-    ).toMatchSnapshot();
+    expect(render(<PageView headerLeft="page title">body content</PageView>)).toMatchSnapshot();
   });
   it('should display only header right but include an empty left side', () => {
     expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView headerRight="right side actions">body content</PageView>
-        </EuiThemeProvider>
-      )
+      render(<PageView headerRight="right side actions">body content</PageView>)
     ).toMatchSnapshot();
   });
   it(`should use custom element for header left and not wrap in EuiTitle`, () => {
     expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView headerLeft={<p>title here</p>}>body content</PageView>
-        </EuiThemeProvider>
-      )
+      render(<PageView headerLeft={<p>title here</p>}>body content</PageView>)
     ).toMatchSnapshot();
   });
   it('should display body header wrapped in EuiTitle', () => {
-    expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView bodyHeader="body header">body content</PageView>
-        </EuiThemeProvider>
-      )
-    ).toMatchSnapshot();
+    expect(render(<PageView bodyHeader="body header">body content</PageView>)).toMatchSnapshot();
   });
   it('should display body header custom element', () => {
     expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView bodyHeader={<p>body header</p>}>body content</PageView>
-        </EuiThemeProvider>
-      )
+      render(<PageView bodyHeader={<p>body header</p>}>body content</PageView>)
     ).toMatchSnapshot();
   });
   it('should pass through EuiPage props', () => {
     expect(
-      mount(
-        <EuiThemeProvider>
-          <PageView
-            restrictWidth="1000"
-            className="test-class-name-here"
-            aria-label="test-aria-label-here"
-            data-test-subj="test-data-test-subj-here"
-          >
-            body content
-          </PageView>
-        </EuiThemeProvider>
+      render(
+        <PageView
+          restrictWidth="1000"
+          className="test-class-name-here"
+          aria-label="test-aria-label-here"
+          data-test-subj="test-data-test-subj-here"
+        >
+          body content
+        </PageView>
       )
     ).toMatchSnapshot();
   });
