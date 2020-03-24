b0c0cb74111b3cc69b53bb81a28320ef99369c9f
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-24 17:26:53 +0000

    use relative path

--- x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
+++ x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
@@ -2,7 +2,6 @@
 set -e
 
 E2E_DIR="x-pack/legacy/plugins/apm/e2e"
-CYPRESS_DIR="${E2E_DIR}/cypress"
 
 echo "1/3 Install dependencies ..."
 # shellcheck disable=SC1091
@@ -12,8 +11,8 @@ yarn kbn clean && yarn kbn bootstrap
 echo "2/3 Ingest test data ..."
 pushd ${E2E_DIR}
 yarn || true
-curl --silent https://storage.googleapis.com/apm-ui-e2e-static-data/events.json --output ${CYPRESS_DIR}/ingest-data/events.json
-node ${CYPRESS_DIR}/ingest-data/replay.js --server-url http://localhost:8200 --secret-token abcd --events ./events.json > ingest-data.log
+curl --silent https://storage.googleapis.com/apm-ui-e2e-static-data/events.json --output cypress/ingest-data/events.json
+node cypress/ingest-data/replay.js --server-url http://localhost:8200 --secret-token abcd --events ./events.json > ingest-data.log
 
 echo "3/3 Start Kibana ..."
 popd
