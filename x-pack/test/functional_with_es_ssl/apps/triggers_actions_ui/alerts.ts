96d235fc9e47df134d2252df379ca2e7c6d56730
Gidi Meir Morris <github@gidi.io>
2020-03-24 17:27:24 +0000

    allow users to unset the throttle of an alert (#60964) (#61088)

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alerts.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alerts.ts
@@ -185,6 +185,108 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       ]);
     });
 
+    it('should set an alert throttle', async () => {
+      const alertName = `edit throttle ${generateUniqueKey()}`;
+      const createdAlert = await createAlert({
+        alertTypeId: '.index-threshold',
+        name: alertName,
+        params: {
+          aggType: 'count',
+          termSize: 5,
+          thresholdComparator: '>',
+          timeWindowSize: 5,
+          timeWindowUnit: 'm',
+          groupBy: 'all',
+          threshold: [1000, 5000],
+          index: ['.kibana_1'],
+          timeField: 'alert',
+        },
+      });
+      await pageObjects.common.navigateToApp('triggersActions');
+      await pageObjects.triggersActionsUI.searchAlerts(createdAlert.name);
+
+      const searchResults = await pageObjects.triggersActionsUI.getAlertsList();
+      expect(searchResults).to.eql([
+        {
+          name: createdAlert.name,
+          tagsText: 'foo, bar',
+          alertType: 'Index Threshold',
+          interval: '1m',
+        },
+      ]);
+
+      const editLink = await testSubjects.findAll('alertsTableCell-editLink');
+      await editLink[0].click();
+
+      const throttleInputToSetInitialValue = await testSubjects.find('throttleInput');
+      await throttleInputToSetInitialValue.click();
+      await throttleInputToSetInitialValue.clearValue();
+      await throttleInputToSetInitialValue.type('1');
+
+      await find.clickByCssSelector('[data-test-subj="saveEditedAlertButton"]:not(disabled)');
+
+      expect(await pageObjects.common.closeToast()).to.eql(`Updated '${createdAlert.name}'`);
+
+      await pageObjects.common.navigateToApp('triggersActions');
+      await pageObjects.triggersActionsUI.searchAlerts(createdAlert.name);
+      await (await testSubjects.findAll('alertsTableCell-editLink'))[0].click();
+      const throttleInput = await testSubjects.find('throttleInput');
+      expect(await throttleInput.getAttribute('value')).to.eql('1');
+    });
+
+    it('should unset an alert throttle', async () => {
+      const alertName = `edit throttle ${generateUniqueKey()}`;
+      const createdAlert = await createAlert({
+        alertTypeId: '.index-threshold',
+        name: alertName,
+        throttle: '10m',
+        params: {
+          aggType: 'count',
+          termSize: 5,
+          thresholdComparator: '>',
+          timeWindowSize: 5,
+          timeWindowUnit: 'm',
+          groupBy: 'all',
+          threshold: [1000, 5000],
+          index: ['.kibana_1'],
+          timeField: 'alert',
+        },
+      });
+      await pageObjects.common.navigateToApp('triggersActions');
+      await pageObjects.triggersActionsUI.searchAlerts(createdAlert.name);
+
+      const searchResults = await pageObjects.triggersActionsUI.getAlertsList();
+      expect(searchResults).to.eql([
+        {
+          name: createdAlert.name,
+          tagsText: 'foo, bar',
+          alertType: 'Index Threshold',
+          interval: '1m',
+        },
+      ]);
+
+      const editLink = await testSubjects.findAll('alertsTableCell-editLink');
+      await editLink[0].click();
+
+      const throttleInputToUnsetValue = await testSubjects.find('throttleInput');
+
+      expect(await throttleInputToUnsetValue.getAttribute('value')).to.eql('10');
+      await throttleInputToUnsetValue.click();
+      await throttleInputToUnsetValue.clearValueWithKeyboard();
+
+      expect(await throttleInputToUnsetValue.getAttribute('value')).to.eql('');
+
+      await find.clickByCssSelector('[data-test-subj="saveEditedAlertButton"]:not(disabled)');
+
+      expect(await pageObjects.common.closeToast()).to.eql(`Updated '${createdAlert.name}'`);
+
+      await pageObjects.common.navigateToApp('triggersActions');
+      await pageObjects.triggersActionsUI.searchAlerts(createdAlert.name);
+      await (await testSubjects.findAll('alertsTableCell-editLink'))[0].click();
+      const throttleInput = await testSubjects.find('throttleInput');
+      expect(await throttleInput.getAttribute('value')).to.eql('');
+    });
+
     it('should reset alert when canceling an edit', async () => {
       const createdAlert = await createAlert({
         alertTypeId: '.index-threshold',
