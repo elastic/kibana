70d292a5c62f748987a55a5b531b7335c8883604
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-03-24 20:28:11 +0100

    [APM] E2E: Zero config for running e2e locally (#59152)

--- x-pack/legacy/plugins/apm/e2e/ingest-data/replay.js
+++ x-pack/legacy/plugins/apm/e2e/ingest-data/replay.js
@@ -4,15 +4,21 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
+
+/* eslint-disable import/no-extraneous-dependencies */
 
 /**
  * This script is useful for ingesting previously generated APM data into Elasticsearch via APM Server
  *
  * You can either:
  *  1. Download a static test data file from: https://storage.googleapis.com/apm-ui-e2e-static-data/events.json
- *  2. Or, generate the test data file yourself by following the steps in: https://github.com/sqren/kibana/blob/master/x-pack/legacy/plugins/apm/cypress/README.md#how-to-generate-static-data
+ *  2. Or, generate the test data file yourself:
+ *    git clone https://github.com/elastic/apm-integration-testing.git
+ *    ./scripts/compose.py start master --no-kibana --with-opbeans-node --apm-server-record
+ *    docker cp localtesting_8.0.0_apm-server-2:/app/events.json . && cat events.json | wc -l
+ *
+ *
  *
  * Run the script:
  *
@@ -27,6 +33,7 @@ const axios = require('axios');
 const readFile = promisify(fs.readFile);
 const pLimit = require('p-limit');
 const { argv } = require('yargs');
+const ora = require('ora');
 
 const APM_SERVER_URL = argv.serverUrl;
 const SECRET_TOKEN = argv.secretToken;
@@ -43,10 +50,27 @@ if (!EVENTS_PATH) {
 }
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const requestProgress = {
+  succeeded: 0,
+  failed: 0,
+  total: 0
+};
+
+const spinner = ora({ text: 'Warming up...', stream: process.stdout });
+
+function updateSpinnerText({ success }) {
+  success ? requestProgress.succeeded++ : requestProgress.failed++;
+  const remaining =
+    requestProgress.total -
+    (requestProgress.succeeded + requestProgress.failed);
+
+  spinner.text = `Remaining: ${remaining}. Succeeded: ${requestProgress.succeeded}. Failed: ${requestProgress.failed}.`;
+}
+
 async function insertItem(item) {
   try {
     const url = `${APM_SERVER_URL}${item.url}`;
-    console.log(Date.now(), url);
 
     const headers = {
       'content-type': 'application/x-ndjson'
@@ -63,20 +87,20 @@ async function insertItem(item) {
       data: item.body
     });
 
+    updateSpinnerText({ success: true });
+
     // add delay to avoid flooding the queue
     return delay(500);
   } catch (e) {
-    console.log('an error occurred');
-    if (e.response) {
-      console.log(e.response.data);
-    } else {
-      console.log('error', e);
-    }
+    console.error(
+      `${e.response ? JSON.stringify(e.response.data) : e.message}`
+    );
+    updateSpinnerText({ success: false });
   }
 }
 
 async function init() {
-  const content = await readFile(path.resolve(__dirname, EVENTS_PATH));
+  const content = await readFile(path.resolve(EVENTS_PATH));
   const items = content
     .toString()
     .split('\n')
@@ -84,10 +108,21 @@ async function init() {
     .map(item => JSON.parse(item))
     .filter(item => item.url === '/intake/v2/events');
 
+  spinner.start();
+  requestProgress.total = items.length;
+
   const limit = pLimit(20); // number of concurrent requests
   await Promise.all(items.map(item => limit(() => insertItem(item))));
 }
 
-init().catch(e => {
-  console.log('An error occurred:', e);
-});
+init()
+  .catch(e => {
+    console.log('An error occurred:', e);
+    process.exit(1);
+  })
+  .then(() => {
+    spinner.succeed(
+      `Successfully ingested ${requestProgress.succeeded} of ${requestProgress.total} events`
+    );
+    process.exit(0);
+  });
