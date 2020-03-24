737d0259d7efc4eb1f210fa7f4e342ad0ec5245f
Oliver Gupte <ogupte@users.noreply.github.com>
2020-03-24 13:56:08 -0700

    Closes #60754 by removing uiFilters from the API route for service map (#61012)
    since it is unused.

--- x-pack/legacy/plugins/apm/public/components/app/ServiceMap/index.tsx
+++ x-pack/legacy/plugins/apm/public/components/app/ServiceMap/index.tsx
@@ -29,7 +29,7 @@ interface ServiceMapProps {
 
 export function ServiceMap({ serviceName }: ServiceMapProps) {
   const license = useLicense();
-  const { urlParams, uiFilters } = useUrlParams();
+  const { urlParams } = useUrlParams();
 
   const { data } = useFetcher(() => {
     const { start, end, environment } = urlParams;
@@ -41,16 +41,12 @@ export function ServiceMap({ serviceName }: ServiceMapProps) {
             start,
             end,
             environment,
-            serviceName,
-            uiFilters: JSON.stringify({
-              ...uiFilters,
-              environment: undefined
-            })
+            serviceName
           }
         }
       });
     }
-  }, [serviceName, uiFilters, urlParams]);
+  }, [serviceName, urlParams]);
 
   const { ref, height, width } = useRefDimensions();
 
