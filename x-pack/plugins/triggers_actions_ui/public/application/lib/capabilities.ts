daf35f9da88e77fabc7ece57bb32fb20952e1850
Patrick Mueller <pmuellr@gmail.com>
2020-03-24 12:40:30 -0400

    [Alerting] add alerting privileges for uptime and metrics
    resolves https://github.com/elastic/kibana/issues/60135
    
    See the reference issue for how to configure Kibana to test this manually.
    There are no automated tests for this ATM.
    
    The basic test scenario is to create a role with just the Uptime space
    privilege, make sure it get to the Alerts and Actions management app,
    make sure you can create an action (server log), and alert (index threshold),
    and that the alert and action run successfully.
    
    Then repeat with just the Metrics space privilege.

--- x-pack/plugins/triggers_actions_ui/public/application/lib/capabilities.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/lib/capabilities.ts
@@ -12,7 +12,7 @@
 
 type Capabilities = Record<string, any>;
 
-const apps = ['apm', 'siem'];
+const apps = ['apm', 'siem', 'uptime', 'infrastructure'];
 
 function hasCapability(capabilities: Capabilities, capability: string) {
   return apps.some(app => capabilities[app]?.[capability]);
