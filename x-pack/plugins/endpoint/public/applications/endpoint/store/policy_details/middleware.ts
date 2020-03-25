82a6c4d88335c8f50d7eb28fcedd4e3f283d9075
Candace Park <56409205+parkiino@users.noreply.github.com>
2020-03-25 18:19:28 -0400

    Task/windows eventing form (#60690)
    Policy Details: Windows Eventing UI card
    
    Co-authored-by: kevinlog <kevin.logan@elastic.co>

--- x-pack/plugins/endpoint/public/applications/endpoint/store/policy_details/middleware.ts
+++ x-pack/plugins/endpoint/public/applications/endpoint/store/policy_details/middleware.ts
@@ -5,7 +5,7 @@
  */
 
 import { MiddlewareFactory, PolicyDetailsState } from '../../types';
-import { selectPolicyIdFromParams, isOnPolicyDetailsPage } from './selectors';
+import { policyIdFromParams, isOnPolicyDetailsPage } from './selectors';
 import { sendGetDatasource } from '../../services/ingest';
 
 export const policyDetailsMiddlewareFactory: MiddlewareFactory<PolicyDetailsState> = coreStart => {
@@ -16,7 +16,7 @@ export const policyDetailsMiddlewareFactory: MiddlewareFactory<PolicyDetailsStat
     const state = getState();
 
     if (action.type === 'userChangedUrl' && isOnPolicyDetailsPage(state)) {
-      const id = selectPolicyIdFromParams(state);
+      const id = policyIdFromParams(state);
 
       const { item: policyItem } = await sendGetDatasource(http, id);
 
@@ -24,6 +24,19 @@ export const policyDetailsMiddlewareFactory: MiddlewareFactory<PolicyDetailsStat
         type: 'serverReturnedPolicyDetailsData',
         payload: {
           policyItem,
+          policyConfig: {
+            windows: {
+              malware: {
+                mode: 'detect',
+              },
+              eventing: {
+                process: true,
+                network: true,
+              },
+            },
+            mac: {},
+            linux: {},
+          },
         },
       });
     }
--- x-pack/plugins/endpoint/public/applications/endpoint/view/policy/policy_details.tsx
+++ x-pack/plugins/endpoint/public/applications/endpoint/view/policy/policy_details.tsx
@@ -5,13 +5,26 @@
  */
 
 import React from 'react';
-import { EuiTitle } from '@elastic/eui';
+import {
+  EuiTitle,
+  EuiPage,
+  EuiPageBody,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButton,
+  EuiButtonEmpty,
+  EuiText,
+  EuiSpacer,
+} from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { usePolicyDetailsSelector } from './policy_hooks';
-import { selectPolicyDetails } from '../../store/policy_details/selectors';
+import { policyDetails } from '../../store/policy_details/selectors';
+import { WindowsEventing } from './policy_forms/eventing/windows';
 
 export const PolicyDetails = React.memo(() => {
-  const policyItem = usePolicyDetailsSelector(selectPolicyDetails);
+  const policyItem = usePolicyDetailsSelector(policyDetails);
 
   function policyName() {
     if (policyItem) {
@@ -29,8 +42,41 @@ export const PolicyDetails = React.memo(() => {
   }
 
   return (
-    <EuiTitle size="l">
-      <h1 data-test-subj="policyDetailsViewTitle">{policyName()}</h1>
-    </EuiTitle>
+    <EuiPage data-test-subj="policyDetailsPage">
+      <EuiPageBody>
+        <EuiPageHeader>
+          <EuiPageHeaderSection>
+            <EuiTitle size="m">
+              <h1 data-test-subj="policyDetailsViewTitle">{policyName()}</h1>
+            </EuiTitle>
+          </EuiPageHeaderSection>
+          <EuiFlexGroup justifyContent="flexEnd" gutterSize="s">
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty>
+                <FormattedMessage
+                  id="xpack.endpoint.policy.details.cancel"
+                  defaultMessage="Cancel"
+                />
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiButton fill={true} iconType="save">
+                <FormattedMessage id="xpack.endpoint.policy.details.save" defaultMessage="Save" />
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPageHeader>
+        <EuiText size="xs" color="subdued">
+          <h4>
+            <FormattedMessage
+              id="xpack.endpoint.policy.details.settings"
+              defaultMessage="Settings"
+            />
+          </h4>
+        </EuiText>
+        <EuiSpacer size="xs" />
+        <WindowsEventing />
+      </EuiPageBody>
+    </EuiPage>
   );
 });
