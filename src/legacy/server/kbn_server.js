0c49908ec194b2dc30ba3a3b169846410d989703
Rudolf Meijering <skaapgif@gmail.com>
2020-03-25 14:46:04 +0100

    Use the default schema factory to create logging config

--- src/legacy/server/kbn_server.js
+++ src/legacy/server/kbn_server.js
@@ -22,6 +22,7 @@ import { constant, once, compact, flatten } from 'lodash';
 import { isWorker } from 'cluster';
 // eslint-disable-next-line @kbn/eslint/no-restricted-paths
 import { fromRoot, pkg } from '../../core/server/utils';
+import { Config } from './config';
 import loggingConfiguration from './logging/configuration';
 import httpMixin from './http';
 import { coreMixin } from './core';
@@ -197,7 +198,8 @@ export default class KbnServer {
     return await this.server.inject(opts);
   }
 
-  applyLoggingConfiguration(config) {
+  applyLoggingConfiguration(settings) {
+    const config = Config.withDefaultSchema(settings);
     const loggingOptions = loggingConfiguration(config);
     const subset = {
       ops: config.get('ops'),
