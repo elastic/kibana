e487e0b809f16863ce187395967d3b85b90d5fa2
Josh Dover <me@joshdover.com>
2020-03-25 16:30:21 -0600

    Fix types

--- x-pack/plugins/upgrade_assistant/server/lib/apm/extract_index_patterns.ts
+++ x-pack/plugins/upgrade_assistant/server/lib/apm/extract_index_patterns.ts
@@ -3,16 +3,28 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { uniq } from 'lodash';
+import { uniq, pick } from 'lodash';
+import { APMOSSConfig } from '../../../../../../src/plugins/apm_oss/server';
 
-export const extractIndexPatterns = (apmConfig: Record<string, string>): string[] =>
-  uniq(
-    [
-      'sourcemapIndices',
-      'errorIndices',
-      'transactionIndices',
-      'spanIndices',
-      'metricsIndices',
-      'onboardingIndices',
-    ].map(type => apmConfig[type]!)
-  );
+type APMIndexConfig = Pick<
+  APMOSSConfig,
+  | 'sourcemapIndices'
+  | 'errorIndices'
+  | 'transactionIndices'
+  | 'spanIndices'
+  | 'metricsIndices'
+  | 'onboardingIndices'
+>;
+
+export const extractIndexPatterns = (apmConfig: APMOSSConfig): string[] => {
+  const indexConfigs = pick<APMIndexConfig, APMOSSConfig>(apmConfig, [
+    'sourcemapIndices',
+    'errorIndices',
+    'transactionIndices',
+    'spanIndices',
+    'metricsIndices',
+    'onboardingIndices',
+  ]);
+
+  return uniq(Object.values(indexConfigs));
+};
