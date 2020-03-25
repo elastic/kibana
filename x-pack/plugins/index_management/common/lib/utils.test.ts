57fb49469ad765de0739d705837bc53c1867c598
Sébastien Loix <sabee77@gmail.com>
2020-03-25 16:53:23 +0100

    Move utils to common folder

--- x-pack/plugins/index_management/common/lib/utils.test.ts
+++ x-pack/plugins/index_management/common/lib/utils.test.ts
@@ -3,31 +3,33 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { TemplateV1, TemplateV2 } from '../../common';
+import { TemplateV1Serialized, TemplateV2Serialized } from '../types';
 import { getTemplateVersion } from './utils';
 
 describe('utils', () => {
   describe('getTemplateVersion', () => {
     test('should detect v1 template', () => {
       const template = {
-        indexPatterns: ['logs*'],
+        name: 'my_template',
+        index_patterns: ['logs*'],
         mappings: {
           properties: {},
         },
       };
-      expect(getTemplateVersion(template as TemplateV1)).toBe(1);
+      expect(getTemplateVersion(template as TemplateV1Serialized)).toBe(1);
     });
 
     test('should detect v2 template', () => {
       const template = {
-        indexPatterns: ['logs*'],
+        name: 'my_template',
+        index_patterns: ['logs*'],
         template: {
           mappings: {
             properties: {},
           },
         },
       };
-      expect(getTemplateVersion(template as TemplateV2)).toBe(2);
+      expect(getTemplateVersion(template as TemplateV2Serialized)).toBe(2);
     });
   });
 });
