e95bfe8842c679a6d7505b4cc687a7523ca569c2
Melissa Alvarez <melissa.alvarez@elastic.co>
2020-03-25 11:45:57 -0400

    remove unused explorer code

--- x-pack/plugins/ml/public/application/explorer/components/explorer_query_bar/explorer_query_bar.tsx
+++ x-pack/plugins/ml/public/application/explorer/components/explorer_query_bar/explorer_query_bar.tsx
@@ -12,10 +12,10 @@ import {
   QueryStringInput,
 } from '../../../../../../../../src/plugins/data/public';
 import { IIndexPattern } from '../../../../../../../../src/plugins/data/common/index_patterns';
-import { QUERY_LANGUAGE_KUERY, QUERY_LANGUAGE_LUCENE } from '../../explorer_constants';
+import { SEARCH_QUERY_LANGUAGE } from '../../../../../common/constants/search';
 import { explorerService } from '../../explorer_dashboard_service';
 
-export const DEFAULT_QUERY_LANG = QUERY_LANGUAGE_KUERY;
+export const DEFAULT_QUERY_LANG = SEARCH_QUERY_LANGUAGE.KUERY;
 
 export function getKqlQueryValues({
   inputString,
@@ -45,12 +45,12 @@ export function getKqlQueryValues({
       }
     });
   }
-  if (queryLanguage === QUERY_LANGUAGE_KUERY) {
+  if (queryLanguage === SEARCH_QUERY_LANGUAGE.KUERY) {
     influencersFilterQuery = esKuery.toElasticsearchQuery(
       esKuery.fromKueryExpression(inputString),
       indexPattern
     );
-  } else if (queryLanguage === QUERY_LANGUAGE_LUCENE) {
+  } else if (queryLanguage === SEARCH_QUERY_LANGUAGE.LUCENE) {
     influencersFilterQuery = esQuery.luceneStringToDsl(inputString);
   }
 
@@ -78,7 +78,7 @@ function getInitSearchInputState({
 }) {
   if (queryString !== undefined && filterActive === true) {
     return {
-      language: QUERY_LANGUAGE_KUERY,
+      language: SEARCH_QUERY_LANGUAGE.KUERY,
       query: queryString,
     };
   } else {
