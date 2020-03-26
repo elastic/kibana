cb8670d7af666a5224efbfe26a503022f715104d
Peter Pisljar <peter.pisljar@gmail.com>
2020-03-26 15:50:54 +0100

    Fixing abort error on dashboard (#61279)

--- src/legacy/core_plugins/visualizations/public/np_ready/public/embeddable/visualize_embeddable.ts
+++ src/legacy/core_plugins/visualizations/public/np_ready/public/embeddable/visualize_embeddable.ts
@@ -86,6 +86,7 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
   private domNode: any;
   public readonly type = VISUALIZE_EMBEDDABLE_TYPE;
   private autoRefreshFetchSubscription: Subscription;
+  private abortController?: AbortController;
 
   constructor(
     timefilter: TimefilterContract,
@@ -322,9 +323,14 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
       },
       uiState: this.vis.uiState,
     };
+    if (this.abortController) {
+      this.abortController.abort();
+    }
+    this.abortController = new AbortController();
     this.expression = await buildPipeline(this.vis, {
       timefilter: this.timefilter,
       timeRange: this.timeRange,
+      abortSignal: this.abortController!.signal,
     });
 
     if (this.handler) {
