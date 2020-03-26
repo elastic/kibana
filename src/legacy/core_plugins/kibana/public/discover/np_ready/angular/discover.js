b7dd1cc984add14f06bb38d0cc0caf5a45042ae2
sulemanof <sul-dan@mail.ru>
2020-03-26 08:13:52 +0300

    Revert fixing filters

--- src/legacy/core_plugins/kibana/public/discover/np_ready/angular/discover.js
+++ src/legacy/core_plugins/kibana/public/discover/np_ready/angular/discover.js
@@ -275,7 +275,6 @@ function discoverController(
       filterManager.getUpdates$(),
       {
         next: () => {
-          $scope.state.filters = filterManager.getAppFilters();
           $scope.updateDataSource();
         },
       },
