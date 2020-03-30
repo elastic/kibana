6d864ed107cba0035be7586654181f0246552d12
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-30 15:15:11 +0100

    ci(jenkins): nohup in the background

--- x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
+++ x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
@@ -9,4 +9,4 @@ yarn kbn bootstrap
 echo "2/2 Start Kibana ..."
 ## Might help to avoid FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
 export NODE_OPTIONS="--max-old-space-size=4096"
-nohup node ./scripts/kibana --no-base-path --no-watch --no-dev-config --config x-pack/legacy/plugins/apm/e2e/ci/kibana.e2e.yml > kibana.log
+nohup node ./scripts/kibana --no-base-path --no-watch --no-dev-config --config x-pack/legacy/plugins/apm/e2e/ci/kibana.e2e.yml > kibana.log 2>&1 &
