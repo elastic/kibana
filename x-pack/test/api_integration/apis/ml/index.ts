fc07473d029b931accd6dbf7e72e7f6fa1ef362b
Robert Oskamp <robert.oskamp@elastic.co>
2020-03-30 11:00:19 +0200

    Add recognize module tests

--- x-pack/test/api_integration/apis/ml/index.ts
+++ x-pack/test/api_integration/apis/ml/index.ts
@@ -26,5 +26,6 @@ export default function({ getService, loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./calculate_model_memory_limit'));
     loadTestFile(require.resolve('./categorization_field_examples'));
     loadTestFile(require.resolve('./get_module'));
+    loadTestFile(require.resolve('./recognize_module'));
   });
 }
