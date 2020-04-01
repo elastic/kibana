cbb3a14352b931c036e038fea7d4a39e3a32e0f2
Gidi Meir Morris <github@gidi.io>
2020-04-01 17:09:09 +0100

    added test for pagination

--- x-pack/test/plugin_api_integration/test_suites/event_log/public_api_integration.ts
+++ x-pack/test/plugin_api_integration/test_suites/event_log/public_api_integration.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { merge, omit } from 'lodash';
+import { merge, omit, times, chunk } from 'lodash';
 import uuid from 'uuid';
 import expect from '@kbn/expect/expect.js';
 import { FtrProviderContext } from '../../ftr_provider_context';
@@ -33,16 +33,60 @@ export default function({ getService }: FtrProviderContext) {
 
         expect(foundEvents.length).to.be(2);
 
-        (foundEvents as IValidatedEvent[]).forEach((foundEvent: IValidatedEvent, index: number) => {
-          expect(foundEvent!.event).to.eql(expectedEvents[index]!.event);
-          expect(omit(foundEvent!.kibana ?? {}, 'server_uuid')).to.eql(
-            expectedEvents[index]!.kibana
-          );
-          expect(foundEvent!.message).to.eql(expectedEvents[index]!.message);
-        });
+        assertEventsFromApiMatchCreatedEvents(foundEvents, expectedEvents);
       });
     });
+
+    it('should support pagination for events', async () => {
+      const id = uuid.v4();
+
+      const [firstExpectedEvent, ...expectedEvents] = times(6, () => fakeEvent(id));
+      // run one first to create the SO and avoid clashes
+      await logTestEvent(id, firstExpectedEvent);
+      await Promise.all(expectedEvents.map(event => logTestEvent(id, event)));
+
+      await retry.try(async () => {
+        const { body: foundEvents } = await supertest
+          .get(`/api/event_log/event_log_test/${id}/_find`)
+          .set('kbn-xsrf', 'foo')
+          .expect(200);
+
+        expect(foundEvents.length).to.be(6);
+      });
+
+      const [expectedFirstPage, expectedSecondPage] = chunk(
+        [firstExpectedEvent, ...expectedEvents],
+        3
+      );
+
+      const { body: firstPage } = await supertest
+        .get(`/api/event_log/event_log_test/${id}/_find?per_page=3`)
+        .set('kbn-xsrf', 'foo')
+        .expect(200);
+
+      expect(firstPage.length).to.be(3);
+      assertEventsFromApiMatchCreatedEvents(firstPage, expectedFirstPage);
+
+      const { body: secondPage } = await supertest
+        .get(`/api/event_log/event_log_test/${id}/_find?per_page=3&page=2`)
+        .set('kbn-xsrf', 'foo')
+        .expect(200);
+
+      expect(secondPage.length).to.be(3);
+      assertEventsFromApiMatchCreatedEvents(secondPage, expectedSecondPage);
+    });
   });
+
+  function assertEventsFromApiMatchCreatedEvents(
+    foundEvents: IValidatedEvent[],
+    expectedEvents: IEvent[]
+  ) {
+    foundEvents.forEach((foundEvent: IValidatedEvent, index: number) => {
+      expect(foundEvent!.event).to.eql(expectedEvents[index]!.event);
+      expect(omit(foundEvent!.kibana ?? {}, 'server_uuid')).to.eql(expectedEvents[index]!.kibana);
+      expect(foundEvent!.message).to.eql(expectedEvents[index]!.message);
+    });
+  }
 
   async function logTestEvent(id: string, event: IEvent) {
     log.debug(`Logging Event for Saved Object ${id}`);
