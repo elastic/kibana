24e7518334a11d3fbf04abe8a9e9d45c30695762
sulemanof <sul-dan@mail.ru>
2020-04-01 13:44:29 +0300

    Import from dashboard_constants in FT

--- x-pack/test/functional/apps/dashboard/feature_controls/dashboard_spaces.ts
+++ x-pack/test/functional/apps/dashboard/feature_controls/dashboard_spaces.ts
@@ -7,7 +7,7 @@ import expect from '@kbn/expect';
 import {
   createDashboardEditUrl,
   DashboardConstants,
-} from '../../../../../../src/plugins/dashboard/public';
+} from '../../../../../../src/plugins/dashboard/public/dashboard_constants';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function({ getPageObjects, getService }: FtrProviderContext) {
