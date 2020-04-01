9a5b7eac70a4174c71729420ce37a80f84f7067b
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-04-01 13:26:30 +0200

    [ML] fix time range initialization

--- x-pack/plugins/ml/public/application/jobs/new_job/pages/new_job/page.tsx
+++ x-pack/plugins/ml/public/application/jobs/new_job/pages/new_job/page.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FC, useEffect, Fragment } from 'react';
+import React, { FC, useEffect, Fragment, useState } from 'react';
 import {
   EuiPage,
   EuiPageBody,
@@ -51,114 +51,137 @@ export interface PageProps {
   jobType: JOB_TYPE;
 }
 
+function getInitialChartInterval() {
+  const chartInterval = new TimeBuckets();
+  chartInterval.setBarTarget(BAR_TARGET);
+  chartInterval.setMaxBars(MAX_BARS);
+  chartInterval.setInterval('auto');
+  return chartInterval;
+}
+
 export const Page: FC<PageProps> = ({ existingJobsAndGroups, jobType }) => {
   const {
     services: { notifications },
   } = useMlKibana();
   const mlContext = useMlContext();
-  const jobCreator = jobCreatorFactory(jobType)(
-    mlContext.currentIndexPattern,
-    mlContext.currentSavedSearch,
-    mlContext.combinedQuery
-  );
 
-  const { from, to } = getTimeFilterRange();
-  jobCreator.setTimeRange(from, to);
+  const [isWizardReady, setIsWizardReady] = useState(false);
+  const [jobCreator] = useState(
+    jobCreatorFactory(jobType)(
+      mlContext.currentIndexPattern,
+      mlContext.currentSavedSearch,
+      mlContext.combinedQuery
+    )
+  );
+  const [chartInterval] = useState(getInitialChartInterval());
+  const [chartLoader] = useState(
+    new ChartLoader(mlContext.currentIndexPattern, mlContext.combinedQuery)
+  );
+  const [jobValidator] = useState(new JobValidator(jobCreator, existingJobsAndGroups));
+  const [resultsLoader] = useState(new ResultsLoader(jobCreator, chartInterval, chartLoader));
 
   let firstWizardStep =
     jobType === JOB_TYPE.ADVANCED
       ? WIZARD_STEPS.ADVANCED_CONFIGURE_DATAFEED
       : WIZARD_STEPS.TIME_RANGE;
 
-  let autoSetTimeRange = false;
+  useEffect(() => {
+    let autoSetTimeRange = false;
 
-  if (mlJobService.tempJobCloningObjects.job !== undefined) {
-    // cloning a job
-    const clonedJob = mlJobService.cloneJob(mlJobService.tempJobCloningObjects.job);
-    const { job, datafeed } = expandCombinedJobConfig(clonedJob);
-    initCategorizationSettings();
-    jobCreator.cloneFromExistingJob(job, datafeed);
+    if (mlJobService.tempJobCloningObjects.job !== undefined) {
+      // cloning a job
+      const clonedJob = mlJobService.cloneJob(mlJobService.tempJobCloningObjects.job);
+      const { job, datafeed } = expandCombinedJobConfig(clonedJob);
+      initCategorizationSettings();
+      jobCreator.cloneFromExistingJob(job, datafeed);
 
-    // if we're not skipping the time range, this is a standard job clone, so wipe the jobId
-    if (mlJobService.tempJobCloningObjects.skipTimeRangeStep === false) {
-      jobCreator.jobId = '';
-    } else if (jobType !== JOB_TYPE.ADVANCED) {
-      firstWizardStep = WIZARD_STEPS.PICK_FIELDS;
-    }
+      // if we're not skipping the time range, this is a standard job clone, so wipe the jobId
+      if (mlJobService.tempJobCloningObjects.skipTimeRangeStep === false) {
+        jobCreator.jobId = '';
+      } else if (jobType !== JOB_TYPE.ADVANCED) {
+        firstWizardStep = WIZARD_STEPS.PICK_FIELDS;
+      }
 
-    mlJobService.tempJobCloningObjects.skipTimeRangeStep = false;
-    mlJobService.tempJobCloningObjects.job = undefined;
+      mlJobService.tempJobCloningObjects.skipTimeRangeStep = false;
+      mlJobService.tempJobCloningObjects.job = undefined;
 
-    if (
-      mlJobService.tempJobCloningObjects.start !== undefined &&
-      mlJobService.tempJobCloningObjects.end !== undefined
-    ) {
-      // auto select the start and end dates for the time range picker
-      jobCreator.setTimeRange(
-        mlJobService.tempJobCloningObjects.start,
-        mlJobService.tempJobCloningObjects.end
-      );
-      mlJobService.tempJobCloningObjects.start = undefined;
-      mlJobService.tempJobCloningObjects.end = undefined;
+      if (
+        mlJobService.tempJobCloningObjects.start !== undefined &&
+        mlJobService.tempJobCloningObjects.end !== undefined
+      ) {
+        // auto select the start and end dates for the time range picker
+        jobCreator.setTimeRange(
+          mlJobService.tempJobCloningObjects.start,
+          mlJobService.tempJobCloningObjects.end
+        );
+        mlJobService.tempJobCloningObjects.start = undefined;
+        mlJobService.tempJobCloningObjects.end = undefined;
+      } else {
+        // if not start and end times are set and this is an advanced job,
+        // auto set the time range based on the index
+        autoSetTimeRange = isAdvancedJobCreator(jobCreator);
+      }
+
+      if (mlJobService.tempJobCloningObjects.calendars) {
+        jobCreator.calendars = mlJobService.tempJobCloningObjects.calendars;
+        mlJobService.tempJobCloningObjects.calendars = undefined;
+      }
     } else {
-      // if not start and end times are set and this is an advanced job,
-      // auto set the time range based on the index
+      // creating a new job
+      jobCreator.bucketSpan = DEFAULT_BUCKET_SPAN;
+
+      if (
+        jobCreator.type !== JOB_TYPE.POPULATION &&
+        jobCreator.type !== JOB_TYPE.ADVANCED &&
+        jobCreator.type !== JOB_TYPE.CATEGORIZATION
+      ) {
+        // for all other than population or advanced, use 10MB
+        jobCreator.modelMemoryLimit = DEFAULT_MODEL_MEMORY_LIMIT;
+      }
+
+      if (jobCreator.type === JOB_TYPE.SINGLE_METRIC) {
+        jobCreator.modelPlot = true;
+      }
+
+      if (mlContext.currentSavedSearch !== null) {
+        // Jobs created from saved searches cannot be cloned in the wizard as the
+        // ML job config holds no reference to the saved search ID.
+        jobCreator.createdBy = null;
+      }
+
+      // auto set the time range if creating a new advanced job
       autoSetTimeRange = isAdvancedJobCreator(jobCreator);
-    }
-
-    if (mlJobService.tempJobCloningObjects.calendars) {
-      jobCreator.calendars = mlJobService.tempJobCloningObjects.calendars;
-      mlJobService.tempJobCloningObjects.calendars = undefined;
-    }
-  } else {
-    // creating a new job
-    jobCreator.bucketSpan = DEFAULT_BUCKET_SPAN;
-
-    if (
-      jobCreator.type !== JOB_TYPE.POPULATION &&
-      jobCreator.type !== JOB_TYPE.ADVANCED &&
-      jobCreator.type !== JOB_TYPE.CATEGORIZATION
-    ) {
-      // for all other than population or advanced, use 10MB
-      jobCreator.modelMemoryLimit = DEFAULT_MODEL_MEMORY_LIMIT;
-    }
-
-    if (jobCreator.type === JOB_TYPE.SINGLE_METRIC) {
-      jobCreator.modelPlot = true;
-    }
-
-    if (mlContext.currentSavedSearch !== null) {
-      // Jobs created from saved searches cannot be cloned in the wizard as the
-      // ML job config holds no reference to the saved search ID.
-      jobCreator.createdBy = null;
-    }
-
-    // auto set the time range if creating a new advanced job
-    autoSetTimeRange = isAdvancedJobCreator(jobCreator);
-    initCategorizationSettings();
-    if (isCategorizationJobCreator(jobCreator)) {
-      const { catFields } = newJobCapsService;
-      if (catFields.length === 1) {
-        jobCreator.categorizationFieldName = catFields[0].name;
+      initCategorizationSettings();
+      if (isCategorizationJobCreator(jobCreator)) {
+        const { catFields } = newJobCapsService;
+        if (catFields.length === 1) {
+          jobCreator.categorizationFieldName = catFields[0].name;
+        }
       }
     }
-  }
 
-  if (autoSetTimeRange && isAdvancedJobCreator(jobCreator)) {
-    // for advanced jobs, load the full time range start and end times
-    // so they can be used for job validation and bucket span estimation
-    try {
-      jobCreator.autoSetTimeRange();
-    } catch (error) {
-      const { toasts } = notifications;
-      toasts.addDanger({
-        title: i18n.translate('xpack.ml.newJob.wizard.autoSetJobCreatorTimeRange.error', {
-          defaultMessage: `Error retrieving beginning and end times of index`,
-        }),
-        text: error,
-      });
+    if (autoSetTimeRange && isAdvancedJobCreator(jobCreator)) {
+      // for advanced jobs, load the full time range start and end times
+      // so they can be used for job validation and bucket span estimation
+      try {
+        jobCreator.autoSetTimeRange().then(() => {
+          setIsWizardReady(true);
+        });
+      } catch (error) {
+        const { toasts } = notifications;
+        toasts.addDanger({
+          title: i18n.translate('xpack.ml.newJob.wizard.autoSetJobCreatorTimeRange.error', {
+            defaultMessage: `Error retrieving beginning and end times of index`,
+          }),
+          text: error,
+        });
+      }
+    } else {
+      const { from, to } = getTimeFilterRange();
+      jobCreator.setTimeRange(from, to);
+      setIsWizardReady(true);
     }
-  }
+  }, []);
 
   function initCategorizationSettings() {
     if (isCategorizationJobCreator(jobCreator)) {
@@ -173,17 +196,6 @@ export const Page: FC<PageProps> = ({ existingJobsAndGroups, jobType }) => {
       jobCreator.categorizationAnalyzer = anomalyDetectors.categorization_analyzer!;
     }
   }
-
-  const chartInterval = new TimeBuckets();
-  chartInterval.setBarTarget(BAR_TARGET);
-  chartInterval.setMaxBars(MAX_BARS);
-  chartInterval.setInterval('auto');
-
-  const chartLoader = new ChartLoader(mlContext.currentIndexPattern, mlContext.combinedQuery);
-
-  const jobValidator = new JobValidator(jobCreator, existingJobsAndGroups);
-
-  const resultsLoader = new ResultsLoader(jobCreator, chartInterval, chartLoader);
 
   useEffect(() => {
     return () => {
@@ -213,15 +225,17 @@ export const Page: FC<PageProps> = ({ existingJobsAndGroups, jobType }) => {
             </EuiPageContentHeader>
 
             <EuiPageContentBody>
-              <Wizard
-                jobCreator={jobCreator}
-                chartLoader={chartLoader}
-                resultsLoader={resultsLoader}
-                chartInterval={chartInterval}
-                jobValidator={jobValidator}
-                existingJobsAndGroups={existingJobsAndGroups}
-                firstWizardStep={firstWizardStep}
-              />
+              {isWizardReady ? (
+                <Wizard
+                  jobCreator={jobCreator}
+                  chartLoader={chartLoader}
+                  resultsLoader={resultsLoader}
+                  chartInterval={chartInterval}
+                  jobValidator={jobValidator}
+                  existingJobsAndGroups={existingJobsAndGroups}
+                  firstWizardStep={firstWizardStep}
+                />
+              ) : null}
             </EuiPageContentBody>
           </EuiPageContent>
         </EuiPageBody>
