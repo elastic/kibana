0bc61968d7ae55539d8fe143e5f4c4c943933a41
Paul Tavares <paul.tavares@elastic.co>
2020-04-02 09:48:59 -0400

    Initial set of `todo` UT

--- x-pack/plugins/endpoint/public/applications/endpoint/view/policy/policy_details.test.tsx
+++ x-pack/plugins/endpoint/public/applications/endpoint/view/policy/policy_details.test.tsx
@@ -5,5 +5,31 @@
  */
 
 import React from 'react';
+import { createAppRootMockRenderer } from '../../mocks';
+import { PolicyDetails } from './policy_details';
 
-describe('when Policy details is shown', () => {});
+describe('Policy Details', () => {
+  const { render } = createAppRootMockRenderer();
+  render(<PolicyDetails />);
+
+  describe('when displayed with invalid id', () => {
+    it.todo('should show error message');
+  });
+  describe('when displayed with valid id', () => {
+    it.todo('should show loader while retrieving data');
+    it.todo('should render the view after data is retrieved');
+    it.todo('should navigate to list if back to link is clicked');
+    it.todo('should display agent stats');
+    it.todo('should display cancel button');
+    it.todo('should redirect to policy list when cancel button is clicked');
+    it.todo('should display save button');
+  });
+  describe('when the save button is clicked', () => {
+    it.todo('should show a modal confirmation');
+    it.todo('should show info callout if policy is in use');
+    it.todo('should hide dialog when save and deploy button is clicked');
+    it.todo('should close dialog if cancel button is clicked');
+    it.todo('should show a success notification toast if update succeeds');
+    it.todo('should show an error notification toast if update fails');
+  });
+});
