f72206deae97d1f862b6bccea59383b75cf36da0
Thomas Watson <w@tson.dk>
2020-04-02 13:16:41 +0200

    Ignore deprecation warning in 'shot' package

--- src/setup_node_env/exit_on_warning.js
+++ src/setup_node_env/exit_on_warning.js
@@ -34,6 +34,12 @@ var IGNORE_WARNINGS = [
   {
     name: 'MaxListenersExceededWarning',
   },
+  {
+    name: 'DeprecationWarning',
+    code: 'DEP0066',
+    file: '/node_modules/shot/lib/response.js',
+    line: 22,
+  },
 ];
 
 if (process.noProcessWarnings !== true) {
