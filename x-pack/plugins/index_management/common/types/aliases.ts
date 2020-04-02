74d84132ff5506150f58fc2947a0c6b3aafe2513
Sébastien Loix <sabee77@gmail.com>
2020-04-02 20:14:46 +0200

    [Index management] Prepare support Index template V2 format (#61588)

--- x-pack/plugins/index_management/common/types/aliases.ts
+++ x-pack/plugins/index_management/common/types/aliases.ts
@@ -3,10 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-export {
-  deserializeTemplateList,
-  deserializeV1Template,
-  serializeV1Template,
-} from './template_serialization';
 
-export { getTemplateParameter } from './utils';
+export interface Aliases {
+  [key: string]: any;
+}
