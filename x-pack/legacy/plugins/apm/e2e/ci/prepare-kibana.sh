6c547f62b0bd7f4843a4945944031cdf64e36e01
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-04-03 15:16:20 +0100

    ci(jenkins): to avoid listening for changes for the log file
    Fixes the system limit for number of file watchers reached

--- x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
+++ x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+E2E_PATH=x-pack/legacy/plugins/apm/e2e
 echo "1/2 Install dependencies ..."
 # shellcheck disable=SC1091
 source src/dev/ci_setup/setup_env.sh true
@@ -9,4 +10,4 @@ yarn kbn clean && yarn kbn bootstrap
 echo "2/2 Start Kibana ..."
 ## Might help to avoid FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
 export NODE_OPTIONS="--max-old-space-size=4096"
-nohup node ./scripts/kibana --no-base-path --dev --no-dev-config --config x-pack/legacy/plugins/apm/e2e/ci/kibana.e2e.yml > kibana.log 2>&1 &
+nohup node ./scripts/kibana --no-base-path --dev --no-dev-config --config ${E2E_PATH}/ci/kibana.e2e.yml > ${E2E_PATH}/kibana.log 2>&1 &
