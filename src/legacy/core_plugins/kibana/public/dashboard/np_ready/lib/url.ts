f1adf38f57e6fb575a6e7383b25ae829935bb3c6
Anton Dosov <anton.dosov@elastic.co>
2020-04-03 12:22:43 +0200

    [Drilldowns] Dashboard state fixes for drilldowns (#61457)
    1. Change logic around deciding wether to use time from url or from saved object. Previously code looked only into if _g is present in the url at all. And didn't consider edge case if time or refreshInterval is missing in _g
    
    2. Fix initial syncing of time from savedobject causing redundant history record. _This changed caused order of _a and g params in url change. One test was affected by it because it relied on the order. I don't think it should be considered breaking as order app puts it's query params shouldn't matter.
    
    3. Fix another race condition between state syncing with url and angular controller $destroy. Similar fix was done before in #57795, but this on covers case when we stay within dashboard app, but change dashboard
    
    4. Fix initial panel state migration causing redundant browser history records
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/legacy/core_plugins/kibana/public/dashboard/np_ready/lib/url.ts
+++ src/legacy/core_plugins/kibana/public/dashboard/np_ready/lib/url.ts
@@ -17,7 +17,19 @@
  * under the License.
  */
 
-export { saveDashboard } from './save_dashboard';
-export { getAppStateDefaults } from './get_app_state_defaults';
-export { migrateAppState } from './migrate_app_state';
-export { getDashboardIdFromUrl } from './url';
+/**
+ * Returns dashboard id from URL
+ * literally looks from id after `dashboard/` string and before `/`, `?` and end of string
+ * @param url to extract dashboardId from
+ * input: http://localhost:5601/lib/app/kibana#/dashboard?param1=x&param2=y&param3=z
+ * output: undefined
+ * input: http://localhost:5601/lib/app/kibana#/dashboard/39292992?param1=x&param2=y&param3=z
+ * output: 39292992
+ */
+export function getDashboardIdFromUrl(url: string): string | undefined {
+  const [, dashboardId] = url.match(/dashboard\/(.*?)(\/|\?|$)/) ?? [
+    undefined, // full match
+    undefined, // group with dashboardId
+  ];
+  return dashboardId ?? undefined;
+}
