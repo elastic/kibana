3f879cb460c031990f00afc2c426de8e645c0724
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-04-03 16:26:31 +0100

    store somewhere else

--- x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
+++ x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+E2E_DIR=x-pack/legacy/plugins/apm/e2e
 echo "1/2 Install dependencies ..."
 # shellcheck disable=SC1091
 source src/dev/ci_setup/setup_env.sh true
@@ -9,4 +10,4 @@ yarn kbn clean && yarn kbn bootstrap
 echo "2/2 Start Kibana ..."
 ## Might help to avoid FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
 export NODE_OPTIONS="--max-old-space-size=4096"
-nohup node ./scripts/kibana --no-base-path --no-watch --dev --no-dev-config --config x-pack/legacy/plugins/apm/e2e/ci/kibana.e2e.yml > kibana.log 2>&1 &
+nohup node ./scripts/kibana --no-base-path --no-watch --dev --no-dev-config --config ${E2E_DIR}/ci/kibana.e2e.yml > ${E2E_DIR}/kibana.log 2>&1 &
