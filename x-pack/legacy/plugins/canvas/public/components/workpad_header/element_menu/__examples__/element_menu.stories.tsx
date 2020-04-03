a50e076ff15f457b0176b9da9b581696acf20eae
Catherine Liu <catherine.liu@elastic.co>
2020-04-02 17:01:34 -0700

    Updated element_menu stories

--- x-pack/legacy/plugins/canvas/public/components/workpad_header/element_menu/__examples__/element_menu.stories.tsx
+++ x-pack/legacy/plugins/canvas/public/components/workpad_header/element_menu/__examples__/element_menu.stories.tsx
@@ -9,7 +9,37 @@ import React from 'react';
 import { ElementMenu } from '../element_menu';
 import { ElementSpec } from '../../../../../types';
 
-const testElements: { [key: string]: ElementSpec } = {};
+const testElements: { [key: string]: ElementSpec } = {
+  areaChart: {
+    name: 'areaChart',
+    displayName: 'Area chart',
+    help: 'A line chart with a filled body',
+    type: 'chart',
+    expression: `filters	
+    | demodata	
+    | pointseries x="time" y="mean(price)"	
+    | plot defaultStyle={seriesStyle lines=1 fill=1}	
+    | render`,
+  },
+  image: {
+    name: 'image',
+    displayName: 'Image',
+    help: 'A static image',
+    type: 'image',
+    expression: `image dataurl=null mode="contain"	
+  | render`,
+  },
+  table: {
+    name: 'table',
+    displayName: 'Data table',
+    type: 'chart',
+    help: 'A scrollable grid for displaying data in a tabular format',
+    expression: `filters	
+  | demodata	
+  | table	
+  | render`,
+  },
+};
 
 storiesOf('components/WorkpadHeader/ElementMenu', module).add('default', () => (
   <ElementMenu elements={testElements} addElement={action('addElement')} />
