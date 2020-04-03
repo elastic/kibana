31e4765fe2707087bcd2c9f3f614459ffd1be525
Jean-Louis Leysens <jloleysens@gmail.com>
2020-04-03 14:17:44 +0200

    Move fieldConfig to variable outside of the UseField

--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/other_type_json_parameter.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/other_type_json_parameter.tsx
@@ -12,6 +12,7 @@ import {
   JsonEditorField,
   ValidationFuncArg,
   fieldValidators,
+  FieldConfig,
 } from '../../../shared_imports';
 
 const { isJsonField } = fieldValidators;
@@ -22,72 +23,70 @@ const { isJsonField } = fieldValidators;
  * We use it to store custom defined parameters in a field called "otherTypeJson".
  */
 
-export const OtherTypeJsonParameter = () => (
-  <UseField
-    path="otherTypeJson"
-    config={{
-      label: i18n.translate('xpack.idxMgmt.mappingsEditor.otherTypeJsonFieldLabel', {
-        defaultMessage: 'Type Fields JSON',
-      }),
-      defaultValue: {},
-      validations: [
-        {
-          validator: isJsonField(
-            i18n.translate(
-              'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeJsonInvalidJSONErrorMessage',
+const fieldConfig: FieldConfig = {
+  label: i18n.translate('xpack.idxMgmt.mappingsEditor.otherTypeJsonFieldLabel', {
+    defaultMessage: 'Type Fields JSON',
+  }),
+  defaultValue: {},
+  validations: [
+    {
+      validator: isJsonField(
+        i18n.translate(
+          'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeJsonInvalidJSONErrorMessage',
+          {
+            defaultMessage: 'Invalid JSON.',
+          }
+        )
+      ),
+    },
+    {
+      validator: ({ value }: ValidationFuncArg<any, any>) => {
+        const json = JSON.parse(value);
+        if (Array.isArray(json)) {
+          return {
+            message: i18n.translate(
+              'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeJsonArrayNotAllowedErrorMessage',
               {
-                defaultMessage: 'Invalid JSON.',
+                defaultMessage: 'Arrays are not allowed.',
               }
-            )
-          ),
-        },
-        {
-          validator: ({ value }: ValidationFuncArg<any, any>) => {
-            const json = JSON.parse(value);
-            if (Array.isArray(json)) {
-              return {
-                message: i18n.translate(
-                  'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeJsonArrayNotAllowedErrorMessage',
-                  {
-                    defaultMessage: 'Arrays are not allowed.',
-                  }
-                ),
-              };
-            }
-          },
-        },
-        {
-          validator: ({ value }: ValidationFuncArg<any, any>) => {
-            const json = JSON.parse(value);
-            if (json.type) {
-              return {
-                code: 'ERR_CUSTOM_TYPE_OVERRIDDEN',
-                message: i18n.translate(
-                  'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeJsonTypeFieldErrorMessage',
-                  {
-                    defaultMessage: 'Cannot override the "type" field.',
-                  }
-                ),
-              };
-            }
-          },
-        },
-      ],
-      deserializer: (value: any) => {
-        if (value === '') {
-          return value;
-        }
-        return JSON.stringify(value, null, 2);
-      },
-      serializer: (value: string) => {
-        try {
-          return JSON.parse(value);
-        } catch (error) {
-          // swallow error and return non-parsed value;
-          return value;
+            ),
+          };
         }
       },
-    }}
-    component={JsonEditorField}
-  />
+    },
+    {
+      validator: ({ value }: ValidationFuncArg<any, any>) => {
+        const json = JSON.parse(value);
+        if (json.type) {
+          return {
+            code: 'ERR_CUSTOM_TYPE_OVERRIDDEN',
+            message: i18n.translate(
+              'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeJsonTypeFieldErrorMessage',
+              {
+                defaultMessage: 'Cannot override the "type" field.',
+              }
+            ),
+          };
+        }
+      },
+    },
+  ],
+  deserializer: (value: any) => {
+    if (value === '') {
+      return value;
+    }
+    return JSON.stringify(value, null, 2);
+  },
+  serializer: (value: string) => {
+    try {
+      return JSON.parse(value);
+    } catch (error) {
+      // swallow error and return non-parsed value;
+      return value;
+    }
+  },
+};
+
+export const OtherTypeJsonParameter = () => (
+  <UseField path="otherTypeJson" config={fieldConfig} component={JsonEditorField} />
 );
--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/other_type_name_parameter.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/other_type_name_parameter.tsx
@@ -7,7 +7,7 @@
 import React from 'react';
 
 import { i18n } from '@kbn/i18n';
-import { UseField, TextField } from '../../../shared_imports';
+import { UseField, TextField, FieldConfig } from '../../../shared_imports';
 import { fieldValidators } from '../../../shared_imports';
 
 const { emptyField } = fieldValidators;
@@ -18,27 +18,25 @@ const { emptyField } = fieldValidators;
  * We use it to store the name of types unknown to the mappings editor in the "subType" path.
  */
 
+const fieldConfig: FieldConfig = {
+  label: i18n.translate('xpack.idxMgmt.mappingsEditor.otherTypeNameFieldLabel', {
+    defaultMessage: 'Type Name',
+  }),
+  defaultValue: '',
+  validations: [
+    {
+      validator: emptyField(
+        i18n.translate(
+          'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeNameIsRequiredErrorMessage',
+          {
+            defaultMessage: 'The type name is required.',
+          }
+        )
+      ),
+    },
+  ],
+};
+
 export const OtherTypeNameParameter = () => (
-  <UseField
-    path="subType"
-    config={{
-      label: i18n.translate('xpack.idxMgmt.mappingsEditor.otherTypeNameFieldLabel', {
-        defaultMessage: 'Type Name',
-      }),
-      defaultValue: '',
-      validations: [
-        {
-          validator: emptyField(
-            i18n.translate(
-              'xpack.idxMgmt.mappingsEditor.parameters.validations.otherTypeNameIsRequiredErrorMessage',
-              {
-                defaultMessage: 'The type name is required.',
-              }
-            )
-          ),
-        },
-      ],
-    }}
-    component={TextField}
-  />
+  <UseField path="subType" config={fieldConfig} component={TextField} />
 );
