2b7d5aaef0be85ee332a1aa6de0df71787f440c8
Thomas Watson <w@tson.dk>
2020-04-06 09:29:50 +0200

    [test] Avoid 'no cipher match' in http_server test
    In Node.js 12 using an invalid cipher will result in a process level
    warning. This wasn't the case in Node.js 10. Simply use a valid cipher
    to avoid this issue.

--- src/core/server/http/http_server.test.ts
+++ src/core/server/http/http_server.test.ts
@@ -79,7 +79,7 @@ beforeEach(() => {
     ssl: {
       enabled: true,
       certificate,
-      cipherSuites: ['cipherSuite'],
+      cipherSuites: ['TLS_AES_256_GCM_SHA384'],
       getSecureOptions: () => 0,
       key,
       redirectHttpFromPort: config.port + 1,
