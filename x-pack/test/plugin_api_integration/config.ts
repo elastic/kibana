e7a4ca261b17418e32567b0f69fd842ffc989318
Gidi Meir Morris <github@gidi.io>
2020-04-06 18:02:58 +0100

    [Event Log] adds query support to the Event Log (#62015)
    * added Start api on Event Log plugin
    
    * added empty skeleton for Event Log FTs
    
    * added functional test to public find events api
    
    * added test for pagination
    
    * fixed unit tests
    
    * added support for date ranges
    
    * removed unused code
    
    * replaces valdiation typing
    
    * Revert "replaces valdiation typing"
    
    This reverts commit 711c098e9b2a8329c58c9674fc99de23842884d1.
    
    * replaces match with term
    
    * added sorting
    
    * fixed saved objects nested query
    
    * updated plugin FTs path
    
    * Update x-pack/plugins/encrypted_saved_objects/README.md
    
    Co-Authored-By: Aleh Zasypkin <aleh.zasypkin@gmail.com>
    
    * Update x-pack/plugins/encrypted_saved_objects/README.md
    
    Co-Authored-By: Aleh Zasypkin <aleh.zasypkin@gmail.com>
    
    * remofed validation from tests
    
    * fixed typos
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Aleh Zasypkin <aleh.zasypkin@gmail.com>

--- x-pack/test/plugin_api_integration/config.ts
+++ x-pack/test/plugin_api_integration/config.ts
@@ -6,9 +6,10 @@
 
 import path from 'path';
 import fs from 'fs';
+import { FtrConfigProviderContext } from '@kbn/test/types/ftr';
 import { services } from './services';
 
-export default async function({ readConfigFile }) {
+export default async function({ readConfigFile }: FtrConfigProviderContext) {
   const integrationConfig = await readConfigFile(require.resolve('../api_integration/config'));
 
   // Find all folders in ./plugins since we treat all them as plugin folder
@@ -18,7 +19,10 @@ export default async function({ readConfigFile }) {
   );
 
   return {
-    testFiles: [require.resolve('./test_suites/task_manager')],
+    testFiles: [
+      require.resolve('./test_suites/task_manager'),
+      require.resolve('./test_suites/event_log'),
+    ],
     services,
     servers: integrationConfig.get('servers'),
     esTestCluster: integrationConfig.get('esTestCluster'),
@@ -34,6 +38,9 @@ export default async function({ readConfigFile }) {
       ...integrationConfig.get('kbnTestServer'),
       serverArgs: [
         ...integrationConfig.get('kbnTestServer.serverArgs'),
+        '--xpack.eventLog.enabled=true',
+        '--xpack.eventLog.logEntries=true',
+        '--xpack.eventLog.indexEntries=true',
         ...plugins.map(
           pluginDir => `--plugin-path=${path.resolve(__dirname, 'plugins', pluginDir)}`
         ),
