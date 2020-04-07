da65b8e82826eadf464a3c19d9b77fa45560a26f
Wylie Conlon <wylieconlon@gmail.com>
2020-04-07 18:45:19 -0400

    [Lens] [7.7] Migration for 7.7

---
+++ x-pack/legacy/plugins/lens/migrations.ts
@@ -0,0 +1,67 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { cloneDeep } from 'lodash';
+import { SimpleSavedObject } from 'src/core/public';
+
+interface RawLensSavedXYObject770 {
+  type: 'lens';
+  attributes: {
+    visualizationType: string;
+    state: {
+      datasourceStates?: {
+        indexpattern?: {
+          layers: Record<string, { columns: Record<string, unknown> }>;
+        };
+      };
+      visualization: {
+        layers: Array<{
+          layerId: string;
+          accessors: string[];
+          xAccessor: string;
+          splitAccessor: string;
+        }>;
+      };
+    };
+  };
+}
+
+type LensSavedXYObjectPost770 = RawLensSavedXYObject770;
+
+function isLensSavedXY770(
+  doc: SimpleSavedObject | RawLensSavedXYObject770
+): doc is RawLensSavedXYObject770 {
+  return (
+    doc.type === 'lens' &&
+    doc.attributes &&
+    (doc.attributes as Record<string, string>).visualizationType === 'lnsXY'
+  );
+}
+
+export const migrations = {
+  '7.7.0': (
+    doc: SimpleSavedObject | RawLensSavedXYObject770
+  ): SimpleSavedObject | LensSavedXYObjectPost770 => {
+    const newDoc = cloneDeep(doc);
+    if (!isLensSavedXY770(newDoc)) {
+      return newDoc;
+    }
+    const datasourceState = newDoc.attributes.state?.datasourceStates?.indexpattern;
+    const datasourceLayers = datasourceState?.layers ?? {};
+    const xyState = newDoc.attributes.state?.visualization;
+    newDoc.attributes.state.visualization.layers = xyState.layers.map(layer => {
+      const layerId = layer.layerId;
+      const datasource = datasourceLayers[layerId];
+      return {
+        ...layer,
+        xAccessor: datasource?.columns[layer.xAccessor] ? layer.xAccessor : undefined,
+        splitAccessor: datasource?.columns[layer.splitAccessor] ? layer.splitAccessor : undefined,
+        accessors: layer.accessors.filter(accessor => !!datasource?.columns[accessor]),
+      };
+    }) as typeof xyState.layers;
+    return newDoc;
+  },
+};
