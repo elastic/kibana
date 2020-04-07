49b2e1a2965ae84e64973220c2e035e1761915bd
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2020-04-07 17:38:49 +0100

    Proper type for UsageCollectionSetup in monitoring

--- x-pack/plugins/monitoring/server/kibana_monitoring/collectors/index.ts
+++ x-pack/plugins/monitoring/server/kibana_monitoring/collectors/index.ts
@@ -4,9 +4,13 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { UsageCollectionSetup } from 'src/plugins/usage_collection/server';
 import { getSettingsCollector } from './get_settings_collector';
 import { MonitoringConfig } from '../../config';
 
-export function registerCollectors(usageCollection: any, config: MonitoringConfig) {
+export function registerCollectors(
+  usageCollection: UsageCollectionSetup,
+  config: MonitoringConfig
+) {
   usageCollection.registerCollector(getSettingsCollector(usageCollection, config));
 }
