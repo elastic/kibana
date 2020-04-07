369183f21b670c830dcdbc517688aec9d9c7a141
Justin Kambic <justin.kambic@elastic.co>
2020-04-07 15:04:57 -0400

    Update URL generator function tests to use inline snapshots instead of snapshot files.

--- x-pack/legacy/plugins/uptime/public/lib/helper/observability_integration/__tests__/get_logging_href.test.ts
+++ x-pack/legacy/plugins/uptime/public/lib/helper/observability_integration/__tests__/get_logging_href.test.ts
@@ -33,10 +33,10 @@ describe('getLoggingHref', () => {
                 uid: 'test-pod-id',
               },
             },
-            timestamp: '123',
+            '@timestamp': 123,
           },
         ],
-        timestamp: 123,
+        '@timestamp': '123',
         url: {},
       },
     };
@@ -45,37 +45,49 @@ describe('getLoggingHref', () => {
   it('creates a container href with base path when present', () => {
     const result = getLoggingContainerHref(summary, 'bar');
     expect(result).not.toBeUndefined();
-    expect(result).toMatchSnapshot();
+    expect(result).toMatchInlineSnapshot(
+      `"bar/app/logs?logFilter=(expression:'container.id%20:%20test-container-id',kind:kuery)"`
+    );
   });
 
   it(`creates a container href without a base path if it's an empty string`, () => {
     const result = getLoggingContainerHref(summary, '');
     expect(result).not.toBeUndefined();
-    expect(result).toMatchSnapshot();
+    expect(result).toMatchInlineSnapshot(
+      `"/app/logs?logFilter=(expression:'container.id%20:%20test-container-id',kind:kuery)"`
+    );
   });
 
   it(`creates an ip href with base path when present`, () => {
     const result = getLoggingKubernetesHref(summary, 'bar');
     expect(result).not.toBeUndefined();
-    expect(result).toMatchSnapshot();
+    expect(result).toMatchInlineSnapshot(
+      `"bar/app/logs?logFilter=(expression:'pod.uid%20:%20test-pod-id',kind:kuery)"`
+    );
   });
 
   it('creates a pod href with base path when present', () => {
     const result = getLoggingKubernetesHref(summary, 'bar');
     expect(result).not.toBeUndefined();
-    expect(result).toMatchSnapshot();
+    expect(result).toMatchInlineSnapshot(
+      `"bar/app/logs?logFilter=(expression:'pod.uid%20:%20test-pod-id',kind:kuery)"`
+    );
   });
 
   it(`creates a pod href without a base path when it's an empty string`, () => {
     const result = getLoggingKubernetesHref(summary, '');
     expect(result).not.toBeUndefined();
-    expect(result).toMatchSnapshot();
+    expect(result).toMatchInlineSnapshot(
+      `"/app/logs?logFilter=(expression:'pod.uid%20:%20test-pod-id',kind:kuery)"`
+    );
   });
 
   it(`creates an ip href without a base path when it's an empty string`, () => {
     const result = getLoggingIpHref(summary, '');
     expect(result).not.toBeUndefined();
-    expect(result).toMatchSnapshot();
+    expect(result).toMatchInlineSnapshot(
+      `"/app/logs?logFilter=(expression:'host.ip%20%3A%20151.101.202.217',kind:kuery)"`
+    );
   });
 
   it('returns undefined if necessary container is not present', () => {
