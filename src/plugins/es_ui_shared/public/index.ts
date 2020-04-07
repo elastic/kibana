5eed7a957dd9b1f7789727d0087500b789269033
spalger <spalger@users.noreply.github.com>
2020-04-07 15:20:00 -0700

    fix typo

--- src/plugins/es_ui_shared/public/index.ts
+++ src/plugins/es_ui_shared/public/index.ts
@@ -36,9 +36,9 @@ export { indices } from './indices';
 
 export { useUIAceKeyboardMode } from './use_ui_ace_keyboard_mode';
 
-/** dummy plugin, we just want kibanaReact to have its own bundle */
+/** dummy plugin, we just want esUiShared to have its own bundle */
 export function plugin() {
-  return new (class KibanaReactPlugin {
+  return new (class EsUiSharedPlugin {
     setup() {}
     start() {}
   })();
