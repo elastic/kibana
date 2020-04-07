2bec29e867598d91c6a38a9f5c5d782a4bf79e50
Dmitry Lemeshko <dzmitry.lemechko@elastic.co>
2020-04-07 20:10:51 +0300

    FTR: Enable w3c for chromedriver (#62542) (#62805)
    * enable w3c for chrome
    
    * update maps tests
    
    * update maps tests
    
    * update common_page
    
    * Revert "update maps tests"
    
    This reverts commit 31f43fd6788546cf76534e3eea2390e882be2283.
    
    * revert changes to maps tests
    
    * undo after removal
    
    * update expect range to pass on Windows, unskip tests for Firefox
    
    * print out value for discover brushing test
    
    * log first timestamp
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- test/functional/page_objects/discover_page.ts
+++ test/functional/page_objects/discover_page.ts
@@ -138,7 +138,7 @@ export function DiscoverPageProvider({ getService, getPageObjects }: FtrProvider
 
       await browser
         .getActions()
-        .move({ x: 200, y: 20, origin: el._webElement })
+        .move({ x: 0, y: 20, origin: el._webElement })
         .click()
         .perform();
     }
@@ -147,8 +147,8 @@ export function DiscoverPageProvider({ getService, getPageObjects }: FtrProvider
       const el = await elasticChart.getCanvas();
 
       await browser.dragAndDrop(
-        { location: el, offset: { x: 200, y: 20 } },
-        { location: el, offset: { x: 400, y: 30 } }
+        { location: el, offset: { x: -300, y: 20 } },
+        { location: el, offset: { x: -100, y: 30 } }
       );
     }
 
