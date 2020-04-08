07055cb960b912e0db3a223d5e177dad1e585c86
Justin Kambic <justin.kambic@elastic.co>
2020-04-08 17:16:40 -0400

    Add cert API request and runtime type checking.

--- x-pack/plugins/uptime/server/lib/requests/uptime_requests.ts
+++ x-pack/plugins/uptime/server/lib/requests/uptime_requests.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { GetCertsParams } from './get_certs';
 import { UMElasticsearchQueryFn } from '../adapters';
 import {
   HistogramResult,
