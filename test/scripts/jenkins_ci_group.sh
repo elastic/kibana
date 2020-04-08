8bf3e44216f9310f2beecac93f404179d19fa009
spalger <spalger@users.noreply.github.com>
2020-04-07 19:13:05 -0700

    just build oss and xpack
    (cherry picked from commit cced893a1f6ce8e5631ebb4e2d9c4cb3a1d9c9df)

--- test/scripts/jenkins_ci_group.sh
+++ test/scripts/jenkins_ci_group.sh
@@ -1,12 +1,1 @@
 #!/usr/bin/env bash
-
-source test/scripts/jenkins_test_setup_oss.sh
-
-checks-reporter-with-killswitch "Functional tests / Group ${CI_GROUP}" yarn run grunt "run:functionalTests_ciGroup${CI_GROUP}";
-
-if [ "$CI_GROUP" == "1" ]; then
-  source test/scripts/jenkins_build_kbn_tp_sample_panel_action.sh
-  yarn run grunt run:pluginFunctionalTestsRelease --from=source;
-  yarn run grunt run:exampleFunctionalTestsRelease --from=source;
-  yarn run grunt run:interpreterFunctionalTestsRelease;
-fi
