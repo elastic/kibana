b3b50c1dc1eb1034746fc89f420e4c1411b6ac47
Yara Tercero <yara.tercero@elastic.co>
2020-04-08 10:43:21 -0400

    wip - building out lists query

--- x-pack/legacy/plugins/siem/server/lib/detection_engine/signals/get_filter.ts
+++ x-pack/legacy/plugins/siem/server/lib/detection_engine/signals/get_filter.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { isEmpty } from 'lodash';
 import { AlertServices } from '../../../../../../../plugins/alerting/server';
 import { assertUnreachable } from '../../../utils/build_query';
 import {
@@ -16,18 +17,84 @@ import {
 import { PartialFilter, RuleAlertParams } from '../types';
 import { BadRequestError } from '../errors/bad_request_error';
 
+export const evaluateValues = listItem => {
+  const queryBuilder = [];
+  if (listItem.values_operator === 'excluded') {
+    queryBuilder.push(`not ${listItem.field}: `);
+  } else {
+    queryBuilder.push(`${listItem.field}: `);
+  }
+
+  switch (listItem.values_type) {
+    case 'exists':
+      queryBuilder.push('*');
+      break;
+    case 'match':
+      queryBuilder.push(listItem.values[0].name);
+      break;
+    case 'match_all':
+      const length = listItem.values?.length;
+      listItem.values?.forEach((element, index) => {
+        if (index !== length - 1) {
+          queryBuilder.push(index === 0 ? `(${element.name} or ` : `${element.name} or `);
+        } else {
+          queryBuilder.push(`${element.name})`);
+        }
+      });
+      break;
+    default:
+      break;
+  }
+
+  return queryBuilder.join('');
+};
+
+export const buildQueryExceptions = (
+  query: string,
+  language: string,
+  lists: RuleAlertParams['lists']
+): Query[] => {
+  if (!isEmpty(lists)) {
+    const queries: query[] = lists?.reduce((acc, listItem, i) => {
+      const queryBuilder = ['('];
+
+      queryBuilder.push(evaluateValues(listItem));
+
+      if (listItem.and) {
+        listItem.and.forEach(item => {
+          queryBuilder.push(`and ${evaluateValues(item)}`);
+        });
+      }
+
+      queryBuilder.push(')');
+
+      if (lists.length > 1 && i !== lists.length - 1) {
+        queryBuilder.push(' or ');
+        return `${acc}${queryBuilder.join('')}`;
+      }
+
+      return `${acc}${queryBuilder.join('')}`;
+    }, `${query} and not `);
+
+    return [{ query: queries, language }];
+  } else {
+    return [{ query, language }];
+  }
+};
+
 export const getQueryFilter = (
   query: string,
   language: string,
   filters: PartialFilter[],
-  index: string[]
+  index: string[],
+  lists: RuleAlertParams['lists']
 ) => {
   const indexPattern = {
     fields: [],
     title: index.join(),
   } as IIndexPattern;
 
-  const queries: Query[] = [{ query, language }];
+  const queries: Query[] = buildQueryExceptions(query, language, lists);
   const config = {
     allowLeadingWildcards: true,
     queryStringOptions: { analyze_wildcard: true },
@@ -39,7 +106,8 @@ export const getQueryFilter = (
     f => f && !esFilters.isFilterDisabled(f)
   );
 
-  return esQuery.buildEsQuery(indexPattern, queries, enabledFilters, config);
+  const a = esQuery.buildEsQuery(indexPattern, queries, enabledFilters, config);
+  return a;
 };
 
 interface GetFilterArgs {
@@ -50,6 +118,7 @@ interface GetFilterArgs {
   savedId: string | undefined | null;
   services: AlertServices;
   index: string[] | undefined | null;
+  lists: RuleAlertParams['lists'];
 }
 
 interface QueryAttributes {
@@ -69,11 +138,12 @@ export const getFilter = async ({
   services,
   type,
   query,
+  lists,
 }: GetFilterArgs): Promise<unknown> => {
   switch (type) {
     case 'query': {
       if (query != null && language != null && index != null) {
-        return getQueryFilter(query, language, filters || [], index);
+        return getQueryFilter(query, language, filters || [], index, lists);
       } else {
         throw new BadRequestError('query, filters, and index parameter should be defined');
       }
@@ -90,13 +160,14 @@ export const getFilter = async ({
             savedObject.attributes.query.query,
             savedObject.attributes.query.language,
             savedObject.attributes.filters,
-            index
+            index,
+            lists
           );
         } catch (err) {
           // saved object does not exist, so try and fall back if the user pushed
           // any additional language, query, filters, etc...
           if (query != null && language != null && index != null) {
-            return getQueryFilter(query, language, filters || [], index);
+            return getQueryFilter(query, language, filters || [], index, lists);
           } else {
             // user did not give any additional fall back mechanism for generating a rule
             // rethrow error for activity monitoring
