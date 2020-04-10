adf02e3e72e3e083e71d401a179cd206ad1d9ce1
Justin Kambic <justin.kambic@elastic.co>
2020-04-10 09:13:02 -0400

    Delete unnecessary helper code.

--- x-pack/legacy/plugins/uptime/public/state/api/ping.ts
+++ x-pack/legacy/plugins/uptime/public/state/api/ping.ts
@@ -15,22 +15,6 @@ import {
 import { apiService } from './utils';
 import { API_URLS } from '../../../common/constants/rest_api';
 
-export const mergeParams = (
-  params: Record<string, any>,
-  maybeParams: Record<string, any>
-): Record<string, any> => {
-  let definedParams = params;
-  Object.keys(maybeParams).forEach(param => {
-    if (maybeParams[param]) {
-      definedParams = {
-        ...definedParams,
-        [param]: maybeParams[param],
-      };
-    }
-  });
-  return definedParams;
-};
-
 export const fetchPings: APIFn<GetPingsParams, PingsResponse> = async ({
   dateRangeStart,
   dateRangeEnd,
@@ -40,10 +24,8 @@ export const fetchPings: APIFn<GetPingsParams, PingsResponse> = async ({
   sort,
   status,
 }) => {
-  const params = mergeParams(
-    { dateRangeStart, dateRangeEnd, monitorId },
-    { location, size, sort, status }
-  );
+  const params = { dateRangeStart, dateRangeEnd, monitorId, location, size, sort, status };
+
   return await apiService.get(API_URLS.PINGS, params, PingsResponseType);
 };
 
@@ -54,17 +36,13 @@ export const fetchPingHistogram: APIFn<GetPingHistogramParams, HistogramResult> 
   statusFilter,
   filters,
 }) => {
-  const queryParams = mergeParams(
-    {
-      dateStart,
-      dateEnd,
-    },
-    {
-      monitorId,
-      statusFilter,
-      filters,
-    }
-  );
+  const queryParams = {
+    dateStart,
+    dateEnd,
+    monitorId,
+    statusFilter,
+    filters,
+  };
 
   return await apiService.get(API_URLS.PING_HISTOGRAM, queryParams);
 };
