3e92310f160d82503a1d8a47b1cea78e3ce1d6d9
Timothy Sullivan <tsullivan@elastic.co>
2020-04-10 14:28:00 -0700

    diff prettiness

--- x-pack/legacy/plugins/reporting/server/config/index.ts
+++ x-pack/legacy/plugins/reporting/server/config/index.ts
@@ -8,6 +8,7 @@ import { Legacy } from 'kibana';
 import { CoreSetup } from 'src/core/server';
 import { get } from 'lodash';
 import { ConfigType as ReportingConfigType } from '../../../../../plugins/reporting/server';
+export { ReportingConfigType };
 
 // make config.get() aware of the value type it returns
 interface Config<BaseType> {
@@ -84,5 +85,3 @@ export const buildConfig = (
     },
   };
 };
-
-export { ReportingConfigType };
