dfae263cf2a9e01ac6d8ec743625667a2ce9049a
Corey Robertson <corey.robertson@elastic.co>
2020-04-13 09:39:40 -0400

    Typecheck fix

--- x-pack/legacy/plugins/canvas/public/lib/element_handler_creators.ts
+++ x-pack/legacy/plugins/canvas/public/lib/element_handler_creators.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Http2ServerResponse } from 'http2';
 import { camelCase } from 'lodash';
 // @ts-ignore unconverted local file
 import { getClipboardData, setClipboardData } from './clipboard';
