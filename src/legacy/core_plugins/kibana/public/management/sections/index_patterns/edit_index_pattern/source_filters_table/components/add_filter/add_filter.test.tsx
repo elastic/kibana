da59c1ce029077efaf54351a6b8672b0b81465fb
Alexey Antonov <alexwizp@gmail.com>
2020-04-13 18:59:48 +0300

    Index pattern management UI -> TypeScript (source_filters_table) (#62925)
    * [WIP] Index pattern management UI -> TypeScript (source_filters_table)

--- src/legacy/core_plugins/kibana/public/management/sections/index_patterns/edit_index_pattern/source_filters_table/components/add_filter/add_filter.test.tsx
+++ src/legacy/core_plugins/kibana/public/management/sections/index_patterns/edit_index_pattern/source_filters_table/components/add_filter/add_filter.test.tsx
@@ -20,11 +20,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { Header } from '../header';
+import { AddFilter } from './add_filter';
 
-describe('Header', () => {
-  it('should render normally', async () => {
-    const component = shallow(<Header />);
+describe('AddFilter', () => {
+  test('should render normally', () => {
+    const component = shallow(<AddFilter onAddFilter={() => {}} />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  test('should allow adding a filter', async () => {
+    const onAddFilter = jest.fn();
+    const component = shallow(<AddFilter onAddFilter={onAddFilter} />);
+
+    component.find('EuiFieldText').simulate('change', { target: { value: 'tim*' } });
+    component.find('EuiButton').simulate('click');
+    component.update();
+
+    expect(onAddFilter).toBeCalledWith('tim*');
+  });
+
+  test('should ignore strings with just spaces', () => {
+    const component = shallow(<AddFilter onAddFilter={() => {}} />);
+
+    // Set a value in the input field
+    component.find('EuiFieldText').simulate('keypress', ' ');
+    component.update();
 
     expect(component).toMatchSnapshot();
   });
