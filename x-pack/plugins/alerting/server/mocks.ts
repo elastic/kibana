d2265d9636e77019cb9a7f16e69deb09654d122e
Gidi Meir Morris <github@gidi.io>
2020-04-14 18:09:06 +0100

    added alert services mock and use it in siem

--- x-pack/plugins/alerting/server/mocks.ts
+++ x-pack/plugins/alerting/server/mocks.ts
@@ -6,6 +6,9 @@
 
 import { alertsClientMock } from './alerts_client.mock';
 import { PluginSetupContract, PluginStartContract } from './plugin';
+import { savedObjectsClientMock } from '../../../../src/core/server/mocks';
+import { createScopedSeachApiMock } from '../../../../src/plugins/data/server/mocks';
+import { AlertInstance } from './alert_instance';
 
 export { alertsClientMock };
 
@@ -24,7 +27,45 @@ const createStartMock = () => {
   return mock;
 };
 
+const createAlertInstanceFactoryMock = () => {
+  const mock = {
+    hasScheduledActions: jest.fn(),
+    isThrottled: jest.fn(),
+    getScheduledActionOptions: jest.fn(),
+    unscheduleActions: jest.fn(),
+    getState: jest.fn(),
+    scheduleActions: jest.fn(),
+    replaceState: jest.fn(),
+    updateLastScheduledActions: jest.fn(),
+    toJSON: jest.fn(),
+    toRaw: jest.fn(),
+  };
+
+  // support chaining
+  mock.replaceState.mockReturnValue(mock);
+  mock.unscheduleActions.mockReturnValue(mock);
+  mock.scheduleActions.mockReturnValue(mock);
+
+  return (mock as unknown) as jest.Mocked<AlertInstance>;
+};
+
+const createAlertServicesMock = () => {
+  const alertInstanceFactoryMock = createAlertInstanceFactoryMock();
+  return {
+    alertInstanceFactory: jest
+      .fn<jest.Mocked<AlertInstance>, []>()
+      .mockReturnValue(alertInstanceFactoryMock),
+    callCluster: jest.fn(),
+    savedObjectsClient: savedObjectsClientMock.create(),
+    search: createScopedSeachApiMock().search,
+  };
+};
+
+export type AlertServicesMock = ReturnType<typeof createAlertServicesMock>;
+
 export const alertsMock = {
+  createAlertInstanceFactory: createAlertInstanceFactoryMock,
   createSetup: createSetupMock,
   createStart: createStartMock,
+  createAlertServices: createAlertServicesMock,
 };
