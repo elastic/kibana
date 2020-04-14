5afac07c8d5d0be27d8262d9dba8634c61f7f368
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-04-14 15:36:36 -0600

    Move logic to NP and add basic plugin structure

---
+++ x-pack/plugins/maps_legacy_licensing/public/plugin.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+// @ts-ignore
+import { CoreSetup, CoreStart, Plugin } from 'kibana/server';
+
+/**
+ * These are the interfaces with your public contracts. You should export these
+ * for other plugins to use in _their_ `SetupDeps`/`StartDeps` interfaces.
+ * @public
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface MapsLegacyLicensingSetupDependencies {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface MapsLegacyLicensingStartDependencies {}
+
+export type MapsLegacyLicensingSetup = ReturnType<MapsLegacyLicensing['setup']>;
+export type MapsLegacyLicensingStart = ReturnType<MapsLegacyLicensing['start']>;
+
+export class MapsLegacyLicensing
+  implements Plugin<MapsLegacyLicensingSetup, MapsLegacyLicensingStart> {
+  public setup(core: CoreSetup, plugins: MapsLegacyLicensingSetupDependencies) {}
+
+  public start(core: CoreStart, plugins: MapsLegacyLicensingStartDependencies) {}
+}
