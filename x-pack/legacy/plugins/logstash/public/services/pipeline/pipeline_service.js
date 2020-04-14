2ce9843ce42085c4911ee92be2b5e6f09022c157
Mikhail Shustov <restrry@gmail.com>
2020-04-14 20:02:48 +0200

    [NP] Migrate logstash server side code to NP (#63135) (#63477)
    * convert api_integration test into TS
    
    * create logstash NP plugin and move models
    
    * move common/constants to NP
    
    * type fetch all from scroll
    
    * move route declaration to NP
    
    * add licence checker wrapper
    
    * register logstash route handlers in NP
    
    * track logstash NP i18n
    
    * address shaunak comment
    
    * fix validation
    
    * udpdate security tests since for new mock defaults
    
    * address Pierres comments
    
    * rename upgrade file route

--- x-pack/legacy/plugins/logstash/public/services/pipeline/pipeline_service.js
+++ x-pack/legacy/plugins/logstash/public/services/pipeline/pipeline_service.js
@@ -5,7 +5,7 @@
  */
 
 import chrome from 'ui/chrome';
-import { ROUTES } from '../../../common/constants';
+import { ROUTES } from '../../../../../../plugins/logstash/common/constants';
 import { Pipeline } from 'plugins/logstash/models/pipeline';
 
 export class PipelineService {
