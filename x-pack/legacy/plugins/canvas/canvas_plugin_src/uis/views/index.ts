b06d539713b5311aa0399b21f16a0f0f174bb8c5
Corey Robertson <corey.robertson@elastic.co>
2020-04-14 09:22:01 -0400

    Typecheck

--- x-pack/legacy/plugins/canvas/canvas_plugin_src/uis/views/index.ts
+++ x-pack/legacy/plugins/canvas/canvas_plugin_src/uis/views/index.ts
@@ -4,19 +4,33 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+// @ts-ignore untyped local
 import { dropdownControl } from './dropdownControl';
+// @ts-ignore untyped local
 import { getCell } from './getCell';
+// @ts-ignore untyped local
 import { image } from './image';
+// @ts-ignore untyped local
 import { markdown } from './markdown';
+// @ts-ignore untyped local
 import { metricInitializer } from './metric';
+// @ts-ignore untyped local
 import { pie } from './pie';
+// @ts-ignore untyped local
 import { plot } from './plot';
+// @ts-ignore untyped local
 import { progress } from './progress';
+// @ts-ignore untyped local
 import { repeatImage } from './repeatImage';
+// @ts-ignore untyped local
 import { revealImage } from './revealImage';
+// @ts-ignore untyped local
 import { render } from './render';
+// @ts-ignore untyped local
 import { shape } from './shape';
+// @ts-ignore untyped local
 import { table } from './table';
+// @ts-ignore untyped local
 import { timefilterControl } from './timefilterControl';
 
 import { SetupInitializer } from '../../plugin';
@@ -39,6 +53,6 @@ export const viewSpecs = [
 
 export const viewInitializers = [metricInitializer];
 
-export const initializeViews: SetupInitializer<unknown> = (core, plugins) => {
+export const initializeViews: SetupInitializer<unknown[]> = (core, plugins) => {
   return [...viewSpecs, ...viewInitializers.map(initializer => initializer(core, plugins))];
 };
