647f798d76e12b662f5d90886efe0b6a0b7b85ec
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-04-16 00:35:11 +0200

    Add `moduleFileExtensions`

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   setupFiles: ['./src/test/setupFiles/automatic-mocks.ts'],
   preset: 'ts-jest',
   testRegex: '(test|src)/.*test.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json'],
   globals: {
     'ts-jest': {
       diagnostics: false,
