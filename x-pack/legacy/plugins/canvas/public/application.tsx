6fce91f84a5bbb203bd7bb2d1f53db8f6ca48788
Corey Robertson <corey.robertson@elastic.co>
2020-04-15 09:27:19 -0400

    Fixes Keyboard Shortcuts help extension

--- x-pack/legacy/plugins/canvas/public/application.tsx
+++ x-pack/legacy/plugins/canvas/public/application.tsx
@@ -104,8 +104,9 @@ export const initializeCanvas = async (
         href: getDocumentationLinks().canvas,
       },
     ],
-    content: domNode => () => {
+    content: domNode => {
       ReactDOM.render(<HelpMenu />, domNode);
+      return () => ReactDOM.unmountComponentAtNode(domNode);
     },
   });
 
