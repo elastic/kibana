868099ea4d48c753edc9d5a317cfe3ab93f9206e
Justin Kambic <justin.kambic@elastic.co>
2020-04-15 15:57:47 -0400

    [Uptime] Remove pings graphql (#59392) (#63625)
    * WIP replacing GQL with redux/rest.
    
    * Finish implementing migration.
    
    * Introduce new connected component for ping list.
    
    * Replace GraphQL type with io-ts.
    
    * Update some broken tests.
    
    * Add test for new helper function.
    
    * Write test snapshots.
    
    * Migrate api tests from graphql to rest.
    
    * Update fixtures that rely on pings.
    
    * Move ping types to runtime_types folder with rest of io-ts files.
    
    * Update Ping type location and imports, type checking.
    
    * Remove reliance on fixtures for ping functional API tests.
    
    * Fix broken unit tests.
    
    * Fix broken types.
    
    * Remove local state storage from parent components.
    
    * Add functional test to cover Ping List functionality.
    
    * Fix monitor page functional test that was broken by merge conflicts.
    
    * Fix broken tests.
    
    * Fix broken API test.
    
    * Replace a test with a describe block that will pre-navigate all tests.
    
    * Delete unused reducer keys.
    
    * Re-introduce loading to ping list reducer.
    
    * Inroduce code that will cause PingList to re-fetch when refresh button is pressed.
    
    * Update expanded rows to support multiple concurrent expanded rows.
    
    * Modify pingList reducer to have singular optional error field.
    
    * Delete unnecessary helper code.
    
    * Delete unused interface.
    
    * Add runtime_type to parse getPings params, fix pagination index.
    
    * Add dedicated monitor type to runtime_types.
    
    * Fix broken tests.
    
    * Fix broken tests.
    
    * Rename '@timestamp' property to 'timestamp' on Ping type.
    
    * Fix broken type and key pings list table on document ID instead of timestamp.
    
    * Fix broken unit tests.
    
    * Fix broken tests and types.
    
    * Fix broken functional test.

--- x-pack/legacy/plugins/uptime/common/runtime_types/ping/index.ts
+++ x-pack/legacy/plugins/uptime/common/runtime_types/ping/index.ts
@@ -4,4 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { pingsSchema } from './schema.gql';
+export * from './histogram';
+export * from './ping';
