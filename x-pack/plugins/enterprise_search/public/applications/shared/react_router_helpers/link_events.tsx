5eb89b9e3938fa39894b05b534e6b405c3fac942
Constance Chen <constance.chen.3@gmail.com>
2020-04-16 13:40:10 -0700

    Convert showSetupGuide action/flag to a React Router link
    - remove showSetupGuide flag
    - add a new shared helper component for combining EuiButton/EuiLink with React Router behavior (https://github.com/elastic/eui/blob/master/wiki/react-router.md#react-router-51)

---
+++ x-pack/plugins/enterprise_search/public/applications/shared/react_router_helpers/link_events.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { SyntheticEvent } from 'react';
+
+/**
+ * Helper functions for determining which events we should
+ * let browsers handle natively, e.g. new tabs/windows
+ */
+
+type THandleEvent = (event: SyntheticEvent) => boolean;
+
+export const letBrowserHandleEvent: THandleEvent = event =>
+  event.defaultPrevented ||
+  isModifiedEvent(event) ||
+  !isLeftClickEvent(event) ||
+  isTargetBlank(event);
+
+const isModifiedEvent: THandleEvent = event =>
+  !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey);
+
+const isLeftClickEvent: THandleEvent = event => event.button === 0;
+
+const isTargetBlank: THandleEvent = event => {
+  const target = event.target.getAttribute('target');
+  return target && target !== '_self';
+};
