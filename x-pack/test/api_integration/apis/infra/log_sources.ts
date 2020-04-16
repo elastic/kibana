86337299f95cf2f6fbe1808f37627dae1d89b618
Felix Stürmer <stuermer@weltenwort.de>
2020-04-17 00:48:34 +0200

    Add tests for source config PATCH route

--- x-pack/test/api_integration/apis/infra/log_sources.ts
+++ x-pack/test/api_integration/apis/infra/log_sources.ts
@@ -5,25 +5,17 @@
  */
 
 import expect from '@kbn/expect';
-import { FtrProviderContext } from '../../ftr_provider_context';
+import { beforeEach } from 'mocha';
 import {
   getLogSourceConfigurationSuccessResponsePayloadRT,
-  getLogSourceConfigurationPath,
+  patchLogSourceConfigurationSuccessResponsePayloadRT,
 } from '../../../../plugins/infra/common/http_api/log_sources';
 import { decodeOrThrow } from '../../../../plugins/infra/common/runtime_types';
+import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function({ getService }: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
-  const supertest = getService('supertest');
-
-  const getLogSourceConfiguration = (sourceId: string) => {
-    return supertest
-      .get(getLogSourceConfigurationPath(sourceId))
-      .set({
-        'kbn-xsrf': 'some-xsrf-token',
-      })
-      .send();
-  };
+  const logSourceConfiguration = getService('infraLogSourceConfiguration');
 
   describe('log sources api', () => {
     before(() => esArchiver.load('infra/metrics_and_logs'));
@@ -31,18 +23,153 @@ export default function({ getService }: FtrProviderContext) {
     beforeEach(() => esArchiver.load('empty_kibana'));
     afterEach(() => esArchiver.unload('empty_kibana'));
 
-    describe('get source configuration', () => {
-      it('returns the default source configuration when none has been saved', async () => {
-        const response = await getLogSourceConfiguration('default').send();
+    describe('source configuration get method for non-existant source', () => {
+      it('returns the default source configuration', async () => {
+        const response = await logSourceConfiguration
+          .createGetLogSourceConfigurationAgent('default')
+          .expect(200);
 
         const {
           data: { configuration, origin },
         } = decodeOrThrow(getLogSourceConfigurationSuccessResponsePayloadRT)(response.body);
 
         expect(origin).to.be('fallback');
+        expect(configuration.name).to.be('Default');
         expect(configuration.logAlias).to.be('filebeat-*,kibana_sample_data_logs*');
         expect(configuration.fields.timestamp).to.be('@timestamp');
         expect(configuration.fields.tiebreaker).to.be('_doc');
+        expect(configuration.logColumns[0]).to.have.key('timestampColumn');
+        expect(configuration.logColumns[1]).to.have.key('fieldColumn');
+        expect(configuration.logColumns[2]).to.have.key('messageColumn');
+      });
+    });
+
+    describe('source configuration patch method for non-existant source', () => {
+      it('creates a source configuration', async () => {
+        const response = await logSourceConfiguration
+          .createUpdateLogSourceConfigurationAgent('default', {
+            name: 'NAME',
+            description: 'DESCRIPTION',
+            logAlias: 'filebeat-**',
+            fields: {
+              tiebreaker: 'TIEBREAKER',
+              timestamp: 'TIMESTAMP',
+            },
+            logColumns: [
+              {
+                messageColumn: {
+                  id: 'MESSAGE_COLUMN',
+                },
+              },
+            ],
+          })
+          .expect(200);
+
+        // check direct response
+        const {
+          data: { configuration, origin },
+        } = decodeOrThrow(patchLogSourceConfigurationSuccessResponsePayloadRT)(response.body);
+
+        expect(configuration.name).to.be('NAME');
+        expect(origin).to.be('stored');
+        expect(configuration.logAlias).to.be('filebeat-**');
+        expect(configuration.fields.timestamp).to.be('TIMESTAMP');
+        expect(configuration.fields.tiebreaker).to.be('TIEBREAKER');
+        expect(configuration.logColumns).to.have.length(1);
+        expect(configuration.logColumns[0]).to.have.key('messageColumn');
+
+        // check for persistence
+        const {
+          data: { configuration: persistedConfiguration },
+        } = await logSourceConfiguration.getLogSourceConfiguration('default');
+
+        expect(configuration).to.eql(persistedConfiguration);
+      });
+
+      it('creates a source configuration with default values for unspecified properties', async () => {
+        const response = await logSourceConfiguration
+          .createUpdateLogSourceConfigurationAgent('default', {})
+          .expect(200);
+
+        const {
+          data: { configuration, origin },
+        } = decodeOrThrow(patchLogSourceConfigurationSuccessResponsePayloadRT)(response.body);
+
+        expect(configuration.name).to.be('Default');
+        expect(origin).to.be('stored');
+        expect(configuration.logAlias).to.be('filebeat-*,kibana_sample_data_logs*');
+        expect(configuration.fields.timestamp).to.be('@timestamp');
+        expect(configuration.fields.tiebreaker).to.be('_doc');
+        expect(configuration.logColumns).to.have.length(3);
+        expect(configuration.logColumns[0]).to.have.key('timestampColumn');
+        expect(configuration.logColumns[1]).to.have.key('fieldColumn');
+        expect(configuration.logColumns[2]).to.have.key('messageColumn');
+
+        // check for persistence
+        const {
+          data: { configuration: persistedConfiguration, origin: persistedOrigin },
+        } = await logSourceConfiguration.getLogSourceConfiguration('default');
+
+        expect(persistedOrigin).to.be('stored');
+        expect(configuration).to.eql(persistedConfiguration);
+      });
+    });
+
+    describe('source configuration patch method for existing source', () => {
+      beforeEach(async () => {
+        await logSourceConfiguration.updateLogSourceConfiguration('default', {});
+      });
+
+      it('updates a source configuration', async () => {
+        const response = await logSourceConfiguration
+          .createUpdateLogSourceConfigurationAgent('default', {
+            name: 'NAME',
+            description: 'DESCRIPTION',
+            logAlias: 'filebeat-**',
+            fields: {
+              tiebreaker: 'TIEBREAKER',
+              timestamp: 'TIMESTAMP',
+            },
+            logColumns: [
+              {
+                messageColumn: {
+                  id: 'MESSAGE_COLUMN',
+                },
+              },
+            ],
+          })
+          .expect(200);
+
+        const {
+          data: { configuration, origin },
+        } = decodeOrThrow(patchLogSourceConfigurationSuccessResponsePayloadRT)(response.body);
+
+        expect(configuration.name).to.be('NAME');
+        expect(origin).to.be('stored');
+        expect(configuration.logAlias).to.be('filebeat-**');
+        expect(configuration.fields.timestamp).to.be('TIMESTAMP');
+        expect(configuration.fields.tiebreaker).to.be('TIEBREAKER');
+        expect(configuration.logColumns).to.have.length(1);
+        expect(configuration.logColumns[0]).to.have.key('messageColumn');
+      });
+
+      it('partially updates a source configuration', async () => {
+        const response = await logSourceConfiguration
+          .createUpdateLogSourceConfigurationAgent('default', {
+            name: 'NAME',
+          })
+          .expect(200);
+
+        const {
+          data: { configuration, origin },
+        } = decodeOrThrow(patchLogSourceConfigurationSuccessResponsePayloadRT)(response.body);
+
+        expect(configuration.name).to.be('NAME');
+        expect(origin).to.be('stored');
+        expect(configuration.logAlias).to.be('filebeat-*,kibana_sample_data_logs*');
+        expect(configuration.fields.timestamp).to.be('@timestamp');
+        expect(configuration.fields.tiebreaker).to.be('_doc');
+        expect(configuration.logColumns).to.have.length(3);
         expect(configuration.logColumns[0]).to.have.key('timestampColumn');
         expect(configuration.logColumns[1]).to.have.key('fieldColumn');
         expect(configuration.logColumns[2]).to.have.key('messageColumn');
