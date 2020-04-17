c485f8e9e2453dad5ab2f46f5828da2ff4379485
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-04-17 13:43:44 +0100

    let's build kibana too

--- x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
+++ x-pack/legacy/plugins/apm/e2e/ci/prepare-kibana.sh
@@ -2,12 +2,15 @@
 set -ex
 
 E2E_DIR=x-pack/legacy/plugins/apm/e2e
-echo "1/2 Install dependencies ..."
+echo "1/3 Install dependencies ..."
 # shellcheck disable=SC1091
 source src/dev/ci_setup/setup_env.sh true
 yarn kbn clean && yarn kbn bootstrap
 
-echo "2/2 Start Kibana (production mode) ..."
+echo "2/3 Build kibana ..."
+yarn build --no-oss --skip-os-packages
+
+echo "3/3 Start Kibana (production mode) ..."
 ## Might help to avoid FATAL ERROR: Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory
 export NODE_OPTIONS="--max-old-space-size=4096"
 nohup node ./scripts/kibana --no-base-path --no-watch --config ${E2E_DIR}/ci/kibana.e2e.yml > ${E2E_DIR}/kibana.log 2>&1 &
