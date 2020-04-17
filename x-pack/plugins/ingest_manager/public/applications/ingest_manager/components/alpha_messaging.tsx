431b8a4d37efb03f7c1ac4875840dc0e0fc5adee
Henry Harding <henry.harding@elastic.co>
2020-04-17 14:45:25 -0400

    remove leftover code

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/components/alpha_messaging.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/components/alpha_messaging.tsx
@@ -8,14 +8,12 @@ import styled from 'styled-components';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { EuiText } from '@elastic/eui';
 
-const Message = styled(EuiText)`
-  padding: ${props => props.theme.eui.paddingSizes.m};
-`;
-
-Message.defaultProps = {
+const Message = styled(EuiText).attrs(props => ({
   color: 'subdued',
   textAlign: 'center',
-};
+}))`
+  padding: ${props => props.theme.eui.paddingSizes.m};
+`;
 
 export const AlphaMessaging: React.FC<{}> = () => (
   <Message>
@@ -33,7 +31,6 @@ export const AlphaMessaging: React.FC<{}> = () => (
           defaultMessage="Ingest Manager is under active development and is not
           intended for production purposes."
         />
-        <strong>Alpha release</strong>
       </small>
     </p>
   </Message>
