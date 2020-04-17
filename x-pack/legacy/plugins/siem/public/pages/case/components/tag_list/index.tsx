69f9e82ce3320e461c7e9d6ff1a450d7bbd5d569
stephmilovic <stephanie.milovic@elastic.co>
2020-04-17 11:35:02 -0600

    rm unused test subj

--- x-pack/legacy/plugins/siem/public/pages/case/components/tag_list/index.tsx
+++ x-pack/legacy/plugins/siem/public/pages/case/components/tag_list/index.tsx
@@ -93,7 +93,7 @@ export const TagList = React.memo(
           )}
         </EuiFlexGroup>
         <EuiHorizontalRule margin="xs" />
-        <MyFlexGroup gutterSize="xs" data-test-subj="grr" wrap>
+        <MyFlexGroup gutterSize="xs" wrap>
           {tags.length === 0 && !isEditTags && <p data-test-subj="no-tags">{i18n.NO_TAGS}</p>}
           {tags.length > 0 &&
             !isEditTags &&
