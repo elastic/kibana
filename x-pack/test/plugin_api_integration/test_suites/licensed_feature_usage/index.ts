6a9d9404b61e92e5ecf279596aa565330c3c0863
pgayvallet <pierre.gayvallet@elastic.co>
2020-04-17 08:26:12 +0200

    fix FTR test suite name

--- x-pack/test/plugin_api_integration/test_suites/licensed_feature_usage/index.ts
+++ x-pack/test/plugin_api_integration/test_suites/licensed_feature_usage/index.ts
@@ -7,7 +7,7 @@
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function({ loadTestFile }: FtrProviderContext) {
-  describe('Resolver embeddable test app', function() {
+  describe('Licensed feature usage APIs', function() {
     this.tags('ciGroup2');
     loadTestFile(require.resolve('./feature_usage'));
   });
