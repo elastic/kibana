f718e051fab8abcd732919ee225b5bcff2bea916
Paul Tavares <paul.tavares@elastic.co>
2020-04-17 08:39:34 -0400

    Added test plan for new hook

---
+++ x-pack/plugins/endpoint/public/applications/endpoint/view/hooks/use_navigate_by_router_event_handler.test.tsx
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+describe('useNavigateByRouterEventHandler hook', () => {
+  it.todo('should support onClick prop');
+  it.todo('should navigate to path via Router');
+  it.todo('should preventDefault if onClick callback throws');
+  it.todo('should not navigate if preventDefault is true');
+  it.todo('should not navigate via router if click was not the primary mouse button');
+  it.todo('should not navigate via router if anchor has target');
+  it.todo('should not to navigate if if meta|alt|ctrl|shift keys are pressed');
+});
