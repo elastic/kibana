6f007bc5e3f68051caf40b834befd2d6dd4b020a
Justin Kambic <justin.kambic@elastic.co>
2020-04-17 10:05:29 -0400

    Extract API params interface to io-ts type.

--- x-pack/plugins/uptime/server/lib/requests/uptime_requests.ts
+++ x-pack/plugins/uptime/server/lib/requests/uptime_requests.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { GetCertsParams } from './get_certs';
 import { UMElasticsearchQueryFn } from '../adapters';
 import {
   HistogramResult,
