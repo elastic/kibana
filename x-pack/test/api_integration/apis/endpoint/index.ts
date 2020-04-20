62eba600d5f36c2c5ce2697fb4a466c215e7daf0
nnamdifrankie <franklin.ejoh@elastic.co>
2020-04-20 14:27:33 -0400

    EMT-146:  remove metadata status test, it cross some boundaries

--- x-pack/test/api_integration/apis/endpoint/index.ts
+++ x-pack/test/api_integration/apis/endpoint/index.ts
@@ -13,13 +13,12 @@ export default function endpointAPIIntegrationTests({
   describe('Endpoint plugin', function() {
     const ingestManager = getService('ingestManager');
     this.tags(['endpoint']);
-    beforeEach(async () => {
+    before(async () => {
       await ingestManager.setup();
     });
     loadTestFile(require.resolve('./index_pattern'));
     loadTestFile(require.resolve('./resolver'));
     loadTestFile(require.resolve('./metadata'));
-    loadTestFile(require.resolve('./metadata_status'));
     loadTestFile(require.resolve('./alerts'));
   });
 }
