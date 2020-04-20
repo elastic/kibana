7dba0d343d5c61724de9ac7f7be7c7fe3181900f
Christos Nasikas <christos.nasikas@elastic.co>
2020-04-20 12:31:25 +0300

    Init

---
+++ x-pack/test/case_api_integration/utils.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { CasesConfigureRequest, CasesConfigureResponse } from '../../plugins/case/common/api';
+
+export const getConfiguration = (connector_id: string = 'connector-1'): CasesConfigureRequest => {
+  return {
+    connector_id,
+    connector_name: 'Connector 1',
+    closure_type: 'close-by-user',
+  };
+};
+
+export const getConfigurationOutput = (): Partial<CasesConfigureResponse> => {
+  return {
+    ...getConfiguration(),
+    created_by: { email: null, full_name: null, username: 'elastic' },
+    updated_by: null,
+  };
+};
+
+export const removeServerGeneratedPropertiesFromConfigure = (
+  config: Partial<CasesConfigureResponse>
+): Partial<CasesConfigureResponse> => {
+  const { created_at, updated_at, version, ...rest } = config;
+  return rest;
+};
+
+export const deleteConfiguration = async (es: any): Promise<void> => {
+  await es.deleteByQuery({
+    index: '.kibana',
+    q: 'type:cases-configure',
+    waitForCompletion: true,
+    refresh: 'wait_for',
+    body: {},
+  });
+};
