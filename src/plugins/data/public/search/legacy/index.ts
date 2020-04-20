6ee7111ca5f9e6f0c0d7d422ce94145275263c73
Liza Katz <liza.katz@elastic.co>
2020-04-20 15:11:50 +0300

    [7.x] [SEARCH] Cleanup fetch soon (#63320) (#63929)
    * [SEARCH] Cleanup fetch soon (#63320)
    
    * move old code to legacy folder
    
    * Use search service directly from search source
    
    * Move get search params to fetch folder
    
    * Delete search strategy folder
    
    * Doc update
    
    * Minor cleanups
    
    * Moved es client to legacy folder
    
    * Clean up some unused code (isViable, old search function)
    
    * Updated tests
    
    * strings update
    
    * Fix jest test
    
    * re-arrange runSearch function
    
    * re-arrange runSearch function
    
    * fix jest tests
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    * remove file (bad merge)
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/plugins/data/public/search/legacy/index.ts
+++ src/plugins/data/public/search/legacy/index.ts
@@ -17,15 +17,5 @@
  * under the License.
  */
 
-export * from './types';
-export {
-  getSearchParams,
-  getPreference,
-  getTimeout,
-  getIgnoreThrottled,
-  getMaxConcurrentShardRequests,
-} from './get_search_params';
-
-export { SearchError, getSearchErrorType } from './search_error';
-export { RequestFailure } from './request_error';
-export { handleResponse } from './handle_response';
+export { fetchSoon } from './fetch_soon';
+export { getEsClient, LegacyApiCaller } from './es_client';
