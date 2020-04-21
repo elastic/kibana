c808ddaf389fe5b4ec5ad36321bbcaa569c27f32
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-04-21 13:07:53 +0200

    [ML] revert package_globs.ts

--- src/dev/renovate/package_globs.ts
+++ src/dev/renovate/package_globs.ts
@@ -26,7 +26,6 @@ import { REPO_ROOT } from '../constants';
 export const PACKAGE_GLOBS = [
   'package.json',
   'x-pack/package.json',
-  'x-pack/plugins/*/package.json',
   'x-pack/legacy/plugins/*/package.json',
   'packages/*/package.json',
   'examples/*/package.json',
