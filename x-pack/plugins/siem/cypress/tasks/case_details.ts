e17755ecef4770c7d11179fa92e7913f6f7ee937
MadameSheema <snootchie.boochies@gmail.com>
2020-04-22 15:04:00 +0200

    [SIEM] Adds 'Create new case' Cypress test (#63971)
    * adds timeline data
    
    * adds 'cases' spec
    
    * adds test
    
    * implements test
    
    * refactor
    
    * prepares test to be executed with the new platform
    
    * finishes refactor
    
    * fixes typo
    
    * renames case interface

--- x-pack/plugins/siem/cypress/tasks/case_details.ts
+++ x-pack/plugins/siem/cypress/tasks/case_details.ts
@@ -4,8 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export interface Timeline {
-  title: string;
-  description: string;
-  query: string;
-}
+import { TIMELINE_TITLE } from '../screens/timeline';
+
+export const openCaseTimeline = (link: string) => {
+  cy.visit('/app/kibana');
+  cy.visit(link);
+  cy.contains('a', 'SIEM');
+  cy.get(TIMELINE_TITLE).should('exist');
+};
