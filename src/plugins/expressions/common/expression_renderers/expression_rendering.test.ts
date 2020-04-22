5f72e14bee570e723589e62e85914c4ed95543a6
streamich <streamich@gmail.com>
2020-04-22 20:22:32 +0200

    test: 💍 make expression rendering tests work

---
+++ src/plugins/expressions/common/expression_renderers/expression_rendering.test.ts
@@ -0,0 +1,171 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { RenderError } from './expression_rendering';
+import { Observable } from 'rxjs';
+import { first, take, toArray } from 'rxjs/operators';
+import { IInterpreterRenderHandlers } from './types';
+import {
+  ExpressionsService,
+  ExpressionsCreateRenderingParams,
+} from '../service/expressions_services';
+
+const setup = (params: ExpressionsCreateRenderingParams) => {
+  const service = new ExpressionsService();
+  const rendering = service.createRendering(params);
+
+  return { service, rendering };
+};
+
+const element: HTMLElement = {} as HTMLElement;
+const mockNotificationService = {
+  toasts: {
+    addError: jest.fn(() => {}),
+  },
+};
+
+const mockMockErrorRenderFunction = jest.fn(
+  (el: HTMLElement, error: RenderError, handlers: IInterpreterRenderHandlers) => handlers.done()
+);
+// extracts data from mockMockErrorRenderFunction call to assert in tests
+const getHandledError = () => {
+  try {
+    return mockMockErrorRenderFunction.mock.calls[0][1];
+  } catch (e) {
+    return null;
+  }
+};
+
+describe('ExpressionRenderHandler', () => {
+  it('constructor creates observers', () => {
+    const { rendering } = setup({ element });
+    expect(rendering.events$).toBeInstanceOf(Observable);
+    expect(rendering.render$).toBeInstanceOf(Observable);
+    expect(rendering.update$).toBeInstanceOf(Observable);
+  });
+
+  it('getElement returns the element', () => {
+    const { rendering } = setup({ element });
+    expect(rendering.getElement()).toBe(element);
+  });
+
+  describe('render()', () => {
+    beforeEach(() => {
+      mockMockErrorRenderFunction.mockClear();
+      mockNotificationService.toasts.addError.mockClear();
+    });
+
+    it('in case of error render$ should emit when error renderer is finished', async () => {
+      const { rendering } = setup({ element });
+      rendering.render(false);
+      const promise1 = rendering.render$.pipe(first()).toPromise();
+      await expect(promise1).resolves.toEqual(1);
+
+      rendering.render(false);
+      const promise2 = rendering.render$.pipe(first()).toPromise();
+      await expect(promise2).resolves.toEqual(2);
+    });
+
+    it('should use custom error handler if provided', async () => {
+      const { rendering } = setup({
+        element,
+        onRenderError: mockMockErrorRenderFunction,
+      });
+      await rendering.render(false);
+      expect(getHandledError()!.message).toEqual(
+        `invalid data provided to the expression renderer`
+      );
+    });
+
+    it('should throw error if the rendering function throws', async () => {
+      const { service, rendering } = setup({
+        element,
+        onRenderError: mockMockErrorRenderFunction,
+      });
+
+      service.registerRenderer({
+        displayName: 'something',
+        name: 'something',
+        reuseDomNode: false,
+        render: (el, config, handlers) => {
+          throw new Error('renderer error');
+        },
+      });
+
+      await rendering.render({ type: 'render', as: 'something' });
+      expect(getHandledError()!.message).toEqual('renderer error');
+    });
+
+    it('sends a next observable once rendering is complete', () => {
+      const { rendering } = setup({ element });
+
+      expect.assertions(1);
+
+      return new Promise(resolve => {
+        rendering.render$.subscribe(renderCount => {
+          expect(renderCount).toBe(1);
+          resolve();
+        });
+
+        rendering.render({ type: 'render', as: 'test' });
+      });
+    });
+
+    // in case render$ subscription happen after render() got called
+    // we still want to be notified about sync render$ updates
+    it("doesn't swallow sync render errors", async () => {
+      const { rendering } = setup({
+        element,
+        onRenderError: mockMockErrorRenderFunction,
+      });
+      rendering.render(false);
+      const renderPromiseAfterRender = rendering.render$.pipe(first()).toPromise();
+      await expect(renderPromiseAfterRender).resolves.toEqual(1);
+      expect(getHandledError()!.message).toEqual(
+        'invalid data provided to the expression renderer'
+      );
+
+      mockMockErrorRenderFunction.mockClear();
+
+      const { rendering: rendering2 } = setup({
+        element,
+        onRenderError: mockMockErrorRenderFunction,
+      });
+      const renderPromiseBeforeRender = rendering2.render$.pipe(first()).toPromise();
+      rendering2.render(false);
+      await expect(renderPromiseBeforeRender).resolves.toEqual(1);
+      expect(getHandledError()!.message).toEqual(
+        'invalid data provided to the expression renderer'
+      );
+    });
+
+    // it is expected side effect of using BehaviorSubject for render$,
+    // that observables will emit previous result if subscription happens after render
+    it('should emit previous render and error results', async () => {
+      const { rendering } = setup({ element });
+      rendering.render(false);
+      const renderPromise = rendering.render$
+        .pipe(take(2), toArray())
+        .toPromise()
+        .catch(() => {});
+      rendering.render(false);
+      await expect(renderPromise).resolves.toEqual([1, 2]);
+    });
+  });
+});
---
+++ src/plugins/expressions/common/expression_renderers/expression_rendering.ts
@@ -0,0 +1,188 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import * as Rx from 'rxjs';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { IInterpreterRenderHandlers } from './types';
+import { ExecutionContextSearch } from '../execution';
+import { ExpressionValue, ExpressionAstExpression, ExpressionRendererRegistry } from '../../public';
+import { Adapters } from '../../../inspector/public';
+
+// import { getNotifications } from './services';
+// import { getRenderersRegistry } from './services';
+/*
+export const onRenderErrorDefault: RenderErrorHandlerFnType = (
+  element: HTMLElement,
+  error: RenderError,
+  handlers: IInterpreterRenderHandlers
+) => {
+  if (error.name === 'AbortError') {
+    handlers.done();
+    return;
+  }
+
+  getNotifications().toasts.addError(error, {
+    title: i18n.translate('expressions.defaultErrorRenderer.errorTitle', {
+      defaultMessage: 'Error in visualisation',
+    }),
+    toastMessage: error.message,
+  });
+  handlers.done();
+};
+*/
+
+export const onRenderErrorDefault: RenderErrorHandlerFnType = (element, error, handlers) => {
+  // eslint-disable-next-line no-console
+  console.error(error);
+  handlers.done();
+};
+
+export interface RenderError extends Error {
+  type?: string;
+}
+
+export type RenderErrorHandlerFnType = (
+  domNode: HTMLElement,
+  error: RenderError,
+  handlers: IInterpreterRenderHandlers
+) => void;
+
+export interface IExpressionLoaderParams {
+  searchContext?: ExecutionContextSearch;
+  context?: ExpressionValue;
+  variables?: Record<string, any>;
+  disableCaching?: boolean;
+  customFunctions?: [];
+  customRenderers?: [];
+  uiState?: unknown;
+  inspectorAdapters?: Adapters;
+  onRenderError?: RenderErrorHandlerFnType;
+}
+
+export type IExpressionRendererExtraHandlers = Record<string, any>;
+
+interface Event {
+  name: string;
+  data: any;
+}
+
+interface UpdateValue {
+  newExpression?: string | ExpressionAstExpression;
+  newParams: IExpressionLoaderParams;
+}
+
+export interface ExpressionRenderingParams {
+  readonly renderers: ExpressionRendererRegistry;
+  readonly element: HTMLElement;
+  readonly onRenderError?: RenderErrorHandlerFnType;
+}
+
+/**
+ * Constructs expression renderer handlers and passes them to expression renderer.
+ */
+export class ExpressionRendering {
+  render$: Observable<number>;
+  update$: Observable<UpdateValue | null>;
+  events$: Observable<Event>;
+
+  private destroyFn?: any;
+  private renderCount: number = 0;
+  private renderSubject: Rx.BehaviorSubject<number | null>;
+  private eventsSubject: Rx.Subject<unknown>;
+  private updateSubject: Rx.Subject<UpdateValue | null>;
+  private handlers: IInterpreterRenderHandlers;
+
+  constructor(private readonly params: ExpressionRenderingParams) {
+    this.eventsSubject = new Rx.Subject();
+    this.events$ = this.eventsSubject.asObservable() as Observable<Event>;
+
+    this.renderSubject = new Rx.BehaviorSubject(null as any | null);
+    this.render$ = this.renderSubject.asObservable().pipe(filter(_ => _ !== null)) as Observable<
+      any
+    >;
+
+    this.updateSubject = new Rx.Subject();
+    this.update$ = this.updateSubject.asObservable();
+
+    this.handlers = {
+      onDestroy: (fn: any) => {
+        this.destroyFn = fn;
+      },
+      done: () => {
+        this.renderCount++;
+        this.renderSubject.next(this.renderCount);
+      },
+      reload: () => {
+        this.updateSubject.next(null);
+      },
+      update: value => {
+        this.updateSubject.next(value);
+      },
+      event: data => {
+        this.eventsSubject.next(data);
+      },
+    };
+  }
+
+  render = async (data: any, uiState: any = {}) => {
+    if (!data || typeof data !== 'object') {
+      return this.handleRenderError(new Error('invalid data provided to the expression renderer'));
+    }
+
+    if (data.type !== 'render' || !data.as) {
+      if (data.type === 'error') {
+        return this.handleRenderError(data.error);
+      } else {
+        return this.handleRenderError(
+          new Error('invalid data provided to the expression renderer')
+        );
+      }
+    }
+
+    if (!this.params.renderers.has(data.as)) {
+      return this.handleRenderError(new Error(`invalid renderer id '${data.as}'`));
+    }
+
+    try {
+      // Rendering is asynchronous, completed by handlers.done()
+      await this.params.renderers.get(data.as)!.render(this.params.element, data.value, {
+        ...this.handlers,
+        uiState,
+      } as any);
+    } catch (e) {
+      return this.handleRenderError(e);
+    }
+  };
+
+  destroy = () => {
+    this.renderSubject.complete();
+    this.eventsSubject.complete();
+    this.updateSubject.complete();
+    if (this.destroyFn) {
+      this.destroyFn();
+    }
+  };
+
+  getElement = () => this.params.element;
+
+  handleRenderError = (error: RenderError) => {
+    (this.params.onRenderError || onRenderErrorDefault)(this.params.element, error, this.handlers);
+  };
+}
