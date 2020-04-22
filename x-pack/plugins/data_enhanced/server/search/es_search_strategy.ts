51f52bc7adbbfb994907437fdf17c58720c690d5
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-04-22 07:59:42 +0200

    undo wait for completion timeout setting too 5s

--- x-pack/plugins/data_enhanced/server/search/es_search_strategy.ts
+++ x-pack/plugins/data_enhanced/server/search/es_search_strategy.ts
@@ -69,7 +69,7 @@ async function asyncSearch(
   const path = encodeURI(request.id ? `/_async_search/${request.id}` : `/${index}/_async_search`);
 
   // Wait up to 1s for the response to return
-  const query = toSnakeCase({ waitForCompletionTimeout: '5s', ...queryParams });
+  const query = toSnakeCase({ waitForCompletionTimeout: '1s', ...queryParams });
 
   const { response, id } = (await caller(
     'transport.request',
