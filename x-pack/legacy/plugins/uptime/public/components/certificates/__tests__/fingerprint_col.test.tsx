40676d00c3f1d06e2464a628ec40e4210ea36a74
shahzad <shahzad31comp@gmail.com>
2020-04-23 18:49:38 +0200

    update snap

--- x-pack/legacy/plugins/uptime/public/components/certificates/__tests__/fingerprint_col.test.tsx
+++ x-pack/legacy/plugins/uptime/public/components/certificates/__tests__/fingerprint_col.test.tsx
@@ -10,23 +10,31 @@ import { FingerprintCol } from '../fingerprint_col';
 import moment from 'moment';
 
 describe('FingerprintCol', () => {
-  const cert = {
-    monitors: [{ name: '', id: 'github' }],
-    certificate_not_valid_after: moment()
-      .add('4', 'months')
-      .toISOString(),
-    certificate_not_valid_before: '2018-05-08T00:00:00.000Z',
-    issuer: 'DigiCert SHA2 Extended Validation Server CA',
-    sha1: 'ca06f56b258b7a0d4f2b05470939478651151984',
-    sha256: '3111500c4a66012cdae333ec3fca1c9dde45c954440e7ee413716bff3663c074',
-    common_name: 'github.com',
-  };
-
   it('shallow renders expected elements for valid props', () => {
+    const cert = {
+      monitors: [{ name: '', id: 'github' }],
+      certificate_not_valid_after: '2020-05-08T00:00:00.000Z',
+      certificate_not_valid_before: '2018-05-08T00:00:00.000Z',
+      issuer: 'DigiCert SHA2 Extended Validation Server CA',
+      sha1: 'ca06f56b258b7a0d4f2b05470939478651151984',
+      sha256: '3111500c4a66012cdae333ec3fca1c9dde45c954440e7ee413716bff3663c074',
+      common_name: 'github.com',
+    };
     expect(shallowWithRouter(<FingerprintCol cert={cert} />)).toMatchSnapshot();
   });
 
   it('renders expected elements for valid props', () => {
+    const cert = {
+      monitors: [{ name: '', id: 'github' }],
+      certificate_not_valid_after: moment()
+        .add('4', 'months')
+        .toISOString(),
+      certificate_not_valid_before: '2018-05-08T00:00:00.000Z',
+      issuer: 'DigiCert SHA2 Extended Validation Server CA',
+      sha1: 'ca06f56b258b7a0d4f2b05470939478651151984',
+      sha256: '3111500c4a66012cdae333ec3fca1c9dde45c954440e7ee413716bff3663c074',
+      common_name: 'github.com',
+    };
     expect(renderWithRouter(<FingerprintCol cert={cert} />)).toMatchSnapshot();
   });
 });
