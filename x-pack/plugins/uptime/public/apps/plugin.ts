c5389d761e3a54da11689221f1af402bb6a44fad
Justin Kambic <justin.kambic@elastic.co>
2020-04-23 18:31:56 -0400

    Remove commented code.

--- x-pack/plugins/uptime/public/apps/plugin.ts
+++ x-pack/plugins/uptime/public/apps/plugin.ts
@@ -3,51 +3,24 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import {
-  LegacyCoreStart,
-  // PluginInitializerContext,
-  CoreSetup,
-  CoreStart,
-  AppMountParameters,
-} from 'src/core/public';
-// import { PluginsStart } from '../../../../../src/' 'ui/new_platform/new_platform';
-// import { Chrome } from 'ui/chrome';
+
+import { LegacyCoreStart, CoreSetup, CoreStart, AppMountParameters } from 'src/core/public';
 import { Plugin } from '../../../../../src/core/public';
 import { UMFrontendLibs } from '../lib/lib';
 import { PLUGIN } from '../../common';
-// import { getKibanaFrameworkAdapter } from '../lib/adapters/framework/new_platform_adapter';
-// import template from './template.html';
-// import { UptimeApp } from '../uptime_app';
-// @ts-ignore
-import { createApolloClient } from '../lib/adapters/framework/apollo_client_adapter';
 // @ts-ignore
 import { FeatureCatalogueCategory } from '../../../../../src/plugins/home/public';
 import { getKibanaFrameworkAdapter } from '../lib/adapters/framework/new_platform_adapter';
-// import { renderApp } from './render_app';
 
 export interface StartObject {
   core: LegacyCoreStart;
   plugins: any;
 }
-console.log('adapter', getKibanaFrameworkAdapter);
 
 export class UptimePlugin implements Plugin {
-  // private el: HTMLElement | undefined;
-  constructor() // @ts-ignore this is added to satisfy the New Platform typing constraint,
-  // but we're not leveraging any of its functionality yet.
-  // private readonly initializerContext: PluginInitializerContext,
-  // private readonly chrome: Chrome
-  {
-    // this.el = undefined;
-    // this.chrome = chrome;
-  }
+  constructor() {}
 
   public async setup(core: CoreSetup, plugins: { home: any }) {
-    // console.log('core setup', core);
-
-    // console.log('plugins from setup', plugins);
-    // console.log(plugins.home);
-
     if (plugins.home) {
       plugins.home.featureCatalogue.register({
         id: PLUGIN.ID,
@@ -66,56 +39,19 @@ export class UptimePlugin implements Plugin {
       euiIconType: 'uptimeApp',
       order: 8900,
       title: PLUGIN.TITLE,
-      // description: PLUGIN.DESCRIPTION,
       async mount(params: AppMountParameters) {
-        const [coreStart, b] = await core.getStartServices();
+        const [coreStart, corePlugins] = await core.getStartServices();
         const { element } = params;
         const libs: UMFrontendLibs = {
-          framework: getKibanaFrameworkAdapter(coreStart, plugins, b),
+          framework: getKibanaFrameworkAdapter(coreStart, plugins, corePlugins),
         };
-        // console.log(libs);
-        console.log('corestart from mount', coreStart);
         libs.framework.render(element);
         return () => {};
-        // console.log('value after setting el', this.el);
-        // return renderApp(element);
       },
     });
   }
 
-  public start(start: CoreStart, plugins: {}): void {
-    // console.log('corestart from start:', start);
-    // const {
-    //   data: { autocomplete },
-    // } = plugins;
-    // console.log('autocomplete', autocomplete);
-    // // const {
-    // //   core,
-    // //   plugins: {
-    // //     data: { autocomplete },
-    // //   },
-    // // } = start;
-    // const libs: UMFrontendLibs = {
-    //   framework: getKibanaFrameworkAdapter(start, autocomplete),
-    // };
-    // console.log('value of el', this.el);
-    // libs.framework.render(UptimeApp, createApolloClient, this.el);
-    // // // @ts-ignore improper type description
-    // // // this.chrome.setRootTemplate(template);
-    // // const checkForRoot = () => {
-    // //   return new Promise(resolve => {
-    // //     const ready = !!document.getElementById(PLUGIN.APP_ROOT_ID);
-    // //     if (ready) {
-    // //       resolve();
-    // //     } else {
-    // //       setTimeout(() => resolve(checkForRoot()), 10);
-    // //     }
-    // //   });
-    // // };
-    // // checkForRoot().then(() => {
-    // //   libs.framework.render(UptimeApp, createApolloClient);
-    // // });
-  }
+  public start(start: CoreStart, plugins: {}): void {}
 
   public stop() {}
 }
