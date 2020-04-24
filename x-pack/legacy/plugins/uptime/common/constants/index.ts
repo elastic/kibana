e918c43535d4109b35ced0723c931ff3e71362bf
Justin Kambic <justin.kambic@elastic.co>
2020-04-24 12:53:28 -0400

    [Uptime] Update TLS settings (#64111)
    * Refactor settings form event handling and modify certs fields.
    
    * Fix/improve broken types/unit/integration/api tests.
    
    * Modify default expiration threshold.
    
    * Rename test vars.
    
    * Implement PR feedback.
    
    * Refresh snapshots, fix broken tests/types.
    
    * Remove unnecessary state spreading.
    
    * Add type for settings field errors.
    
    * Refresh test snapshots.
    
    * Improve punctuation.

--- x-pack/legacy/plugins/uptime/common/constants/index.ts
+++ x-pack/legacy/plugins/uptime/common/constants/index.ts
@@ -9,6 +9,7 @@ export { CHART_FORMAT_LIMITS } from './chart_format_limits';
 export { CLIENT_DEFAULTS } from './client_defaults';
 export { CONTEXT_DEFAULTS } from './context_defaults';
 export * from './capabilities';
+export * from './settings_defaults';
 export { PLUGIN } from './plugin';
 export { QUERY, STATES } from './query';
 export * from './ui';
--- x-pack/legacy/plugins/uptime/public/components/settings/certificate_form.tsx
+++ x-pack/legacy/plugins/uptime/public/components/settings/certificate_form.tsx
@@ -6,155 +6,157 @@
 
 import React from 'react';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { useSelector } from 'react-redux';
 import {
   EuiDescribedFormGroup,
   EuiFormRow,
   EuiCode,
   EuiFieldNumber,
+  EuiText,
   EuiTitle,
   EuiSpacer,
-  EuiSelect,
   EuiFlexGroup,
   EuiFlexItem,
 } from '@elastic/eui';
-import { defaultDynamicSettings, DynamicSettings } from '../../../common/runtime_types';
-import { selectDynamicSettings } from '../../state/selectors';
+import { CertStateThresholds } from '../../../common/runtime_types';
+import { DYNAMIC_SETTINGS_DEFAULTS } from '../../../common/constants';
+import { SettingsFormProps } from '../../pages/settings';
 
-type NumStr = string | number;
-
-export type OnFieldChangeType = (field: string, value?: NumStr) => void;
-
-export interface SettingsFormProps {
-  onChange: OnFieldChangeType;
-  formFields: DynamicSettings | null;
-  fieldErrors: any;
-  isDisabled: boolean;
+interface ChangedValues {
+  heartbeatIndices?: string;
+  certThresholds?: Partial<CertStateThresholds>;
 }
 
+export type OnFieldChangeType = (changedValues: ChangedValues) => void;
+
 export const CertificateExpirationForm: React.FC<SettingsFormProps> = ({
+  loading,
   onChange,
   formFields,
   fieldErrors,
   isDisabled,
-}) => {
-  const dss = useSelector(selectDynamicSettings);
-
-  return (
-    <>
-      <EuiTitle size="s">
-        <h3>
+}) => (
+  <>
+    <EuiTitle size="s">
+      <h3>
+        <FormattedMessage
+          id="xpack.uptime.sourceConfiguration.certificationSectionTitle"
+          defaultMessage="Certificate Expiration"
+        />
+      </h3>
+    </EuiTitle>
+    <EuiSpacer size="m" />
+    <EuiDescribedFormGroup
+      title={
+        <h4>
           <FormattedMessage
-            id="xpack.uptime.sourceConfiguration.certificationSectionTitle"
-            defaultMessage="Certificate Expiration"
+            id="xpack.uptime.sourceConfiguration.expirationThreshold"
+            defaultMessage="Expiration/Age Thresholds"
           />
-        </h3>
-      </EuiTitle>
-      <EuiSpacer size="m" />
-      <EuiDescribedFormGroup
-        title={
-          <h4>
-            <FormattedMessage
-              id="xpack.uptime.sourceConfiguration.stateThresholds"
-              defaultMessage="Expiration State Thresholds"
-            />
-          </h4>
-        }
-        description={
+        </h4>
+      }
+      description={
+        <FormattedMessage
+          id="xpack.uptime.sourceConfiguration.certificateThresholdDescription"
+          defaultMessage="Change the threshold for displaying and alerting on certificate errors. Note: this will affect any configured alerts."
+        />
+      }
+    >
+      <EuiFormRow
+        describedByIds={['errorState']}
+        error={fieldErrors?.certificatesThresholds?.expirationThresholdError}
+        fullWidth
+        helpText={
           <FormattedMessage
-            id="xpack.uptime.sourceConfiguration.stateThresholdsDescription"
-            defaultMessage="Set certificate expiration warning/error thresholds"
+            id="xpack.uptime.sourceConfiguration.expirationThresholdDefaultValue"
+            defaultMessage="The default value is {defaultValue}"
+            values={{
+              defaultValue: (
+                <EuiCode>{DYNAMIC_SETTINGS_DEFAULTS.certThresholds.expiration}</EuiCode>
+              ),
+            }}
+          />
+        }
+        isInvalid={!!fieldErrors?.certificatesThresholds?.expirationThresholdError}
+        label={
+          <FormattedMessage
+            id="xpack.uptime.sourceConfiguration.errorStateLabel"
+            defaultMessage="Expiration threshold"
           />
         }
       >
-        <EuiFormRow
-          describedByIds={['errorState']}
-          error={fieldErrors?.certificatesThresholds?.errorState}
-          fullWidth
-          helpText={
-            <FormattedMessage
-              id="xpack.uptime.sourceConfiguration.errorStateDefaultValue"
-              defaultMessage="The default value is {defaultValue}"
-              values={{
-                defaultValue: (
-                  <EuiCode>{defaultDynamicSettings?.certificatesThresholds?.errorState}</EuiCode>
-                ),
-              }}
+        <EuiFlexGroup>
+          <EuiFlexItem grow={2}>
+            <EuiFieldNumber
+              data-test-subj={`expiration-threshold-input-${loading ? 'loading' : 'loaded'}`}
+              fullWidth
+              disabled={isDisabled}
+              isLoading={loading}
+              value={formFields?.certThresholds?.expiration || ''}
+              onChange={e =>
+                onChange({
+                  certThresholds: {
+                    expiration: Number(e.target.value),
+                  },
+                })
+              }
             />
-          }
-          isInvalid={!!fieldErrors?.certificatesThresholds?.errorState}
-          label={
-            <FormattedMessage
-              id="xpack.uptime.sourceConfiguration.errorStateLabel"
-              defaultMessage="Error state"
-            />
-          }
-        >
-          <EuiFlexGroup>
-            <EuiFlexItem grow={2}>
-              <EuiFieldNumber
-                data-test-subj={`error-state-threshold-input-${dss.loading ? 'loading' : 'loaded'}`}
-                fullWidth
-                disabled={isDisabled}
-                isLoading={dss.loading}
-                value={formFields?.certificatesThresholds?.errorState || ''}
-                onChange={({ currentTarget: { value } }: any) =>
-                  onChange(
-                    'certificatesThresholds.errorState',
-                    value === '' ? undefined : Number(value)
-                  )
-                }
+          </EuiFlexItem>
+          <EuiFlexItem grow={1}>
+            <EuiText>
+              <FormattedMessage
+                id="xpack.uptime.sourceConfiguration.ageLimit.units.days"
+                defaultMessage="Days"
               />
-            </EuiFlexItem>
-            <EuiFlexItem grow={1}>
-              <EuiSelect options={[{ value: 'day', text: 'Days' }]} />
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        </EuiFormRow>
-        <EuiFormRow
-          describedByIds={['warningState']}
-          error={fieldErrors?.certificatesThresholds?.warningState}
-          fullWidth
-          helpText={
-            <FormattedMessage
-              id="xpack.uptime.sourceConfiguration.warningStateDefaultValue"
-              defaultMessage="The default value is {defaultValue}"
-              values={{
-                defaultValue: (
-                  <EuiCode>{defaultDynamicSettings?.certificatesThresholds?.warningState}</EuiCode>
-                ),
-              }}
+            </EuiText>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFormRow>
+      <EuiFormRow
+        describedByIds={['warningState']}
+        error={fieldErrors?.certificatesThresholds?.ageThresholdError}
+        fullWidth
+        helpText={
+          <FormattedMessage
+            id="xpack.uptime.sourceConfiguration.ageThresholdDefaultValue"
+            defaultMessage="The default value is {defaultValue}"
+            values={{
+              defaultValue: <EuiCode>{DYNAMIC_SETTINGS_DEFAULTS.certThresholds.age}</EuiCode>,
+            }}
+          />
+        }
+        isInvalid={!!fieldErrors?.certificatesThresholds?.ageThresholdError}
+        label={
+          <FormattedMessage
+            id="xpack.uptime.sourceConfiguration.warningStateLabel"
+            defaultMessage="Age limit"
+          />
+        }
+      >
+        <EuiFlexGroup>
+          <EuiFlexItem grow={2}>
+            <EuiFieldNumber
+              data-test-subj={`age-threshold-input-${loading ? 'loading' : 'loaded'}`}
+              fullWidth
+              disabled={isDisabled}
+              isLoading={loading}
+              value={formFields?.certThresholds?.age || ''}
+              onChange={e =>
+                onChange({
+                  certThresholds: { age: Number(e.currentTarget.value) },
+                })
+              }
             />
-          }
-          isInvalid={!!fieldErrors?.certificatesThresholds?.warningState}
-          label={
-            <FormattedMessage
-              id="xpack.uptime.sourceConfiguration.warningStateLabel"
-              defaultMessage="Warning state"
-            />
-          }
-        >
-          <EuiFlexGroup>
-            <EuiFlexItem grow={2}>
-              <EuiFieldNumber
-                data-test-subj={`warning-state-threshold-input-${
-                  dss.loading ? 'loading' : 'loaded'
-                }`}
-                fullWidth
-                disabled={isDisabled}
-                isLoading={dss.loading}
-                value={formFields?.certificatesThresholds?.warningState || ''}
-                onChange={(event: any) =>
-                  onChange('certificatesThresholds.warningState', Number(event.currentTarget.value))
-                }
+          </EuiFlexItem>
+          <EuiFlexItem grow={1}>
+            <EuiText>
+              <FormattedMessage
+                id="xpack.uptime.sourceConfiguration.ageLimit.units.days"
+                defaultMessage="Days"
               />
-            </EuiFlexItem>
-            <EuiFlexItem grow={1}>
-              <EuiSelect options={[{ value: 'day', text: 'Days' }]} />
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        </EuiFormRow>
-      </EuiDescribedFormGroup>
-    </>
-  );
-};
+            </EuiText>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFormRow>
+    </EuiDescribedFormGroup>
+  </>
+);
