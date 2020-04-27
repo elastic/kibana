821a90f855b18439e3a7454dd5edc9f3a60963a7
Nicolas Chaulet <n.chaulet@gmail.com>
2020-04-26 21:30:29 -0400

    Update x-pack/plugins/ingest_manager/common/constants/settings.ts
    Co-Authored-By: John Schulz <john.schulz@elastic.co>

--- x-pack/plugins/ingest_manager/common/constants/settings.ts
+++ x-pack/plugins/ingest_manager/common/constants/settings.ts
@@ -4,4 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export const GLOBAL_SETTINGS_SAVED_OBJET_TYPE = 'ingest_manager_settings';
+export const GLOBAL_SETTINGS_SAVED_OBJECT_TYPE = 'ingest_manager_settings';
