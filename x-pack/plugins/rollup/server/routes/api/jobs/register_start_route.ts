2708ba12f1823d0fa8376d872e5a9236e8c6e448
CJ Cenizal <cj@cenizal.com>
2020-04-27 17:14:40 -0700

    Put back 7.x-specific logic.

--- x-pack/plugins/rollup/server/routes/api/jobs/register_start_route.ts
+++ x-pack/plugins/rollup/server/routes/api/jobs/register_start_route.ts
@@ -38,6 +38,17 @@ export const registerStartRoute = ({
         ).then(() => ({ success: true }));
         return response.ok({ body: data });
       } catch (err) {
+        // There is an issue opened on ES to handle the following error correctly
+        // https://github.com/elastic/elasticsearch/issues/39845, which was addressed in 8.0
+        // but not backported to 7.x because it's breaking. So we need to modify the response
+        // here for 7.x.
+        if (err.message.includes('Cannot start task for Rollup Job')) {
+          err.status = 400;
+          err.statusCode = 400;
+          err.body.error.status = 400;
+          err.displayName = 'Bad request';
+        }
+
         if (isEsError(err)) {
           return response.customError({ statusCode: err.statusCode, body: err });
         }
