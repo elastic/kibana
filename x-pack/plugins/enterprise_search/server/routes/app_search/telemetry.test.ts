c6393ed22a388f2a01695cd05fdf9fac501fe7a6
Constance <constancecchen@users.noreply.github.com>
2020-04-28 13:01:28 -0700

    Add UI telemetry tracking to AS in Kibana (#5)
    * Set up Telemetry usageCollection, savedObjects, route, & shared helper
    
    - The Kibana UsageCollection plugin handles collecting our telemetry UI data (views, clicks, errors, etc.) and pushing it to elastic's telemetry servers
    - That data is stored in incremented in Kibana's savedObjects lib/plugin (as well as mapped)
    - When an end-user hits a certain view or action, the shared helper will ping the app search telemetry route which increments the savedObject store
    
    * Update client-side views/links to new shared telemetry helper
    
    * Write tests for new telemetry files

---
+++ x-pack/plugins/enterprise_search/server/routes/app_search/telemetry.test.ts
@@ -0,0 +1,111 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { mockRouter, RouterMock } from 'src/core/server/http/router/router.mock';
+import { savedObjectsServiceMock } from 'src/core/server/saved_objects/saved_objects_service.mock';
+import { httpServerMock } from 'src/core/server/http/http_server.mocks';
+
+import { registerTelemetryRoute } from './telemetry';
+
+jest.mock('../../collectors/app_search/telemetry', () => ({
+  incrementUICounter: jest.fn(),
+}));
+import { incrementUICounter } from '../../collectors/app_search/telemetry';
+
+describe('App Search Telemetry API', () => {
+  let router: RouterMock;
+  const mockResponseFactory = httpServerMock.createResponseFactory();
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    router = mockRouter.create();
+    registerTelemetryRoute({
+      router,
+      getSavedObjectsService: () => savedObjectsServiceMock.create(),
+    });
+  });
+
+  describe('PUT /api/app_search/telemetry', () => {
+    it('increments the saved objects counter', async () => {
+      const successResponse = { success: true };
+      incrementUICounter.mockImplementation(jest.fn(() => successResponse));
+
+      await callThisRoute('put', { body: { action: 'viewed', metric: 'setup_guide' } });
+
+      expect(incrementUICounter).toHaveBeenCalledWith({
+        savedObjects: expect.any(Object),
+        uiAction: 'ui_viewed',
+        metric: 'setup_guide',
+      });
+      expect(mockResponseFactory.ok).toHaveBeenCalledWith({ body: successResponse });
+    });
+
+    it('throws an error when incrementing fails', async () => {
+      incrementUICounter.mockImplementation(jest.fn(() => Promise.reject()));
+
+      await callThisRoute('put', { body: { action: 'error', metric: 'error' } });
+
+      expect(incrementUICounter).toHaveBeenCalled();
+      expect(mockResponseFactory.internalError).toHaveBeenCalled();
+    });
+
+    describe('validates', () => {
+      const itShouldValidate = request => {
+        expect(() => executeRouteValidation(request)).not.toThrow();
+      };
+
+      const itShouldThrow = request => {
+        expect(() => executeRouteValidation(request)).toThrow();
+      };
+
+      it('correctly', () => {
+        const request = { body: { action: 'viewed', metric: 'setup_guide' } };
+        itShouldValidate(request);
+      });
+
+      it('wrong action enum', () => {
+        const request = { body: { action: 'invalid', metric: 'setup_guide' } };
+        itShouldThrow(request);
+      });
+
+      describe('wrong metric type', () => {
+        const request = { body: { action: 'clicked', metric: true } };
+        itShouldThrow(request);
+      });
+
+      describe('action is missing', () => {
+        const request = { body: { metric: 'engines_overview' } };
+        itShouldThrow(request);
+      });
+
+      describe('metric is missing', () => {
+        const request = { body: { action: 'error' } };
+        itShouldThrow(request);
+      });
+    });
+  });
+
+  /**
+   * Test helpers
+   */
+
+  const callThisRoute = async (method, request) => {
+    const [_, handler] = router[method].mock.calls[0];
+
+    const context = {};
+    await handler(context, httpServerMock.createKibanaRequest(request), mockResponseFactory);
+  };
+
+  const executeRouteValidation = request => {
+    const method = 'put';
+
+    const [config] = router[method].mock.calls[0];
+    const validate = config.validate as RouteValidatorConfig<{}, {}, {}>;
+
+    const payload = 'body';
+    validate[payload].validate(request[payload]);
+  };
+});
