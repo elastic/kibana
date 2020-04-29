05f0d0ac8a067d3dade466deaaaa35a0d7e18503
Justin Kambic <justin.kambic@elastic.co>
2020-04-29 07:28:03 -0400

    [Uptime] Migrate client to New Platform (#55086)
    * Move uptime legacy server to plugins directory.
    
    * Re-add feature registration code.
    
    * Move uptime legacy server to plugins directory.
    
    * Re-add feature registration code.
    
    * Move all files.
    
    * WIP fixing things.
    
    * WIP fixing things.
    
    * WIP fixing things.
    
    * Finished fixing server.
    
    * Fix alert type registration glitch.
    
    * Remove obsolete file.
    
    * Fix unwanted revisions, uncomment previously un-migrated code, delete accidentally-merged files.
    
    * More cleanup.
    
    * Fix all non-maps unit tests.
    
    * Get functional tests working.
    
    * Delete misplaced file.
    
    * Add uptime CODEOWNERS entry.
    
    * Get map working in Uptime app on NP.
    
    * Update CODEOWNERS to remove legacy entry.
    
    * Update server imports.
    
    * Delete and clean up unused or redundant code/comments.
    
    * Move new files from legacy to NP to fix merge error.
    
    * Fix broken import.
    
    * Delete obsolete test snapshots.
    
    * Clean up imports and types, delete obsolete code.
    
    * Clean up redundant common imports.
    
    * Delete obsolete translation keys.
    
    * Add additional explicit typing to plugin class.
    
    * Combine redundant common imports.
    
    * Delete unneeded optionalPlugin entry.
    
    * Revert "Combine redundant common imports."
    
    This reverts commit 159e66c349340ba1ede9940c8c3fb980e7be4586.
    
    * Update a translation description.
    
    * Revert "Clean up redundant common imports."
    
    This reverts commit 8794013fbeef776c6abdb504e10da1c17796c651.
    
    * Remove new common index file and revert imports from only common to more specific paths.
    
    * Fix broken imports in `~/x-pack/test`.
    
    * Move new file from legacy to NP location.
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/uptime/server/lib/saved_objects.ts
+++ x-pack/plugins/uptime/server/lib/saved_objects.ts
@@ -4,8 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { DynamicSettings } from '../../../../legacy/plugins/uptime/common/runtime_types';
-import { DYNAMIC_SETTINGS_DEFAULTS } from '../../../../legacy/plugins/uptime/common/constants';
+import { DYNAMIC_SETTINGS_DEFAULTS } from '../../common/constants';
+import { DynamicSettings } from '../../common/runtime_types';
 import { SavedObjectsType, SavedObjectsErrorHelpers } from '../../../../../src/core/server';
 import { UMSavedObjectsQueryFn } from './adapters';
 
