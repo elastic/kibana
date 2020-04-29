77ac31a2d62b60e4ecdbbcc7566b68cf751e6fc6
Justin Kambic <justin.kambic@elastic.co>
2020-04-29 18:42:38 -0400

    Clean up types.

--- x-pack/plugins/uptime/public/lib/alert_types/tls.tsx
+++ x-pack/plugins/uptime/public/lib/alert_types/tls.tsx
@@ -4,27 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { PathReporter } from 'io-ts/lib/PathReporter';
 import React from 'react';
-import { isRight } from 'fp-ts/lib/Either';
+import { AlertTypeModel } from '../../../../triggers_actions_ui/public';
 import { CLIENT_ALERT_TYPES } from '../../../common/constants';
 import { TlsTranslations } from './translations';
-import {
-  AlertTypeModel,
-  ValidationResult,
-} from '../../../../../../plugins/triggers_action_ui/public/types';
 import { AlertTypeInitializer } from '.';
 import { AlertTls } from '../../components/overview/alerts/alerts_containers/alert_tls';
 
 const { name, defaultActionMessage } = TlsTranslations;
-
-const validate = (): ValidationResult => ({});
 
 export const initTlsAlertType: AlertTypeInitializer = (): AlertTypeModel => ({
   id: CLIENT_ALERT_TYPES.TLS,
   iconClass: 'uptimeApp',
   alertParamsExpression: () => <AlertTls />,
   name,
-  validate,
+  validate: () => ({ errors: {} }),
   defaultActionMessage,
 });
