1717736b54aec0dc6771a232f1480994e33e3e2d
Corey Robertson <corey.robertson@elastic.co>
2020-04-29 15:18:53 -0400

    [CANVAS] Moves notify to a canvas service (#63268) (#64666)
    * Moves notify to a canvas service
    
    * Typecheck fix
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/legacy/plugins/canvas/public/components/asset_manager/index.ts
+++ x-pack/legacy/plugins/canvas/public/components/asset_manager/index.ts
@@ -8,29 +8,36 @@ import { connect } from 'react-redux';
 import { compose, withProps } from 'recompose';
 import { set, get } from 'lodash';
 import { fromExpression, toExpression } from '@kbn/interpreter/common';
-import { notify } from '../../lib/notify';
 import { getAssets } from '../../state/selectors/assets';
+// @ts-ignore Untyped local
 import { removeAsset, createAsset } from '../../state/actions/assets';
+// @ts-ignore Untyped local
 import { elementsRegistry } from '../../lib/elements_registry';
+// @ts-ignore Untyped local
 import { addElement } from '../../state/actions/elements';
 import { getSelectedPage } from '../../state/selectors/workpad';
 import { encode } from '../../../common/lib/dataurl';
 import { getId } from '../../lib/get_id';
+// @ts-ignore Untyped Local
 import { findExistingAsset } from '../../lib/find_existing_asset';
 import { VALID_IMAGE_TYPES } from '../../../common/lib/constants';
-import { AssetManager as Component } from './asset_manager';
+import { withKibana } from '../../../../../../../src/plugins/kibana_react/public';
+import { WithKibanaProps } from '../../';
+import { AssetManager as Component, Props as AssetManagerProps } from './asset_manager';
 
-const mapStateToProps = state => ({
+import { State, ExpressionAstExpression, AssetType } from '../../../types';
+
+const mapStateToProps = (state: State) => ({
   assets: getAssets(state),
   selectedPage: getSelectedPage(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  onAddImageElement: pageId => assetId => {
+const mapDispatchToProps = (dispatch: (action: any) => void) => ({
+  onAddImageElement: (pageId: string) => (assetId: string) => {
     const imageElement = elementsRegistry.get('image');
     const elementAST = fromExpression(imageElement.expression);
     const selector = ['chain', '0', 'arguments', 'dataurl'];
-    const subExp = [
+    const subExp: ExpressionAstExpression[] = [
       {
         type: 'expression',
         chain: [
@@ -44,11 +51,11 @@ const mapDispatchToProps = dispatch => ({
         ],
       },
     ];
-    const newAST = set(elementAST, selector, subExp);
+    const newAST = set<ExpressionAstExpression>(elementAST, selector, subExp);
     imageElement.expression = toExpression(newAST);
     dispatch(addElement(pageId, imageElement));
   },
-  onAssetAdd: (type, content) => {
+  onAssetAdd: (type: string, content: string) => {
     // make the ID here and pass it into the action
     const assetId = getId('asset');
     dispatch(createAsset(type, content, assetId));
@@ -56,10 +63,14 @@ const mapDispatchToProps = dispatch => ({
     // then return the id, so the caller knows the id that will be created
     return assetId;
   },
-  onAssetDelete: assetId => dispatch(removeAsset(assetId)),
+  onAssetDelete: (assetId: string) => dispatch(removeAsset(assetId)),
 });
 
-const mergeProps = (stateProps, dispatchProps, ownProps) => {
+const mergeProps = (
+  stateProps: ReturnType<typeof mapStateToProps>,
+  dispatchProps: ReturnType<typeof mapDispatchToProps>,
+  ownProps: AssetManagerProps
+) => {
   const { assets, selectedPage } = stateProps;
   const { onAssetAdd } = dispatchProps;
   const assetValues = Object.values(assets); // pull values out of assets object
@@ -70,16 +81,16 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => {
     onAddImageElement: dispatchProps.onAddImageElement(stateProps.selectedPage),
     selectedPage,
     assetValues,
-    onAssetAdd: file => {
+    onAssetAdd: (file: File) => {
       const [type, subtype] = get(file, 'type', '').split('/');
       if (type === 'image' && VALID_IMAGE_TYPES.indexOf(subtype) >= 0) {
         return encode(file).then(dataurl => {
-          const type = 'dataurl';
-          const existingId = findExistingAsset(type, dataurl, assetValues);
+          const dataurlType = 'dataurl';
+          const existingId = findExistingAsset(dataurlType, dataurl, assetValues);
           if (existingId) {
             return existingId;
           }
-          return onAssetAdd(type, dataurl);
+          return onAssetAdd(dataurlType, dataurl);
         });
       }
 
@@ -88,7 +99,11 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => {
   };
 };
 
-export const AssetManager = compose(
+export const AssetManager = compose<any, any>(
   connect(mapStateToProps, mapDispatchToProps, mergeProps),
-  withProps({ onAssetCopy: asset => notify.success(`Copied '${asset.id}' to clipboard`) })
+  withKibana,
+  withProps(({ kibana }: WithKibanaProps) => ({
+    onAssetCopy: (asset: AssetType) =>
+      kibana.services.canvas.notify.success(`Copied '${asset.id}' to clipboard`),
+  }))
 )(Component);
