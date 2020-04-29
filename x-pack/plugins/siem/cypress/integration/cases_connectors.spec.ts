f34296f7bac6ab328c2e19949b55efbfdd8d8166
Gloria Hornero <snootchie.boochies@gmail.com>
2020-04-29 17:26:24 +0200

    refactor code

--- x-pack/plugins/siem/cypress/integration/cases_connectors.spec.ts
+++ x-pack/plugins/siem/cypress/integration/cases_connectors.spec.ts
@@ -3,54 +3,45 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import { serviceNowConnector } from '../objects/case';
 
+import { TOASTER } from '../screens/configure_cases';
+
+import { goToEditExternalConnection } from '../tasks/all_cases';
+import {
+  addServiceNowConnector,
+  openAddNewConnectorOption,
+  saveChanges,
+  selectLastConnectorCreated,
+} from '../tasks/configure_cases';
 import { loginAndWaitForPageWithoutDateRange } from '../tasks/login';
+
 import { CASES } from '../urls/navigation';
-import { MAIN_PAGE } from '../screens/siem_main';
 
 describe('Cases connectors', () => {
   before(() => {
     cy.server();
     cy.route('POST', '**/api/action').as('createConnector');
-    cy.route('PATCH', '**/api/cases/configure').as('saveConnector');
+    cy.route('POST', '**/api/cases/configure').as('saveConnector');
   });
 
   it('Configures a new connector', () => {
     loginAndWaitForPageWithoutDateRange(CASES);
-    cy.get("[data-test-subj='configure-case-button']").click({ force: true });
-    cy.get("[data-test-subj='header-page-title']").should('exist');
-    cy.get("[data-test-subj='header-page-title']").should('have.text', 'Configure cases Beta');
-    cy.get(MAIN_PAGE).then($page => {
-      if ($page.find("[data-test-subj='.servicenow-card']").length !== 1) {
-        cy.wait(1000);
-        cy.get("[data-test-subj='case-configure-add-connector-button']").click({ force: true });
-      }
-    });
-    cy.get("[data-test-subj='.servicenow-card']").click();
-    cy.get("[data-test-subj='nameInput']").type('New connector');
-    cy.get("[data-test-subj='apiUrlFromInput']").type('https://www.test.service-now.com');
-    cy.get("[data-test-subj='usernameFromInput']").type('usernmae');
-    cy.get("[data-test-subj='passwordFromInput']").type('password');
-    cy.get("[data-test-subj='saveNewActionButton']").click({ force: true });
+    goToEditExternalConnection();
+    openAddNewConnectorOption();
+    addServiceNowConnector(serviceNowConnector);
+
     cy.wait('@createConnector')
       .its('status')
       .should('eql', 200);
-    cy.get('[data-test-subj="euiToastHeader"]').should('exist');
-    cy.get('[data-test-subj="euiToastHeader"]').should('have.text', "Created 'New connector'");
-    cy.get("[data-test-subj='dropdown-connectors']").click({ force: true });
-    cy.get('@createConnector')
-      .its('response')
-      .then(response => {
-        cy.get(`[data-test-subj='dropdown-connector-${response.body.id}']`).click();
-      });
-    cy.get('[data-test-subj="case-configure-action-bottom-bar-save-button"]').click();
-    cy.wait('@saveConnector')
+    cy.get(TOASTER).should('have.text', "Created 'New connector'");
+
+    selectLastConnectorCreated();
+    saveChanges();
+
+    cy.wait('@saveConnector', { timeout: 10000 })
       .its('status')
       .should('eql', 200);
-    cy.get('[data-test-subj="euiToastHeader"]').should('exist');
-    cy.get('[data-test-subj="euiToastHeader"]').should(
-      'have.text',
-      'Saved external connection settings'
-    );
+    cy.get(TOASTER).should('have.text', 'Saved external connection settings');
   });
 });
