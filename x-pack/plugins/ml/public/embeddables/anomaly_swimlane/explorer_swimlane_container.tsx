4894db5c1eef86470e0c51535e83615176f61d44
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-04-29 12:30:31 +0200

    [ML] swimlane input resolver hook

--- x-pack/plugins/ml/public/embeddables/anomaly_swimlane/explorer_swimlane_container.tsx
+++ x-pack/plugins/ml/public/embeddables/anomaly_swimlane/explorer_swimlane_container.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FC, useEffect, useMemo, useState } from 'react';
+import React, { FC, useState } from 'react';
 import {
   EuiCallOut,
   EuiFlexGroup,
@@ -13,38 +13,22 @@ import {
   EuiResizeObserver,
   EuiSpacer,
 } from '@elastic/eui';
-import { combineLatest, from, Observable, of, Subject } from 'rxjs';
-import {
-  catchError,
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  pluck,
-  skipWhile,
-  startWith,
-  switchMap,
-} from 'rxjs/operators';
-import { throttle, isEqual } from 'lodash';
+import { Observable } from 'rxjs';
+
+import { throttle } from 'lodash';
 import { CoreStart } from 'kibana/public';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { ExplorerSwimlane } from '../../application/explorer/explorer_swimlane';
-import { TimeBuckets } from '../../application/util/time_buckets';
-import { ExplorerJob, OverallSwimlaneData } from '../../application/explorer/explorer_utils';
 import { MlStartDependencies } from '../../plugin';
 import {
   AnomalySwimlaneEmbeddableInput,
   AnomalySwimlaneEmbeddableOutput,
   AnomalySwimlaneServices,
 } from './anomaly_swimlane_embeddable';
-import { parseInterval } from '../../../common/util/parse_interval';
-import { SWIMLANE_TYPE } from '../../application/explorer/explorer_constants';
 import { MlTooltipComponent } from '../../application/components/chart_tooltip';
-import { Filter } from '../../../../../../src/plugins/data/common/es_query/filters';
-import { esKuery, Query } from '../../../../../../src/plugins/data/public';
+import { useSwimlaneInputResolver } from './swimlane_input_resolver';
 
 const RESIZE_THROTTLE_TIME_MS = 500;
-const RESIZE_IGNORED_DIFF_PX = 20;
-const FETCH_RESULTS_DEBOUNCE_MS = 500;
 
 export interface ExplorerSwimlaneContainerProps {
   id: string;
@@ -60,132 +44,14 @@ export const ExplorerSwimlaneContainer: FC<ExplorerSwimlaneContainerProps> = ({
   services,
   refresh,
 }) => {
-  const [{ uiSettings }, , { explorerService, anomalyDetectorService }] = services;
-
-  const [swimlaneData, setSwimlaneData] = useState<OverallSwimlaneData>();
   const [chartWidth, setChartWidth] = useState<number>(0);
-  const [swimlaneType, setSwimlaneType] = useState<string>();
-  const [error, setError] = useState();
 
-  const chartWidth$ = useMemo(() => new Subject<number>(), []);
-
-  const timeBuckets = useMemo(() => {
-    return new TimeBuckets({
-      'histogram:maxBars': uiSettings.get('histogram:maxBars'),
-      'histogram:barTarget': uiSettings.get('histogram:barTarget'),
-      dateFormat: uiSettings.get('dateFormat'),
-      'dateFormat:scaled': uiSettings.get('dateFormat:scaled'),
-    });
-  }, []);
-
-  const jobs$ = useMemo(
-    () =>
-      embeddableInput.pipe(
-        pluck('jobIds'),
-        distinctUntilChanged(isEqual),
-        switchMap(jobsIds => anomalyDetectorService.getJobs$(jobsIds))
-      ),
-    []
+  const [swimlaneType, swimlaneData, timeBuckets, error] = useSwimlaneInputResolver(
+    embeddableInput,
+    refresh,
+    services,
+    chartWidth
   );
-
-  useEffect(() => {
-    chartWidth$.next(chartWidth);
-  }, [chartWidth]);
-
-  useEffect(() => {
-    const subscription = combineLatest([
-      jobs$,
-      embeddableInput,
-      chartWidth$.pipe(
-        skipWhile(v => !v),
-        distinctUntilChanged((prev, curr) => {
-          // emit only if the width has been changed significantly
-          return Math.abs(curr - prev) < RESIZE_IGNORED_DIFF_PX;
-        })
-      ),
-      refresh.pipe(startWith(null)),
-    ])
-      .pipe(
-        debounceTime(FETCH_RESULTS_DEBOUNCE_MS),
-        switchMap(([jobs, input, swimlaneContainerWidth]) => {
-          const {
-            viewBy,
-            swimlaneType: swimlaneTypeInput,
-            limit,
-            timeRange,
-            filters,
-            query,
-          } = input;
-
-          explorerService.setTimeRange(timeRange);
-
-          if (!swimlaneType) {
-            setSwimlaneType(swimlaneTypeInput);
-          }
-
-          const explorerJobs: ExplorerJob[] = jobs.map(job => {
-            const bucketSpan = parseInterval(job.analysis_config.bucket_span);
-            return {
-              id: job.job_id,
-              selected: true,
-              bucketSpanSeconds: bucketSpan!.asSeconds(),
-            };
-          });
-
-          let appliedFilters: any;
-          try {
-            appliedFilters = processFilters(filters, query);
-          } catch (e) {
-            // handle query syntax errors
-            setError(e);
-            return of(undefined);
-          }
-
-          return from(explorerService.loadOverallData(explorerJobs, swimlaneContainerWidth)).pipe(
-            switchMap(overallSwimlaneData => {
-              const { earliest, latest } = overallSwimlaneData;
-
-              if (overallSwimlaneData && swimlaneTypeInput === SWIMLANE_TYPE.VIEW_BY) {
-                return from(
-                  explorerService.loadViewBySwimlane(
-                    [],
-                    { earliest, latest },
-                    explorerJobs,
-                    viewBy!,
-                    limit!,
-                    swimlaneContainerWidth,
-                    appliedFilters
-                  )
-                ).pipe(
-                  map(viewBySwimlaneData => {
-                    return {
-                      ...viewBySwimlaneData!,
-                      earliest,
-                      latest,
-                    };
-                  })
-                );
-              }
-              return of(overallSwimlaneData);
-            })
-          );
-        }),
-        catchError(e => {
-          setError(e.body);
-          return of(undefined);
-        })
-      )
-      .subscribe(data => {
-        if (data !== undefined) {
-          setError(null);
-          setSwimlaneData(data);
-        }
-      });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
 
   const onResize = throttle((e: { width: number; height: number }) => {
     const labelWidth = 200;
@@ -248,42 +114,3 @@ export const ExplorerSwimlaneContainer: FC<ExplorerSwimlaneContainerProps> = ({
     </EuiResizeObserver>
   );
 };
-
-export function processFilters(filters: Filter[], query: Query) {
-  const inputQuery =
-    query.language === 'kuery'
-      ? esKuery.toElasticsearchQuery(esKuery.fromKueryExpression(query.query as string))
-      : query.query;
-
-  const must = [inputQuery];
-  const mustNot = [];
-  for (const filter of filters) {
-    if (filter.meta.disabled) continue;
-
-    const {
-      meta: { negate, type, key: fieldName },
-    } = filter;
-
-    let filterQuery = filter.query;
-
-    if (filterQuery === undefined && type === 'exists') {
-      filterQuery = {
-        exists: {
-          field: fieldName,
-        },
-      };
-    }
-
-    if (negate) {
-      mustNot.push(filterQuery);
-    } else {
-      must.push(filterQuery);
-    }
-  }
-  return {
-    bool: {
-      must,
-      must_not: mustNot,
-    },
-  };
-}
