abc9d7d43426127624e0ac658066a3cf1c58978b
Paul Tavares <paul.tavares@elastic.co>
2020-04-29 16:02:07 -0400

    Add workaround to fleet integration service

--- x-pack/test/functional_endpoint/services/endpoint_policy.ts
+++ x-pack/test/functional_endpoint/services/endpoint_policy.ts
@@ -10,6 +10,7 @@ import {
   CreateDatasourceResponse,
 } from '../../../plugins/ingest_manager/common';
 import { Immutable } from '../../../plugins/endpoint/common/types';
+import { factory as policyConfigFactory } from '../../../plugins/endpoint/common/models/policy_config';
 
 const INGEST_API_ROOT = '/api/ingest_manager';
 const INGEST_API_AGENT_CONFIGS = `${INGEST_API_ROOT}/agent_configs`;
@@ -40,6 +41,20 @@ export function EndpointPolicyTestResourcesProvider({ getService }: FtrProviderC
      * stores the Policy configuration data
      */
     async createPolicy(): Promise<PolicyTestResourceInfo> {
+      // FIXME: Refactor after https://github.com/elastic/kibana/issues/64822 is fixed. `isInitialized` setup below should be deleted
+      // Due to an issue in Ingest API, we first create the Fleet user. This avoids the Agent Config api throwing a 500
+      const isFleetSetupResponse = await supertest
+        .get('/api/ingest_manager/fleet/setup')
+        .set('kbn-xsrf', 'xxx')
+        .expect(200);
+      if (!isFleetSetupResponse.body.isInitialized) {
+        await supertest
+          .post('/api/ingest_manager/fleet/setup')
+          .set('kbn-xsrf', 'xxx')
+          .send()
+          .expect(200);
+      }
+
       // create agent config
       const {
         body: { item: agentConfig },
@@ -57,11 +72,22 @@ export function EndpointPolicyTestResourcesProvider({ getService }: FtrProviderC
         .set('kbn-xsrf', 'xxx')
         .send({
           name: 'Protect East Coast',
-          description: 'Protect the worlds data - in the East Coast',
+          description: 'Protect the worlds data - but in the East Coast',
           config_id: agentConfig.id,
           enabled: true,
           output_id: '',
-          inputs: [],
+          inputs: [
+            {
+              type: 'endpoint',
+              enabled: true,
+              streams: [],
+              config: {
+                policy: {
+                  value: policyConfigFactory(),
+                },
+              },
+            },
+          ],
           namespace: '',
           package: {
             name: 'endpoint',
