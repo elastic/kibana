de49742f4bdeedbab2c78fc4b9acef45403df99e
James Gowdy <jgowdy@elastic.co>
2020-04-30 14:56:02 +0100

    [ML] Enabling mml estimation in data recognizer module setup

--- x-pack/plugins/ml/server/models/data_recognizer/data_recognizer.ts
+++ x-pack/plugins/ml/server/models/data_recognizer/data_recognizer.ts
@@ -110,7 +110,7 @@ export class DataRecognizer {
   /**
    * List of the module jobs that require model memory estimation
    */
-  jobsForModelMemoryEstimation: ModuleJob[] = [];
+  jobsForModelMemoryEstimation: Array<{ job: ModuleJob; query: any }> = [];
 
   constructor(
     private callAsCurrentUser: APICaller,
@@ -374,7 +374,7 @@ export class DataRecognizer {
     end?: number,
     jobOverrides?: JobOverride | JobOverride[],
     datafeedOverrides?: DatafeedOverride | DatafeedOverride[],
-    estimateModelMemory?: boolean
+    estimateModelMemory: boolean = true
   ) {
     // load the config from disk
     const moduleConfig = await this.getModule(moduleId, jobPrefix);
@@ -416,7 +416,10 @@ export class DataRecognizer {
       savedObjects: [] as KibanaObjectResponse[],
     };
 
-    this.jobsForModelMemoryEstimation = moduleConfig.jobs;
+    this.jobsForModelMemoryEstimation = moduleConfig.jobs.map(job => ({
+      job,
+      query: moduleConfig.datafeeds.find(d => d.config.job_id === job.id)?.config.query || null,
+    }));
 
     this.applyJobConfigOverrides(moduleConfig, jobOverrides, jobPrefix);
     this.applyDatafeedConfigOverrides(moduleConfig, datafeedOverrides, jobPrefix);
@@ -958,7 +961,7 @@ export class DataRecognizer {
    */
   async updateModelMemoryLimits(
     moduleConfig: Module,
-    estimateMML: boolean = false,
+    estimateMML: boolean,
     start?: number,
     end?: number
   ) {
@@ -968,27 +971,27 @@ export class DataRecognizer {
 
     if (estimateMML && this.jobsForModelMemoryEstimation.length > 0) {
       const calculateModelMemoryLimit = calculateModelMemoryLimitProvider(this.callAsCurrentUser);
-      const query = moduleConfig.query ?? null;
 
       // Checks if all jobs in the module have the same time field configured
       const isSameTimeFields = this.jobsForModelMemoryEstimation.every(
-        job =>
+        ({ job }) =>
           job.config.data_description.time_field ===
-          this.jobsForModelMemoryEstimation[0].config.data_description.time_field
+          this.jobsForModelMemoryEstimation[0].job.config.data_description.time_field
       );
 
       if (isSameTimeFields && (start === undefined || end === undefined)) {
         // In case of time range is not provided and the time field is the same
         // set the fallback range for all jobs
+        // as there may not be a common query, we use a match_all
         const { start: fallbackStart, end: fallbackEnd } = await this.getFallbackTimeRange(
-          this.jobsForModelMemoryEstimation[0].config.data_description.time_field,
-          query
+          this.jobsForModelMemoryEstimation[0].job.config.data_description.time_field,
+          { match_all: {} }
         );
         start = fallbackStart;
         end = fallbackEnd;
       }
 
-      for (const job of this.jobsForModelMemoryEstimation) {
+      for (const { job, query } of this.jobsForModelMemoryEstimation) {
         let earliestMs = start;
         let latestMs = end;
         if (earliestMs === undefined || latestMs === undefined) {
@@ -1098,10 +1101,15 @@ export class DataRecognizer {
     if (generalOverrides.some(override => !!override.analysis_limits?.model_memory_limit)) {
       this.jobsForModelMemoryEstimation = [];
     } else {
-      this.jobsForModelMemoryEstimation = moduleConfig.jobs.filter(job => {
-        const override = jobSpecificOverrides.find(o => `${jobPrefix}${o.job_id}` === job.id);
-        return override?.analysis_limits?.model_memory_limit === undefined;
-      });
+      this.jobsForModelMemoryEstimation = moduleConfig.jobs
+        .filter(job => {
+          const override = jobSpecificOverrides.find(o => `${jobPrefix}${o.job_id}` === job.id);
+          return override?.analysis_limits?.model_memory_limit === undefined;
+        })
+        .map(job => ({
+          job,
+          query: moduleConfig.datafeeds.find(d => d.config.job_id === job.id)?.config.query || null,
+        }));
     }
 
     function processArrayValues(source: any, update: any) {
