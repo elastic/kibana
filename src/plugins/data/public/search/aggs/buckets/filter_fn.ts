a19709de7968a10b51bed65eb3b92798921a54fb
Alexey Antonov <alexwizp@gmail.com>
2020-05-01 11:14:24 +0300

    Update src/plugins/data/public/search/aggs/buckets/filter_fn.ts
    Co-authored-by: Luke Elmers <lukeelmers@gmail.com>

--- src/plugins/data/public/search/aggs/buckets/filter_fn.ts
+++ src/plugins/data/public/search/aggs/buckets/filter_fn.ts
@@ -70,7 +70,7 @@ export const aggFilter = (): FunctionDefinition => ({
       types: ['string'],
       required: false,
       help: i18n.translate('data.search.aggs.buckets.filter.geo_bounding_box.help', {
-        defaultMessage: 'Allowing to filter hits based on a point location using a bounding box.',
+        defaultMessage: 'Filter results based on a point location within a bounding box',
       }),
     },
     json: {
