e84ae0b5dc1e2a9f1361308f3746c2988d0d5a0b
LeeDr <lee.drengenberg@elastic.co>
2020-04-30 19:02:02 -0500

    temporarily add superuser role

--- x-pack/test/functional/apps/dashboard/_async_dashboard.ts
+++ x-pack/test/functional/apps/dashboard/_async_dashboard.ts
@@ -30,7 +30,7 @@ export default function({ getService, getPageObjects }: FtrProviderContext) {
     this.tags('smoke');
 
     before(async () => {
-      await security.testUser.setRoles(['kibana_admin', 'kibana_sample_admin']);
+      await security.testUser.setRoles(['kibana_admin', 'kibana_sample_admin', 'superuser']);
       await PageObjects.common.navigateToUrl('home', 'tutorial_directory/sampleData');
       await PageObjects.header.waitUntilLoadingHasFinished();
       await PageObjects.home.addSampleDataSet('flights');
