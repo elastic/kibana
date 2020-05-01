0589ebd267fe36a772d054369e7cebc6024fbe3d
Alexey Antonov <alexwizp@gmail.com>
2020-05-01 11:16:46 +0300

    Update src/plugins/data/public/search/aggs/buckets/geo_hash_fn.ts
    Co-authored-by: Luke Elmers <lukeelmers@gmail.com>

--- src/plugins/data/public/search/aggs/buckets/geo_hash_fn.ts
+++ src/plugins/data/public/search/aggs/buckets/geo_hash_fn.ts
@@ -69,7 +69,7 @@ export const aggGeoHash = (): FunctionDefinition => ({
       types: ['boolean'],
       required: false,
       help: i18n.translate('data.search.aggs.buckets.geoHash.useGeocentroid.help', {
-        defaultMessage: 'Should be used geocentroid for this aggregation or not?',
+        defaultMessage: 'Specifies whether to use geocentroid for this aggregation',
       }),
     },
     autoPrecision: {
