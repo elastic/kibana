e8360670f4ff1d08cee962d7c34a49552b1edf3b
Anton Dosov <anton.dosov@elastic.co>
2020-05-01 09:27:54 +0200

    trying fix 64709

--- src/legacy/core_plugins/visualizations/public/np_ready/public/embeddable/visualize_embeddable.ts
+++ src/legacy/core_plugins/visualizations/public/np_ready/public/embeddable/visualize_embeddable.ts
@@ -332,13 +332,14 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
       this.abortController.abort();
     }
     this.abortController = new AbortController();
+    const abortController = this.abortController;
     this.expression = await buildPipeline(this.vis, {
       timefilter: this.timefilter,
       timeRange: this.timeRange,
       abortSignal: this.abortController!.signal,
     });
 
-    if (this.handler) {
+    if (this.handler && !abortController.signal.aborted) {
       this.handler.update(this.expression, expressionParams);
     }
   }
