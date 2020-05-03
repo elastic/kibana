86807eaa62fa3d7e21df99b2c29636d5f81f90ac
shahzad <shahzad31comp@gmail.com>
2020-05-03 14:40:50 +0200

    test

--- x-pack/plugins/uptime/public/components/overview/alerts/monitor_expressions/__tests__/time_expression_select.test.tsx
+++ x-pack/plugins/uptime/public/components/overview/alerts/monitor_expressions/__tests__/time_expression_select.test.tsx
@@ -7,23 +7,16 @@
 import React from 'react';
 import { renderWithIntl, shallowWithIntl } from 'test_utils/enzyme_helpers';
 import { DownNoExpressionSelect } from '../down_number_select';
+import { TimeExpressionSelect } from '../time_expression_select';
 
-describe('DownNoExpressionSelect component', () => {
-  const filters =
-    '"{"bool":{"filter":[{"bool":{"should":[{"match":{"observer.geo.name":"US-West"}}],"minimum_should_match":1}},' +
-    '{"bool":{"should":[{"match":{"url.port":443}}],"minimum_should_match":1}}]}}"';
-
+describe('TimeExpressionSelect component', () => {
   it('should shallow renders against props', function() {
-    const component = shallowWithIntl(
-      <DownNoExpressionSelect filters={filters} setAlertParams={jest.fn()} />
-    );
+    const component = shallowWithIntl(<TimeExpressionSelect setAlertParams={jest.fn()} />);
     expect(component).toMatchSnapshot();
   });
 
   it('should renders against props', function() {
-    const component = renderWithIntl(
-      <DownNoExpressionSelect filters={filters} setAlertParams={jest.fn()} />
-    );
+    const component = renderWithIntl(<TimeExpressionSelect setAlertParams={jest.fn()} />);
     expect(component).toMatchSnapshot();
   });
 });
