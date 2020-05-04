b717d3110ffceac0529014326b43e9b95497a15c
Gidi Meir Morris <github@gidi.io>
2020-05-04 17:02:55 +0100

    remoed uneeded mports

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/email/index.test.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/email/index.test.tsx
@@ -3,12 +3,10 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import React, { FunctionComponent } from 'react';
-import { mountWithIntl } from 'test_utils/enzyme_helpers';
 import { TypeRegistry } from '../../../type_registry';
 import { registerBuiltInActionTypes } from '../index';
-import { ActionTypeModel, ActionParamsProps } from '../../../../types';
-import { EmailActionParams, EmailActionConnector } from '../types';
+import { ActionTypeModel } from '../../../../types';
+import { EmailActionConnector } from '../types';
 
 const ACTION_TYPE_ID = '.email';
 let actionTypeModel: ActionTypeModel;
