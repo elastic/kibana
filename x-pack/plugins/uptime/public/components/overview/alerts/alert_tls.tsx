5d57988ede458fab4cacf1b19402f57024949555
Justin Kambic <justin.kambic@elastic.co>
2020-05-04 10:33:23 -0400

    Extract a non-trivial component to a dedicated file.

--- x-pack/plugins/uptime/public/components/overview/alerts/alert_tls.tsx
+++ x-pack/plugins/uptime/public/components/overview/alerts/alert_tls.tsx
@@ -4,73 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Link } from 'react-router-dom';
-import { EuiExpression, EuiFlexItem, EuiFlexGroup, EuiPopover, EuiSpacer } from '@elastic/eui';
-import React, { useState } from 'react';
-import { FormattedMessage } from '@kbn/i18n/react';
+import { EuiExpression, EuiFlexItem, EuiFlexGroup, EuiSpacer } from '@elastic/eui';
+import React from 'react';
 import { TlsTranslations } from './translations';
-import { SETTINGS_ROUTE } from '../../../../common/constants';
-
-interface SettingsMessageExpressionPopoverProps {
-  'aria-label': string;
-  description: string;
-  id: string;
-  setAlertFlyoutVisible: (value: boolean) => void;
-  value: string;
-}
-
-const SettingsMessageExpressionPopover: React.FC<SettingsMessageExpressionPopoverProps> = ({
-  'aria-label': ariaLabel,
-  description,
-  setAlertFlyoutVisible,
-  value,
-  id,
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <EuiPopover
-      id={id}
-      anchorPosition="downLeft"
-      button={
-        <EuiExpression
-          aria-label={ariaLabel}
-          color="secondary"
-          description={description}
-          isActive={isOpen}
-          onClick={() => setIsOpen(!isOpen)}
-          value={value}
-        />
-      }
-      isOpen={isOpen}
-      closePopover={() => setIsOpen(false)}
-    >
-      <FormattedMessage
-        id="xpack.uptime.alerts.tls.settingsPageNav.text"
-        defaultMessage="You can edit these thresholds on the {settingsPageLink}."
-        values={{
-          settingsPageLink: (
-            // this link is wrapped around a span so we can also change the UI state
-            // and hide the alert flyout before triggering the navigation to the settings page
-            <Link to={SETTINGS_ROUTE} data-test-subj="xpack.uptime.alerts.tlsFlyout.linkToSettings">
-              <span
-                onClick={() => {
-                  setAlertFlyoutVisible(false);
-                }}
-                onKeyUp={e => {
-                  if (e.key === 'Enter') {
-                    setAlertFlyoutVisible(false);
-                  }
-                }}
-              >
-                settings page
-              </span>
-            </Link>
-          ),
-        }}
-      />
-    </EuiPopover>
-  );
-};
+import { SettingsMessageExpressionPopover } from './settings_message_expression_popover';
 
 interface Props {
   ageThreshold?: number;
