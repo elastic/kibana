114039aadaa30454dd2f24a29843a77b5d6663eb
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2020-05-04 09:52:49 +0200

    Fix path

--- x-pack/plugins/infra/server/lib/alerting/log_threshold/mocks/index.ts
+++ x-pack/plugins/infra/server/lib/alerting/log_threshold/mocks/index.ts
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { InfraBackendLibs } from '../../infra_types';
+import { InfraBackendLibs } from '../../../infra_types';
 
 export const libsMock = {
   sources: {
