64f3d1f8d59e1cd988724ece1770cdbfede6cfbf
Larry Gregory <larry.gregory@elastic.co>
2020-05-04 09:53:22 -0400

    [6.8] [TSVB] Fixing memory leak (#64918) (#65076)

--- src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/table/sibling_buckets.js
+++ src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/table/sibling_buckets.js
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import _ from 'lodash';
+import { overwrite } from '../../helpers';
 import getBucketSize from '../../helpers/get_bucket_size';
 import bucketTransform from '../../helpers/bucket_transform';
 import getIntervalAndTimefield from '../../get_interval_and_timefield';
@@ -35,7 +35,7 @@ export default function siblingBuckets(req, panel) {
           if (fn) {
             try {
               const bucket = fn(metric, column.metrics, bucketSize);
-              _.set(doc, `${aggRoot}.${metric.id}`, bucket);
+              overwrite(doc, `${aggRoot}.${metric.id}`, bucket);
             } catch (e) {
               // meh
             }
