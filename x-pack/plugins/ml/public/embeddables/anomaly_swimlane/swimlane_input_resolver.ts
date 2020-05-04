bc4773258bb1c84c90a04b906e2dd7e63c4f0e6c
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-05-04 19:09:53 +0200

    [ML] Embeddable Anomaly Swimlane (#64056)
    * [ML] embeddables setup
    
    * [ML] fix initialization
    
    * [ML] ts refactoring
    
    * [ML] refactor time_buckets.js
    
    * [ML] async services
    
    * [ML] extract job_selector_flyout.tsx
    
    * [ML] fetch overall swimlane data
    
    * [ML] import explorer styles
    
    * [ML] revert package_globs.ts
    
    * [ML] refactor with container, services with DI
    
    * [ML] resize throttle, fetch based on chart width
    
    * [ML] swimlane embeddable setup
    
    * [ML] explorer service
    
    * [ML] chart_tooltip_service
    
    * [ML] fix types
    
    * [ML] overall type for single job with no influencers
    
    * [ML] improve anomaly_swimlane_initializer ux
    
    * [ML] fix services initialization, unsubscribe on destroy
    
    * [ML] support custom time range
    
    * [ML] add tooltip
    
    * [ML] rollback initGetSwimlaneBucketInterval
    
    * [ML] new tooltip service
    
    * [ML] MlTooltipComponent with render props, fix warning
    
    * [ML] fix typo in the filename
    
    * [ML] remove redundant time range output
    
    * [ML] fix time_buckets.test.js jest tests
    
    * [ML] fix explorer chart tests
    
    * [ML] swimlane tests
    
    * [ML] store job ids instead of complete job objects
    
    * [ML] swimlane limit input
    
    * [ML] memo tooltip component, loading indicator
    
    * [ML] scrollable content
    
    * [ML] support query and filters
    
    * [ML] handle query syntax errors
    
    * [ML] rename anomaly_swimlane_service
    
    * [ML] introduce constants
    
    * [ML] edit panel title during setup
    
    * [ML] withTimeRangeSelector
    
    * [ML] rename explorer_service
    
    * [ML] getJobs$ method with one API call
    
    * [ML] fix groups selection
    
    * [ML] swimlane input resolver hook
    
    * [ML] useSwimlaneInputResolver tests
    
    * [ML] factory test
    
    * [ML] container test
    
    * [ML] set wrapper
    
    * [ML] tooltip tests
    
    * [ML] fix displayScore
    
    * [ML] label colors
    
    * [ML] support edit mode
    
    * [ML] call super render
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit f62df99ae38801b905dd10d281dbf972c5b17578)

---
+++ x-pack/plugins/ml/public/embeddables/anomaly_swimlane/swimlane_input_resolver.ts
@@ -0,0 +1,211 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { useEffect, useMemo, useState } from 'react';
+import { combineLatest, from, Observable, of, Subject } from 'rxjs';
+import { isEqual } from 'lodash';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  pluck,
+  skipWhile,
+  startWith,
+  switchMap,
+} from 'rxjs/operators';
+import { CoreStart } from 'kibana/public';
+import { TimeBuckets } from '../../application/util/time_buckets';
+import {
+  AnomalySwimlaneEmbeddableInput,
+  AnomalySwimlaneServices,
+} from './anomaly_swimlane_embeddable';
+import { MlStartDependencies } from '../../plugin';
+import { SWIMLANE_TYPE } from '../../application/explorer/explorer_constants';
+import { Filter } from '../../../../../../src/plugins/data/common/es_query/filters';
+import { Query } from '../../../../../../src/plugins/data/common/query';
+import { esKuery } from '../../../../../../src/plugins/data/public';
+import { ExplorerJob, OverallSwimlaneData } from '../../application/explorer/explorer_utils';
+import { parseInterval } from '../../../common/util/parse_interval';
+import { AnomalyDetectorService } from '../../application/services/anomaly_detector_service';
+
+const RESIZE_IGNORED_DIFF_PX = 20;
+const FETCH_RESULTS_DEBOUNCE_MS = 500;
+
+function getJobsObservable(
+  embeddableInput: Observable<AnomalySwimlaneEmbeddableInput>,
+  anomalyDetectorService: AnomalyDetectorService
+) {
+  return embeddableInput.pipe(
+    pluck('jobIds'),
+    distinctUntilChanged(isEqual),
+    switchMap(jobsIds => anomalyDetectorService.getJobs$(jobsIds))
+  );
+}
+
+export function useSwimlaneInputResolver(
+  embeddableInput: Observable<AnomalySwimlaneEmbeddableInput>,
+  refresh: Observable<any>,
+  services: [CoreStart, MlStartDependencies, AnomalySwimlaneServices],
+  chartWidth: number
+) {
+  const [{ uiSettings }, , { explorerService, anomalyDetectorService }] = services;
+
+  const [swimlaneData, setSwimlaneData] = useState<OverallSwimlaneData>();
+  const [swimlaneType, setSwimlaneType] = useState<string>();
+  const [error, setError] = useState();
+
+  const chartWidth$ = useMemo(() => new Subject<number>(), []);
+
+  const timeBuckets = useMemo(() => {
+    return new TimeBuckets({
+      'histogram:maxBars': uiSettings.get('histogram:maxBars'),
+      'histogram:barTarget': uiSettings.get('histogram:barTarget'),
+      dateFormat: uiSettings.get('dateFormat'),
+      'dateFormat:scaled': uiSettings.get('dateFormat:scaled'),
+    });
+  }, []);
+
+  useEffect(() => {
+    const subscription = combineLatest([
+      getJobsObservable(embeddableInput, anomalyDetectorService),
+      embeddableInput,
+      chartWidth$.pipe(
+        skipWhile(v => !v),
+        distinctUntilChanged((prev, curr) => {
+          // emit only if the width has been changed significantly
+          return Math.abs(curr - prev) < RESIZE_IGNORED_DIFF_PX;
+        })
+      ),
+      refresh.pipe(startWith(null)),
+    ])
+      .pipe(
+        debounceTime(FETCH_RESULTS_DEBOUNCE_MS),
+        switchMap(([jobs, input, swimlaneContainerWidth]) => {
+          const {
+            viewBy,
+            swimlaneType: swimlaneTypeInput,
+            limit,
+            timeRange,
+            filters,
+            query,
+          } = input;
+
+          explorerService.setTimeRange(timeRange);
+
+          if (!swimlaneType) {
+            setSwimlaneType(swimlaneTypeInput);
+          }
+
+          const explorerJobs: ExplorerJob[] = jobs.map(job => {
+            const bucketSpan = parseInterval(job.analysis_config.bucket_span);
+            return {
+              id: job.job_id,
+              selected: true,
+              bucketSpanSeconds: bucketSpan!.asSeconds(),
+            };
+          });
+
+          let appliedFilters: any;
+          try {
+            appliedFilters = processFilters(filters, query);
+          } catch (e) {
+            // handle query syntax errors
+            setError(e);
+            return of(undefined);
+          }
+
+          return from(explorerService.loadOverallData(explorerJobs, swimlaneContainerWidth)).pipe(
+            switchMap(overallSwimlaneData => {
+              const { earliest, latest } = overallSwimlaneData;
+
+              if (overallSwimlaneData && swimlaneTypeInput === SWIMLANE_TYPE.VIEW_BY) {
+                return from(
+                  explorerService.loadViewBySwimlane(
+                    [],
+                    { earliest, latest },
+                    explorerJobs,
+                    viewBy!,
+                    limit!,
+                    swimlaneContainerWidth,
+                    appliedFilters
+                  )
+                ).pipe(
+                  map(viewBySwimlaneData => {
+                    return {
+                      ...viewBySwimlaneData!,
+                      earliest,
+                      latest,
+                    };
+                  })
+                );
+              }
+              return of(overallSwimlaneData);
+            })
+          );
+        }),
+        catchError(e => {
+          setError(e.body);
+          return of(undefined);
+        })
+      )
+      .subscribe(data => {
+        if (data !== undefined) {
+          setError(null);
+          setSwimlaneData(data);
+        }
+      });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    chartWidth$.next(chartWidth);
+  }, [chartWidth]);
+
+  return [swimlaneType, swimlaneData, timeBuckets, error];
+}
+
+export function processFilters(filters: Filter[], query: Query) {
+  const inputQuery =
+    query.language === 'kuery'
+      ? esKuery.toElasticsearchQuery(esKuery.fromKueryExpression(query.query as string))
+      : query.query;
+
+  const must = [inputQuery];
+  const mustNot = [];
+  for (const filter of filters) {
+    if (filter.meta.disabled) continue;
+
+    const {
+      meta: { negate, type, key: fieldName },
+    } = filter;
+
+    let filterQuery = filter.query;
+
+    if (filterQuery === undefined && type === 'exists') {
+      filterQuery = {
+        exists: {
+          field: fieldName,
+        },
+      };
+    }
+
+    if (negate) {
+      mustNot.push(filterQuery);
+    } else {
+      must.push(filterQuery);
+    }
+  }
+  return {
+    bool: {
+      must,
+      must_not: mustNot,
+    },
+  };
+}
