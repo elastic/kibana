f9c535fc0ea737f49ae24fc2e4b7f03b67ad931c
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2020-05-05 09:21:07 -0400

    [Fleet] Fix display of local_metadata

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/fleet/agent_list_page/index.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/fleet/agent_list_page/index.tsx
@@ -146,6 +146,13 @@ const RowActions = React.memo<{ agent: Agent; onReassignClick: () => void; refre
   }
 );
 
+function safeMetadata(val: any) {
+  if (typeof val !== 'string') {
+    return '-';
+  }
+  return val;
+}
+
 export const AgentListPage: React.FunctionComponent<{}> = () => {
   const defaultKuery: string = (useUrlParams().urlParams.kuery as string) || '';
   const hasWriteCapabilites = useCapabilities().write;
@@ -238,13 +245,13 @@ export const AgentListPage: React.FunctionComponent<{}> = () => {
 
   const columns = [
     {
-      field: 'local_metadata.host',
+      field: 'local_metadata.host.hostname',
       name: i18n.translate('xpack.ingestManager.agentList.hostColumnTitle', {
         defaultMessage: 'Host',
       }),
       render: (host: string, agent: Agent) => (
         <ConnectedLink color="primary" path={`${FLEET_AGENT_DETAIL_PATH}${agent.id}`}>
-          {agent.local_metadata['host.hostname'] || host || ''}
+          {safeMetadata(host)}
         </ConnectedLink>
       ),
     },
@@ -308,13 +315,12 @@ export const AgentListPage: React.FunctionComponent<{}> = () => {
       },
     },
     {
-      field: 'local_metadata.version',
+      field: 'local_metadata["elastic.agent"].version',
       width: '100px',
       name: i18n.translate('xpack.ingestManager.agentList.versionTitle', {
         defaultMessage: 'Version',
       }),
-      render: (version: string, agent: Agent) =>
-        agent.local_metadata['agent.version'] || version || '',
+      render: (version: string, agent: Agent) => safeMetadata(version),
     },
     {
       field: 'last_checkin',
@@ -505,7 +511,7 @@ export const AgentListPage: React.FunctionComponent<{}> = () => {
         loading={isLoading && agentsRequest.isInitialRequest}
         hasActions={true}
         noItemsMessage={
-          isLoading ? (
+          isLoading && agentsRequest.isInitialRequest ? (
             <FormattedMessage
               id="xpack.ingestManager.agentList.loadingAgentsMessage"
               defaultMessage="Loading agents…"
