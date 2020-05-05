caa1fd48c505c7cf69e58ab9330d99abea4d00a1
Constance <constancecchen@users.noreply.github.com>
2020-05-05 14:06:46 -0700

    Update Setup Guide installation instructions (#9)
    Co-authored-by: Chris Cressman <chris@chriscressman.com>

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/setup_guide/setup_guide.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/setup_guide/setup_guide.tsx
@@ -17,10 +17,11 @@ import {
   EuiText,
   EuiImage,
   EuiIcon,
-  EuiTabbedContent,
   EuiSteps,
   EuiCode,
   EuiCodeBlock,
+  EuiAccordion,
+  EuiLink,
 } from '@elastic/eui';
 
 import { SetAppSearchBreadcrumbs as SetBreadcrumbs } from '../../../shared/kibana_breadcrumbs';
@@ -68,8 +69,8 @@ export const SetupGuide: React.FC<> = () => {
 
         <EuiTitle size="s">
           <p>
-            Set up App Search to leverage dashboards, analytics, and APIs for advanced application
-            search made simple.
+            Elastic App Search provides user-friendly tools to design and deploy a powerful search
+            to your websites or web/mobile applications.
           </p>
         </EuiTitle>
         <EuiSpacer size="m" />
@@ -83,92 +84,97 @@ export const SetupGuide: React.FC<> = () => {
 
       <EuiPageBody>
         <EuiPageContent>
-          <EuiTabbedContent
-            tabs={[
+          <EuiSteps
+            headingElement="h2"
+            steps={[
               {
-                id: 'cloud',
-                name: 'Cloud',
-                content: (
-                  <>
-                    <EuiSpacer />
-                    <EuiSteps
-                      headingElement="h2"
-                      steps={[
-                        {
-                          title: 'Step 1 has intro plus code snippet',
-                          children: (
-                            <EuiText>
-                              <p>Run this code snippet to install things.</p>
-                              <EuiCodeBlock language="bash">npm install</EuiCodeBlock>
-                            </EuiText>
-                          ),
-                        },
-                        {
-                          title: 'Reload your Kibana instance',
-                          children: (
-                            <EuiText>
-                              <p>Run this code snippet to install things.</p>
-                              <EuiCodeBlock language="bash">npm install</EuiCodeBlock>
-                            </EuiText>
-                          ),
-                        },
-                      ]}
-                    />
-                  </>
+                title: 'Add your App Search host URL to your Kibana configuration',
+                children: (
+                  <EuiText>
+                    <p>
+                      Within your <EuiCode>config/kibana.yml</EuiCode> file, set{' '}
+                      <EuiCode>enterpriseSearch.host</EuiCode> to the URL of your App Search
+                      instance. For example:
+                    </p>
+                    <EuiCodeBlock language="yml">
+                      enterpriseSearch.host: &apos;http://localhost:3002&apos;
+                    </EuiCodeBlock>
+                  </EuiText>
                 ),
               },
               {
-                id: 'smas',
-                name: 'Self-Managed',
-                content: (
+                title: 'Reload your Kibana instance',
+                children: (
+                  <EuiText>
+                    <p>
+                      Restart Kibana to pick up the configuration changes from the previous step.
+                    </p>
+                    <p>
+                      If you’re using{' '}
+                      <EuiLink
+                        href="https://swiftype.com/documentation/app-search/self-managed/security#elasticsearch-native-realm"
+                        target="_blank"
+                      >
+                        Elasticsearch Native
+                      </EuiLink>{' '}
+                      auth within App Search - you’re all set! All users should be able to use App
+                      Search in Kibana automatically, inheriting the existing access and permissions
+                      they have within App Search.
+                    </p>
+                  </EuiText>
+                ),
+              },
+              {
+                title: 'Troubleshooting issues',
+                children: (
                   <>
+                    <EuiAccordion
+                      buttonContent="App Search and Kibana are on different Elasticsearch clusters"
+                      id="standard-auth"
+                      paddingSize="s"
+                    >
+                      <EuiText>
+                        <p>
+                          This plugin does not currently support App Search and Kibana running on
+                          different clusters.
+                        </p>
+                      </EuiText>
+                    </EuiAccordion>
                     <EuiSpacer />
-                    <EuiSteps
-                      headingElement="h2"
-                      steps={[
-                        {
-                          title: 'Add your App Search host URL to your Kibana configuration',
-                          children: (
-                            <EuiText>
-                              <p>
-                                Within your <EuiCode>config/kibana.yml</EuiCode> file, add the
-                                following the host URL of your App Search instace as{' '}
-                                <EuiCode>app_search.host</EuiCode>.
-                              </p>
-                              <EuiCodeBlock language="yml">
-                                app_search.host: &apos;http://localhost:3002&apos;
-                              </EuiCodeBlock>
-                            </EuiText>
-                          ),
-                        },
-                        {
-                          title: 'Reload your Kibana instance',
-                          children: (
-                            <EuiText>
-                              <p>
-                                After updating your Kibana config file, restart Kibana to pick up
-                                your changes.
-                              </p>
-                            </EuiText>
-                          ),
-                        },
-                        {
-                          title:
-                            'Ensure that your Kibana users have corresponding App Search accounts',
-                          children: (
-                            <EuiText>
-                              <p>If you’re using Elasticsearch Native auth - you’re all set.</p>
-                              <p>
-                                (If you’re using standard auth) Log into App Search and invite your
-                                Kibana users into App Search! Be sure to use their corresponding
-                                Kibana usernames.
-                              </p>
-                              <p>If you’re on SAML auth - ??????</p>
-                            </EuiText>
-                          ),
-                        },
-                      ]}
-                    />
+                    <EuiAccordion
+                      buttonContent="App Search and Kibana are on different authentication methods"
+                      id="standard-auth"
+                      paddingSize="s"
+                    >
+                      <EuiText>
+                        <p>
+                          This plugin does not currently support App Search and Kibana operating on
+                          different authentication methods (for example, App Search using a
+                          different SAML provider than Kibana).
+                        </p>
+                      </EuiText>
+                    </EuiAccordion>
+                    <EuiSpacer />
+                    <EuiAccordion
+                      buttonContent="App Search on Standard authentication"
+                      id="standard-auth"
+                      paddingSize="s"
+                    >
+                      <EuiText>
+                        <p>
+                          App Search operating on{' '}
+                          <EuiLink
+                            href="https://swiftype.com/documentation/app-search/self-managed/security#standard"
+                            target="_blank"
+                          >
+                            Standard Auth
+                          </EuiLink>{' '}
+                          is currently not fully supported by this plugin. Users created in App
+                          Search must be granted Kibana access. Users created in Kibana will see
+                          &quot;Cannot find App Search account&quot; error messages.
+                        </p>
+                      </EuiText>
+                    </EuiAccordion>
                   </>
                 ),
               },
