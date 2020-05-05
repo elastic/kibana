4328c378c10469cc8ee0754e481f9e220c186711
Walter Rafelsberger <walter@elastic.co>
2020-05-05 21:27:16 +0200

    [ML] Transforms: Edit transform flyout. (#65033)
    Adds an option to be able to edit certain attributes of transforms. This PR adds the ability to edit the description and frequency of the transform config. The PR is prepration to add support for editing throttling in a follow up.

---
+++ x-pack/plugins/transform/public/app/sections/transform_management/components/edit_transform_flyout/use_edit_transform_flyout.ts
@@ -0,0 +1,190 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { isEqual } from 'lodash';
+import { useReducer } from 'react';
+
+import { i18n } from '@kbn/i18n';
+
+import { TransformPivotConfig } from '../../../../common';
+
+const stringNotValidErrorMessage = i18n.translate(
+  'xpack.transform.transformList.editFlyoutFormStringNotValidErrorMessage',
+  {
+    defaultMessage: 'Value needs to be of type string.',
+  }
+);
+
+type Validator = (arg: any) => string[];
+
+// The way the current form is set up,
+// this validator is just a sanity check,
+// it should never trigger an error.
+const stringValidator: Validator = arg =>
+  typeof arg === 'string' ? [] : [stringNotValidErrorMessage];
+
+const frequencyNotValidErrorMessage = i18n.translate(
+  'xpack.transform.transformList.editFlyoutFormFrequencyNotValidErrorMessage',
+  {
+    defaultMessage: 'The frequency value is not valid.',
+  }
+);
+
+// Only allow frequencies in the form of 1s/1h etc.
+export const frequencyValidator: Validator = arg => {
+  if (typeof arg !== 'string' || arg === null) {
+    return [stringNotValidErrorMessage];
+  }
+
+  // split string by groups of numbers and letters
+  const regexStr = arg.match(/[a-z]+|[^a-z]+/gi);
+
+  return (
+    // only valid if one group of numbers and one group of letters
+    regexStr !== null &&
+      regexStr.length === 2 &&
+      // only valid if time unit is one of s/m/h
+      ['s', 'm', 'h'].includes(regexStr[1]) &&
+      // only valid if number is between 1 and 59
+      parseInt(regexStr[0], 10) > 0 &&
+      parseInt(regexStr[0], 10) < 60 &&
+      // if time unit is 'h' then number must not be higher than 1
+      !(parseInt(regexStr[0], 10) > 1 && regexStr[1] === 'h')
+      ? []
+      : [frequencyNotValidErrorMessage]
+  );
+};
+
+interface Validate {
+  [key: string]: Validator;
+}
+
+const validate: Validate = {
+  string: stringValidator,
+  frequency: frequencyValidator,
+};
+
+interface Field {
+  errorMessages: string[];
+  isOptional: boolean;
+  validator: keyof typeof validate;
+  value: string;
+}
+
+const defaultField: Field = {
+  errorMessages: [],
+  isOptional: true,
+  validator: 'string',
+  value: '',
+};
+
+interface EditTransformFlyoutFieldsState {
+  [key: string]: Field;
+  description: Field;
+  frequency: Field;
+}
+
+export interface EditTransformFlyoutState {
+  formFields: EditTransformFlyoutFieldsState;
+  isFormTouched: boolean;
+  isFormValid: boolean;
+}
+
+// This is not a redux type action,
+// since for now we only have one action type.
+interface Action {
+  field: keyof EditTransformFlyoutFieldsState;
+  value: string;
+}
+
+// Some attributes can have a value of `null` to trigger
+// a reset to the default value.
+interface UpdateTransformPivotConfig {
+  description: string;
+  frequency: string;
+}
+
+// Takes in the form configuration and returns a
+// request object suitable to be sent to the
+// transform update API endpoint.
+export const applyFormFieldsToTransformConfig = (
+  config: TransformPivotConfig,
+  { description, frequency }: EditTransformFlyoutFieldsState
+): Partial<UpdateTransformPivotConfig> => {
+  const updateConfig: Partial<UpdateTransformPivotConfig> = {};
+
+  // set the values only if they changed from the default
+  // and actually differ from the previous value.
+  if (
+    !(config.frequency === undefined && frequency.value === '') &&
+    config.frequency !== frequency.value
+  ) {
+    updateConfig.frequency = frequency.value;
+  }
+
+  if (
+    !(config.description === undefined && description.value === '') &&
+    config.description !== description.value
+  ) {
+    updateConfig.description = description.value;
+  }
+
+  return updateConfig;
+};
+
+// Takes in a transform configuration and returns
+// the default state to populate the form.
+export const getDefaultState = (config: TransformPivotConfig): EditTransformFlyoutState => ({
+  formFields: {
+    description: { ...defaultField, value: config?.description ?? '' },
+    frequency: { ...defaultField, value: config?.frequency ?? '', validator: 'frequency' },
+  },
+  isFormTouched: false,
+  isFormValid: true,
+});
+
+// Checks each form field for error messages to return
+// if the overall form is valid or not.
+const isFormValid = (fieldsState: EditTransformFlyoutFieldsState) =>
+  Object.keys(fieldsState).reduce((p, c) => p && fieldsState[c].errorMessages.length === 0, true);
+
+// Updates a form field with its new value,
+// runs validation and populates
+// `errorMessages` if any errors occur.
+const formFieldReducer = (state: Field, value: string): Field => {
+  return {
+    ...state,
+    errorMessages: state.isOptional && value.length === 0 ? [] : validate[state.validator](value),
+    value,
+  };
+};
+
+// Main form reducer triggers
+// - `formFieldReducer` to update the actions field
+// - compares the most recent state against the original one to update `isFormTouched`
+// - sets `isFormValid` to have a flag if any of the form fields contains an error.
+export const formReducerFactory = (config: TransformPivotConfig) => {
+  const defaultState = getDefaultState(config);
+  return (state: EditTransformFlyoutState, { field, value }: Action): EditTransformFlyoutState => {
+    const formFields = {
+      ...state.formFields,
+      [field]: formFieldReducer(state.formFields[field], value),
+    };
+
+    return {
+      ...state,
+      formFields,
+      isFormTouched: !isEqual(defaultState.formFields, formFields),
+      isFormValid: isFormValid(formFields),
+    };
+  };
+};
+
+export const useEditTransformFlyout = (config: TransformPivotConfig) => {
+  return useReducer(formReducerFactory(config), getDefaultState(config));
+};
+
+export type UseEditTransformFlyoutReturnType = ReturnType<typeof useEditTransformFlyout>;
