f62d084f45e47b1edb929825c2494973349e9a5c
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-05-05 10:27:56 +0200

    [ML] Embeddable Anomaly Swimlane (#65180) (#65203)
    * [ML] Embeddable Anomaly Swimlane (#64056)
    
    * [ML] embeddables setup
    
    * [ML] fix initialization
    
    * [ML] ts refactoring
    
    * [ML] refactor time_buckets.js
    
    * [ML] async services
    
    * [ML] extract job_selector_flyout.tsx
    
    * [ML] fetch overall swimlane data
    
    * [ML] import explorer styles
    
    * [ML] revert package_globs.ts
    
    * [ML] refactor with container, services with DI
    
    * [ML] resize throttle, fetch based on chart width
    
    * [ML] swimlane embeddable setup
    
    * [ML] explorer service
    
    * [ML] chart_tooltip_service
    
    * [ML] fix types
    
    * [ML] overall type for single job with no influencers
    
    * [ML] improve anomaly_swimlane_initializer ux
    
    * [ML] fix services initialization, unsubscribe on destroy
    
    * [ML] support custom time range
    
    * [ML] add tooltip
    
    * [ML] rollback initGetSwimlaneBucketInterval
    
    * [ML] new tooltip service
    
    * [ML] MlTooltipComponent with render props, fix warning
    
    * [ML] fix typo in the filename
    
    * [ML] remove redundant time range output
    
    * [ML] fix time_buckets.test.js jest tests
    
    * [ML] fix explorer chart tests
    
    * [ML] swimlane tests
    
    * [ML] store job ids instead of complete job objects
    
    * [ML] swimlane limit input
    
    * [ML] memo tooltip component, loading indicator
    
    * [ML] scrollable content
    
    * [ML] support query and filters
    
    * [ML] handle query syntax errors
    
    * [ML] rename anomaly_swimlane_service
    
    * [ML] introduce constants
    
    * [ML] edit panel title during setup
    
    * [ML] withTimeRangeSelector
    
    * [ML] rename explorer_service
    
    * [ML] getJobs$ method with one API call
    
    * [ML] fix groups selection
    
    * [ML] swimlane input resolver hook
    
    * [ML] useSwimlaneInputResolver tests
    
    * [ML] factory test
    
    * [ML] container test
    
    * [ML] set wrapper
    
    * [ML] tooltip tests
    
    * [ML] fix displayScore
    
    * [ML] label colors
    
    * [ML] support edit mode
    
    * [ML] call super render
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit f62df99ae38801b905dd10d281dbf972c5b17578)
    
    * [ML] fix typing issues
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

---
+++ x-pack/plugins/ml/public/embeddables/anomaly_swimlane/anomaly_swimlane_setup_flyout.tsx
@@ -0,0 +1,95 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { IUiSettingsClient, OverlayStart } from 'kibana/public';
+import { i18n } from '@kbn/i18n';
+import moment from 'moment';
+import { VIEW_BY_JOB_LABEL } from '../../application/explorer/explorer_constants';
+import { toMountPoint } from '../../../../../../src/plugins/kibana_react/public';
+import { AnomalySwimlaneInitializer } from './anomaly_swimlane_initializer';
+import { JobSelectorFlyout } from '../../application/components/job_selector/job_selector_flyout';
+import { AnomalyDetectorService } from '../../application/services/anomaly_detector_service';
+import { getInitialGroupsMap } from '../../application/components/job_selector/job_selector';
+import { AnomalySwimlaneEmbeddableInput } from './anomaly_swimlane_embeddable';
+
+export async function resolveAnomalySwimlaneUserInput(
+  {
+    overlays,
+    anomalyDetectorService,
+    uiSettings,
+  }: {
+    anomalyDetectorService: AnomalyDetectorService;
+    overlays: OverlayStart;
+    uiSettings: IUiSettingsClient;
+  },
+  input?: AnomalySwimlaneEmbeddableInput
+): Promise<Partial<AnomalySwimlaneEmbeddableInput>> {
+  return new Promise(async (resolve, reject) => {
+    const maps = {
+      groupsMap: getInitialGroupsMap([]),
+      jobsMap: {},
+    };
+
+    const tzConfig = uiSettings.get('dateFormat:tz');
+    const dateFormatTz = tzConfig !== 'Browser' ? tzConfig : moment.tz.guess();
+
+    const selectedIds = input?.jobIds;
+
+    const flyoutSession = overlays.openFlyout(
+      toMountPoint(
+        <JobSelectorFlyout
+          selectedIds={selectedIds}
+          withTimeRangeSelector={false}
+          dateFormatTz={dateFormatTz}
+          singleSelection={false}
+          timeseriesOnly={true}
+          onFlyoutClose={() => {
+            flyoutSession.close();
+            reject();
+          }}
+          onSelectionConfirmed={async ({ jobIds, groups }) => {
+            const title =
+              input?.title ??
+              i18n.translate('xpack.ml.swimlaneEmbeddable.title', {
+                defaultMessage: 'ML anomaly swimlane for {jobIds}',
+                values: { jobIds: jobIds.join(', ') },
+              });
+
+            const jobs = await anomalyDetectorService.getJobs$(jobIds).toPromise();
+
+            const influencers = anomalyDetectorService.extractInfluencers(jobs);
+            influencers.push(VIEW_BY_JOB_LABEL);
+
+            await flyoutSession.close();
+
+            const modalSession = overlays.openModal(
+              toMountPoint(
+                <AnomalySwimlaneInitializer
+                  defaultTitle={title}
+                  influencers={influencers}
+                  initialInput={input}
+                  onCreate={({ panelTitle, viewBy, swimlaneType, limit }) => {
+                    modalSession.close();
+                    resolve({ jobIds, title: panelTitle, swimlaneType, viewBy, limit });
+                  }}
+                  onCancel={() => {
+                    modalSession.close();
+                    reject();
+                  }}
+                />
+              )
+            );
+          }}
+          maps={maps}
+        />
+      ),
+      {
+        'data-test-subj': 'mlAnomalySwimlaneEmbeddable',
+      }
+    );
+  });
+}
