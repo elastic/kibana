489155ee802a01fbd14d32cb56f9b2a3a111cc9c
Thomas Neirynck <thomas@elastic.co>
2020-05-05 13:32:07 -0400

    [7.x] [Maps] Code split Maps app (#64594) (#65178)
    * [Maps] Code split Maps app (#64594)
    
    Code-split the maps-plugin to reduce the initial `maps.plugin.js` size.
    
    There were two main code dependencies in the plugin initialization that were the root cause of the large bundle size.
    
    - `GisMap` wraps the entire application UX, including the add-layer-wizard. The layer wizards only need to be available there. This PR moves the `load_layer_wizard` dependency from the plugin-initialization to the `GisMap` component. 
    - The `MapEmbeddableFactory` needs to be registered at plugin-initialization. However, this module imports a lot of core-application code. By code-splitting here, we avoid pulling in the entire Maps-app in the main bundle.
    
    This also  introduces a lazy-initialization of the `GisMap` itself as an additional split to further reduce size of the bundles.
    
    * fix merge
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/maps/public/plugin.ts
+++ x-pack/plugins/maps/public/plugin.ts
@@ -38,9 +38,8 @@ import { featureCatalogueEntry } from './feature_catalogue_entry';
 import { getMapsVisTypeAlias } from './maps_vis_type_alias';
 import { HomePublicPluginSetup } from '../../../../src/plugins/home/public';
 import { VisualizationsSetup } from '../../../../src/plugins/visualizations/public';
-import { registerLayerWizards } from './layers/load_layer_wizards';
 import { MAP_SAVED_OBJECT_TYPE } from '../common/constants';
-import { MapEmbeddableFactory } from './embeddable';
+import { MapEmbeddableFactory } from './embeddable/map_embeddable_factory';
 import { EmbeddableSetup } from '../../../../src/plugins/embeddable/public';
 
 export interface MapsPluginSetupDependencies {
@@ -85,7 +84,6 @@ export const bindStartCoreAndPlugins = (core: CoreStart, plugins: any) => {
   setUiActions(plugins.uiActions);
   setNavigation(plugins.navigation);
   setCoreI18n(core.i18n);
-  registerLayerWizards();
 };
 
 /**
