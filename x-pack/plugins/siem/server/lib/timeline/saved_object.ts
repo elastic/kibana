d8336bed20cc06eb8fa5a40bf8cd0c1597d897e5
Angela Chuang <yi-chun.chuang@elastic.co>
2020-05-05 12:28:02 +0100

    add comments for filters

--- x-pack/plugins/siem/server/lib/timeline/saved_object.ts
+++ x-pack/plugins/siem/server/lib/timeline/saved_object.ts
@@ -95,12 +95,18 @@ export const getTimelineByTemplateTimelineId = async (
   return getAllSavedTimeline(request, options);
 };
 
+/** The filter here is able to handle the legacy data,
+ * which has no timelineType exists in the savedObject */
 const getTimelineTypeFilter = (timelineTypes: string | null) => {
   if (timelineTypes === TimelineType.template) {
+    /** Show only whose timelineType is "template" */
     return `siem-ui-timeline.attributes.timelineType: ${TimelineType.template}`;
   }
 
   if (timelineTypes === TimelineType.default) {
+    /** Show me every timeline whose timelineType is not "template".
+     * which includes timelineType === 'default' and
+     * those timelineType doesn't exists */
     return `not siem-ui-timeline.attributes.timelineType: ${TimelineType.template}`;
   }
 
