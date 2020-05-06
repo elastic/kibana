958bf2d487a99751c363d2d06b7e602d79d97c66
Jason Rhodes <jason.rhodes@elastic.co>
2020-05-06 13:33:05 -0400

    [7.x] Removes legacy infra plugin and moves saved objects registration to NP (#64848) (#65327)

--- x-pack/index.js
+++ x-pack/index.js
@@ -12,7 +12,6 @@ import { dashboardMode } from './legacy/plugins/dashboard_mode';
 import { beats } from './legacy/plugins/beats_management';
 import { maps } from './legacy/plugins/maps';
 import { spaces } from './legacy/plugins/spaces';
-import { infra } from './legacy/plugins/infra';
 import { encryptedSavedObjects } from './legacy/plugins/encrypted_saved_objects';
 import { ingestManager } from './legacy/plugins/ingest_manager';
 
@@ -26,7 +25,6 @@ module.exports = function(kibana) {
     dashboardMode(kibana),
     beats(kibana),
     maps(kibana),
-    infra(kibana),
     encryptedSavedObjects(kibana),
     ingestManager(kibana),
   ];
