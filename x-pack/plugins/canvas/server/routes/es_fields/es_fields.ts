b8bea5b8b2798dac2fe1546c53eded434d408a50
Corey Robertson <corey.robertson@elastic.co>
2020-05-05 20:52:56 -0400

    [Canvas] move files from legacy/plugins to plugins (#65283)
    * Moves the files
    
    * Rename legacy/plugins/canvas paths
    
    * Correct paths
    
    * Fix shareable_runtime and storybook
    
    * ESLint Fixes
    
    * Fix jest test and paths

--- x-pack/plugins/canvas/server/routes/es_fields/es_fields.ts
+++ x-pack/plugins/canvas/server/routes/es_fields/es_fields.ts
@@ -6,7 +6,7 @@
 
 import { mapValues, keys } from 'lodash';
 import { schema } from '@kbn/config-schema';
-import { API_ROUTE } from '../../../../../legacy/plugins/canvas/common/lib';
+import { API_ROUTE } from '../../../common/lib';
 import { catchErrorHandler } from '../catch_error_handler';
 // @ts-ignore unconverted lib
 import { normalizeType } from '../../lib/normalize_type';
--- x-pack/plugins/canvas/server/routes/workpad/delete.ts
+++ x-pack/plugins/canvas/server/routes/workpad/delete.ts
@@ -6,10 +6,7 @@
 
 import { schema } from '@kbn/config-schema';
 import { RouteInitializerDeps } from '../';
-import {
-  CANVAS_TYPE,
-  API_ROUTE_WORKPAD,
-} from '../../../../../legacy/plugins/canvas/common/lib/constants';
+import { CANVAS_TYPE, API_ROUTE_WORKPAD } from '../../../common/lib/constants';
 import { okResponse } from '../ok_response';
 import { catchErrorHandler } from '../catch_error_handler';
 
