e312b205f2da357244d30d0d24c72c7787f4fde1
Gidi Meir Morris <github@gidi.io>
2020-05-07 15:45:08 +0100

    spinner is configurable

--- x-pack/plugins/triggers_actions_ui/public/application/lib/suspended_component_with_props.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/lib/suspended_component_with_props.tsx
@@ -4,13 +4,23 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React, { Suspense } from 'react';
-import { EuiLoadingSpinner } from '@elastic/eui';
+import { EuiLoadingSpinner, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiLoadingSpinnerSize } from '@elastic/eui/src/components/loading/loading_spinner';
 
 export function suspendedComponentWithProps<T = unknown>(
-  ComponentToSuspend: React.ComponentType<T>
+  ComponentToSuspend: React.ComponentType<T>,
+  size?: EuiLoadingSpinnerSize
 ) {
   return (props: T) => (
-    <Suspense fallback={<EuiLoadingSpinner />}>
+    <Suspense
+      fallback={
+        <EuiFlexGroup justifyContent="center">
+          <EuiFlexItem grow={false}>
+            <EuiLoadingSpinner size={size ?? 'm'} />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      }
+    >
       <ComponentToSuspend {...props} />
     </Suspense>
   );
