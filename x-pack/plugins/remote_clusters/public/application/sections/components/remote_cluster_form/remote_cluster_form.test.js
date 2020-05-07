1b4140db4c077e12fb96a461ef2e96c71c2a5b69
Greg Thompson <thompson.glowe@gmail.com>
2020-05-07 15:46:19 -0500

    makeId -> htmlIdGenerator mocks

--- x-pack/plugins/remote_clusters/public/application/sections/components/remote_cluster_form/remote_cluster_form.test.js
+++ x-pack/plugins/remote_clusters/public/application/sections/components/remote_cluster_form/remote_cluster_form.test.js
@@ -10,7 +10,6 @@ import { findTestSubject, takeMountedSnapshot } from '@elastic/eui/lib/test';
 import { RemoteClusterForm } from './remote_cluster_form';
 
 // Make sure we have deterministic aria IDs.
-jest.mock('@elastic/eui/lib/components/form/form_row/make_id', () => () => 'mockId');
 jest.mock('@elastic/eui/lib/services/accessibility/html_id_generator', () => ({
   htmlIdGenerator: (prefix = 'staticGenerator') => (suffix = 'staticId') => `${prefix}_${suffix}`,
 }));
