4bece3544f0a55e45104f2f0a367a2384b5a406b
Joe Reuter <johannes.reuter@elastic.co>
2020-05-07 18:11:39 +0200

    fix drilldown usage of trigger contexts

--- x-pack/plugins/lens/public/editor_frame_service/editor_frame/workspace_panel.tsx
+++ x-pack/plugins/lens/public/editor_frame_service/editor_frame/workspace_panel.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useState, useEffect, useMemo, useContext, useCallback } from 'react';
+import React, { useState, useEffect, useMemo, useContext } from 'react';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { i18n } from '@kbn/i18n';
 import {
