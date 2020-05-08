157df85717929ef1e0205311bafb30a15dbdbeff
nickofthyme <nick.ryan.partridge@gmail.com>
2020-05-08 14:13:26 -0500

    build: remove notice check, selenium tests, fix jest tests

--- src/core_plugins/metrics/server/lib/vis_data/helpers/overwrite.js
+++ src/core_plugins/metrics/server/lib/vis_data/helpers/overwrite.js
@@ -1,7 +1,7 @@
 /*
 * Licensed to Elasticsearch B.V. under one or more contributor
 * license agreements. See the NOTICE file distributed with
-  * this work for additional information regarding copyright
+* this work for additional information regarding copyright
 * ownership. Elasticsearch B.V. licenses this file to you under
 * the Apache License, Version 2.0 (the "License"); you may
 * not use this file except in compliance with the License.
