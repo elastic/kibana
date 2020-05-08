a265d9c71011f22c5feb7382bfa6ad14f3ccd9e2
Marco Vettorello <vettorello.marco@gmail.com>
2020-05-08 16:15:01 +0200

    [TSVB] fix memory leak

--- src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/series/date_histogram.js
+++ src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/series/date_histogram.js
@@ -20,7 +20,7 @@
 import getBucketSize from '../../helpers/get_bucket_size';
 import offsetTime from '../../offset_time';
 import getIntervalAndTimefield from '../../get_interval_and_timefield';
-import { set } from 'lodash';
+import { overwrite } from '../../helpers';
 export default function dateHistogram(req, panel, series) {
   return next => doc => {
     const { timeField, interval } = getIntervalAndTimefield(panel, series);
@@ -28,7 +28,7 @@ export default function dateHistogram(req, panel, series) {
     const { from, to }  = offsetTime(req, series.offset_time);
     const { timezone } = req.payload.timerange;
 
-    set(doc, `aggs.${series.id}.aggs.timeseries.date_histogram`, {
+    overwrite(doc, `aggs.${series.id}.aggs.timeseries.date_histogram`, {
       field: timeField,
       interval: intervalString,
       min_doc_count: 0,
@@ -38,7 +38,7 @@ export default function dateHistogram(req, panel, series) {
         max: to.valueOf()
       }
     });
-    set(doc, `aggs.${series.id}.meta`, {
+    overwrite(doc, `aggs.${series.id}.meta`, {
       timeField,
       intervalString,
       bucketSize
