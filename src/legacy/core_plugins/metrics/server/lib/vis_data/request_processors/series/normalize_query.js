8bf51517b5bab4e20956608930fbf716ffc47479
Marco Vettorello <vettorello.marco@gmail.com>
2020-05-08 22:56:36 +0200

    Revert "[TSVB] Group by "Everything" fails to calculate data with "Overall" metrics (#42074) (#42351)"
    This reverts commit 5efcf19b51cb9c9a01a85a3c042c4a96a64f82ab.

--- src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/series/normalize_query.js
+++ src/legacy/core_plugins/metrics/server/lib/vis_data/request_processors/series/normalize_query.js
@@ -20,20 +20,16 @@ import { get, isEmpty } from 'lodash';
 import { overwrite } from '../../helpers';
 
 const isEmptyFilter = (filter = {}) => Boolean(filter.match_all) && isEmpty(filter.match_all);
-const hasSiblingPipelineAggregation = (aggs = {}) => Object.keys(aggs).length > 1;
 
 /* For grouping by the 'Everything', the splitByEverything request processor
  * creates fake .filter.match_all filter (see split_by_everything.js) to simplify the request processors code.
  * But “filters” are not supported by all of available search strategies (e.g. Rollup search).
  * This method removes that aggregation.
- *
- * Important: for Sibling Pipeline aggregation we cannot apply this logic
- *
  */
 function removeEmptyTopLevelAggregation(doc, series) {
   const filter = get(doc, `aggs.${series.id}.filter`);
 
-  if (isEmptyFilter(filter) && !hasSiblingPipelineAggregation(doc.aggs[series.id].aggs)) {
+  if (isEmptyFilter(filter)) {
     const meta = get(doc, `aggs.${series.id}.meta`);
     overwrite(doc, `aggs`, doc.aggs[series.id].aggs);
     overwrite(doc, `aggs.timeseries.meta`, meta);
