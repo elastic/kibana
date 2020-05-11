4912153ce6c6943815f4944e21de6b6b08597fe7
Mikhail Shustov <restrry@gmail.com>
2020-05-11 18:43:45 +0200

    don't register any features in LP. (#65611)
    * don't register any features in LP. breaks features value reading in KP
    
    * move test plugin to NP
    
    * fix mappings
    
    * update docs
    
    * migrate another test
    
    * use contstants file for BWC with original code
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/maps/server/index.ts
+++ x-pack/plugins/maps/server/index.ts
@@ -3,3 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import { PluginInitializerContext } from 'src/core/server';
+import { MapsPlugin } from './plugin';
+
+export const plugin = (initializerContext: PluginInitializerContext) =>
+  new MapsPlugin(initializerContext);
