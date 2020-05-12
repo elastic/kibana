2446c9692ba0cb07a0541d6bf673acd53a237d4e
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2020-05-11 18:42:26 -0700

    fixed test

--- x-pack/plugins/uptime/public/lib/alert_types/__tests__/monitor_status.test.ts
+++ x-pack/plugins/uptime/public/lib/alert_types/__tests__/monitor_status.test.ts
@@ -174,7 +174,7 @@ describe('monitor status alert type', () => {
       {{context.downMonitorsWithGeo}}",
         "iconClass": "uptimeApp",
         "id": "xpack.uptime.alerts.monitorStatus",
-        "isEditable": false,
+        "requiresAppContext": false,
         "name": <MonitorStatusTitle />,
         "validate": [Function],
       }
