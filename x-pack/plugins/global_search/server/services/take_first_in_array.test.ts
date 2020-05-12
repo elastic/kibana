aa9a0363491e9a0caa49291888c2055c5fa33ff0
pgayvallet <pierre.gayvallet@elastic.co>
2020-05-12 08:36:13 +0200

    add utils tests

---
+++ x-pack/plugins/global_search/server/services/take_first_in_array.test.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { TestScheduler } from 'rxjs/testing';
+import { takeFirstInArray } from './take_first_in_array';
+
+const getTestScheduler = () =>
+  new TestScheduler((actual, expected) => {
+    expect(actual).toEqual(expected);
+  });
+
+describe('takeFirstInArray', () => {
+  it('only emits a given `count` of items from an array observable', () => {
+    getTestScheduler().run(({ expectObservable, hot }) => {
+      const source = hot('a-b-c', { a: [1], b: [2], c: [3] });
+      const expected = 'a-(b|)';
+
+      expectObservable(source.pipe(takeFirstInArray(2))).toBe(expected, {
+        a: [1],
+        b: [2],
+      });
+    });
+  });
+
+  it('completes if the source completes before reaching the given `count`', () => {
+    getTestScheduler().run(({ expectObservable, hot }) => {
+      const source = hot('a-b-c-|', { a: [1, 2], b: [3, 4], c: [5] });
+      const expected = 'a-b-c-|';
+
+      expectObservable(source.pipe(takeFirstInArray(10))).toBe(expected, {
+        a: [1, 2],
+        b: [3, 4],
+        c: [5],
+      });
+    });
+  });
+
+  it('split the emission if `count` is reached in a given emission', () => {
+    getTestScheduler().run(({ expectObservable, hot }) => {
+      const source = hot('a-b-c', { a: [1, 2, 3], b: [4, 5, 6], c: [7, 8] });
+      const expected = 'a-(b|)';
+
+      expectObservable(source.pipe(takeFirstInArray(5))).toBe(expected, {
+        a: [1, 2, 3],
+        b: [4, 5],
+      });
+    });
+  });
+});
