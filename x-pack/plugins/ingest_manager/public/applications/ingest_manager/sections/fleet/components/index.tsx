00b2a52e431d2cb468877426d808795bf3088eee
Jen Huang <its.jenetic@gmail.com>
2020-05-12 16:30:27 -0700

    Remove unused nav components

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/fleet/components/index.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/fleet/components/index.tsx
@@ -7,7 +7,5 @@
 export * from './loading';
 export * from './agent_reassign_config_flyout';
 export * from './agent_enrollment_flyout';
-export * from './navigation/child_routes';
-export * from './navigation/connected_link';
 export * from './agent_health';
 export * from './agent_unenroll_provider';
