daea374908f0d1f0085ad550ab9ba9d1911fd3cc
patrykkopycinski <patryk.kopycinski@elastic.co>
2020-05-12 18:59:14 +0200

    [7.x] [SIEM][Detection Engine] Fix StepDescription value in StepRuleActions (#63677) (#66213)

--- x-pack/plugins/siem/public/pages/detection_engine/rules/components/description_step/index.tsx
+++ x-pack/plugins/siem/public/pages/detection_engine/rules/components/description_step/index.tsx
@@ -34,6 +34,8 @@ import {
 } from './helpers';
 import { useSiemJobs } from '../../../../../components/ml_popover/hooks/use_siem_jobs';
 import { buildMlJobDescription } from './ml_job_description';
+import { buildActionsDescription } from './actions_description';
+import { buildThrottleDescription } from './throttle_description';
 
 const DescriptionListContainer = styled(EuiDescriptionList)`
   &.euiDescriptionList--column .euiDescriptionList__title {
@@ -73,6 +75,15 @@ export const StepRuleDescriptionComponent: React.FC<StepRuleDescriptionProps> = 
         ),
       ];
     }
+
+    if (key === 'throttle') {
+      return [...acc, buildThrottleDescription(get(key, data), get([key, 'label'], schema))];
+    }
+
+    if (key === 'actions') {
+      return [...acc, buildActionsDescription(get(key, data), get([key, 'label'], schema))];
+    }
+
     return [...acc, ...buildListItems(data, pick(key, schema), filterManager, indexPatterns)];
   }, []);
 
