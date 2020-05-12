2e297de3267e7b0d64405b6263c020549efc9bca
Patryk Kopycinski <contact@patrykkopycinski.com>
2020-05-12 17:18:47 +0200

    lint

--- x-pack/legacy/plugins/siem/public/pages/detection_engine/rules/components/step_rule_actions/schema.tsx
+++ x-pack/legacy/plugins/siem/public/pages/detection_engine/rules/components/step_rule_actions/schema.tsx
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/* istanbul ignore file */
-
 import { i18n } from '@kbn/i18n';
 
 import { FormSchema } from '../../../../../shared_imports';
