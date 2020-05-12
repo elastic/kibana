3e3825198df8ccef73ddc23de49de3c9dd2ccda6
CJ Cenizal <cj@cenizal.com>
2020-05-12 15:49:43 -0700

    Remove undefined export. Fix Security and Spaces unit tests.

--- src/plugins/management/public/legacy/index.js
+++ src/plugins/management/public/legacy/index.js
@@ -17,5 +17,5 @@
  * under the License.
  */
 
-export { LegacyManagementAdapter, sections } from './sections_register';
+export { LegacyManagementAdapter } from './sections_register';
 export { LegacyManagementSection } from './section';
