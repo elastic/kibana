e644a38b85f1b7e2d85d7e43e711a22021b9ce07
FrankHassanabad <frank.hassanabad@elastic.co>
2020-05-12 13:24:40 -0600

    Updated mappings to be super set between list and list items

--- x-pack/plugins/lists/server/saved_objects/exception_list.ts
+++ x-pack/plugins/lists/server/saved_objects/exception_list.ts
@@ -10,7 +10,13 @@ export const exceptionListSavedObjectType = 'exception-list';
 export const exceptionListAgnosticSavedObjectType = 'exception-list-agnostic';
 export type SavedObjectType = 'exception-list' | 'exception-list-agnostic';
 
-export const exceptionListMapping: SavedObjectsType['mappings'] = {
+/**
+ * This is a super set of exception list and exception list items. The switch
+ * to determine if you are using an exception list vs. an exception list item
+ * is "list_type". If "list_type" is "list" then it is an exception list. If
+ * "list_type" is "item" then the type is an item.
+ */
+export const commonMapping: SavedObjectsType['mappings'] = {
   properties: {
     _tags: {
       type: 'keyword',
@@ -25,6 +31,9 @@ export const exceptionListMapping: SavedObjectsType['mappings'] = {
       type: 'keyword',
     },
     list_id: {
+      type: 'keyword',
+    },
+    list_type: {
       type: 'keyword',
     },
     meta: {
@@ -48,16 +57,58 @@ export const exceptionListMapping: SavedObjectsType['mappings'] = {
   },
 };
 
+export const exceptionListMapping: SavedObjectsType['mappings'] = {
+  properties: {
+    // There is nothing that is not also used within exceptionListItemMapping
+    // at this time but if there is it should go here
+  },
+};
+
+export const exceptionListItemMapping: SavedObjectsType['mappings'] = {
+  properties: {
+    comment: {
+      type: 'keyword',
+    },
+    entries: {
+      properties: {
+        field: {
+          type: 'keyword',
+        },
+        match: {
+          type: 'keyword',
+        },
+        match_any: {
+          type: 'keyword',
+        },
+        operator: {
+          type: 'keyword',
+        },
+      },
+    },
+    item_id: {
+      type: 'keyword',
+    },
+  },
+};
+
+const combinedMappings: SavedObjectsType['mappings'] = {
+  properties: {
+    ...commonMapping.properties,
+    ...exceptionListMapping.properties,
+    ...exceptionListItemMapping.properties,
+  },
+};
+
 export const exceptionListType: SavedObjectsType = {
   hidden: false,
-  mappings: exceptionListMapping,
+  mappings: combinedMappings,
   name: exceptionListSavedObjectType,
   namespaceType: 'single',
 };
 
 export const exceptionListAgnosticType: SavedObjectsType = {
   hidden: false,
-  mappings: exceptionListMapping,
+  mappings: combinedMappings,
   name: exceptionListAgnosticSavedObjectType,
   namespaceType: 'agnostic',
 };
