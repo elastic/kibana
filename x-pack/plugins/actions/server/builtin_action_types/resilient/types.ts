1235f343aa02757a8e7e223da1e993e380b66c96
Christos Nasikas <christos.nasikas@elastic.co>
2020-05-13 14:03:49 +0300

    Create schema and types

---
+++ x-pack/plugins/actions/server/builtin_action_types/resilient/types.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { TypeOf } from '@kbn/config-schema';
+import { ResilientPublicConfigurationSchema, ResilientSecretConfigurationSchema } from './schema';
+
+export type ResilientPublicConfigurationType = TypeOf<typeof ResilientPublicConfigurationSchema>;
+export type ResilientSecretConfigurationType = TypeOf<typeof ResilientSecretConfigurationSchema>;
+
+interface CreateIncidentBasicRequestArgs {
+  name: string;
+  description: string;
+  discovered_date: number;
+}
+
+interface Comment {
+  text: { format: string; content: string };
+}
+
+interface CreateIncidentRequestArgs extends CreateIncidentBasicRequestArgs {
+  comments?: Comment[];
+}
+
+export type CreateIncidentRequest = CreateIncidentRequestArgs;
+
+export type UpdateIncidentRequest = Partial<CreateIncidentBasicRequestArgs>;
