98a809954b7110a870bd622f810f80a053053ba7
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-05-13 16:36:53 +0200

    Revert "[ML] fix tooltip overflow"
    This reverts commit 103c36bc

--- x-pack/plugins/ml/public/application/components/chart_tooltip/chart_tooltip.tsx
+++ x-pack/plugins/ml/public/application/components/chart_tooltip/chart_tooltip.tsx
@@ -96,11 +96,6 @@ const Tooltip: FC<{ service: ChartTooltipService }> = React.memo(({ service }) =
 
   return (
     <TooltipTrigger
-      modifiers={{
-        preventOverflow: {
-          boundariesElement: 'window',
-        },
-      }}
       placement="right-start"
       trigger="none"
       tooltipShown={isTooltipShown}
