f42d29d2b097ddc345a7ae8d2f1a7588c3124488
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-05-13 11:11:45 +0200

    Bump dependencies

--- x-pack/plugins/apm/e2e/cypress/integration/snapshots.js
+++ x-pack/plugins/apm/e2e/cypress/integration/snapshots.js
@@ -1,16 +1,10 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 module.exports = {
-  APM: {
-    'Transaction duration charts': {
-      '1': '500 ms',
-      '2': '250 ms',
-      '3': '0 ms'
+  "APM": {
+    "Transaction duration charts": {
+      "1": "350 ms",
+      "2": "175 ms",
+      "3": "0 ms"
     }
   },
-  __version: '4.2.0'
-};
+  "__version": "4.2.0"
+}
