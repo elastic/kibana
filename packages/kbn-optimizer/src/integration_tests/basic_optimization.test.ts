5b14d95ed5ce92f96f3b646b9a03e678fab3cbef
Tyler Smalley <tyler.smalley@elastic.co>
2020-05-13 17:38:18 -0700

    [build] Removes commonjs transforms
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- packages/kbn-optimizer/src/integration_tests/basic_optimization.test.ts
+++ packages/kbn-optimizer/src/integration_tests/basic_optimization.test.ts
@@ -34,12 +34,27 @@ const MOCK_REPO_DIR = Path.resolve(TMP_DIR, 'mock_repo');
 
 expect.addSnapshotSerializer(createAbsolutePathSerializer(REPO_ROOT));
 
+let log: ToolingLog;
+
 beforeAll(async () => {
   await del(TMP_DIR);
   await cpy('**/*', MOCK_REPO_DIR, {
     cwd: MOCK_REPO_SRC,
     parents: true,
     deep: true,
+  });
+
+  log = new ToolingLog({
+    level: 'error',
+    writeTo: {
+      write(chunk) {
+        if (chunk.endsWith('\n')) {
+          chunk = chunk.slice(0, -1);
+        }
+        // eslint-disable-next-line no-console
+        console.error(chunk);
+      },
+    },
   });
 });
 
@@ -52,23 +67,11 @@ it('builds expected bundles, saves bundle counts to metadata', async () => {
     repoRoot: MOCK_REPO_DIR,
     pluginScanDirs: [Path.resolve(MOCK_REPO_DIR, 'plugins')],
     maxWorkerCount: 1,
-    dist: true,
+    dist: false,
   });
 
   expect(config).toMatchSnapshot('OptimizerConfig');
 
-  const log = new ToolingLog({
-    level: 'error',
-    writeTo: {
-      write(chunk) {
-        if (chunk.endsWith('\n')) {
-          chunk = chunk.slice(0, -1);
-        }
-        // eslint-disable-next-line no-console
-        console.error(chunk);
-      },
-    },
-  });
   const msgs = await runOptimizer(config)
     .pipe(logOptimizerState(log, config), toArray())
     .toPromise();
@@ -124,13 +127,6 @@ it('builds expected bundles, saves bundle counts to metadata', async () => {
       msg.event?.type !== 'bundle not cached'
   );
   assert('produce zero unexpected states', otherStates.length === 0, otherStates);
-
-  expectFileMatchesSnapshotWithCompression('plugins/foo/target/public/foo.plugin.js', 'foo bundle');
-  expectFileMatchesSnapshotWithCompression(
-    'plugins/foo/target/public/1.plugin.js',
-    '1 async bundle'
-  );
-  expectFileMatchesSnapshotWithCompression('plugins/bar/target/public/bar.plugin.js', 'bar bundle');
 
   const foo = config.bundles.find(b => b.id === 'foo')!;
   expect(foo).toBeTruthy();
@@ -195,9 +191,33 @@ it('uses cache on second run and exist cleanly', async () => {
       "initializing",
       "initializing",
       "initialized",
+      "initialized",
+      "running",
+      "running",
+      "running",
       "success",
     ]
   `);
+});
+
+it('prepares assets for distribution', async () => {
+  const config = OptimizerConfig.create({
+    repoRoot: MOCK_REPO_DIR,
+    pluginScanDirs: [Path.resolve(MOCK_REPO_DIR, 'plugins')],
+    maxWorkerCount: 1,
+    dist: true,
+  });
+
+  await runOptimizer(config)
+    .pipe(logOptimizerState(log, config), toArray())
+    .toPromise();
+
+  expectFileMatchesSnapshotWithCompression('plugins/foo/target/public/foo.plugin.js', 'foo bundle');
+  expectFileMatchesSnapshotWithCompression(
+    'plugins/foo/target/public/1.plugin.js',
+    '1 async bundle'
+  );
+  expectFileMatchesSnapshotWithCompression('plugins/bar/target/public/bar.plugin.js', 'bar bundle');
 });
 
 /**
