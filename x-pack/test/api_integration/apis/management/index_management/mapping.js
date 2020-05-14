2fd58fb8c5d02ce1320f3a443d0620df41f57f51
Yulia Čech <6585477+yuliacech@users.noreply.github.com>
2020-05-14 13:35:45 +0200

    [IM] Changes index mapping api response to plural form (#66012) (#66428)
    * changes index mapping api response to plural form
    
    * changes tab title to "Mappings" in Index Management detail panel
    
    * updates index management jest snapshots for "Mappings" title
    # Conflicts:
    #	x-pack/test/api_integration/apis/management/index_management/mapping.js

--- x-pack/test/api_integration/apis/management/index_management/mapping.js
+++ x-pack/test/api_integration/apis/management/index_management/mapping.js
@@ -33,7 +33,7 @@ export default function({ getService }) {
       const { body } = await getIndexMapping(index).expect(200);
 
       // As, on 7.x we require the mappings with type (include_type_name), the default "_doc" type is returned
-      expect(body.mapping).to.eql({ _doc: mappings });
+      expect(body.mappings).to.eql({ _doc: mappings });
     });
   });
 }
