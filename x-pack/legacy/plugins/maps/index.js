72d7a03ee01fced823691f3108d2567639c37caf
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-05-14 11:10:39 -0600

    Completely remove server, config init and injected vars in legacy

--- x-pack/legacy/plugins/maps/index.js
+++ x-pack/legacy/plugins/maps/index.js
@@ -7,7 +7,6 @@
 import { i18n } from '@kbn/i18n';
 import { resolve } from 'path';
 import { getAppTitle } from '../../../plugins/maps/common/i18n_getters';
-import { MapPlugin } from './server/plugin';
 import { APP_ID, APP_ICON } from '../../../plugins/maps/common/constants';
 import { DEFAULT_APP_CATEGORIES } from '../../../../src/core/server';
 
@@ -29,37 +28,7 @@ export function maps(kibana) {
         category: DEFAULT_APP_CATEGORIES.kibana,
         order: 4000,
       },
-      injectDefaultVars(server) {
-        const serverConfig = server.config();
-
-        return {
-          showMapVisualizationTypes: serverConfig.get('xpack.maps.showMapVisualizationTypes'),
-          showMapsInspectorAdapter: serverConfig.get('xpack.maps.showMapsInspectorAdapter'),
-          enableVectorTiles: serverConfig.get('xpack.maps.enableVectorTiles'),
-          preserveDrawingBuffer: serverConfig.get('xpack.maps.preserveDrawingBuffer'),
-          kbnPkgVersion: serverConfig.get('pkg.version'),
-        };
-      },
       styleSheetPaths: `${__dirname}/public/index.scss`,
-    },
-    config(Joi) {
-      return Joi.object({
-        enabled: Joi.boolean().default(true),
-        showMapVisualizationTypes: Joi.boolean().default(false),
-        showMapsInspectorAdapter: Joi.boolean().default(false), // flag used in functional testing
-        preserveDrawingBuffer: Joi.boolean().default(false), // flag used in functional testing
-        enableVectorTiles: Joi.boolean().default(false), // flag used to enable/disable vector-tiles
-      }).default();
-    },
-
-    init(server) {
-      const __LEGACY = {
-        injectUiAppVars: server.injectUiAppVars,
-        getInjectedUiAppVars: server.getInjectedUiAppVars,
-      };
-
-      const mapPlugin = new MapPlugin();
-      mapPlugin.setup(__LEGACY);
     },
   });
 }
