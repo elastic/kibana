3f4394f682cadd8747bf5019596e98b36e77fbd6
Dario Gieselaar <dario.gieselaar@elastic.co>
2020-05-14 16:23:38 +0200

    [APM] Add license guard for annotations (#65995)

--- x-pack/test/observability_api_integration/trial/tests/index.ts
+++ x-pack/test/observability_api_integration/trial/tests/index.ts
@@ -4,13 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { FtrProviderContext } from '../../ftr_provider_context';
+import { FtrProviderContext } from '../../common/ftr_provider_context';
 
+// eslint-disable-next-line import/no-default-export
 export default function apmApiIntegrationTests({ loadTestFile }: FtrProviderContext) {
-  describe('APM specs', () => {
+  describe('Observability specs (trial)', function() {
+    this.tags('ciGroup1');
     loadTestFile(require.resolve('./annotations'));
-    loadTestFile(require.resolve('./feature_controls'));
-    loadTestFile(require.resolve('./agent_configuration'));
-    loadTestFile(require.resolve('./custom_link'));
   });
 }
