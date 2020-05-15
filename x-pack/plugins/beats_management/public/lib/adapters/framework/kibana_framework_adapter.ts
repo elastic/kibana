db807dfd0dbb25aed0b04c5efadeea30677e3b9a
Josh Dover <me@joshdover.com>
2020-05-15 17:20:06 -0600

    [7.x] Migrate Beats Management UI to KP (#65791) (#66774)

---
+++ x-pack/plugins/beats_management/public/lib/adapters/framework/kibana_framework_adapter.ts
@@ -0,0 +1,182 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+/* eslint-disable max-classes-per-file */
+import { IScope } from 'angular';
+import { PathReporter } from 'io-ts/lib/PathReporter';
+import { isLeft } from 'fp-ts/lib/Either';
+import { first } from 'rxjs/operators';
+import { SecurityPluginSetup } from '../../../../../security/public';
+import { BufferedKibanaServiceCall, KibanaAdapterServiceRefs, KibanaUIConfig } from '../../types';
+import {
+  FrameworkAdapter,
+  FrameworkInfo,
+  FrameworkUser,
+  RuntimeFrameworkInfo,
+  RuntimeFrameworkUser,
+} from './adapter_types';
+import {
+  ManagementSetup,
+  RegisterManagementAppArgs,
+} from '../../../../../../../src/plugins/management/public';
+import { LicensingPluginSetup } from '../../../../../licensing/public';
+import { BeatsManagementConfigType } from '../../../../common';
+
+export class KibanaFrameworkAdapter implements FrameworkAdapter {
+  public get info() {
+    if (this.xpackInfo) {
+      return this.xpackInfo;
+    } else {
+      throw new Error('framework adapter must have init called before anything else');
+    }
+  }
+
+  public get currentUser() {
+    return this.shieldUser!;
+  }
+  private xpackInfo: FrameworkInfo | null = null;
+  private adapterService: KibanaAdapterServiceProvider;
+  private shieldUser: FrameworkUser | null = null;
+  constructor(
+    private readonly PLUGIN_ID: string,
+    private readonly management: ManagementSetup,
+    private readonly getBasePath: () => string,
+    private readonly licensing: LicensingPluginSetup,
+    private readonly securitySetup: SecurityPluginSetup | undefined,
+    private readonly config: BeatsManagementConfigType,
+    public readonly version: string
+  ) {
+    this.adapterService = new KibanaAdapterServiceProvider();
+  }
+
+  public setUISettings = (key: string, value: any) => {
+    this.adapterService.callOrBuffer(({ config }) => {
+      config.set(key, value);
+    });
+  };
+
+  public async waitUntilFrameworkReady(): Promise<void> {
+    const license = await this.licensing.license$.pipe(first()).toPromise();
+    let xpackInfoUnpacked: FrameworkInfo;
+
+    try {
+      xpackInfoUnpacked = {
+        basePath: this.getBasePath(),
+        license: {
+          type: license.type ?? 'oss',
+          expired: !license.isActive,
+          expiry_date_in_millis: license.expiryDateInMillis ?? -1,
+        },
+        security: {
+          enabled: license.getFeature('security').isEnabled,
+          available: license.getFeature('security').isAvailable,
+        },
+        settings: this.config,
+      };
+    } catch (e) {
+      throw new Error(`Unexpected data structure from xpackInfoService, ${JSON.stringify(e)}`);
+    }
+
+    const assertData = RuntimeFrameworkInfo.decode(xpackInfoUnpacked);
+    if (isLeft(assertData)) {
+      throw new Error(
+        `Error parsing xpack info in ${this.PLUGIN_ID},   ${PathReporter.report(assertData)[0]}`
+      );
+    }
+    this.xpackInfo = xpackInfoUnpacked;
+
+    try {
+      this.shieldUser = (await this.securitySetup?.authc.getCurrentUser()) || null;
+      const assertUser = RuntimeFrameworkUser.decode(this.shieldUser);
+
+      if (isLeft(assertUser)) {
+        throw new Error(
+          `Error parsing user info in ${this.PLUGIN_ID},   ${PathReporter.report(assertUser)[0]}`
+        );
+      }
+    } catch (e) {
+      this.shieldUser = null;
+    }
+  }
+
+  public registerManagementSection(settings: {
+    id: string;
+    name: string;
+    iconName: string;
+    order?: number;
+  }) {
+    this.management.sections.register({
+      id: settings.id,
+      title: settings.name,
+      euiIconType: settings.iconName,
+      order: settings.order || 30,
+    });
+  }
+
+  public registerManagementUI(settings: {
+    sectionId: string;
+    appId: string;
+    name: string;
+    order?: number;
+    mount: RegisterManagementAppArgs['mount'];
+  }) {
+    const section = this.management.sections.getSection(settings.sectionId);
+
+    if (!section) {
+      throw new Error(
+        `registerManagementUI was called with a sectionId of ${settings.sectionId}, and that is is not yet regestered as a section`
+      );
+    }
+
+    section.registerApp({
+      id: settings.appId,
+      title: settings.name,
+      order: settings.order || 30,
+      mount: settings.mount,
+    });
+  }
+}
+
+class KibanaAdapterServiceProvider {
+  public serviceRefs: KibanaAdapterServiceRefs | null = null;
+  public bufferedCalls: Array<BufferedKibanaServiceCall<KibanaAdapterServiceRefs>> = [];
+
+  public $get($rootScope: IScope, config: KibanaUIConfig) {
+    this.serviceRefs = {
+      config,
+      rootScope: $rootScope,
+    };
+
+    this.applyBufferedCalls(this.bufferedCalls);
+
+    return this;
+  }
+
+  public callOrBuffer(serviceCall: (serviceRefs: KibanaAdapterServiceRefs) => void) {
+    if (this.serviceRefs !== null) {
+      this.applyBufferedCalls([serviceCall]);
+    } else {
+      this.bufferedCalls.push(serviceCall);
+    }
+  }
+
+  public applyBufferedCalls(
+    bufferedCalls: Array<BufferedKibanaServiceCall<KibanaAdapterServiceRefs>>
+  ) {
+    if (!this.serviceRefs) {
+      return;
+    }
+
+    this.serviceRefs.rootScope.$apply(() => {
+      bufferedCalls.forEach(serviceCall => {
+        if (!this.serviceRefs) {
+          return;
+        }
+        return serviceCall(this.serviceRefs);
+      });
+    });
+  }
+}
