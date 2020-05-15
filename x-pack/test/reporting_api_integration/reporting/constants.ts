00649506dc9a28068cb4e44d8d02a1f7833b83fa
Timothy Sullivan <tsullivan@elastic.co>
2020-05-15 16:00:17 -0700

    revert

--- x-pack/test/reporting_api_integration/reporting/constants.ts
+++ x-pack/test/reporting_api_integration/reporting/constants.ts
@@ -4,14 +4,13 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { REPO_ROOT } from '@kbn/dev-utils';
 import path from 'path';
 
 export const OSS_KIBANA_ARCHIVE_PATH = path.resolve(
-  REPO_ROOT,
-  'test/functional/fixtures/es_archiver/dashboard/current/kibana'
+  __dirname,
+  '../../../../test/functional/fixtures/es_archiver/dashboard/current/kibana'
 );
 export const OSS_DATA_ARCHIVE_PATH = path.resolve(
-  REPO_ROOT,
-  'test/functional/fixtures/es_archiver/dashboard/current/data'
+  __dirname,
+  '../../../../test/functional/fixtures/es_archiver/dashboard/current/data'
 );
