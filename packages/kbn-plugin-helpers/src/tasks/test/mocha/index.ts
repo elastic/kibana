4e0921d41edad38a9d109242fade5087c2fc607d
Spencer <email@spalger.com>
2020-05-15 10:35:44 -0700

    [kbn/plugin-helpers] typescript-ify (#66513)

--- packages/kbn-plugin-helpers/src/tasks/test/mocha/index.ts
+++ packages/kbn-plugin-helpers/src/tasks/test/mocha/index.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /*
  * Licensed to Elasticsearch B.V. under one or more contributor
  * license agreements. See the NOTICE file distributed with
@@ -19,10 +17,4 @@
  * under the License.
  */
 
-const nodeMajorVersion = parseFloat(process.version.replace(/^v(\d+)\..+/, '$1'));
-if (nodeMajorVersion < 6) {
-  console.error('FATAL: kibana-plugin-helpers requires node 6+');
-  process.exit(1);
-}
-
-require('../target/cli');
+export * from './test_mocha_task';
