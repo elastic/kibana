22ba94d8b2278c4e04cb87fadb982d349165062e
Christos Nasikas <christos.nasikas@elastic.co>
2020-05-16 16:05:09 +0300

    Multiple types of error messages

--- x-pack/plugins/siem/public/cases/components/callout/index.tsx
+++ x-pack/plugins/siem/public/cases/components/callout/index.tsx
@@ -12,28 +12,49 @@ import * as i18n from './translations';
 
 export * from './helpers';
 
+interface ErrorMessage {
+  title: string;
+  description: JSX.Element;
+  errorType?: 'primary' | 'success' | 'warning' | 'danger';
+}
+
 interface CaseCallOutProps {
   title: string;
   message?: string;
-  messages?: Array<{ title: string; description: JSX.Element }>;
+  messages?: ErrorMessage[];
 }
 
 const CaseCallOutComponent = ({ title, message, messages }: CaseCallOutProps) => {
   const [showCallOut, setShowCallOut] = useState(true);
   const handleCallOut = useCallback(() => setShowCallOut(false), [setShowCallOut]);
 
+  const groupedErrorMessages = (messages ?? []).reduce((acc, currentMessage: ErrorMessage) => {
+    const key = currentMessage.errorType == null ? 'primary' : currentMessage.errorType;
+    return {
+      ...acc,
+      [key]: [...(acc[key] || []), currentMessage],
+    };
+  }, {} as { [key in NonNullable<ErrorMessage['errorType']>]: ErrorMessage[] });
+
   return showCallOut ? (
     <>
-      <EuiCallOut title={title} color="primary" iconType="gear" data-test-subj="case-call-out">
-        {!isEmpty(messages) && (
-          <EuiDescriptionList data-test-subj="callout-messages" listItems={messages} />
-        )}
-        {!isEmpty(message) && <p data-test-subj="callout-message">{message}</p>}
-        <EuiButton data-test-subj="callout-dismiss" color="primary" onClick={handleCallOut}>
-          {i18n.DISMISS_CALLOUT}
-        </EuiButton>
-      </EuiCallOut>
-      <EuiSpacer />
+      {(Object.keys(groupedErrorMessages) as Array<keyof ErrorMessage['errorType']>).map(key => (
+        <React.Fragment key={key}>
+          <EuiCallOut title={title} color={key} iconType="gear" data-test-subj="case-call-out">
+            {!isEmpty(messages) && (
+              <EuiDescriptionList
+                data-test-subj="callout-messages"
+                listItems={groupedErrorMessages[key]}
+              />
+            )}
+            {!isEmpty(message) && <p data-test-subj="callout-message">{message}</p>}
+            <EuiButton data-test-subj="callout-dismiss" color="primary" onClick={handleCallOut}>
+              {i18n.DISMISS_CALLOUT}
+            </EuiButton>
+          </EuiCallOut>
+          <EuiSpacer />
+        </React.Fragment>
+      ))}
     </>
   ) : null;
 };
