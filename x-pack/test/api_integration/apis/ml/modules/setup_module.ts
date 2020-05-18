98aaacd32280d1eb24843632891cc396cd89c0b1
Robert Oskamp <robert.oskamp@elastic.co>
2020-05-18 10:01:04 +0200

    Setup module - only check record count when datafeed is started

--- x-pack/test/api_integration/apis/ml/modules/setup_module.ts
+++ x-pack/test/api_integration/apis/ml/modules/setup_module.ts
@@ -235,8 +235,10 @@ export default ({ getService }: FtrProviderContext) => {
           for (const job of testData.expected.jobs) {
             const datafeedId = `datafeed-${job.jobId}`;
             await ml.api.waitForAnomalyDetectionJobToExist(job.jobId);
-            await ml.api.waitForADJobRecordCountToBePositive(job.jobId);
             await ml.api.waitForDatafeedToExist(datafeedId);
+            if (testData.requestBody.startDatafeed === true) {
+              await ml.api.waitForADJobRecordCountToBePositive(job.jobId);
+            }
             await ml.api.waitForJobState(job.jobId, job.jobState);
             await ml.api.waitForDatafeedState(datafeedId, job.datafeedState);
           }
