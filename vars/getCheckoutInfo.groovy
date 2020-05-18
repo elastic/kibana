d9d3575f4b5d757fe3f7869c5c82716eb35ff8e1
spalger <spalger@users.noreply.github.com>
2020-05-18 11:47:56 -0700

    use dir() rather than cd in every script

--- vars/getCheckoutInfo.groovy
+++ vars/getCheckoutInfo.groovy
@@ -1,32 +1,28 @@
 def call(scmVars) {
-  def branch = env.ghprbSourceBranch ?: scmVars.GIT_LOCAL_BRANCH ?: scmVars.GIT_BRANCH
+  def repoInfo = [:]
 
-  def commit = sh(
-    script: "cd kibana && git rev-parse HEAD",
-    label: "determining checked out sha",
-    returnStdout: true
-  ).trim()
+  repoInfo.branch = env.ghprbSourceBranch ?: scmVars.GIT_LOCAL_BRANCH ?: scmVars.GIT_BRANCH
+  repoInfo.targetBranch = env.ghprbTargetBranch
 
-  def targetBranch = env.ghprbTargetBranch
-  def mergeBase
-  if (env.ghprbTargetBranch) {
-    sh(
-      script: "cd kibana && git fetch https://github.com/elastic/kibana.git ${env.ghprbTargetBranch}",
-      label: "fetch latest from '${env.ghprbTargetBranch}' at https://github.com/elastic/kibana.git"
-    )
-    mergeBase = sh(
-      script: "cd kibana && git merge-base HEAD FETCH_HEAD",
-      label: "determining merge point with '${env.ghprbTargetBranch}' at https://github.com/elastic/kibana.git",
+  dir("kibana") {
+    repoInfo.commit = sh(
+      script: "git rev-parse HEAD",
+      label: "determining checked out sha",
       returnStdout: true
     ).trim()
-  }
 
-  def repoInfo = [
-    commit: commit,
-    branch: branch,
-    targetBranch: targetBranch,
-    mergeBase: mergeBase,
-  ]
+    if (repoInfo.targetBranch) {
+      sh(
+        script: "git fetch https://github.com/elastic/kibana.git ${repoInfo.targetBranch}",
+        label: "fetch latest from '${repoInfo.targetBranch}' at https://github.com/elastic/kibana.git"
+      )
+      repoInfo.mergeBase = sh(
+        script: "git merge-base HEAD FETCH_HEAD",
+        label: "determining merge point with '${repoInfo.targetBranch}' at https://github.com/elastic/kibana.git",
+        returnStdout: true
+      ).trim()
+    }
+  }
 
   print "repoInfo: ${repoInfo}"
 
