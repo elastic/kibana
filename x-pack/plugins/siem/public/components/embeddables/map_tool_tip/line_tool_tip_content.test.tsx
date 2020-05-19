64c26d1ea5034f32d02b144da1fdca45f14c80ba
Garrett Spong <spong@users.noreply.github.com>
2020-05-18 22:38:53 -0600

    Updating imports...

--- x-pack/plugins/siem/public/components/embeddables/map_tool_tip/line_tool_tip_content.test.tsx
+++ x-pack/plugins/siem/public/components/embeddables/map_tool_tip/line_tool_tip_content.test.tsx
@@ -13,8 +13,8 @@ import {
   SUM_OF_SERVER_BYTES,
   SUM_OF_SOURCE_BYTES,
 } from '../map_config';
-import { ITooltipProperty } from '../../../../../../maps/public';
-import { TooltipProperty } from '../../../../../../maps/public/classes/tooltips/tooltip_property';
+import { ITooltipProperty } from '../../../../../maps/public';
+import { TooltipProperty } from '../../../../../maps/public/layers/tooltips/tooltip_property';
 
 describe('LineToolTipContent', () => {
   const mockFeatureProps: ITooltipProperty[] = [
