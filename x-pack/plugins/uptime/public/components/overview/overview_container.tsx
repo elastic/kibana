010b8eab2decb3f4d798573ec7f5ee3e0f4e6a0f
Shahzad <shahzad.muhammad@elastic.co>
2020-05-19 17:15:49 +0200

    [7.x] [Uptime] Reduce page rendering cycles and api calls (#66216) (#66997)

--- x-pack/plugins/uptime/public/components/overview/overview_container.tsx
+++ x-pack/plugins/uptime/public/components/overview/overview_container.tsx
@@ -12,12 +12,19 @@ import { setEsKueryString } from '../../state/actions';
 
 export const OverviewPage: React.FC = props => {
   const dispatch = useDispatch();
+
   const setEsKueryFilters = useCallback(
     (esFilters: string) => dispatch(setEsKueryString(esFilters)),
     [dispatch]
   );
-  const indexPattern = useSelector(selectIndexPattern);
+  const { index_pattern: indexPattern, loading } = useSelector(selectIndexPattern);
+
   return (
-    <OverviewPageComponent setEsKueryFilters={setEsKueryFilters} {...indexPattern} {...props} />
+    <OverviewPageComponent
+      setEsKueryFilters={setEsKueryFilters}
+      indexPattern={indexPattern}
+      loading={loading}
+      {...props}
+    />
   );
 };
