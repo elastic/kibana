b40414716ba2c36685fa3d2bc2f0312853306b6d
Steph Milovic <stephanie.milovic@elastic.co>
2020-05-19 10:36:34 -0600

    [SIEM] Cases] Capture timeline click and open timeline in case view (#66327) (#66933)

--- x-pack/plugins/siem/cypress/tasks/case_details.ts
+++ x-pack/plugins/siem/cypress/tasks/case_details.ts
@@ -5,10 +5,9 @@
  */
 
 import { TIMELINE_TITLE } from '../screens/timeline';
+import { CASE_DETAILS_TIMELINE_LINK_MARKDOWN } from '../screens/case_details';
 
-export const openCaseTimeline = (link: string) => {
-  cy.visit('/app/kibana');
-  cy.visit(link);
-  cy.contains('a', 'SIEM');
+export const openCaseTimeline = () => {
+  cy.get(CASE_DETAILS_TIMELINE_LINK_MARKDOWN).click();
   cy.get(TIMELINE_TITLE).should('exist');
 };
