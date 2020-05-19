fe5522d84cf8b3fd59aaae4a49279ed70c555e24
kobelb <brandon.kobel@elastic.co>
2020-05-19 13:50:43 -0700

    Using the `vm` module to timebox the regex evaluation to 100ms

--- src/plugins/vis_type_timelion/server/series_functions/label.js
+++ src/plugins/vis_type_timelion/server/series_functions/label.js
@@ -17,6 +17,7 @@
  * under the License.
  */
 
+import vm from 'vm';
 import { i18n } from '@kbn/i18n';
 import alter from '../lib/alter.js';
 import Chainable from '../lib/classes/chainable';
@@ -47,11 +48,19 @@ export default new Chainable('label', {
   help: i18n.translate('timelion.help.functions.labelHelpText', {
     defaultMessage: 'Change the label of the series. Use %s to reference the existing label',
   }),
-  fn: function labelFn(args) {
+  fn: async function labelFn(args) {
     const config = args.byName;
     return alter(args, function(eachSeries) {
       if (config.regex) {
-        eachSeries.label = eachSeries.label.replace(new RegExp(config.regex), config.label);
+        vm.runInNewContext(
+          `exec()`,
+          {
+            exec: () => {
+              eachSeries.label = eachSeries.label.replace(new RegExp(config.regex), config.label);
+            },
+          },
+          { timeout: 100 }
+        );
       } else {
         eachSeries.label = config.label;
       }
