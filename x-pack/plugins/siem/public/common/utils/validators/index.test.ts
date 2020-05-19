a5f44ed15c5f890b8dd111e4924f2884112b1650
Christos Nasikas <christos.nasikas@elastic.co>
2020-05-19 19:57:03 +0300

    [7.x] [SIEM] Use validators from common folder (#66975) (#67011)

--- x-pack/plugins/siem/public/common/utils/validators/index.test.ts
+++ x-pack/plugins/siem/public/common/utils/validators/index.test.ts
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { isUrlInvalid } from './helpers';
+import { isUrlInvalid } from '.';
 
 describe('helpers', () => {
   describe('isUrlInvalid', () => {
