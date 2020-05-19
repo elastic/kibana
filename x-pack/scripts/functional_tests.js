88a41b5822a9b0726bcf6ae6d9665bc9cc36f136
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2020-05-19 14:35:34 -0400

    miss import in cherry-pick

--- x-pack/scripts/functional_tests.js
+++ x-pack/scripts/functional_tests.js
@@ -23,7 +23,6 @@ require('@kbn/test').runTestsCli([
   require.resolve('../test/detection_engine_api_integration/security_and_spaces/config.ts'),
   require.resolve('../test/detection_engine_api_integration/basic/config.ts'),
   require.resolve('../test/plugin_api_integration/config.ts'),
-  require.resolve('../test/plugin_functional/config'),
   require.resolve('../test/kerberos_api_integration/config'),
   require.resolve('../test/kerberos_api_integration/anonymous_access.config'),
   require.resolve('../test/saml_api_integration/config'),
