735c567d2ad725ec2b6af6594c25fa9369b73dc1
Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>
2020-05-20 17:59:58 +0300

    Added comment for workaround

--- src/plugins/index_pattern_management/public/components/test_utils.tsx
+++ src/plugins/index_pattern_management/public/components/test_utils.tsx
@@ -21,6 +21,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { shallow } from 'enzyme';
 
+// since the 'shallow' from 'enzyme' doesn't support context API for React 16 and above (https://github.com/facebook/react/pull/14329)
+// we use this workaround where define legacy contextTypes for react class component
 export function createComponentWithContext(
   MyComponent: React.ComponentClass<any>,
   props: Record<string, any>,
