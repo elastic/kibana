42d21bcdcbd423252cfaf12466bbc6b03cfa1b10
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2020-05-20 10:28:53 +0300

    [Discover] Deangularize timechart header (#66532)
    * Deangularize timechart header
    
    * Add label attr to options
    
    * Watch the interval prop change
    
    * Tweaking the UI
    
    Mainly moved interval notice to an `append` and copy updates
    
    * Remove outdated i18n tokens
    
    * fix functional test
    
    * Change functional test due to dom changes
    
    * fix functional test
    
    * remove unecessary translation
    
    * remove watcher as it is not necessary anymore
    
    * change interval options copies
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: cchaos <caroline.horn@elastic.co>

--- test/functional/apps/discover/_discover_histogram.js
+++ test/functional/apps/discover/_discover_histogram.js
@@ -69,7 +69,7 @@ export default function({ getService, getPageObjects }) {
       const fromTime = 'Nov 01, 2017 @ 00:00:00.000';
       const toTime = 'Mar 21, 2018 @ 00:00:00.000';
       await PageObjects.timePicker.setAbsoluteRange(fromTime, toTime);
-      await PageObjects.discover.setChartInterval('Monthly');
+      await PageObjects.discover.setChartInterval('Month');
       await PageObjects.header.waitUntilLoadingHasFinished();
       const chartCanvasExist = await elasticChart.canvasExists();
       expect(chartCanvasExist).to.be(true);
@@ -78,7 +78,7 @@ export default function({ getService, getPageObjects }) {
       const fromTime = 'Mar 01, 2018 @ 00:00:00.000';
       const toTime = 'May 01, 2018 @ 00:00:00.000';
       await PageObjects.timePicker.setAbsoluteRange(fromTime, toTime);
-      await PageObjects.discover.setChartInterval('Weekly');
+      await PageObjects.discover.setChartInterval('Week');
       await PageObjects.header.waitUntilLoadingHasFinished();
       const chartCanvasExist = await elasticChart.canvasExists();
       expect(chartCanvasExist).to.be(true);
@@ -88,12 +88,12 @@ export default function({ getService, getPageObjects }) {
       const toTime = 'Mar 21, 2019 @ 00:00:00.000';
 
       await PageObjects.timePicker.setAbsoluteRange(fromTime, toTime);
-      await PageObjects.discover.setChartInterval('Daily');
+      await PageObjects.discover.setChartInterval('Day');
       await PageObjects.header.waitUntilLoadingHasFinished();
       const chartCanvasExist = await elasticChart.canvasExists();
       expect(chartCanvasExist).to.be(true);
-      const chartIntervalScaledDesc = await PageObjects.discover.getChartIntervalScaledToDesc();
-      expect(chartIntervalScaledDesc).to.be('Scaled to 30 days');
+      const chartIntervalIconTip = await PageObjects.discover.getChartIntervalWarningIcon();
+      expect(chartIntervalIconTip).to.be(true);
     });
   });
 }
