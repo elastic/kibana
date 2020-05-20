c570ecabea7fd15688b14dba75354c84c9fb5729
Corey Robertson <corey.robertson@elastic.co>
2020-05-20 14:56:51 -0400

    Force embeddables to refresh when renderable reevaluated

--- x-pack/plugins/canvas/canvas_plugin_src/functions/common/saved_visualization.ts
+++ x-pack/plugins/canvas/canvas_plugin_src/functions/common/saved_visualization.ts
@@ -92,6 +92,7 @@ export function savedVisualization(): ExpressionFunctionDefinition<
           vis: visOptions,
         },
         embeddableType: EmbeddableTypes.visualization,
+        generated: Date.now(),
       };
     },
   };
