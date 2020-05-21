32b17a84d0fde1ffe4d47fe406bf956f7dcbe651
Brian Seeders <brian.seeders@elastic.co>
2020-05-21 15:59:06 -0400

    WIP fixing packer cache

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -41,10 +41,8 @@ tar -cf "$HOME/.kibana/bootstrap_cache/$branch.tar" \
   node_modules \
   packages/*/node_modules \
   x-pack/node_modules \
-  x-pack/legacy/plugins/*/node_modules \
   x-pack/legacy/plugins/reporting/.chromium \
   test/plugin_functional/plugins/*/node_modules \
-  examples/*/node_modules \
   .es \
   .chromedriver \
   .geckodriver;
