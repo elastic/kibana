81cd2513cde0f6b34bdb21c7585eb05bdf137a65
Nathan Reese <reese.nathan@gmail.com>
2020-05-21 08:08:35 -0600

    review feedback

--- x-pack/plugins/maps/public/reducers/non_serializable_instances.d.ts
+++ x-pack/plugins/maps/public/reducers/non_serializable_instances.d.ts
@@ -3,9 +3,17 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 
 import { Adapters } from 'src/plugins/inspector/public';
 import { AnyAction } from 'redux';
+import { MapStoreState } from './store';
+
+export type NonSerializableState = {
+  inspectorAdapters: Adapters;
+  cancelRequestCallbacks: Map<symbol, () => {}>; // key is request token, value is cancel callback
+  eventHandlers: Partial<EventHandlers>;
+};
 
 export interface ResultMeta {
   featuresCount?: number;
@@ -44,9 +52,9 @@ interface EventHandlers {
 
 export function setEventHandlers(eventHandlers?: EventHandlers): AnyAction;
 
-export function getInspectorAdapters(args: unknown): Adapters;
+export function getInspectorAdapters(state: MapStoreState): Adapters;
 
-export function getEventHandlers(state: unknown): Partial<EventHandlers>;
+export function getEventHandlers(state: MapStoreState): Partial<EventHandlers>;
 
 export function cancelRequest(requestToken?: symbol): AnyAction;
 
