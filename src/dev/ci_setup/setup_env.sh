57365db1dac491056464c7783e5770a65597370e
Brian Seeders <brian.seeders@elastic.co>
2020-05-21 12:57:30 -0400

    Bump chromedriver and try to force binary download

--- src/dev/ci_setup/setup_env.sh
+++ src/dev/ci_setup/setup_env.sh
@@ -128,8 +128,9 @@ export GECKODRIVER_CDNURL="https://us-central1-elastic-kibana-184716.cloudfuncti
 export CHROMEDRIVER_CDNURL="https://us-central1-elastic-kibana-184716.cloudfunctions.net/kibana-ci-proxy-cache"
 export CYPRESS_DOWNLOAD_MIRROR="https://us-central1-elastic-kibana-184716.cloudfunctions.net/kibana-ci-proxy-cache/cypress"
 
-
 export CHECKS_REPORTER_ACTIVE=false
+
+export CHROMEDRIVER_FORCE_DOWNLOAD=true
 
 ### only run on pr jobs for elastic/kibana, checks-reporter doesn't work for other repos
 if [[ "$ghprbPullId" && "$ghprbGhRepository" == 'elastic/kibana' ]] ; then
