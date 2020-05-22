7cd37355b71fec12e9f6674b72d49b148e69d509
Jean-Louis Leysens <jloleysens@gmail.com>
2020-05-22 10:28:39 +0200

    [Index management] Add support in url for hidden toggle (#66715) (#67166)
    * First iteration of supporting hidden indices in indices table
    
    Tests probably have broken
    
    * Remove unused code
    
    * Fix logic when calling get index endpoint with empty array
    
    Also remove unused translations
    
    * First iteration of new flag being passed in through URL
    
    * Added includeHidden param to index management list view
    
    This allows setting the value of the "inlcude hidden" toggle via
    the url hash query param "includeHidden". Also added a test.
    
    * Remove unused variable
    
    * Revise data flow
    
    The component toggle gets it's initial state form hash query
    param and thereafter the toggle drives the state in the url.
    
    * Fix typo
    
    * Delete ts-ignore
    
    * Slight update to test and comment placement for clarity
    
    * Fix copy-pasta
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/index_management/__jest__/client_integration/home.test.ts
+++ x-pack/plugins/index_management/__jest__/client_integration/home.test.ts
@@ -44,6 +44,18 @@ describe('<IndexManagementHome />', () => {
       });
     });
 
+    test('sets the hash query param base on include hidden indices toggle', () => {
+      const { actions } = testBed;
+      expect(actions.getIncludeHiddenIndicesToggleStatus()).toBe(true);
+      expect(window.location.hash.includes('includeHidden=true')).toBe(true);
+      actions.clickIncludeHiddenIndicesToggle();
+      expect(window.location.hash.includes('includeHidden=true')).toBe(false);
+      // Note: this test modifies the shared location.hash state, we put it back the way it was
+      actions.clickIncludeHiddenIndicesToggle();
+      expect(actions.getIncludeHiddenIndicesToggleStatus()).toBe(true);
+      expect(window.location.hash.includes('includeHidden=true')).toBe(true);
+    });
+
     test('should set the correct app title', () => {
       const { exists, find } = testBed;
       expect(exists('appTitle')).toBe(true);
