d09bd6378632e8640120b2d9f6157d51a7a85527
Yuliia Naumenko <jo.naumenko@gmail.com>
2020-05-24 20:27:42 -0700

    Changed actions API endpoints urls to follow Kibana STYLEGUIDE (#65936)
    * Changed actions API endpoints urls to follow Kibana STYLEGUIDE
    
    * Fixed tests
    
    * fixed test
    
    * fixed test
    
    * resolved conflicts
    
    * Fixed siem tests
    
    * Fixed failing test
    
    * fixed readme and test
    
    * Changed actions api urls to follow the template 'api/{plugin}/{type}/{id}
    
    * Fixed type checks
    
    * Fixed tests and API
    
    * fixed tests
    
    * Fixed type checks
    
    * fixed type check

--- x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/delete.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/delete.ts
@@ -52,7 +52,7 @@ export default function createDeleteTests({ getService }: FtrProviderContext) {
                 error: 'Not Found',
                 message: 'Not Found',
               });
-              objectRemover.add(space.id, createdAlert.id, 'alert');
+              objectRemover.add(space.id, createdAlert.id, 'alert', undefined);
               // Ensure task still exists
               await getScheduledTask(createdAlert.scheduledTaskId);
               break;
@@ -78,7 +78,7 @@ export default function createDeleteTests({ getService }: FtrProviderContext) {
             .set('kbn-xsrf', 'foo')
             .send(getTestAlertData())
             .expect(200);
-          objectRemover.add(space.id, createdAlert.id, 'alert');
+          objectRemover.add(space.id, createdAlert.id, 'alert', undefined);
 
           const response = await supertestWithoutAuth
             .delete(`${getUrlPrefix('other')}/api/alert/${createdAlert.id}`)
@@ -143,7 +143,7 @@ export default function createDeleteTests({ getService }: FtrProviderContext) {
                 error: 'Not Found',
                 message: 'Not Found',
               });
-              objectRemover.add(space.id, createdAlert.id, 'alert');
+              objectRemover.add(space.id, createdAlert.id, 'alert', undefined);
               // Ensure task still exists
               await getScheduledTask(createdAlert.scheduledTaskId);
               break;
