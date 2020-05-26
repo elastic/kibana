fafb173323ad8dea342c75398d2bbe3b71924025
Thomas Watson <w@tson.dk>
2020-05-26 20:21:42 +0200

    Comply with Kibana monorepo rules andi styling

--- packages/elastic-safer-lodash-set/README.md
+++ packages/elastic-safer-lodash-set/README.md
@@ -5,7 +5,6 @@ and [`setWith`] functions from [Lodash] and are API compatible with
 Lodash v4.x.
 
 [![npm](https://img.shields.io/npm/v/@elastic/safer-lodash-set.svg)](https://www.npmjs.com/package/@elastic/safer-lodash-set)
-[![build status](https://travis-ci.org/elastic/safer-lodash-set.svg?branch=master)](https://travis-ci.org/elastic/safer-lodash-set)
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
 
 ## Installation
@@ -17,15 +16,15 @@ npm install @elastic/safer-lodash-set --save
 ## Example Usage
 
 ```js
-const { set } = require('@elastic/safer-loadsh-set')
+const { set } = require('@elastic/safer-loadsh-set');
 
-const object = { a: [{ b: { c: 3 } }] }
+const object = { a: [{ b: { c: 3 } }] };
 
-set(object, 'a[0].b.c', 4)
-console.log(object.a[0].b.c) // => 4
+set(object, 'a[0].b.c', 4);
+console.log(object.a[0].b.c); // => 4
 
-set(object, ['x', '0', 'y', 'z'], 5)
-console.log(object.x[0].y.z) // => 5
+set(object, ['x', '0', 'y', 'z'], 5);
+console.log(object.x[0].y.z); // => 5
 ```
 
 ## API
@@ -33,15 +32,15 @@ console.log(object.x[0].y.z) // => 5
 The main module exposes two functions, `set` and `setWith`:
 
 ```js
-const { set, setWith } = require('@elastic/safer-lodash-set')
+const { set, setWith } = require('@elastic/safer-lodash-set');
 ```
 
 Besides the main module, it's also possible to require each function
 individually:
 
 ```js
-const set = require('@elastic/safer-lodash-set/set')
-const setWith = require('@elastic/safer-lodash-set/setWith')
+const set = require('@elastic/safer-lodash-set/set');
+const setWith = require('@elastic/safer-lodash-set/setWith');
 ```
 
 The APIs of these functions are identical to the equivalent Lodash
@@ -57,21 +56,21 @@ the 2nd argument to the `set` and `setWith` functions:
 ### Only own properties are followed when walking the `path`
 
 ```js
-const parent = { foo: 1 }
-const child = { bar: 2 }
+const parent = { foo: 1 };
+const child = { bar: 2 };
 
-Object.setPrototypeOf(child, parent)
+Object.setPrototypeOf(child, parent);
 
 // Now `child` can access `foo` through prototype inheritance
-console.log(child.foo) // 1
+console.log(child.foo); // 1
 
-set(child, 'foo', 3)
+set(child, 'foo', 3);
 
 // A different `foo` property has now been added directly to the `child`
 // object and the `parent` object has not been modified:
-console.log(child.foo) // 3
-console.log(parent.foo) // 1
-console.log(Object.prototype.hasOwnProperty.call(child, 'foo')) // true
+console.log(child.foo); // 3
+console.log(parent.foo); // 1
+console.log(Object.prototype.hasOwnProperty.call(child, 'foo')); // true
 ```
 
 ### The `path` must not access function prototypes
@@ -79,21 +78,21 @@ console.log(Object.prototype.hasOwnProperty.call(child, 'foo')) // true
 ```js
 const object = {
   fn1: function () {}, // regular functions has a prototype
-  fn2: () => {}        // arrow functions doesn't have a prototype
-}
+  fn2: () => {}, // arrow functions doesn't have a prototype
+};
 
 // Attempting to access any function prototype will result in an
 // exception being thrown:
 assert.throws(() => {
   // Throws: Illegal access of function prototype
-  set(object, 'fn1.prototype.toString', 'bang!')
-})
+  set(object, 'fn1.prototype.toString', 'bang!');
+});
 
 assert.doesNotThrow(() => {
   // Will just create a fresh `prototype` property on the arrow function:
-  set(object, 'fn2.prototype.toString', 'bang!')
-  console.log(object.fn2) // '[Function: fn2] { prototype: { toString: 'bang!' } }'
-})
+  set(object, 'fn2.prototype.toString', 'bang!');
+  console.log(object.fn2); // '[Function: fn2] { prototype: { toString: 'bang!' } }'
+});
 ```
 
 ## License
@@ -101,5 +100,5 @@ assert.doesNotThrow(() => {
 [MIT](LICENSE)
 
 [`set`]: https://lodash.com/docs/4.17.15#set
-[`setWith`]: https://lodash.com/docs/4.17.15#setWith
-[Lodash]: https://lodash.com/
+[`setwith`]: https://lodash.com/docs/4.17.15#setWith
+[lodash]: https://lodash.com/
--- packages/elastic-safer-lodash-set/test.js
+++ packages/elastic-safer-lodash-set/test.js
@@ -1,46 +1,59 @@
-'use strict'
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-const assert = require('assert')
+const assert = require('assert');
 
-const setFunctions = [require('./').set, require('./set')]
-const setWithFunctions = [require('./').setWith, require('./setWith')]
-const setAndSetWithFunctions = [].concat(setFunctions, setWithFunctions)
+const setFunctions = [require('./').set, require('./set')];
+const setWithFunctions = [require('./').setWith, require('./setWith')];
+const setAndSetWithFunctions = [].concat(setFunctions, setWithFunctions);
 
-setAndSetWithFunctions.forEach(set => {
+setAndSetWithFunctions.forEach((set) => {
   /**
    * GENERAL USAGE TESTS
    */
 
   // Returns same object
   {
-    const o1 = {}
-    const o2 = set(o1, 'foo', 'bar')
-    assert.strictEqual(o1, o2)
+    const o1 = {};
+    const o2 = set(o1, 'foo', 'bar');
+    assert.strictEqual(o1, o2);
   }
 
-  assert.strictEqual(set(null, 'a.b', 'foo'), null)
-  assert.strictEqual(set(undefined, 'a.b', 'foo'), undefined)
-  assert.strictEqual(set(NaN, 'a.b', 'foo'), NaN)
-  assert.strictEqual(set(42, 'a.b', 'foo'), 42)
+  assert.strictEqual(set(null, 'a.b', 'foo'), null);
+  assert.strictEqual(set(undefined, 'a.b', 'foo'), undefined);
+  assert.strictEqual(set(NaN, 'a.b', 'foo'), NaN);
+  assert.strictEqual(set(42, 'a.b', 'foo'), 42);
 
   // Overwrites existing object properties
-  assert.deepStrictEqual(set({ a: { b: { c: 3 } } }, 'a.b', 'foo'), { a: { b: 'foo' } })
+  assert.deepStrictEqual(set({ a: { b: { c: 3 } } }, 'a.b', 'foo'), { a: { b: 'foo' } });
 
   // Adds missing properties without touching other areas
   assert.deepStrictEqual(
-    set(
-      { a: [{ aa: { aaa: 3, aab: 4 } }, { ab: 2 }], b: 1 },
-      'a[0].aa.aaa.aaaa',
-      'foo'
-    ),
+    set({ a: [{ aa: { aaa: 3, aab: 4 } }, { ab: 2 }], b: 1 }, 'a[0].aa.aaa.aaaa', 'foo'),
     { a: [{ aa: { aaa: { aaaa: 'foo' }, aab: 4 } }, { ab: 2 }], b: 1 }
-  )
+  );
 
   // Overwrites existing elements in array
-  assert.deepStrictEqual(set({ a: [1, 2, 3] }, 'a[1]', 'foo'), { a: [1, 'foo', 3] })
+  assert.deepStrictEqual(set({ a: [1, 2, 3] }, 'a[1]', 'foo'), { a: [1, 'foo', 3] });
 
   // Create new array
-  assert.deepStrictEqual(set({}, ['x', '0', 'y', 'z'], 'foo'), { x: [{ y: { z: 'foo' } }] })
+  assert.deepStrictEqual(set({}, ['x', '0', 'y', 'z'], 'foo'), { x: [{ y: { z: 'foo' } }] });
 
   /**
    * PROTOTYPE POLLUTION PROTECTION TESTS
@@ -73,79 +86,97 @@ setAndSetWithFunctions.forEach(set => {
     ['a[0].constructor.prototype', { a: [{ constructor: { prototype: 'foo' } }] }],
 
     ['constructor.something.prototype', { constructor: { something: { prototype: 'foo' } } }],
-    ['.constructor.something.prototype', { '': { constructor: { something: { prototype: 'foo' } } } }],
-    ['o.constructor.something.prototype', { o: { constructor: { something: { prototype: 'foo' } } } }],
-    ['a[0].constructor.something.prototype', { a: [{ constructor: { something: { prototype: 'foo' } } }] }]
-  ]
+    [
+      '.constructor.something.prototype',
+      { '': { constructor: { something: { prototype: 'foo' } } } },
+    ],
+    [
+      'o.constructor.something.prototype',
+      { o: { constructor: { something: { prototype: 'foo' } } } },
+    ],
+    [
+      'a[0].constructor.something.prototype',
+      { a: [{ constructor: { something: { prototype: 'foo' } } }] },
+    ],
+  ];
 
   // Object manipulation
   testCases.forEach(([path, expected]) => {
-    assert.deepStrictEqual(set({}, path, 'foo'), expected)
-  })
+    assert.deepStrictEqual(set({}, path, 'foo'), expected);
+  });
 
   // Array manipulation
   testCases.forEach(([path, expected]) => {
-    const arr = []
-    set(arr, path, 'foo')
-    Object.keys(expected).forEach(key => {
-      assert(Object.prototype.hasOwnProperty.call(arr, key))
-      assert.deepStrictEqual(arr[key], expected[key])
-    })
-  })
+    const arr = [];
+    set(arr, path, 'foo');
+    Object.keys(expected).forEach((key) => {
+      assert(Object.prototype.hasOwnProperty.call(arr, key));
+      assert.deepStrictEqual(arr[key], expected[key]);
+    });
+  });
 
   // Function manipulation
   {
-    const obj = { fn: () => {} }
-    set(obj, 'fn.prototype', 'foo')
-    assert.strictEqual(typeof obj.fn, 'function')
-    assert.strictEqual(obj.fn.prototype, 'foo')
+    const obj = { fn: () => {} };
+    set(obj, 'fn.prototype', 'foo');
+    assert.strictEqual(typeof obj.fn, 'function');
+    assert.strictEqual(obj.fn.prototype, 'foo');
   }
   {
-    const obj = () => {}
-    set(obj, 'prototype', 'foo')
-    assert.strictEqual(obj.prototype, 'foo')
+    const obj = () => {};
+    set(obj, 'prototype', 'foo');
+    assert.strictEqual(obj.prototype, 'foo');
   }
   {
-    const obj = function () {}
-    assert.throws(() => {
-      set(obj, 'prototype', 'foo')
-    }, {
-      message: 'Illegal access of function prototype'
-    })
+    const obj = function () {};
+    assert.throws(
+      () => {
+        set(obj, 'prototype', 'foo');
+      },
+      {
+        message: 'Illegal access of function prototype',
+      }
+    );
   }
   {
-    const obj = { fn: function () {} }
-    assert.throws(() => {
-      set(obj, 'fn.prototype', 'foo')
-    }, {
-      message: 'Illegal access of function prototype'
-    })
+    const obj = { fn: function () {} };
+    assert.throws(
+      () => {
+        set(obj, 'fn.prototype', 'foo');
+      },
+      {
+        message: 'Illegal access of function prototype',
+      }
+    );
   }
-})
+});
 
 /**
  * setWith specific tests
  */
 
-setWithFunctions.forEach(setWith => {
+setWithFunctions.forEach((setWith) => {
   // Return undefined
-  assert.deepStrictEqual(setWith({}, 'a.b', 'foo', () => {}), { a: { b: 'foo' } })
+  assert.deepStrictEqual(
+    setWith({}, 'a.b', 'foo', () => {}),
+    { a: { b: 'foo' } }
+  );
 
   // Customizer arguments
   {
     const expectedCustomizerArgs = [
       [{ b: 42 }, 'a', { a: { b: 42 } }],
-      [42, 'b', { b: 42 }]
-    ]
+      [42, 'b', { b: 42 }],
+    ];
 
     assert.deepStrictEqual(
       setWith({ a: { b: 42 } }, 'a.b.c', 'foo', (...args) => {
-        assert.deepStrictEqual(args, expectedCustomizerArgs.shift())
+        assert.deepStrictEqual(args, expectedCustomizerArgs.shift());
       }),
       { a: { b: { c: 'foo' } } }
-    )
+    );
   }
 
   // Return value
-  assert.deepStrictEqual(setWith({}, '[0][1]', 'a', Object), { 0: { 1: 'a' } })
-})
+  assert.deepStrictEqual(setWith({}, '[0][1]', 'a', Object), { 0: { 1: 'a' } });
+});
