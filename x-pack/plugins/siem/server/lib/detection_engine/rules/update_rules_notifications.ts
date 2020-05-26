f7e8c597b16756b2af15e81dfa0695d3d890c619
patrykkopycinski <contact@patrykkopycinski.com>
2020-05-26 21:43:10 +0200

    Apply suggestions from code review
    Co-authored-by: Mike Côté <mikecote@users.noreply.github.com>

--- x-pack/plugins/siem/server/lib/detection_engine/rules/update_rules_notifications.ts
+++ x-pack/plugins/siem/server/lib/detection_engine/rules/update_rules_notifications.ts
@@ -45,7 +45,7 @@ export const updateRulesNotifications = async ({
     interval: ruleActions.alertThrottle,
   });
 
-  // TODO: Workaround for https://github.com/elastic/kibana/issues/53868
+  // TODO: Workaround for https://github.com/elastic/kibana/issues/67290
   await alertsClient.updateApiKey({ id: ruleAlertId });
 
   return ruleActions;
