b0f63a18f24282f2a5f3c1ab021a25cf36ef5455
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-05-26 14:48:15 -0600

    Saved layers loading

--- x-pack/plugins/maps/public/routing/create_edit_view.js
+++ x-pack/plugins/maps/public/routing/create_edit_view.js
@@ -41,246 +41,255 @@ import rison from 'rison-node';
 import { getInitialTimeFilters } from '../angular/get_initial_time_filters';
 import { getInitialRefreshConfig } from '../angular/get_initial_refresh_config';
 import { getInitialQuery } from '../angular/get_initial_query';
-import {getMapsSavedObjectLoader} from "../angular/services/gis_map_saved_object_loader";
+import { getMapsSavedObjectLoader } from '../angular/services/gis_map_saved_object_loader';
 
-export const MapsCreateEditView = withRouter(class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      store: createMapStore(),
-      prevIndexPatternIds: undefined,
-      // TODO: Replace empty object w/ global state replacement
-      globalState: {},
-      filters: [],
-      showSaveQuery: getMapsCapabilities().saveQuery,
-      layerList: [],
-    };
-  }
-
-  componentDidMount() {
-    const { savedMapId } = this.props.match.params;
-    this.initMap(savedMapId);
-  }
-
-  getInitialLayersFromUrlParam() {
-    const locationSplit = window.location.href.split('?');
-    if (locationSplit.length <= 1) {
-      return [];
-    }
-    const mapAppParams = new URLSearchParams(locationSplit[1]);
-    if (!mapAppParams.has('initialLayers')) {
-      return [];
+export const MapsCreateEditView = withRouter(
+  class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        store: createMapStore(),
+        prevIndexPatternIds: undefined,
+        // TODO: Replace empty object w/ global state replacement
+        globalState: {},
+        filters: [],
+        showSaveQuery: getMapsCapabilities().saveQuery,
+        layerList: [],
+      };
     }
 
-    try {
-      return rison.decode_array(mapAppParams.get('initialLayers'));
-    } catch (e) {
-      getToasts().addWarning({
-        title: i18n.translate('xpack.maps.initialLayers.unableToParseTitle', {
-          defaultMessage: `Inital layers not added to map`,
-        }),
-        text: i18n.translate('xpack.maps.initialLayers.unableToParseMessage', {
-          defaultMessage: `Unable to parse contents of 'initialLayers' parameter. Error: {errorMsg}`,
-          values: { errorMsg: e.message },
-        }),
-      });
-      return [];
+    componentDidMount() {
+      const { savedMapId } = this.props.match.params;
+      this.initMap(savedMapId);
     }
-  }
 
-  async updateIndexPatterns(nextIndexPatternIds) {
-    const { prevIndexPatternIds } = this.state;
-    const indexPatterns = [];
-    const getIndexPatternPromises = nextIndexPatternIds.map(async indexPatternId => {
-      try {
-        const indexPattern = await getIndexPatternService().get(indexPatternId);
-        indexPatterns.push(indexPattern);
-      } catch (err) {
-        // unable to fetch index pattern
+    getInitialLayersFromUrlParam() {
+      const locationSplit = window.location.href.split('?');
+      if (locationSplit.length <= 1) {
+        return [];
       }
-    });
+      const mapAppParams = new URLSearchParams(locationSplit[1]);
+      if (!mapAppParams.has('initialLayers')) {
+        return [];
+      }
 
-    await Promise.all(getIndexPatternPromises);
-    // ignore outdated results
-    if (prevIndexPatternIds !== nextIndexPatternIds) {
-      return;
+      try {
+        return rison.decode_array(mapAppParams.get('initialLayers'));
+      } catch (e) {
+        getToasts().addWarning({
+          title: i18n.translate('xpack.maps.initialLayers.unableToParseTitle', {
+            defaultMessage: `Inital layers not added to map`,
+          }),
+          text: i18n.translate('xpack.maps.initialLayers.unableToParseMessage', {
+            defaultMessage: `Unable to parse contents of 'initialLayers' parameter. Error: {errorMsg}`,
+            values: { errorMsg: e.message },
+          }),
+        });
+        return [];
+      }
     }
-    this.setState({
-      indexPatterns,
-    });
-  }
 
-  async handleStoreChanges() {
-    const { store, prevIndexPatternIds } = this.state;
-    const nextIsFullScreen = getIsFullScreen(store.getState());
-    if (nextIsFullScreen !== $scope.isFullScreen) {
-      // Must trigger digest cycle for angular top nav to redraw itself when isFullScreen changes
-      $scope.$evalAsync(() => {
-        $scope.isFullScreen = nextIsFullScreen;
+    async updateIndexPatterns(nextIndexPatternIds) {
+      const { prevIndexPatternIds } = this.state;
+      const indexPatterns = [];
+      const getIndexPatternPromises = nextIndexPatternIds.map(async indexPatternId => {
+        try {
+          const indexPattern = await getIndexPatternService().get(indexPatternId);
+          indexPatterns.push(indexPattern);
+        } catch (err) {
+          // unable to fetch index pattern
+        }
       });
-    }
 
-    const nextIndexPatternIds = getQueryableUniqueIndexPatternIds(store.getState());
-    if (nextIndexPatternIds !== prevIndexPatternIds) {
+      await Promise.all(getIndexPatternPromises);
+      // ignore outdated results
+      if (prevIndexPatternIds !== nextIndexPatternIds) {
+        return;
+      }
       this.setState({
-        prevIndexPatternIds: nextIndexPatternIds,
-      });
-      await this.updateIndexPatterns(nextIndexPatternIds);
-    }
-
-    const nextIsSaveDisabled = hasDirtyState(store.getState());
-    if (nextIsSaveDisabled !== $scope.isSaveDisabled) {
-      $scope.$evalAsync(() => {
-        $scope.isSaveDisabled = nextIsSaveDisabled;
+        indexPatterns,
       });
     }
 
-    const flyoutDisplay = getFlyoutDisplay(store.getState());
-    const nextIsOpenSettingsDisabled = flyoutDisplay !== FLYOUT_STATE.NONE;
-    if (nextIsOpenSettingsDisabled !== $scope.isOpenSettingsDisabled) {
-      $scope.$evalAsync(() => {
-        $scope.isOpenSettingsDisabled = nextIsOpenSettingsDisabled;
-      });
+    async handleStoreChanges() {
+      const { store, prevIndexPatternIds } = this.state;
+      const nextIsFullScreen = getIsFullScreen(store.getState());
+      if (nextIsFullScreen !== $scope.isFullScreen) {
+        // Must trigger digest cycle for angular top nav to redraw itself when isFullScreen changes
+        $scope.$evalAsync(() => {
+          $scope.isFullScreen = nextIsFullScreen;
+        });
+      }
+
+      const nextIndexPatternIds = getQueryableUniqueIndexPatternIds(store.getState());
+      if (nextIndexPatternIds !== prevIndexPatternIds) {
+        this.setState({
+          prevIndexPatternIds: nextIndexPatternIds,
+        });
+        await this.updateIndexPatterns(nextIndexPatternIds);
+      }
+
+      const nextIsSaveDisabled = hasDirtyState(store.getState());
+      if (nextIsSaveDisabled !== $scope.isSaveDisabled) {
+        $scope.$evalAsync(() => {
+          $scope.isSaveDisabled = nextIsSaveDisabled;
+        });
+      }
+
+      const flyoutDisplay = getFlyoutDisplay(store.getState());
+      const nextIsOpenSettingsDisabled = flyoutDisplay !== FLYOUT_STATE.NONE;
+      if (nextIsOpenSettingsDisabled !== $scope.isOpenSettingsDisabled) {
+        $scope.$evalAsync(() => {
+          $scope.isOpenSettingsDisabled = nextIsOpenSettingsDisabled;
+        });
+      }
     }
-  }
 
-  // TODO: Replace empty object w/ app state replacement
-  getAppStateFilters() {
-    return _.get({}, 'filters', []);
-  }
-
-  dispatchSetQuery(refresh) {
-    const { store, filters, query, time } = this.state;
-    store.dispatch(
-      setQuery({
-        filters,
-        query,
-        timeFilters: time,
-        refresh,
-      })
-    );
-  }
-
-  async onQueryChange({ filters, query, time, refresh }) {
-    const { filterManager } = getData().query;
-    if (filters) {
-      filterManager.setFilters(filters); // Maps and merges filters
-      this.setState({
-        filters: filterManager.getFilters(),
-      });
+    // TODO: Replace empty object w/ app state replacement
+    getAppStateFilters() {
+      return _.get({}, 'filters', []);
     }
-    if (query) {
-      this.setState({ query });
-    }
-    if (time) {
-      this.setState({ time });
-    }
-    // this.syncAppAndGlobalState();
-    this.dispatchSetQuery(refresh);
-  }
 
-  async _fetchSavedMap(savedObjectId) {
-    const savedObjectLoader = getMapsSavedObjectLoader();
-    return await savedObjectLoader.get(savedObjectId);
-  }
-
-  getSavedMapFilters(savedMap) {
-    const { globalState, mapsAppState } = this.props;
-    return {
-      query: getInitialQuery({
-        mapStateJSON: savedMap.mapStateJSON,
-        appState: mapsAppState,
-        userQueryLanguage: getUiSettings().get('search:queryLanguage'),
-      }),
-      time: getInitialTimeFilters({
-        mapStateJSON: savedMap.mapStateJSON,
-        globalState: globalState,
-      }),
-      refreshConfig: getInitialRefreshConfig({
-        mapStateJSON: savedMap.mapStateJSON,
-        globalState: globalState,
-      }),
-    };
-  }
-
-  async initMap(savedMapId) {
-    let unsubscribe;
-    const { store, refreshConfig } = this.state;
-    const { globalState } = this.props;
-
-    let savedMap;
-    let savedMapFilters = {};
-    if (savedMapId) {
-      savedMap = await this._fetchSavedMap(savedMapId);
-      savedMapFilters = this.getSavedMapFilters(savedMap);
-    }
-    const layerList = getInitialLayers(savedMap.layerListJSON, this.getInitialLayersFromUrlParam());
-    // Update state
-    this.state = {
-      ...this.state,
-      ...savedMapFilters,
-      initialLayerListConfig: copyPersistentState(layerList),
-      layerList
-    };
-
-    // clear old UI state
-    store.dispatch(setSelectedLayer(null));
-    store.dispatch(updateFlyout(FLYOUT_STATE.NONE));
-    store.dispatch(setReadOnly(!getMapsCapabilities().save));
-
-    // TODO: Handle store changes
-    // await this.handleStoreChanges();
-    // unsubscribe = store.subscribe(async () => {
-    //   await this.handleStoreChanges(store);
-    // });
-
-    // sync store with savedMap mapState
-    let savedObjectFilters = [];
-    if (savedMap.mapStateJSON) {
-      const mapState = JSON.parse(savedMap.mapStateJSON);
+    dispatchSetQuery(refresh) {
+      const { store, filters, query, time } = this.state;
       store.dispatch(
-        setGotoWithCenter({
-          lat: mapState.center.lat,
-          lon: mapState.center.lon,
-          zoom: mapState.zoom,
+        setQuery({
+          filters,
+          query,
+          timeFilters: time,
+          refresh,
         })
       );
-      if (mapState.filters) {
-        savedObjectFilters = mapState.filters;
-      }
-      if (mapState.settings) {
-        store.dispatch(setMapSettings(mapState.settings));
-      }
     }
 
-    if (savedMap.uiStateJSON) {
-      const uiState = JSON.parse(savedMap.uiStateJSON);
-      store.dispatch(
-        setIsLayerTOCOpen(_.get(uiState, 'isLayerTOCOpen', DEFAULT_IS_LAYER_TOC_OPEN))
+    async onQueryChange({ filters, query, time, refresh }) {
+      const { filterManager } = getData().query;
+      if (filters) {
+        filterManager.setFilters(filters); // Maps and merges filters
+        this.setState({
+          filters: filterManager.getFilters(),
+        });
+      }
+      if (query) {
+        this.setState({ query });
+      }
+      if (time) {
+        this.setState({ time });
+      }
+      // this.syncAppAndGlobalState();
+      this.dispatchSetQuery(refresh);
+    }
+
+    async _fetchSavedMap(savedObjectId) {
+      const savedObjectLoader = getMapsSavedObjectLoader();
+      return await savedObjectLoader.get(savedObjectId);
+    }
+
+    getSavedMapFilters(savedMap) {
+      const { globalState, mapsAppState } = this.props;
+      return {
+        query: getInitialQuery({
+          mapStateJSON: savedMap.mapStateJSON,
+          appState: mapsAppState,
+          userQueryLanguage: getUiSettings().get('search:queryLanguage'),
+        }),
+        time: getInitialTimeFilters({
+          mapStateJSON: savedMap.mapStateJSON,
+          globalState: globalState,
+        }),
+        refreshConfig: getInitialRefreshConfig({
+          mapStateJSON: savedMap.mapStateJSON,
+          globalState: globalState,
+        }),
+      };
+    }
+
+    async getSavedMap(savedMapId) {
+      let savedMap;
+      let savedMapFilters = {};
+      if (savedMapId) {
+        savedMap = await this._fetchSavedMap(savedMapId);
+        savedMapFilters = this.getSavedMapFilters(savedMap);
+      }
+      const layerList = getInitialLayers(
+        savedMap.layerListJSON,
+        this.getInitialLayersFromUrlParam()
       );
-      store.dispatch(setOpenTOCDetails(_.get(uiState, 'openTOCDetails', [])));
+      // Update state
+      this.state = {
+        ...this.state,
+        ...savedMapFilters,
+        initialLayerListConfig: copyPersistentState(layerList),
+        layerList,
+      };
+      return savedMap;
     }
 
-    store.dispatch(replaceLayerList(layerList));
-    store.dispatch(setRefreshConfig(refreshConfig));
+    async initMap(savedMapId) {
+      let unsubscribe;
+      const savedMap = await this.getSavedMap(savedMapId);
+      const { globalState } = this.props;
+      const { store, refreshConfig, layerList } = this.state;
 
-    const initialFilters = [
-      ..._.get(globalState, 'filters', []),
-      ...this.getAppStateFilters(),
-      ...savedObjectFilters,
-    ];
-    await this.onQueryChange({ filters: initialFilters });
-  }
+      // clear old UI state
+      store.dispatch(setSelectedLayer(null));
+      store.dispatch(updateFlyout(FLYOUT_STATE.NONE));
+      store.dispatch(setReadOnly(!getMapsCapabilities().save));
 
-  render() {
-    const { store } = this.state;
-    return (
-      <Provider store={store}>
-        <GisMap addFilters={null} />
-      </Provider>
-    );
+      // TODO: Handle store changes
+      // await this.handleStoreChanges();
+      // unsubscribe = store.subscribe(async () => {
+      //   await this.handleStoreChanges(store);
+      // });
+
+      // sync store with savedMap mapState
+      let savedObjectFilters = [];
+      if (savedMap.mapStateJSON) {
+        const mapState = JSON.parse(savedMap.mapStateJSON);
+        store.dispatch(
+          setGotoWithCenter({
+            lat: mapState.center.lat,
+            lon: mapState.center.lon,
+            zoom: mapState.zoom,
+          })
+        );
+        if (mapState.filters) {
+          savedObjectFilters = mapState.filters;
+        }
+        if (mapState.settings) {
+          store.dispatch(setMapSettings(mapState.settings));
+        }
+      }
+
+      if (savedMap.uiStateJSON) {
+        const uiState = JSON.parse(savedMap.uiStateJSON);
+        store.dispatch(
+          setIsLayerTOCOpen(_.get(uiState, 'isLayerTOCOpen', DEFAULT_IS_LAYER_TOC_OPEN))
+        );
+        store.dispatch(setOpenTOCDetails(_.get(uiState, 'openTOCDetails', [])));
+      }
+
+      store.dispatch(replaceLayerList(layerList));
+      store.dispatch(setRefreshConfig(refreshConfig));
+
+      const initialFilters = [
+        ..._.get(globalState, 'filters', []),
+        ...this.getAppStateFilters(),
+        ...savedObjectFilters,
+      ];
+      await this.onQueryChange({ filters: initialFilters });
+    }
+
+    render() {
+      const { store } = this.state;
+      return (
+        <Provider store={store}>
+          <GisMap addFilters={null} />
+        </Provider>
+      );
+    }
   }
-});
+);
 
 MapsCreateEditView.defaultProps = {
   mapsAppState: {},
