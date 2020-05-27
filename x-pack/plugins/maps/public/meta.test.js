0d3d4adf89ef14753706a4e0d4cf8a45f7219327
Nathan Reese <reese.nathan@gmail.com>
2020-05-27 14:46:52 -0600

    add jest test for getGlyphsUrl

--- x-pack/plugins/maps/public/meta.test.js
+++ x-pack/plugins/maps/public/meta.test.js
@@ -5,7 +5,7 @@
  */
 
 import { EMSClient } from '@elastic/ems-client';
-import { getEMSClient } from './meta';
+import { getEMSClient, getGlyphUrl } from './meta';
 
 jest.mock('@elastic/ems-client');
 
@@ -22,10 +22,56 @@ describe('default use without proxy', () => {
     require('./kibana_services').getEmsLandingPageUrl = () => 'http://test.com';
   });
 
-  it('should construct EMSClient with absolute file and tile API urls', async () => {
+  test('should construct EMSClient with absolute file and tile API urls', async () => {
     getEMSClient();
     const mockEmsClientCall = EMSClient.mock.calls[0];
     expect(mockEmsClientCall[0].fileApiUrl.startsWith('https://file-api')).toBe(true);
     expect(mockEmsClientCall[0].tileApiUrl.startsWith('https://tile-api')).toBe(true);
+  });
+});
+
+describe('getGlyphUrl', () => {
+  describe('EMS enabled', () => {
+    const EMS_FONTS_URL_MOCK = 'ems/fonts';
+    beforeAll(() => {
+      require('./kibana_services').getIsEmsEnabled = () => true;
+      require('./kibana_services').getEmsFontLibraryUrl = () => EMS_FONTS_URL_MOCK;
+    });
+
+    describe('EMS proxy enabled', () => {
+      beforeAll(() => {
+        require('./kibana_services').getProxyElasticMapsServiceInMaps = () => true;
+      });
+
+      test('should return proxied EMS fonts URL', async () => {
+        expect(getGlyphUrl()).toBe('http://localhost/api/maps/ems/tiles/fonts/{fontstack}/{range}');
+      });
+    });
+
+    describe('EMS proxy disabled', () => {
+      beforeAll(() => {
+        require('./kibana_services').getProxyElasticMapsServiceInMaps = () => false;
+      });
+
+      test('should return EMS fonts URL', async () => {
+        expect(getGlyphUrl()).toBe(EMS_FONTS_URL_MOCK);
+      });
+    });
+  });
+
+  describe('EMS disabled', () => {
+    beforeAll(() => {
+      const mockHttp = {
+        basePath: {
+          prepend: (path) => `abc${path}`,
+        },
+      };
+      require('./kibana_services').getHttp = () => mockHttp;
+      require('./kibana_services').getIsEmsEnabled = () => false;
+    });
+
+    test('should return kibana fonts URL', async () => {
+      expect(getGlyphUrl()).toBe('abc/api/maps/fonts/{fontstack}/{range}');
+    });
   });
 });
