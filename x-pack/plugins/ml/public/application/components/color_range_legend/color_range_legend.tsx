991ddb373daee3e7fe255f2864eb5d50dca6251c
Mikhail Shustov <restrry@gmail.com>
2020-05-28 18:19:24 +0200

    fix type error

--- x-pack/plugins/ml/public/application/components/color_range_legend/color_range_legend.tsx
+++ x-pack/plugins/ml/public/application/components/color_range_legend/color_range_legend.tsx
@@ -12,7 +12,7 @@ import { EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
 const COLOR_RANGE_RESOLUTION = 10;
 
 interface ColorRangeLegendProps {
-  colorRange: (d: number) => string;
+  colorRange: (d: number) => number;
   justifyTicks?: boolean;
   showTicks?: boolean;
   title?: string;
