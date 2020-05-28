cb8d4f9f49406643ffd19acefdd5220445597ed0
restrry <restrry@gmail.com>
2020-05-28 12:16:57 +0200

    update legacy code

--- src/legacy/core_plugins/elasticsearch/index.js
+++ src/legacy/core_plugins/elasticsearch/index.js
@@ -34,9 +34,9 @@ export default function (kibana) {
       // All methods that ES plugin exposes are synchronous so we should get the first
       // value from all observables here to be able to synchronously return and create
       // cluster clients afterwards.
-      const { adminClient, dataClient } = server.newPlatform.setup.core.elasticsearch;
-      const adminCluster = new Cluster(adminClient);
-      const dataCluster = new Cluster(dataClient);
+      const { client } = server.newPlatform.setup.core.elasticsearch.legacy;
+      const adminCluster = new Cluster(client);
+      const dataCluster = new Cluster(client);
 
       const esConfig = await server.newPlatform.__internals.elasticsearch.legacy.config$
         .pipe(first())
