62326fa5063ca78f4706ef5d259053ca7b9643ba
Frank Hassanabad <frank.hassanabad@elastic.co>
2020-05-28 16:45:29 -0600

    [SIEM][Lists] Adds circular dependency checker for lists plugin
    ## Summary
    
    * Added dependency checker for the public and common folders for lists
    
    # Conflicts:
    #	test/scripts/jenkins_xpack.sh

--- test/scripts/jenkins_xpack.sh
+++ test/scripts/jenkins_xpack.sh
@@ -20,8 +20,28 @@ checks-reporter-with-killswitch "X-Pack SIEM cyclic dependency test" node plugin
 echo ""
 echo ""
 
-# echo " -> Running jest integration tests"
-# cd "$XPACK_DIR"
-# node scripts/jest_integration --ci --verbose
-# echo ""
-# echo ""
+  echo " -> Running List cyclic dependency test"
+  cd "$XPACK_DIR"
+  checks-reporter-with-killswitch "X-Pack List cyclic dependency test" node plugins/lists/scripts/check_circular_deps
+  echo ""
+  echo ""
+
+  # echo " -> Running jest integration tests"
+  # cd "$XPACK_DIR"
+  # node scripts/jest_integration --ci --verbose
+  # echo ""
+  # echo ""
+else
+  echo " -> Running jest tests with coverage"
+  cd "$XPACK_DIR"
+  # build runtime for canvas
+  echo "NODE_ENV=$NODE_ENV"
+  node ./plugins/canvas/scripts/shareable_runtime
+  node --max-old-space-size=6144 scripts/jest --ci --verbose --coverage
+  # rename file in order to be unique one
+  test -f ../target/kibana-coverage/jest/coverage-final.json \
+    && mv ../target/kibana-coverage/jest/coverage-final.json \
+    ../target/kibana-coverage/jest/xpack-coverage-final.json
+  echo ""
+  echo ""
+fi
