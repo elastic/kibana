9bccdd303c8121bfd45f20ffdf1df7f43e9d50f7
Mikhail Shustov <restrry@gmail.com>
2020-05-28 12:13:20 +0200

    move elasticsearch client under legacy namespace

--- src/core/server/elasticsearch/types.ts
+++ src/core/server/elasticsearch/types.ts
@@ -28,58 +28,46 @@ import { ServiceStatus } from '../status';
  * @public
  */
 export interface ElasticsearchServiceSetup {
-  /**
-   * @deprecated
-   * Use {@link ElasticsearchServiceStart.legacy | ElasticsearchServiceStart.legacy.createClient} instead.
-   *
-   * Create application specific Elasticsearch cluster API client with customized config. See {@link IClusterClient}.
-   *
-   * @param type Unique identifier of the client
-   * @param clientConfig A config consists of Elasticsearch JS client options and
-   * valid sub-set of Elasticsearch service config.
-   * We fill all the missing properties in the `clientConfig` using the default
-   * Elasticsearch config so that we don't depend on default values set and
-   * controlled by underlying Elasticsearch JS client.
-   * We don't run validation against the passed config and expect it to be valid.
-   *
-   * @example
-   * ```js
-   * const client = elasticsearch.createCluster('my-app-name', config);
-   * const data = await client.callAsInternalUser();
-   * ```
-   */
-  readonly createClient: (
-    type: string,
-    clientConfig?: Partial<ElasticsearchClientConfig>
-  ) => ICustomClusterClient;
+  legacy: {
+    /**
+     * @deprecated
+     * Use {@link ElasticsearchServiceStart.legacy | ElasticsearchServiceStart.legacy.createClient} instead.
+     *
+     * Create application specific Elasticsearch cluster API client with customized config. See {@link IClusterClient}.
+     *
+     * @param type Unique identifier of the client
+     * @param clientConfig A config consists of Elasticsearch JS client options and
+     * valid sub-set of Elasticsearch service config.
+     * We fill all the missing properties in the `clientConfig` using the default
+     * Elasticsearch config so that we don't depend on default values set and
+     * controlled by underlying Elasticsearch JS client.
+     * We don't run validation against the passed config and expect it to be valid.
+     *
+     * @example
+     * ```js
+     * const client = elasticsearch.createCluster('my-app-name', config);
+     * const data = await client.callAsInternalUser();
+     * ```
+     */
+    readonly createClient: (
+      type: string,
+      clientConfig?: Partial<ElasticsearchClientConfig>
+    ) => ICustomClusterClient;
 
-  /**
-   * @deprecated
-   * Use {@link ElasticsearchServiceStart.legacy | ElasticsearchServiceStart.legacy.client} instead.
-   *
-   * A client for the `admin` cluster. All Elasticsearch config value changes are processed under the hood.
-   * See {@link IClusterClient}.
-   *
-   * @example
-   * ```js
-   * const client = core.elasticsearch.adminClient;
-   * ```
-   */
-  readonly adminClient: IClusterClient;
-
-  /**
-   * @deprecated
-   * Use {@link ElasticsearchServiceStart.legacy | ElasticsearchServiceStart.legacy.client} instead.
-   *
-   * A client for the `data` cluster. All Elasticsearch config value changes are processed under the hood.
-   * See {@link IClusterClient}.
-   *
-   * @example
-   * ```js
-   * const client = core.elasticsearch.dataClient;
-   * ```
-   */
-  readonly dataClient: IClusterClient;
+    /**
+     * @deprecated
+     * Use {@link ElasticsearchServiceStart.legacy | ElasticsearchServiceStart.legacy.client} instead.
+     *
+     * All Elasticsearch config value changes are processed under the hood.
+     * See {@link IClusterClient}.
+     *
+     * @example
+     * ```js
+     * const client = core.elasticsearch.legacy.client;
+     * ```
+     */
+    readonly client: IClusterClient;
+  };
 }
 
 /**
@@ -123,9 +111,9 @@ export interface ElasticsearchServiceStart {
 }
 
 /** @internal */
-export interface InternalElasticsearchServiceSetup extends ElasticsearchServiceSetup {
+export interface InternalElasticsearchServiceSetup {
   // Required for the BWC with the legacy Kibana only.
-  readonly legacy: {
+  readonly legacy: ElasticsearchServiceSetup['legacy'] & {
     readonly config$: Observable<ElasticsearchConfig>;
   };
   esNodesCompatibility$: Observable<NodesVersionCompatibility>;
