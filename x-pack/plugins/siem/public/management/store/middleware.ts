0e7417209e69cce8105f02a3b09ca94e985c3f5c
Paul Tavares <56442535+paul-tavares@users.noreply.github.com>
2020-05-29 10:12:51 -0400

    [SIEMDPOINT][WIP] Add Management section and move Policy related views (#67417)
    * Add Management top-level nav tab item
    * Move of Policy related views to `management`
    * Enhance PageView component to support sub-tabs

---
+++ x-pack/plugins/siem/public/management/store/middleware.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { ImmutableMultipleMiddlewareFactory, substateMiddlewareFactory } from '../../common/store';
+import { policyListMiddlewareFactory } from '../pages/policy/store/policy_list';
+import { policyDetailsMiddlewareFactory } from '../pages/policy/store/policy_details';
+import {
+  MANAGEMENT_STORE_GLOBAL_NAMESPACE,
+  MANAGEMENT_STORE_POLICY_DETAILS_NAMESPACE,
+  MANAGEMENT_STORE_POLICY_LIST_NAMESPACE,
+} from '../common/constants';
+
+// @ts-ignore
+export const managementMiddlewareFactory: ImmutableMultipleMiddlewareFactory = (
+  coreStart,
+  depsStart
+) => {
+  return [
+    substateMiddlewareFactory(
+      (globalState) =>
+        globalState[MANAGEMENT_STORE_GLOBAL_NAMESPACE][MANAGEMENT_STORE_POLICY_LIST_NAMESPACE],
+      policyListMiddlewareFactory(coreStart, depsStart)
+    ),
+    substateMiddlewareFactory(
+      (globalState) =>
+        globalState[MANAGEMENT_STORE_GLOBAL_NAMESPACE][MANAGEMENT_STORE_POLICY_DETAILS_NAMESPACE],
+      policyDetailsMiddlewareFactory(coreStart, depsStart)
+    ),
+  ];
+};
