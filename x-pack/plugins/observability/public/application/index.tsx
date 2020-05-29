bae11f47aac34d2104492c6246c9b349e35d4c6a
cauemarcondes <caue.marcondes@elastic.co>
2020-05-29 14:23:40 +0200

    prepending links

--- x-pack/plugins/observability/public/application/index.tsx
+++ x-pack/plugins/observability/public/application/index.tsx
@@ -7,9 +7,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppMountParameters, CoreStart } from '../../../../../src/core/public';
 import { Home } from '../pages/home';
+import { PluginContext } from '../context/plugin_context';
 
 export const renderApp = (core: CoreStart, { element }: AppMountParameters) => {
-  ReactDOM.render(<Home core={core} />, element);
+  ReactDOM.render(
+    <PluginContext.Provider value={{ core }}>
+      <Home />
+    </PluginContext.Provider>,
+    element
+  );
   return () => {
     ReactDOM.unmountComponentAtNode(element);
   };
