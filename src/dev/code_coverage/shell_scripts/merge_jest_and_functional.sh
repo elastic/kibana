b8665c4f8460cabe6f979e388b0a2b7b6f362d6e
Tre' Seymour <wayne.seymour@elastic.co>
2020-06-01 11:42:41 -0600

    Add debugging to try to see what's going on with merging the reports for jest.

--- src/dev/code_coverage/shell_scripts/merge_jest_and_functional.sh
+++ src/dev/code_coverage/shell_scripts/merge_jest_and_functional.sh
@@ -4,6 +4,18 @@ EXTRACT_START_DIR=tmp/extracted_coverage
 EXTRACT_END_DIR=target/kibana-coverage
 COMBINED_EXTRACT_DIR=/${EXTRACT_START_DIR}/${EXTRACT_END_DIR}
 
+set -e
+function final() {
+  set +e
+  for x in jest functional; do
+    echo "### List temp dir for: ${x}"
+    ls -la $COMBINED_EXTRACT_DIR/${x}
+    echo "### List report dir for: ${x}"
+    ls -la $EXTRACT_END_DIR/${x}-combined
+  done
+}
+trap 'final' EXIT
+
 echo "### Merge coverage reports"
 for x in jest functional; do
   yarn nyc report --temp-dir $COMBINED_EXTRACT_DIR/${x} --report-dir $EXTRACT_END_DIR/${x}-combined --reporter=html --reporter=json-summary
