5d29f12ef440ef93899892cc0c1444955efa273c
Patryk Kopycinski <contact@patrykkopycinski.com>
2020-06-01 20:43:30 +0200

    Fix timelineType

--- x-pack/plugins/siem/server/lib/timeline/saved_object.ts
+++ x-pack/plugins/siem/server/lib/timeline/saved_object.ts
@@ -320,7 +320,11 @@ const updatePartialSavedTimeline = async (
   );
 };
 
-export const resetTimeline = async (request: FrameworkRequest, timelineIds: string[]) => {
+export const resetTimeline = async (
+  request: FrameworkRequest,
+  timelineIds: string[],
+  timelineType: TimelineType
+) => {
   if (!timelineIds.length) {
     return Promise.reject(new Error('timelineIds is empty'));
   }
@@ -336,7 +340,7 @@ export const resetTimeline = async (request: FrameworkRequest, timelineIds: stri
 
   const response = await Promise.all(
     timelineIds.map((timelineId) =>
-      updatePartialSavedTimeline(request, timelineId, draftTimelineDefaults)
+      updatePartialSavedTimeline(request, timelineId, { ...draftTimelineDefaults, timelineType })
     )
   );
 
