baef6f78c45c4347863d382afeec73a78e40877f
Tre' Seymour <wayne.seymour@elastic.co>
2020-06-01 14:20:13 -0600

    change the debugging a bit, list the dirs at the end of merging jest and functional.

--- src/dev/code_coverage/shell_scripts/merge_jest_and_functional.sh
+++ src/dev/code_coverage/shell_scripts/merge_jest_and_functional.sh
@@ -4,9 +4,7 @@ EXTRACT_START_DIR=tmp/extracted_coverage
 EXTRACT_END_DIR=target/kibana-coverage
 COMBINED_EXTRACT_DIR=/${EXTRACT_START_DIR}/${EXTRACT_END_DIR}
 
-set -e
-function final() {
-  set +e
+function listDirs() {
   for x in jest functional; do
     echo "### List temp dir for: ${x}"
     ls -la $COMBINED_EXTRACT_DIR/${x}
@@ -14,9 +12,10 @@ function final() {
     ls -la $EXTRACT_END_DIR/${x}-combined
   done
 }
-trap 'final' EXIT
 
 echo "### Merge coverage reports"
 for x in jest functional; do
   yarn nyc report --temp-dir $COMBINED_EXTRACT_DIR/${x} --report-dir $EXTRACT_END_DIR/${x}-combined --reporter=html --reporter=json-summary
 done
+
+listDirs
