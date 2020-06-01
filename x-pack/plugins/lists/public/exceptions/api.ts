81480ff115ac2142eca8aa47be8eeba376417232
Yara Tercero <yctercero@users.noreply.github.com>
2020-06-01 14:32:42 -0400

    [SIEM][Exceptions] - Update exceptions hooks to include _find filtering (#67435)
    ### Summary 
    
    - Updates exception list hooks to include filtering options and updates corresponding unit tests.
    - Adds refreshList callback to hook that fetches the list and its items
    - Updates hooks tests to test onError callback
    - Updates tests to use type checking more effectively per feedback from @FrankHassanabad (thanks!)

--- x-pack/plugins/lists/public/exceptions/api.ts
+++ x-pack/plugins/lists/public/exceptions/api.ts
@@ -4,7 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EXCEPTION_LIST_ITEM_URL, EXCEPTION_LIST_URL } from '../../common/constants';
+import {
+  EXCEPTION_LIST_ITEM_URL,
+  EXCEPTION_LIST_NAMESPACE,
+  EXCEPTION_LIST_NAMESPACE_AGNOSTIC,
+  EXCEPTION_LIST_URL,
+} from '../../common/constants';
 import {
   ExceptionListItemSchema,
   ExceptionListSchema,
@@ -21,6 +26,7 @@ import {
 /**
  * Add provided ExceptionList
  *
+ * @param http Kibana http service
  * @param list exception list to add
  * @param signal to cancel request
  *
@@ -43,6 +49,7 @@ export const addExceptionList = async ({
 /**
  * Add provided ExceptionListItem
  *
+ * @param http Kibana http service
  * @param listItem exception list item to add
  * @param signal to cancel request
  *
@@ -65,7 +72,9 @@ export const addExceptionListItem = async ({
 /**
  * Fetch an ExceptionList by providing a ExceptionList ID
  *
+ * @param http Kibana http service
  * @param id ExceptionList ID (not list_id)
+ * @param namespaceType ExceptionList namespace_type
  * @param signal to cancel request
  *
  * @throws An error if response is not OK
@@ -73,18 +82,23 @@ export const addExceptionListItem = async ({
 export const fetchExceptionListById = async ({
   http,
   id,
+  namespaceType,
   signal,
 }: ApiCallByIdProps): Promise<ExceptionListSchema> =>
   http.fetch<ExceptionListSchema>(`${EXCEPTION_LIST_URL}`, {
     method: 'GET',
-    query: { id },
+    query: { id, namespace_type: namespaceType },
     signal,
   });
 
 /**
  * Fetch an ExceptionList's ExceptionItems by providing a ExceptionList list_id
  *
- * @param id ExceptionList list_id (not ID)
+ * @param http Kibana http service
+ * @param listId ExceptionList list_id (not ID)
+ * @param namespaceType ExceptionList namespace_type
+ * @param filterOptions optional - filter by field or tags
+ * @param pagination optional
  * @param signal to cancel request
  *
  * @throws An error if response is not OK
@@ -92,18 +106,48 @@ export const fetchExceptionListById = async ({
 export const fetchExceptionListItemsByListId = async ({
   http,
   listId,
+  namespaceType,
+  filterOptions = {
+    filter: '',
+    tags: [],
+  },
+  pagination = {
+    page: 1,
+    perPage: 20,
+    total: 0,
+  },
   signal,
-}: ApiCallByListIdProps): Promise<FoundExceptionListItemSchema> =>
-  http.fetch<FoundExceptionListItemSchema>(`${EXCEPTION_LIST_ITEM_URL}/_find`, {
+}: ApiCallByListIdProps): Promise<FoundExceptionListItemSchema> => {
+  const namespace =
+    namespaceType === 'agnostic' ? EXCEPTION_LIST_NAMESPACE_AGNOSTIC : EXCEPTION_LIST_NAMESPACE;
+  const filters = [
+    ...(filterOptions.filter.length
+      ? [`${namespace}.attributes.entries.field:${filterOptions.filter}*`]
+      : []),
+    ...(filterOptions.tags?.map((t) => `${namespace}.attributes.tags:${t}`) ?? []),
+  ];
+
+  const query = {
+    list_id: listId,
+    namespace_type: namespaceType,
+    page: pagination.page,
+    per_page: pagination.perPage,
+    ...(filters.length ? { filter: filters.join(' AND ') } : {}),
+  };
+
+  return http.fetch<FoundExceptionListItemSchema>(`${EXCEPTION_LIST_ITEM_URL}/_find`, {
     method: 'GET',
-    query: { list_id: listId },
+    query,
     signal,
   });
+};
 
 /**
  * Fetch an ExceptionListItem by providing a ExceptionListItem ID
  *
+ * @param http Kibana http service
  * @param id ExceptionListItem ID (not item_id)
+ * @param namespaceType ExceptionList namespace_type
  * @param signal to cancel request
  *
  * @throws An error if response is not OK
@@ -111,18 +155,21 @@ export const fetchExceptionListItemsByListId = async ({
 export const fetchExceptionListItemById = async ({
   http,
   id,
+  namespaceType,
   signal,
 }: ApiCallByIdProps): Promise<ExceptionListItemSchema> =>
   http.fetch<ExceptionListItemSchema>(`${EXCEPTION_LIST_ITEM_URL}`, {
     method: 'GET',
-    query: { id },
+    query: { id, namespace_type: namespaceType },
     signal,
   });
 
 /**
  * Delete an ExceptionList by providing a ExceptionList ID
  *
+ * @param http Kibana http service
  * @param id ExceptionList ID (not list_id)
+ * @param namespaceType ExceptionList namespace_type
  * @param signal to cancel request
  *
  * @throws An error if response is not OK
@@ -130,18 +177,21 @@ export const fetchExceptionListItemById = async ({
 export const deleteExceptionListById = async ({
   http,
   id,
+  namespaceType,
   signal,
 }: ApiCallByIdProps): Promise<ExceptionListSchema> =>
   http.fetch<ExceptionListSchema>(`${EXCEPTION_LIST_URL}`, {
     method: 'DELETE',
-    query: { id },
+    query: { id, namespace_type: namespaceType },
     signal,
   });
 
 /**
  * Delete an ExceptionListItem by providing a ExceptionListItem ID
  *
+ * @param http Kibana http service
  * @param id ExceptionListItem ID (not item_id)
+ * @param namespaceType ExceptionList namespace_type
  * @param signal to cancel request
  *
  * @throws An error if response is not OK
@@ -149,10 +199,11 @@ export const deleteExceptionListById = async ({
 export const deleteExceptionListItemById = async ({
   http,
   id,
+  namespaceType,
   signal,
 }: ApiCallByIdProps): Promise<ExceptionListItemSchema> =>
   http.fetch<ExceptionListItemSchema>(`${EXCEPTION_LIST_ITEM_URL}`, {
     method: 'DELETE',
-    query: { id },
+    query: { id, namespace_type: namespaceType },
     signal,
   });
