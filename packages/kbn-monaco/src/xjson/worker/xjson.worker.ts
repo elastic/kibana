ad9f4d221f192eda0370d49d612801fcbc98478d
Jean-Louis Leysens <jloleysens@gmail.com>
2020-06-02 10:47:59 +0200

    Added regenerator runtime for worker bundle

--- packages/kbn-monaco/src/xjson/worker/xjson.worker.ts
+++ packages/kbn-monaco/src/xjson/worker/xjson.worker.ts
@@ -20,6 +20,7 @@
 // Please note: this module is intended to be run inside of a webworker.
 /* eslint-disable @kbn/eslint/module_migration */
 
+import 'regenerator-runtime/runtime';
 // @ts-ignore
 import * as worker from 'monaco-editor/esm/vs/editor/editor.worker';
 import { XJsonWorker } from './xjson_worker';
