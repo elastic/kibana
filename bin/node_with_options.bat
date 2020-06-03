c892867729e3a8508b8181fa0f21fa269c67d0e1
Tiago Costa <tiagoffcc@hotmail.com>
2020-06-03 19:46:20 +0100

    chore(NA): complete node_with_options scripts with bat version

--- bin/node_with_options.bat
+++ bin/node_with_options.bat
@@ -1,8 +1,34 @@
 @echo off
 
-SETLOCAL
+SETLOCAL ENABLEDELAYEDEXPANSION
 
+set SCRIPT_DIR=%~dp0
+for %%I in ("%SCRIPT_DIR%..") do set DIR=%%~dpfI
 
+set CONFIG_DIR=%KIBANA_PATH_CONF%
+If [%KIBANA_PATH_CONF%] == [] (
+  set CONFIG_DIR=%DIR%config
+)
+
+:: Setup node dev options file to node.dev.options
+set NODE_DEV_OPTIONS_FILE=%CONFIG_DIR%\node.dev.options
+
+:: Fallback to node.ci.options in case node.dev.options not exists
+If not exist "%NODE_DEV_OPTIONS_FILE%" (
+  set NODE_DEV_OPTIONS_FILE=%CONFIG_DIR%\node.ci.options
+)
+
+for /F "eol=# tokens=*" %%i in (%NODE_DEV_OPTIONS_FILE%) do (
+  If [!NODE_OPTIONS!] == [] (
+    set "NODE_OPTIONS=%%i"
+  )	Else (
+    set "NODE_OPTIONS=!NODE_OPTIONS! %%i"
+  )
+)
+
+:: This should run independently as the last instruction
+:: as we need NODE_OPTIONS previously set to expand
+node %*
 
 :finally
 
