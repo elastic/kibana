7ada624fbb75e60cf76c618834667e1d015583b9
Tim Sullivan <tsullivan@users.noreply.github.com>
2020-06-03 15:40:56 -0700

    [Reporting] Move code out of Legacy (#67904)
    * [Reporting] Move code out of Legacy
    
    * Elasticsearch is not a plugin dep
    
    * add data as plugin dependo
    
    * diff cleanup 1
    
    * log the browser download
    
    * Update paths in outside code for new Reporting home
    
    * fix download test
    
    * add numeral typing for x-pack/test
    
    * Fix jest tests for np migration
    
    * Shorten import paths
    
    * remove this file, add typings to the node module
    
    * remove local typing that has been provided by node module
    
    * Add optional security plugin dep
    
    * revert conflicting apm typings removal
    
    * fix i18n
    
    * fix snakecase whitelist
    
    Co-authored-by: Joel Griffith <joel.griffith@elastic.co>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    # Conflicts:
    #	.ci/packer_cache.sh
    #	src/dev/code_coverage/ingest_coverage/__tests__/transforms.test.js
    #	src/dev/code_coverage/ingest_coverage/integration_tests/mocks/jest-combined/coverage-summary-manual-mix.json
    #	x-pack/plugins/reporting/server/export_types/printable_pdf/server/create_job/index.ts

---
+++ x-pack/plugins/reporting/server/export_types/printable_pdf/server/create_job/index.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { KibanaRequest, RequestHandlerContext } from 'src/core/server';
+import { validateUrls } from '../../../../../common/validate_urls';
+import { cryptoFactory } from '../../../../lib';
+import { CreateJobFactory, ESQueueCreateJobFn } from '../../../../types';
+import { JobParamsPDF } from '../../types';
+// @ts-ignore no module def (deprecated module)
+import { compatibilityShimFactory } from './compatibility_shim';
+
+export const createJobFactory: CreateJobFactory<ESQueueCreateJobFn<
+  JobParamsPDF
+>> = function createJobFactoryFn(reporting, logger) {
+  const config = reporting.getConfig();
+  const setupDeps = reporting.getPluginSetupDeps();
+  const crypto = cryptoFactory(config.get('encryptionKey'));
+  const compatibilityShim = compatibilityShimFactory(logger);
+
+  return compatibilityShim(async function createJobFn(
+    { title, relativeUrls, browserTimezone, layout, objectType }: JobParamsPDF,
+    context: RequestHandlerContext,
+    req: KibanaRequest
+  ) {
+    const serializedEncryptedHeaders = await crypto.encrypt(req.headers);
+
+    validateUrls(relativeUrls);
+
+    return {
+      basePath: setupDeps.basePath(req),
+      browserTimezone,
+      forceNow: new Date().toISOString(),
+      headers: serializedEncryptedHeaders,
+      layout,
+      objects: relativeUrls.map((u) => ({ relativeUrl: u })),
+      title,
+      type: objectType, // Note: this changes the shape of the job params object
+    };
+  });
+};
