c72fe18e79c7cefb8d69c662aa6cd6ad89d42740
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2020-06-03 16:15:48 +0200

    use individual route for each KP app (#65977) (#68072)
    * use individual route for each KP app
    
    * cleanup

--- src/core/public/application/ui/app_container.test.tsx
+++ src/core/public/application/ui/app_container.test.tsx
@@ -54,6 +54,7 @@ describe('AppContainer', () => {
     appBasePath: '/base-path',
     appRoute: '/some-route',
     unmountBeforeMounting: false,
+    legacy: false,
     mount: async ({ element }: AppMountParameters) => {
       await promise;
       const container = document.createElement('div');
@@ -138,9 +139,10 @@ describe('AppContainer', () => {
   it('should call setIsMounting(false) if mounting throws', async () => {
     const [waitPromise, resolvePromise] = createResolver();
     const mounter = {
-      appBasePath: '/base-path',
+      appBasePath: '/base-path/some-route',
       appRoute: '/some-route',
       unmountBeforeMounting: false,
+      legacy: false,
       mount: async ({ element }: AppMountParameters) => {
         await waitPromise;
         throw new Error(`Mounting failed!`);
