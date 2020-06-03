427019b2ef44b89d36c701e1ca2355fe3e3358aa
FrankHassanabad <frank.hassanabad@elastic.co>
2020-06-03 13:49:57 -0600

    Added workaround for the UI sending down strings to get the server to build

--- x-pack/plugins/siem/common/detection_engine/schemas/types/risk_score.ts
+++ x-pack/plugins/siem/common/detection_engine/schemas/types/risk_score.ts
@@ -18,9 +18,14 @@ export const RiskScore: RiskScoreC = new t.Type<number, number, unknown>(
   'RiskScore',
   t.number.is,
   (input, context): Either<t.Errors, number> => {
-    return typeof input === 'number' && Number.isSafeInteger(input) && input >= 0 && input <= 100
-      ? t.success(input)
-      : t.failure(input, context);
+    // TODO: This is only for backwards compatibility with the UI. Once the UI is fixed to not send strings then this can be just a number again.
+    const converted = typeof input === 'string' ? parseInt(input, 10) : input;
+    return typeof converted === 'number' &&
+      Number.isSafeInteger(converted) &&
+      converted >= 0 &&
+      converted <= 100
+      ? t.success(converted)
+      : t.failure(converted, context);
   },
   t.identity
 );
