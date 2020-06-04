87d099f761ec9dbbb2c1c97c9ae6afc6013699d8
Gidi Meir Morris <github@gidi.io>
2020-06-04 20:10:26 +0100

    fixed security acceptance tests

--- x-pack/test/api_integration/apis/features/features/features.ts
+++ x-pack/test/api_integration/apis/features/features/features.ts
@@ -112,6 +112,7 @@ export default function ({ getService }: FtrProviderContext) {
             'uptime',
             'securitySolution',
             'ingestManager',
+            'alerts',
           ].sort()
         );
       });
