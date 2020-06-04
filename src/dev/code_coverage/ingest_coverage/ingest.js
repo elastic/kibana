63dd7ec9f480e2c8b059e02b562b73ebdd00d2b8
Tre' Seymour <wayne.seymour@elastic.co>
2020-06-04 14:10:41 -0600

    Only send to pipeline for data going to the coverage index, not the totals index. Also, add more tests.

--- src/dev/code_coverage/ingest_coverage/ingest.js
+++ src/dev/code_coverage/ingest_coverage/ingest.js
@@ -19,80 +19,34 @@
 
 const { Client } = require('@elastic/elasticsearch');
 import { createFailError } from '@kbn/dev-utils';
-import chalk from 'chalk';
-import { green, always, pretty } from './utils';
-import { fromNullable } from './either';
 import { COVERAGE_INDEX, TOTALS_INDEX } from './constants';
+import { logSuccess, errMsg, redact } from './ingest_helpers';
 
 const node = process.env.ES_HOST || 'http://localhost:9200';
 const redacted = redact(node);
 const client = new Client({ node });
 
-const indexName = (body) => (body.isTotal ? TOTALS_INDEX : COVERAGE_INDEX);
-
 export const ingest = (log) => async (body) => {
-  const index = indexName(body);
+  const index = body.isTotal ? TOTALS_INDEX : COVERAGE_INDEX;
+  const sendIndex = send(index);
 
-  if (process.env.NODE_ENV === 'integration_test') {
-    log.debug(`### Just Logging, ${green('NOT actually sending')} to [${green(index)}]`);
-    logSuccess(log, index, body);
-  } else {
-    try {
-      log.debug(`### Actually sending to: ${green(index)}`);
-      const pipeline = process.env.PIPELINE_NAME || 'team_assignment';
-      await client.index({ index, body, pipeline });
-      logSuccess(log, index, body);
-    } catch (e) {
-      throw createFailError(errMsg(index, body, e));
-    }
-  }
+  process.env.NODE_ENV === 'integration_test'
+    ? logSuccess(log, index, redacted, body)
+    : await sendIndex(log, body);
 };
-function logSuccess(log, index, body) {
-  const logShort = () => `### Sent:
-### ES HOST (redacted): ${redacted}
-### Index: ${green(index)}`;
 
-  logShort();
-  log.verbose(pretty(body));
-
-  const { staticSiteUrl } = body;
-
-  logShort();
-  log.debug(`### staticSiteUrl: ${staticSiteUrl}`);
-}
-function errMsg(index, body, e) {
-  const orig = fromNullable(e.body).fold(
-    always(''),
-    () => `### Orig Err:\n${pretty(e.body.error)}`
-  );
-
-  const red = color('red');
-
-  return `
-### ES HOST (redacted): \n\t${red(redacted)}
-### INDEX: \n\t${red(index)}
-### Partial orig err stack: \n\t${partial(e.stack)}
-### Item BODY:\n${pretty(body)}
-${orig}
-
-### Troubleshooting Hint:
-${red('Perhaps the coverage data was not merged properly?\n')}
-`;
-}
-
-function partial(x) {
-  return x.split('\n').splice(0, 2).join('\n');
-}
-function redact(x) {
-  const url = new URL(x);
-  if (url.password) {
-    return `${url.protocol}//${url.host}`;
-  } else {
-    return x;
-  }
-}
-function color(whichColor) {
-  return function colorInner(x) {
-    return chalk[whichColor].bgWhiteBright(x);
+function send(index) {
+  return async (log, body) => {
+    try {
+      await client.index(request(index, body));
+      logSuccess(log, index, redacted, body);
+    } catch (e) {
+      throw createFailError(errMsg(index, redacted, body, e));
+    }
   };
+}
+
+export function request(index, body) {
+  const pipeline = process.env.PIPELINE_NAME || 'team_assignment';
+  return index.includes('total') ? body : { ...body, pipeline };
 }
