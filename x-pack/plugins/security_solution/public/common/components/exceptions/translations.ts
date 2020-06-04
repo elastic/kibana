878547eb39e287b43ca15d69d42ff6ff25ee3485
Yara Tercero <yara.tercero@elastic.co>
2020-06-04 10:23:07 -0400

    updated translations to use securitySolution after rename

--- x-pack/plugins/security_solution/public/common/components/exceptions/translations.ts
+++ x-pack/plugins/security_solution/public/common/components/exceptions/translations.ts
@@ -5,42 +5,45 @@
  */
 import { i18n } from '@kbn/i18n';
 
-export const EDIT = i18n.translate('xpack.siem.exceptions.editButtonLabel', {
+export const EDIT = i18n.translate('xpack.securitySolution.exceptions.editButtonLabel', {
   defaultMessage: 'Edit',
 });
 
-export const REMOVE = i18n.translate('xpack.siem.exceptions.removeButtonLabel', {
+export const REMOVE = i18n.translate('xpack.securitySolution.exceptions.removeButtonLabel', {
   defaultMessage: 'Remove',
 });
 
 export const COMMENTS_SHOW = (comments: number) =>
-  i18n.translate('xpack.siem.exceptions.showCommentsLabel', {
+  i18n.translate('xpack.securitySolution.exceptions.showCommentsLabel', {
     values: { comments },
     defaultMessage: 'Show ({comments}) {comments, plural, =1 {Comment} other {Comments}}',
   });
 
 export const COMMENTS_HIDE = (comments: number) =>
-  i18n.translate('xpack.siem.exceptions.hideCommentsLabel', {
+  i18n.translate('xpack.securitySolution.exceptions.hideCommentsLabel', {
     values: { comments },
     defaultMessage: 'Hide ({comments}) {comments, plural, =1 {Comment} other {Comments}}',
   });
 
-export const DATE_CREATED = i18n.translate('xpack.siem.exceptions.dateCreatedLabel', {
+export const DATE_CREATED = i18n.translate('xpack.securitySolution.exceptions.dateCreatedLabel', {
   defaultMessage: 'Date created',
 });
 
-export const CREATED_BY = i18n.translate('xpack.siem.exceptions.createdByLabel', {
+export const CREATED_BY = i18n.translate('xpack.securitySolution.exceptions.createdByLabel', {
   defaultMessage: 'Created by',
 });
 
-export const COMMENT = i18n.translate('xpack.siem.exceptions.commentLabel', {
+export const COMMENT = i18n.translate('xpack.securitySolution.exceptions.commentLabel', {
   defaultMessage: 'Comment',
 });
 
-export const COMMENT_EVENT = i18n.translate('xpack.siem.exceptions.commentEventLabel', {
+export const COMMENT_EVENT = i18n.translate('xpack.securitySolution.exceptions.commentEventLabel', {
   defaultMessage: 'added a comment',
 });
 
-export const OPERATING_SYSTEM = i18n.translate('xpack.siem.exceptions.operatingSystemLabel', {
-  defaultMessage: 'OS',
-});
+export const OPERATING_SYSTEM = i18n.translate(
+  'xpack.securitySolution.exceptions.operatingSystemLabel',
+  {
+    defaultMessage: 'OS',
+  }
+);
