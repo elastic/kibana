f2443fe1ac8d9eb903af4a8700c33fc2441ce511
cauemarcondes <caue.marcondes@elastic.co>
2020-06-05 09:28:20 +0200

    addressing pr comments

--- x-pack/plugins/observability/public/pages/home/index.tsx
+++ x-pack/plugins/observability/public/pages/home/index.tsx
@@ -21,76 +21,7 @@ import { i18n } from '@kbn/i18n';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { usePluginContext } from '../../hooks/use_plugin_context';
-import { ISection, Section } from './section';
-
-const appsSection: ISection[] = [
-  {
-    id: 'logs',
-    title: i18n.translate('xpack.observability.section.apps.logs.title', {
-      defaultMessage: 'Logs',
-    }),
-    icon: 'logoLogging',
-    description: i18n.translate('xpack.observability.section.apps.logs.description', {
-      defaultMessage:
-        'The Elastic Stack (sometimes known as the ELK Stack) is the most popular open source logging platform.',
-    }),
-  },
-  {
-    id: 'apm',
-    title: i18n.translate('xpack.observability.section.apps.apm.title', {
-      defaultMessage: 'APM',
-    }),
-    icon: 'logoAPM',
-    description: i18n.translate('xpack.observability.section.apps.apm.description', {
-      defaultMessage:
-        'See exactly where your application is spending time so you can quickly fix issues and feel good about the code you push.',
-    }),
-  },
-  {
-    id: 'metrics',
-    title: i18n.translate('xpack.observability.section.apps.metrics.title', {
-      defaultMessage: 'Metrics',
-    }),
-    icon: 'logoMetrics',
-    description: i18n.translate('xpack.observability.section.apps.metrics.description', {
-      defaultMessage:
-        'Already using the Elastic Stack for logs? Add metrics in just a few steps and correlate metrics and logs in one place.',
-    }),
-  },
-  {
-    id: 'uptime',
-    title: i18n.translate('xpack.observability.section.apps.uptime.title', {
-      defaultMessage: 'Uptime',
-    }),
-    icon: 'logoUptime',
-    description: i18n.translate('xpack.observability.section.apps.uptime.description', {
-      defaultMessage:
-        'React to availability issues across your apps and services before they affect users.',
-    }),
-  },
-];
-
-const tryItOutItemsSection: ISection[] = [
-  {
-    id: 'demo',
-    title: i18n.translate('xpack.observability.section.tryItOut.demo.title', {
-      defaultMessage: 'Demo Playground',
-    }),
-    icon: 'play',
-    description: '',
-    href: 'https://demo.elastic.co/',
-    target: '_blank',
-  },
-  {
-    id: 'sampleData',
-    title: i18n.translate('xpack.observability.section.tryItOut.sampleData.title', {
-      defaultMessage: 'Add sample data',
-    }),
-    icon: 'documents',
-    description: '',
-    href: '/app/home#/tutorial_directory/sampleData',
-  },
-];
+import { appsSection, tryItOutItemsSection } from './section';
 
 const Container = styled.div`
   min-height: calc(100vh - 48px);
@@ -108,6 +39,12 @@ const Page = styled.div`
   margin: 0 auto;
   overflow: hidden;
 }
+`;
+
+const EuiCardWithoutBorder = styled(EuiCard)`
+  border: none;
+  box-shadow: none;
+  padding: 0;
 `;
 
 export const Home = () => {
@@ -178,7 +115,18 @@ export const Home = () => {
               <EuiFlexItem>
                 <EuiFlexGrid columns={2}>
                   {appsSection.map((app) => (
-                    <Section section={app} key={app.id} />
+                    <EuiFlexItem>
+                      <EuiCardWithoutBorder
+                        layout="horizontal"
+                        icon={<EuiIcon size="l" type={app.icon} />}
+                        title={
+                          <EuiTitle size="xs" className="title">
+                            <h3>{app.title}</h3>
+                          </EuiTitle>
+                        }
+                        description={app.description}
+                      />
+                    </EuiFlexItem>
                   ))}
                 </EuiFlexGrid>
               </EuiFlexItem>
