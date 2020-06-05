be519086d29acf458f7a9c98b7ecd1cfd12e725a
Alexey Antonov <alexwizp@gmail.com>
2020-06-05 14:37:46 +0300

    [uiSettings]: Migrate items owned by app arch to new platform (#66040) (#68354)
    * Migrated uiSettings items to new platform
    
    * API changes
    
    * Fixed translations
    
    * Fixed comment and i18n
    
    * Fixed tests
    
    * Fixed internalization
    
    * Fix karma tests
    
    * made code more explicit
    
    * Fixed plugin
    
    * Added consts for ui settings ids.
    
    * Added id for another settings
    
    * Fixed tests.
    
    * Improved imports
    
    * Fix imports to public which were happening from the server.
    
    * Fixed paths
    
    * Moved styles to plugin.ts
    
    * Moved styles
    
    * Fixed docs
    
    * Fix ci
    
    * Fix ci
    
    * fix documentation
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Alexey Antonov <alexwizp@gmail.com>
    Co-authored-by: Luke Elmers <luke.elmers@elastic.co>
    # Conflicts:
    #	src/legacy/core_plugins/kibana/server/ui_setting_defaults.js
    
    Co-authored-by: Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>

--- src/plugins/visualizations/common/constants.ts
+++ src/plugins/visualizations/common/constants.ts
@@ -17,16 +17,4 @@
  * under the License.
  */
 
-import { PluginInitializerContext } from '../../../core/server';
-import { VisualizationsPlugin } from './plugin';
-
-export { VISUALIZE_ENABLE_LABS_SETTING } from '../common/constants';
-
-//  This exports static code and TypeScript types,
-//  as well as, Kibana Platform `plugin()` initializer.
-
-export function plugin(initializerContext: PluginInitializerContext) {
-  return new VisualizationsPlugin(initializerContext);
-}
-
-export { VisualizationsPluginSetup, VisualizationsPluginStart } from './types';
+export const VISUALIZE_ENABLE_LABS_SETTING = 'visualize:enableLabs';
