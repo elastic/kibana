69ca0c655ce6e7e75f9ecddb393dfe43610b7024
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2020-06-08 18:57:36 +0200

    Extract log entry category setup into a flyout

--- x-pack/plugins/infra/public/pages/logs/log_entry_categories/page_setup_content.tsx
+++ x-pack/plugins/infra/public/pages/logs/log_entry_categories/page_setup_content.tsx
@@ -4,99 +4,58 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { EuiSpacer, EuiSteps, EuiText } from '@elastic/eui';
+import React, { useState, useCallback } from 'react';
 import { FormattedMessage } from '@kbn/i18n/react';
-import React, { useMemo } from 'react';
+import { EuiText, EuiButton, EuiSpacer } from '@elastic/eui';
 
-import { BetaBadge } from '../../../components/beta_badge';
 import {
-  createInitialConfigurationStep,
-  createProcessStep,
   LogAnalysisSetupPage,
   LogAnalysisSetupPageContent,
   LogAnalysisSetupPageHeader,
 } from '../../../components/logging/log_analysis_setup';
 import { useTrackPageview } from '../../../../../observability/public';
-import { useLogEntryCategoriesSetup } from './use_log_entry_categories_setup';
+
+import { LogEntryCategoriesSetupFlyout } from './setup_flyout';
 
 export const LogEntryCategoriesSetupContent: React.FunctionComponent = () => {
   useTrackPageview({ app: 'infra_logs', path: 'log_entry_categories_setup' });
   useTrackPageview({ app: 'infra_logs', path: 'log_entry_categories_setup', delay: 15000 });
 
-  const {
-    cleanUpAndSetUp,
-    endTime,
-    isValidating,
-    lastSetupErrorMessages,
-    setEndTime,
-    setStartTime,
-    setValidatedIndices,
-    setUp,
-    setupStatus,
-    startTime,
-    validatedIndices,
-    validationErrors,
-    viewResults,
-  } = useLogEntryCategoriesSetup();
-
-  const steps = useMemo(
-    () => [
-      createInitialConfigurationStep({
-        setStartTime,
-        setEndTime,
-        startTime,
-        endTime,
-        isValidating,
-        validatedIndices,
-        setupStatus,
-        setValidatedIndices,
-        validationErrors,
-      }),
-      createProcessStep({
-        cleanUpAndSetUp,
-        errorMessages: lastSetupErrorMessages,
-        isConfigurationValid: validationErrors.length <= 0 && !isValidating,
-        setUp,
-        setupStatus,
-        viewResults,
-      }),
-    ],
-    [
-      cleanUpAndSetUp,
-      endTime,
-      isValidating,
-      lastSetupErrorMessages,
-      setEndTime,
-      setStartTime,
-      setUp,
-      setValidatedIndices,
-      setupStatus,
-      startTime,
-      validatedIndices,
-      validationErrors,
-      viewResults,
-    ]
-  );
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const openFlyout = useCallback(() => setIsOpen(true), []);
+  const closeFlyout = useCallback(() => setIsOpen(false), []);
 
   return (
-    <LogAnalysisSetupPage data-test-subj="logEntryCategoriesSetupPage">
-      <LogAnalysisSetupPageHeader>
-        <FormattedMessage
-          id="xpack.infra.logs.logEntryCategories.setupTitle"
-          defaultMessage="Enable Machine Learning analysis"
-        />{' '}
-        <BetaBadge />
-      </LogAnalysisSetupPageHeader>
-      <LogAnalysisSetupPageContent>
-        <EuiText size="s">
-          <FormattedMessage
-            id="xpack.infra.logs.logEntryCategories.setupDescription"
-            defaultMessage="Use Machine Learning to automatically categorize log messages."
-          />
-        </EuiText>
-        <EuiSpacer />
-        <EuiSteps steps={steps} />
-      </LogAnalysisSetupPageContent>
-    </LogAnalysisSetupPage>
+    <>
+      <LogAnalysisSetupPage>
+        <LogAnalysisSetupPageHeader>
+          <h3>
+            <FormattedMessage
+              id="xpack.infra.logs.logEntryCategories.setupTitle"
+              defaultMessage="FIXME: Placeholder title"
+            />
+          </h3>
+        </LogAnalysisSetupPageHeader>
+        <LogAnalysisSetupPageContent>
+          <EuiText size="s">
+            <p>
+              <FormattedMessage
+                id="xpack.infra.logs.logEntryCategories.setupDescription"
+                defaultMessage="FIXME: Placeholder description"
+              />
+            </p>
+          </EuiText>
+          <EuiSpacer />
+          <EuiButton fill onClick={openFlyout}>
+            <FormattedMessage
+              id="xpack.infra.logs.logEntryCategories.setupCta"
+              defaultMessage="FIXME: Placeholder CTA"
+            />
+          </EuiButton>
+        </LogAnalysisSetupPageContent>
+      </LogAnalysisSetupPage>
+
+      <LogEntryCategoriesSetupFlyout isOpen={isOpen} onClose={closeFlyout} />
+    </>
   );
 };
