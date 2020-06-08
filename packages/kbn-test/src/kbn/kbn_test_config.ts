0112057aef399a599bd11de73516bf36eab94780
Mikhail Shustov <restrry@gmail.com>
2020-06-08 19:08:16 +0200

    convert kbn test config into TS

--- packages/kbn-test/src/kbn/kbn_test_config.ts
+++ packages/kbn-test/src/kbn/kbn_test_config.ts
@@ -16,26 +16,34 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-import { kibanaTestUser } from './users';
 import url from 'url';
+import { kibanaTestUser } from './users';
+
+interface UrlParts {
+  protocol: string;
+  hostname: string;
+  port: number;
+  auth: string;
+  username: string;
+  password: string;
+}
 
 export const kbnTestConfig = new (class KbnTestConfig {
   getPort() {
     return this.getUrlParts().port;
   }
 
-  getUrlParts() {
+  getUrlParts(): UrlParts {
     // allow setting one complete TEST_KIBANA_URL for ES like https://elastic:changeme@example.com:9200
     if (process.env.TEST_KIBANA_URL) {
       const testKibanaUrl = url.parse(process.env.TEST_KIBANA_URL);
       return {
-        protocol: testKibanaUrl.protocol.slice(0, -1),
-        hostname: testKibanaUrl.hostname,
-        port: parseInt(testKibanaUrl.port, 10),
-        auth: testKibanaUrl.auth,
-        username: testKibanaUrl.auth.split(':')[0],
-        password: testKibanaUrl.auth.split(':')[1],
+        protocol: testKibanaUrl.protocol!.slice(0, -1),
+        hostname: testKibanaUrl.hostname!,
+        port: parseInt(testKibanaUrl.port!, 10),
+        auth: testKibanaUrl.auth!,
+        username: testKibanaUrl.auth!.split(':')[0],
+        password: testKibanaUrl.auth!.split(':')[1],
       };
     }
 
@@ -44,7 +52,7 @@ export const kbnTestConfig = new (class KbnTestConfig {
     return {
       protocol: process.env.TEST_KIBANA_PROTOCOL || 'http',
       hostname: process.env.TEST_KIBANA_HOSTNAME || 'localhost',
-      port: parseInt(process.env.TEST_KIBANA_PORT, 10) || 5620,
+      port: parseInt(process.env.TEST_KIBANA_PORT!, 10) || 5620,
       auth: `${username}:${password}`,
       username,
       password,
