78d3e1c8c4a5164a71c19284445a735d184910c9
Andrea Del Rio <delrio.andre@gmail.com>
2020-06-08 16:13:12 -0700

    removed no_data

--- x-pack/plugins/monitoring/public/components/page_loading/index.js
+++ x-pack/plugins/monitoring/public/components/page_loading/index.js
@@ -14,6 +14,7 @@ import {
   EuiLoadingSpinner,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
+import './page_loading.scss';
 
 export function PageLoading() {
   return (
