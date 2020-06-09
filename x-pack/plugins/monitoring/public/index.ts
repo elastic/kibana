f4e70ec0e4b33e2224be7e496d8505ff578e3ea6
Andrea Del Rio <delrio.andre@gmail.com>
2020-06-09 13:34:00 -0700

    removed line from index.ts

--- x-pack/plugins/monitoring/public/index.ts
+++ x-pack/plugins/monitoring/public/index.ts
@@ -3,7 +3,6 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import './index.scss';
 
 import { PluginInitializerContext } from '../../../../src/core/public';
 import { MonitoringPlugin } from './plugin';
