8dd210997e90346d2a761a4236494c1b6a83ccc5
Sébastien Loix <sabee77@gmail.com>
2020-06-09 13:04:09 +0200

    [7.x] [Composable templates] Table to list templates (#67282) (#68324)
    Co-authored-by: Alison Goryachev <alisonmllr20@gmail.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: spalger <spalger@users.noreply.github.com>

--- x-pack/plugins/index_management/public/application/sections/home/template_list/components/index.ts
+++ x-pack/plugins/index_management/public/application/sections/home/template_list/components/index.ts
@@ -3,11 +3,6 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-export {
-  deserializeLegacyTemplateList,
-  deserializeTemplateList,
-  deserializeLegacyTemplate,
-  serializeLegacyTemplate,
-} from './template_serialization';
 
-export { getTemplateParameter } from './utils';
+export * from './filter_list_button';
+export * from './template_content_indicator';
--- x-pack/plugins/index_management/public/application/sections/home/template_list/legacy_templates/template_details/index.ts
+++ x-pack/plugins/index_management/public/application/sections/home/template_list/legacy_templates/template_details/index.ts
@@ -3,11 +3,5 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-export {
-  deserializeLegacyTemplateList,
-  deserializeTemplateList,
-  deserializeLegacyTemplate,
-  serializeLegacyTemplate,
-} from './template_serialization';
 
-export { getTemplateParameter } from './utils';
+export { LegacyTemplateDetails } from './template_details';
