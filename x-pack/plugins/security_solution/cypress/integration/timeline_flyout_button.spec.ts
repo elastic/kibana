b1f2a6e10fafbd433fb573ce2a2a07e4ad35b67b
MadameSheema <snootchie.boochies@gmail.com>
2020-06-10 15:14:57 +0200

    [SIEM] Cypress renaming (#68659) (#68751)
    * updates naming in integration
    
    * updates naming in objects
    
    * updates naming in screens
    
    * updates naming in support
    
    * updates naming in tasks
    
    * fixes failing test
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/cypress/integration/timeline_flyout_button.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/timeline_flyout_button.spec.ts
@@ -8,7 +8,7 @@ import { TIMELINE_FLYOUT_HEADER, TIMELINE_NOT_READY_TO_DROP_BUTTON } from '../sc
 
 import { dragFirstHostToTimeline, waitForAllHostsToBeLoaded } from '../tasks/hosts/all_hosts';
 import { loginAndWaitForPage } from '../tasks/login';
-import { openTimeline, openTimelineIfClosed } from '../tasks/siem_main';
+import { openTimeline, openTimelineIfClosed } from '../tasks/security_main';
 import { createNewTimeline } from '../tasks/timeline';
 
 import { HOSTS_PAGE } from '../urls/navigation';
