d59b7ddff8534e5b49aaee04c750fe50209e0020
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2020-06-11 11:32:42 -0400

    modify route for management

--- x-pack/plugins/security_solution/public/management/common/routing.ts
+++ x-pack/plugins/security_solution/public/management/common/routing.ts
@@ -6,104 +6,27 @@
 
 import { generatePath } from 'react-router-dom';
 // eslint-disable-next-line import/no-nodejs-modules
-import querystring from 'querystring';
+
 import {
   MANAGEMENT_ROUTING_ENDPOINTS_PATH,
   MANAGEMENT_ROUTING_POLICIES_PATH,
   MANAGEMENT_ROUTING_POLICY_DETAILS_PATH,
-  MANAGEMENT_ROUTING_ROOT_PATH,
 } from './constants';
 import { ManagementSubTab } from '../types';
-import { SecurityPageName } from '../../app/types';
-import { HostIndexUIQueryParams } from '../pages/endpoint_hosts/types';
+import { appendSearch } from '../../common/components/link_to/helpers';
 
-// Taken from: https://github.com/microsoft/TypeScript/issues/12936#issuecomment-559034150
-type ExactKeys<T1, T2> = Exclude<keyof T1, keyof T2> extends never ? T1 : never;
-type Exact<T, Shape> = T extends Shape ? ExactKeys<T, Shape> : never;
+export const getEndpointPath = (search?: string) =>
+  `/${generatePath(MANAGEMENT_ROUTING_ENDPOINTS_PATH, {
+    tabName: ManagementSubTab.endpoints,
+  })}${appendSearch(search ?? undefined)}`;
 
-/**
- * Returns a string to be used in the URL as search query params.
- * Ensures that when creating a URL query param string, that the given input strictly
- * matches the expected interface (guards against possibly leaking internal state)
- */
-const querystringStringify: <ExpectedType extends object, ArgType>(
-  params: Exact<ExpectedType, ArgType>
-) => string = querystring.stringify;
+export const getPoliciesPath = (search?: string) =>
+  `/${generatePath(MANAGEMENT_ROUTING_POLICIES_PATH, {
+    tabName: ManagementSubTab.policies,
+  })}${appendSearch(search ?? undefined)}`;
 
-/** Make `selected_host` required */
-type EndpointDetailsUrlProps = Omit<HostIndexUIQueryParams, 'selected_host'> &
-  Required<Pick<HostIndexUIQueryParams, 'selected_host'>>;
-
-/**
- * Input props for the `getManagementUrl()` method
- */
-export type GetManagementUrlProps = {
-  /**
-   * Exclude the URL prefix (everything to the left of where the router was mounted.
-   * This may be needed when interacting with react-router (ex. to do `history.push()` or
-   * validations against matched path)
-   */
-  excludePrefix?: boolean;
-} & (
-  | ({ name: 'default' | 'endpointList' } & HostIndexUIQueryParams)
-  | ({ name: 'endpointDetails' | 'endpointPolicyResponse' } & EndpointDetailsUrlProps)
-  | { name: 'policyList' }
-  | { name: 'policyDetails'; policyId: string }
-);
-
-// Prefix is (almost) everything to the left of where the Router was mounted. In SIEM, since
-// we're using Hash router, thats the `#`.
-const URL_PREFIX = '#';
-
-/**
- * Returns a URL string for a given Management page view
- * @param props
- */
-export const getManagementUrl = (props: GetManagementUrlProps): string => {
-  let url = props.excludePrefix ? '' : URL_PREFIX;
-
-  if (props.name === 'default' || props.name === 'endpointList') {
-    const { name, excludePrefix, ...queryParams } = props;
-    const urlQueryParams = querystringStringify<HostIndexUIQueryParams, typeof queryParams>(
-      queryParams
-    );
-
-    if (name === 'endpointList') {
-      url += generatePath(MANAGEMENT_ROUTING_ENDPOINTS_PATH, {
-        pageName: SecurityPageName.management,
-        tabName: ManagementSubTab.endpoints,
-      });
-    } else {
-      url += generatePath(MANAGEMENT_ROUTING_ROOT_PATH, {
-        pageName: SecurityPageName.management,
-      });
-    }
-
-    if (urlQueryParams) {
-      url += `?${urlQueryParams}`;
-    }
-  } else if (props.name === 'endpointDetails' || props.name === 'endpointPolicyResponse') {
-    const { name, excludePrefix, ...queryParams } = props;
-    queryParams.show = (props.name === 'endpointPolicyResponse'
-      ? 'policy_response'
-      : '') as HostIndexUIQueryParams['show'];
-
-    url += `${generatePath(MANAGEMENT_ROUTING_ENDPOINTS_PATH, {
-      pageName: SecurityPageName.management,
-      tabName: ManagementSubTab.endpoints,
-    })}?${querystringStringify<EndpointDetailsUrlProps, typeof queryParams>(queryParams)}`;
-  } else if (props.name === 'policyList') {
-    url += generatePath(MANAGEMENT_ROUTING_POLICIES_PATH, {
-      pageName: SecurityPageName.management,
-      tabName: ManagementSubTab.policies,
-    });
-  } else if (props.name === 'policyDetails') {
-    url += generatePath(MANAGEMENT_ROUTING_POLICY_DETAILS_PATH, {
-      pageName: SecurityPageName.management,
-      tabName: ManagementSubTab.policies,
-      policyId: props.policyId,
-    });
-  }
-
-  return url;
-};
+export const getPolicyDetailPath = (policyId: string, search?: string) =>
+  `/${generatePath(MANAGEMENT_ROUTING_POLICY_DETAILS_PATH, {
+    tabName: ManagementSubTab.policies,
+    policyId,
+  })}${appendSearch(search ?? undefined)}`;
--- x-pack/plugins/security_solution/public/management/components/management_page_view.tsx
+++ x-pack/plugins/security_solution/public/management/components/management_page_view.tsx
@@ -4,15 +4,36 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { memo, useMemo } from 'react';
+import React, { memo, useCallback, useMemo } from 'react';
 import { i18n } from '@kbn/i18n';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { PageView, PageViewProps } from '../../common/components/endpoint/page_view';
 import { ManagementSubTab } from '../types';
-import { getManagementUrl } from '..';
+import { SecurityPageName } from '../../app/types';
+import { useFormatUrl } from '../../common/components/link_to';
+import { getEndpointPath, getPoliciesPath } from '../common/routing';
 
 export const ManagementPageView = memo<Omit<PageViewProps, 'tabs'>>((options) => {
+  const history = useHistory();
+  const { formatUrl } = useFormatUrl(SecurityPageName.management);
   const { tabName } = useParams<{ tabName: ManagementSubTab }>();
+
+  const goToEndpoint = useCallback(
+    (ev) => {
+      ev.preventDefault();
+      history.push(getEndpointPath());
+    },
+    [history]
+  );
+
+  const goToPolicies = useCallback(
+    (ev) => {
+      ev.preventDefault();
+      history.push(getPoliciesPath());
+    },
+    [history]
+  );
+
   const tabs = useMemo((): PageViewProps['tabs'] | undefined => {
     if (options.viewType === 'details') {
       return undefined;
@@ -24,7 +45,8 @@ export const ManagementPageView = memo<Omit<PageViewProps, 'tabs'>>((options) =>
         }),
         id: ManagementSubTab.endpoints,
         isSelected: tabName === ManagementSubTab.endpoints,
-        href: getManagementUrl({ name: 'endpointList' }),
+        href: formatUrl(getEndpointPath()),
+        onClick: goToEndpoint,
       },
       {
         name: i18n.translate('xpack.securitySolution.managementTabs.policies', {
@@ -32,10 +54,11 @@ export const ManagementPageView = memo<Omit<PageViewProps, 'tabs'>>((options) =>
         }),
         id: ManagementSubTab.policies,
         isSelected: tabName === ManagementSubTab.policies,
-        href: getManagementUrl({ name: 'policyList' }),
+        href: formatUrl(getPoliciesPath()),
+        onClick: goToPolicies,
       },
     ];
-  }, [options.viewType, tabName]);
+  }, [formatUrl, goToEndpoint, goToPolicies, options.viewType, tabName]);
   return <PageView {...options} tabs={tabs} />;
 });
 
