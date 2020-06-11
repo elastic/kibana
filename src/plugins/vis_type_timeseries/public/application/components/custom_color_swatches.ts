d229d512a58f4f116f396d52088bedd9d0a44147
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2020-06-11 14:45:12 +0300

    Replace the Custom Color Picker on TSVB with the EuiColorPicker

---
+++ src/plugins/vis_type_timeseries/public/application/components/custom_color_swatches.ts
@@ -0,0 +1,57 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+export const COLOR_SWATCHES = [
+  '#0F1419',
+  '#333333',
+  '#4D4D4D',
+  '#666666',
+  '#808080',
+  '#999999',
+  '#B3B3B3',
+  '#CCCCCC',
+  '#FFFFFF',
+  '#9F0500',
+  '#D33115',
+  '#F44E3B',
+  '#C45100',
+  '#E27300',
+  '#FE9200',
+  '#FB9E00',
+  '#FCC400',
+  '#FCDC00',
+  '#808900',
+  '#B0BC00',
+  '#DBDF00',
+  '#194D33',
+  '#68BC00',
+  '#A4DD00',
+  '#0C797D',
+  '#16A5A5',
+  '#68CCCA',
+  '#0062B1',
+  '#009CE0',
+  '#73D8FF',
+  '#653294',
+  '#7B64FF',
+  '#AEA1FF',
+  '#AB149E',
+  '#FA28FF',
+  '#FDA1FF',
+];
