4546f1af1412320319a624479f66f59dc6242d18
Shahzad <shahzad.muhammad@elastic.co>
2020-06-13 00:34:05 +0200

    [Uptime] Fixed metric query broken because of missing mapping (#68999)
    Co-authored-by: Andrew Cholakian <andrew@andrewvc.com>

--- x-pack/plugins/uptime/server/lib/requests/get_latest_monitor.ts
+++ x-pack/plugins/uptime/server/lib/requests/get_latest_monitor.ts
@@ -50,8 +50,8 @@ export const getLatestMonitor: UMElasticsearchQueryFn<GetLatestMonitorParams, Pi
         'monitor',
         'observer',
         '@timestamp',
-        'tls.server.x509.not_after',
-        'tls.server.x509.not_before',
+        'tls.certificate_not_valid_after',
+        'tls.certificate_not_valid_before',
       ],
       sort: {
         '@timestamp': { order: 'desc' },
@@ -68,6 +68,9 @@ export const getLatestMonitor: UMElasticsearchQueryFn<GetLatestMonitorParams, Pi
     ...ping,
     docId,
     timestamp: ping['@timestamp'],
-    tls: { not_after: tls?.server?.x509?.not_after, not_before: tls?.server?.x509?.not_before },
+    tls: {
+      not_after: tls?.certificate_not_valid_after,
+      not_before: tls?.certificate_not_valid_before,
+    },
   };
 };
