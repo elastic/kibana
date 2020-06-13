902a7b6254487fa1898ab5358fc2db09d3f19981
Luke Elmers <luke.elmers@elastic.co>
2020-06-13 13:06:57 -0600

    Add format to serialized agg config.

--- src/plugins/data/public/search/aggs/agg_config.ts
+++ src/plugins/data/public/search/aggs/agg_config.ts
@@ -47,6 +47,10 @@ export type AggConfigSerialized = Ensure<
     enabled?: boolean;
     id?: string;
     params?: {} | SerializableState;
+<<<<<<< HEAD
+=======
+    format?: {} | Ensure<SerializedFieldFormat<SerializableState>, SerializableState>;
+>>>>>>> Add format to serialized agg config.
     schema?: string;
   },
   SerializableState
@@ -303,6 +307,10 @@ export class AggConfig {
       enabled: this.enabled,
       type: this.type && this.type.name,
       params: outParams as SerializableState,
+<<<<<<< HEAD
+=======
+      format: {},
+>>>>>>> Add format to serialized agg config.
       ...(this.schema && { schema: this.schema }),
     };
   }
