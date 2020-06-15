e47f3c24523e588468dbaf760613d910ecd6ebf7
Patryk Kopycinski <contact@patrykkopycinski.com>
2020-06-15 13:17:06 +0200

    cleanup

--- x-pack/plugins/security_solution/cypress/integration/cases.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/cases.spec.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { case1 } from '../objects';
+import { case1 } from '../objects/case';
 
 import {
   ALL_CASES_CLOSE_ACTION,
--- x-pack/plugins/security_solution/cypress/integration/cases_connectors.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/cases_connectors.spec.ts
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { serviceNowConnector } from '../objects';
+import { serviceNowConnector } from '../objects/case';
 
 import { TOASTER } from '../screens/configure_cases';
 
