ac37d1b8cd7f2d5c199dc125ae29090f5a29dc79
Gidi Meir Morris <github@gidi.io>
2020-06-15 16:39:41 +0100

    change feature in privileges bac kto alerts

--- x-pack/test/api_integration/apis/security/privileges.ts
+++ x-pack/test/api_integration/apis/security/privileges.ts
@@ -37,7 +37,7 @@ export default function ({ getService }: FtrProviderContext) {
             apm: ['all', 'read'],
             siem: ['all', 'read'],
             ingestManager: ['all', 'read'],
-            alerts: ['all', 'read'],
+            alerting: ['all', 'read'],
           },
           global: ['all', 'read'],
           space: ['all', 'read'],
