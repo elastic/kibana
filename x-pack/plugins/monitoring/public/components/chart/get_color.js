44fe753953f28fe3859208ecdc37cc122e85c566
chrisronline <chrisronline@gmail.com>
2020-06-15 11:23:48 -0400

    Use EUI color palette

--- x-pack/plugins/monitoring/public/components/chart/get_color.js
+++ x-pack/plugins/monitoring/public/components/chart/get_color.js
@@ -13,27 +13,14 @@
  * @param {Integer} index: index of the chart series, 0-3
  * @returns {String} Hex color to use for chart series at the given index
  */
+import { euiPaletteColorBlind } from '@elastic/eui/lib/services';
+
 export function getColor(app, index) {
   let seriesColors;
   if (app === 'elasticsearch') {
     seriesColors = ['#3ebeb0', '#3b73ac', '#f08656', '#6c478f'];
   } else if (app === 'apm') {
-    // From https://github.com/elastic/elastic-charts/blob/master/src/utils/themes/colors.ts
-    seriesColors = [
-      '#1EA593',
-      '#2B70F7',
-      '#CE0060',
-      '#38007E',
-      '#FCA5D3',
-      '#F37020',
-      '#E49E29',
-      '#B0916F',
-      '#7B000B',
-      '#34130C',
-      '#A5E26A',
-      '#D2E26A',
-      '#EBDF61',
-    ];
+    seriesColors = euiPaletteColorBlind();
   } else {
     // for kibana, and fallback (e.g., Logstash and Beats)
     seriesColors = ['#e8488b', '#3b73ac', '#3cab63', '#6c478f'];
