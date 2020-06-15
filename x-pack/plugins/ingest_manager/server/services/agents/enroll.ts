bb489d0df001dd8424bc5f03411ad268102cb04e
John Schulz <john.schulz@elastic.co>
2020-06-15 11:16:36 -0400

    Remove 'refresh: false' from agent enroll
    Does this this fix the failing test? It does keep the PR more focused

--- x-pack/plugins/ingest_manager/server/services/agents/enroll.ts
+++ x-pack/plugins/ingest_manager/server/services/agents/enroll.ts
@@ -62,14 +62,9 @@ export async function enroll(
 
   const accessAPIKey = await APIKeyService.generateAccessApiKey(soClient, agent.id, configId);
 
-  await soClient.update<AgentSOAttributes>(
-    AGENT_SAVED_OBJECT_TYPE,
-    agent.id,
-    {
-      access_api_key_id: accessAPIKey.id,
-    },
-    { refresh: false }
-  );
+  await soClient.update<AgentSOAttributes>(AGENT_SAVED_OBJECT_TYPE, agent.id, {
+    access_api_key_id: accessAPIKey.id,
+  });
 
   return { ...agent, access_api_key: accessAPIKey.key };
 }
