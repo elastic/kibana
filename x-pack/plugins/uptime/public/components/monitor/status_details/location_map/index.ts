53d4af31ac5e8d9ec60ccd6f33d4fb7ab183f99a
Shahzad <shahzad.muhammad@elastic.co>
2020-06-16 18:04:52 +0200

    [Uptime] Monitor availability reporting (#67790)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/uptime/public/components/monitor/status_details/location_map/index.ts
+++ x-pack/plugins/uptime/public/components/monitor/status_details/location_map/index.ts
@@ -4,9 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from './ml';
-export * from './ping_list';
-export * from './status_details/location_map';
-export * from './status_details';
-export * from './ping_histogram';
-export * from './monitor_charts';
+export * from './location_map';
+export * from '../availability_reporting/location_status_tags';
