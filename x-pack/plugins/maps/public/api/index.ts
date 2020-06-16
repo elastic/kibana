1c9d3898e406407053d857e353e1835b63c0cda3
Nathan Reese <reese.nathan@gmail.com>
2020-06-16 10:23:15 -0600

    [Maps] Security layer wizards (#68290) (#69268)
    * [Maps] security layer wizard
    
    * index pattern select
    
    * use IndexPattern
    
    * destination layer
    
    * source layer
    
    * cache security index patterns
    
    * line layer
    
    * use top hits
    
    * tslint
    
    * unit test
    
    * fix loading speed and i18n
    
    * handle case where ui setting siem:defaultIndex does not exist
    
    * tslint
    
    * review feedback
    
    * extract lazy load bundle from map embeddables
    
    * expose createSecurityLayers via plugin start
    
    * tslint
    
    * export MapsPluginStart
    
    * fix path move
    
    * review feedback
    
    * fix import
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/maps/public/api/index.ts
+++ x-pack/plugins/maps/public/api/index.ts
@@ -4,5 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { ObservabilityLayerWizardConfig } from './observability_layer_wizard';
-export { APM_INDEX_PATTERN_TITLE } from './create_layer_descriptor';
+export { MapsStartApi } from './start_api';
