4fd23d500fb9d049343335258bc2309d4ccbdabe
Constance Chen <constance.chen.3@gmail.com>
2020-06-16 14:40:17 -0700

    Feedback: Flatten nested CSS where possible
    - Prefer setting CSS class overrides on individual EUI components, not on a top-level page
    
    + Change CSS class casing from kebab-case to camelCase to better match EUI/Kibana
    
    + Remove unnecessary .euiPageContentHeader margin-bottom override by changing the panelPaddingSize of euiPageContent
    
    + Decrease engine overview table padding on mobile

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/setup_guide/setup_guide.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/setup_guide/setup_guide.tsx
@@ -33,11 +33,11 @@ import './setup_guide.scss';
 
 export const SetupGuide: React.FC = () => {
   return (
-    <EuiPage className="setup-guide">
+    <EuiPage className="setupGuide">
       <SetBreadcrumbs text="Setup Guide" />
       <SendTelemetry action="viewed" metric="setup_guide" />
 
-      <EuiPageSideBar>
+      <EuiPageSideBar className="setupGuide__sidebar">
         <EuiText color="subdued" size="s">
           <strong>
             <FormattedMessage
@@ -70,7 +70,7 @@ export const SetupGuide: React.FC = () => {
           rel="noopener noreferrer"
         >
           <img
-            className="setup-guide__thumbnail"
+            className="setupGuide__thumbnail"
             src={GettingStarted}
             alt={i18n.translate('xpack.enterpriseSearch.setupGuide.videoAlt', {
               defaultMessage:
@@ -100,7 +100,7 @@ export const SetupGuide: React.FC = () => {
         </EuiText>
       </EuiPageSideBar>
 
-      <EuiPageBody>
+      <EuiPageBody className="setupGuide__body">
         <EuiPageContent>
           <EuiSteps
             headingElement="h2"
