1b111bc84bd292d45fe13ab965c2f91bca7eba96
Nathan Reese <reese.nathan@gmail.com>
2020-06-16 11:41:49 -0600

    [Maps] layer wizard select re-design

--- x-pack/plugins/maps/public/connected_components/add_layer_panel/flyout_body/layer_wizard_select.tsx
+++ x-pack/plugins/maps/public/connected_components/add_layer_panel/flyout_body/layer_wizard_select.tsx
@@ -5,26 +5,62 @@
  */
 
 import _ from 'lodash';
-import React, { Component, Fragment } from 'react';
-import { EuiSpacer, EuiCard, EuiIcon } from '@elastic/eui';
-import { EuiLoadingContent } from '@elastic/eui';
+import React, { Component } from 'react';
+import {
+  EuiCard,
+  EuiIcon,
+  EuiFlexGrid,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiLoadingContent,
+  EuiButtonEmpty,
+} from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { i18n } from '@kbn/i18n';
 import { getLayerWizards, LayerWizard } from '../../../classes/layers/layer_wizard_registry';
+import { LAYER_WIZARD_CATEGORY } from '../../../../common/constants';
 
 interface Props {
   onSelect: (layerWizard: LayerWizard) => void;
 }
 
 interface State {
-  layerWizards: LayerWizard[];
+  activeCategories: LAYER_WIZARD_CATEGORY[];
   hasLoadedWizards: boolean;
+  layerWizards: LayerWizard[];
+  selectedCategory?: LAYER_WIZARD_CATEGORY;
+}
+
+function getCategoryLabel(category: LAYER_WIZARD_CATEGORY): string {
+  if (category === LAYER_WIZARD_CATEGORY.ELASTICSEARCH) {
+    return i18n.translate('xpack.maps.layerWizardSelect.elasticsearchCategoryLabel', {
+      defaultMessage: 'Elasticsearch',
+    });
+  }
+
+  if (category === LAYER_WIZARD_CATEGORY.REFERENCE) {
+    return i18n.translate('xpack.maps.layerWizardSelect.referenceCategoryLabel', {
+      defaultMessage: 'Reference',
+    });
+  }
+
+  if (category === LAYER_WIZARD_CATEGORY.SOLUTIONS) {
+    return i18n.translate('xpack.maps.layerWizardSelect.solutionsCategoryLabel', {
+      defaultMessage: 'Solutions',
+    });
+  }
+
+  return category as string;
 }
 
 export class LayerWizardSelect extends Component<Props, State> {
   private _isMounted: boolean = false;
 
   state = {
-    layerWizards: [],
+    activeCategories: [],
     hasLoadedWizards: false,
+    layerWizards: [],
+    selectedCategory: undefined,
   };
 
   componentDidMount() {
@@ -38,9 +74,56 @@ export class LayerWizardSelect extends Component<Props, State> {
 
   async _loadLayerWizards() {
     const layerWizards = await getLayerWizards();
+    const activeCategories: LAYER_WIZARD_CATEGORY[] = [];
+    layerWizards.forEach((layerWizard: LayerWizard) => {
+      layerWizard.categories.forEach((category: LAYER_WIZARD_CATEGORY) => {
+        if (!activeCategories.includes(category)) {
+          activeCategories.push(category);
+        }
+      });
+    });
+
     if (this._isMounted) {
-      this.setState({ layerWizards, hasLoadedWizards: true });
+      this.setState({
+        activeCategories,
+        layerWizards,
+        hasLoadedWizards: true,
+      });
     }
+  }
+
+  _filterByCategory(category?: LAYER_WIZARD_CATEGORY) {
+    this.setState({ selectedCategory: category });
+  }
+
+  _renderCategoryButtons() {
+    if (this.state.activeCategories.length === 0) {
+      return null;
+    }
+
+    const categoryButtons = this.state.activeCategories.map((category: LAYER_WIZARD_CATEGORY) => {
+      return (
+        <EuiFlexItem grow={false} key={category}>
+          <EuiButtonEmpty color="text" onClick={() => this._filterByCategory(category)} size="xs">
+            {getCategoryLabel(category)}
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+      );
+    });
+
+    return (
+      <EuiFlexGroup gutterSize="s" alignItems="center">
+        <EuiFlexItem grow={false}>
+          <EuiButtonEmpty color="text" onClick={() => this._filterByCategory(undefined)} size="xs">
+            <FormattedMessage
+              id="xpack.maps.layerWizardSelect.allCategories"
+              defaultMessage="All"
+            />
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+        {categoryButtons}
+      </EuiFlexGroup>
+    );
   }
 
   render() {
@@ -51,27 +134,38 @@ export class LayerWizardSelect extends Component<Props, State> {
         </div>
       );
     }
-    return this.state.layerWizards.map((layerWizard: LayerWizard) => {
-      const icon = layerWizard.icon ? <EuiIcon type={layerWizard.icon} size="l" /> : undefined;
 
-      const onClick = () => {
-        this.props.onSelect(layerWizard);
-      };
+    const wizardCards = this.state.layerWizards
+      .filter((layerWizard: LayerWizard) => {
+        return this.state.selectedCategory
+          ? layerWizard.categories.includes(this.state.selectedCategory)
+          : true;
+      })
+      .map((layerWizard: LayerWizard) => {
+        const icon = layerWizard.icon ? <EuiIcon type={layerWizard.icon} size="l" /> : undefined;
 
-      return (
-        <Fragment key={layerWizard.title}>
-          <EuiSpacer size="s" />
-          <EuiCard
-            className="mapLayerAddpanel__card"
-            title={layerWizard.title}
-            icon={icon}
-            onClick={onClick}
-            description={layerWizard.description}
-            layout="horizontal"
-            data-test-subj={_.camelCase(layerWizard.title)}
-          />
-        </Fragment>
-      );
-    });
+        const onClick = () => {
+          this.props.onSelect(layerWizard);
+        };
+
+        return (
+          <EuiFlexItem key={layerWizard.title}>
+            <EuiCard
+              title={layerWizard.title}
+              icon={icon}
+              onClick={onClick}
+              description={layerWizard.description}
+              data-test-subj={_.camelCase(layerWizard.title)}
+            />
+          </EuiFlexItem>
+        );
+      });
+
+    return (
+      <>
+        {this._renderCategoryButtons()}
+        <EuiFlexGrid columns={2}>{wizardCards}</EuiFlexGrid>
+      </>
+    );
   }
 }
