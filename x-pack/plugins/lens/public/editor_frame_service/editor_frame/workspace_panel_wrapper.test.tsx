e983014fa739d20d322148ce47f1a83924afd1bd
Joe Reuter <johannes.reuter@elastic.co>
2020-06-16 15:24:36 +0200

    fix teests

--- x-pack/plugins/lens/public/editor_frame_service/editor_frame/workspace_panel_wrapper.test.tsx
+++ x-pack/plugins/lens/public/editor_frame_service/editor_frame/workspace_panel_wrapper.test.tsx
@@ -5,34 +5,10 @@
  */
 
 import React from 'react';
-import { act } from 'react-dom/test-utils';
-import { ReactExpressionRendererProps } from '../../../../../../src/plugins/expressions/public';
-import { FramePublicAPI, TableSuggestion, Visualization } from '../../types';
-import {
-  createMockVisualization,
-  createMockDatasource,
-  createExpressionRendererMock,
-  DatasourceMock,
-  createMockFramePublicAPI,
-  FrameMock,
-} from '../mocks';
-import { InnerWorkspacePanel, WorkspacePanelProps } from './workspace_panel';
+import { Visualization } from '../../types';
+import { createMockVisualization, createMockFramePublicAPI, FrameMock } from '../mocks';
 import { mountWithIntl as mount } from 'test_utils/enzyme_helpers';
 import { ReactWrapper } from 'enzyme';
-import { DragDrop, ChildDragDropProvider } from '../../drag_drop';
-import { Ast } from '@kbn/interpreter/common';
-import { coreMock } from 'src/core/public/mocks';
-import {
-  DataPublicPluginStart,
-  esFilters,
-  IFieldType,
-  IIndexPattern,
-} from '../../../../../../src/plugins/data/public';
-import { TriggerId, UiActionsStart } from '../../../../../../src/plugins/ui_actions/public';
-import { uiActionsPluginMock } from '../../../../../../src/plugins/ui_actions/public/mocks';
-import { TriggerContract } from '../../../../../../src/plugins/ui_actions/public/triggers';
-import { VIS_EVENT_TO_TRIGGER } from '../../../../../../src/plugins/visualizations/public/embeddable';
-import { dataPluginMock } from '../../../../../../src/plugins/data/public/mocks';
 import { WorkspacePanelWrapper, WorkspacePanelWrapperProps } from './workspace_panel_wrapper';
 
 describe('workspace_panel_wrapper', () => {
@@ -65,7 +41,7 @@ describe('workspace_panel_wrapper', () => {
     expect(instance.find(MyChild)).toHaveLength(1);
   });
 
-  it('should call the tooltip renderer if provided', () => {
+  it('should call the toolbar renderer if provided', () => {
     const renderToolbarMock = jest.fn();
     const visState = { internalState: 123 };
     instance = mount(
