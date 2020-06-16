e6934666b811cf6d60fbfc541ca794b5538c78e6
Tim Sullivan <tsullivan@users.noreply.github.com>
2020-06-16 14:24:35 -0700

    [Reporting] Prepare export type definitions for Task Manager (#65213)
    # Conflicts:
    #	x-pack/plugins/reporting/server/export_types/common/execute_job/get_full_urls.ts
    #	x-pack/plugins/reporting/server/export_types/printable_pdf/server/create_job/index.ts
    #	x-pack/plugins/reporting/server/export_types/printable_pdf/server/execute_job/index.test.ts

--- x-pack/plugins/reporting/server/export_types/printable_pdf/server/create_job/index.ts
+++ x-pack/plugins/reporting/server/export_types/printable_pdf/server/create_job/index.ts
@@ -7,12 +7,12 @@
 import { KibanaRequest, RequestHandlerContext } from 'src/core/server';
 import { validateUrls } from '../../../../../common/validate_urls';
 import { cryptoFactory } from '../../../../lib';
-import { CreateJobFactory, ESQueueCreateJobFn } from '../../../../types';
+import { ESQueueCreateJobFn, ScheduleTaskFnFactory } from '../../../../types';
 import { JobParamsPDF } from '../../types';
 // @ts-ignore no module def (deprecated module)
 import { compatibilityShimFactory } from './compatibility_shim';
 
-export const createJobFactory: CreateJobFactory<ESQueueCreateJobFn<
+export const scheduleTaskFnFactory: ScheduleTaskFnFactory<ESQueueCreateJobFn<
   JobParamsPDF
 >> = function createJobFactoryFn(reporting, logger) {
   const config = reporting.getConfig();
@@ -21,7 +21,7 @@ export const createJobFactory: CreateJobFactory<ESQueueCreateJobFn<
   const compatibilityShim = compatibilityShimFactory(logger);
 
   return compatibilityShim(async function createJobFn(
-    { title, relativeUrls, browserTimezone, layout, objectType }: JobParamsPDF,
+    { title, relativeUrls, browserTimezone, layout, objectType }: ScheduledTaskParamsPDF,
     context: RequestHandlerContext,
     req: KibanaRequest
   ) {
