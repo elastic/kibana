4fbe5a1de75906a727f43b7e5bae715060158496
Oliver Gupte <olivergupte@gmail.com>
2020-06-15 17:05:58 -0700

    fixes some rendering issues in service maps

--- x-pack/plugins/apm/public/components/app/ServiceMap/cytoscapeOptions.ts
+++ x-pack/plugins/apm/public/components/app/ServiceMap/cytoscapeOptions.ts
@@ -36,7 +36,9 @@ const getBorderColorFn = (
     const hasAnomalyDetectionJob = el.data('ml_job_id') !== undefined;
     const nodeSeverity = el.data('anomaly_severity');
     if (hasAnomalyDetectionJob) {
-      return getSeverityColor(nodeSeverity) || theme.eui.euiColorMediumShade;
+      return (
+        getSeverityColor(theme, nodeSeverity) || theme.eui.euiColorMediumShade
+      );
     }
     if (el.hasClass('primary') || el.selected()) {
       return theme.eui.euiColorPrimary;
@@ -88,7 +90,6 @@ export const getAnimationOptions = (
   easing: theme.eui.euiAnimSlightBounce,
 });
 
-const lineColor = '#C5CCD7';
 const zIndexNode = 200;
 const zIndexEdge = 100;
 const zIndexEdgeHighlight = 110;
@@ -101,131 +102,134 @@ function isService(el: cytoscape.NodeSingular) {
   return el.data(SERVICE_NAME) !== undefined;
 }
 
-const getStyle = (theme: EuiTheme): cytoscape.Stylesheet[] => [
-  {
-    selector: 'node',
-    style: {
-      'background-color': theme.eui.euiColorEmptyShade,
-      // The DefinitelyTyped definitions don't specify that a function can be
-      // used here.
-      //
-      // @ts-ignore
-      'background-image': isIE11
-        ? undefined
-        : (el: cytoscape.NodeSingular) => iconForNode(el) ?? defaultIcon,
-      'background-height': (el: cytoscape.NodeSingular) =>
-        isService(el) ? '60%' : '40%',
-      'background-width': (el: cytoscape.NodeSingular) =>
-        isService(el) ? '60%' : '40%',
-      'border-color': getBorderColorFn(theme),
-      'border-style': getBorderStyle,
-      'border-width': getBorderWidth,
-      color: (el: cytoscape.NodeSingular) =>
-        el.hasClass('primary') || el.selected()
-          ? theme.eui.euiColorPrimaryText
-          : theme.eui.textColors.text,
-      // theme.euiFontFamily doesn't work here for some reason, so we're just
-      // specifying a subset of the fonts for the label text.
-      'font-family': 'Inter UI, Segoe UI, Helvetica, Arial, sans-serif',
-      'font-size': theme.eui.euiFontSizeS,
-      ghost: 'yes',
-      'ghost-offset-x': 0,
-      'ghost-offset-y': 2,
-      'ghost-opacity': 0.15,
-      height: getNodeHeight(theme),
-      label: (el: cytoscape.NodeSingular) =>
-        isService(el)
-          ? el.data(SERVICE_NAME)
-          : el.data(SPAN_DESTINATION_SERVICE_RESOURCE),
-      'min-zoomed-font-size': parseInt(theme.eui.euiSizeS, 10),
-      'overlay-opacity': 0,
-      shape: (el: cytoscape.NodeSingular) =>
-        isService(el) ? (isIE11 ? 'rectangle' : 'ellipse') : 'diamond',
-      'text-background-color': theme.eui.euiColorPrimary,
-      'text-background-opacity': (el: cytoscape.NodeSingular) =>
-        el.hasClass('primary') || el.selected() ? 0.1 : 0,
-      'text-background-padding': theme.eui.paddingSizes.xs,
-      'text-background-shape': 'roundrectangle',
-      'text-margin-y': parseInt(theme.eui.paddingSizes.s, 10),
-      'text-max-width': '200px',
-      'text-valign': 'bottom',
-      'text-wrap': 'ellipsis',
-      width: theme.eui.avatarSizing.l.size,
-      'z-index': zIndexNode,
+const getStyle = (theme: EuiTheme): cytoscape.Stylesheet[] => {
+  const lineColor = theme.eui.euiColorMediumShade;
+  return [
+    {
+      selector: 'node',
+      style: {
+        'background-color': theme.eui.euiColorGhost,
+        // The DefinitelyTyped definitions don't specify that a function can be
+        // used here.
+        //
+        // @ts-ignore
+        'background-image': isIE11
+          ? undefined
+          : (el: cytoscape.NodeSingular) => iconForNode(el) ?? defaultIcon,
+        'background-height': (el: cytoscape.NodeSingular) =>
+          isService(el) ? '60%' : '40%',
+        'background-width': (el: cytoscape.NodeSingular) =>
+          isService(el) ? '60%' : '40%',
+        'border-color': getBorderColorFn(theme),
+        'border-style': getBorderStyle,
+        'border-width': getBorderWidth,
+        color: (el: cytoscape.NodeSingular) =>
+          el.hasClass('primary') || el.selected()
+            ? theme.eui.euiColorPrimaryText
+            : theme.eui.textColors.text,
+        // theme.euiFontFamily doesn't work here for some reason, so we're just
+        // specifying a subset of the fonts for the label text.
+        'font-family': 'Inter UI, Segoe UI, Helvetica, Arial, sans-serif',
+        'font-size': theme.eui.euiFontSizeS,
+        ghost: 'yes',
+        'ghost-offset-x': 0,
+        'ghost-offset-y': 2,
+        'ghost-opacity': 0.15,
+        height: getNodeHeight(theme),
+        label: (el: cytoscape.NodeSingular) =>
+          isService(el)
+            ? el.data(SERVICE_NAME)
+            : el.data(SPAN_DESTINATION_SERVICE_RESOURCE),
+        'min-zoomed-font-size': parseInt(theme.eui.euiSizeS, 10),
+        'overlay-opacity': 0,
+        shape: (el: cytoscape.NodeSingular) =>
+          isService(el) ? (isIE11 ? 'rectangle' : 'ellipse') : 'diamond',
+        'text-background-color': theme.eui.euiColorPrimary,
+        'text-background-opacity': (el: cytoscape.NodeSingular) =>
+          el.hasClass('primary') || el.selected() ? 0.1 : 0,
+        'text-background-padding': theme.eui.paddingSizes.xs,
+        'text-background-shape': 'roundrectangle',
+        'text-margin-y': parseInt(theme.eui.paddingSizes.s, 10),
+        'text-max-width': '200px',
+        'text-valign': 'bottom',
+        'text-wrap': 'ellipsis',
+        width: theme.eui.avatarSizing.l.size,
+        'z-index': zIndexNode,
+      },
     },
-  },
-  {
-    selector: 'edge',
-    style: {
-      'curve-style': 'taxi',
-      // @ts-ignore
-      'taxi-direction': 'auto',
-      'line-color': lineColor,
-      'overlay-opacity': 0,
-      'target-arrow-color': lineColor,
-      'target-arrow-shape': isIE11 ? 'none' : 'triangle',
-      // The DefinitelyTyped definitions don't specify this property since it's
-      // fairly new.
-      //
-      // @ts-ignore
-      'target-distance-from-node': isIE11
-        ? undefined
-        : theme.eui.paddingSizes.xs,
-      width: 1,
-      'source-arrow-shape': 'none',
-      'z-index': zIndexEdge,
+    {
+      selector: 'edge',
+      style: {
+        'curve-style': 'taxi',
+        // @ts-ignore
+        'taxi-direction': 'auto',
+        'line-color': lineColor,
+        'overlay-opacity': 0,
+        'target-arrow-color': lineColor,
+        'target-arrow-shape': isIE11 ? 'none' : 'triangle',
+        // The DefinitelyTyped definitions don't specify this property since it's
+        // fairly new.
+        //
+        // @ts-ignore
+        'target-distance-from-node': isIE11
+          ? undefined
+          : theme.eui.paddingSizes.xs,
+        width: 1,
+        'source-arrow-shape': 'none',
+        'z-index': zIndexEdge,
+      },
     },
-  },
-  {
-    selector: 'edge[bidirectional]',
-    style: {
-      'source-arrow-shape': isIE11 ? 'none' : 'triangle',
-      'source-arrow-color': lineColor,
-      'target-arrow-shape': isIE11 ? 'none' : 'triangle',
-      // @ts-ignore
-      'source-distance-from-node': isIE11
-        ? undefined
-        : parseInt(theme.eui.paddingSizes.xs, 10),
-      'target-distance-from-node': isIE11
-        ? undefined
-        : parseInt(theme.eui.paddingSizes.xs, 10),
+    {
+      selector: 'edge[bidirectional]',
+      style: {
+        'source-arrow-shape': isIE11 ? 'none' : 'triangle',
+        'source-arrow-color': lineColor,
+        'target-arrow-shape': isIE11 ? 'none' : 'triangle',
+        // @ts-ignore
+        'source-distance-from-node': isIE11
+          ? undefined
+          : parseInt(theme.eui.paddingSizes.xs, 10),
+        'target-distance-from-node': isIE11
+          ? undefined
+          : parseInt(theme.eui.paddingSizes.xs, 10),
+      },
     },
-  },
-  {
-    selector: 'edge[isInverseEdge]',
-    // @ts-ignore DefinitelyTyped says visibility is "none" but it's
-    // actually "hidden"
-    style: { visibility: 'hidden' },
-  },
-  {
-    selector: 'edge.nodeHover',
-    style: {
-      width: 2,
-      // @ts-ignore
-      'z-index': zIndexEdgeHover,
-      'line-color': theme.eui.euiColorDarkShade,
-      'source-arrow-color': theme.eui.euiColorDarkShade,
-      'target-arrow-color': theme.eui.euiColorDarkShade,
+    {
+      selector: 'edge[isInverseEdge]',
+      // @ts-ignore DefinitelyTyped says visibility is "none" but it's
+      // actually "hidden"
+      style: { visibility: 'hidden' },
     },
-  },
-  {
-    selector: 'node.hover',
-    style: {
-      'border-width': getBorderWidth,
+    {
+      selector: 'edge.nodeHover',
+      style: {
+        width: 4,
+        // @ts-ignore
+        'z-index': zIndexEdgeHover,
+        'line-color': theme.eui.euiColorDarkShade,
+        'source-arrow-color': theme.eui.euiColorDarkShade,
+        'target-arrow-color': theme.eui.euiColorDarkShade,
+      },
     },
-  },
-  {
-    selector: 'edge.highlight',
-    style: {
-      width: 2,
-      'line-color': theme.eui.euiColorPrimary,
-      'source-arrow-color': theme.eui.euiColorPrimary,
-      'target-arrow-color': theme.eui.euiColorPrimary,
-      // @ts-ignore
-      'z-index': zIndexEdgeHighlight,
+    {
+      selector: 'node.hover',
+      style: {
+        'border-width': getBorderWidth,
+      },
     },
-  },
-];
+    {
+      selector: 'edge.highlight',
+      style: {
+        width: 4,
+        'line-color': theme.eui.euiColorPrimary,
+        'source-arrow-color': theme.eui.euiColorPrimary,
+        'target-arrow-color': theme.eui.euiColorPrimary,
+        // @ts-ignore
+        'z-index': zIndexEdgeHighlight,
+      },
+    },
+  ];
+};
 
 // The CSS styles for the div containing the cytoscape element. Makes a
 // background grid of dots.
