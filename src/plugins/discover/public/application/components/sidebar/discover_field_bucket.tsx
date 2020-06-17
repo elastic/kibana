063bf10ba0529afeab950e831726741254cecafe
Andrea Del Rio <delrio.andre@gmail.com>
2020-06-16 21:03:50 -0700

    fixed layout

--- src/plugins/discover/public/application/components/sidebar/discover_field_bucket.tsx
+++ src/plugins/discover/public/application/components/sidebar/discover_field_bucket.tsx
@@ -17,11 +17,12 @@
  * under the License.
  */
 import React from 'react';
-import { EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
+import { EuiText, EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { StringFieldProgressBar } from './string_progress_bar';
 import { Bucket } from './types';
 import { IndexPatternField } from '../../../../../data/public';
+import './discover_field_bucket.scss';
 
 interface Props {
   bucket: Bucket;
@@ -47,30 +48,64 @@ export function DiscoverFieldBucket({ field, bucket, onAddFilter }: Props) {
 
   return (
     <>
-      <EuiFlexGroup
-        justifyContent="spaceBetween"
-        style={{ width: 156 }}
-        gutterSize="xs"
-        responsive={false}
-      >
-        <EuiFlexItem grow={1} className="eui-textTruncate">
-          <EuiText size="xs" className="eui-textTruncate">
-            {bucket.display === '' ? emptyTxt : bucket.display}
-          </EuiText>
+      <EuiFlexGroup justifyContent="spaceBetween" responsive={false} gutterSize="xs">
+        <EuiFlexItem className="dscFieldDetails__barContainer" grow={1}>
+          <EuiFlexGroup justifyContent="spaceBetween" gutterSize="xs" responsive={false}>
+            <EuiFlexItem grow={1} className="eui-textTruncate">
+              <EuiText size="xs" className="eui-textTruncate">
+                {bucket.display === '' ? emptyTxt : bucket.display}
+              </EuiText>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false} className="eui-textTruncate">
+              <EuiText color="secondary" size="xs" className="eui-textTruncate">
+                {bucket.percent}%
+              </EuiText>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+          <StringFieldProgressBar
+            value={bucket.value}
+            percent={bucket.percent}
+            count={bucket.count}
+          />
         </EuiFlexItem>
-        <EuiFlexItem grow={false} className="eui-textTruncate">
-          <EuiText color="secondary" size="xs" className="eui-textTruncate">
-            {bucket.percent}%
-          </EuiText>
-        </EuiFlexItem>
+        {field.filterable && (
+          <EuiFlexItem grow={false}>
+            <div>
+              <EuiButtonIcon
+                iconSize="s"
+                iconType="magnifyWithPlus"
+                onClick={() => onAddFilter(field, bucket.value, '+')}
+                aria-label={addLabel}
+                data-test-subj={`plus-${field.name}-${bucket.value}`}
+                style={{
+                  minHeight: 'auto',
+                  minWidth: 'auto',
+                  paddingRight: 2,
+                  paddingLeft: 2,
+                  paddingTop: 0,
+                  paddingBottom: 0,
+                }}
+              />
+              <EuiButtonIcon
+                iconSize="s"
+                iconType="magnifyWithMinus"
+                onClick={() => onAddFilter(field, bucket.value, '-')}
+                aria-label={removeLabel}
+                data-test-subj={`minus-${field.name}-${bucket.value}`}
+                style={{
+                  minHeight: 'auto',
+                  minWidth: 'auto',
+                  paddingTop: 0,
+                  paddingBottom: 0,
+                  paddingRight: 2,
+                  paddingLeft: 2,
+                }}
+              />
+            </div>
+          </EuiFlexItem>
+        )}
       </EuiFlexGroup>
-      <StringFieldProgressBar
-        field={field}
-        value={bucket.value}
-        percent={bucket.percent}
-        onAddFilter={onAddFilter}
-        count={bucket.count}
-      />
+      <EuiSpacer size="s" />
     </>
   );
 }
--- src/plugins/discover/public/application/components/sidebar/string_progress_bar.tsx
+++ src/plugins/discover/public/application/components/sidebar/string_progress_bar.tsx
@@ -17,38 +17,16 @@
  * under the License.
  */
 import React from 'react';
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiProgress,
-  EuiText,
-  EuiToolTip,
-  EuiButtonIcon,
-} from '@elastic/eui';
-import { IndexPatternField } from '../../../../../data/public';
+import { EuiProgress, EuiText, EuiToolTip } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import './string_progress_bar.scss';
 
 interface Props {
   percent: number;
   count: number;
-  field: IndexPatternField;
-  onAddFilter: (field: IndexPatternField | string, value: string, type: '+' | '-') => void;
   value: string;
 }
 
 export function StringFieldProgressBar({ field, value, onAddFilter, percent, count }: Props) {
-  const addLabel = i18n.translate('discover.fieldChooser.detailViews.filterValueButtonAriaLabel', {
-    defaultMessage: 'Filter for {field}: "{value}"',
-    values: { value: value, field: field.name },
-  });
-  const removeLabel = i18n.translate(
-    'discover.fieldChooser.detailViews.filterOutValueButtonAriaLabel',
-    {
-      defaultMessage: 'Filter out {field}: "{value}"',
-      values: { value: value, field: field.name },
-    }
-  );
   const tooltipContent = `${value} ${count}`;
   return (
     <EuiToolTip
@@ -57,47 +35,7 @@ export function StringFieldProgressBar({ field, value, onAddFilter, percent, cou
       delay="regular"
       position="right"
     >
-      <EuiFlexGroup gutterSize="s" alignItems="center" responsive={false}>
-        <EuiFlexItem>
-          <EuiProgress value={percent} max={100} color="secondary" aria-hidden={true} size="s" />
-        </EuiFlexItem>
-        {field.filterable && (
-          <EuiFlexItem grow={false}>
-            <div>
-              <EuiButtonIcon
-                iconSize="s"
-                iconType="magnifyWithPlus"
-                onClick={() => onAddFilter(field, value, '+')}
-                aria-label={addLabel}
-                data-test-subj={`plus-${field.name}-${value}`}
-                style={{
-                  minHeight: 'auto',
-                  minWidth: 'auto',
-                  paddingRight: 2,
-                  paddingLeft: 2,
-                  paddingTop: 0,
-                  paddingBottom: 0,
-                }}
-              />
-              <EuiButtonIcon
-                iconSize="s"
-                iconType="magnifyWithMinus"
-                onClick={() => onAddFilter(field, value, '-')}
-                aria-label={removeLabel}
-                data-test-subj={`minus-${field.name}-${value}`}
-                style={{
-                  minHeight: 'auto',
-                  minWidth: 'auto',
-                  paddingTop: 0,
-                  paddingBottom: 0,
-                  paddingRight: 2,
-                  paddingLeft: 2,
-                }}
-              />
-            </div>
-          </EuiFlexItem>
-        )}
-      </EuiFlexGroup>
+      <EuiProgress value={percent} max={100} color="secondary" aria-hidden={true} size="s" />
     </EuiToolTip>
   );
 }
