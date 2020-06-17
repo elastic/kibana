73f78ad9eaa655e560640695ae8645dcc0c92f16
Gloria Hornero <snootchie.boochies@gmail.com>
2020-06-17 09:35:07 +0200

    adds 'URL compatibility' test

--- x-pack/plugins/security_solution/cypress/urls/navigation.ts
+++ x-pack/plugins/security_solution/cypress/urls/navigation.ts
@@ -4,8 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export const CASES = '/app/security/cases';
 export const ALERTS_URL = 'app/security/alerts';
+export const CASES = '/app/security/cases';
+export const DETECTIONS = '/app/siem#/detections';
 export const HOSTS_PAGE = '/app/security/hosts/allHosts';
 export const HOSTS_PAGE_TAB_URLS = {
   allHosts: '/app/security/hosts/allHosts',
