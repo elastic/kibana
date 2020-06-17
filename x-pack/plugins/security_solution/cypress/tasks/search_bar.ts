809369129e92d2c97bc5698b78757bc9fb3d7d67
Gloria Hornero <snootchie.boochies@gmail.com>
2020-06-17 14:35:45 +0200

    fixes test

--- x-pack/plugins/security_solution/cypress/tasks/search_bar.ts
+++ x-pack/plugins/security_solution/cypress/tasks/search_bar.ts
@@ -24,6 +24,7 @@ export const openAddFilterPopover = () => {
 
 export const fillAddFilterForm = ({ key, value }: SearchBarFilter) => {
   cy.get(ADD_FILTER_FORM_FIELD_INPUT).type(key);
+  cy.get(ADD_FILTER_FORM_FIELD_INPUT).click();
   cy.get(ADD_FILTER_FORM_FIELD_OPTION(key)).click({ force: true });
   cy.get(ADD_FILTER_FORM_OPERATOR_FIELD).click();
   cy.get(ADD_FILTER_FORM_OPERATOR_OPTION_IS).click();
