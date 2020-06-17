8469b3bed4e58397d85bab498504f9583e9a70d6
Christos Nasikas <christos.nasikas@elastic.co>
2020-06-17 14:55:46 +0300

    [SIEM][Timeline] Persist timeline to localStorage (#67156)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Xavier Mouligneau <189600+XavierM@users.noreply.github.com>

---
+++ x-pack/plugins/security_solution/public/alerts/constants.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const ALERTS_RULES_DETAILS_PAGE_TIMELINE_ID = 'alerts-rules-details-page';
+export const ALERTS_TIMELINE_ID = 'alerts-page';
