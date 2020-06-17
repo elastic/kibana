56a49166607a4945053013f7df6150de93b4c1d5
kevinlog <kevin.logan@elastic.co>
2020-06-17 16:14:35 -0400

    add policy empty state

--- x-pack/plugins/security_solution/public/management/pages/policy/view/policy_list.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/policy_list.tsx
@@ -22,6 +22,8 @@ import {
   EuiCallOut,
   EuiSpacer,
   EuiButton,
+  EuiSteps,
+  EuiTitle,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
@@ -59,6 +61,10 @@ const NO_WRAP_TRUNCATE_STYLE: CSSProperties = Object.freeze({
   overflow: 'hidden',
   textOverflow: 'ellipsis',
   whiteSpace: 'nowrap',
+});
+
+const TEXT_ALIGN_CENTER: CSSProperties = Object.freeze({
+  textAlign: 'center',
 });
 
 const DangerEuiContextMenuItem = styled(EuiContextMenuItem)`
@@ -410,24 +416,49 @@ export const PolicyList = React.memo(() => {
           </EuiButton>
         }
         bodyHeader={
-          <EuiText color="subdued" data-test-subj="policyTotalCount">
-            <FormattedMessage
-              id="xpack.securitySolution.endpoint.policyList.viewTitleTotalCount"
-              defaultMessage="{totalItemCount, plural, one {# Policy} other {# Policies}}"
-              values={{ totalItemCount }}
-            />
-          </EuiText>
+          policyItems &&
+          policyItems.length > 0 && (
+            <EuiText color="subdued" data-test-subj="policyTotalCount">
+              <FormattedMessage
+                id="xpack.securitySolution.endpoint.policyList.viewTitleTotalCount"
+                defaultMessage="{totalItemCount, plural, one {# Policy} other {# Policies}}"
+                values={{ totalItemCount }}
+              />
+            </EuiText>
+          )
         }
       >
-        <EuiBasicTable
-          items={useMemo(() => [...policyItems], [policyItems])}
-          columns={columns}
-          loading={loading}
-          pagination={paginationSetup}
-          onChange={handleTableChange}
-          data-test-subj="policyTable"
-          hasActions={false}
-        />
+        {useMemo(() => {
+          return (
+            <>
+              {policyItems && policyItems.length > 0 ? (
+                <EuiBasicTable
+                  items={[...policyItems]}
+                  columns={columns}
+                  loading={loading}
+                  pagination={paginationSetup}
+                  onChange={handleTableChange}
+                  data-test-subj="policyTable"
+                  hasActions={false}
+                />
+              ) : (
+                <EmptyPolicyTable
+                  loading={loading}
+                  onActionClick={handleCreatePolicyClick}
+                  actionDisabled={isFetchingPackageInfo}
+                />
+              )}
+            </>
+          );
+        }, [
+          policyItems,
+          loading,
+          isFetchingPackageInfo,
+          columns,
+          handleCreatePolicyClick,
+          handleTableChange,
+          paginationSetup,
+        ])}
         <SpyRoute />
       </ManagementPageView>
     </>
@@ -435,6 +466,106 @@ export const PolicyList = React.memo(() => {
 });
 
 PolicyList.displayName = 'PolicyList';
+
+const EmptyPolicyTable = React.memo<{
+  loading: boolean;
+  onActionClick: () => void;
+  actionDisabled: boolean;
+}>(({ loading, onActionClick, actionDisabled }) => {
+  const policySteps = [
+    {
+      title: i18n.translate('xpack.securitySolution.endpoint.policyList.stepOneTitle', {
+        defaultMessage: 'Head over to Ingest Manager.',
+      }),
+      children: (
+        <EuiText color="subdued" size="xs">
+          <FormattedMessage
+            id="xpack.securitySolution.endpoint.policyList.stepOne"
+            defaultMessage="Here, you’ll add the Elastic Endpoint Security Integration to your Agent Configuration."
+          />
+        </EuiText>
+      ),
+    },
+    {
+      title: i18n.translate('xpack.securitySolution.endpoint.policyList.stepTwoTitle', {
+        defaultMessage: 'We’ll create a recommended security policy for you.',
+      }),
+      children: (
+        <EuiText color="subdued" size="xs">
+          <FormattedMessage
+            id="xpack.securitySolution.endpoint.policyList.stepTwo"
+            defaultMessage="You can edit this policy in the “Policies” tab after you’ve added the Elastic Endpoint integration."
+          />
+        </EuiText>
+      ),
+    },
+    {
+      title: i18n.translate('xpack.securitySolution.endpoint.policyList.stepThreeTitle', {
+        defaultMessage: 'Enroll your agents through Fleet.',
+      }),
+      children: (
+        <EuiText color="subdued" size="xs">
+          <FormattedMessage
+            id="xpack.securitySolution.endpoint.policyList.stepThree"
+            defaultMessage="If you haven’t already, enroll your agents through Fleet using the same agent configuration."
+          />
+        </EuiText>
+      ),
+    },
+  ];
+  return (
+    <>
+      {loading ? (
+        <FormattedMessage
+          id="xpack.securitySolution.endpoint.policyList.loading"
+          defaultMessage="Loading..."
+        />
+      ) : (
+        <>
+          <EuiSpacer size="xxl" />
+          <EuiTitle size="m">
+            <h2 style={TEXT_ALIGN_CENTER}>
+              <FormattedMessage
+                id="xpack.securitySolution.endpoint.policyList.noPoliciesPrompt"
+                defaultMessage="Looks like you're not using Elastic Endpoint"
+              />
+            </h2>
+          </EuiTitle>
+          <EuiSpacer size="xxl" />
+          <EuiText textAlign="center" color="subdued" size="s">
+            <FormattedMessage
+              id="xpack.securitySolution.endpoint.policyList.noPoliciesInstructions"
+              defaultMessage="Elastic Endpoint Security gives you the power to keep your endpoints safe from attack, as well as unparalleled visibility into any threat in your environment."
+            />
+          </EuiText>
+          <EuiSpacer size="xxl" />
+          <EuiFlexGroup alignItems="center" justifyContent="center">
+            <EuiFlexItem grow={false}>
+              <EuiSteps steps={policySteps} />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+          <EuiFlexGroup alignItems="center" justifyContent="center">
+            <EuiFlexItem grow={false}>
+              <EuiButton
+                fill
+                onClick={onActionClick}
+                isDisabled={actionDisabled}
+                data-test-subj="emptyCreateNewPolicyButton"
+              >
+                <FormattedMessage
+                  id="xpack.securitySolution.endpoint.policyList.emptyCreateNewButton"
+                  defaultMessage="Click here to get started"
+                />
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </>
+      )}
+    </>
+  );
+});
+
+EmptyPolicyTable.displayName = 'EmptyPolicyTable';
 
 const ConfirmDelete = React.memo<{
   hostCount: number;
