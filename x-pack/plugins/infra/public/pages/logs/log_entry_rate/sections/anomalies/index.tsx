f72104c5fe2ccf8818e8e835a3c17943ec09fe2e
Kerry Gallagher <k.gallagher.05@gmail.com>
2020-06-17 14:49:33 +0100

    Remove top level chart
    Remove top level anomalies chart

--- x-pack/plugins/infra/public/pages/logs/log_entry_rate/sections/anomalies/index.tsx
+++ x-pack/plugins/infra/public/pages/logs/log_entry_rate/sections/anomalies/index.tsx
@@ -9,24 +9,14 @@ import {
   EuiFlexGroup,
   EuiFlexItem,
   EuiSpacer,
-  EuiStat,
   EuiTitle,
   EuiLoadingSpinner,
 } from '@elastic/eui';
-import numeral from '@elastic/numeral';
 import { i18n } from '@kbn/i18n';
 import React, { useMemo } from 'react';
 
-import { euiStyled } from '../../../../../../../observability/public';
 import { LogEntryRateResults } from '../../use_log_entry_rate_results';
 import { TimeRange } from '../../../../../../common/http_api/shared/time_range';
-import { formatAnomalyScore } from '../../../../../../common/log_analysis';
-import {
-  getAnnotationsForAll,
-  getLogEntryRateCombinedSeries,
-  getTopAnomalyScoreAcrossAllPartitions,
-} from '../helpers/data_formatters';
-import { AnomaliesChart } from './chart';
 import { AnomaliesTable } from './table';
 import { RecreateJobButton } from '../../../../../components/logging/log_analysis_job_status';
 import { AnalyzeInMlButton } from '../../../../../components/logging/log_analysis_results';
@@ -49,31 +39,6 @@ export const AnomaliesResults: React.FunctionComponent<{
         })
       : false;
   }, [results]);
-
-  const logEntryRateSeries = useMemo(
-    () => (results && results.histogramBuckets ? getLogEntryRateCombinedSeries(results) : []),
-    [results]
-  );
-  const anomalyAnnotations = useMemo(
-    () =>
-      results && results.histogramBuckets
-        ? getAnnotationsForAll(results)
-        : {
-            warning: [],
-            minor: [],
-            major: [],
-            critical: [],
-          },
-    [results]
-  );
-
-  const topAnomalyScore = useMemo(
-    () =>
-      results && results.histogramBuckets
-        ? getTopAnomalyScoreAcrossAllPartitions(results)
-        : undefined,
-    [results]
-  );
 
   return (
     <>
@@ -123,43 +88,6 @@ export const AnomaliesResults: React.FunctionComponent<{
           />
         ) : (
           <>
-            <EuiFlexGroup>
-              <EuiFlexItem grow={8}>
-                <AnomaliesChart
-                  chartId="overall"
-                  setTimeRange={setTimeRange}
-                  timeRange={timeRange}
-                  series={logEntryRateSeries}
-                  annotations={anomalyAnnotations}
-                  renderAnnotationTooltip={renderAnnotationTooltip}
-                />
-              </EuiFlexItem>
-              <EuiFlexItem grow={2}>
-                <EuiStat
-                  title={numeral(results.totalNumberOfLogEntries).format('0.00a')}
-                  titleSize="m"
-                  description={i18n.translate(
-                    'xpack.infra.logs.analysis.overallAnomaliesNumberOfLogEntriesDescription',
-                    {
-                      defaultMessage: 'Number of log entries',
-                    }
-                  )}
-                  reverse
-                />
-                <EuiStat
-                  title={topAnomalyScore ? formatAnomalyScore(topAnomalyScore) : null}
-                  titleSize="m"
-                  description={i18n.translate(
-                    'xpack.infra.logs.analysis.overallAnomaliesTopAnomalyScoreDescription',
-                    {
-                      defaultMessage: 'Max anomaly score',
-                    }
-                  )}
-                  reverse
-                />
-              </EuiFlexItem>
-            </EuiFlexGroup>
-            <EuiSpacer size="l" />
             <AnomaliesTable
               results={results}
               setTimeRange={setTimeRange}
@@ -176,52 +104,6 @@ export const AnomaliesResults: React.FunctionComponent<{
 const title = i18n.translate('xpack.infra.logs.analysis.anomaliesSectionTitle', {
   defaultMessage: 'Anomalies',
 });
-
-interface ParsedAnnotationDetails {
-  anomalyScoresByPartition: Array<{ partitionName: string; maximumAnomalyScore: number }>;
-}
-
-const overallAnomalyScoreLabel = i18n.translate(
-  'xpack.infra.logs.analysis.overallAnomalyChartMaxScoresLabel',
-  {
-    defaultMessage: 'Max anomaly scores:',
-  }
-);
-
-const AnnotationTooltip: React.FunctionComponent<{ details: string }> = ({ details }) => {
-  const parsedDetails: ParsedAnnotationDetails = JSON.parse(details);
-  return (
-    <TooltipWrapper>
-      <span>
-        <b>{overallAnomalyScoreLabel}</b>
-      </span>
-      <ul>
-        {parsedDetails.anomalyScoresByPartition.map(({ partitionName, maximumAnomalyScore }) => {
-          return (
-            <li key={`overall-anomaly-chart-${partitionName}`}>
-              <span>
-                {`${partitionName}: `}
-                <b>{maximumAnomalyScore}</b>
-              </span>
-            </li>
-          );
-        })}
-      </ul>
-    </TooltipWrapper>
-  );
-};
-
-const renderAnnotationTooltip = (details?: string) => {
-  // Note: Seems to be necessary to get things typed correctly all the way through to elastic-charts components
-  if (!details) {
-    return <div />;
-  }
-  return <AnnotationTooltip details={details} />;
-};
-
-const TooltipWrapper = euiStyled('div')`
-  white-space: nowrap;
-`;
 
 const loadingAriaLabel = i18n.translate(
   'xpack.infra.logs.analysis.anomaliesSectionLoadingAriaLabel',
