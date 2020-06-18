cdbf672274cba866cf73f63053d2f24d2cfb9661
Gidi Meir Morris <github@gidi.io>
2020-06-18 17:56:13 +0100

    Revert "removed alerts migration"
    This reverts commit 7c14ed214e2fa9f2eba3dc985490706b1b46d3c9.

--- x-pack/plugins/alerts/server/saved_objects/index.ts
+++ x-pack/plugins/alerts/server/saved_objects/index.ts
@@ -6,14 +6,14 @@
 
 import { SavedObjectsServiceSetup } from 'kibana/server';
 import mappings from './mappings.json';
+import { getMigrations } from './migrations';
 import { EncryptedSavedObjectsPluginSetup } from '../../../encrypted_saved_objects/server';
 
 export function setupSavedObjects(
   savedObjects: SavedObjectsServiceSetup,
   encryptedSavedObjects: EncryptedSavedObjectsPluginSetup
 ) {
-  // Encrypted attributes
-  encryptedSavedObjects.registerType({
+  const encryptedType = {
     type: 'alert',
     attributesToEncrypt: new Set(['apiKey']),
     attributesToExcludeFromAAD: new Set([
@@ -22,12 +22,16 @@ export function setupSavedObjects(
       'mutedInstanceIds',
       'updatedBy',
     ]),
-  });
+  };
+
+  // Encrypted attributes
+  encryptedSavedObjects.registerType(encryptedType);
 
   savedObjects.registerType({
     name: 'alert',
     hidden: true,
     namespaceType: 'single',
+    migrations: getMigrations(encryptedSavedObjects, encryptedType),
     mappings: mappings.alert,
   });
 }
