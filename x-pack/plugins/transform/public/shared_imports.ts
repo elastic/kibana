d113276d3d8a8b707bc1ca7d639c1aaa2df71540
Quynh Nguyen <43350163+qn895@users.noreply.github.com>
2020-06-23 15:04:24 -0500

    [ML] Transform: Table enhancements (#69307)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/transform/public/shared_imports.ts
+++ x-pack/plugins/transform/public/shared_imports.ts
@@ -16,6 +16,7 @@ export {
 export {
   getErrorMessage,
   extractErrorMessage,
+  formatHumanReadableDateTimeSeconds,
   getDataGridSchemaFromKibanaFieldType,
   getFieldsFromKibanaIndexPattern,
   multiColumnSortFactory,
