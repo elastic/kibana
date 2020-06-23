20b18307646cff5ab594ad478d38b00a89932d01
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2020-06-23 15:58:58 +0200

    Use global `URL` instead and rely on CoreJS polyfill.

--- x-pack/plugins/security/common/is_internal_url.ts
+++ x-pack/plugins/security/common/is_internal_url.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { parse, URL } from 'url';
+import { parse } from 'url';
 
 export function isInternalURL(url: string, basePath = '') {
   const { protocol, hostname, port, pathname } = parse(
