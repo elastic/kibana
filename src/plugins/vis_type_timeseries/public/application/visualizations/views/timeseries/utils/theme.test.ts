18771dc0642e2c8b4cc7c5855724e685a6a3ed74
nickofthyme <nick.ryan.partridge@gmail.com>
2020-06-23 13:05:38 -0500

    fix: broken jest tests

--- src/plugins/vis_type_timeseries/public/application/visualizations/views/timeseries/utils/theme.test.ts
+++ src/plugins/vis_type_timeseries/public/application/visualizations/views/timeseries/utils/theme.test.ts
@@ -17,28 +17,30 @@
  * under the License.
  */
 
-import { getTheme } from './theme';
+import { getBaseTheme } from './theme';
 import { LIGHT_THEME, DARK_THEME } from '@elastic/charts';
 
 describe('TSVB theme', () => {
   it('should return the basic themes if no bg color is specified', () => {
     // use original dark/light theme
-    expect(getTheme(false)).toEqual(LIGHT_THEME);
-    expect(getTheme(true)).toEqual(DARK_THEME);
+    expect(getBaseTheme(LIGHT_THEME)).toEqual(LIGHT_THEME);
+    expect(getBaseTheme(DARK_THEME)).toEqual(DARK_THEME);
 
     // discard any wrong/missing bg color
-    expect(getTheme(true, null)).toEqual(DARK_THEME);
-    expect(getTheme(true, '')).toEqual(DARK_THEME);
-    expect(getTheme(true, undefined)).toEqual(DARK_THEME);
+    expect(getBaseTheme(DARK_THEME, null)).toEqual(DARK_THEME);
+    expect(getBaseTheme(DARK_THEME, '')).toEqual(DARK_THEME);
+    expect(getBaseTheme(DARK_THEME, undefined)).toEqual(DARK_THEME);
   });
   it('should return a highcontrast color theme for a different background', () => {
     // red use a near full-black color
-    expect(getTheme(false, 'red').axes.axisTitleStyle.fill).toEqual('rgb(23,23,23)');
+    expect(getBaseTheme(LIGHT_THEME, 'red').axes.axisTitleStyle.fill).toEqual('rgb(23,23,23)');
 
     // violet increased the text color to full white for higer contrast
-    expect(getTheme(false, '#ba26ff').axes.axisTitleStyle.fill).toEqual('rgb(255,255,255)');
+    expect(getBaseTheme(LIGHT_THEME, '#ba26ff').axes.axisTitleStyle.fill).toEqual(
+      'rgb(255,255,255)'
+    );
 
     // light yellow, prefer the LIGHT_THEME fill color because already with a good contrast
-    expect(getTheme(false, '#fff49f').axes.axisTitleStyle.fill).toEqual('#333');
+    expect(getBaseTheme(LIGHT_THEME, '#fff49f').axes.axisTitleStyle.fill).toEqual('#333');
   });
 });
