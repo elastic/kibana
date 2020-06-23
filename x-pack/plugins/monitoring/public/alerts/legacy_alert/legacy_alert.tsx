1353368a8a6d8e385f5de0bc9990732427655ee7
chrisronline <chrisronline@gmail.com>
2020-06-23 10:41:19 -0400

    Add missing files

---
+++ x-pack/plugins/monitoring/public/alerts/legacy_alert/legacy_alert.tsx
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import React from 'react';
+import { EuiTextColor } from '@elastic/eui';
+// eslint-disable-next-line @kbn/eslint/no-restricted-paths
+import { AlertTypeModel } from '../../../../triggers_actions_ui/public/types';
+import { LEGACY_ALERTS } from '../../../common/constants';
+// eslint-disable-next-line @kbn/eslint/no-restricted-paths
+import { BY_TYPE } from '../../../server/alerts';
+
+export function createLegacyAlertTypes(): AlertTypeModel[] {
+  return LEGACY_ALERTS.map((legacyAlert) => {
+    const alertCls = BY_TYPE[legacyAlert];
+    const alert = new alertCls();
+    return {
+      id: legacyAlert,
+      name: alert.label,
+      iconClass: 'bell',
+      alertParamsExpression: (props: any) => (
+        <EuiTextColor color="subdued">There is nothing to configure.</EuiTextColor>
+      ),
+      validate: () => ({ errors: {} }),
+      requiresAppContext: false,
+    };
+  });
+}
