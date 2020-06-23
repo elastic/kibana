bf12a27da710edae16b424ca8980d4adda7ad620
Paul Tavares <paul.tavares@elastic.co>
2020-06-23 17:52:56 -0400

    Additional mocked services

--- x-pack/plugins/ingest_manager/server/routes/datasource/datasource_handlers.test.ts
+++ x-pack/plugins/ingest_manager/server/routes/datasource/datasource_handlers.test.ts
@@ -5,20 +5,21 @@
  */
 
 import { httpServerMock, httpServiceMock } from 'src/core/server/mocks';
-import { IRouter, RequestHandler, RouteConfig } from 'kibana/server';
+import { IRouter, KibanaRequest, RequestHandler, RouteConfig } from 'kibana/server';
 import { registerRoutes } from './index';
 import { DATASOURCE_API_ROUTES } from '../../../common/constants';
 import { xpackMocks } from '../../../../../mocks';
 import { appContextService } from '../../services';
 import { createAppContextStartContractMock } from '../../mocks';
 import { DatasourceServiceInterface } from '../..';
+import { CreateDatasourceRequestSchema } from '../../types/rest_spec';
 
 jest.mock('../../services/datasource', (): {
   datasourceService: jest.Mock<DatasourceServiceInterface>;
 } => {
   return {
     datasourceService: {
-      assignPackageStream: jest.fn((packageInfo, dataInputs) => Promise.resolved(dataInputs)),
+      assignPackageStream: jest.fn((packageInfo, dataInputs) => Promise.resolve(dataInputs)),
       buildDatasourceFromPackage: jest.fn(),
       bulkCreate: jest.fn(),
       create: jest.fn((soClient, newData) =>
@@ -41,15 +42,29 @@ jest.mock('../../services/datasource', (): {
   };
 });
 
+jest.mock('../../services/epm/packages', () => {
+  return {
+    ensureInstalledPackage: jest.fn(() => Promise.resolve()),
+    getPackageInfo: jest.fn(() => Promise.resolve()),
+  };
+});
+
 describe('When calling datasource', () => {
   let routerMock: jest.Mocked<IRouter>;
   let routeHandler: RequestHandler<any, any, any>;
   let routeConfig: RouteConfig<any, any, any, any>;
+  let context: ReturnType<typeof xpackMocks.createRequestHandlerContext>;
+  let response: ReturnType<typeof httpServerMock.createResponseFactory>;
 
   beforeAll(() => {
     routerMock = httpServiceMock.createRouter();
     appContextService.start(createAppContextStartContractMock());
     registerRoutes(routerMock);
+  });
+
+  beforeEach(() => {
+    context = xpackMocks.createRequestHandlerContext();
+    response = httpServerMock.createResponseFactory();
   });
 
   afterAll(() => {
@@ -61,6 +76,32 @@ describe('When calling datasource', () => {
   });
 
   describe('create api handler', () => {
+    const getCreateKibanaRequest: KibanaRequest<
+      undefined,
+      undefined,
+      typeof CreateDatasourceRequestSchema.body
+    > = (newData?: typeof CreateDatasourceRequestSchema.body) => {
+      return httpServerMock.createKibanaRequest<
+        undefined,
+        undefined,
+        typeof CreateDatasourceRequestSchema.body
+      >({
+        path: routeConfig.path,
+        method: 'post',
+        body: newData || {
+          name: 'endpoint-1',
+          description: '',
+          config_id: 'a5ca00c0-b30c-11ea-9732-1bb05811278c',
+          enabled: true,
+          output_id: '',
+          inputs: [],
+          namespace: 'default',
+          package: { name: 'endpoint', title: 'Elastic Endpoint', version: '0.5.0' },
+        },
+      });
+    };
+
+    // Set the routeConfig and routeHandler to the Create API
     beforeAll(() => {
       [routeConfig, routeHandler] = routerMock.post.mock.calls.find(([{ path }]) =>
         path.startsWith(DATASOURCE_API_ROUTES.CREATE_PATTERN)
@@ -69,14 +110,8 @@ describe('When calling datasource', () => {
 
     describe('and external callbacks are registered', () => {
       it('should call external callbacks', async () => {
-        const context = xpackMocks.createRequestHandlerContext();
-        const request = httpServerMock.createKibanaRequest({
-          path: DATASOURCE_API_ROUTES.CREATE_PATTERN,
-          method: 'post',
-          body: {},
-        });
-        const response = httpServerMock.createResponseFactory();
-        const apiResponseData = await routeHandler(context, request, response);
+        const request = getCreateKibanaRequest();
+        await routeHandler(context, request, response);
         expect(response.ok).toHaveBeenCalled();
       });
     });
