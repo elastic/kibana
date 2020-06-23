98a897736bbd1dd0af78585db7350dd44509acf1
Devon Thomson <devon.thomson@hotmail.com>
2020-06-23 12:21:48 -0400

    Remove App communication from URL (#67064)
    Removed all inter-app communication via url in favour of a new service in the embeddable start contract called the state transfer service.

--- src/plugins/embeddable/public/lib/state_transfer/index.ts
+++ src/plugins/embeddable/public/lib/state_transfer/index.ts
@@ -17,22 +17,5 @@
  * under the License.
  */
 
-import { SavedObjectAttributes } from 'kibana/public';
-import {
-  EmbeddableFactory,
-  EmbeddableInput,
-  EmbeddableOutput,
-  IEmbeddable,
-  EmbeddableFactoryDefinition,
-} from './lib/embeddables';
-
-export type EmbeddableFactoryRegistry = Map<string, EmbeddableFactory>;
-
-export type EmbeddableFactoryProvider = <
-  I extends EmbeddableInput = EmbeddableInput,
-  O extends EmbeddableOutput = EmbeddableOutput,
-  E extends IEmbeddable<I, O> = IEmbeddable<I, O>,
-  T extends SavedObjectAttributes = SavedObjectAttributes
->(
-  def: EmbeddableFactoryDefinition<I, O, E, T>
-) => EmbeddableFactory<I, O, E, T>;
+export { EmbeddableStateTransfer } from './embeddable_state_transfer';
+export { EmbeddableOriginatingAppState, EmbeddablePackageState } from './types';
