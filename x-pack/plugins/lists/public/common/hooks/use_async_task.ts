247f8e45bec37872770d2239c4fed1702e0f036a
Ryland Herrick <ryalnd@gmail.com>
2020-06-24 14:50:11 -0500

    Document current limitations of useAsyncTask

--- x-pack/plugins/lists/public/common/hooks/use_async_task.ts
+++ x-pack/plugins/lists/public/common/hooks/use_async_task.ts
@@ -7,6 +7,12 @@
 import { useCallback, useRef } from 'react';
 import { useAsyncFn } from 'react-use';
 
+// Params can be generalized to a ...rest parameter extending unknown[] once https://github.com/microsoft/TypeScript/pull/39094 is available.
+// for now, the task must still receive unknown as a second argument, and an argument must be passed to start()
+export type UseAsyncTask = <Result, Params extends unknown>(
+  task: (...args: [AbortController, Params]) => Promise<Result>
+) => AsyncTask<Result, Params>;
+
 export interface AsyncTask<Result, Params extends unknown> {
   start: (params: Params) => Promise<Result>;
   abort: () => void;
@@ -15,18 +21,20 @@ export interface AsyncTask<Result, Params extends unknown> {
   result: Result | undefined;
 }
 
-export type UseAsyncTask = <Result, Params extends unknown>(
-  func: (...args: [AbortController, Params]) => Promise<Result>
-) => AsyncTask<Result, Params>;
-
-export const useAsyncTask: UseAsyncTask = (fn) => {
+/**
+ *
+ * @param task Async function receiving an AbortController and optional arguments
+ *
+ * @returns An {@link AsyncTask} containing the underlying task's state along with start/abort helpers
+ */
+export const useAsyncTask: UseAsyncTask = (task) => {
   const ctrl = useRef(new AbortController());
   const abort = useCallback((): void => {
     ctrl.current.abort();
   }, []);
 
   // @ts-ignore typings are incorrect, see: https://github.com/streamich/react-use/pull/589
-  const [state, initiator] = useAsyncFn(fn, [fn]);
+  const [state, initiator] = useAsyncFn(task, [task]);
 
   const start = useCallback(
     (args) => {
