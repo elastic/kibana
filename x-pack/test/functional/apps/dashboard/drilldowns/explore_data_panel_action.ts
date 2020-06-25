4ceefdd9343b363d9bd31ec0691fe3fe68bafea4
streamich <streamich@gmail.com>
2020-06-25 13:17:54 +0200

    test: 💍 add in-chart "Explore underlying data" functional tests

--- x-pack/test/functional/apps/dashboard/drilldowns/explore_data_panel_action.ts
+++ x-pack/test/functional/apps/dashboard/drilldowns/explore_data_panel_action.ts
@@ -8,7 +8,7 @@ import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 const ACTION_ID = 'ACTION_EXPLORE_DATA';
-const EXPLORE_RAW_DATA_ACTION_TEST_SUBJ = `embeddablePanelAction-${ACTION_ID}`;
+const ACTION_TEST_SUBJ = `embeddablePanelAction-${ACTION_ID}`;
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const drilldowns = getService('dashboardDrilldownsManage');
@@ -37,18 +37,18 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     it('action exists in panel context menu', async () => {
       await dashboard.loadSavedDashboard(drilldowns.DASHBOARD_WITH_PIE_CHART_NAME);
       await panelActions.openContextMenu();
-      await testSubjects.existOrFail(EXPLORE_RAW_DATA_ACTION_TEST_SUBJ);
+      await testSubjects.existOrFail(ACTION_TEST_SUBJ);
     });
 
     it('is a link <a> element', async () => {
-      const actionElement = await testSubjects.find(EXPLORE_RAW_DATA_ACTION_TEST_SUBJ);
+      const actionElement = await testSubjects.find(ACTION_TEST_SUBJ);
       const tag = await actionElement.getTagName();
 
       expect(tag.toLowerCase()).to.be('a');
     });
 
     it('navigates to Discover app to index pattern of the panel on action click', async () => {
-      await testSubjects.clickWhenNotDisabled(EXPLORE_RAW_DATA_ACTION_TEST_SUBJ);
+      await testSubjects.clickWhenNotDisabled(ACTION_TEST_SUBJ);
       await discover.waitForDiscoverAppOnScreen();
 
       const el = await testSubjects.find('indexPattern-switch-link');
@@ -71,7 +71,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await dashboard.saveDashboard('Dashboard with Pie Chart');
 
       await panelActions.openContextMenu();
-      await testSubjects.clickWhenNotDisabled(EXPLORE_RAW_DATA_ACTION_TEST_SUBJ);
+      await testSubjects.clickWhenNotDisabled(ACTION_TEST_SUBJ);
       await discover.waitForDiscoverAppOnScreen();
 
       const text = await timePicker.getShowDatesButtonText();
