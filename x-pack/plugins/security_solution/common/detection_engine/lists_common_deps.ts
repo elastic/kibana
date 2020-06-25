0a30e34a414a7b0db556d59e9966ef288ef3f5c0
Yara Tercero <yctercero@users.noreply.github.com>
2020-06-25 11:21:51 -0400

    [SIEM][Detection Engine] - Update DE to work with new exceptions schema (#69715) (#69932)
    ## Summary
    
    The purpose of this PR is to update the detection engine to work with the new exception list schema. Previously, exceptions lived directly in the rule, now they are stored separately, and the rule only stores a reference to said exception lists.
    
    Changes are concentrated in two areas. First `x-pack/plugins/lists`:
    - Updates the `EntryList` schema so it now takes a list `id` and `type` (as needed for the detections engine logic) as opposed to before where it was just a string for the `id`
    - Updates the lists plugin unit tests to account for the updated `EntryList` schema
    - Exposes the exceptions list client (in the lists plugin) to be used by the detection engine
    
    Second in `x-pack/plugins/security_solution`:
    - Updates the detection engine `exceptions_list` schema. Previously, exceptions sat directly on the rule itself, now that exceptions are stored separately, the `exceptions_list` property needs to only keep reference to the exceptions list
    - Updates schema unit tests
    - Updates routes to use new exceptions list schema
    - Updates route unit tests
    - Removes old exception list scripts that are no longer necessary

--- x-pack/plugins/security_solution/common/detection_engine/lists_common_deps.ts
+++ x-pack/plugins/security_solution/common/detection_engine/lists_common_deps.ts
@@ -3,8 +3,5 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-export * from './default_comments_array';
-export * from './default_entries_array';
-export * from './default_namespace';
-export * from './comments';
-export * from './entries';
+
+export { EntriesArray, namespaceType } from '../../../lists/common/schemas';
