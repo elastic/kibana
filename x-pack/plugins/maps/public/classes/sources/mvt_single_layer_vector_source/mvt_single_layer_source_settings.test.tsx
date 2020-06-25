508b1c63fcfd7122d4dd60e6150b6094db715146
Thomas Neirynck <thomas@elastic.co>
2020-06-25 15:12:34 -0400

    remove unused import

--- x-pack/plugins/maps/public/classes/sources/mvt_single_layer_vector_source/mvt_single_layer_source_settings.test.tsx
+++ x-pack/plugins/maps/public/classes/sources/mvt_single_layer_vector_source/mvt_single_layer_source_settings.test.tsx
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { MVTFieldDescriptor } from '../../../../common/descriptor_types';
-
 jest.mock('../../../kibana_services', () => ({}));
 
 import React from 'react';
