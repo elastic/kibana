40bad84e4c0bd15b4d5d2dea96d47b664ad56998
Catherine Liu <catherine.liu@elastic.co>
2020-06-25 16:50:25 -0700

    [7.x] Adds link for Cloud deployment settings (#66486) (#70015)
    Co-authored-by: Michail Yasonik <michail@yasonik.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/core/public/chrome/ui/header/collapsible_nav.tsx
+++ src/core/public/chrome/ui/header/collapsible_nav.tsx
@@ -30,7 +30,7 @@ import {
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { groupBy, sortBy } from 'lodash';
-import React, { useRef } from 'react';
+import React, { Fragment, useRef } from 'react';
 import { useObservable } from 'react-use';
 import * as Rx from 'rxjs';
 import { ChromeNavLink, ChromeRecentlyAccessedHistoryItem } from '../..';
@@ -88,6 +88,7 @@ interface Props {
   onIsLockedUpdate: OnIsLockedUpdate;
   closeNav: () => void;
   navigateToApp: InternalApplicationStart['navigateToApp'];
+  customNavLink$: Rx.Observable<ChromeNavLink | undefined>;
 }
 
 export function CollapsibleNav({
@@ -105,6 +106,7 @@ export function CollapsibleNav({
 }: Props) {
   const navLinks = useObservable(observables.navLinks$, []).filter((link) => !link.hidden);
   const recentlyAccessed = useObservable(observables.recentlyAccessed$, []);
+  const customNavLink = useObservable(observables.customNavLink$, undefined);
   const appId = useObservable(observables.appId$, '');
   const lockRef = useRef<HTMLButtonElement>(null);
   const groupedNavLinks = groupBy(navLinks, (link) => link?.category?.id);
@@ -134,6 +136,38 @@ export function CollapsibleNav({
       isDocked={isLocked}
       onClose={closeNav}
     >
+      {customNavLink && (
+        <Fragment>
+          <EuiFlexItem grow={false} style={{ flexShrink: 0 }}>
+            <EuiCollapsibleNavGroup
+              background="light"
+              className="eui-yScroll"
+              style={{ maxHeight: '40vh' }}
+            >
+              <EuiListGroup
+                listItems={[
+                  createEuiListItem({
+                    link: customNavLink,
+                    legacyMode,
+                    basePath,
+                    navigateToApp,
+                    dataTestSubj: 'collapsibleNavCustomNavLink',
+                    onClick: closeNav,
+                    externalLink: true,
+                  }),
+                ]}
+                maxWidth="none"
+                color="text"
+                gutterSize="none"
+                size="s"
+              />
+            </EuiCollapsibleNavGroup>
+          </EuiFlexItem>
+
+          <EuiHorizontalRule margin="none" />
+        </Fragment>
+      )}
+
       {/* Pinned items */}
       <EuiFlexItem grow={false} style={{ flexShrink: 0 }}>
         <EuiCollapsibleNavGroup
