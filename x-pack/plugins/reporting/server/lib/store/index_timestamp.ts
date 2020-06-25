e1439052238cb7711f226d3cddbfcae22e18b157
Tim Sullivan <tsullivan@users.noreply.github.com>
2020-06-25 14:52:30 -0700

    [Reporting] ReportingStore module (#69426)
    * Add store class
    
    * fix tests
    
    * fix the createIndex bug
    
    * add reportingstore test
    
    * change function args
    
    * nits
    
    * add test for automatic index creation failure recovery

--- x-pack/plugins/reporting/server/lib/store/index_timestamp.ts
+++ x-pack/plugins/reporting/server/lib/store/index_timestamp.ts
@@ -4,19 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import moment from 'moment';
+import moment, { unitOfTime } from 'moment';
 
 export const intervals = ['year', 'month', 'week', 'day', 'hour', 'minute'];
 
 // TODO: This helper function can be removed by using `schema.duration` objects in the reporting config schema
-export function indexTimestamp(intervalStr, separator = '-') {
+export function indexTimestamp(intervalStr: string, separator = '-') {
+  const startOf = intervalStr as unitOfTime.StartOf;
   if (separator.match(/[a-z]/i)) throw new Error('Interval separator can not be a letter');
 
   const index = intervals.indexOf(intervalStr);
-  if (index === -1) throw new Error('Invalid index interval: ', intervalStr);
+  if (index === -1) throw new Error('Invalid index interval: ' + intervalStr);
 
   const m = moment();
-  m.startOf(intervalStr);
+  m.startOf(startOf);
 
   let dateString;
   switch (intervalStr) {
