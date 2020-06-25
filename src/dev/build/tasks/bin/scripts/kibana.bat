e2f13d68f3a5cd29032297c219167f9703737be8
Tiago Costa <tiagoffcc@hotmail.com>
2020-06-25 15:27:53 +0100

    chore(NA): prevent 'The system cannot find the file' error message

--- src/dev/build/tasks/bin/scripts/kibana.bat
+++ src/dev/build/tasks/bin/scripts/kibana.bat
@@ -19,11 +19,13 @@ If [%KIBANA_PATH_CONF%] == [] (
   set CONFIG_DIR=%DIR%config
 )
 
-for /F "eol=# tokens=*" %%i in (%CONFIG_DIR%\node.options) do (
-  If [!NODE_OPTIONS!] == [] (
-    set "NODE_OPTIONS=%%i"
-  )	Else (
-    set "NODE_OPTIONS=!NODE_OPTIONS! %%i"
+IF EXIST "%CONFIG_DIR%\node.options" (
+  for /F "eol=# tokens=*" %%i in (%CONFIG_DIR%\node.options) do (
+    If [!NODE_OPTIONS!] == [] (
+      set "NODE_OPTIONS=%%i"
+    )	Else (
+      set "NODE_OPTIONS=!NODE_OPTIONS! %%i"
+    )
   )
 )
 
