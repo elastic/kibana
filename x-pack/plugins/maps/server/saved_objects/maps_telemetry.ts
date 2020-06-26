8540f53631c4194fc8c133fe5559da84b029b671
Aaron Caldwell <aaronjcaldwell@gmail.com>
2020-06-26 08:30:31 -0600

    Change mappings to type object with 'enabled: false'
    Co-authored-by: Rudolf Meijering <skaapgif@gmail.com>

--- x-pack/plugins/maps/server/saved_objects/maps_telemetry.ts
+++ x-pack/plugins/maps/server/saved_objects/maps_telemetry.ts
@@ -5,19 +5,19 @@
  */
 import { SavedObjectsType } from 'src/core/server';
 
+/*
+ * The maps-telemetry saved object type isn't used, but in order to remove these fields from
+ * the mappings we register this type with `type: 'object', enabled: true` to remove all
+ * previous fields from the mappings until https://github.com/elastic/kibana/issues/67086 is
+ * solved.
+ */
 export const mapsTelemetrySavedObjects: SavedObjectsType = {
   name: 'maps-telemetry',
   hidden: false,
   namespaceType: 'agnostic',
   mappings: {
-    properties: {
-      settings: { type: 'object', index: false },
-      indexPatternsWithGeoFieldCount: { type: 'long', index: false },
-      indexPatternsWithGeoPointFieldCount: { type: 'long', index: false },
-      indexPatternsWithGeoShapeFieldCount: { type: 'long', index: false },
-      mapsTotalCount: { type: 'long', index: false },
-      timeCaptured: { type: 'date', index: false },
-      attributesPerMap: { type: 'object', index: false },
-    },
+    // @ts-ignore Core types don't support this since it's only really valid when removing a previously registered type
+    type: 'object',
+    enabled: false,
   },
 };
