c56debf2696b7b998f5c0cee3d6dde8f14064a70
Dario Gieselaar <dario.gieselaar@elastic.co>
2020-06-26 12:13:02 +0200

    [APM] Run API tests as restricted user
    Run API tests as a user that has the apm_user role and the kibana_admin role. These are (roughly) the minimum requirements for using APM.

---
+++ x-pack/test/apm_api_integration/common/authentication.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const APM_READ_USER = {
+  name: 'apm_read_user',
+  roles: ['apm_read_user', 'superuser'],
+};
+
+export const APM_TEST_PASSWORD = 'changeme';
--- x-pack/test/apm_api_integration/common/config.ts
+++ x-pack/test/apm_api_integration/common/config.ts
@@ -5,6 +5,10 @@
  */
 
 import { FtrConfigProviderContext } from '@kbn/test/types/ftr';
+import { FtrProviderContext } from 'test/functional/ftr_provider_context';
+import supertestAsPromised from 'supertest-as-promised';
+import { format } from 'url';
+import { APM_READ_USER, APM_TEST_PASSWORD } from './authentication';
 
 interface Settings {
   license: 'basic' | 'trial';
@@ -20,14 +24,42 @@ export function createTestConfig(settings: Settings) {
       require.resolve('../../api_integration/config.ts')
     );
 
+    const services = xPackAPITestsConfig.get('services');
+    const servers = xPackAPITestsConfig.get('servers');
+
+    servers.kibana = {
+      ...servers.kibana,
+    };
+
     return {
       testFiles,
-      servers: xPackAPITestsConfig.get('servers'),
-      services: xPackAPITestsConfig.get('services'),
+      servers,
+      services: {
+        ...services,
+        supertest: async (context: FtrProviderContext) => {
+          const security = context.getService('security');
+
+          await security.init();
+
+          await security.user.create(APM_READ_USER.name, {
+            password: APM_TEST_PASSWORD,
+            full_name: APM_READ_USER.name,
+            roles: APM_READ_USER.roles,
+          });
+
+          const url = format({
+            ...servers.kibana,
+            auth: `${APM_READ_USER.name}:${APM_TEST_PASSWORD}`,
+            username: APM_READ_USER.name,
+            password: APM_TEST_PASSWORD,
+          });
+
+          return supertestAsPromised(url);
+        },
+      },
       junit: {
         reportName: name,
       },
-
       esTestCluster: {
         ...xPackAPITestsConfig.get('esTestCluster'),
         license,
