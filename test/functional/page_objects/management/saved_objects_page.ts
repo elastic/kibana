2c4059e4d075ea5b9ae6b0bd27ff01b9ff7f08f0
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2020-06-26 13:58:26 +0200

    add missing await

--- test/functional/page_objects/management/saved_objects_page.ts
+++ test/functional/page_objects/management/saved_objects_page.ts
@@ -169,7 +169,7 @@ export function SavedObjectsPageProvider({ getService, getPageObjects }: FtrProv
     }
 
     async clickTableSelectAll() {
-      testSubjects.click('checkboxSelectAll');
+      await testSubjects.click('checkboxSelectAll');
     }
 
     async canBeDeleted() {
