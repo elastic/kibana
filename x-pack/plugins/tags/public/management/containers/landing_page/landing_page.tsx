13b3e5ad70e0be0d0a87793da1ae2d2de610083f
streamich <streamich@gmail.com>
2020-06-29 20:35:26 +0000

    feat: 🎸 add search table to landing page

--- x-pack/plugins/tags/public/management/containers/landing_page/landing_page.tsx
+++ x-pack/plugins/tags/public/management/containers/landing_page/landing_page.tsx
@@ -6,9 +6,10 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { EuiButton } from '@elastic/eui';
+import { EuiButton, EuiInMemoryTable } from '@elastic/eui';
 import { Page } from '../../components/page';
 import { txtTitle, txtSubtitle, txtCreateATag } from './i18n';
+import { Tag } from '../../../../common';
 
 export const LandingPage: React.FC = () => {
   return (
@@ -21,7 +22,47 @@ export const LandingPage: React.FC = () => {
         </Link>
       }
     >
-      <div>Hello world</div>
+      <EuiInMemoryTable<Tag>
+        itemId={'id'}
+        items={[
+          {
+            id: 'foo',
+            title: 'Hello',
+            description: 'lol',
+            color: '',
+            createdAt: '',
+            createdBy: '',
+            enabled: true,
+            key: '',
+            value: '',
+            updatedAt: '',
+            updatedBy: '',
+          },
+        ]}
+        columns={[
+          {
+            field: 'title',
+            name: 'Tag',
+            sortable: true,
+            render: (value: string, record: Tag) => <div>{value}</div>,
+          },
+          {
+            field: 'description',
+            name: 'Description',
+            sortable: true,
+          },
+        ]}
+        hasActions
+        pagination={true}
+        sorting={true}
+        search={{
+          box: {
+            placeholder: 'Search',
+          },
+        }}
+        loading={false}
+        message={undefined}
+      />
     </Page>
   );
 };
