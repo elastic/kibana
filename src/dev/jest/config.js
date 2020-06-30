0e534b8a3f2c89384ef0ea2a4a03995d42dcf6f9
Thomas Watson <w@tson.dk>
2020-06-30 16:21:16 +0200

    chore: add missing mjs extension

--- src/dev/jest/config.js
+++ src/dev/jest/config.js
@@ -84,7 +84,7 @@ export default {
   moduleFileExtensions: ['js', 'mjs', 'json', 'ts', 'tsx', 'node'],
   modulePathIgnorePatterns: ['__fixtures__/', 'target/'],
   testEnvironment: 'jest-environment-jsdom-thirteen',
-  testMatch: ['**/*.test.{js,ts,tsx}'],
+  testMatch: ['**/*.test.{js,mjs,ts,tsx}'],
   testPathIgnorePatterns: [
     '<rootDir>/packages/kbn-ui-framework/(dist|doc_site|generator-kui)/',
     '<rootDir>/packages/kbn-pm/dist/',
