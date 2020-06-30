3d4180e86005038d15ba9b90e570792e631c361b
Ryland Herrick <ryalnd@gmail.com>
2020-06-30 14:04:21 -0500

    Adds hook for retrieving the component's mount status
    This is useful for dealing with asynchronous tasks that may complete
    after the invoking component has been unmounted. Using this hook,
    callbacks can determine whether they're currently unmounted, i.e.
    whether it's safe to set state or not.

---
+++ x-pack/plugins/lists/public/common/hooks/use_is_mounted.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { useCallback, useEffect, useRef } from 'react';
+
+type GetIsMounted = () => boolean;
+
+export const useIsMounted = (): GetIsMounted => {
+  const isMounted = useRef(false);
+  const getIsMounted: GetIsMounted = useCallback(() => isMounted.current, []);
+  const handleCleanup = useCallback(() => {
+    isMounted.current = false;
+  }, []);
+
+  useEffect(() => {
+    isMounted.current = true;
+    return handleCleanup;
+  }, [handleCleanup]);
+
+  return getIsMounted;
+};
