35bf6b6ad06bf27290c62a3b1319a4768ac6f619
Ryland Herrick <ryalnd@gmail.com>
2020-06-30 14:49:55 -0500

    Update exported Lists API hooks to use useAsync and withOptionalSignal
    Removes the now-unused useAsyncTask as well.

--- x-pack/plugins/lists/public/lists/hooks/use_delete_list.ts
+++ x-pack/plugins/lists/public/lists/hooks/use_delete_list.ts
@@ -4,16 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { useAsyncTask } from '../../common/hooks/use_async_task';
-import { DeleteListParams } from '../types';
+import { withOptionalSignal } from '../../common/with_optional_signal';
+import { useAsync } from '../../common/hooks/use_async';
 import { deleteList } from '../api';
 
-export type DeleteListTaskArgs = Omit<DeleteListParams, 'signal'>;
-
-const deleteListsTask = (
-  { signal }: AbortController,
-  args: DeleteListTaskArgs
-): ReturnType<typeof deleteList> => deleteList({ signal, ...args });
+const deleteListWithOptionalSignal = withOptionalSignal(deleteList);
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const useDeleteList = () => useAsyncTask(deleteListsTask);
+export const useDeleteList = () => useAsync(deleteListWithOptionalSignal);
