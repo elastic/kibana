eae1639fc03e4e031fc287fddb6efbcdd4817882
Madison Caldwell <madison.caldwell@elastic.co>
2020-06-30 15:13:02 -0400

    minor fixup

--- x-pack/plugins/lists/server/index.ts
+++ x-pack/plugins/lists/server/index.ts
@@ -10,8 +10,8 @@ import { ConfigSchema } from './config';
 import { ListPlugin } from './plugin';
 
 // exporting these since its required at top level in siem plugin
-export { ExceptionListClient } from './services/exception_lists/exception_list_client';
 export { ListClient } from './services/lists/list_client';
+export { ExceptionListClient } from './services/exception_lists/exception_list_client';
 export { ListPluginSetup } from './types';
 
 export const config = { schema: ConfigSchema };
