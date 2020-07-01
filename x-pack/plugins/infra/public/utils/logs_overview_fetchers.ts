6209620c1de20be1bec5c63ab3fab9b0cbe51b73
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2020-07-01 11:05:17 +0200

    Extract mocked stats and series

--- x-pack/plugins/infra/public/utils/logs_overview_fetchers.ts
+++ x-pack/plugins/infra/public/utils/logs_overview_fetchers.ts
@@ -7,6 +7,8 @@
 import { InfraClientCoreSetup } from '../types';
 import { FetchData, LogsFetchDataResponse, HasData } from '../../../observability/public';
 
+type StatsAndSeries = Pick<LogsFetchDataResponse, 'stats' | 'series'>;
+
 export function getLogsHasDataFetcher(
   getStartServices: InfraClientCoreSetup['getStartServices']
 ): HasData {
@@ -25,70 +27,77 @@ export function getLogsHasDataFetcher(
 export function getLogsOverviewDataFetcher(
   getStartServices: InfraClientCoreSetup['getStartServices']
 ): FetchData<LogsFetchDataResponse> {
-  return async () => {
-    // const [, startPlugins] = await getStartServices();
-    // const { data } = startPlugins;
+  return async (params) => {
+    const [, startPlugins] = await getStartServices();
+    const { data } = startPlugins;
 
-    // if you need a core dep, we need to pass in more than just getStartServices
+    const { stats, series } = await fetchLogsOverview();
 
-    // perform query
     return {
       title: 'Log rate',
       appLink: 'TBD', // TODO: what format should this be in, relative I assume?
-      stats: {
-        nginx: {
-          type: 'number',
-          label: 'nginx',
-          value: 345341,
-        },
-        'elasticsearch.audit': {
-          type: 'number',
-          label: 'elasticsearch.audit',
-          value: 164929,
-        },
-        'haproxy.log': {
-          type: 'number',
-          label: 'haproxy.log',
-          value: 51101,
-        },
-      },
-      // Note: My understanding is that these series coordinates will be
-      // combined into objects that look like:
-      // { x: timestamp, y: value, g: label (e.g. nginx) }
-      // so they fit the stacked bar chart API
-      // https://elastic.github.io/elastic-charts/?path=/story/bar-chart--stacked-with-axis-and-legend
-      series: {
-        nginx: {
-          label: 'nginx',
-          coordinates: [
-            { x: 1593000000000, y: 10014 },
-            { x: 1593000900000, y: 12827 },
-            { x: 1593001800000, y: 2946 },
-            { x: 1593002700000, y: 14298 },
-            { x: 1593003600000, y: 4096 },
-          ],
-        },
-        'elasticsearch.audit': {
-          label: 'elasticsearch.audit',
-          coordinates: [
-            { x: 1593000000000, y: 5676 },
-            { x: 1593000900000, y: 6783 },
-            { x: 1593001800000, y: 2394 },
-            { x: 1593002700000, y: 4554 },
-            { x: 1593003600000, y: 5659 },
-          ],
-        },
-        'haproxy.log': {
-          label: 'haproxy.log',
-          coordinates: [
-            { x: 1593000000000, y: 9085 },
-            { x: 1593000900000, y: 9002 },
-            { x: 1593001800000, y: 3940 },
-            { x: 1593002700000, y: 5451 },
-            { x: 1593003600000, y: 9133 },
-          ],
-        },
-      },
+      stats,
+      series,
     };
   };
+}
+
+async function fetchLogsOverview(): Promise<StatsAndSeries> {
+  return Promise.resolve({
+    stats: {
+      nginx: {
+        type: 'number',
+        label: 'nginx',
+        value: 345341,
+      },
+      'elasticsearch.audit': {
+        type: 'number',
+        label: 'elasticsearch.audit',
+        value: 164929,
+      },
+      'haproxy.log': {
+        type: 'number',
+        label: 'haproxy.log',
+        value: 51101,
+      },
+    },
+
+    // Note: My understanding is that these series coordinates will be
+    // combined into objects that look like:
+    // { x: timestamp, y: value, g: label (e.g. nginx) }
+    // so they fit the stacked bar chart API
+    // https://elastic.github.io/elastic-charts/?path=/story/bar-chart--stacked-with-axis-and-legend
+    series: {
+      nginx: {
+        label: 'nginx',
+        coordinates: [
+          { x: 1593000000000, y: 10014 },
+          { x: 1593000900000, y: 12827 },
+          { x: 1593001800000, y: 2946 },
+          { x: 1593002700000, y: 14298 },
+          { x: 1593003600000, y: 4096 },
+        ],
+      },
+      'elasticsearch.audit': {
+        label: 'elasticsearch.audit',
+        coordinates: [
+          { x: 1593000000000, y: 5676 },
+          { x: 1593000900000, y: 6783 },
+          { x: 1593001800000, y: 2394 },
+          { x: 1593002700000, y: 4554 },
+          { x: 1593003600000, y: 5659 },
+        ],
+      },
+      'haproxy.log': {
+        label: 'haproxy.log',
+        coordinates: [
+          { x: 1593000000000, y: 9085 },
+          { x: 1593000900000, y: 9002 },
+          { x: 1593001800000, y: 3940 },
+          { x: 1593002700000, y: 5451 },
+          { x: 1593003600000, y: 9133 },
+        ],
+      },
+    },
+  });
 }
