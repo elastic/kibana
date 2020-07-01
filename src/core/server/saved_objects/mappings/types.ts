32dbf22abdbe21b2eafd762c6983c42abd171ef9
Rudolf Meijering <skaapgif@gmail.com>
2020-07-01 17:49:43 +0200

    doc_values not doc_value

--- src/core/server/saved_objects/mappings/types.ts
+++ src/core/server/saved_objects/mappings/types.ts
@@ -133,7 +133,7 @@ export interface SavedObjectsCoreFieldMapping {
   type: string;
   null_value?: number | boolean | string;
   index?: boolean;
-  doc_value?: boolean;
+  doc_values?: boolean;
   enabled?: boolean;
   fields?: {
     [subfield: string]: {
