238e3b4d623a0da2b12d5aa5fba3625987ecaf6a
Nathan Reese <reese.nathan@gmail.com>
2020-07-02 08:39:16 -0600

    [Maps] fix unable to edit heatmap metric

--- x-pack/plugins/maps/public/classes/sources/es_agg_source/es_agg_source.js
+++ x-pack/plugins/maps/public/classes/sources/es_agg_source/es_agg_source.js
@@ -10,6 +10,8 @@ import { esAggFieldsFactory } from '../../fields/es_agg_field';
 import { AGG_TYPE, COUNT_PROP_LABEL, FIELD_ORIGIN } from '../../../../common/constants';
 import { getSourceAggKey } from '../../../../common/get_agg_key';
 
+export const DEFAULT_METRIC = { type: AGG_TYPE.COUNT };
+
 export class AbstractESAggSource extends AbstractESSource {
   constructor(descriptor, inspectorAdapters) {
     super(descriptor, inspectorAdapters);
