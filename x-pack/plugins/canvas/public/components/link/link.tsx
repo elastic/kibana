5bd261fdb0a50fbbc002157379d265be116bae0e
Clint Andrew Hall <clint.hall@elastic.co>
2020-07-02 13:22:21 -0400

    Link args weren't being reapplied

--- x-pack/plugins/canvas/public/components/link/link.tsx
+++ x-pack/plugins/canvas/public/components/link/link.tsx
@@ -4,9 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FC, MouseEventHandler, MouseEvent } from 'react';
+import React, { FC, MouseEvent } from 'react';
 import PropTypes from 'prop-types';
-import { EuiLink } from '@elastic/eui';
+import { EuiLink, EuiLinkProps } from '@elastic/eui';
 
 import { ComponentStrings } from '../../../i18n';
 
@@ -16,14 +16,15 @@ const isModifiedEvent = (ev: MouseEvent) =>
   !!(ev.metaKey || ev.altKey || ev.ctrlKey || ev.shiftKey);
 
 interface Props {
-  target?: string;
-  onClick?: MouseEventHandler;
   name: string;
   params: Record<string, any>;
 }
 
-export const Link: FC<Props> = ({ onClick, target, name, params, children }, { router }) => {
-  const navigateTo: MouseEventHandler = (ev) => {
+export const Link: FC<Props & EuiLinkProps> = (
+  { onClick, target, name, params, children, ...linkArgs },
+  { router }
+) => {
+  const navigateTo = (ev: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
     if (onClick) {
       onClick(ev);
     }
@@ -45,6 +46,7 @@ export const Link: FC<Props> = ({ onClick, target, name, params, children }, { r
       target,
       href,
       onClick: navigateTo,
+      ...linkArgs,
     };
 
     return <EuiLink {...props}>{children}</EuiLink>;
@@ -58,8 +60,6 @@ Link.contextTypes = {
 };
 
 Link.propTypes = {
-  target: PropTypes.string,
-  onClick: PropTypes.func,
   name: PropTypes.string.isRequired,
   params: PropTypes.object,
 };
