27af1e33ace69fde9d36694262518220de6ed9b5
patrykkopycinski <patryk.kopycinski@elastic.co>
2020-07-02 19:43:25 +0200

    [Security Solution] Reposition EuiPopovers on scroll (#69433) (#70615)
    * [Security Solution] Reposition EuiPopovers on scroll
    
    * update snapshots
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/timelines/components/timeline/properties/properties_right.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/properties/properties_right.tsx
@@ -138,6 +138,7 @@ const PropertiesRightComponent: React.FC<PropertiesRightComponentProps> = ({
             id="timelineSettingsPopover"
             isOpen={showActions}
             closePopover={onClosePopover}
+            repositionOnScroll
           >
             <EuiFlexGroup alignItems="flexStart" direction="column" gutterSize="none">
               {capabilitiesCanUserCRUD && (
