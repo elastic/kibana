76970ef256d344a8e8d735131b9c2eca979ac1c0
Constance Chen <constance.chen.3@gmail.com>
2020-07-02 08:54:33 -0700

    Address various telemetry issues
    - saved objects: removing indexing per #43673
    - add schema and generate json per #64942
    - move definitions over to collectors since saved objects is mostly empty at this point, and schema throws an error when it imports an obj instead of being defined inline
    - istanbul ignore saved_objects file since it doesn't have anything meaningful to test but was affecting code coverage

--- x-pack/plugins/enterprise_search/server/saved_objects/app_search/telemetry.ts
+++ x-pack/plugins/enterprise_search/server/saved_objects/app_search/telemetry.ts
@@ -3,73 +3,17 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+/* istanbul ignore file */
 
 import { SavedObjectsType } from 'src/core/server';
-
-export const AS_TELEMETRY_NAME = 'app_search_telemetry';
-
-export interface ITelemetrySavedObject {
-  ui_viewed: {
-    setup_guide: number;
-    engines_overview: number;
-  };
-  ui_error: {
-    cannot_connect: number;
-    no_as_account: number;
-  };
-  ui_clicked: {
-    create_first_engine_button: number;
-    header_launch_button: number;
-    engine_table_link: number;
-  };
-}
+import { AS_TELEMETRY_NAME } from '../../collectors/app_search/telemetry';
 
 export const appSearchTelemetryType: SavedObjectsType = {
   name: AS_TELEMETRY_NAME,
   hidden: false,
   namespaceType: 'single',
   mappings: {
-    properties: {
-      ui_viewed: {
-        properties: {
-          setup_guide: {
-            type: 'long',
-            null_value: 0,
-          },
-          engines_overview: {
-            type: 'long',
-            null_value: 0,
-          },
-        },
-      },
-      ui_error: {
-        properties: {
-          cannot_connect: {
-            type: 'long',
-            null_value: 0,
-          },
-          no_as_account: {
-            type: 'long',
-            null_value: 0,
-          },
-        },
-      },
-      ui_clicked: {
-        properties: {
-          create_first_engine_button: {
-            type: 'long',
-            null_value: 0,
-          },
-          header_launch_button: {
-            type: 'long',
-            null_value: 0,
-          },
-          engine_table_link: {
-            type: 'long',
-            null_value: 0,
-          },
-        },
-      },
-    },
+    dynamic: false,
+    properties: {},
   },
 };
