e09c77b6bef9b5b8e31c11448b2cf54a819ffa4d
Rudolf Meijering <skaapgif@gmail.com>
2020-07-02 14:00:59 +0200

    CI Reporter for saved objects field count

---
+++ x-pack/test/saved_objects_field_count/runner.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { CiStatsReporter } from '@kbn/dev-utils';
+// @ts-ignore not TS yet
+import getUrl from '../../../src/test_utils/get_url';
+
+import { FtrProviderContext } from './../functional/ftr_provider_context';
+
+export async function testRunner({ getService }: FtrProviderContext) {
+  const log = getService('log');
+  const config = getService('config');
+  const es = getService('es');
+
+  const reporter = CiStatsReporter.fromEnv(log);
+
+  const { body: fields } = await es.fieldCaps({
+    index: '.kibana',
+    fields: '*',
+  });
+  const fieldCount = Object.keys(fields).length;
+  log.debug('Saved Objects field count');
+  await reporter.metrics({ group: 'Saved Objects field count', id: 'total', value: fieldCount });
+}
