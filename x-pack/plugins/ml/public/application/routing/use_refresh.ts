90336a18df62b157a6fc3452086ba02360f69c01
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-07-02 14:56:38 +0200

    [ML] fix tests

--- x-pack/plugins/ml/public/application/routing/use_refresh.ts
+++ x-pack/plugins/ml/public/application/routing/use_refresh.ts
@@ -18,6 +18,10 @@ export interface Refresh {
   timeRange?: { start: string; end: string };
 }
 
+/**
+ * Hook that provides the latest refresh timestamp
+ * and the most recent applied time range.
+ */
 export const useRefresh = () => {
   const timefilter = useTimefilter();
 
