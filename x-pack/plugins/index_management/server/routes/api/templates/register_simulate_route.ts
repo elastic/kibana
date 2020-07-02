683ab460c25d972adf4869046b48266cb52f7f8b
Sébastien Loix <sabee77@gmail.com>
2020-07-02 16:23:55 +0200

    Add server api route

---
+++ x-pack/plugins/index_management/server/routes/api/templates/register_simulate_route.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { schema, TypeOf } from '@kbn/config-schema';
+
+import { RouteDependencies } from '../../../types';
+import { addBasePath } from '../index';
+
+const bodySchema = schema.object({}, { unknowns: 'allow' });
+
+export function registerSimulateRoute({ router, license, lib }: RouteDependencies) {
+  router.post(
+    {
+      path: addBasePath('/index_templates/simulate'),
+      validate: { body: bodySchema },
+    },
+    license.guardApiRoute(async (ctx, req, res) => {
+      const { callAsCurrentUser } = ctx.dataManagement!.client;
+      const template = req.body as TypeOf<typeof bodySchema>;
+
+      try {
+        const templatePreview = await callAsCurrentUser('dataManagement.simulateTemplate', {
+          body: template,
+        });
+
+        return res.ok({ body: templatePreview });
+      } catch (e) {
+        if (lib.isEsError(e)) {
+          return res.customError({
+            statusCode: e.statusCode,
+            body: e,
+          });
+        }
+        // Case: default
+        return res.internalError({ body: e });
+      }
+    })
+  );
+}
