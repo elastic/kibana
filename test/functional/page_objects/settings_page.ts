a31888169e568b8ed0a4586850207ef85bc1f1fb
John Dorlus <silne.dorlus@elastic.co>
2020-07-02 18:54:13 -0400

    Fixed assertion in hybrid index pattern test to iterate through indices (#70130) (#70540)
    * Fixed assertion to check through all indices before making assertion.
    
    * Restored yarn.lock file.
    
    * Removed only from describe.
    
    * Fixed linting issue.
    
    * Fixed nits that were in the PR conversation.
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- test/functional/page_objects/settings_page.ts
+++ test/functional/page_objects/settings_page.ts
@@ -303,6 +303,13 @@ export function SettingsPageProvider({ getService, getPageObjects }: FtrProvider
       );
     }
 
+    async getAllIndexPatternNames() {
+      const indexPatterns = await this.getIndexPatternList();
+      return await mapAsync(indexPatterns, async (index) => {
+        return await index.getVisibleText();
+      });
+    }
+
     async isIndexPatternListEmpty() {
       await testSubjects.existOrFail('indexPatternTable', { timeout: 5000 });
       const indexPatternList = await this.getIndexPatternList();
