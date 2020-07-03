8e2dd9b7c6fa34b54903312e75752a85f60f2cd6
Alison Goryachev <alisonmllr20@gmail.com>
2020-07-02 21:15:48 -0400

    fix TS

--- x-pack/plugins/rollup/server/collectors/register.ts
+++ x-pack/plugins/rollup/server/collectors/register.ts
@@ -137,17 +137,23 @@ async function fetchRollupVisualizations(
   let rollupVisualizationsFromSavedSearches = 0;
 
   visualizations.forEach((visualization) => {
-    const references = get(visualization, '_source.references', []);
-    const savedSearchRefName = get(visualization, '_source.visualization.savedSearchRefName');
-    const searchSourceJSON = get(
+    const references: Array<{ name: string; id: string }> | undefined = get(
+      visualization,
+      '_source.references'
+    );
+    const savedSearchRefName: string | undefined = get(
+      visualization,
+      '_source.visualization.savedSearchRefName'
+    );
+    const searchSourceJSON: string | undefined = get(
       visualization,
       '_source.visualization.kibanaSavedObjectMeta.searchSourceJSON'
     );
 
-    if (savedSearchRefName) {
+    if (savedSearchRefName && references?.length) {
       // This visualization depends upon a saved search.
-      const savedSearch = references.find((ref) => ref.name === savedSearchRefName);
-      if (rollupSavedSearchesToFlagMap[savedSearch.id]) {
+      const savedSearch = references.find(({ name }) => name === savedSearchRefName);
+      if (savedSearch && rollupSavedSearchesToFlagMap[savedSearch.id]) {
         rollupVisualizations++;
         rollupVisualizationsFromSavedSearches++;
       }
