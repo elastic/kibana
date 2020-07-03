d9306a9360a3a9ec9c3b8e9281dec061ffe1f24c
Jean-Louis Leysens <jloleysens@gmail.com>
2020-07-03 10:35:19 +0200

    [S&R] Support data streams (#68078) (#70595)
    * Sort endpoint responses into indices and datastreams
    
    The server endpoint for policies now returns data streams and
    filters out backing indices from the indices array it returned
    previously
    
    * Refactor indices switch and field out of the step settings file
    
    * Fix indices field form behaviour
    
    * WiP on UI. Added the second table per mockup for add and edit.
    
    * add support for creating a policy that backs up data streams end to end
    
    * wip on restore flow - added data streams to server response
    
    * add logic for detecting whether an index is part of a data stream
    
    * fix public jest tests
    
    * fix server side jest tests
    
    * pivot to different solution in UI while we do not have data streams nicely separated
    
    * added data stream to snapshot summary details
    
    * move the data streams badge file closer to where it used
    
    * add data stream badge when restoring snapshots too
    
    * update restore copy
    
    * fix pattern specification in indices and data streams field
    
    * first iteration of complete policy UX
    
    * First iteration that is ready for review
    
    Given the contraints on working with data streams and indices in policies
    at the moment the simplest implementation is to just include data streams
    with indices and have the user select them there for now.
    
    The way snapshotting behaviour is currently implemented relies entirely
    on what is specified inside of "indices", this is also where data
    streams must be placed. This unfortunately means that capture patterns
    defined in indices will capture entire data streams too.
    
    * delete unused import
    
    * fix type issue in tests
    
    * added logic for rendering out previous selection as custom pattern
    
    * refactor indices fields to make component smaller
    
    * added CIT for data streams badge
    
    * Data streams > indices
    
    * updates to relevant pieces of copy
    
    * more copy updates
    
    * fix types and remove unused import
    
    * removed backing indices from restore view
    
    * Added data stream restore warning message
    
    * restore CITs
    
    * first round of copy feedback
    
    * refactor help text to provide clearer feedback, for both restore and policy forms
    
    * Restore updates
    
    - added spacer between title and data streams callout
    - added copy to the restore settings tab to indicate that settings
      also apply to backing indices
    
    * further copy refinements
    
    * second round of copy feedback
    
    * fix i18n
    
    * added comment to mock
    
    * line spacing fixes and created issue for tracking backing index discovery in snaphots
    
    * refactor collapsible list logic and tests
    
    * refactor editing managed policy check
    
    * refactor copy to be clearer about pluralisation of data streams
    
    * refactor file structure in components for data stream badge
    
    * added tests for indices and data streams field helper
    
    * refactored types and fixed i18n id per guidelines
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/snapshot_restore/common/lib/utils.ts
+++ x-pack/plugins/snapshot_restore/common/lib/utils.ts
@@ -4,8 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from './repository';
-export * from './snapshot';
-export * from './restore';
-export * from './policy';
-export * from './indices';
+export const csvToArray = (indices?: string | string[]): string[] => {
+  return indices && Array.isArray(indices)
+    ? indices
+    : typeof indices === 'string'
+    ? indices.split(',')
+    : [];
+};
