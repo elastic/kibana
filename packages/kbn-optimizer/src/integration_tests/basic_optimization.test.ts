fe07b03329223b6bd1e511c93c35242304747379
Spencer <email@spalger.com>
2020-07-06 13:08:34 -0700

    [7.8] Fix kbn/optimizer tests (#70827) (#70855)
    Co-authored-by: spalger <spalger@users.noreply.github.com>

--- packages/kbn-optimizer/src/integration_tests/basic_optimization.test.ts
+++ packages/kbn-optimizer/src/integration_tests/basic_optimization.test.ts
@@ -24,7 +24,7 @@ import { inspect } from 'util';
 
 import cpy from 'cpy';
 import del from 'del';
-import { toArray, tap } from 'rxjs/operators';
+import { toArray, tap, filter } from 'rxjs/operators';
 import { ToolingLog, REPO_ROOT } from '@kbn/dev-utils';
 import { runOptimizer, OptimizerConfig, OptimizerUpdate, logOptimizerState } from '@kbn/optimizer';
 
@@ -50,8 +50,7 @@ afterAll(async () => {
   await del(TMP_DIR);
 });
 
-// FLAKY: https://github.com/elastic/kibana/issues/70762
-it.skip('builds expected bundles, saves bundle counts to metadata', async () => {
+it('builds expected bundles, saves bundle counts to metadata', async () => {
   const config = OptimizerConfig.create({
     repoRoot: MOCK_REPO_DIR,
     pluginScanDirs: [Path.resolve(MOCK_REPO_DIR, 'plugins')],
@@ -74,7 +73,11 @@ it.skip('builds expected bundles, saves bundle counts to metadata', async () => 
     },
   });
   const msgs = await runOptimizer(config)
-    .pipe(logOptimizerState(log, config), toArray())
+    .pipe(
+      logOptimizerState(log, config),
+      filter((x) => x.event?.type !== 'worker stdio'),
+      toArray()
+    )
     .toPromise();
 
   const assert = (statement: string, truth: boolean, altStates?: OptimizerUpdate[]) => {
@@ -176,8 +179,7 @@ it.skip('builds expected bundles, saves bundle counts to metadata', async () => 
   `);
 });
 
-// FLAKY: https://github.com/elastic/kibana/issues/70764
-it.skip('uses cache on second run and exist cleanly', async () => {
+it('uses cache on second run and exist cleanly', async () => {
   const config = OptimizerConfig.create({
     repoRoot: MOCK_REPO_DIR,
     pluginScanDirs: [Path.resolve(MOCK_REPO_DIR, 'plugins')],
