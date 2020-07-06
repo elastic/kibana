3507d772516c46a96b88400926b9ea4bdcc8ffbc
Yara Tercero <yara.tercero@elastic.co>
2020-07-06 13:00:27 -0400

    updated rule exceptions_list property to include list type and updated tests

--- x-pack/plugins/lists/common/schemas/common/schemas.test.ts
+++ x-pack/plugins/lists/common/schemas/common/schemas.test.ts
@@ -9,7 +9,7 @@ import { left } from 'fp-ts/lib/Either';
 
 import { foldLeftRight, getPaths } from '../../siem_common_deps';
 
-import { operator, operator_type as operatorType } from './schemas';
+import { exceptionListType, operator, operator_type as operatorType } from './schemas';
 
 describe('Common schemas', () => {
   describe('operatorType', () => {
@@ -87,6 +87,37 @@ describe('Common schemas', () => {
       // for io-ts not yet supporting enums
       // https://github.com/gcanti/io-ts/issues/67
       const keys = Object.keys(operator.keys);
+
+      expect(keys.length).toEqual(2);
+    });
+  });
+
+  describe('exceptionListType', () => {
+    test('it should validate for "detection"', () => {
+      const payload = 'detection';
+      const decoded = exceptionListType.decode(payload);
+      const message = pipe(decoded, foldLeftRight);
+
+      expect(getPaths(left(message.errors))).toEqual([]);
+      expect(message.schema).toEqual(payload);
+    });
+
+    test('it should validate for "endpoint"', () => {
+      const payload = 'endpoint';
+      const decoded = exceptionListType.decode(payload);
+      const message = pipe(decoded, foldLeftRight);
+
+      expect(getPaths(left(message.errors))).toEqual([]);
+      expect(message.schema).toEqual(payload);
+    });
+
+    test('it should contain 2 keys', () => {
+      // Might seem like a weird test, but its meant to
+      // ensure that if exceptionListType is updated, you
+      // also update the ExceptionListTypeEnum, a workaround
+      // for io-ts not yet supporting enums
+      // https://github.com/gcanti/io-ts/issues/67
+      const keys = Object.keys(exceptionListType.keys);
 
       expect(keys.length).toEqual(2);
     });
