b4272ad14f4a9d82543ade7bf37b72b007cc332a
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2020-07-06 15:28:00 -0400

    [Ingest Manager] Rate limit agent config update

--- x-pack/plugins/ingest_manager/server/services/agents/checkin/rxjs_utils.ts
+++ x-pack/plugins/ingest_manager/server/services/agents/checkin/rxjs_utils.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import { Observable } from 'rxjs';
+import * as Rx from 'rxjs';
 
 export class AbortError extends Error {}
 
@@ -41,3 +42,63 @@ export const toPromiseAbortable = <T>(
       signal.addEventListener('abort', listener, { once: true });
     }
   });
+
+export function createLimiter(ratelimitIntervalMs: number, ratelimitRequestPerInterval: number) {
+  function createCurrentInterval() {
+    return {
+      startedAt: Rx.asyncScheduler.now(),
+      numRequests: 0,
+    };
+  }
+
+  let currentInterval: { startedAt: number; numRequests: number } = createCurrentInterval();
+  let observers: Array<[Rx.Subscriber<any>, any]> = [];
+  let timerSubsription: Rx.Subscription | undefined;
+
+  function createTimout() {
+    if (timerSubsription) {
+      return;
+    }
+    timerSubsription = Rx.asyncScheduler.schedule(() => {
+      timerSubsription = undefined;
+      currentInterval = createCurrentInterval();
+      for (const [waitingObserver, value] of observers) {
+        if (currentInterval.numRequests >= ratelimitRequestPerInterval) {
+          createTimout();
+          continue;
+        }
+        currentInterval.numRequests++;
+        waitingObserver.next(value);
+      }
+    }, ratelimitIntervalMs);
+  }
+
+  return function limit<T>(): Rx.MonoTypeOperatorFunction<T> {
+    return (observable) =>
+      new Observable<T>((observer) => {
+        const subscription = observable.subscribe({
+          next(value) {
+            if (currentInterval.numRequests < ratelimitRequestPerInterval) {
+              currentInterval.numRequests++;
+              observer.next(value);
+              return;
+            }
+
+            observers = [...observers, [observer, value]];
+            createTimout();
+          },
+          error(err) {
+            observer.error(err);
+          },
+          complete() {
+            observer.complete();
+          },
+        });
+
+        return () => {
+          observers = observers.filter((o) => o[0] !== observer);
+          subscription.unsubscribe();
+        };
+      });
+  };
+}
