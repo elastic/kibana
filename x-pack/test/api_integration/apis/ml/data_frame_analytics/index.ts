d40fdd41e070abc509d2e599036bc2a09bcc6083
James Gowdy <jgowdy@elastic.co>
2020-07-06 13:50:21 +0100

    removing debug test tag

--- x-pack/test/api_integration/apis/ml/data_frame_analytics/index.ts
+++ x-pack/test/api_integration/apis/ml/data_frame_analytics/index.ts
@@ -8,7 +8,7 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('data frame analytics', function () {
-    this.tags(['mlqa', 'james']);
+    this.tags(['mlqa']);
     loadTestFile(require.resolve('./get'));
     loadTestFile(require.resolve('./delete'));
   });
