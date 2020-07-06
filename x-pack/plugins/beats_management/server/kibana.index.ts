c51112b6ee52cdbc944dec77ef68d163662a3960
pgayvallet <pierre.gayvallet@elastic.co>
2020-07-06 15:30:48 +0200

    move all the legacy codebase to KP plugin

--- x-pack/plugins/beats_management/server/kibana.index.ts
+++ x-pack/plugins/beats_management/server/kibana.index.ts
@@ -4,10 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { KibanaLegacyServer } from './lib/adapters/framework/adapter_types';
 import { compose } from './lib/compose/kibana';
 import { initManagementServer } from './management_server';
 
-export const initServerWithKibana = (hapiServer: any) => {
-  const libs = compose(hapiServer);
+export const initServerWithKibana = (server: KibanaLegacyServer) => {
+  const libs = compose(server);
   initManagementServer(libs);
 };
