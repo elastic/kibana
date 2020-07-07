7d44d022c96f00ed6bcae9d92c714408a97402b9
Oliver Gupte <ogupte@users.noreply.github.com>
2020-07-07 04:30:14 -0700

    [APM] Adds 'Anomaly detection' settings page to create ML jobs per environment (#70560)
    * Adds 'Anomaly detection' settings page along with require API endpoints
    to list and create the apm anomaly detection jobs per environment.
    Some test data is hardcoded while the the required changes in the ML
    plugin are in flight.
    
    * Converts the environment name to a compatible ML id string and persist
    in groups array. Also adds random token to the job ID to prevent
    collisions for job ids where diffferent environment names convert to the
    same string
    
    * - Improve job creation with latest updates for the `apm_transaction` ML module
    - Implements job list in settings by reading from `custom_settings.job_tags['service.environment']`
    - Add ML module method `createModuleItem` for job configuration
    - Don't allow user to type in duplicate environments
    
    * Update x-pack/plugins/apm/public/components/app/Settings/anomaly_detection/add_environments.tsx
    
    Co-authored-by: Casper Hübertz <casper@formgeist.com>
    
    * Update x-pack/plugins/apm/public/components/app/Settings/anomaly_detection/index.tsx
    
    Co-authored-by: Casper Hübertz <casper@formgeist.com>
    
    * UX feedback, adds i18n, and handles failed state for ML jobs fetch.
    
    * - Moves get_all_environments from agent_configuration dir to common dir
    - makes the 'all' environment name ALL_OPTION_VALUE agent configuration-specific
    - replace field literals with constants
    
    * PR feedback
    
    * Adds support to create jobs for environment which are not defined.
    
    * Fixes description copy, rearranges settings links, and makes sure the 'Not defined' option is disabled if it already exists.
    
    * Only show "Not defined" in environment selector if there are actually
    documents without service.environment set
    
    * get the indexPatternName for the ML job from the set of user-definned indices
    
    * updated job_tags type definition
    
    Co-authored-by: Casper Hübertz <casper@formgeist.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/apm/server/lib/environments/get_all_environments.ts
+++ x-pack/plugins/apm/server/lib/environments/get_all_environments.ts
@@ -4,20 +4,22 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { Setup } from '../../../helpers/setup_request';
+import { Setup } from '../helpers/setup_request';
 import {
   PROCESSOR_EVENT,
   SERVICE_NAME,
   SERVICE_ENVIRONMENT,
-} from '../../../../../common/elasticsearch_fieldnames';
-import { ALL_OPTION_VALUE } from '../../../../../common/agent_configuration/all_option';
+} from '../../../common/elasticsearch_fieldnames';
+import { ENVIRONMENT_NOT_DEFINED } from '../../../common/environment_filter_values';
 
 export async function getAllEnvironments({
   serviceName,
   setup,
+  includeMissing = false,
 }: {
-  serviceName: string | undefined;
+  serviceName?: string;
   setup: Setup;
+  includeMissing?: boolean;
 }) {
   const { client, indices } = setup;
 
@@ -49,6 +51,7 @@ export async function getAllEnvironments({
           terms: {
             field: SERVICE_ENVIRONMENT,
             size: 100,
+            missing: includeMissing ? ENVIRONMENT_NOT_DEFINED : undefined,
           },
         },
       },
@@ -60,5 +63,5 @@ export async function getAllEnvironments({
     resp.aggregations?.environments.buckets.map(
       (bucket) => bucket.key as string
     ) || [];
-  return [ALL_OPTION_VALUE, ...environments];
+  return environments;
 }
