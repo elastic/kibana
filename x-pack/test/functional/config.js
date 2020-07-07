9d9633405f668e454352108e2647619025b22ab9
Bhavya RM <bhavya@elastic.co>
2020-07-07 07:52:01 -0400

    Adding test user to maps functional tests - PR 1 (#70649) (#70887)
    adding test user maps functional tests - PR1

--- x-pack/test/functional/config.js
+++ x-pack/test/functional/config.js
@@ -230,6 +230,47 @@ export default async function ({ readConfigFile }) {
             },
           ],
         },
+        global_visualize_read: {
+          kibana: [
+            {
+              feature: {
+                visualize: ['read'],
+              },
+              spaces: ['*'],
+            },
+          ],
+        },
+        global_visualize_all: {
+          kibana: [
+            {
+              feature: {
+                visualize: ['all'],
+              },
+              spaces: ['*'],
+            },
+          ],
+        },
+        global_maps_all: {
+          kibana: [
+            {
+              feature: {
+                maps: ['all'],
+              },
+              spaces: ['*'],
+            },
+          ],
+        },
+
+        geoshape_data_reader: {
+          elasticsearch: {
+            indices: [
+              {
+                names: ['geo_shapes*'],
+                privileges: ['read', 'view_index_metadata'],
+              },
+            ],
+          },
+        },
 
         global_devtools_read: {
           kibana: [
