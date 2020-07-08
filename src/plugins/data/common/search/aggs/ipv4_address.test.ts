d43c4609e1b82801c6e36cffbad77bed7e0bc541
Luke Elmers <luke.elmers@elastic.co>
2020-07-07 21:54:52 -0600

    [data.search.aggs]: Remove remaining client dependencies (#70251)

--- src/plugins/data/common/search/aggs/ipv4_address.test.ts
+++ src/plugins/data/common/search/aggs/ipv4_address.test.ts
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-// @ts-ignore
+
 import expect from '@kbn/expect';
 import { Ipv4Address } from './ipv4_address';
 
