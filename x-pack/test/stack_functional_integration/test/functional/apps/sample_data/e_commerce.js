8d1f027249b4dd359ba89d7ccb587498cc45bcc3
LeeDr <lee.drengenberg@elastic.co>
2020-07-08 09:36:03 -0500

    update methods for navigating to sample data page

--- x-pack/test/stack_functional_integration/test/functional/apps/sample_data/e_commerce.js
+++ x-pack/test/stack_functional_integration/test/functional/apps/sample_data/e_commerce.js
@@ -7,17 +7,20 @@
 export default function ({ getService, getPageObjects }) {
   describe('eCommerce Sample Data', function sampleData() {
     const browser = getService('browser');
-    const PageObjects = getPageObjects(['common']);
+    const PageObjects = getPageObjects(['common', 'home']);
     const testSubjects = getService('testSubjects');
 
     before(async () => {
       await browser.setWindowSize(1200, 800);
-      await PageObjects.common.navigateToApp('sampledata', { insertTimestamp: false });
+      await PageObjects.common.navigateToUrl('home', '/home/tutorial_directory/sampleData', {
+        useActualUrl: true,
+        insertTimestamp: false,
+      });
       await PageObjects.common.sleep(3000);
     });
 
     it('install eCommerce sample data', async function installECommerceData() {
-      await testSubjects.click('addSampleDataSetecommerce');
+      await PageObjects.home.addSampleDataSet('ecommerce');
       await PageObjects.common.sleep(5000);
       // verify it's installed by finding the remove link
       await testSubjects.find('removeSampleDataSetecommerce');
