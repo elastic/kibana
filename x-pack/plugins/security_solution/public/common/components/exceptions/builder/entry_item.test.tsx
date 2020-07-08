4d1ed2513b20b7dabea58f94afc6957ea88d8987
Yara Tercero <yara.tercero@elastic.co>
2020-07-08 19:23:59 -0400

    remove extra license header in test

--- x-pack/plugins/security_solution/public/common/components/exceptions/builder/entry_item.test.tsx
+++ x-pack/plugins/security_solution/public/common/components/exceptions/builder/entry_item.test.tsx
@@ -4,12 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 import { mount } from 'enzyme';
 import React from 'react';
 import { EuiComboBox, EuiComboBoxOptionOption } from '@elastic/eui';
