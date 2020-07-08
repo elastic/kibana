9d285c10b4cbb3381037eba4fe7c1da0f166b622
Angela Chuang <yi-chun.chuang@elastic.co>
2020-07-08 13:41:11 +0100

    update module path

--- x-pack/plugins/security_solution/server/lib/timeline/routes/utils/get_timelines_to_install.ts
+++ x-pack/plugins/security_solution/server/lib/timeline/routes/utils/get_timelines_to_install.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { ImportTimelinesSchema } from '../import_timelines_schema';
+import { ImportTimelinesSchema } from '../schemas/import_timelines_schema';
 import { TimelineSavedObject } from '../../../../../common/types/timeline';
 
 export const getTimelinesToInstall = (
