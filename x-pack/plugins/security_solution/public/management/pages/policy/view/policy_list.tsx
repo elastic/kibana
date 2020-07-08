5cae54078827b4d0ff393021e5397693d337d383
Bohdan Tsymbala <bohdan.tsymbala@elastic.co>
2020-07-08 19:01:37 +0200

    Fixing the consistency of util bar in endpoints and policies pages.

--- x-pack/plugins/security_solution/public/management/pages/policy/view/policy_list.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/policy_list.tsx
@@ -22,6 +22,7 @@ import {
   EuiCallOut,
   EuiSpacer,
   EuiButton,
+  EuiHorizontalRule,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
@@ -406,19 +407,19 @@ export const PolicyList = React.memo(() => {
             />
           </EuiButton>
         }
-        bodyHeader={
-          policyItems &&
-          policyItems.length > 0 && (
-            <EuiText color="subdued" data-test-subj="policyTotalCount">
+      >
+        {policyItems && policyItems.length > 0 && (
+          <>
+            <EuiText color="subdued" data-test-subj="policyTotalCount" size="xs">
               <FormattedMessage
                 id="xpack.securitySolution.endpoint.policyList.viewTitleTotalCount"
                 defaultMessage="{totalItemCount, plural, one {# Policy} other {# Policies}}"
                 values={{ totalItemCount }}
               />
             </EuiText>
-          )
-        }
-      >
+            <EuiHorizontalRule margin="xs" />
+          </>
+        )}
         {useMemo(() => {
           return (
             <>
