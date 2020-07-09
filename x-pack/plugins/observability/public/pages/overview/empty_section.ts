9b27af526c7334676f81d4bb30181f32d8407253
Brandon Morelli <brandon.morelli@elastic.co>
2020-07-09 14:19:28 -0700

    get —> see

--- x-pack/plugins/observability/public/pages/overview/empty_section.ts
+++ x-pack/plugins/observability/public/pages/overview/empty_section.ts
@@ -77,7 +77,7 @@ export const getEmptySections = ({ core }: { core: AppMountContext['core'] }): I
       icon: 'watchesApp',
       description: i18n.translate('xpack.observability.emptySection.apps.alert.description', {
         defaultMessage:
-          'Are 503 errors stacking up? Are services responding? Is CPU and RAM utilization jumping? Get warnings as they happen—not as part of the post-mortem.',
+          'Are 503 errors stacking up? Are services responding? Is CPU and RAM utilization jumping? See warnings as they happen—not as part of the post-mortem.',
       }),
       linkTitle: i18n.translate('xpack.observability.emptySection.apps.alert.link', {
         defaultMessage: 'Create alert',
