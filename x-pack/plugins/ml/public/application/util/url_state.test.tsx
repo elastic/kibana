7dad41b92d8b4c1841a8273e28dcf47815892a6a
Walter Rafelsberger <walter@elastic.co>
2020-07-09 10:48:49 +0200

    [ML] Improvements for urlState hook. (#70576) (#71156)
    Makes two improvements to the urlState hook (also known as appState in some places):
    
    - There was always a risk to run into a race condition because setUrlState could refer to a stale version of the state to act upon, for example if two calls were done in parallel. This is now fixed by using a local state copy of what we get from useLocation(). This allows us to use the callback version of useState's set function so we can make sure we always modify the latest state.
    - Calls to history.push() are now gated by a check if the change actually referred to the corresponding instance of urlState (either _g or _a), this should reduce the updates resulting re-renders.
    
    The two changes should make the use of setUrlState more safe against the pitfalls (race conditions/stale updates/lots of rerenders) we previously faced.
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/ml/public/application/util/url_state.test.tsx
+++ x-pack/plugins/ml/public/application/util/url_state.test.tsx
@@ -4,8 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { renderHook, act } from '@testing-library/react-hooks';
-import { getUrlState, useUrlState } from './url_state';
+import React, { FC } from 'react';
+import { render, act } from '@testing-library/react';
+import { parseUrlState, useUrlState, UrlStateProvider } from './url_state';
 
 const mockHistoryPush = jest.fn();
 
@@ -22,7 +23,7 @@ jest.mock('react-router-dom', () => ({
 describe('getUrlState', () => {
   test('properly decode url with _g and _a', () => {
     expect(
-      getUrlState(
+      parseUrlState(
         "?_a=(mlExplorerFilter:(),mlExplorerSwimlane:(viewByFieldName:action),query:(query_string:(analyze_wildcard:!t,query:'*')))&_g=(ml:(jobIds:!(dec-2)),refreshInterval:(display:Off,pause:!f,value:0),time:(from:'2019-01-01T00:03:40.000Z',mode:absolute,to:'2019-08-30T11:55:07.000Z'))&savedSearchId=571aaf70-4c88-11e8-b3d7-01146121b73d"
       )
     ).toEqual({
@@ -64,13 +65,19 @@ describe('useUrlState', () => {
   });
 
   test('pushes a properly encoded search string to history', () => {
-    const { result } = renderHook(() => useUrlState('_a'));
+    const TestComponent: FC = () => {
+      const [, setUrlState] = useUrlState('_a');
+      return <button onClick={() => setUrlState({ query: {} })}>ButtonText</button>;
+    };
+
+    const { getByText } = render(
+      <UrlStateProvider>
+        <TestComponent />
+      </UrlStateProvider>
+    );
 
     act(() => {
-      const [, setUrlState] = result.current;
-      setUrlState({
-        query: {},
-      });
+      getByText('ButtonText').click();
     });
 
     expect(mockHistoryPush).toHaveBeenCalledWith({
