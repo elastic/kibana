94df85d59cee56b22a7e86ec5b9064fe22febb0d
Ryland Herrick <ryalnd@gmail.com>
2020-07-09 17:18:06 -0500

    Add frontend plugin for lists
    We need this to deteremine in security_solution whether lists is enabled
    or not. There's no other functionality here, just boilerplate.

---
+++ x-pack/plugins/lists/public/types.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PluginSetup {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PluginStart {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SetupPlugins {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface StartPlugins {}
