cbd8b5e468a95dfb9a3a680b7bf30c6c0ecf3d34
scottybollinger <scotty.bollinger@elastic.co>
2020-07-08 19:13:12 -0500

    Fix failing test

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/components/overview/onboarding_steps.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/components/overview/onboarding_steps.test.tsx
@@ -80,7 +80,7 @@ describe('OnboardingSteps', () => {
 
     expect(wrapper.find(OnboardingCard).prop('description')).toStrictEqual(
       <FormattedMessage
-        defaultMessage="You have added {sourcesCount} shared {sourcesCount, number} {sourcesCount, plural, one {source} other {sources} }. Happy searching"
+        defaultMessage="You have added {sourcesCount} shared {sourcesCount, number} {sourcesCount, plural, one {source} other {sources} }. Happy searching."
         id="xpack.enterpriseSearch.workplaceSearch.sourcesOnboardingCard.description"
         values={{ sourcesCount: 1 }}
       />
