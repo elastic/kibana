6e2d2e062e382e858cbb9dc9d6559359fffdc15a
Tyler Smalley <tyler.smalley@elastic.co>
2020-07-09 08:09:40 -0700

    Reverts building all-platforms used for testing
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- test/scripts/jenkins_build_kibana.sh
+++ test/scripts/jenkins_build_kibana.sh
@@ -25,6 +25,5 @@ yarn run grunt functionalTests:ensureAllTestsInCiGroup;
 # Do not build kibana for code coverage run
 if [[ -z "$CODE_COVERAGE" ]] ; then
   echo " -> building and extracting OSS Kibana distributable for use in functional tests"
-  # TEMP: build all platforms for PR testing
-  node scripts/build --debug --oss --all-platforms
+  node scripts/build --debug --oss
 fi
