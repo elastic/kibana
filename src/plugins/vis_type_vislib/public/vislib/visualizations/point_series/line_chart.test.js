7f38a1faea9aafbc276069623b54df8df0f03865
Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>
2020-07-09 16:10:34 +0300

    Migrated vislib tests to the NP (#70309) (#71206)
    * Migrated vislib tests to the NP
    
    * fixed tests
    
    * Fixed tests
    
    * Fixed test

--- src/plugins/vis_type_vislib/public/vislib/visualizations/point_series/line_chart.test.js
+++ src/plugins/vis_type_vislib/public/vislib/visualizations/point_series/line_chart.test.js
@@ -18,18 +18,22 @@
  */
 
 import d3 from 'd3';
-import expect from '@kbn/expect';
 import $ from 'jquery';
 import _ from 'lodash';
+import {
+  setHTMLElementClientSizes,
+  setSVGElementGetBBox,
+  setSVGElementGetComputedTextLength,
+} from '../../../../../../test_utils/public';
 
 // Data
-import seriesPos from '../../../../../../../plugins/vis_type_vislib/public/fixtures/mock_data/date_histogram/_series';
-import seriesPosNeg from '../../../../../../../plugins/vis_type_vislib/public/fixtures/mock_data/date_histogram/_series_pos_neg';
-import seriesNeg from '../../../../../../../plugins/vis_type_vislib/public/fixtures/mock_data/date_histogram/_series_neg';
-import histogramColumns from '../../../../../../../plugins/vis_type_vislib/public/fixtures/mock_data/histogram/_columns';
-import rangeRows from '../../../../../../../plugins/vis_type_vislib/public/fixtures/mock_data/range/_rows';
-import termSeries from '../../../../../../../plugins/vis_type_vislib/public/fixtures/mock_data/terms/_series';
-import { getMockUiState } from '../../../../../../../plugins/vis_type_vislib/public/fixtures/mocks';
+import seriesPos from '../../../fixtures/mock_data/date_histogram/_series';
+import seriesPosNeg from '../../../fixtures/mock_data/date_histogram/_series_pos_neg';
+import seriesNeg from '../../../fixtures/mock_data/date_histogram/_series_neg';
+import histogramColumns from '../../../fixtures/mock_data/histogram/_columns';
+import rangeRows from '../../../fixtures/mock_data/range/_rows';
+import termSeries from '../../../fixtures/mock_data/terms/_series';
+import { getMockUiState } from '../../../fixtures/mocks';
 import { getVis } from '../_vis_fixture';
 
 const dataTypes = [
@@ -41,7 +45,23 @@ const dataTypes = [
   ['term series', termSeries],
 ];
 
+let mockedHTMLElementClientSizes;
+let mockedSVGElementGetBBox;
+let mockedSVGElementGetComputedTextLength;
+
 describe('Vislib Line Chart', function () {
+  beforeAll(() => {
+    mockedHTMLElementClientSizes = setHTMLElementClientSizes(512, 512);
+    mockedSVGElementGetBBox = setSVGElementGetBBox(100);
+    mockedSVGElementGetComputedTextLength = setSVGElementGetComputedTextLength(100);
+  });
+
+  afterAll(() => {
+    mockedHTMLElementClientSizes.mockRestore();
+    mockedSVGElementGetBBox.mockRestore();
+    mockedSVGElementGetComputedTextLength.mockRestore();
+  });
+
   dataTypes.forEach(function (type) {
     const name = type[0];
     const data = type[1];
@@ -94,37 +114,37 @@ describe('Vislib Line Chart', function () {
         // listeners, however, I was not able to test for the listener
         // function being present. I will need to update this test
         // in the future.
-        it('should attach a brush g element', function () {
+        test('should attach a brush g element', function () {
           vis.handler.charts.forEach(function () {
-            expect(onBrush).to.be(true);
+            expect(onBrush).toBe(true);
           });
         });
 
-        it('should attach a click event', function () {
+        test('should attach a click event', function () {
           vis.handler.charts.forEach(function () {
-            expect(onClick).to.be(true);
+            expect(onClick).toBe(true);
           });
         });
 
-        it('should attach a hover event', function () {
+        test('should attach a hover event', function () {
           vis.handler.charts.forEach(function () {
-            expect(onMouseOver).to.be(true);
+            expect(onMouseOver).toBe(true);
           });
         });
       });
 
       describe('addCircles method', function () {
-        it('should append circles', function () {
+        test('should append circles', function () {
           vis.handler.charts.forEach(function (chart) {
-            expect($(chart.chartEl).find('circle').length).to.be.greaterThan(0);
+            expect($(chart.chartEl).find('circle').length).toBeGreaterThan(0);
           });
         });
       });
 
       describe('addLines method', function () {
-        it('should append a paths', function () {
+        test('should append a paths', function () {
           vis.handler.charts.forEach(function (chart) {
-            expect($(chart.chartEl).find('path').length).to.be.greaterThan(0);
+            expect($(chart.chartEl).find('path').length).toBeGreaterThan(0);
           });
         });
       });
@@ -132,7 +152,7 @@ describe('Vislib Line Chart', function () {
       // Cannot seem to get these tests to work on the box
       // They however pass in the browsers
       //describe('addClipPath method', function () {
-      //  it('should append a clipPath', function () {
+      //  test('should append a clipPath', function () {
       //    vis.handler.charts.forEach(function (chart) {
       //      expect($(chart.chartEl).find('clipPath').length).to.be(1);
       //    });
@@ -140,27 +160,27 @@ describe('Vislib Line Chart', function () {
       //});
 
       describe('draw method', function () {
-        it('should return a function', function () {
+        test('should return a function', function () {
           vis.handler.charts.forEach(function (chart) {
-            expect(chart.draw()).to.be.a(Function);
+            expect(chart.draw()).toBeInstanceOf(Function);
           });
         });
 
-        it('should return a yMin and yMax', function () {
+        test('should return a yMin and yMax', function () {
           vis.handler.charts.forEach(function (chart) {
             const yAxis = chart.handler.valueAxes[0];
             const domain = yAxis.getScale().domain();
-            expect(domain[0]).to.not.be(undefined);
-            expect(domain[1]).to.not.be(undefined);
+            expect(domain[0]).not.toBe(undefined);
+            expect(domain[1]).not.toBe(undefined);
           });
         });
 
-        it('should render a zero axis line', function () {
+        test('should render a zero axis line', function () {
           vis.handler.charts.forEach(function (chart) {
             const yAxis = chart.handler.valueAxes[0];
 
             if (yAxis.yMin < 0 && yAxis.yMax > 0) {
-              expect($(chart.chartEl).find('line.zero-line').length).to.be(1);
+              expect($(chart.chartEl).find('line.zero-line').length).toBe(1);
             }
           });
         });
@@ -172,14 +192,14 @@ describe('Vislib Line Chart', function () {
           vis.render(data, mockUiState);
         });
 
-        it('should return yAxis extents equal to data extents', function () {
+        test('should return yAxis extents equal to data extents', function () {
           vis.handler.charts.forEach(function (chart) {
             const yAxis = chart.handler.valueAxes[0];
             const min = vis.handler.valueAxes[0].axisScale.getYMin();
             const max = vis.handler.valueAxes[0].axisScale.getYMax();
             const domain = yAxis.getScale().domain();
-            expect(domain[0]).to.equal(min);
-            expect(domain[1]).to.equal(max);
+            expect(domain[0]).toEqual(min);
+            expect(domain[1]).toEqual(max);
           });
         });
       });
@@ -191,21 +211,21 @@ describe('Vislib Line Chart', function () {
             vis.render(data, mockUiState);
           });
 
-          it('should return yAxis extents equal to data extents with boundsMargin', function () {
+          test('should return yAxis extents equal to data extents with boundsMargin', function () {
             vis.handler.charts.forEach(function (chart) {
               const yAxis = chart.handler.valueAxes[0];
               const min = vis.handler.valueAxes[0].axisScale.getYMin();
               const max = vis.handler.valueAxes[0].axisScale.getYMax();
               const domain = yAxis.getScale().domain();
               if (min < 0 && max < 0) {
-                expect(domain[0]).to.equal(min);
-                expect(domain[1] - boundsMarginValue).to.equal(max);
+                expect(domain[0]).toEqual(min);
+                expect(domain[1] - boundsMarginValue).toEqual(max);
               } else if (min > 0 && max > 0) {
-                expect(domain[0] + boundsMarginValue).to.equal(min);
-                expect(domain[1]).to.equal(max);
+                expect(domain[0] + boundsMarginValue).toEqual(min);
+                expect(domain[1]).toEqual(max);
               } else {
-                expect(domain[0]).to.equal(min);
-                expect(domain[1]).to.equal(max);
+                expect(domain[0]).toEqual(min);
+                expect(domain[1]).toEqual(max);
               }
             });
           });
