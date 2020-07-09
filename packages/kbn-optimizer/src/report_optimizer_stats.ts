2a69e2c6d52c050d2a985c0b38770a547a57b984
spalger <spalger@users.noreply.github.com>
2020-07-09 13:32:43 -0700

    [kbn/optimizer] report sizes of assets produced by optimizer

--- packages/kbn-optimizer/src/report_optimizer_stats.ts
+++ packages/kbn-optimizer/src/report_optimizer_stats.ts
@@ -17,12 +17,18 @@
  * under the License.
  */
 
+import { Stats } from 'fs';
+
 import { materialize, mergeMap, dematerialize } from 'rxjs/operators';
 import { CiStatsReporter } from '@kbn/dev-utils';
+import fastGlob, { Entry } from 'fast-glob';
 
 import { OptimizerUpdate$ } from './run_optimizer';
 import { OptimizerState, OptimizerConfig } from './optimizer';
 import { pipeClosure } from './common';
+
+const flatten = <T>(arr: Array<T | T[]>): T[] =>
+  arr.reduce((acc: T[], item) => acc.concat(item), []);
 
 export function reportOptimizerStats(reporter: CiStatsReporter, config: OptimizerConfig) {
   return pipeClosure((update$: OptimizerUpdate$) => {
@@ -36,16 +42,60 @@ export function reportOptimizerStats(reporter: CiStatsReporter, config: Optimize
 
         if (n.kind === 'C' && lastState) {
           await reporter.metrics(
-            config.bundles.map((bundle) => {
-              // make the cache read from the cache file since it was likely updated by the worker
-              bundle.cache.refresh();
+            flatten(
+              config.bundles.map((bundle) => {
+                // make the cache read from the cache file since it was likely updated by the worker
+                bundle.cache.refresh();
 
-              return {
-                group: `@kbn/optimizer bundle module count`,
-                id: bundle.id,
-                value: bundle.cache.getModuleCount() || 0,
-              };
-            })
+                const outputFiles = fastGlob.sync('**/*', {
+                  cwd: bundle.outputDir,
+                  onlyFiles: true,
+                  unique: true,
+                  stats: true,
+                  absolute: false,
+                  ignore: ['!**/*.map'],
+                });
+
+                const entryName = `${bundle.id}.${bundle.type}.js`;
+                const entry = outputFiles.find((f) => f.path === entryName);
+                if (!entry) {
+                  throw new Error(
+                    `Unable to find bundle entry named [${entryName}] in [${bundle.outputDir}]`
+                  );
+                }
+
+                const chunkPrefix = `${bundle.id}.chunk.`;
+                const asyncChunks = outputFiles.filter((f) => f.path.startsWith(chunkPrefix));
+                const miscFiles = outputFiles.filter(
+                  (f) => f !== entry && !asyncChunks.includes(f)
+                );
+                const sumSize = (files: Entry[]) =>
+                  files.reduce((acc: number, f) => acc + f.stats!.size, 0);
+
+                return [
+                  {
+                    group: `@kbn/optimizer bundle module count`,
+                    id: bundle.id,
+                    value: bundle.cache.getModuleCount() || 0,
+                  },
+                  {
+                    group: `page load bundle size`,
+                    id: bundle.id,
+                    value: entry.stats!.size,
+                  },
+                  {
+                    group: `async chunks size`,
+                    id: bundle.id,
+                    value: sumSize(asyncChunks),
+                  },
+                  {
+                    group: `miscellaneous assets size`,
+                    id: bundle.id,
+                    value: sumSize(miscFiles),
+                  },
+                ];
+              })
+            )
           );
         }
 
