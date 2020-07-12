a64f9c3e82c708e11c6af16b0ad64253b12a551e
Christiane Heiligers <christiane.heiligers@elastic.co>
2020-07-11 17:59:32 -0700

    imports schema from common

--- x-pack/plugins/apm/server/lib/apm_telemetry/index.ts
+++ x-pack/plugins/apm/server/lib/apm_telemetry/index.ts
@@ -6,7 +6,10 @@
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { CoreSetup, Logger } from 'src/core/server';
-import { UsageCollectionSetup } from 'src/plugins/usage_collection/server';
+import {
+  UsageCollectionSetup,
+  Collector,
+} from 'src/plugins/usage_collection/server';
 import { APMConfig } from '../..';
 import {
   TaskManagerSetupContract,
@@ -16,13 +19,13 @@ import {
   APM_TELEMETRY_SAVED_OBJECT_ID,
   APM_TELEMETRY_SAVED_OBJECT_TYPE,
 } from '../../../common/apm_saved_object_constants';
+import { apmTelemetrySchema } from '../../../common/apm_telemetry';
 import { getInternalSavedObjectsClient } from '../helpers/get_internal_saved_objects_client';
 import { getApmIndices } from '../settings/apm_indices/get_apm_indices';
 import {
   collectDataTelemetry,
   CollectTelemetryParams,
 } from './collect_data_telemetry';
-import { APMTelemetry } from './types';
 
 const APM_TELEMETRY_TASK_NAME = 'apm-telemetry-task';
 
@@ -98,305 +101,7 @@ export async function createApmTelemetry({
 
   const collector = usageCollector.makeUsageCollector({
     type: 'apm',
-    schema: {
-      agents: {
-        dotnet: {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-        go: {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-        java: {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-        'js-base': {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-        nodejs: {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-        python: {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-        ruby: {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-        'rum-js': {
-          agent: {
-            version: { type: 'keyword' },
-          },
-          service: {
-            framework: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            language: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-            runtime: {
-              composite: { type: 'keyword' },
-              name: { type: 'keyword' },
-              version: { type: 'keyword' },
-            },
-          },
-        },
-      },
-      cloud: {
-        availability_zone: { type: 'keyword' },
-        provider: { type: 'keyword' },
-        region: { type: 'keyword' },
-      },
-      counts: {
-        agent_configuration: {
-          all: { type: 'long' },
-        },
-        error: {
-          '1d': { type: 'long' },
-          all: { type: 'long' },
-        },
-        max_error_groups_per_service: {
-          '1d': { type: 'long' },
-        },
-        max_transaction_groups_per_service: {
-          '1d': { type: 'long' },
-        },
-        metric: {
-          '1d': { type: 'long' },
-          all: { type: 'long' },
-        },
-        onboarding: {
-          '1d': { type: 'long' },
-          all: { type: 'long' },
-        },
-        services: {
-          '1d': { type: 'long' },
-        },
-        sourcemap: {
-          '1d': { type: 'long' },
-          all: { type: 'long' },
-        },
-        span: {
-          '1d': { type: 'long' },
-          all: { type: 'long' },
-        },
-        traces: {
-          '1d': { type: 'long' },
-        },
-        transaction: {
-          '1d': { type: 'long' },
-          all: { type: 'long' },
-        },
-      },
-      cardinality: {
-        user_agent: {
-          original: {
-            all_agents: {
-              '1d': { type: 'long' },
-            },
-            rum: {
-              '1d': { type: 'long' },
-            },
-          },
-        },
-        transaction: {
-          name: {
-            all_agents: {
-              '1d': { type: 'long' },
-            },
-            rum: {
-              '1d': { type: 'long' },
-            },
-          },
-        },
-      },
-      has_any_services: {
-        type: 'boolean',
-      },
-      indices: {
-        all: {
-          total: {
-            docs: { count: { type: 'long' } },
-            store: { size_in_bytes: { type: 'long' } },
-          },
-        },
-        shards: { total: { type: 'long' } },
-      },
-      integrations: {
-        ml: { all_jobs_count: { type: 'long' } },
-      },
-      retainment: {
-        error: { ms: { type: 'long' } },
-        metric: { ms: { type: 'long' } },
-        onboarding: { ms: { type: 'long' } },
-        span: { ms: { type: 'long' } },
-        transaction: { ms: { type: 'long' } },
-      },
-      services_per_agent: {
-        dotnet: { type: 'long' },
-        go: { type: 'long' },
-        java: { type: 'long' },
-        'js-base': { type: 'long' },
-        nodejs: { type: 'long' },
-        python: { type: 'long' },
-        ruby: { type: 'long' },
-        'rum-js': { type: 'long' },
-      },
-      tasks: {
-        agent_configuration: { took: { ms: { type: 'long' } } },
-        agents: { took: { ms: { type: 'long' } } },
-        cardinality: { took: { ms: { type: 'long' } } },
-        cloud: { took: { ms: { type: 'long' } } },
-        groupings: { took: { ms: { type: 'long' } } },
-        indices_stats: { took: { ms: { type: 'long' } } },
-        integrations: { took: { ms: { type: 'long' } } },
-        processor_events: { took: { ms: { type: 'long' } } },
-        services: { took: { ms: { type: 'long' } } },
-        versions: { took: { ms: { type: 'long' } } },
-      },
-      version: {
-        apm_server: {
-          major: { type: 'long' },
-          minor: { type: 'long' },
-          patch: { type: 'long' },
-        },
-      },
-    },
+    schema: apmTelemetrySchema,
     fetch: async () => {
       try {
         const data = (
@@ -406,7 +111,7 @@ export async function createApmTelemetry({
           )
         ).attributes;
 
-        return data as APMTelemetry;
+        return data;
       } catch (err) {
         if (err.output?.statusCode === 404) {
           // task has not run yet, so no saved object to return
@@ -416,7 +121,7 @@ export async function createApmTelemetry({
       }
     },
     isReady: () => true,
-  });
+  }) as Collector;
 
   usageCollector.registerCollector(collector);
 
