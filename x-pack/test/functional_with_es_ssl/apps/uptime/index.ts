074dec84d8309df96ba85a117c38c212bc3c84c2
Shahzad <shahzad31comp@gmail.com>
2020-07-13 23:29:29 +0200

    update

--- x-pack/test/functional_with_es_ssl/apps/uptime/index.ts
+++ x-pack/test/functional_with_es_ssl/apps/uptime/index.ts
@@ -19,7 +19,7 @@ export default ({ getService, loadTestFile }: FtrProviderContext) => {
         await esArchiver.load(ARCHIVE);
         await kibanaServer.uiSettings.replace({ 'dateFormat:tz': 'UTC' });
       });
-      // after(async () => await esArchiver.unload(ARCHIVE));
+      after(async () => await esArchiver.unload(ARCHIVE));
 
       loadTestFile(require.resolve('./alert_flyout'));
       loadTestFile(require.resolve('./anomaly_alert'));
