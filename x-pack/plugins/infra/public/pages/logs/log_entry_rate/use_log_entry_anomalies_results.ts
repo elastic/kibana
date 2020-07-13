9a95520d7db3c3e12bc923162ec16ded7c2ac1d9
Kerry Gallagher <k.gallagher.05@gmail.com>
2020-07-13 11:03:02 +0100

    Move anomalies hook state over to a reducer for better clarity and bug fixes

--- x-pack/plugins/infra/public/pages/logs/log_entry_rate/use_log_entry_anomalies_results.ts
+++ x-pack/plugins/infra/public/pages/logs/log_entry_rate/use_log_entry_anomalies_results.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { useMemo, useState, useCallback, useEffect } from 'react';
+import { useMemo, useState, useCallback, useEffect, useReducer } from 'react';
 
 import { LogEntryAnomaly } from '../../../../common/http_api';
 import { useTrackedPromise } from '../../../utils/use_tracked_promise';
@@ -24,41 +24,124 @@ interface PaginationCursors {
   nextPageCursor: PaginationCursor;
 }
 
+interface ReducerState {
+  page: number;
+  lastReceivedCursors: PaginationCursors | undefined;
+  paginationCursor: Pagination['cursor'] | undefined;
+  hasNextPage: boolean;
+  paginationOptions: PaginationOptions;
+  sortOptions: Sort;
+  timeRange: {
+    start: number;
+    end: number;
+  };
+}
+
+type ReducerStateDefaults = Pick<
+  ReducerState,
+  'page' | 'lastReceivedCursors' | 'paginationCursor' | 'hasNextPage'
+>;
+
+type ReducerAction =
+  | { type: 'changePaginationOptions'; payload: { paginationOptions: PaginationOptions } }
+  | { type: 'changeSortOptions'; payload: { sortOptions: Sort } }
+  | { type: 'fetchNextPage' }
+  | { type: 'fetchPreviousPage' }
+  | { type: 'changeHasNextPage'; payload: { hasNextPage: boolean } }
+  | { type: 'changeLastReceivedCursors'; payload: { lastReceivedCursors: PaginationCursors } }
+  | { type: 'changeTimeRange'; payload: { timeRange: { start: number; end: number } } };
+
+const stateReducer = (state: ReducerState, action: ReducerAction): ReducerState => {
+  const resetPagination = {
+    page: 1,
+    paginationCursor: undefined,
+  };
+  switch (action.type) {
+    case 'changePaginationOptions':
+      return {
+        ...state,
+        ...resetPagination,
+        ...action.payload,
+      };
+    case 'changeSortOptions':
+      return {
+        ...state,
+        ...resetPagination,
+        ...action.payload,
+      };
+    case 'changeHasNextPage':
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case 'changeLastReceivedCursors':
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case 'fetchNextPage':
+      return state.lastReceivedCursors
+        ? {
+            ...state,
+            page: state.page + 1,
+            paginationCursor: { searchAfter: state.lastReceivedCursors.nextPageCursor },
+          }
+        : state;
+    case 'fetchPreviousPage':
+      return state.lastReceivedCursors
+        ? {
+            ...state,
+            page: state.page - 1,
+            paginationCursor: { searchBefore: state.lastReceivedCursors.previousPageCursor },
+          }
+        : state;
+    case 'changeTimeRange':
+      return {
+        ...state,
+        ...resetPagination,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const STATE_DEFAULTS: ReducerStateDefaults = {
+  // NOTE: This piece of state is purely for the client side, it could be extracted out of the hook.
+  page: 1,
+  // Cursor from the last request
+  lastReceivedCursors: undefined,
+  // Cursor to use for the next request. For the first request, and therefore not paging, this will be undefined.
+  paginationCursor: undefined,
+  hasNextPage: false,
+};
+
 export const useLogEntryAnomaliesResults = ({
   endTime,
   startTime,
   sourceId,
-  lastChangedTime,
   defaultSortOptions,
   defaultPaginationOptions,
 }: {
   endTime: number;
   startTime: number;
   sourceId: string;
-  lastChangedTime: number;
   defaultSortOptions: Sort;
   defaultPaginationOptions: Pick<Pagination, 'pageSize'>;
 }) => {
-  // Pagination
-  const [page, setPage] = useState(1);
-  const [paginationOptions, setPaginationOptions] = useState<PaginationOptions>(
-    defaultPaginationOptions
-  );
-  // Cursor from the last request
-  const [lastReceivedCursors, setLastReceivedCursors] = useState<PaginationCursors | undefined>();
-  // Cursor to use for the next request. For the first request, and therefore not paging, this will be undefined.
-  const [paginationCursor, setPaginationCursor] = useState<Pagination['cursor'] | undefined>(
-    undefined
-  );
-  const [hasNextPage, setHasNextPage] = useState<boolean>(false);
+  const initStateReducer = (stateDefaults: ReducerStateDefaults): ReducerState => {
+    return {
+      ...stateDefaults,
+      paginationOptions: defaultPaginationOptions,
+      sortOptions: defaultSortOptions,
+      timeRange: {
+        start: startTime,
+        end: endTime,
+      },
+    };
+  };
 
-  // Sort
-  const [sortOptions, setSortOptions] = useState<Sort>(defaultSortOptions);
-
-  const resetPagination = useCallback(() => {
-    setPage(1);
-    setPaginationCursor(undefined);
-  }, [setPage, setPaginationCursor]);
+  const [reducerState, dispatch] = useReducer(stateReducer, STATE_DEFAULTS, initStateReducer);
 
   const [logEntryAnomalies, setLogEntryAnomalies] = useState<LogEntryAnomalies>([]);
 
@@ -66,72 +149,92 @@ export const useLogEntryAnomaliesResults = ({
     {
       cancelPreviousOn: 'creation',
       createPromise: async () => {
-        return await callGetLogEntryAnomaliesAPI(sourceId, startTime, endTime, sortOptions, {
-          ...paginationOptions,
-          cursor: paginationCursor,
-        });
+        const {
+          timeRange: { start: queryStartTime, end: queryEndTime },
+          sortOptions,
+          paginationOptions,
+          paginationCursor,
+        } = reducerState;
+        return await callGetLogEntryAnomaliesAPI(
+          sourceId,
+          queryStartTime,
+          queryEndTime,
+          sortOptions,
+          {
+            ...paginationOptions,
+            cursor: paginationCursor,
+          }
+        );
       },
       onResolve: ({ data: { anomalies, paginationCursors: requestCursors, hasMoreEntries } }) => {
+        const { paginationCursor } = reducerState;
         if (requestCursors) {
-          setLastReceivedCursors(requestCursors);
+          dispatch({
+            type: 'changeLastReceivedCursors',
+            payload: { lastReceivedCursors: requestCursors },
+          });
         }
         // Check if we have more "next" entries. "Page" covers the "previous" scenario,
         // since we need to know the page we're on anyway.
         if (!paginationCursor || (paginationCursor && 'searchAfter' in paginationCursor)) {
-          setHasNextPage(hasMoreEntries);
+          dispatch({ type: 'changeHasNextPage', payload: { hasNextPage: hasMoreEntries } });
         } else if (paginationCursor && 'searchBefore' in paginationCursor) {
           // We've requested a previous page, therefore there is a next page.
-          setHasNextPage(true);
+          dispatch({ type: 'changeHasNextPage', payload: { hasNextPage: true } });
         }
         setLogEntryAnomalies(anomalies);
       },
     },
-    [endTime, sourceId, startTime, sortOptions, paginationOptions, setHasNextPage, paginationCursor]
+    [
+      sourceId,
+      dispatch,
+      reducerState.timeRange,
+      reducerState.sortOptions,
+      reducerState.paginationOptions,
+      reducerState.paginationCursor,
+    ]
   );
 
   const changeSortOptions = useCallback(
     (nextSortOptions: Sort) => {
-      resetPagination();
-      setSortOptions(nextSortOptions);
+      dispatch({ type: 'changeSortOptions', payload: { sortOptions: nextSortOptions } });
     },
-    [setSortOptions, resetPagination]
+    [dispatch]
   );
 
   const changePaginationOptions = useCallback(
     (nextPaginationOptions: PaginationOptions) => {
-      resetPagination();
-      setPaginationOptions(nextPaginationOptions);
+      dispatch({
+        type: 'changePaginationOptions',
+        payload: { paginationOptions: nextPaginationOptions },
+      });
     },
-    [resetPagination, setPaginationOptions]
+    [dispatch]
   );
 
+  // Time range has changed
   useEffect(() => {
-    // Time range has changed
-    resetPagination();
-  }, [lastChangedTime, resetPagination]);
+    dispatch({
+      type: 'changeTimeRange',
+      payload: { timeRange: { start: startTime, end: endTime } },
+    });
+  }, [startTime, endTime]);
 
   useEffect(() => {
-    // Refetch entries when options change
     getLogEntryAnomalies();
-  }, [sortOptions, paginationOptions, getLogEntryAnomalies, paginationCursor]);
+  }, [getLogEntryAnomalies]);
 
   const handleFetchNextPage = useCallback(() => {
-    if (lastReceivedCursors) {
-      setPage(page + 1);
-      setPaginationCursor({
-        searchAfter: lastReceivedCursors.nextPageCursor,
-      });
+    if (reducerState.lastReceivedCursors) {
+      dispatch({ type: 'fetchNextPage' });
     }
-  }, [setPage, lastReceivedCursors, page]);
+  }, [dispatch, reducerState]);
 
   const handleFetchPreviousPage = useCallback(() => {
-    if (lastReceivedCursors) {
-      setPage(page - 1);
-      setPaginationCursor({
-        searchBefore: lastReceivedCursors.previousPageCursor,
-      });
+    if (reducerState.lastReceivedCursors) {
+      dispatch({ type: 'fetchPreviousPage' });
     }
-  }, [setPage, lastReceivedCursors, page]);
+  }, [dispatch, reducerState]);
 
   const isLoadingLogEntryAnomalies = useMemo(
     () => getLogEntryAnomaliesRequest.state === 'pending',
@@ -149,11 +252,11 @@ export const useLogEntryAnomaliesResults = ({
     isLoadingLogEntryAnomalies,
     hasFailedLoadingLogEntryAnomalies,
     changeSortOptions,
-    sortOptions,
+    sortOptions: reducerState.sortOptions,
     changePaginationOptions,
-    paginationOptions,
-    fetchPreviousPage: page > 1 ? handleFetchPreviousPage : undefined,
-    fetchNextPage: hasNextPage ? handleFetchNextPage : undefined,
-    page,
+    paginationOptions: reducerState.paginationOptions,
+    fetchPreviousPage: reducerState.page > 1 ? handleFetchPreviousPage : undefined,
+    fetchNextPage: reducerState.hasNextPage ? handleFetchNextPage : undefined,
+    page: reducerState.page,
   };
 };
