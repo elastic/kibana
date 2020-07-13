f249d5d22458550af2c5974104f1cc9309f138a7
Jen Huang <its.jenetic@gmail.com>
2020-07-13 08:12:53 -0700

    [Ingest Manager] Simplify add/edit package config (integration) form (#71187)
    * Match add integration page with designs
    
    * Clean up package config layout code
    
    * Match edit integration config page with designs
    
    * Fix typing and i18n issues
    
    * Add back data test subj
    
    * Add loading UI for second step; code clean up
    
    * Fix limited packages incorrect response
    
    * Add ability to create agent config when selecting config to add integration to
    
    * Add error count to input-level panel; memoize children components
    
    * Add error count next to all advanced options toggles
    
    * Move general form error to bottom bar
    
    * #69750 Auto-expand inputs with required & empty (invalid) vars
    
    * #68019 Enforce unique package config names, per agent config
    
    * Fix typing
    
    * Fix i18n
    
    * Fix reloading when new agent config _wasn't_ created
    
    * Memoize edit integration and fix fields not collapsing on edit
    
    * Really fix types
    # Conflicts:
    #	x-pack/plugins/translations/translations/ja-JP.json
    #	x-pack/plugins/translations/translations/zh-CN.json

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/agent_config/create_package_config_page/step_configure_package.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/agent_config/create_package_config_page/step_configure_package.tsx
@@ -4,12 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React from 'react';
-import { FormattedMessage } from '@kbn/i18n/react';
-import { EuiPanel, EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiCallOut } from '@elastic/eui';
-import { i18n } from '@kbn/i18n';
+import { EuiHorizontalRule, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { PackageInfo, RegistryStream, NewPackageConfig, PackageConfigInput } from '../../../types';
 import { Loading } from '../../../components';
-import { PackageConfigValidationResults, validationHasErrors } from './services';
+import { PackageConfigValidationResults } from './services';
 import { PackageConfigInputPanel, CustomPackageConfig } from './components';
 import { CreatePackageConfigFrom } from './types';
 
@@ -52,8 +50,6 @@ export const StepConfigurePackage: React.FunctionComponent<{
   validationResults,
   submitAttempted,
 }) => {
-  const hasErrors = validationResults ? validationHasErrors(validationResults) : false;
-
   // Configure inputs (and their streams)
   // Assume packages only export one config template for now
   const renderConfigureInputs = () =>
@@ -61,76 +57,50 @@ export const StepConfigurePackage: React.FunctionComponent<{
     packageInfo.config_templates[0] &&
     packageInfo.config_templates[0].inputs &&
     packageInfo.config_templates[0].inputs.length ? (
-      <EuiFlexGroup direction="column">
-        {packageInfo.config_templates[0].inputs.map((packageInput) => {
-          const packageConfigInput = packageConfig.inputs.find(
-            (input) => input.type === packageInput.type
-          );
-          const packageInputStreams = findStreamsForInputType(packageInput.type, packageInfo);
-          return packageConfigInput ? (
-            <EuiFlexItem key={packageInput.type}>
-              <PackageConfigInputPanel
-                packageInput={packageInput}
-                packageInputStreams={packageInputStreams}
-                packageConfigInput={packageConfigInput}
-                updatePackageConfigInput={(updatedInput: Partial<PackageConfigInput>) => {
-                  const indexOfUpdatedInput = packageConfig.inputs.findIndex(
-                    (input) => input.type === packageInput.type
-                  );
-                  const newInputs = [...packageConfig.inputs];
-                  newInputs[indexOfUpdatedInput] = {
-                    ...newInputs[indexOfUpdatedInput],
-                    ...updatedInput,
-                  };
-                  updatePackageConfig({
-                    inputs: newInputs,
-                  });
-                }}
-                inputValidationResults={validationResults!.inputs![packageConfigInput.type]}
-                forceShowErrors={submitAttempted}
-              />
-            </EuiFlexItem>
-          ) : null;
-        })}
-      </EuiFlexGroup>
+      <>
+        <EuiHorizontalRule margin="m" />
+        <EuiFlexGroup direction="column" gutterSize="none">
+          {packageInfo.config_templates[0].inputs.map((packageInput) => {
+            const packageConfigInput = packageConfig.inputs.find(
+              (input) => input.type === packageInput.type
+            );
+            const packageInputStreams = findStreamsForInputType(packageInput.type, packageInfo);
+            return packageConfigInput ? (
+              <EuiFlexItem key={packageInput.type}>
+                <PackageConfigInputPanel
+                  packageInput={packageInput}
+                  packageInputStreams={packageInputStreams}
+                  packageConfigInput={packageConfigInput}
+                  updatePackageConfigInput={(updatedInput: Partial<PackageConfigInput>) => {
+                    const indexOfUpdatedInput = packageConfig.inputs.findIndex(
+                      (input) => input.type === packageInput.type
+                    );
+                    const newInputs = [...packageConfig.inputs];
+                    newInputs[indexOfUpdatedInput] = {
+                      ...newInputs[indexOfUpdatedInput],
+                      ...updatedInput,
+                    };
+                    updatePackageConfig({
+                      inputs: newInputs,
+                    });
+                  }}
+                  inputValidationResults={validationResults!.inputs![packageConfigInput.type]}
+                  forceShowErrors={submitAttempted}
+                />
+                <EuiHorizontalRule margin="m" />
+              </EuiFlexItem>
+            ) : null;
+          })}
+        </EuiFlexGroup>
+      </>
     ) : (
-      <EuiPanel>
-        <CustomPackageConfig
-          from={from}
-          packageName={packageInfo.name}
-          packageConfig={packageConfig}
-          packageConfigId={packageConfigId}
-        />
-      </EuiPanel>
+      <CustomPackageConfig
+        from={from}
+        packageName={packageInfo.name}
+        packageConfig={packageConfig}
+        packageConfigId={packageConfigId}
+      />
     );
 
-  return validationResults ? (
-    <EuiFlexGroup direction="column" gutterSize="none">
-      <EuiFlexItem>{renderConfigureInputs()}</EuiFlexItem>
-      {hasErrors && submitAttempted ? (
-        <EuiFlexItem>
-          <EuiSpacer size="m" />
-          <EuiCallOut
-            title={i18n.translate(
-              'xpack.ingestManager.createPackageConfig.stepConfigure.validationErrorTitle',
-              {
-                defaultMessage: 'Your integration configuration has errors',
-              }
-            )}
-            color="danger"
-          >
-            <p>
-              <FormattedMessage
-                id="xpack.ingestManager.createPackageConfig.stepConfigure.validationErrorText"
-                defaultMessage="Please fix the above errors before continuing"
-              />
-            </p>
-          </EuiCallOut>
-          <EuiSpacer size="m" />
-        </EuiFlexItem>
-      ) : null}
-    </EuiFlexGroup>
-  ) : (
-    <Loading />
-  );
+  return validationResults ? renderConfigureInputs() : <Loading />;
 };
