09bf9f667966197f2407af02cd02facce2bc2d44
Devin Hurley <devin.hurley@elastic.co>
2020-07-14 01:13:27 -0400

    bug fix where two lists when 'anded' together were not filtering down result set

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/filter_events_with_list.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/filter_events_with_list.ts
@@ -7,7 +7,7 @@ import { get } from 'lodash/fp';
 import { Logger } from 'src/core/server';
 
 import { ListClient } from '../../../../../lists/server';
-import { SignalSearchResponse, SearchTypes } from './types';
+import { SignalSearchResponse, SearchTypes, SignalSource } from './types';
 import { BuildRuleMessage } from './rule_messages';
 import {
   entriesList,
@@ -15,6 +15,7 @@ import {
   ExceptionListItemSchema,
 } from '../../../../../lists/common/schemas';
 import { hasLargeValueList } from '../../../../common/detection_engine/utils';
+import { SearchHit } from '../../types';
 
 interface FilterEventsAgainstList {
   listClient: ListClient;
@@ -58,84 +59,87 @@ export const filterEventsAgainstList = async ({
     // narrow unioned type to be single
     const isStringableType = (val: SearchTypes) =>
       ['string', 'number', 'boolean'].includes(typeof val);
+    // const filteredAccum = eventSearchResult;
     // grab the signals with values found in the given exception lists.
-    const filteredHitsPromises = exceptionItemsWithLargeValueLists.map(
-      async (exceptionItem: ExceptionListItemSchema) => {
-        const { entries } = exceptionItem;
 
-        const filteredHitsEntries = await entries
-          .filter((t): t is EntryList => entriesList.is(t))
-          .map(async (entry) => {
-            const { list, field, operator } = entry;
-            const { id, type } = list;
+    // yara's changes below
+    // const filteredHitsPromises = exceptionItemsWithLargeValueLists.map(
+    //   async (exceptionItem: ExceptionListItemSchema) => {
+    //     const { entries } = exceptionItem;
+    // end of yara's changes
+    // maybe turn this into a forEach?
+    const allExceptionsEntries = exceptionsList
+      .map((excItem: ExceptionListItemSchema) => {
+        const { entries } = excItem;
+        return entries;
+      })
+      .flat();
+    const res = await allExceptionsEntries
+      .filter((t): t is EntryList => entriesList.is(t))
+      .reduce<Promise<SignalSearchResponse['hits']['hits']>>(
+        async (filteredAccum: Promise<SignalSearchResponse['hits']['hits']>, entry: EntryList) => {
+          const { list, field, operator } = entry;
+          const { id, type } = list;
 
-            // acquire the list values we are checking for.
-            const valuesOfGivenType = eventSearchResult.hits.hits.reduce(
-              (acc, searchResultItem) => {
-                const valueField = get(field, searchResultItem._source);
-                logger.debug(`valueField: ${valueField}`);
-                logger.debug(`isStringableType(valueField): ${isStringableType(valueField)}`);
-                if (valueField != null && isStringableType(valueField)) {
-                  logger.debug(`valueField.toString(): ${valueField.toString()}`);
-                  acc.add(valueField.toString());
-                }
-                return acc;
-              },
-              new Set<string>()
-            );
-            logger.debug(`valuesOfGivenType: ${JSON.stringify([...valuesOfGivenType], null, 2)}`);
+          // acquire the list values we are checking for.
+          const valuesOfGivenType = (await filteredAccum).reduce((acc, searchResultItem) => {
+            const valueField = get(field, searchResultItem._source);
+            if (valueField != null && isStringableType(valueField)) {
+              acc.add(valueField.toString());
+            }
+            return acc;
+          }, new Set<string>());
+          logger.debug(`valuesOfGivenType: ${JSON.stringify([...valuesOfGivenType], null, 2)}`);
 
-            // matched will contain any list items that matched with the
-            // values passed in from the Set.
-            const matchedListItems = await listClient.getListItemByValues({
-              listId: id,
-              type,
-              value: [...valuesOfGivenType],
-            });
-
-            logger.debug(`matchedListItems: ${JSON.stringify(matchedListItems, null, 2)}`);
-            // create a set of list values that were a hit - easier to work with
-            const matchedListItemsSet = new Set<SearchTypes>(
-              matchedListItems.map((item) => item.value)
-            );
-
-            // do a single search after with these values.
-            // painless script to do nested query in elasticsearch
-            // filter out the search results that match with the values found in the list.
-            const filteredEvents = eventSearchResult.hits.hits.filter((item) => {
-              const eventItem = get(entry.field, item._source);
-              if (operator === 'included') {
-                // only create a signal if the event is not in the value list
-                if (eventItem != null) {
-                  return !matchedListItemsSet.has(eventItem);
-                }
-              } else if (operator === 'excluded') {
-                // only create a signal if the event is in the value list
-                if (eventItem != null) {
-                  return matchedListItemsSet.has(eventItem);
-                }
-              }
-              return false;
-            });
-            const diff = eventSearchResult.hits.hits.length - filteredEvents.length;
-            logger.debug(buildRuleMessage(`Lists filtered out ${diff} events`));
-            return filteredEvents;
+          // matched will contain any list items that matched with the
+          // values passed in from the Set.
+          const matchedListItems = await listClient.getListItemByValues({
+            listId: id,
+            type,
+            value: [...valuesOfGivenType],
           });
-        logger.debug(`filteredHitsEntries: ${JSON.stringify(filteredHitsEntries, null, 2)}`);
-        return (await Promise.all(filteredHitsEntries)).flat();
-      }
-    );
 
-    const filteredHits = await Promise.all(filteredHitsPromises);
+          logger.debug(`matchedListItems: ${JSON.stringify(matchedListItems, null, 2)}`);
+          // create a set of list values that were a hit - easier to work with
+          const matchedListItemsSet = new Set<SearchTypes>(
+            matchedListItems.map((item) => item.value)
+          );
+
+          // do a single search after with these values.
+          // painless script to do nested query in elasticsearch
+          // filter out the search results that match with the values found in the list.
+          const filteredEvents = (await filteredAccum).filter((item) => {
+            const eventItem = get(entry.field, item._source);
+            if (operator === 'included') {
+              // only create a signal if the event is not in the value list
+              if (eventItem != null) {
+                return !matchedListItemsSet.has(eventItem);
+              }
+            } else if (operator === 'excluded') {
+              // only create a signal if the event is in the value list
+              if (eventItem != null) {
+                return matchedListItemsSet.has(eventItem);
+              }
+            }
+            return false;
+          });
+          const diff = eventSearchResult.hits.hits.length - filteredEvents.length;
+          logger.debug(buildRuleMessage(`Lists filtered out ${diff} events`));
+          const toReturn = await filteredEvents;
+          return toReturn;
+        },
+        Promise.resolve<SignalSearchResponse['hits']['hits']>(eventSearchResult.hits.hits)
+      );
+
     // logger.debug(`filteredHits FINAL: ${JSON.stringify(filteredHits, null, 2)}`);
     const toReturn: SignalSearchResponse = {
       took: eventSearchResult.took,
       timed_out: eventSearchResult.timed_out,
       _shards: eventSearchResult._shards,
       hits: {
-        total: filteredHits.length,
+        total: res.length,
         max_score: eventSearchResult.hits.max_score,
-        hits: filteredHits.flat(),
+        hits: res,
       },
     };
 
