ea9756831e79c49c4f32b7373f0d65d0145752ec
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2020-07-14 12:04:06 -0400

    [Ingest Manager] Better display of Fleet requirements

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/fleet/setup_page/index.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/fleet/setup_page/index.tsx
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React, { useState } from 'react';
+import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
 import {
   EuiPageBody,
@@ -14,10 +15,38 @@ import {
   EuiTitle,
   EuiSpacer,
   EuiIcon,
+  EuiCallOut,
+  EuiFlexItem,
+  EuiFlexGroup,
+  EuiCode,
+  EuiCodeBlock,
+  EuiLink,
 } from '@elastic/eui';
 import { useCore, sendPostFleetSetup } from '../../../hooks';
 import { WithoutHeaderLayout } from '../../../layouts';
 import { GetFleetStatusResponse } from '../../../types';
+
+export const RequirementItem: React.FunctionComponent<{ isMissing: boolean }> = ({
+  isMissing,
+  children,
+}) => {
+  return (
+    <EuiFlexGroup gutterSize="s" alignItems="flexStart">
+      <EuiFlexItem grow={false}>
+        <EuiText>
+          {isMissing ? (
+            <EuiIcon type="crossInACircleFilled" color="danger" />
+          ) : (
+            <EuiIcon type="checkInCircleFilled" color="success" />
+          )}
+        </EuiText>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiText>{children}</EuiText>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
 
 export const SetupPage: React.FunctionComponent<{
   refresh: () => Promise<void>;
@@ -38,84 +67,154 @@ export const SetupPage: React.FunctionComponent<{
     }
   };
 
-  const content =
-    missingRequirements.includes('tls_required') ||
-    missingRequirements.includes('api_keys') ||
-    missingRequirements.includes('encrypted_saved_object_encryption_key_required') ? (
-      <>
-        <EuiSpacer size="m" />
-        <EuiIcon type="lock" color="subdued" size="xl" />
-        <EuiSpacer size="m" />
-        <EuiTitle size="l">
-          <h2>
-            <FormattedMessage
-              id="xpack.ingestManager.setupPage.missingRequirementsTitle"
-              defaultMessage="Missing requirements"
-            />
-          </h2>
-        </EuiTitle>
-        <EuiSpacer size="xl" />
-        <EuiText color="subdued" textAlign={'left'}>
-          <FormattedMessage
-            id="xpack.ingestManager.setupPage.missingRequirementsDescription"
-            defaultMessage="To use Fleet, you must enable the following features:
-          {space}- Enable Elasticsearch API keys.
-          {space}- Enable TLS to secure the communication between Agents and Kibana.
-          {space}- Set the encryption key for encrypted saved objects.
-          "
-            values={{
-              space: <EuiSpacer size="m" />,
-            }}
-          />
-        </EuiText>
-        <EuiSpacer size="l" />
-      </>
-    ) : (
-      <>
-        <EuiSpacer size="m" />
-        <EuiIcon type="lock" color="subdued" size="xl" />
-        <EuiSpacer size="m" />
-        <EuiTitle size="l">
-          <h2>
-            <FormattedMessage
-              id="xpack.ingestManager.setupPage.enableTitle"
-              defaultMessage="Enable Fleet"
-            />
-          </h2>
-        </EuiTitle>
-        <EuiSpacer size="xl" />
-        <EuiText color="subdued">
-          <FormattedMessage
-            id="xpack.ingestManager.setupPage.enableText"
-            defaultMessage="In order to use Fleet, you must create an Elastic user. This user can create API keys
-        and write to logs-* and metrics-*."
-          />
-        </EuiText>
-        <EuiSpacer size="l" />
-        <EuiForm>
-          <form onSubmit={onSubmit}>
-            <EuiButton fill isLoading={isFormLoading} type="submit">
+  if (
+    !missingRequirements.includes('tls_required') &&
+    !missingRequirements.includes('api_keys') &&
+    !missingRequirements.includes('encrypted_saved_object_encryption_key_required')
+  ) {
+    return (
+      <WithoutHeaderLayout>
+        <EuiPageBody restrictWidth={548}>
+          <EuiPageContent
+            verticalPosition="center"
+            horizontalPosition="center"
+            className="eui-textCenter"
+            paddingSize="l"
+          >
+            <EuiSpacer size="m" />
+            <EuiIcon type="lock" color="subdued" size="xl" />
+            <EuiSpacer size="m" />
+            <EuiTitle size="l">
+              <h2>
+                <FormattedMessage
+                  id="xpack.ingestManager.setupPage.enableTitle"
+                  defaultMessage="Enable Fleet"
+                />
+              </h2>
+            </EuiTitle>
+            <EuiSpacer size="xl" />
+            <EuiText color="subdued">
               <FormattedMessage
-                id="xpack.ingestManager.setupPage.enableFleet"
-                defaultMessage="Create user and enable Fleet"
+                id="xpack.ingestManager.setupPage.enableText"
+                defaultMessage="In order to use Fleet, you must create an Elastic user. This user can create API keys
+        and write to logs-* and metrics-*."
               />
-            </EuiButton>
-          </form>
-        </EuiForm>
-        <EuiSpacer size="m" />
-      </>
+            </EuiText>
+            <EuiSpacer size="l" />
+            <EuiForm>
+              <form onSubmit={onSubmit}>
+                <EuiButton fill isLoading={isFormLoading} type="submit">
+                  <FormattedMessage
+                    id="xpack.ingestManager.setupPage.enableFleet"
+                    defaultMessage="Create user and enable Fleet"
+                  />
+                </EuiButton>
+              </form>
+            </EuiForm>
+            <EuiSpacer size="m" />
+          </EuiPageContent>
+        </EuiPageBody>
+      </WithoutHeaderLayout>
     );
+  }
 
   return (
     <WithoutHeaderLayout>
-      <EuiPageBody restrictWidth={548}>
-        <EuiPageContent
-          verticalPosition="center"
-          horizontalPosition="center"
-          className="eui-textCenter"
-          paddingSize="l"
-        >
-          {content}
+      <EuiPageBody restrictWidth={820}>
+        <EuiPageContent>
+          <EuiCallOut
+            title={i18n.translate('xpack.ingestManager.setupPage.missingRequirementsCalloutTitle', {
+              defaultMessage: 'Missing security requirements',
+            })}
+            color="warning"
+            iconType="alert"
+          >
+            <FormattedMessage
+              id="xpack.ingestManager.setupPage.missingRequirementsCalloutDescription"
+              defaultMessage="In order to use Fleet, you must enable the following Elasticsearch and Kibana security features."
+            />
+          </EuiCallOut>
+          <EuiSpacer size="m" />
+          <FormattedMessage
+            id="xpack.ingestManager.setupPage.missingRequirementsElasticsearchTitle"
+            defaultMessage="In your Elasticsearch configuration, enable:"
+          />
+          <EuiSpacer size="l" />
+          <RequirementItem isMissing={false}>
+            <FormattedMessage
+              id="xpack.ingestManager.setupPage.elasticsearchSecurityFlagText"
+              defaultMessage="Elasticsearch security. Set {securityFlag} to {true} ."
+              values={{
+                securityFlag: <EuiCode>xpack.security.enabled</EuiCode>,
+                true: <EuiCode>true</EuiCode>,
+              }}
+            />
+          </RequirementItem>
+          <EuiSpacer size="s" />
+          <RequirementItem isMissing={missingRequirements.includes('api_keys')}>
+            <FormattedMessage
+              id="xpack.ingestManager.setupPage.elasticsearchApiKeyFlagText"
+              defaultMessage="API key service. Set {apiKeyFlag} to {true} ."
+              values={{
+                apiKeyFlag: <EuiCode>xpack.security.authc.api_key.enabled</EuiCode>,
+                true: <EuiCode>true</EuiCode>,
+              }}
+            />
+          </RequirementItem>
+          <EuiSpacer size="m" />
+          <EuiCodeBlock isCopyable={true}>
+            {`xpack.security.enabled: true
+xpack.security.authc.api_key.enabled: true`}
+          </EuiCodeBlock>
+          <EuiSpacer size="l" />
+          <FormattedMessage
+            id="xpack.ingestManager.setupPage.missingRequirementsKibanaTitle"
+            defaultMessage="In your Kibana configuration, enable:"
+          />
+          <EuiSpacer size="l" />
+          <RequirementItem isMissing={missingRequirements.includes('tls_required')}>
+            <FormattedMessage
+              id="xpack.ingestManager.setupPage.tlsFlagText"
+              defaultMessage="Kibana security. Set. Set {securityFlag} to {true}. For development purposes, you can disable TLS by setting {tlsFlag} to {true} as an unsafe alternative."
+              values={{
+                securityFlag: <EuiCode>xpack.security.enabled</EuiCode>,
+                tlsFlag: <EuiCode>xpack.ingestManager.fleet.tlsCheckDisabled</EuiCode>,
+                true: <EuiCode>true</EuiCode>,
+              }}
+            />
+          </RequirementItem>
+          <EuiSpacer size="m" />
+          <RequirementItem
+            isMissing={missingRequirements.includes(
+              'encrypted_saved_object_encryption_key_required'
+            )}
+          >
+            <FormattedMessage
+              id="xpack.ingestManager.setupPage.encryptionKeyFlagText"
+              defaultMessage="Encryption key. Set {keyFlag} to use a secret key."
+              values={{
+                keyFlag: <EuiCode>xpack.reporting.encryptionKey</EuiCode>,
+              }}
+            />
+          </RequirementItem>
+          <EuiSpacer size="l" />
+          <FormattedMessage
+            id="xpack.ingestManager.setupPage.gettingStartedText"
+            defaultMessage="For more information, read our {link}."
+            values={{
+              link: (
+                <EuiLink
+                  href="https://www.elastic.co/guide/en/ingest-management/current/index.html"
+                  target="_blank"
+                >
+                  <FormattedMessage
+                    id="xpack.ingestManager.setupPage.gettingStartedLink"
+                    defaultMessage="Getting Started"
+                  />
+                </EuiLink>
+              ),
+            }}
+          />
         </EuiPageContent>
       </EuiPageBody>
     </WithoutHeaderLayout>
