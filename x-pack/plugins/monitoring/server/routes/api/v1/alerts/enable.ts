9cb8f7ef15a6ca73b4b804e30a654b6a69c1058d
chrisronline <chrisronline@gmail.com>
2020-07-14 16:04:57 -0400

    Type

--- x-pack/plugins/monitoring/server/routes/api/v1/alerts/enable.ts
+++ x-pack/plugins/monitoring/server/routes/api/v1/alerts/enable.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { schema } from '@kbn/config-schema';
 // @ts-ignore
 import { handleError } from '../../../../lib/errors';
 import { AlertsFactory } from '../../../../alerts';
