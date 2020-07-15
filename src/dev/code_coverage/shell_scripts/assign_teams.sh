d196a70aca638a029a18e6750fb660360aa1d6bc
Tre' Seymour <wayne.seymour@elastic.co>
2020-07-15 11:44:39 -0600

    Add abilty to change the name of the pipeline directly in groovy, and add auth plumbing (vault).

--- src/dev/code_coverage/shell_scripts/assign_teams.sh
+++ src/dev/code_coverage/shell_scripts/assign_teams.sh
@@ -3,6 +3,9 @@
 echo "### Code Coverage Team Assignment"
 echo ""
 
+PIPELINE_NAME=$1
+export PIPELINE_NAME
+
 node scripts/load_team_assignment.js --verbose
 
 echo "###  Code Coverage Team Assignment - Complete"
--- vars/kibanaTeamAssign.groovy
+++ vars/kibanaTeamAssign.groovy
@@ -1,11 +1,25 @@
-def load (title) {
+def loadIngestionPipeline(ingestionPipelineName, title) {
   kibanaPipeline.bash("""
     source src/dev/ci_setup/setup_env.sh
     yarn kbn bootstrap --prefer-offline
 
-    . src/dev/code_coverage/shell_scripts/assign_teams.sh
+    . src/dev/code_coverage/shell_scripts/assign_teams.sh '${ingestionPipelineName}'
   """, title)
+}
 
+def loadWithVault(ingestionPipelineName, title) {
+  def vaultSecret = 'secret/kibana-issues/prod/coverage/elasticsearch'
+  withVaultSecret(secret: vaultSecret, secret_field: 'host', variable_name: 'HOST_FROM_VAULT') {
+    withVaultSecret(secret: vaultSecret, secret_field: 'username', variable_name: 'USER_FROM_VAULT') {
+      withVaultSecret(secret: vaultSecret, secret_field: 'password', variable_name: 'PASS_FROM_VAULT') {
+        loadIngestionPipeline()
+      }
+    }
+  }
+}
+
+def load(ingestionPipelineName, title) {
+  loadWithVault(ingestionPipelineName, title)
 }
 
 return this
