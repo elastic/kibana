510a68467d1a1b6d22c4c13896cee5233d9f2c8a
Chris Roberson <chrisronline@gmail.com>
2020-07-14 20:13:42 -0400

    [Monitoring] Out of the box alerting (#68805) (#71780)
    * First draft, not quite working but a good start
    
    * More working
    
    * Support configuring throttle
    
    * Get the other alerts working too
    
    * More
    
    * Separate into individual files
    
    * Menu support as well as better integration in existing UIs
    
    * Red borders!
    
    * New overview style, and renamed alert
    
    * more visual updates
    
    * Update cpu usage and improve settings configuration in UI
    
    * Convert cluster health and license expiration alert to use legacy data model
    
    * Remove most of the custom UI and use the flyout
    
    * Add the actual alerts
    
    * Remove more code
    
    * Fix formatting
    
    * Fix up some errors
    
    * Remove unnecessary code
    
    * Updates
    
    * add more links here
    
    * Fix up linkage
    
    * Added nodes changed alert
    
    * Most of the version mismatch working
    
    * Add kibana mismatch
    
    * UI tweaks
    
    * Add timestamp
    
    * Support actions in the enable api
    
    * Move this around
    
    * Better support for changing legacy alerts
    
    * Add missing files
    
    * Update alerts
    
    * Enable alerts whenever any page is visited in SM
    
    * Tweaks
    
    * Use more practical default
    
    * Remove the buggy renderer and ensure setup mode can show all alerts
    
    * Updates
    
    * Remove unnecessary code
    
    * Remove some dead code
    
    * Cleanup
    
    * Fix snapshot
    
    * Fixes
    
    * Fixes
    
    * Fix test
    
    * Add alerts to kibana and logstash listing pages
    
    * Fix test
    
    * Add disable/mute options
    
    * Tweaks
    
    * Fix linting
    
    * Fix i18n
    
    * Adding a couple tests
    
    * Fix localization
    
    * Use http
    
    * Ensure we properly handle when an alert is resolved
    
    * Fix tests
    
    * Hide legacy alerts if not the right license
    
    * Design tweaks
    
    * Fix tests
    
    * PR feedback
    
    * Moar tests
    
    * Fix i18n
    
    * Ensure we have a control over the messaging
    
    * Fix translations
    
    * Tweaks
    
    * More localization
    
    * Copy changes
    
    * Type
    # Conflicts:
    #	x-pack/plugins/monitoring/common/constants.ts
    #	x-pack/plugins/monitoring/public/components/cluster/overview/alerts_panel.js
    #	x-pack/plugins/monitoring/public/components/cluster/overview/index.js
    #	x-pack/plugins/monitoring/public/components/elasticsearch/node/node.js
    #	x-pack/plugins/monitoring/public/components/elasticsearch/nodes/nodes.js
    #	x-pack/plugins/monitoring/public/components/kibana/instances/instances.js
    #	x-pack/plugins/monitoring/server/plugin.ts
    #	x-pack/test/functional/apps/monitoring/cluster/alerts.js

--- x-pack/plugins/monitoring/public/components/kibana/instances/instances.js
+++ x-pack/plugins/monitoring/public/components/kibana/instances/instances.js
@@ -13,21 +13,22 @@ import {
   EuiSpacer,
   EuiLink,
   EuiCallOut,
+  EuiToolTip,
+  EuiHealth,
 } from '@elastic/eui';
 import { capitalize, get } from 'lodash';
 import { ClusterStatus } from '../cluster_status';
 import { EuiMonitoringTable } from '../../table';
-import { KibanaStatusIcon } from '../status_icon';
 import { StatusIcon } from '../../status_icon';
 import { formatMetric, formatNumber } from '../../../lib/format_number';
 import { getSafeForExternalLink } from '../../../lib/get_safe_for_external_link';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n/react';
 import { SetupModeBadge } from '../../setup_mode/badge';
 import { KIBANA_SYSTEM_ID } from '../../../../common/constants';
 import { ListingCallOut } from '../../setup_mode/listing_callout';
+import { AlertsStatus } from '../../../alerts/status';
 
-const getColumns = (setupMode) => {
+const getColumns = (setupMode, alerts) => {
   const columns = [
     {
       name: i18n.translate('xpack.monitoring.kibana.listing.nameColumnTitle', {
@@ -79,32 +80,33 @@ const getColumns = (setupMode) => {
       },
     },
     {
+      name: i18n.translate('xpack.monitoring.kibana.listing.alertsColumnTitle', {
+        defaultMessage: 'Alerts',
+      }),
+      field: 'isOnline',
+      width: '175px',
+      sortable: true,
+      render: () => {
+        return <AlertsStatus showBadge={true} alerts={alerts} />;
+      },
+    },
+    {
       name: i18n.translate('xpack.monitoring.kibana.listing.statusColumnTitle', {
         defaultMessage: 'Status',
       }),
       field: 'status',
-      render: (status, kibana) => (
-        <div
-          title={i18n.translate('xpack.monitoring.kibana.listing.instanceStatusTitle', {
-            defaultMessage: 'Instance status: {kibanaStatus}',
-            values: {
-              kibanaStatus: status,
-            },
-          })}
-          className="monTableCell__status"
-        >
-          <KibanaStatusIcon status={status} availability={kibana.availability} />
-          &nbsp;
-          {!kibana.availability ? (
-            <FormattedMessage
-              id="xpack.monitoring.kibana.listing.instanceStatus.offlineLabel"
-              defaultMessage="Offline"
-            />
-          ) : (
-            capitalize(status)
-          )}
-        </div>
-      ),
+      render: (status, kibana) => {
+        return (
+          <EuiToolTip content={status} position="bottom" trigger="hover">
+            <EuiHealth
+              color={kibana.availability ? 'success' : 'subdued'}
+              data-test-subj="statusIcon"
+            >
+              {capitalize(status)}
+            </EuiHealth>
+          </EuiToolTip>
+        );
+      },
     },
     {
       name: i18n.translate('xpack.monitoring.kibana.listing.loadAverageColumnTitle', {
@@ -157,7 +159,7 @@ const getColumns = (setupMode) => {
 
 export class KibanaInstances extends PureComponent {
   render() {
-    const { clusterStatus, setupMode, sorting, pagination, onTableChange } = this.props;
+    const { clusterStatus, alerts, setupMode, sorting, pagination, onTableChange } = this.props;
 
     let setupModeCallOut = null;
     // Merge the instances data with the setup data if enabled
@@ -245,7 +247,7 @@ export class KibanaInstances extends PureComponent {
       <EuiPage>
         <EuiPageBody>
           <EuiPanel>
-            <ClusterStatus stats={clusterStatus} />
+            <ClusterStatus stats={clusterStatus} alerts={alerts} />
           </EuiPanel>
           <EuiSpacer size="m" />
           {setupModeCallOut}
@@ -253,7 +255,7 @@ export class KibanaInstances extends PureComponent {
             <EuiMonitoringTable
               className="kibanaInstancesTable"
               rows={dataFlattened}
-              columns={getColumns(setupMode)}
+              columns={getColumns(setupMode, alerts)}
               sorting={sorting}
               pagination={pagination}
               setupMode={setupMode}
