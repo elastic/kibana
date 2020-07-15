c621e99d6518c76877f13aaf28ddcc0e264417fb
Thomas Watson <w@tson.dk>
2020-07-15 10:29:57 +0200

    Add @elastic/safer-lodash-set as an alternative to lodash.set (#67452)
    # Conflicts:
    #	yarn.lock

--- tasks/jenkins.js
+++ tasks/jenkins.js
@@ -39,6 +39,7 @@ module.exports = function (grunt) {
     'run:test_projects',
     'run:test_karma_ci',
     'run:test_hardening',
+    'run:test_package_safer_lodash_set',
     'run:apiIntegrationTests',
   ]);
 };
