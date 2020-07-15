c184115288a714dfdf14d9ce5afbbee0cd15b16f
Spencer <email@spalger.com>
2020-07-15 10:09:15 -0700

    [7.x] [kbn/optimizer] report sizes of assets produced by optimizer (#71319) (#71878)
    Co-authored-by: spalger <spalger@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- test/scripts/jenkins_xpack_visual_regression.sh
+++ test/scripts/jenkins_xpack_visual_regression.sh
@@ -17,6 +17,9 @@ tar -xzf "$linuxBuild" -C "$installDir" --strip=1
 cd "$KIBANA_DIR"
 source "test/scripts/jenkins_xpack_saved_objects_field_metrics.sh"
 
+cd "$KIBANA_DIR"
+source "test/scripts/jenkins_xpack_saved_objects_field_metrics.sh"
+
 echo " -> running visual regression tests from x-pack directory"
 cd "$XPACK_DIR"
 yarn percy exec -t 10000 -- -- \
