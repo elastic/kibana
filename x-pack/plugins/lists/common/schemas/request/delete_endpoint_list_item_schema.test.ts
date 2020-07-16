24ded3c30d75946c85d6b9e40e21d64d3b396e5b
Frank Hassanabad <frank.hassanabad@elastic.co>
2020-07-16 15:46:01 -0600

    [SIEM][Detection Engine][Lists] Fixes bugs, adds tests (#71880) (#72135)
    ## Summary
    
    * Adds the createEndpointListSchema to the create_endpoint_list_route for API boundary checks
    * Adds unit tests to the requests
    * Fixes a few bugs found in the find endpoints for types
    * Fixes or deletes the skipped tests
    * Updated TODO blocks and removed ones that are obsolete
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] [Unit or functional tests](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility) were updated or added to match the most common scenarios

--- x-pack/plugins/lists/common/schemas/request/delete_endpoint_list_item_schema.test.ts
+++ x-pack/plugins/lists/common/schemas/request/delete_endpoint_list_item_schema.test.ts
@@ -10,15 +10,15 @@ import { pipe } from 'fp-ts/lib/pipeable';
 import { exactCheck, foldLeftRight, getPaths } from '../../siem_common_deps';
 
 import {
-  DeleteExceptionListItemSchema,
-  deleteExceptionListItemSchema,
-} from './delete_exception_list_item_schema';
-import { getDeleteExceptionListItemSchemaMock } from './delete_exception_list_item_schema.mock';
+  DeleteEndpointListItemSchema,
+  deleteEndpointListItemSchema,
+} from './delete_endpoint_list_item_schema';
+import { getDeleteEndpointListItemSchemaMock } from './delete_endpoint_list_item_schema.mock';
 
-describe('delete_exception_list_item_schema', () => {
-  test('it should validate a typical exception list item request', () => {
-    const payload = getDeleteExceptionListItemSchemaMock();
-    const decoded = deleteExceptionListItemSchema.decode(payload);
+describe('delete_endpoint_list_item_schema', () => {
+  test('it should validate a typical endpoint list item request', () => {
+    const payload = getDeleteEndpointListItemSchemaMock();
+    const decoded = deleteEndpointListItemSchema.decode(payload);
     const checked = exactCheck(payload, decoded);
     const message = pipe(checked, foldLeftRight);
 
@@ -26,22 +26,23 @@ describe('delete_exception_list_item_schema', () => {
     expect(message.schema).toEqual(payload);
   });
 
-  test('it should accept an undefined for "namespace_type" but default to "single"', () => {
-    const payload = getDeleteExceptionListItemSchemaMock();
+  test('it should NOT accept a value for "namespace_type" since it does not require one', () => {
+    const payload: DeleteEndpointListItemSchema & {
+      namespace_type: string;
+    } = { ...getDeleteEndpointListItemSchemaMock(), namespace_type: 'single' };
     delete payload.namespace_type;
-    const decoded = deleteExceptionListItemSchema.decode(payload);
+    const decoded = deleteEndpointListItemSchema.decode(payload);
     const checked = exactCheck(payload, decoded);
     const message = pipe(checked, foldLeftRight);
     expect(getPaths(left(message.errors))).toEqual([]);
-    expect(message.schema).toEqual(getDeleteExceptionListItemSchemaMock());
+    expect(message.schema).toEqual(getDeleteEndpointListItemSchemaMock());
   });
 
   test('it should not allow an extra key to be sent in', () => {
-    const payload: DeleteExceptionListItemSchema & {
+    const payload: DeleteEndpointListItemSchema & {
       extraKey?: string;
-    } = getDeleteExceptionListItemSchemaMock();
-    payload.extraKey = 'some new value';
-    const decoded = deleteExceptionListItemSchema.decode(payload);
+    } = { ...getDeleteEndpointListItemSchemaMock(), extraKey: 'some new value' };
+    const decoded = deleteEndpointListItemSchema.decode(payload);
     const checked = exactCheck(payload, decoded);
     const message = pipe(checked, foldLeftRight);
     expect(getPaths(left(message.errors))).toEqual(['invalid keys "extraKey"']);
