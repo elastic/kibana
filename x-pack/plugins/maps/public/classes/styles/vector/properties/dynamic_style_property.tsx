52597b203b1da74403a782bdc1ca9817b5f950bf
Nathan Reese <reese.nathan@gmail.com>
2020-07-16 13:31:43 -0600

    [Maps] typescript vector style phase 1 (#69994)
    * [Maps] typescript vector style phase 1
    
    * tslint
    
    * unify Ordinal and Categorical field meta since they are mixed in real data
    
    * field formatter type
    
    * review feedback
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/maps/public/classes/styles/vector/properties/dynamic_style_property.tsx
+++ x-pack/plugins/maps/public/classes/styles/vector/properties/dynamic_style_property.tsx
@@ -3,42 +3,87 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 
 import _ from 'lodash';
-import { AbstractStyleProperty } from './style_property';
+import React from 'react';
+import { Feature } from 'geojson';
+import { AbstractStyleProperty, IStyleProperty } from './style_property';
 import { DEFAULT_SIGMA } from '../vector_style_defaults';
 import {
   STYLE_TYPE,
   SOURCE_META_DATA_REQUEST_ID,
   FIELD_ORIGIN,
+  VECTOR_STYLES,
 } from '../../../../../common/constants';
-import React from 'react';
 import { OrdinalFieldMetaPopover } from '../components/field_meta/ordinal_field_meta_popover';
 import { CategoricalFieldMetaPopover } from '../components/field_meta/categorical_field_meta_popover';
+import {
+  CategoryFieldMeta,
+  FieldMetaOptions,
+  StyleMetaData,
+  RangeFieldMeta,
+} from '../../../../../common/descriptor_types';
+import { IField } from '../../../fields/field';
+import { IVectorLayer } from '../../../layers/vector_layer/vector_layer';
+import { IJoin } from '../../../joins/join';
 
-export class DynamicStyleProperty extends AbstractStyleProperty {
+export interface IDynamicStyleProperty<T> extends IStyleProperty<T> {
+  getFieldMetaOptions(): FieldMetaOptions;
+  getField(): IField | null;
+  getFieldName(): string;
+  getFieldOrigin(): FIELD_ORIGIN | null;
+  getRangeFieldMeta(): RangeFieldMeta | null;
+  getCategoryFieldMeta(): CategoryFieldMeta | null;
+  getNumberOfCategories(): number;
+  isFieldMetaEnabled(): boolean;
+  isOrdinal(): boolean;
+  supportsFieldMeta(): boolean;
+  getFieldMetaRequest(): Promise<unknown>;
+  supportsMbFeatureState(): boolean;
+  pluckOrdinalStyleMetaFromFeatures(features: Feature[]): RangeFieldMeta | null;
+  pluckCategoricalStyleMetaFromFeatures(features: Feature[]): CategoryFieldMeta | null;
+  getValueSuggestions(query: string): Promise<string[]>;
+}
+
+type fieldFormatter = (value: string | undefined) => string;
+
+export class DynamicStyleProperty<T> extends AbstractStyleProperty<T>
+  implements IDynamicStyleProperty<T> {
   static type = STYLE_TYPE.DYNAMIC;
 
-  constructor(options, styleName, field, vectorLayer, getFieldFormatter) {
+  protected readonly _field: IField | null;
+  protected readonly _layer: IVectorLayer;
+  protected readonly _getFieldFormatter: (fieldName: string) => null | fieldFormatter;
+
+  constructor(
+    options: T,
+    styleName: VECTOR_STYLES,
+    field: IField | null,
+    vectorLayer: IVectorLayer,
+    getFieldFormatter: (fieldName: string) => null | fieldFormatter
+  ) {
     super(options, styleName);
     this._field = field;
     this._layer = vectorLayer;
     this._getFieldFormatter = getFieldFormatter;
   }
 
-  getValueSuggestions = (query) => {
-    const field = this.getField();
-    const fieldSource = this._getFieldSource();
-    return fieldSource && field ? fieldSource.getValueSuggestions(field, query) : [];
+  // ignore TS error about "Type '(query: string) => Promise<string[]> | never[]' is not assignable to type '(query: string) => Promise<string[]>'."
+  // @ts-expect-error
+  getValueSuggestions = (query: string) => {
+    return this._field === null
+      ? []
+      : this._field.getSource().getValueSuggestions(this._field, query);
   };
 
-  _getStyleMetaDataRequestId(fieldName) {
+  _getStyleMetaDataRequestId(fieldName: string) {
     if (this.getFieldOrigin() === FIELD_ORIGIN.SOURCE) {
       return SOURCE_META_DATA_REQUEST_ID;
     }
 
-    const join = this._layer.getValidJoins().find((join) => {
-      return join.getRightJoinSource().hasMatchingMetricField(fieldName);
+    const join = this._layer.getValidJoins().find((validJoin: IJoin) => {
+      return validJoin.getRightJoinSource().hasMatchingMetricField(fieldName);
     });
     return join ? join.getSourceMetaDataRequestId() : null;
   }
@@ -63,8 +108,8 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
       return rangeFieldMetaFromLocalFeatures;
     }
 
-    const data = styleMetaDataRequest.getData();
-    const rangeFieldMeta = this.pluckOrdinalStyleMetaFromFieldMetaData(data);
+    const data = styleMetaDataRequest.getData() as StyleMetaData;
+    const rangeFieldMeta = this._pluckOrdinalStyleMetaFromFieldMetaData(data);
     return rangeFieldMeta ? rangeFieldMeta : rangeFieldMetaFromLocalFeatures;
   }
 
@@ -88,17 +133,13 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
       return categoryFieldMetaFromLocalFeatures;
     }
 
-    const data = styleMetaDataRequest.getData();
-    const rangeFieldMeta = this.pluckCategoricalStyleMetaFromFieldMetaData(data);
+    const data = styleMetaDataRequest.getData() as StyleMetaData;
+    const rangeFieldMeta = this._pluckCategoricalStyleMetaFromFieldMetaData(data);
     return rangeFieldMeta ? rangeFieldMeta : categoryFieldMetaFromLocalFeatures;
   }
 
   getField() {
     return this._field;
-  }
-
-  _getFieldSource() {
-    return this._field ? this._field.getSource() : null;
   }
 
   getFieldName() {
@@ -126,7 +167,7 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
   }
 
   getFieldOrigin() {
-    return this._field.getOrigin();
+    return this._field ? this._field.getOrigin() : null;
   }
 
   isFieldMetaEnabled() {
@@ -135,10 +176,14 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
   }
 
   supportsFieldMeta() {
-    return this.isComplete() && this._field.supportsFieldMeta();
+    return this.isComplete() && !!this._field && this._field.supportsFieldMeta();
   }
 
   async getFieldMetaRequest() {
+    if (!this._field) {
+      return null;
+    }
+
     if (this.isOrdinal()) {
       return this._field.getOrdinalFieldMetaRequest();
     } else if (this.isCategorical()) {
@@ -154,20 +199,20 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
   }
 
   getFieldMetaOptions() {
-    return _.get(this.getOptions(), 'fieldMetaOptions', {});
+    return _.get(this.getOptions(), 'fieldMetaOptions', { isEnabled: true });
   }
 
-  pluckOrdinalStyleMetaFromFeatures(features) {
+  pluckOrdinalStyleMetaFromFeatures(features: Feature[]) {
     if (!this.isOrdinal()) {
       return null;
     }
 
-    const name = this.getField().getName();
+    const name = this.getFieldName();
     let min = Infinity;
     let max = -Infinity;
     for (let i = 0; i < features.length; i++) {
       const feature = features[i];
-      const newValue = parseFloat(feature.properties[name]);
+      const newValue = parseFloat(feature.properties ? feature.properties[name] : null);
       if (!isNaN(newValue)) {
         min = Math.min(min, newValue);
         max = Math.max(max, newValue);
@@ -176,25 +221,24 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
 
     return min === Infinity || max === -Infinity
       ? null
-      : {
-          min: min,
-          max: max,
+      : ({
+          min,
+          max,
           delta: max - min,
-        };
+        } as RangeFieldMeta);
   }
 
-  pluckCategoricalStyleMetaFromFeatures(features) {
+  pluckCategoricalStyleMetaFromFeatures(features: Feature[]) {
     const size = this.getNumberOfCategories();
     if (!this.isCategorical() || size <= 0) {
       return null;
     }
 
-    const fieldName = this.getField().getName();
     const counts = new Map();
     for (let i = 0; i < features.length; i++) {
       const feature = features[i];
-      const term = feature.properties[fieldName];
-      //properties object may be sparse, so need to check if the field is effectively present
+      const term = feature.properties ? feature.properties[this.getFieldName()] : undefined;
+      // properties object may be sparse, so need to check if the field is effectively present
       if (typeof term !== undefined) {
         if (counts.has(term)) {
           counts.set(term, counts.get(term) + 1);
@@ -215,16 +259,16 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
     const truncated = ordered.slice(0, size);
     return {
       categories: truncated,
-    };
+    } as CategoryFieldMeta;
   }
 
-  pluckOrdinalStyleMetaFromFieldMetaData(fieldMetaData) {
-    if (!this.isOrdinal()) {
+  _pluckOrdinalStyleMetaFromFieldMetaData(styleMetaData: StyleMetaData) {
+    if (!this.isOrdinal() || !this._field) {
       return null;
     }
 
-    const stats = fieldMetaData[this._field.getRootName()];
-    if (!stats) {
+    const stats = styleMetaData[this._field.getRootName()];
+    if (!stats || !('avg' in stats)) {
       return null;
     }
 
@@ -242,55 +286,56 @@ export class DynamicStyleProperty extends AbstractStyleProperty {
     };
   }
 
-  pluckCategoricalStyleMetaFromFieldMetaData(fieldMetaData) {
-    if (!this.isCategorical()) {
+  _pluckCategoricalStyleMetaFromFieldMetaData(styleMetaData: StyleMetaData) {
+    if (!this.isCategorical() || !this._field) {
       return null;
     }
 
-    const rootFieldName = this._field.getRootName();
-    if (!fieldMetaData[rootFieldName] || !fieldMetaData[rootFieldName].buckets) {
+    const fieldMeta = styleMetaData[this._field.getRootName()];
+    if (!fieldMeta || !('buckets' in fieldMeta)) {
       return null;
     }
 
-    const ordered = fieldMetaData[rootFieldName].buckets.map((bucket) => {
-      return {
-        key: bucket.key,
-        count: bucket.doc_count,
-      };
-    });
     return {
-      categories: ordered,
+      categories: fieldMeta.buckets.map((bucket) => {
+        return {
+          key: bucket.key,
+          count: bucket.doc_count,
+        };
+      }),
     };
   }
 
-  formatField(value) {
+  formatField(value: string | undefined): string {
     if (this.getField()) {
-      const fieldName = this.getField().getName();
+      const fieldName = this.getFieldName();
       const fieldFormatter = this._getFieldFormatter(fieldName);
-      return fieldFormatter ? fieldFormatter(value) : value;
+      return fieldFormatter ? fieldFormatter(value) : super.formatField(value);
     } else {
-      return value;
+      return super.formatField(value);
     }
-  }
-
-  getNumericalMbFeatureStateValue(value) {
-    const valueAsFloat = parseFloat(value);
-    return isNaN(valueAsFloat) ? null : valueAsFloat;
   }
 
   renderLegendDetailRow() {
     return null;
   }
 
-  renderFieldMetaPopover(onFieldMetaOptionsChange) {
+  renderFieldMetaPopover(onFieldMetaOptionsChange: (fieldMetaOptions: FieldMetaOptions) => void) {
     if (!this.supportsFieldMeta()) {
       return null;
     }
 
     return this.isCategorical() ? (
-      <CategoricalFieldMetaPopover styleProperty={this} onChange={onFieldMetaOptionsChange} />
+      <CategoricalFieldMetaPopover
+        fieldMetaOptions={this.getFieldMetaOptions()}
+        onChange={onFieldMetaOptionsChange}
+      />
     ) : (
-      <OrdinalFieldMetaPopover styleProperty={this} onChange={onFieldMetaOptionsChange} />
+      <OrdinalFieldMetaPopover
+        fieldMetaOptions={this.getFieldMetaOptions()}
+        styleName={this.getStyleName()}
+        onChange={onFieldMetaOptionsChange}
+      />
     );
   }
 }
