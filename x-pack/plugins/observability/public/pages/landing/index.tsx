756d391f50723e6c4c45e11735c6d398697c80a4
cauemarcondes <caue.marcondes@elastic.co>
2020-07-17 15:37:31 +0200

    fixing TS error

--- x-pack/plugins/observability/public/pages/landing/index.tsx
+++ x-pack/plugins/observability/public/pages/landing/index.tsx
@@ -10,7 +10,6 @@ import {
   EuiFlexGrid,
   EuiFlexGroup,
   EuiFlexItem,
-  EuiIcon,
   EuiImage,
   EuiSpacer,
   EuiText,
@@ -19,10 +18,10 @@ import {
 import { i18n } from '@kbn/i18n';
 import React, { useContext } from 'react';
 import styled, { ThemeContext } from 'styled-components';
+import { IngestManagerPanel } from '../../components/app/ingest_manager_panel';
 import { WithHeaderLayout } from '../../components/app/layout/with_header';
 import { usePluginContext } from '../../hooks/use_plugin_context';
 import { appsSection } from '../home/section';
-import { IngestManagerPanel } from '../../components/app/ingest_manager_panel';
 
 const EuiCardWithoutPadding = styled(EuiCard)`
   padding: 0;
