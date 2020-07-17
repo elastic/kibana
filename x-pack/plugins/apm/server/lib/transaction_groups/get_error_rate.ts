a0eda3dea31e15465b3584f9d3516f62aa724186
cauemarcondes <caue.marcondes@elastic.co>
2020-07-17 13:27:19 +0200

    removing console log

--- x-pack/plugins/apm/server/lib/transaction_groups/get_error_rate.ts
+++ x-pack/plugins/apm/server/lib/transaction_groups/get_error_rate.ts
@@ -90,6 +90,5 @@ export async function getErrorRate({
       .map((errorRate) => errorRate.y)
       .filter((y) => isFinite(y))
   );
-  console.log(JSON.stringify({ noHits, erroneousTransactionsRate, average }));
   return { noHits, erroneousTransactionsRate, average };
 }
