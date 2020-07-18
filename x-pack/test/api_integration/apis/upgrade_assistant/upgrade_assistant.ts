81640fee9cd1a93d0234578418da812fac788e7b
CJ Cenizal <cj@cenizal.com>
2020-07-17 22:36:05 -0700

    Fix import path.

--- x-pack/test/api_integration/apis/upgrade_assistant/upgrade_assistant.ts
+++ x-pack/test/api_integration/apis/upgrade_assistant/upgrade_assistant.ts
@@ -6,7 +6,7 @@
 
 import expect from '@kbn/expect';
 
-import { FtrProviderContext } from '../../../ftr_provider_context';
+import { FtrProviderContext } from '../../ftr_provider_context';
 import { reindexOperationWithLargeErrorMessage } from './reindex_operation_with_large_error_message';
 
 export default function ({ getService }: FtrProviderContext) {
