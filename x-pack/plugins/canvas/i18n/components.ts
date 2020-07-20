05dab35d8cebe1e3e9434bf3dcf367c65f1b141c
Clint Andrew Hall <clint.hall@elastic.co>
2020-07-19 21:15:18 -0400

    Re-adding i18n... ugh

--- x-pack/plugins/canvas/i18n/components.ts
+++ x-pack/plugins/canvas/i18n/components.ts
@@ -110,6 +110,18 @@ export const ComponentStrings = {
       i18n.translate('xpack.canvas.asset.thumbnailAltText', {
         defaultMessage: 'Asset thumbnail',
       }),
+    getConfirmModalButtonLabel: () =>
+      i18n.translate('xpack.canvas.asset.confirmModalButtonLabel', {
+        defaultMessage: 'Remove',
+      }),
+    getConfirmModalMessageText: () =>
+      i18n.translate('xpack.canvas.asset.confirmModalDetail', {
+        defaultMessage: 'Are you sure you want to remove this asset?',
+      }),
+    getConfirmModalTitle: () =>
+      i18n.translate('xpack.canvas.asset.confirmModalTitle', {
+        defaultMessage: 'Remove Asset',
+      }),
   },
   AssetManager: {
     getButtonLabel: () =>
