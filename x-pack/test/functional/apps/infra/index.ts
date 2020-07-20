772eb83446e00968d722dc0fd1b30cb232a7aceb
Larry Gregory <larry.gregory@elastic.co>
2020-07-20 09:50:16 -0400

    re-enable infra functional tests

--- x-pack/test/functional/apps/infra/index.ts
+++ x-pack/test/functional/apps/infra/index.ts
@@ -10,12 +10,12 @@ export default ({ loadTestFile }: FtrProviderContext) => {
   describe('InfraOps app', function () {
     this.tags('ciGroup7');
 
-    // loadTestFile(require.resolve('./home_page'));
+    loadTestFile(require.resolve('./home_page'));
     loadTestFile(require.resolve('./feature_controls'));
-    // loadTestFile(require.resolve('./log_entry_categories_tab'));
-    // loadTestFile(require.resolve('./log_entry_rate_tab'));
-    // loadTestFile(require.resolve('./logs_source_configuration'));
-    // loadTestFile(require.resolve('./metrics_source_configuration'));
-    // loadTestFile(require.resolve('./link_to'));
+    loadTestFile(require.resolve('./log_entry_categories_tab'));
+    loadTestFile(require.resolve('./log_entry_rate_tab'));
+    loadTestFile(require.resolve('./logs_source_configuration'));
+    loadTestFile(require.resolve('./metrics_source_configuration'));
+    loadTestFile(require.resolve('./link_to'));
   });
 };
