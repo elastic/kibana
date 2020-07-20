8234fa43b29b5b6fdbd39f7e150010313c8a4fa2
Brian Seeders <brian.seeders@elastic.co>
2020-07-20 12:32:18 -0400

    Fix setup script

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source test/scripts/jenkins_setup.sh
+source src/dev/ci_setup/setup_env.sh
 
 if [[ -z "$IS_PIPELINE_JOB" ]] ; then
   trap 'node "$KIBANA_DIR/src/dev/failed_tests/cli"' EXIT
