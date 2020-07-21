bc2429ccb14e489b8863a059a4e7ab7a09b19bdc
Steph Milovic <stephanie.milovic@elastic.co>
2020-07-21 11:40:05 -0600

    new link style button and test cleaned

--- x-pack/plugins/security_solution/public/common/components/event_details/event_details.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/event_details.tsx
@@ -4,14 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { noop } from 'lodash/fp';
-import {
-  EuiButtonIcon,
-  EuiPopover,
-  EuiTabbedContent,
-  EuiTabbedContentTab,
-  EuiToolTip,
-} from '@elastic/eui';
+import { EuiLink, EuiTabbedContent, EuiTabbedContentTab } from '@elastic/eui';
 import React, { useMemo } from 'react';
 import styled from 'styled-components';
 
@@ -26,18 +19,11 @@ import { COLLAPSE, COLLAPSE_EVENT } from '../../../timelines/components/timeline
 
 export type View = 'table-view' | 'json-view';
 
-const PopoverContainer = styled.div`
-  left: -40px;
-  position: relative;
-  top: 10px;
-  height: 0;
+const CollapseLink = styled(EuiLink)`
+  margin: 20px 0;
 `;
 
-const CollapseButton = styled(EuiButtonIcon)`
-  border: 1px solid;
-`;
-
-CollapseButton.displayName = 'CollapseButton';
+CollapseLink.displayName = 'CollapseLink';
 
 interface Props {
   browserFields: BrowserFields;
@@ -71,21 +57,6 @@ export const EventDetails = React.memo<Props>(
     timelineId,
     toggleColumn,
   }) => {
-    const button = useMemo(
-      () => (
-        <EuiToolTip content={COLLAPSE_EVENT}>
-          <CollapseButton
-            aria-label={COLLAPSE}
-            data-test-subj="collapse"
-            iconType="cross"
-            size="s"
-            onClick={onEventToggled}
-          />
-        </EuiToolTip>
-      ),
-      [onEventToggled]
-    );
-
     const tabs: EuiTabbedContentTab[] = useMemo(
       () => [
         {
@@ -114,19 +85,14 @@ export const EventDetails = React.memo<Props>(
 
     return (
       <Details data-test-subj="eventDetails">
-        <PopoverContainer>
-          <EuiPopover
-            button={button}
-            isOpen={false}
-            closePopover={noop}
-            repositionOnScroll={true}
-          />
-        </PopoverContainer>
         <EuiTabbedContent
           tabs={tabs}
           selectedTab={view === 'table-view' ? tabs[0] : tabs[1]}
           onTabClick={(e) => onViewSelected(e.id as View)}
         />
+        <CollapseLink aria-label={COLLAPSE} data-test-subj="collapse" onClick={onEventToggled}>
+          {COLLAPSE_EVENT}
+        </CollapseLink>
       </Details>
     );
   }
