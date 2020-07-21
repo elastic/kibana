76d0a66fa0e8299f1b80d61f8303c8549465d3d7
Jen Huang <its.jenetic@gmail.com>
2020-07-21 15:11:41 -0700

    Handle long agent config descriptions without spaces

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/agent_config/details_page/index.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/sections/agent_config/details_page/index.tsx
@@ -92,7 +92,7 @@ export const AgentConfigDetailsPage: React.FunctionComponent = () => {
         {agentConfig && agentConfig.description ? (
           <EuiFlexItem>
             <EuiSpacer size="s" />
-            <EuiText color="subdued" size="s">
+            <EuiText color="subdued" size="s" className="eui-textBreakWord">
               {agentConfig.description}
             </EuiText>
           </EuiFlexItem>
