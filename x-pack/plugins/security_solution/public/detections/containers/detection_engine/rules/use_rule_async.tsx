1dfc3654c838730c89ac386d673d7b472b5279fe
Ryland Herrick <ryalnd@gmail.com>
2020-07-22 16:51:51 -0500

    Display toast error if rule fetch fails
    This should now have feature parity with useRule, while additionally
    providing a function to refresh the rule.

--- x-pack/plugins/security_solution/public/detections/containers/detection_engine/rules/use_rule_async.tsx
+++ x-pack/plugins/security_solution/public/detections/containers/detection_engine/rules/use_rule_async.tsx
@@ -8,8 +8,10 @@ import { useEffect, useCallback } from 'react';
 
 import { useAsync, withOptionalSignal } from '../../../../shared_imports';
 import { useHttp } from '../../../../common/lib/kibana';
+import { useAppToasts } from '../../../../common/hooks/use_app_toasts';
 import { pureFetchRuleById } from './api';
 import { Rule } from './types';
+import * as i18n from './translations';
 
 export interface UseRuleAsync {
   error: unknown;
@@ -24,6 +26,7 @@ const _useRuleAsync = () => useAsync(_fetchRule);
 export const useRuleAsync = (ruleId: string): UseRuleAsync => {
   const { start, loading, result, error } = _useRuleAsync();
   const http = useHttp();
+  const { addError } = useAppToasts();
 
   const fetch = useCallback(() => {
     start({ id: ruleId, http });
@@ -33,6 +36,13 @@ export const useRuleAsync = (ruleId: string): UseRuleAsync => {
   useEffect(() => {
     fetch();
   }, [fetch]);
+
+  // toast on error
+  useEffect(() => {
+    if (error != null) {
+      addError(error, { title: i18n.RULE_AND_TIMELINE_FETCH_FAILURE });
+    }
+  }, [addError, error]);
 
   return { error, loading, refresh: fetch, rule: result ?? null };
 };
