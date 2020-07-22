e392bdb6617fedf63d21139eb1d31df9854afdae
Cauê Marcondes <55978943+cauemarcondes@users.noreply.github.com>
2020-07-22 14:01:29 +0100

    Adding api test for transaction_groups /breakdown and /avg_duration_by_browser (#72623)
    * adding api test for transaction_groups /breakdown and /avg_duration_by_browser
    
    * adding filter by transaction name
    
    * adding filter by transaction name
    
    * addressing pr comments
    
    * fixing TS issue
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/test/apm_api_integration/basic/tests/index.ts
+++ x-pack/test/apm_api_integration/basic/tests/index.ts
@@ -35,6 +35,8 @@ export default function apmApiIntegrationTests({ loadTestFile }: FtrProviderCont
       loadTestFile(require.resolve('./transaction_groups/top_transaction_groups'));
       loadTestFile(require.resolve('./transaction_groups/transaction_charts'));
       loadTestFile(require.resolve('./transaction_groups/error_rate'));
+      loadTestFile(require.resolve('./transaction_groups/breakdown'));
+      loadTestFile(require.resolve('./transaction_groups/avg_duration_by_browser'));
     });
   });
 }
