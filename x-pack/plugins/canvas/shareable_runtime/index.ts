73840f2cd7f7703599e2b34e33eae95f49d46753
Clint Andrew Hall <clint.hall@elastic.co>
2020-07-22 19:48:57 -0400

    Add Canvas Styles

--- x-pack/plugins/canvas/shareable_runtime/index.ts
+++ x-pack/plugins/canvas/shareable_runtime/index.ts
@@ -25,3 +25,6 @@ const uiStyles = require.context(
   /[\/\\](?!mixins|variables|_|\.|bootstrap_(light|dark))[^\/\\]+\.less/
 );
 uiStyles.keys().forEach((key) => uiStyles(key));
+
+// Import Canvas CSS
+require('../public/style/index.scss');
