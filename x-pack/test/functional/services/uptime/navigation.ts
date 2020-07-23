ec6ca08494b8f6b21cdf7438814c41e88e1a3e46
Justin Kambic <justin.kambic@elastic.co>
2020-07-23 09:53:02 -0400

    Trying to resolve flakiness.

--- x-pack/test/functional/services/uptime/navigation.ts
+++ x-pack/test/functional/services/uptime/navigation.ts
@@ -17,7 +17,7 @@ export function UptimeNavigationProvider({ getService, getPageObjects }: FtrProv
       if (await testSubjects.exists('uptimeSettingsToOverviewLink', { timeout: 0 })) {
         await testSubjects.click('uptimeSettingsToOverviewLink');
         await testSubjects.existOrFail('uptimeOverviewPage', { timeout: 2000 });
-      } else if (!(await testSubjects.exists('uptimeOverviewPage', { timeout: 0 }))) {
+      } else {
         await PageObjects.common.navigateToApp('uptime');
         await PageObjects.header.waitUntilLoadingHasFinished();
         await testSubjects.existOrFail('uptimeOverviewPage', { timeout: 2000 });
--- x-pack/test/functional_with_es_ssl/apps/uptime/alert_flyout.ts
+++ x-pack/test/functional_with_es_ssl/apps/uptime/alert_flyout.ts
@@ -137,7 +137,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       });
     });
 
-    describe.skip('tls alert', function () {
+    describe('tls alert', function () {
       const DEFAULT_DATE_START = 'Sep 10, 2019 @ 12:40:08.078';
       const DEFAULT_DATE_END = 'Sep 11, 2019 @ 19:40:08.078';
       let alerts: any;
