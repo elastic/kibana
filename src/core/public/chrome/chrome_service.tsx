4ae2a908cacadfc7cd32e7d79164d98a576358f1
sulemanof <sul-dan@mail.ru>
2020-07-23 17:58:57 +0300

    Reset chrome help extension while switching an app

--- src/core/public/chrome/chrome_service.tsx
+++ src/core/public/chrome/chrome_service.tsx
@@ -157,6 +157,11 @@ export class ChromeService {
     const recentlyAccessed = await this.recentlyAccessed.start({ http });
     const docTitle = this.docTitle.start({ document: window.document });
 
+    // erase the help menu extension from previous app while switching to a next app
+    application.currentAppId$.subscribe(() => {
+      helpExtension$.next(undefined);
+    });
+
     const setIsNavDrawerLocked = (isLocked: boolean) => {
       isNavDrawerLocked$.next(isLocked);
       localStorage.setItem(IS_LOCKED_KEY, `${isLocked}`);
