1b5b756340211412fb9cdf2f6a672fa61cd8da0c
Jonathan Budzenski <jon@budzenski.me>
2020-07-23 01:14:18 -0500

    [build/packages] reload service file changes on upgrade

--- src/dev/build/tasks/os_packages/package_scripts/post_install.sh
+++ src/dev/build/tasks/os_packages/package_scripts/post_install.sh
@@ -3,6 +3,8 @@ set -e
 
 export KBN_PATH_CONF=${KBN_PATH_CONF:-<%= configDir %>}
 
+IS_UPGRADE=false
+
 case $1 in
   # Debian
   configure)
@@ -13,6 +15,10 @@ case $1 in
     if ! getent passwd "<%= user %>" >/dev/null; then
       adduser --quiet --system --no-create-home --disabled-password \
       --ingroup "<%= group %>" --shell /bin/false "<%= user %>"
+    fi
+
+    if [ -n "$2" ]; then
+      IS_UPGRADE=true
     fi
   ;;
   abort-deconfigure|abort-upgrade|abort-remove)
@@ -27,6 +33,10 @@ case $1 in
     if ! getent passwd "<%= user %>" >/dev/null; then
       useradd -r -g "<%= group %>" -M -s /sbin/nologin \
       -c "kibana service user" "<%= user %>"
+    fi
+
+    if [ "$1" = "2" ]; then
+      IS_UPGRADE=true
     fi
   ;;
 
@@ -44,3 +54,9 @@ chown :<%= group %> ${KBN_PATH_CONF}
 chown :<%= group %> ${KBN_PATH_CONF}/kibana.yml
 chmod 2750 ${KBN_PATH_CONF}
 chmod 660 ${KBN_PATH_CONF}/kibana.yml
+
+if [ "$IS_UPGRADE" = "true" ]; then
+  if command -v systemctl >/dev/null; then
+      systemctl daemon-reload
+  fi
+fi
