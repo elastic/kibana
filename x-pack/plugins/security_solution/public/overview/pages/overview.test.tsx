3a0034a93a500be32a62c53281c73fe78f6144a0
Angela Chuang <yi-chun.chuang@elastic.co>
2020-07-27 20:56:48 +0100

    rollback change

--- x-pack/plugins/security_solution/public/overview/pages/overview.test.tsx
+++ x-pack/plugins/security_solution/public/overview/pages/overview.test.tsx
@@ -52,30 +52,6 @@ const endpointNoticeMessage = (hasMessageValue: boolean) => {
 
 describe('Overview', () => {
   describe('rendering', () => {
-    describe('when indicesExist is true', () => {
-      beforeEach(() => {
-        (useWithSource as jest.Mock).mockReturnValue({
-          indicesExist: true,
-        });
-        (useIngestEnabledCheck as jest.Mock).mockReturnValue({ allEnabled: false });
-        const mockuseMessagesStorage: jest.Mock = useMessagesStorage as jest.Mock<
-          UseMessagesStorage
-        >;
-        mockuseMessagesStorage.mockImplementation(() => endpointNoticeMessage(false));
-      });
-
-      it('renders loading icon for sidebar', () => {
-        const wrapper = mount(
-          <TestProviders>
-            <MemoryRouter>
-              <Overview />
-            </MemoryRouter>
-          </TestProviders>
-        );
-        expect(wrapper.find('[data-test-subj="sidebar-loading"]').exists()).toBe(true);
-      });
-    });
-
     describe('when no index is available', () => {
       beforeEach(() => {
         (useWithSource as jest.Mock).mockReturnValue({
