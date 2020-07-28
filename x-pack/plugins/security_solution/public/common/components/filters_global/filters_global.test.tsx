72b11876adae663621c520fff9401dcfe59f417a
Andrew Goldstein <andrew-goldstein@users.noreply.github.com>
2020-07-28 10:00:19 -0600

    [Security Solution] Full screen fixes for Timeline based views (#73421) (#73455)
    ## Full screen fixes for Timeline based views
    
    - Fixes an issue where sometimes, Global navigation is hidden until the page is scrolled when exiting full screen mode
    - Improves performance by adding an intent delay before showing the draggable wrapper hover menu
    - Removes an unnecessary CSS transition
    
    ### Sometimes, Global navigation is hidden until the page is scrolled when exiting full screen mode
    
    Sometimes, after exiting `Full screen` mode in a page, for example, the `Detections` page, the global navigation, e.g. `Overview Detections Hosts...` is hidden until the page is scrolled.
    
    To reproduce:
    
    1) Navigate to the `Detections` page
    2) Click the `Full screen` button in the table
    3) Without scrolling the full screen view, click the `Exit full screen` button
    
    **Expected result**
    - [x] The global navigation e.g. `Overview Detections Hosts...` is visible above the search bar, per the screenshot below:
    
    ![correct-global-navigation](https://user-images.githubusercontent.com/4459398/87717870-571bef80-c76e-11ea-8b7b-1850094326b3.png)
    
    4) Once again, click the `Full screen` button in the table
    5) This time, expand an event, which will scroll the view
    6) Once again, click the `Exit full screen` button
    
    **Expected result**
    - [x] The global navigation e.g. `Overview Detections Hosts...` is visible above the search bar
    
    **Actual result**
    - [ ] Sometimes, the global navigation e.g. `Overview Detections Hosts...` is **not** visible until the page is scrolled

--- x-pack/plugins/security_solution/public/common/components/filters_global/filters_global.test.tsx
+++ x-pack/plugins/security_solution/public/common/components/filters_global/filters_global.test.tsx
@@ -4,20 +4,120 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { shallow } from 'enzyme';
+import { mount, ReactWrapper, shallow } from 'enzyme';
 import React from 'react';
+import { StickyContainer } from 'react-sticky';
 
 import '../../mock/match_media';
 import { FiltersGlobal } from './filters_global';
+import { TestProviders } from '../../mock/test_providers';
 
 describe('rendering', () => {
   test('renders correctly', () => {
     const wrapper = shallow(
-      <FiltersGlobal>
+      <FiltersGlobal globalFullScreen={false}>
         <p>{'Additional filters here.'}</p>
       </FiltersGlobal>
     );
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('full screen mode', () => {
+    let wrapper: ReactWrapper;
+
+    beforeEach(() => {
+      wrapper = mount(
+        <TestProviders>
+          <StickyContainer>
+            <FiltersGlobal globalFullScreen={true}>
+              <p>{'Filter content'}</p>
+            </FiltersGlobal>
+          </StickyContainer>
+        </TestProviders>
+      );
+    });
+
+    test('it does NOT render the sticky container', () => {
+      expect(wrapper.find('[data-test-subj="sticky-filters-global-container"]').exists()).toBe(
+        false
+      );
+    });
+
+    test('it renders the non-sticky container', () => {
+      expect(wrapper.find('[data-test-subj="non-sticky-global-container"]').exists()).toBe(true);
+    });
+
+    test('it does NOT render the container with a `display: none` style when `show` is true (the default)', () => {
+      expect(
+        wrapper.find('[data-test-subj="non-sticky-global-container"]').first()
+      ).not.toHaveStyleRule('display', 'none');
+    });
+  });
+
+  describe('non-full screen mode', () => {
+    let wrapper: ReactWrapper;
+
+    beforeEach(() => {
+      wrapper = mount(
+        <TestProviders>
+          <StickyContainer>
+            <FiltersGlobal globalFullScreen={false}>
+              <p>{'Filter content'}</p>
+            </FiltersGlobal>
+          </StickyContainer>
+        </TestProviders>
+      );
+    });
+
+    test('it renders the sticky container', () => {
+      expect(wrapper.find('[data-test-subj="sticky-filters-global-container"]').exists()).toBe(
+        true
+      );
+    });
+
+    test('it does NOT render the non-sticky container', () => {
+      expect(wrapper.find('[data-test-subj="non-sticky-global-container"]').exists()).toBe(false);
+    });
+
+    test('it does NOT render the container with a `display: none` style when `show` is true (the default)', () => {
+      expect(
+        wrapper.find('[data-test-subj="sticky-filters-global-container"]').first()
+      ).not.toHaveStyleRule('display', 'none');
+    });
+  });
+
+  describe('when show is false', () => {
+    test('in full screen mode it renders the container with a `display: none` style', () => {
+      const wrapper = mount(
+        <TestProviders>
+          <StickyContainer>
+            <FiltersGlobal globalFullScreen={true} show={false}>
+              <p>{'Filter content'}</p>
+            </FiltersGlobal>
+          </StickyContainer>
+        </TestProviders>
+      );
+
+      expect(
+        wrapper.find('[data-test-subj="non-sticky-global-container"]').first()
+      ).toHaveStyleRule('display', 'none');
+    });
+
+    test('in non-full screen mode it renders the container with a `display: none` style', () => {
+      const wrapper = mount(
+        <TestProviders>
+          <StickyContainer>
+            <FiltersGlobal globalFullScreen={false} show={false}>
+              <p>{'Filter content'}</p>
+            </FiltersGlobal>
+          </StickyContainer>
+        </TestProviders>
+      );
+
+      expect(
+        wrapper.find('[data-test-subj="sticky-filters-global-container"]').first()
+      ).toHaveStyleRule('display', 'none');
+    });
   });
 });
