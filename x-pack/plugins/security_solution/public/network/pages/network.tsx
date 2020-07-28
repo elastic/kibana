9bbf28a9816002938ad2c0a93121620b74c8b177
Andrew Goldstein <andrew-goldstein@users.noreply.github.com>
2020-07-28 08:09:35 -0600

    [Security Solution] Full screen fixes for Timeline based views (#73421)
    ## Full screen fixes for Timeline based views
    
    - Fixes an issue where sometimes, Global navigation is hidden until the page is scrolled when exiting full screen mode
    - Improves performance by adding an intent delay before showing the draggable wrapper hover menu
    - Removes an unnecessary CSS transition
    
    ### Sometimes, Global navigation is hidden until the page is scrolled when exiting full screen mode
    
    Sometimes, after exiting `Full screen` mode in a page, for example, the `Detections` page, the global navigation, e.g. `Overview Detections Hosts...` is hidden until the page is scrolled.
    
    To reproduce:
    
    1) Navigate to the `Detections` page
    2) Click the `Full screen` button in the table
    3) Without scrolling the full screen view, click the `Exit full screen` button
    
    **Expected result**
    - [x] The global navigation e.g. `Overview Detections Hosts...` is visible above the search bar, per the screenshot below:
    
    ![correct-global-navigation](https://user-images.githubusercontent.com/4459398/87717870-571bef80-c76e-11ea-8b7b-1850094326b3.png)
    
    4) Once again, click the `Full screen` button in the table
    5) This time, expand an event, which will scroll the view
    6) Once again, click the `Exit full screen` button
    
    **Expected result**
    - [x] The global navigation e.g. `Overview Detections Hosts...` is visible above the search bar
    
    **Actual result**
    - [ ] Sometimes, the global navigation e.g. `Overview Detections Hosts...` is **not** visible until the page is scrolled

--- x-pack/plugins/security_solution/public/network/pages/network.tsx
+++ x-pack/plugins/security_solution/public/network/pages/network.tsx
@@ -106,7 +106,10 @@ const NetworkComponent = React.memo<NetworkComponentProps & PropsFromRedux>(
         {indicesExist ? (
           <StickyContainer>
             <EuiWindowEvent event="resize" handler={noop} />
-            <FiltersGlobal show={showGlobalFilters({ globalFullScreen, graphEventId })}>
+            <FiltersGlobal
+              globalFullScreen={globalFullScreen}
+              show={showGlobalFilters({ globalFullScreen, graphEventId })}
+            >
               <SiemSearchBar indexPattern={indexPattern} id="global" />
             </FiltersGlobal>
 
