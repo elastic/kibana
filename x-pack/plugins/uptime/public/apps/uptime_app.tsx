0a4585a56b21dfffec36e3d9648f5f1480a4f1be
Shahzad <shahzad.muhammad@elastic.co>
2020-07-28 16:30:58 +0200

    [Uptime] Reduce miscellaneous uptime bundle size (#70632)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/uptime/public/apps/uptime_app.tsx
+++ x-pack/plugins/uptime/public/apps/uptime_app.tsx
@@ -9,24 +9,25 @@ import { i18n } from '@kbn/i18n';
 import React, { useEffect } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { I18nStart, ChromeBreadcrumb, CoreStart } from 'src/core/public';
-import { KibanaContextProvider } from '../../../../src/plugins/kibana_react/public';
-import { ClientPluginsSetup, ClientPluginsStart } from './apps/plugin';
-import { UMUpdateBadge } from './lib/lib';
+import { I18nStart, ChromeBreadcrumb, CoreStart } from 'kibana/public';
+import { KibanaContextProvider } from '../../../../../src/plugins/kibana_react/public';
+import { ClientPluginsSetup, ClientPluginsStart } from './plugin';
+import { UMUpdateBadge } from '../lib/lib';
 import {
   UptimeRefreshContextProvider,
   UptimeSettingsContextProvider,
   UptimeThemeContextProvider,
   UptimeStartupPluginsContextProvider,
-} from './contexts';
-import { CommonlyUsedRange } from './components/common/uptime_date_picker';
-import { setBasePath } from './state/actions';
-import { PageRouter } from './routes';
+} from '../contexts';
+import { CommonlyUsedRange } from '../components/common/uptime_date_picker';
+import { setBasePath } from '../state/actions';
+import { PageRouter } from '../routes';
 import {
   UptimeAlertsContextProvider,
   UptimeAlertsFlyoutWrapper,
-} from './components/overview/alerts';
-import { store } from './state';
+} from '../components/overview/alerts';
+import { store } from '../state';
+import { kibanaService } from '../state/kibana_service';
 
 export interface UptimeAppColors {
   danger: string;
@@ -85,6 +86,8 @@ const Application = (props: UptimeAppProps) => {
         : undefined
     );
   }, [canSave, renderGlobalHelpControls, setBadge]);
+
+  kibanaService.core = core;
 
   store.dispatch(setBasePath(basePath));
 
