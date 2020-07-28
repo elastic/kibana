3d00a5e1cae8009ce7cf3268c7844c58cbd12144
Paul Tavares <paul.tavares@elastic.co>
2020-07-28 13:37:23 -0400

    start work on mock host list apis

--- x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/store/mock_host_result_list.ts
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/store/mock_host_result_list.ts
@@ -4,8 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { HttpFetchOptions, HttpResponse, HttpStart } from 'kibana/public';
 import { HostInfo, HostResultList, HostStatus } from '../../../../../common/endpoint/types';
 import { EndpointDocGenerator } from '../../../../../common/endpoint/generate_data';
+import { INGEST_API_EPM_PACKAGES } from '../../policy/store/policy_list/services/ingest';
+
+const generator = new EndpointDocGenerator('seed');
 
 export const mockHostResultList: (options?: {
   total?: number;
@@ -26,7 +30,6 @@ export const mockHostResultList: (options?: {
 
   const hosts = [];
   for (let index = 0; index < actualCountToReturn; index++) {
-    const generator = new EndpointDocGenerator('seed');
     hosts.push({
       metadata: generator.generateHostMetadata(),
       host_status: HostStatus.ERROR,
@@ -45,9 +48,59 @@ export const mockHostResultList: (options?: {
  * returns a mocked API response for retrieving a single host metadata
  */
 export const mockHostDetailsApiResult = (): HostInfo => {
-  const generator = new EndpointDocGenerator('seed');
   return {
     metadata: generator.generateHostMetadata(),
     host_status: HostStatus.ERROR,
   };
+};
+
+/**
+ * Mock API handlers used by the Endpoint Host list
+ */
+const hostListApiPathHandlerMocks = () => {
+  return {
+    // list
+    '/api/endpoint/metadata': (
+      fetchOptions?: HttpFetchOptions = {
+        body: JSON.stringify({
+          paging_properties: [{ page_index: 0 }, { page_size: 10 }],
+        }),
+      }
+    ): HostResultList => {
+      const params = JSON.stringify(fetchOptions.body);
+
+      return {
+        hosts: [],
+        request_page_index: params.page_index,
+        request_page_size: params.page_size,
+        total: params.page_size,
+      };
+    },
+
+    // endpoint package info
+    [INGEST_API_EPM_PACKAGES]: (fetchOptions?: HttpFetchOptions) => {},
+
+    // Do policies referenced in host list exist
+    [INGEST_API_EPM_PACKAGES]: (fetchOptions?: HttpFetchOptions) => {},
+
+    // Policy Response
+    '/api/endpoint/policy_response': (fetchOptions?: HttpFetchOptions) => {},
+  };
+};
+
+export const setHostListApiMockImplementation = (
+  mockedHttpService: jest.Mocked<HttpStart>
+): void => {
+  const apiHandlers = hostListApiPathHandlerMocks();
+
+  mockedHttpService.get.mockImplementation(async (...args) => {
+    const [path] = args;
+    if (typeof path === 'string') {
+      if (apiHandlers[path]) {
+        return apiHandlers[path]();
+      }
+    }
+
+    throw new Error(`MOCK: unknown api request: ${path}`);
+  });
 };
