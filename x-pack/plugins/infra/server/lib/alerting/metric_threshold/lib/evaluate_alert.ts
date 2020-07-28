1a92d7ba4b29d9b2a6d66bc9362db9d6a25af9d2
Zacqary Xeper <zacqary.xeper@elastic.co>
2020-07-28 15:05:05 -0500

    [Metrics UI] Fix evaluating rate-aggregated alerts when there's no normalized value

--- x-pack/plugins/infra/server/lib/alerting/metric_threshold/lib/evaluate_alert.ts
+++ x-pack/plugins/infra/server/lib/alerting/metric_threshold/lib/evaluate_alert.ts
@@ -172,7 +172,7 @@ const getValuesFromAggregations = (
     }
     return buckets.map((bucket) => ({
       key: bucket.key_as_string,
-      value: bucket.aggregatedValue.value,
+      value: bucket.aggregatedValue?.value,
     }));
   } catch (e) {
     return NaN; // Error state
