7c6afea33cec379850d5a599a58f42a96fd6c626
Garrett Spong <spong@users.noreply.github.com>
2020-07-28 14:25:32 -0600

    [Security Solution][Detections] Fixes Risk Score and Severity mapping issues (#73233)
    ## Summary
    
    Fixes the following issues around Risk Score/Severity mapping:
    * Severity override option cannot be unselected during rule creation
    * Risk score override option cannot be unselected during rule creation
    * Cannot fill Critical Severity override at the first attempt
    * Cannot create a rule with just a Critical severity override
    
    Note: When editing rules there is the possibility of the mapping fields remaining `disabled` as they are locked to the 'isLoading' flag from the gql `useFetchIndexPatterns` call, which can sometimes not return/get stuck as loading. @patrykkopycinski has a draft PR to fix this here: https://github.com/elastic/kibana/pull/73199
    
    cc @MadameSheema 
    
    
    ##### Severity Mapping Fixes:
    <p align="center">
      <img width="500" src="https://user-images.githubusercontent.com/2946766/88497829-b653de00-cf7e-11ea-8e14-c351117b4282.gif" />
    </p>
    
    
    Now distinguishes between empty string/value
    <p align="center">
      <img width="500" src="https://user-images.githubusercontent.com/2946766/88497776-94f2f200-cf7e-11ea-821e-3766b7bed3dc.png" />
    </p>
    
    ##### Risk Score Mapping Fixes:
    <p align="center">
      <img width="500" src="https://user-images.githubusercontent.com/2946766/88497842-c075dc80-cf7e-11ea-8c41-606b20a6ac1c.gif" />
    </p>
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [X] Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/master/packages/kbn-i18n/README.md)
    - [X] [Documentation](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#writing-documentation) was added for features that require explanation or tutorials
      * Working with @benskelker on API docs. This PR adds `risk_score` (can be `undefined`) to `risk_score.mapping` for future compatibility with mapping to specific risk score values.
    - [X] [Unit or functional tests](https://github.com/elastic/kibana/blob/master/CONTRIBUTING.md#cross-browser-compatibility) were updated or added to match the most common scenarios

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/helpers.test.tsx
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/helpers.test.tsx
@@ -15,6 +15,7 @@ import {
   getModifiedAboutDetailsData,
   determineDetailsValue,
   userHasNoPermissions,
+  fillEmptySeverityMappings,
 } from './helpers';
 import { mockRuleWithEverything, mockRule } from './all/__mocks__/mock';
 import { esFilters } from '../../../../../../../../src/plugins/data/public';
@@ -95,9 +96,9 @@ describe('rule helpers', () => {
         name: 'Query with rule-id',
         note: '# this is some markdown documentation',
         references: ['www.test.co'],
-        riskScore: { value: 21, mapping: [] },
+        riskScore: { value: 21, mapping: [], isMappingChecked: false },
         ruleNameOverride: 'message',
-        severity: { value: 'low', mapping: [] },
+        severity: { value: 'low', mapping: fillEmptySeverityMappings([]), isMappingChecked: false },
         tags: ['tag1', 'tag2'],
         threat: [
           {
