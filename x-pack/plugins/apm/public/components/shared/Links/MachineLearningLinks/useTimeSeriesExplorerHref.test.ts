7bbf43cdfff7165a8ddfb9c318f66386b3ad7616
Oliver Gupte <olivergupte@gmail.com>
2020-07-29 16:11:54 -0700

    removes commented out test assertion

--- x-pack/plugins/apm/public/components/shared/Links/MachineLearningLinks/useTimeSeriesExplorerHref.test.ts
+++ x-pack/plugins/apm/public/components/shared/Links/MachineLearningLinks/useTimeSeriesExplorerHref.test.ts
@@ -27,12 +27,6 @@ describe('useTimeSeriesExplorerHref', () => {
       transactionType: 'request',
     });
 
-    // expect(href).toMatchInlineSnapshot(
-    //   `"/app/ml#/timeseriesexplorer?_g=(ml:(jobIds:!(apm-production-485b-high_mean_transaction_duration)),refreshInterval:(pause:true,value:'10000'),time:(from:'2020-07-29T17%3A27%3A29.000Z',to:'2020-07-29T18%3A45%3A00.000Z'))&_a=(mlTimeSeriesExplorer:(entities:(service.name:opbeans-java,transaction.type:request)))"`
-    // );
-    // expect(href).toMatchInlineSnapshot(
-    //   `"/app/ml#/timeseriesexplorer?_g=(ml:(jobIds:!(apm-production-485b-high_mean_transaction_duration)),refreshInterval:(pause:!t,value:10000),time:(from:'2020-07-29T17:27:29.000Z',to:'2020-07-29T18:45:00.000Z'))&_a=(mlTimeSeriesExplorer:(entities:(service.name:opbeans-java,transaction.type:request)))"`
-    // );
     expect(href).toMatchInlineSnapshot(
       `"/app/ml#/timeseriesexplorer?_g=(ml:(jobIds:!(apm-production-485b-high_mean_transaction_duration)),refreshInterval:(pause:!t,value:10000),time:(from:'2020-07-29T17:27:29.000Z',to:'2020-07-29T18:45:00.000Z'))&_a=(mlTimeSeriesExplorer:(entities:(service.name:opbeans-java,transaction.type:request),zoom:(from:'2020-07-29T17:27:29.000Z',to:'2020-07-29T18:45:00.000Z')))"`
     );
