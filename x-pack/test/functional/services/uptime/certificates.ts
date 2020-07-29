87a51bab362a6d3d16b15b2053cd445c7d5f56ba
Shahzad <shahzad31comp@gmail.com>
2020-07-29 16:49:48 +0200

    update missing func

--- x-pack/test/functional/services/uptime/certificates.ts
+++ x-pack/test/functional/services/uptime/certificates.ts
@@ -7,7 +7,7 @@
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
-export function UptimeCertProvider({ getService }: FtrProviderContext) {
+export function UptimeCertProvider({ getService, getPageObjects }: FtrProviderContext) {
   const testSubjects = getService('testSubjects');
   const retry = getService('retry');
 
