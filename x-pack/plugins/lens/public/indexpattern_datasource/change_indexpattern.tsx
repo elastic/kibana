2f84eed0aaab639d00c7e0d9fe84e8e3b0fe2604
Greg Thompson <thompson.glowe@gmail.com>
2020-07-29 11:02:23 -0700

    data-test-subj for indexPatter-switcher

--- x-pack/plugins/lens/public/indexpattern_datasource/change_indexpattern.tsx
+++ x-pack/plugins/lens/public/indexpattern_datasource/change_indexpattern.tsx
@@ -23,6 +23,7 @@ export function ChangeIndexPattern({
   onChangeIndexPattern,
   trigger,
   selectableProps,
+  'data-test-subj': dataTestSubj,
 }: {
   trigger: ChangeIndexPatternTriggerProps;
   indexPatternRefs: IndexPatternRef[];
@@ -65,6 +66,7 @@ export function ChangeIndexPattern({
           </EuiPopoverTitle>
           <EuiSelectable
             {...selectableProps}
+            data-test-subj={dataTestSubj}
             searchable
             singleSelection="always"
             options={indexPatternRefs.map(({ title, id }) => ({
