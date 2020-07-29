654f53626ec79df58780b3b3a8d04cfce0090161
John Schulz <john.schulz@elastic.co>
2020-07-29 11:57:28 -0400

    Add registry as npm dep via git url
    Quick example showing getting the public registry server from the spec file

--- x-pack/plugins/ingest_manager/server/services/epm/registry/registry_url.ts
+++ x-pack/plugins/ingest_manager/server/services/epm/registry/registry_url.ts
@@ -3,8 +3,38 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { DEFAULT_REGISTRY_URL } from '../../../constants';
+
+import { readFileSync } from 'fs';
+import path from 'path';
+import { safeLoad } from 'js-yaml';
 import { appContextService, licenseService } from '../../';
+
+function loadSpec() {
+  const xpackRootPath = path.join(__dirname, '../../../../../../..');
+  const openApiPath = path.join(
+    xpackRootPath,
+    'node_modules/@elastic/package-registry/openapi.yml'
+  );
+  const yamlString = readFileSync(openApiPath, 'utf-8');
+  const parsedSpec = safeLoad(yamlString);
+  return parsedSpec;
+}
+
+// https://swagger.io/specification/#server-object
+interface OASServerObject {
+  url: string;
+  description?: string;
+}
+// this works "for now" but not by design/contract
+// description can be emtpy, duplicated, and contain CommonMark markup
+// it's also possible that the registry could add a key like `x-server-id` to each server entry
+const getServer = (key: string) => {
+  const servers: OASServerObject[] = loadSpec()?.servers;
+  const server = servers.find((o: OASServerObject) => o.description === key);
+  return server;
+};
+
+const DEFAULT_REGISTRY_URL = getServer('public')?.url || 'https://epr.elastic.co';
 
 export const getRegistryUrl = (): string => {
   const license = licenseService.getLicenseInformation();
