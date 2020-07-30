bbe0372866f5436a64d7ffaf165a8efbf94e6b69
Shahzad <shahzad31comp@gmail.com>
2020-07-30 10:56:56 +0200

    fix type

--- x-pack/plugins/uptime/public/lib/alert_types/monitor_status.tsx
+++ x-pack/plugins/uptime/public/lib/alert_types/monitor_status.tsx
@@ -42,7 +42,7 @@ export const initMonitorStatusAlertType: AlertTypeInitializer = ({
         validateFunc = validateMonitorStatusParams;
       })();
     }
-    return validateFunc ? validateFunc(alertParams) : {};
+    return validateFunc ? validateFunc(alertParams) : { errors: undefined };
   },
   defaultActionMessage,
   requiresAppContext: false,
