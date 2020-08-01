e8de940d127aa9db03e579c71e6b35fb96456727
Clint Andrew Hall <clint.hall@elastic.co>
2020-07-31 21:04:46 -0400

    [Canvas][tech-debt] Refactor Toolbar (completes Kill Recompose.pure) (#73309)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/canvas/public/components/toolbar/index.ts
+++ x-pack/plugins/canvas/public/components/toolbar/index.ts
@@ -4,7 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { pure } from 'recompose';
-import { Navbar as Component } from './navbar';
-
-export const Navbar = pure(Component);
+export { Toolbar } from './toolbar';
+export { Toolbar as ToolbarComponent } from './toolbar.component';
