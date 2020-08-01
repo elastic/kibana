1caec00ff19fa056c7c9321b481eb59451cf30ec
Clint Andrew Hall <clint.hall@elastic.co>
2020-08-01 07:28:00 -0400

    [Canvas] Storybook Redux Addon (#73227)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    # Conflicts:
    #	x-pack/plugins/canvas/storybook/decorators/router_decorator.tsx

--- x-pack/plugins/canvas/storybook/middleware.ts
+++ x-pack/plugins/canvas/storybook/middleware.ts
@@ -4,11 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-const path = require('path');
-const serve = require('serve-static');
+import path from 'path';
+// @ts-expect-error
+import serve from 'serve-static';
 
 // Extend the Storybook Middleware to include a route to access Legacy UI assets
-module.exports = function (router) {
+module.exports = function (router: { get: (...args: any[]) => void }) {
   router.get(
     '/ui',
     serve(path.resolve(__dirname, '../../../../../src/core/server/core_app/assets'))
