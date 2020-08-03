55ec34af257f67375a8fe743782886bf557cac05
Lukas Olson <olson.lukas@gmail.com>
2020-08-03 15:14:15 -0700

    [Search] Fix telemetry collection to not send extra requests (#73382) (#74158)
    * [Search] Fix telemetry collection to not send extra requests
    
    * Update tracker to collect duration from ES response
    
    * Fix type check
    
    * Update docs and types
    
    * Fix typescript
    
    Co-authored-by: Liza K <liza.katz@elastic.co>
    
    Co-authored-by: Liza K <liza.katz@elastic.co>

--- x-pack/plugins/data_enhanced/server/plugin.ts
+++ x-pack/plugins/data_enhanced/server/plugin.ts
@@ -11,20 +11,28 @@ import {
   Plugin,
 } from '../../../../src/core/server';
 import { ES_SEARCH_STRATEGY } from '../../../../src/plugins/data/common';
-import { PluginSetup as DataPluginSetup } from '../../../../src/plugins/data/server';
+import {
+  PluginSetup as DataPluginSetup,
+  PluginStart as DataPluginStart,
+  usageProvider,
+} from '../../../../src/plugins/data/server';
 import { enhancedEsSearchStrategyProvider } from './search';
+import { UsageCollectionSetup } from '../../../../src/plugins/usage_collection/server';
 
 interface SetupDependencies {
   data: DataPluginSetup;
+  usageCollection?: UsageCollectionSetup;
 }
 
 export class EnhancedDataServerPlugin implements Plugin<void, void, SetupDependencies> {
   constructor(private initializerContext: PluginInitializerContext) {}
 
-  public setup(core: CoreSetup, deps: SetupDependencies) {
+  public setup(core: CoreSetup<DataPluginStart>, deps: SetupDependencies) {
+    const usage = deps.usageCollection ? usageProvider(core) : undefined;
+
     deps.data.search.registerSearchStrategy(
       ES_SEARCH_STRATEGY,
-      enhancedEsSearchStrategyProvider(this.initializerContext.config.legacy.globalConfig$)
+      enhancedEsSearchStrategyProvider(this.initializerContext.config.legacy.globalConfig$, usage)
     );
   }
 
