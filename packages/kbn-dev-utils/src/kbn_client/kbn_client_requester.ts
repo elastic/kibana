9e53c3306783e7b094dbb5f63810ce90265dc8bb
Paul Tavares <paul.tavares@elastic.co>
2020-08-03 18:19:46 -0400

    Add generic response type to KbnClient.request + support for headers

--- packages/kbn-dev-utils/src/kbn_client/kbn_client_requester.ts
+++ packages/kbn-dev-utils/src/kbn_client/kbn_client_requester.ts
@@ -60,6 +60,7 @@ export interface ReqOptions {
   path: string;
   query?: Record<string, any>;
   method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  headers?: Record<string, string>;
   body?: any;
   retries?: number;
 }
@@ -117,6 +118,7 @@ export class KbnClientRequester {
           params: options.query,
           headers: {
             'kbn-xsrf': 'kbn-client',
+            ...options.headers,
           },
           httpsAgent: this.httpsAgent,
         });
