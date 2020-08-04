0f2bdd7486ebd4db91ffb34b57f705052276b352
Gloria Hornero <snootchie.boochies@gmail.com>
2020-08-04 16:18:41 +0200

    refactors code

--- x-pack/plugins/security_solution/cypress/integration/timeline_events.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/timeline_events.spec.ts
@@ -4,9 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { PIN_EVENT } from '../screens/timeline';
+
 import { loginAndWaitForPage } from '../tasks/login';
 import { openTimeline } from '../tasks/security_main';
-import { populateTimeline } from '../tasks/timeline';
+import { pinFirstEvent, populateTimeline, unpinFirstEvent } from '../tasks/timeline';
 
 import { HOSTS_URL } from '../urls/navigation';
 
@@ -18,20 +20,20 @@ describe('timeline events', () => {
   });
 
   after(() => {
-    cy.get('[data-test-subj="pin"]').first().click({ force: true });
+    unpinFirstEvent();
   });
 
   it('pins the first event to the timeline', () => {
     cy.server();
     cy.route('POST', '**/api/solutions/security/graphql').as('persistTimeline');
 
-    cy.get('[data-test-subj="pin"]').first().click({ force: true });
+    pinFirstEvent();
 
     cy.wait('@persistTimeline', { timeout: 10000 }).then((response) => {
       cy.wrap(response.status).should('eql', 200);
       cy.wrap(response.xhr.responseText).should('include', 'persistPinnedEventOnTimeline');
     });
 
-    cy.get('[data-test-subj="pin"]').should('have.attr', 'aria-label', 'Pinned event');
+    cy.get(PIN_EVENT).should('have.attr', 'aria-label', 'Pinned event');
   });
 });
