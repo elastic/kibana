840389706340dd321828cf327eabc150b3068f00
Matt Kime <matt@mattki.me>
2020-08-03 23:47:21 -0500

    fix kibana setting functional test

--- test/functional/apps/management/_kibana_settings.js
+++ test/functional/apps/management/_kibana_settings.js
@@ -68,7 +68,7 @@ export default function ({ getService, getPageObjects }) {
       });
 
       it('when false, dashboard state is unhashed', async function () {
-        await PageObjects.settings.createIndexPattern('logstash-*');
+        // await PageObjects.settings.createIndexPattern('logstash-*');
         await PageObjects.common.navigateToApp('dashboard');
         await PageObjects.dashboard.clickNewDashboard();
         await PageObjects.timePicker.setDefaultAbsoluteRange();
