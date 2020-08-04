0167fc47992afb16c31ec502c00f1f8bd96b47ce
Brian Seeders <brian.seeders@elastic.co>
2020-08-04 13:13:51 -0400

    [CI] Add pipeline task queue framework and merge workers into one (#71268)
    # Conflicts:
    #	Jenkinsfile
    #	src/dev/notice/generate_notice_from_source.ts
    #	test/scripts/jenkins_build_kibana.sh
    #	test/scripts/jenkins_ci_group.sh
    #	test/scripts/jenkins_security_solution_cypress.sh
    #	test/scripts/jenkins_xpack_build_kibana.sh
    #	test/scripts/jenkins_xpack_saved_objects_field_metrics.sh
    #	vars/kibanaPipeline.groovy
    #	x-pack/plugins/canvas/storybook/storyshots.test.tsx

--- test/scripts/jenkins_xpack_saved_objects_field_metrics.sh
+++ test/scripts/jenkins_xpack_saved_objects_field_metrics.sh
@@ -2,8 +2,8 @@
 
 source test/scripts/jenkins_test_setup_xpack.sh
 
-checks-reporter-with-killswitch "X-Pack accessibility tests" \
+checks-reporter-with-killswitch "Capture Kibana Saved Objects field count metrics" \
   node scripts/functional_tests \
     --debug --bail \
     --kibana-install-dir "$KIBANA_INSTALL_DIR" \
-    --config test/accessibility/config.ts;
+    --config test/saved_objects_field_count/config.ts;
