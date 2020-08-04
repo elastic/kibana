d2829daaaca461a80f6391638e08afbd00148a13
Spencer <email@spalger.com>
2020-08-04 15:34:01 -0700

    [src/dev/build] build Kibana Platform bundles from source (#73591)
    Co-authored-by: spalger <spalger@users.noreply.github.com>

--- vars/kibanaPipeline.groovy
+++ vars/kibanaPipeline.groovy
@@ -108,6 +108,7 @@ def uploadGcsArtifact(uploadPrefix, pattern) {
 def withGcsArtifactUpload(workerName, closure) {
   def uploadPrefix = "kibana-ci-artifacts/jobs/${env.JOB_NAME}/${BUILD_NUMBER}/${workerName}"
   def ARTIFACT_PATTERNS = [
+    '**/target/public/.kbn-optimizer-cache',
     'target/kibana-*',
     'target/kibana-security-solution/**/*.png',
     'target/junit/**/*',
