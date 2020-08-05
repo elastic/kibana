27526d9b7da33b0e7bbddb5b159b68a374dccf03
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-08-05 08:34:05 +0200

    Fix TypeScript

--- src/plugins/discover/public/application/angular/doc_table/components/table_row.ts
+++ src/plugins/discover/public/application/angular/doc_table/components/table_row.ts
@@ -37,17 +37,8 @@ const TAGS_WITH_WS = />\s+</g;
 /**
  * Remove all of the whitespace between html tags
  * so that inline elements don't have extra spaces.
- *
- * If you have inline elements (span, a, em, etc.) and any
- * amount of whitespace around them in your markup, then the
- * browser will push them apart. This is ugly in certain
- * scenarios and is only fixed by removing the whitespace
- * from the html in the first place (or ugly css hacks).
- *
- * @param  {string} html - the html to modify
- * @return {string} - modified html
  */
-export function noWhiteSpace(html) {
+export function noWhiteSpace(html: string): string {
   return html.replace(TAGS_WITH_WS, '><');
 }
 
