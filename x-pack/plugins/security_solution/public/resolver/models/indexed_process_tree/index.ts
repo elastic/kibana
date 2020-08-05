cb2e3d8e1be671931ba72bd160d127c6067350ce
Robert Austin <robert.austin@elastic.co>
2020-08-05 15:40:03 -0400

    Update index.ts

--- x-pack/plugins/security_solution/public/resolver/models/indexed_process_tree/index.ts
+++ x-pack/plugins/security_solution/public/resolver/models/indexed_process_tree/index.ts
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/* eslint-disable no-shadow */
-
 import { orderByTime } from '../process_event';
 import { IndexedProcessTree } from '../../types';
 import { SafeResolverEvent } from '../../../../common/endpoint/types';
