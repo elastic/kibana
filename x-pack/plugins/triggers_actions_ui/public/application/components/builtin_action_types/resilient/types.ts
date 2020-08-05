15124482d97341d653717d5d2b44aa22781758a7
Christos Nasikas <christos.nasikas@elastic.co>
2020-08-05 19:04:22 +0300

    Add types (ui)

---
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/resilient/types.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export interface ResilientActionConnector {
+  config: ResilientConfig;
+  secrets: ResilientSecrets;
+}
+
+export interface ResilientActionParams {
+  subAction: string;
+  subActionParams: {
+    savedObjectId: string;
+    title: string;
+    description: string;
+    externalId: string | null;
+    issueTypeIds: string[];
+    severityCode: string;
+    comments: Array<{ commentId: string; comment: string }>;
+  };
+}
+
+interface IncidentConfiguration {
+  mapping: CasesConfigurationMapping[];
+}
+
+interface ResilientConfig {
+  apiUrl: string;
+  orgId: string;
+  incidentConfiguration?: IncidentConfiguration;
+  isCaseOwned?: boolean;
+}
+
+interface ResilientSecrets {
+  apiKeyId: string;
+  apiKeySecret: string;
+}
+
+// to remove
+export interface CasesConfigurationMapping {
+  source: string;
+  target: string;
+  actionType: string;
+}
