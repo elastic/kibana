afbb7e8c3fc939ef7aa803ba5b7d900842d3486b
kobelb <brandon.kobel@elastic.co>
2020-08-06 10:12:50 -0700

    Deleting unused type import

--- src/core/server/http/http_server.ts
+++ src/core/server/http/http_server.ts
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { Request, Server } from 'hapi';
+import { Server } from 'hapi';
 import HapiStaticFiles from 'inert';
 import url from 'url';
 
