ae61c2a01db7ad4283cfe9eb18d2f0f318512be7
Quynh Nguyen <quynh.nguyen@elastic.co>
2020-08-06 14:31:36 -0500

    [ML] Remove unused getStoppedPartitionsSchema

--- x-pack/plugins/ml/server/routes/schemas/anomaly_detectors_schema.ts
+++ x-pack/plugins/ml/server/routes/schemas/anomaly_detectors_schema.ts
@@ -201,11 +201,8 @@ export const updateModelSnapshotSchema = schema.object({
 
 export const forecastAnomalyDetector = schema.object({ duration: schema.any() });
 
-export const getCategorizerStatsSchema = schema.nullable(
+export const getStoppedPartitionsSchema = schema.nullable(
   schema.object({
     partitionByValue: schema.maybe(schema.string()),
   })
 );
-export const getStoppedPartitionsSchema = schema.object({
-  jobIds: schema.arrayOf(schema.string()),
-});
