95c05bb4a5ef7dfc8d054e912406bd1356c8e61a
Nathan Reese <reese.nathan@gmail.com>
2020-08-06 13:03:50 -0600

    [Maps] add initial location option that fits to data bounds

--- x-pack/plugins/maps/public/actions/map_actions.ts
+++ x-pack/plugins/maps/public/actions/map_actions.ts
@@ -6,7 +6,6 @@
 import { Dispatch } from 'redux';
 import turfBboxPolygon from '@turf/bbox-polygon';
 import turfBooleanContains from '@turf/boolean-contains';
-import uuid from 'uuid/v4';
 
 import { Filter, Query, TimeRange } from 'src/plugins/data/public';
 import { MapStoreState } from '../reducers/store';
@@ -44,12 +43,8 @@ import {
   UPDATE_DRAW_STATE,
   UPDATE_MAP_SETTING,
 } from './map_action_constants';
-import {
-  fitToDataBounds,
-  syncDataForAllJoinLayers,
-  syncDataForAllLayers,
-} from './data_request_actions';
-import { addLayer } from './layer_actions';
+import { autoFitToBounds, syncDataForAllLayers } from './data_request_actions';
+import { addLayer, addLayerWithoutDataSync } from './layer_actions';
 import { MapSettings } from '../reducers/map';
 import {
   DrawState,
@@ -57,6 +52,7 @@ import {
   MapExtent,
   MapRefreshConfig,
 } from '../../common/descriptor_types';
+import { INITIAL_LOCATION } from '../../common/constants';
 import { scaleBounds } from '../elasticsearch_geo_utils';
 
 export function setMapInitError(errorMessage: string) {
@@ -98,13 +94,21 @@ export function mapReady() {
       type: MAP_READY,
     });
 
-    getWaitingForMapReadyLayerListRaw(getState()).forEach((layerDescriptor) => {
-      dispatch<any>(addLayer(layerDescriptor));
-    });
-
+    const waitingForMapReadyLayerList = getWaitingForMapReadyLayerListRaw(getState());
     dispatch({
       type: CLEAR_WAITING_FOR_MAP_READY_LAYER_LIST,
     });
+
+    if (getMapSettings(getState()).initialLocation === INITIAL_LOCATION.AUTO_FIT_TO_BOUNDS) {
+      waitingForMapReadyLayerList.forEach((layerDescriptor) => {
+        dispatch<any>(addLayerWithoutDataSync(layerDescriptor));
+      });
+      dispatch<any>(autoFitToBounds());
+    } else {
+      waitingForMapReadyLayerList.forEach((layerDescriptor) => {
+        dispatch<any>(addLayer(layerDescriptor));
+      });
+    }
   };
 }
 
@@ -196,7 +200,6 @@ function generateQueryTimestamp() {
   return new Date().toISOString();
 }
 
-let lastSetQueryCallId: string = '';
 export function setQuery({
   query,
   timeFilters,
@@ -227,18 +230,7 @@ export function setQuery({
     });
 
     if (getMapSettings(getState()).autoFitToDataBounds) {
-      // Joins are performed on the client.
-      // As a result, bounds for join layers must also be performed on the client.
-      // Therefore join layers need to fetch data prior to auto fitting bounds.
-      const localSetQueryCallId = uuid();
-      lastSetQueryCallId = localSetQueryCallId;
-      await dispatch<any>(syncDataForAllJoinLayers());
-
-      // setQuery can be triggered before async data fetching completes
-      // Only continue execution path if setQuery has not been re-triggered.
-      if (localSetQueryCallId === lastSetQueryCallId) {
-        dispatch<any>(fitToDataBounds());
-      }
+      dispatch<any>(autoFitToBounds());
     } else {
       await dispatch<any>(syncDataForAllLayers());
     }
