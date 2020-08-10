68251ef944e3fe832f4550b3d12ea47ff30f147a
Andrea Del Rio <delrio.andre@gmail.com>
2020-08-09 17:59:37 -0700

    fix field_button test

--- src/plugins/kibana_react/public/field_button/field_button.test.tsx
+++ src/plugins/kibana_react/public/field_button/field_button.test.tsx
@@ -20,10 +20,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { FieldButton, SIZES } from './field_button';
 
+const noop = () => {};
+
 describe('sizes', () => {
   SIZES.forEach((size) => {
     test(`${size} is applied`, () => {
-      const component = shallow(<FieldButton fieldName="name" size={size} />);
+      const component = shallow(<FieldButton onClick={noop} fieldName="name" size={size} />);
       expect(component).toMatchSnapshot();
     });
   });
@@ -31,18 +33,36 @@ describe('sizes', () => {
 
 describe('isDraggable', () => {
   it('is rendered', () => {
-    const component = shallow(<FieldButton fieldName="name" isDraggable />);
+    const component = shallow(<FieldButton onClick={noop} fieldName="name" isDraggable />);
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('fieldIcon', () => {
+  it('is rendered', () => {
+    const component = shallow(
+      <FieldButton onClick={noop} fieldName="name" fieldIcon={<span>fieldIcon</span>} />
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('fieldAction', () => {
+  it('is rendered', () => {
+    const component = shallow(
+      <FieldButton onClick={noop} fieldName="name" fieldAction={<span>fieldAction</span>} />
+    );
     expect(component).toMatchSnapshot();
   });
 });
 
 describe('isActive', () => {
   it('defaults to false', () => {
-    const component = shallow(<FieldButton fieldName="name" />);
+    const component = shallow(<FieldButton onClick={noop} fieldName="name" />);
     expect(component).toMatchSnapshot();
   });
   it('renders true', () => {
-    const component = shallow(<FieldButton fieldName="name" isActive />);
+    const component = shallow(<FieldButton onClick={noop} fieldName="name" isActive />);
     expect(component).toMatchSnapshot();
   });
 });
