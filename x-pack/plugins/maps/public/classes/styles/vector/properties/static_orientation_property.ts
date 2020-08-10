ed34d6fe30c4692f98f8a68ab3144ff512e047e5
Nathan Reese <reese.nathan@gmail.com>
2020-08-10 10:46:11 -0600

    [maps] convert vector style properties to TS (#74553)
    * [maps] convert vector style properties to TS
    
    * more conversionts
    
    * tslint cleanup
    
    * final tslint fixes
    
    * update snapshots for bug fix
    
    * review feedback
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/maps/public/classes/styles/vector/properties/static_orientation_property.ts
+++ x-pack/plugins/maps/public/classes/styles/vector/properties/static_orientation_property.ts
@@ -4,10 +4,13 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { Map as MbMap } from 'mapbox-gl';
 import { StaticStyleProperty } from './static_style_property';
+import { VECTOR_STYLES } from '../../../../../common/constants';
+import { OrientationStaticOptions } from '../../../../../common/descriptor_types';
 
-export class StaticOrientationProperty extends StaticStyleProperty {
-  constructor(options, styleName) {
+export class StaticOrientationProperty extends StaticStyleProperty<OrientationStaticOptions> {
+  constructor(options: OrientationStaticOptions, styleName: VECTOR_STYLES) {
     if (typeof options.orientation !== 'number') {
       super({ orientation: 0 }, styleName);
     } else {
@@ -15,7 +18,7 @@ export class StaticOrientationProperty extends StaticStyleProperty {
     }
   }
 
-  syncIconRotationWithMb(symbolLayerId, mbMap) {
+  syncIconRotationWithMb(symbolLayerId: string, mbMap: MbMap) {
     mbMap.setLayoutProperty(symbolLayerId, 'icon-rotate', this._options.orientation);
   }
 }
