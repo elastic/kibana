90bde32964581542a4685b70557b1ba5444ebefa
James Gowdy <jgowdy@elastic.co>
2020-08-11 11:32:35 +0100

    fixing missing filter

--- x-pack/plugins/ml/public/application/timeseriesexplorer/timeseries_search_service.ts
+++ x-pack/plugins/ml/public/application/timeseriesexplorer/timeseries_search_service.ts
@@ -7,6 +7,7 @@
 import each from 'lodash/each';
 import find from 'lodash/find';
 import get from 'lodash/get';
+import filter from 'lodash/filter';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
