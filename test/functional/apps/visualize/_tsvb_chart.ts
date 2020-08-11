5e538d894fa130c962add72b1229052afc908e2d
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2020-08-11 15:14:26 +0300

    uncomment commented lines

--- test/functional/apps/visualize/_tsvb_chart.ts
+++ test/functional/apps/visualize/_tsvb_chart.ts
@@ -125,12 +125,12 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
       after(async () => {
         await security.testUser.restoreDefaults();
-        // await esArchiver.unload('kibana_sample_data_flights');
+        await esArchiver.unload('kibana_sample_data_flights');
       });
 
       it('should be able to switch between index patterns', async () => {
-        // const value = await PageObjects.visualBuilder.getMetricValue();
-        // expect(value).to.eql('156');
+        const value = await PageObjects.visualBuilder.getMetricValue();
+        expect(value).to.eql('156');
         await PageObjects.visualBuilder.clickPanelOptions('metric');
         const fromTime = 'Oct 22, 2018 @ 00:00:00.000';
         const toTime = 'Oct 28, 2018 @ 23:59:59.999';
