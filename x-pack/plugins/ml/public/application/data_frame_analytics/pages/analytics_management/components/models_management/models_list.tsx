e796419b33491593a61b7e329bfb49c6d381c5b1
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-08-11 13:14:50 +0200

    [ML] init tabs

--- x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_management/components/models_management/models_list.tsx
+++ x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_management/components/models_management/models_list.tsx
@@ -4,7 +4,8 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from './analytics_jobs_list';
-export * from './analytics_job_exploration';
-export * from './analytics_job_creation';
-export * from './models_list';
+import React, { FC } from 'react';
+
+export const ModelsList: FC = () => {
+  return <></>;
+};
