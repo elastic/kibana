fbd11766fb84f0b8a3cbfe1a7119243c8257350f
Matt Kime <matt@mattki.me>
2020-08-11 21:25:23 -0500

    reduce number of indexed fields in index pattern saved object

--- src/plugins/data/server/saved_objects/index_patterns.ts
+++ src/plugins/data/server/saved_objects/index_patterns.ts
@@ -42,16 +42,9 @@ export const indexPatternSavedObjectType: SavedObjectsType = {
     },
   },
   mappings: {
+    dynamic: false,
     properties: {
-      fieldFormatMap: { type: 'text' },
-      fields: { type: 'text' },
-      intervalName: { type: 'keyword' },
-      notExpandable: { type: 'boolean' },
-      sourceFilters: { type: 'text' },
-      timeFieldName: { type: 'keyword' },
       title: { type: 'text' },
-      type: { type: 'keyword' },
-      typeMeta: { type: 'keyword' },
     },
   },
   migrations: indexPatternSavedObjectTypeMigrations as any,
