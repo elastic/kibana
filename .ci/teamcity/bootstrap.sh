a569ac2722c37195b102d7681892e9a8035419c5
Brian Seeders <brian.seeders@elastic.co>
2020-08-13 17:46:05 -0400

    Add a couple of verify_no_git_changes

--- .ci/teamcity/bootstrap.sh
+++ .ci/teamcity/bootstrap.sh
@@ -11,11 +11,11 @@ verify_no_git_changes yarn kbn bootstrap --prefer-offline
 tc_end_block "yarn install and kbn bootstrap"
 
 tc_start_block "build kbn-pm"
-yarn kbn run build -i @kbn/pm
+verify_no_git_changes yarn kbn run build -i @kbn/pm
 tc_end_block "build kbn-pm"
 
 tc_start_block "build plugin list docs"
-node scripts/build_plugin_list_docs
+verify_no_git_changes node scripts/build_plugin_list_docs
 tc_end_block "build plugin list docs"
 
 tc_end_block "Bootstrap"
