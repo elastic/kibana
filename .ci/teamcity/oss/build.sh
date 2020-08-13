697e5826bd35758bb4477cf5ab14303749c21adc
Brian Seeders <brian.seeders@elastic.co>
2020-08-13 17:01:41 -0400

    Fix weird paste

--- .ci/teamcity/oss/build.sh
+++ .ci/teamcity/oss/build.sh
@@ -8,8 +8,8 @@ tc_start_block "Build Platform Plugins"
 node scripts/build_kibana_platform_plugins \
   --oss \
   --filter '!alertingExample' \
-  --scan-dir "${'$'}KIBANA_DIR/test/plugin_functional/plugins" \
-  --scan-dir "${'$'}KIBANA_DIR/test/interpreter_functional/plugins" \
+  --scan-dir "$KIBANA_DIR/test/plugin_functional/plugins" \
+  --scan-dir "$KIBANA_DIR/test/interpreter_functional/plugins" \
   --verbose
 tc_end_block "Build Platform Plugins"
 
