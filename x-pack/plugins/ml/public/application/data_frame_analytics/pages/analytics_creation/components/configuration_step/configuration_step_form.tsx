156e135151fc4192e1f7c94d91fda269cf53f110
Melissa Alvarez <melissa.alvarez@elastic.co>
2020-08-13 18:07:56 -0400

    display explain error message to the user

--- x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_creation/components/configuration_step/configuration_step_form.tsx
+++ x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_creation/components/configuration_step/configuration_step_form.tsx
@@ -213,13 +213,12 @@ export const ConfigurationStepForm: FC<CreateAnalyticsStepProps> = ({
         (errorMessage.includes('must have at most') || errorMessage.includes('must have at least'))
       ) {
         maxDistinctValuesErrorMessage = errorMessage;
-      }
-
-      if (errorMessage.includes('status_exception') && errorMessage.includes('unsupported type')) {
+      } else if (
+        errorMessage.includes('status_exception') &&
+        errorMessage.includes('unsupported type')
+      ) {
         unsupportedFieldsErrorMessage = errorMessage;
-      }
-
-      if (
+      } else if (
         errorMessage.includes('status_exception') &&
         errorMessage.includes('Unable to estimate memory usage as no documents')
       ) {
@@ -231,6 +230,19 @@ export const ConfigurationStepForm: FC<CreateAnalyticsStepProps> = ({
             },
           })
         );
+      } else {
+        toastNotifications.addDanger({
+          title: i18n.translate(
+            'xpack.ml.dataframe.analytics.create.unableToFetchExplainDataMessage',
+            {
+              defaultMessage: 'Something went wrong fetching analysis fields data.',
+              values: {
+                index: sourceIndex,
+              },
+            }
+          ),
+          text: errorMessage,
+        });
       }
 
       const fallbackModelMemoryLimit =
