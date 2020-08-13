f94c045e2d60e7ab244e651409a831f9e2a7a69a
Jean-Louis Leysens <jloleysens@gmail.com>
2020-08-13 12:54:28 +0200

    Added foreach processor form

---
+++ x-pack/plugins/ingest_pipelines/public/application/components/pipeline_processors_editor/components/manage_processor_form/processors/foreach.tsx
@@ -0,0 +1,80 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { FunctionComponent } from 'react';
+import { i18n } from '@kbn/i18n';
+
+import { FIELD_TYPES, fieldValidators, UseField } from '../../../../../../shared_imports';
+
+import { XJsonEditor } from '../field_components';
+
+import { FieldNameField } from './common_fields/field_name_field';
+import { FieldsConfig } from './shared';
+
+const { emptyField, isJsonField } = fieldValidators;
+
+const fieldsConfig: FieldsConfig = {
+  processor: {
+    type: FIELD_TYPES.TEXT,
+    deserializer: (v) => (v ? JSON.stringify(v, null, 2) : '{}'),
+    serializer: JSON.parse,
+    label: i18n.translate('xpack.ingestPipelines.pipelineEditor.foreachForm.processorFieldLabel', {
+      defaultMessage: 'Processor',
+    }),
+    helpText: i18n.translate('xpack.ingestPipelines.pipelineEditor.foreachForm.processorHelpText', {
+      defaultMessage: 'The processor to execute against each field value.',
+    }),
+    validations: [
+      {
+        validator: emptyField(
+          i18n.translate('xpack.ingestPipelines.pipelineEditor.failForm.processorRequiredError', {
+            defaultMessage: 'A processor is required.',
+          })
+        ),
+      },
+      {
+        validator: isJsonField(
+          i18n.translate(
+            'xpack.ingestPipelines.pipelineEditor.failForm.processorInvalidJsonError',
+            {
+              defaultMessage: 'Invalid JSON.',
+            }
+          )
+        ),
+      },
+    ],
+  },
+};
+
+export const Foreach: FunctionComponent = () => {
+  return (
+    <>
+      <FieldNameField
+        helpText={i18n.translate(
+          'xpack.ingestPipelines.pipelineEditor.failForm.fieldNameHelpText',
+          { defaultMessage: 'The array field.' }
+        )}
+      />
+
+      <UseField
+        component={XJsonEditor}
+        componentProps={{
+          editorProps: {
+            height: 200,
+            'aria-label': i18n.translate(
+              'xpack.ingestPipelines.pipelineEditor.customForm.optionsFieldAriaLabel',
+              {
+                defaultMessage: 'Configuration JSON editor',
+              }
+            ),
+          },
+        }}
+        config={fieldsConfig.processor}
+        path="fields.processor"
+      />
+    </>
+  );
+};
