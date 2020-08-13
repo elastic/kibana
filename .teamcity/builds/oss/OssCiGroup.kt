b207aeb0f95848a5c73930f073eaaf90e8713075
Brian Seeders <brian.seeders@elastic.co>
2020-08-13 17:32:36 -0400

    More cleanup

--- .teamcity/builds/oss/OssCiGroup.kt
+++ .teamcity/builds/oss/OssCiGroup.kt
@@ -2,39 +2,40 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 class OssCiGroup(val ciGroup: Int, val build: BuildType) : BuildType({
-    id("OssCiGroup_${ciGroup}")
-    name = "CI Group ${ciGroup}"
-    paused = true
+  id("OssCiGroup_${ciGroup}")
+  name = "CI Group ${ciGroup}"
+  paused = true
 
   params {
     param("env.KBN_NP_PLUGINS_BUILT", "true")
   }
 
-    steps {
-        script {
-            scriptContent = """
+  steps {
+    script {
+      name = "OSS CI Group ${ciGroup}"
+      scriptContent = """
                 #!/bin/bash
                 ./.ci/teamcity/oss/ci_group.sh ${ciGroup}
             """.trimIndent()
-        }
     }
+  }
 
-    features {
-        feature {
-            type = "xml-report-plugin"
-            param("xmlReportParsing.reportType", "junit")
-            param("xmlReportParsing.reportDirs", "target/**/TEST-*.xml")
-        }
+  features {
+    feature {
+      type = "xml-report-plugin"
+      param("xmlReportParsing.reportType", "junit")
+      param("xmlReportParsing.reportDirs", "target/**/TEST-*.xml")
     }
+  }
 
-     dependencies {
-         dependency(build) {
-             snapshot {
-             }
+  dependencies {
+    dependency(build) {
+      snapshot {
+      }
 
-             artifacts {
-                 artifactRules = "+:kibana-oss.tar.gz!** => /home/agent/work/kibana-build-oss"
-             }
-         }
-     }
+      artifacts {
+        artifactRules = "+:kibana-oss.tar.gz!** => /home/agent/work/kibana-build-oss"
+      }
+    }
+  }
 })
