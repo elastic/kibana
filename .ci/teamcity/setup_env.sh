708ca0805a059e9a72afcc06683c322836d4688f
Brian Seeders <brian.seeders@elastic.co>
2020-08-13 17:43:05 -0400

    Add some missing env var

--- .ci/teamcity/setup_env.sh
+++ .ci/teamcity/setup_env.sh
@@ -6,8 +6,8 @@ source "$(dirname "${0}")/util.sh"
 
 tc_set_env KIBANA_DIR "$(cd "$(dirname "$0")/../.." && pwd)"
 tc_set_env XPACK_DIR "$KIBANA_DIR/x-pack"
-tc_set_env CACHE_DIR "$HOME/.kibana"
 
+tc_set_env CACHE_DIR "$HOME/.kibana"
 tc_set_env PARENT_DIR "$(cd "$KIBANA_DIR/.."; pwd)"
 tc_set_env WORKSPACE "${WORKSPACE:-$PARENT_DIR}"
 
@@ -19,6 +19,10 @@ tc_set_env CHROMEDRIVER_CDNURL "https://us-central1-elastic-kibana-184716.cloudf
 tc_set_env RE2_DOWNLOAD_MIRROR "https://us-central1-elastic-kibana-184716.cloudfunctions.net/kibana-ci-proxy-cache"
 tc_set_env CYPRESS_DOWNLOAD_MIRROR "https://us-central1-elastic-kibana-184716.cloudfunctions.net/kibana-ci-proxy-cache/cypress"
 
+tc_set_env NODE_OPTIONS "$NODE_OPTIONS --max-old-space-size=4096"
+tc_set_env BABEL_DISABLE_CACHE true
+
+tc_set_env FORCE_COLOR 1
 tc_set_env TEST_BROWSER_HEADLESS 1
 tc_set_env CHECKS_REPORTER_ACTIVE false # TODO
 
