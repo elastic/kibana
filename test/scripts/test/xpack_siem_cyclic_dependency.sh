515aa3c75d7a825a10347b4dbbc632d3cf445f53
Brian Seeders <brian.seeders@elastic.co>
2020-08-14 11:41:20 -0400

    Some fixes

--- test/scripts/test/xpack_siem_cyclic_dependency.sh
+++ test/scripts/test/xpack_siem_cyclic_dependency.sh
@@ -3,4 +3,4 @@
 source src/dev/ci_setup/setup_env.sh
 
 cd x-pack
-checks-reporter-with-killswitch "X-Pack SIEM cyclic dependency test" node plugins/security_solution/scripts/check_circular_deps
+checks-reporter-with-killswitch "X-Pack SIEM cyclic dependency test" node plugins/siem/scripts/check_circular_deps
