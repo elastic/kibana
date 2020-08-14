0041e0c8c22a55a08ef1d97330415a6e102b04f6
spalger <spalger@users.noreply.github.com>
2020-08-14 13:54:25 -0700

    support customizing email for status emails

--- vars/kibanaPipeline.groovy
+++ vars/kibanaPipeline.groovy
@@ -221,7 +221,7 @@ def publishJunit() {
   }
 }
 
-def sendMail() {
+def sendMail(Map params = [:]) {
   // If the build doesn't have a result set by this point, there haven't been any errors and it can be marked as a success
   // The e-mail plugin for the infra e-mail depends upon this being set
   currentBuild.result = currentBuild.result ?: 'SUCCESS'
@@ -230,7 +230,7 @@ def sendMail() {
   if (buildStatus != 'SUCCESS' && buildStatus != 'ABORTED') {
     node('flyweight') {
       sendInfraMail()
-      sendKibanaMail()
+      sendKibanaMail(params)
     }
   }
 }
@@ -246,12 +246,14 @@ def sendInfraMail() {
   }
 }
 
-def sendKibanaMail() {
+def sendKibanaMail(Map params = [:]) {
+  def config = [to: 'build-kibana@elastic.co'] + params
+
   catchErrors {
     def buildStatus = buildUtils.getBuildStatus()
     if(params.NOTIFY_ON_FAILURE && buildStatus != 'SUCCESS' && buildStatus != 'ABORTED') {
       emailext(
-        to: 'build-kibana@elastic.co',
+        config.to,
         subject: "${env.JOB_NAME} - Build # ${env.BUILD_NUMBER} - ${buildStatus}",
         body: '${SCRIPT,template="groovy-html.template"}',
         mimeType: 'text/html',
