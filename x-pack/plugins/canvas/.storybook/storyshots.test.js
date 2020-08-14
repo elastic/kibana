7d081f76de9b3e00057f6ccaff4ae9e32393320c
Brian Seeders <brian.seeders@elastic.co>
2020-08-14 13:46:21 -0400

    Add missing import

--- x-pack/plugins/canvas/.storybook/storyshots.test.js
+++ x-pack/plugins/canvas/.storybook/storyshots.test.js
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import fs from 'fs';
 import path from 'path';
 import moment from 'moment';
 import 'moment-timezone';
