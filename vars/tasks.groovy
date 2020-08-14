f6f99effd571afa7e6f3b5e7ff43d80297f4f393
Brian Seeders <brian.seeders@elastic.co>
2020-08-14 11:43:20 -0400

    Add karma tests

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -31,6 +31,8 @@ def test() {
     // These 2 tasks require isolation because of hard-coded, conflicting ports and such, so let's use Docker here
     kibanaPipeline.scriptTaskDocker('Jest Integration Tests', 'test/scripts/test/jest_integration.sh'),
     kibanaPipeline.scriptTaskDocker('Mocha Tests', 'test/scripts/test/mocha.sh'),
+    kibanaPipeline.scriptTaskDocker('Karma Tests', 'test/scripts/test/karma_ci.sh'),
+    kibanaPipeline.scriptTaskDocker('X-Pack Karma Tests', 'test/scripts/test/xpack_karma.sh'),
 
     kibanaPipeline.scriptTask('Jest Unit Tests', 'test/scripts/test/jest_unit.sh'),
     kibanaPipeline.scriptTask('API Integration Tests', 'test/scripts/test/api_integration.sh'),
