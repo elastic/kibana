7f6682612b2e652f46cb13475c7c793d6fa0be7d
Spencer <email@spalger.com>
2020-08-15 13:31:37 -0700

    [jest] temporarily extend default test timeout (#75118)
    * [jest] temporarily extend default test timeout
    
    * fix, 30 seconds
    
    Co-authored-by: spalger <spalger@users.noreply.github.com>

--- src/dev/jest/config.js
+++ src/dev/jest/config.js
@@ -74,6 +74,7 @@ export default {
   setupFilesAfterEnv: [
     '<rootDir>/src/dev/jest/setup/mocks.js',
     '<rootDir>/src/dev/jest/setup/react_testing_library.js',
+    '<rootDir>/src/dev/jest/setup/default_timeout.js',
   ],
   coverageDirectory: '<rootDir>/target/kibana-coverage/jest',
   coverageReporters: ['html', 'text'],
