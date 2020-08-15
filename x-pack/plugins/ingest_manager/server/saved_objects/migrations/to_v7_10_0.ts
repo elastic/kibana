a2b365b436eab9fc34af28e91566590c741985ff
Jen Huang <its.jenetic@gmail.com>
2020-08-14 17:02:10 -0700

    Fix migration typos

--- x-pack/plugins/ingest_manager/server/saved_objects/migrations/to_v7_10_0.ts
+++ x-pack/plugins/ingest_manager/server/saved_objects/migrations/to_v7_10_0.ts
@@ -10,28 +10,28 @@ import { Agent, AgentEvent, AgentPolicy, PackagePolicy, EnrollmentAPIKey } from 
 
 export const migrateAgentToV7100: SavedObjectMigrationFn<
   Exclude<Agent, 'policy_id' | 'policy_revision'> & {
-    policy_id?: string;
-    policy_revision?: number | null;
+    config_id?: string;
+    config_revision?: number | null;
   },
   Agent
 > = (agentDoc) => {
-  agentDoc.attributes.policy_id = agentDoc.attributes.policy_id;
-  delete agentDoc.attributes.policy_id;
+  agentDoc.attributes.policy_id = agentDoc.attributes.config_id;
+  delete agentDoc.attributes.config_id;
 
-  agentDoc.attributes.policy_revision = agentDoc.attributes.policy_revision;
-  delete agentDoc.attributes.policy_revision;
+  agentDoc.attributes.policy_revision = agentDoc.attributes.config_revision;
+  delete agentDoc.attributes.config_revision;
 
   return agentDoc;
 };
 
 export const migrateAgentEventToV7100: SavedObjectMigrationFn<
   Exclude<AgentEvent, 'policy_id'> & {
-    policy_id?: string;
+    config_id?: string;
   },
   AgentEvent
 > = (agentEventDoc) => {
-  agentEventDoc.attributes.policy_id = agentEventDoc.attributes.policy_id;
-  delete agentEventDoc.attributes.policy_id;
+  agentEventDoc.attributes.policy_id = agentEventDoc.attributes.config_id;
+  delete agentEventDoc.attributes.config_id;
 
   return agentEventDoc;
 };
@@ -50,24 +50,24 @@ export const migrateAgentPolicyToV7100: SavedObjectMigrationFn<
 
 export const migrateEnrollmentApiKeysToV7100: SavedObjectMigrationFn<
   Exclude<EnrollmentAPIKey, 'policy_id'> & {
-    policy_id?: string;
+    config_id?: string;
   },
   EnrollmentAPIKey
 > = (enrollmentApiKeyDoc) => {
-  enrollmentApiKeyDoc.attributes.policy_id = enrollmentApiKeyDoc.attributes.policy_id;
-  delete enrollmentApiKeyDoc.attributes.policy_id;
+  enrollmentApiKeyDoc.attributes.policy_id = enrollmentApiKeyDoc.attributes.config_id;
+  delete enrollmentApiKeyDoc.attributes.config_id;
 
   return enrollmentApiKeyDoc;
 };
 
 export const migratePackagePolicyToV7100: SavedObjectMigrationFn<
   Exclude<PackagePolicy, 'policy_id'> & {
-    policy_id: string;
+    config_id: string;
   },
   PackagePolicy
 > = (packagePolicyDoc) => {
-  packagePolicyDoc.attributes.policy_id = packagePolicyDoc.attributes.policy_id;
-  delete packagePolicyDoc.attributes.policy_id;
+  packagePolicyDoc.attributes.policy_id = packagePolicyDoc.attributes.config_id;
+  delete packagePolicyDoc.attributes.config_id;
 
   return packagePolicyDoc;
 };
