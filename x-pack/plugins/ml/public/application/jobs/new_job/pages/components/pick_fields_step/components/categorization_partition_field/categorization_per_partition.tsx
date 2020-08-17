234756b0b1863d1ea41a1ddcaf382e425519801d
Quynh Nguyen <quynh.nguyen@elastic.co>
2020-08-17 11:23:09 -0500

    [ML] Move per partition dropdown to its own file

--- x-pack/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/categorization_partition_field/categorization_per_partition.tsx
+++ x-pack/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/categorization_partition_field/categorization_per_partition.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FC, useContext, useEffect, useState, Dispatch, SetStateAction } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import { EuiFormRow } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { JobCreatorContext } from '../../../job_creator_context';
@@ -13,65 +13,13 @@ import {
   CategorizationJobCreator,
   isCategorizationJobCreator,
 } from '../../../../../common/job_creator';
-import { newJobCapsService } from '../../../../../../../services/new_job_capabilities_service';
+
 import { Description } from './description';
 
 import { CategorizationPerPartitionSwitch } from './categorization_per_partition_switch';
-import { CategorizationPerPartitionFieldSelect } from './categorization_per_partition_input';
 import { CategorizationPerPartitionStopOnWarnSwitch } from './categorization_stop_on_warn_switch';
+import { CategorizationPerPartitionFieldDropdown } from './categorization_per_partition_dropdown';
 
-export const CategorizationPerPartitionFieldDropdown = ({
-  setEnablePerPartitionCategorization,
-}: {
-  setEnablePerPartitionCategorization: Dispatch<SetStateAction<boolean>>;
-}) => {
-  const { jobCreator: jc, jobCreatorUpdate, jobCreatorUpdated } = useContext(JobCreatorContext);
-  const jobCreator = jc as CategorizationJobCreator;
-
-  const [categorizationPartitionFieldName, setCategorizationPartitionFieldName] = useState<
-    string | null
-  >(jobCreator.categorizationPerPartitionField);
-  const { categoryFields } = newJobCapsService;
-
-  const filteredCategories = categoryFields.filter(
-    (c) => c.id !== jobCreator.categorizationFieldName
-  );
-  useEffect(() => {
-    jobCreator.categorizationPerPartitionField = categorizationPartitionFieldName;
-    jobCreatorUpdate();
-  }, [categorizationPartitionFieldName]);
-
-  useEffect(() => {
-    // set the first item in category as partition field by default
-    // because API requires partition_field to be defined in each detector with mlcategory
-    // if per-partition categorization is enabled
-    if (
-      jobCreator.perPartitionCategorization &&
-      jobCreator.categorizationPerPartitionField === null &&
-      filteredCategories.length > 0
-    ) {
-      jobCreator.categorizationPerPartitionField = filteredCategories[0].id;
-    }
-    setCategorizationPartitionFieldName(jobCreator.categorizationPerPartitionField);
-    setEnablePerPartitionCategorization(jobCreator.perPartitionCategorization);
-  }, [jobCreatorUpdated]);
-  return (
-    <EuiFormRow
-      label={
-        <FormattedMessage
-          id="xpack.ml.newJob.wizard.extraStep.categorizationJob.partitionFieldLabel"
-          defaultMessage="Partition field"
-        />
-      }
-    >
-      <CategorizationPerPartitionFieldSelect
-        fields={filteredCategories}
-        changeHandler={setCategorizationPartitionFieldName}
-        selectedField={categorizationPartitionFieldName || ''}
-      />
-    </EuiFormRow>
-  );
-};
 export const CategorizationPerPartitionField: FC = () => {
   const { jobCreator: jc, jobCreatorUpdated } = useContext(JobCreatorContext);
   const jobCreator = jc as AdvancedJobCreator | CategorizationJobCreator;
