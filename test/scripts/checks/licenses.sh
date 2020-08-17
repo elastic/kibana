6c0e2aa7fc9af54993b0cb706930afafc7784964
Tyler Smalley <tyler.smalley@elastic.co>
2020-08-17 15:41:37 -0700

    Actually run test
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- test/scripts/checks/licenses.sh
+++ test/scripts/checks/licenses.sh
@@ -3,4 +3,4 @@
 source src/dev/ci_setup/setup_env.sh
 
 checks-reporter-with-killswitch "Check licenses" node scripts/check_licenses --dev
-checks-reporter-with-killswitch "FOSSA license scan" fossa analyze -o && fossa test --timeout 3600
+checks-reporter-with-killswitch "FOSSA license scan" fossa analyze && fossa test --timeout 7200
