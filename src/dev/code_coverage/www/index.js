8be7935b307af983e55a0a0d271b635ea6386a4c
Tre' Seymour <wayne.seymour@elastic.co>
2020-08-17 14:21:03 -0600

    Show me what data is available

--- src/dev/code_coverage/www/index.js
+++ src/dev/code_coverage/www/index.js
@@ -1,1 +1,5 @@
 // TODO; Grab artifactStats off of window obj and process
+
+console.log(`\n### artifactStats: \n\t${artifactStats}`);
+console.log(`\n### window.artifactStats: \n\t${window.artifactStats}`);
+console.log(`\n### initialData: \n\t${initialData}`);
