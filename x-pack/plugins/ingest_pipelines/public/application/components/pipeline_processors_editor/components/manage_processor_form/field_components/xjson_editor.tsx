74e5534be630f127602226eed4bc2746f083e268
Jean-Louis Leysens <jloleysens@gmail.com>
2020-08-17 17:53:07 +0200

    remove unused import

--- x-pack/plugins/ingest_pipelines/public/application/components/pipeline_processors_editor/components/manage_processor_form/field_components/xjson_editor.tsx
+++ x-pack/plugins/ingest_pipelines/public/application/components/pipeline_processors_editor/components/manage_processor_form/field_components/xjson_editor.tsx
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { XJsonLang, monaco } from '@kbn/monaco';
+import { XJsonLang } from '@kbn/monaco';
 import React, { FunctionComponent, useCallback } from 'react';
 import { FieldHook, Monaco } from '../../../../../../shared_imports';
 
