e6ae6117df4d197d865023bec847c957ac53c7ec
Luke Elmers <luke.elmers@elastic.co>
2020-08-17 16:44:06 -0600

    Create common GetConfigFn interface.

--- src/plugins/data/common/field_formats/types.ts
+++ src/plugins/data/common/field_formats/types.ts
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 import { FieldFormat } from './field_format';
 import { FieldFormatsRegistry } from './field_formats_registry';
 
