c504feca85359414139ffd0c4006e89ca051c445
Sébastien Loix <sabee77@gmail.com>
2020-08-18 16:05:10 +0200

    [Form lib] Fix issue where serializer on fields are called on every change (#75166)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/fields/create_field/create_field.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/fields/create_field/create_field.tsx
@@ -111,14 +111,17 @@ export const CreateField = React.memo(function CreateFieldComponent({
 
       {/* Field subType (if any) */}
       <FormDataProvider pathsToWatch="type">
-        {({ type }) => (
-          <SubTypeParameter
-            key={type}
-            type={type}
-            isMultiField={isMultiField ?? false}
-            isRootLevelField={isRootLevelField}
-          />
-        )}
+        {({ type }) => {
+          const [fieldType] = type;
+          return (
+            <SubTypeParameter
+              key={fieldType.value}
+              type={fieldType.value}
+              isMultiField={isMultiField ?? false}
+              isRootLevelField={isRootLevelField}
+            />
+          );
+        }}
       </FormDataProvider>
     </EuiFlexGroup>
   );
@@ -188,7 +191,10 @@ export const CreateField = React.memo(function CreateFieldComponent({
 
             <FormDataProvider pathsToWatch={['type', 'subType']}>
               {({ type, subType }) => {
-                const ParametersForm = getParametersFormForType(type, subType);
+                const ParametersForm = getParametersFormForType(
+                  type?.[0].value,
+                  subType?.[0].value
+                );
 
                 if (!ParametersForm) {
                   return null;
