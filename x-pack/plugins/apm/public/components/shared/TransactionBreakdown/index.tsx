624b2a3fe453e668b1e8e2e688b715ed590e44e0
cauemarcondes <caue.marcondes@elastic.co>
2020-08-18 11:55:14 +0200

    removing extra message

--- x-pack/plugins/apm/public/components/shared/TransactionBreakdown/index.tsx
+++ x-pack/plugins/apm/public/components/shared/TransactionBreakdown/index.tsx
@@ -3,23 +3,13 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiPanel,
-  EuiText,
-  EuiTitle,
-} from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiTitle } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
 import { FETCH_STATUS } from '../../../hooks/useFetcher';
 import { useTransactionBreakdown } from '../../../hooks/useTransactionBreakdown';
 import { TransactionBreakdownGraph } from './TransactionBreakdownGraph';
 import { TransactionBreakdownKpiList } from './TransactionBreakdownKpiList';
-
-const emptyMessage = i18n.translate('xpack.apm.transactionBreakdown.noData', {
-  defaultMessage: 'No data within this time range.',
-});
 
 function TransactionBreakdown() {
   const { data, status } = useTransactionBreakdown();
@@ -39,11 +29,7 @@ function TransactionBreakdown() {
           </EuiTitle>
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
-          {noHits ? (
-            <EuiText>{emptyMessage}</EuiText>
-          ) : (
-            <TransactionBreakdownKpiList kpis={kpis} />
-          )}
+          {!noHits && <TransactionBreakdownKpiList kpis={kpis} />}
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
           <TransactionBreakdownGraph timeseries={timeseries} />
