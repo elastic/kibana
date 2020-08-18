2bf5a2c28b9300154766c7063652735245ccccf9
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2020-08-18 12:36:40 +0200

    [7.x] Implement Server-Side sessions (#75265)
    # Conflicts:
    #	.github/CODEOWNERS
    #	x-pack/plugins/security/server/authentication/authenticator.ts
    #	x-pack/plugins/security/server/authentication/index.ts
    #	x-pack/plugins/security/server/authentication/providers/saml.test.ts
    #	x-pack/plugins/security/server/authentication/providers/saml.ts
    #	x-pack/plugins/security/server/config.ts
    #	x-pack/plugins/security/server/routes/authentication/saml.ts
    #	x-pack/scripts/functional_tests.js

--- x-pack/test/oidc_api_integration/apis/authorization_code_flow/index.ts
+++ x-pack/test/oidc_api_integration/apis/authorization_code_flow/index.ts
@@ -4,7 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export default function ({ loadTestFile }) {
+import { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function ({ loadTestFile }: FtrProviderContext) {
   describe('apis', function () {
     this.tags('ciGroup6');
     loadTestFile(require.resolve('./oidc_auth'));
