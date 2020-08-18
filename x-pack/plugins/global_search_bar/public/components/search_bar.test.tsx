7b7a92543ccac529d174feb1c9e1d870f27e0fb0
Michail Yasonik <michail.yasonik@elastic.co>
2020-08-18 10:37:31 -0400

    adding jest tests

---
+++ x-pack/plugins/global_search_bar/public/components/search_bar.test.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { SearchBar } from '../components/search_bar';
+import { globalSearchPluginMock } from '../../../global_search/public/mocks';
+import { GlobalSearchPluginStart } from '../../../global_search/public';
+import { mountWithIntl } from 'test_utils/enzyme_helpers';
+
+describe('SearchBar', () => {
+  let searchService: GlobalSearchPluginStart;
+  let findSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    searchService = globalSearchPluginMock.createStartContract();
+    findSpy = jest.spyOn(globalSearchPluginMock, 'createStartContract');
+  });
+
+  it('basically works', async () => {
+    const component = mountWithIntl(
+      <SearchBar globalSearch={searchService.find} navigateToUrl={jest.fn()} />
+    );
+    expect(component).toMatchSnapshot(); // popover closed
+    // expect(findSpy).toHaveBeenCalledTimes(1);
+    // expect(findSpy).toHaveBeenCalledWith('');
+
+    // component.find('input[data-test-subj="header-search"]').simulate('keydown', { key: 'd' });
+    // expect(component).toMatchSnapshot(); // popover open
+    // expect(findSpy).toHaveBeenCalledTimes(1);
+    // expect(findSpy).toHaveBeenCalledWith('d');
+
+    // click on result
+    // navigate to new page
+    // click into into search bar, results rendered like new
+  });
+  it('supports keyboard shortcuts', () => {
+    // cmd/ctrl+s focuses search bar, popover opens, results rendered
+  });
+});
