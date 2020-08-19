6f2ce189235343ed2cde9870360979d0d1f9ab2c
Paul Tavares <paul.tavares@elastic.co>
2020-08-19 11:13:19 -0400

    Added types for TrustedApp entries

--- x-pack/plugins/security_solution/common/endpoint/types/trusted_apps.ts
+++ x-pack/plugins/security_solution/common/endpoint/types/trusted_apps.ts
@@ -6,6 +6,20 @@
 
 import { TypeOf } from '@kbn/config-schema';
 import { GetTrustedAppsRequestSchema } from '../schema/trusted_apps';
+import { ExceptionListItemSchema } from '../../../../lists/common';
 
 /** API request params for retrieving a list of Trusted Apps */
 export type GetTrustedAppsRequest = TypeOf<typeof GetTrustedAppsRequestSchema.query>;
+
+/** Type for a new Trusted App Entry */
+export type NewTrustedApp = Pick<
+  ExceptionListItemSchema,
+  'created_at' | 'created_by' | 'name' | 'description' | 'entries'
+> & {
+  os: string;
+};
+
+/** A trusted app entry */
+export type TrustedApp = NewTrustedApp & {
+  id: string;
+};
