3cc385f293a523dab78e9e8ec5d72648e48321fa
Luke Elmers <luke.elmers@elastic.co>
2020-08-19 08:31:35 -0600

    Use ts-expect-error

--- src/plugins/vis_type_metric/public/to_ast.ts
+++ src/plugins/vis_type_metric/public/to_ast.ts
@@ -64,7 +64,7 @@ export const toExpressionAst = (vis: Vis, params: any) => {
     });
   }
 
-  // @ts-ignore
+  // @ts-expect-error
   const metricVis = buildExpressionFunction<MetricVisExpressionFunctionDefinition>('metricVis', {
     percentageMode,
     colorSchema,
