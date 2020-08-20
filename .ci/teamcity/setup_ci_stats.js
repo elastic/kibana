33526894bdd0f3090f5b04834fdbbcdc4b69527c
Brian Seeders <brian.seeders@elastic.co>
2020-08-20 15:02:56 -0400

    Fix url

--- .ci/teamcity/setup_ci_stats.js
+++ .ci/teamcity/setup_ci_stats.js
@@ -20,7 +20,7 @@ const ciStats = require('./ci_stats');
     console.log(`\n##teamcity[setParameter name='env.KIBANA_CI_STATS_CONFIG' display='hidden' password='true' value='${configJson}']\n`);
     console.log(`\n##teamcity[setParameter name='env.CI_STATS_BUILD_ID' value='${build.id}']\n`);
 
-    await ciStats.post(`v1/git_info?buildId=${build.id}`, {
+    await ciStats.post(`/v1/git_info?buildId=${build.id}`, {
       branch: process.env.GIT_BRANCH.replace(/^(refs\/heads\/|origin\/)/, ''),
       commit: process.env.GIT_COMMIT,
       targetBranch: null, // TODO once we have PR support
