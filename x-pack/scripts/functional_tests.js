a3e9b59f995c36694a3a5457511e0e9d5fab034a
Timothy Sullivan <tsullivan@elastic.co>
2020-08-20 10:27:48 -0700

    fix bad merge

--- x-pack/scripts/functional_tests.js
+++ x-pack/scripts/functional_tests.js
@@ -55,7 +55,6 @@ require('@kbn/test').runTestsCli([
   require.resolve('../test/licensing_plugin/config.public'),
   require.resolve('../test/licensing_plugin/config.legacy'),
   require.resolve('../test/endpoint_api_integration_no_ingest/config.ts'),
-  require.resolve('../test/reporting_api_integration/config.js'),
   require.resolve('../test/functional_embedded/config.ts'),
   require.resolve('../test/reporting_api_integration/reporting_and_security.config.ts'),
   require.resolve('../test/reporting_api_integration/reporting_without_security.config.ts'),
