53240e818904631ec0b7455475b4ae191d1cd495
Walter Rafelsberger <walter@elastic.co>
2020-08-20 15:06:08 +0200

    [ML] Delete tmp utility file.

--- x-pack/run_functional_tests.sh
+++
@@ -1,3 +0,0 @@
-export TEST_KIBANA_URL="http://elastic:mlqa_admin@localhost:5601"
-export TEST_ES_URL="http://elastic:mlqa_admin@localhost:9200"
-node ../scripts/functional_test_runner --include-tag walterra
