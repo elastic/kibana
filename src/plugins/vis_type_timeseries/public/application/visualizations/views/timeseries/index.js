0c97485fe1ab15d2edac9027ec024948b67e9e6f
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2020-08-20 11:41:31 +0300

    small change to be more human readbale

--- src/plugins/vis_type_timeseries/public/application/visualizations/views/timeseries/index.js
+++ src/plugins/vis_type_timeseries/public/application/visualizations/views/timeseries/index.js
@@ -180,7 +180,7 @@ export const TimeSeries = ({
         ) => {
           const stackAccessors = getStackAccessors(stack);
           const isPercentage = stack === STACKED_OPTIONS.PERCENT;
-          const isNonStacked = stack === STACKED_OPTIONS.NONE;
+          const isStacked = stack !== STACKED_OPTIONS.NONE;
           const key = `${id}-${label}`;
           // Only use color mapping if there is no color from the server
           const finalColor = color ?? colors.mappedColors.mapping[label];
@@ -201,7 +201,7 @@ export const TimeSeries = ({
                 xScaleType={xScaleType}
                 yScaleType={yScaleType}
                 timeZone={timeZone}
-                enableHistogramMode={!isNonStacked}
+                enableHistogramMode={isStacked}
                 useDefaultGroupDomain={useDefaultGroupDomain}
                 sortIndex={sortIndex}
                 y1AccessorFormat={y1AccessorFormat}
@@ -227,7 +227,7 @@ export const TimeSeries = ({
                 xScaleType={xScaleType}
                 yScaleType={yScaleType}
                 timeZone={timeZone}
-                enableHistogramMode={!isNonStacked}
+                enableHistogramMode={isStacked}
                 useDefaultGroupDomain={useDefaultGroupDomain}
                 sortIndex={sortIndex}
                 y1AccessorFormat={y1AccessorFormat}
