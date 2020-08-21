fce7f8c48afb6bbb249c72b67298e61f2c01cc78
Spencer <email@spalger.com>
2020-08-20 18:50:36 -0700

    [kbn/plugin-generator] remove sao, modernize (#75465)
    Co-authored-by: spalger <spalger@users.noreply.github.com>
    # Conflicts:
    #	packages/kbn-pm/dist/index.js
    #	yarn.lock

--- packages/kbn-plugin-generator/src/casing.ts
+++ packages/kbn-plugin-generator/src/casing.ts
@@ -17,5 +17,16 @@
  * under the License.
  */
 
-require('../src/setup_node_env/prebuilt_dev_only_entry');
-require('@kbn/plugin-generator').runCli();
+const words = (input: string) =>
+  input
+    .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+    .toLowerCase()
+    .split(/[^a-z0-9]+/g)
+    .filter(Boolean);
+
+const upperFirst = (input: string) => `${input.slice(0, 1).toUpperCase()}${input.slice(1)}`;
+const lowerFirst = (input: string) => `${input.slice(0, 1).toLowerCase()}${input.slice(1)}`;
+
+export const snakeCase = (input: string) => words(input).join('_');
+export const upperCamelCase = (input: string) => words(input).map(upperFirst).join('');
+export const camelCase = (input: string) => lowerFirst(upperCamelCase(input));
