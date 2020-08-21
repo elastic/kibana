09b9e009a45ae57b2e69fe3233e572aa2c9289c2
Joel Griffith <joel.griffith@elastic.co>
2020-08-21 14:44:01 -0700

    Working screenshot test + config check for RAM

--- x-pack/plugins/reporting/server/routes/diagnose/screenshot.ts
+++ x-pack/plugins/reporting/server/routes/diagnose/screenshot.ts
@@ -18,30 +18,6 @@ interface ScreenShotResponse {
   logs: string;
 }
 
-/*
-{ hashUrl:
-   'http://localhost:5601/app/dashboards#/view/722b74f0-b882-11e8-a6d9-e546fe2bba5f?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!f%2Cvalue%3A900000)%2Ctime%3A(from%3Anow-7d%2Cto%3Anow))&_a=(description%3A\'Analyze%20mock%20eCommerce%20orders%20and%20revenue\'%2Cfilters%3A!()%2CfullScreenMode%3A!f%2Coptions%3A(hidePanelTitles%3A!f%2CuseMargins%3A!t)%2Cquery%3A(language%3Akuery%2Cquery%3A\'\')%2CtimeRestore%3A!t%2Ctitle%3A\'%5BeCommerce%5D%20Revenue%20Dashboard\'%2CviewMode%3Aview)&forceNow=2020-08-11T21%3A04%3A22.664Z',
-  tz: 'America/Los_Angeles',
-  conditionalHeaders:
-   { headers:
-      { 'kbn-version': '8.0.0',
-        'user-agent':
-         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
-        accept: '*\/*',
-        origin: 'http://localhost:5601',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'accept-language': 'en-US,en;q=0.9',
-        cookie:
-        authorization: 'Basic ZWxhc3RpYzpjaGFuZ2VtZQ==' },
-     conditions:
-      { hostname: 'localhost',
-        port: 5601,
-        basePath: '',
-        protocol: 'http' } },
-  layout: { id: 'png', dimensions: { width: 1440, height: 2024 } } }
-*/
 export const registerDiagnoseScreenshot = (reporting: ReportingCore, logger: Logger) => {
   const setupDeps = reporting.getPluginSetupDeps();
   const userHandler = authorizedUserPreRoutingFactory(reporting);
@@ -85,11 +61,18 @@ export const registerDiagnoseScreenshot = (reporting: ReportingCore, logger: Log
         search: '',
       });
 
+      // Hack the layout to make the base/login page work
       const layout = {
         id: 'png',
         dimensions: {
           width: 1440,
           height: 2024,
+        },
+        selectors: {
+          screenshot: '.application',
+          renderComplete: '.application',
+          itemsCountAttribute: 'data-test-subj="kibanaChrome"',
+          timefilterDurationAttribute: 'data-test-subj="kibanaChrome"',
         },
       };
 
