ba181ab52576c8d946df248d3fc157a8ecb62488
streamich <streamich@gmail.com>
2020-08-21 08:35:23 +0000

    fix: 🐛 use relative import

--- x-pack/plugins/discover_enhanced/server/index.ts
+++ x-pack/plugins/discover_enhanced/server/index.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { PluginInitializerContext } from 'kibana/server';
+import { PluginInitializerContext } from '../../../../src/core/server';
 import { DiscoverEnhancedPlugin } from './plugin';
 
 export { config } from './config';
