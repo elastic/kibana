acf5ad66155079ae5ef81d4a412ace3b7260d05e
Constance <constancecchen@users.noreply.github.com>
2020-08-21 10:49:46 -0700

    [Enterprise Search] Convert our `public_url` route to `config_data` and collect initialAppData (#75616) (#75667)
    * [Setup] DRY out stripTrailingSlash helper
    
    - DRYs out repeated code
    - This will be used by an upcoming server/ endpoint change, hence why it's in common
    
    * [Setup] DRY out initial app data types to common/types
    
    - In preparation for upcoming server logic that will need to reuse these types
    + DRY out and clean up workplace_search types
      - remove unused supportEligible
      - remove currentUser - unneeded in Kibana
    
    * Update callEnterpriseSearchConfigAPI to parse and fetch new expected data
    
    * Remove /public_url API for /config_data
    
    * Remove getPublicUrl in favor of directly calling the new /config_data API from public/plugin
    
    + set returned initialData in this.data
    
    * Set up product apps to be passed initial data as props
    
    * Fix for Kea/redux state not resetting between AS<->WS nav
    
    - resetContext at the top level only gets called once total on first plugin load and never after, causing navigating between WS and AS to crash when both have Kea - this fixes the issue
    
    - moves redux Provider to top level app as well
    
    * Add very basic Kea logic file to App Search
    
    * Finish AppSearchConfigured tests & set up kea+useEffect mocks
    
    * [Cleanup] DRY out repeated mock initialAppData to a reusable defaults constant

--- x-pack/plugins/enterprise_search/common/__mocks__/index.ts
+++ x-pack/plugins/enterprise_search/common/__mocks__/index.ts
@@ -4,6 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from '../../../common/types/workplace_search';
-
-export type TSpacerSize = 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
+export * from './initial_app_data';
