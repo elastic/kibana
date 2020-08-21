b75eb98446afed97cd8d69bd86ea3c4426a8f2c7
sulemanof <sul-dan@mail.ru>
2020-08-21 17:03:38 +0300

    Reset other chrome fields

--- src/core/public/chrome/chrome_service.tsx
+++ src/core/public/chrome/chrome_service.tsx
@@ -157,9 +157,12 @@ export class ChromeService {
     const recentlyAccessed = await this.recentlyAccessed.start({ http });
     const docTitle = this.docTitle.start({ document: window.document });
 
-    // erase the help menu extension from previous app while switching to a next app
+    // erase chrome fields from a previous app while switching to a next app
     application.currentAppId$.subscribe(() => {
       helpExtension$.next(undefined);
+      breadcrumbs$.next([]);
+      badge$.next(undefined);
+      docTitle.reset();
     });
 
     const setIsNavDrawerLocked = (isLocked: boolean) => {
