0b9e4e379576079e988b79b53b9223fc83a5e67b
FrankHassanabad <frank.hassanabad@elastic.co>
2020-08-24 16:25:18 -0600

    Change the import to use the friendly way of importing

--- x-pack/plugins/security_solution/public/timelines/containers/helpers.ts
+++ x-pack/plugins/security_solution/public/timelines/containers/helpers.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { isEqual } from 'lodash/fp';
+import isEqual from 'lodash/fp/isEqual';
 import { TimelineId } from '../../../common/types/timeline';
 
 export const detectionsTimelineIds = [
