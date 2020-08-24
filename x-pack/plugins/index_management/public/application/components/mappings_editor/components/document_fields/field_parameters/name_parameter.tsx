31d9e3b23c753a58e2c62d502a91f087721cbcad
Sébastien Loix <sabee77@gmail.com>
2020-08-24 17:48:04 +0200

    Update mappings editor to use memoize config and validation for fields

--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/name_parameter.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/name_parameter.tsx
@@ -4,33 +4,42 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { TextField, UseField, FieldConfig } from '../../../shared_imports';
 import { validateUniqueName } from '../../../lib';
 import { PARAMETERS_DEFINITION } from '../../../constants';
 import { useMappingsState } from '../../../mappings_state_context';
 
+const { validations, ...rest } = PARAMETERS_DEFINITION.name.fieldConfig as FieldConfig;
+
 export const NameParameter = () => {
   const {
     fields: { rootLevelFields, byId },
     documentFields: { fieldToAddFieldTo, fieldToEdit },
   } = useMappingsState();
-  const { validations, ...rest } = PARAMETERS_DEFINITION.name.fieldConfig as FieldConfig;
 
   const initialName = fieldToEdit ? byId[fieldToEdit].source.name : undefined;
   const parentId = fieldToEdit ? byId[fieldToEdit].parentId : fieldToAddFieldTo;
-  const uniqueNameValidator = validateUniqueName({ rootLevelFields, byId }, initialName, parentId);
+  const uniqueNameValidator = useCallback(
+    (arg: any) => {
+      return validateUniqueName({ rootLevelFields, byId }, initialName, parentId)(arg);
+    },
+    [rootLevelFields, byId, initialName, parentId]
+  );
 
-  const nameConfig: FieldConfig = {
-    ...rest,
-    validations: [
-      ...validations!,
-      {
-        validator: uniqueNameValidator,
-      },
-    ],
-  };
+  const nameConfig: FieldConfig = useMemo(
+    () => ({
+      ...rest,
+      validations: [
+        ...validations!,
+        {
+          validator: uniqueNameValidator,
+        },
+      ],
+    }),
+    [uniqueNameValidator]
+  );
 
   return (
     <UseField
