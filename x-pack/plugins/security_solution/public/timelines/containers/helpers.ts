829e4e1d519e2ed9ef16702e03d13e3585de5da0
FrankHassanabad <frank.hassanabad@elastic.co>
2020-08-24 15:09:18 -0600

    Updated text for skipQuery

--- x-pack/plugins/security_solution/public/timelines/containers/helpers.ts
+++ x-pack/plugins/security_solution/public/timelines/containers/helpers.ts
@@ -14,9 +14,10 @@ export const detectionsTimelineIds = [
 
 /**
  * If the defaultIndex for timeline and the defaultKibana index are equal to each other
- * then there is a strong probability that this does not contain the rules index and we would
- * want to exclude it completely from a query. This is to prevent additional querying from happening
- * with the detections page against the default index being pushed into timeline as a workaround.
+ * then this does not contain the signals index and we would want to exclude it completely from a query
+ * while on the detections page or the detection details page. This is to prevent additional querying
+ * from happening with the detections page and detections details when the default index is pushed into timeline.
+ * This code is considered a workaround and should go away in the future after refactoring/rewrites of timeline.
  * @param id The id of the page which if it is set to the detections page or details page then it should not be analyzed
  * @param defaultIndex The default index that is about cause a query from timeline.
  * @param defaultKibanaIndex The default Kibana index to check to see if they're the same thing and if so, let us not query
