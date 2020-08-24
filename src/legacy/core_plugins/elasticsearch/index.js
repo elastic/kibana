af8f5c686f5c7d14de765bc63e03bfae3b34bf84
pgayvallet <pierre.gayvallet@elastic.co>
2020-08-24 18:13:13 +0200

    remove createCluster

--- src/legacy/core_plugins/elasticsearch/index.js
+++ src/legacy/core_plugins/elasticsearch/index.js
@@ -59,25 +59,6 @@ export default function (kibana) {
         return clusters.get(name);
       });
 
-      server.expose('createCluster', (name, clientConfig = {}) => {
-        // NOTE: Not having `admin` and `data` clients provided by the core in `clusters`
-        // map implicitly allows to create custom `data` and `admin` clients. This is
-        // allowed intentionally to support custom `admin` cluster client created by the
-        // x-pack/monitoring bulk uploader. We should forbid that as soon as monitoring
-        // bulk uploader is refactored, see https://github.com/elastic/kibana/issues/31934.
-        if (clusters.has(name)) {
-          throw new Error(`cluster '${name}' already exists`);
-        }
-
-        const cluster = new Cluster(
-          server.newPlatform.setup.core.elasticsearch.legacy.createClient(name, clientConfig)
-        );
-
-        clusters.set(name, cluster);
-
-        return cluster;
-      });
-
       server.events.on('stop', () => {
         for (const cluster of clusters.values()) {
           cluster.close();
