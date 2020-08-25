e71a4fc4769c22844155d1b3ca0407fd148ca7d2
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2020-08-25 12:55:05 +0100

    Add all the existing IDs (even the forward apps just in case)

--- src/plugins/kibana_usage_collection/server/collectors/application_usage/schema.ts
+++ src/plugins/kibana_usage_collection/server/collectors/application_usage/schema.ts
@@ -47,26 +47,31 @@ const commonSchema: MakeSchemaFrom<ApplicationUsageTelemetryReport[string]> = {
   },
 };
 
-// These keys obtained by searching for `application.register({` and checking the value of the attr `id`.
+// These keys obtained by searching for `/application\w*\.register\(/` and checking the value of the attr `id`.
 // TODO: Find a way to update these keys automatically.
 export const applicationUsageSchema = {
   // OSS
   dashboards: commonSchema,
+  dev_tools: commonSchema,
   discover: commonSchema,
   home: commonSchema,
+  kibana: commonSchema, // It's a forward app so we'll likely never report it
   management: commonSchema,
+  short_url_redirect: commonSchema, // It's a forward app so we'll likely never report it
   timelion: commonSchema,
   visualize: commonSchema,
-  dev_tools: commonSchema,
 
   // X-Pack
   apm: commonSchema,
+  csm: commonSchema,
   canvas: commonSchema,
+  dashboard_mode: commonSchema, // It's a forward app so we'll likely never report it
   appSearch: commonSchema,
   workplaceSearch: commonSchema,
   graph: commonSchema,
   logs: commonSchema,
   metrics: commonSchema,
+  infra: commonSchema, // It's a forward app so we'll likely never report it
   ingestManager: commonSchema,
   lens: commonSchema,
   maps: commonSchema,
@@ -75,10 +80,12 @@ export const applicationUsageSchema = {
   'observability-overview': commonSchema,
   security_account: commonSchema,
   security_access_agreement: commonSchema,
+  security_capture_url: commonSchema, // It's a forward app so we'll likely never report it
   security_logged_out: commonSchema,
   security_login: commonSchema,
   security_logout: commonSchema,
   security_overwritten_session: commonSchema,
+  securitySolution: commonSchema, // It's a forward app so we'll likely never report it
   'securitySolution:overview': commonSchema,
   'securitySolution:detections': commonSchema,
   'securitySolution:hosts': commonSchema,
@@ -86,8 +93,7 @@ export const applicationUsageSchema = {
   'securitySolution:timelines': commonSchema,
   'securitySolution:case': commonSchema,
   'securitySolution:administration': commonSchema,
+  siem: commonSchema,
   space_selector: commonSchema,
   uptime: commonSchema,
-
-  enterprise_search: commonSchema,
 };
