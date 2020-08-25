11210e1b782be7a1129d8cc747f720a0361059ad
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2020-08-25 20:44:41 +0200

    Add `seamless` option for seamless rendering

--- x-pack/plugins/infra/public/components/log_stream/index.tsx
+++ x-pack/plugins/infra/public/components/log_stream/index.tsx
@@ -4,47 +4,102 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useEffect } from 'react';
+import React, { useMemo } from 'react';
 import { noop } from 'lodash';
+import { useMount } from 'react-use';
 import { EuiPanel } from '@elastic/eui';
+
+import { useKibana } from '../../../../../../src/plugins/kibana_react/public';
+import { useLogSource } from '../../containers/logs/log_source';
 import { useLogStream } from '../../containers/logs/log_stream';
+
 import { ScrollableLogTextStreamView } from '../logging/log_text_stream';
 
 interface LogStreamProps {
+  sourceId?: string;
   startTimestamp: number;
   endTimestamp: number;
+  seamless?: boolean;
 }
 
-export const LogStream: React.FC<LogStreamProps> = ({ startTimestamp, endTimestamp }) => {
-  const { entries, fetchEntries } = useLogStream({ startTimestamp, endTimestamp });
+export const LogStream: React.FC<LogStreamProps> = ({
+  sourceId = 'default',
+  startTimestamp,
+  endTimestamp,
+  seamless = false,
+}) => {
+  // source boilerplate
+  const { services } = useKibana();
+  const {
+    sourceConfiguration,
+    loadSourceConfiguration,
+    isLoadingSourceConfiguration,
+  } = useLogSource({
+    sourceId,
+    fetch: services.http.fetch,
+  });
 
-  useEffect(() => fetchEntries(), [fetchEntries]);
+  // Internal state
+  const { loadingState, entries, fetchEntries } = useLogStream({
+    sourceId,
+    startTimestamp,
+    endTimestamp,
+  });
 
-  return (
-    <EuiPanel>
-      <ScrollableLogTextStreamView
-        target={null}
-        columnConfigurations={[]}
-        items={entries}
-        scale="medium"
-        wrap={false}
-        isReloading={false}
-        isLoadingMore={false}
-        hasMoreBeforeStart={false}
-        hasMoreAfterEnd={false}
-        isStreaming={false}
-        lastLoadedTime={null}
-        jumpToTarget={noop}
-        reportVisibleInterval={noop}
-        loadNewerItems={noop}
-        reloadItems={fetchEntries}
-        highlightedItem={null}
-        currentHighlightKey={null}
-        startDateExpression={''}
-        endDateExpression={''}
-        updateDateRange={noop}
-        startLiveStreaming={noop}
-      />
-    </EuiPanel>
+  // Derived state
+  const isReloading =
+    isLoadingSourceConfiguration || loadingState === 'uninitialized' || loadingState === 'loading';
+
+  const columnConfigurations = useMemo(() => {
+    return sourceConfiguration ? sourceConfiguration.configuration.logColumns : [];
+  }, [sourceConfiguration]);
+
+  const streamItems = useMemo(
+    () =>
+      entries.map((entry) => ({
+        kind: 'logEntry' as const,
+        logEntry: entry,
+        highlights: [],
+      })),
+    [entries]
   );
+
+  // Component lifetime
+  useMount(() => {
+    loadSourceConfiguration();
+    fetchEntries();
+  });
+
+  const streamView = (
+    <ScrollableLogTextStreamView
+      target={null}
+      columnConfigurations={columnConfigurations}
+      items={streamItems}
+      scale="medium"
+      wrap={false}
+      isReloading={isReloading}
+      isLoadingMore={false}
+      hasMoreBeforeStart={false}
+      hasMoreAfterEnd={false}
+      isStreaming={false}
+      lastLoadedTime={null}
+      jumpToTarget={noop}
+      reportVisibleInterval={noop}
+      loadNewerItems={noop}
+      reloadItems={fetchEntries}
+      highlightedItem={null}
+      currentHighlightKey={null}
+      startDateExpression={''}
+      endDateExpression={''}
+      updateDateRange={noop}
+      startLiveStreaming={noop}
+    />
+  );
+
+  // Rendering
+  if (seamless) {
+    return streamView;
+  } else {
+    return <EuiPanel>{streamView}</EuiPanel>;
+  }
 };
--- x-pack/plugins/infra/public/containers/logs/log_stream/index.ts
+++ x-pack/plugins/infra/public/containers/logs/log_stream/index.ts
@@ -4,9 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { useState, useCallback } from 'react';
+import { useState, useMemo } from 'react';
+import { fetchLogEntries } from '../log_entries/api/fetch_log_entries';
+import { useTrackedPromise } from '../../../utils/use_tracked_promise';
 
 interface LogStreamProps {
+  sourceId: string;
   startTimestamp: number;
   endTimestamp: number;
 }
@@ -14,17 +17,51 @@ interface LogStreamProps {
 interface LogStreamState {
   entries: any[];
   fetchEntries: () => void;
+  loadingState: 'uninitialized' | 'loading' | 'success' | 'error';
 }
 
-export function useLogStream({ startTimestamp, endTimestamp }: LogStreamProps): LogStreamState {
+export function useLogStream({
+  sourceId,
+  startTimestamp,
+  endTimestamp,
+}: LogStreamProps): LogStreamState {
   const [entries, setEntries] = useState<LogStreamState['entries']>([]);
+  const [entriesPromise, fetchEntries] = useTrackedPromise(
+    {
+      cancelPreviousOn: 'creation',
+      createPromise: () => {
+        setEntries([]);
+        return fetchLogEntries({ sourceId, startTimestamp, endTimestamp });
+      },
+      onResolve: ({ data }) => {
+        setEntries(data.entries);
+      },
+    },
+    [sourceId, startTimestamp, endTimestamp]
+  );
 
-  const fetchEntries = useCallback(() => {
-    setEntries([]);
-  }, []);
+  const loadingState = useMemo(() => convertPromiseStateToLoadingState(entriesPromise.state), [
+    entriesPromise.state,
+  ]);
 
   return {
     entries,
     fetchEntries,
+    loadingState,
   };
+}
+
+function convertPromiseStateToLoadingState(
+  state: 'uninitialized' | 'pending' | 'resolved' | 'rejected'
+): LogStreamState['loadingState'] {
+  switch (state) {
+    case 'uninitialized':
+      return 'uninitialized';
+    case 'pending':
+      return 'loading';
+    case 'resolved':
+      return 'success';
+    case 'rejected':
+      return 'error';
+  }
 }
