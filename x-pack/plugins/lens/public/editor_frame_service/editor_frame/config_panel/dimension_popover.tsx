c33d67feca86f26b1e927c7f028a68c56f7bb312
cchaos <caroline.horn@elastic.co>
2020-08-26 12:38:51 -0400

    Moving to a Flyout implementation

--- x-pack/plugins/lens/public/editor_frame_service/editor_frame/config_panel/dimension_popover.tsx
+++ x-pack/plugins/lens/public/editor_frame_service/editor_frame/config_panel/dimension_popover.tsx
@@ -6,7 +6,7 @@
 import './dimension_popover.scss';
 
 import React from 'react';
-import { EuiPopover } from '@elastic/eui';
+import { EuiIcon, EuiFlyoutHeader, EuiFlyoutBody, EuiTitle } from '@elastic/eui';
 import { VisualizationDimensionGroupConfig } from '../../../types';
 import { DimensionPopoverState } from './types';
 
@@ -28,23 +28,41 @@ export function DimensionPopover({
   panel: React.ReactElement;
 }) {
   const noMatch = popoverState.isOpen ? !groups.some((d) => d.accessors.includes(accessor)) : false;
+
+  let flyout;
+  if (
+    popoverState.isOpen &&
+    (popoverState.openId === accessor || (noMatch && popoverState.addingToGroupId === groupId))
+  ) {
+    flyout = (
+      <div role="dialog" aria-labelledby="flyoutTitle" className="lnsDimensionPopover">
+        <EuiFlyoutHeader hasBorder>
+          <EuiTitle size="xs">
+            <button
+              onClick={() => {
+                setPopoverState({
+                  isOpen: false,
+                  openId: null,
+                  addingToGroupId: null,
+                  tabId: null,
+                });
+              }}
+            >
+              <h2 id="flyoutTitle">
+                <EuiIcon type="sortLeft" /> A typical flyout
+              </h2>
+            </button>
+          </EuiTitle>
+        </EuiFlyoutHeader>
+        <EuiFlyoutBody>{panel}</EuiFlyoutBody>
+      </div>
+    );
+  }
+
   return (
-    <EuiPopover
-      className="lnsDimensionPopover"
-      anchorClassName="lnsDimensionPopover__trigger"
-      panelClassName="lnsDimensionPopover__fixTranslateDnd"
-      isOpen={
-        popoverState.isOpen &&
-        (popoverState.openId === accessor || (noMatch && popoverState.addingToGroupId === groupId))
-      }
-      closePopover={() => {
-        setPopoverState({ isOpen: false, openId: null, addingToGroupId: null, tabId: null });
-      }}
-      button={trigger}
-      anchorPosition="leftUp"
-      panelPaddingSize="none"
-    >
-      {panel}
-    </EuiPopover>
+    <>
+      <div className="lnsDimensionPopover__trigger">{trigger}</div>
+      {flyout}
+    </>
   );
 }
