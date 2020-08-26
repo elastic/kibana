bcf8eb9d2905e0316627c72b508e7e0ac1c1d08a
scottybollinger <scotty.bollinger@elastic.co>
2020-08-26 11:04:34 -0500

    Migrate useDidUpdateEffect hook
    Migrates https://github.com/elastic/ent-search/blob/master/app/javascript/shared/utils/useDidUpdateEffect.ts with test added.

---
+++ x-pack/plugins/enterprise_search/public/applications/shared/use_did_update_effect/use_did_update_effect.tsx
@@ -0,0 +1,25 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+/*
+ * Sometimes we don't want to fire the initial useEffect call.
+ * This custom Hook only fires after the intial render has completed.
+ */
+import { useEffect, useRef } from 'react';
+
+type TInput = string | number | object | Function | boolean | null | undefined;
+
+export const useDidUpdateEffect = (fn: Function, inputs: TInput[]) => {
+  const didMountRef = useRef(false);
+
+  useEffect(() => {
+    if (didMountRef.current) {
+      fn();
+    } else {
+      didMountRef.current = true;
+    }
+  }, inputs);
+};
