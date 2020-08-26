9873df8ee0ee88584db70debc3316126ba3205af
Paul Tavares <56442535+paul-tavares@users.noreply.github.com>
2020-08-26 16:02:37 -0400

    [SECURITY_SOLUTION][ENDPOINT] Trusted Apps List API (#75476)
    * Trusted Apps initial setup for route registration
    
    * Added types for TrustedApp entries
    
    * trusted apps list API returns results
    
    * use methods and const from latest PR merge to lists
    
    * a quick generator for trusted apps entries
    
    * support cli options for trusted app data loader
    
    * Add mocked `createTrustedAppsList()` method to `ExceptionListClientMock`
    
    * tests fro trusted apps route handlers
    
    * tests for trusted apps schema
    
    * Correct name of mock method
    
    * Fix service to ensure return value of `getExceptionList` service throws if service not available
    
    * Fix types
    
    * Refactor TrustedApp type + code review feedback

--- x-pack/plugins/security_solution/server/plugin.ts
+++ x-pack/plugins/security_solution/server/plugin.ts
@@ -59,6 +59,7 @@ import { EndpointAppContext } from './endpoint/types';
 import { registerDownloadExceptionListRoute } from './endpoint/routes/artifacts';
 import { initUsageCollectors } from './usage';
 import { AppRequestContext } from './types';
+import { registerTrustedAppsRoutes } from './endpoint/routes/trusted_apps';
 import { securitySolutionSearchStrategyProvider } from './search_strategy/security_solution';
 
 export interface SetupPlugins {
@@ -166,6 +167,7 @@ export class Plugin implements IPlugin<PluginSetup, PluginStart, SetupPlugins, S
     registerLimitedConcurrencyRoutes(core);
     registerResolverRoutes(router, endpointContext);
     registerPolicyRoutes(router, endpointContext);
+    registerTrustedAppsRoutes(router, endpointContext);
     registerDownloadExceptionListRoute(router, endpointContext, this.exceptionsCache);
 
     plugins.features.registerFeature({
@@ -305,6 +307,7 @@ export class Plugin implements IPlugin<PluginSetup, PluginStart, SetupPlugins, S
 
     this.endpointAppContextService.start({
       agentService: plugins.ingestManager?.agentService,
+      exceptionsListService: this.lists!.getExceptionListClient(savedObjectsClient, 'kibana'),
       logger: this.logger,
       manifestManager,
       registerIngestCallback,
