c8f56f3d64edf715a8f1555aa37c866a59ddd3c2
spalger <spalger@users.noreply.github.com>
2020-08-27 11:45:58 -0700

    try more approaches to getting yarn command output on CI

--- packages/kbn-plugin-helpers/src/integration_tests/build.test.ts
+++ packages/kbn-plugin-helpers/src/integration_tests/build.test.ts
@@ -61,17 +61,20 @@ it('builds a generated plugin into a viable archive', async () => {
     "
   `);
 
-  const buildProc = await execa(
-    process.execPath,
-    ['../../scripts/plugin_helpers', 'build', '--kibana-version', '7.5.0'],
-    {
-      cwd: PLUGIN_DIR,
-      all: true,
-    }
-  );
+  const buildProc = await execa('yarn', ['build', '--kibana-version', '7.5.0'], {
+    cwd: PLUGIN_DIR,
+    all: true,
+    env: {
+      // prevent yarn from silencing itself on CI
+      YARN_SILENT: '0',
+    },
+  });
 
   expect(buildProc.all).toMatchInlineSnapshot(`
-    " info deleting the build and target directories
+    "yarn run <version>
+    $ yarn plugin-helpers build --kibana-version 7.5.0
+    $ node ../../scripts/plugin_helpers build --kibana-version 7.5.0
+     info deleting the build and target directories
      info running @kbn/optimizer
      │ info initialized, 0 bundles cached
      │ info starting worker [1 bundle]
@@ -80,16 +83,15 @@ it('builds a generated plugin into a viable archive', async () => {
      info running yarn to install dependencies
 
 
-    yarn install <version>
     info No lockfile found.
     [1/4] Resolving packages...
     [2/4] Fetching packages...
     [3/4] Linking dependencies...
     [4/4] Building fresh packages...
-    Done in <time>.
 
 
-     info compressing plugin into [fooTestPlugin-7.5.0.zip]"
+     info compressing plugin into [fooTestPlugin-7.5.0.zip]
+    Done in <time>."
   `);
 
   await decompress(PLUGIN_ARCHIVE, TMP_DIR);
