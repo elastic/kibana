bab3216fede6508e07e876f69f203f11d263ecf5
pgayvallet <pierre.gayvallet@elastic.co>
2020-08-31 07:50:15 +0200

    adapt ui_render mixin to use core APIs

--- src/legacy/server/kbn_server.d.ts
+++ src/legacy/server/kbn_server.d.ts
@@ -45,7 +45,6 @@ import { LegacyConfig, ILegacyService, ILegacyInternals } from '../../core/serve
 import { UiPlugins } from '../../core/server/plugins';
 import { CallClusterWithRequest, ElasticsearchPlugin } from '../core_plugins/elasticsearch';
 import { UsageCollectionSetup } from '../../plugins/usage_collection/server';
-import { UiSettingsServiceFactoryOptions } from '../../legacy/ui/ui_settings/ui_settings_service_factory';
 import { HomeServerPluginSetup } from '../../plugins/home/server';
 
 // lot of legacy code was assuming this type only had these two methods
