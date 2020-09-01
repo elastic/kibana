67d46e25e511f8ef42cfa284ef9dc97db6d5f96f
pgayvallet <pierre.gayvallet@elastic.co>
2020-09-01 10:29:32 +0200

    fix 7.x code

--- x-pack/plugins/upgrade_assistant/public/application/components/tabs/checkup/deprecations/index_table.test.tsx
+++ x-pack/plugins/upgrade_assistant/public/application/components/tabs/checkup/deprecations/index_table.test.tsx
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-jest.mock('ui/kfetch');
-
 import React from 'react';
 import { shallowWithIntl } from 'test_utils/enzyme_helpers';
 
