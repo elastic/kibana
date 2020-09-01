04bfdd002e3b438f7d4ccd8180a74937d0fdbfe7
cauemarcondes <caue.marcondes@elastic.co>
2020-09-01 10:15:48 +0200

    removing unused prop

--- x-pack/plugins/apm/public/components/app/TransactionDetails/index.tsx
+++ x-pack/plugins/apm/public/components/app/TransactionDetails/index.tsx
@@ -86,7 +86,6 @@ export function TransactionDetails() {
             <TransactionCharts
               charts={transactionChartsData}
               urlParams={urlParams}
-              location={location}
             />
           </ChartsSyncContextProvider>
 
