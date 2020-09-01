3338466f11a9ab6f36b533283208ce54f8ec73d3
Nathan L Smith <smith@nlsmith.com>
2020-09-01 12:47:19 -0500

    some noodles

--- packages/kbn-storybook/composite/.storybook/main.js
+++ packages/kbn-storybook/composite/.storybook/main.js
@@ -1,1 +1,25 @@
-module.exports = require('@kbn/storybook').defaultConfig;
+const { defaultConfig } = require('@kbn/storybook');
+const { storybookAliases } = require('../../../../src/dev/storybook/aliases');
+
+const m = {
+  dashboard_enhanced: 'dashboard-enhanced',
+  infra: 'infra-neon',
+};
+
+const urlSuffix = 'vercel.app';
+const refs = Object.entries(storybookAliases).reduce((prev, [id, options]) => {
+  if (id !== 'composite') {
+    prev[id] = { title: options.title || id, url: `https://${m[id]}.${urlSuffix}` };
+  }
+  return prev;
+}, {});
+
+console.log({ refs, storybookAliases });
+
+// console.log({ refs });
+// process.exit(0);
+module.exports = {
+  ...defaultConfig,
+  refs,
+  stories: [],
+};
