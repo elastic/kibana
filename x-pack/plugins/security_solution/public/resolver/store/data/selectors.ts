1e37369d43854f275a91b350e32ef55d41a4d1f4
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-09-02 17:08:19 -0400

    Removing unnecessary import

--- x-pack/plugins/security_solution/public/resolver/store/data/selectors.ts
+++ x-pack/plugins/security_solution/public/resolver/store/data/selectors.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import _ from 'lodash';
 import rbush from 'rbush';
 import { createSelector, defaultMemoize } from 'reselect';
 import {
