5304adf608dc64cc57eb6bcb109fa5ba6fb82be0
Melissa Alvarez <melissa.alvarez@elastic.co>
2020-09-03 16:36:15 -0400

    update error extraction function

--- x-pack/plugins/ml/common/util/errors.ts
+++ x-pack/plugins/ml/common/util/errors.ts
@@ -135,19 +135,14 @@ export const extractErrorProperties = (
       typeof error.body.attributes === 'object' &&
       error.body.attributes.body?.status !== undefined
     ) {
-      statusCode = error.body.attributes.body?.status;
-    }
+      statusCode = error.body.attributes.body.status;
 
-    if (
-      'attributes' in error.body &&
-      typeof error.body.attributes === 'object' &&
-      error.body.attributes.body?.status !== undefined &&
-      error.body.attributes.body?.error?.reason !== undefined
-    ) {
-      return {
-        message: error.body.attributes.body?.error?.reason,
-        statusCode: error.body.attributes.body?.status,
-      };
+      if (typeof error.body.attributes.body.error?.reason === 'string') {
+        return {
+          message: error.body.attributes.body.error.reason,
+          statusCode,
+        };
+      }
     }
 
     if (typeof error.body.message === 'string') {
