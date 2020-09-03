438d9f26b601bf1ac05ae7282275fc51d628b147
Sébastien Loix <sabee77@gmail.com>
2020-09-03 10:40:41 +0200

    Revert "Update embeddable InputEditor example to use non controlled JsonEditor"
    This reverts commit 1f9ae4115361af48fca0c4d05dc5995747b734a7.

--- examples/dashboard_embeddable_examples/public/by_value/input_editor.tsx
+++ examples/dashboard_embeddable_examples/public/by_value/input_editor.tsx
@@ -21,27 +21,31 @@ import React from 'react';
 import { EuiButton } from '@elastic/eui';
 import { JsonEditor } from '../../../../src/plugins/es_ui_shared/public';
 
-export const InputEditor = <T extends object>(props: {
-  input: T;
-  onSubmit: (value: T) => void;
-}) => {
-  const getJsonData = React.useRef(() => props.input);
-  const [isValid, setIsValid] = React.useState(true);
-
+export const InputEditor = <T,>(props: { input: T; onSubmit: (value: T) => void }) => {
+  const input = JSON.stringify(props.input, null, 4);
+  const [value, setValue] = React.useState(input);
+  const isValid = (() => {
+    try {
+      JSON.parse(value);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  })();
+  React.useEffect(() => {
+    setValue(input);
+  }, [input]);
   return (
     <>
-      <JsonEditor<T>
-        defaultValue={props.input}
-        onUpdate={(jsonState) => {
-          getJsonData.current = jsonState.data.format;
-          setIsValid(jsonState.isValid);
-        }}
+      <JsonEditor
+        value={value}
+        onUpdate={(v) => setValue(v.data.raw)}
         euiCodeEditorProps={{
           'data-test-subj': 'dashboardEmbeddableByValueInputEditor',
         }}
       />
       <EuiButton
-        onClick={() => props.onSubmit(getJsonData.current())}
+        onClick={() => props.onSubmit(JSON.parse(value))}
         disabled={!isValid}
         data-test-subj={'dashboardEmbeddableByValueInputSubmit'}
       >
