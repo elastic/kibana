0d8cceebfd835d2f9a6f13555c97c8199dafb1a6
nnamdifrankie <franklin.ejoh@elastic.co>
2020-09-04 13:54:16 -0400

    EMT-661: fix build

--- x-pack/plugins/ingest_manager/server/services/epm/elasticsearch/transform/remove.ts
+++ x-pack/plugins/ingest_manager/server/services/epm/elasticsearch/transform/remove.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { SavedObjectsClientContract, validBodyOutput } from 'kibana/server';
+import { SavedObjectsClientContract } from 'kibana/server';
 import { CallESAsCurrentUser, ElasticsearchAssetType, EsAssetReference } from '../../../../types';
 import { PACKAGES_SAVED_OBJECT_TYPE } from '../../../../../common/constants';
 
