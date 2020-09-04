d65f3f6023b40e5a2ec036d35c31e9ddad8da6dc
Jonathan Buttner <jonathan.buttner@elastic.co>
2020-09-04 12:45:45 -0400

    Handling case where entity id is not in the mapping

--- x-pack/plugins/security_solution/server/endpoint/routes/resolver/entity.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/resolver/entity.ts
@@ -36,8 +36,8 @@ export function handleEntities(): RequestHandler<unknown, TypeOf<typeof validate
           | [
               {
                 _source: {
-                  process: {
-                    entity_id: string;
+                  process?: {
+                    entity_id?: string;
                   };
                 };
               }
@@ -64,19 +64,6 @@ export function handleEntities(): RequestHandler<unknown, TypeOf<typeof validate
                     values: _id,
                   },
                 },
-                {
-                  exists: {
-                    // only return documents that have process.entity_id
-                    field: 'process.entity_id',
-                  },
-                },
-                {
-                  bool: {
-                    must_not: {
-                      term: { 'process.entity_id': '' },
-                    },
-                  },
-                },
               ],
             },
           },
@@ -85,14 +72,13 @@ export function handleEntities(): RequestHandler<unknown, TypeOf<typeof validate
     );
 
     const responseBody: ResolverEntityIndex = [];
-    for (const {
-      _source: {
-        process: { entity_id },
-      },
-    } of queryResponse.hits.hits) {
-      responseBody.push({
-        entity_id,
-      });
+    for (const hit of queryResponse.hits.hits) {
+      // check that the field is defined and that is not an empty string
+      if (hit._source.process?.entity_id) {
+        responseBody.push({
+          entity_id: hit._source.process.entity_id,
+        });
+      }
     }
     return response.ok({ body: responseBody });
   };
