2d733280e93397e175d41818dc8376c34029bb0c
cauemarcondes <caue.marcondes@elastic.co>
2020-09-04 13:57:09 +0200

    rmeoving snapshot from api test

--- x-pack/test/apm_api_integration/basic/tests/transaction_groups/error_rate.ts
+++ x-pack/test/apm_api_integration/basic/tests/transaction_groups/error_rate.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import expect from '@kbn/expect';
+import { first, last } from 'lodash';
 import { FtrProviderContext } from '../../../common/ftr_provider_context';
 
 export default function ApiTest({ getService }: FtrProviderContext) {
@@ -33,79 +34,32 @@ export default function ApiTest({ getService }: FtrProviderContext) {
       before(() => esArchiver.load('8.0.0'));
       after(() => esArchiver.unload('8.0.0'));
 
-      it('returns the transaction error rate', async () => {
-        const response = await supertest.get(
-          `/api/apm/services/opbeans-java/transaction_groups/error_rate?start=${start}&end=${end}&uiFilters=${uiFilters}`
-        );
+      describe('returns the transaction error rate', () => {
+        let errorRateResponse: {
+          erroneousTransactionsRate: Array<{ x: number; y: number | null }>;
+          average: number;
+        };
+        before(async () => {
+          const response = await supertest.get(
+            `/api/apm/services/opbeans-java/transaction_groups/error_rate?start=${start}&end=${end}&uiFilters=${uiFilters}`
+          );
+          errorRateResponse = response.body;
+        });
 
-        expect(response.status).to.be(200);
+        it('has the correct start date', async () => {
+          expect(first(errorRateResponse.erroneousTransactionsRate)?.x).to.be(1598439600000);
+        });
 
-        expect(response.body).to.eql({
-          noHits: false,
-          erroneousTransactionsRate: [
-            { x: 1598439600000, y: 0.5 },
-            { x: 1598439630000, y: null },
-            { x: 1598439660000, y: 0 },
-            { x: 1598439690000, y: null },
-            { x: 1598439720000, y: 0.14285714285714285 },
-            { x: 1598439750000, y: 0 },
-            { x: 1598439780000, y: 0 },
-            { x: 1598439810000, y: null },
-            { x: 1598439840000, y: 0 },
-            { x: 1598439870000, y: 0 },
-            { x: 1598439900000, y: 0 },
-            { x: 1598439930000, y: null },
-            { x: 1598439960000, y: 0 },
-            { x: 1598439990000, y: 0 },
-            { x: 1598440020000, y: 1 },
-            { x: 1598440050000, y: null },
-            { x: 1598440080000, y: null },
-            { x: 1598440110000, y: null },
-            { x: 1598440140000, y: 0.6666666666666666 },
-            { x: 1598440170000, y: null },
-            { x: 1598440200000, y: 0 },
-            { x: 1598440230000, y: 0 },
-            { x: 1598440260000, y: 0 },
-            { x: 1598440290000, y: null },
-            { x: 1598440320000, y: 0 },
-            { x: 1598440350000, y: 0 },
-            { x: 1598440380000, y: 0 },
-            { x: 1598440410000, y: null },
-            { x: 1598440440000, y: 0 },
-            { x: 1598440470000, y: 0 },
-            { x: 1598440500000, y: 1 },
-            { x: 1598440530000, y: null },
-            { x: 1598440560000, y: 0.25 },
-            { x: 1598440590000, y: 0 },
-            { x: 1598440620000, y: null },
-            { x: 1598440650000, y: null },
-            { x: 1598440680000, y: 0 },
-            { x: 1598440710000, y: 0 },
-            { x: 1598440740000, y: null },
-            { x: 1598440770000, y: 0 },
-            { x: 1598440800000, y: 0 },
-            { x: 1598440830000, y: 0 },
-            { x: 1598440860000, y: null },
-            { x: 1598440890000, y: null },
-            { x: 1598440920000, y: 0.3333333333333333 },
-            { x: 1598440950000, y: 0.6666666666666666 },
-            { x: 1598440980000, y: 0.5 },
-            { x: 1598441010000, y: null },
-            { x: 1598441040000, y: 0.14285714285714285 },
-            { x: 1598441070000, y: 0.6666666666666666 },
-            { x: 1598441100000, y: 0 },
-            { x: 1598441130000, y: null },
-            { x: 1598441160000, y: 0.5 },
-            { x: 1598441190000, y: 0 },
-            { x: 1598441220000, y: null },
-            { x: 1598441250000, y: null },
-            { x: 1598441280000, y: 0 },
-            { x: 1598441310000, y: 0 },
-            { x: 1598441340000, y: null },
-            { x: 1598441370000, y: 1 },
-            { x: 1598441400000, y: null },
-          ],
-          average: 0.18894993894993897,
+        it('has the correct end date', async () => {
+          expect(last(errorRateResponse.erroneousTransactionsRate)?.x).to.be(1598441400000);
+        });
+
+        it('has the correct number of buckets', async () => {
+          expect(errorRateResponse.erroneousTransactionsRate.length).to.be(61);
+        });
+
+        it('has the correct calculation for average', async () => {
+          expect(errorRateResponse.average).to.be(0.25732600732600736);
         });
       });
     });
