3f2bf66116f9480ce9d4c54ac1c66564f9556d00
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2020-09-08 12:25:12 -0700

    closing the alert window if open in the test_user method

--- test/common/services/security/test_user.ts
+++ test/common/services/security/test_user.ts
@@ -81,6 +81,9 @@ export async function createTestUserService(
         });
 
         if (browser && testSubjects && shouldRefreshBrowser) {
+          // accept alert if it pops up
+          const alert = await browser.getAlert();
+          await alert?.accept();
           if (await testSubjects.exists('kibanaChrome', { allowHidden: true })) {
             await browser.refresh();
             await testSubjects.find('kibanaChrome', config.get('timeouts.find') * 10);
