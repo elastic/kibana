619108cac345ed02928e96e54a84cefef2ddab72
Quynh Nguyen <43350163+qn895@users.noreply.github.com>
2020-09-09 11:41:52 -0500

    [ML] Add decision path charts to exploration results table (#73561)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/ml/common/constants/data_frame_analytics.ts
+++ x-pack/plugins/ml/common/constants/data_frame_analytics.ts
@@ -3,7 +3,5 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-export const FEATURE_IMPORTANCE = 'feature_importance';
-export const FEATURE_INFLUENCE = 'feature_influence';
-export const TOP_CLASSES = 'top_classes';
-export const OUTLIER_SCORE = 'outlier_score';
+
+export const DEFAULT_RESULTS_FIELD = 'ml';
