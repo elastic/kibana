5cdade059831ecd87e1192c2299dcdd064bc9630
scottybollinger <scotty.bollinger@elastic.co>
2020-09-09 14:25:00 -0500

    Add constants and image

---
+++ x-pack/plugins/enterprise_search/public/applications/shared/constants/default_meta.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+export const DEFAULT_META = {
+  page: {
+    current: 1,
+    size: 10,
+    total_pages: 0,
+    total_results: 0,
+  },
+};
