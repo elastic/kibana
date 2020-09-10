e7b02d06cc023528ab868891fca8e523bb390917
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2020-09-10 14:26:35 -0400

    [Security Solution] Use safe type in resolver backend (#76969)
    * Moving generator to safe type version
    
    * Finished generator and alert
    
    * Gzipping again
    
    * Finishing type conversions for backend
    
    * Trying to cast front end tests back to unsafe type for now
    
    * Working reducer tests
    
    * Adding more comments and fixing alert type
    
    * Restoring resolver test data
    
    * Updating snapshot with timestamp info
    
    * Removing todo and fixing test
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/server/endpoint/routes/resolver/utils/node.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/resolver/utils/node.ts
@@ -5,14 +5,14 @@
  */
 
 import {
-  ResolverEvent,
-  ResolverAncestry,
-  ResolverLifecycleNode,
-  ResolverRelatedEvents,
-  ResolverTree,
-  ResolverChildNode,
+  SafeResolverAncestry,
+  SafeResolverTree,
   ResolverRelatedAlerts,
-  ResolverChildren,
+  SafeResolverChildren,
+  SafeResolverLifecycleNode,
+  SafeResolverEvent,
+  SafeResolverChildNode,
+  SafeResolverRelatedEvents,
 } from '../../../../../common/endpoint/types';
 
 /**
@@ -24,9 +24,9 @@ import {
  */
 export function createRelatedEvents(
   entityID: string,
-  events: ResolverEvent[] = [],
+  events: SafeResolverEvent[] = [],
   nextEvent: string | null = null
-): ResolverRelatedEvents {
+): SafeResolverRelatedEvents {
   return { entityID, events, nextEvent };
 }
 
@@ -39,7 +39,7 @@ export function createRelatedEvents(
  */
 export function createRelatedAlerts(
   entityID: string,
-  alerts: ResolverEvent[] = [],
+  alerts: SafeResolverEvent[] = [],
   nextAlert: string | null = null
 ): ResolverRelatedAlerts {
   return { entityID, alerts, nextAlert };
@@ -50,7 +50,7 @@ export function createRelatedAlerts(
  *
  * @param entityID the entity_id of the child
  */
-export function createChild(entityID: string): ResolverChildNode {
+export function createChild(entityID: string): SafeResolverChildNode {
   const lifecycle = createLifecycle(entityID, []);
   return {
     ...lifecycle,
@@ -60,7 +60,7 @@ export function createChild(entityID: string): ResolverChildNode {
 /**
  * Creates an empty ancestry response structure.
  */
-export function createAncestry(): ResolverAncestry {
+export function createAncestry(): SafeResolverAncestry {
   return { ancestors: [], nextAncestor: null };
 }
 
@@ -72,8 +72,8 @@ export function createAncestry(): ResolverAncestry {
  */
 export function createLifecycle(
   entityID: string,
-  lifecycle: ResolverEvent[]
-): ResolverLifecycleNode {
+  lifecycle: SafeResolverEvent[]
+): SafeResolverLifecycleNode {
   return { entityID, lifecycle };
 }
 
@@ -84,9 +84,9 @@ export function createLifecycle(
  * @param nextChild the cursor for the response
  */
 export function createChildren(
-  nodes: ResolverChildNode[] = [],
+  nodes: SafeResolverChildNode[] = [],
   nextChild: string | null = null
-): ResolverChildren {
+): SafeResolverChildren {
   return { childNodes: nodes, nextChild };
 }
 
@@ -95,7 +95,7 @@ export function createChildren(
  *
  * @param entityID the entity_id of the tree's origin node
  */
-export function createTree(entityID: string): ResolverTree {
+export function createTree(entityID: string): SafeResolverTree {
   return {
     entityID,
     children: {
