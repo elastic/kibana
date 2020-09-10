1e5d67d08b48776f619be40da398301cea335d49
Constance <constancecchen@users.noreply.github.com>
2020-09-09 20:19:46 -0700

    [Enterprise Search] Add Overview landing page/plugin (#76734) (#77113)
    * [public] Register Enterprise search plugin
    
    + move new Home strings to constants
    
    * [server] Register plugin access/visibility
    
    * Set up Enterprise Search Kibana Chrome
    
    - Add SetEnterpriseSearchChrome
    - Update Enterprise Search breadcrumbs to link to new overview plugin (+ update overview plugin URL per team discussion)
      - Add ability to break out of React Router basename by not using history.createhref
      - Update createHref mock to more closely match Kibana urls (adding /app prefix)
    - Minor documentation fix
    
    * Set up Enterprise Search plugin telemetry
    
    - client-side: SendEnterpriseSearchTelemetry
    - server-side: register saved objects, usage collector, etc.
    
    * Enterprise search overview views (#23)
    
    * Add formatTestSubj util
    
    This allows us to correctly format strings into our casing for data-test-subj attrs
    
    * Add images and stylesheet
    
    * Add product card component
    
    * Add index component
    
    * Remove unused styles
    
    * Fix inter-plugin links
    - by add shouldNotCreateHref prop to RR helpers
    - similiar to breadcrumb change
    
    * Fix/clean up CSS
    
    - Prefer EUI components over bespoke CSS (e.g. EuiCard)
    - Remove unused or unspecific CSS
    - Pull out product card CSS to its component
    - Fix kebab-cased CSS classes to camelCased
    
    * Clean up ProductCard props
    
    - Prefer passing in our plugin consts instead of separate props
    - Move productCardDescription to constants
    - Update tests
    
    * Add telemetry clicked actions to product buttons
    
    + revert data-test-subj strings to previous implementation
    + prune format_test_subj helper by using lodash util directly
    
    * [PR feedback] Add new plugin to applicationUsageSchema per telemetry team request
    
    * Fix failing functional navLinks test
    
    * Fix telemetry schema test
    
    * [Perf] Optimize assets size by switching from 300kb SVG to 25kb PNG
    
    * Only show product cards if the user has access to that product
    
    - adds access checks
    - fixes flex/CSS to show one card at a time
    
    Co-authored-by: Scotty Bollinger <scotty.bollinger@elastic.co>
    
    Co-authored-by: Scotty Bollinger <scotty.bollinger@elastic.co>

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search/components/product_card/index.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search/components/product_card/index.ts
@@ -4,8 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export {
-  SetEnterpriseSearchChrome,
-  SetAppSearchChrome,
-  SetWorkplaceSearchChrome,
-} from './set_chrome';
+export { ProductCard } from './product_card';
