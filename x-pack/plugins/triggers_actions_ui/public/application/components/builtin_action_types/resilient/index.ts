22b4e40ea09316d7209af1ec575f7af59e1948bf
Christos Nasikas <christos.nasikas@elastic.co>
2020-09-11 10:24:46 +0300

    [Alerting][Connectors] Refactor IBM Resilient: Generic Implementation (phase one) (#74357)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/resilient/index.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/resilient/index.ts
@@ -4,4 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { isUrlInvalid } from '../../utils/validators';
+export { getActionType as getResilientActionType } from './resilient';
