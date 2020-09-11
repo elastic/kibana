0ea3f8354eb80796ea61b4af5fa99cfdf12222a1
Christos Nasikas <christos.nasikas@elastic.co>
2020-09-11 12:00:49 +0300

    [7.x] [Alerting][Connectors] Refactor IBM Resilient: Generic Implementation (phase one) (#74357) (#77264)
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/resilient/config.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/resilient/config.ts
@@ -4,14 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { ConnectorConfiguration } from './types';
-
 import * as i18n from './translations';
 import logo from './logo.svg';
 
-export const connector: ConnectorConfiguration = {
+export const connectorConfiguration = {
   id: '.resilient',
-  name: i18n.RESILIENT_TITLE,
+  name: i18n.TITLE,
   logo,
   enabled: true,
   enabledInConfig: true,
