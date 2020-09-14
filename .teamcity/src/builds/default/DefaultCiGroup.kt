671d49c1653ea8979cbc270d6274f8091882af4b
Brian Seeders <brian.seeders@elastic.co>
2020-09-14 17:44:14 -0400

    Add missing env

--- .teamcity/src/builds/default/DefaultCiGroup.kt
+++ .teamcity/src/builds/default/DefaultCiGroup.kt
@@ -13,26 +13,11 @@ class DefaultCiGroup(val ciGroup: Int) : BuildType({
   name = "CI Group $ciGroup"
   paused = true
 
-  steps {
-//    script {
-//      name = "Build OSS Plugins"
-//      scriptContent =
-//        """
-//                #!/bin/bash
-//                ./.ci/teamcity/oss/build_plugins.sh
-//        """.trimIndent()
-//    }
-//
-//    // TODO is there a way to re-use what was built in the DefaultBuild job?
-//    script {
-//      name = "Build Default Plugins"
-//      scriptContent =
-//        """
-//                #!/bin/bash
-//                ./.ci/teamcity/default/build_plugins.sh
-//        """.trimIndent()
-//    }
+  params {
+    param("env.KBN_NP_PLUGINS_BUILT", "true")
+  }
 
+  steps {
     script {
       name = "Default CI Group $ciGroup"
       scriptContent =
