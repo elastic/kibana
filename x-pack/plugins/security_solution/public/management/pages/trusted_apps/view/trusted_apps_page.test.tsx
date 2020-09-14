406b474a22c4b0d782849523fab2107159b105eb
Paul Tavares <paul.tavares@elastic.co>
2020-09-14 12:56:05 -0400

    Fix UI UT for trusted app list page

--- x-pack/plugins/security_solution/public/management/pages/trusted_apps/view/trusted_apps_page.test.tsx
+++ x-pack/plugins/security_solution/public/management/pages/trusted_apps/view/trusted_apps_page.test.tsx
@@ -3,13 +3,23 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { shallow } from 'enzyme';
 import React from 'react';
-
+import * as reactTestingLibrary from '@testing-library/react';
 import { TrustedAppsPage } from './trusted_apps_page';
+import { AppContextTestRender, createAppRootMockRenderer } from '../../../../common/mock/endpoint';
 
 describe('TrustedAppsPage', () => {
+  let render: () => ReturnType<AppContextTestRender['render']>;
+
+  beforeEach(() => {
+    const mockedContext = createAppRootMockRenderer();
+    render = () => mockedContext.render(<TrustedAppsPage />);
+    reactTestingLibrary.act(() => {
+      mockedContext.history.push('/trusted_apps');
+    });
+  });
+
   test('rendering', () => {
-    expect(shallow(<TrustedAppsPage />)).toMatchSnapshot();
+    expect(render()).toMatchSnapshot();
   });
 });
