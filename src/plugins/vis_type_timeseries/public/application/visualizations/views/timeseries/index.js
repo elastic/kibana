21f34e3cc7924be0e8b79139de1b094e059b1566
sulemanof <daniil_suleiman@epam.com>
2020-09-14 13:08:02 +0300

    Show external pointer events in TSVB

--- src/plugins/vis_type_timeseries/public/application/visualizations/views/timeseries/index.js
+++ src/plugins/vis_type_timeseries/public/application/visualizations/views/timeseries/index.js
@@ -70,19 +70,20 @@ export const TimeSeries = ({
   annotations,
 }) => {
   const chartRef = useRef();
-  const updateCursor = (_, cursor) => {
-    if (chartRef.current) {
-      chartRef.current.dispatchExternalPointerEvent(cursor);
-    }
-  };
 
   useEffect(() => {
+    const updateCursor = (_, cursor) => {
+      if (chartRef.current) {
+        chartRef.current.dispatchExternalPointerEvent(cursor);
+      }
+    };
+
     eventBus.on(ACTIVE_CURSOR, updateCursor);
 
     return () => {
       eventBus.off(ACTIVE_CURSOR, undefined, updateCursor);
     };
-  }, []); // eslint-disable-line
+  }, []);
 
   const tooltipFormatter = decorateFormatter(xAxisFormatter);
   const uiSettings = getUISettings();
@@ -139,6 +140,7 @@ export const TimeSeries = ({
           type: tooltipMode === 'show_focused' ? TooltipType.Follow : TooltipType.VerticalCursor,
           headerFormatter: tooltipFormatter,
         }}
+        externalPointerEvents={{ tooltip: { visible: true } }}
       />
 
       {annotations.map(({ id, data, icon, color }) => {
