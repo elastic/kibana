f2b16b2b53c8f3c87b663ced1d3c8bb545b11a27
Patryk Kopycinski <contact@patrykkopycinski.com>
2020-09-15 17:14:30 +0200

    cleanup

--- x-pack/plugins/security_solution/public/network/containers/kpi_network/unique_private_ips/index.tsx
+++ x-pack/plugins/security_solution/public/network/containers/kpi_network/unique_private_ips/index.tsx
@@ -123,14 +123,14 @@ export const useNetworkKpiUniquePrivateIps = ({
                   setLoading(false);
                 }
                 // TODO: Make response error status clearer
-                notifications.toasts.addWarning(i18n.ERROR_NETWORK_KPI_DNS);
+                notifications.toasts.addWarning(i18n.ERROR_NETWORK_KPI_UNIQUE_PRIVATE_IPS);
                 searchSubscription$.unsubscribe();
               }
             },
             error: (msg) => {
               if (!(msg instanceof AbortError)) {
                 notifications.toasts.addDanger({
-                  title: i18n.FAIL_NETWORK_KPI_DNS,
+                  title: i18n.FAIL_NETWORK_KPI_UNIQUE_PRIVATE_IPS,
                   text: msg.message,
                 });
               }
