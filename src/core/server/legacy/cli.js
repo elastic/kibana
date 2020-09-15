e7d8ea3927a7255b14f92f790c02aa1d16ee6004
Mikhail Shustov <restrry@gmail.com>
2020-09-15 13:41:27 +0300

    Extract src/core in a separate TS project (#76785)
    * break dependency on data plugin TS code
    
    * move global typings to @kbn/utility-types
    
    * import types from @kbn/utility-types
    
    * remove type dependency on plugins
    
    * add intermediate js files to break dependency on outter TS code
    
    * temp type declaration for query-string
    
    * declare src/core project
    
    * export types to reference in the built d.ts files
    
    * reference core project
    
    * move jest types out of kbn/utility-types due to a clash with mocha types
    
    * fix wrong es_kuery path and ts project paths
    
    * reference core from packages consuming it
    
    * x-pack & oss should use the same lodash version
    
    * Revert "x-pack & oss should use the same lodash version"
    
    This reverts commit 79cec5781ed5e6a7320afe7cd4fe649ceba0bc68.
    
    * use the same lodash version
    
    * fix @types/lodash TS2300: Duplicate identifier error
    
    * fix wrong imports
    
    * update docs
    
    * update docs
    
    * add a comment why file is needed

--- src/core/server/legacy/cli.js
+++ src/core/server/legacy/cli.js
@@ -16,29 +16,5 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import type { PublicMethodsOf } from '@kbn/utility-types';
-import { ModalService, OverlayModalStart } from './modal_service';
 
-const createStartContractMock = () => {
-  const startContract: jest.Mocked<OverlayModalStart> = {
-    open: jest.fn().mockReturnValue({
-      close: jest.fn(),
-      onClose: Promise.resolve(),
-    }),
-    openConfirm: jest.fn().mockResolvedValue(true),
-  };
-  return startContract;
-};
-
-const createMock = () => {
-  const mocked: jest.Mocked<PublicMethodsOf<ModalService>> = {
-    start: jest.fn(),
-  };
-  mocked.start.mockReturnValue(createStartContractMock());
-  return mocked;
-};
-
-export const overlayModalServiceMock = {
-  create: createMock,
-  createStartContract: createStartContractMock,
-};
+export { startRepl } from '../../../cli/repl';
