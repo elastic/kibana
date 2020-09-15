7f3ec3f1e48aabb6a192e60139935308f3cba785
Ryland Herrick <ryalnd@gmail.com>
2020-09-15 12:29:49 -0500

    [Security Solution] [Detections] EQL Rule Creation (#76831)
    * Use existing predicate helper to avoid hardcoded strings
    
    * Render our field components with React.createElement
    
    Without this, we get some bad behaviors:
    * Cannot use React.memo'd components
    * Cannot switch between UseField components (causes a "change in the
      order of hooks" error from React)
    
    * WIP: EQL Rules can be created
    
    WIP because: they're probably not treated well in the UI, and they're certainly not
    going to execute properly, and there are no tests.
    
    * Add unit tests for changes to schema + helpers
    
    * Add unit tests for new EQL query input component
    
    It's mostly just a glorified textarea for now.
    
    * Add integration test for EQL Rule creation
    
    * Does not assert the query language, as that is not displayed on Rule
      Details
    * Does not exercise rule execution
    
    * Use predicate helper
    
    * Throw an error if an EQL Rule is executed
    
    This is to prevent undefined behavior until EQL execution is
    implemented.
    
    * Fix failing tests
    
    I changed the default value for the form field mock from an array to a
    string; this fixes the few tests that were relying on it being an array.
    
    * Audit our rule statements/switches
    
    I made a pass through our treatment of RuleType to verify that EQL rules
    would be treated appropriately. Since the default/fallthrough case is
    typically the Query rule, and since this rule has the same
    attributes/behavior as the new EQL rule, not much had to change here.
    
    I converted a few if statements to exhaustive switches where possible,
    and used predicate helpers in places where it was not.
    
    * Add tests around use of custom components with UseField
    
    There was an issue previously where memoized components would not work;
    these are primarily regression tests covering that use case.
    
    * Fix typo
    
    * Add keys to UseField to ensure unmount
    
    When swapping between the Custom Query and EQL rule types, we want to
    ensure that the corresponding input component coming from UseField fully
    unmounts and remounts with the new component.
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.ts
@@ -14,6 +14,7 @@ import {
   SERVER_APP_ID,
 } from '../../../../common/constants';
 import { isJobStarted, isMlRule } from '../../../../common/machine_learning/helpers';
+import { isThresholdRule, isEqlRule } from '../../../../common/detection_engine/utils';
 import { parseScheduleDates } from '../../../../common/detection_engine/parse_schedule_dates';
 import { SetupPlugins } from '../../../plugin';
 import { getInputIndex } from './get_input_output_index';
@@ -244,7 +245,9 @@ export const signalRulesAlertType = ({
           if (bulkCreateDuration) {
             result.bulkCreateTimes.push(bulkCreateDuration);
           }
-        } else if (type === 'threshold' && threshold) {
+        } else if (isEqlRule(type)) {
+          throw new Error('EQL Rules are under development, execution is not yet implemented');
+        } else if (isThresholdRule(type) && threshold) {
           const inputIndex = await getInputIndex(services, version, index);
           const esFilter = await getFilter({
             type,
