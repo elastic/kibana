c1db867f2acc44071f05e00003cc4378f357d5ff
Nathan L Smith <smith@nlsmith.com>
2020-09-15 14:56:34 -0500

    Canvas fixes

--- x-pack/plugins/canvas/storybook/main.ts
+++ x-pack/plugins/canvas/storybook/main.ts
@@ -4,11 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { existsSync } = require('fs');
+const { join } = require('path');
+
+// Check for DLL
+if (!existsSync(join(__dirname, '../../../../built_assets/canvas_storybook_dll/manifest.json'))) {
+  // eslint-disable-next-line no-console
+  console.error(
+    'No DLL found. Run `node scripts/storybook --dll` from the Canvas plugin directory.'
+  );
+  process.exit(1);
+}
+
 module.exports = {
   stories: ['../**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    './storybook/addon/target/register',
-  ],
+  addons: ['@storybook/addon-actions', '@storybook/addon-knobs', './addon/src/register'],
 };
