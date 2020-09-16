ad7283ddf9e83c6d25c5168685218f4bb12e2be0
Dario Gieselaar <dario.gieselaar@elastic.co>
2020-09-16 19:20:08 +0200

    [APM] Metrics-powered UI (#73953)
    * [APM] Optimize service overview queries
    
    * Review feedback
    
    * Use correct indices/filters for service overview metrics
    
    * [APM] Optimize traces overview
    
    * Separate queries into separate file
    
    * Support union types for aggregations
    
    * [APM] Don't fetch dynamic index pattern in setupRequest
    
    We don't need a dynamic index pattern for parsing the filters from the query bar. Additionally, instead of fetching uiIndices in `getParamsForSearchRequest`, we can use `indices` that we already fetched in `setupRequest`.
    
    * use join utility function to merge requests
    
    * Correct path for UI_SETTINGS import
    
    * Query transaction indices in getEnvironments()
    
    * [APM] Introduce apm.types
    
    Instead of using a combination of index + terms filters on processor.event, add a top-level setting that allows you to define a type, which can be a processor event type, agent configuration or custom link. This allows us to more easily compose queries.
    
    * Set size of terms agg on error rate aggregation
    
    * Metrics
    
    * Use separate clients for apm events and other uses
    
    * Separate function for calculating relative impact
    
    * use UIProcessorEvent type in IURLParams type
    
    * Remove unused import
    
    * Split out strategy in helper functions
    
    * Use cloneDeep in apmEventClient.search
    
    * Consistent usage of getUseAggregatedTransactions
    
    * Update traces functional test
    
    * Update API tests
    
    * Update responses for functional tests
    
    * Review feedback
    
    * Fix type for filter/filters aggregation
    
    * Review feedback
    
    * Rename useAggregatedTransactions > searchAggregatedTransactions
    
    * Use correct route name for transaction redirect page
    
    * Closes #67744.
    
    * Review feedback; offer other strategies than 'auto'
    
    * Fix functional tests
    
    * [APM] Always load esarchives from common
    
    Instead of requiring every test suite to store its archives under {suite}/fixtures/es_archiver, always load them from common/fixtures/es_archiver.
    
    * Update script
    
    * Make sure tests pass
    
    * Update snapshots for API tests
    
    * Filter for transaction.root when fetching top traces
    
    * Make sure must_not clause is formatted correctly
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/apm/server/lib/service_map/get_service_map_service_node_info.ts
+++ x-pack/plugins/apm/server/lib/service_map/get_service_map_service_node_info.ts
@@ -4,14 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import {
+  TRANSACTION_REQUEST,
+  TRANSACTION_PAGE_LOAD,
+} from '../../../common/transaction_types';
 import { UIFilters } from '../../../typings/ui_filters';
 import {
   SERVICE_NAME,
-  TRANSACTION_DURATION,
-  TRANSACTION_TYPE,
   METRIC_SYSTEM_CPU_PERCENT,
   METRIC_SYSTEM_FREE_MEMORY,
   METRIC_SYSTEM_TOTAL_MEMORY,
+  TRANSACTION_TYPE,
 } from '../../../common/elasticsearch_fieldnames';
 import { ProcessorEvent } from '../../../common/processor_event';
 import { rangeFilter } from '../../../common/utils/range_filter';
@@ -19,9 +22,10 @@ import { ESFilter } from '../../../typings/elasticsearch';
 import { Setup, SetupTimeRange } from '../helpers/setup_request';
 import { percentMemoryUsedScript } from '../metrics/by_agent/shared/memory';
 import {
-  TRANSACTION_REQUEST,
-  TRANSACTION_PAGE_LOAD,
-} from '../../../common/transaction_types';
+  getProcessorEventForAggregatedTransactions,
+  getTransactionDurationFieldForAggregatedTransactions,
+  getDocumentTypeFilterForAggregatedTransactions,
+} from '../helpers/aggregated_transactions';
 import { getErrorRate } from '../transaction_groups/get_error_rate';
 import { getEnvironmentUiFilterES } from '../helpers/convert_ui_filters/get_environment_ui_filter_es';
 
@@ -29,11 +33,13 @@ interface Options {
   setup: Setup & SetupTimeRange;
   environment?: string;
   serviceName: string;
+  searchAggregatedTransactions: boolean;
 }
 
 interface TaskParameters {
   environment?: string;
   filter: ESFilter[];
+  searchAggregatedTransactions: boolean;
   minutes: number;
   serviceName?: string;
   setup: Setup;
@@ -42,6 +48,7 @@ interface TaskParameters {
 export async function getServiceMapServiceNodeInfo({
   serviceName,
   setup,
+  searchAggregatedTransactions,
   uiFilters,
 }: Options & { serviceName: string; uiFilters: UIFilters }) {
   const { start, end } = setup;
@@ -56,6 +63,7 @@ export async function getServiceMapServiceNodeInfo({
   const taskParams = {
     environment: uiFilters.environment,
     filter,
+    searchAggregatedTransactions,
     minutes,
     serviceName,
     setup,
@@ -104,6 +112,7 @@ async function getTransactionStats({
   setup,
   filter,
   minutes,
+  searchAggregatedTransactions,
 }: TaskParameters): Promise<{
   avgTransactionDuration: number | null;
   avgRequestsPerMinute: number | null;
@@ -112,7 +121,11 @@ async function getTransactionStats({
 
   const params = {
     apm: {
-      events: [ProcessorEvent.transaction],
+      events: [
+        getProcessorEventForAggregatedTransactions(
+          searchAggregatedTransactions
+        ),
+      ],
     },
     body: {
       size: 0,
@@ -120,6 +133,9 @@ async function getTransactionStats({
         bool: {
           filter: [
             ...filter,
+            ...getDocumentTypeFilterForAggregatedTransactions(
+              searchAggregatedTransactions
+            ),
             {
               terms: {
                 [TRANSACTION_TYPE]: [
@@ -132,15 +148,31 @@ async function getTransactionStats({
         },
       },
       track_total_hits: true,
-      aggs: { duration: { avg: { field: TRANSACTION_DURATION } } },
+      aggs: {
+        duration: {
+          avg: {
+            field: getTransactionDurationFieldForAggregatedTransactions(
+              searchAggregatedTransactions
+            ),
+          },
+        },
+        count: {
+          value_count: {
+            field: getTransactionDurationFieldForAggregatedTransactions(
+              searchAggregatedTransactions
+            ),
+          },
+        },
+      },
     },
   };
   const response = await apmEventClient.search(params);
-  const docCount = response.hits.total.value;
+
+  const totalRequests = response.aggregations?.count.value ?? 0;
 
   return {
     avgTransactionDuration: response.aggregations?.duration.value ?? null,
-    avgRequestsPerMinute: docCount > 0 ? docCount / minutes : null,
+    avgRequestsPerMinute: totalRequests > 0 ? totalRequests / minutes : null,
   };
 }
 
@@ -178,6 +210,7 @@ async function getMemoryStats({
       events: [ProcessorEvent.metric],
     },
     body: {
+      size: 0,
       query: {
         bool: {
           filter: [
