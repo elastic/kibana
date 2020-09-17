361deda530b573bf163804f8741ecdbdf1fe7034
pgayvallet <pierre.gayvallet@elastic.co>
2020-09-17 14:18:30 +0200

    remove `??` syntax from package

--- packages/kbn-legacy-logging/src/rotate/log_rotator.ts
+++ packages/kbn-legacy-logging/src/rotate/log_rotator.ts
@@ -130,7 +130,7 @@ export class LogRotator {
         // setup conditions that would fire the observable
         this.stalkerUsePollingPolicyTestTimeout = setTimeout(
           () => completeFn(true),
-          this.config.rotate.pollingPolicyTestTimeout ?? 15000
+          this.config.rotate.pollingPolicyTestTimeout || 15000
         );
         testWatcher.on('change', () => completeFn(false));
         testWatcher.on('error', () => completeFn(true));
