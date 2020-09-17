dfc84932cc91e7963f36803985f6d32e932899f2
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-09-17 19:13:40 +0200

    Add testdata

--- x-pack/test/functional/apps/discover/error_handling.ts
+++ x-pack/test/functional/apps/discover/error_handling.ts
@@ -7,7 +7,7 @@
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
-export default function ({ getService, getPageObjects }: FtrProviderContext) {
+export default function ({ getService, getPageObjects }) {
   const esArchiver = getService('esArchiver');
   const testSubjects = getService('testSubjects');
   const PageObjects = getPageObjects(['common', 'discover', 'timePicker']);
