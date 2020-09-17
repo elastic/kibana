cc865b0eadad45b3df6dd81630f472d3b78db974
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2020-09-17 16:15:07 +0200

    Remove `npStart` from `callGetTopLogEntryCategoriesAPI`

--- x-pack/plugins/infra/public/pages/logs/log_entry_categories/service_calls/get_top_log_entry_categories.ts
+++ x-pack/plugins/infra/public/pages/logs/log_entry_categories/service_calls/get_top_log_entry_categories.ts
@@ -4,10 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import type { HttpSetup } from 'src/core/public';
+
 import { fold } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { identity } from 'fp-ts/lib/function';
-import { npStart } from '../../../../legacy_singletons';
 
 import {
   getLogEntryCategoriesRequestPayloadRT,
@@ -16,16 +17,22 @@ import {
 } from '../../../../../common/http_api/log_analysis';
 import { createPlainError, throwErrors } from '../../../../../common/runtime_types';
 
+interface RequestArgs {
+  sourceId: string;
+  startTime: number;
+  endTime: number;
+  categoryCount: number;
+  datasets?: string[];
+}
+
 export const callGetTopLogEntryCategoriesAPI = async (
-  sourceId: string,
-  startTime: number,
-  endTime: number,
-  categoryCount: number,
-  datasets?: string[]
+  requestArgs: RequestArgs,
+  fetch: HttpSetup['fetch']
 ) => {
+  const { sourceId, startTime, endTime, categoryCount, datasets } = requestArgs;
   const intervalDuration = endTime - startTime;
 
-  const response = await npStart.http.fetch(LOG_ANALYSIS_GET_LOG_ENTRY_CATEGORIES_PATH, {
+  const response = await fetch(LOG_ANALYSIS_GET_LOG_ENTRY_CATEGORIES_PATH, {
     method: 'POST',
     body: JSON.stringify(
       getLogEntryCategoriesRequestPayloadRT.encode({
