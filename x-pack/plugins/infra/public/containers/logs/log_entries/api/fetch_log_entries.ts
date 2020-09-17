fa7d95d8cc6e29a256f1cabe62de6c7d561e07cb
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2020-09-17 15:17:29 +0200

    Remove `npStart` from `fetchLogEntries`

--- x-pack/plugins/infra/public/containers/logs/log_entries/api/fetch_log_entries.ts
+++ x-pack/plugins/infra/public/containers/logs/log_entries/api/fetch_log_entries.ts
@@ -4,10 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import type { HttpSetup } from 'src/core/public';
 import { fold } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { identity } from 'fp-ts/lib/function';
-import { npStart } from '../../../../legacy_singletons';
 
 import { throwErrors, createPlainError } from '../../../../../common/runtime_types';
 
@@ -18,8 +18,11 @@ import {
   logEntriesResponseRT,
 } from '../../../../../common/http_api';
 
-export const fetchLogEntries = async (requestArgs: LogEntriesRequest) => {
-  const response = await npStart.http.fetch(LOG_ENTRIES_PATH, {
+export const fetchLogEntries = async (
+  requestArgs: LogEntriesRequest,
+  fetch: HttpSetup['fetch']
+) => {
+  const response = await fetch(LOG_ENTRIES_PATH, {
     method: 'POST',
     body: JSON.stringify(logEntriesRequestRT.encode(requestArgs)),
   });
