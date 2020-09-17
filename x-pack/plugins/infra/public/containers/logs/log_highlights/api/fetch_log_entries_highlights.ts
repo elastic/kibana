4e0abeedc3809afcef800acd486559d96ace5df8
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2020-09-17 15:19:35 +0200

    Remove `npStart` from `fetchLogEntriesHighlights`

--- x-pack/plugins/infra/public/containers/logs/log_highlights/api/fetch_log_entries_highlights.ts
+++ x-pack/plugins/infra/public/containers/logs/log_highlights/api/fetch_log_entries_highlights.ts
@@ -4,10 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import type { HttpSetup } from 'src/core/public';
 import { fold } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { identity } from 'fp-ts/lib/function';
-import { npStart } from '../../../../legacy_singletons';
 
 import { throwErrors, createPlainError } from '../../../../../common/runtime_types';
 
@@ -18,8 +18,11 @@ import {
   logEntriesHighlightsResponseRT,
 } from '../../../../../common/http_api';
 
-export const fetchLogEntriesHighlights = async (requestArgs: LogEntriesHighlightsRequest) => {
-  const response = await npStart.http.fetch(LOG_ENTRIES_HIGHLIGHTS_PATH, {
+export const fetchLogEntriesHighlights = async (
+  requestArgs: LogEntriesHighlightsRequest,
+  fetch: HttpSetup['fetch']
+) => {
+  const response = await fetch(LOG_ENTRIES_HIGHLIGHTS_PATH, {
     method: 'POST',
     body: JSON.stringify(logEntriesHighlightsRequestRT.encode(requestArgs)),
   });
