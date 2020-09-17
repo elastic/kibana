0693578d15dac590f55e61306ef85fa74249004e
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2020-09-17 15:43:45 +0200

    Remove `npStart` from `callGetLogEntryExamplesAPI`

--- x-pack/plugins/infra/public/pages/logs/log_entry_rate/service_calls/get_log_entry_examples.ts
+++ x-pack/plugins/infra/public/pages/logs/log_entry_rate/service_calls/get_log_entry_examples.ts
@@ -4,10 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import type { HttpSetup } from 'src/core/public';
 import { fold } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { identity } from 'fp-ts/lib/function';
-import { npStart } from '../../../../legacy_singletons';
 
 import {
   getLogEntryExamplesRequestPayloadRT,
@@ -16,15 +16,21 @@ import {
 } from '../../../../../common/http_api/log_analysis';
 import { createPlainError, throwErrors } from '../../../../../common/runtime_types';
 
+interface RequestArgs {
+  sourceId: string;
+  startTime: number;
+  endTime: number;
+  dataset: string;
+  exampleCount: number;
+  categoryId?: string;
+}
+
 export const callGetLogEntryExamplesAPI = async (
-  sourceId: string,
-  startTime: number,
-  endTime: number,
-  dataset: string,
-  exampleCount: number,
-  categoryId?: string
+  requestArgs: RequestArgs,
+  fetch: HttpSetup['fetch']
 ) => {
-  const response = await npStart.http.fetch(LOG_ANALYSIS_GET_LOG_ENTRY_RATE_EXAMPLES_PATH, {
+  const { sourceId, startTime, endTime, dataset, exampleCount, categoryId } = requestArgs;
+  const response = await fetch(LOG_ANALYSIS_GET_LOG_ENTRY_RATE_EXAMPLES_PATH, {
     method: 'POST',
     body: JSON.stringify(
       getLogEntryExamplesRequestPayloadRT.encode({
