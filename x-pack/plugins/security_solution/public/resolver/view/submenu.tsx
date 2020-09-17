92553b16c20490a100ad85de0f44e464a8aaf78c
Brent Kimmel <brent.kimmel@elastic.co>
2020-09-17 15:45:56 -0400

    [Security Solution][Resolver] Show all event counts

--- x-pack/plugins/security_solution/public/resolver/view/submenu.tsx
+++ x-pack/plugins/security_solution/public/resolver/view/submenu.tsx
@@ -4,11 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/* eslint-disable react/display-name */
-
 import { i18n } from '@kbn/i18n';
-import React, { useState, useCallback, useRef, useLayoutEffect, useMemo } from 'react';
-import { EuiI18nNumber, EuiButton, EuiPopover, ButtonColor } from '@elastic/eui';
+import React, { useMemo } from 'react';
+import { EuiI18nNumber, ButtonColor } from '@elastic/eui';
 import styled from 'styled-components';
 import { ResolverNodeStats } from '../../../common/endpoint/types';
 import { useRelatedEventByCategoryNavigation } from './use_related_event_by_category_navigation';
@@ -45,53 +43,6 @@ interface ResolverSubmenuOption {
 
 export type ResolverSubmenuOptionList = ResolverSubmenuOption[] | string;
 
-const StyledActionButton = styled(EuiButton)`
-  &.euiButton--small {
-    height: fit-content;
-    line-height: 1;
-    padding: 0.25em;
-    font-size: 0.85rem;
-  }
-`;
-
-/**
- * This will be the "host button" that displays the "total number of related events" and opens
- * the sumbmenu (with counts by category) when clicked.
- */
-const SubButton = React.memo(
-  ({
-    hasMenu,
-    menuIsOpen,
-    action,
-    count,
-    nodeID,
-  }: {
-    hasMenu: boolean;
-    menuIsOpen?: boolean;
-    action: (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-    count?: number;
-    nodeID: string;
-  }) => {
-    const iconType = menuIsOpen === true ? 'arrowUp' : 'arrowDown';
-    return (
-      <StyledActionButton
-        onClick={action}
-        iconType={hasMenu ? iconType : 'none'}
-        fill={false}
-        color={'primary'}
-        size="s"
-        iconSide="right"
-        tabIndex={-1}
-        data-test-subj="resolver:submenu:button"
-        data-test-resolver-node-id={nodeID}
-        id={nodeID}
-      >
-        {count ? <EuiI18nNumber value={count} /> : ''} {subMenuAssets.relatedEvents.title}
-      </StyledActionButton>
-    );
-  }
-);
-
 /**
  * A Submenu to be displayed in one of two forms:
  *   1) Provided a collection of `optionsWithActions`: it will call `menuAction` then - if and when menuData becomes available - display each item with an optional prefix and call the supplied action for the options when that option is clicked.
@@ -120,32 +71,7 @@ const NodeSubMenuComponents = React.memo(
     nodeID: string;
     relatedEventStats: ResolverNodeStats | undefined;
   }) => {
-    // keep a ref to the popover so we can call its reposition method
-    const popoverRef = useRef<EuiPopover>(null);
-
-    const [menuIsOpen, setMenuOpen] = useState(false);
-    const handleMenuOpenClick = useCallback(
-      (clickEvent: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        // stopping propagation/default to prevent other node animations from triggering
-        clickEvent.preventDefault();
-        clickEvent.stopPropagation();
-        setMenuOpen(!menuIsOpen);
-      },
-      [menuIsOpen]
-    );
-    const handleMenuActionClick = useCallback(
-      (clickEvent: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        // stopping propagation/default to prevent other node animations from triggering
-        clickEvent.preventDefault();
-        clickEvent.stopPropagation();
-        if (typeof menuAction === 'function') menuAction();
-        setMenuOpen(true);
-      },
-      [menuAction]
-    );
-
     // The last projection matrix that was used to position the popover
-    const projectionMatrixAtLastRender = useRef<Matrix3>();
     const relatedEventCallbacks = useRelatedEventByCategoryNavigation({
       nodeID,
       categories: relatedEventStats?.events?.byCategory,
@@ -164,24 +90,6 @@ const NodeSubMenuComponents = React.memo(
       }
     }, [relatedEventStats, relatedEventCallbacks]);
 
-    useLayoutEffect(() => {
-      if (
-        /**
-         * If there is a popover component reference,
-         * and this isn't the first render,
-         * and the projectionMatrix has changed since last render,
-         * then force the popover to reposition itself.
-         */
-        popoverRef.current &&
-        projectionMatrixAtLastRender.current &&
-        projectionMatrixAtLastRender.current !== projectionMatrix
-      ) {
-        popoverRef.current.positionPopoverFixed();
-      }
-
-      // no matter what, keep track of the last project matrix that was used to size the popover
-      projectionMatrixAtLastRender.current = projectionMatrix;
-    }, [projectionMatrixAtLastRender, projectionMatrix]);
     const {
       colorMap: { pillStroke: pillBorderStroke, resolverBackground: pillFill },
     } = useResolverTheme();
@@ -191,64 +99,31 @@ const NodeSubMenuComponents = React.memo(
         backgroundColor: pillFill,
       };
     }, [pillBorderStroke, pillFill]);
-    if (relatedEventStats === undefined) {
-      /**
-       * When called with a `menuAction`
-       * Render without dropdown and call the supplied action when host button is clicked
-       */
-      return (
-        <div className={className}>
-          <EuiButton
-            onClick={handleMenuActionClick}
-            color={buttonBorderColor}
-            size="s"
-            tabIndex={-1}
-          >
-            {subMenuAssets.relatedEvents.title}
-          </EuiButton>
-        </div>
-      );
-    }
 
     if (relatedEventOptions === undefined) {
       return null;
     }
 
     return (
-      <>
-        <SubButton
-          hasMenu={true}
-          menuIsOpen={menuIsOpen}
-          action={handleMenuOpenClick}
-          count={count}
-          nodeID={nodeID}
-        />
-        {menuIsOpen ? (
-          <ul
-            className={`${className} options`}
-            aria-hidden={!menuIsOpen}
-            aria-describedby={nodeID}
-          >
-            {relatedEventOptions
-              .sort((opta, optb) => {
-                return opta.optionTitle.localeCompare(optb.optionTitle);
-              })
-              .map((opt) => {
-                return (
-                  <li
-                    className="item"
-                    data-test-subj="resolver:map:node-submenu-item"
-                    style={listStylesFromTheme}
-                  >
-                    <button type="button" className="kbn-resetFocusState" onClick={opt.action}>
-                      {opt.prefix} {opt.optionTitle}
-                    </button>
-                  </li>
-                );
-              })}
-          </ul>
-        ) : null}
-      </>
+      <ul className={`${className} options`} aria-describedby={nodeID}>
+        {relatedEventOptions
+          .sort((opta, optb) => {
+            return opta.optionTitle.localeCompare(optb.optionTitle);
+          })
+          .map((opt) => {
+            return (
+              <li
+                className="item"
+                data-test-subj="resolver:map:node-submenu-item"
+                style={listStylesFromTheme}
+              >
+                <button type="button" className="kbn-resetFocusState" onClick={opt.action}>
+                  {opt.prefix} {opt.optionTitle}
+                </button>
+              </li>
+            );
+          })}
+      </ul>
     );
   }
 );
@@ -300,18 +175,5 @@ export const NodeSubMenu = styled(NodeSubMenuComponents)`
 
   &.options .item button:active {
     transform: scale(0.95);
-  }
-
-  & .euiButton {
-    background-color: ${(props) => props.buttonFill};
-    border-color: ${(props) => props.buttonBorderColor};
-    border-style: solid;
-    border-width: 1px;
-
-    &:hover,
-    &:active,
-    &:focus {
-      background-color: ${(props) => props.buttonFill};
-    }
   }
 `;
