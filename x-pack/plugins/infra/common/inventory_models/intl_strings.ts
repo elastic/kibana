25820079c1eee1abc1a868991e2481b6ed1df89e
Zacqary Xeper <zacqary.xeper@elastic.co>
2020-09-17 16:17:54 -0500

    Remove unused i18n

--- x-pack/plugins/infra/common/inventory_models/intl_strings.ts
+++ x-pack/plugins/infra/common/inventory_models/intl_strings.ts
@@ -7,35 +7,6 @@
 import { i18n } from '@kbn/i18n';
 import { toMetricOpt } from '../snapshot_metric_i18n';
 import { SnapshotMetricType, SnapshotMetricTypeKeys } from './types';
-export const CPUUsage = i18n.translate('xpack.infra.waffle.metricOptions.cpuUsageText', {
-  defaultMessage: 'CPU usage',
-});
-
-export const MemoryUsage = i18n.translate('xpack.infra.waffle.metricOptions.memoryUsageText', {
-  defaultMessage: 'Memory usage',
-});
-
-export const InboundTraffic = i18n.translate(
-  'xpack.infra.waffle.metricOptions.inboundTrafficText',
-  {
-    defaultMessage: 'Inbound traffic',
-  }
-);
-
-export const OutboundTraffic = i18n.translate(
-  'xpack.infra.waffle.metricOptions.outboundTrafficText',
-  {
-    defaultMessage: 'Outbound traffic',
-  }
-);
-
-export const LogRate = i18n.translate('xpack.infra.waffle.metricOptions.hostLogRateText', {
-  defaultMessage: 'Log rate',
-});
-
-export const Load = i18n.translate('xpack.infra.waffle.metricOptions.loadText', {
-  defaultMessage: 'Load',
-});
 
 interface Lookup {
   [id: string]: string;
