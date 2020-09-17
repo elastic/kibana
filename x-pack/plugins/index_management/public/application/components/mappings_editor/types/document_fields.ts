a3ff68e5503124f061fd85716767a76574c58780
Alison Goryachev <alison.goryachev@elastic.co>
2020-09-17 11:14:06 -0400

    [Mappings editor] Add support for constant_keyword field type (#76564) (#77765)

--- x-pack/plugins/index_management/public/application/components/mappings_editor/types/document_fields.ts
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/types/document_fields.ts
@@ -59,6 +59,7 @@ export type MainType =
   | 'geo_point'
   | 'geo_shape'
   | 'token_count'
+  | 'constant_keyword'
   | 'wildcard'
   /**
    * 'other' is a special type that only exists inside of MappingsEditor as a placeholder
@@ -146,7 +147,9 @@ export type ParameterName =
   | 'dims'
   | 'depth_limit'
   | 'relations'
-  | 'max_shingle_size';
+  | 'max_shingle_size'
+  | 'value'
+  | 'meta';
 
 export interface Parameter {
   fieldConfig: FieldConfig;
