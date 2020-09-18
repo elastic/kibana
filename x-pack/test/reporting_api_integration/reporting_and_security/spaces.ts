98e3db00415aa71fe913166a8463be1c748653a4
Timothy Sullivan <tsullivan@elastic.co>
2020-09-18 09:44:59 -0700

    re-add skips of flaky tests

--- x-pack/test/reporting_api_integration/reporting_and_security/spaces.ts
+++ x-pack/test/reporting_api_integration/reporting_and_security/spaces.ts
@@ -102,7 +102,7 @@ export default function ({ getService }: FtrProviderContext) {
     });
 
     // FLAKY: https://github.com/elastic/kibana/issues/76551
-    it('should complete a job of PNG export of a dashboard in non-default space', async () => {
+    it.skip('should complete a job of PNG export of a dashboard in non-default space', async () => {
       const downloadPath = await reportingAPI.postJobJSON(
         `/s/non_default_space/api/reporting/generate/png`,
         {
@@ -116,7 +116,7 @@ export default function ({ getService }: FtrProviderContext) {
     });
 
     // FLAKY: https://github.com/elastic/kibana/issues/76551
-    it('should complete a job of PDF export of a dashboard in non-default space', async () => {
+    it.skip('should complete a job of PDF export of a dashboard in non-default space', async () => {
       const downloadPath = await reportingAPI.postJobJSON(
         `/s/non_default_space/api/reporting/generate/printablePdf`,
         {
