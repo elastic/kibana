79dfdd4a53d450ff808a4a4921c1b434bad2726d
neptunian <sandra.gonzales@elastic.co>
2020-09-18 13:49:32 -0400

    error if trying to update to diff version of kibana

--- x-pack/plugins/ingest_manager/server/routes/agent/upgrade_handler.ts
+++ x-pack/plugins/ingest_manager/server/routes/agent/upgrade_handler.ts
@@ -9,6 +9,7 @@ import { TypeOf } from '@kbn/config-schema';
 import { PostAgentUpgradeResponse } from '../../../common/types';
 import { PostAgentUpgradeRequestSchema } from '../../types';
 import * as AgentService from '../../services/agents';
+import { appContextService } from '../../services';
 
 export const postAgentUpgradeHandler: RequestHandler<
   TypeOf<typeof PostAgentUpgradeRequestSchema.params>,
@@ -17,6 +18,18 @@ export const postAgentUpgradeHandler: RequestHandler<
 > = async (context, request, response) => {
   const soClient = context.core.savedObjects.client;
   const { version, source_uri: sourceUri } = request.body;
+
+  // temporarily only allow upgrading to the same version as the installed kibana version
+  const kibanaVersion = appContextService.getKibanaVersion();
+  if (kibanaVersion !== version) {
+    return response.customError({
+      statusCode: 400,
+      body: {
+        message: `cannot upgrade agent to ${version} because it is different than the installed kibana version ${kibanaVersion}`,
+      },
+    });
+  }
+
   try {
     await AgentService.sendUpgradeAgentAction({
       soClient,
