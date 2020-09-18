c5fec50cd086484504b8f3f1be04ba71eff9cee1
Peter Pisljar <peter.pisljar@gmail.com>
2020-09-18 17:43:00 +0200

    Embeddable telemetry and reference extraction/injection (#74352)

--- x-pack/plugins/ui_actions_enhanced/public/dynamic_actions/dynamic_action_manager.test.ts
+++ x-pack/plugins/ui_actions_enhanced/public/dynamic_actions/dynamic_action_manager.test.ts
@@ -250,7 +250,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition1);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -277,7 +277,7 @@ describe('DynamicActionManager', () => {
 
       test('adds event to UI state', async () => {
         const { manager, uiActions } = setup([]);
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -296,7 +296,7 @@ describe('DynamicActionManager', () => {
 
       test('optimistically adds event to UI state', async () => {
         const { manager, uiActions } = setup([]);
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -319,7 +319,7 @@ describe('DynamicActionManager', () => {
 
       test('instantiates event in actions service', async () => {
         const { manager, uiActions, actions } = setup([]);
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -348,7 +348,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition1);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -361,7 +361,7 @@ describe('DynamicActionManager', () => {
 
       test('does not add even to UI state', async () => {
         const { manager, storage, uiActions } = setup([]);
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -380,7 +380,7 @@ describe('DynamicActionManager', () => {
 
       test('optimistically adds event to UI state and then removes it', async () => {
         const { manager, storage, uiActions } = setup([]);
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -406,7 +406,7 @@ describe('DynamicActionManager', () => {
 
       test('does not instantiate event in actions service', async () => {
         const { manager, storage, uiActions, actions } = setup([]);
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -432,7 +432,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition1);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition1.id,
           name: 'foo',
           config: {},
@@ -457,7 +457,7 @@ describe('DynamicActionManager', () => {
 
         expect(registeredAction1.getDisplayName()).toBe('Action 3');
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition2.id,
           name: 'foo',
           config: {},
@@ -479,7 +479,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition2);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition2.id,
           name: 'foo',
           config: {},
@@ -505,7 +505,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition2);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition2.id,
           name: 'foo',
           config: {},
@@ -524,7 +524,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition2);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition2.id,
           name: 'foo',
           config: {},
@@ -552,7 +552,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition2);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition2.id,
           name: 'foo',
           config: {},
@@ -580,7 +580,7 @@ describe('DynamicActionManager', () => {
 
         expect(registeredAction1.getDisplayName()).toBe('Action 3');
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition2.id,
           name: 'foo',
           config: {},
@@ -604,7 +604,7 @@ describe('DynamicActionManager', () => {
         uiActions.registerActionFactory(actionFactoryDefinition2);
         await manager.start();
 
-        const action: SerializedAction<unknown> = {
+        const action: SerializedAction = {
           factoryId: actionFactoryDefinition2.id,
           name: 'foo',
           config: {},
