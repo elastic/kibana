c47f2c0cf1fa3121c2b693690c2b0bba6547872f
Ryland Herrick <ryalnd@gmail.com>
2020-09-18 13:56:49 -0500

    Add tests for ErrorPopover

--- x-pack/plugins/security_solution/public/detections/components/rules/eql_query_bar/errors_popover.tsx
+++ x-pack/plugins/security_solution/public/detections/components/rules/eql_query_bar/errors_popover.tsx
@@ -30,6 +30,7 @@ export const ErrorsPopover: FC<ErrorsPopoverProps> = ({ ariaLabel, errors }) => 
       data-test-subj="eql-validation-errors-popover"
       button={
         <EuiButtonEmpty
+          data-test-subj="eql-validation-errors-popover-button"
           iconType="crossInACircleFilled"
           size="s"
           color="danger"
@@ -43,12 +44,12 @@ export const ErrorsPopover: FC<ErrorsPopoverProps> = ({ ariaLabel, errors }) => 
       closePopover={handleClose}
       anchorPosition="downCenter"
     >
-      <>
+      <div data-test-subj="eql-validation-errors-popover-content">
         <EuiPopoverTitle>{i18n.EQL_VALIDATION_ERRORS_TITLE}</EuiPopoverTitle>
         {errors.map((message, idx) => (
           <EuiText key={idx}>{message}</EuiText>
         ))}
-      </>
+      </div>
     </EuiPopover>
   );
 };
