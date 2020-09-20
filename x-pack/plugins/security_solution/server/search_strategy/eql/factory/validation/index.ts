5c46444c13906b4cd7a21678ae1668bd79255d45
Ryland Herrick <ryalnd@gmail.com>
2020-09-20 13:35:31 -0500

    Remove potential type assertions
    I don't know what types should be coming out of these functions, so I'm
    going to leave them as they are for now.
    
    As they stand, the format is EQL specific, which conflicts with
    ISearchStrategy.

--- x-pack/plugins/security_solution/server/search_strategy/eql/factory/validation/index.ts
+++ x-pack/plugins/security_solution/server/search_strategy/eql/factory/validation/index.ts
@@ -4,31 +4,26 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { SearchResponse } from 'elasticsearch';
-import { IEsSearchRequest, ISearchOptions } from '../../../../../../../../src/plugins/data/common';
-
 import { EqlQueryTypes } from '../../../../../common/search_strategy/eql';
 import { EqlQueryFactory } from '../types';
 import { getValidationErrors, isValidationErrorResponse } from './helpers';
 
 export const eqlValidationQuery: EqlQueryFactory<EqlQueryTypes.validation> = {
-  buildRequest: (request) =>
-    ({
-      allow_no_indices: true,
-      index: request.index.join(),
-      body: {
-        query: request.query,
-      },
-    } as IEsSearchRequest),
-  buildOptions: (request) =>
-    ({
-      ignore: [400],
-    } as ISearchOptions),
+  buildRequest: (request) => ({
+    allow_no_indices: true,
+    index: request.index.join(),
+    body: {
+      query: request.query,
+    },
+  }),
+  buildOptions: (request) => ({
+    ignore: [400],
+  }),
   parse: async (request, response) => {
     const errors = isValidationErrorResponse(response) ? getValidationErrors(response) : [];
 
     return {
-      rawResponse: response.body as SearchResponse<unknown>,
+      rawResponse: response.body,
       errors,
       valid: errors.length === 0,
     };
