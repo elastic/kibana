90e144583d445c7f6eab4d384c0f0b345d02779f
CJ Cenizal <cj@cenizal.com>
2020-09-21 15:36:06 -0700

    Refine callout appearance in term vector and alias parameters.

--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/painless_script_parameter.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/field_parameters/painless_script_parameter.tsx
@@ -21,7 +21,7 @@ export const PainlessScriptParameter = () => {
           <>
             {isInvalid && (
               <>
-                <EuiCallOut color="danger" title={error} />
+                <EuiCallOut size="s" color="danger" title={error} />
                 <EuiSpacer />
               </>
             )}
