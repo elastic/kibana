3103f905025fe6d51324354cb0889adff70b073c
neptunian <sandra.gonzales@elastic.co>
2020-09-21 17:26:57 -0400

    update integration test

--- x-pack/test/ingest_manager_api_integration/apis/fleet/agents/upgrade.ts
+++ x-pack/test/ingest_manager_api_integration/apis/fleet/agents/upgrade.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import expect from '@kbn/expect/expect.js';
 import { FtrProviderContext } from '../../../../api_integration/ftr_provider_context';
 import { setupIngest } from './services';
 import { skipIfNoDockerRegistry } from '../../../helpers';
@@ -24,7 +25,7 @@ export default function (providerContext: FtrProviderContext) {
       await esArchiver.unload('fleet/agents');
     });
 
-    it('should respond 200 to upgrade agent with valid parameters', async () => {
+    it('should respond 200 to upgrade agent and update the agent SO', async () => {
       const kibanaVersionAccessor = kibanaServer.version;
       const kibanaVersion = await kibanaVersionAccessor.get();
       await supertest
@@ -35,6 +36,11 @@ export default function (providerContext: FtrProviderContext) {
           source_uri: 'http://path/to/download',
         })
         .expect(200);
+      const res = await kibanaServer.savedObjects.get({
+        type: 'fleet-agents',
+        id: 'agent1',
+      });
+      expect(res.attributes.upgrade_started_at).to.be.ok();
     });
 
     it('should respond 400 if trying to upgrade to a version that does not match installed kibana version', async () => {
