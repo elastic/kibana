dd2caff346695651f1d3f3a3b0aab1977abeb18f
Matt Kime <matt@mattki.me>
2020-09-21 14:51:06 -0500

    fix file import

--- x-pack/plugins/ml/public/application/datavisualizer/file_based/components/import_view/import_view.js
+++ x-pack/plugins/ml/public/application/datavisualizer/file_based/components/import_view/import_view.js
@@ -215,7 +215,7 @@ export class ImportView extends Component {
                 // mappings, use this field as the time field.
                 // This relies on the field being populated by
                 // the ingest pipeline on ingest
-                if (mappings[DEFAULT_TIME_FIELD] !== undefined) {
+                if (mappings.properties[DEFAULT_TIME_FIELD] !== undefined) {
                   timeFieldName = DEFAULT_TIME_FIELD;
                   this.setState({ timeFieldName });
                 }
