9be734dca3f34ac0b673fd161a95aff679e29785
Quynh Nguyen <quynh.nguyen@elastic.co>
2020-09-22 14:29:48 -0500

    [ML] Update useTimeSeriesExplorerHref

--- x-pack/plugins/apm/public/components/shared/Links/MachineLearningLinks/useTimeSeriesExplorerHref.ts
+++ x-pack/plugins/apm/public/components/shared/Links/MachineLearningLinks/useTimeSeriesExplorerHref.ts
@@ -48,7 +48,7 @@ export function useTimeSeriesExplorerHref({
                     'transaction.type': transactionType,
                   },
                 }
-              : null),
+              : {}),
           },
         });
         if (!isCancelled) {
