1b1335c5ad4115c489d2086a2f07b70864c6a0af
scottybollinger <scotty.bollinger@elastic.co>
2020-09-22 17:53:06 -0500

    Extract ProductSelector to component

---
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search/components/product_selector/product_selector.test.tsx
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import '../../../__mocks__/shallow_usecontext.mock';
+
+import React, { useContext } from 'react';
+import { shallow } from 'enzyme';
+import { EuiPage } from '@elastic/eui';
+
+import '../../../__mocks__/kea.mock';
+import { useValues } from 'kea';
+
+import { ProductSelector } from './';
+import { ErrorConnecting } from '../error_connecting';
+import { ProductCard } from '../product_card';
+
+describe('ProductSelector', () => {
+  it('renders the overview page and product cards', () => {
+    (useValues as jest.Mock).mockReturnValue({ errorConnecting: false });
+    (useContext as jest.Mock).mockImplementationOnce(() => ({ config: { host: 'localhost' } }));
+    const wrapper = shallow(<ProductSelector />);
+
+    expect(wrapper.find(EuiPage).hasClass('enterpriseSearchOverview')).toBe(true);
+    expect(wrapper.find(ProductCard)).toHaveLength(2);
+  });
+});
---
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search/components/product_selector/product_selector.tsx
@@ -0,0 +1,74 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiPageContentBody,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiSpacer,
+  EuiTitle,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+
+import { APP_SEARCH_PLUGIN, WORKPLACE_SEARCH_PLUGIN } from '../../../../../common/constants';
+
+import { SetEnterpriseSearchChrome as SetPageChrome } from '../../../shared/kibana_chrome';
+import { SendEnterpriseSearchTelemetry as SendTelemetry } from '../../../shared/telemetry';
+
+import { ProductCard } from '../product_card';
+
+import AppSearchImage from '../../assets/app_search.png';
+import WorkplaceSearchImage from '../../assets/workplace_search.png';
+
+export const ProductSelector: React.FC = () => (
+  <EuiPage restrictWidth className="enterpriseSearchOverview">
+    <SetPageChrome isRoot />
+    <SendTelemetry action="viewed" metric="overview" />
+
+    <EuiPageBody>
+      <EuiPageHeader>
+        <EuiPageHeaderSection className="enterpriseSearchOverview__header">
+          <EuiTitle size="l">
+            <h1 className="enterpriseSearchOverview__heading">
+              {i18n.translate('xpack.enterpriseSearch.overview.heading', {
+                defaultMessage: 'Welcome to Elastic Enterprise Search',
+              })}
+            </h1>
+          </EuiTitle>
+          <EuiTitle size="s">
+            <p className="enterpriseSearchOverview__subheading">
+              {i18n.translate('xpack.enterpriseSearch.overview.subheading', {
+                defaultMessage: 'Select a product to get started',
+              })}
+            </p>
+          </EuiTitle>
+        </EuiPageHeaderSection>
+      </EuiPageHeader>
+      <EuiPageContentBody>
+        <EuiFlexGroup justifyContent="center" gutterSize="xl">
+          <EuiFlexItem grow={false} className="enterpriseSearchOverview__card">
+            <ProductCard product={APP_SEARCH_PLUGIN} image={AppSearchImage} />
+          </EuiFlexItem>
+          <EuiFlexItem grow={false} className="enterpriseSearchOverview__card">
+            <ProductCard product={WORKPLACE_SEARCH_PLUGIN} image={WorkplaceSearchImage} />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiSpacer />
+      </EuiPageContentBody>
+    </EuiPageBody>
+  </EuiPage>
+);
