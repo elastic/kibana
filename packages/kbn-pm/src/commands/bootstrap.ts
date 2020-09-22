e0e4bd3c70c5fc14420ddd5092d93119898536b5
spalger <spalger@users.noreply.github.com>
2020-09-21 22:13:54 -0700

    [kbn/pm/bootstrap] analyze yarn.lock to unexpected lodash versions

--- packages/kbn-pm/src/commands/bootstrap.ts
+++ packages/kbn-pm/src/commands/bootstrap.ts
@@ -17,6 +17,10 @@
  * under the License.
  */
 
+// @ts-expect-error published types are worthless
+import { stringify as stringifyLockfile } from '@yarnpkg/lockfile';
+import dedent from 'dedent';
+
 import { linkProjectExecutables } from '../utils/link_project_executables';
 import { log } from '../utils/log';
 import { parallelizeBatches } from '../utils/parallelize';
@@ -25,6 +29,9 @@ import { Project } from '../utils/project';
 import { ICommand } from './';
 import { getAllChecksums } from '../utils/project_checksums';
 import { BootstrapCacheFile } from '../utils/bootstrap_cache_file';
+import { readYarnLock, YarnLock } from '../utils/yarn_lock';
+import { Kibana } from '../utils/kibana';
+import { writeFile } from '../utils/fs';
 
 export const BootstrapCommand: ICommand = {
   description: 'Install dependencies and crosslink projects',
@@ -54,6 +61,12 @@ export const BootstrapCommand: ICommand = {
       }
     }
 
+    const yarnLock = await readYarnLock(kbn);
+
+    await assertSingleLodashV4(kbn, yarnLock);
+    await assertNoProductionLodash3(kbn, yarnLock);
+    log.success('yarn.lock analysis completed without any issues');
+
     await linkProjectExecutables(projects, projectGraph);
 
     /**
@@ -63,7 +76,7 @@ export const BootstrapCommand: ICommand = {
      * have to, as it will slow down the bootstrapping process.
      */
 
-    const checksums = await getAllChecksums(kbn, log);
+    const checksums = await getAllChecksums(kbn, log, yarnLock);
     const caches = new Map<Project, { file: BootstrapCacheFile; valid: boolean }>();
     let cachedProjectCount = 0;
 
@@ -97,3 +110,72 @@ export const BootstrapCommand: ICommand = {
     });
   },
 };
+
+async function assertSingleLodashV4(kbn: Kibana, yarnLock: YarnLock) {
+  const lodash4Versions = new Set<string>();
+  const lodash4Reqs = new Set<string>();
+  for (const [req, dep] of Object.entries(yarnLock)) {
+    const chunks = req.split('@');
+    if (chunks[0] === 'lodash' && dep.version.startsWith('4.')) {
+      lodash4Reqs.add(req);
+      lodash4Versions.add(dep.version);
+    }
+  }
+
+  if (lodash4Versions.size === 1) {
+    return;
+  }
+
+  for (const req of lodash4Reqs) {
+    delete yarnLock[req];
+  }
+
+  await writeFile(kbn.getAbsolute('yarn.lock'), stringifyLockfile(yarnLock), 'utf8');
+
+  log.error(dedent`
+
+    Multiple version of lodash v4 were detected, so they have been removed
+    from the yarn.lock file. Please rerun yarn kbn bootstrap to coalese the
+    lodash versions installed.
+
+    If you still see this error when you re-bootstrap then you might need
+    to force a new dependency to use the latest version of lodash via the
+    "resolutions" field in package.json.
+
+    If you have questions about this please reach out to the operations team.
+
+  `);
+
+  process.exit(1);
+}
+
+async function assertNoProductionLodash3(kbn: Kibana, yarnLock: YarnLock) {
+  const prodDependencies = kbn.resolveAllProductionDependencies(yarnLock, log);
+
+  const lodash3Versions = new Set<string>();
+  for (const dep of prodDependencies.values()) {
+    if (dep.name === 'lodash' && dep.version.startsWith('3.')) {
+      lodash3Versions.add(dep.version);
+    }
+  }
+
+  if (!lodash3Versions.size) {
+    return;
+  }
+
+  log.error(dedent`
+
+    Due to changes in the yarn.lock file and/or package.json files a version of
+    lodash 3 is now included in the production dependencies. To reduce the size of
+    our distributable and especially our front-end bundles we have decided to
+    prevent adding any new instances of lodash 3.
+
+    Please inspect the changes to yarn.lock or package.json files to identify where
+    the lodash 3 version is coming from and remove it.
+
+    If you have questions about this please reack out to the operations team.
+
+  `);
+
+  process.exit(1);
+}
