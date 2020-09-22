a885904026947489b8266f2f8dcb06fe0aebfe54
Jen Huang <its.jenetic@gmail.com>
2020-09-22 15:05:25 -0700

    Fix export

--- x-pack/plugins/ingest_manager/server/services/index.ts
+++ x-pack/plugins/ingest_manager/server/services/index.ts
@@ -9,6 +9,8 @@ import { AgentStatus, Agent, EsAssetReference } from '../types';
 import * as settingsService from './settings';
 import { getAgent, listAgents } from './agents';
 
+export { ESIndexPatternSavedObjectService } from './es_index_pattern';	
+
 export { getRegistryUrl } from './epm/registry/registry_url';
 
 /**
