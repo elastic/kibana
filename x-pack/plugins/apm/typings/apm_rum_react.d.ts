d62889238778270db2c894815460893816e1225d
Nathan L Smith <nathan.smith@elastic.co>
2020-09-22 15:41:28 -0500

    Update to latest rum-react (#78193)
    * Update to latest rum-react
    
    The latest version fixes a problem where you would get a bunch of warnings that you couldn't turn off if you used `render` instead of component with a route. This was causing us to use `component` in some places where `render` should be used.
    
    The latest version fixes this problem so we change back to `render`
    where appropriate.
    
    Also make our `ApmRoute` a `Route` instead of `any`.
    # Conflicts:
    #	x-pack/package.json
    #	x-pack/plugins/apm/typings/apm_rum_react.d.ts
    #	yarn.lock

--- x-pack/plugins/apm/typings/apm_rum_react.d.ts
+++ x-pack/plugins/apm/typings/apm_rum_react.d.ts
@@ -3,7 +3,10 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+
 declare module '@elastic/apm-rum-react' {
+  import { RouteProps } from 'react-router-dom';
+
   export function indexManagementStore(): any;
-  export const ApmRoute: any;
+  export const ApmRoute: React.ComponentClass<RouteProps>;
 }
