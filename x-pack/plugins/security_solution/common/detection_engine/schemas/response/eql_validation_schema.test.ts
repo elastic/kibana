2a97944b528da9b597c0b3ca51e60ac810b65c3f
Ryland Herrick <ryalnd@gmail.com>
2020-09-22 15:07:07 -0500

    Remove duplicated header

--- x-pack/plugins/security_solution/common/detection_engine/schemas/response/eql_validation_schema.test.ts
+++ x-pack/plugins/security_solution/common/detection_engine/schemas/response/eql_validation_schema.test.ts
@@ -3,11 +3,6 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
 
 import { pipe } from 'fp-ts/lib/pipeable';
 import { left } from 'fp-ts/lib/Either';
