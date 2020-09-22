fb3d159a92eae460843b5a78d0d232aa5a259133
Jason Stoltzfus <jastoltz24@gmail.com>
2020-09-22 12:51:52 -0400

    Accepted suggestions for constants file
    Mainly edits for i18n

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/credentials/constants.ts
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/credentials/constants.ts
@@ -3,21 +3,35 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import { i18n } from '@kbn/i18n';
 
 export const ADMIN = 'admin';
 export const PRIVATE = 'private';
 export const SEARCH = 'search';
 
 export const TOKEN_TYPE_DESCRIPTION = {
-  [SEARCH]: 'Public Search Keys are used for search endpoints only.',
-  [PRIVATE]: 'Private API Keys are used for read and/or write access on one or more Engines.',
-  [ADMIN]: 'Private Admin Keys are used to interact with the Credentials API.',
+  [SEARCH]: i18n.translate('xpack.enterpriseSearch.appSearch.tokens.search.description', {
+    defaultMessage: 'Public Search Keys are used for search endpoints only.',
+  }),
+  [PRIVATE]: i18n.translate('xpack.enterpriseSearch.appSearch.tokens.private.description', {
+    defaultMessage:
+      'Private API Keys are used for read and/or write access on one or more Engines.',
+  }),
+  [ADMIN]: i18n.translate('xpack.enterpriseSearch.appSearch.tokens.admin.description', {
+    defaultMessage: 'Private Admin Keys are used to interact with the Credentials API.',
+  }),
 };
 
 export const TOKEN_TYPE_DISPLAY_NAMES = {
-  [SEARCH]: 'Public Search Key',
-  [PRIVATE]: 'Private API Key',
-  [ADMIN]: 'Private Admin Key',
+  [SEARCH]: i18n.translate('xpack.enterpriseSearch.appSearch.tokens.search.name', {
+    defaultMessage: 'Public Search Key',
+  }),
+  [PRIVATE]: i18n.translate('xpack.enterpriseSearch.appSearch.tokens.private.name', {
+    defaultMessage: 'Private API Key',
+  }),
+  [ADMIN]: i18n.translate('xpack.enterpriseSearch.appSearch.tokens.admin.name', {
+    defaultMessage: 'Private Admin Key',
+  }),
 };
 
 export const TOKEN_TYPE_INFO = [
