f870258ce696b74aff1e264b4566a4fe5f466d83
streamich <streamich@gmail.com>
2020-09-23 12:16:55 +0000

    refactor: 💡 move no-embeddable example into a seprate file

--- x-pack/examples/ui_actions_enhanced_examples/public/containers/drilldowns_manager/drilldowns_manager.tsx
+++ x-pack/examples/ui_actions_enhanced_examples/public/containers/drilldowns_manager/drilldowns_manager.tsx
@@ -5,20 +5,9 @@
  */
 
 import React from 'react';
-import {
-  EuiText,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiButton,
-  EuiSpacer,
-  EuiFlyout,
-  EuiPopover,
-  EuiContextMenu,
-  EuiContextMenuPanelDescriptor,
-} from '@elastic/eui';
 import { Section } from '../../components/section/section';
-import { useUiActions } from '../../context';
-import { SAMPLE_ML_JOB_CLICK_TRIGGER, SampleMlJob, SampleMlJobClickContext } from '../../triggers';
+import { SampleMlJob, SampleMlJobClickContext } from '../../triggers';
+import { DrilldownsNoEmbeddableExample } from '../drilldowns_no_embeddable_example';
 
 export const job: SampleMlJob = {
   job_id: '123',
@@ -29,103 +18,11 @@ export const job: SampleMlJob = {
 export const context: SampleMlJobClickContext = { job };
 
 export const DrilldownsManager: React.FC = () => {
-  const { plugins, manager } = useUiActions();
-  const [showManager, setShowManager] = React.useState(false);
-  const [openPopup, setOpenPopup] = React.useState(false);
-  const viewRef = React.useRef<'create' | 'manage'>('create');
-
-  const panels: EuiContextMenuPanelDescriptor[] = [
-    {
-      id: 0,
-      items: [
-        {
-          name: 'Create new view',
-          icon: 'plusInCircle',
-          onClick: () => {
-            setOpenPopup(false);
-            viewRef.current = 'create';
-            setShowManager((x) => !x);
-          },
-        },
-        {
-          name: 'Drilldown list view',
-          icon: 'list',
-          onClick: () => {
-            setOpenPopup(false);
-            viewRef.current = 'manage';
-            setShowManager((x) => !x);
-          },
-        },
-      ],
-    },
-  ];
-
-  const openManagerButton = showManager ? (
-    <EuiButton onClick={() => setShowManager(false)}>Close</EuiButton>
-  ) : (
-    <EuiPopover
-      id="contextMenuExample"
-      button={
-        <EuiButton
-          fill={!showManager}
-          iconType="arrowDown"
-          iconSide="right"
-          onClick={() => setOpenPopup((x) => !x)}
-        >
-          Open Drilldown Manager
-        </EuiButton>
-      }
-      isOpen={openPopup}
-      closePopover={() => setOpenPopup(false)}
-      panelPaddingSize="none"
-      withTitle
-      anchorPosition="downLeft"
-    >
-      <EuiContextMenu initialPanelId={0} panels={panels} />
-    </EuiPopover>
-  );
-
   return (
     <div>
       <Section title={'Drilldowns Manager'}>
-        <EuiText>
-          <p>
-            <em>Drilldown Manager</em> can be integrated into any app in Kibana. Click the button
-            below to open the drilldown manager and see how it works in this example plugin.
-          </p>
-        </EuiText>
-        <EuiSpacer />
-        <EuiFlexGroup>
-          <EuiFlexItem grow={false}>{openManagerButton}</EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            <EuiButton
-              color="secondary"
-              fill
-              iconType="play"
-              iconSide="left"
-              onClick={() =>
-                plugins.uiActionsEnhanced.executeTriggerActions(
-                  SAMPLE_ML_JOB_CLICK_TRIGGER,
-                  context
-                )
-              }
-            >
-              Execute click action
-            </EuiButton>
-          </EuiFlexItem>
-        </EuiFlexGroup>
+        <DrilldownsNoEmbeddableExample />
       </Section>
-
-      {showManager && (
-        <EuiFlyout onClose={() => setShowManager(false)} aria-labelledby="Drilldown Manager">
-          <plugins.uiActionsEnhanced.FlyoutManageDrilldowns
-            onClose={() => setShowManager(false)}
-            viewMode={viewRef.current}
-            dynamicActionManager={manager}
-            triggers={[SAMPLE_ML_JOB_CLICK_TRIGGER]}
-          />
-        </EuiFlyout>
-      )}
     </div>
   );
 };
