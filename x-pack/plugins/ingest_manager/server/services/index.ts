7d852d1717c7e6aead7b53658867bbe0e636d9cc
Jen Huang <its.jenetic@gmail.com>
2020-09-23 09:23:25 -0700

    Fix spacing

--- x-pack/plugins/ingest_manager/server/services/index.ts
+++ x-pack/plugins/ingest_manager/server/services/index.ts
@@ -9,7 +9,7 @@ import { AgentStatus, Agent, EsAssetReference } from '../types';
 import * as settingsService from './settings';
 import { getAgent, listAgents } from './agents';
 
-export { ESIndexPatternSavedObjectService } from './es_index_pattern';	
+export { ESIndexPatternSavedObjectService } from './es_index_pattern';
 
 export { getRegistryUrl } from './epm/registry/registry_url';
 
