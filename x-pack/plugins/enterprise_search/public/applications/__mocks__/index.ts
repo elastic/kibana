1f6f4e425014a9cf4fe33619914ca97dc4b9aaeb
Constance Chen <constance.chen.3@gmail.com>
2020-09-23 01:37:32 -0700

    :fire: Delete KibanaContext and mockKibanaContext
    + minor newline linting/grouping tweaks

--- x-pack/plugins/enterprise_search/public/applications/__mocks__/index.ts
+++ x-pack/plugins/enterprise_search/public/applications/__mocks__/index.ts
@@ -5,7 +5,6 @@
  */
 
 export { mockHistory, mockLocation } from './react_router_history.mock';
-export { mockKibanaContext } from './kibana_context.mock';
 export { mockKibanaValues } from './kibana_logic.mock';
 export { mockLicensingValues } from './licensing_logic.mock';
 export { mockHttpValues } from './http_logic.mock';
--- x-pack/plugins/enterprise_search/public/applications/index.tsx
+++ x-pack/plugins/enterprise_search/public/applications/index.tsx
@@ -7,30 +7,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 import { Store } from 'redux';
 import { getContext, resetContext } from 'kea';
-
 import { I18nProvider } from '@kbn/i18n/react';
-import { AppMountParameters, CoreStart, ApplicationStart, ChromeBreadcrumb } from 'src/core/public';
+
+import { AppMountParameters, CoreStart } from 'src/core/public';
 import { PluginsStart, ClientConfigType, ClientData } from '../plugin';
+import { IInitialAppData } from '../../common/types';
 
 import { mountKibanaLogic } from './shared/kibana';
 import { mountLicensingLogic } from './shared/licensing';
 import { mountHttpLogic } from './shared/http';
 import { mountFlashMessagesLogic } from './shared/flash_messages';
 import { externalUrl } from './shared/enterprise_search_url';
-import { IInitialAppData } from '../../common/types';
-
-export interface IKibanaContext {
-  config: { host?: string };
-  navigateToUrl: ApplicationStart['navigateToUrl'];
-  setBreadcrumbs(crumbs: ChromeBreadcrumb[]): void;
-  setDocTitle(title: string): void;
-}
-
-export const KibanaContext = React.createContext({});
 
 /**
  * This file serves as a reusable wrapper to share Kibana-level context and other helpers
@@ -55,35 +45,25 @@ export const renderApp = (
     setBreadcrumbs: core.chrome.setBreadcrumbs,
     setDocTitle: core.chrome.docTitle.change,
   });
-
   const unmountLicensingLogic = mountLicensingLogic({
     license$: plugins.licensing.license$,
   });
-
   const unmountHttpLogic = mountHttpLogic({
     http: core.http,
     errorConnecting,
     readOnlyMode: initialData.readOnlyMode,
   });
-
-  const unmountFlashMessagesLogic = mountFlashMessagesLogic({ history: params.history });
+  const unmountFlashMessagesLogic = mountFlashMessagesLogic({
+    history: params.history,
+  });
 
   ReactDOM.render(
     <I18nProvider>
-      <KibanaContext.Provider
-        value={{
-          config,
-          navigateToUrl: core.application.navigateToUrl,
-          setBreadcrumbs: core.chrome.setBreadcrumbs,
-          setDocTitle: core.chrome.docTitle.change,
-        }}
-      >
-        <Provider store={store}>
-          <Router history={params.history}>
-            <App {...initialData} />
-          </Router>
-        </Provider>
-      </KibanaContext.Provider>
+      <Provider store={store}>
+        <Router history={params.history}>
+          <App {...initialData} />
+        </Router>
+      </Provider>
     </I18nProvider>,
     params.element
   );
