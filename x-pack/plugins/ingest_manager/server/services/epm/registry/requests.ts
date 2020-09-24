f8e94dad33e15cdf0af981946eee41a92cd34c8d
John Schulz <john.schulz@elastic.co>
2020-09-24 15:30:45 -0400

    Add functions to get fetch options & proxy agent

--- x-pack/plugins/ingest_manager/server/services/epm/registry/requests.ts
+++ x-pack/plugins/ingest_manager/server/services/epm/registry/requests.ts
@@ -4,17 +4,21 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import fetch, { FetchError, Response } from 'node-fetch';
+import fetch, { FetchError, Response, RequestInit } from 'node-fetch';
 import pRetry from 'p-retry';
+import HttpProxyAgent from 'http-proxy-agent';
+import type { HttpsProxyAgent as IHttpsProxyAgent } from 'https-proxy-agent';
+import HttpsProxyAgent from 'https-proxy-agent';
+import { getProxyForUrl as getProxyFromEnvForUrl } from 'proxy-from-env';
 import { streamToString } from './streams';
+import { appContextService } from '../../app_context';
 import { RegistryError, RegistryConnectionError, RegistryResponseError } from '../../../errors';
 
 type FailedAttemptErrors = pRetry.FailedAttemptError | FetchError | Error;
 
 // not sure what to call this function, but we're not exporting it
 async function registryFetch(url: string) {
-  const response = await fetch(url);
-
+  const response = await fetch(url, getFetchOptions(url));
   if (response.ok) {
     return response;
   } else {
@@ -80,4 +84,45 @@ function isFetchError(error: FailedAttemptErrors): error is FetchError {
 
 function isSystemError(error: FailedAttemptErrors): boolean {
   return isFetchError(error) && error.type === 'system';
+}
+
+type ProxyAgent = IHttpsProxyAgent | HttpProxyAgent;
+export function getFetchOptions(url: string): RequestInit | {} {
+  // start with env values and can add from flags, defaults, etc later
+  const proxyUrl = getProxyFromEnvForUrl(url);
+  if (!proxyUrl) {
+    return {};
+  }
+
+  const logger = appContextService.getLogger();
+  logger.info(`Using proxy ${proxyUrl} from environment variable`);
+
+  return {
+    agent: getProxyAgent(proxyUrl, url),
+  };
+}
+
+export function getProxyAgent(proxyUrl: string, endpointUrl: string): ProxyAgent {
+  const endpointParsed = new URL(endpointUrl);
+  const proxyParsed = new URL(proxyUrl);
+
+  const agentOptions = {
+    host: proxyParsed.hostname,
+    port: Number(proxyParsed.port),
+    protocol: proxyParsed.protocol,
+    // the proxied URL's host is put in the header instead of the server's actual host
+    // headers: proxySettings.proxyHeaders,
+    headers: {
+      Host: endpointParsed.host,
+    },
+    // rejectUnauthorized: proxySettings.proxyRejectUnauthorizedCertificates,
+  };
+
+  const isHttps = endpointParsed.protocol === 'https:';
+  const agent: ProxyAgent = isHttps
+    ? // @ts-expect-error ts(7009) HttpsProxyAgent isn't a class so TS complains about using `new`
+      new HttpsProxyAgent(agentOptions)
+    : new HttpProxyAgent(proxyUrl);
+
+  return agent;
 }
