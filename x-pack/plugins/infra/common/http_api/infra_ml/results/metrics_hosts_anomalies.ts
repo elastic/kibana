a3ede09d542b6c3aabe9d53a0db04b78dbe790d9
Phillip Burch <phillip.burch@live.com>
2020-09-24 11:58:17 -0500

    [Metrics UI] Anomaly Detection setup flow for Metrics (#76787) (#78432)
    * adds metrics ml integration
    
    * Add ability to create ml jobs from inventory
    
    * Fix i18n stuff
    
    * Fix typecheck
    
    * renames jobs, updates datafeeds
    
    * adds allow_no_indices: true for datafeeds
    
    * Revert "[Metrics UI] Replace Snapshot API with Metrics API (#76253)"
    
    This reverts commit 0ca647286a5ccabfb76203b8cbcb1d13b05f105d.
    
    * Add ability to fetch anomalies
    
    * Fix typecheck
    
    * Fix typecheck
    
    * Fix i18n
    
    * Fix lint, use the right partition field
    
    * Delete log files
    
    * Fix merge
    
    * Fix merge issues
    
    * Update name of jobs
    
    * Remove CPU job
    
    * [Metrics UI] Replace Snapshot API with Metrics API (#76253)
    
    - Remove server/lib/snapshot
    - Replace backend for /api/infra/snapshot with data from Metrics API
    - Fixing tests with updates to the snapshot node
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    * Add links back to ML for anomalies and manage jobs
    
    * Fix typecheck
    
    * Remove unecessary validation
    
    Co-authored-by: Michael Hirsch <michaelahirsch@gmail.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Chris Cowan <chris@chriscowan.us>
    
    Co-authored-by: Michael Hirsch <michaelahirsch@gmail.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Chris Cowan <chris@chriscowan.us>

---
+++ x-pack/plugins/infra/common/http_api/infra_ml/results/metrics_hosts_anomalies.ts
@@ -0,0 +1,79 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import * as rt from 'io-ts';
+
+import { timeRangeRT, routeTimingMetadataRT } from '../../shared';
+import { anomalyTypeRT, paginationCursorRT, sortRT, paginationRT } from './common';
+
+export const INFA_ML_GET_METRICS_HOSTS_ANOMALIES_PATH =
+  '/api/infra/infra_ml/results/metrics_hosts_anomalies';
+
+const metricsHostAnomalyCommonFieldsRT = rt.type({
+  id: rt.string,
+  anomalyScore: rt.number,
+  typical: rt.number,
+  actual: rt.number,
+  type: anomalyTypeRT,
+  duration: rt.number,
+  startTime: rt.number,
+  jobId: rt.string,
+});
+const metricsHostsAnomalyRT = metricsHostAnomalyCommonFieldsRT;
+
+export type MetricsHostsAnomaly = rt.TypeOf<typeof metricsHostsAnomalyRT>;
+
+export const getMetricsHostsAnomaliesSuccessReponsePayloadRT = rt.intersection([
+  rt.type({
+    data: rt.intersection([
+      rt.type({
+        anomalies: rt.array(metricsHostsAnomalyRT),
+        // Signifies there are more entries backwards or forwards. If this was a request
+        // for a previous page, there are more previous pages, if this was a request for a next page,
+        // there are more next pages.
+        hasMoreEntries: rt.boolean,
+      }),
+      rt.partial({
+        paginationCursors: rt.type({
+          // The cursor to use to fetch the previous page
+          previousPageCursor: paginationCursorRT,
+          // The cursor to use to fetch the next page
+          nextPageCursor: paginationCursorRT,
+        }),
+      }),
+    ]),
+  }),
+  rt.partial({
+    timing: routeTimingMetadataRT,
+  }),
+]);
+
+export type GetMetricsHostsAnomaliesSuccessResponsePayload = rt.TypeOf<
+  typeof getMetricsHostsAnomaliesSuccessReponsePayloadRT
+>;
+
+export const getMetricsHostsAnomaliesRequestPayloadRT = rt.type({
+  data: rt.intersection([
+    rt.type({
+      // the ID of the source configuration
+      sourceId: rt.string,
+      // the time range to fetch the log entry anomalies from
+      timeRange: timeRangeRT,
+    }),
+    rt.partial({
+      // Pagination properties
+      pagination: paginationRT,
+      // Sort properties
+      sort: sortRT,
+      // // Dataset filters
+      // datasets: rt.array(rt.string),
+    }),
+  ]),
+});
+
+export type GetMetricsHostsAnomaliesRequestPayload = rt.TypeOf<
+  typeof getMetricsHostsAnomaliesRequestPayloadRT
+>;
