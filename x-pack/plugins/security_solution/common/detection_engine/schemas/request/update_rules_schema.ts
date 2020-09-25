eb06b8cc5e0891384d26f1105d31e84e02cdc351
Marshall Main <55718608+marshallmain@users.noreply.github.com>
2020-09-25 18:04:48 -0400

    [Detections][EQL] EQL rule execution in detection engine (#77419) (#78550)
    * First draft of EQL rules in detection engine
    
    * Reorganize functions to separate files
    
    * Start adding eventCategoryOverride option for EQL rules
    
    * Add building block alerts for each event within sequence
    
    * Use eql instead of eql_query for rule type
    
    * Remove unused imports
    
    * Fix tests
    
    * Add basic tests for buildEqlSearchRequest
    
    * Add rulesSchema tests for eql
    
    * Add buildSignalFromSequence test
    
    * Add threat rule fields to buildRuleWithoutOverrides
    
    * Fix buildSignalFromSequence typecheck error
    
    * Add more tests
    
    * Add tests for wrapBuildingBlock and generateSignalId
    
    * Use isEqlRule function and fix import error
    
    * delete frank
    
    * Move sequence interface to types.ts
    
    * Fix import
    
    * Remove EQL execution placeholder, add back language to eql rule type
    
    * allow no indices for eql search
    
    * Fix unit tests for language update
    
    * Fix buildEqlSearchRequest tests
    
    * Replace signal.child with signal.group
    
    * remove unused import
    
    * Move sequence signal group building to separate testable function
    
    * Unbork the merge conflict resolution
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/common/detection_engine/schemas/request/update_rules_schema.ts
+++ x-pack/plugins/security_solution/common/detection_engine/schemas/request/update_rules_schema.ts
@@ -47,6 +47,7 @@ import {
   Author,
   RiskScoreMapping,
   SeverityMapping,
+  event_category_override,
 } from '../common/schemas';
 
 import {
@@ -90,6 +91,7 @@ export const updateRulesSchema = t.intersection([
       author: DefaultStringArray, // defaults to empty array of strings if not set during decode
       building_block_type, // defaults to undefined if not set during decode
       enabled: DefaultBooleanTrue, // defaults to true if not set during decode
+      event_category_override,
       false_positives: DefaultStringArray, // defaults to empty string array if not set during decode
       filters, // defaults to undefined if not set during decode
       from: DefaultFromString, // defaults to "now-6m" if not set during decode
