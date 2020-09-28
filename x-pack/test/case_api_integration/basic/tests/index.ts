9b3aae4f8c1f52e1c3359bfc91c7b617f7179669
Christos Nasikas <christos.nasikas@elastic.co>
2020-09-28 16:34:55 +0300

    Add end to end migration tests

--- x-pack/test/case_api_integration/basic/tests/index.ts
+++ x-pack/test/case_api_integration/basic/tests/index.ts
@@ -31,5 +31,9 @@ export default ({ loadTestFile }: FtrProviderContext): void => {
     loadTestFile(require.resolve('./configure/get_connectors'));
     loadTestFile(require.resolve('./configure/patch_configure'));
     loadTestFile(require.resolve('./configure/post_configure'));
+
+    // Migrations
+    loadTestFile(require.resolve('./cases/migrations'));
+    loadTestFile(require.resolve('./configure/migrations'));
   });
 };
