f5ca7d82d5486155f998be685687cfff4221532c
Spencer <email@spalger.com>
2020-09-28 13:25:15 -0700

    [kbn/optimizer] only build xpack examples when building xpack plugins (#78656)
    Co-authored-by: spalger <spalger@users.noreply.github.com>

--- x-pack/examples/alerting_example/public/application.tsx
+++ x-pack/examples/alerting_example/public/application.tsx
@@ -1,20 +1,7 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
  */
 
 import React from 'react';
@@ -28,14 +15,14 @@ import {
   DocLinksStart,
   ToastsSetup,
   ApplicationStart,
-} from '../../../src/core/public';
-import { DataPublicPluginStart } from '../../../src/plugins/data/public';
-import { ChartsPluginStart } from '../../../src/plugins/charts/public';
+} from '../../../../src/core/public';
+import { DataPublicPluginStart } from '../../../../src/plugins/data/public';
+import { ChartsPluginStart } from '../../../../src/plugins/charts/public';
 
 import { Page } from './components/page';
 import { DocumentationPage } from './components/documentation';
 import { ViewAlertPage } from './components/view_alert';
-import { TriggersAndActionsUIPublicPluginStart } from '../../../x-pack/plugins/triggers_actions_ui/public';
+import { TriggersAndActionsUIPublicPluginStart } from '../../../plugins/triggers_actions_ui/public';
 import { AlertingExamplePublicStartDeps } from './plugin';
 import { ViewPeopleInSpaceAlertPage } from './components/view_astros_alert';
 
--- x-pack/examples/alerting_example/public/components/view_astros_alert.tsx
+++ x-pack/examples/alerting_example/public/components/view_astros_alert.tsx
@@ -1,21 +1,9 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
  */
+
 import React, { useState, useEffect, Fragment } from 'react';
 
 import {
@@ -34,11 +22,7 @@ import {
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { CoreStart } from 'kibana/public';
 import { isEmpty } from 'lodash';
-import {
-  Alert,
-  AlertTaskState,
-  BASE_ALERT_API_PATH,
-} from '../../../../x-pack/plugins/alerts/common';
+import { Alert, AlertTaskState, BASE_ALERT_API_PATH } from '../../../../plugins/alerts/common';
 import { ALERTING_EXAMPLE_APP_ID } from '../../common/constants';
 
 type Props = RouteComponentProps & {
