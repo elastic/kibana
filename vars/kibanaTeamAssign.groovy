396d4ce0c85ff6b6a2912c9ac0ebbbb3460e3865
Tre' Seymour <wayne.seymour@elastic.co>
2020-09-28 12:49:12 -0600

    CR fixups

--- vars/kibanaTeamAssign.groovy
+++ vars/kibanaTeamAssign.groovy
@@ -1,13 +1,3 @@
-def generateCodeOwners(destination, title) {
-  kibanaPipeline.bash("""
-    source src/dev/ci_setup/setup_env.sh true
-    yarn kbn bootstrap --prefer-offline
-
-    . packages/kbn-dev-utils/src/code_ownership/shell_scripts/generate_code_owners.sh '${destination}'
-    cat '${destination}'
-  """, title)
-}
-
 def generateTeamAssignments(teamAssignmentsPath, title) {
   kibanaPipeline.bash("""
     source src/dev/ci_setup/setup_env.sh
