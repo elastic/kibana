536f65a2beda8ad8a4cad09095e91dc698e2b269
spalger <spalger@users.noreply.github.com>
2020-09-28 19:35:12 -0700

    [canvas] only import templates when necessary

--- x-pack/plugins/canvas/server/templates/index.ts
+++ x-pack/plugins/canvas/server/templates/index.ts
@@ -5,15 +5,19 @@
  */
 
 import { SavedObjectsRepository } from 'src/core/server';
-import { pitch } from './pitch_presentation';
-import { status } from './status_report';
-import { summary } from './summary_report';
-import { dark } from './theme_dark';
-import { light } from './theme_light';
 
 import { TEMPLATE_TYPE } from '../../common/lib/constants';
 
-export const templates = [status, summary, dark, light, pitch];
+// only load templates when requested to reduce require() cost on startup
+export function loadTemplates() {
+  return [
+    require('./pitch_presentation'),
+    require('./status_report'),
+    require('./summary_report'),
+    require('./theme_dark'),
+    require('./theme_light'),
+  ];
+}
 
 export async function initializeTemplates(
   client: Pick<SavedObjectsRepository, 'bulkCreate' | 'create' | 'find'>
@@ -26,7 +30,7 @@ export async function initializeTemplates(
     // So, rather than doing a bulk create of templates, we're going to fire off individual
     // creates and catch and throw-away any errors that happen.
     // Once packages are ready, we should probably move that pitch that is so large to a package
-    for (const template of templates) {
+    for (const template of loadTemplates()) {
       client.create(TEMPLATE_TYPE, template, { id: template.id }).catch((err) => undefined);
     }
   }
