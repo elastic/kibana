ffacd65df492f99d35930f78d44e0910a89a971f
restrry <restrry@gmail.com>
2020-09-29 15:52:52 +0300

    load apm-agent-lazily

--- src/core/public/kbn_bootstrap.ts
+++ src/core/public/kbn_bootstrap.ts
@@ -27,60 +27,35 @@
 
 import { i18n } from '@kbn/i18n';
 import { CoreSystem } from './core_system';
+import { ApmSystem } from './apm_system';
 
 /** @internal */
-export function __kbnBootstrap__() {
+export async function __kbnBootstrap__() {
   const injectedMetadata = JSON.parse(
     document.querySelector('kbn-injected-metadata')!.getAttribute('data')!
   );
 
-  /**
-   * `apmConfig` would be populated with relavant APM RUM agent
-   * configuration if server is started with `ELASTIC_APM_ACTIVE=true`
-   */
-  const apmConfig = injectedMetadata.vars.apmConfig;
-  const APM_ENABLED = process.env.IS_KIBANA_DISTRIBUTABLE !== 'true' && apmConfig != null;
+  const apmSystem = new ApmSystem(injectedMetadata.vars.apmConfig);
+  await apmSystem.setup();
 
-  if (APM_ENABLED) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { init, apm } = require('@elastic/apm-rum');
-    if (apmConfig.globalLabels) {
-      apm.addLabels(apmConfig.globalLabels);
-    }
-    init(apmConfig);
+  let i18nError: Error | undefined;
+  try {
+    await i18n.load(injectedMetadata.i18n.translationsUrl);
+  } catch (error) {
+    i18nError = error;
   }
 
-  i18n
-    .load(injectedMetadata.i18n.translationsUrl)
-    .catch((e) => e)
-    .then(async (i18nError) => {
-      const coreSystem = new CoreSystem({
-        injectedMetadata,
-        rootDomElement: document.body,
-        browserSupportsCsp: !(window as any).__kbnCspNotEnforced__,
-      });
+  const coreSystem = new CoreSystem({
+    injectedMetadata,
+    rootDomElement: document.body,
+    browserSupportsCsp: !(window as any).__kbnCspNotEnforced__,
+  });
 
-      const setup = await coreSystem.setup();
-      if (i18nError && setup) {
-        setup.fatalErrors.add(i18nError);
-      }
+  const setup = await coreSystem.setup();
+  if (i18nError && setup) {
+    setup.fatalErrors.add(i18nError);
+  }
 
-      const start = await coreSystem.start();
-
-      if (APM_ENABLED && start) {
-        /**
-         * Register listeners for navigation changes and capture them as
-         * route-change transactions after Kibana app is bootstrapped
-         */
-        start.application.currentAppId$.subscribe((appId) => {
-          const apmInstance = (window as any).elasticApm;
-          if (appId && apmInstance && typeof apmInstance.startTransaction === 'function') {
-            apmInstance.startTransaction(`/app/${appId}`, 'route-change', {
-              managed: true,
-              canReuse: true,
-            });
-          }
-        });
-      }
-    });
+  const start = await coreSystem.start();
+  await apmSystem.start(start);
 }
