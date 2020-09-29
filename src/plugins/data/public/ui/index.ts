0dd3af90bfb4233b7c59f0c92008349197e1a595
Luke Elmers <luke.elmers@elastic.co>
2020-09-29 17:10:46 -0600

    Lazy load QueryStringInput.

--- src/plugins/data/public/ui/index.ts
+++ src/plugins/data/public/ui/index.ts
@@ -20,7 +20,7 @@
 export { SuggestionsComponent } from './typeahead';
 export { IndexPatternSelectProps } from './index_pattern_select';
 export { FilterBar } from './filter_bar';
-export { QueryStringInput, QueryStringInputProps } from './query_string_input/query_string_input';
+export { QueryStringInput, QueryStringInputProps } from './query_string_input';
 export { SearchBar, SearchBarProps, StatefulSearchBarProps } from './search_bar';
 
 // @internal
