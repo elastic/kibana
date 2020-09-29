dc51210974998c7b11bdf4c3709da4fd158d0795
bhavyarm <bhavya@elastic.co>
2020-09-29 13:15:28 -0400

    a11y tests for user page

---
+++ x-pack/test/accessibility/apps/users.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+// a11y tests for spaces, space selection and spacce creation and feature controls
+
+import { FtrProviderContext } from '../ftr_provider_context';
+
+export default function ({ getService, getPageObjects }: FtrProviderContext) {
+  const PageObjects = getPageObjects(['security', 'settings', 'common', 'header']);
+  const a11y = getService('a11y');
+  const browser = getService('browser');
+  const esArchiver = getService('esArchiver');
+  const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+  const toasts = getService('toasts');
+
+  describe('Kibana users page meets a11y validations', () => {
+    before(async () => {
+      await esArchiver.load('empty_kibana');
+      await PageObjects.settings.navigateTo();
+      await PageObjects.security.clickElasticsearchUsers();
+    });
+
+    it('a11y test for user page', async () => {
+      await a11y.testAppSnapshot();
+    });
+
+    it('', async () => {
+      await a11y.testAppSnapshot();
+    });
+  });
+}
