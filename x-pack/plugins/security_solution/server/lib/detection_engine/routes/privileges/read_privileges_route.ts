269670e44c8c182933a9990b0e05e8c7db9d0776
Devin Hurley <devin.hurley@elastic.co>
2020-09-29 10:16:09 -0400

    adds new 'can_read_actions' property to privileges api

--- x-pack/plugins/security_solution/server/lib/detection_engine/routes/privileges/read_privileges_route.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/routes/privileges/read_privileges_route.ts
@@ -40,6 +40,12 @@ export const readPrivilegesRoute = (
         const clusterPrivileges = await readPrivileges(clusterClient.callAsCurrentUser, index);
         const privileges = merge(clusterPrivileges, {
           is_authenticated: security?.authc.isAuthenticated(request) ?? false,
+          can_read_actions:
+            (
+              await security?.authz?.checkPrivilegesDynamicallyWithRequest(request)({
+                kibana: [security?.authz?.actions.ui.get('actions', 'show')],
+              })
+            )?.hasAllRequested ?? false,
           has_encryption_key: !usingEphemeralEncryptionKey,
         });
 
