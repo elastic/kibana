340764dd5d2dc71dfa0252fb0ef1b3dc8886dc1e
Justin Kambic <justin.kambic@elastic.co>
2020-09-29 16:58:44 -0400

    Add tests for executed step.

--- x-pack/plugins/uptime/public/components/monitor/synthetics/executed_step.tsx
+++ x-pack/plugins/uptime/public/components/monitor/synthetics/executed_step.tsx
@@ -21,79 +21,74 @@ interface ExecutedStepProps {
   fetchScreenshot: (stepIndex: number) => void;
 }
 
-export const ExecutedStep: FC<ExecutedStepProps> = ({ step, index, fetchScreenshot }) => {
-  return (
-    <>
-      <div style={{ padding: '8px' }}>
-        <div>
-          <EuiText>
-            <strong>
-              <FormattedMessage
-                id="xpack.uptime.synthetics.executedStep.stepName"
-                defaultMessage="{stepNumber}. {stepName}"
-                values={{
-                  stepNumber: index + 1,
-                  stepName: step.synthetics?.step?.name,
-                }}
-              />
-            </strong>
-          </EuiText>
-        </div>
-        <EuiSpacer size="s" />
-        <div>
-          <StatusBadge status={step.synthetics?.payload?.status} />
-        </div>
-        <EuiSpacer />
-        <div>
-          <EuiFlexGroup>
-            <EuiFlexItem grow={false}>
-              <StepScreenshotDisplay
-                isLoading={step.synthetics?.screenshotLoading}
-                screenshot={step.synthetics?.blob}
-                stepIndex={step.synthetics?.step?.index}
-                stepName={step.synthetics?.step?.name}
-                fetchScreenshot={fetchScreenshot}
-              />
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <Accordion
-                id={step.synthetics?.step?.name + String(index)}
-                buttonContent={i18n.translate(
-                  'xpack.uptime.synthetics.executedStep.scriptHeading',
-                  {
-                    defaultMessage: 'Step script',
-                  }
-                )}
-                overflowHeight={CODE_BLOCK_OVERFLOW_HEIGHT}
-                language="javascript"
-              >
-                {step.synthetics?.payload?.source}
-              </Accordion>
-              <Accordion
-                id={`${step.synthetics?.step?.name}_error`}
-                buttonContent={i18n.translate('xpack.uptime.synthetics.executedStep.errorHeading', {
-                  defaultMessage: 'Error',
-                })}
-                language="html"
-                overflowHeight={CODE_BLOCK_OVERFLOW_HEIGHT}
-              >
-                {step.synthetics?.payload?.error?.message}
-              </Accordion>
-              <Accordion
-                id={`${step.synthetics?.step?.name}_stack`}
-                buttonContent={i18n.translate('xpack.uptime.synthetics.executedStep.stackTrace', {
-                  defaultMessage: 'Stack trace',
-                })}
-                language="html"
-                overflowHeight={CODE_BLOCK_OVERFLOW_HEIGHT}
-              >
-                {step.synthetics?.payload?.error?.stack}
-              </Accordion>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        </div>
+export const ExecutedStep: FC<ExecutedStepProps> = ({ step, index, fetchScreenshot }) => (
+  <>
+    <div style={{ padding: '8px' }}>
+      <div>
+        <EuiText>
+          <strong>
+            <FormattedMessage
+              id="xpack.uptime.synthetics.executedStep.stepName"
+              defaultMessage="{stepNumber}. {stepName}"
+              values={{
+                stepNumber: index + 1,
+                stepName: step.synthetics?.step?.name,
+              }}
+            />
+          </strong>
+        </EuiText>
+      </div>
+      <EuiSpacer size="s" />
+      <div>
+        <StatusBadge status={step.synthetics?.payload?.status} />
       </div>
       <EuiSpacer />
-    </>
-  );
-};
+      <div>
+        <EuiFlexGroup>
+          <EuiFlexItem grow={false}>
+            <StepScreenshotDisplay
+              isLoading={step.synthetics?.screenshotLoading}
+              screenshot={step.synthetics?.blob}
+              stepIndex={step.synthetics?.step?.index}
+              stepName={step.synthetics?.step?.name}
+              fetchScreenshot={fetchScreenshot}
+            />
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <Accordion
+              id={step.synthetics?.step?.name + String(index)}
+              buttonContent={i18n.translate('xpack.uptime.synthetics.executedStep.scriptHeading', {
+                defaultMessage: 'Step script',
+              })}
+              overflowHeight={CODE_BLOCK_OVERFLOW_HEIGHT}
+              language="javascript"
+            >
+              {step.synthetics?.payload?.source}
+            </Accordion>
+            <Accordion
+              id={`${step.synthetics?.step?.name}_error`}
+              buttonContent={i18n.translate('xpack.uptime.synthetics.executedStep.errorHeading', {
+                defaultMessage: 'Error',
+              })}
+              language="html"
+              overflowHeight={CODE_BLOCK_OVERFLOW_HEIGHT}
+            >
+              {step.synthetics?.payload?.error?.message}
+            </Accordion>
+            <Accordion
+              id={`${step.synthetics?.step?.name}_stack`}
+              buttonContent={i18n.translate('xpack.uptime.synthetics.executedStep.stackTrace', {
+                defaultMessage: 'Stack trace',
+              })}
+              language="html"
+              overflowHeight={CODE_BLOCK_OVERFLOW_HEIGHT}
+            >
+              {step.synthetics?.payload?.error?.stack}
+            </Accordion>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </div>
+    </div>
+    <EuiSpacer />
+  </>
+);
