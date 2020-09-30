f0ba4304cf5e109a90e19650c4c278817a5323e2
Tyler Smalley <tyler.smalley@elastic.co>
2020-09-30 15:36:47 -0700

    Remove unused import
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- x-pack/plugins/licensing/public/plugin.test.ts
+++ x-pack/plugins/licensing/public/plugin.test.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { skip, take, toArray } from 'rxjs/operators';
+import { take, toArray } from 'rxjs/operators';
 import { mountExpiredBannerMock } from './plugin.test.mocks';
 
 import { LicenseType } from '../common/types';
