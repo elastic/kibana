545fe2e230fdb352aa9c07fcbc7c1152b9f7afd3
Tiago Costa <tiagoffcc@hotmail.com>
2020-10-01 16:10:56 +0100

    chore(NA): remove unused import

--- x-pack/test/case_api_integration/common/config.ts
+++ x-pack/test/case_api_integration/common/config.ts
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import path from 'path';
-
 import { CA_CERT_PATH } from '@kbn/dev-utils';
 import { FtrConfigProviderContext } from '@kbn/test/types/ftr';
 
