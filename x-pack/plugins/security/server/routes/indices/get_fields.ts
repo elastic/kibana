4525f0cfab2045ee9e54bff813a9614f31313602
Larry Gregory <larry.gregory@elastic.co>
2020-10-01 08:26:26 -0400

    Omit runtime fields from FLS suggestions (#78330)
    Co-authored-by: Aleh Zasypkin <aleh.zasypkin@gmail.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/security/server/routes/indices/get_fields.ts
+++ x-pack/plugins/security/server/routes/indices/get_fields.ts
@@ -8,6 +8,20 @@ import { schema } from '@kbn/config-schema';
 import { RouteDefinitionParams } from '../index';
 import { wrapIntoCustomErrorResponse } from '../../errors';
 
+interface FieldMappingResponse {
+  [indexName: string]: {
+    mappings: {
+      [fieldName: string]: {
+        mapping: {
+          [fieldName: string]: {
+            type: string;
+          };
+        };
+      };
+    };
+  };
+}
+
 export function defineGetFieldsRoutes({ router, clusterClient }: RouteDefinitionParams) {
   router.get(
     {
@@ -23,21 +37,35 @@ export function defineGetFieldsRoutes({ router, clusterClient }: RouteDefinition
             fields: '*',
             allowNoIndices: false,
             includeDefaults: true,
-          })) as Record<string, { mappings: Record<string, unknown> }>;
+          })) as FieldMappingResponse;
 
         // The flow is the following (see response format at https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-get-field-mapping.html):
         // 1. Iterate over all matched indices.
         // 2. Extract all the field names from the `mappings` field of the particular index.
-        // 3. Collect and flatten the list of the field names.
+        // 3. Collect and flatten the list of the field names, omitting any fields without mappings, and any runtime fields
         // 4. Use `Set` to get only unique field names.
+        const fields = Array.from(
+          new Set(
+            Object.values(indexMappings).flatMap((indexMapping) => {
+              return Object.keys(indexMapping.mappings).filter((fieldName) => {
+                const mappingValues = Object.values(indexMapping.mappings[fieldName].mapping);
+                const hasMapping = mappingValues.length > 0;
+
+                const isRuntimeField = hasMapping && mappingValues[0]?.type === 'runtime';
+
+                // fields without mappings are internal fields such as `_routing` and `_index`,
+                // and therefore don't make sense as autocomplete suggestions for FLS.
+
+                // Runtime fields are not securable via FLS.
+                // Administrators should instead secure access to the fields which derive this information.
+                return hasMapping && !isRuntimeField;
+              });
+            })
+          )
+        );
+
         return response.ok({
-          body: Array.from(
-            new Set(
-              Object.values(indexMappings)
-                .map((indexMapping) => Object.keys(indexMapping.mappings))
-                .flat()
-            )
-          ),
+          body: fields,
         });
       } catch (error) {
         return response.customError(wrapIntoCustomErrorResponse(error));
