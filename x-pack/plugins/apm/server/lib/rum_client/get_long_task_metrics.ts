4ddcd1d2a6bf3ffbec37844468c4e4af827f739f
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-10-02 09:45:50 +0200

    [APM] Fix anomalies not showing up on transaction charts (#76930)
    * [APM] Fix anomalies not showing up on transaction charts
    
    * Added API tests to check transaction groups charts for anomaly data
    
    * Improve test names and assertions from PR feedback
    
    * Updated the transaction groups chart API to make `environment` a
    required param while making `uiFilters` optional
    
    * updates the basic API tests for transaction_groups/charts with the
    required `environment` param
    
    * makes uiFIltersES default to [] on core setup and removes SetupUIFilters type
    
    * fixes vertical shade
    
    * - replaces uiFiltersES with esFilter & uiFilters and cleans up related code around these
    - deduplicates the required environment in the transaction_groups/charts API
    
    * updates basic apm_api_integration tests
    
    * pr feedback
    
    * updates api test snapshots with correct anomaly data
    
    * removed environment query param from useTransactionCharts and ensures
    it's included in uiFilters returned from useUrlParams
    
    Co-authored-by: Oliver Gupte <olivergupte@gmail.com>

--- x-pack/plugins/apm/server/lib/rum_client/get_long_task_metrics.ts
+++ x-pack/plugins/apm/server/lib/rum_client/get_long_task_metrics.ts
@@ -6,11 +6,7 @@
 
 import { getRumPageLoadTransactionsProjection } from '../../projections/rum_page_load_transactions';
 import { mergeProjection } from '../../projections/util/merge_projection';
-import {
-  Setup,
-  SetupTimeRange,
-  SetupUIFilters,
-} from '../helpers/setup_request';
+import { Setup, SetupTimeRange } from '../helpers/setup_request';
 
 const LONG_TASK_SUM_FIELD = 'transaction.experience.longtask.sum';
 const LONG_TASK_COUNT_FIELD = 'transaction.experience.longtask.count';
@@ -21,7 +17,7 @@ export async function getLongTaskMetrics({
   urlQuery,
   percentile = 50,
 }: {
-  setup: Setup & SetupTimeRange & SetupUIFilters;
+  setup: Setup & SetupTimeRange;
   urlQuery?: string;
   percentile?: number;
 }) {
