426d88a86d528c70f317fb4c0f01efaf353dd1a6
Joe Reuter <johannes.reuter@elastic.co>
2020-10-02 14:58:36 +0200

    improve lens lazy loading

--- x-pack/plugins/lens/public/editor_frame_service/embeddable/index.ts
+++ x-pack/plugins/lens/public/editor_frame_service/embeddable/index.ts
@@ -4,6 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export * from './editor_frame';
-export * from './state_helpers';
-export * from './state_management';
+export * from './embeddable';
