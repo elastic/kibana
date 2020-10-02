e0b57bab722c8b3265035ece167557717c1c434a
Nathan Reese <reese.nathan@gmail.com>
2020-10-02 08:16:28 -0600

    [Maps] enable auto fit to bounds by default

---
+++ x-pack/plugins/maps/common/migrations/set_default_auto_fit_to_bounds.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import _ from 'lodash';
+import {
+  AGG_DELIMITER,
+  AGG_TYPE,
+  FIELD_ORIGIN,
+  JOIN_FIELD_NAME_PREFIX,
+  LAYER_TYPE,
+  VECTOR_STYLES,
+} from '../constants';
+import { getJoinAggKey } from '../get_agg_key';
+import {
+  AggDescriptor,
+  JoinDescriptor,
+  LayerDescriptor,
+  VectorLayerDescriptor,
+} from '../descriptor_types';
+import { MapSavedObjectAttributes } from '../map_saved_object_type';
+
+export function setDefaultAutoFitToBounds({
+  attributes,
+}: {
+  attributes: MapSavedObjectAttributes;
+}): MapSavedObjectAttributes {
+  if (!attributes || !attributes.mapStateJSON) {
+    return attributes;
+  }
+
+  // MapState type is defined in public, no need to bring all of that to common for this migration
+  const mapState: unknown = JSON.parse(attributes.mapStateJSON);
+  if ('settings' in mapState) {
+    mapState.settings.autoFitToDataBounds = false;
+  } else {
+    mapState.settings = {
+      autoFitToDataBounds: false,
+    };
+  }
+
+  return {
+    ...attributes,
+    mapStateJSON: JSON.stringify(mapState),
+  };
+}
