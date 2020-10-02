6e100657c1bdb2bfeb75bbb82bc0c8cc2c7a66ec
Shahzad <shahzad31comp@gmail.com>
2020-10-02 12:19:58 +0200

    apply range filter only on service name

--- x-pack/plugins/apm/server/lib/rum_client/has_rum_data.ts
+++ x-pack/plugins/apm/server/lib/rum_client/has_rum_data.ts
@@ -3,45 +3,55 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+import { Setup, SetupTimeRange } from '../helpers/setup_request';
 import {
-  Setup,
-  SetupTimeRange,
-  SetupUIFilters,
-} from '../helpers/setup_request';
-import { SERVICE_NAME } from '../../../common/elasticsearch_fieldnames';
-import { getRumPageLoadTransactionsProjection } from '../../projections/rum_page_load_transactions';
-import { mergeProjection } from '../../projections/util/merge_projection';
+  SERVICE_NAME,
+  TRANSACTION_TYPE,
+} from '../../../common/elasticsearch_fieldnames';
+import { ProcessorEvent } from '../../../common/processor_event';
+import { rangeFilter } from '../../../common/utils/range_filter';
+import { TRANSACTION_PAGE_LOAD } from '../../../common/transaction_types';
 
-export async function hasRumData({
-  setup,
-}: {
-  setup: Setup & SetupTimeRange & SetupUIFilters;
-}) {
+export async function hasRumData({ setup }: { setup: Setup & SetupTimeRange }) {
   try {
-    const projection = getRumPageLoadTransactionsProjection({
-      setup,
-    });
+    const { start, end } = setup;
 
-    const params = mergeProjection(projection, {
+    const params = {
+      apm: {
+        events: [ProcessorEvent.transaction],
+      },
       body: {
         size: 0,
+        query: {
+          bool: {
+            filter: [{ term: { [TRANSACTION_TYPE]: TRANSACTION_PAGE_LOAD } }],
+          },
+        },
         aggs: {
           services: {
-            terms: {
-              field: SERVICE_NAME,
-              size: 1,
+            filter: {
+              range: rangeFilter(start, end),
+            },
+            aggs: {
+              mostTraffic: {
+                terms: {
+                  field: SERVICE_NAME,
+                  size: 1,
+                },
+              },
             },
           },
         },
       },
-    });
+    };
 
     const { apmEventClient } = setup;
 
     const response = await apmEventClient.search(params);
     return {
       hasData: response.hits.total.value > 0,
-      serviceName: response.aggregations?.services?.buckets?.[0]?.key,
+      serviceName:
+        response.aggregations?.services?.mostTraffic?.buckets?.[0]?.key,
     };
   } catch (e) {
     return false;
