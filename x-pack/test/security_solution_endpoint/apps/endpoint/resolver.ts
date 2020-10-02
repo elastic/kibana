4bfeba73417f3f2cf62ac0c7d716465f58797d43
Jen Huang <its.jenetic@gmail.com>
2020-10-01 18:18:30 -0700

    Skip failing endpoint resolver test

--- x-pack/test/security_solution_endpoint/apps/endpoint/resolver.ts
+++ x-pack/test/security_solution_endpoint/apps/endpoint/resolver.ts
@@ -14,7 +14,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const queryBar = getService('queryBar');
   const browser = getService('browser');
 
-  describe('Endpoint Event Resolver', function () {
+  describe.skip('Endpoint Event Resolver', function () {
     before(async () => {
       await esArchiver.load('endpoint/resolver_tree', { useCreate: true });
       await pageObjects.hosts.navigateToSecurityHostsPage();
