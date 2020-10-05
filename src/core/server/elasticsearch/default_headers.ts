4982ccfcb9ff341bb6229c150b184e15535b458d
kobelb <brandon.kobel@elastic.co>
2020-10-05 15:58:49 -0700

    Adding comment for why `x-elastic-product-origin` exists

--- src/core/server/elasticsearch/default_headers.ts
+++ src/core/server/elasticsearch/default_headers.ts
@@ -20,5 +20,8 @@
 import { deepFreeze } from '@kbn/std';
 
 export const DEFAULT_HEADERS = deepFreeze({
+  // Elasticsearch uses this to identify when a request is coming from Kibana, to allow Kibana to
+  // access system indices using the standard ES APIs without logging a warning. After migrating to
+  // use the new system index APIs, this header can be removed.
   'x-elastic-product-origin': 'kibana',
 });
