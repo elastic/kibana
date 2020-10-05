fd27e5b7359ed2bf9f510571785d88e5a68ad5ee
Patryk Kopyciński <patryk.kopycinski@elastic.co>
2020-10-05 18:10:40 +0200

    [Security Solution] Align skip logic across SearchStrategy queries (#79085)

--- x-pack/plugins/security_solution/public/common/containers/matrix_histogram/index.ts
+++ x-pack/plugins/security_solution/public/common/containers/matrix_histogram/index.ts
@@ -34,6 +34,8 @@ export interface UseMatrixHistogramArgs {
   totalCount: number;
 }
 
+const ID = 'matrixHistogramQuery';
+
 export const useMatrixHistogram = ({
   endDate,
   errorMessage,
@@ -54,6 +56,7 @@ export const useMatrixHistogram = ({
     factoryQueryType: MatrixHistogramQuery,
     filterQuery: createFilter(filterQuery),
     histogramType,
+    id: ID,
     timerange: {
       interval: '12h',
       from: startDate,
