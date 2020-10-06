1aca8534e238088d7f5ca70c89300f98c63f48b6
CJ Cenizal <cj@cenizal.com>
2020-10-05 21:42:50 -0700

    Revert "Add support for runtime field types to mappings editor. (#77420) (#78539)"
    This reverts commit 52b1ad98bebde287d2392d1c0525c987c1569ae4.

--- x-pack/plugins/index_management/public/application/components/mappings_editor/types/document_fields.ts
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/types/document_fields.ts
@@ -8,7 +8,7 @@ import { ReactNode } from 'react';
 import { GenericObject } from './mappings_editor';
 
 import { FieldConfig } from '../shared_imports';
-import { PARAMETERS_DEFINITION, RUNTIME_FIELD_TYPES } from '../constants';
+import { PARAMETERS_DEFINITION } from '../constants';
 
 export interface DataTypeDefinition {
   label: string;
@@ -19,7 +19,6 @@ export interface DataTypeDefinition {
   };
   subTypes?: { label: string; types: SubType[] };
   description?: () => ReactNode;
-  isBeta?: boolean;
 }
 
 export interface ParameterDefinition {
@@ -36,7 +35,6 @@ export interface ParameterDefinition {
 }
 
 export type MainType =
-  | 'runtime'
   | 'text'
   | 'keyword'
   | 'numeric'
@@ -75,8 +73,6 @@ export type MainType =
 export type SubType = NumericType | RangeType;
 
 export type DataType = MainType | SubType;
-
-export type RuntimeType = typeof RUNTIME_FIELD_TYPES[number];
 
 export type NumericType =
   | 'long'
@@ -156,8 +152,6 @@ export type ParameterName =
   | 'depth_limit'
   | 'relations'
   | 'max_shingle_size'
-  | 'runtime_type'
-  | 'script'
   | 'value'
   | 'meta';
 
@@ -175,7 +169,6 @@ export interface Fields {
 interface FieldBasic {
   name: string;
   type: DataType;
-  runtime_type?: DataType;
   subType?: SubType;
   properties?: { [key: string]: Omit<Field, 'name'> };
   fields?: { [key: string]: Omit<Field, 'name'> };
