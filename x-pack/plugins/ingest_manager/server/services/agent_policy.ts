4158547535acd29adc95dfb422fbe190146a9551
John Schulz <john.schulz@elastic.co>
2020-10-06 18:02:44 -0400

    [Ingest Manager] Configure Elasticsearch output with YAML in global output settings (#79019)
    ## Summary
    YAML entered on the Settings page will be added to each Elastic Agent policy
    
    ### Settings form
    <img width="50%" alt="Screen Shot 2020-10-05 at 9 29 27 AM" src="https://user-images.githubusercontent.com/57655/95085934-daa20880-06ed-11eb-9f52-aab5d69666ad.png"><img width="50%" alt="Screen Shot 2020-10-05 at 9 29 58 AM" src="https://user-images.githubusercontent.com/57655/95085886-c9f19280-06ed-11eb-8c85-a56b330cee37.png">
    
    ### Policy screen
    <img width="50%" alt="Screen Shot 2020-10-05 at 9 30 38 AM" src="https://user-images.githubusercontent.com/57655/95085889-ca8a2900-06ed-11eb-9cd7-b7590101a15d.png">
    
    ### Input Validation
    <details><summary>Fails if the value cannot be parsed as YAML, but there are no restricted keys or other guidance</summary>
    
    <img width="50%" alt="Screen Shot 2020-10-05 at 9 32 39 AM" src="https://user-images.githubusercontent.com/57655/95086469-877c8580-06ee-11eb-8189-d70413a0a5c9.png">
    
    </details>
    
    ### Open questions
      1. Is "Additional YAML Configuration" ok for the form?
        * Will use "Elasticsearch output configuration"
      1. Alternatives to [`additional_yaml_config`](https://github.com/elastic/kibana/pull/79019/files#diff-d2cc0ddf9161efb6898baca37350c720) for the new saved object key
        * No comments on the name but will move it to the outputs saved object
    
    ### Checklist
    
    - [x] Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/master/packages/kbn-i18n/README.md)
    
    # Conflicts:
    #	x-pack/plugins/ingest_manager/server/services/agent_policy.ts

--- x-pack/plugins/ingest_manager/server/services/agent_policy.ts
+++ x-pack/plugins/ingest_manager/server/services/agent_policy.ts
@@ -4,6 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import { uniq } from 'lodash';
+import { safeLoad } from 'js-yaml';
 import { SavedObjectsClientContract, SavedObjectsBulkUpdateResponse } from 'src/core/server';
 import { AuthenticatedUser } from '../../../security/server';
 import {
@@ -20,7 +21,12 @@ import {
   AgentPolicyStatus,
   ListWithKuery,
 } from '../types';
-import { DeleteAgentPolicyResponse, storedPackagePoliciesToAgentInputs } from '../../common';
+import {
+  DeleteAgentPolicyResponse,
+  Settings,
+  storedPackagePoliciesToAgentInputs,
+} from '../../common';
+import { AgentPolicyNameExistsError } from '../errors';
 import { createAgentPolicyAction, listAgents } from './agents';
 import { packagePolicyService } from './package_policy';
 import { outputService } from './output';
@@ -452,13 +458,14 @@ class AgentPolicyService {
         // TEMPORARY as we only support a default output
         ...[defaultOutput].reduce(
           // eslint-disable-next-line @typescript-eslint/naming-convention
-          (outputs, { config: outputConfig, name, type, hosts, ca_sha256, api_key }) => {
+          (outputs, { config_yaml, name, type, hosts, ca_sha256, api_key }) => {
+            const configJs = config_yaml ? safeLoad(config_yaml) : {};
             outputs[name] = {
               type,
               hosts,
               ca_sha256,
               api_key,
-              ...outputConfig,
+              ...configJs,
             };
 
             if (options?.standalone) {
@@ -494,7 +501,7 @@ class AgentPolicyService {
 
     // only add settings if not in standalone
     if (!standalone) {
-      let settings;
+      let settings: Settings;
       try {
         settings = await getSettings(soClient);
       } catch (error) {
