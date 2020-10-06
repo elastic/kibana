4ad36d93e0a8d14adcdf4ab8e0cd2595b7f7c7f9
Andrea Del Rio <delrio.andre@gmail.com>
2020-10-06 12:58:08 -0700

    remove fixed height

--- src/plugins/discover/public/application/components/hits_counter/hits_counter.tsx
+++ src/plugins/discover/public/application/components/hits_counter/hits_counter.tsx
@@ -40,7 +40,13 @@ export interface HitsCounterProps {
 export function HitsCounter({ hits, showResetButton, onResetQuery }: HitsCounterProps) {
   return (
     <I18nProvider>
-      <EuiFlexGroup gutterSize="s" responsive={false} justifyContent="center" alignItems="center">
+      <EuiFlexGroup
+        className="dscHitsCounter"
+        gutterSize="s"
+        responsive={false}
+        justifyContent="center"
+        alignItems="center"
+      >
         <EuiFlexItem grow={false}>
           <EuiText>
             <strong data-test-subj="discoverQueryHits">{formatNumWithCommas(hits)}</strong>{' '}
