21d5ad8faaa65fc8aec46e7b11ebe36d4292b339
scottybollinger <scotty.bollinger@elastic.co>
2020-10-06 16:31:03 -0500

    Add GroupManagerModal tests

---
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/groups/components/group_manager_modal.test.tsx
@@ -0,0 +1,85 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import '../../../../__mocks__/kea.mock';
+
+import { setMockValues } from '../../../../__mocks__';
+import { groups } from '../../../__mocks__/groups.mock';
+import { contentSources } from '../../../__mocks__/content_sources.mock';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { GroupManagerModal } from './group_manager_modal';
+
+import { EuiOverlayMask, EuiModal, EuiEmptyPrompt } from '@elastic/eui';
+
+const hideModal = jest.fn();
+const selectAll = jest.fn();
+const saveItems = jest.fn();
+
+const props = {
+  children: <></>,
+  label: 'shared content sources',
+  allItems: [],
+  numSelected: 1,
+  hideModal,
+  selectAll,
+  saveItems,
+};
+
+const mockValues = {
+  group: groups[0],
+  contentSources,
+  managerModalFormErrors: [],
+};
+
+describe('GroupManagerModal', () => {
+  beforeEach(() => {
+    setMockValues(mockValues);
+  });
+
+  it('renders', () => {
+    const wrapper = shallow(<GroupManagerModal {...props} />);
+
+    expect(wrapper.find(EuiModal)).toHaveLength(1);
+    expect(wrapper.find(EuiOverlayMask)).toHaveLength(1);
+  });
+
+  it('renders empty state', () => {
+    setMockValues({ ...mockValues, contentSources: [] });
+    const wrapper = shallow(<GroupManagerModal {...props} />);
+
+    expect(wrapper.find(EuiEmptyPrompt)).toHaveLength(1);
+  });
+
+  it('selects all items when clicked', () => {
+    const wrapper = shallow(<GroupManagerModal {...props} numSelected={1} allItems={[{}]} />);
+
+    const button = wrapper.find('[data-test-subj="SelectAllGroups"]');
+    button.simulate('click');
+
+    expect(selectAll).toHaveBeenCalledWith([]);
+  });
+
+  it('deselects all items when clicked', () => {
+    const wrapper = shallow(<GroupManagerModal {...props} numSelected={0} allItems={[{}]} />);
+
+    const button = wrapper.find('[data-test-subj="SelectAllGroups"]');
+    button.simulate('click');
+
+    expect(selectAll).toHaveBeenCalledWith([{}]);
+  });
+
+  it('handles cancel when clicked', () => {
+    const wrapper = shallow(<GroupManagerModal {...props} />);
+
+    const button = wrapper.find('[data-test-subj="CloseGroupsModal"]');
+    button.simulate('click');
+
+    expect(hideModal).toHaveBeenCalledWith(groups[0]);
+  });
+});
