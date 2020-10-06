1f72ed305efbc1a8bfe220a7de66799756ebc311
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2020-10-06 13:07:45 -0400

    [7.x] [Security Solution][Resolver] Simplify CopyableField styling and add comments (#79594) (#79673)

--- x-pack/plugins/security_solution/public/resolver/view/panels/copyable_panel_field.tsx
+++ x-pack/plugins/security_solution/public/resolver/view/panels/copyable_panel_field.tsx
@@ -9,10 +9,10 @@
 import { EuiToolTip, EuiPopover } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import styled from 'styled-components';
-import React, { memo, useState, useContext } from 'react';
+import React, { memo, useState } from 'react';
 import { WithCopyToClipboard } from '../../../common/lib/clipboard/with_copy_to_clipboard';
 import { useColors } from '../use_colors';
-import { ResolverPanelContext } from './panel_context';
+import { StyledPanel } from '../styles';
 
 interface StyledCopyableField {
   readonly backgroundColor: string;
@@ -20,28 +20,35 @@ interface StyledCopyableField {
 }
 
 const StyledCopyableField = styled.div<StyledCopyableField>`
-  background-color: ${(props) => props.backgroundColor};
   border-radius: 3px;
   padding: 4px;
   transition: background 0.2s ease;
 
-  &:hover {
-    background-color: ${(props) => props.activeBackgroundColor};
-    color: #fff;
+  ${StyledPanel}:hover & {
+    background-color: ${(props) => props.backgroundColor};
+
+    &:hover {
+      background-color: ${(props) => props.activeBackgroundColor};
+      color: #fff;
+    }
   }
 `;
 
+/**
+ * Field that behaves similarly to the current implementation of copyable fields in timeline as of 7.10
+ * When the panel is hovered, these fields will show a gray background
+ * When you then hover over these fields they will show a blue background and a tooltip with a copy button will appear
+ */
 export const CopyablePanelField = memo(
   ({ textToCopy, content }: { textToCopy: string; content: JSX.Element | string }) => {
-    const { linkColor, copyableBackground } = useColors();
+    const { linkColor, copyableFieldBackground } = useColors();
     const [isOpen, setIsOpen] = useState(false);
-    const panelContext = useContext(ResolverPanelContext);
 
     const onMouseEnter = () => setIsOpen(true);
 
     const ButtonContent = memo(() => (
       <StyledCopyableField
-        backgroundColor={panelContext.isHoveringInPanel ? copyableBackground : 'transparent'}
+        backgroundColor={copyableFieldBackground}
         data-test-subj="resolver:panel:copyable-field"
         activeBackgroundColor={linkColor}
         onMouseEnter={onMouseEnter}
