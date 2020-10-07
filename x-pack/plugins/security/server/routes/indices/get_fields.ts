8c09e3618f845cb8fe2b2ccb4a8a8a05d34604bc
spalger <spalger@users.noreply.github.com>
2020-10-07 15:05:54 -0700

    Revert "[7.x] Omit runtime fields from FLS suggestions (#78330) (#79106)"
    This reverts commit f0be469288db6716aad109d281cc3aa893ae3e96.

--- x-pack/plugins/security/server/routes/indices/get_fields.ts
+++ x-pack/plugins/security/server/routes/indices/get_fields.ts
@@ -8,20 +8,6 @@ import { schema } from '@kbn/config-schema';
 import { RouteDefinitionParams } from '../index';
 import { wrapIntoCustomErrorResponse } from '../../errors';
 
-interface FieldMappingResponse {
-  [indexName: string]: {
-    mappings: {
-      [fieldName: string]: {
-        mapping: {
-          [fieldName: string]: {
-            type: string;
-          };
-        };
-      };
-    };
-  };
-}
-
 export function defineGetFieldsRoutes({ router, clusterClient }: RouteDefinitionParams) {
   router.get(
     {
@@ -37,35 +23,21 @@ export function defineGetFieldsRoutes({ router, clusterClient }: RouteDefinition
             fields: '*',
             allowNoIndices: false,
             includeDefaults: true,
-          })) as FieldMappingResponse;
+          })) as Record<string, { mappings: Record<string, unknown> }>;
 
         // The flow is the following (see response format at https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-get-field-mapping.html):
         // 1. Iterate over all matched indices.
         // 2. Extract all the field names from the `mappings` field of the particular index.
-        // 3. Collect and flatten the list of the field names, omitting any fields without mappings, and any runtime fields
+        // 3. Collect and flatten the list of the field names.
         // 4. Use `Set` to get only unique field names.
-        const fields = Array.from(
-          new Set(
-            Object.values(indexMappings).flatMap((indexMapping) => {
-              return Object.keys(indexMapping.mappings).filter((fieldName) => {
-                const mappingValues = Object.values(indexMapping.mappings[fieldName].mapping);
-                const hasMapping = mappingValues.length > 0;
-
-                const isRuntimeField = hasMapping && mappingValues[0]?.type === 'runtime';
-
-                // fields without mappings are internal fields such as `_routing` and `_index`,
-                // and therefore don't make sense as autocomplete suggestions for FLS.
-
-                // Runtime fields are not securable via FLS.
-                // Administrators should instead secure access to the fields which derive this information.
-                return hasMapping && !isRuntimeField;
-              });
-            })
-          )
-        );
-
         return response.ok({
-          body: fields,
+          body: Array.from(
+            new Set(
+              Object.values(indexMappings)
+                .map((indexMapping) => Object.keys(indexMapping.mappings))
+                .flat()
+            )
+          ),
         });
       } catch (error) {
         return response.customError(wrapIntoCustomErrorResponse(error));
