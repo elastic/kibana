b877b80236a887463ab24296191db1184927df4b
Devon A Thomson <devon.thomson@hotmail.com>
2020-10-08 10:40:28 -0400

    Added aria label

--- src/plugins/dashboard/public/application/actions/library_notification_popover.tsx
+++ src/plugins/dashboard/public/application/actions/library_notification_popover.tsx
@@ -56,6 +56,9 @@ export function LibraryNotificationPopover({
           data-test-subj={`embeddablePanelNotification-${id}`}
           iconType={icon}
           style={{ marginTop: '-1px' }}
+          aria-label={i18n.translate('dashboard.panel.libraryNotification.ariaLabel', {
+            defaultMessage: 'View library information and unlink this panel',
+          })}
           color="text"
           onClick={() => setIsPopoverOpen((isOpen) => !isOpen)}
         />
