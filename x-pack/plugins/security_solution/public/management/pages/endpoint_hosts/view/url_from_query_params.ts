89a78c7c1c9066bc03ba2a605241d1bcdae5b3d4
Thomas Watson <w@tson.dk>
2020-10-12 12:15:36 +0200

    Revert fix that's no longer needed

--- x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/url_from_query_params.ts
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/url_from_query_params.ts
@@ -11,7 +11,7 @@ import { EndpointIndexUIQueryParams } from '../types';
 import { AppLocation } from '../../../../../common/endpoint/types';
 
 export function urlFromQueryParams(queryParams: EndpointIndexUIQueryParams): Partial<AppLocation> {
-  const search = querystring.stringify({ ...queryParams }); // TypeScript note: {...x} is a workaround for https://github.com/microsoft/TypeScript/issues/15300
+  const search = querystring.stringify(queryParams);
   return {
     search,
   };
