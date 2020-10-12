f532e9dca68d381231dda70ede2c2866898a071c
Matthew Kime <matt@mattki.me>
2020-10-12 09:49:21 -0500

    index pattern server api simpler dependency (#80114)
    * index pattern server api simpler dependency

--- src/plugins/vis_type_timeseries/server/lib/get_fields.ts
+++ src/plugins/vis_type_timeseries/server/lib/get_fields.ts
@@ -62,8 +62,11 @@ export async function getFields(
   let indexPatternString = indexPattern;
 
   if (!indexPatternString) {
-    const [, { data }] = await framework.core.getStartServices();
-    const indexPatternsService = await data.indexPatterns.indexPatternsServiceFactory(request);
+    const [{ savedObjects }, { data }] = await framework.core.getStartServices();
+    const savedObjectsClient = savedObjects.getScopedClient(request);
+    const indexPatternsService = await data.indexPatterns.indexPatternsServiceFactory(
+      savedObjectsClient
+    );
     const defaultIndexPattern = await indexPatternsService.getDefault();
     indexPatternString = get(defaultIndexPattern, 'title', '');
   }
