e80bb7f55de47b36dda7f0843b04e7f3bd52dcc6
Jean-Louis Leysens <jloleysens@gmail.com>
2020-10-13 18:02:04 +0200

    [7.10] [ILM] Cloud-specific changes for 7.10 (#79650) (#80318)
    * [ILM] Cloud-specific changes for 7.10 (#79650)
    
    * added cloud cold tier specific call out, probably wip
    
    * added jest test
    
    * rephrase copy
    
    * Added logic for showing/hiding data tier allocation option
    
    - Added server-side test for detecting legacy config
    - Added client-side test for asserting on cloud data tier option
      is hidden
    
    * added test for not-on-cloud case
    
    * Refactored logic for rendering data allocation notices
    
    Also updated a comment.
    
    * paraphrashing of Adams copy recommendation
    
    * Fix comment
    
    * address pr feedback
    
    * clarify slight hack comment
    
    * complete refactor of new flag for tests
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	x-pack/plugins/index_lifecycle_management/server/routes/api/nodes/register_list_route.ts
    
    * replace r with (r)

--- x-pack/test/api_integration/apis/management/index_lifecycle_management/nodes.js
+++ x-pack/test/api_integration/apis/management/index_lifecycle_management/nodes.js
@@ -29,6 +29,7 @@ export default function ({ getService }) {
         const nodesIds = Object.keys(nodeStats.nodes);
 
         const { body } = await loadNodes().expect(200);
+        expect(body.isUsingDeprecatedDataRoleConfig).to.eql(false);
         expect(body.nodesByAttributes[NODE_CUSTOM_ATTRIBUTE]).to.eql(nodesIds);
       });
     });
