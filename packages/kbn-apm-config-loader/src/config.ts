180362be8503bb122d1e8f217faaee33ae3b361e
Tyler Smalley <tyler.smalley@elastic.co>
2020-10-13 12:08:50 -0700

    [CI] Preparation for APM tracking on CI
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- packages/kbn-apm-config-loader/src/config.ts
+++ packages/kbn-apm-config-loader/src/config.ts
@@ -26,32 +26,26 @@ import { readFileSync } from 'fs';
 import { ApmAgentConfig } from './types';
 
 const getDefaultConfig = (isDistributable: boolean): ApmAgentConfig => {
-  if (isDistributable) {
-    return {
-      active: false,
-      globalLabels: {},
-      // Do not use a centralized controlled config
-      centralConfig: false,
-      // Capture all exceptions that are not caught
-      logUncaughtExceptions: true,
-      // Can be performance intensive, disabling by default
-      breakdownMetrics: false,
-    };
-  }
-
+  // https://www.elastic.co/guide/en/apm/agent/nodejs/current/configuration.html
   return {
-    active: false,
-    serverUrl: 'https://f1542b814f674090afd914960583265f.apm.us-central1.gcp.cloud.es.io:443',
+    active: process.env.ELASTIC_APM_ACTIVE || false,
+    environment: process.env.ELASTIC_APM_ENVIRONMENT || process.env.NODE_ENV || 'development',
+
+    serverUrl: 'https://b1e3b4b4233e44cdad468c127d0af8d8.apm.europe-west1.gcp.cloud.es.io:443',
+
     // The secretToken below is intended to be hardcoded in this file even though
     // it makes it public. This is not a security/privacy issue. Normally we'd
     // instead disable the need for a secretToken in the APM Server config where
     // the data is transmitted to, but due to how it's being hosted, it's easier,
     // for now, to simply leave it in.
-    secretToken: 'R0Gjg46pE9K9wGestd',
-    globalLabels: {},
-    breakdownMetrics: true,
-    centralConfig: false,
+    secretToken: '2OyjjaI6RVkzx2O5CV',
+
     logUncaughtExceptions: true,
+    globalLabels: {},
+    centralConfig: false,
+
+    // Can be performance intensive, disabling by default
+    breakdownMetrics: isDistributable ? false : true,
   };
 };
 
@@ -84,7 +78,8 @@ export class ApmConfiguration {
         getDefaultConfig(this.isDistributable),
         this.getConfigFromKibanaConfig(),
         this.getDevConfig(),
-        this.getDistConfig()
+        this.getDistConfig(),
+        this.getCIConfig()
       );
 
       const rev = this.getGitRev();
@@ -143,6 +138,21 @@ export class ApmConfiguration {
       // Headers & body may contain sensitive info
       captureHeaders: false,
       captureBody: 'off',
+    };
+  }
+
+  private getCIConfig(): ApmAgentConfig {
+    if (process.env.ELASTIC_APM_ENVIRONMENT !== 'ci') {
+      return {};
+    }
+
+    return {
+      globalLabels: {
+        branch: process.env.ghprbSourceBranch || '',
+        targetBranch: process.env.ghprbTargetBranch || '',
+        ciJobName: process.env.JOB_NAME || '',
+        ciBuildNumber: process.env.BUILD_NUMBER || '',
+      },
     };
   }
 
