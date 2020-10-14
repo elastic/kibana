fda12ccd723c9700b5f211bd76e180a6c480d1fe
Quynh Nguyen <quynh.nguyen@elastic.co>
2020-10-13 19:47:12 -0500

    [ML] Change to formatSingleValue for tickFormatter

--- x-pack/plugins/ml/public/application/formatters/format_value.ts
+++ x-pack/plugins/ml/public/application/formatters/format_value.ts
@@ -53,7 +53,7 @@ export function formatValue(
 // For time_of_day or time_of_week functions the anomaly record
 // containing the timestamp of the anomaly should be supplied in
 // order to correctly format the day or week offset to the time of the anomaly.
-function formatSingleValue(
+export function formatSingleValue(
   value: number,
   mlFunction: string,
   fieldFormat?: any,
