1cda07d8be21eac56153d82aec763f24d04be7d1
FrankHassanabad <frank.hassanabad@elastic.co>
2020-10-14 16:19:40 -0600

    Refactored part of the code and tests

--- x-pack/plugins/security_solution/server/lib/detection_engine/rules/get_rules_to_update.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rules/get_rules_to_update.ts
@@ -7,41 +7,38 @@
 import { AddPrepackagedRulesSchemaDecoded } from '../../../../common/detection_engine/schemas/request/add_prepackaged_rules_schema';
 import { RuleAlertType } from './types';
 
+/**
+ * Returns the rules to update by doing a compare to the rules from the file system against
+ * the installed rules already. This also merges exception list items between the two since
+ * exception list items can exist on both rules to update and already installed rules.
+ * @param rulesFromFileSystem The rules on the file system to check against installed
+ * @param installedRules The installed rules
+ */
 export const getRulesToUpdate = (
   rulesFromFileSystem: AddPrepackagedRulesSchemaDecoded[],
   installedRules: RuleAlertType[]
 ): AddPrepackagedRulesSchemaDecoded[] => {
   return rulesFromFileSystem
-    .filter((ruleFromFileSystem) =>
-      installedRules.some((installedRule) => {
-        return (
-          ruleFromFileSystem.rule_id === installedRule.params.ruleId &&
-          ruleFromFileSystem.version > installedRule.params.version
-        );
-      })
-    )
-    .map((ruleFromFileSystem) => {
-      if (ruleFromFileSystem.exceptions_list != null) {
-        const installedRule = installedRules.find(
-          (ruleToFind) => ruleToFind.params.ruleId === ruleFromFileSystem.rule_id
-        );
-        if (installedRule != null && installedRule.params.exceptionsList != null) {
-          const installedExceptionList = installedRule.params.exceptionsList;
-          const fileSystemExceptions = ruleFromFileSystem.exceptions_list.filter(
-            (potentialDuplicate) =>
-              installedExceptionList.every((item) => item.list_id !== potentialDuplicate.list_id)
-          );
-          return {
-            ...ruleFromFileSystem,
-            exceptions_list: [...fileSystemExceptions, ...installedRule.params.exceptionsList],
-          };
-        } else {
-          return ruleFromFileSystem;
-        }
-      } else {
-        return ruleFromFileSystem;
-      }
-    });
+    .filter((ruleFromFileSystem) => filterInstalledRules(ruleFromFileSystem, installedRules))
+    .map((ruleFromFileSystem) => mergeExceptionLists(ruleFromFileSystem, installedRules));
+};
+
+/**
+ * Filters rules from the file system that do not match the installed rules so you only
+ * get back rules that are going to be updated
+ * @param ruleFromFileSystem The rules from the file system to check if any are updates
+ * @param installedRules The installed rules to compare against for updates
+ */
+export const filterInstalledRules = (
+  ruleFromFileSystem: AddPrepackagedRulesSchemaDecoded,
+  installedRules: RuleAlertType[]
+): boolean => {
+  return installedRules.some((installedRule) => {
+    return (
+      ruleFromFileSystem.rule_id === installedRule.params.ruleId &&
+      ruleFromFileSystem.version > installedRule.params.version
+    );
+  });
 };
 
 /**
@@ -59,15 +56,13 @@ export const mergeExceptionLists = (
       (ruleToFind) => ruleToFind.params.ruleId === ruleFromFileSystem.rule_id
     );
     if (installedRule != null && installedRule.params.exceptionsList != null) {
-      const installedExceptions = installedRule.params.exceptionsList.filter(
-        (installedExceptionList) =>
-          ruleFromFileSystem.exceptions_list.some(
-            (exceptionList) => installedExceptionList.list_id !== exceptionList.list_id
-          )
+      const installedExceptionList = installedRule.params.exceptionsList;
+      const fileSystemExceptions = ruleFromFileSystem.exceptions_list.filter((potentialDuplicate) =>
+        installedExceptionList.every((item) => item.list_id !== potentialDuplicate.list_id)
       );
       return {
         ...ruleFromFileSystem,
-        exceptions_list: [...ruleFromFileSystem.exceptions_list, ...installedExceptions],
+        exceptions_list: [...fileSystemExceptions, ...installedRule.params.exceptionsList],
       };
     } else {
       return ruleFromFileSystem;
