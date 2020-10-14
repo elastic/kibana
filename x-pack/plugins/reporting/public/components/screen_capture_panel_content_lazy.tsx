4be37b94feec0f57c304021ec759a8416ade9b3a
streamich <streamich@gmail.com>
2020-10-14 11:54:12 +0000

    perf: ⚡️ lazy-load screen capture sharing panel React components

---
+++ x-pack/plugins/reporting/public/components/screen_capture_panel_content_lazy.tsx
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import * as React from 'react';
+import { lazy, Suspense, FC } from 'react';
+import type { Props } from './screen_capture_panel_content';
+
+const LazyComponent = lazy(() =>
+  import('./screen_capture_panel_content').then(({ ScreenCapturePanelContent }) => ({
+    default: ScreenCapturePanelContent,
+  }))
+);
+
+export const ScreenCapturePanelContent: FC<Props> = (props) => {
+  return (
+    <Suspense fallback={null}>
+      <LazyComponent {...props} />
+    </Suspense>
+  );
+};
