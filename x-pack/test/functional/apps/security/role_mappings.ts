6843cde00a0b6d67899f9454b1d1891c243e795c
Larry Gregory <lgregorydev@gmail.com>
2020-10-15 12:41:22 -0400

    Update x-pack/test/functional/apps/security/role_mappings.ts
    Co-authored-by: Joe Portner <5295965+jportner@users.noreply.github.com>

--- x-pack/test/functional/apps/security/role_mappings.ts
+++ x-pack/test/functional/apps/security/role_mappings.ts
@@ -17,7 +17,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
   describe('Role Mappings', function () {
     before(async () => {
-      // Delete any exising role mappings. ESS commonly sets up a role mapping automatically.
+      // Delete any existing role mappings. ESS commonly sets up a role mapping automatically.
       const existingMappings = await security.roleMappings.getAll();
       await Promise.all(existingMappings.map((m) => security.roleMappings.delete(m.name)));
 
