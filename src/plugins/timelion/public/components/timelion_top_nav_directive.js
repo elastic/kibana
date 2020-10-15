ce7fb5ae358fce7887d410bce27d388ce60c2ab0
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2020-10-15 13:57:30 +0300

    remove console msg

--- src/plugins/timelion/public/components/timelion_top_nav_directive.js
+++ src/plugins/timelion/public/components/timelion_top_nav_directive.js
@@ -20,7 +20,6 @@ import React from 'react';
 
 export function initTimelionTopNavDirective(app, deps) {
   app.directive('timelionTopNav', function (reactDirective) {
-    console.dir(deps);
     return reactDirective(
       (props) => {
         const { TopNavMenu } = deps.plugins.navigation.ui;
