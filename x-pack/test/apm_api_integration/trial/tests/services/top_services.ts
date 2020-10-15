d789ad20771eceecf1f482b3caffbe973bef49a8
James Gowdy <jgowdy@elastic.co>
2020-10-15 15:17:02 +0100

    disabling apm test

--- x-pack/test/apm_api_integration/trial/tests/services/top_services.ts
+++ x-pack/test/apm_api_integration/trial/tests/services/top_services.ts
@@ -5,7 +5,7 @@
  */
 
 import expect from '@kbn/expect';
-import { expectSnapshot } from '../../../common/match_snapshot';
+// import { expectSnapshot } from '../../../common/match_snapshot';
 import { PromiseReturnType } from '../../../../../plugins/apm/typings/common';
 import { FtrProviderContext } from '../../../common/ftr_provider_context';
 import archives_metadata from '../../../common/archives_metadata';
@@ -48,31 +48,33 @@ export default function ApiTest({ getService }: FtrProviderContext) {
             expect(response.body.items.length).to.be.greaterThan(0);
           });
 
-          it('some items have a health status set', () => {
-            // Under the assumption that the loaded archive has
-            // at least one APM ML job, and the time range is longer
-            // than 15m, at least one items should have a health status
-            // set. Note that we currently have a bug where healthy
-            // services report as unknown (so without any health status):
-            // https://github.com/elastic/kibana/issues/77083
+          // disabling test until apm_8.0.0 archive contains ML saved objects
 
-            const healthStatuses = response.body.items.map((item: any) => item.healthStatus);
+          // it('some items have a health status set', () => {
+          //   // Under the assumption that the loaded archive has
+          //   // at least one APM ML job, and the time range is longer
+          //   // than 15m, at least one items should have a health status
+          //   // set. Note that we currently have a bug where healthy
+          //   // services report as unknown (so without any health status):
+          //   // https://github.com/elastic/kibana/issues/77083
 
-            expect(healthStatuses.filter(Boolean).length).to.be.greaterThan(0);
+          //   const healthStatuses = response.body.items.map((item: any) => item.healthStatus);
 
-            expectSnapshot(healthStatuses).toMatchInline(`
-            Array [
-              "healthy",
-              undefined,
-              "healthy",
-              undefined,
-              "healthy",
-              "healthy",
-              "healthy",
-              "healthy",
-            ]
-          `);
-          });
+          //   expect(healthStatuses.filter(Boolean).length).to.be.greaterThan(0);
+
+          //   expectSnapshot(healthStatuses).toMatchInline(`
+          //   Array [
+          //     "healthy",
+          //     undefined,
+          //     "healthy",
+          //     undefined,
+          //     "healthy",
+          //     "healthy",
+          //     "healthy",
+          //     "healthy",
+          //   ]
+          // `);
+          // });
         });
       });
 
