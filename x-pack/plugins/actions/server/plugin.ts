ce643a6ca03e25cd8b4683d90e09450fc8303244
Justin Kambic <justin.kambic@elastic.co>
2020-10-15 22:55:31 -0400

    Add scoped cluster client to alerts and actions services.

--- x-pack/plugins/actions/server/plugin.ts
+++ x-pack/plugins/actions/server/plugin.ts
@@ -413,6 +413,7 @@ export class ActionsPlugin implements Plugin<Promise<PluginSetupContract>, Plugi
     return (request) => ({
       callCluster: elasticsearch.legacy.client.asScoped(request).callAsCurrentUser,
       savedObjectsClient: getScopedClient(request),
+      scopedClusterClient: elasticsearch.client.asScoped(request).asCurrentUser,
       getLegacyScopedClusterClient(clusterClient: ILegacyClusterClient) {
         return clusterClient.asScoped(request);
       },
