efca90538f13d7d254112af01f31cf6598d96cd8
restrry <restrry@gmail.com>
2020-10-16 12:35:59 +0300

    keep url rewritten in onPreRouting interceptor

--- src/core/server/http/integration_tests/lifecycle.test.ts
+++ src/core/server/http/integration_tests/lifecycle.test.ts
@@ -117,6 +117,45 @@ describe('OnPreRouting', () => {
     expect(urlAfterForwarding).toBe('/redirectUrl');
   });
 
+  it('provides original request url', async () => {
+    const { registerOnPreRouting, server: innerServer, createRouter } = await server.setup(
+      setupDeps
+    );
+    const router = createRouter('/');
+
+    router.get({ path: '/login', validate: false }, (context, req, res) => {
+      return res.ok({ body: { rewrittenUrl: req.rewrittenUrl?.path } });
+    });
+
+    registerOnPreRouting((req, res, t) => t.rewriteUrl('/login'));
+
+    await server.start();
+
+    await supertest(innerServer.listener)
+      .get('/initial?name=foo')
+      .expect(200, { rewrittenUrl: '/initial?name=foo' });
+  });
+
+  it('provides original request url if rewritten several times', async () => {
+    const { registerOnPreRouting, server: innerServer, createRouter } = await server.setup(
+      setupDeps
+    );
+    const router = createRouter('/');
+
+    router.get({ path: '/reroute-2', validate: false }, (context, req, res) => {
+      return res.ok({ body: { rewrittenUrl: req.rewrittenUrl?.path } });
+    });
+
+    registerOnPreRouting((req, res, t) => t.rewriteUrl('/reroute-1'));
+    registerOnPreRouting((req, res, t) => t.rewriteUrl('/reroute-2'));
+
+    await server.start();
+
+    await supertest(innerServer.listener)
+      .get('/initial?name=foo')
+      .expect(200, { rewrittenUrl: '/initial?name=foo' });
+  });
+
   it('supports redirection from the interceptor', async () => {
     const { registerOnPreRouting, server: innerServer, createRouter } = await server.setup(
       setupDeps
