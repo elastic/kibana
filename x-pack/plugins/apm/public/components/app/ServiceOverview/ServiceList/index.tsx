939227c9e37cf4c81a94e249b69434ecc50815e0
cauemarcondes <caue.marcondes@elastic.co>
2020-10-19 09:42:16 +0200

    reverting code

--- x-pack/plugins/apm/public/components/app/ServiceOverview/ServiceList/index.tsx
+++ x-pack/plugins/apm/public/components/app/ServiceOverview/ServiceList/index.tsx
@@ -212,11 +212,7 @@ export function ServiceList({ items, noItemsMessage }: Props) {
   return (
     <ManagedTable
       columns={columns}
-      items={items.map((item) => ({
-        ...item,
-        serviceName:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially",
-      }))}
+      items={items}
       noItemsMessage={noItemsMessage}
       initialSortField={initialSortField}
       initialSortDirection="desc"
