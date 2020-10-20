ea69e7f10670fd79401f1308e2b741448ce9f06f
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-10-20 17:17:30 +0200

    Unskip discover functional test

--- test/functional/apps/discover/_sidebar.js
+++ test/functional/apps/discover/_sidebar.js
@@ -26,7 +26,9 @@ export default function ({ getService, getPageObjects }) {
   const PageObjects = getPageObjects(['common', 'discover', 'timePicker']);
 
   // FLAKY: https://github.com/elastic/kibana/issues/80914
-  describe.skip('discover sidebar', function describeIndexTests() {
+
+  // eslint-disable-next-line mocha/no-exclusive-tests
+  describe.only('discover sidebar', function describeIndexTests() {
     before(async function () {
       // delete .kibana index and update configDoc
       await kibanaServer.uiSettings.replace({
