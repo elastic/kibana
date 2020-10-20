725ed285aafd8ee3a96c52abba714a1614e34036
FrankHassanabad <frank.hassanabad@elastic.co>
2020-10-20 09:55:12 -0600

    Adds back the original value for lists

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/threat_mapping/get_threat_list.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/threat_mapping/get_threat_list.ts
@@ -17,7 +17,7 @@ import {
 /**
  * This should not exceed 10000 (10k)
  */
-export const MAX_PER_PAGE = 200;
+export const MAX_PER_PAGE = 9000;
 
 export const getThreatList = async ({
   callCluster,
