113e0df578f55672c787f790b25976144f19d186
James Gowdy <jgowdy@elastic.co>
2020-10-20 17:06:45 +0100

    removing commented code

--- x-pack/plugins/ml/server/saved_objects/saved_objects.ts
+++ x-pack/plugins/ml/server/saved_objects/saved_objects.ts
@@ -8,38 +8,10 @@ import { SavedObjectsServiceSetup } from 'kibana/server';
 import mappings from './mappings.json';
 
 import { migrations } from './migrations';
-// import { EncryptedSavedObjectsPluginSetup } from '../../../encrypted_saved_objects/server';
 
 export const ML_SAVED_OBJECT_TYPE = 'ml-job';
 
-// const mappings = {
-//   properties: {
-//     job_id: {
-//       type: 'text',
-//       fields: {
-//         keyword: {
-//           type: 'keyword',
-//         },
-//       },
-//     },
-//     datafeed_id: {
-//       type: 'text',
-//       fields: {
-//         keyword: {
-//           type: 'keyword',
-//         },
-//       },
-//     },
-//     type: {
-//       type: 'keyword',
-//     },
-//   },
-// };
-
-export function setupSavedObjects(
-  savedObjects: SavedObjectsServiceSetup
-  // encryptedSavedObjects: EncryptedSavedObjectsPluginSetup
-) {
+export function setupSavedObjects(savedObjects: SavedObjectsServiceSetup) {
   savedObjects.registerType({
     name: ML_SAVED_OBJECT_TYPE,
     hidden: false,
@@ -47,16 +19,4 @@ export function setupSavedObjects(
     migrations,
     mappings: mappings.job,
   });
-
-  // Encrypted attributes
-  // encryptedSavedObjects.registerType({
-  //   type: 'alert',
-  //   attributesToEncrypt: new Set(['apiKey']),
-  //   attributesToExcludeFromAAD: new Set([
-  //     'scheduledTaskId',
-  //     'muteAll',
-  //     'mutedInstanceIds',
-  //     'updatedBy',
-  //   ]),
-  // });
 }
