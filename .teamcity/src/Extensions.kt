1dadd7efbdb4764d1f84adb03b553040cd6d84c2
Brian Seeders <brian.seeders@elastic.co>
2020-10-21 13:01:56 -0400

    Fix runbld heredoc

--- .teamcity/src/Extensions.kt
+++ .teamcity/src/Extensions.kt
@@ -145,43 +145,44 @@ fun BuildSteps.failedTestReporter(init: ScriptBuildStep.() -> Unit = {}) {
 fun BuildSteps.runbld(stepName: String, script: String) {
   script {
     name = stepName
+
+    // The indentation for this string is like this to ensure 100% that the RUNBLD-SCRIPT heredoc termination will not have spaces at the beginning
     scriptContent =
-      """
-        #!/bin/bash
-        branchName="${'$'}GIT_BRANCH"
-        branchName="${'$'}{branchName#refs\/heads\/}"
+"""#!/bin/bash
+branchName="${'$'}GIT_BRANCH"
+branchName="${'$'}{branchName#refs\/heads\/}"
 
-        if [[ "${'$'}GITHUB_PR_NUMBER" ]]; then
-          branchName=pull-request
-        fi
+if [[ "${'$'}GITHUB_PR_NUMBER" ]]; then
+  branchName=pull-request
+fi
 
-        project=kibana
-        if [[ "${'$'}ES_SNAPSHOT_MANIFEST" ]]; then
-          project=kibana-es-snapshot-verify
-        fi
+project=kibana
+if [[ "${'$'}ES_SNAPSHOT_MANIFEST" ]]; then
+  project=kibana-es-snapshot-verify
+fi
 
-        # These parameters are only for runbld reporting
-        export JENKINS_HOME="${'$'}HOME"
-        export BUILD_URL="%teamcity.serverUrl%/build/%teamcity.build.id%"
-        export branch_specifier=${'$'}branchName
-        export NODE_LABELS='teamcity'
-        export BUILD_NUMBER="%build.number%"
-        export EXECUTOR_NUMBER=''
-        export NODE_NAME=''
+# These parameters are only for runbld reporting
+export JENKINS_HOME="${'$'}HOME"
+export BUILD_URL="%teamcity.serverUrl%/build/%teamcity.build.id%"
+export branch_specifier=${'$'}branchName
+export NODE_LABELS='teamcity'
+export BUILD_NUMBER="%build.number%"
+export EXECUTOR_NUMBER=''
+export NODE_NAME=''
 
-        export OLD_PATH="${'$'}PATH"
+export OLD_PATH="${'$'}PATH"
 
-        file=${'$'}(mktemp)
+file=${'$'}(mktemp)
 
-        (
-        cat <<RUNBLD-SCRIPT
-        #!/bin/bash
-        export PATH="${'$'}OLD_PATH"
-        $script
-        RUNBLD-SCRIPT
-        ) > ${'$'}file
+(
+cat <<RUNBLD-SCRIPT
+#!/bin/bash
+export PATH="${'$'}OLD_PATH"
+$script
+RUNBLD-SCRIPT
+) > ${'$'}file
 
-        /usr/local/bin/runbld -d "${'$'}(pwd)" --job-name="elastic+${'$'}project+${'$'}branchName" ${'$'}file
-        """.trimIndent()
+/usr/local/bin/runbld -d "${'$'}(pwd)" --job-name="elastic+${'$'}project+${'$'}branchName" ${'$'}file
+"""
   }
 }
