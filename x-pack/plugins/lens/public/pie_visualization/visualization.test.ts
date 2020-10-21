4c48b23fee6fe11f31e3b3b93d51f8309f30858f
dej611 <dej611@gmail.com>
2020-10-21 18:10:40 +0200

    :white_check_mark: Add tests for pie and metric error messages

---
+++ x-pack/plugins/lens/public/pie_visualization/visualization.test.ts
@@ -0,0 +1,68 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { pieVisualization } from './visualization';
+import { PieVisualizationState } from './types';
+import { createMockDatasource, createMockFramePublicAPI } from '../editor_frame_service/mocks';
+import { DatasourcePublicAPI, FramePublicAPI } from '../types';
+
+jest.mock('../id_generator');
+
+const LAYER_ID = 'l1';
+
+function exampleState(): PieVisualizationState {
+  return {
+    shape: 'pie',
+    layers: [
+      {
+        layerId: LAYER_ID,
+        groups: [],
+        metric: undefined,
+        numberDisplay: 'percent',
+        categoryDisplay: 'default',
+        legendDisplay: 'default',
+        nestedLegend: false,
+      },
+    ],
+  };
+}
+
+function mockFrame(): FramePublicAPI {
+  return {
+    ...createMockFramePublicAPI(),
+    addNewLayer: () => LAYER_ID,
+    datasourceLayers: {
+      [LAYER_ID]: createMockDatasource(LAYER_ID).publicAPIMock,
+    },
+  };
+}
+
+// Just a basic bootstrap here to kickstart the tests
+describe('pie_visualization', () => {
+  describe('#getErrorMessage', () => {
+    it('returns undefined if no error is raised', () => {
+      const datasource: DatasourcePublicAPI = {
+        ...createMockDatasource('l1').publicAPIMock,
+        getOperationForColumnId(_: string) {
+          return {
+            id: 'a',
+            dataType: 'number',
+            isBucketed: false,
+            label: 'shazm',
+          };
+        },
+      };
+      const frame = {
+        ...mockFrame(),
+        datasourceLayers: { l1: datasource },
+      };
+
+      const error = pieVisualization.getErrorMessage(exampleState(), frame);
+
+      expect(error).not.toBeDefined();
+    });
+  });
+});
