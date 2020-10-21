6e64d57f317ca241f369334430971d12a098c6ba
oatkiller <robert.austin@elastic.co>
2020-10-21 15:56:13 -0400

    wip

--- x-pack/test/plugin_functional/test_suites/resolver/index.ts
+++ x-pack/test/plugin_functional/test_suites/resolver/index.ts
@@ -6,9 +6,14 @@
 
 import { FtrProviderContext } from '../../ftr_provider_context';
 
-export default function ({ getPageObjects, getService }: FtrProviderContext) {
+export default function ({
+  getPageObjects,
+  getService,
+  updateBaselines,
+}: FtrProviderContext & { updateBaselines: boolean }) {
   const pageObjects = getPageObjects(['common']);
   const testSubjects = getService('testSubjects');
+  const screenshot = getService('screenshots');
 
   describe('Resolver test app', function () {
     this.tags('ciGroup7');
@@ -22,6 +27,13 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       await testSubjects.existOrFail('resolver:node-list');
       await testSubjects.existOrFail('resolver:graph:edgeline');
       await testSubjects.existOrFail('resolver:graph-controls');
+      const nodeElement = await testSubjects.find('resolver:node');
+
+      // nodes: short name origin node with 13 pills, long name non-origin node, short name non-origin node
+      // node states: node is selected, node is unselected
+      // interaction states: node button is hovered, node button is focused, node button is hovered and focused, pill is focused, pill is hovered, pill is hovered and focused
+
+      await screenshot.compareAgainstBaseline('resolver_test_plugin', updateBaselines, nodeElement);
     });
   });
 }
