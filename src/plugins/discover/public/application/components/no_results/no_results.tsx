94d346eb180a9f3bd3e0852b5c55b6ce965d09be
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-10-21 07:09:48 +0200

    Update src/plugins/discover/public/application/components/no_results/no_results.tsx
    Co-authored-by: Andrea Del Rio <delrio.andre@gmail.com>

--- src/plugins/discover/public/application/components/no_results/no_results.tsx
+++ src/plugins/discover/public/application/components/no_results/no_results.tsx
@@ -69,7 +69,7 @@ export function DiscoverNoResults({
         iconType="alert"
         data-test-subj="discoverNoResultsError"
       >
-        <EuiButton size="s" onClick={() => (data ? data.search.showError(error) : void 0)}>
+        <EuiButton size="s" color="danger" onClick={() => (data ? data.search.showError(error) : void 0)}>
           <FormattedMessage
             id="discover.showErrorMessageAgain"
             defaultMessage="Show error message"
