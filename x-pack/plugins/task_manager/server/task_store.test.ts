bff9befd0280683427d6c66faf036ed835869f52
Patrick Mueller <pmuellr@gmail.com>
2020-10-22 11:09:56 -0400

    [task manager] do not sort tasks to be claimed by score if no pinned tasks (#80692)
    resolves: https://github.com/elastic/kibana/issues/80371
    
    Previously, when claiming tasks, we were always sorting the tasks to claim by
    the score and then by the time they should be run.  We sort by score to
    capture `runNow()` tasks, also referred to internally as "pinned" tasks
    in the update by query.
    
    The change in this PR is to only sort by score if there are pinned tasks, and
    to not sort by score at all if there aren't any.
    # Conflicts:
    #	x-pack/plugins/task_manager/server/task_store.test.ts

--- x-pack/plugins/task_manager/server/task_store.test.ts
+++ x-pack/plugins/task_manager/server/task_store.test.ts
@@ -306,9 +306,7 @@ describe('TaskStore', () => {
       const customMaxAttempts = _.random(44, 100);
       const {
         args: {
-          updateByQuery: {
-            body: { query },
-          },
+          updateByQuery: { body: { query, sort } = {} },
         },
       } = await testClaimAvailableTasks({
         opts: {
@@ -429,6 +427,25 @@ describe('TaskStore', () => {
           ],
         },
       });
+      expect(sort).toMatchObject([
+        {
+          _script: {
+            type: 'number',
+            order: 'asc',
+            script: {
+              lang: 'painless',
+              source: `
+if (doc['task.retryAt'].size()!=0) {
+  return doc['task.retryAt'].value.toInstant().toEpochMilli();
+}
+if (doc['task.runAt'].size()!=0) {
+  return doc['task.runAt'].value.toInstant().toEpochMilli();
+}
+    `,
+            },
+          },
+        },
+      ]);
     });
 
     test('it supports claiming specific tasks by id', async () => {
