ef0cf3bc64c7fef9be6207cff552fa4a9c143114
FrankHassanabad <frank.hassanabad@elastic.co>
2020-10-23 09:39:18 -0600

    Added some form reset logic for the threat match

--- x-pack/plugins/security_solution/public/detections/components/rules/step_define_rule/index.tsx
+++ x-pack/plugins/security_solution/public/detections/components/rules/step_define_rule/index.tsx
@@ -163,9 +163,19 @@ const StepDefineRuleComponent: FC<StepDefineRuleProps> = ({
   ] = useFetchIndex(threatIndex);
 
   // reset form when rule type changes
+  const { setFieldValue } = form;
   useEffect(() => {
+    if (isThreatMatchRule(ruleType)) {
+      setFieldValue('queryBar', {
+        query: { query: '*:*', language: 'kuery' },
+        filters: [],
+        saved_id: undefined,
+      });
+    } else {
+      setFieldValue('queryBar', stepDefineDefaultValue.queryBar);
+    }
     reset({ resetValues: false });
-  }, [reset, ruleType]);
+  }, [reset, ruleType, setFieldValue]);
 
   useEffect(() => {
     setIndexModified(!isEqual(index, indicesConfig));
