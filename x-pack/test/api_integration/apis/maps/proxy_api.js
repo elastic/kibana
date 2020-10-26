d0740e23701ccddad19a9dc624edcb78b2fbd3de
Thomas Neirynck <thomas@elastic.co>
2020-10-26 17:29:06 -0400

    make tests more robust

--- x-pack/test/api_integration/apis/maps/proxy_api.js
+++ x-pack/test/api_integration/apis/maps/proxy_api.js
@@ -16,11 +16,11 @@ export default function ({ getService }) {
         .set('kbn-xsrf', 'kibana')
         .expect(200);
 
-      expect(resp.body.layers.length).to.be(66);
+      expect(resp.body.layers.length).to.be.greaterThan(0);
 
       //Check world-layer
       const worldLayer = resp.body.layers.find((layer) => layer.layer_id === 'world_countries');
-      expect(worldLayer.formats.length).to.be(1);
+      expect(worldLayer.formats.length).to.be.greaterThan(0);
       expect(worldLayer.formats[0].type).to.be('geojson');
       expect(worldLayer.formats[0].url).to.be('file?id=world_countries');
     });
