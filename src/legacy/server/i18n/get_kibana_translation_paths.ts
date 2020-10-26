1fc4de48f83b12f089b6aaac81c425fed07efe4a
Ahmad Bamieh <ahmadbamieh@gmail.com>
2020-10-26 16:34:32 +0200

    add kibana_translation_paths tests

--- src/legacy/server/i18n/get_kibana_translation_paths.ts
+++ src/legacy/server/i18n/get_kibana_translation_paths.ts
@@ -17,4 +17,26 @@
  * under the License.
  */
 
-export { i18nMixin } from './i18n_mixin';
+import { KibanaConfig } from '../kbn_server';
+import { fromRoot } from '../../../core/server/utils';
+import { I18N_RC } from './constants';
+import { getTranslationPaths } from './get_translation_paths';
+
+export async function getKibanaTranslatoinPaths(config: KibanaConfig) {
+  return await Promise.all([
+    getTranslationPaths({
+      cwd: fromRoot('.'),
+      glob: `*/${I18N_RC}`,
+    }),
+    ...(config.get('plugins.paths') as string[]).map((cwd) =>
+      getTranslationPaths({ cwd, glob: I18N_RC })
+    ),
+    ...(config.get('plugins.scanDirs') as string[]).map((cwd) =>
+      getTranslationPaths({ cwd, glob: `*/${I18N_RC}` })
+    ),
+    getTranslationPaths({
+      cwd: fromRoot('../kibana-extra'),
+      glob: `*/${I18N_RC}`,
+    }),
+  ]);
+}
