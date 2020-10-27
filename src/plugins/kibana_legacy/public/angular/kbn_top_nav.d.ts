c9bd1a335b8aa3627ad3871aac900d888eaad8ef
Nathan L Smith <nathan.smith@elastic.co>
2020-10-27 18:21:45 -0500

    Add tsconfig for url_forwarding (#81177) (#81854)
    Also add missing pieces to kibana_react, as a follow-up to #80992.
    
    References #80508
    References #81003

--- src/plugins/kibana_legacy/public/angular/kbn_top_nav.d.ts
+++ src/plugins/kibana_legacy/public/angular/kbn_top_nav.d.ts
@@ -17,8 +17,15 @@
  * under the License.
  */
 
-import { IServiceProvider } from 'angular';
+import { Injectable, IDirectiveFactory, IScope, IAttributes, IController } from 'angular';
 
-export type IPrivate = <T>(provider: (...injectable: any[]) => T) => T;
-
-export function PrivateProvider(): IServiceProvider;
+export const createTopNavDirective: Injectable<IDirectiveFactory<
+  IScope,
+  JQLite,
+  IAttributes,
+  IController
+>>;
+export const createTopNavHelper: (
+  options: unknown
+) => Injectable<IDirectiveFactory<IScope, JQLite, IAttributes, IController>>;
+export function loadKbnTopNavDirectives(navUi: unknown): void;
