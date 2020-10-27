348e045d1160a5bb166af340ddfb119403900e51
Joe Reuter <johannes.reuter@elastic.co>
2020-10-27 11:49:57 +0100

    fix order of functional tests

--- x-pack/test/functional/apps/lens/index.ts
+++ x-pack/test/functional/apps/lens/index.ts
@@ -30,8 +30,8 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
       loadTestFile(require.resolve('./smokescreen'));
       loadTestFile(require.resolve('./dashboard'));
       loadTestFile(require.resolve('./persistent_context'));
-      loadTestFile(require.resolve('./lens_reporting'));
       loadTestFile(require.resolve('./colors'));
+      loadTestFile(require.resolve('./lens_reporting'));
 
       // has to be last one in the suite because it overrides saved objects
       loadTestFile(require.resolve('./rollup'));
