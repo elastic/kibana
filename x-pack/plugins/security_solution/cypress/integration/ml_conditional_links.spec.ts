ef0fb7232f30cbb705cc738ae79d32cee65cb8b2
Patryk Kopyciński <contact@patrykkopycinski.com>
2020-10-27 13:42:24 +0100

    Update ml_conditional_links.spec.ts

--- x-pack/plugins/security_solution/cypress/integration/ml_conditional_links.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/ml_conditional_links.spec.ts
@@ -24,7 +24,7 @@ import {
   mlNetworkSingleIpNullKqlQuery,
 } from '../urls/ml_conditional_links';
 
-describe('ml conditional links', () => {
+describe.skip('ml conditional links', () => {
   it('sets the KQL from a single IP with a value for the query', () => {
     loginAndWaitForPageWithoutDateRange(mlNetworkSingleIpKqlQuery);
     cy.get(KQL_INPUT)
