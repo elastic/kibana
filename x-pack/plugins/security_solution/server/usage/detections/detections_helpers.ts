elastic/kibana

commit 4762cf56f54b5f6fba17dbe4cdc8b18607b2dc61

[ML] Switching to new es client (#74965)
* [ML] Switching to new es client

* further conversions

* fixing tests

* updating responses

* test commit

* refactoring shared services to removed context parameter

* removing last scoped clients

* removing ml js client

* udating file data viz errors

* fixing jest tests

* fixing types after merge with master

* error response changes

* adding default sizes to some requests

* adding custom error types for license checks

* tidying up shared function checks

* removing test data

* removing need for DummyKibanaRequest

* updating comment

* fixing functional api tests

* removing comments

* fixing types after master merge

* throw error rather than return it

* removing placeholder error

* changes based on review comments

* fixing types after merge with master

* fixing missing return

Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

x-pack/plugins/security_solution/server/usage/detections/detections_helpers.ts
@@ -172,18 +172,12 @@ export const getMlJobsUsage = async (ml: MlPluginSetup | undefined): Promise<MlJ
 
   if (ml) {
     try {
-      const fakeRequest = { headers: {}, params: 'DummyKibanaRequest' } as KibanaRequest;
+      const fakeRequest = {} as KibanaRequest;
       const fakeSOClient = {} as SavedObjectsClient;
-      const internalMlClient = {
-        callAsCurrentUser: ml?.mlClient.callAsInternalUser,
-        callAsInternalUser: ml?.mlClient.callAsInternalUser,
-      };
 
-      const modules = await ml
-        .modulesProvider(internalMlClient, fakeRequest, fakeSOClient)
-        .listModules();
+      const modules = await ml.modulesProvider(fakeRequest, fakeSOClient).listModules();
       const moduleJobs = modules.flatMap((module) => module.jobs);
-      const jobs = await ml.jobServiceProvider(internalMlClient, fakeRequest).jobsSummary();
+      const jobs = await ml.jobServiceProvider(fakeRequest).jobsSummary();
 
       jobsUsage = jobs.filter(isSecurityJob).reduce((usage, job) => {
         const isElastic = moduleJobs.some((moduleJob) => moduleJob.id === job.id);

commit c7dacd64c3aa8d725b24c3edb60f0b69bdfe59c2

Fix regression in our ml usage collection
A regression was introduced in #74965 that caused an error to be
thrown while collecting telemetry on ML jobs. Because such exceptions
are caught and we degrade to zeroing out those counts, this one was not
caught until manual testing of telemetry.

x-pack/plugins/security_solution/server/usage/detections/detections_helpers.ts
@@ -172,7 +172,7 @@ export const getMlJobsUsage = async (ml: MlPluginSetup | undefined): Promise<MlJ
 
   if (ml) {
     try {
-      const fakeRequest = {} as KibanaRequest;
+      const fakeRequest = { headers: {} } as KibanaRequest;
       const fakeSOClient = {} as SavedObjectsClient;
 
       const modules = await ml.modulesProvider(fakeRequest, fakeSOClient).listModules();