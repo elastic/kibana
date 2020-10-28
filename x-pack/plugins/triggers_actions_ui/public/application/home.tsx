c2fe4b1d43a15bdcb63c76cbd303af1016a9938e
Mike Cote <mikecote@users.noreply.github.com>
2020-10-28 10:58:04 -0400

    Add a link to documentation in the alerts and actions management UI

--- x-pack/plugins/triggers_actions_ui/public/application/home.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/home.tsx
@@ -10,14 +10,15 @@ import { FormattedMessage } from '@kbn/i18n/react';
 import {
   EuiPageBody,
   EuiPageContent,
-  EuiPageContentHeader,
-  EuiPageContentHeaderSection,
   EuiSpacer,
   EuiTab,
   EuiTabs,
   EuiTitle,
   EuiBetaBadge,
   EuiText,
+  EuiButtonEmpty,
+  EuiFlexGroup,
+  EuiFlexItem,
 } from '@elastic/eui';
 
 import { i18n } from '@kbn/i18n';
@@ -33,7 +34,7 @@ import { PLUGIN } from './constants/plugin';
 import { HealthCheck } from './components/health_check';
 import { HealthContextProvider } from './context/health_context';
 
-interface MatchParams {
+export interface MatchParams {
   section: Section;
 }
 
@@ -83,9 +84,9 @@ export const TriggersActionsUIHome: React.FunctionComponent<RouteComponentProps<
   return (
     <EuiPageBody>
       <EuiPageContent>
-        <EuiPageContentHeader>
-          <EuiPageContentHeaderSection>
-            <EuiTitle size="m">
+        <EuiTitle size="m">
+          <EuiFlexGroup>
+            <EuiFlexItem>
               <h1 data-test-subj="appTitle">
                 <FormattedMessage
                   id="xpack.triggersActionsUI.home.appTitle"
@@ -106,18 +107,31 @@ export const TriggersActionsUIHome: React.FunctionComponent<RouteComponentProps<
                   )}
                 />
               </h1>
-            </EuiTitle>
-            <EuiSpacer size="s" />
-            <EuiText>
-              <p>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty
+                href={`${docLinks.ELASTIC_WEBSITE_URL}guide/en/kibana/${docLinks.DOC_LINK_VERSION}/managing-alerts-and-actions.html`}
+                target="_blank"
+                iconType="help"
+                data-test-subj="documentationLink"
+              >
                 <FormattedMessage
-                  id="xpack.triggersActionsUI.home.sectionDescription"
-                  defaultMessage="Detect conditions using alerts, and take actions using connectors."
+                  id="xpack.triggersActionsUI.home.alertsAndActionsDocsLinkText"
+                  defaultMessage="Alerts and Actions docs"
                 />
-              </p>
-            </EuiText>
-          </EuiPageContentHeaderSection>
-        </EuiPageContentHeader>
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiTitle>
+        <EuiSpacer size="s" />
+        <EuiText>
+          <p>
+            <FormattedMessage
+              id="xpack.triggersActionsUI.home.sectionDescription"
+              defaultMessage="Detect conditions using alerts, and take actions using connectors."
+            />
+          </p>
+        </EuiText>
 
         <EuiTabs>
           {tabs.map((tab) => (
