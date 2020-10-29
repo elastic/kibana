d49ea2271b9ad3b6d04c26e0673d0db168dc5c1d
Wylie Conlon <william.conlon@elastic.co>
2020-10-29 10:48:49 -0400

    [Lens] Stop using multi-level metrics in Lens pie charts (#81523)
    * [Lens] Stop using multi-level metrics in Lens
    
    * Fix linting
    
    * Simplify even more
    # Conflicts:
    #	x-pack/plugins/lens/public/pie_visualization/render_helpers.test.ts
    #	x-pack/plugins/lens/public/pie_visualization/render_helpers.ts
    #	x-pack/plugins/lens/public/pie_visualization/types.ts

--- x-pack/plugins/lens/public/pie_visualization/render_helpers.ts
+++ x-pack/plugins/lens/public/pie_visualization/render_helpers.ts
@@ -5,23 +5,14 @@
  */
 
 import { Datum, LayerValue } from '@elastic/charts';
-import { KibanaDatatable, KibanaDatatableColumn } from 'src/plugins/expressions/public';
-import { ColumnGroups } from './types';
+import { Datatable, DatatableColumn } from 'src/plugins/expressions/public';
 import { LensFilterEvent } from '../types';
 
-export function getSliceValueWithFallback(
-  d: Datum,
-  reverseGroups: ColumnGroups,
-  metricColumn: KibanaDatatableColumn
-) {
+export function getSliceValue(d: Datum, metricColumn: DatatableColumn) {
   if (typeof d[metricColumn.id] === 'number' && d[metricColumn.id] !== 0) {
     return d[metricColumn.id];
   }
-  // Sometimes there is missing data for outer groups
-  // When there is missing data, we fall back to the next groups
-  // This creates a sunburst effect
-  const hasMetric = reverseGroups.find((group) => group.metrics.length && d[group.metrics[0].id]);
-  return hasMetric ? d[hasMetric.metrics[0].id] || Number.EPSILON : Number.EPSILON;
+  return Number.EPSILON;
 }
 
 export function getFilterContext(
