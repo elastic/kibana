509aace97110e1dccc07e137ad265e8acf8414f7
Bohdan Tsymbala <bohdan.tsymbala@elastic.co>
2020-10-30 20:17:44 +0100

    Fixed the visual alignment of titles in the form and added responsiveness.

--- x-pack/plugins/security_solution/public/management/pages/policy/view/policy_forms/config_form.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/policy_forms/config_form.tsx
@@ -4,30 +4,22 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FC, ReactNode, memo, useMemo } from 'react';
+import React, { FC, ReactNode, memo } from 'react';
+import { i18n } from '@kbn/i18n';
 import {
-  EuiCard,
   EuiFlexGroup,
   EuiFlexItem,
   EuiTitle,
   EuiHorizontalRule,
   EuiText,
+  EuiShowFor,
+  EuiPanel,
 } from '@elastic/eui';
-import { FormattedMessage } from '@kbn/i18n/react';
-import styled from 'styled-components';
 
 import { OperatingSystem } from '../../../../../../common/endpoint/types';
 import { OS_TITLES } from '../../../../common/translations';
 
-const PolicyDetailCard = styled.div`
-  .policyDetailTitleOS {
-    flex-grow: 2;
-  }
-  .policyDetailTitleFlexItem {
-    margin: 0;
-  }
-`;
-export const ConfigForm: FC<{
+interface ConfigFormProps {
   /**
    * A subtitle for this component.
    **/
@@ -39,53 +31,49 @@ export const ConfigForm: FC<{
   dataTestSubj?: string;
   /** React Node to be put on the right corner of the card */
   rightCorner?: ReactNode;
-}> = memo(({ type, supportedOss, children, dataTestSubj, rightCorner }) => {
-  const typeTitle = useMemo(() => {
-    return (
-      <EuiFlexGroup direction="row" gutterSize="none" alignItems="center">
-        <EuiFlexGroup direction="column" gutterSize="none">
-          <EuiFlexItem className="policyDetailTitleFlexItem">
-            <EuiTitle size="xxxs">
-              <h6>
-                <FormattedMessage
-                  id="xpack.securitySolution.endpoint.policyDetailType"
-                  defaultMessage="Type"
-                />
-              </h6>
-            </EuiTitle>
-          </EuiFlexItem>
-          <EuiFlexItem className="policyDetailTitleFlexItem">
-            <EuiText size="m">{type}</EuiText>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiFlexGroup direction="column" gutterSize="none" className="policyDetailTitleOS">
-          <EuiFlexItem className="policyDetailTitleFlexItem">
-            <EuiTitle size="xxxs">
-              <h6>
-                <FormattedMessage
-                  id="xpack.securitySolution.endpoint.policyDetailOS"
-                  defaultMessage="Operating System"
-                />
-              </h6>
-            </EuiTitle>
-          </EuiFlexItem>
-          <EuiFlexItem className="policyDetailTitleFlexItem">
-            <EuiText>{supportedOss.map((os) => OS_TITLES[os]).join(', ')}</EuiText>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiFlexItem grow={false}>{rightCorner}</EuiFlexItem>
-      </EuiFlexGroup>
-    );
-  }, [rightCorner, supportedOss, type]);
+}
 
-  return (
-    <PolicyDetailCard>
-      <EuiCard description data-test-subj={dataTestSubj} textAlign="left" title={typeTitle}>
-        <EuiHorizontalRule margin="m" />
-        {children}
-      </EuiCard>
-    </PolicyDetailCard>
-  );
-});
+export const ConfigForm: FC<ConfigFormProps> = memo(
+  ({ type, supportedOss, dataTestSubj, rightCorner, children }) => (
+    <EuiPanel data-test-subj={dataTestSubj}>
+      <EuiFlexGroup direction="row" gutterSize="none" alignItems="center">
+        <EuiFlexItem>
+          <EuiTitle size="xxxs">
+            <h6>
+              {i18n.translate('xpack.securitySolution.endpoint.policyDetailType', {
+                defaultMessage: 'Type',
+              })}
+            </h6>
+          </EuiTitle>
+          <EuiText size="m">{type}</EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={2}>
+          <EuiTitle size="xxxs">
+            <h6>
+              {i18n.translate('xpack.securitySolution.endpoint.policyDetailOS', {
+                defaultMessage: 'Operating System',
+              })}
+            </h6>
+          </EuiTitle>
+          <EuiText>{supportedOss.map((os) => OS_TITLES[os]).join(', ')}</EuiText>
+        </EuiFlexItem>
+        <EuiShowFor sizes={['m', 'l', 'xl']}>
+          <EuiFlexItem>
+            <EuiFlexGroup direction="row" gutterSize="none" justifyContent="flexEnd">
+              <EuiFlexItem grow={false}>{rightCorner}</EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiShowFor>
+        <EuiShowFor sizes={rightCorner ? ['s', 'xs'] : []}>
+          <EuiFlexItem>{rightCorner}</EuiFlexItem>
+        </EuiShowFor>
+      </EuiFlexGroup>
+
+      <EuiHorizontalRule margin="m" />
+
+      {children}
+    </EuiPanel>
+  )
+);
 
 ConfigForm.displayName = 'ConfigForm';
