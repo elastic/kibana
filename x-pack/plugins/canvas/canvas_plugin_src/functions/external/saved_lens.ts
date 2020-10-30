49f22949a0e50bb1e6108c0e40a96e40defa988d
Joe Reuter <johannes.reuter@elastic.co>
2020-10-30 10:41:37 +0100

    review comments

--- x-pack/plugins/canvas/canvas_plugin_src/functions/external/saved_lens.ts
+++ x-pack/plugins/canvas/canvas_plugin_src/functions/external/saved_lens.ts
@@ -66,7 +66,7 @@ export function savedLens(): ExpressionFunctionDefinition<
       },
       palette: {
         types: ['palette'],
-        help: 'abc',
+        help: argHelp.palette!,
         required: false,
       },
     },
