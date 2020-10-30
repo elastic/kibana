56ab2bc895603def4e634258864c58ead2d2eac9
Sébastien Loix <sabee77@gmail.com>
2020-10-30 15:55:40 +0100

    Fix typo in test

--- x-pack/plugins/runtime_fields/public/components/runtime_field_editor/runtime_field_editor.test.tsx
+++ x-pack/plugins/runtime_fields/public/components/runtime_field_editor/runtime_field_editor.test.tsx
@@ -20,7 +20,7 @@ const setup = (props?: Props) =>
   })(props) as TestBed;
 
 const docLinks: DocLinksStart = {
-  ELASTIC_WEBSITE_URL: 'htts://jestTest.elastic.co',
+  ELASTIC_WEBSITE_URL: 'https://jestTest.elastic.co',
   DOC_LINK_VERSION: 'jest',
   links: {} as any,
 };
