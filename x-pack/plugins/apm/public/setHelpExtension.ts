b9481271009db1994aacc712f14a9b96088a3a55
Nathan L Smith <smith@nlsmith.com>
2020-10-30 09:47:09 -0500

    Remove unused import

--- x-pack/plugins/apm/public/setHelpExtension.ts
+++ x-pack/plugins/apm/public/setHelpExtension.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import url from 'url';
 import { i18n } from '@kbn/i18n';
 import { CoreStart } from 'kibana/public';
 
