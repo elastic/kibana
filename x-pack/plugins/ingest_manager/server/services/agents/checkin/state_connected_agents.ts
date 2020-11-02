700225061a3c736aec36ddc12e9669b213894d3b
Thomas Watson <w@tson.dk>
2020-11-02 13:18:00 +0100

    Upgrade to hapi version 18 (#80468)

--- x-pack/plugins/ingest_manager/server/services/agents/checkin/state_connected_agents.ts
+++ x-pack/plugins/ingest_manager/server/services/agents/checkin/state_connected_agents.ts
@@ -23,12 +23,6 @@ function getInternalUserSOClient() {
         url: '/',
       },
     },
-    // TODO: Remove once we upgrade to hapi v18
-    _core: {
-      info: {
-        uri: 'http://localhost',
-      },
-    },
   } as unknown) as KibanaRequest;
 
   return appContextService.getInternalUserSOClient(fakeRequest);
