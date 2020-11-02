eb5c578349cf40775a4dfb2cf778f1a50c29a96d
Joe Reuter <johannes.reuter@elastic.co>
2020-11-02 10:30:44 +0100

    Add derivative function (#81178)

--- src/plugins/expressions/common/expression_functions/specs/derivative.ts
+++ src/plugins/expressions/common/expression_functions/specs/derivative.ts
@@ -22,83 +22,84 @@ import { ExpressionFunctionDefinition } from '../types';
 import { Datatable } from '../../expression_types';
 import { buildResultColumns, getBucketIdentifier } from './series_calculation_helpers';
 
-export interface CumulativeSumArgs {
+export interface DerivativeArgs {
   by?: string[];
   inputColumnId: string;
   outputColumnId: string;
   outputColumnName?: string;
 }
 
-export type ExpressionFunctionCumulativeSum = ExpressionFunctionDefinition<
-  'cumulative_sum',
+export type ExpressionFunctionDerivative = ExpressionFunctionDefinition<
+  'derivative',
   Datatable,
-  CumulativeSumArgs,
+  DerivativeArgs,
   Datatable
 >;
 
 /**
- * Calculates the cumulative sum of a specified column in the data table.
+ * Calculates the derivative of a specified column in the data table.
  *
  * Also supports multiple series in a single data table - use the `by` argument
  * to specify the columns to split the calculation by.
- * For each unique combination of all `by` columns a separate cumulative sum will be calculated.
+ * For each unique combination of all `by` columns a separate derivative will be calculated.
  * The order of rows won't be changed - this function is not modifying any existing columns, it's only
  * adding the specified `outputColumnId` column to every row of the table without adding or removing rows.
  *
  * Behavior:
- * * Will write the cumulative sum of `inputColumnId` into `outputColumnId`
+ * * Will write the derivative of `inputColumnId` into `outputColumnId`
  * * If provided will use `outputColumnName` as name for the newly created column. Otherwise falls back to `outputColumnId`
- * * Cumulative sums always start with 0, a cell will contain its own value plus the values of
- *   all cells of the same series further up in the table.
+ * * Derivative always start with an undefined value for the first row of a series, a cell will contain its own value minus the
+ *   value of the previous cell of the same series.
  *
  * Edge cases:
  * * Will return the input table if `inputColumnId` does not exist
  * * Will throw an error if `outputColumnId` exists already in provided data table
- * * If the row value contains `null` or `undefined`, it will be ignored and overwritten with the cumulative sum of
- *   all cells of the same series further up in the table.
- * * For all values besides `null` and `undefined`, the value will be cast to a number before it's added to the
- *   cumulative sum of the current series - if this results in `NaN` (like in case of objects), all cells of the
- *   current series will be set to `NaN`.
+ * * If there is no previous row of the current series with a non `null` or `undefined` value, the output cell of the current row
+ *   will be set to `undefined`.
+ * * If the row value contains `null` or `undefined`, it will be ignored and the output cell will be set to `undefined`
+ * * If the value of the previous row of the same series contains `null` or `undefined`, the output cell of the current row will be set to `undefined` as well
+ * * For all values besides `null` and `undefined`, the value will be cast to a number before it's used in the
+ *   calculation of the current series even if this results in `NaN` (like in case of objects).
  * * To determine separate series defined by the `by` columns, the values of these columns will be cast to strings
  *   before comparison. If the values are objects, the return value of their `toString` method will be used for comparison.
  *   Missing values (`null` and `undefined`) will be treated as empty strings.
  */
-export const cumulativeSum: ExpressionFunctionCumulativeSum = {
-  name: 'cumulative_sum',
+export const derivative: ExpressionFunctionDerivative = {
+  name: 'derivative',
   type: 'datatable',
 
   inputTypes: ['datatable'],
 
-  help: i18n.translate('expressions.functions.cumulativeSum.help', {
-    defaultMessage: 'Calculates the cumulative sum of a column in a data table',
+  help: i18n.translate('expressions.functions.derivative.help', {
+    defaultMessage: 'Calculates the derivative of a column in a data table',
   }),
 
   args: {
     by: {
-      help: i18n.translate('expressions.functions.cumulativeSum.args.byHelpText', {
-        defaultMessage: 'Column to split the cumulative sum calculation by',
+      help: i18n.translate('expressions.functions.derivative.args.byHelpText', {
+        defaultMessage: 'Column to split the derivative calculation by',
       }),
       multi: true,
       types: ['string'],
       required: false,
     },
     inputColumnId: {
-      help: i18n.translate('expressions.functions.cumulativeSum.args.inputColumnIdHelpText', {
-        defaultMessage: 'Column to calculate the cumulative sum of',
+      help: i18n.translate('expressions.functions.derivative.args.inputColumnIdHelpText', {
+        defaultMessage: 'Column to calculate the derivative of',
       }),
       types: ['string'],
       required: true,
     },
     outputColumnId: {
-      help: i18n.translate('expressions.functions.cumulativeSum.args.outputColumnIdHelpText', {
-        defaultMessage: 'Column to store the resulting cumulative sum in',
+      help: i18n.translate('expressions.functions.derivative.args.outputColumnIdHelpText', {
+        defaultMessage: 'Column to store the resulting derivative in',
       }),
       types: ['string'],
       required: true,
     },
     outputColumnName: {
-      help: i18n.translate('expressions.functions.cumulativeSum.args.outputColumnNameHelpText', {
-        defaultMessage: 'Name of the column to store the resulting cumulative sum in',
+      help: i18n.translate('expressions.functions.derivative.args.outputColumnNameHelpText', {
+        defaultMessage: 'Name of the column to store the resulting derivative in',
       }),
       types: ['string'],
       required: false,
@@ -117,7 +118,7 @@ export const cumulativeSum: ExpressionFunctionCumulativeSum = {
       return input;
     }
 
-    const accumulators: Partial<Record<string, number>> = {};
+    const previousValues: Partial<Record<string, number>> = {};
     return {
       ...input,
       columns: resultColumns,
@@ -125,13 +126,19 @@ export const cumulativeSum: ExpressionFunctionCumulativeSum = {
         const newRow = { ...row };
 
         const bucketIdentifier = getBucketIdentifier(row, by);
-        const accumulatorValue = accumulators[bucketIdentifier] ?? 0;
+        const previousValue = previousValues[bucketIdentifier];
         const currentValue = newRow[inputColumnId];
-        if (currentValue != null) {
-          newRow[outputColumnId] = Number(currentValue) + accumulatorValue;
-          accumulators[bucketIdentifier] = newRow[outputColumnId];
+
+        if (currentValue != null && previousValue != null) {
+          newRow[outputColumnId] = Number(currentValue) - previousValue;
         } else {
-          newRow[outputColumnId] = accumulatorValue;
+          newRow[outputColumnId] = undefined;
+        }
+
+        if (currentValue != null) {
+          previousValues[bucketIdentifier] = Number(currentValue);
+        } else {
+          previousValues[bucketIdentifier] = undefined;
         }
 
         return newRow;
