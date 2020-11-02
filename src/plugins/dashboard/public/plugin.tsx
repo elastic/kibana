2b92c1c6f9a2239098aface711092e2b69171eb3
Michail Yasonik <michail.yasonik@elastic.co>
2020-11-02 16:09:01 -0500

    fixing nav order... again

--- src/plugins/dashboard/public/plugin.tsx
+++ src/plugins/dashboard/public/plugin.tsx
@@ -300,7 +300,7 @@ export class DashboardPlugin
     const app: App = {
       id: DashboardConstants.DASHBOARDS_ID,
       title: 'Dashboard',
-      order: -1001,
+      order: 2500,
       euiIconType: 'logoKibana',
       defaultPath: `#${DashboardConstants.LANDING_PAGE_PATH}`,
       updater$: this.appStateUpdater,
