4e55f61a721f7529d82ff76942fe8629db9944e3
John Schulz <john.schulz@elastic.co>
2020-11-03 17:47:43 -0500

    Update agent_policy.ts

--- x-pack/plugins/ingest_manager/common/constants/agent_policy.ts
+++ x-pack/plugins/ingest_manager/common/constants/agent_policy.ts
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { defaultPackages } from './index';
+import { defaultPackages } from './epm';
 import { AgentPolicy } from '../types';
 export const AGENT_POLICY_SAVED_OBJECT_TYPE = 'ingest-agent-policies';
 
