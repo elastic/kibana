fae0b205ba9f4b477027ad8a61eb2b8cbb17add4
Maja Grubic <maja.grubic@elastic.co>
2020-11-04 13:30:10 +0000

    Testing something

--- test/functional/apps/discover/index.js
+++ test/functional/apps/discover/index.js
@@ -48,6 +48,5 @@ export default function ({ getService, loadTestFile }) {
     loadTestFile(require.resolve('./_doc_navigation'));
     loadTestFile(require.resolve('./_date_nanos'));
     loadTestFile(require.resolve('./_date_nanos_mixed'));
-    loadTestFile(require.resolve('./_indexpattern_without_timefield'));
   });
 }
