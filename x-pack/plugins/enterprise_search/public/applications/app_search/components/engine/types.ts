3f049d57a013ac1b0496707ccc61abba617b6664
Constance Chen <constance.chen.3@gmail.com>
2020-11-04 10:35:36 -0800

    Set up required types
    - creating new schema types for now as part of engine details
    
    + update shared IEngine type

---
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/engine/types.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { ApiToken } from '../credentials/types';
+import { Schema, SchemaConflicts, IndexingStatus } from '../schema/types';
+
+export interface Engine {
+  name: string;
+  type: string;
+  language: string | null;
+  result_fields: {
+    [key: string]: ResultField;
+  };
+}
+
+export interface EngineDetails extends Engine {
+  created_at: string;
+  document_count: number;
+  field_count: number;
+  unsearchedUnconfirmedFields: boolean;
+  apiTokens: ApiToken[];
+  apiKey: string;
+  schema: Schema;
+  schemaConflicts?: SchemaConflicts;
+  activeReindexJob?: IndexingStatus;
+  invalidBoosts: boolean;
+  sample?: boolean;
+  isMeta: boolean;
+  engine_count?: number;
+  includedEngines?: EngineDetails[];
+}
+
+interface ResultField {
+  raw: object;
+  snippet?: {
+    size: number;
+    fallback: boolean;
+  };
+}
