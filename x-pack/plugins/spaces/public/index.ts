27efbe383d944d2101a1e1c74427bad0522b1620
James Gowdy <jgowdy@elastic.co>
2020-11-05 12:33:49 +0000

    removing accidentally added export

--- x-pack/plugins/spaces/public/index.ts
+++ x-pack/plugins/spaces/public/index.ts
@@ -14,8 +14,6 @@ export { SpaceAvatar, getSpaceColor, getSpaceImageUrl, getSpaceInitials } from '
 
 export { SpacesPluginSetup, SpacesPluginStart } from './plugin';
 
-export { SpacesManager } from './spaces_manager';
-
 export const plugin = () => {
   return new SpacesPlugin();
 };
