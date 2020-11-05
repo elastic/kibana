4995cc61f2f60d456ca503d26678c9a13acbd59e
Devin Hurley <devin.hurley@elastic.co>
2020-11-05 12:11:21 -0500

    updated scripts to accept filepath to role / user, added a test to ensure upload value list button is disabled

--- x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/t1_analyst/post_detections_role.sh
+++ x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/t1_analyst/post_detections_role.sh
@@ -1,4 +1,7 @@
+# Uses a default if no argument is specified
+ROLE=(${@:-./detections_role.json})
+
 curl -H 'Content-Type: application/json' -H 'kbn-xsrf: 123'\
  -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} \
 -XPUT ${KIBANA_URL}/api/security/role/t1_analyst \
--d @detections_role.json
+-d @${ROLE}
