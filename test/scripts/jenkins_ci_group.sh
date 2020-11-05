8e9876ef5938a646bb7d753fe7ef07a3119dd1c9
Dmitry <dzmitry.lemechko@elastic.co>
2020-11-05 13:17:33 +0100

    oss tests: replace grunt task with node script (#82371) (#82633)
    # Conflicts:
    #	tasks/function_test_groups.js
    #	test/scripts/jenkins_ci_group.sh

--- test/scripts/jenkins_ci_group.sh
+++ test/scripts/jenkins_ci_group.sh
@@ -2,7 +2,11 @@
 
 source test/scripts/jenkins_test_setup_oss.sh
 
-checks-reporter-with-killswitch "Functional tests / Group ${CI_GROUP}" yarn run grunt "run:functionalTests_ciGroup${CI_GROUP}";
+checks-reporter-with-killswitch "Functional tests / Group ${CI_GROUP}" \
+  node scripts/functional_tests \
+    --debug --bail \
+    --kibana-install-dir "$KIBANA_INSTALL_DIR" \
+    --include-tag "ciGroup$CI_GROUP"
 
 if [[ ! "$TASK_QUEUE_PROCESS_ID" && "$CI_GROUP" == "1" ]]; then
   source test/scripts/jenkins_build_kbn_sample_panel_action.sh
