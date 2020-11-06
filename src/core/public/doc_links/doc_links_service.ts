73d2de6503ac2e9c0a0da781cdeca8f6507e2b70
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2020-11-06 20:49:10 +0200

    [Visualize] New visualization wizard (#79627) (#82858)
    * [Visualizations] New vis wizard
    
    * Update functional tests
    
    * Create oss plugins for maps and lens and unregister alias function
    
    * Add new plugins to .i18nrc.json
    
    * Add readme and codeowners to the new plugins
    
    * update docs
    
    * fix tests
    
    * fix types
    
    * fixes
    
    * Update development docs
    
    * fix oss functional tests
    
    * Fix jest and x-pack functional tests
    
    * Fix functional test
    
    * changes on the layout
    
    * Cleanup and responsiveness
    
    * cleanup unecessary code
    
    * add common folder to the new OSS plugins
    
    * remove unecessary translations
    
    * Update limits.yml file
    
    * Fix basic label
    
    * Add experimental badge on controls vis
    
    * Nice improvements
    
    * fixes
    
    * Improving styles
    
    * Making modal go full height on smaller screens
    
    * Fixing sass lint warning
    
    * fix lint error
    
    * fix internationalization error
    
    * PR fixes
    
    * PR changes
    
    * Use useCallback where possible
    
    * Remove translations that need to be translated again
    
    * Lazy Load wizard modal
    
    * Remove legacyMapVisualizationWarning
    
    * Import the OSS plugins constants from the plugins
    
    * Export constant from lensOss
    
    * Change the new oss plugins from OSS to Oss
    
    * Add a new line to the kibana.json files of the new plugins
    
    * New nit fix
    
    * Fix spaces
    
    * Change the texts for the first step of the modal
    
    * Fix test
    
    * Fixes some of the PR comments
    
    * Add onClick funtionality to the entire aggregation based card
    
    * Cards description changes, introduce a copyFromRoot method to solve the problem of when disabling the x-pack plugic, to also disable the oss
    
    * Create new FTR for testing the functionality of the wizard when both maps and lens apps are disabled
    
    * fix eslint error
    
    * Change groupTitles and descriptions
    
    * Change input vis description
    
    * Remove the copyFromRoot from the signature of the ConfigDeprecationFactory and export it from the main entrypoint
    
    * Make the disabled cards badge clickable
    
    * Changes from code review
    
    * Fix functional tests failures
    
    * Rename groupTitle to titleInWizard to be more specific
    
    * Change vega vis note
    
    * minor design changes
    
    * fix problem with plugins list docs
    
    * Retrieve maps and lens landing page from docs service and add tracking url param
    
    * Fix funtional test for the new dashboard flow
    
    * Fix logic in alias registry for removing the discardOnRegister alias
    
    * no need to remove the alias entry from the discardOnRegister array
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: miukimiu <elizabet.oliveira@elastic.co>
    # Conflicts:
    #	.github/CODEOWNERS
    #	docs/developer/plugin-list.asciidoc
    #	packages/kbn-optimizer/limits.yml
    #	x-pack/scripts/functional_tests.js

--- src/core/public/doc_links/doc_links_service.ts
+++ src/core/public/doc_links/doc_links_service.ts
@@ -37,6 +37,7 @@ export class DocLinksService {
       ELASTIC_WEBSITE_URL,
       links: {
         dashboard: {
+          guide: `${ELASTIC_WEBSITE_URL}guide/en/kibana/${DOC_LINK_VERSION}/dashboard.html`,
           drilldowns: `${ELASTIC_WEBSITE_URL}guide/en/kibana/${DOC_LINK_VERSION}/drilldowns.html`,
           drilldownsTriggerPicker: `${ELASTIC_WEBSITE_URL}guide/en/kibana/${DOC_LINK_VERSION}/url-drilldown.html#trigger-picker`,
           urlDrilldownTemplateSyntax: `${ELASTIC_WEBSITE_URL}guide/en/kibana/${DOC_LINK_VERSION}/url-drilldown.html#templating`,
@@ -135,6 +136,8 @@ export class DocLinksService {
         visualize: {
           guide: `${ELASTIC_WEBSITE_URL}guide/en/kibana/${DOC_LINK_VERSION}/visualize.html`,
           timelionDeprecation: `${ELASTIC_WEBSITE_URL}guide/en/kibana/${DOC_LINK_VERSION}/dashboard.html#timelion-deprecation`,
+          lens: `${ELASTIC_WEBSITE_URL}what-is/kibana-lens`,
+          maps: `${ELASTIC_WEBSITE_URL}maps`,
         },
       },
     });
@@ -147,6 +150,7 @@ export interface DocLinksStart {
   readonly ELASTIC_WEBSITE_URL: string;
   readonly links: {
     readonly dashboard: {
+      readonly guide: string;
       readonly drilldowns: string;
       readonly drilldownsTriggerPicker: string;
       readonly urlDrilldownTemplateSyntax: string;
