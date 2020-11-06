6dd9fa14ee687a992c4372d76143213b14989f96
Thom Heymann <thom.heymann@elastic.co>
2020-11-06 11:30:38 +0000

    Fix test failure

--- x-pack/plugins/uptime/public/components/monitor/synthetics/__tests__/step_screenshot_display.test.tsx
+++ x-pack/plugins/uptime/public/components/monitor/synthetics/__tests__/step_screenshot_display.test.tsx
@@ -6,15 +6,17 @@
 
 import { shallowWithIntl, mountWithIntl } from 'test_utils/enzyme_helpers';
 import React from 'react';
-import * as reactUse from 'react-use';
+import useIntersection from 'react-use/lib/useIntersection';
 import { StepScreenshotDisplay } from '../step_screenshot_display';
 
-describe('StepScreenshotDisplayProps', () => {
-  // @ts-ignore missing fields don't matter in this test, the component in question only relies on `isIntersecting`
-  jest.spyOn(reactUse, 'useIntersection').mockImplementation(() => ({
-    isIntersecting: true,
-  }));
+jest.mock('react-use/lib/useIntersection');
 
+const useIntersectionMock = useIntersection as jest.Mock;
+useIntersectionMock.mockImplementation(() => ({
+  isIntersecting: true,
+}));
+
+describe('StepScreenshotDisplayProps', () => {
   it('displays screenshot thumbnail when present', () => {
     const wrapper = mountWithIntl(
       <StepScreenshotDisplay
