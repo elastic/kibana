e27161b22d15480fe3b7c19ec2e1076b36c9c177
Jean-Louis Leysens <jloleysens@gmail.com>
2020-11-06 09:38:39 +0100

    moved save policy function into edit policy section

--- x-pack/plugins/index_lifecycle_management/public/application/lib/policies.ts
+++ x-pack/plugins/index_lifecycle_management/public/application/lib/policies.ts
@@ -3,16 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { i18n } from '@kbn/i18n';
-import { METRIC_TYPE } from '@kbn/analytics';
 
-import { PolicyFromES, SerializedPolicy } from '../../../common/types';
-
-import { toasts } from '../services/notification';
-import { savePolicy as savePolicyApi } from '../services/api';
-import { getUiMetricsForPhases, trackUiMetric } from '../services/ui_metric';
-import { showApiError } from '../services/api_errors';
-import { UIM_POLICY_CREATE, UIM_POLICY_UPDATE } from '../constants';
+import { PolicyFromES } from '../../../common/types';
 
 export const splitSizeAndUnits = (field: string): { size: string; units: string } => {
   let size = '';
@@ -37,40 +29,4 @@ export const getPolicyByName = (
   if (policies && policies.length > 0) {
     return policies.find((policy: PolicyFromES) => policy.name === policyName);
   }
-};
-
-export const savePolicy = async (
-  serializedPolicy: SerializedPolicy,
-  isNew: boolean
-): Promise<boolean> => {
-  try {
-    await savePolicyApi(serializedPolicy);
-  } catch (err) {
-    const title = i18n.translate('xpack.indexLifecycleMgmt.editPolicy.saveErrorMessage', {
-      defaultMessage: 'Error saving lifecycle policy {lifecycleName}',
-      values: { lifecycleName: serializedPolicy.name },
-    });
-    showApiError(err, title);
-    return false;
-  }
-
-  const uiMetrics = getUiMetricsForPhases(serializedPolicy.phases);
-  uiMetrics.push(isNew ? UIM_POLICY_CREATE : UIM_POLICY_UPDATE);
-  trackUiMetric(METRIC_TYPE.COUNT, uiMetrics);
-
-  const message = i18n.translate('xpack.indexLifecycleMgmt.editPolicy.successfulSaveMessage', {
-    defaultMessage: '{verb} lifecycle policy "{lifecycleName}"',
-    values: {
-      verb: isNew
-        ? i18n.translate('xpack.indexLifecycleMgmt.editPolicy.createdMessage', {
-            defaultMessage: 'Created',
-          })
-        : i18n.translate('xpack.indexLifecycleMgmt.editPolicy.updatedMessage', {
-            defaultMessage: 'Updated',
-          }),
-      lifecycleName: serializedPolicy.name,
-    },
-  });
-  toasts.addSuccess(message);
-  return true;
 };
--- x-pack/plugins/index_lifecycle_management/public/application/sections/edit_policy/edit_policy.tsx
+++ x-pack/plugins/index_lifecycle_management/public/application/sections/edit_policy/edit_policy.tsx
@@ -34,7 +34,7 @@ import { useForm, Form, UseField, TextField, useFormData } from '../../../shared
 
 import { toasts } from '../../services/notification';
 
-import { savePolicy } from '../../lib/policies';
+import { savePolicy } from './save_policy';
 
 import {
   LearnMoreLink,
