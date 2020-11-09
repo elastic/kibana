39e8affd686bb1d0c50e522ae5763c6945adcbd8
Paul Tavares <paul.tavares@elastic.co>
2020-11-09 11:27:24 -0500

    Fix type

--- x-pack/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_policy_create_extension.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_policy_create_extension.tsx
@@ -7,27 +7,29 @@
 import React, { memo } from 'react';
 import { EuiCallOut, EuiSpacer, EuiText } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { IntegrationPolicyCreateExtensionComponent } from '../../../../../../../ingest_manager/common/types/ui_extensions';
+import { IntegrationPolicyCreateExtensionComponentProps } from '../../../../../../../ingest_manager/common/types/ui_extensions';
 
 /**
  * Exports Endpoint-specific package policy instructions
  * for use in the Ingest app create / edit package policy
  */
-export const EndpointPolicyCreateExtension = memo<IntegrationPolicyCreateExtensionComponent>(() => {
-  return (
-    <>
-      <EuiSpacer size="m" />
-      <EuiCallOut data-test-subj="endpointPackagePolicy_create" iconType="iInCircle">
-        <EuiText size="s">
-          <p>
-            <FormattedMessage
-              id="xpack.securitySolution.endpoint.ingestManager.createPackagePolicy.endpointConfiguration"
-              defaultMessage="We'll save your integration with our recommended defaults. You can change this later by editing the Endpoint Security integration within your agent policy."
-            />
-          </p>
-        </EuiText>
-      </EuiCallOut>
-    </>
-  );
-});
+export const EndpointPolicyCreateExtension = memo<IntegrationPolicyCreateExtensionComponentProps>(
+  () => {
+    return (
+      <>
+        <EuiSpacer size="m" />
+        <EuiCallOut data-test-subj="endpointPackagePolicy_create" iconType="iInCircle">
+          <EuiText size="s">
+            <p>
+              <FormattedMessage
+                id="xpack.securitySolution.endpoint.ingestManager.createPackagePolicy.endpointConfiguration"
+                defaultMessage="We'll save your integration with our recommended defaults. You can change this later by editing the Endpoint Security integration within your agent policy."
+              />
+            </p>
+          </EuiText>
+        </EuiCallOut>
+      </>
+    );
+  }
+);
 EndpointPolicyCreateExtension.displayName = 'EndpointPolicyCreateExtension';
