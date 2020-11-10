8890129b59bf638e28076099dbc07d4e552c89e9
Matt Kime <matt@mattki.me>
2020-11-10 17:20:17 -0600

    remove commented code

--- src/plugins/data/common/index_patterns/index_patterns/index_pattern.ts
+++ src/plugins/data/common/index_patterns/index_patterns/index_pattern.ts
@@ -333,25 +333,4 @@ export class IndexPattern implements IIndexPattern {
       return this.fieldFormats.getInstance(formatSpec.id, formatSpec.params);
     }
   }
-  /**
-   * Helper function to extend field specs with e.g. customName
-   */
-  /*
-  I think this is the attribute builder
-  private getFieldSpecs(specs: IndexPatternFieldMap | undefined) {
-    if (!specs) {
-      return {};
-    }
-    if (!this.attributes?.fields) {
-      return specs;
-    }
-
-    for (const key of Object.keys(this.attributes?.fields)) {
-      if (specs[key] && this.attributes.fields[key].customName) {
-        specs[key].customName = this.attributes.fields[key].customName;
-      }
-    }
-    return specs;
-  }
-  */
 }
