b1161cf7ff52a1ffa84b6afd9e4aa410846350c5
Constance Chen <constance.chen.3@gmail.com>
2020-11-09 19:27:20 -0800

    [WS][Opinionated] Move interfaces not used in server/ out of common/ and to public/

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/types.ts
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/types.ts
@@ -8,6 +8,52 @@ export * from '../../../common/types/workplace_search';
 
 export type SpacerSizeTypes = 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
 
+export interface Group {
+  id: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+  contentSources: ContentSource[];
+  users: User[];
+  usersCount: number;
+  color?: string;
+}
+
+export interface GroupDetails extends Group {
+  contentSources: ContentSourceDetails[];
+  canEditGroup: boolean;
+  canDeleteGroup: boolean;
+}
+
+export interface User {
+  id: string;
+  name: string | null;
+  initials: string;
+  pictureUrl: string | null;
+  color: string;
+  email: string;
+  role?: string;
+  groupIds: string[];
+}
+
+export interface ContentSource {
+  id: string;
+  serviceType: string;
+  name: string;
+}
+
+export interface ContentSourceDetails extends ContentSource {
+  status: string;
+  statusMessage: string;
+  documentCount: string;
+  isFederatedSource: boolean;
+  searchable: boolean;
+  supportedByLicense: boolean;
+  errorReason: number;
+  allowsReauth: boolean;
+  boost: number;
+}
+
 export interface SourcePriority {
   [id: string]: number;
 }
