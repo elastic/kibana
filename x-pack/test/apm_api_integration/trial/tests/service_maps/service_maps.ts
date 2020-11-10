feffa04e18e50fc642f7d4d52b56c729c6ec0a52
Nathan L Smith <smith@nlsmith.com>
2020-11-10 11:36:10 -0600

    Move Elasticsearch type definitions out of APM
    ...and into x-pack.
    
    Also remove `PromiseReturnType` from APM and use the copy in observability everywhere.
    
    All of the additional changes to APM imports are just automatic sorting.
    
    This makes doing #77720 a little easier and removes some implicit circular dependencies for #80508.

--- x-pack/test/apm_api_integration/trial/tests/service_maps/service_maps.ts
+++ x-pack/test/apm_api_integration/trial/tests/service_maps/service_maps.ts
@@ -8,7 +8,7 @@ import querystring from 'querystring';
 import expect from '@kbn/expect';
 import { isEmpty, uniq } from 'lodash';
 import archives_metadata from '../../../common/archives_metadata';
-import { PromiseReturnType } from '../../../../../plugins/apm/typings/common';
+import { PromiseReturnType } from '../../../../../plugins/observability/typings/common';
 import { expectSnapshot } from '../../../common/match_snapshot';
 import { FtrProviderContext } from '../../../common/ftr_provider_context';
 
