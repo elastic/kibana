2556da78070f9bf28448ddea57f7aa78fa7a2971
Constance Chen <constance.chen.3@gmail.com>
2020-11-11 08:43:57 -0800

    Split out EngineRouter and EngineNav into separate files

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/engine/engine_nav.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/engine/engine_nav.test.tsx
@@ -4,82 +4,13 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import '../../../__mocks__/react_router_history.mock';
-import { unmountHandler } from '../../../__mocks__/shallow_useeffect.mock';
-import { setMockValues, setMockActions } from '../../../__mocks__/kea.mock';
+import { setMockValues } from '../../../__mocks__/kea.mock';
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Switch, Redirect, useParams } from 'react-router-dom';
 import { EuiBadge } from '@elastic/eui';
 
-jest.mock('../../../shared/flash_messages', () => ({
-  setQueuedErrorMessage: jest.fn(),
-}));
-import { setQueuedErrorMessage } from '../../../shared/flash_messages';
-
-import { EngineRouter, EngineNav } from './';
-
-describe('EngineRouter', () => {
-  const values = { dataLoading: false, engineNotFound: false, myRole: {} };
-  const actions = { setEngineName: jest.fn(), initializeEngine: jest.fn(), clearEngine: jest.fn() };
-
-  beforeEach(() => {
-    setMockValues(values);
-    setMockActions(actions);
-  });
-
-  describe('useEffect', () => {
-    beforeEach(() => {
-      (useParams as jest.Mock).mockReturnValue({ engineName: 'some-engine' });
-      shallow(<EngineRouter />);
-    });
-
-    it('sets engineName based on the current route parameters', () => {
-      expect(actions.setEngineName).toHaveBeenCalledWith('some-engine');
-    });
-
-    it('initializes/fetches engine API data', () => {
-      expect(actions.initializeEngine).toHaveBeenCalled();
-    });
-
-    it('clears engine on unmount', () => {
-      unmountHandler();
-      expect(actions.clearEngine).toHaveBeenCalled();
-    });
-  });
-
-  it('redirects to engines list and flashes an error if the engine param was not found', () => {
-    (useParams as jest.Mock).mockReturnValue({ engineName: '404-engine' });
-    setMockValues({ ...values, engineNotFound: true });
-    const wrapper = shallow(<EngineRouter />);
-
-    expect(wrapper.find(Redirect).prop('to')).toEqual('/engines');
-    expect(setQueuedErrorMessage).toHaveBeenCalledWith(
-      "No engine with name '404-engine' could be found."
-    );
-  });
-
-  it('does not render if async data is still loading', () => {
-    setMockValues({ ...values, dataLoading: true });
-    const wrapper = shallow(<EngineRouter />);
-    expect(wrapper.isEmptyRender()).toBe(true);
-  });
-
-  it('renders a default engine overview', () => {
-    const wrapper = shallow(<EngineRouter />);
-
-    expect(wrapper.find(Switch)).toHaveLength(1);
-    expect(wrapper.find('[data-test-subj="EngineOverviewTODO"]')).toHaveLength(1);
-  });
-
-  it('renders an analytics view', () => {
-    setMockValues({ myRole: { canViewEngineAnalytics: true } });
-    const wrapper = shallow(<EngineRouter />);
-
-    expect(wrapper.find('[data-test-subj="AnalyticsTODO"]')).toHaveLength(1);
-  });
-});
+import { EngineNav } from './';
 
 describe('EngineNav', () => {
   const values = { myRole: {}, engineName: 'some-engine', dataLoading: false, engine: {} };
--- x-pack/plugins/enterprise_search/public/applications/app_search/components/engine/engine_nav.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/engine/engine_nav.tsx
@@ -4,21 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useEffect } from 'react';
-import { Route, Switch, Redirect, useParams } from 'react-router-dom';
-import { useValues, useActions } from 'kea';
+import React from 'react';
+import { useValues } from 'kea';
 
 import { EuiText, EuiBadge, EuiIcon, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
 import { SideNavLink, SideNavItem } from '../../../shared/layout';
-import { SetAppSearchChrome as SetPageChrome } from '../../../shared/kibana_chrome';
-import { setQueuedErrorMessage } from '../../../shared/flash_messages';
 import { AppLogic } from '../../app_logic';
 import {
   getEngineRoute,
-  ENGINES_PATH,
-  ENGINE_PATH,
   ENGINE_ANALYTICS_PATH,
   ENGINE_DOCUMENTS_PATH,
   ENGINE_SCHEMA_PATH,
@@ -51,52 +46,6 @@ import { EngineLogic } from './';
 import { EngineDetails } from './types';
 
 import './engine_nav.scss';
-
-export const EngineRouter: React.FC = () => {
-  const {
-    myRole: { canViewEngineAnalytics },
-  } = useValues(AppLogic);
-
-  const { dataLoading, engineNotFound } = useValues(EngineLogic);
-  const { setEngineName, initializeEngine, clearEngine } = useActions(EngineLogic);
-
-  const { engineName } = useParams() as { engineName: string };
-  const engineBreadcrumb = [ENGINES_TITLE, engineName];
-
-  useEffect(() => {
-    setEngineName(engineName);
-    initializeEngine();
-    return clearEngine;
-  }, [engineName]);
-
-  if (engineNotFound) {
-    setQueuedErrorMessage(
-      i18n.translate('xpack.enterpriseSearch.appSearch.engine.notFound', {
-        defaultMessage: "No engine with name '{engineName}' could be found.",
-        values: { engineName },
-      })
-    );
-    return <Redirect to={ENGINES_PATH} />;
-  }
-
-  if (dataLoading) return null;
-
-  return (
-    // TODO: Add more routes as we migrate them
-    <Switch>
-      {canViewEngineAnalytics && (
-        <Route path={ENGINE_PATH + ENGINE_ANALYTICS_PATH}>
-          <SetPageChrome trail={[...engineBreadcrumb, ANALYTICS_TITLE]} />
-          <div data-test-subj="AnalyticsTODO">Just testing right now</div>
-        </Route>
-      )}
-      <Route>
-        <SetPageChrome trail={[...engineBreadcrumb, OVERVIEW_TITLE]} />
-        <div data-test-subj="EngineOverviewTODO">Overview</div>
-      </Route>
-    </Switch>
-  );
-};
 
 export const EngineNav: React.FC = () => {
   const {
