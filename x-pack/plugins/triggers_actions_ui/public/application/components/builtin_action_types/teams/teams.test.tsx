2f240d07e383eb970eabafc187ebd68f9c3595ed
Ying Mao <ying.mao@elastic.co>
2020-11-12 12:09:30 -0500

    PR comments

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/teams/teams.test.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/teams/teams.test.tsx
@@ -23,7 +23,6 @@ beforeAll(async () => {
 describe('actionTypeRegistry.get() works', () => {
   test('action type static data is as expected', () => {
     expect(actionTypeModel.id).toEqual(ACTION_TYPE_ID);
-    expect(actionTypeModel.iconClass).toEqual('logoWindows');
   });
 });
 
