bc6c33df551b7d8365b69bb0b153d9f5d03de19e
Kaise Cheng <kaise.cheng@elastic.co>
2020-11-12 14:55:56 +0100

    fix flaky test #83231

--- x-pack/test/functional/apps/logstash/pipeline_create.js
+++ x-pack/test/functional/apps/logstash/pipeline_create.js
@@ -5,6 +5,7 @@
  */
 
 import expect from '@kbn/expect';
+import { omit } from 'lodash';
 
 export default function ({ getService, getPageObjects }) {
   const browser = getService('browser');
@@ -89,6 +90,7 @@ export default function ({ getService, getPageObjects }) {
         await PageObjects.logstash.gotoPipelineList();
         await pipelineList.assertExists();
         const originalRows = await pipelineList.readRows();
+        const originalRowsWithoutTime = originalRows.map((row) => omit(row, 'lastModified'));
 
         await PageObjects.logstash.gotoNewPipelineEditor();
         await pipelineEditor.clickCancel();
@@ -96,7 +98,8 @@ export default function ({ getService, getPageObjects }) {
         await retry.try(async () => {
           await pipelineList.assertExists();
           const currentRows = await pipelineList.readRows();
-          expect(originalRows).to.eql(currentRows);
+          const currentRowsWithoutTime = currentRows.map((row) => omit(row, 'lastModified'));
+          expect(originalRowsWithoutTime).to.eql(currentRowsWithoutTime);
         });
       });
     });
