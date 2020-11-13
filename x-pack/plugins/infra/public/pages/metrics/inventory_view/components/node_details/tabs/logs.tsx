ebb02c6a3e96d7ed52c1eea44b9a005e68d144ff
Phillip Burch <phillip.burch@live.com>
2020-11-13 10:52:09 -0600

    Add log stream component to node details

--- x-pack/plugins/infra/public/pages/metrics/inventory_view/components/node_details/tabs/logs.tsx
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/node_details/tabs/logs.tsx
@@ -7,9 +7,24 @@
 import React from 'react';
 import { i18n } from '@kbn/i18n';
 import { TabContent, TabProps } from './shared';
+import { LogStream } from '../../../../../../components/log_stream';
+import { useWaffleOptionsContext } from '../../../hooks/use_waffle_options';
+import { findInventoryFields } from '../../../../../../../common/inventory_models';
 
 const TabComponent = (props: TabProps) => {
-  return <TabContent>Logs Placeholder</TabContent>;
+  const endTimestamp = Date.now();
+  const startTimestamp = endTimestamp - 15 * 60 * 1000; // 15 minutes
+  const { nodeType } = useWaffleOptionsContext();
+  const { options, node } = props;
+
+  const filter = options.fields
+    ? `${findInventoryFields(nodeType, options.fields).id}: "${node.id}"`
+    : '';
+  return (
+    <TabContent>
+      <LogStream startTimestamp={startTimestamp} endTimestamp={endTimestamp} query={filter} />
+    </TabContent>
+  );
 };
 
 export const LogsTab = {
