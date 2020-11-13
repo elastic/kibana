c7fcaf1241abb95cce1adc2d7d5be0f6cdeadb40
Josh Dover <me@joshdover.com>
2020-11-13 12:19:02 -0700

    Include subLinks in GS application results

--- x-pack/plugins/global_search_providers/public/providers/get_app_results.ts
+++ x-pack/plugins/global_search_providers/public/providers/get_app_results.ts
@@ -5,22 +5,39 @@
  */
 
 import levenshtein from 'js-levenshtein';
-import { PublicAppInfo } from 'src/core/public';
+import { PublicAppInfo, PublicAppSubLinkInfo } from 'src/core/public';
 import { GlobalSearchProviderResult } from '../../../global_search/public';
+
+/** Type used internally to represent an application unrolled into its separate sublinks */
+export interface AppLink {
+  id: string;
+  app: PublicAppInfo;
+  subLinkTitles: string[];
+  path: string;
+}
 
 export const getAppResults = (
   term: string,
   apps: PublicAppInfo[]
 ): GlobalSearchProviderResult[] => {
-  return apps
-    .map((app) => ({ app, score: scoreApp(term, app) }))
-    .filter(({ score }) => score > 0)
-    .map(({ app, score }) => appToResult(app, score));
+  return (
+    apps
+      // Unroll all sublinks
+      .flatMap((app) => flattenSubLinks(app))
+      // Only include sublinks if there is a search term
+      .filter((appLink) => term.length > 0 || appLink.subLinkTitles.length === 0)
+      .map((appLink) => ({
+        appLink,
+        score: scoreApp(term, appLink),
+      }))
+      .filter(({ score }) => score > 0)
+      .map(({ appLink, score }) => appToResult(appLink, score))
+  );
 };
 
-export const scoreApp = (term: string, { title }: PublicAppInfo): number => {
+export const scoreApp = (term: string, appLink: AppLink): number => {
   term = term.toLowerCase();
-  title = title.toLowerCase();
+  const title = [appLink.app.title, ...appLink.subLinkTitles].join(' ').toLowerCase();
 
   // shortcuts to avoid calculating the distance when there is an exact match somewhere.
   if (title === term) {
@@ -43,17 +60,56 @@ export const scoreApp = (term: string, { title }: PublicAppInfo): number => {
   return 0;
 };
 
-export const appToResult = (app: PublicAppInfo, score: number): GlobalSearchProviderResult => {
+export const appToResult = (appLink: AppLink, score: number): GlobalSearchProviderResult => {
+  const titleParts =
+    // Stack Management app should not include the app title in the concatenated link label
+    appLink.app.id === 'management' && appLink.subLinkTitles.length > 0
+      ? appLink.subLinkTitles
+      : [appLink.app.title, ...appLink.subLinkTitles];
+
   return {
-    id: app.id,
-    title: app.title,
+    id: appLink.id,
+    // Concatenate title using slashes
+    title: titleParts.join(' / '),
     type: 'application',
-    icon: app.euiIconType,
-    url: app.appRoute,
+    icon: appLink.app.euiIconType,
+    url: appLink.path,
     meta: {
-      categoryId: app.category?.id ?? null,
-      categoryLabel: app.category?.label ?? null,
+      categoryId: appLink.app.category?.id ?? null,
+      categoryLabel: appLink.app.category?.label ?? null,
     },
     score,
   };
+};
+
+const flattenSubLinks = (app: PublicAppInfo, subLink?: PublicAppSubLinkInfo): AppLink[] => {
+  if (!subLink) {
+    return [
+      {
+        id: app.id,
+        app,
+        path: app.appRoute,
+        subLinkTitles: [],
+      },
+      ...app.subLinks.flatMap((appSubLink) => flattenSubLinks(app, appSubLink)),
+    ];
+  }
+
+  const appLink: AppLink = {
+    id: `${app.id}-${subLink.id}`,
+    app,
+    subLinkTitles: [subLink.title],
+    path: `${app.appRoute}${subLink.path}`,
+  };
+
+  return [
+    ...(subLink.path ? [appLink] : []),
+    ...subLink.subLinks
+      .flatMap((subSubLink) => flattenSubLinks(app, subSubLink))
+      .map((subAppLink) => ({
+        ...subAppLink,
+        // shift current sublink title into array of sub-sublink titles
+        subLinkTitles: [subLink.title, ...subAppLink.subLinkTitles],
+      })),
+  ];
 };
