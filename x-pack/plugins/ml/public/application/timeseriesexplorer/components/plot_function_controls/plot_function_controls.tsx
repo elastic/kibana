e35b174451e4b2ca996b89758bdd77eca3f1da19
Quynh Nguyen <quynh.nguyen@elastic.co>
2020-11-16 17:01:58 -0600

    [ML] Remove redundant code

--- x-pack/plugins/ml/public/application/timeseriesexplorer/components/plot_function_controls/plot_function_controls.tsx
+++ x-pack/plugins/ml/public/application/timeseriesexplorer/components/plot_function_controls/plot_function_controls.tsx
@@ -3,7 +3,7 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { EuiFlexItem, EuiFormRow, EuiSelect } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { mlJobService } from '../../../services/job_service';
@@ -44,12 +44,6 @@ export const PlotByFunctionControls = ({
   selectedEntities: Record<string, any>;
 }) => {
   const toastNotificationService = useToastNotificationService();
-  const previous = useRef<string | null>(null);
-
-  const handleOnChange = useCallback((func: string) => {
-    setFunctionDescription(func);
-    previous.current = func;
-  }, []);
 
   useEffect(() => {
     const getFunctionDescriptionToPlot = async () => {
@@ -97,7 +91,7 @@ export const PlotByFunctionControls = ({
         <EuiSelect
           options={plotByFunctionOptions}
           value={functionDescription}
-          onChange={(e) => handleOnChange(e.target.event)}
+          onChange={(e) => setFunctionDescription(e.target.value)}
           aria-label={i18n.translate('xpack.ml.timeSeriesExplorer.metricPlotByOptionLabel', {
             defaultMessage: 'Pick function to plot by (min, max, or average) if metric function',
           })}
