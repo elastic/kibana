edfbc493928cc4beaf534824182507a3bfd726e9
Devin Hurley <devin.hurley@elastic.co>
2020-11-16 09:58:46 -0500

    add copyright to scripts files

--- x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/platform_engineer/post_detections_role.sh
+++ x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/platform_engineer/post_detections_role.sh
@@ -1,3 +1,10 @@
+
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
 ROLE=(${@:-./detections_role.json})
 
 curl -H 'Content-Type: application/json' -H 'kbn-xsrf: 123'\
