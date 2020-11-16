f2a798046c78c8132971427a86c11af1c37032af
Phillip Burch <phillip.burch@live.com>
2020-11-16 09:02:08 -0600

    Add logs tab with ability to search

--- x-pack/plugins/infra/public/pages/metrics/inventory_view/components/node_details/tabs/logs.tsx
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/node_details/tabs/logs.tsx
@@ -4,28 +4,85 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+import { FormattedMessage } from '@kbn/i18n/react';
 import { i18n } from '@kbn/i18n';
+import { EuiFieldSearch } from '@elastic/eui';
+import { EuiFlexGroup } from '@elastic/eui';
+import { EuiFlexItem } from '@elastic/eui';
+import { EuiButtonEmpty } from '@elastic/eui';
 import { TabContent, TabProps } from './shared';
 import { LogStream } from '../../../../../../components/log_stream';
 import { useWaffleOptionsContext } from '../../../hooks/use_waffle_options';
 import { findInventoryFields } from '../../../../../../../common/inventory_models';
+import { euiStyled } from '../../../../../../../../observability/public';
+import { useLinkProps } from '../../../../../../hooks/use_link_props';
+import { getNodeLogsUrl } from '../../../../../link_to';
 
 const TabComponent = (props: TabProps) => {
-  const endTimestamp = Date.now();
-  const startTimestamp = endTimestamp - 15 * 60 * 1000; // 15 minutes
+  const [textQuery, setTextQuery] = useState('');
+  const endTimestamp = props.currentTime;
+  const startTimestamp = endTimestamp - 60 * 60 * 1000; // 15 minutes
   const { nodeType } = useWaffleOptionsContext();
   const { options, node } = props;
 
-  const filter = options.fields
-    ? `${findInventoryFields(nodeType, options.fields).id}: "${node.id}"`
-    : '';
+  const filter = useMemo(() => {
+    let query = options.fields
+      ? `${findInventoryFields(nodeType, options.fields).id}: "${node.id}"`
+      : ``;
+
+    if (textQuery) {
+      query += ` and message: ${textQuery}`;
+    }
+    return query;
+  }, [options, nodeType, node.id, textQuery]);
+
+  const onQueryChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextQuery(e.target.value);
+  }, []);
+
+  const nodeLogsMenuItemLinkProps = useLinkProps(
+    getNodeLogsUrl({
+      nodeType,
+      nodeId: node.id,
+      time: startTimestamp,
+    })
+  );
+
   return (
     <TabContent>
+      <EuiFlexGroup gutterSize={'none'} alignItems="center">
+        <EuiFlexItem grow={true}>
+          <QueryWrapper>
+            <EuiFieldSearch
+              fullWidth={true}
+              placeholder={i18n.translate('xpack.infra.nodeDetails.logs.textFieldPlaceholder', {
+                defaultMessage: 'Search for log entries...',
+              })}
+              value={textQuery}
+              isClearable={true}
+              onChange={onQueryChange}
+            />
+          </QueryWrapper>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiButtonEmpty iconType={'popout'} {...nodeLogsMenuItemLinkProps}>
+            <FormattedMessage
+              id="xpack.infra.nodeDetails.logs.openLogsLink"
+              defaultMessage={'Open in Logs'}
+            />
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+      </EuiFlexGroup>
       <LogStream startTimestamp={startTimestamp} endTimestamp={endTimestamp} query={filter} />
     </TabContent>
   );
 };
+
+const QueryWrapper = euiStyled.div`
+  padding: ${(props) => props.theme.eui.paddingSizes.m};
+  padding-right: 0;
+`;
 
 export const LogsTab = {
   id: 'logs',
