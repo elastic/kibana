1c2f85f836921a21de3a11ccfc07b327eddeec61
Zacqary Xeper <zacqary.xeper@elastic.co>
2020-11-16 10:49:59 -0600

    Format process table

--- x-pack/plugins/infra/public/pages/metrics/inventory_view/components/node_details/tabs/processes.tsx
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/node_details/tabs/processes.tsx
@@ -7,23 +7,58 @@
 import React, { useMemo } from 'react';
 import { i18n } from '@kbn/i18n';
 import {
-  EuiBasicTable,
   EuiTable,
   EuiTableHeader,
   EuiTableBody,
-  EuiTableFooter,
   EuiTableHeaderCell,
   EuiTableRow,
   EuiTableRowCell,
+  EuiSpacer,
+  EuiTablePagination,
   EuiButtonEmpty,
   EuiBadge,
   EuiCode,
+  EuiLoadingChart,
 } from '@elastic/eui';
+import { FORMATTERS } from '../../../../../../../common/formatters';
+import { euiStyled } from '../../../../../../../../observability/public';
 import { useProcessList } from '../../../hooks/use_process_list';
 import { TabContent, TabProps } from './shared';
 
 const ONE_MINUTE = 60 * 1000;
 const ONE_HOUR = ONE_MINUTE * 60;
+
+const TabComponent = ({ currentTime, node, nodeType, options }: TabProps) => {
+  const hostTerm = useMemo(() => {
+    const field = options.fields[nodeType];
+    return { [field]: node.name };
+  }, [options, node, nodeType]);
+
+  const { loading, error, response } = useProcessList(
+    hostTerm,
+    'metricbeat-*',
+    options.fields?.timestamp,
+    currentTime
+  );
+
+  return (
+    <ProcessListTabContent>
+      <ProcessesTable
+        currentTime={currentTime}
+        isLoading={loading || !response}
+        processList={response}
+      />
+    </ProcessListTabContent>
+  );
+};
+
+export const ProcessesTab = {
+  id: 'processes',
+  name: i18n.translate('xpack.infra.nodeDetails.tabs.processes', {
+    defaultMessage: 'Processes',
+  }),
+  content: TabComponent,
+};
 
 const columns = [
   {
@@ -43,7 +78,7 @@ const columns = [
     sortable: true,
     truncateText: true,
     width: '40%',
-    render: (command: string) => <EuiCode>{command.slice(0, 16)}</EuiCode>,
+    render: (command: string) => <CodeLine>{command}</CodeLine>,
   },
   {
     field: 'runtime',
@@ -57,118 +92,136 @@ const columns = [
     name: i18n.translate('xpack.infra.metrics.nodeDetails.processes.columnLabelCPU', {
       defaultMessage: 'CPU',
     }),
+    render: (value: number) => FORMATTERS.percent(value),
   },
   {
     field: 'memory',
     name: i18n.translate('xpack.infra.metrics.nodeDetails.processes.columnLabelMemory', {
       defaultMessage: 'Mem.',
     }),
+    render: (value: number) => FORMATTERS.percent(value),
   },
 ];
 
-const TabComponent = ({ currentTime, node, nodeType, options }: TabProps) => {
-  const hostTerm = useMemo(() => {
-    const field = options.fields[nodeType];
-    return { [field]: node.name };
-  }, [options, node, nodeType]);
-
-  const { loading, error, response } = useProcessList(
-    hostTerm,
-    'metricbeat-*',
-    options.fields?.timestamp,
-    currentTime
+const ProcessesTable = ({ processList, currentTime, isLoading }) => {
+  const body = !isLoading ? (
+    <ProcessesTableBody processList={processList} currentTime={currentTime} />
+  ) : (
+    <LoadingTableBody />
   );
-
-  if (!loading && response) {
-    return (
-      <TabContent>
-        <ProcessesTable currentTime={currentTime} processList={response} />
-      </TabContent>
-    );
-  }
-
-  return <TabContent>Processes Placeholder</TabContent>;
+  return (
+    <>
+      <EuiTable>
+        <EuiTableHeader>
+          <EuiTableHeaderCell width={24} />
+          {columns.map((column) => (
+            <EuiTableHeaderCell
+              key={column.field}
+              align={column.rightAlign ? 'right' : 'left'}
+              width={column.width}
+            >
+              {column.name}
+            </EuiTableHeaderCell>
+          ))}
+        </EuiTableHeader>
+        <StyledTableBody>{body}</StyledTableBody>
+      </EuiTable>
+      <EuiSpacer size="m" />
+      <EuiTablePagination
+        itemsPerPage={10}
+        activePage={0}
+        pageCount={3}
+        itemsPerPageOptions={[10, 20, 50]}
+      />
+    </>
+  );
 };
 
-const ProcessesTable = ({ processList, currentTime }) => {
-  const items = processList.map((process) => {
-    const command = process.id;
-    let mostRecentPoint;
-    for (let i = process.rows.length - 1; i >= 0; i--) {
-      const point = process.rows[i];
-      if (point.meta?.length) {
-        mostRecentPoint = point;
-        break;
-      }
-    }
+const LoadingTableBody = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        padding: '16px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <EuiLoadingChart size="xl" />
+    </div>
+  );
+};
 
-    const { cpu, memory } = mostRecentPoint;
-    const { system } = mostRecentPoint.meta[0];
-    const startTime = system.process.cpu.start_time;
-    const state = system.process.state;
+const ProcessesTableBody = ({ processList, currentTime }) => {
+  const items = useMemo(
+    () =>
+      processList.map((process) => {
+        const command = process.id;
+        let mostRecentPoint;
+        for (let i = process.rows.length - 1; i >= 0; i--) {
+          const point = process.rows[i];
+          if (point.meta?.length) {
+            mostRecentPoint = point;
+            break;
+          }
+        }
 
-    const runtimeLength = currentTime - Date.parse(startTime);
-    let remainingRuntimeMS = runtimeLength;
-    const runtimeHours = Math.floor(remainingRuntimeMS / ONE_HOUR);
-    remainingRuntimeMS -= runtimeHours * ONE_HOUR;
-    const runtimeMinutes = Math.floor(remainingRuntimeMS / ONE_MINUTE);
-    remainingRuntimeMS -= runtimeMinutes * ONE_MINUTE;
-    const runtimeSeconds = Math.floor(remainingRuntimeMS / 1000);
-    remainingRuntimeMS -= runtimeSeconds * 1000;
+        const { cpu, memory } = mostRecentPoint;
+        const { system } = mostRecentPoint.meta[0];
+        const startTime = system.process.cpu.start_time;
+        const state = system.process.state;
 
-    const runtimeDisplayHours = runtimeHours ? `${runtimeHours}:` : '';
-    const runtimeDisplayMinutes =
-      runtimeHours && runtimeMinutes < 10 ? `0${runtimeMinutes}:` : `${runtimeMinutes}:`;
-    const runtimeDisplaySeconds = runtimeSeconds < 10 ? `0${runtimeSeconds}` : runtimeSeconds;
-    const runtimeDisplayMS = !runtimeDisplayHours ? `.${remainingRuntimeMS}` : '';
+        const runtimeLength = currentTime - Date.parse(startTime);
+        let remainingRuntimeMS = runtimeLength;
+        const runtimeHours = Math.floor(remainingRuntimeMS / ONE_HOUR);
+        remainingRuntimeMS -= runtimeHours * ONE_HOUR;
+        const runtimeMinutes = Math.floor(remainingRuntimeMS / ONE_MINUTE);
+        remainingRuntimeMS -= runtimeMinutes * ONE_MINUTE;
+        const runtimeSeconds = Math.floor(remainingRuntimeMS / 1000);
+        remainingRuntimeMS -= runtimeSeconds * 1000;
 
-    const runtime = `${runtimeDisplayHours}${runtimeDisplayMinutes}${runtimeDisplaySeconds}${runtimeDisplayMS}`;
+        const runtimeDisplayHours = runtimeHours ? `${runtimeHours}:` : '';
+        const runtimeDisplayMinutes =
+          runtimeHours && runtimeMinutes < 10 ? `0${runtimeMinutes}:` : `${runtimeMinutes}:`;
+        const runtimeDisplaySeconds = runtimeSeconds < 10 ? `0${runtimeSeconds}` : runtimeSeconds;
+        const runtimeDisplayMS = !runtimeDisplayHours ? `.${remainingRuntimeMS}` : '';
 
-    return {
-      state,
-      command,
-      runtime,
-      cpu,
-      memory,
-    };
-  });
+        const runtime = `${runtimeDisplayHours}${runtimeDisplayMinutes}${runtimeDisplaySeconds}${runtimeDisplayMS}`;
+
+        return {
+          state,
+          command,
+          runtime,
+          cpu,
+          memory,
+        };
+      }),
+    [processList, currentTime]
+  );
 
   return (
-    <EuiTable>
-      <EuiTableHeader>
-        <EuiTableHeaderCell width={24} />
-        {columns.map((column) => (
-          <EuiTableHeaderCell
-            key={column.field}
+    <>
+      {items.map((item, i) => {
+        const cells = columns.map((column) => (
+          <EuiTableRowCell
+            key={`${column.field}-${i}`}
+            header={column.name}
             align={column.rightAlign ? 'right' : 'left'}
-            width={column.width}
           >
-            {column.name}
-          </EuiTableHeaderCell>
-        ))}
-      </EuiTableHeader>
-      <EuiTableBody>
-        {items.map((item, i) => {
-          const cells = columns.map((column) => (
-            <EuiTableRowCell
-              key={column.field}
-              header={column.name}
-              align={column.rightAlign ? 'right' : 'left'}
-            >
-              {column.render ? column.render(item[column.field]) : item[column.field]}
+            {column.render ? column.render(item[column.field]) : item[column.field]}
+          </EuiTableRowCell>
+        ));
+        return (
+          <EuiTableRow>
+            <EuiTableRowCell>
+              <EuiButtonEmpty iconType="arrowRight" />
             </EuiTableRowCell>
-          ));
-          return (
-            <EuiTableRow>
-              <EuiTableRowCell>
-                <EuiButtonEmpty iconType="arrowRight" />
-              </EuiTableRowCell>
-              {cells}
-            </EuiTableRow>
-          );
-        })}
-      </EuiTableBody>
-    </EuiTable>
+            {cells}
+          </EuiTableRow>
+        );
+      })}
+    </>
   );
 };
 
@@ -233,10 +286,24 @@ const StateBadge = ({ state }) => {
   }
 };
 
-export const ProcessesTab = {
-  id: 'processes',
-  name: i18n.translate('xpack.infra.nodeDetails.tabs.processes', {
-    defaultMessage: 'Processes',
-  }),
-  content: TabComponent,
-};
+const ProcessListTabContent = euiStyled(TabContent)`
+  padding-left: 0;
+  padding-right: 0;
+`;
+
+const StyledTableBody = euiStyled(EuiTableBody)`
+  & .euiTableCellContent {
+    padding-top: 0;
+    padding-bottom: 0;
+    
+  }
+`;
+
+const CodeLine = euiStyled(EuiCode).attrs({ transparentBackground: true })`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  & code.euiCodeBlock__code {
+    white-space: nowrap !important;
+    vertical-align: middle;
+  }
+`;
