f2af61519dc46bb56f91c57349d056979e757043
Gidi Meir Morris <github@gidi.io>
2020-11-16 12:19:39 +0000

    hide uptime flyout if no alertTypeId is selected

--- x-pack/plugins/uptime/public/components/overview/alerts/uptime_alerts_flyout_wrapper.tsx
+++ x-pack/plugins/uptime/public/components/overview/alerts/uptime_alerts_flyout_wrapper.tsx
@@ -17,14 +17,15 @@ export const UptimeAlertsFlyoutWrapperComponent = ({
   alertFlyoutVisible,
   alertTypeId,
   setAlertFlyoutVisibility,
-}: Props) => (
-  <AlertAdd
-    addFlyoutVisible={alertFlyoutVisible}
-    consumer="uptime"
-    setAddFlyoutVisibility={setAlertFlyoutVisibility}
-    alertTypeId={alertTypeId}
-    // if we don't have an alert type pre-specified, we need to
-    // let the user choose
-    canChangeTrigger={!alertTypeId}
-  />
-);
+}: Props) =>
+  alertFlyoutVisible && alertTypeId ? (
+    <AlertAdd
+      addFlyoutVisible={alertFlyoutVisible}
+      consumer="uptime"
+      setAddFlyoutVisibility={setAlertFlyoutVisibility}
+      alertTypeId={alertTypeId}
+      // if we don't have an alert type pre-specified, we need to
+      // let the user choose
+      canChangeTrigger={!alertTypeId}
+    />
+  ) : null;
