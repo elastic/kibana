7ac0e3b78fb0e6544409fdd3dc1372e245bf31c7
Constance Chen <constance.chen.3@gmail.com>
2020-11-16 11:49:28 -0800

    Finish Overview page logic, stub out empty/metric views

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/engine_overview/engine_overview.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/engine_overview/engine_overview.tsx
@@ -4,9 +4,41 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useActions, useValues } from 'kea';
+
+import { AppLogic } from '../../app_logic';
+import { EngineLogic } from '../engine';
+import { Loading } from '../../../shared/loading';
+
+import { EngineOverviewLogic } from './';
+import { EmptyEngineOverview } from './engine_overview_empty';
+import { EngineOverviewMetrics } from './engine_overview_metrics';
 
 export const EngineOverview: React.FC = () => {
-  // TODO
-  return <>Hello world</>;
+  const {
+    myRole: { canManageEngineDocuments, canViewEngineCredentials },
+  } = useValues(AppLogic);
+  const { isMetaEngine } = useValues(EngineLogic);
+
+  const { pollForOverviewMetrics } = useActions(EngineOverviewLogic);
+  const { dataLoading, documentCount } = useValues(EngineOverviewLogic);
+
+  useEffect(() => {
+    pollForOverviewMetrics();
+  }, []);
+
+  if (dataLoading) {
+    return <Loading />;
+  }
+
+  const engineHasDocuments = documentCount > 0;
+  const canAddDocuments = canManageEngineDocuments && canViewEngineCredentials;
+  const showEngineOverview = engineHasDocuments || !canAddDocuments || isMetaEngine;
+
+  return (
+    <div data-test-subj="EngineOverview">
+      {showEngineOverview ? <EngineOverviewMetrics /> : <EmptyEngineOverview />}
+    </div>
+  );
 };
