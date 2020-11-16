74c5b884c0d7a380137981ab1c19a11ee27efbef
streamich <streamich@gmail.com>
2020-11-16 16:16:51 +0000

    refactor: 💡 don't pass plugin definition to a function

--- x-pack/plugins/ui_actions_enhanced/server/dynamic_action_enhancement.ts
+++ x-pack/plugins/ui_actions_enhanced/server/dynamic_action_enhancement.ts
@@ -6,22 +6,19 @@
 
 import { EnhancementRegistryDefinition } from '../../../../src/plugins/embeddable/server';
 import { SavedObjectReference } from '../../../../src/core/types';
-import { DynamicActionsState, SerializedEvent } from './types';
-import { AdvancedUiActionsServerPlugin } from './plugin';
+import { ActionFactory, DynamicActionsState, SerializedEvent } from './types';
 import { SerializableState } from '../../../../src/plugins/kibana_utils/common';
 
 export const dynamicActionEnhancement = (
-  uiActionsEnhanced: AdvancedUiActionsServerPlugin
+  getActionFactory: (id: string) => undefined | ActionFactory
 ): EnhancementRegistryDefinition => {
   return {
     id: 'dynamicActions',
     telemetry: (state: SerializableState, telemetry: Record<string, any>) => {
       let telemetryData = telemetry;
       (state as DynamicActionsState).events.forEach((event: SerializedEvent) => {
-        if (uiActionsEnhanced.getActionFactory(event.action.factoryId)) {
-          telemetryData = uiActionsEnhanced
-            .getActionFactory(event.action.factoryId)!
-            .telemetry(event, telemetryData);
+        if (getActionFactory(event.action.factoryId)) {
+          telemetryData = getActionFactory(event.action.factoryId)!.telemetry(event, telemetryData);
         }
       });
       return telemetryData;
@@ -30,8 +27,8 @@ export const dynamicActionEnhancement = (
       const references: SavedObjectReference[] = [];
       const newState: DynamicActionsState = {
         events: (state as DynamicActionsState).events.map((event: SerializedEvent) => {
-          const result = uiActionsEnhanced.getActionFactory(event.action.factoryId)
-            ? uiActionsEnhanced.getActionFactory(event.action.factoryId)!.extract(event)
+          const result = getActionFactory(event.action.factoryId)
+            ? getActionFactory(event.action.factoryId)!.extract(event)
             : {
                 state: event,
                 references: [],
@@ -45,8 +42,8 @@ export const dynamicActionEnhancement = (
     inject: (state: SerializableState, references: SavedObjectReference[]) => {
       return {
         events: (state as DynamicActionsState).events.map((event: SerializedEvent) => {
-          return uiActionsEnhanced.getActionFactory(event.action.factoryId)
-            ? uiActionsEnhanced.getActionFactory(event.action.factoryId)!.inject(event, references)
+          return getActionFactory(event.action.factoryId)
+            ? getActionFactory(event.action.factoryId)!.inject(event, references)
             : event;
         }),
       } as DynamicActionsState;
--- x-pack/plugins/ui_actions_enhanced/server/plugin.ts
+++ x-pack/plugins/ui_actions_enhanced/server/plugin.ts
@@ -5,15 +5,10 @@
  */
 
 import { identity } from 'lodash';
-import { CoreSetup, Plugin, SavedObjectReference } from '../../../../src/core/server';
+import { CoreSetup, Plugin } from '../../../../src/core/server';
 import { EmbeddableSetup } from '../../../../src/plugins/embeddable/server';
 import { dynamicActionEnhancement } from './dynamic_action_enhancement';
-import {
-  ActionFactoryRegistry,
-  SerializedEvent,
-  ActionFactoryDefinition,
-  DynamicActionsState,
-} from './types';
+import { ActionFactoryRegistry, SerializedEvent, ActionFactoryDefinition } from './types';
 
 export interface SetupContract {
   registerActionFactory: (definition: ActionFactoryDefinition) => void;
@@ -32,7 +27,9 @@ export class AdvancedUiActionsServerPlugin
   constructor() {}
 
   public setup(core: CoreSetup, { embeddable }: SetupDependencies) {
-    embeddable.registerEnhancement(dynamicActionEnhancement(this));
+    const getActionFactory = (actionFactoryId: string) => this.actionFactories.get(actionFactoryId);
+
+    embeddable.registerEnhancement(dynamicActionEnhancement(getActionFactory));
 
     return {
       registerActionFactory: this.registerActionFactory,
@@ -63,10 +60,5 @@ export class AdvancedUiActionsServerPlugin
         }),
       migrations: definition.migrations || {},
     });
-  };
-
-  public readonly getActionFactory = (actionFactoryId: string) => {
-    const actionFactory = this.actionFactories.get(actionFactoryId);
-    return actionFactory;
   };
 }
