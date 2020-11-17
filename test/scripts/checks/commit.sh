fa20bbe62f6103212ae76d11c5eb273b657e809d
Tiago Costa <tiagoffcc@hotmail.com>
2020-11-17 17:02:23 +0000

    chore(NA): introduce logging trap to warn about quick commit checks failure and how to reproduce it

--- test/scripts/checks/commit.sh
+++ test/scripts/checks/commit.sh
@@ -2,13 +2,21 @@
 
 source src/dev/ci_setup/setup_env.sh
 
+run_quick_commit_checks() {
+  node scripts/precommit_hook.js \
+    --ref HEAD~1..HEAD \
+    --max-files 200 \
+    --verbose || return 1
+}
+
 # Runs pre-commit hook script for the files touched in the last commit.
 # That way we can ensure a set of quick commit checks earlier as we removed
 # the pre-commit hook installation by default.
 # If files are more than 200 we will skip it and just use
 # the further ci steps that already check linting and file casing for the entire repo.
 checks-reporter-with-killswitch "Quick commit checks" \
-  node scripts/precommit_hook.js \
-    --ref HEAD~1..HEAD \
-    --max-files 200
-    --verbose
+  run_quick_commit_checks || {
+    echo "Quick commit checks failed.";
+    echo "You reproduce that locally by running `node scripts/precommit_hook.js --ref HEAD~1..HEAD --max-files 200`";
+    exit 1;
+}
