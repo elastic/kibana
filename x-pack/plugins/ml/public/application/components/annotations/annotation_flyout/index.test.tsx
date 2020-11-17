3575d6a4256de66628a6c9e9678899d29d8a67bb
Quynh Nguyen <quynh.nguyen@elastic.co>
2020-11-17 12:04:38 -0600

    Update test types

--- x-pack/plugins/ml/public/application/components/annotations/annotation_flyout/index.test.tsx
+++ x-pack/plugins/ml/public/application/components/annotations/annotation_flyout/index.test.tsx
@@ -39,8 +39,8 @@ const ObservableComponent = (props: any) => {
   return (
     <AnnotationFlyout
       annotation={annotationProp}
-      {...props}
       annotationUpdatesService={annotationUpdatesService}
+      {...props}
     />
   );
 };
