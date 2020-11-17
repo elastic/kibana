24e21aac089692876fd7f753a23e92f3781d9f41
Devin Hurley <devin.hurley@elastic.co>
2020-11-16 19:47:58 -0500

    remove unnecessary -XPOST and remove verbose mode from post_detections_user script

--- x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/hunter/post_detections_user.sh
+++ x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/hunter/post_detections_user.sh
@@ -7,7 +7,7 @@
 
 USER=(${@:-./detections_user.json})
 
-curl -v -H 'Content-Type: application/json' -H 'kbn-xsrf: 123'\
+curl -H 'Content-Type: application/json' -H 'kbn-xsrf: 123'\
  -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} \
--XPOST ${ELASTICSEARCH_URL}/_security/user/hunter \
+ ${ELASTICSEARCH_URL}/_security/user/hunter \
 -d @${USER}
--- x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/platform_engineer/post_detections_user.sh
+++ x-pack/plugins/security_solution/server/lib/detection_engine/scripts/roles_users/platform_engineer/post_detections_user.sh
@@ -8,7 +8,7 @@
 USER=(${@:-./detections_user.json})
 
 
-curl -v -H 'Content-Type: application/json' -H 'kbn-xsrf: 123'\
+curl -H 'Content-Type: application/json' -H 'kbn-xsrf: 123'\
  -u ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} \
--XPOST ${ELASTICSEARCH_URL}/_security/user/platform_engineer \
+ ${ELASTICSEARCH_URL}/_security/user/platform_engineer \
 -d @${USER}
