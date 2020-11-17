2b743beb327eb3eb2044af2401fcdec9c699601b
Tiago Costa <tiagoffcc@hotmail.com>
2020-11-17 19:12:25 +0000

    chore(NA): fix script to run

--- test/scripts/checks/commit.sh
+++ test/scripts/checks/commit.sh
@@ -15,7 +15,17 @@ run_quick_commit_checks() {
 # If files are more than 200 we will skip it and just use
 # the further ci steps that already check linting and file casing for the entire repo.
 checks-reporter-with-killswitch "Quick commit checks" \
-  run_quick_commit_checks
+  node scripts/precommit_hook.js \
+    --ref HEAD~1..HEAD \
+    --max-files 200 \
+    --verbose || { \
+        echo "Quick commit checks failed."; \
+        echo "You reproduce that locally by running 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --max-files 200'"; \
+        echo "!!!!!!!! ATTENTION !!!!!!!!"; \
+        echo "That check is intended to provide earlier CI feedback after we remove the automatic install for the local pre-commit hook."; \
+        echo "If you want, you can still manually install the pre-commit hook locally by running 'node scripts/register_git_hook locally'"; \
+        exit 1; \
+      } \
 #  run_quick_commit_checks || {
 #    echo "Quick commit checks failed.";
 #    echo "You reproduce that locally by running 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --max-files 200'";
