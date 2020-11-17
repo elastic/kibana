9b5605f4c412c25d9831ba35ea756780ddebdbf7
Jason Stoltzfus <jastoltz24@gmail.com>
2020-11-17 08:09:21 -0500

    [App Search] Added all Document related routes and logic (#83324)

---
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/documents_logic.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { kea, MakeLogicType } from 'kea';
+
+interface DocumentsLogicValues {
+  isDocumentCreationOpen: boolean;
+}
+
+interface DocumentsLogicActions {
+  closeDocumentCreation(): void;
+  openDocumentCreation(): void;
+}
+
+type DocumentsLogicType = MakeLogicType<DocumentsLogicValues, DocumentsLogicActions>;
+
+export const DocumentsLogic = kea<DocumentsLogicType>({
+  path: ['enterprise_search', 'app_search', 'documents_logic'],
+  actions: () => ({
+    openDocumentCreation: true,
+    closeDocumentCreation: true,
+  }),
+  reducers: () => ({
+    isDocumentCreationOpen: [
+      false,
+      {
+        openDocumentCreation: () => true,
+        closeDocumentCreation: () => false,
+      },
+    ],
+  }),
+});
