1d2be917e2018623ea5a0bba41f26e41b13b0d96
Tiago Costa <tiagoffcc@hotmail.com>
2020-11-17 20:14:07 +0000

    chore(NA): update message to log before quick commit checks

--- test/scripts/checks/commit.sh
+++ test/scripts/checks/commit.sh
@@ -2,12 +2,10 @@
 
 source src/dev/ci_setup/setup_env.sh
 
-run_quick_commit_checks() {
-  node scripts/precommit_hook.js \
-    --ref HEAD~1..HEAD \
-    --max-files 200 \
-    --verbose || return 1
-}
+echo "!!!!!!!! ATTENTION !!!!!!!!"; \
+echo "That check is intended to provide earlier CI feedback after we remove the automatic install for the local pre-commit hook.";
+echo "If you want, you can still manually install the pre-commit hook locally by running 'node scripts/register_git_hook locally'";
+echo "!!!!!!!!!!!!!!!!!! !!!!!!!!"; \
 
 # Runs pre-commit hook script for the files touched in the last commit.
 # That way we can ensure a set of quick commit checks earlier as we removed
@@ -18,21 +16,4 @@ checks-reporter-with-killswitch "Quick commit checks" \
   node scripts/precommit_hook.js \
     --ref HEAD~1..HEAD \
     --max-files 200 \
-    --verbose || { \
-        echo "Quick commit checks failed."; \
-        echo "You reproduce that locally by running 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --max-files 200'"; \
-        echo "!!!!!!!! ATTENTION !!!!!!!!"; \
-        echo "That check is intended to provide earlier CI feedback after we remove the automatic install for the local pre-commit hook."; \
-        echo "If you want, you can still manually install the pre-commit hook locally by running 'node scripts/register_git_hook locally'"; \
-        exit 1; \
-      } \
-#  run_quick_commit_checks || {
-#    echo "Quick commit checks failed.";
-#    echo "You reproduce that locally by running 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --max-files 200'";
-#
-#    echo "!!!!!!!! ATTENTION !!!!!!!!"
-#    echo "That check is intended to provide earlier CI feedback after we remove the automatic install for the local pre-commit hook.";
-#    echo "If you want, you can still manually install the pre-commit hook locally by running 'node scripts/register_git_hook locally'";
-#
-#    exit 1;
-# }
+    --verbose
