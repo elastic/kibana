eded64d35fd3a04a131e99588a833f7c1ee2de46
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2020-11-16 16:06:52 -0800

    Removed skip flacky

--- x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/update.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/update.ts
@@ -31,8 +31,7 @@ export default function createUpdateTests({ getService }: FtrProviderContext) {
       .then((response: SupertestResponse) => response.body);
   }
 
-  // FLAKY: https://github.com/elastic/kibana/issues/82804
-  describe.skip('update', () => {
+  describe('update', () => {
     const objectRemover = new ObjectRemover(supertest);
 
     after(() => objectRemover.removeAll());
