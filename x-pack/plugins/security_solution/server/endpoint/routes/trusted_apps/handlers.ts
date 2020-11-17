fccb3bf213588d91a77a858f14cd41ca0a557511
Bohdan Tsymbala <bohdan.tsymbala@elastic.co>
2020-11-17 16:23:01 +0100

    Separated out service layer for trusted apps.

--- x-pack/plugins/security_solution/server/endpoint/routes/trusted_apps/handlers.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/trusted_apps/handlers.ts
@@ -5,16 +5,17 @@
  */
 
 import { RequestHandler, RequestHandlerContext } from 'kibana/server';
+
+import { ExceptionListClient } from '../../../../../lists/server';
+
 import {
   DeleteTrustedAppsRequestParams,
   GetTrustedAppsListRequest,
-  GetTrustedListAppsResponse,
   PostTrustedAppCreateRequest,
 } from '../../../../common/endpoint/types';
+
 import { EndpointAppContext } from '../../types';
-import { exceptionItemToTrustedAppItem, newTrustedAppItemToExceptionItem } from './utils';
-import { ENDPOINT_TRUSTED_APPS_LIST_ID } from '../../../../../lists/common/constants';
-import { ExceptionListClient } from '../../../../../lists/server';
+import { createTrustedApp, deleteTrustedApp, getTrustedAppsList } from './service';
 
 const exceptionListClientFromContext = (context: RequestHandlerContext): ExceptionListClient => {
   const exceptionLists = context.lists?.getExceptionListClient();
@@ -33,16 +34,10 @@ export const getTrustedAppsDeleteRouteHandler = (
 
   return async (context, req, res) => {
     try {
-      const exceptionsListService = exceptionListClientFromContext(context);
-      const { id } = req.params;
-      const response = await exceptionsListService.deleteExceptionListItem({
-        id,
-        itemId: undefined,
-        namespaceType: 'agnostic',
-      });
+      const response = await deleteTrustedApp(exceptionListClientFromContext(context), req.params);
 
       if (response === null) {
-        return res.notFound({ body: `trusted app id [${id}] not found` });
+        return res.notFound({ body: `trusted app id [${req.params.id}] not found` });
       }
 
       return res.ok();
@@ -59,28 +54,10 @@ export const getTrustedAppsListRouteHandler = (
   const logger = endpointAppContext.logFactory.get('trusted_apps');
 
   return async (context, req, res) => {
-    const { page, per_page: perPage } = req.query;
-
     try {
-      const exceptionsListService = exceptionListClientFromContext(context);
-      // Ensure list is created if it does not exist
-      await exceptionsListService.createTrustedAppsList();
-      const results = await exceptionsListService.findExceptionListItem({
-        listId: ENDPOINT_TRUSTED_APPS_LIST_ID,
-        page,
-        perPage,
-        filter: undefined,
-        namespaceType: 'agnostic',
-        sortField: 'name',
-        sortOrder: 'asc',
+      return res.ok({
+        body: await getTrustedAppsList(exceptionListClientFromContext(context), req.query),
       });
-      const body: GetTrustedListAppsResponse = {
-        data: results?.data.map(exceptionItemToTrustedAppItem) ?? [],
-        total: results?.total ?? 0,
-        page: results?.page ?? 1,
-        per_page: results?.per_page ?? perPage!,
-      };
-      return res.ok({ body });
     } catch (error) {
       logger.error(error);
       return res.internalError({ body: error });
@@ -94,21 +71,9 @@ export const getTrustedAppsCreateRouteHandler = (
   const logger = endpointAppContext.logFactory.get('trusted_apps');
 
   return async (context, req, res) => {
-    const newTrustedApp = req.body;
-
     try {
-      const exceptionsListService = exceptionListClientFromContext(context);
-      // Ensure list is created if it does not exist
-      await exceptionsListService.createTrustedAppsList();
-
-      const createdTrustedAppExceptionItem = await exceptionsListService.createExceptionListItem(
-        newTrustedAppItemToExceptionItem(newTrustedApp)
-      );
-
       return res.ok({
-        body: {
-          data: exceptionItemToTrustedAppItem(createdTrustedAppExceptionItem),
-        },
+        body: await createTrustedApp(exceptionListClientFromContext(context), req.body),
       });
     } catch (error) {
       logger.error(error);
---
+++ x-pack/plugins/security_solution/server/endpoint/routes/trusted_apps/service.ts
@@ -0,0 +1,68 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { ExceptionListClient } from '../../../../../lists/server';
+import { ENDPOINT_TRUSTED_APPS_LIST_ID } from '../../../../../lists/common/constants';
+
+import {
+  DeleteTrustedAppsRequestParams,
+  GetTrustedAppsListRequest,
+  GetTrustedListAppsResponse,
+  PostTrustedAppCreateRequest,
+  PostTrustedAppCreateResponse,
+} from '../../../../common/endpoint/types';
+
+import { exceptionItemToTrustedAppItem, newTrustedAppItemToExceptionItem } from './utils';
+
+export const deleteTrustedApp = async (
+  exceptionsListClient: ExceptionListClient,
+  { id }: DeleteTrustedAppsRequestParams
+) => {
+  await exceptionsListClient.deleteExceptionListItem({
+    id,
+    itemId: undefined,
+    namespaceType: 'agnostic',
+  });
+};
+
+export const getTrustedAppsList = async (
+  exceptionsListClient: ExceptionListClient,
+  { page, per_page: perPage }: GetTrustedAppsListRequest
+): Promise<GetTrustedListAppsResponse> => {
+  // Ensure list is created if it does not exist
+  await exceptionsListClient.createTrustedAppsList();
+
+  const results = await exceptionsListClient.findExceptionListItem({
+    listId: ENDPOINT_TRUSTED_APPS_LIST_ID,
+    page,
+    perPage,
+    filter: undefined,
+    namespaceType: 'agnostic',
+    sortField: 'name',
+    sortOrder: 'asc',
+  });
+
+  return {
+    data: results?.data.map(exceptionItemToTrustedAppItem) ?? [],
+    total: results?.total ?? 0,
+    page: results?.page ?? 1,
+    per_page: results?.per_page ?? perPage!,
+  };
+};
+
+export const createTrustedApp = async (
+  exceptionsListClient: ExceptionListClient,
+  newTrustedApp: PostTrustedAppCreateRequest
+): Promise<PostTrustedAppCreateResponse> => {
+  // Ensure list is created if it does not exist
+  await exceptionsListClient.createTrustedAppsList();
+
+  const createdTrustedAppExceptionItem = await exceptionsListClient.createExceptionListItem(
+    newTrustedAppItemToExceptionItem(newTrustedApp)
+  );
+
+  return { data: exceptionItemToTrustedAppItem(createdTrustedAppExceptionItem) };
+};
