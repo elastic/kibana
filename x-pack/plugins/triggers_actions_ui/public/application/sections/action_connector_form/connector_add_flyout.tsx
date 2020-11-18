23aba7047cb2157be2df1696b98a604a6e1628f6
restrry <restrry@gmail.com>
2020-11-18 18:17:15 +0300

    fix a new error

--- x-pack/plugins/triggers_actions_ui/public/application/sections/action_connector_form/connector_add_flyout.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/action_connector_form/connector_add_flyout.tsx
@@ -56,7 +56,6 @@ export const ConnectorAddFlyout = ({
   const [hasActionsUpgradeableByTrial, setHasActionsUpgradeableByTrial] = useState<boolean>(false);
 
   // hooks
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const initialConnector = {
     actionTypeId: actionType?.id ?? '',
     config: {},
