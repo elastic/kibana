18e8ae07bdd5c0c7958ec7ad520087fcefa79e73
Tiago Costa <tiagoffcc@hotmail.com>
2020-11-18 03:40:28 +0000

    chore(NA): format message to log

--- test/scripts/checks/commit/commit_check_runner.sh
+++ test/scripts/checks/commit/commit_check_runner.sh
@@ -4,7 +4,7 @@ run_quick_commit_checks() {
   echo "!!!!!!!! ATTENTION !!!!!!!!
 That check is intended to provide earlier CI feedback after we remove the automatic install for the local pre-commit hook.
 If you want, you can still manually install the pre-commit hook locally by running 'node scripts/register_git_hook locally'
-!!!!!!!!!!!!!!!!!! !!!!!!!!"
+!!!!!!!!!!!!!!!!!! !!!!!!!!\n"
 
   node scripts/precommit_hook.js --ref HEAD~1..HEAD --max-files 200 --verbose
 }
