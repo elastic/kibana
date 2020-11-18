b9a90931d270b3a7902cc4fd595c20baa15cea46
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-11-18 14:47:46 -0700

    [Maps] Add query bar inputs to geo threshold alerts tracked points & boundaries (#80871)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alert_details/components/alert_details.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alert_details/components/alert_details.tsx
@@ -69,7 +69,7 @@ export const AlertDetails: React.FunctionComponent<AlertDetailsProps> = ({
     uiSettings,
     docLinks,
     charts,
-    dataPlugin,
+    data,
     setBreadcrumbs,
     chrome,
   } = useAppDependencies();
@@ -157,11 +157,11 @@ export const AlertDetails: React.FunctionComponent<AlertDetailsProps> = ({
                             uiSettings,
                             docLinks,
                             charts,
-                            dataFieldsFormats: dataPlugin.fieldFormats,
+                            dataFieldsFormats: data.fieldFormats,
                             reloadAlerts: setAlert,
                             capabilities,
-                            dataUi: dataPlugin.ui,
-                            dataIndexPatterns: dataPlugin.indexPatterns,
+                            dataUi: data.ui,
+                            dataIndexPatterns: data.indexPatterns,
                           }}
                         >
                           <AlertEdit
