9b3a7a677c9569fbaeb9b3c4f0388add452443c9
Jason Stoltzfus <jastoltz24@gmail.com>
2020-11-18 15:34:54 -0500

    Added search-ui and ensured connectivity

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/search_experience.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/search_experience.tsx
@@ -4,9 +4,44 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React from 'react';
+import { useValues } from 'kea';
+// @ts-expect-error types are not available for this package yet
+import { SearchProvider, SearchBox, Results } from '@elastic/react-search-ui';
+// @ts-expect-error types are not available for this package yet
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
 
 import './search_experience.scss';
 
-export const SearchExperience: React.FC = () => (
-  <div className="documents-search-experience">Search Experience</div>
-);
+import { EngineLogic } from '../../engine';
+import { externalUrl } from '../../../../shared/enterprise_search_url';
+
+export const SearchExperience: React.FC = () => {
+  const { engine } = useValues(EngineLogic);
+  const endpointBase = externalUrl.enterpriseSearchUrl;
+
+  const connector = new AppSearchAPIConnector({
+    cacheResponses: false,
+    endpointBase,
+    engineName: engine.name,
+    searchKey: engine.apiKey,
+  });
+
+  const searchProviderConfig = {
+    alwaysSearchOnInitialLoad: true,
+    apiConnector: connector,
+    trackUrlState: false,
+    initialState: {
+      sortDirection: 'desc',
+      sortField: 'id',
+    },
+  };
+
+  return (
+    <div className="documents-search-experience">
+      <SearchProvider config={searchProviderConfig}>
+        <SearchBox />
+        <Results />
+      </SearchProvider>
+    </div>
+  );
+};
