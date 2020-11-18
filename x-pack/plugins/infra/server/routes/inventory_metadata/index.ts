eafc72b8a758f83ef253c009b3e15137aaaa1870
Chris Cowan <chris@chriscowan.us>
2020-11-18 12:06:29 -0700

    Only query inventory metadata for AWS

--- x-pack/plugins/infra/server/routes/inventory_metadata/index.ts
+++ x-pack/plugins/infra/server/routes/inventory_metadata/index.ts
@@ -9,6 +9,7 @@ import Boom from '@hapi/boom';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { fold } from 'fp-ts/lib/Either';
 import { identity } from 'fp-ts/lib/function';
+import { findInventoryModel } from '../../../common/inventory_models';
 import { InfraBackendLibs } from '../../lib/infra_types';
 import { throwErrors } from '../../../common/runtime_types';
 
@@ -38,10 +39,24 @@ export const initInventoryMetaRoute = (libs: InfraBackendLibs) => {
           fold(throwErrors(Boom.badRequest), identity)
         );
 
+        const model = findInventoryModel(nodeType);
+
+        // Only run this for AWS modules, eventually we might have more.
+        if (model.requiredModule !== 'aws') {
+          return response.ok({
+            body: {
+              accounts: [],
+              projects: [],
+              regions: [],
+            },
+          });
+        }
+
         const { configuration } = await libs.sources.getSourceConfiguration(
           requestContext.core.savedObjects.client,
           sourceId
         );
+
         const awsMetadata = await getCloudMetadata(
           framework,
           requestContext,
