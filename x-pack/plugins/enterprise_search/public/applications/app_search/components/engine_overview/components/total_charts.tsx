e9b0609f398281cca9af3e0bb793dc4bbc288eaf
Constance <constancecchen@users.noreply.github.com>
2020-11-18 15:53:45 -0800

    [App Search] Engine overview layout stub (#83504)
    * Set up Overview file
    
    * Finish Overview page logic, stub out empty/metric views
    
    * Stub in basic empty engine overview
    - Minus document creation button & API code example
    
    * Stub out EngineOverviewMetrics and unavailable empty prompt
    
    * Stub out EngineOverMetrics components (stats, charts, logs)
    
    * [Refactor] Pull out some document creation i18n strings to constants
    
    - They're repeated/reused by the DocumentCreationPopover component
    
    * PR feedback: Drop the regex
    
    * PR feedback: RecentLogs -> RecentApiLogs
    
    * PR feedback: Copy
    
    * PR feedback: Copy, sentence-casing

---
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/engine_overview/components/total_charts.tsx
@@ -0,0 +1,99 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { useValues } from 'kea';
+
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPageContent,
+  EuiPageContentHeader,
+  EuiPageContentHeaderSection,
+  EuiPageContentBody,
+  EuiTitle,
+  EuiText,
+} from '@elastic/eui';
+
+import { EuiButton } from '../../../../shared/react_router_helpers';
+
+import { ENGINE_ANALYTICS_PATH, ENGINE_API_LOGS_PATH, getEngineRoute } from '../../../routes';
+import { TOTAL_QUERIES, TOTAL_API_OPERATIONS } from '../../analytics/constants';
+import { VIEW_ANALYTICS, VIEW_API_LOGS, LAST_7_DAYS } from '../constants';
+
+import { EngineLogic } from '../../engine';
+import { EngineOverviewLogic } from '../';
+
+export const TotalCharts: React.FC = () => {
+  const { engineName } = useValues(EngineLogic);
+  const engineRoute = getEngineRoute(engineName);
+
+  const {
+    // startDate,
+    // endDate,
+    // queriesPerDay,
+    // operationsPerDay,
+  } = useValues(EngineOverviewLogic);
+
+  return (
+    <EuiFlexGroup>
+      <EuiFlexItem>
+        <EuiPageContent data-test-subj="TotalQueriesChart">
+          <EuiPageContentHeader responsive={false}>
+            <EuiPageContentHeaderSection>
+              <EuiTitle size="xs">
+                <h2>{TOTAL_QUERIES}</h2>
+              </EuiTitle>
+              <EuiText size="s" color="subdued">
+                {LAST_7_DAYS}
+              </EuiText>
+            </EuiPageContentHeaderSection>
+            <EuiPageContentHeaderSection>
+              <EuiButton to={engineRoute + ENGINE_ANALYTICS_PATH} size="s">
+                {VIEW_ANALYTICS}
+              </EuiButton>
+            </EuiPageContentHeaderSection>
+          </EuiPageContentHeader>
+          <EuiPageContentBody>
+            TODO: Analytics chart
+            {/* <EngineAnalytics
+              data={[queriesPerDay]}
+              startDate={new Date(startDate)}
+              endDate={new Date(endDate)}
+            /> */}
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiPageContent data-test-subj="TotalApiOperationsChart">
+          <EuiPageContentHeader responsive={false}>
+            <EuiPageContentHeaderSection>
+              <EuiTitle size="xs">
+                <h2>{TOTAL_API_OPERATIONS}</h2>
+              </EuiTitle>
+              <EuiText size="s" color="subdued">
+                {LAST_7_DAYS}
+              </EuiText>
+            </EuiPageContentHeaderSection>
+            <EuiPageContentHeaderSection>
+              <EuiButton to={engineRoute + ENGINE_API_LOGS_PATH} size="s">
+                {VIEW_API_LOGS}
+              </EuiButton>
+            </EuiPageContentHeaderSection>
+          </EuiPageContentHeader>
+          <EuiPageContentBody>
+            TODO: API Logs chart
+            {/* <EngineAnalytics
+              data={[operationsPerDay]}
+              startDate={new Date(startDate)}
+              endDate={new Date(endDate)}
+            /> */}
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
