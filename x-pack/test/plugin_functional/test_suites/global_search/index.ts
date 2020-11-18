b2e661e005edbe42b7b6a414ced4b06a4273e25f
pgayvallet <pierre.gayvallet@elastic.co>
2020-11-18 13:11:35 +0100

    move away from CI group 7

--- x-pack/test/plugin_functional/test_suites/global_search/index.ts
+++ x-pack/test/plugin_functional/test_suites/global_search/index.ts
@@ -8,7 +8,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('GlobalSearch API', function () {
-    this.tags('ciGroup7');
+    this.tags('ciGroup10');
     loadTestFile(require.resolve('./global_search_api'));
     loadTestFile(require.resolve('./global_search_providers'));
     loadTestFile(require.resolve('./global_search_bar'));
