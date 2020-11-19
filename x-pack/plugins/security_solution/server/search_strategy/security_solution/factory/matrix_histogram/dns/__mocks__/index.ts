cdb1b9fff654aeef41da512065589e2032924930
Angela Chuang <yi-chun.chuang@elastic.co>
2020-11-19 09:33:38 +0000

    cleanup types

--- x-pack/plugins/security_solution/server/search_strategy/security_solution/factory/matrix_histogram/dns/__mocks__/index.ts
+++ x-pack/plugins/security_solution/server/search_strategy/security_solution/factory/matrix_histogram/dns/__mocks__/index.ts
@@ -4,11 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import {
-  Direction,
-  MatrixHistogramType,
-  NetworkDnsFields,
-} from '../../../../../../../common/search_strategy';
+import { MatrixHistogramType } from '../../../../../../../common/search_strategy';
 
 export const mockOptions = {
   defaultIndex: [
@@ -23,7 +19,6 @@ export const mockOptions = {
   filterQuery: '{"bool":{"must":[],"filter":[{"match_all":{}}],"should":[],"must_not":[]}}',
   histogramType: MatrixHistogramType.dns,
   isPtrIncluded: false,
-  sort: { field: NetworkDnsFields.uniqueDomains, direction: Direction.desc },
   timerange: { interval: '12h', from: '2020-09-08T15:41:15.528Z', to: '2020-09-09T15:41:15.529Z' },
   stackByField: 'dns.question.registered_domain',
 };
