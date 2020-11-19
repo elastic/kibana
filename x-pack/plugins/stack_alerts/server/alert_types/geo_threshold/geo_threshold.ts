d6c5323766f7418691b6e35309da5cd1e4e0adb0
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-11-19 07:52:51 -0700

    [7.x] [Maps] Add query bar inputs to geo threshold alerts tracked points & boundaries (#80871) (#83727)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/stack_alerts/server/alert_types/geo_threshold/geo_threshold.ts
+++ x-pack/plugins/stack_alerts/server/alert_types/geo_threshold/geo_threshold.ts
@@ -197,7 +197,8 @@ export const getGeoThresholdExecutor = (log: Logger) =>
           services.callCluster,
           log,
           alertId,
-          params.boundaryNameField
+          params.boundaryNameField,
+          params.boundaryIndexQuery
         );
 
     const executeEsQuery = await executeEsQueryFactory(params, services, log, shapesFilters);
