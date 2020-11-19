1d268d87c1cba1ea91d3fea46103491b4b7961a6
Nathan Reese <reese.nathan@gmail.com>
2020-11-19 09:38:41 -0700

    [Maps] background color map setting

--- x-pack/plugins/maps/public/reducers/default_map_settings.ts
+++ x-pack/plugins/maps/public/reducers/default_map_settings.ts
@@ -4,12 +4,18 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import euiDarkVars from '@elastic/eui/dist/eui_theme_dark.json';
+import euiLightVars from '@elastic/eui/dist/eui_theme_light.json';
 import { INITIAL_LOCATION, MAX_ZOOM, MIN_ZOOM } from '../../common/constants';
 import { MapSettings } from './map';
+import { getIsDarkMode } from '../kibana_services';
 
 export function getDefaultMapSettings(): MapSettings {
   return {
     autoFitToDataBounds: false,
+    backgroundColor: getIsDarkMode()
+      ? euiDarkVars.euiColorEmptyShade
+      : euiLightVars.euiColorEmptyShade,
     initialLocation: INITIAL_LOCATION.LAST_SAVED_LOCATION,
     fixedLocation: { lat: 0, lon: 0, zoom: 2 },
     browserLocation: { zoom: 2 },
