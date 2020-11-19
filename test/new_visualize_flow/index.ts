b64704514f3b13780f6f6225709a715251851a2c
Matt Kime <matt@mattki.me>
2020-11-19 12:47:41 -0600

    fix functional test by supplying needed index

--- test/new_visualize_flow/index.ts
+++ test/new_visualize_flow/index.ts
@@ -19,9 +19,15 @@
 import { FtrProviderContext } from '../functional/ftr_provider_context';
 
 // eslint-disable-next-line import/no-default-export
-export default function ({ loadTestFile }: FtrProviderContext) {
+export default function ({ loadTestFile, getService }: FtrProviderContext) {
   describe('New Visualize Flow', function () {
     this.tags('ciGroup2');
+    const esArchiver = getService('esArchiver');
+    before(async () => {
+      await esArchiver.loadIfNeeded('logstash_functional');
+      // await esArchiver.loadIfNeeded('dashboard/current');
+    });
+
     loadTestFile(require.resolve('./dashboard_embedding'));
   });
 }
