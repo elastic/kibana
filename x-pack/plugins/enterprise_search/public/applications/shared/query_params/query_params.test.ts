94fc89e51d295f30ea0c68aceb464892fda9e33d
scottybollinger <scotty.bollinger@elastic.co>
2020-11-19 10:40:05 -0600

    Actually test functionality of query-string

--- x-pack/plugins/enterprise_search/public/applications/shared/query_params/query_params.test.ts
+++ x-pack/plugins/enterprise_search/public/applications/shared/query_params/query_params.test.ts
@@ -4,16 +4,10 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import queryString from 'query-string';
-
 import { parseQueryParams } from './';
 
 describe('parseQueryParams', () => {
   it('should call queryString parse method', () => {
-    const parseMock = jest.fn();
-    jest.spyOn(queryString, 'parse').mockImplementation(parseMock);
-    parseQueryParams('?foo=bar');
-
-    expect(queryString.parse).toHaveBeenCalledWith('?foo=bar', { arrayFormat: 'bracket' });
+    expect(parseQueryParams('?foo=bar')).toEqual({ foo: 'bar' });
   });
 });
