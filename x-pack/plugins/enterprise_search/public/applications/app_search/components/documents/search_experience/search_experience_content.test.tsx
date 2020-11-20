7b1fe540df5ddb6cbec1cf63c38c28a681c4c14b
Jason Stoltzfus <jastoltz24@gmail.com>
2020-11-20 11:03:33 -0500

    Handle no results cases in search

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/search_experience_content.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/search_experience_content.test.tsx
@@ -3,7 +3,10 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
+
 import { setMockValues } from '../../../../__mocks__/kea.mock';
+import { setMockSearchContextState } from './__mocks__/hooks.mock';
+
 import React from 'react';
 
 import { shallow } from 'enzyme';
@@ -15,12 +18,21 @@ import { ResultView } from './views';
 import { SearchExperienceContent } from './search_experience_content';
 
 describe('SearchExperienceContent', () => {
+  const searchState = {
+    resultSearchTerm: 'searchTerm',
+    totalResults: 100,
+    wasSearched: true,
+  };
   const values = {
     engineName: 'engine1',
+    isMetaEngine: false,
+    myRole: { canManageEngineDocuments: true },
   };
+
   beforeEach(() => {
     jest.clearAllMocks();
     setMockValues(values);
+    setMockSearchContextState(searchState);
   });
 
   it('renders', () => {
@@ -46,5 +58,103 @@ describe('SearchExperienceContent', () => {
     const wrapper = shallow(<SearchExperienceContent />);
     const pagination = wrapper.find('Pagination').at(0).dive();
     expect(pagination.find(EuiFlexGroup).length).toBe(1);
+  });
+
+  it('renders empty if a search was not performed yet', () => {
+    setMockSearchContextState({
+      ...searchState,
+      wasSearched: false,
+    });
+    const wrapper = shallow(<SearchExperienceContent />);
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  it('renders results if a search was performend and there are more than 0 totalResults', () => {
+    setMockSearchContextState({
+      ...searchState,
+      wasSearched: true,
+      totalResults: 10,
+    });
+    const wrapper = shallow(<SearchExperienceContent />);
+    expect(wrapper.find('[data-test-subj="documentsSearchResults"]').length).toBe(1);
+  });
+
+  it('renders a no results message if a non-empty search was performed and there are no results', () => {
+    setMockSearchContextState({
+      ...searchState,
+      resultSearchTerm: 'searchTerm',
+      wasSearched: true,
+      totalResults: 0,
+    });
+    const wrapper = shallow(<SearchExperienceContent />);
+    expect(wrapper.find('[data-test-subj="documentsSearchResults"]').length).toBe(0);
+    expect(wrapper.find('[data-test-subj="documentsSearchNoResults"]').length).toBe(1);
+  });
+
+  describe('when an empty search was performed and there are no results, meaning there are no documents indexed', () => {
+    beforeEach(() => {
+      setMockSearchContextState({
+        ...searchState,
+        resultSearchTerm: '',
+        wasSearched: true,
+        totalResults: 0,
+      });
+    });
+    it('renders a no documents message', () => {
+      const wrapper = shallow(<SearchExperienceContent />);
+      expect(wrapper.find('[data-test-subj="documentsSearchResults"]').length).toBe(0);
+      expect(wrapper.find('[data-test-subj="documentsSearchNoDocuments"]').length).toBe(1);
+    });
+
+    it('will include a button to index new documents', () => {
+      const wrapper = shallow(<SearchExperienceContent />);
+      const noDocumentsErrorActions: any = wrapper
+        .find('[data-test-subj="documentsSearchNoDocuments"]')
+        .prop('actions');
+
+      expect(
+        shallow(noDocumentsErrorActions).find('[data-test-subj="IndexDocumentsButton"]').length
+      ).toBe(1);
+    });
+
+    it('will include a button to documentation if this is a meta engine', () => {
+      setMockValues({
+        ...values,
+        isMetaEngine: true,
+      });
+
+      const wrapper = shallow(<SearchExperienceContent />);
+
+      const noDocumentsErrorActions: any = wrapper
+        .find('[data-test-subj="documentsSearchNoDocuments"]')
+        .prop('actions');
+
+      expect(
+        shallow(noDocumentsErrorActions).find('[data-test-subj="IndexDocumentsButton"]').length
+      ).toBe(0);
+      expect(
+        shallow(noDocumentsErrorActions).find('[data-test-subj="documentsSearchDocsLink"]').length
+      ).toBe(1);
+    });
+
+    it('will include a button to documentation if the user cannot manage documents', () => {
+      setMockValues({
+        ...values,
+        myRole: { canManageEngineDocuments: false },
+      });
+
+      const wrapper = shallow(<SearchExperienceContent />);
+
+      const noDocumentsErrorActions: any = wrapper
+        .find('[data-test-subj="documentsSearchNoDocuments"]')
+        .prop('actions');
+
+      expect(
+        shallow(noDocumentsErrorActions).find('[data-test-subj="IndexDocumentsButton"]').length
+      ).toBe(0);
+      expect(
+        shallow(noDocumentsErrorActions).find('[data-test-subj="documentsSearchDocsLink"]').length
+      ).toBe(1);
+    });
   });
 });
--- x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/search_experience_content.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/search_experience_content.tsx
@@ -7,13 +7,17 @@
 import React from 'react';
 
 import { i18n } from '@kbn/i18n';
-import { EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiButton, EuiEmptyPrompt } from '@elastic/eui';
 // @ts-expect-error types are not available for this package yet
 import { Results, Paging, ResultsPerPage } from '@elastic/react-search-ui';
 import { useValues } from 'kea';
 
-import { EngineLogic } from '../../engine';
 import { ResultView, PagingView, ResultsPerPageView } from './views';
+import { useSearchContextState } from './hooks';
+import { DocumentCreationButton } from '../document_creation_button';
+import { AppLogic } from '../../../app_logic';
+import { EngineLogic } from '../../engine';
+import { DOCS_PREFIX } from '../../../routes';
 
 const Pagination: React.FC<{ 'aria-label': string }> = ({ 'aria-label': ariaLabel }) => (
   <EuiFlexGroup alignItems="center" className="documents-search-experience__paging-info">
@@ -32,35 +36,100 @@ interface Result {
   };
 }
 
-export const SearchExperienceContent: React.FC = () => {
-  const { engineName } = useValues(EngineLogic);
+const DocumentationLink: React.FC = () => (
+  <EuiButton
+    data-test-subj="documentsSearchDocsLink"
+    href={`${DOCS_PREFIX}/indexing-documents-guide.html`}
+  >
+    {i18n.translate('xpack.enterpriseSearch.appSearch.documents.search.indexingGuide', {
+      defaultMessage: 'Read the Indexing Guide',
+    })}
+  </EuiButton>
+);
 
+export const SearchExperienceContent: React.FC = () => {
+  const { resultSearchTerm, totalResults, wasSearched } = useSearchContextState();
+
+  const { myRole } = useValues(AppLogic);
+  const { engineName, isMetaEngine } = useValues(EngineLogic);
+
+  if (!wasSearched) {
+    return null;
+  }
+
+  if (totalResults) {
+    return (
+      <EuiFlexGroup direction="column" gutterSize="none" data-test-subj="documentsSearchResults">
+        <Pagination
+          aria-label={i18n.translate(
+            'xpack.enterpriseSearch.appSearch.documents.paging.ariaLabelTop',
+            {
+              defaultMessage: 'Search results paging at top of results',
+            }
+          )}
+        />
+        <EuiSpacer />
+        <Results
+          titleField="id"
+          resultView={(props: { result: Result }) => {
+            return <ResultView {...props} engineName={engineName} />;
+          }}
+        />
+        <EuiSpacer />
+        <Pagination
+          aria-label={i18n.translate(
+            'xpack.enterpriseSearch.appSearch.documents.paging.ariaLabelBottom',
+            {
+              defaultMessage: 'Search results paging at bottom of results',
+            }
+          )}
+        />
+      </EuiFlexGroup>
+    );
+  }
+
+  // If we have no results, but have a search term, show a message
+  if (resultSearchTerm) {
+    return (
+      <EuiEmptyPrompt
+        data-test-subj="documentsSearchNoResults"
+        body={i18n.translate('xpack.enterpriseSearch.appSearch.documents.search.noResults', {
+          defaultMessage: 'No results for "{resultSearchTerm}" yet!',
+          values: {
+            resultSearchTerm,
+          },
+        })}
+      />
+    );
+  }
+
+  // If we have no results AND no search term, show a CTA for the user to index documents
   return (
-    <EuiFlexGroup direction="column" gutterSize="none">
-      <Pagination
-        aria-label={i18n.translate(
-          'xpack.enterpriseSearch.appSearch.documents.paging.ariaLabelTop',
-          {
-            defaultMessage: 'Search results paging at top of results',
-          }
-        )}
+    <>
+      <div />
+      <EuiEmptyPrompt
+        data-test-subj="documentsSearchNoDocuments"
+        title={
+          <h2>
+            {i18n.translate(
+              'xpack.enterpriseSearch.appSearch.documents.search.indexDocumentsTitle',
+              {
+                defaultMessage: 'No documents yet!',
+              }
+            )}
+          </h2>
+        }
+        body={i18n.translate('xpack.enterpriseSearch.appSearch.documents.search.indexDocuments', {
+          defaultMessage: 'Indexed documents will show up here.',
+        })}
+        actions={
+          !isMetaEngine && myRole.canManageEngineDocuments ? (
+            <DocumentCreationButton />
+          ) : (
+            <DocumentationLink />
+          )
+        }
       />
-      <EuiSpacer />
-      <Results
-        titleField="id"
-        resultView={(props: { result: Result }) => {
-          return <ResultView {...props} engineName={engineName} />;
-        }}
-      />
-      <EuiSpacer />
-      <Pagination
-        aria-label={i18n.translate(
-          'xpack.enterpriseSearch.appSearch.documents.paging.ariaLabelBottom',
-          {
-            defaultMessage: 'Search results paging at bottom of results',
-          }
-        )}
-      />
-    </EuiFlexGroup>
+    </>
   );
 };
