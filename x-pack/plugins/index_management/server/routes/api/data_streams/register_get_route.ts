25ea117636e0e7673df8f6d2c56c9043aec382be
Yulia Cech <yulia.cech@elastic.co>
2020-11-20 17:13:24 +0100

    Fixed type check issue

--- x-pack/plugins/index_management/server/routes/api/data_streams/register_get_route.ts
+++ x-pack/plugins/index_management/server/routes/api/data_streams/register_get_route.ts
@@ -187,7 +187,7 @@ export function registerGetOneRoute({
 
         return response.notFound();
       } catch (error) {
-        handleEsError({ error, response });
+        return handleEsError({ error, response });
       }
     })
   );
