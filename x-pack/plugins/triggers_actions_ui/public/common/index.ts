95d2f797fdd24fbbc6fb2070a35b44d1e799369d
Christos Nasikas <christos.nasikas@elastic.co>
2020-11-21 12:44:47 +0200

    Remove exports

--- x-pack/plugins/triggers_actions_ui/public/common/index.ts
+++ x-pack/plugins/triggers_actions_ui/public/common/index.ts
@@ -13,7 +13,3 @@ export * from './types';
 export { connectorConfiguration as ServiceNowConnectorConfiguration } from '../application/components/builtin_action_types/servicenow/config';
 export { connectorConfiguration as JiraConnectorConfiguration } from '../application/components/builtin_action_types/jira/config';
 export { connectorConfiguration as ResilientConnectorConfiguration } from '../application/components/builtin_action_types/resilient/config';
-
-export { TextAreaWithMessageVariables } from '../application/components/text_area_with_message_variables';
-export { TextFieldWithMessageVariables } from '../application/components/text_field_with_message_variables';
-export { AddMessageVariables } from '../application/components/add_message_variables';
