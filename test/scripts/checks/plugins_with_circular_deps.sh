30fd6205be57604fd20f47d7e9ac3a6133a41416
Tiago Costa <tiagoffcc@hotmail.com>
2020-11-22 00:26:23 +0000

    chore(NA): remove quiet flag

--- test/scripts/checks/plugins_with_circular_deps.sh
+++ test/scripts/checks/plugins_with_circular_deps.sh
@@ -3,5 +3,4 @@
 source src/dev/ci_setup/setup_env.sh
 
 checks-reporter-with-killswitch "Check plugins with circular dependencies" \
-  node scripts/find_plugins_with_circular_deps \
-    --quiet
+  node scripts/find_plugins_with_circular_deps
