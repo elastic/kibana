b11b3d89d715c2c51270717c84ff709761190481
Ying Mao <ying.mao@elastic.co>
2020-11-23 11:09:28 -0500

    Unskipping update test

--- x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/update.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/update.ts
@@ -32,7 +32,7 @@ export default function createUpdateTests({ getService }: FtrProviderContext) {
   }
 
   // FLAKY: https://github.com/elastic/kibana/issues/82804
-  describe.skip('update', () => {
+  describe('update', () => {
     const objectRemover = new ObjectRemover(supertest);
 
     after(() => objectRemover.removeAll());
