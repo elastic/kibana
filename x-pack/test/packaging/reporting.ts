f54a148c6dd7e2553f671e9b7b06a041ffa3c99f
Jonathan Budzenski <jon@budzenski.me>
2020-11-23 12:48:39 -0600

    [package testing] Create smoke test suite
    This is the initial test suite that will be used for verifying package builds.
    The design goal is to verify that Kibana and its dependencies are
    functional.  For now, this validates that Kibana is running and can
    generate reports.
    
    I'm opening this against the current testing framework for feedback and
    to make sure the tests are stable. When the groundwork for package
    automation is available these tests will be switched over to run against
    remote instances.

---
+++ x-pack/test/packaging/reporting.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from 'test/functional/ftr_provider_context';
+
+// eslint-disable-next-line import/no-default-export
+export default function ({ getService, getPageObjects }: FtrProviderContext) {
+  const pageObjects = getPageObjects(['dashboard', 'common', 'reporting']);
+  const es = getService('es');
+  const esArchiver = getService('esArchiver');
+
+  describe('Reporting', () => {
+    before(async () => {
+      await esArchiver.loadIfNeeded('packaging');
+    });
+
+    after(async () => {
+      await esArchiver.unload('packaging');
+      await es.deleteByQuery({
+        index: '.reporting-*',
+        refresh: true,
+        body: { query: { match_all: {} } },
+      });
+    });
+
+    it('downloaded PDF has OK status', async function () {
+      // Generating and then comparing reports can take longer than the default 60s timeout
+      this.timeout(180000);
+
+      await pageObjects.common.navigateToApp('dashboards');
+      await pageObjects.dashboard.loadSavedDashboard('dashboard');
+      await pageObjects.reporting.openPdfReportingPanel();
+      await pageObjects.reporting.clickGenerateReportButton();
+
+      const url = await pageObjects.reporting.getReportURL(60000);
+      const res = await pageObjects.reporting.getResponse(url);
+
+      expect(res.status).to.equal(200);
+      expect(res.get('content-type')).to.equal('application/pdf');
+    });
+  });
+}
