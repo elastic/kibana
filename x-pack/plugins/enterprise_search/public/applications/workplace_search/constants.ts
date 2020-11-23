07ac5a5b49208a8988d941aa20aa14bdb835bf79
scottybollinger <scotty.bollinger@elastic.co>
2020-11-23 15:19:11 -0600

    Add SourceSubNav component

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/constants.ts
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/constants.ts
@@ -25,11 +25,20 @@ export const NAV = {
     'xpack.enterpriseSearch.workplaceSearch.nav.groups.sourcePrioritization',
     { defaultMessage: 'Source Prioritization' }
   ),
+  CONTENT: i18n.translate('xpack.enterpriseSearch.workplaceSearch.nav.content', {
+    defaultMessage: 'Content',
+  }),
   ROLE_MAPPINGS: i18n.translate('xpack.enterpriseSearch.workplaceSearch.nav.roleMappings', {
     defaultMessage: 'Role Mappings',
   }),
   SECURITY: i18n.translate('xpack.enterpriseSearch.workplaceSearch.nav.security', {
     defaultMessage: 'Security',
+  }),
+  SCHEMA: i18n.translate('xpack.enterpriseSearch.workplaceSearch.nav.schema', {
+    defaultMessage: 'Schema',
+  }),
+  DISPLAY_SETTINGS: i18n.translate('xpack.enterpriseSearch.workplaceSearch.nav.displaySettings', {
+    defaultMessage: 'Display Settings',
   }),
   SETTINGS: i18n.translate('xpack.enterpriseSearch.workplaceSearch.nav.settings', {
     defaultMessage: 'Settings',
