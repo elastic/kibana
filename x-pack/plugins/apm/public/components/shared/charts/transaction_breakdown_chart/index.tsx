c71dab6ff1ba7b026defe5cac678ba75559782be
Nathan L Smith <nathan.smith@elastic.co>
2020-11-24 06:37:24 -0600

    Transaction type handling and breakdown chart (#83587)

--- x-pack/plugins/apm/public/components/shared/charts/transaction_breakdown_chart/index.tsx
+++ x-pack/plugins/apm/public/components/shared/charts/transaction_breakdown_chart/index.tsx
@@ -6,10 +6,16 @@
 import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiTitle } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
-import { useTransactionBreakdown } from '../../../hooks/useTransactionBreakdown';
-import { TransactionBreakdownGraph } from './TransactionBreakdownGraph';
+import { useTransactionBreakdown } from '../../../../hooks/use_transaction_breakdown';
+import { TransactionBreakdownChartContents } from './transaction_breakdown_chart_contents';
 
-function TransactionBreakdown() {
+export function TransactionBreakdownChart({
+  height,
+  showAnnotations = true,
+}: {
+  height?: number;
+  showAnnotations?: boolean;
+}) {
   const { data, status } = useTransactionBreakdown();
   const { timeseries } = data;
 
@@ -20,20 +26,20 @@ function TransactionBreakdown() {
           <EuiTitle size="xs">
             <h3>
               {i18n.translate('xpack.apm.transactionBreakdown.chartTitle', {
-                defaultMessage: 'Time spent by span type',
+                defaultMessage: 'Average duration by span type',
               })}
             </h3>
           </EuiTitle>
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
-          <TransactionBreakdownGraph
-            timeseries={timeseries}
+          <TransactionBreakdownChartContents
             fetchStatus={status}
+            height={height}
+            showAnnotations={showAnnotations}
+            timeseries={timeseries}
           />
         </EuiFlexItem>
       </EuiFlexGroup>
     </EuiPanel>
   );
 }
-
-export { TransactionBreakdown };
