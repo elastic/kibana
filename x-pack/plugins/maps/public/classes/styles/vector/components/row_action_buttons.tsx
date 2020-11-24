83afba341b5662ea49cf43092c7c448fa3594f9d
Nathan Reese <reese.nathan@gmail.com>
2020-11-24 16:20:21 -0700

    remove duplicate file license

--- x-pack/plugins/maps/public/classes/styles/vector/components/row_action_buttons.tsx
+++ x-pack/plugins/maps/public/classes/styles/vector/components/row_action_buttons.tsx
@@ -4,12 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-
 import _ from 'lodash';
 import React from 'react';
 import { i18n } from '@kbn/i18n';
