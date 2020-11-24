13808e019e6ebcf62b392824885b743371120bb1
Brandon Kobel <brandon.kobel@elastic.co>
2020-11-24 12:28:15 -0800

    Deprecate `kibana.index` setting (#83988)
    * Deprecating `kibana.index` setting
    
    * Using ela.st service so this can be changed to the blog in the future
    
    * Adding unit tests
    
    * Revising deprecation log message
    
    * Changing the deprecation log message to be more consistent with others
    
    * Updating kibana.index docs also
    
    * Using rename deprecation as the "standard" for the deprecation messages
    
    * /s/'/`

--- src/core/server/kibana_config.ts
+++ src/core/server/kibana_config.ts
@@ -18,8 +18,21 @@
  */
 
 import { schema, TypeOf } from '@kbn/config-schema';
+import { ConfigDeprecationProvider } from '@kbn/config';
 
 export type KibanaConfigType = TypeOf<typeof config.schema>;
+
+const deprecations: ConfigDeprecationProvider = () => [
+  (settings, fromPath, log) => {
+    const kibana = settings[fromPath];
+    if (kibana?.index) {
+      log(
+        `"kibana.index" is deprecated. Multitenancy by changing "kibana.index" will not be supported starting in 8.0. See https://ela.st/kbn-remove-legacy-multitenancy for more details`
+      );
+    }
+    return settings;
+  },
+];
 
 export const config = {
   path: 'kibana',
@@ -29,4 +42,5 @@ export const config = {
     autocompleteTerminateAfter: schema.duration({ defaultValue: 100000 }),
     autocompleteTimeout: schema.duration({ defaultValue: 1000 }),
   }),
+  deprecations,
 };
