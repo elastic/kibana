f7ffe9b4c57bed956db5a70d79d29ad123579850
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-11-24 19:27:24 +0100

    [APM] Use `asTransactionRate` consistently everywhere (#84213)

--- x-pack/plugins/apm/common/utils/formatters/formatters.ts
+++ x-pack/plugins/apm/common/utils/formatters/formatters.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import numeral from '@elastic/numeral';
-import { i18n } from '@kbn/i18n';
 import { Maybe } from '../../../typings/common';
 import { NOT_AVAILABLE_LABEL } from '../../i18n';
 import { isFiniteNumber } from '../is_finite_number';
@@ -15,16 +14,6 @@ export function asDecimal(value: number) {
 
 export function asInteger(value: number) {
   return numeral(value).format('0,0');
-}
-
-export function tpmUnit(type?: string) {
-  return type === 'request'
-    ? i18n.translate('xpack.apm.formatters.requestsPerMinLabel', {
-        defaultMessage: 'rpm',
-      })
-    : i18n.translate('xpack.apm.formatters.transactionsPerMinLabel', {
-        defaultMessage: 'tpm',
-      });
 }
 
 export function asPercent(
