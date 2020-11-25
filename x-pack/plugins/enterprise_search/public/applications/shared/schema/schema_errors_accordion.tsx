51f21d9b0f9791720c296083ca8857d983394518
scottybollinger <scotty.bollinger@elastic.co>
2020-11-25 14:44:53 -0600

    Migrate shared Schema components
    These components were not changed, with the exception of lint fixes and TypeScript changes (adding types). The only exceptions are:
    
    1. The `formatFieldName` in `SchemaAddFieldModal`. In `ent-search`, this was a shared util, but since it was not used anywhere else, I just added the method as a function in this component.
    
    2. The `IFieldCoercionErrors` interface is only used here so I did not add it to the shared types file and instead jut added it to this component.

---
+++ x-pack/plugins/enterprise_search/public/applications/shared/schema/schema_errors_accordion.tsx
@@ -0,0 +1,125 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import {
+  EuiAccordion,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiTable,
+  EuiTableBody,
+  EuiTableHeader,
+  EuiTableHeaderCell,
+  EuiTableRow,
+  EuiTableRowCell,
+} from '@elastic/eui';
+
+import { TruncatedContent } from '../truncate';
+
+interface IFieldCoercionError {
+  external_id: string;
+  error: string;
+}
+
+interface IFieldCoercionErrors {
+  [key: string]: IFieldCoercionError[];
+}
+
+interface ISchemaErrorsAccordionProps {
+  fieldCoercionErrors: IFieldCoercionErrors;
+  schema: { [key: string]: string };
+  itemId?: string;
+  getRoute?(itemId: string, externalId: string): string;
+}
+
+export const SchemaErrorsAccordion: React.FC<ISchemaErrorsAccordionProps> = ({
+  fieldCoercionErrors,
+  schema,
+  itemId,
+  getRoute,
+}) => (
+  <>
+    {Object.keys(fieldCoercionErrors).map((fieldName, fieldNameIndex) => {
+      const errorInfos = fieldCoercionErrors[fieldName];
+
+      const accordionHeader = (
+        <EuiFlexGroup alignItems="center" justifyContent="spaceBetween" gutterSize="none">
+          <EuiFlexItem grow={false}>
+            <EuiFlexGroup alignItems="center" gutterSize="xl">
+              <EuiFlexItem className="field-error__field-name">
+                <TruncatedContent content={fieldName} length={32} />
+              </EuiFlexItem>
+              <EuiFlexItem className="field-error__field-type">{schema[fieldName]}</EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <span className="field-error__control button">Review</span>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      );
+
+      return (
+        <EuiAccordion
+          key={fieldNameIndex}
+          id={`accordion${fieldNameIndex}`}
+          className="euiAccordionForm field-error"
+          buttonClassName="euiAccordionForm__button field-error__header"
+          buttonContent={accordionHeader}
+          paddingSize="xl"
+        >
+          <EuiTable>
+            <EuiTableHeader>
+              <EuiTableHeaderCell>id</EuiTableHeaderCell>
+              <EuiTableHeaderCell>Error</EuiTableHeaderCell>
+              <EuiTableHeaderCell />
+            </EuiTableHeader>
+            <EuiTableBody>
+              {errorInfos.map((error, errorIndex) => {
+                const showViewButton = getRoute && itemId;
+                const documentPath = getRoute && itemId ? getRoute(itemId, error.external_id) : '';
+
+                const viewButton = showViewButton && (
+                  <EuiTableRowCell className="field-error-document__actions">
+                    <Link
+                      className="euiButtonEmpty euiButtonEmpty--primary euiButtonEmpty--xSmall"
+                      to={documentPath}
+                    >
+                      <span className="euiButtonEmpty__content">
+                        <span className="euiButtonEmpty__text">View</span>
+                      </span>
+                    </Link>
+                  </EuiTableRowCell>
+                );
+
+                return (
+                  <EuiTableRow
+                    key={`schema-change-document-error-${fieldName}-${errorIndex} field-error-document`}
+                  >
+                    <EuiTableRowCell className="field-error-document__id">
+                      <div className="data-type--id">
+                        <TruncatedContent
+                          tooltipType="title"
+                          content={error.external_id}
+                          length={22}
+                        />
+                      </div>
+                    </EuiTableRowCell>
+                    <EuiTableRowCell className="field-error-document__field-content">
+                      {error.error}
+                    </EuiTableRowCell>
+                    {showViewButton ? viewButton : <EuiTableRowCell />}
+                  </EuiTableRow>
+                );
+              })}
+            </EuiTableBody>
+          </EuiTable>
+        </EuiAccordion>
+      );
+    })}
+  </>
+);
