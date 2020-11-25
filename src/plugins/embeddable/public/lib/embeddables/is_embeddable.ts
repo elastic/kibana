0db33e03bb78def16a637001bbadb7266242992f
streamich <streamich@gmail.com>
2020-11-25 12:30:15 +0000

    feat: 🎸 add isEmbeddable helper

--- src/plugins/embeddable/public/lib/embeddables/is_embeddable.ts
+++ src/plugins/embeddable/public/lib/embeddables/is_embeddable.ts
@@ -16,14 +16,14 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-export { EmbeddableOutput, EmbeddableInput, IEmbeddable } from './i_embeddable';
-export { isEmbeddable } from './is_embeddable';
-export { Embeddable } from './embeddable';
-export * from './embeddable_factory';
-export * from './embeddable_factory_definition';
-export * from './default_embeddable_factory_provider';
-export { ErrorEmbeddable, isErrorEmbeddable } from './error_embeddable';
-export { withEmbeddableSubscription } from './with_subscription';
-export { EmbeddableRoot } from './embeddable_root';
-export * from '../../../common/lib/saved_object_embeddable';
-export { EmbeddableRenderer, EmbeddableRendererProps } from './embeddable_renderer';
+
+import { IEmbeddable } from './i_embeddable';
+
+export const isEmbeddable = (x: unknown): x is IEmbeddable => {
+  if (!x) return false;
+  if (typeof x !== 'object') return false;
+  if (typeof (x as IEmbeddable).id !== 'string') return false;
+  if (typeof (x as IEmbeddable).getInput !== 'function') return false;
+  if (typeof (x as IEmbeddable).supportedTriggers !== 'function') return false;
+  return true;
+};
