534647dc02a3a7afd7f6b527bbe0b0f26573e2ed
Shahzad <shahzad.muhammad@elastic.co>
2020-11-25 10:43:07 +0100

    Update index.ts

--- x-pack/test/apm_api_integration/trial/tests/index.ts
+++ x-pack/test/apm_api_integration/trial/tests/index.ts
@@ -3,12 +3,14 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-
 import { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
+import { registerMochaHooksForSnapshots } from '../../common/match_snapshot';
 
 export default function observabilityApiIntegrationTests({ loadTestFile }: FtrProviderContext) {
   describe('APM specs (trial)', function () {
     this.tags('ciGroup1');
+
+    registerMochaHooksForSnapshots();
 
     describe('Services', function () {
       loadTestFile(require.resolve('./services/annotations'));
