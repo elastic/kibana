c967f84b42b16a2e2ffb707180f449d7c00a0d1c
Gidi Meir Morris <github@gidi.io>
2020-11-26 12:22:17 +0000

    support unspecified parent field on jira

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/jira/jira_params.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/jira/jira_params.tsx
@@ -197,7 +197,7 @@ const JiraParamsFields: React.FunctionComponent<ActionParamsProps<JiraActionPara
           />
         </EuiFormRow>
         <EuiHorizontalRule />
-        {hasParent && parent && (
+        {hasParent && (
           <>
             <EuiFlexGroup>
               <EuiFlexItem>
