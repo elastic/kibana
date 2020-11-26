807683319d8eca16f82047a7cb81734ac5613c07
Felix Stürmer <stuermer@weltenwort.de>
2020-11-26 18:16:02 +0100

    Remove commented-out code

--- x-pack/plugins/infra/server/services/log_entries/types.ts
+++ x-pack/plugins/infra/server/services/log_entries/types.ts
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// import type { CoreSetup, CoreStart } from 'kibana/public';
 import {
   PluginSetup as DataPluginSetup,
   PluginStart as DataPluginStart,
