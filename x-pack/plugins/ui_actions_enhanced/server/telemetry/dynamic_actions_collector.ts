2c6b7066ac83753242e38a3d2de184c7a0435623
streamich <streamich@gmail.com>
2020-12-01 14:26:21 +0100

    refactor: 💡 standartize metric naming

--- x-pack/plugins/ui_actions_enhanced/server/telemetry/dynamic_actions_collector.ts
+++ x-pack/plugins/ui_actions_enhanced/server/telemetry/dynamic_actions_collector.ts
@@ -5,33 +5,24 @@
  */
 
 import { DynamicActionsState } from '../../common';
+import { getMetricKey } from './get_metric_key';
 
 export const dynamicActionsCollector = (state: DynamicActionsState): Record<string, any> => {
   const stats: Record<string, any> = {};
 
-  stats['dynamicActions.count'] = state.events.length;
-
-  const factoryCount: Record<string, number | undefined> = {};
-  const triggerCount: Record<string, number | undefined> = {};
+  stats[getMetricKey('count')] = state.events.length;
 
   for (const event of state.events) {
     const factoryId = event.action.factoryId;
-    factoryCount[factoryId] = 1 + (factoryCount[factoryId] || 0);
+    const factoryCountMetric = getMetricKey(`actions.${factoryId}.count`);
+
+    stats[factoryCountMetric] = 1 + (stats[factoryCountMetric] || 0);
+
     for (const trigger of event.triggers) {
-      triggerCount[trigger] = 1 + (triggerCount[trigger] || 0);
+      const triggerCountMetric = getMetricKey(`triggers.${trigger}.count`);
+
+      stats[triggerCountMetric] = 1 + (stats[triggerCountMetric] || 0);
     }
-  }
-
-  const factoryIds = Object.keys(factoryCount);
-
-  for (const factoryId of factoryIds) {
-    stats[`dynamicActions.actions.${factoryId}.count`] = factoryCount[factoryId];
-  }
-
-  const triggers = Object.keys(triggerCount);
-
-  for (const trigger of triggers) {
-    stats[`dynamicActions.triggers.${trigger}.count`] = triggerCount[trigger];
   }
 
   return stats;
