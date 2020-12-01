63acf37bf44e456336edbed13c676aa7b0e889c7
Yulia Cech <yulia.cech@elastic.co>
2020-12-01 17:17:23 +0100

    Removed unnecessary imports and added index.js file for follower_index_actions_provider imports

--- x-pack/plugins/cross_cluster_replication/public/app/components/follower_index_actions_providers/follower_index_actions_provider.js
+++ x-pack/plugins/cross_cluster_replication/public/app/components/follower_index_actions_providers/follower_index_actions_provider.js
@@ -4,8 +4,9 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { FollowerIndexPauseProvider } from './follower_index_pause_provider';
 import React from 'react';
+
+import { FollowerIndexPauseProvider } from './follower_index_pause_provider';
 import { FollowerIndexResumeProvider } from './follower_index_resume_provider';
 import { FollowerIndexUnfollowProvider } from './follower_index_unfollow_provider';
 
