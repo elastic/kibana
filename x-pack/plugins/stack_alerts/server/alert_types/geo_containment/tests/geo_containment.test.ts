86948acfafa20de6be863c9c92d9113b13ecd3cd
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-12-02 13:32:00 -0700

    Update jest tests to handle location map

--- x-pack/plugins/stack_alerts/server/alert_types/geo_containment/tests/geo_containment.test.ts
+++ x-pack/plugins/stack_alerts/server/alert_types/geo_containment/tests/geo_containment.test.ts
@@ -19,36 +19,46 @@ describe('geo_containment', () => {
         dateField,
         geoField
       );
-      expect(transformedResults).toEqual([
-        {
-          dateInShape: '2020-09-28T18:01:41.190Z',
-          docId: 'N-ng1XQB6yyY-xQxnGSM',
-          entityName: '936',
-          location: [-82.8814151789993, 40.62806099653244],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-        {
-          dateInShape: '2020-09-28T18:01:41.191Z',
-          docId: 'iOng1XQB6yyY-xQxnGSM',
-          entityName: 'AAL2019',
-          location: [-82.22068064846098, 39.006176185794175],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-        {
-          dateInShape: '2020-09-28T18:01:41.191Z',
-          docId: 'n-ng1XQB6yyY-xQxnGSM',
-          entityName: 'AAL2323',
-          location: [-84.71324851736426, 41.6677269525826],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-        {
-          dateInShape: '2020-09-28T18:01:41.192Z',
-          docId: 'GOng1XQB6yyY-xQxnGWM',
-          entityName: 'ABD5250',
-          location: [6.073727197945118, 39.07997465226799],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-      ]);
+      expect(transformedResults).toEqual(
+        new Map([
+          [
+            '936',
+            {
+              dateInShape: '2020-09-28T18:01:41.190Z',
+              docId: 'N-ng1XQB6yyY-xQxnGSM',
+              location: [-82.8814151789993, 40.62806099653244],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+          [
+            'AAL2019',
+            {
+              dateInShape: '2020-09-28T18:01:41.191Z',
+              docId: 'iOng1XQB6yyY-xQxnGSM',
+              location: [-82.22068064846098, 39.006176185794175],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+          [
+            'AAL2323',
+            {
+              dateInShape: '2020-09-28T18:01:41.191Z',
+              docId: 'n-ng1XQB6yyY-xQxnGSM',
+              location: [-84.71324851736426, 41.6677269525826],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+          [
+            'ABD5250',
+            {
+              dateInShape: '2020-09-28T18:01:41.192Z',
+              docId: 'GOng1XQB6yyY-xQxnGWM',
+              location: [6.073727197945118, 39.07997465226799],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+        ])
+      );
     });
 
     const nestedDateField = 'time_data.@timestamp';
@@ -59,41 +69,51 @@ describe('geo_containment', () => {
         nestedDateField,
         nestedGeoField
       );
-      expect(transformedResults).toEqual([
-        {
-          dateInShape: '2020-09-28T18:01:41.190Z',
-          docId: 'N-ng1XQB6yyY-xQxnGSM',
-          entityName: '936',
-          location: [-82.8814151789993, 40.62806099653244],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-        {
-          dateInShape: '2020-09-28T18:01:41.191Z',
-          docId: 'iOng1XQB6yyY-xQxnGSM',
-          entityName: 'AAL2019',
-          location: [-82.22068064846098, 39.006176185794175],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-        {
-          dateInShape: '2020-09-28T18:01:41.191Z',
-          docId: 'n-ng1XQB6yyY-xQxnGSM',
-          entityName: 'AAL2323',
-          location: [-84.71324851736426, 41.6677269525826],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-        {
-          dateInShape: '2020-09-28T18:01:41.192Z',
-          docId: 'GOng1XQB6yyY-xQxnGWM',
-          entityName: 'ABD5250',
-          location: [6.073727197945118, 39.07997465226799],
-          shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
-        },
-      ]);
+      expect(transformedResults).toEqual(
+        new Map([
+          [
+            '936',
+            {
+              dateInShape: '2020-09-28T18:01:41.190Z',
+              docId: 'N-ng1XQB6yyY-xQxnGSM',
+              location: [-82.8814151789993, 40.62806099653244],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+          [
+            'AAL2019',
+            {
+              dateInShape: '2020-09-28T18:01:41.191Z',
+              docId: 'iOng1XQB6yyY-xQxnGSM',
+              location: [-82.22068064846098, 39.006176185794175],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+          [
+            'AAL2323',
+            {
+              dateInShape: '2020-09-28T18:01:41.191Z',
+              docId: 'n-ng1XQB6yyY-xQxnGSM',
+              location: [-84.71324851736426, 41.6677269525826],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+          [
+            'ABD5250',
+            {
+              dateInShape: '2020-09-28T18:01:41.192Z',
+              docId: 'GOng1XQB6yyY-xQxnGWM',
+              location: [6.073727197945118, 39.07997465226799],
+              shapeLocationId: '0DrJu3QB6yyY-xQxv6Ip',
+            },
+          ],
+        ])
+      );
     });
 
     it('should return an empty array if no results', async () => {
       const transformedResults = transformResults(undefined, dateField, geoField);
-      expect(transformedResults).toEqual([]);
+      expect(transformedResults).toEqual(new Map());
     });
   });
 });
