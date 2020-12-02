59a405dc80e63abf118ab1a50a2a8bae2f5e4e14
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2020-12-02 11:32:22 +0100

    Make all providers to preserve original URL when session expires. (#84229)

--- x-pack/plugins/security/public/session/session_expired.ts
+++ x-pack/plugins/security/public/session/session_expired.ts
@@ -4,6 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import {
+  LOGOUT_PROVIDER_QUERY_STRING_PARAMETER,
+  LOGOUT_REASON_QUERY_STRING_PARAMETER,
+  NEXT_URL_QUERY_STRING_PARAMETER,
+} from '../../common/constants';
+
 export interface ISessionExpired {
   logout(): void;
 }
@@ -11,13 +17,15 @@ export interface ISessionExpired {
 const getNextParameter = () => {
   const { location } = window;
   const next = encodeURIComponent(`${location.pathname}${location.search}${location.hash}`);
-  return `&next=${next}`;
+  return `&${NEXT_URL_QUERY_STRING_PARAMETER}=${next}`;
 };
 
 const getProviderParameter = (tenant: string) => {
   const key = `${tenant}/session_provider`;
   const providerName = sessionStorage.getItem(key);
-  return providerName ? `&provider=${encodeURIComponent(providerName)}` : '';
+  return providerName
+    ? `&${LOGOUT_PROVIDER_QUERY_STRING_PARAMETER}=${encodeURIComponent(providerName)}`
+    : '';
 };
 
 export class SessionExpired {
@@ -26,6 +34,8 @@ export class SessionExpired {
   logout() {
     const next = getNextParameter();
     const provider = getProviderParameter(this.tenant);
-    window.location.assign(`${this.logoutUrl}?msg=SESSION_EXPIRED${next}${provider}`);
+    window.location.assign(
+      `${this.logoutUrl}?${LOGOUT_REASON_QUERY_STRING_PARAMETER}=SESSION_EXPIRED${next}${provider}`
+    );
   }
 }
