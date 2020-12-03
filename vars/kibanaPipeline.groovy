0ecacbdd988aca20cc4a89d3394e90fb03d9a573
Tyler Smalley <tyler.smalley@elastic.co>
2020-12-03 07:38:26 -0800

    TESTING
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- vars/kibanaPipeline.groovy
+++ vars/kibanaPipeline.groovy
@@ -89,7 +89,6 @@ def withFunctionalTestEnv(List additionalEnvs = [], Closure closure) {
   def esTransportPort = "61${parallelId}3"
   def fleetPackageRegistryPort = "61${parallelId}4"
   def alertingProxyPort = "61${parallelId}5"
-  def apmActive = githubPr.isPr() ? "false" : "true"
 
   withEnv([
     "CI_GROUP=${parallelId}",
@@ -103,7 +102,7 @@ def withFunctionalTestEnv(List additionalEnvs = [], Closure closure) {
     "KBN_NP_PLUGINS_BUILT=true",
     "FLEET_PACKAGE_REGISTRY_PORT=${fleetPackageRegistryPort}",
     "ALERTING_PROXY_PORT=${alertingProxyPort}",
-    "ELASTIC_APM_ACTIVE=${apmActive}",
+    "ELASTIC_APM_ACTIVE=true",
     "ELASTIC_APM_TRANSACTION_SAMPLE_RATE=0.1",
   ] + additionalEnvs) {
     closure()
