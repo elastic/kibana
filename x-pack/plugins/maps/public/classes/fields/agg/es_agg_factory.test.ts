f9892ff59cc5013c4001456e8dfa0cbf4cce2e71
Nathan Reese <reese.nathan@gmail.com>
2020-12-03 15:39:40 -0700

    one more tslint fix

--- x-pack/plugins/maps/public/classes/fields/agg/es_agg_factory.test.ts
+++ x-pack/plugins/maps/public/classes/fields/agg/es_agg_factory.test.ts
@@ -8,7 +8,7 @@ import { esAggFieldsFactory } from './es_agg_factory';
 import { AGG_TYPE, FIELD_ORIGIN } from '../../../../common/constants';
 import { IESAggSource } from '../../sources/es_agg_source';
 
-const mockEsAggSource = ({} as undefined) as IESAggSource;
+const mockEsAggSource = ({} as unknown) as IESAggSource;
 
 describe('esAggFieldsFactory', () => {
   test('Should only create top terms field when term field is not provided', () => {
