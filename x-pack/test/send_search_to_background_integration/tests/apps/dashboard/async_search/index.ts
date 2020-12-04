c2abd552fe1880c9d69164e6683fea325bc8812d
Tim Sullivan <tsullivan@users.noreply.github.com>
2020-12-04 13:35:49 -0700

    [Send To Background UI] Isolate functional test for wip feature (#84833) (#85045)
    * [Send To Background UI] Isolate functional test for wip feature
    
    * add tests to cigroup 3

--- x-pack/test/send_search_to_background_integration/tests/apps/dashboard/async_search/index.ts
+++ x-pack/test/send_search_to_background_integration/tests/apps/dashboard/async_search/index.ts
@@ -3,13 +3,15 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { FtrProviderContext } from '../../../ftr_provider_context';
+import { FtrProviderContext } from '../../../../ftr_provider_context';
 
 export default function ({ loadTestFile, getService }: FtrProviderContext) {
   const kibanaServer = getService('kibanaServer');
   const esArchiver = getService('esArchiver');
 
   describe('async search', function () {
+    this.tags('ciGroup3');
+
     before(async () => {
       await esArchiver.loadIfNeeded('logstash_functional');
       await esArchiver.load('dashboard/async_search');
