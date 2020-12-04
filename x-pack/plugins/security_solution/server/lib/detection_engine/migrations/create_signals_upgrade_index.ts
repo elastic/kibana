5e49142c2afd50adf0576439c5ce712ead97167d
Ryland Herrick <ryalnd@gmail.com>
2020-12-04 13:26:18 -0600

    Pad the version number of our destination migration index
    Instead of e.g. `.siem-signals-default-000001-r5`, this will generate
    `.siem-signals-default-000001-r000005`.
    
    This shouldn't matter much, but it may make it easier for users at a
    glance to see the story of each index.

--- x-pack/plugins/security_solution/server/lib/detection_engine/migrations/create_signals_upgrade_index.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/migrations/create_signals_upgrade_index.ts
@@ -15,7 +15,8 @@ export const createSignalsUpgradeIndex = async ({
   index: string;
   version: number;
 }): Promise<string> => {
-  const destinationIndexName = `${index}-r${version}`;
+  const paddedVersion = `${version}`.padStart(6, '0');
+  const destinationIndexName = `${index}-r${paddedVersion}`;
 
   const response = await esClient.indices.create<{ index: string }>({
     index: destinationIndexName,
