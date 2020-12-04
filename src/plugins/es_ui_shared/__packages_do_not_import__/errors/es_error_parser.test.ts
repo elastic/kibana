a0be161ba9589ac913ff1cbaefab06abf05941f2
Sébastien Loix <sabee77@gmail.com>
2020-12-04 09:18:14 +0100

    [7.x] [Runtime fields] Add support in index template (#84184) (#84928)

--- src/plugins/es_ui_shared/__packages_do_not_import__/errors/es_error_parser.test.ts
+++ src/plugins/es_ui_shared/__packages_do_not_import__/errors/es_error_parser.test.ts
@@ -16,13 +16,27 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import { parseEsError } from './es_error_parser';
 
-export { isEsError, handleEsError, parseEsError } from './errors';
+describe('ES error parser', () => {
+  test('should return all the cause of the error', () => {
+    const esError = `{
+      "error": {
+        "reason": "Houston we got a problem",
+        "caused_by": {
+          "reason": "First reason",
+          "caused_by": {
+            "reason": "Second reason",
+            "caused_by": {
+              "reason": "Third reason"
+            }
+          }
+        }
+      }
+    }`;
 
-/** dummy plugin*/
-export function plugin() {
-  return new (class EsUiSharedPlugin {
-    setup() {}
-    start() {}
-  })();
-}
+    const parsedError = parseEsError(esError);
+    expect(parsedError.message).toEqual('Houston we got a problem');
+    expect(parsedError.cause).toEqual(['First reason', 'Second reason', 'Third reason']);
+  });
+});
