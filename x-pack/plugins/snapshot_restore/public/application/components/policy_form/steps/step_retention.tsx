fa185ef051844e1ea07cc31670ab01797a5829ff
Leaf-Lin <39002973+Leaf-Lin@users.noreply.github.com>
2020-12-07 09:19:32 +1100

    Update x-pack/plugins/snapshot_restore/public/application/components/policy_form/steps/step_retention.tsx
    LGTM
    
    Co-authored-by: Alison Goryachev <alison.goryachev@elastic.co>

--- x-pack/plugins/snapshot_restore/public/application/components/policy_form/steps/step_retention.tsx
+++ x-pack/plugins/snapshot_restore/public/application/components/policy_form/steps/step_retention.tsx
@@ -136,7 +136,10 @@ export const PolicyStepRetention: React.FunctionComponent<StepProps> = ({
       description={
         <FormattedMessage
           id="xpack.snapshotRestore.policyForm.stepRetention.countDescription"
-          defaultMessage="The minimum and maximum number of snapshots to store in your cluster. It should not exceed `200`"
+          defaultMessage="The minimum and maximum number of snapshots to store in your cluster. It should not exceed {maxNumber}."
+          values={{
+            maxNumber: <EuiCode>200</EuiCode>,
+          }}
         />
       }
       fullWidth
