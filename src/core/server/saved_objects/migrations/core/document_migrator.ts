66b850d2e46e1a2da479a033be83f1fbd82e9900
Joe Portner <5295965+jportner@users.noreply.github.com>
2020-12-07 13:01:48 -0500

    Fix 500 error
    The DocumentMigrator would throw an error with one of the recent
    changes that I made. This was causing tons of tests to fail. I
    reverted the change and that seems to have resolved the problem.

--- src/core/server/saved_objects/migrations/core/document_migrator.ts
+++ src/core/server/saved_objects/migrations/core/document_migrator.ts
@@ -403,7 +403,7 @@ function props(doc: SavedObjectUnsanitizedDoc) {
  */
 function propVersion(doc: SavedObjectUnsanitizedDoc | ActiveMigrations, prop: string) {
   return (
-    (doc.hasOwnProperty(prop) && (doc as any)[prop].latestVersion) ||
+    ((doc as any)[prop] && (doc as any)[prop].latestVersion) ||
     (doc.migrationVersion && (doc as any).migrationVersion[prop])
   );
 }
