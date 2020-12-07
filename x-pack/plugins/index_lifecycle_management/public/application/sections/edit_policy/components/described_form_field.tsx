d7f52337020daec5a7cf600c9e516a1ea2d09b27
Jean-Louis Leysens <jloleysens@gmail.com>
2020-12-07 13:28:12 +0100

    merged toggleable field and described form component and moved toggle to left

--- x-pack/plugins/index_lifecycle_management/public/application/sections/edit_policy/components/described_form_field.tsx
+++ x-pack/plugins/index_lifecycle_management/public/application/sections/edit_policy/components/described_form_field.tsx
@@ -4,30 +4,78 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FunctionComponent } from 'react';
-import { EuiDescribedFormGroup, EuiDescribedFormGroupProps } from '@elastic/eui';
+import React, { FunctionComponent, useState } from 'react';
+import {
+  EuiDescribedFormGroup,
+  EuiDescribedFormGroupProps,
+  EuiSwitchProps,
+  EuiSwitch,
+  EuiSpacer,
+} from '@elastic/eui';
 
-import { ToggleableField, Props as ToggleableFieldProps } from './toggleable_field';
+interface SwitchProps extends Omit<EuiSwitchProps, 'children' | 'checked' | 'value' | 'onChange'> {
+  /**
+   * use initialValue to specify an uncontrolled component
+   */
+  initialValue?: boolean;
+
+  /**
+   * checked and onChange together specify a controlled component
+   */
+  checked?: boolean;
+  onChange?: (nextValue: boolean) => void;
+}
 
 type Props = EuiDescribedFormGroupProps & {
   children: (() => JSX.Element) | JSX.Element | JSX.Element[] | undefined;
-  switchProps?: Omit<ToggleableFieldProps, 'children'>;
+  switchProps?: SwitchProps;
 };
 
 export const DescribedFormField: FunctionComponent<Props> = ({
   children,
   switchProps,
+  description,
   ...restDescribedFormProps
 }) => {
+  const [uncontrolledIsContentVisible, setUncontrolledIsContentVisible] = useState<boolean>(
+    () => switchProps?.initialValue ?? false
+  );
+
+  const isContentVisible = Boolean(switchProps?.checked ?? uncontrolledIsContentVisible);
+
+  const renderToggle = () => {
+    if (!switchProps) {
+      return null;
+    }
+    const { onChange, ...restProps } = switchProps;
+    return (
+      <>
+        <EuiSpacer size="m" />
+        <EuiSwitch
+          {...restProps}
+          checked={isContentVisible}
+          onChange={(e) => {
+            const nextValue = e.target.checked;
+            setUncontrolledIsContentVisible(nextValue);
+            if (onChange) {
+              onChange(nextValue);
+            }
+          }}
+        />
+      </>
+    );
+  };
   return (
-    <EuiDescribedFormGroup {...restDescribedFormProps}>
-      {switchProps ? (
-        <ToggleableField {...switchProps}>{children}</ToggleableField>
-      ) : typeof children === 'function' ? (
-        children()
-      ) : (
-        children
-      )}
+    <EuiDescribedFormGroup
+      {...restDescribedFormProps}
+      description={
+        <>
+          {description}
+          {renderToggle()}
+        </>
+      }
+    >
+      {isContentVisible ? (typeof children === 'function' ? children() : children) : null}
     </EuiDescribedFormGroup>
   );
 };
