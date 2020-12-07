2151b20cae2b5b37c96db309555b38743a133ad4
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2020-12-07 11:18:43 +0100

    Add `bulk assign` action to tag management (#84177)
    * initial draft
    
    * move components to their own files
    
    * create services folder and move tags package
    
    * add assignment service
    
    * fix some types
    
    * prepare assign tag route
    
    * move server-side tag client under the `services` folder
    
    * add security check, move a lot of stuff.
    
    * design improvements
    
    * display tags in flyout
    
    * improve button and add notification on save
    
    * add action on tag rows
    
    * fix types
    
    * fix mock import paths
    
    * add lens to the list of assignable types
    
    * update generated doc
    
    * add base functional tests
    
    * move api to internal
    
    * add api/security test suites
    
    * add / use get_assignable_types API
    
    * fix feature control tests
    
    * fix assignable types propagation
    
    * rename actions folder to bulk_actions
    
    * extract actions to their own module
    
    * add common / server unit tests
    
    * add client-side assign tests
    
    * add some tests and tsdoc
    
    * typo
    
    * add getActions test
    
    * revert width change
    
    * fix typo in API
    
    * various minor improvements
    
    * typo
    
    * tsdoc on flyout page object
    
    * close flyout when leaving the page
    
    * fix bug when redirecting to SO management with a tag having whitespaces in its name
    
    * check for dupes in toAdd and toRemove
    
    * add lazy load to assign modal opener
    
    * add lazy load to edit/create modals
    
    * check if at least one assign or unassign tag id is specified
    
    * grammar
    
    * add explicit type existence check

---
+++ x-pack/plugins/saved_objects_tagging/public/components/assign_flyout/components/action_bar.tsx
@@ -0,0 +1,99 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { FC } from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiLink, EuiText } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+
+export interface AssignFlyoutActionBarProps {
+  resultCount: number;
+  initiallyAssigned: number;
+  pendingChanges: number;
+  onReset: () => void;
+  onSelectAll: () => void;
+  onDeselectAll: () => void;
+}
+
+export const AssignFlyoutActionBar: FC<AssignFlyoutActionBarProps> = ({
+  resultCount,
+  initiallyAssigned,
+  pendingChanges,
+  onReset,
+  onSelectAll,
+  onDeselectAll,
+}) => {
+  return (
+    <div className="tagAssignFlyout__actionBar">
+      <EuiFlexGroup justifyContent="spaceBetween" alignItems="center" gutterSize="m">
+        <EuiFlexItem grow={false}>
+          <EuiText size="xs" color="subdued">
+            <FormattedMessage
+              id="xpack.savedObjectsTagging.assignFlyout.actionBar.totalResultsLabel"
+              defaultMessage="{count, plural, one {1 saved object} other {# saved objects}}"
+              values={{
+                count: resultCount,
+              }}
+            />
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <div className="tagMgt__actionBarDivider" />
+        </EuiFlexItem>
+        <EuiFlexItem grow={true}>
+          <EuiText size="xs" color="subdued">
+            {pendingChanges > 0 ? (
+              <FormattedMessage
+                id="xpack.savedObjectsTagging.assignFlyout.actionBar.pendingChanges"
+                defaultMessage="{count} pending changes"
+                values={{
+                  count: pendingChanges,
+                }}
+              />
+            ) : (
+              <FormattedMessage
+                id="xpack.savedObjectsTagging.assignFlyout.actionBar.currentlyAssigned"
+                defaultMessage="{count} currently assigned"
+                values={{
+                  count: initiallyAssigned,
+                }}
+              />
+            )}
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiText size="xs">
+            <EuiLink onClick={onReset} data-test-subj="assignFlyout-resetButton">
+              <FormattedMessage
+                id="xpack.savedObjectsTagging.assignFlyout.actionBar.resetLabel"
+                defaultMessage="Reset"
+              />
+            </EuiLink>
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiText size="xs">
+            <EuiLink onClick={onSelectAll} data-test-subj="assignFlyout-selectAllButton">
+              <FormattedMessage
+                id="xpack.savedObjectsTagging.assignFlyout.actionBar.selectedAllLabel"
+                defaultMessage="Select all"
+              />
+            </EuiLink>
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiText size="xs">
+            <EuiLink onClick={onDeselectAll} data-test-subj="assignFlyout-deselectAllButton">
+              <FormattedMessage
+                id="xpack.savedObjectsTagging.assignFlyout.actionBar.deselectedAllLabel"
+                defaultMessage="Deselect all"
+              />
+            </EuiLink>
+          </EuiText>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </div>
+  );
+};
