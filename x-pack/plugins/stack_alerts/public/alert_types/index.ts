17c64e62f213b41ea22935b49db79f7a4e46ae32
Aaron Caldwell <aaron.caldwell@elastic.co>
2020-12-07 19:40:22 -0700

    Register geo alerts only with gold license, remove otherwise

--- x-pack/plugins/stack_alerts/public/alert_types/index.ts
+++ x-pack/plugins/stack_alerts/public/alert_types/index.ts
@@ -7,7 +7,7 @@
 import { getAlertType as getGeoThresholdAlertType } from './geo_threshold';
 import { getAlertType as getGeoContainmentAlertType } from './geo_containment';
 import { getAlertType as getThresholdAlertType } from './threshold';
-import { Config, STACK_ALERTS_FEATURE_ID } from '../../common';
+import { Config } from '../../common';
 import { TriggersAndActionsUIPublicPluginSetup } from '../../../triggers_actions_ui/public';
 import { LicensingPluginSetup } from '../../../licensing/public';
 
@@ -21,12 +21,23 @@ export function registerAlertTypes({
   licensing: LicensingPluginSetup;
 }) {
   licensing.license$.subscribe((license) => {
-    const { state } = license.check(STACK_ALERTS_FEATURE_ID, 'gold');
-    const hasGoldLicense = state === 'valid';
-    if (hasGoldLicense && config.enableGeoAlerts) {
-      alertTypeRegistry.register(getGeoThresholdAlertType());
-      alertTypeRegistry.register(getGeoContainmentAlertType());
+    const geoThresholdAlertType = getGeoThresholdAlertType();
+    const geoContainmentAlertType = getGeoContainmentAlertType();
+    const thresholdAlertType = getThresholdAlertType();
+
+    if (license.hasAtLeast('gold') && config.enableGeoAlerts) {
+      alertTypeRegistry.register(geoThresholdAlertType);
+      alertTypeRegistry.register(geoContainmentAlertType);
+    } else {
+      if (alertTypeRegistry.has(geoThresholdAlertType.id)) {
+        alertTypeRegistry.delete(geoThresholdAlertType.id);
+      }
+      if (alertTypeRegistry.has(geoContainmentAlertType.id)) {
+        alertTypeRegistry.delete(geoContainmentAlertType.id);
+      }
     }
-    alertTypeRegistry.register(getThresholdAlertType());
+    if (!alertTypeRegistry.has(thresholdAlertType.id)) {
+      alertTypeRegistry.register(thresholdAlertType);
+    }
   });
 }
