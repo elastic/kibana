78755ef0031114e37d3146a58000f0edf12518bf
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2020-12-08 12:31:13 +0100

    use jest flags to catch leaks

--- test/scripts/jenkins_unit.sh
+++ test/scripts/jenkins_unit.sh
@@ -12,7 +12,7 @@ if [[ -z "$CODE_COVERAGE" ]] ; then
   "$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
 else
   echo " -> Running jest tests with coverage"
-  node scripts/jest --ci --verbose --coverage --coverageReporters json
+  node scripts/jest --runInBand --detectOpenHandles --ci --verbose --coverage --coverageReporters json
   rename_coverage_file "oss"
   echo ""
   echo ""
