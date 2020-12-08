f46eaf889d29767d5fe067f914af16b85bb745d0
MadameSheema <snootchie.boochies@gmail.com>
2020-12-08 14:47:48 +0100

    fixes and unskips failing tests (#85232)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/cypress/integration/alerts_detection_rules_custom.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/alerts_detection_rules_custom.spec.ts
@@ -85,6 +85,7 @@ import {
   waitForLoadElasticPrebuiltDetectionRulesTableToBeLoaded,
   waitForRulesToBeLoaded,
 } from '../tasks/alerts_detection_rules';
+import { removeSignalsIndex } from '../tasks/api_calls';
 import {
   createAndActivateRule,
   fillAboutRule,
@@ -119,6 +120,7 @@ describe('Custom detection rules creation', () => {
 
   after(() => {
     deleteRule();
+    removeSignalsIndex();
     esArchiverUnload('timeline');
   });
 
@@ -213,8 +215,7 @@ describe('Custom detection rules creation', () => {
   });
 });
 
-// FLAKY: https://github.com/elastic/kibana/issues/83793
-describe.skip('Custom detection rules deletion and edition', () => {
+describe('Custom detection rules deletion and edition', () => {
   beforeEach(() => {
     esArchiverLoad('custom_rules');
     loginAndWaitForPageWithoutDateRange(DETECTIONS_URL);
@@ -224,6 +225,7 @@ describe.skip('Custom detection rules deletion and edition', () => {
   });
 
   afterEach(() => {
+    removeSignalsIndex();
     esArchiverUnload('custom_rules');
   });
 
