d335e427018e9214af36f724917c5d0061ffb95a
Tre <wayne.seymour@elastic.co>
2020-12-08 10:44:25 -0700

    [7.x] [QA] Encryption Rotation tests via Email Connectors (#82659) (#85182)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/test/stack_functional_integration/apps/alerts/alerts_encryption_keys.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from '@kbn/expect';
+
+const ARCHIVE = 'email_connectors_with_encryption_rotation';
+
+export default ({ getPageObjects, getService }) => {
+  const esArchiver = getService('esArchiver');
+  const testSubjects = getService('testSubjects');
+  const pageObjects = getPageObjects(['common', 'triggersActionsUI']);
+  const find = getService('find');
+  const retry = getService('retry');
+
+  const address = (name) => (emails) => emails.split(',').find((x) => x.includes(name));
+  const toWayne = address('wayne');
+
+  describe('encryption key rotation with', function () {
+    before(async () => {
+      esArchiver.load(ARCHIVE);
+    });
+    beforeEach(async () => {
+      await pageObjects.common.navigateToApp('triggersConnectors');
+    });
+    after(async () => {
+      esArchiver.unload(ARCHIVE);
+    });
+
+    describe(`email connectors`, () => {
+      describe(`without the key used to create it`, () => {
+        it('should show a failure callout', async () => {
+          const connectorName = 'should_fail';
+          await testConnector(connectorName);
+          await retry.try(async () => {
+            const executionFailureResultCallout = await testSubjects.find('executionFailureResult');
+            expect(await executionFailureResultCallout.getVisibleText()).to.match(
+              /Internal Server Error/
+            );
+          });
+          expect(true).to.be(true);
+        });
+      });
+      describe(`with a decryption only key`, () => {
+        it('should show a success callout', async () => {
+          const connectorName = 'decrypt_only';
+          await testConnector(connectorName);
+          await retry.try(async () => {
+            await testSubjects.find('executionSuccessfulResult');
+          });
+        });
+      });
+      describe(`with the current key`, () => {
+        it('should show a success callout', async () => {
+          const connectorName = 'current_key';
+          await testConnector(connectorName);
+          await retry.try(async () => {
+            await testSubjects.find('executionSuccessfulResult');
+          });
+        });
+      });
+    });
+  });
+
+  async function testConnector(name) {
+    await pageObjects.triggersActionsUI.searchConnectors(name);
+    await testSubjects.click('runConnector');
+    await testSubjects.setValue('comboBoxInput', toWayne(process.env.REPORTING_TEST_EMAILS));
+    await testSubjects.setValue('subjectInput', name);
+    await testSubjects.setValue('messageTextArea', name);
+    await find.clickByCssSelector('[data-test-subj="executeActionButton"]:not(disabled)');
+  }
+};
