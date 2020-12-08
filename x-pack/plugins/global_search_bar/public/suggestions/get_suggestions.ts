901ae70890d1c7c12f9e51de695de7692c855609
pgayvallet <pierre.gayvallet@elastic.co>
2020-12-08 11:39:36 +0100

    tests on suggestions

---
+++ x-pack/plugins/global_search_bar/public/suggestions/get_suggestions.ts
@@ -0,0 +1,83 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { i18n } from '@kbn/i18n';
+import { ITagsCache } from '../../../../../src/plugins/saved_objects_tagging_oss/public';
+
+interface GetSuggestionOptions {
+  searchTerm: string;
+  searchableTypes: string[];
+  tagCache?: ITagsCache;
+}
+
+export interface SearchSuggestion {
+  key: string;
+  label: string;
+  description: string;
+  icon: string;
+  suggestedSearch: string;
+}
+
+export const getSuggestions = ({
+  searchTerm,
+  searchableTypes,
+  tagCache,
+}: GetSuggestionOptions): SearchSuggestion[] => {
+  const results: SearchSuggestion[] = [];
+  const suggestionTerm = searchTerm.trim();
+
+  const matchingType = findIgnoreCase(searchableTypes, suggestionTerm);
+  if (matchingType) {
+    const suggestedSearch = escapeIfWhiteSpaces(matchingType);
+    results.push({
+      key: '__type__suggestion__',
+      label: `type: ${matchingType}`,
+      icon: 'filter',
+      description: i18n.translate('xpack.globalSearchBar.suggestions.filterByTypeLabel', {
+        defaultMessage: 'Filter by type',
+      }),
+      suggestedSearch: `type:${suggestedSearch}`,
+    });
+  }
+
+  if (tagCache && searchTerm) {
+    const matchingTag = tagCache
+      .getState()
+      .find((tag) => equalsIgnoreCase(tag.name, suggestionTerm));
+    if (matchingTag) {
+      const suggestedSearch = escapeIfWhiteSpaces(matchingTag.name);
+      results.push({
+        key: '__tag__suggestion__',
+        label: `tag: ${matchingTag.name}`,
+        icon: 'tag',
+        description: i18n.translate('xpack.globalSearchBar.suggestions.filterByTagLabel', {
+          defaultMessage: 'Filter by tag name',
+        }),
+        suggestedSearch: `tag:${suggestedSearch}`,
+      });
+    }
+  }
+
+  return results;
+};
+
+const findIgnoreCase = (array: string[], target: string) => {
+  for (const item of array) {
+    if (equalsIgnoreCase(item, target)) {
+      return item;
+    }
+  }
+  return undefined;
+};
+
+const equalsIgnoreCase = (a: string, b: string) => a.toLowerCase() === b.toLowerCase();
+
+const escapeIfWhiteSpaces = (term: string) => {
+  if (/\s/g.test(term)) {
+    return `"${term}"`;
+  }
+  return term;
+};
