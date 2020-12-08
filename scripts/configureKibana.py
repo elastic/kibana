969f4b94b515ed00720a579b2055833c04d09ba7
Katarina Springer <katarina.springer@logrhythm.com>
2020-12-08 10:17:46 -0700

    moved comments

--- scripts/configureKibana.py
+++ scripts/configureKibana.py
@@ -75,8 +75,6 @@ def load_assets(path_to_files):
             logger.warning('Failed to load kibana object: ' + file)
 
 def setup_config():
-    # setup the default query type to be Lucene (not KQL)
-    # also set the default index pattern to be network_* using its UUID
     try:
         url = 'http://localhost:9200/.kibana/_update/config:7.5.2'
         session = requests.Session()
@@ -87,6 +85,8 @@ def setup_config():
             'Content-type': 'application/json',
             'kbn-version': '7.5.2'
         }
+        # setup the default query type to be Lucene (not KQL)
+        # set the default index pattern to be network_* using its UUID
         data = json.dumps({
             'doc': {
                 'config': {
