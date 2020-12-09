9048a45ba4170eff992009d1442d9f4812cd27fa
sulemanof <daniil_suleiman@epam.com>
2020-12-09 17:02:20 +0300

    Restrict hiding the toolbar

--- src/plugins/vis_type_table/public/components/table_vis_basic.tsx
+++ src/plugins/vis_type_table/public/components/table_vis_basic.tsx
@@ -156,6 +156,7 @@ export const TableVisBasic = memo(
           pagination={pagination}
           sorting={{ columns: sortingColumns, onSort }}
           onColumnResize={onColumnResize}
+          minSizeForControls={1}
         />
       </>
     );
