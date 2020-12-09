631abd21c69b5a59be7df8e7f42c7cc0da09d91a
Shahzad <shahzad.muhammad@elastic.co>
2020-12-09 12:45:40 +0100

    [Uptime]Refactor header and action menu (#83779)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/uptime/public/components/common/header/__tests__/page_header.test.tsx
+++ x-pack/plugins/uptime/public/components/common/header/__tests__/page_header.test.tsx
@@ -6,13 +6,13 @@
 
 import React from 'react';
 import { PageHeader } from '../page_header';
-import { renderWithRouter, MountWithReduxProvider } from '../../lib';
+import { renderWithRouter, MountWithReduxProvider } from '../../../../lib';
 
 describe('PageHeader', () => {
   it('shallow renders with the date picker', () => {
     const component = renderWithRouter(
       <MountWithReduxProvider>
-        <PageHeader headingText={'TestingHeading'} datePicker={true} />
+        <PageHeader />
       </MountWithReduxProvider>
     );
     expect(component).toMatchSnapshot('page_header_with_date_picker');
@@ -21,7 +21,7 @@ describe('PageHeader', () => {
   it('shallow renders without the date picker', () => {
     const component = renderWithRouter(
       <MountWithReduxProvider>
-        <PageHeader headingText={'TestingHeading'} datePicker={false} />
+        <PageHeader />
       </MountWithReduxProvider>
     );
     expect(component).toMatchSnapshot('page_header_no_date_picker');
@@ -30,7 +30,7 @@ describe('PageHeader', () => {
   it('shallow renders extra links', () => {
     const component = renderWithRouter(
       <MountWithReduxProvider>
-        <PageHeader headingText={'TestingHeading'} extraLinks={true} datePicker={true} />
+        <PageHeader />
       </MountWithReduxProvider>
     );
     expect(component).toMatchSnapshot('page_header_with_extra_links');
