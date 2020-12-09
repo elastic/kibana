48579e0b5ccdc66ca2add2fb415d6986c9a62413
Larry Gregory <lgregorydev@gmail.com>
2020-12-09 11:49:36 -0500

    Update x-pack/plugins/security/server/config_deprecations.ts
    Co-authored-by: Aleh Zasypkin <aleh.zasypkin@gmail.com>

--- x-pack/plugins/security/server/config_deprecations.ts
+++ x-pack/plugins/security/server/config_deprecations.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { ConfigDeprecationProvider } from 'src/core/server';
+import type { ConfigDeprecationProvider } from 'src/core/server';
 
 export const securityConfigDeprecationProvider: ConfigDeprecationProvider = ({
   rename,
