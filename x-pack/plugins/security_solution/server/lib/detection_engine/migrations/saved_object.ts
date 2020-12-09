b579f5474e393414bc19d09b3cf5e57f71979c33
Ryland Herrick <ryalnd@gmail.com>
2020-12-08 21:25:32 -0600

    Adds new SO type for persisting our signals migrations

---
+++ x-pack/plugins/security_solution/server/lib/detection_engine/migrations/saved_object.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { SavedObjectsType } from 'src/core/server';
+
+export const signalsMigrationType = 'security-solution-signals-migration';
+
+export const signalsMigrationMappings: SavedObjectsType['mappings'] = {
+  properties: {
+    sourceIndex: {
+      type: 'keyword',
+    },
+    destinationIndex: {
+      type: 'keyword',
+    },
+    version: {
+      type: 'long',
+    },
+    taskId: {
+      type: 'keyword',
+    },
+    status: {
+      type: 'keyword',
+    },
+    created: {
+      type: 'date',
+    },
+    createdBy: {
+      type: 'text',
+    },
+    updated: {
+      type: 'date',
+    },
+    updatedBy: {
+      type: 'text',
+    },
+  },
+};
+
+export const type: SavedObjectsType = {
+  name: signalsMigrationType,
+  hidden: false,
+  namespaceType: 'single',
+  mappings: signalsMigrationMappings,
+};
