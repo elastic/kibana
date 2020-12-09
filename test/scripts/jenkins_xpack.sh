8364444c2a235dd574aeec7ccc5e39d09852353b
Tyler Smalley <tyler.smalley@elastic.co>
2020-12-08 22:38:38 -0800

    Updates test:jest script
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- test/scripts/jenkins_xpack.sh
+++ test/scripts/jenkins_xpack.sh
@@ -4,23 +4,15 @@ source test/scripts/jenkins_test_setup.sh
 
 if [[ -z "$CODE_COVERAGE" ]] ; then
   echo " -> Running jest tests"
-  cd "$XPACK_DIR"
-  checks-reporter-with-killswitch "X-Pack Jest" node --max-old-space-size=6144 scripts/jest --ci --verbose
-  echo ""
-  echo ""
-
-  # echo " -> Running jest integration tests"
-  # cd "$XPACK_DIR"
-  # node scripts/jest_integration --ci --verbose
-  # echo ""
-  # echo ""
+  
+  ./test/scripts/test/xpack_jest_unit.sh
 else
   echo " -> Running jest tests with coverage"
-  cd "$XPACK_DIR"
+
   # build runtime for canvas
   echo "NODE_ENV=$NODE_ENV"
-  node ./plugins/canvas/scripts/shareable_runtime
-  node --max-old-space-size=6144 scripts/jest --ci --verbose --coverage
+  node ./x-pack/plugins/canvas/scripts/shareable_runtime
+  node scripts/jest x-pack --ci --verbose --maxWorkers=5 --coverage
   # rename file in order to be unique one
   test -f ../target/kibana-coverage/jest/coverage-final.json \
     && mv ../target/kibana-coverage/jest/coverage-final.json \
