0244c9498ae0a1f1d66124578ef614d326bfbc88
Patrick Mueller <pmuellr@gmail.com>
2020-12-09 14:44:10 -0500

    [task manager] provide warning when setting max_workers greater than limit
    resolves https://github.com/elastic/kibana/issues/56573
    
    In this PR we create a new task manager limit on the config property
    `xpack.task_manager.max_workers` of 100, but only log a deprecation
    warning if that property exceeds the limit.  We'll enforce the limit
    in 8.0.
    
    The rationale is that it's unlikely going to be useful to run with
    more than some number of workers, due to the amount of simultaneous
    work that would end up happening.  In practice, too many workers can
    slow things down more than speed them up.
    
    We're setting the limit to 100 for now, but may increase / decrease it
    based on further research.

--- x-pack/plugins/task_manager/server/index.ts
+++ x-pack/plugins/task_manager/server/index.ts
@@ -7,7 +7,7 @@
 import { get } from 'lodash';
 import { PluginConfigDescriptor, PluginInitializerContext } from 'src/core/server';
 import { TaskManagerPlugin } from './plugin';
-import { configSchema, TaskManagerConfig } from './config';
+import { configSchema, TaskManagerConfig, MAX_MAX_WORKERS } from './config';
 
 export const plugin = (initContext: PluginInitializerContext) => new TaskManagerPlugin(initContext);
 
@@ -35,6 +35,11 @@ export const config: PluginConfigDescriptor<TaskManagerConfig> = {
       if (taskManager?.index) {
         log(
           `"${fromPath}.index" is deprecated. Multitenancy by changing "kibana.index" will not be supported starting in 8.0. See https://ela.st/kbn-remove-legacy-multitenancy for more details`
+        );
+      }
+      if (taskManager?.max_workers > MAX_MAX_WORKERS) {
+        log(
+          `setting "${fromPath}.max_workers" (${taskManager?.max_workers}) greater than ${MAX_MAX_WORKERS} is deprecated. Values greater than ${MAX_MAX_WORKERS} will not be supported starting in 8.0.`
         );
       }
       return settings;
