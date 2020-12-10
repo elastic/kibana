0968676c6125c7acffaca74945199ad9108d8688
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2020-12-10 09:12:01 -0500

    [7.x] [SECURITY SOLUTION] Bundles _source -> Fields + able to sort on multiple fields in Timeline (#83761) (#85505)
    * [SECURITY SOLUTION] Bundles _source -> Fields + able to sort on multiple fields in Timeline (#83761)
    
    * replace _source with fields
    
    * wip
    
    * unit test
    
    * regroup sorting and number together
    
    * fix bugs from review
    
    * mistake
    
    * Update x-pack/plugins/security_solution/public/timelines/components/timeline/body/column_headers/index.tsx
    
    Co-authored-by: Patryk Kopyciński <contact@patrykkopycinski.com>
    
    * fix snapshot
    
    * review + fix topN and filter from detail view
    
    * fix tests
    
    * fix test
    
    Co-authored-by: Patryk Kopyciński <contact@patrykkopycinski.com>
    
    * Update timeline_details.ts
    
    Co-authored-by: Patryk Kopyciński <contact@patrykkopycinski.com>

--- x-pack/plugins/security_solution/public/timelines/containers/details/index.tsx
+++ x-pack/plugins/security_solution/public/timelines/containers/details/index.tsx
@@ -85,6 +85,9 @@ export const useTimelineEventsDetails = ({
               }
             },
             error: () => {
+              if (!didCancel) {
+                setLoading(false);
+              }
               notifications.toasts.addDanger('Failed to run search');
             },
           });
