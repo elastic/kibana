6de3686d6fc800cd37a6e64b36a5da2f1474b804
Paul Tavares <paul.tavares@elastic.co>
2020-12-10 09:53:48 -0500

    Endpoint: initial UI for Trusted Apps custom entry

--- x-pack/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_package_custom_extension.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_package_custom_extension.tsx
@@ -4,10 +4,64 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
+import { EuiPanel } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { useKibana } from '../../../../../../../../../src/plugins/kibana_react/public';
+import { LinkToApp } from '../../../../../common/components/endpoint/link_to_app';
+import { MANAGEMENT_APP_ID } from '../../../../common/constants';
+import { getTrustedAppsListPath } from '../../../../common/routing';
+import { TrustedAppsListPageRouteState } from '../../../../../../common/endpoint/types';
+import { PLUGIN_ID as FLEET_PLUGIN_ID } from '../../../../../../../fleet/common';
+import {
+  PackageCustomExtensionComponentProps,
+  pagePathGetters,
+} from '../../../../../../../fleet/public';
 
-export const EndpointPackageCustomExtension = memo(() => {
-  return <h1>{'Endpoint custom here'}</h1>;
-});
+export const EndpointPackageCustomExtension = memo<PackageCustomExtensionComponentProps>(
+  ({ pkgkey }) => {
+    const {
+      services: {
+        application: { getUrlForApp },
+      },
+    } = useKibana();
+    const trustedAppsListUrlPath = getTrustedAppsListPath();
+
+    const trustedAppRouteState = useMemo<TrustedAppsListPageRouteState>(() => {
+      const fleetPackageCustomUrlPath = `#${pagePathGetters.integration_details({
+        pkgkey,
+        panel: 'custom',
+      })}`;
+      return {
+        backButtonLabel: i18n.translate(
+          'xpack.securitySolution.endpoint.fleetCustomExtension.backButtonLabel',
+          { defaultMessage: 'Back to Endpoint Integration' }
+        ),
+        onBackButtonNavigateTo: [
+          FLEET_PLUGIN_ID,
+          {
+            path: fleetPackageCustomUrlPath,
+          },
+        ],
+        backButtonUrl: getUrlForApp(FLEET_PLUGIN_ID, {
+          path: fleetPackageCustomUrlPath,
+        }),
+      };
+    }, [getUrlForApp, pkgkey]);
+
+    return (
+      <EuiPanel paddingSize="l">
+        <LinkToApp
+          appId={MANAGEMENT_APP_ID}
+          href={getUrlForApp(MANAGEMENT_APP_ID, { path: trustedAppsListUrlPath })}
+          appPath={trustedAppsListUrlPath}
+          appState={trustedAppRouteState}
+        >
+          {'Trusted Application'}
+        </LinkToApp>
+      </EuiPanel>
+    );
+  }
+);
 
 EndpointPackageCustomExtension.displayName = 'EndpointPackageCustomExtension';
