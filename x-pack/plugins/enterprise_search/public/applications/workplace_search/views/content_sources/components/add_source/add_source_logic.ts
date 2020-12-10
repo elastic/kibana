4706491ca62ee892ed3a87eb49116bf566521cf0
scottybollinger <scotty.bollinger@elastic.co>
2020-12-10 15:59:27 -0600

    Delete duplicated logic data

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/add_source_logic.ts
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/add_source_logic.ts
@@ -11,36 +11,19 @@ import { kea, MakeLogicType } from 'kea';
 import { i18n } from '@kbn/i18n';
 
 import { HttpLogic } from '../../../../../shared/http';
-import { KibanaLogic } from '../../../../../shared/kibana';
 
 import {
   flashAPIErrors,
   setSuccessMessage,
-  setQueuedSuccessMessage,
   FlashMessagesLogic,
 } from '../../../../../shared/flash_messages';
 
-import { DEFAULT_META } from '../../../../../shared/constants';
 import { AppLogic } from '../../../../app_logic';
-import { NOT_FOUND_PATH } from '../../../../routes';
-import {
-  ContentSourceFullData,
-  CustomSource,
-  Meta,
-  DocumentSummaryItem,
-  SourceContentItem,
-} from '../../../../types';
+import { CustomSource } from '../../../../types';
 
 export interface AddSourceActions {
-  onInitializeSource(contentSource: ContentSourceFullData): ContentSourceFullData;
-  onUpdateSourceName(name: string): string;
   setSourceConfigData(sourceConfigData: SourceConfigData): SourceConfigData;
   setSourceConnectData(sourceConnectData: SourceConnectData): SourceConnectData;
-  setSearchResults(searchResultsResponse: SearchResultsResponse): SearchResultsResponse;
-  initializeFederatedSummary(sourceId: string): { sourceId: string };
-  onUpdateSummary(summary: DocumentSummaryItem[]): DocumentSummaryItem[];
-  setContentFilterValue(contentFilterValue: string): string;
-  setActivePage(activePage: number): number;
   setClientIdValue(clientIdValue: string): string;
   setClientSecretValue(clientSecretValue: string): string;
   setBaseUrlValue(baseUrlValue: string): string;
@@ -52,16 +35,7 @@ export interface AddSourceActions {
   setCustomSourceData(data: CustomSource): CustomSource;
   setPreContentSourceConfigData(data: PreContentSourceResponse): PreContentSourceResponse;
   setSelectedGithubOrganizations(option: string): string;
-  searchContentSourceDocuments(sourceId: string): { sourceId: string };
-  updateContentSource(
-    sourceId: string,
-    source: { name: string }
-  ): { sourceId: string; source: { name: string } };
   resetSourceState(): void;
-  removeContentSource(
-    sourceId: string,
-    successCallback: () => void
-  ): { sourceId: string; successCallback(): void };
   createContentSource(
     serviceType: string,
     successCallback: () => void,
@@ -71,7 +45,6 @@ export interface AddSourceActions {
     isUpdating: boolean,
     successCallback?: () => void
   ): { isUpdating: boolean; successCallback?(): void };
-  initializeSource(sourceId: string, history: object): { sourceId: string; history: object };
   getSourceConfigData(serviceType: string): { serviceType: string };
   getSourceConnectData(
     serviceType: string,
@@ -110,13 +83,9 @@ interface OrganizationsMap {
 }
 
 interface AddSourceValues {
-  contentSource: ContentSourceFullData;
   dataLoading: boolean;
   sectionLoading: boolean;
   buttonLoading: boolean;
-  contentItems: SourceContentItem[];
-  contentMeta: Meta;
-  contentFilterValue: string;
   customSourceNameValue: string;
   clientIdValue: string;
   clientSecretValue: string;
@@ -134,11 +103,6 @@ interface AddSourceValues {
   selectedGithubOrganizations: string[];
 }
 
-interface SearchResultsResponse {
-  results: SourceContentItem[];
-  meta: Meta;
-}
-
 interface PreContentSourceResponse {
   id: string;
   serviceType: string;
@@ -146,16 +110,10 @@ interface PreContentSourceResponse {
 }
 
 export const AddSourceLogic = kea<MakeLogicType<AddSourceValues, AddSourceActions>>({
-  path: ['enterprise_search', 'workplace_search', 'source_logic'],
+  path: ['enterprise_search', 'workplace_search', 'add_source_logic'],
   actions: {
-    onInitializeSource: (contentSource: ContentSourceFullData) => contentSource,
-    onUpdateSourceName: (name: string) => name,
     setSourceConfigData: (sourceConfigData: SourceConfigData) => sourceConfigData,
     setSourceConnectData: (sourceConnectData: SourceConnectData) => sourceConnectData,
-    onUpdateSummary: (summary: object[]) => summary,
-    setSearchResults: (searchResultsResponse: SearchResultsResponse) => searchResultsResponse,
-    setContentFilterValue: (contentFilterValue: string) => contentFilterValue,
-    setActivePage: (activePage: number) => activePage,
     setClientIdValue: (clientIdValue: string) => clientIdValue,
     setClientSecretValue: (clientSecretValue: string) => clientSecretValue,
     setBaseUrlValue: (baseUrlValue: string) => baseUrlValue,
@@ -167,14 +125,6 @@ export const AddSourceLogic = kea<MakeLogicType<AddSourceValues, AddSourceAction
     setCustomSourceData: (data: CustomSource) => data,
     setPreContentSourceConfigData: (data: PreContentSourceResponse) => data,
     setSelectedGithubOrganizations: (option: string) => option,
-    initializeSource: (sourceId: string, history: object) => ({ sourceId, history }),
-    initializeFederatedSummary: (sourceId: string) => ({ sourceId }),
-    searchContentSourceDocuments: (sourceId: string) => ({ sourceId }),
-    updateContentSource: (sourceId: string, source: { name: string }) => ({ sourceId, source }),
-    removeContentSource: (sourceId: string, successCallback: () => void) => ({
-      sourceId,
-      successCallback,
-    }),
     getSourceConfigData: (serviceType: string) => ({ serviceType }),
     getSourceConnectData: (serviceType: string, successCallback: (oauthUrl: string) => string) => ({
       serviceType,
@@ -195,20 +145,6 @@ export const AddSourceLogic = kea<MakeLogicType<AddSourceValues, AddSourceAction
     setButtonNotLoading: () => false,
   },
   reducers: {
-    contentSource: [
-      {} as ContentSourceFullData,
-      {
-        onInitializeSource: (_, contentSource) => contentSource,
-        onUpdateSourceName: (contentSource, name) => ({
-          ...contentSource,
-          name,
-        }),
-        onUpdateSummary: (contentSource, summary) => ({
-          ...contentSource,
-          summary,
-        }),
-      },
-    ],
     sourceConfigData: [
       {} as SourceConfigData,
       {
@@ -237,7 +173,6 @@ export const AddSourceLogic = kea<MakeLogicType<AddSourceValues, AddSourceAction
         setSourceConnectData: () => false,
         setSourceConfigData: () => false,
         resetSourceState: () => false,
-        removeContentSource: () => true,
         saveSourceConfig: () => true,
         getSourceConnectData: () => true,
         createContentSource: () => true,
@@ -246,31 +181,9 @@ export const AddSourceLogic = kea<MakeLogicType<AddSourceValues, AddSourceAction
     sectionLoading: [
       true,
       {
-        searchContentSourceDocuments: () => true,
         getPreContentSourceConfigData: () => true,
         setSearchResults: () => false,
         setPreContentSourceConfigData: () => false,
-      },
-    ],
-    contentItems: [
-      [],
-      {
-        setSearchResults: (_, { results }) => results,
-      },
-    ],
-    contentMeta: [
-      DEFAULT_META,
-      {
-        setActivePage: (state, activePage) => setPage(state, activePage),
-        setContentFilterValue: (state) => setPage(state, DEFAULT_META.page.current),
-        setSearchResults: (_, { meta }) => meta,
-      },
-    ],
-    contentFilterValue: [
-      '',
-      {
-        setContentFilterValue: (_, contentFilterValue) => contentFilterValue,
-        resetSourceState: () => '',
       },
     ],
     clientIdValue: [
@@ -371,98 +284,6 @@ export const AddSourceLogic = kea<MakeLogicType<AddSourceValues, AddSourceAction
     ],
   }),
   listeners: ({ actions, values }) => ({
-    initializeSource: async ({ sourceId }) => {
-      const { isOrganization } = AppLogic.values;
-      const route = isOrganization
-        ? `/api/workplace_search/org/sources/${sourceId}`
-        : `/api/workplace_search/account/sources/${sourceId}`;
-
-      try {
-        const response = await HttpLogic.values.http.get(route);
-        actions.onInitializeSource(response);
-        if (response.isFederatedSource) {
-          actions.initializeFederatedSummary(sourceId);
-        }
-      } catch (e) {
-        // TODO: Verify this works once components are there. Not sure if the catch gives a status code.
-        if (e.response.status === 404) {
-          KibanaLogic.values.navigateToUrl(NOT_FOUND_PATH);
-        } else {
-          flashAPIErrors(e);
-        }
-      }
-    },
-    initializeFederatedSummary: async ({ sourceId }) => {
-      const route = `/api/workplace_search/org/sources/${sourceId}/federated_summary`;
-      try {
-        const response = await HttpLogic.values.http.get(route);
-        actions.onUpdateSummary(response.summary);
-      } catch (e) {
-        flashAPIErrors(e);
-      }
-    },
-    searchContentSourceDocuments: async ({ sourceId }, breakpoint) => {
-      await breakpoint(300);
-
-      const { isOrganization } = AppLogic.values;
-      const route = isOrganization
-        ? `/api/workplace_search/org/sources/${sourceId}/documents`
-        : `/api/workplace_search/account/sources/${sourceId}/documents`;
-
-      const {
-        contentFilterValue: query,
-        contentMeta: { page },
-      } = values;
-
-      try {
-        const response = await HttpLogic.values.http.post(route, {
-          body: JSON.stringify({ query, page }),
-        });
-        actions.setSearchResults(response);
-      } catch (e) {
-        flashAPIErrors(e);
-      }
-    },
-    updateContentSource: async ({ sourceId, source }) => {
-      const { isOrganization } = AppLogic.values;
-      const route = isOrganization
-        ? `/api/workplace_search/org/sources/${sourceId}/settings`
-        : `/api/workplace_search/account/sources/${sourceId}/settings`;
-
-      try {
-        const response = await HttpLogic.values.http.patch(route, {
-          body: JSON.stringify({ content_source: source }),
-        });
-        actions.onUpdateSourceName(response.name);
-      } catch (e) {
-        flashAPIErrors(e);
-      }
-    },
-    removeContentSource: async ({ sourceId, successCallback }) => {
-      FlashMessagesLogic.actions.clearFlashMessages();
-      const { isOrganization } = AppLogic.values;
-      const route = isOrganization
-        ? `/api/workplace_search/org/sources/${sourceId}`
-        : `/api/workplace_search/account/sources/${sourceId}`;
-
-      try {
-        const response = await HttpLogic.values.http.delete(route);
-        setQueuedSuccessMessage(
-          i18n.translate(
-            'xpack.enterpriseSearch.workplaceSearch.sources.flashMessages.contentSourceRemoved',
-            {
-              defaultMessage: 'Successfully deleted {sourceName}.',
-              values: { sourceName: response.name },
-            }
-          )
-        );
-        successCallback();
-      } catch (e) {
-        flashAPIErrors(e);
-      } finally {
-        actions.setButtonNotLoading();
-      }
-    },
     getSourceConfigData: async ({ serviceType }) => {
       const route = `/api/workplace_search/org/settings/connectors/${serviceType}`;
 
@@ -616,27 +437,5 @@ export const AddSourceLogic = kea<MakeLogicType<AddSourceValues, AddSourceAction
         actions.setButtonNotLoading();
       }
     },
-    onUpdateSourceName: (name: string) => {
-      setSuccessMessage(
-        i18n.translate(
-          'xpack.enterpriseSearch.workplaceSearch.sources.flashMessages.contentSourceNameChanged',
-          {
-            defaultMessage: 'Successfully changed name to {sourceName}.',
-            values: { sourceName: name },
-          }
-        )
-      );
-    },
-    resetSourceState: () => {
-      FlashMessagesLogic.actions.clearFlashMessages();
-    },
   }),
-});
-
-const setPage = (state: Meta, page: number) => ({
-  ...state,
-  page: {
-    ...state.page,
-    current: page,
-  },
 });
