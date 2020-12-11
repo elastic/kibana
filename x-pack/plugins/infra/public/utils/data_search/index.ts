944eeafea22aa2a381b4db7303d7449d78b30130
Felix Stürmer <weltenwort@users.noreply.github.com>
2020-12-11 14:53:51 +0100

    [7.x] [Logs UI] Add helper hooks with search strategy request cancellation (#83906) (#85697)
    Backports the following commits to 7.x:
     - [Logs UI] Add helper hooks with search strategy request cancellation (#83906)

--- x-pack/plugins/infra/public/utils/data_search/index.ts
+++ x-pack/plugins/infra/public/utils/data_search/index.ts
@@ -4,9 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-const defaultConfig = require('@kbn/storybook').defaultConfig;
-
-module.exports = {
-  ...defaultConfig,
-  stories: ['../**/*.stories.mdx', ...defaultConfig.stories],
-};
+export * from './types';
+export * from './use_data_search_request';
+export * from './use_latest_partial_data_search_response';
