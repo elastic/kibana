ed735aad903c4e120ae150396b63ef7ee9525c4d
James Gowdy <jgowdy@elastic.co>
2020-12-14 10:03:07 +0000

    adding job refresh to anomaly detectors

--- x-pack/plugins/ml/public/application/jobs/jobs_list/components/delete_job_modal/delete_job_modal.tsx
+++ x-pack/plugins/ml/public/application/jobs/jobs_list/components/delete_job_modal/delete_job_modal.tsx
@@ -148,6 +148,7 @@ export const DeleteJobModal: FC<Props> = ({ setShowFunction, unsetShowFunction, 
             setCanDelete(true);
           }}
           onCloseCallback={closeModal}
+          refreshJobsCallback={refreshJobs}
         />
       </>
     );
