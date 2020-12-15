6466943ea3b6115a21edc261f92e218dcb036fce
Thomas Neirynck <thomas@elastic.co>
2020-12-15 17:53:25 -0500

    boilerplate

--- x-pack/plugins/maps/public/classes/layers/solution_layers/security/security_layer_wizard.tsx
+++ x-pack/plugins/maps/public/classes/layers/solution_layers/security/security_layer_wizard.tsx
@@ -10,13 +10,30 @@ import { LAYER_WIZARD_CATEGORY } from '../../../../../common/constants';
 import { LayerWizard, RenderWizardArguments } from '../../layer_wizard_registry';
 import { getSecurityIndexPatterns } from './security_index_pattern_utils';
 import { SecurityLayerTemplate } from './security_layer_template';
+import { getUiSettings } from '../../../../kibana_services';
+
+function getIpId() {
+  const uiSettings = getUiSettings();
+  try {
+    const securityIndexPatternTitles = uiSettings.get('securitySolution:defaultIndex');
+    return securityIndexPatternTitles.join(',');
+  } catch (e) {
+    return '';
+  }
+}
 
 export const SecurityLayerWizardConfig: LayerWizard = {
   categories: [LAYER_WIZARD_CATEGORY.ELASTICSEARCH, LAYER_WIZARD_CATEGORY.SOLUTIONS],
-  checkVisibility: async () => {
+  getIsDisabled: async () => {
     const indexPatterns = await getSecurityIndexPatterns();
-    return indexPatterns.length > 0;
+    return indexPatterns.length === 0;
   },
+  disabledReason: i18n.translate('xpack.maps.security.disabledDesc', {
+    defaultMessage: 'There is no default security index {index}.',
+    values: {
+      index: getIpId(),
+    },
+  }),
   description: i18n.translate('xpack.maps.security.desc', {
     defaultMessage: 'Security layers',
   }),
