77eb86075ae5ade5b67e91eddc2683cd42495758
cauemarcondes <caue.marcondes@elastic.co>
2020-12-15 15:16:24 +0100

    sorting service version

--- x-pack/plugins/apm/server/lib/services/get_service_details.ts
+++ x-pack/plugins/apm/server/lib/services/get_service_details.ts
@@ -94,6 +94,7 @@ export async function getServiceDetails({
           terms: {
             field: SERVICE_VERSION,
             size: 10,
+            order: { _key: 'asc' },
           },
         },
         availabilityZones: {
