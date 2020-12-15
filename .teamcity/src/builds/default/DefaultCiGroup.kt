2deeb257a80620ec4e9c5d147482be4f1614fa85
Brian Seeders <brian.seeders@elastic.co>
2020-12-15 17:16:39 -0500

    X-Pack agents back to 4 cpu machines

--- .teamcity/src/builds/default/DefaultCiGroup.kt
+++ .teamcity/src/builds/default/DefaultCiGroup.kt
@@ -13,5 +13,7 @@ class DefaultCiGroup(val ciGroup: Int = 0, init: BuildType.() -> Unit = {}) : De
     runbld("Default CI Group $ciGroup", "./.ci/teamcity/default/ci_group.sh $ciGroup")
   }
 
+  requireAgent(StandardAgents["4"]!!)
+
   init()
 })
