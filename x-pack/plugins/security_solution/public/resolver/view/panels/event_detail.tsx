d4a631cf8e2e6ef9fcc4c86256013d4389d5df7d
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2020-12-15 08:31:59 -0500

    [Security Solution][Resolver] Fixing resolver functional tests (#85647)
    * Fixing resolver functional tests
    
    * Import the animation constant
    
    * Only check specific nodes instead of all the ones in view
    
    * Removing check for link text
    
    * updating test description
    
    * Adding comments

--- x-pack/plugins/security_solution/public/resolver/view/panels/event_detail.tsx
+++ x-pack/plugins/security_solution/public/resolver/view/panels/event_detail.tsx
@@ -8,7 +8,7 @@
 
 /* eslint-disable react/display-name */
 
-import React, { memo, useMemo, Fragment } from 'react';
+import React, { memo, useMemo, Fragment, HTMLAttributes } from 'react';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { EuiSpacer, EuiText, EuiDescriptionList, EuiTextColor, EuiTitle } from '@elastic/eui';
@@ -246,7 +246,12 @@ function EventDetailBreadcrumbs({
     panelParameters: { nodeID, eventCategory: breadcrumbEventCategory },
   });
   const breadcrumbs = useMemo(() => {
-    const crumbs = [
+    const crumbs: Array<
+      {
+        text: JSX.Element | string;
+        'data-test-subj'?: string;
+      } & HTMLAttributes<HTMLAnchorElement>
+    > = [
       {
         text: i18n.translate(
           'xpack.securitySolution.endpoint.resolver.panel.relatedEventDetail.events',
@@ -254,6 +259,7 @@ function EventDetailBreadcrumbs({
             defaultMessage: 'Events',
           }
         ),
+        'data-test-subj': 'resolver:event-detail:breadcrumbs:node-list-link',
         ...nodesLinkNavProps,
       },
       {
--- x-pack/test/security_solution_endpoint/page_objects/hosts_page.ts
+++ x-pack/test/security_solution_endpoint/page_objects/hosts_page.ts
@@ -4,13 +4,17 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import expect from '@kbn/expect';
+import { WebElementWrapper } from 'test/functional/services/lib/web_element_wrapper';
+import { nudgeAnimationDuration } from '../../../plugins/security_solution/public/resolver/store/camera/scaling_constants';
 import { FtrProviderContext } from '../ftr_provider_context';
-import { deleteEventsStream } from '../../security_solution_endpoint_api_int/apis/data_stream_helper';
-import { deleteAlertsStream } from '../../security_solution_endpoint_api_int/apis/data_stream_helper';
-import { deleteMetadataStream } from '../../security_solution_endpoint_api_int/apis/data_stream_helper';
-import { deletePolicyStream } from '../../security_solution_endpoint_api_int/apis/data_stream_helper';
-import { deleteTelemetryStream } from '../../security_solution_endpoint_api_int/apis/data_stream_helper';
+import {
+  deleteEventsStream,
+  deleteAlertsStream,
+  deleteMetadataStream,
+  deletePolicyStream,
+  deleteTelemetryStream,
+} from '../../security_solution_endpoint_api_int/apis/data_stream_helper';
+
 export interface DataStyle {
   left: string;
   top: string;
@@ -22,6 +26,109 @@ export function SecurityHostsPageProvider({ getService, getPageObjects }: FtrPro
   const pageObjects = getPageObjects(['common', 'header']);
   const testSubjects = getService('testSubjects');
   const queryBar = getService('queryBar');
+  const find = getService('find');
+
+  /**
+   * Returns the node IDs for the visible nodes in the resolver graph.
+   */
+  const findVisibleNodeIDs = async (): Promise<string[]> => {
+    const visibleNodes = await testSubjects.findAll('resolver:node');
+    return Promise.all(
+      visibleNodes.map(async (node: WebElementWrapper) => {
+        return node.getAttribute('data-test-resolver-node-id');
+      })
+    );
+  };
+
+  /**
+   * This assumes you are on the process list in the panel and will find and click the node
+   * with the given ID to bring it into view in the graph.
+   *
+   * @param id the ID of the node to find and click.
+   */
+  const clickNodeLinkInPanel = async (id: string): Promise<void> => {
+    await navigateToProcessListInPanel();
+    const panelNodeButton = await find.byCssSelector(
+      `[data-test-subj='resolver:node-list:node-link'][data-test-node-id='${id}']`
+    );
+
+    await panelNodeButton?.click();
+    // ensure that we wait longer than the animation time
+    await pageObjects.common.sleep(nudgeAnimationDuration * 2);
+  };
+
+  /**
+   * Finds all the pills for a particular node.
+   *
+   * @param id the ID of the node
+   */
+  const findNodePills = async (id: string): Promise<WebElementWrapper[]> => {
+    return testSubjects.findAllDescendant(
+      'resolver:map:node-submenu-item',
+      await find.byCssSelector(
+        `[data-test-subj='resolver:node'][data-test-resolver-node-id='${id}']`
+      )
+    );
+  };
+
+  /**
+   * Navigate back to the process list view in the panel.
+   */
+  const navigateToProcessListInPanel = async () => {
+    const [
+      isOnNodeListPage,
+      isOnCategoryPage,
+      isOnNodeDetailsPage,
+      isOnRelatedEventDetailsPage,
+    ] = await Promise.all([
+      testSubjects.exists('resolver:node-list', { timeout: 1 }),
+      testSubjects.exists('resolver:node-events-in-category:breadcrumbs:node-list-link', {
+        timeout: 1,
+      }),
+      testSubjects.exists('resolver:node-detail:breadcrumbs:node-list-link', { timeout: 1 }),
+      testSubjects.exists('resolver:event-detail:breadcrumbs:node-list-link', { timeout: 1 }),
+    ]);
+
+    if (isOnNodeListPage) {
+      return;
+    } else if (isOnCategoryPage) {
+      await (
+        await testSubjects.find('resolver:node-events-in-category:breadcrumbs:node-list-link')
+      ).click();
+    } else if (isOnNodeDetailsPage) {
+      await (await testSubjects.find('resolver:node-detail:breadcrumbs:node-list-link')).click();
+    } else if (isOnRelatedEventDetailsPage) {
+      await (await testSubjects.find('resolver:event-detail:breadcrumbs:node-list-link')).click();
+    } else {
+      // unknown page
+      return;
+    }
+
+    await pageObjects.common.sleep(100);
+  };
+
+  /**
+   * Click the zoom out control.
+   */
+  const clickZoomOut = async () => {
+    await (await testSubjects.find('resolver:graph-controls:zoom-out')).click();
+  };
+
+  /**
+   * Navigate to Events Panel
+   */
+  const navigateToEventsPanel = async () => {
+    const isFullScreen = await testSubjects.exists('exit-full-screen', { timeout: 400 });
+    if (isFullScreen) {
+      await (await testSubjects.find('exit-full-screen')).click();
+    }
+
+    if (!(await testSubjects.exists('investigate-in-resolver-button', { timeout: 400 }))) {
+      await (await testSubjects.find('navigation-hosts')).click();
+      await testSubjects.click('navigation-events');
+      await testSubjects.existOrFail('event');
+    }
+  };
 
   /**
    * @function parseStyles
@@ -54,101 +161,82 @@ export function SecurityHostsPageProvider({ getService, getPageObjects }: FtrPro
         }),
         {}
       );
-  return {
-    /**
-     * Navigate to the Security Hosts page
-     */
-    async navigateToSecurityHostsPage() {
-      await pageObjects.common.navigateToUrlWithBrowserHistory('security', '/hosts/AllHosts');
-      await pageObjects.header.waitUntilLoadingHasFinished();
-    },
-    /**
-     * Finds a table and returns the data in a nested array with row 0 is the headers if they exist.
-     * It uses euiTableCellContent to avoid poluting the array data with the euiTableRowCell__mobileHeader data.
-     * @param dataTestSubj
-     * @param element
-     * @returns Promise<string[][]>
-     */
-    async getEndpointEventResolverNodeData(dataTestSubj: string, element: string) {
-      await testSubjects.exists(dataTestSubj);
-      const Elements = await testSubjects.findAll(dataTestSubj);
-      const $ = [];
-      for (const value of Elements) {
-        $.push(await value.getAttribute(element));
-      }
-      return $;
-    },
 
-    /**
-     * Gets a array of not parsed styles and returns the Array of parsed styles.
-     * @returns Promise<string[][]>
-     */
-    async parseStyles() {
-      const tableData = await this.getEndpointEventResolverNodeData('resolver:node', 'style');
-      const styles: DataStyle[] = [];
-      for (let i = 1; i < tableData.length; i++) {
-        const eachStyle = parseStyle(tableData[i]);
-        styles.push({
-          top: eachStyle.top ?? '',
-          height: eachStyle.height ?? '',
-          left: eachStyle.left ?? '',
-          width: eachStyle.width ?? '',
-        });
-      }
-      return styles;
-    },
-    /**
-     * Deletes DataStreams from Index Management.
-     */
-    async deleteDataStreams() {
-      await deleteEventsStream(getService);
-      await deleteAlertsStream(getService);
-      await deletePolicyStream(getService);
-      await deleteMetadataStream(getService);
-      await deleteTelemetryStream(getService);
-    },
-    /**
-     * Runs Nodes Events
-     */
-    async runNodeEvents(expectedData: string[]) {
-      await testSubjects.exists('resolver:submenu:button', { timeout: 400 });
-      const NodeSubmenuButtons = await testSubjects.findAll('resolver:submenu:button');
-      for (let b = 0; b < NodeSubmenuButtons.length; b++) {
-        await (await testSubjects.findAll('resolver:submenu:button'))[b].click();
-      }
-      await testSubjects.exists('resolver:map:node-submenu-item', { timeout: 400 });
-      const NodeSubmenuItems = await testSubjects.findAll('resolver:map:node-submenu-item');
-      for (let i = 0; i < NodeSubmenuItems.length; i++) {
-        await (await testSubjects.findAll('resolver:map:node-submenu-item'))[i].click();
-        const Events = await testSubjects.findAll('resolver:map:node-submenu-item');
-        // this sleep is for the AMP enabled run
-        await pageObjects.common.sleep(300);
-        const EventName = await Events[i]._webElement.getText();
-        const LinkText = await testSubjects.find('resolver:breadcrumbs:last');
-        const linkText = await LinkText._webElement.getText();
-        expect(EventName).to.equal(linkText);
-        expect(EventName).to.equal(expectedData[i]);
-      }
-      await testSubjects.click('full-screen');
-    },
-    /**
-     * Navigate to Events Panel
-     */
-    async navigateToEventsPanel() {
-      if (!(await testSubjects.exists('investigate-in-resolver-button', { timeout: 400 }))) {
-        await (await testSubjects.find('navigation-hosts')).click();
-        await testSubjects.click('navigation-events');
-        await testSubjects.existOrFail('event');
-      }
-    },
-    /**
-     * execute Query And Open Resolver
-     */
-    async executeQueryAndOpenResolver(query: string) {
-      await queryBar.setQuery(query);
-      await queryBar.submitQuery();
-      await testSubjects.click('full-screen');
-      await testSubjects.click('investigate-in-resolver-button');
-    },
+  /**
+   * Navigate to the Security Hosts page
+   */
+  const navigateToSecurityHostsPage = async () => {
+    await pageObjects.common.navigateToUrlWithBrowserHistory('security', '/hosts/AllHosts');
+    await pageObjects.header.waitUntilLoadingHasFinished();
+  };
+
+  /**
+   * Finds a table and returns the data in a nested array with row 0 is the headers if they exist.
+   * It uses euiTableCellContent to avoid polluting the array data with the euiTableRowCell__mobileHeader data.
+   * @param dataTestSubj
+   * @param element
+   * @returns Promise<string[][]>
+   */
+  const getEndpointEventResolverNodeData = async (dataTestSubj: string, element: string) => {
+    await testSubjects.exists(dataTestSubj);
+    const Elements = await testSubjects.findAll(dataTestSubj);
+    const $ = [];
+    for (const value of Elements) {
+      $.push(await value.getAttribute(element));
+    }
+    return $;
+  };
+
+  /**
+   * Gets a array of not parsed styles and returns the Array of parsed styles.
+   * @returns Promise<string[][]>
+   */
+  const parseStyles = async () => {
+    const tableData = await getEndpointEventResolverNodeData('resolver:node', 'style');
+    const styles: DataStyle[] = [];
+    for (let i = 1; i < tableData.length; i++) {
+      const eachStyle = parseStyle(tableData[i]);
+      styles.push({
+        top: eachStyle.top ?? '',
+        height: eachStyle.height ?? '',
+        left: eachStyle.left ?? '',
+        width: eachStyle.width ?? '',
+      });
+    }
+    return styles;
+  };
+  /**
+   * Deletes DataStreams from Index Management.
+   */
+  const deleteDataStreams = async () => {
+    await deleteEventsStream(getService);
+    await deleteAlertsStream(getService);
+    await deletePolicyStream(getService);
+    await deleteMetadataStream(getService);
+    await deleteTelemetryStream(getService);
+  };
+
+  /**
+   * execute Query And Open Resolver
+   */
+  const executeQueryAndOpenResolver = async (query: string) => {
+    await navigateToEventsPanel();
+    await queryBar.setQuery(query);
+    await queryBar.submitQuery();
+    await testSubjects.click('full-screen');
+    await testSubjects.click('investigate-in-resolver-button');
+  };
+
+  return {
+    navigateToProcessListInPanel,
+    findNodePills,
+    clickNodeLinkInPanel,
+    findVisibleNodeIDs,
+    clickZoomOut,
+    navigateToEventsPanel,
+    navigateToSecurityHostsPage,
+    parseStyles,
+    deleteDataStreams,
+    executeQueryAndOpenResolver,
   };
 }
