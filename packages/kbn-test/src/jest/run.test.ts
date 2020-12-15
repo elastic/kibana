82225db0a3b7f105637c660a26c555b76ac44221
Tyler Smalley <tyler.smalley@elastic.co>
2020-12-15 09:11:51 -0800

    test:jest improvements to better support our monorepo (#84848) (#85880)
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>
    # Conflicts:
    #	docs/developer/contributing/development-functional-tests.asciidoc
    #	test/scripts/jenkins_unit.sh
    #	test/scripts/jenkins_xpack.sh
    #	x-pack/README.md
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- packages/kbn-test/src/jest/run.test.ts
+++ packages/kbn-test/src/jest/run.test.ts
@@ -17,6 +17,22 @@
  * under the License.
  */
 
-process.argv.push('--runInBand');
+import { commonBasePath } from './run';
 
-require('@kbn/test').runJest('jest.config.integration.js');
+describe('commonBasePath', () => {
+  it('returns a common path', () => {
+    expect(commonBasePath(['foo/bar/baz', 'foo/bar/quux', 'foo/bar'])).toBe('foo/bar');
+  });
+
+  it('handles an empty array', () => {
+    expect(commonBasePath([])).toBe('');
+  });
+
+  it('handles no common path', () => {
+    expect(commonBasePath(['foo', 'bar'])).toBe('');
+  });
+
+  it('matches full paths', () => {
+    expect(commonBasePath(['foo/bar', 'foo/bar_baz'])).toBe('foo');
+  });
+});
