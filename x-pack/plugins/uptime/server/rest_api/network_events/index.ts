604aca4303dfa1ff723048b0f01f56ad8c8c1edd
Kerry Gallagher <k.gallagher.05@gmail.com>
2020-12-15 10:19:22 +0000

    [7.x] [Synthetics] Waterfall view (#84821) (#85875)
    * [Synthetics] Waterfall view (#84821)
    
    * Add a new synthetics step detail page for displaying waterfall data
    
    * Amend test for 7.x index

--- x-pack/plugins/uptime/server/rest_api/network_events/index.ts
+++ x-pack/plugins/uptime/server/rest_api/network_events/index.ts
@@ -4,7 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { MonitorPage } from './monitor';
-export { StepDetailPage } from './step_detail_page';
-export { SettingsPage } from './settings';
-export { NotFoundPage } from './not_found';
+export { createNetworkEventsRoute } from './get_network_events';
