29bf58741ca4b223f5724bc248b6a94406706cdd
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-12-15 17:34:22 +0100

    [APM] Fix broken link to ML when time range is not set

--- x-pack/plugins/apm/public/components/shared/Links/MachineLearningLinks/useTimeSeriesExplorerHref.ts
+++ x-pack/plugins/apm/public/components/shared/Links/MachineLearningLinks/useTimeSeriesExplorerHref.ts
@@ -4,9 +4,14 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { UI_SETTINGS } from '../../../../../../../../src/plugins/data/common';
 import { useApmPluginContext } from '../../../../context/apm_plugin/use_apm_plugin_context';
 import { useMlHref } from '../../../../../../ml/public';
 import { useUrlParams } from '../../../../context/url_params_context/use_url_params';
+import {
+  TimePickerRefreshInterval,
+  TimePickerTimeDefaults,
+} from '../../DatePicker/typings';
 
 export function useTimeSeriesExplorerHref({
   jobId,
@@ -25,19 +30,26 @@ export function useTimeSeriesExplorerHref({
   const { urlParams } = useUrlParams();
   const { rangeFrom, rangeTo, refreshInterval, refreshPaused } = urlParams;
 
-  const timeRange =
-    rangeFrom !== undefined && rangeTo !== undefined
-      ? { from: rangeFrom, to: rangeTo }
-      : undefined;
+  const timePickerTimeDefaults = core.uiSettings.get<TimePickerTimeDefaults>(
+    UI_SETTINGS.TIMEPICKER_TIME_DEFAULTS
+  );
+
+  const timePickerRefreshIntervalDefaults = core.uiSettings.get<TimePickerRefreshInterval>(
+    UI_SETTINGS.TIMEPICKER_REFRESH_INTERVAL_DEFAULTS
+  );
+
   const mlAnomalyDetectionHref = useMlHref(ml, core.http.basePath.get(), {
     page: 'timeseriesexplorer',
     pageState: {
       jobIds: [jobId],
-      timeRange,
-      refreshInterval:
-        refreshPaused !== undefined && refreshInterval !== undefined
-          ? { pause: refreshPaused, value: refreshInterval }
-          : undefined,
+      timeRange: {
+        from: rangeFrom ?? timePickerTimeDefaults.from,
+        to: rangeTo ?? timePickerTimeDefaults.to,
+      },
+      refreshInterval: {
+        pause: refreshPaused ?? timePickerRefreshIntervalDefaults.pause,
+        value: refreshInterval ?? timePickerRefreshIntervalDefaults.value,
+      },
       ...(serviceName && transactionType
         ? {
             entities: {
