0b9a3b28b08636323dafa9602b9d93448dba820f
Dominique Clarke <dominique.clarke@elastic.co>
2020-12-17 09:18:30 -0500

    add Uptime PageHeader tests to test for the presences of tabs or header

--- x-pack/plugins/uptime/public/components/common/header/__tests__/page_header.test.tsx
+++ x-pack/plugins/uptime/public/components/common/header/__tests__/page_header.test.tsx
@@ -6,9 +6,32 @@
 
 import React from 'react';
 import { PageHeader } from '../page_header';
+import { createMemoryHistory } from 'history';
 import { renderWithRouter, MountWithReduxProvider } from '../../../../lib';
+import { AppState, store } from '../../../../state';
 
 describe('PageHeader', () => {
+  const monitorName = 'sample monitor';
+  const initialState = store.getState();
+  const state: AppState = {
+    ...initialState,
+    monitorStatus: {
+      loading: false,
+      status: {
+        docId: '',
+        timestamp: '',
+        monitor: {
+          duration: { us: 0 },
+          id: '',
+          status: '',
+          type: '',
+          ...initialState?.monitorStatus?.status,
+          name: monitorName,
+        },
+      },
+    },
+  };
+
   it('shallow renders with the date picker', () => {
     const component = renderWithRouter(
       <MountWithReduxProvider>
@@ -34,5 +57,47 @@ describe('PageHeader', () => {
       </MountWithReduxProvider>
     );
     expect(component).toMatchSnapshot('page_header_with_extra_links');
+  });
+
+  it('renders null when on a step detail page', () => {
+    const history = createMemoryHistory();
+    // navigate to step page
+    history.push('/journey/1/step/1');
+    const component = renderWithRouter(
+      <MountWithReduxProvider store={state}>
+        <PageHeader />
+      </MountWithReduxProvider>,
+      history
+    );
+    expect(component.html()).toBe(null);
+  });
+
+  it('renders monitor header without tabs when on a monitor page', () => {
+    const history = createMemoryHistory();
+    // navigate to monitor page
+    history.push('/monitor/1');
+    const component = renderWithRouter(
+      <MountWithReduxProvider store={state}>
+        <PageHeader />
+      </MountWithReduxProvider>,
+      history
+    );
+    expect(component.find('h1').text()).toBe(monitorName);
+    expect(component.find('[data-test-subj="uptimeSettingsToOverviewLink"]').length).toBe(0);
+    expect(component.find('[data-test-subj="uptimeCertificatesLink"]').length).toBe(0);
+    expect(component.find('[data-test-subj="settings-page-link"]').length).toBe(0);
+  });
+
+  it('renders tabs when not on a monitor or step detail page', () => {
+    const history = createMemoryHistory();
+    const component = renderWithRouter(
+      <MountWithReduxProvider store={state}>
+        <PageHeader />
+      </MountWithReduxProvider>,
+      history
+    );
+    expect(component.find('[data-test-subj="uptimeSettingsToOverviewLink"]').length).toBe(1);
+    expect(component.find('[data-test-subj="uptimeCertificatesLink"]').length).toBe(1);
+    expect(component.find('[data-test-subj="settings-page-link"]').length).toBe(1);
   });
 });
