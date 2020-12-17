4efb8438705ddb681ef4687f70eb3bebc75dfa2f
lcawl <lcawley@elastic.co>
2020-12-17 10:17:54 -0800

    Renames file and updates linktype

---
+++ x-pack/plugins/ml/public/application/components/help_menu/help_menu.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { FC, useEffect } from 'react';
+import { i18n } from '@kbn/i18n';
+import { useMlKibana } from '../../contexts/kibana';
+
+interface HelpMenuProps {
+  docLink: string;
+}
+
+// Component for adding a documentation link to the help menu
+export const HelpMenu: FC<HelpMenuProps> = React.memo(({ docLink }) => {
+  const { chrome } = useMlKibana().services;
+
+  useEffect(() => {
+    chrome.setHelpExtension({
+      appName: i18n.translate('xpack.ml.chrome.help.appName', {
+        defaultMessage: 'Machine Learning',
+      }),
+      links: [
+        {
+          content: i18n.translate('xpack.ml.chrome.helpMenu.documentation', {
+            defaultMessage: 'Documentation',
+          }),
+          href: docLink,
+          iconType: 'documents',
+          linkType: 'documentation',
+          target: '_blank',
+          rel: 'noopener',
+        },
+      ],
+    });
+  }, []);
+
+  return null;
+});
+
+HelpMenu.displayName = 'HelpMenu';
--- x-pack/plugins/ml/public/application/components/help_menu/index.tsx
+++ x-pack/plugins/ml/public/application/components/help_menu/index.tsx
@@ -4,38 +4,4 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FC, useEffect } from 'react';
-import { i18n } from '@kbn/i18n';
-import { useMlKibana } from '../../contexts/kibana';
-
-interface HelpMenuProps {
-  docLink: string;
-}
-
-export const HelpMenu: FC<HelpMenuProps> = React.memo(({ docLink }) => {
-  const { chrome } = useMlKibana().services;
-
-  useEffect(() => {
-    chrome.setHelpExtension({
-      appName: i18n.translate('xpack.ml.chrome.help.appName', {
-        defaultMessage: 'Machine Learning',
-      }),
-      links: [
-        {
-          content: i18n.translate('xpack.ml.chrome.helpMenu.documentation', {
-            defaultMessage: 'Documentation',
-          }),
-          href: docLink,
-          iconType: 'documents',
-          linkType: 'custom',
-          target: '_blank',
-          rel: 'noopener',
-        },
-      ],
-    });
-  }, []);
-
-  return null;
-});
-
-HelpMenu.displayName = 'HelpMenu';
+export { HelpMenu } from './help_menu';
