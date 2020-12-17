5d7295785de72ff6ee6c40862aac147ad07822c4
Liza Katz <lizka.k@gmail.com>
2020-12-17 12:17:02 +0200

    [Search] Use filter to bulk find (#85551)
    * Use filter to bulk find
    
    * Update x-pack/plugins/data_enhanced/server/search/session/session_service.ts
    
    Co-authored-by: Lukas Olson <olson.lukas@gmail.com>
    
    * Dashboard in space test
    
    * Add warning on update failure
    
    * fix merge
    
    * Added functional test for sessions in space
    
    * snapshot
    
    * test cleanup
    
    * Update src/plugins/data/common/es_query/kuery/node_types/node_builder.ts
    
    Co-authored-by: Lukas Olson <olson.lukas@gmail.com>
    
    * Revert "Update src/plugins/data/common/es_query/kuery/node_types/node_builder.ts"
    
    This reverts commit 4b7e781fe67d274b04a94f62c50a8c0570fb05e5.
    
    Co-authored-by: Lukas Olson <olson.lukas@gmail.com>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/data/common/es_query/kuery/node_types/node_builder.ts
+++ src/plugins/data/common/es_query/kuery/node_types/node_builder.ts
@@ -17,8 +17,22 @@
  * under the License.
  */
 
-export { KQLSyntaxError } from './kuery_syntax_error';
-export { nodeTypes, nodeBuilder } from './node_types';
-export * from './ast';
+import { KueryNode, nodeTypes } from '../types';
 
-export * from './types';
+export const nodeBuilder = {
+  is: (fieldName: string, value: string | KueryNode) => {
+    return nodeTypes.function.buildNodeWithArgumentNodes('is', [
+      nodeTypes.literal.buildNode(fieldName),
+      typeof value === 'string' ? nodeTypes.literal.buildNode(value) : value,
+      nodeTypes.literal.buildNode(false),
+    ]);
+  },
+  or: ([first, ...args]: KueryNode[]): KueryNode => {
+    return args.length ? nodeTypes.function.buildNode('or', [first, nodeBuilder.or(args)]) : first;
+  },
+  and: ([first, ...args]: KueryNode[]): KueryNode => {
+    return args.length
+      ? nodeTypes.function.buildNode('and', [first, nodeBuilder.and(args)])
+      : first;
+  },
+};
