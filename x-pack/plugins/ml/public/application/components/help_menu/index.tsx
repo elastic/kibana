d07a88b60927021a21b8f06e9a1696b378c437d3
lcawl <lcawley@elastic.co>
2020-12-16 16:14:09 -0800

    Adds missing React FC import

--- x-pack/plugins/ml/public/application/components/help_menu/index.tsx
+++ x-pack/plugins/ml/public/application/components/help_menu/index.tsx
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { useEffect } from 'react';
+import React, { FC, useEffect } from 'react';
 import { i18n } from '@kbn/i18n';
 import { useMlKibana } from '../../contexts/kibana';
 
