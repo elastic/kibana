5ece8cbff5ff77f0737627811c9fa97dfb7095e6
lcawl <lcawley@elastic.co>
2020-12-17 16:27:44 -0800

    Fix declarations in advanced_step_form.tsx

--- x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_creation/components/advanced_step/advanced_step_form.tsx
+++ x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_creation/components/advanced_step/advanced_step_form.tsx
@@ -50,14 +50,14 @@ const numClassesTypeMessage = (
   />
 );
 
-function getZeroClassesMessage(elasaticUrl: string, version: string) {
+function getZeroClassesMessage(elasticUrl: string) {
   return (
     <FormattedMessage
       id="xpack.ml.dataframe.analytics.create.zeroClassesMessage"
       defaultMessage="To evaluate the {wikiLink}, select all classes or a value greater than the total number of categories."
       values={{
         wikiLink: (
-          <EuiLink href={docLink} target="_blank" external>
+          <EuiLink href={elasticUrl} target="_blank" external>
             {i18n.translate('xpack.ml.dataframe.analytics.create.aucRocLabel', {
               defaultMessage: 'AUC ROC',
             })}
@@ -418,9 +418,7 @@ export const AdvancedStepForm: FC<CreateAnalyticsStepProps> = ({
               helpText={getTopClassesHelpText(selectedNumTopClasses)}
               isInvalid={selectedNumTopClasses === 0 || selectedNumTopClassesIsInvalid}
               error={[
-                ...(selectedNumTopClasses === 0
-                  ? [getZeroClassesMessage(ELASTIC_WEBSITE_URL, DOC_LINK_VERSION)]
-                  : []),
+                ...(selectedNumTopClasses === 0 ? [getZeroClassesMessage(docLink)] : []),
                 ...(selectedNumTopClassesIsInvalid ? [numClassesTypeMessage] : []),
               ]}
             >
