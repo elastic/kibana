a73da057ba37bb14ced3aba14281cfff2621272f
Paul Tavares <paul.tavares@elastic.co>
2020-12-22 09:26:28 -0500

    refactor integration details Policies list to use LinkedAgentCount component

--- x-pack/plugins/fleet/public/applications/fleet/components/linked_agent_count.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/components/linked_agent_count.tsx
@@ -5,26 +5,27 @@
  */
 
 import React, { memo } from 'react';
-import { EuiLink } from '@elastic/eui';
+import { EuiLink, EuiLinkAnchorProps } from '@elastic/eui';
 import { useLink } from '../hooks';
 import { AGENT_SAVED_OBJECT_TYPE } from '../constants';
 
 /**
  * Displays the provided `count` number as a link to the Agents list if it is greater than zero
  */
-export const LinkedAgentCount = memo<{ count: number; agentPolicyId: string }>(
-  ({ count, agentPolicyId }) => {
-    const { getHref } = useLink();
-    return count > 0 ? (
-      <EuiLink
-        href={getHref('fleet_agent_list', {
-          kuery: `${AGENT_SAVED_OBJECT_TYPE}.policy_id : ${agentPolicyId}`,
-        })}
-      >
-        {count}
-      </EuiLink>
-    ) : (
-      <>count</>
-    );
-  }
-);
+export const LinkedAgentCount = memo<
+  Omit<EuiLinkAnchorProps, 'href'> & { count: number; agentPolicyId: string }
+>(({ count, agentPolicyId, ...otherEuiLinkProps }) => {
+  const { getHref } = useLink();
+  return count > 0 ? (
+    <EuiLink
+      {...otherEuiLinkProps}
+      href={getHref('fleet_agent_list', {
+        kuery: `${AGENT_SAVED_OBJECT_TYPE}.policy_id : ${agentPolicyId}`,
+      })}
+    >
+      {count}
+    </EuiLink>
+  ) : (
+    <>{count}</>
+  );
+});
