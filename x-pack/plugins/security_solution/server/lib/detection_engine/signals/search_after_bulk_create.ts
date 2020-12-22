9b21ace60125b8e7db84209d425f15dc5499dbda
Devin Hurley <devin.hurley@elastic.co>
2020-12-22 02:17:22 -0500

    perf improvement and fix bug where sortIds were being mixed between search after calls

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/search_after_bulk_create.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/search_after_bulk_create.ts
@@ -5,20 +5,20 @@
  */
 /* eslint-disable complexity */
 
-import { isEmpty } from 'lodash';
 import { singleSearchAfter } from './single_search_after';
 import { singleBulkCreate } from './single_bulk_create';
 import { filterEventsAgainstList } from './filters/filter_events_against_list';
 import { sendAlertTelemetryEvents } from './send_telemetry_events';
 import {
   createSearchAfterReturnType,
+  createSearchResultReturnType,
   createSearchAfterReturnTypeFromResponse,
   createTotalHitsFromSearchResult,
   getSignalTimeTuples,
   mergeReturns,
+  mergeSearchResults,
 } from './utils';
 import { SearchAfterAndBulkCreateParams, SearchAfterAndBulkCreateReturnType } from './types';
-import { TotalValue } from '../../../../common/detection_engine/types';
 
 // search_after through documents and re-index using bulk endpoint.
 export const searchAfterAndBulkCreate = async ({
@@ -82,10 +82,11 @@ export const searchAfterAndBulkCreate = async ({
     signalsCreatedCount = 0;
     while (signalsCreatedCount < tuple.maxSignals) {
       try {
+        let mergedSearchResults = createSearchResultReturnType();
         logger.debug(buildRuleMessage(`sortIds: ${sortId}`));
 
         // perform search_after with optionally undefined sortId
-        const { searchResult, searchDuration, searchErrors } = await singleSearchAfter({
+        const singleSearchAfterPromise = singleSearchAfter({
           buildRuleMessage,
           searchAfterSortId: sortId,
           index: inputIndexPattern,
@@ -98,25 +99,10 @@ export const searchAfterAndBulkCreate = async ({
           timestampOverride: ruleParams.timestampOverride,
           excludeDocsWithTimestampOverride: false,
         });
-        toReturn = mergeReturns([
-          toReturn,
-          createSearchAfterReturnTypeFromResponse({
-            searchResult,
-            timestampOverride: ruleParams.timestampOverride,
-          }),
-          createSearchAfterReturnType({
-            searchAfterTimes: [searchDuration],
-            errors: searchErrors,
-          }),
-        ]);
 
         // if there is a timestampOverride param we always want to do a secondary search against @timestamp
         if (ruleParams.timestampOverride != null) {
-          const {
-            searchResult: searchResultB,
-            searchDuration: searchDurationB,
-            searchErrors: searchErrorsB,
-          } = await singleSearchAfter({
+          const singleSearchAfterDefaultTimestamp = singleSearchAfter({
             buildRuleMessage,
             searchAfterSortId: backupSortId,
             index: inputIndexPattern,
@@ -129,41 +115,28 @@ export const searchAfterAndBulkCreate = async ({
             timestampOverride: ruleParams.timestampOverride,
             excludeDocsWithTimestampOverride: true,
           });
+          const {
+            searchResult: searchResultB,
+            searchDuration: searchDurationB,
+            searchErrors: searchErrorsB,
+          } = await singleSearchAfterDefaultTimestamp;
 
           // call this function setSortIdOrExit()
           const lastSortId = searchResultB?.hits?.hits[searchResultB.hits.hits.length - 1]?.sort;
           if (lastSortId != null && lastSortId.length !== 0) {
             backupSortId = lastSortId[0];
-          } else if (lastSortId == null && isEmpty(searchResult.hits.hits)) {
-            // if no sort id on backup search and the initial search result was also empty; break
-            logger.debug(buildRuleMessage('backupSortIds was empty on searchResultB'));
-            break;
-          }
-          searchResult.hits.hits.push(...searchResultB.hits.hits);
-
-          // we have to update the hits.total to include these newfound values.
-          // sometimes hits.total is a value (number) or sometimes its an object
-          const totalTypeGuard = (num: number | TotalValue): num is number =>
-            typeof num === 'number';
-          if (totalTypeGuard(searchResult.hits.total)) {
-            if (totalTypeGuard(searchResultB.hits.total)) {
-              searchResult.hits.total += searchResultB.hits.total;
-            } else {
-              searchResult.hits.total += searchResultB.hits.total.value;
-            }
           } else {
-            if (totalTypeGuard(searchResultB.hits.total)) {
-              searchResult.hits.total.value += searchResultB.hits.total;
-            } else {
-              searchResult.hits.total.value += searchResultB.hits.total.value;
-            }
+            // if no sort id on backup search and the initial search result was also empty
+            logger.debug(buildRuleMessage('backupSortIds was empty on searchResultB'));
           }
+
+          mergedSearchResults = mergeSearchResults([mergedSearchResults, searchResultB]);
 
           // merge the search result from the secondary search with the first
           toReturn = mergeReturns([
             toReturn,
             createSearchAfterReturnTypeFromResponse({
-              searchResult: searchResultB,
+              searchResult: mergedSearchResults,
               timestampOverride: undefined,
             }),
             createSearchAfterReturnType({
@@ -173,11 +146,34 @@ export const searchAfterAndBulkCreate = async ({
           ]);
         }
 
+        const { searchResult, searchDuration, searchErrors } = await singleSearchAfterPromise;
+        mergedSearchResults = mergeSearchResults([mergedSearchResults, searchResult]);
+        toReturn = mergeReturns([
+          toReturn,
+          createSearchAfterReturnTypeFromResponse({
+            searchResult: mergedSearchResults,
+            timestampOverride: ruleParams.timestampOverride,
+          }),
+          createSearchAfterReturnType({
+            searchAfterTimes: [searchDuration],
+            errors: searchErrors,
+          }),
+        ]);
+
+        // we are guaranteed to have searchResult hits at this point
+        // because we check before if the totalHits or
+        // searchResult.hits.hits.length is 0
+        // call this function setSortIdOrExit()
+        const lastSortId = searchResult.hits.hits[searchResult.hits.hits.length - 1]?.sort;
+        if (lastSortId != null && lastSortId.length !== 0) {
+          sortId = lastSortId[0];
+        }
+
         // determine if there are any candidate signals to be processed
-        const totalHits = createTotalHitsFromSearchResult({ searchResult });
+        const totalHits = createTotalHitsFromSearchResult({ searchResult: mergedSearchResults });
         logger.debug(buildRuleMessage(`totalHits: ${totalHits}`));
         logger.debug(
-          buildRuleMessage(`searchResult.hit.hits.length: ${searchResult.hits.hits.length}`)
+          buildRuleMessage(`searchResult.hit.hits.length: ${mergedSearchResults.hits.hits.length}`)
         );
 
         // search results yielded zero hits so exit
@@ -188,7 +184,7 @@ export const searchAfterAndBulkCreate = async ({
         // e.g. totalHits was 156, index 50 of 100 results, do another search-after
         // this time with a new sortId, index 22 of the remaining 56, get another sortId
         // search with that sortId, total is still 156 but the hits.hits array is empty.
-        if (totalHits === 0 || searchResult.hits.hits.length === 0) {
+        if (totalHits === 0 || mergedSearchResults.hits.hits.length === 0) {
           logger.debug(
             buildRuleMessage(
               `${
@@ -206,7 +202,7 @@ export const searchAfterAndBulkCreate = async ({
           listClient,
           exceptionsList,
           logger,
-          eventSearchResult: searchResult,
+          eventSearchResult: mergedSearchResults,
           buildRuleMessage,
         });
 
@@ -274,14 +270,7 @@ export const searchAfterAndBulkCreate = async ({
           );
         }
 
-        // we are guaranteed to have searchResult hits at this point
-        // because we check before if the totalHits or
-        // searchResult.hits.hits.length is 0
-        // call this function setSortIdOrExit()
-        const lastSortId = searchResult.hits.hits[searchResult.hits.hits.length - 1].sort;
-        if (lastSortId != null && lastSortId.length !== 0) {
-          sortId = lastSortId[0];
-        } else {
+        if (sortId == null && backupSortId == null) {
           logger.debug(buildRuleMessage('sortIds was empty on searchResult'));
           break;
         }
