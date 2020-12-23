c2d5a8ea5fab26fa6815e840b248c4f2c787855b
Joe Reuter <johannes.reuter@elastic.co>
2020-12-23 17:15:42 +0100

    remove phase II of drag/drop

--- x-pack/plugins/lens/public/indexpattern_datasource/dimension_panel/droppable.ts
+++ x-pack/plugins/lens/public/indexpattern_datasource/dimension_panel/droppable.ts
@@ -11,7 +11,7 @@ import {
   DraggedOperation,
 } from '../../types';
 import { IndexPatternColumn } from '../indexpattern';
-import { insertOrReplaceColumn, deleteColumn } from '../operations';
+import { insertOrReplaceColumn } from '../operations';
 import { mergeLayer } from '../state_helpers';
 import { hasField, isDraggedField } from '../utils';
 import { IndexPatternPrivateState, IndexPatternField } from '../types';
@@ -160,61 +160,6 @@ const onMoveDropToCompatibleGroup = ({
   return { deleted: droppedItem.columnId };
 };
 
-const onMoveDropToNonCompatibleGroup = ({
-  columnId,
-  setState,
-  state,
-  layerId,
-  droppedItem,
-  operationSupportMatrix,
-}: DropHandlerProps) => {
-  // move to suggest
-  const layer = state.layers[layerId];
-  const op = { ...layer.columns[droppedItem.columnId] };
-  const field =
-    hasField(op) && state.indexPatterns[layer.indexPatternId].getFieldByName(op.sourceField);
-  if (!field) {
-    return false;
-  }
-
-  const operationsForNewField = operationSupportMatrix.operationByField[field.name];
-
-  if (!operationsForNewField || operationsForNewField.size === 0) {
-    return false;
-  }
-
-  const currentIndexPattern = state.indexPatterns[layer.indexPatternId];
-
-  const newLayer = insertOrReplaceColumn({
-    layer: deleteColumn({
-      layer,
-      columnId: droppedItem.columnId,
-      indexPattern: currentIndexPattern,
-    }),
-    columnId,
-    indexPattern: currentIndexPattern,
-    op: operationsForNewField.values().next().value,
-    field,
-  });
-
-  trackUiEvent('drop_onto_dimension');
-  const hasData = Object.values(state.layers).some(({ columns }) => columns.length);
-  trackUiEvent(hasData ? 'drop_non_empty' : 'drop_empty');
-  setState(
-    mergeLayer({
-      state,
-      layerId,
-      newLayer: {
-        ...newLayer,
-      },
-    })
-  );
-
-  return { deleted: droppedItem.columnId }; // -> to do for removing the old one
-
-  return true; // duplicating
-};
-
 const onFieldDrop = ({
   columnId,
   setState,
@@ -332,15 +277,8 @@ export function onDrop(props: DatasourceDimensionDropHandlerProps<IndexPatternPr
         droppedItem,
         operationSupportMatrix,
       });
-    } else {
-      return onMoveDropToNonCompatibleGroup({
-        columnId,
-        setState,
-        state,
-        layerId,
-        droppedItem,
-        operationSupportMatrix,
-      });
     }
   }
+
+  return false;
 }
