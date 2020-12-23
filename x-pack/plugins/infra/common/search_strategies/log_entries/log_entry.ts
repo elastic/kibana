eb8ad64a24606c9ebe9a3e49712bed496784fd62
Felix Stürmer <stuermer@weltenwort.de>
2020-12-23 13:12:49 +0100

    Move log entry types out of http directory

--- x-pack/plugins/infra/common/search_strategies/log_entries/log_entry.ts
+++ x-pack/plugins/infra/common/search_strategies/log_entries/log_entry.ts
@@ -5,8 +5,7 @@
  */
 
 import * as rt from 'io-ts';
-import { logEntryCursorRT } from '../../log_entry';
-import { jsonArrayRT } from '../../typed_json';
+import { logEntryCursorRT, logEntryFieldRT } from '../../log_entry';
 import { searchStrategyErrorRT } from '../common/errors';
 
 export const LOG_ENTRY_SEARCH_STRATEGY = 'infra-log-entry';
@@ -18,18 +17,11 @@ export const logEntrySearchRequestParamsRT = rt.type({
 
 export type LogEntrySearchRequestParams = rt.TypeOf<typeof logEntrySearchRequestParamsRT>;
 
-const logEntryFieldRT = rt.type({
-  field: rt.string,
-  value: jsonArrayRT,
-});
-
-export type LogEntryField = rt.TypeOf<typeof logEntryFieldRT>;
-
 export const logEntryRT = rt.type({
   id: rt.string,
   index: rt.string,
   fields: rt.array(logEntryFieldRT),
-  key: logEntryCursorRT,
+  cursor: logEntryCursorRT,
 });
 
 export type LogEntry = rt.TypeOf<typeof logEntryRT>;
