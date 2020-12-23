dc926566a992588a517f25f6d59b4e5c2b0a5ef0
Christiane Heiligers <christiane.heiligers@elastic.co>
2020-12-23 11:26:37 -0700

    Fix callCluster type in monitoring

--- x-pack/plugins/monitoring/server/telemetry_collection/get_kibana_stats.ts
+++ x-pack/plugins/monitoring/server/telemetry_collection/get_kibana_stats.ts
@@ -7,7 +7,7 @@
 import moment from 'moment';
 import { isEmpty } from 'lodash';
 import { SearchResponse } from 'elasticsearch';
-import { StatsCollectionConfig } from 'src/plugins/telemetry_collection_manager/server';
+import { LegacyAPICaller } from 'kibana/server';
 import { KIBANA_SYSTEM_ID, TELEMETRY_COLLECTION_INTERVAL } from '../../common/constants';
 import {
   fetchHighLevelStats,
@@ -182,7 +182,7 @@ export function ensureTimeSpan(
  * specialized usage data that comes with kibana stats (kibana_stats.usage).
  */
 export async function getKibanaStats(
-  callCluster: StatsCollectionConfig['callCluster'],
+  callCluster: LegacyAPICaller,
   clusterUuids: string[],
   start: string,
   end: string,
