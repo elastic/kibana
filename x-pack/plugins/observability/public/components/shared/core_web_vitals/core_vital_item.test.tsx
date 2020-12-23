9e216cd5e33194f22604ade70aad306f0f792d0f
Dominique Clarke <dominique.clarke@elastic.co>
2020-12-23 10:06:59 -0500

    update CoreVitalItem test to use testing-library/react test_helper

--- x-pack/plugins/observability/public/components/shared/core_web_vitals/core_vital_item.test.tsx
+++ x-pack/plugins/observability/public/components/shared/core_web_vitals/core_vital_item.test.tsx
@@ -3,36 +3,10 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { render } from '@testing-library/react';
 import React from 'react';
-import { Observable } from 'rxjs';
-import { IntlProvider } from 'react-intl';
-
-import { CoreStart } from 'src/core/public';
-import { createKibanaReactContext } from 'src/plugins/kibana_react/public';
-
+import { render } from '../../../utils/test_helper';
 import { CoreVitalItem } from './core_vital_item';
-
 import { NO_DATA } from './translations';
-
-/* HOC to wrap components in KibanaReact context as well as in18 context.
- * is not opinionated and can be used with enzyme, react testing library or
- * react test renderer */
-function withKibanaIntl(WrappedComponent: React.ComponentType<any>) {
-  const KibanaReactContext = createKibanaReactContext(({
-    uiSettings: { get: () => {}, get$: () => new Observable() },
-  } as unknown) as Partial<CoreStart>);
-
-  return (props: any) => (
-    <IntlProvider locale="en">
-      <KibanaReactContext.Provider>
-        <WrappedComponent {...props} />
-      </KibanaReactContext.Provider>
-    </IntlProvider>
-  );
-}
-
-const ConnectedCoreVitalItem = withKibanaIntl(CoreVitalItem);
 
 describe('CoreVitalItem', () => {
   const value = '0.005';
@@ -43,7 +17,7 @@ describe('CoreVitalItem', () => {
 
   it('renders if value is truthy', () => {
     const { getByText } = render(
-      <ConnectedCoreVitalItem
+      <CoreVitalItem
         title={title}
         value={value}
         ranks={[85, 10, 5]}
@@ -62,7 +36,7 @@ describe('CoreVitalItem', () => {
 
   it('renders loading state when loading is truthy', () => {
     const { queryByText, getByText } = render(
-      <ConnectedCoreVitalItem
+      <CoreVitalItem
         title={title}
         value={value}
         ranks={[85, 10, 5]}
@@ -78,7 +52,7 @@ describe('CoreVitalItem', () => {
 
   it('renders no data UI if value is falsey and loading is falsey', () => {
     const { getByText } = render(
-      <ConnectedCoreVitalItem
+      <CoreVitalItem
         title={title}
         value={null}
         ranks={[85, 10, 5]}
