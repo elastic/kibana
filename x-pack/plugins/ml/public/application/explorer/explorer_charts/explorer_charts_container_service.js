2ceb34e2fd35d12b52a4c6c99ee63a315848b357
Dima Arnautov <dmitrii.arnautov@elastic.co>
2020-12-24 16:23:35 +0100

    [ML] Fix charts grid on the Anomaly Explorer page (#86904) (#86937)
    * [ML] fix AR charts grid items width
    
    * [ML] update test snapshot
    # Conflicts:
    #	x-pack/plugins/ml/public/application/explorer/explorer_charts/explorer_charts_container_service.js

--- x-pack/plugins/ml/public/application/explorer/explorer_charts/explorer_charts_container_service.js
+++ x-pack/plugins/ml/public/application/explorer/explorer_charts/explorer_charts_container_service.js
@@ -28,6 +28,7 @@ import { mlJobService } from '../../services/job_service';
 import { explorerService } from '../explorer_dashboard_service';
 
 import { CHART_TYPE } from '../explorer_constants';
+import { SWIM_LANE_LABEL_WIDTH } from '../swimlane_container';
 
 export function getDefaultChartsData() {
   return {
@@ -57,15 +58,14 @@ export const anomalyDataChange = function (
 ) {
   const data = getDefaultChartsData();
 
+  const containerWith = chartsContainerWidth + SWIM_LANE_LABEL_WIDTH;
+
   const filteredRecords = anomalyRecords.filter((record) => {
     return Number(record.record_score) >= severity;
   });
   const allSeriesRecords = processRecordsForDisplay(filteredRecords);
   // Calculate the number of charts per row, depending on the width available, to a max of 4.
-  let chartsPerRow = Math.min(
-    Math.max(Math.floor(chartsContainerWidth / 550), 1),
-    MAX_CHARTS_PER_ROW
-  );
+  let chartsPerRow = Math.min(Math.max(Math.floor(containerWith / 550), 1), MAX_CHARTS_PER_ROW);
   if (allSeriesRecords.length === 1) {
     chartsPerRow = 1;
   }
@@ -81,7 +81,7 @@ export const anomalyDataChange = function (
 
   // Calculate the time range of the charts, which is a function of the chart width and max job bucket span.
   data.tooManyBuckets = false;
-  const chartWidth = Math.floor(chartsContainerWidth / chartsPerRow);
+  const chartWidth = Math.floor(containerWith / chartsPerRow);
   const { chartRange, tooManyBuckets } = calculateChartRange(
     seriesConfigs,
     selectedEarliestMs,
