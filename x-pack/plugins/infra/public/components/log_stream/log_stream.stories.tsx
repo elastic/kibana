7dd435c1e10532ce283f9bf7e4575b6fdfa22e75
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2020-12-29 15:12:21 +0100

    WIP: add default log stream story

---
+++ x-pack/plugins/infra/public/components/log_stream/log_stream.stories.tsx
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import { Subject } from 'rxjs';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+import { I18nProvider } from '@kbn/i18n/react';
+
+import { EuiThemeProvider } from '../../../../observability/public';
+import { KibanaContextProvider } from '../../../../../../src/plugins/kibana_react/public';
+
+import { LogStream } from './';
+import { DEFAULT_SOURCE_CONFIGURATION } from '../../test_utils/source_configuration';
+import { ENTRIES, ENTRIES_EMPTY } from '../../test_utils/entries';
+
+const startTimestamp = ENTRIES.data.topCursor.time;
+const endTimestamp = ENTRIES.data.bottomCursor.time;
+
+// Mocks
+const fetch = function (url, params) {
+  switch (url) {
+    case '/api/infra/log_source_configurations/default':
+      return DEFAULT_SOURCE_CONFIGURATION;
+    case '/api/log_entries/entries':
+      const body = JSON.parse(params.body);
+      if (body.after?.time === body.endTimestamp || body.before?.time === body.startTimestamp) {
+        return ENTRIES_EMPTY;
+      } else {
+        return ENTRIES;
+      }
+    default:
+      return {};
+  }
+};
+
+const uiSettings = {
+  get: () => {},
+  get$: (...args) => {
+    return new Subject();
+  },
+};
+
+export default {
+  title: 'infra/LogStream',
+  component: LogStream,
+  decorators: [
+    (story) => (
+      <I18nProvider>
+        <EuiThemeProvider>
+          <KibanaContextProvider services={{ http: { fetch }, uiSettings }}>
+            {story()}
+          </KibanaContextProvider>
+        </EuiThemeProvider>
+      </I18nProvider>
+    ),
+  ],
+} as Meta;
+
+export const Default: Story = () => {
+  return <LogStream height={600} startTimestamp={startTimestamp} endTimestamp={endTimestamp} />;
+};
