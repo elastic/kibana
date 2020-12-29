c96b6e36de88e5e07d683cf1780824e8445e170b
Dominique Clarke <dominique.clarke@elastic.co>
2020-12-29 18:00:26 -0500

    adjust uptime PageHeader tests

--- x-pack/plugins/uptime/public/components/common/header/__tests__/page_header.test.tsx
+++ x-pack/plugins/uptime/public/components/common/header/__tests__/page_header.test.tsx
@@ -5,57 +5,60 @@
  */
 
 import React from 'react';
+import moment from 'moment';
 import { PageHeader } from '../page_header';
+import * as redux from 'react-redux';
+import { Ping } from '../../../../../common/runtime_types';
 import { createMemoryHistory } from 'history';
-import { renderWithRouter, MountWithReduxProvider } from '../../../../lib';
-import { AppState, store } from '../../../../state';
+import { renderWithRouter } from '../../../../lib';
+
+jest.mock('react-redux', () => {
+  const originalModule = jest.requireActual('react-redux');
+
+  return {
+    ...originalModule,
+    useSelector: jest.fn(),
+  };
+});
 
 describe('PageHeader', () => {
   const monitorName = 'sample monitor';
-  const initialState = store.getState();
-  const state: AppState = {
-    ...initialState,
-    monitorStatus: {
-      loading: false,
-      status: {
-        docId: '',
-        timestamp: '',
-        monitor: {
-          duration: { us: 0 },
-          id: '',
-          status: '',
-          type: '',
-          ...initialState?.monitorStatus?.status,
-          name: monitorName,
-        },
+  const defaultMonitorId = 'always-down';
+
+  const defaultMonitorStatus: Ping = {
+    docId: 'few213kl',
+    timestamp: moment(new Date()).subtract(15, 'm').toString(),
+    monitor: {
+      duration: {
+        us: 1234567,
       },
+      id: defaultMonitorId,
+      status: 'up',
+      type: 'http',
+      name: monitorName,
+    },
+    url: {
+      full: 'https://www.elastic.co/',
     },
   };
 
+  beforeEach(() => {
+    jest.spyOn(redux, 'useDispatch').mockReturnValue(jest.fn());
+    jest.spyOn(redux, 'useSelector').mockReturnValue(defaultMonitorStatus);
+  });
+
   it('shallow renders with the date picker', () => {
-    const component = renderWithRouter(
-      <MountWithReduxProvider>
-        <PageHeader />
-      </MountWithReduxProvider>
-    );
+    const component = renderWithRouter(<PageHeader />);
     expect(component).toMatchSnapshot('page_header_with_date_picker');
   });
 
   it('shallow renders without the date picker', () => {
-    const component = renderWithRouter(
-      <MountWithReduxProvider>
-        <PageHeader />
-      </MountWithReduxProvider>
-    );
+    const component = renderWithRouter(<PageHeader />);
     expect(component).toMatchSnapshot('page_header_no_date_picker');
   });
 
   it('shallow renders extra links', () => {
-    const component = renderWithRouter(
-      <MountWithReduxProvider>
-        <PageHeader />
-      </MountWithReduxProvider>
-    );
+    const component = renderWithRouter(<PageHeader />);
     expect(component).toMatchSnapshot('page_header_with_extra_links');
   });
 
@@ -63,12 +66,7 @@ describe('PageHeader', () => {
     const history = createMemoryHistory();
     // navigate to step page
     history.push('/journey/1/step/1');
-    const component = renderWithRouter(
-      <MountWithReduxProvider store={state}>
-        <PageHeader />
-      </MountWithReduxProvider>,
-      history
-    );
+    const component = renderWithRouter(<PageHeader />, history);
     expect(component.html()).toBe(null);
   });
 
@@ -76,24 +74,14 @@ describe('PageHeader', () => {
     const history = createMemoryHistory();
     // navigate to monitor page
     history.push('/monitor/1');
-    const component = renderWithRouter(
-      <MountWithReduxProvider store={state}>
-        <PageHeader />
-      </MountWithReduxProvider>,
-      history
-    );
+    const component = renderWithRouter(<PageHeader />, history);
     expect(component.find('h1').text()).toBe(monitorName);
     expect(component.find('[data-test-subj="uptimeTabs"]').length).toBe(0);
   });
 
   it('renders tabs when not on a monitor or step detail page', () => {
     const history = createMemoryHistory();
-    const component = renderWithRouter(
-      <MountWithReduxProvider store={state}>
-        <PageHeader />
-      </MountWithReduxProvider>,
-      history
-    );
+    const component = renderWithRouter(<PageHeader />, history);
     expect(component.find('[data-test-subj="uptimeTabs"]').length).toBe(1);
   });
 });
