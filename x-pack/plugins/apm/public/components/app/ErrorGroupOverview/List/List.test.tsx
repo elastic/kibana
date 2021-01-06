f5288d4e3e2f171914f7715246176728caf152af
Tiago Costa <tiagoffcc@hotmail.com>
2021-01-06 18:04:57 +0000

    chore(NA): move apm plugin tests out of __tests__ folder

---
+++ x-pack/plugins/apm/public/components/app/ErrorGroupOverview/List/List.test.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { mount } from 'enzyme';
+import React from 'react';
+import { MockApmPluginContextWrapper } from '../../../../context/apm_plugin/mock_apm_plugin_context';
+import { MockUrlParamsContextProvider } from '../../../../context/url_params_context/mock_url_params_context_provider';
+import { mockMoment, toJson } from '../../../../utils/testHelpers';
+import { ErrorGroupList } from './index';
+import props from './__fixtures__/props.json';
+import { MemoryRouter } from 'react-router-dom';
+
+jest.mock('@elastic/eui/lib/services/accessibility/html_id_generator', () => {
+  return {
+    htmlIdGenerator: () => () => `generated-id`,
+  };
+});
+
+describe('ErrorGroupOverview -> List', () => {
+  beforeAll(() => {
+    mockMoment();
+  });
+
+  it('should render empty state', () => {
+    const storeState = {};
+    const wrapper = mount(
+      <MemoryRouter>
+        <MockUrlParamsContextProvider>
+          <ErrorGroupList items={[]} serviceName="opbeans-python" />
+        </MockUrlParamsContextProvider>
+      </MemoryRouter>,
+      storeState
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render with data', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <MockApmPluginContextWrapper>
+          <MockUrlParamsContextProvider>
+            <ErrorGroupList items={props.items} serviceName="opbeans-python" />
+          </MockUrlParamsContextProvider>
+        </MockApmPluginContextWrapper>
+      </MemoryRouter>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
