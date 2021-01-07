1989cd4fc2bff7fb8f76ae31fd6a0e03a4540e78
Timothy Sullivan <tsullivan@elastic.co>
2021-01-06 17:41:57 -0700

    add pdf snapshot testing to downloaded pdfs

--- x-pack/test/functional/apps/dashboard/reporting/screenshots.ts
+++ x-pack/test/functional/apps/dashboard/reporting/screenshots.ts
@@ -35,11 +35,11 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
     after('clean up archives', async () => {
       await esArchiver.unload('reporting/ecommerce');
       await esArchiver.unload('reporting/ecommerce_kibana');
-      await es.deleteByQuery({
-        index: '.reporting-*',
-        refresh: true,
-        body: { query: { match_all: {} } },
-      });
+      // await es.deleteByQuery({
+      //   index: '.reporting-*',
+      //   refresh: true,
+      //   body: { query: { match_all: {} } },
+      // });
     });
 
     describe('Print PDF button', () => {
@@ -74,6 +74,120 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
 
         expect(res.status).to.equal(200);
         expect(res.get('content-type')).to.equal('application/pdf');
+
+        /* Check the value of the PDF data that was generated
+         * PDF files include dynamic meta info such as creation date.
+         * This checks only the first few thousand bytes of the Buffer
+         */
+        const pdfStrings = (res.body as Buffer).toString('utf8', 14); // start on byte 14 to skip non-utf8 data
+        const [header, , contents, , info] = pdfStrings.split('stream'); // ignore everthing from `stream` to `endstream` - the non-utf8 blocks
+
+        // PDF Header
+
+        expectSnapshot(header).toMatchInline(`
+          "
+          13 0 obj
+          <<
+          /Predictor 15
+          /Colors 1
+          /BitsPerComponent 8
+          /Columns 577
+          >>
+          endobj
+          14 0 obj
+          <<
+          /Length 149
+          /Filter /FlateDecode
+          >>
+          "
+        `);
+
+        // PDF Contents
+
+        expectSnapshot(contents).toMatchInline(`
+          "
+          endobj
+          18 0 obj
+          <<
+          /Type /ExtGState
+          /ca 1
+          /CA 1
+          >>
+          endobj
+          17 0 obj
+          <<
+          /Type /Page
+          /Parent 1 0 R
+          /MediaBox [0 0 595.28 841.89]
+          /Contents 15 0 R
+          /Resources 16 0 R
+          >>
+          endobj
+          16 0 obj
+          <<
+          /ProcSet [/PDF /Text /ImageB /ImageC /ImageI]
+          /ExtGState <<
+          /Gs1 18 0 R
+          >>
+          /Font <<
+          /F1 19 0 R
+          /F2 20 0 R
+          >>
+          /XObject <<
+          /I1 5 0 R
+          /I2 6 0 R
+          /I8 12 0 R
+          >>
+          >>
+          endobj
+          15 0 obj
+          <<
+          /Length 565
+          /Filter /FlateDecode
+          >>
+          "
+        `);
+
+        // PDF Info
+
+        // The Info section of text includes a Datestamp which will obviously not match as a snapshot between tests
+        // This does a .replace on the Info text to erase the dynamic date string
+        expectSnapshot(info.replace(/D:\d+Z/, 'D:DATESTAMP')).toMatchInline(`
+          "
+          endobj
+          23 0 obj
+          <<
+          /Type /Page
+          /Parent 1 0 R
+          /MediaBox [0 0 595.28 841.89]
+          /Contents 21 0 R
+          /Resources 22 0 R
+          >>
+          endobj
+          22 0 obj
+          <<
+          /ProcSet [/PDF /Text /ImageB /ImageC /ImageI]
+          /ExtGState <<
+          /Gs1 18 0 R
+          >>
+          /Font <<
+          /F1 19 0 R
+          /F2 20 0 R
+          >>
+          /XObject <<
+          /I3 7 0 R
+          /I4 8 0 R
+          /I8 12 0 R
+          >>
+          >>
+          endobj
+          21 0 obj
+          <<
+          /Length 559
+          /Filter /FlateDecode
+          >>
+          "
+        `);
       });
     });
 
@@ -93,8 +207,8 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       });
     });
 
-    describe('Preserve Layout', () => {
-      it('matches baseline report', async function () {
+    describe('PNG Layout', () => {
+      it('downloads a PNG file', async function () {
         const writeSessionReport = async (name: string, rawPdf: Buffer, reportExt: string) => {
           const sessionDirectory = path.resolve(REPORTS_FOLDER, 'session');
           await mkdirAsync(sessionDirectory, { recursive: true });
@@ -130,6 +244,157 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
         );
 
         expect(percentSimilar).to.be.lessThan(0.1);
+      });
+    });
+
+    describe('Preserve Layout', () => {
+      it('downloads a PDF file', async function () {
+        // Generating and then comparing reports can take longer than the default 60s timeout because the comparePngs
+        // function is taking about 15 seconds per comparison in jenkins.
+        this.timeout(300000);
+        await PageObjects.common.navigateToApp('dashboard');
+        await PageObjects.dashboard.loadSavedDashboard('Ecom Dashboard');
+        await PageObjects.reporting.openPdfReportingPanel();
+        await PageObjects.reporting.clickGenerateReportButton();
+
+        const url = await PageObjects.reporting.getReportURL(60000);
+        const res = await PageObjects.reporting.getResponse(url);
+
+        expect(res.status).to.equal(200);
+        expect(res.get('content-type')).to.equal('application/pdf');
+
+        /* Check the value of the PDF data that was generated
+         * PDF files include dynamic meta info such as creation date.
+         * This checks only the first few thousand bytes of the Buffer
+         */
+        const pdfStrings = (res.body as Buffer).toString('utf8', 14); // start on byte 14 to skip non-utf8 data
+        const [header, , contents, , info] = pdfStrings.split('stream'); // ignore everthing from `stream` to `endstream` - the non-utf8 blocks
+
+        // PDF Header
+
+        expectSnapshot(header).toMatchInline(`
+          "
+          7 0 obj
+          <<
+          /Predictor 15
+          /Colors 1
+          /BitsPerComponent 8
+          /Columns 577
+          >>
+          endobj
+          8 0 obj
+          <<
+          /Length 149
+          /Filter /FlateDecode
+          >>
+          "
+        `);
+
+        // PDF Contents
+
+        expectSnapshot(contents).toMatchInline(`
+          "
+          endobj
+          12 0 obj
+          <<
+          /Type /ExtGState
+          /ca 1
+          /CA 1
+          >>
+          endobj
+          11 0 obj
+          <<
+          /Type /Page
+          /Parent 1 0 R
+          /MediaBox [0 0 1659.333374 1673]
+          /Contents 9 0 R
+          /Resources 10 0 R
+          >>
+          endobj
+          10 0 obj
+          <<
+          /ProcSet [/PDF /Text /ImageB /ImageC /ImageI]
+          /ExtGState <<
+          /Gs1 12 0 R
+          >>
+          /Font <<
+          /F1 13 0 R
+          /F2 14 0 R
+          >>
+          /XObject <<
+          /I1 5 0 R
+          /I2 6 0 R
+          >>
+          >>
+          endobj
+          9 0 obj
+          <<
+          /Length 516
+          /Filter /FlateDecode
+          >>
+          "
+        `);
+
+        // PDF Info
+
+        // The Info section of text includes a Datestamp which will obviously not match as a snapshot between tests
+        // This does a .replace on the Info text to erase the dynamic date string
+        expectSnapshot(info.replace(/D:\d+Z/, 'D:DATESTAMP')).toMatchInline(`
+          "
+          endobj
+          16 0 obj
+          (pdfmake)
+          endobj
+          17 0 obj
+          (pdfmake)
+          endobj
+          18 0 obj
+          (D:DATESTAMP)
+          endobj
+          15 0 obj
+          <<
+          /Producer 16 0 R
+          /Creator 17 0 R
+          /CreationDate 18 0 R
+          >>
+          endobj
+          20 0 obj
+          <<
+          /Type /FontDescriptor
+          /FontName /AZZZZZ+Roboto-Medium
+          /Flags 4
+          /FontBBox [-732.421875 -270.996094 1192.871094 1047.851563]
+          /ItalicAngle 0
+          /Ascent 927.734375
+          /Descent -244.140625
+          /CapHeight 710.9375
+          /XHeight 528.320313
+          /StemV 0
+          /FontFile2 19 0 R
+          >>
+          endobj
+          21 0 obj
+          <<
+          /Type /Font
+          /Subtype /CIDFontType2
+          /BaseFont /AZZZZZ+Roboto-Medium
+          /CIDSystemInfo <<
+          /Registry (Adobe)
+          /Ordering (Identity)
+          /Supplement 0
+          >>
+          /FontDescriptor 20 0 R
+          /W [0 [510 579.589844 524.902344 566.40625 869.628906 249.023438 672.851563 543.945313 520.019531 566.40625 566.40625 356.445313 566.40625]]
+          /CIDToGIDMap /Identity
+          >>
+          endobj
+          22 0 obj
+          <<
+          /Length 256
+          /Filter /FlateDecode
+          >>
+          "
+        `);
       });
     });
   });
