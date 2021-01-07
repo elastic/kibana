37217794d13e2f7034ed73baa4edcecb089e3755
Brian Seeders <brian.seeders@elastic.co>
2021-01-07 14:21:06 -0500

    Some fixes after PR feedback

--- .teamcity/src/Common.kt
+++ .teamcity/src/Common.kt
@@ -1,5 +1,5 @@
-const val PROJECT_BRANCH = "master_teamcity"
-const val CORRESPONDING_ES_BRANCH = "master"
+import co.elastic.teamcity.common.getOrgProjectIdKebab
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 
 // If set to true, github check/commit status will be reported, failed-test-reporter will run, etc.
 const val ENABLE_REPORTING = false
@@ -8,11 +8,13 @@ const val ENABLE_REPORTING = false
 const val ENABLE_TRIGGERS = false
 
 fun getProjectBranch(): String {
-  return PROJECT_BRANCH
+  // There's no good way to retrieve this information, except to have the convention that the root project name is based on the branch
+  // There are DSL Context Parameters which would work, except that they can only be managed via the UI.
+  return DslContext.projectName
 }
 
 fun getCorrespondingESBranch(): String {
-  return CORRESPONDING_ES_BRANCH
+  return getOrgProjectIdKebab().replace("_teamcity", "")
 }
 
 fun areTriggersEnabled(): Boolean {
--- .teamcity/src/vcs/Elasticsearch.kt
+++ .teamcity/src/vcs/Elasticsearch.kt
@@ -5,7 +5,7 @@ import getProjectBranch
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object Elasticsearch : GitVcsRoot({
-  id("elasticsearch_${getProjectBranch()}")
+  id("elasticsearch_${getCorrespondingESBranch().replace("/", "_")}")
 
   name = "elasticsearch / ${getCorrespondingESBranch()}"
   url = "https://github.com/elastic/elasticsearch.git"
