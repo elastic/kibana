57fd202b2ca2857b7a6f117e03fa74b8ae87c66d
Gloria Hornero <snootchie.boochies@gmail.com>
2021-01-12 09:19:34 +0100

    waits for the timeline modal before doing validations

--- x-pack/plugins/security_solution/cypress/screens/timeline.ts
+++ x-pack/plugins/security_solution/cypress/screens/timeline.ts
@@ -70,6 +70,8 @@ export const NOTES_COUNT = '[data-test-subj="timeline-notes-count"]';
 
 export const OPEN_TIMELINE_ICON = '[data-test-subj="open-timeline-button"]';
 
+export const OPEN_TIMELINE_MODAL = '[data-test-subj="open-timeline-modal"]';
+
 export const OPEN_TIMELINE_TEMPLATE_ICON =
   '[data-test-subj="open-timeline-modal-body-filter-template"]';
 
