30f06b679e387e5e25a8df79b99e400386accac2
Gidi Meir Morris <github@gidi.io>
2021-01-12 20:25:30 +0000

    added comment

--- x-pack/plugins/task_manager/server/monitoring/task_run_statistics.ts
+++ x-pack/plugins/task_manager/server/monitoring/task_run_statistics.ts
@@ -161,6 +161,10 @@ export function createTaskRunAggregator(
     ),
     // get DateTime of latest polling delay refresh
     merge(
+      /**
+       * as `combineLatest` hangs until it has its first value and we're not likely to reconfigure the delay in normal deployments, we needed some initial value.
+        I've used _now_ (`new Date().toISOString()`) as it made the most sense (it would be the time Kibana started), but it _could_ be confusing in the future.
+       */
       of(new Date().toISOString()),
       taskPollingLifecycle.events.pipe(
         filter(
