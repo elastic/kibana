5b06fe1b42e768217f1c4f4f2dc348c3d390c798
kobelb <brandon.kobel@elastic.co>
2021-01-12 07:52:05 -0800

    Updating the full dual-license text

--- src/dev/build/tasks/license_file_task.ts
+++ src/dev/build/tasks/license_file_task.ts
@@ -1,20 +1,5 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Placeholder dual-license header
  */
 
 import { write, read, Task } from '../lib';
@@ -24,10 +9,10 @@ export const UpdateLicenseFile: Task = {
 
   async run(config, log, build) {
     if (build.isOss()) {
-      log.info('Copying Apache 2.0 license to LICENSE.txt');
+      log.info('Copying dual-license to LICENSE.txt');
       await write(
         build.resolvePath('LICENSE.txt'),
-        await read(config.resolveFromRepo('licenses/APACHE-LICENSE-2.0.txt'))
+        await read(config.resolveFromRepo('licenses/DUAL-LICENSE.txt'))
       );
     } else {
       log.info('Copying Elastic license to LICENSE.txt');
