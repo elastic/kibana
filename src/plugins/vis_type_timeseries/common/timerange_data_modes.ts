f7710eeefe67931ffe8f1f0682f528d89126a3b2
Alexey Antonov <alexwizp@gmail.com>
2021-01-13 09:41:46 +0300

    [TSVB] Allow custom label for fields via index pattern field management (#84612) (#88074)
    * [TSVB] Allow custom label for fields via index pattern field management
    
    Closes: #84336
    
    * replace saveObject, elasticsearch client to new one
    
    * fix CI
    
    * update schema
    
    * fix Top Hit
    
    * some changes
    
    * partially move getting fields into client side
    
    * fix PR comments
    
    * fix issue with getting fields
    
    * move SanitizedFieldType to common types
    
    * fix issue on changing index pattern
    
    * fix issue
    
    * fix regression
    
    * some work
    
    * remove extractFieldName, createCustomLabelSelectHandler
    
    * request/response processors should be async
    
    * some work
    
    * remove tests for createCustomLabelSelectHandler
    
    * fix table
    
    * fix placeholder
    
    * some work
    
    * fix jest
    
    * fix CI
    
    * fix label for table view
    
    * test: visualize app visual builder switch index patterns should be able to switch between index patterns
    
    * fix functional tests
    
    * fix sorting
    
    * fix labels for entire timerange mode
    
    * add createFieldsFetcher method
    
    * table view - fix pivot label
    
    * fix PR comments
    
    * fix issue with selecting buckets scripts
    
    * fix types
    
    * Update create_select_handler.test.ts
    
    * fix PR comments
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/vis_type_timeseries/common/timerange_data_modes.ts
+++ src/plugins/vis_type_timeseries/common/timerange_data_modes.ts
@@ -17,23 +17,28 @@
  * under the License.
  */
 
-import { createSelectHandler } from './create_select_handler';
+/**
+ * Time Range data modes.
+ * @constant
+ * @public
+ */
+export enum TIME_RANGE_DATA_MODES {
+  /**
+   * Entire timerange mode will match all the documents selected in the
+   * timerange timepicker
+   */
+  ENTIRE_TIME_RANGE = 'entire_time_range',
 
-describe('createSelectHandler()', () => {
-  let handleChange;
-  let changeHandler;
+  /**
+   * Last value mode will match only the documents for the specified interval
+   * from the end of the timerange.
+   */
+  LAST_VALUE = 'last_value',
+}
 
-  beforeEach(() => {
-    handleChange = jest.fn();
-    changeHandler = createSelectHandler(handleChange);
-    const fn = changeHandler('test');
-    fn([{ value: 'foo' }]);
-  });
-
-  test('calls handleChange() function with partial', () => {
-    expect(handleChange.mock.calls.length).toEqual(1);
-    expect(handleChange.mock.calls[0][0]).toEqual({
-      test: 'foo',
-    });
-  });
-});
+/**
+ * Key for getting the Time Range mode from the Panel configuration object.
+ * @constant
+ * @public
+ */
+export const TIME_RANGE_MODE_KEY = 'time_range_mode';
