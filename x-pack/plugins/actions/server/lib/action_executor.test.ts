7885a93c8072f78914b19571a5780a4985304838
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2021-01-13 11:59:15 -0800

    fixed faling test

--- x-pack/plugins/actions/server/lib/action_executor.test.ts
+++ x-pack/plugins/actions/server/lib/action_executor.test.ts
@@ -324,7 +324,7 @@ test('throws an error when passing isESOAvailable with value of false', async ()
   await expect(
     customActionExecutor.execute(executeParams)
   ).rejects.toThrowErrorMatchingInlineSnapshot(
-    `"Unable to execute action because the Encrypted Saved Objects plugin uses an ephemeral encryption key. Please set xpack.encryptedSavedObjects.encryptionKey in the kibana.yml or use the bin/kibana-encryption-keys command."`
+    `"Unable to execute action because the Encrypted Saved Objects plugin is not available. Please set xpack.encryptedSavedObjects.encryptionKey in the kibana.yml or use the bin/kibana-encryption-keys command."`
   );
 });
 
