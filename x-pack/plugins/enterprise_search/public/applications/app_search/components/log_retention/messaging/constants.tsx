c8a5a44ca4fe4b98a59c4f0fda8b04b983279550
Constance Chen <constance.chen.3@gmail.com>
2021-01-12 23:59:41 -0800

    Refactor log_retention/messaging
    - Convert to all JSX/React vs plain strings (makes it easier to deal with FormattedDate/FormattedMessages)
    
    - Consolidate to single component that takes various log types (e.g. analytics, api)
    
    - This is important for adding future switches & toggles - there is *significantly* less complex strings to localize this way (2 short strings vs 5+ long sentences)

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/log_retention/messaging/constants.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/log_retention/messaging/constants.tsx
@@ -5,122 +5,92 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from '@kbn/i18n/react';
+import { FormattedDate, FormattedMessage } from '@kbn/i18n/react';
 import { i18n } from '@kbn/i18n';
 
-import { LogRetentionMessages } from './types';
-import { renderLogRetentionDate } from '.';
+import { LogRetentionOptions } from '../types';
 
-const ANALYTICS_NO_LOGGING = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.analytics.noLogging',
-  {
-    defaultMessage: 'Analytics collection has been disabled for all engines.',
-  }
+export const renderLogRetentionDate = (dateString: string) => (
+  <FormattedDate value={new Date(dateString)} month="long" day="numeric" year="numeric" />
 );
 
-const ANALYTICS_NO_LOGGING_COLLECTED = (disabledAt: React.ReactElement) => (
+const CAPITALIZATION_MAP = {
+  [LogRetentionOptions.Analytics]: {
+    capitalized: i18n.translate(
+      'xpack.enterpriseSearch.appSearch.logRetention.type.analytics.title.capitalized',
+      { defaultMessage: 'Analytics' }
+    ),
+    lowercase: i18n.translate(
+      'xpack.enterpriseSearch.appSearch.logRetention.type.analytics.title.lowercase',
+      { defaultMessage: 'analytics' }
+    ),
+  },
+  [LogRetentionOptions.API]: {
+    capitalized: i18n.translate(
+      'xpack.enterpriseSearch.appSearch.logRetention.type.api.title.capitalized',
+      { defaultMessage: 'API' }
+    ),
+    lowercase: i18n.translate(
+      'xpack.enterpriseSearch.appSearch.logRetention.type.api.title.lowercase',
+      { defaultMessage: 'API' }
+    ),
+  },
+};
+
+interface Props {
+  type: LogRetentionOptions;
+  disabledAt?: string | null;
+  minAgeDays?: number | null;
+}
+
+export const NoLogging: React.FC<Props> = ({ type, disabledAt }) => {
+  return (
+    <>
+      <FormattedMessage
+        id="xpack.enterpriseSearch.appSearch.logRetention.noLogging"
+        defaultMessage="{logsType} logging has been disabled for all engines."
+        values={{ logsType: CAPITALIZATION_MAP[type].capitalized }}
+      />{' '}
+      {disabledAt ? (
+        <FormattedMessage
+          id="xpack.enterpriseSearch.appSearch.logRetention.noLogging.collected"
+          defaultMessage="The last date {logsType} logs were collected was {disabledAtDate}."
+          values={{
+            logsType: CAPITALIZATION_MAP[type].lowercase,
+            disabledAtDate: renderLogRetentionDate(disabledAt),
+          }}
+        />
+      ) : (
+        <FormattedMessage
+          id="xpack.enterpriseSearch.appSearch.logRetention.noLogging.notCollected"
+          defaultMessage="There are no {logsType} logs collected."
+          values={{ logsType: CAPITALIZATION_MAP[type].lowercase }}
+        />
+      )}
+    </>
+  );
+};
+
+export const ILMDisabled: React.FC<Props> = ({ type }) => (
   <FormattedMessage
-    id="xpack.enterpriseSearch.appSearch.settings.logRetention.analytics.noLogging.collected"
-    defaultMessage="The last date analytics were collected was {disabledAt}."
-    values={{ disabledAt }}
+    id="xpack.enterpriseSearch.appSearch.logRetention.ilmDisabled"
+    defaultMessage="App Search isn't managing {logsType} log retention."
+    values={{ logsType: CAPITALIZATION_MAP[type].lowercase }}
   />
 );
 
-const ANALYTICS_NO_LOGGING_NOT_COLLECTED = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.analytics.noLogging.notCollected',
-  {
-    defaultMessage: 'There are no analytics collected.',
-  }
-);
-
-const ANALYTICS_ILM_DISABLED = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.analytics.ilmDisabled',
-  {
-    defaultMessage: "App Search isn't managing analytics retention.",
-  }
-);
-
-const ANALYTICS_CUSTOM_POLICY = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.analytics.customPolicy',
-  {
-    defaultMessage: 'You have a custom analytics retention policy.',
-  }
-);
-
-const ANALYTICS_STORED = (minAgeDays: number | null | undefined) =>
-  i18n.translate('xpack.enterpriseSearch.appSearch.settings.logRetention.analytics.stored', {
-    defaultMessage: 'Your analytics are being stored for at least {minAgeDays} days.',
-    values: { minAgeDays },
-  });
-
-const API_NO_LOGGING = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.api.noLogging',
-  {
-    defaultMessage: 'API logging has been disabled for all engines.',
-  }
-);
-
-const API_NO_LOGGING_COLLECTED = (disabledAt: React.ReactElement) => (
+export const CustomPolicy: React.FC<Props> = ({ type }) => (
   <FormattedMessage
-    id="xpack.enterpriseSearch.appSearch.settings.logRetention.api.noLogging.collected"
-    defaultMessage="The last date logs were collected was {disabledAt}."
-    values={{ disabledAt }}
+    id="xpack.enterpriseSearch.appSearch.logRetention.customPolicy"
+    defaultMessage="You have a custom {logsType} log retention policy."
+    values={{ logsType: CAPITALIZATION_MAP[type].lowercase }}
   />
 );
 
-const API_NO_LOGGING_NOT_COLLECTED = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.api.noLogging.notCollected',
-  {
-    defaultMessage: 'There are no logs collected.',
-  }
+export const DefaultPolicy: React.FC<Props> = ({ type, minAgeDays }) => (
+  <FormattedMessage
+    id="xpack.enterpriseSearch.appSearch.logRetention.defaultPolicy"
+    defaultMessage="Your {logsType} logs are being stored for at least {minAgeDays} days."
+    values={{ logsType: CAPITALIZATION_MAP[type].lowercase, minAgeDays }}
+  />
 );
-
-const API_ILM_DISABLED = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.api.ilmDisabled',
-  {
-    defaultMessage: "App Search isn't managing API log retention.",
-  }
-);
-
-const API_CUSTOM_POLICY = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.settings.logRetention.api.customPolicy',
-  {
-    defaultMessage: 'You have a custom API log retention policy.',
-  }
-);
-
-const API_STORED = (minAgeDays: number | null | undefined) =>
-  i18n.translate('xpack.enterpriseSearch.appSearch.settings.logRetention.api.stored', {
-    defaultMessage: 'Your logs are being stored for at least {minAgeDays} days.',
-    values: { minAgeDays },
-  });
-
-export const ANALYTICS_MESSAGES: LogRetentionMessages = {
-  noLogging: (_, logRetentionSettings) => (
-    <>
-      {ANALYTICS_NO_LOGGING + ' '}
-      {logRetentionSettings.disabledAt
-        ? ANALYTICS_NO_LOGGING_COLLECTED(renderLogRetentionDate(logRetentionSettings.disabledAt))
-        : ANALYTICS_NO_LOGGING_NOT_COLLECTED}
-    </>
-  ),
-  ilmDisabled: ANALYTICS_ILM_DISABLED,
-  customPolicy: ANALYTICS_CUSTOM_POLICY,
-  defaultPolicy: (_, logRetentionSettings) =>
-    ANALYTICS_STORED(logRetentionSettings.retentionPolicy?.minAgeDays),
-};
-
-export const API_MESSAGES: LogRetentionMessages = {
-  noLogging: (_, logRetentionSettings) => (
-    <>
-      {API_NO_LOGGING + ' '}
-      {logRetentionSettings.disabledAt
-        ? API_NO_LOGGING_COLLECTED(renderLogRetentionDate(logRetentionSettings.disabledAt))
-        : API_NO_LOGGING_NOT_COLLECTED}
-    </>
-  ),
-  ilmDisabled: API_ILM_DISABLED,
-  customPolicy: API_CUSTOM_POLICY,
-  defaultPolicy: (_, logRetentionSettings) =>
-    API_STORED(logRetentionSettings.retentionPolicy?.minAgeDays),
-};
