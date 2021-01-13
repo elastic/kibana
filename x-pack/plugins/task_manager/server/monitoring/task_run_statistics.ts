926af70bb464878019c61a0c4286d9c319f02c18
Gidi Meir Morris <github@gidi.io>
2021-01-13 16:56:50 +0000

    fixed typing

--- x-pack/plugins/task_manager/server/monitoring/task_run_statistics.ts
+++ x-pack/plugins/task_manager/server/monitoring/task_run_statistics.ts
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { combineLatest, merge, Observable, of } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { filter, startWith, map } from 'rxjs/operators';
 import { JsonObject } from 'src/plugins/kibana_utils/common';
 import { isNumber, mapValues } from 'lodash';
