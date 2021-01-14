e81c6daeaed26a9f7d64ef2957faa9daf4bb6fcb
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2021-01-14 15:11:02 +0200

    Remove cyclic dependencies

--- src/plugins/vis_type_pie/common/index.ts
+++ src/plugins/vis_type_pie/common/index.ts
@@ -17,21 +17,4 @@
  * under the License.
  */
 
-import { $Values } from '@kbn/utility-types';
-
-/**
- * Type of charts able to render
- */
-export const ChartType = Object.freeze({
-  Line: 'line' as const,
-  Area: 'area' as const,
-  Histogram: 'histogram' as const,
-});
-export type ChartType = $Values<typeof ChartType>;
-
-/**
- * Type of xy visualizations
- */
-export type XyVisType = ChartType | 'horizontal_bar';
-
 export const LEGACY_CHARTS_LIBRARY = 'visualization:visualize:legacyChartsLibrary';
