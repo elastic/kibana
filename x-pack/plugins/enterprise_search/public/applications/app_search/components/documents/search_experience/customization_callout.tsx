8160382a4d1b340df4982b3f811af7690943189a
Jason Stoltzfus <jastoltz24@gmail.com>
2021-01-14 09:33:44 -0500

    Re-added lost styles

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/customization_callout.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/search_experience/customization_callout.tsx
@@ -15,7 +15,12 @@ interface Props {
 
 export const CustomizationCallout: React.FC<Props> = ({ onClick }) => {
   return (
-    <EuiFlexGroup direction="column" alignItems="center" gutterSize="none">
+    <EuiFlexGroup
+      direction="column"
+      className="customizationCallout"
+      alignItems="center"
+      gutterSize="none"
+    >
       <EuiIcon type="iInCircle" color="primary" size="xxl" />
       <EuiSpacer />
       <EuiText size="s" textAlign="center">
