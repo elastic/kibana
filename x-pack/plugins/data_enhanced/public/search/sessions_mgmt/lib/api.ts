34cd7421b5d4b3ab219e27b91497cebbd33a69e0
Liza K <liza.katz@elastic.co>
2021-01-14 14:00:20 +0200

    fix ts

--- x-pack/plugins/data_enhanced/public/search/sessions_mgmt/lib/api.ts
+++ x-pack/plugins/data_enhanced/public/search/sessions_mgmt/lib/api.ts
@@ -120,7 +120,7 @@ export class SearchSessionsMgmtAPI {
   }
 
   // Delete
-  public async sendDelete(id: string): Promise<UISession[] | null> {
+  public async sendDelete(id: string): Promise<UISession[]> {
     try {
       await this.sessionsClient.delete(id);
 
@@ -144,7 +144,7 @@ export class SearchSessionsMgmtAPI {
   }
 
   // Extend
-  public async sendExtend(id: string): Promise<UISession[] | null> {
+  public async sendExtend(id: string): Promise<UISession[]> {
     this.notifications.toasts.addError(new Error('Not implemented'), {
       title: i18n.translate('xpack.data.mgmt.searchSessions.api.extendError', {
         defaultMessage: 'Failed to extend the session expiration!',
