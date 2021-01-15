f693ecd38456c0e082ed65db056a3ae0c4eb2bed
scottybollinger <scotty.bollinger@elastic.co>
2021-01-15 15:04:31 -0600

    Remove unncessary imports
    Copied from this PR:
    https://github.com/elastic/kibana/pull/88525

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/views/settings/settings_router.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/settings/settings_router.test.tsx
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import '../../../__mocks__/kea.mock';
 import '../../../__mocks__/shallow_useeffect.mock';
 
 import { setMockActions } from '../../../__mocks__';
