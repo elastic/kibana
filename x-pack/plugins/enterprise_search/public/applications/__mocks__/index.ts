59ebcd0e66ae9a0b10cb35466ee93150c891db8d
Constance <constancecchen@users.noreply.github.com>
2021-01-15 12:39:26 -0800

    [Enterprise Search] Automatically mock shared logic files (#88494)
    * Update shared logic mocks to automatically mock their exports
    
    * Update FlashMessages to also mock its helper fns
    
    * Fix tests that were relying on component exports from shared logic files
    
    * Fix broken flash_messages tests
    
    - Caused by leaking auto mocks - work around this by importing from files directly and not from index.ts
    
    - Also clean up / use new auto mocks (e.g. for kibana)
    
    - Convert old instances of useValues mock to setMockValues
    
    * [AS] Update AnalyticsLogic test to use new auto mockers
    
    + move LogicMounter and destructured mock values to top of describe block
    
    * [AS] Update CredentialsLogic
    
    + udpate to use new clearFlashMessages helper
    
    * [AS] Update documents logic tests
    
    * [AS] Update engines logic tests
    
    * [AS] Update log retention logic test
    
    * [Shared] Update IndexingStatus tests
    
    + update to use LogicMounter
    
    * [Shared] Update telemetry logic test
    
    * [WS] Update AddSourceLogic
    
    + update to use new clearFlashMessages helper
    
    * [WS] Update groups logic files
    
    + update to use new flash message helpers
    
    * [WS] Update OverviewLogic test
    
    * [WS] Update AddSource component test
    
    + consolidate mocks imports
    
    * [Shared] Clean up KibanaLogic imports
    
    - all jest.mocks come along from the ride when the __mocks__ folder is imported, so HttpLogic is now automatically already mocked
    
    * [AS] Update EngineRouter test
    # Conflicts:
    #	x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/add_source.test.tsx
    #	x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/add_source_logic.test.ts
    #	x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/add_source_logic.ts

--- x-pack/plugins/enterprise_search/public/applications/__mocks__/index.ts
+++ x-pack/plugins/enterprise_search/public/applications/__mocks__/index.ts
@@ -9,7 +9,11 @@ export { mockKibanaValues } from './kibana_logic.mock';
 export { mockLicensingValues } from './licensing_logic.mock';
 export { mockHttpValues } from './http_logic.mock';
 export { mockTelemetryActions } from './telemetry_logic.mock';
-export { mockFlashMessagesValues, mockFlashMessagesActions } from './flash_messages_logic.mock';
+export {
+  mockFlashMessagesValues,
+  mockFlashMessagesActions,
+  mockFlashMessageHelpers,
+} from './flash_messages_logic.mock';
 export {
   mockAllValues,
   mockAllActions,
