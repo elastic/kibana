95d135c84b35ab1f494ff98d34e8b778cc536bc1
Constance Chen <constance.chen.3@gmail.com>
2021-01-14 18:49:15 -0800

    Remove unnecessary kea.mock file imports
    - the automock already happens when you import something from the kea.mock file
    - in some cases in WS the mock just wasn't needed

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/settings/log_retention/log_retention_confirmation_modal.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/settings/log_retention/log_retention_confirmation_modal.test.tsx
@@ -4,7 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import '../../../../__mocks__/kea.mock';
 import { setMockActions, setMockValues } from '../../../../__mocks__';
 
 import React from 'react';
--- x-pack/plugins/enterprise_search/public/applications/workplace_search/views/groups/components/group_sub_nav.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/groups/components/group_sub_nav.test.tsx
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import '../../../../__mocks__/kea.mock';
-
 import { setMockValues } from '../../../../__mocks__';
 
 import React from 'react';
