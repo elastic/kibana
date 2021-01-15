7e0df18b98457cd90e855ed5b1e6bbe910e50f2c
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2021-01-15 15:35:54 +0100

    Test the apps in the test suite instead of the plugin

--- x-pack/test/usage_collection/plugins/application_usage_test/public/plugin.ts
+++ x-pack/test/usage_collection/plugins/application_usage_test/public/plugin.ts
@@ -6,25 +6,13 @@
 
 import { Plugin, CoreSetup, CoreStart } from 'kibana/public';
 import { first } from 'rxjs/operators';
-import expect from '@kbn/expect';
-import { applicationUsageSchema } from '../../../../../../src/plugins/kibana_usage_collection/server/collectors/application_usage/schema';
+import './types';
 
 export class ApplicationUsageTest implements Plugin {
   public setup(core: CoreSetup) {}
 
   public async start(core: CoreStart) {
-    if (core.http.anonymousPaths.isAnonymous(window.location.pathname)) {
-      return;
-    }
     const applications = await core.application.applications$.pipe(first()).toPromise();
-    const appIds = [...applications.keys()].sort();
-
-    try {
-      // When the lists don't match, the entire page load will fail.
-      expect(Object.keys(applicationUsageSchema).sort()).to.eql(appIds);
-    } catch (err) {
-      err.message = `Application Usage's schema is not up-to-date with the actual registered apps. Please update it at src/plugins/kibana_usage_collection/server/collectors/application_usage/schema.ts.\n${err.message}`;
-      throw err;
-    }
+    window.__applicationIds__ = [...applications.keys()];
   }
 }
--- x-pack/test/usage_collection/test_suites/application_usage/index.ts
+++ x-pack/test/usage_collection/test_suites/application_usage/index.ts
@@ -4,15 +4,20 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
+import { applicationUsageSchema } from '../../../../../src/plugins/kibana_usage_collection/server/collectors/application_usage/schema';
 
-export default function ({ getPageObjects }: FtrProviderContext) {
+export default function ({ getService, getPageObjects }: FtrProviderContext) {
   describe('Application Usage', function () {
     this.tags('ciGroup1');
     const { common } = getPageObjects(['common']);
+    const browser = getService('browser');
 
-    it('successfully loads home (the application_usage_test plugin does not fail)', async () => {
-      await common.navigateToApp('home');
+    it('keys in the schema match the registered application IDs', async () => {
+      await common.navigateToApp('home'); // Navigate to Home to make sure all the appIds are loaded
+      const appIds = await browser.execute(() => window.__applicationIds__);
+      expect(Object.keys(applicationUsageSchema).sort()).to.eql(appIds.sort());
     });
   });
 }
