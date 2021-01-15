ca42e9a59fc7f166adb5e41e2584d43ecaf5a55a
Constance <constancecchen@users.noreply.github.com>
2021-01-14 16:01:24 -0800

    [App Search] New log retention components + major i18n refactors (#88237)
    * Add remaining log retention components
    
    - Will shortly be used by Analytics view
    
    * [i18n] Change log retention date from moment to react-intl FormattedDate
    
    - this correctly localizes the dates when Kibana is in different languages, e.g. zh-CN, ja-JP
    
    * Refactor log_retention/messaging
    
    - Convert to all JSX/React vs plain strings (makes it easier to deal with FormattedDate/FormattedMessages)
    
    - Consolidate to single component that takes various log types (e.g. analytics, api)
    
    - This is important for adding future switches & toggles - there is *significantly* less complex strings to localize this way (2 short strings vs 5+ long sentences)
    
    * Update existing instances to use new LogRetentionMessage
    
    * Update translation strings
    - to account for new i18n logs type values & IDs
    
    * Attempt to fix test timezone shenanigans
    
    * [PR feedback] Types
    
    * [PR feedback] i18n pluralization
    
    * Update LogRetentionTooltip and LogRetentionCallout to manage fetching log retention
    
    + change ILM to LogRetention per PR feedback
    
    * Update LogRetentionLogic to prevent duplicate fetches
    
    - e.g. if both LogRetentionTooltip and LogRetentionCallout are on the same page (which they will be)

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/log_retention/messaging/index.ts
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/log_retention/messaging/index.ts
@@ -4,7 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { LogRetentionLogic } from './log_retention_logic';
-export * from './types';
-export * from './messaging';
-export * from './components';
+export { LogRetentionMessage } from './log_retention_message';
+export { renderLogRetentionDate } from './constants';
