db6cf2c37d54025c29b60a5f0d1494845ffa4eb5
Tim Roes <tim.roes@elastic.co>
2021-01-15 14:50:44 +0100

    [Discover] Add icon for nested fields in sidebar (#85147) (#88427)
    * [WIP] nested field icon
    
    * only import type
    
    * use named lodash import

--- src/plugins/discover/public/application/components/table/table.tsx
+++ src/plugins/discover/public/application/components/table/table.tsx
@@ -17,9 +17,9 @@
  * under the License.
  */
 import React, { useState } from 'react';
-import { escapeRegExp } from 'lodash';
 import { DocViewTableRow } from './table_row';
 import { trimAngularSpan } from './table_helper';
+import { isNestedFieldParent } from '../../helpers/nested_fields';
 import { DocViewRenderProps } from '../../doc_views/doc_views_types';
 
 const COLLAPSE_LINE_LENGTH = 350;
@@ -74,48 +74,9 @@ export function DocViewTable({
                 : undefined;
             const displayUnderscoreWarning = !mapping(field) && field.indexOf('_') === 0;
 
-            // Discover doesn't flatten arrays of objects, so for documents with an `object` or `nested` field that
-            // contains an array, Discover will only detect the top level root field. We want to detect when those
-            // root fields are `nested` so that we can display the proper icon and label. However, those root
-            // `nested` fields are not a part of the index pattern. Their children are though, and contain nested path
-            // info. So to detect nested fields we look through the index pattern for nested children
-            // whose path begins with the current field. There are edge cases where
-            // this could incorrectly identify a plain `object` field as `nested`. Say we had the following document
-            // where `foo` is a plain object field and `bar` is a nested field.
-            // {
-            //   "foo": [
-            //   {
-            //     "bar": [
-            //       {
-            //         "baz": "qux"
-            //       }
-            //     ]
-            //   },
-            //   {
-            //     "bar": [
-            //       {
-            //         "baz": "qux"
-            //       }
-            //     ]
-            //   }
-            // ]
-            // }
-            //
-            // The following code will search for `foo`, find it at the beginning of the path to the nested child field
-            // `foo.bar.baz` and incorrectly mark `foo` as nested. Any time we're searching for the name of a plain object
-            // field that happens to match a segment of a nested path, we'll get a false positive.
-            // We're aware of this issue and we'll have to live with
-            // it in the short term. The long term fix will be to add info about the `nested` and `object` root fields
-            // to the index pattern, but that has its own complications which you can read more about in the following
-            // issue: https://github.com/elastic/kibana/issues/54957
-            const isNestedField =
-              !indexPattern.fields.getByName(field) &&
-              !!indexPattern.fields.getAll().find((patternField) => {
-                // We only want to match a full path segment
-                const nestedRootRegex = new RegExp(escapeRegExp(field) + '(\\.|$)');
-                return nestedRootRegex.test(patternField.subType?.nested?.path ?? '');
-              });
-            const fieldType = isNestedField ? 'nested' : indexPattern.fields.getByName(field)?.type;
+            const fieldType = isNestedFieldParent(field, indexPattern)
+              ? 'nested'
+              : indexPattern.fields.getByName(field)?.type;
 
             return (
               <DocViewTableRow
