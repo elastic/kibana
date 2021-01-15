a27bcaf3322ef8359aa3a919ba9b7fb65ab1543b
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-01-15 12:52:06 +0100

    add jenkins script

---
+++ test/scripts/jenkins_build_load_testing.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cd "$KIBANA_DIR"
+source src/dev/ci_setup/setup_env.sh
+
+if [[ ! "$TASK_QUEUE_PROCESS_ID" ]]; then
+  ./test/scripts/jenkins_xpack_build_plugins.sh
+fi
+
+# doesn't persist, also set in kibanaPipeline.groovy
+export KBN_NP_PLUGINS_BUILT=true
+
+echo " -> building and extracting default Kibana distributable for use in functional tests"
+cd "$KIBANA_DIR"
+node scripts/build --debug --no-oss
+linuxBuild="$(find "$KIBANA_DIR/target" -name 'kibana-*-linux-x86_64.tar.gz')"
+installDir="$KIBANA_DIR/install/kibana"
+mkdir -p "$installDir"
+tar -xzf "$linuxBuild" -C "$installDir" --strip=1
+
+mkdir -p "$WORKSPACE/kibana-build-xpack"
+cp -pR install/kibana/. $WORKSPACE/kibana-build-xpack/
+
+echo " -> Run gatling load testing via ftr"
+cd x-pack
+node scripts/functional_tests.js --config test/load/config.ts
