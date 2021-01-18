8cdb00017d98031fd48f518043e4d0ebea3149a8
Ahmad Bamieh <ahmadbamieh@gmail.com>
2021-01-18 17:57:19 +0200

    eslint

--- src/core/server/saved_objects/saved_objects_service.ts
+++ src/core/server/saved_objects/saved_objects_service.ts
@@ -389,8 +389,8 @@ export class SavedObjectsService
     const skipMigrations = this.config.migration.skip || !pluginsInitialized;
 
     /**
-     * Note: Prepares all migrations maps. If a saved object type was registered with a `migrations` function, 
-     *  this function will be called to get the type's SavedObjectMigrationMap.
+     * Note: Prepares all migrations maps. If a saved object type was registered with property `migrations`
+     * of type function; this function will be called to get the type's SavedObjectMigrationMap.
      */
     migrator.prepareMigrations();
 
