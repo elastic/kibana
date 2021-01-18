5126a028ed990f3b624e355ec25f0ae1fd0ca5bd
Constance Chen <constance.chen.3@gmail.com>
2021-01-18 14:38:01 -0800

    Create mockEngineValues reusable mock

---
+++ x-pack/plugins/enterprise_search/public/applications/app_search/__mocks__/engine_logic.mock.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import { generatePath } from 'react-router-dom';
+
+export const mockEngineValues = {
+  engineName: 'some-engine',
+  // Note: using getters allows us to use `this`, which lets tests
+  // override engineName and still generate correct engine names
+  get generatePath() {
+    return jest.fn((path, pathParams = {}) =>
+      generatePath(path, { engineName: this.engineName, ...pathParams })
+    );
+  },
+  engine: {},
+};
+
+jest.mock('../components/engine', () => ({
+  EngineLogic: { values: mockEngineValues },
+}));
