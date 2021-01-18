775ca951326b6a64a922b67c5d2b36e73cf8810c
Constance Chen <constance.chen.3@gmail.com>
2021-01-18 15:03:02 -0800

    Update AnalyticsRouter to use new routes+generatePath

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/analytics/analytics_router.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/analytics/analytics_router.tsx
@@ -6,14 +6,13 @@
 
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { useValues } from 'kea';
 
 import { APP_SEARCH_PLUGIN } from '../../../../../common/constants';
 import { SetAppSearchChrome as SetPageChrome } from '../../../shared/kibana_chrome';
 import { BreadcrumbTrail } from '../../../shared/kibana_chrome/generate_breadcrumbs';
 import { NotFound } from '../../../shared/not_found';
 import {
-  getEngineRoute,
-  ENGINE_PATH,
   ENGINE_ANALYTICS_PATH,
   ENGINE_ANALYTICS_TOP_QUERIES_PATH,
   ENGINE_ANALYTICS_TOP_QUERIES_NO_RESULTS_PATH,
@@ -23,6 +22,8 @@ import {
   ENGINE_ANALYTICS_QUERY_DETAILS_PATH,
   ENGINE_ANALYTICS_QUERY_DETAIL_PATH,
 } from '../../routes';
+import { EngineLogic } from '../engine';
+
 import {
   ANALYTICS_TITLE,
   TOP_QUERIES,
@@ -31,7 +32,6 @@ import {
   TOP_QUERIES_WITH_CLICKS,
   RECENT_QUERIES,
 } from './constants';
-
 import {
   Analytics,
   TopQueries,
@@ -46,40 +46,41 @@ interface Props {
   engineBreadcrumb: BreadcrumbTrail;
 }
 export const AnalyticsRouter: React.FC<Props> = ({ engineBreadcrumb }) => {
+  const { generatePath } = useValues(EngineLogic);
+
   const ANALYTICS_BREADCRUMB = [...engineBreadcrumb, ANALYTICS_TITLE];
-  const engineName = engineBreadcrumb[1];
 
   return (
     <Switch>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_PATH}>
+      <Route exact path={ENGINE_ANALYTICS_PATH}>
         <SetPageChrome trail={ANALYTICS_BREADCRUMB} />
         <Analytics />
       </Route>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_TOP_QUERIES_PATH}>
+      <Route exact path={ENGINE_ANALYTICS_TOP_QUERIES_PATH}>
         <SetPageChrome trail={[...ANALYTICS_BREADCRUMB, TOP_QUERIES]} />
         <TopQueries />
       </Route>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_TOP_QUERIES_NO_RESULTS_PATH}>
+      <Route exact path={ENGINE_ANALYTICS_TOP_QUERIES_NO_RESULTS_PATH}>
         <SetPageChrome trail={[...ANALYTICS_BREADCRUMB, TOP_QUERIES_NO_RESULTS]} />
         <TopQueriesNoResults />
       </Route>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_TOP_QUERIES_NO_CLICKS_PATH}>
+      <Route exact path={ENGINE_ANALYTICS_TOP_QUERIES_NO_CLICKS_PATH}>
         <SetPageChrome trail={[...ANALYTICS_BREADCRUMB, TOP_QUERIES_NO_CLICKS]} />
         <TopQueriesNoClicks />
       </Route>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_TOP_QUERIES_WITH_CLICKS_PATH}>
+      <Route exact path={ENGINE_ANALYTICS_TOP_QUERIES_WITH_CLICKS_PATH}>
         <SetPageChrome trail={[...ANALYTICS_BREADCRUMB, TOP_QUERIES_WITH_CLICKS]} />
         <TopQueriesWithClicks />
       </Route>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_RECENT_QUERIES_PATH}>
+      <Route exact path={ENGINE_ANALYTICS_RECENT_QUERIES_PATH}>
         <SetPageChrome trail={[...ANALYTICS_BREADCRUMB, RECENT_QUERIES]} />
         <RecentQueries />
       </Route>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_QUERY_DETAIL_PATH}>
+      <Route exact path={ENGINE_ANALYTICS_QUERY_DETAIL_PATH}>
         <QueryDetail breadcrumbs={ANALYTICS_BREADCRUMB} />
       </Route>
-      <Route exact path={ENGINE_PATH + ENGINE_ANALYTICS_QUERY_DETAILS_PATH}>
-        <Redirect to={getEngineRoute(engineName) + ENGINE_ANALYTICS_PATH} />
+      <Route exact path={ENGINE_ANALYTICS_QUERY_DETAILS_PATH}>
+        <Redirect to={generatePath(ENGINE_ANALYTICS_PATH)} />
       </Route>
       <Route>
         <NotFound breadcrumbs={ANALYTICS_BREADCRUMB} product={APP_SEARCH_PLUGIN} />
