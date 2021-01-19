5aa208e9cfc5449a67144a8baeb6c7ac33d08840
restrry <restrry@gmail.com>
2021-01-19 17:22:01 +0100

    adopt logstash

--- x-pack/plugins/logstash/server/types.ts
+++ x-pack/plugins/logstash/server/types.ts
@@ -3,7 +3,8 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-import { ILegacyScopedClusterClient } from 'src/core/server';
+import type { ILegacyScopedClusterClient, IRouter, RequestHandlerContext } from 'src/core/server';
+import type { LicensingApiRequestHandlerContext } from '../../licensing/server';
 
 export interface PipelineListItemOptions {
   id: string;
@@ -12,10 +13,17 @@ export interface PipelineListItemOptions {
   username: string;
 }
 
-declare module 'src/core/server' {
-  interface RequestHandlerContext {
-    logstash?: {
-      esClient: ILegacyScopedClusterClient;
-    };
-  }
+/**
+ * @internal
+ */
+export interface LogstashRequestHandlerContext extends RequestHandlerContext {
+  logstash: {
+    esClient: ILegacyScopedClusterClient;
+  };
+  licensing: LicensingApiRequestHandlerContext;
 }
+
+/**
+ * @internal
+ */
+export type LogstashPluginRouter = IRouter<LogstashRequestHandlerContext>;
