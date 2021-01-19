1bd2b749ef52aff1a86ab3dfc056d6ce82a0bbf0
Gloria Hornero <snootchie.boochies@gmail.com>
2021-01-19 16:25:07 +0100

    splits inspect test files

--- x-pack/plugins/security_solution/cypress/integration/network/inspect.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/network/inspect.spec.ts
@@ -4,25 +4,25 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { INSPECT_HOSTS_BUTTONS_IN_SECURITY, INSPECT_MODAL } from '../../screens/inspect';
+import { INSPECT_MODAL, INSPECT_NETWORK_BUTTONS_IN_SECURITY } from '../../screens/inspect';
 import { cleanKibana } from '../../tasks/common';
 
 import { closesModal, openStatsAndTables } from '../../tasks/inspect';
 import { loginAndWaitForPage } from '../../tasks/login';
 
-import { HOSTS_URL } from '../../urls/navigation';
+import { NETWORK_URL } from '../../urls/navigation';
 
 describe('Inspect', () => {
-  context('Hosts stats and tables', () => {
+  context('Network stats and tables', () => {
     before(() => {
       cleanKibana();
-      loginAndWaitForPage(HOSTS_URL);
+      loginAndWaitForPage(NETWORK_URL);
     });
     afterEach(() => {
       closesModal();
     });
 
-    INSPECT_HOSTS_BUTTONS_IN_SECURITY.forEach((table) =>
+    INSPECT_NETWORK_BUTTONS_IN_SECURITY.forEach((table) =>
       it(`inspects the ${table.title}`, () => {
         openStatsAndTables(table);
         cy.get(INSPECT_MODAL).should('be.visible');
