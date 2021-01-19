d214984c6ac84b945fff43d6a41cbe4a9ce45d6f
ymao1 <ying.mao@elastic.co>
2021-01-19 18:14:56 -0500

    [Alerting] Fixing broken Alerts view when no Global All Kibana privilege (#88727)
    * Making kibanaFeatures an optional parameter and catching error on plugin start
    
    * Gracefully handle 404 errors when no access to features endpoint
    
    * Adding functional test
    # Conflicts:
    #	x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/home_page.ts

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/home_page.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/home_page.ts
@@ -9,91 +9,109 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const testSubjects = getService('testSubjects');
+  const security = getService('security');
   const pageObjects = getPageObjects(['common', 'triggersActionsUI', 'header']);
   const log = getService('log');
   const browser = getService('browser');
   const alerting = getService('alerting');
 
   describe('Home page', function () {
-    before(async () => {
-      await pageObjects.common.navigateToApp('triggersActions');
-    });
+    describe('Loads the app with limited privileges', () => {
+      before(async () => {
+        await security.testUser.setRoles(['alerts_and_actions_role'], true);
+      });
+      after(async () => {
+        await security.testUser.restoreDefaults();
+      });
 
-    it('Loads the app', async () => {
-      await log.debug('Checking for section heading to say Triggers and Actions.');
-
-      const headingText = await pageObjects.triggersActionsUI.getSectionHeadingText();
-      expect(headingText).to.be('Alerts and Actions');
-    });
-
-    describe('Connectors tab', () => {
-      it('renders the connectors tab', async () => {
-        // Navigate to the connectors tab
-        await pageObjects.triggersActionsUI.changeTabs('connectorsTab');
-
-        await pageObjects.header.waitUntilLoadingHasFinished();
-
-        // Verify url
-        const url = await browser.getCurrentUrl();
-        expect(url).to.contain(`/connectors`);
-
-        // Verify content
-        await testSubjects.existOrFail('actionsList');
+      it('Loads the Alerts page', async () => {
+        await pageObjects.common.navigateToApp('triggersActions');
+        const headingText = await pageObjects.triggersActionsUI.getSectionHeadingText();
+        expect(headingText).to.be('Alerts and Actions');
       });
     });
 
-    describe('Alerts tab', () => {
-      it('renders the alerts tab', async () => {
-        // Navigate to the alerts tab
-        await pageObjects.triggersActionsUI.changeTabs('alertsTab');
-
-        await pageObjects.header.waitUntilLoadingHasFinished();
-
-        // Verify url
-        const url = await browser.getCurrentUrl();
-        expect(url).to.contain(`/alerts`);
-
-        // Verify content
-        await testSubjects.existOrFail('alertsList');
+    describe('Loads the app', () => {
+      before(async () => {
+        await pageObjects.common.navigateToApp('triggersActions');
       });
-
-      it('navigates to an alert details page', async () => {
-        const action = await alerting.actions.createAction({
-          name: `Slack-${Date.now()}`,
-          actionTypeId: '.slack',
-          config: {},
-          secrets: {
-            webhookUrl: 'https://test',
-          },
+  
+      it('Loads the app', async () => {
+        await log.debug('Checking for section heading to say Triggers and Actions.');
+  
+        const headingText = await pageObjects.triggersActionsUI.getSectionHeadingText();
+        expect(headingText).to.be('Alerts and Actions');
+      });
+  
+      describe('Connectors tab', () => {
+        it('renders the connectors tab', async () => {
+          // Navigate to the connectors tab
+          await pageObjects.triggersActionsUI.changeTabs('connectorsTab');
+  
+          await pageObjects.header.waitUntilLoadingHasFinished();
+  
+          // Verify url
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain(`/connectors`);
+  
+          // Verify content
+          await testSubjects.existOrFail('actionsList');
         });
-
-        const alert = await alerting.alerts.createAlwaysFiringWithAction(
-          `test-alert-${Date.now()}`,
-          {
-            id: action.id,
-            group: 'default',
-            params: {
-              message: 'from alert 1s',
-              level: 'warn',
+      });
+  
+      describe('Alerts tab', () => {
+        it('renders the alerts tab', async () => {
+          // Navigate to the alerts tab
+          await pageObjects.triggersActionsUI.changeTabs('alertsTab');
+  
+          await pageObjects.header.waitUntilLoadingHasFinished();
+  
+          // Verify url
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain(`/alerts`);
+  
+          // Verify content
+          await testSubjects.existOrFail('alertsList');
+        });
+  
+        it('navigates to an alert details page', async () => {
+          const action = await alerting.actions.createAction({
+            name: `Slack-${Date.now()}`,
+            actionTypeId: '.slack',
+            config: {},
+            secrets: {
+              webhookUrl: 'https://test',
             },
-          }
-        );
-
-        // refresh to see alert
-        await browser.refresh();
-
-        await pageObjects.header.waitUntilLoadingHasFinished();
-
-        // Verify content
-        await testSubjects.existOrFail('alertsList');
-
-        // click on first alert
-        await pageObjects.triggersActionsUI.clickOnAlertInAlertsList(alert.name);
-
-        // Verify url
-        expect(await browser.getCurrentUrl()).to.contain(`/alert/${alert.id}`);
-
-        await alerting.alerts.deleteAlert(alert.id);
+          });
+  
+          const alert = await alerting.alerts.createAlwaysFiringWithAction(
+            `test-alert-${Date.now()}`,
+            {
+              id: action.id,
+              group: 'default',
+              params: {
+                message: 'from alert 1s',
+                level: 'warn',
+              },
+            }
+          );
+  
+          // refresh to see alert
+          await browser.refresh();
+  
+          await pageObjects.header.waitUntilLoadingHasFinished();
+  
+          // Verify content
+          await testSubjects.existOrFail('alertsList');
+  
+          // click on first alert
+          await pageObjects.triggersActionsUI.clickOnAlertInAlertsList(alert.name);
+  
+          // Verify url
+          expect(await browser.getCurrentUrl()).to.contain(`/alert/${alert.id}`);
+  
+          await alerting.alerts.deleteAlert(alert.id);
+        });
       });
     });
   });
