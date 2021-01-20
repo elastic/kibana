47fcc88ef2ca19fde10086f239f5204f1e086f32
Jonathan Budzenski <jon@budzenski.me>
2021-01-19 20:41:21 -0600

    restructure

--- src/dev/precommit_hook/casing_check_config.js
+++ src/dev/precommit_hook/casing_check_config.js
@@ -40,7 +40,7 @@ export const IGNORE_FILE_GLOBS = [
   'vars/*',
   '.ci/pipeline-library/**/*',
   'packages/kbn-test/jest-preset.js',
-  'Vagrantfile',
+  'test/package/Vagrantfile',
 
   // filename must match language code which requires capital letters
   '**/translations/*.json',
