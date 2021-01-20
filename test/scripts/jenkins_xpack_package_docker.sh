47d690977c76d1e3327c3e6d1a9dad5b9fa1b23d
Jonathan Budzenski <jon@budzenski.me>
2021-01-20 10:41:07 -0600

    add jenkins docker

--- test/scripts/jenkins_xpack_package_docker.sh
+++ test/scripts/jenkins_xpack_package_docker.sh
@@ -2,15 +2,15 @@
 
 node scripts/build --all-platforms --debug --no-oss
 
-vagrant up rpm --no-provision
-vagrant provision rpm
+vagrant up docker --no-provision
+vagrant provision docker
 
 node scripts/es snapshot \
   -E network.bind_host=127.0.0.1,192.168.50.1 \
   -E discovery.type=single-node \
   --license=trial
 
-TEST_KIBANA_URL=http://elastic:changeme@192.168.50.6:5601 \
+TEST_KIBANA_URL=http://elastic:changeme@192.168.50.7:5601 \
 TEST_ES_URL=http://elastic:changeme@192.168.50.1:9200 \
   node scripts/functional_test_runner.js --config x-pack/test/packaging/config.ts
   
