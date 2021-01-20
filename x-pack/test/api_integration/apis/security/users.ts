d0bf4944f502994b6852830f31c84a0a32a1730d
Thom Heymann <thom.heymann@elastic.co>
2021-01-20 16:06:24 +0000

    Simplified API integration tests

--- x-pack/test/api_integration/apis/security/users.ts
+++ x-pack/test/api_integration/apis/security/users.ts
@@ -5,38 +5,17 @@
  */
 
 import expect from '@kbn/expect';
-import { Cookie, cookie } from 'request';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ getService }: FtrProviderContext) {
-  const supertest = getService('supertestWithoutAuth');
+  const supertest = getService('supertest');
   const security = getService('security');
-  const config = getService('config');
   const es = getService('es');
-
-  const kibanaServerConfig = config.get('servers.kibana');
-  const validUsername = kibanaServerConfig.username;
-  const validPassword = kibanaServerConfig.password;
 
   const mockUserName = 'test-user';
   const mockUserPassword = 'test-password';
 
   describe('Users', () => {
-    let sessionCookie: Cookie;
-    before(async () => {
-      const loginResponse = await supertest
-        .post('/internal/security/login')
-        .set('kbn-xsrf', 'xxx')
-        .send({
-          providerType: 'basic',
-          providerName: 'basic',
-          currentURL: '/',
-          params: { username: validUsername, password: validPassword },
-        })
-        .expect(200);
-      sessionCookie = cookie(loginResponse.headers['set-cookie'][0])!;
-    });
-
     beforeEach(async () => {
       await security.user.create(mockUserName, { password: mockUserPassword, roles: [] });
     });
@@ -49,7 +28,6 @@ export default function ({ getService }: FtrProviderContext) {
       await supertest
         .post(`/internal/security/users/${mockUserName}/_disable`)
         .set('kbn-xsrf', 'xxx')
-        .set('Cookie', sessionCookie.cookieString())
         .expect(204);
 
       const { body } = await es.security.getUser({ username: mockUserName });
@@ -60,7 +38,6 @@ export default function ({ getService }: FtrProviderContext) {
       await supertest
         .post(`/internal/security/users/${mockUserName}/_enable`)
         .set('kbn-xsrf', 'xxx')
-        .set('Cookie', sessionCookie.cookieString())
         .expect(204);
 
       const { body } = await es.security.getUser({ username: mockUserName });
