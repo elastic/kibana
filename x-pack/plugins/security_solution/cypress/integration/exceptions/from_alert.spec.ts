17e49f169eef93427ca76f8b956c31e72c9894ba
MadameSheema <snootchie.boochies@gmail.com>
2021-01-20 13:25:24 +0100

    [Security Solution] Cypress reorganization (#88687)
    * moves tests to different folders
    
    * cases test files renaming
    
    * renames detection alerts test files
    
    * renames detection rules test files
    
    * renames exceptions test files
    
    * splits inspect test files
    
    * renames timeline templates file tests
    
    * renames timelines file tests
    
    * renames url file tests
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/security_solution/cypress/integration/exceptions/from_alert.spec.ts
@@ -0,0 +1,96 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import { exception } from '../../objects/exception';
+import { newRule } from '../../objects/rule';
+
+import { ALERTS_COUNT, NUMBER_OF_ALERTS } from '../../screens/alerts';
+import { RULE_STATUS } from '../../screens/create_new_rule';
+
+import {
+  addExceptionFromFirstAlert,
+  goToClosedAlerts,
+  goToManageAlertsDetectionRules,
+  goToOpenedAlerts,
+  waitForAlertsIndexToBeCreated,
+} from '../../tasks/alerts';
+import { createCustomRule } from '../../tasks/api_calls/rules';
+import { goToRuleDetails } from '../../tasks/alerts_detection_rules';
+import { waitForAlertsToPopulate } from '../../tasks/create_new_rule';
+import { esArchiverLoad, esArchiverUnload } from '../../tasks/es_archiver';
+import { loginAndWaitForPageWithoutDateRange } from '../../tasks/login';
+import {
+  activatesRule,
+  addsException,
+  goToAlertsTab,
+  goToExceptionsTab,
+  removeException,
+  waitForTheRuleToBeExecuted,
+} from '../../tasks/rule_details';
+import { refreshPage } from '../../tasks/security_header';
+
+import { DETECTIONS_URL } from '../../urls/navigation';
+import { cleanKibana } from '../../tasks/common';
+
+describe('From alert', () => {
+  const NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS = '1';
+
+  beforeEach(() => {
+    cleanKibana();
+    loginAndWaitForPageWithoutDateRange(DETECTIONS_URL);
+    waitForAlertsIndexToBeCreated();
+    createCustomRule(newRule);
+    goToManageAlertsDetectionRules();
+    goToRuleDetails();
+
+    cy.get(RULE_STATUS).should('have.text', '—');
+
+    esArchiverLoad('auditbeat_for_exceptions');
+    activatesRule();
+    waitForTheRuleToBeExecuted();
+    waitForAlertsToPopulate();
+    refreshPage();
+
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS);
+  });
+
+  afterEach(() => {
+    esArchiverUnload('auditbeat_for_exceptions');
+    esArchiverUnload('auditbeat_for_exceptions2');
+  });
+
+  it('Creates an exception and deletes it', () => {
+    addExceptionFromFirstAlert();
+    addsException(exception);
+    esArchiverLoad('auditbeat_for_exceptions2');
+
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', '0');
+
+    goToClosedAlerts();
+    refreshPage();
+
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS);
+
+    goToOpenedAlerts();
+    waitForTheRuleToBeExecuted();
+    refreshPage();
+
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', '0');
+
+    goToExceptionsTab();
+    removeException();
+    goToAlertsTab();
+    waitForTheRuleToBeExecuted();
+    waitForAlertsToPopulate();
+    refreshPage();
+
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS);
+  });
+});
