a99819c1f04d5649f3f522897094764f188264ae
Liza K <liza.katz@elastic.co>
2021-01-21 15:24:42 +0200

    oops

--- test/api_integration/config.js
+++ test/api_integration/config.js
@@ -26,7 +26,6 @@ export default async function ({ readConfigFile }) {
         ...functionalConfig.get('kbnTestServer.serverArgs'),
         '--elasticsearch.healthCheck.delay=3600000',
         '--server.xsrf.disableProtection=true',
-        '--data.search.aggs.shardDelay.enabled=true',
         '--server.compression.referrerWhitelist=["some-host.com"]',
       ],
     },
