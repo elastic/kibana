60fafef5ce5dfb75384c789ad86935ed19465d83
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2021-01-25 15:10:38 +0200

    Remove navigation from Discover to Visualize

--- test/functional/apps/discover/index.ts
+++ test/functional/apps/discover/index.ts
@@ -27,7 +27,6 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./_discover'));
     loadTestFile(require.resolve('./_discover_histogram'));
     loadTestFile(require.resolve('./_doc_table'));
-    loadTestFile(require.resolve('./_field_visualize'));
     loadTestFile(require.resolve('./_filter_editor'));
     loadTestFile(require.resolve('./_errors'));
     loadTestFile(require.resolve('./_field_data'));
