a00c508af80cceaff46af67ca4861b3aee57e67e
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2021-01-25 11:51:48 -0800

    adding the false parameter

--- x-pack/test/functional/apps/watcher/watcher_test.js
+++ x-pack/test/functional/apps/watcher/watcher_test.js
@@ -23,7 +23,7 @@ export default function ({ getService, getPageObjects }) {
     before('initialize tests', async () => {
       // There may be system watches if monitoring was previously enabled
       // These cannot be deleted via the UI, so we need to delete via the API
-      await security.testUser.setRoles(['kibana_admin', 'watcher_admin']);
+      await security.testUser.setRoles(['kibana_admin', 'watcher_admin'], false);
       const watches = await esSupertest.get('/.watches/_search');
 
       if (watches.status === 200) {
