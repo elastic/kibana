da5aeace60a07a5ec53d92b59c1feba09e0a7486
Nathan Reese <reese.nathan@gmail.com>
2021-01-26 12:11:39 -0700

    [Maps] Implement searchSessionId in MapEmbeddable

--- x-pack/plugins/maps/public/embeddable/map_embeddable.tsx
+++ x-pack/plugins/maps/public/embeddable/map_embeddable.tsx
@@ -82,6 +82,7 @@ export class MapEmbeddable
   private _prevQuery?: Query;
   private _prevRefreshConfig?: RefreshInterval;
   private _prevFilters?: Filter[];
+  private _prevSearchSessionId?: string;
   private _domNode?: HTMLElement;
   private _unsubscribeFromStore?: Unsubscribe;
   private _isInitialized = false;
@@ -135,14 +136,13 @@ export class MapEmbeddable
       timeRange: this.input.timeRange,
       filters: this.input.filters,
       forceRefresh: false,
+      searchSessionId: this.input.searchSessionId,
     });
     if (this.input.refreshConfig) {
       this._dispatchSetRefreshConfig(this.input.refreshConfig);
     }
 
-    this._unsubscribeFromStore = this._savedMap.getStore().subscribe(() => {
-      this._handleStoreChanges();
-    });
+    this._subscription = this.getUpdated$().subscribe(() => this.onUpdate());
   }
 
   private async _initializeOutput() {
@@ -201,25 +201,24 @@ export class MapEmbeddable
     return getInspectorAdapters(this._savedMap.getStore().getState());
   }
 
-  onContainerStateChanged(containerState: MapEmbeddableInput) {
+  onUpdate() {
     if (
-      !_.isEqual(containerState.timeRange, this._prevTimeRange) ||
-      !_.isEqual(containerState.query, this._prevQuery) ||
-      !esFilters.onlyDisabledFiltersChanged(containerState.filters, this._prevFilters)
+      !_.isEqual(this.input.timeRange, this._prevTimeRange) ||
+      !_.isEqual(this.input.query, this._prevQuery) ||
+      !esFilters.onlyDisabledFiltersChanged(this.input.filters, this._prevFilters) ||
+      this.input.searchSessionId !== this._prevSearchSessionId
     ) {
       this._dispatchSetQuery({
-        query: containerState.query,
-        timeRange: containerState.timeRange,
-        filters: containerState.filters,
+        query: this.input.query,
+        timeRange: this.input.timeRange,
+        filters: this.input.filters,
         forceRefresh: false,
+        searchSessionId: this.input.searchSessionId,
       });
     }
 
-    if (
-      containerState.refreshConfig &&
-      !_.isEqual(containerState.refreshConfig, this._prevRefreshConfig)
-    ) {
-      this._dispatchSetRefreshConfig(containerState.refreshConfig);
+    if (this.input.refreshConfig && !_.isEqual(this.input.refreshConfig, this._prevRefreshConfig)) {
+      this._dispatchSetRefreshConfig(this.input.refreshConfig);
     }
   }
 
@@ -228,21 +227,25 @@ export class MapEmbeddable
     timeRange,
     filters = [],
     forceRefresh,
+    searchSessionId,
   }: {
     query?: Query;
     timeRange?: TimeRange;
     filters?: Filter[];
     forceRefresh: boolean;
+    searchSessionId?: string;
   }) {
     this._prevTimeRange = timeRange;
     this._prevQuery = query;
     this._prevFilters = filters;
+    this._prevSearchSessionId = searchSessionId;
     this._savedMap.getStore().dispatch<any>(
       setQuery({
         filters: filters.filter((filter) => !filter.meta.disabled),
         query,
         timeFilters: timeRange,
         forceRefresh,
+        searchSessionId,
       })
     );
   }
@@ -380,10 +383,11 @@ export class MapEmbeddable
 
   reload() {
     this._dispatchSetQuery({
-      query: this._prevQuery,
-      timeRange: this._prevTimeRange,
-      filters: this._prevFilters ?? [],
+      query: this.input.query,
+      timeRange: this.input.timeRange,
+      filters: this.input.filters,
       forceRefresh: true,
+      searchSessionId: this.input.searchSessionId,
     });
   }
 
