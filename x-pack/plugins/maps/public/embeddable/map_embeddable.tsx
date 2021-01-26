6dff0d0a1a005188a1838238f72164e9065cbb22
Nathan Reese <reese.nathan@gmail.com>
2021-01-26 12:20:06 -0700

    update method name

--- x-pack/plugins/maps/public/embeddable/map_embeddable.tsx
+++ x-pack/plugins/maps/public/embeddable/map_embeddable.tsx
@@ -100,9 +100,7 @@ export class MapEmbeddable
 
     this._savedMap = new SavedMap({ mapEmbeddableInput: initialInput });
     this._initializeSaveMap();
-    this._subscription = this.getUpdated$().subscribe(() =>
-      this.onContainerStateChanged(this.input)
-    );
+    this._subscription = this.getUpdated$().subscribe(() => this.onUpdate(this.input));
   }
 
   private async _initializeSaveMap() {
