e705f8435046b37be5850f51c7e3f63eb58ed8a0
Tiago Costa <tiagoffcc@hotmail.com>
2021-01-27 22:39:36 +0000

    refact(NA): simplify install logic

--- packages/kbn-pm/src/utils/bazel/install_tools.ts
+++ packages/kbn-pm/src/utils/bazel/install_tools.ts
@@ -11,55 +11,39 @@ import { spawn } from '../child_process';
 import { readFile } from '../fs';
 import { log } from '../log';
 
-export interface BazelToolsVersion {
-  bazelisk: string;
-  bazel: string;
-}
-
 async function readBazelToolsVersionFile(repoRootPath: string, versionFilename: string) {
   const version = (await readFile(resolve(repoRootPath, versionFilename)))
     .toString()
     .split('\n')[0];
 
   if (!version) {
-    throw new Error(`${versionFilename} file do not contain any version set`);
+    throw new Error(
+      `[bazel_tools] Failed on reading bazel tools versions\n ${versionFilename} file do not contain any version set`
+    );
   }
 
   return version;
 }
 
-async function readBazelToolsVersion(repoRootPath: string) {
-  try {
-    const bazeliskVersion = await readBazelToolsVersionFile(repoRootPath, '.bazeliskversion');
-    const bazelVersion = await readBazelToolsVersionFile(repoRootPath, '.bazelversion');
-
-    return {
-      bazelisk: bazeliskVersion,
-      bazel: bazelVersion,
-    } as BazelToolsVersion;
-  } catch (e) {
-    throw new Error(`[bazel_tools] Failed on reading bazel tools versions\n ${e}`);
-  }
-}
-
 export async function installBazelTools(repoRootPath: string) {
   log.debug(`[bazel_tools] reading bazel tools versions from version files`);
-  const bazelToolsVersions = await readBazelToolsVersion(repoRootPath);
+  const bazeliskVersion = await readBazelToolsVersionFile(repoRootPath, '.bazeliskversion');
+  const bazelVersion = await readBazelToolsVersionFile(repoRootPath, '.bazelversion');
 
   // Check what globals are installed
   log.debug(`[bazel_tools] verify if bazelisk is installed`);
   const { stdout } = await spawn('yarn', ['global', 'list'], { stdio: 'pipe' });
 
   // Install bazelisk if not installed
-  if (!stdout.includes(`@bazel/bazelisk@${bazelToolsVersions.bazelisk}`)) {
+  if (!stdout.includes(`@bazel/bazelisk@${bazeliskVersion}`)) {
     log.info(`[bazel_tools] installing Bazel tools`);
 
     log.debug(
-      `[bazel_tools] bazelisk is not installed. Installing @bazel/bazelisk@${bazelToolsVersions.bazelisk} and bazel@${bazelToolsVersions.bazel}`
+      `[bazel_tools] bazelisk is not installed. Installing @bazel/bazelisk@${bazeliskVersion} and bazel@${bazelVersion}`
     );
-    await spawn('yarn', ['global', 'add', `@bazel/bazelisk@${bazelToolsVersions.bazelisk}`], {
+    await spawn('yarn', ['global', 'add', `@bazel/bazelisk@${bazeliskVersion}`], {
       env: {
-        USE_BAZEL_VERSION: bazelToolsVersions.bazel,
+        USE_BAZEL_VERSION: bazelVersion,
       },
       stdio: 'pipe',
     });
