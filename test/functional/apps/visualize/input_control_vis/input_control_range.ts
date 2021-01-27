b82616dbee8ab8e050d6d6a1bd585417ff601a6e
Rudolf Meijering <skaapgif@gmail.com>
2021-01-27 21:52:50 +0100

    Fix oss security functional tests

--- test/functional/apps/visualize/input_control_vis/input_control_range.ts
+++ test/functional/apps/visualize/input_control_vis/input_control_range.ts
@@ -12,7 +12,6 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
-  const kibanaServer = getService('kibanaServer');
   const find = getService('find');
   const security = getService('security');
   const { visualize, visEditor } = getPageObjects(['visualize', 'visEditor']);
