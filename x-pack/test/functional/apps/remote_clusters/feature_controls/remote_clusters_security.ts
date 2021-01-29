6d406dcf503fbff6e13387cb216b8ef52bdee88f
Larry Gregory <larry.gregory@elastic.co>
2021-01-29 09:29:45 -0500

    Reenable skipped CCR & Remote Cluster tests

--- x-pack/test/functional/apps/remote_clusters/feature_controls/remote_clusters_security.ts
+++ x-pack/test/functional/apps/remote_clusters/feature_controls/remote_clusters_security.ts
@@ -13,8 +13,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const appsMenu = getService('appsMenu');
   const managementMenu = getService('managementMenu');
 
-  // FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/89181
-  describe.skip('security', () => {
+  describe('security', () => {
     before(async () => {
       await esArchiver.load('empty_kibana');
       await PageObjects.common.navigateToApp('home');
