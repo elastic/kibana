c00b79ba6591a1a64b60c96536f120d1dc6fc4ef
ymao1 <ying.mao@elastic.co>
2021-01-29 12:55:06 -0500

    [7.x] [Alerting] Search alert (#88528) (#89711)
    * [Alerting] Search alert (#88528)
    
    * Adding es query alert type to server with commented out executor
    
    * Adding skeleton es query alert to client with JSON editor. Pulled out index popoover into component for reuse between index threshold and es query alert types
    
    * Implementing alert executor that performs query and matches condition against doc count
    
    * Added tests for server side alert type
    
    * Updated alert executor to de-duplicate matches and create instance for every document if threshold is not defined
    
    * Moving more index popover code out of index threshold and es query expression components
    
    * Ability to remove threshold condition from es query alert
    
    * Validation tests
    
    * Adding ability to test out query. Need to add error handling and it looks ugly
    
    * Fixing bug with creating alert with threshold and i18n
    
    * wip
    
    * Fixing tests
    
    * Simplifying executor logic to only handle threshold and store hits in action context
    
    * Adding functional test for es query alert
    
    * Types
    
    * Adding functional test for query testing
    
    * Fixing unit test
    
    * Adding link to ES docs. Cleaning up logger statements
    
    * Adding docs
    
    * Updating docs based on feedback
    
    * PR fixes
    
    * Using ES client typings
    
    * Fixing unit test
    
    * Fixing copy based on comments
    
    * Fixing copy based on comments
    
    * Fixing bug in index select popover
    
    * Fixing unit tests
    
    * Making track_total_hits configurable
    
    * Fixing functional test
    
    * PR fixes
    
    * Added unit test
    
    * Removing unused import
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    * Fixing typings
    
    * Fixing query to use ISO string instead of millis
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/alerting_api_integration/spaces_only/tests/alerting/builtin_alert_types/es_query/index.ts
+++ x-pack/test/alerting_api_integration/spaces_only/tests/alerting/builtin_alert_types/es_query/index.ts
@@ -4,12 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { FtrProviderContext } from '../../../../common/ftr_provider_context';
+import { FtrProviderContext } from '../../../../../common/ftr_provider_context';
 
 // eslint-disable-next-line import/no-default-export
 export default function alertingTests({ loadTestFile }: FtrProviderContext) {
-  describe('builtin alertTypes', () => {
-    loadTestFile(require.resolve('./index_threshold'));
-    loadTestFile(require.resolve('./es_query'));
+  describe('es_query', () => {
+    loadTestFile(require.resolve('./alert'));
   });
 }
