6287075bdac0fecb38c0ae75abe9a60435a265ca
Dima Arnautov <dmitrii.arnautov@elastic.co>
2021-01-29 10:00:08 +0100

    [ML] update SeveritySelector component

--- x-pack/plugins/ml/public/alerting/severity_selector.tsx
+++ x-pack/plugins/ml/public/alerting/severity_selector.tsx
@@ -4,34 +4,51 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import React, { FC, useMemo } from 'react';
+import React, { FC } from 'react';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { EuiFormRow, EuiSuperSelect } from '@elastic/eui';
-import {
-  getSeverityOptions,
-  SEVERITY_OPTIONS,
-} from '../application/components/controls/select_severity/select_severity';
+import { EuiFormRow, EuiRange, EuiRangeProps } from '@elastic/eui';
+import { SEVERITY_OPTIONS } from '../application/components/controls/select_severity/select_severity';
+import { ANOMALY_THRESHOLD } from '../../common';
 
 export interface SeveritySelectorProps {
   value: number | undefined;
   onChange: (value: number) => void;
 }
 
-export const SeveritySelector: FC<SeveritySelectorProps> = ({ value, onChange }) => {
-  const options = useMemo(() => getSeverityOptions(), []);
+const MAX_ANOMALY_SCORE = 100;
 
-  const valueOfSelected = useMemo(
-    () => SEVERITY_OPTIONS.find((v) => v.val === value)?.display ?? '',
-    [value]
-  );
+export const SeveritySelector: FC<SeveritySelectorProps> = React.memo(({ value, onChange }) => {
+  const levels: EuiRangeProps['levels'] = [
+    {
+      min: ANOMALY_THRESHOLD.LOW,
+      max: ANOMALY_THRESHOLD.MINOR - 1,
+      color: 'success',
+    },
+    {
+      min: ANOMALY_THRESHOLD.MINOR,
+      max: ANOMALY_THRESHOLD.MAJOR - 1,
+      color: 'primary',
+    },
+    {
+      min: ANOMALY_THRESHOLD.MAJOR,
+      max: ANOMALY_THRESHOLD.CRITICAL,
+      color: 'warning',
+    },
+    {
+      min: ANOMALY_THRESHOLD.CRITICAL,
+      max: MAX_ANOMALY_SCORE,
+      color: 'danger',
+    },
+  ];
 
-  const onValueChange = (selectedValue: string) => {
-    const result = SEVERITY_OPTIONS.find((v) => v.display === selectedValue);
-    onChange(result!.val);
-  };
+  const toggleButtons = SEVERITY_OPTIONS.map((v) => ({
+    value: v.val,
+    label: v.display,
+  }));
 
   return (
     <EuiFormRow
+      fullWidth
       label={
         <FormattedMessage
           id="xpack.ml.severitySelector.formControlLabel"
@@ -39,12 +56,24 @@ export const SeveritySelector: FC<SeveritySelectorProps> = ({ value, onChange })
         />
       }
     >
-      <EuiSuperSelect
-        hasDividers
-        options={options}
-        valueOfSelected={valueOfSelected}
-        onChange={onValueChange}
-      />
+      <>
+        <EuiRange
+          fullWidth
+          min={ANOMALY_THRESHOLD.LOW}
+          max={MAX_ANOMALY_SCORE}
+          value={value ?? ANOMALY_THRESHOLD.LOW}
+          onChange={(e) => {
+            // @ts-ignore Property 'value' does not exist on type 'EventTarget' | (EventTarget & HTMLInputElement)
+            onChange(e.target.value);
+          }}
+          showLabels
+          showValue
+          aria-label="An example of EuiRange with showValue prop"
+          showTicks
+          ticks={toggleButtons}
+          levels={levels}
+        />
+      </>
     </EuiFormRow>
   );
-};
+});
