61a51b568481abfba41f71781d24acfd4f65c7ee
Jean-Louis Leysens <jloleysens@gmail.com>
2021-02-01 11:14:46 +0100

    [ILM] New copy for rollover and small refactor for timeline (#89422)
    * refactor timeline and relative ms calculation logic for easier use outside of edit_policy section
    
    * further refactor, move child component to own file in timeline, and clean up public API for relative timing calculation
    
    * added copy to call out variation in timing (slop) introduced by rollover
    
    * use separate copy for timeline
    
    * remove unused import
    
    * fix unresolved merge
    
    * implement copy feedback
    
    * added component integration for showing/hiding hot phase icon on timeline
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/index_lifecycle_management/public/application/sections/edit_policy/components/timeline/components/index.ts
+++ x-pack/plugins/index_lifecycle_management/public/application/sections/edit_policy/components/timeline/components/index.ts
@@ -3,4 +3,5 @@
  * or more contributor license agreements. Licensed under the Elastic License;
  * you may not use this file except in compliance with the Elastic License.
  */
-export { Timeline } from './timeline.container';
+
+export { TimelinePhaseText } from './timeline_phase_text';
