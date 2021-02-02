77bfde8a991c0b78321f5d94c74bd1ddb443e85f
Nathan L Smith <smith@nlsmith.com>
2021-02-02 15:02:10 -0600

    Updates to select

--- x-pack/plugins/apm/public/components/shared/transaction_type_select.tsx
+++ x-pack/plugins/apm/public/components/shared/transaction_type_select.tsx
@@ -6,11 +6,19 @@
 
 import { EuiSelect } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 import { useApmServiceContext } from '../../context/apm_service/use_apm_service_context';
 import { useUrlParams } from '../../context/url_params_context/use_url_params';
 import { fromQuery, toQuery } from './Links/url_helpers';
+
+// The default transaction type (for non-RUM services) is "request". Set the
+// min-width on here to the width when "request" is loaded so it doesn't start
+// out collapsed and change its width when the list of transaction types is loaded.
+const EuiSelectWithWidth = styled(EuiSelect)`
+  min-width: 157px;
+`;
 
 export function TransactionTypeSelect() {
   const { transactionTypes } = useApmServiceContext();
@@ -19,23 +27,26 @@ export function TransactionTypeSelect() {
     urlParams: { transactionType },
   } = useUrlParams();
 
-  function handleChange(event: FormEvent<HTMLSelectElement>) {
-    const selectedTransactionType = event.currentTarget.value;
-    const newLocation = {
-      ...history.location,
-      search: fromQuery({
-        ...toQuery(history.location.search),
-        transactionType: selectedTransactionType,
-      }),
-    };
-    history.push(newLocation);
-  }
+  const handleChange = useCallback(
+    (event: FormEvent<HTMLSelectElement>) => {
+      const selectedTransactionType = event.currentTarget.value;
+      const newLocation = {
+        ...history.location,
+        search: fromQuery({
+          ...toQuery(history.location.search),
+          transactionType: selectedTransactionType,
+        }),
+      };
+      history.push(newLocation);
+    },
+    [history]
+  );
 
   const options = transactionTypes.map((t) => ({ text: t, value: t }));
 
   return (
     <>
-      <EuiSelect
+      <EuiSelectWithWidth
         onChange={handleChange}
         options={options}
         prepend={i18n.translate('xpack.apm.transactionTypeSelectLabel', {
