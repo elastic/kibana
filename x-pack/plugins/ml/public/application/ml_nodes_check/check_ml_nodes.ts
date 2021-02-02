9adf7d829472f14e56f923e227f0d6fe0befdd91
James Gowdy <jgowdy@elastic.co>
2021-02-02 17:33:19 +0000

    [ML] Fixing cloud behaviour when ML nodes are lazily assigned

--- x-pack/plugins/ml/public/application/ml_nodes_check/check_ml_nodes.ts
+++ x-pack/plugins/ml/public/application/ml_nodes_check/check_ml_nodes.ts
@@ -5,14 +5,16 @@
  */
 
 import { ml } from '../services/ml_api_service';
+import { MlNodeCount } from '../../../common/types/ml_server_info';
 
 let mlNodeCount: number = 0;
+let lazyMlNodeCount: number = 0;
 let userHasPermissionToViewMlNodeCount: boolean = false;
 
 export async function checkMlNodesAvailable(redirectToJobsManagementPage: () => Promise<void>) {
   try {
-    const nodes = await getMlNodeCount();
-    if (nodes.count !== undefined && nodes.count > 0) {
+    const { count, lazyNodeCount } = await getMlNodeCount();
+    if (count > 0 || lazyNodeCount > 0) {
       Promise.resolve();
     } else {
       throw Error('Cannot load count of ML nodes');
@@ -25,23 +27,24 @@ export async function checkMlNodesAvailable(redirectToJobsManagementPage: () => 
   }
 }
 
-export async function getMlNodeCount() {
+export async function getMlNodeCount(): Promise<MlNodeCount> {
   try {
     const nodes = await ml.mlNodeCount();
     mlNodeCount = nodes.count;
+    lazyMlNodeCount = nodes.lazyNodeCount;
     userHasPermissionToViewMlNodeCount = true;
-    return Promise.resolve(nodes);
+    return nodes;
   } catch (error) {
     mlNodeCount = 0;
     if (error.statusCode === 403) {
       userHasPermissionToViewMlNodeCount = false;
     }
-    return Promise.resolve({ count: 0 });
+    return { count: 0, lazyNodeCount: 0 };
   }
 }
 
 export function mlNodesAvailable() {
-  return mlNodeCount !== 0;
+  return mlNodeCount !== 0 || lazyMlNodeCount !== 0;
 }
 
 export function permissionToViewMlNodeCount() {
