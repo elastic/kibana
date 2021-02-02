d53da10e1f9b5363231610b296e9af803fe0f8be
Nathan Reese <reese.nathan@gmail.com>
2021-02-02 08:13:58 -0700

    replace getTile unit test with integration test

--- x-pack/test/api_integration/apis/maps/get_grid_tile.js
+++ x-pack/test/api_integration/apis/maps/get_grid_tile.js
@@ -19,7 +19,15 @@ export default function ({ getService }) {
     it('should return vector tile containing cluster features', async () => {
       const resp = await supertest
         .get(
-          '/api/maps/mvt/getGridTile?x=2&y=3&z=3&geometryFieldName=geo.coordinates&index=logstash-*&requestBody=(_source:(excludes:!()),aggs:(gridSplit:(aggs:(avg_of_bytes:(avg:(field:bytes)),gridCentroid:(geo_centroid:(field:geo.coordinates))),geotile_grid:(bounds:!n,field:geo.coordinates,precision:!n,shard_size:65535,size:65535))),fields:!((field:%27@timestamp%27,format:date_time),(field:%27relatedContent.article:modified_time%27,format:date_time),(field:%27relatedContent.article:published_time%27,format:date_time),(field:utc_time,format:date_time)),query:(bool:(filter:!((match_all:()),(range:(%27@timestamp%27:(format:strict_date_optional_time,gte:%272015-09-20T00:00:00.000Z%27,lte:%272015-09-20T01:00:00.000Z%27)))),must:!(),must_not:!(),should:!())),runtime_mappings:(),script_fields:(hour_of_day:(script:(lang:painless,source:%27doc[!%27@timestamp!%27].value.getHour()%27))),size:0,stored_fields:!(%27*%27))&requestType=point&geoFieldType=geo_point'
+          `/api/maps/mvt/getGridTile\
+?x=2\
+&y=3\
+&z=3\
+&geometryFieldName=geo.coordinates\
+&index=logstash-*\
+&requestBody=(_source:(excludes:!()),aggs:(gridSplit:(aggs:(avg_of_bytes:(avg:(field:bytes)),gridCentroid:(geo_centroid:(field:geo.coordinates))),geotile_grid:(bounds:!n,field:geo.coordinates,precision:!n,shard_size:65535,size:65535))),fields:!((field:%27@timestamp%27,format:date_time),(field:%27relatedContent.article:modified_time%27,format:date_time),(field:%27relatedContent.article:published_time%27,format:date_time),(field:utc_time,format:date_time)),query:(bool:(filter:!((match_all:()),(range:(%27@timestamp%27:(format:strict_date_optional_time,gte:%272015-09-20T00:00:00.000Z%27,lte:%272015-09-20T01:00:00.000Z%27)))),must:!(),must_not:!(),should:!())),runtime_mappings:(),script_fields:(hour_of_day:(script:(lang:painless,source:%27doc[!%27@timestamp!%27].value.getHour()%27))),size:0,stored_fields:!(%27*%27))\
+&requestType=point\
+&geoFieldType=geo_point`
         )
         .set('kbn-xsrf', 'kibana')
         .responseType('blob')
@@ -32,17 +40,22 @@ export default function ({ getService }) {
       expect(clusterFeature.type).to.be(1);
       expect(clusterFeature.extent).to.be(4096);
       expect(clusterFeature.id).to.be(undefined);
-      expect(clusterFeature.properties.doc_count).to.be(1);
-      expect(clusterFeature.properties.avg_of_bytes).to.be(9252);
-      const geometry = clusterFeature.loadGeometry();
-      expect(geometry[0][0].x).to.be(87);
-      expect(geometry[0][0].y).to.be(667);
+      expect(clusterFeature.properties).to.eql({ doc_count: 1, avg_of_bytes: 9252 });
+      expect(clusterFeature.loadGeometry()).to.eql([[{ x: 87, y: 667 }]]);
     });
 
     it('should return vector tile containing grid features', async () => {
       const resp = await supertest
         .get(
-          '/api/maps/mvt/getGridTile?x=2&y=3&z=3&geometryFieldName=geo.coordinates&index=logstash-*&requestBody=(_source:(excludes:!()),aggs:(gridSplit:(aggs:(avg_of_bytes:(avg:(field:bytes)),gridCentroid:(geo_centroid:(field:geo.coordinates))),geotile_grid:(bounds:!n,field:geo.coordinates,precision:!n,shard_size:65535,size:65535))),fields:!((field:%27@timestamp%27,format:date_time),(field:%27relatedContent.article:modified_time%27,format:date_time),(field:%27relatedContent.article:published_time%27,format:date_time),(field:utc_time,format:date_time)),query:(bool:(filter:!((match_all:()),(range:(%27@timestamp%27:(format:strict_date_optional_time,gte:%272015-09-20T00:00:00.000Z%27,lte:%272015-09-20T01:00:00.000Z%27)))),must:!(),must_not:!(),should:!())),runtime_mappings:(),script_fields:(hour_of_day:(script:(lang:painless,source:%27doc[!%27@timestamp!%27].value.getHour()%27))),size:0,stored_fields:!(%27*%27))&requestType=grid&geoFieldType=geo_point'
+          `/api/maps/mvt/getGridTile\
+?x=2\
+&y=3\
+&z=3\
+&geometryFieldName=geo.coordinates\
+&index=logstash-*\
+&requestBody=(_source:(excludes:!()),aggs:(gridSplit:(aggs:(avg_of_bytes:(avg:(field:bytes)),gridCentroid:(geo_centroid:(field:geo.coordinates))),geotile_grid:(bounds:!n,field:geo.coordinates,precision:!n,shard_size:65535,size:65535))),fields:!((field:%27@timestamp%27,format:date_time),(field:%27relatedContent.article:modified_time%27,format:date_time),(field:%27relatedContent.article:published_time%27,format:date_time),(field:utc_time,format:date_time)),query:(bool:(filter:!((match_all:()),(range:(%27@timestamp%27:(format:strict_date_optional_time,gte:%272015-09-20T00:00:00.000Z%27,lte:%272015-09-20T01:00:00.000Z%27)))),must:!(),must_not:!(),should:!())),runtime_mappings:(),script_fields:(hour_of_day:(script:(lang:painless,source:%27doc[!%27@timestamp!%27].value.getHour()%27))),size:0,stored_fields:!(%27*%27))\
+&requestType=grid\
+&geoFieldType=geo_point`
         )
         .set('kbn-xsrf', 'kibana')
         .responseType('blob')
@@ -56,28 +69,27 @@ export default function ({ getService }) {
       expect(gridFeature.type).to.be(3);
       expect(gridFeature.extent).to.be(4096);
       expect(gridFeature.id).to.be(undefined);
-      expect(gridFeature.properties.doc_count).to.be(1);
-      expect(gridFeature.properties.avg_of_bytes).to.be(9252);
-      const gridGeometry = gridFeature.loadGeometry();
-      expect(gridGeometry[0][0].x).to.be(96);
-      expect(gridGeometry[0][0].y).to.be(640);
-      expect(gridGeometry[0][1].x).to.be(96);
-      expect(gridGeometry[0][1].y).to.be(672);
-      expect(gridGeometry[0][2].x).to.be(64);
-      expect(gridGeometry[0][2].y).to.be(672);
-      expect(gridGeometry[0][3].x).to.be(64);
-      expect(gridGeometry[0][3].y).to.be(640);
+      expect(gridFeature.properties).to.eql({ doc_count: 1, avg_of_bytes: 9252 });
+      expect(gridFeature.loadGeometry()).to.eql([
+        [
+          { x: 96, y: 640 },
+          { x: 96, y: 672 },
+          { x: 64, y: 672 },
+          { x: 64, y: 640 },
+          { x: 96, y: 640 },
+        ],
+      ]);
 
       const clusterFeature = layer.feature(1);
       expect(clusterFeature.type).to.be(1);
       expect(clusterFeature.extent).to.be(4096);
       expect(clusterFeature.id).to.be(undefined);
-      expect(clusterFeature.properties.doc_count).to.be(1);
-      expect(clusterFeature.properties.avg_of_bytes).to.be(9252);
-      expect(clusterFeature.properties[KBN_IS_CENTROID_FEATURE]).to.be(true);
-      const clusterGeometry = clusterFeature.loadGeometry();
-      expect(clusterGeometry[0][0].x).to.be(80);
-      expect(clusterGeometry[0][0].y).to.be(656);
+      expect(clusterFeature.properties).to.eql({
+        doc_count: 1,
+        avg_of_bytes: 9252,
+        [KBN_IS_CENTROID_FEATURE]: true,
+      });
+      expect(clusterFeature.loadGeometry()).to.eql([[{ x: 80, y: 656 }]]);
     });
   });
 }
--- x-pack/test/api_integration/apis/maps/get_tile.js
+++ x-pack/test/api_integration/apis/maps/get_tile.js
@@ -4,26 +4,81 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
+import { VectorTile } from '@mapbox/vector-tile';
+import Protobuf from 'pbf';
+import expect from '@kbn/expect';
+import { MVT_SOURCE_LAYER_NAME } from '../../../../plugins/maps/common/constants';
+
 export default function ({ getService }) {
   const supertest = getService('supertest');
 
   describe('getTile', () => {
-    it('should validate params', async () => {
-      await supertest
+    it('should return vector tile containing document', async () => {
+      const resp = await supertest
         .get(
-          `/api/maps/mvt/getTile?x=15&y=11&z=5&geometryFieldName=coordinates&index=logstash*&requestBody=(_source:(includes:!(coordinates)),docvalue_fields:!(),query:(bool:(filter:!((match_all:())),must:!(),must_not:!(),should:!())),script_fields:(),size:10000,stored_fields:!(coordinates))&geoFieldType=geo_point`
+          `/api/maps/mvt/getTile\
+?x=1\
+&y=1\
+&z=2\
+&geometryFieldName=geo.coordinates\
+&index=logstash-*\
+&requestBody=(_source:!f,docvalue_fields:!(bytes,geo.coordinates,machine.os.raw),query:(bool:(filter:!((match_all:()),(range:(%27@timestamp%27:(format:strict_date_optional_time,gte:%272015-09-20T00:00:00.000Z%27,lte:%272015-09-20T01:00:00.000Z%27)))),must:!(),must_not:!(),should:!())),runtime_mappings:(),script_fields:(),size:10000,stored_fields:!(bytes,geo.coordinates,machine.os.raw))\
+&geoFieldType=geo_point`
         )
         .set('kbn-xsrf', 'kibana')
+        .responseType('blob')
         .expect(200);
+
+      const jsonTile = new VectorTile(new Protobuf(resp.body));
+      const layer = jsonTile.layers[MVT_SOURCE_LAYER_NAME];
+      expect(layer.length).to.be(2);
+      const feature = layer.feature(0);
+      expect(feature.type).to.be(1);
+      expect(feature.extent).to.be(4096);
+      expect(feature.id).to.be(undefined);
+      expect(feature.properties).to.eql({
+        __kbn__feature_id__: 'logstash-2015.09.20:AU_x3_BsGFA8no6Qjjug:0',
+        _id: 'AU_x3_BsGFA8no6Qjjug',
+        _index: 'logstash-2015.09.20',
+        bytes: 9252,
+        ['machine.os.raw']: 'ios',
+      });
+      expect(feature.loadGeometry()).to.eql([[{ x: 44, y: 2382 }]]);
     });
 
-    it('should not validate when required params are missing', async () => {
-      await supertest
+    it('should return vector tile containing bounds when count exceeds size', async () => {
+      const resp = await supertest
         .get(
-          `/api/maps/mvt/getTile?&index=logstash*&requestBody=(_source:(includes:!(coordinates)),docvalue_fields:!(),query:(bool:(filter:!((match_all:())),must:!(),must_not:!(),should:!())),script_fields:(),size:10000,stored_fields:!(coordinates))`
+          `/api/maps/mvt/getTile\
+?x=1\
+&y=1\
+&z=2\
+&geometryFieldName=geo.coordinates\
+&index=logstash-*\
+&requestBody=(_source:!f,docvalue_fields:!(bytes,geo.coordinates,machine.os.raw),query:(bool:(filter:!((match_all:()),(range:(%27@timestamp%27:(format:strict_date_optional_time,gte:%272015-09-20T00:00:00.000Z%27,lte:%272015-09-20T01:00:00.000Z%27)))),must:!(),must_not:!(),should:!())),runtime_mappings:(),script_fields:(),size:1,stored_fields:!(bytes,geo.coordinates,machine.os.raw))\
+&geoFieldType=geo_point`
         )
         .set('kbn-xsrf', 'kibana')
-        .expect(400);
+        .responseType('blob')
+        .expect(200);
+
+      const jsonTile = new VectorTile(new Protobuf(resp.body));
+      const layer = jsonTile.layers[MVT_SOURCE_LAYER_NAME];
+      expect(layer.length).to.be(1);
+      const feature = layer.feature(0);
+      expect(feature.type).to.be(3);
+      expect(feature.extent).to.be(4096);
+      expect(feature.id).to.be(undefined);
+      expect(feature.properties).to.eql({ __kbn_too_many_features__: true });
+      expect(feature.loadGeometry()).to.eql([
+        [
+          { x: 44, y: 2382 },
+          { x: 44, y: 1913 },
+          { x: 550, y: 1913 },
+          { x: 550, y: 2382 },
+          { x: 44, y: 2382 },
+        ],
+      ]);
     });
   });
 }
