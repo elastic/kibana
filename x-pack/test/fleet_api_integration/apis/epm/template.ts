f5b4dcc8bc4f16544daab5daa00d340ee4c58929
Sonja Krause-Harder <sonja.krause-harder@elastic.co>
2021-02-02 17:08:49 +0100

    Adjust and reenable tests.

--- x-pack/test/fleet_api_integration/apis/epm/template.ts
+++ x-pack/test/fleet_api_integration/apis/epm/template.ts
@@ -10,8 +10,8 @@ import { FtrProviderContext } from '../../../api_integration/ftr_provider_contex
 import { getTemplate } from '../../../../plugins/fleet/server/services/epm/elasticsearch/template/template';
 
 export default function ({ getService }: FtrProviderContext) {
-  const indexPattern = 'foo';
   const templateName = 'bar';
+  const indexPatternName = 'bar-*';
   const es = getService('es');
   const mappings = {
     properties: {
@@ -26,6 +26,7 @@ export default function ({ getService }: FtrProviderContext) {
       const template = getTemplate({
         type: 'logs',
         templateName,
+        indexPatternName,
         mappings,
         packageName: 'system',
         composedOfTemplates: [],
@@ -34,18 +35,24 @@ export default function ({ getService }: FtrProviderContext) {
       // This test is not an API integration test with Kibana
       // We want to test here if the template is valid and for this we need a running ES instance.
       // If the ES instance takes the template, we assume it is a valid template.
-      const { body: response1 } = await es.indices.putTemplate({
-        name: templateName,
+      const { body: response1 } = await es.transport.request({
+        method: 'PUT',
+        path: `/_index_template/${templateName}`,
         body: template,
       });
+
       // Checks if template loading worked as expected
       expect(response1).to.eql({ acknowledged: true });
 
-      const { body: response2 } = await es.indices.getTemplate({ name: templateName });
+      const { body: response2 } = await es.transport.request({
+        method: 'GET',
+        path: `/_index_template/${templateName}`,
+      });
+
       // Checks if the content of the template that was loaded is as expected
       // We already know based on the above test that the template was valid
       // but we check here also if we wrote the index pattern inside the template as expected
-      expect(response2[templateName].index_patterns).to.eql([`${indexPattern}-*`]);
+      expect(response2.index_templates[0].index_template.index_patterns).to.eql([indexPatternName]);
     });
   });
 }
