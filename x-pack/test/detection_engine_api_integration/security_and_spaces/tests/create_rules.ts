76017b87285ddc1fe3373f01ef2412d5db13c5fa
Devin Hurley <devin.hurley@elastic.co>
2021-02-01 22:48:41 -0500

    update integration tests

--- x-pack/test/detection_engine_api_integration/security_and_spaces/tests/create_rules.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/tests/create_rules.ts
@@ -55,11 +55,13 @@ export default ({ getService }: FtrProviderContext) => {
     describe('creating rules', () => {
       beforeEach(async () => {
         await createSignalsIndex(supertest);
+        await esArchiver.load('auditbeat/hosts');
       });
 
       afterEach(async () => {
         await deleteSignalsIndex(supertest);
         await deleteAllAlerts(supertest);
+        await esArchiver.unload('auditbeat/hosts');
       });
 
       it('should create a single rule with a rule_id', async () => {
