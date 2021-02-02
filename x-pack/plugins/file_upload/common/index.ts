92bf065af7a183104695ad268ceb3350cef21cc2
Nathan Reese <reese.nathan@gmail.com>
2021-02-02 09:55:09 -0700

    [ml] migrate file_data_visualizer/import route to file_upload plugin (#89640)
    * migrate file_upload plugin to maps_file_upload
    
    * update plugins list
    
    * migrate ml import endpoint
    
    * migrate ml telemetry to file_upload plugin
    
    * add fileUpload plugin to ml
    
    * add TS project
    
    * update ML to use file_upload endpoint
    
    * move types to file_upload plugin
    
    * ignore error
    
    * clean up
    
    * i18n clean-up
    
    * remove schemas from ml
    
    * remove usageCollection from ml
    
    * node scripts/build_plugin_list_docs
    
    * update telemety collector
    
    * revert changes to ingestPipeline schema
    
    * change name of TELEMETRY_DOC_ID to unique value
    
    * remove ImportFile from ml/server/routes/apidoc.json
    
    * fix typo in x=pack/tsconfig.json
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	src/plugins/telemetry/schema/oss_plugins.json
    #	x-pack/plugins/telemetry_collection_xpack/schema/xpack_plugins.json

--- x-pack/plugins/file_upload/common/index.ts
+++ x-pack/plugins/file_upload/common/index.ts
@@ -4,4 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { fileDataVisualizerProvider, InputData } from './file_data_visualizer';
+export * from './constants';
+export * from './types';
--- x-pack/plugins/file_upload/server/telemetry/index.ts
+++ x-pack/plugins/file_upload/server/telemetry/index.ts
@@ -4,4 +4,5 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-export { fileDataVisualizerProvider, InputData } from './file_data_visualizer';
+export { initFileUploadTelemetry } from './usage_collector';
+export { updateTelemetry } from './telemetry';
