d035eabf03a907ecc7433a9625b33fc3ab80f5a5
Joe Reuter <johannes.reuter@elastic.co>
2021-02-03 12:02:28 +0100

    fix ml test

--- x-pack/test/functional/services/ml/data_frame_analytics_results.ts
+++ x-pack/test/functional/services/ml/data_frame_analytics_results.ts
@@ -52,7 +52,9 @@ export function MachineLearningDataFrameAnalyticsResultsProvider({
     },
 
     async getResultTableRows() {
-      return await testSubjects.findAll('mlExplorationDataGrid loaded > dataGridRowCell');
+      return (await testSubjects.find('mlExplorationDataGrid loaded')).findAllByTestSubject(
+        'dataGridRowCell'
+      );
     },
 
     async assertResultsTableNotEmpty() {
@@ -87,6 +89,7 @@ export function MachineLearningDataFrameAnalyticsResultsProvider({
       this.assertResultsTableNotEmpty();
 
       const featureImportanceCell = await this.getFirstFeatureImportanceCell();
+      await featureImportanceCell.focus();
       const interactionButton = await featureImportanceCell.findByTagName('button');
 
       // simulate hover and wait for button to appear
@@ -100,11 +103,9 @@ export function MachineLearningDataFrameAnalyticsResultsProvider({
 
     async getFirstFeatureImportanceCell(): Promise<WebElementWrapper> {
       // get first row of the data grid
-      const firstDataGridRow = await testSubjects.find(
-        'mlExplorationDataGrid loaded > dataGridRow'
-      );
+      const dataGrid = await testSubjects.find('mlExplorationDataGrid loaded');
       // find the feature importance cell in that row
-      const featureImportanceCell = await firstDataGridRow.findByCssSelector(
+      const featureImportanceCell = await dataGrid.findByCssSelector(
         '[data-test-subj="dataGridRowCell"][class*="featureImportance"]'
       );
       return featureImportanceCell;
