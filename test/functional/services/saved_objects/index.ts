5a8150b4ee2ae24c700d59bfcb4d11c1276c949f
Tre' Seymour <wayne.seymour@elastic.co>
2021-02-03 16:19:34 -0700

    [QA] Create Saved Objects FTR Svc
    Add svc,
    integrate into one test,
    add exported saved objects,
    add empty kibana call,
    and update ftr schema...add data directory for the new svc.

---
+++ test/functional/services/saved_objects/index.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { fetchSavedObjects } from './fetch_saved_objects';
+import { importSavedObjects } from './import_saved_objects';
+import { FtrProviderContext } from '../../../common/ftr_provider_context';
+
+export const SavedObjectsProvider = ({ getService }: FtrProviderContext) => {
+  const config = getService('config');
+  const dataDir = config.get('savedObjects.directory');
+
+  return {
+    fetch: fetchSavedObjects(dataDir),
+    import: importSavedObjects(dataDir),
+  };
+};
