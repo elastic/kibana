d5b1adcea23c8a8762e2e569b70b25ed0ddf1629
James Gowdy <jgowdy@elastic.co>
2021-02-03 20:04:28 +0000

    [ML] Fixing cloud behaviour when ML nodes are lazily assigned (#90051) (#90207)
    * [ML] Fixing cloud behaviour when ML nodes are lazily assigned
    
    * fixing duplicate id
    
    * updating snapshot
    
    * removing comments
    
    * fixing tests
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/ml/public/application/components/jobs_awaiting_node_warning/new_job_awaiting_node.tsx
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React, { Fragment, FC } from 'react';
+
+import { EuiCallOut, EuiSpacer } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { JobType } from '../../../../common/types/saved_objects';
+
+interface Props {
+  jobType: JobType;
+}
+
+export const NewJobAwaitingNodeWarning: FC<Props> = () => {
+  return (
+    <Fragment>
+      <EuiCallOut
+        title={
+          <FormattedMessage
+            id="xpack.ml.jobsAwaitingNodeWarning.title"
+            defaultMessage="Awaiting ML node provisioning"
+          />
+        }
+        color="primary"
+        iconType="iInCircle"
+      >
+        <div>
+          <FormattedMessage
+            id="xpack.ml.newJobAwaitingNodeWarning.noMLNodesAvailableDescription"
+            defaultMessage="Job cannot be started straight away, an ML node needs to be started. This will happen automatically."
+          />
+        </div>
+      </EuiCallOut>
+      <EuiSpacer size="m" />
+    </Fragment>
+  );
+};
