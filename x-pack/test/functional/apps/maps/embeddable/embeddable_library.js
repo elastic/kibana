20f7abcbe0d100f8e90bdac206d16712a48c9d5d
Catherine Liu <catherine.liu@elastic.co>
2021-02-03 11:19:24 -0800

    Updated map tests according to feedback

--- x-pack/test/functional/apps/maps/embeddable/embeddable_library.js
+++ x-pack/test/functional/apps/maps/embeddable/embeddable_library.js
@@ -9,11 +9,12 @@ import expect from '@kbn/expect';
 export default function ({ getPageObjects, getService }) {
   const find = getService('find');
   const testSubjects = getService('testSubjects');
-  const PageObjects = getPageObjects(['common', 'dashboard', 'maps']);
+  const PageObjects = getPageObjects(['common', 'dashboard', 'header', 'maps', 'visualize']);
   const kibanaServer = getService('kibanaServer');
   const security = getService('security');
   const dashboardAddPanel = getService('dashboardAddPanel');
   const dashboardPanelActions = getService('dashboardPanelActions');
+  const dashboardVisualizations = getService('dashboardVisualizations');
 
   describe('maps in embeddable library', () => {
     before(async () => {
@@ -32,44 +33,46 @@ export default function ({ getPageObjects, getService }) {
       });
       await PageObjects.common.navigateToApp('dashboard');
       await PageObjects.dashboard.clickNewDashboard();
+      await dashboardAddPanel.clickCreateNewLink();
+      await dashboardVisualizations.ensureNewVisualizationDialogIsShowing();
+      await PageObjects.visualize.clickMapsApp();
+      await PageObjects.header.waitUntilLoadingHasFinished();
+      await PageObjects.maps.waitForLayersToLoad();
+      await PageObjects.maps.clickSaveAndReturnButton();
+      await PageObjects.dashboard.waitForRenderComplete();
     });
 
     after(async () => {
       await security.testUser.restoreDefaults();
     });
 
-    it('unlink lens panel from embeddable library', async () => {
-      await PageObjects.common.navigateToApp('dashboard');
-      await PageObjects.dashboard.clickNewDashboard();
-      await dashboardAddPanel.clickOpenAddPanel();
-      await dashboardAddPanel.filterEmbeddableNames('document example');
-      await find.clickByButtonText('document example');
-      await dashboardAddPanel.closeAddPanel();
+    it('save map panel to embeddable library', async () => {
+      await dashboardPanelActions.saveToLibrary('embeddable library map');
+      await testSubjects.existOrFail('addPanelToLibrarySuccess');
 
-      const originalPanel = await testSubjects.find('embeddablePanelHeading-documentexample');
+      const mapPanel = await testSubjects.find('embeddablePanelHeading-embeddablelibrarymap');
+      const libraryActionExists = await testSubjects.descendantExists(
+        'embeddablePanelNotification-ACTION_LIBRARY_NOTIFICATION',
+        mapPanel
+      );
+      expect(libraryActionExists).to.be(true);
+    });
+
+    it('unlink map panel from embeddable library', async () => {
+      const originalPanel = await testSubjects.find('embeddablePanelHeading-embeddablelibrarymap');
       await dashboardPanelActions.unlinkFromLibary(originalPanel);
       await testSubjects.existOrFail('unlinkPanelSuccess');
 
-      const updatedPanel = await testSubjects.find('embeddablePanelHeading-documentexample');
+      const updatedPanel = await testSubjects.find('embeddablePanelHeading-embeddablelibrarymap');
       const libraryActionExists = await testSubjects.descendantExists(
         'embeddablePanelNotification-ACTION_LIBRARY_NOTIFICATION',
         updatedPanel
       );
       expect(libraryActionExists).to.be(false);
-    });
 
-    it('save lens panel to embeddable library', async () => {
-      const originalPanel = await testSubjects.find('embeddablePanelHeading-documentexample');
-      await dashboardPanelActions.saveToLibrary('document example - copy', originalPanel);
-      await testSubjects.click('confirmSaveSavedObjectButton');
-      await testSubjects.existOrFail('addPanelToLibrarySuccess');
-
-      const updatedPanel = await testSubjects.find('embeddablePanelHeading-documentexample-copy');
-      const libraryActionExists = await testSubjects.descendantExists(
-        'embeddablePanelNotification-ACTION_LIBRARY_NOTIFICATION',
-        updatedPanel
-      );
-      expect(libraryActionExists).to.be(true);
+      await dashboardAddPanel.clickOpenAddPanel();
+      await dashboardAddPanel.filterEmbeddableNames('embeddable library map');
+      await find.existsByLinkText('embeddable library map');
     });
   });
 }
