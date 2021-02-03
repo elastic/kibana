3c106913e7fb7751b9c7648a38491f74a7bd1e3e
Daniil Suleiman <daniil_suleiman@epam.com>
2021-02-03 16:26:30 +0200

    Add unit tests for cell

---
+++ src/plugins/vis_type_table/public/components/table_vis_cell.test.tsx
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * and the Server Side Public License, v 1; you may not use this file except in
+ * compliance with, at your election, the Elastic License or the Server Side
+ * Public License, v 1.
+ */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { EuiDataGridCellValueElementProps } from '@elastic/eui';
+import { createTableVisCell } from './table_vis_cell';
+import { FormattedColumns } from '../types';
+
+describe('table vis cell', () => {
+  it('should return a cell component with data in scope', () => {
+    const rows = [{ first: 1, second: 2 }];
+    const formattedColumns = ({
+      second: {
+        formatter: {
+          convert: jest.fn(),
+        },
+      },
+    } as unknown) as FormattedColumns;
+    const Cell = createTableVisCell(rows, formattedColumns);
+    const cellProps = {
+      rowIndex: 0,
+      columnId: 'second',
+    } as EuiDataGridCellValueElementProps;
+
+    const comp = shallow(<Cell {...cellProps} />);
+
+    expect(comp).toMatchSnapshot();
+    expect(formattedColumns.second.formatter.convert).toHaveBeenLastCalledWith(2, 'html');
+  });
+});
