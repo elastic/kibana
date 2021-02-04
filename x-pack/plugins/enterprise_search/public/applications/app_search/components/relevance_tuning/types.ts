f2db188b187a8ccc670eb4d39744a6ad44e1a92c
Jason Stoltzfus <jastoltz24@gmail.com>
2021-02-04 12:07:52 -0500

    Use Record type

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/types.ts
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/types.ts
@@ -1,7 +1,8 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
 export type BoostType = 'value' | 'functional' | 'proximity';
@@ -16,11 +17,7 @@ export interface Boost {
   factor: number;
 }
 
-export interface BoostObject {
-  [key: string]: Boost[];
-}
-
 export interface SearchSettings {
-  boosts: BoostObject;
+  boosts: Record<string, Boost[]>;
   search_fields: object;
 }
