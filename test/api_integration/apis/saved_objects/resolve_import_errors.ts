1c5d4b06a091e3b83154ab5c0fbca5fa7347c7df
Rudolf Meijering <skaapgif@gmail.com>
2021-02-04 13:16:52 +0100

    Fix test after master merge

--- test/api_integration/apis/saved_objects/resolve_import_errors.ts
+++ test/api_integration/apis/saved_objects/resolve_import_errors.ts
@@ -14,6 +14,7 @@ export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
   const esArchiver = getService('esArchiver');
   const es = getService('legacyEs');
+  const esDeleteAllIndices = getService('esDeleteAllIndices');
 
   describe('resolve_import_errors', () => {
     // mock success results including metadata
@@ -38,10 +39,7 @@ export default function ({ getService }: FtrProviderContext) {
       before(
         async () =>
           // just in case the kibana server has recreated it
-          await es.indices.delete({
-            index: '.kibana*',
-            ignore: [404],
-          })
+          await esDeleteAllIndices('.kibana*')
       );
 
       it('should return 200 and import nothing when empty parameters are passed in', async () => {
