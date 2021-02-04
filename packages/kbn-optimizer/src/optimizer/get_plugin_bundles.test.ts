2099495408b514f328129db463b109e341f3bbd1
kobelb <brandon.kobel@elastic.co>
2021-02-03 17:14:53 -0800

    Manually replacing the stragglers

--- packages/kbn-optimizer/src/optimizer/get_plugin_bundles.test.ts
+++ packages/kbn-optimizer/src/optimizer/get_plugin_bundles.test.ts
@@ -77,7 +77,7 @@ it('returns a bundle for core and each plugin', () => {
         "type": "plugin",
       },
       Object {
-        "banner": "/*! Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one or more contributor license agreements.
+        "banner": "/*! Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one or more contributor license agreements. 
      * Licensed under the Elastic License 2.0; you may not use this file except in compliance with the Elastic License 2.0. */
     ",
         "contextDir": <repoRoot>/x-pack/plugins/box,
