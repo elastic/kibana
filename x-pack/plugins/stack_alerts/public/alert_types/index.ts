0080c62be2802c93ce38d38d4370f4dfa456f796
Aaron Caldwell <aaron.caldwell@elastic.co>
2021-02-04 09:46:33 -0700

    Remove config settings and checks

--- x-pack/plugins/stack_alerts/public/alert_types/index.ts
+++ x-pack/plugins/stack_alerts/public/alert_types/index.ts
@@ -17,9 +17,7 @@ export function registerAlertTypes({
   alertTypeRegistry: TriggersAndActionsUIPublicPluginSetup['alertTypeRegistry'];
   config: Config;
 }) {
-  if (config.enableGeoAlerting) {
-    alertTypeRegistry.register(getGeoContainmentAlertType());
-  }
+  alertTypeRegistry.register(getGeoContainmentAlertType());
   alertTypeRegistry.register(getThresholdAlertType());
   alertTypeRegistry.register(getEsQueryAlertType());
 }
