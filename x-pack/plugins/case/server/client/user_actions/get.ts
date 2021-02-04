1c5175505711920eaa0e8b5443dafe261cbb03e8
Christos Nasikas <christos.nasikas@elastic.co>
2021-02-04 10:30:40 +0200

    Fix license

--- x-pack/plugins/case/server/client/user_actions/get.ts
+++ x-pack/plugins/case/server/client/user_actions/get.ts
@@ -1,7 +1,8 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
 import { CASE_SAVED_OBJECT, CASE_COMMENT_SAVED_OBJECT } from '../../saved_object_types';
