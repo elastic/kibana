ba3da4c975e48e975e95bfb50355bc0106f84aa2
Ahmad Bamieh <ahmadbamieh@gmail.com>
2021-02-05 18:08:38 +0200

    remove duplicate 'key' property

--- src/plugins/telemetry_collection_manager/server/encryption/telemetry_jwks.ts
+++ src/plugins/telemetry_collection_manager/server/encryption/telemetry_jwks.ts
@@ -11,26 +11,22 @@ import { PublicJWKS } from '@elastic/request-crypto';
 export const telemetryJWKS: PublicJWKS = {
   keys: [
     {
-      keys: [
-        {
-          kty: 'RSA',
-          kid: 'kibana_1',
-          use: 'enc',
-          alg: 'RSA-OAEP',
-          e: 'AQAB',
-          n:
-            'sZWMmiCy9Ysb_ZByG9024bL8a8cDNnXd4BqF4bPm6QwPponFS8cQhvZclz68qiVOa8aZOwB50bz7hVVzBKHP053u4wNN0PbpKD4w2rHS-pgN0m2hRFXwM9clmtEr-9aPTDqfWHsclS5ZZMT9_qyD42VS-zMrTwxBvQU5FNNcogakcaspXRtW3_8J7ZtbwkdvySY7e1DlHHrM7t60bLQ4RQB4q_ocqoeb447Sx1Tf8bJS4m-klregXQ7xQ7gcuuVW1n7tN9N1KDw_GqGm65wkDYDGskTca52STUbKAqX4sird0LI7QTTBX9oKfcNPpMOiD0XJQKGfXuoXQSFIa6vpuQ',
-        },
-        {
-          kty: 'RSA',
-          kid: 'kibana_dev_1',
-          use: 'enc',
-          alg: 'RSA-OAEP',
-          e: 'AQAB',
-          n:
-            'jTbnpqb3vZheLoedLvTJI8Ci7RT-bTkxpmp_ni0PN_b1lv_9c9crbzbIqkE7EiFrqEsFD1xFHOqrFgYdrQmoE3TmDx6hRLGhhKeBWg89opbutb8ET3YSA97Io4uAHk02mo447NWXg7I1HiQau6TkdZNXrol4mjzkiRtUQGDUmhK428sxH3yVZB69WfJijEwtYI81Ms7FynPPAymPzGo_He3-sV0JG5U2OCy8RTohB9Agjq708F8SrVhJ6whX32gQ537DeYOzGUpFXfoIS3HE__OLYeyLj6OoKnyTLl6FF9iMVY8xfAPbFY5eJuMqCzACTsndRuDlYfOsbYFueUQogw',
-        },
-      ],
+      kty: 'RSA',
+      kid: 'kibana_1',
+      use: 'enc',
+      alg: 'RSA-OAEP',
+      e: 'AQAB',
+      n:
+        'sZWMmiCy9Ysb_ZByG9024bL8a8cDNnXd4BqF4bPm6QwPponFS8cQhvZclz68qiVOa8aZOwB50bz7hVVzBKHP053u4wNN0PbpKD4w2rHS-pgN0m2hRFXwM9clmtEr-9aPTDqfWHsclS5ZZMT9_qyD42VS-zMrTwxBvQU5FNNcogakcaspXRtW3_8J7ZtbwkdvySY7e1DlHHrM7t60bLQ4RQB4q_ocqoeb447Sx1Tf8bJS4m-klregXQ7xQ7gcuuVW1n7tN9N1KDw_GqGm65wkDYDGskTca52STUbKAqX4sird0LI7QTTBX9oKfcNPpMOiD0XJQKGfXuoXQSFIa6vpuQ',
+    },
+    {
+      kty: 'RSA',
+      kid: 'kibana_dev_1',
+      use: 'enc',
+      alg: 'RSA-OAEP',
+      e: 'AQAB',
+      n:
+        'jTbnpqb3vZheLoedLvTJI8Ci7RT-bTkxpmp_ni0PN_b1lv_9c9crbzbIqkE7EiFrqEsFD1xFHOqrFgYdrQmoE3TmDx6hRLGhhKeBWg89opbutb8ET3YSA97Io4uAHk02mo447NWXg7I1HiQau6TkdZNXrol4mjzkiRtUQGDUmhK428sxH3yVZB69WfJijEwtYI81Ms7FynPPAymPzGo_He3-sV0JG5U2OCy8RTohB9Agjq708F8SrVhJ6whX32gQ537DeYOzGUpFXfoIS3HE__OLYeyLj6OoKnyTLl6FF9iMVY8xfAPbFY5eJuMqCzACTsndRuDlYfOsbYFueUQogw',
     },
   ],
 };
