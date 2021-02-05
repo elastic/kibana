253d072047e9cd6c3cfc6ef9d9e86dc56a558691
Jen Huang <its.jenetic@gmail.com>
2021-02-05 13:02:49 -0800

    [7.10] Fix state sharing between home integration components, prevent full page reload when clicking Fleet link (#90334) (#90479)
    * Fix state sharing between home integration components, prevent full page reload when clicking Fleet link (#90334)
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	x-pack/plugins/fleet/public/applications/fleet/components/home_integration/tutorial_directory_header_link.tsx
    #	x-pack/plugins/fleet/public/applications/fleet/components/home_integration/tutorial_directory_notice.tsx
    
    * Fix incorrect hook
    
    * Fix linting

--- x-pack/plugins/ingest_manager/public/applications/ingest_manager/components/home_integration/tutorial_directory_header_link.tsx
+++ x-pack/plugins/ingest_manager/public/applications/ingest_manager/components/home_integration/tutorial_directory_header_link.tsx
@@ -4,19 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 import React, { memo, useState, useEffect } from 'react';
-import { BehaviorSubject } from 'rxjs';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { EuiButtonEmpty } from '@elastic/eui';
 import type { TutorialDirectoryHeaderLinkComponent } from 'src/plugins/home/public';
-import { useLink, useCapabilities } from '../../hooks';
-
-const tutorialDirectoryNoticeState$ = new BehaviorSubject({
-  settingsDataLoaded: false,
-  hasSeenNotice: false,
-});
+import { RedirectAppLinks } from '../../../../../../../../src/plugins/kibana_react/public';
+import { useLink, useCapabilities, useCore } from '../../hooks';
+import { tutorialDirectoryNoticeState$ } from './tutorial_directory_notice';
 
 const TutorialDirectoryHeaderLink: TutorialDirectoryHeaderLinkComponent = memo(() => {
   const { getHref } = useLink();
+  const { application } = useCore();
   const { show: hasIngestManager } = useCapabilities();
   const [noticeState, setNoticeState] = useState({
     settingsDataLoaded: false,
@@ -31,12 +28,14 @@ const TutorialDirectoryHeaderLink: TutorialDirectoryHeaderLinkComponent = memo((
   }, []);
 
   return hasIngestManager && noticeState.settingsDataLoaded && noticeState.hasSeenNotice ? (
-    <EuiButtonEmpty size="s" iconType="link" flush="right" href={getHref('overview')}>
-      <FormattedMessage
-        id="xpack.ingestManager.homeIntegration.tutorialDirectory.fleetAppButtonText"
-        defaultMessage="Try Fleet Beta"
-      />
-    </EuiButtonEmpty>
+    <RedirectAppLinks application={application}>
+      <EuiButtonEmpty size="s" iconType="link" flush="right" href={getHref('overview')}>
+        <FormattedMessage
+          id="xpack.ingestManager.homeIntegration.tutorialDirectory.fleetAppButtonText"
+          defaultMessage="Try Fleet Beta"
+        />
+      </EuiButtonEmpty>
+    </RedirectAppLinks>
   ) : null;
 });
 
