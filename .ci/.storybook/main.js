ce87f6a60042809a1a375f3d533ece053513548c
Brian Seeders <brian.seeders@elastic.co>
2021-02-05 13:17:00 -0500

    Make sure eslint lints the .ci directory

--- .ci/.storybook/main.js
+++ .ci/.storybook/main.js
@@ -11,18 +11,18 @@ const aliases = require('../../src/dev/storybook/aliases.ts').storybookAliases;
 
 config.refs = {};
 
-for(const alias of Object.keys(aliases).filter(a => a !== 'ci_composite')) {
+for (const alias of Object.keys(aliases).filter((a) => a !== 'ci_composite')) {
   // snake_case -> Title Case
   const title = alias
     .replace(/_/g, ' ')
     .split(' ')
-    .map(n => n[0].toUpperCase()+n.slice(1) )
+    .map((n) => n[0].toUpperCase() + n.slice(1))
     .join(' ');
 
   config.refs[alias] = {
     title: title,
     url: `${process.env.STORYBOOK_BASE_URL}/${alias}`,
-  }
+  };
 }
 
 module.exports = config;
