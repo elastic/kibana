2869e125754515329d7f2cd6bcd9efd2215a2dc9
Tyler Smalley <tyler.smalley@elastic.co>
2021-02-05 14:25:33 -0800

    Build TS refs in parallel
    Results in a small (~7%) reduction in the build time. However, it does mean that projects referenced outside of x-pack might be doing half the work at the expense of build time.
    
    I first began by combining both tsconfig.refs.json to see if there would be an improvement in the build time. However, that didn't make a measurable difference.
    
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- src/dev/typescript/build_refs.ts
+++ src/dev/typescript/build_refs.ts
@@ -11,8 +11,10 @@ import Path from 'path';
 import { run, ToolingLog } from '@kbn/dev-utils';
 
 export async function buildAllRefs(log: ToolingLog) {
-  await buildRefs(log, 'tsconfig.refs.json');
-  await buildRefs(log, Path.join('x-pack', 'tsconfig.refs.json'));
+  await Promise.all([
+    buildRefs(log, 'tsconfig.refs.json'),
+    buildRefs(log, Path.join('x-pack', 'tsconfig.refs.json')),
+  ]);
 }
 
 async function buildRefs(log: ToolingLog, projectPath: string) {
