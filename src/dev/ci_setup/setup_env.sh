e36bde50cb203895a0d4adcda8ab7ef87a072144
Tiago Costa <tiagoffcc@hotmail.com>
2021-02-05 02:35:11 +0000

    chore(NA): cleaning implementation

--- src/dev/ci_setup/setup_env.sh
+++ src/dev/ci_setup/setup_env.sh
@@ -190,11 +190,4 @@ cp -f "$KIBANA_DIR/src/dev/ci_setup/.bazelrc-ci" "$HOME/.bazelrc";
 echo "# Appended by $KIBANA_DIR/src/dev/ci_setup/setup.sh" >> "$HOME/.bazelrc"
 echo "build --remote_header=x-buildbuddy-api-key=$KIBANA_BUILDBUDDY_CI_API_KEY" >> "$HOME/.bazelrc"
 
-
-###
-### make sure bazelisk is installed on CI
-###
-# bazeliskVersion=$(head -n 1 "$KIBANA_DIR/.bazeliskversion")
-# yarn global add "@bazel/bazelisk@${bazeliskVersion}"
-
 export CI_ENV_SETUP=true
