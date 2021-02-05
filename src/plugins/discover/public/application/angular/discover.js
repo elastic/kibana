90f66b9b295ee01c672ed43f58d9b709a3077c38
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-02-05 17:55:59 +0100

    Fix discover controller code

--- src/plugins/discover/public/application/angular/discover.js
+++ src/plugins/discover/public/application/angular/discover.js
@@ -464,13 +464,6 @@ function discoverController($route, $scope, Promise) {
         )
       );
 
-      subscriptions.add(
-        data.search.session.onRefresh$.subscribe(() => {
-          searchSessionManager.removeSearchSessionIdFromURL({ replace: false });
-          refetch$.next();
-        })
-      );
-
       $scope.$watchMulti(
         ['rows', 'fetchStatus'],
         (function updateResultState() {
