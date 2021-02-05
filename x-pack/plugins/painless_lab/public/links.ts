d0a469869cdb8731e3a81731003799e352a38dc2
István Zoltán Szabó <istvan.szabo@elastic.co>
2021-02-05 16:14:29 +0100

    Uses doc link service in Painless lab.

--- x-pack/plugins/painless_lab/public/links.ts
+++ x-pack/plugins/painless_lab/public/links.ts
@@ -12,11 +12,11 @@ export type Links = ReturnType<typeof getLinks>;
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const getLinks = ({ DOC_LINK_VERSION, ELASTIC_WEBSITE_URL }: DocLinksStart) =>
   Object.freeze({
-    painlessExecuteAPI: `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/painless/${DOC_LINK_VERSION}/painless-execute-api.html`,
-    painlessExecuteAPIContexts: `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/painless/${DOC_LINK_VERSION}/painless-execute-api.html#_contexts`,
-    painlessAPIReference: `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/painless/${DOC_LINK_VERSION}/painless-api-reference.html`,
-    painlessWalkthrough: `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/painless/${DOC_LINK_VERSION}/painless-walkthrough.html`,
-    painlessLangSpec: `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/painless/${DOC_LINK_VERSION}/painless-lang-spec.html`,
-    esQueryDSL: `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/reference/${DOC_LINK_VERSION}/query-dsl.html`,
-    modulesScriptingPreferParams: `${ELASTIC_WEBSITE_URL}guide/en/elasticsearch/reference/${DOC_LINK_VERSION}/modules-scripting-using.html#prefer-params`,
+    painlessExecuteAPI: `${docLinks.links.apis.painlessExecute}`,
+    painlessExecuteAPIContexts: `${docLinks.links.apis.painlessExecuteAPIContexts}`,
+    painlessAPIReference: `${docLinks.links.scriptedFields.painlessApi}`,
+    painlessWalkthrough: `${docLinks.links.scriptedFields.painlessWalkthrough}`,
+    painlessLangSpec: `${docLinks.links.scriptedFields.painlessLangSpec}`,
+    esQueryDSL: `${docLinks.links.query.queryDsl}`,
+    modulesScriptingPreferParams: `${docLinks.links.elasticsearch.scriptParameters}`,
   });
