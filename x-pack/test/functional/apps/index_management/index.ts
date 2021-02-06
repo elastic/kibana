43d05938910ab7b9f9cb61b2b4988289f50cffc0
John Dorlus <jsdorlus@elastic.co>
2021-02-06 01:58:28 -0500

    Added to group 11 because 13 is not in flaky test runner. Will revert after passing.

--- x-pack/test/functional/apps/index_management/index.ts
+++ x-pack/test/functional/apps/index_management/index.ts
@@ -9,7 +9,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default ({ loadTestFile }: FtrProviderContext) => {
   describe('Index Management app', function () {
-    this.tags('ciGroup13');
+    this.tags('ciGroup11');
     loadTestFile(require.resolve('./feature_controls'));
     loadTestFile(require.resolve('./home_page'));
   });
