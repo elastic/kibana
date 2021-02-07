8d33fb3b2167112a335af961687ef670d587b530
cauemarcondes <caue.marcondes@elastic.co>
2021-02-07 10:50:04 +0100

    fixing eslint

--- x-pack/plugins/apm/public/components/app/service_overview/service_overview.test.tsx
+++ x-pack/plugins/apm/public/components/app/service_overview/service_overview.test.tsx
@@ -80,8 +80,8 @@ describe('ServiceOverview', () => {
         status: FETCH_STATUS.SUCCESS,
       });
 
+    /* eslint-disable @typescript-eslint/naming-convention */
     const calls = {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
       'GET /api/apm/services/{serviceName}/error_groups': {
         error_groups: [],
         total_error_groups: 0,
@@ -92,9 +92,9 @@ describe('ServiceOverview', () => {
         isAggregationAccurate: true,
       },
       'GET /api/apm/services/{serviceName}/dependencies': [],
-      // eslint-disable-next-line @typescript-eslint/naming-convention
       'GET /api/apm/services/{serviceName}/service_overview_instances': [],
     };
+    /* eslint-enable @typescript-eslint/naming-convention */
 
     jest
       .spyOn(callApmApiModule, 'createCallApmApi')
