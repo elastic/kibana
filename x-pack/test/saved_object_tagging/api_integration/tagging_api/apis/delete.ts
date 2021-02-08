4c2c871b8dd2d7dba8ea12109a2436797a3919b5
Rudolf Meijering <skaapgif@gmail.com>
2021-02-08 10:59:41 +0100

    Debug flaky test

--- x-pack/test/saved_object_tagging/api_integration/tagging_api/apis/delete.ts
+++ x-pack/test/saved_object_tagging/api_integration/tagging_api/apis/delete.ts
@@ -14,7 +14,7 @@ export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
 
   // FLAKY: https://github.com/elastic/kibana/issues/90552
-  describe.skip('DELETE /api/saved_objects_tagging/tags/{id}', () => {
+  describe.only('DELETE /api/saved_objects_tagging/tags/{id}', () => {
     beforeEach(async () => {
       await esArchiver.load('delete_with_references');
     });
@@ -24,9 +24,13 @@ export default function ({ getService }: FtrProviderContext) {
     });
 
     it('should delete the tag', async () => {
-      await supertest.get(`/api/saved_objects_tagging/tags/tag-1`).expect(200);
+      const getRes = await supertest.get(`/api/saved_objects_tagging/tags/tag-1`);
+      console.log(getRes.body);
+      expect(getRes.status).to.eql(200);
 
-      await supertest.delete(`/api/saved_objects_tagging/tags/tag-1`).expect(200);
+      const delRes = await supertest.delete(`/api/saved_objects_tagging/tags/tag-1`);
+      console.log(delRes.body);
+      expect(delRes.status).to.eql(200);
 
       await supertest.get(`/api/saved_objects_tagging/tags/tag-1`).expect(404);
     });
