7a31a6997e432ceb48a1fcea0ba8a35358de0501
pgayvallet <pierre.gayvallet@elastic.co>
2021-02-08 08:06:07 +0100

    use delay instead of silence

--- packages/kbn-test/src/functional_tests/tasks.js
+++ packages/kbn-test/src/functional_tests/tasks.js
@@ -95,8 +95,8 @@ export async function runTests(options) {
       try {
         es = await runElasticsearch({ config, options: opts });
         await runKibanaServer({ procs, config, options: opts });
-        // wait 5 seconds to let kibana startup completes
-        await silence(log, 5000);
+        // workaround until https://github.com/elastic/kibana/issues/89828 is addressed
+        await delay(5000);
         await runFtr({ configPath, options: opts });
       } finally {
         try {
@@ -161,4 +161,8 @@ async function silence(log, milliseconds) {
       take(1)
     )
     .toPromise();
+}
+
+async function delay(ms) {
+  await new Promise((resolve) => setTimeout(resolve, ms));
 }
