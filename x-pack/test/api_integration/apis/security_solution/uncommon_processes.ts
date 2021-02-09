afceaaca15a2365c53a54c64cd09444857eaccb1
Angela Chuang <yi-chun.chuang@elastic.co>
2021-02-09 09:35:29 +0800

    avoid multiple requests

--- x-pack/test/api_integration/apis/security_solution/uncommon_processes.ts
+++ x-pack/test/api_integration/apis/security_solution/uncommon_processes.ts
@@ -7,8 +7,15 @@
 
 import expect from '@kbn/expect';
 
-import { HostsQueries } from '../../../../plugins/security_solution/common/search_strategy';
+import {
+  HostsQueries,
+  HostsUncommonProcessesStrategyResponse,
+} from '../../../../plugins/security_solution/common/search_strategy';
 import { FtrProviderContext } from '../../ftr_provider_context';
+
+interface UncommonProcessesResponse {
+  body: HostsUncommonProcessesStrategyResponse;
+}
 
 const FROM = '2000-01-01T00:00:00.000Z';
 const TO = '3000-01-01T00:00:00.000Z';
@@ -24,133 +31,95 @@ export default function ({ getService }: FtrProviderContext) {
     before(() => esArchiver.load('auditbeat/hosts'));
     after(() => esArchiver.unload('auditbeat/hosts'));
 
-    it('should return an edge of length 1 when given a pagination of length 1', async () => {
-      const { body: UncommonProcesses } = await supertest
-        .post('/internal/search/securitySolutionSearchStrategy/')
-        .set('kbn-xsrf', 'true')
-        .send({
-          factoryQueryType: HostsQueries.uncommonProcesses,
-          sourceId: 'default',
-          timerange: {
-            interval: '12h',
-            to: TO,
-            from: FROM,
-          },
-          pagination: {
-            activePage: 0,
-            cursorStart: 0,
-            fakePossibleCount: 3,
-            querySize: 1,
-          },
-          defaultIndex: ['auditbeat-*'],
-          docValueFields: [],
-          inspect: false,
-          wait_for_completion_timeout: '10s',
-        })
-        .expect(200);
-      expect(UncommonProcesses.edges.length).to.be(1);
+    describe('when given a pagination of length 2', () => {
+      let response: null | UncommonProcessesResponse = null;
+
+      before(async () => {
+        response = await supertest
+          .post('/internal/search/securitySolutionSearchStrategy/')
+          .set('kbn-xsrf', 'true')
+          .send({
+            factoryQueryType: HostsQueries.uncommonProcesses,
+            sourceId: 'default',
+            timerange: {
+              interval: '12h',
+              to: TO,
+              from: FROM,
+            },
+            pagination: {
+              activePage: 0,
+              cursorStart: 0,
+              fakePossibleCount: 3,
+              querySize: 2,
+            },
+            defaultIndex: ['auditbeat-*'],
+            docValueFields: [],
+            inspect: false,
+            wait_for_completion_timeout: '10s',
+          });
+      });
+
+      it('should return an edge of length 2 ', () => {
+        expect(response!.body.edges.length).to.be(2);
+      });
     });
 
-    it('should return an edge of length 2 when given a pagination of length 2', async () => {
-      const { body: UncommonProcesses } = await supertest
-        .post('/internal/search/securitySolutionSearchStrategy/')
-        .set('kbn-xsrf', 'true')
-        .send({
-          factoryQueryType: HostsQueries.uncommonProcesses,
-          sourceId: 'default',
-          timerange: {
-            interval: '12h',
-            to: TO,
-            from: FROM,
-          },
-          pagination: {
-            activePage: 0,
-            cursorStart: 0,
-            fakePossibleCount: 3,
-            querySize: 2,
-          },
-          defaultIndex: ['auditbeat-*'],
-          docValueFields: [],
-          inspect: false,
-          wait_for_completion_timeout: '10s',
-        })
-        .expect(200);
+    describe('when given a pagination of length 1', () => {
+      let response: null | UncommonProcessesResponse = null;
+      before(async () => {
+        response = await supertest
+          .post('/internal/search/securitySolutionSearchStrategy/')
+          .set('kbn-xsrf', 'true')
+          .send({
+            factoryQueryType: HostsQueries.uncommonProcesses,
+            sourceId: 'default',
+            timerange: {
+              interval: '12h',
+              to: TO,
+              from: FROM,
+            },
+            pagination: {
+              activePage: 0,
+              cursorStart: 0,
+              fakePossibleCount: 3,
+              querySize: 1,
+            },
+            defaultIndex: ['auditbeat-*'],
+            docValueFields: [],
+            inspect: false,
+            wait_for_completion_timeout: '10s',
+          });
+      });
 
-      expect(UncommonProcesses.edges.length).to.be(2);
-    });
+      it('should return an edge of length 1 ', () => {
+        expect(response!.body.edges.length).to.be(1);
+      });
 
-    it('should return a total count of elements', async () => {
-      const { body: UncommonProcesses } = await supertest
-        .post('/internal/search/securitySolutionSearchStrategy/')
-        .set('kbn-xsrf', 'true')
-        .send({
-          factoryQueryType: HostsQueries.uncommonProcesses,
-          sourceId: 'default',
-          timerange: {
-            interval: '12h',
-            to: TO,
-            from: FROM,
-          },
-          pagination: {
-            activePage: 0,
-            cursorStart: 0,
-            fakePossibleCount: 3,
-            querySize: 1,
-          },
-          defaultIndex: ['auditbeat-*'],
-          docValueFields: [],
-          inspect: false,
-          wait_for_completion_timeout: '10s',
-        })
-        .expect(200);
+      it('should return a total count of elements', () => {
+        expect(response!.body.totalCount).to.be(TOTAL_COUNT);
+      });
 
-      expect(UncommonProcesses.totalCount).to.be(TOTAL_COUNT);
-    });
-
-    it('should return a single data set with pagination of 1', async () => {
-      const { body: UncommonProcesses } = await supertest
-        .post('/internal/search/securitySolutionSearchStrategy/')
-        .set('kbn-xsrf', 'true')
-        .send({
-          factoryQueryType: HostsQueries.uncommonProcesses,
-          sourceId: 'default',
-          timerange: {
-            interval: '12h',
-            to: TO,
-            from: FROM,
+      it('should return a single data set with pagination of 1', () => {
+        const expected = {
+          _id: 'HCFxB2kBR346wHgnL4ik',
+          instances: 1,
+          process: {
+            args: [],
+            name: ['kworker/u2:0'],
           },
-          pagination: {
-            activePage: 0,
-            cursorStart: 0,
-            fakePossibleCount: 3,
-            querySize: 1,
+          user: {
+            id: ['0'],
+            name: ['root'],
           },
-          defaultIndex: ['auditbeat-*'],
-          docValueFields: [],
-          inspect: false,
-          wait_for_completion_timeout: '10s',
-        })
-        .expect(200);
-
-      const expected = {
-        _id: 'HCFxB2kBR346wHgnL4ik',
-        instances: 1,
-        process: {
-          args: [],
-          name: ['kworker/u2:0'],
-        },
-        user: {
-          id: ['0'],
-          name: ['root'],
-        },
-        hosts: [
-          {
-            id: ['zeek-sensor-san-francisco'],
-            name: ['zeek-sensor-san-francisco'],
-          },
-        ],
-      };
-      expect(UncommonProcesses.edges[0].node).to.eql(expected);
+          hosts: [
+            {
+              id: ['zeek-sensor-san-francisco'],
+              name: ['zeek-sensor-san-francisco'],
+            },
+          ],
+        };
+        expect(response!.body.edges[0].node).to.eql(expected);
+      });
     });
   });
 }
