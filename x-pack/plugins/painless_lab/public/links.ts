92fea204ee73039d8d334253564d98f50838c932
István Zoltán Szabó <istvan.szabo@elastic.co>
2021-02-09 10:24:28 +0100

    Minor edits.

--- x-pack/plugins/painless_lab/public/links.ts
+++ x-pack/plugins/painless_lab/public/links.ts
@@ -10,7 +10,7 @@ import { DocLinksStart } from 'src/core/public';
 export type Links = ReturnType<typeof getLinks>;
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const getLinks = ({ links }: DocLinksStart) =>
+export const getLinks = ({ DOC_LINK_VERSION, ELASTIC_WEBSITE_URL }: DocLinksStart) =>
   Object.freeze({
     painlessExecuteAPI: getLinks.links.apis.painlessExecute,
     painlessExecuteAPIContexts: getLinks.links.apis.painlessExecuteAPIContexts,
