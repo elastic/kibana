641d2644390e6f3307c7db1e6128b5b179afee9a
streamich <streamich@gmail.com>
2021-02-10 16:04:20 +0100

    feat: 🎸 improve <ListManageDrilldowns> stories

--- x-pack/plugins/ui_actions_enhanced/public/drilldowns/components/list_manage_drilldowns/list_manage_drilldowns.stories.tsx
+++ x-pack/plugins/ui_actions_enhanced/public/drilldowns/components/list_manage_drilldowns/list_manage_drilldowns.stories.tsx
@@ -7,31 +7,80 @@
 
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { ListManageDrilldowns } from './list_manage_drilldowns';
+import { FlyoutFrame } from '../flyout_frame';
 
-storiesOf('components/ListManageDrilldowns', module).add('default', () => (
-  <ListManageDrilldowns
-    drilldowns={[
-      {
-        id: '1',
-        actionName: 'Dashboard',
-        drilldownName: 'Drilldown 1',
-        icon: 'dashboardApp',
-        triggers: [{ title: 'trigger' }],
-      },
-      {
-        id: '2',
-        actionName: 'Dashboard',
-        drilldownName: 'Drilldown 2',
-        icon: 'dashboardApp',
-        triggers: [{ title: 'trigger' }],
-      },
-      {
-        id: '3',
-        actionName: 'Dashboard',
-        drilldownName: 'Drilldown 3',
-        triggers: [{ title: 'trigger', description: 'trigger' }],
-      },
-    ]}
-  />
-));
+storiesOf('components/ListManageDrilldowns', module)
+  .add('Default', () => (
+    <ListManageDrilldowns
+      drilldowns={[
+        {
+          id: '1',
+          actionName: 'Dashboard',
+          drilldownName: 'Drilldown 1',
+          icon: 'dashboardApp',
+          triggers: [{ title: 'trigger' }],
+        },
+        {
+          id: '2',
+          actionName: 'Dashboard',
+          drilldownName: 'Drilldown 2',
+          icon: 'dashboardApp',
+          triggers: [{ title: 'trigger' }],
+        },
+        {
+          id: '3',
+          actionName: 'Dashboard',
+          drilldownName: 'Drilldown 3',
+          triggers: [{ title: 'trigger', description: 'trigger' }],
+        },
+      ]}
+      onCreate={action('onCreate')}
+      onDelete={action('onDelete')}
+      onEdit={action('onEdit')}
+    />
+  ))
+  .add('Empty list', () => (
+    <ListManageDrilldowns
+      drilldowns={[]}
+      onCreate={action('onCreate')}
+      onDelete={action('onDelete')}
+      onEdit={action('onEdit')}
+    />
+  ))
+  .add('A single drilldown', () => (
+    <ListManageDrilldowns
+      drilldowns={[
+        {
+          id: '1',
+          actionName: 'Dashboard',
+          drilldownName: 'Drilldown 1',
+          icon: 'dashboardApp',
+          triggers: [{ title: 'trigger' }],
+        },
+      ]}
+      onCreate={action('onCreate')}
+      onDelete={action('onDelete')}
+      onEdit={action('onEdit')}
+    />
+  ))
+  .add('Inside a flyout frame', () => (
+    <FlyoutFrame title={'Some Title'} onClose={action('onClose')} banner={null}>
+      <ListManageDrilldowns
+        drilldowns={[
+          { id: '1', actionName: 'Dashboard', drilldownName: 'Drilldown 1' },
+          { id: '2', actionName: 'Dashboard', drilldownName: 'Drilldown 2' },
+          {
+            id: '3',
+            actionName: 'Dashboard',
+            drilldownName: 'Drilldown 3',
+            error: 'Some error...',
+          },
+        ]}
+        onCreate={action('onCreate')}
+        onDelete={action('onDelete')}
+        onEdit={action('onEdit')}
+      />
+    </FlyoutFrame>
+  ));
