e4703c33382dfa67d5e249bb22dcfdd5a915c7a6
Nathan Reese <reese.nathan@gmail.com>
2021-02-09 17:31:57 -0700

    [Maps] remove maps_file_upload plugin and fold public folder into file_upload plugin (#90292)
    * get geojson working with api/file_upload/import
    
    * remove maps_file_upload server code
    
    * remove common folder
    
    * remove maps_file_upload plugin
    
    * fix tsconfig paths
    
    * rename file_upload plugin in maps tsconfig
    
    * fix file path
    
    * node scripts/build_plugin_list_docs
    
    * fix webpack compile errors
    
    * telemetry schema cleanup, i18n cleanup, limits cleanup
    
    * remove mapsFileUpload from limits.yml
    
    * remove index pattern link test case
    
    * update telemetry/v2/clusters/_stats for new file_upload path
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/file_upload/public/util/geo_processing.js
+++ x-pack/plugins/file_upload/public/util/geo_processing.js
@@ -6,25 +6,11 @@
  */
 
 import _ from 'lodash';
-import { ES_GEO_FIELD_TYPE } from '../../common/constants/file_import';
 
-const DEFAULT_SETTINGS = {
-  number_of_shards: 1,
+export const ES_GEO_FIELD_TYPE = {
+  GEO_POINT: 'geo_point',
+  GEO_SHAPE: 'geo_shape',
 };
-
-const DEFAULT_GEO_SHAPE_MAPPINGS = {
-  coordinates: {
-    type: ES_GEO_FIELD_TYPE.GEO_SHAPE,
-  },
-};
-
-const DEFAULT_GEO_POINT_MAPPINGS = {
-  coordinates: {
-    type: ES_GEO_FIELD_TYPE.GEO_POINT,
-  },
-};
-
-const DEFAULT_INGEST_PIPELINE = {};
 
 export function getGeoIndexTypesForFeatures(featureTypes) {
   const hasNoFeatureType = !featureTypes || !featureTypes.length;
@@ -77,11 +63,16 @@ export function geoJsonToEs(parsedGeojson, datatype) {
 export function getGeoJsonIndexingDetails(parsedGeojson, dataType) {
   return {
     data: geoJsonToEs(parsedGeojson, dataType),
-    ingestPipeline: DEFAULT_INGEST_PIPELINE,
-    mappings:
-      dataType === ES_GEO_FIELD_TYPE.GEO_POINT
-        ? DEFAULT_GEO_POINT_MAPPINGS
-        : DEFAULT_GEO_SHAPE_MAPPINGS,
-    settings: DEFAULT_SETTINGS,
+    ingestPipeline: {},
+    mappings: {
+      properties: {
+        coordinates: {
+          type: dataType,
+        },
+      },
+    },
+    settings: {
+      number_of_shards: 1,
+    },
   };
 }
--- x-pack/plugins/file_upload/public/util/size_limited_chunking.js
+++ x-pack/plugins/file_upload/public/util/size_limited_chunking.js
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { MAX_BYTES } from '../../common/constants/file_import';
+const MAX_BYTES = 31457280;
 
 // MAX_BYTES is a good guideline for splitting up posts, but this logic
 // occasionally sizes chunks so closely to the limit, that the remaining content
