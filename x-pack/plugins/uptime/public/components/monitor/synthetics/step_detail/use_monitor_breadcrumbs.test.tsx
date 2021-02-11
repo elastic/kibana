870707502fcad7a8d85e52a5e3858a7913d41b68
Shahzad <shahzad31comp@gmail.com>
2021-02-11 15:28:26 +0100

    update breadcrumb test

--- x-pack/plugins/uptime/public/components/monitor/synthetics/step_detail/use_monitor_breadcrumbs.test.tsx
+++ x-pack/plugins/uptime/public/components/monitor/synthetics/step_detail/use_monitor_breadcrumbs.test.tsx
@@ -17,7 +17,7 @@ import { JourneyState } from '../../../../state/reducers/journey';
 import { chromeServiceMock, uiSettingsServiceMock } from 'src/core/public/mocks';
 
 describe('useMonitorBreadcrumbs', () => {
-  it('sets the given breadcrumbs', () => {
+  it('sets the given breadcrumbs for steps list view', () => {
     let breadcrumbObj: ChromeBreadcrumb[] = [];
     const getBreadcrumbs = () => {
       return breadcrumbObj;
@@ -43,8 +43,13 @@ describe('useMonitorBreadcrumbs', () => {
 
     const Component = () => {
       useMonitorBreadcrumb({
-        activeStep: { monitor: { id: 'test-monitor' } } as Ping,
-        details: { timestamp: '2021-01-04T11:25:19.104Z' } as JourneyState['details'],
+        activeStep: { monitor: { id: 'test-monitor', check_group: 'fake-test-group' } } as Ping,
+        details: {
+          timestamp: '2021-01-04T11:25:19.104Z',
+          journey: {
+            monitor: { id: 'test-monitor', check_group: 'fake-test-group' },
+          },
+        } as JourneyState['details'],
       });
       return <>Step Water Fall</>;
     };
@@ -67,6 +72,80 @@ describe('useMonitorBreadcrumbs', () => {
           "href": "/app/uptime/monitor/dGVzdC1tb25pdG9y",
           "onClick": [Function],
           "text": "test-monitor",
+        },
+        Object {
+          "href": "/app/uptime/journey/fake-test-group/steps",
+          "onClick": [Function],
+          "text": "Jan 4, 2021 @ 06:25:19.104",
+        },
+      ]
+    `);
+  });
+
+  it('sets the given breadcrumbs for performance breakdown page', () => {
+    let breadcrumbObj: ChromeBreadcrumb[] = [];
+    const getBreadcrumbs = () => {
+      return breadcrumbObj;
+    };
+
+    const core = {
+      chrome: {
+        ...chromeServiceMock.createStartContract(),
+        setBreadcrumbs: (newBreadcrumbs: ChromeBreadcrumb[]) => {
+          breadcrumbObj = newBreadcrumbs;
+        },
+      },
+      uiSettings: {
+        ...uiSettingsServiceMock.createSetupContract(),
+        get(key: string, defaultOverride?: any): any {
+          return `MMM D, YYYY @ HH:mm:ss.SSS` || defaultOverride;
+        },
+        get$(key: string, defaultOverride?: any): any {
+          return of(`MMM D, YYYY @ HH:mm:ss.SSS`) || of(defaultOverride);
+        },
+      },
+    };
+
+    const Component = () => {
+      useMonitorBreadcrumb({
+        activeStep: { monitor: { id: 'test-monitor', check_group: 'fake-test-group' } } as Ping,
+        details: {
+          timestamp: '2021-01-04T11:25:19.104Z',
+          journey: {
+            monitor: { id: 'test-monitor', check_group: 'fake-test-group' },
+          },
+        } as JourneyState['details'],
+        performanceBreakDownView: true,
+      });
+      return <>Step Water Fall</>;
+    };
+
+    render(
+      <Route path={OVERVIEW_ROUTE}>
+        <Component />
+      </Route>,
+      { core }
+    );
+
+    expect(getBreadcrumbs()).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "href": "/app/uptime",
+          "onClick": [Function],
+          "text": "Uptime",
+        },
+        Object {
+          "href": "/app/uptime/monitor/dGVzdC1tb25pdG9y",
+          "onClick": [Function],
+          "text": "test-monitor",
+        },
+        Object {
+          "href": "/app/uptime/journey/fake-test-group/steps",
+          "onClick": [Function],
+          "text": "Jan 4, 2021 @ 06:25:19.104",
+        },
+        Object {
+          "text": "Performance breakdown",
         },
       ]
     `);
