a87b8baa12ae69ae4f8580c27384798a09bc3c42
Casper Hübertz <casper@formgeist.com>
2021-02-11 14:16:07 +0100

    [APM] Refactored the agent icon to use EuiIcon

--- x-pack/plugins/apm/public/components/shared/AgentIcon/index.tsx
+++ x-pack/plugins/apm/public/components/shared/AgentIcon/index.tsx
@@ -6,8 +6,8 @@
  */
 
 import React from 'react';
+import { EuiIcon } from '@elastic/eui';
 import { AgentName } from '../../../../typings/es_schemas/ui/fields/agent';
-import { useTheme } from '../../../hooks/use_theme';
 import { getAgentIcon } from './get_agent_icon';
 
 interface Props {
@@ -15,10 +15,8 @@ interface Props {
 }
 
 export function AgentIcon(props: Props) {
-  const theme = useTheme();
   const { agentName } = props;
-  const size = theme.eui.euiIconSizes.large;
   const icon = getAgentIcon(agentName);
 
-  return <img src={icon} height={size} width={size} alt={agentName} />;
+  return <EuiIcon type={icon} size="l" title={agentName} />;
 }
