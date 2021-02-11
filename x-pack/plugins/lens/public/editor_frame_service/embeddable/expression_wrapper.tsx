18922cd4360e13c8ef265211ff7959b75ffce2d3
dej611 <dej611@gmail.com>
2021-02-11 11:30:36 +0100

    :lipstick: Refactor embeddable layout for errors

--- x-pack/plugins/lens/public/editor_frame_service/embeddable/expression_wrapper.tsx
+++ x-pack/plugins/lens/public/editor_frame_service/embeddable/expression_wrapper.tsx
@@ -8,7 +8,7 @@
 import React from 'react';
 import { I18nProvider } from '@kbn/i18n/react';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { EuiFlexGroup, EuiFlexItem, EuiText, EuiIcon } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiText, EuiIcon, EuiEmptyPrompt } from '@elastic/eui';
 import {
   ExpressionRendererEvent,
   ReactExpressionRendererType,
@@ -44,46 +44,37 @@ interface VisualizationErrorProps {
 }
 
 export function VisualizationErrorPanel({ errors }: VisualizationErrorProps) {
-  const defaultMessage = (
-    <EuiFlexItem>
-      <EuiText size="s">
-        <FormattedMessage
-          id="xpack.lens.embeddable.failure"
-          defaultMessage="Visualization couldn't be displayed"
-        />
-      </EuiText>
-    </EuiFlexItem>
-  );
-  // only showing one error as for now
-  const customError =
-    errors == null ? null : (
-      <EuiFlexItem className="eui-textBreakAll">{errors[0].longMessage}</EuiFlexItem>
-    );
-  // show that there are more errors, but it requires to open the editor to actually see them
-  const moreErrors =
-    errors && errors.length > 1 ? (
-      <EuiFlexItem>
-        <FormattedMessage
-          id="xpack.lens.embeddable.moreErrors"
-          defaultMessage="(Edit in Lens editor to see more errors)"
-        />
-      </EuiFlexItem>
-    ) : null;
   return (
-    <div className="lnsExpressionRenderer__embeddedError">
-      <EuiFlexGroup
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        style={{ maxWidth: '100%' }}
-        data-test-subj="embeddable-lens-failure"
-      >
-        <EuiFlexItem grow={false}>
-          <EuiIcon type="alert" size="xl" color="danger" />
-        </EuiFlexItem>
-        {customError || defaultMessage}
-        {moreErrors}
-      </EuiFlexGroup>
+    <div className="lnsEmbeddedError">
+      <EuiEmptyPrompt
+        iconType="alert"
+        iconColor="danger"
+        body={
+          <>
+            {errors ? (
+              <>
+                <p>{errors[0].longMessage}</p>
+                <p>
+                  <FormattedMessage
+                    id="xpack.lens.embeddable.moreErrors"
+                    defaultMessage="Edit in Lens editor {errors, plural, one {to address it} other {to see more errors} }"
+                    values={{
+                      errors: errors.length,
+                    }}
+                  />
+                </p>
+              </>
+            ) : (
+              <p>
+                <FormattedMessage
+                  id="xpack.lens.embeddable.failure"
+                  defaultMessage="Visualization couldn't be displayed"
+                />
+              </p>
+            )}
+          </>
+        }
+      />
     </div>
   );
 }
