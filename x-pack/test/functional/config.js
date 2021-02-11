737c7691e143cb4cdaa2d880dba7e3a11ec05398
Rudolf Meijering <skaapgif@gmail.com>
2021-02-11 12:44:50 +0100

    Disable monitoring plugin email notifications in functional tests

--- x-pack/test/functional/config.js
+++ x-pack/test/functional/config.js
@@ -90,6 +90,13 @@ export default async function ({ readConfigFile }) {
         '--xpack.encryptedSavedObjects.encryptionKey="DkdXazszSCYexXqz4YktBGHCRkV6hyNK"',
         '--timelion.ui.enabled=true',
         '--savedObjects.maxImportPayloadBytes=10485760', // for OSS test management/_import_objects
+        // Disable monitoring plugin's email notifications, otherwise the
+        // monitoring plugin tries to read UiSettings while esArchiver is
+        // loading data causing the test Kibana server to crash
+        '--monitoring.cluster_alerts.email_notifications.enabled=false',
+        '--monitoring.cluster_alerts.email_notifications.email_address=e',
+        '--logging.verbose=true',
+        '--elasticsearch.logQueries=true',
       ],
     },
     uiSettings: {
