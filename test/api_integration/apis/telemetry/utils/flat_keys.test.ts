f0cb981ad4d0405051ad145b32e34777ca277be1
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2021-02-11 12:23:20 +0100

    Add unit tests to the test utils

---
+++ test/api_integration/apis/telemetry/utils/flat_keys.test.ts
@@ -0,0 +1,115 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { flatKeys } from './flat_keys';
+
+describe(`flatKeys`, () => {
+  test('no keys to be listed', () => {
+    expect(flatKeys({})).toStrictEqual([]);
+  });
+  test('one-level list', () => {
+    expect(
+      flatKeys({
+        prop1: 1,
+        prop2: 'a',
+        prop3: true,
+        prop4: [],
+      })
+    ).toStrictEqual(['prop1', 'prop2', 'prop3', 'prop4']);
+  });
+  test('two-level list', () => {
+    expect(
+      flatKeys({
+        prop1: 1,
+        prop2: 'a',
+        prop3: true,
+        prop4: [],
+        prop5: [1],
+        prop6: {
+          prop6_1: 1,
+        },
+      })
+    ).toStrictEqual(['prop1', 'prop2', 'prop3', 'prop4', 'prop5.0', 'prop6.prop6_1']);
+  });
+  test('three-level list', () => {
+    expect(
+      flatKeys({
+        prop1: 1,
+        prop2: 'a',
+        prop3: true,
+        prop4: [],
+        prop5: [1],
+        prop6: {
+          prop6_1: 1,
+          prop6_2: {
+            prop6_2_1: 1,
+          },
+        },
+        prop7: [{ a: 1, b: [] }],
+        prop8: [1, true, { a: 1 }],
+      })
+    ).toStrictEqual([
+      'prop1',
+      'prop2',
+      'prop3',
+      'prop4',
+      'prop5.0',
+      'prop6.prop6_1',
+      'prop6.prop6_2.prop6_2_1',
+      'prop7.0.a',
+      'prop7.0.b',
+      'prop8.0',
+      'prop8.1',
+      'prop8.2.a',
+    ]);
+  });
+  test('four-level+ list: it stays at 3 levels only', () => {
+    expect(
+      flatKeys({
+        prop1: 1,
+        prop2: 'a',
+        prop3: true,
+        prop4: [],
+        prop5: [1],
+        prop6: {
+          prop6_1: 1,
+          prop6_2: {
+            prop6_2_1: 1,
+            prop6_2_2: {
+              prop6_2_2_1: 1,
+            },
+          },
+        },
+        prop7: [{ a: 1, b: [], c: [1], d: [{ a: 1 }], e: [1, { a: 1 }] }],
+        prop8: [1, true, { a: 1 }],
+      })
+    ).toStrictEqual([
+      'prop1',
+      'prop2',
+      'prop3',
+      'prop4',
+      'prop5.0',
+      'prop6.prop6_1',
+      'prop6.prop6_2.prop6_2_1',
+      'prop6.prop6_2.prop6_2_2',
+      // 'prop6.prop6_2.prop6_2_2.prop6_2_2_1',  Not reported because of the depth-limit
+      'prop7.0.a',
+      'prop7.0.b',
+      'prop7.0.c',
+      // 'prop7.0.c.0', Not reported because of the depth-limit
+      'prop7.0.d',
+      // 'prop7.0.d.0.a', Not reported because of the depth-limit
+      'prop7.0.e',
+      // 'prop7.0.e.0', Not reported because of the depth-limit
+      // 'prop7.0.e.1.a', Not reported because of the depth-limit
+      'prop8.0',
+      'prop8.1',
+      'prop8.2.a',
+    ]);
+  });
+});
--- test/api_integration/apis/telemetry/utils/flat_keys.ts
+++ test/api_integration/apis/telemetry/utils/flat_keys.ts
@@ -6,21 +6,22 @@
  * Side Public License, v 1.
  */
 
-/*
+import _ from 'lodash';
+
+/**
  * Create a single-level array with strings for all the paths to values in the
  * source object, up to 3 deep. Going deeper than 3 causes a bit too much churn
  * in the tests.
+ * @param source The object to extract the keys from.
  */
-import _ from 'lodash';
-
 export function flatKeys(source: Record<string, unknown>) {
   const recursivelyFlatKeys = (obj: unknown, path: string[] = [], depth = 0): string[] => {
-    return depth < 3 && _.isObject(obj)
+    return depth < 3 && _.isObject(obj) && _.size(obj) > 0
       ? Object.entries(obj).reduce(
           (acc, [k, v]) => [...acc, ...recursivelyFlatKeys(v, [...path, k], depth + 1)],
           [] as string[]
         )
-      : [path.join('.')];
+      : [path.join('.')].filter(Boolean);
   };
 
   return _.uniq(_.flattenDeep(recursivelyFlatKeys(source))).sort((a, b) => a.localeCompare(b));
