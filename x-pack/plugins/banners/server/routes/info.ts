570dcc07b52245e44cd4b43679e839673b7c7eaa
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2021-02-11 10:12:24 +0100

    Implement custom global header banner (#87438)
    * first draft
    
    * update plugin list
    
    * fix tsproject
    
    * update bundle limits file
    
    * remove unused start dep
    
    * adapt imports
    
    * POC of footer banner
    
    * update styles, mostly
    
    * plug banner to uiSettings
    
    * adding some unit tests
    
    * add tests on sort_fields
    
    * cleanup sums in sass mixins
    
    * some self review stuff
    
    * update generated doc
    
    * add tests for color field
    
    * update chrome header test snapshots
    
    * retrieve license info from the server
    
    * switch from uiSettings to plugin config
    
    * update plugin list description
    
    * update default colors
    
    * NIT
    
    * add markdown support
    
    * fix banner overlap in fullscreen mode
    
    * change banner height to 32px
    
    * change banner's font size to 14
    
    * delete unused uiSettings

---
+++ x-pack/plugins/banners/server/routes/info.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { ILicense } from '../../../licensing/server';
+import { BannerInfoResponse, BannerConfiguration } from '../../common';
+import { BannersRouter } from '../types';
+
+export const registerInfoRoute = (router: BannersRouter, config: BannerConfiguration) => {
+  router.get(
+    {
+      path: '/api/banners/info',
+      validate: false,
+      options: {
+        authRequired: false,
+      },
+    },
+    (ctx, req, res) => {
+      const allowed = isValidLicense(ctx.licensing.license);
+
+      return res.ok({
+        body: {
+          allowed,
+          banner: config,
+        } as BannerInfoResponse,
+      });
+    }
+  );
+};
+
+const isValidLicense = (license: ILicense): boolean => {
+  return license.hasAtLeast('gold');
+};
