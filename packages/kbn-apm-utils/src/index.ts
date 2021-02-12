4c1bc7c7bce7b2738a24bf06d9a12becda407b06
Dario Gieselaar <dario.gieselaar@elastic.co>
2021-02-12 17:13:53 +0100

    Await Promise.resolve() to prevent ended spans from creating children

--- packages/kbn-apm-utils/src/index.ts
+++ packages/kbn-apm-utils/src/index.ts
@@ -22,27 +22,44 @@ export function parseSpanOptions(optionsOrName: SpanOptions | string) {
   return options;
 }
 
-export function withSpan<T>(optionsOrName: SpanOptions | string, cb: () => Promise<T>): Promise<T> {
+const runInNewContext = <T extends (...args: any[]) => any>(cb: T): ReturnType<T> => {
+  const resource = new asyncHooks.AsyncResource('fake_async');
+
+  return resource.runInAsyncScope(cb);
+};
+
+export async function withSpan<T>(
+  optionsOrName: SpanOptions | string,
+  cb: () => Promise<T>
+): Promise<T> {
   const options = parseSpanOptions(optionsOrName);
 
   const { name, type, subtype, labels } = options;
 
   if (!agent.isStarted()) {
-    return cb() as any;
+    return cb();
   }
+
+  // When a span starts, it's marked as the active span in its context.
+  // When it ends, it's not untracked, which means that if a span
+  // starts directly after this one ends, the newly started span is a
+  // child of this span, even though it should be a sibling.
+  // To mitigate this, we queue a microtask by awaiting a promise.
+  await Promise.resolve();
 
   const span = agent.startSpan(name);
 
   if (!span) {
-    return cb() as any;
+    return cb();
   }
 
-  const resource = new asyncHooks.AsyncResource('fake_async');
+  // If a span is created in the same context as the span that we just
+  // started, it will be a sibling, not a child. E.g., the Elasticsearch span
+  // that is created when calling search() happens in the same context. To
+  // mitigate this we create a new context.
 
-  return resource.runInAsyncScope(() => {
-    // set the active span for the newly created async context
+  return runInNewContext(() => {
     // @ts-ignore
-    agent._instrumentation.activeSpan = span;
     if (type) {
       span.type = type;
     }
@@ -54,22 +71,17 @@ export function withSpan<T>(optionsOrName: SpanOptions | string, cb: () => Promi
       span.addLabels(labels);
     }
 
-    const promise = cb();
-
-    return promise
-      .then(
-        (res) => {
-          span.outcome = 'success';
-          return res;
-        },
-        (err) => {
-          span.outcome = 'failure';
-          throw err;
-        }
-      )
+    return cb()
+      .then((res) => {
+        span.outcome = 'success';
+        return res;
+      })
+      .catch((err) => {
+        span.outcome = 'failure';
+        throw err;
+      })
       .finally(() => {
         span.end();
-        resource.emitDestroy();
       });
   });
 }
