0ada1c55c18db37d0d3cda7bb4d94209b96f3a29
lcawl <lcawley@elastic.co>
2021-02-11 18:41:11 -0800

    [ML] Edits text in Data Visualizer cards

--- x-pack/plugins/ml/public/application/datavisualizer/index_based/components/actions_panel/actions_panel.tsx
+++ x-pack/plugins/ml/public/application/datavisualizer/index_based/components/actions_panel/actions_panel.tsx
@@ -110,7 +110,7 @@ export const ActionsPanel: FC<Props> = ({ indexPattern, searchString, searchQuer
             <h2>
               <FormattedMessage
                 id="xpack.ml.datavisualizer.actionsPanel.createJobTitle"
-                defaultMessage="Create Job"
+                defaultMessage="Create job"
               />
             </h2>
           </EuiTitle>
@@ -118,14 +118,6 @@ export const ActionsPanel: FC<Props> = ({ indexPattern, searchString, searchQuer
           {showCreateAnomalyDetectionJob && (
             <>
               <div hidden={recognizerResultsCount === 0}>
-                <EuiText size="s" color="subdued">
-                  <p>
-                    <FormattedMessage
-                      id="xpack.ml.datavisualizer.actionsPanel.selectKnownConfigurationDescription"
-                      defaultMessage="Select known configurations for recognized data:"
-                    />
-                  </p>
-                </EuiText>
                 <EuiSpacer size="m" />
                 <EuiFlexGroup gutterSize="l" responsive={true} wrap={true}>
                   <DataRecognizer
@@ -136,26 +128,18 @@ export const ActionsPanel: FC<Props> = ({ indexPattern, searchString, searchQuer
                 </EuiFlexGroup>
                 <EuiSpacer size="l" />
               </div>
-              <EuiText size="s" color="subdued">
-                <p>
-                  <FormattedMessage
-                    id="xpack.ml.datavisualizer.actionsPanel.createJobDescription"
-                    defaultMessage="Use the Advanced job wizard to create a job to find anomalies in this data:"
-                  />
-                </p>
-              </EuiText>
               <EuiSpacer size="m" />
               <LinkCard
                 href={createJobLink}
                 icon="createAdvancedJob"
                 title={i18n.translate('xpack.ml.datavisualizer.actionsPanel.advancedTitle', {
-                  defaultMessage: 'Advanced',
+                  defaultMessage: 'Advanced anomaly detection',
                 })}
                 description={i18n.translate(
                   'xpack.ml.datavisualizer.actionsPanel.advancedDescription',
                   {
                     defaultMessage:
-                      'Create a job with the full range of options for more advanced use cases',
+                      'Create a job with the full range of options for more advanced use cases.',
                   }
                 )}
                 data-test-subj="mlDataVisualizerCreateAdvancedJobCard"
@@ -167,14 +151,6 @@ export const ActionsPanel: FC<Props> = ({ indexPattern, searchString, searchQuer
       )}
       {mlAvailable && indexPattern.id !== undefined && createDataFrameAnalyticsLink && (
         <>
-          <EuiText size="s" color="subdued">
-            <p>
-              <FormattedMessage
-                id="xpack.ml.datavisualizer.actionsPanel.createDataFrameAnalyticsDescription"
-                defaultMessage="Use the Data Frame Analytics wizard to perform analyses of your data:"
-              />
-            </p>
-          </EuiText>
           <EuiSpacer size="m" />
           <LinkCard
             href={createDataFrameAnalyticsLink}
@@ -182,13 +158,14 @@ export const ActionsPanel: FC<Props> = ({ indexPattern, searchString, searchQuer
             description={i18n.translate(
               'xpack.ml.datavisualizer.actionsPanel.dataframeTypesDescription',
               {
-                defaultMessage: 'Create outlier detection, regression, or classification analytics',
+                defaultMessage:
+                  'Create outlier detection, regression, or classification analytics.',
               }
             )}
             title={
               <FormattedMessage
                 id="xpack.ml.datavisualizer.actionsPanel.dataframeAnalyticsTitle"
-                defaultMessage="Data Frame Analytics"
+                defaultMessage="Data frame analytics"
               />
             }
             data-test-subj="mlDataVisualizerCreateDataFrameAnalyticsCard"
@@ -203,7 +180,7 @@ export const ActionsPanel: FC<Props> = ({ indexPattern, searchString, searchQuer
             <h2>
               <FormattedMessage
                 id="xpack.ml.datavisualizer.actionsPanel.exploreTitle"
-                defaultMessage="Explore"
+                defaultMessage="Explore your data"
               />
             </h2>
           </EuiTitle>
@@ -214,7 +191,7 @@ export const ActionsPanel: FC<Props> = ({ indexPattern, searchString, searchQuer
             description={i18n.translate(
               'xpack.ml.datavisualizer.actionsPanel.viewIndexInDiscoverDescription',
               {
-                defaultMessage: 'Explore index in Discover',
+                defaultMessage: 'Explore the documents in your index.',
               }
             )}
             title={
