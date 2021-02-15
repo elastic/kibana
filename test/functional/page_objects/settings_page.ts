4db968c45789dbace4c9a9edd20e7337d3106052
Matt Kime <matt@mattki.me>
2021-02-15 17:18:05 -0600

    add functional test

--- test/functional/page_objects/settings_page.ts
+++ test/functional/page_objects/settings_page.ts
@@ -486,6 +486,46 @@ export function SettingsPageProvider({ getService, getPageObjects }: FtrProvider
       await this.clickSaveScriptedField();
     }
 
+    async addRuntimeField(name: string, type: string, script: string) {
+      await this.clickAddField();
+      await this.setFieldName(name);
+      await this.setFieldType(type);
+      if (script) {
+        await this.toggleScriptRow();
+        await this.setFieldScript(script);
+      }
+      await this.clickSaveField();
+    }
+
+    async clickAddField() {
+      log.debug('click Add Field');
+      await testSubjects.click('addField');
+    }
+
+    async clickSaveField() {
+      log.debug('click Save');
+      await testSubjects.click('fieldSaveButton');
+    }
+
+    async setFieldName(name: string) {
+      log.debug('set field name = ' + name);
+      await testSubjects.setValue('nameField', name);
+    }
+
+    async setFieldType(type: string) {
+      log.debug('set type = ' + type);
+      await testSubjects.setValue('typeField', type);
+    }
+
+    async setFieldScript(script: string) {
+      log.debug('set script = ' + script);
+      await testSubjects.setValue('scriptField', script);
+    }
+
+    async toggleScriptRow() {
+      await (await testSubjects.findAll('formRowToggle'))[1].click();
+    }
+
     async clickAddScriptedField() {
       log.debug('click Add Scripted Field');
       await testSubjects.click('addScriptedFieldLink');
