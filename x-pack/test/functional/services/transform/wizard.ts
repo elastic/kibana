d18cd4d11f3a18d77f3f7fe3d677717e9e26158b
Walter Rafelsberger <walter@elastic.co>
2021-02-16 16:19:45 +0100

    [ML] Fix color stats sorting for asserting data grid mini charts.

--- x-pack/test/functional/services/transform/wizard.ts
+++ x-pack/test/functional/services/transform/wizard.ts
@@ -213,17 +213,25 @@ export function TransformWizardProvider({ getService }: FtrProviderContext) {
             await testSubjects.existOrFail(`mlDataGridChart-${index}-histogram`);
 
             if (expected.colorStats !== undefined) {
-              const actualColorStats = await canvasElement.getColorStats(
-                `[data-test-subj="mlDataGridChart-${index}-histogram"] .echCanvasRenderer`,
-                expected.colorStats
+              const sortedExpectedColorStats = [...expected.colorStats].sort((a, b) =>
+                a.key.localeCompare(b.key)
               );
 
+              const actualColorStats = await canvasElement.getColorStats(
+                `[data-test-subj="mlDataGridChart-${index}-histogram"] .echCanvasRenderer`,
+                sortedExpectedColorStats
+              );
+
+              expect(actualColorStats.length).to.eql(
+                sortedExpectedColorStats.length,
+                `Expected and actual color stats for column '${expected.id}' should have the same amount of elements. Expected: ${sortedExpectedColorStats.length} (got ${actualColorStats.length})`
+              );
               expect(actualColorStats.every((d) => d.withinTolerance)).to.eql(
                 true,
                 `Color stats for column '${
                   expected.id
                 }' should be within tolerance. Expected: '${JSON.stringify(
-                  expected.colorStats
+                  sortedExpectedColorStats
                 )}' (got '${JSON.stringify(actualColorStats)}')`
               );
             }
