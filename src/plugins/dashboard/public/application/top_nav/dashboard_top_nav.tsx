8f3de935fe569eea649257f3294d7de14eadca7d
Devon Thomson <devon.thomson@hotmail.com>
2021-02-16 13:03:56 -0500

    [Time to Visualize] Combine Discard & Cancel (#91267)
    * recombined discard and cancel button functionality
    # Conflicts:
    #	test/functional/apps/dashboard/view_edit.ts

--- src/plugins/dashboard/public/application/top_nav/dashboard_top_nav.tsx
+++ src/plugins/dashboard/public/application/top_nav/dashboard_top_nav.tsx
@@ -43,7 +43,7 @@ import { showOptionsPopover } from './show_options_popover';
 import { TopNavIds } from './top_nav_ids';
 import { ShowShareModal } from './show_share_modal';
 import { PanelToolbar } from './panel_toolbar';
-import { confirmDiscardUnsavedChanges } from '../listing/confirm_overlays';
+import { confirmDiscardOrKeepUnsavedChanges } from '../listing/confirm_overlays';
 import { OverlayRef } from '../../../../../core/public';
 import { getNewDashboardTitle } from '../../dashboard_strings';
 import { DASHBOARD_PANELS_UNSAVED_ID } from '../lib/dashboard_panel_storage';
@@ -152,34 +152,53 @@ export function DashboardTopNav({
     }
   }, [state.addPanelOverlay]);
 
-  const onDiscardChanges = useCallback(() => {
-    function revertChangesAndExitEditMode() {
-      dashboardStateManager.resetState();
-      dashboardStateManager.clearUnsavedPanels();
-
-      // We need to do a hard reset of the timepicker. appState will not reload like
-      // it does on 'open' because it's been saved to the url and the getAppState.previouslyStored() check on
-      // reload will cause it not to sync.
-      if (dashboardStateManager.getIsTimeSavedWithDashboard()) {
-        dashboardStateManager.syncTimefilterWithDashboardTime(timefilter);
-        dashboardStateManager.syncTimefilterWithDashboardRefreshInterval(timefilter);
-      }
-      dashboardStateManager.switchViewMode(ViewMode.VIEW);
-    }
-    confirmDiscardUnsavedChanges(core.overlays, revertChangesAndExitEditMode);
-  }, [core.overlays, dashboardStateManager, timefilter]);
-
   const onChangeViewMode = useCallback(
     (newMode: ViewMode) => {
       clearAddPanel();
-      if (savedDashboard?.id && allowByValueEmbeddables) {
-        const { getFullEditPath, title, id } = savedDashboard;
-        chrome.recentlyAccessed.add(getFullEditPath(newMode === ViewMode.EDIT), title, id);
+      const isPageRefresh = newMode === dashboardStateManager.getViewMode();
+      const isLeavingEditMode = !isPageRefresh && newMode === ViewMode.VIEW;
+      const willLoseChanges = isLeavingEditMode && dashboardStateManager.getIsDirty(timefilter);
+
+      function switchViewMode() {
+        dashboardStateManager.switchViewMode(newMode);
+        dashboardStateManager.restorePanels();
+
+        if (savedDashboard?.id && allowByValueEmbeddables) {
+          const { getFullEditPath, title, id } = savedDashboard;
+          chrome.recentlyAccessed.add(getFullEditPath(newMode === ViewMode.EDIT), title, id);
+        }
       }
-      dashboardStateManager.switchViewMode(newMode);
-      dashboardStateManager.restorePanels();
+
+      if (!willLoseChanges) {
+        switchViewMode();
+        return;
+      }
+
+      function discardChanges() {
+        dashboardStateManager.resetState();
+        dashboardStateManager.clearUnsavedPanels();
+
+        // We need to do a hard reset of the timepicker. appState will not reload like
+        // it does on 'open' because it's been saved to the url and the getAppState.previouslyStored() check on
+        // reload will cause it not to sync.
+        if (dashboardStateManager.getIsTimeSavedWithDashboard()) {
+          dashboardStateManager.syncTimefilterWithDashboardTime(timefilter);
+          dashboardStateManager.syncTimefilterWithDashboardRefreshInterval(timefilter);
+        }
+        dashboardStateManager.switchViewMode(ViewMode.VIEW);
+      }
+      confirmDiscardOrKeepUnsavedChanges(core.overlays).then((selection) => {
+        if (selection === 'discard') {
+          discardChanges();
+        }
+        if (selection !== 'cancel') {
+          switchViewMode();
+        }
+      });
     },
     [
+      timefilter,
+      core.overlays,
       clearAddPanel,
       savedDashboard,
       dashboardStateManager,
@@ -381,7 +400,6 @@ export function DashboardTopNav({
       },
       [TopNavIds.EXIT_EDIT_MODE]: () => onChangeViewMode(ViewMode.VIEW),
       [TopNavIds.ENTER_EDIT_MODE]: () => onChangeViewMode(ViewMode.EDIT),
-      [TopNavIds.DISCARD_CHANGES]: onDiscardChanges,
       [TopNavIds.SAVE]: runSave,
       [TopNavIds.QUICK_SAVE]: runQuickSave,
       [TopNavIds.CLONE]: runClone,
@@ -417,7 +435,6 @@ export function DashboardTopNav({
   }, [
     dashboardCapabilities,
     dashboardStateManager,
-    onDiscardChanges,
     onChangeViewMode,
     savedDashboard,
     runClone,
