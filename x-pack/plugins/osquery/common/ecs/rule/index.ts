3edc7f2f2ad41d68dde7e53df3b5aef143554215
Madison Caldwell <madison.caldwell@elastic.co>
2021-02-17 17:24:17 +0000

    unnecessary change

--- x-pack/plugins/osquery/common/ecs/rule/index.ts
+++ x-pack/plugins/osquery/common/ecs/rule/index.ts
@@ -28,7 +28,7 @@ export interface RuleEcs {
   tags?: string[];
   threat?: unknown;
   threshold?: {
-    field: string | string[];
+    field: string;
     value: number;
   };
   type?: string[];
