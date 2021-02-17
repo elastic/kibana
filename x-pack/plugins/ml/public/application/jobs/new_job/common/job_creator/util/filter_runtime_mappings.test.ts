8230fd6aeedc4965ceef5b28cd876a66b0ab84b1
James Gowdy <jgowdy@elastic.co>
2021-02-17 09:41:53 +0000

    removing async from tests

--- x-pack/plugins/ml/public/application/jobs/new_job/common/job_creator/util/filter_runtime_mappings.test.ts
+++ x-pack/plugins/ml/public/application/jobs/new_job/common/job_creator/util/filter_runtime_mappings.test.ts
@@ -99,7 +99,7 @@ describe('filter_runtime_mappings', () => {
       datafeed = getDatafeed();
     });
 
-    test('returns no runtime mappings, no mappings in aggs', async () => {
+    test('returns no runtime mappings, no mappings in aggs', () => {
       const resp = filterRuntimeMappings(job, datafeed);
       expect(Object.keys(resp.runtime_mappings).length).toEqual(0);
 
@@ -108,7 +108,7 @@ describe('filter_runtime_mappings', () => {
       expect(resp.discarded_mappings.airline_lower).not.toEqual(undefined);
     });
 
-    test('returns no runtime mappings, no runtime mappings in datafeed', async () => {
+    test('returns no runtime mappings, no runtime mappings in datafeed', () => {
       datafeed.runtime_mappings = undefined;
       const resp = filterRuntimeMappings(job, datafeed);
       expect(Object.keys(resp.runtime_mappings).length).toEqual(0);
@@ -118,7 +118,7 @@ describe('filter_runtime_mappings', () => {
       expect(resp.discarded_mappings.airline_lower).toEqual(undefined);
     });
 
-    test('return one runtime mapping and one unused mapping, mappings in aggs', async () => {
+    test('return one runtime mapping and one unused mapping, mappings in aggs', () => {
       datafeed.aggregations = getAggs();
       const resp = filterRuntimeMappings(job, datafeed);
       expect(Object.keys(resp.runtime_mappings).length).toEqual(1);
@@ -128,7 +128,7 @@ describe('filter_runtime_mappings', () => {
       expect(resp.discarded_mappings.airline_lower).not.toEqual(undefined);
     });
 
-    test('return no runtime mappings, no mappings in aggs', async () => {
+    test('return no runtime mappings, no mappings in aggs', () => {
       datafeed.aggregations = getAggs();
       datafeed.aggregations!.buckets!.aggregations!.responsetime!.avg!.field! = 'responsetime';
 
@@ -140,7 +140,7 @@ describe('filter_runtime_mappings', () => {
       expect(resp.discarded_mappings.airline_lower).not.toEqual(undefined);
     });
 
-    test('return one runtime mapping and one unused mapping, no mappings in aggs', async () => {
+    test('return one runtime mapping and one unused mapping, no mappings in aggs', () => {
       // set the detector field to be a runtime mapping
       job.analysis_config.detectors[0].field_name = 'responsetime_big';
       const resp = filterRuntimeMappings(job, datafeed);
@@ -151,7 +151,7 @@ describe('filter_runtime_mappings', () => {
       expect(resp.discarded_mappings.airline_lower).not.toEqual(undefined);
     });
 
-    test('return two runtime mappings, no mappings in aggs', async () => {
+    test('return two runtime mappings, no mappings in aggs', () => {
       // set the detector field to be a runtime mapping
       job.analysis_config.detectors[0].field_name = 'responsetime_big';
       // set the detector by field to be a runtime mapping
@@ -164,7 +164,7 @@ describe('filter_runtime_mappings', () => {
       expect(Object.keys(resp.discarded_mappings).length).toEqual(0);
     });
 
-    test('return two runtime mappings, no mappings in aggs, categorization job', async () => {
+    test('return two runtime mappings, no mappings in aggs, categorization job', () => {
       job.analysis_config.detectors[0].function = 'count';
       // set the detector field to be a runtime mapping
       job.analysis_config.detectors[0].field_name = undefined;
