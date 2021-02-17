c2d6c4f4144a6c6270ca10ee233423dc8973d3f4
scottybollinger <scotty.bollinger@elastic.co>
2021-02-17 12:14:02 -0600

    Add constants

---
+++ x-pack/plugins/enterprise_search/public/applications/shared/role_mapping/constants.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { i18n } from '@kbn/i18n';
+
+export const ANY_AUTH_PROVIDER = '*';
+export const ANY_AUTH_PROVIDER_LABEL = i18n.translate(
+  'xpack.enterpriseSearch.roleMapping.any.label',
+  {
+    defaultMessage: 'Any',
+  }
+);
