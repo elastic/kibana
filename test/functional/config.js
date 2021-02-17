a4a74c81ef11f7d5e9145e69b1fcd2d1090f8f96
Nick Partridge <nick.ryan.partridge@gmail.com>
2021-02-17 13:31:03 -0600

    Enable new chart library setting (#86538) (#91705)

--- test/functional/config.js
+++ test/functional/config.js
@@ -50,6 +50,7 @@ export default async function ({ readConfigFile }) {
       defaults: {
         'accessibility:disableAnimations': true,
         'dateFormat:tz': 'UTC',
+        'visualization:visualize:legacyChartsLibrary': true,
       },
     },
 
