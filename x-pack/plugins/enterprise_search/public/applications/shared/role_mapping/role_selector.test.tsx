4f755d1848ad6156cc12ad9c7c51bc5c5a8e870c
scottybollinger <scotty.bollinger@elastic.co>
2021-02-17 13:40:19 -0600

    Add tests for smaller components
    - AddRoleMappingButton
    - DeleteMappingCallout
    - RoleSelector

---
+++ x-pack/plugins/enterprise_search/public/applications/shared/role_mapping/role_selector.test.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { EuiRadio, EuiCallOut } from '@elastic/eui';
+
+import { RoleSelector } from './role_selector';
+
+describe('RoleSelector', () => {
+  const onChange = jest.fn();
+
+  const props = {
+    disabled: false,
+    disabledText: 'Disabled',
+    roleType: 'user',
+    roleTypeOption: 'option',
+    description: 'This a thing',
+    onChange,
+  };
+
+  it('renders', () => {
+    const wrapper = shallow(<RoleSelector {...props} />);
+
+    expect(wrapper.find(EuiRadio)).toHaveLength(1);
+  });
+
+  it('calls method on change', () => {
+    const wrapper = shallow(<RoleSelector {...props} />);
+    const radio = wrapper.find(EuiRadio);
+    const event = { target: { value: 'bar' } };
+    radio.prop('onChange')(event as any);
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it('renders callout when disabled', () => {
+    const wrapper = shallow(<RoleSelector {...props} disabled />);
+    const radio = wrapper.find(EuiRadio);
+
+    expect(radio.dive().find(EuiCallOut)).toHaveLength(1);
+  });
+});
