119d266a3e00d3b0da1f1e24d2a67fb4a8053a4d
Brian Seeders <brian.seeders@elastic.co>
2021-02-18 14:13:23 -0500

    [CI] Build and publish storybooks (#87701)

--- vars/githubPr.groovy
+++ vars/githubPr.groovy
@@ -169,12 +169,18 @@ def getNextCommentMessage(previousCommentInfo = [:], isFinal = false) {
     ? getBuildStatusIncludingMetrics()
     : buildUtils.getBuildStatus()
 
+  def storybooksUrl = buildState.get('storybooksUrl')
+  def storybooksMessage = storybooksUrl ? "* [Storybooks Preview](${storybooksUrl})" : "* Storybooks not built"
+
   if (!isFinal) {
+    storybooksMessage = storybooksUrl ? storybooksMessage : "* Storybooks not built yet"
+
     def failuresPart = status != 'SUCCESS' ? ', with failures' : ''
     messages << """
       ## :hourglass_flowing_sand: Build in-progress${failuresPart}
       * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
       * Commit: ${getCommitHash()}
+      ${storybooksMessage}
       * This comment will update when the build is complete
     """
   } else if (status == 'SUCCESS') {
@@ -182,6 +188,7 @@ def getNextCommentMessage(previousCommentInfo = [:], isFinal = false) {
       ## :green_heart: Build Succeeded
       * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
       * Commit: ${getCommitHash()}
+      ${storybooksMessage}
       ${getDocsChangesLink()}
     """
   } else if(status == 'UNSTABLE') {
@@ -189,6 +196,7 @@ def getNextCommentMessage(previousCommentInfo = [:], isFinal = false) {
       ## :yellow_heart: Build succeeded, but was flaky
       * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
       * Commit: ${getCommitHash()}
+      ${storybooksMessage}
       ${getDocsChangesLink()}
     """.stripIndent()
 
@@ -204,6 +212,7 @@ def getNextCommentMessage(previousCommentInfo = [:], isFinal = false) {
       ## :broken_heart: Build Failed
       * [continuous-integration/kibana-ci/pull-request](${env.BUILD_URL})
       * Commit: ${getCommitHash()}
+      ${storybooksMessage}
       * [Pipeline Steps](${env.BUILD_URL}flowGraphTable) (look for red circles / failed steps)
       * [Interpreting CI Failures](https://www.elastic.co/guide/en/kibana/current/interpreting-ci-failures.html)
       ${getDocsChangesLink()}
