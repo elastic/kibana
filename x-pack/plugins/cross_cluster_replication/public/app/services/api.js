a975d73f24cf9766d43bccc3e5ccb4971e7d0c96
Joe Portner <5295965+jportner@users.noreply.github.com>
2021-02-18 11:45:16 -0500

    Fix calls to `/api/cross_cluster_replication/follower_indices`
    Seen on page: /app/kibana#/management/elasticsearch/cross_cluster_replication/follower_indices

--- x-pack/plugins/cross_cluster_replication/public/app/services/api.js
+++ x-pack/plugins/cross_cluster_replication/public/app/services/api.js
@@ -5,6 +5,7 @@
  */
 
 import chrome from 'ui/chrome';
+import { addSystemApiHeader } from 'ui/system_api';
 import {
   API_BASE_PATH,
   API_REMOTE_CLUSTERS_BASE_PATH,
@@ -62,9 +63,10 @@ export const deleteAutoFollowPattern = (id) => {
 };
 
 /* Follower Index */
-export const loadFollowerIndices = () => (
-  httpClient.get(`${apiPrefix}/follower_indices`).then(extractData)
-);
+export const loadFollowerIndices = (asSystemRequest) => {
+  const headers = asSystemRequest ? addSystemApiHeader({}) : undefined;
+  return httpClient.get(`${apiPrefix}/follower_indices`, { headers }).then(extractData);
+};
 
 export const getFollowerIndex = (id) => (
   httpClient.get(`${apiPrefix}/follower_indices/${encodeURIComponent(id)}`).then(extractData)
