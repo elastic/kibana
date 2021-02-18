f74c894dc38590adc121dda9673768203f7a428e
Brian Seeders <brian.seeders@elastic.co>
2021-02-18 16:17:50 -0500

    Fix-up storybooks to match what's in 7.11

--- test/scripts/jenkins_storybook.sh
+++ test/scripts/jenkins_storybook.sh
@@ -9,8 +9,8 @@ cd "$KIBANA_DIR"
 
 # yarn storybook --site apm # TODO re-enable after being fixed
 yarn storybook --site canvas
-yarn storybook --site ci_composite
 yarn storybook --site codeeditor
+yarn storybook --site ci_composite
 yarn storybook --site dashboard
 yarn storybook --site dashboard_enhanced
 yarn storybook --site data_enhanced
@@ -19,4 +19,3 @@ yarn storybook --site infra
 yarn storybook --site security_solution
 yarn storybook --site ui_actions_enhanced
 yarn storybook --site observability
-yarn storybook --site presentation
