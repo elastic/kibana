0f821b12a0b53a6332803feebf4790b0675c3bd1
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-02-19 21:59:52 +0300

    zip and copy to checkout dir

--- src/dev/code_coverage/shell_scripts/merge_functional.sh
+++ src/dev/code_coverage/shell_scripts/merge_functional.sh
@@ -3,6 +3,9 @@
 COVERAGE_TEMP_DIR=/tmp/extracted_coverage/target/kibana-coverage/
 export COVERAGE_TEMP_DIR
 
+checkoutDir="$(pwd)"
+echo  "checkoutDir=${checkoutDir}"
+
 echo "### Clone kibana to /dev/shm/workspace/"
 mkdir -p /dev/shm/workspace/kibana
 # rsync -avz kibana /dev/shm/workspace/kibana
@@ -18,3 +21,9 @@ yarn kbn bootstrap
 cd ..
 echo "### Merge coverage reports"
 yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.functional.config.js
+
+echo "### zip functional combined report"
+tar -czf kibana-functional-coverage.tar.gz target/kibana-coverage/functional-combined/*
+
+echo "### Copy archive to checkoutDir"
+cp kibana-functional-coverage.tar.gz $checkoutDir
