21252ae62be900f7b0d742901f3161da2a498359
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-02-20 13:25:45 +0300

    cleanup

--- src/dev/code_coverage/shell_scripts/merge_functional.sh
+++ src/dev/code_coverage/shell_scripts/merge_functional.sh
@@ -4,16 +4,15 @@ COVERAGE_TEMP_DIR=/tmp/extracted_coverage/target/kibana-coverage/
 export COVERAGE_TEMP_DIR
 
 checkoutDir="$(pwd)"
-echo  "checkoutDir=${checkoutDir}"
+echo  "### checkoutDir=${checkoutDir}"
 
-echo "### Clone kibana to /dev/shm/workspace/"
-mkdir -p /dev/shm/workspace/kibana
-rsync -ahSD --ignore-errors --force --delete --stats ./ /dev/shm/workspace/kibana/
-cd /dev/shm/workspace/kibana
-echo "### Show folders tree"
-ls -d -- */
+coverageBasePath="/dev/shm/workspace"
+echo "### Clone kibana to ${coverageBasePath}"
+mkdir -p "$coverageBasePath/kibana"
+rsync -ahSD --ignore-errors --force --delete --stats ./ "$coverageBasePath/kibana/"
+cd "$coverageBasePath/kibana"
+
 echo "### bootstrap from x-pack folder"
-# bootstrap from x-pack folder
 cd x-pack
 yarn kbn bootstrap
 # Return to project root
@@ -21,8 +20,8 @@ cd ..
 echo "### Merge coverage reports"
 yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.functional.config.js
 
+echo "### Copy 'target' to ${checkoutDir}"
 rsync -ahSD --ignore-errors --force --delete --stats target "$checkoutDir/"
 
 echo "### Back to $checkoutDir"
 cd "$checkoutDir"
-ls -als
