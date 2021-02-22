c3a548d75555fe1061d7ef27ea5a3e2144fafcd1
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-02-22 23:37:25 +0100

    Fix discover.tsx

--- src/plugins/discover/public/application/components/discover.tsx
+++ src/plugins/discover/public/application/components/discover.tsx
@@ -105,14 +105,13 @@ export function Discover({
     () =>
       getStateColumnActions({
         capabilities,
-        config,
         indexPattern,
         indexPatterns,
         setAppState,
         state,
         useNewFieldsApi,
       }),
-    [capabilities, indexPattern, indexPatterns, setAppState, state, config, useNewFieldsApi]
+    [capabilities, indexPattern, indexPatterns, setAppState, state, useNewFieldsApi]
   );
 
   const onOpenInspector = useCallback(() => {
