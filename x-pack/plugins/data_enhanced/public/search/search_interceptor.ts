fc6a00dbf7e7d76c4f49e3fd4367a86fbd05a481
Liza K <liza.katz@elastic.co>
2021-02-23 17:53:10 +0200

    client cache

--- x-pack/plugins/data_enhanced/public/search/search_interceptor.ts
+++ x-pack/plugins/data_enhanced/public/search/search_interceptor.ts
@@ -15,12 +15,16 @@ import {
   UI_SETTINGS,
   IKibanaSearchRequest,
   SearchSessionState,
+  IKibanaSearchResponse,
+  isCompleteResponse,
 } from '../../../../../src/plugins/data/public';
 import { ENHANCED_ES_SEARCH_STRATEGY, IAsyncSearchOptions, pollSearch } from '../../common';
+import { createRequestHash } from './utils';
 
 export class EnhancedSearchInterceptor extends SearchInterceptor {
   private uiSettingsSub: Subscription;
   private searchTimeout: number;
+  private responseCache: Map<string, IKibanaSearchResponse<any>>;
 
   /**
    * @internal
@@ -28,6 +32,7 @@ export class EnhancedSearchInterceptor extends SearchInterceptor {
   constructor(deps: SearchInterceptorDeps) {
     super(deps);
     this.searchTimeout = deps.uiSettings.get(UI_SETTINGS.SEARCH_TIMEOUT);
+    this.responseCache = new Map();
 
     this.uiSettingsSub = deps.uiSettings
       .get$(UI_SETTINGS.SEARCH_TIMEOUT)
@@ -37,6 +42,7 @@ export class EnhancedSearchInterceptor extends SearchInterceptor {
   }
 
   public stop() {
+    this.responseCache.clear();
     this.uiSettingsSub.unsubscribe();
   }
 
@@ -53,7 +59,14 @@ export class EnhancedSearchInterceptor extends SearchInterceptor {
     });
     const strategy = options?.strategy ?? ENHANCED_ES_SEARCH_STRATEGY;
     const searchOptions = { ...options, strategy, abortSignal: combinedSignal };
-    const search = () => this.runSearch({ id, ...request }, searchOptions);
+    const search = async () => {
+      const shaKey = await createRequestHash(request);
+      const cached = this.responseCache.get(shaKey);
+      if (!id && cached) {
+        return cached;
+      }
+      return this.runSearch({ id, ...request }, searchOptions);
+    };
 
     this.pendingCount$.next(this.pendingCount$.getValue() + 1);
 
@@ -86,6 +99,15 @@ export class EnhancedSearchInterceptor extends SearchInterceptor {
 
     return pollSearch(search, cancel, { ...options, abortSignal: combinedSignal }).pipe(
       tap((response) => (id = response.id)),
+      tap(async (response) => {
+        const shaKey = await createRequestHash(request);
+        if (!this.responseCache.has(shaKey) && isCompleteResponse(response)) {
+          this.responseCache.set(shaKey, response);
+        }
+        setTimeout(() => {
+          this.responseCache.delete(shaKey);
+        }, 30000);
+      }),
       catchError((e: Error) => {
         cancel();
         return throwError(this.handleSearchError(e, timeoutSignal, options));
