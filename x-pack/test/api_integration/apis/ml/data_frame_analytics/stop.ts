f8afaac64597404e1cc08b9840d00547a901bf14
Melissa Alvarez <melissa.alvarez@elastic.co>
2021-02-24 11:44:32 -0800

    wip: move stop and stop_spaces to separate files

---
+++ x-pack/test/api_integration/apis/ml/data_frame_analytics/stop.ts
@@ -0,0 +1,131 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../../../ftr_provider_context';
+import { USER } from '../../../../functional/services/ml/security_common';
+import { DataFrameAnalyticsConfig } from '../../../../../plugins/ml/public/application/data_frame_analytics/common';
+import { DeepPartial } from '../../../../../plugins/ml/common/types/common';
+import { COMMON_REQUEST_HEADERS } from '../../../../functional/services/ml/common_api';
+
+export default ({ getService }: FtrProviderContext) => {
+  const esArchiver = getService('esArchiver');
+  const supertest = getService('supertestWithoutAuth');
+  const ml = getService('ml');
+
+  const jobId = `bm_${Date.now()}`;
+  const generateDestinationIndex = (analyticsId: string) => `user-${analyticsId}`;
+  const commonJobConfig = {
+    source: {
+      index: ['ft_bank_marketing'],
+      query: {
+        match_all: {},
+      },
+    },
+    analysis: {
+      classification: {
+        dependent_variable: 'y',
+        training_percent: 20,
+      },
+    },
+    analyzed_fields: {
+      includes: [],
+      excludes: [],
+    },
+    model_memory_limit: '60mb',
+    allow_lazy_start: false, // default value
+    max_num_threads: 1, // default value
+  };
+
+  const destinationIndex = generateDestinationIndex(`${jobId}_0`);
+
+  const testJobConfigs: Array<DeepPartial<DataFrameAnalyticsConfig>> = [
+    {
+      id: `${jobId}_0`,
+      description: 'Test stop for analytics',
+      dest: {
+        index: destinationIndex,
+        results_field: 'ml',
+      },
+      ...commonJobConfig,
+    },
+  ];
+
+  async function createJobs(mockJobConfigs: Array<DeepPartial<DataFrameAnalyticsConfig>>) {
+    for (const jobConfig of mockJobConfigs) {
+      await ml.api.createDataFrameAnalyticsJob(jobConfig as DataFrameAnalyticsConfig);
+    }
+  }
+
+  describe('POST data_frame/analytics/{analyticsId}/_stop', () => {
+    before(async () => {
+      await esArchiver.loadIfNeeded('ml/bm_classification');
+      await ml.testResources.setKibanaTimeZoneToUTC();
+      await createJobs(testJobConfigs);
+    });
+
+    after(async () => {
+      await ml.api.cleanMlIndices();
+      await ml.api.deleteIndices(destinationIndex);
+    });
+
+    describe('StopsDataFrameAnalyticsJob', () => {
+      it('should stop analytics job for specified id when job exists', async () => {
+        const analyticsId = `${jobId}_0`;
+
+        const { body } = await supertest
+          .post(`/api/ml/data_frame/analytics/${analyticsId}/_stop`)
+          .auth(USER.ML_POWERUSER, ml.securityCommon.getPasswordForUser(USER.ML_POWERUSER))
+          .set(COMMON_REQUEST_HEADERS)
+          .expect(200);
+
+        expect(body).not.to.be(undefined);
+        expect(body.stopped).to.be(true);
+      });
+
+      it('should show 404 error if job does not exist', async () => {
+        const id = `${jobId}_invalid`;
+        const message = `No known job with id '${id}'`;
+
+        const { body } = await supertest
+          .post(`/api/ml/data_frame/analytics/${id}/_stop`)
+          .auth(USER.ML_POWERUSER, ml.securityCommon.getPasswordForUser(USER.ML_POWERUSER))
+          .set(COMMON_REQUEST_HEADERS)
+          .expect(404);
+
+        expect(body.error).to.eql('Not Found');
+        expect(body.message).to.eql(message);
+      });
+
+      it('should not allow to stop analytics job for unauthorized user', async () => {
+        const analyticsId = `${jobId}_0`;
+
+        const { body } = await supertest
+          .post(`/api/ml/data_frame/analytics/${analyticsId}/_stop`)
+          .auth(USER.ML_UNAUTHORIZED, ml.securityCommon.getPasswordForUser(USER.ML_UNAUTHORIZED))
+          .set(COMMON_REQUEST_HEADERS)
+          .expect(403);
+
+        expect(body.error).to.eql('Forbidden');
+        expect(body.message).to.eql('Forbidden');
+      });
+
+      it('should not allow to stop analytics job for user with view only permission', async () => {
+        const analyticsId = `${jobId}_0`;
+
+        const { body } = await supertest
+          .post(`/api/ml/data_frame/analytics/${analyticsId}/_stop`)
+          .auth(USER.ML_VIEWER, ml.securityCommon.getPasswordForUser(USER.ML_VIEWER))
+          .set(COMMON_REQUEST_HEADERS)
+          .expect(403);
+
+        expect(body.error).to.eql('Forbidden');
+        expect(body.message).to.eql('Forbidden');
+      });
+    });
+  });
+};
---
+++ x-pack/test/api_integration/apis/ml/data_frame_analytics/stop_spaces.ts
@@ -0,0 +1,85 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+
+import { FtrProviderContext } from '../../../ftr_provider_context';
+import { COMMON_REQUEST_HEADERS } from '../../../../functional/services/ml/common_api';
+import { USER } from '../../../../functional/services/ml/security_common';
+
+export default ({ getService }: FtrProviderContext) => {
+  const esArchiver = getService('esArchiver');
+  const ml = getService('ml');
+  const spacesService = getService('spaces');
+  const supertest = getService('supertestWithoutAuth');
+
+  const jobIdSpace1 = 'ihp_od_space1';
+  const jobIdSpace2 = 'ihp_od_space2';
+  const idSpace1 = 'space1';
+  const idSpace2 = 'space2';
+
+  const initialModelMemoryLimit = '17mb';
+
+  async function runStopRequest(jobId: string, space: string, expectedStatusCode: number) {
+    const { body } = await supertest
+      .post(`/s/${space}/api/ml/data_frame/analytics/${jobId}/_stop`)
+      .auth(
+        USER.ML_POWERUSER_ALL_SPACES,
+        ml.securityCommon.getPasswordForUser(USER.ML_POWERUSER_ALL_SPACES)
+      )
+      .set(COMMON_REQUEST_HEADERS)
+      .expect(expectedStatusCode);
+
+    return body;
+  }
+
+  let jobConfigSpace1: any;
+  let jobConfigSpace2: any;
+
+  describe('POST data_frame/analytics/{analyticsId}/_stop with spaces', function () {
+    before(async () => {
+      await esArchiver.loadIfNeeded('ml/ihp_outlier');
+      await spacesService.create({ id: idSpace1, name: 'space_one', disabledFeatures: [] });
+      await spacesService.create({ id: idSpace2, name: 'space_two', disabledFeatures: [] });
+
+      jobConfigSpace1 = ml.commonConfig.getDFAIhpOutlierDetectionJobConfig(jobIdSpace1);
+      await ml.api.createDataFrameAnalyticsJob(
+        { ...jobConfigSpace1, model_memory_limit: initialModelMemoryLimit },
+        idSpace1
+      );
+
+      jobConfigSpace2 = ml.commonConfig.getDFAIhpOutlierDetectionJobConfig(jobIdSpace2);
+      await ml.api.createDataFrameAnalyticsJob(
+        { ...jobConfigSpace2, model_memory_limit: initialModelMemoryLimit },
+        idSpace2
+      );
+      await ml.testResources.setKibanaTimeZoneToUTC();
+    });
+
+    after(async () => {
+      const space1JobDestIndex = jobConfigSpace1.dest.index;
+      const space2JobDestIndex = jobConfigSpace2.dest.index;
+
+      await spacesService.delete(idSpace1);
+      await spacesService.delete(idSpace2);
+      await ml.api.cleanMlIndices();
+      await ml.api.deleteIndices(space1JobDestIndex);
+      await ml.api.deleteIndices(space2JobDestIndex);
+      await ml.testResources.cleanMLSavedObjects();
+    });
+
+    it('should stop job from same space', async () => {
+      const body = await runStopRequest(jobIdSpace1, idSpace1, 200);
+      expect(body).to.have.property('stopped', true);
+    });
+
+    it('should fail to stop job from different space', async () => {
+      const body = await runStopRequest(jobIdSpace2, idSpace1, 404);
+      expect(body.error).to.eql('Not Found');
+    });
+  });
+};
