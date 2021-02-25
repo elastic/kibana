a826e9b28e5082b1cb80739c1d76343d440eec20
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-02-25 16:53:42 -0700

    Deletes unused interface

--- src/plugins/kibana_usage_collection/server/collectors/ops_stats/ops_stats_collector.ts
+++ src/plugins/kibana_usage_collection/server/collectors/ops_stats/ops_stats_collector.ts
@@ -12,14 +12,6 @@ import moment from 'moment';
 import { OpsMetrics } from 'kibana/server';
 import { UsageCollectionSetup } from 'src/plugins/usage_collection/server';
 import { KIBANA_STATS_TYPE } from '../../../common/constants';
-
-// interface OpsStatsMetrics extends Omit<OpsMetrics, 'response_times' | 'collected_at'> {
-//   timestamp: string;
-//   response_times: {
-//     average: number;
-//     max: number;
-//   };
-// }
 interface OpsStatsMetricsUsage {
   timestamp: string;
   response_times: {
