89415980e7b8dd19415a5803c80926f848b74567
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2021-02-24 20:25:33 -0800

    unskipping the functional test, removing the unload and adding an empty kibana in the after method

--- test/functional/apps/management/_scripted_fields_filter.js
+++ test/functional/apps/management/_scripted_fields_filter.js
@@ -16,9 +16,7 @@ export default function ({ getService, getPageObjects }) {
   const esArchiver = getService('esArchiver');
   const PageObjects = getPageObjects(['settings']);
 
-  // this functionality is no longer functional as of 7.0 but still needs cleanup
-  // https://github.com/elastic/kibana/issues/74118
-  describe.skip('filter scripted fields', function describeIndexTests() {
+  describe('filter scripted fields', function describeIndexTests() {
     before(async function () {
       // delete .kibana index and then wait for Kibana to re-create it
       await browser.setWindowSize(1200, 800);
@@ -30,7 +28,6 @@ export default function ({ getService, getPageObjects }) {
 
     after(async function () {
       await esArchiver.unload('management');
-      await kibanaServer.uiSettings.replace({});
     });
 
     const scriptedPainlessFieldName = 'ram_pain1';
