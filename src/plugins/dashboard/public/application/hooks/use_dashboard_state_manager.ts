7c17741226008f2dbde4812e7cc54640d525ce5a
Devon A Thomson <devon.thomson@hotmail.com>
2021-02-25 11:57:12 -0500

    type fix

--- src/plugins/dashboard/public/application/hooks/use_dashboard_state_manager.ts
+++ src/plugins/dashboard/public/application/hooks/use_dashboard_state_manager.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { History } from 'history';
 import _ from 'lodash';
 import { map } from 'rxjs/operators';
