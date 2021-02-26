351fd33b5a1475e39e4f1544bbc5b2fe632776a5
Jason Stoltzfus <jastoltz24@gmail.com>
2021-02-26 13:29:02 -0500

    [PR Feedback] Refactor

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/relevance_tuning.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/relevance_tuning.test.tsx
@@ -7,13 +7,13 @@
 import '../../../__mocks__/shallow_useeffect.mock';
 import { setMockActions, setMockValues } from '../../../__mocks__/kea.mock';
 
- import '../../__mocks__/engine_logic.mock'; 
-
 import React from 'react';
 
-import { shallow, ShallowWrapper } from 'enzyme';
+import { shallow } from 'enzyme';
 
-import { EuiEmptyPrompt, EuiPageHeader } from '@elastic/eui';
+import { EuiEmptyPrompt } from '@elastic/eui';
+
+import { UnsavedChangesPrompt } from '../../../shared/unsaved_changes_prompt';
 
 import { RelevanceTuning } from './relevance_tuning';
 import { RelevanceTuningForm } from './relevance_tuning_form';
@@ -26,6 +26,7 @@ describe('RelevanceTuning', () => {
       unsearchedUnconfirmedFields: false,
     },
     schemaFieldsWithConflicts: [],
+    unsavedChanges: false,
   };
 
   const actions = {
@@ -45,13 +46,6 @@ describe('RelevanceTuning', () => {
   it('renders', () => {
     const wrapper = subject();
     expect(wrapper.find(RelevanceTuningForm).exists()).toBe(true);
-    expect(wrapper.find('[data-test-subj="RelevanceTuningInvalidBoostsCallout"]').exists()).toBe(
-      false
-    );
-    expect(wrapper.find('[data-test-subj="RelevanceTuningUnsearchedFieldsCallout"]').exists()).toBe(
-      false
-    );
-    expect(subject().find('[data-test-subj="SchemaConflictsCallout"]').exists()).toBe(false);
   });
 
   it('initializes relevance tuning data', () => {
@@ -59,93 +53,21 @@ describe('RelevanceTuning', () => {
     expect(actions.initializeRelevanceTuning).toHaveBeenCalled();
   });
 
-  it('renders a Save button that will save the current changes', () => {
-    const buttons = subject().find(EuiPageHeader).prop('rightSideItems') as React.ReactElement[];
-    expect(buttons.length).toBe(2);
-    const saveButton = shallow(buttons[0]);
-    saveButton.simulate('click');
-    expect(actions.updateSearchSettings).toHaveBeenCalled();
-  });
-
-  it('renders a Reset button that will remove all weights and boosts', () => {
-    const buttons = subject().find(EuiPageHeader).prop('rightSideItems') as React.ReactElement[];
-    expect(buttons.length).toBe(2);
-    const resetButton = shallow(buttons[1]);
-    resetButton.simulate('click');
-    expect(actions.resetSearchSettings).toHaveBeenCalled();
-  });
-
-  describe('when the engine has no schema', () => {
-    let wrapper: ShallowWrapper;
-
-    beforeAll(() => {
-      // An engine would have no schema if it is newly created, and no documents have been indexed
-      // yet.
-      setMockValues({
-        ...values,
-        engineHasSchemaFields: false,
-      });
-      wrapper = subject();
-    });
-
-    it('will not render buttons if the engine has no schema', () => {
-      setMockValues({
-        ...values,
-        engineHasSchemaFields: false,
-      });
-      const buttons = wrapper.find(EuiPageHeader).prop('rightSideItems') as React.ReactElement[];
-      expect(buttons.length).toBe(0);
-    });
-
-    it('will render an empty message', () => {
-      setMockValues({
-        ...values,
-        engineHasSchemaFields: false,
-      });
-      expect(wrapper.find(EuiEmptyPrompt).exists()).toBe(true);
-      expect(wrapper.find(RelevanceTuningForm).exists()).toBe(false);
-    });
-  });
-
-  it('shows a message when there are invalid boosts', () => {
-    // An invalid boost would be if a user creats a functional boost on a number field, then that
-    // field later changes to text. At this point, the boost still exists but is invalid for
-    // a text field.
+  it('will render an empty message when the engine has no schema', () => {
     setMockValues({
       ...values,
-      engine: {
-        invalidBoosts: true,
-        unsearchedUnconfirmedFields: false,
-      },
+      engineHasSchemaFields: false,
     });
-    expect(subject().find('[data-test-subj="RelevanceTuningInvalidBoostsCallout"]').exists()).toBe(
-      true
-    );
+    const wrapper = subject();
+    expect(wrapper.find('EmptyCallout').dive().find(EuiEmptyPrompt).exists()).toBe(true);
+    expect(wrapper.find(RelevanceTuningForm).exists()).toBe(false);
   });
 
-  it('shows a message when there are unconfirmed fields', () => {
-    // An invalid boost would be if a user creats a functional boost on a number field, then that
-    // field later changes to text. At this point, the boost still exists but is invalid for
-    // a text field.
+  it('will prevent user from leaving the page if there are unsaved changes', () => {
     setMockValues({
       ...values,
-      engine: {
-        invalidBoosts: false,
-        unsearchedUnconfirmedFields: true,
-      },
+      unsavedChanges: true,
     });
-    expect(
-      subject().find('[data-test-subj="RelevanceTuningUnsearchedFieldsCallout"]').exists()
-    ).toBe(true);
-  });
-
-  it('shows a message when there are schema field conflicts', () => {
-    // Schema conflicts occur when a meta engine has fields in source engines with have differing types,
-    // hence relevance tuning cannot be applied as we don't know the actual type
-    setMockValues({
-      ...values,
-      schemaFieldsWithConflicts: ['fe', 'fi', 'fo'],
-    });
-    expect(subject().find('[data-test-subj="SchemaConflictsCallout"]').exists()).toBe(true);
+    expect(subject().find(UnsavedChangesPrompt).prop('hasUnsavedChanges')).toBe(true);
   });
 });
--- x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/relevance_tuning.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/relevance_tuning.tsx
@@ -9,183 +9,70 @@ import React, { useEffect } from 'react';
 
 import { useActions, useValues } from 'kea';
 
-import {
-  EuiPageHeader,
-  EuiSpacer,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiButton,
-  EuiCallOut,
-  EuiLink,
-  EuiEmptyPrompt,
-} from '@elastic/eui';
-
+import { EuiButton, EuiEmptyPrompt, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n/react';
 
-import { FlashMessages } from '../../../shared/flash_messages';
-import { SetAppSearchChrome as SetPageChrome } from '../../../shared/kibana_chrome';
-import { EuiLinkTo } from '../../../shared/react_router_helpers';
 import { UnsavedChangesPrompt } from '../../../shared/unsaved_changes_prompt';
+import { DOCS_PREFIX } from '../../routes';
 
-import { DOCS_PREFIX, ENGINE_SCHEMA_PATH } from '../../routes';
-import { EngineLogic, generateEnginePath } from '../engine';
-
-import { RELEVANCE_TUNING_TITLE } from './constants';
 import { RelevanceTuningForm } from './relevance_tuning_form';
-import { RelevanceTuningLogic } from './relevance_tuning_logic';
+import { RelevanceTuningLayout } from './relevance_tuning_layout';
+
+import { RelevanceTuningLogic } from '.';
 
 interface Props {
   engineBreadcrumb: string[];
 }
 
-export const RelevanceTuning: React.FC<Props> = ({ engineBreadcrumb }) => {
-  const { resetSearchSettings, initializeRelevanceTuning, updateSearchSettings } = useActions(
-    RelevanceTuningLogic
+const EmptyCallout: React.FC = () => {
+  return (
+    <EuiEmptyPrompt
+      title={
+        <h2>
+          {i18n.translate(
+            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessageTitle',
+            {
+              defaultMessage: 'Tuning requires schema fields',
+            }
+          )}
+        </h2>
+      }
+      body={i18n.translate(
+        'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessage',
+        {
+          defaultMessage: 'Index documents to tune relevance.',
+        }
+      )}
+      actions={
+        <EuiButton
+          size="s"
+          color="primary"
+          href={`${DOCS_PREFIX}/relevance-tuning-guide.html`}
+          fill
+        >
+          {i18n.translate(
+            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyButtonLabel',
+            {
+              defaultMessage: 'Read the relevance tuning guide',
+            }
+          )}
+        </EuiButton>
+      }
+    />
   );
-  const { schemaFieldsWithConflicts, engineHasSchemaFields, unsavedChanges } = useValues(
-    RelevanceTuningLogic
-  );
-  const {
-    engine: { invalidBoosts, unsearchedUnconfirmedFields },
-  } = useValues(EngineLogic);
+};
 
-  const schemaFieldsWithConflictsCount = schemaFieldsWithConflicts.length;
+export const RelevanceTuning: React.FC<Props> = ({ engineBreadcrumb }) => {
+  const { engineHasSchemaFields, unsavedChanges } = useValues(RelevanceTuningLogic);
+  const { initializeRelevanceTuning } = useActions(RelevanceTuningLogic);
 
   useEffect(() => {
     initializeRelevanceTuning();
   }, []);
 
   return (
-    <>
+    <RelevanceTuningLayout engineBreadcrumb={engineBreadcrumb}>
       <UnsavedChangesPrompt hasUnsavedChanges={unsavedChanges} />
-      <SetPageChrome trail={[...engineBreadcrumb, RELEVANCE_TUNING_TITLE]} />
-      <EuiPageHeader
-        className="relevanceTuningHeader"
-        pageTitle={RELEVANCE_TUNING_TITLE}
-        description={i18n.translate(
-          'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.description',
-          {
-            defaultMessage: 'Set field weights and boosts',
-          }
-        )}
-        rightSideItems={
-          engineHasSchemaFields
-            ? [
-                <EuiButton
-                  data-test-subj="SaveRelevanceTuning"
-                  color="primary"
-                  fill
-                  onClick={updateSearchSettings}
-                >
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.saveButtonLabel',
-                    {
-                      defaultMessage: 'Save',
-                    }
-                  )}
-                </EuiButton>,
-                <EuiButton
-                  data-test-subj="ResetRelevanceTuning"
-                  color="danger"
-                  onClick={resetSearchSettings}
-                >
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.resetButtonLabel',
-                    {
-                      defaultMessage: 'Restore defaults',
-                    }
-                  )}
-                </EuiButton>,
-              ]
-            : []
-        }
-      />
-
-      <FlashMessages />
-      {invalidBoosts && (
-        <EuiCallOut
-          color="warning"
-          iconType="alert"
-          title={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.invalidBoostsBannerLabel',
-            {
-              defaultMessage: 'You have invalid boosts!',
-            }
-          )}
-          data-test-subj="RelevanceTuningInvalidBoostsCallout"
-        >
-          {i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.invalidBoostsErrorMessage',
-            {
-              defaultMessage:
-                'One or more of your boosts is no longer valid, possibly due to a schema type change. Delete any old or invalid boosts to dismiss this alert.',
-            }
-          )}
-        </EuiCallOut>
-      )}
-      {unsearchedUnconfirmedFields && (
-        <EuiCallOut
-          color="warning"
-          iconType="alert"
-          title={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.uncofirmedFieldsBannerLabel',
-            {
-              defaultMessage: 'Recently added fields are not being searched by default',
-            }
-          )}
-          data-test-subj="RelevanceTuningUnsearchedFieldsCallout"
-        >
-          <FormattedMessage
-            id="xpack.enterpriseSearch.appSearch.engine.relevanceTuning.uncofirmedFieldsErrorMessage"
-            defaultMessage="If these new fields should be searchable, turn them on here by toggling Text Search. Otherwise, confirm your new {schemaLink} to dismiss this alert."
-            values={{
-              schemaLink: (
-                <EuiLinkTo to={generateEnginePath(ENGINE_SCHEMA_PATH)}>
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaFieldsLinkLabel',
-                    {
-                      defaultMessage: 'schema fields',
-                    }
-                  )}
-                </EuiLinkTo>
-              ),
-            }}
-          />
-        </EuiCallOut>
-      )}
-      {schemaFieldsWithConflictsCount > 0 && (
-        <EuiCallOut
-          color="warning"
-          iconType="alert"
-          title={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaConflictsBannerLabel',
-            {
-              defaultMessage: 'Disabled fields',
-            }
-          )}
-          data-test-subj="SchemaConflictsCallout"
-        >
-          <FormattedMessage
-            id="xpack.enterpriseSearch.appSearch.engine.relevanceTuning.schemaConflictsErrorMessage"
-            defaultMessage="{schemaFieldsWithConflictsCount, number} inactive {schemaFieldsWithConflictsCount, plural, one {field} other {fields}} due to field-type conflicts. {link}"
-            values={{
-              schemaFieldsWithConflictsCount,
-              link: (
-                <EuiLink href={`${DOCS_PREFIX}/meta-engines-guide.html`} target="_blank">
-                  {i18n.translate(
-                    'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.whatsThisLinkLabel',
-                    {
-                      defaultMessage: "What's this?",
-                    }
-                  )}
-                </EuiLink>
-              ),
-            }}
-          />
-        </EuiCallOut>
-      )}
-      <EuiSpacer />
       {engineHasSchemaFields ? (
         <EuiFlexGroup>
           <EuiFlexItem>
@@ -194,40 +81,8 @@ export const RelevanceTuning: React.FC<Props> = ({ engineBreadcrumb }) => {
           <EuiFlexItem />
         </EuiFlexGroup>
       ) : (
-        <EuiEmptyPrompt
-          title={
-            <h2>
-              {i18n.translate(
-                'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessageTitle',
-                {
-                  defaultMessage: 'Tuning requires schema fields',
-                }
-              )}
-            </h2>
-          }
-          body={i18n.translate(
-            'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyErrorMessage',
-            {
-              defaultMessage: 'Index documents to tune relevance.',
-            }
-          )}
-          actions={
-            <EuiButton
-              size="s"
-              color="primary"
-              href={`${DOCS_PREFIX}/relevance-tuning-guide.html`}
-              fill
-            >
-              {i18n.translate(
-                'xpack.enterpriseSearch.appSearch.engine.relevanceTuning.emptyButtonLabel',
-                {
-                  defaultMessage: 'Read the relevance tuning guide',
-                }
-              )}
-            </EuiButton>
-          }
-        />
+        <EmptyCallout />
       )}
-    </>
+    </RelevanceTuningLayout>
   );
 };
