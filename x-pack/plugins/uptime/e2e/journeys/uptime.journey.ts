7c037345454d63600b71fa69209914b3f94447a3
Shahzad <shahzad31comp@gmail.com>
2021-03-01 13:22:34 +0100

    update json file

--- x-pack/plugins/uptime/e2e/journeys/uptime.journey.ts
+++ x-pack/plugins/uptime/e2e/journeys/uptime.journey.ts
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import { journey, step } from '@elastic/synthetics';
 
 export const byTestId = (testId: string) => {
