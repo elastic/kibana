8054b8ece6ff595031141fbe9633689a440b79be
Shahzad <shahzad31comp@gmail.com>
2021-03-01 12:41:01 +0100

    remove fixture

--- x-pack/plugins/uptime/e2e/config.ts
+++ x-pack/plugins/uptime/e2e/config.ts
@@ -5,8 +5,6 @@
  * 2.0.
  */
 
-import { resolve } from 'path';
-
 import { FtrConfigProviderContext } from '@kbn/test/types/ftr';
 
 import { CA_CERT_PATH } from '@kbn/dev-utils';
@@ -20,10 +18,6 @@ async function config({ readConfigFile }: FtrConfigProviderContext) {
 
   return {
     ...kibanaCommonTestsConfig.getAll(),
-
-    esArchiver: {
-      directory: resolve(__dirname, 'elastic_synthetics/fixtures/es_archiver'),
-    },
 
     esTestCluster: {
       skipEsCluster: true,
