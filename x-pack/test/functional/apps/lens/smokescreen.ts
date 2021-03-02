2dbbaf2a8509ddf10c4c0b92faf2f2f69b177ee3
Joe Reuter <johannes.reuter@elastic.co>
2021-03-02 16:43:11 +0100

    fix

--- x-pack/test/functional/apps/lens/smokescreen.ts
+++ x-pack/test/functional/apps/lens/smokescreen.ts
@@ -12,7 +12,6 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const PageObjects = getPageObjects(['visualize', 'lens', 'common', 'header']);
   const find = getService('find');
-  const retry = getService('retry');
   const listingTable = getService('listingTable');
   const testSubjects = getService('testSubjects');
   const elasticChart = getService('elasticChart');
