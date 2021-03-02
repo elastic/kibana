30b6eb3724eb08112ab7456d6985f17d4c08ee2a
Devin Hurley <devin.hurley@elastic.co>
2021-03-02 13:08:57 -0500

    fix ftr test

--- x-pack/test/detection_engine_api_integration/security_and_spaces/tests/create_rules.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/tests/create_rules.ts
@@ -138,28 +138,6 @@ export default ({ getService }: FtrProviderContext) => {
         );
       });
 
-      it('should create a single rule with a rule_id and an index pattern that does not match anything available and warning for the rule', async () => {
-        const simpleRule = getRuleForSignalTesting(['does-not-exist-*']);
-        const { body } = await supertest
-          .post(DETECTION_ENGINE_RULES_URL)
-          .set('kbn-xsrf', 'true')
-          .send(simpleRule)
-          .expect(200);
-
-        await waitForRuleSuccessOrStatus(supertest, body.id, 'warning');
-
-        const { body: statusBody } = await supertest
-          .post(DETECTION_ENGINE_RULES_STATUS_URL)
-          .set('kbn-xsrf', 'true')
-          .send({ ids: [body.id] })
-          .expect(200);
-
-        expect(statusBody[body.id].current_status.status).to.eql('warning');
-        expect(statusBody[body.id].current_status.last_success_message).to.eql(
-          'The following index patterns did not match any indices: ["does-not-exist-*"]'
-        );
-      });
-
       it('should create a single rule with a rule_id and an index pattern that does not match anything and an index pattern that does and the rule should be successful', async () => {
         const simpleRule = getRuleForSignalTesting(['does-not-exist-*', 'auditbeat-*']);
         const { body } = await supertest
