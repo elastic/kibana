f99ee5800abf5993fff11fbf73a62e3cc49cd6f1
Steph Milovic <stephanie.milovic@elastic.co>
2021-03-01 22:52:48 -0600

    rm var

--- x-pack/plugins/security_solution/public/timelines/components/timeline/body/column_headers/header/index.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/body/column_headers/header/index.tsx
@@ -82,7 +82,6 @@ export const HeaderComponent: React.FC<Props> = ({
     getManageTimelineById,
     timelineId,
   ]);
-  const showSortingCapability = !isEqlOn && !(header.subType && header.subType.nested);
 
   return (
     <>
@@ -91,10 +90,7 @@ export const HeaderComponent: React.FC<Props> = ({
         isLoading={isLoading}
         isResizing={false}
         onClick={onColumnSort}
-<<<<<<< HEAD
-=======
-        showSortingCapability={showSortingCapability}
->>>>>>> 90976ee1198... [Security Solution] [Timeline] Bugfix to include unmapped fields in the timeline event details JSON (#92025)
+        showSortingCapability={!(header.subType && header.subType.nested)}
         sort={sort}
       >
         <Actions
