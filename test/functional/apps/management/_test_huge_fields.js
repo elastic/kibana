60281f6d8a6e8ea9c85bb63b439ffd79952e8ed4
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2021-03-02 20:37:14 -0800

    fix eslint

--- test/functional/apps/management/_test_huge_fields.js
+++ test/functional/apps/management/_test_huge_fields.js
@@ -18,7 +18,7 @@ export default function ({ getService, getPageObjects }) {
 
     const EXPECTED_FIELD_COUNT = '10006';
     before(async function () {
-      await security.testUser.setRoles(['kibana_admin', 'test_testhuge_reader'],false);
+      await security.testUser.setRoles(['kibana_admin', 'test_testhuge_reader'], false);
       await esArchiver.emptyKibanaIndex();
       await esArchiver.loadIfNeeded('large_fields');
       await PageObjects.settings.navigateTo();
