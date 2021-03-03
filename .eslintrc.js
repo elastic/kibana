c7f3dcc8b43ca59fa67b5285b6fcffd2b79c4563
Anton Dosov <anton.dosov@elastic.co>
2021-03-03 16:59:31 +0100

    fix wrong import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1348,7 +1348,7 @@ module.exports = {
         'no-restricted-imports': [
           'error',
           {
-            patterns: ['lodash/*', '!lodash/fp'],
+            patterns: ['lodash/*', '!lodash/fp', 'rxjs/internal-compatibility'],
           },
         ],
       },
