dd2a10d2611cf4739fd81d70aad941354d2ceeae
Tiago Costa <tiagoffcc@hotmail.com>
2021-03-03 23:24:07 +0000

    chore(NA): update src/dev/bazel_workspace_status.js
    Co-authored-by: Tyler Smalley <tylersmalley@me.com>

--- src/dev/bazel_workspace_status.js
+++ src/dev/bazel_workspace_status.js
@@ -12,7 +12,7 @@
 // workspace. The output should be like
 //
 // KEY1 VALUE1
-// KEY2 VALUE2ba
+// KEY2 VALUE2
 //
 // If the script exits with non-zero code, it's considered as a failure
 // and the output will be discarded.
