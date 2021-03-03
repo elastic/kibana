b7bfbeff31379d25de3efefd4822c59c4ca73b7e
Nathan Reese <reese.nathan@gmail.com>
2021-03-02 21:23:49 -0700

    rename advanced setting ml:fileDataVisualizerMaxFileSize to fileUpload:maxFileSize and increase max geojson upload size to 1GB (#92620) (#93358)
    * rename ml:fileDataVisualizerMaxFileSize to fileUppload:maxFileSize
    
    * add saved object migration
    
    * file preview
    
    * importing status
    
    * remove console statement
    
    * import complete view
    
    * fix geojson_importer test
    
    * tslint
    
    * i18n fixes
    
    * cleanup
    
    * update documenation for advanced setting rename
    
    * advanced settings usage_collection
    
    * remove ml:fileDataVisualizerMaxFileSize from schemas and types
    
    * add copy buttons for import response and fix geojson upload functional tests
    
    * tslint
    
    * remove clipboard-read check
    
    * return early if env does not support reading from clipboard
    
    * fix reporting tests
    
    * review feedback
    
    * update GeoJsonFileSource to support showing results trimmed icon and tooltip
    
    * add fileUpload to useMlKibana context and replace dependencyCache with useMlKibana
    
    * tslint
    
    * review feedback
    
    * lower case file name
    
    * default to selecting geo_shape when file contains both points and shapes
    
    * fix wizard onError callback to not advance to next step
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/ml/public/application/datavisualizer/file_based/components/utils/utils.ts
+++ x-pack/plugins/ml/public/application/datavisualizer/file_based/components/utils/utils.ts
@@ -6,19 +6,9 @@
  */
 
 import { isEqual } from 'lodash';
-// @ts-ignore
-import numeral from '@elastic/numeral';
 import { ml } from '../../../../services/ml_api_service';
 import { AnalysisResult, InputOverrides } from '../../../../../../common/types/file_datavisualizer';
-import {
-  MB,
-  MAX_FILE_SIZE,
-  MAX_FILE_SIZE_BYTES,
-  ABSOLUTE_MAX_FILE_SIZE_BYTES,
-  FILE_SIZE_DISPLAY_FORMAT,
-} from '../../../../../../../file_upload/public';
-import { getUiSettings } from '../../../../util/dependency_cache';
-import { FILE_DATA_VISUALIZER_MAX_FILE_SIZE } from '../../../../../../common/constants/settings';
+import { MB } from '../../../../../../../file_upload/public';
 
 export const DEFAULT_LINES_TO_SAMPLE = 1000;
 const UPLOAD_SIZE_MB = 5;
@@ -64,20 +54,6 @@ export function readFile(file: File) {
       reject();
     }
   });
-}
-
-export function getMaxBytes() {
-  const maxFileSize = getUiSettings().get(FILE_DATA_VISUALIZER_MAX_FILE_SIZE, MAX_FILE_SIZE);
-  // @ts-ignore
-  const maxBytes = numeral(maxFileSize.toUpperCase()).value();
-  if (maxBytes < MAX_FILE_SIZE_BYTES) {
-    return MAX_FILE_SIZE_BYTES;
-  }
-  return maxBytes <= ABSOLUTE_MAX_FILE_SIZE_BYTES ? maxBytes : ABSOLUTE_MAX_FILE_SIZE_BYTES;
-}
-
-export function getMaxBytesFormatted() {
-  return numeral(getMaxBytes()).format(FILE_SIZE_DISPLAY_FORMAT);
 }
 
 export function createUrlOverrides(overrides: InputOverrides, originalSettings: InputOverrides) {
