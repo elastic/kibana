96f3ff1f22b98cddb7cc227eb4caa996e4357233
Anton Dosov <anton.dosov@elastic.co>
2021-03-04 11:42:36 +0100

    Fix wrong import in data plugin causing 100kB bundle increase (#93448)
    # Conflicts:
    #	api_docs/data.json
    #	api_docs/data_search.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1368,7 +1368,7 @@ module.exports = {
         'no-restricted-imports': [
           'error',
           {
-            patterns: ['lodash/*', '!lodash/fp'],
+            patterns: ['lodash/*', '!lodash/fp', 'rxjs/internal-compatibility'],
           },
         ],
       },
