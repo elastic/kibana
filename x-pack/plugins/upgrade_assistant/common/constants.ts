bfbb4b4e2a098dd15ee577db22cc41831f3efd12
Alison Goryachev <alisonmllr20@gmail.com>
2021-03-04 13:30:02 -0500

    update backport

--- x-pack/plugins/upgrade_assistant/common/constants.ts
+++ x-pack/plugins/upgrade_assistant/common/constants.ts
@@ -5,6 +5,16 @@
  * 2.0.
  */
 
+import SemVer from 'semver/classes/semver';
+
+/*
+ * These constants are used only in tests to add conditional logic based on Kibana version
+ * On master, the version should represent the next major version (e.g., master --> 8.0.0)
+ * The release branch should match the release version (e.g., 7.x --> 7.0.0)
+ */
+export const mockKibanaVersion = '8.0.0';
+export const mockKibanaSemverVersion = new SemVer(mockKibanaVersion);
+
 /*
  * This will be set to true up until the last minor before the next major.
  * In readonly mode, the user will not be able to perform any actions in the UI
