ccf7f119983f00c6451138bc87059c60bf06166f
Lukas Olson <olson.lukas@gmail.com>
2021-03-04 11:12:41 -0700

    [KQL] Remove number parsing

--- src/plugins/data/common/es_query/kuery/ast/ast.test.ts
+++ src/plugins/data/common/es_query/kuery/ast/ast.test.ts
@@ -150,10 +150,10 @@ describe('kuery AST API', () => {
     test('should support exclusive range operators', () => {
       const expected = nodeTypes.function.buildNode('and', [
         nodeTypes.function.buildNode('range', 'bytes', {
-          gt: 1000,
+          gt: '1000',
         }),
         nodeTypes.function.buildNode('range', 'bytes', {
-          lt: 8000,
+          lt: '8000',
         }),
       ]);
       const actual = fromKueryExpression('bytes > 1000 and bytes < 8000');
@@ -163,10 +163,10 @@ describe('kuery AST API', () => {
     test('should support inclusive range operators', () => {
       const expected = nodeTypes.function.buildNode('and', [
         nodeTypes.function.buildNode('range', 'bytes', {
-          gte: 1000,
+          gte: '1000',
         }),
         nodeTypes.function.buildNode('range', 'bytes', {
-          lte: 8000,
+          lte: '8000',
         }),
       ]);
       const actual = fromKueryExpression('bytes >= 1000 and bytes <= 8000');
@@ -261,25 +261,24 @@ describe('kuery AST API', () => {
       const stringLiteral = nodeTypes.literal.buildNode('foo');
       const booleanFalseLiteral = nodeTypes.literal.buildNode(false);
       const booleanTrueLiteral = nodeTypes.literal.buildNode(true);
-      const numberLiteral = nodeTypes.literal.buildNode(42);
 
       expect(fromLiteralExpression('foo')).toEqual(stringLiteral);
       expect(fromLiteralExpression('true')).toEqual(booleanTrueLiteral);
       expect(fromLiteralExpression('false')).toEqual(booleanFalseLiteral);
-      expect(fromLiteralExpression('42')).toEqual(numberLiteral);
 
-      expect(fromLiteralExpression('.3').value).toEqual(0.3);
-      expect(fromLiteralExpression('.36').value).toEqual(0.36);
-      expect(fromLiteralExpression('.00001').value).toEqual(0.00001);
-      expect(fromLiteralExpression('3').value).toEqual(3);
-      expect(fromLiteralExpression('-4').value).toEqual(-4);
-      expect(fromLiteralExpression('0').value).toEqual(0);
-      expect(fromLiteralExpression('0.0').value).toEqual(0);
-      expect(fromLiteralExpression('2.0').value).toEqual(2.0);
-      expect(fromLiteralExpression('0.8').value).toEqual(0.8);
-      expect(fromLiteralExpression('790.9').value).toEqual(790.9);
-      expect(fromLiteralExpression('0.0001').value).toEqual(0.0001);
-      expect(fromLiteralExpression('96565646732345').value).toEqual(96565646732345);
+      expect(fromLiteralExpression('.3').value).toEqual('.3');
+      expect(fromLiteralExpression('.36').value).toEqual('.36');
+      expect(fromLiteralExpression('.00001').value).toEqual('.00001');
+      expect(fromLiteralExpression('3').value).toEqual('3');
+      expect(fromLiteralExpression('-4').value).toEqual('-4');
+      expect(fromLiteralExpression('0').value).toEqual('0');
+      expect(fromLiteralExpression('0.0').value).toEqual('0.0');
+      expect(fromLiteralExpression('2.0').value).toEqual('2.0');
+      expect(fromLiteralExpression('0.8').value).toEqual('0.8');
+      expect(fromLiteralExpression('790.9').value).toEqual('790.9');
+      expect(fromLiteralExpression('0.0001').value).toEqual('0.0001');
+      expect(fromLiteralExpression('96565646732345').value).toEqual('96565646732345');
+      expect(fromLiteralExpression('070').value).toEqual('070');
 
       expect(fromLiteralExpression('..4').value).toEqual('..4');
       expect(fromLiteralExpression('.3text').value).toEqual('.3text');
