7b026353761cce75d851c5160873722b46d02817
Davey Holler <daveyholler@gmail.com>
2021-03-03 18:03:55 -0800

    App Search - Polishing Analytics Views (#92939) (#93537)
    * WIP analytics polish
    
    * Working on tests.
    
    * Finishing up tests.
    
    * Updating the engine overview page.
    
    * eslint fixes
    
    * i18n fixes
    
    * Icons feedback
    
    - Remove unused svg file
    - Add TODO comment
    - Add test coverage
    
    * linting - unnecessary ternaries
    
    * EnginesOverview feedback
    
    TotalCharts
    - adjust chart height
    - tests: simplify / convert back to shallow
    
    RecentApiLogs
    - switch to DataPanel
    
    * DataPanel feedback
    
    Component
    - move CSS table bg override out from being an AnalyticsTable concern to a DataPanel concern
      + bem naming, todo comment
    - add responsive={false} for better mobile UX
    - add className and data-test-subj prop passing
    - change title to pass full heading tags rather than a string
    - move subtitle below title for better screen reader order
    - add index.ts export
    
    Tests
    - capitalization for consistency, ordering/describe
    - remove data-test-subjs on source code (simpler to grab & inspect tags directly so we can more easily call .text() on passed content)
    - add new tests for props (fliled/className/data-test-subj)
    
    Usage
    - update other files using DataPanel to start passing heading tags + use index export
    - fix RecentApiLogs tests
    - change RecentQueries section to use a DataPanel (per Davey)
    
    * Analytics tags - updates & responsive tweaks
    
    Tags
    - Rename to more general tags.tsx file
    - Add CSS limiting width of variable length tags
    - Break up into two separate components for easier readability & testing
    - Split up tags column constants so that the wider tables can use the old tags list component
    
    Tables
    - add isSmall flag to AnalyticsTable to use new tag count  component
    - reduce actions column width
    - revert unnecessary table test changes
    
    Analytics
    - add custom CSS that switches tables/panels into full-width earlier
    
    * AnalyticsSection fixes
    
    - fix responsive behavior with icon
    - add missing AnalyticsSection branch coverage
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Constance Chen <constance.chen.3@gmail.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Constance Chen <constance.chen.3@gmail.com>

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/engines/engines_overview.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/engines/engines_overview.tsx
@@ -25,10 +25,9 @@ import { LicensingLogic } from '../../../shared/licensing';
 import { EuiButtonTo } from '../../../shared/react_router_helpers';
 import { convertMetaToPagination, handlePageChange } from '../../../shared/table_pagination';
 import { SendAppSearchTelemetry as SendTelemetry } from '../../../shared/telemetry';
+import { EngineIcon, MetaEngineIcon } from '../../icons';
 import { ENGINE_CREATION_PATH, META_ENGINE_CREATION_PATH } from '../../routes';
 
-import { EngineIcon } from './assets/engine_icon';
-import { MetaEngineIcon } from './assets/meta_engine_icon';
 import { EnginesOverviewHeader, LoadingState, EmptyState } from './components';
 import {
   CREATE_AN_ENGINE_BUTTON_LABEL,
