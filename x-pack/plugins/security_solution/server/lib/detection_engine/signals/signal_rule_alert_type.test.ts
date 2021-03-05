5a36e44b74a066c5175241f22d2c2e989af2508a
Devin W. Hurley <devin.hurley@elastic.co>
2021-03-05 17:24:47 -0500

    [Security Solution] [Detections] Fixes validation on response from find status route (#93684)
    * fix validation on response of find status route when rule has a partial failure status
    
    * replaces warning in rule status service with partial failure to maintain backwards compatibility from an API standpoint, also displays 'warning' on UI if a rule's status is partial failure
    
    * display partial failure as 'warning' on all rules table and update e2e test to check for partial failure not warning
    
    * add util function, show 'warning' on monitoring table, fix e2e tests
    # Conflicts:
    #	x-pack/plugins/security_solution/common/detection_engine/utils.ts
    #	x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/details/index.tsx
    #	x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.test.ts
    #	x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.ts
    #	x-pack/test/detection_engine_api_integration/security_and_spaces/tests/create_rules.ts

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.test.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.test.ts
@@ -106,7 +106,7 @@ describe('rules_notification_alert_type', () => {
       find: jest.fn(),
       goingToRun: jest.fn(),
       error: jest.fn(),
-      warning: jest.fn(),
+      partialFailure: jest.fn(),
     };
     (ruleStatusServiceFactory as jest.Mock).mockReturnValue(ruleStatusService);
     (getGapBetweenRuns as jest.Mock).mockReturnValue(moment.duration(0));
@@ -223,8 +223,8 @@ describe('rules_notification_alert_type', () => {
       });
       payload.params.index = ['some*', 'myfa*', 'anotherindex*'];
       await alert.executor(payload);
-      expect(ruleStatusService.warning).toHaveBeenCalled();
-      expect(ruleStatusService.warning.mock.calls[0][0]).toContain(
+      expect(ruleStatusService.partialFailure).toHaveBeenCalled();
+      expect(ruleStatusService.partialFailure.mock.calls[0][0]).toContain(
         'Missing required read privileges on the following indices: ["some*"]'
       );
     });
@@ -246,8 +246,8 @@ describe('rules_notification_alert_type', () => {
       });
       payload.params.index = ['some*', 'myfa*'];
       await alert.executor(payload);
-      expect(ruleStatusService.warning).toHaveBeenCalled();
-      expect(ruleStatusService.warning.mock.calls[0][0]).toContain(
+      expect(ruleStatusService.partialFailure).toHaveBeenCalled();
+      expect(ruleStatusService.partialFailure.mock.calls[0][0]).toContain(
         'This rule may not have the required read privileges to the following indices: ["myfa*","some*"]'
       );
     });
