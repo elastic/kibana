4cc20692f0a1ce69b5e5815c9419654f5ce7dc72
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2021-03-08 08:52:37 -0800

    fixed tests

--- x-pack/plugins/lists/server/services/items/find_list_item.test.ts
+++ x-pack/plugins/lists/server/services/items/find_list_item.test.ts
@@ -8,6 +8,7 @@
 // eslint-disable-next-line @kbn/eslint/no-restricted-paths
 import { elasticsearchClientMock } from 'src/core/server/elasticsearch/client/mocks';
 
+import { getShardMock } from '../../../common/get_shard.mock';
 import { getFoundListItemSchemaMock } from '../../../common/schemas/response/found_list_item_schema.mock';
 import { getEmptySearchListMock } from '../../../common/schemas/elastic_response/search_es_list_schema.mock';
 
@@ -18,26 +19,36 @@ describe('find_list_item', () => {
   test('should find a simple single list item', async () => {
     const options = getFindListItemOptionsMock();
     const esClient = elasticsearchClientMock.createScopedClusterClient().asCurrentUser;
+    esClient.count.mockReturnValue(elasticsearchClientMock.createSuccessTransportRequestPromise(1));
     esClient.search.mockReturnValue(
       elasticsearchClientMock.createSuccessTransportRequestPromise({
+        _scroll_id: '123',
+        _shards: getShardMock(),
         hits: {
-          hits: {
-            _id: 'some-list-item-id',
-            _source: {
-              _version: 'undefined',
-              created_at: '2020-04-20T15:25:31.830Z',
-              created_by: 'some user',
-              deserializer: 'undefined',
-              list_id: 'some-list-id',
-              meta: {},
-              serializer: 'undefined',
-              tie_breaker_id: '6a76b69d-80df-4ab2-8c3e-85f466b06a0e',
-              type: 'ip',
-              updated_at: '2020-04-20T15:25:31.830Z',
-              updated_by: 'some user',
+          hits: [
+            {
+              _id: 'some-list-item-id',
+              _source: {
+                _version: 'undefined',
+                created_at: '2020-04-20T15:25:31.830Z',
+                created_by: 'some user',
+                date_range: '127.0.0.1',
+                deserializer: undefined,
+                list_id: 'some-list-id',
+                meta: {},
+                serializer: undefined,
+                tie_breaker_id: '6a76b69d-80df-4ab2-8c3e-85f466b06a0e',
+                type: 'ip',
+                updated_at: '2020-04-20T15:25:31.830Z',
+                updated_by: 'some user',
+              },
             },
-          },
+          ],
+          max_score: 0,
+          total: 1,
         },
+        timed_out: false,
+        took: 10,
       })
     );
     const item = await findListItem({ ...options, esClient });
