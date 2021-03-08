3ca2e2129fffdc55f86e9ea5dd669c0c868da164
Casper Hübertz <casper@formgeist.com>
2021-03-08 14:37:29 +0100

    Update x-pack/plugins/apm/public/components/app/ServiceMap/icons.ts
    Co-authored-by: Søren Louv-Jansen <sorenlouv@gmail.com>

--- x-pack/plugins/apm/public/components/app/ServiceMap/icons.ts
+++ x-pack/plugins/apm/public/components/app/ServiceMap/icons.ts
@@ -14,7 +14,7 @@ import {
 import { getAgentIcon } from '../../shared/AgentIcon/get_agent_icon';
 import { getSpanIcon } from '../../shared/span_icon/get_span_icon';
 
-export function iconForNode(node: cytoscape.NodeSingular, isDarkMode: boolean) {
+export function iconForNode(node: cytoscape.NodeSingular) {
   const agentName = node.data(AGENT_NAME);
   const subtype = node.data(SPAN_SUBTYPE);
   const type = node.data(SPAN_TYPE);
