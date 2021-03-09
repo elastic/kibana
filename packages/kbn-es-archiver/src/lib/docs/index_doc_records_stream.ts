3ebcd33f07a29b5135acbf2016a974037b5dc6ac
restrry <restrry@gmail.com>
2021-03-09 12:39:44 +0100

    mute error in kbn-es-archiver

--- packages/kbn-es-archiver/src/lib/docs/index_doc_records_stream.ts
+++ packages/kbn-es-archiver/src/lib/docs/index_doc_records_stream.ts
@@ -46,6 +46,7 @@ export function createIndexDocRecordsStream(
           errors.push(`Bulk doc failure [operation=${operation}]:\n  doc: ${dj}\n  error: ${ej}`);
         },
       },
+      // @ts-expect-error doesn't support TransportRequestOptions as second argument
       {
         headers: ES_CLIENT_HEADERS,
       }
