66d8475a396f4c376c61bba61e4fa6c9323ac9ab
Christos Nasikas <christos.nasikas@elastic.co>
2021-03-09 10:17:37 +0200

    PR review

--- x-pack/plugins/security_solution/public/cases/containers/use_get_case_user_actions.tsx
+++ x-pack/plugins/security_solution/public/cases/containers/use_get_case_user_actions.tsx
@@ -307,7 +307,7 @@ export const useGetCaseUserActions = (
       }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [caseConnectorId]
+    [caseUserActionsState]
   );
 
   useEffect(() => {
