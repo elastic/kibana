68677596476208df078a401e730089da30d205b1
bryan <bclement01@gmail.com>
2021-03-09 21:01:55 -0800

    arm wrestling with the compiler

--- x-pack/plugins/osquery/public/editor/osquery_mode.ts
+++ x-pack/plugins/osquery/public/editor/osquery_mode.ts
@@ -5,193 +5,26 @@
  * 2.0.
  */
 
-import { osqueryTableNames } from './osquery_tables';
+import 'ace';
+import { AceInterface } from './ace_types';
+import './osquery_highlight_rules';
 
-const keywords = [
-  'select',
-  'insert',
-  'update',
-  'delete',
-  'from',
-  'where',
-  'and',
-  'or',
-  'group',
-  'by',
-  'order',
-  'limit',
-  'offset',
-  'having',
-  'as',
-  'case',
-  'when',
-  'else',
-  'end',
-  'type',
-  'left',
-  'right',
-  'join',
-  'on',
-  'outer',
-  'desc',
-  'asc',
-  'union',
-  'create',
-  'table',
-  'primary',
-  'key',
-  'if',
-  'foreign',
-  'not',
-  'references',
-  'default',
-  'null',
-  'inner',
-  'cross',
-  'natural',
-  'database',
-  'drop',
-  'grant',
-].join('|');
-
-const builtinConstants = ['true', 'false'].join('|');
-
-const builtinFunctions = [
-  'avg',
-  'count',
-  'first',
-  'last',
-  'max',
-  'min',
-  'sum',
-  'ucase',
-  'lcase',
-  'mid',
-  'len',
-  'round',
-  'rank',
-  'now',
-  'format',
-  'coalesce',
-  'ifnull',
-  'isnull',
-  'nvl',
-].join('|');
-
-const dataTypes = [
-  'int',
-  'numeric',
-  'decimal',
-  'date',
-  'varchar',
-  'char',
-  'bigint',
-  'float',
-  'double',
-  'bit',
-  'binary',
-  'text',
-  'set',
-  'timestamp',
-  'money',
-  'real',
-  'number',
-  'integer',
-].join('|');
-
-const osqueryTables = osqueryTableNames.join('|');
-
-ace.define(
-  'ace/mode/osquery_highlight_rules',
-  ['require', 'exports', 'ace/lib/oop', 'ace/mode/sql_highlight_rules'],
-  function (acequire, exports) {
-    'use strict';
-
-    const oop = acequire('../lib/oop');
-    const SqlHighlightRules = acequire('./sql_highlight_rules').SqlHighlightRules;
-
-    const OsqueryHighlightRules = function () {
-      const keywordMapper = this.createKeywordMapper(
-        {
-          'osquery-token': osqueryTables,
-          'support.function': builtinFunctions,
-          keyword: keywords,
-          'constant.language': builtinConstants,
-          'storage.type': dataTypes,
-        },
-        'identifier',
-        true
-      );
-
-      this.$rules = {
-        start: [
-          {
-            token: 'comment',
-            regex: '--.*$',
-          },
-          {
-            token: 'comment',
-            start: '/\\*',
-            end: '\\*/',
-          },
-          {
-            token: 'string', // " string
-            regex: '".*?"',
-          },
-          {
-            token: 'string', // ' string
-            regex: "'.*?'",
-          },
-          {
-            token: 'constant.numeric', // float
-            regex: '[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b',
-          },
-          {
-            token: keywordMapper,
-            regex: '[a-zA-Z_$][a-zA-Z0-9_$]*\\b',
-          },
-          {
-            token: 'keyword.operator',
-            regex: '\\+|\\-|\\/|\\/\\/|%|<@>|@>|<@|&|\\^|~|<|>|<=|=>|==|!=|<>|=',
-          },
-          {
-            token: 'paren.lparen',
-            regex: '[\\(]',
-          },
-          {
-            token: 'paren.rparen',
-            regex: '[\\)]',
-          },
-          {
-            token: 'text',
-            regex: '\\s+',
-          },
-        ],
-      };
-
-      this.normalizeRules();
-    };
-
-    oop.inherits(OsqueryHighlightRules, SqlHighlightRules);
-
-    exports.OsqueryHighlightRules = OsqueryHighlightRules;
-  }
-);
-
-ace.define(
+(ace as AceInterface).define(
   'ace/mode/osquery',
-  ['require', 'exports', 'ace/lib/oop', 'ace/mode/sql', 'ace/mode/osquery_highlight_rules'],
+  ['require', 'exports', 'ace/mode/sql', 'ace/mode/osquery_highlight_rules'],
   function (acequire, exports) {
     'use strict';
 
-    const oop = acequire('../lib/oop');
     const TextMode = acequire('./sql').Mode;
     const OsqueryHighlightRules = acequire('./osquery_highlight_rules').OsqueryHighlightRules;
 
-    const Mode = function () {
-      this.HighlightRules = OsqueryHighlightRules;
-    };
-    oop.inherits(Mode, TextMode);
+    class Mode extends TextMode {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      constructor(...args: any[]) {
+        super(...args);
+        this.HighlightRules = OsqueryHighlightRules;
+      }
+    }
 
     Mode.prototype.lineCommentStart = '--';
     Mode.prototype.$id = 'ace/mode/osquery';
--- x-pack/plugins/osquery/public/editor/osquery_tables.ts
+++ x-pack/plugins/osquery/public/editor/osquery_tables.ts
@@ -1,13 +1,32 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
 import { flatMap, sortBy } from 'lodash';
 import osqueryTablesJSON from './osquery_schema/v4.6.0.json';
 
-export const normalizeTables = (tablesJSON) => {
+type TablesJSON =
+  | string
+  | Array<{
+      name: string;
+      description: string;
+      url: string;
+      platforms: string[];
+      evented: boolean;
+      cacheable: boolean;
+      columns: Array<{
+        name: string;
+        description: string;
+        type: string;
+        hidden: boolean;
+        required: boolean;
+        index: boolean;
+      }>;
+    }>;
+export const normalizeTables = (tablesJSON: TablesJSON) => {
   // osquery JSON needs less parsing than it used to
   const parsedTables = typeof tablesJSON === 'object' ? tablesJSON : JSON.parse(tablesJSON);
   return sortBy(parsedTables, (table) => {
