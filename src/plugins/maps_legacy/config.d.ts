788be874e244e4ef55819acb6a91ec69a8c3fe62
Thomas Neirynck <thomas@elastic.co>
2021-03-10 12:00:10 -0500

    generate bundles

---
+++ src/plugins/maps_legacy/config.d.ts
@@ -0,0 +1,4 @@
+import { TypeOf } from '@kbn/config-schema';
+export declare const configSchema: import("@kbn/config-schema").ObjectType<{}>;
+export declare type MapsLegacyConfig = TypeOf<typeof configSchema>;
+//# sourceMappingURL=config.d.ts.map
