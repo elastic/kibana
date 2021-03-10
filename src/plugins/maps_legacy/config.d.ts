40d3f902bd667bf74020c276ebf239cd1cbe9805
Thomas Neirynck <thomas@elastic.co>
2021-03-10 12:01:15 -0500

    remove autogen

--- src/plugins/maps_legacy/config.d.ts
+++
@@ -1,4 +0,0 @@
-import { TypeOf } from '@kbn/config-schema';
-export declare const configSchema: import("@kbn/config-schema").ObjectType<{}>;
-export declare type MapsLegacyConfig = TypeOf<typeof configSchema>;
-//# sourceMappingURL=config.d.ts.map
