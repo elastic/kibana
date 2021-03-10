cb37f650ff4e752777da1cc143e4a77b544c068f
dej611 <dej611@gmail.com>
2021-03-10 10:16:21 +0100

    :bug: Fix missing collection

--- x-pack/plugins/lens/public/editor_frame_service/editor_frame/state_helpers.ts
+++ x-pack/plugins/lens/public/editor_frame_service/editor_frame/state_helpers.ts
@@ -147,10 +147,10 @@ export function getMissingIndexPattern(
     return [];
   }
   const datasourceState = currentDatasourceState.state as {
-    layers: Record<string, unknown>;
+    layers: Record<string, { indexPatternId: string }>;
     indexPatterns: Record<string, unknown>;
   };
-  const ids = Object.keys(datasourceState.layers);
+  const ids = Object.values(datasourceState.layers).map(({ indexPatternId }) => indexPatternId);
   return ids.filter((id) => !datasourceState.indexPatterns[id]);
 }
 
