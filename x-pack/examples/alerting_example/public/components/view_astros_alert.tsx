73f2c4c1fd8b7619c0b5f108ce96b2b55846dd87
Ying Mao <ying.mao@elastic.co>
2021-03-10 08:22:50 -0500

    Updating terminology in alerting examples

--- x-pack/examples/alerting_example/public/components/view_astros_alert.tsx
+++ x-pack/examples/alerting_example/public/components/view_astros_alert.tsx
@@ -49,21 +49,20 @@ export const ViewPeopleInSpaceAlertPage = withRouter(({ http, id }: Props) => {
 
   return alert && alertState ? (
     <Fragment>
-      <EuiCallOut title={`Alert "${alert.name}"`} iconType="search">
+      <EuiCallOut title={`Rule "${alert.name}"`} iconType="search">
         <p>
           This is a specific view for all
-          <EuiTextColor color="accent"> example.people-in-space </EuiTextColor> Alerts created by
-          the
+          <EuiTextColor color="accent"> example.people-in-space </EuiTextColor> Rules created by the
           <EuiTextColor color="accent"> {ALERTING_EXAMPLE_APP_ID} </EuiTextColor>
           plugin.
         </p>
       </EuiCallOut>
       <EuiSpacer size="l" />
       <EuiText>
-        <h2>Alert Instances</h2>
+        <h2>Alerts</h2>
       </EuiText>
       {isEmpty(alertState.alertInstances) ? (
-        <EuiCallOut title="No Alert Instances!" color="warning" iconType="help">
+        <EuiCallOut title="No Alerts!" color="warning" iconType="help">
           <p>
             The people in {alert.params.craft} at the moment <b>are not</b> {alert.params.op}{' '}
             {alert.params.outerSpaceCapacity}
@@ -73,7 +72,7 @@ export const ViewPeopleInSpaceAlertPage = withRouter(({ http, id }: Props) => {
         <Fragment>
           <EuiCallOut title="Active State" color="success" iconType="user">
             <p>
-              The alert has been triggered because the people in {alert.params.craft} at the moment{' '}
+              The rule has been triggered because the people in {alert.params.craft} at the moment{' '}
               {alert.params.op} {alert.params.outerSpaceCapacity}
             </p>
           </EuiCallOut>
