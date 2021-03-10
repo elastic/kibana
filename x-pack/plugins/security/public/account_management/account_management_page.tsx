3b478c9abdc79d05641519723b4725fe6f59dbdd
Ryan Keairns <contactryank@gmail.com>
2021-03-10 10:47:16 -0600

    Tidying up

--- x-pack/plugins/security/public/account_management/account_management_page.tsx
+++ x-pack/plugins/security/public/account_management/account_management_page.tsx
@@ -5,11 +5,11 @@
  * 2.0.
  */
 
-import { FormattedMessage } from '@kbn/i18n/react';
 import { EuiPage, EuiPageBody, EuiPanel, EuiSpacer, EuiText } from '@elastic/eui';
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
+import { FormattedMessage } from '@kbn/i18n/react';
 import type { PublicMethodsOf } from '@kbn/utility-types';
 import type { CoreStart, NotificationsStart } from 'src/core/public';
 
--- x-pack/plugins/security/public/nav_control/nav_control_service.tsx
+++ x-pack/plugins/security/public/nav_control/nav_control_service.tsx
@@ -78,17 +78,16 @@ export class SecurityNavControlService {
       addUserMenuLinks: (userMenuLinks: UserMenuLink[]) => {
         const currentLinks = this.userMenuLinks$.value;
         const hasCustomProfileLink = currentLinks.find(({ setAsProfile }) => setAsProfile === true);
-        const passedCustomProfileLinkCount = userMenuLinks.reduce(
-          (linkCounter, { setAsProfile }) =>
-            setAsProfile === true ? linkCounter + 1 : linkCounter,
-          0
-        );
+        const passedCustomProfileLinkCount = userMenuLinks.filter(
+          ({ setAsProfile }) => setAsProfile === true
+        ).length;
 
         if (hasCustomProfileLink && passedCustomProfileLinkCount > 0) {
           throw new Error(
             `Only one custom profile link can be set. A custom profile link named ${hasCustomProfileLink.label} (${hasCustomProfileLink.href}) already exists`
           );
-        } else if (passedCustomProfileLinkCount > 1) {
+        }
+        if (passedCustomProfileLinkCount > 1) {
           throw new Error(
             `Only one custom profile link can be passed at a time (found ${passedCustomProfileLinkCount})`
           );
