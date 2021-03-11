4c52d275318ea9eaffccc3b5aa7385e1ff60fa8c
scottybollinger <scotty.bollinger@elastic.co>
2021-03-10 18:20:14 -0600

    Add RoleMappings component

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/role_mappings/constants.ts
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/role_mappings/constants.ts
@@ -7,9 +7,12 @@
 
 import { i18n } from '@kbn/i18n';
 
-export const ROLE_MAPPINGS_TITLE = i18n.translate(
-  'xpack.enterpriseSearch.appSearch.roleMappings.title',
-  { defaultMessage: 'Role Mappings' }
+export const EMPTY_ROLE_MAPPINGS_BODY = i18n.translate(
+  'xpack.enterpriseSearch.appSearch.roleMapping.emptyRoleMappingsBody',
+  {
+    defaultMessage:
+      'All users who successfully authenticate will be assigned the Owner role and have access to all engines. Add a new role to override the default.',
+  }
 );
 
 export const DELETE_ROLE_MAPPINGS_MESSAGE = i18n.translate(
@@ -17,5 +20,40 @@ export const DELETE_ROLE_MAPPINGS_MESSAGE = i18n.translate(
   {
     defaultMessage:
       'Are you sure you want to permanently delete this mapping? This action is not reversible and some users might lose access.',
+  }
+);
+
+export const ROLE_MAPPINGS_ENGINE_ACCESS_HEADING = i18n.translate(
+  'xpack.enterpriseSearch.appSearch.roleMappingsEngineAccessHeading',
+  {
+    defaultMessage: 'Engine access',
+  }
+);
+
+export const ROLE_MAPPINGS_RESET_BUTTON = i18n.translate(
+  'xpack.enterpriseSearch.appSearch.roleMappingsResetButton',
+  {
+    defaultMessage: 'Reset mappings',
+  }
+);
+
+export const ROLE_MAPPINGS_RESET_CONFIRM_TITLE = i18n.translate(
+  'xpack.enterpriseSearch.appSearch.roleMappingsResetConfirmTitle',
+  {
+    defaultMessage: 'Are you sure you want to reset role mappings?',
+  }
+);
+
+export const ROLE_MAPPINGS_RESET_CONFIRM_BUTTON = i18n.translate(
+  'xpack.enterpriseSearch.appSearch.roleMappingsResetConfirmButton',
+  {
+    defaultMessage: 'Reset role mappings',
+  }
+);
+
+export const ROLE_MAPPINGS_RESET_CANCEL_BUTTON = i18n.translate(
+  'xpack.enterpriseSearch.appSearch.roleMappingsResetCancelButton',
+  {
+    defaultMessage: 'Cancel',
   }
 );
