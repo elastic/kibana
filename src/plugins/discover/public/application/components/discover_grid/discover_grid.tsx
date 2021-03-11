3c6abe21ecd0fa006e9de65a932b306ba13098a9
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-03-11 16:16:05 +0100

    Add hits to the DiscoverGridFlyout

--- src/plugins/discover/public/application/components/discover_grid/discover_grid.tsx
+++ src/plugins/discover/public/application/components/discover_grid/discover_grid.tsx
@@ -323,6 +323,7 @@ export const DiscoverGrid = ({
           <DiscoverGridFlyout
             indexPattern={indexPattern}
             hit={expandedDoc}
+            hits={rows}
             // if default columns are used, dont make them part of the URL - the context state handling will take care to restore them
             columns={defaultColumns ? [] : displayedColumns}
             onFilter={onFilter}
