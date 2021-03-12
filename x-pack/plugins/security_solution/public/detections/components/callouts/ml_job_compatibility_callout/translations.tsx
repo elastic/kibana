5d095d1896ed09f3da1f651b2b16911a99cc005a
Ryland Herrick <ryalnd@gmail.com>
2021-03-11 19:43:15 -0600

    Add some placeholder copy to get the ball rolling

--- x-pack/plugins/security_solution/public/detections/components/callouts/ml_job_compatibility_callout/translations.tsx
+++ x-pack/plugins/security_solution/public/detections/components/callouts/ml_job_compatibility_callout/translations.tsx
@@ -13,7 +13,7 @@ import { MlJobCompatibilityLink } from '../../../../common/components/links_to_d
 export const ML_JOB_COMPATIBILITY_CALLOUT_TITLE = i18n.translate(
   'xpack.securitySolution.detectionEngine.mlJobCompatibilityCallout.messageTitle',
   {
-    defaultMessage: 'TODO title',
+    defaultMessage: 'Your ML jobs may be incompatible with your data sources and/or ML rules',
   }
 );
 
@@ -26,7 +26,10 @@ export const MlJobCompatibilityCalloutBody = () => (
         <p>
           <FormattedMessage
             id="xpack.securitySolution.detectionEngine.mlJobCompatibilityCallout.messageBody.summary"
-            defaultMessage="TODO summary"
+            defaultMessage="The ML jobs that are installed are compatible
+            with specific versions of ECS. To avoid missing alerts, ensure
+            that your ML jobs are compatible with your data sources, and that
+            your ML rules are configured with the appropriate ML jobs."
           />
         </p>
       ),
