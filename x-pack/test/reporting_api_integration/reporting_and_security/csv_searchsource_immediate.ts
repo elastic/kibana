78d9a8a25da6ffe737ad435f38f1b654e7ddaedf
Timothy Sullivan <tsullivan@elastic.co>
2021-03-15 17:06:06 -0700

    add functional test for ES returning an error

--- x-pack/test/reporting_api_integration/reporting_and_security/csv_searchsource_immediate.ts
+++ x-pack/test/reporting_api_integration/reporting_and_security/csv_searchsource_immediate.ts
@@ -197,6 +197,48 @@ export default function ({ getService }: FtrProviderContext) {
       await esArchiver.unload('reporting/ecommerce_kibana');
     });
 
+    it('Logs the error explanation if the search query returns an error', async () => {
+      await esArchiver.load('reporting/ecommerce');
+      await esArchiver.load('reporting/ecommerce_kibana');
+
+      const { status: resStatus, text: resText } = (await generateAPI.getCSVFromSearchSource(
+        getMockJobParams({
+          searchSource: {
+            query: { query: '', language: 'kuery' },
+            index: '5193f870-d861-11e9-a311-0fa548c5f953',
+            sort: [{ order_date: 'desc' }],
+            fields: ['order_date', 'products'], // products is a non-leaf field
+            filter: [],
+            parent: {
+              query: { language: 'kuery', query: '' },
+              filter: [],
+              parent: {
+                filter: [
+                  {
+                    meta: { index: '5193f870-d861-11e9-a311-0fa548c5f953', params: {} },
+                    range: {
+                      order_date: {
+                        gte: '2019-03-23T03:06:17.785Z',
+                        lte: '2019-10-04T02:33:16.708Z',
+                        format: 'strict_date_optional_time',
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+          browserTimezone: 'UTC',
+          title: 'testfooyu78yt90-',
+        })
+      )) as supertest.Response;
+      expect(resStatus).to.eql(500);
+      expectSnapshot(resText).toMatch();
+
+      await esArchiver.unload('reporting/ecommerce');
+      await esArchiver.unload('reporting/ecommerce_kibana');
+    });
+
     describe('date formatting', () => {
       before(async () => {
         // load test data that contains a saved search and documents
