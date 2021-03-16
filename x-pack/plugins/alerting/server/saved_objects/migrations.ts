391ed9fa1796e2fed8f2dcdda0d04ff796d3b677
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2021-03-16 12:07:41 -0400

    do not modify connector with the right structure

--- x-pack/plugins/alerting/server/saved_objects/migrations.ts
+++ x-pack/plugins/alerting/server/saved_objects/migrations.ts
@@ -10,6 +10,7 @@ import {
   SavedObjectUnsanitizedDoc,
   SavedObjectMigrationFn,
   SavedObjectMigrationContext,
+  SavedObjectAttributes,
 } from '../../../../../src/core/server';
 import { RawAlert, RawAlertAction } from '../types';
 import { EncryptedSavedObjectsPluginSetup } from '../../../encrypted_saved_objects/server';
@@ -186,6 +187,15 @@ function restructureConnectorsThatSupportIncident(
   const { actions } = doc.attributes;
   const newActions = actions.reduce((acc, action) => {
     if (action.params.subAction !== 'pushToService') {
+      return [...acc, action];
+    }
+    // Future developer, we needed to do that because when we created this migration
+    // we forget to think about user already using 7.11.0 and having an incident attribute build the right way
+    // IMPORTANT -> if you change this code please do the same inside of this file
+    // x-pack/plugins/security_solution/server/lib/detection_engine/rule_actions/migrations.ts
+    const subActionParamsIncident =
+      (action.params?.subActionParams as SavedObjectAttributes)?.incident ?? null;
+    if (subActionParamsIncident != null) {
       return [...acc, action];
     }
 
