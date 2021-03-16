4a83a024336c70f518b6bd3b838eade73c1064ae
Jean-Louis Leysens <jloleysens@gmail.com>
2021-03-16 13:40:13 +0100

    [ILM] Hide node allocation notices on Cloud (#94581)
    * block node allocation notices on cloud
    
    * added test to check that notices are not showing
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/index_lifecycle_management/__jest__/client_integration/edit_policy/features/node_allocation.test.ts
+++ x-pack/plugins/index_lifecycle_management/__jest__/client_integration/edit_policy/features/node_allocation.test.ts
@@ -365,9 +365,9 @@ describe('<EditPolicy /> node allocation', () => {
         await act(async () => {
           testBed = await setup({ appServicesContext: { cloud: { isCloudEnabled: true } } });
         });
-        const { actions, component, exists, find } = testBed;
+        testBed.component.update();
 
-        component.update();
+        const { actions, component, exists, find } = testBed;
         await actions.warm.enable(true);
         expect(component.find('.euiLoadingSpinner').exists()).toBeFalsy();
 
@@ -375,35 +375,29 @@ describe('<EditPolicy /> node allocation', () => {
         expect(exists('defaultDataAllocationOption')).toBeTruthy();
         expect(exists('customDataAllocationOption')).toBeTruthy();
         expect(exists('noneDataAllocationOption')).toBeTruthy();
-        // We should not be showing the call-to-action for users to activate data tier in cloud
-        expect(exists('cloudDataTierCallout')).toBeFalsy();
         // Do not show the call-to-action for users to migrate their cluster to use node roles
         expect(find('cloudDataTierCallout').exists()).toBeFalsy();
       });
-
-      test(`shows cloud notice when cold tier nodes do not exist`, async () => {
+      test('do not show node allocation specific warnings on cloud', async () => {
         httpRequestsMockHelpers.setListNodes({
-          nodesByAttributes: {},
-          nodesByRoles: { data: ['test'], data_hot: ['test'], data_warm: ['test'] },
+          nodesByAttributes: { test: ['123'] },
+          // No nodes with node roles like "data_hot" or "data_warm"
+          nodesByRoles: {},
           isUsingDeprecatedDataRoleConfig: false,
         });
         await act(async () => {
           testBed = await setup({ appServicesContext: { cloud: { isCloudEnabled: true } } });
         });
-        const { actions, component, exists, find } = testBed;
+        testBed.component.update();
 
-        component.update();
+        const { actions, component, exists } = testBed;
+        await actions.warm.enable(true);
         await actions.cold.enable(true);
         expect(component.find('.euiLoadingSpinner').exists()).toBeFalsy();
 
-        expect(exists('cloudMissingTierCallout')).toBeTruthy();
-        expect(find('cloudMissingTierCallout').text()).toContain(
-          `Edit your Elastic Cloud deployment to set up a cold tier`
-        );
-
-        // Assert that other notices are not showing
-        expect(actions.cold.hasDefaultAllocationNotice()).toBeFalsy();
-        expect(actions.cold.hasNoNodeAttrsWarning()).toBeFalsy();
+        expect(exists('cloudDataTierCallout')).toBeFalsy();
+        expect(exists('defaultAllocationNotice')).toBeFalsy();
+        expect(exists('defaultAllocationWarning')).toBeFalsy();
       });
     });
   });
