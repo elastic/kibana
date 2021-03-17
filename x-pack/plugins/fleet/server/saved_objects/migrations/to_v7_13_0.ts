b3e3f854f9649980a1d3e00ef17d46ea1c496cfa
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2021-03-17 09:54:22 -0400

    [Fleet] Remove unused Fleet settings package|agent auto upgrade

---
+++ x-pack/plugins/fleet/server/saved_objects/migrations/to_v7_13_0.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { SavedObjectMigrationFn } from 'kibana/server';
+
+import type { Settings } from '../../types';
+
+export const migrateSettingsToV7130: SavedObjectMigrationFn<
+  Exclude<Settings, 'kibana_urls'> & {
+    package_auto_upgrade: string;
+    agent_auto_upgrade: string;
+  },
+  Settings
+> = (settingsDoc) => {
+  // @ts-expect-error
+  delete settingsDoc.attributes.package_auto_upgrade;
+  // @ts-expect-error
+  delete settingsDoc.attributes.agent_auto_upgrade;
+
+  return settingsDoc;
+};
