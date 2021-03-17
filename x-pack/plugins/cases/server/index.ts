07bd45a6576b7864b2288e3b2ff3b073d64569ae
Steph Milovic <stephanie.milovic@elastic.co>
2021-03-17 11:50:36 -0600

    add type for deprecations

--- x-pack/plugins/cases/server/index.ts
+++ x-pack/plugins/cases/server/index.ts
@@ -5,12 +5,12 @@
  * 2.0.
  */
 
-import { PluginInitializerContext } from 'kibana/server';
-import { ConfigSchema } from './config';
+import { PluginConfigDescriptor, PluginInitializerContext } from 'kibana/server';
+import { ConfigType, ConfigSchema } from './config';
 import { CasePlugin } from './plugin';
 
 export { CaseRequestContext } from './types';
-export const config = {
+export const config: PluginConfigDescriptor<ConfigType> = {
   schema: ConfigSchema,
   deprecations: ({ renameFromRoot }) => [
     renameFromRoot('xpack.case.enabled', 'xpack.cases.enabled'),
