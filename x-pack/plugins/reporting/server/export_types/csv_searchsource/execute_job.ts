b97b3813f0e42740b9200b9ce50b954777eae0f4
Timothy Sullivan <tsullivan@elastic.co>
2021-03-16 17:35:09 -0700

    fix types again

--- x-pack/plugins/reporting/server/export_types/csv_searchsource/execute_job.ts
+++ x-pack/plugins/reporting/server/export_types/csv_searchsource/execute_job.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import { CSV_JOB_TYPE } from '../../../common/constants';
 import { getFieldFormats } from '../../services';
 import { RunTaskFn, RunTaskFnFactory } from '../../types';
 import { decryptJobHeaders } from '../common';
