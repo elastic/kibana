be9ea93a1532c598f1016765716693b6162f09f8
Spencer <email@spalger.com>
2021-03-17 09:49:39 -0700

    [theme] default to v8 theme (#94370)
    Co-authored-by: spalger <spalger@users.noreply.github.com>
    # Conflicts:
    #	packages/kbn-optimizer/src/integration_tests/basic_optimization.test.ts
    #	test/functional/page_objects/visual_builder_page.ts
    #	test/functional/screenshots/baseline/area_chart.png
    #	test/functional/screenshots/baseline/tsvb_dashboard.png
    #	test/interpreter_functional/screenshots/baseline/metric_multi_metric_data.png
    #	x-pack/test/functional/apps/ml/data_frame_analytics/outlier_detection_creation.ts

--- packages/kbn-optimizer/src/common/theme_tags.ts
+++ packages/kbn-optimizer/src/common/theme_tags.ts
@@ -17,7 +17,7 @@ const isArrayOfStrings = (input: unknown): input is string[] =>
 
 export type ThemeTags = readonly ThemeTag[];
 export type ThemeTag = 'v7light' | 'v7dark' | 'v8light' | 'v8dark';
-export const DEFAULT_THEMES = tags('v7light', 'v7dark');
+export const DEFAULT_THEMES = tags('v8light', 'v8dark');
 export const ALL_THEMES = tags('v7light', 'v7dark', 'v8light', 'v8dark');
 
 export function parseThemeTags(input?: any): ThemeTags {
