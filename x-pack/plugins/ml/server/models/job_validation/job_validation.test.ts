805b814cb486497e10dfea3eee59333c4183787e
Walter Rafelsberger <walter@elastic.co>
2021-03-17 07:53:46 +0100

    [ML] Anomaly Detection: Migrate validation messages links to use docLinks. (#94568)
    - To make use of the docsLinks service which is only usable in client side code, Anomaly Detection's validation messages are not fully returned from the server anymore. Instead just the message ID and necessary metadata to parse the message template gets returned.
    - getMessages() no longer uses inline hard coded documentation links but picks links from the docsLinks service.
    - The code that rendered the messages originally on the server has been move to a function parseMessages() which can now be used on the client side and accepts the docsLinks services to get URLs to documentation from it.
    - This means we no longer need to get the current version/branch information for the server side code.
    - Tests have been updated to reflect the changes: API integration tests only check for the now reduced messages containing only message IDs and metadata. The expected results of the API integration tests are used as mocks for the client side function parseMessages(), this allows use to cover the same code and messages as previously.

--- x-pack/plugins/ml/server/models/job_validation/job_validation.test.ts
+++ x-pack/plugins/ml/server/models/job_validation/job_validation.test.ts
@@ -8,7 +8,6 @@
 import { IScopedClusterClient } from 'kibana/server';
 
 import { validateJob, ValidateJobPayload } from './job_validation';
-import { JobValidationMessage } from '../../../common/constants/messages';
 import { HITS_TOTAL_RELATION } from '../../../common/types/es_client';
 import type { MlClient } from '../../lib/ml_client';
 
@@ -396,29 +395,6 @@ describe('ML - validateJob', () => {
         'time_field_invalid',
         'influencer_low_suggestion',
       ]);
-    });
-  });
-
-  // the following two tests validate the correct template rendering of
-  // urls in messages with {{version}} in them to be replaced with the
-  // specified version. (defaulting to 'current')
-  const docsTestPayload = getBasicPayload() as any;
-  docsTestPayload.job.analysis_config.detectors = [{ function: 'count', by_field_name: 'airline' }];
-  it('creates a docs url pointing to the current docs version', () => {
-    return validateJob(mlClusterClient, mlClient, docsTestPayload).then((messages) => {
-      const message = messages[
-        messages.findIndex((m) => m.id === 'field_not_aggregatable')
-      ] as JobValidationMessage;
-      expect(message.url!.search('/current/')).not.toBe(-1);
-    });
-  });
-
-  it('creates a docs url pointing to the master docs version', () => {
-    return validateJob(mlClusterClient, mlClient, docsTestPayload, 'master').then((messages) => {
-      const message = messages[
-        messages.findIndex((m) => m.id === 'field_not_aggregatable')
-      ] as JobValidationMessage;
-      expect(message.url!.search('/master/')).not.toBe(-1);
     });
   });
 });
