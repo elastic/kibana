6597f24119096d0e76f76a1758800539838a5cb6
Angela Chuang <yi-chun.chuang@elastic.co>
2021-03-18 17:36:27 +0800

    update prepackaged timelines path

--- x-pack/plugins/security_solution/server/lib/timeline/utils/check_timelines_status.ts
+++ x-pack/plugins/security_solution/server/lib/timeline/utils/check_timelines_status.ts
@@ -69,7 +69,7 @@ export const checkTimelinesStatus = async (
     timeline: TimelineSavedObject[];
   };
   const dir = resolve(
-    join(__dirname, filePath ?? '../../../detection_engine/rules/prepackaged_timelines')
+    join(__dirname, filePath ?? '../../detection_engine/rules/prepackaged_timelines')
   );
   const file = fileName ?? 'index.ndjson';
   const dataPath = path.join(dir, file);
