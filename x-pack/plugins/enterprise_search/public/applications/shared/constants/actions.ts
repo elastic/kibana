e0add0ff52109998f9e178ca58dea70d8a210526
Constance Chen <constance.chen.3@gmail.com>
2021-03-22 13:00:34 -0700

    DRY out repeated button/table/modal actions copy to a shared file

---
+++ x-pack/plugins/enterprise_search/public/applications/shared/constants/actions.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { i18n } from '@kbn/i18n';
+
+export const EDIT = i18n.translate('xpack.enterpriseSearch.actions.edit', {
+  defaultMessage: 'Edit',
+});
+
+export const DELETE = i18n.translate('xpack.enterpriseSearch.actions.delete', {
+  defaultMessage: 'Delete',
+});
+
+export const SAVE = i18n.translate('xpack.enterpriseSearch.actions.save', {
+  defaultMessage: 'Save',
+});
+
+export const CANCEL = i18n.translate('xpack.enterpriseSearch.actions.cancel', {
+  defaultMessage: 'Cancel',
+});
+
+export const CONTINUE = i18n.translate('xpack.enterpriseSearch.actions.continue', {
+  defaultMessage: 'Continue',
+});
+
+export const CLOSE = i18n.translate('xpack.enterpriseSearch.actions.close', {
+  defaultMessage: 'Close',
+});
