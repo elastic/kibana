8c1f55441dfa54f6c0ad32c57328acdc59c66884
Constance Chen <constance.chen.3@gmail.com>
2021-03-22 15:26:07 -0700

    Engine table - fix incorrect conditional around when users can delete engines
    - The check for that should be around Dev/Editor/Analyst roles, not around whether the account has a platinum license
    
    - Tests - DRY out reset mock
    - ideally would be in a beforeEach, but mount/beforeAll perf makes that difficult

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/engines/engines_table.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/engines/engines_table.tsx
@@ -19,9 +19,9 @@ import { i18n } from '@kbn/i18n';
 import { FormattedNumber } from '@kbn/i18n/react';
 
 import { KibanaLogic } from '../../../shared/kibana';
-import { LicensingLogic } from '../../../shared/licensing';
 import { EuiLinkTo } from '../../../shared/react_router_helpers';
 import { TelemetryLogic } from '../../../shared/telemetry';
+import { AppLogic } from '../../app_logic';
 import { UNIVERSAL_LANGUAGE } from '../../constants';
 import { ENGINE_PATH } from '../../routes';
 import { generateEncodedPath } from '../../utils/encode_path_params';
@@ -52,7 +52,9 @@ export const EnginesTable: React.FC<EnginesTableProps> = ({
 }) => {
   const { sendAppSearchTelemetry } = useActions(TelemetryLogic);
   const { navigateToUrl } = useValues(KibanaLogic);
-  const { hasPlatinumLicense } = useValues(LicensingLogic);
+  const {
+    myRole: { canManageEngines },
+  } = useValues(AppLogic);
 
   const generateEncodedEnginePath = (engineName: string) =>
     generateEncodedPath(ENGINE_PATH, { engineName });
@@ -200,7 +202,7 @@ export const EnginesTable: React.FC<EnginesTableProps> = ({
     ],
   };
 
-  if (hasPlatinumLicense) {
+  if (canManageEngines) {
     columns.push(actionsColumn);
   }
 
