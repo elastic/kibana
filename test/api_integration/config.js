59d1d208b98ae40f8f3c149c64a965ffdc397fcf
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2021-03-22 10:52:18 +0100

    API Integration tests: telemetry.optIn=true

--- test/api_integration/config.js
+++ test/api_integration/config.js
@@ -27,6 +27,7 @@ export default async function ({ readConfigFile }) {
       ...functionalConfig.get('kbnTestServer'),
       serverArgs: [
         ...functionalConfig.get('kbnTestServer.serverArgs'),
+        '--telemetry.optIn=true',
         '--elasticsearch.healthCheck.delay=3600000',
         '--server.xsrf.disableProtection=true',
         '--server.compression.referrerWhitelist=["some-host.com"]',
