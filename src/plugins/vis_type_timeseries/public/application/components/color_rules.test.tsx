059d561ca9b31cbbc2910a8e772a856ab4f18ada
Daniil <sul-dan@mail.ru>
2021-03-23 16:31:50 +0300

    [TSVB] Type public code. Step 2 - panel configs (#94403)
    * Remove request facade and update search strategies
    
    * Use typescript
    
    * Type files
    
    * Update structure
    
    * Update tests
    
    * Type annotations
    
    * Fix type for infra
    
    * Type editor_controller
    
    * Type vis_editor
    
    * Type vis_picker
    
    * Fix types
    
    * Type panel_config
    
    * Fix vis data type
    
    * Enhance types
    
    * Remove generics
    
    * Use constant
    
    * Update docs
    
    * Use empty object as default data
    
    * Convert yes_no component to typescript
    
    * Type color rules
    
    * Type panel configs
    
    * Type helpers
    
    * Type color rules
    
    * Type collection actions
    
    * Get rid of create_text_handler
    
    * Fix collection actions types
    
    * Revert get_request_params changes, do some code refactoring, type create_number_handler and get rid of detect_ie
    
    Co-authored-by: Daniil Suleiman <daniil_suleiman@epam.com>
    Co-authored-by: Diana Derevyankina <dziyana_dzeraviankina@epam.com>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/vis_type_timeseries/public/application/components/color_rules.test.tsx
+++ src/plugins/vis_type_timeseries/public/application/components/color_rules.test.tsx
@@ -7,59 +7,58 @@
  */
 
 import React from 'react';
-import { collectionActions } from './lib/collection_actions';
-import { ColorRules } from './color_rules';
 import { keys } from '@elastic/eui';
 import { findTestSubject } from '@elastic/eui/lib/test';
 import { mountWithIntl } from '@kbn/test/jest';
 
+import { collectionActions } from './lib/collection_actions';
+import { ColorRules, ColorRulesProps } from './color_rules';
+
 describe('src/legacy/core_plugins/metrics/public/components/color_rules.test.js', () => {
-  let defaultProps;
-  beforeAll(() => {
-    defaultProps = {
-      name: 'gauge_color_rules',
-      model: {
-        gauge_color_rules: [
-          {
-            gauge: null,
-            value: 0,
-            id: 'unique value',
-          },
-        ],
-      },
-      onChange: jest.fn(),
-    };
-  });
+  const defaultProps = ({
+    name: 'gauge_color_rules',
+    model: {
+      gauge_color_rules: [
+        {
+          gauge: null,
+          value: 0,
+          id: 'unique value',
+        },
+      ],
+    },
+    onChange: jest.fn(),
+  } as unknown) as ColorRulesProps;
+
   describe('ColorRules', () => {
     it('should render empty <div/> node', () => {
-      const emptyProps = {
+      const emptyProps = ({
         name: 'gauge_color_rules',
         model: {},
         onChange: jest.fn(),
-      };
-      const wrapper = mountWithIntl(<ColorRules.WrappedComponent {...emptyProps} />);
+      } as unknown) as ColorRulesProps;
+      const wrapper = mountWithIntl(<ColorRules {...emptyProps} />);
       const isNode = wrapper.find('div').children().exists();
       expect(isNode).toBeFalsy();
     });
 
     it('should render non-empty <div/> node', () => {
-      const wrapper = mountWithIntl(<ColorRules.WrappedComponent {...defaultProps} />);
+      const wrapper = mountWithIntl(<ColorRules {...defaultProps} />);
       const isNode = wrapper.find('div.tvbColorPicker').exists();
 
       expect(isNode).toBeTruthy();
     });
     it('should handle change of operator and value correctly', () => {
       collectionActions.handleChange = jest.fn();
-      const wrapper = mountWithIntl(<ColorRules.WrappedComponent {...defaultProps} />);
+      const wrapper = mountWithIntl(<ColorRules {...defaultProps} />);
       const operatorInput = findTestSubject(wrapper, 'colorRuleOperator');
       operatorInput.simulate('keyDown', { key: keys.ARROW_DOWN });
       operatorInput.simulate('keyDown', { key: keys.ARROW_DOWN });
       operatorInput.simulate('keyDown', { key: keys.ENTER });
-      expect(collectionActions.handleChange.mock.calls[0][1].operator).toEqual('gt');
+      expect((collectionActions.handleChange as jest.Mock).mock.calls[0][1].operator).toEqual('gt');
 
       const numberInput = findTestSubject(wrapper, 'colorRuleValue');
       numberInput.simulate('change', { target: { value: '123' } });
-      expect(collectionActions.handleChange.mock.calls[1][1].value).toEqual(123);
+      expect((collectionActions.handleChange as jest.Mock).mock.calls[1][1].value).toEqual(123);
     });
   });
 });
