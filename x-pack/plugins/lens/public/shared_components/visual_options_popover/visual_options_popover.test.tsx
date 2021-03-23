43087855d6d37baeb7a1aaeb26e5a7b888bbab97
Shahzad <shahzad31comp@gmail.com>
2021-03-23 13:52:19 +0100

    ununsed import

--- x-pack/plugins/lens/public/shared_components/visual_options_popover/visual_options_popover.test.tsx
+++ x-pack/plugins/lens/public/shared_components/visual_options_popover/visual_options_popover.test.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { mountWithIntl, shallowWithIntl as shallow } from '@kbn/test/jest';
+import { shallowWithIntl as shallow } from '@kbn/test/jest';
 import { Position } from '@elastic/charts';
 import { FramePublicAPI } from '../../types';
 import { createMockDatasource, createMockFramePublicAPI } from '../../editor_frame_service/mocks';
