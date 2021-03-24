b12ad8c20dab85f1a47dcc453da794f008a7466e
Yara Tercero <yara.tercero@elastic.co>
2021-03-23 22:51:48 -0700

    added logic buttons to lists plugin

--- x-pack/plugins/lists/public/exceptions/components/builder/logic_buttons.tsx
+++ x-pack/plugins/lists/public/exceptions/components/builder/logic_buttons.tsx
@@ -6,38 +6,37 @@
  */
 
 import React from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiButton } from '@elastic/eui';
+import { EuiButton, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import styled from 'styled-components';
 
 import * as i18n from './translations';
-import * as i18nShared from '../translations';
 
 const MyEuiButton = styled(EuiButton)`
   min-width: 95px;
 `;
 
 interface BuilderLogicButtonsProps {
-  isOrDisabled: boolean;
   isAndDisabled: boolean;
-  isNestedDisabled: boolean;
   isNested: boolean;
+  isNestedDisabled: boolean;
+  isOrDisabled: boolean;
   showNestedButton: boolean;
-  onAndClicked: () => void;
-  onOrClicked: () => void;
-  onNestedClicked: () => void;
   onAddClickWhenNested: () => void;
+  onAndClicked: () => void;
+  onNestedClicked: () => void;
+  onOrClicked: () => void;
 }
 
 export const BuilderLogicButtons: React.FC<BuilderLogicButtonsProps> = ({
-  isOrDisabled = false,
   isAndDisabled = false,
-  showNestedButton = false,
-  isNestedDisabled = true,
   isNested,
-  onAndClicked,
-  onOrClicked,
-  onNestedClicked,
+  isNestedDisabled = true,
+  isOrDisabled = false,
+  showNestedButton = false,
   onAddClickWhenNested,
+  onAndClicked,
+  onNestedClicked,
+  onOrClicked,
 }) => (
   <EuiFlexGroup gutterSize="s" alignItems="center">
     <EuiFlexItem grow={false}>
@@ -49,7 +48,7 @@ export const BuilderLogicButtons: React.FC<BuilderLogicButtonsProps> = ({
         data-test-subj="exceptionsAndButton"
         isDisabled={isAndDisabled}
       >
-        {i18nShared.AND}
+        {i18n.AND}
       </MyEuiButton>
     </EuiFlexItem>
     <EuiFlexItem grow={false}>
@@ -61,7 +60,7 @@ export const BuilderLogicButtons: React.FC<BuilderLogicButtonsProps> = ({
         isDisabled={isOrDisabled}
         data-test-subj="exceptionsOrButton"
       >
-        {i18nShared.OR}
+        {i18n.OR}
       </MyEuiButton>
     </EuiFlexItem>
     {showNestedButton && (
