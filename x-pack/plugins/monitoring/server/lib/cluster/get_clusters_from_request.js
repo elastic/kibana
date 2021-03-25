983ebad298efa6e9f3ee2ba4e4c1aed671533cc3
Chris Roberson <chrisronline@gmail.com>
2021-03-25 11:35:04 -0400

    [Monitoring] Fetch status once and change fetchStatus to support an array of clusters (#91749) (#95409)
    * Fetch status once and change fetchStatus to support an array of clusters
    
    * Update test

--- x-pack/plugins/monitoring/server/lib/cluster/get_clusters_from_request.js
+++ x-pack/plugins/monitoring/server/lib/cluster/get_clusters_from_request.js
@@ -120,6 +120,13 @@ export async function getClustersFromRequest(
     // add alerts data
     if (isInCodePath(codePaths, [CODE_PATH_ALERTS])) {
       const alertsClient = req.getAlertsClient();
+      const alertStatus = await fetchStatus(
+        alertsClient,
+        req.server.plugins.monitoring.info,
+        undefined,
+        clusters.map((cluster) => cluster.cluster_uuid)
+      );
+
       for (const cluster of clusters) {
         const verification = verifyMonitoringLicense(req.server);
         if (!verification.enabled) {
@@ -154,12 +161,20 @@ export async function getClustersFromRequest(
         if (prodLicenseInfo.clusterAlerts.enabled) {
           try {
             cluster.alerts = {
-              list: await fetchStatus(
-                alertsClient,
-                req.server.plugins.monitoring.info,
-                undefined,
-                cluster.cluster_uuid
-              ),
+              list: Object.keys(alertStatus).reduce((accum, alertName) => {
+                const value = alertStatus[alertName];
+                if (value.states && value.states.length) {
+                  accum[alertName] = {
+                    ...value,
+                    states: value.states.filter(
+                      (state) => state.state.cluster.clusterUuid === cluster.cluster_uuid
+                    ),
+                  };
+                } else {
+                  accum[alertName] = value;
+                }
+                return accum;
+              }, {}),
               alertsMeta: {
                 enabled: true,
               },
