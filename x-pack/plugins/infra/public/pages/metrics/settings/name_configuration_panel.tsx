964e4bae9d0c7b9bde7a64c2a90a9ab19ff1b784
Felix Stürmer <stuermer@weltenwort.de>
2021-03-26 19:06:52 +0100

    Move code only used for metrics settings

--- x-pack/plugins/infra/public/pages/metrics/settings/name_configuration_panel.tsx
+++ x-pack/plugins/infra/public/pages/metrics/settings/name_configuration_panel.tsx
@@ -15,7 +15,6 @@ import {
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import React from 'react';
-
 import { InputFieldProps } from './input_fields';
 
 interface NameConfigurationPanelProps {
