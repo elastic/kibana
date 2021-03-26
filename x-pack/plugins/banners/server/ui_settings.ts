707ea3b269f0d3a61f500b73f3cfd79910abe4db
pgayvallet <pierre.gayvallet@gmail.com>
2021-03-26 09:24:33 +0100

    add descriptions to banner settings

--- x-pack/plugins/banners/server/ui_settings.ts
+++ x-pack/plugins/banners/server/ui_settings.ts
@@ -15,10 +15,25 @@ export const registerSettings = (uiSettings: UiSettingsServiceSetup, config: Ban
   if (config.disableSpaceBanners) {
     return;
   }
+
+  const subscriptionLink = `
+      <a href="https://www.elastic.co/subscriptions" target="_blank" rel="noopener noreferrer">
+        ${i18n.translate('xpack.banners.settings.subscriptionRequiredLink.text', {
+          defaultMessage: 'Subscription required.',
+        })}
+      </a>
+  `;
+
   uiSettings.register({
     'banners:placement': {
       name: i18n.translate('xpack.banners.settings.placement.title', {
         defaultMessage: 'Banner placement',
+      }),
+      description: i18n.translate('xpack.banners.settings.placement.description', {
+        defaultMessage: 'Set the placement of the banner. {subscriptionLink}',
+        values: {
+          subscriptionLink,
+        },
       }),
       category: ['banner'],
       order: 1,
@@ -33,6 +48,12 @@ export const registerSettings = (uiSettings: UiSettingsServiceSetup, config: Ban
       name: i18n.translate('xpack.banners.settings.textContent.title', {
         defaultMessage: 'Banner text',
       }),
+      description: i18n.translate('xpack.banners.settings.text.description', {
+        defaultMessage: 'Add Markdown-formatted text to the banner. {subscriptionLink}',
+        values: {
+          subscriptionLink,
+        },
+      }),
       sensitive: true,
       category: ['banner'],
       order: 2,
@@ -44,6 +65,12 @@ export const registerSettings = (uiSettings: UiSettingsServiceSetup, config: Ban
     'banners:textColor': {
       name: i18n.translate('xpack.banners.settings.textColor.title', {
         defaultMessage: 'Banner text color',
+      }),
+      description: i18n.translate('xpack.banners.settings.textColor.description', {
+        defaultMessage: 'Set the color of the banner text. {subscriptionLink}',
+        values: {
+          subscriptionLink,
+        },
       }),
       category: ['banner'],
       order: 3,
@@ -61,6 +88,12 @@ export const registerSettings = (uiSettings: UiSettingsServiceSetup, config: Ban
     'banners:backgroundColor': {
       name: i18n.translate('xpack.banners.settings.backgroundColor.title', {
         defaultMessage: 'Banner background color',
+      }),
+      description: i18n.translate('xpack.banners.settings.backgroundColor.description', {
+        defaultMessage: 'Set the background color for the banner. {subscriptionLink}',
+        values: {
+          subscriptionLink,
+        },
       }),
       category: ['banner'],
       order: 4,
