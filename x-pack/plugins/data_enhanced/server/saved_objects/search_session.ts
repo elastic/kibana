08d347b494bb608ef2c59ca77b4fe981723e1af7
Anton Dosov <anton.dosov@elastic.co>
2021-03-26 17:10:24 +0100

    fix types

--- x-pack/plugins/data_enhanced/server/saved_objects/search_session.ts
+++ x-pack/plugins/data_enhanced/server/saved_objects/search_session.ts
@@ -7,7 +7,7 @@
 
 import { SavedObjectsType } from 'kibana/server';
 import { SEARCH_SESSION_TYPE } from '../../common';
-import { searchSessionSavedObjectMigrations } from './search_sessions_migrations';
+import { searchSessionSavedObjectMigrations } from './search_session_migration';
 
 export const searchSessionSavedObjectType: SavedObjectsType = {
   name: SEARCH_SESSION_TYPE,
