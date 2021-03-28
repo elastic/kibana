b1871142e5c62901fc10e479114d5d2e7bb5118e
Robert Monfera <monfera.robert@gmail.com>
2021-03-28 11:20:05 +0200

    feat: ports

--- x-pack/plugins/canvas/canvas_plugin_src/renderers/flow/index.tsx
+++ x-pack/plugins/canvas/canvas_plugin_src/renderers/flow/index.tsx
@@ -26,8 +26,37 @@ export const flow: RendererFactory<Output> = () => ({
       const width = domNode.offsetWidth;
       const height = domNode.offsetHeight;
 
-      const tWidth = 16;
-      const tHeight = 16;
+      const ports = [
+        { place: 'left', index: 0, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 1, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 2, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 3, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 4, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 5, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 6, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 7, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 8, color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 9, color: 'rgb(84, 179, 153)' },
+        { place: 'right', index: 0, color: 'rgb(96, 146, 192)' },
+        { place: 'right', index: 2, color: 'rgb(96, 146, 192)' },
+      ];
+
+      const leftCount = ports.reduce((p, { place }) => (place === 'left' ? p + 1 : p), 0);
+      // const rightCount = ports.reduce((p, { place }) => (place === 'right' ? p + 1 : p), 0);
+
+      const tWidthDesired = 16;
+      const tHeightDesired = 16;
+      const paddingDesired = 4;
+
+      // const tHorizPitchDesired = tWidthDesired + paddingDesired;
+      const tVertPitchDesired = tHeightDesired + paddingDesired;
+
+      // const tHorizPitch = Math.min(tHorizPitchDesired, height / leftCount)
+      const tVertPitch = Math.min(tVertPitchDesired, height / leftCount);
+
+      const tWidth = tWidthDesired;
+      const tHeight = tHeightDesired * (tVertPitch / tVertPitchDesired);
+      const vertPadding = paddingDesired * (tVertPitch / tVertPitchDesired);
 
       ReactDOM.render(
         <svg
@@ -35,8 +64,16 @@ export const flow: RendererFactory<Output> = () => ({
           style={{ strokeWidth: borderWidth, stroke: border }}
         >
           <rect x={0} y={0} width={width} height={height} style={{ fill }} />
-          <rect x={0} y={0} width={tWidth} height={tHeight} fill="rgb(84, 179, 153)" />
-          <rect x={width - tWidth} y={0} width={tWidth} height={tHeight} fill="rgb(96, 146, 192)" />
+          {ports.map(({ place, index, color }, i) => (
+            <rect
+              key={i}
+              x={place === 'left' ? 0 : width - tWidth}
+              y={vertPadding / 2 + index * (tHeight + vertPadding)}
+              width={tWidth}
+              height={tHeight}
+              style={{ fill: color }}
+            />
+          ))}
         </svg>,
         domNode
       );
