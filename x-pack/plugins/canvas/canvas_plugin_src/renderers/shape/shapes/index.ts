d8906b93d8f1803ae13c377054620494c390cb69
Robert Monfera <monfera.robert@gmail.com>
2021-03-28 13:38:06 +0200

    feat: line shapes

--- x-pack/plugins/canvas/canvas_plugin_src/renderers/shape/shapes/index.ts
+++ x-pack/plugins/canvas/canvas_plugin_src/renderers/shape/shapes/index.ts
@@ -11,6 +11,14 @@ import bookmark from '!!raw-loader!./bookmark.svg';
 import cross from '!!raw-loader!./cross.svg';
 import circle from '!!raw-loader!./circle.svg';
 import hexagon from '!!raw-loader!./hexagon.svg';
+import lineElbowUp from '!!raw-loader!./line_elbow_up.svg';
+import lineElbowDown from '!!raw-loader!./line_elbow_down.svg';
+import lineSmoothUp from '!!raw-loader!./line_smooth_up.svg';
+import lineSmoothDown from '!!raw-loader!./line_smooth_down.svg';
+import lineStraightUp from '!!raw-loader!./line_straight_up.svg';
+import lineStraightDown from '!!raw-loader!./line_straight_down.svg';
+import lineHorizontal from '!!raw-loader!./line_horizontal.svg';
+import lineVertical from '!!raw-loader!./line_vertical.svg';
 import kite from '!!raw-loader!./kite.svg';
 import pentagon from '!!raw-loader!./pentagon.svg';
 import rhombus from '!!raw-loader!./rhombus.svg';
@@ -29,6 +37,14 @@ export const shapes = {
   cross,
   circle,
   hexagon,
+  lineElbowUp,
+  lineElbowDown,
+  lineSmoothUp,
+  lineSmoothDown,
+  lineStraightUp,
+  lineStraightDown,
+  lineHorizontal,
+  lineVertical,
   kite,
   pentagon,
   rhombus,
