a74649a306d60c9009bc488bc838efbb38df1476
Robert Monfera <monfera.robert@gmail.com>
2021-03-28 12:16:34 +0200

    feat: ports respond to available height

--- x-pack/plugins/canvas/canvas_plugin_src/renderers/flow/index.tsx
+++ x-pack/plugins/canvas/canvas_plugin_src/renderers/flow/index.tsx
@@ -27,52 +27,68 @@ export const flow: RendererFactory<Output> = () => ({
       const height = domNode.offsetHeight;
 
       const ports = [
-        { place: 'left', index: 0, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 1, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 2, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 3, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 4, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 5, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 6, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 7, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 8, color: 'rgb(84, 179, 153)' },
-        { place: 'left', index: 9, color: 'rgb(84, 179, 153)' },
-        { place: 'right', index: 0, color: 'rgb(96, 146, 192)' },
-        { place: 'right', index: 2, color: 'rgb(96, 146, 192)' },
+        { place: 'left', index: 0, title: 'left 0', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 1, title: 'left 1', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 2, title: 'left 2', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 3, title: 'left 3', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 4, title: 'left 4', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 5, title: 'left 5', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 6, title: 'left 6', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 7, title: 'left 7', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 8, title: 'left 8', color: 'rgb(84, 179, 153)' },
+        { place: 'left', index: 9, title: 'left 9', color: 'rgb(84, 179, 153)' },
+        { place: 'right', index: 0, title: 'right 0', color: 'rgb(96, 146, 192)' },
+        { place: 'right', index: 1, title: 'right 1', color: 'rgb(96, 146, 192)' },
       ];
 
       const leftCount = ports.reduce((p, { place }) => (place === 'left' ? p + 1 : p), 0);
-      // const rightCount = ports.reduce((p, { place }) => (place === 'right' ? p + 1 : p), 0);
+      const rightCount = ports.reduce((p, { place }) => (place === 'right' ? p + 1 : p), 0);
 
       const tWidthDesired = 16;
       const tHeightDesired = 16;
       const paddingDesired = 4;
 
-      // const tHorizPitchDesired = tWidthDesired + paddingDesired;
       const tVertPitchDesired = tHeightDesired + paddingDesired;
 
-      // const tHorizPitch = Math.min(tHorizPitchDesired, height / leftCount)
-      const tVertPitch = Math.min(tVertPitchDesired, height / leftCount);
+      const tVertPitchLeft = Math.min(tVertPitchDesired, height / leftCount);
+      const tVertPitchRight = Math.min(tVertPitchDesired, height / rightCount);
 
       const tWidth = tWidthDesired;
-      const tHeight = tHeightDesired * (tVertPitch / tVertPitchDesired);
-      const vertPadding = paddingDesired * (tVertPitch / tVertPitchDesired);
+      const tHeightLeft = tHeightDesired * (tVertPitchLeft / tVertPitchDesired);
+      const tHeightRight = tHeightDesired * (tVertPitchRight / tVertPitchDesired);
+      const vertPaddingLeft = paddingDesired * (tVertPitchLeft / tVertPitchDesired);
+      const vertPaddingRight = paddingDesired * (tVertPitchRight / tVertPitchDesired);
+
+      const remainingPlaceLeft = Math.max(0, height - leftCount * tVertPitchLeft);
+      const remainingPlaceRight = Math.max(0, height - rightCount * tVertPitchRight);
 
       ReactDOM.render(
         <svg
           viewBox={`0 0 ${width} ${height}`}
           style={{ strokeWidth: borderWidth, stroke: border }}
         >
-          <rect x={0} y={0} width={width} height={height} style={{ fill }} />
-          {ports.map(({ place, index, color }, i) => (
+          <rect x={0} y={0} width={width} height={height} style={{ fill }}>
+            <title>Flow node</title>
+          </rect>
+          {ports.map(({ place, index, title, color }, i) => (
             <rect
               key={i}
               x={place === 'left' ? 0 : width - tWidth}
-              y={vertPadding / 2 + index * (tHeight + vertPadding)}
+              y={
+                place === 'left'
+                  ? vertPaddingLeft / 2 +
+                    index * (tHeightLeft + vertPaddingLeft) +
+                    remainingPlaceLeft / 2
+                  : vertPaddingRight / 2 +
+                    index * (tHeightRight + vertPaddingRight) +
+                    remainingPlaceRight / 2
+              }
               width={tWidth}
-              height={tHeight}
+              height={place === 'left' ? tHeightLeft : tHeightRight}
               style={{ fill: color }}
-            />
+            >
+              <title>{title}</title>
+            </rect>
           ))}
         </svg>,
         domNode
