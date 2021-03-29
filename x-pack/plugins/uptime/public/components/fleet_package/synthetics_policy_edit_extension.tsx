12ea76c808f3e58b766553de85e637a5d184ad57
Dominique Clarke <dominique.clarke@elastic.co>
2021-03-29 09:32:44 -0400

    add SyntheticsPolicyCreateExtension tests

--- x-pack/plugins/uptime/public/components/fleet_package/synthetics_policy_edit_extension.tsx
+++ x-pack/plugins/uptime/public/components/fleet_package/synthetics_policy_edit_extension.tsx
@@ -10,6 +10,7 @@ import { PackagePolicyEditExtensionComponentProps } from '../../../../fleet/publ
 import { Config, ConfigKeys, ICustomFields, contentTypesToMode, ContentType } from './types';
 import { CustomFields } from './custom_fields';
 import { useUpdatePolicy } from './use_update_policy';
+import { defaultConfig as fallbackConfig } from './synthetics_policy_create_extension';
 import { validate } from './validation';
 
 /**
@@ -23,10 +24,10 @@ export const SyntheticsPolicyEditExtension = memo<PackagePolicyEditExtensionComp
         const currentInput = currentPolicy.inputs.find((input) => input.enabled === true);
         const vars = currentInput?.streams[0]?.vars;
 
-        let configKeys: ConfigKeys[] = [];
-        if (vars) {
-          configKeys = Object.keys(vars) as ConfigKeys[];
-        }
+        const configKeys = Object.values(ConfigKeys) as ConfigKeys[];
+        // if (vars) {
+        //   configKeys = Object.keys(vars) as ConfigKeys[];
+        // }
         const formattedDefaultConfig = configKeys.reduce(
           (acc: Record<string, unknown>, key: ConfigKeys) => {
             const value = vars?.[key]?.value;
@@ -36,36 +37,48 @@ export const SyntheticsPolicyEditExtension = memo<PackagePolicyEditExtensionComp
                 break;
               case ConfigKeys.SCHEDULE:
                 // split unit and number
-                const fullString = JSON.parse(value);
-                const fullSchedule = fullString.replace('@every ', '');
-                const unit = fullSchedule.slice(-1);
-                const number = fullSchedule.slice(0, fullSchedule.length - 1);
-                acc[key] = {
-                  unit,
-                  number,
-                };
+                if (value) {
+                  const fullString = JSON.parse(value);
+                  const fullSchedule = fullString.replace('@every ', '');
+                  const unit = fullSchedule.slice(-1);
+                  const number = fullSchedule.slice(0, fullSchedule.length - 1);
+                  acc[key] = {
+                    unit,
+                    number,
+                  };
+                } else {
+                  acc[key] = fallbackConfig[key];
+                }
                 break;
               case ConfigKeys.WAIT:
-                acc[key] = value.slice(0, value.length - 1); // remove unit
+                acc[key] = value ? value.slice(0, value.length - 1) : fallbackConfig[key]; // remove unit
                 break;
               case ConfigKeys.TAGS:
               case ConfigKeys.RESPONSE_BODY_CHECK_NEGATIVE:
               case ConfigKeys.RESPONSE_BODY_CHECK_POSITIVE:
               case ConfigKeys.RESPONSE_RECEIVE_CHECK:
               case ConfigKeys.RESPONSE_STATUS_CHECK:
-                acc[key] = JSON.parse(value) || [];
-                break;
               case ConfigKeys.RESPONSE_HEADERS_CHECK:
               case ConfigKeys.REQUEST_HEADERS_CHECK:
-                acc[key] = JSON.parse(value) || {};
+                acc[key] = value ? JSON.parse(value) : fallbackConfig[key];
                 break;
               case ConfigKeys.REQUEST_BODY_CHECK:
-                const headers = JSON.parse(vars?.[ConfigKeys.REQUEST_HEADERS_CHECK].value) || {};
-                const requestBodyValue = JSON.parse(value) || '';
+                const headers = value
+                  ? JSON.parse(vars?.[ConfigKeys.REQUEST_HEADERS_CHECK].value)
+                  : fallbackConfig[ConfigKeys.REQUEST_HEADERS_CHECK];
+                const requestBodyValue =
+                  value !== null && value !== undefined
+                    ? JSON.parse(value)
+                    : fallbackConfig[key].value;
                 let type;
                 Object.keys(headers || []).map((headerKey) => {
-                  if (headerKey === 'Content-Type') {
+                  if (
+                    headerKey === 'Content-Type' &&
+                    contentTypesToMode[headers[headerKey] as ContentType]
+                  ) {
                     type = contentTypesToMode[headers[headerKey] as ContentType];
+                  } else {
+                    type = fallbackConfig[key].type;
                   }
                 });
                 acc[key] = {
@@ -74,7 +87,7 @@ export const SyntheticsPolicyEditExtension = memo<PackagePolicyEditExtensionComp
                 };
                 break;
               default:
-                acc[key] = value;
+                acc[key] = value ?? fallbackConfig[key];
             }
             return acc;
           },
