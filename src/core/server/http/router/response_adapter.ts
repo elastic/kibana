1e6a290edd2f79f41dc476550cfd7a140bb9a005
restrry <restrry@gmail.com>
2021-03-29 17:22:46 +0200

    pass error reason for ES responses provided in HTTP response body

--- src/core/server/http/router/response_adapter.ts
+++ src/core/server/http/router/response_adapter.ts
@@ -14,6 +14,8 @@ import typeDetect from 'type-detect';
 import Boom from '@hapi/boom';
 import * as stream from 'stream';
 
+import { isResponseError as isElasticsearchResponseError } from '../../elasticsearch/client/errors';
+
 import {
   HttpResponsePayload,
   KibanaResponse,
@@ -147,6 +149,11 @@ function getErrorMessage(payload?: ResponseError): string {
     throw new Error('expected error message to be provided');
   }
   if (typeof payload === 'string') return payload;
+  // for ES response errors include nested error reason message. it doesn't contain sensitive data.
+  if (isElasticsearchResponseError(payload)) {
+    return `[${payload.message}]: ${payload.meta.body?.error?.reason}`;
+  }
+
   return getErrorMessage(payload.message);
 }
 
