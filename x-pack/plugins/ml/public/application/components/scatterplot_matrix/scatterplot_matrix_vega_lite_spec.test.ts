7264d92250ee0eeb51099f3c0e6add84623f9812
Wylie Conlon <wylieconlon@gmail.com>
2021-03-29 17:08:15 -0400

    Update vega and vega-lite

--- x-pack/plugins/ml/public/application/components/scatterplot_matrix/scatterplot_matrix_vega_lite_spec.test.ts
+++ x-pack/plugins/ml/public/application/components/scatterplot_matrix/scatterplot_matrix_vega_lite_spec.test.ts
@@ -6,7 +6,7 @@
  */
 
 // @ts-ignore
-import { compile } from 'vega-lite/build-es5/vega-lite';
+import { compile } from 'vega-lite/build/vega-lite';
 
 import euiThemeLight from '@elastic/eui/dist/eui_theme_light.json';
 
--- x-pack/plugins/ml/public/application/components/vega_chart/vega_chart_view.tsx
+++ x-pack/plugins/ml/public/application/components/vega_chart/vega_chart_view.tsx
@@ -9,11 +9,11 @@ import React, { useMemo, useEffect, FC } from 'react';
 
 // There is still an issue with Vega Lite's typings with the strict mode Kibana is using.
 // @ts-ignore
-import type { TopLevelSpec } from 'vega-lite/build-es5/vega-lite';
+import type { TopLevelSpec } from 'vega-lite/build/vega-lite';
 
 // There is still an issue with Vega Lite's typings with the strict mode Kibana is using.
 // @ts-ignore
-import { compile } from 'vega-lite/build-es5/vega-lite';
+import { compile } from 'vega-lite/build/vega-lite';
 import { parse, View, Warn } from 'vega';
 import { Handler } from 'vega-tooltip';
 
