f430eb5344d077cdf3816e9e206cb27e272069cd
Alexey Antonov <alexwizp@gmail.com>
2021-03-29 11:32:01 +0300

    [TSVB] Enable `dual mode`, support index patterns and strings (#92812)
    * [TSVB] Enable `dual mode`, support index patterns and strings
    
    * modify UI
    
    * add migration script
    
    * refactoring
    
    * fix CI
    
    * prefill the index pattern name
    
    * modify UI
    
    * modify UI
    
    * update UI
    
    * fix functional test
    
    * some work
    
    * remove callouts
    
    * fix rollup test
    
    * update UI
    
    * fix typo
    
    * add some unit tests
    
    * add functional test
    
    * fix CI
    
    * correct labels
    
    * fix ci group 12
    
    * cleanup interface
    
    * fix CI
    
    * cleanup API
    
    * fix some of PR comments
    
    * move index patterns into so references
    
    * remove wrong logic
    
    * fix JEST
    
    * fix some ui issues
    
    * update sample data
    
    * indexPatternObject -> indexPatternValue
    
    * fix comments
    
    * I have a dashboard with two TSVB viz. One with the default (haven't applied it to the combobox) and one with the logs. The filter contains fields only from the logs index pattern
    
    * When I am on the string mode and try to write my index, sometimes some of the chars are not added or they are deleted while typing, something with the denounce maybe?
    
    * fix merge conflicts
    
    * Does this PR also supports runtime fields? I created one from the editor and I see that I can select it
    
    * fix UI issue
    
    * If I create a viz with the string mode and a wildcard e.g. kibana_sample*, the index patterns are not communicated correctly to the dashboard.
    
    * fix import/export refs for dashboard
    
    * remove MigrationPopover
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/vis_type_timeseries/server/lib/vis_data/request_processors/series/query.js
+++ src/plugins/vis_type_timeseries/server/lib/vis_data/request_processors/series/query.js
@@ -10,16 +10,16 @@ import { offsetTime } from '../../offset_time';
 import { getIntervalAndTimefield } from '../../get_interval_and_timefield';
 import { esQuery } from '../../../../../../data/server';
 
-export function query(req, panel, series, esQueryConfig, indexPatternObject) {
+export function query(req, panel, series, esQueryConfig, indexPattern) {
   return (next) => (doc) => {
-    const { timeField } = getIntervalAndTimefield(panel, series, indexPatternObject);
+    const { timeField } = getIntervalAndTimefield(panel, series, indexPattern);
     const { from, to } = offsetTime(req, series.offset_time);
 
     doc.size = 0;
     const ignoreGlobalFilter = panel.ignore_global_filter || series.ignore_global_filter;
     const queries = !ignoreGlobalFilter ? req.body.query : [];
     const filters = !ignoreGlobalFilter ? req.body.filters : [];
-    doc.query = esQuery.buildEsQuery(indexPatternObject, queries, filters, esQueryConfig);
+    doc.query = esQuery.buildEsQuery(indexPattern, queries, filters, esQueryConfig);
 
     const timerange = {
       range: {
@@ -34,13 +34,13 @@ export function query(req, panel, series, esQueryConfig, indexPatternObject) {
 
     if (panel.filter) {
       doc.query.bool.must.push(
-        esQuery.buildEsQuery(indexPatternObject, [panel.filter], [], esQueryConfig)
+        esQuery.buildEsQuery(indexPattern, [panel.filter], [], esQueryConfig)
       );
     }
 
     if (series.filter) {
       doc.query.bool.must.push(
-        esQuery.buildEsQuery(indexPatternObject, [series.filter], [], esQueryConfig)
+        esQuery.buildEsQuery(indexPattern, [series.filter], [], esQueryConfig)
       );
     }
 
