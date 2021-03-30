6b30bd8e1a1e38a3dbaefc944eaaf2ef42e50fdb
Nathan L Smith <smith@nlsmith.com>
2021-03-30 12:42:35 -0500

    Add more instances to example

--- x-pack/plugins/apm/public/components/shared/charts/instances_latency_distribution_chart/instances_latency_distribution_chart.stories.tsx
+++ x-pack/plugins/apm/public/components/shared/charts/instances_latency_distribution_chart/instances_latency_distribution_chart.stories.tsx
@@ -43,21 +43,33 @@ Example.args = {
   items: [
     {
       serviceNodeName:
-        '2f3221afa3f00d3bc07069d69efd5bd4c1607be6155a204551c8fe2e2b5dd750',
-      latency: 1130156.5424836602,
-      throughput: 9.71285705065604,
+        '3f67bfc39c7891dc0c5657befb17bf58c19cf10f99472cf8df263c8e5bb1c766',
+      latency: 15802930.92133213,
+      throughput: 0.4019360641691481,
     },
     {
       serviceNodeName:
-        '2f3221afa3f00d3bc07069d69efd5bd4c1607be6155a204551c8fe2e2b5dd750 (2)',
-      latency: 1190156.5424836602,
-      throughput: 9.71285705065604,
+        'd52c64bea9327f3e960ac1cb63c1b7ea922e3cb3d76ab9b254e57a7cb2f760a0',
+      latency: 8296442.578550679,
+      throughput: 0.3932978392703585,
     },
     {
       serviceNodeName:
-        '3b50ad269c45be69088905c4b355cc75ab94aaac1b35432bb752050438f4216f',
-      latency: 10975.761538461538,
-      throughput: 8.252754356766571,
+        '797e0a906ad342223468ca51b663e1af8bdeb40bab376c46c7f7fa2021349290',
+      latency: 34842576.51204916,
+      throughput: 0.3353931699532713,
+    },
+    {
+      serviceNodeName:
+        '21e1c648bd73434a8a1bf6e849817930e8b43eacf73a5c39c30520ee3b79d8c0',
+      latency: 40713854.354498595,
+      throughput: 0.32947224189485164,
+    },
+    {
+      serviceNodeName:
+        'a1c99c8675372af4c74bb01cc48e75989faa6f010a4ccb027df1c410dde0c72c',
+      latency: 18565471.348388012,
+      throughput: 0.3261219384041683,
     },
   ],
 } as InstancesLatencyDistributionChartProps;
