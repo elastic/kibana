88a6e76739ce277ed8e0af74467c41d89d7559f3
spalger <spalger@users.noreply.github.com>
2021-03-30 01:59:21 -0700

    disable cache

--- test/scripts/checks/type_check.sh
+++ test/scripts/checks/type_check.sh
@@ -3,7 +3,7 @@
 source src/dev/ci_setup/setup_env.sh
 
 checks-reporter-with-killswitch "Build TS Refs" \
-  node scripts/build_ts_refs --ignore-type-failures --force
+  node scripts/build_ts_refs --ignore-type-failures --no-cache --force
 
 checks-reporter-with-killswitch "Check Types" \
   node scripts/type_check
