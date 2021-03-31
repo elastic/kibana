af252c37935626dc43b9b1d002310e1121ab2bd0
spalger <spalger@users.noreply.github.com>
2021-03-31 13:09:58 -0700

    also disable docker pull on arm instances

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ "$(which docker)" != "" ]]; then
+if [[ "$(which docker)" != "" && "$(command uname -m)" != "aarch64" ]]; then
   # cache image used by kibana-load-testing project
   docker pull "maven:3.6.3-openjdk-8-slim"
 fi
