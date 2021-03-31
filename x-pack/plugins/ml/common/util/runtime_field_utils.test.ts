08a015a3b2d28efc373e07d43b6199c6a4b038be
Walter Rafelsberger <walter@elastic.co>
2021-03-31 15:47:04 +0200

    [ML] Fix tests.

--- x-pack/plugins/ml/common/util/runtime_field_utils.test.ts
+++ x-pack/plugins/ml/common/util/runtime_field_utils.test.ts
@@ -9,63 +9,94 @@ import { isRuntimeField, isRuntimeMappings } from './runtime_field_utils';
 
 describe('ML runtime field utils', () => {
   describe('isRuntimeField()', () => {
-    expect(isRuntimeField(1)).toBe(false);
-    expect(isRuntimeField(null)).toBe(false);
-    expect(isRuntimeField([])).toBe(false);
-    expect(isRuntimeField({})).toBe(false);
-    expect(isRuntimeField({ someAttribute: 'someValue' })).toBe(false);
-    expect(isRuntimeField({ type: 'wrong-type' })).toBe(false);
-    expect(isRuntimeField({ type: 'keyword', someAttribute: 'some value' })).toBe(false);
-
-    expect(isRuntimeField({ type: 'keyword' })).toBe(true);
-    expect(isRuntimeField({ type: 'keyword', script: 'some script' })).toBe(true);
+    it('does not allow numbers', () => {
+      expect(isRuntimeField(1)).toBe(false);
+    });
+    it('does not allow null', () => {
+      expect(isRuntimeField(null)).toBe(false);
+    });
+    it('does not allow arrays', () => {
+      expect(isRuntimeField([])).toBe(false);
+    });
+    it('does not allow empty objects', () => {
+      expect(isRuntimeField({})).toBe(false);
+    });
+    it('does not allow objects with non-matching attributes', () => {
+      expect(isRuntimeField({ someAttribute: 'someValue' })).toBe(false);
+      expect(isRuntimeField({ type: 'wrong-type' })).toBe(false);
+      expect(isRuntimeField({ type: 'keyword', someAttribute: 'some value' })).toBe(false);
+    });
+    it('allows objects with type attribute only', () => {
+      expect(isRuntimeField({ type: 'keyword' })).toBe(true);
+    });
+    it('allows objects with both type and script attributes', () => {
+      expect(isRuntimeField({ type: 'keyword', script: 'some script' })).toBe(true);
+    });
   });
 
   describe('isRuntimeMappings()', () => {
-    expect(isRuntimeMappings(1)).toBe(false);
-    expect(isRuntimeMappings(null)).toBe(false);
-    expect(isRuntimeMappings([])).toBe(false);
-    expect(isRuntimeMappings({})).toBe(false);
-    expect(isRuntimeMappings({ someAttribute: 'someValue' })).toBe(false);
-    expect(isRuntimeMappings({ fieldName1: { type: 'keyword' }, fieldName2: 'someValue' })).toBe(
-      false
-    );
-    expect(
-      isRuntimeMappings({
-        fieldName1: { type: 'keyword' },
-        fieldName2: { type: 'keyword', someAttribute: 'some value' },
-      })
-    ).toBe(false);
-    expect(
-      isRuntimeMappings({
-        fieldName: { type: 'long', script: 1234 },
-      })
-    ).toBe(false);
-    expect(
-      isRuntimeMappings({
-        fieldName: { type: 'long', script: { someAttribute: 'some value' } },
-      })
-    ).toBe(false);
-    expect(
-      isRuntimeMappings({
-        fieldName: { type: 'long', script: { source: 1234 } },
-      })
-    ).toBe(false);
+    it('does not allow numbers', () => {
+      expect(isRuntimeMappings(1)).toBe(false);
+    });
+    it('does not allow null', () => {
+      expect(isRuntimeMappings(null)).toBe(false);
+    });
+    it('does not allow arrays', () => {
+      expect(isRuntimeMappings([])).toBe(false);
+    });
+    it('does not allow empty objects', () => {
+      expect(isRuntimeMappings({})).toBe(false);
+    });
+    it('does not allow objects with non-object inner structure', () => {
+      expect(isRuntimeMappings({ someAttribute: 'someValue' })).toBe(false);
+    });
+    it('does not allow objects with objects with unsupported inner structure', () => {
+      expect(isRuntimeMappings({ fieldName1: { type: 'keyword' }, fieldName2: 'someValue' })).toBe(
+        false
+      );
+      expect(
+        isRuntimeMappings({
+          fieldName1: { type: 'keyword' },
+          fieldName2: { type: 'keyword', someAttribute: 'some value' },
+        })
+      ).toBe(false);
+      expect(
+        isRuntimeMappings({
+          fieldName: { type: 'long', script: 1234 },
+        })
+      ).toBe(false);
+      expect(
+        isRuntimeMappings({
+          fieldName: { type: 'long', script: { someAttribute: 'some value' } },
+        })
+      ).toBe(false);
+      expect(
+        isRuntimeMappings({
+          fieldName: { type: 'long', script: { source: 1234 } },
+        })
+      ).toBe(false);
+    });
 
-    expect(isRuntimeMappings({ fieldName: { type: 'keyword' } })).toBe(true);
-    expect(
-      isRuntimeMappings({ fieldName1: { type: 'keyword' }, fieldName2: { type: 'keyword' } })
-    ).toBe(true);
-    expect(
-      isRuntimeMappings({
-        fieldName1: { type: 'keyword' },
-        fieldName2: { type: 'keyword', script: 'some script as script' },
-      })
-    ).toBe(true);
-    expect(
-      isRuntimeMappings({
-        fieldName: { type: 'long', script: { source: 'some script as source' } },
-      })
-    ).toBe(true);
+    it('allows object with most basic runtime mapping', () => {
+      expect(isRuntimeMappings({ fieldName: { type: 'keyword' } })).toBe(true);
+    });
+    it('allows object with multiple most basic runtime mappings', () => {
+      expect(
+        isRuntimeMappings({ fieldName1: { type: 'keyword' }, fieldName2: { type: 'keyword' } })
+      ).toBe(true);
+    });
+    it('allows object with runtime mappings including scripts', () => {
+      expect(
+        isRuntimeMappings({
+          fieldName1: { type: 'keyword' },
+          fieldName2: { type: 'keyword', script: 'some script as script' },
+        })
+      ).toBe(true);
+      expect(
+        isRuntimeMappings({
+          fieldName: { type: 'long', script: { source: 'some script as source' } },
+        })
+      ).toBe(true);
+    });
   });
 });
