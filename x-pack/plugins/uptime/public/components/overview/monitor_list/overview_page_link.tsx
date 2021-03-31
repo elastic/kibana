2a4c8a9fdcf4e5114f391baaa9250944b2ca0004
Shahzad <shahzad31comp@gmail.com>
2021-03-31 16:30:26 +0200

    update margin

--- x-pack/plugins/uptime/public/components/overview/monitor_list/overview_page_link.tsx
+++ x-pack/plugins/uptime/public/components/overview/monitor_list/overview_page_link.tsx
@@ -12,7 +12,7 @@ import styled from 'styled-components';
 import { useUrlParams } from '../../../hooks';
 
 const OverviewPageLinkButtonIcon = styled(EuiButtonIcon)`
-  padding-top: 12px;
+  margin-top: 12px;
 `;
 
 interface OverviewPageLinkProps {
