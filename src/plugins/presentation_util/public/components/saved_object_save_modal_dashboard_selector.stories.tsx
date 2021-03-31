db12a6a18fcb501ff6f6e99046240f22d90ad158
Devon Thomson <devon.thomson@hotmail.com>
2021-03-31 09:44:56 -0400

    [Time to Visualize] Allow By Value Flow Without Visualize Save Permissions (#93846)
    * Made sure users can use by value workflow without visualize save permissions

--- src/plugins/presentation_util/public/components/saved_object_save_modal_dashboard_selector.stories.tsx
+++ src/plugins/presentation_util/public/components/saved_object_save_modal_dashboard_selector.stories.tsx
@@ -33,15 +33,21 @@ export default {
       control: 'boolean',
       defaultValue: true,
     },
+    canSaveVisualizations: {
+      control: 'boolean',
+      defaultValue: true,
+    },
   },
 };
 
 export function Example({
   copyOnSave,
   hasDocumentId,
+  canSaveVisualizations,
 }: {
   copyOnSave: boolean;
   hasDocumentId: boolean;
+  canSaveVisualizations: boolean;
 } & StorybookParams) {
   const [dashboardOption, setDashboardOption] = useState<'new' | 'existing' | null>('existing');
   const [isAddToLibrarySelected, setAddToLibrary] = useState(false);
@@ -52,6 +58,7 @@ export function Example({
       onChange={setDashboardOption}
       dashboardOption={dashboardOption}
       copyOnSave={copyOnSave}
+      canSaveByReference={canSaveVisualizations}
       documentId={hasDocumentId ? 'abc' : undefined}
       isAddToLibrarySelected={isAddToLibrarySelected}
       setAddToLibrary={setAddToLibrary}
