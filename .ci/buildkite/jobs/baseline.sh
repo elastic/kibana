b99dae548e8d153cf8bc8ff65c4d5f050c73dd8b
Brian Seeders <brian.seeders@elastic.co>
2021-03-31 17:24:21 -0400

    Enable BUILD_TS_REFS_CACHE_CAPTURE

--- .ci/buildkite/jobs/baseline.sh
+++ .ci/buildkite/jobs/baseline.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # TODO enable
 export BUILD_TS_REFS_CACHE_ENABLE=true
-# export BUILD_TS_REFS_CACHE_CAPTURE=true
+export BUILD_TS_REFS_CACHE_CAPTURE=true
 export DISABLE_BOOTSTRAP_VALIDATION=true
 
 .ci/buildkite/bootstrap.sh
