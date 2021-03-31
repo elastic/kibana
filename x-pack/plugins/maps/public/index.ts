b4dec374e8f54797a4b2440864b0f791673ea076
Thomas Neirynck <thomas@elastic.co>
2021-03-31 11:00:02 -0400

    export from public

--- x-pack/plugins/maps/public/index.ts
+++ x-pack/plugins/maps/public/index.ts
@@ -22,3 +22,5 @@ export { MAP_SAVED_OBJECT_TYPE } from '../common/constants';
 export type { RenderTooltipContentParams } from './classes/tooltips/tooltip_property';
 
 export { MapsStartApi } from './api';
+
+export type { MapEmbeddable, MapEmbeddableInput } from './embeddable';
--- x-pack/plugins/security_solution/public/network/components/embeddables/embedded_map_helpers.tsx
+++ x-pack/plugins/security_solution/public/network/components/embeddables/embedded_map_helpers.tsx
@@ -12,15 +12,11 @@ import minimatch from 'minimatch';
 import { IndexPatternMapping } from './types';
 import { getLayerList } from './map_config';
 import { MAP_SAVED_OBJECT_TYPE } from '../../../../../maps/public';
-import {
-  MapEmbeddable,
-  MapEmbeddableInput,
-  // eslint-disable-next-line @kbn/eslint/no-restricted-paths
-} from '../../../../../../plugins/maps/public/embeddable';
 import type {
   RenderTooltipContentParams,
-  // eslint-disable-next-line @kbn/eslint/no-restricted-paths
-} from '../../../../../../plugins/maps/public/classes/tooltips/tooltip_property';
+  MapEmbeddable,
+  MapEmbeddableInput,
+} from '../../../../../../plugins/maps/public';
 import * as i18n from './translations';
 import { Query, Filter } from '../../../../../../../src/plugins/data/public';
 import {
