f08c30d3ad637b4f86973399cb8c80e1ed4aeb8e
Patryk Kopycinski <contact@patrykkopycinski.com>
2021-04-01 12:36:08 +0200

    fix imports

--- packages/kbn-eslint-plugin-eslint/rules/disallow_license_headers.test.js
+++ packages/kbn-eslint-plugin-eslint/rules/disallow_license_headers.test.js
@@ -11,7 +11,7 @@ const rule = require('./disallow_license_headers');
 const dedent = require('dedent');
 
 const ruleTester = new RuleTester({
-  parser: require.resolve('babel-eslint'),
+  parser: require.resolve('@babel/eslint-parser'),
   parserOptions: {
     ecmaVersion: 2018,
   },
--- packages/kbn-eslint-plugin-eslint/rules/no_restricted_paths.test.js
+++ packages/kbn-eslint-plugin-eslint/rules/no_restricted_paths.test.js
@@ -32,7 +32,7 @@ const { RuleTester } = require('eslint');
 const rule = require('./no_restricted_paths');
 
 const ruleTester = new RuleTester({
-  parser: require.resolve('babel-eslint'),
+  parser: require.resolve('@babel/eslint-parser'),
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2018,
