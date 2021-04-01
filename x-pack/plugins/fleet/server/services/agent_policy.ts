5711a80c6289f7228ca015427a8ae11556e720fc
John Schulz <john.schulz@elastic.co>
2021-04-01 15:03:19 -0400

    Can't select managed agent. Bulk upgrade agent UI changes

--- x-pack/plugins/fleet/server/services/agent_policy.ts
+++ x-pack/plugins/fleet/server/services/agent_policy.ts
@@ -397,7 +397,7 @@ class AgentPolicyService {
     soClient: SavedObjectsClientContract,
     esClient: ElasticsearchClient,
     options?: { user?: AuthenticatedUser }
-  ): Promise<Promise<SavedObjectsBulkUpdateResponse<AgentPolicy>>> {
+  ): Promise<SavedObjectsBulkUpdateResponse<AgentPolicy>> {
     const currentPolicies = await soClient.find<AgentPolicySOAttributes>({
       type: SAVED_OBJECT_TYPE,
       fields: ['revision'],
