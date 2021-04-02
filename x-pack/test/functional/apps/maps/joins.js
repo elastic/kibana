1cea5e6723d43e4a0a0ac61ab2a93e6ec844c571
Thomas Neirynck <thomas@elastic.co>
2021-04-02 14:27:15 -0400

    test fix

--- x-pack/test/functional/apps/maps/joins.js
+++ x-pack/test/functional/apps/maps/joins.js
@@ -121,7 +121,9 @@ export default function ({ getPageObjects, getService }) {
       });
 
       it('should not apply query to source and apply query to join', async () => {
-        const joinResponse = await PageObjects.maps.getResponse('meta_for_geo_shapes*.shape_name');
+        const joinResponse = await PageObjects.maps.getResponse(
+          'meta_for_geo_shapes*.runtime_shape_name'
+        );
         expect(joinResponse.aggregations.join.buckets.length).to.equal(2);
       });
     });
