622ef283f701761d9b5cf8b4ad1c8e77d7ce1246
Brian Seeders <brian.seeders@elastic.co>
2021-04-02 17:35:55 -0400

    Fill the buffer quicker

--- loop.sh
+++ loop.sh
@@ -2,5 +2,7 @@
 
 while true; do
   date
+  cat test/functional/fixtures/es_archiver/dashboard/current/data/mappings.json
+  cat test/functional/fixtures/es_archiver/dashboard/current/data/mappings.json
   sleep 5
 done
