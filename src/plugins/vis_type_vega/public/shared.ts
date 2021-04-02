ebe9a0790059222510b79d1a141838d6fdfbca56
Walter Rafelsberger <walter@elastic.co>
2021-04-02 17:46:56 +0200

    [ML] ts-ignore for exported vega-lite compile.

--- src/plugins/vis_type_vega/public/shared.ts
+++ src/plugins/vis_type_vega/public/shared.ts
@@ -7,6 +7,12 @@
  */
 
 // DRAFT PR bundle size experiment
-export { compile } from 'vega-lite';
+
+// There is still an issue with Vega Lite's typings with the strict mode Kibana is using.
+// Because we're exposing this to be used by other plugins with `strictNullChecks` enabled,
+// we need to ignore this for now. A PR is up in vega-lite to hopefully fix this in future
+// releases: https://github.com/vega/vega-lite/pull/7352
+// @ts-ignore
+import { compile } from 'vega-lite/build/vega-lite';
 export { parse, View, Warn } from 'vega';
 export { Handler } from 'vega-tooltip';
