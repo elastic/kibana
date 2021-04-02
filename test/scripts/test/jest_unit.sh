628fd075d4235d25245336d8d897dbc57aeea8ec
Brian Seeders <brian.seeders@elastic.co>
2021-04-02 17:23:09 -0400

    Double max-old-space-size

--- test/scripts/test/jest_unit.sh
+++ test/scripts/test/jest_unit.sh
@@ -2,5 +2,7 @@
 
 source src/dev/ci_setup/setup_env.sh
 
+export NODE_OPTIONS="$NODE_OPTIONS --max-old-space-size=8192"
+
 checks-reporter-with-killswitch "Jest Unit Tests" \
   node scripts/jest --ci --verbose --runInBand
