41d3fbbcce9848cb179ada4045033e728072d714
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2021-04-05 11:29:19 -0400

    [SECURITY SOLUTIONS][Timeline] correlation bug (#96099) (#96137)
    * fix pagination bug with correlation
    
    * fix close resolver to go back to prev tab
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/common/mock/global_state.ts
+++ x-pack/plugins/security_solution/public/common/mock/global_state.ts
@@ -204,6 +204,7 @@ export const mockGlobalState: State = {
     timelineById: {
       test: {
         activeTab: TimelineTabs.query,
+        prevActiveTab: TimelineTabs.notes,
         deletedEventIds: [],
         id: 'test',
         savedObjectId: null,
