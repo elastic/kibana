47fe8215f6c56cb65707871383d9a14d03ae8689
Nathan Reese <reese.nathan@gmail.com>
2021-04-05 14:01:03 -0600

    range control with ticks

---
+++ x-pack/plugins/maps/public/connected_components/timeslider/get_ticks.ts
@@ -0,0 +1,66 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import moment from 'moment-timezone';
+import { EuiRangeTick } from '@elastic/eui';
+import { getTimeFilter, getUiSettings } from '../../kibana_services';
+
+function getTimezone() {
+  const detectedTimezone = moment.tz.guess();
+  const dateFormatTZ = getUiSettings().get('dateFormat:tz', 'Browser');
+
+  return dateFormatTZ === 'Browser' ? detectedTimezone : dateFormatTZ;
+}
+
+function getScaledDateFormat(interval: number): string {
+  if (interval >= moment.duration(1, 'y')) {
+    return 'YYYY';
+  }
+
+  if (interval >= moment.duration(1, 'd')) {
+    return 'MMM D';
+  }
+
+  if (interval >= moment.duration(12, 'h')) {
+    return 'MMM D HH';
+  }
+
+  if (interval >= moment.duration(3, 'h')) {
+    return 'D HH';
+  }
+
+  if (interval >= moment.duration(1, 'h')) {
+    return 'HH';
+  }
+
+  if (interval >= moment.duration(1, 'm')) {
+    return 'HH:mm';
+  }
+
+  if (interval >= moment.duration(1, 's')) {
+    return 'HH:mm:ss';
+  }
+
+  return 'HH:mm:ss.SSS';
+}
+
+export function getTicks(min: number, max: number, interval: number): EuiRangeTick[] {
+  const format = getScaledDateFormat(interval);
+  const timezone = getTimezone();
+
+  let tick = Math.ceil(min / interval) * interval;
+  const ticks: EuiRangeTick[] = [];
+  while (tick < max) {
+    ticks.push({
+      value: tick,
+      label: moment.tz(tick, timezone).format(format),
+    });
+    tick += interval;
+  }
+
+  return ticks;
+}
--- x-pack/plugins/maps/public/connected_components/timeslider/timeslider.tsx
+++ x-pack/plugins/maps/public/connected_components/timeslider/timeslider.tsx
@@ -6,27 +6,27 @@
  */
 
 import React, { ChangeEvent, Component } from 'react';
-import {
-  EuiForm,
-  EuiFormRow,
-  EuiButton,
-  EuiFieldNumber,
-  EuiButtonIcon,
-  EuiPopover,
-  EuiTextAlign,
-  EuiSpacer,
-} from '@elastic/eui';
+import { EuiDualRange, EuiRangeTick } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n/react';
-import { MapCenter } from '../../../../common/descriptor_types';
-import { MapSettings } from '../../../reducers/map';
+import { getTicks } from './get_ticks';
+import { calcAutoIntervalNear, TimeRange } from '../../../../../../src/plugins/data/common';
+import { getTimeFilter } from '../../kibana_services';
+
+const NUM_TICKS = 6;
+const MAX_TICKS = 8;
 
 export interface Props {
   isTimesliderOpen: boolean;
   timeRange: TimeRange;
 }
 
-interface State {}
+interface State {
+  interval: number;
+  max: number;
+  min: number;
+  timeslice: number[];
+  ticks: EuiRangeTick[];
+}
 
 // Why Timeslider and KeyedTimeslider?
 // Using react 'key' property to ensure new KeyedTimeslider instance whenever props.timeRange changes
@@ -37,9 +37,50 @@ export function Timeslider(props: props) {
 }
 
 class KeyedTimeslider extends Component<Props, State> {
-  state: State = {};
+  constructor(props: Props) {
+    super(props);
+    const timeRangeBounds = getTimeFilter().calculateBounds(props.timeRange);
+    const duration = timeRangeBounds.max - timeRangeBounds.min;
+    let interval = calcAutoIntervalNear(NUM_TICKS, duration);
+    // Sometimes auto interval is not quite right and returns 2X or 3X requested ticks
+    // Adjust the interval to get the requested number of ticks
+    const actualTicks = duration / interval;
+    if (actualTicks > MAX_TICKS) {
+      const factor = Math.ceil(actualTicks / NUM_TICKS);
+      interval *= factor;
+    }
+    const min = timeRangeBounds.min.valueOf();
+    const max = timeRangeBounds.max.valueOf();
+
+    this.state = {
+      interval,
+      max,
+      min,
+      ticks: getTicks(min, max, interval),
+      timeslice: [min, max],
+    };
+  }
+
+  _onChange = (value: number[]) => {
+    this.setState({ timeslice: value });
+  };
 
   render() {
-    return <div className="mapTimeslider" />;
+    return (
+      <div className="mapTimeslider">
+        <div className="mapTimeslider__row">
+          <EuiDualRange
+            fullWidth={true}
+            value={this.state.timeslice}
+            onChange={this._onChange}
+            showTicks={true}
+            min={this.state.min}
+            max={this.state.max}
+            step={1}
+            ticks={this.state.ticks}
+          />
+        </div>
+      </div>
+    );
   }
 }
