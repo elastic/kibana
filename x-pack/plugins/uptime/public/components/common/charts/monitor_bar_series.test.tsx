a26c99bde5d53b0287e87810416c0a816f112c52
Mikhail Shustov <restrry@gmail.com>
2021-04-05 20:59:26 +0200

    Revert "TS Incremental build exclude test files (#95610)" (#96223)
    This reverts commit b6e582c53ebb9c496c232408066b128d2ca2f92c.

--- x-pack/plugins/uptime/public/components/common/charts/monitor_bar_series.test.tsx
+++ x-pack/plugins/uptime/public/components/common/charts/monitor_bar_series.test.tsx
@@ -7,8 +7,7 @@
 
 import React from 'react';
 import { MonitorBarSeries, MonitorBarSeriesProps } from './monitor_bar_series';
-import { renderWithRouter, shallowWithRouter } from '../../../lib/helper/enzyme_helpers';
-import { MountWithReduxProvider } from '../../../lib/helper/helper_with_redux';
+import { renderWithRouter, shallowWithRouter, MountWithReduxProvider } from '../../../lib';
 import { HistogramPoint } from '../../../../common/runtime_types';
 
 describe('MonitorBarSeries component', () => {
