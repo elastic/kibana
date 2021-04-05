04ebd254bd297c7f55b33bea1f4fc94e60d1f667
Mike Cote <mikecote@users.noreply.github.com>
2021-04-05 14:36:09 -0400

    Fix import errors?

--- x-pack/test/alerting_api_integration/common/fixtures/plugins/alerts/server/index.ts
+++ x-pack/test/alerting_api_integration/common/fixtures/plugins/alerts/server/index.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
+import { PluginInitializerContext } from 'kibana/server';
 import { FixturePlugin } from './plugin';
-import { PluginInitializerContext } from '../../../../../../../../src/core/server';
 
 export const plugin = (initContext: PluginInitializerContext) => new FixturePlugin(initContext);
