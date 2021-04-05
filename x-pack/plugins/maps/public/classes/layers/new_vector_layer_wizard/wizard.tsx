001c87f7c28285a15dbc3babcb3521966dcd4ea2
Aaron Caldwell <aaron.caldwell@elastic.co>
2021-04-05 09:31:57 -0600

    Add index checking utils

--- x-pack/plugins/maps/public/classes/layers/new_vector_layer_wizard/wizard.tsx
+++ x-pack/plugins/maps/public/classes/layers/new_vector_layer_wizard/wizard.tsx
@@ -5,27 +5,174 @@
  * 2.0.
  */
 
-import React, { Component } from 'react';
-import { EuiPanel } from '@elastic/eui';
+import React, { ChangeEvent, Component } from 'react';
+import { EuiCallOut, EuiFieldText, EuiFormRow, EuiPanel, EuiSpacer } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import {
+  getExistingIndexNames,
+  getExistingIndexPatternNames,
+  checkIndexPatternValid,
+  // @ts-expect-error
+} from './utils/indexing_service';
 
 interface Props {
   setEditModeActive: () => void;
   setEditModeInActive: () => void;
 }
 
-export class NewVectorLayerEditor extends Component<Props> {
-  componentDidMount() {
+interface State {
+  indexName: string;
+  indexValid: boolean;
+  indexError: string;
+  indexNames: string[];
+}
+
+export class NewVectorLayerEditor extends Component<Props, State> {
+  state = {
+    indexName: '',
+    indexValid: false,
+    indexError: '',
+    indexNames: [],
+  };
+
+  private _isMounted = false;
+
+  async componentDidMount() {
     this.props.setEditModeActive();
+    this._isMounted = true;
+    this._loadIndexNames();
   }
 
   componentWillUnmount() {
     this.props.setEditModeInActive();
+    this._isMounted = false;
   }
+
+  _loadIndexNames = async () => {
+    const indexNameList = await getExistingIndexNames();
+    const indexPatternList = await getExistingIndexPatternNames();
+    if (this._isMounted) {
+      this.setState({
+        indexNames: [...indexNameList, ...indexPatternList],
+      });
+    }
+  };
+
+  _onIndexNameChange = (indexName: string) => {
+    let error: string | undefined;
+    if (this.state.indexNames.includes(indexName)) {
+      error = i18n.translate(
+        'xpack.layers.newVectorLayerWizard.indexSettings.indexNameAlreadyExistsErrorMessage',
+        {
+          defaultMessage: 'Index name already exists.',
+        }
+      );
+    } else if (!checkIndexPatternValid(indexName)) {
+      error = i18n.translate(
+        'xpack.layers.newVectorLayerWizard.indexSettings.indexNameContainsIllegalCharactersErrorMessage',
+        {
+          defaultMessage: 'Index name contains illegal characters.',
+        }
+      );
+    }
+  };
+
+  _onIndexNameChangeEvent = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ indexName: event.target.value });
+    this._onIndexNameChange(event.target.value);
+  };
 
   render() {
     return (
       <EuiPanel>
-        <div>Draw shapes</div>
+        <>
+          <EuiFormRow
+            label={i18n.translate(
+              'xpack.layers.newVectorLayerWizard.indexSettings.enterIndexNameLabel',
+              {
+                defaultMessage: 'Index name',
+              }
+            )}
+            isInvalid={this.state.indexValid}
+            error={this.state.indexValid ? [this.state.indexError] : []}
+          >
+            <EuiFieldText
+              data-test-subj="fileUploadIndexNameInput"
+              value={this.state.indexName}
+              onChange={this._onIndexNameChangeEvent}
+              isInvalid={this.state.indexValid}
+              aria-label={i18n.translate('xpack.layers.newVectorLayerWizard.indexNameReqField', {
+                defaultMessage: 'Index name, required field',
+              })}
+            />
+          </EuiFormRow>
+          <EuiSpacer size="m" />
+          <EuiCallOut
+            title={i18n.translate(
+              'xpack.layers.newVectorLayerWizard.indexSettings.indexNameGuidelines',
+              {
+                defaultMessage: 'Index name guidelines',
+              }
+            )}
+            size="s"
+          >
+            <ul style={{ marginBottom: 0 }}>
+              <li>
+                {i18n.translate(
+                  'xpack.layers.newVectorLayerWizard.indexSettings.guidelines.mustBeNewIndex',
+                  {
+                    defaultMessage: 'Must be a new index',
+                  }
+                )}
+              </li>
+              <li>
+                {i18n.translate(
+                  'xpack.layers.newVectorLayerWizard.indexSettings.guidelines.lowercaseOnly',
+                  {
+                    defaultMessage: 'Lowercase only',
+                  }
+                )}
+              </li>
+              <li>
+                {i18n.translate(
+                  'xpack.layers.newVectorLayerWizard.indexSettings.guidelines.cannotInclude',
+                  {
+                    defaultMessage:
+                      'Cannot include \\\\, /, *, ?, ", <, >, |, \
+                    " " (space character), , (comma), #',
+                  }
+                )}
+              </li>
+              <li>
+                {i18n.translate(
+                  'xpack.layers.newVectorLayerWizard.indexSettings.guidelines.cannotStartWith',
+                  {
+                    defaultMessage: 'Cannot start with -, _, +',
+                  }
+                )}
+              </li>
+              <li>
+                {i18n.translate(
+                  'xpack.layers.newVectorLayerWizard.indexSettings.guidelines.cannotBe',
+                  {
+                    defaultMessage: 'Cannot be . or ..',
+                  }
+                )}
+              </li>
+              <li>
+                {i18n.translate(
+                  'xpack.layers.newVectorLayerWizard.indexSettings.guidelines.length',
+                  {
+                    defaultMessage:
+                      'Cannot be longer than 255 bytes (note it is bytes, \
+                    so multi-byte characters will count towards the 255 \
+                    limit faster)',
+                  }
+                )}
+              </li>
+            </ul>
+          </EuiCallOut>
+        </>
       </EuiPanel>
     );
   }
