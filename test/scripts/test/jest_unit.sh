f96856e3344145c136f7fdfe3a7fd23e68410517
Tyler Smalley <tyler.smalley@elastic.co>
2021-04-06 09:47:15 -0700

    [CI] Run Jest serially
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- test/scripts/test/jest_unit.sh
+++ test/scripts/test/jest_unit.sh
@@ -3,4 +3,4 @@
 source src/dev/ci_setup/setup_env.sh
 
 checks-reporter-with-killswitch "Jest Unit Tests" \
-  node scripts/jest --ci --verbose --maxWorkers=6
+  node --max-old-space-size=8192 --expose-gc ./node_modules/.bin/jest --ci --logHeapUsage --runInBand --config jest.config.js
