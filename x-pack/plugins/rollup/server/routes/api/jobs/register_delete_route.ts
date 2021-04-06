36309d5ae2f90a92cecdf332dec4f703b84d8955
Jean-Louis Leysens <jloleysens@gmail.com>
2021-04-06 14:40:57 +0200

    remove unused import

--- x-pack/plugins/rollup/server/routes/api/jobs/register_delete_route.ts
+++ x-pack/plugins/rollup/server/routes/api/jobs/register_delete_route.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { errors } from '@elastic/elasticsearch';
+
 import { schema } from '@kbn/config-schema';
 import { addBasePath } from '../../../services';
 import { RouteDependencies } from '../../../types';
