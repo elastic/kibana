d270834d07063d6ca7744d697100a6dac607d7c0
Dmitriynj <diaamnj@mail.ru>
2021-04-06 14:36:26 +0300

    [TSVB] fix not populating fields list if "Auto apply" is off

--- src/plugins/vis_type_timeseries/public/application/components/vis_editor.tsx
+++ src/plugins/vis_type_timeseries/public/application/components/vis_editor.tsx
@@ -93,19 +93,6 @@ export class VisEditor extends Component<TimeseriesEditorProps, TimeseriesEditor
     this.props.eventEmitter.emit('dirtyStateChange', {
       isDirty: false,
     });
-
-    const extractedIndexPatterns = extractIndexPatternValues(
-      this.state.model,
-      this.state.model.default_index_pattern
-    );
-    if (!isEqual(this.state.extractedIndexPatterns, extractedIndexPatterns)) {
-      this.abortableFetchFields(extractedIndexPatterns).then((visFields) => {
-        this.setState({
-          visFields,
-          extractedIndexPatterns,
-        });
-      });
-    }
   }, VIS_STATE_DEBOUNCE_DELAY);
 
   abortableFetchFields = (extractedIndexPatterns: IndexPatternValue[]) => {
@@ -120,15 +107,28 @@ export class VisEditor extends Component<TimeseriesEditorProps, TimeseriesEditor
       return;
     }
     const hasTypeChanged = partialModel.type && this.state.model.type !== partialModel.type;
-    const nextModel = {
-      ...this.state.model,
-      ...partialModel,
-    };
     let dirty = true;
     if (this.state.autoApply || hasTypeChanged) {
       this.updateVisState();
 
       dirty = false;
+    }
+
+    const nextModel = {
+      ...this.state.model,
+      ...partialModel,
+    };
+    const extractedIndexPatterns = extractIndexPatternValues(
+      nextModel,
+      this.state.model.default_index_pattern
+    );
+    if (!isEqual(this.state.extractedIndexPatterns, extractedIndexPatterns)) {
+      this.abortableFetchFields(extractedIndexPatterns).then((visFields) => {
+        this.setState({
+          visFields,
+          extractedIndexPatterns,
+        });
+      });
     }
 
     this.setState({
