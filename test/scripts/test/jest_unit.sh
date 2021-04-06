a3a0ac3295b8af74527229bba23992786aa63fa3
Brian Seeders <brian.seeders@elastic.co>
2021-04-06 10:52:02 -0400

    Try running yarn jest directly

--- test/scripts/test/jest_unit.sh
+++ test/scripts/test/jest_unit.sh
@@ -4,5 +4,7 @@ source src/dev/ci_setup/setup_env.sh
 
 export NODE_OPTIONS="$NODE_OPTIONS --max-old-space-size=8192"
 
-checks-reporter-with-killswitch "Jest Unit Tests" \
-  node --preserve-symlinks --preserve-symlinks-main scripts/jest --ci --verbose --runInBand
+# checks-reporter-with-killswitch "Jest Unit Tests" \
+#   node --preserve-symlinks --preserve-symlinks-main scripts/jest --ci --verbose --runInBand
+
+yarn jest --preserve-symlinks --preserve-symlinks-main --ci --verbose --runInBand --config jest.config.js
