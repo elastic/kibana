c40c7b6f7b722ab15610c24ecae7ba7f11394250
Greg Thompson <thompson.glowe@gmail.com>
2021-04-06 16:45:46 -0500

    type updates

--- x-pack/plugins/spaces/public/space_avatar/space_avatar_internal.tsx
+++ x-pack/plugins/spaces/public/space_avatar/space_avatar_internal.tsx
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import type { PropsOf } from '@elastic/eui';
 import { EuiAvatar, isValidHex } from '@elastic/eui';
 import type { FC } from 'react';
 import React from 'react';
@@ -28,6 +29,15 @@ export const SpaceAvatarInternal: FC<Props> = (props: Props) => {
 
   const spaceColor = getSpaceColor(space);
 
+  const spaceInitials = getSpaceInitials(space);
+
+  const spaceImageUrl = getSpaceImageUrl(space);
+
+  // TODO: Replace `PropsOf<typeof EuiAvatar>` with `EuiAvatarProps` when available
+  const avatarConfig: Partial<PropsOf<typeof EuiAvatar>> = spaceImageUrl
+    ? { imageUrl: spaceImageUrl }
+    : { initials: spaceInitials, initialsLength: MAX_SPACE_INITIALS };
+
   return (
     <EuiAvatar
       type="space"
@@ -39,10 +49,8 @@ export const SpaceAvatarInternal: FC<Props> = (props: Props) => {
         'aria-hidden': true,
       })}
       size={size || 'm'}
-      initialsLength={MAX_SPACE_INITIALS}
-      initials={getSpaceInitials(space)}
       color={isValidHex(spaceColor) ? spaceColor : ''}
-      imageUrl={getSpaceImageUrl(space)}
+      {...avatarConfig}
       {...rest}
     />
   );
