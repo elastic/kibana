dfeaf5fc681801ecb7df69175f5fc63c45b191d9
Catherine Liu <catherine.liu@elastic.co>
2021-04-07 10:06:26 -0700

    Fixed types

--- src/plugins/presentation_util/public/components/solution_toolbar/index.ts
+++ src/plugins/presentation_util/public/components/solution_toolbar/index.ts
@@ -5,6 +5,6 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import React from 'react';
 
+export { SolutionToolbar } from './solution_toolbar';
 export * from './items';
