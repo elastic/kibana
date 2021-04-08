4f43921e59eb4c6b894dce3abddd9cacfe948f4a
Dario Gieselaar <dario.gieselaar@elastic.co>
2021-04-08 15:00:04 +0200

    Export APMRuleRegistry type again

--- x-pack/plugins/apm/server/plugin.ts
+++ x-pack/plugins/apm/server/plugin.ts
@@ -43,6 +43,8 @@ import {
 import { registerRoutes } from './routes/register_routes';
 import { getGlobalApmServerRouteRepository } from './routes/get_global_apm_server_route_repository';
 
+export type APMRuleRegistry = ReturnType<APMPlugin['setup']>['ruleRegistry'];
+
 export class APMPlugin
   implements
     Plugin<
