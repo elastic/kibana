6fe593fe4381fe14a38b44b57ac0cbbde15ba5d0
Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>
2021-04-08 15:44:51 +0300

    Remove the logic to overwrite the default value because it is not being used

--- src/plugins/vis_type_timeseries/common/get_last_value.js
+++ src/plugins/vis_type_timeseries/common/get_last_value.js
@@ -12,10 +12,10 @@ export const DEFAULT_VALUE = '-';
 
 const extractValue = (data) => (data && data[1]) ?? null;
 
-export const getLastValue = (data, defaultValue = DEFAULT_VALUE) => {
+export const getLastValue = (data) => {
   if (!isArray(data)) {
-    return data ?? defaultValue;
+    return data ?? DEFAULT_VALUE;
   }
 
-  return extractValue(last(data)) ?? defaultValue;
+  return extractValue(last(data)) ?? DEFAULT_VALUE;
 };
