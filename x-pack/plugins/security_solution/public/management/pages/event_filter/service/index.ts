df077e3bbbfc2933b774e86994366ad952b94a60
David Sanchez Soler <davidsansol92@gmail.com>
2021-04-08 17:07:18 +0200

    Creates even filter list on service constructor

--- x-pack/plugins/security_solution/public/management/pages/event_filter/service/index.ts
+++ x-pack/plugins/security_solution/public/management/pages/event_filter/service/index.ts
@@ -11,19 +11,33 @@ import {
   CreateExceptionListItemSchema,
 } from '../../../../../public/shared_imports';
 import { Immutable } from '../../../../../common/endpoint/types';
+// import { EXCEPTION_LIST_ITEM_URL } from '../../../../../../../plugins/lists/common/constants';
 
-// TODO: pending to add Types
 export interface EventFiltersService {
   addEventFilter(
     exception: Immutable<ExceptionListItemSchema | CreateExceptionListItemSchema>
   ): Promise<ExceptionListItemSchema>;
 }
 
+// TODO: to be moved with right fields.
+const EVENT_FILTER_LIST = {
+  name: 'Endpoint Event Filter List',
+  namespace_type: 'agnostic',
+  description: 'Endpoint Event Filter List',
+  list_id: 'endpointEventFilterList',
+  type: 'endpoint_event_filter',
+};
+
 export class EventFiltersHttpService implements EventFiltersService {
-  constructor(private http: HttpStart) {}
+  constructor(private http: HttpStart) {
+    http.post<ExceptionListItemSchema>('/api/exception_lists', {
+      body: JSON.stringify(EVENT_FILTER_LIST),
+    });
+  }
 
   async addEventFilter(exception: ExceptionListItemSchema | CreateExceptionListItemSchema) {
-    return this.http.post<ExceptionListItemSchema>('apiCall', {
+    // TODO: change this url path string by a constant
+    return this.http.post<ExceptionListItemSchema>('/api/exception_lists/items', {
       body: JSON.stringify(exception),
     });
   }
