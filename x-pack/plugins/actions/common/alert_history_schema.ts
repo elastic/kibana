38e7da3aed075585061aae7aba2eaad9f2ae1c55
ymao1 <ying.mao@elastic.co>
2021-04-08 18:18:44 -0400

    [Alerting] Preconfigured alert history index connector (#94909)
    * Adding preconfigured alert history index
    
    * Adding functions to build alert history document
    
    * Adding functions to build alert history document
    
    * Moving index template creation to plugin start
    
    * Adding unit tests
    
    * Adding unit tests
    
    * Adding unit tests
    
    * Simplifying
    
    * Revert "Merge branch 'master' of https://github.com/elastic/kibana into alerting/default-es-index-schema"
    
    This reverts commit 957c333aa474e4a65e47a3119e4d60a7f19215fe, reversing
    changes made to 4b1b78761ea6d26bede52d0785419cfc0ae3d5a0.
    
    * Reverting some changes
    
    * Reverting some changes
    
    * Adding index override
    
    * Updating UI with index override
    
    * Only allow indexOverride for preconfigured alert history connector
    
    * Handling preconfigured connector id clashes
    
    * Cleanup
    
    * UI unit tests
    
    * Fixing default schema shown in UI
    
    * Fixing functional tests
    
    * Adding functional test
    
    * Fixing functional tests
    
    * Adding docs and link to docs
    
    * Adding config to docker allowlist
    
    * Fixing wrong typescript operator
    
    * Changing default for config to false
    
    * Cleanup
    
    * Adding note about index privileges to docs
    
    * Fixing i18n
    
    * PR fixes
    
    * PR fixes
    
    * PR fixes
    
    * PR fixes - wording
    
    * PR fixes
    
    * Fixing unit and functional tests
    
    * Fixing types check
    
    * ES -> Elasticsearch
    
    * Moving files
    
    * Adding kibana- to beginning of prefix
    
    * Namespacing alert data within schema with kibana
    
    * Fix i18n
    
    * Updating docs
    
    * Fixing unit tests
    
    * Fixing doc links
    
    * Fixing types check
    
    * PR fixes
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/actions/common/alert_history_schema.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { isEmpty } from 'lodash';
+
+export const ALERT_HISTORY_PREFIX = 'kibana-alert-history-';
+export const AlertHistoryDefaultIndexName = `${ALERT_HISTORY_PREFIX}default`;
+export const AlertHistoryEsIndexConnectorId = 'preconfigured-alert-history-es-index';
+
+export const buildAlertHistoryDocument = (variables: Record<string, unknown>) => {
+  const { date, alert: alertVariables, context, params, tags, rule: ruleVariables } = variables as {
+    date: string;
+    alert: Record<string, unknown>;
+    context: Record<string, unknown>;
+    params: Record<string, unknown>;
+    rule: Record<string, unknown>;
+    tags: string[];
+  };
+
+  if (!alertVariables || !ruleVariables) {
+    return null;
+  }
+
+  const { actionGroup, actionGroupName, id: alertId } = alertVariables as {
+    actionGroup: string;
+    actionGroupName: string;
+    id: string;
+  };
+
+  const { id: ruleId, name, spaceId, type } = ruleVariables as {
+    id: string;
+    name: string;
+    spaceId: string;
+    type: string;
+  };
+
+  if (!type) {
+    // can't build the document without a type
+    return null;
+  }
+
+  const ruleType = type.replace(/\./g, '__');
+
+  const rule = {
+    ...(ruleId ? { id: ruleId } : {}),
+    ...(name ? { name } : {}),
+    ...(!isEmpty(params) ? { params: { [ruleType]: params } } : {}),
+    ...(spaceId ? { space: spaceId } : {}),
+    ...(type ? { type } : {}),
+  };
+  const alert = {
+    ...(alertId ? { id: alertId } : {}),
+    ...(!isEmpty(context) ? { context: { [ruleType]: context } } : {}),
+    ...(actionGroup ? { actionGroup } : {}),
+    ...(actionGroupName ? { actionGroupName } : {}),
+  };
+
+  const alertHistoryDoc = {
+    '@timestamp': date ? date : new Date().toISOString(),
+    ...(tags && tags.length > 0 ? { tags } : {}),
+    ...(context?.message ? { message: context.message } : {}),
+    ...(!isEmpty(rule) ? { rule } : {}),
+    ...(!isEmpty(alert) ? { kibana: { alert } } : {}),
+  };
+
+  return !isEmpty(alertHistoryDoc) ? { ...alertHistoryDoc, event: { kind: 'alert' } } : null;
+};
+
+export const AlertHistoryDocumentTemplate = Object.freeze(
+  buildAlertHistoryDocument({
+    rule: {
+      id: '{{rule.id}}',
+      name: '{{rule.name}}',
+      type: '{{rule.type}}',
+      spaceId: '{{rule.spaceId}}',
+    },
+    context: '{{context}}',
+    params: '{{params}}',
+    tags: '{{rule.tags}}',
+    alert: {
+      id: '{{alert.id}}',
+      actionGroup: '{{alert.actionGroup}}',
+      actionGroupName: '{{alert.actionGroupName}}',
+    },
+  })
+);
