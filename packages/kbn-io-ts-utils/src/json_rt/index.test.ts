adbf9dbc946c3f0cb79d1271de66fccffdad0341
Dario Gieselaar <dario.gieselaar@elastic.co>
2021-04-08 13:26:43 +0200

    [APM] Extract server type utils to package (#96349)

--- packages/kbn-io-ts-utils/src/json_rt/index.test.ts
+++ packages/kbn-io-ts-utils/src/json_rt/index.test.ts
@@ -1,8 +1,9 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import * as t from 'io-ts';
@@ -12,9 +13,7 @@ import { Right } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { identity } from 'fp-ts/lib/function';
 
-function getValueOrThrow<TEither extends Either<any, any>>(
-  either: TEither
-): Right<TEither> {
+function getValueOrThrow<TEither extends Either<any, any>>(either: TEither): Right<TEither> {
   const value = pipe(
     either,
     fold(() => {
