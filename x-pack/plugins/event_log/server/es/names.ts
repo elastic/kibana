74d3eddcb04746bc3bcb92096c03f6f50cfb38a8
Dario Gieselaar <dario.gieselaar@elastic.co>
2021-04-08 12:59:36 +0200

    Revert change to getEsNames

--- x-pack/plugins/event_log/server/es/names.ts
+++ x-pack/plugins/event_log/server/es/names.ts
@@ -17,11 +17,7 @@ export interface EsNames {
   indexTemplate: string;
 }
 
-export function getEsNames(
-  baseName: string,
-  kibanaVersion: string,
-  suffix: string = EVENT_LOG_NAME_SUFFIX
-): EsNames {
+export function getEsNames(baseName: string, kibanaVersion: string): EsNames {
   const EVENT_LOG_VERSION_SUFFIX = `-${kibanaVersion.toLocaleLowerCase()}`;
   const eventLogName = `${baseName}${EVENT_LOG_NAME_SUFFIX}`;
   const eventLogNameWithVersion = `${eventLogName}${EVENT_LOG_VERSION_SUFFIX}`;
