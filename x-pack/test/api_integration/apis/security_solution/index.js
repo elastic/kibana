845baa7978c7fba93a4410b1d0efddc4f3363e14
Angela Chuang <yi-chun.chuang@elastic.co>
2021-04-09 08:13:06 +0100

    fix tests

--- x-pack/test/api_integration/apis/security_solution/index.js
+++ x-pack/test/api_integration/apis/security_solution/index.js
@@ -26,6 +26,5 @@ export default function ({ loadTestFile }) {
     loadTestFile(require.resolve('./uncommon_processes'));
     loadTestFile(require.resolve('./users'));
     loadTestFile(require.resolve('./tls'));
-    loadTestFile(require.resolve('./feature_controls'));
   });
 }
