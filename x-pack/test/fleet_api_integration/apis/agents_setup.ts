6454c3301491a8a5e587c277ca36f94785599509
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2021-04-12 13:04:22 -0400

    [Fleet] Fix Fleet API integration tests

--- x-pack/test/fleet_api_integration/apis/agents_setup.ts
+++ x-pack/test/fleet_api_integration/apis/agents_setup.ts
@@ -15,8 +15,7 @@ export default function (providerContext: FtrProviderContext) {
   const es = getService('es');
   const esArchiver = getService('esArchiver');
 
-  // FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/96515
-  describe.skip('fleet_agents_setup', () => {
+  describe('fleet_agents_setup', () => {
     skipIfNoDockerRegistry(providerContext);
     before(async () => {
       await esArchiver.load('empty_kibana');
@@ -25,7 +24,7 @@ export default function (providerContext: FtrProviderContext) {
 
     after(async () => {
       await esArchiver.unload('empty_kibana');
-      await esArchiver.load('fleet/empty_fleet_server');
+      await esArchiver.unload('fleet/empty_fleet_server');
     });
 
     beforeEach(async () => {
