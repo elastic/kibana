c22420a9f9fe172a726317e170eb357dae074f89
Jonathan Budzenski <jon@budzenski.me>
2021-04-12 14:17:04 -0500

    fix include-tag arg

--- test/scripts/jenkins_xpack_package_docker.sh
+++ test/scripts/jenkins_xpack_package_docker.sh
@@ -20,4 +20,4 @@ export TEST_BROWSER_HEADLESS=1
 export TEST_KIBANA_URL=http://elastic:changeme@192.168.50.7:5601
 export TEST_ES_URL=http://elastic:changeme@192.168.50.1:9200
 
-node scripts/functional_test_runner.js --include-tags=smoke
+node scripts/functional_test_runner.js --include-tag=smoke
