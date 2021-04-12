92bc1542c6f4d4f1e1adb3e828a747a6a6ac7d2c
streamich <streamich@gmail.com>
2021-04-12 15:39:46 +0200

    test: 💍 remove implementation from todo placeholders

--- x-pack/plugins/ui_actions_enhanced/public/drilldowns/drilldown_manager/state/drilldown_manager_state.test.tsx
+++ x-pack/plugins/ui_actions_enhanced/public/drilldowns/drilldown_manager/state/drilldown_manager_state.test.tsx
@@ -5,20 +5,20 @@
  * 2.0.
  */
 
-test.todo('Allows to manage drilldowns', async () => {});
+test.todo('Allows to manage drilldowns');
 
-test.todo('Can delete multiple drilldowns', async () => {});
+test.todo('Can delete multiple drilldowns');
 
-test.todo('Create only mode', async () => {});
+test.todo('Create only mode');
 
-test.todo('After switching between action factories state is restored', async () => {});
+test.todo('After switching between action factories state is restored');
 
 test.todo("Error when can't fetch drilldown list");
 
-test.todo("Error when can't save drilldown changes", async () => {});
+test.todo("Error when can't save drilldown changes");
 
-test.todo('Should show drilldown welcome message. Should be able to dismiss it', async () => {});
+test.todo('Should show drilldown welcome message. Should be able to dismiss it');
 
-test.todo('Drilldown type is not shown if no supported trigger', async () => {});
+test.todo('Drilldown type is not shown if no supported trigger');
 
-test.todo('Can pick a trigger', async () => {});
+test.todo('Can pick a trigger');
