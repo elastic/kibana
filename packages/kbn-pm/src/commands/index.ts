91cfff87a45d3766503d144f1eebe8547b5568ce
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-04-12 22:30:36 -0400

    chore(NA): @kbn/pm new commands to support development on Bazel packages (#96465) (#96911)
    * chore(NA): add warnings both to run and watch commands about Bazel built packages
    
    * chore(NA): add new commands to build and watch bazel packages
    
    * docs(NA): add documentation about how to deal with bazel packages
    
    * chore(NA): addressed majority of the feedback received except for improved error logging
    
    * chore(NA): disable ibazel info notification.
    
    * chore(NA): remove iBazel notification
    
    * chore(NA): remove iBazel notification - kbn pm dist
    
    * chore(NA): move show_results option to kbn-pm only
    
    * chore(NA): patch build bazel command to include packages target list
    
    * chore(NA): add pretty logging for elastic-datemath
    
    * chore(NA): remove double error output from commands ran with Bazel
    
    * fix(NA): include simple error message to preserve subprocess failure state
    
    * docs(NA): missing docs about how to independentely watch non bazel packages
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Tiago Costa <tiagoffcc@hotmail.com>

--- packages/kbn-pm/src/commands/index.ts
+++ packages/kbn-pm/src/commands/index.ts
@@ -27,16 +27,20 @@ export interface ICommand {
 }
 
 import { BootstrapCommand } from './bootstrap';
+import { BuildBazelCommand } from './build_bazel';
 import { CleanCommand } from './clean';
 import { ResetCommand } from './reset';
 import { RunCommand } from './run';
 import { WatchCommand } from './watch';
+import { WatchBazelCommand } from './watch_bazel';
 import { Kibana } from '../utils/kibana';
 
 export const commands: { [key: string]: ICommand } = {
   bootstrap: BootstrapCommand,
+  'build-bazel': BuildBazelCommand,
   clean: CleanCommand,
   reset: ResetCommand,
   run: RunCommand,
   watch: WatchCommand,
+  'watch-bazel': WatchBazelCommand,
 };
