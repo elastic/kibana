b9a48aca522446233c937aedd4e7ac9e92d99a52
Jonathan Budzenski <jon@budzenski.me>
2021-04-13 15:21:30 -0500

    assert kibana service is available

--- test/scripts/jenkins_xpack_package_docker.sh
+++ test/scripts/jenkins_xpack_package_docker.sh
@@ -6,16 +6,16 @@ source src/dev/ci_setup/setup_env.sh
 
 gsutil -q -m cp "gs://ci-artifacts.kibana.dev/package-testing/$GIT_COMMIT/kibana-*-docker-image.tar.gz" ./target
 
-cd test/package
-vagrant up docker --no-provision
-vagrant provision docker
-cd -
-
 node scripts/es snapshot \
   -E network.bind_host=127.0.0.1,192.168.50.1 \
   -E discovery.type=single-node \
   --license=trial &
-while ! timeout 1 bash -c "echo > /dev/tcp/localhost/9200"; do sleep 60; done
+while ! timeout 1 bash -c "echo > /dev/tcp/localhost/9200"; do sleep 30; done
+
+cd test/package
+vagrant up docker --no-provision
+vagrant provision docker
+cd -
 
 export TEST_BROWSER_HEADLESS=1
 export TEST_KIBANA_URL=http://elastic:changeme@192.168.50.7:5601
