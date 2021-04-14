fd1a036589449db54ce714b14a4e9955f0be0d5f
Brian Seeders <brian.seeders@elastic.co>
2021-04-14 16:37:43 -0400

    Try GAR instead of GCR

--- .buildkite/scripts/bootstrap_docker.sh
+++ .buildkite/scripts/bootstrap_docker.sh
@@ -4,5 +4,13 @@ set -euo pipefail
 
 export DOCKER_BUILDKIT=1
 
-docker build -t "gcr.io/elastic-kibana-184716/buildkite/ci/base:$BUILDKITE_COMMIT" -f .buildkite/Dockerfile . --progress plain
-docker push "gcr.io/elastic-kibana-184716/buildkite/ci/base:$BUILDKITE_COMMIT"
+gcloud auth configure-docker us-central1-docker.pkg.dev,gcr.io,us.gcr.io --quiet # TODO remove
+
+# docker build -t "us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT" -f .buildkite/Dockerfile . --progress plain
+# docker push "us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT"
+
+docker buildx create --use && \
+docker buildx build -t us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT -f .buildkite/Dockerfile . --progress plain \
+  --output type=image,name=us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT,push=true \
+  --cache-to type=registry,ref=us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:cache \
+  --cache-from type=registry,ref=us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:cache
