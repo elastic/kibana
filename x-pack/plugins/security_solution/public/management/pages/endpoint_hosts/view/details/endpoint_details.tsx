839851980fc39ab1afcd891e321bfe6361ab70f5
Ashokaditya <am.struktr@gmail.com>
2021-04-15 16:43:13 +0200

    use utility class instead
    review change

--- x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/details/endpoint_details.tsx
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/details/endpoint_details.tsx
@@ -245,9 +245,7 @@ export const EndpointDetails = memo(
           title: i18n.translate('xpack.securitySolution.endpoint.details.hostname', {
             defaultMessage: 'Hostname',
           }),
-          description: (
-            <EuiText style={{ wordBreak: 'break-all' }}>{details.host.hostname}</EuiText>
-          ),
+          description: <EuiText className="eui-textBreakAll">{details.host.hostname}</EuiText>,
         },
         {
           title: i18n.translate('xpack.securitySolution.endpoint.details.endpointVersion', {
