06d09667160dedd3707a912883b0b56193704bf1
Brian Seeders <brian.seeders@elastic.co>
2021-04-15 20:31:07 -0400

    Update xpack-cigroup.sh

--- .buildkite/scripts/xpack-cigroup.sh
+++ .buildkite/scripts/xpack-cigroup.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # source "$(dirname "${0}")/env.sh"
 
+export CI_GROUP=${CI_GROUP:-$(( BUILDKITE_PARALLEL_JOB + 1))}
 export JOB=kibana-default-ciGroup${CI_GROUP}
 
 cd "$XPACK_DIR"
