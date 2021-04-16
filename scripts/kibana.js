b18df1d087ed3d8036488d096c7f1c58b5626986
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2021-04-16 03:17:21 +0200

    Do not stop the process until Kibana has gracefully shut down

--- scripts/kibana.js
+++ scripts/kibana.js
@@ -6,4 +6,10 @@
  * Side Public License, v 1.
  */
 
+// Kibana already handles these events internally for graceful shutdowns.
+// `src/cli/dev` is spawning Kibana in a child process to apply the preserve-symlinks options.
+// We need to catch these events here to avoid killing the parent process before Kibana (the child) is fully finished.
+process.on('SIGINT', function () {});
+process.on('SIGTERM', function () {});
+
 require('../src/cli/dev');
