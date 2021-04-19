b59ee0607dbe818bf1d293b84be54bb1398a9a15
Dima Arnautov <dmitrii.arnautov@elastic.co>
2021-04-19 14:01:58 +0200

    [ML] update functional test assertion

--- x-pack/test/functional_with_es_ssl/apps/ml/alert_flyout.ts
+++ x-pack/test/functional_with_es_ssl/apps/ml/alert_flyout.ts
@@ -112,7 +112,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
         await ml.testExecution.logTestStep('should populate advanced settings with default values');
         await ml.alerting.assertTopNBuckets(1);
-        await ml.alerting.assertLookbackInterval('7321s');
+        await ml.alerting.assertLookbackInterval('123m');
 
         await ml.testExecution.logTestStep('should preview the alert condition');
         await ml.alerting.assertPreviewButtonState(false);
