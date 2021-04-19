434b1cd3fdbe97273ed317cf4f776ed2f8b127bc
Brian Seeders <brian.seeders@elastic.co>
2021-04-19 17:03:39 -0400

    Add some annotations

--- .buildkite/scripts/bootstrap_docker.sh
+++ .buildkite/scripts/bootstrap_docker.sh
@@ -4,11 +4,25 @@ set -euo pipefail
 
 export DOCKER_BUILDKIT=1
 
-# docker build -t "us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT" -f .buildkite/Dockerfile . --progress plain
-# docker push "us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT"
+export BASE_IMAGE="us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT"
+
+# docker build -t "$BASE_IMAGE" -f .buildkite/Dockerfile . --progress plain
+# docker push "$BASE_IMAGE"
 
 docker buildx create --use && \
-docker buildx build -t us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT -f .buildkite/Dockerfile . --progress plain \
-  --output type=image,name=us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:$BUILDKITE_COMMIT,push=true \
+docker buildx build -t "$BASE_IMAGE" -f .buildkite/Dockerfile . --progress plain \
+  --output type=image,name="$BASE_IMAGE",push=true \
   --cache-to type=registry,ref=us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:cache \
   --cache-from type=registry,ref=us-central1-docker.pkg.dev/elastic-kibana-184716/kibana-buildkite-docker/buildkite/ci/base:cache
+
+cat << EOF | buildkite-agent annotate --style 'info' --context 'docker-images'
+## Docker Images
+
+### Bootstrapped
+
+\`$BASE_IMAGE\`
+
+#### Bash Shell
+
+\`docker run --rm -it $BASE_IMAGE bash\`
+EOF
