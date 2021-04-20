59e45cb58311c52a022ba332dbe2e927108073f3
Brian Seeders <brian.seeders@elastic.co>
2021-04-20 17:40:16 -0400

    WIP non-docker pipeline

--- .buildkite/scripts/xpack-cigroup.sh
+++ .buildkite/scripts/xpack-cigroup.sh
@@ -7,6 +7,21 @@ set -euo pipefail
 export CI_GROUP=${CI_GROUP:-$(( BUILDKITE_PARALLEL_JOB + 1))}
 export JOB=kibana-default-ciGroup${CI_GROUP}
 
+echo '--- Downloading Distribution and Plugin artifacts'
+
+cd "$WORKSPACE"
+
+buildkite-agent artifact download kibana-default.tar.gz
+buildkite-agent artifact download kibana-default-plugins.tar.gz
+
+tar -xvf kibana-default.tar.gz -C "$KIBANA_BUILD_LOCATION"
+
+cd "$KIBANA_DIR"
+
+tar -xvf kibana-default-plugins.tar.gz
+
+echo "--- Running $JOB"
+
 cd "$XPACK_DIR"
 
 node scripts/functional_tests \
