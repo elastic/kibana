5d83010e7d125b9d3ffa914977a03fe8ed44afd2
Walter Rafelsberger <walter@elastic.co>
2021-04-20 11:41:01 +0200

    [ML] Remove ts-ignore for vega import.

--- src/plugins/vis_type_vega/public/shared.ts
+++ src/plugins/vis_type_vega/public/shared.ts
@@ -7,12 +7,6 @@
  */
 
 // DRAFT PR bundle size experiment
-
-// There is still an issue with Vega Lite's typings with the strict mode Kibana is using.
-// Because we're exposing this to be used by other plugins with `strictNullChecks` enabled,
-// we need to ignore this for now. A PR is up in vega-lite to hopefully fix this in future
-// releases: https://github.com/vega/vega-lite/pull/7352
-// @ts-ignore
 export { compile } from 'vega-lite';
 export { parse, View, Warn } from 'vega';
 export { Handler } from 'vega-tooltip';
