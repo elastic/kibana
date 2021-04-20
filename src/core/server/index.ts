0f4538195f5494f3292b8021a8726ff31df81cd4
Ahmad Bamieh <ahmadbamieh@gmail.com>
2021-04-20 18:02:27 +0300

    [Usage collection] Collect non-default kibana configs (#97368)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/core/server/index.ts
+++ src/core/server/index.ts
@@ -64,6 +64,7 @@ import {
   CoreUsageStats,
   CoreUsageData,
   CoreConfigUsageData,
+  ConfigUsageData,
   CoreEnvironmentUsageData,
   CoreServicesUsageData,
 } from './core_usage_data';
@@ -74,6 +75,7 @@ export type {
   CoreConfigUsageData,
   CoreEnvironmentUsageData,
   CoreServicesUsageData,
+  ConfigUsageData,
 };
 
 export { bootstrap } from './bootstrap';
@@ -256,6 +258,7 @@ export type {
   PluginManifest,
   PluginName,
   SharedGlobalConfig,
+  MakeUsageFromSchema,
 } from './plugins';
 
 export {
