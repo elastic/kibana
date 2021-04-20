3c06ce85d4d8891368c3e0777f1844af4cd847b3
Devin Hurley <devin.hurley@elastic.co>
2021-04-19 21:49:30 -0400

    fixes changes from rebase with master

--- x-pack/plugins/security_solution/server/plugin.ts
+++ x-pack/plugins/security_solution/server/plugin.ts
@@ -27,12 +27,9 @@ import {
   PluginSetupContract as AlertingSetup,
   PluginStartContract as AlertPluginStartContract,
 } from '../../alerting/server';
-import { SecurityPluginSetup as SecuritySetup, SecurityPluginStart } from '../../security/server';
-import {
-  ecsFieldMap,
-  pickWithPatterns,
-  RuleRegistryPluginSetupContract,
-} from '../../rule_registry/server';
+import { ecsFieldMap, pickWithPatterns } from '../../rule_registry/common';
+import { RuleRegistryPluginSetupContract } from '../../rule_registry/server';
+import { SecurityPluginSetup as SecuritySetup } from '../../security/server';
 import { PluginSetupContract as FeaturesSetup } from '../../features/server';
 import { MlPluginSetup as MlSetup } from '../../ml/server';
 import { ListPluginSetup } from '../../lists/server';
@@ -95,7 +92,7 @@ export interface SetupPlugins {
   features: FeaturesSetup;
   lists?: ListPluginSetup;
   ml?: MlSetup;
-  ruleRegistry: RacPluginSetupContract;
+  ruleRegistry: RuleRegistryPluginSetupContract;
   security?: SecuritySetup;
   spaces?: SpacesSetup;
   taskManager?: TaskManagerSetupContract;
@@ -108,7 +105,7 @@ export interface StartPlugins {
   data: DataPluginStart;
   fleet?: FleetStartContract;
   licensing: LicensingPluginStart;
-  ruleRegistry: RacPluginSetupContract;
+  ruleRegistry: RuleRegistryPluginSetupContract;
   taskManager?: TaskManagerStartContract;
   telemetry?: TelemetryPluginStart;
   security: SecurityPluginStart;
