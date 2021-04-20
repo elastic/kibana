b16f0f68a3055ad59aa7fd88050551f57c7049f0
Thomas Watson <w@tson.dk>
2021-04-20 18:30:02 +0200

    Override commercial hapi licenses in license check

--- src/dev/license_checker/config.js
+++ src/dev/license_checker/config.js
@@ -79,6 +79,40 @@ export const LICENSE_OVERRIDES = {
   'jsts@1.1.2': ['Eclipse Distribution License - v 1.0'], //cf. https://github.com/bjornharrtell/jsts
   '@mapbox/jsonlint-lines-primitives@2.0.2': ['MIT'], //license in readme https://github.com/tmcw/jsonlint
 
+  // Override for the commercial hapi license
+  '@commercial/accept@3.2.4': ['BSD-3-Clause'],
+  '@commercial/ammo@3.1.2': ['BSD-3-Clause'],
+  '@commercial/b64@4.2.1': ['BSD-3-Clause'],
+  '@commercial/boom@7.4.12': ['BSD-3-Clause'],
+  '@commercial/bounce@1.3.2': ['BSD-3-Clause'],
+  '@commercial/bourne@1.3.2': ['BSD-3-Clause'],
+  '@commercial/call@5.1.3': ['BSD-3-Clause'],
+  '@commercial/catbox-memory@3.2.2': ['BSD-3-Clause'],
+  '@commercial/catbox@10.2.3': ['BSD-3-Clause'],
+  '@commercial/content@4.1.1': ['BSD-3-Clause'],
+  '@commercial/cryptiles@4.2.1': ['BSD-3-Clause'],
+  '@commercial/file@1.0.0': ['BSD-3-Clause'],
+  '@commercial/formula@1.2.0': ['BSD-3-Clause'],
+  '@commercial/hapi@17.9.4': ['BSD-3-Clause'],
+  '@commercial/heavy@6.2.2': ['BSD-3-Clause'],
+  '@commercial/hoek@6.2.5': ['BSD-3-Clause'],
+  '@commercial/hoek@8.5.1': ['BSD-3-Clause'],
+  '@commercial/iron@5.1.4': ['BSD-3-Clause'],
+  '@commercial/joi@15.1.1': ['BSD-3-Clause'],
+  '@commercial/joi@16.1.8': ['BSD-3-Clause'],
+  '@commercial/mimos@4.1.1': ['BSD-3-Clause'],
+  '@commercial/nigel@3.1.1': ['BSD-3-Clause'],
+  '@commercial/pez@4.1.2': ['BSD-3-Clause'],
+  '@commercial/pinpoint@1.0.2': ['BSD-3-Clause'],
+  '@commercial/podium@3.4.3': ['BSD-3-Clause'],
+  '@commercial/somever@2.1.1': ['BSD-3-Clause'],
+  '@commercial/statehood@6.1.2': ['BSD-3-Clause'],
+  '@commercial/subtext@6.1.3': ['BSD-3-Clause'],
+  '@commercial/teamwork@3.3.1': ['BSD-3-Clause'],
+  '@commercial/topo@3.1.6': ['BSD-3-Clause'],
+  '@commercial/vise@3.1.1': ['BSD-3-Clause'],
+  '@commercial/wreck@15.1.0': ['BSD-3-Clause'],
+
   // TODO can be removed once we upgrade past elasticsearch-browser@14.0.0
   'elasticsearch-browser@13.0.1': ['Apache-2.0'],
 
