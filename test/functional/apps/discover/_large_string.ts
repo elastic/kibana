2e674d8c49447354cde97a44e01908cb91593731
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-04-20 14:43:02 +0200

    [Discover] Enable EuiDataGrid (#89264)
    * Now the new EuiDataGrid based document table is the default
    
    * Columns can be sorted by drag and drop
    
    * Column width can be changed by drag and drop
    
    * There's a fullscreen mode
    
    * There's document selection
    
    * There's document navigation in the flyover of a expanded document
    
    * Sorting is much more user friendly, less confusing and sort order can be changed by drag and drop
    # Conflicts:
    #	test/functional/apps/discover/_discover.ts
    #	x-pack/test/functional/apps/security/doc_level_security_roles.js
    #	x-pack/test/functional/apps/security/field_level_security.js

--- test/functional/apps/discover/_large_string.ts
+++ test/functional/apps/discover/_large_string.ts
@@ -32,7 +32,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
     it('verify the large string book present', async function () {
       const ExpectedDoc =
-        'mybook:Project Gutenberg EBook of Hamlet, by William Shakespeare' +
+        'mybookProject Gutenberg EBook of Hamlet, by William Shakespeare' +
         ' This eBook is for the use of anyone anywhere in the United States' +
         ' and most other parts of the world at no cost and with almost no restrictions whatsoever.' +
         ' You may copy it, give it away or re-use it under the terms of the' +
