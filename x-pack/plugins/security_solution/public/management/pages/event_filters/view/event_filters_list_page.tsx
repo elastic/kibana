15ff0decec6fff2e54db980823c4e742b948beb3
Paul Tavares <paul.tavares@elastic.co>
2021-04-21 16:17:17 -0400

    clean up test code

--- x-pack/plugins/security_solution/public/management/pages/event_filters/view/event_filters_list_page.tsx
+++ x-pack/plugins/security_solution/public/management/pages/event_filters/view/event_filters_list_page.tsx
@@ -9,12 +9,6 @@ import React, { memo } from 'react';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { AdministrationListPage } from '../../../components/administration_list_page';
-import { PaginatedContent } from '../../../components/paginated_content';
-
-// eslint-disable-next-line react/display-name
-const TestComponent = memo<{ id: string }>(({ id }) => (
-  <div style={{ height: '5em', marginTop: '1em', backgroundColor: 'lightblue' }}>{id}</div>
-));
 
 export const EventFiltersListPage = memo(() => {
   return (
@@ -30,23 +24,7 @@ export const EventFiltersListPage = memo(() => {
         defaultMessage: 'Something here about Event Filtering....',
       })}
     >
-      <PaginatedContent<{ id: string }, typeof TestComponent>
-        items={[{ id: '1' }, { id: '2' }]}
-        itemId={'id'}
-        ItemComponent={TestComponent}
-        itemComponentProps={(item) => ({ id: item.id })}
-        onChange={(d) => {
-          // eslint-disable-next-line no-console
-          console.log(d);
-        }}
-        pagination={{
-          totalItemCount: 100,
-          pageIndex: 0,
-          pageSize: 20,
-          pageSizeOptions: [10, 20, 50],
-        }}
-        // noItemsMessage={<>What?</>}
-      />
+      {/* <PaginatedContent />*/}
     </AdministrationListPage>
   );
 });
