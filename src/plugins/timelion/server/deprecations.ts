68cff12e0cf5c30da27ea150ba32c5088b78ddbd
Alison Goryachev <alisonmllr20@gmail.com>
2021-04-22 16:23:56 -0400

    Revert "TEMP commit: test Kibana API action"
    This reverts commit 480b6cd300975cb988478948de1bc49ab61a7033.

--- src/plugins/timelion/server/deprecations.ts
+++ src/plugins/timelion/server/deprecations.ts
@@ -62,10 +62,6 @@ export async function getDeprecations({
           'In the toolbar, click Save.',
           'On the Save visualization window, enter the visualization Title, then click Save and return.',
         ],
-        api: {
-          method: 'POST',
-          path: '/api/upgrade_assistant/test',
-        },
       },
     });
   }
