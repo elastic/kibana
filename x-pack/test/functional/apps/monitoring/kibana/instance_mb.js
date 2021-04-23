c9ce295a0bcb716e546523cc3b6133e9df379473
Chris Roberson <chrisronline@gmail.com>
2021-04-23 11:54:32 -0400

    [Monitoring] Ensure Stack Monitoring UI works with `metricbeat-*` indices (#96205)
    * WIP
    
    * WIP
    
    * Remove unnecessary fields
    
    * Work on node detail page
    
    * Cluster overview looking good
    
    * Index page
    
    * Fix types
    
    * ML jobs
    
    * CCR
    
    * CCR
    
    * We just need total here
    
    * Standalone cluster fix
    
    * Re-enable logstash
    
    * FIx jest test
    
    * Fix tests
    
    * Fix tests
    
    * Fix unused import
    
    * Add new MB-based archives
    
    * Add actual archives
    
    * Fix types
    
    * Add this file back in
    
    * Fix tests and add more
    
    * Update whitelist
    
    * Renames
    
    * Renames
    
    * Only do ccs if enabled
    
    * Updates
    
    * Comment out
    
    * More tests passing
    
    * Another passing test
    
    * More passing, yay
    
    * Forgot to add the actual tests, wow
    
    * CCR
    
    * Fix CI issues
    
    * Missed a field here
    
    * Kibana tests passing
    
    * Fix type issues
    
    * Fix type
    
    * Fix types
    
    * Good chunk of logstash work
    
    * Fix types
    
    * Fix jest test
    
    * Fix issue with get usage in logstash code
    
    * Fix small bug here
    
    * Update archives with proper mappings
    
    * Handle both legacy and mb fields properly
    
    * Fixes
    
    * Fix jest test
    
    * Fix issue
    
    * Getting setup tests in a better state
    
    * Only beats is failing now, which is good
    
    * Progress on cluster listing
    
    * Functional tests passing!
    
    * More progress on cluster tests
    
    * More cluster work
    
    * Fix test
    
    * Last recovery working
    
    * Fix types
    
    * Fix tests
    
    * More tweaks
    
    * Fix snapshot
    
    * Use stats instead of kibana_stats
    
    * Use node and node_stats for logstash
    
    * Beats tests passing
    
    * APM tests passing
    
    * API tests passing!
    
    * Fix types
    
    * Fix tests
    
    * Renames beats-with-restarted-instance archive dirs
    
    Kebab case is disallowed for all newly added files.
    
    * Renames logstash-pipeline dirs
    
    Kebab case disallowed for all new files
    
    * Renames multi-basic dirs
    
    Kebab case disallowed for all new files
    
    * Renames singlecluster-* dirs
    
    Kebab case disallowed for all new files
    
    * Fixes inaccurate path change for archive setup
    
    * Reverts changes to rebuild_all script
    
    Co-authored-by: Jason Rhodes <jason.matthew.rhodes@gmail.com>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/test/functional/apps/monitoring/kibana/instance_mb.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import { getLifecycleMethods } from '../_get_lifecycle_methods';
+
+export default function ({ getService, getPageObjects }) {
+  const clusterOverview = getService('monitoringClusterOverview');
+  const instances = getService('monitoringKibanaInstances');
+  const instance = getService('monitoringKibanaInstance');
+
+  describe('Kibana instance detail mb', () => {
+    const { setup, tearDown } = getLifecycleMethods(getService, getPageObjects);
+
+    before(async () => {
+      await setup('monitoring/singlecluster_yellow_platinum_mb', {
+        from: 'Aug 29, 2017 @ 17:24:14.254',
+        to: 'Aug 29, 2017 @ 17:25:44.142',
+      });
+
+      // go to kibana instance
+      await clusterOverview.clickKibanaInstances();
+      expect(await instances.isOnInstances()).to.be(true);
+
+      await instances.clickRowByName('tsullivan.local');
+
+      expect(await instance.isOnInstance()).to.be(true);
+    });
+
+    after(async () => {
+      await tearDown();
+    });
+
+    it('should have Instance Summary Status showing correct info', async () => {
+      expect(await instance.getSummary()).to.eql({
+        transportAddress: 'Transport Address\ntsullivan.local:5601',
+        osFreeMemory: 'OS Free Memory\n1.5 GB',
+        version: 'Version\n7.0.0-alpha1',
+        uptime: 'Uptime\n3 minutes',
+        health: 'Health: green',
+      });
+    });
+  });
+}
