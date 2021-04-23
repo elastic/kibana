8f9a38ce69dc33a2f1871363a013e7e88a92603d
Constance <constancecchen@users.noreply.github.com>
2021-04-23 10:33:56 -0700

    [App Search] UI polish - empty states (#97875) (#98162)
    * Empty State in Curations
    
    * Adds Empty State to Documents View
    
    * Updates Empty State in Engines Overview
    
    * Updates Relevance Tuning empty state
    
    * Updates the Result Settings empty state
    
    * Updating test files.
    
    * Display the empty state in the table only
    
    * Copy changes
    
    Small copy changes to bring it more inline with Elastic guidelines
    
    * Update api_logs_table test
    
    * Copy adjustments
    
    * Fixing failing type check
    
    * Copy adjustments on credentials empty state
    
    * Copy changes on meta engines table
    
    * Removes emptyState__prompt class
    
    * Removes emptyState__prompt class
    
    Forgot to save a file
    
    * Update x-pack/plugins/enterprise_search/public/applications/app_search/components/engines/components/empty_state.tsx
    
    * Adding the popout icon to docs links
    
    * Removes the need  for the engineIcon class
    
    Also allows EuiIcon props to be passed to metaEngineIcon
    
    * Updates meta engine empty state button to docs link
    
    * Adds a separate constant for the empty meta engine button
    
    * Meta Engines empty state refactor
    
    - pull it out to its own component
    - simplify tests
    - simplify constants, they're only in 1 location so prob don't need to be in DRY'd out
    - fix some i18n IDs
    
    * Curations refactor
    
    * Documents empty state refactor
    
     - create components/ folder, move document creation button to there (feels like it belongs)
     - move empty state there
     - remove other empty state prompt from SearchExperienceContent??? Not sure why that wasn't showing up, but fixed tests/returns
    
    * Relevance Tuning refactor
    
    - create components/ folder
    - move empty state + shared boost icon & value badge to it
    - write test for missing value badge coverage
    
    MISC
    - Fix loading screen to an early return (matches UI of all other views)
    - Remove extra EuiSpacer in layout (matches UI of all other views)
    
    Sorry for the extra noise in your PR Davey!
    
    * Result Settings refactor
    
    - move empty state to its own component
    - fix broken .html.html link
    
    * Fix missing popout icon on documentation button link
    
    * EnginesOverview cleanup
    
    - Remove unnecessary CSS
    - fix responsive behavior of icons
    - standardize responsive behavior of create button actions
    - standardize spacing between content headers & tables
    - Tweak gap between 2 tables
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Constance <constancecchen@users.noreply.github.com>
    Co-authored-by: Constance Chen <constance.chen.3@gmail.com>
    
    Co-authored-by: Davey Holler <daveyholler@gmail.com>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/components/boost_icon.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/relevance_tuning/components/boost_icon.tsx
@@ -9,8 +9,8 @@ import React from 'react';
 
 import { EuiToken } from '@elastic/eui';
 
-import { BOOST_TYPE_TO_ICON_MAP } from './constants';
-import { BoostType } from './types';
+import { BOOST_TYPE_TO_ICON_MAP } from '../constants';
+import { BoostType } from '../types';
 
 interface Props {
   type: BoostType;
