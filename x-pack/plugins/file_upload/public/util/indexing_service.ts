66bcdf9c33eeb26d510c1d6566bb2948d8f45b6f
Aaron Caldwell <aaron.caldwell@elastic.co>
2021-04-23 16:58:51 -0600

    Linting fix

--- x-pack/plugins/file_upload/public/util/indexing_service.ts
+++ x-pack/plugins/file_upload/public/util/indexing_service.ts
@@ -10,13 +10,17 @@ import { i18n } from '@kbn/i18n';
 import { http as httpService } from './http_service';
 import { getIndexPatternService } from '../kibana_services';
 
-export const getExistingIndexNames = _.debounce(async () => {
-  const indexes = await httpService({
-    url: `/api/index_management/indices`,
-    method: 'GET',
-  });
-  return indexes ? indexes.map(({ name }: { name: string }) => name) : [];
-}, 10000, { leading: true });
+export const getExistingIndexNames = _.debounce(
+  async () => {
+    const indexes = await httpService({
+      url: `/api/index_management/indices`,
+      method: 'GET',
+    });
+    return indexes ? indexes.map(({ name }: { name: string }) => name) : [];
+  },
+  10000,
+  { leading: true }
+);
 
 export function checkIndexPatternValid(name: string) {
   const byteLength = encodeURI(name).split(/%(?:u[0-9A-F]{2})?[0-9A-F]{2}|./).length - 1;
