b89af7f5117d95cd7bf2f881e4496da4b49cfa2c
Ryland Herrick <ryalnd@gmail.com>
2021-04-23 16:10:43 -0500

    Update BEATS_VERSION to reflect what's been captured thus far
    I simply neglected to commit this change previously

--- x-pack/plugins/security_solution/scripts/beat_docs/build.js
+++ x-pack/plugins/security_solution/scripts/beat_docs/build.js
@@ -26,7 +26,7 @@ const zlib = require('zlib');
 
 const OUTPUT_DIRECTORY = resolve('scripts', 'beat_docs');
 const OUTPUT_SERVER_DIRECTORY = resolve('server', 'utils', 'beat_schema');
-const BEATS_VERSION = '7.9.0';
+const BEATS_VERSION = '7.12.0';
 
 const beats = [
   {
