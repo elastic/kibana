607dff1c99dc230de14ccbbdea74ab445adb7b1a
James Gowdy <jgowdy@elastic.co>
2021-04-23 11:24:52 +0100

    test commit

--- x-pack/plugins/ml/server/plugin.ts
+++ x-pack/plugins/ml/server/plugin.ts
@@ -168,6 +168,7 @@ export class MlServerPlugin
       ? () => coreSetup.getStartServices().then(([, { spaces }]) => spaces!)
       : undefined;
 
+    // test comment
     annotationRoutes(routeInit, plugins.security);
     calendars(routeInit);
     dataFeedRoutes(routeInit);
