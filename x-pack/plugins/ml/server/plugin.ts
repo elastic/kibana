12b0328cded4c2c5415e0c3bc9fa0a31f57a2ad9
James Gowdy <jgowdy@elastic.co>
2021-04-23 10:21:30 +0100

    [ML] removing unused notification settings endpoint

--- x-pack/plugins/ml/server/plugin.ts
+++ x-pack/plugins/ml/server/plugin.ts
@@ -42,7 +42,6 @@ import { jobRoutes } from './routes/anomaly_detectors';
 import { jobServiceRoutes } from './routes/job_service';
 import { savedObjectsRoutes } from './routes/saved_objects';
 import { jobValidationRoutes } from './routes/job_validation';
-import { notificationRoutes } from './routes/notification_settings';
 import { resultsServiceRoutes } from './routes/results_service';
 import { systemRoutes } from './routes/system';
 import { MlLicense } from '../common/license';
