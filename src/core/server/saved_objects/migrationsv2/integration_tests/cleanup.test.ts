e6ba8ccdc21b2b03b65597f218db4b32d5110746
Mikhail Shustov <restrry@gmail.com>
2021-04-26 15:57:29 +0200

    Rewriting SO id during migration (#97222)
    * some typos
    
    * implement an alternative client-side migration algorithm
    
    required to enforce idempotent id generation for SO
    
    * update tests
    
    * lol
    
    * remove unnecessary param from request generic
    
    * remove unused parameter
    
    * optimize search when quierying SO for migration
    
    * fix wrong type in fixtures
    
    * try shard_doc asc
    
    * add an integration test
    
    * cleanup
    
    * track_total_hits: false to improve perf
    
    * add happy path test for transformDocs action
    
    * remove unused types
    
    * fix wrong typing
    
    * add cleanup phase
    
    * add an integration test for cleanup phase
    
    * add unit-tests for cleanup function
    
    * address comments
    
    * Fix functional test
    
    * set defaultIndex before each test. otherwise it is deleted in the first test file during cleanup phase
    
    * sourceIndex: Option.some<> for consistency
    
    * Revert "set defaultIndex before each test. otherwise it is deleted in the first test file during cleanup phase"
    
    This reverts commit a128d7b7c03493a06d76662902877e535a424b9a.
    
    * address comments from Pierre
    
    * fix test
    
    * Revert "fix test"
    
    This reverts commit 97315b6dc2bc133bdb55a523cacd35f162eac53b.
    
    * revert min convert version back to 8.0
    
    Co-authored-by: Matthias Wilhelm <matthias.wilhelm@elastic.co>

---
+++ src/core/server/saved_objects/migrationsv2/integration_tests/cleanup.test.ts
@@ -0,0 +1,131 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import Path from 'path';
+import Fs from 'fs';
+import Util from 'util';
+import JSON5 from 'json5';
+import * as kbnTestServer from '../../../../test_helpers/kbn_server';
+import type { Root } from '../../../root';
+
+const logFilePath = Path.join(__dirname, 'cleanup_test.log');
+
+const asyncUnlink = Util.promisify(Fs.unlink);
+const asyncReadFile = Util.promisify(Fs.readFile);
+async function removeLogFile() {
+  // ignore errors if it doesn't exist
+  await asyncUnlink(logFilePath).catch(() => void 0);
+}
+
+function createRoot() {
+  return kbnTestServer.createRootWithCorePlugins(
+    {
+      migrations: {
+        skip: false,
+        enableV2: true,
+      },
+      logging: {
+        appenders: {
+          file: {
+            type: 'file',
+            fileName: logFilePath,
+            layout: {
+              type: 'json',
+            },
+          },
+        },
+        loggers: [
+          {
+            name: 'root',
+            appenders: ['file'],
+          },
+        ],
+      },
+    },
+    {
+      oss: true,
+    }
+  );
+}
+
+describe('migration v2', () => {
+  let esServer: kbnTestServer.TestElasticsearchUtils;
+  let root: Root;
+
+  beforeAll(async () => {
+    await removeLogFile();
+  });
+
+  afterAll(async () => {
+    if (root) {
+      await root.shutdown();
+    }
+    if (esServer) {
+      await esServer.stop();
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+  });
+
+  it('clean ups if migration fails', async () => {
+    const { startES } = kbnTestServer.createTestServers({
+      adjustTimeout: (t: number) => jest.setTimeout(t),
+      settings: {
+        es: {
+          license: 'trial',
+          // original SO:
+          // {
+          //   _index: '.kibana_7.13.0_001',
+          //     _type: '_doc',
+          //   _id: 'index-pattern:test_index*',
+          //   _version: 1,
+          //   result: 'created',
+          //   _shards: { total: 2, successful: 1, failed: 0 },
+          //   _seq_no: 0,
+          //     _primary_term: 1
+          // }
+          dataArchive: Path.join(__dirname, 'archives', '7.13.0_with_corrupted_so.zip'),
+        },
+      },
+    });
+
+    root = createRoot();
+
+    esServer = await startES();
+    await root.setup();
+
+    await expect(root.start()).rejects.toThrow(
+      /Unable to migrate the corrupt saved object document with _id: 'index-pattern:test_index\*'/
+    );
+
+    const logFileContent = await asyncReadFile(logFilePath, 'utf-8');
+    const records = logFileContent
+      .split('\n')
+      .filter(Boolean)
+      .map((str) => JSON5.parse(str));
+
+    const logRecordWithPit = records.find(
+      (rec) => rec.message === '[.kibana] REINDEX_SOURCE_TO_TEMP_OPEN_PIT RESPONSE'
+    );
+
+    expect(logRecordWithPit).toBeTruthy();
+
+    const pitId = logRecordWithPit.right.pitId;
+    expect(pitId).toBeTruthy();
+
+    const client = esServer.es.getClient();
+    await expect(
+      client.search({
+        body: {
+          pit: { id: pitId },
+        },
+      })
+      // throws an exception that cannot search with closed PIT
+    ).rejects.toThrow(/search_phase_execution_exception/);
+  });
+});
