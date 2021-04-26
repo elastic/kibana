e67d0c3c9a2e97b1cee1a4a8daece88e78a8e8ef
dej611 <dej611@gmail.com>
2021-04-26 12:42:55 +0200

    bug: Fix React error on frist drop

--- x-pack/plugins/lens/public/drag_drop/drag_drop.tsx
+++ x-pack/plugins/lens/public/drag_drop/drag_drop.tsx
@@ -485,14 +485,18 @@ const DropsInner = memo(function DropsInner(props: DropsInnerProps) {
   }, [order, registerDropTarget, dropTypes, keyboardMode]);
 
   useEffect(() => {
+    let isMounted = true;
     if (activeDropTarget && activeDropTarget.id !== value.id) {
       setIsInZone(false);
     }
     setTimeout(() => {
-      if (!activeDropTarget) {
+      if (!activeDropTarget && isMounted) {
         setIsInZone(false);
       }
     }, 1000);
+    return () => {
+      isMounted = false;
+    };
   }, [activeDropTarget, setIsInZone, value.id]);
 
   const dragEnter = () => {
