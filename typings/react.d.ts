255a8123517d47c4d64c82cf3bb09790d9188853
Greg Thompson <thompson.glowe@gmail.com>
2021-04-26 15:51:05 -0500

    declaration location

--- typings/react.d.ts
+++ typings/react.d.ts
@@ -6,12 +6,11 @@
  * Side Public License, v 1.
  */
 
-// TODO: Remove once typescript definitions are in EUI
+import { Attributes } from 'react';
 
-declare module '@elastic/eui/lib/services' {
-  export const RIGHT_ALIGNMENT: any;
-}
-
-declare module '@elastic/eui/lib/services/format' {
-  export const dateFormatAliases: any;
+// TODO: Remove when EUI uses Emotion
+declare module 'react' {
+  interface Attributes {
+    css?: any;
+  }
 }
