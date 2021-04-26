8745b5fe6362e273f5e03a7bf0d23c4f057f5917
Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>
2021-04-26 19:38:42 +0300

    Fix test

--- test/functional/apps/visualize/_point_series_options.ts
+++ test/functional/apps/visualize/_point_series_options.ts
@@ -60,7 +60,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     await PageObjects.visEditor.clickGo();
   }
 
-  describe('point series', function describeIndexTests() {
+  describe('vlad point series', function describeIndexTests() {
     before(initChart);
 
     describe('secondary value axis', function () {
@@ -314,7 +314,22 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
             const xLabels = await PageObjects.visChart.getExpectedValue(
               ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00'],
-              ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00']
+              [
+                '09:30',
+                '10:00',
+                '10:30',
+                '11:00',
+                '11:30',
+                '12:00',
+                '12:30',
+                '13:00',
+                '13:30',
+                '14:00',
+                '14:30',
+                '15:00',
+                '15:30',
+                '16:00',
+              ]
             );
             return labels.toString() === xLabels.toString();
           }
@@ -368,7 +383,22 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
             const xLabels2 = await PageObjects.visChart.getExpectedValue(
               ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'],
-              ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+              [
+                '16:30',
+                '17:00',
+                '17:30',
+                '18:00',
+                '18:30',
+                '19:00',
+                '19:30',
+                '20:00',
+                '20:30',
+                '21:00',
+                '21:30',
+                '22:00',
+                '22:30',
+                '23:00',
+              ]
             );
             return labels2.toString() === xLabels2.toString();
           }
