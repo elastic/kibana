64db0c2c7b53bf63a2cb5452ae6bdf6ff7ff91dc
Diana Derevyankina <dziyana_dzeraviankina@epam.com>
2021-04-26 16:51:30 +0300

    Update to_ast_pie

--- src/plugins/vis_type_vislib/public/to_ast_pie.ts
+++ src/plugins/vis_type_vislib/public/to_ast_pie.ts
@@ -25,11 +25,10 @@ export const toExpressionAst: VisToExpressionAst<PieVisParams> = async (vis, par
     },
   };
 
-  const configStr = JSON.stringify(visConfig);
   const visTypePie = buildExpressionFunction<VisTypeVislibPieExpressionFunctionDefinition>(
     vislibPieName,
     {
-      visConfig: configStr,
+      visConfig: JSON.stringify(visConfig),
     }
   );
 
