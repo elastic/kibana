b0800a207bf14cb57d5154bd80dc8fe95be9e31d
igoristic <igor.zaytsev.dev@gmail.com>
2021-04-26 11:30:17 -0400

    Update constants.ts

--- x-pack/plugins/monitoring/common/constants.ts
+++ x-pack/plugins/monitoring/common/constants.ts
@@ -564,7 +564,7 @@ export const ALERT_ACTION_TYPE_EMAIL = '.email';
 export const ALERT_ACTION_TYPE_LOG = '.server-log';
 
 /**
- * To enable modifing of alerts in Stack Managment > Actions
+ * To enable modifing of alerts in Stack Management > Actions
  */
 export const ALERT_REQUIRES_APP_CONTEXT = false;
 
