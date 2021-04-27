45b9c3b498c1089896715a373d9d29a100a8d8f2
Mikhail Shustov <restrry@gmail.com>
2021-04-27 15:38:13 +0200

    revert changes to the File appender

--- src/core/server/logging/appenders/file/file_appender.ts
+++ src/core/server/logging/appenders/file/file_appender.ts
@@ -53,11 +53,7 @@ export class FileAppender implements DisposableAppender {
       });
     }
 
-    // There might be a race between Stream ended and append method is called.
-    // In this case we have to drop the record to avoid a runtime exception.
-    if (!this.outputStream.writableEnded) {
-      this.outputStream.write(`${this.layout.format(record)}\n`);
-    }
+    this.outputStream.write(`${this.layout.format(record)}\n`);
   }
 
   /**
