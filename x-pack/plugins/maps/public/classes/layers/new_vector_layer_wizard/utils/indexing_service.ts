bc758309df008271f8f4f35f18a45edc7424dcaa
Aaron Caldwell <aaron.caldwell@elastic.co>
2021-04-27 13:58:02 -0600

    Revise index calls to use kibana https fetch. Remove http service

--- x-pack/plugins/maps/public/classes/layers/new_vector_layer_wizard/utils/indexing_service.ts
+++ x-pack/plugins/maps/public/classes/layers/new_vector_layer_wizard/utils/indexing_service.ts
@@ -5,23 +5,22 @@
  * 2.0.
  */
 
-import { http as httpService } from './http_service';
-import { getSavedObjectsClient } from '../../../../kibana_services';
+import { getSavedObjectsClient, getHttp } from '../../../../kibana_services';
 import { INDEX_FEATURE_PATH, INDEX_SOURCE_API_PATH } from '../../../../../common';
 
 export const getExistingIndexNames = async () => {
-  const indexes = await httpService({
-    url: `/api/index_management/indices`,
+  const indexes = await getHttp().fetch({
+    path: `/api/index_management/indices`,
     method: 'GET',
   });
   return indexes ? indexes.map(({ name }: { name: string }) => name) : [];
 };
 
 export const createNewIndexAndPattern = async (indexName: string) => {
-  return await httpService({
-    url: `/${INDEX_SOURCE_API_PATH}`,
+  return await getHttp().fetch({
+    path: `/${INDEX_SOURCE_API_PATH}`,
     method: 'POST',
-    data: {
+    body: convertObjectToBlob({
       index: indexName,
       // Initially set to static mappings
       mappings: {
@@ -31,21 +30,25 @@ export const createNewIndexAndPattern = async (indexName: string) => {
           },
         },
       },
-    },
+    }),
   });
 };
 
 export const addFeatureToIndex = async (indexName: string, geometry: unknown) => {
-  return await httpService({
-    url: `/${INDEX_FEATURE_PATH}`,
+  return await getHttp().fetch({
+    path: `/${INDEX_FEATURE_PATH}`,
     method: 'POST',
-    data: {
+    body: convertObjectToBlob({
       index: indexName,
       data: {
         coordinates: geometry,
       },
-    },
+    }),
   });
+};
+
+const convertObjectToBlob = (obj: unknown) => {
+  return new Blob([JSON.stringify(obj)], { type: 'application/json' });
 };
 
 export const getExistingIndexPatternNames = async () => {
