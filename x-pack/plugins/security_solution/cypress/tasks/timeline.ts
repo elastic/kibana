e0e4bee1210e2e9c13e7aea49267f66d74f995f5
Steph Milovic <stephanie.milovic@elastic.co>
2021-04-28 08:29:45 -0600

    rid of unnecessary log

--- x-pack/plugins/security_solution/cypress/tasks/timeline.ts
+++ x-pack/plugins/security_solution/cypress/tasks/timeline.ts
@@ -73,7 +73,6 @@ export const addDescriptionToTimeline = (description: string) => {
 
 export const addNameToTimeline = (name: string) => {
   cy.get(TIMELINE_EDIT_MODAL_OPEN_BUTTON).first().click();
-  // cy.log('HERE:', cy.get(TIMELINE_TITLE_INPUT));
   cy.get(TIMELINE_TITLE_INPUT).type(`${name}{enter}`);
   cy.get(TIMELINE_TITLE_INPUT).should('have.attr', 'value', name);
   cy.get(TIMELINE_EDIT_MODAL_SAVE_BUTTON).click();
