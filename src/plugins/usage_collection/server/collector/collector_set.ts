df430897664a971683eacdded0da6a7d42977468
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2021-04-28 19:02:08 +0200

    [API DOCS] Usage Collection

--- src/plugins/usage_collection/server/collector/collector_set.ts
+++ src/plugins/usage_collection/server/collector/collector_set.ts
@@ -13,12 +13,13 @@ import type {
   SavedObjectsClientContract,
   KibanaRequest,
 } from 'src/core/server';
-import { Collector, CollectorOptions } from './collector';
+import { Collector } from './collector';
+import type { ICollector, CollectorOptions } from './types';
 import { UsageCollector, UsageCollectorOptions } from './usage_collector';
 
 // Needed for the general array containing all the collectors. We don't really care about their types here
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type AnyCollector = Collector<any, any>;
+type AnyCollector = ICollector<any, any>;
 
 interface CollectorSetConfig {
   logger: Logger;
