4b64db2a4a6357cebefee0bac9e238e99cad0c48
Brian Seeders <brian.seeders@elastic.co>
2021-04-28 13:58:42 -0400

    Re-enable ts refs for baseline

--- .buildkite/scripts/jobs/baseline.sh
+++ .buildkite/scripts/jobs/baseline.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 export BUILD_TS_REFS_CACHE_ENABLE=true
 export BUILD_TS_REFS_CACHE_CAPTURE=true
 export DISABLE_BOOTSTRAP_VALIDATION=true
+export BUILD_TS_REFS_DISABLE=false
 
 .buildkite/scripts/bootstrap.sh
 .buildkite/scripts/build_kibana.sh
