0bf5601715bab741f472ac97148dafa9b83f9955
scottybollinger <scotty.bollinger@elastic.co>
2021-04-28 16:14:53 -0500

    Wrap header actions in EuiHeaderLinks for mobile
    This is an add-on and should be reviewed with white space changes hidden.

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/components/layout/kibana_header_actions.tsx
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/components/layout/kibana_header_actions.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 
-import { EuiButtonEmpty, EuiText, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiButtonEmpty, EuiText, EuiFlexGroup, EuiFlexItem, EuiHeaderLinks } from '@elastic/eui';
 
 import { externalUrl, getWorkplaceSearchUrl } from '../../../shared/enterprise_search_url';
 import { EuiButtonEmptyTo } from '../../../shared/react_router_helpers';
@@ -18,26 +18,28 @@ export const WorkplaceSearchHeaderActions: React.FC = () => {
   if (!externalUrl.enterpriseSearchUrl) return null;
 
   return (
-    <EuiFlexGroup gutterSize="s">
-      <EuiFlexItem>
-        <EuiButtonEmptyTo
-          data-test-subj="PersonalDashboardButton"
-          iconType="user"
-          to={PERSONAL_SOURCES_PATH}
-        >
-          <EuiText size="s">{NAV.PERSONAL_DASHBOARD}</EuiText>
-        </EuiButtonEmptyTo>
-      </EuiFlexItem>
-      <EuiFlexItem>
-        <EuiButtonEmpty
-          data-test-subj="HeaderSearchButton"
-          href={getWorkplaceSearchUrl('/search')}
-          target="_blank"
-          iconType="search"
-        >
-          <EuiText size="s">{NAV.SEARCH}</EuiText>
-        </EuiButtonEmpty>
-      </EuiFlexItem>
-    </EuiFlexGroup>
+    <EuiHeaderLinks>
+      <EuiFlexGroup gutterSize="s">
+        <EuiFlexItem>
+          <EuiButtonEmptyTo
+            data-test-subj="PersonalDashboardButton"
+            iconType="user"
+            to={PERSONAL_SOURCES_PATH}
+          >
+            <EuiText size="s">{NAV.PERSONAL_DASHBOARD}</EuiText>
+          </EuiButtonEmptyTo>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiButtonEmpty
+            data-test-subj="HeaderSearchButton"
+            href={getWorkplaceSearchUrl('/search')}
+            target="_blank"
+            iconType="search"
+          >
+            <EuiText size="s">{NAV.SEARCH}</EuiText>
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiHeaderLinks>
   );
 };
