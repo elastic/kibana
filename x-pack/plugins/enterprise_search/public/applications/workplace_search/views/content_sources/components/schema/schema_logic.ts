2c3a6c0831c0ce25188c8f61b335f7f2499ab7ab
Constance Chen <constance.chen.3@gmail.com>
2021-04-29 10:18:32 -0700

    Update files to use new schema type enum

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/schema/schema_logic.ts
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/schema/schema_logic.ts
@@ -10,7 +10,6 @@ import { cloneDeep, isEqual } from 'lodash';
 
 import { i18n } from '@kbn/i18n';
 
-import { TEXT } from '../../../../../shared/constants/field_types';
 import { ADD, UPDATE } from '../../../../../shared/constants/operations';
 import {
   flashAPIErrors,
@@ -19,7 +18,8 @@ import {
   clearFlashMessages,
 } from '../../../../../shared/flash_messages';
 import { HttpLogic } from '../../../../../shared/http';
-import { IndexJob, TOperation, Schema, SchemaTypes } from '../../../../../shared/types';
+import { IndexJob, Schema, SchemaType } from '../../../../../shared/schema/types';
+import { TOperation } from '../../../../../shared/types';
 import { AppLogic } from '../../../../app_logic';
 import { OptionValue } from '../../../../types';
 import { SourceLogic } from '../../source_logic';
@@ -37,7 +37,7 @@ interface SchemaActions {
   ): SchemaChangeErrorsProps;
   onSchemaSetSuccess(schemaProps: SchemaResponseProps): SchemaResponseProps;
   onSchemaSetFormErrors(errors: string[]): string[];
-  updateNewFieldType(newFieldType: SchemaTypes): SchemaTypes;
+  updateNewFieldType(newFieldType: SchemaType): SchemaType;
   onFieldUpdate({
     schema,
     formUnchanged,
@@ -51,8 +51,8 @@ interface SchemaActions {
   setFilterValue(filterValue: string): string;
   addNewField(
     fieldName: string,
-    newFieldType: SchemaTypes
-  ): { fieldName: string; newFieldType: SchemaTypes };
+    newFieldType: SchemaType
+  ): { fieldName: string; newFieldType: SchemaType };
   updateFields(): void;
   openAddFieldModal(): void;
   closeAddFieldModal(): void;
@@ -64,8 +64,8 @@ interface SchemaActions {
   ): { activeReindexJobId: string; sourceId: string };
   updateExistingFieldType(
     fieldName: string,
-    newFieldType: SchemaTypes
-  ): { fieldName: string; newFieldType: SchemaTypes };
+    newFieldType: SchemaType
+  ): { fieldName: string; newFieldType: SchemaType };
   setServerField(
     updatedSchema: Schema,
     operation: TOperation
@@ -142,7 +142,7 @@ export const SchemaLogic = kea<MakeLogicType<SchemaValues, SchemaActions>>({
       activeReindexJobId,
       sourceId,
     }),
-    addNewField: (fieldName: string, newFieldType: SchemaTypes) => ({ fieldName, newFieldType }),
+    addNewField: (fieldName: string, newFieldType: SchemaType) => ({ fieldName, newFieldType }),
     updateExistingFieldType: (fieldName: string, newFieldType: string) => ({
       fieldName,
       newFieldType,
@@ -196,10 +196,10 @@ export const SchemaLogic = kea<MakeLogicType<SchemaValues, SchemaActions>>({
       },
     ],
     newFieldType: [
-      TEXT,
+      SchemaType.Text,
       {
         updateNewFieldType: (_, newFieldType) => newFieldType,
-        onSchemaSetSuccess: () => TEXT,
+        onSchemaSetSuccess: () => SchemaType.Text,
       },
     ],
     addFieldFormErrors: [
