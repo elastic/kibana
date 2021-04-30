a5d910d50cc0f06e5ad971286110de039165337f
Constance <constancecchen@users.noreply.github.com>
2021-04-30 09:22:16 -0700

    [App Search] Schema: route & bare-bones views setup (#98679)
    * Set up basic Schema skeleton views+router
    
    * Update engine router + nav with new schema routes
    
    * Breadcrumbs
    
    * PR feedback: derpin
    
    Co-authored-by: Jason Stoltzfus <jastoltz24@gmail.com>
    
    * reindexJobId pls
    
    * my life is in shambles
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Jason Stoltzfus <jastoltz24@gmail.com>

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/schema/views/index.ts
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/schema/views/index.ts
@@ -5,5 +5,5 @@
  * 2.0.
  */
 
-export { SCHEMA_TITLE } from './constants';
-export { SchemaRouter } from './schema_router';
+export { Schema } from './schema';
+export { MetaEngineSchema } from './meta_engine_schema';
