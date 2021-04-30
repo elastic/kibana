a7bd46b0f0797c4ea5b700163b7e56039ff077b0
FrankHassanabad <frank.hassanabad@elastic.co>
2021-04-30 17:33:17 -0600

    First file to move from io-ts shared with several components into packages

--- x-pack/plugins/security_solution/server/utils/build_validation/route_validation.ts
+++ x-pack/plugins/security_solution/server/utils/build_validation/route_validation.ts
@@ -8,7 +8,8 @@
 import { fold } from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/pipeable';
 import * as rt from 'io-ts';
-import { formatErrors } from '../../../common/format_errors';
+
+import { formatErrors } from '@kbn/securitysolution-io-ts-utils';
 import { exactCheck } from '../../../common/exact_check';
 import {
   RouteValidationFunction,
