168a0b8700115290bf631d11f56e60e4a365fa24
neptunian <sandra.gonzales@elastic.co>
2021-04-30 13:04:12 -0400

    use EuiTooltip to control tooltip component

--- x-pack/plugins/infra/public/pages/metrics/inventory_view/components/waffle/conditional_tooltip.tsx
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/waffle/conditional_tooltip.tsx
@@ -5,8 +5,8 @@
  * 2.0.
  */
 
-import React, { useCallback, useState, useEffect } from 'react';
-import { EuiToolTip, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import React, { useEffect } from 'react';
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { first } from 'lodash';
 import { getCustomMetricLabel } from '../../../../../../common/formatters/get_custom_metric_label';
 import { SnapshotCustomMetricInput } from '../../../../../../common/http_api';
@@ -18,7 +18,7 @@ import {
   SnapshotMetricType,
   SnapshotMetricTypeRT,
 } from '../../../../../../common/inventory_models/types';
-import { InfraWaffleMapNode, InfraWaffleMapOptions } from '../../../../../lib/lib';
+import { InfraWaffleMapNode } from '../../../../../lib/lib';
 import { useSnapshot } from '../../hooks/use_snaphot';
 import { createInventoryMetricFormatter } from '../../lib/create_inventory_metric_formatter';
 import { SNAPSHOT_METRIC_TRANSLATIONS } from '../../../../../../common/inventory_models/intl_strings';
@@ -27,77 +27,57 @@ import { createFormatterForMetric } from '../../../metrics_explorer/components/h
 
 export interface Props {
   currentTime: number;
-  hidden: boolean;
   node: InfraWaffleMapNode;
-  options: InfraWaffleMapOptions;
-  formatter: (val: number) => string;
-  children: React.ReactElement;
   nodeType: InventoryItemType;
   theme: EuiTheme | undefined;
 }
-
-export const ConditionalToolTip = withTheme(
-  ({ theme, hidden, node, children, nodeType, currentTime }: Props) => {
-    const { sourceId } = useSourceContext();
-    const [timer, setTimer] = useState<ReturnType<typeof setTimeout> | null>(null);
-    const model = findInventoryModel(nodeType);
-    const { customMetrics } = useWaffleOptionsContext();
-    const requestMetrics = model.tooltipMetrics
-      .map((type) => ({ type }))
-      .concat(customMetrics) as Array<
-      | {
-          type: SnapshotMetricType;
-        }
-      | SnapshotCustomMetricInput
-    >;
-    const query = JSON.stringify({
-      bool: {
-        filter: {
-          match_phrase: { [model.fields.id]: node.id },
-        },
+export const ConditionalToolTip = withTheme(({ theme, node, nodeType, currentTime }: Props) => {
+  const { sourceId } = useSourceContext();
+  const model = findInventoryModel(nodeType);
+  const { customMetrics } = useWaffleOptionsContext();
+  const requestMetrics = model.tooltipMetrics
+    .map((type) => ({ type }))
+    .concat(customMetrics) as Array<
+    | {
+        type: SnapshotMetricType;
+      }
+    | SnapshotCustomMetricInput
+  >;
+  const query = JSON.stringify({
+    bool: {
+      filter: {
+        match_phrase: { [model.fields.id]: node.id },
       },
-    });
-    const { nodes, reload } = useSnapshot(
-      query,
-      requestMetrics,
-      [],
-      nodeType,
-      sourceId,
-      currentTime,
-      '',
-      '',
-      false // Doesn't send request until reload() is called
-    );
+    },
+  });
+  const { nodes, reload } = useSnapshot(
+    query,
+    requestMetrics,
+    [],
+    nodeType,
+    sourceId,
+    currentTime,
+    '',
+    '',
+    false // Doesn't send request until reload() is called
+  );
 
-    const handleDataLoad = useCallback(() => {
-      const id = setTimeout(reload, 200);
-      setTimer(id);
-    }, [reload]);
+  useEffect(() => {
+    reload();
+  }, [reload]);
 
-    const cancelDataLoad = useCallback(() => {
-      return (timer && clearTimeout(timer)) || void 0;
-    }, [timer]);
-
-    useEffect(() => {
-      return cancelDataLoad;
-    }, [timer, cancelDataLoad]);
-
-    if (hidden) {
-      return children;
-    }
-    const dataNode = first(nodes);
-    const metrics = (dataNode && dataNode.metrics) || [];
-    const content = (
-      <div style={{ minWidth: 200 }} data-test-subj="conditionalTooltipContent">
-        <div
-          style={{
-            borderBottom: `1px solid ${theme?.eui.euiColorMediumShade}`,
-            paddingBottom: theme?.eui.paddingSizes.xs,
-            marginBottom: theme?.eui.paddingSizes.xs,
-          }}
-        >
-          {node.name}
-        </div>
+  const dataNode = first(nodes);
+  const metrics = (dataNode && dataNode.metrics) || [];
+  return (
+    <div style={{ minWidth: 200 }} data-test-subj="conditionalTooltipContent">
+      <div
+        style={{
+          borderBottom: `1px solid ${theme?.eui.euiColorMediumShade}`,
+          paddingBottom: theme?.eui.paddingSizes.xs,
+          marginBottom: theme?.eui.paddingSizes.xs,
+        }}
+      >
+        <div>{node.name}</div>
         {metrics.map((metric) => {
           const metricName = SnapshotMetricTypeRT.is(metric.name) ? metric.name : 'custom';
           const name = SNAPSHOT_METRIC_TRANSLATIONS[metricName] || metricName;
@@ -120,20 +100,6 @@ export const ConditionalToolTip = withTheme(
           );
         })}
       </div>
-    );
-
-    return (
-      <EuiToolTip delay="regular" position="right" content={content}>
-        <div
-          data-test-subj="conditionalTooltipMouseHandler"
-          onMouseOver={handleDataLoad}
-          onFocus={handleDataLoad}
-          onMouseOut={cancelDataLoad}
-          onBlur={cancelDataLoad}
-        >
-          {children}
-        </div>
-      </EuiToolTip>
-    );
-  }
-);
+    </div>
+  );
+});
