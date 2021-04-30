38f2d4854c6242b28de90e3609ce72aded9d1cce
Shahzad <shahzad.muhammad@elastic.co>
2021-04-30 19:08:45 +0200

    [Exploratory view] Restore links for ux/synthetics data (#98759)

--- x-pack/plugins/uptime/public/components/common/header/action_menu.tsx
+++ x-pack/plugins/uptime/public/components/common/header/action_menu.tsx
@@ -6,22 +6,60 @@
  */
 
 import React from 'react';
-import { EuiButtonEmpty, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiButtonEmpty, EuiFlexGroup, EuiFlexItem, EuiToolTip } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { useKibana } from '../../../../../../../src/plugins/kibana_react/public';
-import { HeaderMenuPortal } from '../../../../../observability/public';
+import {
+  createExploratoryViewUrl,
+  HeaderMenuPortal,
+  SeriesUrl,
+} from '../../../../../observability/public';
 import { AppMountParameters } from '../../../../../../../src/core/public';
+import { useUptimeSettingsContext } from '../../../contexts/uptime_settings_context';
+import { useGetUrlParams } from '../../../hooks';
 
 const ADD_DATA_LABEL = i18n.translate('xpack.uptime.addDataButtonLabel', {
   defaultMessage: 'Add data',
 });
 
+const ANALYZE_DATA = i18n.translate('xpack.uptime.analyzeDataButtonLabel', {
+  defaultMessage: 'Analyze data',
+});
+
+const ANALYZE_MESSAGE = i18n.translate('xpack.uptime.analyzeDataButtonLabel.message', {
+  defaultMessage:
+    'EXPERIMENTAL - Analyze Data allows you to select and filter result data in any dimension and look for the cause or impact of performance problems.',
+});
+
 export const ActionMenu = ({ appMountParameters }: { appMountParameters: AppMountParameters }) => {
   const kibana = useKibana();
+  const { basePath } = useUptimeSettingsContext();
+  const { dateRangeStart, dateRangeEnd } = useGetUrlParams();
+
+  const syntheticExploratoryViewLink = createExploratoryViewUrl(
+    {
+      'synthetics-series': {
+        dataType: 'synthetics',
+        time: { from: dateRangeStart, to: dateRangeEnd },
+      } as SeriesUrl,
+    },
+    basePath
+  );
 
   return (
     <HeaderMenuPortal setHeaderActionMenu={appMountParameters.setHeaderActionMenu}>
       <EuiFlexGroup alignItems="flexEnd" responsive={false} style={{ paddingRight: 20 }}>
+        <EuiFlexItem>
+          <EuiToolTip position="top" content={<p>{ANALYZE_MESSAGE}</p>}>
+            <EuiButtonEmpty
+              href={syntheticExploratoryViewLink}
+              color="primary"
+              iconType="visBarVerticalStacked"
+            >
+              {ANALYZE_DATA}
+            </EuiButtonEmpty>
+          </EuiToolTip>
+        </EuiFlexItem>
         <EuiFlexItem grow={false}>
           <EuiButtonEmpty
             href={kibana.services?.application?.getUrlForApp('/home#/tutorial/uptimeMonitors')}
