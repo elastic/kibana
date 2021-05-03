54f848dc0c6236d617f5209048a2cebcb1ab61f3
Dario Gieselaar <dario.gieselaar@elastic.co>
2021-05-03 14:14:52 +0200

    Don't refresh after SO updates

--- x-pack/plugins/actions/server/create_execute_function.ts
+++ x-pack/plugins/actions/server/create_execute_function.ts
@@ -63,7 +63,10 @@ export function createExecutionEnqueuerFunction({
         params,
         apiKey,
       },
-      executionSourceAsSavedObjectReferences(source)
+      {
+        ...executionSourceAsSavedObjectReferences(source),
+        refresh: false,
+      }
     );
 
     await taskManager.schedule({
