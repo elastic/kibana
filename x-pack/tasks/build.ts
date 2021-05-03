f495af83b8637575bf9ccb34a89183e2cf09f09e
spalger <spalger@users.noreply.github.com>
2021-05-03 08:01:36 -0700

    target dirs are not direct children of plugins

--- x-pack/tasks/build.ts
+++ x-pack/tasks/build.ts
@@ -76,7 +76,7 @@ async function copySourceAndBabelify() {
           '**/node_modules/**',
           '**/public/**/*.{js,ts,tsx,json}',
           '**/{__tests__,__mocks__,__snapshots__}/**',
-          'plugins/target/**/*',
+          'plugins/*/target/**',
           'plugins/canvas/shareable_runtime/test/**',
           'plugins/telemetry_collection_xpack/schema/**', // Skip telemetry schemas
         ],
