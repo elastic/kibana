c4fc65ca27c83b6c15ec7a681d017ea4283a7813
Nathan Reese <reese.nathan@gmail.com>
2021-05-03 14:35:48 -0600

    convert react class to function component

--- x-pack/plugins/lens/public/indexpattern_datasource/visualize_geo_field_button.tsx
+++ x-pack/plugins/lens/public/indexpattern_datasource/visualize_geo_field_button.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { EuiButton } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import {
@@ -20,72 +20,52 @@ interface Props {
   uiActions: UiActionsStart;
 }
 
-interface State {
-  hasLoadedHref: boolean;
-  href?: string;
-}
+export function VisualizeGeoFieldButton(props: Props) {
+  const [href, setHref] = useState<string | undefined>(undefined);
 
-export class VisualizeGeoFieldButton extends Component<Props, State> {
-  private _isMounted = false;
-  state: State = {
-    hasLoadedHref: false,
-  };
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-    this.loadHref();
-  }
-
-  async loadHref() {
-    const actions = await this.props.uiActions.getTriggerCompatibleActions(
-      VISUALIZE_GEO_FIELD_TRIGGER,
-      {
-        indexPatternId: this.props.indexPatternId,
-        fieldName: this.props.fieldName,
-      }
-    );
+  async function loadHref() {
+    const actions = await props.uiActions.getTriggerCompatibleActions(VISUALIZE_GEO_FIELD_TRIGGER, {
+      indexPatternId: props.indexPatternId,
+      fieldName: props.fieldName,
+    });
     const triggerOptions = {
-      indexPatternId: this.props.indexPatternId,
-      fieldName: this.props.fieldName,
+      indexPatternId: props.indexPatternId,
+      fieldName: props.fieldName,
       trigger: visualizeGeoFieldTrigger,
     };
-    const href = actions.length ? await actions[0].getHref?.(triggerOptions) : undefined;
-
-    if (this._isMounted) {
-      this.setState({
-        hasLoadedHref: true,
-        href,
-      });
-    }
+    const loadedHref = actions.length ? await actions[0].getHref?.(triggerOptions) : undefined;
+    setHref(loadedHref);
   }
 
-  onClick = (event: unknown) => {
-    this.props.uiActions.getTrigger(VISUALIZE_GEO_FIELD_TRIGGER).exec({
-      indexPatternId: this.props.indexPatternId,
-      fieldName: this.props.fieldName,
+  useEffect(
+    () => {
+      loadHref();
+    },
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    []
+  );
+
+  function onClick() {
+    props.uiActions.getTrigger(VISUALIZE_GEO_FIELD_TRIGGER).exec({
+      indexPatternId: props.indexPatternId,
+      fieldName: props.fieldName,
     });
-  };
-
-  render() {
-    return (
-      <>
-        {/* eslint-disable-next-line @elastic/eui/href-or-on-click */}
-        <EuiButton
-          onClick={this.onClick}
-          href={this.state.href}
-          size="s"
-          data-test-subj={`lensGeoFieldVisualize-${this.props.fieldName}`}
-        >
-          <FormattedMessage
-            id="xpack.lens.indexPattern.fieldItem.visualizeGeoFieldLinkText"
-            defaultMessage="Visualize in Maps"
-          />
-        </EuiButton>
-      </>
-    );
   }
+
+  return (
+    <>
+      {/* eslint-disable-next-line @elastic/eui/href-or-on-click */}
+      <EuiButton
+        onClick={onClick}
+        href={href}
+        size="s"
+        data-test-subj={`lensGeoFieldVisualize-${props.fieldName}`}
+      >
+        <FormattedMessage
+          id="xpack.lens.indexPattern.fieldItem.visualizeGeoFieldLinkText"
+          defaultMessage="Visualize in Maps"
+        />
+      </EuiButton>
+    </>
+  );
 }
