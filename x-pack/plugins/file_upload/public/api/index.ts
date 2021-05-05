e4c1c92b5776f4babb81c22a5dee387534ac5dba
Aaron Caldwell <aaron.caldwell@elastic.co>
2021-05-05 09:52:24 -0600

    Review feedback. Move wrapper components to api dir

--- x-pack/plugins/file_upload/public/api/index.ts
+++ x-pack/plugins/file_upload/public/api/index.ts
@@ -9,8 +9,8 @@ import { lazyLoadModules } from '../lazy_load_bundle';
 import type { IImporter, ImportFactoryOptions } from '../importer';
 import type { HasImportPermission, FindFileStructureResponse } from '../../common';
 import type { getMaxBytes, getMaxBytesFormatted } from '../importer/get_max_bytes';
-import { JsonUploadAndParseAsyncWrapper } from '../components/json_upload_and_parse_async_wrapper';
-import { IndexNameFormAsyncWrapper } from '../components/index_name_form_async_wrapper';
+import { JsonUploadAndParseAsyncWrapper } from './json_upload_and_parse_async_wrapper';
+import { IndexNameFormAsyncWrapper } from './index_name_form_async_wrapper';
 
 export interface FileUploadStartApi {
   getFileUploadComponent(): ReturnType<typeof getFileUploadComponent>;
