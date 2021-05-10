927c67d161d429634697b29975a3a4c6cba1319d
Nathan Reese <reese.nathan@gmail.com>
2021-05-10 12:54:15 -0600

    [Maps] use deep equals for mapbox filter expression equals check

--- x-pack/plugins/maps/public/classes/layers/vector_layer/vector_layer.tsx
+++ x-pack/plugins/maps/public/classes/layers/vector_layer/vector_layer.tsx
@@ -781,7 +781,7 @@ export class VectorLayer extends AbstractLayer implements IVectorLayer {
     }
 
     const filterExpr = getPointFilterExpression(this.hasJoins());
-    if (filterExpr !== mbMap.getFilter(pointLayerId)) {
+    if (!_.isEqual(filterExpr, mbMap.getFilter(pointLayerId))) {
       mbMap.setFilter(pointLayerId, filterExpr);
       mbMap.setFilter(textLayerId, filterExpr);
     }
@@ -817,7 +817,7 @@ export class VectorLayer extends AbstractLayer implements IVectorLayer {
     }
 
     const filterExpr = getPointFilterExpression(this.hasJoins());
-    if (filterExpr !== mbMap.getFilter(symbolLayerId)) {
+    if (!_.isEqual(filterExpr, mbMap.getFilter(symbolLayerId))) {
       mbMap.setFilter(symbolLayerId, filterExpr);
     }
 
@@ -899,14 +899,14 @@ export class VectorLayer extends AbstractLayer implements IVectorLayer {
     this.syncVisibilityWithMb(mbMap, fillLayerId);
     mbMap.setLayerZoomRange(fillLayerId, this.getMinZoom(), this.getMaxZoom());
     const fillFilterExpr = getFillFilterExpression(hasJoins);
-    if (fillFilterExpr !== mbMap.getFilter(fillLayerId)) {
+    if (!_.isEqual(fillFilterExpr, mbMap.getFilter(fillLayerId))) {
       mbMap.setFilter(fillLayerId, fillFilterExpr);
     }
 
     this.syncVisibilityWithMb(mbMap, lineLayerId);
     mbMap.setLayerZoomRange(lineLayerId, this.getMinZoom(), this.getMaxZoom());
     const lineFilterExpr = getLineFilterExpression(hasJoins);
-    if (lineFilterExpr !== mbMap.getFilter(lineLayerId)) {
+    if (!_.isEqual(lineFilterExpr, mbMap.getFilter(lineLayerId))) {
       mbMap.setFilter(lineLayerId, lineFilterExpr);
     }
 
@@ -930,7 +930,7 @@ export class VectorLayer extends AbstractLayer implements IVectorLayer {
     }
 
     const filterExpr = getCentroidFilterExpression(this.hasJoins());
-    if (filterExpr !== mbMap.getFilter(centroidLayerId)) {
+    if (!_.isEqual(filterExpr, mbMap.getFilter(centroidLayerId))) {
       mbMap.setFilter(centroidLayerId, filterExpr);
     }
 
