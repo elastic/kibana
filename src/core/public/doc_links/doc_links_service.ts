2dd97a17eee1950496c79de7e9baecd7ce425686
James Rodewig <40268737+jrodewig@users.noreply.github.com>
2021-05-10 10:40:01 -0400

    Update src/core/public/doc_links/doc_links_service.ts

--- src/core/public/doc_links/doc_links_service.ts
+++ src/core/public/doc_links/doc_links_service.ts
@@ -184,7 +184,7 @@ export class DocLinksService {
           remoteClustersProxy: `${ELASTICSEARCH_DOCS}modules-remote-clusters.html#proxy-mode`,
           remoteClusersProxySettings: `${ELASTICSEARCH_DOCS}modules-remote-clusters.html#remote-cluster-proxy-settings`,
           scriptParameters: `${ELASTICSEARCH_DOCS}modules-scripting-using.html#prefer-params`,
-          transportSettings: `${ELASTICSEARCH_DOCS}modules-network.htmll#common-network-settings`,
+          transportSettings: `${ELASTICSEARCH_DOCS}modules-network.html#common-network-settings`,
           typesRemoval: `${ELASTICSEARCH_DOCS}removal-of-types.html`,
           deprecationLogging: `${ELASTICSEARCH_DOCS}logging.html#deprecation-logging`,
         },
