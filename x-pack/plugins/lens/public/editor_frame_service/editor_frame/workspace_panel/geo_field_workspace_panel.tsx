2eb13d5ab9bb41022f468bec13a0f095053124d3
Nathan Reese <reese.nathan@gmail.com>
2021-05-10 17:42:30 -0600

    tslint

--- x-pack/plugins/lens/public/editor_frame_service/editor_frame/workspace_panel/geo_field_workspace_panel.tsx
+++ x-pack/plugins/lens/public/editor_frame_service/editor_frame/workspace_panel/geo_field_workspace_panel.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { EuiPageContentBody, EuiSpacer, EuiText } from '@elastic/eui';
+import { EuiPageContentBody, EuiText } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 import { i18n } from '@kbn/i18n';
 import {
