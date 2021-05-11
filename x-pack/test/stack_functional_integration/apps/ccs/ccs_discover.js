0e1280caf2169971bc9c72283ffda3ed09dfe73c
Domenico Andreoli <domenico.andreoli@elastic.co>
2021-05-11 18:52:34 +0200

    Split out test preparation and cleanup

--- x-pack/test/stack_functional_integration/apps/ccs/ccs_discover.js
+++ x-pack/test/stack_functional_integration/apps/ccs/ccs_discover.js
@@ -24,7 +24,6 @@ export default ({ getService, getPageObjects }) => {
     const kibanaServer = getService('kibanaServer');
     const queryBar = getService('queryBar');
     const filterBar = getService('filterBar');
-    const supertest = getService('supertest');
 
     before(async () => {
       await browser.setWindowSize(1200, 800);
@@ -98,8 +97,6 @@ export default ({ getService, getPageObjects }) => {
         );
         await PageObjects.security.logout();
       }
-      // visit app/security so to create .siem-signals-* as side effect
-      await PageObjects.common.navigateToApp('security', { insertTimestamp: false });
       const url = await browser.getCurrentUrl();
       log.debug(url);
       if (!url.includes('kibana')) {
@@ -136,35 +133,6 @@ export default ({ getService, getPageObjects }) => {
       await PageObjects.settings.createIndexPattern('*:makelogs工程-*', '@timestamp', true, false);
       const patternName = await PageObjects.settings.getIndexPageHeading();
       expect(patternName).to.be('*:makelogs工程-*');
-    });
-
-    it('create local siem signals index pattern', async () => {
-      log.debug('Add index pattern: .siem-signals-*');
-      await supertest
-        .post('/api/index_patterns/index_pattern')
-        .set('kbn-xsrf', 'true')
-        .send({
-          index_pattern: {
-            title: '.siem-signals-*',
-          },
-          override: true,
-        })
-        .expect(200);
-    });
-
-    it('create remote monitoring ES index pattern', async () => {
-      log.debug('Add index pattern: data:.monitoring-es-*');
-      await supertest
-        .post('/api/index_patterns/index_pattern')
-        .set('kbn-xsrf', 'true')
-        .send({
-          index_pattern: {
-            title: 'data:.monitoring-es-*',
-            timeFieldName: 'timestamp',
-          },
-          override: true,
-        })
-        .expect(200);
     });
 
     it('local:makelogs(star) should discover data from the local cluster', async () => {
@@ -236,34 +204,123 @@ export default ({ getService, getPageObjects }) => {
       });
     });
 
-    it('should generate alerts based on remote events', async () => {
-      log.debug('Add detection rule type:shards on data:.monitoring-es-*');
-      await supertest
-        .post('/api/detection_engine/rules')
-        .set('kbn-xsrf', 'true')
-        .send({
-          description: 'This is the description of the rule',
-          risk_score: 17,
-          severity: 'low',
-          interval: '10s',
-          name: 'CCS_Detection_test',
-          type: 'query',
-          from: 'now-1d',
-          index: ['data:.monitoring-es-*'],
-          timestamp_override: 'timestamp',
-          query: 'type:shards',
-          language: 'kuery',
-          enabled: true,
-        })
-        .expect(200);
+    describe('Detection engine', async function () {
+      const supertest = getService('supertest');
+      const esSupertest = getService('esSupertest');
 
-      log.debug('Check if any alert got to .siem-signals-*');
-      await PageObjects.common.navigateToApp('discover', { insertTimestamp: false });
-      await PageObjects.discover.selectIndexPattern('.siem-signals-*');
-      await retry.tryForTime(40000, async () => {
-        const hitCount = await PageObjects.discover.getHitCount();
-        log.debug('### hit count = ' + hitCount);
-        expect(hitCount).to.be.greaterThan('0');
+      let signalsId;
+      let dataId;
+      let ruleId;
+
+      before('Prepare .siem-signal-*', async function () {
+        log.info('Create index');
+        // visit app/security so to create .siem-signals-* as side effect
+        await PageObjects.common.navigateToApp('security', { insertTimestamp: false });
+
+        log.info('Create index pattern');
+        signalsId = await supertest
+          .post('/api/index_patterns/index_pattern')
+          .set('kbn-xsrf', 'true')
+          .send({
+            index_pattern: {
+              title: '.siem-signals-*',
+            },
+            override: true,
+          })
+          .expect(200)
+          .then((res) => JSON.parse(res.text).index_pattern.id);
+        log.debug('id: ' + signalsId);
+      });
+
+      before('Prepare data:.monitoring-es-*', async function () {
+        log.info('Create index pattern');
+        dataId = await supertest
+          .post('/api/index_patterns/index_pattern')
+          .set('kbn-xsrf', 'true')
+          .send({
+            index_pattern: {
+              title: 'data:.monitoring-es-*',
+              timeFieldName: 'timestamp',
+            },
+            override: true,
+          })
+          .expect(200)
+          .then((res) => JSON.parse(res.text).index_pattern.id);
+        log.debug('id: ' + dataId);
+      });
+
+      before('Add detection rule', async function () {
+        ruleId = await supertest
+          .post('/api/detection_engine/rules')
+          .set('kbn-xsrf', 'true')
+          .send({
+            description: 'This is the description of the rule',
+            risk_score: 17,
+            severity: 'low',
+            interval: '10s',
+            name: 'CCS_Detection_test',
+            type: 'query',
+            from: 'now-1d',
+            index: ['data:.monitoring-es-*'],
+            timestamp_override: 'timestamp',
+            query: 'type:shards',
+            language: 'kuery',
+            enabled: true,
+          })
+          .expect(200)
+          .then((res) => JSON.parse(res.text).id);
+        log.debug('id: ' + ruleId);
+      });
+
+      after('Clean up detection rule', async function () {
+        if (ruleId !== undefined) {
+          log.debug('id: ' + ruleId);
+          await supertest
+            .delete('/api/detection_engine/rules?id=' + ruleId)
+            .set('kbn-xsrf', 'true')
+            .expect(200);
+        }
+      });
+
+      after('Clean up data:.monitoring-es-*', async function () {
+        if (dataId !== undefined) {
+          log.info('Delete index pattern');
+          log.debug('id: ' + dataId);
+          await supertest
+            .delete('/api/index_patterns/index_pattern/' + dataId)
+            .set('kbn-xsrf', 'true')
+            .expect(200);
+        }
+      });
+
+      after('Clean up .siem-signal-*', async function () {
+        if (signalsId !== undefined) {
+          log.info('Delete index pattern: .siem-signals-*');
+          log.debug('id: ' + signalsId);
+          await supertest
+            .delete('/api/index_patterns/index_pattern/' + signalsId)
+            .set('kbn-xsrf', 'true')
+            .expect(200);
+        }
+
+        log.info('Delete index alias: .siem-signals-default');
+        await esSupertest
+          .delete('/.siem-signals-default-000001/_alias/.siem-signals-default')
+          .expect(200);
+
+        log.info('Delete index: .siem-signals-default-000001');
+        await esSupertest.delete('/.siem-signals-default-000001').expect(200);
+      });
+
+      it('Should generate alerts based on remote events', async function () {
+        log.info('Check if any alert got to .siem-signals-*');
+        await PageObjects.common.navigateToApp('discover', { insertTimestamp: false });
+        await PageObjects.discover.selectIndexPattern('.siem-signals-*');
+        await retry.tryForTime(40000, async () => {
+          const hitCount = await PageObjects.discover.getHitCount();
+          log.debug('### hit count = ' + hitCount);
+          expect(hitCount).to.be.greaterThan('0');
+        });
       });
     });
   });
