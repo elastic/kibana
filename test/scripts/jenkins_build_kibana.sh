29bc88b8dde2c82db39de82f3752f993a309a746
Tyler Smalley <tyler.smalley@elastic.co>
2021-05-12 12:24:25 -0700

    [FTR][CI] Use default distribution for all tests (#94968)
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- test/scripts/jenkins_build_kibana.sh
+++ test/scripts/jenkins_build_kibana.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cd "$KIBANA_DIR"
 source src/dev/ci_setup/setup_env.sh
 
 if [[ ! "$TASK_QUEUE_PROCESS_ID" ]]; then
@@ -12,13 +13,30 @@ export KBN_NP_PLUGINS_BUILT=true
 echo " -> Ensuring all functional tests are in a ciGroup"
 node scripts/ensure_all_tests_in_ci_group;
 
-echo " -> building and extracting OSS Kibana distributable for use in functional tests"
-node scripts/build --debug --oss
+echo " -> Ensuring all x-pack functional tests are in a ciGroup"
+node x-pack/scripts/functional_tests --assert-none-excluded \
+  --include-tag ciGroup1 \
+  --include-tag ciGroup2 \
+  --include-tag ciGroup3 \
+  --include-tag ciGroup4 \
+  --include-tag ciGroup5 \
+  --include-tag ciGroup6 \
+  --include-tag ciGroup7 \
+  --include-tag ciGroup8 \
+  --include-tag ciGroup9 \
+  --include-tag ciGroup10 \
+  --include-tag ciGroup11 \
+  --include-tag ciGroup12 \
+  --include-tag ciGroup13 \
+  --include-tag ciGroupDocker
+
+echo " -> building and extracting default Kibana distributable for use in functional tests"
+node scripts/build --debug --no-oss
 
 echo " -> shipping metrics from build to ci-stats"
 node scripts/ship_ci_stats \
   --metrics target/optimizer_bundle_metrics.json \
   --metrics packages/kbn-ui-shared-deps/target/metrics.json
 
-mkdir -p "$WORKSPACE/kibana-build-oss"
-cp -pR build/oss/kibana-*-SNAPSHOT-linux-x86_64/. $WORKSPACE/kibana-build-oss/
+mkdir -p "$WORKSPACE/kibana-build"
+cp -pR build/oss/kibana-*-SNAPSHOT-linux-x86_64/. $WORKSPACE/kibana-build/
