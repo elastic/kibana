9fc4772b9c2d8e2b354dbb8a92be500fb5dbeaf9
Christos Nasikas <christos.nasikas@elastic.co>
2021-05-13 12:27:51 +0300

    Throw an error if owner context is not set

--- x-pack/plugins/cases/public/components/owner_context/use_owner_context.ts
+++ x-pack/plugins/cases/public/components/owner_context/use_owner_context.ts
@@ -9,5 +9,13 @@ import { useContext } from 'react';
 import { OwnerContext } from '.';
 
 export const useOwnerContext = () => {
-  return useContext(OwnerContext);
+  const ownerContext = useContext(OwnerContext);
+
+  if (ownerContext.length === 0) {
+    throw new Error(
+      'useOwnerContext must be used within a OwnerProvider and not be an empty array'
+    );
+  }
+
+  return ownerContext;
 };
