bd39b45cddac785db3a16b97d3f63d649a6a56de
Elizabeth Jarrett <m.elizabeth.jarrett@gmail.com>
2021-05-13 13:46:49 -0600

    Additional cleanup

--- scripts/removeOldKibanaIndices.py
+++ scripts/removeOldKibanaIndices.py
@@ -4,18 +4,18 @@ logFile = open('/var/log/probe/KibanaStartup.log', 'w')
 
 res = requests.get('http://localhost:9200/_cat/indices/.kibana_*?s=index')
 content = res.content
-kibanaIndexNums = []
+kibanaIndices = []
 
 for indexData in content.split(" "):
    if ".kibana" in indexData:
-      kibanaIndexNums.append(indexData)
+      kibanaIndices.append(indexData)
 
-print >>logFile, "Removing .kibana_1 index and current daily index .kibana_{} from list to delete.".format(kibanaIndexNums[-1])
-kibanaIndexNums.remove(".kibana_1")
-kibanaIndexNums = kibanaIndexNums[:-1]
-print >>logFile, "Deleting .kibana_* indices for: {}".format(kibanaIndexNums)
+print >>logFile, "Removing .kibana_1 index and current index {} from list to delete.".format(kibanaIndices[-1])
+kibanaIndices.remove(".kibana_1")
+kibanaIndices = kibanaIndices[:-1]
+print >>logFile, "Deleting indices: {}".format(kibanaIndices)
 
-for index in kibanaIndexNums:
+for index in kibanaIndices:
    url = "http://localhost:9200/{}".format(index)
    res = requests.delete(url)
    if res.status_code != 200:
