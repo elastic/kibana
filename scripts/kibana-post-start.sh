db2a6a4b6cc6e5c7cd97b225eb5c25e77e834f13
Elizabeth Jarrett <m.elizabeth.jarrett@gmail.com>
2021-05-13 13:22:41 -0600

    Address PR comments

--- scripts/kibana-post-start.sh
+++ scripts/kibana-post-start.sh
@@ -8,6 +8,3 @@ do
    /usr/bin/sleep 1
 done
 echo "[INFO] Kibana is reachable and returned a 200 status" >> $KIBANA_STARTUP_LOG
-
-# Call script to clean up old .kibana_* indices
-python /usr/local/kibana-7.5.2-linux-x64/scripts/rmOldKibanaIndices.py
--- scripts/removeOldKibanaIndices.py
+++ scripts/removeOldKibanaIndices.py
@@ -2,26 +2,21 @@ import requests
 
 logFile = open('/var/log/probe/KibanaStartup.log', 'w')
 
-res = requests.get('http://localhost:9200/_cat/indices')
+res = requests.get('http://localhost:9200/_cat/indices/.kibana_*?s=index')
 content = res.content
 kibanaIndexNums = []
 
 for indexData in content.split(" "):
-   if ".kibana" not in indexData:
-      continue
-   for term in indexData.split("_"):
-      if ".kibana" in term:
-         continue
-      kibanaIndexNums.append(int(term))
+   if ".kibana" in indexData:
+      kibanaIndexNums.append(indexData)
 
-kibanaIndexNums.remove(1)
-kibanaIndexNums = sorted(kibanaIndexNums)
 print >>logFile, "Removing .kibana_1 index and current daily index .kibana_{} from list to delete.".format(kibanaIndexNums[-1])
+kibanaIndexNums.remove(".kibana_1")
 kibanaIndexNums = kibanaIndexNums[:-1]
 print >>logFile, "Deleting .kibana_* indices for: {}".format(kibanaIndexNums)
 
 for index in kibanaIndexNums:
-   url = "http://localhost:9200/.kibana_{}".format(index)
+   url = "http://localhost:9200/{}".format(index)
    res = requests.delete(url)
    if res.status_code != 200:
       print >>logFile, "Could not delete {}".format(url)
