004938e344d0e920f98b49e79373984be8dcc3d8
Ying Mao <ying.mao@elastic.co>
2021-05-13 11:38:47 -0400

    Changing schema of alerting feature privilege

--- x-pack/plugins/stack_alerts/server/feature.ts
+++ x-pack/plugins/stack_alerts/server/feature.ts
@@ -6,13 +6,14 @@
  */
 
 import { i18n } from '@kbn/i18n';
+import { KibanaFeatureConfig } from '../../../plugins/features/common';
 import { ID as IndexThreshold } from './alert_types/index_threshold/alert_type';
 import { GEO_CONTAINMENT_ID as GeoContainment } from './alert_types/geo_containment/alert_type';
 import { ES_QUERY_ID as ElasticsearchQuery } from './alert_types/es_query/alert_type';
 import { STACK_ALERTS_FEATURE_ID } from '../common';
 import { DEFAULT_APP_CATEGORIES } from '../../../../src/core/server';
 
-export const BUILT_IN_ALERTS_FEATURE = {
+export const BUILT_IN_ALERTS_FEATURE: KibanaFeatureConfig = {
   id: STACK_ALERTS_FEATURE_ID,
   name: i18n.translate('xpack.stackAlerts.featureRegistry.actionsFeatureName', {
     defaultMessage: 'Stack Rules',
@@ -31,13 +32,11 @@ export const BUILT_IN_ALERTS_FEATURE = {
         insightsAndAlerting: ['triggersActions'],
       },
       alerting: {
-        all: {
-          rule: [IndexThreshold, GeoContainment, ElasticsearchQuery],
-          alert: [],
+        rule: {
+          all: [IndexThreshold, GeoContainment, ElasticsearchQuery],
         },
-        read: {
-          rule: [],
-          alert: [IndexThreshold, GeoContainment, ElasticsearchQuery],
+        alert: {
+          read: [IndexThreshold, GeoContainment, ElasticsearchQuery],
         },
       },
       savedObject: {
@@ -54,13 +53,11 @@ export const BUILT_IN_ALERTS_FEATURE = {
         insightsAndAlerting: ['triggersActions'],
       },
       alerting: {
-        all: {
-          rule: [],
-          alert: [],
+        rule: {
+          read: [IndexThreshold, GeoContainment, ElasticsearchQuery],
         },
-        read: {
-          rule: [IndexThreshold, GeoContainment, ElasticsearchQuery],
-          alert: [IndexThreshold, GeoContainment, ElasticsearchQuery],
+        alert: {
+          read: [IndexThreshold, GeoContainment, ElasticsearchQuery],
         },
       },
       savedObject: {
@@ -83,13 +80,8 @@ export const BUILT_IN_ALERTS_FEATURE = {
               name: 'Manage Alerts',
               includeIn: 'all',
               alerting: {
-                all: {
-                  rule: [],
-                  alert: [IndexThreshold, GeoContainment, ElasticsearchQuery],
-                },
-                read: {
-                  rule: [],
-                  alert: [],
+                alert: {
+                  all: [IndexThreshold, GeoContainment, ElasticsearchQuery],
                 },
               },
               savedObject: {
