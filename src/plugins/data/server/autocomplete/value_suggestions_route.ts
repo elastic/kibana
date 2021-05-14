f5812734ab2b0ba3cdc2c1d1ca59c99d2aac6d71
Lukas Olson <olson.lukas@gmail.com>
2021-05-14 14:51:19 -0700

    Use new terms enum API for autocomplete value suggestions

--- src/plugins/data/server/autocomplete/value_suggestions_route.ts
+++ src/plugins/data/server/autocomplete/value_suggestions_route.ts
@@ -6,16 +6,15 @@
  * Side Public License, v 1.
  */
 
-import { get, map } from 'lodash';
 import { schema } from '@kbn/config-schema';
 import { IRouter, SharedGlobalConfig } from 'kibana/server';
 
 import { Observable } from 'rxjs';
-import { first } from 'rxjs/operators';
-import type { estypes } from '@elastic/elasticsearch';
 import type { IFieldType } from '../index';
 import { findIndexPatternById, getFieldByName } from '../index_patterns';
 import { getRequestAbortedSignal } from '../lib';
+import { getKbnServerError } from '../../../kibana_utils/server';
+import { shimAbortSignal } from '../index';
 
 export function registerValueSuggestionsRoute(
   router: IRouter,
@@ -43,16 +42,9 @@ export function registerValueSuggestionsRoute(
       },
     },
     async (context, request, response) => {
-      const config = await config$.pipe(first()).toPromise();
       const { field: fieldName, query, filters, fieldMeta } = request.body;
       const { index } = request.params;
-      const { client } = context.core.elasticsearch.legacy;
       const signal = getRequestAbortedSignal(request.events.aborted$);
-
-      const autocompleteSearchOptions = {
-        timeout: `${config.kibana.autocompleteTimeout.asMilliseconds()}ms`,
-        terminate_after: config.kibana.autocompleteTerminateAfter.asMilliseconds(),
-      };
 
       let field: IFieldType | undefined = fieldMeta;
 
@@ -62,72 +54,34 @@ export function registerValueSuggestionsRoute(
         field = indexPattern && getFieldByName(fieldName, indexPattern);
       }
 
-      const body = await getBody(autocompleteSearchOptions, field || fieldName, query, filters);
+      try {
+        const promise = context.core.elasticsearch.client.asCurrentUser.transport.request({
+          method: 'POST',
+          path: encodeURI(`/${index}/_terms_enum`),
+          body: {
+            field: field?.name ?? field,
+            string: query,
+            index_filter: {
+              bool: {
+                must: [
+                  ...filters,
+                  {
+                    terms: {
+                      _tier: ['data_warm', 'data_hot'],
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        });
 
-      const result = await client.callAsCurrentUser('search', { index, body }, { signal });
+        const result = await shimAbortSignal(promise, signal);
 
-      const buckets: any[] =
-        get(result, 'aggregations.suggestions.buckets') ||
-        get(result, 'aggregations.nestedSuggestions.suggestions.buckets');
-
-      return response.ok({ body: map(buckets || [], 'key') });
+        return response.ok({ body: result.body.terms });
+      } catch (e) {
+        throw getKbnServerError(e);
+      }
     }
   );
-}
-
-async function getBody(
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  { timeout, terminate_after }: Record<string, any>,
-  field: IFieldType | string,
-  query: string,
-  filters: estypes.QueryContainer[] = []
-) {
-  const isFieldObject = (f: any): f is IFieldType => Boolean(f && f.name);
-
-  // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-regexp-query.html#_standard_operators
-  const getEscapedQuery = (q: string = '') =>
-    q.replace(/[.?+*|{}[\]()"\\#@&<>~]/g, (match) => `\\${match}`);
-
-  // Helps ensure that the regex is not evaluated eagerly against the terms dictionary
-  const executionHint = 'map' as const;
-
-  // We don't care about the accuracy of the counts, just the content of the terms, so this reduces
-  // the amount of information that needs to be transmitted to the coordinating node
-  const shardSize = 10;
-  const body = {
-    size: 0,
-    timeout,
-    terminate_after,
-    query: {
-      bool: {
-        filter: filters,
-      },
-    },
-    aggs: {
-      suggestions: {
-        terms: {
-          field: isFieldObject(field) ? field.name : field,
-          include: `${getEscapedQuery(query)}.*`,
-          execution_hint: executionHint,
-          shard_size: shardSize,
-        },
-      },
-    },
-  };
-
-  if (isFieldObject(field) && field.subType && field.subType.nested) {
-    return {
-      ...body,
-      aggs: {
-        nestedSuggestions: {
-          nested: {
-            path: field.subType.nested.path,
-          },
-          aggs: body.aggs,
-        },
-      },
-    };
-  }
-
-  return body;
 }
