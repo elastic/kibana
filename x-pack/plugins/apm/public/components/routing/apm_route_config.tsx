0a88079a89698c1f07333c86a33620837e5430da
Dario Gieselaar <dario.gieselaar@elastic.co>
2021-05-18 16:39:19 +0200

    [APM] link-to/transaction

--- x-pack/plugins/apm/public/components/routing/apm_route_config.tsx
+++ x-pack/plugins/apm/public/components/routing/apm_route_config.tsx
@@ -20,6 +20,7 @@ import { AnomalyDetection } from '../app/Settings/anomaly_detection';
 import { ApmIndices } from '../app/Settings/ApmIndices';
 import { CustomizeUI } from '../app/Settings/CustomizeUI';
 import { TraceLink } from '../app/TraceLink';
+import { TransactionLink } from '../app/transaction_link';
 import { TransactionDetails } from '../app/transaction_details';
 import { enableServiceOverview } from '../../../common/ui_settings_keys';
 import { redirectTo } from './redirect_to';
@@ -508,6 +509,12 @@ export const apmRouteConfig: APMRouteDefinition[] = [
     exact: true,
     path: '/link-to/trace/:traceId',
     component: TraceLink,
+    breadcrumb: null,
+  },
+  {
+    exact: true,
+    path: '/link-to/transaction/:transactionId',
+    component: TransactionLink,
     breadcrumb: null,
   },
 ];
