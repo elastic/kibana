deb794c600c5f4bcc3b37f2d892fed21f0718b28
Justin Kambic <justin.kambic@elastic.co>
2021-05-18 10:22:01 -0400

    Move uptime actions to Kibana's HeaderActionsMenu.

--- x-pack/plugins/uptime/public/components/common/header/action_menu.tsx
+++ x-pack/plugins/uptime/public/components/common/header/action_menu.tsx
@@ -8,6 +8,8 @@
 import React from 'react';
 import { EuiButtonEmpty, EuiFlexGroup, EuiFlexItem, EuiToolTip } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n/react';
+import { useHistory } from 'react-router-dom';
 import { useKibana } from '../../../../../../../src/plugins/kibana_react/public';
 import {
   createExploratoryViewUrl,
@@ -17,6 +19,9 @@ import {
 import { AppMountParameters } from '../../../../../../../src/core/public';
 import { useUptimeSettingsContext } from '../../../contexts/uptime_settings_context';
 import { useGetUrlParams } from '../../../hooks';
+import { ToggleAlertFlyoutButton } from '../../overview/alerts/alerts_containers';
+import { SETTINGS_ROUTE } from '../../../../common/constants';
+import { stringifyUrlParams } from '../../../lib/helper/stringify_url_params';
 
 const ADD_DATA_LABEL = i18n.translate('xpack.uptime.addDataButtonLabel', {
   defaultMessage: 'Add data',
@@ -34,7 +39,9 @@ const ANALYZE_MESSAGE = i18n.translate('xpack.uptime.analyzeDataButtonLabel.mess
 export const ActionMenu = ({ appMountParameters }: { appMountParameters: AppMountParameters }) => {
   const kibana = useKibana();
   const { basePath } = useUptimeSettingsContext();
-  const { dateRangeStart, dateRangeEnd } = useGetUrlParams();
+  const params = useGetUrlParams();
+  const { dateRangeStart, dateRangeEnd } = params;
+  const history = useHistory();
 
   const syntheticExploratoryViewLink = createExploratoryViewUrl(
     {
@@ -50,6 +57,25 @@ export const ActionMenu = ({ appMountParameters }: { appMountParameters: AppMoun
     <HeaderMenuPortal setHeaderActionMenu={appMountParameters.setHeaderActionMenu}>
       <EuiFlexGroup alignItems="flexEnd" responsive={false} style={{ paddingRight: 20 }}>
         <EuiFlexItem>
+          <ToggleAlertFlyoutButton />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiButtonEmpty
+            color="primary"
+            data-test-subj="settings-page-link"
+            href={history.createHref({
+              pathname: SETTINGS_ROUTE,
+              search: stringifyUrlParams(params, true),
+            })}
+            iconType="gear"
+          >
+            <FormattedMessage
+              id="xpack.uptime.page_header.settingsLink"
+              defaultMessage="Settings"
+            />
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
           <EuiToolTip position="top" content={<p>{ANALYZE_MESSAGE}</p>}>
             <EuiButtonEmpty
               href={syntheticExploratoryViewLink}
