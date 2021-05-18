c4ef7873c48d62bdfcdf6bf842b7a22c40070f5e
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-05-18 17:45:22 +0300

    Tests added.
    Tests for getLastValue refactored.
    Tests for getLastValueOrDefault and getLastValueOrZero written.

--- src/plugins/vis_type_timeseries/common/get_last_value.test.js
+++ src/plugins/vis_type_timeseries/common/get_last_value.test.js
@@ -6,35 +6,100 @@
  * Side Public License, v 1.
  */
 
-import { getLastValue } from './get_last_value';
+import {
+  getLastValue,
+  getLastValueOrDefault,
+  getLastValueOrZero,
+  DEFAULT_VALUE,
+} from './get_last_value';
 
 describe('getLastValue(data)', () => {
-  test('should returns data if data is not array', () => {
-    expect(getLastValue('foo')).toBe('foo');
+  test('should return undefined ', () => {
+    expect(getLastValue()).toBeUndefined();
   });
 
-  test('should returns 0 as a value when not an array', () => {
+  test('should return data, if data is not an array', () => {
+    const data = 'foo';
+    expect(getLastValue(data)).toBe(data);
+  });
+
+  test('should return 0 as a value, when data is not an array', () => {
     expect(getLastValue(0)).toBe(0);
   });
 
-  test('should returns the last value', () => {
-    expect(getLastValue([[1, 2]])).toBe(2);
+  test('should return the last value', () => {
+    const lastVal = 2;
+    expect(getLastValue([[1, lastVal]])).toBe(lastVal);
   });
 
   test('should return 0 as a valid value', () => {
     expect(getLastValue([[0, 0]])).toBe(0);
   });
 
-  test('should returns the default value ', () => {
-    expect(getLastValue()).toBe('-');
-  });
-
-  test('should returns 0 if second to last is not defined (default)', () => {
+  test("should return null, if second array is empty or it's last element is null/undefined (default)", () => {
     expect(
       getLastValue([
         [1, null],
         [2, null],
       ])
-    ).toBe('-');
+    ).toBeNull();
+
+    expect(
+      getLastValue([
+        [1, null],
+        [2, undefined],
+      ])
+    ).toBeNull();
+  });
+});
+
+describe('getLastValueOrDefault(data)', () => {
+  test('should return the default value', () => {
+    expect(getLastValueOrDefault()).toBe(DEFAULT_VALUE);
+  });
+
+  test('should return the passed default value', () => {
+    const defVal = 'DEFAULT';
+    expect(getLastValueOrDefault(undefined, defVal)).toBe(defVal);
+  });
+
+  test('should return the last value', () => {
+    const lastValue = 10;
+    const data = [[1, lastValue]];
+    expect(getLastValueOrDefault(data)).toBe(lastValue);
+  });
+
+  test(`should return default value, if second array is empty or it's last element is null/undefined (default)`, () => {
+    const data = [[1, null]];
+    expect(getLastValueOrDefault(data)).toBe(DEFAULT_VALUE);
+  });
+
+  test(`should return passed default value, if second array is empty or it's last element is null/undefined (default)`, () => {
+    let data = [[1, null]];
+    const defVal = 'DEFAULT';
+    expect(getLastValueOrDefault(data, defVal)).toBe(defVal);
+
+    data = [[1, undefined]];
+    expect(getLastValueOrDefault(data, defVal)).toBe(defVal);
+  });
+});
+
+describe('getLastValueOrZero(data)', () => {
+  test('should return 0', () => {
+    expect(getLastValueOrZero()).toBe(0);
+  });
+
+  test('should return the last value', () => {
+    const lastValue = 10;
+    const data = [[1, lastValue]];
+    expect(getLastValueOrZero(data)).toBe(lastValue);
+  });
+
+  test(`should return zero, if second array is empty or it's last element is null/undefined (default)`, () => {
+    let data = [[1, null]];
+    expect(getLastValueOrZero(data)).toBe(0);
+
+    data = [[1, undefined]];
+    expect(getLastValueOrZero(data)).toBe(0);
   });
 });
