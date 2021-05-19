f8ce3ed16eb804ddca67d04d4b4619b59509c806
Paul Tavares <paul.tavares@elastic.co>
2021-05-19 12:57:41 -0400

    Reusable mocks for host isolation api service

--- x-pack/plugins/security_solution/public/common/lib/host_isolation/index.test.ts
+++ x-pack/plugins/security_solution/public/common/lib/host_isolation/index.test.ts
@@ -7,30 +7,21 @@
 
 import { KibanaServices } from '../kibana';
 import { coreMock } from '../../../../../../../src/core/public/mocks';
-import { HostIsolationRequestBody } from '../../../../common/endpoint/types';
 import { isolateHost, unIsolateHost } from './index';
 import { ISOLATE_HOST_ROUTE, UNISOLATE_HOST_ROUTE } from '../../../../common/endpoint/constants';
+import { hostIsolationRequestBodyMock } from './mocks';
 
 jest.mock('../kibana');
 
 describe('When using Host Isolation library', () => {
   const mockKibanaServices = KibanaServices.get as jest.Mock;
-  const generateActionPayloadMock = (): HostIsolationRequestBody => {
-    return {
-      agent_ids: ['fd8a122b-4c54-4c05-b295-111'],
-      endpoint_ids: ['88c04a90-b19c-11eb-b838-222'],
-      alert_ids: ['88c04a90-b19c-11eb-b838-333'],
-      case_ids: ['88c04a90-b19c-11eb-b838-444'],
-      comment: 'Lock it',
-    };
-  };
 
   beforeEach(() => {
     mockKibanaServices.mockReturnValue(coreMock.createStart({ basePath: '/mock' }));
   });
 
   it('should send an isolate POST request', async () => {
-    const requestBody = generateActionPayloadMock();
+    const requestBody = hostIsolationRequestBodyMock();
     await isolateHost(requestBody);
 
     expect(mockKibanaServices().http.post).toHaveBeenCalledWith(ISOLATE_HOST_ROUTE, {
@@ -39,7 +30,7 @@ describe('When using Host Isolation library', () => {
   });
 
   it('should send an un-isolate POST request', async () => {
-    const requestBody = generateActionPayloadMock();
+    const requestBody = hostIsolationRequestBodyMock();
     await unIsolateHost(requestBody);
 
     expect(mockKibanaServices().http.post).toHaveBeenCalledWith(UNISOLATE_HOST_ROUTE, {
