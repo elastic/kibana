291113caa7a66f84d1bb557156f0eb60ee5e5b36
Paul Tavares <paul.tavares@elastic.co>
2021-05-25 14:25:35 -0400

    Adjust unisolate panel

--- x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/details/components/endpoint_unisolate_flyout_panel.tsx
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/details/components/endpoint_unisolate_flyout_panel.tsx
@@ -7,8 +7,16 @@
 
 import React, { memo } from 'react';
 import { HostMetadata } from '../../../../../../../common/endpoint/types';
+import { BackToEndpointDetailsFlyoutSubHeader } from './back_to_endpoint_details_flyout_subheader';
+import { FlyoutBodyNoTopPadding } from './flyout_body_no_top_padding';
 
 export const EndpointUnIsolateFlyoutPanel = memo<{ hostMeta: HostMetadata }>(({ hostMeta }) => {
-  return <div>{'WIP'}</div>;
+  return (
+    <>
+      <BackToEndpointDetailsFlyoutSubHeader endpointId={hostMeta.agent.id} />
+
+      <FlyoutBodyNoTopPadding>{'WIP'}</FlyoutBodyNoTopPadding>
+    </>
+  );
 });
 EndpointUnIsolateFlyoutPanel.displayName = 'EndpointUnIsolateFlyoutPanel';
