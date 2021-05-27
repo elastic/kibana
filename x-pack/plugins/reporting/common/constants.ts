587de77cdbd3f2525ec0db4726ddb156f1d6d8e7
Jean-Louis Leysens <jloleysens@gmail.com>
2021-05-27 14:58:16 +0200

    very wip

--- x-pack/plugins/reporting/common/constants.ts
+++ x-pack/plugins/reporting/common/constants.ts
@@ -63,9 +63,11 @@ export const CSV_JOB_TYPE = 'csv_searchsource';
 
 export const PDF_REPORT_TYPE = 'printablePdf';
 export const PDF_JOB_TYPE = 'printable_pdf';
+export const PDF_JOB_TYPE_V2 = 'printable_pdf_v2';
 
 export const PNG_REPORT_TYPE = 'PNG';
 export const PNG_JOB_TYPE = 'PNG';
+export const PNG_JOB_TYPE_V2 = 'PNG_v2';
 
 export const CSV_SEARCHSOURCE_IMMEDIATE_TYPE = 'csv_searchsource_immediate';
 
---
+++ x-pack/plugins/reporting/server/export_types/printable_pdf/v2/create_job.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { cryptoFactory } from '../../../lib';
+import { CreateJobFn, CreateJobFnFactory } from '../../../types';
+import { JobParamsPDFV2, TaskPayloadPDFV2 } from './types';
+
+export const createJobFnFactory: CreateJobFnFactory<
+  CreateJobFn<JobParamsPDFV2, TaskPayloadPDFV2>
+> = function createJobFactoryFn(reporting, logger) {
+  const config = reporting.getConfig();
+  const crypto = cryptoFactory(config.get('encryptionKey'));
+
+  return async function createJob(
+    { title, locator, browserTimezone, layout, objectType },
+    context,
+    req
+  ) {
+    const serializedEncryptedHeaders = await crypto.encrypt(req.headers);
+
+    return {
+      headers: serializedEncryptedHeaders,
+      spaceId: reporting.getSpaceId(req, logger),
+      browserTimezone,
+      forceNow: new Date().toISOString(),
+      layout,
+      locator,
+      title,
+      objectType,
+    };
+  };
+};
