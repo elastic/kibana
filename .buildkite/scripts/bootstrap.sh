bd6b0826e5955ffd8476b4b1143453a9f9dbabc6
Brian Seeders <brian.seeders@elastic.co>
2021-05-27 11:50:17 -0400

    Remove if for bootstrap

--- .buildkite/scripts/bootstrap.sh
+++ .buildkite/scripts/bootstrap.sh
@@ -4,21 +4,19 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
-if [[ ! -f "target/.bootstrap-cache" ]]; then
-  echo "--- yarn install and bootstrap"
-  yarn kbn bootstrap --verbose
+echo "--- yarn install and bootstrap"
+yarn kbn bootstrap --verbose
 
-  ###
-  ### upload ts-refs-cache artifacts as quickly as possible so they are available for download
-  ###
-  if [[ "${BUILD_TS_REFS_CACHE_CAPTURE:-}" == "true" ]]; then
-    echo "--- Upload ts-refs-cache"
-    cd "$KIBANA_DIR/target/ts_refs_cache"
-    gsutil cp "*.zip" 'gs://kibana-ci-ts-refs-cache/'
-    cd "$KIBANA_DIR"
-  fi
+###
+### upload ts-refs-cache artifacts as quickly as possible so they are available for download
+###
+if [[ "${BUILD_TS_REFS_CACHE_CAPTURE:-}" == "true" ]]; then
+  echo "--- Upload ts-refs-cache"
+  cd "$KIBANA_DIR/target/ts_refs_cache"
+  gsutil cp "*.zip" 'gs://kibana-ci-ts-refs-cache/'
+  cd "$KIBANA_DIR"
+fi
 
-  if [[ "$DISABLE_BOOTSTRAP_VALIDATION" != "true" ]]; then
-    verify_no_git_changes 'yarn kbn bootstrap'
-  fi
+if [[ "$DISABLE_BOOTSTRAP_VALIDATION" != "true" ]]; then
+  verify_no_git_changes 'yarn kbn bootstrap'
 fi
