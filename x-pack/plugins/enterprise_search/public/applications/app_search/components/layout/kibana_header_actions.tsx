ea8d123acfce7f7dc5a1016c244821deda6cba2a
Jason Stoltzfus <jastoltz24@gmail.com>
2021-05-27 06:57:20 -0400

    Remove a thing

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/layout/kibana_header_actions.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/layout/kibana_header_actions.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 
 import { useValues } from 'kea';
 
-import { EuiButtonEmpty, EuiFlexGroup, EuiFlexItem, EuiHeaderLinks } from '@elastic/eui';
+import { EuiButtonEmpty, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
 import { EngineLogic } from '../engine';
