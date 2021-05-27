3f95e874cc3d6b8c4cb5371558bad81c01340e7a
Brian Seeders <brian.seeders@elastic.co>
2021-05-27 10:45:11 -0400

    API docs needs to run after TS refs are built

--- .buildkite/scripts/steps/baseline_public_api_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-.buildkite/scripts/bootstrap.sh
-
-echo "--- Build API Docs"
-node scripts/build_api_docs
--- .buildkite/scripts/steps/baseline_ts_refs_api_docs.sh
+++ .buildkite/scripts/steps/baseline_ts_refs_api_docs.sh
@@ -8,3 +8,6 @@ export DISABLE_BOOTSTRAP_VALIDATION=true
 export BUILD_TS_REFS_DISABLE=false
 
 .buildkite/scripts/bootstrap.sh
+
+echo "--- Build API Docs"
+node scripts/build_api_docs
