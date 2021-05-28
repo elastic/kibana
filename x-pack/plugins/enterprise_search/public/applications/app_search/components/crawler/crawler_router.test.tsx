2e407afe64bf301671e67894e7a71e0ba194bbf8
Byron Hulcher <byronhulcher@gmail.com>
2021-05-28 09:17:19 -0400

    CrawlerRouter should use CrawlerOverview in dev mode

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/crawler_router.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/crawler_router.test.tsx
@@ -14,21 +14,32 @@ import { Switch } from 'react-router-dom';
 import { shallow } from 'enzyme';
 
 import { CrawlerLanding } from './crawler_landing';
+import { CrawlerOverview } from './crawler_overview';
 import { CrawlerRouter } from './crawler_router';
 
 describe('CrawlerRouter', () => {
+  const OLD_ENV = process.env;
+
   beforeEach(() => {
     setMockValues({ ...mockEngineValues });
   });
 
   afterEach(() => {
     jest.clearAllMocks();
+    process.env = OLD_ENV;
   });
 
-  it('renders a landing page', () => {
+  it('renders a landing page by default', () => {
     const wrapper = shallow(<CrawlerRouter />);
 
     expect(wrapper.find(Switch)).toHaveLength(1);
     expect(wrapper.find(CrawlerLanding)).toHaveLength(1);
+  });
+
+  it('renders a crawler overview in dev', () => {
+    process.env.NODE_ENV = 'development';
+    const wrapper = shallow(<CrawlerRouter />);
+
+    expect(wrapper.find(CrawlerOverview)).toHaveLength(1);
   });
 });
