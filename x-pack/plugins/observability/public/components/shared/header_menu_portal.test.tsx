6315b88d9be0b26fa76ca376a8e186b8c09fd09b
Nathan L Smith <smith@nlsmith.com>
2021-05-27 23:26:35 -0500

    Fix bug with Observability > APM header navigation
    Call `setHeaderActionMenu(undefined)` when the HeaderMenuPortal is unmounted.
    
    Found this line in the docs:
    
    > Calling the handler with `undefined` will unmount the current mount point.
    
    Which we weren't doing before.
    
    Previous behavior:
    
    * Go to /app/observability/alerts
    * Click the "View in app" button for an APM alert
    * Click back
    * Click the "View in app" button for an APM alert
    * Get a weird toast error message and the header menu is gone forever
    
    Now:
    
    * Go to /app/observability/alerts
    * Click the "View in app" button for an APM alert
    * Click back
    * Click the "View in app" button for an APM alert
    * Get a working header menu
    
    Fixes #97140

---
+++ x-pack/plugins/observability/public/components/shared/header_menu_portal.test.tsx
@@ -0,0 +1,22 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { render } from '@testing-library/react';
+import React from 'react';
+import HeaderMenuPortal from './header_menu_portal';
+
+describe('HeaderMenuPortal', () => {
+  describe('render', () => {
+    it('sets the header action menu', () => {
+      const setHeaderActionMenu = jest.fn();
+
+      render(<HeaderMenuPortal setHeaderActionMenu={setHeaderActionMenu}>test</HeaderMenuPortal>);
+
+      expect(setHeaderActionMenu).toHaveBeenCalled();
+    });
+  });
+});
