b7ebeaa1811209f598a21724e3f5f462df13cee5
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2021-05-28 15:00:51 -0400

    need to migrate page header and empty pages

--- x-pack/plugins/security_solution/public/app/home/index.tsx
+++ x-pack/plugins/security_solution/public/app/home/index.tsx
@@ -5,63 +5,20 @@
  * 2.0.
  */
 
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import styled from 'styled-components';
-import { EuiPanel } from '@elastic/eui';
+import React, { useRef } from 'react';
 
-import { TimelineId } from '../../../common/types/timeline';
 import { DragDropContextWrapper } from '../../common/components/drag_and_drop/drag_drop_context_wrapper';
-import { Flyout } from '../../timelines/components/flyout';
 import { SecuritySolutionAppWrapper } from '../../common/components/page';
 import { HelpMenu } from '../../common/components/help_menu';
-import { AutoSaveWarningMsg } from '../../timelines/components/timeline/auto_save_warning';
 import { UseUrlState } from '../../common/components/url_state';
-import { useShowTimeline } from '../../common/utils/timeline/use_show_timeline';
 import { navTabs } from './home_navigations';
 import { useInitSourcerer, useSourcererScope } from '../../common/containers/sourcerer';
 import { useKibana } from '../../common/lib/kibana';
 import { DETECTIONS_SUB_PLUGIN_ID } from '../../../common/constants';
 import { SourcererScopeName } from '../../common/store/sourcerer/model';
 import { useUpgradeEndpointPackage } from '../../common/hooks/endpoint/upgrade';
-import { useThrottledResizeObserver } from '../../common/components/utils';
 import { AppLeaveHandler, AppMountParameters } from '../../../../../../src/core/public';
-import { KibanaPageTemplate } from '../../../../../../src/plugins/kibana_react/public';
-import { MainNavigation } from './main_navigation';
 import { HeaderGlobal } from './header_global';
-import { IS_DRAGGING_CLASS_NAME } from '../../common/components/drag_and_drop/helpers';
-import { getTimelineShowStatusByIdSelector } from '../../timelines/components/flyout/selectors';
-import { useDeepEqualSelector } from '../../common/hooks/use_selector';
-import { KQLHeaderGlobal } from './kql_global';
-import { useMainNavigationVisibility } from './temp_collapse_sidenav_context';
-
-const Main = styled.main.attrs<{ paddingTop: number }>(({ paddingTop }) => ({
-  style: {
-    paddingTop: `${paddingTop}px`,
-  },
-}))<{ paddingTop: number }>`
-  overflow: auto;
-  display: flex;
-  flex-direction: column;
-  flex: 1 1 auto;
-`;
-
-const StyledKibanaPageTemplate = styled(KibanaPageTemplate)<{
-  $isShowingTimelineOverlay?: boolean;
-}>`
-  .timeline-bottom-bar {
-    background: #ffffff;
-    color: inherit;
-    left: 240px !important;
-    transform: ${({ $isShowingTimelineOverlay }) =>
-      $isShowingTimelineOverlay ? 'none' : 'translateY(calc(100% - 50px))'};
-    z-index: ${({ theme }) => theme.eui.euiZLevel8};
-
-    .${IS_DRAGGING_CLASS_NAME} & {
-      transform: none;
-    }
-  }
-`;
-
 interface HomePageProps {
   children: React.ReactNode;
   onAppLeave: (handler: AppLeaveHandler) => void;
@@ -73,22 +30,8 @@ const HomePageComponent: React.FC<HomePageProps> = ({
   onAppLeave,
   setHeaderActionMenu,
 }) => {
-  const { application, overlays } = useKibana().services;
+  const { application } = useKibana().services;
   const subPluginId = useRef<string>('');
-  const { ref, height = 0 } = useThrottledResizeObserver(300);
-  const banners$ = overlays.banners.get$();
-  const [headerFixed, setHeaderFixed] = useState<boolean>(true);
-  const mainPaddingTop = headerFixed ? height : 0;
-  const getTimelineShowStatus = useMemo(() => getTimelineShowStatusByIdSelector(), []);
-  const { show } = useDeepEqualSelector((state) => getTimelineShowStatus(state, TimelineId.active));
-
-  // TODO: Remove when collapsible side nav is introduced
-  const { isCollapsed } = useMainNavigationVisibility();
-
-  useEffect(() => {
-    const subscription = banners$.subscribe((banners) => setHeaderFixed(!banners.length));
-    return () => subscription.unsubscribe();
-  }, [banners$]); // Only un/re-subscribe if the Observable changes
 
   application.currentAppId$.subscribe((appId) => {
     subPluginId.current = appId ?? '';
@@ -99,9 +42,8 @@ const HomePageComponent: React.FC<HomePageProps> = ({
       ? SourcererScopeName.detections
       : SourcererScopeName.default
   );
-  const [showTimeline] = useShowTimeline();
 
-  const { browserFields, indexPattern, indicesExist } = useSourcererScope(
+  const { browserFields, indexPattern } = useSourcererScope(
     subPluginId.current === DETECTIONS_SUB_PLUGIN_ID
       ? SourcererScopeName.detections
       : SourcererScopeName.default
@@ -113,44 +55,12 @@ const HomePageComponent: React.FC<HomePageProps> = ({
   // can remove this.
   useUpgradeEndpointPackage();
 
-  const shouldShowTimelineBottomBar = indicesExist && showTimeline;
   return (
     <SecuritySolutionAppWrapper className="kbnAppWrapper">
       <HeaderGlobal setHeaderActionMenu={setHeaderActionMenu} />
       <DragDropContextWrapper browserFields={browserFields}>
         <UseUrlState indexPattern={indexPattern} navTabs={navTabs} />
-        <StyledKibanaPageTemplate
-          paddingSize="none"
-          pageSideBar={isCollapsed ? undefined : <MainNavigation />}
-          restrictWidth={false}
-          template="default"
-          $isShowingTimelineOverlay={show}
-          bottomBarProps={{
-            // Using a classname to target the bottom bar for the show/hide functionality
-            className: 'timeline-bottom-bar',
-            position: 'fixed',
-            usePortal: false,
-          }}
-          bottomBar={
-            shouldShowTimelineBottomBar && (
-              <>
-                <AutoSaveWarningMsg />
-                <Flyout timelineId={TimelineId.active} onAppLeave={onAppLeave} />
-              </>
-            )
-          }
-        >
-          <EuiPanel color="subdued" paddingSize="none">
-            <KQLHeaderGlobal ref={ref} isFixed={headerFixed} />
-          </EuiPanel>
-          <Main
-            className="kbnAppWrapper"
-            data-test-subj="pageContainer"
-            paddingTop={mainPaddingTop}
-          >
-            {children}
-          </Main>
-        </StyledKibanaPageTemplate>
+        {children}
       </DragDropContextWrapper>
       <HelpMenu />
     </SecuritySolutionAppWrapper>
---
+++ x-pack/plugins/security_solution/public/common/components/drag_and_drop/drag_classnames.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+// TODO: This can be a generic constants file, but for right now making it classnames
+// As these were causing circular dependencies when in './helpers'
+
+/** This class is added to the document body while dragging */
+export const IS_DRAGGING_CLASS_NAME = 'is-dragging';
+
+/** This class is added to the document body while timeline field dragging */
+export const IS_TIMELINE_FIELD_DRAGGING_CLASS_NAME = 'is-timeline-field-dragging';
