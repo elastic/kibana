ce0b1fd3a3e3b0234b64d98295b80ed91e12a915
Brian Seeders <brian.seeders@elastic.co>
2021-05-28 10:33:23 -0400

    Use build_ts_refs script

--- test/scripts/checks/plugin_public_api_docs.sh
+++ test/scripts/checks/plugin_public_api_docs.sh
@@ -5,7 +5,7 @@ source src/dev/ci_setup/setup_env.sh
 export BUILD_TS_REFS_DISABLE=false
 export BUILD_TS_REFS_CACHE_ENABLE=false
 
-yarn kbn bootstrap
+node scripts/build_ts_refs.js --force
 
 ###
 ### rebuild plugin api docs to ensure it's not out of date
