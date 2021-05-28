a33ebafc6daa90ca775b1e93b2e8872a73813709
Mike Cote <mikecote@users.noreply.github.com>
2021-05-28 09:25:08 -0400

    Expand tests

--- x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/health.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/tests/alerting/health.ts
@@ -25,7 +25,7 @@ export default function createFindTests({ getService }: FtrProviderContext) {
   const supertestWithoutAuth = getService('supertestWithoutAuth');
   const esTestIndexTool = new ESTestIndexTool(es, retry);
 
-  describe.only('health', () => {
+  describe('health', () => {
     const objectRemover = new ObjectRemover(supertest);
 
     before(async () => {
@@ -74,10 +74,9 @@ export default function createFindTests({ getService }: FtrProviderContext) {
         after(() => objectRemover.removeAll());
 
         it('should return healthy status by default', async () => {
-          const { body: health } = await supertest.get(
-            `${getUrlPrefix(space.id)}/api/alerting/_health`
-          );
-          console.log('health', health);
+          const { body: health } = await supertestWithoutAuth
+            .get(`${getUrlPrefix(space.id)}/api/alerting/_health`)
+            .auth(user.username, user.password);
           expect(health.is_sufficiently_secure).to.eql(true);
           expect(health.has_permanent_encryption_key).to.eql(true);
           expect(health.alerting_framework_heath.decryption_health.status).to.eql('ok');
@@ -87,11 +86,14 @@ export default function createFindTests({ getService }: FtrProviderContext) {
 
         it('should return error when a rule in the default space is failing', async () => {
           const reference = alertUtils.generateReference();
-          const response = await supertest
+          const { body: createdRule } = await supertest
             .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
             .set('kbn-xsrf', 'foo')
             .send(
               getTestAlertData({
+                schedule: {
+                  interval: '5m',
+                },
                 rule_type_id: 'test.failing',
                 params: {
                   index: ES_TEST_INDEX_NAME,
@@ -100,9 +102,25 @@ export default function createFindTests({ getService }: FtrProviderContext) {
               })
             )
             .expect(200);
-          objectRemover.add(space.id, response.body.id, 'rule', 'alerting');
+          objectRemover.add(space.id, createdRule.id, 'rule', 'alerting');
 
-          console.log('RESP', response.body);
+          const ruleInErrorStatus = await retry.tryForTime(30000, async () => {
+            const { body: rule } = await supertest
+              .get(`${getUrlPrefix(space.id)}/api/alerting/rule/${createdRule.id}`)
+              .expect(200);
+            expect(rule.execution_status.status).to.eql('error');
+            return rule;
+          });
+
+          await retry.tryForTime(30000, async () => {
+            const { body: health } = await supertestWithoutAuth
+              .get(`${getUrlPrefix(space.id)}/api/alerting/_health`)
+              .auth(user.username, user.password);
+            expect(health.alerting_framework_heath.execution_health.status).to.eql('warn');
+            expect(health.alerting_framework_heath.execution_health.timestamp).to.eql(
+              ruleInErrorStatus.execution_status.last_execution_date
+            );
+          });
         });
       });
     }
