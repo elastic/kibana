43422f3b642a013f4cccbacc1ee91502a35e2342
CJ Cenizal <cj@cenizal.com>
2021-06-01 16:26:01 -0700

    Migrate CCR to new ES JS client. (#100131)
    * Update SectionError component to render error root causes correctly.
    * Fix 404 error rendering.
    * Add test for follower index update API route.

--- x-pack/plugins/cross_cluster_replication/server/routes/api/follower_index/register_update_route.test.ts
+++ x-pack/plugins/cross_cluster_replication/server/routes/api/follower_index/register_update_route.test.ts
@@ -14,7 +14,7 @@ import { registerUpdateRoute } from './register_update_route';
 
 const httpService = httpServiceMock.createSetupContract();
 
-describe('[CCR API] Update auto-follow pattern', () => {
+describe('[CCR API] Update follower index', () => {
   let routeHandler: RequestHandler<any, any, any>;
 
   beforeEach(() => {
@@ -34,28 +34,45 @@ describe('[CCR API] Update auto-follow pattern', () => {
   it('should serialize the payload before sending it to Elasticsearch', async () => {
     const routeContextMock = mockRouteContext({
       ccr: {
+        followInfo: jest
+          .fn()
+          .mockResolvedValueOnce({ body: { follower_indices: [{ status: 'paused' }] } }),
         // Just echo back what we send so we can inspect it.
-        putAutoFollowPattern: jest.fn().mockImplementation((payload) => ({ body: payload })),
+        resumeFollow: jest.fn().mockImplementation((payload) => ({ body: payload })),
       },
     });
 
     const request = httpServerMock.createKibanaRequest({
       params: { id: 'foo' },
       body: {
-        remoteCluster: 'bar1',
-        leaderIndexPatterns: ['bar2'],
-        followIndexPattern: 'bar3',
+        maxReadRequestOperationCount: 1,
+        maxOutstandingReadRequests: 1,
+        maxReadRequestSize: '1b',
+        maxWriteRequestOperationCount: 1,
+        maxWriteRequestSize: '1b',
+        maxOutstandingWriteRequests: 1,
+        maxWriteBufferCount: 1,
+        maxWriteBufferSize: '1b',
+        maxRetryDelay: '1s',
+        readPollTimeout: '1s',
       },
     });
 
     const response = await routeHandler(routeContextMock, request, kibanaResponseFactory);
 
     expect(response.payload).toEqual({
-      name: 'foo',
+      index: 'foo',
       body: {
-        remote_cluster: 'bar1',
-        leader_index_patterns: ['bar2'],
-        follow_index_pattern: 'bar3',
+        max_outstanding_read_requests: 1,
+        max_outstanding_write_requests: 1,
+        max_read_request_operation_count: 1,
+        max_read_request_size: '1b',
+        max_retry_delay: '1s',
+        max_write_buffer_count: 1,
+        max_write_buffer_size: '1b',
+        max_write_request_operation_count: 1,
+        max_write_request_size: '1b',
+        read_poll_timeout: '1s',
       },
     });
   });
