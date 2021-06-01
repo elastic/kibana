4d7bb8a4c5ed6997d3141132fbe9b71043002f80
Vadim Yakhin <yakhin.v@gmail.com>
2021-06-01 11:05:01 -0300

    Copy lazy_wrapper and suspense_error_boundary from Spaces plugin
    These components are needed to enable async loading of Security components into Enterprise Search.
    
    The components are copied without any changes except for i18n ids, so it's easier to DRY out in the future if needed.

---
+++ x-pack/plugins/security/public/suspense_error_boundary/suspense_error_boundary.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiLoadingSpinner } from '@elastic/eui';
+import type { PropsWithChildren } from 'react';
+import React, { Component, Suspense } from 'react';
+
+import { i18n } from '@kbn/i18n';
+import type { NotificationsStart } from 'src/core/public';
+
+interface Props {
+  notifications: NotificationsStart;
+}
+
+interface State {
+  error: Error | null;
+}
+
+export class SuspenseErrorBoundary extends Component<PropsWithChildren<Props>, State> {
+  state: State = {
+    error: null,
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so next render shows fallback UI.
+    return { error };
+  }
+
+  public componentDidCatch(error: Error) {
+    const { notifications } = this.props;
+    if (notifications) {
+      const title = i18n.translate('xpack.security.uiApi.errorBoundaryToastTitle', {
+        defaultMessage: 'Failed to load Kibana asset',
+      });
+      const toastMessage = i18n.translate('xpack.security.uiApi.errorBoundaryToastMessage', {
+        defaultMessage: 'Reload page to continue.',
+      });
+      notifications.toasts.addError(error, { title, toastMessage });
+    }
+  }
+
+  render() {
+    const { children, notifications } = this.props;
+    const { error } = this.state;
+    if (!notifications || error) {
+      return null;
+    }
+    return <Suspense fallback={<EuiLoadingSpinner />}>{children}</Suspense>;
+  }
+}
