51c16f4c821556b5fa61dfab4843589d3168cfdc
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2021-06-01 13:22:41 -0400

    removed temp collapse and moved nav logic

--- x-pack/plugins/security_solution/public/common/components/wrapper_page/index.tsx
+++ x-pack/plugins/security_solution/public/common/components/wrapper_page/index.tsx
@@ -5,30 +5,33 @@
  * 2.0.
  */
 
-/* eslint-disable react/display-name */
-
 import React, { useEffect, useMemo, useState, useRef } from 'react';
 import styled from 'styled-components';
-import { CommonProps, EuiPanel } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { matchPath } from 'react-router-dom';
+import { CommonProps, EuiPanel, EuiSideNavItemType } from '@elastic/eui';
 import { KibanaPageTemplate } from '../../../../../../../src/plugins/kibana_react/public';
 import { AutoSaveWarningMsg } from '../../../timelines/components/timeline/auto_save_warning';
 import { useGlobalFullScreen } from '../../../common/containers/use_full_screen';
 import { AppGlobalStyle } from '../../../common/components/page';
 import { gutterTimeline } from '../../../common/lib/helpers';
 import { Flyout } from '../../../timelines/components/flyout';
-import { MainNavigation } from '../../../app/home/main_navigation';
 import { KQLHeaderGlobal } from '../../../app/home/kql_global';
 import { IS_DRAGGING_CLASS_NAME } from '../../../common/components/drag_and_drop/drag_classnames';
 import { useThrottledResizeObserver } from '../../../common/components/utils';
 import { useKibana } from '../../../common/lib/kibana';
 import { getTimelineShowStatusByIdSelector } from '../../../timelines/components/flyout/selectors';
 import { useDeepEqualSelector } from '../../../common/hooks/use_selector';
-import { useMainNavigationVisibility } from '../../../app/home/temp_collapse_sidenav_context';
 import { TimelineId } from '../../../../common/types/timeline';
 import { useShowTimeline } from '../../../common/utils/timeline/use_show_timeline';
 import { useSourcererScope } from '../../../common/containers/sourcerer';
-import { DETECTIONS_SUB_PLUGIN_ID } from '../../../../common/constants';
+import { APP_ID, APP_NAME, DETECTIONS_SUB_PLUGIN_ID } from '../../../../common/constants';
 import { SourcererScopeName } from '../../../common/store/sourcerer/model';
+import { navTabs } from '../../../app/home/home_navigations';
+
+const translatedNavTitle = i18n.translate('xpack.securitySolution.navigation.mainLabel', {
+  defaultMessage: APP_NAME,
+});
 
 const ChildrenWrapper = styled(EuiPanel)<{
   $paddingTop: number;
@@ -47,6 +50,7 @@ const StyledKibanaPageTemplate = styled(KibanaPageTemplate)<{
   $isShowingTimelineOverlay?: boolean;
 }>`
   .timeline-bottom-bar {
+    animation: 'none !important'; // disable the default bottom bar slide animation
     background: #ffffff; // Override bottom bar black background
     color: inherit; // Necessary to override the bottom bar 'white text'
     transform: ${(
@@ -88,9 +92,6 @@ export const WrapperPage: React.FC<WrapperPageProps & CommonProps> = React.memo(
       getTimelineShowStatus(state, TimelineId.active)
     );
 
-    // TODO: Remove when collapsible side nav is introduced
-    const { isCollapsed } = useMainNavigationVisibility();
-
     useEffect(() => {
       const subscription = banners$.subscribe((banners) => setHeaderFixed(!banners.length));
       return () => subscription.unsubscribe();
@@ -109,6 +110,37 @@ export const WrapperPage: React.FC<WrapperPageProps & CommonProps> = React.memo(
     );
 
     const shouldShowTimelineBottomBar = indicesExist && showTimeline;
+
+    const { navigateToApp } = application;
+    const { pathname: currentPath } = window.location;
+
+    const upperFirst = (str: string = ''): string => {
+      return str ? str.charAt(0).toUpperCase() + str.slice(1) : '';
+    };
+
+    const topLevelNavItems = useMemo<Array<EuiSideNavItemType<unknown>>>(
+      () =>
+        Object.values(navTabs).map(({ href, id, name }) => ({
+          href,
+          onClick: (ev: React.MouseEvent) => {
+            ev.preventDefault();
+            navigateToApp(`${APP_ID}:${id}`);
+          },
+          id,
+          name: upperFirst(name),
+          isSelected: matchPath(currentPath, { path: href }) !== null,
+        })),
+      [navigateToApp, currentPath]
+    );
+
+    const navItems = [
+      {
+        name: '',
+        id: APP_ID,
+        items: topLevelNavItems,
+      },
+    ];
+
     return (
       <StyledKibanaPageTemplate
         $isShowingTimelineOverlay={show}
@@ -130,8 +162,12 @@ export const WrapperPage: React.FC<WrapperPageProps & CommonProps> = React.memo(
         pageHeader={{
           children: pageHeaderChildren,
         }}
-        pageSideBar={isCollapsed ? undefined : <MainNavigation />}
         restrictWidth={false}
+        solutionNav={{
+          icon: 'logoSecurity',
+          items: navItems,
+          name: translatedNavTitle,
+        }}
         template="default"
       >
         <EuiPanel color="subdued" paddingSize="none">
