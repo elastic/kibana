cf2a5f29d0b60c7838be499ed37192e5bceec6c7
oatkiller <robert.austin@elastic.co>
2021-06-02 12:23:38 -0400

    Document the Rule Status SavedObject

--- x-pack/plugins/security_solution/server/lib/detection_engine/rules/saved_object_mappings.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rules/saved_object_mappings.ts
@@ -9,38 +9,84 @@ import { SavedObjectsType } from '../../../../../../../src/core/server';
 
 export const ruleStatusSavedObjectType = 'siem-detection-engine-rule-status';
 
-export const ruleStatusSavedObjectMappings: SavedObjectsType['mappings'] = {
+/**
+ * Records monitoring information about a rule, such as a when it last failed and why.
+ * This object is separate of the Rule SavedObject (why?)
+ * See `IRuleStatusSOAttributes` for types that related to this mapping
+ */
+const ruleStatusSavedObjectMappings: SavedObjectsType['mappings'] = {
   properties: {
+    /**
+     * A reference to a saved object
+     */
     alertId: {
       type: 'keyword',
     },
+    /**
+     * A `JobStatus`
+     */
     status: {
       type: 'keyword',
     },
+    /**
+     * The last date any status was recorded
+     * an `IsoDateString`
+     */
     statusDate: {
       type: 'date',
     },
+    /**
+     * The last time the rule failed to run
+     * an `IsoDateString`
+     */
     lastFailureAt: {
       type: 'date',
     },
+    /**
+     * The last time the rule ran successfully
+     * an `IsoDateString`
+     */
     lastSuccessAt: {
       type: 'date',
     },
+    /**
+     * Provides context about the last failed run.
+     * A non-i18n English language message
+     */
     lastFailureMessage: {
       type: 'text',
     },
+    /**
+     * Provides context about the last successful run.
+     * A non-i18n English language message
+     */
     lastSuccessMessage: {
       type: 'text',
     },
+    /**
+     * ???
+     */
     lastLookBackDate: {
       type: 'date',
     },
+    /**
+     * When a rule began running late, this explains how much time passed between the expected and actual start times.
+     * A non-i18n English language message
+     */
     gap: {
       type: 'text',
     },
+    /**
+     * ???
+     * A time duration, in milliseconds.
+     */
     bulkCreateTimeDurations: {
       type: 'float',
     },
+    /**
+     * ???
+     * A time duration, in milliseconds.
+     */
     searchAfterTimeDurations: {
       type: 'float',
     },
