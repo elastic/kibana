54ebeade7a258328e1aa7372d474096f2974d062
Pierre Gayvallet <pierre.gayvallet@gmail.com>
2021-06-02 16:07:34 +0200

    Fix newsfeed unread notifications always on when reloading Kibana (#100357)
    * fix the implementation
    
    * add unit tests
    
    * add API unit tests
    
    * fix public interface
    
    * address review comments
    
    * name convertItem to localizeItem
    
    * use fetch instead of core.http and add tests

--- src/plugins/newsfeed/public/lib/driver.test.mocks.ts
+++ src/plugins/newsfeed/public/lib/driver.test.mocks.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-export const NEWSFEED_FALLBACK_LANGUAGE = 'en';
-export const NEWSFEED_DEFAULT_SERVICE_BASE_URL = 'https://feeds.elastic.co';
-export const NEWSFEED_DEV_SERVICE_BASE_URL = 'https://feeds-staging.elastic.co';
-export const NEWSFEED_DEFAULT_SERVICE_PATH = '/kibana/v{VERSION}.json';
+export const convertItemsMock = jest.fn();
+jest.doMock('./convert_items', () => ({
+  convertItems: convertItemsMock,
+}));
