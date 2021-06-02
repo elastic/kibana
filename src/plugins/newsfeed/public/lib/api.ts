0c57d8d0a097eb75585d2011504f51cd1da084dc
Pierre Gayvallet <pierre.gayvallet@gmail.com>
2021-06-02 18:01:28 +0200

    Fix newsfeed unread notifications always on when reloading Kibana (#100357) (#101163)
    * fix the implementation
    
    * add unit tests
    
    * add API unit tests
    
    * fix public interface
    
    * address review comments
    
    * name convertItem to localizeItem
    
    * use fetch instead of core.http and add tests

--- src/plugins/newsfeed/public/lib/api.ts
+++ src/plugins/newsfeed/public/lib/api.ts
@@ -6,21 +6,12 @@
  * Side Public License, v 1.
  */
 
-import * as Rx from 'rxjs';
-import moment from 'moment';
-import uuid from 'uuid';
+import { combineLatest, Observable, timer, of } from 'rxjs';
+import { map, catchError, filter, mergeMap, tap } from 'rxjs/operators';
 import { i18n } from '@kbn/i18n';
-import { catchError, filter, mergeMap, tap } from 'rxjs/operators';
-import { HttpSetup } from 'src/core/public';
-import {
-  NEWSFEED_DEFAULT_SERVICE_BASE_URL,
-  NEWSFEED_FALLBACK_LANGUAGE,
-  NEWSFEED_LAST_FETCH_STORAGE_KEY,
-  NEWSFEED_HASH_SET_STORAGE_KEY,
-} from '../../common/constants';
-import { ApiItem, NewsfeedItem, FetchResult, NewsfeedPluginBrowserConfig } from '../types';
-
-type ApiConfig = NewsfeedPluginBrowserConfig['service'];
+import { FetchResult, NewsfeedPluginBrowserConfig } from '../types';
+import { NewsfeedApiDriver } from './driver';
+import { NewsfeedStorage } from './storage';
 
 export enum NewsfeedApiEndpoint {
   KIBANA = 'kibana',
@@ -29,145 +20,17 @@ export enum NewsfeedApiEndpoint {
   OBSERVABILITY = 'observability',
 }
 
-export class NewsfeedApiDriver {
-  private readonly id = uuid.v4();
-  private readonly kibanaVersion: string;
-  private readonly loadedTime = moment().utc(); // the date is compared to time in UTC format coming from the service
-  private readonly lastFetchStorageKey: string;
-  private readonly hashSetStorageKey: string;
+export interface NewsfeedApi {
+  /**
+   * The current fetch results
+   */
+  fetchResults$: Observable<void | null | FetchResult>;
 
-  constructor(
-    kibanaVersion: string,
-    private readonly userLanguage: string,
-    private readonly fetchInterval: number
-  ) {
-    // The API only accepts versions in the format `X.Y.Z`, so we need to drop the `-SNAPSHOT` or any other label after it
-    this.kibanaVersion = kibanaVersion.replace(/^(\d+\.\d+\.\d+).*/, '$1');
-    this.lastFetchStorageKey = `${NEWSFEED_LAST_FETCH_STORAGE_KEY}.${this.id}`;
-    this.hashSetStorageKey = `${NEWSFEED_HASH_SET_STORAGE_KEY}.${this.id}`;
-  }
-
-  shouldFetch(): boolean {
-    const lastFetchUtc: string | null = sessionStorage.getItem(this.lastFetchStorageKey);
-    if (lastFetchUtc == null) {
-      return true;
-    }
-    const last = moment(lastFetchUtc, 'x'); // parse as unix ms timestamp (already is UTC)
-
-    // does the last fetch time precede the time that the page was loaded?
-    if (this.loadedTime.diff(last) > 0) {
-      return true;
-    }
-
-    const now = moment.utc(); // always use UTC to compare timestamps that came from the service
-    const duration = moment.duration(now.diff(last));
-
-    return duration.asMilliseconds() > this.fetchInterval;
-  }
-
-  updateLastFetch() {
-    sessionStorage.setItem(this.lastFetchStorageKey, Date.now().toString());
-  }
-
-  updateHashes(items: NewsfeedItem[]): { previous: string[]; current: string[] } {
-    // replace localStorage hashes with new hashes
-    const stored: string | null = localStorage.getItem(this.hashSetStorageKey);
-    let old: string[] = [];
-    if (stored != null) {
-      old = stored.split(',');
-    }
-
-    const newHashes = items.map((i) => i.hash);
-    const updatedHashes = [...new Set(old.concat(newHashes))];
-    localStorage.setItem(this.hashSetStorageKey, updatedHashes.join(','));
-
-    return { previous: old, current: updatedHashes };
-  }
-
-  fetchNewsfeedItems(http: HttpSetup, config: ApiConfig): Rx.Observable<FetchResult> {
-    const urlPath = config.pathTemplate.replace('{VERSION}', this.kibanaVersion);
-    const fullUrl = (config.urlRoot || NEWSFEED_DEFAULT_SERVICE_BASE_URL) + urlPath;
-
-    return Rx.from(
-      http
-        .fetch(fullUrl, {
-          method: 'GET',
-        })
-        .then(({ items }: { items: ApiItem[] }) => {
-          return this.modelItems(items);
-        })
-    );
-  }
-
-  validateItem(item: Partial<NewsfeedItem>) {
-    const hasMissing = [
-      item.title,
-      item.description,
-      item.linkText,
-      item.linkUrl,
-      item.publishOn,
-      item.hash,
-    ].includes(undefined);
-
-    return !hasMissing;
-  }
-
-  modelItems(items: ApiItem[]): FetchResult {
-    const feedItems: NewsfeedItem[] = items.reduce((accum: NewsfeedItem[], it: ApiItem) => {
-      let chosenLanguage = this.userLanguage;
-      const {
-        expire_on: expireOnUtc,
-        publish_on: publishOnUtc,
-        languages,
-        title,
-        description,
-        link_text: linkText,
-        link_url: linkUrl,
-        badge,
-        hash,
-      } = it;
-
-      if (moment(expireOnUtc).isBefore(Date.now())) {
-        return accum; // ignore item if expired
-      }
-
-      if (moment(publishOnUtc).isAfter(Date.now())) {
-        return accum; // ignore item if publish date hasn't occurred yet (pre-published)
-      }
-
-      if (languages && !languages.includes(chosenLanguage)) {
-        chosenLanguage = NEWSFEED_FALLBACK_LANGUAGE; // don't remove the item: fallback on a language
-      }
-
-      const tempItem: NewsfeedItem = {
-        title: title[chosenLanguage],
-        description: description[chosenLanguage],
-        linkText: linkText != null ? linkText[chosenLanguage] : null,
-        linkUrl: linkUrl[chosenLanguage],
-        badge: badge != null ? badge![chosenLanguage] : null,
-        publishOn: moment(publishOnUtc),
-        expireOn: moment(expireOnUtc),
-        hash: hash.slice(0, 10), // optimize for storage and faster parsing
-      };
-
-      if (!this.validateItem(tempItem)) {
-        return accum; // ignore if title, description, etc is missing
-      }
-
-      return [...accum, tempItem];
-    }, []);
-
-    // calculate hasNew
-    const { previous, current } = this.updateHashes(feedItems);
-    const hasNew = current.length > previous.length;
-
-    return {
-      error: null,
-      kibanaVersion: this.kibanaVersion,
-      hasNew,
-      feedItems,
-    };
-  }
+  /**
+   * Mark the given items as read.
+   * Will refresh the `hasNew` value of the emitted FetchResult accordingly
+   */
+  markAsRead(itemHashes: string[]): void;
 }
 
 /*
@@ -175,22 +38,23 @@ export class NewsfeedApiDriver {
  * Computes hasNew value from new item hashes saved in localStorage
  */
 export function getApi(
-  http: HttpSetup,
   config: NewsfeedPluginBrowserConfig,
-  kibanaVersion: string
-): Rx.Observable<void | FetchResult> {
+  kibanaVersion: string,
+  newsfeedId: string
+): NewsfeedApi {
   const userLanguage = i18n.getLocale();
   const fetchInterval = config.fetchInterval.asMilliseconds();
   const mainInterval = config.mainInterval.asMilliseconds();
-  const driver = new NewsfeedApiDriver(kibanaVersion, userLanguage, fetchInterval);
+  const storage = new NewsfeedStorage(newsfeedId);
+  const driver = new NewsfeedApiDriver(kibanaVersion, userLanguage, fetchInterval, storage);
 
-  return Rx.timer(0, mainInterval).pipe(
+  const results$ = timer(0, mainInterval).pipe(
     filter(() => driver.shouldFetch()),
     mergeMap(() =>
-      driver.fetchNewsfeedItems(http, config.service).pipe(
+      driver.fetchNewsfeedItems(config.service).pipe(
         catchError((err) => {
           window.console.error(err);
-          return Rx.of({
+          return of({
             error: err,
             kibanaVersion,
             hasNew: false,
@@ -199,6 +63,22 @@ export function getApi(
         })
       )
     ),
-    tap(() => driver.updateLastFetch())
+    tap(() => storage.setLastFetchTime(new Date()))
   );
+
+  const merged$ = combineLatest([results$, storage.isAnyUnread$()]).pipe(
+    map(([results, isAnyUnread]) => {
+      return {
+        ...results,
+        hasNew: results.error ? false : isAnyUnread,
+      };
+    })
+  );
+
+  return {
+    fetchResults$: merged$,
+    markAsRead: (itemHashes) => {
+      storage.markItemsAsRead(itemHashes);
+    },
+  };
 }
