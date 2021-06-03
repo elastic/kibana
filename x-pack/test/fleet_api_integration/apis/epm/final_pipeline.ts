6c123b4bee16f93e42f2747c4f18c5237ac98c9a
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2021-06-03 08:29:43 -0400

    Fix duplicate licence header

--- x-pack/test/fleet_api_integration/apis/epm/final_pipeline.ts
+++ x-pack/test/fleet_api_integration/apis/epm/final_pipeline.ts
@@ -5,13 +5,6 @@
  * 2.0.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
 import { setupFleetAndAgents } from '../agents/services';
