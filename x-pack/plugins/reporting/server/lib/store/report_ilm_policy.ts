d0f0a27c6b53e7c16470a52afcc7420169fdb286
Mikhail Shustov <restrry@gmail.com>
2021-06-03 18:50:51 +0300

    fix new errors in reporting

--- x-pack/plugins/reporting/server/lib/store/report_ilm_policy.ts
+++ x-pack/plugins/reporting/server/lib/store/report_ilm_policy.ts
@@ -5,9 +5,9 @@
  * 2.0.
  */
 
-import { PutLifecycleRequest } from '@elastic/elasticsearch/api/types';
+import type { estypes } from '@elastic/elasticsearch';
 
-export const reportingIlmPolicy: PutLifecycleRequest['body'] = {
+export const reportingIlmPolicy: estypes.IlmPutLifecycleRequest['body'] = {
   policy: {
     phases: {
       hot: {
