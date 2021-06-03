225c7839333f93d649be22bc221bc239b0e3dcd6
Ahmad Bamieh <ahmadbamieh@gmail.com>
2021-06-03 15:52:38 +0300

    uncomment test files

--- test/api_integration/apis/index.ts
+++ test/api_integration/apis/index.ts
@@ -10,22 +10,22 @@ import { FtrProviderContext } from '../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('apis', () => {
-    // loadTestFile(require.resolve('./console'));
-    // loadTestFile(require.resolve('./core'));
-    // loadTestFile(require.resolve('./general'));
-    // loadTestFile(require.resolve('./home'));
-    // loadTestFile(require.resolve('./index_patterns'));
-    // loadTestFile(require.resolve('./kql_telemetry'));
-    // loadTestFile(require.resolve('./saved_objects_management'));
-    // loadTestFile(require.resolve('./saved_objects'));
-    // loadTestFile(require.resolve('./scripts'));
-    // loadTestFile(require.resolve('./search'));
-    // loadTestFile(require.resolve('./shorten'));
-    // loadTestFile(require.resolve('./suggestions'));
-    // loadTestFile(require.resolve('./status'));
-    // loadTestFile(require.resolve('./stats'));
-    // loadTestFile(require.resolve('./ui_metric'));
+    loadTestFile(require.resolve('./console'));
+    loadTestFile(require.resolve('./core'));
+    loadTestFile(require.resolve('./general'));
+    loadTestFile(require.resolve('./home'));
+    loadTestFile(require.resolve('./index_patterns'));
+    loadTestFile(require.resolve('./kql_telemetry'));
+    loadTestFile(require.resolve('./saved_objects_management'));
+    loadTestFile(require.resolve('./saved_objects'));
+    loadTestFile(require.resolve('./scripts'));
+    loadTestFile(require.resolve('./search'));
+    loadTestFile(require.resolve('./shorten'));
+    loadTestFile(require.resolve('./suggestions'));
+    loadTestFile(require.resolve('./status'));
+    loadTestFile(require.resolve('./stats'));
+    loadTestFile(require.resolve('./ui_metric'));
     loadTestFile(require.resolve('./ui_counters'));
-    // loadTestFile(require.resolve('./telemetry'));
+    loadTestFile(require.resolve('./telemetry'));
   });
 }
