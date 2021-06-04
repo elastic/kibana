3d24f705adcbcfcf7ed8a515d89b0934c551f3c4
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2021-06-04 11:16:50 -0400

    Moves executor inline for easier types (temp)

--- x-pack/plugins/infra/server/lib/alerting/metric_threshold/metric_threshold_executor.ts
+++ x-pack/plugins/infra/server/lib/alerting/metric_threshold/metric_threshold_executor.ts
@@ -5,135 +5,9 @@
  * 2.0.
  */
 
-import { first, last } from 'lodash';
 import { i18n } from '@kbn/i18n';
-import moment from 'moment';
-import { RecoveredActionGroup } from '../../../../../alerting/common';
-import { InfraBackendLibs } from '../../infra_types';
-import {
-  buildErrorAlertReason,
-  buildFiredAlertReason,
-  buildNoDataAlertReason,
-  // buildRecoveredAlertReason,
-  stateToAlertMessage,
-} from '../common/messages';
 import { createFormatter } from '../../../../common/formatters';
-import { AlertStates, Comparator } from './types';
-import { evaluateAlert, EvaluatedAlertParams } from './lib/evaluate_alert';
-import {
-  MetricThresholdAlertExecutorOptions,
-  MetricThresholdAlertType,
-} from './register_metric_threshold_alert_type';
-
-export const createMetricThresholdExecutor = (
-  libs: InfraBackendLibs
-): MetricThresholdAlertType['executor'] =>
-  async function (options: MetricThresholdAlertExecutorOptions) {
-    const { services, params } = options;
-    const { criteria } = params;
-    if (criteria.length === 0) throw new Error('Cannot execute an alert with 0 conditions');
-
-    const { sourceId, alertOnNoData } = params as {
-      sourceId?: string;
-      alertOnNoData: boolean;
-    };
-
-    const source = await libs.sources.getSourceConfiguration(
-      services.savedObjectsClient,
-      sourceId || 'default'
-    );
-    const config = source.configuration;
-    const alertResults = await evaluateAlert(
-      services.scopedClusterClient.asCurrentUser,
-      params as EvaluatedAlertParams,
-      config
-    );
-
-    // Because each alert result has the same group definitions, just grab the groups from the first one.
-    const groups = Object.keys(first(alertResults)!);
-    for (const group of groups) {
-      // AND logic; all criteria must be across the threshold
-      const shouldAlertFire = alertResults.every((result) =>
-        // Grab the result of the most recent bucket
-        last(result[group].shouldFire)
-      );
-      const shouldAlertWarn = alertResults.every((result) => last(result[group].shouldWarn));
-      // AND logic; because we need to evaluate all criteria, if one of them reports no data then the
-      // whole alert is in a No Data/Error state
-      const isNoData = alertResults.some((result) => last(result[group].isNoData));
-      const isError = alertResults.some((result) => result[group].isError);
-
-      const nextState = isError
-        ? AlertStates.ERROR
-        : isNoData
-        ? AlertStates.NO_DATA
-        : shouldAlertFire
-        ? AlertStates.ALERT
-        : shouldAlertWarn
-        ? AlertStates.WARNING
-        : AlertStates.OK;
-
-      let reason;
-      if (nextState === AlertStates.ALERT || nextState === AlertStates.WARNING) {
-        reason = alertResults
-          .map((result) =>
-            buildFiredAlertReason(
-              formatAlertResult(result[group], nextState === AlertStates.WARNING)
-            )
-          )
-          .join('\n');
-        /*
-         * Custom recovery actions aren't yet available in the alerting framework
-         * Uncomment the code below once they've been implemented
-         * Reference: https://github.com/elastic/kibana/issues/87048
-         */
-        // } else if (nextState === AlertStates.OK && prevState?.alertState === AlertStates.ALERT) {
-        // reason = alertResults
-        //   .map((result) => buildRecoveredAlertReason(formatAlertResult(result[group])))
-        //   .join('\n');
-      }
-      if (alertOnNoData) {
-        if (nextState === AlertStates.NO_DATA) {
-          reason = alertResults
-            .filter((result) => result[group].isNoData)
-            .map((result) => buildNoDataAlertReason(result[group]))
-            .join('\n');
-        } else if (nextState === AlertStates.ERROR) {
-          reason = alertResults
-            .filter((result) => result[group].isError)
-            .map((result) => buildErrorAlertReason(result[group].metric))
-            .join('\n');
-        }
-      }
-      if (reason) {
-        const firstResult = first(alertResults);
-        const timestamp = (firstResult && firstResult[group].timestamp) ?? moment().toISOString();
-        const actionGroupId =
-          nextState === AlertStates.OK
-            ? RecoveredActionGroup.id
-            : nextState === AlertStates.WARNING
-            ? WARNING_ACTIONS.id
-            : FIRED_ACTIONS.id;
-        const alertInstance = services.alertInstanceFactory(`${group}`);
-
-        alertInstance.scheduleActions(actionGroupId, {
-          group,
-          alertState: stateToAlertMessage[nextState],
-          reason,
-          timestamp,
-          value: mapToConditionsLookup(
-            alertResults,
-            (result) => formatAlertResult(result[group]).currentValue
-          ),
-          threshold: mapToConditionsLookup(
-            alertResults,
-            (result) => formatAlertResult(result[group]).threshold
-          ),
-          metric: mapToConditionsLookup(criteria, (c) => c.metric),
-        });
-      }
-    }
-  };
+import { Comparator } from './types';
 
 export const FIRED_ACTIONS = {
   id: 'metrics.threshold.fired',
@@ -149,7 +23,7 @@ export const WARNING_ACTIONS = {
   }),
 };
 
-const mapToConditionsLookup = (
+export const mapToConditionsLookup = (
   list: any[],
   mapFn: (value: any, index: number, array: any[]) => unknown
 ) =>
@@ -160,7 +34,7 @@ const mapToConditionsLookup = (
       {}
     );
 
-const formatAlertResult = <AlertResult>(
+export const formatAlertResult = <AlertResult>(
   alertResult: {
     metric: string;
     currentValue: number;
--- x-pack/plugins/infra/server/lib/alerting/metric_threshold/register_metric_threshold_alert_type.ts
+++ x-pack/plugins/infra/server/lib/alerting/metric_threshold/register_metric_threshold_alert_type.ts
@@ -5,6 +5,8 @@
  * 2.0.
  */
 
+import { first, last } from 'lodash';
+import moment from 'moment';
 import { schema } from '@kbn/config-schema';
 import { i18n } from '@kbn/i18n';
 import {
@@ -16,11 +18,12 @@ import {
 } from '../../../../../alerting/server';
 import { METRIC_EXPLORER_AGGREGATIONS } from '../../../../common/http_api/metrics_explorer';
 import {
-  createMetricThresholdExecutor,
+  mapToConditionsLookup,
+  formatAlertResult,
   FIRED_ACTIONS,
   WARNING_ACTIONS,
 } from './metric_threshold_executor';
-import { METRIC_THRESHOLD_ALERT_TYPE_ID, Comparator } from './types';
+import { AlertStates, METRIC_THRESHOLD_ALERT_TYPE_ID, Comparator } from './types';
 import { InfraBackendLibs } from '../../infra_types';
 import { oneOfLiterals, validateIsStringElasticsearchJSONFilter } from '../common/utils';
 import {
@@ -32,6 +35,16 @@ import {
   metricActionVariableDescription,
   thresholdActionVariableDescription,
 } from '../common/messages';
+
+import { RecoveredActionGroup } from '../../../../../alerting/common';
+import {
+  buildErrorAlertReason,
+  buildFiredAlertReason,
+  buildNoDataAlertReason,
+  // buildRecoveredAlertReason,
+  stateToAlertMessage,
+} from '../common/messages';
+import { evaluateAlert, EvaluatedAlertParams } from './lib/evaluate_alert';
 
 export type MetricThresholdAlertType = AlertType<
   /**
@@ -100,7 +113,112 @@ export function registerMetricThresholdAlertType(libs: InfraBackendLibs): Metric
     defaultActionGroupId: FIRED_ACTIONS.id,
     actionGroups: [FIRED_ACTIONS, WARNING_ACTIONS],
     minimumLicenseRequired: 'basic',
-    executor: createMetricThresholdExecutor(libs),
+    executor: async (options: MetricThresholdAlertExecutorOptions) => {
+      const { services, params } = options;
+      const { criteria } = params;
+      if (criteria.length === 0) throw new Error('Cannot execute an alert with 0 conditions');
+
+      const { sourceId, alertOnNoData } = params as {
+        sourceId?: string;
+        alertOnNoData: boolean;
+      };
+
+      const source = await libs.sources.getSourceConfiguration(
+        services.savedObjectsClient,
+        sourceId || 'default'
+      );
+      const config = source.configuration;
+      const alertResults = await evaluateAlert(
+        services.scopedClusterClient.asCurrentUser,
+        params as EvaluatedAlertParams,
+        config
+      );
+
+      // Because each alert result has the same group definitions, just grab the groups from the first one.
+      const groups = Object.keys(first(alertResults)!);
+      for (const group of groups) {
+        // AND logic; all criteria must be across the threshold
+        const shouldAlertFire = alertResults.every((result) =>
+          // Grab the result of the most recent bucket
+          last(result[group].shouldFire)
+        );
+        const shouldAlertWarn = alertResults.every((result) => last(result[group].shouldWarn));
+        // AND logic; because we need to evaluate all criteria, if one of them reports no data then the
+        // whole alert is in a No Data/Error state
+        const isNoData = alertResults.some((result) => last(result[group].isNoData));
+        const isError = alertResults.some((result) => result[group].isError);
+
+        const nextState = isError
+          ? AlertStates.ERROR
+          : isNoData
+          ? AlertStates.NO_DATA
+          : shouldAlertFire
+          ? AlertStates.ALERT
+          : shouldAlertWarn
+          ? AlertStates.WARNING
+          : AlertStates.OK;
+
+        let reason;
+        if (nextState === AlertStates.ALERT || nextState === AlertStates.WARNING) {
+          reason = alertResults
+            .map((result) =>
+              buildFiredAlertReason(
+                formatAlertResult(result[group], nextState === AlertStates.WARNING)
+              )
+            )
+            .join('\n');
+          /*
+           * Custom recovery actions aren't yet available in the alerting framework
+           * Uncomment the code below once they've been implemented
+           * Reference: https://github.com/elastic/kibana/issues/87048
+           */
+          // } else if (nextState === AlertStates.OK && prevState?.alertState === AlertStates.ALERT) {
+          // reason = alertResults
+          //   .map((result) => buildRecoveredAlertReason(formatAlertResult(result[group])))
+          //   .join('\n');
+        }
+        if (alertOnNoData) {
+          if (nextState === AlertStates.NO_DATA) {
+            reason = alertResults
+              .filter((result) => result[group].isNoData)
+              .map((result) => buildNoDataAlertReason(result[group]))
+              .join('\n');
+          } else if (nextState === AlertStates.ERROR) {
+            reason = alertResults
+              .filter((result) => result[group].isError)
+              .map((result) => buildErrorAlertReason(result[group].metric))
+              .join('\n');
+          }
+        }
+        if (reason) {
+          const firstResult = first(alertResults);
+          const timestamp = (firstResult && firstResult[group].timestamp) ?? moment().toISOString();
+          const actionGroupId =
+            nextState === AlertStates.OK
+              ? RecoveredActionGroup.id
+              : nextState === AlertStates.WARNING
+              ? WARNING_ACTIONS.id
+              : FIRED_ACTIONS.id;
+          const alertInstance = services.alertInstanceFactory(`${group}`);
+
+          alertInstance.scheduleActions(actionGroupId, {
+            group,
+            alertState: stateToAlertMessage[nextState],
+            reason,
+            timestamp,
+            value: mapToConditionsLookup(
+              alertResults,
+              (result) => formatAlertResult(result[group]).currentValue
+            ),
+            threshold: mapToConditionsLookup(
+              alertResults,
+              (result) => formatAlertResult(result[group]).threshold
+            ),
+            metric: mapToConditionsLookup(criteria, (c) => c.metric),
+          });
+        }
+      }
+    },
     actionVariables: {
       context: [
         { name: 'group', description: groupActionVariableDescription },
