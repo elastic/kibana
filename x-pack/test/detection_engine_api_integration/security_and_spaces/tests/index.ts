5b28ee0461587e2b5afc37401a45f95cb90a9dfa
Frank Hassanabad <frank.hassanabad@elastic.co>
2021-06-04 20:34:52 -0600

    [Security Solution][Detection Engine] Test cases for alias failure test cases where we don't copy aliases correctly (#101437)
    ## Summary
    
    Test cases for signals and aliases, including a failure of where we do not copy alias data at the moment even if the target is an ECS compatible field.
    
    For example with this mapping:
    
    ```json
    {
      "dynamic": "strict",
      "properties": {
        "@timestamp": {
          "type": "date"
        },
        "host": {
          "properties": {
            "name": {
              "type": "alias",
              "path": "host_alias.name"
            }
          }
        },
        "host_alias": {
          "properties": {
            "name": {
              "type": "keyword"
            }
          }
        }
      }
    }
    ```
    
    If we detect this as a signal hit we should be copying over both:
    * `host_alias.name` -> `host.name`
    * `host_alias.name` -> `host_alias.name`
    
    to the target signal index, but we only copy:
    * `host_alias.name` -> `host_alias.name`
    
    ### Checklist
    
    - [x] [Unit or functional tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html) were updated or added to match the most common scenarios

--- x-pack/test/detection_engine_api_integration/security_and_spaces/tests/index.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/tests/index.ts
@@ -13,6 +13,7 @@ export default ({ loadTestFile }: FtrProviderContext): void => {
     describe('', function () {
       this.tags('ciGroup11');
 
+      loadTestFile(require.resolve('./aliases'));
       loadTestFile(require.resolve('./add_actions'));
       loadTestFile(require.resolve('./update_actions'));
       loadTestFile(require.resolve('./add_prepackaged_rules'));
