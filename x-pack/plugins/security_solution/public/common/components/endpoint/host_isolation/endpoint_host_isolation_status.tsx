43956909f832c4d0c78149257c6900aced5abb08
Paul Tavares <paul.tavares@elastic.co>
2021-06-08 16:44:37 -0400

    first iteration of EndpointHostIsolationStatus component

---
+++ x-pack/plugins/security_solution/public/common/components/endpoint/host_isolation/endpoint_host_isolation_status.tsx
@@ -0,0 +1,71 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { memo, useMemo } from 'react';
+import { EuiBadge } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+
+export interface EndpointHostIsolationStatusProps {
+  isIsolated: boolean;
+  /** the count of pending isolate actions */
+  pendingIsolate?: number;
+  /** the count of pending unisoalte actions */
+  pendingUnIsolate?: number;
+}
+
+/**
+ * Component will display a host isoaltion status based on whether it is currently isolated or there are
+ * isolate/unisolate actions pending. If none of these are applicable, no UI component will be rendered
+ * (`null` is returned)
+ */
+export const EndpointHostIsolationStatus = memo<EndpointHostIsolationStatusProps>(
+  ({ isIsolated, pendingIsolate = 0, pendingUnIsolate = 0 }) => {
+    return useMemo(() => {
+      // If nothing is pending and host is not currently isolated, then render nothing
+      if (!isIsolated && !pendingIsolate && !pendingUnIsolate) {
+        return null;
+      }
+
+      // If nothing is pending, but host is isolated, then show isolation badge
+      if (!pendingIsolate && !pendingUnIsolate) {
+        return (
+          <EuiBadge color="ghost">
+            <FormattedMessage
+              id="xpack.securitySolution.endpoint.hostIsolationStatus.isolated"
+              defaultMessage="Isolated"
+            />
+          </EuiBadge>
+        );
+      }
+
+      // If there are multiple types of pending isolation actions, then show count of actions with tooltip that displays breakdown
+      // TODO: implement edge case
+      // if () {
+      //
+      // }
+
+      // Show 'pending [un]isolate' depending on what's pending
+      return (
+        <EuiBadge>
+          {pendingIsolate ? (
+            <FormattedMessage
+              id="xpack.securitySolution.endpoint.hostIsolationStatus.isIsolating"
+              defaultMessage="Isolating pending"
+            />
+          ) : (
+            <FormattedMessage
+              id="xpack.securitySolution.endpoint.hostIsolationStatus.isIsolating"
+              defaultMessage="Unisolating pending"
+            />
+          )}
+        </EuiBadge>
+      );
+    }, [isIsolated, pendingIsolate, pendingUnIsolate]);
+  }
+);
+
+EndpointHostIsolationStatus.displayName = 'EndpointHostIsolationStatus';
