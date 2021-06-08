7e7f0c0f9d2ef6a9cc8da74d58b463d1dbd38978
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2021-06-08 16:15:07 +0200

    Fix investigation guide is not formatted under Alert details flyout
    * Add LineClamp to investigation guide field
    * It enhances LineClamp to support a react node instead of only text

--- x-pack/plugins/security_solution/public/common/components/event_details/alert_summary_view.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/alert_summary_view.tsx
@@ -36,6 +36,7 @@ import { DESTINATION_IP_FIELD_NAME, SOURCE_IP_FIELD_NAME } from '../../../networ
 import { SummaryView } from './summary_view';
 import { AlertSummaryRow, getSummaryColumns, SummaryRow } from './helpers';
 import { useRuleAsync } from '../../../detections/containers/detection_engine/rules/use_rule_async';
+import { MarkdownRenderer } from '../markdown_editor';
 import { LineClamp } from '../line_clamp';
 
 const StyledEuiDescriptionList = styled(EuiDescriptionList)`
@@ -216,7 +217,9 @@ const AlertSummaryViewComponent: React.FC<{
         <StyledEuiDescriptionList data-test-subj={`summary-view-guide`} compressed>
           <EuiDescriptionListTitle>{i18n.INVESTIGATION_GUIDE}</EuiDescriptionListTitle>
           <EuiDescriptionListDescription>
-            <LineClamp content={maybeRule?.note} />
+            <LineClamp>
+              <MarkdownRenderer>{maybeRule.note}</MarkdownRenderer>
+            </LineClamp>
           </EuiDescriptionListDescription>
         </StyledEuiDescriptionList>
       )}
