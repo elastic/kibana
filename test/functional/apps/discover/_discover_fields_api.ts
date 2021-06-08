0c646a4fd2df3c28745e6c4a66952281209cba01
Maja Grubic <maja.grubic@elastic.co>
2021-06-08 15:17:48 +0100

    Remove unnecessary import

--- test/functional/apps/discover/_discover_fields_api.ts
+++ test/functional/apps/discover/_discover_fields_api.ts
@@ -12,7 +12,6 @@ import { FtrProviderContext } from './ftr_provider_context';
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const log = getService('log');
   const docTable = getService('docTable');
-  const find = getService('find');
   const retry = getService('retry');
   const esArchiver = getService('esArchiver');
   const kibanaServer = getService('kibanaServer');
