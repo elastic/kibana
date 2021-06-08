7da779c729b3654b6857630341a85a866c951342
Ying Mao <ying.mao@elastic.co>
2021-06-08 09:20:21 -0400

    Adding warning to logs when alerting task runner encounters saved object not found and doesn't reschedule rule

--- x-pack/plugins/alerting/server/task_runner/task_runner.ts
+++ x-pack/plugins/alerting/server/task_runner/task_runner.ts
@@ -581,6 +581,9 @@ export class TaskRunner<
       ),
       schedule: resolveErr<IntervalSchedule | undefined, Error>(schedule, (error) => {
         if (isAlertSavedObjectNotFoundError(error, alertId)) {
+          this.logger.warn(
+            `Unable to execute rule "${alertId}" because ${error.message} - this rule will not be rescheduled. To restart rule execution, try disabling and re-enabling this rule.`
+          );
           throwUnrecoverableError(error);
         }
         return { interval: taskSchedule?.interval ?? FALLBACK_RETRY_INTERVAL };
