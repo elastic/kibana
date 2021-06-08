eb267179fde91456a920a377863878a4f8bb9947
chrisronline <chrisronline@gmail.com>
2021-06-08 13:05:53 -0400

    Update config for this suite too

--- x-pack/test/plugin_api_integration/config.ts
+++ x-pack/test/plugin_api_integration/config.ts
@@ -45,6 +45,7 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
         '--xpack.eventLog.logEntries=true',
         '--xpack.eventLog.indexEntries=true',
         '--xpack.task_manager.monitored_aggregated_stats_refresh_rate=5000',
+        '--xpack.task_manager.ephemeral_tasks.enabled=true',
         ...plugins.map(
           (pluginDir) => `--plugin-path=${path.resolve(__dirname, 'plugins', pluginDir)}`
         ),
