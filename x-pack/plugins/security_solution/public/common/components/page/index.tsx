a57441a43750cbe74929be9fa0276f9c0eca05b5
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2021-06-09 16:38:48 -0400

    remove commented code

--- x-pack/plugins/security_solution/public/common/components/page/index.tsx
+++ x-pack/plugins/security_solution/public/common/components/page/index.tsx
@@ -63,11 +63,6 @@ export const AppGlobalStyle = createGlobalStyle<{ theme: { eui: { euiColorPrimar
     ${({ theme }) => `background-color: ${theme.eui.euiColorPrimary} !important`};
   }
 
-  /* .${SCROLLING_DISABLED_CLASS_NAME} ${SecuritySolutionAppWrapper} {
-    max-height: calc(100vh - ${GLOBAL_HEADER_HEIGHT}px);
-    overflow-y: scroll;
-  } */
-
   /*
      EuiScreenReaderOnly has a default 1px height and width. These extra pixels
      were adding additional height to every table row in the alerts table on the
