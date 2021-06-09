5e32dc3db2494cb44a909faa7067b71ac3637719
Tiago Costa <tiagoffcc@hotmail.com>
2021-06-09 17:10:50 +0100

    chore(NA): solve eslint problems

--- x-pack/plugins/cases/public/components/markdown_editor/types.ts
+++ x-pack/plugins/cases/public/components/markdown_editor/types.ts
@@ -8,7 +8,6 @@
 import { FunctionComponent } from 'react';
 import { Plugin, PluggableList } from 'unified';
 // Remove after this issue is resolved: https://github.com/elastic/eui/issues/4688
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Options as Remark2RehypeOptions } from 'mdast-util-to-hast';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import rehype2react from 'rehype-react';
