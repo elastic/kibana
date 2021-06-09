2fd3c37fdd92aac949d740164f7947aa1945fa50
ymao1 <ying.mao@elastic.co>
2021-06-09 15:55:01 -0400

    [Alerting] Log warning when rules are not rescheduled due to Saved Object not found error (#101591)
    * Adding warning to logs when alerting task runner encounters saved object not found and doesn't reschedule rule
    
    * Adding space id to warning message
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/alerting/server/task_runner/task_runner.test.ts
+++ x-pack/plugins/alerting/server/task_runner/task_runner.test.ts
@@ -2223,9 +2223,58 @@ describe('Task Runner', () => {
       references: [],
     });
 
+    const logger = taskRunnerFactoryInitializerParams.logger;
     return taskRunner.run().catch((ex) => {
       expect(ex).toMatchInlineSnapshot(`[Error: Saved object [alert/1] not found]`);
+      expect(logger.debug).toHaveBeenCalledWith(
+        `Executing Alert "1" has resulted in Error: Saved object [alert/1] not found`
+      );
+      expect(logger.warn).toHaveBeenCalledTimes(1);
+      expect(logger.warn).nthCalledWith(
+        1,
+        `Unable to execute rule "1" because Saved object [alert/1] not found - this rule will not be rescheduled. To restart rule execution, try disabling and re-enabling this rule.`
+      );
       expect(isUnrecoverableError(ex)).toBeTruthy();
+    });
+  });
+
+  test('correctly logs warning when Alert Task Runner throws due to failing to fetch the alert in a space', async () => {
+    alertsClient.get.mockImplementation(() => {
+      throw SavedObjectsErrorHelpers.createGenericNotFoundError('alert', '1');
+    });
+
+    const taskRunner = new TaskRunner(
+      alertType,
+      {
+        ...mockedTaskInstance,
+        params: {
+          ...mockedTaskInstance.params,
+          spaceId: 'test space',
+        },
+      },
+      taskRunnerFactoryInitializerParams
+    );
+
+    encryptedSavedObjectsClient.getDecryptedAsInternalUser.mockResolvedValue({
+      id: '1',
+      type: 'alert',
+      attributes: {
+        apiKey: Buffer.from('123:abc').toString('base64'),
+      },
+      references: [],
+    });
+
+    const logger = taskRunnerFactoryInitializerParams.logger;
+    return taskRunner.run().catch((ex) => {
+      expect(ex).toMatchInlineSnapshot(`[Error: Saved object [alert/1] not found]`);
+      expect(logger.debug).toHaveBeenCalledWith(
+        `Executing Alert "1" has resulted in Error: Saved object [alert/1] not found`
+      );
+      expect(logger.warn).toHaveBeenCalledTimes(1);
+      expect(logger.warn).nthCalledWith(
+        1,
+        `Unable to execute rule "1" in the "test space" space because Saved object [alert/1] not found - this rule will not be rescheduled. To restart rule execution, try disabling and re-enabling this rule.`
+      );
     });
   });
 
