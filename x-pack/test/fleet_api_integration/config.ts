5c18e47ec69819fa254629f85709b9519f82d123
John Schulz <john.schulz@elastic.co>
2021-06-09 08:57:52 -0400

    Support using a different registry Docker image

--- x-pack/test/fleet_api_integration/config.ts
+++ x-pack/test/fleet_api_integration/config.ts
@@ -15,6 +15,7 @@ import { defineDockerServersConfig } from '@kbn/test';
 // example: https://beats-ci.elastic.co/blue/organizations/jenkins/Ingest-manager%2Fpackage-storage/detail/snapshot/74/pipeline/257#step-302-log-1.
 // It should be updated any time there is a new Docker image published for the Snapshot Distribution of the Package Registry.
 export const dockerImage =
+  process.env.FLEET_PACKAGE_REGISTRY_DOCKER_IMAGE ||
   'docker.elastic.co/package-registry/distribution:b6a53ac9300333a4a45f3f7d350c9aed72061a66';
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
@@ -51,17 +52,11 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
       },
     }),
     esArchiver: xPackAPITestsConfig.get('esArchiver'),
-    services: {
-      ...xPackAPITestsConfig.get('services'),
-    },
+    services: xPackAPITestsConfig.get('services'),
     junit: {
       reportName: 'X-Pack EPM API Integration Tests',
     },
-
-    esTestCluster: {
-      ...xPackAPITestsConfig.get('esTestCluster'),
-    },
-
+    esTestCluster: xPackAPITestsConfig.get('esTestCluster'),
     kbnTestServer: {
       ...xPackAPITestsConfig.get('kbnTestServer'),
       serverArgs: [
