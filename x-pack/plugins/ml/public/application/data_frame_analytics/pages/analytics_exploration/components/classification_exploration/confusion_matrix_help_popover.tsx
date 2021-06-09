64194af02fb7ccd3cb02d8234021933bebde629d
István Zoltán Szabó <istvan.szabo@elastic.co>
2021-06-09 14:37:45 +0200

    [ML] Changes string IDs.

--- x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_exploration/components/classification_exploration/confusion_matrix_help_popover.tsx
+++ x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_exploration/components/classification_exploration/confusion_matrix_help_popover.tsx
@@ -28,31 +28,31 @@ export const MulticlassConfusionMatrixHelpPopover = () => {
       }
       closePopover={() => setIsPopoverOpen(false)}
       isOpen={isPopoverOpen}
-      title={i18n.translate('xpack.ml.dataframeanalytics.confusionMatrixPopoverTitle', {
+      title={i18n.translate('xpack.ml.dataframe.analytics.confusionMatrixPopoverTitle', {
         defaultMessage: 'Multiclass confusion matrix',
       })}
     >
       <p>
         <FormattedMessage
-          id="xpack.ml.dataframeanalytics.confusionMatrixBasicExplanation"
+          id="xpack.ml.dataframe.analytics.confusionMatrixBasicExplanation"
           defaultMessage="The multiclass confusion matrix provides a summary of the performance of the classification analysis. It contains the proportion of the data points that the analysis classified correctly with their actual class as well as the proportion of the misclassified data points."
         />
       </p>
       <p>
         <FormattedMessage
-          id="xpack.ml.dataframeanalytics.confusionMatrixAxisExplanation"
+          id="xpack.ml.dataframe.analytics.confusionMatrixAxisExplanation"
           defaultMessage="The matrix contains the actual labels on the left side while the predicted labels are on the top. The proportion of correct and incorrect predictions is broken down for each class. This enables you to examine how the classification analysis confused the different classes while it made its predictions."
         />
       </p>
       <p>
         <FormattedMessage
-          id="xpack.ml.dataframeanalytics.confusionMatrixShadeExplanation"
+          id="xpack.ml.dataframe.analytics.confusionMatrixShadeExplanation"
           defaultMessage="As the number of classes in the classification analysis increases, the confusion matrix also increases in complexity. For an easier overview, darker cells indicate higher percentage of the predictions."
         />
       </p>
       <p>
         <FormattedMessage
-          id="xpack.ml.dataframeanalytics.confusionMatrixColumnExplanation"
+          id="xpack.ml.dataframe.analytics.confusionMatrixColumnExplanation"
           defaultMessage="The Columns selector enables you to toggle between showing or hiding some of the columns or all of them."
         />
       </p>
