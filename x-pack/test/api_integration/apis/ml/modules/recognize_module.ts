62f256eb33134f84860130b032ef0031b6b6c4e0
Apoorva <appujo@gmail.com>
2021-06-09 14:37:56 -0700

    Changing memory limits and linting change

--- x-pack/test/api_integration/apis/ml/modules/recognize_module.ts
+++ x-pack/test/api_integration/apis/ml/modules/recognize_module.ts
@@ -139,7 +139,12 @@ export default ({ getService }: FtrProviderContext) => {
       user: USER.ML_POWERUSER,
       expected: {
         responseCode: 200,
-        moduleIds: ['auditbeat_process_hosts_ecs', 'security_linux', 'security_auth', 'siem_auditbeat'],
+        moduleIds: [
+          'auditbeat_process_hosts_ecs',
+          'security_linux',
+          'security_auth',
+          'siem_auditbeat'
+        ],
       },
     },
     {
