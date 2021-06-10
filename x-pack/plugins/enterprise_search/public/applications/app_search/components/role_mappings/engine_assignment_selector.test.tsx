d7d67df5ebebad90ce7d15c492950327a127b799
Scotty Bollinger <scotty.bollinger@elastic.co>
2021-06-10 12:36:11 -0500

    [Enterprise Search] Create reusable Group and Engine assignment selectors (#101790)
    * Create AssignmentSelectors
    
    These components will be used in both the Role Mapping and User flyouts to create and edit role mappings and users, respectively
    
    * Implement AssignmentSelectors in components

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/role_mappings/engine_assignment_selector.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/role_mappings/engine_assignment_selector.test.tsx
@@ -13,17 +13,16 @@ import { engines } from '../../__mocks__/engines.mock';
 
 import React from 'react';
 
+import { waitFor } from '@testing-library/dom';
 import { shallow } from 'enzyme';
 
-import { AttributeSelector, RoleSelector, RoleMappingFlyout } from '../../../shared/role_mapping';
+import { EuiComboBox, EuiComboBoxOptionOption, EuiRadioGroup } from '@elastic/eui';
+
 import { asRoleMapping } from '../../../shared/role_mapping/__mocks__/roles';
 
-import { STANDARD_ROLE_TYPES } from './constants';
-
 import { EngineAssignmentSelector } from './engine_assignment_selector';
-import { RoleMapping } from './role_mapping';
 
-describe('RoleMapping', () => {
+describe('EngineAssignmentSelector', () => {
   const mockRole = DEFAULT_INITIAL_APP_DATA.appSearch.role;
   const actions = {
     initializeRoleMappings: jest.fn(),
@@ -67,29 +66,36 @@ describe('RoleMapping', () => {
 
   it('renders', () => {
     setMockValues({ ...mockValues, roleMapping: asRoleMapping });
-    const wrapper = shallow(<RoleMapping />);
+    const wrapper = shallow(<EngineAssignmentSelector />);
 
-    expect(wrapper.find(AttributeSelector)).toHaveLength(1);
-    expect(wrapper.find(RoleSelector)).toHaveLength(1);
-    expect(wrapper.find(EngineAssignmentSelector)).toHaveLength(1);
+    expect(wrapper.find(EuiRadioGroup)).toHaveLength(1);
+    expect(wrapper.find(EuiComboBox)).toHaveLength(1);
   });
 
-  it('only passes standard role options for non-advanced roles', () => {
-    setMockValues({ ...mockValues, hasAdvancedRoles: false });
-    const wrapper = shallow(<RoleMapping />);
+  it('sets initial selected state when accessAllEngines is true', () => {
+    setMockValues({ ...mockValues, accessAllEngines: true });
+    const wrapper = shallow(<EngineAssignmentSelector />);
 
-    expect(wrapper.find(RoleSelector).prop('roleOptions')).toHaveLength(STANDARD_ROLE_TYPES.length);
+    expect(wrapper.find(EuiRadioGroup).prop('idSelected')).toBe('all');
   });
 
-  it('enables flyout when attribute value is valid', () => {
-    setMockValues({
-      ...mockValues,
-      attributeValue: 'foo',
-      attributeName: 'role',
-      accessAllEngines: true,
-    });
-    const wrapper = shallow(<RoleMapping />);
+  it('handles all/specific engines radio change', () => {
+    const wrapper = shallow(<EngineAssignmentSelector />);
+    const radio = wrapper.find(EuiRadioGroup);
+    radio.simulate('change', { target: { checked: false } });
 
-    expect(wrapper.find(RoleMappingFlyout).prop('disabled')).toBe(false);
+    expect(actions.handleAccessAllEnginesChange).toHaveBeenCalledWith(false);
+  });
+
+  it('handles engine checkbox click', async () => {
+    const wrapper = shallow(<EngineAssignmentSelector />);
+    await waitFor(() =>
+      ((wrapper.find(EuiComboBox).props() as unknown) as {
+        onChange: (a: EuiComboBoxOptionOption[]) => void;
+      }).onChange([{ label: engines[0].name, value: engines[0].name }])
+    );
+    wrapper.update();
+
+    expect(actions.handleEngineSelectionChange).toHaveBeenCalledWith([engines[0].name]);
   });
 });
