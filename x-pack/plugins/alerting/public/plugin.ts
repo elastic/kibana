62a781f3ac24a3b67787261aa89b128fb084fb26
Stacey Gammon <gammon@elastic.co>
2021-06-10 13:08:14 -0400

    Update x-pack/plugins/alerting/public/plugin.ts
    Co-authored-by: ymao1 <ying.mao@elastic.co>

--- x-pack/plugins/alerting/public/plugin.ts
+++ x-pack/plugins/alerting/public/plugin.ts
@@ -16,7 +16,7 @@ export interface PluginSetupContract {
    * Register a customized view of the particular rule type. Stack Management provides a generic overview, but a developer can register a
    * custom navigation to provide the user an extra link to a more curated view. The alerting plugin doesn't actually do
    * anything with this information, but it can be used by other plugins via the `getNavigation` functionality. Currently
-   * the trigger_actions_ui plugin uses it to expose the link from the generic rule view in Stack Management.
+   * the trigger_actions_ui plugin uses it to expose the link from the generic rule details view in Stack Management.
    *
    * @param applicationId The application id that the user should be navigated to, to view a particular alert in a custom way.
    * @param ruleType The rule type that has been registered with Alerting.Server.PluginSetupContract.registerType. If
