856cefbb5f47bb17b95b55cd81c848c87549a4bf
Ashokaditya <am.struktr@gmail.com>
2021-06-10 17:19:44 +0200

    move handler logic to a service
    review changes

--- x-pack/plugins/security_solution/server/endpoint/routes/actions/audit_log_handler.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/actions/audit_log_handler.ts
@@ -6,12 +6,11 @@
  */
 
 import { RequestHandler } from 'kibana/server';
-import { AGENT_ACTIONS_INDEX, AGENT_ACTIONS_RESULTS_INDEX } from '../../../../../fleet/common';
 import {
   EndpointActionLogRequestParams,
   EndpointActionLogRequestQuery,
 } from '../../../../common/endpoint/schema/actions';
-
+import { getAuditLogResponse } from './service';
 import { SecuritySolutionRequestHandlerContext } from '../../../types';
 import { EndpointAppContext } from '../../types';
 
@@ -31,64 +30,9 @@ export const actionsLogRequestHandler = (
       query: { page = 1, page_size: pageSize = 50 },
     } = req;
 
-    const size = pageSize;
-    const from = page <= 1 ? 0 : page * pageSize - pageSize + 1;
-
-    const options = {
-      headers: {
-        'X-elastic-product-origin': 'fleet',
-      },
-      ignore: [404],
-    };
-    const esClient = context.core.elasticsearch.client.asCurrentUser;
-    let result;
-
-    try {
-      result = await esClient.search(
-        {
-          index: [AGENT_ACTIONS_INDEX, AGENT_ACTIONS_RESULTS_INDEX],
-          size,
-          from,
-          body: {
-            query: {
-              bool: {
-                should: [
-                  { terms: { agents: [elasticAgentId] } },
-                  { terms: { agent_id: [elasticAgentId] } },
-                ],
-              },
-            },
-            sort: [
-              {
-                '@timestamp': {
-                  order: 'desc',
-                },
-              },
-            ],
-          },
-        },
-        options
-      );
-    } catch (error) {
-      logger.error(error);
-      throw error;
-    }
-    if (result?.statusCode !== 200) {
-      logger.error(`Error fetching actions log for agent_id ${req.params.agent_id}`);
-      throw new Error(`Error fetching actions log for agent_id ${req.params.agent_id}`);
-    }
-
+    const body = await getAuditLogResponse({ elasticAgentId, page, pageSize, context, logger });
     return res.ok({
-      body: {
-        total:
-          typeof result.body.hits.total === 'number'
-            ? result.body.hits.total
-            : result.body.hits.total.value,
-        items: result.body.hits.hits.map((e) => ({
-          type: e._index.startsWith('.fleet-actions') ? 'action' : 'response',
-          item: { id: e._id, data: e._source },
-        })),
-      },
+      body,
     });
   };
 };
