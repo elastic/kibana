b8aa4557316d8cb693774229fea7453e356d7e56
lcawl <lcawley@elastic.co>
2021-06-10 16:09:50 -0700

    [Monitoring] Updated Kibana alerting/rules language

--- x-pack/plugins/monitoring/public/alerts/configuration.tsx
+++ x-pack/plugins/monitoring/public/alerts/configuration.tsx
@@ -111,8 +111,8 @@ export const AlertConfiguration: React.FC<Props> = (props: Props) => {
               hideBottomBar();
             }}
           >
-            {i18n.translate('xpack.monitoring.alerts.panel.editAlert', {
-              defaultMessage: `Edit alert`,
+            {i18n.translate('xpack.monitoring.alerts.panel.editRule', {
+              defaultMessage: `Edit rule`,
             })}
           </EuiButton>
         </EuiFlexItem>
