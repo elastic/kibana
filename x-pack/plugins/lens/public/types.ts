dc9c7cbb8b411b8b61aa79db051342f7c538059f
Joe Reuter <johannes.reuter@elastic.co>
2021-06-10 14:14:36 +0200

    rename beta badge

--- x-pack/plugins/lens/public/types.ts
+++ x-pack/plugins/lens/public/types.ts
@@ -49,6 +49,7 @@ export interface EditorFrameProps {
   initialContext?: VisualizeFieldContext;
   showNoDataPopover: () => void;
 }
+
 export interface EditorFrameInstance {
   EditorFrameContainer: (props: EditorFrameProps) => React.ReactElement;
 }
@@ -561,9 +562,9 @@ export interface VisualizationType {
    */
   sortPriority?: number;
   /**
-   * Indicates if visualization is in the beta stage.
+   * Indicates if visualization is in the experimental stage.
    */
-  showBetaBadge?: boolean;
+  showExperimentalBadge?: boolean;
 }
 
 export interface Visualization<T = unknown> {
@@ -725,6 +726,7 @@ interface LensEditContextMapping {
   [LENS_EDIT_RESIZE_ACTION]: LensResizeActionData;
   [LENS_TOGGLE_ACTION]: LensToggleActionData;
 }
+
 type LensEditSupportedActions = keyof LensEditContextMapping;
 
 export type LensEditPayload<T extends LensEditSupportedActions> = {
@@ -737,6 +739,7 @@ export interface LensEditEvent<T> {
   name: 'edit';
   data: EditPayloadContext<T>;
 }
+
 export interface LensTableRowContextMenuEvent {
   name: 'tableRowContextMenuClick';
   data: RowClickContext['data'];
