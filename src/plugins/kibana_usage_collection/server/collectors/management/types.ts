0bc12e16dee35b529b115f498557cf0dcf9d7cb8
Alexey Antonov <alexwizp@gmail.com>
2021-06-11 10:47:12 +0300

    fix CI

--- src/plugins/kibana_usage_collection/server/collectors/management/types.ts
+++ src/plugins/kibana_usage_collection/server/collectors/management/types.ts
@@ -42,6 +42,7 @@ export interface UsageStats {
   'visualization:tileMap:maxPrecision': number;
   'csv:separator': string;
   'visualization:tileMap:WMSdefaults': string;
+  'timelion:legacyChartsLibrary': boolean;
   'timelion:target_buckets': number;
   'timelion:max_buckets': number;
   'timelion:es.timefield': string;
