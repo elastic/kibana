11c9892eba8a2a5f1f73bc007b43ae2ccde5c72b
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-06-14 16:53:11 +0300

    Moved type of element to types.

--- src/plugins/expression_reveal_image/common/types/elements.ts
+++ src/plugins/expression_reveal_image/common/types/elements.ts
@@ -5,6 +5,16 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-export * from './expression_functions';
-export * from './expression_renderers';
-export * from './elements';
+export interface ElementSpec {
+  name: string;
+  icon?: string;
+  expression: string;
+  displayName?: string;
+  type?: string;
+  help?: string;
+  filter?: string;
+  width?: number;
+  height?: number;
+}
+
+export type ElementFactory = () => ElementSpec;
