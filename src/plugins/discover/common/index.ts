7db4beb3b0445c7a9e0a5525490f4a3faebc7863
Maja Grubic <maja.grubic@elastic.co>
2021-06-14 09:35:12 +0100

    Fix telemetry

--- src/plugins/discover/common/index.ts
+++ src/plugins/discover/common/index.ts
@@ -19,4 +19,4 @@ export const DOC_TABLE_LEGACY = 'doc_table:legacy';
 export const MODIFY_COLUMNS_ON_SWITCH = 'discover:modifyColumnsOnSwitch';
 export const SEARCH_FIELDS_FROM_SOURCE = 'discover:searchFieldsFromSource';
 export const MAX_DOC_FIELDS_DISPLAYED = 'discover:maxDocFieldsDisplayed';
-export const SHOW_MULTIFIELDS = 'discover:showMultifields';
+export const SHOW_MULTIFIELDS = 'discover:showMultiFields';
