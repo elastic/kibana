94b7dbc491fbf08e3dd99d399041852b3fbc1af9
Josh Dover <1813008+joshdover@users.noreply.github.com>
2021-06-14 18:46:08 +0200

    Revert "Revert "Display Kibana overall status in the logs and have FTR wait for green status before running tests (#92568)" (#93855)"
    This reverts commit 020a8ee7dd6a54059e8d3cf2358a3058627c62d1.

--- packages/kbn-test/src/functional_tests/lib/run_kibana_server.js
+++ packages/kbn-test/src/functional_tests/lib/run_kibana_server.js
@@ -38,7 +38,7 @@ export async function runKibanaServer({ procs, config, options }) {
       ...extendNodeOptions(installDir),
     },
     cwd: installDir || KIBANA_ROOT,
-    wait: /\[Kibana\]\[http\] http server running/,
+    wait: /Kibana is now available/,
   });
 }
 
