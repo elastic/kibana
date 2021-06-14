38e516e65fdf277316e071a6980ba7e1b9da6cd1
Jonathan Budzenski <jon@budzenski.me>
2021-06-14 11:28:09 -0500

    fix import

--- packages/kbn-interpreter/src/common/lib/ast.ts
+++ packages/kbn-interpreter/src/common/lib/ast.ts
@@ -8,7 +8,7 @@
 
 import { getType } from './get_type';
 // @ts-expect-error
-import { parse } from '../../../grammar/grammar';
+import { parse } from '../../../grammar';
 
 export type ExpressionArgAST = string | boolean | number | Ast;
 
