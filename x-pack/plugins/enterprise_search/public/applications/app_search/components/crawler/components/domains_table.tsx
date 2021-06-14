f820eda202c8c61ed21c98d9a51f2e418a1e4944
Byron Hulcher <byronhulcher@gmail.com>
2021-06-14 19:20:08 -0400

    Clean up DomainsTable

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/domains_table.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/domains_table.tsx
@@ -13,11 +13,13 @@ import { EuiInMemoryTable, EuiBasicTableColumn } from '@elastic/eui';
 
 import { i18n } from '@kbn/i18n';
 
+import { FormattedNumber } from '@kbn/i18n/react';
+
 import { DELETE_BUTTON_LABEL, MANAGE_BUTTON_LABEL } from '../../../../shared/constants';
 import { KibanaLogic } from '../../../../shared/kibana';
 import { AppLogic } from '../../../app_logic';
 import { ENGINE_CRAWLER_DOMAIN_PATH } from '../../../routes';
-import { EngineLogic, generateEnginePath } from '../../engine';
+import { generateEnginePath } from '../../engine';
 import { CrawlerOverviewLogic } from '../crawler_overview_logic';
 import { CrawlerDomain } from '../types';
 
@@ -28,8 +30,6 @@ export const DomainsTable: React.FC = () => {
 
   const { deleteDomain } = useActions(CrawlerOverviewLogic);
 
-  const { engineName } = useValues(EngineLogic);
-
   const {
     myRole: { canManageEngineCrawler },
   } = useValues(AppLogic);
@@ -37,50 +37,64 @@ export const DomainsTable: React.FC = () => {
   const columns: Array<EuiBasicTableColumn<CrawlerDomain>> = [
     {
       field: 'url',
-      name: 'Domain URL',
+      name: i18n.translate(
+        'xpack.enterpriseSearch.appSearch.crawler.domainsTable.column.domainURL',
+        {
+          defaultMessage: 'Domain URL',
+        }
+      ),
     },
     {
       field: 'lastCrawl',
-      name: 'Last activity',
+      name: i18n.translate(
+        'xpack.enterpriseSearch.appSearch.crawler.domainsTable.column.lastActivity',
+        {
+          defaultMessage: 'Last activity',
+        }
+      ),
       render: (lastCrawl: CrawlerDomain['lastCrawl']) =>
         lastCrawl ? determineTimestampDisplay(lastCrawl) : '',
     },
     {
       field: 'documentCount',
-      name: 'Documents',
-      render: (documentCount: CrawlerDomain['documentCount']) => documentCount.toLocaleString(),
+      name: i18n.translate(
+        'xpack.enterpriseSearch.appSearch.crawler.domainsTable.column.documents',
+        {
+          defaultMessage: 'Documents',
+        }
+      ),
+      render: (documentCount: CrawlerDomain['documentCount']) => (
+        <FormattedNumber value={documentCount} />
+      ),
     },
   ];
 
   const actionsColumn: EuiBasicTableColumn<CrawlerDomain> = {
-    name: i18n.translate(
-      'xpack.enterpriseSearch.appSearch.crawlerOverview.domainsTable.column.actions',
-      {
-        defaultMessage: 'Actions',
-      }
-    ),
+    name: i18n.translate('xpack.enterpriseSearch.appSearch.crawler.domainsTable.column.actions', {
+      defaultMessage: 'Actions',
+    }),
     actions: [
       {
         name: MANAGE_BUTTON_LABEL,
         description: i18n.translate(
-          'xpack.enterpriseSearch.appSearch.crawlerOverview.domainsTable.action.manage.buttonDescription',
+          'xpack.enterpriseSearch.appSearch.crawler.domainsTable.action.manage.buttonLabel',
           {
-            defaultMessage: 'Manage this engine',
+            defaultMessage: 'Manage this domain',
           }
         ),
         type: 'icon',
         icon: 'eye',
         onClick: (domain) =>
           KibanaLogic.values.navigateToUrl(
-            generateEnginePath(ENGINE_CRAWLER_DOMAIN_PATH, { engineName, domainId: domain.id })
+            generateEnginePath(ENGINE_CRAWLER_DOMAIN_PATH, { domainId: domain.id })
           ),
       },
       {
         name: DELETE_BUTTON_LABEL,
         description: i18n.translate(
-          'xpack.enterpriseSearch.appSearch.crawlerOverview.domainsTable.action.delete.buttonDescription',
+          'xpack.enterpriseSearch.appSearch.crawler.domainsTable.action.delete.buttonLabel',
           {
-            defaultMessage: 'Delete this engine',
+            defaultMessage: 'Delete this domain',
           }
         ),
         type: 'icon',
@@ -90,7 +104,7 @@ export const DomainsTable: React.FC = () => {
           if (
             window.confirm(
               i18n.translate(
-                'xpack.enterpriseSearch.appSearch.crawlerOverview.domainsTable.action.delete.confirmationPopupMessage',
+                'xpack.enterpriseSearch.appSearch.crawler.domainsTable.action.delete.confirmationPopupMessage',
                 {
                   defaultMessage:
                     'Are you sure you want to remove the domain "{domainUrl}" and all of its settings?',
@@ -112,13 +126,5 @@ export const DomainsTable: React.FC = () => {
     columns.push(actionsColumn);
   }
 
-  return (
-    <EuiInMemoryTable
-      data-test-subj="DomainsTable"
-      items={domains || []}
-      loading={domains === null}
-      columns={columns}
-      sorting
-    />
-  );
+  return <EuiInMemoryTable items={domains} columns={columns} />;
 };
