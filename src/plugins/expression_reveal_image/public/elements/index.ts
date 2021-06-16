7b2b608f878b408248b2f39d2f8186d505c44b86
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-06-16 12:56:01 +0300

    Small imports fix.

--- src/plugins/expression_reveal_image/public/elements/index.ts
+++ src/plugins/expression_reveal_image/public/elements/index.ts
@@ -5,17 +5,9 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
+import { translateElements } from '../../common/lib/translate_elements';
+import { revealImageElement } from './reveal_image_element';
 
-import { ElementFactory } from '../../common/types';
+export const elements = translateElements([revealImageElement]);
 
-export const revealImageElement: ElementFactory = () => ({
-  name: 'revealImage',
-  displayName: 'Image reveal',
-  type: 'image',
-  help: 'Reveals a percentage of an image',
-  expression: `filters
-    | demodata
-    | math "mean(percent_uptime)"
-    | revealImage origin=bottom image=null
-    | render`,
-});
+export { revealImageElement };
---
+++ src/plugins/expression_reveal_image/public/elements/reveal_image_element.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { ElementFactory } from '../../common/types';
+
+export const revealImageElement: ElementFactory = () => ({
+  name: 'revealImage',
+  displayName: 'Image reveal',
+  type: 'image',
+  help: 'Reveals a percentage of an image',
+  expression: `filters
+    | demodata
+    | math "mean(percent_uptime)"
+    | revealImage origin=bottom image=null
+    | render`,
+});
