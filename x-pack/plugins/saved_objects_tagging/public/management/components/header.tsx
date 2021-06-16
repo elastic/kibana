f940cbc7a77d3989919f95ef13c7b0e2bd3b01fa
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-06-16 08:10:12 -0700

    Deletes unused imports

--- x-pack/plugins/saved_objects_tagging/public/management/components/header.tsx
+++ x-pack/plugins/saved_objects_tagging/public/management/components/header.tsx
@@ -6,16 +6,7 @@
  */
 
 import React, { FC } from 'react';
-import {
-  EuiSpacer,
-  EuiTitle,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiText,
-  EuiTextColor,
-  EuiButton,
-  EuiPageHeader,
-} from '@elastic/eui';
+import { EuiButton, EuiPageHeader } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 
 interface HeaderProps {
@@ -57,49 +48,6 @@ export const Header: FC<HeaderProps> = ({ canCreate, onCreate }) => {
           </EuiButton>
         ),
       ]}
-    >
-      {/* <EuiFlexGroup justifyContent="spaceBetween" alignItems="baseline">
-        <EuiFlexItem grow={false}>
-          <EuiTitle>
-            <h1>
-              <FormattedMessage
-                id="xpack.savedObjectsTagging.management.header.title"
-                defaultMessage="Tags"
-              />
-            </h1>
-          </EuiTitle>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          {canCreate && (
-            <EuiButton
-              key="createTag"
-              iconType="tag"
-              color="primary"
-              fill
-              data-test-subj="createTagButton"
-              onClick={onCreate}
-              isDisabled={false}
-            >
-              <FormattedMessage
-                id="xpack.savedObjectsTagging.management.actions.createTag"
-                defaultMessage="Create tag"
-              />
-            </EuiButton>
-          )}
-        </EuiFlexItem>
-      </EuiFlexGroup>
-      <EuiSpacer size="m" />
-      <EuiText size="s">
-        <p>
-          <EuiTextColor color="subdued">
-            <FormattedMessage
-              id="xpack.savedObjectsTagging.management.header.description"
-              defaultMessage="Use tags to categorize and easily find your objects."
-            />
-          </EuiTextColor>
-        </p>
-      </EuiText>
-      <EuiSpacer size="m" /> */}
-    </EuiPageHeader>
+    />
   );
 };
