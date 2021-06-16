df5c56d625500da60180c30264b11d81fb3b415a
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-06-16 18:20:28 -0400

    [Uptime] refactor Synthetics Integration package UI (#102080) (#102404)
    * refactor contexts
    
    * add http, tcp, and icmp folders
    
    * adjust types
    
    * adjust useUpdatePolicy hook
    
    * adjust synthetics policy create and edit wrappers
    
    * adjust validation
    
    * fix typo and types
    
    * remove typo
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Dominique Clarke <doclarke71@gmail.com>

--- x-pack/plugins/uptime/public/components/fleet_package/custom_fields.tsx
+++ x-pack/plugins/uptime/public/components/fleet_package/custom_fields.tsx
@@ -5,28 +5,26 @@
  * 2.0.
  */
 
-import React, { useEffect, useState, memo } from 'react';
+import React, { useState, memo } from 'react';
 import { FormattedMessage } from '@kbn/i18n/react';
 import {
   EuiFlexGroup,
   EuiFlexItem,
   EuiForm,
   EuiFormRow,
-  EuiFieldText,
-  EuiFieldNumber,
   EuiSelect,
   EuiSpacer,
   EuiDescribedFormGroup,
   EuiCheckbox,
 } from '@elastic/eui';
-import { ConfigKeys, DataStream, ISimpleFields, Validation } from './types';
-import { useSimpleFieldsContext } from './contexts';
+import { ConfigKeys, DataStream, Validation } from './types';
+import { useMonitorTypeContext } from './contexts';
 import { TLSFields, TLSRole } from './tls_fields';
-import { ComboBox } from './combo_box';
-import { OptionalLabel } from './optional_label';
-import { HTTPAdvancedFields } from './http_advanced_fields';
-import { TCPAdvancedFields } from './tcp_advanced_fields';
-import { ScheduleField } from './schedule_field';
+import { HTTPSimpleFields } from './http/simple_fields';
+import { HTTPAdvancedFields } from './http/advanced_fields';
+import { TCPSimpleFields } from './tcp/simple_fields';
+import { TCPAdvancedFields } from './tcp/advanced_fields';
+import { ICMPSimpleFields } from './icmp/simple_fields';
 
 interface Props {
   typeEditable?: boolean;
@@ -37,26 +35,22 @@ interface Props {
 export const CustomFields = memo<Props>(
   ({ typeEditable = false, isTLSEnabled: defaultIsTLSEnabled = false, validate }) => {
     const [isTLSEnabled, setIsTLSEnabled] = useState<boolean>(defaultIsTLSEnabled);
-    const { fields, setFields, defaultValues } = useSimpleFieldsContext();
-    const { type } = fields;
+    const { monitorType, setMonitorType } = useMonitorTypeContext();
 
-    const isHTTP = fields[ConfigKeys.MONITOR_TYPE] === DataStream.HTTP;
-    const isTCP = fields[ConfigKeys.MONITOR_TYPE] === DataStream.TCP;
-    const isICMP = fields[ConfigKeys.MONITOR_TYPE] === DataStream.ICMP;
+    const isHTTP = monitorType === DataStream.HTTP;
+    const isTCP = monitorType === DataStream.TCP;
 
-    // reset monitor type specific fields any time a monitor type is switched
-    useEffect(() => {
-      if (typeEditable) {
-        setFields((prevFields: ISimpleFields) => ({
-          ...prevFields,
-          [ConfigKeys.HOSTS]: defaultValues[ConfigKeys.HOSTS],
-          [ConfigKeys.URLS]: defaultValues[ConfigKeys.URLS],
-        }));
+    const renderSimpleFields = (type: DataStream) => {
+      switch (type) {
+        case DataStream.HTTP:
+          return <HTTPSimpleFields validate={validate} />;
+        case DataStream.ICMP:
+          return <ICMPSimpleFields validate={validate} />;
+        case DataStream.TCP:
+          return <TCPSimpleFields validate={validate} />;
+        default:
+          return null;
       }
-    }, [defaultValues, type, typeEditable, setFields]);
-
-    const handleInputChange = ({ value, configKey }: { value: unknown; configKey: ConfigKeys }) => {
-      setFields((prevFields) => ({ ...prevFields, [configKey]: value }));
     };
 
     return (
@@ -88,7 +82,7 @@ export const CustomFields = memo<Props>(
                       defaultMessage="Monitor Type"
                     />
                   }
-                  isInvalid={!!validate[ConfigKeys.MONITOR_TYPE]?.(fields[ConfigKeys.MONITOR_TYPE])}
+                  isInvalid={!!validate[ConfigKeys.MONITOR_TYPE]?.(monitorType)}
                   error={
                     <FormattedMessage
                       id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.monitorType.error"
@@ -98,281 +92,13 @@ export const CustomFields = memo<Props>(
                 >
                   <EuiSelect
                     options={dataStreamOptions}
-                    value={fields[ConfigKeys.MONITOR_TYPE]}
-                    onChange={(event) =>
-                      handleInputChange({
-                        value: event.target.value,
-                        configKey: ConfigKeys.MONITOR_TYPE,
-                      })
-                    }
+                    value={monitorType}
+                    onChange={(event) => setMonitorType(event.target.value as DataStream)}
                     data-test-subj="syntheticsMonitorTypeField"
                   />
                 </EuiFormRow>
               )}
-              {isHTTP && (
-                <EuiFormRow
-                  label={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.URL"
-                      defaultMessage="URL"
-                    />
-                  }
-                  isInvalid={!!validate[ConfigKeys.URLS]?.(fields[ConfigKeys.URLS])}
-                  error={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.URL.error"
-                      defaultMessage="URL is required"
-                    />
-                  }
-                >
-                  <EuiFieldText
-                    value={fields[ConfigKeys.URLS]}
-                    onChange={(event) =>
-                      handleInputChange({ value: event.target.value, configKey: ConfigKeys.URLS })
-                    }
-                    data-test-subj="syntheticsUrlField"
-                  />
-                </EuiFormRow>
-              )}
-              {isTCP && (
-                <EuiFormRow
-                  label={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.tcp.hosts"
-                      defaultMessage="Host:Port"
-                    />
-                  }
-                  isInvalid={!!validate[ConfigKeys.HOSTS]?.(fields[ConfigKeys.HOSTS])}
-                  error={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.tcp.hosts.error"
-                      defaultMessage="Host and port are required"
-                    />
-                  }
-                >
-                  <EuiFieldText
-                    value={fields[ConfigKeys.HOSTS]}
-                    onChange={(event) =>
-                      handleInputChange({
-                        value: event.target.value,
-                        configKey: ConfigKeys.HOSTS,
-                      })
-                    }
-                    data-test-subj="syntheticsTCPHostField"
-                  />
-                </EuiFormRow>
-              )}
-              {isICMP && (
-                <EuiFormRow
-                  label={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.icmp.hosts"
-                      defaultMessage="Host"
-                    />
-                  }
-                  isInvalid={!!validate[ConfigKeys.HOSTS]?.(fields[ConfigKeys.HOSTS])}
-                  error={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.icmp.hosts.error"
-                      defaultMessage="Host is required"
-                    />
-                  }
-                >
-                  <EuiFieldText
-                    value={fields[ConfigKeys.HOSTS]}
-                    onChange={(event) =>
-                      handleInputChange({
-                        value: event.target.value,
-                        configKey: ConfigKeys.HOSTS,
-                      })
-                    }
-                    data-test-subj="syntheticsICMPHostField"
-                  />
-                </EuiFormRow>
-              )}
-              <EuiFormRow
-                id="syntheticsFleetScheduleField--number syntheticsFleetScheduleField--unit"
-                label={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.monitorInterval"
-                    defaultMessage="Monitor interval"
-                  />
-                }
-                isInvalid={!!validate[ConfigKeys.SCHEDULE]?.(fields[ConfigKeys.SCHEDULE])}
-                error={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.monitorInterval.error"
-                    defaultMessage="Monitor interval is required"
-                  />
-                }
-              >
-                <ScheduleField
-                  onChange={(schedule) =>
-                    handleInputChange({
-                      value: schedule,
-                      configKey: ConfigKeys.SCHEDULE,
-                    })
-                  }
-                  number={fields[ConfigKeys.SCHEDULE].number}
-                  unit={fields[ConfigKeys.SCHEDULE].unit}
-                />
-              </EuiFormRow>
-              {isICMP && (
-                <EuiFormRow
-                  label={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.wait.label"
-                      defaultMessage="Wait in seconds"
-                    />
-                  }
-                  isInvalid={!!validate[ConfigKeys.WAIT]?.(fields[ConfigKeys.WAIT])}
-                  error={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.wait.error"
-                      defaultMessage="Wait must be 0 or greater"
-                    />
-                  }
-                  labelAppend={<OptionalLabel />}
-                  helpText={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.wait.helpText"
-                      defaultMessage="The duration to wait before emitting another ICMP Echo Request if no response is received."
-                    />
-                  }
-                >
-                  <EuiFieldNumber
-                    min={0}
-                    value={fields[ConfigKeys.WAIT]}
-                    onChange={(event) =>
-                      handleInputChange({ value: event.target.value, configKey: ConfigKeys.WAIT })
-                    }
-                    step={'any'}
-                  />
-                </EuiFormRow>
-              )}
-              <EuiFormRow
-                label={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.APMServiceName.label"
-                    defaultMessage="APM service name"
-                  />
-                }
-                labelAppend={<OptionalLabel />}
-                helpText={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.APMServiceName.helpText"
-                    defaultMessage="APM service name for this monitor. Corresponds to the service.name ECS field. Set this when monitoring an app that is also using APM to enable integrations between Uptime and APM data in Kibana."
-                  />
-                }
-              >
-                <EuiFieldText
-                  value={fields[ConfigKeys.APM_SERVICE_NAME]}
-                  onChange={(event) =>
-                    handleInputChange({
-                      value: event.target.value,
-                      configKey: ConfigKeys.APM_SERVICE_NAME,
-                    })
-                  }
-                  data-test-subj="syntheticsAPMServiceName"
-                />
-              </EuiFormRow>
-              {isHTTP && (
-                <EuiFormRow
-                  label={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.maxRedirects"
-                      defaultMessage="Max redirects"
-                    />
-                  }
-                  isInvalid={
-                    !!validate[ConfigKeys.MAX_REDIRECTS]?.(fields[ConfigKeys.MAX_REDIRECTS])
-                  }
-                  error={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.maxRedirects.error"
-                      defaultMessage="Max redirects must be 0 or greater"
-                    />
-                  }
-                  labelAppend={<OptionalLabel />}
-                  helpText={
-                    <FormattedMessage
-                      id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.maxRedirects.helpText"
-                      defaultMessage="The total number of redirections to follow."
-                    />
-                  }
-                >
-                  <EuiFieldNumber
-                    min={0}
-                    value={fields[ConfigKeys.MAX_REDIRECTS]}
-                    onChange={(event) =>
-                      handleInputChange({
-                        value: event.target.value,
-                        configKey: ConfigKeys.MAX_REDIRECTS,
-                      })
-                    }
-                  />
-                </EuiFormRow>
-              )}
-              <EuiFormRow
-                label={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.timeout.label"
-                    defaultMessage="Timeout in seconds"
-                  />
-                }
-                isInvalid={
-                  !!validate[ConfigKeys.TIMEOUT]?.(
-                    fields[ConfigKeys.TIMEOUT],
-                    fields[ConfigKeys.SCHEDULE].number,
-                    fields[ConfigKeys.SCHEDULE].unit
-                  )
-                }
-                error={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.timeout.error"
-                    defaultMessage="Timeout must be 0 or greater and less than schedule interval"
-                  />
-                }
-                helpText={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.timeout.helpText"
-                    defaultMessage="The total time allowed for testing the connection and exchanging data."
-                  />
-                }
-              >
-                <EuiFieldNumber
-                  min={0}
-                  value={fields[ConfigKeys.TIMEOUT]}
-                  onChange={(event) =>
-                    handleInputChange({
-                      value: event.target.value,
-                      configKey: ConfigKeys.TIMEOUT,
-                    })
-                  }
-                  step={'any'}
-                />
-              </EuiFormRow>
-              <EuiFormRow
-                label={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.tags.label"
-                    defaultMessage="Tags"
-                  />
-                }
-                labelAppend={<OptionalLabel />}
-                helpText={
-                  <FormattedMessage
-                    id="xpack.uptime.createPackagePolicy.stepConfigure.monitorIntegrationSettingsSection.tags.helpText"
-                    defaultMessage="A list of tags that will be sent with the monitor event. Press enter to add a new tab. Displayed in Uptime and enables searching by tag."
-                  />
-                }
-              >
-                <ComboBox
-                  selectedOptions={fields[ConfigKeys.TAGS]}
-                  onChange={(value) => handleInputChange({ value, configKey: ConfigKeys.TAGS })}
-                  data-test-subj="syntheticsTags"
-                />
-              </EuiFormRow>
+              {renderSimpleFields(monitorType)}
             </EuiFlexItem>
           </EuiFlexGroup>
         </EuiDescribedFormGroup>
