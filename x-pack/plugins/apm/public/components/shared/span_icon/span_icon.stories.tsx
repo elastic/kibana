824618a04c22577c1b43f0f3a42c79633352ecb8
Søren Louv-Jansen <sorenlouv@gmail.com>
2021-06-16 22:10:28 +0200

    Remove unnecessary nesting

--- x-pack/plugins/apm/public/components/shared/span_icon/span_icon.stories.tsx
+++ x-pack/plugins/apm/public/components/shared/span_icon/span_icon.stories.tsx
@@ -46,60 +46,54 @@ export function SpanIcons() {
       <EuiFlexGroup gutterSize="l" wrap={true}>
         {spanTypes.map((type) => {
           const subTypes = Object.keys(spanTypeIcons[type]);
-          return (
-            <>
-              {subTypes.map((subType) => {
-                const id = `${type}.${subType}`;
+          return subTypes.map((subType) => {
+            const id = `${type}.${subType}`;
 
-                return (
-                  <EuiFlexItem key={id}>
-                    <EuiCard
-                      icon={
-                        <>
-                          <p>
-                            <EuiToolTip
-                              position="top"
-                              content="Icon rendered with `EuiImage`"
-                            >
-                              <EuiImage
-                                size="s"
-                                hasShadow
-                                alt={id}
-                                src={getSpanIcon(type, subType)}
-                              />
-                            </EuiToolTip>
-                          </p>
-                        </>
-                      }
-                      title={id}
-                      description={
-                        <>
-                          <div>
-                            <EuiToolTip
-                              position="bottom"
-                              content="Icon rendered with `SpanIcon`"
-                            >
-                              <SpanIcon type={type} subType={subType} />
-                            </EuiToolTip>
-                          </div>
+            return (
+              <EuiFlexItem key={id}>
+                <EuiCard
+                  icon={
+                    <p>
+                      <EuiToolTip
+                        position="top"
+                        content="Icon rendered with `EuiImage`"
+                      >
+                        <EuiImage
+                          size="s"
+                          hasShadow
+                          alt={id}
+                          src={getSpanIcon(type, subType)}
+                        />
+                      </EuiToolTip>
+                    </p>
+                  }
+                  title={id}
+                  description={
+                    <>
+                      <div>
+                        <EuiToolTip
+                          position="bottom"
+                          content="Icon rendered with `SpanIcon`"
+                        >
+                          <SpanIcon type={type} subType={subType} />
+                        </EuiToolTip>
+                      </div>
 
-                          <code
-                            style={{
-                              textAlign: 'left',
-                              whiteSpace: 'nowrap',
-                            }}
-                          >
-                            <div>span.type: {type}</div>
-                            <div>span.subtype: {subType}</div>
-                          </code>
-                        </>
-                      }
-                    />
-                  </EuiFlexItem>
-                );
-              })}
-            </>
-          );
+                      <code
+                        style={{
+                          textAlign: 'left',
+                          whiteSpace: 'nowrap',
+                        }}
+                      >
+                        <div>span.type: {type}</div>
+                        <div>span.subtype: {subType}</div>
+                      </code>
+                    </>
+                  }
+                />
+              </EuiFlexItem>
+            );
+          });
         })}
       </EuiFlexGroup>
     </>
