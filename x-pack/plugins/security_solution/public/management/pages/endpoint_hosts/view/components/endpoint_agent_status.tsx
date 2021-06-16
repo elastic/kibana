2d30947958c3f7aac22f8750e80f3da8f8f690e5
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-06-16 12:08:32 -0400

    [Security Solution][Endpoint] Show Endpoint Host Isolation status on endpoint list (#101961) (#102348)
    * Add endpoint isolation status for when multiple actions of different types are pending
    * Refactored List to break out Agent status code to separate component
    * Generator improvements for how actions are generated for Endpoints
    * Add HTTP mock for fleet EPM packages (to silence console errors)
    * new `.updateCommonInfo()` method to generator (to regenerate stateful data)
    
    Co-authored-by: Paul Tavares <56442535+paul-tavares@users.noreply.github.com>

---
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/components/endpoint_agent_status.tsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { memo } from 'react';
+import { EuiBadge, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n/react';
+import styled from 'styled-components';
+import { HostInfo, HostMetadata } from '../../../../../../common/endpoint/types';
+import { HOST_STATUS_TO_BADGE_COLOR } from '../host_constants';
+import { EndpointHostIsolationStatus } from '../../../../../common/components/endpoint/host_isolation';
+import { useEndpointSelector } from '../hooks';
+import { getEndpointHostIsolationStatusPropsCallback } from '../../store/selectors';
+
+const EuiFlexGroupStyled = styled(EuiFlexGroup)`
+  .isolation-status {
+    margin-left: ${({ theme }) => theme.eui.paddingSizes.s};
+  }
+`;
+
+export interface EndpointAgentStatusProps {
+  hostStatus: HostInfo['host_status'];
+  endpointMetadata: HostMetadata;
+}
+export const EndpointAgentStatus = memo<EndpointAgentStatusProps>(
+  ({ endpointMetadata, hostStatus }) => {
+    const getEndpointIsolationStatusProps = useEndpointSelector(
+      getEndpointHostIsolationStatusPropsCallback
+    );
+
+    return (
+      <EuiFlexGroupStyled gutterSize="none" responsive={false} className="eui-textTruncate">
+        <EuiFlexItem grow={false}>
+          <EuiBadge
+            color={hostStatus != null ? HOST_STATUS_TO_BADGE_COLOR[hostStatus] : 'warning'}
+            data-test-subj="rowHostStatus"
+          >
+            <FormattedMessage
+              id="xpack.securitySolution.endpoint.list.hostStatusValue"
+              defaultMessage="{hostStatus, select, healthy {Healthy} unhealthy {Unhealthy} updating {Updating} offline {Offline} inactive {Inactive} other {Unhealthy}}"
+              values={{ hostStatus }}
+            />
+          </EuiBadge>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false} className="eui-textTruncate isolation-status">
+          <EndpointHostIsolationStatus
+            data-test-subj="rowIsolationStatus"
+            {...getEndpointIsolationStatusProps(endpointMetadata)}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroupStyled>
+    );
+  }
+);
+
+EndpointAgentStatus.displayName = 'EndpointAgentStatus';
