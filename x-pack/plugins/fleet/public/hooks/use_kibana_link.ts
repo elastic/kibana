8669f7e855e2684788a847f989d3d5a0eb12de30
Jean-Louis Leysens <jloleysens@gmail.com>
2021-06-17 14:32:28 +0200

    added comment about deprecation of current linking functionality

--- x-pack/plugins/fleet/public/hooks/use_kibana_link.ts
+++ x-pack/plugins/fleet/public/hooks/use_kibana_link.ts
@@ -10,10 +10,17 @@ import { useStartServices } from './';
 
 const KIBANA_BASE_PATH = '/app/kibana';
 
+/**
+ * @deprecated See comment on {@link useKibanaLink}
+ */
 export const getKibanaLink = (http: HttpStart, path: string) => {
   return http.basePath.prepend(`${KIBANA_BASE_PATH}#${path}`);
 };
 
+/**
+ * TODO: This functionality needs to be replaced with use of the new URL service locators
+ * @deprecated
+ */
 export function useKibanaLink(path: string = '/') {
   const { http } = useStartServices();
   return getKibanaLink(http, path);
