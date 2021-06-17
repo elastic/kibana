24f51bf52d06607287f80f47d60e74339a1a977e
Dmitry Tomashevich <39378793+Dmitriynj@users.noreply.github.com>
2021-06-17 18:46:00 +0300

    [Discover] Migrate angular context app controllers to react (#100530) (#102503)
    * [Discover] migrate remaining context files from js to ts
    
    * [Discover] get rid of any types
    
    * [Discover] replace constants with enums, update imports
    
    * [Discover] use unknown instead of any, correct types
    
    * [Discover] skip any type for tests
    
    * [Discover] add euiDataGrid view
    
    * [Discover] add support dataGrid columns, provide ability to do not change sorting, highlight anchor doc, rename legacy variables
    
    * [Discover] update context_legacy test and types
    
    * [Discover] update unit tests, add context header
    
    * [Discover] update unit and functional tests
    
    * [Discover] remove docTable from context test which uses new data grid
    
    * [Discover] update EsHitRecord type, use it for context app. add no pagination support
    
    * [Discover] resolve type error in test
    
    * [Discover] move fetching methods
    
    * [Discover] complete fetching part
    
    * [Discover] remove redundant controller
    
    * [Discover] split up context state and components
    
    * [Discover] revert redundant css class
    
    * [Discover] rename component
    
    * [Discover] revert to upstream changes
    
    * [Discover] return upstream changes
    
    * [Discover] refactoring, context test update
    
    * [Discover] add tests for fetching methods, remove redundant files
    
    * [Discover] remove redundant angular utils, add filter test
    
    * [Discover] refactor error feedback
    
    * [Discover] fix functional test
    
    * [Discover] provide defaultSize
    
    * [Discover] clean up code
    
    * [Discover] fix eslint error
    
    * [Discover] fiix context settings
    
    * [Discover] return tieBreaker field check
    
    * [Discover] optimize things
    
    * [Discover] optimize rerenders
    
    * Update src/plugins/discover/public/application/components/context_app/context_app.tsx
    
    Co-authored-by: Matthias Wilhelm <ankertal@gmail.com>
    
    * [Discover] resolve comments
    
    * [Discover] replace url instead of pushing to history. refactoring
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Matthias Wilhelm <ankertal@gmail.com>
    # Conflicts:
    #	src/plugins/discover/public/application/angular/context/query_parameters/state.ts

--- src/plugins/discover/public/application/components/context_app/utils/constants.ts
+++ src/plugins/discover/public/application/components/context_app/utils/constants.ts
@@ -6,8 +6,5 @@
  * Side Public License, v 1.
  */
 
-import { createInitialQueryParametersState } from './state';
-
 export const MAX_CONTEXT_SIZE = 10000; // Elasticsearch's default maximum size limit
 export const MIN_CONTEXT_SIZE = 0;
-export const QUERY_PARAMETER_KEYS = Object.keys(createInitialQueryParametersState());
