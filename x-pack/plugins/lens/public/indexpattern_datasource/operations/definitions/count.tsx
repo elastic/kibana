bca422cf73f8630ca1058362ee41e5fc9f3475ad
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-06-17 08:22:20 -0400

    [Lens] Formula time shift (#101718) (#102480)
    Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

--- x-pack/plugins/lens/public/indexpattern_datasource/operations/definitions/count.tsx
+++ x-pack/plugins/lens/public/indexpattern_datasource/operations/definitions/count.tsx
@@ -81,7 +81,7 @@ export const countOperation: OperationDefinition<CountIndexPatternColumn, 'field
       sourceField: field.name,
       timeScale: previousColumn?.timeScale,
       filter: getFilter(previousColumn, columnParams),
-      timeShift: previousColumn?.timeShift,
+      timeShift: columnParams?.shift || previousColumn?.timeShift,
       params:
         previousColumn?.dataType === 'number' &&
         previousColumn.params &&
@@ -118,7 +118,7 @@ export const countOperation: OperationDefinition<CountIndexPatternColumn, 'field
       defaultMessage: `
 Calculates the number of documents.
 
-Example: Calculate the number of documents: 
+Example: Calculate the number of documents:
 \`count()\`
 
 Example: Calculate the number of documents matching a certain filter:
