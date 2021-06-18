16dc5faed65d1dd09ac7cf681c9eea6b69b326f3
Tiago Costa <tiagoffcc@hotmail.com>
2021-06-18 23:24:01 +0100

    chore(NA): add some typing changes

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -26,14 +26,6 @@ declare module '*.svg' {
 
 declare module 'axios/lib/adapters/xhr';
 
-// Storybook references this module. It's @ts-ignored in the codebase but when
-// built into its dist it strips that out. Add it here to avoid a type checking
-// error.
-//
-// See https://github.com/storybookjs/storybook/issues/11684
-declare module 'react-syntax-highlighter/dist/cjs/create-element';
-declare module 'react-syntax-highlighter/dist/cjs/prism-light';
-
 // Monaco languages support
 declare module 'monaco-editor/esm/vs/basic-languages/markdown/markdown';
 declare module 'monaco-editor/esm/vs/basic-languages/css/css';
