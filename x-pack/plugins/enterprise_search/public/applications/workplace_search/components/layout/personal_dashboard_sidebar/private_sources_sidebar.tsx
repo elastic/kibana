57dcd76e7f18d771bf08ab0b5d9b626761ad2cbe
Constance Chen <constance.chen.3@gmail.com>
2021-06-21 14:14:51 -0700

    Ignore Typescript error on soon-to-come EUI prop

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/components/layout/personal_dashboard_sidebar/private_sources_sidebar.tsx
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/components/layout/personal_dashboard_sidebar/private_sources_sidebar.tsx
@@ -43,6 +43,7 @@ export const PrivateSourcesSidebar = () => {
   return (
     <>
       <ViewContentHeader title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
+      {/* @ts-expect-error: TODO, uncomment this once EUI 34.x lands in Kibana & `mobileBreakpoints` is a valid prop */}
       {id && <EuiSideNav items={navItems} mobileBreakpoints={[]} />}
     </>
   );
