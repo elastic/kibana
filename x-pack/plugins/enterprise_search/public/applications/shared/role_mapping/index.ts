cfb296b3ede2f475a085cbce58893f35b7bd4994
scottybollinger <scotty.bollinger@elastic.co>
2021-06-21 11:43:29 -0500

    Add UserAddedInfo component

--- x-pack/plugins/enterprise_search/public/applications/shared/role_mapping/index.ts
+++ x-pack/plugins/enterprise_search/public/applications/shared/role_mapping/index.ts
@@ -6,9 +6,11 @@
  */
 
 export { AttributeSelector } from './attribute_selector';
+export { RolesEmptyPrompt } from './roles_empty_prompt';
 export { RoleMappingsTable } from './role_mappings_table';
 export { RoleOptionLabel } from './role_option_label';
 export { RoleSelector } from './role_selector';
 export { RoleMappingFlyout } from './role_mapping_flyout';
 export { RoleMappingsHeading } from './role_mappings_heading';
+export { UserAddedInfo } from './user_added_info';
 export { UsersAndRolesRowActions } from './users_and_roles_row_actions';
