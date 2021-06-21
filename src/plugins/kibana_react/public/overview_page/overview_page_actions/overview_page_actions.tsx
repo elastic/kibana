844aab8f8950aba9877a8bb5477c1c4531f56dd3
Michael Marcialis <michael.marcialis@elastic.co>
2021-06-21 09:26:27 -0400

    apply page template comp to kibana overview

---
+++ src/plugins/kibana_react/public/overview_page/overview_page_actions/overview_page_actions.tsx
@@ -0,0 +1,93 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import { EuiButtonEmpty, EuiFlexItem } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { CoreStart } from 'kibana/public';
+import { RedirectAppLinks } from '../../app_links';
+import { useKibana } from '../../context';
+
+interface Props {
+  addBasePath: (path: string) => string;
+  hidden?: boolean;
+  showDevToolsLink?: boolean;
+  showManagementLink?: boolean;
+}
+
+export const OverviewPageActions = ({
+  addBasePath,
+  hidden,
+  showDevToolsLink,
+  showManagementLink,
+}: Props) => {
+  const {
+    services: { application },
+  } = useKibana<CoreStart>();
+
+  const {
+    management: isManagementEnabled,
+    dev_tools: isDevToolsEnabled,
+  } = application.capabilities.navLinks;
+
+  const actionAddData = (
+    <RedirectAppLinks application={application}>
+      <EuiButtonEmpty
+        data-test-subj="homeAddData"
+        className="kbnOverviewPageHeader__actionButton"
+        flush="both"
+        href={addBasePath('/app/home#/tutorial_directory')}
+        iconType="indexOpen"
+      >
+        {i18n.translate('kibana-react.kbnOverviewPageHeader.addDataButtonLabel', {
+          defaultMessage: 'Add data',
+        })}
+      </EuiButtonEmpty>
+    </RedirectAppLinks>
+  );
+
+  const actionStackManagement =
+    showManagementLink && isManagementEnabled ? (
+      <EuiFlexItem className="kbnOverviewPageHeader__actionItem" grow={false}>
+        <RedirectAppLinks application={application}>
+          <EuiButtonEmpty
+            data-test-subj="homeManage"
+            className="kbnOverviewPageHeader__actionButton"
+            flush="both"
+            iconType="gear"
+            href={addBasePath('/app/management')}
+          >
+            {i18n.translate('kibana-react.kbnOverviewPageHeader.stackManagementButtonLabel', {
+              defaultMessage: 'Manage',
+            })}
+          </EuiButtonEmpty>
+        </RedirectAppLinks>
+      </EuiFlexItem>
+    ) : null;
+
+  const actionDevTools =
+    showDevToolsLink && isDevToolsEnabled ? (
+      <EuiFlexItem className="kbnOverviewPageHeader__actionItem" grow={false}>
+        <RedirectAppLinks application={application}>
+          <EuiButtonEmpty
+            data-test-subj="homeDevTools"
+            className="kbnOverviewPageHeader__actionButton"
+            flush="both"
+            iconType="wrench"
+            href={addBasePath('/app/dev_tools#/console')}
+          >
+            {i18n.translate('kibana-react.kbnOverviewPageHeader.devToolsButtonLabel', {
+              defaultMessage: 'Dev tools',
+            })}
+          </EuiButtonEmpty>
+        </RedirectAppLinks>
+      </EuiFlexItem>
+    ) : null;
+
+  return !hidden ? [actionAddData, actionStackManagement, actionDevTools] : [];
+};
