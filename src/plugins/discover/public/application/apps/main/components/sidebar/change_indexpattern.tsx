a06c52dfc6d34fbc822e311432f01b263da9bb04
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-06-21 21:43:03 +0200

    Add tests

--- src/plugins/discover/public/application/apps/main/components/sidebar/change_indexpattern.tsx
+++ src/plugins/discover/public/application/apps/main/components/sidebar/change_indexpattern.tsx
@@ -26,17 +26,17 @@ export type ChangeIndexPatternTriggerProps = EuiButtonProps & {
 // TODO: refactor to shared component with ../../../../../../../../x-pack/legacy/plugins/lens/public/indexpattern_plugin/change_indexpattern
 
 export function ChangeIndexPattern({
-  indexPatternRefs,
   indexPatternId,
+  indexPatternRefs,
   onChangeIndexPattern,
-  trigger,
   selectableProps,
+  trigger,
 }: {
-  trigger: ChangeIndexPatternTriggerProps;
+  indexPatternId?: string;
   indexPatternRefs: IndexPatternRef[];
   onChangeIndexPattern: (newId: string) => void;
-  indexPatternId?: string;
   selectableProps?: EuiSelectableProps<{ value: string }>;
+  trigger: ChangeIndexPatternTriggerProps;
 }) {
   const [isPopoverOpen, setPopoverIsOpen] = useState(false);
 
