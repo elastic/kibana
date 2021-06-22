5975060992e0b2f602aa0461611070c2025d63a5
Justin Kambic <justin.kambic@elastic.co>
2021-06-22 15:00:01 -0400

    Add tests for `getJourneyDetails`.

---
+++ x-pack/plugins/uptime/server/lib/requests/get_journey_details.test.ts
@@ -0,0 +1,142 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { getJourneyDetails } from './get_journey_details';
+import { getUptimeESMockClient } from './helper';
+
+function mockEs(data: unknown) {
+  const { esClient: mockEsClient, uptimeEsClient } = getUptimeESMockClient();
+  mockEsClient.search.mockResolvedValue({
+    body: {
+      took: 18,
+      timed_out: false,
+      _shards: {
+        total: 1,
+        successful: 1,
+        skipped: 0,
+        failed: 0,
+      },
+      hits: {
+        hits: Array.isArray(data) ? data : [data],
+        max_score: 0.0,
+        total: 0,
+      },
+    },
+  } as any);
+  return uptimeEsClient;
+}
+
+describe('getJourneyDetails', () => {
+  let mockData: unknown;
+
+  beforeEach(() => {
+    mockData = {
+      _id: 'uTjtNHoBu1okBtVwOgMb',
+      _source: {
+        '@timestamp': '2021-06-22T18:13:19.013Z',
+        synthetics: {
+          package_version: '1.0.0-beta.2',
+          journey: {
+            name: 'inline',
+            id: 'inline',
+          },
+          payload: {
+            source:
+              'async ({ page, browser, params }) => {\n        scriptFn.apply(null, [core_1.step, page, browser, params]);\n    }',
+            params: {},
+          },
+          index: 0,
+          type: 'journey/start',
+        },
+        monitor: {
+          name: 'My Monitor - inline',
+          timespan: {
+            lt: '2021-06-22T18:14:19.013Z',
+            gte: '2021-06-22T18:13:19.013Z',
+          },
+          check_group: '85946468-d385-11eb-8848-acde48001122',
+          id: 'my-browser-monitor-inline',
+          type: 'browser',
+          status: 'up',
+        },
+      },
+    };
+  });
+
+  it('formats ref detail data', async () => {
+    expect(
+      await getJourneyDetails({
+        uptimeEsClient: mockEs(mockData),
+        checkGroup: '85946468-d385-11eb-8848-acde48001122',
+      })
+    ).toMatchInlineSnapshot(`
+      Object {
+        "journey": Object {
+          "@timestamp": "2021-06-22T18:13:19.013Z",
+          "_id": "uTjtNHoBu1okBtVwOgMb",
+          "monitor": Object {
+            "check_group": "85946468-d385-11eb-8848-acde48001122",
+            "id": "my-browser-monitor-inline",
+            "name": "My Monitor - inline",
+            "status": "up",
+            "timespan": Object {
+              "gte": "2021-06-22T18:13:19.013Z",
+              "lt": "2021-06-22T18:14:19.013Z",
+            },
+            "type": "browser",
+          },
+          "synthetics": Object {
+            "index": 0,
+            "journey": Object {
+              "id": "inline",
+              "name": "inline",
+            },
+            "package_version": "1.0.0-beta.2",
+            "payload": Object {
+              "params": Object {},
+              "source": "async ({ page, browser, params }) => {
+              scriptFn.apply(null, [core_1.step, page, browser, params]);
+          }",
+            },
+            "type": "journey/start",
+          },
+        },
+        "next": Object {
+          "checkGroup": "85946468-d385-11eb-8848-acde48001122",
+          "timestamp": "2021-06-22T18:13:19.013Z",
+        },
+        "previous": Object {
+          "checkGroup": "85946468-d385-11eb-8848-acde48001122",
+          "timestamp": "2021-06-22T18:13:19.013Z",
+        },
+        "timestamp": "2021-06-22T18:13:19.013Z",
+      }
+    `);
+  });
+
+  it('returns null for 0 hits', async () => {
+    expect(await getJourneyDetails({ uptimeEsClient: mockEs([]), checkGroup: 'check_group' })).toBe(
+      null
+    );
+  });
+
+  it('throws an error for malformed data', async () => {
+    let exception: unknown;
+    try {
+      await getJourneyDetails({
+        uptimeEsClient: mockEs({ _id: 'id', _source: { foo: 'bar' } }),
+        checkGroup: '85946468-d385-11eb-8848-acde48001122',
+      });
+    } catch (e: unknown) {
+      exception = e;
+    }
+    expect(exception).toBeDefined();
+    expect(exception).toMatchInlineSnapshot(
+      `[Error: Could not process journey step for check group 85946468-d385-11eb-8848-acde48001122, malformed data]`
+    );
+  });
+});
