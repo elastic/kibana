da0159085ea35868a12ef53693497a2e9655993a
Constance Chen <constance.chen.3@gmail.com>
2021-06-21 21:16:44 -0700

    Update AnalyticsLayout to use new page template
    + add new test_helper for header children

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/analytics/analytics_layout.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/analytics/analytics_layout.tsx
@@ -10,15 +10,12 @@ import { useParams } from 'react-router-dom';
 
 import { useValues, useActions } from 'kea';
 
-import { EuiSpacer } from '@elastic/eui';
-
-import { FlashMessages } from '../../../shared/flash_messages';
 import { KibanaLogic } from '../../../shared/kibana';
-import { Loading } from '../../../shared/loading';
+import { AppSearchPageTemplate } from '../layout';
 
-import { LogRetentionCallout, LogRetentionOptions } from '../log_retention';
+import { LogRetentionTooltip, LogRetentionCallout, LogRetentionOptions } from '../log_retention';
 
-import { AnalyticsHeader } from './components';
+import { AnalyticsFilters } from './components';
 
 import { AnalyticsLogic } from './';
 
@@ -43,15 +40,20 @@ export const AnalyticsLayout: React.FC<Props> = ({
     if (isAnalyticsView) loadAnalyticsData();
   }, [history.location.search]);
 
-  if (dataLoading) return <Loading />;
-
   return (
-    <>
-      <AnalyticsHeader title={title} />
-      <FlashMessages />
+    <AppSearchPageTemplate
+      pageHeader={{
+        pageTitle: title,
+        rightSideItems: [
+          <LogRetentionTooltip type={LogRetentionOptions.Analytics} position="left" />,
+        ],
+        children: <AnalyticsFilters />,
+        responsive: false,
+      }}
+      isLoading={dataLoading}
+    >
       <LogRetentionCallout type={LogRetentionOptions.Analytics} />
       {children}
-      <EuiSpacer />
-    </>
+    </AppSearchPageTemplate>
   );
 };
