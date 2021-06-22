0331fc1c3fcdc1ac3a8344e8839d4ca56ec484de
Quynh Nguyen <quynh.nguyen@elastic.co>
2021-06-22 14:22:19 -0500

    Add from to follow MDN guideline

--- x-pack/plugins/apm/server/lib/search_strategies/correlations/utils/math_utils.ts
+++ x-pack/plugins/apm/server/lib/search_strategies/correlations/utils/math_utils.ts
@@ -15,7 +15,7 @@ export function* range(start: number, end: number, step: number) {
   }
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 export function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
