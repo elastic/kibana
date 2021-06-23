0875f1f8ec561dfb8253457d5c828d4320397390
scottybollinger <scotty.bollinger@elastic.co>
2021-06-23 10:20:19 -0500

    Rename mock

--- x-pack/plugins/enterprise_search/public/applications/shared/role_mapping/user_selector.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/shared/role_mapping/user_selector.test.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { elasticsearchUsers } from './__mocks__/elasticsearchUsers';
+import { elasticsearchUsers } from './__mocks__/elasticsearch_users';
 
 import React from 'react';
 
