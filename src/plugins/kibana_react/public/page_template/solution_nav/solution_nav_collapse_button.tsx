ed3041c18d6319cddf0bb185424820e833fa46be
cchaos <caroline.horn@elastic.co>
2021-06-23 17:18:10 -0400

    Added a collapse button component

---
+++ src/plugins/kibana_react/public/page_template/solution_nav/solution_nav_collapse_button.tsx
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+import './solution_nav_collapse_button.scss';
+
+import React, { FunctionComponent } from 'react';
+import classNames from 'classnames';
+
+import { EuiButtonIcon, EuiButtonIconPropsForButton } from '@elastic/eui';
+
+export type KibanaPageTemplateSolutionNavCollapseButtonProps = Partial<EuiButtonIconPropsForButton> & {
+  /**
+   * Boolean state of current collapsed status
+   */
+  isCollapsed: boolean;
+};
+
+/**
+ * Applies extra styling to a typical EuiAvatar
+ */
+export const KibanaPageTemplateSolutionNavCollapseButton: FunctionComponent<KibanaPageTemplateSolutionNavCollapseButtonProps> = ({
+  className,
+  isCollapsed,
+  ...rest
+}) => {
+  const classes = classNames(
+    'kbnPageTemplateSolutionNavCollapseButton',
+    {
+      'kbnPageTemplateSolutionNavCollapseButton-isCollapsed': isCollapsed,
+    },
+    className
+  );
+
+  return (
+    <EuiButtonIcon
+      className={classes}
+      iconType={isCollapsed ? 'menuRight' : 'menuLeft'}
+      size="s"
+      display="fill"
+      color="text"
+      aria-label="Collapse side navigation"
+      title="Collapse side navigation"
+      {...rest}
+    />
+  );
+};
