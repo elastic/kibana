8942ffe1b5a6e5c6434e0f0fb8a3501c275bb39f
Jonathan Budzenski <jon@budzenski.me>
2021-06-23 11:37:22 -0500

    allowJs

--- packages/kbn-test/src/index.ts
+++ packages/kbn-test/src/index.ts
@@ -18,7 +18,6 @@ import {
 // @internal
 export { runTestsCli, processRunTestsCliOptions, startServersCli, processStartServersCliOptions };
 
-// @ts-expect-error not typed yet
 // @internal
 export { runTests, startServers } from './functional_tests/tasks';
 
