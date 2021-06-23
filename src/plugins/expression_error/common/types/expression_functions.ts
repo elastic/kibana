ee655aa1b681b0a71282dcf09de9621e718346a6
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-06-23 10:15:20 +0300

    Basic setup of error plugin.

---
+++ src/plugins/expression_error/common/types/expression_functions.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+import { ExpressionFunctionDefinition, ExpressionValueRender } from 'src/plugins/expressions';
+
+export enum Origin {
+  TOP = 'top',
+  LEFT = 'left',
+  BOTTOM = 'bottom',
+  RIGHT = 'right',
+}
+
+interface Arguments {
+  image: string | null;
+  emptyImage: string | null;
+  origin: Origin;
+}
+
+export interface Output {
+  image: string;
+  emptyImage: string;
+  origin: Origin;
+  percent: number;
+}
+
+export type ExpressionErrorFunction = () => ExpressionFunctionDefinition<
+  'error',
+  number,
+  Arguments,
+  ExpressionValueRender<Output>
+>;
+
+export enum Position {
+  TOP = 'top',
+  BOTTOM = 'bottom',
+  LEFT = 'left',
+  RIGHT = 'right',
+}
