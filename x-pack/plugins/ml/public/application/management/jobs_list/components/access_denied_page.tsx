ca81c265c1a8229df0964e975f44c1fa200086b9
Pete Harverson <pete@elastic.co>
2021-06-23 16:09:49 +0100

    [ML] Converts management app jobs list pages to new layout

--- x-pack/plugins/ml/public/application/management/jobs_list/components/access_denied_page.tsx
+++ x-pack/plugins/ml/public/application/management/jobs_list/components/access_denied_page.tsx
@@ -5,59 +5,46 @@
  * 2.0.
  */
 
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import { FormattedMessage } from '@kbn/i18n/react';
-import { i18n } from '@kbn/i18n';
 
-import {
-  EuiCallOut,
-  EuiPage,
-  EuiPageBody,
-  EuiPageContentBody,
-  EuiPageContentHeader,
-  EuiPageContentHeaderSection,
-  EuiSpacer,
-  EuiText,
-  EuiTitle,
-} from '@elastic/eui';
+import { EuiEmptyPrompt, EuiPageContent, EuiPageHeader, EuiSpacer } from '@elastic/eui';
 
 export const AccessDeniedPage = () => (
-  <Fragment>
-    <EuiPage data-test-subj="mlPageAccessDenied">
-      <EuiPageBody>
-        <EuiPageContentHeader>
-          <EuiPageContentHeaderSection>
-            <EuiTitle>
-              <h1>
-                <FormattedMessage
-                  id="xpack.ml.management.jobsList.insufficientLicenseTitle"
-                  defaultMessage="Machine Learning"
-                />
-              </h1>
-            </EuiTitle>
-          </EuiPageContentHeaderSection>
-        </EuiPageContentHeader>
-        <EuiPageContentBody>
-          <EuiSpacer size="m" />
-          <EuiCallOut
-            title={i18n.translate('xpack.ml.management.jobsList.noPermissionToAccessLabel', {
-              defaultMessage: 'Access denied',
-            })}
-            color="danger"
-            iconType="cross"
-          >
-            <EuiText size="s">
-              <p>
-                <FormattedMessage
-                  id="xpack.ml.management.jobsList.noGrantedPrivilegesDescription"
-                  defaultMessage="You don’t have permission to manage ML jobs"
-                />
-              </p>
-            </EuiText>
-          </EuiCallOut>
-        </EuiPageContentBody>
-      </EuiPageBody>
-    </EuiPage>
-  </Fragment>
+  <>
+    <EuiPageHeader
+      data-test-subj="mlPageAccessDenied"
+      pageTitle={
+        <FormattedMessage
+          id="xpack.ml.management.jobsList.noPermissionToAccessPageTitle"
+          defaultMessage="Machine Learning"
+        />
+      }
+      bottomBorder
+    />
+
+    <EuiSpacer size="l" />
+    <EuiPageContent verticalPosition="center" horizontalPosition="center" color="danger">
+      <EuiEmptyPrompt
+        iconType="alert"
+        title={
+          <h2>
+            <FormattedMessage
+              id="xpack.ml.management.jobsList.noPermissionToAccessLabel"
+              defaultMessage="Access denied"
+            />
+          </h2>
+        }
+        body={
+          <p>
+            <FormattedMessage
+              id="xpack.ml.management.jobsList.noGrantedPrivilegesDescription"
+              defaultMessage="You don’t have permission to manage Machine Learning jobs."
+            />
+          </p>
+        }
+      />
+    </EuiPageContent>
+  </>
 );
