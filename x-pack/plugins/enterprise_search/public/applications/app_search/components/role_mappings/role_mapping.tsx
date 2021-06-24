1289c8dde7d7d4ae811804838dc99dbf58e8f378
Scotty Bollinger <scotty.bollinger@elastic.co>
2021-06-24 07:57:49 -0500

    [Enterprise Search] Add User management feature (#103173)
    * Rename method to close both flyouts
    
    This is shared with the forthcoming user flyouts
    
    closeRoleMappingFlyout -> closeUsersAndRolesFlyout
    
    * Add logic for elasticsearch users and single user role mappings
    
    * Add logic for various form states
    
    - Showing and hiding flyouts
    - Select and text input values
    - User created state to turn flyout into a success message state
    
    * Add User server routes
    
    * Add logic for saving a user
    
    * Add User components
    
    * Add User list and User flyout to RoleMappings view
    
    * Fix path
    
    * Rename things
    
    - Users & roles -> Users and roles
    - roleId -> roleMappingId (matches other places in code)
    - also added a missing prop to the actions col
    
    * Set default group when modal closed
    
    The UI sets the default group on page load but did not cover the case where the user has chosen a group in a previous interaction and the closed the flyout. This commit adds a method that resets that state when the flyout is closed
    
    Part of porting of https://github.com/elastic/ent-search/pull/3865
    
    Specifically:
    https://github.com/elastic/ent-search/commit/a4131b95dab7c0df97bd78e660f25e09ac3e7cec
    
    * Adds tooltip for external attribute
    
    This was missed from the design
    
    Part of porting of https://github.com/elastic/ent-search/pull/3865
    
    Specifically:
    https://github.com/elastic/ent-search/commit/03aa349cab4fb32069b64ab8c51a7252ba52e805
    
    * Fix invitations link
    
    * Fix incorrect role type
    
    Role-> RoleTypes
    🤷🏽‍♀️
    
    * Add EuiPortal to Flyout
    
    Wasn’t needed in ent-search; already done for RomeMappingFlyout. Hide whitespace changes plskthx
    
    * Auth provider deprecation warning in mapping UI
    
    Since we're moving fully into Kibana, we're losing our concept of auth providers. In 8.0, role mappings the specify an auth provider will no
    longer work, so this adds a small deprecation warning in the role mappings table.
    
    https://github.com/elastic/ent-search/pull/3885
    
    * Email is no longer required
    
    After a slack discussion, it was determined that email should be optional.
    
    This commit also fixes another instance of the App Search role type being wrong.
    
    * Existing users’ usernames should not be editable
    
    * Use EuiLink instead of anchor
    
    * Add validation tests
    
    * Change URL for users_and_roles
    
    Need to change folder and file names but will punt until after 7.14FF
    
    I did throw in updating the logic file path
    
    * Remove unused import
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/role_mappings/role_mapping.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/role_mappings/role_mapping.tsx
@@ -28,7 +28,7 @@ export const RoleMapping: React.FC = () => {
     handleAuthProviderChange,
     handleRoleChange,
     handleSaveMapping,
-    closeRoleMappingFlyout,
+    closeUsersAndRolesFlyout,
   } = useActions(RoleMappingsLogic);
 
   const {
@@ -68,7 +68,7 @@ export const RoleMapping: React.FC = () => {
     <RoleMappingFlyout
       disabled={attributeValueInvalid || !hasEngineAssignment}
       isNew={isNew}
-      closeRoleMappingFlyout={closeRoleMappingFlyout}
+      closeUsersAndRolesFlyout={closeUsersAndRolesFlyout}
       handleSaveMapping={handleSaveMapping}
     >
       <EuiForm isInvalid={roleMappingErrors.length > 0} error={roleMappingErrors}>
