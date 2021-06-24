c26d2be49f326af7de132d0404a7a8156c5ab741
Nathan L Smith <smith@nlsmith.com>
2021-06-24 11:58:40 -0500

    fix import

--- x-pack/plugins/observability/common/rules/observability_rule_registry_settings.ts
+++ x-pack/plugins/observability/common/rules/observability_rule_registry_settings.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { observabilityFeatureId } from '../const';
+import { observabilityFeatureId } from '..';
 
 export const observabilityRuleRegistrySettings = {
   name: observabilityFeatureId,
