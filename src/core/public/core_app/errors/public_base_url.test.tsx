75ae0ce5863c07cba604f3d1e7cc0962fe600060
Josh Dover <1813008+joshdover@users.noreply.github.com>
2021-06-24 19:13:04 +0200

    Remove mismatch warning

--- src/core/public/core_app/errors/public_base_url.test.tsx
+++ src/core/public/core_app/errors/public_base_url.test.tsx
@@ -1,20 +1,9 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { docLinksServiceMock } from '../../doc_links/doc_links_service.mock';
@@ -103,50 +92,6 @@ describe('publicBaseUrl warning', () => {
 
     it('does not add toast if storage key set', () => {
       const http = httpServiceMock.createStartContract({ publicBaseUrl: undefined });
-
-      setupPublicBaseUrlConfigWarning({
-        docLinks,
-        notifications,
-        http,
-        location: {
-          hostname: 'myhost.com',
-          toString() {
-            return 'http://myhost.com/';
-          },
-        } as Location,
-        storage: {
-          getItem: (id: string) => 'true',
-        } as Storage,
-      });
-
-      expect(notifications.toasts.addWarning).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('config mismatch toast', () => {
-    it('adds toast if location does not start with publicBaseUrl', () => {
-      const http = httpServiceMock.createStartContract({ publicBaseUrl: 'http://myhost.com:9999' });
-
-      setupPublicBaseUrlConfigWarning({
-        docLinks,
-        notifications,
-        http,
-        location: {
-          hostname: 'myhost.com',
-          toString() {
-            return 'http://myhost.com/';
-          },
-        } as Location,
-      });
-
-      expect(notifications.toasts.addWarning).toHaveBeenCalledWith({
-        title: 'Configuration issue',
-        text: expect.any(Function),
-      });
-    });
-
-    it('does not add toast if storage key set', () => {
-      const http = httpServiceMock.createStartContract({ publicBaseUrl: 'http://myhost.com:9999' });
 
       setupPublicBaseUrlConfigWarning({
         docLinks,
--- src/core/public/core_app/errors/public_base_url.tsx
+++ src/core/public/core_app/errors/public_base_url.tsx
@@ -1,66 +1,22 @@
 /*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
+import React from 'react';
 import { EuiButton, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage } from '@kbn/i18n/react';
 
-import { HttpStart, NotificationsStart } from '../..';
-import { DocLinksStart } from '../../doc_links';
+import type { HttpStart, NotificationsStart } from '../..';
+import type { DocLinksStart } from '../../doc_links';
 import { mountReactNode } from '../../utils';
 
 /** Only exported for tests */
 export const MISSING_CONFIG_STORAGE_KEY = `core.warnings.publicBaseUrlMissingDismissed`;
-export const MISMATCHED_CONFIG_STORAGE_KEY = `core.warnings.publicBaseUrlMismatchedDismissed`;
-
-/** Small helper component for the toast content */
-const WarningToastContent: React.FC<{ docLinks: DocLinksStart; onMute: () => void }> = ({
-  children,
-  docLinks,
-  onMute,
-}) => {
-  return (
-    <>
-      <p>
-        {children}{' '}
-        <a href={`${docLinks.links.settings}#server-publicBaseUrl`} target="_blank">
-          <FormattedMessage
-            id="core.ui.publicBaseUrlWarning.seeDocumentationLinkLabel"
-            defaultMessage="See the documentation."
-          />
-        </a>
-      </p>
-
-      <EuiFlexGroup justifyContent="flexEnd" gutterSize="s">
-        <EuiFlexItem grow={false}>
-          <EuiButton size="s" onClick={onMute} id="mute">
-            <FormattedMessage
-              id="core.ui.publicBaseUrlWarning.muteWarningButtonLabel"
-              defaultMessage="Mute warning"
-            />
-          </EuiButton>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    </>
-  );
-};
 
 interface Deps {
   docLinks: DocLinksStart;
@@ -83,57 +39,50 @@ export const setupPublicBaseUrlConfigWarning = ({
   }
 
   const missingWarningSeen = storage.getItem(MISSING_CONFIG_STORAGE_KEY) === 'true';
-  const mismatchedWarningSeen = storage.getItem(MISMATCHED_CONFIG_STORAGE_KEY) === 'true';
+  if (missingWarningSeen || http.basePath.publicBaseUrl) {
+    return;
+  }
 
-  if (!missingWarningSeen && !http.basePath.publicBaseUrl) {
-    const toast = notifications.toasts.addWarning({
-      title: i18n.translate('core.ui.publicBaseUrlWarning.configMissingTitle', {
-        defaultMessage: 'Configuration missing',
-      }),
-      text: mountReactNode(
-        <WarningToastContent
-          docLinks={docLinks}
-          onMute={() => {
-            notifications.toasts.remove(toast);
-            storage.setItem(MISSING_CONFIG_STORAGE_KEY, 'true');
-          }}
-        >
+  const toast = notifications.toasts.addWarning({
+    title: i18n.translate('core.ui.publicBaseUrlWarning.configMissingTitle', {
+      defaultMessage: 'Configuration missing',
+    }),
+    text: mountReactNode(
+      <>
+        <p>
           <FormattedMessage
             id="core.ui.publicBaseUrlWarning.configMissingDescription"
             defaultMessage="{configKey} is missing and should be configured when running in a production environment. Some features may not behave correctly."
             values={{
               configKey: <code>server.publicBaseUrl</code>,
             }}
-          />
-        </WarningToastContent>
-      ),
-    });
-  } else if (
-    !mismatchedWarningSeen &&
-    http.basePath.publicBaseUrl &&
-    !location.toString().startsWith(http.basePath.publicBaseUrl)
-  ) {
-    const toast = notifications.toasts.addWarning({
-      title: i18n.translate('core.ui.publicBaseUrlWarning.configMismatchTitle', {
-        defaultMessage: 'Configuration issue',
-      }),
-      text: mountReactNode(
-        <WarningToastContent
-          docLinks={docLinks}
-          onMute={() => {
-            notifications.toasts.remove(toast);
-            storage.setItem(MISMATCHED_CONFIG_STORAGE_KEY, 'true');
-          }}
-        >
-          <FormattedMessage
-            id="core.ui.publicBaseUrlWarning.configMismatchDescription"
-            defaultMessage="{configKey} does not match the URL that you are using to access Kibana. Some features may not behave correctly."
-            values={{
-              configKey: <code>server.publicBaseUrl</code>,
-            }}
-          />
-        </WarningToastContent>
-      ),
-    });
-  }
+          />{' '}
+          <a href={`${docLinks.links.settings}#server-publicBaseUrl`} target="_blank">
+            <FormattedMessage
+              id="core.ui.publicBaseUrlWarning.seeDocumentationLinkLabel"
+              defaultMessage="See the documentation."
+            />
+          </a>
+        </p>
+
+        <EuiFlexGroup justifyContent="flexEnd" gutterSize="s">
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              size="s"
+              onClick={() => {
+                notifications.toasts.remove(toast);
+                storage.setItem(MISSING_CONFIG_STORAGE_KEY, 'true');
+              }}
+              id="mute"
+            >
+              <FormattedMessage
+                id="core.ui.publicBaseUrlWarning.muteWarningButtonLabel"
+                defaultMessage="Mute warning"
+              />
+            </EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </>
+    ),
+  });
 };
