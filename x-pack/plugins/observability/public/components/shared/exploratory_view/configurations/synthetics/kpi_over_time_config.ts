ea0e1644663465fbc84847db4b45ead808bbc252
Shahzad <shahzad.muhammad@elastic.co>
2021-06-28 15:55:53 +0200

    [Exploratory view] Update types names (#103214)

--- x-pack/plugins/observability/public/components/shared/exploratory_view/configurations/synthetics/kpi_over_time_config.ts
+++ x-pack/plugins/observability/public/components/shared/exploratory_view/configurations/synthetics/kpi_over_time_config.ts
@@ -5,15 +5,15 @@
  * 2.0.
  */
 
-import { ConfigProps, DataSeries } from '../../types';
-import { FieldLabels, OPERATION_COLUMN } from '../constants';
+import { ConfigProps, SeriesConfig } from '../../types';
+import { FieldLabels, OPERATION_COLUMN, REPORT_METRIC_FIELD } from '../constants';
 import { buildExistsFilter } from '../utils';
 import { DOWN_LABEL, MONITORS_DURATION_LABEL, UP_LABEL } from '../constants/labels';
 import { MONITOR_DURATION_US } from '../constants/field_names/synthetics';
 const SUMMARY_UP = 'summary.up';
 const SUMMARY_DOWN = 'summary.down';
 
-export function getSyntheticsKPIConfig({ indexPattern }: ConfigProps): DataSeries {
+export function getSyntheticsKPIConfig({ indexPattern }: ConfigProps): SeriesConfig {
   return {
     reportType: 'kpi-over-time',
     defaultSeriesType: 'bar_stacked',
@@ -23,45 +23,34 @@ export function getSyntheticsKPIConfig({ indexPattern }: ConfigProps): DataSerie
     },
     yAxisColumns: [
       {
-        sourceField: 'business.kpi',
+        sourceField: REPORT_METRIC_FIELD,
         operationType: 'median',
       },
     ],
     hasOperationType: false,
-    defaultFilters: ['observer.geo.name', 'monitor.type', 'tags'],
-    breakdowns: ['observer.geo.name', 'monitor.type'],
-    filters: [...buildExistsFilter('summary.up', indexPattern)],
+    filterFields: ['observer.geo.name', 'monitor.type', 'tags'],
+    breakdownFields: ['observer.geo.name', 'monitor.type'],
+    baseFilters: [...buildExistsFilter('summary.up', indexPattern)],
     palette: { type: 'palette', name: 'status' },
-    reportDefinitions: [
+    definitionFields: ['monitor.name', 'url.full'],
+    metricOptions: [
       {
-        field: 'monitor.name',
+        label: MONITORS_DURATION_LABEL,
+        field: MONITOR_DURATION_US,
+        id: MONITOR_DURATION_US,
+        columnType: OPERATION_COLUMN,
       },
       {
-        field: 'url.full',
+        field: SUMMARY_UP,
+        id: SUMMARY_UP,
+        label: UP_LABEL,
+        columnType: OPERATION_COLUMN,
       },
       {
-        field: 'business.kpi',
-        custom: true,
-        options: [
-          {
-            label: MONITORS_DURATION_LABEL,
-            field: MONITOR_DURATION_US,
-            id: MONITOR_DURATION_US,
-            columnType: OPERATION_COLUMN,
-          },
-          {
-            field: SUMMARY_UP,
-            id: SUMMARY_UP,
-            label: UP_LABEL,
-            columnType: OPERATION_COLUMN,
-          },
-          {
-            field: SUMMARY_DOWN,
-            id: SUMMARY_DOWN,
-            label: DOWN_LABEL,
-            columnType: OPERATION_COLUMN,
-          },
-        ],
+        field: SUMMARY_DOWN,
+        id: SUMMARY_DOWN,
+        label: DOWN_LABEL,
+        columnType: OPERATION_COLUMN,
       },
     ],
     labels: { ...FieldLabels },
