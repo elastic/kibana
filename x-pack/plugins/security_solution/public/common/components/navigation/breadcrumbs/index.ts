30a4568949c4b59540855c8b3a5467aae58a866a
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2021-06-28 12:02:44 +0200

    Revert 'timeline.isOpen' breadcrumb code that was deleted during merge

--- x-pack/plugins/security_solution/public/common/components/navigation/breadcrumbs/index.ts
+++ x-pack/plugins/security_solution/public/common/components/navigation/breadcrumbs/index.ts
@@ -77,10 +77,14 @@ const isRulesRoutes = (spyState: RouteSpyState): spyState is AdministrationRoute
 
 // eslint-disable-next-line complexity
 export const getBreadcrumbsForRoute = (
-  object: RouteSpyState & TabNavigationProps,
+  objectParam: RouteSpyState & TabNavigationProps,
   getUrlForApp: GetUrlForApp
 ): ChromeBreadcrumb[] | null => {
-  const spyState: RouteSpyState = omit('navTabs', object);
+  const spyState: RouteSpyState = omit('navTabs', objectParam);
+
+  // Sets `timeline.isOpen` to false in the state to avoid reopening the timeline on breadcrumb click. https://github.com/elastic/kibana/issues/100322
+  const object = { ...objectParam, timeline: { ...objectParam.timeline, isOpen: false } };
+
   const overviewPath = getUrlForApp(APP_ID, { deepLinkId: SecurityPageName.overview });
   const siemRootBreadcrumb: ChromeBreadcrumb = {
     text: APP_NAME,
