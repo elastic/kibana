43984ac0423bad94c39db2f4a937afae25caf867
Sébastien Loix <sabee77@gmail.com>
2021-06-28 17:26:23 +0100

    Render image preview in a modal

--- src/plugins/index_pattern_field_editor/public/components/preview/field_list/field_list_item.tsx
+++ src/plugins/index_pattern_field_editor/public/components/preview/field_list/field_list_item.tsx
@@ -6,10 +6,12 @@
  * Side Public License, v 1.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import { i18n } from '@kbn/i18n';
-import { EuiFlexGroup, EuiFlexItem, EuiToolTip, EuiButtonIcon } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiToolTip, EuiButtonIcon, EuiButtonEmpty } from '@elastic/eui';
+
+import { ImagePreviewModal } from '../image_preview_modal';
 import type { DocumentField } from './field_list';
 
 interface Props {
@@ -23,6 +25,8 @@ export const PreviewListItem: React.FC<Props> = ({
   highlighted,
   toggleIsPinned,
 }) => {
+  const [isPreviewImageModalVisible, setIsPreviewImageModalVisible] = useState(false);
+
   /* eslint-disable @typescript-eslint/naming-convention */
   const classes = classnames('indexPatternFieldEditor__previewFieldList__item', {
     'indexPatternFieldEditor__previewFieldList__item--highlighted': highlighted,
@@ -30,48 +34,83 @@ export const PreviewListItem: React.FC<Props> = ({
   });
   /* eslint-enable @typescript-eslint/naming-convention */
 
-  return (
-    <EuiFlexGroup className={classes} gutterSize="none">
-      <EuiFlexItem className="indexPatternFieldEditor__previewFieldList__item__key">
-        <div className="indexPatternFieldEditor__previewFieldList__item__key__wrapper">{key}</div>
-      </EuiFlexItem>
-      <EuiFlexItem className="indexPatternFieldEditor__previewFieldList__item__value">
-        <EuiToolTip
-          position="top"
-          content={typeof value !== 'string' ? JSON.stringify(value) : value}
-        >
-          {formattedValue !== undefined ? (
-            <span
-              className="indexPatternFieldEditor__previewFieldList__item__value__wrapper"
-              // We  can dangerously set HTML here because this content is guaranteed to have been run through a valid field formatter first.
-              dangerouslySetInnerHTML={{ __html: formattedValue! }} // eslint-disable-line react/no-danger
-            />
-          ) : (
-            <span className="indexPatternFieldEditor__previewFieldList__item__value__wrapper">
-              {value}
-            </span>
-          )}
-        </EuiToolTip>
-      </EuiFlexItem>
+  const doesContainImage = formattedValue?.includes('<img');
 
-      <EuiFlexItem
-        className="indexPatternFieldEditor__previewFieldList__item__actions"
-        grow={false}
-      >
-        {toggleIsPinned && (
-          <EuiButtonIcon
-            onClick={() => {
-              toggleIsPinned(key);
-            }}
-            color="text"
-            iconType="pinFilled"
-            aria-label={i18n.translate('indexPatternFieldEditor.fieldPreview.pinFieldButtonLabel', {
-              defaultMessage: 'Pin field',
-            })}
-            className="indexPatternFieldEditor__previewFieldList__item__actionsBtn"
-          />
-        )}
-      </EuiFlexItem>
-    </EuiFlexGroup>
+  const renderValue = () => {
+    if (doesContainImage) {
+      return (
+        <EuiButtonEmpty
+          color="text"
+          onClick={() => setIsPreviewImageModalVisible(true)}
+          iconType="image"
+        >
+          {i18n.translate('indexPatternFieldEditor.fieldPreview.viewImageButtonLabel', {
+            defaultMessage: 'View image',
+          })}
+        </EuiButtonEmpty>
+      );
+    }
+
+    if (formattedValue !== undefined) {
+      return (
+        <span
+          className="indexPatternFieldEditor__previewFieldList__item__value__wrapper"
+          // We  can dangerously set HTML here because this content is guaranteed to have been run through a valid field formatter first.
+          dangerouslySetInnerHTML={{ __html: formattedValue! }} // eslint-disable-line react/no-danger
+        />
+      );
+    }
+
+    return (
+      <span className="indexPatternFieldEditor__previewFieldList__item__value__wrapper">
+        {value}
+      </span>
+    );
+  };
+
+  return (
+    <>
+      <EuiFlexGroup className={classes} gutterSize="none">
+        <EuiFlexItem className="indexPatternFieldEditor__previewFieldList__item__key">
+          <div className="indexPatternFieldEditor__previewFieldList__item__key__wrapper">{key}</div>
+        </EuiFlexItem>
+        <EuiFlexItem className="indexPatternFieldEditor__previewFieldList__item__value">
+          <EuiToolTip
+            position="top"
+            content={typeof value !== 'string' ? JSON.stringify(value) : value}
+          >
+            {renderValue()}
+          </EuiToolTip>
+        </EuiFlexItem>
+
+        <EuiFlexItem
+          className="indexPatternFieldEditor__previewFieldList__item__actions"
+          grow={false}
+        >
+          {toggleIsPinned && (
+            <EuiButtonIcon
+              onClick={() => {
+                toggleIsPinned(key);
+              }}
+              color="text"
+              iconType="pinFilled"
+              aria-label={i18n.translate(
+                'indexPatternFieldEditor.fieldPreview.pinFieldButtonLabel',
+                {
+                  defaultMessage: 'Pin field',
+                }
+              )}
+              className="indexPatternFieldEditor__previewFieldList__item__actionsBtn"
+            />
+          )}
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      {isPreviewImageModalVisible && (
+        <ImagePreviewModal
+          imgHTML={formattedValue!}
+          closeModal={() => setIsPreviewImageModalVisible(false)}
+        />
+      )}
+    </>
   );
 };
