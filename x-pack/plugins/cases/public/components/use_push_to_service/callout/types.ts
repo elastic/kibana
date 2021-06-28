6a445a657b878f7b730fec24e31df667a569ee42
Steph Milovic <stephanie.milovic@elastic.co>
2021-06-28 15:38:02 -0600

    [RAC] [Cases] Push to 3rd party UI updates (#103418) (#103546)

--- x-pack/plugins/cases/public/components/use_push_to_service/callout/types.ts
+++ x-pack/plugins/cases/public/components/use_push_to_service/callout/types.ts
@@ -6,8 +6,10 @@
  */
 
 export interface ErrorMessage {
+  description: JSX.Element | string;
+  errorType?: 'primary' | 'success' | 'warning' | 'danger';
   id: string;
   title: string;
-  description: JSX.Element;
-  errorType?: 'primary' | 'success' | 'warning' | 'danger';
 }
+
+export const CLOSED_CASE_PUSH_ERROR_ID = 'closed-case-push-error';
