8e16c67b4b5747644686e346c732fc876f84f449
Søren Louv-Jansen <sorenlouv@gmail.com>
2021-06-29 11:13:00 +0200

    Update comment

--- x-pack/plugins/apm/server/routes/settings/agent_configuration.ts
+++ x-pack/plugins/apm/server/routes/settings/agent_configuration.ts
@@ -206,6 +206,9 @@ const agentConfigurationSearchRoute = createApmServerRoute({
       throw Boom.notFound();
     }
 
+    // whether to update `applied_by_agent` field
+    // It will be set to true of the etags match or if `markAsAppliedByAgent=true`
+    // `markAsAppliedByAgent=true` means "force setting it to true regardless of etag". This is needed for Jaeger agent that doesn't have etags
     const willMarkAsApplied =
       (markAsAppliedByAgent || etag === config._source.etag) &&
       !config._source.applied_by_agent;
@@ -220,10 +223,6 @@ const agentConfigurationSearchRoute = createApmServerRoute({
         willMarkAsApplied = ${willMarkAsApplied}`
     );
 
-    // update `applied_by_agent` field
-    // when `markAsAppliedByAgent` is true (Jaeger agent doesn't have etags)
-    // or if etags match.
-    // this happens in the background and doesn't block the response
     if (willMarkAsApplied) {
       markAppliedByAgent({ id: config._id, body: config._source, setup });
     }
