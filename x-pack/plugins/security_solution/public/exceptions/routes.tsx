63516d9024158bbae478c473415ddbe16b93e169
Angela Chuang <yi-chun.chuang@elastic.co>
2021-06-29 13:43:19 +0100

    add not found page

--- x-pack/plugins/security_solution/public/exceptions/routes.tsx
+++ x-pack/plugins/security_solution/public/exceptions/routes.tsx
@@ -5,18 +5,25 @@
  * 2.0.
  */
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import { TrackApplicationView } from '../../../../../src/plugins/usage_collection/public';
 import { EXCEPTIONS_PATH, SecurityPageName } from '../../common/constants';
 import { ExceptionListsTable } from '../detections/pages/detection_engine/rules/all/exceptions/exceptions_table';
 import { SpyRoute } from '../common/utils/route/spy_routes';
+import { NotFoundPage } from '../app/404';
 
 export const ExceptionsRoutes = () => {
   return (
-    <TrackApplicationView viewId={SecurityPageName.exceptions}>
-      <ExceptionListsTable />
-      <SpyRoute pageName={SecurityPageName.exceptions} />
-    </TrackApplicationView>
+    <Switch>
+      <Route path={EXCEPTIONS_PATH} exact>
+        <TrackApplicationView viewId={SecurityPageName.exceptions}>
+          <ExceptionListsTable />
+          <SpyRoute pageName={SecurityPageName.exceptions} />
+        </TrackApplicationView>
+      </Route>
+      <Route component={NotFoundPage} />
+    </Switch>
   );
 };
 
