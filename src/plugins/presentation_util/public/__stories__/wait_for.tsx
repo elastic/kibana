9f27c37b43e2bc5f1e64775fd23be36dad45245a
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-06-29 17:07:30 +0300

    waitFor fixed.

--- src/plugins/presentation_util/public/__stories__/wait_for.tsx
+++ src/plugins/presentation_util/public/__stories__/wait_for.tsx
@@ -9,21 +9,20 @@
 import React, { useState, useEffect, ReactElement } from 'react';
 import { Story } from '@storybook/react';
 import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
-import { StoryContext } from '@storybook/addons';
 import { EuiLoadingSpinner } from '@elastic/eui';
 
 export const waitFor = (
   waitTarget: Promise<any>,
   spinner: ReactElement | null = <EuiLoadingSpinner />
-) => (story: Story) => {
+) => (CurrentStory: Story) => {
   const [storyComponent, setStory] = useState<StoryFnReactReturnType>();
   useEffect(() => {
     if (!storyComponent) {
       waitTarget.then((waitedTarget: any) => {
-        setStory(story(waitedTarget, {} as StoryContext));
+        setStory(<CurrentStory {...waitedTarget} />);
       });
     }
-  }, [story, storyComponent]);
+  }, [CurrentStory, storyComponent]);
 
   return storyComponent ?? spinner;
 };
