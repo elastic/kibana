60c994aa9c464e7610f610240f6f53424fce866e
Ahmad Bamieh <ahmadbamieh@gmail.com>
2021-06-29 10:38:40 +0300

    update tests

--- src/plugins/kibana_usage_collection/server/collectors/event_loop_delays/track_threshold.test.ts
+++ src/plugins/kibana_usage_collection/server/collectors/event_loop_delays/track_threshold.test.ts
@@ -7,9 +7,11 @@
  */
 
 import { Subject } from 'rxjs';
-
-import { mockMonitorPercentile, monitorEventLoopDelay } from './event_loop_delays.mocks';
-
+import {
+  mockMonitorPercentile,
+  monitorEventLoopDelay,
+  mockMonitorReset,
+} from './event_loop_delays.mocks';
 import { startTrackingEventLoopDelaysThreshold } from './track_threshold';
 import { loggingSystemMock } from '../../../../../core/server/mocks';
 import { usageCountersServiceMock } from '../../../../usage_collection/server/usage_counters/usage_counters_service.mock';
@@ -57,65 +59,43 @@ describe('startTrackingEventLoopDelaysThreshold', () => {
 
     expect(logger.warn).toBeCalledTimes(0);
     expect(mockEventLoopCounter.incrementCounter).toBeCalledTimes(0);
+    expect(mockMonitorReset).toBeCalledTimes(0);
 
     jest.advanceTimersByTime(collectionStartDelay);
     expect(logger.warn).toBeCalledTimes(1);
     expect(mockEventLoopCounter.incrementCounter).toBeCalledTimes(1);
+    expect(mockMonitorReset).toBeCalledTimes(1);
 
     jest.advanceTimersByTime(collectionInterval);
     expect(logger.warn).toBeCalledTimes(2);
     expect(mockEventLoopCounter.incrementCounter).toBeCalledTimes(2);
+    expect(mockMonitorReset).toBeCalledTimes(2);
 
     jest.advanceTimersByTime(collectionInterval);
     expect(mockEventLoopCounter.incrementCounter).toBeCalledTimes(3);
     expect(logger.warn).toBeCalledTimes(3);
+    expect(mockMonitorReset).toBeCalledTimes(3);
   });
 
-  // it('does nothing if threshold does not exceed mean delay', () => {
-  //   const collectionStartDelay = 100;
-  //   const collectionInterval = 1000;
-  //   const collectionWarnThreshold = 1000;
-  //   startTrackingEventLoopDelaysThreshold(
-  //     mockEventLoopCounter,
-  //     logger,
-  //     stopMonitoringEventLoop$,
-  //     collectionWarnThreshold,
-  //     collectionStartDelay
-  //   );
+  it('does not log warning or increment usage if threshold did not exceed mean delay', () => {
+    const collectionStartDelay = 100;
+    const collectionWarnThreshold = 15;
 
-  //   expect(mockInternalRepository.create).toBeCalledTimes(0);
-  //   jest.advanceTimersByTime(collectionStartDelay);
-  //   expect(mockInternalRepository.create).toBeCalledTimes(1);
-  //   jest.advanceTimersByTime(collectionInterval);
-  //   expect(mockInternalRepository.create).toBeCalledTimes(2);
-  //   jest.advanceTimersByTime(collectionInterval);
-  //   expect(mockInternalRepository.create).toBeCalledTimes(3);
-  // });
+    startTrackingEventLoopDelaysThreshold(
+      mockEventLoopCounter,
+      logger,
+      stopMonitoringEventLoop$,
+      collectionWarnThreshold,
+      collectionStartDelay
+    );
 
-  // it('resets histogram every histogramReset duration', () => {
-  //   const collectionStartDelay = 0;
-  //   const collectionInterval = 1000;
-  //   const histogramReset = 5000;
-  //   startTrackingEventLoopDelaysUsage(
-  //     mockInternalRepository,
-  //     stopMonitoringEventLoop$,
-  //     collectionStartDelay,
-  //     collectionInterval,
-  //     histogramReset
-  //   );
+    expect(logger.warn).toBeCalledTimes(0);
+    expect(mockEventLoopCounter.incrementCounter).toBeCalledTimes(0);
+    expect(mockMonitorReset).toBeCalledTimes(0);
 
-  //   expect(mockMonitorReset).toBeCalledTimes(0);
-  //   jest.advanceTimersByTime(collectionInterval * 5);
-  //   expect(mockMonitorReset).toBeCalledTimes(1);
-  //   jest.advanceTimersByTime(collectionInterval * 5);
-  //   expect(mockMonitorReset).toBeCalledTimes(2);
-  // });
-
-  // it('stops monitoring event loop delays once stopMonitoringEventLoop$.next is called', () => {
-  //   startTrackingEventLoopDelaysUsage(mockInternalRepository, stopMonitoringEventLoop$);
-
-  //   expect(mockMonitorDisable).toBeCalledTimes(0);
-  //   stopMonitoringEventLoop$.next();
-  //   expect(mockMonitorDisable).toBeCalledTimes(1);
-  // });
+    jest.advanceTimersByTime(collectionStartDelay);
+    expect(logger.warn).toBeCalledTimes(0);
+    expect(mockEventLoopCounter.incrementCounter).toBeCalledTimes(0);
+    expect(mockMonitorReset).toBeCalledTimes(1);
+  });
 });
