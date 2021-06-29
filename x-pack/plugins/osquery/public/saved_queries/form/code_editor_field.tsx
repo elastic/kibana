62980ca02e080307aa6de897a31114cf41bff3eb
Patryk Kopyciński <patryk.kopycinski@elastic.co>
2021-06-29 04:19:02 +0300

    [Osquery] Add Saved queries (#100965)

--- x-pack/plugins/osquery/public/saved_queries/form/code_editor_field.tsx
+++ x-pack/plugins/osquery/public/saved_queries/form/code_editor_field.tsx
@@ -5,30 +5,17 @@
  * 2.0.
  */
 
-import { FormattedMessage } from '@kbn/i18n/react';
 import { isEmpty } from 'lodash/fp';
-import { EuiFormRow, EuiLink, EuiText } from '@elastic/eui';
+import { EuiFormRow } from '@elastic/eui';
 import React from 'react';
 
+import { OsquerySchemaLink } from '../../components/osquery_schema_link';
 import { OsqueryEditor } from '../../editor';
 import { FieldHook } from '../../shared_imports';
 
 interface CodeEditorFieldProps {
   field: FieldHook<string>;
 }
-
-const OsquerySchemaLink = React.memo(() => (
-  <EuiText size="xs">
-    <EuiLink href="https://osquery.io/schema/4.7.0" target="_blank">
-      <FormattedMessage
-        id="xpack.osquery.codeEditorField.osquerySchemaLinkLabel"
-        defaultMessage="Osquery schema"
-      />
-    </EuiLink>
-  </EuiText>
-));
-
-OsquerySchemaLink.displayName = 'OsquerySchemaLink';
 
 const CodeEditorFieldComponent: React.FC<CodeEditorFieldProps> = ({ field }) => {
   const { value, label, labelAppend, helpText, setValue, errors } = field;
