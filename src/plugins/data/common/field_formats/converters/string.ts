8f9010351cdb28e641ed0a5ad204d385d457b4f4
Tim Roes <tim.roes@elastic.co>
2021-06-30 11:49:01 +0200

    Fix highlighting in values

--- src/plugins/data/common/field_formats/converters/string.ts
+++ src/plugins/data/common/field_formats/converters/string.ts
@@ -8,7 +8,7 @@
 
 import escape from 'lodash/escape';
 import { i18n } from '@kbn/i18n';
-import { asPrettyString } from '../utils';
+import { asPrettyString, getHighlightHtml } from '../utils';
 import { KBN_FIELD_TYPES } from '../../kbn_field_types/types';
 import { FieldFormat } from '../field_format';
 import { TextContextTypeConvert, FIELD_FORMAT_IDS, HtmlContextTypeConvert } from '../types';
@@ -129,11 +129,13 @@ export class StringFormat extends FieldFormat {
     }
   };
 
-  htmlConvert: HtmlContextTypeConvert = (val) => {
+  htmlConvert: HtmlContextTypeConvert = (val, { hit, field } = {}) => {
     if (val === '') {
       return `<span class="ffString__emptyValue">${emptyLabel}</span>`;
     }
 
-    return escape(this.textConvert(val));
+    return hit?.highlight?.[field.name]
+      ? getHighlightHtml(val, hit.highlight[field.name])
+      : escape(this.textConvert(val));
   };
 }
