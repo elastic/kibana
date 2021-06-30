1a098e088baf2000b6611c31f77aec3f9467b669
mgiota <panagiota.mitsopoulou@elastic.co>
2021-06-30 23:10:53 +0200

    refactor use_readonly_badge

--- x-pack/plugins/infra/public/hooks/use_readonly_badge.tsx
+++ x-pack/plugins/infra/public/hooks/use_readonly_badge.tsx
@@ -5,33 +5,28 @@
  * 2.0.
  */
 
-import { useCallback, useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { i18n } from '@kbn/i18n';
 import { useKibana } from '../../../../../src/plugins/kibana_react/public';
 
 export const useReadOnlyBadge = (isReadOnly = false) => {
   const chrome = useKibana().services.chrome;
-  const badge = useMemo(() => {
-    return isReadOnly
-      ? {
-          text: i18n.translate('xpack.infra.header.badge.readOnly.text', {
-            defaultMessage: 'Read only',
-          }),
-          tooltip: i18n.translate('xpack.infra.header.badge.readOnly.tooltip', {
-            defaultMessage: 'Unable to change source configuration',
-          }),
-          iconType: 'glasses',
-        }
-      : undefined;
-  }, [isReadOnly]);
-
-  const setBadge = useCallback(() => {
-    return chrome?.setBadge(badge);
-  }, [badge, chrome]);
 
   useEffect(() => {
-    setBadge();
-  }, [badge, setBadge]);
+    chrome?.setBadge(
+      isReadOnly
+        ? {
+            text: i18n.translate('xpack.infra.header.badge.readOnly.text', {
+              defaultMessage: 'Read only',
+            }),
+            tooltip: i18n.translate('xpack.infra.header.badge.readOnly.tooltip', {
+              defaultMessage: 'Unable to change source configuration',
+            }),
+            iconType: 'glasses',
+          }
+        : undefined
+    );
+  }, [chrome, isReadOnly]);
 
   return null;
 };
