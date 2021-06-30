5fd3f6085320c73d9416499ba17a65dce64ff21c
Ece Ozalp <ozale272@newschool.edu>
2021-06-30 12:24:40 -0400

    fixes rebase

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.ts
@@ -261,11 +261,12 @@ export const signalRulesAlertType = ({
               listClient,
               exceptionItems,
               services,
-            logger,
-            buildRuleMessage,
-            bulkCreate,
-            wrapHits,
-          });}
+              logger,
+              buildRuleMessage,
+              bulkCreate,
+              wrapHits,
+            });
+          }
         } else if (isThresholdRule(type)) {
           const thresholdRuleSO = asTypeSpecificSO(savedObject, thresholdRuleParams);
           for (const tuple of tuples) {
@@ -274,13 +275,14 @@ export const signalRulesAlertType = ({
               tuple,
               exceptionItems,
               services,
-            version,
-            logger,
-            buildRuleMessage,
-            startedAt,
-            bulkCreate,
-            wrapHits,
-          });}
+              version,
+              logger,
+              buildRuleMessage,
+              startedAt,
+              bulkCreate,
+              wrapHits,
+            });
+          }
         } else if (isThreatMatchRule(type)) {
           const threatRuleSO = asTypeSpecificSO(savedObject, threatRuleParams);
           for (const tuple of tuples) {
@@ -325,11 +327,11 @@ export const signalRulesAlertType = ({
               tuple,
               exceptionItems,
               services,
-            version,
-            searchAfterSize,
-            bulkCreate,
-            logger,
-          wrapHits,
+              version,
+              searchAfterSize,
+              bulkCreate,
+              logger,
+              wrapHits,
               wrapSequences,
             });
           }
@@ -466,105 +468,4 @@ export const asTypeSpecificSO = <T extends t.Mixed>(
       params: validated,
     },
   };
-};
-
-const bulkCreateFactory = (
-  logger: Logger,
-  esClient: ElasticsearchClient,
-  buildRuleMessage: BuildRuleMessage,
-  refreshForBulkCreate: RefreshTypes
-) => async <T>(wrappedDocs: Array<BaseHit<T>>): Promise<GenericBulkCreateResponse<T>> => {
-  if (wrappedDocs.length === 0) {
-    return {
-      errors: [],
-      success: true,
-      bulkCreateDuration: '0',
-      createdItemsCount: 0,
-      createdItems: [],
-    };
-  }
-
-  const bulkBody = wrappedDocs.flatMap((wrappedDoc) => [
-    {
-      create: {
-        _index: wrappedDoc._index,
-        _id: wrappedDoc._id,
-      },
-    },
-    wrappedDoc._source,
-  ]);
-  const start = performance.now();
-
-  const { body: response } = await esClient.bulk({
-    refresh: refreshForBulkCreate,
-    body: bulkBody,
-  });
-
-  const end = performance.now();
-  logger.debug(
-    buildRuleMessage(
-      `individual bulk process time took: ${makeFloatString(end - start)} milliseconds`
-    )
-  );
-  logger.debug(buildRuleMessage(`took property says bulk took: ${response.took} milliseconds`));
-  const createdItems = wrappedDocs
-    .map((doc, index) => ({
-      _id: response.items[index].create?._id ?? '',
-      _index: response.items[index].create?._index ?? '',
-      ...doc._source,
-    }))
-    .filter((_, index) => get(response.items[index], 'create.status') === 201);
-  const createdItemsCount = createdItems.length;
-  const duplicateSignalsCount = countBy(response.items, 'create.status')['409'];
-  const errorCountByMessage = errorAggregator(response, [409]);
-
-  logger.debug(buildRuleMessage(`bulk created ${createdItemsCount} signals`));
-  if (duplicateSignalsCount > 0) {
-    logger.debug(buildRuleMessage(`ignored ${duplicateSignalsCount} duplicate signals`));
-  }
-  if (!isEmpty(errorCountByMessage)) {
-    logger.error(
-      buildRuleMessage(
-        `[-] bulkResponse had errors with responses of: ${JSON.stringify(errorCountByMessage)}`
-      )
-    );
-    return {
-      errors: Object.keys(errorCountByMessage),
-      success: false,
-      bulkCreateDuration: makeFloatString(end - start),
-      createdItemsCount,
-      createdItems,
-    };
-  } else {
-    return {
-      errors: [],
-      success: true,
-      bulkCreateDuration: makeFloatString(end - start),
-      createdItemsCount,
-      createdItems,
-    };
-  }
-};
-
-const buildWrappedSignalsFactory = ({
-  ruleSO,
-  signalsIndex,
-}: {
-  ruleSO: SearchAfterAndBulkCreateParams['ruleSO'];
-  signalsIndex: string;
-}): WrapHits => (events) => {
-  const wrappedDocs: WrappedSignalHit[] = events.flatMap((doc) => [
-    {
-      _index: signalsIndex,
-      _id: generateId(
-        doc._index,
-        doc._id,
-        doc._version ? doc._version.toString() : '',
-        ruleSO.attributes.params.ruleId ?? ''
-      ),
-      _source: buildBulkBody(ruleSO, doc),
-    },
-  ]);
-
-  return filterDuplicateSignals(ruleSO.id, wrappedDocs);
 };
