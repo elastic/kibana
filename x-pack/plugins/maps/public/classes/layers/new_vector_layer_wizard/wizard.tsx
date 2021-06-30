b0862a58112c7137441a1ea3749d18bad41446cc
Nathan Reese <reese.nathan@gmail.com>
2021-06-30 06:21:16 -0600

    tslint

--- x-pack/plugins/maps/public/classes/layers/new_vector_layer_wizard/wizard.tsx
+++ x-pack/plugins/maps/public/classes/layers/new_vector_layer_wizard/wizard.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { EuiPanel, EuiCallOut } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { createNewIndexAndPattern } from './create_new_index_pattern';
