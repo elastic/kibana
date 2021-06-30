9605d241c825ce4e77e8eb9b86cef195874dd062
mgiota <panagiota.mitsopoulou@elastic.co>
2021-06-30 20:03:38 +0200

    refactor use_breadcrumbs

--- x-pack/plugins/infra/public/hooks/use_breadcrumbs.ts
+++ x-pack/plugins/infra/public/hooks/use_breadcrumbs.ts
@@ -6,65 +6,69 @@
  */
 
 import { ChromeBreadcrumb } from 'kibana/public';
-import { MouseEvent, useEffect } from 'react';
-import { EuiBreadcrumb } from '@elastic/eui';
-import { useKibana } from '../../../../../src/plugins/kibana_react/public';
+import { useEffect } from 'react';
+// import { EuiBreadcrumb } from '@elastic/eui';
 import { observabilityTitle } from '../translations';
+import { useKibanaContextForPlugin } from './use_kibana';
+import { useLinkProps } from './use_link_props';
 
 type AppId = 'logs' | 'metrics';
 
-function handleBreadcrumbClick(
-  breadcrumbs: ChromeBreadcrumb[],
-  navigateToHref?: (url: string) => Promise<void>
-) {
-  return breadcrumbs.map((bc) => ({
-    ...bc,
-    ...(bc.href
-      ? {
-          onClick: (event: MouseEvent) => {
-            if (navigateToHref && bc.href) {
-              event.preventDefault();
-              navigateToHref(bc.href);
-            }
-          },
-        }
-      : {}),
-  }));
-}
+// function handleBreadcrumbClick(
+//   breadcrumbs: ChromeBreadcrumb[],
+//   navigateToHref?: (url: string) => Promise<void>
+// ) {
+//   return breadcrumbs.map((bc) => ({
+//     ...bc,
+//     ...(bc.href
+//       ? {
+//           onClick: (event: MouseEvent) => {
+//             if (navigateToHref && bc.href) {
+//               event.preventDefault();
+//               navigateToHref(bc.href);
+//             }
+//           },
+//         }
+//       : {}),
+//   }));
+// }
 
-const makeBaseBreadcrumb = (
-  path: string,
-  observabilityPath: string,
-  appTitle: string
-): [EuiBreadcrumb, EuiBreadcrumb] => {
-  return [
-    {
-      text: observabilityTitle,
-      href: observabilityPath,
-    },
-    {
-      text: appTitle,
-      href: path,
-    },
-  ];
-};
+// const makeBaseBreadcrumb = (
+//   path: string,
+//   observabilityPath: string,
+//   appTitle: string
+// ): [EuiBreadcrumb, EuiBreadcrumb] => {
+//   return [
+//     {
+//       text: observabilityTitle,
+//       href: observabilityPath,
+//     },
+//     {
+//       text: appTitle,
+//       href: path,
+//     },
+//   ];
+// };
 
 export const useBreadcrumbs = (app: AppId, appTitle: string, extraCrumbs: ChromeBreadcrumb[]) => {
-  const kibana = useKibana();
-  const setBreadcrumbs = kibana.services.chrome?.setBreadcrumbs;
-  const path = kibana.services.application?.getUrlForApp(app) ?? '';
-  const observabilityPath =
-    kibana.services.application?.getUrlForApp('observability-overview') ?? '';
-  const navigate = kibana.services.application?.navigateToUrl;
+  const {
+    services: { chrome },
+  } = useKibanaContextForPlugin();
+
+  const observabilityLinkProps = useLinkProps({ app: 'observability-overview' });
+  const appLinkProps = useLinkProps({ app });
 
   useEffect(() => {
-    if (setBreadcrumbs) {
-      setBreadcrumbs(
-        handleBreadcrumbClick(
-          makeBaseBreadcrumb(path, observabilityPath, appTitle).concat(extraCrumbs),
-          navigate
-        )
-      );
-    }
-  }, [path, appTitle, observabilityPath, extraCrumbs, navigate, setBreadcrumbs]);
+    chrome?.setBreadcrumbs?.([
+      {
+        ...observabilityLinkProps,
+        text: observabilityTitle,
+      },
+      {
+        ...appLinkProps,
+        text: appTitle,
+      },
+      ...extraCrumbs,
+    ]);
+  }, [appLinkProps, appTitle, chrome, extraCrumbs, observabilityLinkProps]);
 };
