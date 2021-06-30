2ab4b9f677697ab47b5fa7c97c1d294c5ca9626d
Angela Chuang <yi-chun.chuang@elastic.co>
2021-06-30 22:15:43 +0100

    add test

--- x-pack/plugins/security_solution/public/hosts/pages/details/index.tsx
+++ x-pack/plugins/security_solution/public/hosts/pages/details/index.tsx
@@ -96,7 +96,7 @@ const HostDetailsComponent: React.FC<HostDetailsProps> = ({ detailName, hostDeta
   );
 
   const { docValueFields, indicesExist, indexPattern, selectedPatterns } = useSourcererScope();
-  const [loading, { hostDetails: hostOverview, id, refetch }] = useHostDetails({
+  const [loading, { inspect, hostDetails: hostOverview, id, refetch }] = useHostDetails({
     endDate: to,
     startDate: from,
     hostName: detailName,
@@ -166,6 +166,7 @@ const HostDetailsComponent: React.FC<HostDetailsProps> = ({ detailName, hostDeta
                     }}
                     setQuery={setQuery}
                     refetch={refetch}
+                    inspect={inspect}
                   />
                 )}
               </AnomalyTableProvider>
