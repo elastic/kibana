5d89ca0d1af80c61d9e39e100ace1d25d3333696
Shahzad <shahzad31comp@gmail.com>
2021-07-02 00:39:02 +0200

    add test

--- x-pack/plugins/uptime/public/components/monitor/synthetics/step_detail/use_step_waterfall_metrics.ts
+++ x-pack/plugins/uptime/public/components/monitor/synthetics/step_detail/use_step_waterfall_metrics.ts
@@ -13,6 +13,9 @@ import { MarkerItems } from '../waterfall/context/waterfall_chart';
 export interface Props {
   checkGroup: string;
 }
+export const BROWSER_TRACE_TYPE = 'browser.relative_trace.type';
+export const BROWSER_TRACE_NAME = 'browser.relative_trace.name';
+export const BROWSER_TRACE_START = 'browser.relative_trace.start.us';
 
 export const useStepWaterfallMetrics = ({ checkGroup }: Props) => {
   const { settings } = useSelector(selectDynamicSettings);
@@ -54,11 +57,8 @@ export const useStepWaterfallMetrics = ({ checkGroup }: Props) => {
     let navigationStartExist = false;
 
     metricDocs.forEach(({ fields }) => {
-      if (fields['browser.relative_trace.type']?.[0] === 'mark') {
-        const {
-          'browser.relative_trace.name': metricType,
-          'browser.relative_trace.start.us': metricValue,
-        } = fields;
+      if (fields[BROWSER_TRACE_TYPE]?.[0] === 'mark') {
+        const { [BROWSER_TRACE_NAME]: metricType, [BROWSER_TRACE_START]: metricValue } = fields;
         if (metricType?.[0] === 'navigationStart') {
           navigationStart = metricValue?.[0];
           navigationStartExist = true;
