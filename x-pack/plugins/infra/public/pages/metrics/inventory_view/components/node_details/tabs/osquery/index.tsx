30e6a0d8eb361e7c8fbd8077d929a94767052d3c
Patryk Kopycinski <contact@patrykkopycinski.com>
2021-07-04 22:44:24 +0200

    [Osquery] Add Osquery tab to node details tabs

---
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/node_details/tabs/osquery/index.tsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiLoadingContent } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import React, { useContext } from 'react';
+import { useKibanaContextForPlugin } from '../../../../../../../hooks/use_kibana';
+import { TabContent, TabProps } from '../shared';
+import { Source } from '../../../../../../../containers/metrics_source';
+import { findInventoryModel } from '../../../../../../../../common/inventory_models';
+import { InventoryItemType } from '../../../../../../../../common/inventory_models/types';
+import { useMetadata } from '../../../../../metric_detail/hooks/use_metadata';
+import { useWaffleTimeContext } from '../../../../hooks/use_waffle_time';
+
+const TabComponent = (props: TabProps) => {
+  const nodeId = props.node.id;
+  const nodeType = props.nodeType as InventoryItemType;
+  const inventoryModel = findInventoryModel(nodeType);
+  const { sourceId } = useContext(Source.Context);
+  const { currentTimeRange } = useWaffleTimeContext();
+  const { metadata } = useMetadata(
+    nodeId,
+    nodeType,
+    inventoryModel.requiredMetrics,
+    sourceId,
+    currentTimeRange
+  );
+  const {
+    services: { osquery },
+  } = useKibanaContextForPlugin();
+
+  const OsqueryAction = osquery?.OsqueryAction;
+
+  if (!OsqueryAction || !metadata?.info?.host?.id) {
+    return (
+      <TabContent>
+        <EuiLoadingContent lines={10} />
+      </TabContent>
+    );
+  }
+
+  return (
+    <TabContent>
+      <OsqueryAction hostId={metadata?.info?.host?.id} />
+    </TabContent>
+  );
+};
+
+export const OsqueryTab = {
+  id: 'osquery',
+  name: i18n.translate('xpack.infra.nodeDetails.tabs.osquery', {
+    defaultMessage: 'Osquery',
+  }),
+  content: TabComponent,
+};
