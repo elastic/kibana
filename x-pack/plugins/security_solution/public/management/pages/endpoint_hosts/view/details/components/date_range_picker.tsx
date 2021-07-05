4f97e4b78cbbc1e93cf953d75f3f44efbcf4dd6f
Ashokaditya <am.struktr@gmail.com>
2021-07-05 14:49:09 +0200

    refactor date pickers into a component
    refs elastic/security-team/issues/1137

---
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/details/components/date_range_picker.tsx
@@ -0,0 +1,115 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useDispatch } from 'react-redux';
+import React, { memo, useCallback, useState } from 'react';
+import styled from 'styled-components';
+import moment, { Moment } from 'moment';
+import { EuiFlexGroup, EuiFlexItem, EuiDatePicker, EuiDatePickerRange } from '@elastic/eui';
+
+import * as i18 from '../../translations';
+import { useEndpointSelector } from '../../hooks';
+import { getActivityLogDataPaging } from '../../../store/selectors';
+
+const DatePickerWrapper = styled.div`
+  width: ${(props) => props.theme.eui.fractions.single.percentage};
+  background: white;
+`;
+const StickyFlexItem = styled(EuiFlexItem)`
+  position: sticky;
+  top: ${(props) => props.theme.eui.euiSizeM};
+  z-index: 1;
+`;
+
+export const DateRangePicker = memo(() => {
+  const dispatch = useDispatch();
+  const {
+    page,
+    pageSize,
+    startDate: initialStartDate,
+    endDate: initialEndDate,
+  } = useEndpointSelector(getActivityLogDataPaging);
+  const [startDate, setStartDate] = useState<Moment | undefined>(
+    initialStartDate ? moment(initialStartDate) : undefined
+  );
+  const [endDate, setEndDate] = useState<Moment | undefined>(
+    initialEndDate ? moment(initialEndDate) : undefined
+  );
+
+  const onChangeStartDate = useCallback(
+    (date) => {
+      setStartDate(date);
+      dispatch({
+        type: 'endpointDetailsActivityLogUpdatePaging',
+        payload: {
+          page,
+          pageSize,
+          startDate: date ? date?.toISOString() : undefined,
+          endDate: endDate ? endDate.toISOString() : undefined,
+        },
+      });
+    },
+    [dispatch, endDate, page, pageSize]
+  );
+
+  const onChangeEndDate = useCallback(
+    (date) => {
+      setEndDate(date);
+      dispatch({
+        type: 'endpointDetailsActivityLogUpdatePaging',
+        payload: {
+          page,
+          pageSize,
+          startDate: startDate ? startDate.toISOString() : undefined,
+          endDate: date ? date.toISOString() : undefined,
+        },
+      });
+    },
+    [dispatch, startDate, page, pageSize]
+  );
+  const isInvalidDateRange = startDate && endDate ? startDate > endDate : false;
+  return (
+    <StickyFlexItem grow={false}>
+      <EuiFlexGroup justifyContent="flexEnd" responsive>
+        <DatePickerWrapper>
+          <EuiFlexItem>
+            <EuiDatePickerRange
+              startDateControl={
+                <EuiDatePicker
+                  aria-label="Start date"
+                  endDate={endDate}
+                  isInvalid={isInvalidDateRange}
+                  onChange={onChangeStartDate}
+                  onClear={() => onChangeStartDate(undefined)}
+                  placeholderText={i18.ACTIVITY_LOG.datePicker.startDate}
+                  selected={startDate}
+                  showTimeSelect
+                  startDate={startDate}
+                />
+              }
+              endDateControl={
+                <EuiDatePicker
+                  aria-label="End date"
+                  endDate={endDate}
+                  isInvalid={isInvalidDateRange}
+                  onChange={onChangeEndDate}
+                  onClear={() => onChangeEndDate(undefined)}
+                  placeholderText={i18.ACTIVITY_LOG.datePicker.endDate}
+                  selected={endDate}
+                  showTimeSelect
+                  startDate={startDate}
+                />
+              }
+            />
+          </EuiFlexItem>
+        </DatePickerWrapper>
+      </EuiFlexGroup>
+    </StickyFlexItem>
+  );
+});
+
+DateRangePicker.displayName = 'DateRangePicker';
--- x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/details/endpoint_activity_log.tsx
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/details/endpoint_activity_log.tsx
@@ -5,9 +5,8 @@
  * 2.0.
  */
 
-import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
+import React, { memo, useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import moment, { Moment } from 'moment';
 
 import {
   EuiText,
@@ -15,11 +14,10 @@ import {
   EuiFlexItem,
   EuiLoadingContent,
   EuiEmptyPrompt,
-  EuiDatePicker,
-  EuiDatePickerRange,
 } from '@elastic/eui';
 import { useDispatch } from 'react-redux';
 import { LogEntry } from './components/log_entry';
+import { DateRangePicker } from './components/date_range_picker';
 import * as i18 from '../translations';
 import { Immutable, ActivityLog } from '../../../../../../common/endpoint/types';
 import { AsyncResourceState } from '../../../../state';
@@ -42,16 +40,6 @@ const LoadMoreTrigger = styled.div`
   width: ${(props) => props.theme.eui.fractions.single.percentage};
 `;
 
-const DatePickerWrapper = styled.div`
-  width: ${(props) => props.theme.eui.fractions.single.percentage};
-  background: white;
-`;
-const StickyFlexItem = styled(EuiFlexItem)`
-  position: sticky;
-  top: ${(props) => props.theme.eui.euiSizeM};
-  z-index: 1;
-`;
-
 export const EndpointActivityLog = memo(
   ({ activityLog }: { activityLog: AsyncResourceState<Immutable<ActivityLog>> }) => {
     const activityLogLoading = useEndpointSelector(getActivityLogRequestLoading);
@@ -61,21 +49,9 @@ export const EndpointActivityLog = memo(
     const activityLogSize = activityLogData.length;
     const activityLogError = useEndpointSelector(getActivityLogError);
     const dispatch = useDispatch<(action: EndpointAction) => void>();
-    const {
-      page,
-      pageSize,
-      startDate: initialStartDate,
-      endDate: initialEndDate,
-      disabled: isPagingDisabled,
-    } = useEndpointSelector(getActivityLogDataPaging);
-
-    const [startDate, setStartDate] = useState<Moment | undefined>(
-      initialStartDate ? moment(initialStartDate) : undefined
+    const { page, pageSize, startDate, endDate, disabled: isPagingDisabled } = useEndpointSelector(
+      getActivityLogDataPaging
     );
-    const [endDate, setEndDate] = useState<Moment | undefined>(
-      initialEndDate ? moment(initialEndDate) : undefined
-    );
-    const isInvalidDateRange = startDate && endDate ? startDate > endDate : false;
 
     const loadMoreTrigger = useRef<HTMLInputElement | null>(null);
     const getActivityLog = useCallback(
@@ -87,45 +63,13 @@ export const EndpointActivityLog = memo(
             payload: {
               page: page + 1,
               pageSize,
-              startDate: startDate?.toISOString(),
-              endDate: endDate?.toISOString(),
+              startDate,
+              endDate,
             },
           });
         }
       },
       [activityLogLoaded, dispatch, isPagingDisabled, page, pageSize, startDate, endDate]
-    );
-
-    const onChangeStartDate = useCallback(
-      (date) => {
-        setStartDate(date);
-        dispatch({
-          type: 'endpointDetailsActivityLogUpdatePaging',
-          payload: {
-            page,
-            pageSize,
-            startDate: date ? date?.toISOString() : undefined,
-            endDate: endDate ? endDate.toISOString() : undefined,
-          },
-        });
-      },
-      [dispatch, endDate, page, pageSize]
-    );
-
-    const onChangeEndDate = useCallback(
-      (date) => {
-        setEndDate(date);
-        dispatch({
-          type: 'endpointDetailsActivityLogUpdatePaging',
-          payload: {
-            page,
-            pageSize,
-            startDate: startDate ? startDate.toISOString() : undefined,
-            endDate: date ? date.toISOString() : undefined,
-          },
-        });
-      },
-      [dispatch, startDate, page, pageSize]
     );
 
     useEffect(() => {
@@ -154,42 +98,7 @@ export const EndpointActivityLog = memo(
             </EuiFlexItem>
           ) : (
             <>
-              <StickyFlexItem grow={false}>
-                <EuiFlexGroup justifyContent="flexEnd" responsive>
-                  <DatePickerWrapper>
-                    <EuiFlexItem>
-                      <EuiDatePickerRange
-                        startDateControl={
-                          <EuiDatePicker
-                            aria-label="Start date"
-                            endDate={endDate}
-                            isInvalid={isInvalidDateRange}
-                            onChange={onChangeStartDate}
-                            onClear={() => onChangeStartDate(undefined)}
-                            placeholderText={i18.ACTIVITY_LOG.datePicker.startDate}
-                            selected={startDate}
-                            showTimeSelect
-                            startDate={startDate}
-                          />
-                        }
-                        endDateControl={
-                          <EuiDatePicker
-                            aria-label="End date"
-                            endDate={endDate}
-                            isInvalid={isInvalidDateRange}
-                            onChange={onChangeEndDate}
-                            onClear={() => onChangeEndDate(undefined)}
-                            placeholderText={i18.ACTIVITY_LOG.datePicker.endDate}
-                            selected={endDate}
-                            showTimeSelect
-                            startDate={startDate}
-                          />
-                        }
-                      />
-                    </EuiFlexItem>
-                  </DatePickerWrapper>
-                </EuiFlexGroup>
-              </StickyFlexItem>
+              <DateRangePicker />
               <EuiFlexItem grow={true}>
                 {activityLogLoaded &&
                   activityLogData.map((logEntry) => (
