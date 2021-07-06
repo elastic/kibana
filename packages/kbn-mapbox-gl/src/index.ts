9ade654736b0e9be0277225e1ec973340bc37b10
Thomas Neirynck <thomas@elastic.co>
2021-07-06 18:55:22 +0200

    replace import

--- packages/kbn-mapbox-gl/src/index.ts
+++ packages/kbn-mapbox-gl/src/index.ts
@@ -24,14 +24,15 @@ import type {
   MapboxGeoJSONFeature,
   Point,
   CustomLayerInterface,
-} from 'mapbox-gl';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+} from 'maplibre-gl';
+import maplibregl from 'maplibre-gl/dist/maplibre-gl-csp';
 // @ts-expect-error
 import mbRtlPlugin from '!!file-loader!@mapbox/mapbox-gl-rtl-text/mapbox-gl-rtl-text.min.js';
 // @ts-expect-error
-import mbWorkerUrl from '!!file-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import mbWorkerUrl from '!!file-loader!maplibre-gl/dist/maplibre-gl-csp-worker';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
+const mapboxgl = maplibregl;
 mapboxgl.workerUrl = mbWorkerUrl;
 mapboxgl.setRTLTextPlugin(mbRtlPlugin);
 
