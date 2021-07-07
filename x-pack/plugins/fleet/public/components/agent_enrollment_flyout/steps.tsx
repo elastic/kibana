6fbeb12ffbd675d1b581463d9ecd0935b050a9b1
Kyle Pollich <kyle.pollich@elastic.co>
2021-07-07 13:57:51 -0400

    [Fleet + Integrations UI] Integrations UI Cleanup (#104641)
    * Remove Beta wording from Fleet messaging in ingestion tutorials
    
    * Rename fleet feature registration to include integrations
    
    * Pluralize + sentence case assets and expand dashboards by default
    
    * Update global search from packages -> integrations
    
    * Expand auth settings by default in package policy add agent flyout
    
    * Update wording for various fleet modules
    
    * Fix i18n
    
    * Fix unit test
    
    * Fix failing tests
    # Conflicts:
    #	x-pack/plugins/translations/translations/ja-JP.json
    #	x-pack/plugins/translations/translations/zh-CN.json

--- x-pack/plugins/fleet/public/components/agent_enrollment_flyout/steps.tsx
+++ x-pack/plugins/fleet/public/components/agent_enrollment_flyout/steps.tsx
@@ -121,14 +121,14 @@ export const AgentEnrollmentKeySelectionStep = ({
 }) => {
   return {
     title: i18n.translate('xpack.fleet.agentEnrollment.stepConfigurePolicyAuthenticationTitle', {
-      defaultMessage: 'Configure agent authentication',
+      defaultMessage: 'Select enrollment token',
     }),
     children: (
       <>
         <EuiText>
           <FormattedMessage
             id="xpack.fleet.agentEnrollment.agentAuthenticationSettings"
-            defaultMessage="{agentPolicyName} has been selected. Configure agent authentication based on the available policy options."
+            defaultMessage="{agentPolicyName} has been selected. Select which enrollment token to use when enrolling agents."
             values={{
               agentPolicyName: <strong>{agentPolicy.name}</strong>,
             }}
@@ -138,6 +138,7 @@ export const AgentEnrollmentKeySelectionStep = ({
         <AdvancedAgentAuthenticationSettings
           agentPolicyId={agentPolicy.id}
           selectedApiKeyId={selectedApiKeyId}
+          initialAuthenticationSettingsOpen
           onKeyChange={setSelectedAPIKeyId}
         />
       </>
