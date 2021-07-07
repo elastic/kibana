53a1f5dfb0c1072bc084bc6f5ae40ef749fed01c
Dzmitry Tamashevich <diaamnj@mail.ru>
2021-07-07 20:23:09 +0300

    [Discover] remove redundant files

--- src/plugins/discover/public/application/angular/doc_table/index.ts
+++ src/plugins/discover/public/application/angular/doc_table/index.ts
@@ -6,7 +6,6 @@
  * Side Public License, v 1.
  */
 
-export { createDocTableDirective } from './doc_table';
 export { getSort, getSortArray } from './lib/get_sort';
 export { getSortForSearchSource } from './lib/get_sort_for_search_source';
 export { getDefaultSort } from './lib/get_default_sort';
