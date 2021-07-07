0a333961ea0b288b39d87e3cc9c5027ee3358a6e
Felix Stürmer <stuermer@weltenwort.de>
2021-07-07 19:19:32 +0200

    Import less from io-ts-utils to reduce bundle size

--- x-pack/plugins/infra/common/alerting/logs/log_threshold/rule_data.ts
+++ x-pack/plugins/infra/common/alerting/logs/log_threshold/rule_data.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { jsonRt } from '@kbn/io-ts-utils';
+import { jsonRt } from '@kbn/io-ts-utils/target/json_rt';
 import * as rt from 'io-ts';
 import { alertParamsRT as logThresholdAlertParamsRT } from './types';
 
