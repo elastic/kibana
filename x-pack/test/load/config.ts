48917ccc760d7d6d850dce5027bd783cc3dcbbb5
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-07-08 10:38:26 +0200

    [load testing] adjust ES heap size (#101906) (#104810)
    * [load testing] increase es heap to 2g
    
    * update default simulation
    
    * [heap size] 4g
    
    * [heap size] 6g
    
    * [load testing] es heap size 8g
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/load/config.ts
+++ x-pack/test/load/config.ts
@@ -34,6 +34,7 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
     esTestCluster: {
       ...xpackFunctionalTestsConfig.get('esTestCluster'),
       serverArgs: [...xpackFunctionalTestsConfig.get('esTestCluster.serverArgs')],
+      esJavaOpts: '-Xms8g -Xmx8g',
     },
 
     kbnTestServer: {
