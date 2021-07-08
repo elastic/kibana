04eb47aebb9d1e3908d830cd3b937236d273a513
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2021-07-08 14:47:37 -0600

    table changes WIP

--- x-pack/plugins/security_solution/public/common/components/event_details/columns.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/columns.tsx
@@ -7,37 +7,20 @@
 
 /* eslint-disable react/display-name */
 
-import {
-  EuiCheckbox,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiIcon,
-  EuiPanel,
-  EuiToolTip,
-  EuiIconTip,
-  EuiText,
-} from '@elastic/eui';
-import { get, isEmpty } from 'lodash';
+import { EuiCheckbox, EuiPanel, EuiToolTip } from '@elastic/eui';
+import { get } from 'lodash';
 import memoizeOne from 'memoize-one';
 import React from 'react';
 import styled from 'styled-components';
-import { onFocusReFocusDraggable } from '../../../../../timelines/public';
 import { BrowserFields } from '../../containers/source';
-import { DragEffects } from '../drag_and_drop/draggable_wrapper';
-import { DroppableWrapper } from '../drag_and_drop/droppable_wrapper';
-import { DRAG_TYPE_FIELD, getDroppableId } from '../drag_and_drop/helpers';
-import { DraggableFieldBadge } from '../draggables/field_badge';
-import { DraggableFieldsBrowserField } from '../../../timelines/components/fields_browser/field_items';
-import { OverflowField } from '../tables/helpers';
 import { defaultColumnHeaderType } from '../../../timelines/components/timeline/body/column_headers/default_headers';
 import { DEFAULT_COLUMN_MIN_WIDTH } from '../../../timelines/components/timeline/body/constants';
-import { MESSAGE_FIELD_NAME } from '../../../timelines/components/timeline/body/renderers/constants';
-import { FormattedFieldValue } from '../../../timelines/components/timeline/body/renderers/formatted_field';
 import { OnUpdateColumns } from '../../../timelines/components/timeline/events';
-import { getIconFromType, getExampleText } from './helpers';
 import * as i18n from './translations';
 import { EventFieldsData } from './types';
 import { ColumnHeaderOptions } from '../../../../common';
+import { ValueCell } from './table/value_cell';
+import { FieldCell } from './table/field_cell';
 
 const HoverActionsContainer = styled(EuiPanel)`
   align-items: center;
@@ -53,13 +36,6 @@ const HoverActionsContainer = styled(EuiPanel)`
 
 HoverActionsContainer.displayName = 'HoverActionsContainer';
 
-const FullWidthFlexGroup = styled(EuiFlexGroup)`
-  width: 100%;
-`;
-
-const FullWidthFlexItem = styled(EuiFlexItem)`
-  width: 100%;
-`;
 export const getFieldFromBrowserField = memoizeOne(
   (keys: string[], browserFields: BrowserFields): BrowserFields => get(browserFields, keys),
   (newArgs, lastArgs) => newArgs[0].join() === lastArgs[0].join()
@@ -123,61 +99,13 @@ export const getColumns = ({
         browserFields
       );
       return (
-        <EuiFlexGroup alignItems="center" gutterSize="none">
-          <EuiFlexItem grow={false}>
-            <EuiToolTip content={data.type}>
-              <EuiIcon data-test-subj="field-type-icon" type={getIconFromType(data.type)} />
-            </EuiToolTip>
-          </EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            {(data.isObjectArray && data.type !== 'geo_point') || fieldFromBrowserField == null ? (
-              <EuiText size="xs">{field}</EuiText>
-            ) : (
-              <DroppableWrapper
-                droppableId={getDroppableId(
-                  `event-details-field-droppable-wrapper-${contextId}-${eventId}-${data.category}-${field}`
-                )}
-                key={getDroppableId(
-                  `event-details-field-droppable-wrapper-${contextId}-${eventId}-${data.category}-${field}`
-                )}
-                isDropDisabled={true}
-                type={DRAG_TYPE_FIELD}
-                renderClone={(provided) => (
-                  <div
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                    tabIndex={-1}
-                  >
-                    <DragEffects>
-                      <DraggableFieldBadge fieldId={field} />
-                    </DragEffects>
-                  </div>
-                )}
-              >
-                <DraggableFieldsBrowserField
-                  browserFields={browserFields}
-                  categoryId={data.category}
-                  fieldName={field}
-                  fieldCategory={data.category}
-                  onUpdateColumns={onUpdateColumns}
-                  timelineId={timelineId}
-                  toggleColumn={toggleColumn}
-                />
-              </DroppableWrapper>
-            )}
-          </EuiFlexItem>
-          {!isEmpty(data.description) && (
-            <EuiFlexItem grow={false}>
-              <EuiIconTip
-                aria-label={i18n.DESCRIPTION}
-                type="iInCircle"
-                color="subdued"
-                content={`${data.description} ${getExampleText(data.example)}`}
-              />
-            </EuiFlexItem>
-          )}
-        </EuiFlexGroup>
+        <FieldCell
+          browserFields={browserFields}
+          data={data}
+          field={field}
+          fieldFromBrowserField={fieldFromBrowserField}
+          onUpdateColumns={onUpdateColumns}
+        />
       );
     },
   },
@@ -192,48 +120,14 @@ export const getColumns = ({
         browserFields
       );
       return (
-        <FullWidthFlexGroup
-          direction="column"
-          alignItems="flexStart"
-          component="span"
-          gutterSize="none"
-        >
-          {values != null &&
-            values.map((value, i) => {
-              if (fieldFromBrowserField == null) {
-                return <EuiText size="s">{value}</EuiText>;
-              }
-              return (
-                <FullWidthFlexItem
-                  grow={false}
-                  component="span"
-                  key={`event-details-value-flex-item-${contextId}-${eventId}-${data.field}-${i}-${value}`}
-                >
-                  <div
-                    data-colindex={3}
-                    onFocus={onFocusReFocusDraggable}
-                    role="button"
-                    tabIndex={0}
-                  >
-                    {data.field === MESSAGE_FIELD_NAME ? (
-                      <OverflowField value={value} />
-                    ) : (
-                      <FormattedFieldValue
-                        contextId={`event-details-value-formatted-field-value-${contextId}-${eventId}-${data.field}-${i}-${value}`}
-                        eventId={eventId}
-                        fieldFormat={data.format}
-                        fieldName={data.field}
-                        fieldType={data.type}
-                        isObjectArray={data.isObjectArray}
-                        value={value}
-                        linkValue={getLinkValue(data.field)}
-                      />
-                    )}
-                  </div>
-                </FullWidthFlexItem>
-              );
-            })}
-        </FullWidthFlexGroup>
+        <ValueCell
+          contextId={contextId}
+          data={data}
+          eventId={eventId}
+          fieldFromBrowserField={fieldFromBrowserField}
+          getLinkValue={getLinkValue}
+          values={values}
+        />
       );
     },
   },
