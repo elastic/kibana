2a5974e45b3070d20dc611c018a6c109a5950c4e
Oliver Gupte <ogupte@users.noreply.github.com>
2021-07-09 17:52:17 -0400

    [APM] Support for data streams index patterns for cloud migration (#105015)
    * [APM] Support for data streams index patterns for cloud migration (#101095)
    
    * [APM] Update apm package version to 0.3.0 (elastic/apm-server/#5579)

--- x-pack/plugins/apm/server/index.ts
+++ x-pack/plugins/apm/server/index.ts
@@ -74,7 +74,7 @@ export function mergeConfigs(
     'apm_oss.metricsIndices': apmOssConfig.metricsIndices,
     'apm_oss.sourcemapIndices': apmOssConfig.sourcemapIndices,
     'apm_oss.onboardingIndices': apmOssConfig.onboardingIndices,
-    'apm_oss.indexPattern': apmOssConfig.indexPattern, // TODO: add data stream indices: traces-apm*,logs-apm*,metrics-apm*. Blocked by https://github.com/elastic/kibana/issues/87851
+    'apm_oss.indexPattern': apmOssConfig.indexPattern,
     /* eslint-enable @typescript-eslint/naming-convention */
     'xpack.apm.serviceMapEnabled': apmConfig.serviceMapEnabled,
     'xpack.apm.serviceMapFingerprintBucketSize':
@@ -118,6 +118,10 @@ export function mergeConfigs(
   mergedConfig[
     'apm_oss.metricsIndices'
   ] = `metrics-apm*,${mergedConfig['apm_oss.metricsIndices']}`;
+
+  mergedConfig[
+    'apm_oss.indexPattern'
+  ] = `traces-apm*,logs-apm*,metrics-apm*,${mergedConfig['apm_oss.indexPattern']}`;
 
   return mergedConfig;
 }
