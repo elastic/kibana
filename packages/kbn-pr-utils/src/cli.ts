30a883d4a6a089e3338144d7d1e595e4101decfb
spalger <spalger@users.noreply.github.com>
2021-07-09 11:34:12 -0700

    add support for GH_TOKEN/GITHUB_TOKEN environment variables

--- packages/kbn-pr-utils/src/cli.ts
+++ packages/kbn-pr-utils/src/cli.ts
@@ -19,9 +19,11 @@ import { ForceEsDocFailuresCommand } from './commands/force_es_doc_failures';
  * Define additional properties attached to the runContext which will be provided to all commands
  */
 function extendContext({ flags, log }: RunContext) {
-  const token = flags['github-token'];
+  const token = flags['github-token'] || process.env.GH_TOKEN || process.env.GITHUB_TOKEN;
   if (!token || typeof token !== 'string') {
-    throw createFailError('--github-token is required');
+    throw createFailError(
+      '--github-token, or $GH_TOKEN/$GITHUB_TOKEN environment variable is required'
+    );
   }
 
   const api = new PrApi(log, token);
@@ -41,8 +43,8 @@ export function runPrUtilsCli() {
       globalFlags: {
         string: ['github-token'],
         help: `
-        --github-token     Token used to talk to the Github API
-      `,
+          --github-token     Token used to talk to the Github API. Defaults to GH_TOKEN or GITHUB_TOKEN environment variable.
+        `,
       },
       extendContext,
     },
