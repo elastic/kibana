65df8b1da779e97be6e3c46767e9ab10181faad9
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-07-09 18:31:14 +0300

    Added temp v2.

--- x-pack/plugins/canvas/public/components/sidebar/__stories__/deprecated_element_settings.stories.tsx
+++ x-pack/plugins/canvas/public/components/sidebar/__stories__/deprecated_element_settings.stories.tsx
@@ -14,13 +14,16 @@ import {
   EuiIcon,
   EuiPanel,
   EuiSpacer,
+  EuiSplitPanel,
   EuiTabbedContent,
+  EuiText,
   EuiTitle,
+  EuiToken,
+  EuiTreeView,
 } from '@elastic/eui';
 
-storiesOf('Deprecated/Sidebar/ElementSettings', module).add(
-  'Markdown deprecation. v1. Example',
-  () => {
+storiesOf('Deprecated/Sidebar/ElementSettings', module)
+  .add('Markdown deprecation. v1. Example', () => {
     const columns = [
       {
         field: 'deprecated',
@@ -93,5 +96,81 @@ storiesOf('Deprecated/Sidebar/ElementSettings', module).add(
       },
     ];
     return <EuiTabbedContent tabs={tabs} initialSelectedTab={tabs[0]} size="s" />;
-  }
-);
+  })
+  .add('Markdown deprecation. v2. Example', () => {
+    const items = [
+      {
+        label: 'markdown -> markdownVis',
+        id: 'item_one',
+        icon: <EuiToken iconType="tokenModule" />,
+        iconWhenExpanded: <EuiToken iconType="tokenModule" />,
+        isExpanded: true,
+        children: [
+          {
+            label: 'content -> markdown',
+            id: 'item_a',
+            icon: <EuiToken iconType="tokenParameter" />,
+          },
+          {
+            label: 'font',
+            id: 'item_a',
+            icon: <EuiToken iconType="tokenParameter" />,
+          },
+          {
+            label: 'openLinksInNewTab (removed)',
+            id: 'item_a',
+            icon: <EuiToken iconType="tokenParameter" />,
+          },
+        ],
+      },
+    ];
+    const tabs = [
+      {
+        id: 'deprecated',
+        name: (
+          <span>
+            <EuiIcon type="alert" color="warning" />
+            &nbsp;Deprecated
+          </span>
+        ),
+        content: (
+          <EuiPanel paddingSize="l">
+            <EuiSpacer size="m" />
+            <EuiCallOut
+              title="Attention! `Markdown` is deprecated in favor of `MarkdownVis`."
+              color="warning"
+              iconType="flag"
+            >
+              <p>
+                Please, update <strong>Expression</strong> based on the guidelines to avoid problems
+                in the future <strong>v8.0.0 release</strong>.
+              </p>
+            </EuiCallOut>
+            <EuiHorizontalRule size="full" />
+            <EuiTitle size="m">
+              <h4>Deprecation notes</h4>
+            </EuiTitle>
+            <EuiHorizontalRule size="full" />
+            <EuiSpacer size="m" />
+            <EuiSplitPanel.Outer direction="row">
+              <EuiSplitPanel.Inner>
+                <EuiTreeView items={items} aria-label="Sample Folder Tree" />
+              </EuiSplitPanel.Inner>
+              <EuiSplitPanel.Inner color="subdued">
+                <EuiText>
+                  <p>Attribute `markdown`:</p>
+                </EuiText>
+                <EuiText>
+                  <span>Type: string</span>
+                </EuiText>
+                <EuiText>
+                  <span>Required: true</span>
+                </EuiText>
+              </EuiSplitPanel.Inner>
+            </EuiSplitPanel.Outer>
+          </EuiPanel>
+        ),
+      },
+    ];
+    return <EuiTabbedContent tabs={tabs} initialSelectedTab={tabs[0]} size="s" />;
+  });
