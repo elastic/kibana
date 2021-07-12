180a52e985574b31d26d5bc844ac141a0530dcca
Ying Mao <ying.mao@elastic.co>
2021-07-12 11:45:38 -0400

    Reverting change to Serializable state

--- src/plugins/kibana_utils/common/persistable_state/index.ts
+++ src/plugins/kibana_utils/common/persistable_state/index.ts
@@ -8,14 +8,7 @@
 
 import { SavedObjectReference } from '../../../../core/types';
 
-export type SerializableValue =
-  | string
-  | number
-  | boolean
-  | null
-  | undefined
-  | unknown
-  | SerializableState;
+export type SerializableValue = string | number | boolean | null | undefined | SerializableState;
 export type Serializable = SerializableValue | SerializableValue[];
 
 export type SerializableState = {
