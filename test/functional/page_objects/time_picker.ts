8631812c2aec9d47e948ffc9f707cbf1feb480ba
Jean-Louis Leysens <jloleysens@gmail.com>
2021-07-13 10:11:49 +0200

    rather wait for the timepicker to close

--- test/functional/page_objects/time_picker.ts
+++ test/functional/page_objects/time_picker.ts
@@ -7,7 +7,6 @@
  */
 
 import moment from 'moment';
-import expect from '@kbn/expect';
 import { FtrService } from '../ftr_provider_context';
 import { WebElementWrapper } from '../services/lib/web_element_wrapper';
 
@@ -126,9 +125,6 @@ export class TimePickerPageObject extends FtrService {
     await this.inputValue('superDatePickerAbsoluteDateInput', toTime);
     await this.browser.pressKeys(this.browser.keys.ESCAPE); // close popover because sometimes browser can't find start input
 
-    const endDateButton = await this.testSubjects.find('superDatePickerendDatePopoverButton');
-    expect(await endDateButton.getVisibleText()).to.be(toTime);
-
     // set from time
     await this.testSubjects.click('superDatePickerstartDatePopoverButton');
     await this.waitPanelIsGone(panel);
@@ -136,10 +132,11 @@ export class TimePickerPageObject extends FtrService {
     await this.testSubjects.click('superDatePickerAbsoluteTab');
     await this.testSubjects.click('superDatePickerAbsoluteDateInput');
     await this.inputValue('superDatePickerAbsoluteDateInput', fromTime);
-    await this.browser.pressKeys(this.browser.keys.ESCAPE); // close popover because sometimes browser can't find start input
+    await this.browser.pressKeys(this.browser.keys.ESCAPE);
 
-    const startDateButton = await this.testSubjects.find('superDatePickerstartDatePopoverButton');
-    expect(await startDateButton.getVisibleText()).to.be(fromTime);
+    await this.retry.waitFor('Timepicker popover to close', async () => {
+      return !(await this.testSubjects.exists('superDatePickerAbsoluteDateInput'));
+    });
 
     const superDatePickerApplyButtonExists = await this.testSubjects.exists(
       'superDatePickerApplyTimeButton'
