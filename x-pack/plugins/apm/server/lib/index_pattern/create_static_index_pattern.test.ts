92babadbb2d4215f515b2dc60f35ae8faf5e3c5b
Oliver Gupte <olivergupte@gmail.com>
2021-07-13 14:45:02 -0400

    Fixed and added more unit tests

--- x-pack/plugins/apm/server/lib/index_pattern/create_static_index_pattern.test.ts
+++ x-pack/plugins/apm/server/lib/index_pattern/create_static_index_pattern.test.ts
@@ -13,6 +13,11 @@ import { APMConfig } from '../..';
 
 function getMockSavedObjectsClient() {
   return ({
+    get: jest.fn(() => ({
+      attributes: {
+        title: 'apm-*',
+      },
+    })),
     create: jest.fn(),
   } as unknown) as InternalSavedObjectsClient;
 }
@@ -68,5 +73,63 @@ describe('createStaticIndexPattern', () => {
     });
 
     expect(savedObjectsClient.create).toHaveBeenCalled();
+  });
+
+  it(`should upgrade an index pattern if 'apm_oss.indexPattern' does not match title`, async () => {
+    const setup = {} as Setup;
+
+    // does have APM data
+    jest
+      .spyOn(HistoricalAgentData, 'hasHistoricalAgentData')
+      .mockResolvedValue(true);
+
+    const savedObjectsClient = getMockSavedObjectsClient();
+    const apmIndexPatternTitle = 'traces-apm*,logs-apm*,metrics-apm*,apm-*';
+
+    await createStaticIndexPattern({
+      setup,
+      config: {
+        'xpack.apm.autocreateApmIndexPattern': true,
+        'apm_oss.indexPattern': apmIndexPatternTitle,
+      } as APMConfig,
+      savedObjectsClient,
+      spaceId: 'default',
+    });
+
+    expect(savedObjectsClient.get).toHaveBeenCalled();
+    expect(savedObjectsClient.create).toHaveBeenCalled();
+    // @ts-ignore
+    expect(savedObjectsClient.create.mock.calls[0][1].title).toBe(
+      apmIndexPatternTitle
+    );
+    // @ts-ignore
+    expect(savedObjectsClient.create.mock.calls[0][2].overwrite).toBe(true);
+  });
+
+  it(`should not upgrade an index pattern if 'apm_oss.indexPattern' already match existing title`, async () => {
+    const setup = {} as Setup;
+
+    // does have APM data
+    jest
+      .spyOn(HistoricalAgentData, 'hasHistoricalAgentData')
+      .mockResolvedValue(true);
+
+    const savedObjectsClient = getMockSavedObjectsClient();
+    const apmIndexPatternTitle = 'apm-*';
+
+    await createStaticIndexPattern({
+      setup,
+      config: {
+        'xpack.apm.autocreateApmIndexPattern': true,
+        'apm_oss.indexPattern': apmIndexPatternTitle,
+      } as APMConfig,
+      savedObjectsClient,
+      spaceId: 'default',
+    });
+
+    expect(savedObjectsClient.get).toHaveBeenCalled();
+    expect(savedObjectsClient.create).toHaveBeenCalled();
+    // @ts-ignore
+    expect(savedObjectsClient.create.mock.calls[0][2].overwrite).toBe(false);
   });
 });
