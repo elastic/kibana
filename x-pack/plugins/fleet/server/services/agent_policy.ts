34e2d09046d807094a1d114d0e07eb2f2d2c82b1
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2021-07-13 14:51:30 +0200

    Add specific datasets

--- x-pack/plugins/fleet/server/services/agent_policy.ts
+++ x-pack/plugins/fleet/server/services/agent_policy.ts
@@ -63,8 +63,25 @@ import { appContextService } from './app_context';
 
 const SAVED_OBJECT_TYPE = AGENT_POLICY_SAVED_OBJECT_TYPE;
 
-const MONITORING_LOG_DATASETS = ['logs-elastic_agent*'];
-const MONITORING_METRICS_DATASETS = ['metrics-elastic_agent*'];
+const MONITORING_LOG_DATASETS = [
+  'elastic_agent',
+  'elastic_agent.elastic_agent',
+  'elastic_agent.filebeat',
+  'elastic_agent.fleet_server',
+  'elastic_agent.metricbeat',
+  'elastic_agent.osquerybeat',
+  'elastic_agent.packetbeat',
+];
+const MONITORING_METRICS_DATASETS = [
+  'elastic_agent',
+  'elastic_agent.elastic_agent',
+  'elastic_agent.apm_server',
+  'elastic_agent.filebeat',
+  'elastic_agent.fleet_server',
+  'elastic_agent.metricbeat',
+  'elastic_agent.osquerybeat',
+  'elastic_agent.packetbeat',
+];
 
 class AgentPolicyService {
   private triggerAgentPolicyUpdatedEvent = async (
@@ -777,12 +794,12 @@ class AgentPolicyService {
       let names: string[] = [];
       if (fullAgentPolicy.agent.monitoring.logs) {
         names = names.concat(
-          MONITORING_LOG_DATASETS.map((dataset) => `${dataset}-${monitoringNamespace}`)
+          MONITORING_LOG_DATASETS.map((dataset) => `logs-${dataset}-${monitoringNamespace}`)
         );
       }
       if (fullAgentPolicy.agent.monitoring.metrics) {
         names = names.concat(
-          MONITORING_METRICS_DATASETS.map((dataset) => `${dataset}-${monitoringNamespace}`)
+          MONITORING_METRICS_DATASETS.map((dataset) => `metrics-${dataset}-${monitoringNamespace}`)
         );
       }
 
