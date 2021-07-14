2ec94e3c8096532bdcf902b46bcbd3533bd6f8bd
Ashokaditya <am.struktr@gmail.com>
2021-07-14 14:19:51 +0200

    handle promise rejections
    fixes elastic/security-team/issues/1407

--- x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/store/middleware.ts
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/store/middleware.ts
@@ -311,6 +311,9 @@ export const endpointMiddlewareFactory: ImmutableMiddlewareFactory<EndpointState
         const response = await coreStart.http.get<HostInfo>(
           resolvePathVariables(HOST_METADATA_GET_ROUTE, { id: selectedEndpoint as string })
         );
+        if (!response) {
+          return;
+        }
         dispatch({
           type: 'serverReturnedEndpointDetails',
           payload: response,
@@ -696,7 +699,12 @@ const loadEndpointsPendingActions = async ({
   }
 
   try {
-    const { data: pendingActions } = await fetchPendingActionsByAgentId(Array.from(agentsIds));
+    const response = await fetchPendingActionsByAgentId(Array.from(agentsIds));
+    if (!response) {
+      return;
+    }
+    const { data: pendingActions } = response;
+
     const agentIdToPendingActions: AgentIdsPendingActions = new Map();
 
     for (const pendingAction of pendingActions) {
