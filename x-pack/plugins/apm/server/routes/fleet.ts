5c990ddefa50cc7a3287d0ec96fda183928c4c4c
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-07-14 03:29:05 -0400

    [APM] Updates the APM static index pattern regardless of fleet migration status (#105471) (#105540)
    * [APM] Updates the APM static index pattern regardless of fleet migration status (#105469)
    
    * Fixed and added more unit tests
    
    * ignores eslint error for property name in mock object
    
    Co-authored-by: Oliver Gupte <ogupte@users.noreply.github.com>

--- x-pack/plugins/apm/server/routes/fleet.ts
+++ x-pack/plugins/apm/server/routes/fleet.ts
@@ -25,8 +25,6 @@ import { createCloudApmPackgePolicy } from '../lib/fleet/create_cloud_apm_packag
 import { getUnsupportedApmServerSchema } from '../lib/fleet/get_unsupported_apm_server_schema';
 import { isSuperuser } from '../lib/fleet/is_superuser';
 import { getInternalSavedObjectsClient } from '../lib/helpers/get_internal_saved_objects_client';
-import { setupRequest } from '../lib/helpers/setup_request';
-import { createStaticIndexPattern } from '../lib/index_pattern/create_static_index_pattern';
 
 const hasFleetDataRoute = createApmServerRoute({
   endpoint: 'GET /api/apm/fleet/has_data',
@@ -156,7 +154,7 @@ const createCloudApmPackagePolicyRoute = createApmServerRoute({
   endpoint: 'POST /api/apm/fleet/cloud_apm_package_policy',
   options: { tags: ['access:apm', 'access:apm_write'] },
   handler: async (resources) => {
-    const { plugins, context, config, request, logger, core } = resources;
+    const { plugins, context, config, request, logger } = resources;
     const cloudApmMigrationEnabled =
       config['xpack.apm.agent.migrations.enabled'];
     if (!plugins.fleet || !plugins.security) {
@@ -174,7 +172,7 @@ const createCloudApmPackagePolicyRoute = createApmServerRoute({
       throw Boom.forbidden(CLOUD_SUPERUSER_REQUIRED_MESSAGE);
     }
 
-    const cloudApmAackagePolicy = await createCloudApmPackgePolicy({
+    const cloudApmPackagePolicy = await createCloudApmPackgePolicy({
       cloudPluginSetup,
       fleetPluginStart,
       savedObjectsClient,
@@ -182,25 +180,7 @@ const createCloudApmPackagePolicyRoute = createApmServerRoute({
       logger,
     });
 
-    const [setup, internalSavedObjectsClient] = await Promise.all([
-      setupRequest(resources),
-      core
-        .start()
-        .then(({ savedObjects }) => savedObjects.createInternalRepository()),
-    ]);
-
-    const spaceId = plugins.spaces?.setup.spacesService.getSpaceId(request);
-
-    // force update the index pattern title with data streams
-    await createStaticIndexPattern(
-      setup,
-      config,
-      internalSavedObjectsClient,
-      spaceId,
-      true
-    );
-
-    return { cloud_apm_package_policy: cloudApmAackagePolicy };
+    return { cloudApmPackagePolicy };
   },
 });
 
