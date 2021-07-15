1e5ff97ac9e896bc398dc2feb8800b55b18804a6
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2021-07-15 14:47:18 -0700

    [test] Migrating to kbn_archiver from es_archiver - for the Maps app (#103028) (#105838)
    * converting Maps es_archiver to kbn_archiver
    
    * delete the esArchiver .kibana reference directory
    
    * fix the path of the json file
    
    * use the delete API to delete the missing references populated in the data.json
    
    * fix the path
    
    * kbn_archiver_maps.json
    
    * added the missing ref
    
    * restoring it to use esArchiver
    
    * replace esArchiver to use kbnArchiver
    
    * moved the data.json directly under kbnArchiver
    
     Please enter the commit message for your changes. Lines starting
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	x-pack/test/functional/es_archives/maps/kibana/mappings.json

--- x-pack/test/functional/apps/maps/index.js
+++ x-pack/test/functional/apps/maps/index.js
@@ -9,14 +9,38 @@ export default function ({ loadTestFile, getService }) {
   const kibanaServer = getService('kibanaServer');
   const esArchiver = getService('esArchiver');
   const browser = getService('browser');
+  const log = getService('log');
+  const supertest = getService('supertest');
 
   describe('maps app', function () {
     this.tags(['skipFirefox']);
 
     before(async () => {
       await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/logstash_functional');
+      await kibanaServer.importExport.load(
+        'x-pack/test/functional/fixtures/kbn_archiver/maps.json'
+      );
+      //Find the missing references manually, use the below API to delete it after successful import.
+      log.info('Delete index pattern');
+      log.debug('id: ' + 'idThatDoesNotExitForESGeoGridSource');
+      log.debug('id: ' + 'idThatDoesNotExitForESSearchSource');
+      log.debug('id: ' + 'idThatDoesNotExitForESJoinSource');
+      await supertest
+        .delete('/api/index_patterns/index_pattern/' + 'idThatDoesNotExitForESGeoGridSource')
+        .set('kbn-xsrf', 'true')
+        .expect(200);
+
+      await supertest
+        .delete('/api/index_patterns/index_pattern/' + 'idThatDoesNotExitForESSearchSource')
+        .set('kbn-xsrf', 'true')
+        .expect(200);
+
+      await supertest
+        .delete('/api/index_patterns/index_pattern/' + 'idThatDoesNotExitForESJoinSource')
+        .set('kbn-xsrf', 'true')
+        .expect(200);
+
       await esArchiver.load('x-pack/test/functional/es_archives/maps/data');
-      await esArchiver.load('x-pack/test/functional/es_archives/maps/kibana');
       await kibanaServer.uiSettings.replace({
         defaultIndex: 'c698b940-e149-11e8-a35a-370a8516603a',
       });
@@ -25,7 +49,9 @@ export default function ({ loadTestFile, getService }) {
 
     after(async () => {
       await esArchiver.unload('x-pack/test/functional/es_archives/maps/data');
-      await esArchiver.unload('x-pack/test/functional/es_archives/maps/kibana');
+      await kibanaServer.importExport.unload(
+        'x-pack/test/functional/fixtures/kbn_archiver/maps.json'
+      );
     });
 
     describe('', async function () {
