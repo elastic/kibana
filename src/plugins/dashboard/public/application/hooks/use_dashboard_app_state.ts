d6ee95471ad34af1fdc48603c06b78a0082eebc3
Devon Thomson <devon.thomson@elastic.co>
2021-07-20 13:11:27 -0300

    Update use_dashboard_app_state.ts

--- src/plugins/dashboard/public/application/hooks/use_dashboard_app_state.ts
+++ src/plugins/dashboard/public/application/hooks/use_dashboard_app_state.ts
@@ -335,7 +335,6 @@ export const useDashboardAppState = ({
       if (refreshInterval) timefilter.setRefreshInterval(refreshInterval);
     }
     dispatchDashboardStateChange(setDashboardState(lastSavedState));
-    // setDashboardAppState((s) => ({ ...s, hasUnsavedChanges: false }));
   }, [lastSavedState, dashboardAppState, data.query.timefilter, dispatchDashboardStateChange]);
 
   /**
