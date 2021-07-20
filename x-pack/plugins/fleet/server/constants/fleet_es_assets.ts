3a7bdc38fdd1ab5de839b36d098e984337615d3b
Andrew Kroh <andrew.kroh@elastic.co>
2021-07-20 11:26:29 -0400

    Add elastic/fleet-server service account to trusted users

--- x-pack/plugins/fleet/server/constants/fleet_es_assets.ts
+++ x-pack/plugins/fleet/server/constants/fleet_es_assets.ts
@@ -120,10 +120,12 @@ processors:
       params:
         # List of users responsible for creating Fleet output API keys.
         trusted_users:
-          - username: elastic
-            realm: reserved
+          - username: elastic/fleet-server
+            realm: _service_account
           - username: cloud-internal-agent-server
             realm: found
+          - username: elastic
+            realm: reserved
   - remove:
       field: _security
       ignore_missing: true
