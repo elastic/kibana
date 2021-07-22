fe7b01d73830cb72c8bdee06549ef79427be0957
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-07-22 12:44:50 +0300

    Removed export common types from public/index.ts

--- src/plugins/presentation_util/public/index.ts
+++ src/plugins/presentation_util/public/index.ts
@@ -29,7 +29,6 @@ export { PresentationUtilPluginSetup, PresentationUtilPluginStart } from './type
 export { SaveModalDashboardProps } from './components/types';
 export { projectIDs, ProjectID, Project } from '../common/labs';
 export * from '../common/lib';
-export * from '../common/types';
 
 export {
   LazyLabsBeakerButton,
