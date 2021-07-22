330129a6c019ee61c6ac51bc5bdc067c930b711b
James Gowdy <jgowdy@elastic.co>
2021-07-22 09:59:03 +0100

    renaming exported file name

--- x-pack/plugins/ml/public/application/components/import_export_jobs/export_jobs_flyout/jobs_export_service.ts
+++ x-pack/plugins/ml/public/application/components/import_export_jobs/export_jobs_flyout/jobs_export_service.ts
@@ -8,27 +8,47 @@
 // @ts-expect-error
 import { saveAs } from '@elastic/filesaver';
 import type { MlApiServices } from '../../../services/ml_api_service';
+import type { JobType } from '../../../../../common/types/saved_objects';
+import type { Job, Datafeed } from '../../../../../common/types/anomaly_detection_jobs';
+import type { DataFrameAnalyticsConfig } from '../../../../../common/types/data_frame_analytics';
+
+type ExportableConfigs =
+  | Array<
+      | {
+          job?: Job;
+          datafeed?: Datafeed;
+        }
+      | undefined
+    >
+  | DataFrameAnalyticsConfig[];
 
 export class JobsExportService {
   constructor(private _mlApiServices: MlApiServices) {}
 
   public async exportAnomalyDetectionJobs(jobIds: string[]) {
     const configs = await Promise.all(jobIds.map(this._mlApiServices.jobs.jobForCloning));
-    const configsForExport = configs.length === 1 ? configs[0] : configs;
-    const blob = new Blob([JSON.stringify(configsForExport, null, 2)], {
-      type: 'application/json',
-    });
-    saveAs(blob, 'ml_jobs.json');
+    this._export(configs, 'anomaly-detector');
   }
 
   public async exportDataframeAnalyticsJobs(jobIds: string[]) {
     const {
       data_frame_analytics: configs,
     } = await this._mlApiServices.dataFrameAnalytics.getDataFrameAnalytics(jobIds.join(','), true);
+    this._export(configs, 'data-frame-analytics');
+  }
+
+  private _export(configs: ExportableConfigs, jobType: JobType) {
     const configsForExport = configs.length === 1 ? configs[0] : configs;
     const blob = new Blob([JSON.stringify(configsForExport, null, 2)], {
       type: 'application/json',
     });
-    saveAs(blob, 'ml_jobs.json');
+    const fileName = this._createFileName(jobType);
+    saveAs(blob, fileName);
+  }
+
+  private _createFileName(jobType: JobType) {
+    return (
+      (jobType === 'anomaly-detector' ? 'anomaly_detection' : 'data_frame_analytics') + '_jobs.json'
+    );
   }
 }
