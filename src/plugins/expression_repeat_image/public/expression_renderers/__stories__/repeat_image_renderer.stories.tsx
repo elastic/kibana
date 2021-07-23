3e4b64b779c518e94eb339cb821f9cc2a65566f0
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-07-23 11:29:06 +0300

    [Canvas] Expression repeat image (#104255)
    * Repeat Image plugin added.

--- src/plugins/expression_repeat_image/public/expression_renderers/__stories__/repeat_image_renderer.stories.tsx
+++ src/plugins/expression_repeat_image/public/expression_renderers/__stories__/repeat_image_renderer.stories.tsx
@@ -1,19 +1,20 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { repeatImage } from '../repeat_image';
+import { Render } from '../../../../presentation_util/public/__stories__';
+import { repeatImageRenderer } from '../repeat_image_renderer';
 import {
   getElasticLogo,
   getElasticOutline,
 } from '../../../../../../src/plugins/presentation_util/common/lib';
 import { waitFor } from '../../../../../../src/plugins/presentation_util/public/__stories__';
-import { Render } from './render';
 
 const Renderer = ({
   elasticLogo,
@@ -30,7 +31,7 @@ const Renderer = ({
     emptyImage: elasticOutline,
   };
 
-  return <Render renderer={repeatImage} config={config} width="400px" />;
+  return <Render renderer={repeatImageRenderer} config={config} width="400px" />;
 };
 
 storiesOf('enderers/repeatImage', module).add(
--- x-pack/plugins/canvas/shareable_runtime/supported_renderers.js
+++ x-pack/plugins/canvas/shareable_runtime/supported_renderers.js
@@ -6,7 +6,6 @@
  */
 
 import { image } from '../canvas_plugin_src/renderers/image';
-import { repeatImage } from '../canvas_plugin_src/renderers/repeat_image';
 import { markdown } from '../canvas_plugin_src/renderers/markdown';
 import { metric } from '../canvas_plugin_src/renderers/metric';
 import { pie } from '../canvas_plugin_src/renderers/pie';
@@ -14,11 +13,12 @@ import { plot } from '../canvas_plugin_src/renderers/plot';
 import { progress } from '../canvas_plugin_src/renderers/progress';
 import { table } from '../canvas_plugin_src/renderers/table';
 import { text } from '../canvas_plugin_src/renderers/text';
-import { revealImageRenderer as revealImage } from '../../../../src/plugins/expression_reveal_image/public';
 import {
   errorRenderer as error,
   debugRenderer as debug,
 } from '../../../../src/plugins/expression_error/public';
+import { repeatImageRenderer as repeatImage } from '../../../../src/plugins/expression_repeat_image/public';
+import { revealImageRenderer as revealImage } from '../../../../src/plugins/expression_reveal_image/public';
 import { shapeRenderer as shape } from '../../../../src/plugins/expression_shape/public';
 
 /**
