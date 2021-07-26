db7ebb3e8ae54000ec250f55548bccb3659ca4e9
Josh Dover <1813008+joshdover@users.noreply.github.com>
2021-07-26 16:33:10 +0200

    Add 404 handling to oldest action task query

--- x-pack/plugins/task_manager/server/queries/oldest_idle_action_task.ts
+++ x-pack/plugins/task_manager/server/queries/oldest_idle_action_task.ts
@@ -15,50 +15,58 @@ export const getOldestIdleActionTask = async (
   taskManagerIndex: string
 ): Promise<number | string> => {
   let oldestAlertAction: number | string = Date.now();
-  const results = await client.search<{ task: { runAt: string } }>({
-    size: 1,
-    index: taskManagerIndex,
-    body: {
-      sort: [{ 'task.runAt': { order: 'asc' } }],
-      query: {
-        bool: {
-          filter: {
-            bool: {
-              must: [
-                {
-                  terms: {
-                    'task.taskType': [
-                      'actions:.email',
-                      'actions:.index',
-                      'actions:.pagerduty',
-                      'actions:.swimlane',
-                      'actions:.server-log',
-                      'actions:.slack',
-                      'actions:.webhook',
-                      'actions:.servicenow',
-                      'actions:.servicenow-sir',
-                      'actions:.jira',
-                      'actions:.resilient',
-                      'actions:.teams',
-                    ],
+  const response = await client.search<{ task: { runAt: string } }>(
+    {
+      size: 1,
+      index: taskManagerIndex,
+      body: {
+        sort: [{ 'task.runAt': { order: 'asc' } }],
+        query: {
+          bool: {
+            filter: {
+              bool: {
+                must: [
+                  {
+                    terms: {
+                      'task.taskType': [
+                        'actions:.email',
+                        'actions:.index',
+                        'actions:.pagerduty',
+                        'actions:.swimlane',
+                        'actions:.server-log',
+                        'actions:.slack',
+                        'actions:.webhook',
+                        'actions:.servicenow',
+                        'actions:.servicenow-sir',
+                        'actions:.jira',
+                        'actions:.resilient',
+                        'actions:.teams',
+                      ],
+                    },
                   },
-                },
-                {
-                  term: { type: 'task' },
-                },
-                {
-                  term: { 'task.status': 'idle' },
-                },
-              ],
+                  {
+                    term: { type: 'task' },
+                  },
+                  {
+                    term: { 'task.status': 'idle' },
+                  },
+                ],
+              },
             },
           },
         },
       },
     },
-  });
+    { ignore: [404] }
+  );
 
-  if (results.body.hits.hits.length !== 0) {
-    oldestAlertAction = results.body.hits.hits[0]._source?.task.runAt ?? oldestAlertAction;
+  // If the index doesn't exist, fallback to default
+  if ((response.body as { error?: { status: number } }).error?.status === 404) {
+    return oldestAlertAction;
+  }
+
+  if (response.body?.hits?.hits?.length !== 0) {
+    oldestAlertAction = response.body.hits.hits[0]._source?.task.runAt ?? oldestAlertAction;
   }
 
   return oldestAlertAction;
