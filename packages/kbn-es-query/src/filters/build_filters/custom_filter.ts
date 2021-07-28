d4ef7a7eab18b81a924b0931964b76871247ff68
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-07-28 09:50:29 -0400

    [Data][Filters] Move more filter utils to package and cleanup API (#106566) (#106988)
    * Move more utils to package and cleanup API
    
    * docs and imports
    
    * better imports
    
    * change comment
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Liza Katz <lizka.k@gmail.com>

--- packages/kbn-es-query/src/filters/build_filters/custom_filter.ts
+++ packages/kbn-es-query/src/filters/build_filters/custom_filter.ts
@@ -6,8 +6,8 @@
  * Side Public License, v 1.
  */
 
-export { IndexPatternSelectProps } from './index_pattern_select';
-export { FilterLabel, FilterItem } from './filter_bar';
-export { QueryStringInput, QueryStringInputProps } from './query_string_input';
-export { SearchBar, SearchBarProps, StatefulSearchBarProps } from './search_bar';
-export { SuggestionsComponent } from './typeahead';
+import type { Filter } from './types';
+
+export type CustomFilter = Filter & {
+  query: any;
+};
