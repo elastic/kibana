8b89515aac2fe0efa7bc9c3e88a03fb365af34b1
Kevin Qualters <56408403+kqualters-elastic@users.noreply.github.com>
2021-07-28 17:10:37 -0400

    [Security Solution][RAC] Migrate add to case action to timelines plugin (#106205)
    * First pass add to case action in timelines plugin
    
    * Fix fake duplicate import lint rule and some type errors
    
    * Fix some tests
    
    * Remove use_insert_timeline and pass as prop
    
    * Remove unneeded ports, fix types/tests
    
    * Finish fixing types and tests for add to case action
    
    * Remove duplicated security_solution code
    
    * Pass appId as props
    
    * Fix lint and a type error
    
    * Use react-router-dom instead of window.location.search
    
    * Fix broken test
    
    * Remove unused imports
    
    * Remove unused export and related code

--- x-pack/plugins/timelines/public/components/actions/timeline/cases/translations.ts
+++ x-pack/plugins/timelines/public/components/actions/timeline/cases/translations.ts
@@ -7,63 +7,60 @@
 
 import { i18n } from '@kbn/i18n';
 
-export const ACTION_ADD_CASE = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.addCase',
-  {
-    defaultMessage: 'Add to case',
-  }
-);
+export const ACTION_ADD_CASE = i18n.translate('xpack.timelines.cases.timeline.actions.addCase', {
+  defaultMessage: 'Add to case',
+});
 
 export const ACTION_ADD_NEW_CASE = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.addNewCase',
+  'xpack.timelines.cases.timeline.actions.addNewCase',
   {
     defaultMessage: 'Add to new case',
   }
 );
 
 export const ACTION_ADD_EXISTING_CASE = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.addExistingCase',
+  'xpack.timelines.cases.timeline.actions.addExistingCase',
   {
     defaultMessage: 'Add to existing case',
   }
 );
 
 export const ACTION_ADD_TO_CASE_ARIA_LABEL = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.addToCaseAriaLabel',
+  'xpack.timelines.cases.timeline.actions.addToCaseAriaLabel',
   {
     defaultMessage: 'Attach alert to case',
   }
 );
 
 export const ACTION_ADD_TO_CASE_TOOLTIP = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.addToCaseTooltip',
+  'xpack.timelines.cases.timeline.actions.addToCaseTooltip',
   {
     defaultMessage: 'Add to case',
   }
 );
 
 export const CASE_CREATED_SUCCESS_TOAST = (title: string) =>
-  i18n.translate('xpack.securitySolution.cases.timeline.actions.caseCreatedSuccessToast', {
+  i18n.translate('xpack.timelines.cases.timeline.actions.caseCreatedSuccessToast', {
     values: { title },
     defaultMessage: 'An alert has been added to "{title}"',
   });
 
 export const CASE_CREATED_SUCCESS_TOAST_TEXT = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.caseCreatedSuccessToastText',
+  'xpack.timelines.cases.timeline.actions.caseCreatedSuccessToastText',
   {
     defaultMessage: 'Alerts in this case have their status synched with the case status',
   }
 );
 
 export const VIEW_CASE = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.caseCreatedSuccessToastViewCaseLink',
+  'xpack.timelines.cases.timeline.actions.caseCreatedSuccessToastViewCaseLink',
   {
     defaultMessage: 'View Case',
   }
 );
 
 export const PERMISSIONS_MSG = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.permissionsMessage',
+  'xpack.timelines.cases.timeline.actions.permissionsMessage',
   {
     defaultMessage:
       'You are currently missing the required permissions to attach alerts to cases. Please contact your administrator for further assistance.',
@@ -71,8 +68,12 @@ export const PERMISSIONS_MSG = i18n.translate(
 );
 
 export const UNSUPPORTED_EVENTS_MSG = i18n.translate(
-  'xpack.securitySolution.cases.timeline.actions.unsupportedEventsMessage',
+  'xpack.timelines.cases.timeline.actions.unsupportedEventsMessage',
   {
     defaultMessage: 'This event cannot be attached to a case',
   }
 );
+
+export const CREATE_TITLE = i18n.translate('xpack.timelines.cases.caseView.create', {
+  defaultMessage: 'Create new case',
+});
