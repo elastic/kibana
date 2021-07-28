e9ec6954e22d20370a7d5aed15960a4020be5871
Anton Dosov <anton.dosov@elastic.co>
2021-07-28 13:59:32 +0200

    [FieldFormats] Lazy load field formats editors (#106439)

--- src/plugins/index_pattern_field_editor/public/service/format_editor_service.ts
+++ src/plugins/index_pattern_field_editor/public/service/format_editor_service.ts
@@ -9,18 +9,19 @@
 import { FieldFormatEditors } from './field_format_editors';
 
 import {
-  BytesFormatEditor,
-  ColorFormatEditor,
-  DateFormatEditor,
-  DateNanosFormatEditor,
-  DurationFormatEditor,
-  NumberFormatEditor,
-  PercentFormatEditor,
-  StaticLookupFormatEditor,
-  StringFormatEditor,
-  TruncateFormatEditor,
-  UrlFormatEditor,
-  HistogramFormatEditor,
+  bytesFormatEditorFactory,
+  colorFormatEditorFactory,
+  dateFormatEditorFactory,
+  dateNanosFormatEditorFactory,
+  durationFormatEditorFactory,
+  numberFormatEditorFactory,
+  percentFormatEditorFactory,
+  staticLookupFormatEditorFactory,
+  stringFormatEditorFactory,
+  truncateFormatEditorFactory,
+  urlFormatEditorFactory,
+  histogramFormatEditorFactory,
+  FieldFormatEditorFactory,
 } from '../components';
 
 /**
@@ -36,22 +37,24 @@ export class FormatEditorService {
   }
 
   public setup() {
-    const defaultFieldFormatEditors = [
-      BytesFormatEditor,
-      ColorFormatEditor,
-      DateFormatEditor,
-      DateNanosFormatEditor,
-      DurationFormatEditor,
-      NumberFormatEditor,
-      PercentFormatEditor,
-      StaticLookupFormatEditor,
-      StringFormatEditor,
-      TruncateFormatEditor,
-      UrlFormatEditor,
-      HistogramFormatEditor,
+    const defaultFieldFormatEditorFactories: FieldFormatEditorFactory[] = [
+      bytesFormatEditorFactory,
+      colorFormatEditorFactory,
+      dateFormatEditorFactory,
+      dateNanosFormatEditorFactory,
+      durationFormatEditorFactory,
+      numberFormatEditorFactory,
+      percentFormatEditorFactory,
+      staticLookupFormatEditorFactory,
+      stringFormatEditorFactory,
+      truncateFormatEditorFactory,
+      urlFormatEditorFactory,
+      histogramFormatEditorFactory,
     ];
 
-    const fieldFormatEditorsSetup = this.fieldFormatEditors.setup(defaultFieldFormatEditors);
+    const fieldFormatEditorsSetup = this.fieldFormatEditors.setup(
+      defaultFieldFormatEditorFactories
+    );
 
     return {
       fieldFormatEditors: fieldFormatEditorsSetup,
