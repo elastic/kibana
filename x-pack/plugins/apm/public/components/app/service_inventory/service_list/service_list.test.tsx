bf7d45d02cef6d7c059cad356ab7e4445868482c
cauemarcondes <caue.marcondes@elastic.co>
2021-07-29 15:41:54 -0400

    fixing TS issue

--- x-pack/plugins/apm/public/components/app/service_inventory/service_list/service_list.test.tsx
+++ x-pack/plugins/apm/public/components/app/service_inventory/service_list/service_list.test.tsx
@@ -28,13 +28,17 @@ describe('ServiceList', () => {
 
   it('renders empty state', () => {
     expect(() =>
-      renderWithTheme(<ServiceList items={[]} />, { wrapper: Wrapper })
+      renderWithTheme(<ServiceList isLoading={false} items={[]} />, {
+        wrapper: Wrapper,
+      })
     ).not.toThrowError();
   });
 
   it('renders with data', () => {
     expect(() =>
-      renderWithTheme(<ServiceList items={items} />, { wrapper: Wrapper })
+      renderWithTheme(<ServiceList isLoading={false} items={items} />, {
+        wrapper: Wrapper,
+      })
     ).not.toThrowError();
   });
 
@@ -69,18 +73,24 @@ describe('ServiceList', () => {
 
   describe('without ML data', () => {
     it('does not render the health column', () => {
-      const { queryByText } = renderWithTheme(<ServiceList items={items} />, {
-        wrapper: Wrapper,
-      });
+      const { queryByText } = renderWithTheme(
+        <ServiceList isLoading={false} items={items} />,
+        {
+          wrapper: Wrapper,
+        }
+      );
       const healthHeading = queryByText('Health');
 
       expect(healthHeading).toBeNull();
     });
 
     it('sorts by throughput', async () => {
-      const { findByTitle } = renderWithTheme(<ServiceList items={items} />, {
-        wrapper: Wrapper,
-      });
+      const { findByTitle } = renderWithTheme(
+        <ServiceList isLoading={false} items={items} />,
+        {
+          wrapper: Wrapper,
+        }
+      );
 
       expect(await findByTitle('Throughput')).toBeInTheDocument();
     });
@@ -90,6 +100,7 @@ describe('ServiceList', () => {
     it('renders the health column', async () => {
       const { findByTitle } = renderWithTheme(
         <ServiceList
+          isLoading={false}
           items={items.map((item) => ({
             ...item,
             healthStatus: ServiceHealthStatus.warning,
