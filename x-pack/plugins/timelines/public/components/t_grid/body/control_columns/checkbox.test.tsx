00b25595e415ec58172e11f265973c3d19cb1d19
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2021-07-29 14:58:10 +0200

    Add unit tests

--- x-pack/plugins/timelines/public/components/t_grid/body/control_columns/checkbox.test.tsx
+++ x-pack/plugins/timelines/public/components/t_grid/body/control_columns/checkbox.test.tsx
@@ -5,12 +5,72 @@
  * 2.0.
  */
 
-describe('checkbox controll column', () => {
+import { render, fireEvent } from '@testing-library/react';
+import { ActionProps, HeaderActionProps, TimelineTabs } from '../../../../../common';
+import { HeaderCheckBox, RowCheckBox } from './checkbox';
+import React from 'react';
+
+describe('checkbox control column', () => {
   describe('RowCheckBox', () => {
-    test('displays loader when id is included on loadingEventIds', () => {});
-    test('calls onRowSelected when checked', () => {});
+    const defaultProps: ActionProps = {
+      ariaRowindex: 1,
+      columnId: 'test-columnId',
+      columnValues: 'test-columnValues',
+      checked: false,
+      onRowSelected: jest.fn(),
+      eventId: 'test-event-id',
+      loadingEventIds: [],
+      onEventDetailsPanelOpened: jest.fn(),
+      showCheckboxes: true,
+      data: [],
+      ecsData: {
+        _id: 'test-ecsData-id',
+      },
+      index: 1,
+      rowIndex: 1,
+      showNotes: true,
+      timelineId: 'test-timelineId',
+    };
+    test('displays loader when id is included on loadingEventIds', () => {
+      const { getByTestId } = render(
+        <RowCheckBox {...defaultProps} loadingEventIds={[defaultProps.eventId]} />
+      );
+      expect(getByTestId('event-loader')).not.toBeNull();
+    });
+
+    test('calls onRowSelected when checked', () => {
+      const onRowSelected = jest.fn();
+      const { getByTestId } = render(
+        <RowCheckBox {...defaultProps} onRowSelected={onRowSelected} />
+      );
+
+      fireEvent.click(getByTestId('select-event'));
+
+      expect(onRowSelected).toHaveBeenCalled();
+    });
   });
   describe('HeaderCheckBox', () => {
-    test('calls onSelectAll when checked', () => {});
+    const defaultProps: HeaderActionProps = {
+      width: 99999,
+      browserFields: {},
+      columnHeaders: [],
+      isSelectAllChecked: true,
+      onSelectAll: jest.fn(),
+      showEventsSelect: true,
+      showSelectAllCheckbox: true,
+      sort: [],
+      tabType: TimelineTabs.query,
+      timelineId: 'test-timelineId',
+    };
+
+    test('calls onSelectAll when checked', () => {
+      const onSelectAll = jest.fn();
+      const { getByTestId } = render(
+        <HeaderCheckBox {...defaultProps} onSelectAll={onSelectAll} />
+      );
+      fireEvent.click(getByTestId('select-all-events'));
+
+      expect(onSelectAll).toHaveBeenCalled();
+    });
   });
 });
