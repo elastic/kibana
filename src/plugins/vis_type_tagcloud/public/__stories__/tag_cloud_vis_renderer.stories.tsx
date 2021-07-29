67b70120f3638d1a41ff8f4daff231c5c4408175
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-07-29 18:14:55 +0300

    Added storybook ( still doesn't work ).

---
+++ src/plugins/vis_type_tagcloud/public/__stories__/tag_cloud_vis_renderer.stories.tsx
@@ -0,0 +1,98 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { PaletteDefinition, SeriesLayer } from '../../../charts/public';
+import { getTagCloudVisRenderer } from '../tag_cloud_vis_renderer';
+import { Render } from '../../../presentation_util/public/__stories__';
+import { TagCloudVisRenderValue } from '../tag_cloud_fn';
+
+export const getPaletteRegistry = () => {
+  const mockPalette1: PaletteDefinition = {
+    id: 'default',
+    title: 'My Palette',
+    getCategoricalColor: (_: SeriesLayer[]) => 'black',
+    getCategoricalColors: (num: number) => ['red', 'black'],
+    toExpression: () => ({
+      type: 'expression',
+      chain: [
+        {
+          type: 'function',
+          function: 'system_palette',
+          arguments: {
+            name: ['default'],
+          },
+        },
+      ],
+    }),
+  };
+
+  return {
+    get: (name: string) =>
+      name === 'custom' ? mockPalette1 : name !== 'default' ? mockPalette1 : mockPalette1,
+    getAll: () => [mockPalette1],
+  };
+};
+
+const palettes = {
+  getPalettes: async () => getPaletteRegistry(),
+};
+
+const config: TagCloudVisRenderValue = {
+  visType: 'tagcloud',
+  visData: {
+    type: 'datatable',
+    columns: [
+      { id: 'country', name: 'country', meta: { type: 'string' } },
+      { id: 'Count', name: 'Count', meta: { type: 'number' } },
+    ],
+    rows: [
+      { country: 'UK', Count: 50 },
+      { country: 'US', Count: 20 },
+      { country: 'GM', Count: 30 },
+      { country: 'UA', Count: 10 },
+      { country: 'FR', Count: 40 },
+    ],
+  },
+  visParams: {
+    orientation: 'single',
+    scale: 'linear',
+    minFontSize: 18,
+    maxFontSize: 72,
+    showLabel: true,
+    palette: {
+      type: 'palette',
+      name: 'default',
+    },
+    metric: {
+      accessor: { id: 'Count', name: 'Count', meta: { type: 'number' } },
+      type: 'vis_dimension',
+      format: { params: { type: 'number' } },
+    },
+    bucket: {
+      accessor: { id: 'country', name: 'country', meta: { type: 'string' } },
+      type: 'vis_dimension',
+      format: { params: { type: 'string' } },
+    },
+  },
+  syncColors: false,
+};
+
+storiesOf('renderers/tag_cloud_vis', module)
+  .add('default', () => {
+    return <Render renderer={() => getTagCloudVisRenderer({ palettes })} config={config} />;
+  })
+  .add('With empty result', () => {
+    return (
+      <Render
+        renderer={() => getTagCloudVisRenderer({ palettes })}
+        config={{ ...config, visData: { ...config.visData, rows: [] } }}
+      />
+    );
+  });
