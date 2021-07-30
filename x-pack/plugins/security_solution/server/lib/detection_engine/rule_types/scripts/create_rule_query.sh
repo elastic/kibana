668dddc22e9e3bebaffc2ff507379c1982d227e8
Madison Caldwell <madison.caldwell@elastic.co>
2021-07-30 11:54:48 -0400

    Use ruleId

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/scripts/create_rule_query.sh
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/scripts/create_rule_query.sh
@@ -28,7 +28,7 @@ curl -X POST ${KIBANA_URL}${SPACE_URL}/api/alerts/alert \
      "references": [],
      "riskScore": 21,
      "riskScoreMapping": [],
-     "ruleId": "siem.queryRule",
+     "ruleId": "81dec1ba-b779-469c-9667-6b0e865fb86b",
      "severity": "low",
      "severityMapping": [],
      "threat": [],
