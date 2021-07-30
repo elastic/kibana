29acec13a4bb65531ae71de2b1cb061e82f242d0
Paul Tavares <paul.tavares@elastic.co>
2021-07-30 16:35:29 -0400

    Data loading for tests (via generator indexer) + initial test setup for Alert Details

--- x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_permissions.ts
+++ x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_permissions.ts
@@ -5,21 +5,37 @@
  * 2.0.
  */
 
+import { Client } from '@elastic/elasticsearch';
 import { FtrProviderContext } from '../../ftr_provider_context';
 import {
   createUserAndRole,
   deleteUserAndRole,
   ROLES,
 } from '../../../common/services/security_solution';
+import { indexHostsAndAlerts } from '../../../../plugins/security_solution/common/endpoint/index_data';
 
 export default ({ getPageObjects, getService }: FtrProviderContext) => {
-  const PageObjects = getPageObjects(['security', 'endpoint']);
+  const PageObjects = getPageObjects(['security', 'endpoint', 'detections']);
   const testSubjects = getService('testSubjects');
+  const esClient = getService('es');
+  const kbnClient = getService('kibanaServer');
 
   describe('Endpoint permissions:', () => {
     before(async () => {
       // load data into the system
-      // should we use the data_indexer? or esArchive?
+      await indexHostsAndAlerts(
+        esClient as Client,
+        kbnClient,
+        'seed',
+        1,
+        1,
+        'metrics-endpoint.metadata-default', // TODO: are there `const`'s for these indexes so that we don't use static names here?
+        'metrics-endpoint.policy-default',
+        'logs-endpoint.events.process-default',
+        'logs-endpoint.alerts-default',
+        1,
+        true
+      );
 
       // Force a logout so that we start from the login page
       await PageObjects.security.forceLogout();
@@ -32,7 +48,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     // Run the same set of tests against all of the Security Solution roles
     for (const role of Object.keys(ROLES) as Array<keyof typeof ROLES>) {
       describe(`when running with user/role [${role}]`, () => {
-        beforeEach(async () => {
+        before(async () => {
           // create role/user
           await createUserAndRole(getService, ROLES[role]);
 
@@ -40,7 +56,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
           await PageObjects.security.login(role, 'changeme');
         });
 
-        afterEach(async () => {
+        after(async () => {
           // Log the user back out
           await PageObjects.security.forceLogout();
 
@@ -53,9 +69,16 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
           await testSubjects.existOrFail('noIngestPermissions');
         });
 
-        it('should display endpoint data on Host Details', async () => {});
+        it('should display endpoint data on Host Details', async () => {
+          // path: http://localhost:5601/app/security/hosts/Host-ku5jy6j0pw
+          // await new Promise((r) => setTimeout(r, 60000));
+        });
 
-        it('should display endpoint data on Alert Details', async () => {});
+        it('should display endpoint data on Alert Details', async () => {
+          await PageObjects.detections.navigateToAlerts();
+          await PageObjects.detections.openFirstAlertDetailsForHostName('abc');
+          await new Promise((r) => setTimeout(r, 60000));
+        });
       });
     }
   });
