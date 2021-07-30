72b5cba6110ca7fa81dc4c9d79d957eb9c3e5bdc
vadimkibana <vadimkibana@gmail.com>
2021-07-30 21:29:46 +0200

    implement memory storage for short urls

---
+++ src/plugins/share/server/url_service/short_urls/storage/memory_short_url_storage.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { v4 as uuidv4 } from 'uuid';
+import { SerializableState } from 'src/plugins/kibana_utils/common';
+import { ShortUrlData } from 'src/plugins/share/common/url_service/short_urls/types';
+import { ShortUrlStorage } from '../types';
+
+export class MemoryShortUrlStorage implements ShortUrlStorage {
+  private urls = new Map<string, ShortUrlData>();
+
+  public async create<P extends SerializableState = SerializableState>(
+    data: Omit<ShortUrlData<P>, 'id'>
+  ): Promise<ShortUrlData<P>> {
+    const id = uuidv4();
+    const url: ShortUrlData<P> = { ...data, id };
+    this.urls.set(id, url);
+    return url;
+  }
+
+  public async getById<P extends SerializableState = SerializableState>(
+    id: string
+  ): Promise<ShortUrlData<P>> {
+    if (!this.urls.has(id)) {
+      throw new Error(`No short url with id "${id}"`);
+    }
+    return this.urls.get(id)! as ShortUrlData<P>;
+  }
+
+  public async getBySlug<P extends SerializableState = SerializableState>(
+    slug: string
+  ): Promise<ShortUrlData<P>> {
+    for (const url of this.urls.values()) {
+      if (url.slug === slug) {
+        return url as ShortUrlData<P>;
+      }
+    }
+    throw new Error(`No short url with slug "${slug}".`);
+  }
+
+  public async delete(id: string): Promise<void> {
+    this.urls.delete(id);
+  }
+}
