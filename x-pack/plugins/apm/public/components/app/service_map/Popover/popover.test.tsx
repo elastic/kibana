ed39a25ece19e3422af23817c194018b2e921226
Nathan L Smith <smith@nlsmith.com>
2021-08-01 11:12:13 -0500

    type and i18n fixes

--- x-pack/plugins/apm/public/components/app/service_map/Popover/popover.test.tsx
+++ x-pack/plugins/apm/public/components/app/service_map/Popover/popover.test.tsx
@@ -8,7 +8,7 @@
 import { composeStories } from '@storybook/testing-react';
 import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
-import * as stories from './popover.stories';
+import * as stories from './Popover.stories';
 
 const { Backend, ExternalsList, Resource, Service } = composeStories(stories);
 
