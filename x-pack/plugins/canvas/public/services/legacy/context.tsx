41b4945c0fa4aa2f31bd71cec45afa6d44779392
Clint Andrew Hall <clint.hall@elastic.co>
2021-08-02 17:10:38 -0400

    [canvas] Create Expressions Service; remove legacy service (#107350)

--- x-pack/plugins/canvas/public/services/legacy/context.tsx
+++ x-pack/plugins/canvas/public/services/legacy/context.tsx
@@ -21,7 +21,6 @@ export interface WithServicesProps {
 
 const defaultContextValue = {
   embeddables: {},
-  expressions: {},
   search: {},
 };
 
@@ -29,7 +28,6 @@ export const ServicesContext = createContext<CanvasServices>(defaultContextValue
 
 export const useServices = () => useContext(ServicesContext);
 export const useEmbeddablesService = () => useServices().embeddables;
-export const useExpressionsService = () => useServices().expressions;
 export const useLabsService = () => useServices().labs;
 export const withServices = <Props extends WithServicesProps>(type: ComponentType<Props>) => {
   const EnhancedType: FC<Props> = (props) =>
@@ -44,7 +42,6 @@ export const LegacyServicesProvider: FC<{
   const specifiedProviders: CanvasServiceProviders = { ...services, ...providers };
   const value = {
     embeddables: specifiedProviders.embeddables.getService(),
-    expressions: specifiedProviders.expressions.getService(),
     search: specifiedProviders.search.getService(),
     labs: specifiedProviders.labs.getService(),
   };
