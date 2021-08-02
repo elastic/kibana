d10c4ce6ff48816eb4c184ed50ee4b40ffca78f6
Clint Andrew Hall <clint.hall@elastic.co>
2021-08-02 14:20:12 -0400

    [canvas] Create Reporting Service; remove legacy service (#107352)
    * [canvas] Create Reporting Service; remove legacy service
    
    * Fix merge mistake

--- x-pack/plugins/canvas/public/services/kibana/index.ts
+++ x-pack/plugins/canvas/public/services/kibana/index.ts
@@ -16,12 +16,14 @@ import { workpadServiceFactory } from './workpad';
 import { navLinkServiceFactory } from './nav_link';
 import { notifyServiceFactory } from './notify';
 import { platformServiceFactory } from './platform';
+import { reportingServiceFactory } from './reporting';
 import { CanvasPluginServices } from '..';
 import { CanvasStartDeps } from '../../plugin';
 
 export { workpadServiceFactory } from './workpad';
 export { notifyServiceFactory } from './notify';
 export { platformServiceFactory } from './platform';
+export { reportingServiceFactory } from './reporting';
 
 export const pluginServiceProviders: PluginServiceProviders<
   CanvasPluginServices,
@@ -31,6 +33,7 @@ export const pluginServiceProviders: PluginServiceProviders<
   navLink: new PluginServiceProvider(navLinkServiceFactory),
   notify: new PluginServiceProvider(notifyServiceFactory),
   platform: new PluginServiceProvider(platformServiceFactory),
+  reporting: new PluginServiceProvider(reportingServiceFactory),
 };
 
 export const pluginServiceRegistry = new PluginServiceRegistry<
