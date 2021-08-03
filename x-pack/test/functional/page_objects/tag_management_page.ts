28fb9613ce393e7a94e50e45a00babdf13a0c94d
Brian Seeders <brian.seeders@elastic.co>
2021-08-03 11:44:51 -0400

    Use the Enter key to dismiss the color picker instead

--- x-pack/test/functional/page_objects/tag_management_page.ts
+++ x-pack/test/functional/page_objects/tag_management_page.ts
@@ -22,6 +22,7 @@ type TagFormValidation = FillTagFormFields;
  * Sub page object to manipulate the create/edit tag modal.
  */
 class TagModal extends FtrService {
+  private readonly browser = this.ctx.getService('browser');
   private readonly find = this.ctx.getService('find');
   private readonly testSubjects = this.ctx.getService('testSubjects');
   private readonly retry = this.ctx.getService('retry');
@@ -62,7 +63,9 @@ class TagModal extends FtrService {
       // Close the popover before moving to the next input, as it can get in the way of interacting with other elements
       await this.testSubjects.existOrFail('euiSaturation');
       await this.retry.try(async () => {
-        await this.find.clickByCssSelector('.euiModalHeader');
+        if (await this.testSubjects.exists('euiSaturation', { timeout: 10 })) {
+          await this.browser.pressKeys(this.browser.keys.ENTER);
+        }
         await this.testSubjects.missingOrFail('euiSaturation', { timeout: 250 });
       });
     }
