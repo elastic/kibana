15379a632f99e9a1f2ba81fedda52b94c9dbf587
mgiota <panagiota.mitsopoulou@elastic.co>
2021-08-03 12:49:45 +0300

    update alerts table placeholder to use the status field

--- x-pack/plugins/observability/public/pages/alerts/alerts_search_bar.tsx
+++ x-pack/plugins/observability/public/pages/alerts/alerts_search_bar.tsx
@@ -35,7 +35,7 @@ export function AlertsSearchBar({
     <SearchBar
       indexPatterns={dynamicIndexPattern}
       placeholder={i18n.translate('xpack.observability.alerts.searchBarPlaceholder', {
-        defaultMessage: '"rule.id" : "metrics.alert.threshold"',
+        defaultMessage: 'kibana.alert.status : "open"',
       })}
       query={{ query: query ?? '', language: queryLanguage }}
       timeHistory={timeHistory}
