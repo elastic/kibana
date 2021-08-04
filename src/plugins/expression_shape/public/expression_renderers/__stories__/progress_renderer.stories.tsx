6286954dec635a9a84795a914f99dc1bfff86f95
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-08-04 11:33:01 +0300

    [Canvas] Expression progress (#104457)
    * Added `expression_progress` plugin.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/expression_shape/public/expression_renderers/__stories__/progress_renderer.stories.tsx
+++ src/plugins/expression_shape/public/expression_renderers/__stories__/progress_renderer.stories.tsx
@@ -1,15 +1,16 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { progress } from '../';
-import { Render } from '../../__stories__/render';
-import { Shape } from '../../../functions/common/progress';
+import { Render } from '../../../../presentation_util/public/__stories__';
+import { progressRenderer } from '../progress_renderer';
+import { Progress } from '../../../common';
 
 storiesOf('renderers/progress', module).add('default', () => {
   const config = {
@@ -22,11 +23,11 @@ storiesOf('renderers/progress', module).add('default', () => {
     },
     label: '66%',
     max: 1,
-    shape: Shape.UNICORN,
+    shape: Progress.UNICORN,
     value: 0.66,
     valueColor: '#000',
     valueWeight: 15,
   };
 
-  return <Render renderer={progress} config={config} />;
+  return <Render renderer={progressRenderer} config={config} />;
 });
