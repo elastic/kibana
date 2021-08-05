904fda7c6c7c45d3793513f60f13c0db70b005d6
Byron Hulcher <byronhulcher@gmail.com>
2021-08-05 14:06:13 -0400

    New DeleteDomainPanel component

---
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/delete_domain_panel.tsx
@@ -0,0 +1,80 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+
+import { useActions, useValues } from 'kea';
+
+import { EuiButton, EuiSpacer, EuiText } from '@elastic/eui';
+
+import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n/react';
+
+import { CrawlerSingleDomainLogic } from '../crawler_single_domain_logic';
+
+export const DeleteDomainPanel: React.FC = ({}) => {
+  const { domain } = useValues(CrawlerSingleDomainLogic);
+  const { deleteDomain } = useActions(CrawlerSingleDomainLogic);
+
+  if (!domain) {
+    return null;
+  }
+
+  return (
+    <>
+      <EuiText size="s">
+        <p>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.appSearch.crawler.deleteDomainPanel.description"
+            defaultMessage="Remove this domain from your crawler. This will also delete all entry points and crawl
+                    rules you have setup. {cannotUndoMessage}."
+            values={{
+              cannotUndoMessage: (
+                <strong>
+                  {i18n.translate(
+                    'xpack.enterpriseSearch.appSearch.crawler.deleteDomainPanel.cannotUndoMessage',
+                    {
+                      defaultMessage: 'This cannot be undone',
+                    }
+                  )}
+                </strong>
+              ),
+            }}
+          />
+        </p>
+      </EuiText>
+      <EuiSpacer size="m" />
+      <EuiButton
+        color="danger"
+        iconType="trash"
+        onClick={() => {
+          if (
+            confirm(
+              i18n.translate(
+                'xpack.enterpriseSearch.appSearch.crawler.deleteDomainPanel.confirmationPopupMessage',
+                {
+                  defaultMessage:
+                    "Are you sure you want to remove the domain {domainUrl} from this engine's crawler?",
+                  values: { domainUrl: domain.url },
+                }
+              )
+            )
+          ) {
+            deleteDomain(domain);
+          }
+        }}
+      >
+        {i18n.translate(
+          'xpack.enterpriseSearch.appSearch.crawler.deleteDomainPanel.deleteDomainButtonLabel',
+          {
+            defaultMessage: 'Delete domain',
+          }
+        )}
+      </EuiButton>
+    </>
+  );
+};
