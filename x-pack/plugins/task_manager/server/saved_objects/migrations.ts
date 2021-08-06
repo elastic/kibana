43564473750a61fc1056b13f9efd0639295d4b29
chrisronline <chrisronline@gmail.com>
2021-08-06 14:21:11 -0400

    Use new API

--- x-pack/plugins/task_manager/server/saved_objects/migrations.ts
+++ x-pack/plugins/task_manager/server/saved_objects/migrations.ts
@@ -4,14 +4,13 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import uuidv5 from 'uuid/v5';
-import { SavedObjectMigrationMap, SavedObjectUnsanitizedDoc } from '../../../../../src/core/server';
-import { TaskInstance, TaskInstanceWithDeprecatedFields } from '../task';
 
-// TODO: Use https://github.com/elastic/kibana/issues/107744 when it's available
-function deterministicallyRegenerateObjectId(namespace: string, type: string, id: string) {
-  return uuidv5(`${namespace}:${type}:${id}`, uuidv5.DNS); // the uuidv5 namespace constant (uuidv5.DNS) is arbitrary
-}
+import {
+  SavedObjectMigrationMap,
+  SavedObjectsUtils,
+  SavedObjectUnsanitizedDoc,
+} from '../../../../../src/core/server';
+import { TaskInstance, TaskInstanceWithDeprecatedFields } from '../task';
 
 export const migrations: SavedObjectMigrationMap = {
   '7.4.0': (doc) => ({
@@ -32,7 +31,7 @@ function migrationSavedObjectIds(doc: SavedObjectUnsanitizedDoc<TaskInstanceWith
     }
 
     if (params.alertId && params.spaceId && params.spaceId !== 'default') {
-      const newId = deterministicallyRegenerateObjectId(params.spaceId, 'alert', params.alertId);
+      const newId = SavedObjectsUtils.getConvertedObjectId(params.spaceId, 'alert', params.alertId);
       return {
         ...doc,
         attributes: {
@@ -53,7 +52,7 @@ function migrationSavedObjectIds(doc: SavedObjectUnsanitizedDoc<TaskInstanceWith
     }
 
     if (params.actionTaskParamsId && params.spaceId && params.spaceId !== 'default') {
-      const newId = deterministicallyRegenerateObjectId(
+      const newId = SavedObjectsUtils.getConvertedObjectId(
         params.spaceId,
         'action_task_params',
         params.actionTaskParamsId
