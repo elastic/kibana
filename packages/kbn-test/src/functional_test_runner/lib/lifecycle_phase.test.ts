9d69f94190d7b3089ad4e6d76ee9c0b608f62bdc
Tyler Smalley <tyler.smalley@elastic.co>
2021-08-08 22:28:26 -0700

    Update snapshot
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- packages/kbn-test/src/functional_test_runner/lib/lifecycle_phase.test.ts
+++ packages/kbn-test/src/functional_test_runner/lib/lifecycle_phase.test.ts
@@ -135,6 +135,12 @@ describe('without randomness', () => {
         },
         Notification {
           "error": undefined,
+          "hasValue": true,
+          "kind": "N",
+          "value": undefined,
+        },
+        Notification {
+          "error": undefined,
           "hasValue": false,
           "kind": "C",
           "value": undefined,
@@ -143,6 +149,12 @@ describe('without randomness', () => {
     `);
     expect(afterNotifs).toMatchInlineSnapshot(`
       Array [
+        Notification {
+          "error": undefined,
+          "hasValue": true,
+          "kind": "N",
+          "value": undefined,
+        },
         Notification {
           "error": undefined,
           "hasValue": true,
@@ -183,12 +195,6 @@ describe('without randomness', () => {
     await expect(phase.after$.pipe(materialize(), toArray()).toPromise()).resolves
       .toMatchInlineSnapshot(`
             Array [
-              Notification {
-                "error": undefined,
-                "hasValue": true,
-                "kind": "N",
-                "value": undefined,
-              },
               Notification {
                 "error": undefined,
                 "hasValue": false,
