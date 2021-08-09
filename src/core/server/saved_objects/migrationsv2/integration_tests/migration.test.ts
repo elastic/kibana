4e66c9662b3207afbd5c5a288b1b3e67bf4d04cd
Tiago Costa <tiagoffcc@hotmail.com>
2021-08-09 16:20:01 +0100

    chore(NA): moving @kbn/config to babel transpiler (#107763)
    * chore(NA): moving @kbn/config to babel transpiler
    
    * chore(NA): fix types
    
    * chore(NA): include missing deps
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/core/server/saved_objects/migrationsv2/integration_tests/migration.test.ts
+++ src/core/server/saved_objects/migrationsv2/integration_tests/migration.test.ts
@@ -12,7 +12,7 @@ import Util from 'util';
 import Semver from 'semver';
 import { REPO_ROOT } from '@kbn/dev-utils';
 import { Env } from '@kbn/config';
-import { getEnvOptions } from '@kbn/config/target/mocks';
+import { getEnvOptions } from '../../../config/mocks';
 import * as kbnTestServer from '../../../../test_helpers/kbn_server';
 import { ElasticsearchClient } from '../../../elasticsearch';
 import { SavedObjectsRawDoc } from '../../serialization';
