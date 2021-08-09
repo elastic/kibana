6929a58cd39d8535964faf8b5a6af4f41d093c9e
Ece Ozalp <ozale272@newschool.edu>
2021-08-09 17:56:06 -0400

    adds indicator match rule

---
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/indicator_match/create_indicator_match_alert_type.test.ts
@@ -0,0 +1,152 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+// eslint-disable-next-line @kbn/eslint/no-restricted-paths
+import { elasticsearchClientMock } from 'src/core/server/elasticsearch/client/mocks';
+
+import { allowedExperimentalValues } from '../../../../../common/experimental_features';
+import { createRuleTypeMocks } from '../__mocks__/rule_type';
+import { createIndicatorMatchAlertType } from './create_indicator_match_alert_type';
+
+jest.mock('../utils/get_list_client', () => ({
+  getListClient: jest.fn().mockReturnValue({
+    listClient: jest.fn(),
+    exceptionsClient: jest.fn(),
+  }),
+}));
+
+jest.mock('../../signals/rule_status_service', () => ({
+  ruleStatusServiceFactory: () => ({
+    goingToRun: jest.fn(),
+    success: jest.fn(),
+    partialFailure: jest.fn(),
+    error: jest.fn(),
+  }),
+}));
+
+describe('Indicator Match Alerts', () => {
+  it('does not send an alert when no events found', async () => {
+    const { services, dependencies, executor } = createRuleTypeMocks();
+    const indicatorMatchAlertType = createIndicatorMatchAlertType({
+      experimentalFeatures: allowedExperimentalValues,
+      indexAlias: 'alerts.security-alerts',
+      lists: dependencies.lists,
+      logger: dependencies.logger,
+      mergeStrategy: 'allFields',
+      ruleDataClient: dependencies.ruleDataClient,
+      ruleDataService: dependencies.ruleDataService,
+      version: '1.0.0',
+    });
+
+    dependencies.alerting.registerType(indicatorMatchAlertType);
+
+    const params = {
+      from: 'now-1m',
+      index: ['test-*'],
+      threatIndex: ['filebeat-*'],
+      threatLanguage: 'kuery',
+      threatMapping: [
+        {
+          entries: [
+            {
+              field: 'file.hash.md5',
+              type: 'mapping',
+              value: 'threatintel.indicator.file.hash.md5',
+            },
+          ],
+        },
+      ],
+      threatQuery: '*:*',
+      to: 'now',
+    };
+
+    services.scopedClusterClient.asCurrentUser.search.mockReturnValue(
+      elasticsearchClientMock.createSuccessTransportRequestPromise({
+        hits: {
+          hits: [],
+          sequences: [],
+          events: [],
+          total: {
+            relation: 'eq',
+            value: 0,
+          },
+        },
+        took: 0,
+        timed_out: false,
+        _shards: {
+          failed: 0,
+          skipped: 0,
+          successful: 1,
+          total: 1,
+        },
+      })
+    );
+
+    await executor({ params });
+    expect(dependencies.ruleDataClient.getWriter).not.toBeCalled();
+  });
+
+  // TODO: fix with mock enrichment
+  // it('sends a properly formatted alert when events are found', async () => {
+  //   const { services, dependencies, executor } = createRuleTypeMocks();
+  //   const indicatorMatchAlertType = createIndicatorMatchAlertType({
+  //     experimentalFeatures: allowedExperimentalValues,
+  //     indexAlias: 'alerts.security-alerts',
+  //     lists: dependencies.lists,
+  //     logger: dependencies.logger,
+  //     mergeStrategy: 'allFields',
+  //     ruleDataClient: dependencies.ruleDataClient,
+  //     ruleDataService: dependencies.ruleDataService,
+  //     version: '1.0.0',
+  //   });
+  //
+  //   dependencies.alerting.registerType(indicatorMatchAlertType);
+  //
+  //   const params = {
+  //     from: 'now-1m',
+  //     index: ['*'],
+  //     threatIndex: ['filebeat-*'],
+  //     threatLanguage: 'kuery',
+  //     threatMapping: [
+  //       {
+  //         entries: [
+  //           {
+  //             field: 'file.hash.md5',
+  //             type: 'mapping',
+  //             value: 'threatintel.indicator.file.hash.md5',
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //     threatQuery: '*:*',
+  //     to: 'now',
+  //   };
+  //
+  //   services.scopedClusterClient.asCurrentUser.search.mockReturnValue(
+  //     elasticsearchClientMock.createSuccessTransportRequestPromise({
+  //       hits: {
+  //         hits: [sampleDocNoSortId(v4()), sampleDocNoSortId(v4()), sampleDocNoSortId(v4())],
+  //         total: {
+  //           relation: 'eq',
+  //           value: 3,
+  //         },
+  //       },
+  //       took: 0,
+  //       timed_out: false,
+  //       _shards: {
+  //         failed: 0,
+  //         skipped: 0,
+  //         successful: 1,
+  //         total: 1,
+  //       },
+  //     })
+  //   );
+  //
+  //   await executor({ params });
+  //   expect(dependencies.ruleDataClient.getWriter).toBeCalled();
+  // });
+});
