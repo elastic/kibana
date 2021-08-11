6c4d699d9ab1d6a3e5297fa5c6e3d333ac2b1d19
Timothy Sullivan <tsullivan@elastic.co>
2021-08-11 12:00:40 -0700

    restore legacy shim
    Restore the legacy shim for PDF job creation from the 7.x branch.

--- x-pack/plugins/reporting/server/export_types/printable_pdf/create_job/index.ts
+++ x-pack/plugins/reporting/server/export_types/printable_pdf/create_job/index.ts
@@ -5,27 +5,41 @@
  * 2.0.
  */
 
+import { KibanaRequest } from 'src/core/server';
 import { cryptoFactory } from '../../../lib';
-import { CreateJobFn, CreateJobFnFactory } from '../../../types';
+import { CreateJobFn, CreateJobFnFactory, ReportingRequestHandlerContext } from '../../../types';
 import { validateUrls } from '../../common';
-import { JobParamsPDF, TaskPayloadPDF } from '../types';
+import { JobParamsPDF, JobParamsPDFLegacy, TaskPayloadPDF } from '../types';
+import { compatibilityShim } from './compatibility_shim';
 
+/*
+ * Incoming job params can be `JobParamsPDF` or `JobParamsPDFLegacy` depending
+ * on the version that the POST URL was copied from.
+ */
 export const createJobFnFactory: CreateJobFnFactory<
-  CreateJobFn<JobParamsPDF, TaskPayloadPDF>
+  CreateJobFn<JobParamsPDF | JobParamsPDFLegacy, TaskPayloadPDF>
 > = function createJobFactoryFn(reporting, logger) {
   const config = reporting.getConfig();
   const crypto = cryptoFactory(config.get('encryptionKey'));
 
-  return async function createJob(jobParams, _context, req) {
+  return compatibilityShim(async function createJobFn(
+    jobParams: JobParamsPDF,
+    _context: ReportingRequestHandlerContext,
+    req: KibanaRequest
+  ) {
     const serializedEncryptedHeaders = await crypto.encrypt(req.headers);
 
     validateUrls(jobParams.relativeUrls);
 
-    return {
+    const payload: TaskPayloadPDF = {
+      ...jobParams,
       headers: serializedEncryptedHeaders,
       spaceId: reporting.getSpaceId(req, logger),
       forceNow: new Date().toISOString(),
-      ...jobParams,
+      objects: jobParams.relativeUrls.map((u) => ({ relativeUrl: u })),
     };
-  };
+
+    return payload;
+  },
+  logger);
 };
