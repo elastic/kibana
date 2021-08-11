93a5703c7b10a378abd049bd2ae0139e93505ad9
Walter Rafelsberger <walter@elastic.co>
2021-08-11 18:29:27 +0200

    [ML] Add license gate to failed transactions.

--- x-pack/plugins/apm/public/components/app/transaction_details/failed_transactions_tab.tsx
+++ x-pack/plugins/apm/public/components/app/transaction_details/failed_transactions_tab.tsx
@@ -9,7 +9,29 @@ import React from 'react';
 
 import { i18n } from '@kbn/i18n';
 
+import { isActivePlatinumLicense } from '../../../../common/license_check';
+
+import { useLicenseContext } from '../../../context/license/use_license_context';
+
+import { LicensePrompt } from '../../shared/license_prompt';
+
 import { ErrorCorrelations } from '../correlations/error_correlations';
+
+import type { TabContentProps } from './types';
+
+function FailedTransactionsTab({}: TabContentProps) {
+  const license = useLicenseContext();
+
+  return isActivePlatinumLicense(license) ? (
+    <ErrorCorrelations onClose={() => {}} />
+  ) : (
+    <LicensePrompt
+      text={i18n.translate('xpack.apm.failedTransactions.licenseCheckText', {
+        defaultMessage: `To use failedTransactions, you must be subscribed to an Elastic Platinum license.`,
+      })}
+    />
+  );
+}
 
 export const failedTransactionsTab = {
   key: 'failedTransactions',
@@ -19,5 +41,5 @@ export const failedTransactionsTab = {
       defaultMessage: 'Failing transactions',
     }
   ),
-  component: () => <ErrorCorrelations onClose={() => {}} />,
+  component: FailedTransactionsTab,
 };
