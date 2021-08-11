7f620659ccebe7d90ed7fda0904857b00d288b9c
Ignacio Rivas <rivasign@gmail.com>
2021-08-11 15:51:20 +0200

    Fix spacing between links

--- x-pack/plugins/upgrade_assistant/public/application/components/overview/observe_step/observe_step.tsx
+++ x-pack/plugins/upgrade_assistant/public/application/components/overview/observe_step/observe_step.tsx
@@ -65,7 +65,7 @@ const DeprecationLogsPreview: FunctionComponent = () => {
 
       {canSeeExternalLinks && (
         <>
-          <EuiSpacer size="l" />
+          <EuiSpacer size="xl" />
           <EuiText>
             <h4>{i18nTexts.analyzeTitle}</h4>
           </EuiText>
