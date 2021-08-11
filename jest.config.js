5e7f1b8dcc3113cc4d8e0924cc5b294fdab6f191
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-08-11 10:43:45 +0300

    Replaced `*` with `(vis_types|chart_expressions)`.

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
     '<rootDir>/packages/*/jest.config.js',
     '<rootDir>/src/*/jest.config.js',
     '<rootDir>/src/plugins/*/jest.config.js',
-    '<rootDir>/src/plugins/*/*/jest.config.js',
+    '<rootDir>/src/plugins/(vis_types|chart_expressions)/*/jest.config.js',
     '<rootDir>/test/*/jest.config.js',
     '<rootDir>/x-pack/plugins/*/jest.config.js',
   ],
