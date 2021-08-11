c795739fbef1abf020c863509ba31ca39b056e78
Stacey Gammon <gammon@elastic.co>
2021-08-11 12:07:51 -0400

    make owner attribute required

--- src/core/server/plugins/plugins_service.test.ts
+++ src/core/server/plugins/plugins_service.test.ts
@@ -101,6 +101,10 @@ const createPlugin = (
       requiredBundles,
       optionalPlugins,
       server,
+      owner: {
+        name: 'Core',
+        githubTeam: 'elastic/kibana-core',
+      },
       ui,
     },
     opaqueId: Symbol(id),
