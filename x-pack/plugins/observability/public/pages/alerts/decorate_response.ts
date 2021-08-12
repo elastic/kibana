30da2ed4e80c5c85cbe1216bdd3f376f70748670
Tiago Costa <tiagoffcc@hotmail.com>
2021-08-12 04:30:09 +0100

    [7.14] chore(NA): moving @kbn/rule-data-utils to babel transpiler (#107573) (#108222)
    * chore(NA): moving @kbn/rule-data-utils to babel transpiler (#107573)
    
    * chore(NA): moving @kbn/rule-data-utils to babel transpiler
    
    * chore(NA): update imports
    
    * chore(NA): targetted imports for apm
    
    * chore(NA): fix imports
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	packages/kbn-rule-data-utils/BUILD.bazel
    #	packages/kbn-rule-data-utils/tsconfig.json
    #	x-pack/plugins/apm/public/components/app/error_group_details/Distribution/index.tsx
    #	x-pack/plugins/apm/public/components/shared/charts/helper/get_alert_annotations.tsx
    #	x-pack/plugins/apm/public/components/shared/charts/latency_chart/index.tsx
    #	x-pack/plugins/observability/public/pages/alerts/alerts_flyout/index.tsx
    #	x-pack/plugins/observability/public/pages/alerts/alerts_table_t_grid.tsx
    #	x-pack/plugins/observability/public/pages/alerts/alerts_table_t_grid_actions.tsx
    #	x-pack/plugins/observability/public/pages/alerts/decorate_response.ts
    #	x-pack/plugins/observability/public/pages/alerts/render_cell_value.tsx
    #	x-pack/plugins/rule_registry/server/alert_data_client/alerts_client.ts
    #	x-pack/plugins/rule_registry/server/routes/get_alert_index.ts
    #	x-pack/plugins/rule_registry/server/rule_data_client/types.ts
    #	x-pack/plugins/rule_registry/server/rule_data_plugin_service/index.ts
    #	x-pack/plugins/rule_registry/server/utils/create_persistence_rule_type_factory.ts
    #	x-pack/plugins/timelines/common/search_strategy/timeline/index.ts
    #	x-pack/plugins/timelines/public/components/t_grid/integrated/index.tsx
    #	x-pack/plugins/timelines/public/components/t_grid/standalone/index.tsx
    #	x-pack/plugins/timelines/public/container/index.tsx
    #	x-pack/plugins/timelines/server/search_strategy/timeline/index.ts
    #	x-pack/plugins/uptime/server/lib/alerts/duration_anomaly.test.ts
    #	x-pack/plugins/uptime/server/lib/alerts/duration_anomaly.ts
    #	x-pack/test/plugin_functional/plugins/timelines_test/public/applications/timelines_test/index.tsx
    
    * chore(NA): remove @kbn/es-query package from deps
    
    * chore(NA): merge and solve conflicts
    
    * chore(NA): fix imports
    
    * chore(NA): fix types
    
    * chore(NA): fix lint

--- x-pack/plugins/observability/public/pages/alerts/decorate_response.ts
+++ x-pack/plugins/observability/public/pages/alerts/decorate_response.ts
@@ -5,16 +5,28 @@
  * 2.0.
  */
 
+import type {
+  RULE_ID as RULE_ID_TYPED,
+  RULE_NAME as RULE_NAME_TYPED,
+  ALERT_STATUS as ALERT_STATUS_TYPED,
+  ALERT_START as ALERT_START_TYPED,
+} from '@kbn/rule-data-utils';
 import {
-  RULE_ID,
-  RULE_NAME,
-  ALERT_STATUS,
-  ALERT_START,
-} from '@kbn/rule-data-utils/target/technical_field_names';
+  RULE_ID as RULE_ID_NON_TYPED,
+  RULE_NAME as RULE_NAME_NON_TYPED,
+  ALERT_STATUS as ALERT_STATUS_NON_TYPED,
+  ALERT_START as ALERT_START_NON_TYPED,
+  // @ts-expect-error
+} from '@kbn/rule-data-utils/target_node/technical_field_names';
 import type { TopAlertResponse, TopAlert } from '.';
 import { parseTechnicalFields } from '../../../../rule_registry/common/parse_technical_fields';
 import { asDuration, asPercent } from '../../../common/utils/formatters';
 import { ObservabilityRuleTypeRegistry } from '../../rules/create_observability_rule_type_registry';
+
+const RULE_ID: typeof RULE_ID_TYPED = RULE_ID_NON_TYPED;
+const RULE_NAME: typeof RULE_NAME_TYPED = RULE_NAME_NON_TYPED;
+const ALERT_STATUS: typeof ALERT_STATUS_TYPED = ALERT_STATUS_NON_TYPED;
+const ALERT_START: typeof ALERT_START_TYPED = ALERT_START_NON_TYPED;
 
 export function decorateResponse(
   alerts: TopAlertResponse[],
