19472d77186dcace912b7ee31577757a6cd11fd2
Ryland Herrick <ryalnd@gmail.com>
2021-08-12 17:20:41 -0500

    Update CTI enrichment UI tests

--- x-pack/plugins/security_solution/cypress/integration/detection_alerts/cti_enrichments.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/detection_alerts/cti_enrichments.spec.ts
@@ -56,13 +56,13 @@ describe('CTI Enrichment', () => {
 
   it('Displays enrichment matched.* fields on the timeline', () => {
     const expectedFields = {
-      'threat.indicator.matched.atomic': getNewThreatIndicatorRule().atomic,
-      'threat.indicator.matched.type': 'indicator_match_rule',
-      'threat.indicator.matched.field': getNewThreatIndicatorRule().indicatorMappingField,
+      'threat.enrichments.matched.atomic': getNewThreatIndicatorRule().atomic,
+      'threat.enrichments.matched.type': 'indicator_match_rule',
+      'threat.enrichments.matched.field': getNewThreatIndicatorRule().indicatorMappingField,
     };
     const fields = Object.keys(expectedFields) as Array<keyof typeof expectedFields>;
 
-    addsFieldsToTimeline('threat.indicator.matched', fields);
+    addsFieldsToTimeline('threat.enrichments.matched', fields);
 
     fields.forEach((field) => {
       cy.get(TIMELINE_FIELD(field)).should('have.text', expectedFields[field]);
@@ -75,7 +75,7 @@ describe('CTI Enrichment', () => {
       {
         line: 3,
         text:
-          '    "indicator": "{\\"first_seen\\":\\"2021-03-10T08:02:14.000Z\\",\\"file\\":{\\"size\\":80280,\\"pe\\":{},\\"type\\":\\"elf\\",\\"hash\\":{\\"sha256\\":\\"a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3\\",\\"tlsh\\":\\"6D7312E017B517CC1371A8353BED205E9128223972AE35302E97528DF957703BAB2DBE\\",\\"ssdeep\\":\\"1536:87vbq1lGAXSEYQjbChaAU2yU23M51DjZgSQAvcYkFtZTjzBht5:8D+CAXFYQChaAUk5ljnQssL\\",\\"md5\\":\\"9b6c3518a91d23ed77504b5416bfb5b3\\"}},\\"type\\":\\"file\\",\\"event\\":{\\"reference\\":\\"https://urlhaus-api.abuse.ch/v1/download/a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3/\\",\\"ingested\\":\\"2021-03-10T14:51:09.809069Z\\",\\"created\\":\\"2021-03-10T14:51:07.663Z\\",\\"kind\\":\\"enrichment\\",\\"module\\":\\"threatintel\\",\\"category\\":\\"threat\\",\\"type\\":\\"indicator\\",\\"dataset\\":\\"threatintel.abusemalware\\"},\\"matched\\":{\\"atomic\\":\\"a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3\\",\\"field\\":\\"myhash.mysha256\\",\\"id\\":\\"84cf452c1e0375c3d4412cb550bd1783358468a3b3b777da4829d72c7d6fb74f\\",\\"index\\":\\"filebeat-7.12.0-2021.03.10-000001\\",\\"type\\":\\"indicator_match_rule\\"}}"',
+          '    "enrichments": "{\\"indicator\\":{\\"first_seen\\":\\"2021-03-10T08:02:14.000Z\\",\\"file\\":{\\"size\\":80280,\\"pe\\":{},\\"type\\":\\"elf\\",\\"hash\\":{\\"sha256\\":\\"a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3\\",\\"tlsh\\":\\"6D7312E017B517CC1371A8353BED205E9128223972AE35302E97528DF957703BAB2DBE\\",\\"ssdeep\\":\\"1536:87vbq1lGAXSEYQjbChaAU2yU23M51DjZgSQAvcYkFtZTjzBht5:8D+CAXFYQChaAUk5ljnQssL\\",\\"md5\\":\\"9b6c3518a91d23ed77504b5416bfb5b3\\"}},\\"type\\":\\"file\\"},\\"matched\\":{\\"atomic\\":\\"a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3\\",\\"field\\":\\"myhash.mysha256\\",\\"id\\":\\"84cf452c1e0375c3d4412cb550bd1783358468a3b3b777da4829d72c7d6fb74f\\",\\"index\\":\\"filebeat-7.12.0-2021.03.10-000001\\",\\"type\\":\\"indicator_match_rule\\"}}"',
       },
       { line: 2, text: '  }' },
     ];
@@ -97,34 +97,23 @@ describe('CTI Enrichment', () => {
 
   it('Displays threat indicator details on the threat intel tab', () => {
     const expectedThreatIndicatorData = [
-      { field: 'event.category', value: 'threat' },
-      { field: 'event.created', value: '2021-03-10T14:51:07.663Z' },
-      { field: 'event.dataset', value: 'threatintel.abusemalware' },
-      { field: 'event.ingested', value: '2021-03-10T14:51:09.809069Z' },
-      { field: 'event.kind', value: 'enrichment' },
-      { field: 'event.module', value: 'threatintel' },
+      { field: 'indicator.file.hash.md5', value: '9b6c3518a91d23ed77504b5416bfb5b3' },
       {
-        field: 'event.reference',
-        value:
-          'https://urlhaus-api.abuse.ch/v1/download/a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3/(opens in a new tab or window)',
-      },
-      { field: 'event.type', value: 'indicator' },
-      { field: 'file.hash.md5', value: '9b6c3518a91d23ed77504b5416bfb5b3' },
-      {
-        field: 'file.hash.sha256',
+        field: 'indicator.file.hash.sha256',
         value: 'a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3',
       },
       {
-        field: 'file.hash.ssdeep',
+        field: 'indicator.file.hash.ssdeep',
         value: '1536:87vbq1lGAXSEYQjbChaAU2yU23M51DjZgSQAvcYkFtZTjzBht5:8D+CAXFYQChaAUk5ljnQssL',
       },
       {
-        field: 'file.hash.tlsh',
+        field: 'indicator.file.hash.tlsh',
         value: '6D7312E017B517CC1371A8353BED205E9128223972AE35302E97528DF957703BAB2DBE',
       },
-      { field: 'file.size', value: '80280' },
-      { field: 'file.type', value: 'elf' },
-      { field: 'first_seen', value: '2021-03-10T08:02:14.000Z' },
+      { field: 'indicator.file.size', value: '80280' },
+      { field: 'indicator.file.type', value: 'elf' },
+      { field: 'indicator.first_seen', value: '2021-03-10T08:02:14.000Z' },
+      { field: 'indicator.type', value: 'file' },
       {
         field: 'matched.atomic',
         value: 'a04ac6d98ad989312783d4fe3456c53730b212c79a426fb215708b6c6daa3de3',
@@ -136,7 +125,6 @@ describe('CTI Enrichment', () => {
       },
       { field: 'matched.index', value: 'filebeat-7.12.0-2021.03.10-000001' },
       { field: 'matched.type', value: 'indicator_match_rule' },
-      { field: 'type', value: 'file' },
     ];
 
     expandFirstAlert();
