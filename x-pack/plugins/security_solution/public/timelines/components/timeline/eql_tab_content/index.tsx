2ab5c2c40a61134d9d9e903ed46ea5a40a8b3fe8
Jonathan Budzenski <jon@budzenski.me>
2021-08-12 08:20:28 -0500

    Revert "[RAC] integrating rbac search strategy with alert flyout (#107748)"
    This reverts commit e9ac0c6674d2979b48565452713bb5cf41d2dd88.

--- x-pack/plugins/security_solution/public/timelines/components/timeline/eql_tab_content/index.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/eql_tab_content/index.tsx
@@ -13,7 +13,6 @@ import {
   EuiFlyoutFooter,
   EuiBadge,
 } from '@elastic/eui';
-import { AlertConsumers } from '@kbn/rule-data-utils/target/alerts_as_data_rbac';
 import { isEmpty } from 'lodash/fp';
 import React, { useEffect, useCallback } from 'react';
 import styled from 'styled-components';
@@ -151,8 +150,6 @@ const EMPTY_EVENTS: TimelineItem[] = [];
 export type Props = OwnProps & PropsFromRedux;
 
 const NO_SORTING: Sort[] = [];
-
-const alertConsumers: AlertConsumers[] = [AlertConsumers.SIEM];
 
 export const EqlTabContentComponent: React.FC<Props> = ({
   activeTab,
@@ -349,7 +346,6 @@ export const EqlTabContentComponent: React.FC<Props> = ({
             <VerticalRule />
             <ScrollableFlexItem grow={1}>
               <DetailsPanel
-                alertConsumers={alertConsumers}
                 browserFields={browserFields}
                 docValueFields={docValueFields}
                 tabType={TimelineTabs.eql}
