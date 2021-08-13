8068994b0d69ddc7257b655e23ad31b375e21aff
Anton Dosov <anton.dosov@elastic.co>
2021-08-13 14:33:25 +0200

    wip

--- src/plugins/discover/public/__fixtures__/stubbed_logstash_index_pattern.js
+++ src/plugins/discover/public/__fixtures__/stubbed_logstash_index_pattern.js
@@ -8,6 +8,9 @@
 
 import stubbedLogstashFields from './logstash_fields';
 import { getKbnFieldType } from '../../../data/common';
+
+// eslint-disable-next-line @kbn/eslint/no-restricted-paths
+import { getStubIndexPattern } from '../../../data/public/test_utils';
 import { uiSettingsServiceMock } from '../../../../core/public/mocks';
 
 const uiSettingSetupMock = uiSettingsServiceMock.createSetupContract();
@@ -16,30 +19,29 @@ uiSettingSetupMock.get.mockImplementation((item, defaultValue) => {
 });
 
 export default function stubbedLogstashIndexPatternService() {
-  // const mockLogstashFields = stubbedLogstashFields();
+  const mockLogstashFields = stubbedLogstashFields();
 
-  // const fields = mockLogstashFields.map(function (field) {
-  //   const kbnType = getKbnFieldType(field.type);
-  //
-  //   if (!kbnType || kbnType.name === 'unknown') {
-  //     throw new TypeError(`unknown type ${field.type}`);
-  //   }
-  //
-  //   return {
-  //     ...field,
-  //     sortable: 'sortable' in field ? !!field.sortable : kbnType.sortable,
-  //     filterable: 'filterable' in field ? !!field.filterable : kbnType.filterable,
-  //     displayName: field.name,
-  //   };
-  // });
+  const fields = mockLogstashFields.map(function (field) {
+    const kbnType = getKbnFieldType(field.type);
 
-  // const indexPattern = getStubIndexPattern('logstash-*', (cfg) => cfg, 'time', fields, {
-  //   uiSettings: uiSettingSetupMock,
-  // });
-  //
-  // indexPattern.id = 'logstash-*';
-  // indexPattern.isTimeNanosBased = () => false;
-  //
-  // return indexPattern;
-  return null;
+    if (!kbnType || kbnType.name === 'unknown') {
+      throw new TypeError(`unknown type ${field.type}`);
+    }
+
+    return {
+      ...field,
+      sortable: 'sortable' in field ? !!field.sortable : kbnType.sortable,
+      filterable: 'filterable' in field ? !!field.filterable : kbnType.filterable,
+      displayName: field.name,
+    };
+  });
+
+  const indexPattern = getStubIndexPattern('logstash-*', (cfg) => cfg, 'time', fields, {
+    uiSettings: uiSettingSetupMock,
+  });
+
+  indexPattern.id = 'logstash-*';
+  indexPattern.isTimeNanosBased = () => false;
+
+  return indexPattern;
 }
