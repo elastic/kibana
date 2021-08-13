a4429298ebc944bb08eee4ee2bf2281aeb78823c
Devin Hurley <devin.hurley@elastic.co>
2021-08-13 14:40:22 -0400

    fix describe in the integration test

--- x-pack/test/detection_engine_api_integration/basic/tests/query_signals.ts
+++ x-pack/test/detection_engine_api_integration/basic/tests/query_signals.ts
@@ -18,7 +18,7 @@ import { getSignalStatus, createSignalsIndex, deleteSignalsIndex } from '../../u
 export default ({ getService }: FtrProviderContext) => {
   const supertest = getService('supertest');
 
-  describe('query_signals_route', () => {
+  describe('query_signals_route and find_alerts_route', () => {
     describe('validation checks', () => {
       it('should not give errors when querying and the signals index does not exist yet', async () => {
         const { body } = await supertest
@@ -60,47 +60,48 @@ export default ({ getService }: FtrProviderContext) => {
         await deleteSignalsIndex(supertest);
       });
     });
-  });
-  describe('find_alerts_route', () => {
-    describe('validation checks', () => {
-      it('should not give errors when querying and the signals index does not exist yet', async () => {
-        const { body } = await supertest
-          .post(ALERTS_AS_DATA_FIND_URL)
-          .set('kbn-xsrf', 'true')
-          .send({ ...getSignalStatus(), index: '.siem-signals-default' })
-          .expect(200);
 
-        // remove any server generated items that are indeterministic
-        delete body.took;
+    describe('find_alerts_route', () => {
+      describe('validation checks', () => {
+        it('should not give errors when querying and the signals index does not exist yet', async () => {
+          const { body } = await supertest
+            .post(ALERTS_AS_DATA_FIND_URL)
+            .set('kbn-xsrf', 'true')
+            .send({ ...getSignalStatus(), index: '.siem-signals-default' })
+            .expect(200);
 
-        expect(body).to.eql({
-          timed_out: false,
-          _shards: { total: 0, successful: 0, skipped: 0, failed: 0 },
-          hits: { total: { value: 0, relation: 'eq' }, max_score: 0, hits: [] },
-        });
-      });
+          // remove any server generated items that are indeterministic
+          delete body.took;
 
-      it('should not give errors when querying and the signals index does exist and is empty', async () => {
-        await createSignalsIndex(supertest);
-        const { body } = await supertest
-          .post(ALERTS_AS_DATA_FIND_URL)
-          .set('kbn-xsrf', 'true')
-          .send({ ...getSignalStatus(), index: '.siem-signals-default' })
-          .expect(200);
-
-        // remove any server generated items that are indeterministic
-        delete body.took;
-
-        expect(body).to.eql({
-          timed_out: false,
-          _shards: { total: 1, successful: 1, skipped: 0, failed: 0 },
-          hits: { total: { value: 0, relation: 'eq' }, max_score: null, hits: [] },
-          aggregations: {
-            statuses: { doc_count_error_upper_bound: 0, sum_other_doc_count: 0, buckets: [] },
-          },
+          expect(body).to.eql({
+            timed_out: false,
+            _shards: { total: 0, successful: 0, skipped: 0, failed: 0 },
+            hits: { total: { value: 0, relation: 'eq' }, max_score: 0, hits: [] },
+          });
         });
 
-        await deleteSignalsIndex(supertest);
+        it('should not give errors when querying and the signals index does exist and is empty', async () => {
+          await createSignalsIndex(supertest);
+          const { body } = await supertest
+            .post(ALERTS_AS_DATA_FIND_URL)
+            .set('kbn-xsrf', 'true')
+            .send({ ...getSignalStatus(), index: '.siem-signals-default' })
+            .expect(200);
+
+          // remove any server generated items that are indeterministic
+          delete body.took;
+
+          expect(body).to.eql({
+            timed_out: false,
+            _shards: { total: 1, successful: 1, skipped: 0, failed: 0 },
+            hits: { total: { value: 0, relation: 'eq' }, max_score: null, hits: [] },
+            aggregations: {
+              statuses: { doc_count_error_upper_bound: 0, sum_other_doc_count: 0, buckets: [] },
+            },
+          });
+
+          await deleteSignalsIndex(supertest);
+        });
       });
     });
   });
