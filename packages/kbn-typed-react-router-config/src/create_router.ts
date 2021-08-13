de45de51ef0dd2068160184ff07c25779c4397fb
Tiago Costa <tiagoffcc@hotmail.com>
2021-08-13 13:20:09 +0100

    chore(NA): moving @kbn/io-ts-utils to babel transpiler

--- packages/kbn-typed-react-router-config/src/create_router.ts
+++ packages/kbn-typed-react-router-config/src/create_router.ts
@@ -14,9 +14,15 @@ import {
 } from 'react-router-config';
 import qs from 'query-string';
 import { findLastIndex, merge, compact } from 'lodash';
-import { deepExactRt } from '@kbn/io-ts-utils/target/deep_exact_rt';
-import { mergeRt } from '@kbn/io-ts-utils/target/merge_rt';
+import type { deepExactRt as deepExactRtTyped, mergeRt as mergeRtTyped } from '@kbn/io-ts-utils';
+// @ts-expect-error
+import { deepExactRt as deepExactRtNonTyped } from '@kbn/io-ts-utils/target_node/deep_exact_rt';
+// @ts-expect-error
+import { mergeRt as mergeRtNonTyped } from '@kbn/io-ts-utils/target_node/merge_rt';
 import { Route, Router } from './types';
+
+const deepExactRt: typeof deepExactRtTyped = deepExactRtNonTyped;
+const mergeRt: typeof mergeRtTyped = mergeRtNonTyped;
 
 export function createRouter<TRoutes extends Route[]>(routes: TRoutes): Router<TRoutes> {
   const routesByReactRouterConfig = new Map<ReactRouterConfig, Route>();
