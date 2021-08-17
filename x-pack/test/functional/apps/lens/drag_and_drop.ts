87a785b9278c888c84a72108ba56673ada72e90b
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2021-08-17 14:19:53 +0300

    Unskip test

--- x-pack/test/functional/apps/lens/drag_and_drop.ts
+++ x-pack/test/functional/apps/lens/drag_and_drop.ts
@@ -13,8 +13,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const retry = getService('retry');
 
   describe('lens drag and drop tests', () => {
-    // FLAKY: https://github.com/elastic/kibana/issues/108352
-    describe.skip('basic drag and drop', () => {
+    describe('basic drag and drop', () => {
       it('should construct the basic split xy chart', async () => {
         await PageObjects.visualize.navigateToNewVisualization();
         await PageObjects.visualize.clickVisType('lens');
