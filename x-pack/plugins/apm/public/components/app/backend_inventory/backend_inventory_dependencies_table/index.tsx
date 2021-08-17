d95a5448e0ca4660ab0b09c8755d71c9c2c83a48
Nathan L Smith <smith@nlsmith.com>
2021-08-17 09:13:19 -0500

    Remove unused import

--- x-pack/plugins/apm/public/components/app/backend_inventory/backend_inventory_dependencies_table/index.tsx
+++ x-pack/plugins/apm/public/components/app/backend_inventory/backend_inventory_dependencies_table/index.tsx
@@ -5,19 +5,18 @@
  * 2.0.
  */
 
+import { METRIC_TYPE } from '@kbn/analytics';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
-import { METRIC_TYPE } from '@kbn/analytics';
-import { useApmRouter } from '../../../../hooks/use_apm_router';
-import { getNodeName, NodeType } from '../../../../../common/connections';
-import { useApmParams } from '../../../../hooks/use_apm_params';
-import { useUrlParams } from '../../../../context/url_params_context/use_url_params';
-import { useFetcher } from '../../../../hooks/use_fetcher';
-import { getTimeRangeComparison } from '../../../shared/time_comparison/get_time_range_comparison';
-import { DependenciesTable } from '../../../shared/dependencies_table';
-import { BackendLink } from '../../../shared/backend_link';
-import { DependenciesTableServiceMapLink } from '../../../shared/dependencies_table/dependencies_table_service_map_link';
 import { useUiTracker } from '../../../../../../observability/public';
+import { getNodeName, NodeType } from '../../../../../common/connections';
+import { useUrlParams } from '../../../../context/url_params_context/use_url_params';
+import { useApmParams } from '../../../../hooks/use_apm_params';
+import { useApmRouter } from '../../../../hooks/use_apm_router';
+import { useFetcher } from '../../../../hooks/use_fetcher';
+import { BackendLink } from '../../../shared/backend_link';
+import { DependenciesTable } from '../../../shared/dependencies_table';
+import { getTimeRangeComparison } from '../../../shared/time_comparison/get_time_range_comparison';
 
 export function BackendInventoryDependenciesTable() {
   const {
@@ -31,15 +30,6 @@ export function BackendInventoryDependenciesTable() {
   const router = useApmRouter();
 
   const trackEvent = useUiTracker();
-
-  const serviceMapLink = router.link('/service-map', {
-    query: {
-      rangeFrom,
-      rangeTo,
-      environment,
-      kuery,
-    },
-  });
 
   const { offset } = getTimeRangeComparison({
     start,
