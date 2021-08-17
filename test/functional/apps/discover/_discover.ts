d54c94c4a935a2d9604e94df315d084f3c406ddd
Tre' Seymour <wayne.seymour@elastic.co>
2021-08-17 13:24:09 +0100

    Add my name to the discover test title, & see if I can use console.log to see the types in ci, even if having the log fn fails the build.
    Signed-off-by: Tre' Seymour <wayne.seymour@elastic.co>

--- test/functional/apps/discover/_discover.ts
+++ test/functional/apps/discover/_discover.ts
@@ -26,13 +26,13 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     defaultIndex: 'logstash-*',
   };
 
-  describe('discover test', function describeIndexTests() {
+  describe('tre so info discover test', function describeIndexTests() {
     before(async function () {
       log.debug('load kibana index with default index pattern');
 
       await kibanaServer.importExport.load('test/functional/fixtures/kbn_archiver/discover.json');
 
-      console.log(`\n### soInfo.getTypes(): \n${await soInfo.getTypes()}`);
+      console.log(`\n### so info types: \n${await soInfo.getTypes()}`);
       // and load a set of makelogs data
       await esArchiver.loadIfNeeded('test/functional/fixtures/es_archiver/logstash_functional');
       await kibanaServer.uiSettings.replace(defaultSettings);
