19308576f333143bb4c7a43722d11e4ff66ff894
Jonathan Budzenski <jon@budzenski.me>
2021-08-17 18:09:08 -0500

    flaky test - fix issue reference

--- x-pack/test/fleet_functional/apps/home/welcome.ts
+++ x-pack/test/fleet_functional/apps/home/welcome.ts
@@ -14,7 +14,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const PageObjects = getPageObjects(['common', 'home']);
   const kibanaServer = getService('kibanaServer');
 
-  // flaky https://github.com/elastic/kibana/pull/108193
+  // flaky https://github.com/elastic/kibana/issues/109017
   describe.skip('Welcome interstitial', () => {
     before(async () => {
       // Need to navigate to page first to clear storage before test can be run
