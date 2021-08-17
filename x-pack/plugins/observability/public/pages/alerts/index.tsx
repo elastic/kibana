9108e1c7b3fca36cc3cff6937fe63e5ad1ce8e6f
mgiota <panagiota.mitsopoulou@elastic.co>
2021-08-17 19:15:54 +0200

    fix type errors

--- x-pack/plugins/observability/public/pages/alerts/index.tsx
+++ x-pack/plugins/observability/public/pages/alerts/index.tsx
@@ -14,7 +14,7 @@ import {
   EuiSpacer,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import React, { useCallback, useMemo, useRef, useEffect } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { ParsedTechnicalFields } from '../../../../rule_registry/common/parse_technical_fields';
 import type { AlertStatus } from '../../../common/typings';
@@ -67,7 +67,7 @@ export function AlertsPage({ routeParams }: AlertsPageProps) {
   // observability. For now link to the settings page.
   const manageRulesHref = prepend('/app/management/insightsAndAlerting/triggersActions/alerts');
 
-  const { data: dynamicIndexPatternResp, status: fetcherStatus } = useFetcher(({ signal }) => {
+  const { data: dynamicIndexPatternResp } = useFetcher(({ signal }) => {
     return callObservabilityApi({
       signal,
       endpoint: 'GET /api/observability/rules/alerts/dynamic_index_pattern',
