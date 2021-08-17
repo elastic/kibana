49fa8c5ea09089622798101e69959fc05823c895
nathan <reese.nathan@gmail.com>
2021-08-17 14:55:31 -0600

    cleanup functional test

--- x-pack/test/functional/apps/maps/embeddable/tooltip_filter_actions.js
+++ x-pack/test/functional/apps/maps/embeddable/tooltip_filter_actions.js
@@ -55,9 +55,8 @@ export default function ({ getPageObjects, getService }) {
         await testSubjects.click('mapTooltipCreateFilterButton');
         await testSubjects.click('applyFiltersPopoverButton');
 
-        // TODO: Fix me #64861
-        // const hasSourceFilter = await filterBar.hasFilter('name', 'charlie');
-        // expect(hasSourceFilter).to.be(true);
+        const numFilters = await filterBar.getFilterCount();
+        expect(numFilters).to.be(1);
 
         const hasJoinFilter = await filterBar.hasFilter('runtime_shape_name', 'charlie');
         expect(hasJoinFilter).to.be(true);
