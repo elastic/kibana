48c55c38ce5a8958496dbff7558e3cc33bdd11ba
Marco Vettorello <vettorello.marco@gmail.com>
2021-08-17 10:57:51 +0200

    fix functional test heatmap legend symbol

--- x-pack/test/functional/apps/lens/heatmap.ts
+++ x-pack/test/functional/apps/lens/heatmap.ts
@@ -60,11 +60,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
       // assert legend
       expect(debugState.legend!.items).to.eql([
-        { key: '5722.77', name: '> 5,722.77', color: '#6092c0' },
-        { key: '8529.22', name: '> 8,529.22', color: '#a8bfda' },
-        { key: '11335.66', name: '> 11,335.66', color: '#ebeff5' },
-        { key: '14142.11', name: '> 14,142.11', color: '#ecb385' },
-        { key: '16948.55', name: '> 16,948.55', color: '#e7664c' },
+        { key: '≥ 5722.77', name: '≥ 5,722.77', color: '#6092c0' },
+        { key: '≥ 8529.22', name: '≥ 8,529.22', color: '#a8bfda' },
+        { key: '≥ 11335.66', name: '≥ 11,335.66', color: '#ebeff5' },
+        { key: '≥ 14142.11', name: '≥ 14,142.11', color: '#ecb385' },
+        { key: '≥ 16948.55', name: '≥ 16,948.55', color: '#e7664c' },
       ]);
     });
 
