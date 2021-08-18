13574cbd04723537fae286a060678e24dac39a52
Joe Portner <5295965+jportner@users.noreply.github.com>
2021-08-18 15:08:52 -0400

    Remove spaces dependency on savedObjectsManagement

--- x-pack/plugins/spaces/public/copy_saved_objects_to_space/lib/summarize_copy_result.test.ts
+++ x-pack/plugins/spaces/public/copy_saved_objects_to_space/lib/summarize_copy_result.test.ts
@@ -5,12 +5,7 @@
  * 2.0.
  */
 
-import type {
-  FailedImport,
-  ProcessedImportResponse,
-  SavedObjectsManagementRecord,
-} from 'src/plugins/saved_objects_management/public';
-
+import type { FailedImport, ProcessedImportResponse } from '../lib';
 import type { CopyToSpaceSavedObjectTarget } from '../types';
 import { summarizeCopyResult } from './summarize_copy_result';
 
@@ -43,7 +38,7 @@ const OBJECTS = {
       { type: 'visualization', id: 'foo', name: 'Visualization foo' },
       { type: 'visualization', id: 'bar', name: 'Visualization bar' },
     ],
-  } as SavedObjectsManagementRecord,
+  },
   VISUALIZATION_FOO: {
     type: 'visualization',
     id: 'bar',
@@ -54,7 +49,7 @@ const OBJECTS = {
       hiddenType: false,
     },
     references: [{ type: 'index-pattern', id: 'foo', name: 'Index pattern foo' }],
-  } as SavedObjectsManagementRecord,
+  },
   VISUALIZATION_BAR: {
     type: 'visualization',
     id: 'baz',
@@ -65,7 +60,7 @@ const OBJECTS = {
       hiddenType: false,
     },
     references: [{ type: 'index-pattern', id: 'bar', name: 'Index pattern bar' }],
-  } as SavedObjectsManagementRecord,
+  },
   INDEX_PATTERN_FOO: {
     type: 'index-pattern',
     id: 'foo',
@@ -76,7 +71,7 @@ const OBJECTS = {
       hiddenType: false,
     },
     references: [],
-  } as SavedObjectsManagementRecord,
+  },
   INDEX_PATTERN_BAR: {
     type: 'index-pattern',
     id: 'bar',
@@ -87,7 +82,7 @@ const OBJECTS = {
       hiddenType: false,
     },
     references: [],
-  } as SavedObjectsManagementRecord,
+  },
 };
 
 interface ObjectProperties {
