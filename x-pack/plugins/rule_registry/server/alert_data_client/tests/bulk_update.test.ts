080010dbfffc0f800b78b7da45b6138f98e174c1
Marshall Main <marshall.main@elastic.co>
2021-08-17 22:05:10 -0700

    Update unit tests

--- x-pack/plugins/rule_registry/server/alert_data_client/tests/bulk_update.test.ts
+++ x-pack/plugins/rule_registry/server/alert_data_client/tests/bulk_update.test.ts
@@ -352,8 +352,8 @@ describe('bulkUpdate()', () => {
             status: 'closed',
           })
         ).rejects.toThrowErrorMatchingInlineSnapshot(`
-                "queryAndAuditAllAlerts threw an error: Unable to retrieve alerts with query \\"kibana.alert.status: open\\" and operation update 
-                 Error: Unable to retrieve alert details for alert with id of \\"null\\" or with query \\"kibana.alert.status: open\\" and operation update 
+                "queryAndAuditAllAlerts threw an error: Unable to retrieve alerts with query \\"kibana.alert.status: active\\" and operation update 
+                 Error: Unable to retrieve alert details for alert with id of \\"null\\" or with query \\"kibana.alert.status: active\\" and operation update 
                 Error: Error: Unauthorized for fake.rule and apm"
               `);
 
@@ -423,8 +423,8 @@ describe('bulkUpdate()', () => {
             status: 'closed',
           })
         ).rejects.toThrowErrorMatchingInlineSnapshot(`
-                "queryAndAuditAllAlerts threw an error: Unable to retrieve alerts with query \\"kibana.alert.status: open\\" and operation update 
-                 Error: Unable to retrieve alert details for alert with id of \\"null\\" or with query \\"kibana.alert.status: open\\" and operation update 
+                "queryAndAuditAllAlerts threw an error: Unable to retrieve alerts with query \\"kibana.alert.status: active\\" and operation update 
+                 Error: Unable to retrieve alert details for alert with id of \\"null\\" or with query \\"kibana.alert.status: active\\" and operation update 
                 Error: Error: Unauthorized for fake.rule and apm"
               `);
 
