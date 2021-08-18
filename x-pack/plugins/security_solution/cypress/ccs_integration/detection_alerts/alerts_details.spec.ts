5f365c41a3ca40e1917d1e623b4ddd6ff752264f
Domenico Andreoli <domenico.andreoli@elastic.co>
2021-08-18 09:46:45 +0200

    [7.x] Realign cypress/ccs_integration with cypress/integration
    cypress/ccs_integration is a subset/fork of cypress/integration and
    already tends to be left behind.
    
    Situation is expected to improve once proper signaling of failing tests is
    reported to #security-solution-slack-testing.
    
    For the moment, let's keep aligning manually.

--- x-pack/plugins/security_solution/cypress/ccs_integration/detection_alerts/alerts_details.spec.ts
+++ x-pack/plugins/security_solution/cypress/ccs_integration/detection_alerts/alerts_details.spec.ts
@@ -58,7 +58,7 @@ describe('Alert details with unmapped fields', () => {
 
   it('Displays the unmapped field on the table', () => {
     const expectedUnmmappedField = {
-      row: 89,
+      row: 91,
       field: 'unmapped',
       text: 'This is the unmapped field',
     };
