e60ab01f0f1ef89796c1a841ad15e6610ef0e5f9
Liza Katz <lizka.k@gmail.com>
2021-08-19 14:27:51 +0200

    Update types.ts

--- x-pack/plugins/lens/common/types.ts
+++ x-pack/plugins/lens/common/types.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { Filter, FilterMeta } from '@kbn/es-query';
+import type { Filter, FilterMeta } from '@kbn/es-query';
 import type { IFieldFormat } from '../../../../src/plugins/field_formats/common';
 import type { Datatable, SerializedFieldFormat } from '../../../../src/plugins/expressions/common';
 
