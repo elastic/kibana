53a94397765a22ae038e6ab4ea72adde59d79bd3
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2021-08-19 15:44:23 -0400

    snapshot updates

--- x-pack/plugins/security_solution/common/utils/field_formatters.test.ts
+++ x-pack/plugins/security_solution/common/utils/field_formatters.test.ts
@@ -162,14 +162,14 @@ describe('Events Details Helpers', () => {
         isObjectArray: false,
       },
       {
-        category: 'signal',
+        category: 'kibana',
         field: ALERT_WORKFLOW_STATUS,
         values: ['open'],
         originalValue: ['open'],
         isObjectArray: false,
       },
       {
-        category: 'signal',
+        category: 'kibana',
         field: ALERT_RULE_NAME,
         values: ['Rawr'],
         originalValue: ['Rawr'],
