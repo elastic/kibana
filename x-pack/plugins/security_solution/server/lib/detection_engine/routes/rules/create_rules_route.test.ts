2556a5a5a55306316ec3bb6f3bb7b954126970de
Madison Caldwell <madison.caldwell@elastic.co>
2021-08-19 15:54:44 -0400

    Remove console log

--- x-pack/plugins/security_solution/server/lib/detection_engine/routes/rules/create_rules_route.test.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/routes/rules/create_rules_route.test.ts
@@ -58,7 +58,6 @@ describe('create_rules', () => {
         clients.rulesClient.create.mockResolvedValue(
           getAlertMock(getQueryRuleParams(ruleDataClient != null))
         ); // creation succeeds
-        console.log('namespace' in getAlertMock(getQueryRuleParams(ruleDataClient != null)).params);
         createRulesRoute(server.router, ml, ruleDataClient);
         (updateRulesNotifications as jest.Mock).mockResolvedValue({
           id: 'id',
