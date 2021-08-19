f58ff4cb0c1180adf3466bb3a3291dda117d5b17
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-08-19 13:11:33 -0400

    [APM] Refactor Cypress e2e tests (#109024) (#109256)
    * Improve script to setup users and roles
    
    * fix readme
    
    * CI fixes
    
    * add index permissions to roles
    
    * disable welcome screen
    
    * Run es archive once before tests
    
    * Fix ts issues
    
    * Update x-pack/plugins/apm/readme.md
    
    Co-authored-by: Nathan L Smith <nathan.smith@elastic.co>
    
    Co-authored-by: Nathan L Smith <nathan.smith@elastic.co>
    
    Co-authored-by: Søren Louv-Jansen <sorenlouv@gmail.com>
    Co-authored-by: Nathan L Smith <nathan.smith@elastic.co>

--- x-pack/plugins/apm/scripts/create-apm-users-and-roles/roles/read_only_user_role.ts
+++ x-pack/plugins/apm/scripts/create-apm-users-and-roles/roles/read_only_user_role.ts
@@ -5,10 +5,41 @@
  * 2.0.
  */
 
-import { RoleType } from './create_role';
+import { RoleType } from '../helpers/create_role';
 
 export const readOnlyUserRole: RoleType = {
-  elasticsearch: { cluster: [], indices: [], run_as: [] },
+  elasticsearch: {
+    run_as: [],
+    cluster: [],
+    indices: [
+      // apm
+      {
+        names: [
+          'apm-*',
+          'logs-apm*',
+          'metrics-apm*',
+          'traces-apm*',
+          'observability-annotations',
+        ],
+        privileges: ['read', 'view_index_metadata'],
+      },
+      // logs
+      {
+        names: ['logs-*', 'filebeat-*', 'kibana_sample_data_logs*'],
+        privileges: ['read', 'view_index_metadata'],
+      },
+      // metrics
+      {
+        names: ['metrics-*', 'metricbeat-*'],
+        privileges: ['read', 'view_index_metadata'],
+      },
+      // uptime
+      {
+        names: ['heartbeat-*', 'synthetics-*'],
+        privileges: ['read', 'view_index_metadata'],
+      },
+    ],
+  },
   kibana: [
     {
       base: [],
