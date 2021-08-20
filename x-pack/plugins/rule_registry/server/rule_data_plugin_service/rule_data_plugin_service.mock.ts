f9c3928b98c35cdc60c2f569848d5951cf10dde9
Xavier Mouligneau <189600+XavierM@users.noreply.github.com>
2021-08-19 21:30:49 -0400

    fix types

--- x-pack/plugins/rule_registry/server/rule_data_plugin_service/rule_data_plugin_service.mock.ts
+++ x-pack/plugins/rule_registry/server/rule_data_plugin_service/rule_data_plugin_service.mock.ts
@@ -12,6 +12,7 @@ type Schema = PublicMethodsOf<RuleDataPluginService>;
 
 const createRuleDataPluginService = () => {
   const mocked: jest.Mocked<Schema> = {
+    getBaseNameByRegistrationContext: jest.fn(),
     getResourcePrefix: jest.fn(),
     getResourceName: jest.fn(),
     isWriteEnabled: jest.fn(),
