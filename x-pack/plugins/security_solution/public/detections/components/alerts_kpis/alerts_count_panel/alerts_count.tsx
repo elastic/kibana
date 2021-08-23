e002340ecbf51173a0242c3abb66442962445ece
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-08-23 10:14:45 -0400

    Small improvements around Reason field and AlertCountTable draggable fields (#109239) (#109629)
    * Make count table field and alert table event renderer undraggable
    
    * Remove tooltip from alert count table
    
    * Remove DefaultDraggable wrapper from reason field
    
    * Fix unit test and remove unused props
    
    I am not 100% sure, but apparently, the test was broken due to a long import chain loop.
    I cut the import chain by extracting some constants to a file.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Pablo Machado <pablo.nevesmachado@elastic.co>

--- x-pack/plugins/security_solution/public/detections/components/alerts_kpis/alerts_count_panel/alerts_count.tsx
+++ x-pack/plugins/security_solution/public/detections/components/alerts_kpis/alerts_count_panel/alerts_count.tsx
@@ -40,9 +40,11 @@ const getAlertsCountTableColumns = (
       render: function DraggableStackOptionField(value: string) {
         return (
           <DefaultDraggable
+            isDraggable={false}
             field={selectedStackByOption}
             id={`alert-count-draggable-${selectedStackByOption}-${value}`}
             value={value}
+            tooltipContent={null}
           />
         );
       },
