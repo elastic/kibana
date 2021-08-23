4371cad89a7b7668fcbfb4a8c2cf72623d75dbc6
Andrew Goldstein <andrew-goldstein@users.noreply.github.com>
2021-08-23 08:09:00 -0600

    [RAC] [Security Solution] Hides the `Show top <field>` action in chart legends (#109566)
    ## Summary
    
    Fixes <https://github.com/elastic/kibana/issues/108910> , which allowed users to launch a new `Top <field>` popover from an existing popover, to infinity (and beyond)
    
    The `Show top <field>` action is now hidden in the `Top <field>` popover's chart legend, and also:
    
    - In the legend items of charts throughout the Security Solution (e.g. on the `Overview` page, and in the `Trend` chart on the `Alerts` page)
    - For items in the `Count` aggregation table on the `Alerts` page
    
    ## Screenshots
    
    ### Before (Top `signal.rule.name` popover)
    
    ![before-top-signal-rule-name](https://user-images.githubusercontent.com/4459398/130302784-00a6c24d-17c8-4361-979e-01b8467f100e.png)
    
    _Above: It was possible to launch another `Top <field>` popover from the legend of an existing popover_
    
    ### After (Top `signal.rule.name` popover)
    
    ![after-top-signal-rule-name](https://user-images.githubusercontent.com/4459398/130302925-d5aaa1ff-9565-4374-aa87-bde5880cb64f.png)
    
    _Above: It is no longer possible to launch another `Top <field>` popover from the legend of an existing popover_
    
    ### Before (Chart legends)
    
    ![before-overview](https://user-images.githubusercontent.com/4459398/130303169-dc6c6de3-a2ba-40fe-a1f0-fe0d78b9638c.png)
    
    _Above: It was possible to launch a `Top <field>` popover from chart legends_
    
    ### After (Chart legends)
    
    ![after-overview](https://user-images.githubusercontent.com/4459398/130303519-2eb0a60e-c6cd-4659-b6b2-d5ba234f668f.png)
    
    _Above: It is no longer possible to launch a `Top <field>` popover from chart legends_
    
    ### Before (`Count` items)
    
    ![before-count](https://user-images.githubusercontent.com/4459398/130304111-b37373cf-1afb-41b8-9f38-b5d9b37cdb2d.png)
    
    _Above: It was possible to launch a `Top <field>` popover from `Count` items_
    
    ### After (`Count` items)
    
    ![after-count](https://user-images.githubusercontent.com/4459398/130304166-fb641fa2-b52e-44ff-8210-0e228a43330c.png)
    
    _Above: It is no longer possible to launch a `Top <field>` popover from `Count` items_
    
    cc @mdefazio

--- x-pack/plugins/security_solution/public/detections/components/alerts_kpis/alerts_count_panel/alerts_count.tsx
+++ x-pack/plugins/security_solution/public/detections/components/alerts_kpis/alerts_count_panel/alerts_count.tsx
@@ -41,6 +41,7 @@ const getAlertsCountTableColumns = (
         return (
           <DefaultDraggable
             field={selectedStackByOption}
+            hideTopN={true}
             id={`alert-count-draggable-${selectedStackByOption}-${value}`}
             value={value}
           />
