5da446630a16ce2b9ef337de25f0286883c98ec5
Patryk Kopycinski <contact@patrykkopycinski.com>
2021-08-24 14:28:04 +0200

    update mocks

--- x-pack/plugins/cases/public/common/lib/kibana/kibana_react.mock.ts
+++ x-pack/plugins/cases/public/common/lib/kibana/kibana_react.mock.ts
@@ -14,6 +14,7 @@ import { StartServices } from '../../../types';
 import { EuiTheme } from '../../../../../../../src/plugins/kibana_react/common';
 import { securityMock } from '../../../../../security/public/mocks';
 import { triggersActionsUiMock } from '../../../../../triggers_actions_ui/public/mocks';
+import { lensPluginMock } from '../../../../../lens/public/mocks';
 
 export const mockCreateStartServicesMock = (): StartServices =>
   (({
@@ -25,6 +26,7 @@ export const mockCreateStartServicesMock = (): StartServices =>
 export const createStartServicesMock = (): StartServices =>
   (({
     ...coreMock.createStart(),
+    lens: lensPluginMock.createStartContract(),
     security: securityMock.createStart(),
     triggersActionsUi: triggersActionsUiMock.createStart(),
   } as unknown) as StartServices);
