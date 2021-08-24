e55ba57e5c0c31b5621d6223d0449835aef85c5d
Timothy Sullivan <tsullivan@elastic.co>
2021-08-24 16:33:09 -0700

    add fixes

--- x-pack/plugins/reporting/server/export_types/csv_searchsource/generate_csv/generate_csv.ts
+++ x-pack/plugins/reporting/server/export_types/csv_searchsource/generate_csv/generate_csv.ts
@@ -66,7 +66,6 @@ function isPlainStringArray(
 export class CsvGenerator {
   private _columns?: string[];
   private _formatters?: Record<string, FieldFormat>;
-  private includeFrozen: boolean;
   private csvContainsFormulas = false;
   private maxSizeReached = false;
   private csvRowCount = 0;
@@ -84,8 +83,9 @@ export class CsvGenerator {
   private async scan(
     index: IndexPattern,
     searchSource: ISearchSource,
-    scrollSettings: CsvExportSettings['scroll']
+    settings: CsvExportSettings
   ) {
+    const { scroll: scrollSettings, includeFrozen } = settings;
     const searchBody = searchSource.getSearchRequestBody();
     this.logger.debug(`executing search request`);
     const searchParams = {
@@ -94,9 +94,10 @@ export class CsvGenerator {
         index: index.title,
         scroll: scrollSettings.duration,
         size: scrollSettings.size,
-        ignore_throttled: this.includeFrozen,
+        ignore_throttled: !includeFrozen,
       },
     };
+
     const results = (
       await this.clients.data.search(searchParams, { strategy: ES_SEARCH_STRATEGY }).toPromise()
     ).rawResponse as estypes.SearchResponse<unknown>;
@@ -328,7 +329,7 @@ export class CsvGenerator {
         let results: estypes.SearchResponse<unknown> | undefined;
         if (scrollId == null) {
           // open a scroll cursor in Elasticsearch
-          results = await this.scan(index, searchSource, scrollSettings);
+          results = await this.scan(index, searchSource, settings);
           scrollId = results?._scroll_id;
           if (results.hits?.total != null) {
             totalRecords = results.hits.total as number;
--- x-pack/test/reporting_api_integration/reporting_and_security/search_frozen_indices.ts
+++ x-pack/test/reporting_api_integration/reporting_and_security/search_frozen_indices.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import expect from '@kbn/expect';
 import { Filter, FilterMeta } from '@kbn/es-query';
 import { SortDirectionFormat } from 'src/plugins/data/common';
 import { JobParamsDownloadCSV } from '../../../plugins/reporting/server/export_types/csv_searchsource_immediate/types';
@@ -15,7 +16,6 @@ export default function ({ getService }: FtrProviderContext) {
   const kibanaServer = getService('kibanaServer');
   const supertestSvc = getService('supertest');
   const esSupertest = getService('esSupertest');
-  const log = getService('log');
 
   const generateAPI = {
     getCSVFromSearchSource: async (job: JobParamsDownloadCSV) => {
@@ -27,16 +27,18 @@ export default function ({ getService }: FtrProviderContext) {
   };
 
   describe('Frozen indices search', () => {
-    before(async () => {
+    const indexPatternId = 'cool-test-index-pattern';
+    const reset = async () => {
+      await esSupertest.delete('/test1,test2,test3');
+      await kibanaServer.savedObjects.delete({ type: 'index-pattern', id: indexPatternId });
       await kibanaServer.uiSettings.update({
         'csv:quoteValues': true,
         'search:includeFrozen': false,
       });
-    });
+    };
 
-    after(async () => {
-      await kibanaServer.uiSettings.update({ 'search:includeFrozen': false });
-    });
+    before(reset);
+    after(reset);
 
     it('Search includes frozen indices based on Advanced Setting', async () => {
       // setup: settings and clear data from previous testing
@@ -46,18 +48,18 @@ export default function ({ getService }: FtrProviderContext) {
       // setup: add mwhdflaksjefhultiple indices of test data
       await Promise.all([
         esSupertest
-          .post('/test1/_doc?refresh=true')
+          .post('/test1/_doc')
           .send({ '@timestamp': '2021-08-24T21:36:40Z', ip: '43.98.8.183', utilization: 18725 }),
         esSupertest
-          .post('/test2/_doc?refresh=true')
+          .post('/test2/_doc')
           .send({ '@timestamp': '2021-08-21T09:36:40Z', ip: '63.91.103.79', utilization: 8480 }),
         esSupertest
-          .post('/test3/_doc?refresh=true')
+          .post('/test3/_doc')
           .send({ '@timestamp': '2021-08-17T21:36:40Z', ip: '139.108.162.171', utilization: 3078 }),
       ]);
+      await esSupertest.post('/test*/_refresh');
 
       // setup: create index pattern
-      const indexPatternId = 'cool-test-index-pattern';
       const indexPatternCreateResponse = await kibanaServer.savedObjects.create({
         type: 'index-pattern',
         id: indexPatternId,
@@ -65,9 +67,9 @@ export default function ({ getService }: FtrProviderContext) {
         attributes: { title: 'test*', timeFieldName: '@timestamp' },
       });
 
-      log.info(`index pattern create response: ${JSON.stringify(indexPatternCreateResponse)}`);
+      expect(indexPatternCreateResponse.id).to.be(indexPatternId);
 
-      const callExportApi = async () => {
+      const callExportAPI = async () => {
         return await generateAPI.getCSVFromSearchSource({
           browserTimezone: 'UTC',
           columns: ['@timestamp', 'ip', 'utilization'],
@@ -104,46 +106,41 @@ export default function ({ getService }: FtrProviderContext) {
         });
       };
 
-      // testing: call the API to export the search data as CSV
-      try {
-        // check the initial data with a CSV export
-        const initialResult = await callExportApi();
-        expectSnapshot(initialResult.text).toMatchInline(`
-          "\\"@timestamp\\",ip,utilization
-          \\"Aug 24, 2021 @ 21:36:40.000\\",\\"43.98.8.183\\",\\"18,725\\"
-          \\"Aug 21, 2021 @ 09:36:40.000\\",\\"63.91.103.79\\",\\"8,480\\"
-          \\"Aug 17, 2021 @ 21:36:40.000\\",\\"139.108.162.171\\",\\"3,078\\"
-          "
-        `);
+      // 1. check the initial data with a CSV export
+      const initialSearch = await callExportAPI();
+      expectSnapshot(initialSearch.text).toMatchInline(`
+        "\\"@timestamp\\",ip,utilization
+        \\"Aug 24, 2021 @ 21:36:40.000\\",\\"43.98.8.183\\",\\"18,725\\"
+        \\"Aug 21, 2021 @ 09:36:40.000\\",\\"63.91.103.79\\",\\"8,480\\"
+        \\"Aug 17, 2021 @ 21:36:40.000\\",\\"139.108.162.171\\",\\"3,078\\"
+        "
+      `);
 
-        // freeze an index in the pattern
-        // recheck the search results
-        const freezeResult = await esSupertest.post('/test3/_freeze');
-        log.info(`freeze index response: ${JSON.stringify(freezeResult)}`);
-        const afterFreezeResult = await callExportApi();
-        expectSnapshot(afterFreezeResult.text).toMatchInline(`
-          "\\"@timestamp\\",ip,utilization
-          \\"Aug 24, 2021 @ 21:36:40.000\\",\\"43.98.8.183\\",\\"18,725\\"
-          \\"Aug 21, 2021 @ 09:36:40.000\\",\\"63.91.103.79\\",\\"8,480\\"
-          "
-        `);
+      // 2. freeze an index in the pattern
+      await esSupertest.post('/test3/_freeze').expect(200);
+      await esSupertest.post('/test*/_refresh').expect(200);
 
-        // allow searching frozen data
-        // recheck the search results
-        await kibanaServer.uiSettings.update({ 'search:includeFrozen': true });
-        const afterAllowSearchResult = await callExportApi();
-        expectSnapshot(afterAllowSearchResult.text).toMatchInline(`
-          "\\"@timestamp\\",ip,utilization
-          \\"Aug 24, 2021 @ 21:36:40.000\\",\\"43.98.8.183\\",\\"18,725\\"
-          \\"Aug 21, 2021 @ 09:36:40.000\\",\\"63.91.103.79\\",\\"8,480\\"
-          \\"Aug 17, 2021 @ 21:36:40.000\\",\\"139.108.162.171\\",\\"3,078\\"
-          "
-        `);
-      } finally {
-        // cleanup
-        await esSupertest.delete('/test1,test2,test3');
-        await kibanaServer.savedObjects.delete({ type: 'index-pattern', id: indexPatternId });
-      }
+      // 3. recheck the search results
+      const afterFreezeSearch = await callExportAPI();
+      expectSnapshot(afterFreezeSearch.text).toMatchInline(`
+        "\\"@timestamp\\",ip,utilization
+        \\"Aug 24, 2021 @ 21:36:40.000\\",\\"43.98.8.183\\",\\"18,725\\"
+        \\"Aug 21, 2021 @ 09:36:40.000\\",\\"63.91.103.79\\",\\"8,480\\"
+        "
+      `);
+
+      // 4. update setting to allow searching frozen data
+      await kibanaServer.uiSettings.update({ 'search:includeFrozen': true });
+
+      // 5. recheck the search results
+      const afterAllowSearch = await callExportAPI();
+      expectSnapshot(afterAllowSearch.text).toMatchInline(`
+        "\\"@timestamp\\",ip,utilization
+        \\"Aug 24, 2021 @ 21:36:40.000\\",\\"43.98.8.183\\",\\"18,725\\"
+        \\"Aug 21, 2021 @ 09:36:40.000\\",\\"63.91.103.79\\",\\"8,480\\"
+        \\"Aug 17, 2021 @ 21:36:40.000\\",\\"139.108.162.171\\",\\"3,078\\"
+        "
+      `);
     });
   });
 }
