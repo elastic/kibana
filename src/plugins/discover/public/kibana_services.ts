2c893f5c0f41a7eca5c6e92ab86c935462daa9bb
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-08-24 09:35:29 +0200

    Cleanup

--- src/plugins/discover/public/kibana_services.ts
+++ src/plugins/discover/public/kibana_services.ts
@@ -15,26 +15,8 @@ import { createGetterSetter } from '../../kibana_utils/public';
 import { search } from '../../data/public';
 import { DocViewsRegistry } from './application/doc_views/doc_views_registry';
 
-let angularModule: ng.IModule | null = null;
 let services: DiscoverServices | null = null;
 let uiActions: UiActionsStart;
-
-/**
- * set bootstrapped inner angular module
- */
-export function setAngularModule(module: ng.IModule) {
-  angularModule = module;
-}
-
-/**
- * get boostrapped inner angular module
- */
-export function getAngularModule(): ng.IModule {
-  if (!angularModule) {
-    throw new Error('Discover angular module not yet available');
-  }
-  return angularModule;
-}
 
 export function getServices(): DiscoverServices {
   if (!services) {
