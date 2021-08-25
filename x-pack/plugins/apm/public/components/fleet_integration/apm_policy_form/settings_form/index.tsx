dbcb05112e60dfb9dad3fc058697c55f359b57bb
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-08-24 20:52:58 -0400

    [APM] update policy editor with additional config values (#109516) (#109974)
    * refactoring apm integration sections
    
    * adding agent auth section
    
    * refactoring
    
    * adding some unit tests
    
    * fixing ts issue
    
    * removing unnecessary section
    
    * hide fields
    
    * removing suggestions when combo
    
    * fixing apm migration
    
    * addressing PR comments
    
    * addressing PR changes
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Cauê Marcondes <55978943+cauemarcondes@users.noreply.github.com>

--- x-pack/plugins/apm/public/components/fleet_integration/apm_policy_form/settings_form/index.tsx
+++ x-pack/plugins/apm/public/components/fleet_integration/apm_policy_form/settings_form/index.tsx
@@ -17,9 +17,8 @@ import {
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React, { useState } from 'react';
-import { PackagePolicyVars } from '../typings';
+import { PackagePolicyVars, SettingsRow } from '../typings';
 import { FormRowSetting } from './form_row_setting';
-import { SettingDefinition } from './typings';
 import { validateSettingValue } from './utils';
 
 export type FormRowOnChange = (key: string, value: any) => void;
@@ -29,73 +28,74 @@ function FormRow({
   vars,
   onChange,
 }: {
-  initialSetting: SettingDefinition;
+  initialSetting: SettingsRow;
   vars?: PackagePolicyVars;
   onChange: FormRowOnChange;
 }) {
-  function getSettingFormRow(setting: SettingDefinition) {
-    if (setting.type === 'advanced_settings') {
+  function getSettingFormRow(row: SettingsRow) {
+    if (row.type === 'advanced_setting') {
       return (
         <AdvancedOptions>
-          {setting.settings.map((advancedSetting) =>
+          {row.settings.map((advancedSetting) =>
             getSettingFormRow(advancedSetting)
           )}
         </AdvancedOptions>
       );
-    } else {
-      const { key } = setting;
-      const value = vars?.[key]?.value;
-      const { isValid, message } = validateSettingValue(setting, value);
-      return (
-        <React.Fragment key={key}>
-          <EuiDescribedFormGroup
-            title={<h3>{setting.rowTitle}</h3>}
-            description={setting.rowDescription}
-          >
-            <EuiFormRow
-              label={setting.label}
-              isInvalid={!isValid}
-              error={isValid ? undefined : message}
-              helpText={<EuiText size="xs">{setting.helpText}</EuiText>}
-              labelAppend={
-                <EuiText size="xs" color="subdued">
-                  {setting.labelAppend}
-                </EuiText>
-              }
-            >
-              <FormRowSetting
-                setting={setting}
-                onChange={onChange}
-                value={value}
-              />
-            </EuiFormRow>
-          </EuiDescribedFormGroup>
-          {setting.settings &&
-            value &&
-            setting.settings.map((childSettings) =>
-              getSettingFormRow(childSettings)
-            )}
-        </React.Fragment>
-      );
     }
+
+    const { key } = row;
+    const configEntry = vars?.[key];
+    // hides a field that doesn't have its key defined in vars.
+    // This is most likely to happen when a field is no longer supported in the current package version
+    if (!configEntry) {
+      return null;
+    }
+    const { value } = configEntry;
+    const { isValid, message } = validateSettingValue(row, value);
+    return (
+      <React.Fragment key={key}>
+        <EuiDescribedFormGroup
+          title={<h3>{row.rowTitle}</h3>}
+          description={row.rowDescription}
+        >
+          <EuiFormRow
+            label={row.label}
+            isInvalid={!isValid}
+            error={isValid ? undefined : message}
+            helpText={<EuiText size="xs">{row.helpText}</EuiText>}
+            labelAppend={
+              <EuiText size="xs" color="subdued">
+                {row.labelAppend}
+              </EuiText>
+            }
+          >
+            <FormRowSetting row={row} onChange={onChange} value={value} />
+          </EuiFormRow>
+        </EuiDescribedFormGroup>
+        {row.settings &&
+          value &&
+          row.settings.map((childSettings) => getSettingFormRow(childSettings))}
+      </React.Fragment>
+    );
   }
   return getSettingFormRow(initialSetting);
 }
-interface Props {
+
+export interface SettingsSection {
+  id: string;
   title: string;
-  subtitle: string;
-  settings: SettingDefinition[];
+  subtitle?: string;
+  settings: SettingsRow[];
+}
+
+interface Props {
+  settingsSection: SettingsSection;
   vars?: PackagePolicyVars;
   onChange: FormRowOnChange;
 }
 
-export function SettingsForm({
-  title,
-  subtitle,
-  settings,
-  vars,
-  onChange,
-}: Props) {
+export function SettingsForm({ settingsSection, vars, onChange }: Props) {
+  const { title, subtitle, settings } = settingsSection;
   return (
     <EuiPanel>
       <EuiFlexGroup direction="column" gutterSize="s">
@@ -104,11 +104,13 @@ export function SettingsForm({
             <h3>{title}</h3>
           </EuiTitle>
         </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiText size="s" color="subdued">
-            {subtitle}
-          </EuiText>
-        </EuiFlexItem>
+        {subtitle && (
+          <EuiFlexItem>
+            <EuiText size="s" color="subdued">
+              {subtitle}
+            </EuiText>
+          </EuiFlexItem>
+        )}
       </EuiFlexGroup>
       <EuiHorizontalRule margin="s" />
 
