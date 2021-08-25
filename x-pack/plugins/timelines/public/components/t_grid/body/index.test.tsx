470c5cac10c177652493177b14712b802499f9a4
Angela Chuang <yi-chun.chuang@elastic.co>
2021-08-25 16:51:31 +0100

    add unit tests

--- x-pack/plugins/timelines/public/components/t_grid/body/index.test.tsx
+++ x-pack/plugins/timelines/public/components/t_grid/body/index.test.tsx
@@ -128,9 +128,13 @@ describe('Body', () => {
       expect(wrapper.find('div.euiDataGridRowCell').first().exists()).toEqual(true);
     });
 
-    test.skip('it renders a tooltip for timestamp', () => {
+    test('it renders cell value', () => {
       const headersJustTimestamp = defaultHeaders.filter((h) => h.id === '@timestamp');
-      const testProps = { ...props, columnHeaders: headersJustTimestamp };
+      const testProps = {
+        ...props,
+        columnHeaders: headersJustTimestamp,
+        data: mockTimelineData.slice(0, 1),
+      };
       const wrapper = mount(
         <TestProviders>
           <BodyComponent {...testProps} />
@@ -139,10 +143,10 @@ describe('Body', () => {
       wrapper.update();
       expect(
         wrapper
-          .find('[data-test-subj="data-driven-columns"]')
-          .first()
-          .find('[data-test-subj="statefulCell"]')
-          .last()
+          .find('[data-test-subj="dataGridRowCell"]')
+          .at(0)
+          .find('.euiDataGridRowCell__truncate')
+          .childAt(0)
           .text()
       ).toEqual(mockTimelineData[0].ecs.timestamp);
     });
