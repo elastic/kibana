87feb8d2eb900d5f84929df44c55e4a737a2ad03
pgayvallet <pierre.gayvallet@gmail.com>
2021-08-25 14:24:04 +0200

    fix test types

--- src/plugins/saved_objects_management/public/management_section/objects_table/components/export_modal.test.tsx
+++ src/plugins/saved_objects_management/public/management_section/objects_table/components/export_modal.test.tsx
@@ -15,6 +15,7 @@ describe('ExportModal', () => {
   let onCancel: jest.Mock;
   let onSelectedOptionsChange: jest.Mock;
   let onIncludeReferenceChange: jest.Mock;
+  let onIncludeNamespacesChange: jest.Mock;
 
   const options = [
     { id: '1', label: 'option 1' },
@@ -30,6 +31,7 @@ describe('ExportModal', () => {
     onCancel = jest.fn();
     onSelectedOptionsChange = jest.fn();
     onIncludeReferenceChange = jest.fn();
+    onIncludeNamespacesChange = jest.fn();
   });
 
   it('Displays a checkbox for each option', () => {
@@ -43,6 +45,8 @@ describe('ExportModal', () => {
         selectedOptions={selectedOptions}
         includeReferences={false}
         onIncludeReferenceChange={onIncludeReferenceChange}
+        includeNamespaces={false}
+        onIncludeNamespacesChange={onIncludeNamespacesChange}
       />
     );
 
@@ -60,6 +64,8 @@ describe('ExportModal', () => {
         selectedOptions={selectedOptions}
         includeReferences={false}
         onIncludeReferenceChange={onIncludeReferenceChange}
+        includeNamespaces={false}
+        onIncludeNamespacesChange={onIncludeNamespacesChange}
       />
     );
     wrapper.find('EuiButtonEmpty').simulate('click');
@@ -79,6 +85,8 @@ describe('ExportModal', () => {
         selectedOptions={selectedOptions}
         includeReferences={false}
         onIncludeReferenceChange={onIncludeReferenceChange}
+        includeNamespaces={false}
+        onIncludeNamespacesChange={onIncludeNamespacesChange}
       />
     );
     wrapper.find('EuiButton').simulate('click');
