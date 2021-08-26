be99e68aaa07913ae931fcd7a23be6a7ace02a0c
cauemarcondes <caue.marcondes@elastic.co>
2021-08-26 16:22:36 -0400

    redirect to page adding transaction type

--- x-pack/plugins/apm/public/components/app/transaction_details/index.tsx
+++ x-pack/plugins/apm/public/components/app/transaction_details/index.tsx
@@ -7,24 +7,37 @@
 
 import { EuiSpacer, EuiTitle } from '@elastic/eui';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useApmServiceContext } from '../../../context/apm_service/use_apm_service_context';
 import { useBreadcrumb } from '../../../context/breadcrumbs/use_breadcrumb';
 import { ChartPointerEventContextProvider } from '../../../context/chart_pointer_event/chart_pointer_event_context';
 import { useApmParams } from '../../../hooks/use_apm_params';
 import { useApmRouter } from '../../../hooks/use_apm_router';
 import { useTimeRange } from '../../../hooks/use_time_range';
 import { TransactionCharts } from '../../shared/charts/transaction_charts';
-
+import { replace } from '../../shared/Links/url_helpers';
 import { TransactionDetailsTabs } from './transaction_details_tabs';
 
 export function TransactionDetails() {
   const { path, query } = useApmParams(
     '/services/:serviceName/transactions/view'
   );
-  const { transactionName, rangeFrom, rangeTo } = query;
-
+  const {
+    transactionName,
+    rangeFrom,
+    rangeTo,
+    transactionType: transactionTypeFromUrl,
+  } = query;
   const { start, end } = useTimeRange({ rangeFrom, rangeTo });
-
   const apmRouter = useApmRouter();
+  const { transactionType } = useApmServiceContext();
+
+  const history = useHistory();
+
+  // redirect to first transaction type
+  if (!transactionTypeFromUrl && transactionType) {
+    replace(history, { query: { transactionType } });
+  }
 
   useBreadcrumb({
     title: transactionName,
