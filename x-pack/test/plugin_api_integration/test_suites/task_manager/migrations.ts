e01e314602ba239358fc3c05a56f2caae2a2d29d
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2021-08-26 13:42:42 -0700

    added func tests

--- x-pack/test/plugin_api_integration/test_suites/task_manager/migrations.ts
+++ x-pack/test/plugin_api_integration/test_suites/task_manager/migrations.ts
@@ -7,8 +7,9 @@
 
 import expect from '@kbn/expect';
 import type { ApiResponse, estypes } from '@elastic/elasticsearch';
+import { SavedObject } from 'kibana/server';
+import { TaskInstanceWithDeprecatedFields } from '../../../../plugins/task_manager/server/task';
 import { FtrProviderContext } from '../../../common/ftr_provider_context';
-import type { TaskInstanceWithDeprecatedFields } from '../../../../../plugins/task_management/server/saved_objects/tasks';
 
 export default function createGetTests({ getService }: FtrProviderContext) {
   const es = getService('es');
@@ -16,32 +17,23 @@ export default function createGetTests({ getService }: FtrProviderContext) {
 
   describe('migrations', () => {
     before(async () => {
-      await esArchiver.load('x-pack/test/functional/es_archives/task');
+      await esArchiver.load('x-pack/test/functional/es_archives/task_manager_tasks');
     });
 
     after(async () => {
-      await esArchiver.unload('x-pack/test/functional/es_archives/task');
+      await esArchiver.unload('x-pack/test/functional/es_archives/task_manager_tasks');
     });
 
-    it('7.15.0 migrates security_solution alerts with exceptionLists to be saved object references', async () => {
+    it('8.0.0 migrates actions tasks with legacy id to saved object ids', async () => {
       // NOTE: We hae to use elastic search directly against the ".kibana" index because alerts do not expose the references which we want to test exists
-      const response = await es.get<{ references: [{}] }>({
-        index: '.kibana',
-        id: 'alert:38482620-ef1b-11eb-ad71-7de7959be71c',
+      const response = await es.get<SavedObject<TaskInstanceWithDeprecatedFields>>({
+        index: '.kibana_task_manager_1',
+        id: 'task:be7e1250-3322-11eb-94c1-db6995e84f6a',
       });
       expect(response.statusCode).to.eql(200);
-      expect(response.body._source?.references).to.eql([
-        {
-          name: 'param:exceptionsList_0',
-          id: 'endpoint_list',
-          type: 'exception-list-agnostic',
-        },
-        {
-          name: 'param:exceptionsList_1',
-          id: '50e3bd70-ef1b-11eb-ad71-7de7959be71c',
-          type: 'exception-list',
-        },
-      ]);
+      expect(response.body._source?.task.params).to.eql(
+        '{"spaceId":"user1","alertId":"0359d7fcc04da9878ee9aadbda38ba55"}'
+      );
     });
 
     it('8.0.0 migrates actions tasks from legacy id to saved object ids', async () => {
@@ -52,7 +44,7 @@ export default function createGetTests({ getService }: FtrProviderContext) {
         body: {
           query: {
             term: {
-              _id: 'alert:74f3e6d7-b7bb-477d-ac28-92ee22728e6e',
+              _id: 'task:be7e1250-3322-11eb-94c1-db6995e8389f',
             },
           },
         },
@@ -60,9 +52,7 @@ export default function createGetTests({ getService }: FtrProviderContext) {
       expect(searchResult.statusCode).to.equal(200);
       expect((searchResult.body.hits.total as estypes.SearchTotalHits).value).to.equal(1);
       const hit = searchResult.body.hits.hits[0];
-      expect((hit!._source!.alert! as TaskInstanceWithDeprecatedFields).legacyId).to.equal(
-        '74f3e6d7-b7bb-477d-ac28-92ee22728e6e'
-      );
+      expect(hit!._source!.params!).to.equal('74f3e6d7-b7bb-477d-ac28-92ee22728e6e');
     });
   });
 }
