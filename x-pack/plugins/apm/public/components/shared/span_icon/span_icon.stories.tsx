7714b99d4b20c7634ec74b47064e093239b9f945
Casper Hübertz <casper@formgeist.com>
2021-08-26 14:12:51 +0200

    [APM] WIP commit of SpanIcon storybook

--- x-pack/plugins/apm/public/components/shared/span_icon/span_icon.stories.tsx
+++ x-pack/plugins/apm/public/components/shared/span_icon/span_icon.stories.tsx
@@ -14,21 +14,31 @@ import {
 } from '@elastic/eui';
 import type { Story } from '@storybook/react';
 import React from 'react';
-import { getSpanIcon, spanTypeIcons } from './get_span_icon';
+import {
+  getSpanIcon,
+  spanTypeIcons,
+  defaultSpanTypeIcons,
+} from './get_span_icon';
 import { SpanIcon } from './index';
 
-const spanTypes = Object.keys(spanTypeIcons);
+const spanTypes = [
+  // ...Object.keys(spanTypeIcons),
+  ...Object.keys(defaultSpanTypeIcons),
+];
 
 export default {
   title: 'shared/SpanIcon',
   component: SpanIcon,
 };
 
-export const List: Story = () => {
+export const List: Story = (_args, { globals }) => {
+  const darkMode = globals?.euiTheme?.includes('dark') ?? false;
   return (
     <EuiFlexGroup gutterSize="l" wrap={true}>
       {spanTypes.map((type) => {
-        const subtypes = Object.keys(spanTypeIcons[type]);
+        const subtypes = Object.keys(
+          spanTypeIcons[type] ?? { 'my default subtype': 'some ico' }
+        );
         return subtypes.map((subtype) => {
           const id = `${type}.${subtype}`;
 
@@ -45,7 +55,11 @@ export const List: Story = () => {
                         size="s"
                         hasShadow
                         alt={id}
-                        src={getSpanIcon(type, subtype)}
+                        src={getSpanIcon({
+                          type,
+                          subtype,
+                          isDarkMode: darkMode,
+                        })}
                       />
                     </EuiToolTip>
                   </p>
