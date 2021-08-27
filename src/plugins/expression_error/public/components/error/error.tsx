f41a3832cd981181e0369d2333782b6c1a77a6e5
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-08-27 09:06:31 +0300

    [Canvas] Fixes Expression Failed Exit Button not Clickable . (#110191)
    * Fixed not clickable cross at error popup.

--- src/plugins/expression_error/public/components/error/error.tsx
+++ src/plugins/expression_error/public/components/error/error.tsx
@@ -7,15 +7,15 @@
  */
 
 import React, { FC } from 'react';
-import { EuiCallOut } from '@elastic/eui';
+import { EuiButtonIcon, EuiCallOut } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { get } from 'lodash';
 import { ShowDebugging } from './show_debugging';
 
 export interface Props {
   payload: {
     error: Error;
   };
+  onClose?: () => void;
 }
 
 const strings = {
@@ -29,14 +29,18 @@ const strings = {
     }),
 };
 
-export const Error: FC<Props> = ({ payload }) => {
-  const message = get(payload, 'error.message');
+export const Error: FC<Props> = ({ payload, onClose }) => {
+  const message = payload.error?.message;
+
+  const CloseIconButton = () => (
+    <EuiButtonIcon color="danger" iconType="cross" onClick={onClose} aria-hidden />
+  );
 
   return (
     <EuiCallOut
       style={{ maxWidth: 500 }}
       color="danger"
-      iconType="cross"
+      iconType={CloseIconButton}
       title={strings.getTitle()}
     >
       <p>{message ? strings.getDescription() : ''}</p>
