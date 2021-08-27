4a74c6afc094488edca7f0c4ed137e63e34e8051
Alexey Antonov <alexwizp@gmail.com>
2021-08-27 16:54:23 +0300

    add namespaces: ['*'],

--- src/plugins/vis_type_table/server/usage_collector/get_stats.ts
+++ src/plugins/vis_type_table/server/usage_collector/get_stats.ts
@@ -49,6 +49,7 @@ export async function getStats(
   const finder = await soClient.createPointInTimeFinder({
     type: 'visualization',
     perPage: 1000,
+    namespaces: ['*'],
   });
 
   const stats: VisTypeTableUsage = {
