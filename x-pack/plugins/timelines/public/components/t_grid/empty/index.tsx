bc791cf690267a4dbe97d2734d2d51879241ec42
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2021-08-30 18:24:54 +0200

    Implement empty page according to design

--- x-pack/plugins/timelines/public/components/t_grid/empty/index.tsx
+++ x-pack/plugins/timelines/public/components/t_grid/empty/index.tsx
@@ -6,29 +6,58 @@
  */
 
 import React from 'react';
-import { EuiEmptyPrompt } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiImage, EuiPanel, EuiText, EuiTitle } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
+import { useKibana } from '../../../../../../../src/plugins/kibana_react/public';
+import { FullWidthFlexGroup } from '../styles';
+
+const panelStyle = {
+  maxWidth: 500,
+};
 
 export const TGridEmpty: React.FC = () => {
+  const { http } = useKibana().services;
+
   return (
-    <EuiEmptyPrompt
-      title={
-        <h2>
-          <FormattedMessage
-            id="xpack.timelines.tGrid.noResultsMatchSearchCriteriaTitle"
-            defaultMessage="No results match your search criteria"
-          />
-        </h2>
-      }
-      titleSize="s"
-      body={
-        <p>
-          <FormattedMessage
-            id="xpack.timelines.tGrid.noResultsMatchSearchCriteriaDescription"
-            defaultMessage="Try searching over a longer period of time or modifying your search."
-          />
-        </p>
-      }
-    />
+    <FullWidthFlexGroup
+      $visible={true}
+      $color="subdued"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <EuiFlexItem grow={false}>
+        <EuiPanel hasBorder={true} style={panelStyle}>
+          <EuiFlexGroup>
+            <EuiFlexItem>
+              <EuiText size="s">
+                <EuiTitle>
+                  <h3>
+                    <FormattedMessage
+                      id="xpack.timelines.tgrid.empty.title"
+                      defaultMessage="No results match your search criteria"
+                    />
+                  </h3>
+                </EuiTitle>
+                <p>
+                  <FormattedMessage
+                    id="xpack.timelines.tgrid.empty.description"
+                    defaultMessage="Try searching over a longer period of time or modifying your search"
+                  />
+                </p>
+              </EuiText>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiImage
+                size="200"
+                alt="observability overview image"
+                url={http!.basePath.prepend(
+                  '/plugins/timelines/assets/illustration_product_no_results_magnifying_glass.svg'
+                )}
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPanel>
+      </EuiFlexItem>
+    </FullWidthFlexGroup>
   );
 };
