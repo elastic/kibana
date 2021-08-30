bfea5667ef98dfe4e406b2ecb0e333a6d9f45ca5
Josh Dover <1813008+joshdover@users.noreply.github.com>
2021-08-30 21:11:27 +0200

    [7.15] Update onboarding interstitial to handle default Fleet assets (#108193) (#110496)
    * Update onboarding interstitial to handle default Fleet assets (#108193)
    
    * skip flaky suite.  #108193
    
    Co-authored-by: Jonathan Budzenski <jon@budzenski.me>

--- src/plugins/home/public/application/components/home_app.js
+++ src/plugins/home/public/application/components/home_app.js
@@ -33,6 +33,7 @@ export function HomeApp({ directories, solutions }) {
     addBasePath,
     environmentService,
     telemetry,
+    http,
   } = getServices();
   const environment = environmentService.getEnvironment();
   const isCloudEnabled = environment.cloud;
@@ -71,10 +72,10 @@ export function HomeApp({ directories, solutions }) {
               addBasePath={addBasePath}
               directories={directories}
               solutions={solutions}
-              find={savedObjectsClient.find}
               localStorage={localStorage}
               urlBasePath={getBasePath()}
               telemetry={telemetry}
+              http={http}
             />
           </Route>
           <Route path="*" exact={true} component={RedirectToDefaultApp} />
