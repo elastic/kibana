112be625b297b35dd2eb4122ec4527769c820eb1
Ahmad Bamieh <ahmadbamieh@gmail.com>
2021-08-30 10:34:47 +0300

    first itteration

--- src/plugins/kibana_usage_collection/server/collectors/cloud/detector/gcp.ts
+++ src/plugins/kibana_usage_collection/server/collectors/cloud/detector/gcp.ts
@@ -6,9 +6,8 @@
  * Side Public License, v 1.
  */
 
-import { isString } from 'lodash';
-import { promisify } from 'util';
-import { CloudService, CloudServiceOptions, Request, Response } from './cloud_service';
+import fetch from 'node-fetch';
+import { CloudService } from './cloud_service';
 import { CloudServiceResponse } from './cloud_response';
 
 // GCP documentation shows both 'metadata.google.internal' (mostly) and '169.254.169.254' (sometimes)
@@ -21,61 +20,47 @@ const SERVICE_ENDPOINT = 'http://169.254.169.254/computeMetadata/v1/instance';
  * @internal
  */
 export class GCPCloudService extends CloudService {
-  constructor(options: CloudServiceOptions = {}) {
-    super('gcp', options);
+  constructor() {
+    super('gcp');
   }
 
-  _checkIfService(request: Request) {
+  _checkIfService = async () => {
     // we need to call GCP individually for each field we want metadata for
     const fields = ['id', 'machine-type', 'zone'];
 
-    const create = this._createRequestForField;
-    const allRequests = fields.map((field) => promisify(request)(create(field)));
-    return (
-      Promise.all(allRequests)
-        // Note: there is no fallback option for GCP;
-        // responses are arrays containing [fullResponse, body];
-        // because GCP returns plaintext, we have no way of validating
-        // without using the response code.
-        .then((responses) => {
-          return responses.map((response) => {
-            if (!response || response.statusCode === 404) {
-              throw new Error('GCP request failed');
-            }
-            return this._extractBody(response, response.body);
-          });
-        })
-        .then(([id, machineType, zone]) => this._combineResponses(id, machineType, zone))
+    const responses = await Promise.all(
+      fields.map(async (field) => {
+        return await fetch(`${SERVICE_ENDPOINT}/${field}`, {
+          method: 'GET',
+          headers: {
+            // GCP requires this header
+            'Metadata-Flavor': 'Google',
+          },
+        });
+      })
     );
-  }
 
-  _createRequestForField(field: string) {
-    return {
-      method: 'GET',
-      uri: `${SERVICE_ENDPOINT}/${field}`,
-      headers: {
-        // GCP requires this header
-        'Metadata-Flavor': 'Google',
-      },
-      // GCP does _not_ return JSON
-      json: false,
-    };
-  }
+    // Note: there is no fallback option for GCP;
+    // responses are arrays containing [fullResponse, body];
+    // because GCP returns plaintext, we have no way of validating
+    // without using the response code.
+    const [id, machineType, zone] = await Promise.all(
+      responses.map(async (response) => {
+        if (
+          !response.ok ||
+          response.status === 404 ||
+          response.headers.get('metadata-flavor') !== 'Google'
+        ) {
+          throw new Error('GCP request failed');
+        }
 
-  /**
-   * Extract the body if the response is valid and it came from GCP.
-   */
-  _extractBody(response: Response, body?: Response['body']) {
-    if (
-      response?.statusCode === 200 &&
-      response.headers &&
-      response.headers['metadata-flavor'] === 'Google'
-    ) {
-      return body;
-    }
+        // GCP does _not_ return JSON
+        return await response.text();
+      })
+    );
 
-    return null;
-  }
+    return this.combineResponses(id, machineType, zone);
+  };
 
   /**
    * Parse the GCP responses, if possible.
@@ -86,10 +71,10 @@ export class GCPCloudService extends CloudService {
    * machineType: 'projects/441331612345/machineTypes/f1-micro'
    * zone: 'projects/441331612345/zones/us-east4-c'
    */
-  _combineResponses(id: string, machineType: string, zone: string) {
-    const vmId = isString(id) ? id.trim() : undefined;
-    const vmType = this._extractValue('machineTypes/', machineType);
-    const vmZone = this._extractValue('zones/', zone);
+  private combineResponses = (id: string, machineType: string, zone: string) => {
+    const vmId = typeof id === 'string' ? id.trim() : undefined;
+    const vmType = this.extractValue('machineTypes/', machineType);
+    const vmZone = this.extractValue('zones/', zone);
 
     let region;
 
@@ -104,7 +89,7 @@ export class GCPCloudService extends CloudService {
     }
 
     throw new Error('unrecognized responses');
-  }
+  };
 
   /**
    * Extract the useful information returned from GCP while discarding
@@ -113,8 +98,8 @@ export class GCPCloudService extends CloudService {
    * For example, this turns something like
    * 'projects/441331612345/machineTypes/f1-micro' into 'f1-micro'.
    */
-  _extractValue(fieldPrefix: string, value: string) {
-    if (isString(value)) {
+  private extractValue = (fieldPrefix: string, value: string) => {
+    if (typeof value === 'string') {
       const index = value.lastIndexOf(fieldPrefix);
 
       if (index !== -1) {
@@ -123,5 +108,5 @@ export class GCPCloudService extends CloudService {
     }
 
     return undefined;
-  }
+  };
 }
