726d0006c15ead45e0b9fcd917e56d63884fba5e
Kyle Pollich <kpollich1@gmail.com>
2021-08-31 14:17:38 -0400

    Fix bug when upgrading Windows package policies
    Ensure package policy merge logics accounts for cases in which an
    input/stream which previously had no variables declared but has
    variables in a later package version.
    
    Fixes #110202

--- x-pack/plugins/fleet/server/services/package_policy.ts
+++ x-pack/plugins/fleet/server/services/package_policy.ts
@@ -944,7 +944,7 @@ export function overridePackageInputs(
     // If there's no corresponding input on the original package policy, just
     // take the override value from the new package as-is. This case typically
     // occurs when inputs or package policies are added/removed between versions.
-    if (!originalInput) {
+    if (originalInput === undefined) {
       inputs.push(override as NewPackagePolicyInput);
       continue;
     }
@@ -958,7 +958,11 @@ export function overridePackageInputs(
     }
 
     if (override.vars) {
-      originalInput = deepMergeVars(originalInput, override);
+      if (!originalInput.vars) {
+        originalInput.vars = { ...override.vars };
+      } else {
+        originalInput = deepMergeVars(originalInput, override) as NewPackagePolicyInput;
+      }
     }
 
     if (override.streams) {
@@ -967,12 +971,21 @@ export function overridePackageInputs(
           (s) => s.data_stream.dataset === stream.data_stream.dataset
         );
 
+        if (originalStream === undefined) {
+          originalInput.streams.push(stream);
+          continue;
+        }
+
         if (typeof stream.enabled !== 'undefined' && originalStream) {
           originalStream.enabled = stream.enabled;
         }
 
         if (stream.vars) {
-          originalStream = deepMergeVars(originalStream, stream as InputsOverride);
+          if (!originalStream.vars) {
+            originalStream.vars = { ...stream.vars };
+          } else {
+            originalStream = deepMergeVars(originalStream, stream as InputsOverride);
+          }
         }
       }
     }
