226a94657c2a188af98584194b972a304b805ffd
Brian Seeders <brian.seeders@elastic.co>
2021-08-31 16:25:55 -0400

    Attempt to balance ci groups a little bit

--- x-pack/test/case_api_integration/spaces_only/tests/trial/index.ts
+++ x-pack/test/case_api_integration/spaces_only/tests/trial/index.ts
@@ -12,7 +12,7 @@ import { createSpaces, deleteSpaces } from '../../../common/lib/authentication';
 export default ({ loadTestFile, getService }: FtrProviderContext): void => {
   describe('cases spaces only enabled: trial', function () {
     // Fastest ciGroup for the moment.
-    this.tags('ciGroup5');
+    this.tags('ciGroup10');
 
     before(async () => {
       await createSpaces(getService);
