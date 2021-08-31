82b187c6dc47773cd23bf7e34f4e8812a299f6fb
Kyle Pollich <kyle.pollich@elastic.co>
2021-08-31 08:08:00 -0400

    [Fleet] Fix policy upgrade from APM 0.3.0 to 0.4.0 (#110505)
    * Fix policy upgrade from APM 0.3.0 to 0.4.0
    
    Add debug log + logic to skip over any package variables that have been
    removed from the base policy object. Issue was initially surfaced
    testing upgrade from APM integration v0.3.0 to v0.4.0.
    
    Ref #109907
    
    * Fix type error in test
    
    * Remove translation for validation debug log
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/edit_package_policy_page/index.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/edit_package_policy_page/index.tsx
@@ -658,7 +658,7 @@ const UpgradeStatusCallout: React.FunctionComponent<{
         </EuiPortal>
       )}
 
-      {isReadyForUpgrade ? (
+      {isReadyForUpgrade && currentPackagePolicy ? (
         <EuiCallOut
           title={i18n.translate('xpack.fleet.upgradePackagePolicy.statusCallOut.successTitle', {
             defaultMessage: 'Ready to upgrade',
