5f8895e04bb2b64b3486d3b2ac597245b31c8a6f
Jonathan Buttner <jonathan.buttner@elastic.co>
2021-08-31 13:59:12 -0400

    Adding connector test

--- x-pack/test/case_api_integration/security_and_spaces/tests/common/cases/export.ts
+++ x-pack/test/case_api_integration/security_and_spaces/tests/common/cases/export.ts
@@ -101,7 +101,53 @@ export default ({ getService }: FtrProviderContext): void => {
           'file',
           join(
             __dirname,
-            '../../../../common/fixtures/case_exports/single_case_user_actions_one_comment.ndjson'
+            '../../../../common/fixtures/saved_object_exports/single_case_user_actions_one_comment.ndjson'
+          )
+        )
+        .set('kbn-xsrf', 'true')
+        .expect(200);
+
+      const findResponse = await findCases({ supertest, query: {} });
+      expect(findResponse.total).to.eql(1);
+      expect(findResponse.cases[0].title).to.eql('A case to export');
+      expect(findResponse.cases[0].description).to.eql('a description');
+
+      const { body: commentsResponse }: { body: CommentsResponse } = await supertest
+        .get(`${CASES_URL}/${findResponse.cases[0].id}/comments/_find`)
+        .send()
+        .expect(200);
+
+      const comment = (commentsResponse.comments[0] as unknown) as AttributesTypeUser;
+      expect(comment.comment).to.eql('A comment for my case');
+
+      const userActions = await getCaseUserActions({
+        supertest,
+        caseID: findResponse.cases[0].id,
+      });
+
+      expect(userActions).to.have.length(2);
+      expect(userActions[0].action).to.eql('create');
+      expect(includesAllRequiredFields(userActions[0].action_field)).to.eql(true);
+
+      expect(userActions[1].action).to.eql('create');
+      expect(userActions[1].action_field).to.eql(['comment']);
+      expect(userActions[1].old_value).to.eql(null);
+      expect(JSON.parse(userActions[1].new_value!)).to.eql({
+        comment: 'A comment for my case',
+        type: 'user',
+        owner: 'securitySolution',
+      });
+    });
+
+    it('imports a case with a connector', async () => {
+      await supertest
+        .post('/api/saved_objects/_import')
+        .query({ overwrite: true })
+        .attach(
+          'file',
+          join(
+            __dirname,
+            '../../../../common/fixtures/saved_object_exports/single_case_with_connector_update_to_none.ndjson'
           )
         )
         .set('kbn-xsrf', 'true')
