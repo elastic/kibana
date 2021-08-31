3743c7b88356136055c491d1e54c9d65035776b2
Phillip Burch <phillip.burch@live.com>
2021-08-31 09:04:11 -0500

    Remove unused import

--- x-pack/plugins/monitoring/public/application/pages/use_monitoring_time.tsx
+++ x-pack/plugins/monitoring/public/application/pages/use_monitoring_time.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { useCallback, useState } from 'react';
+import { useCallback, useState } from 'react';
 import createContainer from 'constate';
 
 interface TimeOptions {
