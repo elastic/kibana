bb6487a69a5332d4fd67f917769ddc8f7d33a29f
Pete Hampton <pjhampton@users.noreply.github.com>
2021-09-02 00:37:01 +0100

    Resolve merge conflict.

--- x-pack/plugins/security_solution/server/lib/telemetry/constants.ts
+++ x-pack/plugins/security_solution/server/lib/telemetry/constants.ts
@@ -5,6 +5,8 @@
  * 2.0.
  */
 
+export const TELEMETRY_MAX_BUFFER_SIZE = 100;
+
 export const TELEMETRY_CHANNEL_LISTS = 'security-lists';
 
 export const TELEMETRY_CHANNEL_ENDPOINT_META = 'endpoint-metadata';
