9e6a2821c9149438d9882c4e8cc3dc2a32584700
Sergi Massaneda <sergi.massaneda@elastic.co>
2021-09-01 16:31:01 +0200

    [7.x] [RAC] Remove rbac on security solution side (#110472) (#110765)
    * [RAC] Remove rbac on security solution side (#110472)
    
    * wip to remove rbac
    
    * Revert "[Cases] Include rule registry client for updating alert statuses (#108588)"
    
    This reverts commit 1fd7038b34084052895bb926b80c2301e4588de9.
    
    This leaves the rule registry mock changes
    
    * remove rbac on Trend/Count alert
    
    * update detection api for status
    
    * remove @kbn-alerts packages
    
    * fix leftover
    
    * Switching cases to leverage update by query for alert status
    
    * Adding missed files
    
    * fix bad logic
    
    * updating tests for use_alerts_privileges
    
    * remove index alias/fields
    
    * fix types
    
    * fix plugin to get the right index names
    
    * left over of alis on template
    
    * forget to use current user for create/read route index
    
    * updated alerts page to not show table when no privileges and updates to tests
    
    * fix bug when switching between o11y and security solution
    
    * updates tests and move to use privileges page when user tries to access alerts without proper access
    
    * updating jest tests
    
    * pairing with yara
    
    * bring back kbn-alerts after discussion with the team
    
    * fix types
    
    * fix index field for o11y
    
    * fix bug with updating index priv state
    
    * fix i18n issue and update api docs
    
    * fix refresh on alerts
    
    * fix render view on alerts
    
    * updating tests and checking for null in alerts page to not show no privileges page before load
    
    * fix details rules
    
    Co-authored-by: Jonathan Buttner <jonathan.buttner@elastic.co>
    Co-authored-by: Yara Tercero <yara.tercero@elastic.co>
    # Conflicts:
    #	x-pack/plugins/security_solution/cypress/integration/detection_alerts/alerts_details.spec.ts
    
    * skip test
    
    Co-authored-by: Xavier Mouligneau <189600+XavierM@users.noreply.github.com>

--- x-pack/plugins/timelines/public/components/t_grid/integrated/index.tsx
+++ x-pack/plugins/timelines/public/components/t_grid/integrated/index.tsx
@@ -41,7 +41,7 @@ import { useDeepEqualSelector } from '../../../hooks/use_selector';
 import { defaultHeaders } from '../body/column_headers/default_headers';
 import { buildCombinedQuery, getCombinedFilterQuery, resolverIsShowing } from '../helpers';
 import { tGridActions, tGridSelectors } from '../../../store/t_grid';
-import { useTimelineEvents } from '../../../container';
+import { useTimelineEvents, InspectResponse, Refetch } from '../../../container';
 import { StatefulBody } from '../body';
 import { SELECTOR_TIMELINE_GLOBAL_CONTAINER, UpdatedFlexGroup, UpdatedFlexItem } from '../styles';
 import { Sort } from '../body/sort';
@@ -114,6 +114,7 @@ export interface TGridIntegratedProps {
   query: Query;
   renderCellValue: (props: CellValueElementProps) => React.ReactNode;
   rowRenderers: RowRenderer[];
+  setQuery: (inspect: InspectResponse, loading: boolean, refetch: Refetch) => void;
   sort: Sort[];
   start: string;
   tGridEventRenderedViewEnabled: boolean;
@@ -150,6 +151,7 @@ const TGridIntegratedComponent: React.FC<TGridIntegratedProps> = ({
   query,
   renderCellValue,
   rowRenderers,
+  setQuery,
   sort,
   start,
   tGridEventRenderedViewEnabled,
@@ -269,6 +271,10 @@ const TGridIntegratedComponent: React.FC<TGridIntegratedProps> = ({
     }
   }, [loading]);
 
+  useEffect(() => {
+    setQuery(inspect, loading, refetch);
+  }, [inspect, loading, refetch, setQuery]);
+
   return (
     <InspectButtonContainer>
       <StyledEuiPanel
@@ -294,11 +300,12 @@ const TGridIntegratedComponent: React.FC<TGridIntegratedProps> = ({
               <UpdatedFlexItem grow={false} $show={!loading}>
                 {!resolverIsShowing(graphEventId) && additionalFilters}
               </UpdatedFlexItem>
-              {tGridEventRenderedViewEnabled && entityType === 'alerts' && (
-                <UpdatedFlexItem grow={false} $show={!loading}>
-                  <SummaryViewSelector viewSelected={tableView} onViewChange={setTableView} />
-                </UpdatedFlexItem>
-              )}
+              {tGridEventRenderedViewEnabled &&
+                ['detections-page', 'detections-rules-details-page'].includes(id) && (
+                  <UpdatedFlexItem grow={false} $show={!loading}>
+                    <SummaryViewSelector viewSelected={tableView} onViewChange={setTableView} />
+                  </UpdatedFlexItem>
+                )}
             </UpdatedFlexGroup>
 
             {!graphEventId && graphOverlay == null && (
