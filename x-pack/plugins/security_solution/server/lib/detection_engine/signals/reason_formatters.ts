bdc2414def2fc911257dae6db4b552d8b4847a86
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2021-09-01 09:19:27 -0400

    update tests

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/reason_formatters.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/reason_formatters.ts
@@ -54,13 +54,17 @@ export const buildReasonMessageUtil = ({ rule, mergedDoc }: BuildReasonMessageUt
     userName = mergedDoc.fields['user.name'] ?? null;
   }
 
+  const fieldPresenceTracker = { hasFieldOfInterest: false };
+
   const getFieldTemplateValue = (field: string | string[] | undefined | null): string => {
     if (!field || !field.length || (field.length === 1 && field[0] === '-')) return 'null';
+    if (!fieldPresenceTracker.hasFieldOfInterest) fieldPresenceTracker.hasFieldOfInterest = true;
     return Array.isArray(field) ? field.join(', ') : field;
   };
 
   return i18n.translate('xpack.securitySolution.detectionEngine.signals.alertReasonDescription', {
-    defaultMessage: `{eventCategory, select, null {} other {{eventCategory}{whitespace}}}event with\
+    defaultMessage: `{eventCategory, select, null {} other {{eventCategory}{whitespace}}}event\
+{hasFieldOfInterest, select, null {} other {{whitespace}with}}\
 {processName, select, null {} other {{whitespace}process {processName},} }\
 {processParentName, select, null {} other {{whitespace}parent process {processParentName},} }\
 {fileName, select, null {} other {{whitespace}file {fileName},} }\
@@ -82,6 +86,7 @@ created {alertSeverity} alert {alertName}.`,
       sourceAddress: getFieldTemplateValue(sourceAddress),
       sourcePort: getFieldTemplateValue(sourcePort),
       userName: getFieldTemplateValue(userName),
+      hasFieldOfInterest: fieldPresenceTracker.hasFieldOfInterest, // Tracking if we have any fields to show the 'with' word
       whitespace: ' ', // there isn't support for the unicode /u0020 for whitespace, and leading spaces are deleted, so to prevent double-whitespace explicitly passing the space in.
     },
   });
