bcd68adb45467f17b975e298691ef1308d3a61bb
Mat Schaffer <mat@elastic.co>
2021-09-01 23:42:08 +0900

    Wire getData and redirect for no data page

--- x-pack/plugins/monitoring/public/application/pages/no_data_page.tsx
+++ x-pack/plugins/monitoring/public/application/pages/no_data_page.tsx
@@ -5,25 +5,80 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
 // @ts-ignore
 import { NoData } from '../../components/no_data';
+import { PageTemplate } from './page_template';
+import { useKibana } from '../../../../../../src/plugins/kibana_react/public';
+import { CODE_PATH_LICENSE, STANDALONE_CLUSTER_CLUSTER_UUID } from '../../../common/constants';
 
-export const NoDataPage: React.FC = () => {
+export const NoDataPage = () => {
+  const { services } = useKibana<{ data: any }>();
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+
+  const title = i18n.translate('xpack.monitoring.noData.routeTitle', {
+    defaultMessage: 'Setup Monitoring',
+  });
+
+  const url = '../api/monitoring/v1/clusters';
+
+  const getData = async () => {
+    const bounds = services.data?.query.timefilter.timefilter.getBounds();
+    const min = bounds.min.toISOString();
+    const max = bounds.max.toISOString();
+
+    const codePaths = [CODE_PATH_LICENSE];
+
+    let catchReason;
+
+    try {
+      const response = await services.http?.fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          css: undefined,
+          timeRange: {
+            min,
+            max,
+          },
+          codePaths,
+        }),
+      });
+
+      const clusters = formatClusters(response);
+
+      if (clusters && clusters.length) {
+        setShouldRedirect(true);
+        // TODO should this return?
+        // return clusters;
+      }
+    } catch (err) {
+      if (err && err.status === 503) {
+        // TODO something useful with the error reason
+        catchReason = {
+          property: 'custom',
+          message: err.message,
+        };
+      }
+    }
+  };
+
   return (
-    <div className="app-container">
-      <EuiFlexGroup gutterSize="l" justifyContent="spaceBetween" responsive>
-        <EuiFlexItem />
-        <EuiFlexItem>
-          <div>HERE GOES THE TIMEPICKER</div>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-
-      <div>
-        <NoData />
-      </div>
-    </div>
+    <PageTemplate title={title} getData={getData}>
+      {shouldRedirect ? <Redirect to="/home" /> : <NoData />}
+    </PageTemplate>
   );
 };
+
+function formatClusters(clusters: any) {
+  return clusters.map(formatCluster);
+}
+
+function formatCluster(cluster: any) {
+  if (cluster.cluster_uuid === STANDALONE_CLUSTER_CLUSTER_UUID) {
+    cluster.cluster_name = 'Standalone Cluster';
+  }
+  return cluster;
+}
