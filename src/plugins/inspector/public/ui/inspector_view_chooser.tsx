3122e3f7e01063019916038757a25dcd16cb36f8
Nathan L Smith <smith@nlsmith.com>
2021-09-03 10:12:58 -0500

    start messing with view chooser

--- src/plugins/inspector/public/ui/inspector_view_chooser.tsx
+++ src/plugins/inspector/public/ui/inspector_view_chooser.tsx
@@ -14,111 +14,153 @@ import {
   EuiContextMenuItem,
   EuiContextMenuPanel,
   EuiPopover,
+  EuiSuperSelect,
+  EuiSuperSelectOption,
+  EuiText,
   EuiToolTip,
 } from '@elastic/eui';
 import { InspectorViewDescription } from '../types';
 
-interface Props {
+interface InspectorViewChooserProps {
   views: InspectorViewDescription[];
   onViewSelected: (view: InspectorViewDescription) => void;
   selectedView: InspectorViewDescription;
 }
 
-interface State {
-  isSelectorOpen: boolean;
-}
+export function InspectorViewChooser({
+  onViewSelected,
+  selectedView,
+  views,
+}: InspectorViewChooserProps) {
+  const options: EuiSuperSelectOption<InspectorViewDescription> = views.map((view) => {
+    return {
+      value: view.title,
+      inputDisplay: view.title,
+      dropdownDisplay: (
+        <>
+          <strong>{view.title}</strong>
+          <EuiText size="s" color="subdued">
+            <p className="euiTextColor--subdued">{view.help}</p>
+          </EuiText>
+        </>
+      ),
+    };
+  });
 
-export class InspectorViewChooser extends Component<Props, State> {
-  static propTypes = {
-    views: PropTypes.array.isRequired,
-    onViewSelected: PropTypes.func.isRequired,
-    selectedView: PropTypes.object.isRequired,
-  };
-
-  state: State = {
-    isSelectorOpen: false,
-  };
-
-  toggleSelector = () => {
-    this.setState((prev) => ({
-      isSelectorOpen: !prev.isSelectorOpen,
-    }));
-  };
-
-  closeSelector = () => {
-    this.setState({
-      isSelectorOpen: false,
-    });
-  };
-
-  renderView = (view: InspectorViewDescription, index: number) => {
+  if (views.length < 2) {
     return (
-      <EuiContextMenuItem
-        key={index}
-        onClick={() => {
-          this.props.onViewSelected(view);
-          this.closeSelector();
-        }}
-        toolTipContent={view.help}
-        toolTipPosition="left"
-        data-test-subj={`inspectorViewChooser${view.title}`}
-      >
-        {view.title}
-      </EuiContextMenuItem>
-    );
-  };
-
-  renderViewButton() {
-    return (
-      <EuiButtonEmpty
-        size="s"
-        iconType="arrowDown"
-        iconSide="right"
-        onClick={this.toggleSelector}
-        data-test-subj="inspectorViewChooser"
-      >
+      <EuiToolTip position="bottom" content={selectedView.help}>
         <FormattedMessage
           id="inspector.view"
           defaultMessage="View: {viewName}"
-          values={{ viewName: this.props.selectedView.title }}
-        />
-      </EuiButtonEmpty>
-    );
-  }
-
-  renderSingleView() {
-    return (
-      <EuiToolTip position="bottom" content={this.props.selectedView.help}>
-        <FormattedMessage
-          id="inspector.view"
-          defaultMessage="View: {viewName}"
-          values={{ viewName: this.props.selectedView.title }}
+          values={{ viewName: selectedView.title }}
         />
       </EuiToolTip>
     );
   }
 
-  render() {
-    const { views } = this.props;
-
-    if (views.length < 2) {
-      return this.renderSingleView();
-    }
-
-    const triggerButton = this.renderViewButton();
-
-    return (
-      <EuiPopover
-        id="inspectorViewChooser"
-        button={triggerButton}
-        isOpen={this.state.isSelectorOpen}
-        closePopover={this.closeSelector}
-        panelPaddingSize="none"
-        anchorPosition="downRight"
-        repositionOnScroll
-      >
-        <EuiContextMenuPanel items={views.map(this.renderView)} />
-      </EuiPopover>
-    );
-  }
+  return (
+    <EuiToolTip position="bottom" content={selectedView.help}>
+      <EuiSuperSelect hasDividers={true} options={options} valueOfSelected={selectedView.title} />
+    </EuiToolTip>
+  );
 }
+
+// interface State {
+//   isSelectorOpen: boolean;
+// }
+
+// export class InspectorViewChooser extends Component<Props, State> {
+//   static propTypes = {
+//     views: PropTypes.array.isRequired,
+//     onViewSelected: PropTypes.func.isRequired,
+//     selectedView: PropTypes.object.isRequired,
+//   };
+
+//   state: State = {
+//     isSelectorOpen: false,
+//   };
+
+//   toggleSelector = () => {
+//     this.setState((prev) => ({
+//       isSelectorOpen: !prev.isSelectorOpen,
+//     }));
+//   };
+
+//   closeSelector = () => {
+//     this.setState({
+//       isSelectorOpen: false,
+//     });
+//   };
+
+//   renderView = (view: InspectorViewDescription, index: number) => {
+//     return (
+//       <EuiContextMenuItem
+//         key={index}
+//         onClick={() => {
+//           this.props.onViewSelected(view);
+//           this.closeSelector();
+//         }}
+//         toolTipContent={view.help}
+//         toolTipPosition="left"
+//         data-test-subj={`inspectorViewChooser${view.title}`}
+//       >
+//         {view.title}
+//       </EuiContextMenuItem>
+//     );
+//   };
+
+//   renderViewButton() {
+//     return (
+//       <EuiButtonEmpty
+//         size="s"
+//         iconType="arrowDown"
+//         iconSide="right"
+//         onClick={this.toggleSelector}
+//         data-test-subj="inspectorViewChooser"
+//       >
+//         <FormattedMessage
+//           id="inspector.view"
+//           defaultMessage="View: {viewName}"
+//           values={{ viewName: this.props.selectedView.title }}
+//         />
+//       </EuiButtonEmpty>
+//     );
+//   }
+
+//   renderSingleView() {
+//     return (
+//       <EuiToolTip position="bottom" content={this.props.selectedView.help}>
+//         <FormattedMessage
+//           id="inspector.view"
+//           defaultMessage="View: {viewName}"
+//           values={{ viewName: this.props.selectedView.title }}
+//         />
+//       </EuiToolTip>
+//     );
+//   }
+
+//   render() {
+//     const { views } = this.props;
+
+//     if (views.length < 2) {
+//       return this.renderSingleView();
+//     }
+
+//     const triggerButton = this.renderViewButton();
+
+//     return (
+//       <EuiPopover
+//         id="inspectorViewChooser"
+//         button={triggerButton}
+//         isOpen={this.state.isSelectorOpen}
+//         closePopover={this.closeSelector}
+//         panelPaddingSize="none"
+//         anchorPosition="downRight"
+//         repositionOnScroll
+//       >
+//         <EuiContextMenuPanel items={views.map(this.renderView)} />
+//       </EuiPopover>
+//     );
+//   }
+// }
