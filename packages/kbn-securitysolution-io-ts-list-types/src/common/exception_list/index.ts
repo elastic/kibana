1773c791be3a1bea91f0b39bec95b7c3c5394da1
Esteban Beltran <sergies@gmail.com>
2021-09-06 09:46:32 +0200

    Add isolation_ips_whitelist to the exception list endpoint types

--- packages/kbn-securitysolution-io-ts-list-types/src/common/exception_list/index.ts
+++ packages/kbn-securitysolution-io-ts-list-types/src/common/exception_list/index.ts
@@ -12,6 +12,7 @@ export const exceptionListType = t.keyof({
   detection: null,
   endpoint: null,
   endpoint_events: null,
+  isolation_ips_whitelist: null,
 });
 export const exceptionListTypeOrUndefined = t.union([exceptionListType, t.undefined]);
 export type ExceptionListType = t.TypeOf<typeof exceptionListType>;
@@ -20,4 +21,5 @@ export enum ExceptionListTypeEnum {
   DETECTION = 'detection',
   ENDPOINT = 'endpoint',
   ENDPOINT_EVENTS = 'endpoint_events',
+  ISOLATION_IPS_WHITELIST = 'isolation_ips_whitelist',
 }
