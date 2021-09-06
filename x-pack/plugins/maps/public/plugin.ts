4123e0964206efc81c709d6cf12933b01ef62830
Thomas Neirynck <thomas@elastic.co>
2021-09-06 19:04:58 -0400

    linting

--- x-pack/plugins/maps/public/plugin.ts
+++ x-pack/plugins/maps/public/plugin.ts
@@ -196,8 +196,6 @@ export class MapsPlugin
   }
 
   public start(core: CoreStart, plugins: MapsPluginStartDependencies): MapsStartApi {
-
-
     setLicensingPluginStart(plugins.licensing);
     setStartServices(core, plugins);
 
