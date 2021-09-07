22436cae074ef988f215283f42f78faeb9773b8e
CJ Cenizal <cj@cenizal.com>
2021-09-07 16:45:41 -0700

    Don't export const flushPromiseJobQueue.

--- x-pack/plugins/upgrade_assistant/__jest__/client_integration/helpers/time_manipulation.ts
+++ x-pack/plugins/upgrade_assistant/__jest__/client_integration/helpers/time_manipulation.ts
@@ -11,7 +11,7 @@ import { act } from 'react-dom/test-utils';
  * These helpers are intended to be used in conjunction with jest.useFakeTimers().
  */
 
-export const flushPromiseJobQueue = async () => {
+const flushPromiseJobQueue = async () => {
   // See https://stackoverflow.com/questions/52177631/jest-timer-and-promise-dont-work-well-settimeout-and-async-function
   await Promise.resolve();
 };
