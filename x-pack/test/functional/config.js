2f3b6c7ae99c49ca7514be1f639655ae062f4f5c
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2021-09-08 18:35:51 +0300

    [Vislib] Removes old implementation of xy chart (#110786)
    * [Vislib] Remove xy chart
    
    * Update i18n
    
    * Remove uncecessary file
    
    * Fix types
    
    * More fixes
    
    * Fix functional tests part 1
    
    * Fix functional tests part 2
    
    * Fix bug with shard-delay
    
    * Fix functional tests part 3
    
    * fix functional tests part4
    
    * Fix async_serch FT
    
    * Fix functional dashboard async test
    
    * REplace screenshot area chart image
    
    * Cleanup vislib from xy charts
    
    * Remove unused fixtures
    
    * Address PR comments
    
    * Remove miaou :D
    
    * Address PR comments
    
    * Fix i18n
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	docs/management/advanced-options.asciidoc
    #	test/functional/screenshots/baseline/area_chart.png

--- x-pack/test/functional/config.js
+++ x-pack/test/functional/config.js
@@ -105,7 +105,6 @@ export default async function ({ readConfigFile }) {
       defaults: {
         'accessibility:disableAnimations': true,
         'dateFormat:tz': 'UTC',
-        'visualization:visualize:legacyChartsLibrary': true,
         'visualization:visualize:legacyPieChartsLibrary': true,
       },
     },
