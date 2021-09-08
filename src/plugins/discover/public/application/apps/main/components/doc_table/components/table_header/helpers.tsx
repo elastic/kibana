5cd411c8e82225c00a401e8d8d32fb7113f8546b
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-09-08 19:55:40 +0200

    [Discover] Cleanup legacy code fragments (#110646)

--- src/plugins/discover/public/application/apps/main/components/doc_table/components/table_header/helpers.tsx
+++ src/plugins/discover/public/application/apps/main/components/doc_table/components/table_header/helpers.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 import { i18n } from '@kbn/i18n';
-import { IndexPattern } from '../../../../../../../kibana_services';
+import type { IndexPattern } from 'src/plugins/data/common';
 
 export type SortOrder = [string, string];
 export interface ColumnProps {
