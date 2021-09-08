b2481a65a556f407615673b6e34ba08f9bfbac25
spalger <spalger@users.noreply.github.com>
2021-09-08 12:56:46 -0700

    skip flaky suite (#111628)
    (cherry picked from commit 32e98730b50b87238aafb2e291063d6eaae36699)

--- x-pack/test/functional/apps/lens/add_to_dashboard.ts
+++ x-pack/test/functional/apps/lens/add_to_dashboard.ts
@@ -62,7 +62,8 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     await PageObjects.lens.assertMetric('Maximum of bytes', '19,986');
   };
 
-  describe('lens add-to-dashboards tests', () => {
+  // FLAKY: https://github.com/elastic/kibana/issues/111628
+  describe.skip('lens add-to-dashboards tests', () => {
     it('should allow new lens to be added by value to a new dashboard', async () => {
       await createNewLens();
       await PageObjects.lens.save('New Lens from Modal', false, false, false, 'new');
