7135973311bfcd3d815faf29525ec1e48a0c476d
Brian Seeders <brian.seeders@elastic.co>
2021-09-08 11:12:36 -0400

    Prep for production before merge

--- .buildkite/scripts/steps/es_snapshots/bucket_config.js
+++ .buildkite/scripts/steps/es_snapshots/bucket_config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BASE_BUCKET_DAILY: 'kibana-ci-es-snapshots-daily/buildkite',
-  BASE_BUCKET_PERMANENT: 'kibana-ci-es-snapshots-permanent/buildkite',
+  BASE_BUCKET_DAILY: 'kibana-ci-es-snapshots-daily',
+  BASE_BUCKET_PERMANENT: 'kibana-ci-es-snapshots-permanent',
 };
