2627f237c719b3f576e525052e468dae1f5b9416
Stacey Gammon <gammon@elastic.co>
2021-09-08 15:26:30 -0400

    Clean up api doc infra

--- packages/kbn-docs-utils/src/api_docs/build_api_declarations/build_parameter_decs.ts
+++ packages/kbn-docs-utils/src/api_docs/build_api_declarations/build_parameter_decs.ts
@@ -7,77 +7,43 @@
  */
 
 import { ParameterDeclaration, JSDoc, SyntaxKind } from 'ts-morph';
-import { ToolingLog, KibanaPlatformPlugin } from '@kbn/dev-utils';
 import { extractImportReferences } from './extract_import_refs';
-import { AnchorLink, ApiDeclaration } from '../types';
+import { ApiDeclaration } from '../types';
 import { buildApiDeclaration } from './build_api_declaration';
 import { getJSDocParamComment } from './js_doc_utils';
 import { buildBasicApiDeclaration } from './build_basic_api_declaration';
+import { BuildApiDecOpts } from './types';
+import { buildApiId, getOptsForChild } from './utils';
 
 /**
  * A helper function to capture function parameters, whether it comes from an arrow function, a regular function or
  * a function type.
- *
- * @param params
- * @param plugins
- * @param anchorLink
- * @param log
- * @param jsDocs
  */
 export function buildApiDecsForParameters(
   params: ParameterDeclaration[],
-  plugins: KibanaPlatformPlugin[],
-  parentAnchorLink: AnchorLink,
-  currentPluginId: string,
-  log: ToolingLog,
-  captureReferences: boolean,
+  parentOpts: BuildApiDecOpts,
   jsDocs?: JSDoc[]
 ): ApiDeclaration[] {
-  let paramIndex = 0;
-  return params.reduce((acc, param) => {
-    paramIndex++;
-    const apiName = param.getName();
-    // Destructured parameters can make these ids look ugly. Instead of parameter name, use an index for the position.
-    const apiId = parentAnchorLink.apiName
-      ? parentAnchorLink.apiName + `.$${paramIndex}`
-      : `$${paramIndex}`;
-    const anchorLink: AnchorLink = {
-      scope: parentAnchorLink.scope,
-      pluginName: parentAnchorLink.pluginName,
-      apiName: apiId,
+  return params.reduce((acc, param, index) => {
+    const id = buildApiId(`$${index + 1}`, parentOpts.id);
+    const opts = {
+      ...getOptsForChild(param, parentOpts),
+      id,
     };
-    log.debug(`Getting parameter doc def for ${apiName} of kind ${param.getKindName()}`);
+
+    opts.log.debug(`Getting parameter doc def for ${opts.name} of kind ${param.getKindName()}`);
     // Literal types are non primitives that aren't references to other types. We add them as a more
     // defined node, with children.
     // If we don't want the docs to be too deeply nested we could avoid this special handling.
     if (param.getTypeNode() && param.getTypeNode()!.getKind() === SyntaxKind.TypeLiteral) {
-      acc.push(
-        buildApiDeclaration({
-          node: param.getTypeNode()!,
-          plugins,
-          log,
-          currentPluginId: anchorLink.pluginName,
-          scope: anchorLink.scope,
-          captureReferences,
-          parentApiId: anchorLink.apiName,
-          name: apiName,
-        })
-      );
+      acc.push(buildApiDeclaration(param.getTypeNode()!, opts));
     } else {
-      const apiDec = buildBasicApiDeclaration({
-        currentPluginId,
-        anchorLink,
-        node: param,
-        plugins,
-        log,
-        apiName,
-        captureReferences,
-      });
+      const apiDec = buildBasicApiDeclaration(param, opts);
       acc.push({
         ...apiDec,
         isRequired: param.getType().isNullable() === false,
-        signature: extractImportReferences(param.getType().getText(), plugins, log),
-        description: jsDocs ? getJSDocParamComment(jsDocs, apiName) : [],
+        signature: extractImportReferences(param.getType().getText(), opts.plugins, opts.log),
+        description: jsDocs ? getJSDocParamComment(jsDocs, opts.name) : [],
       });
     }
     return acc;
