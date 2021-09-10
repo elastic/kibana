0e7676d14895acb3ab7048c431058a77fe22b77c
Madison Caldwell <madison.caldwell@elastic.co>
2021-09-09 22:23:38 -0400

    ALERT_TYPE_ID => RULE_TYPE_ID

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/threshold/create_threshold_alert_type.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/threshold/create_threshold_alert_type.ts
@@ -8,7 +8,7 @@
 import { validateNonExact } from '@kbn/securitysolution-io-ts-utils';
 
 import { PersistenceServices } from '../../../../../../rule_registry/server';
-import { THRESHOLD_ALERT_TYPE_ID } from '../../../../../common/constants';
+import { THRESHOLD_RULE_TYPE_ID } from '../../../../../common/constants';
 import { thresholdRuleParams, ThresholdRuleParams } from '../../schemas/rule_schemas';
 import { thresholdExecutor } from '../../signals/executors/threshold';
 import { ThresholdAlertState } from '../../signals/types';
@@ -35,7 +35,7 @@ export const createThresholdAlertType = (createOptions: CreateRuleOptions) => {
     ruleDataService,
   });
   return createSecurityRuleType<ThresholdRuleParams, {}, PersistenceServices, ThresholdAlertState>({
-    id: THRESHOLD_ALERT_TYPE_ID,
+    id: THRESHOLD_RULE_TYPE_ID,
     name: 'Threshold Rule',
     validate: {
       params: {
