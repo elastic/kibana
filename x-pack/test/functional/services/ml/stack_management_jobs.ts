ceaf75cfe5e1498dcaefac69877f2e4a670bfce7
Robert Oskamp <robert.oskamp@elastic.co>
2021-09-13 10:50:51 +0200

    [ML] Functional tests - stabilize import job test

--- x-pack/test/functional/services/ml/stack_management_jobs.ts
+++ x-pack/test/functional/services/ml/stack_management_jobs.ts
@@ -86,7 +86,7 @@ export function MachineLearningStackManagementJobsProvider(
     },
 
     async executeSync() {
-      await testSubjects.click('mlJobMgmtSyncFlyoutSyncButton', 2000);
+      await testSubjects.clickWhenNotDisabled('mlJobMgmtSyncFlyoutSyncButton', { timeout: 5000 });
 
       // check and close success toast
       const resultToast = await toasts.getToastElement(1);
@@ -285,7 +285,7 @@ export function MachineLearningStackManagementJobsProvider(
     },
 
     async importJobs() {
-      await testSubjects.click('mlJobMgmtImportImportButton', 1000);
+      await testSubjects.clickWhenNotDisabled('mlJobMgmtImportImportButton', { timeout: 5000 });
       await testSubjects.missingOrFail('mlJobMgmtImportJobsFlyout', { timeout: 60 * 1000 });
     },
 
@@ -340,7 +340,7 @@ export function MachineLearningStackManagementJobsProvider(
     },
 
     async selectExportJobs() {
-      await testSubjects.click('mlJobMgmtExportExportButton');
+      await testSubjects.clickWhenNotDisabled('mlJobMgmtExportExportButton', { timeout: 5000 });
       await testSubjects.missingOrFail('mlJobMgmtExportJobsFlyout', { timeout: 60 * 1000 });
     },
 
