a6cbdcab451330a2f70b9e16af8e462e60125d3b
chrisronline <chrisronline@gmail.com>
2021-09-14 09:02:13 -0400

    Add test

--- x-pack/plugins/alerting/server/routes/find_rules.test.ts
+++ x-pack/plugins/alerting/server/routes/find_rules.test.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
+import { usageCountersServiceMock } from 'src/plugins/usage_collection/server/usage_counters/usage_counters_service.mock';
 import { findRulesRoute } from './find_rules';
 import { httpServiceMock } from 'src/core/server/mocks';
 import { licenseStateMock } from '../lib/license_state.mock';
@@ -144,5 +144,43 @@ describe('findRulesRoute', () => {
     expect(handler(context, req, res)).rejects.toMatchInlineSnapshot(`[Error: OMG]`);
 
     expect(verifyApiAccess).toHaveBeenCalledWith(licenseState);
+  });
+
+  it('should track calls to deprecated functionality', async () => {
+    const licenseState = licenseStateMock.create();
+    const router = httpServiceMock.createRouter();
+    const mockUsageCountersSetup = usageCountersServiceMock.createSetupContract();
+    const mockUsageCounter = mockUsageCountersSetup.createUsageCounter('test');
+
+    findRulesRoute(router, licenseState, mockUsageCounter);
+
+    const findResult = {
+      page: 1,
+      perPage: 1,
+      total: 0,
+      data: [],
+    };
+    rulesClient.find.mockResolvedValueOnce(findResult);
+
+    const [, handler] = router.get.mock.calls[0];
+    const [context, req, res] = mockHandlerArguments(
+      { rulesClient },
+      {
+        params: {},
+        query: {
+          fields: ['foo', 'bar'],
+          per_page: 1,
+          page: 1,
+          default_search_operator: 'OR',
+        },
+      },
+      ['ok']
+    );
+    await handler(context, req, res);
+    expect(mockUsageCounter.incrementCounter).toHaveBeenCalledWith({
+      counterName: `alertingFieldsUsage`,
+      counterType: 'alertingFieldsUsage',
+      incrementBy: 1,
+    });
   });
 });
