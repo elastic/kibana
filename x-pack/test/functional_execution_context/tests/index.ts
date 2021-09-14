4a2acad218e0d7700edd12dd6b53b0fbe73d9567
Mikhail Shustov <restrry@gmail.com>
2021-09-14 12:52:33 +0300

    Functional tests for execution context propagation in alerting and task manager plugins (#111179)
    * move execution context tests to x-pack folder
    
    * add execution context tests for alerts and tasks
    
    * cleanup tests
    
    * remove obsolete test folder
    
    * use ToolingLog instead of console.log

--- x-pack/test/functional_execution_context/tests/index.ts
+++ x-pack/test/functional_execution_context/tests/index.ts
@@ -1,9 +1,8 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
 import { FtrProviderContext } from '../ftr_provider_context';
@@ -11,6 +10,7 @@ import { FtrProviderContext } from '../ftr_provider_context';
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('Execution context', function () {
     this.tags('ciGroup1');
-    loadTestFile(require.resolve('./execution_context'));
+    loadTestFile(require.resolve('./browser'));
+    loadTestFile(require.resolve('./server'));
   });
 }
