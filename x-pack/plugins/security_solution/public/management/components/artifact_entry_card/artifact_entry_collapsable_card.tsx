8fe778032632bc3275060f02cdd5a8c6cabc9c8e
Paul Tavares <paul.tavares@elastic.co>
2021-09-14 16:16:03 -0400

    Created CardCompressedHeader + added handling for responsive mode

--- x-pack/plugins/security_solution/public/management/components/artifact_entry_card/artifact_entry_collapsable_card.tsx
+++ x-pack/plugins/security_solution/public/management/components/artifact_entry_card/artifact_entry_collapsable_card.tsx
@@ -5,20 +5,15 @@
  * 2.0.
  */
 
-import React, { memo, useCallback, useMemo } from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiHorizontalRule } from '@elastic/eui';
-import classNames from 'classnames';
+import React, { memo } from 'react';
+import { EuiHorizontalRule } from '@elastic/eui';
 import { ArtifactEntryCardProps } from './artifact_entry_card';
 import { CardContainerPanel } from './components/card_container_panel';
 import { useNormalizedArtifact } from './hooks/use_normalized_artifact';
 import { useTestIdGenerator } from '../hooks/use_test_id_generator';
-import { usePolicyNavLinks } from './hooks/use_policy_nav_links';
 import { CardSectionPanel } from './components/card_section_panel';
-import { TextValueDisplay } from './components/text_value_display';
-import { EffectScope } from './components/effect_scope';
-import { CardExpandButton } from './components/card_expand_button';
-import { CardActionsFlexItem } from './components/card_actions_flex_item';
 import { CriteriaConditions, CriteriaConditionsProps } from './components/criteria_conditions';
+import { CardCompressedHeader } from './components/card_compressed_header';
 
 export interface ArtifactEntryCollapsableCardProps extends ArtifactEntryCardProps {
   onExpandCollapse: () => void;
@@ -37,46 +32,18 @@ export const ArtifactEntryCollapsableCard = memo<ArtifactEntryCollapsableCardPro
   }) => {
     const artifact = useNormalizedArtifact(item);
     const getTestId = useTestIdGenerator(dataTestSubj);
-    const policyNavLinks = usePolicyNavLinks(artifact, policies);
-
-    const cssClassNames = useMemo(() => {
-      return classNames({
-        'eui-textTruncate': !expanded,
-      });
-    }, [expanded]);
-
-    const handleExpandCollapseClick = useCallback(() => {
-      onExpandCollapse();
-    }, [onExpandCollapse]);
 
     return (
       <CardContainerPanel {...commonProps} data-test-subj={dataTestSubj}>
         <CardSectionPanel>
-          <EuiFlexGroup alignItems="center">
-            <EuiFlexItem grow={false}>
-              <CardExpandButton
-                expanded={expanded}
-                onClick={handleExpandCollapseClick}
-                data-test-subj={getTestId('expandCollapse')}
-              />
-            </EuiFlexItem>
-            <EuiFlexItem grow={2} className={cssClassNames} data-test-subj={getTestId('title')}>
-              <TextValueDisplay bold truncate={!expanded}>
-                {artifact.name}
-              </TextValueDisplay>
-            </EuiFlexItem>
-            <EuiFlexItem
-              grow={3}
-              className={cssClassNames}
-              data-test-subj={getTestId('description')}
-            >
-              <TextValueDisplay truncate={!expanded}>{artifact.description}</TextValueDisplay>
-            </EuiFlexItem>
-            <EuiFlexItem grow={1}>
-              <EffectScope policies={policyNavLinks} data-test-subj={getTestId('effectScope')} />
-            </EuiFlexItem>
-            <CardActionsFlexItem actions={actions} data-test-subj={getTestId('actions')} />
-          </EuiFlexGroup>
+          <CardCompressedHeader
+            artifact={artifact}
+            actions={actions}
+            policies={policies}
+            expanded={expanded}
+            onExpandCollapse={onExpandCollapse}
+            data-test-subj={getTestId('header')}
+          />
         </CardSectionPanel>
         {expanded && (
           <>
---
+++ x-pack/plugins/security_solution/public/management/components/artifact_entry_card/components/card_compressed_header.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { memo, useCallback } from 'react';
+import { CommonProps, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { CardExpandButton } from './card_expand_button';
+import { TextValueDisplay } from './text_value_display';
+import { EffectScope } from './effect_scope';
+import { CardActionsFlexItem } from './card_actions_flex_item';
+import { ArtifactInfo } from '../types';
+import { ArtifactEntryCollapsableCardProps } from '../artifact_entry_collapsable_card';
+import { useTestIdGenerator } from '../../hooks/use_test_id_generator';
+import { useCollapsedCssClassNames } from '../hooks/use_collapsed_css_class_names';
+import { usePolicyNavLinks } from '../hooks/use_policy_nav_links';
+
+export interface CardCompressedHeaderProps
+  extends Pick<CommonProps, 'data-test-subj'>,
+    Pick<
+      ArtifactEntryCollapsableCardProps,
+      'onExpandCollapse' | 'expanded' | 'actions' | 'policies'
+    > {
+  artifact: ArtifactInfo;
+}
+
+export const CardCompressedHeader = memo<CardCompressedHeaderProps>(
+  ({
+    artifact,
+    onExpandCollapse,
+    policies,
+    actions,
+    expanded = false,
+    'data-test-subj': dataTestSubj,
+  }) => {
+    const getTestId = useTestIdGenerator(dataTestSubj);
+    const cssClassNames = useCollapsedCssClassNames(expanded);
+    const policyNavLinks = usePolicyNavLinks(artifact, policies);
+
+    const handleExpandCollapseClick = useCallback(() => {
+      onExpandCollapse();
+    }, [onExpandCollapse]);
+
+    return (
+      <EuiFlexGroup responsive={false} alignItems="center" data-test-subj={dataTestSubj}>
+        <EuiFlexItem grow={false}>
+          <CardExpandButton
+            expanded={expanded}
+            onClick={handleExpandCollapseClick}
+            data-test-subj={getTestId('expandCollapse')}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem className={cssClassNames}>
+          <EuiFlexGroup alignItems="center">
+            <EuiFlexItem grow={2} className={cssClassNames} data-test-subj={getTestId('title')}>
+              <TextValueDisplay bold truncate={!expanded}>
+                {artifact.name}
+              </TextValueDisplay>
+            </EuiFlexItem>
+            <EuiFlexItem
+              grow={3}
+              className={cssClassNames}
+              data-test-subj={getTestId('description')}
+            >
+              <TextValueDisplay truncate={!expanded}>{artifact.description}</TextValueDisplay>
+            </EuiFlexItem>
+            <EuiFlexItem grow={1}>
+              <EffectScope policies={policyNavLinks} data-test-subj={getTestId('effectScope')} />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+        <CardActionsFlexItem actions={actions} data-test-subj={getTestId('actions')} />
+      </EuiFlexGroup>
+    );
+  }
+);
+CardCompressedHeader.displayName = 'CardCompressedHeader';
