4c03754e5d7df3e455e71c8dc2f207bee5509100
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2021-09-14 11:57:42 +0300

    [Datatable] Removes the old implementation  (#111339)
    * [Datatable] Removes the angular implementation
    
    * Fix i18n errors
    
    * Fix types
    
    * Remove unecessary funtional tests
    
    * Fix documentation
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	scripts/functional_tests.js

--- scripts/functional_tests.js
+++ scripts/functional_tests.js
@@ -16,6 +16,15 @@ require('@kbn/test').runTestsCli([
   require.resolve('../test/examples/config.js'),
   require.resolve('../test/new_visualize_flow/config.ts'),
   require.resolve('../test/security_functional/config.ts'),
+<<<<<<< HEAD
   require.resolve('../test/functional/config.legacy.ts'),
+=======
+];
+// eslint-disable-next-line no-restricted-syntax
+const onlyNotInCoverageTests = [
+  require.resolve('../test/api_integration/config.js'),
+  require.resolve('../test/interpreter_functional/config.ts'),
+  require.resolve('../test/examples/config.js'),
+>>>>>>> 182533f5650... [Datatable] Removes the old implementation  (#111339)
   require.resolve('../test/functional_execution_context/config.ts'),
 ]);
