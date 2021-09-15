b88a76fce6964f23ee7c7e105fd3f65c4ae1b4f0
1Copenut <trevor.pierce@elastic.co>
2021-09-15 13:15:30 -0500

    Finished refactoring the Request Selector to use EUI Combobox
    * Removed three helper methods for the EUIPopover.
      * `togglePopover()`
      * `closePopover()`
      * `renderRequestDropdownItem()`
    * Removed the local state object and interface (no longer needed)
    * Renamed the const `options` to `selectedOptions` in `handleSelectd()`
      method to better reflect where the options array was coming from.

--- src/plugins/inspector/public/views/requests/components/request_selector.tsx
+++ src/plugins/inspector/public/views/requests/components/request_selector.tsx
@@ -13,25 +13,16 @@ import { i18n } from '@kbn/i18n';
 
 import {
   EuiBadge,
-  EuiButtonEmpty,
   EuiComboBox,
   EuiComboBoxOptionOption,
-  EuiContextMenuPanel,
-  EuiContextMenuItem,
   EuiFlexGroup,
   EuiFlexItem,
   EuiLoadingSpinner,
-  EuiPopover,
-  EuiTextColor,
   EuiToolTip,
 } from '@elastic/eui';
 
 import { RequestStatus } from '../../../../common/adapters';
 import { Request } from '../../../../common/adapters/request/types';
-
-interface RequestSelectorState {
-  isPopoverOpen: boolean;
-}
 
 interface RequestSelectorProps {
   requests: Request[];
@@ -39,112 +30,32 @@ interface RequestSelectorProps {
   onRequestChanged: (request: Request) => void;
 }
 
-export class RequestSelector extends Component<RequestSelectorProps, RequestSelectorState> {
+export class RequestSelector extends Component<RequestSelectorProps> {
   static propTypes = {
     requests: PropTypes.array.isRequired,
     selectedRequest: PropTypes.object.isRequired,
     onRequestChanged: PropTypes.func,
   };
 
-  state = {
-    isPopoverOpen: false,
-  };
-
   handleSelected = (selectedOptions: Array<EuiComboBoxOptionOption<string>>) => {
-    const selectedOption = this.props.requests.find(request => request.id === selectedOptions[0].value);
-    console.log({selectedOptions})
+    const selectedOption = this.props.requests.find(
+      (request) => request.id === selectedOptions[0].value
+    );
 
-    if(selectedOption) {
+    if (selectedOption) {
       this.props.onRequestChanged(selectedOption);
     }
-  }
-
-  togglePopover = () => {
-    this.setState((prevState: RequestSelectorState) => ({
-      isPopoverOpen: !prevState.isPopoverOpen,
-    }));
-  };
-
-  closePopover = () => {
-    this.setState({
-      isPopoverOpen: false,
-    });
-  };
-
-  renderRequestDropdownItem = (request: Request, index: number) => {
-    const hasFailed = request.status === RequestStatus.ERROR;
-    const inProgress = request.status === RequestStatus.PENDING;
-
-    return (
-      <EuiContextMenuItem
-        key={index}
-        icon={request === this.props.selectedRequest ? 'check' : 'empty'}
-        onClick={() => {
-          this.props.onRequestChanged(request);
-          this.closePopover();
-        }}
-        toolTipContent={request.description}
-        toolTipPosition="left"
-        data-test-subj={`inspectorRequestChooser${request.name}`}
-      >
-        <EuiTextColor color={hasFailed ? 'danger' : 'default'}>
-          {request.name}
-
-          {hasFailed && (
-            <FormattedMessage id="inspector.requests.failedLabel" defaultMessage=" (failed)" />
-          )}
-
-          {inProgress && (
-            <EuiLoadingSpinner
-              size="s"
-              aria-label={i18n.translate('inspector.requests.requestInProgressAriaLabel', {
-                defaultMessage: 'Request in progress',
-              })}
-              className="insRequestSelector__menuSpinner"
-            />
-          )}
-        </EuiTextColor>
-      </EuiContextMenuItem>
-    );
   };
 
   renderRequestCombobox() {
-    // const button = (
-    //   <EuiButtonEmpty
-    //     iconType="arrowDown"
-    //     iconSide="right"
-    //     size="s"
-    //     onClick={this.togglePopover}
-    //     data-test-subj="inspectorRequestChooser"
-    //   >
-    //     {this.props.selectedRequest.name}
-    //   </EuiButtonEmpty>
-    // );
-
-    const options = this.props.requests.map(item => {
+    const options = this.props.requests.map((item) => {
       return {
         label: item.name,
         value: item.id,
-      }
+      };
     });
 
     return (
-      <>
-        {/* <EuiPopover
-          id="inspectorRequestChooser"
-          button={button}
-          isOpen={this.state.isPopoverOpen}
-          closePopover={this.closePopover}
-          panelPaddingSize="none"
-          anchorPosition="downLeft"
-          repositionOnScroll
-        >
-          <EuiContextMenuPanel
-            items={this.props.requests.map(this.renderRequestDropdownItem)}
-            data-test-subj="inspectorRequestChooserMenuPanel"
-          />
-        </EuiPopover> */}
-
       <EuiComboBox
         id="inspectorRequestChooser"
         isClearable={false}
@@ -152,17 +63,14 @@ export class RequestSelector extends Component<RequestSelectorProps, RequestSele
         onChange={this.handleSelected}
         options={options}
         prepend="Request"
-        selectedOptions={
-          [
-            {
-              label: this.props.selectedRequest.name,
-              value: this.props.selectedRequest.id
-            }
-          ]
-        }
+        selectedOptions={[
+          {
+            label: this.props.selectedRequest.name,
+            value: this.props.selectedRequest.id,
+          },
+        ]}
         singleSelection={{ asPlainText: true }}
       />
-      </>
     );
   }
 
@@ -171,21 +79,7 @@ export class RequestSelector extends Component<RequestSelectorProps, RequestSele
 
     return (
       <EuiFlexGroup alignItems="center">
-        <EuiFlexItem grow={true}>
-          {/* {requests.length <= 1 && (
-            <div
-              className="insRequestSelector__singleRequest"
-              data-test-subj="inspectorRequestName"
-            >
-              <strong>
-                <FormattedMessage id="inspector.requests.requestLabel" defaultMessage="Request:" />{" "}
-              </strong>
-              {selectedRequest.name}
-            </div>
-          )}
-          {requests.length > 1 && this.renderRequestDropdown()} */}
-          {requests.length && this.renderRequestCombobox()}
-        </EuiFlexItem>
+        <EuiFlexItem grow={true}>{requests.length && this.renderRequestCombobox()}</EuiFlexItem>
         <EuiFlexItem grow={false}>
           {selectedRequest.status !== RequestStatus.PENDING && (
             <EuiToolTip
