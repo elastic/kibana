1fac110fcec0a044fb382e7ed530946386a7250a
Matthew Kime <matt@mattki.me>
2021-09-15 11:29:23 -0500

    convert deep imports to top level imports (#112202)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/maps/server/kibana_server_services.ts
+++ x-pack/plugins/maps/server/kibana_server_services.ts
@@ -7,9 +7,10 @@
 
 import { ElasticsearchClient, ISavedObjectsRepository } from 'kibana/server';
 import { SavedObjectsClient } from '../../../../src/core/server';
-import { IndexPatternsCommonService } from '../../../../src/plugins/data/server';
-// eslint-disable-next-line @kbn/eslint/no-restricted-paths
-import { IndexPatternsServiceStart } from '../../../../src/plugins/data/server/index_patterns';
+import {
+  IndexPatternsCommonService,
+  IndexPatternsServiceStart,
+} from '../../../../src/plugins/data/server';
 
 let internalRepository: ISavedObjectsRepository;
 export const setInternalRepository = (
