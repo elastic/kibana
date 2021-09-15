d039288a287f246508b4d4dec26a708c951a6ad8
Luke Elmers <luke.elmers@elastic.co>
2021-09-15 11:35:25 -0600

    Update security jest tests.

--- x-pack/plugins/security/server/config_deprecations.test.ts
+++ x-pack/plugins/security/server/config_deprecations.test.ts
@@ -342,17 +342,33 @@ describe('Config Deprecations', () => {
     expect(migrated).toEqual(config);
     expect(messages).toMatchInlineSnapshot(`
       Array [
+        "Configuring \\"xpack.security.enabled\\" is deprecated and will be removed in 8.0.0.",
         "Disabling the security plugin \\"xpack.security.enabled\\" will only be supported by disable security in Elasticsearch.",
       ]
     `);
   });
 
-  it('does not warn when the security plugin is enabled', () => {
+  it('warns when the security plugin is enabled', () => {
     const config = {
       xpack: {
         security: {
           enabled: true,
         },
+      },
+    };
+    const { messages, migrated } = applyConfigDeprecations(cloneDeep(config));
+    expect(migrated).toEqual(config);
+    expect(messages).toMatchInlineSnapshot(`
+      Array [
+        "Configuring \\"xpack.security.enabled\\" is deprecated and will be removed in 8.0.0.",
+      ]
+    `);
+  });
+
+  it('does not warn with default config', () => {
+    const config = {
+      xpack: {
+        security: {},
       },
     };
     const { messages, migrated } = applyConfigDeprecations(cloneDeep(config));
