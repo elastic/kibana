c6b5475e4576e9592c44ecc05d351f303379cb6d
Kevin Qualters <kevin.qualters@elastic.co>
2021-09-15 13:42:41 -0400

    Fix template literal types used in siem

--- x-pack/plugins/timelines/public/components/t_grid/styles.tsx
+++ x-pack/plugins/timelines/public/components/t_grid/styles.tsx
@@ -281,7 +281,7 @@ export const EventsTrSupplementContainer = styled.div.attrs<WidthProp>(({ width 
 }))<WidthProp>``;
 
 export const EventsTrSupplement = styled.div.attrs(({ className = '' }) => ({
-  className: `siemEventsTable__trSupplement ${className}`,
+  className: `siemEventsTable__trSupplement ${className}` as string,
 }))<{ className: string }>`
   font-size: ${({ theme }) => theme.eui.euiFontSizeXS};
   line-height: ${({ theme }) => theme.eui.euiLineHeight};
@@ -409,7 +409,7 @@ export const EventsHeadingTitleSpan = styled.span.attrs(({ className }) => ({
 `;
 
 export const EventsHeadingExtra = styled.div.attrs(({ className = '' }) => ({
-  className: `siemEventsHeading__extra ${className}`,
+  className: `siemEventsHeading__extra ${className}` as string,
 }))`
   margin-left: auto;
   margin-right: 2px;
