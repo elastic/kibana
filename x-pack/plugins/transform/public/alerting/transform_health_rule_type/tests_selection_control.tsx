bbad4833eccbd568fdeddfac98b213a5e84a9cd7
Dima Arnautov <dmitrii.arnautov@elastic.co>
2021-09-16 16:08:03 +0200

    [ML] disable control

--- x-pack/plugins/transform/public/alerting/transform_health_rule_type/tests_selection_control.tsx
+++ x-pack/plugins/transform/public/alerting/transform_health_rule_type/tests_selection_control.tsx
@@ -52,6 +52,7 @@ export const TestsSelectionControl: FC<TestsSelectionControlProps> = React.memo(
               >
                 <EuiFormRow>
                   <EuiSwitch
+                    disabled
                     label={
                       <FormattedMessage
                         id="xpack.transform.alertTypes.transformHealth.testsSelection.enableTestLabel"
