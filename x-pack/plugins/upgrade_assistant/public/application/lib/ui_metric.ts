f483ab79d229551239231fe4da65c35558165cf5
Alison Goryachev <alisonmllr20@gmail.com>
2021-09-16 10:27:47 -0400

    add new ui counters to UA

--- x-pack/plugins/upgrade_assistant/public/application/lib/ui_metric.ts
+++ x-pack/plugins/upgrade_assistant/public/application/lib/ui_metric.ts
@@ -16,6 +16,15 @@ export const UIM_REINDEX_OPEN_FLYOUT_CLICK = 'reindex_open_flyout_click';
 export const UIM_REINDEX_CLOSE_FLYOUT_CLICK = 'reindex_close_flyout_click';
 export const UIM_REINDEX_START_CLICK = 'reindex_start_click';
 export const UIM_REINDEX_STOP_CLICK = 'reindex_stop_click';
+export const UIM_BACKUP_DATA_CLOUD_CLICK = 'backup_data_cloud_click';
+export const UIM_BACKUP_DATA_ON_PREM_CLICK = 'backup_data_on_prem_click';
+export const UIM_RESET_LOGS_COUNTER_CLICK = 'reset_logs_counter_click';
+export const UIM_OBSERVABILITY_CLICK = 'observability_click';
+export const UIM_DISCOVER_CLICK = 'discover_click';
+export const UIM_ML_SNAPSHOT_UPGRADE_CLICK = 'ml_snapshot_upgrade_click';
+export const UIM_ML_SNAPSHOT_DELETE_CLICK = 'ml_snapshot_delete_click';
+export const UIM_INDEX_SETTINGS_DELETE_CLICK = 'index_settings_delete_click';
+export const UIM_KIBANA_QUICK_RESOLVE_CLICK = 'kibana_quick_resolve_click';
 
 export class UiMetricService {
   private usageCollection: UsageCollectionSetup | undefined;
