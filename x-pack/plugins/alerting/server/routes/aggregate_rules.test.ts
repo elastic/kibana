535070357d37a2873fa4c63bfc96133ad5b95f86
Chris Roberson <chrisronline@gmail.com>
2021-09-20 13:59:27 -0400

    [Alerting] Add telemetry for HTTP API calls with legacy terminology (#112173)
    * Deprecated attributes telemetry
    
    * Add additional term and improve test quality
    
    * PR feedback

--- x-pack/plugins/alerting/server/routes/aggregate_rules.test.ts
+++ x-pack/plugins/alerting/server/routes/aggregate_rules.test.ts
@@ -11,11 +11,19 @@ import { licenseStateMock } from '../lib/license_state.mock';
 import { verifyApiAccess } from '../lib/license_api_access';
 import { mockHandlerArguments } from './_mock_handler_arguments';
 import { rulesClientMock } from '../rules_client.mock';
+import { trackLegacyTerminology } from './lib/track_legacy_terminology';
+import { usageCountersServiceMock } from 'src/plugins/usage_collection/server/usage_counters/usage_counters_service.mock';
 
 const rulesClient = rulesClientMock.create();
+const mockUsageCountersSetup = usageCountersServiceMock.createSetupContract();
+const mockUsageCounter = mockUsageCountersSetup.createUsageCounter('test');
 
 jest.mock('../lib/license_api_access.ts', () => ({
   verifyApiAccess: jest.fn(),
+}));
+
+jest.mock('./lib/track_legacy_terminology', () => ({
+  trackLegacyTerminology: jest.fn(),
 }));
 
 beforeEach(() => {
@@ -146,5 +154,40 @@ describe('aggregateRulesRoute', () => {
     expect(handler(context, req, res)).rejects.toMatchInlineSnapshot(`[Error: OMG]`);
 
     expect(verifyApiAccess).toHaveBeenCalledWith(licenseState);
+  });
+
+  it('should track calls with deprecated param values', async () => {
+    const licenseState = licenseStateMock.create();
+    const router = httpServiceMock.createRouter();
+
+    aggregateRulesRoute(router, licenseState, mockUsageCounter);
+    const aggregateResult = {
+      alertExecutionStatus: {
+        ok: 15,
+        error: 2,
+        active: 23,
+        pending: 1,
+        unknown: 0,
+      },
+    };
+    rulesClient.aggregate.mockResolvedValueOnce(aggregateResult);
+    const [, handler] = router.get.mock.calls[0];
+    const [context, req, res] = mockHandlerArguments(
+      { rulesClient },
+      {
+        params: {},
+        query: {
+          search_fields: ['alertTypeId:1', 'message:foo'],
+          search: 'alertTypeId:2',
+        },
+      },
+      ['ok']
+    );
+    await handler(context, req, res);
+    expect(trackLegacyTerminology).toHaveBeenCalledTimes(1);
+    expect((trackLegacyTerminology as jest.Mock).mock.calls[0][0]).toStrictEqual([
+      'alertTypeId:2',
+      ['alertTypeId:1', 'message:foo'],
+    ]);
   });
 });
