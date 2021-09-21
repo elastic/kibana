b7d622a17b2108c54e7a1bffc44916c445a2e900
1Copenut <trevor.pierce@elastic.co>
2021-09-21 10:04:50 -0500

    Updating one functional test for Discover > Data Grid

--- test/functional/apps/discover/_data_grid.ts
+++ test/functional/apps/discover/_data_grid.ts
@@ -39,19 +39,19 @@ export default function ({
       const getTitles = async () =>
         (await testSubjects.getVisibleText('dataGridHeader')).replace(/\s|\r?\n|\r/g, ' ');
 
-      expect(await getTitles()).to.be('Time (@timestamp) Document');
+      expect(await getTitles()).to.be('Control column Select column Time (@timestamp) Document');
 
       await PageObjects.discover.clickFieldListItemAdd('bytes');
-      expect(await getTitles()).to.be('Time (@timestamp) bytes');
+      expect(await getTitles()).to.be('Control column Select column Time (@timestamp) bytes');
 
       await PageObjects.discover.clickFieldListItemAdd('agent');
-      expect(await getTitles()).to.be('Time (@timestamp) bytes agent');
+      expect(await getTitles()).to.be('Control column Select column Time (@timestamp) bytes agent');
 
       await PageObjects.discover.clickFieldListItemRemove('bytes');
-      expect(await getTitles()).to.be('Time (@timestamp) agent');
+      expect(await getTitles()).to.be('Control column Select column Time (@timestamp) agent');
 
       await PageObjects.discover.clickFieldListItemRemove('agent');
-      expect(await getTitles()).to.be('Time (@timestamp) Document');
+      expect(await getTitles()).to.be('Control column Select column Time (@timestamp) Document');
     });
   });
 }
