66323d05a26e2ed9a991ee85cc29b6ab72c6c809
Mat Schaffer <mat@elastic.co>
2021-09-22 01:31:02 +0900

    [Stack Monitoring][Angular removal] No-data page (#110432)
    * Basic no-data page
    
    * Rename to NoDataPage
    
    * Add getData property to pass into EuiSuperDatePicker from pages
    
    * Wire getData and redirect for no data page
    
    * Draft port of isLoading & model updating
    
    * Add todo on handling checkers
    
    * Switch to model as state object
    
    * Add checkers
    
    * Porting enabler
    
    * Fix build checks
    
    * Attempting to smooth out enablement
    
    * Clean up CI errors
    
    * Fix breadcrumbs
    
    * Fix linter warning
    
    * Fix checkers dependency (I hope)
    
    * Hook up catchReason
    
    * Add a stub for react setup mode
    
    * Clean warnings
    
    * Fix toggleSetupMode by calling initSetupModeState first
    
    * Translating checker strings
    
    * typo on "xpack"
    
    * Move isCollection/reason check in NoData
    
    This replicates how the angular app did selective re-rendering of the react component, but while still being able to render the component.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/monitoring/public/application/index.tsx
+++ x-pack/plugins/monitoring/public/application/index.tsx
@@ -18,6 +18,7 @@ import { GlobalStateProvider } from './global_state_context';
 import { ExternalConfigContext, ExternalConfig } from './external_config_context';
 import { createPreserveQueryHistory } from './preserve_query_history';
 import { RouteInit } from './route_init';
+import { NoDataPage } from './pages/no_data';
 import { ElasticsearchOverviewPage } from './pages/elasticsearch/overview';
 import { CODE_PATH_ELASTICSEARCH } from '../../common/constants';
 import { MonitoringTimeContainer } from './hooks/use_monitoring_time';
@@ -54,7 +55,7 @@ const MonitoringApp: React.FC<{
             <BreadcrumbContainer.Provider history={history}>
               <Router history={history}>
                 <Switch>
-                  <Route path="/no-data" component={NoData} />
+                  <Route path="/no-data" component={NoDataPage} />
                   <Route path="/loading" component={LoadingPage} />
                   <RouteInit
                     path="/license"
@@ -96,10 +97,6 @@ const MonitoringApp: React.FC<{
       </ExternalConfigContext.Provider>
     </KibanaContextProvider>
   );
-};
-
-const NoData: React.FC<{}> = () => {
-  return <div>No data page</div>;
 };
 
 const Home: React.FC<{}> = () => {
