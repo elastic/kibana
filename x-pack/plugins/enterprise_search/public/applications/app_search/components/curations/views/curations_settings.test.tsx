ae608304a4745025bed1084bbe505a41ea69a19e
Byron Hulcher <byronhulcher@gmail.com>
2021-09-21 15:03:31 -0400

    Added form elements to CurationsSuggestions component

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/views/curations_settings.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/views/curations_settings.test.tsx
@@ -8,20 +8,110 @@
 import '../../../../__mocks__/react_router';
 import '../../../__mocks__/engine_logic.mock';
 
+import { setMockActions, setMockValues } from '../../../../__mocks__/kea_logic';
+
 import React from 'react';
 
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
+
+import { EuiSwitch } from '@elastic/eui';
+
+import { mountWithIntl } from '@kbn/test/jest';
 
 import { CurationsSettings } from './curations_settings';
+
+const MOCK_VALUES = {
+  dataLoading: false,
+  curationsSettings: {
+    enabled: true,
+    mode: 'automatic',
+  },
+};
+
+const MOCK_ACTIONS = {
+  loadCurationsSettings: jest.fn(),
+  toggleCurationsEnabled: jest.fn(),
+  toggleCurationsMode: jest.fn(),
+};
 
 describe('CurationsSettings', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    setMockActions(MOCK_ACTIONS);
   });
 
-  it('renders empty', () => {
+  it('loads curations settings on load', () => {
+    setMockValues(MOCK_VALUES);
+    mountWithIntl(<CurationsSettings />);
+
+    expect(MOCK_ACTIONS.loadCurationsSettings).toHaveBeenCalledTimes(1);
+  });
+
+  it('contains a switch to toggle curations settings', () => {
+    let wrapper: ShallowWrapper;
+
+    setMockValues({
+      ...MOCK_VALUES,
+      curationsSettings: { ...MOCK_VALUES.curationsSettings, enabled: true },
+    });
+    wrapper = shallow(<CurationsSettings />);
+
+    expect(wrapper.find(EuiSwitch).at(0).prop('checked')).toBe(true);
+
+    setMockValues({
+      ...MOCK_VALUES,
+      curationsSettings: { ...MOCK_VALUES.curationsSettings, enabled: false },
+    });
+    wrapper = shallow(<CurationsSettings />);
+
+    expect(wrapper.find(EuiSwitch).at(0).prop('checked')).toBe(false);
+
+    wrapper.find(EuiSwitch).at(0).simulate('change');
+    expect(MOCK_ACTIONS.toggleCurationsEnabled).toHaveBeenCalled();
+  });
+
+  it('contains a switch to toggle the curations mode', () => {
+    let wrapper: ShallowWrapper;
+
+    setMockValues({
+      ...MOCK_VALUES,
+      curationsSettings: { ...MOCK_VALUES.curationsSettings, mode: 'automated' },
+    });
+    wrapper = shallow(<CurationsSettings />);
+
+    expect(wrapper.find(EuiSwitch).at(1).prop('checked')).toBe(true);
+
+    setMockValues({
+      ...MOCK_VALUES,
+      curationsSettings: { ...MOCK_VALUES.curationsSettings, mode: 'manual' },
+    });
+    wrapper = shallow(<CurationsSettings />);
+
+    expect(wrapper.find(EuiSwitch).at(1).prop('checked')).toBe(false);
+
+    wrapper.find(EuiSwitch).at(1).simulate('change');
+    expect(MOCK_ACTIONS.toggleCurationsMode).toHaveBeenCalled();
+  });
+
+  it('disable both switches when data is loading', () => {
+    setMockValues({
+      ...MOCK_VALUES,
+      dataLoading: true,
+    });
     const wrapper = shallow(<CurationsSettings />);
 
-    expect(wrapper.isEmptyRender()).toBe(true);
+    expect(wrapper.find(EuiSwitch).at(0).prop('disabled')).toBe(true);
+    expect(wrapper.find(EuiSwitch).at(1).prop('disabled')).toBe(true);
+  });
+
+  it('enables both switches when data is done loading', () => {
+    setMockValues({
+      ...MOCK_VALUES,
+      dataLoading: false,
+    });
+    const wrapper = shallow(<CurationsSettings />);
+
+    expect(wrapper.find(EuiSwitch).at(0).prop('disabled')).toBe(false);
+    expect(wrapper.find(EuiSwitch).at(1).prop('disabled')).toBe(false);
   });
 });
--- x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/views/curations_settings.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/views/curations_settings.tsx
@@ -5,8 +5,71 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useActions, useValues } from 'kea';
+
+import { EuiSwitch, EuiText, EuiTitle } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+
+import { CurationsSettingsLogic } from './curations_settings_logic';
 
 export const CurationsSettings: React.FC = () => {
-  return null;
+  const {
+    curationsSettings: { enabled, mode },
+    dataLoading,
+  } = useValues(CurationsSettingsLogic);
+  const { loadCurationsSettings, toggleCurationsEnabled, toggleCurationsMode } = useActions(
+    CurationsSettingsLogic
+  );
+
+  useEffect(() => {
+    loadCurationsSettings();
+  }, []);
+
+  return (
+    <>
+      <EuiTitle size="s">
+        <h2>
+          {i18n.translate(
+            'xpack.enterpriseSearch.appSearch.curations.settings.automatedCurationsTitle',
+            {
+              defaultMessage: 'Curations',
+            }
+          )}
+        </h2>
+      </EuiTitle>
+      <EuiText>
+        {i18n.translate(
+          'xpack.enterpriseSearch.appSearch.curations.settings.automatedCurationsDescription',
+          {
+            defaultMessage:
+              "Suggesssted curations will monitor your engine's analytics and make automatic suggestions to help you deliver the most relevant results. Each suggested curation can be accepted, rejected, or modified.",
+          }
+        )}
+      </EuiText>
+      <EuiSwitch
+        label={i18n.translate(
+          'xpack.enterpriseSearch.appSearch.curations.settings.enableAutomatedCurationsSwitchLabel',
+          {
+            defaultMessage: 'Enable automation suggestions',
+          }
+        )}
+        checked={enabled}
+        disabled={dataLoading}
+        onChange={toggleCurationsEnabled}
+      />
+      <EuiSwitch
+        label={i18n.translate(
+          'xpack.enterpriseSearch.appSearch.curations.settings.acceptNewSuggesstionsSwitchLabel',
+          {
+            defaultMessage: 'Automatically accept new suggestions',
+          }
+        )}
+        checked={mode === 'automated'}
+        disabled={dataLoading}
+        onChange={toggleCurationsMode}
+      />
+    </>
+  );
 };
