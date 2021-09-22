8e84b09f6f92978080e634cb919ac682507bb3d9
Ignacio Rivas <rivasign@gmail.com>
2021-09-22 08:44:06 +0200

    Change count poll time to 15s (#112669)

--- x-pack/plugins/upgrade_assistant/common/constants.ts
+++ x-pack/plugins/upgrade_assistant/common/constants.ts
@@ -35,4 +35,4 @@ export const DEPRECATION_LOGS_INDEX = '.logs-deprecation.elasticsearch-default';
 export const DEPRECATION_LOGS_INDEX_PATTERN = '.logs-deprecation.elasticsearch-default';
 
 export const CLOUD_BACKUP_STATUS_POLL_INTERVAL_MS = 60000;
-export const DEPRECATION_LOGS_COUNT_POLL_INTERVAL_MS = 60000;
+export const DEPRECATION_LOGS_COUNT_POLL_INTERVAL_MS = 15000;
