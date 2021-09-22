a52209f545541fb430a43b776fd7f18bf441c3ee
Lukas Olson <olson.lukas@gmail.com>
2021-09-22 13:36:17 -0700

    [data.search] Do not send ignore_throttled when search:includeFrozen is disabled (#112755)
    * Do not send ignore_throttled when search:includeFrozen is disabled
    
    * Fix tests

--- src/plugins/data/server/search/strategies/eql_search/eql_search_strategy.test.ts
+++ src/plugins/data/server/search/strategies/eql_search/eql_search_strategy.test.ts
@@ -132,7 +132,6 @@ describe('EQL search strategy', () => {
         expect(request).toEqual(
           expect.objectContaining({
             ignore_unavailable: true,
-            ignore_throttled: true,
           })
         );
       });
