11f749e0c073f7785933e8a03625dcfcf0cbc9a2
Anton Dosov <anton.dosov@elastic.co>
2021-09-23 13:31:27 +0200

    lazy load handlebars

--- x-pack/plugins/ui_actions_enhanced/public/index.ts
+++ x-pack/plugins/ui_actions_enhanced/public/index.ts
@@ -5,9 +5,6 @@
  * 2.0.
  */
 
-// TODO: https://github.com/elastic/kibana/issues/109891
-/* eslint-disable @kbn/eslint/no_export_all */
-
 import { PluginInitializerContext } from '../../../../src/core/public';
 import { AdvancedUiActionsPublicPlugin } from './plugin';
 
@@ -42,4 +39,13 @@ export {
   DrilldownDefinition as UiActionsEnhancedDrilldownDefinition,
   DrilldownTemplate as UiActionsEnhancedDrilldownTemplate,
 } from './drilldowns';
-export * from './drilldowns/url_drilldown';
+export {
+  urlDrilldownCompileUrl,
+  UrlDrilldownCollectConfig,
+  UrlDrilldownConfig,
+  UrlDrilldownGlobalScope,
+  urlDrilldownGlobalScopeProvider,
+  UrlDrilldownScope,
+  urlDrilldownValidateUrl,
+  urlDrilldownValidateUrlTemplate,
+} from './drilldowns/url_drilldown';
