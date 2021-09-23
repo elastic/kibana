858b3f6d698f7c957d3ed36fbb8ff5b740e56298
Christos Nasikas <christos.nasikas@elastic.co>
2021-09-23 16:20:45 +0300

    Add value to config

--- x-pack/test/case_api_integration/common/config.ts
+++ x-pack/test/case_api_integration/common/config.ts
@@ -93,7 +93,7 @@ export function createTestConfig(name: string, options: CreateTestConfigOptions)
         .isDirectory()
     );
 
-    const casesConfig = ['--xpack.cases.enabled'];
+    const casesConfig = ['--xpack.cases.enabled=true'];
 
     return {
       testFiles: testFiles ? testFiles : [require.resolve('../tests/common')],
