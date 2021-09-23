3486320af284b3dd4b0cf72b1a31f9aa674f446d
shahzad31 <shahzad31comp@gmail.com>
2021-09-23 15:00:21 +0200

    fix create new case click

--- x-pack/plugins/observability/public/components/shared/exploratory_view/header/add_to_case_action.tsx
+++ x-pack/plugins/observability/public/components/shared/exploratory_view/header/add_to_case_action.tsx
@@ -7,90 +7,42 @@
 
 import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiLink } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import React, { useState } from 'react';
-import { HttpSetup } from 'kibana/public';
+import React, { useCallback } from 'react';
 import { toMountPoint, useKibana } from '../../../../../../../../src/plugins/kibana_react/public';
 import { ObservabilityAppServices } from '../../../../application/types';
 import { AllCasesSelectorModalProps } from '../../../../../../cases/public';
 import { TypedLensByValueInput } from '../../../../../../lens/public';
+import { useAddToCase } from '../hooks/use_add_to_case';
 import { Case, SubCase } from '../../../../../../cases/common';
 
-interface Props {
+export interface AddToCaseProps {
   timeRange: { from: string; to: string };
   lensAttributes: TypedLensByValueInput['attributes'] | null;
 }
 
-async function addToCase(
-  http: HttpSetup,
-  theCase: Case | SubCase,
-  attributes: TypedLensByValueInput['attributes'],
-  timeRange: { from: string; to: string }
-) {
-  const apiPath = `/api/cases/${theCase?.id}/comments`;
-
-  const vizPayload = {
-    attributes,
-    timeRange,
-  };
-
-  const payload = {
-    comment: `!{lens${JSON.stringify(vizPayload)}}`,
-    type: 'user',
-    owner: 'observability',
-  };
-
-  return http.post(apiPath, { body: JSON.stringify(payload) });
-}
-
-export function AddToCaseAction({ lensAttributes, timeRange }: Props) {
+export function AddToCaseAction({ lensAttributes, timeRange }: AddToCaseProps) {
   const kServices = useKibana<ObservabilityAppServices>().services;
 
-  const { cases, http, notifications } = kServices;
-  const [isCasesOpen, setIsCasesOpen] = useState(false);
-  const [isSaving, setIsSaving] = useState(false);
+  const { cases, http } = kServices;
+
+  const getToastText = useCallback(
+    (thaCase) => toMountPoint(<CaseToastText theCase={thaCase} basePath={http.basePath.get()} />),
+    [http.basePath]
+  );
+
+  const { createCaseUrl, goToCreateCase, onCaseClicked, isCasesOpen, setIsCasesOpen, isSaving } =
+    useAddToCase({
+      lensAttributes,
+      getToastText,
+      timeRange,
+    });
 
   const getAllCasesSelectorModalProps: AllCasesSelectorModalProps = {
     createCaseNavigation: {
-      href: '',
-      onClick: () => {},
+      href: createCaseUrl,
+      onClick: goToCreateCase,
     },
-    onRowClick: (theCase) => {
-      setIsCasesOpen(false);
-      if (theCase && lensAttributes) {
-        setIsSaving(true);
-        addToCase(http, theCase, lensAttributes, timeRange).then(() => {
-          setIsSaving(false);
-          notifications.toasts.addSuccess(
-            {
-              title: i18n.translate('xpack.observability.expView.heading.addToCase.notification', {
-                defaultMessage: 'Successfully added visualization to the case: {caseTitle}',
-                values: { caseTitle: theCase.title },
-              }),
-              text: toMountPoint(
-                <EuiFlexGroup justifyContent="center">
-                  <EuiFlexItem>
-                    <EuiLink
-                      href={`${http.basePath.get()}/app/observability/cases/${theCase.id}`}
-                      target="_blank"
-                    >
-                      {i18n.translate(
-                        'xpack.observability.expView.heading.addToCase.notification.viewCase',
-                        {
-                          defaultMessage: 'View case',
-                        }
-                      )}
-                    </EuiLink>
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              ),
-            },
-            {
-              toastLifeTimeMs: 10000,
-            }
-          );
-        });
-      }
-    },
+    onRowClick: onCaseClicked,
     userCanCrud: true,
     owner: ['observability'],
   };
@@ -115,5 +67,19 @@ export function AddToCaseAction({ lensAttributes, timeRange }: Props) {
         lensAttributes &&
         cases.getAllCasesSelectorModal(getAllCasesSelectorModalProps)}
     </>
+  );
+}
+
+function CaseToastText({ theCase, basePath }: { theCase: Case | SubCase; basePath: string }) {
+  return (
+    <EuiFlexGroup justifyContent="center">
+      <EuiFlexItem>
+        <EuiLink href={`${basePath}/app/observability/cases/${theCase.id}`} target="_blank">
+          {i18n.translate('xpack.observability.expView.heading.addToCase.notification.viewCase', {
+            defaultMessage: 'View case',
+          })}
+        </EuiLink>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 }
