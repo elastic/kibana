45d89c5c576578f8282d270c4885ab033162f05e
Kristof-Pierre Cummings <kristof@kristoferresmbp.myfiosgateway.com>
2021-09-23 12:30:10 -0500

    blah

--- x-pack/plugins/cases/public/components/create/connector.tsx
+++ x-pack/plugins/cases/public/components/create/connector.tsx
@@ -131,7 +131,7 @@ const ConnectorComponent: React.FC<Props> = ({
           component={ConnectorFields}
           componentProps={{
             connectors,
-            hideConnectorServiceNowSir,
+            hideConnectorServiceNowSir,//
             isEdit: true,
           }}
         />
