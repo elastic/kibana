a2c5787b5b090de6deb39d1b8e655591f3a224cb
Quynh Nguyen <quynh.nguyen@elastic.co>
2021-09-23 16:14:58 -0500

    [ML] Fix functional tests should be 0/0%

--- x-pack/test/functional/apps/ml/data_visualizer/index_data_visualizer.ts
+++ x-pack/test/functional/apps/ml/data_visualizer/index_data_visualizer.ts
@@ -92,7 +92,7 @@ export default function ({ getService }: FtrProviderContext) {
           aggregatable: false,
           loading: false,
           exampleCount: 1,
-          docCountFormatted: '',
+          docCountFormatted: '0 (0%)',
           viewableInLens: false,
         },
         {
@@ -122,7 +122,7 @@ export default function ({ getService }: FtrProviderContext) {
           aggregatable: false,
           loading: false,
           exampleCount: 1,
-          docCountFormatted: '',
+          docCountFormatted: '0 (0%)',
           viewableInLens: false,
         },
         {
@@ -188,7 +188,7 @@ export default function ({ getService }: FtrProviderContext) {
           aggregatable: false,
           loading: false,
           exampleCount: 1,
-          docCountFormatted: '',
+          docCountFormatted: '0 (0%)',
           viewableInLens: false,
         },
         {
@@ -218,7 +218,7 @@ export default function ({ getService }: FtrProviderContext) {
           aggregatable: false,
           loading: false,
           exampleCount: 1,
-          docCountFormatted: '',
+          docCountFormatted: '0 (0%)',
           viewableInLens: false,
         },
         {
@@ -284,7 +284,7 @@ export default function ({ getService }: FtrProviderContext) {
           aggregatable: false,
           loading: false,
           exampleCount: 1,
-          docCountFormatted: '',
+          docCountFormatted: '0 (0%)',
           viewableInLens: false,
         },
         {
@@ -314,7 +314,7 @@ export default function ({ getService }: FtrProviderContext) {
           aggregatable: false,
           loading: false,
           exampleCount: 1,
-          docCountFormatted: '',
+          docCountFormatted: '0 (0%)',
           viewableInLens: false,
         },
         {
