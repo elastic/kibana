8ebe7417475b538f9a009b17a2e81fa29901da4c
Sébastien Loix <sabee77@gmail.com>
2021-09-23 13:00:03 +0100

    Move logic to detect hidden & system indices to single place

---
+++ x-pack/plugins/index_management/public/application/lib/indices.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { Index } from '../../../common';
+
+export const isHiddenIndex = (index: Index): boolean => {
+  return (index.name ?? '').startsWith('.') || index.hidden === true;
+};
+
+export const isSystemIndex = (index: Index): boolean => {
+  return (index.name ?? '').startsWith('.');
+};
