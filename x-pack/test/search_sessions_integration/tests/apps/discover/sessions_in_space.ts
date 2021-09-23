d620e10fa3d647f3a2f536188ea7ca7f3952ce97
Jonathan Budzenski <jon@budzenski.me>
2021-09-23 10:16:31 -0500

    skip flaky suite.  #112913

--- x-pack/test/search_sessions_integration/tests/apps/discover/sessions_in_space.ts
+++ x-pack/test/search_sessions_integration/tests/apps/discover/sessions_in_space.ts
@@ -23,7 +23,8 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const browser = getService('browser');
   const searchSessions = getService('searchSessions');
 
-  describe('discover in space', () => {
+  // FLAKY https://github.com/elastic/kibana/issues/112913
+  describe.skip('discover in space', () => {
     describe('Storing search sessions in space', () => {
       before(async () => {
         await esArchiver.load('x-pack/test/functional/es_archives/dashboard/session_in_space');
