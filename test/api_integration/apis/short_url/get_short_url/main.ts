0ded7fa3efe3e2f07af9dc90cba6bca533e3c121
vadimkibana <vadimkibana@gmail.com>
2021-09-26 18:44:16 +0200

    add test for legacy short url support

--- test/api_integration/apis/short_url/get_short_url/main.ts
+++ test/api_integration/apis/short_url/get_short_url/main.ts
@@ -22,5 +22,30 @@ export default function ({ getService }: FtrProviderContext) {
 
       expect(response2.body).to.eql(response1.body);
     });
+
+    it('supports legacy short URLs', async () => {
+      const id = 'abcdefghjabcdefghjabcdefghjabcdefghj';
+      await supertest.post('/api/saved_objects/url/' + id).send({
+        attributes: {
+          accessCount: 25,
+          accessDate: 1632672537546,
+          createDate: 1632672507685,
+          url: '/app/dashboards#/view/123',
+        },
+      });
+      const response = await supertest.get('/api/short_url/' + id);
+      await supertest.delete('/api/saved_objects/url/' + id).send();
+
+      expect(response.body.id).to.be(id);
+      expect(response.body.slug).to.be(id);
+      expect(response.body.locator).to.eql({
+        id: 'LEGACY_SHORT_URL_LOCATOR',
+        version: '7.15.0',
+        state: { url: '/app/dashboards#/view/123' },
+      });
+      expect(response.body.accessCount).to.be(25);
+      expect(response.body.accessDate).to.be(1632672537546);
+      expect(response.body.createDate).to.be(1632672507685);
+    });
   });
 }
