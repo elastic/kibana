8549b4709630a7211b020164b788467f3c1248d2
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2021-09-27 10:26:27 +0300

    Add unit tests

--- src/plugins/vis_types/xy/public/sample_vis.test.mocks.ts
+++ src/plugins/vis_types/xy/public/sample_vis.test.mocks.ts
@@ -1780,7 +1780,28 @@ export const sampleAreaVis = {
     },
     aggs: {
       typesRegistry: {},
-      bySchemaName: jest.fn(),
+      bySchemaName: () => [
+        {
+          id: '1',
+          enabled: true,
+          type: 'sum',
+          params: {
+            field: 'total_quantity',
+          },
+          schema: 'metric',
+          makeLabel: () => 'Total quantity',
+          toSerializedFieldFormat: () => ({
+            id: 'number',
+            params: {
+              parsedUrl: {
+                origin: 'http://localhost:5801',
+                pathname: '/app/visualize',
+                basePath: '',
+              },
+            },
+          }),
+        },
+      ],
       getResponseAggs: () => [
         {
           id: '1',
