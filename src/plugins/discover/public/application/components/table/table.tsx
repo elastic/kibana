15a63d01d00c42eec22f985a201260fc4ab93b86
Dzmitry Tamashevich <diaamnj@mail.ru>
2021-09-27 17:07:13 +0300

    [Discover] fix doc viewer table columns

--- src/plugins/discover/public/application/components/table/table.tsx
+++ src/plugins/discover/public/application/components/table/table.tsx
@@ -136,7 +136,6 @@ export const DocViewerTable = ({
 
   return (
     <EuiInMemoryTable
-      tableLayout="auto"
       className="kbnDocViewer__table"
       items={items}
       columns={tableColumns}
