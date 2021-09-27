35b5b85625d9ce048d0c0da830081bbf8c5391c8
Mat Schaffer <mat@elastic.co>
2021-09-27 12:40:59 +0900

    [Stack Monitoring] Beats Overview view migration (#112377)
    * Wire up a stub beats overview page
    
    * Filling in some bits
    
    * Figured out this todo already
    
    * Fix importa and product name
    
    * Wire up BeatsOverview component
    
    * Set breadcrumbs
    
    * Add "beats" part to breadcrumbs
    
    * Remove unused vars copied from ES overview
    
    * Use passed data rather than outer scope
    
    * Fixing basic type checks
    
    * Fix breadcrumb type issues
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/monitoring/public/application/pages/beats/beats_template.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React from 'react';
+import { i18n } from '@kbn/i18n';
+import { PageTemplate } from '../page_template';
+import { TabMenuItem, PageTemplateProps } from '../page_template';
+
+interface BeatsTemplateProps extends PageTemplateProps {
+  cluster: any;
+}
+
+export const BeatsTemplate: React.FC<BeatsTemplateProps> = ({ cluster, ...props }) => {
+  const tabs: TabMenuItem[] = [
+    {
+      id: 'overview',
+      label: i18n.translate('xpack.monitoring.beatsNavigation.overviewLinkText', {
+        defaultMessage: 'Overview',
+      }),
+      route: '/beats',
+    },
+    {
+      id: 'instances',
+      label: i18n.translate('xpack.monitoring.beatsNavigation.instancesLinkText', {
+        defaultMessage: 'Instances',
+      }),
+      route: '/beats/beats',
+    },
+  ];
+
+  return <PageTemplate {...props} tabs={tabs} product="beats" />;
+};
