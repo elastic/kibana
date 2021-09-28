76967dee9a0afd0c684930d65c3f6f8ac5243eba
Paul Tavares <paul.tavares@elastic.co>
2021-09-28 10:19:46 -0400

    View action on the card

--- x-pack/plugins/security_solution/public/management/pages/policy/view/trusted_apps/list/policy_trusted_apps_list.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/trusted_apps/list/policy_trusted_apps_list.tsx
@@ -5,9 +5,10 @@
  * 2.0.
  */
 
-import React, { memo, useCallback, useEffect, useState } from 'react';
+import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
 import { EuiLoadingSpinner, Pagination } from '@elastic/eui';
 import { useHistory } from 'react-router-dom';
+import { i18n } from '@kbn/i18n';
 import {
   ArtifactCardGrid,
   ArtifactCardGridProps,
@@ -21,11 +22,17 @@ import {
   isPolicyTrustedAppListLoading,
   policyIdFromParams,
 } from '../../../store/policy_details/selectors';
-import { getPolicyDetailsArtifactsListPath } from '../../../../../common/routing';
-import { TrustedApp } from '../../../../../../../common/endpoint/types';
+import {
+  getPolicyDetailsArtifactsListPath,
+  getTrustedAppsListPath,
+} from '../../../../../common/routing';
+import { Immutable, TrustedApp } from '../../../../../../../common/endpoint/types';
+import { useAppUrl } from '../../../../../../common/lib/kibana';
+import { APP_ID } from '../../../../../../../common/constants';
 
 export const PolicyTrustedAppsList = memo(() => {
   const history = useHistory();
+  const { getAppUrl } = useAppUrl();
   const policyId = usePolicyDetailsSelector(policyIdFromParams);
   const hasTrustedApps = usePolicyDetailsSelector(doesPolicyHaveTrustedApps);
   const isLoading = usePolicyDetailsSelector(isPolicyTrustedAppListLoading);
@@ -68,15 +75,39 @@ export const PolicyTrustedAppsList = memo(() => {
     []
   );
 
-  const provideCardProps = useCallback(
-    (item: TrustedApp) => {
-      return {
-        expanded: Boolean(isCardExpanded[item.id]),
-        // FIXME:PT implement card menu
-        actions: [{ children: 'one' }, { children: 'two' }],
-      };
+  const cardProps = useMemo<
+    Map<Immutable<TrustedApp>, ReturnType<Required<ArtifactCardGridProps>['cardComponentProps']>>
+  >(() => {
+    const newCardProps = new Map();
+
+    for (const trustedApp of trustedAppItems) {
+      const viewUrlPath = getTrustedAppsListPath({ id: trustedApp.id, show: 'edit' });
+
+      newCardProps.set(trustedApp, {
+        expanded: Boolean(isCardExpanded[trustedApp.id]),
+        actions: [
+          {
+            icon: 'controlsHorizontal',
+            children: i18n.translate(
+              'xpack.securitySolution.endpoint.policy.trustedApps.list.viewAction',
+              { defaultMessage: 'View full details' }
+            ),
+            href: getAppUrl({ appId: APP_ID, path: viewUrlPath }),
+            navigateAppId: APP_ID,
+            navigateOptions: { path: viewUrlPath },
+          },
+        ],
+      });
+    }
+
+    return newCardProps;
+  }, [getAppUrl, isCardExpanded, trustedAppItems]);
+
+  const provideCardProps = useCallback<Required<ArtifactCardGridProps>['cardComponentProps']>(
+    (item) => {
+      return cardProps.get(item as Immutable<TrustedApp>)!;
     },
-    [isCardExpanded]
+    [cardProps]
   );
 
   // Anytime a new set of data (trusted apps) is retrieved, reset the card expand state
