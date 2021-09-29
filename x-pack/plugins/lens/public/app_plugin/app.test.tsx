3564582132f741a65d04014bcc038acc02d3ad87
Marta Bondyra <marta.bondyra@gmail.com>
2021-09-29 12:33:34 +0200

    Update x-pack/plugins/lens/public/app_plugin/app.test.tsx

--- x-pack/plugins/lens/public/app_plugin/app.test.tsx
+++ x-pack/plugins/lens/public/app_plugin/app.test.tsx
@@ -337,7 +337,7 @@ describe('Lens App', () => {
       );
     });
     it('handles rejected index pattern', async () => {
-      const customServices = makeDefaultServices(sessionIdSubject, 'sessionId-1');
+      const customServices = makeDefaultServices(sessionIdSubject);
       customServices.data.indexPatterns.get = jest
         .fn()
         .mockImplementation((id) => Promise.reject({ reason: 'Could not locate that data view' }));
