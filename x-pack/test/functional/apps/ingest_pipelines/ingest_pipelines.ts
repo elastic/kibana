e9096fb8191843051543aa4f1933290871b59e52
spalger <spalger@users.noreply.github.com>
2021-09-29 15:46:56 +0000

    skip suite failing es promotion (#113439)

--- x-pack/test/functional/apps/ingest_pipelines/ingest_pipelines.ts
+++ x-pack/test/functional/apps/ingest_pipelines/ingest_pipelines.ts
@@ -20,7 +20,8 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const es = getService('es');
   const security = getService('security');
 
-  describe('Ingest Pipelines', function () {
+  // FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/113439
+  describe.skip('Ingest Pipelines', function () {
     this.tags('smoke');
     before(async () => {
       await security.testUser.setRoles(['ingest_pipelines_user']);
