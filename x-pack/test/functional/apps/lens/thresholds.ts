31343933b0cf252aa672d50333a2aaa68cd2dbf6
Marco Liberati <dej611@users.noreply.github.com>
2021-09-29 18:52:50 +0200

    [Lens] Thresholds: moving a threshold to another group should carry also its styling (#112853)
    * :bug: When dragging a threshold to another group carry also its styling
    
    * :white_check_mark: Add functional test
    
    * :sparkles: Make duplicate carry style too
    
    * :white_check_mark: Add functional test for duplicate use case
    
    * :bug: Fix table duplication issue
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/functional/apps/lens/thresholds.ts
+++ x-pack/test/functional/apps/lens/thresholds.ts
@@ -64,5 +64,57 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         'Median of bytes',
       ]);
     });
+
+    it('should add a new group to the threshold layer when a right axis is enabled', async () => {
+      await PageObjects.lens.configureDimension({
+        dimension: 'lnsXY_yDimensionPanel > lns-empty-dimension',
+        operation: 'average',
+        field: 'bytes',
+        keepOpen: true,
+      });
+
+      await PageObjects.lens.changeAxisSide('right');
+
+      await PageObjects.lens.closeDimensionEditor();
+
+      await testSubjects.existOrFail('lnsXY_yThresholdRightPanel > lns-empty-dimension');
+    });
+
+    it('should carry the style when moving a threshold to another group', async () => {
+      // style it enabling the fill
+      await testSubjects.click('lnsXY_yThresholdLeftPanel > lns-dimensionTrigger');
+      await testSubjects.click('lnsXY_fill_below');
+      await PageObjects.lens.closeDimensionEditor();
+
+      // drag and drop it to the left axis
+      await PageObjects.lens.dragDimensionToDimension(
+        'lnsXY_yThresholdLeftPanel > lns-dimensionTrigger',
+        'lnsXY_yThresholdRightPanel > lns-empty-dimension'
+      );
+
+      await testSubjects.click('lnsXY_yThresholdRightPanel > lns-dimensionTrigger');
+      expect(
+        await find.existsByCssSelector('[data-test-subj="lnsXY_fill_below"][class$="isSelected"]')
+      ).to.be(true);
+      await PageObjects.lens.closeDimensionEditor();
+    });
+
+    it('should duplicate also the original style when duplicating a threshold', async () => {
+      // drag and drop to the empty field to generate a duplicate
+      await PageObjects.lens.dragDimensionToDimension(
+        'lnsXY_yThresholdRightPanel > lns-dimensionTrigger',
+        'lnsXY_yThresholdRightPanel > lns-empty-dimension'
+      );
+
+      await (
+        await find.byCssSelector(
+          '[data-test-subj="lnsXY_yThresholdRightPanel"]:nth-child(2) [data-test-subj="lns-dimensionTrigger"]'
+        )
+      ).click();
+      expect(
+        await find.existsByCssSelector('[data-test-subj="lnsXY_fill_below"][class$="isSelected"]')
+      ).to.be(true);
+      await PageObjects.lens.closeDimensionEditor();
+    });
   });
 }
