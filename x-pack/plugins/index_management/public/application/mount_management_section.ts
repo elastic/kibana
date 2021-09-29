fd1f6e84a84921aa191dd6cd6305e7dae212ff45
spalger <spalger@users.noreply.github.com>
2021-09-29 17:51:29 +0000

    autofix all violations

--- x-pack/plugins/index_management/public/application/mount_management_section.ts
+++ x-pack/plugins/index_management/public/application/mount_management_section.ts
@@ -7,7 +7,7 @@
 
 import { i18n } from '@kbn/i18n';
 import { CoreSetup } from 'src/core/public';
-import { ManagementAppMountParams } from 'src/plugins/management/public/';
+import { ManagementAppMountParams } from 'src/plugins/management/public';
 import { UsageCollectionSetup } from 'src/plugins/usage_collection/public';
 
 import { UIM_APP_NAME } from '../../common/constants';
