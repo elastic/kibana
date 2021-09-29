ec6714e725fa0b6f764bb319c4a580b0b06df3f1
Julia Bardi <julia.bardi@elastic.co>
2021-09-29 14:42:53 +0200

    trying jenkins run

--- test/scripts/jenkins_fleet_cypress.sh
+++ test/scripts/jenkins_fleet_cypress.sh
@@ -9,7 +9,7 @@ checks-reporter-with-killswitch "Fleet Cypress Tests" \
  node scripts/functional_tests \
    --debug --bail \
    --kibana-install-dir "$KIBANA_INSTALL_DIR" \
-   --config test/fleet/cli_config.ts
+   --config test/fleet_cypress/cli_config.ts
 
 echo ""
 echo ""
