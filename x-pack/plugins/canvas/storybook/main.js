2ff4e806d6ef2832e89b71f7c067b5c7434673b3
shahzad31 <shahzad31comp@gmail.com>
2021-09-30 10:55:52 +0200

    added a comment

--- x-pack/plugins/canvas/storybook/main.js
+++ x-pack/plugins/canvas/storybook/main.js
@@ -8,3 +8,8 @@
 import { canvasStorybookConfig } from './canvas.webpack';
 
 module.exports = canvasStorybookConfig;
+
+// This file wsa converted to .js again because of ts-node causing issues with
+// storybook typescript setup, this is more or less relevant to
+// https://github.com/storybookjs/storybook/issues/9610
+// ts usage was moved to separate file canvas.webpack.ts, keep this file only was module.exports
