8bfff128146de235c0b7f75dc3401e9cd930a457
Matt Kime <matt@mattki.me>
2021-09-30 18:52:34 -0500

    path fixes

--- src/plugins/data_views/server/fetcher/lib/field_capabilities/field_caps_response.test.js
+++ src/plugins/data_views/server/fetcher/lib/field_capabilities/field_caps_response.test.js
@@ -13,7 +13,7 @@ import sinon from 'sinon';
 import * as shouldReadFieldFromDocValuesNS from './should_read_field_from_doc_values';
 import { shouldReadFieldFromDocValues } from './should_read_field_from_doc_values';
 
-import { getKbnFieldType } from '../../../../common';
+import { getKbnFieldType } from '@kbn/field-types';
 import { readFieldCapsResponse } from './field_caps_response';
 import esResponse from './__fixtures__/es_field_caps_response.json';
 
