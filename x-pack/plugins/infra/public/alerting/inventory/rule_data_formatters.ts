f79283258f829f63c48433c147aae5cc25c7260d
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2021-09-30 17:58:56 +0200

    Link inventory alerts to the right inventory view

--- x-pack/plugins/infra/public/alerting/inventory/rule_data_formatters.ts
+++ x-pack/plugins/infra/public/alerting/inventory/rule_data_formatters.ts
@@ -5,12 +5,36 @@
  * 2.0.
  */
 
-import { ALERT_REASON } from '@kbn/rule-data-utils';
+import { ALERT_REASON, ALERT_RULE_PARAMS, TIMESTAMP } from '@kbn/rule-data-utils';
+import { encode } from 'rison-node';
 import { ObservabilityRuleTypeFormatter } from '../../../../observability/public';
+
+const LINK_PARAMS_TEMPLATE =
+  '?waffleFilter=(expression:%27%27,kind:kuery)&waffleTime=(currentTime:{timestamp},isAutoReloading:!f)&waffleOptions=(accountId:%27%27,autoBounds:!t,boundsOverride:(max:1,min:0),customMetrics:!({customMetric}),customOptions:!(),groupBy:!(),legend:(palette:cool,reverseColors:!f,steps:10),metric:{metric},nodeType:{nodeType},region:%27%27,sort:(by:name,direction:desc),timelineOpen:!f,view:map)';
 
 export const formatReason: ObservabilityRuleTypeFormatter = ({ fields }) => {
   const reason = fields[ALERT_REASON] ?? '-';
-  const link = '/app/metrics/inventory'; // TODO https://github.com/elastic/kibana/issues/106497
+  const ruleParams =
+    typeof fields[ALERT_RULE_PARAMS] === 'string' ? JSON.parse(fields[ALERT_RULE_PARAMS]!) : {};
+
+  const urlParams: Record<string, any> = {
+    nodeType: ruleParams.nodeType,
+    timestamp: Date.parse(fields[TIMESTAMP]),
+    customMetric: '',
+  };
+
+  // We always pick the first criteria for the URL
+  const criteria = ruleParams.criteria[0];
+  if (criteria.customMetric.id !== 'alert-custom-metric') {
+    const customMetric = encode(criteria.customMetric);
+    urlParams.customMetric = customMetric;
+    urlParams.metric = customMetric;
+  } else {
+    urlParams.metric = encode({ type: criteria.metric });
+  }
+
+  const link =
+    '/app/metrics/inventory' + LINK_PARAMS_TEMPLATE.replace(/{(\w+)}/g, (_, key) => urlParams[key]);
 
   return {
     reason,
