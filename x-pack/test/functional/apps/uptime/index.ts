1b09f5202dd2dfc9910c49616d57524f66e60a10
Brian Seeders <brian.seeders@elastic.co>
2021-09-30 15:58:44 -0400

    Update index.ts

--- x-pack/test/functional/apps/uptime/index.ts
+++ x-pack/test/functional/apps/uptime/index.ts
@@ -42,7 +42,7 @@ export default ({ loadTestFile, getService }: FtrProviderContext) => {
   const uptime = getService('uptime');
 
   describe('Uptime app', function () {
-    this.tags('ciGroup10');
+    this.tags('ciGroup6');
 
     beforeEach('delete settings', async () => {
       await deleteUptimeSettingsObject(server);
