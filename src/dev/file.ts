964e34959e33490f1b0248e0eb38607ff0490768
Domenico Andreoli <domenico.andreoli@elastic.co>
2021-10-01 07:03:36 +0200

    Lint git index content on commit (#113300)
    This fixes two fundamental issues:
    * the list of files to be linted is built looking at the diffs of the ref
      specified with `--ref` (or otherwise the current index) with those on
      the filesystem
    * the content to be linted is read from the filesystem instead of the
      specified `ref` or the one in the index

--- src/dev/file.ts
+++ src/dev/file.ts
@@ -7,11 +7,13 @@
  */
 
 import { dirname, extname, join, relative, resolve, sep, basename } from 'path';
+import { createFailError } from '@kbn/dev-utils';
 
 export class File {
   private path: string;
   private relativePath: string;
   private ext: string;
+  private fileReader: undefined | (() => Promise<string>);
 
   constructor(path: string) {
     this.path = resolve(path);
@@ -55,6 +57,11 @@ export class File {
     );
   }
 
+  // Virtual files cannot be read as usual, an helper is needed
+  public isVirtual() {
+    return this.fileReader !== undefined;
+  }
+
   public getRelativeParentDirs() {
     const parents: string[] = [];
 
@@ -80,5 +87,16 @@ export class File {
 
   public toJSON() {
     return this.relativePath;
+  }
+
+  public setFileReader(fileReader: () => Promise<string>) {
+    this.fileReader = fileReader;
+  }
+
+  public getContent() {
+    if (this.fileReader) {
+      return this.fileReader();
+    }
+    throw createFailError('getContent() was invoked on a non-virtual File');
   }
 }
--- src/dev/stylelint/lint_files.js
+++ src/dev/stylelint/lint_files.js
@@ -16,15 +16,8 @@ import { createFailError } from '@kbn/dev-utils';
 const stylelintPath = path.resolve(__dirname, '..', '..', '..', '.stylelintrc');
 const styleLintConfig = safeLoad(fs.readFileSync(stylelintPath));
 
-/**
- * Lints a list of files with eslint. eslint reports are written to the log
- * and a FailError is thrown when linting errors occur.
- *
- * @param  {ToolingLog} log
- * @param  {Array<File>} files
- * @return {undefined}
- */
-export async function lintFiles(log, files) {
+// For files living on the filesystem
+function lintFilesOnFS(files) {
   const paths = files.map((file) => file.getRelativePath());
 
   const options = {
@@ -34,7 +27,52 @@ export async function lintFiles(log, files) {
     ignorePath: path.resolve(__dirname, '..', '..', '..', '.stylelintignore'),
   };
 
-  const report = await stylelint.lint(options);
+  return stylelint.lint(options);
+}
+
+// For files living somewhere else (ie. git object)
+async function lintFilesOnContent(files) {
+  const report = {
+    errored: false,
+    output: '',
+    postcssResults: [],
+    results: [],
+    maxWarningsExceeded: {
+      maxWarnings: 0,
+      foundWarnings: 0,
+    },
+  };
+
+  for (let i = 0; i < files.length; i++) {
+    const options = {
+      code: await files[i].getContent(),
+      config: styleLintConfig,
+      formatter: 'string',
+      ignorePath: path.resolve(__dirname, '..', '..', '..', '.stylelintignore'),
+    };
+    const r = await stylelint.lint(options);
+    report.errored = report.errored || r.errored;
+    report.output += r.output.replace(/<input css \d+>/, files[i].getRelativePath()).slice(0, -1);
+    report.postcssResults.push(...(r.postcssResults || []));
+    report.maxWarnings = r.maxWarnings;
+    report.foundWarnings += r.foundWarnings;
+  }
+
+  return report;
+}
+
+/**
+ * Lints a list of files with eslint. eslint reports are written to the log
+ * and a FailError is thrown when linting errors occur.
+ *
+ * @param  {ToolingLog} log
+ * @param  {Array<File>} files
+ * @return {undefined}
+ */
+export async function lintFiles(log, files) {
+  const virtualFilesCount = files.filter((file) => file.isVirtual()).length;
+  const report = virtualFilesCount ? await lintFilesOnContent(files) : await lintFilesOnFS(files);
+
   if (report.errored) {
     log.error(report.output);
     throw createFailError('[stylelint] errors');
