9c825e92ed0715eb7e33aef8c61e9fd0e46f555b
Steph Milovic <stephanie.milovic@elastic.co>
2021-10-04 09:30:22 -0600

    rm whoops

--- x-pack/plugins/security_solution/public/timelines/components/timeline/search_or_filter/pick_events.test.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/search_or_filter/pick_events.test.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import { PickEventType } from './pick_events';
 import {
