e48877e19cafc29c9db918fc7dad98b03c6629ef
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2021-10-04 15:23:59 -0400

    additional happy/unhappy tests

--- x-pack/test/api_integration/apis/security_solution/timeline.ts
+++ x-pack/test/api_integration/apis/security_solution/timeline.ts
@@ -74,11 +74,62 @@ export default function ({ getService }: FtrProviderContext) {
       it('should return outcome exactMatch when the id is unchanged', async () => {
         const resp = await supertest
           .get('/api/timeline/resolve')
-          .query({ id: '6484cc90-126e-11ec-83d2-db1096c73738' });
+          .query({ id: '8dc70950-1012-11ec-9ad3-2d7c6600c0f7' });
 
         expect(resp.body.data.outcome).to.be('exactMatch');
         expect(resp.body.data.alias_target_id).to.be(undefined);
         expect(resp.body.data.timeline.title).to.be('Awesome Timeline');
+      });
+
+      describe('notes', () => {
+        it('should return notes with eventId', async () => {
+          const resp = await supertest
+            .get('/api/timeline/resolve')
+            .query({ id: '6484cc90-126e-11ec-83d2-db1096c73738' });
+
+          expect(resp.body.data.timeline.notes[0].eventId).to.be('Edo00XsBEVtyvU-8LGNe');
+        });
+
+        it('should return notes with the timelineId matching request id', async () => {
+          const resp = await supertest
+            .get('/api/timeline/resolve')
+            .query({ id: '6484cc90-126e-11ec-83d2-db1096c73738' });
+
+          expect(resp.body.data.timeline.notes[0].timelineId).to.be(
+            '6484cc90-126e-11ec-83d2-db1096c73738'
+          );
+          expect(resp.body.data.timeline.notes[1].timelineId).to.be(
+            '6484cc90-126e-11ec-83d2-db1096c73738'
+          );
+        });
+      });
+
+      describe('pinned events', () => {
+        it('should pinned events with eventId', async () => {
+          const resp = await supertest
+            .get('/api/timeline/resolve')
+            .query({ id: '6484cc90-126e-11ec-83d2-db1096c73738' });
+
+          expect(resp.body.data.timeline.pinnedEventsSaveObject[0].eventId).to.be(
+            'DNo00XsBEVtyvU-8LGNe'
+          );
+          expect(resp.body.data.timeline.pinnedEventsSaveObject[1].eventId).to.be(
+            'Edo00XsBEVtyvU-8LGNe'
+          );
+        });
+
+        it('should return pinned events with the timelineId matching request id', async () => {
+          const resp = await supertest
+            .get('/api/timeline/resolve')
+            .query({ id: '6484cc90-126e-11ec-83d2-db1096c73738' });
+
+          expect(resp.body.data.timeline.pinnedEventsSaveObject[0].timelineId).to.be(
+            '6484cc90-126e-11ec-83d2-db1096c73738'
+          );
+          expect(resp.body.data.timeline.pinnedEventsSaveObject[1].timelineId).to.be(
+            '6484cc90-126e-11ec-83d2-db1096c73738'
+          );
+        });
       });
     });
   });
