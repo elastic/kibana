0d2fcbc1de9367597aad216ac8b64035ad732f46
Matthew Kime <matt@mattki.me>
2021-10-04 16:55:53 -0500

    [7.x] [data views] Move data views api from data plugin and into its own (#113497) (#113833)
    * [data views] Move data views api from data plugin and into its own (#113497)
    
    * initial pass at moving data views into own plugin
    
    * require expressions plugin, fix META_FIELDS reference
    
    * bundle limits and localization
    
    * fix integration test
    
    * update plugin list and jest config
    
    * type fixes
    
    * search fixes
    
    * fix localization
    
    * fix mocks
    
    * fix mocks
    
    * fix stub
    
    * type fixes
    
    * fix import on test file
    
    * path fixes
    
    * remove shorted dotted from data plugin
    
    * more todo removal
    
    * eslint fixes
    
    * eslint fix
    
    * simplify data views server plugin
    
    * simplify data views server plugin
    
    * simplify data views server plugin
    
    * fix imports on api routes
    
    * fix imports on api routes
    
    * update plugin list
    
    * ts fixes
    
    * ts fixes
    
    * add deprecation notice
    
    * fix circular dependency and api integration test
    
    * fix circular dependency and api integration test
    
    * rename types for better clarity
    
    * path fixes
    
    * jest.config and tsconfig cleanup
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    * localization fix
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ src/plugins/data_views/public/index.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export {
+  ILLEGAL_CHARACTERS_KEY,
+  CONTAINS_SPACES_KEY,
+  ILLEGAL_CHARACTERS_VISIBLE,
+  ILLEGAL_CHARACTERS,
+  validateDataView,
+} from '../common/lib';
+export {
+  flattenHitWrapper,
+  formatHitProvider,
+  onRedirectNoIndexPattern,
+  onUnsupportedTimePattern,
+} from './data_views';
+
+export { IndexPatternField, IIndexPatternFieldList, TypeMeta } from '../common';
+
+export {
+  IndexPatternsService,
+  IndexPatternsContract,
+  IndexPattern,
+  DataViewsApiClient,
+  DataViewsService,
+  DataViewsContract,
+  DataView,
+} from './data_views';
+export { UiSettingsPublicToCommon } from './ui_settings_wrapper';
+export { SavedObjectsClientPublicToCommon } from './saved_objects_client_wrapper';
+
+/*
+ * Plugin setup
+ */
+
+import { DataViewsPublicPlugin } from './plugin';
+
+export function plugin() {
+  return new DataViewsPublicPlugin();
+}
+
+export type { DataViewsPublicPluginSetup, DataViewsPublicPluginStart } from './types';
+
+// Export plugin after all other imports
+export type { DataViewsPublicPlugin as DataPlugin };
