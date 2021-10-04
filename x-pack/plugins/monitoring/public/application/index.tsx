163133827b894468cb9dce4ed0a4aad493aff1f2
Kevin Lacabane <klacabane@gmail.com>
2021-10-04 15:57:47 +0200

    [Stack Monitoring] React migration kibana overview (#113604)
    * Create react Kibana template
    
    * React Kibana overview
    
    * Add breadcrumb to kibana overview
    
    * fix linting errors
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/monitoring/public/application/index.tsx
+++ x-pack/plugins/monitoring/public/application/index.tsx
@@ -23,7 +23,8 @@ import { ElasticsearchOverviewPage } from './pages/elasticsearch/overview';
 import { BeatsOverviewPage } from './pages/beats/overview';
 import { BeatsInstancesPage } from './pages/beats/instances';
 import { BeatsInstancePage } from './pages/beats/instance';
-import { CODE_PATH_ELASTICSEARCH, CODE_PATH_BEATS } from '../../common/constants';
+import { KibanaOverviewPage } from './pages/kibana/overview';
+import { CODE_PATH_ELASTICSEARCH, CODE_PATH_BEATS, CODE_PATH_KIBANA } from '../../common/constants';
 import { ElasticsearchNodesPage } from './pages/elasticsearch/nodes_page';
 import { ElasticsearchIndicesPage } from './pages/elasticsearch/indices_page';
 import { ElasticsearchNodePage } from './pages/elasticsearch/node_page';
@@ -130,6 +131,14 @@ const MonitoringApp: React.FC<{
                     path="/beats"
                     component={BeatsOverviewPage}
                     codePaths={[CODE_PATH_BEATS]}
+                    fetchAllClusters={false}
+                  />
+
+                  {/* Kibana Views */}
+                  <RouteInit
+                    path="/kibana"
+                    component={KibanaOverviewPage}
+                    codePaths={[CODE_PATH_KIBANA]}
                     fetchAllClusters={false}
                   />
 
