2b9c3e46b367fb4b15e2e33d30b31306c75f307f
Liza K <liza.katz@elastic.co>
2021-10-04 12:06:43 +0300

    dist

--- packages/kbn-pm/dist/index.js
+++ packages/kbn-pm/dist/index.js
@@ -9035,34 +9035,27 @@ class CiStatsReporter {
     }
 
     const memUsage = process.memoryUsage();
-    const isElasticCommitter = email ? email.endsWith('@elastic.co') : undefined;
-
-    const getReportValue = value => {
-      if (value) {
-        return isElasticCommitter ? value : _crypto.default.createHash('sha256').update(value).digest('hex').substring(0, 20);
-      } else {
-        return undefined;
-      }
-    };
-
+    const isElasticCommitter = email && email.endsWith('@elastic.co') ? true : false;
     const defaultMetadata = {
-      osPlatform: _os.default.platform(),
-      osRelease: _os.default.release(),
-      osArch: _os.default.arch(),
+      kibanaUuid,
+      isElasticCommitter,
+      committerHash: email ? _crypto.default.createHash('sha256').update(email).digest('hex').substring(0, 20) : undefined,
+      email: isElasticCommitter ? email : undefined,
+      branch: isElasticCommitter ? branch : undefined,
       cpuCount: (_Os$cpus = _os.default.cpus()) === null || _Os$cpus === void 0 ? void 0 : _Os$cpus.length,
       cpuModel: (_Os$cpus$ = _os.default.cpus()[0]) === null || _Os$cpus$ === void 0 ? void 0 : _Os$cpus$.model,
       cpuSpeed: (_Os$cpus$2 = _os.default.cpus()[0]) === null || _Os$cpus$2 === void 0 ? void 0 : _Os$cpus$2.speed,
       freeMem: _os.default.freemem(),
-      totalMem: _os.default.totalmem(),
       memoryUsageRss: memUsage.rss,
       memoryUsageHeapTotal: memUsage.heapTotal,
       memoryUsageHeapUsed: memUsage.heapUsed,
       memoryUsageExternal: memUsage.external,
       memoryUsageArrayBuffers: memUsage.arrayBuffers,
-      branchHash: getReportValue(branch),
-      committerHash: getReportValue(email),
-      isElasticCommitter,
-      kibanaUuid
+      nestedTiming: process.env.CI_STATS_NESTED_TIMING ? true : false,
+      osArch: _os.default.arch(),
+      osPlatform: _os.default.platform(),
+      osRelease: _os.default.release(),
+      totalMem: _os.default.totalmem()
     };
     this.log.debug('CIStatsReporter committerHash: %s', defaultMetadata.committerHash);
     return await this.req({
