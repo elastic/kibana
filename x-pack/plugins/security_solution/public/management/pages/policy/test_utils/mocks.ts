9032748d2dcb56e5c9f17d48cd160f4ea1dcef64
Paul Tavares <paul.tavares@elastic.co>
2021-10-04 18:44:58 -0400

    HTTP mocks for Policy Details Trusted apps list page

--- x-pack/plugins/security_solution/public/management/pages/policy/test_utils/mocks.ts
+++ x-pack/plugins/security_solution/public/management/pages/policy/test_utils/mocks.ts
@@ -5,5 +5,40 @@
  * 2.0.
  */
 
-export const policyDetailsTrustedAppsHttpMocks = () => {};
+import {
+  httpHandlerMockFactory,
+  ResponseProvidersInterface,
+} from '../../../../common/mock/endpoint/http_handler_mock_factory';
+import {
+  GetTrustedAppsListRequest,
+  GetTrustedAppsListResponse,
+} from '../../../../../common/endpoint/types';
+import { TRUSTED_APPS_LIST_API } from '../../../../../common/endpoint/constants';
+import { TrustedAppGenerator } from '../../../../../common/endpoint/data_generators/trusted_app_generator';
 
+type PolicyDetailsTrustedAppsHttpMocksInterface = ResponseProvidersInterface<{
+  policyTrustedAppsList: () => GetTrustedAppsListResponse;
+}>;
+
+/**
+ * HTTP mocks that support the Trusted Apps tab of the Policy Details page
+ */
+export const policyDetailsTrustedAppsHttpMocks =
+  httpHandlerMockFactory<PolicyDetailsTrustedAppsHttpMocksInterface>([
+    {
+      id: 'policyTrustedAppsList',
+      path: TRUSTED_APPS_LIST_API,
+      method: 'post',
+      handler: ({ query }): GetTrustedAppsListResponse => {
+        const apiQueryParams = query as GetTrustedAppsListRequest;
+        const generator = new TrustedAppGenerator('seed');
+
+        return {
+          page: apiQueryParams.page ?? 1,
+          per_page: apiQueryParams.per_page ?? 10,
+          total: 1,
+          data: [generator.generate()],
+        };
+      },
+    },
+  ]);
