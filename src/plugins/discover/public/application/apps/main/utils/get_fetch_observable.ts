7f3f67652657edfea056d7377dd9c24eb5a6f6ff
Alexey Antonov <alexwizp@gmail.com>
2021-10-05 12:30:25 +0300

    fix ts error

--- src/plugins/discover/public/application/apps/main/utils/get_fetch_observable.ts
+++ src/plugins/discover/public/application/apps/main/utils/get_fetch_observable.ts
@@ -12,7 +12,7 @@ import { FetchStatus } from '../../../types';
 import type {
   AutoRefreshDoneFn,
   DataPublicPluginStart,
-  SearchSource,
+  ISearchSource,
 } from '../../../../../../data/public';
 import { DataMain$, DataRefetch$ } from '../services/use_saved_search';
 import { DiscoverSearchSessionManager } from '../services/discover_search_session';
@@ -32,7 +32,7 @@ export function getFetch$({
   main$: DataMain$;
   refetch$: DataRefetch$;
   searchSessionManager: DiscoverSearchSessionManager;
-  searchSource: SearchSource;
+  searchSource: ISearchSource;
 }) {
   const { timefilter } = data.query.timefilter;
   const { filterManager } = data.query;
