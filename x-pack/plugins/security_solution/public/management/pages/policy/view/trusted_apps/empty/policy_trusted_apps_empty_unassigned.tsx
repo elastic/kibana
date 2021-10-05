7d31317cd8e4abe7c11ce06d60363607e8886645
David Sanchez Soler <davidsansol92@gmail.com>
2021-10-05 10:01:47 +0200

    Fix multilang keys and join code into a hook to avoid duplications

--- x-pack/plugins/security_solution/public/management/pages/policy/view/trusted_apps/empty/policy_trusted_apps_empty_unassigned.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/trusted_apps/empty/policy_trusted_apps_empty_unassigned.tsx
@@ -5,15 +5,11 @@
  * 2.0.
  */
 
-import React, { memo, useMemo } from 'react';
+import React, { memo } from 'react';
 import { EuiEmptyPrompt, EuiButton, EuiPageTemplate, EuiLink } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { i18n } from '@kbn/i18n';
-import { useNavigateToAppEventHandler } from '../../../../../../common/hooks/endpoint/use_navigate_to_app_event_handler';
-import { useAppUrl } from '../../../../../../common/lib/kibana/hooks';
-import { getPolicyTrustedAppsPath, getTrustedAppsListPath } from '../../../../../common/routing';
-import { APP_ID } from '../../../../../../../common/constants';
 import { usePolicyDetailsNavigateCallback } from '../../policy_hooks';
+import { useGetLinkTo } from './use_policy_trusted_apps_empty_hooks';
 
 interface CommonProps {
   policyId: string;
@@ -22,41 +18,7 @@ interface CommonProps {
 
 export const PolicyTrustedAppsEmptyUnassigned = memo<CommonProps>(({ policyId, policyName }) => {
   const navigateCallback = usePolicyDetailsNavigateCallback();
-  const { getAppUrl } = useAppUrl();
-  const { toRoutePath, toRouteUrl } = useMemo(() => {
-    const path = getTrustedAppsListPath();
-    return {
-      toRoutePath: path,
-      toRouteUrl: getAppUrl({ path }),
-    };
-  }, [getAppUrl]);
-
-  const policyTrustedAppsPath = useMemo(() => getPolicyTrustedAppsPath(policyId), [policyId]);
-  const policyTrustedAppRouteState = useMemo(() => {
-    return {
-      backButtonLabel: i18n.translate(
-        'xpack.securitySolution.endpoint.fleetCustomExtension.artifacts.backButtonLabel',
-        {
-          defaultMessage: `Back to ${policyName} policy`,
-        }
-      ),
-      onBackButtonNavigateTo: [
-        APP_ID,
-        {
-          path: policyTrustedAppsPath,
-        },
-      ],
-      backButtonUrl: getAppUrl({
-        appId: APP_ID,
-        path: policyTrustedAppsPath,
-      }),
-    };
-  }, [getAppUrl, policyName, policyTrustedAppsPath]);
-
-  const onClickHandler = useNavigateToAppEventHandler(APP_ID, {
-    state: policyTrustedAppRouteState,
-    path: toRoutePath,
-  });
+  const { onClickHandler, toRouteUrl } = useGetLinkTo(policyId, policyName);
   return (
     <EuiPageTemplate template="centeredContent">
       <EuiEmptyPrompt
--- x-pack/plugins/security_solution/public/management/pages/policy/view/trusted_apps/empty/policy_trusted_apps_empty_unexisting.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/trusted_apps/empty/policy_trusted_apps_empty_unexisting.tsx
@@ -5,14 +5,10 @@
  * 2.0.
  */
 
-import React, { memo, useMemo } from 'react';
+import React, { memo } from 'react';
 import { EuiEmptyPrompt, EuiButton, EuiPageTemplate } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
-import { i18n } from '@kbn/i18n';
-import { useNavigateToAppEventHandler } from '../../../../../../common/hooks/endpoint/use_navigate_to_app_event_handler';
-import { useAppUrl } from '../../../../../../common/lib/kibana/hooks';
-import { getPolicyTrustedAppsPath, getTrustedAppsListPath } from '../../../../../common/routing';
-import { APP_ID } from '../../../../../../../common/constants';
+import { useGetLinkTo } from './use_policy_trusted_apps_empty_hooks';
 
 interface CommonProps {
   policyId: string;
@@ -20,41 +16,7 @@ interface CommonProps {
 }
 
 export const PolicyTrustedAppsEmptyUnexisting = memo<CommonProps>(({ policyId, policyName }) => {
-  const { getAppUrl } = useAppUrl();
-  const { toRoutePath, toRouteUrl } = useMemo(() => {
-    const path = getTrustedAppsListPath();
-    return {
-      toRoutePath: path,
-      toRouteUrl: getAppUrl({ path }),
-    };
-  }, [getAppUrl]);
-
-  const policyTrustedAppsPath = useMemo(() => getPolicyTrustedAppsPath(policyId), [policyId]);
-  const policyTrustedAppRouteState = useMemo(() => {
-    return {
-      backButtonLabel: i18n.translate(
-        'xpack.securitySolution.endpoint.fleetCustomExtension.artifacts.backButtonLabel',
-        {
-          defaultMessage: `Back to ${policyName} policy`,
-        }
-      ),
-      onBackButtonNavigateTo: [
-        APP_ID,
-        {
-          path: policyTrustedAppsPath,
-        },
-      ],
-      backButtonUrl: getAppUrl({
-        appId: APP_ID,
-        path: policyTrustedAppsPath,
-      }),
-    };
-  }, [getAppUrl, policyName, policyTrustedAppsPath]);
-
-  const onClickHandler = useNavigateToAppEventHandler(APP_ID, {
-    state: policyTrustedAppRouteState,
-    path: toRoutePath,
-  });
+  const { onClickHandler, toRouteUrl } = useGetLinkTo(policyId, policyName);
   return (
     <EuiPageTemplate template="centeredContent">
       <EuiEmptyPrompt
