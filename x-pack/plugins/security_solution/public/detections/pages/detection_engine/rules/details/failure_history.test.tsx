bbde55f1b3c36d24eaee82cdfe056a67b432ef9a
Devin Hurley <devin.hurley@elastic.co>
2021-10-05 10:08:18 -0400

    unit test changes

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/details/failure_history.test.tsx
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/details/failure_history.test.tsx
@@ -6,11 +6,71 @@
  */
 
 import React from 'react';
-import { shallow } from 'enzyme';
-import { TestProviders } from '../../../../../common/mock';
+import { shallow, mount } from 'enzyme';
+import {
+  TestProviders,
+  createSecuritySolutionStorageMock,
+  kibanaObservable,
+  mockGlobalState,
+  SUB_PLUGINS_REDUCER,
+} from '../../../../../common/mock';
 import { FailureHistory } from './failure_history';
 import { useRuleStatus } from '../../../../containers/detection_engine/rules';
 jest.mock('../../../../containers/detection_engine/rules');
+
+import { waitFor } from '@testing-library/react';
+
+import '../../../../../common/mock/match_media';
+
+import { createStore, State } from '../../../../../common/store';
+import { mockHistory, Router } from '../../../../../common/mock/router';
+
+const state: State = {
+  ...mockGlobalState,
+};
+const { storage } = createSecuritySolutionStorageMock();
+const store = createStore(state, SUB_PLUGINS_REDUCER, kibanaObservable, storage);
+
+describe('RuleDetailsPageComponent', () => {
+  beforeAll(() => {
+    (useRuleStatus as jest.Mock).mockReturnValue([
+      false,
+      {
+        status: 'succeeded',
+        last_failure_at: new Date().toISOString(),
+        last_failure_message: 'my fake failure message',
+        failures: [
+          {
+            alert_id: 'myfakeid',
+            status_date: new Date().toISOString(),
+            status: 'failed',
+            last_failure_at: new Date().toISOString(),
+            last_success_at: new Date().toISOString(),
+            last_failure_message: 'my fake failure message',
+            last_look_back_date: new Date().toISOString(), // NOTE: This is no longer used on the UI, but left here in case users are using it within the API
+          },
+        ],
+      },
+    ]);
+  });
+
+  it('renders correctly', async () => {
+    const wrapper = mount(
+      <TestProviders store={store}>
+        <Router history={mockHistory}>
+          <FailureHistory id="id" />
+        </Router>
+      </TestProviders>
+    );
+
+    await waitFor(() => {
+      expect(wrapper.find('EuiBasicTable')).toHaveLength(1);
+      expect(wrapper.find('EuiTableRowCell').at(2).find('div').at(1).text()).toEqual(
+        'my fake failure message'
+      );
+    });
+  });
+});
 
 describe('FailureHistory', () => {
   beforeAll(() => {
@@ -25,34 +85,7 @@ describe('FailureHistory', () => {
     expect(wrapper.find('EuiBasicTable')).toHaveLength(1);
   });
 
-  it('renders correctly with failures', () => {
-    (useRuleStatus as jest.Mock).mockReturnValue([
-      false,
-      { failures: [{ last_failure_at: new Date().toISOString() }] },
-    ]);
-    const wrapper = shallow(<FailureHistory id="id" />, {
-      wrappingComponent: TestProviders,
-    });
-
-    expect(wrapper.find('EuiBasicTable')).toHaveLength(1);
-  });
-
-  it('renders correctly without failures', () => {
-    (useRuleStatus as jest.Mock).mockReturnValue([
-      false,
-      {
-        status: 'succeeded',
-        last_failure_at: new Date().toISOString(),
-        last_failure_message: 'my fake failure message',
-        failures: [],
-      },
-    ]);
-    const wrapper = shallow(<FailureHistory id="id" />, {
-      wrappingComponent: TestProviders,
-    });
-
-    expect(wrapper.find('EuiBasicTable')).toHaveLength(1);
-  });
+  // WORKING
 
   it('renders incorrectly', () => {
     (useRuleStatus as jest.Mock).mockReturnValue([true, null]);
