5eabe763473792d3376b5cef9424e8ef8445855e
Brian Seeders <brian.seeders@elastic.co>
2021-10-04 21:16:54 -0400

    Also move security cigroup4 to 1

--- x-pack/test/functional/apps/security/basic_license/index.ts
+++ x-pack/test/functional/apps/security/basic_license/index.ts
@@ -9,7 +9,7 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('security app - basic license', function () {
-    this.tags('ciGroup4');
+    this.tags('ciGroup1');
 
     loadTestFile(require.resolve('./role_mappings'));
   });
