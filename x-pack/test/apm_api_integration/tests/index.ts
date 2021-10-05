33d0cff137dc17898e6b9be3bbdcade80ba54aba
Dario Gieselaar <dario.gieselaar@elastic.co>
2021-10-05 13:38:51 +0200

    [APM] Display all properties by default in flyout (#113221)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/apm_api_integration/tests/index.ts
+++ x-pack/test/apm_api_integration/tests/index.ts
@@ -37,6 +37,10 @@ export default function apmApiIntegrationTests(providerContext: FtrProviderConte
       loadTestFile(require.resolve('./correlations/latency'));
     });
 
+    describe('metadata/event_metadata', function () {
+      loadTestFile(require.resolve('./metadata/event_metadata'));
+    });
+
     describe('metrics_charts/metrics_charts', function () {
       loadTestFile(require.resolve('./metrics_charts/metrics_charts'));
     });
