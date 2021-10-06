aedd21df96b11eb046489061b7cc12c10c77e6cf
Kevin Lacabane <kevin.lacabane@elastic.co>
2021-10-06 12:00:35 +0200

    [Stack Monitoring] kibana instance react (#113962)
    * kibana instance page
    
    * add kibana instance route
    
    * fix linter error
    
    * remove page title translation wrapper
    
    * fix another linter error
    # Conflicts:
    #	x-pack/plugins/monitoring/public/application/index.tsx
    #	x-pack/plugins/monitoring/public/application/pages/kibana/instance.tsx

--- x-pack/plugins/monitoring/public/application/index.tsx
+++ x-pack/plugins/monitoring/public/application/index.tsx
@@ -32,6 +32,8 @@ import {
 import { BeatsInstancePage } from './pages/beats/instance';
 import { ApmOverviewPage, ApmInstancesPage, ApmInstancePage } from './pages/apm';
 import { KibanaOverviewPage } from './pages/kibana/overview';
+import { KibanaInstancesPage } from './pages/kibana/instances';
+import { KibanaInstancePage } from './pages/kibana/instance';
 import { ElasticsearchNodesPage } from './pages/elasticsearch/nodes_page';
 import { ElasticsearchIndicesPage } from './pages/elasticsearch/indices_page';
 import { ElasticsearchIndexPage } from './pages/elasticsearch/index_page';
@@ -186,6 +188,20 @@ const MonitoringApp: React.FC<{
                     />
 
                     {/* Kibana Views */}
+                    <RouteInit
+                      path="/kibana/instances/:instance"
+                      component={KibanaInstancePage}
+                      codePaths={[CODE_PATH_KIBANA]}
+                      fetchAllClusters={false}
+                    />
+
+                    <RouteInit
+                      path="/kibana/instances"
+                      component={KibanaInstancesPage}
+                      codePaths={[CODE_PATH_KIBANA]}
+                      fetchAllClusters={false}
+                    />
+
                     <RouteInit
                       path="/kibana"
                       component={KibanaOverviewPage}
