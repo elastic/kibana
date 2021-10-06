814205a2bead7cb734b200ddc93acd25b5e41a09
Anton Dosov <anton.dosov@elastic.co>
2021-10-06 14:46:19 +0200

    add test

--- src/plugins/data/server/search/services/cached_ui_settings_client.test.ts
+++ src/plugins/data/server/search/services/cached_ui_settings_client.test.ts
@@ -10,7 +10,7 @@ import { coreMock, httpServerMock } from '../../../../../core/server/mocks';
 
 import { CachedUiSettingsClient } from './cached_ui_settings_client';
 
-test('fetching uiSettings once', async () => {
+test('fetching uiSettings once for the same key', async () => {
   const request = httpServerMock.createKibanaRequest();
   const soClient = coreMock.createStart().savedObjects.getScopedClient(request);
   const uiSettings = coreMock.createStart().uiSettings.asScopedToClient(soClient);
@@ -30,4 +30,30 @@ test('fetching uiSettings once', async () => {
 
   expect(await res1).toBe(value);
   expect(await res2).toBe(value);
+});
+
+test('fetching uiSettings once for different keys', async () => {
+  const request = httpServerMock.createKibanaRequest();
+  const soClient = coreMock.createStart().savedObjects.getScopedClient(request);
+  const uiSettings = coreMock.createStart().uiSettings.asScopedToClient(soClient);
+
+  const key1 = 'key1';
+  const value1 = 'value1';
+
+  const key2 = 'key2';
+  const value2 = 'value2';
+
+  const spy = jest
+    .spyOn(uiSettings, 'getAll')
+    .mockImplementation(() => Promise.resolve({ [key1]: value1, [key2]: value2 }));
+
+  const cachedUiSettings = new CachedUiSettingsClient(uiSettings);
+
+  const res1 = cachedUiSettings.get(key1);
+  const res2 = cachedUiSettings.get(key2);
+
+  expect(spy).toHaveBeenCalledTimes(1); // check that internally uiSettings.getAll() called only once
+
+  expect(await res1).toBe(value1);
+  expect(await res2).toBe(value2);
 });
