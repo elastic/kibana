dd409a112f77d372a473714db4d09cab34623455
Caroline Horn <549577+cchaos@users.noreply.github.com>
2021-10-06 08:50:10 -0400

    [Observability] New "No Data" screens (#107709)
    Adds empty states for all of Obs that lead to their various ingest flows.

--- x-pack/test/functional/apps/infra/feature_controls/infrastructure_spaces.ts
+++ x-pack/test/functional/apps/infra/feature_controls/infrastructure_spaces.ts
@@ -51,7 +51,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
         await PageObjects.common.navigateToApp('infraOps', {
           basePath: '/s/custom_space',
         });
-        await testSubjects.existOrFail('~noMetricsIndicesPrompt');
+        await testSubjects.existOrFail('~noDataPage');
       });
     });
 
@@ -118,7 +118,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
         await PageObjects.common.navigateToApp('infraOps', {
           basePath: '/s/custom_space',
         });
-        await testSubjects.existOrFail('~noMetricsIndicesPrompt');
+        await testSubjects.existOrFail('~noDataPage');
       });
     });
 
@@ -143,7 +143,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
         await PageObjects.common.navigateToApp('infraOps', {
           basePath: '/s/custom_space',
         });
-        await testSubjects.existOrFail('~noMetricsIndicesPrompt');
+        await testSubjects.existOrFail('~noDataPage');
       });
     });
   });
