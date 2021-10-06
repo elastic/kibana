1cbc3bfbd95c6ef51c6d69edfa7c09529fefacf9
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2021-10-06 15:09:53 +0200

    ci(.buildkite/scripts/steps/test/performance.sh): run performance tests in parallel

--- .buildkite/scripts/steps/test/performance.sh
+++ .buildkite/scripts/steps/test/performance.sh
@@ -5,4 +5,10 @@ set -uo pipefail
 ITERATION_COUNT=`echo "$(buildkite-agent meta-data get performance-test-iteration-count)" | xargs`
 TARGET_BRANCH=`echo "$(buildkite-agent meta-data get performance-test-target-branch)" | xargs`
 
-echo "Performance test will be run at ${TARGET_BRANCH} ${ITERATION_COUNT} times"
+tput setab 2; tput setaf 0; echo "Performance test will be run at ${TARGET_BRANCH} ${ITERATION_COUNT} times"
+
+cat << EOF | buildkite-agent pipeline upload
+steps:
+  - command: "git checkout $TARGET_BRANCH && git fetch && cd x-pack && yarn install && yarn test:jest"
+    parallelism: "$ITERATION_COUNT"
+EOF
