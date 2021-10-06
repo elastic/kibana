59202531a1eafcd0ee9ebd182c72dafad7a1840a
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2021-10-06 18:09:56 +0200

    fix(.buildkite/scripts/steps/test/performance.sh): run performance tests from script
    - Replace manually provided branch info to environment value
    - Use "scripts/functional_tests" to run performance tests

--- .buildkite/scripts/steps/test/performance.sh
+++ .buildkite/scripts/steps/test/performance.sh
@@ -3,12 +3,11 @@
 set -uo pipefail
 
 ITERATION_COUNT=`echo "$(buildkite-agent meta-data get performance-test-iteration-count)" | xargs`
-TARGET_BRANCH=`echo "$(buildkite-agent meta-data get performance-test-target-branch)" | xargs`
 
-tput setab 2; tput setaf 0; echo "Performance test will be run at ${TARGET_BRANCH} ${ITERATION_COUNT} times"
+tput setab 2; tput setaf 0; echo "Performance test will be run at ${BUILDKITE_BRANCH} ${ITERATION_COUNT} times"
 
 cat << EOF | buildkite-agent pipeline upload
 steps:
-  - command: "git checkout $TARGET_BRANCH && git fetch && yarn kbn bootstrap && cd x-pack && yarn install && yarn test:jest"
+  - command: "yarn kbn bootstrap && node scripts/functional_tests --config x-pack/test/performance/config.ts"
     parallelism: "$ITERATION_COUNT"
 EOF
