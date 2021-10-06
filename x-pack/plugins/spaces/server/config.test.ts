6ef198a1aed4019b9f9a22be642929ca08914081
Thomas Watson <w@tson.dk>
2021-10-06 10:31:17 +0200

    Apply suggestions from code review

--- x-pack/plugins/spaces/server/config.test.ts
+++ x-pack/plugins/spaces/server/config.test.ts
@@ -39,7 +39,7 @@ describe('spaces config', () => {
 
         expect(messages).toMatchInlineSnapshot(`
           Array [
-            "In 8.0.0 and later, using \\"xpack.spaces.enabled\\" is unsupported, and the Spaces plugin is always enabled.",
+            "This setting will be removed in 8.0 and the Spaces plugin will always be enabled.",
           ]
         `);
         expect(migrated).toEqual(originalConfig);
