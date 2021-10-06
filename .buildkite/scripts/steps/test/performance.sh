57f99fbd7b5d493301a476e38f515a34c48cb5eb
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2021-10-06 14:55:06 +0200

    fix(.buildkite/scripts/steps/test/performance.sh): enclose value generating command

--- .buildkite/scripts/steps/test/performance.sh
+++ .buildkite/scripts/steps/test/performance.sh
@@ -2,7 +2,7 @@
 
 set -uo pipefail
 
-ITERATION_COUNT=echo "$(buildkite-agent meta-data get performance-test-iteration-count)" | xargs
-TARGET_BRANCH=echo "$(buildkite-agent meta-data get performance-test-target-branch)" | xargs
+ITERATION_COUNT=`echo "$(buildkite-agent meta-data get performance-test-iteration-count)" | xargs`
+TARGET_BRANCH=`echo "$(buildkite-agent meta-data get performance-test-target-branch)" | xargs`
 
 echo "Performance test will be run at ${TARGET_BRANCH} ${ITERATION_COUNT} times"
