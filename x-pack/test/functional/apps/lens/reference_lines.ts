7e580dcc7535677b08becd3ac9b8cb8928e4d25a
dej611 <dej611@gmail.com>
2021-10-07 19:03:25 +0200

    :white_check_mark: Fix functional tests after renaming

--- x-pack/test/functional/apps/lens/reference_lines.ts
+++ x-pack/test/functional/apps/lens/reference_lines.ts
@@ -14,21 +14,21 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const retry = getService('retry');
   const testSubjects = getService('testSubjects');
 
-  describe('lens thresholds tests', () => {
-    it('should show a disabled threshold layer button if no data dimension is defined', async () => {
+  describe('lens reference lines tests', () => {
+    it('should show a disabled reference layer button if no data dimension is defined', async () => {
       await PageObjects.visualize.navigateToNewVisualization();
       await PageObjects.visualize.clickVisType('lens');
 
       await testSubjects.click('lnsLayerAddButton');
       await retry.waitFor('wait for layer popup to appear', async () =>
-        testSubjects.exists(`lnsLayerAddButton-threshold`)
+        testSubjects.exists(`lnsLayerAddButton-referenceLine`)
       );
       expect(
-        await (await testSubjects.find(`lnsLayerAddButton-threshold`)).getAttribute('disabled')
+        await (await testSubjects.find(`lnsLayerAddButton-referenceLine`)).getAttribute('disabled')
       ).to.be('true');
     });
 
-    it('should add a threshold layer with a static value in it', async () => {
+    it('should add a reference layer with a static value in it', async () => {
       await PageObjects.lens.goToTimeRange();
 
       await PageObjects.lens.configureDimension({
@@ -43,29 +43,28 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         field: 'bytes',
       });
 
-      await PageObjects.lens.createLayer('threshold');
+      await PageObjects.lens.createLayer('referenceLine');
 
       expect((await find.allByCssSelector(`[data-test-subj^="lns-layerPanel-"]`)).length).to.eql(2);
       expect(
         await (
-          await testSubjects.find('lnsXY_yThresholdLeftPanel > lns-dimensionTrigger')
+          await testSubjects.find('lnsXY_yReferenceLineLeftPanel > lns-dimensionTrigger')
         ).getVisibleText()
       ).to.eql('Static value: 4992.44');
     });
 
-    it('should create a dynamic threshold when dragging a field to a threshold dimension group', async () => {
+    it('should create a dynamic referenceLine when dragging a field to a referenceLine dimension group', async () => {
       await PageObjects.lens.dragFieldToDimensionTrigger(
         'bytes',
-        'lnsXY_yThresholdLeftPanel > lns-empty-dimension'
+        'lnsXY_yReferenceLineLeftPanel > lns-empty-dimension'
       );
 
-      expect(await PageObjects.lens.getDimensionTriggersTexts('lnsXY_yThresholdLeftPanel')).to.eql([
-        'Static value: 4992.44',
-        'Median of bytes',
-      ]);
+      expect(
+        await PageObjects.lens.getDimensionTriggersTexts('lnsXY_yReferenceLineLeftPanel')
+      ).to.eql(['Static value: 4992.44', 'Median of bytes']);
     });
 
-    it('should add a new group to the threshold layer when a right axis is enabled', async () => {
+    it('should add a new group to the reference layer when a right axis is enabled', async () => {
       await PageObjects.lens.configureDimension({
         dimension: 'lnsXY_yDimensionPanel > lns-empty-dimension',
         operation: 'average',
@@ -77,42 +76,46 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
       await PageObjects.lens.closeDimensionEditor();
 
-      await testSubjects.existOrFail('lnsXY_yThresholdRightPanel > lns-empty-dimension');
+      await testSubjects.existOrFail('lnsXY_yReferenceLineRightPanel > lns-empty-dimension');
     });
 
-    it('should carry the style when moving a threshold to another group', async () => {
+    it('should carry the style when moving a reference line to another group', async () => {
       // style it enabling the fill
-      await testSubjects.click('lnsXY_yThresholdLeftPanel > lns-dimensionTrigger');
-      await testSubjects.click('lnsXY_fill_below');
+      await testSubjects.click('lnsXY_yReferenceLineLeftPanel > lns-dimensionTrigger');
+      await testSubjects.click('lnsXY_referenceLine_fill_below');
       await PageObjects.lens.closeDimensionEditor();
 
       // drag and drop it to the left axis
       await PageObjects.lens.dragDimensionToDimension(
-        'lnsXY_yThresholdLeftPanel > lns-dimensionTrigger',
-        'lnsXY_yThresholdRightPanel > lns-empty-dimension'
+        'lnsXY_yReferenceLineLeftPanel > lns-dimensionTrigger',
+        'lnsXY_yReferenceLineRightPanel > lns-empty-dimension'
       );
 
-      await testSubjects.click('lnsXY_yThresholdRightPanel > lns-dimensionTrigger');
+      await testSubjects.click('lnsXY_yReferenceLineRightPanel > lns-dimensionTrigger');
       expect(
-        await find.existsByCssSelector('[data-test-subj="lnsXY_fill_below"][class$="isSelected"]')
+        await find.existsByCssSelector(
+          '[data-test-subj="lnsXY_referenceLine_fill_below"][class$="isSelected"]'
+        )
       ).to.be(true);
       await PageObjects.lens.closeDimensionEditor();
     });
 
-    it('should duplicate also the original style when duplicating a threshold', async () => {
+    it('should duplicate also the original style when duplicating a reference line', async () => {
       // drag and drop to the empty field to generate a duplicate
       await PageObjects.lens.dragDimensionToDimension(
-        'lnsXY_yThresholdRightPanel > lns-dimensionTrigger',
-        'lnsXY_yThresholdRightPanel > lns-empty-dimension'
+        'lnsXY_yReferenceLineRightPanel > lns-dimensionTrigger',
+        'lnsXY_yReferenceLineRightPanel > lns-empty-dimension'
       );
 
       await (
         await find.byCssSelector(
-          '[data-test-subj="lnsXY_yThresholdRightPanel"]:nth-child(2) [data-test-subj="lns-dimensionTrigger"]'
+          '[data-test-subj="lnsXY_yReferenceLineRightPanel"]:nth-child(2) [data-test-subj="lns-dimensionTrigger"]'
         )
       ).click();
       expect(
-        await find.existsByCssSelector('[data-test-subj="lnsXY_fill_below"][class$="isSelected"]')
+        await find.existsByCssSelector(
+          '[data-test-subj="lnsXY_referenceLine_fill_below"][class$="isSelected"]'
+        )
       ).to.be(true);
       await PageObjects.lens.closeDimensionEditor();
     });
