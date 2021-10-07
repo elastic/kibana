e686e868e54073dc9e670765a03e461e65012b43
restrry <restrry@gmail.com>
2021-10-07 15:19:02 +0200

    fix errors in lens

--- x-pack/plugins/lens/server/routes/field_stats.ts
+++ x-pack/plugins/lens/server/routes/field_stats.ts
@@ -4,7 +4,8 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { errors, estypes } from '@elastic/elasticsearch';
+import { errors } from '@elastic/elasticsearch';
+import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import DateMath from '@elastic/datemath';
 import { schema } from '@kbn/config-schema';
 import { CoreSetup } from 'src/core/server';
