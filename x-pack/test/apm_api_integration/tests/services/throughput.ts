ec5d5bd36ba2739a5ed31d2a4f93086b733dffe0
cauemarcondes <caue.marcondes@elastic.co>
2021-10-07 14:49:17 -0400

    using data generator

--- x-pack/test/apm_api_integration/tests/services/throughput.ts
+++ x-pack/test/apm_api_integration/tests/services/throughput.ts
@@ -7,13 +7,10 @@
 
 import { service, timerange } from '@elastic/apm-generator';
 import expect from '@kbn/expect';
-import { first, last, mean, meanBy, uniq } from 'lodash';
+import { first, last, meanBy } from 'lodash';
 import moment from 'moment';
-import { ENVIRONMENT_ALL } from '../../../../plugins/apm/common/environment_filter_values';
 import { isFiniteNumber } from '../../../../plugins/apm/common/utils/is_finite_number';
 import { APIReturnType } from '../../../../plugins/apm/public/services/rest/createCallApmApi';
-import { PromiseReturnType } from '../../../../plugins/observability/typings/common';
-import archives_metadata from '../../common/fixtures/es_archiver/archives_metadata';
 import { FtrProviderContext } from '../../common/ftr_provider_context';
 import { registry } from '../../common/registry';
 import { roundNumber } from '../../utils';
@@ -24,31 +21,59 @@ export default function ApiTest({ getService }: FtrProviderContext) {
   const apmApiClient = getService('apmApiClient');
   const traceData = getService('traceData');
 
-  const archiveName = 'apm_synthetic_8.0.0';
-  const metadata = archives_metadata[archiveName];
+  const serviceName = 'synth-go';
+  const start = new Date('2021-01-01T00:00:00.000Z').getTime();
+  const end = new Date('2021-01-01T00:15:00.000Z').getTime() - 1;
 
-  registry.when(
-    'Throughput with statically generated data',
-    { config: 'basic', archives: ['apm_8.0.0_empty'] },
-    () => {
-      const start = new Date('2021-01-01T00:00:00.000Z').getTime();
-      const end = new Date('2021-01-01T00:15:00.000Z').getTime() - 1;
+  async function callApi(overrides?: {
+    start?: string;
+    end?: string;
+    transactionType?: string;
+    environment?: string;
+    kuery?: string;
+    comparisonStart?: string;
+    comparisonEnd?: string;
+  }) {
+    const response = await apmApiClient.readUser({
+      endpoint: 'GET /api/apm/services/{serviceName}/throughput',
+      params: {
+        path: {
+          serviceName: 'synth-go',
+        },
+        query: {
+          start: new Date(start).toISOString(),
+          end: new Date(end).toISOString(),
+          transactionType: 'request',
+          environment: 'ENVIRONMENT_ALL',
+          kuery: '',
+          ...overrides,
+        },
+      },
+    });
 
-      const GO_PROD_RATE = 10;
-      const GO_DEV_RATE = 5;
-      const JAVA_PROD_RATE = 20;
+    return response;
+  }
 
+  registry.when('Throughput when data is not loaded', { config: 'basic', archives: [] }, () => {
+    it('handles the empty state', async () => {
+      const response = await callApi();
+      expect(response.status).to.be(200);
+      expect(response.body.currentPeriod.length).to.be(0);
+      expect(response.body.previousPeriod.length).to.be(0);
+    });
+  });
+
+  registry.when('data is loaded', { config: 'basic', archives: ['apm_8.0.0_empty'] }, () => {
+    describe('Throughput chart api', () => {
       before(async () => {
-        const serviceGoProdInstance = service('synth-go', 'production', 'go').instance(
+        const GO_PROD_RATE = 10;
+        const GO_DEV_RATE = 5;
+        const serviceGoProdInstance = service(serviceName, 'production', 'go').instance(
           'instance-a'
         );
-        const serviceGoDevInstance = service('synth-go', 'development', 'go').instance(
+        const serviceGoDevInstance = service(serviceName, 'development', 'go').instance(
           'instance-b'
         );
-        const serviceJavaInstance = service('synth-java', 'production', 'java').instance(
-          'instance-c'
-        );
-
         await traceData.index([
           ...timerange(start, end)
             .interval('1s')
@@ -70,148 +95,22 @@ export default function ApiTest({ getService }: FtrProviderContext) {
                 .timestamp(timestamp)
                 .serialize()
             ),
-          ...timerange(start, end)
-            .interval('1s')
-            .rate(JAVA_PROD_RATE)
-            .flatMap((timestamp) =>
-              serviceJavaInstance
-                .transaction('POST /api/product/buy')
-                .duration(1000)
-                .timestamp(timestamp)
-                .serialize()
-            ),
         ]);
       });
 
       after(() => traceData.clean());
 
-      async function callApi(overrides?: {
-        start?: string;
-        end?: string;
-        transactionType?: string;
-        environment?: string;
-        kuery?: string;
-      }) {
-        const response = await apmApiClient.readUser({
-          endpoint: 'GET /api/apm/services/{serviceName}/throughput',
-          params: {
-            path: {
-              serviceName: 'synth-go',
-            },
-            query: {
-              start: new Date(start).toISOString(),
-              end: new Date(end).toISOString(),
-              transactionType: 'request',
-              environment: 'production',
-              kuery: 'processor.event:transaction',
-              ...overrides,
-            },
-          },
-        });
-
-        return response.body;
-      }
-
-      describe('when calling it with the default parameters', () => {
-        let body: PromiseReturnType<typeof callApi>;
-
-        before(async () => {
-          body = await callApi();
-        });
-
-        it('returns the expected throughput', () => {
-          const throughputValues = uniq(body.currentPeriod.map((coord) => coord.y));
-          expect(throughputValues).to.eql([GO_PROD_RATE]);
-        });
-      });
-
-      describe('when setting environment to all', () => {
-        let body: PromiseReturnType<typeof callApi>;
-
-        before(async () => {
-          body = await callApi({
-            environment: ENVIRONMENT_ALL.value,
-          });
-        });
-
-        it('returns data for all environments', () => {
-          const throughputValues = body.currentPeriod.map(({ y }) => y);
-          expect(uniq(throughputValues)).to.eql([GO_PROD_RATE + GO_DEV_RATE]);
-        });
-      });
-
-      describe('when defining a kuery', () => {
-        let body: PromiseReturnType<typeof callApi>;
-
-        before(async () => {
-          body = await callApi({
-            kuery: `processor.event:transaction and transaction.name:"GET /api/product/:id"`,
-            environment: ENVIRONMENT_ALL.value,
-          });
-        });
-
-        it('returns data that matches the kuery', () => {
-          const throughputValues = body.currentPeriod.map(({ y }) => y);
-          expect(uniq(throughputValues)).to.eql([GO_DEV_RATE]);
-        });
-      });
-    }
-  );
-
-  registry.when('Throughput when data is not loaded', { config: 'basic', archives: [] }, () => {
-    it('handles the empty state', async () => {
-      const response = await apmApiClient.readUser({
-        endpoint: 'GET /api/apm/services/{serviceName}/throughput',
-        params: {
-          path: {
-            serviceName: 'opbeans-go',
-          },
-          query: {
-            start: metadata.start,
-            end: metadata.end,
-            transactionType: 'request',
-            environment: 'ENVIRONMENT_ALL',
-            kuery: '',
-          },
-        },
-      });
-
-      expect(response.status).to.be(200);
-      expect(response.body.currentPeriod.length).to.be(0);
-      expect(response.body.previousPeriod.length).to.be(0);
-    });
-  });
-
-  async function fetchThroughput(processorEvent: 'metric' | 'transaction') {
-    const throughputResponse = await apmApiClient.readUser({
-      endpoint: 'GET /api/apm/services/{serviceName}/throughput',
-      params: {
-        path: { serviceName: 'opbeans-go' },
-        query: {
-          start: metadata.start,
-          end: metadata.end,
-          transactionType: 'request',
-          environment: 'ENVIRONMENT_ALL',
-          kuery: `processor.event : "${processorEvent}"`,
-        },
-      },
-    });
-    return throughputResponse.body;
-  }
-
-  registry.when(
-    'Throughput when data is loaded',
-    { config: 'basic', archives: [archiveName] },
-    () => {
-      describe('without comparisons', () => {
+      describe('compare transactions and metrics based throughput', () => {
         let throughputMetrics: ThroughputReturn;
         let throughputTransactions: ThroughputReturn;
 
         before(async () => {
-          [throughputMetrics, throughputTransactions] = await Promise.all([
-            fetchThroughput('metric'),
-            fetchThroughput('transaction'),
+          const [throughputMetricsResponse, throughputTransactionsResponse] = await Promise.all([
+            callApi({ kuery: 'processor.event : "metric"' }),
+            callApi({ kuery: 'processor.event : "transaction"' }),
           ]);
+          throughputMetrics = throughputMetricsResponse.body;
+          throughputTransactions = throughputTransactionsResponse.body;
         });
 
         it('returns some transactions data', () => {
@@ -229,9 +128,7 @@ export default function ApiTest({ getService }: FtrProviderContext) {
         it('has same mean value for metrics and transactions data', () => {
           const transactionsMean = meanBy(throughputTransactions.currentPeriod, 'y');
           const metricsMean = meanBy(throughputMetrics.currentPeriod, 'y');
-          expect(transactionsMean).to.equal(metricsMean);
-          expectSnapshot(transactionsMean).toMatchInline(`100`);
-          expectSnapshot(metricsMean).toMatchInline(`100`);
+          [transactionsMean, metricsMean].every((value) => roundNumber(value) === '900.0');
         });
 
         it('has a bucket size of 10 seconds for transactions data', () => {
@@ -248,73 +145,85 @@ export default function ApiTest({ getService }: FtrProviderContext) {
           expect(timeIntervalAsMinutes).to.equal(1);
         });
       });
+    });
 
-      describe('with comparisons', () => {
-        let throughputResponse: ThroughputReturn;
+    describe('switch environment', () => {
+      let throughput: ThroughputReturn;
 
-        before(async () => {
-          const response = await apmApiClient.readUser({
-            endpoint: 'GET /api/apm/services/{serviceName}/throughput',
-            params: {
-              path: {
-                serviceName: 'opbeans-go',
-              },
-              query: {
-                transactionType: 'request',
-                start: moment(metadata.end).subtract(15, 'minutes').toISOString(),
-                end: metadata.end,
-                comparisonStart: metadata.start,
-                comparisonEnd: moment(metadata.start).add(15, 'minutes').toISOString(),
-                environment: 'ENVIRONMENT_ALL',
-                kuery: '',
-              },
-            },
-          });
-
-          throughputResponse = response.body;
-        });
-
-        it('returns some data', () => {
-          expect(throughputResponse.currentPeriod.length).to.be.greaterThan(0);
-          expect(throughputResponse.previousPeriod.length).to.be.greaterThan(0);
-
-          const currentPeriodNonNullDataPoints = throughputResponse.currentPeriod.filter(({ y }) =>
-            isFiniteNumber(y)
-          );
-          const previousPeriodNonNullDataPoints = throughputResponse.previousPeriod.filter(
-            ({ y }) => isFiniteNumber(y)
-          );
-
-          expect(currentPeriodNonNullDataPoints.length).to.be.greaterThan(0);
-          expect(previousPeriodNonNullDataPoints.length).to.be.greaterThan(0);
-        });
-
-        it('has same start time for both periods', () => {
-          expect(
-            new Date(first(throughputResponse.currentPeriod)?.x ?? NaN).toISOString()
-          ).to.equal(new Date(first(throughputResponse.previousPeriod)?.x ?? NaN).toISOString());
-        });
-
-        it('has same end time for both periods', () => {
-          expect(new Date(last(throughputResponse.currentPeriod)?.x ?? NaN).toISOString()).to.equal(
-            new Date(last(throughputResponse.previousPeriod)?.x ?? NaN).toISOString()
-          );
-        });
-
-        it('returns same number of buckets for both periods', () => {
-          expect(throughputResponse.currentPeriod.length).to.be(
-            throughputResponse.previousPeriod.length
-          );
-        });
-
-        it('has same mean value for both periods', () => {
-          const currentPeriodAvg = mean(throughputResponse.currentPeriod.map((d) => d.y));
-          const previousPeriodAvg = mean(throughputResponse.previousPeriod.map((d) => d.y));
-          expect(currentPeriodAvg).to.equal(previousPeriodAvg);
-          expectSnapshot(roundNumber(currentPeriodAvg)).toMatchInline(`"98.90"`);
-          expectSnapshot(roundNumber(previousPeriodAvg)).toMatchInline(`"98.90"`);
-        });
+      before(async () => {
+        const throughputMetricsResponse = await callApi({ environment: 'production' });
+        throughput = throughputMetricsResponse.body;
       });
-    }
-  );
+
+      it('returns some data', () => {
+        expect(throughput.currentPeriod.length).to.be.greaterThan(0);
+        const hasData = throughput.currentPeriod.some(({ y }) => isFiniteNumber(y));
+        expect(hasData).to.equal(true);
+      });
+
+      it('returns throughput related to production environment', () => {
+        const throughputMean = meanBy(throughput.currentPeriod, 'y');
+        expect(roundNumber(throughputMean)).to.be.equal('600.0');
+      });
+    });
+
+    describe('time comparisons', () => {
+      let throughputResponse: ThroughputReturn;
+
+      before(async () => {
+        const response = await callApi({
+          start: moment(end).subtract(15, 'minutes').toISOString(),
+          end: new Date(end).toISOString(),
+          comparisonStart: new Date(start).toISOString(),
+          comparisonEnd: moment(start).add(15, 'minutes').toISOString(),
+        });
+        throughputResponse = response.body;
+      });
+
+      it('returns some data', () => {
+        expect(throughputResponse.currentPeriod.length).to.be.greaterThan(0);
+        expect(throughputResponse.previousPeriod.length).to.be.greaterThan(0);
+
+        const hasCurrentPeriodData = throughputResponse.currentPeriod.some(({ y }) =>
+          isFiniteNumber(y)
+        );
+        const hasPreivousPeriodData = throughputResponse.previousPeriod.some(({ y }) =>
+          isFiniteNumber(y)
+        );
+
+        expect(hasCurrentPeriodData).to.equal(true);
+        expect(hasPreivousPeriodData).to.equal(true);
+      });
+
+      it('has same start time for both periods', () => {
+        expect(new Date(first(throughputResponse.currentPeriod)?.x ?? NaN).toISOString()).to.equal(
+          new Date(first(throughputResponse.previousPeriod)?.x ?? NaN).toISOString()
+        );
+      });
+
+      it('has same end time for both periods', () => {
+        expect(new Date(last(throughputResponse.currentPeriod)?.x ?? NaN).toISOString()).to.equal(
+          new Date(last(throughputResponse.previousPeriod)?.x ?? NaN).toISOString()
+        );
+      });
+
+      it('returns same number of buckets for both periods', () => {
+        expect(throughputResponse.currentPeriod.length).to.be(
+          throughputResponse.previousPeriod.length
+        );
+      });
+
+      it('has same mean value for both periods', () => {
+        const currentPeriodMean = meanBy(
+          throughputResponse.currentPeriod.filter((item) => isFiniteNumber(item.y) && item.y > 0),
+          'y'
+        );
+        const previousPeriodMean = meanBy(
+          throughputResponse.previousPeriod.filter((item) => isFiniteNumber(item.y) && item.y > 0),
+          'y'
+        );
+        [currentPeriodMean, previousPeriodMean].every((value) => roundNumber(value) === '900.0');
+      });
+    });
+  });
 }
---
+++ x-pack/test/apm_api_integration/tests/throughput/service_apis.ts
@@ -0,0 +1,223 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { service, timerange } from '@elastic/apm-generator';
+import expect from '@kbn/expect';
+import { meanBy, sumBy } from 'lodash';
+import { LatencyAggregationType } from '../../../../plugins/apm/common/latency_aggregation_types';
+import { PromiseReturnType } from '../../../../plugins/observability/typings/common';
+import { FtrProviderContext } from '../../common/ftr_provider_context';
+import { registry } from '../../common/registry';
+import { roundNumber } from '../../utils';
+
+export default function ApiTest({ getService }: FtrProviderContext) {
+  const apmApiClient = getService('apmApiClient');
+  const traceData = getService('traceData');
+
+  const serviceName = 'synth-go';
+  const start = new Date('2021-01-01T00:00:00.000Z').getTime();
+  const end = new Date('2021-01-01T00:15:00.000Z').getTime() - 1;
+
+  async function getThroughputValues(processorEvent: 'transaction' | 'metric') {
+    const commonQuery = {
+      start: new Date(start).toISOString(),
+      end: new Date(end).toISOString(),
+      environment: 'ENVIRONMENT_ALL',
+    };
+    const [
+      serviceInventoryAPIResponse,
+      serviceThroughputAPIResponse,
+      transactionsGroupDetailsAPIResponse,
+      serviceInstancesAPIResponse,
+    ] = await Promise.all([
+      apmApiClient.readUser({
+        endpoint: 'GET /api/apm/services',
+        params: {
+          query: {
+            ...commonQuery,
+            kuery: `service.name : "${serviceName}" and processor.event : "${processorEvent}"`,
+          },
+        },
+      }),
+      apmApiClient.readUser({
+        endpoint: 'GET /api/apm/services/{serviceName}/throughput',
+        params: {
+          path: { serviceName },
+          query: {
+            ...commonQuery,
+            kuery: `processor.event : "${processorEvent}"`,
+            transactionType: 'request',
+          },
+        },
+      }),
+      apmApiClient.readUser({
+        endpoint: `GET /api/apm/services/{serviceName}/transactions/groups/main_statistics`,
+        params: {
+          path: { serviceName },
+          query: {
+            ...commonQuery,
+            kuery: `processor.event : "${processorEvent}"`,
+            transactionType: 'request',
+            latencyAggregationType: 'avg' as LatencyAggregationType,
+          },
+        },
+      }),
+      apmApiClient.readUser({
+        endpoint: `GET /api/apm/services/{serviceName}/service_overview_instances/main_statistics`,
+        params: {
+          path: { serviceName },
+          query: {
+            ...commonQuery,
+            kuery: `processor.event : "${processorEvent}"`,
+            transactionType: 'request',
+            latencyAggregationType: 'avg' as LatencyAggregationType,
+          },
+        },
+      }),
+      // TODO: not using it for now since the instance name is empty for metric documents
+      // apmApiClient.readUser({
+      //   endpoint: `GET /api/apm/services/{serviceName}/service_overview_instances/detailed_statistics`,
+      //   params: {
+      //     path: { serviceName },
+      //     query: {
+      //       ...commonQuery,
+      //       numBuckets: 20,
+      //       kuery: `processor.event : "${processorEvent}"`,
+      //       transactionType: 'request',
+      //       latencyAggregationType: 'avg' as LatencyAggregationType,
+      //       serviceNodeIds: JSON.stringify(['instance-a', 'instance-b']),
+      //     },
+      //   },
+      // }),
+    ]);
+
+    const serviceInventoryThroughput = serviceInventoryAPIResponse.body.items[0].throughput;
+
+    const throughputChartApiMean = meanBy(serviceThroughputAPIResponse.body.currentPeriod, 'y');
+
+    const transactionsGroupThroughputSum = sumBy(
+      transactionsGroupDetailsAPIResponse.body.transactionGroups,
+      'throughput'
+    );
+
+    const serviceInstancesThroughputSum = sumBy(
+      serviceInstancesAPIResponse.body.currentPeriod,
+      'throughput'
+    );
+
+    // TODO: not using it for now since the instance name is empty for metric documents
+    // const serviceInstancesDetailedThroughputSum = sum(
+    //   Object.values(serviceInstancesDetailedAPIResponse.body.currentPeriod).map(({ throughput }) =>
+    //     meanBy(throughput, 'y')
+    //   )
+    // );
+
+    return {
+      serviceInventoryThroughput,
+      throughputChartApiMean,
+      transactionsGroupThroughputSum,
+      serviceInstancesThroughputSum,
+    };
+  }
+
+  let throughputMetricValues: PromiseReturnType<typeof getThroughputValues>;
+  let throughputTransactionValues: PromiseReturnType<typeof getThroughputValues>;
+
+  registry.when('Services APIs', { config: 'basic', archives: ['apm_8.0.0_empty'] }, () => {
+    describe('when data is loaded ', () => {
+      before(async () => {
+        const GO_PROD_RATE = 10;
+        const GO_DEV_RATE = 5;
+        const serviceGoProdInstance = service(serviceName, 'production', 'go').instance(
+          'instance-a'
+        );
+        const serviceGoDevInstance = service(serviceName, 'development', 'go').instance(
+          'instance-b'
+        );
+        await traceData.index([
+          ...timerange(start, end)
+            .interval('1s')
+            .rate(GO_PROD_RATE)
+            .flatMap((timestamp) =>
+              serviceGoProdInstance
+                .transaction('GET /api/product/list')
+                .duration(1000)
+                .timestamp(timestamp)
+                .serialize()
+            ),
+          ...timerange(start, end)
+            .interval('1s')
+            .rate(GO_DEV_RATE)
+            .flatMap((timestamp) =>
+              serviceGoDevInstance
+                .transaction('GET /api/product/:id')
+                .duration(1000)
+                .timestamp(timestamp)
+                .serialize()
+            ),
+        ]);
+      });
+
+      after(() => traceData.clean());
+
+      describe('compare throughput value between service inventory, throughput chart, service inventory and transactions apis', () => {
+        before(async () => {
+          [throughputTransactionValues, throughputMetricValues] = await Promise.all([
+            getThroughputValues('transaction'),
+            getThroughputValues('metric'),
+          ]);
+        });
+
+        describe('Transaction-based throughput', () => {
+          it('returns same throughput value', () => {
+            Object.values(throughputTransactionValues).every(
+              (value) => roundNumber(value) === '900.0'
+            );
+          });
+        });
+
+        describe('Metric-based throughput', () => {
+          it('returns same throughput value', () => {
+            Object.values(throughputMetricValues).every((value) => roundNumber(value) === '900.0');
+          });
+        });
+
+        describe('Compare Transaction-based and Metric-based throughput', () => {
+          it('returns same throughput value on service inventory api', () => {
+            expect(roundNumber(throughputMetricValues.serviceInventoryThroughput)).to.be(
+              roundNumber(throughputTransactionValues.serviceInventoryThroughput)
+            );
+          });
+
+          it('returns same throughput value on throughput chart api', () => {
+            expect(roundNumber(throughputMetricValues.throughputChartApiMean)).to.be(
+              roundNumber(throughputTransactionValues.throughputChartApiMean)
+            );
+          });
+
+          it('returns same throughput value on transactions group api', () => {
+            expect(roundNumber(throughputMetricValues.transactionsGroupThroughputSum)).to.be(
+              roundNumber(throughputTransactionValues.transactionsGroupThroughputSum)
+            );
+          });
+
+          it('returns same throughput value on service instances api', () => {
+            expect(roundNumber(throughputMetricValues.serviceInstancesThroughputSum)).to.be(
+              roundNumber(throughputTransactionValues.serviceInstancesThroughputSum)
+            );
+          });
+
+          // TODO: not using it for now since the instance name is empty for metric documents
+          // it('returns same throughput value on service instances detailed api', () => {
+          //   expect(roundNumber(throughputMetricValues.serviceInstancesDetailedThroughputSum)).to.be(
+          //     roundNumber(throughputTransactionValues.serviceInstancesDetailedThroughputSum)
+          //   );
+          // });
+        });
+      });
+    });
+  });
+}
