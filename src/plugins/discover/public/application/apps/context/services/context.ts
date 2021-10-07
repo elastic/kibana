779d194b59b2f3278a39083db6220fa4d70ee243
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-10-07 15:53:46 +0200

    Improve context code

--- src/plugins/discover/public/application/apps/context/services/context.ts
+++ src/plugins/discover/public/application/apps/context/services/context.ts
@@ -5,7 +5,7 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import { Filter, IndexPattern, IndexPatternsContract, SearchSource } from 'src/plugins/data/public';
+import { Filter, IndexPattern, SearchSource } from 'src/plugins/data/public';
 import { reverseSortDir, SortDirection } from './utils/sorting';
 import { convertIsoToMillis, extractNanos } from './utils/date_conversion';
 import { fetchHitsInInterval } from './utils/fetch_hits_in_interval';
@@ -25,88 +25,82 @@ const DAY_MILLIS = 24 * 60 * 60 * 1000;
 // look from 1 day up to 10000 days into the past and future
 const LOOKUP_OFFSETS = [0, 1, 7, 30, 365, 10000].map((days) => days * DAY_MILLIS);
 
-function fetchContextProvider(indexPatterns: IndexPatternsContract, useNewFieldsApi?: boolean) {
-  return {
-    fetchSurroundingDocs,
-  };
-
-  /**
-   * Fetch successor or predecessor documents of a given anchor document
-   *
-   * @param {SurrDocType} type - `successors` or `predecessors`
-   * @param {string} indexPatternId
-   * @param {EsHitRecord} anchor - anchor record
-   * @param {string} timeField - name of the timefield, that's sorted on
-   * @param {string} tieBreakerField - name of the tie breaker, the 2nd sort field
-   * @param {SortDirection} sortDir - direction of sorting
-   * @param {number} size - number of records to retrieve
-   * @param {Filter[]} filters - to apply in the elastic query
-   * @returns {Promise<object[]>}
-   */
-  async function fetchSurroundingDocs(
-    type: SurrDocType,
-    indexPatternId: string,
-    anchor: EsHitRecord,
-    timeField: string,
-    tieBreakerField: string,
-    sortDir: SortDirection,
-    size: number,
-    filters: Filter[]
-  ): Promise<EsHitRecordList> {
-    if (typeof anchor !== 'object' || anchor === null || !size) {
-      return [];
-    }
-    const indexPattern = await indexPatterns.get(indexPatternId);
-    const { data } = getServices();
-    const searchSource = data.search.searchSource.createEmpty() as SearchSource;
-    updateSearchSource(searchSource, indexPattern, filters, Boolean(useNewFieldsApi));
-    const sortDirToApply = type === SurrDocType.SUCCESSORS ? sortDir : reverseSortDir(sortDir);
-
-    const nanos = indexPattern.isTimeNanosBased() ? extractNanos(anchor.fields[timeField][0]) : '';
-    const timeValueMillis =
-      nanos !== '' ? convertIsoToMillis(anchor.fields[timeField][0]) : anchor.sort[0];
-
-    const intervals = generateIntervals(LOOKUP_OFFSETS, timeValueMillis as number, type, sortDir);
-    let documents: EsHitRecordList = [];
-
-    for (const interval of intervals) {
-      const remainingSize = size - documents.length;
-
-      if (remainingSize <= 0) {
-        break;
-      }
-
-      const searchAfter = getEsQuerySearchAfter(
-        type,
-        documents,
-        timeField,
-        anchor,
-        nanos,
-        useNewFieldsApi
-      );
-
-      const sort = getEsQuerySort(timeField, tieBreakerField, sortDirToApply, nanos);
-
-      const hits = await fetchHitsInInterval(
-        searchSource,
-        timeField,
-        sort,
-        sortDirToApply,
-        interval,
-        searchAfter,
-        remainingSize,
-        nanos,
-        anchor._id
-      );
-
-      documents =
-        type === SurrDocType.SUCCESSORS
-          ? [...documents, ...hits]
-          : [...hits.slice().reverse(), ...documents];
-    }
-
-    return documents;
+/**
+ * Fetch successor or predecessor documents of a given anchor document
+ *
+ * @param {SurrDocType} type - `successors` or `predecessors`
+ * @param {IndexPattern} indexPattern
+ * @param {EsHitRecord} anchor - anchor record
+ * @param {string} tieBreakerField - name of the tie breaker, the 2nd sort field
+ * @param {SortDirection} sortDir - direction of sorting
+ * @param {number} size - number of records to retrieve
+ * @param {Filter[]} filters - to apply in the elastic query
+ * @param {boolean} useNewFieldsApi
+ * @returns {Promise<object[]>}
+ */
+export async function fetchSurroundingDocs(
+  type: SurrDocType,
+  indexPattern: IndexPattern,
+  anchor: EsHitRecord,
+  tieBreakerField: string,
+  sortDir: SortDirection,
+  size: number,
+  filters: Filter[],
+  useNewFieldsApi?: boolean
+): Promise<EsHitRecordList> {
+  if (typeof anchor !== 'object' || anchor === null || !size) {
+    return [];
   }
+  const { data } = getServices();
+  const timeField = indexPattern.timeFieldName!;
+  const searchSource = data.search.searchSource.createEmpty() as SearchSource;
+  updateSearchSource(searchSource, indexPattern, filters, Boolean(useNewFieldsApi));
+  const sortDirToApply = type === SurrDocType.SUCCESSORS ? sortDir : reverseSortDir(sortDir);
+
+  const nanos = indexPattern.isTimeNanosBased() ? extractNanos(anchor.fields[timeField][0]) : '';
+  const timeValueMillis =
+    nanos !== '' ? convertIsoToMillis(anchor.fields[timeField][0]) : anchor.sort[0];
+
+  const intervals = generateIntervals(LOOKUP_OFFSETS, timeValueMillis as number, type, sortDir);
+  let documents: EsHitRecordList = [];
+
+  for (const interval of intervals) {
+    const remainingSize = size - documents.length;
+
+    if (remainingSize <= 0) {
+      break;
+    }
+
+    const searchAfter = getEsQuerySearchAfter(
+      type,
+      documents,
+      timeField,
+      anchor,
+      nanos,
+      useNewFieldsApi
+    );
+
+    const sort = getEsQuerySort(timeField, tieBreakerField, sortDirToApply, nanos);
+
+    const hits = await fetchHitsInInterval(
+      searchSource,
+      timeField,
+      sort,
+      sortDirToApply,
+      interval,
+      searchAfter,
+      remainingSize,
+      nanos,
+      anchor._id
+    );
+
+    documents =
+      type === SurrDocType.SUCCESSORS
+        ? [...documents, ...hits]
+        : [...hits.slice().reverse(), ...documents];
+  }
+
+  return documents;
 }
 
 export function updateSearchSource(
@@ -125,5 +119,3 @@ export function updateSearchSource(
     .setField('filter', filters)
     .setField('trackTotalHits', false);
 }
-
-export { fetchContextProvider };
