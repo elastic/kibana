02822a66fa7041b65d10910eb545bdde371b8c68
Nathan L Smith <nathan.smith@elastic.co>
2021-10-07 19:11:20 -0500

    Relocate internal APM API endpoints to /internal  (#114196)
    All endpoints except annotations, source maps, and agent configuration are now at /internal/apm instead of /api/apm.
    
    None of the UX endpoints have been updated, only APM.
    
    If you search for "/api/apm" in the codebase, you should only see the above endpoints.
    
    Fixes #113383.

--- x-pack/plugins/apm/public/tutorial/config_agent/index.tsx
+++ x-pack/plugins/apm/public/tutorial/config_agent/index.tsx
@@ -21,7 +21,7 @@ import { CopyCommands } from './copy_commands';
 import { getPolicyOptions, PolicyOption } from './get_policy_options';
 import { PolicySelector } from './policy_selector';
 
-export type APIResponseType = APIReturnType<'GET /api/apm/fleet/agents'>;
+export type APIResponseType = APIReturnType<'GET /internal/apm/fleet/agents'>;
 
 const CentralizedContainer = styled.div`
   display: flex;
@@ -90,7 +90,7 @@ function TutorialConfigAgent({
     async function fetchData() {
       setIsLoading(true);
       try {
-        const response = await http.get('/api/apm/fleet/agents');
+        const response = await http.get('/internal/apm/fleet/agents');
         if (response) {
           setData(response as APIResponseType);
         }
