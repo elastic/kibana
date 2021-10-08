e76433f9c3ac743a0479f17804d1c8d70b3cd264
vadimkibana <vadimkibana@gmail.com>
2021-10-09 00:31:19 +0200

    dont export twice

--- x-pack/plugins/reporting/common/types/index.ts
+++ x-pack/plugins/reporting/common/types/index.ts
@@ -8,7 +8,7 @@
 import type { Size, LayoutParams } from './layout';
 import type { JobId, BaseParams, BasePayload } from './base';
 
-export { JobId, BaseParams, BasePayload };
+export { BaseParams, BasePayload };
 export { Size, LayoutParams };
 export {
   DownloadReportFn,
