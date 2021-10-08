622852d9d81965f6dc9e23a451a258fcf59c72ee
vadimkibana <vadimkibana@gmail.com>
2021-10-08 23:45:29 +0200

    move png types to /common

--- x-pack/plugins/reporting/server/export_types/png/types.ts
+++ x-pack/plugins/reporting/server/export_types/png/types.ts
@@ -5,7 +5,4 @@
  * 2.0.
  */
 
-export * from './png';
-export * from './png_v2';
-export * from './printable_pdf';
-export * from './printable_pdf_v2';
+export type { JobParamsPNG, TaskPayloadPNG } from '../../../common/types';
