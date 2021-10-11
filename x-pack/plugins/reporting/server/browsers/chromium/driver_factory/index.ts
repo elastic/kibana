a176caadb0f404a5294218cdb4388f2f97fad8f6
Jean-Louis Leysens <jloleysens@gmail.com>
2021-10-11 15:04:58 +0200

    [Reporting] Update chromium exit behaviour (#113544) (#114465)
    * move uncaught exception out of exit$
    
    * reintroduce original error, but as a log instead
    
    * change log level: error -> warning. also update copy to make it explicit that the error will be ignored
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/reporting/server/browsers/chromium/driver_factory/index.ts
+++ x-pack/plugins/reporting/server/browsers/chromium/driver_factory/index.ts
@@ -219,6 +219,17 @@ export class HeadlessChromiumDriverFactory {
       })
     );
 
+    const uncaughtExceptionPageError$ = Rx.fromEvent<Error>(page, 'pageerror').pipe(
+      map((err) => {
+        logger.warning(
+          i18n.translate('xpack.reporting.browsers.chromium.pageErrorDetected', {
+            defaultMessage: `Reporting encountered an uncaught error on the page that will be ignored: {err}`,
+            values: { err: err.toString() },
+          })
+        );
+      })
+    );
+
     const pageRequestFailed$ = Rx.fromEvent<puppeteer.HTTPRequest>(page, 'requestfailed').pipe(
       map((req) => {
         const failure = req.failure && req.failure();
@@ -230,7 +241,7 @@ export class HeadlessChromiumDriverFactory {
       })
     );
 
-    return Rx.merge(consoleMessages$, pageRequestFailed$);
+    return Rx.merge(consoleMessages$, uncaughtExceptionPageError$, pageRequestFailed$);
   }
 
   getProcessLogger(browser: puppeteer.Browser, logger: LevelLogger): Rx.Observable<void> {
@@ -265,21 +276,10 @@ export class HeadlessChromiumDriverFactory {
       })
     );
 
-    const uncaughtExceptionPageError$ = Rx.fromEvent<Error>(page, 'pageerror').pipe(
-      mergeMap((err) => {
-        return Rx.throwError(
-          i18n.translate('xpack.reporting.browsers.chromium.pageErrorDetected', {
-            defaultMessage: `Reporting encountered an error on the page: {err}`,
-            values: { err: err.toString() },
-          })
-        );
-      })
-    );
-
     const browserDisconnect$ = Rx.fromEvent(browser, 'disconnected').pipe(
       mergeMap(() => Rx.throwError(getChromiumDisconnectedError()))
     );
 
-    return Rx.merge(pageError$, uncaughtExceptionPageError$, browserDisconnect$);
+    return Rx.merge(pageError$, browserDisconnect$);
   }
 }
