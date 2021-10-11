72e030e0dc8ae7f583483afb199f1bf1959d0247
Angela Chuang <yi-chun.chuang@elastic.co>
2021-10-11 15:47:53 +0100

    security data view badge in kibana settings

--- src/plugins/index_pattern_management/public/components/index_pattern_table/index_pattern_table.tsx
+++ src/plugins/index_pattern_management/public/components/index_pattern_table/index_pattern_table.tsx
@@ -50,6 +50,13 @@ const title = i18n.translate('indexPatternManagement.indexPatternTable.title', {
   defaultMessage: 'Index patterns',
 });
 
+const securityDataView = i18n.translate(
+  'indexPatternManagement.indexPatternTable.badge.securityDataViewTitle',
+  {
+    defaultMessage: 'Security Data View',
+  }
+);
+
 interface Props extends RouteComponentProps {
   canSave: boolean;
   showCreateDialog?: boolean;
@@ -114,6 +121,10 @@ export const IndexPatternTable = ({
           </EuiButtonEmpty>
           &emsp;
           <EuiBadgeGroup gutterSize="s">
+            {index.id && index.id === 'security-solution' && (
+              <EuiBadge key="security-solution">{securityDataView}</EuiBadge>
+            )}
+
             {index.tags &&
               index.tags.map(({ key: tagKey, name: tagName }) => (
                 <EuiBadge key={tagKey}>{tagName}</EuiBadge>
