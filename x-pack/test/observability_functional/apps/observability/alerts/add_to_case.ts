5b1d48997de227b7c43c57aee531f728563ceee2
mgiota <panagiota.mitsopoulou@elastic.co>
2021-10-11 23:06:15 +0200

    use observability test helpers for user creation

--- x-pack/test/observability_functional/apps/observability/alerts/add_to_case.ts
+++ x-pack/test/observability_functional/apps/observability/alerts/add_to_case.ts
@@ -8,15 +8,13 @@
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
-export default ({ getService }: FtrProviderContext) => {
+export default ({ getService, getPageObjects }: FtrProviderContext) => {
   const esArchiver = getService('esArchiver');
-
+  const observability = getService('observability');
   describe('Observability alerts / Add to case', function () {
     this.tags('includeFirefox');
 
-    const retry = getService('retry');
-    const observability = getService('observability');
-
+    // const retry = getService('retry');
     before(async () => {
       await esArchiver.load('x-pack/test/functional/es_archives/observability/alerts');
       await observability.alerts.common.navigateToTimeWithData();
@@ -24,6 +22,44 @@ export default ({ getService }: FtrProviderContext) => {
 
     after(async () => {
       await esArchiver.unload('x-pack/test/functional/es_archives/observability/alerts');
+    });
+
+    describe('When user has all priviledges', () => {
+      before(async () => {
+        await observability.users.setTestUserRole(
+          observability.users.defineBasicObservabilityRole({
+            observabilityCases: ['all'],
+            logs: ['all'],
+          })
+        );
+      });
+
+      after(async () => {
+        await observability.users.restoreDefaultTestUserRole();
+      });
+
+      it('test', () => {
+        expect(true).to.be(true);
+      });
+    });
+
+    describe('When user has read permissions', () => {
+      before(async () => {
+        await observability.users.setTestUserRole(
+          observability.users.defineBasicObservabilityRole({
+            observabilityCases: ['read'],
+            logs: ['all'],
+          })
+        );
+      });
+
+      after(async () => {
+        await observability.users.restoreDefaultTestUserRole();
+      });
+
+      it('test', () => {
+        expect(true).to.be(true);
+      });
     });
   });
 };
