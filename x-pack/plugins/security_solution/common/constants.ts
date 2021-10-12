44dfae942c7274a5df9bd8909dbe37343d1135dd
Khristinin Nikita <nikita.khristinin@elastic.co>
2021-10-12 10:30:39 +0200

    Update default threat index

--- x-pack/plugins/security_solution/common/constants.ts
+++ x-pack/plugins/security_solution/common/constants.ts
@@ -64,7 +64,7 @@ export const DEFAULT_SPACE_ID = 'default';
 export const DEFAULT_INDICATOR_SOURCE_PATH = 'threat.indicator';
 export const ENRICHMENT_DESTINATION_PATH = 'threat.enrichments';
 export const DEFAULT_THREAT_INDEX_KEY = 'securitySolution:defaultThreatIndex';
-export const DEFAULT_THREAT_INDEX_VALUE = ['filebeat-*'];
+export const DEFAULT_THREAT_INDEX_VALUE = ['logs-ti_*'];
 export const DEFAULT_THREAT_MATCH_QUERY = '@timestamp >= "now-30d"';
 
 export enum SecurityPageName {
