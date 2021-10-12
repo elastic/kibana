2dabce6efc7e8643d223c87a204abc67402f162f
Dzmitry Tamashevich <diaamnj@mail.ru>
2021-10-12 14:58:18 +0300

    [Discover] return i18n naming

--- src/core/server/ui_settings/settings/misc.ts
+++ src/core/server/ui_settings/settings/misc.ts
@@ -13,16 +13,15 @@ import { UiSettingsParams } from '../types';
 export const getMiscUiSettings = (): Record<string, UiSettingsParams> => {
   return {
     'truncate:maxHeight': {
-      name: i18n.translate('discover.advancedSettings.discover.maxCellHeightTitle', {
+      name: i18n.translate('core.ui_settings.params.maxCellHeightTitle', {
         defaultMessage: 'Maximum table cell height',
       }),
       value: 115,
-      description: i18n.translate('discover.advancedSettings.maxCellHeightText', {
+      description: i18n.translate('core.ui_settings.params.maxCellHeightText', {
         defaultMessage:
           'The maximum height that a cell in a table should occupy. Set to 0 to disable truncation',
       }),
       schema: schema.number({ min: 0 }),
-      category: ['discover'],
     },
     buildNum: {
       readonly: true,
