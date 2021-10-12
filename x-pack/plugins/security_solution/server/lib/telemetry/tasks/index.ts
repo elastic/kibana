79bc87220a0000364a66ced63ee814f78f3f9f2a
Thiago Souza <thiago@elastic.co>
2021-10-12 12:20:47 +0100

    adding the new detection rule task to the new hot stuff

--- x-pack/plugins/security_solution/server/lib/telemetry/tasks/index.ts
+++ x-pack/plugins/security_solution/server/lib/telemetry/tasks/index.ts
@@ -9,14 +9,17 @@ import { SecurityTelemetryTaskConfig } from '../task';
 import { createTelemetryDiagnosticsTaskConfig } from './diagnostic';
 import { createTelemetryEndpointTaskConfig } from './endpoint';
 import { createTelemetrySecurityListTaskConfig } from './security_lists';
+import { createTelemetryDetectionRuleListsTaskConfig } from './detection_rule';
 
 const MAX_SECURITY_LIST_TELEMETRY_BATCH = 100;
 const MAX_ENDPOINT_TELEMETRY_BATCH = 1_000;
+const MAX_DETECTION_RULE_TELEMETRY_BATCH = 1_000;
 
 export function createTelemetryTaskConfigs(): SecurityTelemetryTaskConfig[] {
   return [
     createTelemetryDiagnosticsTaskConfig(),
     createTelemetryEndpointTaskConfig(MAX_SECURITY_LIST_TELEMETRY_BATCH),
     createTelemetrySecurityListTaskConfig(MAX_ENDPOINT_TELEMETRY_BATCH),
+    createTelemetryDetectionRuleListsTaskConfig(MAX_DETECTION_RULE_TELEMETRY_BATCH),
   ];
 }
