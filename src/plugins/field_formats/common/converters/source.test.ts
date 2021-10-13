5f7d2f1cb7f1e38e5a3716cdfb7b4488376fd534
Dzmitry Tamashevich <diaamnj@mail.ru>
2021-10-13 16:40:36 +0300

    [Discover] apply suggestions

--- src/plugins/field_formats/common/converters/source.test.ts
+++ src/plugins/field_formats/common/converters/source.test.ts
@@ -19,20 +19,7 @@ describe('Source Format', () => {
     convertHtml = source.getConverterFor(HTML_CONTEXT_TYPE) as HtmlContextTypeConvert;
   });
 
-  test('should use the text content type if a field is not passed', () => {
-    const hit = {
-      foo: 'bar',
-      number: 42,
-      hello: '<h1>World</h1>',
-      also: 'with "quotes" or \'single quotes\'',
-    };
-
-    expect(convertHtml(hit)).toBe(
-      '{&quot;foo&quot;:&quot;bar&quot;,&quot;number&quot;:42,&quot;hello&quot;:&quot;&lt;h1&gt;World&lt;/h1&gt;&quot;,&quot;also&quot;:&quot;with \\&quot;quotes\\&quot; or &#39;single quotes&#39;&quot;}'
-    );
-  });
-
-  test('should render a description list if a field is passed', () => {
+  test('should render stringified object', () => {
     const hit = {
       foo: 'bar',
       number: 42,
@@ -43,7 +30,7 @@ describe('Source Format', () => {
     expect(
       convertHtml(hit, { field: 'field', indexPattern: { formatHit: (h: string) => h }, hit })
     ).toMatchInlineSnapshot(
-      `"<dl class=\\"source\\"><dt>foo:</dt><dd>bar</dd> <dt>number:</dt><dd>42</dd> <dt>hello:</dt><dd><h1>World</h1></dd> <dt>also:</dt><dd>with \\"quotes\\" or 'single quotes'</dd> </dl>"`
+      `"{\\"foo\\":\\"bar\\",\\"number\\":42,\\"hello\\":\\"<h1>World</h1>\\",\\"also\\":\\"with \\\\\\"quotes\\\\\\" or 'single quotes'\\"}"`
     );
   });
 });
