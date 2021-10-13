faa4e1ff4b2b5adb7432ca21716f95670be45fb6
Jean-Louis Leysens <jloleysens@gmail.com>
2021-10-13 14:36:07 +0200

    added reporting examples test folder

---
+++ x-pack/test/examples/reporting_examples/index.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { PluginFunctionalProviderContext } from 'test/plugin_functional/services';
+
+// eslint-disable-next-line import/no-default-export
+export default function ({ getService, loadTestFile }: PluginFunctionalProviderContext) {
+  describe('search examples', function () {
+    this.tags('ciGroup13');
+
+    loadTestFile(require.resolve('./search_session_example'));
+    loadTestFile(require.resolve('./search_example'));
+    loadTestFile(require.resolve('./search_sessions_cache'));
+    loadTestFile(require.resolve('./partial_results_example'));
+  });
+}
