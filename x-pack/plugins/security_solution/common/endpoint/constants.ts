e32e958702f62ad3261420d02c0e783dcc85da9b
Ashokaditya <1849116+ashokaditya@users.noreply.github.com>
2021-10-13 21:01:18 +0200

    use correct name for responses index
    refs elastic/kibana/pull/113621

--- x-pack/plugins/security_solution/common/endpoint/constants.ts
+++ x-pack/plugins/security_solution/common/endpoint/constants.ts
@@ -10,7 +10,7 @@
 export const ENDPOINT_ACTIONS_DS = '.logs-endpoint.actions';
 export const ENDPOINT_ACTIONS_INDEX = `${ENDPOINT_ACTIONS_DS}-default`;
 export const ENDPOINT_ACTION_RESPONSES_DS = '.logs-endpoint.action.responses';
-export const ENDPOINT_ACTION_RESPONSES_INDEX = `${ENDPOINT_ACTIONS_DS}-default`;
+export const ENDPOINT_ACTION_RESPONSES_INDEX = `${ENDPOINT_ACTION_RESPONSES_DS}-default`;
 
 export const eventsIndexPattern = 'logs-endpoint.events.*';
 export const alertsIndexPattern = 'logs-endpoint.alerts-*';
