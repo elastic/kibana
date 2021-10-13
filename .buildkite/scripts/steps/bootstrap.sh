854ea970ed5b27f3df41e9116cbb78c87a772921
Brian Seeders <brian.seeders@elastic.co>
2021-10-13 13:42:28 -0400

    Create target dir if needed

--- .buildkite/scripts/steps/bootstrap.sh
+++ .buildkite/scripts/steps/bootstrap.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 
 .buildkite/scripts/bootstrap.sh
 
+mkdir -p target
 tar -cf target/bootstrap.tar --anchored --exclude=./target --exclude=./.git .
 
 cd target
 buildkite-agent artifact upload bootstrap.tar
 buildkite-agent meta-data set "bootstrap_available" "true"
+cd -
