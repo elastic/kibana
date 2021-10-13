990c29df3a6529d74d7512cb3f9029a24bf6096d
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2021-10-13 17:58:17 +0200

    fix: allow automatically trigger performance tests by setting default env variable

--- .buildkite/scripts/steps/functional/performance.sh
+++ .buildkite/scripts/steps/functional/performance.sh
@@ -2,7 +2,12 @@
 
 set -uo pipefail
 
-ITERATION_COUNT="$(buildkite-agent meta-data get performance-test-iteration-count)"
+if [[ $ITERATION_COUNT_ENV -gt 0 ]]
+then
+  ITERATION_COUNT=ITERATION_COUNT_ENV
+else
+  ITERATION_COUNT=$(buildkite-agent meta-data get performance-test-iteration-count)
+fi
 
 tput setab 2; tput setaf 0; echo "Performance test will be run at ${BUILDKITE_BRANCH} ${ITERATION_COUNT} times"
 
@@ -11,5 +16,6 @@ steps:
   - command: .buildkite/scripts/steps/functional/performance_sub.sh
     parallelism: "$ITERATION_COUNT"
 EOF
+
 
 
