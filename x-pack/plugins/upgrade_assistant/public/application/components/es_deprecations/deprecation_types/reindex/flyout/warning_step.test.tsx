d93ca8c452ba7f5a273e2b73c645b53c4932523a
Sébastien Loix <sabee77@gmail.com>
2021-10-13 12:57:55 +0100

    [UA] Align with other apps and use the "MAJOR_VERSION" constant

--- x-pack/plugins/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/reindex/flyout/warning_step.test.tsx
+++ x-pack/plugins/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/reindex/flyout/warning_step.test.tsx
@@ -8,11 +8,14 @@
 import { I18nProvider } from '@kbn/i18n/react';
 import { mount, shallow } from 'enzyme';
 import React from 'react';
+import { SemVer } from 'semver';
 
 import { ReindexWarning } from '../../../../../../../common/types';
-import { mockKibanaSemverVersion } from '../../../../../../../common/constants';
+import { MAJOR_VERSION } from '../../../../../../../common/constants';
 
 import { idForWarning, WarningsFlyoutStep } from './warnings_step';
+
+const kibanaVersion = new SemVer(MAJOR_VERSION);
 
 jest.mock('../../../../../app_context', () => {
   const { docLinksServiceMock } = jest.requireActual(
@@ -24,9 +27,9 @@ jest.mock('../../../../../app_context', () => {
       return {
         docLinks: docLinksServiceMock.createStartContract(),
         kibanaVersionInfo: {
-          currentMajor: mockKibanaSemverVersion.major,
-          prevMajor: mockKibanaSemverVersion.major - 1,
-          nextMajor: mockKibanaSemverVersion.major + 1,
+          currentMajor: kibanaVersion.major,
+          prevMajor: kibanaVersion.major - 1,
+          nextMajor: kibanaVersion.major + 1,
         },
       };
     },
@@ -45,7 +48,7 @@ describe('WarningsFlyoutStep', () => {
     expect(shallow(<WarningsFlyoutStep {...defaultProps} />)).toMatchSnapshot();
   });
 
-  if (mockKibanaSemverVersion.major === 7) {
+  if (kibanaVersion.major === 7) {
     it('does not allow proceeding until all are checked', () => {
       const defaultPropsWithWarnings = {
         ...defaultProps,
