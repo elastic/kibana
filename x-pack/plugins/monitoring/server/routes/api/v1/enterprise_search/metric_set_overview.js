59ae44d9964791ef39e95fb026c6b072c3486f73
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2021-10-13 09:34:37 -0400

    Revert "Simplify metrics loading in the overview page since we load all metrics anyways"
    This reverts commit 4bd67ab88a26696e6940d0807fbe7c68a6a52770.

---
+++ x-pack/plugins/monitoring/server/routes/api/v1/enterprise_search/metric_set_overview.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export const metricSet = [
+  'app_search_total_engines',
+  'workplace_search_total_org_sources',
+  'workplace_search_total_private_sources',
+];
