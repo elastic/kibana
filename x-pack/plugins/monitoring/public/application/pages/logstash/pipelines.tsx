a16afbd16861bc17de57ec74d8a8d370978480a7
Ester Marti <ester.martivilaseca@elastic.co>
2021-10-13 14:48:44 +0200

    Add sorting to table options for pipelines table

--- x-pack/plugins/monitoring/public/application/pages/logstash/pipelines.tsx
+++ x-pack/plugins/monitoring/public/application/pages/logstash/pipelines.tsx
@@ -46,7 +46,6 @@ export const LogStashPipelinesPage: React.FC<ComponentProps> = ({ clusters }) =>
     const bounds = services.data?.query.timefilter.timefilter.getBounds();
     const url = `../api/monitoring/v1/clusters/${clusterUuid}/logstash/pipelines`;
 
-    const options: any = getPaginationRouteOptions();
     const response = await services.http?.fetch(url, {
       method: 'POST',
       body: JSON.stringify({
@@ -55,8 +54,7 @@ export const LogStashPipelinesPage: React.FC<ComponentProps> = ({ clusters }) =>
           min: bounds.min.toISOString(),
           max: bounds.max.toISOString(),
         },
-        pagination: options.pagination,
-        queryText: options.queryText,
+        ...getPaginationRouteOptions(),
       }),
     });
 
