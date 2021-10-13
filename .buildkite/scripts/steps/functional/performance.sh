8b36d7c4ed13da0892c524f2588d188733a24b21
Cavit Baturalp Gurdin <cavit.gurdin@gmail.com>
2021-10-13 19:52:52 +0200

    fix: fix - check if environment variable exist

--- .buildkite/scripts/steps/functional/performance.sh
+++ .buildkite/scripts/steps/functional/performance.sh
@@ -2,11 +2,10 @@
 
 set -uo pipefail
 
-if [[ $ITERATION_COUNT_ENV -gt 0 ]]
-then
-  ITERATION_COUNT=$ITERATION_COUNT_ENV
-else
+if [ -z "${ITERATION_COUNT_ENV+x}" ]; then
   ITERATION_COUNT="$(buildkite-agent meta-data get performance-test-iteration-count)"
+else
+  ITERATION_COUNT=$ITERATION_COUNT_ENV
 fi
 
 tput setab 2; tput setaf 0; echo "Performance test will be run at ${BUILDKITE_BRANCH} ${ITERATION_COUNT} times"
