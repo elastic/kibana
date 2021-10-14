0a35a426a2a6f27d273132a47eeb72cc6dd8c31f
restrry <restrry@gmail.com>
2021-10-14 17:45:52 +0200

    fix transport

--- src/core/server/elasticsearch/client/configure_client.ts
+++ src/core/server/elasticsearch/client/configure_client.ts
@@ -8,7 +8,7 @@
 
 import { Buffer } from 'buffer';
 import { stringify } from 'querystring';
-import { Client, errors, Transport, HttpConnection } from '@elastic/elasticsearch';
+import { Client, errors } from '@elastic/elasticsearch';
 import type {
   TransportRequestParams,
   TransportRequestOptions,
@@ -16,6 +16,7 @@ import type {
   DiagnosticResult,
 } from '@elastic/elasticsearch';
 import type { RequestBody } from '@elastic/transport/lib/types';
+import { Transport, HttpConnection } from '@elastic/transport';
 import { Logger } from '../../logging';
 import { parseClientOptions, ElasticsearchClientConfig } from './client_config';
 import type { ElasticsearchErrorDetails } from './types';
