836da3dc01a95a82884f8c4c65d167c0dfd37a04
Marshall Main <marshall.main@elastic.co>
2021-10-14 07:49:19 -0700

    Prevent error in field_caps from silencing privilege errors

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/signal_rule_alert_type.ts
@@ -8,12 +8,9 @@
 
 import { Logger, SavedObject } from 'src/core/server';
 import isEmpty from 'lodash/isEmpty';
-import { chain, tryCatch } from 'fp-ts/lib/TaskEither';
-import { flow } from 'fp-ts/lib/function';
 
 import * as t from 'io-ts';
 import { validateNonExact, parseScheduleDates } from '@kbn/securitysolution-io-ts-utils';
-import { toError, toPromise } from '@kbn/securitysolution-list-api';
 
 import {
   SIGNALS_ID,
@@ -185,53 +182,44 @@ export const signalRulesAlertType = ({
             index,
             experimentalFeatures,
           });
-          const [privileges, timestampFieldCaps] = await Promise.all([
-            checkPrivileges(services, inputIndices),
-            services.scopedClusterClient.asCurrentUser.fieldCaps({
+          const privileges = await checkPrivileges(services, inputIndices);
+
+          wroteWarningStatus = await hasReadIndexPrivileges({
+            ...basicLogArguments,
+            privileges,
+            logger,
+            buildRuleMessage,
+            ruleStatusClient,
+          });
+
+          if (!wroteWarningStatus) {
+            const timestampFieldCaps = await services.scopedClusterClient.asCurrentUser.fieldCaps({
               index,
               fields: hasTimestampOverride
                 ? ['@timestamp', timestampOverride as string]
                 : ['@timestamp'],
               include_unmapped: true,
-            }),
-          ]);
-
-          wroteWarningStatus = await flow(
-            () =>
-              tryCatch(
-                () =>
-                  hasReadIndexPrivileges({
-                    ...basicLogArguments,
-                    privileges,
-                    logger,
-                    buildRuleMessage,
-                    ruleStatusClient,
-                  }),
-                toError
-              ),
-            chain((wroteStatus) =>
-              tryCatch(
-                () =>
-                  hasTimestampFields({
-                    ...basicLogArguments,
-                    wroteStatus: wroteStatus as boolean,
-                    timestampField: hasTimestampOverride
-                      ? (timestampOverride as string)
-                      : '@timestamp',
-                    timestampFieldCapsResponse: timestampFieldCaps,
-                    inputIndices,
-                    ruleStatusClient,
-                    logger,
-                    buildRuleMessage,
-                  }),
-                toError
-              )
-            ),
-            toPromise
-          )();
+            });
+            wroteWarningStatus = await hasTimestampFields({
+              ...basicLogArguments,
+              timestampField: hasTimestampOverride ? (timestampOverride as string) : '@timestamp',
+              timestampFieldCapsResponse: timestampFieldCaps,
+              inputIndices,
+              ruleStatusClient,
+              logger,
+              buildRuleMessage,
+            });
+          }
         }
       } catch (exc) {
-        logger.error(buildRuleMessage(`Check privileges failed to execute ${exc}`));
+        const errorMessage = buildRuleMessage(`Check privileges failed to execute ${exc}`);
+        logger.error(errorMessage);
+        await ruleStatusClient.logStatusChange({
+          ...basicLogArguments,
+          message: errorMessage,
+          newStatus: RuleExecutionStatus['partial failure'],
+        });
+        wroteWarningStatus = true;
       }
       const { tuples, remainingGap } = getRuleRangeTuples({
         logger,
