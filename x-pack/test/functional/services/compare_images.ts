dbbe45a2b3b85047fa9ef4009640e8db0c01bbaa
Jean-Louis Leysens <jloleysens@gmail.com>
2021-10-15 16:26:55 +0200

    png service -> compare images service

--- x-pack/test/functional/services/compare_images.ts
+++ x-pack/test/functional/services/compare_images.ts
@@ -7,10 +7,11 @@
 
 import path from 'path';
 import { promises as fs } from 'fs';
+import { pdf } from 'pdf-to-img';
 import { comparePngs } from '../../../../test/functional/services/lib/compare_pngs';
 import { FtrProviderContext } from '../ftr_provider_context';
 
-export function PngProvider({ getService }: FtrProviderContext) {
+export function CompareImagesProvider({ getService }: FtrProviderContext) {
   const log = getService('log');
   const config = getService('config');
 
@@ -71,6 +72,76 @@ export function PngProvider({ getService }: FtrProviderContext) {
         sessionDirectoryPath,
         log
       );
+
+      return diffTotal;
+    },
+    async checkIfPdfsMatch(
+      actualPdfPath: string,
+      baselinePdfPath: string,
+      screenshotsDirectory = screenshotsDir
+    ) {
+      log.debug(`checkIfPdfsMatch: ${actualPdfPath} vs ${baselinePdfPath}`);
+      // Copy the pdfs into the screenshot session directory, as that's where the generated pngs will automatically be
+      // stored.
+      const sessionDirectoryPath = path.resolve(screenshotsDirectory, 'session');
+      const failureDirectoryPath = path.resolve(screenshotsDirectory, 'failure');
+
+      await fs.mkdir(sessionDirectoryPath, { recursive: true });
+      await fs.mkdir(failureDirectoryPath, { recursive: true });
+
+      const actualPdfFileName = path.basename(actualPdfPath, '.pdf');
+      const baselinePdfFileName = path.basename(baselinePdfPath, '.pdf');
+
+      const baselineCopyPath = path.resolve(
+        sessionDirectoryPath,
+        `${baselinePdfFileName}_baseline.pdf`
+      );
+      const actualCopyPath = path.resolve(sessionDirectoryPath, `${actualPdfFileName}_actual.pdf`);
+
+      // Don't cause a test failure if the baseline snapshot doesn't exist - we don't have all OS's covered and we
+      // don't want to start causing failures for other devs working on OS's which are lacking snapshots.  We have
+      // mac and linux covered which is better than nothing for now.
+      try {
+        log.debug(`writeFileSync: ${baselineCopyPath}`);
+        await fs.writeFile(baselineCopyPath, await fs.readFile(baselinePdfPath));
+      } catch (error) {
+        log.error(`No baseline pdf found at ${baselinePdfPath}`);
+        return 0;
+      }
+      log.debug(`writeFileSync: ${actualCopyPath}`);
+      await fs.writeFile(actualCopyPath, await fs.readFile(actualPdfPath));
+
+      const actualPdf = await pdf(actualCopyPath);
+      const baselinePdf = await pdf(actualCopyPath);
+
+      log.debug(`Checking number of pages`);
+
+      if (actualPdf.length !== baselinePdf.length) {
+        throw new Error(
+          `Expected ${baselinePdf.length} pages but got ${actualPdf.length} in PDFs expected: "${baselineCopyPath}" actual: "${actualCopyPath}".`
+        );
+      }
+
+      let diffTotal = 0;
+      let pageNum = 1;
+
+      for await (const actualPage of actualPdf) {
+        for await (const baselinePage of baselinePdf) {
+          const diffPngPath = path.resolve(
+            failureDirectoryPath,
+            `${baselinePdfFileName}-${pageNum}.png`
+          );
+          diffTotal += await comparePngs(
+            { path: screenshotsDirectory, buffer: actualPage },
+            { path: screenshotsDirectory, buffer: baselinePage },
+            diffPngPath,
+            sessionDirectoryPath,
+            log
+          );
+          ++pageNum;
+          break;
+        }
+      }
 
       return diffTotal;
     },
