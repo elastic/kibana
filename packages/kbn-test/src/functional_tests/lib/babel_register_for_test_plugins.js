7d1a329af00577ab464f37e49094e8a13e299425
Tiago Costa <tiagoffcc@hotmail.com>
2021-10-15 22:53:59 +0100

    chore(NA): test mappings

--- packages/kbn-test/src/functional_tests/lib/babel_register_for_test_plugins.js
+++ packages/kbn-test/src/functional_tests/lib/babel_register_for_test_plugins.js
@@ -12,15 +12,18 @@ const Path = require('path');
 const { REPO_ROOT } = require('@kbn/dev-utils');
 
 const KIBANA_ROOT = Path.resolve(__dirname, '../../../../../');
+const KIBANA_ROOT2 = Fs.realpathSync(REPO_ROOT).includes('parallel')
+  ? Path.resolve(Fs.realpathSync(REPO_ROOT), '../../')
+  : Fs.realpathSync(REPO_ROOT);
 const testMap = [
-  Path.resolve(REPO_ROOT, 'test'),
-  Path.resolve(REPO_ROOT, 'x-pack/test'),
-  Path.resolve(REPO_ROOT, 'examples'),
-  Path.resolve(REPO_ROOT, 'x-pack/examples'),
+  Path.resolve(KIBANA_ROOT2, 'test'),
+  Path.resolve(KIBANA_ROOT2, 'x-pack/test'),
+  Path.resolve(KIBANA_ROOT2, 'examples'),
+  Path.resolve(KIBANA_ROOT2, 'x-pack/examples'),
   // TODO: should should probably remove this link back to the source
-  Path.resolve(REPO_ROOT, 'x-pack/plugins/task_manager/server/config.ts'),
-  Path.resolve(REPO_ROOT, 'src/core/utils/default_app_categories.ts'),
-].map((path) => Fs.realpathSync(Fs.realpathSync(path)));
+  Path.resolve(KIBANA_ROOT2, 'x-pack/plugins/task_manager/server/config.ts'),
+  Path.resolve(KIBANA_ROOT2, 'src/core/utils/default_app_categories.ts'),
+];
 
 console.log('SIMPLE REPO ROOT: ');
 console.log(Path.resolve(REPO_ROOT, 'test'));
@@ -30,21 +33,22 @@ console.log('KBN ROOT: ');
 console.log(Path.resolve(KIBANA_ROOT, 'test'));
 console.log('TEST PATHS: ');
 console.log(testMap);
-throw new Error('FAIL CI');
+// throw new Error('FAIL CI');
 
 // modifies all future calls to require() to automatically
 // compile the required source with babel
 require('@babel/register')({
   ignore: [/[\/\\](node_modules|target|dist)[\/\\]/],
-  only: [
-    Path.resolve(REPO_ROOT, 'test'),
-    Path.resolve(REPO_ROOT, 'x-pack/test'),
-    Path.resolve(REPO_ROOT, 'examples'),
-    Path.resolve(REPO_ROOT, 'x-pack/examples'),
-    // TODO: should should probably remove this link back to the source
-    Path.resolve(REPO_ROOT, 'x-pack/plugins/task_manager/server/config.ts'),
-    Path.resolve(REPO_ROOT, 'src/core/utils/default_app_categories.ts'),
-  ].map((path) => Fs.realpathSync(path)),
+  // only: [
+  //   Path.resolve(REPO_ROOT, 'test'),
+  //   Path.resolve(REPO_ROOT, 'x-pack/test'),
+  //   Path.resolve(REPO_ROOT, 'examples'),
+  //   Path.resolve(REPO_ROOT, 'x-pack/examples'),
+  //   // TODO: should should probably remove this link back to the source
+  //   Path.resolve(REPO_ROOT, 'x-pack/plugins/task_manager/server/config.ts'),
+  //   Path.resolve(REPO_ROOT, 'src/core/utils/default_app_categories.ts'),
+  // ].map((path) => Fs.realpathSync(path)),
+  only: testMap,
   babelrc: false,
   presets: [require.resolve('@kbn/babel-preset/node_preset')],
   extensions: ['.js', '.ts', '.tsx'],
