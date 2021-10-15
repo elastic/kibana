4c561275de3b177dbfddb072e48df00f53685805
Jonathan Budzenski <jon@budzenski.me>
2021-10-15 10:33:10 -0500

    parallelism=10, heap=4800mb

--- .buildkite/scripts/steps/test/jest.sh
+++ .buildkite/scripts/steps/test/jest.sh
@@ -10,4 +10,4 @@ is_test_execution_step
 
 echo '--- Jest'
 checks-reporter-with-killswitch "Jest Unit Tests" \
-  node scripts/jest --ci --verbose --maxWorkers=10
+  node --max-old-space-size=4800 scripts/jest --ci --verbose --maxWorkers=10
