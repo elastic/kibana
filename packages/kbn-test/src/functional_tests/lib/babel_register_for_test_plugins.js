5a372055086451bd630734cf9f1fb4db70ce4827
Tiago Costa <tiagoffcc@hotmail.com>
2021-10-15 02:42:26 +0100

    chore(NA): removes debug code

--- packages/kbn-test/src/functional_tests/lib/babel_register_for_test_plugins.js
+++ packages/kbn-test/src/functional_tests/lib/babel_register_for_test_plugins.js
@@ -11,13 +11,6 @@ const Path = require('path');
 
 const { REPO_ROOT } = require('@kbn/dev-utils');
 
-// const KIBANA_ROOT = Path.resolve(__dirname, '../../../../../');
-
-// console.log(Path.resolve(REPO_ROOT, 'test'));
-// console.log(require('fs').realpathSync(Path.resolve(REPO_ROOT, 'test')));
-// console.log(Path.resolve(KIBANA_ROOT, 'test'));
-// throw new Error('FAIL CI');
-
 // modifies all future calls to require() to automatically
 // compile the required source with babel
 require('@babel/register')({
