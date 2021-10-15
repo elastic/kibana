b85ec15b431daecbe33b5e570ce91ce4f946f937
Dave Snider <dave.snider@gmail.com>
2021-10-15 00:17:42 -0400

    i18n and other test fixes

--- x-pack/plugins/apm/public/components/routing/templates/no_data_config.ts
+++ x-pack/plugins/apm/public/components/routing/templates/no_data_config.ts
@@ -25,11 +25,11 @@ export function getNoDataConfig({
       }),
       actions: {
         elasticAgent: {
-          title: i18n.translate('xpack.apm.ux.overview.beatsCard.title', {
+          title: i18n.translate('xpack.apm.ux.overview.agent.title', {
             defaultMessage: 'Add the APM integration',
           }),
           description: i18n.translate(
-            'xpack.apm.ux.overview.beatsCard.description',
+            'xpack.apm.ux.overview.agent.description',
             {
               defaultMessage:
                 'Use APM agents to collect APM data. We make it easy with agents for many popular languages.',
