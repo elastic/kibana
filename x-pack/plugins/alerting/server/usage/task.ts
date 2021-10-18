e9e1cec1a1cc5829e96b96eeec06b717f1bc35ee
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2021-10-17 19:06:50 -0700

    fixed test data

--- x-pack/plugins/alerting/server/usage/task.ts
+++ x-pack/plugins/alerting/server/usage/task.ts
@@ -43,7 +43,7 @@ function registerAlertingTelemetryTask(
   taskManager.registerTaskDefinitions({
     [TELEMETRY_TASK_TYPE]: {
       title: 'Alerting usage fetch task',
-      timeout: '5s',
+      timeout: '5m',
       createTaskRunner: telemetryTaskRunner(logger, core, kibanaIndex),
     },
   });
@@ -107,5 +107,5 @@ export function telemetryTaskRunner(logger: Logger, core: CoreSetup, kibanaIndex
 }
 
 function getNextMidnight() {
-  return moment().add(1, 'm').startOf('m').toDate();
+  return moment().add(1, 'd').startOf('d').toDate();
 }
