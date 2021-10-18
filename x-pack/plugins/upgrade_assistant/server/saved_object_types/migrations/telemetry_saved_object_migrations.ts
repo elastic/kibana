b405eec51aab7559bd9da1c8ec9e61f3b4c1aaad
CJ Cenizal <cj@cenizal.com>
2021-10-18 11:46:22 -0700

    Add condition to avoid sanitizing every doc.

--- x-pack/plugins/upgrade_assistant/server/saved_object_types/migrations/telemetry_saved_object_migrations.ts
+++ x-pack/plugins/upgrade_assistant/server/saved_object_types/migrations/telemetry_saved_object_migrations.ts
@@ -5,32 +5,39 @@
  * 2.0.
  */
 
-import { omit, flow } from 'lodash';
+import { get, omit, flow, some } from 'lodash';
 import type { SavedObjectMigrationFn } from 'kibana/server';
 
 const v716RemoveUnusedTelemetry: SavedObjectMigrationFn<any, any> = (doc) => {
   // Dynamically defined in 6.7 (https://github.com/elastic/kibana/pull/28878)
   // and then statically defined in 7.8 (https://github.com/elastic/kibana/pull/64332).
   const attributesBlocklist = [
-    'ui_open.overview',
     'ui_open.cluster',
     'ui_open.indices',
+    'ui_open.overview',
     'ui_reindex.close',
     'ui_reindex.open',
     'ui_reindex.start',
     'ui_reindex.stop',
   ];
 
-  try {
-    return {
-      ...doc,
-      attributes: {
-        ...omit(doc.attributes, attributesBlocklist),
-      },
-    };
-  } catch (e) {
-    // Let it go, the data is invalid and we'll leave it as is
+  const isDocEligible = some(attributesBlocklist, (attribute: string) => {
+    return get(doc, 'attributes', attribute);
+  });
+
+  if (isDocEligible) {
+    try {
+      return {
+        ...doc,
+        attributes: {
+          ...omit(doc.attributes, attributesBlocklist),
+        },
+      };
+    } catch (e) {
+      // Let it go, the data is invalid and we'll leave it as is
+    }
   }
+
   return doc;
 };
 
