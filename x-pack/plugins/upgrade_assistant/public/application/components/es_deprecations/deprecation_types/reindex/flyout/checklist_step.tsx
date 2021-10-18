19cad6d3743712995dc13a75ac9bef0fc12185ec
CJ Cenizal <cj@cenizal.com>
2021-10-18 15:07:59 -0700

    Revert "Revert "[Upgrade Assistant] Refactor telemetry (#112177)" (#113665)" (#114804)
    This reverts commit c385d498874d4ca34f454e3cb9ad9e4c0e3219ae.
    * Add migration to remove obsolete attributes from telemetry saved object.
    * Refactor UA telemetry constants by extracting it from common/types.

--- x-pack/plugins/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/reindex/flyout/checklist_step.tsx
+++ x-pack/plugins/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/reindex/flyout/checklist_step.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { Fragment } from 'react';
+import React, { Fragment, useCallback } from 'react';
 
 import {
   EuiButton,
@@ -18,8 +18,14 @@ import {
   EuiSpacer,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
+import { METRIC_TYPE } from '@kbn/analytics';
 
 import { ReindexStatus } from '../../../../../../../common/types';
+import {
+  uiMetricService,
+  UIM_REINDEX_START_CLICK,
+  UIM_REINDEX_STOP_CLICK,
+} from '../../../../../lib/ui_metric';
 import { LoadingState } from '../../../../types';
 import type { ReindexState } from '../use_reindex_state';
 import { ReindexProgress } from './progress';
@@ -78,6 +84,16 @@ export const ChecklistFlyoutStep: React.FunctionComponent<{
   const isCompleted = status === ReindexStatus.completed;
   const hasFetchFailed = status === ReindexStatus.fetchFailed;
   const hasReindexingFailed = status === ReindexStatus.failed;
+
+  const onStartReindex = useCallback(() => {
+    uiMetricService.trackUiMetric(METRIC_TYPE.CLICK, UIM_REINDEX_START_CLICK);
+    startReindex();
+  }, [startReindex]);
+
+  const onStopReindex = useCallback(() => {
+    uiMetricService.trackUiMetric(METRIC_TYPE.CLICK, UIM_REINDEX_STOP_CLICK);
+    cancelReindex();
+  }, [cancelReindex]);
 
   return (
     <Fragment>
@@ -148,7 +164,7 @@ export const ChecklistFlyoutStep: React.FunctionComponent<{
           </p>
         </EuiCallOut>
         <EuiSpacer />
-        <ReindexProgress reindexState={reindexState} cancelReindex={cancelReindex} />
+        <ReindexProgress reindexState={reindexState} cancelReindex={onStopReindex} />
       </EuiFlyoutBody>
       <EuiFlyoutFooter>
         <EuiFlexGroup justifyContent="spaceBetween">
@@ -166,7 +182,7 @@ export const ChecklistFlyoutStep: React.FunctionComponent<{
                 fill
                 color={status === ReindexStatus.paused ? 'warning' : 'primary'}
                 iconType={status === ReindexStatus.paused ? 'play' : undefined}
-                onClick={startReindex}
+                onClick={onStartReindex}
                 isLoading={loading}
                 disabled={loading || !hasRequiredPrivileges}
                 data-test-subj="startReindexingButton"
