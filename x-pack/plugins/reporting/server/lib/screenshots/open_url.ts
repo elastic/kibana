9f7966e419a38386c549057291a86dffc2952739
Tim Sullivan <tsullivan@users.noreply.github.com>
2021-10-18 10:24:16 -0700

    [Reporting] Revisit handling timeouts for different phases of screenshot capture (#113807)
    * [Reporting] Revisit handling timeouts for different phases of screenshot capture
    
    * remove translations for changed text
    
    * add wip unit test
    
    * simplify class
    
    * todo more testing
    
    * fix ts
    
    * update snapshots
    
    * simplify open_url
    
    * fixup me
    
    * move setupPage to a method of the ObservableHandler class
    
    * do not pass entire config object to helper functions
    
    * distinguish internal timeouts vs external timeout
    
    * add tests for waitUntil
    
    * checkIsPageOpen test
    
    * restore passing of renderErrors
    
    * updates per feedback
    
    * Update x-pack/plugins/reporting/server/lib/screenshots/observable_handler.ts
    
    Co-authored-by: Michael Dokolin <dokmic@gmail.com>
    
    * Update x-pack/plugins/reporting/server/lib/screenshots/observable_handler.ts
    
    Co-authored-by: Michael Dokolin <dokmic@gmail.com>
    
    * Update x-pack/plugins/reporting/server/lib/screenshots/observable_handler.ts
    
    Co-authored-by: Michael Dokolin <dokmic@gmail.com>
    
    * Update x-pack/plugins/reporting/server/lib/screenshots/observable_handler.ts
    
    Co-authored-by: Michael Dokolin <dokmic@gmail.com>
    
    * Update x-pack/plugins/reporting/server/lib/screenshots/observable_handler.ts
    
    Co-authored-by: Michael Dokolin <dokmic@gmail.com>
    
    * fix parsing
    
    * apply simplifications consistently
    
    * dont main waitUntil a higher order component
    
    * resolve the timeouts options outside of the service
    
    * comment correction
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Michael Dokolin <dokmic@gmail.com>

--- x-pack/plugins/reporting/server/lib/screenshots/open_url.ts
+++ x-pack/plugins/reporting/server/lib/screenshots/open_url.ts
@@ -6,21 +6,25 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import { LocatorParams, UrlOrUrlLocatorTuple } from '../../../common/types';
 import { LevelLogger, startTrace } from '../';
-import { durationToNumber } from '../../../common/schema_utils';
+import { LocatorParams, UrlOrUrlLocatorTuple } from '../../../common/types';
 import { HeadlessChromiumDriver } from '../../browsers';
 import { ConditionalHeaders } from '../../export_types/common';
-import { CaptureConfig } from '../../types';
+import { DEFAULT_PAGELOAD_SELECTOR } from './constants';
 
 export const openUrl = async (
-  captureConfig: CaptureConfig,
+  timeout: number,
   browser: HeadlessChromiumDriver,
+  index: number,
   urlOrUrlLocatorTuple: UrlOrUrlLocatorTuple,
-  waitForSelector: string,
   conditionalHeaders: ConditionalHeaders,
   logger: LevelLogger
 ): Promise<void> => {
+  // If we're moving to another page in the app, we'll want to wait for the app to tell us
+  // it's loaded the next page.
+  const page = index + 1;
+  const waitForSelector = page > 1 ? `[data-shared-page="${page}"]` : DEFAULT_PAGELOAD_SELECTOR;
+
   const endTrace = startTrace('open_url', 'wait');
   let url: string;
   let locator: undefined | LocatorParams;
@@ -32,14 +36,13 @@ export const openUrl = async (
   }
 
   try {
-    const timeout = durationToNumber(captureConfig.timeouts.openUrl);
     await browser.open(url, { conditionalHeaders, waitForSelector, timeout, locator }, logger);
   } catch (err) {
     logger.error(err);
     throw new Error(
       i18n.translate('xpack.reporting.screencapture.couldntLoadKibana', {
-        defaultMessage: `An error occurred when trying to open the Kibana URL. You may need to increase '{configKey}'. {error}`,
-        values: { configKey: 'xpack.reporting.capture.timeouts.openUrl', error: err },
+        defaultMessage: `An error occurred when trying to open the Kibana URL: {error}`,
+        values: { error: err },
       })
     );
   }
