ec404b8b3d0801338c1c0ab99878b5a57d836940
Dominique Clarke <dominique.clarke@elastic.co>
2021-10-18 08:40:39 -0400

    adjust tests and aria labels

--- x-pack/plugins/uptime/public/components/common/header/action_menu_content.test.tsx
+++ x-pack/plugins/uptime/public/components/common/header/action_menu_content.test.tsx
@@ -45,7 +45,9 @@ describe('ActionMenuContent', () => {
   it('renders Add Data link', () => {
     const { getByLabelText, getByText } = render(<ActionMenuContent />);
 
-    const addDataAnchor = getByLabelText('Navigate to a tutorial about adding Uptime data');
+    const addDataAnchor = getByLabelText(
+      'Navigate to the Elastic Synthetics integration to add Uptime data'
+    );
 
     // this href value is mocked, so it doesn't correspond to the real link
     // that Kibana core services will provide
