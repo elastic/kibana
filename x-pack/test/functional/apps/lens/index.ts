ed2de4720d61777ec460a31cae3d37ae70232193
bhavyarm <bhavya@elastic.co>
2021-10-18 08:48:29 -0400

    uncommenting test files in index.ts

--- x-pack/test/functional/apps/lens/index.ts
+++ x-pack/test/functional/apps/lens/index.ts
@@ -18,26 +18,27 @@ export default function ({ getService, loadTestFile, getPageObjects }: FtrProvid
     before(async () => {
       log.debug('Starting lens before method');
       await browser.setWindowSize(1280, 800);
+      await esArchiver.load('x-pack/test/functional/es_archives/logstash_functional');
       await kibanaServer.importExport.load(
         'x-pack/test/functional/fixtures/kbn_archiver/lens/lens_basic.json'
       );
-      await esArchiver.load('x-pack/test/functional/es_archives/logstash_functional');
       // changing the timepicker default here saves us from having to set it in Discover (~8s)
       await PageObjects.timePicker.setDefaultAbsoluteRangeViaUiSettings();
       await kibanaServer.uiSettings.update({ defaultIndex: 'logstash-*', 'dateFormat:tz': 'UTC' });
     });
 
     after(async () => {
+      await esArchiver.unload('x-pack/test/functional/es_archives/logstash_functional');
+      await PageObjects.timePicker.resetDefaultAbsoluteRangeViaUiSettings();
       await kibanaServer.importExport.unload(
         'x-pack/test/functional/fixtures/kbn_archiver/lens/lens_basic.json'
       );
-      await esArchiver.unload('x-pack/test/functional/es_archives/logstash_functional');
-      await PageObjects.timePicker.resetDefaultAbsoluteRangeViaUiSettings();
     });
 
     describe('', function () {
       this.tags(['ciGroup3', 'skipFirefox']);
       loadTestFile(require.resolve('./smokescreen'));
+      loadTestFile(require.resolve('./persistent_context'));
     });
 
     describe('', function () {
@@ -45,23 +46,21 @@ export default function ({ getService, loadTestFile, getPageObjects }: FtrProvid
 
       loadTestFile(require.resolve('./add_to_dashboard'));
       loadTestFile(require.resolve('./table'));
-      //       loadTestFile(require.resolve('./runtime_fields'));
-      //       loadTestFile(require.resolve('./dashboard'));
-      //       loadTestFile(require.resolve('./persistent_context'));
-      //       loadTestFile(require.resolve('./colors'));
-      //       loadTestFile(require.resolve('./chart_data'));
-      //       loadTestFile(require.resolve('./time_shift'));
-      //       loadTestFile(require.resolve('./drag_and_drop'));
-      //       loadTestFile(require.resolve('./geo_field'));
-      //       loadTestFile(require.resolve('./lens_reporting'));
-      //       loadTestFile(require.resolve('./lens_tagging'));
-      //       loadTestFile(require.resolve('./formula'));
-      //       loadTestFile(require.resolve('./heatmap'));
-      //       loadTestFile(require.resolve('./reference_lines'));
-      //       loadTestFile(require.resolve('./inspector'));
-
-      //       // has to be last one in the suite because it overrides saved objects
-      //       loadTestFile(require.resolve('./rollup'));
+      loadTestFile(require.resolve('./runtime_fields'));
+      loadTestFile(require.resolve('./dashboard'));
+      loadTestFile(require.resolve('./colors'));
+      loadTestFile(require.resolve('./chart_data'));
+      loadTestFile(require.resolve('./time_shift'));
+      loadTestFile(require.resolve('./drag_and_drop'));
+      loadTestFile(require.resolve('./geo_field'));
+      loadTestFile(require.resolve('./formula'));
+      loadTestFile(require.resolve('./heatmap'));
+      loadTestFile(require.resolve('./reference_lines'));
+      loadTestFile(require.resolve('./inspector'));
+      loadTestFile(require.resolve('./lens_tagging'));
+      loadTestFile(require.resolve('./lens_reporting'));
+      // has to be last one in the suite because it overrides saved objects
+      loadTestFile(require.resolve('./rollup'));
     });
   });
 }
