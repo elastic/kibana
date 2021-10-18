a6e9d8ff34c827f0f393a82e359035247cb3b813
Shahzad <shahzad.muhammad@elastic.co>
2021-10-18 18:45:14 +0200

    [Uptime] Added uptime query inspector panel (#115170) (#115376)

--- x-pack/plugins/observability/server/index.ts
+++ x-pack/plugins/observability/server/index.ts
@@ -13,9 +13,12 @@ import { PluginConfigDescriptor, PluginInitializerContext } from 'src/core/serve
 import { ObservabilityPlugin, ObservabilityPluginSetup } from './plugin';
 import { createOrUpdateIndex, Mappings } from './utils/create_or_update_index';
 import { ScopedAnnotationsClient } from './lib/annotations/bootstrap_annotations';
-import { unwrapEsResponse, WrappedElasticsearchClientError } from './utils/unwrap_es_response';
+import {
+  unwrapEsResponse,
+  WrappedElasticsearchClientError,
+} from '../common/utils/unwrap_es_response';
 export { rangeQuery, kqlQuery } from './utils/queries';
-export { getInspectResponse } from './utils/get_inspect_response';
+export { getInspectResponse } from '../common/utils/get_inspect_response';
 
 export * from './types';
 
