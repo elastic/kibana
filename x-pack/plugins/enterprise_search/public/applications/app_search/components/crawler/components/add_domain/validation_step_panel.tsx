7b9ed68216fdb60c89e7228eaa375c5bf8900457
Orhan Toy <toyorhan@gmail.com>
2021-10-19 13:16:21 +0200

    Move spacer to before action

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/add_domain/validation_step_panel.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/add_domain/validation_step_panel.tsx
@@ -57,8 +57,8 @@ export const ValidationStepPanel: React.FC<ValidationStepPanelProps> = ({
           </EuiMarkdownFormat>
           {action && (
             <>
+              <EuiSpacer size="s" />
               {action}
-              <EuiSpacer />
             </>
           )}
         </>
