5993846f292e1fcf0f01c63c158df6a42dc1f700
Thomas Neirynck <thomas@elastic.co>
2021-10-19 09:59:29 -0400

    [Fleet] Add telemetry for integration cards (#115413)

--- x-pack/plugins/fleet/public/applications/integrations/sections/epm/screens/home/index.tsx
+++ x-pack/plugins/fleet/public/applications/integrations/sections/epm/screens/home/index.tsx
@@ -74,7 +74,7 @@ export const mapToCard = (
   }
 
   return {
-    id: `${item.type === 'ui_link' ? 'ui_link' : 'epr'}-${item.id}`,
+    id: `${item.type === 'ui_link' ? 'ui_link' : 'epr'}:${item.id}`,
     description: item.description,
     icons: !item.icons || !item.icons.length ? [] : item.icons,
     title: item.title,
