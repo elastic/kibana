868cd6dd898d15d5eb33ae84b695e2856bbf3f64
Tyler Smalley <tyler.smalley@elastic.co>
2021-10-19 16:49:47 -0700

    [ci] Run Jest tests in parallel
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

---
+++ .buildkite/scripts/steps/test/jest_parallel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -uo pipefail
+
+JOB=$BUILDKITE_PARALLEL_JOB
+JOB_COUNT=$BUILDKITE_PARALLEL_JOB_COUNT
+
+# a jest failure will result in the script returning an exit
+# code of 10
+
+i=0
+exitCode=0
+
+find src x-pack packages -name jest.config.js -not -path "*/__fixtures__/*" | sort | while read config; do
+  if [ "$(($i % $JOB_COUNT))" -eq $JOB ]; then
+    echo "--- $ node scripts/jest --config $config"
+    # TODO: sort by list of test files identified by --listFiles
+    # Also allows us to ignore configs with no tests so they can still exist.
+    node --max-old-space-size=5632 ./node_modules/.bin/jest --config $config --runInBand
+
+    if [ $? -ne 0 ]; then
+      exitCode=10
+      echo "^^^ +++"
+    fi
+  fi
+
+  ((i=i+1))
+done
+
+exit $exitCode
