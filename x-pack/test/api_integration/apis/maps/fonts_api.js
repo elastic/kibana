a63b9f52bde0c632a6fe0aea68772cec6a4ef619
Thom Heymann <thom.heymann@elastic.co>
2021-10-19 09:55:50 +0100

    fix functional tests

--- x-pack/test/api_integration/apis/maps/fonts_api.js
+++ x-pack/test/api_integration/apis/maps/fonts_api.js
@@ -6,11 +6,34 @@
  */
 
 import expect from '@kbn/expect';
+import path from 'path';
+import { copyFile, rm } from 'fs/promises';
 
 export default function ({ getService }) {
   const supertest = getService('supertest');
 
   describe('fonts', () => {
+    const fontPath = path.join(
+      process.cwd(),
+      'x-pack',
+      'plugins',
+      'maps',
+      'server',
+      'fonts',
+      'open_sans',
+      '0-255.pbf'
+    );
+    const destinationPath = path.join(path.dirname(fontPath), '..', path.basename(fontPath));
+    console.log({ fontPath, destinationPath });
+
+    before(async () => {
+      await copyFile(fontPath, destinationPath);
+    });
+
+    after(async () => {
+      await rm(destinationPath);
+    });
+
     it('should return fonts', async () => {
       const resp = await supertest
         .get(`/api/maps/fonts/Open%20Sans%20Regular,Arial%20Unicode%20MS%20Regular/0-255`)
@@ -25,12 +48,12 @@ export default function ({ getService }) {
         .expect(404);
     });
 
-    it('should return 404 when file is not in font folder (../)', async () => {
-      await supertest.get(`/api/maps/fonts/open_sans/..%2fopen_sans%2f0-255`).expect(404);
+    it('should return 404 when file is not in font folder (..)', async () => {
+      await supertest.get(`/api/maps/fonts/open_sans/..%2f0-255`).expect(404);
     });
 
-    it('should return 404 when file is not in font folder (./../)', async () => {
-      await supertest.get(`/api/maps/fonts/open_sans/.%2f..%2fopen_sans%2f0-255`).expect(404);
+    it('should return 404 when file is not in font folder (./..)', async () => {
+      await supertest.get(`/api/maps/fonts/open_sans/.%2f..%2f0-255`).expect(404);
     });
   });
 }
