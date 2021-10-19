97b0e2a40116330923855a1a8da82a951e077b65
Jean-Louis Leysens <jloleysens@gmail.com>
2021-10-19 18:48:01 +0200

    remove unused report info button

--- x-pack/plugins/reporting/public/management/components/index.ts
+++ x-pack/plugins/reporting/public/management/components/index.ts
@@ -9,6 +9,5 @@ export { MigrateIlmPolicyCallOut } from './migrate_ilm_policy_callout';
 export { IlmPolicyLink } from './ilm_policy_link';
 export { ReportDeleteButton } from './report_delete_button';
 export { ReportDiagnostic } from './report_diagnostic';
-export { ReportInfoButton } from './report_info_button';
 export { ReportStatusIndicator } from './report_status_indicator';
 export { ReportInfoFlyout } from './report_info_flyout';
