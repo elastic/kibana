b6caf7899b98f7e5102cce6c4c6c757af070d64a
ppisljar <peter.pisljar@gmail.com>
2021-10-20 05:03:51 -0700

    more updates

--- src/plugins/data/common/search/expressions/select_filter.ts
+++ src/plugins/data/common/search/expressions/select_filter.ts
@@ -62,7 +62,7 @@ export const selectFilterFunction: ExpressionFunctionSelectFilter = {
           const isGroupMatching =
             (!group && !ungrouped) || group === meta.group || (ungrouped && !meta.group);
           const isOriginMatching = !from || from === meta.controlledBy;
-          return !isGroupMatching || !isOriginMatching;
+          return isGroupMatching && isOriginMatching;
         }) || [],
     };
   },
