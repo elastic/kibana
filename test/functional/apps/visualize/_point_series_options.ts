82e1ac628f3bf18b4b190dd0cbe89359f2b3c5e0
nickofthyme <nick.ryan.partridge@gmail.com>
2021-10-19 19:41:16 -0500

    fix functional test

--- test/functional/apps/visualize/_point_series_options.ts
+++ test/functional/apps/visualize/_point_series_options.ts
@@ -230,10 +230,10 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     describe('timezones', async function () {
       it('should show round labels in default timezone', async function () {
         const expectedLabels = [
-          '2015-09-19 12:00',
-          '2015-09-20 12:00',
-          '2015-09-21 12:00',
-          '2015-09-22 12:00',
+          '2015-09-20 00:00',
+          '2015-09-21 00:00',
+          '2015-09-22 00:00',
+          '2015-09-23 00:00',
         ];
         await initChart();
         const labels = await PageObjects.visChart.getXAxisLabels(xyChartSelector);
