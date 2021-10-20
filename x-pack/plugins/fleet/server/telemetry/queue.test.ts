caef79a0ff118dc7f73c6eed237571f113c2e1b4
Julia Bardi <julia.bardi@elastic.co>
2021-10-20 15:00:21 +0200

    fix types

--- x-pack/plugins/fleet/server/telemetry/queue.test.ts
+++ x-pack/plugins/fleet/server/telemetry/queue.test.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
+/* eslint-disable dot-notation */
 import { loggingSystemMock } from 'src/core/server/mocks';
 
 import type { ESClusterInfo } from './types';
@@ -21,25 +21,25 @@ describe('TelemetryQueue', () => {
     it('queues two events', () => {
       const queue = new TelemetryQueue(logger);
       queue.addEvents([{ 'event.kind': '1' }, { 'event.kind': '2' }]);
-      expect(queue.queue.length).toBe(2);
+      expect(queue['queue'].length).toBe(2);
     });
 
     it('queues more than maxQueueSize events', () => {
       const queue = new TelemetryQueue(logger);
       queue.addEvents([{ 'event.kind': '1' }, { 'event.kind': '2' }]);
-      queue.maxQueueSize = 5;
+      queue['maxQueueSize'] = 5;
       queue.addEvents([{ 'event.kind': '3' }, { 'event.kind': '4' }]);
       queue.addEvents([{ 'event.kind': '5' }, { 'event.kind': '6' }]);
       queue.addEvents([{ 'event.kind': '7' }, { 'event.kind': '8' }]);
-      expect(queue.queue.length).toBe(5);
+      expect(queue['queue'].length).toBe(5);
     });
 
     it('empties the queue when sending', async () => {
       const queue = new TelemetryQueue(logger);
-      queue.send = jest.fn();
+      queue['send'] = jest.fn();
       queue.addEvents([{ 'event.kind': '1' }, { 'event.kind': '2' }]);
 
-      expect(queue.queue.length).toBe(2);
+      expect(queue['queue'].length).toBe(2);
       await queue.sendEvents('https://telemetry.elastic.co/v3/send/my-channel', {
         cluster_uuid: '1',
         cluster_name: 'cluster',
@@ -47,8 +47,8 @@ describe('TelemetryQueue', () => {
           number: '7.16.0',
         },
       } as ESClusterInfo);
-      expect(queue.queue.length).toBe(0);
-      expect(queue.send).toHaveBeenCalledWith(
+      expect(queue['queue'].length).toBe(0);
+      expect(queue['send']).toHaveBeenCalledWith(
         [
           { cluster_name: 'cluster', cluster_uuid: '1', 'event.kind': '1' },
           { cluster_name: 'cluster', cluster_uuid: '1', 'event.kind': '2' },
