b30af3d9bcf556c12239e3b8b8fb84ff3d71da2a
Gloria Hornero <snootchie.boochies@gmail.com>
2021-10-20 14:25:39 +0200

    waits for the page to be loaded before continuing with the actions

--- x-pack/plugins/security_solution/cypress/tasks/timeline.ts
+++ x-pack/plugins/security_solution/cypress/tasks/timeline.ts
@@ -162,6 +162,7 @@ export const addFilter = (filter: TimelineFilter): Cypress.Chainable<JQuery<HTML
 };
 
 export const addDataProvider = (filter: TimelineFilter): Cypress.Chainable<JQuery<HTMLElement>> => {
+  cy.get(LOADING_INDICATOR).should('not.exist');
   cy.get(TIMELINE_ADD_FIELD_BUTTON).click();
   cy.get(TIMELINE_DATA_PROVIDER_VALUE).should('have.focus'); // make sure the focus is ready before start typing
 
