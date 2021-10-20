a50f7ccc9af8f73c6613cd27fdc40d8d8df61f58
Thom Heymann <190132+thomheymann@users.noreply.github.com>
2021-10-20 10:22:04 +0100

    Fix maps font path (#115453)
    * fixed font path
    
    * fix functional tests
    
    * fix directory issue
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/api_integration/apis/maps/fonts_api.js
+++ x-pack/test/api_integration/apis/maps/fonts_api.js
@@ -6,11 +6,33 @@
  */
 
 import expect from '@kbn/expect';
+import path from 'path';
+import { copyFile, rm } from 'fs/promises';
 
 export default function ({ getService }) {
   const supertest = getService('supertest');
+  const log = getService('log');
 
   describe('fonts', () => {
+    // [HACK]: On CI tests are run from the different directories than the built and running Kibana
+    // instance. To workaround that we use Kibana `process.cwd()` to construct font path manually.
+    // x-pack tests can be run from root directory or from within x-pack so need to cater for both possibilities.
+    const fontPath = path.join(
+      process.cwd().replace(/x-pack.*$/, ''),
+      'x-pack/plugins/maps/server/fonts/open_sans/0-255.pbf'
+    );
+    const destinationPath = path.join(path.dirname(fontPath), '..', path.basename(fontPath));
+
+    before(async () => {
+      log.debug(`Copying test file from '${fontPath}' to '${destinationPath}'`);
+      await copyFile(fontPath, destinationPath);
+    });
+
+    after(async () => {
+      log.debug(`Removing test file '${destinationPath}'`);
+      await rm(destinationPath);
+    });
+
     it('should return fonts', async () => {
       const resp = await supertest
         .get(`/api/maps/fonts/Open%20Sans%20Regular,Arial%20Unicode%20MS%20Regular/0-255`)
@@ -25,12 +47,12 @@ export default function ({ getService }) {
         .expect(404);
     });
 
-    it('should return 404 when file is not in font folder (../)', async () => {
-      await supertest.get(`/api/maps/fonts/open_sans/..%2fopen_sans%2f0-255`).expect(404);
+    it('should return 404 when file is not in font folder (..)', async () => {
+      await supertest.get(`/api/maps/fonts/open_sans/..%2f0-255`).expect(404);
     });
 
-    it('should return 404 when file is not in font folder (./../)', async () => {
-      await supertest.get(`/api/maps/fonts/open_sans/.%2f..%2fopen_sans%2f0-255`).expect(404);
+    it('should return 404 when file is not in font folder (./..)', async () => {
+      await supertest.get(`/api/maps/fonts/open_sans/.%2f..%2f0-255`).expect(404);
     });
   });
 }
