abd391ce25adc1855300a287bfa6510643ce4471
Tiago Costa <tiagoffcc@hotmail.com>
2021-10-20 16:30:22 +0100

    chore(NA): include all correct transpilations for each jenkins path

--- packages/kbn-test/src/functional_tests/lib/babel_register_for_test_plugins.js
+++ packages/kbn-test/src/functional_tests/lib/babel_register_for_test_plugins.js
@@ -14,15 +14,12 @@ const BASE_REPO_ROOT = Path.resolve(
   Fs.realpathSync(Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'package.json')),
   '..'
 );
-// const REPO_ROOT = process.env.JENKINS_HOME
-//   ? Path.join(
-//       Path.dirname(BASE_REPO_ROOT),
-//       'parallel',
-//       process.env.CI_PARALLEL_PROCESS_NUMBER,
-//       Path.basename(BASE_REPO_ROOT)
-//     )
-//   : BASE_REPO_ROOT;
 
+// Jenkins use a special symlink setup between a main checkout at
+// /dev/shm/workspace/kibana and a workers on at /dev/shm/workspace/parallel/X/kibana
+// We have jobs running at both and as such we need to transpile for both paths.
+// Once we no longer run on Jenkins our REPO_ROOT can just be calculated as
+// Fs.realpathSync($PATH) where $PATH is any Path.resolve(REPO_ROOT, TRANSPILE_KBN_PATH)
 const REPO_ROOT =
   process.env.JENKINS_HOME && !BASE_REPO_ROOT.includes('parallel')
     ? Path.join(
@@ -33,111 +30,30 @@ const REPO_ROOT =
       )
     : BASE_REPO_ROOT;
 
-// // process.env.CI_PARALLEL_PROCESS_NUMBER
-// const KIBANA_ROOT = Path.resolve(__dirname, '../../../../../');
-// const KIBANA_ROOT2 = Fs.realpathSync(REPO_ROOT).includes(
-//   `parallel${Path.separator}${process.env.CI_PARALLEL_PROCESS_NUMBER}${Path.separator}`
-// )
-//   ? Fs.realpathSync(REPO_ROOT).replace(
-//       `parallel${Path.separator}${process.env.CI_PARALLEL_PROCESS_NUMBER}${Path.separator}`,
-//       ''
-//     )
-//   : Fs.realpathSync(REPO_ROOT);
-// // const KIBANA_ROOT2 = process.env.CI_PARALLEL_PROCESS_NUMBER
-// //   ? Path.join(Fs.realpathSync(REPO_ROOT), 'parallel', process.env.CI_PARALLEL_PROCESS_NUMBER)
-// //   : Fs.realpathSync(REPO_ROOT);
-// const testMap = [
-//   Path.resolve(KIBANA_ROOT2, 'test'),
-//   Path.resolve(KIBANA_ROOT2, 'x-pack/test'),
-//   Path.resolve(KIBANA_ROOT2, 'examples'),
-//   Path.resolve(KIBANA_ROOT2, 'x-pack/examples'),
-//   // TODO: should should probably remove this link back to the source
-//   Path.resolve(KIBANA_ROOT2, 'x-pack/plugins/task_manager/server/config.ts'),
-//   Path.resolve(KIBANA_ROOT2, 'src/core/utils/default_app_categories.ts'),
-// ];
-//
-// const testMap2 = testMap.map((path) => Fs.realpathSync(path));
-//
-// console.log(process.env);
-// console.log('REAL REPO ROOT 1: ');
-// console.log(Fs.realpathSync(REPO_ROOT));
-//
-// console.log('REAL REPO ROOT 2: ');
-// console.log(Fs.realpathSync(Fs.realpathSync(REPO_ROOT)));
-//
-// console.log('SIMPLE REPO ROOT: ');
-// console.log(Path.resolve(REPO_ROOT, 'test'));
-// console.log('REAL PATH REPO ROOT: ');
-// console.log(require('fs').realpathSync(Path.resolve(REPO_ROOT, 'test')));
-// console.log('KBN ROOT: ');
-// console.log(Path.resolve(KIBANA_ROOT, 'test'));
-// console.log('TEST PATHS: ');
-// console.log(testMap);
-// console.log('TEST PATHS 2: ');
-// console.log(testMap2);
-// // throw new Error('FAIL CI');
-
-const testMap = [
-  Path.resolve(REPO_ROOT, 'test'),
-  Path.resolve(REPO_ROOT, 'x-pack/test'),
-  Path.resolve(REPO_ROOT, 'examples'),
-  Path.resolve(REPO_ROOT, 'x-pack/examples'),
+const transpileKbnBasePaths = [
+  'test',
+  'x-pack/test',
+  'examples',
+  'x-pack/examples',
   // TODO: should should probably remove this link back to the source
-  Path.resolve(REPO_ROOT, 'x-pack/plugins/task_manager/server/config.ts'),
-  Path.resolve(REPO_ROOT, 'src/core/utils/default_app_categories.ts'),
+  'x-pack/plugins/task_manager/server/config.ts',
+  'src/core/utils/default_app_categories.ts',
 ];
 
-// const testMap2 = [
-//   Fs.realpathSync(Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'test')),
-//   Fs.realpathSync(Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'x-pack/test')),
-//   Fs.realpathSync(Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'examples')),
-//   Fs.realpathSync(Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'x-pack/examples')),
-//   // TODO: should should probably remove this link back to the source
-//   Fs.realpathSync(
-//     Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'x-pack/plugins/task_manager/server/config.ts')
-//   ),
-//   Fs.realpathSync(
-//     Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'src/core/utils/default_app_categories.ts')
-//   ),
-// ];
-
-// const testMap3 = [
-//   Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'test'),
-//   Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'x-pack/test'),
-//   Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'examples'),
-//   Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'x-pack/examples'),
-//   // TODO: should should probably remove this link back to the source
-//   Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'x-pack/plugins/task_manager/server/config.ts'),
-//   Path.resolve(REPO_ROOT_FOLLOWING_SYMLINKS, 'src/core/utils/default_app_categories.ts'),
-// ];
-
-// testMap2.concat(testMap3);
-// testMap.concat(testMap2);
-
-// const finalTestMap = testMap.concat(testMap2);
-const finalTestMap = testMap;
+const transpileKbnPaths = transpileKbnBasePaths.reduce(
+  (prev, curr) => prev.concat([Path.resolve(REPO_ROOT, curr), Path.resolve(BASE_REPO_ROOT, curr)]),
+  []
+);
 
 console.log('TEST PATHS: ');
-console.log(finalTestMap);
+console.log(transpileKbnPaths);
+throw new Error('WAIT');
 
 // modifies all future calls to require() to automatically
 // compile the required source with babel
 require('@babel/register')({
   ignore: [/[\/\\](node_modules|target|dist)[\/\\]/],
-  // only: [
-  //   Path.resolve(REPO_ROOT, 'test'),
-  //   Path.resolve(REPO_ROOT, 'x-pack/test'),
-  //   Path.resolve(REPO_ROOT, 'examples'),
-  //   Path.resolve(REPO_ROOT, 'x-pack/examples'),
-  //   // TODO: should should probably remove this link back to the source
-  //   Path.resolve(REPO_ROOT, 'x-pack/plugins/task_manager/server/config.ts'),
-  //   Path.resolve(REPO_ROOT, 'src/core/utils/default_app_categories.ts'),
-  // ].map((path) =>
-  //   process.env.JENKINS_HOME
-  //     ? Path.join(path, 'parallel', process.env.CI_PARALLEL_PROCESS_NUMBER)
-  //     : path
-  // ),
-  only: finalTestMap,
+  only: transpileKbnBasePaths,
   babelrc: false,
   presets: [require.resolve('@kbn/babel-preset/node_preset')],
   extensions: ['.js', '.ts', '.tsx'],
