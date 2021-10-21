5271bab59bee18d0c062501849dc7d3924c5a909
dej611 <dej611@gmail.com>
2021-10-21 14:54:49 +0200

    :zap: Avoid over time suggestion if too expensive

--- x-pack/plugins/lens/public/indexpattern_datasource/indexpattern_suggestions.ts
+++ x-pack/plugins/lens/public/indexpattern_datasource/indexpattern_suggestions.ts
@@ -21,6 +21,7 @@ import {
   getExistingColumnGroups,
   isReferenced,
   getReferencedColumnIds,
+  hasTermsFunctionWithHighSize,
 } from './operations';
 import { hasField } from './utils';
 import type {
@@ -424,7 +425,7 @@ export function getDatasourceSuggestionsFromCurrentState(
         );
 
         if (!references.length && metrics.length && buckets.length === 0) {
-          if (timeField) {
+          if (timeField && buckets.length < 1 && !hasTermsFunctionWithHighSize(layer)) {
             // suggest current metric over time if there is a default time field
             suggestions.push(createSuggestionWithDefaultDateHistogram(state, layerId, timeField));
           }
@@ -434,7 +435,13 @@ export function getDatasourceSuggestionsFromCurrentState(
 
           // base range intervals are of number dataType.
           // Custom range/intervals have a different dataType so they still receive the Over Time suggestion
-          if (!timeDimension && timeField && !hasNumericDimension) {
+          if (
+            !timeDimension &&
+            timeField &&
+            buckets.length < 2 &&
+            !hasNumericDimension &&
+            !hasTermsFunctionWithHighSize(layer)
+          ) {
             // suggest current configuration over time if there is a default time field
             // and no time dimension yet
             suggestions.push(createSuggestionWithDefaultDateHistogram(state, layerId, timeField));
