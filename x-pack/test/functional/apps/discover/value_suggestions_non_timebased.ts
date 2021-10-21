5ee450228ab76b1a8af9cb6a8f2ce36d228a509e
Maja Grubic <maja.grubic@elastic.co>
2021-10-21 12:11:12 +0200

    Dismiss popover before Discover even loads

--- x-pack/test/functional/apps/discover/value_suggestions_non_timebased.ts
+++ x-pack/test/functional/apps/discover/value_suggestions_non_timebased.ts
@@ -11,7 +11,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
   const queryBar = getService('queryBar');
-  const retry = getService('retry');
+  const browser = getService('browser');
   const PageObjects = getPageObjects(['common', 'settings', 'context', 'header']);
 
   describe('value suggestions non time based', function describeIndexTests() {
@@ -28,15 +28,16 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
 
     it('shows all autosuggest options for a filter in discover context app', async () => {
+      await PageObjects.common.navigateToApp('home');
+      await browser.setLocalStorageItem('data.autocompleteFtuePopover', 'true');
+
       await PageObjects.common.navigateToApp('discover');
       await PageObjects.header.waitUntilLoadingHasFinished();
 
       await queryBar.setQuery('type.keyword : ');
-      await retry.try(async () => {
-        const suggestions = await queryBar.getSuggestions();
-        expect(suggestions.length).to.be(1);
-        expect(suggestions).to.contain('"apache"');
-      });
+      const suggestions = await queryBar.getSuggestions();
+      expect(suggestions.length).to.be(1);
+      expect(suggestions).to.contain('"apache"');
     });
   });
 }
