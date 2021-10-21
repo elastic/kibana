8240a8443014fb05728903d3c92125859ea6fa3f
restrry <restrry@gmail.com>
2021-10-21 20:05:18 +0200

    fix fleet tests

--- x-pack/test/fleet_api_integration/apis/agents/services.ts
+++ x-pack/test/fleet_api_integration/apis/agents/services.ts
@@ -37,10 +37,8 @@ export function setupFleetAndAgents(providerContext: FtrProviderContext) {
   before(async () => {
     // Use elastic/fleet-server service account to execute setup to verify privilege configuration
     const es = providerContext.getService('es');
-    const {
-      body: { token },
-      // @ts-expect-error SecurityCreateServiceTokenRequest should not require `name`
-    } = await es.security.createServiceToken({
+    // @ts-expect-error SecurityCreateServiceTokenRequest should not require `name`
+    const { token } = await es.security.createServiceToken({
       namespace: 'elastic',
       service: 'fleet-server',
     });
