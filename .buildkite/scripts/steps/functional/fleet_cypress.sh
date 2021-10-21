b47d46cc58034256fd273895e9f3dd9af19181cc
Julia Bardi <julia.bardi@elastic.co>
2021-10-21 10:55:34 +0200

    added to buildkite

---
+++ .buildkite/scripts/steps/functional/fleet_cypress.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/steps/functional/common.sh
+
+export JOB=kibana-fleet-cypress
+
+echo "--- Fleet Cypress tests"
+
+cd "$XPACK_DIR"
+
+checks-reporter-with-killswitch "Fleet Cypress Tests" \
+ node scripts/functional_tests \
+   --debug --bail \
+   --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
+   --config test/fleet_cypress/cli_config.ts
