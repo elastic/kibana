e0e3ca3c3c3f75619381c4e6f31c8adcec14cfa8
Catherine Liu <catherine.liu@elastic.co>
2021-10-20 18:39:47 -0700

    [Canvas] Adds editor menu to Canvas (#113194)

--- x-pack/plugins/canvas/public/components/workpad_header/element_menu/element_menu.tsx
+++ x-pack/plugins/canvas/public/components/workpad_header/element_menu/element_menu.tsx
@@ -5,34 +5,4 @@
  * 2.0.
  */
 
-import React, { FC, useCallback } from 'react';
-import { useLocation } from 'react-router-dom';
-import { trackCanvasUiMetric, METRIC_TYPE } from '../../../../public/lib/ui_metric';
-import { CANVAS_APP } from '../../../../common/lib';
-import { ElementMenu as Component, Props } from './element_menu.component';
-import { useEmbeddablesService } from '../../../services';
-
-export const ElementMenu: FC<Omit<Props, 'createNewEmbeddable'>> = (props) => {
-  const embeddablesService = useEmbeddablesService();
-  const stateTransferService = embeddablesService.getStateTransfer();
-  const { pathname, search } = useLocation();
-
-  const createNewEmbeddable = useCallback(() => {
-    const path = '#/';
-    const appId = 'lens';
-
-    if (trackCanvasUiMetric) {
-      trackCanvasUiMetric(METRIC_TYPE.CLICK, `${appId}:create`);
-    }
-
-    stateTransferService.navigateToEditor(appId, {
-      path,
-      state: {
-        originatingApp: CANVAS_APP,
-        originatingPath: `#/${pathname}${search}`,
-      },
-    });
-  }, [pathname, search, stateTransferService]);
-
-  return <Component {...props} createNewEmbeddable={createNewEmbeddable} />;
-};
+export * from './element_menu.component';
