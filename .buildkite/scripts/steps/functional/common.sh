a071bf05ad8f6b24ee37e60fc247a4bc6b429748
Brian Seeders <brian.seeders@elastic.co>
2021-10-21 17:25:25 -0400

    Try out running multiple jobs on agents, including with bootstrap failures

--- .buildkite/scripts/steps/functional/common.sh
+++ .buildkite/scripts/steps/functional/common.sh
@@ -6,6 +6,10 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
+if [[ "${BUILDKITE_PARALLEL_JOB:-}" == 0 ]]; then
+  export CYPRESS_DOWNLOAD_MIRROR="https://us-central1-elastic-kibana-184718as7hd89a7dhas98d6.cloudfunctions.net/kibana-ci-proxy-cache/cypress"
+fi
+
 .buildkite/scripts/bootstrap.sh
 .buildkite/scripts/download_build_artifacts.sh
 
--- .buildkite/scripts/steps/test/api_integration.sh
+++ .buildkite/scripts/steps/test/api_integration.sh
@@ -6,6 +6,8 @@ source .buildkite/scripts/common/util.sh
 
 is_test_execution_step
 
+export CYPRESS_DOWNLOAD_MIRROR="https://us-central1-elastic-kibana-184718as7hd89a7dhas98d6.cloudfunctions.net/kibana-ci-proxy-cache/cypress"
+
 .buildkite/scripts/bootstrap.sh
 
 echo --- API Integration Tests
