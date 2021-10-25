b4fb4ae49632d30b69e112327db13ddeb95533ed
Walter Rafelsberger <walter@elastic.co>
2021-10-25 16:22:24 +0200

    [ML] Functional tests for anomaly detection forecasts.

--- x-pack/plugins/ml/public/application/timeseriesexplorer/timeseriesexplorer.js
+++ x-pack/plugins/ml/public/application/timeseriesexplorer/timeseriesexplorer.js
@@ -1172,9 +1172,13 @@ export class TimeSeriesExplorer extends React.Component {
                     <EuiFlexItem grow={false}>
                       <EuiCheckbox
                         id="toggleShowForecastCheckbox"
-                        label={i18n.translate('xpack.ml.timeSeriesExplorer.showForecastLabel', {
-                          defaultMessage: 'show forecast',
-                        })}
+                        label={
+                          <span data-test-subj={'mlForecastCheckbox'}>
+                            {i18n.translate('xpack.ml.timeSeriesExplorer.showForecastLabel', {
+                              defaultMessage: 'show forecast',
+                            })}
+                          </span>
+                        }
                         checked={showForecast}
                         onChange={this.toggleShowForecastHandler}
                       />
