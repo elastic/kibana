1264a6eafb372098092f1b19c8b933c2bbe2f39f
Greg Thompson <thompson.glowe@gmail.com>
2021-10-25 11:54:37 -0500

    Upgrade EUI to v40.0.0 (#115639)
    * eui to v40.0.0
    
    * tokenKeyword -> tokenTag
    
    * mobileOptions type
    
    * snapshot updates
    
    * Revert "tokenKeyword -> tokenTag"
    
    This reverts commit 0e5eae64e773ffa7e744de5da5e1c277423cd890.
    
    * token snapshot

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/components/curations_table.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/components/curations_table.tsx
@@ -73,11 +73,8 @@ export const CurationsTable: React.FC = () => {
       truncateText: true,
       mobileOptions: {
         header: true,
-        // Note: the below props are valid props per https://elastic.github.io/eui/#/tabular-content/tables (Responsive tables), but EUI's types have a bug reporting it as an error
-        // @ts-ignore
         enlarge: true,
         width: '100%',
-        truncateText: false,
       },
     },
     {
