19b781f824037ee508bcb717c691ca1ebf5c53db
Timothy Sullivan <tsullivan@elastic.co>
2021-10-25 12:49:40 -0700

    update snapshots

--- x-pack/plugins/reporting/server/lib/layouts/preserve_layout.test.ts
+++ x-pack/plugins/reporting/server/lib/layouts/preserve_layout.test.ts
@@ -9,11 +9,23 @@ import { PreserveLayout } from './preserve_layout';
 
 it('preserve layout uses default layout selectors', () => {
   const testPreserveLayout = new PreserveLayout({ width: 16, height: 16 });
-  expect(testPreserveLayout.getCssOverridesPath()).toMatchInlineSnapshot();
-  expect(testPreserveLayout.getBrowserViewport()).toMatchInlineSnapshot();
-  expect(testPreserveLayout.getBrowserZoom()).toMatchInlineSnapshot();
-  expect(testPreserveLayout.getPdfImageSize()).toMatchInlineSnapshot();
-  expect(testPreserveLayout.getPdfPageOrientation()).toMatchInlineSnapshot();
+  expect(testPreserveLayout.getCssOverridesPath()).toMatchInlineSnapshot(
+    `"/home/tsullivan/elastic/kibana/x-pack/plugins/reporting/server/lib/layouts/preserve_layout.css"`
+  );
+  expect(testPreserveLayout.getBrowserViewport()).toMatchInlineSnapshot(`
+    Object {
+      "height": 32,
+      "width": 32,
+    }
+  `);
+  expect(testPreserveLayout.getBrowserZoom()).toMatchInlineSnapshot(`2`);
+  expect(testPreserveLayout.getPdfImageSize()).toMatchInlineSnapshot(`
+    Object {
+      "height": 16,
+      "width": 16,
+    }
+  `);
+  expect(testPreserveLayout.getPdfPageOrientation()).toMatchInlineSnapshot(`undefined`);
   expect(
     testPreserveLayout.getPdfPageSize({
       pageMarginTop: 27,
@@ -23,12 +35,26 @@ it('preserve layout uses default layout selectors', () => {
       headingHeight: 82,
       subheadingHeight: 96,
     })
-  ).toMatchInlineSnapshot();
+  ).toMatchInlineSnapshot(`
+    Object {
+      "height": 382,
+      "width": 176,
+    }
+  `);
 
-  expect(testPreserveLayout.selectors).toMatchInlineSnapshot();
-  expect(testPreserveLayout.groupCount).toMatchInlineSnapshot();
-  expect(testPreserveLayout.height).toMatchInlineSnapshot();
-  expect(testPreserveLayout.width).toMatchInlineSnapshot();
+  expect(testPreserveLayout.selectors).toMatchInlineSnapshot(`
+    Object {
+      "itemsCountAttribute": "data-shared-items-count",
+      "renderComplete": "[data-shared-item]",
+      "renderError": "[data-render-error]",
+      "renderErrorAttribute": "data-render-error",
+      "screenshot": "[data-shared-items-container]",
+      "timefilterDurationAttribute": "data-shared-timefilter-duration",
+    }
+  `);
+  expect(testPreserveLayout.groupCount).toMatchInlineSnapshot(`1`);
+  expect(testPreserveLayout.height).toMatchInlineSnapshot(`16`);
+  expect(testPreserveLayout.width).toMatchInlineSnapshot(`16`);
 });
 
 it('preserve layout allows customizable selectors', () => {
@@ -36,5 +62,14 @@ it('preserve layout allows customizable selectors', () => {
     { width: 16, height: 16 },
     { renderComplete: '[great-test-selectors]' }
   );
-  expect(testPreserveLayout.selectors).toMatchInlineSnapshot();
+  expect(testPreserveLayout.selectors).toMatchInlineSnapshot(`
+    Object {
+      "itemsCountAttribute": "data-shared-items-count",
+      "renderComplete": "[great-test-selectors]",
+      "renderError": "[data-render-error]",
+      "renderErrorAttribute": "data-render-error",
+      "screenshot": "[data-shared-items-container]",
+      "timefilterDurationAttribute": "data-shared-timefilter-duration",
+    }
+  `);
 });
