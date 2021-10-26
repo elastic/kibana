7c8b12079659ec076532f371be8cec382d987cf6
Constance Chen <constance.chen.3@gmail.com>
2021-10-26 11:50:44 -0700

    Remove memo()

--- src/plugins/vis_types/table/public/components/table_vis_basic.tsx
+++ src/plugins/vis_types/table/public/components/table_vis_basic.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import React, { memo, useCallback, useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { EuiDataGrid, EuiDataGridProps, EuiDataGridSorting, EuiTitle } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { orderBy } from 'lodash';
@@ -26,140 +26,138 @@ interface TableVisBasicProps {
   uiStateProps: TableVisUseUiStateProps;
 }
 
-export const TableVisBasic = memo(
-  ({
-    fireEvent,
-    table,
-    visConfig,
-    title,
-    uiStateProps: { columnsWidth, sort, setColumnsWidth, setSort },
-  }: TableVisBasicProps) => {
-    const { columns, rows, formattedColumns } = table;
+export const TableVisBasic = ({
+  fireEvent,
+  table,
+  visConfig,
+  title,
+  uiStateProps: { columnsWidth, sort, setColumnsWidth, setSort },
+}: TableVisBasicProps) => {
+  const { columns, rows, formattedColumns } = table;
 
-    // custom sorting is in place until the EuiDataGrid sorting gets rid of flaws -> https://github.com/elastic/eui/issues/4108
-    const sortedRows = useMemo(
-      () =>
-        sort.columnIndex !== null && sort.direction
-          ? orderBy(rows, columns[sort.columnIndex]?.id, sort.direction)
-          : rows,
-      [columns, rows, sort]
-    );
+  // custom sorting is in place until the EuiDataGrid sorting gets rid of flaws -> https://github.com/elastic/eui/issues/4108
+  const sortedRows = useMemo(
+    () =>
+      sort.columnIndex !== null && sort.direction
+        ? orderBy(rows, columns[sort.columnIndex]?.id, sort.direction)
+        : rows,
+    [columns, rows, sort]
+  );
 
-    // renderCellValue is a component which renders a cell based on column and row indexes
-    const renderCellValue = useMemo(
-      () => createTableVisCell(sortedRows, formattedColumns, visConfig.autoFitRowToContent),
-      [formattedColumns, sortedRows, visConfig.autoFitRowToContent]
-    );
+  // renderCellValue is a component which renders a cell based on column and row indexes
+  const renderCellValue = useMemo(
+    () => createTableVisCell(sortedRows, formattedColumns, visConfig.autoFitRowToContent),
+    [formattedColumns, sortedRows, visConfig.autoFitRowToContent]
+  );
 
-    const rowHeightsOptions = useMemo(
-      () =>
-        visConfig.autoFitRowToContent
-          ? ({ defaultHeight: 'auto' } as unknown as EuiDataGridProps['rowHeightsOptions'])
-          : undefined,
-      [visConfig.autoFitRowToContent]
-    );
+  const rowHeightsOptions = useMemo(
+    () =>
+      visConfig.autoFitRowToContent
+        ? ({ defaultHeight: 'auto' } as unknown as EuiDataGridProps['rowHeightsOptions'])
+        : undefined,
+    [visConfig.autoFitRowToContent]
+  );
 
-    // Columns config
-    const gridColumns = createGridColumns(
-      columns,
-      sortedRows,
-      formattedColumns,
-      columnsWidth,
-      fireEvent
-    );
+  // Columns config
+  const gridColumns = createGridColumns(
+    columns,
+    sortedRows,
+    formattedColumns,
+    columnsWidth,
+    fireEvent
+  );
 
-    // Pagination config
-    const pagination = usePagination(visConfig, rows.length);
-    // Sorting config
-    const sortingColumns = useMemo(
-      () =>
-        sort.columnIndex !== null && sort.direction
-          ? [{ id: columns[sort.columnIndex]?.id, direction: sort.direction }]
-          : [],
-      [columns, sort]
-    );
-    const onSort = useCallback(
-      (sortingCols: EuiDataGridSorting['columns'] | []) => {
-        // data table vis sorting now only handles one column sorting
-        // if data grid provides more columns to sort, pick only the next column to sort
-        const newSortValue = sortingCols.length <= 1 ? sortingCols[0] : sortingCols[1];
-        setSort(
-          newSortValue && {
-            columnIndex: columns.findIndex((c) => c.id === newSortValue.id),
-            direction: newSortValue.direction,
-          }
-        );
-      },
-      [columns, setSort]
-    );
+  // Pagination config
+  const pagination = usePagination(visConfig, rows.length);
+  // Sorting config
+  const sortingColumns = useMemo(
+    () =>
+      sort.columnIndex !== null && sort.direction
+        ? [{ id: columns[sort.columnIndex]?.id, direction: sort.direction }]
+        : [],
+    [columns, sort]
+  );
+  const onSort = useCallback(
+    (sortingCols: EuiDataGridSorting['columns'] | []) => {
+      // data table vis sorting now only handles one column sorting
+      // if data grid provides more columns to sort, pick only the next column to sort
+      const newSortValue = sortingCols.length <= 1 ? sortingCols[0] : sortingCols[1];
+      setSort(
+        newSortValue && {
+          columnIndex: columns.findIndex((c) => c.id === newSortValue.id),
+          direction: newSortValue.direction,
+        }
+      );
+    },
+    [columns, setSort]
+  );
 
-    const dataGridAriaLabel =
-      title ||
-      visConfig.title ||
-      i18n.translate('visTypeTable.defaultAriaLabel', {
-        defaultMessage: 'Data table visualization',
+  const dataGridAriaLabel =
+    title ||
+    visConfig.title ||
+    i18n.translate('visTypeTable.defaultAriaLabel', {
+      defaultMessage: 'Data table visualization',
+    });
+
+  const onColumnResize: EuiDataGridProps['onColumnResize'] = useCallback(
+    ({ columnId, width }) => {
+      const colIndex = columns.findIndex((c) => c.id === columnId);
+      setColumnsWidth({
+        colIndex,
+        width,
       });
+    },
+    [columns, setColumnsWidth]
+  );
 
-    const onColumnResize: EuiDataGridProps['onColumnResize'] = useCallback(
-      ({ columnId, width }) => {
-        const colIndex = columns.findIndex((c) => c.id === columnId);
-        setColumnsWidth({
-          colIndex,
-          width,
-        });
-      },
-      [columns, setColumnsWidth]
-    );
-
-    return (
-      <>
-        {title && (
-          <EuiTitle size="xs">
-            <h3>{title}</h3>
-          </EuiTitle>
-        )}
-        <EuiDataGrid
-          aria-label={dataGridAriaLabel}
-          columns={gridColumns}
-          gridStyle={{
-            border: 'horizontal',
-            header: 'underline',
-          }}
-          rowHeightsOptions={rowHeightsOptions}
-          rowCount={rows.length}
-          columnVisibility={{
-            visibleColumns: columns.map(({ id }) => id),
-            setVisibleColumns: () => {},
-          }}
-          toolbarVisibility={
-            visConfig.showToolbar && {
-              showColumnSelector: false,
-              showFullScreenSelector: false,
-              showSortSelector: false,
-              showStyleSelector: false,
-              additionalControls: (
-                <TableVisControls
-                  dataGridAriaLabel={dataGridAriaLabel}
-                  columns={columns}
-                  // csv exports sorted table
-                  rows={sortedRows}
-                  filename={visConfig.title}
-                />
-              ),
-            }
+  return (
+    <>
+      {title && (
+        <EuiTitle size="xs">
+          <h3>{title}</h3>
+        </EuiTitle>
+      )}
+      <EuiDataGrid
+        aria-label={dataGridAriaLabel}
+        columns={gridColumns}
+        gridStyle={{
+          border: 'horizontal',
+          header: 'underline',
+        }}
+        rowHeightsOptions={rowHeightsOptions}
+        rowCount={rows.length}
+        columnVisibility={{
+          visibleColumns: columns.map(({ id }) => id),
+          setVisibleColumns: () => {},
+        }}
+        toolbarVisibility={
+          visConfig.showToolbar && {
+            showColumnSelector: false,
+            showFullScreenSelector: false,
+            showSortSelector: false,
+            showStyleSelector: false,
+            additionalControls: (
+              <TableVisControls
+                dataGridAriaLabel={dataGridAriaLabel}
+                columns={columns}
+                // csv exports sorted table
+                rows={sortedRows}
+                filename={visConfig.title}
+              />
+            ),
           }
-          renderCellValue={renderCellValue}
-          renderFooterCellValue={
-            visConfig.showTotal
-              ? ({ columnId }) => formattedColumns[columnId].formattedTotal || null
-              : undefined
-          }
-          pagination={pagination}
-          sorting={{ columns: sortingColumns, onSort }}
-          onColumnResize={onColumnResize}
-          minSizeForControls={1}
-        />
-      </>
-    );
-  }
-);
+        }
+        renderCellValue={renderCellValue}
+        renderFooterCellValue={
+          visConfig.showTotal
+            ? ({ columnId }) => formattedColumns[columnId].formattedTotal || null
+            : undefined
+        }
+        pagination={pagination}
+        sorting={{ columns: sortingColumns, onSort }}
+        onColumnResize={onColumnResize}
+        minSizeForControls={1}
+      />
+    </>
+  );
+};
