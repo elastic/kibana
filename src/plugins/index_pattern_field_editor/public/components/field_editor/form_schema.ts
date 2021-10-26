c36a193b5afd3dbe67e1a9271e614a368e78d760
Shivindera Singh <shivindera@gmail.com>
2021-10-26 16:45:45 +0200

    [AppServices] Disallow creating runtime and scripted fields with * in the name (#110081) (#116119)

--- src/plugins/index_pattern_field_editor/public/components/field_editor/form_schema.ts
+++ src/plugins/index_pattern_field_editor/public/components/field_editor/form_schema.ts
@@ -11,7 +11,7 @@ import { fieldValidators } from '../../shared_imports';
 
 import { RUNTIME_FIELD_OPTIONS } from './constants';
 
-const { emptyField, numberGreaterThanField } = fieldValidators;
+const { containsCharsField, emptyField, numberGreaterThanField } = fieldValidators;
 
 export const schema = {
   name: {
@@ -28,6 +28,17 @@ export const schema = {
             }
           )
         ),
+      },
+      {
+        validator: containsCharsField({
+          message: i18n.translate(
+            'indexPatternFieldEditor.editor.form.validations.starCharacterNotAllowedValidationErrorMessage',
+            {
+              defaultMessage: 'The field cannot have * in the name.',
+            }
+          ),
+          chars: '*',
+        }),
       },
     ],
   },
