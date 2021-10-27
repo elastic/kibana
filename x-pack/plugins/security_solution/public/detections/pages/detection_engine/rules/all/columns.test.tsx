2fe926697ed36a32f2b042857ec70252ccfafc68
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2021-10-27 10:10:05 +0100

    CR: remove unnecesary tests

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/columns.test.tsx
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/columns.test.tsx
@@ -5,19 +5,11 @@
  * 2.0.
  */
 
-import uuid from 'uuid';
-
-import type { DocLinksStart } from 'src/core/public';
 import { scopedHistoryMock } from 'src/core/public/mocks';
+import uuid from 'uuid';
 import '../../../../../common/mock/match_media';
 import { deleteRulesAction, duplicateRulesAction, editRuleAction } from './actions';
-import {
-  getActions,
-  getColumns,
-  getMonitoringColumns,
-  RulesColumns,
-  RulesStatusesColumns,
-} from './columns';
+import { getActions } from './columns';
 import { mockRule } from './__mocks__/mock';
 
 jest.mock('./actions', () => ({
@@ -32,15 +24,12 @@ const deleteRulesActionMock = deleteRulesAction as jest.Mock;
 const editRuleActionMock = editRuleAction as jest.Mock;
 
 describe('AllRulesTable Columns', () => {
-  const dispatch = jest.fn();
-  const dispatchToaster = jest.fn();
-  const reFetchRules = jest.fn();
-  const refetchPrePackagedRulesStatus = jest.fn();
-  const formatUrl = jest.fn();
-  const navigateToApp = jest.fn();
-
   describe('getActions', () => {
     const rule = mockRule(uuid.v4());
+    const dispatch = jest.fn();
+    const dispatchToaster = jest.fn();
+    const reFetchRules = jest.fn();
+    const refetchPrePackagedRulesStatus = jest.fn();
 
     beforeEach(() => {
       duplicateRulesActionMock.mockClear();
@@ -50,6 +39,7 @@ describe('AllRulesTable Columns', () => {
 
     test('duplicate rule onClick should call rule edit after the rule is duplicated', async () => {
       const ruleDuplicate = mockRule('newRule');
+      const navigateToApp = jest.fn();
       duplicateRulesActionMock.mockImplementation(() => Promise.resolve([ruleDuplicate]));
 
       const duplicateRulesActionObject = getActions(
@@ -67,6 +57,8 @@ describe('AllRulesTable Columns', () => {
     });
 
     test('delete rule onClick should call refetch after the rule is deleted', async () => {
+      const navigateToApp = jest.fn();
+
       const deleteRulesActionObject = getActions(
         dispatch,
         dispatchToaster,
@@ -82,38 +74,6 @@ describe('AllRulesTable Columns', () => {
       expect(deleteRulesActionMock.mock.invocationCallOrder[0]).toBeLessThan(
         reFetchRules.mock.invocationCallOrder[0]
       );
-    });
-  });
-
-  describe('Columns', () => {
-    const rulesColumns = getColumns({
-      dispatch,
-      dispatchToaster,
-      history,
-      formatUrl,
-      navigateToApp,
-      reFetchRules,
-      refetchPrePackagedRulesStatus,
-      hasMlPermissions: false,
-      hasPermissions: false,
-      loadingRuleIds: [],
-      hasReadActionsPrivileges: false,
-    });
-
-    const docsLinksStartMock = { links: { siem: { troubleshootGaps: 'mock' } } } as DocLinksStart;
-    const ruleMonitoringColumns = getMonitoringColumns(
-      navigateToApp,
-      formatUrl,
-      docsLinksStartMock
-    );
-
-    test.each([
-      ['Rules', rulesColumns],
-      ['Rule Monitoring', ruleMonitoringColumns],
-    ])('table "%s" should not have truncated text options for column items', (_, columns) => {
-      columns.forEach((column: RulesColumns | RulesStatusesColumns) => {
-        expect(column).not.toHaveProperty('truncateText');
-      });
     });
   });
 });
