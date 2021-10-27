25ff5a6ea27d2f3bda846bff60474b1ce90ab25b
Brian Seeders <brian.seeders@elastic.co>
2021-10-26 22:47:35 -0400

    Try running functional tests from source

--- .buildkite/scripts/steps/functional/common.sh
+++ .buildkite/scripts/steps/functional/common.sh
@@ -7,6 +7,8 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 .buildkite/scripts/bootstrap.sh
-.buildkite/scripts/download_build_artifacts.sh
+# .buildkite/scripts/download_build_artifacts.sh
+
+export KIBANA_BUILD_LOCATION=''
 
 is_test_execution_step
