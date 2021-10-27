88d7c79c7d74856f3b914e922ca3f3ab4b6fef07
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-10-27 12:52:34 +0300

    Added new GlobalConfig layout.

--- x-pack/plugins/canvas/public/components/workpad_config/workpad_config.component.tsx
+++ x-pack/plugins/canvas/public/components/workpad_config/workpad_config.component.tsx
@@ -76,10 +76,6 @@ const strings = {
     i18n.translate('xpack.canvas.workpadConfig.widthLabel', {
       defaultMessage: 'Width',
     }),
-  getTitle: () =>
-    i18n.translate('xpack.canvas.workpadConfig.title', {
-      defaultMessage: 'Workpad settings',
-    }),
   getUSLetterButtonLabel: () =>
     i18n.translate('xpack.canvas.workpadConfig.USLetterButtonLabel', {
       defaultMessage: 'US Letter',
@@ -127,14 +123,6 @@ export const WorkpadConfig: FunctionComponent<Props> = (props) => {
 
   return (
     <div>
-      <div className="canvasLayout__sidebarHeaderWorkpad">
-        <EuiTitle size="xs">
-          <h4>{strings.getTitle()}</h4>
-        </EuiTitle>
-      </div>
-
-      <EuiSpacer size="m" />
-
       <EuiFormRow label={strings.getNameLabel()} display="rowCompressed">
         <EuiFieldText compressed value={name} onChange={(e) => setName(e.target.value)} />
       </EuiFormRow>
