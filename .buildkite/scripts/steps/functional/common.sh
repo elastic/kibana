790a25e7720488b0604323eda7a8ec89623609d6
Tyler Smalley <tyler.smalley@elastic.co>
2021-10-27 12:33:32 -0700

    Testing verbose logging
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/steps/functional/common.sh
+++ .buildkite/scripts/steps/functional/common.sh
@@ -7,6 +7,9 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 .buildkite/scripts/bootstrap.sh
+
+export KIBANA_BUILD_ID="90bfc8e5-5ebb-40c8-88a9-660b53562e9d"
+
 .buildkite/scripts/download_build_artifacts.sh
 
 is_test_execution_step
