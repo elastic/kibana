d4d4c5f0e9bef81c03491fe61f9329f1ec488136
cauemarcondes <caue.marcondes@elastic.co>
2021-10-28 13:56:45 -0400

    addressing pr changes

--- x-pack/test/apm_api_integration/tests/errors/errors_distribution.ts
+++ x-pack/test/apm_api_integration/tests/errors/errors_distribution.ts
@@ -61,154 +61,147 @@ export default function ApiTest({ getService }: FtrProviderContext) {
     });
   });
 
-  registry.when(
-    'when data is not loaded',
-    { config: 'basic', archives: ['apm_8.0.0_empty'] },
-    () => {
-      describe('errors distribution', () => {
-        const PROD_LIST_RATE = 75;
-        const PROD_LIST_ERROR_RATE = 25;
-        const PROD_ID_RATE = 50;
-        const PROD_ID_ERROR_RATE = 50;
-        const ERROR_NAME_1 = 'Error test 1';
-        const ERROR_NAME_2 = 'Error test 2';
+  registry.when('when data is loaded', { config: 'basic', archives: ['apm_8.0.0_empty'] }, () => {
+    describe('errors distribution', () => {
+      const PROD_LIST_RATE = 75;
+      const PROD_LIST_ERROR_RATE = 25;
+      const PROD_ID_RATE = 50;
+      const PROD_ID_ERROR_RATE = 50;
+      const ERROR_NAME_1 = 'Error test 1';
+      const ERROR_NAME_2 = 'Error test 2';
+      before(async () => {
+        const serviceGoProdInstance = service(serviceName, 'production', 'go').instance(
+          'instance-a'
+        );
+
+        const transactionNameProductList = 'GET /api/product/list';
+        const transactionNameProductId = 'GET /api/product/:id';
+
+        await synthtraceEsClient.index([
+          ...timerange(start, end)
+            .interval('1m')
+            .rate(PROD_LIST_RATE)
+            .flatMap((timestamp) =>
+              serviceGoProdInstance
+                .transaction(transactionNameProductList)
+                .timestamp(timestamp)
+                .duration(1000)
+                .success()
+                .serialize()
+            ),
+          ...timerange(start, end)
+            .interval('1m')
+            .rate(PROD_LIST_ERROR_RATE)
+            .flatMap((timestamp) =>
+              serviceGoProdInstance
+                .transaction(transactionNameProductList)
+                .errors(serviceGoProdInstance.error(ERROR_NAME_1, 'foo').timestamp(timestamp))
+                .duration(1000)
+                .timestamp(timestamp)
+                .failure()
+                .serialize()
+            ),
+          ...timerange(start, end)
+            .interval('1m')
+            .rate(PROD_ID_RATE)
+            .flatMap((timestamp) =>
+              serviceGoProdInstance
+                .transaction(transactionNameProductId)
+                .timestamp(timestamp)
+                .duration(1000)
+                .success()
+                .serialize()
+            ),
+          ...timerange(start, end)
+            .interval('1m')
+            .rate(PROD_ID_ERROR_RATE)
+            .flatMap((timestamp) =>
+              serviceGoProdInstance
+                .transaction(transactionNameProductId)
+                .errors(serviceGoProdInstance.error(ERROR_NAME_2, 'bar').timestamp(timestamp))
+                .duration(1000)
+                .timestamp(timestamp)
+                .failure()
+                .serialize()
+            ),
+        ]);
+      });
+
+      after(() => synthtraceEsClient.clean());
+
+      describe('without comparison', () => {
+        let errorsDistribution: ErrorsDistributionReturn;
         before(async () => {
-          const serviceGoProdInstance = service(serviceName, 'production', 'go').instance(
-            'instance-a'
-          );
-
-          const transactionNameProductList = 'GET /api/product/list';
-          const transactionNameProductId = 'GET /api/product/:id';
-
-          await synthtraceEsClient.index([
-            ...timerange(start, end)
-              .interval('1m')
-              .rate(PROD_LIST_RATE)
-              .flatMap((timestamp) =>
-                serviceGoProdInstance
-                  .transaction(transactionNameProductList)
-                  .timestamp(timestamp)
-                  .duration(1000)
-                  .success()
-                  .serialize()
-              ),
-            ...timerange(start, end)
-              .interval('1m')
-              .rate(PROD_LIST_ERROR_RATE)
-              .flatMap((timestamp) =>
-                serviceGoProdInstance
-                  .transaction(transactionNameProductList)
-                  .errors(serviceGoProdInstance.error(ERROR_NAME_1, 'foo').timestamp(timestamp))
-                  .duration(1000)
-                  .timestamp(timestamp)
-                  .failure()
-                  .serialize()
-              ),
-            ...timerange(start, end)
-              .interval('1m')
-              .rate(PROD_ID_RATE)
-              .flatMap((timestamp) =>
-                serviceGoProdInstance
-                  .transaction(transactionNameProductId)
-                  .timestamp(timestamp)
-                  .duration(1000)
-                  .success()
-                  .serialize()
-              ),
-            ...timerange(start, end)
-              .interval('1m')
-              .rate(PROD_ID_ERROR_RATE)
-              .flatMap((timestamp) =>
-                serviceGoProdInstance
-                  .transaction(transactionNameProductId)
-                  .errors(serviceGoProdInstance.error(ERROR_NAME_2, 'bar').timestamp(timestamp))
-                  .duration(1000)
-                  .timestamp(timestamp)
-                  .failure()
-                  .serialize()
-              ),
-          ]);
+          const response = await callApi();
+          errorsDistribution = response.body;
         });
 
-        after(() => synthtraceEsClient.clean());
-
-        describe('without comparison', () => {
-          let errorsDistribution: ErrorsDistributionReturn;
-          before(async () => {
-            const response = await callApi();
-            errorsDistribution = response.body;
-          });
-
-          it('has correct number of occurences', () => {
-            const countSum = sumBy(errorsDistribution.currentPeriod, 'y');
-            const numberOfBucket = 15;
-            expect(countSum).to.equal((PROD_ID_ERROR_RATE + PROD_LIST_ERROR_RATE) * numberOfBucket);
-          });
-        });
-
-        describe('group id Error test 2', () => {
-          let errorsDistribution: ErrorsDistributionReturn;
-          before(async () => {
-            const response = await callApi({
-              query: { kuery: 'error.exception.type:"foo"' },
-            });
-            errorsDistribution = response.body;
-          });
-          it('has correct number of occurences', () => {
-            const countSum = sumBy(errorsDistribution.currentPeriod, 'y');
-            const numberOfBucket = 15;
-            expect(countSum).to.equal(PROD_LIST_ERROR_RATE * numberOfBucket);
-          });
-        });
-
-        describe('with comparison', () => {
-          let errorsDistribution: ErrorsDistributionReturn;
-          before(async () => {
-            const response = await callApi({
-              query: {
-                start: moment(end).subtract(7, 'minutes').toISOString(),
-                end: new Date(end).toISOString(),
-                comparisonStart: new Date(start).toISOString(),
-                comparisonEnd: moment(start).add(7, 'minutes').toISOString(),
-              },
-            });
-            errorsDistribution = response.body;
-          });
-          it('returns some data', () => {
-            expect(Object.keys(errorsDistribution.currentPeriod).length).to.be.greaterThan(0);
-            expect(Object.keys(errorsDistribution.previousPeriod).length).to.be.greaterThan(0);
-
-            const hasCurrentPeriodData = errorsDistribution.currentPeriod.some(({ y }) =>
-              isFiniteNumber(y)
-            );
-
-            const hasPreviousPeriodData = errorsDistribution.previousPeriod.some(({ y }) =>
-              isFiniteNumber(y)
-            );
-
-            expect(hasCurrentPeriodData).to.equal(true);
-            expect(hasPreviousPeriodData).to.equal(true);
-          });
-
-          it('has same start time for both periods', () => {
-            expect(first(errorsDistribution.currentPeriod)?.x).to.equal(
-              first(errorsDistribution.previousPeriod)?.x
-            );
-          });
-
-          it('has same end time for both periods', () => {
-            expect(last(errorsDistribution.currentPeriod)?.x).to.equal(
-              last(errorsDistribution.previousPeriod)?.x
-            );
-          });
-
-          it('returns same number of buckets for both periods', () => {
-            expect(errorsDistribution.currentPeriod.length).to.equal(
-              errorsDistribution.previousPeriod.length
-            );
-          });
+        it('has correct number of occurences', () => {
+          const countSum = sumBy(errorsDistribution.currentPeriod, 'y');
+          const numberOfBuckets = 15;
+          expect(countSum).to.equal((PROD_ID_ERROR_RATE + PROD_LIST_ERROR_RATE) * numberOfBuckets);
         });
       });
-    }
-  );
+
+      describe('group id Error test 2', () => {
+        let errorsDistribution: ErrorsDistributionReturn;
+        before(async () => {
+          const response = await callApi({
+            query: { kuery: 'error.exception.type:"foo"' },
+          });
+          errorsDistribution = response.body;
+        });
+        it('has correct number of occurences', () => {
+          const countSum = sumBy(errorsDistribution.currentPeriod, 'y');
+          const numberOfBuckets = 15;
+          expect(countSum).to.equal(PROD_LIST_ERROR_RATE * numberOfBuckets);
+        });
+      });
+
+      describe('with comparison', () => {
+        let errorsDistribution: ErrorsDistributionReturn;
+        before(async () => {
+          const response = await callApi({
+            query: {
+              start: moment(end).subtract(7, 'minutes').toISOString(),
+              end: new Date(end).toISOString(),
+              comparisonStart: new Date(start).toISOString(),
+              comparisonEnd: moment(start).add(7, 'minutes').toISOString(),
+            },
+          });
+          errorsDistribution = response.body;
+        });
+        it('returns some data', () => {
+          const hasCurrentPeriodData = errorsDistribution.currentPeriod.some(({ y }) =>
+            isFiniteNumber(y)
+          );
+
+          const hasPreviousPeriodData = errorsDistribution.previousPeriod.some(({ y }) =>
+            isFiniteNumber(y)
+          );
+
+          expect(hasCurrentPeriodData).to.equal(true);
+          expect(hasPreviousPeriodData).to.equal(true);
+        });
+
+        it('has same start time for both periods', () => {
+          expect(first(errorsDistribution.currentPeriod)?.x).to.equal(
+            first(errorsDistribution.previousPeriod)?.x
+          );
+        });
+
+        it('has same end time for both periods', () => {
+          expect(last(errorsDistribution.currentPeriod)?.x).to.equal(
+            last(errorsDistribution.previousPeriod)?.x
+          );
+        });
+
+        it('returns same number of buckets for both periods', () => {
+          expect(errorsDistribution.currentPeriod.length).to.equal(
+            errorsDistribution.previousPeriod.length
+          );
+        });
+      });
+    });
+  });
 }
