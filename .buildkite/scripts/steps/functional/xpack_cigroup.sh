b75d9a9cce59cfba125e507f6999588fb6c11b3b
Brian Seeders <brian.seeders@elastic.co>
2021-10-28 16:06:43 -0400

    Try to get debug logs

--- .buildkite/scripts/steps/functional/xpack_cigroup.sh
+++ .buildkite/scripts/steps/functional/xpack_cigroup.sh
@@ -11,8 +11,22 @@ echo "--- Default CI Group $CI_GROUP"
 
 cd "$XPACK_DIR"
 
-echo 'Sleeping...'
-sleep 86400
+echo 'logging:
+  appenders:
+    console:
+      type: console
+      layout:
+        type: pattern
+        pattern: "[%date][%level] %message - %meta"
+    file:
+      type: file
+      fileName: /var/lib/buildkite-agent/kibana.log
+      layout:
+        type: pattern
+        pattern: "[%date][%level] %message - %meta"
+  root:
+    appenders: [default, console, file]
+    level: debug' > "$KIBANA_BUILD_LOCATION/config/kibana.yml"
 
 checks-reporter-with-killswitch "X-Pack Chrome Functional tests / Group ${CI_GROUP}" \
   node scripts/functional_tests \
@@ -20,4 +34,4 @@ checks-reporter-with-killswitch "X-Pack Chrome Functional tests / Group ${CI_GRO
     --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
     --include-tag "ciGroup$CI_GROUP"
 
-cd "$KIBANA_DIR"
+buildkite-agent artifact upload /var/lib/buildkite-agent/kibana.log
