dabfd3928d6af840a2fe74bb13c17bacbca50c06
Christos Nasikas <christos.nasikas@elastic.co>
2021-10-28 16:26:00 +0300

    Add tests

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/servicenow/helpers.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/servicenow/helpers.ts
@@ -28,7 +28,7 @@ export const isFieldInvalid = (
 ): boolean => error !== undefined && error.length > 0 && field != null;
 
 // TODO: Remove when the applications are certified
-export const isDeprecatedConnector = (connector: ServiceNowActionConnector): boolean => {
+export const isDeprecatedConnector = (connector?: ServiceNowActionConnector): boolean => {
   if (connector == null) {
     return false;
   }
