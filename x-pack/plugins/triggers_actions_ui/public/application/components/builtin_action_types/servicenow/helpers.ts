f621386678ebc0839a77e73f063102a6276b0359
Christos Nasikas <christos.nasikas@elastic.co>
2021-10-28 15:39:08 +0300

    Fix bug when deleting connector

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/servicenow/helpers.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/servicenow/helpers.ts
@@ -30,7 +30,7 @@ export const isFieldInvalid = (
 // TODO: Remove when the applications are certified
 export const isDeprecatedConnector = (connector: ServiceNowActionConnector): boolean => {
   if (connector == null) {
-    return true;
+    return false;
   }
 
   if (!ENABLE_NEW_SN_ITSM_CONNECTOR && connector.actionTypeId === '.servicenow') {
