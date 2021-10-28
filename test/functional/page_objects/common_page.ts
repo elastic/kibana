2899b4f3cdcf014749946c97ced680d69f15da92
Tre' Seymour <wayne.seymour@elastic.co>
2021-10-28 13:58:52 +0100

    [QA][refactor] discover test
    Drop two saved objects: search and idx pattern,
    in the after method.
    
    Add the formatting fn to the common page,
    such that every time setTime is invoked, the time is
    formatted to address: https://momentjs.com/guides/#/warnings/js-date/

--- test/functional/page_objects/common_page.ts
+++ test/functional/page_objects/common_page.ts
@@ -11,6 +11,7 @@ import expect from '@kbn/expect';
 // @ts-ignore
 import fetch from 'node-fetch';
 import { getUrl } from '@kbn/test';
+import moment from 'moment';
 import { FtrService } from '../ftr_provider_context';
 
 interface NavigateProps {
@@ -502,11 +503,30 @@ export class CommonPageObject extends FtrService {
     }
   }
 
+  formatTime(time: TimeStrings, fmt: string = 'MMM D, YYYY @ HH:mm:ss.SSS') {
+    return Object.keys(time)
+      .map((x) => moment(time[x], [fmt]).format())
+      .reduce(
+        (acc, curr, idx) => {
+          if (idx === 0) acc.from = curr;
+          acc.to = curr;
+          return acc;
+        },
+        { from: '', to: '' }
+      );
+  }
+
   async setTime(time: { from: string; to: string }) {
-    await this.kibanaServer.uiSettings.replace({ 'timepicker:timeDefaults': JSON.stringify(time) });
+    await this.kibanaServer.uiSettings.replace({
+      'timepicker:timeDefaults': JSON.stringify(this.formatTime(time)),
+    });
   }
 
   async unsetTime() {
     await this.kibanaServer.uiSettings.unset('timepicker:timeDefaults');
   }
+}
+export interface TimeStrings extends Record<string, any> {
+  from: string;
+  to: string;
 }
