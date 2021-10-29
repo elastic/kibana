7d38b4b85ee7065702b6e115d9c03b5466ba1f88
Brian Seeders <brian.seeders@elastic.co>
2021-10-29 17:26:13 -0400

    Add GITHUB_STEP_COMMIT_STATUS_ENABLED and start removing checks reporter

--- .buildkite/scripts/steps/lint.sh
+++ .buildkite/scripts/steps/lint.sh
@@ -7,9 +7,7 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 
 echo '--- Lint: stylelint'
-checks-reporter-with-killswitch "Lint: stylelint" \
-  node scripts/stylelint
+node scripts/stylelint
 
 echo '--- Lint: eslint'
-checks-reporter-with-killswitch "Lint: eslint" \
-  node scripts/eslint --no-cache
+node scripts/eslint --no-cache
