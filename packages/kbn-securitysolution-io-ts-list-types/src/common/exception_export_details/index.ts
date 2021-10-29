d108e4a1a9b9213c24fcf87d15e2d00e39bf1692
Yara Tercero <yara.tercero@elastic.co>
2021-10-29 07:25:07 -0700

    updating per feedback

--- packages/kbn-securitysolution-io-ts-list-types/src/common/exception_export_details/index.ts
+++ packages/kbn-securitysolution-io-ts-list-types/src/common/exception_export_details/index.ts
@@ -9,26 +9,26 @@
 import * as t from 'io-ts';
 import { NonEmptyString } from '@kbn/securitysolution-io-ts-types';
 
-export const exportExceptionDetails = t.exact(
-  t.type({
-    exported_exception_list_count: t.number,
-    exported_exception_list_item_count: t.number,
-    missing_exception_list_item_count: t.number,
-    missing_exception_list_items: t.array(
-      t.exact(
-        t.type({
-          item_id: NonEmptyString,
-        })
-      )
-    ),
-    missing_exception_lists: t.array(
-      t.exact(
-        t.type({
-          list_id: NonEmptyString,
-        })
-      )
-    ),
-    missing_exception_lists_count: t.number,
-  })
-);
-export type ExportExceptionDetails = t.TypeOf<typeof exportExceptionDetails>;
+export const exportExceptionDetails = {
+  exported_exception_list_count: t.number,
+  exported_exception_list_item_count: t.number,
+  missing_exception_list_item_count: t.number,
+  missing_exception_list_items: t.array(
+    t.exact(
+      t.type({
+        item_id: NonEmptyString,
+      })
+    )
+  ),
+  missing_exception_lists: t.array(
+    t.exact(
+      t.type({
+        list_id: NonEmptyString,
+      })
+    )
+  ),
+  missing_exception_lists_count: t.number,
+};
+
+export const exportExceptionDetailsSchema = t.exact(t.type(exportExceptionDetails));
+export type ExportExceptionDetails = t.TypeOf<typeof exportExceptionDetailsSchema>;
