1d618bea6c03c53b5f44484631b64b0b0ba8402c
Stacey Gammon <gammon@elastic.co>
2021-11-01 12:36:16 -0400

    Remove unused public APIs in share plugin

--- src/plugins/share/public/url_generators/url_generator_service.test.ts
+++ src/plugins/share/public/url_generators/url_generator_service.test.ts
@@ -32,9 +32,6 @@ test('Registering and retrieving a generator', async () => {
       "migrate": [Function],
     }
   `);
-  await expect(generator.migrate({})).rejects.toEqual(
-    new Error('You cannot call migrate on a non-deprecated generator.')
-  );
   expect(await generator.createUrl({})).toBe('myurl');
 
   const retrievedGenerator = start.getUrlGenerator('TEST_GENERATOR');
@@ -46,9 +43,6 @@ test('Registering and retrieving a generator', async () => {
       "migrate": [Function],
     }
   `);
-  await expect(generator.migrate({})).rejects.toEqual(
-    new Error('You cannot call migrate on a non-deprecated generator.')
-  );
   expect(await generator.createUrl({})).toBe('myurl');
 });
 
@@ -124,6 +118,5 @@ test('Generator returns migrated url', async () => {
 
   const generator = start.getUrlGenerator('v1');
   expect(generator.isDeprecated).toBe(true);
-  expect(await generator.migrate({ bar: 'hi' })).toEqual({ id: 'v2', state: { foo: 'hi' } });
   expect(await generator.createUrl({ bar: 'hi' })).toEqual('www.hi.com');
 });
