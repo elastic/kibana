9950081fc509ff7d73fac20022d131569a68e30a
Tyler Smalley <tyler.smalley@elastic.co>
2021-11-01 09:03:05 -0700

    Disable coverage
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/steps/test/jest_parallel.sh
+++ .buildkite/scripts/steps/test/jest_parallel.sh
@@ -16,7 +16,7 @@ find src x-pack packages -name jest.config.js -not -path "*/__fixtures__/*" | so
     echo "--- $ node scripts/jest --config $config"
     # TODO: sort by list of test files identified by --listFiles
     # Also allows us to ignore configs with no tests so they can still exist.
-    node --max-old-space-size=5632 ./node_modules/.bin/jest --config $config --runInBand
+    node --max-old-space-size=5632 ./node_modules/.bin/jest --config=$config --runInBand --coverage=false
 
     if [ $? -ne 0 ]; then
       exitCode=10
