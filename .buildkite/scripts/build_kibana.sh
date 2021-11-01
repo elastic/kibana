67af48dfef0086f460e598dc8d68874825cf2548
Jonathan Budzenski <jon@budzenski.me>
2021-11-01 16:23:02 -0500

    [build] Add download cloud dependencies task
    This adds a task to download filebeat and metricbeat for use in building
    our cloud image.  Previously, these were run using local artifacts added
    by the release manager.  As we transition towards building our own releasable
    artifacts, we need to be able to fetch these dependencies at build time.
    
    This includes argument changes to the build command:
    
    - Docker cloud images are built by default, to skip add
    `--skip-docker-cloud`.  `--docker-cloud` has been removed to be
    consistent with other arguments.
    - Artifacts are downloaded by default, to use local artifacts add
    `--skip-cloud-dependencies-download`

--- .buildkite/scripts/build_kibana.sh
+++ .buildkite/scripts/build_kibana.sh
@@ -11,6 +11,17 @@ else
   node scripts/build
 fi
 
+echo "--- Build Kibana Cloud Distribution"
+node scripts/build \
+  --skip-initialize \
+  --skip-generic-folders \
+  --skip-platform-folders \
+  --skip-archives \
+  --docker-images \
+  --skip-docker-ubi \
+  --skip-docker-centos \
+  --skip-docker-contexts
+
 echo "--- Archive Kibana Distribution"
 linuxBuild="$(find "$KIBANA_DIR/target" -name 'kibana-*-linux-x86_64.tar.gz')"
 installDir="$KIBANA_DIR/install/kibana"
