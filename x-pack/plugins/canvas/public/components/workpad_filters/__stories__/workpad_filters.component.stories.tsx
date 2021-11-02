73e72e33688ee3e2d8fd3c174c951284c226236e
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-11-02 12:53:41 +0200

    WorkpadFiltersComponent storybook and snapshots added.

---
+++ x-pack/plugins/canvas/public/components/workpad_filters/__stories__/workpad_filters.component.stories.tsx
@@ -0,0 +1,66 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import moment from 'moment';
+import { WorkpadFilters } from '../workpad_filters.component';
+import { FiltersGroup as FiltersGroupType } from '../types';
+import { prepareFilterViews } from './utils';
+import { Filter } from '../../../../types';
+
+prepareFilterViews();
+
+const filters: Filter[] = [
+  { type: 'exactly', column: 'project', value: 'kibana', filterGroup: 'Group 1' },
+  {
+    type: 'time',
+    column: '@timestamp',
+    value: { from: moment('1.01.2021 8:15').format(), to: moment('2.01.2021 17:22').format() },
+    filterGroup: 'Group 1',
+  },
+  { type: 'exactly', column: 'country', value: 'US', filterGroup: 'Group 2' },
+  {
+    type: 'time',
+    column: 'time',
+    value: { from: moment('05.21.2021 10:50').format(), to: moment('05.22.2021 4:40').format() },
+    filterGroup: 'Group 2',
+  },
+];
+
+const filtersGroups: FiltersGroupType[] = [
+  {
+    name: filters[0].filterGroup,
+    filters: [filters[0], filters[1]],
+  },
+  {
+    name: filters[2].filterGroup,
+    filters: [filters[2], filters[3]],
+  },
+];
+
+storiesOf('components/WorkpadFilters/WorkpadFiltersComponent', module)
+  .addDecorator((story) => (
+    <div>
+      <div className="canvasLayout__sidebar">
+        <div style={{ width: '100%' }}>{story()}</div>
+      </div>
+    </div>
+  ))
+  .add('default', () => (
+    <WorkpadFilters filtersGroups={filtersGroups} onGroupByChange={action('onGroupByChange')} />
+  ))
+  .add('Filters groups without name and filters', () => (
+    <WorkpadFilters
+      filtersGroups={[{ name: null, filters: [] }]}
+      onGroupByChange={action('onGroupByChange')}
+    />
+  ))
+  .add('Empty filters groups', () => (
+    <WorkpadFilters filtersGroups={[]} onGroupByChange={action('onGroupByChange')} />
+  ));
