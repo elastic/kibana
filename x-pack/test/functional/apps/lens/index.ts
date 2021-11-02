9e6ba79d12ff6f2ada490bf7bc836b7040d63050
Marta Bondyra <marta.bondyra@gmail.com>
2021-11-02 16:21:32 +0100

    Update index.ts

--- x-pack/test/functional/apps/lens/index.ts
+++ x-pack/test/functional/apps/lens/index.ts
@@ -33,17 +33,13 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
     });
 
     describe('', function () {
-      this.tags(['ciGroup3', 'skipFirefox']);
-      loadTestFile(require.resolve('./smokescreen'));
-      loadTestFile(require.resolve('./persistent_context'));
-    });
-
-    describe('', function () {
       this.tags(['ciGroup4', 'skipFirefox']);
+      loadTestFile(require.resolve('./smokescreen'));
       loadTestFile(require.resolve('./add_to_dashboard'));
       loadTestFile(require.resolve('./table'));
       loadTestFile(require.resolve('./runtime_fields'));
       loadTestFile(require.resolve('./dashboard'));
+      loadTestFile(require.resolve('./persistent_context'));
       loadTestFile(require.resolve('./colors'));
       loadTestFile(require.resolve('./chart_data'));
       loadTestFile(require.resolve('./time_shift'));
