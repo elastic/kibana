830fef4d23370db4b357ab24d63bc0fdb2b73cca
Byron Hulcher <byronhulcher@gmail.com>
2021-11-02 11:32:40 -0400

    [App Search] Update API namespace and routes for Search Relevance Suggestions/Adaptive Relevance (#116994)
    # Conflicts:
    #	x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/components/suggestions_logic.tsx
    #	x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/views/curation_suggestion/curation_suggestion_logic.ts
    #	x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/views/curations_settings/curations_settings_logic.ts

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/curation/curation_logic.ts
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/curations/curation/curation_logic.ts
@@ -194,15 +194,18 @@ export const CurationLogic = kea<MakeLogicType<CurationValues, CurationActions, 
       const { engineName } = EngineLogic.values;
 
       try {
-        await http.put(`/internal/app_search/engines/${engineName}/search_relevance_suggestions`, {
-          body: JSON.stringify([
-            {
-              query: values.activeQuery,
-              type: 'curation',
-              status: 'applied',
-            },
-          ]),
-        });
+        await http.put(
+          `/internal/app_search/engines/${engineName}/adaptive_relevance/suggestions`,
+          {
+            body: JSON.stringify([
+              {
+                query: values.activeQuery,
+                type: 'curation',
+                status: 'applied',
+              },
+            ]),
+          }
+        );
         actions.loadCuration();
       } catch (e) {
         flashAPIErrors(e);
