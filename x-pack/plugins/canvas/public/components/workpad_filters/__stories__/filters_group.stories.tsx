616d25a692bfc47475874dcb75b1bf0f2e3cccfd
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-11-02 15:15:59 +0200

    WorkpadFilters redux storybook added.

--- x-pack/plugins/canvas/public/components/workpad_filters/__stories__/filters_group.stories.tsx
+++ x-pack/plugins/canvas/public/components/workpad_filters/__stories__/filters_group.stories.tsx
@@ -14,6 +14,8 @@ import { prepareFilterViews } from './utils';
 
 prepareFilterViews();
 
+const timeFormat = 'MM.dd.YYYY HH:mm';
+
 const filtersGroup: FiltersGroupType = {
   name: 'Group 1',
   filters: [
@@ -21,14 +23,20 @@ const filtersGroup: FiltersGroupType = {
     {
       type: 'time',
       column: '@timestamp',
-      value: { from: moment('1.01.2021 8:15').format(), to: moment('2.01.2021 17:22').format() },
+      value: {
+        from: moment('1.01.2021 8:15', timeFormat).format(),
+        to: moment('2.01.2021 17:22', timeFormat).format(),
+      },
       filterGroup: 'Group 1',
     },
     { type: 'exactly', column: 'country', value: 'US', filterGroup: 'Group 1' },
     {
       type: 'time',
       column: 'time',
-      value: { from: moment('05.21.2021 10:50').format(), to: moment('05.22.2021 4:40').format() },
+      value: {
+        from: moment('05.21.2021 10:50', timeFormat).format(),
+        to: moment('05.22.2021 4:40', timeFormat).format(),
+      },
       filterGroup: 'Group 1',
     },
   ],
--- x-pack/plugins/canvas/public/components/workpad_filters/__stories__/workpad_filters.component.stories.tsx
+++ x-pack/plugins/canvas/public/components/workpad_filters/__stories__/workpad_filters.component.stories.tsx
@@ -16,19 +16,27 @@ import { Filter } from '../../../../types';
 
 prepareFilterViews();
 
+const timeFormat = 'MM.dd.YYYY HH:mm';
+
 const filters: Filter[] = [
   { type: 'exactly', column: 'project', value: 'kibana', filterGroup: 'Group 1' },
   {
     type: 'time',
     column: '@timestamp',
-    value: { from: moment('1.01.2021 8:15').format(), to: moment('2.01.2021 17:22').format() },
+    value: {
+      from: moment('1.01.2021 8:15', timeFormat).format(),
+      to: moment('2.01.2021 17:22', timeFormat).format(),
+    },
     filterGroup: 'Group 1',
   },
   { type: 'exactly', column: 'country', value: 'US', filterGroup: 'Group 2' },
   {
     type: 'time',
     column: 'time',
-    value: { from: moment('05.21.2021 10:50').format(), to: moment('05.22.2021 4:40').format() },
+    value: {
+      from: moment('05.21.2021 10:50', timeFormat).format(),
+      to: moment('05.22.2021 4:40', timeFormat).format(),
+    },
     filterGroup: 'Group 2',
   },
 ];
