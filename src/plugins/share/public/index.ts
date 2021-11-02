dac5e557d91fe780f2064a786696f1b836edf354
Stacey Gammon <gammon@elastic.co>
2021-11-02 08:13:20 -0400

    remove unused public code from share plugin (#116905)

--- src/plugins/share/public/index.ts
+++ src/plugins/share/public/index.ts
@@ -37,7 +37,6 @@ export { useLocatorUrl } from '../common/url_service/locators/use_locator_url';
 
 import { SharePlugin } from './plugin';
 
-export { KibanaURL } from './kibana_url';
 export { downloadMultipleAs, downloadFileAs } from './lib/download_as';
 export type { DownloadableContent } from './lib/download_as';
 
