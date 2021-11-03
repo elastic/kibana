deded1f1325ccc976f98a6880fc3df62e06ac153
Faisal Kanout <faisal@kanout.com>
2021-11-03 16:39:16 +0100

    Add localStorage flag to ack the experimental message

--- x-pack/plugins/observability/public/pages/alerts/alerts_disclaimer.tsx
+++ x-pack/plugins/observability/public/pages/alerts/alerts_disclaimer.tsx
@@ -5,42 +5,56 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { EuiLink, EuiCallOut, EuiButton, EuiSpacer } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
 
+const LOCAL_STORAGE_KEY_MSG_ACK = 'xpack.observability.alert.ack.experimental.message';
+
 export function AlertsDisclaimer() {
-  const dismissMessage = () => {};
+  const [experimentalMsgAck, setExperimentalMsgAck] = useState(false);
+
+  const dismissMessage = () => {
+    setExperimentalMsgAck(true);
+    localStorage.setItem(LOCAL_STORAGE_KEY_MSG_ACK, 'true');
+  };
+
+  useEffect(() => {
+    const isExperimentalMsgAck = localStorage.getItem(LOCAL_STORAGE_KEY_MSG_ACK);
+    setExperimentalMsgAck(isExperimentalMsgAck === 'true');
+  }, []);
 
   return (
-    <EuiCallOut
-      title={i18n.translate('xpack.observability.alertsDisclaimerTitle', {
-        defaultMessage: 'Alert history is currently an experimental feature within observability',
-      })}
-      color="warning"
-      iconType="beaker"
-    >
-      <FormattedMessage
-        id="xpack.observability.alertsDisclaimerText"
-        defaultMessage="This functionality may change or be removed completely in a future release. We value your {feedback} as we work to add new capabilities. "
-        values={{
-          feedback: (
-            <EuiLink href="https://discuss.elastic.co/c/observability/82" target="_blank">
-              {i18n.translate('xpack.observability.alertsDisclaimerLinkText', {
-                defaultMessage: 'feedback',
-              })}
-            </EuiLink>
-          ),
-        }}
-      />
-
-      <EuiSpacer size="l" />
-      <EuiButton fill color="warning" onClick={dismissMessage}>
-        {i18n.translate('xpack.observability.alertsDisclaimerDismissMessage', {
-          defaultMessage: 'Dismiss message',
+    !experimentalMsgAck && (
+      <EuiCallOut
+        title={i18n.translate('xpack.observability.alertsDisclaimerTitle', {
+          defaultMessage: 'Alert history is currently an experimental feature within observability',
         })}
-      </EuiButton>
-    </EuiCallOut>
+        color="warning"
+        iconType="beaker"
+      >
+        <FormattedMessage
+          id="xpack.observability.alertsDisclaimerText"
+          defaultMessage="This functionality may change or be removed completely in a future release. We value your {feedback} as we work to add new capabilities. "
+          values={{
+            feedback: (
+              <EuiLink href="https://discuss.elastic.co/c/observability/82" target="_blank">
+                {i18n.translate('xpack.observability.alertsDisclaimerLinkText', {
+                  defaultMessage: 'feedback',
+                })}
+              </EuiLink>
+            ),
+          }}
+        />
+
+        <EuiSpacer size="l" />
+        <EuiButton fill color="warning" onClick={dismissMessage} tabIndex={0}>
+          {i18n.translate('xpack.observability.alertsDisclaimerDismissMessage', {
+            defaultMessage: 'Dismiss message',
+          })}
+        </EuiButton>
+      </EuiCallOut>
+    )
   );
 }
