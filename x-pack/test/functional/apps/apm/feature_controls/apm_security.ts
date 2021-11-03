c8532860c2767e38b00035265ffd91f7d51130d9
Tre <wayne.seymour@elastic.co>
2021-11-03 11:44:15 -0400

    [QA][X Pack] Swap es archiver for kbn archiver - APM specs (#102102)
    Drop all empty kibana calls
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/functional/apps/apm/feature_controls/apm_security.ts
+++ x-pack/test/functional/apps/apm/feature_controls/apm_security.ts
@@ -9,7 +9,6 @@ import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
-  const esArchiver = getService('esArchiver');
   const security = getService('security');
   const PageObjects = getPageObjects(['common', 'error', 'security']);
   const testSubjects = getService('testSubjects');
@@ -18,7 +17,6 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
 
   describe('security', () => {
     before(async () => {
-      await esArchiver.load('x-pack/test/functional/es_archives/empty_kibana');
       // ensure we're logged out so we can login as the appropriate users
       await PageObjects.security.forceLogout();
     });
