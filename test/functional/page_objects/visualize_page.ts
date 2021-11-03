004e0b6f0a4a0d158f050cc3cb767774a1a6e692
Joe Reuter <johannes.reuter@elastic.co>
2021-11-03 11:04:48 +0100

    retry visualize link navigation (#116582)

--- test/functional/page_objects/visualize_page.ts
+++ test/functional/page_objects/visualize_page.ts
@@ -315,7 +315,10 @@ export class VisualizePageObject extends FtrService {
 
   public async openSavedVisualization(vizName: string) {
     const dataTestSubj = `visListingTitleLink-${vizName.split(' ').join('-')}`;
-    await this.testSubjects.click(dataTestSubj, 20000);
+    await this.retry.try(async () => {
+      await this.testSubjects.click(dataTestSubj, 20000);
+      await this.notOnLandingPageOrFail();
+    });
     await this.header.waitUntilLoadingHasFinished();
   }
 
@@ -335,6 +338,11 @@ export class VisualizePageObject extends FtrService {
   public async onLandingPage() {
     this.log.debug(`VisualizePage.onLandingPage`);
     return await this.testSubjects.exists('visualizationLandingPage');
+  }
+
+  public async notOnLandingPageOrFail() {
+    this.log.debug(`VisualizePage.notOnLandingPageOrFail`);
+    return await this.testSubjects.missingOrFail('visualizationLandingPage');
   }
 
   public async gotoLandingPage() {
