382b22978a00197ecb22c0268833ac7b0f680ce7
Esteban Beltran <esteban.beltran@elastic.co>
2021-11-03 10:50:48 +0100

    Add query client provider to routes

--- x-pack/plugins/security_solution/public/management/routes.tsx
+++ x-pack/plugins/security_solution/public/management/routes.tsx
@@ -6,17 +6,21 @@
  */
 
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { MANAGEMENT_PATH } from '../../common/constants';
 import { ManagementContainer } from './pages';
 import { SecuritySubPluginRoutes } from '../app/types';
 import { CurrentLicense } from '../common/components/current_license';
+const queryClient = new QueryClient();
 
 /**
  * Returns the React Router Routes for the management area
  */
 const ManagementRoutes = () => (
   <CurrentLicense>
-    <ManagementContainer />
+    <QueryClientProvider client={queryClient}>
+      <ManagementContainer />
+    </QueryClientProvider>
   </CurrentLicense>
 );
 
