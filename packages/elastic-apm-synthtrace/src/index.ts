e6f35c7395bdab48b1933f343b1bb6a652f3ac97
Søren Louv-Jansen <soren.louv@elastic.co>
2021-11-03 11:09:38 +0100

    [APM] Use Synthtrace in Cypress tests (#116948) (#117262)
    Co-authored-by: Søren Louv-Jansen <sorenlouv@gmail.com>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    
    Co-authored-by: Dario Gieselaar <dario.gieselaar@elastic.co>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- packages/elastic-apm-synthtrace/src/index.ts
+++ packages/elastic-apm-synthtrace/src/index.ts
@@ -7,10 +7,12 @@
  */
 
 export { service } from './lib/service';
+export { browser } from './lib/browser';
 export { timerange } from './lib/timerange';
 export { getTransactionMetrics } from './lib/utils/get_transaction_metrics';
 export { getSpanDestinationMetrics } from './lib/utils/get_span_destination_metrics';
 export { getObserverDefaults } from './lib/defaults/get_observer_defaults';
+export { getChromeUserAgentDefaults } from './lib/defaults/get_chrome_user_agent_defaults';
 export { toElasticsearchOutput } from './lib/output/to_elasticsearch_output';
 export { getBreakdownMetrics } from './lib/utils/get_breakdown_metrics';
 export { cleanWriteTargets } from './lib/utils/clean_write_targets';
