98c57063f793b6cf30a028697d3233177be7aabc
Spencer <email@spalger.com>
2021-11-03 23:58:42 -0600

    [7.16] [eslint] enable type-specific lint rules (#114184) (#117451)
    * [eslint] enable type-specific lint rules
    
    (cherry picked from commit e824fdc2c8e418b927c5b6b53c1de6b548cca2c6)
    
    * autofix all violations
    
    Co-authored-by: spalger <spalger@users.noreply.github.com>

--- x-pack/plugins/fleet/server/index.ts
+++ x-pack/plugins/fleet/server/index.ts
@@ -18,18 +18,18 @@ import {
 import { FleetPlugin } from './plugin';
 
 export { default as apm } from 'elastic-apm-node';
-export {
+export type {
   AgentService,
   ESIndexPatternService,
-  getRegistryUrl,
   PackageService,
   AgentPolicyServiceInterface,
   ArtifactsClientInterface,
   Artifact,
   ListArtifactsProps,
 } from './services';
+export { getRegistryUrl } from './services';
 
-export { FleetSetupContract, FleetSetupDeps, FleetStartContract } from './plugin';
+export type { FleetSetupContract, FleetSetupDeps, FleetStartContract } from './plugin';
 export type {
   ExternalCallback,
   PutPackagePolicyUpdateCallback,
@@ -127,7 +127,7 @@ export const config: PluginConfigDescriptor = {
 
 export type FleetConfigType = TypeOf<typeof config.schema>;
 
-export { PackagePolicyServiceInterface } from './services/package_policy';
+export type { PackagePolicyServiceInterface } from './services/package_policy';
 
 export { relativeDownloadUrlFromArtifact } from './services/artifacts/mappings';
 
