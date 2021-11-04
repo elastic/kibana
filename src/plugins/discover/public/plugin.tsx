cc3692988b17d3f18b5d8859c8ae5c20ca570cc9
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2021-11-04 19:11:57 +0100

    Update plugin.tsx

--- src/plugins/discover/public/plugin.tsx
+++ src/plugins/discover/public/plugin.tsx
@@ -62,7 +62,7 @@ import { DeferredSpinner } from './components';
 import { ViewSavedSearchAction } from './embeddable/view_saved_search_action';
 import type { SpacesPluginStart } from '../../../../x-pack/plugins/spaces/public';
 import { FieldFormatsStart } from '../../field_formats/public';
-import { injectTruncateStyles } from './application/helpers/truncate_styles';
+import { injectTruncateStyles } from './utils/truncate_styles';
 import { TRUNCATE_MAX_HEIGHT } from '../common';
 
 declare module '../../share/public' {
