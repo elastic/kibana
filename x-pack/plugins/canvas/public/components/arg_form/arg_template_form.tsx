8c50d62f2f1a94dcddc8306a3134a9104660c48a
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-11-05 18:13:27 +0200

    Remove ref on unmount to avoid leaks.

--- x-pack/plugins/canvas/public/components/arg_form/arg_template_form.tsx
+++ x-pack/plugins/canvas/public/components/arg_form/arg_template_form.tsx
@@ -7,6 +7,7 @@
 
 import React, { useState, useEffect, useCallback, useRef, RefObject } from 'react';
 import usePrevious from 'react-use/lib/usePrevious';
+import useEffectOnce from 'react-use/lib/useEffectOnce';
 import { RenderToDom } from '../render_to_dom';
 import { ExpressionFormHandlers } from '../../../common/lib/expression_form_handlers';
 import { UpdatePropsRef } from '../../../types/arguments';
@@ -50,6 +51,10 @@ export const ArgTemplateForm: React.FunctionComponent<ArgTemplateFormProps> = ({
   const mountedArgumentRef = useRef<UpdatePropsRef>();
 
   const domNodeRef = useRef<HTMLElement>();
+
+  useEffectOnce(() => () => {
+    mountedArgumentRef.current = undefined;
+  });
 
   const renderTemplate = useCallback(
     (domNode) =>
