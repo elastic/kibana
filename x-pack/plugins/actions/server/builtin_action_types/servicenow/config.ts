e556127731dc5cf68b313aa7aba2b5f7801015cd
Christos Nasikas <christos.nasikas@elastic.co>
2021-11-05 12:44:45 +0200

    [Connectors][ServiceNow] Remove SN flags (#117511)

--- x-pack/plugins/actions/server/builtin_action_types/servicenow/config.ts
+++ x-pack/plugins/actions/server/builtin_action_types/servicenow/config.ts
@@ -5,11 +5,6 @@
  * 2.0.
  */
 
-import {
-  ENABLE_ITOM,
-  ENABLE_NEW_SN_ITSM_CONNECTOR,
-  ENABLE_NEW_SN_SIR_CONNECTOR,
-} from '../../constants/connectors';
 import { SNProductsConfig } from './types';
 
 export const serviceNowITSMTable = 'incident';
@@ -24,21 +19,21 @@ export const snExternalServiceConfig: SNProductsConfig = {
     importSetTable: 'x_elas2_inc_int_elastic_incident',
     appScope: 'x_elas2_inc_int',
     table: 'incident',
-    useImportAPI: ENABLE_NEW_SN_ITSM_CONNECTOR,
+    useImportAPI: true,
     commentFieldKey: 'work_notes',
   },
   '.servicenow-sir': {
     importSetTable: 'x_elas2_sir_int_elastic_si_incident',
     appScope: 'x_elas2_sir_int',
     table: 'sn_si_incident',
-    useImportAPI: ENABLE_NEW_SN_SIR_CONNECTOR,
+    useImportAPI: true,
     commentFieldKey: 'work_notes',
   },
   '.servicenow-itom': {
     importSetTable: 'x_elas2_inc_int_elastic_incident',
     appScope: 'x_elas2_inc_int',
     table: 'em_event',
-    useImportAPI: ENABLE_ITOM,
+    useImportAPI: false,
     commentFieldKey: 'work_notes',
   },
 };
