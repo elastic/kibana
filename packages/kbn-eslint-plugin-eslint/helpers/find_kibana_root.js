e068350f793236a40d09c66ba8d6bb8937f25967
Esteban Beltran <esteban.beltran@elastic.co>
2021-11-08 10:11:31 +0100

    remove console log

--- packages/kbn-eslint-plugin-eslint/helpers/find_kibana_root.js
+++ packages/kbn-eslint-plugin-eslint/helpers/find_kibana_root.js
@@ -29,7 +29,6 @@ module.exports = function findKibanaRoot() {
     return maybeKibanaRoot;
   }
 
-  console.log(process.argv[1]);
   // process.argv[1] would be the eslint binary, a corrected set editor
   // will use a local eslint inside the repo node_modules and its value
   // should be `ACTUAL_KIBANA_ROOT/node_modules/.bin/eslint`
