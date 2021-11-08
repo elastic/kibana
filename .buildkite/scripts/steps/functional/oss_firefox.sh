a8800af44242dfc2e98a1ef3b8913e273ab2d912
Brian Seeders <brian.seeders@elastic.co>
2021-11-08 16:34:26 -0500

    Split oss firefox across multiple agents

--- .buildkite/scripts/steps/functional/oss_firefox.sh
+++ .buildkite/scripts/steps/functional/oss_firefox.sh
@@ -6,9 +6,11 @@ source .buildkite/scripts/steps/functional/common.sh
 
 echo --- OSS Firefox Smoke Tests
 
+export CI_GROUPS=11
+
 checks-reporter-with-killswitch "Firefox smoke test" \
   node scripts/functional_tests \
     --bail --debug \
     --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
     --include-tag "includeFirefox" \
-    --config test/functional/config.firefox.js
+    --config test/functional/config.firefox.ci.js
