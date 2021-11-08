61e1634006902a47a1c7e3c415bbc3bc13395c3d
Esteban Beltran <esteban.beltran@elastic.co>
2021-11-08 09:30:18 +0100

    Update comments

--- packages/kbn-eslint-plugin-eslint/helpers/find_kibana_root.js
+++ packages/kbn-eslint-plugin-eslint/helpers/find_kibana_root.js
@@ -29,8 +29,10 @@ module.exports = function findKibanaRoot() {
     return maybeKibanaRoot;
   }
 
+  console.log(process.argv[1]);
   // process.argv[1] would be the eslint binary, a corrected set editor
-  // will use a local eslint inside the repo node_modules
+  // will use a local eslint inside the repo node_modules and its value
+  // should be `ACTUAL_KIBANA_ROOT/node_modules/.bin/eslint`
   maybeKibanaRoot = path.resolve(process.argv[1], '../../../');
   if (isKibanaRoot(maybeKibanaRoot)) {
     return maybeKibanaRoot;
