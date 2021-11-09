420317d9f43d589ea5bf9644ae44575627a2d3c3
Hannah Mudge <hannah.wright@elastic.co>
2021-11-09 14:56:30 -0700

    Fixed fullscreen error when pressing backspace.

--- src/plugins/dashboard/public/application/embeddable/dashboard_container.tsx
+++ src/plugins/dashboard/public/application/embeddable/dashboard_container.tsx
@@ -97,6 +97,7 @@ export class DashboardContainer extends Container<InheritedChildInput, Dashboard
 
   private onDestroyControlGroup?: () => void;
   public controlGroup?: ControlGroupContainer;
+  private domNode?: HTMLElement;
 
   public getPanelCount = () => {
     return Object.keys(this.getInput().panels).length;
@@ -254,6 +255,10 @@ export class DashboardContainer extends Container<InheritedChildInput, Dashboard
   }
 
   public render(dom: HTMLElement) {
+    if (this.domNode) {
+      ReactDOM.unmountComponentAtNode(this.domNode);
+    }
+    this.domNode = dom;
     ReactDOM.render(
       <I18nProvider>
         <KibanaContextProvider services={this.services}>
@@ -269,7 +274,8 @@ export class DashboardContainer extends Container<InheritedChildInput, Dashboard
   public destroy() {
     super.destroy();
     this.onDestroyControlGroup?.();
-  }
+    if (this.domNode) ReactDOM.unmountComponentAtNode(this.domNode);
+  } 
 
   protected getInheritedInput(id: string): InheritedChildInput {
     const {
--- src/plugins/dashboard/public/application/lib/diff_dashboard_state.ts
+++ src/plugins/dashboard/public/application/lib/diff_dashboard_state.ts
@@ -41,7 +41,7 @@ export const diffDashboardState = (
   const common = commonDiffFilters<DashboardState>(
     original as unknown as DashboardDiffCommonFilters,
     newState as unknown as DashboardDiffCommonFilters,
-    ['viewMode', 'panels', 'options', 'savedQuery', 'expandedPanelId', 'controlGroupInput'],
+    ['viewMode', 'panels', 'options', 'fullScreenMode', 'savedQuery', 'expandedPanelId', 'controlGroupInput'],
     true
   );
 
@@ -124,7 +124,7 @@ const commonDiff = <T>(
     (key) => !omitKeys.includes(key)
   );
   keys.forEach((key) => {
-    if (key === undefined) return;
+    if (key === undefined) return; 
     if (!_.isEqual(originalObj[key], newObj[key])) {
       (differences as { [key: string]: unknown })[key] = newObj[key];
     }
