9fc04ab564c0f0d44bb81a64c5fae072d03d0c56
Walter Rafelsberger <walter@elastic.co>
2021-11-09 10:26:51 +0100

    [ML] APM Correlations: Fix usage in load balancing/HA setups. (#115145)
    - The way we customized the use of search strategies caused issues with race conditions when multiple Kibana instances were used for load balancing. This PR migrates away from search strategies and uses regular APM API endpoints.
    - The task that manages calling the sequence of queries to run the correlations analysis is now in a custom React hook (useFailedTransactionsCorrelations / useLatencyCorrelations) instead of a task on the Kibana server side. While they show up as new lines/files in the git diff, the code for the hooks is more or less a combination of the previous useSearchStrategy and the server side service files that managed queries and state.
    - The consuming React UI components only needed minimal changes. The above mentioned hooks return the same data structure as the previously used useSearchStrategy. This also means functional UI tests didn't need any changes and should pass as is.
    - API integration tests have been added for the individual new endpoints. The test files that were previously used for the search strategies are still there to simulate a full analysis run, the assertions for the resulting data have the same values, it's just the structure that had to be adapted.
    - Previously all ES queries of the analysis were run sequentially. The new endpoints run ES queries in parallel where possible. Chunking is managed in the hooks on the client side.
    - For now the endpoints use the standard current user's esClient. I tried to use the APM client, but it was missing a wrapper for the fieldCaps method and I ran into a problem when trying to construct a random_score query. Sticking to the esClient allowed to leave most of the functions that run the actual queries unchanged. If possible I'd like to pick this up in a follow up. All the endpoints still use withApmSpan() now though.
    - The previous use of generators was also refactored away, as mentioned above, the queries are now run in parallel.
    Because we might run up to hundreds of similar requests for correlation analysis, we don't want the analysis to fail if just a single query fails like we did in the previous search strategy based task. I created a util splitAllSettledPromises() to handle Promise.allSettled() and split the results and errors to make the handling easier. Better naming suggestions are welcome 😅 . A future improvement could be to not run individual queries but combine them into nested aggs or using msearch. That's out of scope for this PR though.

--- x-pack/plugins/apm/server/lib/latency/get_overall_latency_distribution.ts
+++ x-pack/plugins/apm/server/lib/latency/get_overall_latency_distribution.ts
@@ -14,8 +14,8 @@ import { withApmSpan } from '../../utils/with_apm_span';
 import {
   getHistogramIntervalRequest,
   getHistogramRangeSteps,
-} from '../search_strategies/queries/query_histogram_range_steps';
-import { getTransactionDurationRangesRequest } from '../search_strategies/queries/query_ranges';
+} from '../correlations/queries/query_histogram_range_steps';
+import { getTransactionDurationRangesRequest } from '../correlations/queries/query_ranges';
 
 import { getPercentileThresholdValue } from './get_percentile_threshold_value';
 import type {
@@ -27,9 +27,7 @@ export async function getOverallLatencyDistribution(
   options: OverallLatencyDistributionOptions
 ) {
   return withApmSpan('get_overall_latency_distribution', async () => {
-    const overallLatencyDistribution: OverallLatencyDistributionResponse = {
-      log: [],
-    };
+    const overallLatencyDistribution: OverallLatencyDistributionResponse = {};
 
     const { setup, termFilters, ...rawParams } = options;
     const { apmEventClient } = setup;
---
+++ x-pack/test/apm_api_integration/tests/correlations/field_value_pairs.spec.ts
@@ -0,0 +1,71 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../../common/ftr_provider_context';
+
+export default function ApiTest({ getService }: FtrProviderContext) {
+  const apmApiClient = getService('apmApiClient');
+  const registry = getService('registry');
+
+  const endpoint = 'POST /internal/apm/correlations/field_value_pairs';
+
+  const getOptions = () => ({
+    params: {
+      body: {
+        environment: 'ENVIRONMENT_ALL',
+        start: '2020',
+        end: '2021',
+        kuery: '',
+        fieldCandidates: [
+          'service.version',
+          'service.node.name',
+          'service.framework.version',
+          'service.language.version',
+          'service.runtime.version',
+          'kubernetes.pod.name',
+          'kubernetes.pod.uid',
+          'container.id',
+          'source.ip',
+          'client.ip',
+          'host.ip',
+          'service.environment',
+          'process.args',
+          'http.response.status_code',
+        ],
+      },
+    },
+  });
+
+  registry.when('field value pairs without data', { config: 'trial', archives: [] }, () => {
+    it('handles the empty state', async () => {
+      const response = await apmApiClient.readUser({
+        endpoint,
+        ...getOptions(),
+      });
+
+      expect(response.status).to.be(200);
+      expect(response.body?.fieldValuePairs.length).to.be(0);
+    });
+  });
+
+  registry.when(
+    'field value pairs with data and default args',
+    { config: 'trial', archives: ['8.0.0'] },
+    () => {
+      it('returns field value pairs', async () => {
+        const response = await apmApiClient.readUser({
+          endpoint,
+          ...getOptions(),
+        });
+
+        expect(response.status).to.eql(200);
+        expect(response.body?.fieldValuePairs.length).to.be(124);
+      });
+    }
+  );
+}
