a583793f1f7bc86cd33d67752aa97dadbb55da62
Thom Heymann <190132+thomheymann@users.noreply.github.com>
2021-11-09 12:00:54 +0000

    Fix flaky enter spaces tests (#117510)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/functional/apps/spaces/enter_space.ts
+++ x-pack/test/functional/apps/spaces/enter_space.ts
@@ -14,8 +14,7 @@ export default function enterSpaceFunctonalTests({
   const esArchiver = getService('esArchiver');
   const PageObjects = getPageObjects(['security', 'spaceSelector']);
 
-  // FLAKY: https://github.com/elastic/kibana/issues/100570
-  describe.skip('Enter Space', function () {
+  describe('Enter Space', function () {
     this.tags('includeFirefox');
     before(async () => {
       await esArchiver.load('x-pack/test/functional/es_archives/spaces/enter_space');
