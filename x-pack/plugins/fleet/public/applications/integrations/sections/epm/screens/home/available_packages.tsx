81fb25fb7e3c97ba635a0f570645b20bb05a46af
Thomas Neirynck <thomas@elastic.co>
2021-11-09 14:00:27 -0500

    [Fleet] Show callout when EPR unavailable (#117598)

--- x-pack/plugins/fleet/public/applications/integrations/sections/epm/screens/home/available_packages.tsx
+++ x-pack/plugins/fleet/public/applications/integrations/sections/epm/screens/home/available_packages.tsx
@@ -5,10 +5,12 @@
  * 2.0.
  */
 
+import type { FunctionComponent } from 'react';
 import React, { memo, useMemo, useState } from 'react';
 import { useLocation, useHistory, useParams } from 'react-router-dom';
 import _ from 'lodash';
 import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n/react';
 import {
   EuiHorizontalRule,
   EuiFlexItem,
@@ -16,6 +18,8 @@ import {
   EuiSpacer,
   EuiCard,
   EuiIcon,
+  EuiCallOut,
+  EuiLink,
 } from '@elastic/eui';
 
 import { useStartServices } from '../../../../hooks';
@@ -51,6 +55,76 @@ import type { CategoryFacet } from './category_facets';
 
 import type { CategoryParams } from '.';
 import { getParams, categoryExists, mapToCard } from '.';
+
+const NoEprCallout: FunctionComponent<{ statusCode?: number }> = ({
+  statusCode,
+}: {
+  statusCode?: number;
+}) => {
+  let titleMessage;
+  let descriptionMessage;
+  if (statusCode === 502) {
+    titleMessage = i18n.translate('xpack.fleet.epmList.eprUnavailableBadGatewayCalloutTitle', {
+      defaultMessage:
+        'Kibana cannot reach the Elastic Package Registry, which provides Elastic Agent integrations\n',
+    });
+    descriptionMessage = (
+      <FormattedMessage
+        id="xpack.fleet.epmList.eprUnavailableCallouBdGatewaytTitleMessage"
+        defaultMessage="To view these integrations, configure a  {registryproxy} or host {onpremregistry}."
+        values={{
+          registryproxy: <ProxyLink />,
+          onpremregistry: <OnPremLink />,
+        }}
+      />
+    );
+  } else {
+    titleMessage = i18n.translate('xpack.fleet.epmList.eprUnavailable400500CalloutTitle', {
+      defaultMessage:
+        'Kibana cannot connect to the Elastic Package Registry, which provides Elastic Agent integrations\n',
+    });
+    descriptionMessage = (
+      <FormattedMessage
+        id="xpack.fleet.epmList.eprUnavailableCallout400500TitleMessage"
+        defaultMessage="Ensure the {registryproxy} or {onpremregistry} is configured correctly, or try again later."
+        values={{
+          registryproxy: <ProxyLink />,
+          onpremregistry: <OnPremLink />,
+        }}
+      />
+    );
+  }
+
+  return (
+    <EuiCallOut title={titleMessage} iconType="iInCircle" color={'warning'}>
+      <p>{descriptionMessage}</p>
+    </EuiCallOut>
+  );
+};
+
+function ProxyLink() {
+  const { docLinks } = useStartServices();
+
+  return (
+    <EuiLink href={docLinks.links.fleet.settingsFleetServerProxySettings} target="_blank">
+      {i18n.translate('xpack.fleet.epmList.proxyLinkSnippedText', {
+        defaultMessage: 'proxy server',
+      })}
+    </EuiLink>
+  );
+}
+
+function OnPremLink() {
+  const { docLinks } = useStartServices();
+
+  return (
+    <EuiLink href={docLinks.links.fleet.onPremRegistry} target="_blank">
+      {i18n.translate('xpack.fleet.epmList.onPremLinkSnippetText', {
+        defaultMessage: 'your own registry',
+      })}
+    </EuiLink>
+  );
+}
 
 function getAllCategoriesFromIntegrations(pkg: PackageListItem) {
   if (!doesPackageHaveIntegrations(pkg)) {
@@ -133,10 +207,13 @@ export const AvailablePackages: React.FC = memo(() => {
     history.replace(pagePathGetters.integrations_all({ searchTerm: search })[1]);
   }
 
-  const { data: eprPackages, isLoading: isLoadingAllPackages } = useGetPackages({
+  const {
+    data: eprPackages,
+    isLoading: isLoadingAllPackages,
+    error: eprPackageLoadingError,
+  } = useGetPackages({
     category: '',
   });
-
   const eprIntegrationList = useMemo(
     () => packageListToIntegrationsList(eprPackages?.response || []),
     [eprPackages]
@@ -166,18 +243,23 @@ export const AvailablePackages: React.FC = memo(() => {
     return a.title.localeCompare(b.title);
   });
 
-  const { data: eprCategories, isLoading: isLoadingCategories } = useGetCategories({
+  const {
+    data: eprCategories,
+    isLoading: isLoadingCategories,
+    error: eprCategoryLoadingError,
+  } = useGetCategories({
     include_policy_templates: true,
   });
 
   const categories = useMemo(() => {
-    const eprAndCustomCategories: CategoryFacet[] =
-      isLoadingCategories || !eprCategories
-        ? []
-        : mergeCategoriesAndCount(
-            eprCategories.response as Array<{ id: string; title: string; count: number }>,
-            cards
-          );
+    const eprAndCustomCategories: CategoryFacet[] = isLoadingCategories
+      ? []
+      : mergeCategoriesAndCount(
+          eprCategories
+            ? (eprCategories.response as Array<{ id: string; title: string; count: number }>)
+            : [],
+          cards
+        );
     return [
       {
         ...ALL_CATEGORY,
@@ -281,6 +363,12 @@ export const AvailablePackages: React.FC = memo(() => {
     </>
   );
 
+  let noEprCallout;
+  if (eprPackageLoadingError || eprCategoryLoadingError) {
+    const error = eprPackageLoadingError || eprCategoryLoadingError;
+    noEprCallout = <NoEprCallout statusCode={error?.statusCode} />;
+  }
+
   return (
     <PackageListGrid
       featuredList={featuredList}
@@ -291,6 +379,7 @@ export const AvailablePackages: React.FC = memo(() => {
       setSelectedCategory={setSelectedCategory}
       onSearchChange={setSearchTerm}
       showMissingIntegrationMessage
+      callout={noEprCallout}
     />
   );
 });
