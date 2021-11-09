52f3957992bc90ed674864f53399450fcbb9ec98
Tyler Smalley <tyler.smalley@elastic.co>
2021-11-09 11:25:25 -0800

    [ci] Run Jest tests in parallel (#117188)
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- packages/kbn-cli-dev-mode/src/dev_server.test.ts
+++ packages/kbn-cli-dev-mode/src/dev_server.test.ts
@@ -79,6 +79,7 @@ expect.addSnapshotSerializer(extendedEnvSerializer);
 beforeEach(() => {
   jest.clearAllMocks();
   log.messages.length = 0;
+  process.execArgv = ['--inheritted', '--exec', '--argv'];
   currentProc = undefined;
 });
 
@@ -139,8 +140,9 @@ describe('#run$', () => {
               "isDevCliChild": "true",
             },
             "nodeOptions": Array [
-              "--preserve-symlinks-main",
-              "--preserve-symlinks",
+              "--inheritted",
+              "--exec",
+              "--argv",
             ],
             "stdio": "pipe",
           },
