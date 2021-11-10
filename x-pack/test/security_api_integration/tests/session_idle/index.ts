f7da50bbd88cef2e7fb744580d7645e8ca3f3568
Brian Seeders <brian.seeders@elastic.co>
2021-11-10 10:33:20 -0500

    [CI] Rebalance/split cigroups and speed up overall CI time (#113676)

--- x-pack/test/security_api_integration/tests/session_idle/index.ts
+++ x-pack/test/security_api_integration/tests/session_idle/index.ts
@@ -9,7 +9,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('security APIs - Session Idle', function () {
-    this.tags('ciGroup6');
+    this.tags('ciGroup18');
 
     loadTestFile(require.resolve('./cleanup'));
     loadTestFile(require.resolve('./extension'));
