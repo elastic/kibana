8613ffaf33696833e086fa23324300c669f2c3b5
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-11-10 22:46:42 +0100

    [coverage] increase memory limit to 6gb

--- .buildkite/scripts/steps/code_coverage/jest.sh
+++ .buildkite/scripts/steps/code_coverage/jest.sh
@@ -9,7 +9,7 @@ is_test_execution_step
 .buildkite/scripts/bootstrap.sh
 
 echo '--- Jest with coverage'
-node scripts/jest --ci --maxWorkers=10 --coverage || true
+node --max-old-space-size=6144 scripts/jest --ci --maxWorkers=10 --coverage || true
 
 echo '--- Jest Integration Tests with coverage'
 node --max-old-space-size=5120 scripts/jest_integration  --ci --coverage || true
