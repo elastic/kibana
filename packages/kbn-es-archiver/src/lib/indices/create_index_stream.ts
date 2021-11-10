e026218274fa3792f9ae1386dfc7f0015bc0f1dd
Joe Portner <joseph.portner@elastic.co>
2021-11-10 16:42:05 -0500

    Don't delete all .kibana indices when only adding .kibana_task_manager

--- packages/kbn-es-archiver/src/lib/indices/create_index_stream.ts
+++ packages/kbn-es-archiver/src/lib/indices/create_index_stream.ts
@@ -44,6 +44,7 @@ export function createCreateIndexStream({
   // previous indices are removed so we're starting w/ a clean slate for
   // migrations. This only needs to be done once per archive load operation.
   let kibanaIndexAlreadyDeleted = false;
+  let kibanaTaskManagerIndexAlreadyDeleted = false;
 
   async function handleDoc(stream: Readable, record: DocRecord) {
     if (skipDocsFromIndices.has(record.value.index)) {
@@ -55,7 +56,8 @@ export function createCreateIndexStream({
 
   async function handleIndex(record: DocRecord) {
     const { index, settings, mappings, aliases } = record.value;
-    const isKibana = index.startsWith('.kibana');
+    const isKibanaTaskManager = index.startsWith('.kibana_task_manager');
+    const isKibana = index.startsWith('.kibana') && !isKibanaTaskManager;
 
     if (docsOnly) {
       return;
@@ -64,8 +66,11 @@ export function createCreateIndexStream({
     async function attemptToCreate(attemptNumber = 1) {
       try {
         if (isKibana && !kibanaIndexAlreadyDeleted) {
-          await deleteKibanaIndices({ client, stats, log });
-          kibanaIndexAlreadyDeleted = true;
+          await deleteKibanaIndices({ client, stats, log }); // delete all .kibana* indices
+          kibanaIndexAlreadyDeleted = kibanaTaskManagerIndexAlreadyDeleted = true;
+        } else if (isKibanaTaskManager && !kibanaTaskManagerIndexAlreadyDeleted) {
+          await deleteKibanaIndices({ client, stats, onlyTaskManager: true, log }); // delete only .kibana_task_manager* indices
+          kibanaTaskManagerIndexAlreadyDeleted = true;
         }
 
         await client.indices.create(
