0c39338344ad3035e2b6d17a8f973b25240370eb
Esteban Beltran <esteban.beltran@elastic.co>
2021-11-10 11:16:57 +0100

    Move the query provider up to the security app component

--- x-pack/plugins/security_solution/public/management/routes.tsx
+++ x-pack/plugins/security_solution/public/management/routes.tsx
@@ -6,21 +6,17 @@
  */
 
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { MANAGEMENT_PATH } from '../../common/constants';
 import { ManagementContainer } from './pages';
 import { SecuritySubPluginRoutes } from '../app/types';
 import { CurrentLicense } from '../common/components/current_license';
-const queryClient = new QueryClient();
 
 /**
  * Returns the React Router Routes for the management area
  */
 const ManagementRoutes = () => (
   <CurrentLicense>
-    <QueryClientProvider client={queryClient}>
-      <ManagementContainer />
-    </QueryClientProvider>
+    <ManagementContainer />
   </CurrentLicense>
 );
 
