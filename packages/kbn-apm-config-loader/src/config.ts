de2d385041a0746c2fee58513467da14f4e23211
Tyler Smalley <tyler.smalley@elastic.co>
2021-11-10 21:15:44 -0800

    Use endpoint alias
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- packages/kbn-apm-config-loader/src/config.ts
+++ packages/kbn-apm-config-loader/src/config.ts
@@ -23,7 +23,7 @@ const DEFAULT_CONFIG: AgentConfigOptions = {
 };
 
 const CENTRALIZED_SERVICE_BASE_CONFIG: AgentConfigOptions = {
-  serverUrl: 'https://kibana-cloud-apm-server.elastic.dev',
+  serverUrl: 'https://kibana-cloud-apm.apm.us-east-1.aws.found.io',
 
   // The secretToken below is intended to be hardcoded in this file even though
   // it makes it public. This is not a security/privacy issue. Normally we'd
