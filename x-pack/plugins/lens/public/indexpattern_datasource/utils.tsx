e26f82bd544aeee4c5f4a280433b3394680d31ab
Alexey Antonov <alexwizp@gmail.com>
2021-11-11 14:30:37 +0300

    [Lens] Expose Elasticsearch accuracy warnings to the user (#116632)
    * [Lens] Expose Elasticsearch accuracy warnings to the user
    
    Closes: #94918
    
    * fix comments
    
    * update text
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/lens/public/indexpattern_datasource/utils.tsx
+++ x-pack/plugins/lens/public/indexpattern_datasource/utils.tsx
@@ -5,17 +5,30 @@
  * 2.0.
  */
 
-import { DataType } from '../types';
-import { IndexPattern, IndexPatternLayer, DraggedField } from './types';
+import React from 'react';
+import { FormattedMessage } from '@kbn/i18n/react';
+import type { DocLinksStart } from 'kibana/public';
+import { EuiLink, EuiTextColor } from '@elastic/eui';
+
+import { DatatableColumn } from 'src/plugins/expressions';
+import type { DataType, FramePublicAPI } from '../types';
+import type {
+  IndexPattern,
+  IndexPatternLayer,
+  DraggedField,
+  IndexPatternPrivateState,
+} from './types';
 import type {
   BaseIndexPatternColumn,
   FieldBasedIndexPatternColumn,
   ReferenceBasedIndexPatternColumn,
 } from './operations/definitions/column_types';
+
 import { operationDefinitionMap, IndexPatternColumn } from './operations';
 
 import { getInvalidFieldMessage } from './operations/definitions/helpers';
 import { isQueryValid } from './operations/definitions/filters';
+import { checkColumnForPrecisionError } from '../../../../../src/plugins/data/common';
 
 /**
  * Normalizes the specified operation type. (e.g. document operations
@@ -100,4 +113,61 @@ export function fieldIsInvalid(column: IndexPatternColumn | undefined, indexPatt
     return false;
   }
   return !!getInvalidFieldMessage(column, indexPattern)?.length;
+}
+
+export function getPrecisionErrorWarningMessages(
+  state: IndexPatternPrivateState,
+  { activeData }: FramePublicAPI,
+  docLinks: DocLinksStart
+) {
+  const warningMessages: React.ReactNode[] = [];
+
+  if (state && activeData) {
+    Object.values(activeData)
+      .reduce((acc: DatatableColumn[], { columns }) => [...acc, ...columns], [])
+      .forEach((column) => {
+        if (checkColumnForPrecisionError(column)) {
+          warningMessages.push(
+            <FormattedMessage
+              id="xpack.lens.indexPattern.precisionErrorWarning"
+              defaultMessage="{name} for this visualization may be approximate due to how the data is indexed. Try increasing the number of {topValues} or use {filters} instead of {topValues} for precise results. To learn more about this limit, {link}."
+              values={{
+                name: <EuiTextColor color="accent">{column.name}</EuiTextColor>,
+                topValues: (
+                  <EuiTextColor color="subdued">
+                    <FormattedMessage
+                      id="xpack.lens.indexPattern.precisionErrorWarning.topValues"
+                      defaultMessage="Top values"
+                    />
+                  </EuiTextColor>
+                ),
+                filters: (
+                  <EuiTextColor color="subdued">
+                    <FormattedMessage
+                      id="xpack.lens.indexPattern.precisionErrorWarning.filters"
+                      defaultMessage="Filters"
+                    />
+                  </EuiTextColor>
+                ),
+                link: (
+                  <EuiLink
+                    href={docLinks.links.aggs.terms_doc_count_error}
+                    color="text"
+                    target="_blank"
+                    external={true}
+                  >
+                    <FormattedMessage
+                      defaultMessage="visit the documentation"
+                      id="xpack.lens.indexPattern.precisionErrorWarning.link"
+                    />
+                  </EuiLink>
+                ),
+              }}
+            />
+          );
+        }
+      });
+  }
+
+  return warningMessages;
 }
