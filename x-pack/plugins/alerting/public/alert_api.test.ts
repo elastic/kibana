64821b0bbfabbe4c919718bf415cc94c4aa7eee0
YulNaumenko <jo.naumenko@gmail.com>
2021-11-11 18:13:22 -0800

    fixed test

--- x-pack/plugins/alerting/public/alert_api.test.ts
+++ x-pack/plugins/alerting/public/alert_api.test.ts
@@ -7,15 +7,15 @@
 
 import { AlertType, RecoveredActionGroup } from '../common';
 import { httpServiceMock } from '../../../../src/core/public/mocks';
-import { loadAlert, loadAlertType, loadAlertTypes } from './alert_api';
+import { loadRule, loadRuleTypes } from './alert_api';
 import uuid from 'uuid';
 
 const http = httpServiceMock.createStartContract();
 
 beforeEach(() => jest.resetAllMocks());
 
-describe('loadAlertTypes', () => {
-  test('should call get alert types API', async () => {
+describe('loadRuleTypes', () => {
+  test('should call get rule types API', async () => {
     const resolvedValue: AlertType[] = [
       {
         id: 'test',
@@ -31,63 +31,21 @@ describe('loadAlertTypes', () => {
     ];
     http.get.mockResolvedValueOnce(resolvedValue);
 
-    const result = await loadAlertTypes({ http });
+    const result = await loadRuleTypes({ http });
     expect(result).toEqual(resolvedValue);
     expect(http.get.mock.calls[0]).toMatchInlineSnapshot(`
       Array [
-        "/api/alerts/list_alert_types",
+        "/api/alerting/rule_types",
       ]
     `);
   });
 });
 
-describe('loadAlertType', () => {
-  test('should call get alert types API', async () => {
-    const alertType: AlertType = {
-      id: 'test',
-      name: 'Test',
-      actionVariables: ['var1'],
-      actionGroups: [{ id: 'default', name: 'Default' }],
-      defaultActionGroupId: 'default',
-      minimumLicenseRequired: 'basic',
-      isExportable: true,
-      recoveryActionGroup: RecoveredActionGroup,
-      producer: 'alerts',
-    };
-    http.get.mockResolvedValueOnce([alertType]);
-
-    await loadAlertType({ http, id: alertType.id });
-
-    expect(http.get.mock.calls[0]).toMatchInlineSnapshot(`
-      Array [
-        "/api/alerts/list_alert_types",
-      ]
-    `);
-  });
-
-  test('should find the required alertType', async () => {
-    const alertType: AlertType = {
-      id: 'test-another',
-      name: 'Test Another',
-      actionVariables: [],
-      actionGroups: [{ id: 'default', name: 'Default' }],
-      defaultActionGroupId: 'default',
-      minimumLicenseRequired: 'basic',
-      isExportable: true,
-      recoveryActionGroup: RecoveredActionGroup,
-      producer: 'alerts',
-    };
-    http.get.mockResolvedValueOnce([alertType]);
-
-    expect(await loadAlertType({ http, id: 'test-another' })).toEqual(alertType);
-  });
-});
-
-describe('loadAlert', () => {
+describe('loadRule', () => {
   test('should call get API with base parameters', async () => {
-    const alertId = uuid.v4();
+    const ruleId = uuid.v4();
     const resolvedValue = {
-      id: alertId,
+      id: ruleId,
       name: 'name',
       tags: [],
       enabled: true,
@@ -103,7 +61,7 @@ describe('loadAlert', () => {
     };
     http.get.mockResolvedValueOnce(resolvedValue);
 
-    expect(await loadAlert({ http, alertId })).toEqual(resolvedValue);
-    expect(http.get).toHaveBeenCalledWith(`/api/alerts/alert/${alertId}`);
+    expect(await loadRule({ http, ruleId })).toEqual(resolvedValue);
+    expect(http.get).toHaveBeenCalledWith(`/api/alerting/rule/${ruleId}`);
   });
 });
