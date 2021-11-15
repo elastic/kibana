7358736ad0f56073428ed08bf2b080ec37f7c978
Uladzislau Lasitsa <vlad.lasitsa@gmail.com>
2021-11-15 19:35:07 +0300

    [Lens] Move reporting data- attributes from within the renderer to the embeddable (#116950)
    * Move logic about data-attrs for reporting to embeddable layer
    
    * Fix test
    
    * Fix CI
    
    * Fix test
    
    * Add onRender method so that correctly handle dispatchComplete
    
    * Update src/plugins/expressions/public/react_expression_renderer.tsx
    
    Co-authored-by: Tim Sullivan <tsullivan@users.noreply.github.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Tim Sullivan <tsullivan@users.noreply.github.com>

--- x-pack/plugins/lens/public/heatmap_visualization/chart_component.tsx
+++ x-pack/plugins/lens/public/heatmap_visualization/chart_component.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { FC, useEffect, useMemo, useState } from 'react';
+import React, { FC, useMemo } from 'react';
 import {
   Chart,
   ElementClickListener,
@@ -396,23 +396,8 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = ({
 const MemoizedChart = React.memo(HeatmapComponent);
 
 export function HeatmapChartReportable(props: HeatmapRenderProps) {
-  const [state, setState] = useState({
-    isReady: false,
-  });
-
-  // It takes a cycle for the chart to render. This prevents
-  // reporting from printing a blank chart placeholder.
-  useEffect(() => {
-    setState({ isReady: true });
-  }, [setState]);
-
   return (
-    <VisualizationContainer
-      className="lnsHeatmapExpression__container"
-      isReady={state.isReady}
-      reportTitle={props.args.title}
-      reportDescription={props.args.description}
-    >
+    <VisualizationContainer className="lnsHeatmapExpression__container">
       <MemoizedChart {...props} />
     </VisualizationContainer>
   );
