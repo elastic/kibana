d5e2e78d902c1c78648d9202fd1369476abc79e9
Nathan Reese <reese.nathan@gmail.com>
2021-11-16 14:36:21 -0700

    [maps] restructure layers folder (#118395)
    * [maps] restructure layers folder
    
    * eslint
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/maps/public/classes/layers/wizards/load_layer_wizards.ts
+++ x-pack/plugins/maps/public/classes/layers/wizards/load_layer_wizards.ts
@@ -10,21 +10,24 @@ import { uploadLayerWizardConfig } from './file_upload_wizard';
 import {
   esDocumentsLayerWizardConfig,
   esTopHitsLayerWizardConfig,
-} from '../sources/es_search_source';
-import { clustersLayerWizardConfig, heatmapLayerWizardConfig } from '../sources/es_geo_grid_source';
-import { geoLineLayerWizardConfig } from '../sources/es_geo_line_source';
+} from '../../sources/es_search_source';
+import {
+  clustersLayerWizardConfig,
+  heatmapLayerWizardConfig,
+} from '../../sources/es_geo_grid_source';
+import { geoLineLayerWizardConfig } from '../../sources/es_geo_line_source';
 // @ts-ignore
-import { point2PointLayerWizardConfig } from '../sources/es_pew_pew_source';
+import { point2PointLayerWizardConfig } from '../../sources/es_pew_pew_source';
 // @ts-ignore
-import { emsBoundariesLayerWizardConfig } from '../sources/ems_file_source';
+import { emsBoundariesLayerWizardConfig } from '../../sources/ems_file_source';
 // @ts-ignore
-import { emsBaseMapLayerWizardConfig } from '../sources/ems_tms_source';
+import { emsBaseMapLayerWizardConfig } from '../../sources/ems_tms_source';
 // @ts-ignore
-import { kibanaBasemapLayerWizardConfig } from '../sources/kibana_tilemap_source';
-import { tmsLayerWizardConfig } from '../sources/xyz_tms_source';
+import { kibanaBasemapLayerWizardConfig } from '../../sources/kibana_tilemap_source';
+import { tmsLayerWizardConfig } from '../../sources/xyz_tms_source';
 // @ts-ignore
-import { wmsLayerWizardConfig } from '../sources/wms_source';
-import { mvtVectorSourceWizardConfig } from '../sources/mvt_single_layer_vector_source';
+import { wmsLayerWizardConfig } from '../../sources/wms_source';
+import { mvtVectorSourceWizardConfig } from '../../sources/mvt_single_layer_vector_source';
 import { ObservabilityLayerWizardConfig } from './solution_layers/observability';
 import { SecurityLayerWizardConfig } from './solution_layers/security';
 import { choroplethLayerWizardConfig } from './choropleth_layer_wizard';
