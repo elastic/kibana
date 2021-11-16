0e73c6d7c7757238d61e18f62b096b841ac5e386
Angela Chuang <yi-chun.chuang@elastic.co>
2021-11-16 12:57:17 +0000

    update placeholder text for data view selector

--- x-pack/plugins/security_solution/public/common/components/sourcerer/index.tsx
+++ x-pack/plugins/security_solution/public/common/components/sourcerer/index.tsx
@@ -246,7 +246,7 @@ export const Sourcerer = React.memo<SourcererComponentProps>(({ scope: scopeId }
               fullWidth
               onChange={onChangeSuper}
               options={dataViewSelectOptions}
-              placeholder={i18n.PICK_INDEX_PATTERNS}
+              placeholder={i18n.INDEX_PATTERNS_CHOOSE_DATA_VIEW_LABEL}
               valueOfSelected={dataViewId}
             />
           </StyledFormRow>
