3b89fd10292bffebce593c5fb1909da7f2f54cc2
Timothy Sullivan <tsullivan@elastic.co>
2021-11-17 16:36:17 -0700

    get clips logic

--- x-pack/plugins/reporting/server/lib/screenshots/stitcher.ts
+++ x-pack/plugins/reporting/server/lib/screenshots/stitcher.ts
@@ -15,6 +15,7 @@ interface ScreenshotStitcherConstructorOpts {
 
 export class ScreenshotStitcher {
   private clip: ScreenshotClip;
+  private rowPixelMax = 100;
 
   constructor(opts: ScreenshotStitcherConstructorOpts) {
     this.clip = opts.outputClip;
@@ -22,26 +23,40 @@ export class ScreenshotStitcher {
 
   public getClips$(): Rx.Observable<ScreenshotClip> {
     const clip = this.clip;
+    const max = this.rowPixelMax;
 
-    // Divide the output dimensions proportionally
+    /*
+     * Divide the output dimensions proportionally
+     * 2 columns hardcoded to make re-arrangement easier when downloading
+     * N rows, based on the height
+     *
+     * AB
+     * CD
+     * ...
+     */
+
+    const columns = 2;
+    const rows = Math.ceil(clip.height / this.rowPixelMax);
+
     return Rx.from(
       (function* (): Generator<ScreenshotClip> {
-        const { x, y, width, height } = clip;
-        // AB
-        // CD
+        for (let row = 0; row < rows; ++row) {
+          for (let column = 0; column < columns; ++column) {
+            const dimensions = {
+              x: column % 2 === 0 ? clip.x : clip.x + clip.width / 2, // alternate left and right row
+              y: row * max + clip.y,
+              width: clip.width / 2,
+              height: row === rows - 1 ? clip.height - row * max : max,
+            };
+            console.log({ row, column });
+            console.log(JSON.stringify({ dimensions }));
 
-        // A
-        yield { x, y, width: width / 2, height: height / 2 };
-
-        // B
-        yield { x: x + width / 2, y, width: width / 2, height: height / 2 };
-
-        // C
-        yield { x, y: y + height / 2, width: width / 2, height: height / 2 };
-
-        // D
-        yield { x: x + width / 2, y: y + height / 2, width: width / 2, height: height / 2 };
+            yield dimensions;
+          }
+        }
       })()
     );
   }
+
+  // FIXME add a combine$ method for downloading the rebuilt PNG
 }
