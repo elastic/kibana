778f014460501a892eec495086c30609a6fd279e
Chris Cowan <chris@elastic.co>
2021-11-17 12:40:15 -0700

    Adding error handing to cleanupTargetIndices

--- x-pack/test/rule_registry/common/lib/helpers/cleanup_target_indices.ts
+++ x-pack/test/rule_registry/common/lib/helpers/cleanup_target_indices.ts
@@ -12,8 +12,8 @@ import { getAlertsTargetIndices } from './get_alerts_target_indices';
 
 export const cleanupTargetIndices = async (getService: GetService, user: User, spaceId: string) => {
   const es = getService('es');
-  const { body: targetIndices } = await getAlertsTargetIndices(getService, user, spaceId);
   try {
+    const { body: targetIndices } = await getAlertsTargetIndices(getService, user, spaceId);
     const aliasMap = await es.indices.getAlias({
       name: targetIndices,
       allow_no_indices: true,
@@ -21,8 +21,10 @@ export const cleanupTargetIndices = async (getService: GetService, user: User, s
     });
     const indices = Object.keys(aliasMap);
     expect(indices.length > 0).to.be(true);
-    return es.indices.delete({ index: indices });
-  } catch (err) {
-    // do nothing
+    return es.indices.delete({ index: indices }, { ignore: [404] });
+  } catch (error) {
+    if (error.meta.statusCode !== 404) {
+      throw error;
+    }
   }
 };
