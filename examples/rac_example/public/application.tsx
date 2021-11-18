ee6bff2265e75c38e7a48fb28cd0d90558a7d1ed
mgiota <panagiota.mitsopoulou@elastic.co>
2021-11-18 17:58:10 +0100

    clean up code

--- examples/rac_example/public/application.tsx
+++ examples/rac_example/public/application.tsx
@@ -9,46 +9,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { /* EuiSideNav,*/ EuiPage } from '@elastic/eui';
+import { EuiPage } from '@elastic/eui';
 import { AppMountParameters, CoreStart } from '../../../src/core/public';
 import { AlertsDemoClientStartDeps } from './types';
-import {
-  KibanaContextProvider,
-  /* KibanaPageTemplate, */
-} from '../../../src/plugins/kibana_react/public';
+import { KibanaContextProvider } from '../../../src/plugins/kibana_react/public';
 import { TriggersAndActionsUIPublicPluginStart } from '../../../x-pack/plugins/triggers_actions_ui/public';
 import { CreateRule } from './components/create_rule';
-
-/* function AlertsDemoPageTemplate({ children, ...rest }) {
-  const items = [
-    {
-      name: 'Section1',
-      id: 'home',
-      items: [
-        {
-          name: 'nav item1',
-        },
-        {
-          name: 'nav item2',
-        },
-      ],
-    },
-    {
-      name: 'Section2',
-      id: 'about',
-      items: [
-        {
-          name: 'nav item1',
-        },
-        {
-          name: 'nav item2',
-        },
-      ],
-    },
-  ];
-  const sideNav = <EuiSideNav heading="Sidebar" items={items} />;
-  return <KibanaPageTemplate pageSideBar={sideNav}>{children}</KibanaPageTemplate>;
-}*/
 
 export const renderApp = (
   core: CoreStart,
