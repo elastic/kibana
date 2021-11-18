14f81ae510771ff0e99f2bb2600c136165aca2f0
Frank Hassanabad <frank.hassanabad@elastic.co>
2021-11-17 19:08:43 -0700

    [Security Solutions] Removes plugins/data/public deprecations from cases plugin (#118977)
    ## Summary
    
    This removes all the areas marked as deprecated from `.../src/plugins/data/public` with their `@kbn/es-query` equivalent or it uses the directly exported version from `.../src/plugins/data/public`. Anywhere else this adds the `import type {` where it can to encourage the build system to do more type erasures.
    
    ### Checklist
    - [x] [Unit or functional tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html) were updated or added to match the most common scenarios

--- x-pack/plugins/cases/server/client/attachments/add.ts
+++ x-pack/plugins/cases/server/client/attachments/add.ts
@@ -10,6 +10,7 @@ import { pipe } from 'fp-ts/lib/pipeable';
 import { fold } from 'fp-ts/lib/Either';
 import { identity } from 'fp-ts/lib/function';
 
+import { nodeBuilder } from '@kbn/es-query';
 import {
   SavedObject,
   SavedObjectsClientContract,
@@ -17,7 +18,6 @@ import {
   SavedObjectsUtils,
 } from '../../../../../../src/core/server';
 import { LensServerPluginSetup } from '../../../../lens/server';
-import { nodeBuilder } from '../../../../../../src/plugins/data/common';
 
 import {
   AlertCommentRequestRt,
