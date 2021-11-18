1dc7dc96a3e0e0e2cc2dcda7291a845c2c46232d
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-11-17 22:05:20 -0500

    [ftr/remote] delay chrome restarts, limit attempts (#118945) (#118980)
    Co-authored-by: Spencer <email@spalger.com>

--- test/functional/services/remote/webdriver.ts
+++ test/functional/services/remote/webdriver.ts
@@ -10,7 +10,7 @@ import { resolve } from 'path';
 import Fs from 'fs';
 
 import * as Rx from 'rxjs';
-import { mergeMap, map, takeUntil, catchError } from 'rxjs/operators';
+import { mergeMap, map, takeUntil, catchError, ignoreElements } from 'rxjs/operators';
 import { Lifecycle } from '@kbn/test';
 import { ToolingLog } from '@kbn/dev-utils';
 import chromeDriver from 'chromedriver';
@@ -52,6 +52,8 @@ const chromiumUserPrefs = {
     },
   },
 };
+
+const sleep$ = (ms: number) => Rx.timer(ms).pipe(ignoreElements());
 
 /**
  * Best we can tell WebDriver locks up sometimes when we send too many
@@ -321,6 +323,7 @@ export async function initWebDriver(
     edgePaths = await installDriver();
   }
 
+  let attempt = 1;
   return await Rx.race(
     Rx.timer(2 * MINUTE).pipe(
       map(() => {
@@ -345,8 +348,14 @@ export async function initWebDriver(
       catchError((error, resubscribe) => {
         log.warning('Failure while creating webdriver instance');
         log.warning(error);
-        log.warning('...retrying...');
-        return resubscribe;
+
+        if (attempt > 5) {
+          throw new Error('out of retry attempts');
+        }
+
+        attempt += 1;
+        log.warning('...retrying in 15 seconds...');
+        return Rx.concat(sleep$(15000), resubscribe);
       })
     )
   ).toPromise();
