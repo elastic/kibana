224dc023a73f44255806fdeb247700060d241d5b
Sébastien Loix <sebastien.loix@elastic.co>
2021-11-19 12:24:07 +0000

    Fix TS issue

--- x-pack/test/accessibility/apps/upgrade_assistant.ts
+++ x-pack/test/accessibility/apps/upgrade_assistant.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { IndicesCreateRequest } from '@elastic/elasticsearch/api/types';
+import type { IndicesCreateRequest } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { FtrProviderContext } from '../ftr_provider_context';
 
 const translogSettingsIndexDeprecation: IndicesCreateRequest = {
