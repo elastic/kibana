9f372f3f44f4a00247657a60f6aea2e502aefca1
Mark Hopkin <mark.hopkin@elastic.co>
2021-11-19 15:54:17 +0000

    use resolve when deleting index patterns

--- x-pack/plugins/fleet/server/services/epm/kibana/index_pattern/install.ts
+++ x-pack/plugins/fleet/server/services/epm/kibana/index_pattern/install.ts
@@ -64,15 +64,23 @@ export async function removeUnusedIndexPatterns(savedObjectsClient: SavedObjects
     return [];
   }
 
+  const patternsToDelete = indexPatternTypes.map((indexPatternType) => `${indexPatternType}-*`);
+
+  const { resolved_objects: resolvedObjects } = await savedObjectsClient.bulkResolve(
+    patternsToDelete.map((pattern) => ({ id: pattern, type: INDEX_PATTERN_SAVED_OBJECT_TYPE }))
+  );
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const idsToDelete = resolvedObjects.map(({ saved_object }) => saved_object.id);
+
   return Promise.all(
-    indexPatternTypes.map(async (indexPatternType) => {
-      const pattern = `${indexPatternType}-*`;
+    idsToDelete.map(async (id) => {
       try {
-        logger.debug(`deleting index pattern ${pattern}`);
-        await savedObjectsClient.delete(INDEX_PATTERN_SAVED_OBJECT_TYPE, `${pattern}`);
+        logger.debug(`deleting index pattern ${id}`);
+        await savedObjectsClient.delete(INDEX_PATTERN_SAVED_OBJECT_TYPE, id);
       } catch (err) {
         // index pattern was probably deleted by the user already
-        logger.debug(`Non fatal error encountered deleting index pattern ${pattern} : ${err}`);
+        logger.debug(`Non fatal error encountered deleting index pattern ${id} : ${err}`);
       }
       return;
     })
