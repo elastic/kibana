766e8d1350ec3c8c33de710c32a0294ccddfef11
Lisa Cawley <lcawley@elastic.co>
2021-11-19 13:20:43 -0800

    Update x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/components/empty_state.test.tsx
    Co-authored-by: Scotty Bollinger <scotty.bollinger@elastic.co>

--- x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/components/empty_state.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/documents/components/empty_state.test.tsx
@@ -23,7 +23,7 @@ describe('EmptyState', () => {
 
     expect(wrapper.find('h2').text()).toEqual('Add your first documents');
     expect(wrapper.find(EuiButton).prop('href')).toEqual(
-      expect.stringContaining(`${docLinks.appSearchIndexingDocs}`)
+      expect.stringContaining(docLinks.appSearchIndexingDocs)
     );
   });
 });
