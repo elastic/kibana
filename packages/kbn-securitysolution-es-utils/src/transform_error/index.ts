8699c930126bc8a046cb38abab22d55dda2c08b1
Ashokaditya <am.struktr@gmail.com>
2021-11-19 13:33:53 +0100

    Revert "remove hapi/boom from transformError"
    This reverts commit 90e4b504456f4733c2f01fb9fefd561805f68322.

--- packages/kbn-securitysolution-es-utils/src/transform_error/index.ts
+++ packages/kbn-securitysolution-es-utils/src/transform_error/index.ts
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 
+import Boom from '@hapi/boom';
 import { errors } from '@elastic/elasticsearch';
 import { BadRequestError } from '../bad_request_error';
 
@@ -15,30 +16,37 @@ export interface OutputError {
 }
 
 export const transformError = (err: Error & Partial<errors.ResponseError>): OutputError => {
-  if (err.statusCode != null) {
-    if (err.body != null && err.body.error != null) {
-      return {
-        statusCode: err.statusCode,
-        message: `${err.body.error.type}: ${err.body.error.reason}`,
-      };
-    } else {
-      return {
-        statusCode: err.statusCode,
-        message: err.message,
-      };
-    }
-  } else if (err instanceof BadRequestError) {
-    // allows us to throw request validation errors in the absence of Boom
+  if (Boom.isBoom(err)) {
     return {
-      message: err.message,
-      statusCode: 400,
+      message: err.output.payload.message,
+      statusCode: err.output.statusCode,
     };
   } else {
-    // natively return the err and allow the regular framework
-    // to deal with the error when it is a non Boom
-    return {
-      message: err.message != null ? err.message : '(unknown error message)',
-      statusCode: 500,
-    };
+    if (err.statusCode != null) {
+      if (err.body != null && err.body.error != null) {
+        return {
+          statusCode: err.statusCode,
+          message: `${err.body.error.type}: ${err.body.error.reason}`,
+        };
+      } else {
+        return {
+          statusCode: err.statusCode,
+          message: err.message,
+        };
+      }
+    } else if (err instanceof BadRequestError) {
+      // allows us to throw request validation errors in the absence of Boom
+      return {
+        message: err.message,
+        statusCode: 400,
+      };
+    } else {
+      // natively return the err and allow the regular framework
+      // to deal with the error when it is a non Boom
+      return {
+        message: err.message != null ? err.message : '(unknown error message)',
+        statusCode: 500,
+      };
+    }
   }
 };
