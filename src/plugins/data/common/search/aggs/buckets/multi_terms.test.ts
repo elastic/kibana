7ef32a9bf1ad72a6671d0e4c73918aa90cd4bbcc
Joe Reuter <johannes.reuter@elastic.co>
2021-11-22 11:06:26 +0100

    adjust tests

--- src/plugins/data/common/search/aggs/buckets/multi_terms.test.ts
+++ src/plugins/data/common/search/aggs/buckets/multi_terms.test.ts
@@ -13,80 +13,71 @@ import { BUCKET_TYPES } from './bucket_agg_types';
 import type { IndexPatternField } from '../../..';
 import { IndexPattern } from '../../..';
 
-describe('Terms Agg', () => {
-  describe('order agg editor UI', () => {
-    const getAggConfigs = (params: Record<string, any> = {}) => {
-      const indexPattern = {
-        id: '1234',
-        title: 'logstash-*',
-        fields: [
-          {
-            name: 'field',
-            type: 'string',
-            esTypes: ['string'],
-            aggregatable: true,
-            filterable: true,
-            searchable: true,
-          },
-          {
-            name: 'string_field',
-            type: 'string',
-            esTypes: ['string'],
-            aggregatable: true,
-            filterable: true,
-            searchable: true,
-          },
-          {
-            name: 'empty_number_field',
-            type: 'number',
-            esTypes: ['number'],
-            aggregatable: true,
-            filterable: true,
-            searchable: true,
-          },
-          {
-            name: 'number_field',
-            type: 'number',
-            esTypes: ['number'],
-            aggregatable: true,
-            filterable: true,
-            searchable: true,
-          },
-        ],
-      } as IndexPattern;
-
-      indexPattern.fields.getByName = (name) => ({ name } as unknown as IndexPatternField);
-      indexPattern.fields.filter = () => indexPattern.fields;
-
-      return new AggConfigs(
-        indexPattern,
-        [
-          {
-            id: 'test',
-            params,
-            type: BUCKET_TYPES.TERMS,
-          },
-        ],
-        { typesRegistry: mockAggTypesRegistry() }
-      );
-    };
-
-    test('produces the expected expression ast', () => {
-      const aggConfigs = getAggConfigs({
-        include: {
-          pattern: '404',
-        },
-        exclude: {
-          pattern: '400',
-        },
-        field: {
+describe('Multi Terms Agg', () => {
+  const getAggConfigs = (params: Record<string, any> = {}) => {
+    const indexPattern = {
+      id: '1234',
+      title: 'logstash-*',
+      fields: [
+        {
           name: 'field',
+          type: 'string',
+          esTypes: ['string'],
+          aggregatable: true,
+          filterable: true,
+          searchable: true,
         },
-        orderAgg: {
-          type: 'count',
+        {
+          name: 'string_field',
+          type: 'string',
+          esTypes: ['string'],
+          aggregatable: true,
+          filterable: true,
+          searchable: true,
         },
-      });
-      expect(aggConfigs.aggs[0].toExpressionAst()).toMatchInlineSnapshot(`
+        {
+          name: 'empty_number_field',
+          type: 'number',
+          esTypes: ['number'],
+          aggregatable: true,
+          filterable: true,
+          searchable: true,
+        },
+        {
+          name: 'number_field',
+          type: 'number',
+          esTypes: ['number'],
+          aggregatable: true,
+          filterable: true,
+          searchable: true,
+        },
+      ],
+    } as IndexPattern;
+
+    indexPattern.fields.getByName = (name) => ({ name } as unknown as IndexPatternField);
+    indexPattern.fields.filter = () => indexPattern.fields;
+
+    return new AggConfigs(
+      indexPattern,
+      [
+        {
+          id: 'test',
+          params,
+          type: BUCKET_TYPES.MULTI_TERMS,
+        },
+      ],
+      { typesRegistry: mockAggTypesRegistry() }
+    );
+  };
+
+  test('produces the expected expression ast', () => {
+    const aggConfigs = getAggConfigs({
+      fields: ['field', 'string_field'],
+      orderAgg: {
+        type: 'count',
+      },
+    });
+    expect(aggConfigs.aggs[0].toExpressionAst()).toMatchInlineSnapshot(`
         Object {
           "chain": Array [
             Object {
@@ -94,17 +85,12 @@ describe('Terms Agg', () => {
                 "enabled": Array [
                   true,
                 ],
-                "field": Array [
+                "fields": Array [
                   "field",
+                  "string_field",
                 ],
                 "id": Array [
                   "test",
-                ],
-                "missingBucket": Array [
-                  false,
-                ],
-                "missingBucketLabel": Array [
-                  "Missing",
                 ],
                 "order": Array [
                   "desc",
@@ -148,145 +134,58 @@ describe('Terms Agg', () => {
           "type": "expression",
         }
       `);
-    });
+  });
 
-    test('converts object to string type', () => {
-      const aggConfigs = getAggConfigs({
-        include: {
-          pattern: '404',
-        },
-        exclude: {
-          pattern: '400',
-        },
-        field: {
-          name: 'field',
-        },
-        orderAgg: {
-          type: 'count',
-        },
-      });
-
-      const { [BUCKET_TYPES.TERMS]: params } = aggConfigs.aggs[0].toDsl();
-
-      expect(params.field).toBe('field');
-      expect(params.include).toBe('404');
-      expect(params.exclude).toBe('400');
-    });
-
-    test('accepts string from string field type and writes this value', () => {
-      const aggConfigs = getAggConfigs({
-        include: 'include value',
-        exclude: 'exclude value',
-        field: {
+  test('uses correct bucket path for sorting by median', () => {
+    const indexPattern = {
+      id: '1234',
+      title: 'logstash-*',
+      fields: [
+        {
           name: 'string_field',
           type: 'string',
+          esTypes: ['string'],
+          aggregatable: true,
+          filterable: true,
+          searchable: true,
         },
-        orderAgg: {
-          type: 'count',
-        },
-      });
-
-      const { [BUCKET_TYPES.TERMS]: params } = aggConfigs.aggs[0].toDsl();
-
-      expect(params.field).toBe('string_field');
-      expect(params.include).toBe('include value');
-      expect(params.exclude).toBe('exclude value');
-    });
-
-    test('accepts empty array from number field type and does not write a value', () => {
-      const aggConfigs = getAggConfigs({
-        include: [],
-        exclude: [],
-        field: {
-          name: 'empty_number_field',
-          type: 'number',
-        },
-        orderAgg: {
-          type: 'count',
-        },
-      });
-
-      const { [BUCKET_TYPES.TERMS]: params } = aggConfigs.aggs[0].toDsl();
-
-      expect(params.field).toBe('empty_number_field');
-      expect(params.include).toBe(undefined);
-      expect(params.exclude).toBe(undefined);
-    });
-
-    test('filters array with empty strings from number field type and writes only numbers', () => {
-      const aggConfigs = getAggConfigs({
-        include: [1.1, 2, '', 3.33, ''],
-        exclude: ['', 4, 5.555, '', 6],
-        field: {
+        {
           name: 'number_field',
           type: 'number',
+          esTypes: ['number'],
+          aggregatable: true,
+          filterable: true,
+          searchable: true,
         },
-        orderAgg: {
-          type: 'count',
-        },
-      });
+      ],
+    } as IndexPattern;
 
-      const { [BUCKET_TYPES.TERMS]: params } = aggConfigs.aggs[0].toDsl();
+    indexPattern.fields.getByName = (name) => ({ name } as unknown as IndexPatternField);
+    indexPattern.fields.filter = () => indexPattern.fields;
 
-      expect(params.field).toBe('number_field');
-      expect(params.include).toStrictEqual([1.1, 2, 3.33]);
-      expect(params.exclude).toStrictEqual([4, 5.555, 6]);
-    });
-
-    test('uses correct bucket path for sorting by median', () => {
-      const indexPattern = {
-        id: '1234',
-        title: 'logstash-*',
-        fields: [
-          {
-            name: 'string_field',
-            type: 'string',
-            esTypes: ['string'],
-            aggregatable: true,
-            filterable: true,
-            searchable: true,
-          },
-          {
-            name: 'number_field',
-            type: 'number',
-            esTypes: ['number'],
-            aggregatable: true,
-            filterable: true,
-            searchable: true,
-          },
-        ],
-      } as IndexPattern;
-
-      indexPattern.fields.getByName = (name) => ({ name } as unknown as IndexPatternField);
-      indexPattern.fields.filter = () => indexPattern.fields;
-
-      const aggConfigs = new AggConfigs(
-        indexPattern,
-        [
-          {
-            id: 'test',
-            params: {
-              field: {
-                name: 'string_field',
-                type: 'string',
-              },
-              orderAgg: {
-                type: METRIC_TYPES.MEDIAN,
-                params: {
-                  field: {
-                    name: 'number_field',
-                    type: 'number',
-                  },
+    const aggConfigs = new AggConfigs(
+      indexPattern,
+      [
+        {
+          id: 'test',
+          params: {
+            fields: ['string_field'],
+            orderAgg: {
+              type: METRIC_TYPES.MEDIAN,
+              params: {
+                field: {
+                  name: 'number_field',
+                  type: 'number',
                 },
               },
             },
-            type: BUCKET_TYPES.TERMS,
           },
-        ],
-        { typesRegistry: mockAggTypesRegistry() }
-      );
-      const { [BUCKET_TYPES.TERMS]: params } = aggConfigs.aggs[0].toDsl();
-      expect(params.order).toEqual({ 'test-orderAgg.50': 'desc' });
-    });
+          type: BUCKET_TYPES.MULTI_TERMS,
+        },
+      ],
+      { typesRegistry: mockAggTypesRegistry() }
+    );
+    const { [BUCKET_TYPES.MULTI_TERMS]: params } = aggConfigs.aggs[0].toDsl();
+    expect(params.order).toEqual({ 'test-orderAgg.50': 'desc' });
   });
 });
