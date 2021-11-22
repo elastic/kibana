6ce568c3820ed4dc97487caf2f918e0e80b20bbc
Kristof C <kpac.ja@gmail.com>
2021-11-22 16:20:17 -0600

    [Cases] [104961] Remove conditional preventing additional comment field to be blank (#117901)
    * Remove conditional preventing additional comment field to be blank
    
    * Remove tests for unwanted feature
    
    * Update server to prevent empty string comments
    
    * PR fix
    
    Co-authored-by: Kristof-Pierre Cummings <kristofpierre.cummings@elastic.co>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/swimlane/swimlane_params.test.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/swimlane/swimlane_params.test.tsx
@@ -125,13 +125,5 @@ describe('SwimlaneParamsFields renders', () => {
       expect(comments.simulate('change', changeEvent));
       expect(editAction.mock.calls[0][1].comments.length).toEqual(1);
     });
-
-    test('An empty comment does not trigger editAction', () => {
-      const wrapper = mountWithIntl(<SwimlaneParamsFields {...defaultProps} />);
-      const emptyComment = { target: { value: '' } };
-      const comments = wrapper.find('[data-test-subj="commentsTextArea"] textarea');
-      expect(comments.simulate('change', emptyComment));
-      expect(editAction.mock.calls.length).toEqual(0);
-    });
   });
 });
