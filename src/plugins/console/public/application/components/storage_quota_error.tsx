39c0b9e3cf1732f024c47f21f86bd434507a84d4
Muhammad Ibragimov <muhammad.ibragimov@elastic.co>
2021-11-22 16:32:12 +0500

    Remove EuiSpacer

--- src/plugins/console/public/application/components/storage_quota_error.tsx
+++ src/plugins/console/public/application/components/storage_quota_error.tsx
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiButton, EuiSpacer, EuiButtonEmpty } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiButton, EuiButtonEmpty } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n/react';
 
 interface Props {
@@ -15,28 +15,23 @@ interface Props {
   onDisableSavingToHistory: () => void;
 }
 
-export const StorageQuotaError = ({ onClearHistory, onDisableSavingToHistory }: Props) => {
-  return (
-    <>
-      <EuiSpacer size="s" />
-      <EuiFlexGroup alignItems="center">
-        <EuiFlexItem>
-          <EuiButtonEmpty onClick={onClearHistory}>
-            <FormattedMessage
-              id="console.notification.clearHistory"
-              defaultMessage="Clear history"
-            />
-          </EuiButtonEmpty>
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiButton onClick={onDisableSavingToHistory}>
-            <FormattedMessage
-              id="console.notification.disableSavingToHistory"
-              defaultMessage="Disable saving"
-            />
-          </EuiButton>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    </>
-  );
-};
+export const StorageQuotaError = ({ onClearHistory, onDisableSavingToHistory }: Props) => (
+  <EuiFlexGroup alignItems='center'>
+    <EuiFlexItem>
+      <EuiButtonEmpty onClick={onClearHistory}>
+        <FormattedMessage
+          id='console.notification.clearHistory'
+          defaultMessage='Clear history'
+        />
+      </EuiButtonEmpty>
+    </EuiFlexItem>
+    <EuiFlexItem>
+      <EuiButton onClick={onDisableSavingToHistory}>
+        <FormattedMessage
+          id='console.notification.disableSavingToHistory'
+          defaultMessage='Disable saving'
+        />
+      </EuiButton>
+    </EuiFlexItem>
+  </EuiFlexGroup>
+);
