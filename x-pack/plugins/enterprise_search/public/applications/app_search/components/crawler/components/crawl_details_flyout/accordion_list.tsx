6c5e07aa4fc78d6df267d8b8a32b972b80e5902a
Byron Hulcher <byron.hulcher@elastic.co>
2021-11-24 17:34:55 -0500

    New AccordionList component

---
+++ x-pack/plugins/enterprise_search/public/applications/app_search/components/crawler/components/crawl_details_flyout/accordion_list.tsx
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+
+import {
+  EuiAccordion,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiInMemoryTable,
+  EuiNotificationBadge,
+  EuiTitle,
+  IconType,
+  useGeneratedHtmlId,
+} from '@elastic/eui';
+
+interface Props {
+  iconType: IconType;
+  items: string[];
+  title: string;
+}
+
+export const AccordionList: React.FC<Props> = ({ iconType, items, title }) => {
+  const accordionId = useGeneratedHtmlId({
+    prefix: 'accordionList',
+  });
+
+  return (
+    <EuiAccordion
+      buttonContent={
+        <EuiFlexGroup direction="row" responsive={false} gutterSize="s" alignItems="center">
+          <EuiFlexItem grow={false}>
+            <EuiIcon type={iconType} />
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiTitle size="s">
+              <h3>{title}</h3>
+            </EuiTitle>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      }
+      id={accordionId}
+      extraAction={<EuiNotificationBadge size="m">{items.length}</EuiNotificationBadge>}
+    >
+      <EuiInMemoryTable
+        items={items.map((item) => ({ item }))}
+        columns={[
+          {
+            render: ({ item }: { item: string }) => item,
+          },
+        ]}
+        pagination={{
+          hidePerPageOptions: true,
+        }}
+      />
+    </EuiAccordion>
+  );
+};
