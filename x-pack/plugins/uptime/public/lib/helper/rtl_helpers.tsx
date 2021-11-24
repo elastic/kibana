34911cc7f8555685fe637ad4774233a3af5e249f
Dominique Clarke <dominique.clarke@elastic.co>
2021-11-24 15:16:09 -0500

    adjust rtl_helpers

--- x-pack/plugins/uptime/public/lib/helper/rtl_helpers.tsx
+++ x-pack/plugins/uptime/public/lib/helper/rtl_helpers.tsx
@@ -12,9 +12,8 @@ import { render as reactTestLibRender, RenderOptions } from '@testing-library/re
 import { Router } from 'react-router-dom';
 import { createMemoryHistory, History } from 'history';
 import { CoreStart } from 'kibana/public';
-import { I18nProvider } from '@kbn/i18n/react';
-import { EuiPageTemplate } from '@elastic/eui';
 import { I18nProvider } from '@kbn/i18n-react';
+import { EuiPageTemplate } from '@elastic/eui';
 import { coreMock } from 'src/core/public/mocks';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { configure } from '@testing-library/dom';
