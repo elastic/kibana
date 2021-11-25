c4fab44f30fdaec484cdd04f91a089557da4b4b1
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-11-25 17:49:09 +0200

    Added KibanaThemeProvider to filters.

--- x-pack/plugins/canvas/canvas_plugin_src/renderers/filters/advanced_filter/index.tsx
+++ x-pack/plugins/canvas/canvas_plugin_src/renderers/filters/advanced_filter/index.tsx
@@ -7,27 +7,33 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { KibanaThemeProvider } from '../../../../../../../src/plugins/kibana_react/public';
 import { RendererFactory } from '../../../../types';
 import { AdvancedFilter } from './component';
 import { RendererStrings } from '../../../../i18n';
 
 const { advancedFilter: strings } = RendererStrings;
 
-export const advancedFilter: RendererFactory<{}> = () => ({
-  name: 'advanced_filter',
-  displayName: strings.getDisplayName(),
-  help: strings.getHelpDescription(),
-  reuseDomNode: true,
-  height: 50,
-  render(domNode, _, handlers) {
-    ReactDOM.render(
-      <AdvancedFilter commit={handlers.setFilter} value={handlers.getFilter()} />,
-      domNode,
-      () => handlers.done()
-    );
+export const advancedFilterFactory: StartInitializer<RendererFactory<{}>> = (core, plugins) => {
+  const { theme } = core;
+  return () => ({
+    name: 'advanced_filter',
+    displayName: strings.getDisplayName(),
+    help: strings.getHelpDescription(),
+    reuseDomNode: true,
+    height: 50,
+    render(domNode, _, handlers) {
+      ReactDOM.render(
+        <KibanaThemeProvider theme$={theme.theme$}>
+          <AdvancedFilter commit={handlers.setFilter} value={handlers.getFilter()} />
+        </KibanaThemeProvider>,
+        domNode,
+        () => handlers.done()
+      );
 
-    handlers.onDestroy(() => {
-      ReactDOM.unmountComponentAtNode(domNode);
-    });
-  },
-});
+      handlers.onDestroy(() => {
+        ReactDOM.unmountComponentAtNode(domNode);
+      });
+    },
+  });
+};
