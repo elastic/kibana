6eba35d767a55bd0a8c42b95174dc7fa56697169
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2021-11-27 00:18:38 +0100

    fix(functional_tests.js): add synthetics config import

--- x-pack/scripts/functional_tests.js
+++ x-pack/scripts/functional_tests.js
@@ -90,7 +90,8 @@ const onlyNotInCoverageTests = [
   require.resolve('../test/saved_object_tagging/api_integration/security_and_spaces/config.ts'),
   require.resolve('../test/saved_object_tagging/api_integration/tagging_api/config.ts'),
   require.resolve('../test/examples/config.ts'),
-  require.resolve('../test/performance/config.ts'),
+  require.resolve('../test/performance/config.ftr.ts'),
+  require.resolve('../test/performance/config.synthetics.ts'),
   require.resolve('../test/functional_execution_context/config.ts'),
 ];
 
