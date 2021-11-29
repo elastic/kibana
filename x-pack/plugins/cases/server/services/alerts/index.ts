17d9eb0f076a598329210412786ae28ec6bbaf34
Jonathan Buttner <jonathan.buttner@elastic.co>
2021-11-29 16:19:48 -0500

    Removing alerts internal client

--- x-pack/plugins/cases/server/services/alerts/index.ts
+++ x-pack/plugins/cases/server/services/alerts/index.ts
@@ -21,18 +21,6 @@ import {
 
 export type AlertServiceContract = PublicMethodsOf<AlertService>;
 
-interface UpdateAlertsStatusArgs {
-  alerts: UpdateAlertRequest[];
-  scopedClusterClient: ElasticsearchClient;
-  logger: Logger;
-}
-
-interface GetAlertsArgs {
-  alertsInfo: AlertInfo[];
-  scopedClusterClient: ElasticsearchClient;
-  logger: Logger;
-}
-
 interface Alert {
   _id: string;
   _index: string;
@@ -48,31 +36,112 @@ function isEmptyAlert(alert: AlertInfo): boolean {
 }
 
 export class AlertService {
-  public async updateAlertsStatus({ alerts, scopedClusterClient, logger }: UpdateAlertsStatusArgs) {
+  constructor(
+    private readonly scopedClusterClient: ElasticsearchClient,
+    private readonly logger: Logger
+  ) {}
+
+  public async updateAlertsStatus(alerts: UpdateAlertRequest[]) {
     try {
-      const bucketedAlerts = bucketAlertsByIndexAndStatus(alerts, logger);
+      const bucketedAlerts = this.bucketAlertsByIndexAndStatus(alerts);
       const indexBuckets = Array.from(bucketedAlerts.entries());
 
       await pMap(
         indexBuckets,
         async (indexBucket: [string, Map<STATUS_VALUES, TranslatedUpdateAlertRequest[]>]) =>
-          updateByQuery(indexBucket, scopedClusterClient),
+          this.updateByQuery(indexBucket),
         { concurrency: MAX_CONCURRENT_SEARCHES }
       );
     } catch (error) {
       throw createCaseError({
         message: `Failed to update alert status ids: ${JSON.stringify(alerts)}: ${error}`,
         error,
-        logger,
+        logger: this.logger,
       });
     }
   }
 
-  public async getAlerts({
-    scopedClusterClient,
-    alertsInfo,
-    logger,
-  }: GetAlertsArgs): Promise<AlertsResponse | undefined> {
+  private bucketAlertsByIndexAndStatus(
+    alerts: UpdateAlertRequest[]
+  ): Map<string, Map<STATUS_VALUES, TranslatedUpdateAlertRequest[]>> {
+    return alerts.reduce<Map<string, Map<STATUS_VALUES, TranslatedUpdateAlertRequest[]>>>(
+      (acc, alert) => {
+        // skip any alerts that are empty
+        if (isEmptyAlert(alert)) {
+          return acc;
+        }
+
+        const translatedAlert = { ...alert, status: this.translateStatus(alert) };
+        const statusToAlertId = acc.get(translatedAlert.index);
+
+        // if we haven't seen the index before
+        if (!statusToAlertId) {
+          // add a new index in the parent map, with an entry for the status the alert set to pointing
+          // to an initial array of only the current alert
+          acc.set(translatedAlert.index, createStatusToAlertMap(translatedAlert));
+        } else {
+          // We had the index in the map so check to see if we have a bucket for the
+          // status, if not add a new status entry with the alert, if so update the status entry
+          // with the alert
+          updateIndexEntryWithStatus(statusToAlertId, translatedAlert);
+        }
+
+        return acc;
+      },
+      new Map()
+    );
+  }
+
+  private translateStatus(alert: UpdateAlertRequest): STATUS_VALUES {
+    const translatedStatuses: Record<string, STATUS_VALUES> = {
+      [CaseStatuses.open]: 'open',
+      [CaseStatuses['in-progress']]: 'acknowledged',
+      [CaseStatuses.closed]: 'closed',
+    };
+
+    const translatedStatus = translatedStatuses[alert.status];
+    if (!translatedStatus) {
+      this.logger.error(
+        `Unable to translate case status ${alert.status} during alert update: ${JSON.stringify(
+          alert
+        )}`
+      );
+    }
+    return translatedStatus ?? 'open';
+  }
+
+  private async updateByQuery([index, statusToAlertMap]: [
+    string,
+    Map<STATUS_VALUES, TranslatedUpdateAlertRequest[]>
+  ]) {
+    const statusBuckets = Array.from(statusToAlertMap);
+    return Promise.all(
+      // this will create three update by query calls one for each of the three statuses
+      statusBuckets.map(([status, translatedAlerts]) =>
+        this.scopedClusterClient.updateByQuery({
+          index,
+          conflicts: 'abort',
+          body: {
+            script: {
+              source: `if (ctx._source['${ALERT_WORKFLOW_STATUS}'] != null) {
+                ctx._source['${ALERT_WORKFLOW_STATUS}'] = '${status}'
+              }
+              if (ctx._source.signal != null && ctx._source.signal.status != null) {
+                ctx._source.signal.status = '${status}'
+              }`,
+              lang: 'painless',
+            },
+            // the query here will contain all the ids that have the same status for the same index
+            // being updated
+            query: { ids: { values: translatedAlerts.map(({ id }) => id) } },
+          },
+          ignore_unavailable: true,
+        })
+      )
+    );
+  }
+
+  public async getAlerts(alertsInfo: AlertInfo[]): Promise<AlertsResponse | undefined> {
     try {
       const docs = alertsInfo
         .filter((alert) => !isEmptyAlert(alert))
@@ -83,7 +152,7 @@ export class AlertService {
         return;
       }
 
-      const results = await scopedClusterClient.mget<Alert>({ body: { docs } });
+      const results = await this.scopedClusterClient.mget<Alert>({ body: { docs } });
 
       // @ts-expect-error @elastic/elasticsearch _source is optional
       return results.body;
@@ -91,7 +160,7 @@ export class AlertService {
       throw createCaseError({
         message: `Failed to retrieve alerts ids: ${JSON.stringify(alertsInfo)}: ${error}`,
         error,
-        logger,
+        logger: this.logger,
       });
     }
   }
@@ -101,62 +170,6 @@ interface TranslatedUpdateAlertRequest {
   id: string;
   index: string;
   status: STATUS_VALUES;
-}
-
-function bucketAlertsByIndexAndStatus(
-  alerts: UpdateAlertRequest[],
-  logger: Logger
-): Map<string, Map<STATUS_VALUES, TranslatedUpdateAlertRequest[]>> {
-  return alerts.reduce<Map<string, Map<STATUS_VALUES, TranslatedUpdateAlertRequest[]>>>(
-    (acc, alert) => {
-      // skip any alerts that are empty
-      if (isEmptyAlert(alert)) {
-        return acc;
-      }
-
-      const translatedAlert = { ...alert, status: translateStatus({ alert, logger }) };
-      const statusToAlertId = acc.get(translatedAlert.index);
-
-      // if we haven't seen the index before
-      if (!statusToAlertId) {
-        // add a new index in the parent map, with an entry for the status the alert set to pointing
-        // to an initial array of only the current alert
-        acc.set(translatedAlert.index, createStatusToAlertMap(translatedAlert));
-      } else {
-        // We had the index in the map so check to see if we have a bucket for the
-        // status, if not add a new status entry with the alert, if so update the status entry
-        // with the alert
-        updateIndexEntryWithStatus(statusToAlertId, translatedAlert);
-      }
-
-      return acc;
-    },
-    new Map()
-  );
-}
-
-function translateStatus({
-  alert,
-  logger,
-}: {
-  alert: UpdateAlertRequest;
-  logger: Logger;
-}): STATUS_VALUES {
-  const translatedStatuses: Record<string, STATUS_VALUES> = {
-    [CaseStatuses.open]: 'open',
-    [CaseStatuses['in-progress']]: 'acknowledged',
-    [CaseStatuses.closed]: 'closed',
-  };
-
-  const translatedStatus = translatedStatuses[alert.status];
-  if (!translatedStatus) {
-    logger.error(
-      `Unable to translate case status ${alert.status} during alert update: ${JSON.stringify(
-        alert
-      )}`
-    );
-  }
-  return translatedStatus ?? 'open';
 }
 
 function createStatusToAlertMap(
@@ -176,35 +189,4 @@ function updateIndexEntryWithStatus(
   } else {
     statusBucket.push(alert);
   }
-}
-
-async function updateByQuery(
-  [index, statusToAlertMap]: [string, Map<STATUS_VALUES, TranslatedUpdateAlertRequest[]>],
-  scopedClusterClient: ElasticsearchClient
-) {
-  const statusBuckets = Array.from(statusToAlertMap);
-  return Promise.all(
-    // this will create three update by query calls one for each of the three statuses
-    statusBuckets.map(([status, translatedAlerts]) =>
-      scopedClusterClient.updateByQuery({
-        index,
-        conflicts: 'abort',
-        body: {
-          script: {
-            source: `if (ctx._source['${ALERT_WORKFLOW_STATUS}'] != null) {
-              ctx._source['${ALERT_WORKFLOW_STATUS}'] = '${status}'
-            }
-            if (ctx._source.signal != null && ctx._source.signal.status != null) {
-              ctx._source.signal.status = '${status}'
-            }`,
-            lang: 'painless',
-          },
-          // the query here will contain all the ids that have the same status for the same index
-          // being updated
-          query: { ids: { values: translatedAlerts.map(({ id }) => id) } },
-        },
-        ignore_unavailable: true,
-      })
-    )
-  );
 }
