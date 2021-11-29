270f54cf79173a91deb4b220b6cfe0c210b10315
Esteban Beltran <esteban.beltran@elastic.co>
2021-11-29 12:20:10 -0500

    Display the name of policies on the artifact card

--- x-pack/plugins/security_solution/public/management/pages/host_isolation_exceptions/view/host_isolation_exceptions_list.tsx
+++ x-pack/plugins/security_solution/public/management/pages/host_isolation_exceptions/view/host_isolation_exceptions_list.tsx
@@ -5,37 +5,39 @@
  * 2.0.
  */
 
-import { ExceptionListItemSchema } from '@kbn/securitysolution-io-ts-list-types';
+import { EuiButton, EuiSpacer, EuiText } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import React, { useCallback, useEffect, useState } from 'react';
-import { EuiButton, EuiText, EuiSpacer } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
+import { ExceptionListItemSchema } from '@kbn/securitysolution-io-ts-list-types';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { ExceptionItem } from '../../../../common/components/exceptions/viewer/exception_item';
-import { getCurrentLocation } from '../store/selector';
-import {
-  useFetchHostIsolationExceptionsList,
-  useHostIsolationExceptionsNavigateCallback,
-  useHostIsolationExceptionsSelector,
-} from './hooks';
-import { PaginatedContent, PaginatedContentProps } from '../../../components/paginated_content';
 import { Immutable } from '../../../../../common/endpoint/types';
-import { AdministrationListPage } from '../../../components/administration_list_page';
-import { SearchExceptions } from '../../../components/search_exceptions';
-import { ArtifactEntryCard, ArtifactEntryCardProps } from '../../../components/artifact_entry_card';
-import { HostIsolationExceptionsEmptyState } from './components/empty';
-import { HostIsolationExceptionDeleteModal } from './components/delete_modal';
-import { HostIsolationExceptionsFormFlyout } from './components/form_flyout';
-import {
-  DELETE_HOST_ISOLATION_EXCEPTION_LABEL,
-  EDIT_HOST_ISOLATION_EXCEPTION_LABEL,
-} from './components/translations';
-import { getEndpointListPath } from '../../../common/routing';
+import { ExceptionItem } from '../../../../common/components/exceptions/viewer/exception_item';
 import { useEndpointPrivileges } from '../../../../common/components/user_privileges/endpoint';
 import {
   MANAGEMENT_DEFAULT_PAGE_SIZE,
   MANAGEMENT_PAGE_SIZE_OPTIONS,
 } from '../../../common/constants';
+import { getEndpointListPath } from '../../../common/routing';
+import { AdministrationListPage } from '../../../components/administration_list_page';
+import { ArtifactEntryCard, ArtifactEntryCardProps } from '../../../components/artifact_entry_card';
+import { useEndpointPoliciesToArtifactPolicies } from '../../../components/artifact_entry_card/hooks/use_endpoint_policies_to_artifact_policies';
+import { PaginatedContent, PaginatedContentProps } from '../../../components/paginated_content';
+import { SearchExceptions } from '../../../components/search_exceptions';
+import { useGetEndpointSpecificPolicies } from '../../../services/policies/hooks';
+import { getCurrentLocation } from '../store/selector';
+import { HostIsolationExceptionDeleteModal } from './components/delete_modal';
+import { HostIsolationExceptionsEmptyState } from './components/empty';
+import { HostIsolationExceptionsFormFlyout } from './components/form_flyout';
+import {
+  DELETE_HOST_ISOLATION_EXCEPTION_LABEL,
+  EDIT_HOST_ISOLATION_EXCEPTION_LABEL,
+} from './components/translations';
+import {
+  useFetchHostIsolationExceptionsList,
+  useHostIsolationExceptionsNavigateCallback,
+  useHostIsolationExceptionsSelector,
+} from './hooks';
 
 type HostIsolationExceptionPaginatedContent = PaginatedContentProps<
   Immutable<ExceptionListItemSchema>,
@@ -52,6 +54,11 @@ export const HostIsolationExceptionsList = () => {
   const [itemToDelete, setItemToDelete] = useState<ExceptionListItemSchema | null>(null);
 
   const { isLoading, data, error, refetch } = useFetchHostIsolationExceptionsList();
+
+  // load the list of policies>
+  const policiesRequest = useGetEndpointSpecificPolicies({
+    onError: () => {},
+  });
 
   const pagination = {
     totalItemCount: data?.total ?? 0,
@@ -79,6 +86,8 @@ export const HostIsolationExceptionsList = () => {
     [navigateCallback]
   );
 
+  const artifactCardPolicies = useEndpointPoliciesToArtifactPolicies(policiesRequest.data?.items);
+
   function handleItemComponentProps(element: ExceptionListItemSchema): ArtifactEntryCardProps {
     const editAction = {
       icon: 'controlsHorizontal',
@@ -103,6 +112,7 @@ export const HostIsolationExceptionsList = () => {
       item: element,
       'data-test-subj': `hostIsolationExceptionsCard`,
       actions: privileges.canIsolateHost ? [editAction, deleteAction] : [deleteAction],
+      policies: artifactCardPolicies,
     };
   }
 
