d1c3b1011842545f2faa6f21008270421dbb14ae
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2021-11-29 23:04:40 +0100

    Make fixture app wait until network is idle before running authentication tests. (#119715)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/security_functional/fixtures/common/test_endpoints/public/plugin.tsx
+++ x-pack/test/security_functional/fixtures/common/test_endpoints/public/plugin.tsx
@@ -8,21 +8,34 @@
 import type { CoreSetup, Plugin } from 'src/core/public';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { debounce, filter, first } from 'rxjs/operators';
+import { timer } from 'rxjs';
 
 export class TestEndpointsPlugin implements Plugin {
   public setup(core: CoreSetup) {
     // Prevent auto-logout on server `401` errors.
     core.http.anonymousPaths.register('/authentication/app');
+
+    const networkIdle$ = core.http.getLoadingCount$().pipe(
+      debounce(() => timer(3000)),
+      filter((count) => count === 0),
+      first()
+    );
+
     core.application.register({
       id: 'authentication_app',
       title: 'Authentication app',
       appRoute: '/authentication/app',
       chromeless: true,
       async mount({ element }) {
-        ReactDOM.render(
-          <div data-test-subj="testEndpointsAuthenticationApp">Authenticated!</div>,
-          element
-        );
+        // Promise is resolved as soon there are no requests has been made in the last 3 seconds. We need this to make
+        // sure none of the unrelated requests interferes with the test logic.
+        networkIdle$.toPromise().then(() => {
+          ReactDOM.render(
+            <div data-test-subj="testEndpointsAuthenticationApp">Authenticated!</div>,
+            element
+          );
+        });
         return () => ReactDOM.unmountComponentAtNode(element);
       },
     });
