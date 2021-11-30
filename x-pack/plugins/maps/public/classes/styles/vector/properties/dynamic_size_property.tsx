5b43f9dac25b55a5e8816d5cf073fae4744aa3b1
Thomas Neirynck <thomas@elastic.co>
2021-11-29 19:04:57 -0500

    [Maps] Use minimum symbol size if meta is not loaded yet. (#119119)

--- x-pack/plugins/maps/public/classes/styles/vector/properties/dynamic_size_property.tsx
+++ x-pack/plugins/maps/public/classes/styles/vector/properties/dynamic_size_property.tsx
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-import _ from 'lodash';
 import React from 'react';
 import type { Map as MbMap } from '@kbn/mapbox-gl';
 import { DynamicStyleProperty } from './dynamic_style_property';
@@ -111,7 +110,11 @@ export class DynamicSizeProperty extends DynamicStyleProperty<SizeDynamicOptions
   getMbSizeExpression() {
     const rangeFieldMeta = this.getRangeFieldMeta();
     if (!this._isSizeDynamicConfigComplete() || !rangeFieldMeta) {
-      return null;
+      // return min of size to avoid flashing
+      // returning minimum allows "growing" of the symbols when the meta comes in
+      // A grow effect us less visually jarring as shrinking.
+      // especially relevant when displaying fine-grained grids using mvt
+      return this._options.minSize >= 0 ? this._options.minSize : null;
     }
 
     return this._getMbDataDrivenSize({
@@ -156,8 +159,8 @@ export class DynamicSizeProperty extends DynamicStyleProperty<SizeDynamicOptions
     return (
       this._field &&
       this._field.isValid() &&
-      _.has(this._options, 'minSize') &&
-      _.has(this._options, 'maxSize')
+      this._options.minSize >= 0 &&
+      this._options.maxSize >= 0
     );
   }
 
