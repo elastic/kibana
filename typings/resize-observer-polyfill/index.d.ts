a6098e6f9f6d1fc85beef7c935ed932cc8f40623
Tyler Smalley <tyler.smalley@elastic.co>
2021-11-30 12:29:08 -0800

    Fix types
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- typings/resize-observer-polyfill/index.d.ts
+++ typings/resize-observer-polyfill/index.d.ts
@@ -1,29 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 declare global {
-    interface ResizeObserverCallback {
-        (entries: ResizeObserverEntry[], observer: ResizeObserver): void
-    }
+  type ResizeObserverCallback = (entries: ResizeObserverEntry[], observer: ResizeObserver) => void;
 
-    interface ResizeObserverEntry {
-        readonly target: Element;
-        readonly contentRect: DOMRectReadOnly;
-    }
+  interface ResizeObserverEntry {
+    readonly target: Element;
+    readonly contentRect: DOMRectReadOnly;
+  }
 
-    interface ResizeObserver {
-        observe(target: Element): void;
-        unobserve(target: Element): void;
-        disconnect(): void;
-    }
-}
-
-declare var ResizeObserver: {
-    prototype: ResizeObserver;
-    new(callback: ResizeObserverCallback): ResizeObserver;
-}
-
-interface ResizeObserver {
+  interface ResizeObserver {
     observe(target: Element): void;
     unobserve(target: Element): void;
     disconnect(): void;
+  }
 }
 
+declare const ResizeObserver: {
+  prototype: ResizeObserver;
+  new (callback: ResizeObserverCallback): ResizeObserver;
+};
+
+interface ResizeObserver {
+  observe(target: Element): void;
+  unobserve(target: Element): void;
+  disconnect(): void;
+}
+
+// eslint-disable-next-line import/no-default-export
 export default ResizeObserver;
