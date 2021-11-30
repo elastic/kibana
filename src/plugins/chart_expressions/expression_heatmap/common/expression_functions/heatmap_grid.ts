fea4d2acfbc789cdbde56eb70a4a864ddd714e38
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2021-11-30 19:40:02 +0200

    [Visualize] New heatmap implementation with elastic-charts (#118338)
    * [WIP][Heatmap] Creates implementation with elastic-charts
    
    * Fix types and connection with vislib
    
    * Add coloring options
    
    * Brush, click events, coloring etc
    
    * Cleaning up the expression function
    
    * Add legend picker, fix sorting and other fixes
    
    * Further fixes
    
    * Use the shared expression to Lens and cleanup
    
    * PrepareLogTables for new expression function
    
    * Use common renderer and expression function with lens
    
    * Fix i18n
    
    * Small tweaks
    
    * Add unit tests
    
    * Adds a unit test to the heatmap component
    
    * update plugin list
    
    * Fix types
    
    * Fix types
    
    * update limits
    
    * Change to the expression function
    
    * Cleanup translations
    
    * Refactor to use vis
    
    * Fix types
    
    * further cleanup of the translations
    
    * register new setting
    
    * Fix sorting for histogram
    
    * Adds functional tests for the new nisualize heatmap
    
    * Cleanup
    
    * Fix
    
    * Apply PR comments
    
    * Address PR comments
    
    * Fix i18n
    
    * Fix i18n
    
    * Makes the <Heatmap /> id dynamic
    
    * reverse
    
    * fix translation file
    
    * Apply design PR comments
    
    * Fix package
    
    * More fixes
    
    * Fix brush problem
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/chart_expressions/expression_heatmap/common/expression_functions/heatmap_grid.ts
+++ src/plugins/chart_expressions/expression_heatmap/common/expression_functions/heatmap_grid.ts
@@ -1,70 +1,53 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { i18n } from '@kbn/i18n';
-import type { ExpressionFunctionDefinition } from '../../../../../../src/plugins/expressions/common';
-
-export const HEATMAP_GRID_FUNCTION = 'lens_heatmap_grid';
-
-export interface HeatmapGridConfig {
-  // grid
-  strokeWidth?: number;
-  strokeColor?: string;
-  cellHeight?: number;
-  cellWidth?: number;
-  // cells
-  isCellLabelVisible: boolean;
-  // Y-axis
-  isYAxisLabelVisible: boolean;
-  yAxisLabelWidth?: number;
-  yAxisLabelColor?: string;
-  // X-axis
-  isXAxisLabelVisible: boolean;
-}
-
-export type HeatmapGridConfigResult = HeatmapGridConfig & { type: typeof HEATMAP_GRID_FUNCTION };
+import type { ExpressionFunctionDefinition } from '../../../../expressions/common';
+import { EXPRESSION_HEATMAP_GRID_NAME } from '../constants';
+import { HeatmapGridConfig, HeatmapGridConfigResult } from '../types';
 
 export const heatmapGridConfig: ExpressionFunctionDefinition<
-  typeof HEATMAP_GRID_FUNCTION,
+  typeof EXPRESSION_HEATMAP_GRID_NAME,
   null,
   HeatmapGridConfig,
   HeatmapGridConfigResult
 > = {
-  name: HEATMAP_GRID_FUNCTION,
+  name: EXPRESSION_HEATMAP_GRID_NAME,
   aliases: [],
-  type: HEATMAP_GRID_FUNCTION,
+  type: EXPRESSION_HEATMAP_GRID_NAME,
   help: `Configure the heatmap layout `,
   inputTypes: ['null'],
   args: {
     // grid
     strokeWidth: {
       types: ['number'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.strokeWidth.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.strokeWidth.help', {
         defaultMessage: 'Specifies the grid stroke width',
       }),
       required: false,
     },
     strokeColor: {
       types: ['string'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.strokeColor.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.strokeColor.help', {
         defaultMessage: 'Specifies the grid stroke color',
       }),
       required: false,
     },
     cellHeight: {
       types: ['number'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.cellHeight.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.cellHeight.help', {
         defaultMessage: 'Specifies the grid cell height',
       }),
       required: false,
     },
     cellWidth: {
       types: ['number'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.cellWidth.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.cellWidth.help', {
         defaultMessage: 'Specifies the grid cell width',
       }),
       required: false,
@@ -72,27 +55,27 @@ export const heatmapGridConfig: ExpressionFunctionDefinition<
     // cells
     isCellLabelVisible: {
       types: ['boolean'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.isCellLabelVisible.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.isCellLabelVisible.help', {
         defaultMessage: 'Specifies whether or not the cell label is visible.',
       }),
     },
     // Y-axis
     isYAxisLabelVisible: {
       types: ['boolean'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.isYAxisLabelVisible.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.isYAxisLabelVisible.help', {
         defaultMessage: 'Specifies whether or not the Y-axis labels are visible.',
       }),
     },
     yAxisLabelWidth: {
       types: ['number'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.yAxisLabelWidth.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.yAxisLabelWidth.help', {
         defaultMessage: 'Specifies the width of the Y-axis labels.',
       }),
       required: false,
     },
     yAxisLabelColor: {
       types: ['string'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.yAxisLabelColor.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.yAxisLabelColor.help', {
         defaultMessage: 'Specifies the color of the Y-axis labels.',
       }),
       required: false,
@@ -100,14 +83,14 @@ export const heatmapGridConfig: ExpressionFunctionDefinition<
     // X-axis
     isXAxisLabelVisible: {
       types: ['boolean'],
-      help: i18n.translate('xpack.lens.heatmapChart.config.isXAxisLabelVisible.help', {
+      help: i18n.translate('expressionHeatmap.function.args.grid.isXAxisLabelVisible.help', {
         defaultMessage: 'Specifies whether or not the X-axis labels are visible.',
       }),
     },
   },
   fn(input, args) {
     return {
-      type: HEATMAP_GRID_FUNCTION,
+      type: EXPRESSION_HEATMAP_GRID_NAME,
       ...args,
     };
   },
