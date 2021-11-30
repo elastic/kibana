c33b64f3034e0cf4a99196f0d60369e9759f934a
Ignacio Rivas <rivasign@gmail.com>
2021-11-30 10:41:05 +0100

    Fix tests

--- x-pack/test/api_integration/apis/watcher/watcher.ts
+++ x-pack/test/api_integration/apis/watcher/watcher.ts
@@ -10,9 +10,29 @@ import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ getService }: FtrProviderContext) {
+  const log = getService('log');
   const supertest = getService('supertest');
+  const transform = getService('transform');
 
   describe('watcher', () => {
+    before(async () => {
+      try {
+        await transform.testResources.createIndexPatternIfNeeded('ft_ecommerce', 'order_date');
+      } catch (error) {
+        log.debug('[Setup error] Error creating index pattern');
+        throw error;
+      }
+    });
+
+    after(async () => {
+      try {
+        await transform.testResources.deleteIndexPatternByTitle('ft_ecommerce');
+      } catch (error) {
+        log.debug('[Cleanup error] Error deleting index pattern');
+        throw error;
+      }
+    });
+
     describe('POST /api/watcher/indices/index_patterns', () => {
       it('returns list of index patterns', async () => {
         const response = await supertest
@@ -20,8 +40,7 @@ export default function ({ getService }: FtrProviderContext) {
           .set('kbn-xsrf', 'kibana')
           .expect(200);
 
-        const expectedResponse = ['metrics-*', 'logs-*'];
-        expect(response.body).to.eql(expectedResponse);
+        expect(response.body).to.contain('ft_ecommerce');
       });
     });
   });
