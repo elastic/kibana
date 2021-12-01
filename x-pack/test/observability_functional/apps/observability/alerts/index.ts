5127293de4f4f3039ee7be14a621d93f882ca8ee
Ersin Erdal <92688503+ersin-erdal@users.noreply.github.com>
2021-12-01 12:58:18 +0100

    [RAC] fix failing alerts table pagination functional tests (#119985)
    * [RAC] fix failing alerts table pagination functional tests

--- x-pack/test/observability_functional/apps/observability/alerts/index.ts
+++ x-pack/test/observability_functional/apps/observability/alerts/index.ts
@@ -15,14 +15,14 @@ async function asyncForEach<T>(array: T[], callback: (item: T, index: number) =>
 }
 
 const ACTIVE_ALERTS_CELL_COUNT = 78;
-const RECOVERED_ALERTS_CELL_COUNT = 100;
+const RECOVERED_ALERTS_CELL_COUNT = 120;
 const TOTAL_ALERTS_CELL_COUNT = 165;
 
 export default ({ getService }: FtrProviderContext) => {
   const esArchiver = getService('esArchiver');
   const find = getService('find');
 
-  describe('Observability alerts 1', function () {
+  describe('Observability alerts', function () {
     this.tags('includeFirefox');
 
     const testSubjects = getService('testSubjects');
