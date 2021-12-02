bd9776752aaab355bd8fab7ed77ef651f9373bfb
FrankHassanabad <frank.hassanabad@elastic.co>
2021-12-01 17:46:35 -0700

    [Security Solutions] Removes tech debt of exporting all from linter rule

--- x-pack/plugins/security_solution/public/common/components/event_details/alert_summary_view.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/alert_summary_view.tsx
@@ -14,7 +14,7 @@ import { AlertSummaryRow, getSummaryColumns, SummaryRow } from './helpers';
 
 import { ActionCell } from './table/action_cell';
 import { FieldValueCell } from './table/field_value_cell';
-import { TimelineEventsDetailsItem } from '../../../../common';
+import { TimelineEventsDetailsItem } from '../../../../common/search_strategy';
 
 import { getSummaryRows } from './get_alert_summary_rows';
 
