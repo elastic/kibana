5b839e9118ba37a660d0a8713d27a1bd4e0cc1d6
Vadim Yakhin <vadim.yakhin@elastic.co>
2021-12-02 13:50:23 -0800

    Add new view and logic for creating a GitHub content source via GitHub apps
    Also rename github_app to github_via_app to match service_type on backend

---
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/github_via_app_logic.ts
@@ -0,0 +1,111 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { kea, MakeLogicType } from 'kea';
+
+import { flashAPIErrors, flashSuccessToast } from '../../../../../shared/flash_messages';
+import { HttpLogic } from '../../../../../shared/http';
+import { KibanaLogic } from '../../../../../shared/kibana';
+import { AppLogic } from '../../../../app_logic';
+import { SOURCES_PATH, getSourcesPath } from '../../../../routes';
+import { ContentSourceFullData } from '../../../../types';
+
+interface GithubViaAppValues {
+  githubAppId: string;
+  githubEnterpriseServerUrl: string;
+  stagedPrivateKey: string | null;
+  isSubmitButtonLoading: boolean;
+  indexPermissionsValue: boolean;
+}
+
+interface GithubViaAppActions {
+  setGithubAppId(githubAppId: string): string;
+  setGithubEnterpriseServerUrl(githubEnterpriseServerUrl: string): string;
+  setStagedPrivateKey(stagedPrivateKey: string | null): string | null;
+  setButtonNotLoading(): void;
+  createContentSource(): void;
+  setSourceIndexPermissionsValue(indexPermissionsValue: boolean): boolean;
+}
+
+export const GithubViaAppLogic = kea<MakeLogicType<GithubViaAppValues, GithubViaAppActions>>({
+  path: ['enterprise_search', 'workplace_search', 'github_via_app_logic'],
+  actions: {
+    setGithubAppId: (githubAppId: string) => githubAppId,
+    setGithubEnterpriseServerUrl: (githubEnterpriseServerUrl: string) => githubEnterpriseServerUrl,
+    createContentSource: () => true,
+    setStagedPrivateKey: (stagedPrivateKey: string) => stagedPrivateKey,
+    setButtonNotLoading: () => false,
+    setSourceIndexPermissionsValue: (indexPermissionsValue: boolean) => indexPermissionsValue,
+  },
+  reducers: {
+    githubAppId: [
+      '',
+      {
+        setGithubAppId: (_, githubAppId) => githubAppId,
+      },
+    ],
+    githubEnterpriseServerUrl: [
+      '',
+      {
+        setGithubEnterpriseServerUrl: (_, githubEnterpriseServerUrl) => githubEnterpriseServerUrl,
+      },
+    ],
+    stagedPrivateKey: [
+      null,
+      {
+        setStagedPrivateKey: (_, stagedPrivateKey) => stagedPrivateKey,
+      },
+    ],
+    isSubmitButtonLoading: [
+      false,
+      {
+        createContentSource: () => true,
+        setButtonNotLoading: () => false,
+      },
+    ],
+    indexPermissionsValue: [
+      true,
+      {
+        setSourceIndexPermissionsValue: (_, indexPermissionsValue) => indexPermissionsValue,
+        resetSourceState: () => false,
+      },
+    ],
+  },
+  listeners: ({ actions, values }) => ({
+    createContentSource: async () => {
+      const { isOrganization } = AppLogic.values;
+      const route = isOrganization
+        ? '/internal/workplace_search/org/create_source'
+        : '/internal/workplace_search/account/create_source';
+
+      const { githubAppId, githubEnterpriseServerUrl, stagedPrivateKey } = values;
+
+      const params = {
+        service_type: githubEnterpriseServerUrl
+          ? 'github_enterprise_server_via_app'
+          : 'github_via_app',
+        app_id: githubAppId,
+        base_url: githubEnterpriseServerUrl,
+        private_key: stagedPrivateKey,
+        indexPermissions: values.indexPermissionsValue,
+      };
+
+      try {
+        const response = await HttpLogic.values.http.post<ContentSourceFullData>(route, {
+          body: JSON.stringify({ ...params }),
+        });
+
+        KibanaLogic.values.navigateToUrl(`${getSourcesPath(SOURCES_PATH, isOrganization)}`);
+        flashSuccessToast(`${response.serviceName} connected`);
+      } catch (e) {
+        flashAPIErrors(e);
+      } finally {
+        actions.setButtonNotLoading();
+      }
+    },
+  }),
+});
