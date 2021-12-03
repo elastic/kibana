1a43dba30d37811436dc9503375911f09505a245
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2021-12-03 14:31:43 +0100

    Fix globalsearchg functional tests (#120333)

--- x-pack/plugins/global_search/server/routes/integration_tests/get_searchable_types.test.ts
+++ x-pack/plugins/global_search/server/routes/integration_tests/get_searchable_types.test.ts
@@ -14,8 +14,7 @@ import { registerInternalSearchableTypesRoute } from '../get_searchable_types';
 type SetupServerReturn = UnwrapPromise<ReturnType<typeof setupServer>>;
 const pluginId = Symbol('globalSearch');
 
-// FAILING: https://github.com/elastic/kibana/issues/120268
-describe.skip('GET /internal/global_search/searchable_types', () => {
+describe('GET /internal/global_search/searchable_types', () => {
   let server: SetupServerReturn['server'];
   let httpSetup: SetupServerReturn['httpSetup'];
   let globalSearchHandlerContext: ReturnType<
@@ -47,7 +46,7 @@ describe.skip('GET /internal/global_search/searchable_types', () => {
 
   it('calls the handler context with correct parameters', async () => {
     await supertest(httpSetup.server.listener)
-      .post('/internal/global_search/searchable_types')
+      .get('/internal/global_search/searchable_types')
       .expect(200);
 
     expect(globalSearchHandlerContext.getSearchableTypes).toHaveBeenCalledTimes(1);
@@ -57,7 +56,7 @@ describe.skip('GET /internal/global_search/searchable_types', () => {
     globalSearchHandlerContext.getSearchableTypes.mockResolvedValue(['type-a', 'type-b']);
 
     const response = await supertest(httpSetup.server.listener)
-      .post('/internal/global_search/searchable_types')
+      .get('/internal/global_search/searchable_types')
       .expect(200);
 
     expect(response.body).toEqual({
@@ -69,8 +68,8 @@ describe.skip('GET /internal/global_search/searchable_types', () => {
     globalSearchHandlerContext.getSearchableTypes.mockRejectedValue(new Error());
 
     const response = await supertest(httpSetup.server.listener)
-      .post('/internal/global_search/searchable_types')
-      .expect(200);
+      .get('/internal/global_search/searchable_types')
+      .expect(500);
 
     expect(response.body).toEqual(
       expect.objectContaining({
