4b6758048f3a0cb6dac7e7e3622cffd471e0d539
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-12-03 19:26:03 +0200

    [Canvas] Added KibanaThemeProvider to expression_reveal_image. (#120094)
    * Added kibanaThemeProvider to expression_reveal_image.
    
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/canvas/shareable_runtime/supported_renderers.js
+++ x-pack/plugins/canvas/shareable_runtime/supported_renderers.js
@@ -15,8 +15,8 @@ import {
   getErrorRenderer,
   getDebugRenderer,
 } from '../../../../src/plugins/expression_error/public';
+import { getRevealImageRenderer } from '../../../../src/plugins/expression_reveal_image/public';
 import { getRepeatImageRenderer } from '../../../../src/plugins/expression_repeat_image/public';
-import { revealImageRenderer as revealImage } from '../../../../src/plugins/expression_reveal_image/public';
 import {
   shapeRenderer as shape,
   progressRenderer as progress,
@@ -31,6 +31,7 @@ const renderFunctionsFactories = [
   getTableRenderer,
   getErrorRenderer,
   getDebugRenderer,
+  getRevealImageRenderer,
   getRepeatImageRenderer,
   getMetricRenderer,
 ];
@@ -42,7 +43,6 @@ const renderFunctionsFactories = [
  */
 export const renderFunctions = [
   image,
-  revealImage,
   pie,
   plot,
   progress,
