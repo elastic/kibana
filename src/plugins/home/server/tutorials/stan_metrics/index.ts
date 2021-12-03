b96f439dfb22361f9ee49551d91047f1c1922f98
Josh Dover <doverfake@elastic.co>
2021-12-03 19:25:18 +0100

    Add self-signed fields to metricbeat tutorials

--- src/plugins/home/server/tutorials/stan_metrics/index.ts
+++ src/plugins/home/server/tutorials/stan_metrics/index.ts
@@ -56,8 +56,8 @@ export function stanMetricsSpecProvider(context: TutorialContext): TutorialSchem
     completionTimeMinutes: 10,
     previewImagePath: '/plugins/home/assets/stan_metrics/screenshot.png',
     onPrem: onPremInstructions(moduleName, context),
-    elasticCloud: cloudInstructions(moduleName),
-    onPremElasticCloud: onPremCloudInstructions(moduleName),
+    elasticCloud: cloudInstructions(moduleName, context),
+    onPremElasticCloud: onPremCloudInstructions(moduleName, context),
     integrationBrowserCategories: ['message_queue', 'kubernetes'],
   };
 }
