e85c00738576935735251cbdc5de5baa1613c45f
Rudolf Meijering <rudolf.meijering@elastic.co>
2021-12-03 22:18:21 +0100

    Second try to commit pre-built @kbn/pm package

--- packages/kbn-pm/dist/index.js
+++ packages/kbn-pm/dist/index.js
@@ -6642,9 +6642,7 @@ class ToolingLogTextWriter {
       if (msg.type === 'write') {
         const txt = (0, _util.format)(msg.args[0], ...msg.args.slice(1)); // Ensure that Elasticsearch deprecation log messages from Kibana aren't ignored
 
-        if (/\[deprecation\]\[elasticsearch\]/.test(txt)) {
-          return true;
-        } else {
+        if (!/\[deprecation\]\[elasticsearch\]/.test(txt)) {
           return false;
         }
       } else {
