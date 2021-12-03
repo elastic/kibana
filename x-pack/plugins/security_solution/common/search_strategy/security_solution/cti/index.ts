a0db43e07bfe611bc7d89a234c8511a2a49769fd
Brian Seeders <brian.seeders@elastic.co>
2021-12-03 10:50:17 -0500

    Revert " [CTI] Threat Intel Card on Overview page needs to accommodate Fleet TI integrations (#115940)"
    This reverts commit 6640357eb6e2a4b5db39e468dc315bce74109259.

--- x-pack/plugins/security_solution/common/search_strategy/security_solution/cti/index.ts
+++ x-pack/plugins/security_solution/common/search_strategy/security_solution/cti/index.ts
@@ -5,16 +5,13 @@
  * 2.0.
  */
 
-import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
-import { IEsSearchResponse, IEsSearchRequest } from 'src/plugins/data/public';
-import { FactoryQueryTypes } from '../..';
+import type { IEsSearchResponse } from 'src/plugins/data/public';
 import { EVENT_ENRICHMENT_INDICATOR_FIELD_MAP } from '../../../cti/constants';
-import { Inspect, Maybe, TimerangeInput } from '../../common';
+import { Inspect } from '../../common';
 import { RequestBasicOptions } from '..';
 
 export enum CtiQueries {
   eventEnrichment = 'eventEnrichment',
-  dataSource = 'dataSource',
 }
 
 export interface CtiEventEnrichmentRequestOptions extends RequestBasicOptions {
@@ -43,33 +40,3 @@ export const validEventFields = Object.keys(EVENT_ENRICHMENT_INDICATOR_FIELD_MAP
 
 export const isValidEventField = (field: string): field is EventField =>
   validEventFields.includes(field as EventField);
-
-export interface CtiDataSourceRequestOptions extends IEsSearchRequest {
-  defaultIndex: string[];
-  factoryQueryType?: FactoryQueryTypes;
-  timerange?: TimerangeInput;
-}
-
-export interface BucketItem {
-  key: string;
-  doc_count: number;
-}
-export interface Bucket {
-  buckets: Array<BucketItem & { bucket?: Bucket[] }>;
-}
-
-export type DatasetBucket = {
-  name?: Bucket;
-  dashboard?: Bucket;
-} & BucketItem;
-
-export interface CtiDataSourceStrategyResponse extends Omit<IEsSearchResponse, 'rawResponse'> {
-  inspect?: Maybe<Inspect>;
-  rawResponse: {
-    aggregations?: Record<string, estypes.AggregationsAggregate> & {
-      dataset?: {
-        buckets: DatasetBucket[];
-      };
-    };
-  };
-}
