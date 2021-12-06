04e438132ac7bf347bd097ab59756294e80fa585
Claudio Procida <claudio.procida@elastic.co>
2021-12-06 13:36:49 +0100

    Adds failing test case

---
+++ x-pack/plugins/rule_registry/common/parse_technical_fields.test.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { parseTechnicalFields } from './parse_technical_fields';
+
+describe('parseTechnicalFields', () => {
+  it('parses an alert with default fields', () => {
+    const ALERT_WITH_DEFAULT_FIELDS = {
+      'kibana.alert.status': ['active'],
+      '@timestamp': ['2021-12-06T12:30:59.411Z'],
+      'kibana.alert.duration.us': ['488935266000'],
+      'kibana.alert.reason': ['host.uptime has reported no data over the past 1m for *'],
+      'kibana.alert.workflow_status': ['open'],
+      'kibana.alert.rule.uuid': ['c8ef4420-4604-11ec-b08c-c590e7b8c4cd'],
+      'kibana.alert.rule.producer': ['infrastructure'],
+      'kibana.alert.rule.consumer': ['alerts'],
+      'kibana.alert.rule.category': ['Metric threshold'],
+      'kibana.alert.start': ['2021-11-30T20:42:04.145Z'],
+      'kibana.alert.rule.rule_type_id': ['metrics.alert.threshold'],
+      'event.action': ['active'],
+      'kibana.alert.instance.id': ['*'],
+      'kibana.alert.rule.name': ['Uptime'],
+      'kibana.alert.uuid': ['f31f5726-3c47-4c88-bc42-4e1fbde17e34'],
+      'kibana.space_ids': ['default'],
+      'kibana.version': ['8.1.0'],
+      'event.kind': ['signal'],
+    };
+    expect(() => parseTechnicalFields(ALERT_WITH_DEFAULT_FIELDS)).not.toThrow();
+  });
+
+  it('parses an alert without reason', () => {
+    const ALERT_WITHOUT_REASON = {
+      '@timestamp': ['2021-12-06T12:25:47.356Z'],
+      'kibana.alert.status': ['active'],
+      'kibana.alert.duration.us': ['1806762716000'],
+    };
+    expect(() => parseTechnicalFields(ALERT_WITHOUT_REASON)).not.toThrow();
+  });
+});
