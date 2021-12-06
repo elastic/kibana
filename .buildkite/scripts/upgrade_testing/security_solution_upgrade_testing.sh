6ad0a5d4b379698a2f9c9cc2b3e0f6db7d2c3a67
Patryk Kopycinski <patryk.kopycinski@elastic.co>
2021-12-06 19:29:37 +0100

    fix

--- .buildkite/scripts/upgrade_testing/security_solution_upgrade_testing.sh
+++ .buildkite/scripts/upgrade_testing/security_solution_upgrade_testing.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 .buildkite/scripts/lifecycle/pre_build.sh
 .buildkite/scripts/bootstrap.sh
 
-node .buildkite/scripts/upgrade_testing/security_soltution_upgrade_testing.js
+node .buildkite/scripts/upgrade_testing/security_solution_upgrade_testing.js
