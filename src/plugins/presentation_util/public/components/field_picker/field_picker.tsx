e6845643145760f52342d78c7d77c8656f34ce66
Devon Thomson <devon.thomson@elastic.co>
2021-12-06 13:58:33 -0500

    added functional tests for dashboard controls integration (#119755)
    # Conflicts:
    #	src/plugins/presentation_util/public/components/controls/control_group/component/control_group_component.tsx

--- src/plugins/presentation_util/public/components/field_picker/field_picker.tsx
+++ src/plugins/presentation_util/public/components/field_picker/field_picker.tsx
@@ -87,6 +87,7 @@ export const FieldPicker = ({
                 return (
                   <EuiFlexItem key={f.name}>
                     <FieldButton
+                      data-test-subj={`field-picker-select-${f.name}`}
                       className={classNames('presFieldPicker__fieldButton', {
                         presFieldPickerFieldButtonActive: f.name === selectedFieldName,
                       })}
