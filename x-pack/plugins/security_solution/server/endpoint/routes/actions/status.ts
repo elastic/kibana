d14c40f69b88974e6a9e8b857acec6cec29e832f
Paul Tavares <paul.tavares@elastic.co>
2021-12-06 14:08:05 -0500

    Update Host Isolation Log route handler + correct isolation status permission

--- x-pack/plugins/security_solution/server/endpoint/routes/actions/status.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/actions/status.ts
@@ -31,7 +31,7 @@ export function registerActionStatusRoutes(
       options: { authRequired: true, tags: ['access:securitySolution'] },
     },
     withEndpointAuthz(
-      { all: ['canIsolateHost'] },
+      { all: ['canUnIsolateHost'] },
       endpointContext.logFactory.get('hostIsolationStatus'),
       actionStatusRequestHandler(endpointContext)
     )
