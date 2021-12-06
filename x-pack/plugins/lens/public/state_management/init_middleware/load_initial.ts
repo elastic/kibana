d1594a8f70b168c849c9b073d4c22f13928e6e2b
Andrew Tate <andrew.tate@elastic.co>
2021-12-06 09:32:29 -0600

    replacing dynamic injection with static import

--- x-pack/plugins/lens/public/state_management/init_middleware/load_initial.ts
+++ x-pack/plugins/lens/public/state_management/init_middleware/load_initial.ts
@@ -16,6 +16,7 @@ import { getInitialDatasourceId } from '../../utils';
 import { initializeDatasources } from '../../editor_frame_service/editor_frame';
 import { LensAppServices } from '../../app_plugin/types';
 import { getEditPath, getFullPath, LENS_EMBEDDABLE_TYPE } from '../../../common/constants';
+import { inject as injectFilterReferences } from '../../../../../../src/plugins/data/common';
 import { Document } from '../../persistence';
 
 export const getPersisted = async ({
@@ -163,7 +164,7 @@ export function loadInitial(
             {}
           );
 
-          const filters = data.query.filterManager.inject(doc.state.filters, doc.references);
+          const filters = injectFilterReferences(doc.state.filters, doc.references);
           // Don't overwrite any pinned filters
           data.query.filterManager.setAppFilters(filters);
 
