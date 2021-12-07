2a0d560b27b80f6d4350b2e717babed6191d42fd
cauemarcondes <caue.marcondes@elastic.co>
2021-12-07 09:04:35 -0500

    fixing ci

--- x-pack/plugins/apm/server/deprecations/deprecations.test.ts
+++ x-pack/plugins/apm/server/deprecations/deprecations.test.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
+import { kibanaPackageJson } from '@kbn/dev-utils';
 import { GetDeprecationsContext } from '../../../../../src/core/server';
 import { CloudSetup } from '../../../cloud/server';
 import { getDeprecations } from './';
