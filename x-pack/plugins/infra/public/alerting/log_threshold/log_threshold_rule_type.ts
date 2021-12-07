785faa4b0461e56ceb9034cc802b67926adce500
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-12-07 03:33:42 -0500

    [RAC] Rename occurrences of alert_type to rule_type in Infra (#120455) (#120573)
    [RAC] Rename occurrences of alert_type to rule_type in Infra
    
    Co-authored-by: Ersin Erdal <92688503+ersin-erdal@users.noreply.github.com>

--- x-pack/plugins/infra/public/alerting/log_threshold/log_threshold_rule_type.ts
+++ x-pack/plugins/infra/public/alerting/log_threshold/log_threshold_rule_type.ts
@@ -11,11 +11,11 @@ import { ObservabilityRuleTypeModel } from '../../../../observability/public';
 import {
   LOG_DOCUMENT_COUNT_ALERT_TYPE_ID,
   PartialAlertParams,
-} from '../../../common/alerting/logs/log_threshold/types';
+} from '../../../common/alerting/logs/log_threshold';
 import { formatRuleData } from './rule_data_formatters';
 import { validateExpression } from './validation';
 
-export function createLogThresholdAlertType(): ObservabilityRuleTypeModel<PartialAlertParams> {
+export function createLogThresholdRuleType(): ObservabilityRuleTypeModel<PartialAlertParams> {
   return {
     id: LOG_DOCUMENT_COUNT_ALERT_TYPE_ID,
     description: i18n.translate('xpack.infra.logs.alertFlyout.alertDescription', {
