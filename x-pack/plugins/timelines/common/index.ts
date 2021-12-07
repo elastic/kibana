ef65e048c3e272014983968e0128b789b6e01153
FrankHassanabad <frank.hassanabad@elastic.co>
2021-12-06 19:36:13 -0700

    Added comments about when you should consider using packages

--- x-pack/plugins/timelines/common/index.ts
+++ x-pack/plugins/timelines/common/index.ts
@@ -5,6 +5,15 @@
  * 2.0.
  */
 
+// Careful of exporting anything from this file as any file(s) you export here will cause your page bundle size to increase.
+// If you're using functions/types/etc... internally or within integration tests it's best to import directly from their paths
+// than expose the functions/types/etc... here. You should _only_ expose functions/types/etc... that need to be shared with other plugins here.
+
+// When you do have to add things here you might want to consider creating a package to share with
+// other plugins instead as packages are easier to break down and you do not have to carry the cost of extra plugin weight on
+// first download since the other plugins/areas of your code can directly pull from the package in their async imports.
+// See: https://docs.elastic.dev/kibana-dev-docs/key-concepts/platform-intro#public-plugin-api
+
 export { DELETED_SECURITY_SOLUTION_DATA_VIEW } from './constants';
 
 export type {
