910de502391e8ac8ef5ac0169f7239bcd38f3e91
Sébastien Loix <sebastien.loix@elastic.co>
2021-12-07 11:40:15 +0000

    Address copy review
    Co-authored-by: debadair <debadair@elastic.co>

--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/configuration_form/configuration_form_schema.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/configuration_form/configuration_form_schema.tsx
@@ -195,7 +195,7 @@ export const configurationFormSchema: FormSchema = {
   _size: {
     enabled: {
       label: i18n.translate('xpack.idxMgmt.mappingsEditor.configuration.sizeLabel', {
-        defaultMessage: 'Index the size in bytes of the _source field',
+        defaultMessage: 'Index the _source field size in bytes',
       }),
       defaultValue: false,
     },
