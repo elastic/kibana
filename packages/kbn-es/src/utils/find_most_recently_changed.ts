6a869a34faf57d23d8c0d8ae62514324c193ecbc
Spencer <email@spalger.com>
2021-12-07 09:28:19 -0700

    [kbn-es/installers] convert to TypeScript (#120560)
    * [kbn-es/installers] convert to TypeScript
    
    * catch invalid urls passed to installArchive()

--- packages/kbn-es/src/utils/find_most_recently_changed.ts
+++ packages/kbn-es/src/utils/find_most_recently_changed.ts
@@ -6,25 +6,22 @@
  * Side Public License, v 1.
  */
 
-const path = require('path');
-const fs = require('fs');
-const glob = require('glob');
+import path from 'path';
+import fs from 'fs';
+import glob from 'glob';
 
 /**
  *  Find the most recently modified file that matches the pattern pattern
- *
- *  @param  {String} pattern absolute path with glob expressions
- *  @return {String} Absolute path
  */
-exports.findMostRecentlyChanged = function findMostRecentlyChanged(pattern) {
+export function findMostRecentlyChanged(pattern: string) {
   if (!path.isAbsolute(pattern)) {
     throw new TypeError(`Pattern must be absolute, got ${pattern}`);
   }
 
-  const ctime = (path) => fs.statSync(path).ctime.getTime();
+  const ctime = (p: string) => fs.statSync(p).ctime.getTime();
 
   return glob
     .sync(pattern)
     .sort((a, b) => ctime(a) - ctime(b))
     .pop();
-};
+}
