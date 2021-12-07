716f52d2cddc9003d7b30014dcf1a7a0626cf0d1
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2021-12-07 15:00:07 +0100

    fix: add port number to es start script

--- .buildkite/scripts/steps/functional/performance_sub_ftr.sh
+++ .buildkite/scripts/steps/functional/performance_sub_ftr.sh
@@ -11,8 +11,15 @@ cd "$XPACK_DIR"
 
 echo --- Run Performance Tests with FTR config
 
-node scripts/es snapshot &
+node scripts/es snapshot \
+  -E network.bind_host=127.0.0.1,192.168.50.1 \
+  -E discovery.type=single-node \
+  --license=trial &
+
 esPid=$!
+
+export TEST_KIBANA_URL=http://elastic:changeme@192.168.50.5:5601
+export TEST_ES_URL=http://elastic:changeme@192.168.50.1:9200
 
 # warmup round 1
 DONT_START_ES=true DISABLE_APM=true checks-reporter-with-killswitch "Run Performance Tests (warmup 1)" \
