94b7b5e1789a4e063fa7e83467018b0a1d43f22b
Marshall Main <marshall.main@elastic.co>
2021-12-08 08:16:55 -0800

    Continue overriding kibana.alert.rule.risk_score and severity for now

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/factories/utils/build_alert.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/factories/utils/build_alert.ts
@@ -158,6 +158,8 @@ export const buildAlert = (
       updated_by: updatedBy ?? '',
       type: completeRule.ruleParams.type,
       ...commonRuleParams,
+      severity: overrides?.severityOverride ?? completeRule.ruleParams.severity,
+      risk_score: overrides?.riskScoreOverride ?? completeRule.ruleParams.riskScore,
     }),
   } as unknown as RACAlert;
 };
