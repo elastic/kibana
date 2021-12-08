b04a6f7e6498bb4b1b8d321eba83b3f01ed55c64
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2021-12-08 17:17:16 +0100

    make ci jobs faster

--- .buildkite/scripts/steps/functional/performance_sub_ftr.sh
+++ .buildkite/scripts/steps/functional/performance_sub_ftr.sh
@@ -14,24 +14,30 @@ node scripts/es snapshot&
 esPid=$!
 
 export TEST_ES_URL=http://elastic:changeme@192.168.50.1:9200
+export DONT_START_ES=true
+export DISABLE_APM=true
+
+sleep 120
 
 cd "$XPACK_DIR"
 
 # warmup round 1
-DONT_START_ES=true DISABLE_APM=true checks-reporter-with-killswitch "Run Performance Tests (warmup 1)" \
+checks-reporter-with-killswitch "Run Performance Tests (warmup 1)" \
   node scripts/functional_tests \
     --debug --bail \
     --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
     --config test/performance/config.ftr.ts;
 
 # warmup round 2
-DONT_START_ES=true DISABLE_APM=true checks-reporter-with-killswitch "Run Performance Tests (warmup 2)" \
+checks-reporter-with-killswitch "Run Performance Tests (warmup 2)" \
    node scripts/functional_tests \
      --debug --bail \
      --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
      --config test/performance/config.ftr.ts;
 
-DONT_START_ES=true DISABLE_APM=false checks-reporter-with-killswitch "Run Performance Tests" \
+export DISABLE_APM=false
+
+checks-reporter-with-killswitch "Run Performance Tests" \
    node scripts/functional_tests \
      --debug --bail \
      --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
