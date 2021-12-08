052b3e2266ecc34afb9b388e65e73428270d4daf
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2021-12-08 14:30:35 +0100

    Add unit test

--- x-pack/plugins/security_solution/public/hosts/components/common/host_risk_score.test.tsx
+++ x-pack/plugins/security_solution/public/hosts/components/common/host_risk_score.test.tsx
@@ -11,13 +11,92 @@ import { HostRiskSeverity } from '../../../../common/search_strategy';
 import { TestProviders } from '../../../common/mock';
 import { HostRiskScore } from './host_risk_score';
 
+import { EuiHealth, EuiHealthProps } from '@elastic/eui';
+
+import { euiThemeVars } from '@kbn/ui-shared-deps-src/theme';
+
+jest.mock('@elastic/eui', () => {
+  const original = jest.requireActual('@elastic/eui');
+  return {
+    ...jest.requireActual('@elastic/eui'),
+    EuiHealth: jest.fn((props: EuiHealthProps) => <original.EuiHealth {...props} />),
+  };
+});
+
 describe('HostRiskScore', () => {
-  it('renders', () => {
+  const context = {};
+  it('renders critical severity risk score', () => {
+    const { container } = render(
+      <TestProviders>
+        <HostRiskScore severity={HostRiskSeverity.critical} />
+      </TestProviders>
+    );
+
+    expect(container).toHaveTextContent(HostRiskSeverity.critical);
+
+    expect(EuiHealth as jest.Mock).toHaveBeenLastCalledWith(
+      expect.objectContaining({ color: euiThemeVars.euiColorDanger }),
+      context
+    );
+  });
+
+  it('renders hight severity risk score', () => {
     const { container } = render(
       <TestProviders>
         <HostRiskScore severity={HostRiskSeverity.high} />
       </TestProviders>
     );
+
     expect(container).toHaveTextContent(HostRiskSeverity.high);
+
+    expect(EuiHealth as jest.Mock).toHaveBeenLastCalledWith(
+      expect.objectContaining({ color: euiThemeVars.euiColorVis9_behindText }),
+      context
+    );
+  });
+
+  it('renders moderate severity risk score', () => {
+    const { container } = render(
+      <TestProviders>
+        <HostRiskScore severity={HostRiskSeverity.moderate} />
+      </TestProviders>
+    );
+
+    expect(container).toHaveTextContent(HostRiskSeverity.moderate);
+
+    expect(EuiHealth as jest.Mock).toHaveBeenLastCalledWith(
+      expect.objectContaining({ color: euiThemeVars.euiColorWarning }),
+      context
+    );
+  });
+
+  it('renders low severity risk score', () => {
+    const { container } = render(
+      <TestProviders>
+        <HostRiskScore severity={HostRiskSeverity.low} />
+      </TestProviders>
+    );
+
+    expect(container).toHaveTextContent(HostRiskSeverity.low);
+
+    expect(EuiHealth as jest.Mock).toHaveBeenLastCalledWith(
+      expect.objectContaining({ color: euiThemeVars.euiColorVis0 }),
+      context
+    );
+  });
+
+  it('renders unknown severity risk score', () => {
+    const { container } = render(
+      <TestProviders>
+        <HostRiskScore severity={HostRiskSeverity.unknown} />
+      </TestProviders>
+    );
+
+    expect(container).toHaveTextContent(HostRiskSeverity.unknown);
+
+    expect(EuiHealth as jest.Mock).toHaveBeenLastCalledWith(
+      expect.objectContaining({ color: euiThemeVars.euiColorMediumShade }),
+      context
+    );
   });
 });
