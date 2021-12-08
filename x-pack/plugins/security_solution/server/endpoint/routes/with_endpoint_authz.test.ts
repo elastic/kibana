ee712d28af7634042de2ca7e028d6e62fc6a42ab
Paul Tavares <paul.tavares@elastic.co>
2021-12-08 16:12:34 -0500

    Add tests for `withEndpointAuthz`

--- x-pack/plugins/security_solution/server/endpoint/routes/with_endpoint_authz.test.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/with_endpoint_authz.test.ts
@@ -5,15 +5,19 @@
  * 2.0.
  */
 
-import { httpServerMock } from '../../../../../../src/core/server/mocks';
+import { httpServerMock, loggingSystemMock } from '../../../../../../src/core/server/mocks';
 import { RequestHandler } from 'kibana/server';
 import { requestContextMock } from '../../lib/detection_engine/routes/__mocks__';
+import { EndpointApiNeededAuthz, withEndpointAuthz } from './with_endpoint_authz';
+import { EndpointAuthz } from '../../../common/endpoint/types/authz';
+import { EndpointAuthorizationError } from '../errors';
 
 describe('When using `withEndpointAuthz()`', () => {
   let mockRequestHandler: jest.Mocked<RequestHandler>;
   let mockContext: jest.Mocked<ReturnType<typeof requestContextMock.create>>;
   let mockRequest: ReturnType<typeof httpServerMock.createKibanaRequest>;
   let mockResponse: ReturnType<typeof httpServerMock.createResponseFactory>;
+  let logger: ReturnType<typeof loggingSystemMock.createLogger>;
 
   beforeEach(() => {
     mockRequestHandler = jest.fn(async (_context, _request, response) => {
@@ -22,17 +26,77 @@ describe('When using `withEndpointAuthz()`', () => {
     mockContext = requestContextMock.create();
     mockResponse = httpServerMock.createResponseFactory();
     mockRequest = httpServerMock.createKibanaRequest();
+    logger = loggingSystemMock.createLogger();
   });
 
-  it.todo('should turn off authz validation when using %j');
+  it.each<EndpointApiNeededAuthz[]>([
+    [{}],
+    [{ all: undefined }],
+    [{ all: [] }],
+    [{ any: undefined }],
+    [{ any: [] }],
+    [{ all: undefined, any: undefined }],
+    [{ all: [], any: [] }],
+  ])('should turn off authz validation when using %j', (neededAuthz) => {
+    withEndpointAuthz(neededAuthz, logger, mockRequestHandler);
 
-  it.todo('should allow if `all` authz are granted');
+    expect(logger.warn).toHaveBeenCalledWith(
+      expect.stringContaining('Authorization disabled for API route:')
+    );
+  });
 
-  it.todo('should forbid if any of the `all` authz is not granted');
+  it.each<[EndpointApiNeededAuthz, Partial<EndpointAuthz>]>([
+    [{ all: ['canAccessEndpointManagement', 'canCreateArtifactsByPolicy'] }, {}],
+    [
+      { any: ['canAccessEndpointManagement', 'canCreateArtifactsByPolicy'] },
+      { canCreateArtifactsByPolicy: false },
+    ],
+    [
+      {
+        all: ['canAccessEndpointManagement'],
+        any: ['canCreateArtifactsByPolicy', 'canIsolateHost'],
+      },
+      { canCreateArtifactsByPolicy: false },
+    ],
+  ])('should grant access when needed authz is %j', (neededAuthz, authzOverrides) => {
+    Object.assign(mockContext.securitySolution.endpointAuthz, authzOverrides);
+    withEndpointAuthz(neededAuthz, logger, mockRequestHandler)(
+      mockContext,
+      mockRequest,
+      mockResponse
+    );
 
-  it.todo('should allow if at least one in the `any` list of authz is granted');
+    expect(mockRequestHandler).toHaveBeenCalled();
+  });
 
-  it.todo('should forbid if none in the `any` list of authz is granted');
+  it.each<[EndpointApiNeededAuthz, Partial<EndpointAuthz>]>([
+    [
+      { all: ['canAccessEndpointManagement', 'canCreateArtifactsByPolicy'] },
+      { canCreateArtifactsByPolicy: false },
+    ],
+    [
+      { any: ['canAccessEndpointManagement', 'canCreateArtifactsByPolicy'] },
+      { canAccessEndpointManagement: false, canCreateArtifactsByPolicy: false },
+    ],
+    [
+      {
+        all: ['canAccessEndpointManagement'],
+        any: ['canCreateArtifactsByPolicy'],
+      },
+      { canCreateArtifactsByPolicy: false },
+    ],
+  ])('should deny access when not authorized for %j', (neededAuthz, authzOverrides) => {
+    Object.assign(mockContext.securitySolution.endpointAuthz, authzOverrides);
 
-  it.todo('should forbid if `all` is allowed, but none in the `any` list is granted');
+    withEndpointAuthz(neededAuthz, logger, mockRequestHandler)(
+      mockContext,
+      mockRequest,
+      mockResponse
+    );
+
+    expect(mockRequestHandler).not.toHaveBeenCalled();
+    expect(mockResponse.forbidden).toHaveBeenCalledWith({
+      body: expect.any(EndpointAuthorizationError),
+    });
+  });
 });
