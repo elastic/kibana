b2152fd901cbbf609fa5c2d9ec0793be73accbbe
Brian Seeders <brian.seeders@elastic.co>
2021-12-09 12:34:32 -0500

    Add comment

--- packages/kbn-test/src/jest/utils/enzyme_helpers.tsx
+++ packages/kbn-test/src/jest/utils/enzyme_helpers.tsx
@@ -47,6 +47,7 @@ function getOptions(context = {}, childContextTypes = {}, props = {}) {
  * When using @kbn/i18n `injectI18n` on components, props.intl is required.
  */
 // This function is exported solely to fix the types output in TS 4.5.2, likely a bug
+// Otherwise, InjectedIntl is missing from the output
 export function nodeWithIntlProp<T>(
   node: ReactElement<T>
 ): ReactElement<T & { intl: InjectedIntl }> {
