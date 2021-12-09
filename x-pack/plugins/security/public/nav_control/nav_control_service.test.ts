138ab580630879e52f6b63422497591a454263ab
Greg Thompson <thompson.glowe@gmail.com>
2021-12-09 11:18:54 -0600

    snapshots

--- x-pack/plugins/security/public/nav_control/nav_control_service.test.ts
+++ x-pack/plugins/security/public/nav_control/nav_control_service.test.ts
@@ -87,6 +87,7 @@ describe('SecurityNavControlService', () => {
                       aria-label="some-user"
                       class="euiAvatar euiAvatar--s euiAvatar--user"
                       data-test-subj="userMenuAvatar"
+                      role="img"
                       style="background-color: rgb(255, 126, 98); color: rgb(0, 0, 0);"
                       title="some-user"
                     >
