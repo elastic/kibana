69cbd002f4878c669530e44185a09c49ad4e0d06
Dzmitry Tamashevich <diaamnj@mail.ru>
2021-12-13 13:48:49 +0300

    [Discover] add unit tests

---
+++ src/plugins/discover/public/utils/with_query_params.test.tsx
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React, { ReactElement } from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { mountWithIntl } from '@kbn/test/jest';
+import { withQueryParams } from './with_query_params';
+import { DiscoverServices } from '../build_services';
+
+const mountComponent = (children: ReactElement, query = '') => {
+  const history = createMemoryHistory({
+    initialEntries: ['/' + query],
+  });
+  return mountWithIntl(<Router history={history}>{children}</Router>);
+};
+
+describe('withQueryParams', () => {
+  it('should display error message, when query does not contain required parameters', () => {
+    const Component = withQueryParams(() => <div />, ['id', 'query']);
+    const component = mountComponent(<Component services={{} as DiscoverServices} />);
+
+    expect(component.html()).toContain('Query should contain id, query.');
+  });
+
+  it('should not display error message, when query contain required parameters', () => {
+    const Component = withQueryParams(() => <div />, ['id', 'query']);
+    const component = mountComponent(
+      <Component services={{} as DiscoverServices} />,
+      '?id=one&query=another'
+    );
+
+    expect(component.html()).not.toContain('Query should contain id, query.');
+  });
+});
