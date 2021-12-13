559e325c6b1a6942841ac02fd180a293f22c0fa3
Marta Bondyra <marta.bondyra@elastic.co>
2021-12-13 10:35:49 +0100

    one option changed

--- x-pack/test/functional/apps/lens/gauge.ts
+++ x-pack/test/functional/apps/lens/gauge.ts
@@ -80,7 +80,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
       await PageObjects.lens.openVisualOptions();
       await PageObjects.lens.retrySetValue('lnsToolbarGaugeLabelMajor', 'custom title');
-      await PageObjects.lens.retrySetValue('lnsToolbarGaugeLabelMinor-select', 'custom');
+      await PageObjects.lens.retrySetValue('lnsToolbarGaugeLabelMinor-select', 'custom', {});
       await PageObjects.lens.retrySetValue('lnsToolbarGaugeLabelMinor', 'custom subtitle');
 
       await PageObjects.lens.waitForVisualization();
