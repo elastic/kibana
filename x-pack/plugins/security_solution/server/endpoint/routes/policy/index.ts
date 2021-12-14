9b156973904f4e1a29d2aa4497b1671070aa6ccf
Candace Park <56409205+parkiino@users.noreply.github.com>
2021-12-14 16:51:01 -0500

    [Security Solution][Endpoint][Admin][Policy List] GET endpoint package policy api (#119545)

--- x-pack/plugins/security_solution/server/endpoint/routes/policy/index.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/policy/index.ts
@@ -10,10 +10,16 @@ import { EndpointAppContext } from '../../types';
 import {
   GetPolicyResponseSchema,
   GetAgentPolicySummaryRequestSchema,
+  GetEndpointPackagePolicyRequestSchema,
 } from '../../../../common/endpoint/schema/policy';
-import { getHostPolicyResponseHandler, getAgentPolicySummaryHandler } from './handlers';
+import {
+  getHostPolicyResponseHandler,
+  getAgentPolicySummaryHandler,
+  getPolicyListHandler,
+} from './handlers';
 import {
   AGENT_POLICY_SUMMARY_ROUTE,
+  BASE_POLICY_ROUTE,
   BASE_POLICY_RESPONSE_ROUTE,
 } from '../../../../common/endpoint/constants';
 import { withEndpointAuthz } from '../with_endpoint_authz';
@@ -47,5 +53,14 @@ export function registerPolicyRoutes(router: IRouter, endpointAppContext: Endpoi
       logger,
       getAgentPolicySummaryHandler(endpointAppContext)
     )
+  );
+
+  router.get(
+    {
+      path: BASE_POLICY_ROUTE,
+      validate: GetEndpointPackagePolicyRequestSchema,
+      options: { authRequired: true },
+    },
+    getPolicyListHandler(endpointAppContext)
   );
 }
