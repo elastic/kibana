124f057fc5be980bc55fee5991b211db4f0a71f4
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2021-12-14 10:15:55 -0500

    [8.0] [Osquery] Add Osquery cypress tests to buildkite (#115902) (#120443)
    * [Osquery] Add Osquery cypress tests to buildkite (#115902)
    
    * hardcode agent version
    
    Co-authored-by: Patryk Kopyciński <patryk.kopycinski@elastic.co>
    Co-authored-by: Patryk Kopycinski <contact@patrykkopycinski.com>

--- x-pack/plugins/osquery/cypress/screens/fleet.ts
+++ x-pack/plugins/osquery/cypress/screens/fleet.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
-export async function getLatestVersion(): Promise<string> {
-  return '8.0.0-SNAPSHOT';
-}
+export const ADD_AGENT_BUTTON = 'addAgentButton';
+
+export const AGENT_POLICIES_TAB = 'fleet-agent-policies-tab';
+export const ENROLLMENT_TOKENS_TAB = 'fleet-enrollment-tokens-tab';
