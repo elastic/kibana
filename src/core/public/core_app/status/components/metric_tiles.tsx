02da6eabda32d78fef448e3bbae16709e5779445
Christiane Heiligers <christiane.heiligers@elastic.co>
2021-12-15 11:17:22 -0700

    Splits metric tiles into isolated components

--- src/core/public/core_app/status/components/metric_tiles.tsx
+++ src/core/public/core_app/status/components/metric_tiles.tsx
@@ -13,7 +13,7 @@ import { DataType, formatNumber, Metric } from '../lib';
 /*
  * Displays metadata for a metric.
  */
-export const MetricCardFooter: FunctionComponent<{
+const MetricCardFooter: FunctionComponent<{
   title: string;
   description: string;
 }> = ({ title, description }) => {
@@ -28,6 +28,60 @@ export const MetricCardFooter: FunctionComponent<{
   );
 };
 
+const DelayMetricTile: FunctionComponent<{ metric: Metric }> = ({ metric }) => {
+  const { name, meta } = metric;
+  return (
+    <EuiCard
+      data-test-subj={`serverMetric-${formatMetricId(name)}`}
+      title={formatMetric(metric)}
+      textAlign="left"
+      description={`${name} avg`}
+      footer={
+        meta?.value && (
+          <MetricCardFooter
+            title={formatDelayFooterTitle(meta.value, meta.type)}
+            description={meta.description}
+          />
+        )
+      }
+    />
+  );
+};
+
+const LoadMetricTile: FunctionComponent<{ metric: Metric }> = ({ metric }) => {
+  const { name, meta } = metric;
+  return (
+    <EuiCard
+      data-test-subj={`serverMetric-${formatMetricId(name)}`}
+      title={formatMetric(metric)}
+      textAlign="left"
+      description={name}
+      footer={meta && <MetricCardFooter title={meta.title} description={meta.description} />}
+    />
+  );
+};
+
+const ResponseTimeMetric: FunctionComponent<{ metric: Metric }> = ({ metric }) => {
+  const { name, meta } = metric;
+  return (
+    <EuiCard
+      data-test-subj={`serverMetric-${formatMetricId(name)}`}
+      title={formatMetric(metric)}
+      textAlign="left"
+      description={name}
+      footer={
+        meta?.value &&
+        Array.isArray(meta.value) && (
+          <MetricCardFooter
+            title={formatNumber(meta.value[0], meta.type)}
+            description={meta.description}
+          />
+        )
+      }
+    />
+  );
+};
+
 /*
  * Displays a metric with the correct format.
  */
@@ -35,58 +89,15 @@ export const MetricTile: FunctionComponent<{ metric: Metric }> = ({ metric }) =>
   const { name } = metric;
   switch (name) {
     case 'Delay':
-      return (
-        <EuiCard
-          data-test-subj={`serverMetric-${formatMetricId(metric)}`}
-          title={formatMetric(metric)}
-          textAlign="left"
-          description={`${name} avg`}
-          footer={
-            metric.meta?.value && (
-              <MetricCardFooter
-                title={formatDelayFooterTitle(metric.meta.value, metric.meta.type)}
-                description={metric.meta.description}
-              />
-            )
-          }
-        />
-      );
+      return <DelayMetricTile metric={metric} />;
     case 'Load':
-      return (
-        <EuiCard
-          data-test-subj={`serverMetric-${formatMetricId(metric)}`}
-          title={formatMetric(metric)}
-          textAlign="left"
-          description={name}
-          footer={
-            metric.meta && (
-              <MetricCardFooter title={metric.meta.title} description={metric.meta.description} />
-            )
-          }
-        />
-      );
+      return <LoadMetricTile metric={metric} />;
     case 'Response time avg':
-      return (
-        <EuiCard
-          data-test-subj={`serverMetric-${formatMetricId(metric)}`}
-          title={formatMetric(metric)}
-          textAlign="left"
-          description={name}
-          footer={
-            metric.meta?.value &&
-            Array.isArray(metric.meta.value) && (
-              <MetricCardFooter
-                title={formatNumber(metric.meta.value[0], metric.meta.type)}
-                description={metric.meta.description}
-              />
-            )
-          }
-        />
-      );
+      return <ResponseTimeMetric metric={metric} />;
     default:
       return (
         <EuiCard
-          data-test-subj={`serverMetric-${formatMetricId(metric)}`}
+          data-test-subj={`serverMetric-${formatMetricId(name)}`}
           textAlign="left"
           title={formatMetric(metric)}
           description={name}
@@ -108,12 +119,14 @@ export const MetricTiles: FunctionComponent<{ metrics: Metric[] }> = ({ metrics 
   </EuiFlexGrid>
 );
 
+// formatting helper functions
+
 const formatMetric = ({ value, type }: Metric) => {
   const metrics = Array.isArray(value) ? value : [value];
   return metrics.map((metric) => formatNumber(metric, type)).join(', ');
 };
 
-const formatMetricId = ({ name }: Metric) => {
+const formatMetricId = (name: Metric['name']) => {
   return name.toLowerCase().replace(/[ ]+/g, '-');
 };
 
