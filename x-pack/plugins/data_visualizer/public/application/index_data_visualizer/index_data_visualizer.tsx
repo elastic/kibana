9737faf82302b351c53adb7306d739f9ca11fb4e
Quynh Nguyen <quynh.nguyen@elastic.co>
2021-12-15 09:16:21 -0600

    Fix linting

--- x-pack/plugins/data_visualizer/public/application/index_data_visualizer/index_data_visualizer.tsx
+++ x-pack/plugins/data_visualizer/public/application/index_data_visualizer/index_data_visualizer.tsx
@@ -69,191 +69,192 @@ export const getLocatorParams = (params: {
   return locatorParams;
 };
 
-export const DataVisualizerUrlStateContextProvider: FC<DataVisualizerUrlStateContextProviderProps> =
-  ({ IndexDataVisualizerComponent, additionalLinks }) => {
-    const { services } = useDataVisualizerKibana();
-    const {
-      data: { indexPatterns, search },
-      savedObjects: { client: savedObjectsClient },
-      notifications: { toasts },
-    } = services;
-    const history = useHistory();
-    const { search: searchString } = useLocation();
+export const DataVisualizerUrlStateContextProvider: FC<
+  DataVisualizerUrlStateContextProviderProps
+> = ({ IndexDataVisualizerComponent, additionalLinks }) => {
+  const { services } = useDataVisualizerKibana();
+  const {
+    data: { indexPatterns, search },
+    savedObjects: { client: savedObjectsClient },
+    notifications: { toasts },
+  } = services;
+  const history = useHistory();
+  const { search: searchString } = useLocation();
 
-    const [currentIndexPattern, setCurrentIndexPattern] = useState<DataView | undefined>(undefined);
-    const [currentSavedSearch, setCurrentSavedSearch] = useState<SimpleSavedObject<unknown> | null>(
-      null
-    );
+  const [currentIndexPattern, setCurrentIndexPattern] = useState<DataView | undefined>(undefined);
+  const [currentSavedSearch, setCurrentSavedSearch] = useState<SimpleSavedObject<unknown> | null>(
+    null
+  );
 
-    const [currentSessionId, setCurrentSessionId] = useState<string | undefined>(undefined);
+  const [currentSessionId, setCurrentSessionId] = useState<string | undefined>(undefined);
 
-    useEffect(() => {
-      if (search.session) {
-        search.session.enableStorage({
-          getName: async () => {
-            // return the name you want to give the saved Search Session
-            return `Data visualizer`;
-          },
-          getLocatorData: async () => {
-            return {
-              id: DATA_VISUALIZER_APP_LOCATOR,
-              initialState: getLocatorParams({
-                ...services,
-                searchString,
-                indexPatternId: currentIndexPattern?.id,
-                savedSearchId: currentSavedSearch?.id,
-                shouldRestoreSearchSession: false,
-                searchSessionId: search.session.getSessionId(),
-              }),
-              restoreState: getLocatorParams({
-                ...services,
-                searchString,
-                indexPatternId: currentIndexPattern?.id,
-                savedSearchId: currentSavedSearch?.id,
-                shouldRestoreSearchSession: true,
-                searchSessionId: search.session.getSessionId(),
-              }),
-            };
-          },
+  useEffect(() => {
+    if (search.session) {
+      search.session.enableStorage({
+        getName: async () => {
+          // return the name you want to give the saved Search Session
+          return `Data visualizer`;
+        },
+        getLocatorData: async () => {
+          return {
+            id: DATA_VISUALIZER_APP_LOCATOR,
+            initialState: getLocatorParams({
+              ...services,
+              searchString,
+              indexPatternId: currentIndexPattern?.id,
+              savedSearchId: currentSavedSearch?.id,
+              shouldRestoreSearchSession: false,
+              searchSessionId: search.session.getSessionId(),
+            }),
+            restoreState: getLocatorParams({
+              ...services,
+              searchString,
+              indexPatternId: currentIndexPattern?.id,
+              savedSearchId: currentSavedSearch?.id,
+              shouldRestoreSearchSession: true,
+              searchSessionId: search.session.getSessionId(),
+            }),
+          };
+        },
+      });
+    }
+
+    const urlState = parseUrlState(searchString);
+
+    if (urlState.searchSessionId !== undefined && urlState.searchSessionId !== currentSessionId) {
+      search.session?.restore(urlState.searchSessionId);
+      setCurrentSessionId(urlState.searchSessionId);
+    } else {
+      const newSessionId = search.session?.start();
+      setCurrentSessionId(newSessionId);
+    }
+    return () => {
+      search.session.clear();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search.session, searchString]);
+
+  useEffect(() => {
+    const prevSearchString = searchString;
+    const parsedQueryString = parse(prevSearchString, { sort: false });
+
+    const getIndexPattern = async () => {
+      if (typeof parsedQueryString?.savedSearchId === 'string') {
+        const savedSearchId = parsedQueryString.savedSearchId;
+        try {
+          const savedSearch = await savedObjectsClient.get('search', savedSearchId);
+          const indexPatternId = savedSearch.references.find(
+            (ref) => ref.type === 'index-pattern'
+          )?.id;
+          if (indexPatternId !== undefined && savedSearch) {
+            try {
+              const indexPattern = await indexPatterns.get(indexPatternId);
+              setCurrentSavedSearch(savedSearch);
+              setCurrentIndexPattern(indexPattern);
+            } catch (e) {
+              toasts.addError(e, {
+                title: i18n.translate('xpack.dataVisualizer.index.dataViewErrorMessage', {
+                  defaultMessage: 'Error finding data view',
+                }),
+              });
+            }
+          }
+        } catch (e) {
+          toasts.addError(e, {
+            title: i18n.translate('xpack.dataVisualizer.index.savedSearchErrorMessage', {
+              defaultMessage: 'Error retrieving saved search {savedSearchId}',
+              values: { savedSearchId },
+            }),
+          });
+        }
+      }
+
+      if (typeof parsedQueryString?.index === 'string') {
+        const indexPattern = await indexPatterns.get(parsedQueryString.index);
+        setCurrentIndexPattern(indexPattern);
+      }
+    };
+    getIndexPattern();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [savedObjectsClient, toasts, indexPatterns]);
+
+  const setUrlState: SetUrlState = useCallback(
+    (
+      accessor: Accessor,
+      attribute: string | Dictionary<any>,
+      value?: any,
+      replaceState?: boolean
+    ) => {
+      const prevSearchString = searchString;
+      const urlState = parseUrlState(prevSearchString);
+      const parsedQueryString = parse(prevSearchString, { sort: false });
+
+      if (!Object.prototype.hasOwnProperty.call(urlState, accessor)) {
+        urlState[accessor] = {};
+      }
+
+      if (typeof attribute === 'string') {
+        if (isEqual(getNestedProperty(urlState, `${accessor}.${attribute}`), value)) {
+          return prevSearchString;
+        }
+
+        urlState[accessor][attribute] = value;
+      } else {
+        const attributes = attribute;
+        Object.keys(attributes).forEach((a) => {
+          urlState[accessor][a] = attributes[a];
         });
       }
 
-      const urlState = parseUrlState(searchString);
+      try {
+        const oldLocationSearchString = stringify(parsedQueryString, {
+          sort: false,
+          encode: false,
+        });
 
-      if (urlState.searchSessionId !== undefined && urlState.searchSessionId !== currentSessionId) {
-        search.session?.restore(urlState.searchSessionId);
-        setCurrentSessionId(urlState.searchSessionId);
-      } else {
-        const newSessionId = search.session?.start();
-        setCurrentSessionId(newSessionId);
+        Object.keys(urlState).forEach((a) => {
+          if (isRisonSerializationRequired(a)) {
+            parsedQueryString[a] = encode(urlState[a]);
+          } else {
+            parsedQueryString[a] = urlState[a];
+          }
+        });
+        const newLocationSearchString = stringify(parsedQueryString, {
+          sort: false,
+          encode: false,
+        });
+
+        if (oldLocationSearchString !== newLocationSearchString) {
+          const newSearchString = stringify(parsedQueryString, { sort: false });
+          if (replaceState) {
+            history.replace({ search: newSearchString });
+          } else {
+            history.push({ search: newSearchString });
+          }
+        }
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Could not save url state', error);
       }
-      return () => {
-        search.session.clear();
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [search.session, searchString]);
+    },
+    [history, searchString]
+  );
 
-    useEffect(() => {
-      const prevSearchString = searchString;
-      const parsedQueryString = parse(prevSearchString, { sort: false });
-
-      const getIndexPattern = async () => {
-        if (typeof parsedQueryString?.savedSearchId === 'string') {
-          const savedSearchId = parsedQueryString.savedSearchId;
-          try {
-            const savedSearch = await savedObjectsClient.get('search', savedSearchId);
-            const indexPatternId = savedSearch.references.find(
-              (ref) => ref.type === 'index-pattern'
-            )?.id;
-            if (indexPatternId !== undefined && savedSearch) {
-              try {
-                const indexPattern = await indexPatterns.get(indexPatternId);
-                setCurrentSavedSearch(savedSearch);
-                setCurrentIndexPattern(indexPattern);
-              } catch (e) {
-                toasts.addError(e, {
-                  title: i18n.translate('xpack.dataVisualizer.index.dataViewErrorMessage', {
-                    defaultMessage: 'Error finding data view',
-                  }),
-                });
-              }
-            }
-          } catch (e) {
-            toasts.addError(e, {
-              title: i18n.translate('xpack.dataVisualizer.index.savedSearchErrorMessage', {
-                defaultMessage: 'Error retrieving saved search {savedSearchId}',
-                values: { savedSearchId },
-              }),
-            });
-          }
-        }
-
-        if (typeof parsedQueryString?.index === 'string') {
-          const indexPattern = await indexPatterns.get(parsedQueryString.index);
-          setCurrentIndexPattern(indexPattern);
-        }
-      };
-      getIndexPattern();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [savedObjectsClient, toasts, indexPatterns]);
-
-    const setUrlState: SetUrlState = useCallback(
-      (
-        accessor: Accessor,
-        attribute: string | Dictionary<any>,
-        value?: any,
-        replaceState?: boolean
-      ) => {
-        const prevSearchString = searchString;
-        const urlState = parseUrlState(prevSearchString);
-        const parsedQueryString = parse(prevSearchString, { sort: false });
-
-        if (!Object.prototype.hasOwnProperty.call(urlState, accessor)) {
-          urlState[accessor] = {};
-        }
-
-        if (typeof attribute === 'string') {
-          if (isEqual(getNestedProperty(urlState, `${accessor}.${attribute}`), value)) {
-            return prevSearchString;
-          }
-
-          urlState[accessor][attribute] = value;
-        } else {
-          const attributes = attribute;
-          Object.keys(attributes).forEach((a) => {
-            urlState[accessor][a] = attributes[a];
-          });
-        }
-
-        try {
-          const oldLocationSearchString = stringify(parsedQueryString, {
-            sort: false,
-            encode: false,
-          });
-
-          Object.keys(urlState).forEach((a) => {
-            if (isRisonSerializationRequired(a)) {
-              parsedQueryString[a] = encode(urlState[a]);
-            } else {
-              parsedQueryString[a] = urlState[a];
-            }
-          });
-          const newLocationSearchString = stringify(parsedQueryString, {
-            sort: false,
-            encode: false,
-          });
-
-          if (oldLocationSearchString !== newLocationSearchString) {
-            const newSearchString = stringify(parsedQueryString, { sort: false });
-            if (replaceState) {
-              history.replace({ search: newSearchString });
-            } else {
-              history.push({ search: newSearchString });
-            }
-          }
-        } catch (error) {
-          // eslint-disable-next-line no-console
-          console.error('Could not save url state', error);
-        }
-      },
-      [history, searchString]
-    );
-
-    return (
-      <UrlStateContextProvider value={{ searchString, setUrlState }}>
-        {currentIndexPattern ? (
-          <IndexDataVisualizerComponent
-            currentIndexPattern={currentIndexPattern}
-            currentSavedSearch={currentSavedSearch}
-            additionalLinks={additionalLinks}
-            currentSessionId={currentSessionId}
-          />
-        ) : (
-          <div />
-        )}
-      </UrlStateContextProvider>
-    );
-  };
+  return (
+    <UrlStateContextProvider value={{ searchString, setUrlState }}>
+      {currentIndexPattern ? (
+        <IndexDataVisualizerComponent
+          currentIndexPattern={currentIndexPattern}
+          currentSavedSearch={currentSavedSearch}
+          additionalLinks={additionalLinks}
+          currentSessionId={currentSessionId}
+        />
+      ) : (
+        <div />
+      )}
+    </UrlStateContextProvider>
+  );
+};
 
 export const IndexDataVisualizer: FC<{ additionalLinks: ResultLink[] }> = ({ additionalLinks }) => {
   const coreStart = getCoreStart();
