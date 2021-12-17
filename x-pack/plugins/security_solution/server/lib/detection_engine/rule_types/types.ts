3c09faae6c07067cd5f3de752b53d06afcf07e8d
Marshall Main <55718608+marshallmain@users.noreply.github.com>
2021-12-17 13:23:27 -0800

    [Security Solution] Remove extra rule fields from kibana.alert.rule (#121135)
    * Remove kibana.alert.rule.risk_score and severity
    
    * Fix tests related to risk_score and severity
    
    * Make translation a template
    
    * Can't use expression in template literal
    
    * Remove commented line added by bad merge
    
    * Fix linting
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    # Conflicts:
    #	x-pack/plugins/security_solution/public/common/components/event_details/overview/__snapshots__/index.test.tsx.snap
    #	x-pack/plugins/security_solution/public/common/components/event_details/overview/index.test.tsx
    #	x-pack/plugins/security_solution/public/common/components/event_details/overview/index.tsx
    #	x-pack/plugins/security_solution/public/detections/configurations/examples/observablity_alerts/render_cell_value.tsx
    #	x-pack/plugins/security_solution/public/detections/configurations/examples/security_solution_rac/render_cell_value.tsx
    #	x-pack/plugins/timelines/server/search_strategy/timeline/factory/events/all/constants.ts

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/types.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/types.ts
@@ -9,6 +9,7 @@ import { Moment } from 'moment';
 
 import { SearchHit } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { Logger } from '@kbn/logging';
+import { ALERT_RULE_PARAMETERS } from '@kbn/rule-data-utils';
 import { ExceptionListItemSchema } from '@kbn/securitysolution-io-ts-list-types';
 
 import { AlertExecutorOptions, AlertType } from '../../../../../alerting/server';
@@ -37,6 +38,7 @@ import { ExperimentalFeatures } from '../../../../common/experimental_features';
 import { IEventLogService } from '../../../../../event_log/server';
 import { AlertsFieldMap, RulesFieldMap } from '../../../../common/field_maps';
 import { IRuleExecutionLogClient } from '../rule_execution_log';
+import { commonParamsCamelToSnake } from '../schemas/rule_converters';
 
 export interface SecurityAlertTypeReturnValue<TState extends AlertTypeState> {
   bulkCreateTimes: string[];
@@ -110,11 +112,12 @@ export type CreateSecurityRuleTypeWrapper = (
 ) => AlertType<TParams, TParams, TState, AlertInstanceState, TInstanceContext, 'default'>;
 
 export type RACAlertSignal = TypeOfFieldMap<AlertsFieldMap> & TypeOfFieldMap<RulesFieldMap>;
-export type RACAlert = Exclude<
+export type RACAlert = Omit<
   TypeOfFieldMap<TechnicalRuleFieldMap> & RACAlertSignal,
-  '@timestamp'
+  '@timestamp' | typeof ALERT_RULE_PARAMETERS
 > & {
   '@timestamp': string;
+  [ALERT_RULE_PARAMETERS]: ReturnType<typeof commonParamsCamelToSnake>;
 };
 
 export type RACSourceHit = SearchHit<RACAlert>;
