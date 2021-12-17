65efff8125cd3f4df06613f864cb3a6bd93032db
Faisal Kanout <faisal@kanout.com>
2021-12-17 18:22:49 +0100

    Fix moment and add types for  moment-duration-format

--- x-pack/plugins/infra/server/lib/alerting/log_threshold/reason_formatters.ts
+++ x-pack/plugins/infra/server/lib/alerting/log_threshold/reason_formatters.ts
@@ -6,8 +6,9 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import moment from 'moment';
-import 'moment-duration-format';
+import * as moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+momentDurationFormatSetup(moment);
 
 import {
   Comparator,
