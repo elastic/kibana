ea97f04a031f1fb2263cfe6cd8a9b006e9016beb
Ester Marti <ester.martivilaseca@elastic.co>
2021-12-20 12:42:07 +0100

    Fix alerts flyout stories

--- x-pack/plugins/observability/public/pages/alerts/components/alerts_flyout/example_data.ts
+++ x-pack/plugins/observability/public/pages/alerts/components/alerts_flyout/example_data.ts
@@ -18,6 +18,8 @@ import {
   ALERT_RULE_NAME,
   ALERT_RULE_CATEGORY,
   ALERT_RULE_PRODUCER,
+  ALERT_RULE_CONSUMER,
+  SPACE_IDS,
 } from '@kbn/rule-data-utils/technical_field_names';
 import {
   ALERT_STATUS_ACTIVE,
@@ -29,6 +31,8 @@ export const apmAlertResponseExample = [
     [ALERT_RULE_TYPE_ID]: ['apm.error_rate'],
     'service.name': ['opbeans-java'],
     [ALERT_RULE_NAME]: ['Error count threshold | opbeans-java (smith test)'],
+    [ALERT_RULE_CONSUMER]: ['apm'],
+    [SPACE_IDS]: ['default'],
     [ALERT_DURATION]: [180057000],
     [ALERT_STATUS]: [ALERT_STATUS_ACTIVE],
     [ALERT_SEVERITY]: ['warning'],
@@ -49,6 +53,8 @@ export const apmAlertResponseExample = [
     [ALERT_RULE_TYPE_ID]: ['apm.error_rate'],
     'service.name': ['opbeans-java'],
     [ALERT_RULE_NAME]: ['Error count threshold | opbeans-java (smith test)'],
+    [ALERT_RULE_CONSUMER]: ['apm'],
+    [SPACE_IDS]: ['default'],
     [ALERT_DURATION]: [2419005000],
     [ALERT_END]: ['2021-04-12T13:49:49.446Z'],
     [ALERT_STATUS]: [ALERT_STATUS_RECOVERED],
