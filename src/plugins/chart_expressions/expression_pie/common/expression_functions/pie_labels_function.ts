282c017a10263cba45070dd922dfc6c29ba80717
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2021-12-20 11:40:22 +0200

    Fixed labels.

--- src/plugins/chart_expressions/expression_pie/common/expression_functions/pie_labels_function.ts
+++ src/plugins/chart_expressions/expression_pie/common/expression_functions/pie_labels_function.ts
@@ -28,14 +28,14 @@ export const pieLabelsFunction = (): ExpressionFunctionDefinition<
   ExpressionValuePieLabels
 > => ({
   name: PIE_LABELS_FUNCTION,
-  help: i18n.translate('visTypePie.function.pieLabels.help', {
+  help: i18n.translate('expressionPie.pieLabels.function.help', {
     defaultMessage: 'Generates the pie labels object',
   }),
   type: PIE_LABELS_VALUE,
   args: {
     show: {
       types: ['boolean'],
-      help: i18n.translate('visTypePie.function.pieLabels.show.help', {
+      help: i18n.translate('expressionPie.pieLabels.function.args.pieLabels.show.help', {
         defaultMessage: 'Displays the pie labels',
       }),
       required: true,
@@ -43,34 +43,34 @@ export const pieLabelsFunction = (): ExpressionFunctionDefinition<
     position: {
       types: ['string'],
       default: 'default',
-      help: i18n.translate('visTypePie.function.pieLabels.position.help', {
+      help: i18n.translate('expressionPie.pieLabels.function.args.pieLabels.position.help', {
         defaultMessage: 'Defines the label position',
       }),
     },
     values: {
       types: ['boolean'],
-      help: i18n.translate('visTypePie.function.pieLabels.values.help', {
+      help: i18n.translate('expressionPie.pieLabels.function.args.pieLabels.values.help', {
         defaultMessage: 'Displays the values inside the slices',
       }),
       default: true,
     },
     percentDecimals: {
       types: ['number'],
-      help: i18n.translate('visTypePie.function.pieLabels.percentDecimals.help', {
+      help: i18n.translate('expressionPie.pieLabels.function.args.pieLabels.percentDecimals.help', {
         defaultMessage: 'Defines the number of decimals that will appear on the values as percent',
       }),
       default: 2,
     },
     lastLevel: {
       types: ['boolean'],
-      help: i18n.translate('visTypePie.function.pieLabels.lastLevel.help', {
+      help: i18n.translate('expressionPie.pieLabels.function.args.pieLabels.lastLevel.help', {
         defaultMessage: 'Show top level labels only',
       }),
       default: true,
     },
     truncate: {
       types: ['number'],
-      help: i18n.translate('visTypePie.function.pieLabels.truncate.help', {
+      help: i18n.translate('expressionPie.pieLabels.function.args.truncate.help', {
         defaultMessage: 'Defines the number of characters that the slice value will display',
       }),
       default: null,
@@ -78,7 +78,7 @@ export const pieLabelsFunction = (): ExpressionFunctionDefinition<
     valuesFormat: {
       types: ['string'],
       default: 'percent',
-      help: i18n.translate('visTypePie.function.pieLabels.valuesFormat.help', {
+      help: i18n.translate('expressionPie.pieLabels.function.args.valuesFormat.help', {
         defaultMessage: 'Defines the format of the values',
       }),
     },
