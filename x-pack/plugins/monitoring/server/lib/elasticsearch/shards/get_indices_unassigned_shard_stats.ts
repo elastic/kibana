90256baa68fc7a70371c6f855fa1eddb4c2195a3
klacabane <kevin.lacabane@elastic.co>
2021-12-21 00:30:27 +0100

    Revert "lint"
    This reverts commit 2ad61e406c04725bfa9bfbd08f8917d841831862.

--- x-pack/plugins/monitoring/server/lib/elasticsearch/shards/get_indices_unassigned_shard_stats.ts
+++ x-pack/plugins/monitoring/server/lib/elasticsearch/shards/get_indices_unassigned_shard_stats.ts
@@ -32,7 +32,8 @@ async function getUnassignedShardData(
   } else if (cluster.elasticsearch?.cluster?.stats?.state?.state_uuid) {
     filters.push({
       term: {
-        'elasticsearch.cluster.state.id': cluster.elasticsearch?.cluster?.stats?.state?.state_uuid,
+        'elasticsearch.cluster.state.id':
+          cluster.elasticsearch?.cluster?.stats?.state?.state_uuid,
       },
     });
   }
