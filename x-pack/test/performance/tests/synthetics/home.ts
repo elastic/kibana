ac8960e97d7599aeb79496990cd36465e95514ac
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2021-12-21 13:48:58 +0100

    run tests inside a test phases only once

--- x-pack/test/performance/tests/synthetics/home.ts
+++ x-pack/test/performance/tests/synthetics/home.ts
@@ -8,32 +8,30 @@
 import { before, journey, step } from '@elastic/synthetics';
 import { NETWORK_PROFILES } from '../../../../../test/functional/services/remote/network_profiles';
 
-for (let i = 0; i < 10; i++) {
-  journey('perf_login_and_home', async ({ page, params, client }) => {
-    before(async () => {
-      await client.send('Network.setCacheDisabled', { cacheDisabled: true });
-      await client.send('Network.emulateNetworkConditions', {
-        latency: NETWORK_PROFILES.CLOUD_USER.LATENCY,
-        downloadThroughput: NETWORK_PROFILES.CLOUD_USER.DOWNLOAD,
-        uploadThroughput: NETWORK_PROFILES.CLOUD_USER.UPLOAD,
-        offline: false,
-      });
-    });
-
-    step('Go to Kibana login page', async () => {
-      await page.goto(`${params.kibanaUrl}`, { waitUntil: 'networkidle' });
-    });
-
-    step('Login to Kibana', async () => {
-      await page.fill('[data-test-subj=loginUsername]', 'elastic', { timeout: 60 * 1000 });
-      await page.fill('[data-test-subj=loginPassword]', 'changeme');
-      await page.click('[data-test-subj=loginSubmit]');
-      await page.waitForNavigation({ waitUntil: 'networkidle' });
-    });
-
-    step('Dismiss Synthetics Notice', async () => {
-      await page.click('[data-test-subj=skipWelcomeScreen]', { timeout: 60 * 1000 });
-      await page.locator('Welcome home');
+journey('perf_login_and_home', async ({ page, params, client }) => {
+  before(async () => {
+    await client.send('Network.setCacheDisabled', { cacheDisabled: true });
+    await client.send('Network.emulateNetworkConditions', {
+      latency: NETWORK_PROFILES.CLOUD_USER.LATENCY,
+      downloadThroughput: NETWORK_PROFILES.CLOUD_USER.DOWNLOAD,
+      uploadThroughput: NETWORK_PROFILES.CLOUD_USER.UPLOAD,
+      offline: false,
     });
   });
-}
+
+  step('Go to Kibana login page', async () => {
+    await page.goto(`${params.kibanaUrl}`, { waitUntil: 'networkidle' });
+  });
+
+  step('Login to Kibana', async () => {
+    await page.fill('[data-test-subj=loginUsername]', 'elastic', { timeout: 60 * 1000 });
+    await page.fill('[data-test-subj=loginPassword]', 'changeme');
+    await page.click('[data-test-subj=loginSubmit]');
+    await page.waitForNavigation({ waitUntil: 'networkidle' });
+  });
+
+  step('Dismiss Synthetics Notice', async () => {
+    await page.click('[data-test-subj=skipWelcomeScreen]', { timeout: 60 * 1000 });
+    await page.locator('Welcome home');
+  });
+});
