ee97223a08d7cbc63bfae297a8b8e243ee6551ba
Claudio Procida <claudio.procida@elastic.co>
2021-12-21 15:17:13 +0100

    Adds test case for use_get_user_cases_permissions module

---
+++ x-pack/plugins/observability/public/hooks/use_get_user_cases_permissions.test.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { renderHook } from '@testing-library/react-hooks';
+import { applicationServiceMock } from 'src/core/public/mocks';
+import { casesFeatureId } from '../../common';
+import { useGetUserCasesPermissions } from './use_get_user_cases_permissions';
+
+let mockUseKibanaReturnValue = null;
+
+jest.mock('../utils/kibana_react', () => ({
+  __esModule: true,
+  useKibana: jest.fn(() => mockUseKibanaReturnValue),
+}));
+
+describe('useGetUserCasesPermissions', function () {
+  it('returns expected permissions when capabilities entry exists', () => {
+    mockUseKibanaReturnValue = {
+      services: {
+        application: {
+          capabilities: Object.assign(
+            {},
+            applicationServiceMock.createStartContract().capabilities,
+            {
+              [casesFeatureId]: { crud_cases: false, read_cases: true },
+            }
+          ),
+        },
+      },
+    };
+    const { result } = renderHook(() => useGetUserCasesPermissions(), {});
+    expect(result.current?.read).toBe(true);
+    expect(result.current?.crud).toBe(false);
+  });
+
+  it('returns false when capabilities entry permissions are missing', () => {
+    mockUseKibanaReturnValue = {
+      services: {
+        application: {
+          capabilities: Object.assign(
+            {},
+            applicationServiceMock.createStartContract().capabilities,
+            {
+              [casesFeatureId]: {
+                /* intentionally empty */
+              },
+            }
+          ),
+        },
+      },
+    };
+    const { result } = renderHook(() => useGetUserCasesPermissions(), {});
+    expect(result.current?.read).toBe(false);
+    expect(result.current?.crud).toBe(false);
+  });
+
+  it('returns false when capabilities entry is missing entirely', () => {
+    mockUseKibanaReturnValue = {
+      services: {
+        application: {
+          capabilities: Object.assign(
+            {},
+            applicationServiceMock.createStartContract().capabilities,
+            {}
+          ),
+        },
+      },
+    };
+    const { result } = renderHook(() => useGetUserCasesPermissions(), {});
+    expect(result.current?.read).toBe(false);
+    expect(result.current?.crud).toBe(false);
+  });
+});
