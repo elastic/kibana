510ac5cb3cf672fb2e1b0a9ef822257b71ca4024
Thom Heymann <190132+thomheymann@users.noreply.github.com>
2021-12-21 21:01:54 +0000

    Capture user logout events in audit log (#121455)
    * Audit user logout events
    
    * Capture all user initiated logout events
    
    * Added suggestions from code review

--- x-pack/plugins/security/server/audit/index.ts
+++ x-pack/plugins/security/server/audit/index.ts
@@ -10,6 +10,7 @@ export { AuditService } from './audit_service';
 export type { AuditEvent } from './audit_events';
 export {
   userLoginEvent,
+  userLogoutEvent,
   accessAgreementAcknowledgedEvent,
   httpRequestEvent,
   savedObjectEvent,
