a487dfeafd91d09b55b0316ea9555de837a66be6
Patryk Kopycinski <patryk.kopycinski@elastic.co>
2021-12-22 09:01:05 +0100

    test

--- .buildkite/scripts/steps/functional/osquery_cypress.sh
+++ .buildkite/scripts/steps/functional/osquery_cypress.sh
@@ -10,11 +10,11 @@ export JOB=kibana-osquery-cypress
 
 echo "--- Osquery Cypress tests"
 
-cd "$KIBANA_DIR"
+cd "$XPACK_DIR"
 
 checks-reporter-with-killswitch "Osquery Cypress Tests" \
   node scripts/functional_tests \
     --debug --bail \
     --config test/osquery_cypress/cli_config.ts
 
-buildkite-agent artifact upload 'target/kibana-osquery/**/*'
+buildkite-agent artifact upload '../target/kibana-osquery/**/*'
