8c70c0e6f31c963a82982159ff410c6aeb1d24f8
Kristof C <kpac.ja@gmail.com>
2021-12-22 08:28:36 -0600

    Update reporter to use username instead of full name (#121820)
    * Update reporter fto use username instead of full name
    
    * Fix test for username
    
    Co-authored-by: Kristof-Pierre Cummings <kristofpierre.cummings@elastic.co>

--- x-pack/plugins/cases/public/components/all_cases/all_cases_list.test.tsx
+++ x-pack/plugins/cases/public/components/all_cases/all_cases_list.test.tsx
@@ -174,7 +174,7 @@ describe('AllCasesListGeneric', () => {
         wrapper.find(`span[data-test-subj="case-table-column-tags-0"]`).first().prop('title')
       ).toEqual(useGetCasesMockState.data.cases[0].tags[0]);
       expect(wrapper.find(`[data-test-subj="case-table-column-createdBy"]`).first().text()).toEqual(
-        useGetCasesMockState.data.cases[0].createdBy.fullName
+        useGetCasesMockState.data.cases[0].createdBy.username
       );
       expect(
         wrapper
