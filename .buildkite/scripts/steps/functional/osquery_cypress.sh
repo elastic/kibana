b5b13f3babf4ae4cf4daef158e547da0bc3bca04
Patryk Kopycinski <patryk.kopycinski@elastic.co>
2021-12-22 01:05:06 +0100

    test

--- .buildkite/scripts/steps/functional/osquery_cypress.sh
+++ .buildkite/scripts/steps/functional/osquery_cypress.sh
@@ -5,11 +5,12 @@ set -uo pipefail
 source .buildkite/scripts/common/util.sh
 
 .buildkite/scripts/bootstrap.sh
-.buildkite/scripts/download_build_artifacts.sh
 
 export JOB=kibana-osquery-cypress
 
 echo "--- Osquery Cypress tests"
+
+cd "$KIBANA_DIR"
 
 checks-reporter-with-killswitch "Osquery Cypress Tests" \
    yarn --cwd x-pack/plugins/osquery cypress:run-as-ci
