b5830bd4e01ce01376cc4b0363ad0d12ac277055
Nathan Reese <reese.nathan@gmail.com>
2021-12-23 16:22:42 -0700

    [maps] refactor map telemetry to incrementally calculate usage stats (#121467)
    * [maps] refactor map telemetry to incrementally calculate usage stats
    
    * move to folder and break out types
    
    * map stat tests
    
    * clean up
    
    * tslint
    
    * index pattern stats
    
    * indexPatternStatsCollector test
    
    * add test for findMaps
    
    * add API intergration test
    
    * eslint
    
    * tslint api test
    
    * cleanup to avoid storing any arrays in collector
    
    * move kibana import to index to issues where saved objects not read
    
    * load kibana objects first
    
    * set refreshCache
    
    * run stats test first
    
    * remove where kibana archive unloaded in migrations test
    
    * run stats tests first so there are no side effects
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/maps/server/maps_telemetry/index_pattern_stats/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export { IndexPatternStatsCollector } from './index_pattern_stats_collector';
+export type { IndexPatternStats } from './types';
