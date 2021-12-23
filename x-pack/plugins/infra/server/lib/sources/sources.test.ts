30cffea5d0e9b546b2ca425400244f12382cfbd7
Chris Cowan <chris@elastic.co>
2021-12-23 10:20:59 -0700

    Fixing integration test and config mock

--- x-pack/plugins/infra/server/lib/sources/sources.test.ts
+++ x-pack/plugins/infra/server/lib/sources/sources.test.ts
@@ -109,6 +109,9 @@ describe('the InfraSources lib', () => {
 });
 
 const createMockStaticConfiguration = (sources: any) => ({
+  alerting: {
+    group_by_page_size: 10000,
+  },
   enabled: true,
   inventory: {
     compositeSize: 2000,
