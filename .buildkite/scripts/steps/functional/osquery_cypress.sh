ada9278c80b66952e49e595a28a68aefd66a9a84
Patryk Kopycinski <patryk.kopycinski@elastic.co>
2021-12-27 14:48:49 +0100

    env

--- .buildkite/scripts/steps/functional/osquery_cypress.sh
+++ .buildkite/scripts/steps/functional/osquery_cypress.sh
@@ -12,6 +12,9 @@ export CACHE_DIR="$HOME/.kibana"
 export GCS_UPLOAD_PREFIX="FAKE_GCS_UPLOAD_PREFIX"
 export CI="false"
 export DEBUG="code-coverage"
+export NODE_OPTIONS=--max_old_space_size=8192
+export CODE_COVERAGE=1
+
 source .buildkite/scripts/common/setup_node.sh
 
 yarn kbn bootstrap
