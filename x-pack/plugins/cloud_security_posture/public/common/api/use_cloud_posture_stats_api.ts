317b17e8b0207469c03d2dde03156db4953e7bb3
Jordan <51442161+JordanSh@users.noreply.github.com>
2022-01-03 18:00:23 +0200

    fixing-constant-name-typo (#80)

--- x-pack/plugins/cloud_security_posture/public/common/api/use_cloud_posture_stats_api.ts
+++ x-pack/plugins/cloud_security_posture/public/common/api/use_cloud_posture_stats_api.ts
@@ -8,9 +8,9 @@
 import { useQuery } from 'react-query';
 import { useKibana } from '../../../../../../src/plugins/kibana_react/public';
 import { CloudPostureStats } from '../../../common/types';
-import { STATS_ROUTH_PATH } from '../../../common/constants';
+import { STATS_ROUTE_PATH } from '../../../common/constants';
 
 export const useCloudPostureStatsApi = () => {
   const { http } = useKibana().services;
-  return useQuery(['csp_dashboard_stats'], () => http!.get<CloudPostureStats>(STATS_ROUTH_PATH));
+  return useQuery(['csp_dashboard_stats'], () => http!.get<CloudPostureStats>(STATS_ROUTE_PATH));
 };
