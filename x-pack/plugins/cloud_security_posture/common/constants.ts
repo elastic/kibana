7cad78b71dc9f623f72d6c1fc483e94b742742d9
CohenIdo <90558359+CohenIdo@users.noreply.github.com>
2022-01-03 15:02:04 +0200

    change index pattern from kubebeat* to logs-k8s_cis* (#79)

--- x-pack/plugins/cloud_security_posture/common/constants.ts
+++ x-pack/plugins/cloud_security_posture/common/constants.ts
@@ -4,10 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-export const CSP_KUBEBEAT_INDEX_NAME = 'kubebeat*';
+export const CSP_KUBEBEAT_INDEX_NAME = 'logs-k8s_cis*';
 export const CSP_FINDINGS_INDEX_NAME = 'findings';
 export const STATS_ROUTH_PATH = '/api/csp/stats';
 export const FINDINGS_ROUTH_PATH = '/api/csp/finding';
-export const AGENT_LOGS_INDEX = 'kubebeat*';
+export const AGENT_LOGS_INDEX = 'logs-k8s_cis*';
 export const RULE_PASSED = `passed`;
 export const RULE_FAILED = `failed`;
