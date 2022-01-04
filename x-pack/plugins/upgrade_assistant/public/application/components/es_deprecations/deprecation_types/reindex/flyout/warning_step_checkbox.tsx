af0b1cf09c00ed9a227314ed4fb02936e890b7d7
Ignacio Rivas <rivasign@gmail.com>
2022-01-04 15:41:22 +0100

    Fix linter issues

--- x-pack/plugins/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/reindex/flyout/warning_step_checkbox.tsx
+++ x-pack/plugins/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/reindex/flyout/warning_step_checkbox.tsx
@@ -161,33 +161,34 @@ export const DeprecatedSettingWarningCheckbox: React.FunctionComponent<WarningCh
   );
 };
 
-export const ReplaceIndexWithAliasWarningCheckbox: React.FunctionComponent<WarningCheckboxProps> =
-  ({ isChecked, onChange, docLinks, id, meta }) => {
-    return (
-      <WarningCheckbox
-        isChecked={isChecked}
-        onChange={onChange}
-        warningId={id}
-        label={
-          <FormattedMessage
-            id="xpack.upgradeAssistant.checkupTab.reindexing.flyout.warningsStep.replaceIndexWithAliasWarningTitle"
-            defaultMessage="Replace {indexName} index with {reindexName} index and create {indexName} index alias"
-            values={{
-              indexName: <EuiCode>{meta?.indexName}</EuiCode>,
-              reindexName: <EuiCode>{meta?.reindexName}</EuiCode>,
-            }}
-          />
-        }
-        description={
-          <FormattedMessage
-            id="xpack.upgradeAssistant.checkupTab.reindexing.flyout.warningsStep.replaceIndexWithAliasWarningDetail"
-            defaultMessage="You can search {indexName} as before. To delete the data you'll have to delete {reindexName}"
-            values={{
-              indexName: <EuiCode>{meta?.indexName}</EuiCode>,
-              reindexName: <EuiCode>{meta?.reindexName}</EuiCode>,
-            }}
-          />
-        }
-      />
-    );
-  };
+export const ReplaceIndexWithAliasWarningCheckbox: React.FunctionComponent<
+  WarningCheckboxProps
+> = ({ isChecked, onChange, docLinks, id, meta }) => {
+  return (
+    <WarningCheckbox
+      isChecked={isChecked}
+      onChange={onChange}
+      warningId={id}
+      label={
+        <FormattedMessage
+          id="xpack.upgradeAssistant.checkupTab.reindexing.flyout.warningsStep.replaceIndexWithAliasWarningTitle"
+          defaultMessage="Replace {indexName} index with {reindexName} index and create {indexName} index alias"
+          values={{
+            indexName: <EuiCode>{meta?.indexName}</EuiCode>,
+            reindexName: <EuiCode>{meta?.reindexName}</EuiCode>,
+          }}
+        />
+      }
+      description={
+        <FormattedMessage
+          id="xpack.upgradeAssistant.checkupTab.reindexing.flyout.warningsStep.replaceIndexWithAliasWarningDetail"
+          defaultMessage="You can search {indexName} as before. To delete the data you'll have to delete {reindexName}"
+          values={{
+            indexName: <EuiCode>{meta?.indexName}</EuiCode>,
+            reindexName: <EuiCode>{meta?.reindexName}</EuiCode>,
+          }}
+        />
+      }
+    />
+  );
+};
