dc55c3be6f5409476721268b6db4938066885c9f
Devin W. Hurley <devin.hurley@elastic.co>
2022-01-04 18:12:22 -0500

    [Security Solution] [Platform] Migrate pre-8.0 action connector ids in rule params on import to post-8.0 _id (#120975)
    Security Solution import route needed to find actions where the action SO id was the old, pre-8.0 _id so we find references to the _id and the originId to make sure the rule we are importing will not error out.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/server/lib/detection_engine/routes/rules/import_rules_route.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/routes/rules/import_rules_route.ts
@@ -31,7 +31,11 @@ import {
   buildSiemResponse,
 } from '../utils';
 
-import { getTupleDuplicateErrorsAndUniqueRules, getInvalidConnectors } from './utils';
+import {
+  getTupleDuplicateErrorsAndUniqueRules,
+  getInvalidConnectors,
+  migrateLegacyActionsIds,
+} from './utils';
 import { createRulesAndExceptionsStreamFromNdJson } from '../../rules/create_rules_stream_from_ndjson';
 import { buildRouteValidation } from '../../../../utils/build_validation/route_validation';
 import { HapiReadableStream } from '../../rules/types';
@@ -74,6 +78,9 @@ export const importRulesRoute = (
         const rulesClient = context.alerting.getRulesClient();
         const actionsClient = context.actions.getActionsClient();
         const esClient = context.core.elasticsearch.client;
+        const actionSOClient = context.core.savedObjects.getClient({
+          includedHiddenTypes: ['action'],
+        });
         const savedObjectsClient = context.core.savedObjects.client;
         const siemClient = context.securitySolution.getAppClient();
         const exceptionsClient = context.lists?.getExceptionListClient();
@@ -127,8 +134,13 @@ export const importRulesRoute = (
         const [duplicateIdErrors, parsedObjectsWithoutDuplicateErrors] =
           getTupleDuplicateErrorsAndUniqueRules(rules, request.query.overwrite);
 
-        const [nonExistentActionErrors, uniqueParsedObjects] = await getInvalidConnectors(
+        const migratedParsedObjectsWithoutDuplicateErrors = await migrateLegacyActionsIds(
           parsedObjectsWithoutDuplicateErrors,
+          actionSOClient
+        );
+
+        const [nonExistentActionErrors, uniqueParsedObjects] = await getInvalidConnectors(
+          migratedParsedObjectsWithoutDuplicateErrors,
           actionsClient
         );
 
