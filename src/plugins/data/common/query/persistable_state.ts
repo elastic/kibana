6049389338824e514fd6df7598eebbda6cd79914
Andrew Tate <drewctate@gmail.com>
2022-01-04 08:34:15 -0600

    Update persistable_state.ts

--- src/plugins/data/common/query/persistable_state.ts
+++ src/plugins/data/common/query/persistable_state.ts
@@ -61,7 +61,5 @@ export const migrateToLatest = (filters: Filter[], version: string) => {
 };
 
 export const getAllMigrations = (): MigrateFunctionsObject => {
-  return {
-    '8.1.0': (filterState) => ({ ...filterState, migrated: true }),
-  };
+  return {};
 };
