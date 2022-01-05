fcf8f552fe83a840e9bf08cd686f6ee7b389d867
John Dorlus <jsdorlus@gmail.com>
2022-01-05 03:10:53 -0500

    Update indices_tab.helpers.ts

--- x-pack/plugins/index_management/__jest__/client_integration/home/indices_tab.helpers.ts
+++ x-pack/plugins/index_management/__jest__/client_integration/home/indices_tab.helpers.ts
@@ -28,7 +28,6 @@ export interface IndicesTestBed extends TestBed<TestSubjects> {
     getIncludeHiddenIndicesToggleStatus: () => boolean;
     clickIncludeHiddenIndicesToggle: () => void;
     clickDataStreamAt: (index: number) => void;
-    clickContextMenuOption: (subj: string) => void;
     clickManageContextMenuButton: () => void;
     clickContextMenuOption: (optionDataTestSubject: string) => void;
     clickModalConfirm: () => void;
