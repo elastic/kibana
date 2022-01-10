4c24522b02d7c274565095b84df55572ece5973b
Dmitry Tomashevich <39378793+Dmitriynj@users.noreply.github.com>
2022-01-10 19:55:28 +0300

    [Discover] Add kibana services provider for embeddable (#121621)
    * [Discover] add kibana services provider for embeddable
    
    * [Discover] add functional test
    
    * [Discover] fix functional test
    
    * [Discover] fix data grid test
    
    * [Discover] go to discover main page before each test
    
    * [Discover] remove redundant navigation to discover
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit f5aa068ef2e071148b886bc8fb2b3dd4fe97a5cf)
    
    # Conflicts:
    #	src/plugins/discover/public/application/embeddable/saved_search_grid.tsx

--- test/functional/services/data_grid.ts
+++ test/functional/services/data_grid.ts
@@ -161,7 +161,7 @@ export class DataGridService extends FtrService {
     options: SelectOptions = { isAnchorRow: false, rowIndex: 0 }
   ): Promise<void> {
     const row = await this.getRow(options);
-    const toggle = await row[0];
+    const toggle = await row[0].findByTestSubject('~docTableExpandToggleColumn');
     await toggle.click();
   }
 
