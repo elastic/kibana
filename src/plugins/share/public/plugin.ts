2ab94c458c0fd93e1f8d7f87f7f8ebeed008d523
vadimkibana <vadimkibana@gmail.com>
2022-01-10 12:48:01 +0100

    add tests for short URL creation

--- src/plugins/share/public/plugin.ts
+++ src/plugins/share/public/plugin.ts
@@ -19,13 +19,13 @@ import {
 import { UrlService } from '../common/url_service';
 import { RedirectManager } from './url_service';
 import type { RedirectOptions } from '../common/url_service/locators/redirect';
-import { LegacyShortUrlLocatorDefinition } from '../common/url_service/locators/legacy_short_url_locator';
 import {
   BrowserShortUrlClientFactory,
   BrowserShortUrlClientFactoryCreateParams,
 } from './url_service/short_urls/short_url_client_factory';
 import type { BrowserShortUrlClient } from './url_service/short_urls/short_url_client';
 import { AnonymousAccessServiceContract } from '../common';
+import { LegacyShortUrlLocatorDefinition } from '../common/url_service/locators/legacy_short_url_locator';
 import { ShortUrlRedirectLocatorDefinition } from '../common/url_service/locators/short_url_redirect_locator';
 import type { BrowserUrlService } from './types';
 
