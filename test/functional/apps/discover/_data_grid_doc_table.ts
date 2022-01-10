e88cc1fd4c37bdd4e8d8b4e4c6a68455cc6c673d
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-01-10 12:43:27 +0300

    [Discover] go to discover main page before each test

--- test/functional/apps/discover/_data_grid_doc_table.ts
+++ test/functional/apps/discover/_data_grid_doc_table.ts
@@ -33,6 +33,9 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await esArchiver.loadIfNeeded('test/functional/fixtures/es_archiver/logstash_functional');
       await kibanaServer.uiSettings.replace(defaultSettings);
       await PageObjects.timePicker.setDefaultAbsoluteRangeViaUiSettings();
+    });
+
+    beforeEach(async () => {
       await PageObjects.common.navigateToApp('discover');
     });
 
