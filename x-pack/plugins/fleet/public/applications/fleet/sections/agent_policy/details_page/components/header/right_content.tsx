e94d25e1c31de80491dea30075eaa8d354c8d489
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-01-11 16:46:47 -0500

    fix merge errors

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/details_page/components/header/right_content.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/details_page/components/header/right_content.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import { i18n } from '@kbn/i18n';
-import { FormattedDate, FormattedMessage } from '@kbn/i18n-react';
+import { FormattedDate, FormattedMessage } from '@kbn/i18n/react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import {
