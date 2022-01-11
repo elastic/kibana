4820a46f6876f21a96ec6802f9cd4930d7034a7a
Quynh Nguyen <quynh.nguyen@elastic.co>
2022-01-11 11:45:09 -0600

    Remove warning message for start transform

--- x-pack/plugins/transform/public/app/sections/transform_management/components/action_start/start_action_modal.tsx
+++ x-pack/plugins/transform/public/app/sections/transform_management/components/action_start/start_action_modal.tsx
@@ -5,16 +5,12 @@
  * 2.0.
  */
 
-import React, { FC, useMemo } from 'react';
+import React, { FC } from 'react';
 import { i18n } from '@kbn/i18n';
-import { EuiConfirmModal, EUI_MODAL_CONFIRM_BUTTON, EuiSpacer } from '@elastic/eui';
+import { EuiConfirmModal, EUI_MODAL_CONFIRM_BUTTON } from '@elastic/eui';
 import { StartAction } from './use_start_action';
-import { isManagedTransform } from '../../../../common/managed_transforms_utils';
-import { ManagedTransformsWarningCallout } from '../managed_transforms_callout/managed_transforms_callout';
 
 export const StartActionModal: FC<StartAction> = ({ closeModal, items, startAndCloseModal }) => {
-  const hasManagedTransforms = useMemo(() => items.some((t) => isManagedTransform(t)), [items]);
-
   const isBulkAction = items.length > 1;
 
   const bulkStartModalTitle = i18n.translate('xpack.transform.transformList.bulkStartModalTitle', {
@@ -42,19 +38,6 @@ export const StartActionModal: FC<StartAction> = ({ closeModal, items, startAndC
       buttonColor="primary"
     >
       <p>
-        {hasManagedTransforms ? (
-          <>
-            <ManagedTransformsWarningCallout
-              count={items.length}
-              action={i18n.translate(
-                'xpack.transform.transformList.startManagedTransformsDescription',
-                { defaultMessage: 'starting' }
-              )}
-            />
-            <EuiSpacer />
-          </>
-        ) : null}
-
         {i18n.translate('xpack.transform.transformList.startModalBody', {
           defaultMessage:
             'A transform increases search and indexing load in your cluster. If excessive load is experienced, stop the transform.',
