0db884bdf427ce2420b0519289b7ac0ae19e8235
cauemarcondes <caue.marcondes@elastic.co>
2022-01-11 11:22:27 -0300

    addressing pr comments

--- x-pack/plugins/apm/public/components/shared/service_icons/icon_popover.tsx
+++ x-pack/plugins/apm/public/components/shared/service_icons/icon_popover.tsx
@@ -29,8 +29,12 @@ interface IconPopoverProps {
 
 const StyledPanel = styled(EuiPanel)`
   &.serviceIcon {
-    box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.3),
-      0 1px 5px -2px rgba(0, 0, 0, 0.3) !important;
+    box-shadow: ${({ theme }) => {
+      const shadowColor = `${theme.eui.euiShadowColor}${60}`;
+      return `0 2px 2px -1px ${shadowColor}, 0 1px 5px -2px ${shadowColor} !important;`;
+    }};
+    height: 50px;
+    padding: 0 16px;
   }
 `;
 export function IconPopover({
