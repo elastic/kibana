092d33a3bbcf6634245706014b2707e48be791d3
Madison Caldwell <madison.caldwell@elastic.co>
2022-01-11 11:45:36 -0500

    Regenerate snapshot of behavior event summary rows

--- x-pack/plugins/security_solution/public/common/components/event_details/alert_summary_view.test.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/alert_summary_view.test.tsx
@@ -107,7 +107,7 @@ describe('AlertSummaryView', () => {
     );
     expect(container.querySelector('div[data-test-subj="summary-view"]')).toMatchSnapshot();
   });
-  test.skip('Behavior event code renders additional summary rows', () => {
+  test('Behavior event code renders additional summary rows', () => {
     const renderProps = {
       ...props,
       data: mockAlertDetailsData.map((item) => {
