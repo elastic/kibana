8c7413d1f37ae75ca7e8e36b736dbcffafdf13d7
Jonathan Budzenski <jon@elastic.co>
2022-01-12 17:13:05 -0600

    bootstrap

--- .buildkite/scripts/steps/docker_context/build.sh
+++ .buildkite/scripts/steps/docker_context/build.sh
@@ -2,14 +2,13 @@
 
 set -euo pipefail
 
+.buildkite/scripts/bootstrap.sh
+
 echo "--- Create Kibana Docker contexts"
 node scripts/build --skip-initialize --skip-generic-folders --skip-platform-folders --skip-archives
 
-DOCKER_BUILD_FOLDER=$(mktemp)
-
-tar -xf target/kibana-[0-9]*-docker-build-context.tar.gz -C "$DOCKER_BUILD_FOLDER"
-
-cd $DOCKER_BUILD_FOLDER
-
 echo "--- Build default context"
+DOCKER_BUILD_FOLDER=$(mktemp)
+tar -xf target/kibana-[0-9]*-docker-build-context.tar.gz -C "$DOCKER_BUILD_FOLDER"
+cd $DOCKER_BUILD_FOLDER
 docker build .
