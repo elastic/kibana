a3ca8129f86a15a082fd5cb982e91c1996953979
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-01-12 18:23:36 +0300

    [Discover] fix functional test

--- test/functional/apps/discover/_data_grid_doc_table.ts
+++ test/functional/apps/discover/_data_grid_doc_table.ts
@@ -96,12 +96,12 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await dashboardAddPanel.addSavedSearch('expand-cell-search');
 
       await retry.waitForWithTimeout('timestamp matches expected doc', 5000, async () => {
-        const cell = await dataGrid.getCellElement(0, 2);
+        const cell = await dataGrid.getCellElement(1, 3);
         const text = await cell.getVisibleText();
         log.debug(`row document timestamp: ${text}`);
         return text === 'Sep 22, 2015 @ 23:50:13.253';
       });
-      const docCell = await dataGrid.getCellElement(0, 3);
+      const docCell = await dataGrid.getCellElement(1, 4);
       await docCell.click();
       const expandCellContentButton = await docCell.findByClassName(
         'euiDataGridRowCell__expandButtonIcon'
