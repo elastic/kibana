0493f00d7f40e36fc1a1b160b9d81e09bea735c5
Jonathan Budzenski <jon@elastic.co>
2022-01-11 20:17:38 -0600

    [ci/package-testing] Skip ubi and cloud build, update artifact glob (#122614)

--- .buildkite/scripts/steps/package_testing/build.sh
+++ .buildkite/scripts/steps/package_testing/build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 .buildkite/scripts/bootstrap.sh
 
 echo "--- Build Kibana Distribution"
-node scripts/build --all-platforms --debug
+node scripts/build --all-platforms --debug --skip-docker-cloud --skip-docker-ubi --skip-docker-contexts
 
 cd target
-buildkite-agent artifact upload "./kibana-[0-9]*-docker-image.tar.gz;./*.deb;./*.rpm"
+buildkite-agent artifact upload "./*-docker-image.tar.gz;./*.deb;./*.rpm"
 cd ..
