9da6a843ae96234909de446488f4fc93f6288e51
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-01-12 12:58:29 +0100

    print index page before upload

--- .buildkite/scripts/steps/code_coverage/reporting/uploadStaticSite.sh
+++ .buildkite/scripts/steps/code_coverage/reporting/uploadStaticSite.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 uploadPrefix="gs://elastic-bekitzur-kibana-coverage-live/"
 uploadPrefixWithTimeStamp="${uploadPrefix}${TIME_STAMP}/"
 
+cat src/dev/code_coverage/www/index.html
+
 for x in 'src/dev/code_coverage/www/index.html' 'src/dev/code_coverage/www/404.html'; do
-    gsutil -m cp -r -a public-read -z js,css,html ${x} '${uploadPrefix}'
+    gsutil -m cp -r -a public-read -z js,css,html ${x} ${uploadPrefix}
 done
 
 gsutil -m cp -r -a public-read -z js,css,html ${x} ${uploadPrefixWithTimeStamp}
