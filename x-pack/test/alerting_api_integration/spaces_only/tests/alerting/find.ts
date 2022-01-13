51044a860e15039f2c59bbb6a5105543bef68814
chrisronline <chris.roberson@elastic.co>
2022-01-12 21:33:40 -0500

    Find more places

--- x-pack/test/alerting_api_integration/spaces_only/tests/alerting/find.ts
+++ x-pack/test/alerting_api_integration/spaces_only/tests/alerting/find.ts
@@ -69,6 +69,7 @@ export default function createFindTests({ getService }: FtrProviderContext) {
         created_at: match.created_at,
         updated_at: match.updated_at,
         execution_status: match.execution_status,
+        monitoring: match.monitoring,
       });
       expect(Date.parse(match.created_at)).to.be.greaterThan(0);
       expect(Date.parse(match.updated_at)).to.be.greaterThan(0);
