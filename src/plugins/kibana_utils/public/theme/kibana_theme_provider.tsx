6dc31d768d75870c9c7ba833317b2197480465ce
Shivindera Singh <shivindera@gmail.com>
2022-01-13 15:30:10 +0100

    add KibanaThemeProvider support for kibana-app-services (#122370)
    add KibanaThemeProvider support for kibana-app-services

--- src/plugins/kibana_utils/public/theme/kibana_theme_provider.tsx
+++ src/plugins/kibana_utils/public/theme/kibana_theme_provider.tsx
@@ -6,10 +6,12 @@
  * Side Public License, v 1.
  */
 
-import React, { FC, useMemo } from 'react';
-import useObservable from 'react-use/lib/useObservable';
-import { Observable } from 'rxjs';
 import { EuiThemeProvider } from '@elastic/eui';
+import type { FC } from 'react';
+import React, { useMemo } from 'react';
+import useObservable from 'react-use/lib/useObservable';
+import type { Observable } from 'rxjs';
+
 import type { CoreTheme } from '../../../../core/public';
 import { getColorMode } from './utils';
 
@@ -21,10 +23,9 @@ const defaultTheme: CoreTheme = {
   darkMode: false,
 };
 
-/* IMPORTANT: This code has been copied to the `interactive_setup` plugin, any changes here should be applied there too.
-That copy and this comment can be removed once https://github.com/elastic/kibana/issues/119204 is implemented.*/
-// IMPORTANT: This code has been copied to the `kibana_utils` plugin, to avoid cyclical dependency, any changes here should be applied there too.
-
+/**
+ * Copied from the `kibana_react` plugin, to avoid cyclical dependency
+ */
 export const KibanaThemeProvider: FC<KibanaThemeProviderProps> = ({ theme$, children }) => {
   const theme = useObservable(theme$, defaultTheme);
   const colorMode = useMemo(() => getColorMode(theme), [theme]);
--- src/plugins/kibana_utils/public/theme/utils.ts
+++ src/plugins/kibana_utils/public/theme/utils.ts
@@ -8,12 +8,12 @@
 
 import { COLOR_MODES_STANDARD } from '@elastic/eui';
 import type { EuiThemeColorModeStandard } from '@elastic/eui';
+
 import type { CoreTheme } from '../../../../core/public';
 
-/* IMPORTANT: This code has been copied to the `interactive_setup` plugin, any changes here should be applied there too.
-That copy and this comment can be removed once https://github.com/elastic/kibana/issues/119204 is implemented.*/
-// IMPORTANT: This code has been copied to the `kibana_utils` plugin, to avoid cyclical dependency, any changes here should be applied there too.
-
+/**
+ * Copied from the `kibana_react` plugin, to avoid cyclical dependency
+ */
 export const getColorMode = (theme: CoreTheme): EuiThemeColorModeStandard => {
   return theme.darkMode ? COLOR_MODES_STANDARD.dark : COLOR_MODES_STANDARD.light;
 };
