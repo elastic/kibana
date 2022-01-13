54ed86b7b49c2d692a21a57c7bbbfb026d2fb193
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-01-13 19:46:04 +0100

    remove step var

--- .buildkite/scripts/steps/code_coverage/ingest.sh
+++ .buildkite/scripts/steps/code_coverage/ingest.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
-is_test_execution_step
-
 export CODE_COVERAGE=1
 echo "--- Reading Kibana stats cluster creds from vault"
 export USER_FROM_VAULT="$(retry 5 5 vault read -field=username secret/kibana-issues/prod/coverage/elasticsearch)"
@@ -39,16 +37,16 @@ sed -i "s|/opt/local-ssd/buildkite/builds/kb-[[:alnum:]\-]\{20,27\}/elastic/kiba
 
 echo "--- Jest: merging coverage files and generating the final combined report"
 yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.jest.config.js
-rm -rf target/kibana-coverage/jest && mkdir target/kibana-coverage/jest
 
 echo "--- Functional: merging json files and generating the final combined report"
 yarn nyc report --nycrc-path src/dev/code_coverage/nyc_config/nyc.functional.config.js
-rm -rf target/kibana-coverage/functional && mkdir target/kibana-coverage/functional
 
 # archive reports to upload as build artifacts
-echo "--- Archive combined jest & functional reports"
+echo "--- Archive and upload combined reports"
 tar -czf target/kibana-coverage/jest/kibana-jest-coverage.tar.gz target/kibana-coverage/jest-combined
 tar -czf target/kibana-coverage/functional/kibana-functional-coverage.tar.gz target/kibana-coverage/functional-combined
+buildkite-agent artifact upload 'target/kibana-coverage/jest/kibana-jest-coverage.tar.gz'
+buildkite-agent artifact upload 'target/kibana-coverage/functional/kibana-functional-coverage.tar.gz'
 
 echo "--- Upload coverage static site"
 .buildkite/scripts/steps/code_coverage/reporting/uploadStaticSite.sh
