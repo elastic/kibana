ec8ae5a0d3a46a370ec9f60a98b1b8a80a436a45
Justin Kambic <justin.kambic@elastic.co>
2022-01-14 13:10:59 -0500

    [Uptime] Add locator to aid other plugins in linking properly to Uptime (#123004)
    * Implement locator for Uptime Overview page.
    
    * Give more descriptive name to locator ID const.
    
    * Lazy load locator init code to reduce bundle size.

--- x-pack/plugins/observability/common/index.ts
+++ x-pack/plugins/observability/common/index.ts
@@ -24,3 +24,7 @@ export const observabilityFeatureId = 'observability';
 
 // Used by Cases to install routes
 export const casesPath = '/cases';
+
+// Name of a locator created by the uptime plugin. Intended for use
+// by other plugins as well, so defined here to prevent cross-references.
+export const uptimeOverviewLocatorID = 'uptime-overview-locator';
