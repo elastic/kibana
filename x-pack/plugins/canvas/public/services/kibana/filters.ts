96d0af82b7bb9244772300b10985b26f6b063ebd
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-01-14 18:03:53 +0200

    Removed double license.

--- x-pack/plugins/canvas/public/services/kibana/filters.ts
+++ x-pack/plugins/canvas/public/services/kibana/filters.ts
@@ -4,12 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
 
 import { KibanaPluginServiceFactory } from '../../../../../../src/plugins/presentation_util/public';
 // @ts-expect-error untyped local
