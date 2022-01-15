fdfcf73ee5cd5efea4ae968008de520ab99607d1
restrry <mikhail.shustov@elastic.co>
2022-01-15 10:53:13 +0100

    fix a few other failures

--- x-pack/test/observability_api_integration/trial/tests/annotations.ts
+++ x-pack/test/observability_api_integration/trial/tests/annotations.ts
@@ -102,6 +102,8 @@ export default function annotationApiTests({ getService }: FtrProviderContext) {
 
         const { _source, _id, _index } = response.body;
 
+        // the test can be executed against a ES version with or without _type support
+        delete response.body._type;
         expect(response.body).to.eql({
           _index,
           _id,
