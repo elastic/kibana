634e5a4c79d44d9d4119ddd66c98ea82219b1573
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-01-17 18:11:26 +0300

    [Discover] add comments

--- src/plugins/discover/public/utils/use_row_heights_options.ts
+++ src/plugins/discover/public/utils/use_row_heights_options.ts
@@ -20,9 +20,18 @@ interface UseRowHeightProps {
   uiSettings: IUiSettingsClient;
 }
 
+/**
+ * Row height might be a value from -1 to 20
+ * A value of -1 automatically adjusts the row height to fit the contents.
+ * A value of 0 displays the content in a single line.
+ * A value from 1 to 20 represents number of lines of Document explorer row to display.
+ */
 const SINGLE_ROW_HEIGHT_OPTION = 0;
 const AUTO_ROW_HEIGHT_OPTION = -1;
 
+/**
+ * Converts rowHeight of EuiDataGrid to rowHeight number (-1 to 20)
+ */
 const serializeRowHeight = (rowHeight?: EuiDataGridRowHeightOption): number => {
   if (rowHeight === 'auto') {
     return AUTO_ROW_HEIGHT_OPTION;
@@ -32,6 +41,10 @@ const serializeRowHeight = (rowHeight?: EuiDataGridRowHeightOption): number => {
 
   return SINGLE_ROW_HEIGHT_OPTION;
 };
+
+/**
+ * Converts rowHeight number (-1 to 20) of EuiDataGrid rowHeight
+ */
 const deserializeRowHeight = (number: number): EuiDataGridRowHeightOption | undefined => {
   if (number === AUTO_ROW_HEIGHT_OPTION) {
     return 'auto';
