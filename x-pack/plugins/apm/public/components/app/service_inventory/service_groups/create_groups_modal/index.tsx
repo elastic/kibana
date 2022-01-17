d5dbf1ba126d14dcfd519daec6c0772173c06b36
cauemarcondes <caue.marcondes@elastic.co>
2022-01-17 15:14:28 -0300

    removing import

--- x-pack/plugins/apm/public/components/app/service_inventory/service_groups/create_groups_modal/index.tsx
+++ x-pack/plugins/apm/public/components/app/service_inventory/service_groups/create_groups_modal/index.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EuiForm, EuiModal } from '@elastic/eui';
+import { EuiModal } from '@elastic/eui';
 import React, { useState } from 'react';
 import { GroupDetails } from './group_details_form';
 import { SelectServices } from './select_services_form';
