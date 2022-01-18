a13f50253ef14f4e5cd7a64f8c4c2ded30d9dff0
Quynh Nguyen <quynh.nguyen@elastic.co>
2022-01-18 11:23:30 -0600

    Delete attr instead of setting it to false

--- x-pack/plugins/transform/public/app/common/transform.ts
+++ x-pack/plugins/transform/public/app/common/transform.ts
@@ -9,8 +9,8 @@ import { useEffect } from 'react';
 import { BehaviorSubject } from 'rxjs';
 import { filter, distinctUntilChanged } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
-
-import { TransformConfigUnion, TransformId } from '../../../common/types/transform';
+import { cloneDeep } from 'lodash';
+import type { TransformConfigUnion, TransformId } from '../../../common/types/transform';
 
 // Via https://github.com/elastic/elasticsearch/blob/master/x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/transform/utils/TransformStrings.java#L24
 // Matches a string that contains lowercase characters, digits, hyphens, underscores or dots.
@@ -82,5 +82,7 @@ export const useRefreshTransformList = (
 export const overrideTransformForCloning = (originalConfig: TransformConfigUnion) => {
   // 'Managed' means job is preconfigured and deployed by other solutions
   // we should not clone this setting
-  return { ...originalConfig, _meta: { ...originalConfig._meta, managed: false } };
+  const clonedConfig = cloneDeep(originalConfig);
+  delete clonedConfig._meta?.managed;
+  return clonedConfig;
 };
