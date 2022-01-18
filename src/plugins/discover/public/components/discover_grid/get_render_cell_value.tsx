63e9b83952668f352d62b76861ec3ab3171303cd
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2022-01-18 08:20:40 +0100

    Update get_render_cell_value.tsx

--- src/plugins/discover/public/components/discover_grid/get_render_cell_value.tsx
+++ src/plugins/discover/public/components/discover_grid/get_render_cell_value.tsx
@@ -177,7 +177,7 @@ export const getRenderCellValueFn =
         );
       }
 
-      return (<span>{ formatFieldValue(rowFlattened[columnId], row, indexPattern, field) }</span>);
+      return <span>{formatFieldValue(rowFlattened[columnId], row, indexPattern, field)}</span>;
     }
 
     return (
