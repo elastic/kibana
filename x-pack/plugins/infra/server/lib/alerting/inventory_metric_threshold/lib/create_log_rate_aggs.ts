47f8874c91455a744d49bfcd0b1c4c9bbcadbd81
Chris Cowan <chris@elastic.co>
2022-01-18 11:54:07 -0700

    Fixing logRate aggregation

--- x-pack/plugins/infra/server/lib/alerting/inventory_metric_threshold/lib/create_log_rate_aggs.ts
+++ x-pack/plugins/infra/server/lib/alerting/inventory_metric_threshold/lib/create_log_rate_aggs.ts
@@ -9,39 +9,14 @@ import { InfraTimerangeInput } from '../../../../../common/http_api';
 import { calculateRateTimeranges } from './calculate_rate_timeranges';
 
 export const createLogRateAggs = (timerange: InfraTimerangeInput, id: string) => {
-  const { firstBucketRange, secondBucketRange, intervalInSeconds } =
-    calculateRateTimeranges(timerange);
-
+  const intervalInSeconds = (timerange.to - timerange.from) / 1000;
   return {
-    [`${id}_first_bucket`]: {
-      filter: {
-        range: {
-          '@timestamp': {
-            gte: firstBucketRange.from,
-            lt: firstBucketRange.to,
-            format: 'epoch_millis',
-          },
-        },
-      },
-    },
-    [`${id}_second_bucket`]: {
-      filter: {
-        range: {
-          '@timestamp': {
-            gte: secondBucketRange.from,
-            lt: secondBucketRange.to,
-            format: 'epoch_millis',
-          },
-        },
-      },
-    },
     [id]: {
       bucket_script: {
         buckets_path: {
-          first: `${id}_first_bucket._count`,
-          second: `${id}_second_bucket._count`,
+          count: `_count`,
         },
-        script: `params.second > 0.0 && params.first > 0.0 && params.second > params.first ? (params.second - params.first) / ${intervalInSeconds}: null`,
+        script: `params.count > 0.0 ? params.count / ${intervalInSeconds}: null`,
       },
     },
   };
