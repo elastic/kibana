53d71a001fb157df9a5f7d958684970708677254
Tyler Smalley <tyler.smalley@elastic.co>
2022-01-18 22:03:49 -0800

    TEMP: Use nightly ES snapshot
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/steps/functional/common.sh
+++ .buildkite/scripts/steps/functional/common.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
+export ES_SNAPSHOT_MANIFEST="https://storage.googleapis.com/kibana-ci-es-snapshots-daily/8.1.0/archives/20220118-151627_25dd4a31/manifest.json"
+
 .buildkite/scripts/bootstrap.sh
 .buildkite/scripts/download_build_artifacts.sh
 
