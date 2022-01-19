4fe782dfe2772f4800fc3f0f4054eb96504d275b
ppisljar <peter.pisljar@elastic.co>
2022-01-18 23:42:49 -0800

    updating persistable state types

--- src/plugins/kibana_utils/common/persistable_state/types.ts
+++ src/plugins/kibana_utils/common/persistable_state/types.ts
@@ -82,8 +82,10 @@ export interface PersistableState<P extends SerializableRecord = SerializableRec
    * keyed by the Kibana version using semver, where the version indicates to
    * which version the state will be migrated to.
    */
-  migrations: MigrateFunctionsObject;
+  migrations: MigrateFunctionsObject | GetMigrationFunctionObjectFn;
 }
+
+type GetMigrationFunctionObjectFn = () => MigrateFunctionsObject;
 
 /**
  * Collection of migrations that a given type of persistable state object has
