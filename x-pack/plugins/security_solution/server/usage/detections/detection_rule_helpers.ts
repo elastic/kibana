a36e4fb5d0c943276a41c24fa6efd1538daa7f89
Frank Hassanabad <frank.hassanabad@elastic.co>
2022-01-19 10:39:47 -0700

    [Security Solution] Adds telemetry for legacy notifications and regular notifications at a finer grained level   (#123332)
    ## Summary
    
    Related and previous PR:
    https://github.com/elastic/kibana/pull/122472
    
    This removes the above structure from the PR above and instead opts to use a more finer grained level of telemetry. The new structure adds to each rule these four counters to the telemetry:
    
    * legacy_notifications_enabled - The number of legacy notifications on rules that are enabled/active
    * legacy_notifications_disabled - The number of legacy notifications on rules that are disabled/in-active
    * notifications_enabled - The number of notifications on rules that are enabled/active
    * notifications_disabled - The number of notifications on rules that are disabled/in-active
    
    For pre-built rules you have these booleans:
    * has_legacy_notification - True if the pre-built rule has a legacy notification attached, otherwise false.
    * has_notification - True if the pre-built rule has a notification attached, otherwise false.
    
    Note, both those booleans are `false` if the pre-built rule has no notifications attached and both can never be `true` together.
    
    These will show up within each rule type like for example on a query rule it will look like:
    
    ```json
    "detection_rule_usage": {
      "query": {
        "enabled": 2,
        "disabled": 1,
        "cases": 0,
        "legacy_notifications_enabled": 1, <-- New
        "legacy_notifications_disabled": 0, <-- New
        "notifications_enabled": 1, <-- New
        "notifications_disabled": 1 <-- New
    }
    ```
    
    Within the counts/total sections it will show up on both the `elastic` rules and the `custom` rules like so:
    
    ```json
    "elastic_total": {
      "enabled": 0,
      "disabled": 0,
      "alerts": 0,
      "cases": 0,
      "legacy_notifications_enabled": 0, <-- New
      "legacy_notifications_disabled": 0, <-- New
      "notifications_enabled": 0, <-- New
      "notifications_disabled": 0 <-- New
    },
    "custom_total": {
      "enabled": 2,
      "disabled": 1,
      "alerts": 7218,
      "cases": 0,
      "legacy_notifications_enabled": 1, <-- New
      "legacy_notifications_disabled": 0, <-- New
      "notifications_enabled": 1, <-- New
      "notifications_disabled": 1 <-- New
    }
    ```
    
    For pre-built it will be:
    
    ```json
    "detection_rule_detail": [
      {
        "rule_name": "Potential Evasion via Filter Manager",
        "rule_id": "06dceabf-adca-48af-ac79-ffdf4c3b1e9a",
        "rule_type": "eql",
        "rule_version": 8,
        "enabled": false,
        "elastic_rule": true,
        "created_on": "2022-01-19T01:29:25.540Z",
        "updated_on": "2022-01-19T01:29:25.540Z",
        "alert_count_daily": 0,
        "cases_count_total": 0,
        "has_legacy_notification": false, <-- New
        "has_notification": false <-- New
      },
    ```
    
    Screen shot of it if you go to "Advanced settings -> cluster data":
    <img width="802" alt="Screen Shot 2022-01-18 at 6 27 14 PM" src="https://user-images.githubusercontent.com/1151048/150046445-b1850b1c-bca6-41e0-b101-1bac5f67dbb3.png">
    
    <img width="798" alt="Screen Shot 2022-01-18 at 6 30 33 PM" src="https://user-images.githubusercontent.com/1151048/150046808-1109a4c9-8a54-4da8-8b42-5f957a9d3ed5.png">
    
    Follow the manual test instructions on https://github.com/elastic/kibana/pull/122472 for how to test this. The same manual testing applies here for seeing how these work out. You should be able to see a higher granularity with these stats.
    
    ### Checklist
    
    - [ ] [Unit or functional tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html) were updated or added to match the most common scenarios

--- x-pack/plugins/security_solution/server/usage/detections/detection_rule_helpers.ts
+++ x-pack/plugins/security_solution/server/usage/detections/detection_rule_helpers.ts
@@ -15,7 +15,6 @@ import {
   SAVED_QUERY_RULE_TYPE_ID,
 } from '@kbn/securitysolution-rules';
 import { ALERT_RULE_UUID } from '@kbn/rule-data-utils';
-import { LEGACY_NOTIFICATIONS_ID } from '../../../common/constants';
 import { CASE_COMMENT_SAVED_OBJECT } from '../../../../cases/common/constants';
 
 import { ElasticsearchClient, SavedObjectsClientContract } from '../../../../../../src/core/server';
@@ -30,6 +29,10 @@ import type {
   RuleSearchResult,
   DetectionMetrics,
 } from './types';
+// eslint-disable-next-line no-restricted-imports
+import { legacyRuleActionsSavedObjectType } from '../../lib/detection_engine/rule_actions/legacy_saved_object_mappings';
+// eslint-disable-next-line no-restricted-imports
+import { LegacyIRuleActionsAttributesSavedObjectAttributes } from '../../lib/detection_engine/rule_actions/legacy_types';
 
 /**
  * Initial detection metrics initialized.
@@ -63,45 +66,70 @@ export const initialDetectionRulesUsage: DetectionRulesTypeUsage = {
     disabled: 0,
     alerts: 0,
     cases: 0,
+    legacy_notifications_enabled: 0,
+    legacy_notifications_disabled: 0,
+    notifications_enabled: 0,
+    notifications_disabled: 0,
   },
   threshold: {
     enabled: 0,
     disabled: 0,
     alerts: 0,
     cases: 0,
+    legacy_notifications_enabled: 0,
+    legacy_notifications_disabled: 0,
+    notifications_enabled: 0,
+    notifications_disabled: 0,
   },
   eql: {
     enabled: 0,
     disabled: 0,
     alerts: 0,
     cases: 0,
+    legacy_notifications_enabled: 0,
+    legacy_notifications_disabled: 0,
+    notifications_enabled: 0,
+    notifications_disabled: 0,
   },
   machine_learning: {
     enabled: 0,
     disabled: 0,
     alerts: 0,
     cases: 0,
+    legacy_notifications_enabled: 0,
+    legacy_notifications_disabled: 0,
+    notifications_enabled: 0,
+    notifications_disabled: 0,
   },
   threat_match: {
     enabled: 0,
     disabled: 0,
     alerts: 0,
     cases: 0,
+    legacy_notifications_enabled: 0,
+    legacy_notifications_disabled: 0,
+    notifications_enabled: 0,
+    notifications_disabled: 0,
   },
   elastic_total: {
     enabled: 0,
     disabled: 0,
     alerts: 0,
     cases: 0,
-  },
-  legacy_notifications: {
-    total: 0,
+    legacy_notifications_enabled: 0,
+    legacy_notifications_disabled: 0,
+    notifications_enabled: 0,
+    notifications_disabled: 0,
   },
   custom_total: {
     enabled: 0,
     disabled: 0,
     alerts: 0,
     cases: 0,
+    legacy_notifications_enabled: 0,
+    legacy_notifications_disabled: 0,
+    notifications_enabled: 0,
+    notifications_disabled: 0,
   },
 };
 
@@ -112,6 +140,16 @@ export const updateDetectionRuleUsage = (
 ): DetectionRulesTypeUsage => {
   let updatedUsage = usage;
 
+  const legacyNotificationEnabled =
+    detectionRuleMetric.has_legacy_notification && detectionRuleMetric.enabled;
+
+  const legacyNotificationDisabled =
+    detectionRuleMetric.has_legacy_notification && !detectionRuleMetric.enabled;
+
+  const notificationEnabled = detectionRuleMetric.has_notification && detectionRuleMetric.enabled;
+
+  const notificationDisabled = detectionRuleMetric.has_notification && !detectionRuleMetric.enabled;
+
   if (detectionRuleMetric.rule_type === 'query') {
     updatedUsage = {
       ...usage,
@@ -121,6 +159,18 @@ export const updateDetectionRuleUsage = (
         disabled: !detectionRuleMetric.enabled ? usage.query.disabled + 1 : usage.query.disabled,
         alerts: usage.query.alerts + detectionRuleMetric.alert_count_daily,
         cases: usage.query.cases + detectionRuleMetric.cases_count_total,
+        legacy_notifications_enabled: legacyNotificationEnabled
+          ? usage.query.legacy_notifications_enabled + 1
+          : usage.query.legacy_notifications_enabled,
+        legacy_notifications_disabled: legacyNotificationDisabled
+          ? usage.query.legacy_notifications_disabled + 1
+          : usage.query.legacy_notifications_disabled,
+        notifications_enabled: notificationEnabled
+          ? usage.query.notifications_enabled + 1
+          : usage.query.notifications_enabled,
+        notifications_disabled: notificationDisabled
+          ? usage.query.notifications_disabled + 1
+          : usage.query.notifications_disabled,
       },
     };
   } else if (detectionRuleMetric.rule_type === 'threshold') {
@@ -136,6 +186,18 @@ export const updateDetectionRuleUsage = (
           : usage.threshold.disabled,
         alerts: usage.threshold.alerts + detectionRuleMetric.alert_count_daily,
         cases: usage.threshold.cases + detectionRuleMetric.cases_count_total,
+        legacy_notifications_enabled: legacyNotificationEnabled
+          ? usage.threshold.legacy_notifications_enabled + 1
+          : usage.threshold.legacy_notifications_enabled,
+        legacy_notifications_disabled: legacyNotificationDisabled
+          ? usage.threshold.legacy_notifications_disabled + 1
+          : usage.threshold.legacy_notifications_disabled,
+        notifications_enabled: notificationEnabled
+          ? usage.threshold.notifications_enabled + 1
+          : usage.threshold.notifications_enabled,
+        notifications_disabled: notificationDisabled
+          ? usage.threshold.notifications_disabled + 1
+          : usage.threshold.notifications_disabled,
       },
     };
   } else if (detectionRuleMetric.rule_type === 'eql') {
@@ -147,6 +209,18 @@ export const updateDetectionRuleUsage = (
         disabled: !detectionRuleMetric.enabled ? usage.eql.disabled + 1 : usage.eql.disabled,
         alerts: usage.eql.alerts + detectionRuleMetric.alert_count_daily,
         cases: usage.eql.cases + detectionRuleMetric.cases_count_total,
+        legacy_notifications_enabled: legacyNotificationEnabled
+          ? usage.eql.legacy_notifications_enabled + 1
+          : usage.eql.legacy_notifications_enabled,
+        legacy_notifications_disabled: legacyNotificationDisabled
+          ? usage.eql.legacy_notifications_disabled + 1
+          : usage.eql.legacy_notifications_disabled,
+        notifications_enabled: notificationEnabled
+          ? usage.eql.notifications_enabled + 1
+          : usage.eql.notifications_enabled,
+        notifications_disabled: notificationDisabled
+          ? usage.eql.notifications_disabled + 1
+          : usage.eql.notifications_disabled,
       },
     };
   } else if (detectionRuleMetric.rule_type === 'machine_learning') {
@@ -162,6 +236,18 @@ export const updateDetectionRuleUsage = (
           : usage.machine_learning.disabled,
         alerts: usage.machine_learning.alerts + detectionRuleMetric.alert_count_daily,
         cases: usage.machine_learning.cases + detectionRuleMetric.cases_count_total,
+        legacy_notifications_enabled: legacyNotificationEnabled
+          ? usage.machine_learning.legacy_notifications_enabled + 1
+          : usage.machine_learning.legacy_notifications_enabled,
+        legacy_notifications_disabled: legacyNotificationDisabled
+          ? usage.machine_learning.legacy_notifications_disabled + 1
+          : usage.machine_learning.legacy_notifications_disabled,
+        notifications_enabled: notificationEnabled
+          ? usage.machine_learning.notifications_enabled + 1
+          : usage.machine_learning.notifications_enabled,
+        notifications_disabled: notificationDisabled
+          ? usage.machine_learning.notifications_disabled + 1
+          : usage.machine_learning.notifications_disabled,
       },
     };
   } else if (detectionRuleMetric.rule_type === 'threat_match') {
@@ -177,6 +263,18 @@ export const updateDetectionRuleUsage = (
           : usage.threat_match.disabled,
         alerts: usage.threat_match.alerts + detectionRuleMetric.alert_count_daily,
         cases: usage.threat_match.cases + detectionRuleMetric.cases_count_total,
+        legacy_notifications_enabled: legacyNotificationEnabled
+          ? usage.threat_match.legacy_notifications_enabled + 1
+          : usage.threat_match.legacy_notifications_enabled,
+        legacy_notifications_disabled: legacyNotificationDisabled
+          ? usage.threat_match.legacy_notifications_disabled + 1
+          : usage.threat_match.legacy_notifications_disabled,
+        notifications_enabled: notificationEnabled
+          ? usage.threat_match.notifications_enabled + 1
+          : usage.threat_match.notifications_enabled,
+        notifications_disabled: notificationDisabled
+          ? usage.threat_match.notifications_disabled + 1
+          : usage.threat_match.notifications_disabled,
       },
     };
   }
@@ -194,6 +292,18 @@ export const updateDetectionRuleUsage = (
           : updatedUsage.elastic_total.disabled,
         alerts: updatedUsage.elastic_total.alerts + detectionRuleMetric.alert_count_daily,
         cases: updatedUsage.elastic_total.cases + detectionRuleMetric.cases_count_total,
+        legacy_notifications_enabled: legacyNotificationEnabled
+          ? updatedUsage.elastic_total.legacy_notifications_enabled + 1
+          : updatedUsage.elastic_total.legacy_notifications_enabled,
+        legacy_notifications_disabled: legacyNotificationDisabled
+          ? updatedUsage.elastic_total.legacy_notifications_disabled + 1
+          : updatedUsage.elastic_total.legacy_notifications_disabled,
+        notifications_enabled: notificationEnabled
+          ? updatedUsage.elastic_total.notifications_enabled + 1
+          : updatedUsage.elastic_total.notifications_enabled,
+        notifications_disabled: notificationDisabled
+          ? updatedUsage.elastic_total.notifications_disabled + 1
+          : updatedUsage.elastic_total.notifications_disabled,
       },
     };
   } else {
@@ -209,6 +319,18 @@ export const updateDetectionRuleUsage = (
           : updatedUsage.custom_total.disabled,
         alerts: updatedUsage.custom_total.alerts + detectionRuleMetric.alert_count_daily,
         cases: updatedUsage.custom_total.cases + detectionRuleMetric.cases_count_total,
+        legacy_notifications_enabled: legacyNotificationEnabled
+          ? updatedUsage.custom_total.legacy_notifications_enabled + 1
+          : updatedUsage.custom_total.legacy_notifications_enabled,
+        legacy_notifications_disabled: legacyNotificationDisabled
+          ? updatedUsage.custom_total.legacy_notifications_disabled + 1
+          : updatedUsage.custom_total.legacy_notifications_disabled,
+        notifications_enabled: notificationEnabled
+          ? updatedUsage.custom_total.notifications_enabled + 1
+          : updatedUsage.custom_total.notifications_enabled,
+        notifications_disabled: notificationDisabled
+          ? updatedUsage.custom_total.notifications_disabled + 1
+          : updatedUsage.custom_total.notifications_disabled,
       },
     };
   }
@@ -287,18 +409,28 @@ export const getDetectionRuleMetrics = async (
       filter: `${CASE_COMMENT_SAVED_OBJECT}.attributes.type: alert`,
     });
 
-    // We get just 1 per a single page so we can get the total count to add to the rulesUsage.
     // Once we are confident all rules relying on side-car actions SO's have been migrated to SO references we should remove this function.
-    const legacyNotificationsCount = (
-      await savedObjectClient.find({
-        type: 'alert',
+    const legacyRuleActions =
+      await savedObjectClient.find<LegacyIRuleActionsAttributesSavedObjectAttributes>({
+        type: legacyRuleActionsSavedObjectType,
         page: 1,
+        perPage: MAX_RESULTS_WINDOW,
         namespaces: ['*'],
-        perPage: 1,
-        filter: `alert.attributes.alertTypeId: ${LEGACY_NOTIFICATIONS_ID}`,
-      })
-    ).total;
-    rulesUsage = { ...rulesUsage, legacy_notifications: { total: legacyNotificationsCount } };
+      });
+
+    const legacyNotificationRuleIds = legacyRuleActions.saved_objects.reduce(
+      (cache, legacyNotificationsObject) => {
+        const ruleRef = legacyNotificationsObject.references.find(
+          (reference) => reference.name === 'alert_0' && reference.type === 'alert'
+        );
+        if (ruleRef != null) {
+          const enabled = legacyNotificationsObject.attributes.ruleThrottle !== 'no_actions';
+          cache.set(ruleRef.id, { enabled });
+        }
+        return cache;
+      },
+      new Map<string, { enabled: boolean }>()
+    );
 
     const casesCache = cases.saved_objects.reduce((cache, { attributes: casesObject }) => {
       const ruleId = casesObject.rule.id;
@@ -320,6 +452,17 @@ export const getDetectionRuleMetrics = async (
       const ruleObjects = ruleResults.hits.hits.map((hit) => {
         const ruleId = hit._id.split(':')[1];
         const isElastic = isElasticRule(hit._source?.alert.tags);
+
+        // Even if the legacy notification is set to "no_actions" we still count the rule as having a legacy notification that is not migrated yet.
+        const hasLegacyNotification = legacyNotificationRuleIds.get(ruleId) != null;
+
+        // We only count a rule as having a notification and being "enabled" if it is _not_ set to "no_actions"/"muteAll" and it has at least one action within its array.
+        const hasNotification =
+          !hasLegacyNotification &&
+          hit._source?.alert.actions != null &&
+          hit._source?.alert.actions.length > 0 &&
+          hit._source?.alert.muteAll !== true;
+
         return {
           rule_name: hit._source?.alert.name,
           rule_id: hit._source?.alert.params.ruleId,
@@ -331,6 +474,8 @@ export const getDetectionRuleMetrics = async (
           updated_on: hit._source?.alert.updatedAt,
           alert_count_daily: alertsCache.get(ruleId) || 0,
           cases_count_total: casesCache.get(ruleId) || 0,
+          has_legacy_notification: hasLegacyNotification,
+          has_notification: hasNotification,
         } as DetectionRuleMetric;
       });
 
