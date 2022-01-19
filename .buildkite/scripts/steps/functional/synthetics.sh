a60cdb5256c1c4ef5ead8d752b5830f906c82feb
Dominique Clarke <dominique.clarke@elastic.co>
2022-01-19 18:48:58 -0500

    Revert "remove artifacts step"
    This reverts commit ba751bca842b95a396c0eb937f12df87f11411c9.

--- .buildkite/scripts/steps/functional/synthetics.sh
+++ .buildkite/scripts/steps/functional/synthetics.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
+.buildkite/scripts/bootstrap.sh
+.buildkite/scripts/download_build_artifacts.sh
+
 export JOB=kibana-uptime-playwright
 
 echo "--- Uptime @elastic/synthetics Tests"
