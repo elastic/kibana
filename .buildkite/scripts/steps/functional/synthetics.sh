ba751bca842b95a396c0eb937f12df87f11411c9
Dominique Clarke <dominique.clarke@elastic.co>
2022-01-19 16:14:52 -0500

    remove artifacts step

--- .buildkite/scripts/steps/functional/synthetics.sh
+++ .buildkite/scripts/steps/functional/synthetics.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
-.buildkite/scripts/bootstrap.sh
-.buildkite/scripts/download_build_artifacts.sh
-
 export JOB=kibana-uptime-playwright
 
 echo "--- Uptime @elastic/synthetics Tests"
