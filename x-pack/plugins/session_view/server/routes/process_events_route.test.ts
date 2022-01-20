e37a06c1c30b00b1240b1577b510d1a2c24da035
mitodrummer <karlgodard@elastic.co>
2022-01-20 15:58:27 -0800

    initial test added for process routes. mock in place. more empty stubs added.

--- x-pack/plugins/session_view/server/routes/process_events_route.test.ts
+++ x-pack/plugins/session_view/server/routes/process_events_route.test.ts
@@ -6,20 +6,42 @@
  */
 import { elasticsearchServiceMock } from '../../../../../src/core/server/elasticsearch/elasticsearch_service.mock';
 import { doSearch } from './process_events_route';
+const getEmptyResponse = async () => {
+
+  return {
+    body: {
+      hits: { 
+        total: { value: 0, relation: 'eq' }, 
+        hits: [] 
+      } 
+    }
+  }
+}
 
 describe('process_events_route.ts', () => {
-  beforeEach(() => {
-  });
-
   describe('doSearch(client, entityId, cursor, forward)', () => {
-    describe('with non existant session entity_id', () => {
-      it('should return an empty events array', async () => {
-        const client = elasticsearchServiceMock.createScopedClusterClient();
-        const body = await doSearch(client, 'asdf', undefined);
+    it('should return an empty events array for a non existant entity_id', async () => {
+      const client = elasticsearchServiceMock.createElasticsearchClient(getEmptyResponse());
 
-        expect(body.events.length).toBe(0);
-      });
+      const body = await doSearch(client, 'asdf', undefined);
 
+      expect(body.events.length).toBe(0);
+    });
+
+    it('returns results for a particular session entity_id', () => {
+      throw "not implemented";
+    });
+    
+    it('has a page size which mathces Constants.PROCESS_EVENTS_PER_PAGE', () => {
+      throw "not implemented";
+    });
+    
+    it('the date of the last event in the first page can be used as a cursor for the next page', () => {
+      throw "not implemented";
+    });
+    
+    it('should allow reverse pagination by setting forward=false', () => {
+      throw "not implemented";
     });
   });
 });
