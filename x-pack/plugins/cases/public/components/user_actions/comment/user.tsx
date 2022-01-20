8cf3ec1bef74c3f3a6457eb71e665f29c917ad42
Esteban Beltran <esteban.beltran@elastic.co>
2022-01-20 15:29:14 +0100

    Fix error commited by mistake

--- x-pack/plugins/cases/public/components/user_actions/comment/user.tsx
+++ x-pack/plugins/cases/public/components/user_actions/comment/user.tsx
@@ -43,7 +43,7 @@ export const createUserAttachmentUserActionBuilder = ({
   handleSaveComment,
   handleManageQuote,
 }: BuilderArgs): ReturnType<UserActionBuilder> => ({
-  build: Builder() => [
+  build: () => [
     {
       username: (
         <UserActionUsername
