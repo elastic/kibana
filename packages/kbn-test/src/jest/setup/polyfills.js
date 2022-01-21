81d77077335359d309c6a5c3135c11583d1b8169
nickofthyme <nicholas.partridge@elastic.co>
2022-01-21 10:24:29 -0600

    add ResizeObserver pollyfill to jest setup

--- packages/kbn-test/src/jest/setup/polyfills.js
+++ packages/kbn-test/src/jest/setup/polyfills.js
@@ -18,3 +18,5 @@ if (!global.URL.hasOwnProperty('createObjectURL')) {
 // Will be replaced with a better solution in EUI
 // https://github.com/elastic/eui/issues/3713
 global._isJest = true;
+
+global.ResizeObserver = require('resize-observer-polyfill');
