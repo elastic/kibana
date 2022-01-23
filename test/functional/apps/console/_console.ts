960c48784c02bde2a423c69198026ca61a5a3e48
Brian Seeders <brian.seeders@elastic.co>
2022-01-23 16:12:23 -0500

    Revert "skip failing test suite (#123456)"
    This reverts commit ac72ef20f8717db1aa92c6f0b5e805da8cb565c7.

--- test/functional/apps/console/_console.ts
+++ test/functional/apps/console/_console.ts
@@ -27,8 +27,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const PageObjects = getPageObjects(['common', 'console']);
   const toasts = getService('toasts');
 
-  // Failing: See https://github.com/elastic/kibana/issues/123456
-  describe.skip('console app', function describeIndexTests() {
+  describe('console app', function describeIndexTests() {
     this.tags('includeFirefox');
     before(async () => {
       log.debug('navigateTo console');
