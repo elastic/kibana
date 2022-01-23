64b525a9a3c3d2c71d1cf4223d82c159e80f104c
mgiota <panagiota.mitsopoulou@elastic.co>
2022-01-24 00:05:20 +0100

    fix failing tests

--- x-pack/plugins/uptime/public/lib/alert_types/monitor_status.test.ts
+++ x-pack/plugins/uptime/public/lib/alert_types/monitor_status.test.ts
@@ -202,7 +202,7 @@ describe('monitor status alert type', () => {
       })
     ).toMatchInlineSnapshot(`
       Object {
-        "defaultActionMessage": "{{state.monitorName}} from {{state.observerLocation}} {{{state.statusMessage}}}",
+        "defaultActionMessage": "Monitor {{state.monitorName}} with url {{{state.monitorUrl}}} from {{state.observerLocation}} {{{state.statusMessage}}}. The latest error message is {{{state.latestErrorMessage}}}",
         "description": "Alert when a monitor is down or an availability threshold is breached.",
         "documentationUrl": [Function],
         "format": [Function],
