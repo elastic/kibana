ec2b9bf77bab9a9e24597598ce5dea3e116bf8c9
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-01-24 22:01:29 +0300

    [Discover] fix imports

--- src/plugins/discover/public/components/discover_grid/get_render_cell_value.tsx
+++ src/plugins/discover/public/components/discover_grid/get_render_cell_value.tsx
@@ -7,10 +7,7 @@
  */
 
 import React, { Fragment, useContext, useEffect, useMemo } from 'react';
-import {
-  euiLightVars as themeLight,
-  euiDarkVars as themeDark,
-} from '@kbn/ui-shared-deps-src/theme';
+import { euiLightVars as themeLight, euiDarkVars as themeDark } from '@kbn/ui-theme';
 import type { DataView } from 'src/plugins/data/common';
 
 import {
