ab44b01d7b990f39da6da13da8bca910da3579c4
nreese <reese.nathan@elastic.co>
2022-01-24 15:59:35 -0500

    break geojson_importer into abstract class

--- x-pack/plugins/file_upload/public/importer/geo/shapefile_importer/shapefile_importer.tsx
+++ x-pack/plugins/file_upload/public/importer/geo/shapefile_importer/shapefile_importer.tsx
@@ -7,32 +7,15 @@
 
 import React from 'react';
 import { ShapefileEditor } from './shapefile_editor';
-import { GeoFileImporter, GeoFilePreview } from '../types';
-import { CreateDocsResponse } from '../../types';
-import { Importer } from '../../importer';
-import { ES_FIELD_TYPES } from '../../../../../../../src/plugins/data/public';
+import { AbstractGeoFileImporter } from '../abstract_geo_file_importer';
 
 export const SHAPEFILE_TYPES = ['.shp'];
 
-export class ShapefileImporter extends Importer implements GeoFileImporter {
-  private _file: File;
+export class ShapefileImporter extends AbstractGeoFileImporter {
   private _dbfFile: File | null = null;
   private _prjFile: File | null = null;
   private _shxFile: File | null = null;
-  private _isActive = true;
-  private _geoFieldType: ES_FIELD_TYPES.GEO_POINT | ES_FIELD_TYPES.GEO_SHAPE =
-    ES_FIELD_TYPES.GEO_SHAPE;
-
-  constructor(file: File) {
-    super();
-
-    this._file = file;
-  }
-
-  public destroy() {
-    this._isActive = false;
-  }
-
+  
   public canPreview() {
     return this._dbfFile !== null && this._prjFile !== null && this._shxFile !== null;
   }
@@ -54,26 +37,5 @@ export class ShapefileImporter extends Importer implements GeoFileImporter {
         }}
       />
     );
-  }
-
-  public async previewFile(rowLimit?: number, sizeLimit?: number): Promise<GeoFilePreview> {
-    return {
-      features: [],
-      previewCoverage: 0,
-      hasPoints: false,
-      hasShapes: false,
-    };
-  }
-
-  public setGeoFieldType(geoFieldType: ES_FIELD_TYPES.GEO_POINT | ES_FIELD_TYPES.GEO_SHAPE) {
-    this._geoFieldType = geoFieldType;
-  }
-
-  public read(data: ArrayBuffer): { success: boolean } {
-    throw new Error('read(data: ArrayBuffer) not supported, use readFile instead.');
-  }
-
-  protected _createDocs(text: string): CreateDocsResponse {
-    throw new Error('_createDocs not implemented.');
   }
 }
