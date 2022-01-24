48d2d1c50b3bf562cc41d5f96e0b703666dcd8d3
Andrew Tate <andrew.tate@elastic.co>
2022-01-24 14:06:00 -0600

    add more documentation to mergeSavedObjectMigrationMaps comment

--- src/core/server/saved_objects/migrations/utils.ts
+++ src/core/server/saved_objects/migrations/utils.ts
@@ -16,6 +16,16 @@ import {
 
 /**
  * Merges two saved object migration maps.
+ *
+ * If there is a migration for a given version on only one of the maps,
+ * that migration function will be used:
+ *
+ * mergeSavedObjectMigrationMaps({ '1.2.3': f }, { '4.5.6': g }) -> { '1.2.3': f, '4.5.6': g }
+ *
+ * If there is a migration for a given version on both maps, the migrations will be composed:
+ *
+ * mergeSavedObjectMigrationMaps({ '1.2.3': f }, { '1.2.3': g }) -> { '1.2.3': (doc, context) => f(g(doc, context), context) }
+ *
  * @public
  */
 export const mergeSavedObjectMigrationMaps = (
