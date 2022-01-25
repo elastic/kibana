ba72e6c15036deaed34837c8db1e455a8782834a
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-01-25 12:50:14 +0200

    Added tests for legend.

--- src/plugins/chart_expressions/expression_partition_vis/public/utils/legend.test.ts
+++ src/plugins/chart_expressions/expression_partition_vis/public/utils/legend.test.ts
@@ -6,9 +6,9 @@
  * Side Public License, v 1.
  */
 
-import { ChartTypes } from '../../common/types';
+import { ChartTypes, LegendDisplay } from '../../common/types';
 import { createMockVisData } from '../mocks';
-import { isLegendFlat } from './legend';
+import { isLegendFlat, shouldShowLegend } from './legend';
 
 describe('isLegendFlat', () => {
   const visData = createMockVisData();
@@ -20,7 +20,7 @@ describe('isLegendFlat', () => {
       expect(flat).toBeTruthy();
     });
 
-    it('legend should be not flat for ${chartType} if split dimension is not specified', () => {
+    it(`legend should be not flat for ${chartType} if split dimension is not specified`, () => {
       const flat = isLegendFlat(chartType, undefined);
       expect(flat).toBeFalsy();
     });
@@ -40,4 +40,101 @@ describe('isLegendFlat', () => {
     const flat = isLegendFlat(ChartTypes.WAFFLE, undefined);
     expect(flat).toBeTruthy();
   });
+});
+
+describe('shouldShowLegend', () => {
+  const visData = createMockVisData();
+
+  const runCommonShouldShowLegendScenario = (chartType: ChartTypes) => {
+    it(`should hide legend if legendDisplay = hide for ${chartType}`, () => {
+      const show = shouldShowLegend(chartType, LegendDisplay.HIDE);
+      expect(show).toBeFalsy();
+    });
+
+    it(`should show legend if legendDisplay = show for ${chartType}`, () => {
+      const show = shouldShowLegend(chartType, LegendDisplay.SHOW);
+      expect(show).toBeTruthy();
+    });
+  };
+
+  const runShouldShowLegendDefaultBucketsScenario = (chartType: ChartTypes) => {
+    it(`should show legend if legendDisplay = default and multiple buckets for ${chartType}`, () => {
+      const show = shouldShowLegend(chartType, LegendDisplay.DEFAULT, [
+        visData.columns[0],
+        visData.columns[1],
+      ]);
+
+      expect(show).toBeTruthy();
+    });
+
+    it(`should hide legend if legendDisplay = default and one bucket or less for ${chartType}`, () => {
+      const show1 = shouldShowLegend(chartType, LegendDisplay.DEFAULT, [visData.columns[0]]);
+      expect(show1).toBeFalsy();
+
+      const show2 = shouldShowLegend(chartType, LegendDisplay.DEFAULT, []);
+      expect(show2).toBeFalsy();
+
+      const show3 = shouldShowLegend(chartType, LegendDisplay.DEFAULT);
+      expect(show3).toBeFalsy();
+    });
+  };
+
+  const runShouldShowLegendDefaultAlwaysFalsyScenario = (chartType: ChartTypes) => {
+    it(`should hide legend if legendDisplay = default and multiple buckets for ${chartType}`, () => {
+      const show = shouldShowLegend(chartType, LegendDisplay.DEFAULT, [
+        visData.columns[0],
+        visData.columns[1],
+      ]);
+
+      expect(show).toBeFalsy();
+    });
+
+    it(`should hide legend if legendDisplay = default and one bucket or less for ${chartType}`, () => {
+      const show1 = shouldShowLegend(chartType, LegendDisplay.DEFAULT, [visData.columns[0]]);
+      expect(show1).toBeFalsy();
+
+      const show2 = shouldShowLegend(chartType, LegendDisplay.DEFAULT, []);
+      expect(show2).toBeFalsy();
+
+      const show3 = shouldShowLegend(chartType, LegendDisplay.DEFAULT);
+      expect(show3).toBeFalsy();
+    });
+  };
+
+  const runShouldShowLegendDefaultAlwaysTruthyScenario = (chartType: ChartTypes) => {
+    it(`should show legend if legendDisplay = default and multiple buckets for ${chartType}`, () => {
+      const show = shouldShowLegend(chartType, LegendDisplay.DEFAULT, [
+        visData.columns[0],
+        visData.columns[1],
+      ]);
+
+      expect(show).toBeTruthy();
+    });
+
+    it(`should show legend if legendDisplay = default and one bucket or less for ${chartType}`, () => {
+      const show1 = shouldShowLegend(chartType, LegendDisplay.DEFAULT, [visData.columns[0]]);
+      expect(show1).toBeTruthy();
+
+      const show2 = shouldShowLegend(chartType, LegendDisplay.DEFAULT, []);
+      expect(show2).toBeTruthy();
+
+      const show3 = shouldShowLegend(chartType, LegendDisplay.DEFAULT);
+      expect(show3).toBeTruthy();
+    });
+  };
+
+  runCommonShouldShowLegendScenario(ChartTypes.PIE);
+  runShouldShowLegendDefaultBucketsScenario(ChartTypes.PIE);
+
+  runCommonShouldShowLegendScenario(ChartTypes.DONUT);
+  runShouldShowLegendDefaultBucketsScenario(ChartTypes.DONUT);
+
+  runCommonShouldShowLegendScenario(ChartTypes.TREEMAP);
+  runShouldShowLegendDefaultAlwaysFalsyScenario(ChartTypes.TREEMAP);
+
+  runCommonShouldShowLegendScenario(ChartTypes.MOSAIC);
+  runShouldShowLegendDefaultAlwaysFalsyScenario(ChartTypes.MOSAIC);
+
+  runCommonShouldShowLegendScenario(ChartTypes.WAFFLE);
+  runShouldShowLegendDefaultAlwaysTruthyScenario(ChartTypes.WAFFLE);
 });
