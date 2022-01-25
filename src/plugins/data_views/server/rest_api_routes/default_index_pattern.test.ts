a8965f0e34c31de2aa9745e6cede515600d58619
Matt Kime <matt@mattki.me>
2022-01-25 00:08:38 -0600

    partial progress, getting there

---
+++ src/plugins/data_views/server/rest_api_routes/default_index_pattern.test.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { setDefault } from './default_index_pattern';
+import { dataViewsService } from '../mocks';
+import { getUsageCollection } from './test_utils';
+
+describe('set default data view', () => {
+  it('call usageCollection', () => {
+    const usageCollection = getUsageCollection();
+    setDefault({
+      indexPatternsService: dataViewsService,
+      path: '/path',
+      usageCollection,
+      newDefaultId: '1',
+      force: false,
+    });
+    expect(usageCollection.incrementCounter).toBeCalledTimes(1);
+  });
+});
