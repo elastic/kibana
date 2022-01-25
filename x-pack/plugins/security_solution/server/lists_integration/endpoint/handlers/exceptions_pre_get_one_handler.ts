322046069f877a3f95821d543c20518d275933ab
Paul Tavares <56442535+paul-tavares@users.noreply.github.com>
2022-01-25 17:56:47 -0500

    [Security Solution][Endpoint] Register NOOP callbacks for all Lists extension points (#123761)

---
+++ x-pack/plugins/security_solution/server/lists_integration/endpoint/handlers/exceptions_pre_get_one_handler.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EndpointAppContextService } from '../../../endpoint/endpoint_app_context_services';
+import { ExtensionPoint } from '../../../../../lists/server';
+
+export const getExceptionsPreGetOneHandler = (
+  endpointAppContext: EndpointAppContextService
+): (ExtensionPoint & { type: 'exceptionsListPreGetOneItem' })['callback'] => {
+  return async function ({ data }) {
+    // Individual validators here
+
+    return data;
+  };
+};
--- x-pack/plugins/security_solution/server/lists_integration/endpoint/register_endpoint_extension_points.ts
+++ x-pack/plugins/security_solution/server/lists_integration/endpoint/register_endpoint_extension_points.ts
@@ -9,6 +9,12 @@ import { EndpointAppContextService } from '../../endpoint/endpoint_app_context_s
 import { getExceptionsPreCreateItemHandler } from './handlers/exceptions_pre_create_handler';
 import { getExceptionsPreUpdateItemHandler } from './handlers/exceptions_pre_update_handler';
 import type { ListsServerExtensionRegistrar } from '../../../../lists/server';
+import { getExceptionsPreGetOneHandler } from './handlers/exceptions_pre_get_one_handler';
+import { getExceptionsPreSummaryHandler } from './handlers/exceptions_pre_summary_handler';
+import { getExceptionsPreDeleteItemHandler } from './handlers/exceptions_pre_delete_item_handler';
+import { getExceptionsPreExportHandler } from './handlers/exceptions_pre_export_handler';
+import { getExceptionsPreMultiListFindHandler } from './handlers/exceptions_pre_multi_list_find_handler';
+import { getExceptionsPreSingleListFindHandler } from './handlers/exceptions_pre_single_list_find_handler';
 
 export const registerListsPluginEndpointExtensionPoints = (
   registerListsExtensionPoint: ListsServerExtensionRegistrar,
@@ -24,5 +30,41 @@ export const registerListsPluginEndpointExtensionPoints = (
   registerListsExtensionPoint({
     type: 'exceptionsListPreUpdateItem',
     callback: getExceptionsPreUpdateItemHandler(endpointAppContextService),
+  });
+
+  // PRE-GET ONE
+  registerListsExtensionPoint({
+    type: 'exceptionsListPreGetOneItem',
+    callback: getExceptionsPreGetOneHandler(endpointAppContextService),
+  });
+
+  // PRE-SUMMARY
+  registerListsExtensionPoint({
+    type: 'exceptionsListPreSummary',
+    callback: getExceptionsPreSummaryHandler(endpointAppContextService),
+  });
+
+  // PRE-DELETE item
+  registerListsExtensionPoint({
+    type: 'exceptionsListPreDeleteItem',
+    callback: getExceptionsPreDeleteItemHandler(endpointAppContextService),
+  });
+
+  // PRE-EXPORT
+  registerListsExtensionPoint({
+    type: 'exceptionsListPreExport',
+    callback: getExceptionsPreExportHandler(endpointAppContextService),
+  });
+
+  // PRE-MULTI-LIST-FIND
+  registerListsExtensionPoint({
+    type: 'exceptionsListPreMultiListFind',
+    callback: getExceptionsPreMultiListFindHandler(endpointAppContextService),
+  });
+
+  // PRE-SINGLE-LIST-FIND
+  registerListsExtensionPoint({
+    type: 'exceptionsListPreSingleListFind',
+    callback: getExceptionsPreSingleListFindHandler(endpointAppContextService),
   });
 };
