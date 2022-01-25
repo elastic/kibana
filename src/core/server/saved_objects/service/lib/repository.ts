7be45391d738a5d1922a32d0ba7b8e914f4a56e6
pgayvallet <pierre.gayvallet@elastic.co>
2022-01-25 10:41:34 +0100

    fix repo code

--- src/core/server/saved_objects/service/lib/repository.ts
+++ src/core/server/saved_objects/service/lib/repository.ts
@@ -304,7 +304,7 @@ export class SavedObjectsRepository {
    * @property {array} [options.references=[]] - [{ name, type, id }]
    * @returns {promise} - { id, type, version, attributes }
    */
-  public async create<T extends Record<string, unknown> = SavedObjectAttributes>(
+  public async create<T extends SavedObjectAttributes = SavedObjectAttributes>(
     type: string,
     attributes: T,
     options: SavedObjectsCreateOptions = {}
@@ -416,7 +416,7 @@ export class SavedObjectsRepository {
    * @property {string} [options.namespace]
    * @returns {promise} -  {saved_objects: [[{ id, type, version, references, attributes, error: { message } }]}
    */
-  async bulkCreate<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async bulkCreate<T extends SavedObjectAttributes = SavedObjectAttributes>(
     objects: Array<SavedObjectsBulkCreateObject<T>>,
     options: SavedObjectsCreateOptions = {}
   ): Promise<SavedObjectsBulkResponse<T>> {
@@ -895,7 +895,7 @@ export class SavedObjectsRepository {
    * @property {string} [options.preference]
    * @returns {promise} - { saved_objects: [{ id, type, version, attributes }], total, per_page, page }
    */
-  async find<T extends Record<string, unknown> = SavedObjectAttributes, A = unknown>(
+  async find<T extends SavedObjectAttributes = SavedObjectAttributes, A = unknown>(
     options: SavedObjectsFindOptions
   ): Promise<SavedObjectsFindResponse<T, A>> {
     const {
@@ -1068,7 +1068,7 @@ export class SavedObjectsRepository {
    *   { id: 'foo', type: 'index-pattern' }
    * ])
    */
-  async bulkGet<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async bulkGet<T extends SavedObjectAttributes = SavedObjectAttributes>(
     objects: SavedObjectsBulkGetObject[] = [],
     options: SavedObjectsBaseOptions = {}
   ): Promise<SavedObjectsBulkResponse<T>> {
@@ -1186,7 +1186,7 @@ export class SavedObjectsRepository {
    *   { id: 'foo', type: 'index-pattern' }
    * ])
    */
-  async bulkResolve<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async bulkResolve<T extends SavedObjectAttributes = SavedObjectAttributes>(
     objects: SavedObjectsBulkResolveObject[],
     options: SavedObjectsBaseOptions = {}
   ): Promise<SavedObjectsBulkResolveResponse<T>> {
@@ -1224,7 +1224,7 @@ export class SavedObjectsRepository {
    * @property {string} [options.namespace]
    * @returns {promise} - { id, type, version, attributes }
    */
-  async get<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async get<T extends SavedObjectAttributes = SavedObjectAttributes>(
     type: string,
     id: string,
     options: SavedObjectsBaseOptions = {}
@@ -1266,7 +1266,7 @@ export class SavedObjectsRepository {
    * @property {string} [options.namespace]
    * @returns {promise} - { saved_object, outcome }
    */
-  async resolve<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async resolve<T extends SavedObjectAttributes = SavedObjectAttributes>(
     type: string,
     id: string,
     options: SavedObjectsBaseOptions = {}
@@ -1299,7 +1299,7 @@ export class SavedObjectsRepository {
    * @property {array} [options.references] - [{ name, type, id }]
    * @returns {promise}
    */
-  async update<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async update<T extends SavedObjectAttributes = SavedObjectAttributes>(
     type: string,
     id: string,
     attributes: Partial<T>,
@@ -1472,7 +1472,7 @@ export class SavedObjectsRepository {
    * @property {string} [options.namespace]
    * @returns {promise} -  {saved_objects: [[{ id, type, version, references, attributes, error: { message } }]}
    */
-  async bulkUpdate<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async bulkUpdate<T extends SavedObjectAttributes = SavedObjectAttributes>(
     objects: Array<SavedObjectsBulkUpdateObject<T>>,
     options: SavedObjectsBulkUpdateOptions = {}
   ): Promise<SavedObjectsBulkUpdateResponse<T>> {
@@ -1808,7 +1808,7 @@ export class SavedObjectsRepository {
    * @param options - {@link SavedObjectsIncrementCounterOptions}
    * @returns The saved object after the specified fields were incremented
    */
-  async incrementCounter<T extends Record<string, unknown> = SavedObjectAttributes>(
+  async incrementCounter<T extends SavedObjectAttributes = SavedObjectAttributes>(
     type: string,
     id: string,
     counterFields: Array<string | SavedObjectsIncrementCounterField>,
@@ -1838,7 +1838,7 @@ export class SavedObjectsRepository {
   }
 
   /** @internal incrementCounter function that is used internally and bypasses validation checks. */
-  private async incrementCounterInternal<T extends Record<string, unknown> = SavedObjectAttributes>(
+  private async incrementCounterInternal<T extends SavedObjectAttributes = SavedObjectAttributes>(
     type: string,
     id: string,
     counterFields: Array<string | SavedObjectsIncrementCounterField>,
@@ -2177,7 +2177,7 @@ export class SavedObjectsRepository {
     return unique(types.map((t) => this.getIndexForType(t)));
   }
 
-  private _rawToSavedObject<T extends Record<string, unknown> = SavedObjectAttributes>(
+  private _rawToSavedObject<T extends SavedObjectAttributes = SavedObjectAttributes>(
     raw: SavedObjectsRawDoc
   ): SavedObject<T> {
     const savedObject = this._serializer.rawToSavedObject(raw);
