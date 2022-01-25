a395dfac458ae43310c3699e7c9c0ccf410fee07
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-01-25 15:40:46 +0100

    update config to enable apm

--- x-pack/test/load/config.ts
+++ x-pack/test/load/config.ts
@@ -40,6 +40,23 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
         '--no-base-path',
         '--env.name=development',
       ],
+      env: {
+        ELASTIC_APM_ACTIVE: process.env.ELASTIC_APM_ACTIVE,
+        ELASTIC_APM_CONTEXT_PROPAGATION_ONLY: 'false',
+        ELASTIC_APM_ENVIRONMENT: process.env.CI ? 'ci' : 'development',
+        ELASTIC_APM_TRANSACTION_SAMPLE_RATE: '1.0',
+        ELASTIC_APM_SERVER_URL: 'https://apm-testing.apm.us-central1.gcp.foundit.no',
+        ELASTIC_APM_SECRET_TOKEN: 'bwF17RD4rmj6JcdvKR',
+        ELASTIC_APM_GLOBAL_LABELS: Object.entries({
+          simulation: process.env.GATLING_SIMULATIONS
+            ? process.env.GATLING_SIMULATIONS
+            : 'unknown simulation',
+        })
+          .filter(([, v]) => !!v)
+          .reduce((acc, [k, v]) => (acc ? `${acc},${k}=${v}` : `${k}=${v}`), ''),
+      },
+      // delay shutdown by 15 seconds to ensure that APM can report the data it collects during test execution
+      delayShutdown: 15_000,
     },
   };
 }
