a3fc3b4bac8c3837eed83b5e9524e205b595db1e
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-01-26 21:42:05 +0300

    [Discover] fix jest test

--- src/plugins/discover/public/services/doc_views/components/doc_viewer/doc_viewer.test.tsx
+++ src/plugins/discover/public/services/doc_views/components/doc_viewer/doc_viewer.test.tsx
@@ -51,7 +51,7 @@ beforeEach(() => {
 test('Render <DocViewer/> with 3 different tabs', () => {
   const registry = getDocViewsRegistry();
   registry.addDocView({ order: 10, title: 'Render function', render: jest.fn() });
-  registry.addDocView({ order: 20, title: 'React component', Component: () => <div>test</div> });
+  registry.addDocView({ order: 20, title: 'React component', component: () => <div>test</div> });
   // @ts-expect-error This should be invalid and will throw an error when rendering
   registry.addDocView({ order: 30, title: 'Invalid doc view' });
 
@@ -72,7 +72,7 @@ test('Render <DocViewer/> with 1 tab displaying error message', () => {
   registry.addDocView({
     order: 10,
     title: 'React component',
-    Component: SomeComponent,
+    component: SomeComponent,
   });
 
   const renderProps = { hit: {} } as DocViewRenderProps;
