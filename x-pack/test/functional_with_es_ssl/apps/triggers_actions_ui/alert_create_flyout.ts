8c7853834465dd4dc757f4388fb1f7c557281d98
Zacqary Xeper <zacqary.xeper@elastic.co>
2022-01-26 15:44:24 -0600

    Reenable alert_create_flyout create alert test

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alert_create_flyout.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alert_create_flyout.ts
@@ -105,8 +105,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     await testSubjects.click('test.always-firing-SelectOption');
   }
 
-  // Failing: See https://github.com/elastic/kibana/issues/89397
-  describe.skip('create alert', function () {
+  describe('create alert', function () {
     before(async () => {
       await pageObjects.common.navigateToApp('triggersActions');
       await testSubjects.click('rulesTab');
