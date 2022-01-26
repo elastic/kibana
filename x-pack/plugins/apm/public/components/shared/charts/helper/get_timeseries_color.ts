f3ac72c3794aaf37997f647c2c24eb27e99f1201
gbamparop <georgios.bamparopoulos@elastic.co>
2022-01-26 18:15:55 +0000

    Refactor timeSeriesColorMap

--- x-pack/plugins/apm/public/components/shared/charts/helper/get_timeseries_color.ts
+++ x-pack/plugins/apm/public/components/shared/charts/helper/get_timeseries_color.ts
@@ -7,29 +7,52 @@
 
 import { euiPaletteColorBlind } from '@elastic/eui';
 
-const palette = euiPaletteColorBlind({ rotations: 2 });
-
 export const enum ChartType {
   LATENCY_AVG,
+  LATENCY_P95,
+  LATENCY_P99,
   THROUGHPUT,
   FAILED_TRANSACTION_RATE,
   CPU_USAGE,
   MEMORY_USAGE,
 }
 
-const colorMap: Record<ChartType, [number, number]> = {
-  [ChartType.LATENCY_AVG]: [1, 11],
-  [ChartType.THROUGHPUT]: [0, 10],
-  [ChartType.FAILED_TRANSACTION_RATE]: [7, 17],
-  [ChartType.CPU_USAGE]: [3, 13],
-  [ChartType.MEMORY_USAGE]: [8, 18],
+const palette = euiPaletteColorBlind({ rotations: 2 });
+
+const timeSeriesColorMap: Record<
+  ChartType,
+  { currentPeriodColor: string; previousPeriodColor: string }
+> = {
+  [ChartType.LATENCY_AVG]: {
+    currentPeriodColor: palette[1],
+    previousPeriodColor: palette[11],
+  },
+  [ChartType.LATENCY_P95]: {
+    currentPeriodColor: palette[5],
+    previousPeriodColor: palette[15],
+  },
+  [ChartType.LATENCY_P99]: {
+    currentPeriodColor: palette[7],
+    previousPeriodColor: palette[17],
+  },
+  [ChartType.THROUGHPUT]: {
+    currentPeriodColor: palette[0],
+    previousPeriodColor: palette[10],
+  },
+  [ChartType.FAILED_TRANSACTION_RATE]: {
+    currentPeriodColor: palette[7],
+    previousPeriodColor: palette[17],
+  },
+  [ChartType.CPU_USAGE]: {
+    currentPeriodColor: palette[3],
+    previousPeriodColor: palette[13],
+  },
+  [ChartType.MEMORY_USAGE]: {
+    currentPeriodColor: palette[8],
+    previousPeriodColor: palette[18],
+  },
 };
 
 export function getTimeSeriesColor(chartType: ChartType) {
-  const val = colorMap[chartType];
-
-  return {
-    currentPeriodColor: palette[val[0]] || palette[0],
-    previousPeriodColor: palette[val[1]] || palette[1],
-  };
+  return timeSeriesColorMap[chartType];
 }
