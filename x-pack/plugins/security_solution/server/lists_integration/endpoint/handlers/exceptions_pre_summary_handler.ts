09cdb6c2f644a077295d03818efe9ef32f9a96d7
Paul Tavares <paul.tavares@elastic.co>
2022-01-26 17:27:27 -0500

    Validate host isolation exceptions Summary

--- x-pack/plugins/security_solution/server/lists_integration/endpoint/handlers/exceptions_pre_summary_handler.ts
+++ x-pack/plugins/security_solution/server/lists_integration/endpoint/handlers/exceptions_pre_summary_handler.ts
@@ -6,13 +6,30 @@
  */
 
 import { EndpointAppContextService } from '../../../endpoint/endpoint_app_context_services';
-import { ExtensionPoint } from '../../../../../lists/server';
+import { ExceptionsListPreSummaryServerExtension } from '../../../../../lists/server';
+import { HostIsolationExceptionsValidator } from '../validators/host_isolation_exceptions_validator';
 
 export const getExceptionsPreSummaryHandler = (
   endpointAppContext: EndpointAppContextService
-): (ExtensionPoint & { type: 'exceptionsListPreSummary' })['callback'] => {
-  return async function ({ data }) {
-    // Individual validators here
+): ExceptionsListPreSummaryServerExtension['callback'] => {
+  return async function ({ data, context: { request } }) {
+    const { listId: maybeListId, id } = data;
+    let listId: string | null | undefined = maybeListId;
+
+    if (!listId && id) {
+      listId =
+        (await endpointAppContext.getExceptionListsClient().getExceptionList(data))?.list_id ??
+        null;
+    }
+
+    if (!listId) {
+      return data;
+    }
+
+    // Host Isolation Exceptions
+    if (HostIsolationExceptionsValidator.isHostIsolationException(listId)) {
+      await new HostIsolationExceptionsValidator(endpointAppContext, request).validatePreSummary();
+    }
 
     return data;
   };
