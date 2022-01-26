66d921f71a6b6d550f414aa43f9a14f7da14c568
Tyler Smalley <tyler.smalley@elastic.co>
2022-01-25 21:21:58 -0800

    TEMP
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/common/env.sh
+++ .buildkite/scripts/common/env.sh
@@ -101,8 +101,8 @@ export DISABLE_BOOTSTRAP_VALIDATION=true
 
 export TEST_KIBANA_HOST=localhost
 export TEST_KIBANA_PORT=6101
-export TEST_KIBANA_URL="http://elastic:changeme@localhost:6101"
-export TEST_ES_URL="http://elastic:changeme@localhost:6102"
+export TEST_KIBANA_URL="http://system_indices_superuser:changeme@localhost:6101"
+export TEST_ES_URL="http://system_indices_superuser:changeme@localhost:6102"
 export TEST_ES_TRANSPORT_PORT=6301-6309
 export TEST_CORS_SERVER_PORT=6106
 export ALERTING_PROXY_PORT=6105
