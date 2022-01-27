07a3bb6b306a1124f20218100361e35b4ddb9dd7
Maja Grubic <maja.grubic@elastic.co>
2022-01-27 15:05:58 +0100

    [Discover] Redirect if no data views (#123366)
    * [Discover] Redirect if new Kibana instance
    
    * Add a functional test
    
    * Remove state; add redirect
    
    * Code polishing
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- test/functional/apps/discover/index.ts
+++ test/functional/apps/discover/index.ts
@@ -54,5 +54,6 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./_search_on_page_load'));
     loadTestFile(require.resolve('./_chart_hidden'));
     loadTestFile(require.resolve('./_context_encoded_url_param'));
+    loadTestFile(require.resolve('./_empty_state'));
   });
 }
