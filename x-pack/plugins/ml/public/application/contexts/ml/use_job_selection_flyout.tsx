a8593a384c72d44974c304f0d7ca88677ea6802e
Dima Arnautov <dmitrii.arnautov@elastic.co>
2022-01-27 13:27:33 +0100

    SMV job selection flyout on open

--- x-pack/plugins/ml/public/application/contexts/ml/use_job_selection_flyout.tsx
+++ x-pack/plugins/ml/public/application/contexts/ml/use_job_selection_flyout.tsx
@@ -13,6 +13,8 @@ import { JobSelectorFlyout } from '../../../embeddables/common/components/job_se
 import { getInitialGroupsMap } from '../../components/job_selector/job_selector';
 import type { JobSelectionResult } from '../../components/job_selector/job_selector_flyout';
 
+export type GetJobSelection = ReturnType<typeof useJobSelectionFlyout>;
+
 /**
  * Hook for invoking Anomaly Detection jobs selection
  * inside the ML app.
