4b0c3a91047c7d80dfe754fc6be7c9fc75daa404
Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>
2022-01-27 12:35:11 +0300

    Allow to use correct max value in percentage mode for heatmap

--- src/plugins/chart_expressions/expression_heatmap/public/components/heatmap_component.tsx
+++ src/plugins/chart_expressions/expression_heatmap/public/components/heatmap_component.tsx
@@ -270,8 +270,13 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = memo(
 
     // adds a very small number to the max value to make sure the max value will be included
     const smattering = 0.00001;
-    const endValue =
-      (paletteParams?.range === 'number' ? paletteParams.rangeMax : max) + smattering;
+    let endValue = max + smattering;
+    if (paletteParams?.rangeMax) {
+      endValue =
+        (paletteParams?.range === 'number'
+          ? paletteParams.rangeMax
+          : min + ((max - min) * paletteParams.rangeMax) / 100) + smattering;
+    }
 
     const overwriteColors = uiState?.get('vis.colors') ?? null;
 
