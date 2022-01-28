6c1065bc302ff1f837646036e42df028d3a0b3aa
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2022-01-28 17:17:50 +0000

    CR: explanation behind max number of ids in bulk API

--- x-pack/plugins/security_solution/common/constants.ts
+++ x-pack/plugins/security_solution/common/constants.ts
@@ -413,7 +413,11 @@ export const MAX_RULES_TO_UPDATE_IN_PARALLEL = 50;
 export const LIMITED_CONCURRENCY_ROUTE_TAG_PREFIX = `${APP_ID}:limitedConcurrency`;
 
 /**
- * Max number of rules to display on UI in table, hence max number of rules that can be edited in a single bulk edit API request
+ * Max number of rules to display on UI in table, max number of rules that can be edited in a single bulk edit API request
+ * We limit number of rules in bulk edit API, because rulesClient doesn't support bulkGet of rules by ids.
+ * Given this limitation, current implementation fetches each rule separately through rulesClient.resolve method.
+ * As max number of rules displayed on a page is 100, max 100 rules can be bulk edited by passing their ids to API.
+ * We decided add this limit(number of ids less than 100) in bulk edit API as well, to prevent a huge number of single rule fetches
  */
 export const RULES_TABLE_MAX_PAGE_SIZE = 100;
 export const RULES_TABLE_PAGE_SIZE_OPTIONS = [5, 10, 20, 50, RULES_TABLE_MAX_PAGE_SIZE];
