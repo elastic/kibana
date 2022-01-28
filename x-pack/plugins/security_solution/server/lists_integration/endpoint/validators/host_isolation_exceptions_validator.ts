8678a2099e1f44f833fb0da7265f4973566be58e
Joey F. Poon <joey.poon@elastic.co>
2022-01-28 13:49:07 -0600

    [Security Solution] add api validations for host isolation

--- x-pack/plugins/security_solution/server/lists_integration/endpoint/validators/host_isolation_exceptions_validator.ts
+++ x-pack/plugins/security_solution/server/lists_integration/endpoint/validators/host_isolation_exceptions_validator.ts
@@ -6,12 +6,45 @@
  */
 
 import { ENDPOINT_HOST_ISOLATION_EXCEPTIONS_LIST_ID } from '@kbn/securitysolution-list-constants';
+import { ExceptionListItemSchema } from '@kbn/securitysolution-io-ts-list-types';
 import { BaseValidator } from './base_validator';
 import { EndpointArtifactExceptionValidationError } from './errors';
+
+import { ExceptionItemLikeOptions } from '../types';
+
+import {
+  CreateExceptionListItemOptions,
+  UpdateExceptionListItemOptions,
+} from '../../../../../lists/server';
 
 export class HostIsolationExceptionsValidator extends BaseValidator {
   static isHostIsolationException(listId: string): boolean {
     return listId === ENDPOINT_HOST_ISOLATION_EXCEPTIONS_LIST_ID;
+  }
+
+  async validatePreCreateItem(
+    item: CreateExceptionListItemOptions
+  ): Promise<CreateExceptionListItemOptions> {
+    // must be plat -- authz.canIsolateHost
+    await this.validateCanIsolateHosts();
+    // policy validation -- validateByPolicyItem
+    await this.validateByPolicyItem(item);
+    // ip validation - this schema type might already exist
+    await this.validateHostIsolationData(item);
+
+    return item;
+  }
+
+  async validatePreUpdateItem(
+    _updatedItem: UpdateExceptionListItemOptions,
+    currentItem: ExceptionListItemSchema
+  ): Promise<UpdateExceptionListItemOptions> {
+    await this.validateCanIsolateHosts();
+    // must be plat
+    // policy validation
+    // ip validation
+
+    return _updatedItem;
   }
 
   async validatePreGetOneItem(): Promise<void> {
@@ -42,5 +75,9 @@ export class HostIsolationExceptionsValidator extends BaseValidator {
     throw new EndpointArtifactExceptionValidationError(
       'Import is not supported for Endpoint artifact exceptions'
     );
+  }
+
+  private async validateHostIsolationData(item: ExceptionItemLikeOptions): Promise<void> {
+    await this.validateBasicData(item);
   }
 }
