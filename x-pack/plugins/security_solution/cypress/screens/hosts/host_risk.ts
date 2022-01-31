5a32f14d4673a506fdaf431a88a4a2f265baa688
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2022-01-31 15:00:35 +0100

    Add cypress test to risk by host tab

--- x-pack/plugins/security_solution/cypress/screens/hosts/host_risk.ts
+++ x-pack/plugins/security_solution/cypress/screens/hosts/host_risk.ts
@@ -14,3 +14,13 @@ export const RISK_DETAILS_NAV = '[data-test-subj="navigation-hostRisk"]';
 export const RISK_FLYOUT_TRIGGER = '[data-test-subj="open-risk-information-flyout-trigger"]';
 
 export const LOADING_TABLE = '.euiBasicTable-loading';
+
+export const HOST_BY_RISK_TABLE = '.table-hostRisk-loading-false';
+
+export const HOST_BY_RISK_TABLE_CELL =
+  '[data-test-subj="table-hostRisk-loading-false"] .euiTableCellContent';
+
+export const HOST_BY_RISK_TABLE_FILTER = '[data-test-subj="host-risk-filter-button"]';
+
+export const HOST_BY_RISK_TABLE_FILTER_CRITICAL =
+  '[data-test-subj="host-risk-filter-item-Critical"]';
