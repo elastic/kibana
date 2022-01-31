7bd74b65c619acb80b85498536878fcad1f1105d
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-01-31 21:21:10 +0100

    [code coverage] collect coverage on server side

--- .buildkite/scripts/steps/code_coverage/oss_cigroup.sh
+++ .buildkite/scripts/steps/code_coverage/oss_cigroup.sh
@@ -20,9 +20,17 @@ export CODE_COVERAGE=1
 echo "--- OSS CI Group $CI_GROUP"
 echo " -> Running Functional tests with code coverage"
 
-node scripts/functional_tests \
+
+
+NODE_OPTIONS=--max_old_space_size=14336 \
+  ./node_modules/.bin/nyc \
+  --nycrc-path src/dev/code_coverage/nyc_config/nyc.api_integration.config.js \
+  node scripts/functional_tests \
   --include-tag "ciGroup$CI_GROUP" \
   --exclude-tag "skipCoverage" || true
+
+if [[ -d "$KIBANA_DIR/target/kibana-coverage/server" ]]; then
+  echo "Server side code coverage collected"
 
 if [[ -d "$KIBANA_DIR/target/kibana-coverage/functional" ]]; then
   echo "--- Merging code coverage for CI Group $CI_GROUP"
--- scripts/functional_tests.js
+++ scripts/functional_tests.js
@@ -25,10 +25,10 @@ const alwaysImportedTests = [
   require.resolve(
     '../test/interactive_setup_functional/manual_configuration_without_tls.config.ts'
   ),
+  require.resolve('../test/api_integration/config.js'),
 ];
 // eslint-disable-next-line no-restricted-syntax
 const onlyNotInCoverageTests = [
-  require.resolve('../test/api_integration/config.js'),
   require.resolve('../test/interpreter_functional/config.ts'),
   require.resolve('../test/examples/config.js'),
 ];
