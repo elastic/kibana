c8583e2298f2c62eb48799bea09502155535323a
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-01-31 11:41:26 +0100

    added comment

--- x-pack/plugins/screenshotting/server/layouts/create_layout.ts
+++ x-pack/plugins/screenshotting/server/layouts/create_layout.ts
@@ -13,6 +13,11 @@ import { CanvasLayout } from './canvas_layout';
 import { PreserveLayout } from './preserve_layout';
 import { PrintLayout } from './print_layout';
 
+/**
+ * We naively round all numeric values in the object, this will break screenshotting
+ * if ever a have a non-number set as a value, but this points to an issue
+ * in the code responsible for creating the dimensions object.
+ */
 const roundNumbers = mapRecord(Math.round);
 
 export function createLayout({ id, dimensions, selectors, ...config }: LayoutParams): Layout {
