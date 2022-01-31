1decd8c3fd325184b4c264f9df4ba531e91c5691
shahzad31 <shahzad.muhammad@elastic.co>
2022-01-31 10:26:48 +0100

    fix issue while editing

--- x-pack/plugins/uptime/public/components/monitor_management/monitor_config/use_monitor_name.test.tsx
+++ x-pack/plugins/uptime/public/components/monitor_management/monitor_config/use_monitor_name.test.tsx
@@ -9,13 +9,17 @@ import { defaultCore, WrappedHelper } from '../../../lib/helper/rtl_helpers';
 import { renderHook } from '@testing-library/react-hooks';
 import { useMonitorName } from './use_monitor_name';
 
+import * as hooks from '../../../hooks/use_monitor';
+
 describe('useMonitorName', () => {
   it('returns expected results', () => {
     const { result } = renderHook(() => useMonitorName({}), { wrapper: WrappedHelper });
 
     expect(result.current).toStrictEqual({ nameAlreadyExists: false, validName: '' });
     expect(defaultCore.savedObjects.client.find).toHaveBeenCalledWith({
-      aggs: { monitorNames: { terms: { field: 'synthetics-monitor.attributes.name' } } },
+      aggs: {
+        monitorNames: { terms: { field: 'synthetics-monitor.attributes.name', size: 10000 } },
+      },
       perPage: 0,
       type: 'synthetics-monitor',
     });
@@ -39,5 +43,27 @@ describe('useMonitorName', () => {
     await waitForNextUpdate();
 
     expect(result.current).toStrictEqual({ nameAlreadyExists: true, validName: '' });
+  });
+
+  it('returns expected results after data while editing monitor', async () => {
+    defaultCore.savedObjects.client.find = jest.fn().mockReturnValue({
+      aggregations: {
+        monitorNames: {
+          buckets: [{ key: 'Test' }, { key: 'Test 1' }],
+        },
+      },
+    });
+
+    jest.spyOn(hooks, 'useMonitorId').mockReturnValue('test-id');
+
+    const { result, waitForNextUpdate } = renderHook(() => useMonitorName({ search: 'Test' }), {
+      wrapper: WrappedHelper,
+    });
+
+    expect(result.current).toStrictEqual({ nameAlreadyExists: false, validName: 'Test' });
+
+    await waitForNextUpdate();
+
+    expect(result.current).toStrictEqual({ nameAlreadyExists: false, validName: 'Test' });
   });
 });
