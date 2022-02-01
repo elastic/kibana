e3db48d422b5811d81475ee1743d947c34155128
pgayvallet <pierre.gayvallet@elastic.co>
2022-02-01 08:28:31 +0100

    update links due to merge

--- packages/kbn-doc-links/src/types.ts
+++ packages/kbn-doc-links/src/types.ts
@@ -21,7 +21,8 @@ export interface DocLinks {
   readonly settings: string;
   readonly elasticStackGetStarted: string;
   readonly upgrade: {
-    readonly upgradingElasticStack: string;
+    readonly upgradingStackOnPrem: string;
+    readonly upgradingStackOnCloud: string;
   };
   readonly apm: {
     readonly kibanaSettings: string;
