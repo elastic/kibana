d5877d711dd095cb85ee59f0cba1cf9c42f78a27
pgayvallet <pierre.gayvallet@elastic.co>
2022-02-01 08:23:47 +0100

    update tsdoc

--- src/core/server/doc_links/types.ts
+++ src/core/server/doc_links/types.ts
@@ -10,8 +10,11 @@ import type { DocLinks } from '@kbn/doc-links';
 
 /** @public */
 export interface DocLinksServiceSetup {
+  /** The branch/version the docLinks are pointing to */
   readonly version: string;
+  /** The base url for the elastic website */
   readonly elasticWebsiteUrl: string;
+  /** A record of all registered doc links */
   readonly links: DocLinks;
 }
 
