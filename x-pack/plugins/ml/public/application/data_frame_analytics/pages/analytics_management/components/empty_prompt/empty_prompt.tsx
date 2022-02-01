a38c73524e6ed1ec062ab1fb800ca3dcd2f2dd52
István Zoltán Szabó <szabosteve@gmail.com>
2022-02-01 10:44:49 +0100

    [ML] Addresses feedback.

--- x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_management/components/empty_prompt/empty_prompt.tsx
+++ x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_management/components/empty_prompt/empty_prompt.tsx
@@ -63,7 +63,7 @@ export const AnalyticsEmptyPrompt: FC<Props> = ({ disabled, onCreateFirstJobClic
           <p>
             <FormattedMessage
               id="xpack.ml.overview.analyticsList.emptyPromptText"
-              defaultMessage="Perform outlier detection, regression, or classification analysis and put the annotated data in a new index. Create trained models to add inference in pipelines and aggregations."
+              defaultMessage="Build and train outlier detection, regression, or classification machine learning models using data frame analytics."
             />
           </p>
           <EuiCallOut
@@ -71,8 +71,16 @@ export const AnalyticsEmptyPrompt: FC<Props> = ({ disabled, onCreateFirstJobClic
             title={
               <FormattedMessage
                 id="xpack.ml.overview.analyticsList.emptyPromptHelperText"
-                defaultMessage="Data frame analytics requires {sourcedata}. Use {transforms} to create data frames before you create the jobs."
+                defaultMessage="Before building a data frame analytics job, use {transforms} to construct an {sourcedata} to analyze."
                 values={{
+                  transforms: (
+                    <EuiLink href={transformsLink} target="blank" color={'accent'}>
+                      <FormattedMessage
+                        id="xpack.ml.overview.gettingStartedSectionTransforms"
+                        defaultMessage="transforms"
+                      />
+                    </EuiLink>
+                  ),
                   sourcedata: (
                     <EuiLink
                       href={docLinks.links.ml.dFAPrepareData}
@@ -82,15 +90,7 @@ export const AnalyticsEmptyPrompt: FC<Props> = ({ disabled, onCreateFirstJobClic
                     >
                       <FormattedMessage
                         id="xpack.ml.overview.gettingStartedSectionSourceData"
-                        defaultMessage="specifically structured source data"
-                      />
-                    </EuiLink>
-                  ),
-                  transforms: (
-                    <EuiLink href={transformsLink} target="blank" color={'accent'}>
-                      <FormattedMessage
-                        id="xpack.ml.overview.gettingStartedSectionTransforms"
-                        defaultMessage="transforms"
+                        defaultMessage="entity-centric source data set"
                       />
                     </EuiLink>
                   ),
