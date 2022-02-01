7695d3f681b1d8cb2a2048bf7c319c1d52cb88c0
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-02-01 12:31:29 +0200

    Fixed saving source order for pie/donut.

--- src/plugins/chart_expressions/expression_partition_vis/public/utils/layers/sort_predicate.ts
+++ src/plugins/chart_expressions/expression_partition_vis/public/utils/layers/sort_predicate.ts
@@ -17,13 +17,9 @@ type SortPredicateDefaultFn = (
   columns: Array<Partial<BucketColumns>>
 ) => SortFn;
 
-type SortPredicatePieDonutFn = (
-  visParams: PartitionVisParams,
-  visData: Datatable,
-  columns: Array<Partial<BucketColumns>>
-) => SortFn;
+type SortPredicatePieDonutFn = (visParams: PartitionVisParams) => SortFn;
 
-type SortPredicateWaffleFn = () => SortFn;
+type SortPredicatePureFn = () => SortFn;
 
 export const extractUniqTermsMap = (dataTable: Datatable, columnId: string) =>
   [...new Set(dataTable.rows.map((item) => item[columnId]))].reduce(
@@ -34,22 +30,34 @@ export const extractUniqTermsMap = (dataTable: Datatable, columnId: string) =>
     {}
   );
 
-const sortPredicatePieDonut: SortPredicatePieDonutFn = (visParams, ...args) =>
-  visParams.respectSourceOrder ? sortPredicateDefault(...args) : undefined;
+const sortPredicateSaveSourceOrder: SortPredicatePureFn =
+  () =>
+  ([, node1], [, node2]) => {
+    const [index1] = node1.inputIndex ?? [];
+    if (index1 !== undefined) {
+      return index1;
+    }
+    return node2.value - node1.value;
+  };
 
-const sortPredicateDefault: SortPredicateDefaultFn = (visData, columns) => {
+const sortPredicatePieDonut: SortPredicatePieDonutFn = (visParams) =>
+  visParams.respectSourceOrder ? sortPredicateSaveSourceOrder() : undefined;
+
+const sortPredicateMosaic: SortPredicateDefaultFn = (visData, columns) => {
   const sortingMap = columns[0]?.id ? extractUniqTermsMap(visData, columns[0].id) : {};
+
   return ([name1, node1], [, node2]) => {
     // Sorting for first group
     if (columns.length === 1 || (node1.children.length && name1 in sortingMap)) {
       return sortingMap[name1];
     }
+
     // Sorting for second group
     return node2.value - node1.value;
   };
 };
 
-const sortPredicateWaffle: SortPredicateWaffleFn =
+const sortPredicateWaffle: SortPredicatePureFn =
   () =>
   ([, node1], [, node2]) =>
     node2.value - node1.value;
@@ -61,9 +69,9 @@ export const sortPredicateByType = (
   columns: Array<Partial<BucketColumns>>
 ) =>
   ({
-    [ChartTypes.PIE]: () => sortPredicatePieDonut(visParams, visData, columns),
-    [ChartTypes.DONUT]: () => sortPredicatePieDonut(visParams, visData, columns),
+    [ChartTypes.PIE]: () => sortPredicatePieDonut(visParams),
+    [ChartTypes.DONUT]: () => sortPredicatePieDonut(visParams),
     [ChartTypes.WAFFLE]: () => sortPredicateWaffle(),
     [ChartTypes.TREEMAP]: () => undefined,
-    [ChartTypes.MOSAIC]: () => sortPredicateDefault(visData, columns),
+    [ChartTypes.MOSAIC]: () => sortPredicateMosaic(visData, columns),
   }[chartType]());
