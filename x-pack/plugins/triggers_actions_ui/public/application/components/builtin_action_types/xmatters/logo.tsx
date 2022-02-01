04a9b5ffd8112d26b5b7959569d5d67b03af0b43
msimpson <msimpson@xmatters.com>
2022-02-01 09:21:46 -0800

    Fix alignment of the xmatters logo on the connectors page

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/xmatters/logo.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/xmatters/logo.tsx
@@ -12,6 +12,7 @@ const Logo = () => (
     width="32px"
     height="32px"
     viewBox="0 0 32 32"
+    className="euiIcon euiIcon--xLarge euiCard__icon"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
