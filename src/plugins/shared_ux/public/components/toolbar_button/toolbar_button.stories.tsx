6ab9f6a9a0151d3947d3b07b878947b08d83d1a7
rshen91 <rshen@elastic.co>
2022-02-02 14:03:19 -0700

    add story and mdx

--- src/plugins/shared_ux/public/components/toolbar_button/toolbar_button.stories.tsx
+++ src/plugins/shared_ux/public/components/toolbar_button/toolbar_button.stories.tsx
@@ -5,3 +5,24 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
+
+import React from 'react';
+import { ToolbarButton as ToolbarButtonComponent } from './toolbar_button.component';
+import { ToolbarButton } from './toolbar_button';
+import mdx from './toolbar_button.mdx';
+
+export default {
+  title: 'Toolbar Button',
+  description: 'A button in the toolbar',
+  parameters: {
+    docs: { page: mdx },
+  },
+};
+
+export const ConnectedComponent = () => {
+  return <ToolbarButton />;
+};
+
+export const PureComponent = () => {
+  return <ToolbarButtonComponent />;
+};
