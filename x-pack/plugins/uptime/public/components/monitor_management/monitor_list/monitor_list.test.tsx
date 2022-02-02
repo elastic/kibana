2dfded557f6700745acdbda5e2edefd40e22dd73
Abdul Zahid <awahab07@yahoo.com>
2022-02-02 11:05:43 +0100

    Fix test.
    #121117

--- x-pack/plugins/uptime/public/components/monitor_management/monitor_list/monitor_list.test.tsx
+++ x-pack/plugins/uptime/public/components/monitor_management/monitor_list/monitor_list.test.tsx
@@ -101,76 +101,19 @@ describe('<MonitorManagementList />', () => {
     expect(onPageStateChange).toBeCalledWith(expect.objectContaining({ pageSize: 10 }));
   });
 
-  it.skip('handles refreshing and changing page when navigating to the next page', async () => {
-    render(
+  it('handles refreshing and changing page when navigating to the next page', async () => {
+    const { getByTestId } = render(
       <MonitorManagementList
         onUpdate={onUpdate}
         onPageStateChange={onPageStateChange}
         monitorList={state.monitorManagementList}
-        pageState={pageState}
+        pageState={{ ...pageState, pageSize: 3, pageIndex: 1 }}
       />,
       { state }
     );
 
-    userEvent.click(screen.getByTestId('pagination-button-next'));
+    userEvent.click(getByTestId('pagination-button-next'));
 
     expect(onPageStateChange).toBeCalledWith(expect.objectContaining({ pageIndex: 2 }));
   });
-
-  it.each([
-    [DataStream.BROWSER, ConfigKey.SOURCE_INLINE],
-    [DataStream.HTTP, ConfigKey.URLS],
-    [DataStream.TCP, ConfigKey.HOSTS],
-    [DataStream.ICMP, ConfigKey.HOSTS],
-  ])(
-    'appends inline to the monitor id for browser monitors and omits for lightweight checks',
-    (type, configKey) => {
-      const id = '123456';
-      const name = 'sample monitor';
-      const browserState = {
-        monitorManagementList: {
-          ...state.monitorManagementList,
-          list: {
-            ...state.monitorManagementList.list,
-            monitors: [
-              {
-                id,
-                attributes: {
-                  name,
-                  schedule: {
-                    unit: ScheduleUnit.MINUTES,
-                    number: '1',
-                  },
-                  [configKey]: 'test',
-                  type,
-                  tags: [`tag-1`],
-                },
-              },
-            ],
-          },
-        },
-      };
-
-      render(
-        <MonitorManagementList
-          onUpdate={onUpdate}
-          onPageStateChange={onPageStateChange}
-          monitorList={browserState.monitorManagementList as unknown as MonitorManagementListState}
-          pageState={{ ...pageState, pageIndex: 1 }}
-        />,
-        { state: browserState }
-      );
-
-      const link = screen.getByText(name) as HTMLAnchorElement;
-
-      expect(link.href).toEqual(
-        expect.stringContaining(
-          `/app/uptime/monitor/${Buffer.from(
-            `${id}${type === DataStream.BROWSER ? `-inline` : ''}`,
-            'utf8'
-          ).toString('base64')}`
-        )
-      );
-    }
-  );
 });
