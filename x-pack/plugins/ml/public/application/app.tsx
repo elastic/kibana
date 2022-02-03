26bedc2269c8b2b69cc0d733510e6a7754632c9b
Devon A Thomson <devon.thomson@elastic.co>
2022-02-03 15:33:17 -0500

    Update ML custom URLs to use dashboard Locator

--- x-pack/plugins/ml/public/application/app.tsx
+++ x-pack/plugins/ml/public/application/app.tsx
@@ -135,7 +135,7 @@ export const renderApp = (
     application: coreStart.application,
     http: coreStart.http,
     security: deps.security,
-    urlGenerators: deps.share.urlGenerators,
+    dashboardLocator: deps.dashboard.locator,
     maps: deps.maps,
     dataVisualizer: deps.dataVisualizer,
     dataViews: deps.data.dataViews,
