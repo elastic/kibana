7c0ae5b64adaff14312b0574469080e00e40e443
Quynh Nguyen <quynh.nguyen@elastic.co>
2022-02-03 15:31:20 -0600

    Assert Lens show chart after navigation from data viz

--- x-pack/test/functional/services/ml/data_visualizer_table.ts
+++ x-pack/test/functional/services/ml/data_visualizer_table.ts
@@ -565,6 +565,18 @@ export function MachineLearningDataVisualizerTableProvider(
       }
     }
 
+    public async assertLensActionShowChart(fieldType: string, fieldName: string) {
+      await retry.tryForTime(10 * 1000, async () => {
+        await testSubjects.clickWhenNotDisabled(
+          this.rowSelector(fieldName, 'dataVisualizerActionViewInLensButton')
+        );
+        await testSubjects.existOrFail('lnsVisualizationContainer', {
+          timeout: 1000,
+        });
+        await browser.goBack();
+      });
+    }
+
     public async ensureNumRowsPerPage(n: 10 | 25 | 50) {
       const paginationButton = 'dataVisualizerTable > tablePaginationPopoverButton';
       await retry.tryForTime(10000, async () => {
