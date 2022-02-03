86368b5510b02add168d5cefc005ace9580554f7
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-02-03 13:11:56 -0500

    Fix merge typo

--- x-pack/plugins/fleet/server/integration_tests/reset_preconfiguration.test.ts
+++ x-pack/plugins/fleet/server/integration_tests/reset_preconfiguration.test.ts
@@ -57,6 +57,7 @@ describe.skip('Fleet preconfiguration rest', () => {
         {
           xpack: {
             fleet: {
+              registryUrl,
               // Preconfigure two policies test-12345 and test-456789
               agentPolicies: [
                 {
