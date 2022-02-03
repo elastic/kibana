0309ce5f9a4d1f1874ffa1679e4ffa130ba045b2
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-02-03 08:16:34 -0500

    [Lens] Various fixes for Lens embeddables (#123587) (#124512)
    * :alchemy: Initial investigation
    
    * :bug: Mark heatmap respect disableTriggers
    
    * :bug: Make table respect the disableTriggers flag
    
    * :bug: Add heatmap and gauges to the supported embeddables list
    
    * :alchemy: Debug gauges issues and other embeddables
    
    * :label: Add missing column type export
    
    * :bug: Fix timeRange prop issue + show validation errors when found
    
    * :bug: Fix heatmap issue on load
    
    * Update x-pack/plugins/lens/public/embeddable/embeddable.test.tsx
    
    Co-authored-by: Marta Bondyra <marta.bondyra@gmail.com>
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Marta Bondyra <marta.bondyra@gmail.com>
    (cherry picked from commit 640d9e75b86164bc2ecdc5893c4308943bff7413)
    
    Co-authored-by: Marco Liberati <dej611@users.noreply.github.com>

--- src/plugins/chart_expressions/expression_heatmap/public/components/heatmap_component.tsx
+++ src/plugins/chart_expressions/expression_heatmap/public/components/heatmap_component.tsx
@@ -135,6 +135,7 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = memo(
     onSelectRange,
     paletteService,
     uiState,
+    interactive,
   }) => {
     const chartTheme = chartsThemeService.useChartsTheme();
     const isDarkTheme = chartsThemeService.useDarkMode();
@@ -145,12 +146,15 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = memo(
     });
 
     const toggleLegend = useCallback(() => {
+      if (!interactive) {
+        return;
+      }
       setShowLegend((value) => {
         const newValue = !value;
         uiState?.set?.('vis.legendOpen', newValue);
         return newValue;
       });
-    }, [uiState]);
+    }, [uiState, interactive]);
 
     const setColor = useCallback(
       (newColor: string | null, seriesLabel: string | number) => {
@@ -302,77 +306,30 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = memo(
       };
     });
 
-    const onElementClick = ((e: HeatmapElementEvent[]) => {
-      const cell = e[0][0];
-      const { x, y } = cell.datum;
+    const onElementClick = useCallback(
+      (e: HeatmapElementEvent[]) => {
+        const cell = e[0][0];
+        const { x, y } = cell.datum;
 
-      const xAxisFieldName = xAxisColumn?.meta?.field;
-      const timeFieldName = isTimeBasedSwimLane ? xAxisFieldName : '';
+        const xAxisFieldName = xAxisColumn?.meta?.field;
+        const timeFieldName = isTimeBasedSwimLane ? xAxisFieldName : '';
 
-      const points = [
-        {
-          row: table.rows.findIndex((r) => r[xAxisColumn.id] === x),
-          column: xAxisColumnIndex,
-          value: x,
-        },
-        ...(yAxisColumn
-          ? [
-              {
-                row: table.rows.findIndex((r) => r[yAxisColumn.id] === y),
-                column: yAxisColumnIndex,
-                value: y,
-              },
-            ]
-          : []),
-      ];
-
-      const context: FilterEvent['data'] = {
-        data: points.map((point) => ({
-          row: point.row,
-          column: point.column,
-          value: point.value,
-          table,
-        })),
-        timeFieldName,
-      };
-      onClickValue(context);
-    }) as ElementClickListener;
-
-    const onBrushEnd = (e: HeatmapBrushEvent) => {
-      const { x, y } = e;
-
-      const xAxisFieldName = xAxisColumn?.meta?.field;
-      const timeFieldName = isTimeBasedSwimLane ? xAxisFieldName : '';
-
-      if (isTimeBasedSwimLane) {
-        const context: BrushEvent['data'] = {
-          range: x as number[],
-          table,
-          column: xAxisColumnIndex,
-          timeFieldName,
-        };
-        onSelectRange(context);
-      } else {
-        const points: Array<{ row: number; column: number; value: string | number }> = [];
-
-        if (yAxisColumn) {
-          (y as string[]).forEach((v) => {
-            points.push({
-              row: table.rows.findIndex((r) => r[yAxisColumn.id] === v),
-              column: yAxisColumnIndex,
-              value: v,
-            });
-          });
-        }
-        if (xAxisColumn) {
-          (x as string[]).forEach((v) => {
-            points.push({
-              row: table.rows.findIndex((r) => r[xAxisColumn.id] === v),
-              column: xAxisColumnIndex,
-              value: v,
-            });
-          });
-        }
+        const points = [
+          {
+            row: table.rows.findIndex((r) => r[xAxisColumn.id] === x),
+            column: xAxisColumnIndex,
+            value: x,
+          },
+          ...(yAxisColumn
+            ? [
+                {
+                  row: table.rows.findIndex((r) => r[yAxisColumn.id] === y),
+                  column: yAxisColumnIndex,
+                  value: y,
+                },
+              ]
+            : []),
+        ];
 
         const context: FilterEvent['data'] = {
           data: points.map((point) => ({
@@ -384,8 +341,79 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = memo(
           timeFieldName,
         };
         onClickValue(context);
-      }
-    };
+      },
+      [
+        isTimeBasedSwimLane,
+        onClickValue,
+        table,
+        xAxisColumn?.id,
+        xAxisColumn?.meta?.field,
+        xAxisColumnIndex,
+        yAxisColumn,
+        yAxisColumnIndex,
+      ]
+    );
+
+    const onBrushEnd = useCallback(
+      (e: HeatmapBrushEvent) => {
+        const { x, y } = e;
+
+        const xAxisFieldName = xAxisColumn?.meta?.field;
+        const timeFieldName = isTimeBasedSwimLane ? xAxisFieldName : '';
+
+        if (isTimeBasedSwimLane) {
+          const context: BrushEvent['data'] = {
+            range: x as number[],
+            table,
+            column: xAxisColumnIndex,
+            timeFieldName,
+          };
+          onSelectRange(context);
+        } else {
+          const points: Array<{ row: number; column: number; value: string | number }> = [];
+
+          if (yAxisColumn) {
+            (y as string[]).forEach((v) => {
+              points.push({
+                row: table.rows.findIndex((r) => r[yAxisColumn.id] === v),
+                column: yAxisColumnIndex,
+                value: v,
+              });
+            });
+          }
+          if (xAxisColumn) {
+            (x as string[]).forEach((v) => {
+              points.push({
+                row: table.rows.findIndex((r) => r[xAxisColumn.id] === v),
+                column: xAxisColumnIndex,
+                value: v,
+              });
+            });
+          }
+
+          const context: FilterEvent['data'] = {
+            data: points.map((point) => ({
+              row: point.row,
+              column: point.column,
+              value: point.value,
+              table,
+            })),
+            timeFieldName,
+          };
+          onClickValue(context);
+        }
+      },
+      [
+        isTimeBasedSwimLane,
+        onClickValue,
+        onSelectRange,
+        table,
+        xAxisColumn,
+        xAxisColumnIndex,
+        yAxisColumn,
+        yAxisColumnIndex,
+      ]
+    );
 
     const themeOverrides: PartialTheme = {
       legend: {
@@ -458,7 +486,7 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = memo(
         )}
         <Chart>
           <Settings
-            onElementClick={onElementClick}
+            onElementClick={interactive ? (onElementClick as ElementClickListener) : undefined}
             showLegend={showLegend ?? args.legend.isVisible}
             legendPosition={args.legend.position}
             legendColorPicker={uiState ? legendColorPicker : undefined}
@@ -475,7 +503,7 @@ export const HeatmapComponent: FC<HeatmapRenderProps> = memo(
                   ? new Date(dateHistogramMeta.timeRange.to).getTime()
                   : NaN,
             }}
-            onBrushEnd={onBrushEnd as BrushEndListener}
+            onBrushEnd={interactive ? (onBrushEnd as BrushEndListener) : undefined}
           />
           <Heatmap
             id="heatmap"
