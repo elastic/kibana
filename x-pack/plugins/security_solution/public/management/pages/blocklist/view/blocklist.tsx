e034e4a3bce640e95b3388d6d1d3fd502637f5a8
pzl <danpan@elastic.co>
2022-02-04 10:28:45 -0500

    use AdminListPage as page wrapper, includes a routespy

--- x-pack/plugins/security_solution/public/management/pages/blocklist/view/blocklist.tsx
+++ x-pack/plugins/security_solution/public/management/pages/blocklist/view/blocklist.tsx
@@ -5,10 +5,81 @@
  * 2.0.
  */
 
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { EuiButton } from '@elastic/eui';
+import { AdministrationListPage } from '../../../components/administration_list_page';
+import { ListPageRouteState } from '../../../../../common/endpoint/types';
+import { useMemoizedRouteState } from '../../../common/hooks';
+import { BackToExternalAppButton } from '../../../components/back_to_external_app_button';
+import { BlocklistEmptyState } from './components/empty';
+import { BackToExternalAppSecondaryButton } from '../../../components/back_to_external_app_secondary_button';
 
 export const Blocklist = memo(() => {
-  return <p>{'Hello World'}</p>;
+  const { state: routeState } = useLocation<ListPageRouteState | undefined>();
+  const memoizedRouteState = useMemoizedRouteState(routeState);
+
+  const backButtonEmptyComponent = useMemo(() => {
+    if (memoizedRouteState && memoizedRouteState.onBackButtonNavigateTo) {
+      return <BackToExternalAppSecondaryButton {...memoizedRouteState} />;
+    }
+  }, [memoizedRouteState]);
+
+  const backButtonHeaderComponent = useMemo(() => {
+    if (memoizedRouteState && memoizedRouteState.onBackButtonNavigateTo) {
+      return <BackToExternalAppButton {...memoizedRouteState} />;
+    }
+  }, [memoizedRouteState]);
+
+  const hasDataToShow = false;
+
+  const handleAddButtonClick = () => {};
+
+  return (
+    <AdministrationListPage
+      headerBackComponent={backButtonHeaderComponent}
+      title={
+        <FormattedMessage
+          id="xpack.securitySolution.blocklist.pageTitle"
+          defaultMessage="Blocklist"
+        />
+      }
+      subtitle={
+        <FormattedMessage
+          id="xpack.securitySolution.blocklist.pageSubTitle"
+          defaultMessage="Put that thing back where it came from or so help me"
+        />
+      }
+      actions={
+        hasDataToShow ? (
+          <EuiButton
+            fill
+            iconType="plusInCircle"
+            data-test-subj="blocklistAddButton"
+            onClick={handleAddButtonClick}
+          >
+            <FormattedMessage
+              id="xpack.securitySolution.blocklist.addButton"
+              defaultMessage="Add blocklist entry"
+            />
+          </EuiButton>
+        ) : (
+          []
+        )
+      }
+      hideHeader={!hasDataToShow}
+    >
+      {hasDataToShow ? (
+        <p>{'Data, search bar, etc here'}</p>
+      ) : (
+        <BlocklistEmptyState
+          onAdd={handleAddButtonClick}
+          backComponent={backButtonEmptyComponent}
+        />
+      )}
+    </AdministrationListPage>
+  );
 });
 
 Blocklist.displayName = 'Blocklist';
