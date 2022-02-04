100d946ac9c1e5b68bd29be60563297a7ec85bc3
criamico <mariacristina.amico@elastic.co>
2022-02-04 17:16:08 +0100

    Use login and roles utilities from security-solution cypress library

--- x-pack/plugins/fleet/cypress/screens/navigation.ts
+++ x-pack/plugins/fleet/cypress/screens/navigation.ts
@@ -5,38 +5,5 @@
  * 2.0.
  */
 
-export const TOGGLE_NAVIGATION_BTN = '[data-test-subj="toggleNavButton"]';
-
-// Stack Management - Roles creation
-// export const STACK_MANAGEMENT_LINK = '[data-test-subj="collapsibleNavAppLink"]';
-// export const ROLES_LINK = '[data-test-subj="roles"]';
-export const CREATE_ROLE_BTN = 'createRoleButton';
-export const ADD_KIBANA_PRIVILEGE = 'addSpacePrivilegeButton';
-export const ROLE_NAME_INPUT = '[data-test-subj="roleFormNameInput"]';
-export const SAVE_ROLE_BTN = 'roleFormSaveButton';
-export const CREATE_PRIVILEGE_BTN = 'createSpacePrivilegeButton';
-
-// Kibana privileges flyout
-export const SPACE_SELECTOR_DROPDOWN = 'spaceSelectorComboBox';
-export const ALL_SPACES_OPTION = '[id="spaceOption_*"]';
-export const FEATURE_CATEGORY_MANAGEMENT = 'featureCategory_management';
-export const FLEET_PRIVILEGE_ALL_BTN = 'fleetv2_all';
-export const FLEET_PRIVILEGE_NONE_BTN = 'fleetv2_none';
-export const INTEGRATIONS_PRIVILEGE_ALL_BTN = 'fleet_all';
-export const INTEGRATIONS_PRIVILEGE_READ_BTN = 'fleet_read';
-export const INTEGRATIONS_PRIVILEGE_NONE_BTN = 'fleet_none';
-
-// Users
-export const CREATE_USER_BTN = 'createUserButton';
-export const USERNAME_INPUT = 'userFormUserNameInput';
-export const FULLNAME_INPUT = 'userFormFullNameInput';
-export const PASSWORD_INPUT = 'passwordInput';
-export const CONFIRM_PASSWORD_INPUT = 'passwordConfirmationInput';
-export const SELECT_ROLES_DROPDOWN = 'rolesDropdown';
-
-export const SKIP_WELCOME_SCREEN_BTN = 'skipWelcomeScreen';
-
-// Login screen
-export const LOGIN_USERNAME_INPUT = 'loginUsername';
-export const LOGIN_PASSWORD_INPUT = 'loginPassword';
-export const LOGIN_BTN = 'loginSubmit';
+export const TOGGLE_NAVIGATION_BTN = 'toggleNavButton';
+export const NAV_APP_LINK = 'collapsibleNavAppLink';
