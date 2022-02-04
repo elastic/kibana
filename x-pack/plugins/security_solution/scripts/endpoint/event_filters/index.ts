b92958ff58dc563f58378ab840b6628cd4f6ad38
Paul Tavares <paul.tavares@elastic.co>
2022-02-04 12:54:39 -0500

    Fix Event Filters mock data loader

--- x-pack/plugins/security_solution/scripts/endpoint/event_filters/index.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/event_filters/index.ts
@@ -17,8 +17,9 @@ import {
   EXCEPTION_LIST_ITEM_URL,
   EXCEPTION_LIST_URL,
 } from '@kbn/securitysolution-list-constants';
-import { EventFilterGenerator } from '../../../common/endpoint/data_generators/event_filter_generator';
 import { randomPolicyIdGenerator } from '../common/random_policy_id_generator';
+import { ExceptionsListItemGenerator } from '../../../common/endpoint/data_generators/exceptions_list_item_generator';
+import { isArtifactByPolicy } from '../../../common/endpoint/service/artifacts';
 
 export const cli = () => {
   run(
@@ -66,7 +67,7 @@ const handleThrowAxiosHttpError = (err: AxiosError): never => {
 };
 
 const createEventFilters: RunFn = async ({ flags, log }) => {
-  const eventGenerator = new EventFilterGenerator();
+  const eventGenerator = new ExceptionsListItemGenerator();
   const kbn = new KbnClient({ log, url: flags.kibana as string });
 
   await ensureCreateEndpointEventFiltersList(kbn);
@@ -76,8 +77,9 @@ const createEventFilters: RunFn = async ({ flags, log }) => {
   await pMap(
     Array.from({ length: flags.count as unknown as number }),
     () => {
-      const body = eventGenerator.generate();
-      if (body.tags?.length && body.tags[0] !== 'policy:all') {
+      const body = eventGenerator.generateEventFilterForCreate();
+
+      if (isArtifactByPolicy(body)) {
         const nmExceptions = Math.floor(Math.random() * 3) || 1;
         body.tags = Array.from({ length: nmExceptions }, () => {
           return `policy:${randomPolicyId()}`;
