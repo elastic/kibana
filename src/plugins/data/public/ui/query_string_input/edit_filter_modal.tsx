022f17a2e15625086ca040c8d6f2b2e0d92ba088
Nodir <nodir.latypov@gmail.com>
2022-02-04 19:03:51 +0500

    fix: create filter

--- src/plugins/data/public/ui/query_string_input/edit_filter_modal.tsx
+++ src/plugins/data/public/ui/query_string_input/edit_filter_modal.tsx
@@ -483,7 +483,7 @@ export function EditFilterModal({
                                       value: undefined,
                                       relationship: undefined,
                                       groupId: localfilter.groupId,
-                                      id: Number(multipleFilters?.length) + localFilters.length,
+                                      id: Number(multipleFilters.length) - countOfFilterBeforeFilterEdit.length + localFilters.length,
                                       subGroupId,
                                     },
                                   ]);
