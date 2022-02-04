b2077994e6d50f3bf561b0a549550ad97548ed8e
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-02-04 15:30:37 +0100

    exclude more test-related paths

--- src/dev/code_coverage/nyc_config/nyc.functional.config.js
+++ src/dev/code_coverage/nyc_config/nyc.functional.config.js
@@ -9,7 +9,8 @@
 const defaultExclude = require('@istanbuljs/schema/default-exclude');
 const extraExclude = [
   'data/optimize/**',
-  '**/{__test__,__snapshots__,__examples__,*mock*,.storybook,target,tests,test_helpers,integration_tests,types}/**/*',
+  '**/{__jest__,__test__,__examples__,__fixtures__ ,__snapshots__,__stories__ ,*mock*,.storybook,target,types}/**/*',
+  '**/{integration_tests,tests,test_helpers,test_data,test_samples,test_utils,test_utilities,scripts}/**/*',
   '**/*mock*.{ts,tsx}',
   '**/*.test.{ts,tsx}',
   '**/*.spec.{ts,tsx}',
--- src/dev/code_coverage/nyc_config/nyc.server.config.js
+++ src/dev/code_coverage/nyc_config/nyc.server.config.js
@@ -20,7 +20,8 @@ module.exports = {
     'x-pack/plugins/**/*.{js,mjs,jsx,ts,tsx}',
   ],
   exclude: [
-    '**/{__test__,__snapshots__,__examples__,*mock*,.storybook,target,tests,test_helpers,integration_tests,types}/**/*',
+    '**/{__jest__,__test__,__examples__,__fixtures__ ,__snapshots__,__stories__ ,*mock*,.storybook,target,types}/**/*',
+    '**/{integration_tests,tests,test_helpers,test_data,test_samples,test_utils,test_utilities,scripts}/**/*',
     '**/*mock*.{ts,tsx}',
     '**/*.test.{ts,tsx}',
     '**/*.spec.{ts,tsx}',
