ba127410b3bf929d7c995de3d7fc9345e579862b
Byron Hulcher <byron.hulcher@elastic.co>
2022-02-04 17:59:57 -0500

    Revert export

--- x-pack/plugins/enterprise_search/public/applications/shared/flash_messages/handle_api_errors.ts
+++ x-pack/plugins/enterprise_search/public/applications/shared/flash_messages/handle_api_errors.ts
@@ -22,7 +22,7 @@ import { IFlashMessage } from './types';
  * `errors` property in the response's data, which will contain messages we can
  * display to the user.
  */
-export interface ErrorResponse {
+interface ErrorResponse {
   statusCode: number;
   error: string;
   message: string;
