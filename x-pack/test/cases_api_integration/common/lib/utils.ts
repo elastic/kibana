d59f1b988cfd76308c242670b7014d6deaad9da6
Christos Nasikas <christos.nasikas@elastic.co>
2022-02-07 18:08:35 +0200

    Move assertWarningHeader to validations

--- x-pack/test/cases_api_integration/common/lib/utils.ts
+++ x-pack/test/cases_api_integration/common/lib/utils.ts
@@ -1143,16 +1143,3 @@ export const extractWarningValueFromWarningHeader = (warningHeader: string) => {
   const warningValue = warningHeader.substring(firstQuote + 1, lastQuote);
   return warningValue;
 };
-
-/**
- * Regular expression to test if a string matches the RFC7234 specification (without warn-date) for warning headers. This pattern assumes that the warn code
- * is always 299. Further, this pattern assumes that the warn agent represents a version of Kibana.
- *
- * Example: 299 Kibana-8.2.0 "Deprecated endpoint"
- */
-const WARNING_HEADER_REGEX = /299 Kibana-\d+.\d+.\d+ \".+\"/g;
-
-export const assertWarningHeader = (warningHeader: string) => {
-  const res = warningHeader.match(WARNING_HEADER_REGEX);
-  expect(res).not.to.be(null);
-};
