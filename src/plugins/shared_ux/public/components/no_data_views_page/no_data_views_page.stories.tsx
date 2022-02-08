c46f1646e9b0ee75b4a40b0a8692e964bd9463b4
Maja Grubic <maja.grubic@elastic.co>
2022-02-08 21:07:17 +0100

    Add .mdx file

--- src/plugins/shared_ux/public/components/no_data_views_page/no_data_views_page.stories.tsx
+++ src/plugins/shared_ux/public/components/no_data_views_page/no_data_views_page.stories.tsx
@@ -9,10 +9,16 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { NoDataViewsPage } from './no_data_views_page';
 import { NoDataViewsComponent } from './no_data_views_component';
+import mdx from './no_data_views_page.mdx';
 
 export default {
   title: 'No Data Views Page',
   description: 'A page that displays when there are no user-created data views',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
 };
 
 export const ConnectedComponent = () => {
