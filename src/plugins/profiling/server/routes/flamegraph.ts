d3033ef97a15faa22fab13542c52e243789b0e0f
Joseph Crail <joseph.crail@elastic.co>
2022-02-07 16:53:09 -0800

    Fix input for Elastic flamegraph
    This is not a complete solution, but at least the flamechart is
    rendering.

---
+++ src/plugins/profiling/server/routes/flamegraph.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export class FlameGraph {
+  constructor(events, totalEvents, stackTraces, stackFrames) {
+    this.events = events;
+    this.totalEvents = totalEvents;
+    this.stacktraces = stackTraces;
+    this.stackframes = stackFrames;
+  }
+
+  toElastic() {
+    let facts = [];
+    for (const trace of this.stacktraces) {
+      if (trace.found) {
+        const pairs = ['root'].concat(trace._source.FrameID).map((item, i) => [i, item]);
+        const fact = {
+          id: trace._source.FrameID[trace._source.FrameID.length - 1],
+          value: 1,
+          depth: trace._source.FrameID.length,
+          layers: Object.fromEntries(pairs),
+        };
+        facts.push(fact);
+      }
+    }
+    return { facts };
+  }
+
+  toPixi() {
+    return {};
+  }
+}
