fd928cf820d62a39ff8698b95d0f3071c0030c61
Julia Bardi <90178898+juliaElastic@users.noreply.github.com>
2022-02-09 08:36:20 +0100

    [7.16] Add missing package policies for managed preconfigured policies (#124969)
    * [7.16] Add missing package policies for managed preconfigured policies
    
    * fixed conflict resolution
    
    Co-authored-by: Nicolas Chaulet <nicolas.chaulet@elastic.co>

--- x-pack/plugins/fleet/server/services/package_policy.ts
+++ x-pack/plugins/fleet/server/services/package_policy.ts
@@ -106,17 +106,22 @@ class PackagePolicyService {
       bumpRevision?: boolean;
       force?: boolean;
       skipEnsureInstalled?: boolean;
+      skipUniqueNameVerification?: boolean;
+      overwrite?: boolean;
     }
   ): Promise<PackagePolicy> {
-    const existingPoliciesWithName = await this.list(soClient, {
-      perPage: 1,
-      kuery: `${PACKAGE_POLICY_SAVED_OBJECT_TYPE}.name: "${packagePolicy.name}"`,
-    });
+    if (!options?.skipUniqueNameVerification) {
+      const existingPoliciesWithName = await this.list(soClient, {
+        perPage: 1,
+        kuery: `${PACKAGE_POLICY_SAVED_OBJECT_TYPE}.name: "${packagePolicy.name}"`,
+      });
 
-    // Check that the name does not exist already
-    if (existingPoliciesWithName.items.length > 0) {
-      throw new IngestManagerError('There is already a package with the same name');
+      // Check that the name does not exist already
+      if (existingPoliciesWithName.items.length > 0) {
+        throw new IngestManagerError('There is already an integration policy with the same name');
+      }
     }
+
     let elasticsearch: PackagePolicy['elasticsearch'];
     // Add ids to stream
     const packagePolicyId = options?.id || uuid.v4();
