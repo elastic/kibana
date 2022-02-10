elastic/kibana

commit 10997900d526da77ce80e7a4b57913cab3d0eea9

Add enable_fields_emulation flag to search requests (#123267)
* Add enable_fields_emulation flag to search requests

* Move parameter to ese strategy

* Update search source to not fetch fields if size is 0

* Don't send flag if specifying fields and _source

* Update maps functional test with search source changes

* Add flag to other search strategies

* Fix ESE usage of params

* Fix tests

* Other fixes

* Fix linting

Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

src/plugins/data/server/search/strategies/eql_search/eql_search_strategy.ts
@@ -42,7 +42,8 @@ export const eqlSearchStrategyProvider = (
 
       const search = async () => {
         const { track_total_hits: _, ...defaultParams } = await getDefaultSearchParams(
-          uiSettingsClient
+          uiSettingsClient,
+          request.params?.body
         );
         const params = id
           ? getDefaultAsyncGetParams(null, options)

src/plugins/data/common/search/search_source/search_source.test.ts
@@ -157,6 +157,22 @@ describe('SearchSource', () => {
         expect(request.runtime_mappings).toEqual(runtimeFields);
       });
 
+      test('except when size is 0', async () => {
+        const runtimeFields = { runtime_field: runtimeFieldDef };
+        searchSource.setField('size', 0).setField('index', {
+          ...indexPattern,
+          getComputedFields: () => ({
+            storedFields: ['hello'],
+            scriptFields: { world: {} },
+            docvalueFields: ['@timestamp'],
+            runtimeFields,
+          }),
+        } as unknown as IndexPattern);
+
+        const request = searchSource.getSearchRequestBody();
+        expect(request.fields).toBeUndefined();
+      });
+
       test('never includes docvalue_fields', async () => {
         searchSource.setField('index', {
           ...indexPattern,
@@ -211,25 +227,21 @@ describe('SearchSource', () => {
         expect(request.fields).toEqual(['c', { field: 'a', format: 'date_time' }]);
       });
 
-      test('allows you to override computed fields if you provide a format', async () => {
-        const indexPatternFields = indexPattern.fields;
-        indexPatternFields.getByType = (type) => {
-          return [];
-        };
-        searchSource.setField('index', {
+      test('does not allow any field info when size is 0', async () => {
+        searchSource.setField('size', 0).setField('index', {
           ...indexPattern,
-          fields: indexPatternFields,
           getComputedFields: () => ({
             storedFields: [],
             scriptFields: {},
-            docvalueFields: [{ field: 'hello', format: 'date_time' }],
+            docvalueFields: [{ field: 'a', format: 'date_time' }],
           }),
         } as unknown as IndexPattern);
-        searchSource.setField('fields', [{ field: 'hello', format: 'strict_date_time' }]);
+        searchSource.setField('fields', ['c']);
+        searchSource.setField('fieldsFromSource', ['a', 'b', 'd']);
 
         const request = searchSource.getSearchRequestBody();
-        expect(request).toHaveProperty('fields');
-        expect(request.fields).toEqual([{ field: 'hello', format: 'strict_date_time' }]);
+        expect(request).not.toHaveProperty('_source');
+        expect(request).not.toHaveProperty('fields');
       });
 
       test('injects a date format for computed docvalue fields if none is provided', async () => {

src/plugins/data/common/search/search_source/search_source.ts
@@ -838,6 +838,12 @@ export class SearchSource {
       body.fields = filteredDocvalueFields;
     }
 
+    // If we aren't requesting any documents, there isn't any reason to request any field information
+    if (body.size === 0) {
+      delete body._source;
+      delete body.fields;
+    }
+
     // If sorting by _score, build queries in the "must" clause instead of "filter" clause to enable scoring
     const filtersInMustClause = (body.sort ?? []).some((sort: EsQuerySortValue[]) =>
       sort.hasOwnProperty('_score')

src/plugins/data/server/search/routes/call_msearch.ts
@@ -60,8 +60,12 @@ export function getCallMsearch(dependencies: CallMsearchDependencies) {
     const config = await globalConfig$.pipe(first()).toPromise();
     const timeout = getShardTimeout(config);
 
-    // trackTotalHits is not supported by msearch
-    const { track_total_hits: _, ...defaultParams } = await getDefaultSearchParams(uiSettings);
+    // track_total_hits/enable_fields_emulation are not supported by msearch
+    const {
+      track_total_hits: tth,
+      enable_fields_emulation: efe,
+      ...defaultParams
+    } = await getDefaultSearchParams(uiSettings);
 
     try {
       const promise = esClient.asCurrentUser.msearch(

src/plugins/data/server/search/strategies/es_search/es_search_strategy.test.ts
@@ -74,6 +74,7 @@ describe('ES search strategy', () => {
           ...params,
           ignore_unavailable: true,
           track_total_hits: true,
+          enable_fields_emulation: true,
         });
         done();
       });
@@ -89,6 +90,7 @@ describe('ES search strategy', () => {
         expect(mockApiCaller.mock.calls[0][0]).toEqual({
           ...params,
           track_total_hits: true,
+          enable_fields_emulation: true,
         });
         done();
       });
@@ -126,6 +128,7 @@ describe('ES search strategy', () => {
     expect(mockApiCaller.mock.calls[0][0]).toEqual({
       ...params,
       track_total_hits: true,
+      enable_fields_emulation: true,
     });
     expect(mockedApiCaller.abort).toBeCalled();
   });

src/plugins/data/server/search/strategies/es_search/es_search_strategy.ts
@@ -39,7 +39,7 @@ export const esSearchStrategyProvider = (
       try {
         const config = await config$.pipe(first()).toPromise();
         const params = {
-          ...(await getDefaultSearchParams(uiSettingsClient)),
+          ...(await getDefaultSearchParams(uiSettingsClient, request.params?.body)),
           ...getShardTimeout(config),
           ...request.params,
         };

src/plugins/data/server/search/strategies/es_search/request_utils.ts
@@ -7,6 +7,7 @@
  */
 
 import type { TransportRequestPromise } from '@elastic/elasticsearch/lib/Transport';
+import { SearchRequest } from '@elastic/elasticsearch/api/types';
 import type { Search } from '@elastic/elasticsearch/api/requestParams';
 import type { IUiSettingsClient, SharedGlobalConfig } from 'kibana/server';
 import { UI_SETTINGS } from '../../../../common';
@@ -17,18 +18,29 @@ export function getShardTimeout(config: SharedGlobalConfig): Pick<Search, 'timeo
 }
 
 export async function getDefaultSearchParams(
-  uiSettingsClient: Pick<IUiSettingsClient, 'get'>
+  uiSettingsClient: Pick<IUiSettingsClient, 'get'>,
+  body: SearchRequest['body'] = {}
 ): Promise<
-  Pick<Search, 'max_concurrent_shard_requests' | 'ignore_unavailable' | 'track_total_hits'>
+  Pick<Search, 'max_concurrent_shard_requests' | 'ignore_unavailable' | 'track_total_hits'> & {
+    enable_fields_emulation: boolean;
+  }
 > {
   const maxConcurrentShardRequests = await uiSettingsClient.get<number>(
     UI_SETTINGS.COURIER_MAX_CONCURRENT_SHARD_REQUESTS
   );
+
+  // Specifying specific fields from both "_source" and "fields' while emulating the fields API will throw errors in ES
+  // See https://github.com/elastic/elasticsearch/pull/75745
+  const hasFields = Array.isArray(body?.fields) && body?.fields.length > 0;
+  const hasSourceFields = body?.hasOwnProperty('_source') && typeof body?._source !== 'boolean';
+  const enableFieldsEmulation = !(hasFields && hasSourceFields);
+
   return {
     max_concurrent_shard_requests:
       maxConcurrentShardRequests > 0 ? maxConcurrentShardRequests : undefined,
     ignore_unavailable: true, // Don't fail if the index/indices don't exist
     track_total_hits: true,
+    enable_fields_emulation: enableFieldsEmulation,
   };
 }
 

src/plugins/data/server/search/strategies/ese_search/ese_search_strategy.ts
@@ -64,6 +64,7 @@ export const enhancedEsSearchStrategyProvider = (
             ...(await getDefaultAsyncSubmitParams(
               uiSettingsClient,
               searchSessionsClient.getConfig(),
+              request.params?.body,
               options
             )),
             ...request.params,
@@ -110,7 +111,7 @@ export const enhancedEsSearchStrategyProvider = (
     const querystring = {
       ...getShardTimeout(legacyConfig),
       ...(await getIgnoreThrottled(uiSettingsClient)),
-      ...(await getDefaultSearchParams(uiSettingsClient)),
+      ...(await getDefaultSearchParams(uiSettingsClient, request.params?.body)),
       ...params,
     };
 

commit cba14669c737508f7b24f0cbff2ca48656040a4f

Fix EQL search strategy: omit enable_fields_emulation (#125076)
**Fixes:** https://github.com/elastic/kibana/issues/125059

## Summary

Fixes a bug in the EQL search strategy presumably introduced in https://github.com/elastic/kibana/pull/123267.

## Screenshots

**Before** (request to EQL search strategy is failing, and the rule creation form can't perform validation)

![](https://puu.sh/II2fT/0ffde0f0d6.png)

**After** (request to EQL search strategy is returning results, and the rule creation form can properly validate the query)

![](https://puu.sh/II2gJ/060ff92e77.png)

![](https://puu.sh/II2ex/d7fbb31517.png)

![](https://puu.sh/II2f2/09cc4483a3.png)

## Notes on testing

Create a test index and write a few documents to it:

```
PUT /test-eql
{
  "mappings": {
    "properties": {
      "@timestamp": {
        "type": "date"
      },
      "process": {
        "type": "object",
        "properties": {
          "name": {
            "type": "keyword"
          }
        }
      }
    }
  }  
}

POST /test-eql/_doc
{
  "@timestamp": "2022-02-05T10:00:08Z",
  "process": {
    "name": "mdworker"
  }
}
```

src/plugins/data/server/search/strategies/eql_search/eql_search_strategy.ts
@@ -41,10 +41,13 @@ export const eqlSearchStrategyProvider = (
       const client = esClient.asCurrentUser.eql;
 
       const search = async () => {
-        const { track_total_hits: _, ...defaultParams } = await getDefaultSearchParams(
-          uiSettingsClient,
-          request.params?.body
-        );
+        // track_total_hits/enable_fields_emulation are not supported by _eql/search
+        const {
+          track_total_hits: tth,
+          enable_fields_emulation: efe,
+          ...defaultParams
+        } = await getDefaultSearchParams(uiSettingsClient, request.params?.body);
+
         const params = id
           ? getDefaultAsyncGetParams(null, options)
           : {
@@ -53,6 +56,7 @@ export const eqlSearchStrategyProvider = (
               ...getDefaultAsyncGetParams(null, options),
               ...request.params,
             };
+
         const promise = id
           ? client.get({ ...params, id }, request.options)
           : // @ts-expect-error EqlRequestParams | undefined is not assignable to EqlRequestParams