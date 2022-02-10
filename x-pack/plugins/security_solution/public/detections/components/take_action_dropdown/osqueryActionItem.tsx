c3927311bf7b7e0e61834ffb19b958fb8c8266cd
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2022-02-10 17:47:56 +0100

    fix i18n place

--- x-pack/plugins/security_solution/public/detections/components/take_action_dropdown/osqueryActionItem.tsx
+++ x-pack/plugins/security_solution/public/detections/components/take_action_dropdown/osqueryActionItem.tsx
@@ -9,13 +9,17 @@ import React from 'react';
 import { EuiContextMenuItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
-export const ACTION_OSQUERY = i18n.translate('xpack.osquery.alerts.osqueryAlertTitle', {
-  defaultMessage: 'Run Osquery',
-});
+export const ACTION_OSQUERY = i18n.translate(
+  'xpack.securitySolution.alertsView.osqueryAlertTitle',
+  {
+    defaultMessage: 'Run Osquery',
+  }
+);
 
 interface IProps {
   onClick: () => void;
 }
+
 export const OsqueryActionItem = ({ onClick }: IProps) => {
   return (
     <EuiContextMenuItem
