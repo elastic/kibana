3f49dac4dc0b55b084b41354c47aa3fde2e2d0c1
Christos Nasikas <christos.nasikas@elastic.co>
2022-02-11 14:03:39 +0200

    [Cases] Deprecate endpoints (#124773)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit eb7591b04f2547d569c54393f1b9ba07df49c4c7)
    
    # Conflicts:
    #	x-pack/plugins/cases/server/routes/api/stats/get_status.ts

--- x-pack/test/cases_api_integration/security_and_spaces/tests/common/comments/get_all_comments.ts
+++ x-pack/test/cases_api_integration/security_and_spaces/tests/common/comments/get_all_comments.ts
@@ -15,6 +15,7 @@ import {
   createComment,
   getAllComments,
   superUserSpace1Auth,
+  extractWarningValueFromWarningHeader,
 } from '../../../../common/lib/utils';
 import {
   globalRead,
@@ -27,6 +28,8 @@ import {
   secOnlyRead,
   superUser,
 } from '../../../../common/lib/authentication/users';
+import { getCaseCommentsUrl } from '../../../../../../plugins/cases/common/api';
+import { assertWarningHeader } from '../../../../common/lib/validation';
 
 // eslint-disable-next-line import/no-default-export
 export default ({ getService }: FtrProviderContext): void => {
@@ -146,6 +149,19 @@ export default ({ getService }: FtrProviderContext): void => {
           auth: { user: secOnly, space: 'space2' },
           expectedHttpCode: 403,
         });
+      });
+    });
+
+    describe('deprecations', () => {
+      it('should return a warning header', async () => {
+        const theCase = await createCase(supertest, postCaseReq);
+        const res = await supertest.get(getCaseCommentsUrl(theCase.id)).expect(200);
+        const warningHeader = res.header.warning;
+
+        assertWarningHeader(warningHeader);
+
+        const warningValue = extractWarningValueFromWarningHeader(warningHeader);
+        expect(warningValue).to.be('Deprecated endpoint');
       });
     });
   });
