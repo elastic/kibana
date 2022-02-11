e06f40ee7311c4bc9105dde04a53c99625779cab
Maja Grubic <maja.grubic@elastic.co>
2022-02-11 13:32:25 +0100

    Add components

---
+++ src/plugins/shared_ux/public/components/no_data_views_page/no_data_views_component.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import { i18n } from '@kbn/i18n';
+import { EuiButton, EuiCard } from '@elastic/eui';
+import { NoResultsIllustration } from './assets';
+
+interface NoDataViewsComponentProps {
+  onClick?: () => void;
+  canCreateNewDataView: boolean;
+}
+
+export const NoDataViewsComponent = (props: NoDataViewsComponentProps) => {
+  const { onClick, canCreateNewDataView } = props;
+  const button = (
+    <EuiButton color={'primary'} onClick={onClick}>
+      {i18n.translate('sharedUX.noDataViewsPage.addDataViewText', {
+        defaultMessage: 'Add a Data View',
+      })}
+    </EuiButton>
+  );
+  return (
+    <EuiCard
+      title={i18n.translate('sharedUX.noDataViewsPage.noDataViewsText', {
+        defaultMessage: 'No Data Views',
+      })}
+      textAlign="center"
+      description={i18n.translate('sharedUX.noDataViewsPage.descriptionText', {
+        defaultMessage: 'You have data in Elasticsearch, but no data views',
+      })}
+      style={{ width: '50%', margin: 'auto', flexGrow: 0 }}
+    >
+      <div className="dscNoResults__illustration">
+        <NoResultsIllustration />
+      </div>
+      <div>{canCreateNewDataView ? button : undefined}</div>
+    </EuiCard>
+  );
+};
---
+++ src/plugins/shared_ux/public/components/no_data_views_page/no_data_views_page.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React, { useCallback, useEffect, useRef } from 'react';
+import { NoDataViewsComponent } from './no_data_views_component';
+import { useServices } from '../../services';
+import { DataView } from '../../../../data_views/public';
+
+export interface NoDataViewsPageProps {
+  onDataViewCreated: (dataView: DataView) => void;
+}
+
+export const NoDataViewsPage = (props: NoDataViewsPageProps) => {
+  const { onDataViewCreated } = props;
+  const closeDataViewEditor = useRef<() => void | undefined>();
+
+  useEffect(() => {
+    const cleanup = () => {
+      if (closeDataViewEditor?.current) {
+        closeDataViewEditor?.current();
+      }
+    };
+    return () => {
+      // Make sure to close the editor when unmounting
+      cleanup();
+    };
+  }, []);
+
+  const setDataViewEditorRef = useCallback((ref: () => void | undefined) => {
+    closeDataViewEditor.current = ref;
+  }, []);
+
+  const { dataViewEditor } = useServices();
+  const canCreateNewDataView = dataViewEditor.userPermissions.editDataView();
+  const createNewDataView = useCallback(() => {
+    if (!canCreateNewDataView) {
+      return;
+    }
+    const ref = dataViewEditor.openEditor({
+      onSave: (dataView) => {
+        onDataViewCreated(dataView);
+      },
+    });
+    if (setDataViewEditorRef) {
+      setDataViewEditorRef(ref);
+    }
+  }, [canCreateNewDataView, dataViewEditor, setDataViewEditorRef, onDataViewCreated]);
+
+  return (
+    <NoDataViewsComponent onClick={createNewDataView} canCreateNewDataView={canCreateNewDataView} />
+  );
+};
