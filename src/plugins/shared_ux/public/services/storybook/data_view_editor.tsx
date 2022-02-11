02d403261ea581ba27346e45a2d41a43c1343c9d
Maja Grubic <maja.grubic@elastic.co>
2022-02-11 14:55:15 +0100

    Add fake data view editor

---
+++ src/plugins/shared_ux/public/services/storybook/data_view_editor.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+import { action } from '@storybook/addon-actions';
+import { FakeDataViewEditor } from './fake_data_view_editor';
+import { DataViewEditorProps, DataViewEditorStart } from '../../../../data_view_editor/public';
+
+const createStartContract = (): DataViewEditorStart => {
+  return {
+    IndexPatternEditorComponent: FakeDataViewEditor,
+    openEditor: action('openEditor') as any as (options: DataViewEditorProps) => () => void,
+    userPermissions: {
+      editDataView: () => {
+        return true;
+      },
+    },
+  };
+};
+
+export const dataViewEditorPluginMock = {
+  createStartContract,
+};
+
+export const dataViewEditorFactory = () => {
+  return dataViewEditorPluginMock.createStartContract();
+};
---
+++ src/plugins/shared_ux/public/services/stub/fake_data_view_editor.tsx
@@ -0,0 +1,13 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+import React from 'react';
+import { DataViewEditorProps } from '../../../../data_view_editor/public';
+
+export const FakeDataViewEditor = (props: DataViewEditorProps) => {
+  return <div>Data View Editor</div>;
+};
