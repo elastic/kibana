f6b3bb3b42c545ad41b56c05c7e5efb0ce3bb1ef
Maja Grubic <maja.grubic@elastic.co>
2022-02-11 14:13:47 +0100

    Fix tsconfig path

--- src/plugins/shared_ux/public/components/no_data_views_page/index.tsx
+++ src/plugins/shared_ux/public/components/no_data_views_page/index.tsx
@@ -6,7 +6,8 @@
  * Side Public License, v 1.
  */
 import { NoDataViewsPage } from './no_data_views_page';
-export { NoDataViewsPage, NoDataViewsPageProps } from './no_data_views_page';
+export type { NoDataViewsPageProps } from './no_data_views_page';
+export { NoDataViewsPage } from './no_data_views_page';
 
 // eslint-disable-next-line import/no-default-export
 export default NoDataViewsPage;
