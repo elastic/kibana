54de36f85a5b72cbd8a62381af548847c53aebe9
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-02-14 16:55:57 +0200

    [PieVis] PartitionVis integration to Lens. (#123937)
    * Removed pie/donut/mosaic/treemap expressions from lens
    
    * Replaced pie/donut/mosaic/treemap expressions with expressions from expression_partition_vis
    
    * Fixed bug with __other__ labels.
    
    * Cleaned up not used fields at Lens.
    
    * Added support of empty results for multiple chart types.]
    
    * Refactored visualization_noresults.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/lens/public/pie_visualization/index.ts
+++ x-pack/plugins/lens/public/pie_visualization/index.ts
@@ -6,16 +6,12 @@
  */
 
 import type { CoreSetup } from 'src/core/public';
-import type { ExpressionsSetup } from 'src/plugins/expressions/public';
 import type { EditorFrameSetup } from '../types';
 import type { UiActionsStart } from '../../../../../src/plugins/ui_actions/public';
 import type { ChartsPluginSetup } from '../../../../../src/plugins/charts/public';
-import type { FormatFactory } from '../../common';
 
 export interface PieVisualizationPluginSetupPlugins {
   editorFrame: EditorFrameSetup;
-  expressions: ExpressionsSetup;
-  formatFactory: FormatFactory;
   charts: ChartsPluginSetup;
 }
 
@@ -24,22 +20,11 @@ export interface PieVisualizationPluginStartPlugins {
 }
 
 export class PieVisualization {
-  setup(
-    core: CoreSetup,
-    { expressions, formatFactory, editorFrame, charts }: PieVisualizationPluginSetupPlugins
-  ) {
+  setup(core: CoreSetup, { editorFrame, charts }: PieVisualizationPluginSetupPlugins) {
     editorFrame.registerVisualization(async () => {
-      const { getPieVisualization, getPieRenderer } = await import('../async_services');
+      const { getPieVisualization } = await import('../async_services');
       const palettes = await charts.palettes.getPalettes();
 
-      expressions.registerRenderer(
-        getPieRenderer({
-          formatFactory,
-          chartsThemeService: charts.theme,
-          paletteService: palettes,
-          kibanaTheme: core.theme,
-        })
-      );
       return getPieVisualization({ paletteService: palettes, kibanaTheme: core.theme });
     });
   }
