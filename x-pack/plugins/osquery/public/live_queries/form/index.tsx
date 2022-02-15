bc42c254cff8ad8ef4613adc1c09b4edff88b752
Patryk Kopyciński <contact@patrykkopycinski.com>
2022-02-15 11:22:33 +0100

    Update index.tsx

--- x-pack/plugins/osquery/public/live_queries/form/index.tsx
+++ x-pack/plugins/osquery/public/live_queries/form/index.tsx
@@ -86,6 +86,7 @@ const LiveQueryFormComponent: React.FC<LiveQueryFormProps> = ({
 
   const { data, isLoading, mutateAsync, isError, isSuccess } = useMutation(
     (payload: Record<string, unknown>) =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       http.post<any>('/internal/osquery/action', {
         body: JSON.stringify(payload),
       }),
