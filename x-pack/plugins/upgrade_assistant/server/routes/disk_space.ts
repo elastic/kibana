b70098ba22890e1ade4ad0c4af5ab6cff5e943c8
Alison Goryachev <alisonmllr20@gmail.com>
2022-02-15 13:21:33 -0500

    add logic for determining low disk space

--- x-pack/plugins/upgrade_assistant/server/routes/disk_space.ts
+++ x-pack/plugins/upgrade_assistant/server/routes/disk_space.ts
@@ -5,9 +5,20 @@
  * 2.0.
  */
 
+import { ByteSizeValue } from '@kbn/config-schema';
 import { API_BASE_PATH } from '../../common/constants';
 import { versionCheckHandlerWrapper } from '../lib/es_version_precheck';
 import { RouteDependencies } from '../types';
+
+interface NodeWithLowDiskSpace {
+  nodeId: string;
+  used: string;
+  lowDiskWatermarkSetting: string;
+}
+
+// The percentage used to determine if the user has low disk space
+// For example, if a user has used 80%, and has a low disk watermark setting of 85%, we would warn the user
+const LOW_DISK_SPACE_BUFFER_PERCENTAGE = 5;
 
 export function registerDiskSpaceRoute({ router, lib: { handleEsError } }: RouteDependencies) {
   router.get(
@@ -34,7 +45,7 @@ export function registerDiskSpaceRoute({ router, lib: { handleEsError } }: Route
           const { defaults, persistent, transient } = clusterSettings;
 
           const defaultLowDiskWatermarkSetting =
-            defaults['cluster.routing.allocation.disk.watermark.low'];
+            defaults && defaults['cluster.routing.allocation.disk.watermark.low'];
           const transientLowDiskWatermarkSetting =
             transient['cluster.routing.allocation.disk.watermark.low'];
           const persistentLowDiskWatermarkSetting =
@@ -51,27 +62,77 @@ export function registerDiskSpaceRoute({ router, lib: { handleEsError } }: Route
             lowDiskWatermarkSetting = defaultLowDiskWatermarkSetting;
           }
 
-          // This may be undefined if configured in elasticsearch.yml
-          // if (lowDiskWatermarkSetting) {
-          //   const { body: nodeStats } = await client.asCurrentUser.nodes.stats({
-          //     metric: 'fs',
-          //   });
+          if (lowDiskWatermarkSetting) {
+            const { body: nodeStats } = await client.asCurrentUser.nodes.stats({
+              metric: 'fs',
+            });
 
-          //   const nodeIds = Object.keys(nodeStats.nodes);
+            const nodeIds = Object.keys(nodeStats.nodes);
 
-          // TODO finish implementing
-          //   nodeIds.forEach((nodeId) => {
-          //     const node = nodeStats.nodes[nodeId];
-          //     const byteStats = node.fs.total;
-          //     const { total_in_bytes: totalInBytes, available_in_bytes: availableInBytes } =
-          //       byteStats;
-          //   });
+            const nodesWithLowDiskSpace: NodeWithLowDiskSpace[] = [];
 
-          //   return response.ok({ body: {} });
-          // }
+            nodeIds.forEach((nodeId) => {
+              const node = nodeStats.nodes[nodeId];
+              const byteStats = node.fs.total;
+              const { total_in_bytes: totalInBytes, available_in_bytes: availableInBytes } =
+                byteStats;
 
-          // TODO temp
-          return response.ok({ body: {} });
+              // The low disk watermark setting can be configured as a percentage or bytes value
+              const isLowDiskWatermarkPercentage = /^(\d+|(\.\d+))(\.\d+)?%$/.test(
+                lowDiskWatermarkSetting!
+              );
+
+              if (isLowDiskWatermarkPercentage) {
+                const percentageAvailable = Math.round(availableInBytes / totalInBytes) * 100;
+                const percentageUsed = 100 - percentageAvailable;
+                const rawLowDiskWatermarkPercentageValue = Number(
+                  lowDiskWatermarkSetting!.replace('%', '')
+                );
+
+                // Substract LOW_DISK_SPACE_BUFFER_PERCENTAGE (5%) from the low disk watermark setting
+                // If the percentage in use is >= to this, mark node as having low disk space
+                if (
+                  percentageUsed >=
+                  rawLowDiskWatermarkPercentageValue - LOW_DISK_SPACE_BUFFER_PERCENTAGE
+                ) {
+                  nodesWithLowDiskSpace.push({
+                    nodeId,
+                    used: `${percentageUsed}%`,
+                    lowDiskWatermarkSetting: lowDiskWatermarkSetting!,
+                  });
+                }
+              } else {
+                // If not a percentage value, assume user configured low disk watermark setting in bytes
+                const rawLowDiskWatermarkBytesValue = ByteSizeValue.parse(
+                  lowDiskWatermarkSetting!
+                ).getValueInBytes();
+
+                const percentageAvailable = Math.round(availableInBytes / totalInBytes) * 100;
+                const percentageUsed = 100 - percentageAvailable;
+                const rawLowDiskWatermarkPercentageValue =
+                  Math.round(rawLowDiskWatermarkBytesValue / totalInBytes) * 100;
+
+                // Substract LOW_DISK_SPACE_BUFFER_PERCENTAGE (5%) from the low disk watermark setting
+                // If the percentage in use is >= to this, mark node as having low disk space
+                if (
+                  percentageUsed >=
+                  rawLowDiskWatermarkPercentageValue - LOW_DISK_SPACE_BUFFER_PERCENTAGE
+                ) {
+                  nodesWithLowDiskSpace.push({
+                    nodeId,
+                    used: `${percentageUsed}%`,
+                    lowDiskWatermarkSetting: lowDiskWatermarkSetting!,
+                  });
+                }
+              }
+            });
+
+            return response.ok({ body: nodesWithLowDiskSpace });
+          }
+
+          // If the low disk watermark setting is undefined, send empty array
+          // This may occur if configured in elasticsearch.yml
+          return response.ok({ body: [] });
         } catch (error) {
           return handleEsError({ error, response });
         }
