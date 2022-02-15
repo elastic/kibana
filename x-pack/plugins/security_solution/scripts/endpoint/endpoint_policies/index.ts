45fee422c325acef48230f0dcdbe522c74c16c92
Paul Tavares <paul.tavares@elastic.co>
2022-02-15 12:18:55 -0500

    New generated data loading script for endpoint policies

---
+++ x-pack/plugins/security_solution/scripts/endpoint/endpoint_policies/index.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { run, createFailError } from '@kbn/dev-utils';
+import { KbnClient } from '@kbn/test';
+import { indexFleetEndpointPolicy } from '../../../common/endpoint/data_loaders/index_fleet_endpoint_policy';
+import { setupFleetForEndpoint } from '../../../common/endpoint/data_loaders/setup_fleet_for_endpoint';
+
+export const cli = () => {
+  run(
+    async ({ log, flags: { kibana, count } }) => {
+      const kbn = new KbnClient({ log, url: kibana as string });
+      const max = Number(count);
+      let created = 0;
+
+      log.info(`Creating ${count} endpoint policies...`);
+
+      try {
+        const { endpointPackage } = await setupFleetForEndpoint(kbn);
+
+        while (created < max) {
+          created++;
+          await indexFleetEndpointPolicy(kbn, `${count} Endpoint Policy`, endpointPackage.version);
+        }
+      } catch (error) {
+        log.error(error);
+        throw createFailError(error.message);
+      }
+
+      log.success(`Done!`);
+    },
+    {
+      description: 'Load Endpoint Policies into fleet (also creates associated Agent Policies',
+      flags: {
+        string: ['kibana'],
+        default: {
+          count: 15,
+          kibana: 'http://elastic:changeme@localhost:5601',
+        },
+        help: `
+        --count            Number of Endpoint Policies to create. Default: 15
+        --kibana           The URL to kibana including credentials. Default: http://elastic:changeme@localhost:5601
+      `,
+      },
+    }
+  );
+};
