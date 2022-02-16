c119cf22b007853db85cf6f68ce43072abe6654f
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-02-16 15:22:25 +0100

    revert test commits

--- x-pack/plugins/reporting/server/export_types/common/pdf/integration_tests/pdfmaker.test.ts
+++ x-pack/plugins/reporting/server/export_types/common/pdf/integration_tests/pdfmaker.test.ts
@@ -5,11 +5,13 @@
  * 2.0.
  */
 
+/* eslint-disable max-classes-per-file */
+
 import path from 'path';
 import { isUint8Array } from 'util/types';
 import { createMockLayout } from '../../../../../../screenshotting/server/layouts/mock';
 import { PdfMaker } from '../';
-// import { PdfWorkerOutOfMemoryError } from '../pdfmaker_errors';
+import { PdfWorkerOutOfMemoryError } from '../pdfmaker_errors';
 
 const imageBase64 = Buffer.from(
   `iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAGFBMVEXy8vJpaWn7+/vY2Nj39/cAAACcnJzx8fFvt0oZAAAAi0lEQVR4nO3SSQoDIBBFwR7U3P/GQXKEIIJULXr9H3TMrHhX5Yysvj3jjM8+XRnVa9wec8QuHKv3h74Z+PNyGwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/xu3Bxy026rXu4ljdUVW395xUFfGzLo946DK+QW+bgCTFcecSAAAAABJRU5ErkJggg==`,
@@ -43,17 +45,17 @@ describe('PdfMaker', () => {
      * limits we cannot guarantee this test will always execute in a reasonable
      * amount of time.
      */
-    // it.skip('should report when the PDF worker runs out of memory instead of crashing the main thread', async () => {
-    //   const leakyMaker = new (class MemoryLeakPdfMaker extends PdfMaker {
-    //     // From local testing:
-    //     // OOMs after 456.486 seconds with high young generation size
-    //     // OOMs after 53.538 seconds low young generation size
-    //     protected workerMaxOldHeapSizeMb = 2;
-    //     protected workerMaxYoungHeapSizeMb = 2;
-    //     protected workerModulePath = path.resolve(__dirname, './memory_leak_worker.js');
-    //   })(layout, undefined);
-    //   await expect(leakyMaker.generate()).rejects.toBeInstanceOf(PdfWorkerOutOfMemoryError);
-    // });
+    it.skip('should report when the PDF worker runs out of memory instead of crashing the main thread', async () => {
+      const leakyMaker = new (class MemoryLeakPdfMaker extends PdfMaker {
+        // From local testing:
+        // OOMs after 456.486 seconds with high young generation size
+        // OOMs after 53.538 seconds low young generation size
+        protected workerMaxOldHeapSizeMb = 2;
+        protected workerMaxYoungHeapSizeMb = 2;
+        protected workerModulePath = path.resolve(__dirname, './memory_leak_worker.js');
+      })(layout, undefined);
+      await expect(leakyMaker.generate()).rejects.toBeInstanceOf(PdfWorkerOutOfMemoryError);
+    });
 
     it('restarts the PDF worker if it crashes', async () => {
       const buggyMaker = new (class BuggyPdfMaker extends PdfMaker {
