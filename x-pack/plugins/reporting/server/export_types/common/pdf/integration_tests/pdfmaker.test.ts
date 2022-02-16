3bb2a64b849a5685354897c962a11966b10616e8
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-02-16 14:36:24 +0100

    test if jest integration works with skipped test commented out? :doubt:

--- x-pack/plugins/reporting/server/export_types/common/pdf/integration_tests/pdfmaker.test.ts
+++ x-pack/plugins/reporting/server/export_types/common/pdf/integration_tests/pdfmaker.test.ts
@@ -45,17 +45,17 @@ describe('PdfMaker', () => {
      * limits we cannot guarantee this test will always execute in a reasonable
      * amount of time.
      */
-    it.skip('should report when the PDF worker runs out of memory instead of crashing the main thread', async () => {
-      const leakyMaker = new (class MemoryLeakPdfMaker extends PdfMaker {
-        // From local testing:
-        // OOMs after 456.486 seconds with high young generation size
-        // OOMs after 53.538 seconds low young generation size
-        protected workerMaxOldHeapSizeMb = 2;
-        protected workerMaxYoungHeapSizeMb = 2;
-        protected workerModulePath = path.resolve(__dirname, './memory_leak_worker.js');
-      })(layout, undefined);
-      await expect(leakyMaker.generate()).rejects.toBeInstanceOf(PdfWorkerOutOfMemoryError);
-    });
+    // it.skip('should report when the PDF worker runs out of memory instead of crashing the main thread', async () => {
+    //   const leakyMaker = new (class MemoryLeakPdfMaker extends PdfMaker {
+    //     // From local testing:
+    //     // OOMs after 456.486 seconds with high young generation size
+    //     // OOMs after 53.538 seconds low young generation size
+    //     protected workerMaxOldHeapSizeMb = 2;
+    //     protected workerMaxYoungHeapSizeMb = 2;
+    //     protected workerModulePath = path.resolve(__dirname, './memory_leak_worker.js');
+    //   })(layout, undefined);
+    //   await expect(leakyMaker.generate()).rejects.toBeInstanceOf(PdfWorkerOutOfMemoryError);
+    // });
 
     it('restarts the PDF worker if it crashes', async () => {
       const buggyMaker = new (class BuggyPdfMaker extends PdfMaker {
