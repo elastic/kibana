c73cbc46f6fc9dcb458776f75c5fd67aabaf9d97
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-02-16 17:03:03 +0100

    remove unused errors

--- x-pack/plugins/reporting/common/errors/index.ts
+++ x-pack/plugins/reporting/common/errors/index.ts
@@ -24,28 +24,6 @@ export abstract class ReportingError extends Error {
 }
 
 /**
- * When system/user unexpectedly interrupts a reporting job by stopping Kibana.
- */
-export class KibanaStoppedUnexpectedlyError extends ReportingError {
-  code = 'kibana_stopped_unexpectedly';
-}
-
-/**
- * We detected that some Chromium dependency is missing and therefore cannot
- * launch.
- */
-export class MissingChromiumDependenciesError extends ReportingError {
-  code = 'missing_chromium_dependencies';
-}
-
-/**
- * For some unspecified reason we cannot launch the browser. See details.
- */
-export class CannotStartChromiumError extends ReportingError {
-  code = 'cannot_start_chromium';
-}
-
-/**
  * While performing some reporting action, like fetching data from ES, our
  * access token expired.
  */
@@ -63,3 +41,8 @@ export class QueueTimeoutError extends ReportingError {
 export class UnknownError extends ReportingError {
   code = 'unknown_error';
 }
+
+// TODO: Add ReportingError for Kibana stopping unexpectedly
+// TODO: Add ReportingError for missing Chromium dependencies
+// TODO: Add ReportingError for missing Chromium dependencies
+// TODO: Add ReportingError for Chromium not starting for an unknown reason
