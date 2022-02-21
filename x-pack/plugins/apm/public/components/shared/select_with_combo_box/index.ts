1f99101b054335851242d9299d146ae7bde27e1a
Kate Patticha <aikaterini.patticha@elastic.co>
2022-02-21 13:32:25 +0100

    Pass the correct allOption value

--- x-pack/plugins/apm/public/components/shared/select_with_combo_box/index.ts
+++ x-pack/plugins/apm/public/components/shared/select_with_combo_box/index.ts
@@ -9,13 +9,15 @@ import { EuiComboBoxOptionOption } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { ENVIRONMENT_ALL } from '../../../../common/environment_filter_values';
 
+export const ALL_OPTION_VALUE = 'ALL_OPTION_VALUE';
+
 const allOptionText = i18n.translate('xpack.apm.allOption', {
   defaultMessage: 'All',
 });
 
 export const allOption: EuiComboBoxOptionOption<string> = {
   label: allOptionText,
-  value: allOptionText,
+  value: ALL_OPTION_VALUE,
 };
 
 export const environmentAllOption: EuiComboBoxOptionOption<string> = {
