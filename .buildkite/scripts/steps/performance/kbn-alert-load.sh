41fffbaa7b7620a6d1b3eaf61797dc3beff93f48
Khristinin Nikita <nikita.khristinin@elastic.co>
2022-02-22 12:10:44 +0100

    Try npm

--- .buildkite/scripts/steps/performance/kbn-alert-load.sh
+++ .buildkite/scripts/steps/performance/kbn-alert-load.sh
@@ -6,8 +6,8 @@ echo "--- Install KBN-ALERT-LOAD"
 
 mkdir kbn-alert-load
 cd kbn-alert-load
-yarn add nkhristinin/kbn-alert-load
+yarn add test-alert-load-kbn
 
 echo "--- Run IM tests"
 
-npx kbn-alert-load run im-test
+npx test-alert-load-kbn run im-test
