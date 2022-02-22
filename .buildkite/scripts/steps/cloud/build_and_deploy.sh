03ebf7790aee2f5c3656e92911fdbaef3d274b5f
Jonathan Budzenski <jon@elastic.co>
2022-02-22 14:05:13 -0600

    update all versions

--- .buildkite/scripts/steps/cloud/build_and_deploy.sh
+++ .buildkite/scripts/steps/cloud/build_and_deploy.sh
@@ -63,7 +63,7 @@ if [ -z "${CLOUD_DEPLOYMENT_ID}" ]; then
 else
 ecctl deployment show "$CLOUD_DEPLOYMENT_ID" --generate-update-payload | jq '
   .resources.kibana[0].plan.kibana.docker_image = "'$CLOUD_IMAGE'" |
-  .resources.kibana[0].plan.kibana.version = "'$VERSION'" |
+  (.. | select(.version? != null).version) = "'$VERSION'"
   ' > /tmp/deploy.json
   ecctl deployment update "$CLOUD_DEPLOYMENT_ID" --track --output json --file /tmp/deploy.json &> "$JSON_FILE"
 fi
