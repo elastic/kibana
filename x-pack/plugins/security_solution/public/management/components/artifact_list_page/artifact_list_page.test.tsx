3f52c80c3b907bc0e25fc02b48704f775da41d12
Paul Tavares <paul.tavares@elastic.co>
2022-02-22 16:37:50 -0500

    Tests outline for artifact list page

--- x-pack/plugins/security_solution/public/management/components/artifact_list_page/artifact_list_page.test.tsx
+++ x-pack/plugins/security_solution/public/management/components/artifact_list_page/artifact_list_page.test.tsx
@@ -11,7 +11,8 @@ import { trustedAppsAllHttpMocks } from '../../pages/mocks';
 import { ArtifactListPage, ArtifactListPageProps } from './artifact_list_page';
 import { TrustedAppsApiClient } from '../../pages/trusted_apps/service/trusted_apps_api_client';
 import { artifactListPageLabels } from './translations';
-import { act, waitForElementToBeRemoved } from '@testing-library/react';
+import { act, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('When using the ArtifactListPage component', () => {
   let render: () => ReturnType<AppContextTestRender['render']>;
@@ -92,10 +93,123 @@ describe('When using the ArtifactListPage component', () => {
         per_page: 10,
         total: 0,
       });
+
+      render();
+    });
+
+    it('should display empty state', async () => {
+      await waitFor(async () => {
+        expect(renderResult.getByTestId('testPage-emptyState'));
+      });
+    });
+
+    it('should hide page headers', async () => {
+      expect(renderResult.queryByTestId('header-page-title')).toBe(null);
+    });
+
+    it('should open create flyout when primary button is clicked', async () => {
+      const addButton = await renderResult.findByTestId('testPage-emptyState-addButton');
+
+      act(() => {
+        userEvent.click(addButton);
+      });
+
+      expect(renderResult.getByTestId('testPage-flyout')).toBeTruthy();
+      expect(history.location.search).toMatch(/show=create/);
+    });
+  });
+
+  describe('and the flyout is opened', () => {
+    beforeEach(async () => {
+      history.push('somepage?show=create');
+      render();
+
+      await waitFor(async () => {
+        expect(renderResult.getByTestId('testPage-flyout'));
+      });
+    });
+
+    it.todo('should display `Cancel` button enabled');
+
+    it.todo('should display `Submit` button as disabled');
+
+    it.each([
+      ['Cancel', ''],
+      ['Close', 'euiFlyoutCloseButton'],
+    ])('should close flyout when `%s` button is clicked', (_, testId) => {
+      // FIXME: PT implement
+    });
+
+    it.todo(
+      'should show expired license warning when unsupported features are being used (downgrade scenario)'
+    );
+
+    describe('and form data is valid', () => {
+      it.todo('should enable the `Submit` button');
+
+      it.todo('should disable all buttons while an update is in flight');
+
+      it.todo('should display loading indicator on Submit while an update is in flight');
+
+      it.todo('should pass `disabled=true` to the Form component while an update is in flight');
+
+      describe('and submit is successful', () => {
+        it.todo('should show a success toast');
+
+        it.todo('should close flyout');
+      });
+
+      describe('and submit fails', () => {
+        it.todo('should re-enable `Cancel` and `Close` and `Submit` buttons');
+
+        it.todo('should pass error along to the Form component and reset disabled back to `false`');
+      });
+    });
+
+    describe('and in Edit mode with invalid item id in url', () => {
+      it.todo('should show loader while initializing in edit mode');
+
+      it.todo('should show error toast and close flyout if item for edit does not exist');
     });
   });
 
   describe('and data exists', () => {
     // FIXME:PT with data
+
+    it.todo('should show list loading indicator while results are retrieved');
+
+    it.todo(`should show cards with results`);
+
+    it.todo('should show card actions');
+
+    it.todo('should persist pagination changes to the URL');
+
+    describe('and interacting with card actions', () => {
+      it.todo('should display the Edit flyout');
+
+      it.todo('should display the Delete modal');
+
+      describe('and interacting with the deletion modal', () => {
+        it.todo('should show Cancel and Delete buttons enabled');
+
+        it.todo('should close modal if Cancel/Close buttons are clicked');
+
+        it.todo('should prevent modal from being closed while deletion is in flight');
+
+        it.todo('should show success toast if deleted successfully');
+
+        it.todo('should show error toast if deletion failed');
+      });
+    });
+
+    describe('and search bar is used', () => {
+      it.todo('should persist filter to the URL params');
+
+      it.todo('should persist policy filter to the URL params');
+
+      it.todo('should trigger a current page data fetch when Refresh button is clicked');
+
+      it.todo('should show a no results found message if filter did not return any results');
+    });
   });
 });
