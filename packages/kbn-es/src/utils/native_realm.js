7179bfa0ac0c98e4cead959e65374c2f43a63ec7
spalger <spencer@elastic.co>
2022-02-22 18:45:30 -0600

    [kbn/es] add support for --ready-timeout

--- packages/kbn-es/src/utils/native_realm.js
+++ packages/kbn-es/src/utils/native_realm.js
@@ -6,7 +6,6 @@
  * Side Public License, v 1.
  */
 
-const { Client } = require('@elastic/elasticsearch');
 const chalk = require('chalk');
 
 const { log: defaultLog } = require('./log');
@@ -15,14 +14,9 @@ export const SYSTEM_INDICES_SUPERUSER =
   process.env.TEST_ES_SYSTEM_INDICES_USER || 'system_indices_superuser';
 
 exports.NativeRealm = class NativeRealm {
-  constructor({ elasticPassword, port, log = defaultLog, ssl = false, caCert }) {
-    const auth = { username: 'elastic', password: elasticPassword };
-    this._client = new Client(
-      ssl
-        ? { node: `https://localhost:${port}`, tls: { ca: caCert, rejectUnauthorized: true }, auth }
-        : { node: `http://localhost:${port}`, auth }
-    );
+  constructor({ elasticPassword, log = defaultLog, client }) {
     this._elasticPassword = elasticPassword;
+    this._client = client;
     this._log = log;
   }
 
@@ -53,24 +47,14 @@ exports.NativeRealm = class NativeRealm {
     });
   }
 
-  async clusterReady() {
-    return await this._autoRetry({ maxAttempts: 10 }, async () => {
-      const { status } = await this._client.cluster.health();
-      if (status === 'red') {
-        throw new Error(`not ready, cluster health is ${status}`);
-      }
-    });
-  }
-
   async setPasswords(options) {
-    await this.clusterReady();
-
     if (!(await this.isSecurityEnabled())) {
       this._log.info('security is not enabled, unable to set native realm passwords');
       return;
     }
 
     const reservedUsers = await this.getReservedUsers();
+    this._log.info(`Set up ${reservedUsers.length} ES users`);
     await Promise.all([
       ...reservedUsers.map(async (user) => {
         await this.setPassword(user, options[`password.${user}`]);
@@ -108,7 +92,7 @@ exports.NativeRealm = class NativeRealm {
   }
 
   async _autoRetry(opts, fn) {
-    const { attempt = 1, maxAttempts = 3, sleep = 1000 } = opts;
+    const { attempt = 1, maxAttempts = 3 } = opts;
 
     try {
       return await fn(attempt);
@@ -119,7 +103,7 @@ exports.NativeRealm = class NativeRealm {
 
       const sec = 1.5 * attempt;
       this._log.warning(`assuming ES isn't initialized completely, trying again in ${sec} seconds`);
-      await new Promise((resolve) => setTimeout(resolve, sleep));
+      await new Promise((resolve) => setTimeout(resolve, sec * 1000));
 
       const nextOpts = {
         ...opts,
