a537f76eeefceb6303b8c89364bdea847b605ff0
Khristinin Nikita <nikita.khristinin@elastic.co>
2022-02-23 12:59:09 +0100

    comment meta info

--- .buildkite/scripts/steps/performance/kbn-alert-load.sh
+++ .buildkite/scripts/steps/performance/kbn-alert-load.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 CLOUD_DEPLOYMENT_NAME="kibana-pr-$BUILDKITE_PULL_REQUEST"
 
-KIBANA_URL="$(buildkite-agent meta-data get 'META_CLOUD_DEPLOYMENT_KIBANA_URL')"
-ELASTIC_URL="$(buildkite-agent meta-data get 'META_CLOUD_DEPLOYMENT_ELASTICSEARCH_URL')"
+# KIBANA_URL="$(buildkite-agent meta-data get 'META_CLOUD_DEPLOYMENT_KIBANA_URL')"
+# ELASTIC_URL="$(buildkite-agent meta-data get 'META_CLOUD_DEPLOYMENT_ELASTICSEARCH_URL')"
 
 DEPLOYMENT_USERNAME="$(retry 5 5 vault read -field=username secret/kibana-issues/dev/cloud-deploy/$CLOUD_DEPLOYMENT_NAME)"
 DEPLOYMENT_PASSWORD="$(retry 5 5 vault read -field=password secret/kibana-issues/dev/cloud-deploy/$CLOUD_DEPLOYMENT_NAME)"
