0b310d5f4df1d8fa64897f96caf49369ad28bcc8
Christos Nasikas <christos.nasikas@elastic.co>
2022-02-24 14:10:32 +0200

    Draft schema

---
+++ x-pack/plugins/cases/server/telemetry/schema.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { MakeSchemaFrom } from 'src/plugins/usage_collection/server';
+import { CasesTelemetry, Long, Count } from './types';
+
+const long: Long = { type: 'long' };
+
+const countSchema: MakeSchemaFrom<Count> = {
+  all: long,
+  '1m': long,
+  '1w': long,
+  '1d': long,
+};
+
+export const casesSchema: MakeSchemaFrom<CasesTelemetry> = {
+  cases: {
+    all: countSchema,
+    sec: countSchema,
+    obs: countSchema,
+    main: countSchema,
+    syncAlertsOn: long,
+    syncAlertsOff: long,
+  },
+  userActions: { all: countSchema, maxOnACase: long },
+  comments: { all: countSchema, maxOnACase: long },
+  alerts: { all: countSchema, maxOnACase: long },
+  connectors: {
+    maxAttachedToACase: countSchema,
+    itsm: { totalAttached: long },
+    sir: { totalAttached: long },
+    jira: { totalAttached: long },
+    ibm: { totalAttached: long },
+    swimlane: { totalAttached: long },
+  },
+  externalServices: {
+    totalPushes: long,
+    maxPushesOnACase: long,
+  },
+  configuration: {
+    closeCaseAfterPush: long,
+  },
+};
---
+++ x-pack/plugins/cases/server/telemetry/types.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export interface Long {
+  type: 'long';
+}
+
+export interface Count {
+  all: number;
+  '1m': number;
+  '1w': number;
+  '1d': number;
+}
+
+export interface CasesTelemetry {
+  cases: {
+    all: Count;
+    sec: Count;
+    obs: Count;
+    main: Count;
+    syncAlertsOn: number;
+    syncAlertsOff: number;
+  };
+  userActions: { all: Count; maxOnACase: number };
+  comments: { all: Count; maxOnACase: number };
+  alerts: { all: Count; maxOnACase: number };
+  connectors: {
+    maxAttachedToACase: Count;
+    itsm: { totalAttached: number };
+    sir: { totalAttached: number };
+    jira: { totalAttached: number };
+    ibm: { totalAttached: number };
+    swimlane: { totalAttached: number };
+  };
+  externalServices: {
+    totalPushes: number;
+    maxPushesOnACase: number;
+  };
+  configuration: {
+    closeCaseAfterPush: number;
+  };
+}
