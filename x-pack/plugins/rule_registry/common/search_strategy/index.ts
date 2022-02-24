f6f7f0104848f1d1a5c2bd756eda0b214e8d1c61
chrisronline <chris.roberson@elastic.co>
2022-02-24 16:45:03 -0500

    Use nested key TS definition

--- x-pack/plugins/rule_registry/common/search_strategy/index.ts
+++ x-pack/plugins/rule_registry/common/search_strategy/index.ts
@@ -14,6 +14,45 @@ export type RuleRegistrySearchRequest = IEsSearchRequest & {
   query?: { bool: estypes.QueryDslBoolQuery };
 };
 
-export type RuleRegistrySearchResponse = IEsSearchResponse<{
-  [Property in keyof Ecs]: any[];
-}>;
+type Prev = [
+  never,
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  ...Array<0>
+];
+
+type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${'' extends P ? '' : '.'}${P}`
+    : never
+  : never;
+
+type DotNestedKeys<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? { [K in keyof T]-?: Join<K, DotNestedKeys<T[K], Prev[D]>> }[keyof T]
+  : '';
+
+type EcsFieldsResponse = {
+  [Property in DotNestedKeys<Ecs>]: string[];
+};
+export type RuleRegistrySearchResponse = IEsSearchResponse<EcsFieldsResponse>;
