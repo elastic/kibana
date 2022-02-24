065f3dba5e83bb509b90d7083c2bb03efb7dfdf7
Chris Cowan <chris@elastic.co>
2022-02-24 10:28:11 -0700

    Remove unused dependencies

--- x-pack/plugins/infra/server/lib/alerting/inventory_metric_threshold/evaluate_condition.ts
+++ x-pack/plugins/infra/server/lib/alerting/inventory_metric_threshold/evaluate_condition.ts
@@ -8,14 +8,13 @@
 import { ElasticsearchClient } from 'kibana/server';
 import { mapValues } from 'lodash';
 import moment from 'moment';
-import { Comparator, InventoryMetricConditions } from '../../../../common/alerting/metrics';
+import { InventoryMetricConditions } from '../../../../common/alerting/metrics';
 import { InfraTimerangeInput } from '../../../../common/http_api';
-import { InventoryItemType, SnapshotMetricType } from '../../../../common/inventory_models/types';
+import { InventoryItemType } from '../../../../common/inventory_models/types';
 import { LogQueryFields } from '../../../services/log_queries/get_log_query_fields';
 import { InfraSource } from '../../sources';
 import { calcualteFromBasedOnMetric } from './lib/calculate_from_based_on_metric';
 import { getData } from './lib/get_data';
-import { convertMetricValue } from './lib/convert_metric_value';
 
 type ConditionResult = InventoryMetricConditions & {
   shouldFire: boolean[];
