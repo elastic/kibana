4c7f574e3677161c62e38f78e336d6d48c83d23d
John Dorlus <jsdorlus@gmail.com>
2022-02-24 16:33:11 -0500

    Added CCS test for data view functionality. (#124586)
    * Added CCS test for data view functionality.
    
    * Changed how we are approaching the test. Just adding a remote to exercise the CCS functionality.
    
    * Adjusted test to use correct port for remote cluster.
    
    * Added and corrected config for the tests to be able to run.
    
    * Changed test title.
    
    * Added functional ccs suite to functional test runner script.
    
    * Changed config.ts to config.js
    
    * Moved es to before clause.
    
    * Added await for an async call.
    
    * Added await for an async call.
    
    * Updated saved queries test to use CCS. One test is failing, all others are passing.
    
    * Merge branch 'main' of github.com:elastic/kibana into CCS_Functional_Test_POC
    
    * Fixed a reference.
    
    * Removed the OSS tests since we created CCS versions of them.
    
    * Removed tests from index file.
    
    * Restored test files per comment with matthias.
    
    * Did some cleanup of comments.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 1e16db4d18302c4434acfb8ce2e7fb6a414c5f2f)

--- scripts/functional_tests.js
+++ scripts/functional_tests.js
@@ -9,6 +9,7 @@
 // eslint-disable-next-line no-restricted-syntax
 const alwaysImportedTests = [
   require.resolve('../test/functional/config.js'),
+  require.resolve('../test/functional_ccs/config.js'),
   require.resolve('../test/plugin_functional/config.ts'),
   require.resolve('../test/ui_capabilities/newsfeed_err/config.ts'),
   require.resolve('../test/new_visualize_flow/config.ts'),
