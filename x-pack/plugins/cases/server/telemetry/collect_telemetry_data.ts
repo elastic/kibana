164904a191fbd397abb0d877d2dc8c4148ad61d0
Christos Nasikas <christos.nasikas@elastic.co>
2022-02-28 12:53:30 +0200

    Return empty state

--- x-pack/plugins/cases/server/telemetry/collect_telemetry_data.ts
+++ x-pack/plugins/cases/server/telemetry/collect_telemetry_data.ts
@@ -7,4 +7,33 @@
 
 import { CasesTelemetry } from './types';
 
-export const collectTelemetryData = async (): Promise<CasesTelemetry> => {};
+export const collectTelemetryData = async (): Promise<CasesTelemetry> => {
+  return {
+    cases: {
+      all: { all: 0, '1d': 0, '1w': 0, '1m': 0 },
+      sec: { all: 0, '1d': 0, '1w': 0, '1m': 0 },
+      obs: { all: 0, '1d': 0, '1w': 0, '1m': 0 },
+      main: { all: 0, '1d': 0, '1w': 0, '1m': 0 },
+      syncAlertsOn: 0,
+      syncAlertsOff: 0,
+    },
+    userActions: { all: { all: 0, '1d': 0, '1w': 0, '1m': 0 }, maxOnACase: 0 },
+    comments: { all: { all: 0, '1d': 0, '1w': 0, '1m': 0 }, maxOnACase: 0 },
+    alerts: { all: { all: 0, '1d': 0, '1w': 0, '1m': 0 }, maxOnACase: 0 },
+    connectors: {
+      maxAttachedToACase: { all: 0, '1d': 0, '1w': 0, '1m': 0 },
+      itsm: { totalAttached: 0 },
+      sir: { totalAttached: 0 },
+      jira: { totalAttached: 0 },
+      ibm: { totalAttached: 0 },
+      swimlane: { totalAttached: 0 },
+    },
+    externalServices: {
+      totalPushes: 0,
+      maxPushesOnACase: 0,
+    },
+    configuration: {
+      closeCaseAfterPush: 0,
+    },
+  };
+};
