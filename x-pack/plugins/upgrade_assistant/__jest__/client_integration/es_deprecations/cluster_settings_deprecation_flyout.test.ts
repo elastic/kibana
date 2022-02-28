e5662531466f4c63af54f89dd6f7cc85a627ef3d
Alison Goryachev <alisonmllr20@gmail.com>
2022-02-28 12:27:34 -0500

    remove unused imports

--- x-pack/plugins/upgrade_assistant/__jest__/client_integration/es_deprecations/cluster_settings_deprecation_flyout.test.ts
+++ x-pack/plugins/upgrade_assistant/__jest__/client_integration/es_deprecations/cluster_settings_deprecation_flyout.test.ts
@@ -9,7 +9,7 @@ import { act } from 'react-dom/test-utils';
 
 import { setupEnvironment } from '../helpers';
 import { ElasticsearchTestBed, setupElasticsearchPage } from './es_deprecations.helpers';
-import { esDeprecationsMockResponse, MOCK_SNAPSHOT_ID, MOCK_JOB_ID } from './mocked_responses';
+import { esDeprecationsMockResponse } from './mocked_responses';
 
 describe('Cluster settings deprecation flyout', () => {
   let testBed: ElasticsearchTestBed;
