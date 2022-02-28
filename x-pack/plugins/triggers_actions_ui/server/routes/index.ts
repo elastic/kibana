f51131c42c722775bd018857a7ad2bbb59321451
Ying Mao <ying.mao@elastic.co>
2022-02-28 17:48:08 -0500

    [Alerting] Adding a configurable minimal rule interval for newly created rules (#125396)
    * Removing minimumScheduleInterval on rule type registration
    
    * Adding minimumScheduleInterval to config and enforcing in rule type registry
    
    * Validating interval on create and update
    
    * Fixing types and tests
    
    * Fixing types and tests
    
    * Fixing types and tests
    
    * Passing config to client and using to validate on rule creation
    
    * Fixing small bug and tests
    
    * Fixing tests
    
    * Fixing tests
    
    * Fixing tests
    
    * Updating interval in docs
    
    * Updating interval in docs
    
    * Updating UI copy
    
    * Fixing types and tests
    
    * Fixing i18n
    
    * Fixing tests from bad merge
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/triggers_actions_ui/server/routes/index.ts
+++ x-pack/plugins/triggers_actions_ui/server/routes/index.ts
@@ -4,9 +4,5 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-// TODO: https://github.com/elastic/kibana/issues/110895
-/* eslint-disable @kbn/eslint/no_export_all */
-
-export * from './data';
-export const BASE_TRIGGERS_ACTIONS_UI_API_PATH = '/api/triggers_actions_ui';
+export { createHealthRoute } from './health';
+export { createConfigRoute } from './config';
