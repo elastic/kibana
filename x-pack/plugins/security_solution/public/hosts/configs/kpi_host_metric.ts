0cc92f6eac39e24eef95aa291dcce78f51a5603a
Angela Chuang <yi-chun.chuang@elastic.co>
2022-03-01 03:00:11 +0800

    add chart configs

---
+++ x-pack/plugins/security_solution/public/hosts/configs/kpi_host_metric.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { LensAttributes } from '../../common/components/matrix_histogram/types';
+
+export const kpiHostMetric: LensAttributes = {
+  description: '',
+  state: {
+    datasourceStates: {
+      indexpattern: {
+        layers: {
+          '416b6fad-1923-4f6a-a2df-b223bb287e30': {
+            columnOrder: ['b00c65ea-32be-4163-bfc8-f795b1ef9d06'],
+            columns: {
+              'b00c65ea-32be-4163-bfc8-f795b1ef9d06': {
+                customLabel: true,
+                dataType: 'number',
+                isBucketed: false,
+                label: ' ',
+                operationType: 'unique_count',
+                scale: 'ratio',
+                sourceField: 'host.name',
+              },
+            },
+            incompleteColumns: {},
+          },
+        },
+      },
+    },
+    filters: [],
+    query: { language: 'kuery', query: '' },
+    visualization: {
+      accessor: 'b00c65ea-32be-4163-bfc8-f795b1ef9d06',
+      layerId: '416b6fad-1923-4f6a-a2df-b223bb287e30',
+      layerType: 'data',
+    },
+  },
+  title: '[Host] KPI Hosts - metric',
+  visualizationType: 'lnsMetric',
+  references: [
+    {
+      id: 'security-solution-default',
+      name: 'indexpattern-datasource-current-indexpattern',
+      type: 'index-pattern',
+    },
+    {
+      id: 'security-solution-default',
+      name: 'indexpattern-datasource-layer-416b6fad-1923-4f6a-a2df-b223bb287e30',
+      type: 'index-pattern',
+    },
+    {
+      id: '880973d0-89cb-11ec-acbb-112a5cf3323a',
+      name: 'tag-ref-880973d0-89cb-11ec-acbb-112a5cf3323a',
+      type: 'tag',
+    },
+  ],
+} as LensAttributes;
