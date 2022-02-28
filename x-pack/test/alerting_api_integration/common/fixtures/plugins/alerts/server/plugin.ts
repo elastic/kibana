67af8b8ad2b3385ec86c1e06be3a7d332cd81883
Ying Mao <ying.mao@elastic.co>
2022-02-28 14:02:00 -0500

    Adding run now route to alerting_api_integration tests

--- x-pack/test/alerting_api_integration/common/fixtures/plugins/alerts/server/plugin.ts
+++ x-pack/test/alerting_api_integration/common/fixtures/plugins/alerts/server/plugin.ts
@@ -5,9 +5,15 @@
  * 2.0.
  */
 
-import { Plugin, CoreSetup, Logger, PluginInitializerContext } from 'kibana/server';
+import { Plugin, CoreSetup, CoreStart, Logger, PluginInitializerContext } from 'kibana/server';
+import { Subject } from 'rxjs';
+import { first } from 'rxjs/operators';
 import { PluginSetupContract as ActionsPluginSetup } from '../../../../../../../plugins/actions/server/plugin';
 import { PluginSetupContract as AlertingPluginSetup } from '../../../../../../../plugins/alerting/server/plugin';
+import {
+  TaskManagerSetupContract,
+  TaskManagerStartContract,
+} from '../../../../../../../plugins/task_manager/server/plugin';
 import { EncryptedSavedObjectsPluginStart } from '../../../../../../../plugins/encrypted_saved_objects/server';
 import { PluginSetupContract as FeaturesPluginSetup } from '../../../../../../../plugins/features/server';
 import { defineAlertTypes } from './alert_types';
@@ -21,6 +27,7 @@ export interface FixtureSetupDeps {
   features: FeaturesPluginSetup;
   actions: ActionsPluginSetup;
   alerting: AlertingPluginSetup;
+  taskManager: TaskManagerSetupContract;
 }
 
 export interface FixtureStartDeps {
@@ -28,10 +35,16 @@ export interface FixtureStartDeps {
   security?: SecurityPluginStart;
   spaces?: SpacesPluginStart;
   actions: ActionsPluginStart;
+  taskManager: TaskManagerStartContract;
 }
 
 export class FixturePlugin implements Plugin<void, void, FixtureSetupDeps, FixtureStartDeps> {
   private readonly logger: Logger;
+
+  taskManagerStart$: Subject<TaskManagerStartContract> = new Subject<TaskManagerStartContract>();
+  taskManagerStart: Promise<TaskManagerStartContract> = this.taskManagerStart$
+    .pipe(first())
+    .toPromise();
 
   constructor(initializerContext: PluginInitializerContext) {
     this.logger = initializerContext.logger.get('fixtures', 'plugins', 'alerts');
@@ -127,9 +140,12 @@ export class FixturePlugin implements Plugin<void, void, FixtureSetupDeps, Fixtu
 
     defineActionTypes(core, { actions });
     defineAlertTypes(core, { alerting });
-    defineRoutes(core, { logger: this.logger });
+    defineRoutes(core, this.taskManagerStart, { logger: this.logger });
   }
 
-  public start() {}
+  public start(core: CoreStart, { taskManager }: FixtureStartDeps) {
+    this.taskManagerStart$.next(taskManager);
+    this.taskManagerStart$.complete();
+  }
   public stop() {}
 }
