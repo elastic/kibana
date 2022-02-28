2177bf17454e24d9ae05dc2a5d1c1645e34df2b5
Christos Nasikas <christos.nasikas@elastic.co>
2022-02-28 12:53:52 +0200

    Register custom usage collector

--- x-pack/plugins/cases/server/telemetry/index.ts
+++ x-pack/plugins/cases/server/telemetry/index.ts
@@ -10,6 +10,7 @@ import {
   ISavedObjectsRepository,
   Logger,
   PluginInitializerContext,
+  SavedObjectsErrorHelpers,
 } from 'src/core/server';
 import { TaskManagerSetupContract } from '../../../task_manager/server';
 import { UsageCollectionSetup } from '../../../../../src/plugins/usage_collection/server';
@@ -19,6 +20,8 @@ import {
   CASES_TELEMETRY_TASK_NAME,
   CASE_TELEMETRY_SAVED_OBJECT_ID,
 } from '../../common/constants';
+import { CasesTelemetry } from './types';
+import { casesSchema } from './schema';
 
 export { scheduleCasesTelemetryTask } from './schedule_telemetry_task';
 
@@ -59,13 +62,34 @@ export const createCasesTelemetry = async ({
     const savedObjectsClient = await getInternalSavedObjectClient();
     const telemetryData = await collectTelemetryData();
 
-    await savedObjectsClient.create(
-      CASE_TELEMETRY_SAVED_OBJECT,
-      {
-        ...telemetryData,
-        kibanaVersion,
-      },
-      { id: CASE_TELEMETRY_SAVED_OBJECT_ID, overwrite: true }
-    );
+    await savedObjectsClient.create(CASE_TELEMETRY_SAVED_OBJECT, telemetryData, {
+      id: CASE_TELEMETRY_SAVED_OBJECT_ID,
+      overwrite: true,
+    });
   };
+
+  const collector = usageCollection.makeUsageCollector<CasesTelemetry | {}>({
+    type: 'cases',
+    schema: casesSchema,
+    isReady: () => true,
+    fetch: async () => {
+      try {
+        const savedObjectsClient = await getInternalSavedObjectClient();
+        const data = (
+          await savedObjectsClient.get(CASE_TELEMETRY_SAVED_OBJECT, CASE_TELEMETRY_SAVED_OBJECT_ID)
+        ).attributes as CasesTelemetry;
+
+        return data;
+      } catch (err) {
+        if (SavedObjectsErrorHelpers.isNotFoundError(err)) {
+          // task has not run yet, so no saved object to return
+          return {};
+        }
+
+        throw err;
+      }
+    },
+  });
+
+  usageCollection.registerCollector(collector);
 };
