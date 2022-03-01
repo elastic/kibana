f8bf38b1250e7bbeb3a42c72ab680bb296d58bc3
Kevin Qualters <kevin.qualters@elastic.co>
2022-02-28 20:45:59 -0500

    Update hover actions css selector to re-hide default cell actions

--- x-pack/plugins/security_solution/public/common/components/page/index.tsx
+++ x-pack/plugins/security_solution/public/common/components/page/index.tsx
@@ -35,12 +35,13 @@ export const AppGlobalStyle = createGlobalStyle<{ theme: { eui: { euiColorPrimar
     z-index: 9950 !important;
   }
 
-  .euiDataGridRowCell__expandButton .euiDataGridRowCell__actionButtonIcon {
+  .euiDataGridRowCell .euiDataGridRowCell__expandActions .euiDataGridRowCell__actionButtonIcon {
     display: none;
 
     &:first-child,
     &:nth-child(2),
-    &:nth-child(3) {
+    &:nth-child(3),
+    &:nth-child(6) {
       display: inline-flex;
     }
 
