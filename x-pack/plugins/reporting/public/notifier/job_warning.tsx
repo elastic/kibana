b47c03915391e5036abd3b0aba653400c977eacc
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-03-01 09:32:29 +0100

    updated report link copy, removed unnecessary copy and rather use new fragment in job warning

--- x-pack/plugins/reporting/public/notifier/job_warning.tsx
+++ x-pack/plugins/reporting/public/notifier/job_warning.tsx
@@ -6,7 +6,7 @@
  */
 
 import { FormattedMessage } from '@kbn/i18n-react';
-import React, { Fragment } from 'react';
+import React from 'react';
 import { ThemeServiceStart, ToastInput } from 'src/core/public';
 import { toMountPoint } from '../../../../../src/plugins/kibana_react/public';
 import { JobId, JobSummary } from '../../common/types';
@@ -28,18 +28,12 @@ export const getWarningToast = (
     { theme$: theme.theme$ }
   ),
   text: toMountPoint(
-    <Fragment>
-      <p>
-        <FormattedMessage
-          id="xpack.reporting.publicNotifier.warning.description"
-          defaultMessage="The report completed with issues."
-        />
-      </p>
+    <>
       <p>
         <ReportLink getUrl={getReportLink} />
       </p>
       <DownloadButton getUrl={getDownloadLink} job={job} />
-    </Fragment>,
+    </>,
     { theme$: theme.theme$ }
   ),
   'data-test-subj': 'completeReportWarning',
