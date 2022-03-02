155b232f7638c21040b60fd52c086337ce3ed1b1
Tyler Smalley <tyler.smalley@elastic.co>
2022-03-01 22:05:02 -0800

    TESTING
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>

--- .buildkite/scripts/steps/functional/common.sh
+++ .buildkite/scripts/steps/functional/common.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 # Note, changes here might also need to be made in other scripts, e.g. uptime.sh
 
+# TEMP: DO NOT MERGE
+export ES_SNAPSHOT_MANIFEST="https://storage.googleapis.com/kibana-ci-es-snapshots-daily/8.0.2/archives/20220301-190149_d95c69ce/manifest.json"
+
 source .buildkite/scripts/common/util.sh
 
 .buildkite/scripts/bootstrap.sh
