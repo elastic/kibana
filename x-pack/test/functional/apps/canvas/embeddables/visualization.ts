5f61ccbf08ea8b60e75b3cb4e7e9a578472185fe
Lee Drengenberg <lee.drengenberg@elastic.co>
2022-03-02 15:27:09 -0600

    remove unused esArchiver

--- x-pack/test/functional/apps/canvas/embeddables/visualization.ts
+++ x-pack/test/functional/apps/canvas/embeddables/visualization.ts
@@ -11,7 +11,6 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const retry = getService('retry');
   const PageObjects = getPageObjects(['canvas', 'common', 'header', 'visualize']);
-  const esArchiver = getService('esArchiver');
   const kibanaServer = getService('kibanaServer');
   const testSubjects = getService('testSubjects');
   const dashboardAddPanel = getService('dashboardAddPanel');
