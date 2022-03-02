8a58de1f195368c5d14062fefe4dacccdc6109c8
emilioalvap <emilio.alvarezpineiro@elastic.co>
2022-03-02 18:25:08 +0100

    Add concurreny option (limited)

--- x-pack/plugins/uptime/.buildkite/pipelines/flaky.js
+++ x-pack/plugins/uptime/.buildkite/pipelines/flaky.js
@@ -17,8 +17,10 @@ const pipeline = {
 };
 
 // Default config
-const concurrency = 25;
-const defaultCount = 50;
+const defaultCount = 25;
+const maxCount = 500;
+const defaultConcurrency = 25;
+const maxConcurrency = 50;
 const initialJobs = 2;
 
 const UUID = process.env.UUID;
@@ -27,6 +29,7 @@ const BUILD_UUID = 'build';
 
 // Metada keys, should match the ones specified in pipeline step configuration
 const E2E_COUNT = 'e2e/count';
+const E2E_CONCURRENCY = 'e2e/concurrent';
 const E2E_GREP = 'e2e/grep';
 const E2E_ARTIFACTS_ID = 'e2e/build-id';
 
@@ -34,11 +37,11 @@ const env = getEnvFromMetadata();
 
 const totalJobs = env[E2E_COUNT] + initialJobs;
 
-if (totalJobs > 500) {
-  console.error('+++ Too many tests');
+if (totalJobs > maxCount) {
+  console.error('+++ Too many steps');
   console.error(
     `Buildkite builds can only contain 500 steps in total. Found ${totalJobs} in total. Make sure your test runs are less than ${
-      500 - initialJobs
+      maxCount - initialJobs
     }`
   );
   process.exit(1);
@@ -52,6 +55,10 @@ steps.push(getBuildJob());
 steps.push(getGroupRunnerJob(env));
 
 console.log(JSON.stringify(pipeline, null, 2));
+
+/***
+ * Utils
+ */
 
 function getBuildJob() {
   return {
@@ -72,7 +79,7 @@ function getGroupRunnerJob(env) {
     agents: { queue: 'n2-4' },
     depends_on: BUILD_UUID,
     parallelism: env[E2E_COUNT],
-    concurrency: concurrency,
+    concurrency: env[E2E_CONCURRENCY],
     concurrency_group: UUID,
     concurrency_method: 'eager',
   };
@@ -81,25 +88,30 @@ function getGroupRunnerJob(env) {
 function getEnvFromMetadata() {
   const env = {};
 
-  const value = execSync(`buildkite-agent meta-data get '${E2E_COUNT}' --default ${defaultCount}`)
+  env[E2E_COUNT] = getIntValue(E2E_COUNT, defaultCount);
+  env[E2E_CONCURRENCY] = getIntValue(E2E_CONCURRENCY, defaultConcurrency);
+  env[E2E_GREP] = getStringValue(E2E_GREP);
+  env[E2E_ARTIFACTS_ID] = getStringValue(E2E_ARTIFACTS_ID);
+
+  env[E2E_CONCURRENCY] =
+    env[E2E_CONCURRENCY] > maxConcurrency ? maxConcurrency : env[E2E_CONCURRENCY];
+
+  return env;
+}
+
+function getIntValue(key, defaultValue) {
+  let value = defaultValue;
+  const cli = execSync(`buildkite-agent meta-data get '${key}' --default ${defaultValue} `)
     .toString()
     .trim();
 
   try {
-    env[E2E_COUNT] = parseInt(value, 10);
-  } catch (e) {
-    console.error(`+++ Failed parsing run count, found: ${value}`);
+    value = parseInt(cli, 10);
+  } finally {
+    return value;
   }
+}
 
-  env[E2E_GREP] = execSync(`buildkite-agent meta-data get '${E2E_GREP}' --default ''`)
-    .toString()
-    .trim();
-
-  env[E2E_ARTIFACTS_ID] = execSync(
-    `buildkite-agent meta-data get '${E2E_ARTIFACTS_ID}' --default ''`
-  )
-    .toString()
-    .trim();
-
-  return env;
+function getStringValue(key) {
+  return execSync(`buildkite-agent meta-data get '${key}' --default ''`).toString().trim();
 }
