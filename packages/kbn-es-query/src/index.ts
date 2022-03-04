d1a61be21223cc43f9f9e2752e4f7fb709a436b9
Lukas Olson <lukas@elastic.co>
2022-03-04 10:07:57 -0700

    Remove export * from kbn/es-query package

--- packages/kbn-es-query/src/index.ts
+++ packages/kbn-es-query/src/index.ts
@@ -6,12 +6,109 @@
  * Side Public License, v 1.
  */
 
-export * from './es_query';
-export * from './filters';
-export * from './kuery';
+export type {
+  BoolQuery,
+  DataViewBase,
+  DataViewFieldBase,
+  EsQueryConfig,
+  IFieldSubType,
+  IFieldSubTypeMulti,
+  IFieldSubTypeNested,
+} from './es_query';
+
+export type {
+  CustomFilter,
+  ExistsFilter,
+  FieldFilter,
+  Filter,
+  FilterCompareOptions,
+  FilterMeta,
+  LatLon,
+  MatchAllFilter,
+  PhraseFilter,
+  PhrasesFilter,
+  Query,
+  QueryStringFilter,
+  RangeFilter,
+  RangeFilterMeta,
+  RangeFilterParams,
+  ScriptedPhraseFilter,
+  ScriptedRangeFilter,
+} from './filters';
+
+export type {
+  DslQuery,
+  FunctionTypeBuildNode,
+  KueryNode,
+  KueryParseOptions,
+  KueryQueryOptions,
+  NodeTypes,
+} from './kuery';
+
 export {
-  isDataViewFieldSubtypeMulti,
-  isDataViewFieldSubtypeNested,
+  buildEsQuery,
+  buildQueryFromFilters,
+  decorateQuery,
+  luceneStringToDsl,
+  migrateFilter,
+} from './es_query';
+
+export {
+  COMPARE_ALL_OPTIONS,
+  FILTERS,
+  FilterStateStore,
+  buildCustomFilter,
+  buildEmptyFilter,
+  buildExistsFilter,
+  buildFilter,
+  buildPhraseFilter,
+  buildPhrasesFilter,
+  buildQueryFilter,
+  buildRangeFilter,
+  cleanFilter,
+  compareFilters,
+  dedupFilters,
+  disableFilter,
+  enableFilter,
+  getConvertedValueForField,
+  getFilterField,
+  getFilterParams,
+  getPhraseFilterField,
+  getPhraseFilterValue,
+  getPhraseScript,
+  getRangeScript,
+  isExistsFilter,
+  isFilter,
+  isFilterDisabled,
+  isFilterPinned,
+  isFilters,
+  isMatchAllFilter,
+  isPhraseFilter,
+  isPhrasesFilter,
+  isQueryStringFilter,
+  isRangeFilter,
+  isScriptedPhraseFilter,
+  isScriptedRangeFilter,
+  onlyDisabledFiltersChanged,
+  pinFilter,
+  toggleFilterDisabled,
+  toggleFilterNegated,
+  toggleFilterPinned,
+  uniqFilters,
+  unpinFilter,
+} from './filters';
+
+export {
+  KQLSyntaxError,
+  fromKueryExpression,
+  nodeBuilder,
+  nodeTypes,
+  toElasticsearchQuery,
+} from './kuery';
+
+export {
   getDataViewFieldSubtypeMulti,
   getDataViewFieldSubtypeNested,
+  isDataViewFieldSubtypeMulti,
+  isDataViewFieldSubtypeNested,
 } from './utils';
