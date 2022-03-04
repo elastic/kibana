339c2e33b171036af3004d97b7169d3b6ea4d1bb
Ashokaditya <ashokaditya@elastic.co>
2022-03-04 09:49:49 +0100

    Move entries back in `ExceptionsListItemGenerator`
    review changes

--- x-pack/plugins/security_solution/scripts/endpoint/blocklists/index.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/blocklists/index.ts
@@ -9,11 +9,7 @@ import { run, RunFn, createFailError } from '@kbn/dev-utils';
 import { KbnClient } from '@kbn/test';
 import { AxiosError } from 'axios';
 import pMap from 'p-map';
-import type {
-  CreateExceptionListSchema,
-  CreateExceptionListItemSchema,
-  ExceptionListItemSchema,
-} from '@kbn/securitysolution-io-ts-list-types';
+import type { CreateExceptionListSchema } from '@kbn/securitysolution-io-ts-list-types';
 import {
   ENDPOINT_BLOCKLISTS_LIST_DESCRIPTION,
   ENDPOINT_BLOCKLISTS_LIST_ID,
@@ -81,69 +77,7 @@ const createBlocklists: RunFn = async ({ flags, log }) => {
   await pMap(
     Array.from({ length: flags.count as unknown as number }),
     () => {
-      let options: Partial<CreateExceptionListItemSchema> = {};
-      const os = eventGenerator.randomOSFamily() as ExceptionListItemSchema['os_types'][number];
-      const entriesList: CreateExceptionListItemSchema['entries'] = [
-        {
-          field: 'file.path.text',
-          operator: 'included',
-          type: 'wildcard',
-          value: os === 'windows' ? 'C:\\Fol*\\file.*' : '/usr/*/*.dmg',
-        },
-        {
-          field: 'file.path.text',
-          operator: 'included',
-          type: 'wildcard',
-          value: os === 'windows' ? 'C:\\Fol*\\file.exe' : '/usr/*/app.dmg',
-        },
-        {
-          field: 'process.executable.caseless',
-          value:
-            os === 'windows'
-              ? ['C:\\some\\path', 'C:\\some\\other\\path', 'C:\\yet\\another\\path']
-              : ['/some/path', 'some/other/path', 'yet/another/path'],
-          type: 'match_any',
-          operator: 'included',
-        },
-        {
-          field: 'process.hash.sha256',
-          value: [
-            'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3',
-            '2C26B46B68FFC68FF99B453C1D30413413422D706483BFA0F98A5E886266E7AE',
-            'FCDE2B2EDBA56BF408601FB721FE9B5C338D10EE429EA04FAE5511B68FBF8FB9',
-          ],
-          type: 'match_any',
-          operator: 'included',
-        },
-        {
-          field: 'process.Ext.code_signature',
-          entries: [
-            {
-              field: 'trusted',
-              value: 'true',
-              type: 'match',
-              operator: 'included',
-            },
-            {
-              field: 'subject_name',
-              value:
-                os === 'windows'
-                  ? ['notsus.app', 'verynotsus.app', 'superlegit.app']
-                  : ['notsus.exe', 'verynotsus.exe', 'superlegit.exe'],
-              type: 'match_any',
-              operator: 'included',
-            },
-          ],
-          type: 'nested',
-        },
-      ];
-
-      options = {
-        os_types: [os],
-        entries: [entriesList[eventGenerator.randomN(5)]],
-      };
-
-      const body = eventGenerator.generateBlocklistForCreate(options);
+      const body = eventGenerator.generateBlocklistForCreate();
 
       if (isArtifactByPolicy(body)) {
         const nmExceptions = eventGenerator.randomN(3) || 1;
