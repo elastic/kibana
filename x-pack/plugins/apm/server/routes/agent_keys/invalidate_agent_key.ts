97b85c5639083705a1982a7932a58164db396e8e
miriam.aparicio <miriam.aparicio@gmail.com>
2022-03-07 08:52:02 +0000

    [APM] Invalidate keys created by other when user has privilages

--- x-pack/plugins/apm/server/routes/agent_keys/invalidate_agent_key.ts
+++ x-pack/plugins/apm/server/routes/agent_keys/invalidate_agent_key.ts
@@ -9,17 +9,20 @@ import { ApmPluginRequestHandlerContext } from '../typings';
 export async function invalidateAgentKey({
   context,
   id,
+  isAdmin,
 }: {
   context: ApmPluginRequestHandlerContext;
   id: string;
+  isAdmin: boolean;
 }) {
+  const body: { ids: string[]; owner?: boolean } = { ids: [id] };
+  if (!isAdmin) {
+    body.owner = true;
+  }
   const { invalidated_api_keys: invalidatedAgentKeys } =
     await context.core.elasticsearch.client.asCurrentUser.security.invalidateApiKey(
       {
-        body: {
-          ids: [id],
-          owner: true,
-        },
+        body,
       }
     );
 
