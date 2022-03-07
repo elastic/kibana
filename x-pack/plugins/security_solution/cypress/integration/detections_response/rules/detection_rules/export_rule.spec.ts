5e54347020e13b742e9f281015d87f9117dd1842
Gloria Hornero <gloria.hornero@elastic.co>
2022-03-07 08:54:49 +0100

    wip

---
+++ x-pack/plugins/security_solution/cypress/integration/detections_response/rules/detection_rules/export_rule.spec.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { expectedExportedRule, getNewRule } from '../../../../objects/rule';
+import {
+  exportFirstRule,
+  getRulesImportExportToast,
+} from '../../../../tasks/alerts_detection_rules';
+import { createCustomRule } from '../../../../tasks/api_calls/rules';
+import { cleanKibana } from '../../../../tasks/common';
+import { loginAndWaitForPageWithoutDateRange } from '../../../../tasks/login';
+
+import { DETECTIONS_RULE_MANAGEMENT_URL } from '../../../../urls/navigation';
+
+// Flaky https://github.com/elastic/kibana/issues/69849
+describe.skip('Export rules', () => {
+  beforeEach(() => {
+    cleanKibana();
+    cy.intercept(
+      'POST',
+      '/api/detection_engine/rules/_export?exclude_export_details=false&file_name=rules_export.ndjson'
+    ).as('export');
+    loginAndWaitForPageWithoutDateRange(DETECTIONS_RULE_MANAGEMENT_URL);
+    createCustomRule(getNewRule()).as('ruleResponse');
+  });
+
+  it('Exports a custom rule', function () {
+    exportFirstRule();
+    cy.wait('@export').then(({ response }) => {
+      cy.wrap(response?.body).should('eql', expectedExportedRule(this.ruleResponse));
+      getRulesImportExportToast([
+        'Successfully exported 1 of 1 rule. Prebuilt rules were excluded from the resulting file.',
+      ]);
+    });
+  });
+});
