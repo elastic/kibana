fc8826b88b3a40ad5d230f96c0978c144bbe0f69
neptunian <sandra.gonzales@elastic.co>
2022-03-06 21:25:18 -0500

    comment tests back in for es, kibana, cluster, logstash

--- x-pack/test/api_integration/apis/monitoring/elasticsearch/index.js
+++ x-pack/test/api_integration/apis/monitoring/elasticsearch/index.js
@@ -8,20 +8,20 @@
 export default function ({ loadTestFile }) {
   describe('Elasticsearch', () => {
     loadTestFile(require.resolve('./overview'));
-    // loadTestFile(require.resolve('./overview_mb'));
+    loadTestFile(require.resolve('./overview_mb'));
     loadTestFile(require.resolve('./nodes'));
-    // loadTestFile(require.resolve('./nodes_mb'));
+    loadTestFile(require.resolve('./nodes_mb'));
     loadTestFile(require.resolve('./node_detail'));
-    // loadTestFile(require.resolve('./node_detail_mb'));
+    loadTestFile(require.resolve('./node_detail_mb'));
     loadTestFile(require.resolve('./node_detail_advanced'));
-    // loadTestFile(require.resolve('./node_detail_advanced_mb'));
+    loadTestFile(require.resolve('./node_detail_advanced_mb'));
     loadTestFile(require.resolve('./indices'));
-    // loadTestFile(require.resolve('./indices_mb'));
+    loadTestFile(require.resolve('./indices_mb'));
     loadTestFile(require.resolve('./index_detail'));
-    // loadTestFile(require.resolve('./index_detail_mb'));
+    loadTestFile(require.resolve('./index_detail_mb'));
     loadTestFile(require.resolve('./ccr'));
-    // loadTestFile(require.resolve('./ccr_mb'));
+    loadTestFile(require.resolve('./ccr_mb'));
     loadTestFile(require.resolve('./ccr_shard'));
-    // loadTestFile(require.resolve('./ccr_shard_mb'));
+    loadTestFile(require.resolve('./ccr_shard_mb'));
   });
 }
