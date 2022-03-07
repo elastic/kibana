8193afca67802cdf40b5368f539ebc3928036981
Davey Holler <daveyholler@hey.com>
2022-03-07 15:01:33 -0800

    Stubbing out the UI for the search indices page

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_indices/search_indices.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_indices/search_indices.tsx
@@ -9,24 +9,125 @@ import React from 'react';
 
 import { generatePath } from 'react-router-dom';
 
+import { EuiBasicTable, EuiButton, EuiButtonIcon, EuiSpacer, EuiTitle } from '@elastic/eui';
+
 import { EuiLinkTo } from '../../../shared/react_router_helpers';
 
 import { SEARCH_INDEX_OVERVIEW_PATH } from '../../routes';
 import { EnterpriseSearchContentPageTemplate } from '../layout/page_template';
 
 export const SearchIndices: React.FC = () => {
+  // TODO: Replace with a real list of indices
+  const columns = [
+    {
+      field: 'name',
+      name: 'Search index name',
+      sortable: true,
+      truncateText: true,
+      render: (indexSlug: string) => (
+        <EuiLinkTo
+          data-test-subj="search-index-link"
+          to={generatePath(SEARCH_INDEX_OVERVIEW_PATH, { indexSlug: 'foo123' })}
+        >
+          {indexSlug}
+        </EuiLinkTo>
+      ),
+    },
+    {
+      field: 'source_type',
+      name: 'Source type',
+      sortable: true,
+      truncateText: true,
+    },
+    {
+      field: 'elasticsearch_index_name',
+      name: 'Elasticsearch index name',
+      sortable: true,
+      truncateText: true,
+    },
+    {
+      field: 'search_engines',
+      name: 'Attached search engines',
+      truncateText: true,
+    },
+    {
+      field: 'docs.count',
+      name: 'Documents',
+      sortable: true,
+      truncateText: true,
+      align: 'right',
+    },
+    {
+      name: 'Actions',
+      actions: [
+        {
+          render: (indexSlug: string) => (
+            <EuiButtonIcon
+              iconType="eye"
+              data-test-subj="view-search-index-button"
+              href={generatePath(SEARCH_INDEX_OVERVIEW_PATH, { indexSlug: 'foo123' })}
+            />
+          ),
+        },
+      ],
+    },
+  ];
+
+  const indices = [
+    {
+      name: 'Our API Index',
+      indexSlug: 'index-1',
+      source_type: 'API',
+      elasticsearch_index_name: 'ent-search-api-one',
+      search_engines: 'Search Engine One, Search Engine Two',
+      docs: {
+        count: 100,
+      },
+    },
+    {
+      name: 'Customer Feedback',
+      indexSlug: 'index-2',
+      source_type: 'Elasticsearch Index',
+      elasticsearch_index_name: 'es-index-two',
+      search_engines: 'Search Engine One',
+      docs: {
+        count: 100,
+      },
+    },
+    {
+      name: 'Dharma Crawler',
+      indexSlug: 'index-3',
+      source_type: 'Crawler',
+      elasticsearch_index_name: 'ent-search-crawler-one',
+      search_engines: 'Search Engine One, Search Engine Two',
+      docs: {
+        count: 100,
+      },
+    },
+  ];
+
+  const createNewIndexButton = (
+    <EuiLinkTo
+      data-test-subj="create-new-index-button"
+      to={generatePath(SEARCH_INDEX_OVERVIEW_PATH, { indexSlug: 'new' })}
+    >
+      <EuiButton iconType="plusInCircle" color="primary" fill>
+        Create new index
+      </EuiButton>
+    </EuiLinkTo>
+  );
+
   return (
     <EnterpriseSearchContentPageTemplate
       pageChrome={[]}
       pageViewTelemetry="Search indices"
       isLoading={false}
+      pageHeader={{
+        pageTitle: 'Search indices',
+        rightSideItems: [createNewIndexButton],
+      }}
     >
-      <>Search indices</>
-      <br />
-      <br />
-      <EuiLinkTo to={generatePath(SEARCH_INDEX_OVERVIEW_PATH, { indexSlug: 'foo123' })}>
-        Sample Index
-      </EuiLinkTo>
+      <EuiBasicTable items={indices} columns={columns} />
     </EnterpriseSearchContentPageTemplate>
   );
 };
