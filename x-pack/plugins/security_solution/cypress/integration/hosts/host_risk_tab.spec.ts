e98197e8fdc29b1f4ad87dd77b09955da844cafd
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2022-03-07 14:09:49 +0100

    Please code review

--- x-pack/plugins/security_solution/cypress/integration/hosts/host_risk_tab.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/hosts/host_risk_tab.spec.ts
@@ -7,14 +7,15 @@
 
 import { cleanKibana } from '../../tasks/common';
 import { esArchiverLoad, esArchiverUnload } from '../../tasks/es_archiver';
-import { navigateToHostRiskDetailTab } from '../../tasks/host_risk';
+import {
+  navigateToHostRiskDetailTab,
+  openRiskTableFilterAndSelectTheCriticalOption,
+  removeCritialFilter,
+  selectFiveItemsPerPageOption,
+} from '../../tasks/host_risk';
 import {
   HOST_BY_RISK_TABLE_CELL,
-  HOST_BY_RISK_TABLE_FILTER,
-  HOST_BY_RISK_TABLE_FILTER_CRITICAL,
   HOST_BY_RISK_TABLE_HOSTNAME_CELL,
-  HOST_BY_RISK_TABLE_PERPAGE_BUTTON,
-  HOST_BY_RISK_TABLE_PERPAGE_OPTIONS,
   HOST_BY_RISK_TABLE_NEXT_PAGE_BUTTON,
 } from '../../screens/hosts/host_risk';
 import { loginAndWaitForPage } from '../../tasks/login';
@@ -42,18 +43,15 @@ describe('risk tab', () => {
   });
 
   it('filters the table', () => {
-    cy.get(HOST_BY_RISK_TABLE_FILTER).click();
-    cy.get(HOST_BY_RISK_TABLE_FILTER_CRITICAL).click();
+    openRiskTableFilterAndSelectTheCriticalOption();
 
     cy.get(HOST_BY_RISK_TABLE_CELL).eq(3).should('not.have.text', 'siem-kibana');
 
-    // remove filter
-    cy.get(HOST_BY_RISK_TABLE_FILTER_CRITICAL).click();
+    removeCritialFilter();
   });
 
   it('should be able to change items count per page', () => {
-    cy.get(HOST_BY_RISK_TABLE_PERPAGE_BUTTON).click();
-    cy.get(HOST_BY_RISK_TABLE_PERPAGE_OPTIONS).first().click();
+    selectFiveItemsPerPageOption();
 
     cy.get(HOST_BY_RISK_TABLE_HOSTNAME_CELL).should('have.length', 5);
   });
