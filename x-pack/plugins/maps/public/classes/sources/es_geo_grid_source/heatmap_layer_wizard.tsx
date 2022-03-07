afe90d2342ece6493b7599209235bdbd99f68abf
Maksim Kovalev <maksim_kovalev@epam.com>
2022-03-07 11:29:58 +0200

    116653 - prettier fixes

--- x-pack/plugins/maps/public/classes/sources/es_geo_grid_source/heatmap_layer_wizard.tsx
+++ x-pack/plugins/maps/public/classes/sources/es_geo_grid_source/heatmap_layer_wizard.tsx
@@ -13,7 +13,12 @@ import { ESGeoGridSource, heatmapTitle } from './es_geo_grid_source';
 import { LayerWizard, RenderWizardArguments } from '../../layers';
 import { HeatmapLayer } from '../../layers/heatmap_layer';
 import { ESGeoGridSourceDescriptor } from '../../../../common/descriptor_types';
-import { GRID_RESOLUTION, LAYER_WIZARD_CATEGORY, RENDER_AS, WIZARD_ID } from '../../../../common/constants';
+import {
+  GRID_RESOLUTION,
+  LAYER_WIZARD_CATEGORY,
+  RENDER_AS,
+  WIZARD_ID,
+} from '../../../../common/constants';
 import { HeatmapLayerIcon } from '../../layers/wizards/icons/heatmap_layer_icon';
 
 export const heatmapLayerWizardConfig: LayerWizard = {
