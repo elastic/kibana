dec84fed43cc601e958faa00b2697533dd005281
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-03-08 14:44:08 -0500

    Add cypress test

--- x-pack/plugins/fleet/cypress/integration/fleet_settings.spec.ts
+++ x-pack/plugins/fleet/cypress/integration/fleet_settings.spec.ts
@@ -78,4 +78,42 @@ describe('Edit settings', () => {
       expect(interception.request.body.name).to.equal('output-1');
     });
   });
+
+  it('should allow to create a logstash output', () => {
+    cy.getBySel('addOutputBtn').click();
+    cy.get('[placeholder="Specify name"]').clear().type('output-logstash-1');
+    cy.get('[placeholder="Specify type"]').select('logstash');
+    cy.get('[placeholder="Specify host"').clear().type('logstash:5044');
+    cy.get('[placeholder="Specify ssl certificate"]').clear().type('SSL CERTIFICATE');
+    cy.get('[placeholder="Specify certificate key"]').clear().type('SSL KEY');
+
+    cy.intercept('/api/fleet/outputs', {
+      items: [
+        {
+          id: 'fleet-default-output',
+          name: 'output-1',
+          type: 'elasticsearch',
+          is_default: true,
+          is_default_monitoring: true,
+        },
+      ],
+    });
+    cy.intercept('POST', '/api/fleet/outputs', {
+      name: 'output-logstash-1',
+      type: 'logstash',
+      hosts: ['logstash:5044'],
+      is_default: false,
+      is_default_monitoring: false,
+      ssl: {
+        certificate: "SSL CERTIFICATE');",
+        key: 'SSL KEY',
+      },
+    }).as('postLogstashOutput');
+
+    cy.getBySel('saveApplySettingsBtn').click();
+
+    cy.wait('@postLogstashOutput').then((interception) => {
+      expect(interception.request.body.name).to.equal('output-logstash-1');
+    });
+  });
 });
