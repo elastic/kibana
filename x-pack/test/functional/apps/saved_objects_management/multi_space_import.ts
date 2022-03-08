5e134f8e313200078805fa108dd5bc1b16bb9bcc
bhavyarm <bhavya@elastic.co>
2022-03-08 15:43:14 -0500

    tests for making sure saved objects handle conflict well in multi-space import/copy

---
+++ x-pack/test/functional/apps/saved_objects_management/multi_space_import.ts
@@ -0,0 +1,144 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+/*
+ * This test is making sure that saved object id collisions across multiple spaces are handled correctly
+ * https://github.com/elastic/kibana/issues/126940
+ */
+
+import expect from '@kbn/expect';
+import path from 'path';
+import { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function ({ getService, getPageObjects }: FtrProviderContext) {
+  const kibanaServer = getService('kibanaServer');
+  const esArchiver = getService('esArchiver');
+  const PageObjects = getPageObjects([
+    'common',
+    'security',
+    'settings',
+    'header',
+    'spaceSelector',
+    'savedObjects',
+    'dashboard',
+    'timePicker',
+  ]);
+  const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+  const toasts = getService('toasts');
+  const spacesService = getService('spaces');
+
+  const getExportCount = async () => {
+    return await retry.tryForTime(10000, async () => {
+      const exportText = await testSubjects.getVisibleText('exportAllObjects');
+      const parts = exportText.trim().split(' ');
+      if (parts.length !== 3) {
+        throw new Error('text not loaded yet');
+      }
+      const count = Number.parseInt(parts[1], 10);
+      if (count === 0) {
+        throw new Error('text not loaded yet');
+      }
+      return count;
+    });
+  };
+
+  const setTimeRange = async () => {
+    const fromTime = 'Apr 27, 2015 @ 00:00:00.000';
+    const toTime = 'June 25, 2015 @ 00:00:00.000';
+    await PageObjects.timePicker.setDefaultAbsoluteRangeViaUiSettings(fromTime, toTime);
+  };
+
+  describe('should be able to handle multi-space imports correctly', function () {
+    before(async function () {
+      await spacesService.create({
+        id: 'another_space',
+        name: 'another_space',
+        disabledFeatures: [],
+      });
+      await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/logstash_functional');
+      await setTimeRange();
+    });
+
+    after(async () => {
+      await esArchiver.unload('x-pack/test/functional/es_archives/logstash_functional');
+      await spacesService.delete('another_space');
+    });
+
+    it('should be able to import saved objects into default space', async function () {
+      await PageObjects.settings.navigateTo();
+      await PageObjects.settings.clickKibanaSavedObjects();
+
+      const initialObjectCount = await getExportCount();
+
+      await PageObjects.savedObjects.importFile(
+        path.join(__dirname, 'exports', '_8.0.0_multispace_import.ndjson')
+      );
+      await PageObjects.savedObjects.checkImportSucceeded();
+      await PageObjects.savedObjects.clickImportDone();
+      await PageObjects.savedObjects.waitTableIsLoaded();
+
+      const newObjectCount = await getExportCount();
+      expect(newObjectCount - initialObjectCount).to.eql(6);
+    });
+
+    it('should render all panels on the dashboard', async () => {
+      await PageObjects.common.navigateToApp('dashboard');
+      await PageObjects.dashboard.loadSavedDashboard('multi_space_import_8.0.0');
+      // dashboard should load properly
+      await PageObjects.dashboard.expectOnDashboard('multi_space_import_8.0.0');
+      await PageObjects.dashboard.waitForRenderComplete();
+      // There should be 0 error embeddables on the dashboard
+      const errorEmbeddables = await testSubjects.findAll('embeddableStackError');
+      expect(errorEmbeddables.length).to.be(0);
+    });
+
+    it('should be able to import saved objects into another space', async function () {
+      const getSpacePrefix = (spaceId: string) => {
+        return spaceId && spaceId !== 'default' ? `/s/${spaceId}` : ``;
+      };
+      const spaceId = 'another_space';
+      await PageObjects.common.navigateToUrl('settings', 'kibana/objects', {
+        basePath: getSpacePrefix(spaceId),
+        shouldUseHashForSubUrl: false,
+      });
+      await PageObjects.savedObjects.waitTableIsLoaded();
+
+      const initialObjectCount = await getExportCount();
+
+      await PageObjects.savedObjects.importFile(
+        path.join(__dirname, 'exports', '_8.0.0_multispace_import.ndjson')
+      );
+      await PageObjects.savedObjects.checkImportSucceeded();
+      await PageObjects.savedObjects.clickImportDone();
+      await PageObjects.savedObjects.waitTableIsLoaded();
+
+      const newObjectCount = await getExportCount();
+      expect(newObjectCount - initialObjectCount).to.eql(6);
+    });
+
+    it('should render all panels on the dashboard in another space', async () => {
+      const getSpacePrefix = (spaceId: string) => {
+        return spaceId && spaceId !== 'default' ? `/s/${spaceId}` : ``;
+      };
+      const spaceId = 'another_space';
+      await PageObjects.common.navigateToUrl('dashboard', undefined, {
+        basePath: getSpacePrefix(spaceId),
+        shouldUseHashForSubUrl: false,
+      });
+      await PageObjects.dashboard.loadSavedDashboard('multi_space_import_8.0.0');
+
+      // dashboard should load properly
+      await PageObjects.dashboard.expectOnDashboard('multi_space_import_8.0.0');
+      await PageObjects.dashboard.waitForRenderComplete();
+
+      // There should be 0 error embeddables on the dashboard
+      const errorEmbeddables = await testSubjects.findAll('embeddableStackError');
+      expect(errorEmbeddables.length).to.be(0);
+    });
+  });
+}
