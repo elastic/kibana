45b2f53ec54b72c361f3013c0c2415b825260b1f
Jonathan Budzenski <jon@elastic.co>
2022-03-09 09:52:18 -0600

    [package testing] Update networking profile to account for virtualbox update
    With a recent virtualbox update, we're getting failures in CI with:
    
    > The IP address configured for the host-only network is not within the
    > allowed ranges. Please update the address used to be within the allowed
    > ranges and run the command again.
    
    This updates the IP range used to be compatible.

--- test/scripts/jenkins_xpack_package_deb.sh
+++ test/scripts/jenkins_xpack_package_deb.sh
@@ -11,7 +11,7 @@ export VAGRANT_CWD=test/package
 vagrant up deb --no-provision
 
 node scripts/es snapshot \
-  -E network.bind_host=127.0.0.1,192.168.50.1 \
+  -E network.bind_host=127.0.0.1,192.168.56.1 \
   -E discovery.type=single-node \
   --license=trial &
 while ! timeout 1 bash -c "echo > /dev/tcp/localhost/9200"; do sleep 30; done
@@ -19,8 +19,8 @@ while ! timeout 1 bash -c "echo > /dev/tcp/localhost/9200"; do sleep 30; done
 vagrant provision deb
 
 export TEST_BROWSER_HEADLESS=1
-export TEST_KIBANA_URL=http://elastic:changeme@192.168.50.5:5601
-export TEST_ES_URL=http://elastic:changeme@192.168.50.1:9200
+export TEST_KIBANA_URL=http://elastic:changeme@192.168.56.5:5601
+export TEST_ES_URL=http://elastic:changeme@192.168.56.1:9200
 
 cd x-pack
 node scripts/functional_test_runner.js --include-tag=smoke
