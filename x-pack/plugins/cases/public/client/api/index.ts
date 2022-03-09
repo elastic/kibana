82d30f5b12d4d53597e409ab16a0e37eb7a8b7b0
Christos Nasikas <christos.nasikas@elastic.co>
2022-03-09 11:36:18 +0200

    Add tests

--- x-pack/plugins/cases/public/client/api/index.ts
+++ x-pack/plugins/cases/public/client/api/index.ts
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import { HttpStart } from 'kibana/public';
 import { CasesByAlertId, getCasesFromAlertsUrl } from '../../../common/api';
 
