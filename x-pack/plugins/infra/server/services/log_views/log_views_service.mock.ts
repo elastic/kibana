a468e81b8b2d6780b337f648034ff10fa8e34a7f
Felix Stürmer <felix.stuermer@elastic.co>
2022-03-09 10:23:30 +0000

    Add new getClient method to log views service mock

--- x-pack/plugins/infra/server/services/log_views/log_views_service.mock.ts
+++ x-pack/plugins/infra/server/services/log_views/log_views_service.mock.ts
@@ -9,5 +9,8 @@ import { createLogViewsClientMock } from './log_views_client.mock';
 import { LogViewsServiceStart } from './types';
 
 export const createLogViewsServiceStartMock = (): jest.Mocked<LogViewsServiceStart> => ({
+  getClient: jest.fn((_savedObjectsClient: any, _elasticsearchClient: any) =>
+    createLogViewsClientMock()
+  ),
   getScopedClient: jest.fn((_request: any) => createLogViewsClientMock()),
 });
