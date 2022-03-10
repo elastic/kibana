d89efe65c8e3e4968e9af695baf1fa70468730a6
Yulia Cech <yulia.cech@elastic.co>
2022-03-10 18:54:25 +0100

    Removed import comment

--- packages/kbn-test-jest-helpers/src/axe_helpers.ts
+++ packages/kbn-test-jest-helpers/src/axe_helpers.ts
@@ -6,7 +6,6 @@
  * Side Public License, v 1.
  */
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { configureAxe } from 'jest-axe';
 import { Result } from 'axe-core';
 import { AXE_OPTIONS, AXE_CONFIG } from '@kbn/test';
