0ce00146527564c651b315a616e101990080bd86
Brian Seeders <brian.seeders@elastic.co>
2022-03-10 15:09:36 -0500

    Fix type_check and try it without ts refs

--- .buildkite/scripts/steps/checks/check_types.sh
+++ .buildkite/scripts/steps/checks/check_types.sh
@@ -5,12 +5,13 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 # TODO remove ts refs if possible
-echo --- Build TS Refs
-checks-reporter-with-killswitch "Build TS Refs" \
-  node scripts/build_ts_refs \
-    --clean \
-    --no-cache \
-    --force
+# echo --- Build TS Refs
+# checks-reporter-with-killswitch "Build TS Refs" \
+#   node scripts/build_ts_refs \
+#     --clean \
+#     --no-cache \
+#     --force
 
 echo --- Check Types
-cat target/check_types.log
+checks-reporter-with-killswitch "Check Types" \
+  node scripts/type_check
