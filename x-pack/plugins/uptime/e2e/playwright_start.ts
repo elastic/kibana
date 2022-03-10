8321bad4423a90a14731c8c62b00833b99434749
shahzad31 <shahzad.muhammad@elastic.co>
2022-03-10 20:57:36 +0100

    fix test

--- x-pack/plugins/uptime/e2e/playwright_start.ts
+++ x-pack/plugins/uptime/e2e/playwright_start.ts
@@ -55,7 +55,6 @@ async function playwrightStart(getService: any, headless = true, match?: string)
     params: { kibanaUrl, getService },
     playwrightOptions: { headless, chromiumSandbox: false, timeout: 60 * 1000 },
     match: match === 'undefined' ? '' : match,
-    pauseOnError: true,
   });
 
   console.log('Removing esArchiver...');
