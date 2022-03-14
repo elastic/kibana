b05d8aa4e67c05edeedcf3eb1fde5fcee4f1653a
Gerard Soldevila <gerard.soldevila@softonic.com>
2022-03-14 13:40:29 +0100

    WIP

--- test/functional/apps/dashboard/bwc_import.ts
+++ test/functional/apps/dashboard/bwc_import.ts
@@ -14,7 +14,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const dashboardExpect = getService('dashboardExpect');
 
   // https://github.com/elastic/kibana/issues/114053
-  describe.skip('bwc import', function describeIndexTests() {
+  describe('bwc import', function describeIndexTests() {
     before(async function () {
       await PageObjects.dashboard.initTests();
       await PageObjects.settings.navigateTo();
