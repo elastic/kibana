2b77de2670168cddb5cee51be7a226568be76beb
gbamparop <georgios.bamparopoulos@elastic.co>
2022-03-14 16:09:27 +0000

    Add missing url param

--- x-pack/plugins/apm/public/components/app/service_overview/service_overview_dependencies_table/index.tsx
+++ x-pack/plugins/apm/public/components/app/service_overview/service_overview_dependencies_table/index.tsx
@@ -38,6 +38,7 @@ export function ServiceOverviewDependenciesTable({
       kuery,
       rangeFrom,
       rangeTo,
+      serviceGroup,
       comparisonEnabled,
       comparisonType,
       latencyAggregationType,
