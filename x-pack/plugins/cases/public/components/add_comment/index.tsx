e46af5d070e6e7c179d5d27dfd88cb8a9471e6ce
Esteban Beltran <esteban.beltran@elastic.co>
2022-03-14 16:17:38 +0100

    do not show the lens plugins if not capable of visualizing things

--- x-pack/plugins/cases/public/components/add_comment/index.tsx
+++ x-pack/plugins/cases/public/components/add_comment/index.tsx
@@ -27,6 +27,7 @@ import * as i18n from './translations';
 import { schema, AddCommentFormSchema } from './schema';
 import { InsertTimeline } from '../insert_timeline';
 import { useCasesContext } from '../cases_context/use_cases_context';
+import { useKibanaCapabilities } from '../../common/lib/kibana';
 
 const MySpinner = styled(EuiLoadingSpinner)`
   position: absolute;
@@ -78,6 +79,9 @@ export const AddComment = React.memo(
         options: { stripEmptyFields: false },
         schema,
       });
+
+      const appCapabilities = useKibanaCapabilities();
+      const disabledUiPlugins: string[] = [...(!appCapabilities.visualize ? ['lens'] : [])];
 
       const fieldName = 'comment';
       const { setFieldValue, reset, submit } = form;
@@ -164,6 +168,7 @@ export const AddComment = React.memo(
                 componentProps={{
                   ref: editorRef,
                   id,
+                  disabledUiPlugins,
                   idAria: 'caseComment',
                   isDisabled: isLoading,
                   dataTestSubj: 'add-comment',
