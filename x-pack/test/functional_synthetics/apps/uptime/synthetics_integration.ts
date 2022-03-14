6f6117a001e202b410298ae81f0e6861f8524022
Dominique Clarke <dominique.clarke@elastic.co>
2022-03-14 16:39:21 -0400

    Update synthetics_integration.ts

--- x-pack/test/functional_synthetics/apps/uptime/synthetics_integration.ts
+++ x-pack/test/functional_synthetics/apps/uptime/synthetics_integration.ts
@@ -131,7 +131,7 @@ export default function (providerContext: FtrProviderContext) {
     type: `synthetics/${monitorType}`,
     use_output: 'default',
   });
-  // FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/125881
+
   describe('When on the Synthetics Integration Policy Create Page', function () {
     skipIfNoDockerRegistry(providerContext);
     const basicConfig = {
