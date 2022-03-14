84561a5f4cb3ac13e09c13c5648e6b9ab9ac5068
rshen91 <rshen@elastic.co>
2022-03-14 13:11:39 -0600

    update tests

--- src/plugins/shared_ux/public/components/toolbar/buttons/add_from_library/add_from_library.test.tsx
+++ src/plugins/shared_ux/public/components/toolbar/buttons/add_from_library/add_from_library.test.tsx
@@ -5,3 +5,33 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
+
+import { mount as enzymeMount, ReactWrapper } from 'enzyme';
+import React from 'react';
+import { ServicesProvider, SharedUXServices } from '../../../../services';
+import { servicesFactory } from '../../../../services/mocks';
+
+import { SolutionToolbarButton } from '../primary/primary';
+
+describe('<SolutionToolbarButton />', () => {
+  let services: SharedUXServices;
+  let mount: (element: JSX.Element) => ReactWrapper;
+
+  beforeEach(() => {
+    services = servicesFactory();
+    mount = (element: JSX.Element) =>
+      enzymeMount(<ServicesProvider {...services}>{element}</ServicesProvider>);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('is rendered', () => {
+    const component = mount(
+      <SolutionToolbarButton iconType="folderOpen" label="Add from library" />
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
