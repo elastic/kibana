be624b19937789c625bbea62556d4a7239e52c51
rshen91 <rshen@elastic.co>
2022-03-14 12:54:22 -0600

    fix file name import

--- src/plugins/shared_ux/public/components/toolbar/buttons/icon_button_group/icon_button_group.stories.tsx
+++ src/plugins/shared_ux/public/components/toolbar/buttons/icon_button_group/icon_button_group.stories.tsx
@@ -10,7 +10,7 @@ import { action } from '@storybook/addon-actions';
 import { Story } from '@storybook/react';
 import React from 'react';
 import { IconButtonGroup } from './icon_button_group';
-import mdx from './quick_group.mdx';
+import mdx from './icon_button_group.mdx';
 
 export default {
   title: 'Toolbar/Toolbar Icon Button Group',
