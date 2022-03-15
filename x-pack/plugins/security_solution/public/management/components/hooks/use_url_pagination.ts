b5f4ac82d7b72e2665bed3c501171d76657030c9
David Sanchez Soler <david.sanchezsoler@elastic.co>
2022-03-15 11:33:34 +0100

    Fixes error with url params when opening flyout

--- x-pack/plugins/security_solution/public/management/components/hooks/use_url_pagination.ts
+++ x-pack/plugins/security_solution/public/management/components/hooks/use_url_pagination.ts
@@ -25,7 +25,7 @@ interface UrlPagination {
 
 type UrlPaginationParams = Partial<Pagination>;
 
-const paginationFromUrlParams = (urlParams: UrlPaginationParams): Pagination => {
+export const paginationFromUrlParams = (urlParams: UrlPaginationParams): Pagination => {
   const pagination: Pagination = {
     pageSize: MANAGEMENT_DEFAULT_PAGE_SIZE,
     page: 1,
