b0d8ff37ed81eaed9f57a37310db2bbea83524cc
bhavyarm <bhavya@elastic.co>
2022-03-15 10:32:45 -0400

    a11y tests for search sessions

--- x-pack/test/accessibility/apps/search_sessions.ts
+++ x-pack/test/accessibility/apps/search_sessions.ts
@@ -6,36 +6,54 @@
  */
 
 import { FtrProviderContext } from '../ftr_provider_context';
+import uuid from 'uuid/v4';
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
-  const PageObjects = getPageObjects(['licenseManagement', 'common']);
+  const PageObjects = getPageObjects(['common',
+  'header',
+  'dashboard',
+  'visChart',
+  'searchSessionsManagement',]);
   const a11y = getService('a11y');
+  const browser = getService('browser');
+  const esArchiver = getService('esArchiver');
   const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+  //const toasts = getService('toasts');
+  const searchSessions = getService('searchSessions');
+  const kibanaServer = getService('kibanaServer');
+const log = getService('log');
 
-  describe('Upgrade Assistant a11y tests', () => {
+  describe('Search sessions a11y tests', () => {
     before(async () => {
-      await PageObjects.common.navigateToApp('licenseManagement');
+      await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/logstash_functional');
+      await esArchiver.load('x-pack/test/functional/es_archives/dashboard/async_search');
+      await kibanaServer.uiSettings.replace({ defaultIndex: 'logstash-*' });
+      await kibanaServer.uiSettings.replace({ 'search:timeout': 10000 });
+      await PageObjects.common.navigateToApp('dashboard');
+      log.debug('wait for dashboard landing page');
+      await retry.tryForTime(10000, async () => {
+        testSubjects.existOrFail('dashboardLandingPage');
+      });
+      await searchSessions.markTourDone();
+      log.debug('loading the "Not Delayed" dashboard');
+      await PageObjects.dashboard.loadSavedDashboard('Not Delayed');
+      await PageObjects.dashboard.waitForRenderComplete();
+      await searchSessions.expectState('completed');
+      const searchSessionName = `Session - ${uuid()}`;
+      await searchSessions.save({ searchSessionName });
+      await searchSessions.expectState('backgroundCompleted');
+      await PageObjects.searchSessionsManagement.goTo();
     });
 
-    it('License management page overview meets a11y requirements', async () => {
+    // after(async () => {
+    //   await searchSessions.deleteAllSearchSessions();
+    // });
+
+    it('Search sessions page meets a11y requirements', async () => {
       await a11y.testAppSnapshot();
     });
 
-    it('Update license panel meets a11y requirements', async () => {
-      await testSubjects.click('updateLicenseButton');
-      await a11y.testAppSnapshot();
-    });
 
-    it('Upload license error panel meets a11y requirements', async () => {
-      await testSubjects.click('uploadLicenseButton');
-      await a11y.testAppSnapshot();
-    });
-
-    it('Revert to basic license confirmation panel meets a11y requirements', async () => {
-      await testSubjects.click('cancelUploadButton');
-      await testSubjects.click('revertToBasicButton');
-      await a11y.testAppSnapshot();
-      await testSubjects.click('confirmModalCancelButton');
-    });
   });
 }
