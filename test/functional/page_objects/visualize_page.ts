3d354e53416955fa6a2b288fe7188186e72c8b0b
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2022-03-16 09:11:20 +0200

    Cleanup

--- test/functional/page_objects/visualize_page.ts
+++ test/functional/page_objects/visualize_page.ts
@@ -39,7 +39,6 @@ export class VisualizePageObject extends FtrService {
   private readonly elasticChart = this.ctx.getService('elasticChart');
   private readonly common = this.ctx.getPageObject('common');
   private readonly header = this.ctx.getPageObject('header');
-  private readonly unifiedSearch = this.ctx.getPageObject('unifiedSearch');
   private readonly visEditor = this.ctx.getPageObject('visEditor');
   private readonly visChart = this.ctx.getPageObject('visChart');
 
@@ -152,10 +151,6 @@ export class VisualizePageObject extends FtrService {
   public async clickVisType(type: string) {
     await this.testSubjects.click(`visType-${type}`);
     await this.header.waitUntilLoadingHasFinished();
-
-    if (type === 'lens') {
-      this.unifiedSearch.closeTour();
-    }
   }
 
   public async clickAreaChart() {
