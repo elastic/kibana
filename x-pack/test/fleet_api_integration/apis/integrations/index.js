f55e8297159a83df4239d1b986bd8e07d1e5d77a
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-03-16 17:43:16 -0400

    [Fleet] Add a link from agent details page to agent dashboard (#127882)

--- x-pack/test/fleet_api_integration/apis/integrations/index.js
+++ x-pack/test/fleet_api_integration/apis/integrations/index.js
@@ -5,7 +5,8 @@
  * 2.0.
  */
 
-export { AgentLogs } from './agent_logs';
-export { AgentDetailsActionMenu } from './actions_menu';
-export { AgentDetailsContent } from './agent_details';
-export { AgentDashboardLink } from './agent_dashboard_link';
+export default function loadTests({ loadTestFile }) {
+  describe('Integrations', () => {
+    loadTestFile(require.resolve('./elastic_agent'));
+  });
+}
