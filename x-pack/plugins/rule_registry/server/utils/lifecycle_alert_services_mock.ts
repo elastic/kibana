9926792e0168f50ddc3afe975ad887e6237eb5f2
Faisal Kanout <faisal.kanout@elastic.co>
2022-03-16 12:46:45 +0100

    Update tests

--- x-pack/plugins/rule_registry/server/utils/lifecycle_alert_services_mock.ts
+++ x-pack/plugins/rule_registry/server/utils/lifecycle_alert_services_mock.ts
@@ -35,5 +35,5 @@ export const createLifecycleAlertServicesMock = <
   alertServices: AlertServices<InstanceState, InstanceContext>
 ): LifecycleAlertServices<InstanceState, InstanceContext, ActionGroupIds> => ({
   alertWithLifecycle: ({ id }) => alertServices.alertFactory.create(id),
-  getAlertStartedDate: (id) => '2022-03-15T13:44:24.205Z',
+  getAlertStartedDate: jest.fn((id: string) => null),
 });
