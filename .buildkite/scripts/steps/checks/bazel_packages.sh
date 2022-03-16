a58e5ac7c9c14294e2f70d672822c63abc2f26f6
Brian Seeders <brian.seeders@elastic.co>
2022-03-16 11:41:53 -0400

    Auto-commit bazel packages

--- .buildkite/scripts/steps/checks/bazel_packages.sh
+++ .buildkite/scripts/steps/checks/bazel_packages.sh
@@ -7,4 +7,4 @@ source .buildkite/scripts/common/util.sh
 echo --- Check Bazel Packages Manifest
 node scripts/generate packages_build_manifest
 
-check_for_changed_files 'node scripts/generate packages_build_manifest'
+check_for_changed_files 'node scripts/generate packages_build_manifest' true
