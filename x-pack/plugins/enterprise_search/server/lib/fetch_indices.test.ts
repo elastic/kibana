fba130e21dad1b431fc99fd5353f3cf38a5645a7
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2022-03-17 14:25:08 +0100

    Add tests

---
+++ x-pack/plugins/enterprise_search/server/lib/fetch_indices.test.ts
@@ -0,0 +1,145 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { IScopedClusterClient } from 'kibana/server';
+
+import { ByteSizeValue } from '@kbn/config-schema';
+
+import { fetchIndices } from './fetch_indices';
+
+describe('fetchIndices lib function', () => {
+  const mockClient = {
+    asCurrentUser: {
+      indices: {
+        get: jest.fn(),
+        stats: jest.fn(),
+      },
+    },
+    asInternalUser: {},
+  };
+
+  const regularIndexResponse = {
+    'search-regular-index': {
+      aliases: {},
+    },
+  };
+
+  const regularIndexStatsResponse = {
+    indices: {
+      'search-regular-index': {
+        health: 'green',
+        status: 'open',
+        uuid: '83a81e7e-5955-4255-b008-5d6961203f57',
+        total: {
+          docs: {
+            count: 100,
+            deleted: 0,
+          },
+          store: {
+            size_in_bytes: 108000,
+          },
+        },
+        documents_deleted: 0,
+        size: new ByteSizeValue(108000).toString(),
+      },
+    },
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('regular index', async () => {
+    mockClient.asCurrentUser.indices.get.mockImplementation(() => regularIndexResponse);
+    mockClient.asCurrentUser.indices.stats.mockImplementation(() => regularIndexStatsResponse);
+
+    await expect(fetchIndices(mockClient as unknown as IScopedClusterClient)).resolves.toEqual([
+      {
+        health: 'green',
+        status: 'open',
+        uuid: '83a81e7e-5955-4255-b008-5d6961203f57',
+        documents: 100,
+        documents_deleted: 0,
+        size: '105.47kb',
+        aliases: 'none',
+        name: 'search-regular-index',
+      },
+    ]);
+    expect(mockClient.asCurrentUser.indices.get).toHaveBeenCalledWith({
+      expand_wildcards: ['open'],
+      features: ['aliases', 'settings'],
+      filter_path: ['*.aliases'],
+      index: 'search-*',
+    });
+
+    expect(mockClient.asCurrentUser.indices.stats).toHaveBeenCalledWith({
+      metric: ['docs', 'store'],
+      expand_wildcards: ['open'],
+      index: 'search-*',
+    });
+  });
+  test('index with aliases', async () => {
+    const aliasedIndexResponse = {
+      'index-without-prefix': {
+        ...regularIndexResponse['search-regular-index'],
+        aliases: {
+          'search-aliased': {},
+          'search-double-aliased': {},
+        },
+      },
+    };
+    const aliasedStatsResponse = {
+      indices: {
+        'index-without-prefix': { ...regularIndexStatsResponse.indices['search-regular-index'] },
+      },
+    };
+
+    mockClient.asCurrentUser.indices.get.mockImplementationOnce(() => aliasedIndexResponse);
+    mockClient.asCurrentUser.indices.stats.mockImplementationOnce(() => aliasedStatsResponse);
+    await expect(fetchIndices(mockClient as unknown as IScopedClusterClient)).resolves.toEqual([
+      {
+        health: 'green',
+        status: 'open',
+        uuid: '83a81e7e-5955-4255-b008-5d6961203f57',
+        documents: 100,
+        documents_deleted: 0,
+        size: '105.47kb',
+        aliases: ['search-aliased', 'search-double-aliased'],
+        name: 'index-without-prefix',
+      },
+    ]);
+  });
+  test('index missing in stats call', async () => {
+    const missingStatsResponse = {
+      indices: {
+        some_other_index: { ...regularIndexStatsResponse.indices['search-regular-index'] },
+      },
+    };
+
+    mockClient.asCurrentUser.indices.get.mockImplementationOnce(() => regularIndexResponse);
+    mockClient.asCurrentUser.indices.stats.mockImplementationOnce(() => missingStatsResponse);
+    // simulates when an index has been deleted after get indices call
+    // deleted index won't be present in the indices stats call response
+    await expect(fetchIndices(mockClient as unknown as IScopedClusterClient)).resolves.toEqual([
+      {
+        documents_deleted: 0,
+        aliases: 'none',
+        name: 'search-regular-index',
+        uuid: undefined,
+        health: undefined,
+        status: undefined,
+        documents: 0,
+        size: '0b',
+      },
+    ]);
+  });
+  test('no index found', async () => {
+    mockClient.asCurrentUser.indices.get.mockImplementationOnce(() => ({}));
+    await expect(fetchIndices(mockClient as unknown as IScopedClusterClient)).resolves.toEqual([]);
+    expect(mockClient.asCurrentUser.indices.stats).not.toHaveBeenCalled();
+  });
+});
