73242184ce8137922631c3088ef0f3d47b04c495
Josh Dover <doverfake@elastic.co>
2022-03-17 13:10:55 +0100

    Add Fleet authz checks to CSP endpoints

--- x-pack/plugins/cloud_security_posture/server/routes/findings/findings.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/findings/findings.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { IRouter, Logger } from 'src/core/server';
+import type { Logger } from 'src/core/server';
 import { SearchRequest, QueryDslQueryContainer } from '@elastic/elasticsearch/lib/api/types';
 import { fromKueryExpression, toElasticsearchQuery } from '@kbn/es-query';
 import { QueryDslBoolQuery } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
@@ -16,6 +16,7 @@ import { getLatestCycleIds } from './get_latest_cycle_ids';
 
 import { CSP_KUBEBEAT_INDEX_PATTERN, FINDINGS_ROUTE_PATH } from '../../../common/constants';
 import { CspAppContext } from '../../plugin';
+import { CspRouter } from '../../types';
 
 type FindingsQuerySchema = TypeOf<typeof findingsInputSchema>;
 
@@ -103,7 +104,7 @@ const buildOptionsRequest = (queryParams: FindingsQuerySchema): FindingsOptions 
   ...getSearchFields(queryParams.fields),
 });
 
-export const defineFindingsIndexRoute = (router: IRouter, cspContext: CspAppContext): void =>
+export const defineFindingsIndexRoute = (router: CspRouter, cspContext: CspAppContext): void =>
   router.get(
     {
       path: FINDINGS_ROUTE_PATH,
