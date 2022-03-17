932a078a4696208fda3fa6d721e2c51c9ace5c19
Ying Mao <ying.mao@elastic.co>
2022-03-17 14:09:05 -0400

    Fixing checks

--- x-pack/plugins/alerting/server/saved_objects/is_rule_exportable.test.ts
+++ x-pack/plugins/alerting/server/saved_objects/is_rule_exportable.test.ts
@@ -13,6 +13,7 @@ import { ILicenseState } from '../lib/license_state';
 import { licenseStateMock } from '../lib/license_state.mock';
 import { licensingMock } from '../../../licensing/server/mocks';
 import { isRuleExportable } from './is_rule_exportable';
+import { loggingSystemMock } from 'src/core/server/mocks';
 
 let ruleTypeRegistryParams: ConstructorOptions;
 let logger: MockedLogger;
@@ -24,6 +25,7 @@ beforeEach(() => {
   mockedLicenseState = licenseStateMock.create();
   logger = loggerMock.create();
   ruleTypeRegistryParams = {
+    logger: loggingSystemMock.create().get(),
     taskManager,
     taskRunnerFactory: new TaskRunnerFactory(),
     licenseState: mockedLicenseState,
