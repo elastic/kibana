76e5d9de7acf8e78cd91ad9dc05caf5b61665638
Kate Patticha <aikaterini.patticha@elastic.co>
2022-03-17 11:32:16 +0100

    move getPreferredServiceAnomalyTimeseries

--- x-pack/plugins/apm/public/hooks/use_preferred_service_anomaly_timeseries.ts
+++ x-pack/plugins/apm/public/hooks/use_preferred_service_anomaly_timeseries.ts
@@ -6,10 +6,33 @@
  */
 
 import { ApmMlDetectorType } from '../../common/anomaly_detection/apm_ml_detectors';
-import { getPreferredServiceAnomalyTimeseries } from '../../common/anomaly_detection/get_preferred_service_anomaly_timeseries';
+import { Environment } from '../../common/environment_rt';
+import { ServiceAnomalyTimeseries } from '../../common/anomaly_detection/service_anomaly_timeseries';
 import { useApmServiceContext } from '../context/apm_service/use_apm_service_context';
 import { useEnvironmentsContext } from '../context/environments_context/use_environments_context';
 import { useServiceAnomalyTimeseriesContext } from '../context/service_anomaly_timeseries/use_service_anomaly_timeseries_context';
+
+export function getPreferredServiceAnomalyTimeseries({
+  environment,
+  detectorType,
+  allAnomalyTimeseries,
+  fallbackToTransactions,
+}: {
+  environment: Environment;
+  detectorType: ApmMlDetectorType;
+  allAnomalyTimeseries: ServiceAnomalyTimeseries[];
+  fallbackToTransactions: boolean;
+}) {
+  const seriesForType = allAnomalyTimeseries.filter(
+    (serie) => serie.type === detectorType
+  );
+
+  return seriesForType.find(
+    (serie) =>
+      serie.environment === environment &&
+      (fallbackToTransactions ? serie.version <= 2 : serie.version >= 3)
+  );
+}
 
 export function usePreferredServiceAnomalyTimeseries(
   detectorType: ApmMlDetectorType
