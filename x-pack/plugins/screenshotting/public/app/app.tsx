39116c6eea8028ccc194dbfc324bb099774a2c09
Michael Dokolin <mikhail.dokolin@elastic.co>
2022-03-17 16:23:45 +0100

    Add screenshotting app for expressions rendering

---
+++ x-pack/plugins/screenshotting/public/app/app.tsx
@@ -0,0 +1,62 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import './app.scss';
+import React, { useContext, useMemo, useRef } from 'react';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+} from '@elastic/eui';
+import type { ExpressionRendererParams } from 'src/plugins/expressions/public';
+import { useExpressionRenderer } from '../../../../../src/plugins/expressions/public';
+import { ScreenshotModeContext } from './screenshot_mode_context';
+
+export function App() {
+  const elementRef = useRef(null);
+  const screenshotMode = useContext(ScreenshotModeContext);
+  const expression = useMemo(
+    () =>
+      screenshotMode?.getScreenshotContext<ExpressionRendererParams['expression']>('expression') ??
+      '',
+    [screenshotMode]
+  );
+  const context = useMemo(() => screenshotMode?.getScreenshotContext('context'), [screenshotMode]);
+  const { error, isEmpty } = useExpressionRenderer(elementRef, {
+    expression,
+    context,
+  });
+
+  return (
+    <EuiPage paddingSize="none" data-shared-items-container data-shared-items-count={1}>
+      <EuiPageBody>
+        <EuiPageContent
+          borderRadius="none"
+          hasShadow={false}
+          paddingSize="none"
+          className="eui-fullHeight"
+        >
+          <EuiPageContentBody className="eui-fullHeight">
+            <EuiFlexGroup className="eui-fullHeight" gutterSize="none" direction="column">
+              <EuiFlexItem className="eui-fullHeight">
+                <div
+                  data-shared-item={(!isEmpty && !error) || null}
+                  data-render-error={!isEmpty && error ? error.message : null}
+                  ref={elementRef}
+                  className="expression"
+                />
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
+  );
+}
