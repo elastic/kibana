532a5c735338dc56be67f54d95d8a08cba9630f0
Faisal Kanout <faisal.kanout@elastic.co>
2022-03-18 12:14:32 +0100

    fix i18n

--- x-pack/plugins/infra/server/lib/alerting/log_threshold/register_log_threshold_rule_type.ts
+++ x-pack/plugins/infra/server/lib/alerting/log_threshold/register_log_threshold_rule_type.ts
@@ -79,7 +79,7 @@ const alertReasonMessageActionVariableDescription = i18n.translate(
 );
 
 const viewInAppUrlActionVariableDescription = i18n.translate(
-  'xpack.infra.logs.alerting.threshold.alertReasonMessageActionVariableDescription',
+  'xpack.infra.logs.alerting.threshold.viewInAppUrlActionVariableDescription',
   {
     defaultMessage:
       'Link to the view or feature within Elastic that can be used to investigate the alert and its context further',
