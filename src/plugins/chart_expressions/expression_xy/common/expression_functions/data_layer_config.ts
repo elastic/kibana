49b058e29d3af05a03b55f63dc79a0bee53b5ccc
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-03-18 12:14:33 +0200

    Fixed palette of the lens.

--- src/plugins/chart_expressions/expression_xy/common/expression_functions/data_layer_config.ts
+++ src/plugins/chart_expressions/expression_xy/common/expression_functions/data_layer_config.ts
@@ -106,11 +106,11 @@ export const dataLayerConfigFunction: ExpressionFunctionDefinition<
       }),
     },
     palette: {
-      default: `{theme "palette" default={system_palette name="default"} }`,
+      types: ['palette', 'system_palette'],
       help: i18n.translate('expressionXY.dataLayer.palette.help', {
         defaultMessage: 'Palette',
       }),
-      types: ['palette'],
+      default: '{palette}',
     },
   },
   fn(input, args) {
