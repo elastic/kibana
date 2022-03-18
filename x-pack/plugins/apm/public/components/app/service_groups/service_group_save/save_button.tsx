5aa07ce478dd4e3702611ede1abf9d1fc82d3e29
cauemarcondes <caue.marcondes@elastic.co>
2022-03-18 13:37:18 -0400

    spliting buttons

--- x-pack/plugins/apm/public/components/app/service_groups/service_group_save/save_button.tsx
+++ x-pack/plugins/apm/public/components/app/service_groups/service_group_save/save_button.tsx
@@ -4,12 +4,11 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EuiButton } from '@elastic/eui';
-import { i18n } from '@kbn/i18n';
 import React, { useState } from 'react';
 import { useAnyOfApmParams } from '../../../../hooks/use_apm_params';
 import { useFetcher } from '../../../../hooks/use_fetcher';
-import { ServiceGroupsTour } from '../service_groups_tour';
+import { CreateButton } from './create_button';
+import { EditButton } from './edit_button';
 import { SaveGroupModal } from './save_modal';
 
 export function ServiceGroupSaveButton() {
@@ -33,62 +32,16 @@ export function ServiceGroupSaveButton() {
   );
   const savedServiceGroup = data?.serviceGroup;
 
+  function onClick() {
+    setIsModalVisible((state) => !state);
+  }
+
   return (
     <>
-      {isGroupEditMode && (
-        <ServiceGroupsTour
-          type="editGroup"
-          title={i18n.translate(
-            'xpack.apm.serviceGroups.tour.editGroups.title',
-            { defaultMessage: 'Edit this service group' }
-          )}
-          content={i18n.translate(
-            'xpack.apm.serviceGroups.tour.editGroups.content',
-            {
-              defaultMessage:
-                'Use the edit option to change the name, query, or details of this service group.',
-            }
-          )}
-        >
-          <EuiButton
-            iconType="pencil"
-            onClick={async () => {
-              setIsModalVisible((state) => !state);
-            }}
-          >
-            {i18n.translate('xpack.apm.serviceGroups.editGroupLabel', {
-              defaultMessage: 'Edit group',
-            })}
-          </EuiButton>
-        </ServiceGroupsTour>
-      )}
-
-      {!isGroupEditMode && (
-        <ServiceGroupsTour
-          type="createGroup"
-          title={i18n.translate(
-            'xpack.apm.serviceGroups.tour.createGroups.title',
-            { defaultMessage: 'Introducing service groups' }
-          )}
-          content={i18n.translate(
-            'xpack.apm.serviceGroups.tour.createGroups.content',
-            {
-              defaultMessage:
-                'Group services together to build curated inventory views that remove noise and simplify investigations across services. Groups are Kibana space-specific and available for any users with appropriate access.',
-            }
-          )}
-        >
-          <EuiButton
-            iconType="plusInCircle"
-            onClick={async () => {
-              setIsModalVisible((state) => !state);
-            }}
-          >
-            {i18n.translate('xpack.apm.serviceGroups.createGroupLabel', {
-              defaultMessage: 'Create group',
-            })}
-          </EuiButton>
-        </ServiceGroupsTour>
+      {isGroupEditMode ? (
+        <EditButton onClick={onClick} />
+      ) : (
+        <CreateButton onClick={onClick} />
       )}
 
       {isModalVisible && (
