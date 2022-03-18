e82ab100d12b84f07de1096e45f9907c12f4657d
Jen Huang <jen.huang@elastic.co>
2022-03-18 12:00:30 -0700

    Use validated fields for index setting

--- x-pack/plugins/fleet/server/services/epm/elasticsearch/template/install.ts
+++ x-pack/plugins/fleet/server/services/epm/elasticsearch/template/install.ts
@@ -465,7 +465,7 @@ export async function installTemplate({
   const defaultSettings = buildDefaultSettings({
     templateName,
     packageName,
-    fields,
+    fields: validFields,
     type: dataStream.type,
     ilmPolicy: dataStream.ilm_policy,
   });
