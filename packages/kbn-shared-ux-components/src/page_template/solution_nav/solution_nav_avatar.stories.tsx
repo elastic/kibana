26643720a349deddc87d174792bfdfc90d967bbb
Maja Grubic <maja.grubic@elastic.co>
2022-03-22 16:14:02 +0100

    [SharedUX] Migrate PageTemplate > NoDataPage > SolutionNav

---
+++ packages/kbn-shared-ux-components/src/page_template/solution_nav/solution_nav_avatar.stories.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import {
+  KibanaPageTemplateSolutionNavAvatar,
+  KibanaPageTemplateSolutionNavAvatarProps,
+} from './solution_nav_avatar';
+
+export default {
+  title: 'Solution Nav Avatar',
+  description: 'A wrapper around EuiAvatar, with some extra styling',
+};
+
+type Params = Pick<KibanaPageTemplateSolutionNavAvatarProps, 'size' | 'name'>;
+
+export const PureComponent = (params: Params) => {
+  return <KibanaPageTemplateSolutionNavAvatar {...params} />;
+};
+
+PureComponent.argTypes = {
+  name: {
+    control: 'text',
+    defaultValue: 'Solution Name',
+  },
+  size: {
+    control: 'radio',
+    options: ['s', 'm', 'l', 'xl', 'xxl'],
+  },
+};
