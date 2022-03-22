5fc2b734b3d93059a6f1bb0421cae4f379d84c38
Tre' Seymour <wayne.seymour@elastic.co>
2022-03-22 13:30:17 +0000

    Just run one test, save the kbn archive and print it to the screen. So I can copy it.

--- x-pack/test/functional/apps/visualize/feature_controls/visualize_spaces.ts
+++ x-pack/test/functional/apps/visualize/feature_controls/visualize_spaces.ts
@@ -5,17 +5,32 @@
  * 2.0.
  */
 
+import readline from 'readline';
+import {createReadStream} from 'fs';
+import {fromEvent} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 import expect from '@kbn/expect';
-import { VisualizeConstants } from '../../../../../../src/plugins/visualize/public/application/visualize_constants';
-import { FtrProviderContext } from '../../../ftr_provider_context';
+import {VisualizeConstants} from '../../../../../../src/plugins/visualize/public/application/visualize_constants';
+import {FtrProviderContext} from '../../../ftr_provider_context';
 
-export default function ({ getPageObjects, getService }: FtrProviderContext) {
+export default function ({getPageObjects, getService}: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
   const config = getService('config');
   const spacesService = getService('spaces');
   const PageObjects = getPageObjects(['common', 'visualize', 'security', 'spaceSelector', 'error']);
   const testSubjects = getService('testSubjects');
   const appsMenu = getService('appsMenu');
+  const kibanaServer = getService('kibanaServer');
+
+  const showFile = (x: any) => {
+    const rl = readline.createInterface({input: createReadStream(x)});
+    const line$ = fromEvent(rl, 'line').pipe(takeUntil(fromEvent(rl, 'close')));
+    line$.subscribe(
+      (line: any) => process.stderr.write(`${line}\n`),
+      (err) => process.stderr.write(err),
+      () => process.stderr.write(`### showed file: ${x}\n`)
+    );
+  };
 
   describe('visualize', () => {
     before(async () => {
@@ -27,24 +42,47 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
         // we need to load the following in every situation as deleting
         // a space deletes all of the associated saved objects
         await esArchiver.load('x-pack/test/functional/es_archives/visualize/default');
-        await spacesService.create({
-          id: 'custom_space',
-          name: 'custom_space',
-          disabledFeatures: [],
-        });
+
+        await kibanaServer.importExport.save(
+          'x-pack/test/functional/fixtures/kbn_archiver/visualize/default',
+          {
+            types: [
+              'search',
+              'index-pattern',
+              'visualization',
+              'dashboard',
+              'lens',
+              'map',
+              'graph-workspace',
+              'query',
+              'tag',
+              'url',
+              'canvas-workpad',
+            ],
+          }
+        );
+        showFile('x-pack/test/functional/fixtures/kbn_archiver/visualize/default.json');
+
+        // await kibanaServer.savedObjects.cleanStandardList();
+        // await spacesService.create({
+        //   id: 'custom_space',
+        //   name: 'custom_space',
+        //   disabledFeatures: [],
+        // });    // we need to load the following in every situation as deleting
       });
 
-      after(async () => {
-        await spacesService.delete('custom_space');
-        await esArchiver.unload('x-pack/test/functional/es_archives/visualize/default');
-      });
+      // after(async () => {
+      //   await spacesService.delete('custom_space');
+      //   await esArchiver.unload('x-pack/test/functional/es_archives/visualize/default');
+      // });
 
-      it('shows visualize navlink', async () => {
-        await PageObjects.common.navigateToApp('home', {
-          basePath: '/s/custom_space',
-        });
-        const navLinks = (await appsMenu.readLinks()).map((link) => link.text);
-        expect(navLinks).to.contain('Visualize Library');
+      it.only('shows visualize navlink', async () => {
+        // await PageObjects.common.navigateToApp('home', {
+        //   basePath: '/s/custom_space',
+        // });
+        // const navLinks = (await appsMenu.readLinks()).map((link) => link.text);
+        // expect(navLinks).to.contain('Visualize Library');
+        expect(true).to.be(true);
       });
 
       it(`can view existing Visualization`, async () => {
