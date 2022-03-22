49ac9a3b96336c7accb359a90cfcd8419b922ed4
Esteban Beltran <esteban.beltran@elastic.co>
2022-03-22 12:54:33 +0100

    add more create case validatioons

--- x-pack/test/functional/services/cases/common.ts
+++ x-pack/test/functional/services/cases/common.ts
@@ -36,19 +36,27 @@ export function CasesCommonServiceProvider({ getService, getPageObject }: FtrPro
      * Doesn't do navigation. Only works if you are already inside a cases app page.
      * Does not work with the cases flyout.
      */
-    async createCaseFromCreateCasePage(caseTitle: string = 'test-' + uuid.v4()) {
+    async createCaseFromCreateCasePage({
+      title = 'test-' + uuid.v4(),
+      description = 'desc' + uuid.v4(),
+      tag = 'tagme',
+    }: {
+      title: string;
+      description: string;
+      tag: string;
+    }) {
       await this.openCreateCasePage();
 
       // case name
-      await testSubjects.setValue('input', caseTitle);
+      await testSubjects.setValue('input', title);
 
       // case tag
-      await comboBox.setCustom('comboBoxInput', 'tagme');
+      await comboBox.setCustom('comboBoxInput', tag);
 
       // case description
       const descriptionArea = await find.byCssSelector('textarea.euiMarkdownEditorTextArea');
       await descriptionArea.focus();
-      await descriptionArea.type('Test description');
+      await descriptionArea.type(description);
 
       // save
       await testSubjects.click('create-case-submit');
