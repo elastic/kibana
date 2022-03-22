ced6a70fb822fc6fe9e70c1945921bf01bb997a4
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-03-22 19:29:50 -0400

    [reporting/upgrade/tests] rewrite waitForJobToFinish to handle errors (#128309) (#128322)
    (cherry picked from commit a28b25a9b9d14110ba91e467c4efacd26aec0a23)
    
    Co-authored-by: Spencer <spencer@elastic.co>

--- packages/kbn-test/src/functional_test_runner/lib/config/schema.ts
+++ packages/kbn-test/src/functional_test_runner/lib/config/schema.ts
@@ -114,6 +114,7 @@ export const schema = Joi.object()
         try: Joi.number().default(120000),
         waitFor: Joi.number().default(20000),
         esRequestTimeout: Joi.number().default(30000),
+        kibanaReportCompletion: Joi.number().default(60_000),
         kibanaStabilize: Joi.number().default(15000),
         navigateStatusPageCheck: Joi.number().default(250),
 
@@ -166,7 +167,9 @@ export const schema = Joi.object()
 
     mochaReporter: Joi.object()
       .keys({
-        captureLogOutput: Joi.boolean().default(!!process.env.CI),
+        captureLogOutput: Joi.boolean().default(
+          !!process.env.CI && !process.env.DISABLE_CI_LOG_OUTPUT_CAPTURE
+        ),
         sendToCiStats: Joi.boolean().default(!!process.env.CI),
       })
       .default(),
