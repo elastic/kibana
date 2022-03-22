e0b73ed89f5585c145b1b0e3133b5a94797694af
Corey Robertson <corey.robertson@elastic.co>
2022-03-22 11:13:09 -0400

    wip

--- src/plugins/controls/public/control_types/time_slider/__stories__/time_slider.component.stories.tsx
+++ src/plugins/controls/public/control_types/time_slider/__stories__/time_slider.component.stories.tsx
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 import React, { FC, useCallback, useState } from 'react';
+import moment from 'moment';
 import { ControlGroupStoryComponent } from '../../../__stories__/controls.stories';
 
 import { TimeSliderComponent, TimeSliderProps, TimeSlider } from '../time_slider.component';
@@ -16,7 +17,7 @@ export default {
 };
 
 const TimeSliderWrapper: FC<Omit<TimeSliderProps, 'onChange'>> = (props) => {
-  const [value, setValue] = useState([props.range[0], props.range[1]]);
+  const [value, setValue] = useState([props.value[0], props.value[1]]);
   const onChange = useCallback(
     (newValue: [number | string, number | string]) => {
       const lowValue = typeof newValue[0] === 'string' ? parseFloat(newValue[0]) : newValue[0];
@@ -27,17 +28,93 @@ const TimeSliderWrapper: FC<Omit<TimeSliderProps, 'onChange'>> = (props) => {
     [setValue]
   );
 
-  return <TimeSlider {...props} onChange={onChange} value={value} />;
+  return <TimeSlider {...props} onChange={onChange} />;
 };
 
-export const TimeSliderLoading = () => {
-  return <TimeSliderWrapper range={[1643985198009, 1644589998009]} isLoading={true} />;
-};
+const undefinedValue: [undefined, undefined] = [undefined, undefined];
 
 export const TimeSliderNoValuesOrRange = () => {
-  return <TimeSliderWrapper range={[undefined, undefined]}
-}
+  // If range is undefined, that should be inndicate that we are loading the range
+  return <TimeSliderWrapper value={undefinedValue} />;
+};
 
-export const TimeSliderStory = () => {
-  return <TimeSliderWrapper range={[1643985198009, 1644589998009]} />;
+export const TimeSliderUndefinedRangeNoValue = () => {
+  // If a range is [undefined, undefined] then it was loaded, but no values were found.
+  return <TimeSliderWrapper range={undefinedValue} value={undefinedValue} />;
+};
+
+export const TimeSliderUndefinedRangeWithValue = () => {
+  const lastWeek = moment().subtract(7, 'days');
+  const now = moment();
+
+  return (
+    <TimeSliderWrapper range={undefinedValue} value={[lastWeek.unix() * 1000, now.unix() * 1000]} />
+  );
+};
+
+export const TimeSliderWithRangeAndNoValue = () => {
+  const lastWeek = moment().subtract(7, 'days');
+  const now = moment();
+
+  return (
+    <TimeSliderWrapper range={[lastWeek.unix() * 1000, now.unix() * 1000]} value={undefinedValue} />
+  );
+};
+
+export const TimeSliderWithRangeAndLowerValue = () => {
+  const lastWeek = moment().subtract(7, 'days');
+  const now = moment();
+
+  const threeDays = moment().subtract(3, 'days');
+
+  return (
+    <TimeSliderWrapper
+      range={[lastWeek.unix() * 1000, now.unix() * 1000]}
+      value={[threeDays.unix() * 1000, undefined]}
+    />
+  );
+};
+
+export const TimeSliderWithRangeAndUpperValue = () => {
+  const lastWeek = moment().subtract(7, 'days');
+  const now = moment();
+
+  const threeDays = moment().subtract(3, 'days');
+
+  return (
+    <TimeSliderWrapper
+      range={[lastWeek.unix() * 1000, now.unix() * 1000]}
+      value={[undefined, threeDays.unix() * 1000]}
+    />
+  );
+};
+
+export const TimeSliderWithRangeLowerThanValue = () => {
+  const twoWeeksAgo = moment().subtract(14, 'days');
+  const lastWeek = moment().subtract(7, 'days');
+
+  const now = moment();
+  const threeDays = moment().subtract(3, 'days');
+
+  return (
+    <TimeSliderWrapper
+      range={[twoWeeksAgo.unix() * 1000, lastWeek.unix() * 1000]}
+      value={[threeDays.unix() * 1000, now.unix() * 1000]}
+    />
+  );
+};
+
+export const TimeSliderWithRangeHigherThanValue = () => {
+  const twoWeeksAgo = moment().subtract(14, 'days');
+  const lastWeek = moment().subtract(7, 'days');
+
+  const now = moment();
+  const threeDays = moment().subtract(3, 'days');
+
+  return (
+    <TimeSliderWrapper
+      value={[twoWeeksAgo.unix() * 1000, lastWeek.unix() * 1000]}
+      range={[threeDays.unix() * 1000, now.unix() * 1000]}
+    />
+  );
 };
--- src/plugins/controls/public/control_types/time_slider/time_slider.component.tsx
+++ src/plugins/controls/public/control_types/time_slider/time_slider.component.tsx
@@ -7,6 +7,7 @@
  */
 
 import React, { FC, useState, useMemo, useCallback } from 'react';
+import { css } from '@emotion/react';
 import {
   EuiDualRange,
   EuiRangeProps,
@@ -17,11 +18,15 @@ import {
   EuiButton,
   EuiPopover,
   EuiPopoverTitle,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButtonEmpty,
 } from '@elastic/eui';
 import { EuiRangeTick } from '@elastic/eui/src/components/form/range/range_ticks';
 import moment from 'moment-timezone';
 import { TimeRange, calcAutoIntervalNear } from '../../../../data/common';
 import { ValidatedDualRange } from '../../../../kibana_react/public';
+import './time_slider.component.scss';
 
 function getTimezone() {
   const detectedTimezone = moment.tz.guess();
@@ -97,29 +102,84 @@ export function epochToKbnDateFormat(epoch: number): string {
   return moment.tz(epoch, timezone).format(dateFormat);
 }
 
+export interface TimeSliderProps {
+  range?: [number | undefined, number | undefined];
+  value: [number | undefined, number | undefined];
+  onChange: EuiDualRangeProps['onChange'];
+}
+
+const isValidRange = (maybeRange: TimeSliderProps['range']): maybeRange is [number, number] => {
+  return maybeRange !== undefined && maybeRange[0] !== undefined && maybeRange[1] !== undefined;
+};
+
 export const TimeSlider: FC<TimeSliderProps> = (props) => {
-  const { range, value, isLoading } = props;
+  const { range, value } = props;
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const togglePopover = useCallback(() => {
     setIsPopoverOpen(!isPopoverOpen);
   }, [isPopoverOpen, setIsPopoverOpen]);
-  const [lowerBound, upperBound] = range;
-  const [lowerValue, upperValue] = value;
+
+  // If we don't have a range or we have is loading, show the loading state
+  const hasRange = range !== undefined;
+
+  // We have values if we have a range or value entry for both position
+  const hasValues =
+    (value[0] !== undefined || (hasRange && range[0] !== undefined)) &&
+    (value[1] !== undefined || (hasRange && range[1] !== undefined));
+
+  let valueText: JSX.Element | null = null;
+  if (hasValues) {
+    const lower = value[0] !== undefined ? value[0] : range![0];
+    const upper = value[1] !== undefined ? value[1] : range![1];
+
+    // has value and doesn't have a
+    const hasLowerValueInRange =
+      value[0] !== undefined &&
+      (!isValidRange(range) || (value[0] >= range[0] && value[0] <= range[1]));
+    // It's out of range if the upper value is above the upper range or below the lower range
+    const hasUpperValueInRange =
+      value[1] !== undefined &&
+      (!isValidRange(range) || (value[1] <= range[1] && value[1] >= range[0]));
+
+    valueText = (
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiText
+            className={
+              hasLowerValueInRange ? 'timeSlider_anchorText' : 'timeSlider_anchorText__default'
+            }
+            textAlign="right"
+          >
+            {epochToKbnDateFormat(lower)}
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}> → </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiText
+            className={
+              hasUpperValueInRange ? 'timeSlider_anchorText' : 'timeSlider_anchorText__default'
+            }
+            textAlign="left"
+          >
+            {epochToKbnDateFormat(upper)}
+          </EuiText>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  }
 
   const button = (
-    <EuiFilterButton
-      isLoading={isLoading}
-      data-test-subj={``}
+    <EuiButtonEmpty
+      className="timeSlider_anchor"
+      color="text"
+      iconType={'empty'}
+      iconSide="right"
+      isLoading={!hasRange}
       onClick={togglePopover}
       isSelected={true}
     >
-      {!isLoading ? (
-        <EuiText>
-          {epochToKbnDateFormat(lowerValue < lowerBound ? lowerBound : lowerValue)} →{' '}
-          {epochToKbnDateFormat(upperValue > upperBound ? upperBound : upperValue)}
-        </EuiText>
-      ) : null}
-    </EuiFilterButton>
+      {valueText}
+    </EuiButtonEmpty>
   );
 
   return (
@@ -135,28 +195,33 @@ export const TimeSlider: FC<TimeSliderProps> = (props) => {
         ownFocus
         repositionOnScroll
       >
-        <TimeSliderComponent {...props} />
+        {isValidRange(range) ? (
+          <TimeSliderComponentPopover range={range} value={value} onChange={props.onChange} />
+        ) : (
+          <TimeSliderComponentPopoverNoDocuments />
+        )}
       </EuiPopover>
     </EuiFilterGroup>
   );
 };
 
-export interface TimeSliderProps {
-  range: [number, number];
-  value: [number, number];
-  onChange: EuiDualRangeProps['onChange'];
-  isLoading: boolean;
-}
+const TimeSliderComponentPopoverNoDocuments: FC = () => {
+  return <EuiText color="default">There were no documents found, so no range is available</EuiText>;
+};
 
-export const TimeSliderComponent: FC<TimeSliderProps> = ({ range, value, onChange }) => {
+export const TimeSliderComponentPopover: FC<TimeSliderProps & { range: [number, number] }> = ({
+  range,
+  value,
+  onChange,
+}) => {
   const [lowerBound, upperBound] = range;
   let [lowerValue, upperValue] = value;
 
-  if (lowerValue < lowerBound) {
+  if (lowerValue === undefined) {
     lowerValue = lowerBound;
   }
 
-  if (upperValue > upperBound) {
+  if (upperValue === undefined) {
     upperValue = upperBound;
   }
 
@@ -188,27 +253,5 @@ export const TimeSliderComponent: FC<TimeSliderProps> = ({ range, value, onChang
         />
       </div>
     </>
-  );
-
-  // We want to have some sort of delay in actually pushing up a change so we don't continually push changes
-  // But for now we'll just report everything up.  Maybe a layer up is where that should be happening anyways
-  return (
-    <div>
-      <div>
-        <EuiText textAlign="center">
-          {epochToKbnDateFormat(lowerValue)} - {epochToKbnDateFormat(upperValue)}
-        </EuiText>
-      </div>
-
-      <EuiDualRange
-        min={lowerBound}
-        max={upperBound}
-        value={value}
-        fullWidth={true}
-        onChange={onChange}
-        showTicks={true}
-        ticks={ticks}
-      />
-    </div>
   );
 };
