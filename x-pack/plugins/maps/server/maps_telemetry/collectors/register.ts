839130358f8c5d421e01172a851c41198f46ce75
nreese <reese.nathan@elastic.co>
2022-03-23 12:13:16 -0600

    update comments

--- x-pack/plugins/maps/server/maps_telemetry/collectors/register.ts
+++ x-pack/plugins/maps/server/maps_telemetry/collectors/register.ts
@@ -65,12 +65,12 @@ export function registerMapsUsageCollector(usageCollection?: UsageCollectionSetu
           },
         },
         es_agg_hexagons: {
-          min: { type: 'long', _meta: { description: 'min number of es grid layers per map' } },
-          max: { type: 'long', _meta: { description: 'max number of es grid layers per map' } },
-          avg: { type: 'float', _meta: { description: 'avg number of es grid layers per map' } },
+          min: { type: 'long', _meta: { description: 'min number of es hexagon layers per map' } },
+          max: { type: 'long', _meta: { description: 'max number of es hexagon layers per map' } },
+          avg: { type: 'float', _meta: { description: 'avg number of es hexagon layers per map' } },
           total: {
             type: 'long',
-            _meta: { description: 'total number of es grid layers in cluster' },
+            _meta: { description: 'total number of es hexagon layers in cluster' },
           },
         },
         es_agg_heatmap: {
