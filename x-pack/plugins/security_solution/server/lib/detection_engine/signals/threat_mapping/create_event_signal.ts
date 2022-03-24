8f6322596c8765eaf61d6fb99908cdf0004dda01
Khristinin Nikita <nikita.khristinin@elastic.co>
2022-03-24 09:45:19 +0100

    Add events-first (reverse) search  for IM rule (#127428)
    * WIP
    
    * Add tets and refactoring
    
    * Add abstraction to run threat im rule
    
    * Add per page to search threat indicators
    
    * Fix tests and linting
    
    * fix tests
    
    * Add integrations tests
    
    * Fix IP
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/threat_mapping/create_event_signal.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/threat_mapping/create_event_signal.ts
@@ -6,20 +6,24 @@
  */
 
 import { buildThreatMappingFilter } from './build_threat_mapping_filter';
-
 import { getFilter } from '../get_filter';
 import { searchAfterAndBulkCreate } from '../search_after_bulk_create';
 import { buildReasonMessageForThreatMatchAlert } from '../reason_formatters';
-import { CreateThreatSignalOptions } from './types';
-import { SearchAfterAndBulkCreateReturnType } from '../types';
+import { CreateEventSignalOptions } from './types';
+import { SearchAfterAndBulkCreateReturnType, SignalSearchResponse } from '../types';
+import { getAllThreatListHits } from './get_threat_list';
+import {
+  enrichSignalThreatMatches,
+  getSignalMatchesFromThreatList,
+} from './enrich_signal_threat_matches';
 
-export const createThreatSignal = async ({
+export const createEventSignal = async ({
   alertId,
   buildRuleMessage,
   bulkCreate,
   completeRule,
   currentResult,
-  currentThreatList,
+  currentEventList,
   eventsTelemetry,
   exceptionItems,
   filters,
@@ -32,20 +36,26 @@ export const createThreatSignal = async ({
   savedId,
   searchAfterSize,
   services,
-  threatEnrichment,
   threatMapping,
   tuple,
   type,
   wrapHits,
-}: CreateThreatSignalOptions): Promise<SearchAfterAndBulkCreateReturnType> => {
+  threatQuery,
+  threatFilters,
+  threatLanguage,
+  threatIndex,
+  threatListConfig,
+  threatIndicatorPath,
+  perPage,
+}: CreateEventSignalOptions): Promise<SearchAfterAndBulkCreateReturnType> => {
   const threatFilter = buildThreatMappingFilter({
     threatMapping,
-    threatList: currentThreatList,
-    entryKey: 'value',
+    threatList: currentEventList,
+    entryKey: 'field',
   });
 
   if (!threatFilter.query || threatFilter.query?.bool.should.length === 0) {
-    // empty threat list and we do not want to return everything as being
+    // empty event list and we do not want to return everything as being
     // a hit so opt to return the existing result.
     logger.debug(
       buildRuleMessage(
@@ -54,9 +64,39 @@ export const createThreatSignal = async ({
     );
     return currentResult;
   } else {
+    const threatListHits = await getAllThreatListHits({
+      esClient: services.scopedClusterClient.asCurrentUser,
+      exceptionItems,
+      threatFilters: [...threatFilters, threatFilter],
+      query: threatQuery,
+      language: threatLanguage,
+      index: threatIndex,
+      logger,
+      buildRuleMessage,
+      threatListConfig: {
+        _source: [`${threatIndicatorPath}.*`, 'threat.feed.*'],
+        fields: undefined,
+      },
+      perPage,
+    });
+
+    const signalMatches = getSignalMatchesFromThreatList(threatListHits);
+
+    const ids = signalMatches.map((item) => item.signalId);
+
+    const indexFilter = {
+      query: {
+        bool: {
+          filter: {
+            ids: { values: ids },
+          },
+        },
+      },
+    };
+
     const esFilter = await getFilter({
       type,
-      filters: [...filters, threatFilter],
+      filters: [...filters, indexFilter],
       language,
       query,
       savedId,
@@ -67,9 +107,17 @@ export const createThreatSignal = async ({
 
     logger.debug(
       buildRuleMessage(
-        `${threatFilter.query?.bool.should.length} indicator items are being checked for existence of matches`
+        `${ids?.length} matched signals found from ${threatListHits.length} indicators`
       )
     );
+
+    const threatEnrichment = (signals: SignalSearchResponse): Promise<SignalSearchResponse> =>
+      enrichSignalThreatMatches(
+        signals,
+        () => Promise.resolve(threatListHits),
+        threatIndicatorPath,
+        signalMatches
+      );
 
     const result = await searchAfterAndBulkCreate({
       buildReasonMessage: buildReasonMessageForThreatMatchAlert,
