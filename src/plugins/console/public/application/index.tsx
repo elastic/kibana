e75c1daef91635fe4897a8e935ae7d633e4e9141
Muhammad Ibragimov <muhammad.ibragimov@elastic.co>
2022-03-24 16:25:56 +0500

    Remove unused imports

--- src/plugins/console/public/application/index.tsx
+++ src/plugins/console/public/application/index.tsx
@@ -75,7 +75,6 @@ export function renderApp({
               notifications,
               trackUiMetric,
               objectStorageClient,
-              http,
             },
             theme$,
           }}
