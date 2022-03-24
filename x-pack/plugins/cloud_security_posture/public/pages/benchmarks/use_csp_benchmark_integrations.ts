d8064e44aad2d603b84cb8718e7dd987a0a39e11
Ari Aviran <ari.aviran@elastic.co>
2022-03-24 14:37:54 +0200

    [Cloud Posture] Support pagination in benchmarks page

--- x-pack/plugins/cloud_security_posture/public/pages/benchmarks/use_csp_benchmark_integrations.ts
+++ x-pack/plugins/cloud_security_posture/public/pages/benchmarks/use_csp_benchmark_integrations.ts
@@ -8,28 +8,39 @@
 import { useQuery } from 'react-query';
 import type { ListResult } from '../../../../fleet/common';
 import { BENCHMARKS_ROUTE_PATH } from '../../../common/constants';
+import { BenchmarksQuerySchema } from '../../../common/schemas/benchmark';
 import { useKibana } from '../../common/hooks/use_kibana';
 import type { Benchmark } from '../../../common/types';
 
 const QUERY_KEY = 'csp_benchmark_integrations';
 
-interface Props {
+export interface UseCspBenchmarkIntegrationsProps {
   name: string;
   page: number;
   perPage: number;
+  sortField: BenchmarksQuerySchema['sort_field'];
+  sortOrder: BenchmarksQuerySchema['sort_order'];
 }
 
-export const useCspBenchmarkIntegrations = ({ name, perPage, page }: Props) => {
+export const useCspBenchmarkIntegrations = ({
+  name,
+  perPage,
+  page,
+  sortField,
+  sortOrder,
+}: UseCspBenchmarkIntegrationsProps) => {
   const { http } = useKibana().services;
-  return useQuery([QUERY_KEY, { name, perPage, page }], () =>
-    http.get<ListResult<Benchmark>>(BENCHMARKS_ROUTE_PATH, {
-      query: {
-        benchmark_name: name,
-        per_page: perPage,
-        page,
-        sort_field: 'name',
-        sort_order: 'asc',
-      },
-    })
+  const query: BenchmarksQuerySchema = {
+    benchmark_name: name,
+    per_page: perPage,
+    page,
+    sort_field: sortField,
+    sort_order: sortOrder,
+  };
+
+  return useQuery(
+    [QUERY_KEY, { name, perPage, page, sortField, sortOrder }],
+    () => http.get<ListResult<Benchmark>>(BENCHMARKS_ROUTE_PATH, { query }),
+    { keepPreviousData: true }
   );
 };
