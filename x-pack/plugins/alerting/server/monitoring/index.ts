f981d53b6f8771b2b6483cf91315f888c774fdb1
Chris Roberson <chris.roberson@elastic.co>
2022-03-24 12:23:12 -0400

    [ResponseOps] Integrate rule and action monitoring data to the monitoring collection plugin (#123416)
    * Add new plugint to collect additional kibana monitoring metrics
    
    * Readme
    
    * Update generated document
    
    * WIP
    
    * Remove task manager and add support for max number
    
    * Use MAX_SAFE_INTEGER
    
    * We won't use this route
    
    * Tests and lint
    
    * Track actions
    
    * Use dynamic route style
    
    * Fix test
    
    * Add in mapping verification
    
    * Adapt to new changes in base PR
    
    * Fix types
    
    * Feedback from PR
    
    * PR feedback
    
    * We do not need this
    
    * PR feedback
    
    * Match options to api/stats
    
    * Remove internal collection support
    
    * Fix api change
    
    * Fix small issues
    
    * Separate cluster and node metrics
    
    * Add more tests
    
    * Add retryAt in the test too
    
    * Add logging and use a class
    
    * fix types
    
    * Fix tests
    
    * PR feedback
    
    * Add types
    
    * Fix types
    
    * Linting fixes
    
    * Remove unnecessary changes
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/alerting/server/monitoring/index.ts
+++ x-pack/plugins/alerting/server/monitoring/index.ts
@@ -4,4 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-export const TYPE_ALLOWLIST = ['node_rules', 'cluster_rules', 'node_actions', 'cluster_actions'];
+export { registerNodeCollector } from './register_node_collector';
+export { registerClusterCollector } from './register_cluster_collector';
+export * from './types';
+export * from './in_memory_metrics';
