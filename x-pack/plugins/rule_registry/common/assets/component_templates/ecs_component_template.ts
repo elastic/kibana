8ec91a584b95360f5eea02b8fd8cfcc79edf2d2b
Kevin Qualters <kevin.qualters@elastic.co>
2022-03-24 12:22:24 -0400

    Remove all linux eventing model fields from mapping save for absolutely needed fields

--- x-pack/plugins/rule_registry/common/assets/component_templates/ecs_component_template.ts
+++ x-pack/plugins/rule_registry/common/assets/component_templates/ecs_component_template.ts
@@ -14,7 +14,7 @@ export const ecsComponentTemplate: ClusterPutComponentTemplateBody = {
   template: {
     settings: {
       number_of_shards: 1,
-      'index.mapping.total_fields.limit': 1800,
+      'index.mapping.total_fields.limit': 1700,
     },
     mappings: merge(
       {},
