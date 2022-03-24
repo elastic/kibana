2bc3615a3561570471764cc46853c3296b359d12
Gloria Hornero <gloria.hornero@elastic.co>
2022-03-24 17:16:33 +0100

    fixing failures

--- x-pack/plugins/security_solution/cypress/tasks/common.ts
+++ x-pack/plugins/security_solution/cypress/tasks/common.ts
@@ -79,6 +79,7 @@ export const deleteAlertsAndRules = () => {
       query: '',
       action: 'delete',
     },
+    failOnStatusCode: false,
     headers: { 'kbn-xsrf': 'cypress-creds-via-config' },
   });
 
