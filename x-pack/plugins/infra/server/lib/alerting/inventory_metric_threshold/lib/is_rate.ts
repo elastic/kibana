61fed45c3ba3cc3dba67cf4c70597bfd5a4c843b
mgiota <panagiota.mitsopoulou@elastic.co>
2022-03-24 21:55:17 +0100

    fix inventory rule type custom metric error

--- x-pack/plugins/infra/server/lib/alerting/inventory_metric_threshold/lib/is_rate.ts
+++ x-pack/plugins/infra/server/lib/alerting/inventory_metric_threshold/lib/is_rate.ts
@@ -9,21 +9,30 @@ import { has } from 'lodash';
 import { MetricsUIAggregation } from '../../../../../common/inventory_models/types';
 import { SnapshotCustomMetricInput } from '../../../../../common/http_api';
 
-export const isMetricRate = (metric: MetricsUIAggregation): boolean => {
-  const values = Object.values(metric);
-  return values.some((agg) => has(agg, 'derivative')) && values.some((agg) => has(agg, 'max'));
+export const isMetricRate = (metric: MetricsUIAggregation | undefined): boolean => {
+  if (metric != null) {
+    const values = Object.values(metric);
+    return values.some((agg) => has(agg, 'derivative')) && values.some((agg) => has(agg, 'max'));
+  }
+  return false;
 };
 
 export const isCustomMetricRate = (customMetric: SnapshotCustomMetricInput) => {
   return customMetric.aggregation === 'rate';
 };
 
-export const isInterfaceRateAgg = (metric: MetricsUIAggregation) => {
-  const values = Object.values(metric);
-  return values.some((agg) => has(agg, 'terms')) && values.some((agg) => has(agg, 'sum_bucket'));
+export const isInterfaceRateAgg = (metric: MetricsUIAggregation | undefined) => {
+  if (metric != null) {
+    const values = Object.values(metric);
+    return values.some((agg) => has(agg, 'terms')) && values.some((agg) => has(agg, 'sum_bucket'));
+  }
+  return false;
 };
 
-export const isRate = (metric: MetricsUIAggregation, customMetric?: SnapshotCustomMetricInput) => {
+export const isRate = (
+  metric: MetricsUIAggregation | undefined,
+  customMetric?: SnapshotCustomMetricInput
+) => {
   return (
     isMetricRate(metric) ||
     isInterfaceRateAgg(metric) ||
