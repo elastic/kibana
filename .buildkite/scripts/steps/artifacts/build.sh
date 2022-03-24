3365643950d3a1fb6d9a9111f0199b4a9f2e958a
Jonathan Budzenski <jon@elastic.co>
2022-03-24 13:47:26 -0500

    [artifacts] Integrate release manager CLI and upload artifacts

--- .buildkite/scripts/steps/artifacts/build.sh
+++ .buildkite/scripts/steps/artifacts/build.sh
@@ -5,7 +5,42 @@ set -euo pipefail
 .buildkite/scripts/bootstrap.sh
 
 echo "--- Build Kibana Distribution"
+# TODO: ?--release
 node scripts/build --all-platforms --debug --skip-docker-cloud
 
 echo "--- Build dependencies report"
 node scripts/licenses_csv_report --csv=target/dependencies_report.csv
+
+echo "$KIBANA_DOCKER_PASSWORD" | docker login -u "$KIBANA_DOCKER_USERNAME" --password-stdin docker.elastic.co
+trap 'docker logout docker.elastic.co' EXIT
+
+docker pull docker.elastic.co/infra/release-manager:latest
+
+VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-role-id)"
+VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-secret-id)"
+VAULT_ADDR="https://secrets.elastic.co:8200"
+
+
+# TODO: snapshot | staging
+WORKFLOW="snapshot"
+
+BRANCH=$(jq -r .branch "package.json")
+VERSION=$(jq -r .version "package.json")
+COMMIT=$(git rev-parse HEAD)
+QUALIFIER=""
+
+docker run --rm \
+  --name release-manager \
+  -e VAULT_ADDR \
+  -e VAULT_ROLE_ID \
+  -e VAULT_SECRET_ID \
+  --mount type=bind,readonly=false,src="$PWD/target",target=/artifacts \
+  docker.elastic.co/infra/release-manager:latest \
+    cli collect \
+      --project kibana \
+      --branch "$BRANCH" \
+      --commit "$COMMIT" \
+      --workflow "$WORKFLOW" \
+      --version "$VERSION"
+      --qualifier "$QUALIFIER"
+      --artifact-set main
