0a9058dc10fb887e4c46fb0b878072b853aca88e
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2022-03-25 15:56:33 +0100

    Add functional test

--- src/plugins/discover/public/application/main/components/document_explorer_callout/document_explorer_callout.tsx
+++ src/plugins/discover/public/application/main/components/document_explorer_callout/document_explorer_callout.tsx
@@ -102,6 +102,7 @@ function CalloutTitle({ onCloseCallout }: { onCloseCallout: () => void }) {
           aria-label={i18n.translate('discover.docExplorerCallout.closeButtonAriaLabel', {
             defaultMessage: 'Close',
           })}
+          data-test-subj="dscExplorerCalloutClose"
           onClick={onCloseCallout}
           type="button"
           iconType="cross"
