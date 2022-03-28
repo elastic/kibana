b3b760af55e3732c5a97d0377b63ee4e03bda6bb
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-03-28 15:39:24 -0700

    Handles new options and nav method

--- x-pack/plugins/fleet/.storybook/context/application.ts
+++ x-pack/plugins/fleet/.storybook/context/application.ts
@@ -21,6 +21,9 @@ export const getApplication = () => {
     navigateToApp: async (app: string) => {
       action(`Navigate to: ${app}`);
     },
+    navigateToUrlSkipUnload: async (url: string) => {
+      action(`Navigate to: ${url} skip unload`);
+    },
     getUrlForApp: (url: string) => url,
     capabilities: {
       catalogue: {},
