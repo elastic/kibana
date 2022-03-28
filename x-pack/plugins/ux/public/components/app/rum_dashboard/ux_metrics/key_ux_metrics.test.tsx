ed0bccb59b5188273e78818758674140ba425daa
Bree Hall <briannajdhall@gmail.com>
2022-03-28 15:16:43 -0400

    Completed test case revisions for Jest test suites 1, 3, 6, 7, and 8. Updated required snapshots. Updated various tests to account for text rendering of the EuiIcon text.

--- x-pack/plugins/ux/public/components/app/rum_dashboard/ux_metrics/key_ux_metrics.test.tsx
+++ x-pack/plugins/ux/public/components/app/rum_dashboard/ux_metrics/key_ux_metrics.test.tsx
@@ -22,7 +22,7 @@ describe('KeyUXMetrics', () => {
       status: FETCH_STATUS.SUCCESS,
       refetch: jest.fn(),
     });
-    const { getAllByText } = render(
+    const { getAllByText, getByText } = render(
       <KeyUXMetrics
         loading={false}
         data={{
@@ -45,20 +45,22 @@ describe('KeyUXMetrics', () => {
       };
     };
 
+    // Tests include the word "info" between the task and time to account for the rendered text coming from
+    // the EuiIcon (tooltip) embedded within each stat description
     expect(
-      getAllByText(checkText('Longest long task duration271 ms'))[0]
+      getAllByText(checkText('Longest long task durationInfo271 ms'))[0]
     ).toBeInTheDocument();
     expect(
-      getAllByText(checkText('Total long tasks duration520 ms'))[0]
+      getAllByText(checkText('Total long tasks durationInfo520 ms'))[0]
     ).toBeInTheDocument();
     expect(
-      getAllByText(checkText('No. of long tasks3'))[0]
+      getAllByText(checkText('No. of long tasksInfo3'))[0]
     ).toBeInTheDocument();
     expect(
-      getAllByText(checkText('Total blocking time271 ms'))[0]
+      getAllByText(checkText('Total blocking timeInfo271 ms'))[0]
     ).toBeInTheDocument();
     expect(
-      getAllByText(checkText('First contentful paint1.27 s'))[0]
+      getAllByText(checkText('First contentful paintInfo1.27 s'))[0]
     ).toBeInTheDocument();
   });
 });
