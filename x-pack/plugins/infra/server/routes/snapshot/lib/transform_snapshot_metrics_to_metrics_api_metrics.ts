cf6e54583373782e99089c33c97f35a3c629eca1
Chris Cowan <chris@elastic.co>
2022-03-28 12:18:50 -0600

    Adding check for missing aggregations

--- x-pack/plugins/infra/server/routes/snapshot/lib/transform_snapshot_metrics_to_metrics_api_metrics.ts
+++ x-pack/plugins/infra/server/routes/snapshot/lib/transform_snapshot_metrics_to_metrics_api_metrics.ts
@@ -16,27 +16,33 @@ import {
 export const transformSnapshotMetricsToMetricsAPIMetrics = (
   snapshotRequest: SnapshotRequest
 ): MetricsAPIMetric[] => {
-  return snapshotRequest.metrics.map((metric, index) => {
-    const inventoryModel = findInventoryModel(snapshotRequest.nodeType);
-    if (SnapshotCustomMetricInputRT.is(metric)) {
-      const isUniqueId = snapshotRequest.metrics.findIndex((m) =>
-        SnapshotCustomMetricInputRT.is(m) ? m.id === metric.id : false
-      );
-      const customId = isUniqueId ? metric.id : `custom_${index}`;
-      if (metric.aggregation === 'rate') {
-        return { id: customId, aggregations: networkTraffic(customId, metric.field) };
-      }
-      return {
-        id: customId,
-        aggregations: {
-          [customId]: {
-            [metric.aggregation]: {
-              field: metric.field,
+  return snapshotRequest.metrics
+    .map((metric, index) => {
+      const inventoryModel = findInventoryModel(snapshotRequest.nodeType);
+      if (SnapshotCustomMetricInputRT.is(metric)) {
+        const isUniqueId = snapshotRequest.metrics.findIndex((m) =>
+          SnapshotCustomMetricInputRT.is(m) ? m.id === metric.id : false
+        );
+        const customId = isUniqueId ? metric.id : `custom_${index}`;
+        if (metric.aggregation === 'rate') {
+          return { id: customId, aggregations: networkTraffic(customId, metric.field) };
+        }
+        return {
+          id: customId,
+          aggregations: {
+            [customId]: {
+              [metric.aggregation]: {
+                field: metric.field,
+              },
             },
           },
-        },
-      };
-    }
-    return { id: metric.type, aggregations: inventoryModel.metrics.snapshot?.[metric.type] };
-  });
+        };
+      }
+      const aggregations = inventoryModel.metrics.snapshot?.[metric.type];
+      if (!aggregations) {
+        return null;
+      }
+      return { id: metric.type, aggregations };
+    })
+    .filter((val) => val != null) as MetricsAPIMetric[];
 };
