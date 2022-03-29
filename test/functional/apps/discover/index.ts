6c355dc1e8380d739d99bae4e33fe13f6a8eac93
liza-mae <liza-mae@users.noreply.github.com>
2022-03-29 15:03:56 -0600

    Update test/functional/apps/discover/index.ts
    Co-authored-by: Spencer <email@spalger.com>

--- test/functional/apps/discover/index.ts
+++ test/functional/apps/discover/index.ts
@@ -20,7 +20,7 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
     });
 
     after(async function unloadMakelogs() {
-      return await esArchiver.unload('test/functional/fixtures/es_archiver/logstash_functional');
+      await esArchiver.unload('test/functional/fixtures/es_archiver/logstash_functional');
     });
 
     loadTestFile(require.resolve('./_saved_queries'));
