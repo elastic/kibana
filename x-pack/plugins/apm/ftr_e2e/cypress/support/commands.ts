e7feb624fab67248ac83a40d8ec09bbbd20ba3b7
spalger <spencer@elastic.co>
2022-03-29 13:28:57 -0600

    [axe-config] extract module to it's own package

--- x-pack/plugins/apm/ftr_e2e/cypress/support/commands.ts
+++ x-pack/plugins/apm/ftr_e2e/cypress/support/commands.ts
@@ -8,8 +8,7 @@ import 'cypress-real-events/support';
 import { Interception } from 'cypress/types/net-stubbing';
 import 'cypress-axe';
 import moment from 'moment';
-// Commenting this out since it's breaking the tests. It was caused by https://github.com/elastic/kibana/commit/bef90a58663b6c4b668a7fe0ce45a002fb68c474#diff-8a4659c6955a712376fe5ca0d81636164d1b783a63fe9d1a23da4850bd0dfce3R10
-// import { AXE_CONFIG, AXE_OPTIONS } from '@kbn/test';
+import { AXE_CONFIG, AXE_OPTIONS } from '@kbn/axe-config';
 
 Cypress.Commands.add('loginAsReadOnlyUser', () => {
   cy.loginAs({ username: 'apm_read_user', password: 'changeme' });
