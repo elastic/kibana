a5ceb0acfe00401675995b3e2a50b34b4c8a8bf9
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-03-29 11:51:46 +0200

    remove unnecessary mock

--- x-pack/plugins/screenshotting/server/formats/pdf/pdf_maker/integration_tests/pdfmaker.test.ts
+++ x-pack/plugins/screenshotting/server/formats/pdf/pdf_maker/integration_tests/pdfmaker.test.ts
@@ -7,13 +7,6 @@
 
 /* eslint-disable max-classes-per-file */
 
-jest.mock('worker_threads', () => {
-  return {
-    ...jest.requireActual('worker_threads'),
-    isMainThread: false,
-  };
-});
-
 import path from 'path';
 import { loggingSystemMock } from 'src/core/server/mocks';
 import { isUint8Array } from 'util/types';
