7e5b67e95934ffe26a7c3f6ee057db59463d3bc2
Joseph Crail <jbcrail@gmail.com>
2022-03-30 00:54:31 -0700

    Add argument for top N items (#51)

--- src/plugins/profiling/server/routes/search_topn.test.ts
+++ src/plugins/profiling/server/routes/search_topn.test.ts
@@ -90,6 +90,7 @@ describe('TopN data from Elasticsearch', () => {
         '123',
         '456',
         '789',
+        200,
         'field',
         kibanaResponseFactory
       );
@@ -112,6 +113,7 @@ describe('TopN data from Elasticsearch', () => {
         '123',
         '456',
         '789',
+        200,
         'StackTraceID',
         kibanaResponseFactory
       );
