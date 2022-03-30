0a056cc62a6102a930d354dcc4fa520bc41aedf3
Jonathan Budzenski <jon@elastic.co>
2022-03-30 11:36:58 -0500

    [artifacts] Extract default i18n messages

--- .buildkite/scripts/steps/artifacts/build.sh
+++ .buildkite/scripts/steps/artifacts/build.sh
@@ -9,3 +9,8 @@ node scripts/build --all-platforms --debug --docker-cross-compile --skip-docker-
 
 echo "--- Build dependencies report"
 node scripts/licenses_csv_report --csv=target/dependencies_report.csv
+
+echo "--- Extract default i18n messages"
+mkdir -p target/i18n
+node scripts/i18n_extract
+buildkite-agent artifact upload "target/i18n/en.json"
