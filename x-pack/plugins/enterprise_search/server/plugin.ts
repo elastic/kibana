748d79f4a049112967fbd22b684e250353d58c81
scottybollinger <scotty.bollinger@elastic.co>
2022-03-30 10:47:34 -0500

    Register gated Content plugin

--- x-pack/plugins/enterprise_search/server/plugin.ts
+++ x-pack/plugins/enterprise_search/server/plugin.ts
@@ -24,6 +24,7 @@ import { SpacesPluginStart } from '../../spaces/server';
 
 import {
   ENTERPRISE_SEARCH_OVERVIEW_PLUGIN,
+  ENTERPRISE_SEARCH_CONTENT_PLUGIN,
   APP_SEARCH_PLUGIN,
   WORKPLACE_SEARCH_PLUGIN,
   ENTERPRISE_SEARCH_RELEVANCE_LOGS_SOURCE_ID,
@@ -89,6 +90,18 @@ export class EnterpriseSearchPlugin implements Plugin {
   ) {
     const config = this.config;
     const log = this.logger;
+    const PLUGIN_IDS = [
+      ENTERPRISE_SEARCH_OVERVIEW_PLUGIN.ID,
+      APP_SEARCH_PLUGIN.ID,
+      WORKPLACE_SEARCH_PLUGIN.ID,
+    ];
+
+    /*
+     * We are gating the Content plugin to develpers only until release
+     */
+    if (process.env.NODE_ENV === 'development') {
+      PLUGIN_IDS.push(ENTERPRISE_SEARCH_CONTENT_PLUGIN.ID);
+    }
 
     if (customIntegrations) {
       registerEnterpriseSearchIntegrations(http, customIntegrations);
@@ -107,17 +120,8 @@ export class EnterpriseSearchPlugin implements Plugin {
       name: ENTERPRISE_SEARCH_OVERVIEW_PLUGIN.NAME,
       order: 0,
       category: DEFAULT_APP_CATEGORIES.enterpriseSearch,
-      app: [
-        'kibana',
-        ENTERPRISE_SEARCH_OVERVIEW_PLUGIN.ID,
-        APP_SEARCH_PLUGIN.ID,
-        WORKPLACE_SEARCH_PLUGIN.ID,
-      ],
-      catalogue: [
-        ENTERPRISE_SEARCH_OVERVIEW_PLUGIN.ID,
-        APP_SEARCH_PLUGIN.ID,
-        WORKPLACE_SEARCH_PLUGIN.ID,
-      ],
+      app: ['kibana', ...PLUGIN_IDS],
+      catalogue: PLUGIN_IDS,
       privileges: null,
     });
 
