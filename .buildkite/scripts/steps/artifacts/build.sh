1203da662bd64e3a90ad59dfc30151f7ef3ec20f
Jonathan Budzenski <jon@elastic.co>
2022-03-30 10:55:42 -0500

    [artifacts] Setup conditional release vs snapshot build (#128801)
    * [artifacts] Setup conditional release vs snapshot build
    
    * Update .buildkite/scripts/steps/artifacts/build.sh
    
    Co-authored-by: Brian Seeders <seeders@gmail.com>
    
    Co-authored-by: Brian Seeders <seeders@gmail.com>

--- .buildkite/scripts/steps/artifacts/build.sh
+++ .buildkite/scripts/steps/artifacts/build.sh
@@ -4,8 +4,16 @@ set -euo pipefail
 
 .buildkite/scripts/bootstrap.sh
 
+if [[ "${RELEASE_BUILD:-}" == "true" ]]; then
+  VERSION="$(jq -r '.version' package.json)"
+  RELEASE_ARG="--release"
+else
+  VERSION="$(jq -r '.version' package.json)-SNAPSHOT"
+  RELEASE_ARG=""
+fi
+
 echo "--- Build Kibana Distribution"
-node scripts/build --all-platforms --debug --skip-docker-cloud
+node scripts/build "$RELEASE_ARG" --all-platforms --debug --docker-cross-compile
 
 echo "--- Build dependencies report"
-node scripts/licenses_csv_report --csv=target/dependencies_report.csv
+node scripts/licenses_csv_report "--csv=target/dependencies-$VERSION.csv"
