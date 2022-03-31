aed3b2b8f6ca7ca38f0918eb8beca34108741604
spalger <spencer@elastic.co>
2022-03-31 16:47:54 -0600

    fix "exit immediately" mode

--- .buildkite/scripts/steps/lint.sh
+++ .buildkite/scripts/steps/lint.sh
@@ -11,17 +11,23 @@ checks-reporter-with-killswitch "Lint: stylelint" \
   node scripts/stylelint
 echo "stylelint ✅"
 
+# disable "Exit immediately" mode so that we can run eslint, capture it's exit code, and respond appropriately
+# after possibly commiting fixed files to the repo
+set +e;
+
 echo '--- Lint: eslint'
-# run eslint in a subshell so that we can record it's exit value and react accordingly after possibly
-# commiting changes caused by --fix
-(checks-reporter-with-killswitch "Lint: eslint" \
-  node scripts/eslint --no-cache --fix)
+checks-reporter-with-killswitch "Lint: eslint" \
+  node scripts/eslint --no-cache --fix
 
 eslint_exit=$?
+
+# re-enable "Exit immediately" mode
+set -e;
 
 check_for_changed_files 'node scripts/eslint --no-cache --fix' true
 
 if [[ "${eslint_exit}" != "0" ]]; then
   exit 1
 fi
+
 echo "eslint ✅"
