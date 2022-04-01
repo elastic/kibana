d71d1d43246bfea708ed3f521f1aea6989c01cba
spalger <spencer@elastic.co>
2022-04-01 10:46:32 -0600

    improve check so we can run this in on-merge jobs too

--- .buildkite/scripts/steps/checks/no_codeowners.sh
+++ .buildkite/scripts/steps/checks/no_codeowners.sh
@@ -4,8 +4,27 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
-if [[ "${GITHUB_PR_LABELS:-}" == *"backport"* ]]; then
-  echo "--- removing codeowners file"
-  rm .github/CODEOWNERS || true
-  check_for_changed_files 'rm .github/CODEOWNERS' true
+# logic is different for PRs
+if [ "$GITHUB_PR_TARGET_BRANCH" != "" ]; then
+  # ignore prs targetting main
+  if [ "$GITHUB_PR_TARGET_BRANCH" != "main" ]; then
+    # try to find a version matching the target branch
+    version=$(jq '.versions[] | select(.branch == env.GITHUB_PR_TARGET_BRANCH) | .version' versions.json)
+    # if the target branch represents a tracked version then run the check
+    if [ "$version" != "" ]; then
+      echo "--- removing codeowners file"
+      rm .github/CODEOWNERS || true
+      check_for_changed_files 'rm .github/CODEOWNERS' true
+    fi
+  fi
+# when we're running on-merge or something on a non-pr
+elif [ "$BUILDKITE_BRANCH" != "main" ]; then
+  # try to find a version matching the branch being built
+  version=$(jq '.versions[] | select(.branch == env.BUILDKITE_BRANCH) | .version' versions.json)
+  # if the branch being built represents a tracked version then run the check
+  if [ "$version" != "" ]; then
+    echo "--- removing codeowners file"
+    rm .github/CODEOWNERS || true
+    check_for_changed_files 'rm .github/CODEOWNERS' true
+  fi
 fi
