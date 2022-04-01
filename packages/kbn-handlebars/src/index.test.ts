dcd9a2bac3934acc67204850ba180a5a8558ac78
Thomas Watson <watson@elastic.co>
2022-04-01 08:46:10 +0200

    handlebars: remove duplicate tests

--- packages/kbn-handlebars/src/index.test.ts
+++ packages/kbn-handlebars/src/index.test.ts
@@ -14,16 +14,6 @@ it('Handlebars.create', () => {
 });
 
 describe('Handlebars.compileAST', () => {
-  const testCases: [[string, string, object, string]] = [
-    ['valid simple template', '{{value}}', { value: 42 }, '42'],
-  ];
-
-  for (const [testName, template, context, outputExpected] of testCases) {
-    test(testName, () => {
-      expectTemplate(template).withInput(context).toCompileTo(outputExpected);
-    });
-  }
-
   describe('compiler options', () => {
     it('noEscape', () => {
       expectTemplate('{{value}}').withInput({ value: '<foo>' }).toCompileTo('&lt;foo&gt;');
@@ -96,18 +86,4 @@ it('Only provide options.fn/inverse to block helpers', () => {
 
   expect(toNotHaveProperties.calls).toEqual(nonBlockTemplates.length * 2 * 2);
   expect(toHaveProperties.calls).toEqual(blockTemplates.length * 2 * 2);
-});
-
-it('Handlebars.registerHelpers', () => {
-  expectTemplate(
-    'https://elastic.co/{{lookup (split value ",") 0 }}&{{lookup (split value ",") 1 }}'
-  )
-    .withHelper('split', (...args) => {
-      const [str, splitter] = args.slice(0, -1) as [string, string];
-      if (typeof splitter !== 'string')
-        throw new Error('[split] "splitter" expected to be a string');
-      return String(str).split(splitter);
-    })
-    .withInput({ value: '47.766201,-122.257057' })
-    .toCompileTo('https://elastic.co/47.766201&-122.257057');
 });
