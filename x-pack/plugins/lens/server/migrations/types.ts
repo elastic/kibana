627e3254c379518437bb995e0ddafaa982197aee
Alexey Antonov <alexwizp@gmail.com>
2022-04-04 13:16:22 +0500

    [Step 1] Move Coloring into Shared-ux-Components

--- x-pack/plugins/lens/server/migrations/types.ts
+++ x-pack/plugins/lens/server/migrations/types.ts
@@ -5,11 +5,11 @@
  * 2.0.
  */
 
-import type { PaletteOutput } from 'src/plugins/charts/common';
-import { Filter } from '@kbn/es-query';
-import { Query } from 'src/plugins/data/public';
+import type { PaletteOutput, CustomPaletteParams } from '@kbn/coloring';
+import type { Filter } from '@kbn/es-query';
+import type { Query } from 'src/plugins/data/public';
 import type { MigrateFunctionsObject } from 'src/plugins/kibana_utils/common';
-import type { CustomPaletteParams, LayerType, PersistableFilter } from '../../common';
+import type { LayerType, PersistableFilter } from '../../common';
 
 export type CustomVisualizationMigrations = Record<string, () => MigrateFunctionsObject>;
 
