77cd7a2da55f21f4d6fb10a191ebc0ddb7821847
Yarden Shalom <yarden.shalom@elastic.co>
2022-04-04 19:28:06 +0300

    before name changing

--- x-pack/plugins/cloud_security_posture/server/routes/compliance_dashboard/get_trends.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/compliance_dashboard/get_trends.ts
@@ -9,6 +9,7 @@ import { ElasticsearchClient } from 'kibana/server';
 import { SearchResponseBody } from '@elastic/elasticsearch/lib/api/types';
 import { BENCHMARK_SCORE_INDEX_PATTERN } from '../../../common/constants';
 import type { ComplianceDashboardData, Score } from '../../../common/types';
+import { FindingsEvaluation } from '../../../common/types';
 
 /**
  * @param value value is [0, 1] range
@@ -18,44 +19,59 @@ export const roundScore = (value: number): Score => Number((value * 100).toFixed
 export const calculatePostureScore = (passed: number, failed: number): Score =>
   roundScore(passed / (passed + failed));
 
-export type TrendsESQueryResult = SearchResponseBody<{
-  _source: {
-    '@timestamp': string;
-    total_findings: number;
-    passed_findings: number;
-    failed_findings: number;
-    score_by_cluster_id: Record<
-      string,
-      {
-        total_findings: number;
-        passed_findings: number;
-        failed_findings: number;
-      }
-    >;
-  };
-}>;
+export interface TrendsESQueryResult {
+  '@timestamp': string;
+  total_findings: number;
+  passed_findings: number;
+  failed_findings: number;
+  score_by_cluster_id: Record<
+    string,
+    {
+      total_findings: number;
+      passed_findings: number;
+      failed_findings: number;
+    }
+  >;
+}
 
 export const getTrendsAggsQuery = () => ({
   index: BENCHMARK_SCORE_INDEX_PATTERN,
   size: 5,
-  sort: [
-    {
-      '@timestamp': {
-        order: 'desc',
-      },
+  sort: {
+    '@timestamp': {
+      order: 'desc',
     },
-  ],
+  },
 });
 
-export const getTrends = async (
-  esClient: ElasticsearchClient
-): Promise<ComplianceDashboardData['trend']> => {
-  const trendQueryResult = await esClient.search<unknown, TrendsESQueryResult>(
-    // @ts-ignore
-    getTrendsAggsQuery(),
-    { meta: true }
-  );
+type GetTrendsResult = Array<{
+  timestamp: string;
+  summary: FindingsEvaluation;
+  clusters: Record<string, FindingsEvaluation>;
+}>;
 
+export const getTrends = async (esClient: ElasticsearchClient): Promise<GetTrendsResult> => {
+  // @ts-ignore
+  const trendsQueryResult = await esClient.search<TrendsESQueryResult>(getTrendsAggsQuery(), {
+    meta: true,
+  });
 
-  return getTrendsFromTrendAggs(trendAggs);
+  if (!trendsQueryResult.hits.hits) throw new Error('missing trend results from score index');
+
+  const trends = trendsQueryResult.hits.hits.map((hit) => {
+    if (!hit._source) throw new Error('missing data for one or more of the trend results');
+    const data = hit._source;
+
+    return {
+      timestamp: data['@timestamp'],
+      summary: {
+        totalFindings: data.total_findings,
+        failedFindings: data.failed_findings,
+        passedFindings: data.passed_findings,
+      },
+      clusters: data.score_by_cluster_id,
+    };
+  });
+
+  return trends;
 };
