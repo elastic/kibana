68b9da5b9dc413ba7aa1660f7d1bce82ea34095a
Paul Tavares <paul.tavares@elastic.co>
2022-04-04 16:25:31 -0400

    Enable use of `useEndpointPrivileges()` from component rendered in fleet's context

---
+++ x-pack/plugins/security_solution/public/common/components/user_privileges/endpoint/security_solution_start_dependencies.tsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { useContext } from 'react';
+import { StartPlugins } from '../../../../types';
+
+/**
+ * For use with the Fleet UI extensions, where `useKibana().services.**` does not return the services
+ * provided to the Security Solution plugin.
+ */
+export const SecuritySolutionStartDependenciesContext = React.createContext<
+  undefined | StartPlugins
+>(undefined);
+
+/**
+ * Hook used in `useEndpointPrivileges()` when that hook is being invoked from outside of
+ * security solution, as is the case with UI extensions that are rendered within the Fleet
+ * pages.
+ */
+export const useSecuritySolutionStartDependencies = (): undefined | StartPlugins => {
+  return useContext(SecuritySolutionStartDependenciesContext);
+};
