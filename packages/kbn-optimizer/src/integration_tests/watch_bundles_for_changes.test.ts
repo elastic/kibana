bcc2c111834141d88964f89b13d5be646ccd4055
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2022-04-04 11:37:47 +0200

    Fix kbn-optimizer integration tests

--- packages/kbn-optimizer/src/integration_tests/watch_bundles_for_changes.test.ts
+++ packages/kbn-optimizer/src/integration_tests/watch_bundles_for_changes.test.ts
@@ -8,6 +8,7 @@
 
 import * as Rx from 'rxjs';
 import { map } from 'rxjs/operators';
+import { fakeSchedulers } from 'rxjs-marbles/jest';
 import ActualWatchpack from 'watchpack';
 import { lastValueFrom } from '@kbn/std';
 
@@ -68,66 +69,69 @@ afterEach(async () => {
 it('notifies of changes and completes once all bundles have changed', async () => {
   expect.assertions(18);
 
-  const promise = lastValueFrom(
-    watchBundlesForChanges$(bundleCacheEvent$, Date.now()).pipe(
-      map((event, i) => {
-        // each time we trigger a change event we get a 'changed detected' event
-        if (i === 0 || i === 2 || i === 4 || i === 6) {
-          expect(event).toHaveProperty('type', 'changes detected');
-          return;
-        }
+  const promise = fakeSchedulers((advance) => {
+    const _promise = lastValueFrom(
+      watchBundlesForChanges$(bundleCacheEvent$, Date.now()).pipe(
+        map((event, i) => {
+          // each time we trigger a change event we get a 'changed detected' event
+          if (i === 0 || i === 2 || i === 4 || i === 6) {
+            expect(event).toHaveProperty('type', 'changes detected');
+            return;
+          }
 
-        expect(event).toHaveProperty('type', 'changes');
-        // to teach TS what we're doing
-        if (event.type !== 'changes') {
-          return;
-        }
+          expect(event).toHaveProperty('type', 'changes');
+          // to teach TS what we're doing
+          if (event.type !== 'changes') {
+            return;
+          }
 
-        // first we change foo and bar, after 1 second that change comes though
-        if (i === 1) {
-          expect(event.bundles).toHaveLength(2);
-          const [bar, foo] = event.bundles.sort(ascending((b) => b.id));
-          expect(bar).toHaveProperty('id', 'bar');
-          expect(foo).toHaveProperty('id', 'foo');
-        }
+          // first we change foo and bar, after 1 second that change comes though
+          if (i === 1) {
+            expect(event.bundles).toHaveLength(2);
+            const [bar, foo] = event.bundles.sort(ascending((b) => b.id));
+            expect(bar).toHaveProperty('id', 'bar');
+            expect(foo).toHaveProperty('id', 'foo');
+          }
 
-        // next we change just the baz package and it's represented on its own
-        if (i === 3) {
-          expect(event.bundles).toHaveLength(1);
-          expect(event.bundles[0]).toHaveProperty('id', 'baz');
-        }
+          // next we change just the baz package and it's represented on its own
+          if (i === 3) {
+            expect(event.bundles).toHaveLength(1);
+            expect(event.bundles[0]).toHaveProperty('id', 'baz');
+          }
 
-        // finally we change box and car together
-        if (i === 5) {
-          expect(event.bundles).toHaveLength(2);
-          const [bar, foo] = event.bundles.sort(ascending((b) => b.id));
-          expect(bar).toHaveProperty('id', 'box');
-          expect(foo).toHaveProperty('id', 'car');
-        }
-      })
-    )
-  );
+          // finally we change box and car together
+          if (i === 5) {
+            expect(event.bundles).toHaveLength(2);
+            const [bar, foo] = event.bundles.sort(ascending((b) => b.id));
+            expect(bar).toHaveProperty('id', 'box');
+            expect(foo).toHaveProperty('id', 'car');
+          }
+        })
+      )
+    );
 
-  expect(MockWatchPack.mock.instances).toHaveLength(1);
-  const [watcher] = MockWatchPack.mock.instances as any as Array<jest.Mocked<ActualWatchpack>>;
-  expect(watcher.on).toHaveBeenCalledTimes(1);
-  expect(watcher.on).toHaveBeenCalledWith('change', expect.any(Function));
-  const [, changeListener] = watcher.on.mock.calls[0];
+    expect(MockWatchPack.mock.instances).toHaveLength(1);
+    const [watcher] = MockWatchPack.mock.instances as any as Array<jest.Mocked<ActualWatchpack>>;
+    expect(watcher.on).toHaveBeenCalledTimes(1);
+    expect(watcher.on).toHaveBeenCalledWith('change', expect.any(Function));
+    const [, changeListener] = watcher.on.mock.calls[0];
 
-  // foo and bar are changes without 1sec so they are batched
-  changeListener(bundleEntryPath(FOO_BUNDLE), 'modified');
-  jest.advanceTimersByTime(900);
-  changeListener(bundleEntryPath(BAR_BUNDLE), 'modified');
-  jest.advanceTimersByTime(1000);
+    // foo and bar are changes without 1sec so they are batched
+    changeListener(bundleEntryPath(FOO_BUNDLE), 'modified');
+    advance(900);
+    changeListener(bundleEntryPath(BAR_BUNDLE), 'modified');
+    advance(1000);
 
-  // baz is the only change in 1sec so it is on its own
-  changeListener(bundleEntryPath(BAZ_BUNDLE), 'modified');
-  jest.advanceTimersByTime(1000);
+    // baz is the only change in 1sec so it is on its own
+    changeListener(bundleEntryPath(BAZ_BUNDLE), 'modified');
+    advance(1000);
 
-  // finish by changing box and car
-  changeListener(bundleEntryPath(BOX_BUNDLE), 'deleted');
-  changeListener(bundleEntryPath(CAR_BUNDLE), 'deleted');
-  jest.advanceTimersByTime(1000);
+    // finish by changing box and car
+    changeListener(bundleEntryPath(BOX_BUNDLE), 'deleted');
+    changeListener(bundleEntryPath(CAR_BUNDLE), 'deleted');
+    advance(1000);
 
+    return _promise;
+  })();
   await expect(promise).resolves.toEqual(undefined);
 });
