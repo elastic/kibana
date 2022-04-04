fc7127d08e45bfa3b0ec1744e4ac2333d88bed45
Jonathan Budzenski <jon@elastic.co>
2022-04-04 17:08:59 -0500

    [build] Add flag for using a local Kibana artifact in the Docker context (#129375) (#129409)
    * [build] Add flag for using a local Kibana artifact in the Docker context
    
    Currently Docker contexts always install a Kibana distribution from  an
    http endpoint.  These endpoints are only available after a release has
    been published, making pre-release testing difficult.
    
    This adds a flag to use a local Kibana artifact, with the intention of
    verifying the Docker contexts with release artifacts prior to publishing
    the release.
    
    * fix types

--- src/dev/build/build_distributables.ts
+++ src/dev/build/build_distributables.ts
@@ -13,6 +13,7 @@ import * as Tasks from './tasks';
 
 export interface BuildOptions {
   isRelease: boolean;
+  dockerContextUseLocalArtifact: boolean | null;
   dockerCrossCompile: boolean;
   downloadFreshNode: boolean;
   initialize: boolean;
