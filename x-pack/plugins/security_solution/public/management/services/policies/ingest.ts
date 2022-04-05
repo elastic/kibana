92831591184f176fcb39d824f3d3c1f3a81402e0
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-04-05 13:32:34 -0400

    [Security Solution][Admin][Policy] Clean up Policy list (#129139) (#129492)
    (cherry picked from commit a9e6f0aed0c03d55839dda611bdd75b133f24435)
    
    Co-authored-by: Candace Park <56409205+parkiino@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/management/services/policies/ingest.ts
+++ x-pack/plugins/security_solution/public/management/services/policies/ingest.ts
@@ -14,9 +14,9 @@ import {
   GetPackagesResponse,
   GetAgentPoliciesRequest,
   GetAgentPoliciesResponse,
-} from '../../../../../../../fleet/common';
-import { GetPolicyResponse, UpdatePolicyResponse } from '../../types';
-import { NewPolicyData } from '../../../../../../common/endpoint/types';
+} from '../../../../../fleet/common';
+import { NewPolicyData } from '../../../../common/endpoint/types';
+import { GetPolicyResponse, UpdatePolicyResponse } from '../../pages/policy/types';
 
 const INGEST_API_ROOT = `/api/fleet`;
 export const INGEST_API_PACKAGE_POLICIES = `${INGEST_API_ROOT}/package_policies`;
