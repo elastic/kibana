333c81d1f5a5a9ff99262e56df3b6869f548785a
Clint Andrew Hall <clint.hall@elastic.co>
2022-04-04 21:56:07 -0500

    Addressing feedback

--- packages/kbn-ambient-ui-types/index.d.ts
+++ packages/kbn-ambient-ui-types/index.d.ts
@@ -6,8 +6,26 @@
  * Side Public License, v 1.
  */
 
+declare module '*.html' {
+  const template: string;
+  // eslint-disable-next-line import/no-default-export
+  export default template;
+}
+
+declare module '*.png' {
+  const content: string;
+  // eslint-disable-next-line import/no-default-export
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: string;
+  // eslint-disable-next-line import/no-default-export
+  export default content;
+}
+
 declare module '*.mdx' {
   let MDXComponent: (props: any) => JSX.Element;
-
+  // eslint-disable-next-line import/no-default-export
   export default MDXComponent;
 }
