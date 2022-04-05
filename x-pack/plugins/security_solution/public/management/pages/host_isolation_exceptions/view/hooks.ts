e7518f571c615b831a02d7e9e77048c0d4737fb3
Paul Tavares <paul.tavares@elastic.co>
2022-04-05 19:22:38 -0400

    Refactor `useCanSeeHostIsolationExceptionsMenu()` hook to use generic `useSummaryArtifact()` hook

--- x-pack/plugins/security_solution/public/management/pages/host_isolation_exceptions/view/hooks.ts
+++ x-pack/plugins/security_solution/public/management/pages/host_isolation_exceptions/view/hooks.ts
@@ -4,10 +4,11 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useEndpointPrivileges } from '../../../../common/components/user_privileges/endpoint';
 import { useHttp } from '../../../../common/lib/kibana/hooks';
-import { getHostIsolationExceptionSummary } from '../service';
+import { useSummaryArtifact } from '../../../hooks/artifacts';
+import { HostIsolationExceptionsApiClient } from '../host_isolation_exceptions_api_client';
 
 /**
  * Checks if the current user should be able to see the host isolation exceptions
@@ -16,27 +17,23 @@ import { getHostIsolationExceptionSummary } from '../service';
 export function useCanSeeHostIsolationExceptionsMenu(): boolean {
   const http = useHttp();
   const privileges = useEndpointPrivileges();
+  const {
+    data: summary,
+    isError: isApiError,
+    refetch: checkIfHasExceptions,
+  } = useSummaryArtifact(HostIsolationExceptionsApiClient.getInstance(http), undefined, undefined, {
+    enabled: false,
+  });
 
-  const [canSeeMenu, setCanSeeMenu] = useState(privileges.canIsolateHost);
+  const canSeeMenu = useMemo(() => {
+    return privileges.canIsolateHost || (!isApiError && Boolean(summary?.total));
+  }, [isApiError, privileges.canIsolateHost, summary?.total]);
 
   useEffect(() => {
-    async function checkIfHasExceptions() {
-      try {
-        const summary = await getHostIsolationExceptionSummary(http);
-        if (summary?.total > 0) {
-          setCanSeeMenu(true);
-        }
-      } catch (error) {
-        // an error will ocurr if the exception list does not exist
-        setCanSeeMenu(false);
-      }
-    }
-    if (!privileges.canIsolateHost) {
+    if (!canSeeMenu && !privileges.loading) {
       checkIfHasExceptions();
-    } else {
-      setCanSeeMenu(true);
     }
-  }, [http, privileges.canIsolateHost]);
+  }, [canSeeMenu, checkIfHasExceptions, http, privileges.canIsolateHost, privileges.loading]);
 
   return canSeeMenu;
 }
