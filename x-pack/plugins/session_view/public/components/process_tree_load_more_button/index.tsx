ab66291f47c6974852b747eeeaf546acf970b47b
mitodrummer <karlgodard@elastic.co>
2022-04-06 15:08:01 -0700

    load more/previous buttons style now matches figma design. also added remaining events indication as per design
    updated test to look for total prop

---
+++ x-pack/plugins/session_view/public/components/process_tree_load_more_button/index.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { EuiButtonEmpty } from '@elastic/eui';
+import { useStyles } from './styles';
+
+export interface ProcessTreeLoadMoreButtonDeps {
+  onClick: () => void;
+  text: string;
+  isFetching: boolean;
+  eventsRemaining: number;
+}
+
+export const ProcessTreeLoadMoreButton = ({
+  onClick,
+  text,
+  isFetching,
+  eventsRemaining,
+}: ProcessTreeLoadMoreButtonDeps) => {
+  const styles = useStyles();
+
+  return (
+    <div css={styles.wrapper}>
+      <EuiButtonEmpty size="xs" iconType="arrowDown" onClick={onClick} isLoading={isFetching}>
+        {text}
+        {eventsRemaining !== 0 && (
+          <FormattedMessage
+            id="xpack.sessionView.processTreeLoadMoreButton"
+            defaultMessage="({count} left)"
+            values={{ count: eventsRemaining }}
+          />
+        )}
+      </EuiButtonEmpty>
+      <span css={styles.dottedLine} />
+    </div>
+  );
+};
