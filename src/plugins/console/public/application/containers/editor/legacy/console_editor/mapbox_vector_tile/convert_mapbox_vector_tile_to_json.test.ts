65ef4479b8871d674eaa2bdf23bdcc1ece313936
Maksim Kovalev <maksim_kovalev@epam.com>
2022-04-06 21:23:40 +0300

    128534 - refactoring for test file

--- src/plugins/console/public/application/containers/editor/legacy/console_editor/mapbox_vector_tile/convert_mapbox_vector_tile_to_json.test.ts
+++ src/plugins/console/public/application/containers/editor/legacy/console_editor/mapbox_vector_tile/convert_mapbox_vector_tile_to_json.test.ts
@@ -5,19 +5,19 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import fs from 'fs';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { VectorTile } from '@mapbox/vector-tile';
 import Protobuf from 'pbf';
 import { convertMapboxVectorTileToJson } from './convert_mapbox_vector_tile_to_json';
+
+const PATH_TO_PBF = resolve(__dirname, './response.pbf'); // Query sample "GET kibana_sample_data_logs/_mvt/geo.coordinates/8/47/98"
 
 describe('Convert mapbox vector tile to json', () => {
   let vectorTile: VectorTile;
 
   beforeAll(() => {
-    // Query sample "get kibana_sample_data_logs/_mvt/geo.coordinates/8/47/98"
-    const response = fs.readFileSync(
-      'src/plugins/console/public/application/containers/editor/legacy/console_editor/mapbox_vector_tile/response.pbf'
-    );
+    const response = readFileSync(PATH_TO_PBF);
     vectorTile = new VectorTile(new Protobuf(response));
   });
 
@@ -29,10 +29,10 @@ describe('Convert mapbox vector tile to json', () => {
   });
 
   it('response should have [meta, aggs, hits] layers', () => {
-    const processed = JSON.parse(convertMapboxVectorTileToJson(vectorTile));
+    const vectorTileJson = JSON.parse(convertMapboxVectorTileToJson(vectorTile));
 
-    expect(processed).toHaveProperty('meta');
-    expect(processed).toHaveProperty('aggs');
-    expect(processed).toHaveProperty('hits');
+    expect(vectorTileJson).toHaveProperty('meta');
+    expect(vectorTileJson).toHaveProperty('aggs');
+    expect(vectorTileJson).toHaveProperty('hits');
   });
 });
