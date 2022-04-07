2bedbea21fce4975f02c1eed4057bc150609d5d7
DziyanaDzeraviankina <Dziyana_Dzeraviankina@epam.com>
2022-04-07 16:32:06 +0300

    Fix export of MultiFieldKey

--- src/plugins/data/common/search/aggs/buckets/index.ts
+++ src/plugins/data/common/search/aggs/buckets/index.ts
@@ -38,7 +38,7 @@ export * from './significant_text_fn';
 export * from './significant_text';
 export * from './terms_fn';
 export * from './terms';
-export * from './multi_field_key';
+export { MultiFieldKey } from './multi_field_key';
 export * from './multi_terms_fn';
 export * from './multi_terms';
 export * from './rare_terms_fn';
