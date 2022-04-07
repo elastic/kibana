f3ac9dba797e59ebb52e99c0b5fd6060d535067f
criamico <mariacristina.amico@elastic.co>
2022-04-07 18:17:09 +0200

    Fix download link

--- x-pack/plugins/fleet/public/components/agent_enrollment_flyout/installation_message.tsx
+++ x-pack/plugins/fleet/public/components/agent_enrollment_flyout/installation_message.tsx
@@ -4,11 +4,24 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 import { EuiText, EuiLink } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 
+import semverMajor from 'semver/functions/major';
+import semverMinor from 'semver/functions/minor';
+import semverPatch from 'semver/functions/patch';
+
+import { useKibanaVersion } from '../../hooks';
+
 export const InstallationMessage: React.FunctionComponent = () => {
+  const kibanaVersion = useKibanaVersion();
+  const kibanaVersionURLString = useMemo(
+    () =>
+      `${semverMajor(kibanaVersion)}-${semverMinor(kibanaVersion)}-${semverPatch(kibanaVersion)}`,
+    [kibanaVersion]
+  );
+
   return (
     <EuiText>
       <FormattedMessage
@@ -16,7 +29,11 @@ export const InstallationMessage: React.FunctionComponent = () => {
         defaultMessage="Select the appropriate platform and run commands to install, enroll, and start Elastic Agent. Reuse commands to set up agents on more than one host. For aarch64, see our {downloadLink}. For additional guidance, see our {installationLink}."
         values={{
           downloadLink: (
-            <EuiLink target="_blank" external href="https://ela.st/download-elastic-agent">
+            <EuiLink
+              target="_blank"
+              external
+              href={`https://www.elastic.co/downloads/past-releases/elastic-agent-${kibanaVersionURLString}`}
+            >
               <FormattedMessage
                 id="xpack.fleet.enrollmentInstructions.downloadLink"
                 defaultMessage="downloads page"
