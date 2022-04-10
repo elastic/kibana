37119d5ce62812d9cd28d52dec085eb0eabbd355
oatkiller <robert.austin@elastic.co>
2022-04-10 17:47:38 -0400

    Enhance Event Fields Browser performance

--- x-pack/plugins/security_solution/public/common/components/event_details/event_fields_browser.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/event_fields_browser.tsx
@@ -279,6 +279,7 @@ export const EventFieldsBrowser = React.memo<Props>(
         <StyledEuiInMemoryTable
           className={EVENT_FIELDS_TABLE_CLASS_NAME}
           items={items}
+          itemId={(item: TimelineEventsDetailsItem) => item.field}
           columns={columns}
           pagination={false}
           rowProps={onSetRowProps}
