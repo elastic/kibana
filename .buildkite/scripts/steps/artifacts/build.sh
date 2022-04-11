2cbd49c4e312f319a2f0dbe4a0aff2eb23d18ab8
Jonathan Budzenski <jon@elastic.co>
2022-04-11 17:10:09 -0500

    remove duplicate upload

--- .buildkite/scripts/steps/artifacts/build.sh
+++ .buildkite/scripts/steps/artifacts/build.sh
@@ -21,7 +21,6 @@ node scripts/licenses_csv_report "--csv=target/dependencies-$VERSION.csv"
 echo "--- Extract default i18n messages"
 mkdir -p target/i18n
 node scripts/i18n_extract
-buildkite-agent artifact upload "target/i18n/en.json"
 
 # Release verification
 if [[ "${RELEASE_BUILD:-}" == "true" ]]; then
