8ea618d7237129c679358a79399217c784e1e3f7
najmieh <najmieh.safarabadi@protonmail.com>
2022-04-13 00:41:16 +0430

    Edited the validation message

--- x-pack/plugins/fleet/public/applications/fleet/sections/settings/components/fleet_server_hosts_flyout/use_fleet_server_host_form.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/settings/components/fleet_server_hosts_flyout/use_fleet_server_host_form.tsx
@@ -73,21 +73,20 @@ function validateFleetServerHosts(value: string[]) {
       },
     ];
   }
-  
-  if (!value) {
-    return [
-      {
-        message: i18n.translate('xpack.fleet.settings.fleetServerHostsEmptyError', {
-          defaultMessage: 'Host URL is required',
-        }),
-      },
-    ];
-  }
 
   const res: Array<{ message: string; index: number }> = [];
   const hostIndexes: { [key: string]: number[] } = {};
   value.forEach((val, idx) => {
-    if (!val.match(URL_REGEX)) {
+     if(val === ''){
+       return [
+         {
+           message: i18n.translate('xpack.fleet.settings.fleetServerHostsEmptyError', {
+             defaultMessage: 'Host URL is required',
+           }),
+         },
+       ];
+     }
+    else if (!val.match(URL_REGEX)) {
       res.push({
         message: i18n.translate('xpack.fleet.settings.fleetServerHostsError', {
           defaultMessage: 'Invalid URL',
