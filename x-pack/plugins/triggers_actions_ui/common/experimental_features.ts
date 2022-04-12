061c34343b08f43d98939c484623b57f43aa1fa1
Esteban Beltran <esteban.beltran@elastic.co>
2022-04-12 15:48:58 +0200

    Remove wrong commit

--- x-pack/plugins/triggers_actions_ui/common/experimental_features.ts
+++ x-pack/plugins/triggers_actions_ui/common/experimental_features.ts
@@ -13,7 +13,7 @@ export type ExperimentalFeatures = typeof allowedExperimentalValues;
  */
 export const allowedExperimentalValues = Object.freeze({
   rulesListDatagrid: true,
-  internalAlertsTable: true,
+  internalAlertsTable: false,
   rulesDetailLogs: true,
 });
 
