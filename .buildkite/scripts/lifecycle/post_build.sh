8b607c151a0ff2fd81edb8379f83f13922406378
Brian Seeders <brian.seeders@elastic.co>
2022-04-12 13:09:33 -0400

    Remove coveralls hook

--- .buildkite/scripts/lifecycle/post_build.sh
+++ .buildkite/scripts/lifecycle/post_build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 BUILD_SUCCESSFUL=$(node "$(dirname "${0}")/build_status.js")
 export BUILD_SUCCESSFUL
 
-if [[ "$(buildkite-agent meta-data get "did-upload-coveralls" --default '')" == 'true' ]]; then
-  curl -k "https://coveralls.io/webhook?repo_token=$COVERALLS_REPO_TOKEN" -d "payload[build_num]=$BUILDKITE_BUILD_ID&payload[status]=done"
+if [[ "$(buildkite-agent meta-data get "did-upload-codecov" --default '')" == 'true' ]]; then
+  echo 'TODO anything for codecov?'
 fi
 
 if [[ "${GITHUB_BUILD_COMMIT_STATUS_ENABLED:-}" != "true" ]]; then
