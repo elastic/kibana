33f9c6c87b4e760bf170f4ea2885862a4c623345
najmiehsa <98463228+najmiehsa@users.noreply.github.com>
2022-04-13 13:18:31 +0430

    Made changes following the review

--- x-pack/plugins/fleet/public/applications/fleet/sections/settings/components/fleet_server_hosts_flyout/use_fleet_server_host_form.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/settings/components/fleet_server_hosts_flyout/use_fleet_server_host_form.tsx
@@ -77,14 +77,14 @@ function validateFleetServerHosts(value: string[]) {
   const res: Array<{ message: string; index: number }> = [];
   const hostIndexes: { [key: string]: number[] } = {};
   value.forEach((val, idx) => {
-     if(val === ''){
-       return [
-         {
+     if(!val){
+       res.push({
            message: i18n.translate('xpack.fleet.settings.fleetServerHostsEmptyError', {
              defaultMessage: 'Host URL is required',
            }),
+           index: idx
          },
-       ];
+       );
      }
     else if (!val.match(URL_REGEX)) {
       res.push({
