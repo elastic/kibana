5afeec9604c1cafb0fa349452d2e436b9b45aefb
Maksim Kovalev <maksim_kovalev@epam.com>
2022-04-13 18:26:12 +0300

    128534 - tests was updated; New query example added

--- src/plugins/console/public/application/containers/editor/legacy/console_editor/mapbox_vector_tile/convert_mapbox_vector_tile_to_json.test.ts
+++ src/plugins/console/public/application/containers/editor/legacy/console_editor/mapbox_vector_tile/convert_mapbox_vector_tile_to_json.test.ts
@@ -11,7 +11,77 @@ import { VectorTile } from '@mapbox/vector-tile';
 import Protobuf from 'pbf';
 import { convertMapboxVectorTileToJson } from './convert_mapbox_vector_tile_to_json';
 
-const PATH_TO_PBF = resolve(__dirname, './response.pbf'); // Query sample "GET kibana_sample_data_logs/_mvt/geo.coordinates/8/47/98"
+const PATH_TO_PBF = resolve(__dirname, './response.pbf'); // Query sample "GET kibana_sample_data_logs/_mvt/geo.coordinates/8/23/63"
+
+const MOCK_JSON = JSON.parse(`
+{
+	"meta": [
+		{
+			"geometry": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[0,4096],
+						[4096,4096],
+						[4096,0],
+						[0,0],
+						[0,4096]
+					]
+				]
+			},
+			"properties": {
+				"_shards.failed": 0,
+				"_shards.skipped": 0,
+				"_shards.successful": 1,
+				"_shards.total": 1,
+				"aggregations._count.avg": 1,
+				"aggregations._count.count": 1,
+				"aggregations._count.max": 1,
+				"aggregations._count.min": 1,
+				"aggregations._count.sum": 1,
+				"hits.total.relation": "eq",
+				"hits.total.value": 1,
+				"timed_out": false,
+				"took": 1
+			}
+		}
+	],
+	"hits": [
+		{
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					3619,
+					334
+				]
+			},
+			"properties": {
+				"_id": "vFVzI4ABgP1gvXfjt0mI",
+				"_index": "kibana_sample_data_logs"
+			}
+		}
+	],
+	"aggs": [
+		{
+			"geometry": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[3616,336],
+						[3632,336],
+						[3632,320],
+						[3616,320],
+						[3616,336]
+					]
+				]
+			},
+			"properties": {
+				"_key": "16/6114/16148",
+				"_count": 1
+			}
+		}
+	]
+}`);
 
 describe('Convert mapbox vector tile to json', () => {
   let vectorTile: VectorTile;
@@ -30,9 +100,6 @@ describe('Convert mapbox vector tile to json', () => {
 
   it('response should have [meta, aggs, hits] layers', () => {
     const vectorTileJson = JSON.parse(convertMapboxVectorTileToJson(vectorTile));
-
-    expect(vectorTileJson).toHaveProperty('meta');
-    expect(vectorTileJson).toHaveProperty('aggs');
-    expect(vectorTileJson).toHaveProperty('hits');
+    expect(vectorTileJson).toEqual(MOCK_JSON);
   });
 });
