c8c47118165813046f48a2aaa8e78cd5d7697c05
Julia Rechkunova <julia.rechkunova@elastic.co>
2022-04-13 13:37:51 +0200

    [Discover] Allow to view sample data in Discover

---
+++ src/plugins/discover/server/sample_data/register_sample_data.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { i18n } from '@kbn/i18n';
+import type { SampleDataRegistrySetup } from '../../../home/server';
+import { APP_ICON } from '../../common';
+
+// TODO: update
+const getSampleSearchPath = (objId: string) => `/app/discover#/view/${encodeURIComponent(objId)}`;
+
+export function registerSampleData(sampleDataRegistry: SampleDataRegistrySetup) {
+  const linkLabel = i18n.translate('discover.sampleData.viewLinkLabel', {
+    defaultMessage: 'Discover',
+  });
+  const { addAppLinksToSampleDataset, getSampleDatasets } = sampleDataRegistry;
+  const sampleDatasets = getSampleDatasets();
+
+  sampleDatasets.forEach((sampleDataset) => {
+    const sampleSavedSearchObject = sampleDataset.savedObjects.find(
+      (object) => object.type === 'search'
+    );
+
+    if (sampleSavedSearchObject) {
+      addAppLinksToSampleDataset(sampleDataset.id, [
+        {
+          sampleObject: sampleSavedSearchObject,
+          getPath: getSampleSearchPath,
+          label: linkLabel,
+          icon: APP_ICON,
+        },
+      ]);
+    }
+  });
+}
