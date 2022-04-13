126272072a544176600f4cf3377d145803d87265
Christos Nasikas <christos.nasikas@elastic.co>
2022-04-13 16:01:10 +0300

    Create api

--- x-pack/plugins/cases/public/containers/api.ts
+++ x-pack/plugins/cases/public/containers/api.ts
@@ -9,6 +9,7 @@ import { omit } from 'lodash';
 
 import { StatusAll, ResolvedCase } from '../../common/ui/types';
 import {
+  BulkCreateCommentRequest,
   CasePatchRequest,
   CasePostRequest,
   CaseResponse,
@@ -32,6 +33,8 @@ import {
   CASE_STATUS_URL,
   CASE_TAGS_URL,
   CASES_URL,
+  CASES_INTERNAL_URL,
+  INTERNAL_BULK_CREATE_ATTACHMENTS_URL,
 } from '../../common/constants';
 import { getAllConnectorTypesUrl } from '../../common/utils/connectors_api';
 
@@ -307,4 +310,20 @@ export const getActionLicense = async (signal: AbortSignal): Promise<ActionLicen
   );
 
   return convertArrayToCamelCase(response) as ActionLicense[];
+};
+
+export const bulkCreateAttachments = async (
+  attachments: BulkCreateCommentRequest,
+  caseId: string,
+  signal: AbortSignal
+): Promise<Case> => {
+  const response = await KibanaServices.get().http.fetch<CaseResponse>(
+    INTERNAL_BULK_CREATE_ATTACHMENTS_URL.replace('{case_id}', caseId),
+    {
+      method: 'POST',
+      body: JSON.stringify(attachments),
+      signal,
+    }
+  );
+  return convertToCamelCase<CaseResponse, Case>(decodeCaseResponse(response));
 };
