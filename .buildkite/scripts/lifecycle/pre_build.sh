385dcad94b58838c9bf995074273695004355679
Brian Seeders <brian.seeders@elastic.co>
2022-04-14 14:30:28 -0400

    Fix URL

--- .buildkite/scripts/lifecycle/pre_build.sh
+++ .buildkite/scripts/lifecycle/pre_build.sh
@@ -17,6 +17,6 @@ node "$(dirname "${0}")/ci_stats_start.js"
 # Otherwise, the manifest could change if a step is running around the time that a new one is promoted
 if [[ ! "${ES_SNAPSHOT_MANIFEST:-}" ]]; then
   BUCKET=$(curl -s "https://storage.googleapis.com/kibana-ci-es-snapshots-daily/$(cat package.json | jq -r .version)/manifest-latest-verified.json" | jq -r .bucket)
-  ES_SNAPSHOT_MANIFEST_DEFAULT="https://storage.googleapis.com/$BUCKET/manifest-latest-verified.json"
+  ES_SNAPSHOT_MANIFEST_DEFAULT="https://storage.googleapis.com/$BUCKET/manifest.json"
   buildkite-agent meta-data set ES_SNAPSHOT_MANIFEST_DEFAULT "$ES_SNAPSHOT_MANIFEST_DEFAULT"
 fi
