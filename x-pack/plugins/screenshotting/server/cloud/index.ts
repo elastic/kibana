03dbd84f47cc1e8748520c072b294c53fb18b14a
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-04-14 11:30:51 +0200

    removed logger being passed to system check function

--- x-pack/plugins/screenshotting/server/cloud/index.ts
+++ x-pack/plugins/screenshotting/server/cloud/index.ts
@@ -16,7 +16,7 @@ const MIN_CLOUD_OS_MEM_BYTES: number = MIN_CLOUD_OS_MEM_GB * Math.pow(1024, 3);
  * If we are on Cloud we need to ensure that we have sufficient memory available,
  * if we do not Chromium cannot start. See {@link MIN_CLOUD_OS_MEM_BYTES}.
  */
-export function systemHasInsufficientMemory(logger: Logger, cloud?: CloudSetup): boolean {
+export function systemHasInsufficientMemory(cloud?: CloudSetup): boolean {
   return (
     Boolean(cloud?.isCloudEnabled || cloud?.deploymentId) && os.totalmem() < MIN_CLOUD_OS_MEM_BYTES
   );
