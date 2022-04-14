6732cd435284f23cdebaaa5aab64c49dddb30693
Mike Cote <michel.cote@elastic.co>
2022-04-14 14:35:52 -0400

    Rename telemetry field terminology

--- x-pack/plugins/alerting/server/usage/task.ts
+++ x-pack/plugins/alerting/server/usage/task.ts
@@ -144,9 +144,9 @@ export function telemetryTaskRunner(
                   avg_total_search_duration_per_day: dailyExecutionCounts.avgTotalSearchDuration,
                   avg_total_search_duration_by_type_per_day:
                     dailyExecutionCounts.avgTotalSearchDurationByType,
-                  percentile_num_scheduled_actions_per_day:
+                  percentile_num_generated_actions_per_day:
                     dailyExecutionCounts.scheduledActionsPercentiles,
-                  percentile_num_scheduled_actions_by_type_per_day:
+                  percentile_num_generated_actions_by_type_per_day:
                     dailyExecutionCounts.scheduledActionsPercentilesByType,
                 },
                 runAt: getNextMidnight(),
