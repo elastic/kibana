b181b5e75a8b46b23bcdea89ec7630d239128330
Jonathan Budzenski <jon@elastic.co>
2022-04-14 09:10:00 -0500

    fix

--- .buildkite/scripts/steps/package_testing/test.sh
+++ .buildkite/scripts/steps/package_testing/test.sh
@@ -29,11 +29,13 @@ node scripts/es snapshot \
   --license=trial &
 while ! timeout 1 bash -c "echo > /dev/tcp/localhost/9200"; do sleep 30; done
 
+trap "echo '--- Kibana logs'" EXIT
 if [[ "$TEST_PACKAGE" == "deb" ]] || [[ "$TEST_PACKAGE" == "rpm" ]]; then
-  trap "vagrant ssh $TEST_PACKAGE --command 'sudo cat /var/log/kibana/kibana.log'" EXIT
+  trap "vagrant ssh $TEST_PACKAGE -t -c 'sudo cat /var/log/kibana/kibana.log'" EXIT
 elif [[ "$TEST_PACKAGE" == "docker" ]]; then
-  trap "vagrant ssh $TEST_PACKAGE --command 'sudo docker logs kibana'" EXIT
+  trap "vagrant ssh $TEST_PACKAGE -t -c 'sudo docker logs kibana'" EXIT
 fi
+
 vagrant provision "$TEST_PACKAGE"
 
 export TEST_BROWSER_HEADLESS=1
