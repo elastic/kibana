0663fcb0d701982ecf3e42a610346eb371e78cd4
Jonathan Budzenski <jon@elastic.co>
2022-04-14 16:44:51 -0500

    [artifacts] Skip publish if untracked branch

--- .buildkite/scripts/steps/artifacts/publish.sh
+++ .buildkite/scripts/steps/artifacts/publish.sh
@@ -51,30 +51,36 @@ trap 'docker logout docker.elastic.co' EXIT
 docker pull docker.elastic.co/infra/release-manager:latest
 
 echo "--- Publish artifacts"
-export VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-role-id)"
-export VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-secret-id)"
-export VAULT_ADDR="https://secrets.elastic.co:8200"
-docker run --rm \
-  --name release-manager \
-  -e VAULT_ADDR \
-  -e VAULT_ROLE_ID \
-  -e VAULT_SECRET_ID \
-  --mount type=bind,readonly=false,src="$PWD/target",target=/artifacts/target \
-  docker.elastic.co/infra/release-manager:latest \
-    cli collect \
-      --project kibana \
-      --branch "$KIBANA_BASE_BRANCH" \
-      --commit "$GIT_COMMIT" \
-      --workflow "$WORKFLOW" \
-      --version "$BASE_VERSION" \
-      --qualifier "$VERSION_QUALIFIER" \
-      --artifact-set main
+TRACKED_BRANCH=$(jq '.versions | map(.branch == "'$BUILDKITE_BRANCH'") | any' versions.json)
+if [[ "$TRACKED_BRANCH" == "true" ]]; then
+  export VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-role-id)"
+  export VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-secret-id)"
+  export VAULT_ADDR="https://secrets.elastic.co:8200"
+  docker run --rm \
+    --name release-manager \
+    -e VAULT_ADDR \
+    -e VAULT_ROLE_ID \
+    -e VAULT_SECRET_ID \
+    --mount type=bind,readonly=false,src="$PWD/target",target=/artifacts/target \
+    docker.elastic.co/infra/release-manager:latest \
+      cli collect \
+        --project kibana \
+        --branch "$KIBANA_BASE_BRANCH" \
+        --commit "$GIT_COMMIT" \
+        --workflow "$WORKFLOW" \
+        --version "$BASE_VERSION" \
+        --qualifier "$VERSION_QUALIFIER" \
+        --artifact-set main
 
-ARTIFACTS_SUBDOMAIN="artifacts-$WORKFLOW"
-ARTIFACTS_SUMMARY=$(curl -s "https://$ARTIFACTS_SUBDOMAIN.elastic.co/kibana/latest/$FULL_VERSION.json" | jq -re '.summary_url')
+  ARTIFACTS_SUBDOMAIN="artifacts-$WORKFLOW"
+  ARTIFACTS_SUMMARY=$(curl -s "https://$ARTIFACTS_SUBDOMAIN.elastic.co/kibana/latest/$FULL_VERSION.json" | jq -re '.summary_url')
 
-cat << EOF | buildkite-agent annotate --style "info" --context artifacts-summary
+  cat << EOF | buildkite-agent annotate --style "info" --context artifacts-summary
   ### Artifacts Summary
 
   $ARTIFACTS_SUMMARY
 EOF
+
+else
+  echo "Skipping publish for untracked branch $BUILDKITE_BRANCH"
+fi
