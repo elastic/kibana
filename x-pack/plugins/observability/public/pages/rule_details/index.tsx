92d674c9228f7716e1b83bd242ff94f3fa4c7417
Faisal Kanout <faisal.kanout@elastic.co>
2022-04-14 23:04:09 +0200

    Add useBreadcrumbs

--- x-pack/plugins/observability/public/pages/rule_details/index.tsx
+++ x-pack/plugins/observability/public/pages/rule_details/index.tsx
@@ -7,15 +7,34 @@
 
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { i18n } from '@kbn/i18n';
+import { useBreadcrumbs } from '../../hooks/use_breadcrumbs';
 import { usePluginContext } from '../../hooks/use_plugin_context';
-
+import { useKibana } from '../../utils/kibana_react';
+import { RULES_BREADCRUMB_TEXT } from '../rules/translations';
 interface RuleDetailsPathParams {
   ruleId: string;
 }
 export function RuleDetailsPage() {
   const { ObservabilityPageTemplate } = usePluginContext();
+  const { http } = useKibana().services;
 
   const { ruleId } = useParams<RuleDetailsPathParams>();
+  useBreadcrumbs([
+    {
+      text: i18n.translate('xpack.observability.breadcrumbs.alertsLinkText', {
+        defaultMessage: 'Alerts',
+      }),
+      href: http.basePath.prepend('/app/observability/alerts/'),
+    },
+    {
+      href: http.basePath.prepend('/app/observability/alerts/rules'),
+      text: RULES_BREADCRUMB_TEXT,
+    },
+    {
+      text: ruleId, // replace the ruleId with rule name
+    },
+  ]);
 
-  return <ObservabilityPageTemplate>{ruleId}</ObservabilityPageTemplate>;
+  return <ObservabilityPageTemplate />;
 }
