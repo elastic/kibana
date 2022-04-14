2e3cf59cd4d18a9163a7c112ebc3447619b80806
Brian Seeders <brian.seeders@elastic.co>
2022-04-14 13:54:24 -0400

    [CI] Resolve ES Snapshot manifest url at the beginning of the build

--- .buildkite/scripts/lifecycle/pre_build.sh
+++ .buildkite/scripts/lifecycle/pre_build.sh
@@ -12,3 +12,11 @@ export CI_STATS_TOKEN="$(retry 5 5 vault read -field=api_token secret/kibana-iss
 export CI_STATS_HOST="$(retry 5 5 vault read -field=api_host secret/kibana-issues/dev/kibana_ci_stats)"
 
 node "$(dirname "${0}")/ci_stats_start.js"
+
+# We resolve the latest manifest URL at the beginning of the build to ensure that all steps in the build will use the same manifest
+# Otherwise, the manifest could change if a step is running around the time that a new one is promoted
+if [[ ! "${ES_SNAPSHOT_MANIFEST:-}" ]]; then
+  BUCKET=$(curl -s "https://storage.googleapis.com/kibana-ci-es-snapshots-daily/$(cat package.json | jq -r .version)/manifest-latest-verified.json" | jq -r .bucket)
+  ES_SNAPSHOT_MANIFEST_DEFAULT="https://storage.googleapis.com/$BUCKET/manifest-latest-verified.json"
+  buildkite-agent meta-data set ES_SNAPSHOT_MANIFEST_DEFAULT "$ES_SNAPSHOT_MANIFEST_DEFAULT"
+fi
