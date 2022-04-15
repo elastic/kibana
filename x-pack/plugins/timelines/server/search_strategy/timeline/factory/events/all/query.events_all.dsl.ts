2f3a9d73706f46864d91cb879f003cf82dd73156
Kevin Qualters <kevin.qualters@elastic.co>
2022-04-15 12:05:22 -0400

    Use keyword for unmapped non-date fields

--- x-pack/plugins/timelines/server/search_strategy/timeline/factory/events/all/query.events_all.dsl.ts
+++ x-pack/plugins/timelines/server/search_strategy/timeline/factory/events/all/query.events_all.dsl.ts
@@ -58,7 +58,7 @@ export const buildTimelineEventsAllQuery = ({
       return {
         [field]: {
           order: item.direction,
-          unmapped_type: 'keyword',
+          unmapped_type: item.type === 'date' ? 'date' : 'keyword',
         },
       };
     });
