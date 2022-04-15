3627b866a3dc2ffe979a928acd5d96455703deb2
Michael Dokolin <mikhail.dokolin@elastic.co>
2022-04-15 08:29:05 +0200

    [Screenshotting] Documentation (#129794)
    * Provide defaults for the screenshotting options to make usage closer to the zero-conf
    * Add screenshotting example integration
    * Add integration tests to cover screenshotting example
    * Add screenshotting plugin readme
    * Add tutorial to the developer guide

---
+++ x-pack/examples/screenshotting_example/server/plugin.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { lastValueFrom } from 'rxjs';
+import { schema } from '@kbn/config-schema';
+import type { CoreSetup, Plugin } from 'src/core/server';
+import type { ScreenshottingStart } from '../../../plugins/screenshotting/server';
+import { API_ENDPOINT, ScreenshottingExpressionResponse } from '../common';
+
+interface StartDeps {
+  screenshotting: ScreenshottingStart;
+}
+
+export class ScreenshottingExamplePlugin implements Plugin<void, void> {
+  setup({ http, getStartServices }: CoreSetup<StartDeps>) {
+    const router = http.createRouter();
+
+    router.get(
+      {
+        path: API_ENDPOINT,
+        validate: {
+          query: schema.object({
+            expression: schema.string(),
+          }),
+        },
+      },
+      async (context, request, response) => {
+        const [, { screenshotting }] = await getStartServices();
+        const { metrics, results } = await lastValueFrom(
+          screenshotting.getScreenshots({
+            request,
+            expression: request.query.expression,
+          })
+        );
+
+        return response.ok({
+          body: JSON.stringify({
+            metrics,
+            image: results[0]?.screenshots[0]?.data.toString('base64'),
+            errors: results[0]?.renderErrors,
+          } as ScreenshottingExpressionResponse),
+        });
+      }
+    );
+  }
+
+  start() {}
+}
