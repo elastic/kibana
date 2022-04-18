3ca2fae13994bbea2e085399563d97d709385fe2
Eyal Kraft <eyalkraft@gmail.com>
2022-04-18 14:53:37 +0300

    use exact pattern

--- x-pack/plugins/cloud_security_posture/server/create_transforms/latest_findings_transform.ts
+++ x-pack/plugins/cloud_security_posture/server/create_transforms/latest_findings_transform.ts
@@ -5,13 +5,13 @@
  * 2.0.
  */
 import type { TransformPutTransformRequest } from '@elastic/elasticsearch/lib/api/types';
-import { CSP_KUBEBEAT_INDEX_PATTERN, LATEST_FINDINGS_INDEX_PATTERN } from '../../common/constants';
+import { FINDINGS_INDEX_PATTERN, LATEST_FINDINGS_INDEX_PATTERN } from '../../common/constants';
 
 export const latestFindingsTransform: TransformPutTransformRequest = {
   transform_id: 'cloud_security_posture.findings_latest-default-0.0.1',
   description: 'Defines findings transformation to view only the latest finding per resource',
   source: {
-    index: CSP_KUBEBEAT_INDEX_PATTERN,
+    index: FINDINGS_INDEX_PATTERN,
   },
   dest: {
     index: LATEST_FINDINGS_INDEX_PATTERN,
