e979a58b5ab1fcc567abdac7cb61a9e5a789b9a0
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-04-18 09:07:11 -0500

    [8.2] [Ingest Pipelines] add tests for grok processor (#130123) (#130249)
    * [Ingest Pipelines] add tests for grok processor (#130123)
    
    (cherry picked from commit 9e900a15ecc3bda8bc245d918aa004f9badfc5a0)
    
    * update test
    
    Co-authored-by: Alison Goryachev <alison.goryachev@elastic.co>

---
+++ x-pack/plugins/ingest_pipelines/public/application/components/pipeline_editor/__jest__/processors/grok.test.ts
@@ -0,0 +1,106 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { act } from 'react-dom/test-utils';
+import { setup, SetupResult, getProcessorValue } from './processor.helpers';
+
+const GROK_TYPE = 'grok';
+
+describe('Processor: Grok', () => {
+  let onUpdate: jest.Mock;
+  let testBed: SetupResult;
+  let clickAddPattern: () => Promise<void>;
+
+  beforeAll(() => {
+    jest.useFakeTimers();
+    // disable all react-beautiful-dnd development warnings
+    (window as any)['__react-beautiful-dnd-disable-dev-warnings'] = true;
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+    // enable all react-beautiful-dnd development warnings
+    (window as any)['__react-beautiful-dnd-disable-dev-warnings'] = false;
+  });
+
+  beforeEach(async () => {
+    onUpdate = jest.fn();
+
+    await act(async () => {
+      testBed = await setup({
+        value: {
+          processors: [],
+        },
+        onFlyoutOpen: jest.fn(),
+        onUpdate,
+      });
+    });
+
+    const { find, component, actions } = testBed;
+
+    clickAddPattern = async () => {
+      await act(async () => {
+        find('droppableList.addButton').simulate('click');
+      });
+      component.update();
+    };
+
+    component.update();
+
+    // Open flyout to add new processor
+    actions.addProcessor();
+    // Add type (the other fields are not visible until a type is selected)
+    await actions.addProcessorType(GROK_TYPE);
+  });
+
+  test('prevents form submission if required fields are not provided', async () => {
+    const {
+      actions: { saveNewProcessor },
+      form,
+      exists,
+    } = testBed;
+
+    // Click submit button with only the type defined
+    await saveNewProcessor();
+
+    // Expect form error as "field" is a required parameter
+    expect(form.getErrorsMessages()).toEqual(['A field value is required.']);
+    // Patterns field is also required; it uses EuiDraggable and only shows an error icon when invalid
+    expect(exists('droppableList.errorIcon')).toBe(true);
+  });
+
+  test('saves with default parameter values', async () => {
+    const {
+      actions: { saveNewProcessor },
+      form,
+    } = testBed;
+
+    // Add "field" value
+    form.setInputValue('fieldNameField.input', 'test_grok_processor');
+
+    // Add pattern 1
+    form.setInputValue('droppableList.input-0', 'pattern1');
+
+    // Add pattern 2
+    await clickAddPattern();
+    form.setInputValue('droppableList.input-1', 'pattern2');
+
+    // Add pattern 3
+    await clickAddPattern();
+    form.setInputValue('droppableList.input-2', 'pattern3');
+
+    // Save the field
+    await saveNewProcessor();
+
+    const processors = getProcessorValue(onUpdate, GROK_TYPE);
+
+    expect(processors[0][GROK_TYPE]).toEqual({
+      field: 'test_grok_processor',
+      patterns: ['pattern1', 'pattern2', 'pattern3'],
+    });
+  });
+});
