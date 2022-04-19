96f3867a75b093f8a07861fef40562a6551bfb68
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2022-04-19 13:15:00 +0200

    Improve getDocTableIndex code

--- test/functional/page_objects/discover_page.ts
+++ test/functional/page_objects/discover_page.ts
@@ -251,7 +251,12 @@ export class DiscoverPageObject extends FtrService {
     }
 
     const row = await this.dataGrid.getRow({ rowIndex: index - 1 });
-    const result = await Promise.all(row.map(async (cell) => await cell.getVisibleText()));
+    const result = await Promise.all(
+      row.map(async (cell) => {
+        const textContent = await cell.getAttribute('textContent');
+        return textContent.trim();
+      })
+    );
     // Remove control columns
     return result.slice(2).join(' ');
   }
