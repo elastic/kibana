95b8022f3c9ccd0bb20f2dba5701b883613bcd2f
Nathan Reese <reese.nathan@elastic.co>
2022-04-18 20:07:20 -0600

    fix import

--- x-pack/plugins/maps/server/mvt/mvt_routes.test.ts
+++ x-pack/plugins/maps/server/mvt/mvt_routes.test.ts
@@ -7,7 +7,7 @@
 
 import { Readable } from 'stream';
 import sinon from 'sinon';
-import { KibanaResponseFactory } from '@kbn/core/server';
+import { KibanaResponseFactory } from 'src/core/server';
 import { sendResponse } from './mvt_routes';
 
 const mockStream = Readable.from(['{}']);
