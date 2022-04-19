121df495b24f6798484024bb785fcc46c13286ba
Scotty Bollinger <scotty.bollinger@elastic.co>
2022-04-19 12:14:42 -0500

    [Enterprise Search] Add documentation link for BYOEI (#130494)
    * Add link and type to doc links packages
    
    * Add link to UI

--- packages/kbn-doc-links/src/get_doc_links.ts
+++ packages/kbn-doc-links/src/get_doc_links.ts
@@ -96,6 +96,7 @@ export const getDocLinks = ({ kibanaBranch }: GetDocLinkOptions): DocLinks => {
       crawlRules: `${APP_SEARCH_DOCS}crawl-web-content.html#crawl-web-content-manage-crawl-rules`,
       curations: `${APP_SEARCH_DOCS}curations-guide.html`,
       duplicateDocuments: `${APP_SEARCH_DOCS}web-crawler-reference.html#web-crawler-reference-content-deduplication`,
+      elasticsearchIndexedEngines: `${APP_SEARCH_DOCS}elasticsearch-engines.html`,
       entryPoints: `${APP_SEARCH_DOCS}crawl-web-content.html#crawl-web-content-manage-entry-points`,
       guide: `${APP_SEARCH_DOCS}index.html`,
       indexingDocuments: `${APP_SEARCH_DOCS}indexing-documents-guide.html`,
