58954cfa8917097ac1ae0f275ebebca9a9243401
Paul Tavares <paul.tavares@elastic.co>
2022-04-19 12:27:40 -0400

    new endpoint hooks for showing a console ++ get endpoint host info from api

---
+++ x-pack/plugins/security_solution/public/management/hooks/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export { useGetEndpointHostInfo } from './use_get_endpoint_host_info';
+export { useShowEndpointResponseActionsConsole } from './use_show_endpoint_response_actions_console';
---
+++ x-pack/plugins/security_solution/public/management/hooks/use_show_endpoint_response_actions_console.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useCallback } from 'react';
+import { EndpointConsoleCommandService } from '../components/endpoint_console/endpoint_console_command_service';
+import { useConsoleManager } from '../components/console';
+import { HostInfo } from '../../../common/endpoint/types';
+
+export const useShowEndpointResponseActionsConsole = (): ((endpointHostInfo: HostInfo) => void) => {
+  const consoleManager = useConsoleManager();
+
+  return useCallback(
+    (endpointHostInfo: HostInfo) => {
+      const endpointRunningConsole = consoleManager.getOne(endpointHostInfo.metadata.agent.id);
+
+      if (endpointRunningConsole) {
+        endpointRunningConsole.show();
+      } else {
+        consoleManager
+          .register({
+            id: endpointHostInfo.metadata.agent.id,
+            title: `${endpointHostInfo.metadata.host.name} - Endpoint v${endpointHostInfo.metadata.agent.version}`,
+            consoleProps: {
+              'data-test-subj': 'endpointResponseActionsConsole',
+              prompt: `endpoint-${endpointHostInfo.metadata.agent.version}`,
+              commandService: new EndpointConsoleCommandService(endpointHostInfo),
+            },
+          })
+          .show();
+      }
+    },
+    [consoleManager]
+  );
+};
