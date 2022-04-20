f4b76a472c70e86f56aa98adaa1678f251c7eecd
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2022-04-20 11:15:12 +0100

    add docs

--- x-pack/plugins/encrypted_saved_objects/server/saved_objects/index.ts
+++ x-pack/plugins/encrypted_saved_objects/server/saved_objects/index.ts
@@ -49,6 +49,26 @@ export interface EncryptedSavedObjectsClient {
     options?: SavedObjectsBaseOptions
   ) => Promise<SavedObject<T>>;
 
+  /**
+   * API method, that can be used to help page through large sets of saved objects and returns decrypted properties in result SO.
+   * Its interface matches interface of the corresponding Saved Objects API `createPointInTimeFinder` method:
+   *
+   * @example
+   * ```ts
+   * const finder = await this.encryptedSavedObjectsClient.createPointInTimeFinderAsInternalUser({
+   *   filter,
+   *   type: 'my-saved-object-type',
+   *   perPage: 1000,
+   * });
+   * for await (const response of finder.find()) {
+   *   // process response
+   * }
+   * ```
+   *
+   * @param findOptions matches interface of corresponding argument of Saved Objects API `createPointInTimeFinder` {@link SavedObjectsCreatePointInTimeFinderOptions}
+   * @param dependencies matches interface of corresponding argument of Saved Objects API `createPointInTimeFinder` {@link SavedObjectsCreatePointInTimeFinderDependencies}
+   *
+   */
   createPointInTimeFinderAsInternalUser<T = unknown, A = unknown>(
     findOptions: SavedObjectsCreatePointInTimeFinderOptions,
     dependencies?: SavedObjectsCreatePointInTimeFinderDependencies
