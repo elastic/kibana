28c08472700cd302e747adc1285a73745be84a9a
shahzad31 <shahzad.muhammad@elastic.co>
2022-04-21 08:57:22 +0200

    update test keys

--- x-pack/test/functional_with_es_ssl/apps/uptime/simple_down_alert.ts
+++ x-pack/test/functional_with_es_ssl/apps/uptime/simple_down_alert.ts
@@ -75,7 +75,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     it('displays relevant alert in list drawer', async () => {
       await toasts.dismissAllToasts();
 
-      await testSubjects.click(`xpack.uptime.monitorList.${monitorId}.expandMonitorDetail`);
+      await testSubjects.click(`xpack.synthetics.monitorList.${monitorId}.expandMonitorDetail`);
       await pageObjects.header.waitUntilLoadingHasFinished();
       await testSubjects.existOrFail('uptimeMonitorListDrawerAlert0');
     });
@@ -104,7 +104,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
         },
         {
           actionTypeId: '.slack',
-          group: 'xpack.uptime.alerts.actionGroups.monitorStatus',
+          group: 'xpack.synthetics.alerts.actionGroups.monitorStatus',
           params: {
             message:
               'Monitor {{state.monitorName}} with url {{{state.monitorUrl}}} from {{state.observerLocation}} {{{state.statusMessage}}} The latest error message is {{{state.latestErrorMessage}}}',
@@ -112,7 +112,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
           id: 'my-slack1',
         },
       ]);
-      expect(alertTypeId).to.eql('xpack.uptime.alerts.monitorStatus');
+      expect(alertTypeId).to.eql('xpack.synthetics.alerts.monitorStatus');
       expect(consumer).to.eql('uptime');
       expect(tags).to.eql(['UPTIME_AUTO']);
     });
