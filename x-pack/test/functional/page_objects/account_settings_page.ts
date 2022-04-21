ee80e5507e1aeb1a07b1fe830151d71b132479c1
Thom Heymann <thom.heymann@elastic.co>
2022-04-21 21:19:03 +0100

    .

--- x-pack/test/functional/page_objects/account_settings_page.ts
+++ x-pack/test/functional/page_objects/account_settings_page.ts
@@ -12,16 +12,13 @@ export class AccountSettingsPageObject extends FtrService {
   private readonly testSubjects = this.ctx.getService('testSubjects');
   private readonly userMenu = this.ctx.getService('userMenu');
 
-  async verifyAccountSettings(expectedEmail: string, expectedUserName: string) {
+  async verifyAccountSettings(expectedUserName: string) {
     await this.userMenu.clickProvileLink();
 
     const usernameField = await this.testSubjects.find('username');
     const userName = await usernameField.getVisibleText();
     expect(userName).to.be(expectedUserName);
 
-    const emailIdField = await this.testSubjects.find('email');
-    const emailField = await emailIdField.getVisibleText();
-    expect(emailField).to.be(expectedEmail);
     await this.userMenu.closeMenu();
   }
 
