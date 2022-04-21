e585b2eccb962cbc4b9f80c97790d1915369051f
Dima Arnautov <dmitrii.arnautov@elastic.co>
2022-04-21 14:37:53 +0200

    report execution context for routes

--- x-pack/plugins/ml/public/application/routing/ml_page_wrapper.tsx
+++ x-pack/plugins/ml/public/application/routing/ml_page_wrapper.tsx
@@ -4,9 +4,22 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import React, { FC } from 'react';
 import { TrackApplicationView } from '@kbn/usage-collection-plugin/public';
+import { useExecutionContext } from '@kbn/kibana-react-plugin/public';
+import { useMlKibana } from '../contexts/kibana';
 
 export const MlPageWrapper: FC<{ path: string }> = ({ path, children }) => {
+  const {
+    services: { executionContext },
+  } = useMlKibana();
+
+  useExecutionContext(executionContext, {
+    name: 'Machine Learning',
+    type: 'application',
+    page: path,
+  });
+
   return <TrackApplicationView viewId={path}>{children}</TrackApplicationView>;
 };
