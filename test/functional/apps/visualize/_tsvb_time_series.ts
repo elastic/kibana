e32f166d547ad356b2765fe36fe9cd81926d7cac
Alexey Antonov <alexwizp@gmail.com>
2022-04-21 09:02:49 +0500

    Update _tsvb_time_series.ts

--- test/functional/apps/visualize/_tsvb_time_series.ts
+++ test/functional/apps/visualize/_tsvb_time_series.ts
@@ -27,7 +27,6 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const browser = getService('browser');
   const kibanaServer = getService('kibanaServer');
 
-  // Failing: See https://github.com/elastic/kibana/issues/129785
   describe('visual builder', function describeIndexTests() {
     before(async () => {
       await security.testUser.setRoles([
