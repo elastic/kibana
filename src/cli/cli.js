16ec4124a7bab2ca3e2a99e6a2d485c0c72e583a
Tiago Costa <tiago.costa@elastic.co>
2022-04-22 21:48:51 +0100

    chore(NA): include free packages on cli

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -10,6 +10,14 @@ import _ from 'lodash';
 import { kibanaPackageJson as pkg } from '@kbn/utils';
 import Command from './command';
 import serveCommand from './serve/serve';
+import { foo as foo1 } from '@kbn/location-free-pkg';
+import { foo as foo2 } from '@kbn/location-free-pkg-two';
+import { foo as foo3 } from '@kbn/location_free_pkg_three';
+import { foo as foo4 } from '@kbn/location_free_xpack_pkg';
+
+if (1 === 0) {
+  console.log(foo1, foo2, foo3, foo4);
+}
 
 const argv = process.argv.slice();
 const program = new Command('bin/kibana');
