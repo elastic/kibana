8073b4698629916806f4fa08eebeea368871b0b1
Tiago Costa <tiago.costa@elastic.co>
2022-04-22 04:29:51 +0100

    chore(NA): impossible if so cli integartion test passes

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -15,7 +15,9 @@ import { foo as foo2 } from '@kbn/location-free-pkg-two';
 import { foo as foo3 } from '@kbn/location_free_pkg_three';
 import { foo as foo4 } from '@kbn/location_free_xpack_pkg';
 
-console.log(foo1, foo2, foo3, foo4);
+if (1 === 0) {
+  console.log(foo1, foo2, foo3, foo4);
+}
 
 const argv = process.argv.slice();
 const program = new Command('bin/kibana');
