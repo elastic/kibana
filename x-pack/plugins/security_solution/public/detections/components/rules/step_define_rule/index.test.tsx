864c178e6d195aec2a590807c4620fa47254e76a
Devin Hurley <devin.hurley@elastic.co>
2022-04-25 15:48:20 -0400

    fix test

--- x-pack/plugins/security_solution/public/detections/components/rules/step_define_rule/index.test.tsx
+++ x-pack/plugins/security_solution/public/detections/components/rules/step_define_rule/index.test.tsx
@@ -14,6 +14,29 @@ import mockBrowserFields from './mock_browser_fields.json';
 import { aggregatableFields } from '.';
 
 jest.mock('../../../../common/lib/kibana');
+jest.mock('../../../../common/hooks/use_selector', () => {
+  const actual = jest.requireActual('../../../../common/hooks/use_selector');
+  return {
+    ...actual,
+    useDeepEqualSelector: () => ({
+      kibanaDataViews: [{ id: 'world' }],
+      sourcererScope: 'my-selected-dataview-id',
+    }),
+  };
+});
+jest.mock('../../../../common/containers/sourcerer', () => {
+  const actual = jest.requireActual('../../../../common/containers/sourcerer');
+  return {
+    ...actual,
+    useSourcererDataView: jest
+      .fn()
+      .mockReturnValue({ indexPattern: ['fakeindex'], loading: false }),
+  };
+});
+jest.mock('react-router-dom', () => {
+  const actual = jest.requireActual('react-router-dom');
+  return { ...actual, useLocation: jest.fn().mockReturnValue({ pathname: '/alerts' }) };
+});
 
 test('aggregatableFields', function () {
   expect(aggregatableFields(mockBrowserFields)).toMatchSnapshot();
