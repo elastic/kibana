ead04ce6523555557f2916fab1a0870dc785bd1d
Marshall Main <marshall.main@elastic.co>
2022-04-25 16:02:04 -0700

    Remove experimental value list exception implementation

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/new_terms/create_new_terms_alert_type.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/new_terms/create_new_terms_alert_type.ts
@@ -175,14 +175,6 @@ export const createNewTermsAlertType = (
         });
       });
 
-      /* const [included, excluded] = await filterEventsAgainstList({
-          listClient,
-          exceptionsList: exceptionItems,
-          logger,
-          events,
-          buildRuleMessage,
-        });*/
-
       const wrappedAlerts = wrapNewTermsAlerts({
         eventsAndTerms,
         spaceId,
@@ -191,116 +183,7 @@ export const createNewTermsAlertType = (
         buildReasonMessage: buildReasonMessageForNewTermsAlert,
       });
 
-      // TODO: better way of aggregating results from multiple separate bulkCreate calls
       addBulkCreateResults(bulkCreateResults, await bulkCreate(wrappedAlerts));
-
-      /* const excludedBuckets = bucketsForField
-          .filter((bucket) =>
-            excluded.some(
-              (event) =>
-                event._id === bucket.docs.hits.hits[0]._id &&
-                event._index === bucket.docs.hits.hits[0]._index
-            )
-          )
-          .map((bucket) => {
-            const bucketFilter = {
-              term: {
-                [field]: bucket.key,
-              },
-            };
-            return {
-              bucketFilter: getQueryFilter(
-                params.query,
-                params.language,
-                params.filters ? [...params.filters, bucketFilter] : [bucketFilter],
-                inputIndex,
-                exceptionItems
-              ),
-              searchAfterSortIds: undefined,
-            };
-          });
-
-        combinedExcludedBuckets.push(...excludedBuckets); */
-
-      /* const pageSize = 1000;
-      const CHUNK_SIZE = 10;
-      while (combinedExcludedBuckets.length > 0) {
-        // Process one page of each excluded bucket and for each page return either: 1. a candidate alert
-        // not filtered out by the value list exceptions, 2. the search after IDs for the next page, or
-        // 3. undefined if there are no more pages to search through for that bucket
-        // We keep processing more pages until all buckets are finished (return an alert or no more pages)
-        // or the rule times out. By processing one page from every bucket on each iteration we prevent
-        // a single huge bucket from starving out all other buckets.
-        const bucketChunks = chunk(combinedExcludedBuckets, CHUNK_SIZE);
-        const results: Array<BucketResult | undefined> = [];
-        for (const bucketChunk of bucketChunks) {
-          results.push(
-            ...(await Promise.all(
-              bucketChunk.map(async (bucket): Promise<BucketResult | undefined> => {
-                const { searchResult: postSearchResult, searchErrors: postSearchErrors } =
-                  await singleSearchAfter({
-                    searchAfterSortIds: bucket.searchAfterSortIds,
-                    index: inputIndex,
-                    from: tuple.from.toISOString(),
-                    to: tuple.to.toISOString(),
-                    services,
-                    filter: bucket.bucketFilter,
-                    logger,
-                    pageSize,
-                    timestampOverride: params.timestampOverride,
-                    buildRuleMessage,
-                  });
-                searchAfterResults.searchErrors.push(...postSearchErrors);
-
-                const [includedEvents, _] = await filterEventsAgainstList({
-                  listClient,
-                  exceptionsList: exceptionItems,
-                  logger,
-                  events: postSearchResult.hits.hits,
-                  buildRuleMessage,
-                });
-
-                if (includedEvents.length > 0) {
-                  return {
-                    type: 'alert',
-                    result: includedEvents[0],
-                  };
-                }
-
-                const nextSortIds = getSafeSortIds(
-                  searchResult.hits.hits[searchResult.hits.hits.length - 1]?.sort
-                );
-                if (nextSortIds == null || searchResult.hits.hits.length < pageSize) {
-                  return undefined;
-                }
-                return {
-                  type: 'newPage',
-                  result: {
-                    ...bucket,
-                    searchAfterSortIds: nextSortIds,
-                  },
-                };
-              })
-            ))
-          );
-        }
-
-        const alertResults = results.filter(
-          (result): result is BucketAlertResult => result != null && result.type === 'alert'
-        );
-        // Gather up the buckets that have another page to process, we'll do another iteration on
-        // these buckets
-        const newPageResults = results.filter(
-          (result): result is BucketPageResult => result != null && result.type === 'newPage'
-        );
-
-        const wrappedAlerts = wrapHits(
-          alertResults.map((result) => result.result),
-          buildReasonMessageForNewTermsAlert
-        );
-        addBulkCreateResults(bulkCreateResults, await bulkCreate(wrappedAlerts));
-        combinedExcludedBuckets = newPageResults.map((result) => result.result);
-      } */
 
       return {
         // If an error occurs but doesn't cause us to throw then we still count the execution as a success.
