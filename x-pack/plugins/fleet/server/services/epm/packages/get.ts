04fa98c04f3e0bbc195d478798408999113bed83
Josh Dover <doverfake@elastic.co>
2022-04-25 18:08:15 +0200

    Fix bad merge conflict

--- x-pack/plugins/fleet/server/services/epm/packages/get.ts
+++ x-pack/plugins/fleet/server/services/epm/packages/get.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { SavedObjectsClientContract, SavedObjectsFindOptions } from '@kbn/core/server';
+import type { SavedObjectsClientContract, SavedObjectsFindOptions } from 'src/core/server';
 import semverGte from 'semver/functions/gte';
 
 import {
