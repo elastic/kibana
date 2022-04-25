d359ef2775bf1bf1dc4d705d8ff51b4ef6c48e44
Jonathan Budzenski <jon@elastic.co>
2022-04-25 12:13:20 -0500

    copy

--- src/dev/build/tasks/create_cdn_assets_task.ts
+++ src/dev/build/tasks/create_cdn_assets_task.ts
@@ -6,6 +6,12 @@
  * Side Public License, v 1.
  */
 
+import { readFileSync } from 'fs';
+import { resolve, dirname } from 'path';
+import { asyncForEach } from '@kbn/std';
+
+import globby from 'globby';
+
 import { mkdirp, compressTar, Task, copyAll } from '../lib';
 
 export const CreateCdnAssets: Task = {
@@ -13,11 +19,24 @@ export const CreateCdnAssets: Task = {
 
   async run(config, log, build) {
     const buildSource = build.resolvePath();
+    const buildNum = config.getBuildNumber();
     const assets = config.resolveFromRepo('build', 'cdn-assets');
-    await mkdirp(assets);
+    const bundles = resolve(assets, String(buildNum), 'bundles');
+    const plugin = resolve(bundles, 'plugin');
+    await mkdirp(plugin);
 
-    await copyAll(buildSource, assets, {
-      select: ['**/target/public/*'],
+    const plugins = globby.sync([
+      `${buildSource}/x-pack/plugins/*/kibana.json`,
+      `${buildSource}/src/plugins/*/kibana.json`,
+      '!node_modules',
+    ]);
+    asyncForEach(plugins, async (path) => {
+      const spec = JSON.parse(readFileSync(path, 'utf8'));
+      const { id, version } = spec;
+      const dest = resolve(plugin, id, version);
+      await copyAll(resolve(dirname(path), 'target/public'), dest, {
+        select: ['*'],
+      });
     });
     await compressTar({
       source: assets,
