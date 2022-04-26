4a34ec4b3ce26c4c2b639cefc13b7e52205bb654
Sébastien Loix <sebastien.loix@elastic.co>
2022-04-26 09:33:36 +0100

    Add UserContent and MetadataEventsStreams plugins

---
+++ src/plugins/metadata_events_streams/server/index.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { PluginInitializerContext } from '@kbn/core/server';
+import { MetadataEventsStreamsPlugin } from './plugin';
+
+export const plugin = (context: PluginInitializerContext) =>
+  new MetadataEventsStreamsPlugin(context);
+
+export type { MetadataEventsStreamsPluginSetup, MetadataEventsStreamsPluginStart } from './types';
---
+++ src/plugins/user_content/server/plugin.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+import { CoreSetup, CoreStart, Logger, Plugin, PluginInitializerContext } from '@kbn/core/server';
+import { UserContentPluginSetup, UserContentPluginStart } from './types';
+// import { SavedObjectsManagement } from './services';
+// import { registerRoutes } from './routes';
+// import { capabilitiesProvider } from './capabilities_provider';
+
+export class UserContentPlugin
+  implements Plugin<UserContentPluginSetup, UserContentPluginStart, {}, {}>
+{
+  private readonly logger: Logger;
+
+  constructor(private readonly context: PluginInitializerContext) {
+    this.logger = this.context.logger.get();
+  }
+
+  public setup({ http }: CoreSetup) {
+    this.logger.debug('Setting up UserContent plugin');
+    // registerRoutes({
+    //   http,
+    //   managementServicePromise: firstValueFrom(this.managementService$),
+    // });
+
+    return {};
+  }
+
+  public start(core: CoreStart) {
+    this.logger.debug('Starting up UserContent plugin');
+    // const managementService = new SavedObjectsManagement(core.savedObjects.getTypeRegistry());
+    // this.managementService$.next(managementService);
+
+    return {};
+  }
+}
