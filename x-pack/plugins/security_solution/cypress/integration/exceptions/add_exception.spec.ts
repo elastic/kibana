801301954e25509c481b98b409a5bc7db7a1a0e2
Gloria Hornero <gloria.hornero@elastic.co>
2022-04-26 18:00:03 +0200

    groups add exceptions tests in one spec file (#130908)

--- x-pack/plugins/security_solution/cypress/integration/exceptions/add_exception.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/exceptions/add_exception.spec.ts
@@ -10,13 +10,14 @@ import { getNewRule } from '../../objects/rule';
 
 import { ALERTS_COUNT, EMPTY_ALERT_TABLE, NUMBER_OF_ALERTS } from '../../screens/alerts';
 
-import { goToClosedAlerts, goToOpenedAlerts } from '../../tasks/alerts';
+import { addExceptionFromFirstAlert, goToClosedAlerts, goToOpenedAlerts } from '../../tasks/alerts';
 import { createCustomRule } from '../../tasks/api_calls/rules';
 import { goToRuleDetails } from '../../tasks/alerts_detection_rules';
 import { waitForAlertsToPopulate } from '../../tasks/create_new_rule';
 import { esArchiverLoad, esArchiverUnload } from '../../tasks/es_archiver';
 import { login, visitWithoutDateRange } from '../../tasks/login';
 import {
+  addsException,
   addsExceptionFromRuleSettings,
   goToAlertsTab,
   goToExceptionsTab,
@@ -27,15 +28,16 @@ import {
 import { DETECTIONS_RULE_MANAGEMENT_URL } from '../../urls/navigation';
 import { cleanKibana, deleteAlertsAndRules } from '../../tasks/common';
 
-describe('From rule', () => {
-  const NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS = '1';
+describe('Adds rule exception', () => {
+  const NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS = '1 alert';
+
   before(() => {
     cleanKibana();
     login();
+    esArchiverLoad('exceptions');
   });
 
   beforeEach(() => {
-    esArchiverLoad('exceptions');
     deleteAlertsAndRules();
     createCustomRule(
       { ...getNewRule(), customQuery: 'agent.name:*', index: ['exceptions*'] },
@@ -47,17 +49,48 @@ describe('From rule', () => {
     goToRuleDetails();
     waitForTheRuleToBeExecuted();
     waitForAlertsToPopulate();
-
-    cy.get(ALERTS_COUNT).should('exist');
-    cy.get(NUMBER_OF_ALERTS).should('have.text', `${NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS} alert`);
   });
 
   afterEach(() => {
-    esArchiverUnload('exceptions');
     esArchiverUnload('exceptions_2');
   });
 
-  it('Creates an exception and deletes it', () => {
+  after(() => {
+    esArchiverUnload('exceptions');
+  });
+
+  it('Creates an exception from an alert and deletes it', () => {
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS);
+    // Create an exception from the alerts actions menu that matches
+    // the existing alert
+    addExceptionFromFirstAlert();
+    addsException(getException());
+
+    // Alerts table should now be empty from having added exception and closed
+    // matching alert
+    cy.get(EMPTY_ALERT_TABLE).should('exist');
+
+    // Closed alert should appear in table
+    goToClosedAlerts();
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', `${NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS}`);
+
+    // Remove the exception and load an event that would have matched that exception
+    // to show that said exception now starts to show up again
+    goToExceptionsTab();
+    removeException();
+    esArchiverLoad('exceptions_2');
+    goToAlertsTab();
+    goToOpenedAlerts();
+    waitForTheRuleToBeExecuted();
+    waitForAlertsToPopulate();
+
+    cy.get(ALERTS_COUNT).should('exist');
+    cy.get(NUMBER_OF_ALERTS).should('have.text', `${NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS}`);
+  });
+
+  it('Creates an exception from a rule and deletes it', () => {
     // Create an exception from the exception tab that matches
     // the existing alert
     goToExceptionsTab();
@@ -71,7 +104,7 @@ describe('From rule', () => {
     // Closed alert should appear in table
     goToClosedAlerts();
     cy.get(ALERTS_COUNT).should('exist');
-    cy.get(NUMBER_OF_ALERTS).should('have.text', `${NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS} alert`);
+    cy.get(NUMBER_OF_ALERTS).should('have.text', `${NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS}`);
 
     // Remove the exception and load an event that would have matched that exception
     // to show that said exception now starts to show up again
@@ -84,6 +117,6 @@ describe('From rule', () => {
     waitForAlertsToPopulate();
 
     cy.get(ALERTS_COUNT).should('exist');
-    cy.get(NUMBER_OF_ALERTS).should('have.text', `${NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS} alert`);
+    cy.get(NUMBER_OF_ALERTS).should('have.text', `${NUMBER_OF_AUDITBEAT_EXCEPTIONS_ALERTS}`);
   });
 });
