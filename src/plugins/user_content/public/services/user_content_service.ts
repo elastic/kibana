b273f0d91e1b512cdb582ac2e18c33fb04841b58
Sébastien Loix <sebastien.loix@elastic.co>
2022-04-26 14:08:29 +0100

    Move UserContentService to public folder

---
+++ src/plugins/user_content/public/services/user_content_service.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+import { ContentType } from '../types';
+
+interface InitOptions<T> {
+  /** Handler to fetch the saved object */
+  get: (contentId: string) => Promise<T>;
+}
+
+interface Content<T> {
+  /** Handler to fetch the saved object */
+  get: (contentId: string) => Promise<T>;
+}
+
+export class UserContentService {
+  private contents: Map<ContentType, Content<unknown>>;
+
+  constructor() {
+    this.contents = new Map<ContentType, Content<unknown>>();
+  }
+
+  init() {}
+
+  register<T>(contentType: ContentType, { get }: InitOptions<T>) {
+    this.contents.set(contentType, {
+      get,
+    });
+  }
+
+  get<T = unknown>(contentType: ContentType, contentId: string) {
+    if (!this.contents.has(contentType)) {
+      throw new Error(`Can't fetch content [${contentId}]. Unknown content type [${contentType}].`);
+    }
+
+    return this.contents.get(contentType)!.get(contentId) as unknown as T;
+  }
+}
