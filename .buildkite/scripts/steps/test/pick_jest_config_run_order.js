947c417cac5cdf32ca11156484898a14772a5e8e
spalger <spencer@elastic.co>
2022-04-26 08:44:35 -0500

    use new helper name

--- .buildkite/scripts/steps/test/pick_jest_config_run_order.js
+++ .buildkite/scripts/steps/test/pick_jest_config_run_order.js
@@ -10,7 +10,7 @@ const { CiStats } = require('kibana-buildkite-library');
 
 (async () => {
   try {
-    await CiStats.pickJestConfigRunOrder();
+    await CiStats.pickTestGroupRunOrder();
   } catch (ex) {
     console.error('CI Stats Error', ex.message);
     if (ex.response) {
