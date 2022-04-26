5660ae364ef5e5bb2a15144ddcd78c0cfabad44c
Paulo Henrique <paulo.scape@gmail.com>
2022-04-26 17:06:00 -0300

    refining use_eui_theme hook

--- x-pack/plugins/session_view/public/hooks/use_eui_theme.ts
+++ x-pack/plugins/session_view/public/hooks/use_eui_theme.ts
@@ -7,33 +7,35 @@
 
 import { shade, useEuiTheme as useEuiThemeHook } from '@elastic/eui';
 import { euiLightVars, euiDarkVars } from '@kbn/ui-theme';
+import { useMemo } from 'react';
 
 type EuiThemeProps = Parameters<typeof useEuiThemeHook>;
 type ExtraEuiVars = {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  euiColorVis3_asText: string;
   // eslint-disable-next-line @typescript-eslint/naming-convention
   euiColorVis6_asText: string;
 };
 type EuiVars = typeof euiLightVars & ExtraEuiVars;
 type EuiThemeReturn = ReturnType<typeof useEuiThemeHook> & { euiVars: EuiVars };
 
+// Not all Eui Tokens were fully migrated to @elastic/eui/useEuiTheme yet, so
+// this hook overrides the default useEuiTheme hook to provide a custom hook that
+// allows the use the euiVars tokens from the euiLightVars and euiDarkVars
 export const useEuiTheme = (...props: EuiThemeProps): EuiThemeReturn => {
   const euiThemeHook = useEuiThemeHook(...props);
 
-  const vars = euiThemeHook.colorMode === 'DARK' ? euiDarkVars : euiLightVars;
+  const euiVars = useMemo(() => {
+    const themeVars = euiThemeHook.colorMode === 'DARK' ? euiDarkVars : euiLightVars;
 
-  const extraEuiVars: ExtraEuiVars = {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    euiColorVis3_asText: shade(vars.euiColorVis3, 0.148),
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    euiColorVis6_asText: shade(vars.euiColorVis6, 0.335),
-  };
+    const extraEuiVars: ExtraEuiVars = {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      euiColorVis6_asText: shade(themeVars.euiColorVis6, 0.335),
+    };
 
-  const euiVars = {
-    ...vars,
-    ...extraEuiVars,
-  };
+    return {
+      ...themeVars,
+      ...extraEuiVars,
+    };
+  }, [euiThemeHook.colorMode]);
 
   return {
     ...euiThemeHook,
