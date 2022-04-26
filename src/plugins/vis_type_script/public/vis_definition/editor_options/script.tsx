5f58bfc36adedf4f5a473b3c1920b83f4931598e
Andrew Tate <andrew.tate@elastic.co>
2022-04-26 10:09:16 -0500

    use Monaco instead of textarea

--- src/plugins/vis_type_script/public/vis_definition/editor_options/script.tsx
+++ src/plugins/vis_type_script/public/vis_definition/editor_options/script.tsx
@@ -7,27 +7,15 @@
  */
 
 import React, { useCallback } from 'react';
-import {
-  EuiPanel,
-  EuiTitle,
-  EuiLink,
-  EuiTextArea,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiText,
-} from '@elastic/eui';
+import { EuiPanel, EuiTitle, EuiLink, EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 
+import { CodeEditor } from '../../../../kibana_react/public';
 import type { VisEditorOptionsProps } from '../../../../visualizations/public';
 import type { VisParams } from '../../types';
 
-import './script.scss';
-
 function ScriptOptions({ stateParams, setValue }: VisEditorOptionsProps<VisParams>) {
-  const onScriptUpdate = useCallback(
-    ({ target: { value } }: React.ChangeEvent<HTMLTextAreaElement>) => setValue('script', value),
-    [setValue]
-  );
+  const onScriptUpdate = useCallback((value: string) => setValue('script', value), [setValue]);
 
   return (
     <EuiPanel paddingSize="s">
@@ -59,14 +47,12 @@ function ScriptOptions({ stateParams, setValue }: VisEditorOptionsProps<VisParam
         </EuiFlexItem>
 
         <EuiFlexItem>
-          <EuiTextArea
-            id="scriptVisInput"
-            className="visEditor--script__textarea"
+          <CodeEditor
+            languageId="javascript"
             value={stateParams.script}
             onChange={onScriptUpdate}
             fullWidth={true}
             data-test-subj="scriptTextarea"
-            resize="none"
           />
         </EuiFlexItem>
       </EuiFlexGroup>
