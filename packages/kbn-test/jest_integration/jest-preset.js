f149a17871ae6b33d4d92c4e6a883741d51d0d97
spalger <spencer@elastic.co>
2022-04-27 11:38:19 -0500

    Revert "[jest] automatically determine run order (#130857)"
    This reverts commit 1a05ce5cab240299d1ebe50debae18323308164a.

--- packages/kbn-test/jest_integration/jest-preset.js
+++ packages/kbn-test/jest_integration/jest-preset.js
@@ -28,16 +28,12 @@ module.exports = {
         reportName: 'Jest Integration Tests',
       },
     ],
-    ...(process.env.TEST_GROUP_TYPE_INTEGRATION
-      ? [
-          [
-            '@kbn/test/target_node/jest/ci_stats_jest_reporter',
-            {
-              testGroupType: process.env.TEST_GROUP_TYPE_INTEGRATION,
-            },
-          ],
-        ]
-      : []),
+    [
+      '@kbn/test/target_node/jest/ci_stats_jest_reporter',
+      {
+        testGroupType: 'Jest Integration Tests',
+      },
+    ],
   ],
   coverageReporters: !!process.env.CI
     ? [['json', { file: 'jest-integration.json' }]]
