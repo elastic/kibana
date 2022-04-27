91c67d2d32a08100b1fe9a16247080d50344b438
Faisal Kanout <faisal.kanout@elastic.co>
2022-04-27 15:40:37 +0200

    Fix tag style

--- x-pack/plugins/observability/public/pages/rule_details/components/page_title.tsx
+++ x-pack/plugins/observability/public/pages/rule_details/components/page_title.tsx
@@ -6,14 +6,13 @@
  */
 import React from 'react';
 import moment from 'moment';
-import { EuiText, EuiButtonEmpty, EuiFlexGroup, EuiFlexItem, EuiPanel } from '@elastic/eui';
+import { EuiText, EuiFlexGroup, EuiFlexItem, EuiBadge } from '@elastic/eui';
 import { ExperimentalBadge } from '../../../components/shared/experimental_badge';
 import { PageHeaderProps } from '../types';
 
 export function PageTitle({ rule }: PageHeaderProps) {
   return (
     <>
-      {/* TODO: Add return back to rule navigation button */}
       {rule.name} <ExperimentalBadge />
       <EuiFlexGroup alignItems="center">
         <EuiFlexItem component="span" grow={false}>
@@ -22,12 +21,19 @@ export function PageTitle({ rule }: PageHeaderProps) {
             <b>Created</b> by {rule.createdBy} on {moment(rule.createdAt).format('ll')}
           </EuiText>
         </EuiFlexItem>
-        <EuiFlexItem style={{ alignSelf: 'flexStart' }} component="span" grow={false}>
-          <EuiPanel hasShadow={false} hasBorder={true} paddingSize={'none'}>
-            <EuiButtonEmpty iconType="tag" color="text">
-              {rule.tags.length}
-            </EuiButtonEmpty>
-          </EuiPanel>
+        <EuiFlexItem grow={false}>
+          <EuiBadge
+            color="hollow"
+            iconType="tag"
+            iconSide="left"
+            onClick={() => {}}
+            onClickAriaLabel="Rule tags"
+            iconOnClick={() => {}}
+            iconOnClickAriaLabel="Rule tags"
+            data-test-sub="ruleTag"
+          >
+            {rule.tags.length}
+          </EuiBadge>
         </EuiFlexItem>
       </EuiFlexGroup>
     </>
