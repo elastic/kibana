ca34d03a11c27d0f9667dd762fcad948b1ebef67
Robert Oskamp <robert.oskamp@elastic.co>
2022-04-27 17:07:41 +0200

    [ML] Functional tests - re-enable trained models tests (#131051)
    This PR re-enables the trained models functional tests.

--- x-pack/test/functional/apps/ml/model_management/model_list.ts
+++ x-pack/test/functional/apps/ml/model_management/model_list.ts
@@ -10,8 +10,7 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 export default function ({ getService }: FtrProviderContext) {
   const ml = getService('ml');
 
-  // Failing: See https://github.com/elastic/kibana/issues/125455
-  describe.skip('trained models', function () {
+  describe('trained models', function () {
     before(async () => {
       await ml.trainedModels.createTestTrainedModels('classification', 15, true);
       await ml.trainedModels.createTestTrainedModels('regression', 15);
