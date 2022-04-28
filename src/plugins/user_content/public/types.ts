950d7f89a707cc647fd65957b5dce9ad52bee237
Sébastien Loix <sebastien.loix@elastic.co>
2022-04-28 14:29:50 +0100

    Remove circular TS project reference

--- src/plugins/user_content/public/types.ts
+++ src/plugins/user_content/public/types.ts
@@ -5,11 +5,7 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import type { UserContentType as DashboardContentType } from '@kbn/dashboard-plugin/common';
-
 import { UserContentService } from './services';
-
-export type UserContentType = DashboardContentType;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface UserContentPluginSetup {}
