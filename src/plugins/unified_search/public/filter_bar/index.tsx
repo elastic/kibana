cc20fdd9743a95236ef5bce8ddafdb8b8e621197
Nodir Latipov <nodir.latypov@gmail.com>
2022-04-28 17:57:55 +0500

    feat: rollback filter bar changes

--- src/plugins/unified_search/public/filter_bar/index.tsx
+++ src/plugins/unified_search/public/filter_bar/index.tsx
@@ -7,14 +7,8 @@
  */
 
 import React from 'react';
-import { withSuspense } from '@kbn/shared-ux-utility';
 
 const Fallback = () => <div />;
-
-/**
- * The Lazily-loaded `FilterBar` component.  Consumers should use `React.Suspense` or
- * the withSuspense` HOC to load this component.
- */
 
 const LazyFilterBar = React.lazy(() => import('./filter_bar'));
 export const FilterBar = (props: React.ComponentProps<typeof LazyFilterBar>) => (
@@ -23,35 +17,16 @@ export const FilterBar = (props: React.ComponentProps<typeof LazyFilterBar>) => 
   </React.Suspense>
 );
 
-/**
- * A `FilterBar` component that is wrapped by the `withSuspense` HOC.  This component can
- * be used directly by consumers and will load the `FilterBarLazy` component lazily with
- * a predefined fallback and error boundary.
- */
-// export const FilterBar = withSuspense(FilterBarLazy);
+const LazyFilterLabel = React.lazy(() => import('./filter_editor/lib/filter_label'));
+export const FilterLabel = (props: React.ComponentProps<typeof LazyFilterLabel>) => (
+  <React.Suspense fallback={<Fallback />}>
+    <LazyFilterLabel {...props} />
+  </React.Suspense>
+);
 
-/**
- * The Lazily-loaded `FilterLabel` component.  Consumers should use `React.Suspense` or
- * the withSuspense` HOC to load this component.
- */
-const FilterLabelLazy = React.lazy(() => import('./filter_editor/lib/filter_label'));
-
-/**
- * A `FilterLabel` component that is wrapped by the `withSuspense` HOC.  This component can
- * be used directly by consumers and will load the `FilterLabelLazy` component lazily with
- * a predefined fallback and error boundary.
- */
-export const FilterLabel = withSuspense(FilterLabelLazy);
-
-/**
- * The Lazily-loaded `FilterItem` component.  Consumers should use `React.Suspense` or
- * the withSuspense` HOC to load this component.
- */
-const FilterItemLazy = React.lazy(() => import('./filter_item'));
-
-/**
- * A `FilterItem` component that is wrapped by the `withSuspense` HOC.  This component can
- * be used directly by consumers and will load the `FilterItemLazy` component lazily with
- * a predefined fallback and error boundary.
- */
-export const FilterItem = withSuspense(FilterItemLazy);
+const LazyFilterItem = React.lazy(() => import('./filter_item'));
+export const FilterItem = (props: React.ComponentProps<typeof LazyFilterItem>) => (
+  <React.Suspense fallback={<Fallback />}>
+    <LazyFilterItem {...props} />
+  </React.Suspense>
+);
