c1cc4009464d8c4927aed7f50e70ef48c4f4e7ea
Ersin Erdal <92688503+ersin-erdal@users.noreply.github.com>
2022-04-28 15:51:25 +0200

    Fix optional spaceId in rules_client (#130704)
    * Fix optional spaceId in rules_client

--- x-pack/plugins/alerting/server/rules_client/rules_client.ts
+++ x-pack/plugins/alerting/server/rules_client/rules_client.ts
@@ -144,7 +144,7 @@ export interface ConstructorOptions {
   ruleTypeRegistry: RuleTypeRegistry;
   minimumScheduleInterval: AlertingRulesConfig['minimumScheduleInterval'];
   encryptedSavedObjectsClient: EncryptedSavedObjectsClient;
-  spaceId?: string;
+  spaceId: string;
   namespace?: string;
   getUserName: () => Promise<string | null>;
   createAPIKey: (name: string) => Promise<CreateAPIKeyResult>;
@@ -281,7 +281,7 @@ const alertingAuthorizationFilterOpts: AlertingAuthorizationFilterOpts = {
 export class RulesClient {
   private readonly logger: Logger;
   private readonly getUserName: () => Promise<string | null>;
-  private readonly spaceId?: string;
+  private readonly spaceId: string;
   private readonly namespace?: string;
   private readonly taskManager: TaskManagerStartContract;
   private readonly unsecuredSavedObjectsClient: SavedObjectsClientContract;
