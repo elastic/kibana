dfa51d6d77d648cccb93e4e3956e2751726facbb
Anton Dosov <anton.dosov@elastic.co>
2022-04-28 16:06:38 +0200

    add sql api

--- src/plugins/vis_type_script/public/kibana_api/kibana_api.ts
+++ src/plugins/vis_type_script/public/kibana_api/kibana_api.ts
@@ -8,6 +8,12 @@
 
 import type * as estypes from '@elastic/elasticsearch/lib/api/types';
 import { lastValueFrom } from 'rxjs';
+import {
+  ENHANCED_ES_SEARCH_STRATEGY,
+  SQL_SEARCH_STRATEGY,
+  SqlSearchStrategyRequest,
+  SqlSearchStrategyResponse,
+} from '@kbn/data-plugin/common';
 import { DataPublicPluginStart } from '@kbn/data-plugin/public';
 import { VisSearchContext } from '../types';
 
@@ -15,11 +21,55 @@ export interface VisTypeScriptKibanaApiDeps {
   data: DataPublicPluginStart;
 }
 
-export interface EsSearchOptions {
+export interface ESSearchOptions {
   useKibanaContext: boolean;
+  timeField?: string;
 }
-export type ESSearchRequest = estypes.SearchRequest;
+
+// disallow transport properties that are handled by data_plugin and allow only query related properties
+const allowedESSearchRequestKeys = [
+  'index',
+  'aggregations',
+  'aggs',
+  'query',
+  'q',
+  'sort',
+  'from',
+] as const;
+export type ESSearchRequest = Pick<
+  estypes.SearchRequest,
+  typeof allowedESSearchRequestKeys[number]
+>;
 export type ESSearchResponse = estypes.SearchResponse;
+
+export interface SQLSearchOptions {
+  useKibanaContext: boolean;
+  timeField?: string;
+}
+
+const allowedSQLSearchRequestKeys = [
+  'query',
+  'columnar',
+  'cursor',
+  'fetch_size',
+  'filter',
+  'time_zone',
+] as const;
+export type SQLSearchRequest = Pick<
+  estypes.SqlQueryRequest,
+  typeof allowedSQLSearchRequestKeys[number]
+>;
+export type SQLSearchResponse = estypes.SqlQueryResponse;
+
+function sanitizeRequest<T extends Record<string, unknown>>(payload: T, allowedKeys: string[]): T {
+  if (payload == null || typeof payload !== 'object') return payload;
+  return allowedKeys.reduce((sanitized, allowedKey) => {
+    if (allowedKey in payload) {
+      sanitized[allowedKey] = payload[allowedKey];
+    }
+    return sanitized;
+  }, {} as Record<string, unknown>) as T;
+}
 
 export class VisTypeScriptKibanaApi {
   constructor(
@@ -29,15 +79,41 @@ export class VisTypeScriptKibanaApi {
 
   async esSearch(
     payload: ESSearchRequest,
-    { useKibanaContext = true }: EsSearchOptions = { useKibanaContext: true }
+    { useKibanaContext = true }: ESSearchOptions = { useKibanaContext: true }
   ): Promise<ESSearchResponse> {
+    payload = sanitizeRequest(payload, [...allowedESSearchRequestKeys]);
+
     if (useKibanaContext) {
       // TODO: adjust request based on this.visSearchContext
       // eslint-disable-next-line no-console
       console.log(this.visSearchContext);
     }
 
-    const response = await lastValueFrom(this.deps.data.search.search({ params: payload }));
+    const response = await lastValueFrom(
+      this.deps.data.search.search({ params: payload }, { strategy: ENHANCED_ES_SEARCH_STRATEGY })
+    );
+    return response.rawResponse;
+  }
+
+  async sqlSearch(
+    payload: SQLSearchRequest,
+    { useKibanaContext = true }: ESSearchOptions = { useKibanaContext: true }
+  ): Promise<SQLSearchResponse> {
+    payload = sanitizeRequest(payload, [...allowedSQLSearchRequestKeys]);
+
+    if (useKibanaContext) {
+      // TODO: adjust request based on this.visSearchContext
+      // eslint-disable-next-line no-console
+      console.log(this.visSearchContext);
+    }
+
+    const response = await lastValueFrom(
+      this.deps.data.search.search<SqlSearchStrategyRequest, SqlSearchStrategyResponse>(
+        { params: payload },
+        { strategy: SQL_SEARCH_STRATEGY }
+      )
+    );
+
     return response.rawResponse;
   }
 }
