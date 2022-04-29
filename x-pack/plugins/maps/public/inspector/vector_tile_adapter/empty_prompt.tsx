8a1f4a36519ff6647a2c4c388fcf6210b4edf40a
nreese <reese.nathan@elastic.co>
2022-04-29 15:36:15 -0600

    empty prompt

---
+++ x-pack/plugins/maps/public/inspector/vector_tile_adapter/empty_prompt.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import type { Adapters } from '@kbn/inspector-plugin/public';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { EuiEmptyPrompt } from '@elastic/eui';
+
+export function EmptyPrompt() {
+  return (
+    <EuiEmptyPrompt
+      title={
+        <h2>
+          <FormattedMessage
+            id="xpack.maps.inspector.vectorTile.noRequestsLoggedTitle"
+            defaultMessage="No vector tile requests logged"
+          />
+        </h2>
+      }
+      body={
+        <React.Fragment>
+          <p>
+            <FormattedMessage
+              id="xpack.maps.inspector.vectorTile.noRequestsLoggedDescription.mapHasNotLoggedAnyRequestsText"
+              defaultMessage="The map hasn't logged any vector tile requests (yet)."
+            />
+          </p>
+          <p>
+            <FormattedMessage
+              id="xpack.maps.inspector.vectorTile.noRequestsLoggedDescription.whatDoesItUsuallyMeanText"
+              defaultMessage="This usually means that there are no visible layers that request data from Elasticsearch vector tile search API."
+            />
+          </p>
+        </React.Fragment>
+      }
+    />
+  );
+}
--- x-pack/plugins/maps/public/inspector/vector_tile_adapter/vector_tile_view_component.tsx
+++ x-pack/plugins/maps/public/inspector/vector_tile_adapter/vector_tile_view_component.tsx
@@ -5,32 +5,92 @@
  * 2.0.
  */
 
+import _ from 'lodash';
 import React, { Component } from 'react';
 import type { Adapters } from '@kbn/inspector-plugin/public';
+import { EuiComboBox, EuiComboBoxOptionOption } from '@elastic/eui';
+import { EmptyPrompt } from './empty_prompt';
 
 interface Props {
   adapters: Adapters;
 }
 
-interface State {}
+interface State {
+  selectedLayerId: string | null;
+  tileRequests: TileRequest[];
+  layerOptions: Array<EuiComboBoxOptionOption<string>>;
+}
 
 class VectorTileViewComponent extends Component<Props, State> {
-  // state: State = this.props.adapters.vectorTiles.getMapState();
+  private _isMounted = false;
 
-  /* _onMapChange = () => {
-    this.setState(this.props.adapters.map.getMapState());
+  state: State = {
+    selectedLayerId: null,
+    tileRequests: [],
+    layerOptions: [],
   };
 
+  _onAdapterChange = () => {
+    const layerOptions = this.props.adapters.vectorTiles.getLayerOptions();
+    if (layerOptions.length === 0) {
+      this.setState({
+        selectedLayerId: null,
+        tileRequests: [],
+        layerOptions: [],
+      });
+      return;
+    }
+
+    const selectedLayerId =
+      this.state.selectedLayerId &&
+      layerOptions.some((layerOption) => {
+        return this.state.selectedLayerId === layerOption.value;
+      })
+        ? this.state.selectedLayerId
+        : layerOptions[0].value;
+
+    this.setState({
+      selectedLayerId,
+      tileRequests: this.props.adapters.vectorTiles.getTileRequests(selectedLayerId),
+      layerOptions,
+    });
+  };
+
+  _debouncedOnAdapterChange = _.debounce(() => {
+    if (this._isMounted) {
+      this._onAdapterChange();
+    }
+  }, 256);
+
   componentDidMount() {
-    this.props.adapters.map.on('change', this._onMapChange);
+    this._isMounted = true;
+    this._onAdapterChange();
+    this.props.adapters.map.on('change', this._debouncedOnAdapterChange);
   }
 
   componentWillUnmount() {
-    this.props.adapters.map.removeListener('change', this._onMapChange);
-  }*/
+    this._isMounted = false;
+    this.props.adapters.map.removeListener('change', this._debouncedOnAdapterChange);
+  }
+
+  _onLayerSelect = (selectedOptions: Array<EuiComboBoxOptionOption<string>>) => {
+    if (selectedOptions.length === 0) {
+      this.setState({
+        selectedLayerId: null,
+        tileRequests: [],
+      });
+      return;
+    }
+
+    const selectedLayerId = selectedOptions[0].value;
+    this.setState({
+      selectedLayerId,
+      tileRequests: this.props.adapters.vectorTiles.getTileRequests(selectedLayerId),
+    });
+  };
 
   render() {
-    return <div>Hello world</div>;
+    return this.state.layerOptions.length === 0 ? <EmptyPrompt /> : <div>Hello world</div>;
   }
 }
 
