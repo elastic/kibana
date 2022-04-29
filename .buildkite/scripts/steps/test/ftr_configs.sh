d3f5227950211ec6eb0493d70fbdb25ee252f7ae
Brian Seeders <brian.seeders@elastic.co>
2022-04-29 17:01:19 -0400

    Try retrying only failed configs

--- .buildkite/scripts/steps/test/ftr_configs.sh
+++ .buildkite/scripts/steps/test/ftr_configs.sh
@@ -8,32 +8,57 @@ source .buildkite/scripts/steps/test/test_group_env.sh
 export JOB_NUM=$BUILDKITE_PARALLEL_JOB
 export JOB=ftr-configs-${JOB_NUM}
 
+FAILED_CONFIGS_KEY="${BUILDKITE_STEP_ID}${BUILDKITE_PARALLEL_JOB:-0}"
+
 # a FTR failure will result in the script returning an exit code of 10
 exitCode=0
 
 configs="${FTR_CONFIG:-}"
+
+if [[ ! "$configs" && "${BUILDKITE_RETRY_COUNT:-0}" == "1" ]]; then
+  configs=$(buildkite-agent meta-data get "$FAILED_CONFIGS_KEY" --default '')
+  if [[ "$configs" ]]; then
+    echo "--- Retrying only failed configs"
+    echo "$configs"
+  fi
+fi
+
 if [[ "$configs" == "" ]]; then
   echo "--- downloading ftr test run order"
   buildkite-agent artifact download ftr_run_order.json .
   configs=$(jq -r '.groups[env.JOB_NUM | tonumber].names | .[]' ftr_run_order.json)
 fi
 
+failedConfigs=""
+
 while read -r config; do
+  if [[ ! "$config" ]]; then
+    continue;
+  fi
+
   echo "--- $ node scripts/functional_tests --bail --config $config"
   # prevent non-zero exit code from breaking the loop
   set +e;
-  node ./scripts/functional_tests \
-    --bail \
-    --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
-    --config="$config"
-  lastCode=$?
+
+  # node ./scripts/functional_tests \
+  #   --bail \
+  #   --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
+  #   --config="$config"
+  # lastCode=$?
+  echo "$config"
+  lastCode=$(((1 + $RANDOM % 2) - 1))
   set -e;
 
   if [ $lastCode -ne 0 ]; then
     exitCode=10
     echo "FTR exited with code $lastCode"
     echo "^^^ +++"
+    failedConfigs="${failedConfigs}"$'\n'"$config"
   fi
 done <<< "$configs"
+
+if [[ "$failedConfigs" ]]; then
+  buildkite-agent meta-data set "$FAILED_CONFIGS_KEY" "$failedConfigs"
+fi
 
 exit $exitCode
