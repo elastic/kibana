da90d4abf39d87e7c88a4e8db17ccfe43c042c33
Jen Huang <jen.huang@elastic.co>
2022-04-28 23:16:23 -0700

    Renable logs disabled callout for managed policies (#131204)

--- x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_details_page/components/agent_logs/agent_logs.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_details_page/components/agent_logs/agent_logs.tsx
@@ -84,25 +84,27 @@ const AgentPolicyLogsNotEnabledCallout: React.FunctionComponent<{ agentPolicy: A
           />
         }
       >
-        <FormattedMessage
-          id="xpack.fleet.agentLogs.logDisabledCallOutDescription"
-          defaultMessage="Update the agent's policy {settingsLink} to enable logs collection."
-          values={{
-            settingsLink: (
-              <EuiLink
-                href={getHref('policy_details', {
-                  policyId: agentPolicy.id,
-                  tabId: 'settings',
-                })}
-              >
-                <FormattedMessage
-                  id="xpack.fleet.agentLogs.settingsLink"
-                  defaultMessage="settings"
-                />
-              </EuiLink>
-            ),
-          }}
-        />
+        {agentPolicy.is_managed ? null : (
+          <FormattedMessage
+            id="xpack.fleet.agentLogs.logDisabledCallOutDescription"
+            defaultMessage="Update the agent's policy {settingsLink} to enable logs collection."
+            values={{
+              settingsLink: (
+                <EuiLink
+                  href={getHref('policy_details', {
+                    policyId: agentPolicy.id,
+                    tabId: 'settings',
+                  })}
+                >
+                  <FormattedMessage
+                    id="xpack.fleet.agentLogs.settingsLink"
+                    defaultMessage="settings"
+                  />
+                </EuiLink>
+              ),
+            }}
+          />
+        )}
       </EuiCallOut>
     </EuiFlexItem>
   );
@@ -278,9 +280,9 @@ export const AgentLogsUI: React.FunctionComponent<AgentLogsProps> = memo(
 
     return (
       <WrapperFlexGroup direction="column" gutterSize="m">
-        {agentPolicy &&
-          !agentPolicy.monitoring_enabled?.includes('logs') &&
-          !agentPolicy.is_managed && <AgentPolicyLogsNotEnabledCallout agentPolicy={agentPolicy} />}
+        {agentPolicy && !agentPolicy.monitoring_enabled?.includes('logs') && (
+          <AgentPolicyLogsNotEnabledCallout agentPolicy={agentPolicy} />
+        )}
         <EuiFlexItem grow={false}>
           <EuiFlexGroup gutterSize="m">
             <EuiFlexItem>
