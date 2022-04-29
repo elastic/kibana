d44ce29f65d7010260082c9fb04e39176d7d238a
spalger <spencer@elastic.co>
2022-04-28 22:17:48 -0500

    split up maps tests

--- x-pack/test/functional/apps/maps/group4/index.js
+++ x-pack/test/functional/apps/maps/group4/index.js
@@ -58,46 +58,18 @@ export default function ({ loadTestFile, getService }) {
       );
     });
 
-    describe('', async function () {
-      this.tags('ciGroup9');
-      loadTestFile(require.resolve('./documents_source'));
-      loadTestFile(require.resolve('./blended_vector_layer'));
-      loadTestFile(require.resolve('./vector_styling'));
-      loadTestFile(require.resolve('./saved_object_management'));
-      loadTestFile(require.resolve('./sample_data'));
-      loadTestFile(require.resolve('./auto_fit_to_bounds'));
-      loadTestFile(require.resolve('./layer_visibility'));
-      loadTestFile(require.resolve('./feature_controls/maps_security'));
-      loadTestFile(require.resolve('./feature_controls/maps_spaces'));
-      loadTestFile(require.resolve('./full_screen_mode'));
-    });
-
-    describe('', function () {
-      this.tags('ciGroup22');
-      loadTestFile(require.resolve('./es_geo_grid_source'));
-      loadTestFile(require.resolve('./embeddable'));
-    });
-
-    describe('', function () {
-      this.tags('ciGroup2'); // same group used in x-pack/test/reporting_functional
-      loadTestFile(require.resolve('./reports'));
-    });
-
-    describe('', function () {
-      this.tags('ciGroup10');
-      loadTestFile(require.resolve('./es_pew_pew_source'));
-      loadTestFile(require.resolve('./joins'));
-      loadTestFile(require.resolve('./mvt_joins'));
-      loadTestFile(require.resolve('./mapbox_styles'));
-      loadTestFile(require.resolve('./mvt_scaling'));
-      loadTestFile(require.resolve('./mvt_geotile_grid'));
-      loadTestFile(require.resolve('./add_layer_panel'));
-      loadTestFile(require.resolve('./file_upload'));
-      loadTestFile(require.resolve('./layer_errors'));
-      loadTestFile(require.resolve('./visualize_create_menu'));
-      loadTestFile(require.resolve('./discover'));
-      loadTestFile(require.resolve('./geofile_wizard_auto_open'));
-      loadTestFile(require.resolve('./lens'));
-    });
+    loadTestFile(require.resolve('./es_pew_pew_source'));
+    loadTestFile(require.resolve('./joins'));
+    loadTestFile(require.resolve('./mvt_joins'));
+    loadTestFile(require.resolve('./mapbox_styles'));
+    loadTestFile(require.resolve('./mvt_scaling'));
+    loadTestFile(require.resolve('./mvt_geotile_grid'));
+    loadTestFile(require.resolve('./add_layer_panel'));
+    loadTestFile(require.resolve('./file_upload'));
+    loadTestFile(require.resolve('./layer_errors'));
+    loadTestFile(require.resolve('./visualize_create_menu'));
+    loadTestFile(require.resolve('./discover'));
+    loadTestFile(require.resolve('./geofile_wizard_auto_open'));
+    loadTestFile(require.resolve('./lens'));
   });
 }
