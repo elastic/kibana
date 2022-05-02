84f100352d8de3136aa62e357c728580bbe1efce
Ido Cohen <ido.cohen@elastic.co>
2022-05-02 10:08:10 +0300

    fix code review comments for changing constans names

--- x-pack/plugins/cloud_security_posture/common/constants.ts
+++ x-pack/plugins/cloud_security_posture/common/constants.ts
@@ -13,8 +13,6 @@ export const UPDATE_RULES_CONFIG_ROUTE_PATH =
 
 export const CLOUD_SECURITY_POSTURE_PACKAGE_NAME = 'cloud_security_posture';
 
-export const AGENT_LOGS_INDEX_PATTERN = '.logs-cloud_security_posture.metadata*';
-export const CSP_LATEST_FINDINGS_DATA_VIEW = 'logs-cloud_security_posture.findings_latest-*';
 export const FINDINGS_INDEX_PATTERN = 'logs-cloud_security_posture.findings-default*';
 
 export const LATEST_FINDINGS_INDEX_NAME = 'cloud_security_posture.findings_latest';
