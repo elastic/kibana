495d422b1dccf9fd194a623111580a91ebf07a6c
spalger <spencer@elastic.co>
2022-05-02 15:07:42 -0500

    refuse to execute config files which aren't listed in the .buildkite/ftr_configs.yml

--- packages/kbn-test/src/functional_test_runner/lib/config/read_config_file.ts
+++ packages/kbn-test/src/functional_test_runner/lib/config/read_config_file.ts
@@ -6,25 +6,45 @@
  * Side Public License, v 1.
  */
 
+import Path from 'path';
+import Fs from 'fs';
+
 import { ToolingLog } from '@kbn/tooling-log';
 import { defaultsDeep } from 'lodash';
+import { REPO_ROOT } from '@kbn/utils';
+import { createFlagError } from '@kbn/dev-utils';
+import JsYaml from 'js-yaml';
 
 import { Config } from './config';
 import { EsVersion } from '../es_version';
 
 const cache = new WeakMap();
+const FTR_MANIFEST_REL = '.buildkite/ftr_configs.yml';
+const ftrConfigsManifest = JsYaml.safeLoad(
+  Fs.readFileSync(Path.resolve(REPO_ROOT, FTR_MANIFEST_REL), 'utf8')
+);
+const VALID_FTR_CONFIG_PATHS = Object.values(ftrConfigsManifest).flat() as string[];
 
 async function getSettingsFromFile(
   log: ToolingLog,
   esVersion: EsVersion,
-  path: string,
-  settingOverrides: any
+  options: {
+    path: string;
+    settingOverrides: any;
+    primary: boolean;
+  }
 ) {
-  const configModule = require(path); // eslint-disable-line @typescript-eslint/no-var-requires
+  if (options.primary && !VALID_FTR_CONFIG_PATHS.includes(options.path)) {
+    throw createFlagError(
+      `Refusing to load FTR Config which is not listed in [${FTR_MANIFEST_REL}]. All FTR Config files must be listed there, use the "enabled" key if the FTR Config should be run on automatically on PR CI, or the "disabled" key if it is run manually or by a special job.`
+    );
+  }
+
+  const configModule = require(options.path); // eslint-disable-line @typescript-eslint/no-var-requires
   const configProvider = configModule.__esModule ? configModule.default : configModule;
 
   if (!cache.has(configProvider)) {
-    log.debug('Loading config file from %j', path);
+    log.debug('Loading config file from %j', options.path);
     cache.set(
       configProvider,
       configProvider({
@@ -32,7 +52,11 @@ async function getSettingsFromFile(
         esVersion,
         async readConfigFile(p: string, o: any) {
           return new Config({
-            settings: await getSettingsFromFile(log, esVersion, p, o),
+            settings: await getSettingsFromFile(log, esVersion, {
+              path: p,
+              settingOverrides: o,
+              primary: false,
+            }),
             primary: false,
             path: p,
           });
@@ -43,7 +67,7 @@ async function getSettingsFromFile(
 
   const settingsWithDefaults: any = defaultsDeep(
     {},
-    settingOverrides,
+    options.settingOverrides,
     await cache.get(configProvider)!
   );
 
@@ -57,7 +81,11 @@ export async function readConfigFile(
   settingOverrides: any = {}
 ) {
   return new Config({
-    settings: await getSettingsFromFile(log, esVersion, path, settingOverrides),
+    settings: await getSettingsFromFile(log, esVersion, {
+      path,
+      settingOverrides,
+      primary: true,
+    }),
     primary: true,
     path,
   });
