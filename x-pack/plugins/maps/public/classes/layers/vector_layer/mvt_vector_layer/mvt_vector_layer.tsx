bc6636a84508d6ed8a813ca24321ab6b49a4a94c
Nathan Reese <reese.nathan@elastic.co>
2022-05-02 12:42:42 -0600

    [Reporting/Maps] fix background tiles in a map panel might not load in a screenshot report (#131185)
    * [Reporting/Maps] fix background tiles in a map panel might not load in a screenshot report
    
    * add unit tests
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/maps/public/classes/layers/vector_layer/mvt_vector_layer/mvt_vector_layer.tsx
+++ x-pack/plugins/maps/public/classes/layers/vector_layer/mvt_vector_layer/mvt_vector_layer.tsx
@@ -68,6 +68,12 @@ export class MvtVectorLayer extends AbstractVectorLayer {
     this._source = args.source as IMvtVectorSource;
   }
 
+  isInitialDataLoadComplete(): boolean {
+    return this._descriptor.__areTilesLoaded === undefined || !this._descriptor.__areTilesLoaded
+      ? false
+      : super.isInitialDataLoadComplete();
+  }
+
   async getBounds(syncContext: DataRequestContext) {
     // Add filter to narrow bounds to features with matching join keys
     let joinKeyFilter;
