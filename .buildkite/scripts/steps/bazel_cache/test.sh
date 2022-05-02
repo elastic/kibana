39975027afda5b5cb085b1259bc0d05feb1a73f5
Brian Seeders <brian.seeders@elastic.co>
2022-05-02 14:47:40 -0400

    Overwrite bazelrc altogether

--- .buildkite/scripts/steps/bazel_cache/test.sh
+++ .buildkite/scripts/steps/bazel_cache/test.sh
@@ -6,7 +6,8 @@ source .buildkite/scripts/common/util.sh
 
 REMOTE_CACHE_PASSWORD="$(retry 5 5 vault read -field=password secret/kibana-issues/dev/bazel-remote-cache-test)"
 
-cat << EOF >> .bazelrc
+cat << EOF > .bazelrc
+import %workspace%/.bazelrc.common
 build --remote_cache=grpc://test:${REMOTE_CACHE_PASSWORD}@34.121.74.141:9092
 EOF
 
