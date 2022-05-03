31d86f0e9836f23edcd4c6adfb712fd2995e574c
mgiota <panagiota.mitsopoulou@elastic.co>
2022-05-03 23:27:09 +0200

    [Actionable Observability] Add license modal to rules table (#131232)
    * Add fix license link
    
    * fix localization
    
    * fix CI error
    
    * fix more translation issues
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/observability/public/pages/rules/types.ts
+++ x-pack/plugins/observability/public/pages/rules/types.ts
@@ -45,6 +45,8 @@ export interface StatusFilterProps {
 
 export interface ExecutionStatusProps {
   executionStatus: RuleExecutionStatus;
+  item: RuleTableItem;
+  licenseType: string;
 }
 
 export interface LastRunProps {
