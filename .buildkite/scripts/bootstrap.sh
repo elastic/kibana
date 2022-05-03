3d8d11230ee7a90569d0cf8bfc4baf749898953f
Brian Seeders <brian.seeders@elastic.co>
2022-05-03 15:30:01 -0400

    Prepare final changes for bazel remote cache in CI

--- .buildkite/scripts/bootstrap.sh
+++ .buildkite/scripts/bootstrap.sh
@@ -5,22 +5,7 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 source .buildkite/scripts/common/setup_bazel.sh
 
-BAZEL_REGION="us-central1"
-if [[ "$(curl -is metadata.google.internal || true)" ]]; then
-  # projects/1003139005402/zones/us-central1-a -> us-central1-a -> us-central1
-  BAZEL_REGION=$(curl -sH Metadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/zone | rev | cut -d'/' -f1 | cut -c3- | rev)
-fi
-
-BAZEL_BUCKET="kibana-ci-bazel_$BAZEL_REGION"
-
-cat << EOF > .bazelrc
-import %workspace%/.bazelrc.common
-build --remote_cache=https://storage.googleapis.com/$BAZEL_BUCKET
-build --google_default_credentials
-EOF
-
 echo "--- yarn install and bootstrap"
-echo "Using Bazel remote cache bucket: $BAZEL_BUCKET"
 
 if ! yarn kbn bootstrap; then
   echo "bootstrap failed, trying again in 15 seconds"
