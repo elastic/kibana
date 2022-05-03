918cc38c0af467cb1e14c57586f075d58d06d946
Kristof-Pierre Cummings <kristofpierre.cummings@elastic.co>
2022-05-03 16:55:03 -0500

    stability fix. remove useUserInfo

--- x-pack/plugins/security_solution/public/overview/pages/detection_response.tsx
+++ x-pack/plugins/security_solution/public/overview/pages/detection_response.tsx
@@ -24,6 +24,8 @@ import { UserAlertsTable } from '../components/detection_response/user_alerts_ta
 import * as i18n from './translations';
 import { CasesTable } from '../components/detection_response/cases_table';
 import { CasesByStatus } from '../components/detection_response/cases_by_status';
+import { useSignalIndex } from '../../detections/containers/detection_engine/alerts/use_signal_index';
+import { useAlertsPrivileges } from '../../detections/containers/detection_engine/alerts/use_alerts_privileges';
 
 const NoPrivilegePage: React.FC = () => {
   const { docLinks } = useKibana().services;
@@ -50,9 +52,10 @@ const NoPrivilegePage: React.FC = () => {
 
 const DetectionResponseComponent = () => {
   const { indicesExist, indexPattern, loading: isSourcererLoading } = useSourcererDataView();
-  const { signalIndexName, canUserREAD, hasIndexRead } = useUserInfo();
+  const { signalIndexName } = useSignalIndex();
+  const { hasKibanaREAD, hasIndexRead } = useAlertsPrivileges();
   const canReadCases = useGetUserCasesPermissions()?.read;
-  const canReadAlerts = canUserREAD && hasIndexRead;
+  const canReadAlerts = hasKibanaREAD && hasIndexRead;
 
   if (!canReadAlerts && !canReadCases) {
     return <NoPrivilegePage />;
