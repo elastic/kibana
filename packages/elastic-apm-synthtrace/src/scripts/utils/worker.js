elastic/kibana

commit 3730dd0779ed8efd74aee88f57422781ec1ac122

fix all violations

packages/elastic-apm-synthtrace/src/scripts/utils/worker.js
@@ -12,4 +12,4 @@ require('@babel/register')({
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
 });
 
-require('./worker.ts');
+require('./worker');

commit df590ba152991609629194480aa1d57701893a54

Fixes worker silent failure after style violations fixes
That were introduced with: https://github.com/elastic/kibana/commit/3730dd0779ed8efd74aee88f57422781ec1ac122

packages/elastic-apm-synthtrace/src/scripts/utils/worker.js
@@ -12,4 +12,4 @@ require('@babel/register')({
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
 });
 
-require('./worker');
+require('./synthtrace_worker');