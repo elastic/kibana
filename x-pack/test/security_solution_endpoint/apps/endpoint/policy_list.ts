66f5ad90d36714801b8636bf12e25f50fcc44197
Candace Park <candace.park@elastic.co>
2022-05-03 15:23:19 -0400

    add awaits for async page object functions

--- x-pack/test/security_solution_endpoint/apps/endpoint/policy_list.ts
+++ x-pack/test/security_solution_endpoint/apps/endpoint/policy_list.ts
@@ -47,7 +47,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       it('navigates back to the policy list page', async () => {
         const cancelButton = await testSubjects.find('createPackagePolicy_cancelBackLink');
         cancelButton.click();
-        pageObjects.policy.ensureIsOnListPage();
+        await pageObjects.policy.ensureIsOnListPage();
       });
     });
     describe('with policies', () => {
@@ -73,38 +73,37 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       it('navigates to the policy details page when the policy name is clicked and returns back to the policy list page using the header back button', async () => {
         const policyName = (await testSubjects.findAll('policyNameCellLink'))[0];
         await policyName.click();
-        pageObjects.policy.ensureIsOnDetailsPage();
+        await pageObjects.policy.ensureIsOnDetailsPage();
         const backButton = await testSubjects.find('policyDetailsBackLink');
         await backButton.click();
-        pageObjects.policy.ensureIsOnListPage();
+        await pageObjects.policy.ensureIsOnListPage();
       });
       describe('when the endpoint count link is clicked', () => {
         it('navigates to the endpoint list page filtered by policy', async () => {
           const endpointCount = (await testSubjects.findAll('policyEndpointCountLink'))[1];
           await endpointCount.click();
-          pageObjects.endpoint.ensureIsOnEndpointListPage();
+          await pageObjects.endpoint.ensureIsOnEndpointListPage();
         });
         it('admin searchbar contains the selected policy id', async () => {
           const expectedPolicyId = indexedData.integrationPolicies[0].id;
-          pageObjects.endpoint.ensureIsOnEndpointListPage();
+          await pageObjects.endpoint.ensureIsOnEndpointListPage();
           expect(await testSubjects.getVisibleText('adminSearchBar')).to.equal(
             `united.endpoint.Endpoint.policy.applied.id : "${expectedPolicyId}"`
           );
         });
         it('endpoint table shows the endpoints associated with selected policy', async () => {
           const expectedPolicyName = indexedData.integrationPolicies[0].name;
-          pageObjects.endpoint.ensureIsOnEndpointListPage();
-          await testSubjects.existOrFail('endpointListTable');
+          await pageObjects.endpoint.ensureIsOnEndpointListPage();
           const policyName = (await testSubjects.findAll('policyNameCellLink'))[0];
           expect(await policyName.getVisibleText()).to.be.equal(
             expectedPolicyName.substring(0, expectedPolicyName.indexOf('-'))
           );
         });
         it('returns back to the policy list page when the header back button is clicked', async () => {
-          pageObjects.endpoint.ensureIsOnEndpointListPage();
+          await pageObjects.endpoint.ensureIsOnEndpointListPage();
           const backButton = await testSubjects.find('endpointListBackLink');
           await backButton.click();
-          pageObjects.policy.ensureIsOnListPage();
+          await pageObjects.policy.ensureIsOnListPage();
         });
       });
     });
