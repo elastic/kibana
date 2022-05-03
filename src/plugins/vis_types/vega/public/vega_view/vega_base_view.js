58cee08cc4c8a77108c21109fdc671fc7787e895
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2022-05-03 09:07:24 +0200

    [Vega] fix requesting not permitted or used data views (#130878)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/vis_types/vega/public/vega_view/vega_base_view.js
+++ src/plugins/vis_types/vega/public/vega_view/vega_base_view.js
@@ -162,7 +162,7 @@ export class VegaBaseView {
     let idxObj;
 
     if (index) {
-      [idxObj] = await dataViews.find(index);
+      [idxObj] = await dataViews.find(index, 1);
       if (!idxObj) {
         throw new Error(
           i18n.translate('visTypeVega.vegaParser.baseView.indexNotFoundErrorMessage', {
