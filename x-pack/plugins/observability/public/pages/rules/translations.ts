066cecb32ec9598a67f1e49de88a7f57537ef69e
mgiota <panagiota.mitsopoulou@elastic.co>
2022-05-04 19:57:32 +0200

    [Actionable Observability] consume shareable status filter (#131219)
    * use shareable status filter
    
    * clean up and remove remnants from old status implementation
    
    * bring back props
    
    * fix more CI issues after renaming
    
    * fix i118n issues
    
    * fix failing unit tests

--- x-pack/plugins/observability/public/pages/rules/translations.ts
+++ x-pack/plugins/observability/public/pages/rules/translations.ts
@@ -53,27 +53,6 @@ export const RULE_STATUS_WARNING = i18n.translate(
   }
 );
 
-export const RULE_STATUS_ENABLED = i18n.translate(
-  'xpack.observability.rules.rulesTable.ruleStatusEnabled',
-  {
-    defaultMessage: 'Enabled',
-  }
-);
-
-export const RULE_STATUS_DISABLED = i18n.translate(
-  'xpack.observability.rules.rulesTable.ruleStatusDisabled',
-  {
-    defaultMessage: 'Disabled',
-  }
-);
-
-export const RULE_STATUS_SNOOZED_INDEFINITELY = i18n.translate(
-  'xpack.observability.rules.rulesTable.ruleStatusSnoozedIndefinitely',
-  {
-    defaultMessage: 'Snoozed indefinitely',
-  }
-);
-
 export const LAST_RESPONSE_COLUMN_TITLE = i18n.translate(
   'xpack.observability.rules.rulesTable.columns.lastResponseTitle',
   {
