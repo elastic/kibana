4a9af058b615d94bd577960efaefd8f63ab2e001
Alexey Antonov <alexwizp@gmail.com>
2022-05-04 18:20:50 +0300

    move RelinkSavedObject to lazy

--- src/plugins/visualizations/public/components/relink_saved_object/index.ts
+++ src/plugins/visualizations/public/components/relink_saved_object/index.ts
@@ -5,8 +5,25 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
+import { withSuspense } from '@kbn/shared-ux-utility';
+import React from 'react';
 
-export { RelinkSavedObject } from './relink_saved_object';
+/**
+ *  A `RelinkSavedObject` component, with service hooks. Consumers should use `React.Suspennse` or the
+ * `withSuspense` HOC to load this component.
+ */
+export const RelinkSavedObjectLazy = React.lazy(() =>
+  import('./relink_saved_object').then(({ RelinkSavedObject }) => ({
+    default: RelinkSavedObject,
+  }))
+);
+
+/**
+ * A `RelinkSavedObject` component. The component is wrapped by the `withSuspense` HOC.
+ * This component can be used directly by consumers and will load the `RelinkSavedObjectLazy` lazily with
+ * a predefined fallback and error boundary.
+ */
+export const RelinkSavedObject = withSuspense(RelinkSavedObjectLazy);
 
 export {
   shouldShowRelinkSavedObjectError,
