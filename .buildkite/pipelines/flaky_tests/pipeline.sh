0c8ffa535d30267082f4cb1513bc1be18cb354ec
spalger <spencer@elastic.co>
2022-05-04 10:30:58 -0500

    remove pipeline/runner distinction

--- .buildkite/pipelines/flaky_tests/pipeline.sh
+++ .buildkite/pipelines/flaky_tests/pipeline.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-if [[ "$KIBANA_FLAKY_TEST_RUNNER_CONFIG" == "" ]]; then
+export config="${KIBANA_FLAKY_TEST_RUNNER_CONFIG:-}"
+
+if [[ "$config" == "" ]]; then
   echo "Please use the 'Trigger Flaky Test Runner' UI to trigger flaky test executions";
   echo ""
   echo "    https://ci-stats.kibana.dev/trigger_flaky_test_runner    "
@@ -10,3 +12,8 @@ if [[ "$KIBANA_FLAKY_TEST_RUNNER_CONFIG" == "" ]]; then
   echo ""
   exit 1
 fi
+
+UUID="$(cat /proc/sys/kernel/random/uuid)"
+export UUID
+
+node .buildkite/pipelines/flaky_tests/pipeline.js | buildkite-agent pipeline upload
