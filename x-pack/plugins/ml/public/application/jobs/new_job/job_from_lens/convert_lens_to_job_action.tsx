fec60376f58cd7706319e15d903a757e912d2eeb
James Gowdy <jgowdy@elastic.co>
2022-05-04 16:49:53 +0100

    refactoring url generation

--- x-pack/plugins/ml/public/application/jobs/new_job/job_from_lens/convert_lens_to_job_action.tsx
+++ x-pack/plugins/ml/public/application/jobs/new_job/job_from_lens/convert_lens_to_job_action.tsx
@@ -11,9 +11,20 @@ import { getJobsItemsFromEmbeddable } from './create_job';
 
 export function convertLensToADJob(embeddable: Embeddable) {
   const { query, filters, to, from, vis } = getJobsItemsFromEmbeddable(embeddable);
-  const visRison = rison.encode(vis as any);
+  const visRison = rison.encode<any>(vis);
   const queryRison = rison.encode(query);
   const filtersRison = rison.encode(filters);
-  const url = `ml/jobs/new_job/from_lens?vis=${visRison}&from=${from}&to=${to}&query=${queryRison}&filters=${filtersRison}`;
+
+  const params = [
+    ['vis', visRison],
+    ['from', from],
+    ['to', to],
+    ['query', queryRison],
+    ['filters', filtersRison],
+  ]
+    .map(([a, b]) => `${a}=${b}`)
+    .join('&');
+
+  const url = `ml/jobs/new_job/from_lens?${params}`;
   window.open(url, '_blank');
 }
