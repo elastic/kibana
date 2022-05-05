4292def2ceda1e8e446d63e102a24d6417fffbd5
Julia Rechkunova <julia.rechkunova@elastic.co>
2022-05-05 11:34:43 +0200

    [Discover] Provide direct link from sample data UI to Discover (#130108)
    * [Discover] Allow to view sample data in Discover
    
    * [Discover] Update deps format
    
    * [Discover] Define order of items in the context menu
    
    * [Discover] Update for tests
    
    * [Discover] Add upgrade tests
    
    * [Discover] Add a test for ordering appLinks
    
    * [Discover] Use existing helpers
    
    * [Discover] Add 7 days time range to Discover link
    
    * [Discover] Rename the helper
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/discover/common/index.ts
+++ src/plugins/discover/common/index.ts
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 
+export const APP_ICON = 'discoverApp';
 export const DEFAULT_COLUMNS_SETTING = 'defaultColumns';
 export const SAMPLE_SIZE_SETTING = 'discover:sampleSize';
 export const SORT_DEFAULT_ORDER_SETTING = 'discover:sort:defaultOrder';
--- x-pack/test/upgrade/apps/discover/discover_smoke_tests.ts
+++ x-pack/test/upgrade/apps/discover/discover_smoke_tests.ts
@@ -8,7 +8,7 @@
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
-export default function ({ getPageObjects, getService }: FtrProviderContext) {
+export default function ({ getPageObjects }: FtrProviderContext) {
   const PageObjects = getPageObjects(['common', 'header', 'home', 'discover', 'timePicker']);
 
   describe('upgrade discover smoke tests', function describeIndexTests() {
@@ -18,9 +18,9 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
     ];
 
     const discoverTests = [
-      { name: 'kibana_sample_data_flights', timefield: true, hits: '' },
-      { name: 'kibana_sample_data_logs', timefield: true, hits: '' },
-      { name: 'kibana_sample_data_ecommerce', timefield: true, hits: '' },
+      { name: 'flights', timefield: true, hits: '' },
+      { name: 'logs', timefield: true, hits: '' },
+      { name: 'ecommerce', timefield: true, hits: '' },
     ];
 
     spaces.forEach(({ space, basePath }) => {
@@ -31,8 +31,37 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
               basePath,
             });
             await PageObjects.header.waitUntilLoadingHasFinished();
-            await PageObjects.discover.selectIndexPattern(name);
+            await PageObjects.discover.selectIndexPattern(`kibana_sample_data_${name}`);
             await PageObjects.discover.waitUntilSearchingHasFinished();
+            if (timefield) {
+              await PageObjects.timePicker.setCommonlyUsedTime('Last_24 hours');
+              await PageObjects.discover.waitUntilSearchingHasFinished();
+            }
+          });
+          it('shows hit count greater than zero', async () => {
+            const hitCount = await PageObjects.discover.getHitCount();
+            if (hits === '') {
+              expect(hitCount).to.be.greaterThan(0);
+            } else {
+              expect(hitCount).to.be.equal(hits);
+            }
+          });
+          it('shows table rows not empty', async () => {
+            const tableRows = await PageObjects.discover.getDocTableRows();
+            expect(tableRows.length).to.be.greaterThan(0);
+          });
+        });
+      });
+
+      discoverTests.forEach(({ name, timefield, hits }) => {
+        describe('space: ' + space + ', name: ' + name, () => {
+          before(async () => {
+            await PageObjects.common.navigateToActualUrl('home', '/tutorial_directory/sampleData', {
+              basePath,
+            });
+            await PageObjects.header.waitUntilLoadingHasFinished();
+            await PageObjects.home.launchSampleDiscover(name);
+            await PageObjects.header.waitUntilLoadingHasFinished();
             if (timefield) {
               await PageObjects.timePicker.setCommonlyUsedTime('Last_24 hours');
               await PageObjects.discover.waitUntilSearchingHasFinished();
