f93e28e669843c151bcab635977019cb549a17d8
Brian Seeders <brian.seeders@elastic.co>
2022-05-05 12:07:38 -0400

    Also include in-memory workspaces

--- .buildkite/scripts/bootstrap.sh
+++ .buildkite/scripts/bootstrap.sh
@@ -8,9 +8,9 @@ source .buildkite/scripts/common/setup_bazel.sh
 echo "--- yarn install and bootstrap"
 
 # Use the node_modules that is baked into the agent image, if it exists, as a cache
-# But only for agents not mounting the workspace on a local ssd
+# But only for agents not mounting the workspace on a local ssd or in memory
 # It actually ends up being slower to move all of the tiny files between the disks vs extracting archives from the yarn cache
-if [[ -d ~/.kibana/node_modules && "$(pwd)" != *"/local-ssd/"* ]]; then
+if [[ -d ~/.kibana/node_modules && "$(pwd)" != *"/local-ssd/"* && "$(pwd)" != "/dev/shm"* ]]; then
   echo "Using ~/.kibana/node_modules as a starting point"
   mv ~/.kibana/node_modules ./
 fi
