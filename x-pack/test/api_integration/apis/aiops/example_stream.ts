cbc8b541acdb1acd0c5cc29069b934f6452501c0
Walter Rafelsberger <walter@elastic.co>
2022-05-05 13:46:59 +0200

    [ML] API integration test to read stream.

--- x-pack/test/api_integration/apis/aiops/example_stream.ts
+++ x-pack/test/api_integration/apis/aiops/example_stream.ts
@@ -12,11 +12,17 @@
  * 2.0.
  */
 
+import fetch from 'node-fetch';
+import { format as formatUrl } from 'url';
+
 import expect from '@kbn/expect';
+
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default ({ getService }: FtrProviderContext) => {
   const supertest = getService('supertest');
+  const config = getService('config');
+  const kibanaServerUrl = formatUrl(config.get('servers.kibana'));
 
   describe('POST /internal/aiops/example_stream', () => {
     it('should return full data without streaming', async () => {
@@ -51,6 +57,55 @@ export default ({ getService }: FtrProviderContext) => {
       expect(progressData.length).to.be(100);
       expect(progressData[0].payload).to.be(1);
       expect(progressData[progressData.length - 1].payload).to.be(100);
+    });
+
+    it('should return data in chunks with streaming', async () => {
+      const response = await fetch(`${kibanaServerUrl}/internal/aiops/example_stream`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'kbn-xsrf': 'stream',
+        },
+        body: JSON.stringify({ timeout: 1 }),
+      });
+
+      const stream = response.body;
+
+      expect(stream).not.to.be(null);
+
+      if (stream !== null) {
+        let partial = '';
+        let throwed = false;
+        const progressData: any[] = [];
+
+        try {
+          for await (const value of stream) {
+            const full = `${partial}${value}`;
+            const parts = full.split('\n');
+            const last = parts.pop();
+
+            partial = last ?? '';
+
+            const actions = parts.map((p) => JSON.parse(p));
+
+            actions.forEach((action) => {
+              expect(typeof action.type).to.be('string');
+
+              if (action.type === 'update_progress') {
+                progressData.push(action);
+              }
+            });
+          }
+        } catch (e) {
+          throwed = true;
+        }
+
+        expect(throwed).to.be(false);
+
+        expect(progressData.length).to.be(100);
+        expect(progressData[0].payload).to.be(1);
+        expect(progressData[progressData.length - 1].payload).to.be(100);
+      }
     });
   });
 };
