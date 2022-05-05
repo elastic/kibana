22aeb212411e471e53b61c7ca2d46d19940b6ca8
Brian Seeders <brian.seeders@elastic.co>
2022-05-05 13:26:34 -0400

    [CI] Split check types into its own step, use spot for checks (#131096)

--- .buildkite/scripts/steps/check_types.sh
+++ .buildkite/scripts/steps/check_types.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
+.buildkite/scripts/bootstrap.sh
+
 echo --- Check Types
 checks-reporter-with-killswitch "Check Types" \
   node scripts/type_check
