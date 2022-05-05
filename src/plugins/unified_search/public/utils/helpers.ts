03f418fdaaa1aef3a010d48432e1803564e3491b
Nodir Latipov <nodir.latypov@gmail.com>
2022-05-05 22:08:32 +0500

    refact: show error message

--- src/plugins/unified_search/public/utils/helpers.ts
+++ src/plugins/unified_search/public/utils/helpers.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import { IFieldType } from '@kbn/data-views-plugin/common';
+import type { IFieldType } from '@kbn/data-views-plugin/common';
 import { i18n } from '@kbn/i18n';
 import { isEmpty } from 'lodash';
 import { validateParams } from '../filter_bar/filter_editor/lib/filter_editor_utils';
@@ -16,32 +16,32 @@ export const getFieldValidityAndErrorMessage = (
   value?: string | undefined
 ): { isInvalid: boolean; errorMessage: string[] } => {
   const type = field.type;
-  let isInvalid = false;
-  let errorMessage = [''];
-
   switch (type) {
     case 'string':
-      isInvalid = !validateParams(value, field);
-      break;
+      return {
+        isInvalid: !validateParams(value, field),
+        errorMessage: [''],
+      };
     case 'date':
     case 'date_range':
-      isInvalid = !isEmpty(value) && !validateParams(value, field);
-      errorMessage = [
-        i18n.translate('unifiedSearch.filter.filterBar.invalidDateFormatProvidedErrorMessage', {
-          defaultMessage: 'Invalid date format provided',
-        }),
-      ];
-      break;
+      return {
+        isInvalid: !isEmpty(value) && !validateParams(value, field),
+        errorMessage: [
+          i18n.translate('unifiedSearch.filter.filterBar.invalidDateFormatProvidedErrorMessage', {
+            defaultMessage: 'Invalid date format provided',
+          }),
+        ],
+      };
     case 'ip':
     case 'ip_range':
-      isInvalid = !isEmpty(value) && !validateParams(value, field);
-      break;
+      return {
+        isInvalid: !isEmpty(value) && !validateParams(value, field),
+        errorMessage: [''],
+      };
     case 'number':
     case 'number_range':
     case 'boolean':
     default:
-      break;
+      return { isInvalid: false, errorMessage: [''] };
   }
-
-  return { isInvalid, errorMessage };
 };
