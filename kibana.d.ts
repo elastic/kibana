80a83ff60e63880c4cdb8ec1c15a74f9e011f3a1
rashmikulkarni <rashmi.kulkarni@elastic.co>
2022-05-05 10:37:33 -0700

    restored

---
+++ kibana.d.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+/**
+ * All exports from TS source files (where the implementation is actually done in TS).
+ */
+import * as Public from '@kbn/core/public';
+import * as Server from '@kbn/core/server';
+
+export { Public, Server };
