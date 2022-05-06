1e0f7e0be8e848cb82f2b20754d97c57ba2e1fb9
Anton Dosov <anton.dosov@elastic.co>
2022-05-06 16:48:18 +0200

    [Search] Share async strategy utils (#128358)

--- src/plugins/data/server/search/strategies/sql_search/request_utils.ts
+++ src/plugins/data/server/search/strategies/sql_search/request_utils.ts
@@ -9,6 +9,10 @@
 import { SqlGetAsyncRequest, SqlQueryRequest } from '@elastic/elasticsearch/lib/api/types';
 import { ISearchOptions } from '../../../../common';
 import { SearchSessionsConfigSchema } from '../../../../config';
+import {
+  getCommonDefaultAsyncGetParams,
+  getCommonDefaultAsyncSubmitParams,
+} from '../common/async_utils';
 
 /**
  @internal
@@ -17,19 +21,8 @@ export function getDefaultAsyncSubmitParams(
   searchSessionsConfig: SearchSessionsConfigSchema | null,
   options: ISearchOptions
 ): Pick<SqlQueryRequest, 'keep_alive' | 'wait_for_completion_timeout' | 'keep_on_completion'> {
-  const useSearchSessions = searchSessionsConfig?.enabled && !!options.sessionId;
-
-  const keepAlive = useSearchSessions
-    ? `${searchSessionsConfig!.defaultExpiration.asMilliseconds()}ms`
-    : '1m';
-
   return {
-    // Wait up to 100ms for the response to return
-    wait_for_completion_timeout: '100ms',
-    // If search sessions are used, store and get an async ID even for short running requests.
-    keep_on_completion: useSearchSessions,
-    // The initial keepalive is as defined in defaultExpiration if search sessions are used or 1m otherwise.
-    keep_alive: keepAlive,
+    ...getCommonDefaultAsyncSubmitParams(searchSessionsConfig, options),
   };
 }
 
@@ -40,17 +33,7 @@ export function getDefaultAsyncGetParams(
   searchSessionsConfig: SearchSessionsConfigSchema | null,
   options: ISearchOptions
 ): Pick<SqlGetAsyncRequest, 'keep_alive' | 'wait_for_completion_timeout'> {
-  const useSearchSessions = searchSessionsConfig?.enabled && !!options.sessionId;
-
   return {
-    // Wait up to 100ms for the response to return
-    wait_for_completion_timeout: '100ms',
-    ...(useSearchSessions
-      ? // Don't change the expiration of search requests that are tracked in a search session
-        undefined
-      : {
-          // We still need to do polling for searches not within the context of a search session or when search session disabled
-          keep_alive: '1m',
-        }),
+    ...getCommonDefaultAsyncGetParams(searchSessionsConfig, options),
   };
 }
