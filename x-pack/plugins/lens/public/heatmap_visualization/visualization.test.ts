58b4dcd491f03186488e938746d869e7b2686c6f
Andrew Tate <andrew.tate@elastic.co>
2022-05-06 09:40:57 -0500

    remove unused import

--- x-pack/plugins/lens/public/heatmap_visualization/visualization.test.ts
+++ x-pack/plugins/lens/public/heatmap_visualization/visualization.test.ts
@@ -22,7 +22,6 @@ import { Position } from '@elastic/charts';
 import type { HeatmapVisualizationState } from './types';
 import type { DatasourceLayers, OperationDescriptor } from '../types';
 import { chartPluginMock } from '@kbn/charts-plugin/public/mocks';
-import { DEFAULT_LEGEND_SIZE, LegendSizeToPixels } from '@kbn/visualizations-plugin/public';
 import { layerTypes } from '../../common';
 import { themeServiceMock } from '@kbn/core/public/mocks';
 
