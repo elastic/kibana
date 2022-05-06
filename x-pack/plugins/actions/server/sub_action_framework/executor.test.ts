8759237f98d09e667bd624c4cbdf791204abeb50
Christos Nasikas <christos.nasikas@elastic.co>
2022-05-06 11:46:24 +0300

    Executor unit tests

---
+++ x-pack/plugins/actions/server/sub_action_framework/executor.test.ts
@@ -0,0 +1,176 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { loggingSystemMock } from '@kbn/core/server/mocks';
+import { MockedLogger } from '@kbn/logging-mocks';
+import { ActionsConfigurationUtilities } from '../actions_config';
+import { actionsConfigMock } from '../actions_config.mock';
+import { actionsMock } from '../mocks';
+import { buildExecutor } from './executor';
+import {
+  TestSecretsSchema,
+  TestConfigSchema,
+  TestNoSubActions,
+  TestConfig,
+  TestSecrets,
+  TestExecutor,
+} from './mocks';
+import { IService } from './types';
+
+describe('BasicConnector', () => {
+  const actionId = 'test-action-id';
+  const config = { url: 'https://example.com' };
+  const secrets = { username: 'elastic', password: 'changeme' };
+  const params = { subAction: 'testUrl', subActionParams: { url: 'https://example.com' } };
+  let logger: MockedLogger;
+  let services: ReturnType<typeof actionsMock.createServices>;
+  let mockedActionsConfig: jest.Mocked<ActionsConfigurationUtilities>;
+
+  const createExecutor = (Service: IService<TestConfig, TestSecrets>) => {
+    const connector = {
+      id: '.test',
+      name: 'Test',
+      minimumLicenseRequired: 'basic' as const,
+      schema: {
+        config: TestConfigSchema,
+        secrets: TestSecretsSchema,
+      },
+      Service,
+    };
+
+    return buildExecutor({ configurationUtilities: mockedActionsConfig, logger, connector });
+  };
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+
+    logger = loggingSystemMock.createLogger();
+    services = actionsMock.createServices();
+    mockedActionsConfig = actionsConfigMock.create();
+  });
+
+  it('should execute correctly', async () => {
+    const executor = createExecutor(TestExecutor);
+
+    const res = await executor({
+      actionId,
+      params: { subAction: 'echo', subActionParams: { id: 'test-id' } },
+      config,
+      secrets,
+      services,
+    });
+
+    expect(res).toEqual({
+      actionId: 'test-action-id',
+      data: {
+        id: 'test-id',
+      },
+      status: 'ok',
+    });
+  });
+
+  it('should execute correctly without schema validation', async () => {
+    const executor = createExecutor(TestExecutor);
+
+    const res = await executor({
+      actionId,
+      params: { subAction: 'noSchema', subActionParams: { id: 'test-id' } },
+      config,
+      secrets,
+      services,
+    });
+
+    expect(res).toEqual({
+      actionId: 'test-action-id',
+      data: {
+        id: 'test-id',
+      },
+      status: 'ok',
+    });
+  });
+
+  it('should return an empty object if the func returns undefined', async () => {
+    const executor = createExecutor(TestExecutor);
+
+    const res = await executor({
+      actionId,
+      params: { ...params, subAction: 'noData' },
+      config,
+      secrets,
+      services,
+    });
+
+    expect(res).toEqual({
+      actionId: 'test-action-id',
+      data: {},
+      status: 'ok',
+    });
+  });
+
+  it('should execute a non async function', async () => {
+    const executor = createExecutor(TestExecutor);
+
+    const res = await executor({
+      actionId,
+      params: { ...params, subAction: 'noAsync' },
+      config,
+      secrets,
+      services,
+    });
+
+    expect(res).toEqual({
+      actionId: 'test-action-id',
+      data: {},
+      status: 'ok',
+    });
+  });
+
+  it('throws if the are not sub actions registered', async () => {
+    const executor = createExecutor(TestNoSubActions);
+
+    await expect(async () =>
+      executor({ actionId, params, config, secrets, services })
+    ).rejects.toThrowError('You should register at least one subAction for your connector type');
+  });
+
+  it('throws if the the sub action is not registered', async () => {
+    const executor = createExecutor(TestExecutor);
+
+    await expect(async () =>
+      executor({
+        actionId,
+        params,
+        config,
+        secrets,
+        services,
+      })
+    ).rejects.toThrowError('Not valid method for registered sub action');
+  });
+
+  it('throws if the registered method is not a function', async () => {
+    const executor = createExecutor(TestExecutor);
+
+    await expect(async () =>
+      executor({
+        actionId,
+        params: { ...params, subAction: 'notAFunction' },
+        config,
+        secrets,
+        services,
+      })
+    ).rejects.toThrowError('Method must be a valid function');
+  });
+
+  it('throws if the sub actions params are not valid', async () => {
+    const executor = createExecutor(TestExecutor);
+
+    await expect(async () =>
+      executor({ actionId, params: { ...params, subAction: 'echo' }, config, secrets, services })
+    ).rejects.toThrowError('[id]: expected value of type [string] but got [undefined]');
+  });
+});
--- x-pack/plugins/actions/server/sub_action_framework/mocks.ts
+++ x-pack/plugins/actions/server/sub_action_framework/mocks.ts
@@ -4,24 +4,29 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+/* eslint-disable max-classes-per-file */
 
 import { schema, TypeOf } from '@kbn/config-schema';
 import { AxiosError } from 'axios';
+import { Resolver } from 'dns';
 import { BasicConnector } from './basic';
 import { ServiceParams } from './types';
 
-const ConfigSchema = schema.object({ url: schema.string() });
-const SecretsSchema = schema.object({ username: schema.string(), password: schema.string() });
-type Config = TypeOf<typeof ConfigSchema>;
-type Secrets = TypeOf<typeof SecretsSchema>;
+export const TestConfigSchema = schema.object({ url: schema.string() });
+export const TestSecretsSchema = schema.object({
+  username: schema.string(),
+  password: schema.string(),
+});
+export type TestConfig = TypeOf<typeof TestConfigSchema>;
+export type TestSecrets = TypeOf<typeof TestSecretsSchema>;
 
 interface ErrorSchema {
   errorMessage: string;
   errorCode: number;
 }
 
-export class TestBasicConnector extends BasicConnector<Config, Secrets> {
-  constructor(params: ServiceParams<Config, Secrets>) {
+export class TestBasicConnector extends BasicConnector<TestConfig, TestSecrets> {
+  constructor(params: ServiceParams<TestConfig, TestSecrets>) {
     super(params);
     this.registerSubAction({
       name: 'testUrl',
@@ -44,4 +49,71 @@ export class TestBasicConnector extends BasicConnector<Config, Secrets> {
 
     return res;
   }
+}
+
+export class TestNoSubActions extends BasicConnector<TestConfig, TestSecrets> {
+  protected getResponseErrorMessage(error: AxiosError<ErrorSchema>) {
+    return `Error`;
+  }
+}
+
+export class TestExecutor extends BasicConnector<TestConfig, TestSecrets> {
+  public notAFunction: string = 'notAFunction';
+
+  constructor(params: ServiceParams<TestConfig, TestSecrets>) {
+    super(params);
+    this.registerSubAction({
+      name: 'testUrl',
+      method: 'not-exist',
+      schema: schema.object({}),
+    });
+
+    this.registerSubAction({
+      name: 'notAFunction',
+      method: 'notAFunction',
+      schema: schema.object({}),
+    });
+
+    this.registerSubAction({
+      name: 'echo',
+      method: 'echo',
+      schema: schema.object({ id: schema.string() }),
+    });
+
+    this.registerSubAction({
+      name: 'noSchema',
+      method: 'noSchema',
+      schema: null,
+    });
+
+    this.registerSubAction({
+      name: 'noData',
+      method: 'noData',
+      schema: null,
+    });
+
+    this.registerSubAction({
+      name: 'noAsync',
+      method: 'noAsync',
+      schema: null,
+    });
+  }
+
+  protected getResponseErrorMessage(error: AxiosError<ErrorSchema>) {
+    return `Error`;
+  }
+
+  public async echo({ id }: { id: string }) {
+    return Promise.resolve({ id });
+  }
+
+  public async noSchema({ id }: { id: string }) {
+    return Promise.resolve({ id });
+  }
+
+  public async noData() {
+    return Promise.resolve();
+  }
+
+  public noAsync() {}
 }
