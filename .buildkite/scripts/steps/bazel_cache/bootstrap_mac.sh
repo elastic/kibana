645de6f77f1d95c748ecc82754cdc998281507db
Brian Seeders <brian.seeders@elastic.co>
2022-05-09 17:29:11 -0400

    Use service account from vault

--- .buildkite/scripts/steps/bazel_cache/bootstrap_mac.sh
+++ .buildkite/scripts/steps/bazel_cache/bootstrap_mac.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 export BAZEL_CACHE_MODE=populate-local-gcs
 export DISABLE_BOOTSTRAP_VALIDATION=true
 
+retry 5 5 vault read -field=service_account_json secret/kibana-issues/dev/kibana-ci-bazel-remote-cache-local-dev > target/kibana-ci-bazel-remote-cache-local-dev.json
+GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/target/kibana-ci-bazel-remote-cache-local-dev.json
+export GOOGLE_APPLICATION_CREDENTIALS
+
 # Clear out bazel cache between runs to make sure that any artifacts that don't exist in the cache are uploaded
 rm -rf ~/.bazel-cache
 
