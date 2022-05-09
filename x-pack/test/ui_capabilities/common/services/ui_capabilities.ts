9ecedd20f8f5d1c257896eccdb8c9e232018bfda
Aleh Zasypkin <aleh.zasypkin@elastic.co>
2022-05-09 15:37:08 +0200

    Few more simplifications.

--- x-pack/test/ui_capabilities/common/services/ui_capabilities.ts
+++ x-pack/test/ui_capabilities/common/services/ui_capabilities.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestHeaders } from 'axios';
 import type { Capabilities as UICapabilities } from '@kbn/core/types';
 import { format as formatUrl } from 'url';
 import util from 'util';
@@ -61,13 +61,13 @@ export class UICapabilitiesService {
     this.log.debug(
       `requesting ${spaceUrlPrefix}/api/core/capabilities to parse the uiCapabilities`
     );
-    const requestHeaders = credentials
+    const requestHeaders: AxiosRequestHeaders = credentials
       ? {
           Authorization: `Basic ${Buffer.from(
             `${credentials.username}:${credentials.password}`
           ).toString('base64')}`,
         }
-      : undefined;
+      : {};
     const response = await this.axios.post(
       `${spaceUrlPrefix}/api/core/capabilities`,
       { applications: [...applications, 'kibana:stack_management'] },
