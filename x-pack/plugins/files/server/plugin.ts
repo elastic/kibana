d07e14d0c75549b9a805902da622f63e2147e046
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-05-09 12:10:53 +0200

    added files plugin

---
+++ x-pack/plugins/files/server/plugin.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type {
+  PluginInitializerContext,
+  CoreSetup,
+  CoreStart,
+  Plugin,
+  Logger,
+} from '@kbn/core/server';
+
+export class FilesPlugin implements Plugin {
+  private readonly logger: Logger;
+
+  constructor(initializerContext: PluginInitializerContext) {
+    this.logger = initializerContext.logger.get();
+  }
+
+  public setup(core: CoreSetup) {
+    this.logger.debug('files: Setup');
+    return {};
+  }
+
+  public start(core: CoreStart) {
+    this.logger.debug('files: Started');
+    return {};
+  }
+
+  public stop() {}
+}
