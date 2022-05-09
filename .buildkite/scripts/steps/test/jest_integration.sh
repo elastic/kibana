c8fcf568eec8885452b54ded8ebb234fda82d745
spalger <spencer@elastic.co>
2022-05-09 14:27:42 -0500

    default BUILDKITE_PARALLEL_JOB to 0

--- .buildkite/scripts/steps/test/jest_integration.sh
+++ .buildkite/scripts/steps/test/jest_integration.sh
@@ -8,6 +8,8 @@ is_test_execution_step
 
 .buildkite/scripts/bootstrap.sh
 
+BUILDKITE_PARALLEL_JOB="${BUILDKITE_PARALLEL_JOB:-0}"
+
 echo '--- Jest Integration Tests'
 checks-reporter-with-killswitch "Jest Integration Tests $((BUILDKITE_PARALLEL_JOB+1))" \
   .buildkite/scripts/steps/test/jest_parallel.sh jest.integration.config.js
