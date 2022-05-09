df8ad1d0ecf002b8a0be8aa33778ad728f497c95
Larry Gregory <larry.gregory@elastic.co>
2022-05-09 14:31:27 -0400

    Unskip flaky test

--- x-pack/test/functional/apps/spaces/enter_space.ts
+++ x-pack/test/functional/apps/spaces/enter_space.ts
@@ -14,9 +14,7 @@ export default function enterSpaceFunctonalTests({
   const esArchiver = getService('esArchiver');
   const PageObjects = getPageObjects(['security', 'spaceSelector']);
 
-  // FLAKY: https://github.com/elastic/kibana/issues/99879
-  describe.skip('Enter Space', function () {
-    // FLAKY: https://github.com/elastic/kibana/issues/100570
+  describe('Enter Space', function () {
     // These tests fail very intermittently in Firefox. Skip Firefox testing until resolved.
     // this.tags('includeFirefox');
     before(async () => {
