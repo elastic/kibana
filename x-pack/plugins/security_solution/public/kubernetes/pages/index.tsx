98477ead0f70d4f3b718c3d28816b388381d0537
Zizhou Wang <zizhou.wang@elastic.co>
2022-05-10 11:16:38 -0400

    Add global searchbar

--- x-pack/plugins/security_solution/public/kubernetes/pages/index.tsx
+++ x-pack/plugins/security_solution/public/kubernetes/pages/index.tsx
@@ -12,12 +12,28 @@ import { Route, Switch } from 'react-router-dom';
 import { SecuritySolutionPageWrapper } from '../../common/components/page_wrapper';
 import { KUBERNETES_PATH, SecurityPageName } from '../../../common/constants';
 import { SpyRoute } from '../../common/utils/route/spy_routes';
+import { FiltersGlobal } from '../../common/components/filters_global';
+import { SiemSearchBar } from '../../common/components/search_bar';
+import { showGlobalFilters } from '../../timelines/components/timeline/helpers';
+import { useGlobalFullScreen } from '../../common/containers/use_full_screen';
+import { useSourcererDataView } from '../../common/containers/sourcerer';
+import { SourcererScopeName } from '../../common/store/sourcerer/model';
 
 export const KubernetesContainer = React.memo(() => {
+  const { globalFullScreen } = useGlobalFullScreen();
+  const {
+    indexPattern,
+    // runtimeMappings,
+    // loading: isLoadingIndexPattern,
+  } = useSourcererDataView(SourcererScopeName.detections);
+
   return (
     <SecuritySolutionPageWrapper noPadding>
       <Switch>
         <Route strict exact path={KUBERNETES_PATH}>
+          <FiltersGlobal show={showGlobalFilters({ globalFullScreen, graphEventId: undefined })}>
+            <SiemSearchBar id="global" indexPattern={indexPattern} />
+          </FiltersGlobal>
           <div>
             <span>this is where the place is</span>
           </div>
