4eb22a1f695952a3b3b3c9219be36c1db7cc2f70
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-05-10 08:34:10 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/test/observability_functional/apps/observability/index.ts
+++ x-pack/test/observability_functional/apps/observability/index.ts
@@ -10,7 +10,7 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('ObservabilityApp', function () {
     this.tags('ciGroup22');
-    
+
     loadTestFile(require.resolve('./alerts'));
     loadTestFile(require.resolve('./alerts/add_to_case'));
     loadTestFile(require.resolve('./alerts/alert_disclaimer'));
