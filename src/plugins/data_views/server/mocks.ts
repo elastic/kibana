9b32e3cb3e838d8aa5622e3032c5ce9ff2a7904c
Lukas Olson <lukas@elastic.co>
2022-05-10 13:47:55 -0700

    Add docs

--- src/plugins/data_views/server/mocks.ts
+++ src/plugins/data_views/server/mocks.ts
@@ -29,4 +29,5 @@ export const dataViewsService = {
   getDefaultId: jest.fn(),
   updateSavedObject: jest.fn(),
   refreshFields: jest.fn(),
+  getIdsWithTitle: jest.fn(),
 } as unknown as jest.Mocked<DataViewsService>;
