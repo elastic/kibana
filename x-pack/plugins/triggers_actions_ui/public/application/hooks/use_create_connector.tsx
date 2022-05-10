e279e08b447d778b007889ef46a4ce04f1b79782
Christos Nasikas <christos.nasikas@elastic.co>
2022-05-10 12:29:59 +0300

    Create useCreateConnector

---
+++ x-pack/plugins/triggers_actions_ui/public/application/hooks/use_create_connector.tsx
@@ -0,0 +1,71 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useRef, useState, useEffect } from 'react';
+import { i18n } from '@kbn/i18n';
+import { ActionConnector, ActionConnectorWithoutId } from '../../types';
+import { createActionConnector } from '../lib/action_connector_api';
+import { useKibana } from '../../common/lib/kibana';
+
+type Connector = Omit<ActionConnectorWithoutId, 'referencedByCount'>;
+
+interface UseCreateConnectorReturnValue {
+  isLoading: boolean;
+  createConnector: (connector: Connector) => Promise<ActionConnector | undefined>;
+}
+
+export const useCreateConnector = (): UseCreateConnectorReturnValue => {
+  const {
+    http,
+    notifications: { toasts },
+  } = useKibana().services;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const abortCtrlRef = useRef(new AbortController());
+  const isMounted = useRef(false);
+
+  async function createConnector(connector: Connector) {
+    setIsLoading(true);
+    isMounted.current = true;
+    abortCtrlRef.current.abort();
+    abortCtrlRef.current = new AbortController();
+
+    try {
+      const res = await createActionConnector({ http, connector });
+
+      if (isMounted.current) {
+        setIsLoading(false);
+      }
+
+      return res;
+    } catch (error) {
+      if (isMounted.current) {
+        setIsLoading(false);
+
+        if (error.name !== 'AbortError') {
+          toasts.addDanger(
+            error.body?.message ??
+              i18n.translate(
+                'xpack.triggersActionsUI.sections.useCreateConnector.updateErrorNotificationText',
+                { defaultMessage: 'Cannot create a connector.' }
+              )
+          );
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    isMounted.current = true;
+    return () => {
+      isMounted.current = false;
+      abortCtrlRef.current.abort();
+    };
+  }, []);
+
+  return { isLoading, createConnector };
+};
