b3d83ee75e4b3a81c814bb5f31b72dec43f57057
Faisal Kanout <faisal.kanout@elastic.co>
2022-05-10 16:21:03 +0200

    Fix text move vertically on small screen

--- x-pack/plugins/observability/public/pages/rule_details/components/item_title_rule_summary.tsx
+++ x-pack/plugins/observability/public/pages/rule_details/components/item_title_rule_summary.tsx
@@ -14,7 +14,7 @@ export function ItemTitleRuleSummary({
   defaultMessage,
 }: ItemTitleRuleSummaryProps) {
   return (
-    <EuiTitle size="xxs">
+    <EuiTitle style={{ whiteSpace: 'nowrap' }} size="xxs">
       <EuiFlexItem grow={1}>
         {i18n.translate(translationKey, {
           defaultMessage,
