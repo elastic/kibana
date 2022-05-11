ea4e788a6f75330eb7ee5de10782583cc6712634
Mat Schaffer <mat@elastic.co>
2022-05-11 23:47:20 +0900

    [NOT FOR MERGE] A very exciting new kibana homepage panel

--- x-pack/test/functional/apps/home/feature_controls/home_security.ts
+++ x-pack/test/functional/apps/home/feature_controls/home_security.ts
@@ -70,6 +70,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
           'observability',
           'securitySolution',
           'kibana',
+          'kibana',
         ]);
       });
 
@@ -120,7 +121,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
 
       it('shows only the kibana solution', async () => {
         const solutions = await PageObjects.home.getVisibileSolutions();
-        expect(solutions).to.eql(['kibana']);
+        expect(solutions).to.eql(['kibana', 'kibana']);
       });
 
       it('does not show the management section', async () => {
