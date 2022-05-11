078503e0844090ed3ed1b22b64c1d9818bc6dec4
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-05-11 10:33:10 +0200

    [Files] Create files saved object and blob storage (#131886)
    * initial version of file saved objects
    
    * initial version of file saved objects part ii
    
    * added .blob index and setup logic for the file service and the blob service
    
    * wip: need to add .blob access to kibana_system role
    
    * use multiple-isolated after speaking of oleg
    
    * added metadata field
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    * [CI] Auto-commit changed files from 'node scripts/build_plugin_list_docs'
    
    * updated file statuses per feedback
    
    * removed created_by and added "alt" text field and converted "name" to type "text" too
    
    * split content_type into mime and extension
    
    * use BlobStorage interface in adapters prop
    
    * minor fixes
    
    * handle multiple Kibanas at startup time
    
    * fix jest test
    
    * do not set up index at setup time
    
    * set importable exportable to false explicitly
    
    * remove use of non-existent function
    
    * added plugin ID, use type instead of interface to avoid specific interface issue and use typescript to check that properties map corresponds to type
    
    * remove storage_id for now
    
    * make the name snake case!
    
    * added fixmes
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/files/server/plugin.ts
+++ x-pack/plugins/files/server/plugin.ts
@@ -5,28 +5,45 @@
  * 2.0.
  */
 
-import type {
-  PluginInitializerContext,
-  CoreSetup,
-  CoreStart,
-  Plugin,
-  Logger,
-} from '@kbn/core/server';
+import type { PluginInitializerContext, CoreSetup, Plugin, Logger } from '@kbn/core/server';
+
+import { BlobStorageService } from './blob_storage_service';
+import { InternalFileService } from './file_service';
+import { fileObjectType } from './saved_objects';
 
 export class FilesPlugin implements Plugin {
   private readonly logger: Logger;
+  private fileService: undefined | InternalFileService;
+  private readyPromise: Promise<void> = Promise.resolve();
 
   constructor(initializerContext: PluginInitializerContext) {
     this.logger = initializerContext.logger.get();
   }
 
   public setup(core: CoreSetup) {
-    this.logger.debug('files: Setup');
+    core.savedObjects.registerType(fileObjectType);
+
+    this.readyPromise = core.getStartServices().then(async ([coreStart]) => {
+      const esClient = coreStart.elasticsearch.client.asInternalUser;
+      const blobStorageService = new BlobStorageService(
+        esClient,
+        this.logger.get('blob-storage-service')
+      );
+      this.fileService = new InternalFileService(
+        core.savedObjects,
+        coreStart.savedObjects,
+        blobStorageService,
+        this.logger.get('files-service')
+      );
+    });
+
     return {};
   }
 
-  public start(core: CoreStart) {
-    this.logger.debug('files: Started');
+  public start() {
+    this.readyPromise!.then(() => {
+      this.logger.info(`Files ready: ${Boolean(this.fileService)}`);
+    });
     return {};
   }
 
