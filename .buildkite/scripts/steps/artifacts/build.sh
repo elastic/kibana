0cfa979dd009cc28490002ebe1fe25f975b8a19f
Jonathan Budzenski <jon@elastic.co>
2022-05-11 14:03:40 -0500

    [artifacts] Test cloud context (#130394)
    * [artifacts] Test cloud context
    
    This artifact currently has no consumers, but in the near future the
    release-manager will produce Cloud images using the context.  This
    ensures builds are working before we publish artifacts.
    
    * update permissions
    
    * fix if statement
    
    * DOCKER_CONTEXT -> KIBANA_DOCKER_CONTEXT

--- .buildkite/scripts/steps/artifacts/build.sh
+++ .buildkite/scripts/steps/artifacts/build.sh
@@ -24,3 +24,8 @@ buildkite-agent artifact upload "dependencies-$FULL_VERSION.csv"
 buildkite-agent artifact upload "dependencies-$FULL_VERSION.csv.sha512.txt"
 buildkite-agent artifact upload 'i18n/*.json'
 cd -
+
+cd .beats
+buildkite-agent artifact upload 'metricbeat-*'
+buildkite-agent artifact upload 'filebeat-*'
+cd -
