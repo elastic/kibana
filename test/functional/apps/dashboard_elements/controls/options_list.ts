f71e5582b499e05bdee3531016beea1600ee8302
Catherine Liu <catherine.liu@elastic.co>
2022-05-11 07:36:59 -0700

    [Controls] Delete sync with query bar setting (#131554)
    * Delete sync with query bar settings
    
    * Removed query sync functional tests

--- test/functional/apps/dashboard_elements/controls/options_list.ts
+++ test/functional/apps/dashboard_elements/controls/options_list.ts
@@ -264,46 +264,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         });
       });
 
-      describe('Does not apply query settings to controls', async () => {
-        before(async () => {
-          await dashboardControls.updateAllQuerySyncSettings(false);
-        });
-
-        after(async () => {
-          await dashboardControls.updateAllQuerySyncSettings(true);
-        });
-
-        it('Does not apply query to options list control', async () => {
-          await queryBar.setQuery('isDog : true ');
-          await queryBar.submitQuery();
-          await dashboard.waitForRenderComplete();
-          await header.waitUntilLoadingHasFinished();
-          await ensureAvailableOptionsEql(allAvailableOptions);
-          await queryBar.setQuery('');
-          await queryBar.submitQuery();
-        });
-
-        it('Does not apply filters to options list control', async () => {
-          await filterBar.addFilter('sound.keyword', 'is one of', ['bark', 'bow ow ow', 'ruff']);
-          await dashboard.waitForRenderComplete();
-          await header.waitUntilLoadingHasFinished();
-          await ensureAvailableOptionsEql(allAvailableOptions);
-          await filterBar.removeFilter('sound.keyword');
-        });
-
-        it('Does not apply time range to options list control', async () => {
-          // set time range to time with no documents
-          await timePicker.setAbsoluteRange(
-            'Jan 1, 2017 @ 00:00:00.000',
-            'Jan 1, 2017 @ 00:00:00.000'
-          );
-          await dashboard.waitForRenderComplete();
-          await header.waitUntilLoadingHasFinished();
-          await ensureAvailableOptionsEql(allAvailableOptions);
-          await timePicker.setDefaultDataRange();
-        });
-      });
-
       describe('Selections made in control apply to dashboard', async () => {
         it('Shows available options in options list', async () => {
           await ensureAvailableOptionsEql(allAvailableOptions);
