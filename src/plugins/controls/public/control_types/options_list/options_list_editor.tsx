50dc923a57cc16d876a93c731f33785e2b572559
Devon Thomson <devon.thomson@elastic.co>
2022-05-12 13:22:30 -0400

    [Dashboard][Controls] Make Text Field Based Options List Controls Case Insensitive (#131198)
    * Adds case insensitive search and run past timeout option

--- src/plugins/controls/public/control_types/options_list/options_list_editor.tsx
+++ src/plugins/controls/public/control_types/options_list/options_list_editor.tsx
@@ -8,24 +8,25 @@
 
 import useMount from 'react-use/lib/useMount';
 import React, { useEffect, useState } from 'react';
-import { EuiFormRow, EuiSwitch } from '@elastic/eui';
 
-import { DataViewListItem, DataView } from '@kbn/data-views-plugin/common';
 import {
   LazyDataViewPicker,
   LazyFieldPicker,
   withSuspense,
 } from '@kbn/presentation-util-plugin/public';
+import { IFieldSubTypeMulti } from '@kbn/es-query';
+import { EuiFormRow, EuiSwitch } from '@elastic/eui';
+import { DataViewListItem, DataView } from '@kbn/data-views-plugin/common';
+
 import { pluginServices } from '../../services';
 import { ControlEditorProps } from '../../types';
-import { OptionsListEmbeddableInput } from './types';
 import { OptionsListStrings } from './options_list_strings';
-
+import { OptionsListEmbeddableInput, OptionsListField } from './types';
 interface OptionsListEditorState {
   singleSelect?: boolean;
-
+  runPastTimeout?: boolean;
   dataViewListItems: DataViewListItem[];
-
+  fieldsMap?: { [key: string]: OptionsListField };
   dataView?: DataView;
   fieldName?: string;
 }
@@ -48,6 +49,7 @@ export const OptionsListEditor = ({
   const [state, setState] = useState<OptionsListEditorState>({
     fieldName: initialInput?.fieldName,
     singleSelect: initialInput?.singleSelect,
+    runPastTimeout: initialInput?.runPastTimeout,
     dataViewListItems: [],
   });
 
@@ -64,12 +66,53 @@ export const OptionsListEditor = ({
         dataView = await get(initialId);
       }
       if (!mounted) return;
-      setState((s) => ({ ...s, dataView, dataViewListItems }));
+      setState((s) => ({ ...s, dataView, dataViewListItems, fieldsMap: {} }));
     })();
     return () => {
       mounted = false;
     };
   });
+
+  useEffect(() => {
+    if (!state.dataView) return;
+
+    // double link the parent-child relationship so that we can filter in fields which are multi-typed to text / keyword
+    const doubleLinkedFields: OptionsListField[] = state.dataView?.fields.getAll();
+    for (const field of doubleLinkedFields) {
+      const parentFieldName = (field.subType as IFieldSubTypeMulti)?.multi?.parent;
+      if (parentFieldName) {
+        (field as OptionsListField).parentFieldName = parentFieldName;
+        const parentField = state.dataView?.getFieldByName(parentFieldName);
+        (parentField as OptionsListField).childFieldName = field.name;
+      }
+    }
+
+    const newFieldsMap: OptionsListEditorState['fieldsMap'] = {};
+    for (const field of doubleLinkedFields) {
+      if (field.type === 'boolean') {
+        newFieldsMap[field.name] = field;
+      }
+
+      // field type is keyword, check if this field is related to a text mapped field and include it.
+      else if (field.aggregatable && field.type === 'string') {
+        const childField =
+          (field.childFieldName && state.dataView?.fields.getByName(field.childFieldName)) ||
+          undefined;
+        const parentField =
+          (field.parentFieldName && state.dataView?.fields.getByName(field.parentFieldName)) ||
+          undefined;
+
+        const textFieldName = childField?.esTypes?.includes('text')
+          ? childField.name
+          : parentField?.esTypes?.includes('text')
+          ? parentField.name
+          : undefined;
+
+        newFieldsMap[field.name] = { ...field, textFieldName } as OptionsListField;
+      }
+    }
+    setState((s) => ({ ...s, fieldsMap: newFieldsMap }));
+  }, [state.dataView]);
 
   useEffect(
     () => setValidState(Boolean(state.fieldName) && Boolean(state.dataView)),
@@ -100,14 +143,16 @@ export const OptionsListEditor = ({
       </EuiFormRow>
       <EuiFormRow label={OptionsListStrings.editor.getFieldTitle()}>
         <FieldPicker
-          filterPredicate={(field) =>
-            (field.aggregatable && field.type === 'string') || field.type === 'boolean'
-          }
+          filterPredicate={(field) => Boolean(state.fieldsMap?.[field.name])}
           selectedFieldName={fieldName}
           dataView={dataView}
           onSelectField={(field) => {
             setDefaultTitle(field.displayName ?? field.name);
-            onChange({ fieldName: field.name });
+            const textFieldName = state.fieldsMap?.[field.name].textFieldName;
+            onChange({
+              fieldName: field.name,
+              textFieldName,
+            });
             setState((s) => ({ ...s, fieldName: field.name }));
           }}
         />
@@ -119,6 +164,16 @@ export const OptionsListEditor = ({
           onChange={() => {
             onChange({ singleSelect: !state.singleSelect });
             setState((s) => ({ ...s, singleSelect: !s.singleSelect }));
+          }}
+        />
+      </EuiFormRow>
+      <EuiFormRow>
+        <EuiSwitch
+          label={OptionsListStrings.editor.getRunPastTimeoutTitle()}
+          checked={Boolean(state.runPastTimeout)}
+          onChange={() => {
+            onChange({ runPastTimeout: !state.runPastTimeout });
+            setState((s) => ({ ...s, runPastTimeout: !s.runPastTimeout }));
           }}
         />
       </EuiFormRow>
