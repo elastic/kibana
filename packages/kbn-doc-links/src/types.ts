99d7fde9773c5fb3d3c59f29fabdddb19098a9ba
Byron Hulcher <byron.hulcher@elastic.co>
2022-05-13 10:48:03 -0400

    [App Search] Add instructions for PDF Extraction to Crawler Overview (#132179)

--- packages/kbn-doc-links/src/types.ts
+++ packages/kbn-doc-links/src/types.ts
@@ -98,6 +98,7 @@ export interface DocLinks {
     readonly synonyms: string;
     readonly webCrawler: string;
     readonly webCrawlerEventLogs: string;
+    readonly webCrawlerReference: string;
   };
   readonly enterpriseSearch: {
     readonly configuration: string;
@@ -337,6 +338,7 @@ export interface DocLinks {
     azureRepo: string;
     gcsRepo: string;
     hdfsRepo: string;
+    ingestAttachment: string;
     s3Repo: string;
     snapshotRestoreRepos: string;
     mapperSize: string;
