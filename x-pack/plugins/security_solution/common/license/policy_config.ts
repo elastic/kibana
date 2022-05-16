942881a5a61df0fdc827b4b3a7fa97483c5764fa
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-05-16 20:48:45 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/security_solution/common/license/policy_config.ts
+++ x-pack/plugins/security_solution/common/license/policy_config.ts
@@ -211,9 +211,7 @@ function isEndpointAdvancedPolicyValidForLicense(policy: PolicyConfig, license: 
   const defaults = policyFactoryWithoutPaidFeatures();
 
   // only platinum or higher may use rollback
-  if (
-    policy.windows.advanced?.rollback === true || policy.windows.advanced?.rollback === 'true'
-  ) {
+  if (policy.windows.advanced?.rollback === true || policy.windows.advanced?.rollback === 'true') {
     console.log('LICENSE CHECK.....');
     return false;
   }
