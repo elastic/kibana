322d54ab2aac76a397b4934eef569b4c366f042c
James Gowdy <jgowdy@elastic.co>
2022-05-16 20:32:07 +0100

    [ML] Adding UI for question_answering model testing (#132033)
    * [ML] Adding UI for question_answering mmodel testing
    
    * translations

--- x-pack/plugins/ml/common/constants/trained_models.ts
+++ x-pack/plugins/ml/common/constants/trained_models.ts
@@ -22,6 +22,7 @@ export type TrainedModelType = typeof TRAINED_MODEL_TYPE[keyof typeof TRAINED_MO
 
 export const SUPPORTED_PYTORCH_TASKS = {
   NER: 'ner',
+  QUESTION_ANSWERING: 'question_answering',
   ZERO_SHOT_CLASSIFICATION: 'zero_shot_classification',
   TEXT_CLASSIFICATION: 'text_classification',
   TEXT_EMBEDDING: 'text_embedding',
