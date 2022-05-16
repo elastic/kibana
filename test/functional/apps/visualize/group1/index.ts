1b7c5391900f045c672b6f7df81aca09404969c3
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2022-05-16 16:44:05 +0300

    Adds a functional test

--- test/functional/apps/visualize/group1/index.ts
+++ test/functional/apps/visualize/group1/index.ts
@@ -22,7 +22,7 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
       await esArchiver.loadIfNeeded('test/functional/fixtures/es_archiver/logstash_functional');
       await esArchiver.loadIfNeeded('test/functional/fixtures/es_archiver/long_window_logstash');
     });
-
+    loadTestFile(require.resolve('./no_data'));
     loadTestFile(require.resolve('./_embedding_chart'));
     loadTestFile(require.resolve('./_data_table'));
     loadTestFile(require.resolve('./_data_table_nontimeindex'));
