44046f72c5d0faa2c0de103257ca94941a1ed3f8
Byron Hulcher <byron.hulcher@elastic.co>
2022-05-16 17:14:12 -0400

    New external connector icon

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/components/shared/assets/source_icons/index.ts
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/components/shared/assets/source_icons/index.ts
@@ -9,6 +9,7 @@ import box from './box.svg';
 import confluence from './confluence.svg';
 import custom from './custom.svg';
 import dropbox from './dropbox.svg';
+import external from './external.svg';
 import github from './github.svg';
 import gmail from './gmail.svg';
 import googleDrive from './google_drive.svg';
@@ -34,7 +35,7 @@ export const images = {
   confluenceServer: confluence,
   custom,
   dropbox,
-  external: custom,
+  external,
   github,
   githubEnterpriseServer: github,
   githubViaApp: github,
