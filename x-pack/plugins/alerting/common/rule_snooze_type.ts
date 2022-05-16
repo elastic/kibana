083782c07b2a284f2f4ceaef6d90a408b30f2fc3
Zacqary Xeper <zacqary.xeper@elastic.co>
2022-05-16 11:42:18 -0500

    Add comments to rulesnoozetype

--- x-pack/plugins/alerting/common/rule_snooze_type.ts
+++ x-pack/plugins/alerting/common/rule_snooze_type.ts
@@ -9,6 +9,8 @@ export type RuleSnooze = Array<{
   startTime: string;
   duration: number;
   timeZone: string;
+  // For scheduled/recurring snoozes, `id` uniquely identifies them so that they can be displayed, modified, and deleted individually
   id?: string;
+  // An iCal RRULE string to define a recurrence schedule, see https://github.com/jakubroztocil/rrule for the spec
   rRule?: string;
 }>;
