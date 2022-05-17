e1928bae4866853bb3fc222fa5b381cdc787519e
Timothy Sullivan <tsullivan@elastic.co>
2022-05-16 17:56:58 -0700

    cleanup unnecessary async

--- x-pack/plugins/screenshotting/server/screenshots/observable.ts
+++ x-pack/plugins/screenshotting/server/screenshots/observable.ts
@@ -141,7 +141,7 @@ export class ScreenshotObservableHandler {
   }
 
   private openUrl(index: number, urlOrUrlWithContext: UrlOrUrlWithContext) {
-    return defer(async () => {
+    return defer(() => {
       let url: string;
       let context: Context | undefined;
 
