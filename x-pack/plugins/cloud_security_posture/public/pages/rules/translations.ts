49df6753efc0522df85fd666b99aa7166e7a0bf4
Yarden Shalom <yarden.shalom@elastic.co>
2022-05-17 16:46:49 +0300

    lint

--- x-pack/plugins/cloud_security_posture/public/pages/rules/translations.ts
+++ x-pack/plugins/cloud_security_posture/public/pages/rules/translations.ts
@@ -39,9 +39,12 @@ export const SECTION = i18n.translate('xpack.csp.rules.rulesTable.rulesTableColu
   defaultMessage: 'Section',
 });
 
-export const CIS_SECTION = i18n.translate('xpack.csp.rules.rulesTable.rulesTableColumn.sectionLabel', {
-  defaultMessage: 'CIS Section',
-});
+export const CIS_SECTION = i18n.translate(
+  'xpack.csp.rules.rulesTable.rulesTableColumn.sectionLabel',
+  {
+    defaultMessage: 'CIS Section',
+  }
+);
 
 export const LAST_MODIFIED = i18n.translate(
   'xpack.csp.rules.rulesTable.rulesTableColumn.lastModifiedLabel',
