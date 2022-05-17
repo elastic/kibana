f43859e72162bcc53194997106e6764a017a70ce
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-05-17 17:50:12 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/monitoring/server/routes/api/v1/_health/monitored_clusters/build_monitored_clusters.ts
+++ x-pack/plugins/monitoring/server/routes/api/v1/_health/monitored_clusters/build_monitored_clusters.ts
@@ -1,4 +1,11 @@
-import { mapValues, omit } from 'lodash';
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { mapValues } from 'lodash';
 
 const reInternalMonitoring = /^\.monitoring-(es|kibana|beats|logstash)-7-[0-9]{4}\..*/;
 const reMetricbeatMonitoring7 = /^\.monitoring-(es|kibana|beats|logstash)-7-mb.*/;
