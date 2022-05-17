9daa0f51d2ae063768e3b3fe9f249efea7cab461
Paul Tavares <paul.tavares@elastic.co>
2022-05-17 15:42:09 -0400

    EndpointActionGenerator `generateActionDetails()` method

--- x-pack/plugins/security_solution/common/endpoint/data_generators/endpoint_action_generator.ts
+++ x-pack/plugins/security_solution/common/endpoint/data_generators/endpoint_action_generator.ts
@@ -11,6 +11,7 @@ import * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { ENDPOINT_ACTION_RESPONSES_DS, ENDPOINT_ACTIONS_INDEX } from '../constants';
 import { BaseDataGenerator } from './base_data_generator';
 import {
+  ActionDetails,
   ActivityLogItemTypes,
   EndpointActivityLogActionResponse,
   ISOLATION_ACTIONS,
@@ -101,6 +102,80 @@ export class EndpointActionGenerator extends BaseDataGenerator {
     return Object.assign(this.toEsSearchHit(this.generateResponse(overrides)), {
       _index: `.ds-${ENDPOINT_ACTION_RESPONSES_DS}-some_namespace-something`,
     });
+  }
+
+  generateActionDetails(overrides: DeepPartial<ActionDetails>): ActionDetails {
+    const details: ActionDetails = {
+      agents: ['agent-a'],
+      command: 'isolate',
+      completedAt: '2022-04-30T16:08:47.449Z',
+      id: '123',
+      isCompleted: true,
+      isExpired: false,
+      logEntries: [
+        {
+          item: {
+            data: {
+              '@timestamp': '2022-04-27T16:08:47.449Z',
+              action_id: '123',
+              agents: ['agent-a'],
+              data: {
+                command: 'isolate',
+                comment: '5wb6pu6kh2xix5i',
+              },
+              expiration: '2022-04-29T16:08:47.449Z',
+              input_type: 'endpoint',
+              type: 'INPUT_ACTION',
+              user_id: 'elastic',
+            },
+            id: '44d8b915-c69c-4c48-8c86-b57d0bd631d0',
+          },
+          type: 'fleetAction',
+        },
+        {
+          item: {
+            data: {
+              '@timestamp': '2022-04-30T16:08:47.449Z',
+              action_data: {
+                command: 'unisolate',
+                comment: '',
+              },
+              action_id: '123',
+              agent_id: 'agent-a',
+              completed_at: '2022-04-30T16:08:47.449Z',
+              error: '',
+              started_at: '2022-04-30T16:08:47.449Z',
+            },
+            id: '54-65-65-98',
+          },
+          type: 'fleetResponse',
+        },
+        {
+          item: {
+            data: {
+              '@timestamp': '2022-04-30T16:08:47.449Z',
+              EndpointActions: {
+                action_id: '123',
+                completed_at: '2022-04-30T16:08:47.449Z',
+                data: {
+                  command: 'unisolate',
+                  comment: '',
+                },
+                started_at: '2022-04-30T16:08:47.449Z',
+              },
+              agent: {
+                id: 'agent-a',
+              },
+            },
+            id: '32-65-98',
+          },
+          type: 'response',
+        },
+      ],
+      startedAt: '2022-04-27T16:08:47.449Z',
+    };
+
+    return merge(details, overrides);
   }
 
   generateActivityLogActionResponse(
