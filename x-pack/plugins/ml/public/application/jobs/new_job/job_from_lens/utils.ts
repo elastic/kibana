14f62f740b5805fe312585f971f33d2ba77cffba
James Gowdy <jgowdy@elastic.co>
2022-05-18 11:45:03 +0100

    fixing full label selection

--- x-pack/plugins/ml/public/application/jobs/new_job/job_from_lens/utils.ts
+++ x-pack/plugins/ml/public/application/jobs/new_job/job_from_lens/utils.ts
@@ -108,7 +108,7 @@ export async function getVisTypeFactory(lens: LensPublicStart) {
       case layerTypes.DATA:
         const type = visTypes.find((t) => t.id === layer.seriesType);
         return {
-          label: type?.fullLabel ?? layer.layerType,
+          label: type?.fullLabel || type?.label || layer.layerType,
           icon: type?.icon ?? '',
         };
       case layerTypes.ANNOTATIONS:
