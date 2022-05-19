b69016c1ebecff2e7c0b1a19b11aef3d5c9608cf
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2022-05-19 11:17:58 -0700

    changed the recursive approach

--- x-pack/plugins/timelines/server/search_strategy/timeline/factory/helpers/build_object_recursive.ts
+++ x-pack/plugins/timelines/server/search_strategy/timeline/factory/helpers/build_object_recursive.ts
@@ -10,19 +10,7 @@ import { get } from 'lodash/fp';
 import { Ecs } from '../../../../../common/ecs';
 import { Fields } from '../../../../../common/search_strategy';
 import { toStringArray } from '../../../../../common/utils/to_array';
-import { getNestedParentPath } from './get_nested_parent_path';
 
 export const buildObjectRecursive = (fieldPath: string, fields: Fields): Partial<Ecs> => {
-  const nestedParentPath = getNestedParentPath(fieldPath, fields);
-  if (!nestedParentPath) {
-    return set({}, fieldPath, toStringArray(get(fieldPath, fields)));
-  }
-
-  const subPath = fieldPath.replace(`${nestedParentPath}.`, '');
-  const subFields = (get(nestedParentPath, fields) ?? []) as Fields[];
-  return set(
-    {},
-    nestedParentPath,
-    subFields.map((subField) => buildObjectRecursive(subPath, subField))
-  );
+  return set({}, fieldPath, toStringArray(get(fieldPath, fields)));
 };
