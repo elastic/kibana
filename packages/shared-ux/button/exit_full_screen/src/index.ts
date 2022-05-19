9c48a8cbdc11b266f81a1e4fb88995b75f6013cd
Clint Andrew Hall <clint.hall@elastic.co>
2022-05-19 17:12:20 -0500

    WIP

--- packages/shared-ux/button/exit_full_screen/src/index.ts
+++ packages/shared-ux/button/exit_full_screen/src/index.ts
@@ -6,36 +6,6 @@
  * Side Public License, v 1.
  */
 
-import React from 'react';
-import { withSuspense } from '@kbn/shared-ux-utility';
-
 export { ExitFullScreenButtonKibanaProvider, ExitFullScreenButtonProvider } from './services';
-
-/**
- * Lazy-loaded pure component.  Must be wrapped in `React.Suspense`.
- */
-export const LazyExitFullScreenButtonComponent = React.lazy(() =>
-  import('./exit_full_screen_button.component').then(({ ExitFullScreenButton }) => ({
-    default: ExitFullScreenButton,
-  }))
-);
-
-/**
- * A pure component that resembles a button to exit full screen mode.
- */
-export const ExitFullScreenButtonComponent = withSuspense(LazyExitFullScreenButtonComponent);
-
-/**
- * Lazy-loaded connected component.  Must be wrapped in `React.Suspense` and a Provider.
- */
-export const LazyExitFullScreenButton = React.lazy(() =>
-  import('./exit_full_screen_button').then(({ ExitFullScreenButton }) => ({
-    default: ExitFullScreenButton,
-  }))
-);
-
-/**
- * A component that can be used to exit full screen mode in Kibana.  Requires a Provider for
- * relevant services.
- */
-export const ExitFullScreenButton = withSuspense(LazyExitFullScreenButton);
+export { ExitFullScreenButton } from './exit_full_screen_button';
+export { ExitFullScreenButton as ExitFullScreenButtonComponent } from './exit_full_screen_button.component';
