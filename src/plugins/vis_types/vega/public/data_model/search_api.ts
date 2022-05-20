4b6302d80e270a1ef888be89e8ab64d411787f83
Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>
2022-05-20 12:52:26 +0300

    Remove `injectedMetadata` in `vega` (#132521)
    * Remove injectedMetadata in vega
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/vis_types/vega/public/data_model/search_api.ts
+++ src/plugins/vis_types/vega/public/data_model/search_api.ts
@@ -8,7 +8,7 @@
 
 import { combineLatest, from } from 'rxjs';
 import { map, tap, switchMap } from 'rxjs/operators';
-import type { CoreStart, IUiSettingsClient, KibanaExecutionContext } from '@kbn/core/public';
+import type { IUiSettingsClient, KibanaExecutionContext } from '@kbn/core/public';
 import {
   getSearchParamsFromRequest,
   SearchRequest,
@@ -47,7 +47,6 @@ export const extendSearchParamsWithRuntimeFields = async (
 
 export interface SearchAPIDependencies {
   uiSettings: IUiSettingsClient;
-  injectedMetadata: CoreStart['injectedMetadata'];
   search: DataPublicPluginStart['search'];
   indexPatterns: DataViewsPublicPluginStart;
 }
