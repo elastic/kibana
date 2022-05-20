794179a2fda5b282919d29d5725dded22dc8cfd6
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-05-20 11:50:43 +0200

    [Files] File service CRUD functionality and audit logging (#132416)
    * big ol wipperooni, but got a lot of initial functionality written out
    
    * added some todos and fixed import
    
    * finish adding integration tests for file service! also added the partial update capability to file
    
    * added file service integration tests and finalized Files and InternalFileService APIs
    
    * added security plugin
    
    * big refactor for security audit logging: FileServiceFactory
    
    * type lint
    
    * update delete file integration test and remove done TODOs
    
    * move comment
    
    * docs, accessibility modifiers and updated some import ordering
    
    * fix comments
    
    * pass in index name to ES blob store
    
    * tidy up debug and error messages

---
+++ x-pack/plugins/files/server/file/file_attributes_reducer.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { FileSavedObjectAttributes, UpdatableFileAttributes } from '../../common';
+
+export type Action =
+  | {
+      action: 'uploading';
+      payload?: undefined;
+    }
+  | { action: 'uploaded'; payload: { content_ref: string; size: number } }
+  | { action: 'uploadError'; payload?: undefined }
+  | { action: 'updateFile'; payload: UpdatableFileAttributes };
+
+export function createDefaultFileAttributes(): Pick<
+  FileSavedObjectAttributes,
+  'created_at' | 'updated_at' | 'status'
+> {
+  const dateString = new Date().toISOString();
+  return {
+    created_at: dateString,
+    updated_at: dateString,
+    status: 'AWAITING_UPLOAD',
+  };
+}
+
+export function fileAttributesReducer(
+  state: FileSavedObjectAttributes,
+  { action, payload }: Action
+): FileSavedObjectAttributes {
+  switch (action) {
+    case 'uploading':
+      return { ...state, content_ref: undefined, status: 'UPLOADING' };
+    case 'uploaded':
+      return { ...state, ...payload, status: 'READY' };
+    case 'uploadError':
+      return { ...state, status: 'UPLOAD_ERROR', content_ref: undefined };
+    case 'updateFile':
+      const d = new Date();
+      return {
+        ...state,
+        ...payload,
+        updated_at: d.toISOString(),
+      };
+    default:
+      return state;
+  }
+}
