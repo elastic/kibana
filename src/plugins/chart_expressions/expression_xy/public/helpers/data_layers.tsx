24cbb32a97288792ae4f3719926b73a7071ac093
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-05-20 16:27:14 +0300

    [XY] `pointsRadius`, `showPoints` and `lineWidth`. (#130391)
    * Added pointsRadius, showPoints and lineWidth.
    
    * Added tests.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/chart_expressions/expression_xy/public/helpers/data_layers.tsx
+++ src/plugins/chart_expressions/expression_xy/public/helpers/data_layers.tsx
@@ -8,6 +8,7 @@
 
 import {
   AreaSeriesProps,
+  AreaSeriesStyle,
   BarSeriesProps,
   ColorVariant,
   LineSeriesProps,
@@ -79,6 +80,14 @@ type GetColorFn = (
     syncColors?: boolean;
   }
 ) => string | null;
+
+type GetLineConfigFn = (config: {
+  xAccessor: string | undefined;
+  markSizeAccessor: string | undefined;
+  emphasizeFitting?: boolean;
+  showPoints?: boolean;
+  pointsRadius?: number;
+}) => Partial<AreaSeriesStyle['line']>;
 
 export interface DatatableWithFormatInfo {
   table: Datatable;
@@ -227,17 +236,26 @@ const getSeriesName: GetSeriesNameFn = (
   return splitColumnId ? data.seriesKeys[0] : columnToLabelMap[data.seriesKeys[0]] ?? null;
 };
 
-const getPointConfig = (
-  xAccessor: string | undefined,
-  markSizeAccessor: string | undefined,
-  emphasizeFitting?: boolean
-) => ({
-  visible: !xAccessor || markSizeAccessor !== undefined,
-  radius: xAccessor && !emphasizeFitting ? 5 : 0,
+const getPointConfig: GetLineConfigFn = ({
+  xAccessor,
+  markSizeAccessor,
+  emphasizeFitting,
+  showPoints,
+  pointsRadius,
+}) => ({
+  visible: showPoints !== undefined ? showPoints : !xAccessor || markSizeAccessor !== undefined,
+  radius: pointsRadius !== undefined ? pointsRadius : xAccessor && !emphasizeFitting ? 5 : 0,
   fill: markSizeAccessor ? ColorVariant.Series : undefined,
 });
 
-const getLineConfig = () => ({ visible: true, stroke: ColorVariant.Series, opacity: 1, dash: [] });
+const getFitLineConfig = () => ({
+  visible: true,
+  stroke: ColorVariant.Series,
+  opacity: 1,
+  dash: [],
+});
+
+const getLineConfig = (strokeWidth?: number) => ({ strokeWidth });
 
 const getColor: GetColorFn = (
   { yAccessor, seriesKeys },
@@ -363,15 +381,29 @@ export const getSeriesProps: GetSeriesPropsFn = ({
     stackMode: isPercentage ? StackMode.Percentage : undefined,
     timeZone,
     areaSeriesStyle: {
-      point: getPointConfig(xColumnId, markSizeColumnId, emphasizeFitting),
+      point: getPointConfig({
+        xAccessor: xColumnId,
+        markSizeAccessor: markSizeColumnId,
+        emphasizeFitting,
+        showPoints: layer.showPoints,
+        pointsRadius: layer.pointsRadius,
+      }),
       ...(fillOpacity && { area: { opacity: fillOpacity } }),
       ...(emphasizeFitting && {
-        fit: { area: { opacity: fillOpacity || 0.5 }, line: getLineConfig() },
+        fit: { area: { opacity: fillOpacity || 0.5 }, line: getFitLineConfig() },
       }),
+      line: getLineConfig(layer.lineWidth),
     },
     lineSeriesStyle: {
-      point: getPointConfig(xColumnId, markSizeColumnId, emphasizeFitting),
-      ...(emphasizeFitting && { fit: { line: getLineConfig() } }),
+      point: getPointConfig({
+        xAccessor: xColumnId,
+        markSizeAccessor: markSizeColumnId,
+        emphasizeFitting,
+        showPoints: layer.showPoints,
+        pointsRadius: layer.pointsRadius,
+      }),
+      ...(emphasizeFitting && { fit: { line: getFitLineConfig() } }),
+      line: getLineConfig(layer.lineWidth),
     },
     name(d) {
       return getSeriesName(d, {
