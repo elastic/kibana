f75b6fa1561fb8592a493c41c08302fddd136760
Uladzislau Lasitsa <Uladzislau_Lasitsa@epam.com>
2022-05-20 12:50:46 +0300

    [XY] Add `addTimeMarker` arg (#131495)
    * Add `addTimeMarker` arg
    
    * Some fixes
    
    * Update validation
    
    * Fix snapshots
    
    * Some fixes after merge
    
    * Add unit tests
    
    * Fix CI
    
    * Update src/plugins/chart_expressions/expression_xy/public/helpers/data_layers.tsx
    
    Co-authored-by: Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
    
    * Fixed tests
    
    * Fix checks
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>

--- src/plugins/chart_expressions/expression_xy/public/helpers/data_layers.tsx
+++ src/plugins/chart_expressions/expression_xy/public/helpers/data_layers.tsx
@@ -53,6 +53,7 @@ type GetSeriesPropsFn = (config: {
   emphasizeFitting?: boolean;
   fillOpacity?: number;
   formattedDatatableInfo: DatatableWithFormatInfo;
+  defaultXScaleType: XScaleType;
 }) => SeriesSpec;
 
 type GetSeriesNameFn = (
@@ -280,6 +281,7 @@ export const getSeriesProps: GetSeriesPropsFn = ({
   emphasizeFitting,
   fillOpacity,
   formattedDatatableInfo,
+  defaultXScaleType,
 }): SeriesSpec => {
   const { table, markSizeAccessor } = layer;
   const isStacked = layer.seriesType.includes('stacked');
@@ -342,7 +344,7 @@ export const getSeriesProps: GetSeriesPropsFn = ({
     markSizeAccessor: markSizeColumnId,
     markFormat: (value) => markFormatter.convert(value),
     data: rows,
-    xScaleType: xColumnId ? layer.xScaleType : 'ordinal',
+    xScaleType: xColumnId ? layer.xScaleType ?? defaultXScaleType : 'ordinal',
     yScaleType:
       formatter?.id === 'bytes' && yAxis?.scale === ScaleType.Linear
         ? ScaleType.LinearBinary
