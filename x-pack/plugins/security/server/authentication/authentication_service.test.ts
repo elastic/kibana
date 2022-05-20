45da50656b722f17e6cdd0c5856535ca419d5ed9
shahzad31 <shahzad.muhammad@elastic.co>
2022-05-20 11:35:24 +0200

    update test

--- x-pack/plugins/security/server/authentication/authentication_service.test.ts
+++ x-pack/plugins/security/server/authentication/authentication_service.test.ts
@@ -108,6 +108,7 @@ describe('AuthenticationService', () => {
       loggers: loggingSystemMock.create(),
       featureUsageService: securityFeatureUsageServiceMock.createStartContract(),
       session: sessionMock.create(),
+      applicationName: '.kibana,.kibana',
     };
     (mockStartAuthenticationParams.http.basePath.get as jest.Mock).mockImplementation(
       () => mockStartAuthenticationParams.http.basePath.serverBasePath
