c7cafbe9574213654a412be690bd6b301acd4b51
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-05-20 11:50:43 +0200

    [Files] File service CRUD functionality and audit logging (#132416)
    * big ol wipperooni, but got a lot of initial functionality written out
    
    * added some todos and fixed import
    
    * finish adding integration tests for file service! also added the partial update capability to file
    
    * added file service integration tests and finalized Files and InternalFileService APIs
    
    * added security plugin
    
    * big refactor for security audit logging: FileServiceFactory
    
    * type lint
    
    * update delete file integration test and remove done TODOs
    
    * move comment
    
    * docs, accessibility modifiers and updated some import ordering
    
    * fix comments
    
    * pass in index name to ES blob store
    
    * tidy up debug and error messages

---
+++ x-pack/plugins/files/server/file_service/internal_file_service.ts
@@ -0,0 +1,129 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { Logger, SavedObjectsClientContract, ISavedObjectsRepository } from '@kbn/core/server';
+import { AuditEvent, AuditLogger } from '@kbn/security-plugin/server';
+
+import { BlobStorageService } from '../blob_storage_service';
+import {
+  FileSavedObjectAttributes,
+  File as IFile,
+  FileSavedObject,
+  UpdatableFileAttributes,
+} from '../../common';
+import { File } from '../file';
+
+interface CreateFileArgs<Meta = unknown> {
+  name: string;
+  fileKind: string;
+  alt?: string;
+  meta?: Meta;
+}
+
+interface UpdateFileArgs {
+  id: string;
+  fileKind: string;
+  attributes: UpdatableFileAttributes;
+}
+
+interface DeleteFileArgs {
+  id: string;
+  fileKind: string;
+}
+
+interface ListFilesArgs {
+  fileKind: string;
+}
+
+interface FindFileArgs {
+  id: string;
+  fileKind: string;
+}
+
+/**
+ * Service containing methods for working with files.
+ *
+ * All file business logic is encapsulated in the {@link File} class.
+ *
+ * @internal
+ */
+export class InternalFileService {
+  constructor(
+    private readonly savedObjectType: string,
+    private readonly soClient: SavedObjectsClientContract | ISavedObjectsRepository,
+    private readonly blobStorageService: BlobStorageService,
+    private readonly auditLogger: undefined | AuditLogger,
+    private readonly logger: Logger
+  ) {}
+
+  // TODO: Enforce that file kind exists based on registry
+  public async createFile(args: CreateFileArgs): Promise<IFile> {
+    return await File.create(args, this);
+  }
+
+  public createAuditLog(event: AuditEvent) {
+    if (this.auditLogger) {
+      this.auditLogger.log(event);
+    } else {
+      // Otherwise just log to info
+      this.logger.info(event.message);
+    }
+  }
+
+  public async updateFile({ attributes, fileKind, id }: UpdateFileArgs): Promise<void> {
+    const file = await this.find({ fileKind, id });
+    await file.update(attributes);
+  }
+
+  public async deleteFile({ id, fileKind }: DeleteFileArgs): Promise<void> {
+    const file = await this.find({ id, fileKind });
+    await file.delete();
+  }
+
+  public async find({ fileKind, id }: FindFileArgs): Promise<IFile> {
+    try {
+      const result = await this.soClient.get<FileSavedObjectAttributes>(this.savedObjectType, id);
+      const actualFileKind = result.attributes.file_kind;
+      if (actualFileKind !== fileKind) {
+        throw new Error(`Unexpected file kind "${actualFileKind}", expected "${fileKind}".`);
+      }
+      return this.toFile(result);
+    } catch (e) {
+      this.logger.error(`Could not retrieve file: ${e}`);
+      throw e;
+    }
+  }
+
+  public async list({ fileKind }: ListFilesArgs): Promise<IFile[]> {
+    const result = await this.soClient.find<FileSavedObjectAttributes>({
+      type: this.savedObjectType,
+      filter: `${this.savedObjectType}.attributes.file_kind: ${fileKind}`,
+    });
+
+    return result.saved_objects.map(this.toFile.bind(this));
+  }
+
+  public toFile(fileSO: FileSavedObject): IFile {
+    return new File(fileSO, this, this.blobStorageService, this.logger.get(`file-${fileSO.id}`));
+  }
+
+  public async createSO(attributes: FileSavedObjectAttributes): Promise<FileSavedObject<unknown>> {
+    return this.soClient.create<FileSavedObjectAttributes>(this.savedObjectType, attributes);
+  }
+
+  public async deleteSO(id: string): Promise<void> {
+    await this.soClient.delete(this.savedObjectType, id);
+  }
+
+  public async updateSO(
+    id: string,
+    attributes: FileSavedObjectAttributes
+  ): Promise<FileSavedObject> {
+    const updateResponse = await this.soClient.update(this.savedObjectType, id, attributes);
+    return updateResponse as FileSavedObject;
+  }
+}
