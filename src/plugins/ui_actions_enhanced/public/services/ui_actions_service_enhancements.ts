30acf53d37919e134a3321cf98d02de69f032943
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-05-20 12:10:55 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- src/plugins/ui_actions_enhanced/public/services/ui_actions_service_enhancements.ts
+++ src/plugins/ui_actions_enhanced/public/services/ui_actions_service_enhancements.ts
@@ -1,8 +1,9 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { SerializableRecord } from '@kbn/utility-types';
@@ -28,7 +29,7 @@ export interface UiActionsServiceEnhancementsParams {
   readonly actionFactories?: ActionFactoryRegistry;
   readonly getLicense: () => ILicense;
   readonly featureUsageSetup?: LicensingPluginSetup['featureUsage'];
-  readonly getFeatureUsageStart: () => (LicensingPluginStart['featureUsage'] | undefined);
+  readonly getFeatureUsageStart: () => LicensingPluginStart['featureUsage'] | undefined;
 }
 
 export class UiActionsServiceEnhancements
