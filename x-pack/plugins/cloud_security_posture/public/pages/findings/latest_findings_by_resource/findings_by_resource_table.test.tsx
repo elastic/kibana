720a953cffe5c9bba12d505c7cd4b6d221a534da
Kfir Peled <61654899+kfirpeled@users.noreply.github.com>
2022-05-22 13:18:42 +0300

    [Cloud Posture] Findings - Group by resource - Fixed bug not showing results (#132529)

--- x-pack/plugins/cloud_security_posture/public/pages/findings/latest_findings_by_resource/findings_by_resource_table.test.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/findings/latest_findings_by_resource/findings_by_resource_table.test.tsx
@@ -21,14 +21,23 @@ import { TestProvider } from '../../../test/test_provider';
 
 const chance = new Chance();
 
-const getFakeFindingsByResource = (): CspFindingsByResource => ({
-  resource_id: chance.guid(),
-  cis_sections: [chance.word(), chance.word()],
-  failed_findings: {
-    total: chance.integer(),
-    normalized: chance.integer({ min: 0, max: 1 }),
-  },
-});
+const getFakeFindingsByResource = (): CspFindingsByResource => {
+  const count = chance.integer();
+  const total = chance.integer() + count + 1;
+  const normalized = count / total;
+
+  return {
+    resource_id: chance.guid(),
+    resource_name: chance.word(),
+    resource_subtype: chance.word(),
+    cis_sections: [chance.word(), chance.word()],
+    failed_findings: {
+      count,
+      normalized,
+      total_findings: total,
+    },
+  };
+};
 
 type TableProps = PropsOf<typeof FindingsByResourceTable>;
 
@@ -74,8 +83,11 @@ describe('<FindingsByResourceTable />', () => {
       );
       expect(row).toBeInTheDocument();
       expect(within(row).getByText(item.resource_id)).toBeInTheDocument();
+      if (item.resource_name) expect(within(row).getByText(item.resource_name)).toBeInTheDocument();
+      if (item.resource_subtype)
+        expect(within(row).getByText(item.resource_subtype)).toBeInTheDocument();
       expect(within(row).getByText(item.cis_sections.join(', '))).toBeInTheDocument();
-      expect(within(row).getByText(formatNumber(item.failed_findings.total))).toBeInTheDocument();
+      expect(within(row).getByText(formatNumber(item.failed_findings.count))).toBeInTheDocument();
       expect(
         within(row).getByText(new RegExp(numeral(item.failed_findings.normalized).format('0%')))
       ).toBeInTheDocument();
