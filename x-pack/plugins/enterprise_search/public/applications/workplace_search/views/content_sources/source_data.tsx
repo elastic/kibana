efdb7adc3dd3c80e7b6ae72a4dbcb69f054b1c1d
Byron Hulcher <byron.hulcher@elastic.co>
2022-05-22 21:02:58 -0400

    Add categories for SharePoint

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/source_data.tsx
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/source_data.tsx
@@ -607,6 +607,13 @@ export const staticSourceData: SourceDataItem[] = [
   {
     name: SOURCE_NAMES.SHAREPOINT,
     serviceType: 'share_point',
+    categories: [
+      SOURCE_CATEGORIES.FILE_SHARING,
+      SOURCE_CATEGORIES.STORAGE,
+      SOURCE_CATEGORIES.CLOUD,
+      SOURCE_CATEGORIES.MICROSOFT,
+      SOURCE_CATEGORIES.OFFICE_365,
+    ],
     configuration: {
       isPublicKey: false,
       hasOauthRedirect: true,
