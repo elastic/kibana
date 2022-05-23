4f5687d64d27f21232ff57facaecce43b6f2f49d
Ievgen Sorokopud <e40pud@gmail.com>
2022-05-23 12:20:32 +0200

    Update x-pack/plugins/security_solution/public/detections/components/rules/description_step/helpers.tsx
    Co-authored-by: Marshall Main <55718608+marshallmain@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/detections/components/rules/description_step/helpers.tsx
+++ x-pack/plugins/security_solution/public/detections/components/rules/description_step/helpers.tsx
@@ -146,7 +146,7 @@ export const buildEqlOptionsDescription = (eqlOptions: EqlOptionsSelected): List
       ...items,
       {
         title: <>{i18n.EQL_TIMESTAMP_FIELD_LABEL}</>,
-        description: <Query>{eqlOptions.timestampField}</Query>,
+        description: <>{eqlOptions.timestampField}</>,
       },
     ];
   }
