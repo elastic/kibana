7f6b11a0492d0b35c02806bf033144b6d5d996a7
Paul Tavares <paul.tavares@elastic.co>
2022-05-23 17:43:33 -0400

    Styles for the Command input area

--- x-pack/plugins/security_solution/public/management/components/console/components/command_input/command_input.tsx
+++ x-pack/plugins/security_solution/public/management/components/console/components/command_input/command_input.tsx
@@ -15,6 +15,10 @@ import { useTestIdGenerator } from '../../../hooks/use_test_id_generator';
 import { useDataTestSubj } from '../../hooks/state_selectors/use_data_test_subj';
 
 const CommandInputContainer = styled.div`
+  background-color: ${({ theme: { eui } }) => eui.euiColorGhost};
+  border-radius: ${({ theme: { eui } }) => eui.euiBorderRadius};
+  padding: ${({ theme: { eui } }) => eui.paddingSizes.s};
+
   .prompt {
     padding-right: 1ch;
   }
