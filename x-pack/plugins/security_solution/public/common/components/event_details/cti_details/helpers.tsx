94b4bfd6f45affc2270c4d8348f03c9fceb17236
Yuliia Naumenko <yuliia.naumenko@elastic.com>
2022-05-22 22:40:49 -0700

    fixed tests

--- x-pack/plugins/security_solution/public/common/components/event_details/cti_details/helpers.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/cti_details/helpers.tsx
@@ -24,7 +24,7 @@ import type {
 } from '../../../../../common/search_strategy/security_solution/cti';
 import { isValidEventField } from '../../../../../common/search_strategy/security_solution/cti';
 import { getFirstElement } from '../../../../../common/utils/data_retrieval';
-import { getDataFromSourceHits } from '../../../../../common/utils/field_formatters';
+import { getDataFromFieldsHits } from '../../../../../common/utils/field_formatters';
 
 export const isInvestigationTimeEnrichment = (type: string | undefined) =>
   type === ENRICHMENT_TYPES.InvestigationTime;
@@ -45,7 +45,7 @@ export const parseExistingEnrichments = (
   return enrichmentStrings.reduce<TimelineEventsDetailsItem[][]>(
     (enrichments, enrichmentString) => {
       try {
-        const enrichment = getDataFromSourceHits(JSON.parse(enrichmentString));
+        const enrichment = getDataFromFieldsHits(originalValue, enrichmentString);
         enrichments.push(enrichment);
       } catch (e) {
         // omit failed parse
