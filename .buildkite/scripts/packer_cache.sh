3d349ae8b5753597a2d252a8c48eba59b48b970d
Brian Seeders <brian.seeders@elastic.co>
2022-05-23 16:43:00 -0400

    Allow packer cache to use remote cache

--- .buildkite/scripts/packer_cache.sh
+++ .buildkite/scripts/packer_cache.sh
@@ -6,6 +6,8 @@ source .buildkite/scripts/common/util.sh
 source .buildkite/scripts/common/env.sh
 source .buildkite/scripts/common/setup_node.sh
 
+export FORCE_BOOTSTRAP_REMOTE_CACHE=true
+
 yarn kbn bootstrap
 
 for version in $(cat versions.json | jq -r '.versions[].version'); do
