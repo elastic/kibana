f80c06e9e69dac8ee802da35ea5192680ee402e9
Zacqary Xeper <zacqary.xeper@elastic.co>
2022-05-24 17:03:09 -0500

    Fix half-baked dropdown work:

--- x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_details.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_details.tsx
@@ -327,8 +327,10 @@ export const RuleDetails: React.FunctionComponent<RuleDetailsProps> = ({
                   <RuleStatusDropdown
                     disableRule={async () => await disableRule(rule)}
                     enableRule={async () => await enableRule(rule)}
-                    snoozeRule={async (snoozeSchedule) => await snoozeRule(rule, snoozeSchedule)}
-                    unsnoozeRule={async (scheduleIds) => await unsnoozeRule(rule, scheduleIds)}
+                    snoozeRule={async (snoozeEndTime: string | -1) =>
+                      await snoozeRule(rule, snoozeEndTime)
+                    }
+                    unsnoozeRule={async () => await unsnoozeRule(rule)}
                     rule={rule as RuleTableItem}
                     onRuleChanged={requestRefresh}
                     direction="row"
