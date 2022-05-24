e80dffccefada9bbd1241c36768ec04585e6df29
Walter Rafelsberger <walter@elastic.co>
2022-05-24 11:42:07 +0200

    [ML] Document stringReducer().

--- packages/kbn-aiops-utils/src/lib/string_reducer.ts
+++ packages/kbn-aiops-utils/src/lib/string_reducer.ts
@@ -11,16 +11,26 @@ import { Reducer, ReducerAction, ReducerState } from 'react';
 type StringReducerPayload = string | string[] | undefined;
 export type StringReducer = Reducer<string, StringReducerPayload>;
 
+/**
+ * The `stringReducer` is provided to handle plain string based streams with `streamFactory()`.
+ *
+ * @param state   - The current state, being the string fetched so far.
+ * @param payload — The state update can be a plain string, an array of strings or `undefined`.
+ *                  * An array of strings will be joined without a delimiter and added to the current string.
+ *                    In combination with `useFetchStream`'s buffering this allows to do bulk updates
+ *                    within the reducer without triggering a React/DOM update on every stream chunk.
+ *                  * `undefined` can be used to reset the state to an empty string, for example, when a
+ *                    UI has the option to trigger a refetch of a stream.
+ *
+ * @returns The updated state, a string that combines the previous string and the payload.
+ */
 export function stringReducer(
   state: ReducerState<StringReducer>,
   payload: ReducerAction<StringReducer>
 ): ReducerState<StringReducer> {
-  // If the payload is undefined reset the state
   if (payload === undefined) {
     return '';
   }
 
-  // Supporting arrays of strings allows to do bulk updates without
-  // triggering DOM updates on every dispatch.
   return `${state}${Array.isArray(payload) ? payload.join('') : payload}`;
 }
