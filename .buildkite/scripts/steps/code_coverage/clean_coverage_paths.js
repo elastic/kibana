74c6d5ed80e88f245938f5d7c2077977215b9b6c
Tre' Seymour <wayne.seymour@elastic.co>
2022-05-24 10:50:05 +0100

    [QA][Code Coverage] Change replace anchor and other cleanups
    Drop old replacment anchor.
    Cleanup commented out code.
    Cleanup ci formatting.
    Use named fn's for merging, reporting, etc.
      The purpose is to make the names make sense as some of these commands
      are confusing.

--- .buildkite/scripts/steps/code_coverage/clean_coverage_paths.js
+++ .buildkite/scripts/steps/code_coverage/clean_coverage_paths.js
@@ -1,6 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 const { readFileSync, writeFileSync } = require('fs');
+
 const FILE = process.argv[2];
-console.log(`\n### process.env.KIBANA_DIR: \n\t${process.env.KIBANA_DIR}`);
-console.log(`\n\t### Replacing paths in: \n\t${FILE}`);
-// TODO-TRE: Drop hardcoded replacement
-writeFileSync(FILE, readFileSync(FILE).toString().replaceAll(process.env.KIBANA_DIR, 'LEETRE'));
+const replacementAnchor = process.env.REPLACEMENT_ANCHOR || 'CC_REPLACEMENT_ANCHOR';
+writeFileSync(
+  FILE,
+  readFileSync(FILE).toString().replaceAll(process.env.KIBANA_DIR, replacementAnchor)
+);
