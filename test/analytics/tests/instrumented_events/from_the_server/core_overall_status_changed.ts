b3d73d9d519e2f4e58b94737718db5f9437fdb8d
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2022-05-25 19:16:08 +0200

    Unskip new skipped tests

--- test/analytics/tests/instrumented_events/from_the_server/core_overall_status_changed.ts
+++ test/analytics/tests/instrumented_events/from_the_server/core_overall_status_changed.ts
@@ -13,8 +13,7 @@ import { FtrProviderContext } from '../../../services';
 export default function ({ getService }: FtrProviderContext) {
   const ebtServerHelper = getService('kibana_ebt_server');
 
-  // Failing: See https://github.com/elastic/kibana/issues/132953
-  describe.skip('core-overall_status_changed', () => {
+  describe('core-overall_status_changed', () => {
     let initialEvent: Event;
     let secondEvent: Event;
 
