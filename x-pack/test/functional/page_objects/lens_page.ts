72d78131b3c06cbc999890b1d2c75a36435b326c
Joe Reuter <johannes.reuter@elastic.co>
2022-05-25 09:44:26 +0200

    try another thing

--- x-pack/test/functional/page_objects/lens_page.ts
+++ x-pack/test/functional/page_objects/lens_page.ts
@@ -124,6 +124,9 @@ export function LensPageProvider({ getService, getPageObjects }: FtrProviderCont
       },
       layerIndex = 0
     ) {
+      const operationSelector = opts.isPreviousIncompatible
+        ? `lns-indexPatternDimension-${opts.operation} incompatible`
+        : `lns-indexPatternDimension-${opts.operation}`;
       await retry.try(async () => {
         if (!(await testSubjects.exists(operationSelector))) {
           await testSubjects.click(`lns-layerPanel-${layerIndex} > ${opts.dimension}`);
@@ -134,9 +137,6 @@ export function LensPageProvider({ getService, getPageObjects }: FtrProviderCont
       if (opts.operation === 'formula') {
         await this.switchToFormula();
       } else {
-        const operationSelector = opts.isPreviousIncompatible
-          ? `lns-indexPatternDimension-${opts.operation} incompatible`
-          : `lns-indexPatternDimension-${opts.operation}`;
         async function getAriaPressed() {
           const operationSelectorContainer = await testSubjects.find(operationSelector);
           await testSubjects.click(operationSelector);
