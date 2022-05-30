63f68cd488974499d80dfd56626a85c02f02964f
shahzad31 <shahzad.muhammad@elastic.co>
2022-05-30 12:25:50 +0200

    add ci step

--- vars/tasks.groovy
+++ vars/tasks.groovy
@@ -164,6 +164,14 @@ def functionalXpack(Map params = [:]) {
         task(kibanaPipeline.functionalTestProcess('xpack-UptimePlaywright', './test/scripts/jenkins_uptime_playwright.sh'))
       }
     }
+    whenChanged([
+      'x-pack/plugins/ux/',
+    ]) {
+      if (githubPr.isPr()) {
+        task(kibanaPipeline.functionalTestProcess('xpack-uxPluginSynthetics', './test/scripts/jenkins_ux_synthetics.sh'))
+      }
+    }
+
 
     whenChanged([
       'x-pack/plugins/fleet/',
