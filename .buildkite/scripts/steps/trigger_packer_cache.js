61d7fcb4b736fd9947116ac054e9d0c035b20855
Brian Seeders <brian.seeders@elastic.co>
2022-05-31 11:52:16 -0400

    [es-snapshots] Trigger packer cache job using API instead of pipeline

---
+++ .buildkite/scripts/steps/trigger_packer_cache.js
@@ -0,0 +1,20 @@
+const { BuildkiteClient } = require('kibana-buildkite-library');
+
+(async () => {
+  try {
+    const client = new BuildkiteClient();
+    const build = await client.triggerBuild('kibana-agent-packer-cache', {
+      commit: 'HEAD',
+      branch: 'main',
+    });
+    console.log(`Triggered build: ${build.web_url}`);
+    process.exit(0);
+  } catch (ex) {
+    console.error('Buildkite API Error', ex.toString());
+    if (ex.response) {
+      console.error('HTTP Error Response Status', ex.response.status);
+      console.error('HTTP Error Response Body', ex.response.data);
+    }
+    process.exit(1);
+  }
+})();
