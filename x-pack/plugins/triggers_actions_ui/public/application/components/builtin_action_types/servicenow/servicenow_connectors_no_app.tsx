b945e6807e4f641ecd863361a3a3bfff746e33dc
Christos Nasikas <christos.nasikas@elastic.co>
2022-05-31 12:01:20 +0300

    Fix itom

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/servicenow/servicenow_connectors_no_app.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/servicenow/servicenow_connectors_no_app.tsx
@@ -6,59 +6,23 @@
  */
 
 import React from 'react';
-import { EuiFormRow, EuiLink, EuiSpacer } from '@elastic/eui';
-import { FormattedMessage } from '@kbn/i18n-react';
-import { useKibana } from '../../../../common/lib/kibana';
+import { useFormData } from '@kbn/es-ui-shared-plugin/static/forms/hook_form_lib';
 
 import { ActionConnectorFieldsProps } from '../../../../types';
-
-import {
-  ConfigFieldSchema,
-  SecretsFieldSchema,
-  SimpleConnectorForm,
-} from '../../simple_connector_form';
-import * as i18n from './translations';
-
-const configFormSchema: ConfigFieldSchema[] = [
-  { id: 'apiUrl', label: i18n.API_URL_LABEL, isUrlField: true, helpText: i18n.API_URL_HELPTEXT },
-];
-const secretsFormSchema: SecretsFieldSchema[] = [
-  { id: 'username', label: i18n.USERNAME_LABEL },
-  { id: 'password', label: i18n.PASSWORD_LABEL, isPasswordField: true },
-];
+import { Credentials } from './credentials';
+import { ConnectorFormSchema } from '../../../sections/action_connector_form/types';
+import { ServiceNowConfig, ServiceNowSecrets } from './types';
 
 const ServiceNowConnectorFieldsNoApp: React.FC<ActionConnectorFieldsProps> = ({
   isEdit,
   readOnly,
 }) => {
-  const { docLinks } = useKibana().services;
+  const [{ config }] = useFormData<ConnectorFormSchema<ServiceNowConfig, ServiceNowSecrets>>({
+    watch: ['config.isOAuth'],
+  });
+  const { isOAuth = false } = config ?? {};
 
-  return (
-    <>
-      <EuiFormRow fullWidth>
-        <p>
-          <FormattedMessage
-            id="xpack.triggersActionsUI.components.builtinActionTypes.serviceNowAction.apiUrlHelpLabel"
-            defaultMessage="Provide the full URL to the desired ServiceNow instance. If you don't have one, {instance}."
-            values={{
-              instance: (
-                <EuiLink href={docLinks.links.alerting.serviceNowAction} target="_blank">
-                  {i18n.SETUP_DEV_INSTANCE}
-                </EuiLink>
-              ),
-            }}
-          />
-        </p>
-      </EuiFormRow>
-      <EuiSpacer size="l" />
-      <SimpleConnectorForm
-        isEdit={isEdit}
-        readOnly={readOnly}
-        configFormSchema={configFormSchema}
-        secretsFormSchema={secretsFormSchema}
-      />
-    </>
-  );
+  return <Credentials readOnly={readOnly} isLoading={false} isOAuth={isOAuth} />;
 };
 
 // eslint-disable-next-line import/no-default-export
