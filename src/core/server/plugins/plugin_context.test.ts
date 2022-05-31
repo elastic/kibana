ec7f3d703d302574e96ef8ad50724616abd03559
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2022-05-31 16:04:56 +0200

    Adapt core imports to use `@kbn/config` directly (#133204)
    * Adapt core imports to use `@kbn/config` directly
    
    * fix import from test_helpers
    
    * lint
    
    * update generated doc
    
    * adapt core usages to use `@kbn/config-mocks`
    
    * missing mock imports

--- src/core/server/plugins/plugin_context.test.ts
+++ src/core/server/plugins/plugin_context.test.ts
@@ -10,15 +10,15 @@ import { duration } from 'moment';
 import { first } from 'rxjs/operators';
 import { REPO_ROOT } from '@kbn/utils';
 import { fromRoot } from '@kbn/utils';
+import { Env } from '@kbn/config';
+import { rawConfigServiceMock, getEnvOptions, configServiceMock } from '@kbn/config-mocks';
 import {
   createPluginInitializerContext,
   createPluginPrebootSetupContext,
   InstanceInfo,
 } from './plugin_context';
 import { CoreContext } from '../core_context';
-import { Env } from '../config';
 import { loggingSystemMock } from '../logging/logging_system.mock';
-import { rawConfigServiceMock, getEnvOptions, configServiceMock } from '../config/mocks';
 import { PluginManifest, PluginType } from './types';
 import { Server } from '../server';
 import { schema, ByteSizeValue } from '@kbn/config-schema';
