96a69080dd065356dbf0f08d331054cccb8050a5
klacabane <kevin.lacabane@elastic.co>
2022-05-31 12:51:38 +0200

    fix api tests

--- x-pack/test/api_integration/apis/monitoring/_health/fixtures/response_es_beats.js
+++ x-pack/test/api_integration/apis/monitoring/_health/fixtures/response_es_beats.js
@@ -18,25 +18,25 @@ export const esBeatsResponse = (date = moment().format('YYYY.MM.DD')) => {
           cluster: {
             tqiiSubfSgWrl68VJn4y2g: {
               index_summary: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: esIndex,
                   lastSeen: '2022-05-23T22:10:15.135Z',
                 },
               },
               index_recovery: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: esIndex,
                   lastSeen: '2022-05-23T22:10:13.584Z',
                 },
               },
               index: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: esIndex,
                   lastSeen: '2022-05-23T22:10:11.994Z',
                 },
               },
               cluster_stats: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: esIndex,
                   lastSeen: '2022-05-23T22:10:18.917Z',
                 },
@@ -46,13 +46,13 @@ export const esBeatsResponse = (date = moment().format('YYYY.MM.DD')) => {
           elasticsearch: {
             QR7smK2oReK_jWHtt5UOSQ: {
               node_stats: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: esIndex,
                   lastSeen: '2022-05-23T22:10:14.268Z',
                 },
               },
               shard: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: esIndex,
                   lastSeen: '2022-05-23T22:09:48.321Z',
                 },
@@ -62,13 +62,13 @@ export const esBeatsResponse = (date = moment().format('YYYY.MM.DD')) => {
           beats: {
             'metricbeat|726039e5-67fe-4e78-837f-072cf2ba0fe7': {
               stats: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: beatsIndex,
                   lastSeen: '2022-05-23T22:17:10.622Z',
                 },
               },
               state: {
-                'Metricbeat 8': {
+                'metricbeat-8': {
                   index: beatsIndex,
                   lastSeen: '2022-05-23T22:17:08.837Z',
                 },
