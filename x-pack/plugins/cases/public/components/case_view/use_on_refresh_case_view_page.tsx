64cb94e217b79effd233413e474eccf4305afa39
Esteban Beltran <esteban.beltran@elastic.co>
2022-05-31 14:38:42 +0200

    add clarifying comment

--- x-pack/plugins/cases/public/components/case_view/use_on_refresh_case_view_page.tsx
+++ x-pack/plugins/cases/public/components/case_view/use_on_refresh_case_view_page.tsx
@@ -9,6 +9,14 @@ import { useCallback } from 'react';
 import { useQueryClient } from 'react-query';
 import { CASE_VIEW_CACHE_KEY } from '../../containers/constants';
 
+/**
+ * Using react-query queryClient to invalidate all the
+ * case view page cache namespace.
+ *
+ * This effectively clears the cache for all the case view pages and
+ * forces the page to fetch all the data again. Including
+ * metrics, actions, comments, etc.
+ */
 export const useRefreshCaseViewPage = () => {
   const queryClient = useQueryClient();
   return useCallback(() => {
