d25838b4a752c84d3777333d663bc6b756217d0a
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-05-31 11:54:25 +0200

    [Files] File kinds registry v0.1 (#132700)
    * initial commit of file kind registry
    
    * add actual registry
    
    * updated attributes reduceer slightly
    
    * separate blob storage interface, make the blob storage type a unique const
    
    * added integration test for file kinds
    
    * update test after adding registry logic
    
    * partial attributes

--- x-pack/plugins/files/server/blob_storage_service/blob_storage_service.ts
+++ x-pack/plugins/files/server/blob_storage_service/blob_storage_service.ts
@@ -5,23 +5,14 @@
  * 2.0.
  */
 
-import { Readable } from 'stream';
 import type { ElasticsearchClient, Logger } from '@kbn/core/server';
+import { BlobStorageSettings } from '../../common';
 import { BlobStorage } from './types';
 import { ElasticsearchBlobStorage } from './adapters';
 
 interface ElasticsearchBlobStorageSettings {
   index?: string;
   chunkSize?: string;
-}
-
-/**
- * This object maps command-specific settings, like the index to use for ES uploading,
- * to the appropriate adapter. The key names must be IDs of the adapter so that it
- * can be correctly mapped.
- */
-interface BlobStorageSettings {
-  es: ElasticsearchBlobStorageSettings;
 }
 
 export class BlobStorageService {
@@ -36,18 +27,7 @@ export class BlobStorageService {
     );
   }
 
-  public async upload(
-    content: Readable,
-    args?: BlobStorageSettings
-  ): Promise<{ id: string; size: number }> {
-    return this.createESBlobStorage({ ...args?.es }).upload(content);
-  }
-
-  public async delete(id: string, args?: BlobStorageSettings): Promise<void> {
-    return this.createESBlobStorage({ ...args?.es }).delete(id);
-  }
-
-  public async download(id: string, size?: number, args?: BlobStorageSettings): Promise<Readable> {
-    return this.createESBlobStorage({ ...args?.es }).download({ id, size });
+  public createBlobStore(args?: BlobStorageSettings) {
+    return this.createESBlobStorage({ ...args?.esSingleIndex });
   }
 }
