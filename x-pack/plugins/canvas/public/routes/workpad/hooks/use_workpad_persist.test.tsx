4578a4a24775641cd444698b8e3bfdda627dd9fa
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-05-31 16:55:04 +0300

    Fixed upload assets and update workpad logic.

--- x-pack/plugins/canvas/public/routes/workpad/hooks/use_workpad_persist.test.tsx
+++ x-pack/plugins/canvas/public/routes/workpad/hooks/use_workpad_persist.test.tsx
@@ -5,14 +5,7 @@
  * 2.0.
  */
 import { renderHook } from '@testing-library/react-hooks';
-import React, { useRef } from 'react';
-import { Subject, of, delay, map } from 'rxjs';
-import { TestScheduler } from 'rxjs/testing';
-import { syncUpdatesStream, useWorkpadPersist, UpdateRequest } from './use_workpad_persist';
-
-const UPDATE_WORKPAD = 'update-workpad';
-const UPDATE = 'update';
-const UPDATE_ASSETS = 'update-assets';
+import { useWorkpadPersist } from './use_workpad_persist';
 
 const mockGetState = jest.fn();
 const mockUpdateWorkpad = jest.fn();
@@ -21,25 +14,17 @@ const mockUpdate = jest.fn();
 
 const mockNotifyError = jest.fn();
 
-let updateRequests$: Subject<UpdateRequest>;
-
 // Mock the hooks and actions used by the UseWorkpad hook
 jest.mock('react-redux', () => ({
   useSelector: (selector: any) => selector(mockGetState()),
 }));
 
 jest.mock('../../../services', () => ({
-  useWorkpadService: () => {
-    mockUpdateWorkpad.mockImplementation(() => UPDATE_WORKPAD);
-    mockUpdateAssets.mockImplementation(() => UPDATE_ASSETS);
-    mockUpdate.mockImplementation(() => UPDATE);
-
-    return {
-      updateWorkpad: mockUpdateWorkpad,
-      updateAssets: mockUpdateAssets,
-      update: mockUpdate,
-    };
-  },
+  useWorkpadService: () => ({
+    updateWorkpad: mockUpdateWorkpad,
+    updateAssets: mockUpdateAssets,
+    update: mockUpdate,
+  }),
   useNotifyService: () => ({
     error: mockNotifyError,
   }),
@@ -48,7 +33,6 @@ jest.mock('../../../services', () => ({
 describe('useWorkpadPersist', () => {
   beforeEach(() => {
     jest.resetAllMocks();
-    updateRequests$ = new Subject<UpdateRequest>();
   });
 
   afterAll(() => {
@@ -71,8 +55,6 @@ describe('useWorkpadPersist', () => {
     renderHook(useWorkpadPersist);
 
     expect(mockUpdateWorkpad).not.toBeCalled();
-    expect(mockUpdateAssets).not.toBeCalled();
-    expect(mockUpdate).not.toBeCalled();
   });
 
   test('changes to workpad cause a workpad update', () => {
@@ -103,73 +85,11 @@ describe('useWorkpadPersist', () => {
     expect(mockUpdateWorkpad).toHaveBeenCalled();
   });
 
-  test('changes to assets cause an asset update', () => {
-    const state = {
-      persistent: {
-        workpad: { some: 'workpad' },
-      },
-      assets: {
-        asset1: 'some asset',
-        asset2: 'other asset',
-      },
-    };
-
-    mockGetState.mockReturnValue(state);
-
-    const { rerender } = renderHook(useWorkpadPersist);
-
-    const newState = {
-      ...state,
-      assets: {
-        asset1: 'some asset',
-      },
-    };
-    mockGetState.mockReturnValue(newState);
-
-    rerender();
-
-    expect(mockUpdateAssets).toHaveBeenCalled();
-  });
-
-  test('changes to both assets and workpad causes a full update', () => {
-    const state = {
-      persistent: {
-        workpad: { some: 'workpad' },
-      },
-      assets: {
-        asset1: 'some asset',
-        asset2: 'other asset',
-      },
-    };
-
-    mockGetState.mockReturnValue(state);
-
-    const { rerender } = renderHook(useWorkpadPersist);
-
-    const newState = {
-      persistent: {
-        workpad: { new: 'workpad' },
-      },
-      assets: {
-        asset1: 'some asset',
-      },
-    };
-    mockGetState.mockReturnValue(newState);
-
-    rerender();
-
-    expect(mockUpdate).toHaveBeenCalled();
-  });
-
   test('non changes causes no updated', () => {
     const state = {
       persistent: {
         workpad: { some: 'workpad' },
       },
-      assets: {
-        asset1: 'some asset',
-        asset2: 'other asset',
-      },
     };
     mockGetState.mockReturnValue(state);
 
@@ -177,19 +97,13 @@ describe('useWorkpadPersist', () => {
 
     rerender();
 
-    expect(mockUpdate).not.toHaveBeenCalled();
     expect(mockUpdateWorkpad).not.toHaveBeenCalled();
-    expect(mockUpdateAssets).not.toHaveBeenCalled();
   });
 
   test('non write permissions causes no updates', () => {
     const state = {
       persistent: {
         workpad: { some: 'workpad' },
-      },
-      assets: {
-        asset1: 'some asset',
-        asset2: 'other asset',
       },
       transient: {
         canUserWrite: false,
@@ -203,9 +117,6 @@ describe('useWorkpadPersist', () => {
       persistent: {
         workpad: { new: 'workpad value' },
       },
-      assets: {
-        asset3: 'something',
-      },
       transient: {
         canUserWrite: false,
       },
@@ -214,121 +125,6 @@ describe('useWorkpadPersist', () => {
 
     rerender();
 
-    expect(mockUpdate).not.toHaveBeenCalled();
     expect(mockUpdateWorkpad).not.toHaveBeenCalled();
-    expect(mockUpdateAssets).not.toHaveBeenCalled();
-  });
-
-  test('waiting for the update to be performed before the execution of the next one', () => {
-    jest.spyOn(React, 'useRef').mockImplementationOnce((value?: unknown | null) => {
-      return useRef(updateRequests$);
-    });
-
-    jest.spyOn(React, 'useRef').mockImplementation(useRef);
-
-    const fnsOrder: Record<number, string> = {
-      0: UPDATE_ASSETS,
-      1: UPDATE_WORKPAD,
-      2: UPDATE,
-    };
-
-    let updateRequestNumber = 0;
-    updateRequests$.asObservable().subscribe((value) => {
-      expect(value()).toBe(fnsOrder[updateRequestNumber]);
-      updateRequestNumber++;
-    });
-
-    const state = {
-      persistent: {
-        workpad: { some: 'workpad' },
-      },
-      assets: {
-        asset1: 'some asset',
-        asset2: 'other asset',
-      },
-      transient: {
-        canUserWrite: true,
-      },
-    };
-    mockGetState.mockReturnValue(state);
-    const { rerender } = renderHook(useWorkpadPersist);
-
-    // Changing assets. `updateAssets` should be enqueued.
-    const nextState1 = { ...state, assets: { asset1: 'some asset' } };
-    mockGetState.mockReturnValue(nextState1);
-    rerender();
-
-    // Changing workpad. `updateWorkpad` should be enqueued.
-    const nextState2 = { ...nextState1, persistent: { workpad: { other: 'workpad' } } };
-    mockGetState.mockReturnValue(nextState2);
-    rerender();
-
-    // Changing workpad and assets. `update` should be enqueued.
-    const nextState3 = {
-      ...state,
-      persistent: { workpad: { other: 'workpad-new' }, assets: { asset3: 'some asset' } },
-    };
-    mockGetState.mockReturnValue(nextState3);
-    rerender();
-  });
-});
-
-describe('syncUpdatesStream', () => {
-  let testScheduler: TestScheduler;
-  const mockErrorFn = jest.fn();
-
-  beforeEach(() => {
-    testScheduler = new TestScheduler((actual, expected) => {
-      expect(actual).toEqual(expected);
-    });
-    jest.resetAllMocks();
-  });
-
-  afterAll(() => {
-    jest.clearAllMocks();
-  });
-
-  it('should wait of the previous observer/promise completion before running the next', () => {
-    testScheduler.run(({ expectObservable }) => {
-      const updateFns = [1, 2, 3, 4].map((value) => () => of(value).pipe(delay(10, testScheduler)));
-
-      const input$ = of(...updateFns);
-      const output$ = syncUpdatesStream(input$, (err) => {});
-
-      expectObservable(output$, '41ms !').toBe('10ms a 9ms b 9ms c 9ms (d|)', {
-        a: 1,
-        b: 2,
-        c: 3,
-        d: 4,
-      });
-    });
-  });
-
-  it('should continue processing the update requests, if some has thrown an error', () => {
-    const error = new Error('OOOPS');
-
-    testScheduler.run(({ expectObservable }) => {
-      const [firstUpdateFn, ...restUpdateFns] = [1, 2, 3, 4].map(
-        (value) => () => of(value).pipe(delay(10, testScheduler))
-      );
-      const throwErrorFn = () =>
-        of([1]).pipe(
-          delay(10, testScheduler),
-          map(() => {
-            throw error;
-          })
-        );
-
-      const input$ = of(firstUpdateFn, throwErrorFn, ...restUpdateFns);
-      const output$ = syncUpdatesStream(input$, mockErrorFn);
-
-      expectObservable(output$, '51ms !').toBe('10ms a 10ms 9ms b 9ms c 9ms (d|)', {
-        a: 1,
-        b: 2,
-        c: 3,
-        d: 4,
-      });
-    });
-    expect(mockErrorFn).toHaveBeenCalledWith(error);
   });
 });
--- x-pack/plugins/canvas/public/routes/workpad/hooks/use_workpad_persist.ts
+++ x-pack/plugins/canvas/public/routes/workpad/hooks/use_workpad_persist.ts
@@ -4,124 +4,36 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { useEffect, useCallback, useRef } from 'react';
-import { isEqual } from 'lodash';
+import { useEffect } from 'react';
 import usePrevious from 'react-use/lib/usePrevious';
 import { useSelector } from 'react-redux';
-import { i18n } from '@kbn/i18n';
-import { catchError, concatMap, EMPTY, from, Observable, ObservableInput, Subject } from 'rxjs';
-import useEffectOnce from 'react-use/lib/useEffectOnce';
 import { CanvasWorkpad, State } from '../../../../types';
-import { getWorkpad, getFullWorkpadPersisted } from '../../../state/selectors/workpad';
+import { getWorkpad } from '../../../state/selectors/workpad';
 import { canUserWrite } from '../../../state/selectors/app';
-import { getAssetIds } from '../../../state/selectors/assets';
 import { useWorkpadService, useNotifyService } from '../../../services';
-
-export type UpdateRequest = () => Promise<unknown> | Observable<unknown>;
-
-const strings = {
-  getSaveFailureTitle: () =>
-    i18n.translate('xpack.canvas.error.esPersist.saveFailureTitle', {
-      defaultMessage: "Couldn't save your changes to Elasticsearch",
-    }),
-  getTooLargeErrorMessage: () =>
-    i18n.translate('xpack.canvas.error.esPersist.tooLargeErrorMessage', {
-      defaultMessage:
-        'The server gave a response that the workpad data was too large. This usually means uploaded image assets that are too large for Kibana or a proxy. Try removing some assets in the asset manager.',
-    }),
-  getUpdateFailureTitle: () =>
-    i18n.translate('xpack.canvas.error.esPersist.updateFailureTitle', {
-      defaultMessage: "Couldn't update workpad",
-    }),
-};
-
-export const syncUpdatesStream = (
-  updateRequests$: Observable<UpdateRequest>,
-  onError: (err: string | Error) => void
-) =>
-  updateRequests$.pipe(
-    concatMap<UpdateRequest, ObservableInput<unknown>>((updateRequest) =>
-      from(updateRequest()).pipe(
-        catchError((err) => {
-          onError(err);
-          return EMPTY;
-        })
-      )
-    )
-  );
+import { notifyError } from '../../../lib/assets';
 
 export const useWorkpadPersist = () => {
-  const { current: updateRequests$ } = useRef(new Subject<UpdateRequest>());
   const service = useWorkpadService();
   const notifyService = useNotifyService();
 
-  const notifyError = useCallback(
-    (err: any) => {
-      const statusCode = err.response && err.response.status;
-      switch (statusCode) {
-        case 400:
-          return notifyService.error(err.response, {
-            title: strings.getSaveFailureTitle(),
-          });
-        case 413:
-          return notifyService.error(strings.getTooLargeErrorMessage(), {
-            title: strings.getSaveFailureTitle(),
-          });
-        default:
-          return notifyService.error(err, {
-            title: strings.getUpdateFailureTitle(),
-          });
-      }
-    },
-    [notifyService]
-  );
-
-  useEffectOnce(() => {
-    const stream$ = syncUpdatesStream(updateRequests$.asObservable(), notifyService.error);
-    const subscription = stream$.subscribe();
-    return () => {
-      updateRequests$.complete();
-      subscription.unsubscribe();
-    };
-  });
-
-  // Watch for workpad state or workpad assets to change and then persist those changes
-  const [workpad, assetIds, fullWorkpad, canWrite]: [
-    CanvasWorkpad,
-    Array<string | number>,
-    CanvasWorkpad,
-    boolean
-  ] = useSelector((state: State) => [
+  // Watch for workpad state and then persist those changes
+  const [workpad, canWrite]: [CanvasWorkpad, boolean] = useSelector((state: State) => [
     getWorkpad(state),
-    getAssetIds(state),
-    getFullWorkpadPersisted(state),
     canUserWrite(state),
   ]);
 
   const previousWorkpad = usePrevious(workpad);
-  const previousAssetIds = usePrevious(assetIds);
 
   const workpadChanged = previousWorkpad && workpad !== previousWorkpad;
-  const assetsChanged = previousAssetIds && !isEqual(assetIds, previousAssetIds);
 
   useEffect(() => {
     if (canWrite) {
-      if (workpadChanged && assetsChanged) {
-        updateRequests$.next(() => service.update(workpad.id, fullWorkpad));
-      } else if (workpadChanged) {
-        updateRequests$.next(() => service.updateWorkpad(workpad.id, workpad));
-      } else if (assetsChanged) {
-        updateRequests$.next(() => service.updateAssets(workpad.id, fullWorkpad.assets));
+      if (workpadChanged) {
+        service.updateWorkpad(workpad.id, workpad).catch((err) => {
+          notifyError(err, notifyService.error);
+        });
       }
     }
-  }, [
-    service,
-    workpad,
-    fullWorkpad,
-    workpadChanged,
-    assetsChanged,
-    canWrite,
-    notifyError,
-    updateRequests$,
-  ]);
+  }, [service, workpad, workpadChanged, canWrite, notifyService.error]);
 };
