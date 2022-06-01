b322c62f080de1c19a4576ccfccead8207616fe9
Faisal Kanout <faisal.kanout@elastic.co>
2022-06-01 15:06:42 +0200

    Add rule title check

--- x-pack/test/observability_functional/apps/observability/alerts/rule_details.ts
+++ x-pack/test/observability_functional/apps/observability/alerts/rule_details.ts
@@ -36,18 +36,6 @@ export default ({ getService }: FtrProviderContext) => {
     return true;
   }
 
-  const getRulesList = async (tableRows: any[]) => {
-    const rows = [];
-    for (const euiTableRow of tableRows) {
-      const $ = await euiTableRow.parseDomContent();
-      rows.push({
-        name: $.findTestSubjects('rulesTableCell-name').find('a').text(),
-        enabled: $.findTestSubjects('rulesTableCell-ContextStatus').find('button').attr('title'),
-      });
-    }
-    return rows;
-  };
-
   describe('Observability Rule Details page', function () {
     this.tags('includeFirefox');
     const observability = getService('observability');
@@ -114,6 +102,7 @@ export default ({ getService }: FtrProviderContext) => {
           async () => await testSubjects.exists('ruleDetails')
         );
       });
+
       it('should navigate to the new rule details page by URL', async () => {
         await observability.alerts.common.navigateToRuleDetailsByRuleId(uptimeRuleId);
         await retry.waitFor(
@@ -123,114 +112,26 @@ export default ({ getService }: FtrProviderContext) => {
       });
     });
 
-    //   describe('Create rule button', () => {
-    //     it('Show Create Rule flyout when Create Rule button is clicked', async () => {
-    //       await observability.alerts.common.navigateToRulesPage();
-    //       await retry.waitFor(
-    //         'Create Rule button is visible',
-    //         async () => await testSubjects.exists('createRuleButton')
-    //       );
-    //       await observability.alerts.rulesPage.clickCreateRuleButton();
-    //       await retry.waitFor(
-    //         'Create Rule flyout is visible',
-    //         async () => await testSubjects.exists('addRuleFlyoutTitle')
-    //       );
-    //     });
-    //   });
+    describe('Rule details page components', () => {
+      it('show the rule name as the page title', async () => {
+        await observability.alerts.common.navigateToRuleDetailsByRuleId(logThresholdRuleId);
+        await retry.waitFor(
+          'Rule name to be visible',
+          async () => await testSubjects.exists('ruleName')
+        );
+        const ruleName = await testSubjects.getVisibleText('ruleName');
+        expect(ruleName).to.be(logThresholdRuleName);
+      });
 
-    //   describe('Rules table', () => {
-    //     let uptimeRuleId: string;
-    //     let logThresholdRuleId: string;
-    //     before(async () => {
-    //       const uptimeRule = {
-    //         params: {
-    //           search: '',
-    //           numTimes: 5,
-    //           timerangeUnit: 'm',
-    //           timerangeCount: 15,
-    //           shouldCheckStatus: true,
-    //           shouldCheckAvailability: true,
-    //           availability: { range: 30, rangeUnit: 'd', threshold: '99' },
-    //         },
-    //         consumer: 'alerts',
-    //         schedule: { interval: '1m' },
-    //         tags: [],
-    //         name: 'uptime',
-    //         rule_type_id: 'xpack.uptime.alerts.monitorStatus',
-    //         notify_when: 'onActionGroupChange',
-    //         actions: [],
-    //       };
-    //       const logThresholdRule = {
-    //         params: {
-    //           timeSize: 5,
-    //           timeUnit: 'm',
-    //           count: { value: 75, comparator: 'more than' },
-    //           criteria: [{ field: 'log.level', comparator: 'equals', value: 'error' }],
-    //         },
-    //         consumer: 'alerts',
-    //         schedule: { interval: '1m' },
-    //         tags: [],
-    //         name: 'error-log',
-    //         rule_type_id: 'logs.alert.document.count',
-    //         notify_when: 'onActionGroupChange',
-    //         actions: [],
-    //       };
-    //       uptimeRuleId = await createRule(uptimeRule);
-    //       logThresholdRuleId = await createRule(logThresholdRule);
-    //       await observability.alerts.common.navigateToRulesPage();
-    //     });
-    //     after(async () => {
-    //       await deleteRuleById(uptimeRuleId);
-    //       await deleteRuleById(logThresholdRuleId);
-    //     });
-
-    //     it('shows the rules table ', async () => {
-    //       await testSubjects.existOrFail('rulesList');
-    //       await testSubjects.waitForDeleted('centerJustifiedSpinner');
-    //       const tableRows = await find.allByCssSelector('.euiTableRow');
-    //       const rows = await getRulesList(tableRows);
-    //       expect(rows.length).to.be(2);
-    //       expect(rows[0].name).to.be('error-log');
-    //       expect(rows[0].enabled).to.be('Enabled');
-    //       expect(rows[1].name).to.be('uptime');
-    //       expect(rows[1].enabled).to.be('Enabled');
-    //     });
-
-    //     it('changes the rule status to "disabled"', async () => {
-    //       await testSubjects.existOrFail('rulesList');
-    //       await observability.alerts.rulesPage.clickRuleStatusDropDownMenu();
-    //       await observability.alerts.rulesPage.clickDisableFromDropDownMenu();
-    //       await retry.waitFor('The rule to be disabled', async () => {
-    //         const tableRows = await find.allByCssSelector('.euiTableRow');
-    //         const rows = await getRulesList(tableRows);
-    //         expect(rows[0].enabled).to.be('Disabled');
-    //         return true;
-    //       });
-    //     });
-    //   });
-
-    //   describe('User permissions', () => {
-    //     it('shows the Create Rule button when user has permissions', async () => {
-    //       await observability.alerts.common.navigateToRulesPage();
-    //       await retry.waitFor(
-    //         'Create rule button',
-    //         async () => await testSubjects.exists('createRuleButton')
-    //       );
-    //     });
-
-    //     it(`shows the no permission prompt when the user has no permissions`, async () => {
-    //       await observability.users.setTestUserRole(
-    //         observability.users.defineBasicObservabilityRole({
-    //           logs: ['read'],
-    //         })
-    //       );
-    //       await observability.alerts.common.navigateToRulesPage();
-    //       await retry.waitFor(
-    //         'No permissions prompt',
-    //         async () => await testSubjects.exists('noPermissionPrompt')
-    //       );
-    //     });
-    //   });
-    // });
+      it('show the rule name as the page title', async () => {
+        await observability.alerts.common.navigateToRuleDetailsByRuleId(logThresholdRuleId);
+        await retry.waitFor(
+          'Rule name to be visible',
+          async () => await testSubjects.exists('ruleName')
+        );
+        const ruleName = await testSubjects.getVisibleText('ruleName');
+        expect(ruleName).to.be(logThresholdRuleName);
+      });
+    });
   });
 };
