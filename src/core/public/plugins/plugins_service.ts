c1bc9440ba5f92adc38fca00afa313f644d98ff0
pgayvallet <pierre.gayvallet@elastic.co>
2022-06-01 10:38:04 +0200

    adapt client-side usages of CoreService

--- src/core/public/plugins/plugins_service.ts
+++ src/core/public/plugins/plugins_service.ts
@@ -6,9 +6,8 @@
  * Side Public License, v 1.
  */
 
-import { CoreContext } from '@kbn/core-public-internal-base';
-import { PluginName, PluginOpaqueId } from '../../server';
-import { CoreService } from '../../types';
+import type { CoreService, CoreContext } from '@kbn/core-base-browser-internal';
+import type { PluginName, PluginOpaqueId } from '@kbn/core-base-common';
 import { PluginWrapper } from './plugin';
 import {
   createPluginInitializerContext,
