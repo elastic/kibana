c6f30f03f17e14e421994cf2b2a32768c2b39b3e
Christos Nasikas <christos.nasikas@elastic.co>
2022-06-02 19:22:14 +0300

    [Cases] Fix spaces_only integration tests (#133365)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/cases_api_integration/spaces_only/tests/common/alerts/get_cases.ts
+++ x-pack/test/cases_api_integration/spaces_only/tests/common/alerts/get_cases.ts
@@ -20,7 +20,7 @@ import { validateCasesFromAlertIDResponse } from '../../../../common/lib/validat
 
 // eslint-disable-next-line import/no-default-export
 export default ({ getService }: FtrProviderContext): void => {
-  const supertest = getService('supertest');
+  const supertestWithoutAuth = getService('supertestWithoutAuth');
   const es = getService('es');
   const authSpace1 = getAuthWithSuperUser();
   const authSpace2 = getAuthWithSuperUser('space2');
@@ -32,26 +32,26 @@ export default ({ getService }: FtrProviderContext): void => {
 
     it('should return all cases with the same alert ID attached to them in space1', async () => {
       const [case1, case2, case3] = await Promise.all([
-        createCase(supertest, getPostCaseRequest(), 200, authSpace1),
-        createCase(supertest, getPostCaseRequest(), 200, authSpace1),
-        createCase(supertest, getPostCaseRequest(), 200, authSpace1),
+        createCase(supertestWithoutAuth, getPostCaseRequest(), 200, authSpace1),
+        createCase(supertestWithoutAuth, getPostCaseRequest(), 200, authSpace1),
+        createCase(supertestWithoutAuth, getPostCaseRequest(), 200, authSpace1),
       ]);
 
       await Promise.all([
         createComment({
-          supertest,
+          supertest: supertestWithoutAuth,
           caseId: case1.id,
           params: postCommentAlertReq,
           auth: authSpace1,
         }),
         createComment({
-          supertest,
+          supertest: supertestWithoutAuth,
           caseId: case2.id,
           params: postCommentAlertReq,
           auth: authSpace1,
         }),
         createComment({
-          supertest,
+          supertest: supertestWithoutAuth,
           caseId: case3.id,
           params: postCommentAlertReq,
           auth: authSpace1,
@@ -59,7 +59,7 @@ export default ({ getService }: FtrProviderContext): void => {
       ]);
 
       const cases = await getCasesByAlert({
-        supertest,
+        supertest: supertestWithoutAuth,
         alertID: 'test-id',
         auth: authSpace1,
       });
@@ -70,26 +70,26 @@ export default ({ getService }: FtrProviderContext): void => {
 
     it('should return 1 case in space2 when 2 cases were created in space1 and 1 in space2', async () => {
       const [case1, case2, case3] = await Promise.all([
-        createCase(supertest, getPostCaseRequest(), 200, authSpace1),
-        createCase(supertest, getPostCaseRequest(), 200, authSpace1),
-        createCase(supertest, getPostCaseRequest(), 200, authSpace2),
+        createCase(supertestWithoutAuth, getPostCaseRequest(), 200, authSpace1),
+        createCase(supertestWithoutAuth, getPostCaseRequest(), 200, authSpace1),
+        createCase(supertestWithoutAuth, getPostCaseRequest(), 200, authSpace2),
       ]);
 
       await Promise.all([
         createComment({
-          supertest,
+          supertest: supertestWithoutAuth,
           caseId: case1.id,
           params: postCommentAlertReq,
           auth: authSpace1,
         }),
         createComment({
-          supertest,
+          supertest: supertestWithoutAuth,
           caseId: case2.id,
           params: postCommentAlertReq,
           auth: authSpace1,
         }),
         createComment({
-          supertest,
+          supertest: supertestWithoutAuth,
           caseId: case3.id,
           params: postCommentAlertReq,
           auth: authSpace2,
@@ -97,7 +97,7 @@ export default ({ getService }: FtrProviderContext): void => {
       ]);
 
       const casesByAlert = await getCasesByAlert({
-        supertest,
+        supertest: supertestWithoutAuth,
         alertID: 'test-id',
         auth: authSpace2,
       });
--- x-pack/test/cases_api_integration/spaces_only/tests/common/cases/get_case.ts
+++ x-pack/test/cases_api_integration/spaces_only/tests/common/cases/get_case.ts
@@ -19,7 +19,7 @@ import {
 
 // eslint-disable-next-line import/no-default-export
 export default ({ getService }: FtrProviderContext): void => {
-  const supertest = getService('supertest');
+  const supertestWithoutAuth = getService('supertestWithoutAuth');
   const es = getService('es');
   const authSpace1 = getAuthWithSuperUser();
 
@@ -29,17 +29,31 @@ export default ({ getService }: FtrProviderContext): void => {
     });
 
     it('should return a case in space1', async () => {
-      const postedCase = await createCase(supertest, getPostCaseRequest(), 200, authSpace1);
-      const theCase = await getCase({ supertest, caseId: postedCase.id, auth: authSpace1 });
+      const postedCase = await createCase(
+        supertestWithoutAuth,
+        getPostCaseRequest(),
+        200,
+        authSpace1
+      );
+      const theCase = await getCase({
+        supertest: supertestWithoutAuth,
+        caseId: postedCase.id,
+        auth: authSpace1,
+      });
 
       const data = removeServerGeneratedPropertiesFromCase(theCase);
       expect(data).to.eql({ ...postCaseResp(), created_by: nullUser });
     });
 
     it('should not return a case in the wrong space', async () => {
-      const postedCase = await createCase(supertest, getPostCaseRequest(), 200, authSpace1);
+      const postedCase = await createCase(
+        supertestWithoutAuth,
+        getPostCaseRequest(),
+        200,
+        authSpace1
+      );
       await getCase({
-        supertest,
+        supertest: supertestWithoutAuth,
         caseId: postedCase.id,
         auth: getAuthWithSuperUser('space2'),
         expectedHttpCode: 404,
