180a1f23c170b718c44c9d5b63cbcd088d117871
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-06-02 15:38:41 +0300

    [Discover] add test

--- test/functional/apps/discover/index.ts
+++ test/functional/apps/discover/index.ts
@@ -56,7 +56,7 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
       loadTestFile(require.resolve('./_date_nested'));
       loadTestFile(require.resolve('./_search_on_page_load'));
       loadTestFile(require.resolve('./_chart_hidden'));
-      loadTestFile(require.resolve('./_context_encoded_url_param'));
+      loadTestFile(require.resolve('./_context_encoded_url_params'));
       loadTestFile(require.resolve('./_data_view_editor'));
     }
   });
