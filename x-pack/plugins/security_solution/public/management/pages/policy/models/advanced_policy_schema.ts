eed841d5af2fed9005a55a0f81cd0c16f8f6ef08
Kevin Logan <kevin.logan@elastic.co>
2022-06-03 15:44:33 -0400

    [Security Solution] Advanced Endpoint Policy otions for 8.3

--- x-pack/plugins/security_solution/public/management/pages/policy/models/advanced_policy_schema.ts
+++ x-pack/plugins/security_solution/public/management/pages/policy/models/advanced_policy_schema.ts
@@ -914,4 +914,26 @@ export const AdvancedPolicySchema: AdvancedPolicySchemaType[] = [
       }
     ),
   },
+  {
+    key: 'windows.advanced.utilization_limits.cpu',
+    first_supported_version: '8.3',
+    documentation: i18n.translate(
+      'xpack.securitySolution.endpoint.policy.advanced.windows.advanced.utilization_limits.cpu',
+      {
+        defaultMessage:
+          'The percentage of the aggregate system CPU to restrict Endpoint to.  Default: 100',
+      }
+    ),
+  },
+  {
+    key: 'linux.advanced.utilization_limits.cpu',
+    first_supported_version: '8.3',
+    documentation: i18n.translate(
+      'xpack.securitySolution.endpoint.policy.advanced.linux.advanced.utilization_limits.cpu',
+      {
+        defaultMessage:
+          'The percentage of the aggregate system CPU to restrict Endpoint to.  Default: 100',
+      }
+    ),
+  },
 ];
