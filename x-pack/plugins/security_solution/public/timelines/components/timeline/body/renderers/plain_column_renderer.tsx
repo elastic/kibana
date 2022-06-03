a39a1e5193913682c4a5b68693fb77994948b867
Jan Monschke <jan.monschke@elastic.co>
2022-06-03 15:33:44 +0200

    chore: reintroduce `parseValue`
    We currently don't know exactly if it's still required or not. Hence we're reintroducing it and will revisit its usage in a future PR.

--- x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/plain_column_renderer.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/plain_column_renderer.tsx
@@ -14,6 +14,7 @@ import { TimelineNonEcsData } from '../../../../../../common/search_strategy/tim
 import { getEmptyTagValue } from '../../../../../common/components/empty_value';
 import { ColumnRenderer } from './column_renderer';
 import { FormattedFieldValue } from './formatted_field';
+import { parseValue } from './parse_value';
 
 export const dataExistsAtColumn = (columnName: string, data: TimelineNonEcsData[]): boolean =>
   data.findIndex((item) => item.field === columnName) !== -1;
@@ -62,7 +63,7 @@ export const plainColumnRenderer: ColumnRenderer = {
           key={`plain-column-renderer-formatted-field-value-${timelineId}-${columnName}-${eventId}-${field.id}-${value}-${i}`}
           linkValue={head(linkValues)}
           truncate={truncate}
-          value={value}
+          value={parseValue(value)}
         />
       ));
     } else {
@@ -92,7 +93,7 @@ export const plainColumnRenderer: ColumnRenderer = {
 function joinValues(values: string[] | undefined | null): string | undefined {
   if (Array.isArray(values)) {
     if (values.length > 0) {
-      return values.join(', ');
+      return values.map(parseValue).join(', ');
     } else {
       return values[0];
     }
