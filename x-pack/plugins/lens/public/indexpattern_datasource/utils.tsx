e6e49e6747bae5d9f91047fe991177ee86a4a610
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-06-03 10:03:39 -0500

    [8.3] [Lens] Fix discover drilldown for non-time field case (#133334) (#133501)
    * [Lens] Fix discover drilldown for non-time field case (#133334)
    
    * fix discover drilldown for non-time field case
    
    * add test
    
    * Update lens_top_nav.tsx
    
    Co-authored-by: Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
    (cherry picked from commit d53b592ee1c0607ab2f2078b9455f6ca221708a5)
    
    * Update indexpattern.tsx
    
    * Update utils.tsx
    
    * Update types.ts
    
    * Update show_underlying_data.ts
    
    * Update indexpattern.tsx
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

--- x-pack/plugins/lens/public/indexpattern_datasource/utils.tsx
+++ x-pack/plugins/lens/public/indexpattern_datasource/utils.tsx
@@ -13,7 +13,7 @@ import { EuiLink, EuiTextColor, EuiButton, EuiSpacer } from '@elastic/eui';
 
 import { DatatableColumn } from '@kbn/expressions-plugin';
 import { groupBy, escape } from 'lodash';
-import { checkColumnForPrecisionError, Query } from '@kbn/data-plugin/common';
+import { checkColumnForPrecisionError, Query, TimeRange } from '@kbn/data-plugin/common';
 import type { FramePublicAPI, StateSetter } from '../types';
 import type { IndexPattern, IndexPatternLayer, IndexPatternPrivateState } from './types';
 import type { ReferenceBasedIndexPatternColumn } from './operations/definitions/column_types';
@@ -27,6 +27,7 @@ import {
   updateDefaultLabels,
   RangeIndexPatternColumn,
   FormulaIndexPatternColumn,
+  DateHistogramIndexPatternColumn,
 } from './operations';
 
 import { getInvalidFieldMessage, isColumnOfType } from './operations/definitions/helpers';
@@ -342,6 +343,21 @@ function extractQueriesFromRanges(column: RangeIndexPatternColumn) {
 }
 
 /**
+ * If the data view doesn't have a default time field, Discover can't use the global time range - construct an equivalent filter instead
+ */
+function extractTimeRangeFromDateHistogram(
+  column: DateHistogramIndexPatternColumn,
+  timeRange: TimeRange
+) {
+  return [
+    {
+      language: 'kuery',
+      query: `${column.sourceField} >= "${timeRange.from}" AND ${column.sourceField} <= "${timeRange.to}"`,
+    },
+  ];
+}
+
+/**
  * Given an Terms/Top values column transform each entry into a "field: term" KQL query
  * This works also for multi-terms variant
  */
@@ -442,14 +458,16 @@ function collectOnlyValidQueries(
 export function getFiltersInLayer(
   layer: IndexPatternLayer,
   columnIds: string[],
-  layerData: NonNullable<FramePublicAPI['activeData']>[string] | undefined
+  layerData: NonNullable<FramePublicAPI['activeData']>[string] | undefined,
+  indexPattern: IndexPattern,
+  timeRange: TimeRange | undefined
 ) {
   const filtersGroupedByState = collectFiltersFromMetrics(layer, columnIds);
   const [enabledFiltersFromMetricsByLanguage, disabledFitleredFromMetricsByLanguage] = (
     ['enabled', 'disabled'] as const
   ).map((state) => groupBy(filtersGroupedByState[state], 'language') as unknown as GroupedQueries);
 
-  const filterOperation = columnIds
+  const filterOperationsOrErrors = columnIds
     .map((colId) => {
       const column = layer.columns[colId];
 
@@ -472,6 +490,28 @@ export function getFiltersInLayer(
       }
 
       if (
+        isColumnOfType<DateHistogramIndexPatternColumn>('date_histogram', column) &&
+        timeRange &&
+        column.sourceField &&
+        !column.params.ignoreTimeRange &&
+        indexPattern.timeFieldName !== column.sourceField
+      ) {
+        if (indexPattern.timeFieldName) {
+          // non-default time field is not supported in Discover if data view has a time field
+          return {
+            error: i18n.translate('xpack.lens.indexPattern.nonDefaultTimeFieldError', {
+              defaultMessage:
+                'Underlying data does not support date histograms on non-default time fields if time field is set on the data view',
+            }),
+          };
+        }
+        // if the data view has no default time field but the date histograms' time field is bound to the time range, create a KQL query for the time range
+        return {
+          kuery: extractTimeRangeFromDateHistogram(column, timeRange),
+        };
+      }
+
+      if (
         isColumnOfType<TermsIndexPatternColumn>('terms', column) &&
         !(column.params.otherBucket || column.params.missingBucket)
       ) {
@@ -490,13 +530,30 @@ export function getFiltersInLayer(
         };
       }
     })
-    .filter(Boolean) as GroupedQueries[];
+    .filter(Boolean);
+
+  const errors = filterOperationsOrErrors.filter((filter) => filter && 'error' in filter) as Array<{
+    error: string;
+  }>;
+
+  if (errors.length) {
+    return {
+      error: errors.map(({ error }) => error).join(', '),
+    };
+  }
+
+  const filterOperations = filterOperationsOrErrors as GroupedQueries[];
+
   return {
     enabled: {
-      kuery: collectOnlyValidQueries(enabledFiltersFromMetricsByLanguage, filterOperation, 'kuery'),
+      kuery: collectOnlyValidQueries(
+        enabledFiltersFromMetricsByLanguage,
+        filterOperations,
+        'kuery'
+      ),
       lucene: collectOnlyValidQueries(
         enabledFiltersFromMetricsByLanguage,
-        filterOperation,
+        filterOperations,
         'lucene'
       ),
     },
