b3017725fb8ad2adfa5862572511b3c6f35439c3
Justin Kambic <jk@elastic.co>
2022-06-03 14:01:31 -0400

    Delete obsolete API test.

--- x-pack/test/apm_api_integration/tests/csm/web_core_vitals.spec.ts
+++ x-pack/test/apm_api_integration/tests/csm/web_core_vitals.spec.ts
@@ -32,47 +32,4 @@ export default function rumServicesApiTests({ getService }: FtrProviderContext) 
       });
     });
   });
-
-  registry.when(
-    'CSM web core vitals with data',
-    { config: 'trial', archives: ['8.0.0', 'rum_8.0.0'] },
-    () => {
-      it('returns web core vitals values', async () => {
-        const response = await supertest.get('/internal/apm/ux/web-core-vitals').query({
-          start: '2020-09-07T20:35:54.654Z',
-          end: '2020-09-16T20:35:54.654Z',
-          uiFilters: '{"serviceName":["kibana-frontend-8_0_0"]}',
-          percentile: 50,
-        });
-
-        expect(response.status).to.be(200);
-
-        expectSnapshot(response.body).toMatchInline(`
-          Object {
-            "cls": 0,
-            "clsRanks": Array [
-              100,
-              0,
-              0,
-            ],
-            "coreVitalPages": 6,
-            "fcp": 817.5,
-            "fid": 1352.13,
-            "fidRanks": Array [
-              0,
-              0,
-              100,
-            ],
-            "lcp": 1019,
-            "lcpRanks": Array [
-              100,
-              0,
-              0,
-            ],
-            "tbt": 0,
-          }
-        `);
-      });
-    }
-  );
 }
