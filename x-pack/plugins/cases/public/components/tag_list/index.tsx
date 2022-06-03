bcf9082f096d4d174b885ef5a7772f22c789b8b7
Esteban Beltran <academo@users.noreply.github.com>
2022-06-03 12:01:46 +0200

    fix edit tags list displaying old values after edit (#133465)

--- x-pack/plugins/cases/public/components/tag_list/index.tsx
+++ x-pack/plugins/cases/public/components/tag_list/index.tsx
@@ -70,8 +70,10 @@ export const TagList = React.memo(
       const { isValid, data: newData } = await submit();
       if (isValid && newData.tags) {
         onSubmit(newData.tags);
+        form.reset({ defaultValue: newData });
         setIsEditTags(false);
       }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [onSubmit, submit]);
 
     const { tags: tagOptions } = useGetTags();
