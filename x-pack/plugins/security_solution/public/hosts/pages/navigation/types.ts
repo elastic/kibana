b0902fd8040b84923381f52bf8effdb98e94cde5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-06-05 03:43:08 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/security_solution/public/hosts/pages/navigation/types.ts
+++ x-pack/plugins/security_solution/public/hosts/pages/navigation/types.ts
@@ -13,7 +13,6 @@ import { GlobalTimeArgs } from '../../../common/containers/use_global_time';
 import { HostsTableType, HostsType } from '../../store/model';
 import { NavTab } from '../../../common/components/navigation/types';
 import { UpdateDateRange } from '../../../common/components/charts/common';
-import { DocValueFields } from '../../../common/containers/source';
 
 export type KeyHostsNavTabWithoutMlPermission = HostsTableType.hosts &
   HostsTableType.authentications &
