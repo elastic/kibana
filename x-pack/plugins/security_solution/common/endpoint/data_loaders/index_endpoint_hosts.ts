3b3920514897b7ec5395131c1be25a71f95fe1b0
Devin Hurley <devin.hurley@elastic.co>
2022-06-07 12:59:19 -0400

    undo changes to endpoint data loader while testing

--- x-pack/plugins/security_solution/common/endpoint/data_loaders/index_endpoint_hosts.ts
+++ x-pack/plugins/security_solution/common/endpoint/data_loaders/index_endpoint_hosts.ts
@@ -124,9 +124,8 @@ export async function indexEndpointHostDocs({
     generator.updateHostData();
     generator.updateHostPolicyData();
 
-    // timestamp - timeBetweenDocs * (numDocs - j - 1),
     hostMetadata = generator.generateHostMetadata(
-      timestamp,
+      timestamp - timeBetweenDocs * (numDocs - j - 1),
       EndpointDocGenerator.createDataStreamFromIndex(metadataIndex)
     );
 
@@ -199,8 +198,7 @@ export async function indexEndpointHostDocs({
       // there is an extra delay and fleet-agents gets populated much sooner.
       // we manually add a delay to the time sync field so that the united transform
       // will pick up the latest metadata doc.
-      // hostMetadata['@timestamp'] + 60000,
-      '@timestamp': hostMetadata['@timestamp'],
+      '@timestamp': hostMetadata['@timestamp'] + 60000,
     };
     await client
       .index({
