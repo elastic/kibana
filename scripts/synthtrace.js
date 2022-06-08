88025d35fc848afc5c8898114b415401180017fd
Søren Louv-Jansen <soren.louv@elastic.co>
2022-06-08 13:41:04 +0200

    Add comment

--- scripts/synthtrace.js
+++ scripts/synthtrace.js
@@ -7,5 +7,10 @@
  */
 
 require('../src/setup_node_env');
+
+// We have to import directly from package since scenarios and worker.js are imported dynamically,
+// If we import the package (require('@elastic/apm-synthtrace')) the program will be executed on the compiled files, and thus we need to
+// compile scenarios with `yarn kbn bootstrap` every time scenario changes.
+
 // eslint-disable-next-line @kbn/imports/uniform_imports
 require('../packages/elastic-apm-synthtrace/src/index').runSynthtrace();
