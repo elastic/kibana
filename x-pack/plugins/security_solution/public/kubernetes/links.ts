1eecf1d08b7766c8c054d537224f3e024bdaf48c
Jack <zizhou.wang@elastic.co>
2022-06-08 11:10:23 -0400

    [8.4] [Security Solution] [Kubernetes Security] Move kubernetes link to dashboard in new security nav (#133411)
    * Move kubernetes link to dashboard in new security nav
    
    * Add kubernetes page image and description

--- x-pack/plugins/security_solution/public/kubernetes/links.ts
+++ x-pack/plugins/security_solution/public/kubernetes/links.ts
@@ -4,15 +4,23 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+import { i18n } from '@kbn/i18n';
 import { KUBERNETES_PATH, SecurityPageName } from '../../common/constants';
 import { KUBERNETES } from '../app/translations';
 import { LinkItem } from '../common/links/types';
+import kubernetesPageImg from '../common/images/kubernetes_page.png';
 
 export const links: LinkItem = {
   id: SecurityPageName.kubernetes,
   title: KUBERNETES,
+  landingImage: kubernetesPageImg,
+  description: i18n.translate('xpack.securitySolution.appLinks.kubernetesDescription', {
+    defaultMessage:
+      'Provides interactive visualizations of your Kubernetes workload and session data.',
+  }),
   path: KUBERNETES_PATH,
-  globalNavEnabled: true,
+  globalNavEnabled: false,
   experimentalKey: 'kubernetesEnabled',
   globalSearchKeywords: ['Kubernetes'],
   globalNavOrder: 9005,
