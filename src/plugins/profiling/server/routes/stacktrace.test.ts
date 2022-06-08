7d402da5e2b6e3a6ddcfb459d27365b8fc54a867
Joseph Crail <jbcrail@gmail.com>
2022-06-08 03:28:24 -0700

    Fix regression when decoding stack trace (#73)
    * Import necessary types
    
    * Reverse decoding of stack frames and file IDs
    
    * Test decoding of stack traces
    
    * Add reverse run length encoder
    
    This was added to make testing easier.

--- src/plugins/profiling/server/routes/stacktrace.test.ts
+++ src/plugins/profiling/server/routes/stacktrace.test.ts
@@ -7,7 +7,34 @@
  */
 
 import { StackTrace } from '../../common/profiling';
-import { decodeStackTrace, EncodedStackTrace, runLengthDecodeReverse } from './stacktrace';
+import {
+  decodeStackTrace,
+  EncodedStackTrace,
+  runLengthDecodeReverse,
+  runLengthEncodeReverse,
+} from './stacktrace';
+
+enum fileID {
+  A = 'aQpJmTLWydNvOapSFZOwKg==',
+  B = 'hz_u-HGyrN6qeIk6UIJeCA==',
+  C = 'AJ8qrcXSoJbl_haPhlc4og==',
+  D = 'lHZiv7a58px6Gumcpo-6yA==',
+  E = 'fkbxUTZgljnk71ZMnqJnyA==',
+  F = 'gnEsgxvvEODj6iFYMQWYlA==',
+}
+
+enum frameID {
+  A = 'aQpJmTLWydNvOapSFZOwKgAAAAAAB924',
+  B = 'hz_u-HGyrN6qeIk6UIJeCAAAAAAAAAZZ',
+  C = 'AJ8qrcXSoJbl_haPhlc4ogAAAAAAAAAH',
+  D = 'lHZiv7a58px6Gumcpo-6yAAAAAAAAAAf',
+  E = 'fkbxUTZgljnk71ZMnqJnyAAAAAAAAABv',
+  F = 'gnEsgxvvEODj6iFYMQWYlAAAAAAGVDgH',
+  G = 'gnEsgxvvEODj6iFYMQWYlAAAAAAGBJv6',
+}
+
+const frameTypeA = [0, 0, 0];
+const frameTypeB = [8, 8, 8, 8];
 
 describe('Stack trace operations', () => {
   test('decodeStackTrace', () => {
@@ -17,30 +44,38 @@ describe('Stack trace operations', () => {
     }> = [
       {
         original: {
-          FrameID: 'aQpJmTLWydNvOapSFZOwKgAAAAAAB924',
-          Type: Buffer.from([0x1, 0x0]).toString('base64url'),
+          FrameID: frameID.A + frameID.B + frameID.C,
+          Type: runLengthEncodeReverse(frameTypeA).toString('base64url'),
         } as EncodedStackTrace,
         expected: {
-          FileID: ['aQpJmTLWydNvOapSFZOwKg=='],
-          FrameID: ['aQpJmTLWydNvOapSFZOwKgAAAAAAB924'],
-          Type: [0],
+          FileID: [fileID.C, fileID.B, fileID.A],
+          FrameID: [frameID.C, frameID.B, frameID.A],
+          Type: frameTypeA,
         } as StackTrace,
       },
       {
         original: {
-          FrameID: 'hz_u-HGyrN6qeIk6UIJeCAAAAAAAAAZZ',
-          Type: Buffer.from([0x1, 0x8]).toString('base64url'),
+          FrameID: frameID.D + frameID.E + frameID.F + frameID.G,
+          Type: runLengthEncodeReverse(frameTypeB).toString('base64url'),
         } as EncodedStackTrace,
         expected: {
-          FileID: ['hz_u-HGyrN6qeIk6UIJeCA=='],
-          FrameID: ['hz_u-HGyrN6qeIk6UIJeCAAAAAAAAAZZ'],
-          Type: [8],
+          FileID: [fileID.F, fileID.F, fileID.E, fileID.D],
+          FrameID: [frameID.G, frameID.F, frameID.E, frameID.D],
+          Type: frameTypeB,
         } as StackTrace,
       },
     ];
 
     for (const t of tests) {
       expect(decodeStackTrace(t.original)).toEqual(t.expected);
+    }
+  });
+
+  test('run length is fully reversible', () => {
+    const tests: number[][] = [[], [0], [0, 1, 2, 3], [0, 1, 1, 2, 2, 2, 3, 3, 3, 3]];
+
+    for (const t of tests) {
+      expect(runLengthDecodeReverse(runLengthEncodeReverse(t))).toEqual(t);
     }
   });
 
