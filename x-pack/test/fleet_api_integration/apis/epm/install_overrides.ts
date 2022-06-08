a9dcea3b0dd8be5fb08cebc9a72746695310dfe8
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-06-08 16:31:57 +0200

    [Fleet] Renable fleet api integration against ES >= 8

--- x-pack/test/fleet_api_integration/apis/epm/install_overrides.ts
+++ x-pack/test/fleet_api_integration/apis/epm/install_overrides.ts
@@ -113,6 +113,9 @@ export default function (providerContext: FtrProviderContext) {
         },
       }));
 
+      // omit routings
+      delete body.template.settings.index.routing;
+
       expect(body).to.eql({
         template: {
           settings: {
