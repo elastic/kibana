6d4bd09135608aac5ce561ca9acb4c48b63884ce
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2022-06-08 16:35:16 -0400

    formatting

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_table.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_table.tsx
@@ -174,7 +174,11 @@ const AlertsTable: React.FunctionComponent<AlertsTableProps> = (props: AlertsTab
   );
 
   return (
-    <section className='alertsTableResponseOps' style={{ width: '100%' }} data-test-subj={props['data-test-subj']}>
+    <section
+      className="alertsTableResponseOps"
+      style={{ width: '100%' }}
+      data-test-subj={props['data-test-subj']}
+    >
       <Suspense fallback={null}>
         {flyoutAlertIndex > -1 && (
           <AlertsFlyout
