7a6c17e1defafb266fb2ae6be596fd28e6b2ef63
Pete Hampton <pjhampton@users.noreply.github.com>
2022-06-08 16:18:36 +0100

    Add a default request timeout for Axios telemetry connections. (#133615)
    * Add a default request timeout for axios telemetry connections.
    
    * Update synthetics test.
    
    * Update telemetry tests to include new timeout config.
    
    * Skip test that is erroring out.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/server/lib/telemetry/sender.ts
+++ x-pack/plugins/security_solution/server/lib/telemetry/sender.ts
@@ -304,6 +304,7 @@ export class TelemetryEventsSender implements ITelemetryEventsSender {
           'X-Elastic-Stack-Version': clusterVersionNumber ? clusterVersionNumber : '8.0.0',
           ...(licenseId ? { 'X-Elastic-License-ID': licenseId } : {}),
         },
+        timeout: 5000,
       });
       this.telemetryUsageCounter?.incrementCounter({
         counterName: createUsageCounterLabel(usageLabelPrefix.concat(['payloads', channel])),
