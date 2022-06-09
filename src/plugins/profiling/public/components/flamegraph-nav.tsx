37747753e9e484f8cabc36dd839453757257eaae
Joseph Crail <jbcrail@gmail.com>
2022-06-09 08:35:23 -0700

    Bump elastic-charts to 46.9.0 (#74)
    * Bump elastic-charts to 46.9.0
    
    * Add useEffect dependencies
    
    This ensures logging only happens if state changes.
    
    * Clean up log messages

--- src/plugins/profiling/public/components/flamegraph-nav.tsx
+++ src/plugins/profiling/public/components/flamegraph-nav.tsx
@@ -10,10 +10,10 @@ import React, { useEffect } from 'react';
 
 export const FlameGraphNavigation = ({ index, projectID, n, timeRange, getter, setter }) => {
   useEffect(() => {
-    console.log(new Date().toISOString(), timeRange);
     console.log(new Date().toISOString(), 'started payload retrieval');
     getter(index, projectID, timeRange.unixStart, timeRange.unixEnd, n).then((response) => {
       console.log(new Date().toISOString(), 'finished payload retrieval');
+      console.log(new Date().toISOString(), response);
       setter(response);
       console.log(new Date().toISOString(), 'updated local state');
     });
