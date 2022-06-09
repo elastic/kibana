305f716f88379b5aa10c5bcbdf9fd86a23ab93af
Patryk Kopycinski <contact@patrykkopycinski.com>
2022-06-09 17:45:33 +0200

    test

--- test/scripts/jenkins_security_solution_cypress_chrome.sh
+++ test/scripts/jenkins_security_solution_cypress_chrome.sh
@@ -3,9 +3,10 @@
 source test/scripts/jenkins_test_setup_xpack.sh
 
 echo " -> Running security solution cypress tests"
+cd "$XPACK_DIR"
 
 checks-reporter-with-killswitch "Security Solution Cypress Tests (Chrome)" \
   node scripts/functional_tests \
     --debug --bail \
     --kibana-install-dir "$KIBANA_INSTALL_DIR" \
-    --config x-pack/test/security_solution_cypress/cli_config.ts
+    --config test/security_solution_cypress/cli_config.ts
