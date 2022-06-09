0b6a03355b410e75a50cb15a38276e5b846c8d3d
Patryk Kopyciński <contact@patrykkopycinski.com>
2022-06-09 19:57:02 +0200

    [8.3] [Osquery] Fix ECS editor field array value (#132786) (#134054)
    * [Osquery] Fix ECS editor field array value (#132786)
    
    (cherry picked from commit 272d9be67047ba20d5d03ba1725409c8d9b488e7)
    
    * revert

--- x-pack/plugins/osquery/cypress/integration/all/add_integration.spec.ts
+++ x-pack/plugins/osquery/cypress/integration/all/add_integration.spec.ts
@@ -79,7 +79,7 @@ describe('ALL - Add Integration', () => {
   it.skip('should have integration and packs copied when upgrading integration', () => {
     const packageName = 'osquery_manager';
     const oldVersion = '1.2.0';
-    const newVersion = '1.3.0';
+    const newVersion = '1.3.1';
 
     cy.visit(`app/integrations/detail/${packageName}-${oldVersion}/overview`);
     cy.contains('Add Osquery Manager').click();
