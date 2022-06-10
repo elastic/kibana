ccaf494c95b3086e1fb7f0d55ccb95de79a65805
Jonathan Budzenski <jon@elastic.co>
2022-06-10 08:12:39 -0500

    [artifacts] fix download, partial revert of #134046

--- .buildkite/scripts/steps/artifacts/docker_context.sh
+++ .buildkite/scripts/steps/artifacts/docker_context.sh
@@ -26,5 +26,7 @@ fi
 tar -xf "target/$DOCKER_CONTEXT_FILE" -C "$DOCKER_BUILD_FOLDER"
 cd $DOCKER_BUILD_FOLDER
 
+buildkite-agent artifact download "kibana-$FULL_VERSION-linux-x86_64.tar.gz" . --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
+
 echo "--- Build context"
 docker build .
