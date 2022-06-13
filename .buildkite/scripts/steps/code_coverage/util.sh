d55b93961c8e925e696771893caddf7a2559aae4
Tre <wayne.seymour@elastic.co>
2022-06-13 15:40:59 +0100

    [QA][Code Coverage] Modularize Code Coverage (#133759)
    Kinda sneaky...since we already mutate the jest portion of the
    file system (target/kibana-coverage/jest)
    by dumping "jest unit" & "jest integration" coverage
    into the same "final" directory...go ahead an make "jest integration"
    use the same ran file designator as "jest unit".
    This saves me from having to add logic for this later on.

--- .buildkite/scripts/steps/code_coverage/util.sh
+++ .buildkite/scripts/steps/code_coverage/util.sh
@@ -2,15 +2,27 @@
 
 set -euo pipefail
 
+header() {
+  local fileName=$1
+
+  echo "" >"$fileName"
+
+  echo "### File Name:" >>"$fileName"
+  printf "  %s\n\n" "$fileName" >>"$fileName"
+}
+
 # $1 file name, ex: "target/dir-listing-jest.txt"
 # $2 directory to be listed, ex: target/kibana-coverage/jest
 dirListing() {
   local fileName=$1
   local dir=$2
 
-  ls -l "$dir" >"$fileName"
+  header "$fileName"
+
+  ls -l "$dir" >>"$fileName"
 
   printf "\n### %s \n\tlisted to: %s\n" "$dir" "$fileName"
+
   buildkite-agent artifact upload "$fileName"
 
   printf "\n### %s Uploaded\n" "$fileName"
@@ -27,15 +39,6 @@ replacePaths() {
       "$search" \
       "$replace"
   done
-}
-
-header() {
-  local fileName=$1
-
-  echo "" >"$fileName"
-
-  echo "### File Name:" >>"$fileName"
-  printf "\t%s\n" "$fileName" >>"$fileName"
 }
 
 fileHeads() {
