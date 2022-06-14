1b1543cf12740fabdb109e689180c5d37c7ff2bb
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-06-14 15:03:29 -0400

    Fix linting

--- x-pack/plugins/fleet/server/services/epm/elasticsearch/ingest_pipeline/install.test.ts
+++ x-pack/plugins/fleet/server/services/epm/elasticsearch/ingest_pipeline/install.test.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
+// eslint-disable-next-line @kbn/eslint/no-restricted-paths
 import { elasticsearchClientMock } from '@kbn/core/server/elasticsearch/client/mocks';
 import { loggerMock } from '@kbn/logging-mocks';
 import { getArchiveEntry } from '../../archive/cache';
