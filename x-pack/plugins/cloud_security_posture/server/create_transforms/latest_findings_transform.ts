60b184d5fd95b2b01791e5b3e81c2d2abee391f9
Ido Cohen <ido.cohen@elastic.co>
2022-06-14 11:07:32 +0300

    update latest transform key

--- x-pack/plugins/cloud_security_posture/server/create_transforms/latest_findings_transform.ts
+++ x-pack/plugins/cloud_security_posture/server/create_transforms/latest_findings_transform.ts
@@ -31,7 +31,7 @@ export const latestFindingsTransform: TransformPutTransformRequest = {
   },
   latest: {
     sort: '@timestamp',
-    unique_key: ['resource_id.keyword', 'rule.id.keyword'],
+    unique_key: ['resource.id.keyword', 'rule.id'],
   },
   _meta: {
     managed: 'true',
