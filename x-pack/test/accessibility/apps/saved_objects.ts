49381564300a48d48a94dad8c0597bccb815dde0
Tre' Seymour <wayne.seymour@elastic.co>
2022-06-14 10:45:48 +0100

    Revert the suite to how it was and add a couple of lambdas for clicking. Also, only run this one test (not sure if ci still allows this).

--- x-pack/test/accessibility/apps/saved_objects.ts
+++ x-pack/test/accessibility/apps/saved_objects.ts
@@ -18,7 +18,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const find = getService('find');
   const browser = getService('browser');
 
-  describe('Kibana saved objects management page a11y tests', () => {
+  describe.only('Kibana saved objects management page a11y tests', () => {
     before(async () => {
       await PageObjects.common.navigateToUrl('home', '/tutorial_directory/sampleData', {
         useActualUrl: true,
@@ -84,72 +84,80 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await a11y.testAppSnapshot();
     });
 
-    describe(`second pass`, () => {
-      before(async () => {
-        await PageObjects.common.navigateToUrl('home', '/tutorial_directory/sampleData', {
-          useActualUrl: true,
-        });
-        await PageObjects.header.waitUntilLoadingHasFinished();
-        await PageObjects.settings.navigateTo();
-        await PageObjects.settings.clickKibanaSavedObjects();
-      })
-      it('share to space panel from actions meets a11y requirements ', async () => {
-        await testSubjects.click('euiCollapsedItemActionsButton');
-        await find.byCssSelector('.euiContextMenuPanel');
-        await testSubjects.click('savedObjectsTableAction-share_saved_objects_to_space');
-        await retry.try(async () => {
-          await a11y.testAppSnapshot();
-        });
-        // creating a new space from this panel
-        await testSubjects.click('sts-new-space-link');
-        await PageObjects.spaceSelector.clickEnterSpaceName();
-        await PageObjects.spaceSelector.addSpaceName('space_a');
-        await PageObjects.spaceSelector.clickSaveSpaceCreation();
-        await PageObjects.settings.navigateTo();
-        await PageObjects.settings.clickKibanaSavedObjects();
-        await (await find.byCssSelector('[title="Type"]')).click();
-        await (await find.byCssSelector('[title="data view"]')).click();
-        await browser.pressKeys(browser.keys.ESCAPE);
-        await find.byCssSelector('.euiContextMenuPanel');
-        await retry.waitFor(
-          'actions button visible',
-          async () => await testSubjects.exists('euiCollapsedItemActionsButton')
-        );
-        await testSubjects.click('euiCollapsedItemActionsButton');
-        await testSubjects.click('savedObjectsTableAction-share_saved_objects_to_space');
-        await (await find.byCssSelector('[title="All spaces"]')).click();
-        await testSubjects.click('sts-save-button');
-        await retry.waitFor('toasts.toBeVisible', async () => (await toasts.getToastCount()) > 0);
-        await toasts.dismissAllToasts();
-      });
-
-      it('copy to space panel from actions menu meets a11y requirements ', async () => {
-        await find.byCssSelector('.euiContextMenuPanel');
-        await retry.waitFor(
-          'actions button visible',
-          async () => await testSubjects.exists('euiCollapsedItemActionsButton')
-        );
-        await testSubjects.click('euiCollapsedItemActionsButton');
-        await PageObjects.common.sleep(1000);
-        await find.byCssSelector('.euiContextMenuPanel');
-        await testSubjects.click('euiCollapsedItemActionsButton');
-        await testSubjects.click('savedObjectsTableAction-copy_saved_objects_to_space');
+    const clickIfExists = async (x: string) => {
+      const isShown = await testSubjects.exists(x);
+      if (isShown) {
+        try {
+          await testSubjects.click(x);
+        } catch (err) {
+          // noop
+        }
+      }
+    }
+    const clickOneOf = async (x: string) => {
+      const xs = await testSubjects.findAll(x)
+      await xs[2].click()
+    }
+    it.only('share to space panel from actions meets a11y requirements ', async () => {
+      await clickIfExists('euiFlyoutCloseButton')
+      await clickOneOf('euiCollapsedItemActionsButton')
+      await testSubjects.click('savedObjectsTableAction-copy_saved_objects_to_space');
+      await retry.try(async () => {
         await a11y.testAppSnapshot();
       });
+      // creating a new space from this panel
+      await testSubjects.click('sts-new-space-link');
+      await PageObjects.spaceSelector.clickEnterSpaceName();
+      await PageObjects.spaceSelector.addSpaceName('space_a');
+      await PageObjects.spaceSelector.clickSaveSpaceCreation();
+      await PageObjects.settings.navigateTo();
+      await PageObjects.settings.clickKibanaSavedObjects();
+      await (await find.byCssSelector('[title="Type"]')).click();
+      await (await find.byCssSelector('[title="data view"]')).click();
+      await browser.pressKeys(browser.keys.ESCAPE);
+      await find.byCssSelector('.euiContextMenuPanel');
+      await retry.waitFor(
+        'actions button visible',
+        async () => await testSubjects.exists('euiCollapsedItemActionsButton')
+      );
+      await testSubjects.click('euiCollapsedItemActionsButton');
+      await testSubjects.click('savedObjectsTableAction-share_saved_objects_to_space');
+      await (await find.byCssSelector('[title="All spaces"]')).click();
+      await testSubjects.click('sts-save-button');
+      await retry.waitFor('toasts.toBeVisible', async () => (await toasts.getToastCount()) > 0);
+      await toasts.dismissAllToasts();
+    });
 
-      // inspecting a different saved object than data views because the inspect screen is different
-      it('inspect action from actions menu meets a11y requirements', async () => {
-        await testSubjects.click('cts-cancel-button');
-        await (await find.byCssSelector('[title="Type"]')).click();
-        await (await find.byCssSelector('[title="data view"]')).click();
-        await (await find.byCssSelector('[title="config"]')).click();
-        await browser.pressKeys(browser.keys.ESCAPE);
-        await find.byCssSelector('.euiContextMenuPanel');
-        await testSubjects.click('euiCollapsedItemActionsButton');
-        await testSubjects.click('savedObjectsTableAction-inspect');
-        await retry.try(async () => {
-          await a11y.testAppSnapshot();
-        });
+    it('copy to space panel from actions menu meets a11y requirements ', async () => {
+      await find.byCssSelector('.euiContextMenuPanel');
+      await retry.waitFor(
+        'actions button visible',
+        async () => await testSubjects.exists('euiCollapsedItemActionsButton')
+      );
+      await testSubjects.click('euiCollapsedItemActionsButton');
+      await PageObjects.common.sleep(1000);
+      await find.byCssSelector('.euiContextMenuPanel');
+      await testSubjects.click('euiCollapsedItemActionsButton');
+      await testSubjects.click('savedObjectsTableAction-copy_saved_objects_to_space');
+      await a11y.testAppSnapshot();
+    });
+
+
+
+
+    // inspecting a different saved object than data views because the inspect screen is different
+
+    it('inspect action from actions menu meets a11y requirements', async () => {
+      await testSubjects.click('cts-cancel-button');
+      await (await find.byCssSelector('[title="Type"]')).click();
+      await (await find.byCssSelector('[title="data view"]')).click();
+      await (await find.byCssSelector('[title="config"]')).click();
+      await browser.pressKeys(browser.keys.ESCAPE);
+      await find.byCssSelector('.euiContextMenuPanel');
+      await testSubjects.click('euiCollapsedItemActionsButton');
+      await testSubjects.click('savedObjectsTableAction-inspect');
+      await retry.try(async () => {
+        await a11y.testAppSnapshot();
       });
     });
   });
