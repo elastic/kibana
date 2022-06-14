383d12e15ed9c2ab7427589263ac0cf654512007
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-06-14 08:53:52 -0400

    Fix types

--- x-pack/plugins/fleet/server/services/preconfiguration/reset_agent_policies.test.ts
+++ x-pack/plugins/fleet/server/services/preconfiguration/reset_agent_policies.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { elasticsearchServiceMock, savedObjectsClientMock } from '@kbn/core/server/mocks';
+import { elasticsearchServiceMock, savedObjectsClientMock } from 'src/core/server/mocks';
 
 import { agentPolicyService } from '../agent_policy';
 import { packagePolicyService } from '../package_policy';
