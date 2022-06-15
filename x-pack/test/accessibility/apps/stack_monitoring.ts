020cf7cbe1a2adefb12eb15f75c83de0139c4f2c
Rashmi Kulkarni <rashmi.kulkarni@elastic.co>
2022-06-15 11:37:57 -0700

    a11y test for stack monitoring (#134140)
    * a11y test for stack monitoring
    
    * monitoring tests a11y
    
    * removed unused constants
    
    * addressed review comments
    
    * review changes
    
    * changed the description of a test
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/test/accessibility/apps/stack_monitoring.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+// a11y tests for spaces, space selection and space creation and feature controls
+
+import { FtrProviderContext } from '../ftr_provider_context';
+
+export default function ({ getService, getPageObjects }: FtrProviderContext) {
+  const PageObjects = getPageObjects(['common', 'spaceSelector', 'home', 'header', 'security']);
+  const a11y = getService('a11y');
+  const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+  const noData = getService('monitoringNoData');
+  const kibanaOverview = getService('monitoringKibanaOverview');
+  const clusterOverview = getService('monitoringClusterOverview');
+
+  describe('Kibana Stack Monitoring a11y tests', () => {
+    before(async () => {
+      await PageObjects.common.navigateToApp('monitoring');
+      await a11y.testAppSnapshot();
+    });
+
+    it('a11y tests for no monitoring data found page', async () => {
+      await noData.isOnNoDataPage();
+      await a11y.testAppSnapshot();
+    });
+
+    it('a11y tests for self monitoring home page', async function () {
+      await noData.enableMonitoring();
+      await a11y.testAppSnapshot();
+      await retry.waitForWithTimeout('alert button to be visible', 30000, async () => {
+        return await testSubjects.isDisplayed('alerts-modal-remind-later-button');
+      });
+    });
+
+    // a11y violation caught here - if this rest is unskipped remove the test below it.
+    // https://github.com/elastic/kibana/issues/134139
+    it.skip('a11y tests for Alerts modal remind later button', async function () {
+      await testSubjects.click('alerts-modal-remind-later-button');
+      await a11y.testAppSnapshot();
+    });
+
+    it('Alerts Page', async function () {
+      await testSubjects.click('alerts-modal-remind-later-button');
+    });
+
+    it('a11y tests for Kibana Overview', async function () {
+      await clusterOverview.clickKibanaOverview();
+      await kibanaOverview.isOnOverview();
+      await a11y.testAppSnapshot();
+    });
+
+    it('a11y tests for Kibana Instances Page', async function () {
+      await kibanaOverview.isOnOverview();
+      await kibanaOverview.clickInstanceTab();
+      await a11y.testAppSnapshot();
+    });
+  });
+}
