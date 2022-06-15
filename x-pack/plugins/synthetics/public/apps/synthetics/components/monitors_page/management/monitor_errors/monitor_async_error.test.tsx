9639d5b37720e016ee2101492c4abf58a4647e89
Abdul Wahab Zahid <awahab07@yahoo.com>
2022-06-15 21:37:18 +0200

    [Synthetics App] Fix monitor list table pagination. (#134282)
    * Fix monitor list table pagination.
    
    * Use absoluteTotal to denote total number of saved objects.

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/management/monitor_errors/monitor_async_error.test.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/management/monitor_errors/monitor_async_error.test.tsx
@@ -52,7 +52,7 @@ describe('<MonitorAsyncError />', () => {
       error: null,
       loading: true,
       data: {
-        allTotal: 6,
+        absoluteTotal: 6,
         perPage: 5,
         page: 1,
         total: 6,
