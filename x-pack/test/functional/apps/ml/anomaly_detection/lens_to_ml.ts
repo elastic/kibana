055ae3c65ff0a2751d5dcf1f198534d18d40eacb
James Gowdy <jgowdy@elastic.co>
2022-06-16 09:02:13 +0100

    test commit

--- x-pack/test/functional/apps/ml/anomaly_detection/lens_to_ml.ts
+++ x-pack/test/functional/apps/ml/anomaly_detection/lens_to_ml.ts
@@ -28,6 +28,7 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 //     },
 //   },
 // ];
+//
 
 export default function ({ getService, getPageObject, getPageObjects }: FtrProviderContext) {
   // const esArchiver = getService('esArchiver');
