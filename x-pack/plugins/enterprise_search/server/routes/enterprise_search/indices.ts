878ad3a637df61c7e692947eef5a8c3f81bf0374
James Rucker <james.rucker@elastic.co>
2022-06-16 13:41:14 -0700

    Refactor existing indices route to add a second more general option

--- x-pack/plugins/enterprise_search/server/routes/enterprise_search/indices.ts
+++ x-pack/plugins/enterprise_search/server/routes/enterprise_search/indices.ts
@@ -5,16 +5,18 @@
  * 2.0.
  */
 
+import { schema } from '@kbn/config-schema';
+
 import { fetchIndices } from '../../lib/fetch_indices';
 import { RouteDependencies } from '../../plugin';
 
-export function registerListRoute({ router }: RouteDependencies) {
+export function registerIndexRoutes({ router }: RouteDependencies) {
   router.get(
     { path: '/internal/enterprise_search/search_indices', validate: false },
     async (context, _, response) => {
       const { client } = (await context.core).elasticsearch;
       try {
-        const indices = await fetchIndices(client);
+        const indices = await fetchIndices(client, 'search-*', /^search-.*/);
         return response.ok({
           body: indices,
           headers: { 'content-type': 'application/json' },
@@ -23,6 +25,47 @@ export function registerListRoute({ router }: RouteDependencies) {
         return response.customError({
           statusCode: 502,
           body: 'Error fetching data from Enterprise Search',
+        });
+      }
+    }
+  );
+  router.get(
+    {
+      path: '/internal/enterprise_search/indices',
+      validate: {
+        query: schema.object({
+          page: schema.number({ defaultValue: 1, min: 0 }),
+          size: schema.number({ defaultValue: 10, min: 0 }),
+        }),
+      },
+    },
+    async (context, request, response) => {
+      const { page, size } = request.query;
+      const { client } = (await context.core).elasticsearch;
+      try {
+        const indices = await fetchIndices(client, '*', /.*/);
+        const totalResults = indices.length;
+        const totalPages = Math.ceil(totalResults / size);
+        const startIndex = (page - 1) * size;
+        const endIndex = page * size;
+        return response.ok({
+          body: {
+            meta: {
+              page: {
+                current: page,
+                size,
+                totalPages,
+                totalResults,
+              },
+            },
+            indices: indices.slice(startIndex, endIndex),
+          },
+          headers: { 'content-type': 'application/json' },
+        });
+      } catch (error) {
+        return response.customError({
+          statusCode: 502,
+          body: 'Error fetching index data from Elasticsearch',
         });
       }
     }
