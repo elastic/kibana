b2a1f957da6131eb736d1b41d15c0328563ae3f7
Julia Rechkunova <julia.rechkunova@elastic.co>
2022-06-17 17:07:22 +0200

    [Discover] Make footer visible under Document Explorer if sample size is less than hits number (#134231)
    * [Discover] Make footer visible under Document Explorer if sample size is less than hits number
    
    * [Discover] Simplify changes diff
    
    * [Discover] Update copy
    
    * [Discover] Update copy id
    
    * [Discover] Remove other translations
    
    * [Discover] Add link to Advanced settings and tests
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 28915d29224b6b0dfe10100d8b05a0409a4cded1)
    
    # Conflicts:
    #	test/functional/apps/discover/index.ts

--- test/functional/apps/discover/index.ts
+++ test/functional/apps/discover/index.ts
@@ -52,6 +52,7 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
       loadTestFile(require.resolve('./_data_grid_row_navigation'));
       loadTestFile(require.resolve('./_data_grid_doc_table'));
       loadTestFile(require.resolve('./_indexpattern_with_unmapped_fields'));
+      loadTestFile(require.resolve('./_data_grid_pagination'));
       loadTestFile(require.resolve('./_runtime_fields_editor'));
       loadTestFile(require.resolve('./_huge_fields'));
       loadTestFile(require.resolve('./_date_nested'));
