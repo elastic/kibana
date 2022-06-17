993e3759076e23fedae65dc09d3fbceb9e394488
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-06-17 12:27:39 +0200

    Use query in getServiceDestinationMetricsCount()

--- x-pack/plugins/apm/server/lib/helpers/spans/get_is_using_service_destination_metrics.ts
+++ x-pack/plugins/apm/server/lib/helpers/spans/get_is_using_service_destination_metrics.ts
@@ -86,6 +86,7 @@ export async function getIsUsingServiceDestinationMetrics({
                 ...rangeQuery(start, end),
                 ...kqlQuery(kuery),
                 ...getDocumentTypeFilterForServiceDestinationStatistics(true),
+                ...(query ? [query] : []),
               ],
             },
           },
