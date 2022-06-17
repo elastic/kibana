c3c6b5ac86f5453ec96339c19129372c9efc0860
Tiago Costa <tiago.costa@elastic.co>
2022-06-17 16:03:30 +0100

    skip flaky suite (#132399)

--- x-pack/test/functional/apps/ml/data_frame_analytics/results_view_content.ts
+++ x-pack/test/functional/apps/ml/data_frame_analytics/results_view_content.ts
@@ -268,7 +268,8 @@ export default function ({ getService }: FtrProviderContext) {
     });
 
     for (const testData of testDataList) {
-      describe(`${testData.suiteTitle}`, function () {
+      // FLAKY: https://github.com/elastic/kibana/issues/132399
+      describe.skip(`${testData.suiteTitle}`, function () {
         before(async () => {
           await ml.navigation.navigateToMl();
           await ml.navigation.navigateToDataFrameAnalytics();
