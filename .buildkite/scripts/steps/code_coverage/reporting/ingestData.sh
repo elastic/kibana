e05ffdfe6e156c82d8e6ba748e90b1bc8c4d56e1
Tre' Seymour <wayne.seymour@elastic.co>
2022-06-20 11:10:24 +0100

    [QA][Code Coverage] Add a link to discover pinned to the current build.
    When personnel other than myself wish to visually verify code coverage,
    this link will be available in the ci output.

--- .buildkite/scripts/steps/code_coverage/reporting/ingestData.sh
+++ .buildkite/scripts/steps/code_coverage/reporting/ingestData.sh
@@ -49,4 +49,11 @@ ingestModular() {
 
   echo "---  Ingesting Code Coverage - Complete"
   echo ""
+
+  currentBuildNumber="$BUILDKITE_BUILD_NUMBER"
+  url="https://kibana-stats.elastic.dev/app/discover#/?_g=(filters:!(),query:(language:kuery,query:''),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(columns:!(),filters:!(),hideChart:!f,index:'64419790-4218-11ea-b2d8-81bcbf78dfcb',interval:auto,query:(language:kuery,query:'BUILD_ID%20:%20${currentBuildNumber}'),sort:!(!('@timestamp',desc)))"
+
+  echo "--- Browse the following url to visually verify in Kibana (Disover):"
+  printf "%s \n" "'$url'"
+
 }
