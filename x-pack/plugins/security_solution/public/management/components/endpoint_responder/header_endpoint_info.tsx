375ce6a7574e7e62b41eba1a0f4d428f0c569ee5
Candace Park <candace.park@elastic.co>
2022-06-20 00:05:39 -0400

    add loading lines, remove eui health, add truncation

--- x-pack/plugins/security_solution/public/management/components/endpoint_responder/header_endpoint_info.tsx
+++ x-pack/plugins/security_solution/public/management/components/endpoint_responder/header_endpoint_info.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { memo, useMemo } from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiHealth, EuiText } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiText, EuiLoadingContent } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
 import { useGetEndpointDetails } from '../../hooks/endpoint/use_get_endpoint_details';
@@ -14,7 +14,6 @@ import { useGetEndpointPendingActionsSummary } from '../../hooks/endpoint/use_ge
 import { EndpointHostIsolationStatusProps } from '../../../common/components/endpoint/host_isolation';
 import { EndpointAgentAndIsolationStatus } from '../endpoint_agent_and_isolation_status';
 import { FormattedDate } from '../../../common/components/formatted_date';
-import { HOST_STATUS_TO_HEALTH_COLOR } from '../../pages/endpoint_hosts/view/host_constants';
 
 interface HeaderEndpointInfoProps {
   endpointId: string;
@@ -47,61 +46,55 @@ export const HeaderEndpointInfo = memo<HeaderEndpointInfoProps>(({ endpointId })
     return null;
   }
 
+  if (isFetching) {
+    return <EuiLoadingContent lines={2} />;
+  }
+
   return (
-    <EuiFlexGroup gutterSize="none" alignItems="flexStart">
+    <EuiFlexGroup direction="column" gutterSize="none">
       <EuiFlexItem grow={false}>
-        <EuiHealth
-          color={HOST_STATUS_TO_HEALTH_COLOR[endpointDetails.host_status]}
-          data-test-subj="responderHeaderEndpointHealth"
-        />
-      </EuiFlexItem>
-      <EuiFlexItem grow={false}>
-        <EuiFlexGroup direction="column" gutterSize="none">
-          <EuiFlexItem grow={false}>
-            <EuiFlexGroup alignItems="center" gutterSize="xs" wrap>
-              <EuiFlexItem grow={false}>
-                <EuiText size="s" data-test-subj="responderHeaderEndpointName">
-                  <h6 className="eui-textTruncate">
-                    <FormattedMessage
-                      id="xpack.securitySolution.responder.header.endpointName"
-                      defaultMessage="ENDPOINT {name}"
-                      values={{ name: endpointDetails.metadata.host.name }}
-                    />
-                  </h6>
-                </EuiText>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EndpointAgentAndIsolationStatus
-                  status={endpointDetails.host_status}
-                  isIsolated={endpointDetails.metadata.Endpoint.state?.isolation}
-                  {...pendingIsolationActions}
-                  data-test-subj="responderHeaderEndpointAgentIsolationStatus"
+        <EuiFlexGroup alignItems="center" gutterSize="xs">
+          <EuiFlexItem grow={false} className="eui-textTruncate">
+            <EuiText size="s" data-test-subj="responderHeaderEndpointName">
+              <h6 className="eui-textTruncate">
+                <FormattedMessage
+                  id="xpack.securitySolution.responder.header.endpointName"
+                  defaultMessage="ENDPOINT {name}"
+                  values={{ name: endpointDetails.metadata.host.name }}
                 />
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            <EuiText color="subdued" size="s" data-test-subj="responderHeaderLastSeen">
-              <FormattedMessage
-                id="xpack.securitySolution.responder.header.lastSeen"
-                defaultMessage="Last seen {date}"
-                values={{
-                  date: (
-                    <FormattedDate
-                      fieldName={i18n.translate(
-                        'xpack.securitySolution.responder.header.lastSeen.date',
-                        {
-                          defaultMessage: 'Last seen',
-                        }
-                      )}
-                      value={endpointDetails.metadata['@timestamp']}
-                    />
-                  ),
-                }}
-              />
+              </h6>
             </EuiText>
           </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EndpointAgentAndIsolationStatus
+              status={endpointDetails.host_status}
+              isIsolated={endpointDetails.metadata.Endpoint.state?.isolation}
+              {...pendingIsolationActions}
+              data-test-subj="responderHeaderEndpointAgentIsolationStatus"
+            />
+          </EuiFlexItem>
         </EuiFlexGroup>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiText color="subdued" size="s" data-test-subj="responderHeaderLastSeen">
+          <FormattedMessage
+            id="xpack.securitySolution.responder.header.lastSeen"
+            defaultMessage="Last seen {date}"
+            values={{
+              date: (
+                <FormattedDate
+                  fieldName={i18n.translate(
+                    'xpack.securitySolution.responder.header.lastSeen.date',
+                    {
+                      defaultMessage: 'Last seen',
+                    }
+                  )}
+                  value={endpointDetails.metadata['@timestamp']}
+                />
+              ),
+            }}
+          />
+        </EuiText>
       </EuiFlexItem>
     </EuiFlexGroup>
   );
