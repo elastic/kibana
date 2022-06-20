e2b47ff9040db861c923f9b99799f919e8d518a7
Garrett Spong <spong@users.noreply.github.com>
2022-06-20 04:39:03 -0600

    [Security Solution][Detections] Updates Get Installed Integrations route to use Internal SO Client (#134373)
    ## Summary
    
    Follow up to https://github.com/elastic/kibana/pull/134299 that updates the `Get Installed Integrations` route to use the Internal SO Client and removes client-side privilege checks. This change ensures the `Related Integrations` feature works regardless of client users Fleet/Integration/SOM privileges, enabling all Security Solution users the ability to see which integrations are installed/configured with relation to their Detection Rules.
    
    This change is helpful for low privilege users like T1/T2 Analysts that may not have more broad privileges like SOM or Fleet to still see which of their Detection Rules have the necessary integrations.
    
    ### Test instructions
    
    To test, configure a role with the 3 mentioned privileges (SOM/Integrations/Fleet) as `None`, e.g.
    
    <p align="center">
      <img width="500" src="https://user-images.githubusercontent.com/2946766/173156872-dfaece7e-a6ef-4774-b01d-e2fa7b66a068.png" />
    </p>
    
    
    Then the UI should _still show_ installed details (installed/not installed badges):
    
    ##### Rules Table
    <p align="center">
      <img width="500" src="https://user-images.githubusercontent.com/2946766/173658501-9b384ce6-ddef-4643-a36e-3da3082c8972.png" />
    </p>
    
    
    ##### Rules Details
    <p align="center">
      <img width="500" src="https://user-images.githubusercontent.com/2946766/173658533-a60b6e96-7283-441a-8faa-ed186476c1ac.png" />
    </p>
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [ ] [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html) was added for features that require explanation or tutorials
      * Working docs as part of https://github.com/elastic/security-docs/issues/2015
    - [X] [Unit or functional tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html) were updated or added to match the most common scenarios
    
    ### Risk Ma

--- x-pack/plugins/security_solution/public/detections/components/rules/related_integrations/use_related_integrations.ts
+++ x-pack/plugins/security_solution/public/detections/components/rules/related_integrations/use_related_integrations.ts
@@ -9,33 +9,27 @@ import { useMemo } from 'react';
 
 import { RelatedIntegrationArray } from '../../../../../common/detection_engine/schemas/common';
 import { calculateIntegrationDetails, IntegrationDetails } from './integration_details';
-import { IntegrationPrivileges } from './integration_privileges';
-import { useIntegrationPrivileges } from './use_integration_privileges';
 import { useInstalledIntegrations } from './use_installed_integrations';
 
 export interface UseRelatedIntegrationsResult {
   integrations: IntegrationDetails[];
-  privileges: IntegrationPrivileges;
   isLoaded: boolean;
 }
 
 export const useRelatedIntegrations = (
   relatedIntegrations: RelatedIntegrationArray
 ): UseRelatedIntegrationsResult => {
-  const privileges = useIntegrationPrivileges();
   const { data: installedIntegrations } = useInstalledIntegrations({ packages: [] });
 
   return useMemo(() => {
     const integrationDetails = calculateIntegrationDetails(
-      privileges,
       relatedIntegrations,
       installedIntegrations
     );
 
     return {
       integrations: integrationDetails,
-      privileges,
       isLoaded: installedIntegrations != null,
     };
-  }, [privileges, relatedIntegrations, installedIntegrations]);
+  }, [relatedIntegrations, installedIntegrations]);
 };
