84fa10f0be1c0c31d6044a3783588438ae3a9c56
Kyle Pollich <kyle.pollich@elastic.co>
2022-06-20 14:52:06 -0400

    Fix type

--- x-pack/plugins/fleet/common/types/rest_spec/package_policy.ts
+++ x-pack/plugins/fleet/common/types/rest_spec/package_policy.ts
@@ -49,8 +49,8 @@ export type UpdatePackagePolicyResponse = CreatePackagePolicyResponse;
 export interface DeletePackagePoliciesRequest {
   body: {
     packagePolicyIds: string[];
+    force?: boolean;
   };
-  force?: boolean;
 }
 
 export type DeletePackagePoliciesResponse = Array<{
