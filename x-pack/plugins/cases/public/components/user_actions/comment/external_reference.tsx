946f5f46b48e91357311def39487fa49df2bf3f3
Christos Nasikas <christos.nasikas@elastic.co>
2022-06-20 15:30:06 +0300

    Handle unregistered events

--- x-pack/plugins/cases/public/components/user_actions/comment/external_reference.tsx
+++ x-pack/plugins/cases/public/components/user_actions/comment/external_reference.tsx
@@ -7,13 +7,14 @@
 
 import React, { Suspense } from 'react';
 
-import { EuiLoadingSpinner } from '@elastic/eui';
+import { EuiCallOut, EuiCode, EuiLoadingSpinner } from '@elastic/eui';
 import { CommentResponseExternalReferenceType } from '../../../../common/api';
 import { UserActionBuilder, UserActionBuilderArgs } from '../types';
 import { UserActionTimestamp } from '../timestamp';
 import { SnakeToCamelCase } from '../../../../common/types';
 import { UserActionUsernameWithAvatar } from '../avatar_username';
 import { UserActionCopyLink } from '../copy_link';
+import { ATTACHMENT_NOT_REGISTERED_ERROR } from './translations';
 
 type BuilderArgs = Pick<
   UserActionBuilderArgs,
@@ -31,13 +32,38 @@ export const createExternalReferenceAttachmentUserActionBuilder = ({
   // TODO: Fix this manually. Issue #123375
   // eslint-disable-next-line react/display-name
   build: () => {
-    const externalReferenceType = externalReferenceAttachmentTypeRegistry.get(
+    const isTypeRegistered = externalReferenceAttachmentTypeRegistry.has(
       comment.externalReferenceAttachmentTypeId
     );
 
-    if (!externalReferenceType) {
-      return [];
+    if (!isTypeRegistered) {
+      return [
+        {
+          username: (
+            <UserActionUsernameWithAvatar
+              username={comment.createdBy.username}
+              fullName={comment.createdBy.fullName}
+            />
+          ),
+          event: (
+            <>
+              {'added an attachment of type '}
+              <EuiCode>{comment.externalReferenceAttachmentTypeId}</EuiCode>
+            </>
+          ),
+          className: 'comment-external-reference-not-found',
+          'data-test-subj': 'comment-external-reference-not-found',
+          timestamp: <UserActionTimestamp createdAt={userAction.createdAt} />,
+          children: (
+            <EuiCallOut title={ATTACHMENT_NOT_REGISTERED_ERROR} color="danger" iconType="alert" />
+          ),
+        },
+      ];
     }
+
+    const externalReferenceType = externalReferenceAttachmentTypeRegistry.get(
+      comment.externalReferenceAttachmentTypeId
+    );
 
     const externalReferenceViewObject = externalReferenceType.getAttachmentViewObject({
       externalReferenceId: externalReferenceType.id,
@@ -54,9 +80,9 @@ export const createExternalReferenceAttachmentUserActionBuilder = ({
           />
         ),
         type: externalReferenceViewObject.type,
-        className: `comment-external-reference${externalReferenceType.id}`,
+        className: `comment-external-reference${comment.externalReferenceAttachmentTypeId}`,
         event: externalReferenceViewObject.event,
-        'data-test-subj': `comment-external-reference${externalReferenceType.id}`,
+        'data-test-subj': `comment-external-reference${comment.externalReferenceAttachmentTypeId}`,
         timestamp: <UserActionTimestamp createdAt={userAction.createdAt} />,
         timelineIcon: externalReferenceViewObject.timelineIcon,
         actions: (
