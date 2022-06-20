50a0605804b42678a10537fa50bcfe7b3ec097be
Tre' Seymour <wayne.seymour@elastic.co>
2022-06-20 11:37:18 +0100

    Drop single quotes.

--- .buildkite/scripts/steps/code_coverage/reporting/ingestData.sh
+++ .buildkite/scripts/steps/code_coverage/reporting/ingestData.sh
@@ -54,6 +54,6 @@ ingestModular() {
   url="https://kibana-stats.elastic.dev/app/discover#/?_g=(filters:!(),query:(language:kuery,query:''),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(columns:!(),filters:!(),hideChart:!f,index:'64419790-4218-11ea-b2d8-81bcbf78dfcb',interval:auto,query:(language:kuery,query:'BUILD_ID%20:%20${currentBuildNumber}'),sort:!(!('@timestamp',desc)))"
 
   echo "--- Browse the following url to visually verify in Kibana (Disover):"
-  printf "%s \n" "'$url'"
+  printf "%s \n" "$url"
 
 }
