1f5a67986dcadb1e90be73ba954dd79f3a54ac55
Madi Caldwell <madison.caldwell@elastic.co>
2022-06-20 13:00:17 -0400

    fixes

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/threshold/build_threshold_aggregation.test.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/threshold/build_threshold_aggregation.test.ts
@@ -4,3 +4,25 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+describe('build_threshold_aggregation', () => {
+  describe('build_threshold_multi_bucket_aggregation', () => {
+    it('Generates aggregation without cardinality', async () => {
+      // TODO
+    });
+
+    it('Generates aggregation with cardinality', async () => {
+      // TODO
+    });
+  });
+
+  describe('build_threshold_single_bucket_aggregation', () => {
+    it('Generates aggregation without cardinality', async () => {
+      // TODO
+    });
+
+    it('Generates aggregation with cardinality', async () => {
+      // TODO
+    });
+  });
+});
