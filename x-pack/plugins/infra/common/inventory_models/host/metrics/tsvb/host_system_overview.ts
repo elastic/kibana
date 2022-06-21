1cfd335c5295df3e19aacc26727300a9ce176623
neptunian <sandra.gonzales@elastic.co>
2022-06-21 17:17:46 -0400

    update tsvb charts to handle gauge correctly

--- x-pack/plugins/infra/common/inventory_models/host/metrics/tsvb/host_system_overview.ts
+++ x-pack/plugins/infra/common/inventory_models/host/metrics/tsvb/host_system_overview.ts
@@ -54,25 +54,77 @@ export const hostSystemOverview: TSVBMetricModelCreator = (
     },
     {
       id: 'rx',
-      split_mode: 'everything',
       metrics: [
         {
           field: 'host.network.ingress.bytes',
           id: 'avg-net-in',
           type: 'avg',
         },
+        {
+          id: 'max-period',
+          type: 'max',
+          field: 'metricset.period',
+        },
+        {
+          id: '3216b170-f192-11ec-a8e3-dd984b7213e2',
+          type: 'calculation',
+          variables: [
+            {
+              id: '34e64c30-f192-11ec-a8e3-dd984b7213e2',
+              name: 'value',
+              field: 'avg-net-in',
+            },
+            {
+              id: '3886cb80-f192-11ec-a8e3-dd984b7213e2',
+              name: 'period',
+              field: 'max-period',
+            },
+          ],
+          script: 'params.value / (params.period / 1000)',
+        },
       ],
+      filter: {
+        language: 'kuery',
+        query: 'host.network.ingress.bytes : * ',
+      },
+      split_mode: 'everything',
     },
     {
       id: 'tx',
-      split_mode: 'everything',
       metrics: [
         {
           field: 'host.network.egress.bytes',
           id: 'avg-net-out',
           type: 'avg',
         },
+        {
+          id: 'max-period',
+          type: 'max',
+          field: 'metricset.period',
+        },
+        {
+          id: '3216b170-f192-11ec-a8e3-dd984b7213e2',
+          type: 'calculation',
+          variables: [
+            {
+              id: '34e64c30-f192-11ec-a8e3-dd984b7213e2',
+              name: 'value',
+              field: 'avg-net-out',
+            },
+            {
+              id: '3886cb80-f192-11ec-a8e3-dd984b7213e2',
+              name: 'period',
+              field: 'max-period',
+            },
+          ],
+          script: 'params.value / (params.period / 1000)',
+        },
       ],
+      filter: {
+        language: 'kuery',
+        query: 'host.network.egress.bytes : * ',
+      },
+      split_mode: 'everything',
     },
   ],
 });
