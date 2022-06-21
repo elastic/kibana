32a2e434b0170a9b64c57afec3e27df13195fc42
10xtechie <koder.kay@gmail.com>
2022-06-21 16:27:27 +0000

    issue #133857 - EuiToolTipProps is used instead of ToolTipProps

--- src/plugins/navigation/public/top_nav_menu/top_nav_menu.tsx
+++ src/plugins/navigation/public/top_nav_menu/top_nav_menu.tsx
@@ -13,7 +13,7 @@ import {
   EuiBadgeProps,
   EuiHeaderLinks,
   EuiToolTip,
-  ToolTipPositions,
+  EuiToolTipProps,
 } from '@elastic/eui';
 import classNames from 'classnames';
 
@@ -32,7 +32,7 @@ export type TopNavMenuProps<QT extends Query | AggregateQuery = Query> =
       badges?: Array<
         EuiBadgeProps & {
           badgeText: string;
-          toolTipProps: { content: string; position: ToolTipPositions };
+          toolTipProps: EuiToolTipProps;
         }
       >;
       showSearchBar?: boolean;
@@ -89,16 +89,13 @@ export function TopNavMenu<QT extends AggregateQuery | Query = Query>(
           (
             badge: EuiBadgeProps & {
               badgeText: string;
-              toolTipProps: { content: string; position: string };
+              toolTipProps: EuiToolTipProps;
             },
             i: number
           ) => {
             const { badgeText, toolTipProps, ...badgeProps } = badge;
             return (
-              <EuiToolTip
-                content={toolTipProps.content}
-                position={toolTipProps.position as ToolTipPositions}
-              >
+              <EuiToolTip {...toolTipProps} >
                 <EuiBadge key={`nav-menu-badge-${i}`} {...badgeProps}>
                   {badgeText}
                 </EuiBadge>
