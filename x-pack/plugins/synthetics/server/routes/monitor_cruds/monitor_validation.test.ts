9c9242a101cba7000713ce01b267094c95664bca
Dominique Clarke <dominique.clarke@elastic.co>
2022-06-21 19:50:39 -0400

    synthetics - ensure port is never undefined

--- x-pack/plugins/synthetics/server/routes/monitor_cruds/monitor_validation.test.ts
+++ x-pack/plugins/synthetics/server/routes/monitor_cruds/monitor_validation.test.ts
@@ -173,8 +173,8 @@ describe('validateMonitor', () => {
       [ConfigKey.SOURCE_ZIP_PASSWORD]: 'password',
       [ConfigKey.SOURCE_ZIP_PROXY_URL]: 'http://proxy-url.com',
       [ConfigKey.PARAMS]: '',
-      [ConfigKey.URLS]: undefined,
-      [ConfigKey.PORT]: undefined,
+      [ConfigKey.URLS]: null,
+      [ConfigKey.PORT]: null,
     };
 
     testBrowserAdvancedFields = {
