253de5f9e916216edd83fc0f58020ea8f7f4dac7
Walter Rafelsberger <walter@elastic.co>
2022-06-21 12:17:06 +0200

    [ML] Fix directory name.

--- x-pack/packages/ml/data_utils/jest.config.js
+++ x-pack/packages/ml/data_utils/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/data-utils'],
+  roots: ['<rootDir>/x-pack/packages/ml/data_utils'],
 };
