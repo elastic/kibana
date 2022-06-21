5dfe45ec82421650ca7590c82412c2c5876c977c
Brian Seeders <brian.seeders@elastic.co>
2022-06-21 14:34:25 -0400

    node -> ts-node

--- .buildkite/scripts/steps/es_snapshots/promote.sh
+++ .buildkite/scripts/steps/es_snapshots/promote.sh
@@ -11,7 +11,7 @@ cat << EOF | buildkite-agent annotate --style "info"
   $ES_SNAPSHOT_MANIFEST
 EOF
 
-node "$(dirname "${0}")/promote_manifest.js" "$ES_SNAPSHOT_MANIFEST"
+ts-node "$(dirname "${0}")/promote_manifest.js" "$ES_SNAPSHOT_MANIFEST"
 
 if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
   echo "--- Trigger agent packer cache pipeline"
