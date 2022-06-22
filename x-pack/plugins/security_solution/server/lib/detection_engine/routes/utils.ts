e995672f27fe293a1f4475f928674456ca5acc3d
Joanne <68304724+icemakegirl@users.noreply.github.com>
2022-06-21 22:46:28 -0300

    Update utils.ts

--- x-pack/plugins/security_solution/server/lib/detection_engine/routes/utils.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/routes/utils.ts
@@ -5,192 +5,111 @@
  * 2.0.
  */
 
-import { has, snakeCase } from 'lodash/fp';
 import { BadRequestError } from '@kbn/securitysolution-es-utils';
-
-import {
-  RouteValidationFunction,
-  KibanaResponseFactory,
-  CustomHttpResponseOptions,
-} from '@kbn/core/server';
-
+import { transformBulkError, BulkError, convertToSnakeCase, SiemResponseFactory } from './utils';
+import { responseMock } from './__mocks__';
 import { CustomHttpRequestError } from '../../../utils/custom_http_request_error';
 
-export interface OutputError {
-  message: string;
-  statusCode: number;
-}
-export interface BulkError {
-  id?: string;
-  rule_id?: string;
-  error: {
-    status_code: number;
-    message: string;
-  };
-}
-
-export const createBulkErrorObject = ({
-  ruleId,
-  id,
-  statusCode,
-  message,
-}: {
-  ruleId?: string;
-  id?: string;
-  statusCode: number;
-  message: string;
-}): BulkError => {
-  if (id != null && ruleId != null) {
-    return {
-      id,
-      rule_id: ruleId,
-      error: {
-        status_code: statusCode,
-        message,
-      },
-    };
-  } else if (id != null) {
-    return {
-      id,
-      error: {
-        status_code: statusCode,
-        message,
-      },
-    };
-  } else if (ruleId != null) {
-    return {
-      rule_id: ruleId,
-      error: {
-        status_code: statusCode,
-        message,
-      },
-    };
-  } else {
-    return {
-      rule_id: '(unknown id)',
-      error: {
-        status_code: statusCode,
-        message,
-      },
-    };
-  }
-};
-
-export interface ImportRegular {
-  rule_id: string;
-  status_code: number;
-  message?: string;
-}
-
-export type ImportRuleResponse = ImportRegular | BulkError;
-
-export const isBulkError = (
-  importRuleResponse: ImportRuleResponse
-): importRuleResponse is BulkError => {
-  return has('error', importRuleResponse);
-};
-
-export const isImportRegular = (
-  importRuleResponse: ImportRuleResponse
-): importRuleResponse is ImportRegular => {
-  return !has('error', importRuleResponse) && has('status_code', importRuleResponse);
-};
-
-export const transformBulkError = (
-  ruleId: string,
-  err: Error & { statusCode?: number }
-): BulkError => {
-  if (err instanceof CustomHttpRequestError) {
-    return createBulkErrorObject({
-      ruleId,
-      statusCode: err.statusCode ?? 400,
-      message: err.message,
+describe('utils', () => {
+  describe('transformBulkError', () => {
+    test('returns transformed object if it is a custom error object', () => {
+      const customError = new CustomHttpRequestError('some custom error message', 400);
+      const transformed = transformBulkError('rule-1', customError);
+      const expected: BulkError = {
+        rule_id: 'rule-1',
+        error: { message: 'some custom error message', status_code: 400 },
+      };
+      expect(transformed).toEqual(expected);
     });
-  } else if (err instanceof BadRequestError) {
-    return createBulkErrorObject({
-      ruleId,
-      statusCode: 400,
-      message: err.message,
+
+    test('returns a normal error if it is some non custom error that has a statusCode', () => {
+      const error: Error & { statusCode?: number } = {
+        statusCode: 403,
+        name: 'some name',
+        message: 'some message',
+      };
+      const transformed = transformBulkError('rule-1', error);
+      const expected: BulkError = {
+        rule_id: 'rule-1',
+        error: { message: 'some message', status_code: 403 },
+      };
+      expect(transformed).toEqual(expected);
     });
-  } else {
-    return createBulkErrorObject({
-      ruleId,
-      statusCode: err.statusCode ?? 500,
-      message: err.message,
+
+    test('returns a 500 if the status code is not set', () => {
+      const error: Error & { statusCode?: number } = {
+        name: 'some name',
+        message: 'some message',
+      };
+      const transformed = transformBulkError('rule-1', error);
+      const expected: BulkError = {
+        rule_id: 'rule-1',
+        error: { message: 'some message', status_code: 500 },
+      };
+      expect(transformed).toEqual(expected);
     });
-  }
-};
 
-interface Schema {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  validate: (input: any) => { value: any; error?: Error };
-}
+    test('it detects a BadRequestError and returns an error status of 400', () => {
+      const error: BadRequestError = new BadRequestError('I have a type error');
+      const transformed = transformBulkError('rule-1', error);
+      const expected: BulkError = {
+        rule_id: 'rule-1',
+        error: { message: 'I have a type error', status_code: 400 },
+      };
+      expect(transformed).toEqual(expected);
+    });
+  });
 
-export const buildRouteValidation =
-  <T>(schema: Schema): RouteValidationFunction<T> =>
-  (payload: T, { ok, badRequest }) => {
-    const { value, error } = schema.validate(payload);
-    if (error) {
-      return badRequest(error.message);
-    }
-    return ok(value);
-  };
+  describe('convertToSnakeCase', () => {
+    it('converts camelCase to snakeCase', () => {
+      const values = { myTestCamelCaseKey: 'something' };
+      expect(convertToSnakeCase(values)).toEqual({ my_test_camel_case_key: 'something' });
+    });
+    it('returns empty object when object is empty', () => {
+      const values = {};
+      expect(convertToSnakeCase(values)).toEqual({});
+    });
+    it('returns null when passed in undefined', () => {
+      interface Foo {
+        bar: Record<string, unknown>;
+      }
 
-const statusToErrorMessage = (statusCode: number) => {
-  switch (statusCode) {
-    case 400:
-      return 'Bad Request';
-    case 401:
-      return 'Unauthorized';
-    case 403:
-      return 'Forbidden';
-    case 404:
-      return 'Not Found';
-    case 409:
-      return 'Conflict';
-    case 500:
-      return 'Internal Error';
-    default:
-      return '(unknown error)';
-  }
-};
+      // Array accessors can result in undefined but
+      // this is not represented in typescript for some reason,
+      // https://github.com/Microsoft/TypeScript/issues/11122
+      const array: Foo[] = [];
 
-export class SiemResponseFactory {
-  constructor(private response: KibanaResponseFactory) {}
+      // This is undefined, but it says it's not
+      const undefinedValue = array[0]?.bar;
 
-  error<T>({ statusCode, body, headers }: CustomHttpResponseOptions<T>) {
-    const contentType: CustomHttpResponseOptions<T>['headers'] = {
-      'content-type': 'application/json',
-    };
-    const defaultedHeaders: CustomHttpResponseOptions<T>['headers'] = {
-      ...contentType,
-      ...(headers ?? {}),
-    };
+      expect(convertToSnakeCase(undefinedValue)).toEqual(null);
+    });
+  });
 
-    return this.response.custom({
-      headers: defaultedHeaders,
-      statusCode,
-      body: Buffer.from(
-        JSON.stringify({
-          message: body ?? statusToErrorMessage(statusCode),
-          status_code: statusCode,
+  describe('SiemResponseFactory', () => {
+    it('builds a custom response', () => {
+      const response = responseMock.create();
+      const responseFactory = new SiemResponseFactory(response);
+
+      responseFactory.error({ statusCode: 400 });
+      expect(response.custom).toHaveBeenCalled();
+    });
+
+    it('generates a status_code key on the response', () => {
+      const response = responseMock.create();
+      const responseFactory = new SiemResponseFactory(response);
+
+      responseFactory.error({statusCode: 400 });
+      const [[{ error_code, attributes,message }]] = response.custom.mock.calls;
+
+      expect(error_code).toEqual(400);
+      expect(message).toEqual('Bad Request');
+      expect(attributes).toBeInstanceOf(Buffer);
+      expect(JSON.parse(attributes!.toString())).toEqual(
+        expect.objectContaining({
+          status_code: 400,
         })
-      ),
+      );
     });
-  }
-}
-
-export const buildSiemResponse = (response: KibanaResponseFactory) =>
-  new SiemResponseFactory(response);
-
-export const convertToSnakeCase = <T extends Record<string, unknown>>(
-  obj: T
-): Partial<T> | null => {
-  if (!obj) {
-    return null;
-  }
-  return Object.keys(obj).reduce((acc, item) => {
-    const newKey = snakeCase(item);
-    return { ...acc, [newKey]: obj[item] };
-  }, {});
-};
+  });
+});
