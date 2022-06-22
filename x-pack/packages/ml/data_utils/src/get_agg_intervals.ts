21429bfc81d34867afcf40f6a52c822d7a9fe19a
Walter Rafelsberger <walter@elastic.co>
2022-06-22 10:59:58 +0200

    [ML] Updated comment.

--- x-pack/packages/ml/data_utils/src/get_agg_intervals.ts
+++ x-pack/packages/ml/data_utils/src/get_agg_intervals.ts
@@ -18,9 +18,12 @@ import { getSamplerAggregationsResponsePath } from './get_sampler_aggregations_r
 import { stringHash } from './string_hash';
 
 // TODO Temporary type definition until we can import from `@kbn/core`.
+// Copied from src/core/server/elasticsearch/client/types.ts
+// as these types aren't part of any package yet. Once they are, remove this completely
+
 /**
  * Client used to query the elasticsearch cluster.
- *
+ * @deprecated At some point use the one from src/core/server/elasticsearch/client/types.ts when it is made into a package. If it never is, then keep using this one.
  * @public
  */
 export type ElasticsearchClient = Omit<
