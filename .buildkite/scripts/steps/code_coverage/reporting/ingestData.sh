713dbe9e3280ce273c96932094ef48c65775777b
Tre <wayne.seymour@elastic.co>
2022-06-23 16:00:48 +0100

    [QA][Code Coverage] Add meta data links ci runs. (#134731)
    * [QA][Code Coverage] Add a link to discover pinned to the current build.
    
    When personnel other than myself wish to visually verify code coverage,
    this link will be available in the ci output.
    
    * Drop single quotes.
    
    * Add annotation per J Budz.
    
    * Fixup text.
    
    * Drop debug.

--- .buildkite/scripts/steps/code_coverage/reporting/ingestData.sh
+++ .buildkite/scripts/steps/code_coverage/reporting/ingestData.sh
@@ -29,6 +29,23 @@ echo "### debug TEAM_ASSIGN_PATH: ${TEAM_ASSIGN_PATH}"
 BUFFER_SIZE=500
 export BUFFER_SIZE
 
+annotateForKibanaLinks() {
+  local currentBuildNumber="$BUILDKITE_BUILD_NUMBER"
+  local coverageUrl="https://kibana-stats.elastic.dev/app/discover#/?_g=(filters:!(),query:(language:kuery,query:''),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(columns:!(),filters:!(),hideChart:!f,index:'64419790-4218-11ea-b2d8-81bcbf78dfcb',interval:auto,query:(language:kuery,query:'BUILD_ID%20:%20${currentBuildNumber}'),sort:!(!('@timestamp',desc)))"
+  local totalCoverageUrl="https://kibana-stats.elastic.dev/app/discover#/?_g=(filters:!(),query:(language:kuery,query:''),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(columns:!(),filters:!(),hideChart:!f,index:d78f9120-4218-11ea-b2d8-81bcbf78dfcb,interval:auto,query:(language:kuery,query:'BUILD_ID%20:%20${currentBuildNumber}'),sort:!(!('@timestamp',desc)))"
+
+  cat <<EOF | buildkite-agent annotate --style "info" --context 'ctx-kibana-links'
+### Browse the following url(s) to visually verify in Kibana
+
+_Links are pinned to the current build number._
+
+  - [Code Coverage]($coverageUrl)
+  - [Total Code Coverage]($totalCoverageUrl)
+
+EOF
+
+}
+
 ingestModular() {
   local xs=("$@")
 
