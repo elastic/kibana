7c0095bd6fc5461c4039120ba9290ecbe517f026
Sander Philipse <94373878+sphilipse@users.noreply.github.com>
2022-06-23 13:33:39 +0200

    [Enterprise Search]Add util function to create Kea logic files for API calls (#134932)
    * [Enterprise Search]Add util function to create Kea logic files for API calls
    
    * Fixed unit tests for add custom source logic
    
    * Make Status an enum, add tests, move flash messages
    
    * Fix some more tests

--- x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/add_custom_source/add_custom_source_logic.test.ts
+++ x-pack/plugins/enterprise_search/public/applications/workplace_search/views/content_sources/components/add_source/add_custom_source/add_custom_source_logic.test.ts
@@ -7,12 +7,17 @@
 
 import { LogicMounter } from '../../../../../../__mocks__/kea_logic';
 
+import { mockFlashMessageHelpers } from '../../../../../../__mocks__/kea_logic';
+
+import { Status } from '../../../../../../../../common/types/api';
+
 jest.mock('../../../../../app_logic', () => ({
   AppLogic: { values: { isOrganization: true } },
 }));
 
 import { CustomSource } from '../../../../../types';
 
+import { AddCustomSourceApiLogic } from './add_custom_source_api_logic';
 import { AddCustomSourceLogic, AddCustomSourceSteps } from './add_custom_source_logic';
 
 const DEFAULT_VALUES = {
@@ -20,15 +25,14 @@ const DEFAULT_VALUES = {
   buttonLoading: false,
   customSourceNameValue: '',
   newCustomSource: {} as CustomSource,
-  sourceApi: {
-    status: 'IDLE',
-  },
+  status: Status.IDLE,
 };
 
 const MOCK_NAME = 'name';
 
 describe('AddCustomSourceLogic', () => {
   const { mount } = new LogicMounter(AddCustomSourceLogic);
+  const { clearFlashMessages, flashAPIErrors } = mockFlashMessageHelpers;
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -40,24 +44,45 @@ describe('AddCustomSourceLogic', () => {
   });
 
   describe('actions', () => {
-    describe('addSourceSuccess', () => {
+    describe('apiSuccess', () => {
       it('sets a new source', () => {
-        const customSource: CustomSource = {
+        AddCustomSourceLogic.actions.makeRequest({ name: 'name' });
+        const source: CustomSource = {
           accessToken: 'a',
           name: 'b',
           id: '1',
         };
-        AddCustomSourceLogic.actions.addSourceSuccess(customSource);
+        AddCustomSourceLogic.actions.apiSuccess({ source });
 
         expect(AddCustomSourceLogic.values).toEqual({
           ...DEFAULT_VALUES,
           customSourceNameValue: '',
-          newCustomSource: customSource,
-          sourceApi: {
-            status: 'SUCCESS',
-            data: customSource,
-          },
+          newCustomSource: source,
+          status: Status.SUCCESS,
           currentStep: AddCustomSourceSteps.SaveCustomStep,
+        });
+      });
+    });
+    describe('makeRequest', () => {
+      it('sets button to loading', () => {
+        AddCustomSourceLogic.actions.makeRequest({ name: 'name' });
+
+        expect(AddCustomSourceLogic.values).toEqual({
+          ...DEFAULT_VALUES,
+          buttonLoading: true,
+          status: Status.LOADING,
+        });
+      });
+    });
+    describe('apiError', () => {
+      it('sets button to not loading', () => {
+        AddCustomSourceLogic.actions.makeRequest({ name: 'name' });
+        AddCustomSourceLogic.actions.apiError('error' as any);
+
+        expect(AddCustomSourceLogic.values).toEqual({
+          ...DEFAULT_VALUES,
+          buttonLoading: false,
+          status: Status.ERROR,
         });
       });
     });
@@ -98,32 +123,47 @@ describe('AddCustomSourceLogic', () => {
         customSourceNameValue: MOCK_NAME,
       });
     });
+    describe('createContentSource', () => {
+      it('calls addSource on AddCustomSourceApi logic', async () => {
+        const addSourceSpy = jest.spyOn(AddCustomSourceLogic.actions, 'makeRequest');
 
-    describe('organization context', () => {
-      describe('createContentSource', () => {
-        it('calls addSource on AddCustomSourceApi logic', async () => {
-          const addSourceSpy = jest.spyOn(AddCustomSourceLogic.actions, 'addSource');
-
-          AddCustomSourceLogic.actions.createContentSource();
-          expect(addSourceSpy).toHaveBeenCalledWith(MOCK_NAME, undefined);
+        AddCustomSourceLogic.actions.createContentSource();
+        expect(addSourceSpy).toHaveBeenCalledWith({
+          name: MOCK_NAME,
+          baseServiceType: undefined,
         });
+      });
 
-        it('submits a base service type for pre-configured sources', () => {
-          mount(
-            {
-              customSourceNameValue: MOCK_NAME,
-            },
-            {
-              baseServiceType: 'share_point_server',
-            }
-          );
+      it('submits a base service type for pre-configured sources', () => {
+        mount(
+          {
+            customSourceNameValue: MOCK_NAME,
+          },
+          {
+            baseServiceType: 'share_point_server',
+          }
+        );
 
-          const addSourceSpy = jest.spyOn(AddCustomSourceLogic.actions, 'addSource');
+        const addSourceSpy = jest.spyOn(AddCustomSourceLogic.actions, 'makeRequest');
 
-          AddCustomSourceLogic.actions.createContentSource();
+        AddCustomSourceLogic.actions.createContentSource();
 
-          expect(addSourceSpy).toHaveBeenCalledWith(MOCK_NAME, 'share_point_server');
+        expect(addSourceSpy).toHaveBeenCalledWith({
+          name: MOCK_NAME,
+          baseServiceType: 'share_point_server',
         });
+      });
+    });
+    describe('makeRequest', () => {
+      it('should call clearFlashMessages', () => {
+        AddCustomSourceApiLogic.actions.makeRequest({ name: 'name' });
+        expect(clearFlashMessages).toHaveBeenCalled();
+      });
+    });
+    describe('apiError', () => {
+      it('should call flashAPIError', () => {
+        AddCustomSourceApiLogic.actions.apiError('error' as any);
+        expect(flashAPIErrors).toHaveBeenCalledWith('error');
       });
     });
   });
