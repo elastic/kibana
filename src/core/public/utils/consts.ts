8133605b894804cd834aea6152980c6164b9768f
Liza Katz <liza.katz@elastic.co>
2022-06-23 12:46:07 +0300

    [EBT] Enrich kibana loaded with timings (#134770)
    * Add timings to kibana loaded event
    
    * jest
    
    * PR failures
    
    * code review
    
    * docs
    
    * add first_app_nav and first_app
    
    * tests
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * Update src/core/public/core_system.ts
    
    Co-authored-by: Alejandro Fernández Haro <afharo@gmail.com>
    
    * Update src/core/public/core_system.ts
    
    Co-authored-by: Alejandro Fernández Haro <afharo@gmail.com>
    
    * review @afjaro
    
    * typo
    
    * KBN_LOAD_MARKS
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Alejandro Fernández Haro <afharo@gmail.com>

--- src/core/public/utils/consts.ts
+++ src/core/public/utils/consts.ts
@@ -6,7 +6,5 @@
  * Side Public License, v 1.
  */
 
-export { Sha256 } from './crypto';
-export { MountWrapper, mountReactNode } from './mount';
-export { CoreContextProvider } from './core_context_provider';
-export { KBN_LOAD_MARKS } from './consts';
+/** @internal */
+export const KBN_LOAD_MARKS = 'kbnLoad';
