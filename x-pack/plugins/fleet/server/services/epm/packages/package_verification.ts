d3f11aca1bf840f8f0cfec750bc765edac447149
Mark Hopkin <mark.hopkin@elastic.co>
2022-06-23 15:35:32 +0100

    Use opengpg to read gpg key

--- x-pack/plugins/fleet/server/services/epm/packages/package_verification.ts
+++ x-pack/plugins/fleet/server/services/epm/packages/package_verification.ts
@@ -7,22 +7,22 @@
 
 import { readFile } from 'fs/promises';
 
+import * as openpgp from 'openpgp';
+
 import { appContextService } from '../../app_context';
+let cachedKey: openpgp.Key | undefined | null = null;
 
-let cachedKey: string | undefined | null = null;
-
-export async function getGpgKeyOrUndefined(): Promise<string | undefined> {
+export async function getGpgKeyOrUndefined(): Promise<openpgp.Key | undefined> {
   if (cachedKey !== null) return cachedKey;
 
   cachedKey = await _readGpgKey();
   return cachedKey;
 }
 
-export async function _readGpgKey(): Promise<string | undefined> {
+export async function _readGpgKey(): Promise<openpgp.Key | undefined> {
   const config = appContextService.getConfig();
   const logger = appContextService.getLogger();
   const gpgKeyPath = config?.packageVerification?.gpgKeyPath;
-
   if (!gpgKeyPath) {
     logger.warn('GPG key path not configured at "xpack.fleet.packageVerification.gpgKeyPath"');
     return undefined;
@@ -35,8 +35,12 @@ export async function _readGpgKey(): Promise<string | undefined> {
     logger.warn(`Unable to retrieve GPG key from '${gpgKeyPath}': ${e.code}`);
     return undefined;
   }
+  let key;
+  try {
+    key = await openpgp.readKey({ armoredKey: buffer.toString() });
+  } catch (e) {
+    logger.warn(`Unable to parse GPG key from '${gpgKeyPath}': ${e}`);
+  }
 
-  const key = buffer.toString();
-  cachedKey = key;
   return key;
 }
