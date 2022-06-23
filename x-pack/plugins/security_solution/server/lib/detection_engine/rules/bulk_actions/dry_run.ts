1f871c10a4996c2793c3320e25aab73a0e6bb120
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2022-06-23 13:12:27 +0100

    lint

--- x-pack/plugins/security_solution/server/lib/detection_engine/rules/bulk_actions/dry_run.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rules/bulk_actions/dry_run.ts
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import { invariant } from '../../../../../common/utils/invariant';
 import { isMlRule } from '../../../../../common/machine_learning/helpers';
 import { BULK_ACTIONS_DRY_RUN_ERR_CODE } from '../../../../../common/constants';
