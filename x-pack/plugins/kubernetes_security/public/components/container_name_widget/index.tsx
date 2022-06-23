d1e2215c929004c619c97c8353b66f9290fc9e6e
Ricky Ang <rickyangwyn@gmail.com>
2022-06-22 20:51:08 -0700

    some clean up

--- x-pack/plugins/kubernetes_security/public/components/container_name_widget/index.tsx
+++ x-pack/plugins/kubernetes_security/public/components/container_name_widget/index.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { ReactNode, useMemo, useState, useCallback } from 'react';
-import { EuiDataGrid, EuiPanel, EuiDataGridColumn } from '@elastic/eui';
+import { EuiDataGrid, EuiPanel, EuiDataGridColumn, EuiDataGridColumnVisibility, EuiDataGridStyle, EuiDataGridSorting, EuiDataGridInMemory } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { useStyles } from './styles';
 import type { IndexPattern, GlobalFilter } from '../../types';
@@ -23,6 +23,7 @@ export interface ContainerNameWidgetDataValueMap {
 }
 
 export interface ContainerNameArrayDataValue {
+  [key: string]: any;
   name: string;
   count: number;
 }
@@ -158,29 +159,37 @@ export const ContainerNameWidget = ({
     [setSortingColumns]
   );
 
+  const GridStyle : EuiDataGridStyle = {
+    border: 'none',
+    header: 'underline',
+    fontSize: 's',
+  }
+
+  const ColumnVisibility : EuiDataGridColumnVisibility = {
+    visibleColumns,
+    setVisibleColumns,
+  }
+
+  const InMemory : EuiDataGridInMemory =   { level: 'sorting' }
+
+  const Sorting : EuiDataGridSorting = {
+    columns: sortingColumns,
+    onSort,
+  }
+
   return (
     <EuiPanel css={styles.tablePadding}>
       <EuiDataGrid
         columns={columns}
         rowCount={containerNameArray.length}
         toolbarVisibility={false}
-        gridStyle={{
-          border: 'none',
-          header: 'underline',
-          fontSize: 's',
-        }}
-        columnVisibility={{
-          visibleColumns,
-          setVisibleColumns,
-        }}
+        gridStyle={GridStyle}
+        columnVisibility={ColumnVisibility}
         renderCellValue={({ rowIndex, columnId }: { rowIndex: number; columnId: string }) =>
           containerNameArray[rowIndex][columnId]
         }
-        inMemory={{ level: 'sorting' }}
-        sorting={{
-          columns: sortingColumns,
-          onSort,
-        }}
+        inMemory={InMemory}
+        sorting={Sorting}
       />
     </EuiPanel>
   );
