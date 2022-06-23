f8c5b6ccc07d18278a9a7572706b335d08631300
Julia Bardi <julia.bardi@elastic.co>
2022-06-23 13:51:16 +0200

    added tests

--- x-pack/plugins/fleet/server/routes/agent/handlers.ts
+++ x-pack/plugins/fleet/server/routes/agent/handlers.ts
@@ -221,7 +221,7 @@ export const postBulkAgentsReassignHandler: RequestHandler<
     const results = await AgentService.reassignAgents(
       soClient,
       esClient,
-      agentOptions,
+      { ...agentOptions, perPage: request.body.perPage },
       request.body.policy_id
     );
 
