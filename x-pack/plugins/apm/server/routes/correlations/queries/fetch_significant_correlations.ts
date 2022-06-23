ec3e3b27c6b0bbe3237098fc32a454e476bf2f83
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-06-23 15:49:53 +0200

    [APM] Backend operation distribution chart (#134561)
    * [APM] Backend operation distribution chart
    
    Closes #133483.
    
    * Update labels
    
    * Take sampleRangeFrom/To into account for span operations table
    
    * Remove console.log statements
    
    * Review feedback
    
    * Add event.outcome and truncated trace id to table
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    * Update API tests
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/apm/server/routes/correlations/queries/fetch_significant_correlations.ts
+++ x-pack/plugins/apm/server/routes/correlations/queries/fetch_significant_correlations.ts
@@ -7,40 +7,51 @@
 
 import { range } from 'lodash';
 
-import type { ElasticsearchClient } from '@kbn/core/server';
-
-import type {
-  FieldValuePair,
-  CorrelationsParams,
-} from '../../../../common/correlations/types';
+import { termQuery } from '@kbn/observability-plugin/server';
 import type { LatencyCorrelation } from '../../../../common/correlations/latency_correlations/types';
+import type {
+  CommonCorrelationsQueryParams,
+  FieldValuePair,
+} from '../../../../common/correlations/types';
 
+import { ProcessorEvent } from '../../../../common/processor_event';
+import { Setup } from '../../../lib/helpers/setup_request';
 import {
   computeExpectationsAndRanges,
   splitAllSettledPromises,
 } from '../utils';
+import { fetchDurationPercentiles } from './fetch_duration_percentiles';
+import { fetchDurationCorrelationWithHistogram } from './fetch_duration_correlation_with_histogram';
+import { fetchDurationFractions } from './fetch_duration_fractions';
+import { fetchDurationHistogramRangeSteps } from './fetch_duration_histogram_range_steps';
+import { fetchDurationRanges } from './fetch_duration_ranges';
 
-import {
-  fetchTransactionDurationCorrelationWithHistogram,
-  fetchTransactionDurationFractions,
-  fetchTransactionDurationHistogramRangeSteps,
-  fetchTransactionDurationPercentiles,
-  fetchTransactionDurationRanges,
-} from '.';
-
-export const fetchSignificantCorrelations = async (
-  esClient: ElasticsearchClient,
-  paramsWithIndex: CorrelationsParams,
-  fieldValuePairs: FieldValuePair[]
-) => {
+export const fetchSignificantCorrelations = async ({
+  setup,
+  eventType,
+  start,
+  end,
+  environment,
+  kuery,
+  query,
+  fieldValuePairs,
+}: CommonCorrelationsQueryParams & {
+  setup: Setup;
+  eventType: ProcessorEvent;
+  fieldValuePairs: FieldValuePair[];
+}) => {
   // Create an array of ranges [2, 4, 6, ..., 98]
   const percentileAggregationPercents = range(2, 100, 2);
-  const { percentiles: percentilesRecords } =
-    await fetchTransactionDurationPercentiles(
-      esClient,
-      paramsWithIndex,
-      percentileAggregationPercents
-    );
+  const { percentiles: percentilesRecords } = await fetchDurationPercentiles({
+    setup,
+    eventType,
+    start,
+    end,
+    environment,
+    kuery,
+    query,
+    percents: percentileAggregationPercents,
+  });
 
   // We need to round the percentiles values
   // because the queries we're using based on it
@@ -49,30 +60,45 @@ export const fetchSignificantCorrelations = async (
 
   const { expectations, ranges } = computeExpectationsAndRanges(percentiles);
 
-  const { fractions, totalDocCount } = await fetchTransactionDurationFractions(
-    esClient,
-    paramsWithIndex,
-    ranges
-  );
+  const { fractions, totalDocCount } = await fetchDurationFractions({
+    setup,
+    eventType,
+    start,
+    end,
+    environment,
+    kuery,
+    query,
+    ranges,
+  });
 
-  const histogramRangeSteps = await fetchTransactionDurationHistogramRangeSteps(
-    esClient,
-    paramsWithIndex
-  );
+  const histogramRangeSteps = await fetchDurationHistogramRangeSteps({
+    setup,
+    eventType,
+    start,
+    end,
+    environment,
+    kuery,
+    query,
+  });
 
   const { fulfilled, rejected } = splitAllSettledPromises(
     await Promise.allSettled(
       fieldValuePairs.map((fieldValuePair) =>
-        fetchTransactionDurationCorrelationWithHistogram(
-          esClient,
-          paramsWithIndex,
+        fetchDurationCorrelationWithHistogram({
+          setup,
+          eventType,
+          start,
+          end,
+          environment,
+          kuery,
+          query,
           expectations,
           ranges,
           fractions,
           histogramRangeSteps,
           totalDocCount,
-          fieldValuePair
-        )
+          fieldValuePair,
+        })
       )
     )
   );
@@ -80,6 +106,7 @@ export const fetchSignificantCorrelations = async (
   const latencyCorrelations = fulfilled.filter(
     (d) => d && 'histogram' in d
   ) as LatencyCorrelation[];
+
   let fallbackResult: LatencyCorrelation | undefined =
     latencyCorrelations.length > 0
       ? undefined
@@ -103,12 +130,20 @@ export const fetchSignificantCorrelations = async (
           }, undefined);
   if (latencyCorrelations.length === 0 && fallbackResult) {
     const { fieldName, fieldValue } = fallbackResult;
-    const logHistogram = await fetchTransactionDurationRanges(
-      esClient,
-      paramsWithIndex,
-      histogramRangeSteps,
-      [{ fieldName, fieldValue }]
-    );
+    const logHistogram = await fetchDurationRanges({
+      setup,
+      eventType,
+      start,
+      end,
+      environment,
+      kuery,
+      query: {
+        bool: {
+          filter: [query, ...termQuery(fieldName, fieldValue)],
+        },
+      },
+      rangeSteps: histogramRangeSteps,
+    });
 
     if (fallbackResult) {
       fallbackResult = {
@@ -118,8 +153,9 @@ export const fetchSignificantCorrelations = async (
     }
   }
 
-  const ccsWarning =
-    rejected.length > 0 && paramsWithIndex?.index.includes(':');
+  const index = setup.indices[eventType as keyof typeof setup.indices];
+
+  const ccsWarning = rejected.length > 0 && index.includes(':');
 
   return {
     latencyCorrelations,
