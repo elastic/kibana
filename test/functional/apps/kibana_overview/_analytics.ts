527eb3c5c8f33606d123da1ebdfa7d5757991623
Maja Grubic <maja.grubic@elastic.co>
2022-06-24 10:58:31 +0200

    Add logging

--- test/functional/apps/kibana_overview/_analytics.ts
+++ test/functional/apps/kibana_overview/_analytics.ts
@@ -23,6 +23,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await kibanaServer.importExport.load(
         'test/functional/fixtures/kbn_archiver/kibana_sample_data_flights_index_pattern'
       );
+      log.error('Navigating to kibana_overview...');
+      await PageObjects.common.navigateToApp('kibana_overview');
+      log.error('Waiting until loading has finished...');
+      await PageObjects.header.waitUntilLoadingHasFinished();
+      log.error('Done...');
     });
 
     after(async () => {
@@ -50,6 +55,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
 
     it('click on a card should lead to the appropriate app', async () => {
+      log.error('Starting a test');
       const kbnOverviewAppsCards = await find.allByCssSelector('.kbnOverviewApps__item');
       const dashboardCard = kbnOverviewAppsCards.at(0);
       log.debug('kbnOverviewAppsCards ' + kbnOverviewAppsCards.length);
