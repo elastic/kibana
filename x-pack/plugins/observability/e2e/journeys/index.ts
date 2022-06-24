457d8d1f780d7b2ab8cf45a0ffb3767956597e99
shahzad31 <shahzad.muhammad@elastic.co>
2022-06-24 20:32:00 +0200

    Add tests

---
+++ x-pack/plugins/observability/e2e/journeys/index.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export * from './exploratory_view';
+
+// const seriesUrl = createExploratoryViewUrl({
+//   reportType: 'kpi-over-time',
+//   allSeries: [
+//     {
+//       name: 'elastic-page-view',
+//       dataType: 'ux',
+//       time: {
+//         from: '2021-01-18T12:20:01.682Z',
+//         to: '2021-01-18T12:25:27.484Z',
+//       },
+//       selectedMetricField: '___records___',
+//       reportDefinitions: { 'service.name': ['elastic-co-frontend'] },
+//     },
+//   ],
+// });
