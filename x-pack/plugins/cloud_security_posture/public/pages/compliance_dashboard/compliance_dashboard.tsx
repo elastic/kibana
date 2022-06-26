c68235dfef32eb2218eab17d8ea62bd68ef4fc23
Jordan <51442161+JordanSh@users.noreply.github.com>
2022-06-26 12:39:55 +0300

    [Cloud Posture] I18n guidelines dashboard (#134998)

--- x-pack/plugins/cloud_security_posture/public/pages/compliance_dashboard/compliance_dashboard.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/compliance_dashboard/compliance_dashboard.tsx
@@ -9,18 +9,24 @@ import React from 'react';
 import { EuiSpacer, EuiIcon } from '@elastic/eui';
 import { type KibanaPageTemplateProps } from '@kbn/kibana-react-plugin/public';
 import { UseQueryResult } from 'react-query';
+import { i18n } from '@kbn/i18n';
+import { DASHBOARD_PAGE_HEADER, MISSING_FINDINGS_NO_DATA_CONFIG } from './test_subjects';
 import { allNavigationItems } from '../../common/navigation/constants';
 import { useCspBreadcrumbs } from '../../common/navigation/use_csp_breadcrumbs';
 import { SummarySection } from './dashboard_sections/summary_section';
 import { BenchmarksSection } from './dashboard_sections/benchmarks_section';
 import { useComplianceDashboardDataApi } from '../../common/api';
 import { CspPageTemplate } from '../../components/csp_page_template';
-import { CLOUD_POSTURE, NO_DATA_CONFIG_TEXT } from './translations';
 import { useCspSetupStatusApi } from '../../common/api/use_setup_status_api';
 
 const getNoDataConfig = (onClick: () => void): KibanaPageTemplateProps['noDataConfig'] => ({
-  pageTitle: NO_DATA_CONFIG_TEXT.PAGE_TITLE,
-  solution: NO_DATA_CONFIG_TEXT.SOLUTION,
+  'data-test-subj': MISSING_FINDINGS_NO_DATA_CONFIG,
+  pageTitle: i18n.translate('xpack.csp.dashboard.noDataConfig.pageTitle', {
+    defaultMessage: 'Cloud Posture Dashboard',
+  }),
+  solution: i18n.translate('xpack.csp.dashboard.noDataConfig.solutionNameTitle', {
+    defaultMessage: 'Cloud Security Posture',
+  }),
   // TODO: Add real docs link once we have it
   docsLink: 'https://www.elastic.co/guide/index.html',
   logo: 'logoSecurity',
@@ -28,8 +34,13 @@ const getNoDataConfig = (onClick: () => void): KibanaPageTemplateProps['noDataCo
     dashboardNoDataCard: {
       icon: <EuiIcon type="refresh" size="xxl" />,
       onClick,
-      title: NO_DATA_CONFIG_TEXT.BUTTON_TITLE,
-      description: NO_DATA_CONFIG_TEXT.DESCRIPTION,
+      title: i18n.translate('xpack.csp.dashboard.noDataConfig.actionTitle', {
+        defaultMessage: 'Try Again',
+      }),
+      description: i18n.translate('xpack.csp.dashboard.noDataConfig.actionDescription', {
+        defaultMessage:
+          "The cloud posture dashboard can't be presented since there are no findings. This can happen due to the agent not being installed yet, or since data is still being processed.",
+      }),
     },
   },
 });
@@ -46,7 +57,12 @@ export const ComplianceDashboard = () => {
 
   return (
     <CspPageTemplate
-      pageHeader={{ pageTitle: CLOUD_POSTURE }}
+      pageHeader={{
+        'data-test-subj': DASHBOARD_PAGE_HEADER,
+        pageTitle: i18n.translate('xpack.csp.dashboard.cspPageTemplate.pageTitle', {
+          defaultMessage: 'Cloud Posture',
+        }),
+      }}
       restrictWidth={1600}
       query={pageQuery}
       noDataConfig={!isFindingsIndexApplicable ? getNoDataConfig(getInfo.refetch) : undefined}
