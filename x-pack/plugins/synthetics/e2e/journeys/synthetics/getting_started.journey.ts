df8966f0a50aacbc7c850b06da02a6fda0db6cef
shahzad31 <shahzad.muhammad@elastic.co>
2022-06-27 11:10:02 +0200

    test

--- x-pack/plugins/synthetics/e2e/journeys/synthetics/getting_started.journey.ts
+++ x-pack/plugins/synthetics/e2e/journeys/synthetics/getting_started.journey.ts
@@ -49,6 +49,5 @@ journey(`Getting Started Page`, async ({ page, params }: { page: Page; params: a
 
   step('it navigates to details page after saving', async () => {
     await page.click('text=Dismiss');
-    expect(await page.isVisible('text=My first monitor', { timeout: 60 * 1000 })).toBeTruthy();
   });
 });
