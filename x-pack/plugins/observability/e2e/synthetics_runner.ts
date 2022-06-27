ffd8f630cc339f7163a1e176940b9aa5c9e1a036
shahzad31 <shahzad.muhammad@elastic.co>
2022-06-27 21:39:12 +0200

    up

--- x-pack/plugins/observability/e2e/synthetics_runner.ts
+++ x-pack/plugins/observability/e2e/synthetics_runner.ts
@@ -85,8 +85,6 @@ export class SyntheticsRunner {
       playwrightOptions: { headless, chromiumSandbox: false, timeout: 60 * 1000 },
       match: match === 'undefined' ? '' : match,
       pauseOnError,
-      screenshots: 'on',
-      ssblocks: false,
     });
 
     await this.assertResults(results);
