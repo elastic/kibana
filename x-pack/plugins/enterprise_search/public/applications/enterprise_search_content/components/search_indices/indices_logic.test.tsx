49ef71e4e6ccea7a0221c1ff1d4c0d1d3fae52bb
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2022-06-27 19:15:44 +0200

    Fix tests

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_indices/indices_logic.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_indices/indices_logic.test.tsx
@@ -8,7 +8,7 @@
 import { LogicMounter, mockFlashMessageHelpers } from '../../../__mocks__/kea_logic';
 import { searchIndices } from '../../__mocks__/search_indices.mock';
 
-import { HttpError } from '../../../../../common/types/api';
+import { HttpError, Status } from '../../../../../common/types/api';
 
 import { DEFAULT_META } from '../../../shared/constants';
 
@@ -17,10 +17,13 @@ import { IndicesAPILogic } from '../../logic/indices_api/indices_api_logic';
 import { IndicesLogic } from './indices_logic';
 
 const DEFAULT_VALUES = {
-  isLoading: true,
+  data: undefined,
   indices: [],
+  isLoading: false,
   meta: DEFAULT_META,
+  status: Status.IDLE,
 };
+
 describe('IndicesLogic', () => {
   const { mount: apiLogicMount } = new LogicMounter(IndicesAPILogic);
   const { mount } = new LogicMounter(IndicesLogic);
@@ -53,18 +56,6 @@ describe('IndicesLogic', () => {
     });
   });
   describe('reducers', () => {
-    describe('indices', () => {
-      it('updates when apiSuccess listener triggered', () => {
-        expect(IndicesLogic.values).toEqual(DEFAULT_VALUES);
-        IndicesLogic.actions.apiSuccess({ indices: searchIndices, meta: DEFAULT_META });
-        expect(IndicesLogic.values).toEqual({
-          isLoading: false,
-          indices: searchIndices,
-          meta: DEFAULT_META,
-        });
-      });
-    });
-
     describe('meta', () => {
       it('updates when apiSuccess listener triggered', () => {
         const newMeta = {
@@ -78,9 +69,14 @@ describe('IndicesLogic', () => {
         expect(IndicesLogic.values).toEqual(DEFAULT_VALUES);
         IndicesLogic.actions.apiSuccess({ indices: searchIndices, meta: newMeta });
         expect(IndicesLogic.values).toEqual({
-          isLoading: false,
+          data: {
+            indices: searchIndices,
+            meta: newMeta,
+          },
           indices: searchIndices,
+          isLoading: false,
           meta: newMeta,
+          status: Status.SUCCESS,
         });
       });
     });
@@ -95,6 +91,26 @@ describe('IndicesLogic', () => {
       IndicesLogic.actions.apiError({} as HttpError);
       expect(mockFlashMessageHelpers.flashAPIErrors).toHaveBeenCalledTimes(1);
       expect(mockFlashMessageHelpers.flashAPIErrors).toHaveBeenCalledWith({});
+    });
+  });
+
+  describe('selectors', () => {
+    describe('indices', () => {
+      it('updates when apiSuccess listener triggered', () => {
+        expect(IndicesLogic.values).toEqual(DEFAULT_VALUES);
+        IndicesLogic.actions.apiSuccess({ indices: searchIndices, meta: DEFAULT_META });
+
+        expect(IndicesLogic.values).toEqual({
+          data: {
+            indices: searchIndices,
+            meta: DEFAULT_META,
+          },
+          indices: searchIndices,
+          isLoading: false,
+          meta: DEFAULT_META,
+          status: Status.SUCCESS,
+        });
+      });
     });
   });
 });
