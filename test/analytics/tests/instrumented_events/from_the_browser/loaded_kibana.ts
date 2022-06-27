e1548e0bd8f39c2f66b02d4928c7a5a669d1f3ef
Liza Katz <lizka.k@gmail.com>
2022-06-27 18:13:53 +0300

    Report kibana protocol (#135202)
    * kibana protocol
    
    * oops
    
    * Update loaded_kibana.ts
    
    * Update core_system.ts

--- test/analytics/tests/instrumented_events/from_the_browser/loaded_kibana.ts
+++ test/analytics/tests/instrumented_events/from_the_browser/loaded_kibana.ts
@@ -24,6 +24,8 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       expect(event.event_type).to.eql('Loaded Kibana');
       expect(event.properties).to.have.property('kibana_version');
       expect(event.properties.kibana_version).to.be.a('string');
+      expect(event.properties).to.have.property('protocol');
+      expect(event.properties.protocol).to.be.a('string');
 
       // Kibana Loaded timings
       expect(event.properties).to.have.property('load_started');
