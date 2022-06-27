0fa20eb954586ad4ba769e1b67a5a67b67a0db24
shahzad31 <shahzad.muhammad@elastic.co>
2022-06-27 11:58:30 +0200

    update

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_summary/tabs_content/step_duration_panel.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_summary/tabs_content/step_duration_panel.tsx
@@ -6,10 +6,12 @@
  */
 
 import React from 'react';
-import { EuiPanel, EuiTitle } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiText, EuiTitle } from '@elastic/eui';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
 import { ReportTypes } from '@kbn/observability-plugin/public';
 import { useParams } from 'react-router-dom';
+import { i18n } from '@kbn/i18n';
+
 import { ClientPluginsStart } from '../../../../../plugin';
 export const StepDurationPanel = () => {
   const { observability } = useKibana<ClientPluginsStart>().services;
@@ -20,24 +22,43 @@ export const StepDurationPanel = () => {
 
   return (
     <EuiPanel>
-      <EuiTitle size="xs">
-        <h3>Duration by step</h3>
-      </EuiTitle>
+      <EuiFlexGroup alignItems="center" gutterSize="s">
+        <EuiFlexItem grow={false}>
+          <EuiTitle size="xs">
+            <h3>{DURATION_BY_STEP_LABEL}</h3>
+          </EuiTitle>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiText size="s" color="subdued">
+            {LAST_24H_LABEL}
+          </EuiText>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+
       <ExploratoryViewEmbeddable
         customHeight={'300px'}
         reportType={ReportTypes.KPI}
         attributes={[
           {
-            name: 'Step duration',
+            name: DURATION_BY_STEP_LABEL,
             reportDefinitions: { 'monitor.id': [monitorId] },
             selectedMetricField: 'synthetics.step.duration.us',
             dataType: 'synthetics',
-            time: { from: 'now-12h/h', to: 'now' },
+            time: { from: 'now-24h/h', to: 'now' },
             breakdown: 'synthetics.step.name.keyword',
             operationType: 'last_value',
+            seriesType: 'area_stacked',
           },
         ]}
       />
     </EuiPanel>
   );
 };
+
+const DURATION_BY_STEP_LABEL = i18n.translate('xpack.synthetics.detailsPanel.durationByStep', {
+  defaultMessage: 'Duration by step',
+});
+
+const LAST_24H_LABEL = i18n.translate('xpack.synthetics.detailsPanel.last24Hours', {
+  defaultMessage: 'Last 24 hours',
+});
