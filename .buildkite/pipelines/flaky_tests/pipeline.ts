c9be1e147fd35392db6e15c8ff21887bea3acb5c
Brian Seeders <brian.seeders@elastic.co>
2022-06-28 13:19:05 -0400

    [CI] Move kibana-buildkite-library into kibana (#134787)
    (cherry picked from commit 0b0b68786a7c66e27cfd8b6379de035dc3a11ebd)
    
    # Conflicts:
    #	.buildkite/package-lock.json
    #	.buildkite/package.json
    #	.buildkite/pipelines/on_merge.yml
    #	.buildkite/scripts/pipelines/pull_request/pipeline.ts
    #	.buildkite/scripts/steps/code_coverage/clean_coverage_paths.ts
    #	.buildkite/scripts/steps/code_coverage/util.sh
    #	.buildkite/scripts/steps/es_snapshots/promote.sh
    #	.buildkite/scripts/steps/trigger_pipeline.ts

--- .buildkite/pipelines/flaky_tests/pipeline.ts
+++ .buildkite/pipelines/flaky_tests/pipeline.ts
@@ -6,6 +6,8 @@
  * Side Public License, v 1.
  */
 
+import { groups } from './groups.json';
+
 const configJson = process.env.KIBANA_FLAKY_TEST_RUNNER_CONFIG;
 if (!configJson) {
   console.error('+++ Triggering directly is not supported anymore');
@@ -15,10 +17,6 @@ if (!configJson) {
   console.error('\n    https://ci-stats.kibana.dev/trigger_flaky_test_runner\n');
   process.exit(1);
 }
-
-const groups = /** @type {Array<{key: string, name: string, ciGroups: number }>} */ (
-  require('./groups.json').groups
-);
 
 const concurrency = process.env.KIBANA_FLAKY_TEST_CONCURRENCY
   ? parseInt(process.env.KIBANA_FLAKY_TEST_CONCURRENCY, 10)
@@ -33,8 +31,8 @@ if (Number.isNaN(concurrency)) {
 const BASE_JOBS = 1;
 const MAX_JOBS = 500;
 
-function getTestSuitesFromJson(json) {
-  const fail = (errorMsg) => {
+function getTestSuitesFromJson(json: string) {
+  const fail = (errorMsg: string) => {
     console.error('+++ Invalid test config provided');
     console.error(`${errorMsg}: ${json}`);
     process.exit(1);
@@ -108,7 +106,7 @@ if (totalJobs > MAX_JOBS) {
   process.exit(1);
 }
 
-const steps = [];
+const steps: any[] = [];
 const pipeline = {
   env: {
     IGNORE_SHIP_CI_STATS_ERROR: 'true',
@@ -137,7 +135,7 @@ for (const testSuite of testSuites) {
       },
       label: `FTR Config: ${testSuite.ftrConfig}`,
       parallelism: testSuite.count,
-      concurrency: concurrency,
+      concurrency,
       concurrency_group: process.env.UUID,
       concurrency_method: 'eager',
       agents: {
@@ -159,7 +157,7 @@ for (const testSuite of testSuites) {
   switch (keyParts[0]) {
     case 'cypress':
       const CYPRESS_SUITE = keyParts[1];
-      const group = groups.find((group) => group.key.includes(CYPRESS_SUITE));
+      const group = groups.find((g) => g.key.includes(CYPRESS_SUITE));
       if (!group) {
         throw new Error(
           `Group configuration was not found in groups.json for the following cypress suite: {${CYPRESS_SUITE}}.`
@@ -171,7 +169,7 @@ for (const testSuite of testSuites) {
         agents: { queue: 'ci-group-6' },
         depends_on: 'build',
         parallelism: testSuite.count,
-        concurrency: concurrency,
+        concurrency,
         concurrency_group: process.env.UUID,
         concurrency_method: 'eager',
       });
