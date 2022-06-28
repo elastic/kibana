149de1f9fd7f2b7c908952680ae75358cbd5bd78
shahzad31 <shahzad.muhammad@elastic.co>
2022-06-28 12:08:09 +0200

    add availability metric

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_summary/tabs_content/summary_tab_content.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_summary/tabs_content/summary_tab_content.tsx
@@ -6,22 +6,43 @@
  */
 
 import React from 'react';
-import { EuiTitle, EuiPanel } from '@elastic/eui';
+import { EuiTitle, EuiPanel, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
+import { AvailabilityPanel } from './availability_panel';
 import { MonitorDetailsPanel } from './monitor_details_panel';
 
 export const SummaryTabContent = () => {
   return (
-    <EuiPanel>
-      <EuiTitle size="s">
-        <h3>{MONITOR_DETAILS_LABEL}</h3>
-      </EuiTitle>
-      <MonitorDetailsPanel />
-    </EuiPanel>
+    <EuiFlexGroup>
+      <EuiFlexItem grow={1}>
+        <EuiPanel>
+          <EuiTitle size="s">
+            <h3>{MONITOR_DETAILS_LABEL}</h3>
+          </EuiTitle>
+          <MonitorDetailsPanel />
+        </EuiPanel>
+      </EuiFlexItem>
+      <EuiFlexItem grow={2}>
+        <EuiFlexGroup>
+          <EuiFlexItem style={{ width: 200, height: 150 }}>
+            <EuiPanel style={{ width: 200, height: 200 }}>
+              <EuiTitle size="xs">
+                <h3>{LAST_30DAYS_LABEL}</h3>
+              </EuiTitle>
+              <AvailabilityPanel />
+            </EuiPanel>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 };
 
 const MONITOR_DETAILS_LABEL = i18n.translate('xpack.synthetics.detailsPanel.monitorDetails', {
   defaultMessage: 'Monitor details',
+});
+
+const LAST_30DAYS_LABEL = i18n.translate('xpack.synthetics.detailsPanel.last30Days', {
+  defaultMessage: 'Last 30 days',
 });
