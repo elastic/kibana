45be641abec02ea903ebf4b8008ce71154bdfec4
Tim Sullivan <tsullivan@users.noreply.github.com>
2022-06-29 13:49:50 -0700

    [reporting] functional test: increase allowance for visual difference… (#135356)
    * [reporting] functional test: increase allowance for visual difference in downloaded report
    
    * Revert "skip flaky suite (#135309)"
    
    This reverts commit b556857350d987d1adab928be92d491aa210c45f.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/reporting_functional/reporting_and_timeout/index.ts
+++ x-pack/test/reporting_functional/reporting_and_timeout/index.ts
@@ -17,8 +17,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const browser = getService('browser');
   const compareImages = getService('compareImages');
 
-  // FLAKY: https://github.com/elastic/kibana/issues/135309
-  describe.skip('Reporting Functional Tests with forced timeout', function () {
+  describe('Reporting Functional Tests with forced timeout', function () {
     const dashboardTitle = 'Ecom Dashboard Hidden Panel Titles';
     const baselineAPng = path.resolve(__dirname, 'fixtures/baseline/warnings_capture_a.png');
     const sessionPng = 'warnings_capture_session_a';
@@ -51,8 +50,9 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       const captureData = await PageObjects.reporting.getRawPdfReportData(url);
       const pngSessionFilePath = await compareImages.writeToSessionFile(sessionPng, captureData);
 
+      // allow minor visual differences: https://github.com/elastic/kibana/issues/135309#issuecomment-1169095186
       expect(await compareImages.checkIfPngsMatch(pngSessionFilePath, baselineAPng)).to.be.lessThan(
-        0.01
+        0.011
       );
     });
   });
