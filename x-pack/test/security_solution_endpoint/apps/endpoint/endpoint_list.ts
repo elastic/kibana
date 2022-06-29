e7d93b4be7ab793e637e1de121f148d3ade30368
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-06-29 13:26:53 -0400

    skip failing test suite (#114249)

--- x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_list.ts
+++ x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_list.ts
@@ -72,7 +72,8 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     return tableData;
   };
 
-  describe('endpoint list', function () {
+  // Failing: See https://github.com/elastic/kibana/issues/114249
+  describe.skip('endpoint list', function () {
     const sleep = (ms = 100) => new Promise((resolve) => setTimeout(resolve, ms));
     let indexedData: IndexedHostsAndAlertsResponse;
     describe('when initially navigating to page', () => {
