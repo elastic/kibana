3a6546a2403f62bf532080c9554f899c3e04eb18
Julia Bardi <90178898+juliaElastic@users.noreply.github.com>
2022-06-29 16:13:11 +0200

    [Fleet] add upload report to cypress tests (#135282)
    * add upload report to cypress tests
    
    * added missing cypress reporter
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- .buildkite/scripts/lifecycle/post_command.sh
+++ .buildkite/scripts/lifecycle/post_command.sh
@@ -14,6 +14,7 @@ if [[ "$IS_TEST_EXECUTION_STEP" == "true" ]]; then
   buildkite-agent artifact upload 'target/kibana-coverage/functional/**/*'
   buildkite-agent artifact upload 'target/kibana-*'
   buildkite-agent artifact upload 'target/kibana-security-solution/**/*.png'
+  buildkite-agent artifact upload 'target/kibana-fleet/**/*.png'
   buildkite-agent artifact upload 'target/test-metrics/*'
   buildkite-agent artifact upload 'target/test-suites-ci-plan.json'
   buildkite-agent artifact upload 'test/**/screenshots/diff/*.png'
