21d1fcdfb98500ce52a924c4a212a757ccee5508
Maxim Kholod <maxim.kholod@elastic.co>
2022-06-29 10:29:01 +0200

    remove unneeded return from EmptyPage component

--- x-pack/plugins/threat_intelligence/public/components/empty_page/empty_page.tsx
+++ x-pack/plugins/threat_intelligence/public/components/empty_page/empty_page.tsx
@@ -17,44 +17,38 @@ interface EmptyPageProps {
   integrationsPageLink: string;
 }
 
-export const EmptyPage: VFC<EmptyPageProps> = ({ integrationsPageLink }) => {
-  return (
-    <EuiEmptyPrompt
-      icon={
-        <EuiImage
-          size="fullWidth"
-          alt="Enable Threat Intelligence Integrations"
-          src={illustration}
-        />
-      }
-      title={<h3>Get started with Elastic Threat intelligence</h3>}
-      titleSize="s"
-      layout="horizontal"
-      color="transparent"
-      body={
-        <>
-          <p>
-            Elastic Threat Intelligence makes it easy to analyze and investigate potential security
-            threats by aggregating data from multiple sources in one place.
-          </p>
-          <p>
-            You’ll be able to view data from all activated threat intelligence feeds and take action
-            from this page.
-          </p>
-          <p>
-            To get started with Elastic Threat Intelligence, enable one or more Threat Intelligence
-            Integrations from the Integrations page. For more information, view the Security app
-            documentation.
-          </p>
-        </>
-      }
-      actions={
-        <EuiButton href={integrationsPageLink} color="primary" iconType="plusInCircle" fill>
-          Add Integrations
-        </EuiButton>
-      }
-    />
-  );
-};
+export const EmptyPage: VFC<EmptyPageProps> = ({ integrationsPageLink }) => (
+  <EuiEmptyPrompt
+    icon={
+      <EuiImage size="fullWidth" alt="Enable Threat Intelligence Integrations" src={illustration} />
+    }
+    title={<h3>Get started with Elastic Threat intelligence</h3>}
+    titleSize="s"
+    layout="horizontal"
+    color="transparent"
+    body={
+      <>
+        <p>
+          Elastic Threat Intelligence makes it easy to analyze and investigate potential security
+          threats by aggregating data from multiple sources in one place.
+        </p>
+        <p>
+          You’ll be able to view data from all activated threat intelligence feeds and take action
+          from this page.
+        </p>
+        <p>
+          To get started with Elastic Threat Intelligence, enable one or more Threat Intelligence
+          Integrations from the Integrations page. For more information, view the Security app
+          documentation.
+        </p>
+      </>
+    }
+    actions={
+      <EuiButton href={integrationsPageLink} color="primary" iconType="plusInCircle" fill>
+        Add Integrations
+      </EuiButton>
+    }
+  />
+);
 
 EmptyPage.displayName = displayName;
