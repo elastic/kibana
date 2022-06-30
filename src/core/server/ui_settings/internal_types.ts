4b7e8c679652de1444dc25f09648e66512d9089d
pgayvallet <pierre.gayvallet@elastic.co>
2022-06-30 09:36:43 +0200

    adapt uiSettings route definitions

---
+++ src/core/server/ui_settings/internal_types.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import type { RequestHandlerContextBase } from '..';
+import type { IRouter } from '../http';
+import type { UiSettingsRequestHandlerContext } from './ui_settings_route_handler_context';
+
+/**
+ * Request handler context only defining `core.savedObjects` and `core.elasticsearch`.
+ * Only used for module isolation purposes.
+ * @internal
+ */
+export interface InternalUiSettingsRequestHandlerContext extends RequestHandlerContextBase {
+  core: Promise<{
+    uiSettings: UiSettingsRequestHandlerContext;
+  }>;
+}
+
+/**
+ * Router bound to the {@link InternalSavedObjectsRequestHandlerContext}.
+ * Only used for module isolation purposes.
+ * @internal
+ */
+export type InternalUiSettingsRouter = IRouter<InternalUiSettingsRequestHandlerContext>;
