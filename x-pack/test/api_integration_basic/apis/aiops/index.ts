c684889b66f67628d5635adf5cd358a080aa3223
Walter Rafelsberger <walter@elastic.co>
2022-06-30 09:58:13 +0200

    [ML] Adds tests to check 403 forbidden response without proper license.

--- x-pack/test/api_integration_basic/apis/aiops/index.ts
+++ x-pack/test/api_integration_basic/apis/aiops/index.ts
@@ -5,16 +5,13 @@
  * 2.0.
  */
 
-import { AIOPS_ENABLED } from '@kbn/aiops-plugin/common';
-
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
-  describe('AIOps', function () {
-    this.tags(['aiops']);
+  describe('aiops basic license', function () {
+    this.tags(['aiops', 'walterra']);
 
-    if (AIOPS_ENABLED) {
-      loadTestFile(require.resolve('./explain_log_rate_spikes'));
-    }
+    // The aiops API should return forbidden when called without a trial/platinum license.
+    loadTestFile(require.resolve('./permissions'));
   });
 }
