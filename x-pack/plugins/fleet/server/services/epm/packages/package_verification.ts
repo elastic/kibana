94e0b093cbf1352741a57f4abf5d474412123b45
Mark Hopkin <mark.hopkin@elastic.co>
2022-06-30 11:00:27 +0100

    [Fleet] Add GPG verify function to fleet (#135223)
    * add LGPL-3.0+ to list of allowed licenses
    
    * Add openpgp (LGPLv3 license)to kibana
    
    * Add jsdom TextEncoder and TextDecoder polyfills
    
    * Use opengpg to read gpg key
    
    * add basic verification function
    
    * add lgpl to license overrides for now
    
    * Revert "add lgpl to license overrides for now"
    
    This reverts commit 3730eb07540d8b537712267a5430085f54c088c0.
    
    * collect verification result
    
    * use Key ID of the verification key
    
    * add @openpgp/web-stream-tools as a dev dependency
    
    * verified -> isVerified
    
    * only allow LGPL-v3 for openpgp
    
    * fix: use @ as separator when checking license overrides
    
    * fix isValidIndexName test

--- x-pack/plugins/fleet/server/services/epm/packages/package_verification.ts
+++ x-pack/plugins/fleet/server/services/epm/packages/package_verification.ts
@@ -7,22 +7,23 @@
 
 import { readFile } from 'fs/promises';
 
+import * as openpgp from 'openpgp';
+import type { Logger } from '@kbn/logging';
+
 import { appContextService } from '../../app_context';
+let cachedKey: openpgp.Key | undefined | null = null;
 
-let cachedKey: string | undefined | null = null;
-
-export async function getGpgKeyOrUndefined(): Promise<string | undefined> {
+export async function getGpgKeyOrUndefined(): Promise<openpgp.Key | undefined> {
   if (cachedKey !== null) return cachedKey;
 
   cachedKey = await _readGpgKey();
   return cachedKey;
 }
 
-export async function _readGpgKey(): Promise<string | undefined> {
+export async function _readGpgKey(): Promise<openpgp.Key | undefined> {
   const config = appContextService.getConfig();
   const logger = appContextService.getLogger();
   const gpgKeyPath = config?.packageVerification?.gpgKeyPath;
-
   if (!gpgKeyPath) {
     logger.warn('GPG key path not configured at "xpack.fleet.packageVerification.gpgKeyPath"');
     return undefined;
@@ -35,8 +36,53 @@ export async function _readGpgKey(): Promise<string | undefined> {
     logger.warn(`Unable to retrieve GPG key from '${gpgKeyPath}': ${e.code}`);
     return undefined;
   }
+  let key;
+  try {
+    key = await openpgp.readKey({ armoredKey: buffer.toString() });
+  } catch (e) {
+    logger.warn(`Unable to parse GPG key from '${gpgKeyPath}': ${e}`);
+  }
 
-  const key = buffer.toString();
-  cachedKey = key;
   return key;
+}
+
+export interface PackageVerificationResult {
+  isVerified: boolean;
+  keyId: string;
+}
+
+export async function verifyPackageSignature({
+  pkgZipBuffer,
+  pkgZipSignature,
+  verificationKey,
+  logger,
+}: {
+  pkgZipBuffer: Buffer;
+  pkgZipSignature: string;
+  verificationKey: openpgp.Key;
+  logger: Logger;
+}): Promise<PackageVerificationResult> {
+  const signature = await openpgp.readSignature({
+    armoredSignature: pkgZipSignature,
+  });
+
+  const message = await openpgp.createMessage({
+    binary: pkgZipBuffer,
+  });
+
+  const verificationResult = await openpgp.verify({
+    verificationKeys: verificationKey,
+    signature,
+    message,
+  });
+
+  const signatureVerificationResult = verificationResult.signatures[0];
+
+  let isVerified = false;
+  try {
+    isVerified = await signatureVerificationResult.verified;
+  } catch (e) {
+    logger.error(`Error verifying package signature: ${e}`);
+  }
+  return { isVerified, keyId: verificationKey.getKeyID().toHex() };
 }
