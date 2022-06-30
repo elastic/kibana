580e637b0df960bd3f0a2b0c83416cce0b78bddf
Sébastien Loix <sebastien.loix@elastic.co>
2022-06-30 16:18:00 +0100

    Update docs code for UseArray stories

--- src/plugins/es_ui_shared/static/forms/hook_form_lib/components/__stories__/use_array_basic.tsx
+++ src/plugins/es_ui_shared/static/forms/hook_form_lib/components/__stories__/use_array_basic.tsx
@@ -98,3 +98,90 @@ export function Basic() {
 }
 
 Basic.storyName = 'Basic';
+
+Basic.parameters = {
+  docs: {
+    source: {
+      code: `
+const defaultValue = {
+  employees: [
+    {
+      name: 'John',
+      lastName: 'Snow',
+    },
+  ],
+};
+
+const MyFormComponent = () => {
+  const { form } = useForm({ defaultValue });
+
+  const submitForm = async () => {
+    const { isValid, data } = await form.submit();
+    if (isValid) {
+      // ... do something with the data
+    }
+  };
+
+  return (
+    <Form form={form}>
+      <EuiTitle>
+        <h2>Employees</h2>
+      </EuiTitle>
+      <EuiSpacer />
+      <UseArray path="employees">
+        {({ items, addItem, removeItem }) => {
+          return (
+            <>
+              {items.map(({ id, path }) => {
+                return (
+                  <EuiFlexGroup key={id} alignItems="center">
+                    <EuiFlexItem>
+                      <UseField
+                        path={\`\${path}.name\`}
+                        config={{
+                          label: 'Name',
+                          validations: [{ validator: emptyField('A name is required.') }],
+                        }}
+                        component={TextField}
+                      />
+                    </EuiFlexItem>
+                    <EuiFlexItem>
+                      <UseField
+                        path={\`\${path}.lastName\`}
+                        config={{
+                          label: 'Last name',
+                          validations: [{ validator: emptyField('A last name is required.') }],
+                        }}
+                        component={TextField}
+                      />
+                    </EuiFlexItem>
+                    {items.length > 1 && (
+                      <EuiFlexItem grow={false}>
+                        <EuiButtonIcon
+                          iconType="minusInCircle"
+                          onClick={() => removeItem(id)}
+                          aria-label="Remove item"
+                        />
+                      </EuiFlexItem>
+                    )}
+                  </EuiFlexGroup>
+                );
+              })}
+              <EuiSpacer size="m" />
+              <EuiFlexGroup justifyContent="flexEnd">
+                <EuiButtonEmpty onClick={addItem}>Add employee</EuiButtonEmpty>
+              </EuiFlexGroup>
+            </>
+          );
+        }}
+      </UseArray>
+      <EuiSpacer />
+      <EuiButton onClick={submitForm}>Send</EuiButton>
+    </Form>
+  );
+}
+      `,
+      language: 'tsx',
+    },
+  },
+};
