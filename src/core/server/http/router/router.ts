642715f66437f17c1d6acf0ea04573c9da810c6a
pgayvallet <pierre.gayvallet@elastic.co>
2022-07-01 12:28:44 +0200

    adapt imports

--- src/core/server/http/router/router.ts
+++ src/core/server/http/router/router.ts
@@ -14,7 +14,7 @@ import type { Logger } from '@kbn/logging';
 import {
   isUnauthorizedError as isElasticsearchUnauthorizedError,
   UnauthorizedError as EsNotAuthorizedError,
-} from '../../elasticsearch/client/errors';
+} from '@kbn/es-errors';
 import { KibanaRequest } from './request';
 import {
   KibanaResponseFactory,
