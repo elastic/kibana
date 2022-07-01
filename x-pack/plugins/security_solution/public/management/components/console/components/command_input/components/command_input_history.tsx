b1edd5a8d9dfcc90fcf15fca949ac1053648df16
Paul Tavares <paul.tavares@elastic.co>
2022-07-01 13:14:02 -0400

    Add filter bar to the History popout

--- x-pack/plugins/security_solution/public/management/components/console/components/command_input/components/command_input_history.tsx
+++ x-pack/plugins/security_solution/public/management/components/console/components/command_input/components/command_input_history.tsx
@@ -5,7 +5,15 @@
  * 2.0.
  */
 
-import React, { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, {
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  KeyboardEvent,
+} from 'react';
 import type { EuiSelectableOption, EuiSelectableProps } from '@elastic/eui';
 import { EuiSelectable } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
@@ -21,6 +29,16 @@ const NO_HISTORY_EMPTY_MESSAGE = i18n.translate(
   'xpack.securitySolution.commandInputHistory.noHistoryEmptyMessage',
   { defaultMessage: 'No commands have been entered' }
 );
+const FILTER_HISTORY_PLACEHOLDER = i18n.translate(
+  'xpack.securitySolution.commandInputHistory.filterPlaceholder',
+  {
+    defaultMessage: 'Filter previously entered actions',
+  }
+);
+const NO_FILTERED_MATCHES = i18n.translate(
+  'xpack.securitySolution.commandInputHistory.noFilteredMatchesFoundMessage',
+  { defaultMessage: 'No entries found matching the filter entered' }
+);
 
 export const CommandInputHistory = memo(() => {
   const dispatch = useConsoleStateDispatch();
@@ -28,6 +46,7 @@ export const CommandInputHistory = memo(() => {
   const [priorInputState] = useState(useWithInputTextEntered());
   const optionWasSelected = useRef(false);
   const getTestId = useTestIdGenerator(useDataTestSubj());
+  const showFilterBar = inputHistory.length > 9;
 
   const selectableHistoryOptions = useMemo(() => {
     return inputHistory.map<EuiSelectableProps['options'][number]>((inputItem, index) => {
@@ -39,24 +58,38 @@ export const CommandInputHistory = memo(() => {
     });
   }, [inputHistory]);
 
+  const handleEnterOnKeyDownCapture = useCallback((ev: KeyboardEvent) => {
+    // Works around an issue where the `enter` key event from the EuiSelectable gets capture
+    // by the outer `KeyCapture` input somehow. Likely due to the sequence of events between
+    // keyup, focus and the Focus trap component having the `returnFocus` on by default
+    if (ev.key === 'Enter') {
+      // @ts-expect-error
+      ev._CONSOLE_IGNORE_KEY = true;
+    }
+  }, []);
+
   const selectableListProps: EuiSelectableProps['listProps'] = useMemo(() => {
     return {
       showIcons: false,
-      onKeyDownCapture: (ev) => {
-        // Works around an issue where the `enter` key event from the EuiSelectable gets capture
-        // by the outer `KeyCapture` input somehow. Likely due to the sequence of events between
-        // keyup, focus and the Focus trap component having the `returnFocus` on by default
-        if (ev.key === 'Enter') {
-          // @ts-expect-error
-          ev._CONSOLE_IGNORE_KEY = true;
-        }
-      },
+      onKeyDownCapture: handleEnterOnKeyDownCapture,
     };
-  }, []);
+  }, [handleEnterOnKeyDownCapture]);
 
-  const renderSelectionContent: EuiSelectableProps['children'] = useCallback((list, search) => {
-    return list;
-  }, []);
+  const selectableSearchProps = useMemo(() => {
+    return {
+      placeholder: FILTER_HISTORY_PLACEHOLDER,
+      compressed: true,
+      fullWidth: true,
+      onKeyDownCapture: handleEnterOnKeyDownCapture,
+    };
+  }, [handleEnterOnKeyDownCapture]);
+
+  const renderSelectionContent: EuiSelectableProps['children'] = useCallback(
+    (list, search) => {
+      return showFilterBar ? [list, search] : list;
+    },
+    [showFilterBar]
+  );
 
   const handleSelectableOnChange: EuiSelectableProps['onChange'] = useCallback(
     (items: EuiSelectableOption[]) => {
@@ -122,7 +155,10 @@ export const CommandInputHistory = memo(() => {
       renderOption={handleRenderOption}
       listProps={selectableListProps}
       singleSelection={true}
+      searchable={true}
+      searchProps={selectableSearchProps}
       emptyMessage={NO_HISTORY_EMPTY_MESSAGE}
+      noMatchesMessage={NO_FILTERED_MATCHES}
       data-test-subj={getTestId('inputHistorySelector')}
     >
       {renderSelectionContent}
