434816386671c1aa05a11d2cf5fe4d9b9fa2b2a2
Ying Mao <ying.mao@elastic.co>
2022-07-03 14:39:49 -0400

    Apply suggestions from code review
    Co-authored-by: Gidi Meir Morris <github@gidi.io>

--- x-pack/plugins/alerting/server/saved_objects/index.ts
+++ x-pack/plugins/alerting/server/saved_objects/index.ts
@@ -22,9 +22,9 @@ import { isRuleExportable } from './is_rule_exportable';
 import { RuleTypeRegistry } from '../rule_type_registry';
 export { partiallyUpdateAlert } from './partially_update_alert';
 
-// Use caution when removing items from this array! If the field
-// exists in the rule SO, it needs to be included in this array to
-// prevent decryption failures during migration.
+// Use caution when removing items from this array! Any field which has
+// ever existed in the rule SO must be included in this array to prevent
+// decryption failures during migration.
 export const AlertAttributesExcludedFromAAD = [
   'scheduledTaskId',
   'muteAll',
@@ -33,7 +33,7 @@ export const AlertAttributesExcludedFromAAD = [
   'updatedAt',
   'executionStatus',
   'monitoring',
-  'snoozeEndTime',
+  'snoozeEndTime', // field removed in 8.2, but must be retained in case an rule created/updated in 8.2 is being migrated
   'snoozeSchedule',
   'isSnoozedUntil',
 ];
