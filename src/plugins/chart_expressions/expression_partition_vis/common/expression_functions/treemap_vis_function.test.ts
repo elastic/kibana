85e72e4b1befe27cd167da9c31ea372c0aad88d4
Alexey Antonov <alexwizp@gmail.com>
2022-07-04 12:43:59 +0300

    update tests

--- src/plugins/chart_expressions/expression_partition_vis/common/expression_functions/treemap_vis_function.test.ts
+++ src/plugins/chart_expressions/expression_partition_vis/common/expression_functions/treemap_vis_function.test.ts
@@ -17,6 +17,7 @@ import { ExpressionValueVisDimension } from '@kbn/visualizations-plugin/common';
 import { Datatable } from '@kbn/expressions-plugin/common/expression_types/specs';
 import { treemapVisFunction } from './treemap_vis_function';
 import { PARTITION_LABELS_VALUE } from '../constants';
+import { ExecutionContext } from '@kbn/expressions-plugin';
 
 describe('interpreter/functions#treemapVis', () => {
   const fn = functionWrapper(treemapVisFunction());
@@ -138,8 +139,10 @@ describe('interpreter/functions#treemapVis', () => {
           reset: () => {},
         },
       },
-    };
-    await fn(context, visConfig, handlers as any);
+      getExecutionContext: jest.fn(),
+    } as unknown as ExecutionContext;
+
+    await fn(context, visConfig, handlers);
 
     expect(loggedTable!).toMatchSnapshot();
   });
