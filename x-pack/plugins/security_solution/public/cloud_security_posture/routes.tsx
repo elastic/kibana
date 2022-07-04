84a5fd2310ebe1050f643e89f6d42b4027a9a0d2
Ari Aviran <ari.aviran@elastic.co>
2022-07-04 14:56:38 +0300

    Fix navbar highlighting

--- x-pack/plugins/security_solution/public/cloud_security_posture/routes.tsx
+++ x-pack/plugins/security_solution/public/cloud_security_posture/routes.tsx
@@ -10,14 +10,20 @@ import React from 'react';
 import { TrackApplicationView } from '@kbn/usage-collection-plugin/public';
 import { SecuritySolutionPageWrapper } from '../common/components/page_wrapper';
 import { SpyRoute } from '../common/utils/route/spy_routes';
-import { CLOUD_SECURITY_POSTURE_PATH, SecurityPageName } from '../../common/constants';
+import {
+  CLOUD_SECURITY_POSTURE_BENCHMARKS_PATH,
+  CLOUD_SECURITY_POSTURE_DASHBOARD_PATH,
+  CLOUD_SECURITY_POSTURE_FINDINGS_PATH,
+  CLOUD_SECURITY_POSTURE_PATH,
+  SecurityPageName,
+} from '../../common/constants';
 import type { SecuritySubPluginRoutes } from '../app/types';
 
-const CloudSecurityPosture = () => {
+const CloudSecurityPosture = ({ pageName }: { pageName: SecurityPageName }) => {
   return (
-    <TrackApplicationView viewId={SecurityPageName.cloudSecurityPosture}>
+    <TrackApplicationView viewId={pageName}>
       <SecuritySolutionPageWrapper noPadding noTimeline>
-        <SpyRoute pageName={SecurityPageName.cloudSecurityPosture} />
+        <SpyRoute pageName={pageName} />
         <EuiTitle>
           <h1>{'Coming soon'}</h1>
         </EuiTitle>
@@ -26,9 +32,29 @@ const CloudSecurityPosture = () => {
   );
 };
 
+// TODO: We'll probably use a single route here, and we'll manage all CSP pages in an internal router in the CSP plugin.
+//  For now we have multiple routes as we need `SpyRoute` to use a specific `pageName` for highlighting the correct
+//  navigation bar entry
 export const routes: SecuritySubPluginRoutes = [
   {
     path: CLOUD_SECURITY_POSTURE_PATH,
-    render: () => <CloudSecurityPosture />,
+    render: () => <CloudSecurityPosture pageName={SecurityPageName.cloudSecurityPosture} />,
+    exact: true,
+  },
+  {
+    path: CLOUD_SECURITY_POSTURE_FINDINGS_PATH,
+    render: () => <CloudSecurityPosture pageName={SecurityPageName.cloudSecurityPostureFindings} />,
+  },
+  {
+    path: CLOUD_SECURITY_POSTURE_DASHBOARD_PATH,
+    render: () => (
+      <CloudSecurityPosture pageName={SecurityPageName.cloudSecurityPostureDashboard} />
+    ),
+  },
+  {
+    path: CLOUD_SECURITY_POSTURE_BENCHMARKS_PATH,
+    render: () => (
+      <CloudSecurityPosture pageName={SecurityPageName.cloudSecurityPostureBenchmarks} />
+    ),
   },
 ];
