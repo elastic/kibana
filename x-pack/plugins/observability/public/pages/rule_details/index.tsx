cfd8ddba4eae2a2d880876d3705597644825552e
Faisal Kanout <faisal.kanout@elastic.co>
2022-07-04 14:37:39 +0200

    Use getRuleStatusPanel in Rule Details page

--- x-pack/plugins/observability/public/pages/rule_details/index.tsx
+++ x-pack/plugins/observability/public/pages/rule_details/index.tsx
@@ -6,7 +6,6 @@
  */
 
 import React, { useState, useEffect, useCallback, useRef } from 'react';
-import moment from 'moment';
 import { useParams } from 'react-router-dom';
 import { i18n } from '@kbn/i18n';
 import {
@@ -20,10 +19,8 @@ import {
   EuiPanel,
   EuiTitle,
   EuiPopover,
-  EuiHorizontalRule,
   EuiTabbedContent,
   EuiEmptyPrompt,
-  EuiLoadingSpinner,
   EuiSuperSelectOption,
 } from '@elastic/eui';
 
@@ -39,12 +36,11 @@ import {
   RuleEventLogListProps,
 } from '@kbn/triggers-actions-ui-plugin/public';
 // TODO: use a Delete modal from triggersActionUI when it's sharable
-import { ALERTS_FEATURE_ID } from '@kbn/alerting-plugin/common';
-
+import { ALERTS_FEATURE_ID, RuleExecutionStatusErrorReasons } from '@kbn/alerting-plugin/common';
 import { AlertConsumers } from '@kbn/rule-data-utils';
 import { DeleteModalConfirmation } from '../rules/components/delete_modal_confirmation';
 import { CenterJustifiedSpinner } from '../rules/components/center_justified_spinner';
-import { OBSERVABILITY_SOLUTIONS } from '../rules/config';
+import { getHealthColor, OBSERVABILITY_SOLUTIONS } from '../rules/config';
 import { RuleDetailsPathParams, EVENT_LOG_LIST_TAB, ALERT_LIST_TAB } from './types';
 import { useBreadcrumbs } from '../../hooks/use_breadcrumbs';
 import { usePluginContext } from '../../hooks/use_plugin_context';
@@ -52,12 +48,11 @@ import { useFetchRule } from '../../hooks/use_fetch_rule';
 import { RULES_BREADCRUMB_TEXT } from '../rules/translations';
 import { PageTitle, ItemTitleRuleSummary, ItemValueRuleSummary, Actions } from './components';
 import { useKibana } from '../../utils/kibana_react';
-import { useFetchLast24hAlerts } from '../../hooks/use_fetch_last24h_alerts';
-import { useFetchLast24hRuleExecutionLog } from '../../hooks/use_fetch_last24h_rule_execution_log';
 import { formatInterval } from './utils';
 import { hasExecuteActionsCapability, hasAllPrivilege } from './config';
 import { paths } from '../../config/paths';
 import { observabilityFeatureId } from '../../../common';
+import { ALERT_STATUS_LICENSE_ERROR, rulesStatusesTranslationsMapping } from './translations';
 
 export function RuleDetailsPage() {
   const {
@@ -70,6 +65,7 @@ export function RuleDetailsPage() {
       actionTypeRegistry,
       getRuleEventLogList,
       getAlertsStateTable,
+      getRuleStatusPanel,
     },
     application: { capabilities, navigateToUrl },
     notifications: { toasts },
@@ -78,21 +74,13 @@ export function RuleDetailsPage() {
   const { ruleId } = useParams<RuleDetailsPathParams>();
   const { ObservabilityPageTemplate } = usePluginContext();
   const { isRuleLoading, rule, errorRule, reloadRule } = useFetchRule({ ruleId, http });
-  const { isLoadingExecutionLog, executionLog } = useFetchLast24hRuleExecutionLog({ http, ruleId });
   const { ruleTypes, ruleTypeIndex } = useLoadRuleTypes({
     filteredSolutions: OBSERVABILITY_SOLUTIONS,
   });
-
   const [features, setFeatures] = useState<string>('');
   const [ruleType, setRuleType] = useState<RuleType<string, string>>();
   const [ruleToDelete, setRuleToDelete] = useState<string[]>([]);
   const [isPageLoading, setIsPageLoading] = useState(false);
-  const { isLoadingLast24hAlerts, last24hAlerts } = useFetchLast24hAlerts({
-    http,
-    features,
-    ruleId,
-  });
-
   const [editFlyoutVisible, setEditFlyoutVisible] = useState<boolean>(false);
   const [isRuleEditPopoverOpen, setIsRuleEditPopoverOpen] = useState(false);
 
@@ -146,6 +134,7 @@ export function RuleDetailsPage() {
   ]);
 
   const canExecuteActions = hasExecuteActionsCapability(capabilities);
+
   const canSaveRule =
     rule &&
     hasAllPrivilege(rule, ruleType) &&
@@ -244,6 +233,13 @@ export function RuleDetailsPage() {
       </EuiPanel>
     );
 
+  const isLicenseError =
+    rule.executionStatus.error?.reason === RuleExecutionStatusErrorReasons.License;
+
+  const statusMessage = isLicenseError
+    ? ALERT_STATUS_LICENSE_ERROR
+    : rulesStatusesTranslationsMapping[rule.executionStatus.status];
+
   const getRuleStatusComponent = () =>
     getRuleStatusDropdown({
       rule,
@@ -336,85 +332,15 @@ export function RuleDetailsPage() {
     >
       <EuiFlexGroup wrap={true} gutterSize="m">
         {/* Left side of Rule Summary */}
-        <EuiFlexItem data-test-subj="ruleSummaryRuleStatus" grow={1}>
-          <EuiPanel color="subdued" hasBorder={false} paddingSize={'m'}>
-            <EuiFlexGroup direction="column" gutterSize="xs">
-              <EuiFlexGroup>
-                <ItemTitleRuleSummary>
-                  {i18n.translate('xpack.observability.ruleDetails.lastRun', {
-                    defaultMessage: 'Last Run',
-                  })}
-                </ItemTitleRuleSummary>
-                <ItemValueRuleSummary
-                  extraSpace={false}
-                  itemValue={moment(rule.executionStatus.lastExecutionDate).fromNow()}
-                />
-              </EuiFlexGroup>
-              <EuiSpacer size="m" />
-              <EuiFlexGroup>
-                <ItemTitleRuleSummary>
-                  {i18n.translate('xpack.observability.ruleDetails.ruleIs', {
-                    defaultMessage: 'Rule is',
-                  })}
-                </ItemTitleRuleSummary>
-                <EuiFlexItem>{getRuleStatusComponent()}</EuiFlexItem>
-              </EuiFlexGroup>
-              <EuiHorizontalRule margin="s" />
-              <EuiFlexGroup>
-                <ItemTitleRuleSummary>
-                  {i18n.translate('xpack.observability.ruleDetails.alerts', {
-                    defaultMessage: 'Alerts',
-                  })}
-                </ItemTitleRuleSummary>
-                {isLoadingLast24hAlerts ? (
-                  <EuiFlexItem>
-                    <EuiLoadingSpinner size="s" />
-                  </EuiFlexItem>
-                ) : (
-                  <ItemValueRuleSummary
-                    extraSpace={false}
-                    itemValue={`
-                    ${String(last24hAlerts)} ${i18n.translate(
-                      'xpack.observability.ruleDetails.last24h',
-                      {
-                        defaultMessage: '(last 24 h)',
-                      }
-                    )}`}
-                  />
-                )}
-              </EuiFlexGroup>
-              <EuiSpacer size="s" />
-
-              <EuiFlexGroup>
-                <ItemTitleRuleSummary>
-                  {i18n.translate('xpack.observability.ruleDetails.execution', {
-                    defaultMessage: 'Executions',
-                  })}
-                </ItemTitleRuleSummary>
-
-                {isLoadingExecutionLog ? (
-                  <EuiFlexItem>
-                    <EuiLoadingSpinner size="s" />
-                  </EuiFlexItem>
-                ) : (
-                  <ItemValueRuleSummary
-                    extraSpace={false}
-                    itemValue={`
-                        ${String(executionLog.total)} ${i18n.translate(
-                      'xpack.observability.ruleDetails.last24h',
-                      {
-                        defaultMessage: '(last 24 h)',
-                      }
-                    )}`}
-                  />
-                )}
-              </EuiFlexGroup>
-            </EuiFlexGroup>
-          </EuiPanel>
-        </EuiFlexItem>
+        {getRuleStatusPanel({
+          rule,
+          isEditable: hasEditButton,
+          requestRefresh: reloadRule,
+          healthColor: getHealthColor(rule.executionStatus.status),
+          statusMessage,
+        })}
 
         {/* Right side of Rule Summary */}
-
         <EuiFlexItem data-test-subj="ruleSummaryRuleDefinition" grow={3}>
           <EuiPanel color="subdued" hasBorder={false} paddingSize={'m'}>
             <EuiFlexGroup justifyContent="spaceBetween">
--- x-pack/plugins/observability/public/pages/rule_details/translations.ts
+++ x-pack/plugins/observability/public/pages/rule_details/translations.ts
@@ -46,3 +46,61 @@ export const ON_WORD = i18n.translate('xpack.observability.ruleDetails.onWord', 
 export const CREATED_WORD = i18n.translate('xpack.observability.ruleDetails.createdWord', {
   defaultMessage: 'Created',
 });
+
+export const ALERT_STATUS_LICENSE_ERROR = i18n.translate(
+  'xpack.observability.sections.ruleDetails.ruleStatusLicenseError',
+  {
+    defaultMessage: 'License Error',
+  }
+);
+
+export const ALERT_STATUS_OK = i18n.translate(
+  'xpack.observability.sections.ruleDetails.ruleStatusOk',
+  {
+    defaultMessage: 'Ok',
+  }
+);
+
+export const ALERT_STATUS_ACTIVE = i18n.translate(
+  'xpack.observability.sections.ruleDetails.ruleStatusActive',
+  {
+    defaultMessage: 'Active',
+  }
+);
+
+export const ALERT_STATUS_ERROR = i18n.translate(
+  'xpack.observability.sections.ruleDetails.ruleStatusError',
+  {
+    defaultMessage: 'Error',
+  }
+);
+
+export const ALERT_STATUS_PENDING = i18n.translate(
+  'xpack.observability.sections.ruleDetails.ruleStatusPending',
+  {
+    defaultMessage: 'Pending',
+  }
+);
+
+export const ALERT_STATUS_UNKNOWN = i18n.translate(
+  'xpack.observability.sections.ruleDetails.ruleStatusUnknown',
+  {
+    defaultMessage: 'Unknown',
+  }
+);
+
+export const ALERT_STATUS_WARNING = i18n.translate(
+  'xpack.observability.sections.ruleDetails.ruleStatusWarning',
+  {
+    defaultMessage: 'Warning',
+  }
+);
+
+export const rulesStatusesTranslationsMapping = {
+  ok: ALERT_STATUS_OK,
+  active: ALERT_STATUS_ACTIVE,
+  error: ALERT_STATUS_ERROR,
+  pending: ALERT_STATUS_PENDING,
+  unknown: ALERT_STATUS_UNKNOWN,
+  warning: ALERT_STATUS_WARNING,
+};
