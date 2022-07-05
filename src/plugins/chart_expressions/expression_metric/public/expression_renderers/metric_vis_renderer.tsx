32c0a9ee1d6784da2a19c1a8feaf140dd074a9b7
Andrew Tate <andrew.tate@elastic.co>
2022-07-05 09:35:41 -0500

    using arrow function for handlers.done()

--- src/plugins/chart_expressions/expression_metric/public/expression_renderers/metric_vis_renderer.tsx
+++ src/plugins/chart_expressions/expression_metric/public/expression_renderers/metric_vis_renderer.tsx
@@ -39,7 +39,7 @@ export const getMetricVisRenderer = (
             showNoResult={!visData.rows.length}
             handlers={handlers}
           >
-            <MetricVis data={visData} config={visConfig} renderComplete={handlers.done} />
+            <MetricVis data={visData} config={visConfig} renderComplete={() => handlers.done()} />
           </VisualizationContainer>
         </KibanaThemeProvider>,
         domNode
