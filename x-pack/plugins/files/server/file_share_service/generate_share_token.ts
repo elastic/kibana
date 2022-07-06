eac5eb3645fa737a94827e92b6a94802c2a9f58c
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-07-06 13:31:25 +0200

    commit simplify random string generator

--- x-pack/plugins/files/server/file_share_service/generate_share_token.ts
+++ x-pack/plugins/files/server/file_share_service/generate_share_token.ts
@@ -8,50 +8,24 @@
 import crypto from 'crypto';
 
 /**
- * This code is adapted and borrowed from
- * https://github.com/sindresorhus/crypto-random-string/blob/25f8930282bc3803d03b9080e710db8426430b63/core.js
+ * Char set of 55 characters gets an even distribution for each byte of randomness
+ * generated because 255 (max number) % 55 = 0.
  */
-
-const CHAR_SET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-const CHAR_SET_LENGTH = CHAR_SET.length;
-
-// We are going to read as 16 bits numbers at a time which means this is the largest
-// number we are going to see.
-const MAX_16_BIT_INT = Math.pow(2, 16) - 1; // OR 0x10000 - 1
-
-// The biggest number we can use for modulo given the charset to ensure good distribution
-// This number works because we know CHAR_SET_LENGTH fits into it CHAR_SET_LENGTH number of times
-const MAX_USABLE_VALUE = Math.floor(MAX_16_BIT_INT / CHAR_SET_LENGTH) * CHAR_SET_LENGTH;
-
-// Tokens must be 40 chars long
-const TOKEN_LENGTH = 40;
-// How much entropy we will generate for each pass
-const ENTROPY_LENGTH = 2 * Math.floor(1.1 * TOKEN_LENGTH);
+const CHAR_SET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRS0123456789';
 
 /**
  * Generate 40 random characters of a pre-checked charset for share tokens.
  *
  * Samples:
  *
- * XBWUuHRZsxXZgS03VUp2hSifmCoF4zznsNOehTea
- * 40RIzynCs2OFTkYC2fy9vfLgYhxjmwzYrJvGj2Ih
- * ZFM1vFSz6k8k9QU4Ve5mu0PbcBi3rwV4BykbcrDU
- * 2oweE4dmWT7warVYA59l0thCDWnIMvUsAC9yCWyQ
- * q8R4LneRCBhcsCNKgcebZRh1Mh4m746GNTuifmAA
- * kpUzKyynu8uXDtek2Xg5yJRwPkw37Cd1VaUezNZ2
+ * EyLtMzvK27QPOfINlJ6AmzfIRlenExBLBOtmrxvl
+ * 1yr1oOmIEiBqRANHA5ztKrSf2dlB8cP6RMGDDHBt
+ * FAgw2kh7af8s42zd1SguN8I0sev0LL3JvklyudHc
+ * Qawttijgvhpc6zocBhoQ91Ajb8cdNQi8I4wrdIPk
+ * FcseNpS7H6dQv96qzPrmaxQtzu61d5ibpbBOS9ML
  */
 export function generateShareToken(): string {
-  let token = '';
-  while (token.length < TOKEN_LENGTH) {
-    // It is highly unlikely we will call this function more than once per token
-    const entropy = crypto.randomBytes(ENTROPY_LENGTH);
-    let entropyPointer = 0;
-    while (entropyPointer < entropy.length && token.length < TOKEN_LENGTH) {
-      const value = entropy.readUInt16LE(entropyPointer);
-      entropyPointer += 2; // Move ahead 2 bytes to the next number
-      if (value > MAX_USABLE_VALUE) continue;
-      token += CHAR_SET[value % CHAR_SET_LENGTH];
-    }
-  }
-  return token;
+  return [...crypto.randomBytes(40)].reduce((acc, byte) => {
+    return acc + CHAR_SET[byte % CHAR_SET.length];
+  }, '');
 }
