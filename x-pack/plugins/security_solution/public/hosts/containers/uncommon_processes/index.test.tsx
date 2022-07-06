b88dcbaa3c3795307f7db778f718334a4f1a1d05
Angela Chuang <yi-chun.chuang@elastic.co>
2022-07-06 20:43:04 +0100

    useSearchStrategy

--- x-pack/plugins/security_solution/public/hosts/containers/uncommon_processes/index.test.tsx
+++ x-pack/plugins/security_solution/public/hosts/containers/uncommon_processes/index.test.tsx
@@ -9,10 +9,64 @@ import { act, renderHook } from '@testing-library/react-hooks';
 import { TestProviders } from '../../../common/mock';
 import { useUncommonProcesses } from '.';
 import { HostsType } from '../../store/model';
+import { useSearchStrategy } from '../../../common/containers/use_search_strategy';
+
+jest.mock('../../../common/containers/use_search_strategy', () => ({
+  useSearchStrategy: jest.fn(),
+}));
+const mockUseSearchStrategy = useSearchStrategy as jest.Mock;
+const mockSearch = jest.fn();
 
 describe('useUncommonProcesses', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockUseSearchStrategy.mockReturnValue({
+      loading: false,
+      result: {
+        edges: [],
+        totalCount: -1,
+        pageInfo: {
+          activePage: 0,
+          fakeTotalCount: 0,
+          showMorePagesIndicator: false,
+        },
+      },
+      search: mockSearch,
+      refetch: jest.fn(),
+      inspect: {},
+    });
+  });
+
+  it('runs search', () => {
+    const localProps = {
+      startDate: '2020-07-07T08:20:18.966Z',
+      endDate: '2020-07-08T08:20:18.966Z',
+      indexNames: ['cool'],
+      type: HostsType.page,
+      skip: false,
+    };
+    renderHook(() => useUncommonProcesses(localProps), {
+      wrapper: TestProviders,
+    });
+
+    expect(mockSearch).toHaveBeenCalled();
+  });
+
+  it('does not run search when skip = true', () => {
+    const localProps = {
+      startDate: '2020-07-07T08:20:18.966Z',
+      endDate: '2020-07-08T08:20:18.966Z',
+      indexNames: ['cool'],
+      type: HostsType.page,
+      skip: true,
+    };
+    renderHook(() => useUncommonProcesses(localProps), {
+      wrapper: TestProviders,
+    });
+
+    expect(mockSearch).not.toHaveBeenCalled();
+  });
   it('skip = true will cancel any running request', () => {
-    const abortSpy = jest.spyOn(AbortController.prototype, 'abort');
     const localProps = {
       startDate: '2020-07-07T08:20:18.966Z',
       endDate: '2020-07-08T08:20:18.966Z',
@@ -25,6 +79,7 @@ describe('useUncommonProcesses', () => {
     });
     localProps.skip = true;
     act(() => rerender());
-    expect(abortSpy).toHaveBeenCalledTimes(4);
+    expect(mockUseSearchStrategy).toHaveBeenCalledTimes(3);
+    expect(mockUseSearchStrategy.mock.calls[2][0].abort).toEqual(true);
   });
 });
