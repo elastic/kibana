12327c2fa035e2958ec414550583c9a41a896a31
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-07-06 15:59:04 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/profiling/server/routes/functions.ts
+++ x-pack/plugins/profiling/server/routes/functions.ts
@@ -1,12 +1,12 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
+
 import { schema, TypeOf } from '@kbn/config-schema';
-import type { ElasticsearchClient, IRouter, Logger } from 'kibana/server';
+import type { ElasticsearchClient, IRouter, Logger } from '@kbn/core/server';
 import type { DataRequestHandlerContext } from '../../../data/server';
 import { getRoutePaths } from '../../common';
 import { createTopNFunctions } from '../../common/functions';
@@ -18,7 +18,6 @@ import {
   mgetStackFrames,
   mgetStackTraces,
   searchEventsGroupByStackTrace,
-  searchStackTraces,
 } from './stacktrace';
 import { getClient } from './compat';
 
