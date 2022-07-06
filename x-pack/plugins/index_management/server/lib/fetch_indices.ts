3331adc851ea8114a15a0c55672a4e496ce46586
Timothy Sullivan <tsullivan@elastic.co>
2022-07-06 11:51:31 -0700

    fix reference to the number of deleted documents

--- x-pack/plugins/index_management/server/lib/fetch_indices.ts
+++ x-pack/plugins/index_management/server/lib/fetch_indices.ts
@@ -57,7 +57,7 @@ async function fetchIndicesCall(
       primary: indexData.settings?.index?.number_of_shards,
       replica: indexData.settings?.index?.number_of_replicas,
       documents: indexStats?.primaries?.docs?.count ?? 0,
-      documents_deleted: indexStats?.total?.docs?.deleted ?? 0,
+      documents_deleted: indexStats?.primaries?.docs?.deleted ?? 0,
       size: new ByteSizeValue(indexStats?.total?.store?.size_in_bytes ?? 0).toString(),
       primary_size: new ByteSizeValue(indexStats?.primaries?.store?.size_in_bytes ?? 0).toString(),
       // @ts-expect-error
