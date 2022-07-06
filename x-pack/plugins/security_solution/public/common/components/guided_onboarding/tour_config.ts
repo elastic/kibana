837e5d03f5f14d4d7bcf68b7adf8f105206cc9d6
Yulia Cech <yulia.cech@elastic.co>
2022-07-06 17:23:35 +0200

    [Guided onboarding] Added i18n to the tour config

--- x-pack/plugins/security_solution/public/common/components/guided_onboarding/tour_config.ts
+++ x-pack/plugins/security_solution/public/common/components/guided_onboarding/tour_config.ts
@@ -6,6 +6,7 @@
  */
 
 import { EuiTourStepProps } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
 
 type TourConfig = Array<
   Pick<EuiTourStepProps, 'step' | 'content' | 'anchor' | 'anchorPosition' | 'title'>
@@ -14,40 +15,66 @@ type TourConfig = Array<
 export const tourConfig: TourConfig = [
   {
     step: 1,
-    title: 'Welcome to Elastic Security',
-    content:
-      'Take a quick tour to explore a unified workflow for  investigating suspicious activity.',
+    title: i18n.translate('xpack.securitySolution.guided_onboarding.tour.overviewStep.tourTitle', {
+      defaultMessage: 'Welcome to Elastic Security',
+    }),
+    content: i18n.translate(
+      'xpack.securitySolution.guided_onboarding.tour.overviewStep.tourContent',
+      {
+        defaultMessage:
+          'Take a quick tour to explore a unified workflow for  investigating suspicious activity.',
+      }
+    ),
     anchor: `[id^="KibanaPageTemplateSolutionNav"]`,
     anchorPosition: 'rightUp',
   },
   {
     step: 2,
-    title: 'Define rules to track unusual activity',
-    content:
-      'Decide what matters to you and your environment. Choose from pre-built rules or customize them to suit your needs.',
+    title: i18n.translate('xpack.securitySolution.guided_onboarding.tour.rulesStep.tourTitle', {
+      defaultMessage: 'Define rules to track unusual activity',
+    }),
+    content: i18n.translate('xpack.securitySolution.guided_onboarding.tour.rulesStep.tourContent', {
+      defaultMessage:
+        'Decide what matters to you and your environment. Choose from pre-built rules or customize them to suit your needs.',
+    }),
     anchor: `[data-test-subj="navigation-rules"]`,
     anchorPosition: 'rightUp',
   },
   {
     step: 3,
-    title: 'Get notified when something changes',
-    content:
-      "Know when a rule's conditions are met, so you can start your investigation right away. Set up notifications with third-party platforms like Slack, PagerDuty, and ServiceNow.",
+    title: i18n.translate('xpack.securitySolution.guided_onboarding.tour.alertsStep.tourTitle', {
+      defaultMessage: 'Get notified when something changes',
+    }),
+    content: i18n.translate(
+      'xpack.securitySolution.guided_onboarding.tour.alertsStep.tourContent',
+      {
+        defaultMessage:
+          "Know when a rule's conditions are met, so you can start your investigation right away. Set up notifications with third-party platforms like Slack, PagerDuty, and ServiceNow.",
+      }
+    ),
     anchor: `[data-test-subj="navigation-alerts"]`,
     anchorPosition: 'rightUp',
   },
   {
     step: 4,
-    title: 'Create a case to track your investigation',
-    content:
-      'Collect evidence, add more collaborators, and even push case details to third-party case management systems.',
+    title: i18n.translate('xpack.securitySolution.guided_onboarding.tour.casesStep.tourTitle', {
+      defaultMessage: 'Create a case to track your investigation',
+    }),
+    content: i18n.translate('xpack.securitySolution.guided_onboarding.tour.casesStep.tourContent', {
+      defaultMessage:
+        'Collect evidence, add more collaborators, and even push case details to third-party case management systems.',
+    }),
     anchor: `[data-test-subj="navigation-cases"]`,
     anchorPosition: 'rightUp',
   },
   {
     step: 5,
-    title: `Start gathering your data!`,
-    content: `Collect data from your endpoints using the Elastic Agent and a variety of third-party integrations.`,
+    title: i18n.translate('xpack.securitySolution.guided_onboarding.tour.dataStep.tourTitle', {
+      defaultMessage: `Start gathering your data!`,
+    }),
+    content: i18n.translate('xpack.securitySolution.guided_onboarding.tour.dataStep.tourContent', {
+      defaultMessage: `Collect data from your endpoints using the Elastic Agent and a variety of third-party integrations.`,
+    }),
     anchor: `[data-test-subj="add-data"]`,
     anchorPosition: 'rightUp',
   },
