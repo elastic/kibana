50293985257106d3b4244ca7faf168916fbef9cb
rashmikulkarni <rashmi.kulkarni@elastic.co>
2022-07-06 11:57:16 -0700

    timepicker change

--- x-pack/test/functional/apps/maps/group1/vector_styling.js
+++ x-pack/test/functional/apps/maps/group1/vector_styling.js
@@ -8,18 +8,17 @@
 import expect from '@kbn/expect';
 
 export default function ({ getService, getPageObjects }) {
-  const PageObjects = getPageObjects(['maps', 'timePicker']);
+  const PageObjects = getPageObjects(['common', 'maps', 'timePicker']);
   const security = getService('security');
+  const from = 'Mar 1, 2015 @ 00:00:00.000';
+  const to = 'Mar 1, 2016 @ 00:00:00.000';
 
   describe('vector styling', () => {
     before(async () => {
       await security.testUser.setRoles(['test_logstash_reader', 'global_maps_all']);
       await PageObjects.maps.loadSavedMap('document example');
-      await PageObjects.timePicker.setAbsoluteRange(
-        'Mar 1, 2015 @ 00:00:00.000',
-        'Mar 1, 2016 @ 00:00:00.000'
-      );
     });
+
     after(async () => {
       await PageObjects.maps.refreshAndClearUnsavedChangesWarning();
       await security.testUser.restoreDefaults();
@@ -27,6 +26,7 @@ export default function ({ getService, getPageObjects }) {
 
     describe('categorical styling', () => {
       before(async () => {
+        await PageObjects.common.setTime({ from, to });
         await PageObjects.maps.openLayerPanel('logstash');
       });
 
@@ -35,6 +35,10 @@ export default function ({ getService, getPageObjects }) {
         await PageObjects.maps.selectCustomColorRamp('fillColor');
         const suggestions = await PageObjects.maps.getCategorySuggestions();
         expect(suggestions.trim().split('\n').join()).to.equal('ios,osx,win 7,win 8,win xp');
+      });
+
+      after(async () => {
+        await PageObjects.common.unsetTime();
       });
     });
   });
