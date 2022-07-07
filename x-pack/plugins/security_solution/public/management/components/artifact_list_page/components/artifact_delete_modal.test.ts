6f802105e8fb0301adb8ab2dad38d96e8449b91b
Jonathan Budzenski <jon@elastic.co>
2022-07-07 09:54:02 -0500

    skip flaky suite. #135794

--- x-pack/plugins/security_solution/public/management/components/artifact_list_page/components/artifact_delete_modal.test.ts
+++ x-pack/plugins/security_solution/public/management/components/artifact_list_page/components/artifact_delete_modal.test.ts
@@ -15,7 +15,8 @@ import {
 import { act, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-describe('When displaying the Delete artfifact modal in the Artifact List Page', () => {
+// FLAKY: https://github.com/elastic/kibana/issues/135794
+describe.skip('When displaying the Delete artfifact modal in the Artifact List Page', () => {
   let renderResult: ReturnType<AppContextTestRender['render']>;
   let history: AppContextTestRender['history'];
   let coreStart: AppContextTestRender['coreStart'];
