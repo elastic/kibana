42ffa372f8205a797efd9bb8c8f71b541abce0f2
Hannah Mudge <Heenawter@users.noreply.github.com>
2022-07-07 08:38:00 -0600

    [Dashboard] (Accessibility) Adds an H1 tag with the dashboard title  (#135757)
    * Make sample dashboards follow tag order standards
    
    * Add h1 tag to dashboard
    
    * Focus on dashboard title element

--- src/plugins/home/server/services/sample_data/data_sets/ecommerce/saved_objects.ts
+++ src/plugins/home/server/services/sample_data/data_sets/ecommerce/saved_objects.ts
@@ -177,7 +177,7 @@ export const getSavedObjects = (): SavedObject[] => [
       uiStateJSON: '{}',
       version: 1,
       visState:
-        '{"title":"[eCommerce] Markdown","type":"markdown","params":{"fontSize":12,"openLinksInNewTab":false,"markdown":"### Sample eCommerce Data\\nThis dashboard contains sample data for you to play with. You can view it, search it, and interact with the visualizations. For more information about Kibana, check our [docs](https://www.elastic.co/guide/en/kibana/current/index.html)."},"aggs":[]}',
+        '{"title":"[eCommerce] Markdown","type":"markdown","params":{"fontSize":12,"openLinksInNewTab":false,"markdown":"## Sample eCommerce Data\\nThis dashboard contains sample data for you to play with. You can view it, search it, and interact with the visualizations. For more information about Kibana, check our [docs](https://www.elastic.co/guide/en/kibana/current/index.html)."},"aggs":[]}',
     },
     coreMigrationVersion: '8.0.0',
     id: 'c00d1f90-f5ea-11eb-a78e-83aac3c38a60',
