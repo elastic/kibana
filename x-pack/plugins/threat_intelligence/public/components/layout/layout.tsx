80296d32392e7db6c23a22b9b448dfb60f500180
Maxim Kholod <maxim.kholod@elastic.co>
2022-07-07 14:37:13 +0200

    remove shared Threat Intelligence header

--- x-pack/plugins/threat_intelligence/public/components/layout/layout.tsx
+++ x-pack/plugins/threat_intelligence/public/components/layout/layout.tsx
@@ -6,10 +6,8 @@
  */
 
 import {
-  EuiHorizontalRule,
   EuiPageBody,
   EuiPageContent,
-  EuiPageHeader,
   EuiText,
 } from '@elastic/eui';
 import React, { FC } from 'react';
@@ -18,19 +16,11 @@ interface LayoutProps {
   pageTitle?: string;
 }
 
-const ROOT_HEADER = 'Threat intelligence';
-
 export const displayName = 'DefaultPageLayout';
 
 export const DefaultPageLayout: FC<LayoutProps> = ({ children, pageTitle }) => (
   <EuiPageBody>
     <EuiPageContent>
-      <EuiPageHeader pageTitle={ROOT_HEADER}>
-        Elastic threat intelligence help you see if you are open to or have been subject to current
-        or historical known threats
-      </EuiPageHeader>
-      <EuiHorizontalRule />
-
       {pageTitle && (
         <EuiText>
           <h2 data-testid={`${displayName}-subheader`}>{pageTitle}</h2>
