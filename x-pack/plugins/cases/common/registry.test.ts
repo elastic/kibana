5928e687739257e9669cdb4f9a2e2a8fe1e92128
Christos Nasikas <christos.nasikas@elastic.co>
2022-07-07 19:17:05 +0300

    [Cases] Add persistable state attachment type (#135452)
    * Create external reference attachment registry
    
    * Pass externalReferenceAttachmentTypeRegistry to cases client
    
    * Better types
    
    * Show external references user action
    
    * Handle unregistered events
    
    * Add e2e tests
    
    * Fixe fixture plugin naming
    
    * Add cases fixture plugin to tsconfig
    
    * Fix types
    
    * Improvements
    
    * Fix types
    
    * Fixes
    
    * Create persistable state attachment type
    
    * Create persistable state attachment registry
    
    * Migrate persistable attachments
    
    * Add tests
    
    * Move test
    
    * Add tests
    
    * Add intergration tests
    
    * Migrate user actions
    
    * Extract/Inject persistable state attributes
    
    * Add tests
    
    * Fix i18n
    
    * Minor improvements
    
    * PR feedback
    
    * PR feedback
    
    * Add more unit tests

--- x-pack/plugins/cases/common/registry.test.ts
+++ x-pack/plugins/cases/common/registry.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { AttachmentTypeRegistry } from './registry';
+import { CaseRegistry } from './registry';
 
 export const ExpressionComponent: React.FunctionComponent = () => {
   return null;
@@ -15,18 +15,18 @@ const getItem = (id: string = 'test') => {
   return { id };
 };
 
-describe('AttachmentTypeRegistry', () => {
+describe('CaseRegistry', () => {
   beforeEach(() => jest.resetAllMocks());
 
   describe('has()', () => {
     it('returns false for unregistered items', () => {
-      const registry = new AttachmentTypeRegistry();
+      const registry = new CaseRegistry('TestRegistry');
 
       expect(registry.has('test')).toEqual(false);
     });
 
     it('returns true after registering an item', () => {
-      const registry = new AttachmentTypeRegistry();
+      const registry = new CaseRegistry('TestRegistry');
       registry.register(getItem());
 
       expect(registry.has('test'));
@@ -35,25 +35,25 @@ describe('AttachmentTypeRegistry', () => {
 
   describe('register()', () => {
     it('able to register items', () => {
-      const registry = new AttachmentTypeRegistry();
+      const registry = new CaseRegistry('TestRegistry');
       registry.register(getItem());
 
       expect(registry.has('test')).toEqual(true);
     });
 
     it('throws error if item is already registered', () => {
-      const registry = new AttachmentTypeRegistry();
+      const registry = new CaseRegistry('TestRegistry');
       registry.register(getItem('test'));
 
       expect(() => registry.register(getItem('test'))).toThrowErrorMatchingInlineSnapshot(
-        `"Attachment type \\"test\\" is already registered."`
+        `"Item \\"test\\" is already registered on registry TestRegistry"`
       );
     });
   });
 
   describe('get()', () => {
     it('returns item', () => {
-      const registry = new AttachmentTypeRegistry();
+      const registry = new CaseRegistry('TestRegistry');
       registry.register(getItem());
       const actionType = registry.get('test');
 
@@ -63,16 +63,16 @@ describe('AttachmentTypeRegistry', () => {
     });
 
     it(`throw error when action type doesn't exist`, () => {
-      const registry = new AttachmentTypeRegistry();
+      const registry = new CaseRegistry('TestRegistry');
       expect(() => registry.get('not-exist-item')).toThrowErrorMatchingInlineSnapshot(
-        `"Attachment type \\"not-exist-item\\" is not registered."`
+        `"Item \\"not-exist-item\\" is not registered on registry TestRegistry"`
       );
     });
   });
 
   describe('list()', () => {
     it('returns list of items', () => {
-      const actionTypeRegistry = new AttachmentTypeRegistry();
+      const actionTypeRegistry = new CaseRegistry('TestRegistry');
       actionTypeRegistry.register(getItem());
       const actionTypes = actionTypeRegistry.list();
 
