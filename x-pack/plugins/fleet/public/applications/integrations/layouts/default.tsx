ee397cbf29b1524e19c94dd13692066f88e27d2e
Mark Hopkin <mark.hopkin@elastic.co>
2022-07-07 21:12:35 +0100

    self review

--- x-pack/plugins/fleet/public/applications/integrations/layouts/default.tsx
+++ x-pack/plugins/fleet/public/applications/integrations/layouts/default.tsx
@@ -20,83 +20,83 @@ const TabBadge = styled(EuiBadge)`
   margin-left: 4px;
 `;
 
-const TabTitle: React.FC<{ title: JSX.Element; hasWarning: boolean }> = ({ title, hasWarning }) => {
-  return (
-    <>
-      {title}
-      {hasWarning && <TabBadge color="warning" iconType="alert" />}
-    </>
-  );
-};
+const TabTitle: React.FC<{ title: JSX.Element; hasWarning: boolean }> = memo(
+  ({ title, hasWarning }) => {
+    return (
+      <>
+        {title}
+        {hasWarning && <TabBadge color="warning" iconType="alert" />}
+      </>
+    );
+  }
+);
 interface Props {
   section?: Section;
   children?: React.ReactNode;
   sectionsWithWarning?: Section[];
 }
 
-export const DefaultLayout: React.FunctionComponent<Props> = memo(
-  ({ section, children, sectionsWithWarning }) => {
-    const { getHref } = useLink();
-    const tabs = [
-      {
-        name: (
-          <FormattedMessage
-            id="xpack.fleet.appNavigation.integrationsAllLinkText"
-            defaultMessage="Browse integrations"
-          />
-        ),
-        section: 'browse' as Section,
-        href: getHref('integrations_all'),
-      },
-      {
-        name: (
-          <FormattedMessage
-            id="xpack.fleet.appNavigation.integrationsInstalledLinkText"
-            defaultMessage="Installed integrations"
-          />
-        ),
-        section: 'manage' as Section,
-        href: getHref('integrations_installed'),
-      },
-    ];
+export const DefaultLayout: React.FC<Props> = memo(({ section, children, sectionsWithWarning }) => {
+  const { getHref } = useLink();
+  const tabs = [
+    {
+      name: (
+        <FormattedMessage
+          id="xpack.fleet.appNavigation.integrationsAllLinkText"
+          defaultMessage="Browse integrations"
+        />
+      ),
+      section: 'browse' as Section,
+      href: getHref('integrations_all'),
+    },
+    {
+      name: (
+        <FormattedMessage
+          id="xpack.fleet.appNavigation.integrationsInstalledLinkText"
+          defaultMessage="Installed integrations"
+        />
+      ),
+      section: 'manage' as Section,
+      href: getHref('integrations_installed'),
+    },
+  ];
 
-    return (
-      <WithHeaderLayout
-        leftColumn={
-          <EuiFlexGroup direction="column" gutterSize="none" justifyContent="center">
-            <EuiText>
-              <h1>
+  return (
+    <WithHeaderLayout
+      leftColumn={
+        <EuiFlexGroup direction="column" gutterSize="none" justifyContent="center">
+          <EuiText>
+            <h1>
+              <FormattedMessage
+                id="xpack.fleet.integrationsHeaderTitle"
+                defaultMessage="Integrations"
+              />
+            </h1>
+          </EuiText>
+
+          <EuiSpacer size="s" />
+
+          <EuiFlexItem grow={false}>
+            <EuiText size="s" color="subdued">
+              <p>
                 <FormattedMessage
-                  id="xpack.fleet.integrationsHeaderTitle"
-                  defaultMessage="Integrations"
+                  id="xpack.fleet.epm.pageSubtitle"
+                  defaultMessage="Choose an integration to start collecting and analyzing your data."
                 />
-              </h1>
+              </p>
             </EuiText>
-
-            <EuiSpacer size="s" />
-
-            <EuiFlexItem grow={false}>
-              <EuiText size="s" color="subdued">
-                <p>
-                  <FormattedMessage
-                    id="xpack.fleet.epm.pageSubtitle"
-                    defaultMessage="Choose an integration to start collecting and analyzing your data."
-                  />
-                </p>
-              </EuiText>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        }
-        tabs={tabs.map((tab) => ({
-          name: (
-            <TabTitle title={tab.name} hasWarning={!!sectionsWithWarning?.includes(tab.section)} />
-          ),
-          href: tab.href,
-          isSelected: section === tab.section,
-        }))}
-      >
-        {children}
-      </WithHeaderLayout>
-    );
-  }
-);
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      }
+      tabs={tabs.map((tab) => ({
+        name: (
+          <TabTitle title={tab.name} hasWarning={!!sectionsWithWarning?.includes(tab.section)} />
+        ),
+        href: tab.href,
+        isSelected: section === tab.section,
+      }))}
+    >
+      {children}
+    </WithHeaderLayout>
+  );
+});
