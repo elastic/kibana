e30e7a296e61c94c339af1150c17be97273e6389
pgayvallet <pierre.gayvallet@elastic.co>
2022-07-08 11:49:06 +0200

    move router mocks to correct package

--- src/core/server/http/http_server.mocks.ts
+++ src/core/server/http/http_server.mocks.ts
@@ -6,118 +6,14 @@
  * Side Public License, v 1.
  */
 
-import { URL } from 'url';
-import { Socket } from 'net';
-import { stringify } from 'query-string';
 import { hapiMocks } from '@kbn/hapi-mocks';
-
-import { schema } from '@kbn/config-schema';
-
 import type {
   LifecycleResponseFactory,
-  RouteMethod,
-  KibanaRequest,
-  KibanaResponseFactory,
-  RouteValidationSpec,
-  KibanaRouteOptions,
-  KibanaRequestState,
   OnPreResponseToolkit,
   OnPostAuthToolkit,
   OnPreRoutingToolkit,
 } from '@kbn/core-http-server';
-import { CoreKibanaRequest } from './router';
-
-/**
- * @internal
- */
-export interface RequestFixtureOptions<P = any, Q = any, B = any> {
-  auth?: { isAuthenticated: boolean };
-  headers?: Record<string, string>;
-  params?: Record<string, any>;
-  body?: Record<string, any>;
-  query?: Record<string, any>;
-  path?: string;
-  method?: RouteMethod;
-  socket?: Socket;
-  routeTags?: string[];
-  kibanaRouteOptions?: KibanaRouteOptions;
-  kibanaRequestState?: KibanaRequestState;
-  routeAuthRequired?: false;
-  validation?: {
-    params?: RouteValidationSpec<P>;
-    query?: RouteValidationSpec<Q>;
-    body?: RouteValidationSpec<B>;
-  };
-}
-
-function createKibanaRequestMock<P = any, Q = any, B = any>({
-  path = '/path',
-  headers = { accept: 'something/html' },
-  params = {},
-  body = {},
-  query = {},
-  method = 'get',
-  socket = new Socket(),
-  routeTags,
-  routeAuthRequired,
-  validation = {},
-  kibanaRouteOptions = { xsrfRequired: true },
-  kibanaRequestState = {
-    requestId: '123',
-    requestUuid: '123e4567-e89b-12d3-a456-426614174000',
-  },
-  auth = { isAuthenticated: true },
-}: RequestFixtureOptions<P, Q, B> = {}): KibanaRequest<P, Q, B> {
-  const queryString = stringify(query, { sort: false });
-  const url = new URL(`${path}${queryString ? `?${queryString}` : ''}`, 'http://localhost');
-
-  return CoreKibanaRequest.from<P, Q, B>(
-    hapiMocks.createRequest({
-      app: kibanaRequestState,
-      auth,
-      headers,
-      params,
-      query,
-      payload: body,
-      path,
-      method,
-      url,
-      route: {
-        // @ts-expect-error According to types/hapi__hapi the following settings-fields have problems:
-        // - `auth` can't be a boolean, but it can according to the @hapi/hapi source (https://github.com/hapijs/hapi/blob/v18.4.2/lib/route.js#L139)
-        // - `app` isn't a valid property, but it is and this was fixed in the types in v19.0.1 (https://github.com/DefinitelyTyped/DefinitelyTyped/pull/41968)
-        settings: { tags: routeTags, auth: routeAuthRequired, app: kibanaRouteOptions },
-      },
-      raw: {
-        req: {
-          socket,
-          // these are needed to avoid an error when consuming KibanaRequest.events
-          on: jest.fn(),
-          off: jest.fn(),
-        },
-      },
-    }),
-    {
-      params: validation.params || schema.any(),
-      body: validation.body || schema.any(),
-      query: validation.query || schema.any(),
-    }
-  );
-}
-
-const createResponseFactoryMock = (): jest.Mocked<KibanaResponseFactory> => ({
-  ok: jest.fn(),
-  accepted: jest.fn(),
-  noContent: jest.fn(),
-  custom: jest.fn(),
-  redirected: jest.fn(),
-  badRequest: jest.fn(),
-  unauthorized: jest.fn(),
-  forbidden: jest.fn(),
-  notFound: jest.fn(),
-  conflict: jest.fn(),
-  customError: jest.fn(),
-});
+import { mockRouter } from './router/router.mock';
 
 const createLifecycleResponseFactoryMock = (): jest.Mocked<LifecycleResponseFactory> => ({
   redirected: jest.fn(),
@@ -140,9 +36,9 @@ const createToolkitMock = (): ToolkitMock => {
 };
 
 export const httpServerMock = {
-  createKibanaRequest: createKibanaRequestMock,
+  createKibanaRequest: mockRouter.createKibanaRequest,
   createRawRequest: hapiMocks.createRequest,
-  createResponseFactory: createResponseFactoryMock,
+  createResponseFactory: mockRouter.createResponseFactory,
   createLifecycleResponseFactory: createLifecycleResponseFactoryMock,
   createToolkit: createToolkitMock,
 };
--- src/core/server/http/router/router.mock.ts
+++ src/core/server/http/router/router.mock.ts
@@ -6,11 +6,25 @@
  * Side Public License, v 1.
  */
 
-import type { IRouter } from '@kbn/core-http-server';
+import { URL } from 'url';
+import { Socket } from 'net';
+import { stringify } from 'query-string';
+import { hapiMocks } from '@kbn/hapi-mocks';
+import { schema } from '@kbn/config-schema';
+import type {
+  IRouter,
+  KibanaRequest,
+  RouteMethod,
+  RouteValidationSpec,
+  KibanaRouteOptions,
+  KibanaRequestState,
+  KibanaResponseFactory,
+} from '@kbn/core-http-server';
+import { CoreKibanaRequest } from './request';
 
 export type RouterMock = jest.Mocked<IRouter<any>>;
 
-function create({ routerPath = '' }: { routerPath?: string } = {}): RouterMock {
+function createRouterMock({ routerPath = '' }: { routerPath?: string } = {}): RouterMock {
   return {
     routerPath,
     get: jest.fn(),
@@ -23,6 +37,100 @@ function create({ routerPath = '' }: { routerPath?: string } = {}): RouterMock {
   };
 }
 
+/**
+ * @internal
+ */
+export interface RequestFixtureOptions<P = any, Q = any, B = any> {
+  auth?: { isAuthenticated: boolean };
+  headers?: Record<string, string>;
+  params?: Record<string, any>;
+  body?: Record<string, any>;
+  query?: Record<string, any>;
+  path?: string;
+  method?: RouteMethod;
+  socket?: Socket;
+  routeTags?: string[];
+  kibanaRouteOptions?: KibanaRouteOptions;
+  kibanaRequestState?: KibanaRequestState;
+  routeAuthRequired?: false;
+  validation?: {
+    params?: RouteValidationSpec<P>;
+    query?: RouteValidationSpec<Q>;
+    body?: RouteValidationSpec<B>;
+  };
+}
+
+function createKibanaRequestMock<P = any, Q = any, B = any>({
+  path = '/path',
+  headers = { accept: 'something/html' },
+  params = {},
+  body = {},
+  query = {},
+  method = 'get',
+  socket = new Socket(),
+  routeTags,
+  routeAuthRequired,
+  validation = {},
+  kibanaRouteOptions = { xsrfRequired: true },
+  kibanaRequestState = {
+    requestId: '123',
+    requestUuid: '123e4567-e89b-12d3-a456-426614174000',
+  },
+  auth = { isAuthenticated: true },
+}: RequestFixtureOptions<P, Q, B> = {}): KibanaRequest<P, Q, B> {
+  const queryString = stringify(query, { sort: false });
+  const url = new URL(`${path}${queryString ? `?${queryString}` : ''}`, 'http://localhost');
+
+  return CoreKibanaRequest.from<P, Q, B>(
+    hapiMocks.createRequest({
+      app: kibanaRequestState,
+      auth,
+      headers,
+      params,
+      query,
+      payload: body,
+      path,
+      method,
+      url,
+      route: {
+        // @ts-expect-error According to types/hapi__hapi the following settings-fields have problems:
+        // - `auth` can't be a boolean, but it can according to the @hapi/hapi source (https://github.com/hapijs/hapi/blob/v18.4.2/lib/route.js#L139)
+        // - `app` isn't a valid property, but it is and this was fixed in the types in v19.0.1 (https://github.com/DefinitelyTyped/DefinitelyTyped/pull/41968)
+        settings: { tags: routeTags, auth: routeAuthRequired, app: kibanaRouteOptions },
+      },
+      raw: {
+        req: {
+          socket,
+          // these are needed to avoid an error when consuming KibanaRequest.events
+          on: jest.fn(),
+          off: jest.fn(),
+        },
+      },
+    }),
+    {
+      params: validation.params || schema.any(),
+      body: validation.body || schema.any(),
+      query: validation.query || schema.any(),
+    }
+  );
+}
+
+const createResponseFactoryMock = (): jest.Mocked<KibanaResponseFactory> => ({
+  ok: jest.fn(),
+  accepted: jest.fn(),
+  noContent: jest.fn(),
+  custom: jest.fn(),
+  redirected: jest.fn(),
+  badRequest: jest.fn(),
+  unauthorized: jest.fn(),
+  forbidden: jest.fn(),
+  notFound: jest.fn(),
+  conflict: jest.fn(),
+  customError: jest.fn(),
+});
+
 export const mockRouter = {
-  create,
+  create: createRouterMock,
+  createKibanaRequest: createKibanaRequestMock,
+  createResponseFactory: createResponseFactoryMock,
 };
