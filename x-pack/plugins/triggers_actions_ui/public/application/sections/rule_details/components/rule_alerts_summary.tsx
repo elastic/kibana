beeb88f43283a2bb74b289737996762847232986
Faisal Kanout <faisal.kanout@elastic.co>
2022-07-08 15:38:24 +0200

    Fix style

--- x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_alerts_summary.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_alerts_summary.tsx
@@ -5,8 +5,17 @@
  * 2.0.
  */
 
-import { EuiFlexGroup, EuiFlexItem, EuiLoadingSpinner, EuiPanel } from '@elastic/eui';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiHorizontalRule,
+  EuiLoadingSpinner,
+  EuiPanel,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
 import { ALERTS_FEATURE_ID } from '@kbn/alerting-plugin/common';
+import { FormattedMessage } from '@kbn/i18n-react';
 import React, { useEffect, useState } from 'react';
 import { Rule } from '../../../../types';
 import { useLoadRuleAlertsAggs } from '../../../hooks/use_load_rule_alerts_aggregations';
@@ -21,7 +30,11 @@ export const RuleAlertsSummary = ({ rule, filteredSolutions }: RuleAlertsSummary
   const { ruleTypes } = useLoadRuleTypes({
     filteredSolutions,
   });
-  const { ruleAlertsAggs, isLoadingRuleAlertsAggs, errorRuleAlertsAggs } = useLoadRuleAlertsAggs({
+  const {
+    ruleAlertsAggs: { active, recovered },
+    isLoadingRuleAlertsAggs,
+    errorRuleAlertsAggs,
+  } = useLoadRuleAlertsAggs({
     ruleId: rule.id,
     features,
   });
@@ -36,11 +49,64 @@ export const RuleAlertsSummary = ({ rule, filteredSolutions }: RuleAlertsSummary
   if (isLoadingRuleAlertsAggs) return <EuiLoadingSpinner />;
   if (errorRuleAlertsAggs) return <EuiFlexItem>Error</EuiFlexItem>;
   return (
-    <EuiPanel hasShadow={false}>
-      <EuiFlexGroup>
-        <EuiFlexItem>Total: {ruleAlertsAggs.active + ruleAlertsAggs.recovered}</EuiFlexItem>
-        <EuiFlexItem>Active: {ruleAlertsAggs.active}</EuiFlexItem>
-        <EuiFlexItem>Recovered: {ruleAlertsAggs.recovered}</EuiFlexItem>
+    <EuiPanel hasShadow={false} hasBorder>
+      <EuiFlexGroup direction="column">
+        <EuiFlexItem grow={false}>
+          <EuiTitle size="xxs">
+            <h5>
+              <FormattedMessage
+                id="xpack.triggersActionsUI.sections.ruleDetails.rule.ruleSummary.alertsTitle"
+                defaultMessage="Alerts"
+              />
+            </h5>
+          </EuiTitle>
+        </EuiFlexItem>
+        <EuiPanel hasShadow={false}>
+          <EuiFlexGroup gutterSize="s" alignItems="center">
+            <EuiFlexGroup direction="row">
+              <EuiFlexItem>
+                <EuiText size="s" color="subdued">
+                  <FormattedMessage
+                    id="xpack.triggersActionsUI.sections.ruleDetails.rule.ruleSummary.allAlertsTitle"
+                    defaultMessage="All alerts"
+                  />
+                </EuiText>
+                <EuiText>
+                  <h4>{active + recovered}</h4>
+                </EuiText>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiFlexGroup direction="row">
+              <EuiFlexItem>
+                <EuiText size="s" color="subdued">
+                  <FormattedMessage
+                    id="xpack.triggersActionsUI.sections.ruleDetails.rule.ruleSummary.activeTitle"
+                    defaultMessage="Active"
+                  />
+                </EuiText>
+                <EuiText color="#4A7194">
+                  <h4>{active}</h4>
+                </EuiText>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiFlexGroup direction="row">
+              <EuiFlexItem>
+                <EuiText size="s" color="subdued">
+                  <FormattedMessage
+                    id="xpack.triggersActionsUI.sections.ruleDetails.rule.ruleSummary.recoveredTitle"
+                    defaultMessage="Recovered"
+                  />
+                </EuiText>
+                <EuiFlexItem>
+                  <EuiText color="#C4407C">
+                    <h4>{recovered}</h4>
+                  </EuiText>
+                </EuiFlexItem>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexGroup>
+        </EuiPanel>
+        <EuiHorizontalRule margin="none" />
       </EuiFlexGroup>
     </EuiPanel>
   );
