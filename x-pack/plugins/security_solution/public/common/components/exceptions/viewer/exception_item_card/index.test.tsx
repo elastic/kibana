0a0b0a18e2c53320e0932d458ac3c59543719ddf
Yara Tercero <yctercero@users.noreply.github.com>
2022-07-08 13:34:37 -0700

    [Security Solution] - Update exceptions item UI (#135255)
    ## Summary
    
    Addresses https://github.com/elastic/kibana/issues/135254

--- x-pack/plugins/security_solution/public/common/components/exceptions/viewer/exception_item_card/index.test.tsx
+++ x-pack/plugins/security_solution/public/common/components/exceptions/viewer/exception_item_card/index.test.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { mount } from 'enzyme';
 
-import { ExceptionItem } from '.';
+import { ExceptionItemCard } from '.';
 import { getExceptionListItemSchemaMock } from '@kbn/lists-plugin/common/schemas/response/exception_list_item_schema.mock';
 import { getCommentsArrayMock } from '@kbn/lists-plugin/common/schemas/types/comment.mock';
 import { getMockTheme } from '../../../../lib/kibana/kibana_react.mock';
@@ -25,75 +25,72 @@ const mockTheme = getMockTheme({
   },
 });
 
-describe('ExceptionItem', () => {
-  it('it renders ExceptionDetails and ExceptionEntries', () => {
-    const exceptionItem = getExceptionListItemSchemaMock();
+describe('ExceptionItemCard', () => {
+  it('it renders header, item meta information and conditions', () => {
+    const exceptionItem = { ...getExceptionListItemSchemaMock(), comments: [] };
 
     const wrapper = mount(
       <ThemeProvider theme={mockTheme}>
-        <ExceptionItem
+        <ExceptionItemCard
           disableActions={false}
           loadingItemIds={[]}
-          commentsAccordionId={'accordion--comments'}
           onDeleteException={jest.fn()}
           onEditException={jest.fn()}
           exceptionItem={exceptionItem}
+          dataTestSubj="item"
         />
       </ThemeProvider>
     );
 
-    expect(wrapper.find('ExceptionDetails')).toHaveLength(1);
-    expect(wrapper.find('ExceptionEntries')).toHaveLength(1);
+    expect(wrapper.find('ExceptionItemCardHeader')).toHaveLength(1);
+    expect(wrapper.find('ExceptionItemCardMetaInfo')).toHaveLength(1);
+    expect(wrapper.find('ExceptionItemCardConditions')).toHaveLength(1);
+    expect(
+      wrapper.find('[data-test-subj="exceptionsViewerCommentAccordion"]').exists()
+    ).toBeFalsy();
   });
 
-  it('it renders ExceptionDetails with Name and Modified info when showName and showModified are true ', () => {
-    const exceptionItem = getExceptionListItemSchemaMock();
+  it('it renders header, item meta information, conditions, and comments if any exist', () => {
+    const exceptionItem = { ...getExceptionListItemSchemaMock(), comments: getCommentsArrayMock() };
 
     const wrapper = mount(
       <ThemeProvider theme={mockTheme}>
-        <ExceptionItem
+        <ExceptionItemCard
           disableActions={false}
           loadingItemIds={[]}
-          commentsAccordionId={'accordion--comments'}
           onDeleteException={jest.fn()}
           onEditException={jest.fn()}
           exceptionItem={exceptionItem}
-          showModified={true}
-          showName={true}
+          dataTestSubj="item"
         />
       </ThemeProvider>
     );
 
-    expect(wrapper.find('ExceptionDetails').props()).toEqual(
-      expect.objectContaining({
-        showModified: true,
-        showName: true,
-      })
-    );
+    expect(wrapper.find('ExceptionItemCardHeader')).toHaveLength(1);
+    expect(wrapper.find('ExceptionItemCardMetaInfo')).toHaveLength(1);
+    expect(wrapper.find('ExceptionItemCardConditions')).toHaveLength(1);
+    expect(
+      wrapper.find('[data-test-subj="exceptionsViewerCommentAccordion"]').exists()
+    ).toBeTruthy();
   });
 
   it('it does not render edit or delete action buttons when "disableActions" is "true"', () => {
-    const mockOnEditException = jest.fn();
     const exceptionItem = getExceptionListItemSchemaMock();
 
     const wrapper = mount(
       <ThemeProvider theme={mockTheme}>
-        <ExceptionItem
+        <ExceptionItemCard
           disableActions
           loadingItemIds={[]}
-          commentsAccordionId={'accordion--comments'}
           onDeleteException={jest.fn()}
-          onEditException={mockOnEditException}
+          onEditException={jest.fn()}
           exceptionItem={exceptionItem}
+          dataTestSubj="item"
         />
       </ThemeProvider>
     );
 
-    const editBtn = wrapper.find('[data-test-subj="exceptionsViewerEditBtn"] button');
-    const deleteBtn = wrapper.find('[data-test-subj="exceptionsViewerDeleteBtn"] button');
-
-    expect(editBtn).toHaveLength(0);
-    expect(deleteBtn).toHaveLength(0);
+    expect(wrapper.find('button[data-test-subj="item-actionButton"]').exists()).toBeFalsy();
   });
 
   it('it invokes "onEditException" when edit button clicked', () => {
@@ -102,19 +99,25 @@ describe('ExceptionItem', () => {
 
     const wrapper = mount(
       <ThemeProvider theme={mockTheme}>
-        <ExceptionItem
+        <ExceptionItemCard
           disableActions={false}
           loadingItemIds={[]}
-          commentsAccordionId={'accordion--comments'}
           onDeleteException={jest.fn()}
           onEditException={mockOnEditException}
           exceptionItem={exceptionItem}
+          dataTestSubj="item"
         />
       </ThemeProvider>
     );
 
-    const editBtn = wrapper.find('[data-test-subj="exceptionsViewerEditBtn"] button').at(0);
-    editBtn.simulate('click');
+    // click on popover
+    wrapper
+      .find('button[data-test-subj="exceptionItemCardHeader-actionButton"]')
+      .at(0)
+      .simulate('click');
+    wrapper
+      .find('button[data-test-subj="exceptionItemCardHeader-actionItem-edit"]')
+      .simulate('click');
 
     expect(mockOnEditException).toHaveBeenCalledWith(getExceptionListItemSchemaMock());
   });
@@ -125,19 +128,25 @@ describe('ExceptionItem', () => {
 
     const wrapper = mount(
       <ThemeProvider theme={mockTheme}>
-        <ExceptionItem
+        <ExceptionItemCard
           disableActions={false}
           loadingItemIds={[]}
-          commentsAccordionId={'accordion--comments'}
           onDeleteException={mockOnDeleteException}
           onEditException={jest.fn()}
           exceptionItem={exceptionItem}
+          dataTestSubj="item"
         />
       </ThemeProvider>
     );
 
-    const deleteBtn = wrapper.find('[data-test-subj="exceptionsViewerDeleteBtn"] button').at(0);
-    deleteBtn.simulate('click');
+    // click on popover
+    wrapper
+      .find('button[data-test-subj="exceptionItemCardHeader-actionButton"]')
+      .at(0)
+      .simulate('click');
+    wrapper
+      .find('button[data-test-subj="exceptionItemCardHeader-actionItem-delete"]')
+      .simulate('click');
 
     expect(mockOnDeleteException).toHaveBeenCalledWith({
       id: '1',
@@ -146,47 +155,21 @@ describe('ExceptionItem', () => {
   });
 
   it('it renders comment accordion closed to begin with', () => {
-    const mockOnDeleteException = jest.fn();
     const exceptionItem = getExceptionListItemSchemaMock();
     exceptionItem.comments = getCommentsArrayMock();
     const wrapper = mount(
       <ThemeProvider theme={mockTheme}>
-        <ExceptionItem
+        <ExceptionItemCard
           disableActions={false}
           loadingItemIds={[]}
-          commentsAccordionId={'accordion--comments'}
-          onDeleteException={mockOnDeleteException}
+          onDeleteException={jest.fn()}
           onEditException={jest.fn()}
           exceptionItem={exceptionItem}
+          dataTestSubj="item"
         />
       </ThemeProvider>
     );
 
     expect(wrapper.find('.euiAccordion-isOpen')).toHaveLength(0);
-  });
-
-  it('it renders comment accordion open when showComments is true', () => {
-    const mockOnDeleteException = jest.fn();
-    const exceptionItem = getExceptionListItemSchemaMock();
-    exceptionItem.comments = getCommentsArrayMock();
-    const wrapper = mount(
-      <ThemeProvider theme={mockTheme}>
-        <ExceptionItem
-          disableActions={false}
-          loadingItemIds={[]}
-          commentsAccordionId={'accordion--comments'}
-          onDeleteException={mockOnDeleteException}
-          onEditException={jest.fn()}
-          exceptionItem={exceptionItem}
-        />
-      </ThemeProvider>
-    );
-
-    const commentsBtn = wrapper
-      .find('.euiButtonEmpty[data-test-subj="exceptionsViewerItemCommentsBtn"]')
-      .at(0);
-    commentsBtn.simulate('click');
-
-    expect(wrapper.find('.euiAccordion-isOpen')).toHaveLength(1);
   });
 });
