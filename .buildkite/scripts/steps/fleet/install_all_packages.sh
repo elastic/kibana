18ca0e5b7347688332ad7daae9e891344f66d029
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-07-08 18:49:46 -0400

    [Fleet] Run install all packages in CI (#136035)

--- .buildkite/scripts/steps/fleet/install_all_packages.sh
+++ .buildkite/scripts/steps/fleet/install_all_packages.sh
@@ -2,10 +2,11 @@
 
 set -euo pipefail
 
-source .buildkite/scripts/common/util.sh
-
-.buildkite/scripts/bootstrap.sh
+source .buildkite/scripts/steps/functional/common.sh
 
 echo '--- Installing all packages'
-cd x-pack/plugins/fleet
-node scripts/install_all_packages
+
+checks-reporter-with-killswitch "Fleet packages Tests" \
+ node scripts/functional_tests \
+   --debug --bail \
+   --config x-pack/test/fleet_packages/config.ts
