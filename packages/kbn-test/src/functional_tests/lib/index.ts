60240ad7571301fb7c7cd61606edeb501817cdda
spalger <spencer@elastic.co>
2022-07-11 08:49:15 -0500

    remove paths module

--- packages/kbn-test/src/functional_tests/lib/index.ts
+++ packages/kbn-test/src/functional_tests/lib/index.ts
@@ -10,5 +10,4 @@ export { runKibanaServer } from './run_kibana_server';
 export { runElasticsearch } from './run_elasticsearch';
 export type { CreateFtrOptions, CreateFtrParams } from './run_ftr';
 export { runFtr, hasTests, assertNoneExcluded } from './run_ftr';
-export { KIBANA_FTR_SCRIPT } from './paths';
 export { runCli } from './run_cli';
