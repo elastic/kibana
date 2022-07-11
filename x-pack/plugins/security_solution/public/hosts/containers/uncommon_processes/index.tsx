cc74d21feaee729aaef4d258477a88e6f6fb14f1
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2022-07-11 14:46:07 +0100

    Refactoring uncommon processes to use useSearchStrategy (#135856)
    * useSearchStrategy
    
    * update unit test
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/hosts/containers/uncommon_processes/index.tsx
+++ x-pack/plugins/security_solution/public/hosts/containers/uncommon_processes/index.tsx
@@ -6,14 +6,10 @@
  */
 
 import deepEqual from 'fast-deep-equal';
-import { noop } from 'lodash/fp';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { Subscription } from 'rxjs';
-
-import { isCompleteResponse, isErrorResponse } from '@kbn/data-plugin/common';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import type { inputsModel, State } from '../../../common/store';
-import { useKibana } from '../../../common/lib/kibana';
+
 import { generateTablePaginationOptions } from '../../../common/components/paginated_table/helpers';
 import { createFilter } from '../../../common/containers/helpers';
 import type { hostsModel } from '../../store';
@@ -23,16 +19,14 @@ import type {
   PageInfoPaginated,
   HostsUncommonProcessesEdges,
   HostsUncommonProcessesRequestOptions,
-  HostsUncommonProcessesStrategyResponse,
 } from '../../../../common/search_strategy';
 import { HostsQueries } from '../../../../common/search_strategy';
 
 import * as i18n from './translations';
 import type { ESTermQuery } from '../../../../common/typed_json';
-import { getInspectResponse } from '../../../helpers';
 import type { InspectResponse } from '../../../types';
 import { useDeepEqualSelector } from '../../../common/hooks/use_selector';
-import { useAppToasts } from '../../../common/hooks/use_app_toasts';
+import { useSearchStrategy } from '../../../common/containers/use_search_strategy';
 
 export const ID = 'hostsUncommonProcessesQuery';
 
@@ -57,8 +51,8 @@ interface UseUncommonProcesses {
 }
 
 export const useUncommonProcesses = ({
-  filterQuery,
   endDate,
+  filterQuery,
   indexNames,
   skip = false,
   startDate,
@@ -71,14 +65,8 @@ export const useUncommonProcesses = ({
   const { activePage, limit } = useDeepEqualSelector((state: State) =>
     getUncommonProcessesSelector(state, type)
   );
-  const { data } = useKibana().services;
-  const refetch = useRef<inputsModel.Refetch>(noop);
-  const abortCtrl = useRef(new AbortController());
-  const searchSubscription$ = useRef(new Subscription());
-  const [loading, setLoading] = useState(false);
   const [uncommonProcessesRequest, setUncommonProcessesRequest] =
     useState<HostsUncommonProcessesRequestOptions | null>(null);
-  const { addError, addWarning } = useAppToasts();
 
   const wrappedLoadMore = useCallback(
     (newActivePage: number) => {
@@ -96,77 +84,39 @@ export const useUncommonProcesses = ({
     [limit]
   );
 
-  const [uncommonProcessesResponse, setUncommonProcessesResponse] = useState<UncommonProcessesArgs>(
-    {
-      uncommonProcesses: [],
-      id: ID,
-      inspect: {
-        dsl: [],
-        response: [],
-      },
-      isInspected: false,
-      loadPage: wrappedLoadMore,
+  const {
+    loading,
+    result: response,
+    search,
+    refetch,
+    inspect,
+  } = useSearchStrategy<HostsQueries.uncommonProcesses>({
+    factoryQueryType: HostsQueries.uncommonProcesses,
+    initialResult: {
+      edges: [],
+      totalCount: -1,
       pageInfo: {
         activePage: 0,
         fakeTotalCount: 0,
         showMorePagesIndicator: false,
       },
-      refetch: refetch.current,
-      totalCount: -1,
-    }
-  );
-
-  const uncommonProcessesSearch = useCallback(
-    (request: HostsUncommonProcessesRequestOptions | null) => {
-      if (request == null || skip) {
-        return;
-      }
-      const asyncSearch = async () => {
-        abortCtrl.current = new AbortController();
-        setLoading(true);
-
-        searchSubscription$.current = data.search
-          .search<HostsUncommonProcessesRequestOptions, HostsUncommonProcessesStrategyResponse>(
-            request,
-            {
-              strategy: 'securitySolutionSearchStrategy',
-              abortSignal: abortCtrl.current.signal,
-            }
-          )
-          .subscribe({
-            next: (response) => {
-              if (isCompleteResponse(response)) {
-                setLoading(false);
-                setUncommonProcessesResponse((prevResponse) => ({
-                  ...prevResponse,
-                  uncommonProcesses: response.edges,
-                  inspect: getInspectResponse(response, prevResponse.inspect),
-                  pageInfo: response.pageInfo,
-                  refetch: refetch.current,
-                  totalCount: response.totalCount,
-                }));
-                searchSubscription$.current.unsubscribe();
-              } else if (isErrorResponse(response)) {
-                setLoading(false);
-                addWarning(i18n.ERROR_UNCOMMON_PROCESSES);
-                searchSubscription$.current.unsubscribe();
-              }
-            },
-            error: (msg) => {
-              setLoading(false);
-              addError(msg, {
-                title: i18n.FAIL_UNCOMMON_PROCESSES,
-              });
-              searchSubscription$.current.unsubscribe();
-            },
-          });
-      };
-      searchSubscription$.current.unsubscribe();
-      abortCtrl.current.abort();
-      asyncSearch();
-      refetch.current = asyncSearch;
     },
-    [data.search, addError, addWarning, skip]
+    errorMessage: i18n.FAIL_UNCOMMON_PROCESSES,
+    abort: skip,
+  });
+
+  const uncommonProcessesResponse = useMemo(
+    () => ({
+      id: ID,
+      inspect,
+      isInspected: false,
+      loadPage: wrappedLoadMore,
+      pageInfo: response.pageInfo,
+      refetch,
+      totalCount: response.totalCount,
+      uncommonProcesses: response.edges,
+    }),
+    [inspect, refetch, response.edges, response.pageInfo, response.totalCount, wrappedLoadMore]
   );
 
   useEffect(() => {
@@ -192,20 +142,10 @@ export const useUncommonProcesses = ({
   }, [activePage, indexNames, endDate, filterQuery, limit, startDate]);
 
   useEffect(() => {
-    uncommonProcessesSearch(uncommonProcessesRequest);
-    return () => {
-      searchSubscription$.current.unsubscribe();
-      abortCtrl.current.abort();
-    };
-  }, [uncommonProcessesRequest, uncommonProcessesSearch]);
-
-  useEffect(() => {
-    if (skip) {
-      setLoading(false);
-      searchSubscription$.current.unsubscribe();
-      abortCtrl.current.abort();
+    if (!skip && uncommonProcessesRequest) {
+      search(uncommonProcessesRequest);
     }
-  }, [skip]);
+  }, [search, skip, uncommonProcessesRequest]);
 
   return [loading, uncommonProcessesResponse];
 };
