2ab1d202ca578150682dcf4eabe1e176e579480d
JD Kurma <JDKurma@gmail.com>
2022-07-11 16:44:41 -0400

    [Security Solution] Add usage counter for num of endpoints (#135953)
    * add usage counter for num of endpoints
    
    * unit tests for endpoint counter
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/server/lib/telemetry/__mocks__/index.ts
+++ x-pack/plugins/security_solution/server/lib/telemetry/__mocks__/index.ts
@@ -13,6 +13,7 @@ import type { TelemetryReceiver } from '../receiver';
 import type { SecurityTelemetryTaskConfig } from '../task';
 import type { PackagePolicy } from '@kbn/fleet-plugin/common/types/models/package_policy';
 import { stubEndpointAlertResponse, stubProcessTree, stubFetchTimelineEvents } from './timeline';
+import { stubEndpointMetricsResponse } from './metrics';
 
 export const createMockTelemetryEventsSender = (
   enableTelemetry?: boolean,
@@ -82,7 +83,7 @@ export const createMockTelemetryReceiver = (
     copyLicenseFields: jest.fn(),
     fetchFleetAgents: jest.fn(),
     fetchDiagnosticAlerts: jest.fn().mockReturnValue(diagnosticsAlert ?? jest.fn()),
-    fetchEndpointMetrics: jest.fn(),
+    fetchEndpointMetrics: jest.fn().mockReturnValue(stubEndpointMetricsResponse),
     fetchEndpointPolicyResponses: jest.fn(),
     fetchTrustedApplications: jest.fn(),
     fetchEndpointList: jest.fn(),
