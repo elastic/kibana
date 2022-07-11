3afed8761933f68483c0360fca37e8f3aa9a29f8
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2022-07-11 16:38:24 +0100

    another wording change

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/translations.ts
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/translations.ts
@@ -308,7 +308,7 @@ export const BULK_EDIT_FLYOUT_FORM_ADD_INDEX_PATTERNS_TITLE = i18n.translate(
 export const BULK_EDIT_FLYOUT_FORM_ADD_INDEX_PATTERNS_OVERWRITE_LABEL = i18n.translate(
   'xpack.securitySolution.detectionEngine.components.allRules.bulkActions.bulkEditFlyoutForm.addIndexPatternsOverwriteCheckboxLabel',
   {
-    defaultMessage: 'Overwrite all selected rules index patterns',
+    defaultMessage: "Overwrite all selected rules' index patterns",
   }
 );
 
@@ -373,7 +373,7 @@ export const BULK_EDIT_FLYOUT_FORM_ADD_TAGS_TITLE = i18n.translate(
 export const BULK_EDIT_FLYOUT_FORM_ADD_TAGS_OVERWRITE_LABEL = i18n.translate(
   'xpack.securitySolution.detectionEngine.components.allRules.bulkActions.bulkEditFlyoutForm.addTagsOverwriteCheckboxLabel',
   {
-    defaultMessage: 'Overwrite all selected rules tags',
+    defaultMessage: 'Overwrite all selected rules' tags",
   }
 );
 
