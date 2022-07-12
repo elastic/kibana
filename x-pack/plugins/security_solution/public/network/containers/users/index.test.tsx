be60151be846e512462c387255793e715b2d4c5f
Angela Chuang <yi-chun.chuang@elastic.co>
2022-07-12 15:18:45 +0100

    Refactoring network users to use useSearchStrategy

--- x-pack/plugins/security_solution/public/network/containers/users/index.test.tsx
+++ x-pack/plugins/security_solution/public/network/containers/users/index.test.tsx
@@ -7,29 +7,78 @@
 
 import { act, renderHook } from '@testing-library/react-hooks';
 import { TestProviders } from '../../../common/mock';
-import { ID, useNetworkUsers } from '.';
-import { NetworkType } from '../../store/model';
+import { useNetworkUsers, ID } from '.';
+import { useSearchStrategy } from '../../../common/containers/use_search_strategy';
+import { networkModel } from '../../store';
 import { FlowTargetSourceDest } from '../../../../common/search_strategy';
 
+jest.mock('../../../common/containers/use_search_strategy', () => ({
+  useSearchStrategy: jest.fn(),
+}));
+const mockUseSearchStrategy = useSearchStrategy as jest.Mock;
+const mockSearch = jest.fn();
+
+const props = {
+  endDate: '2020-07-08T08:20:18.966Z',
+  flowTarget: FlowTargetSourceDest.source,
+  id: ID,
+  indexNames: ['auditbeat-*'],
+  ip: '192.168.1.1',
+  skip: false,
+  startDate: '2020-07-07T08:20:18.966Z',
+  type: networkModel.NetworkType.details,
+};
+
 describe('useNetworkUsers', () => {
-  it('skip = true will cancel any running request', () => {
-    const abortSpy = jest.spyOn(AbortController.prototype, 'abort');
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockUseSearchStrategy.mockReturnValue({
+      loading: false,
+      result: {
+        edges: [],
+        totalCount: -1,
+        pageInfo: {
+          activePage: 0,
+          fakeTotalCount: 0,
+          showMorePagesIndicator: false,
+        },
+      },
+      search: mockSearch,
+      refetch: jest.fn(),
+      inspect: {},
+    });
+  });
+
+  it('runs search', () => {
+    renderHook(() => useNetworkUsers(props), {
+      wrapper: TestProviders,
+    });
+
+    expect(mockSearch).toHaveBeenCalled();
+  });
+
+  it('does not run search when skip = true', () => {
     const localProps = {
-      docValueFields: [],
-      id: `${ID}-${NetworkType.page}`,
-      ip: '1.1.1.1',
-      flowTarget: FlowTargetSourceDest.source,
-      startDate: '2020-07-07T08:20:18.966Z',
-      endDate: '2020-07-08T08:20:18.966Z',
-      indexNames: ['cool'],
-      type: NetworkType.page,
-      skip: false,
+      ...props,
+      skip: true,
+    };
+    renderHook(() => useNetworkUsers(localProps), {
+      wrapper: TestProviders,
+    });
+
+    expect(mockSearch).not.toHaveBeenCalled();
+  });
+
+  it('skip = true will cancel any running request', () => {
+    const localProps = {
+      ...props,
     };
     const { rerender } = renderHook(() => useNetworkUsers(localProps), {
       wrapper: TestProviders,
     });
     localProps.skip = true;
     act(() => rerender());
-    expect(abortSpy).toHaveBeenCalledTimes(4);
+    expect(mockUseSearchStrategy).toHaveBeenCalledTimes(3);
+    expect(mockUseSearchStrategy.mock.calls[2][0].abort).toEqual(true);
   });
 });
