313c3055460a0f514be0be1b13546c3a75fbc6ea
Luke Elmers <lukeelmers@gmail.com>
2022-07-12 15:34:09 -0600

    [http][elasticsearch] Updates header filters for request/response logs. (#136044)

--- src/core/server/http/logging/get_response_log.ts
+++ src/core/server/http/logging/get_response_log.ts
@@ -14,7 +14,9 @@ import type { LogMeta, Logger } from '@kbn/logging';
 import type { KibanaRequestState } from '@kbn/core-http-server';
 import { getResponsePayloadBytes } from './get_payload_size';
 
-const FORBIDDEN_HEADERS = ['authorization', 'cookie', 'set-cookie'];
+// If you are updating these, consider whether they should also be updated in the
+// elasticsearch service `getEcsResponseLog`
+const FORBIDDEN_HEADERS = ['authorization', 'cookie', 'set-cookie', 'x-elastic-app-auth'];
 const REDACTED_HEADER_TEXT = '[REDACTED]';
 
 type HapiHeaders = Record<string, string | string[]>;
