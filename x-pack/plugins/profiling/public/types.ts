9b496b9bb46bcf4a2ef22efbb3e825c6bbd39716
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-07-12 09:40:45 +0200

    Add types

--- x-pack/plugins/profiling/public/types.ts
+++ x-pack/plugins/profiling/public/types.ts
@@ -4,24 +4,21 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type {
-  ObservabilityPublicSetup,
-  ObservabilityPublicStart,
-} from '@kbn/observability-plugin/public';
-import { DataPublicPluginSetup, DataPublicPluginStart } from '@kbn/data-plugin/public';
 import {
   DataViewsPublicPluginSetup,
   DataViewsPublicPluginStart,
 } from '@kbn/data-views-plugin/public';
+import type {
+  ObservabilityPublicSetup,
+  ObservabilityPublicStart,
+} from '@kbn/observability-plugin/public';
 
 export interface ProfilingPluginPublicSetupDeps {
   observability: ObservabilityPublicSetup;
-  data: DataPublicPluginSetup;
   dataViews: DataViewsPublicPluginSetup;
 }
 
 export interface ProfilingPluginPublicStartDeps {
   observability: ObservabilityPublicStart;
-  data: DataPublicPluginStart;
   dataViews: DataViewsPublicPluginStart;
 }
