432bc771cb02afa00676ed458924760ed77d2634
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-07-12 06:45:15 -0700

    Updates imports

--- src/core/server/saved_objects/deprecations/unknown_object_types.ts
+++ src/core/server/saved_objects/deprecations/unknown_object_types.ts
@@ -7,7 +7,7 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import type { DeprecationsDetails } from '../../deprecations';
+import type { DeprecationsDetails } from '@kbn/core-deprecations-common';
 import { IScopedClusterClient } from '../../elasticsearch';
 import { getAggregatedTypesDocuments } from '../migrations/actions/check_for_unknown_docs';
 import { addExcludedTypesToBoolQuery } from '../migrations/model/helpers';
