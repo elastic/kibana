ed1849688ec2a61aaadc3bc7388920e1e4dd019d
Sébastien Loix <sebastien.loix@elastic.co>
2022-07-13 14:09:17 +0100

    [Form lib] Add storybook (#127898)

--- src/plugins/es_ui_shared/.storybook/main.ts
+++ src/plugins/es_ui_shared/.storybook/main.ts
@@ -6,10 +6,6 @@
  * Side Public License, v 1.
  */
 
-export type { InternalFieldConfig } from './use_field';
-export { useField } from './use_field';
-export { useFieldFromProps } from './use_field_from_props';
-export { useForm } from './use_form';
-export { useFormData } from './use_form_data';
-export { useFormIsModified } from './use_form_is_modified';
-export { useBehaviorSubject } from './utils';
+import { defaultConfig } from '@kbn/storybook';
+
+module.exports = defaultConfig;
--- src/plugins/es_ui_shared/static/forms/hook_form_lib/components/__stories__/constants.ts
+++ src/plugins/es_ui_shared/static/forms/hook_form_lib/components/__stories__/constants.ts
@@ -6,10 +6,12 @@
  * Side Public License, v 1.
  */
 
-export type { InternalFieldConfig } from './use_field';
-export { useField } from './use_field';
-export { useFieldFromProps } from './use_field_from_props';
-export { useForm } from './use_form';
-export { useFormData } from './use_form_data';
-export { useFormIsModified } from './use_form_is_modified';
-export { useBehaviorSubject } from './utils';
+import { FieldConfig } from '../../types';
+
+/**
+ * The "title" field base configuration
+ */
+export const titleConfigBase: FieldConfig<string> = {
+  label: 'Title',
+  helpText: 'This is a help text for the field.',
+};
