2cb28a75aa95742f3a230dfe58dda0e252a1e12a
Ying Mao <ying.mao@elastic.co>
2022-07-13 13:46:15 -0400

    [Response Ops] Refactor task runner - consolidate determination of active/recovered/new alerts into library function (#135069)
    * wip
    
    * Consolidating alert logic into single function and using it in task runner
    
    * Updating unit tests
    
    * Setting duration to 0 for new alerts
    
    * Fixing types
    
    * Fixing types
    
    * Preserving original alert data
    
    * Reverting change
    
    * Fixing types
    
    * PR feedback
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/alerting/server/lib/process_alerts.ts
@@ -0,0 +1,82 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { millisToNanos } from '@kbn/event-log-plugin/server';
+import { Alert } from '../alert';
+import { AlertInstanceState, AlertInstanceContext } from '../types';
+
+interface ProcessAlertsResult<
+  State extends AlertInstanceState,
+  Context extends AlertInstanceContext,
+  ActionGroupIds extends string,
+  RecoveryActionGroupId extends string
+> {
+  newAlerts: Record<string, Alert<State, Context, ActionGroupIds>>;
+  activeAlerts: Record<string, Alert<State, Context, ActionGroupIds>>;
+  recoveredAlerts: Record<string, Alert<State, Context, RecoveryActionGroupId>>;
+}
+
+export function processAlerts<
+  State extends AlertInstanceState,
+  Context extends AlertInstanceContext,
+  ActionGroupIds extends string,
+  RecoveryActionGroupId extends string
+>(
+  alerts: Record<string, Alert<State, Context>>,
+  originalAlerts: Record<string, Alert<State, Context>>
+): ProcessAlertsResult<State, Context, ActionGroupIds, RecoveryActionGroupId> {
+  const originalAlertIds = new Set(Object.keys(originalAlerts));
+
+  const currentTime = new Date().toISOString();
+  const newAlerts: Record<string, Alert<State, Context, ActionGroupIds>> = {};
+  const activeAlerts: Record<string, Alert<State, Context, ActionGroupIds>> = {};
+  const recoveredAlerts: Record<string, Alert<State, Context, RecoveryActionGroupId>> = {};
+
+  for (const id in alerts) {
+    if (alerts.hasOwnProperty(id)) {
+      // alerts with scheduled actions are considered "active"
+      if (alerts[id].hasScheduledActions()) {
+        activeAlerts[id] = alerts[id];
+
+        // if this alert did not exist in previous run, it is considered "new"
+        if (!originalAlertIds.has(id)) {
+          newAlerts[id] = alerts[id];
+
+          // Inject start time into alert state for new alerts
+          const state = newAlerts[id].getState();
+          newAlerts[id].replaceState({ ...state, start: currentTime, duration: '0' });
+        } else {
+          // this alert did exist in previous run
+          // calculate duration to date for active alerts
+          const state = originalAlerts[id].getState();
+          const durationInMs =
+            new Date(currentTime).valueOf() - new Date(state.start as string).valueOf();
+          const duration = state.start ? millisToNanos(durationInMs) : undefined;
+          activeAlerts[id].replaceState({
+            ...state,
+            ...(state.start ? { start: state.start } : {}),
+            ...(duration !== undefined ? { duration } : {}),
+          });
+        }
+      } else if (originalAlertIds.has(id)) {
+        recoveredAlerts[id] = alerts[id];
+
+        // Inject end time into alert state of recovered alerts
+        const state = recoveredAlerts[id].getState();
+        const durationInMs =
+          new Date(currentTime).valueOf() - new Date(state.start as string).valueOf();
+        const duration = state.start ? millisToNanos(durationInMs) : undefined;
+        recoveredAlerts[id].replaceState({
+          ...state,
+          ...(duration ? { duration } : {}),
+          ...(state.start ? { end: currentTime } : {}),
+        });
+      }
+    }
+  }
+  return { recoveredAlerts, newAlerts, activeAlerts };
+}
