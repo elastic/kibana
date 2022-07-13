e6ea7a8d394e5c2f9dea7ac3676fc605e884bc02
Julia Rechkunova <julia.rechkunova@elastic.co>
2022-07-13 16:34:22 +0200

    [UnifiedFieldList] Bootstrap a new unifiedFieldList plugin

--- src/plugins/discover/public/application/main/components/sidebar/discover_field.tsx
+++ src/plugins/discover/public/application/main/components/sidebar/discover_field.tsx
@@ -26,6 +26,7 @@ import { UiCounterMetricType } from '@kbn/analytics';
 import classNames from 'classnames';
 import { FieldButton, FieldIcon } from '@kbn/react-field';
 import type { DataViewField, DataView } from '@kbn/data-views-plugin/public';
+import { FieldStats } from '@kbn/unified-field-list-plugin/public';
 import { getFieldCapabilities } from '../../../../utils/get_field_capabilities';
 import { getTypeForFieldIcon } from '../../../../utils/get_type_for_field_icon';
 import { DiscoverFieldDetails } from './discover_field_details';
@@ -375,6 +376,7 @@ function DiscoverFieldComponent({
       <>
         {showFieldStats && (
           <>
+            <FieldStats />
             <EuiTitle size="xxxs">
               <h5>
                 {i18n.translate('discover.fieldChooser.discoverField.fieldTopValuesLabel', {
