55dcbb3adc299f010a663c0f5d3f7907897df0de
Oliver Gupte <ogupte@users.noreply.github.com>
2022-07-12 23:00:25 -0700

    [APM] Adds support for metrics for latency distribution histogram (#136083)
    * [APM] Adds support for metrics for latency distribution histogram (#132394)
    
    * PR feedback
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/apm/server/routes/correlations/utils/index.ts
+++ x-pack/plugins/apm/server/routes/correlations/utils/index.ts
@@ -7,3 +7,4 @@
 
 export { computeExpectationsAndRanges } from './compute_expectations_and_ranges';
 export { splitAllSettledPromises } from './split_all_settled_promises';
+export { getDurationField } from './get_duration_field';
