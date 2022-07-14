c8880380118eea9471c1be83d31c95830e254ecf
Kyle Pollich <kyle.pollich@elastic.co>
2022-07-14 15:53:45 -0400

    [Fleet] Use `unmapped_type: long` when sorting datasets that don't include `event.ingested` (#136114)
    * Add ignore_unmapped: true to fix data streams API in e2e tests
    
    * Fix sort setup
    
    * Skip failing cypress suite for now
    
    * Filter out non-integration data streams from Fleet data streams API
    
    * Try using timestamp instead of event.ingested
    
    * Use unmapped type setting
    
    * Provide explicit missing value for event.ingested
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/fleet/server/routes/data_streams/handlers.ts
+++ x-pack/plugins/fleet/server/routes/data_streams/handlers.ts
@@ -190,7 +190,7 @@ export const getListHandler: RequestHandler = async (context, request, response)
     });
 
     // Return final data streams objects sorted by last activity, descending
-    // After filtering out data streams that are missing dataset/namespace/type fields
+    // After filtering out data streams that are missing dataset/namespace/type/package fields
     body.data_streams = (await Promise.all(dataStreamPromises))
       .filter(({ dataset, namespace, type }) => dataset && namespace && type)
       .sort((a, b) => b.last_activity_ms - a.last_activity_ms);
