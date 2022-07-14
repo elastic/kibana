876bc6acd6a41110fa1e7c4c36b29d16f5111d46
Nathan Reese <reese.nathan@gmail.com>
2022-07-14 10:42:19 -0600

    Update x-pack/plugins/maps/public/classes/layers/ems_vector_tile_layer/ems_vector_tile_layer.tsx
    Co-authored-by: gchaps <33642766+gchaps@users.noreply.github.com>

--- x-pack/plugins/maps/public/classes/layers/ems_vector_tile_layer/ems_vector_tile_layer.tsx
+++ x-pack/plugins/maps/public/classes/layers/ems_vector_tile_layer/ems_vector_tile_layer.tsx
@@ -492,7 +492,7 @@ export class EmsVectorTileLayer extends AbstractLayer {
     return {
       icon: <EuiIcon size="m" type="grid" />,
       tooltipContent: i18n.translate('xpack.maps.emsVectorTileLayer.layerDescription', {
-        defaultMessage: `Reference map provided by Elastic Maps Service (EMS)`,
+        defaultMessage: `Reference map provided by Elastic Maps Service (EMS).`,
       }),
     };
   }
