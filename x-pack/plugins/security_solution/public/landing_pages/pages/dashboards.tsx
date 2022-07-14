eb3f534b6f59cdb759130ae5922b8d19cda1a702
Sergi Massaneda <sergi.massaneda@elastic.co>
2022-07-14 12:07:50 +0200

    [Security] Dashboards table in landing page (#136221)
    * dashboard landing cards
    
    * useSecurityDashboards hook implementation
    
    * useSecurityDashboards hook implementation
    
    * add savedObjectsTagging to security
    
    * tests implemented
    
    * rename section titles
    
    * remove test code
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    * PR suggestions
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/landing_pages/pages/dashboards.tsx
+++ x-pack/plugins/security_solution/public/landing_pages/pages/dashboards.tsx
@@ -4,22 +4,39 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { EuiHorizontalRule, EuiSpacer, EuiTitle } from '@elastic/eui';
 import React from 'react';
 import { SecurityPageName } from '../../app/types';
+import { DashboardsTable } from '../../common/components/dashboards/dashboards_table';
 import { HeaderPage } from '../../common/components/header_page';
 import { useAppRootNavLink } from '../../common/components/navigation/nav_links';
 import { SecuritySolutionPageWrapper } from '../../common/components/page_wrapper';
 import { SpyRoute } from '../../common/utils/route/spy_routes';
-import { LandingLinksImages } from '../components/landing_links_images';
-import { DASHBOARDS_PAGE_TITLE } from './translations';
+import { LandingImageCards } from '../components/landing_links_images';
+import * as i18n from './translations';
 
 export const DashboardsLandingPage = () => {
   const dashboardLinks = useAppRootNavLink(SecurityPageName.dashboardsLanding)?.links ?? [];
 
   return (
     <SecuritySolutionPageWrapper>
-      <HeaderPage title={DASHBOARDS_PAGE_TITLE} />
-      <LandingLinksImages items={dashboardLinks} />
+      <HeaderPage title={i18n.DASHBOARDS_PAGE_TITLE} />
+      <EuiSpacer size="s" />
+
+      <EuiTitle size="xxxs">
+        <h2>{i18n.DASHBOARDS_PAGE_SECTION_DEFAULT}</h2>
+      </EuiTitle>
+      <EuiHorizontalRule margin="s" />
+      <LandingImageCards items={dashboardLinks} />
+      <EuiSpacer size="xxl" />
+
+      <EuiTitle size="xxxs">
+        <h2>{i18n.DASHBOARDS_PAGE_SECTION_CUSTOM}</h2>
+      </EuiTitle>
+      <EuiHorizontalRule margin="s" />
+      <EuiSpacer size="m" />
+      <DashboardsTable />
+
       <SpyRoute pageName={SecurityPageName.dashboardsLanding} />
     </SecuritySolutionPageWrapper>
   );
