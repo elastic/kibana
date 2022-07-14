d9f9c70b96b2fcfb4daa04230e53bc3fad111a21
Patryk Kopycinski <contact@patrykkopycinski.com>
2022-07-14 15:53:15 +0200

    fix jest

--- x-pack/plugins/cases/public/components/all_cases/selector_modal/all_cases_selector_modal.test.tsx
+++ x-pack/plugins/cases/public/components/all_cases/selector_modal/all_cases_selector_modal.test.tsx
@@ -12,7 +12,7 @@ import { AllCasesSelectorModal } from '.';
 import { TestProviders } from '../../../common/mock';
 import { AllCasesList } from '../all_cases_list';
 
-jest.mock('../all_cases_list', () => ({ AllCasesList: () => <></> }));
+jest.mock('../all_cases_list', () => ({ AllCasesList: jest.fn().mockReturnValue(<></>) }));
 
 const onRowClick = jest.fn();
 const defaultProps = {
@@ -45,7 +45,7 @@ describe('AllCasesSelectorModal', () => {
     expect(wrapper.find(`[data-test-subj='all-cases-modal']`).exists()).toBeFalsy();
   });
 
-  it.skip('pass the correct props to getAllCases method', () => {
+  it('pass the correct props to getAllCases method', () => {
     const fullProps = {
       ...defaultProps,
       hiddenStatuses: [],
@@ -57,8 +57,7 @@ describe('AllCasesSelectorModal', () => {
       </TestProviders>
     );
 
-    // @ts-ignore idk what this mock style is but it works ¯\_(ツ)_/¯
-    expect(AllCasesList.type.mock.calls[0][0]).toEqual(
+    expect((AllCasesList as unknown as jest.Mock).mock.calls[0][0]).toEqual(
       expect.objectContaining({
         hiddenStatuses: fullProps.hiddenStatuses,
         isSelectorView: true,
