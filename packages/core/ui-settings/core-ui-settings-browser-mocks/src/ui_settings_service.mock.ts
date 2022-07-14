6eea56449e90eb6a9151f215beb5fc04ff86aa8d
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-07-14 08:30:28 -0700

    Removes unused react dependency

--- packages/core/ui-settings/core-ui-settings-browser-mocks/src/ui_settings_service.mock.ts
+++ packages/core/ui-settings/core-ui-settings-browser-mocks/src/ui_settings_service.mock.ts
@@ -8,7 +8,7 @@
 
 import * as Rx from 'rxjs';
 import type { PublicMethodsOf } from '@kbn/utility-types';
-import { UiSettingsService } from '@kbn/core-ui-settings-browser-internal';
+import type { UiSettingsService } from '@kbn/core-ui-settings-browser-internal';
 import type { IUiSettingsClient } from '@kbn/core-ui-settings-browser';
 
 const createSetupContractMock = () => {
