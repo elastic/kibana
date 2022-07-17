e2ac9a7052dd4b71df99b338c936867bdc8bbd15
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-07-17 16:09:10 -0700

    redeclares createMockRawNsDataHistogram in perf_hooks mock

--- packages/core/metrics/core-metrics-server-internal/src/event_loop_delays/__mocks__/perf_hooks.ts
+++ packages/core/metrics/core-metrics-server-internal/src/event_loop_delays/__mocks__/perf_hooks.ts
@@ -6,7 +6,32 @@
  * Side Public License, v 1.
  */
 
-import { createMockRawNsDataHistogram } from '../event_loop_delays_monitor.test.mocks';
+import { IntervalHistogram } from '@kbn/core-metrics-server';
+import moment from 'moment';
+
+export function createMockRawNsDataHistogram(
+  overwrites: Partial<IntervalHistogram> = {}
+): IntervalHistogram {
+  const now = Date.now();
+
+  const mockedRawCollectedDataInNs = {
+    min: 9093120,
+    max: 53247999,
+    mean: 11993238,
+    exceeds: 0,
+    stddev: 1168191,
+    fromTimestamp: moment(now).toISOString(),
+    lastUpdatedAt: moment(now).toISOString(),
+    percentiles: {
+      '50': 12607487,
+      '75': 12615679,
+      '95': 12648447,
+      '99': 12713983,
+    },
+    ...overwrites,
+  };
+  return mockedRawCollectedDataInNs;
+}
 
 export const monitorEventLoopDelay = jest.fn().mockImplementation(() => {
   const mockedHistogram = createMockRawNsDataHistogram();
