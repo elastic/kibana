b4f31ab23ef5cc8a42394065c29b2481b372e2d5
Joseph Crail <joseph.crail@elastic.co>
2022-07-18 10:52:24 -0700

    Fix types for render functions in table

--- x-pack/plugins/profiling/public/components/topn_functions.tsx
+++ x-pack/plugins/profiling/public/components/topn_functions.tsx
@@ -11,10 +11,46 @@ import { EuiBasicTable } from '@elastic/eui';
 
 import { FunctionContext } from './contexts/function';
 
+function getCalleeFunction(frame: StackFrameMetadata): string {
+  // In the best case scenario, we have the file names, source lines,
+  // and function names. However we need to deal with missing function or
+  // executable info.
+  const exeDisplayName = frame.ExeFileName ? frame.ExeFileName : frame.FrameTypeString;
+
+  // When there is no function name, only use the executable name
+  return frame.FunctionName ? exeDisplayName + ': ' + frame.FunctionName : exeDisplayName;
+}
+
+function getCalleeSource(frame: StackFrameMetadata): string {
+  if (frame.FunctionName === '' && frame.SourceLine === 0) {
+    if (frame.ExeFileName) {
+      // If no source line or filename available, display the executable offset
+      return frame.ExeFileName + '+0x' + frame.AddressOrLine.toString(16);
+    }
+
+    // If we don't have the executable filename, display <unsymbolized>
+    return '<unsymbolized>';
+  }
+
+  if (frame.SourceFilename !== '' && frame.SourceLine === 0) {
+    return frame.SourceFilename;
+  }
+
+  return frame.SourceFilename + (frame.FunctionOffset !== 0 ? `#${frame.FunctionOffset}` : '');
+}
+
+interface Row {
+  rank: number;
+  frame: StackFrameMetadata;
+  samples: number;
+  exclusiveCPU: number;
+  inclusiveCPU: number;
+}
+
 export const TopNFunctionsTable = () => {
   const ctx = useContext(FunctionContext);
 
-  const items = useMemo(() => {
+  const rows: Row[] = useMemo(() => {
     if (!ctx || !ctx.TotalCount || ctx.TotalCount === 0) {
       return [];
     }
@@ -44,18 +80,18 @@ export const TopNFunctionsTable = () => {
     {
       field: 'exclusiveCPU',
       name: 'Exclusive CPU',
-      render: (item) => `${Number(item).toFixed(2)}%`,
+      render: (row: Row) => `${row.exclusiveCPU.toFixed(2)}%`,
     },
     {
       field: 'inclusiveCPU',
       name: 'Inclusive CPU',
-      render: (item) => `${Number(item).toFixed(2)}%`,
+      render: (row: Row) => `${row.inclusiveCPU.toFixed(2)}%`,
     },
   ];
 
   return (
     <>
-      <EuiBasicTable items={items} columns={columns} />
+      <EuiBasicTable items={rows} columns={columns} />
     </>
   );
 };
