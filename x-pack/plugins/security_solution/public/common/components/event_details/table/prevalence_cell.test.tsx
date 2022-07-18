bb7d141a9b8ebc9834ad5ff7e78640142fa5f549
Jan Monschke <jan.monschke@elastic.co>
2022-07-18 17:55:08 +0200

    test: remove unused tests

--- x-pack/plugins/security_solution/public/common/components/event_details/table/prevalence_cell.test.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/table/prevalence_cell.test.tsx
@@ -15,7 +15,6 @@ import type { EventFieldsData } from '../types';
 import { TimelineId } from '../../../../../common/types';
 import type { AlertSummaryRow } from '../helpers';
 import { useAlertPrevalence } from '../../../containers/alerts/use_alert_prevalence';
-import { useAlertPrevalenceFromProcessTree } from '../../../containers/alerts/use_alert_prevalence_from_process_tree';
 import { getEmptyValue } from '../../empty_value';
 
 jest.mock('../../../lib/kibana');
@@ -23,11 +22,6 @@ jest.mock('../../../containers/alerts/use_alert_prevalence', () => ({
   useAlertPrevalence: jest.fn(),
 }));
 const mockUseAlertPrevalence = useAlertPrevalence as jest.Mock;
-
-jest.mock('../../../containers/alerts/use_alert_prevalence_from_process_tree', () => ({
-  useAlertPrevalenceFromProcessTree: jest.fn(),
-}));
-const mockUseAlertPrevalenceFromProcessTree = useAlertPrevalenceFromProcessTree as jest.Mock;
 
 const eventId = 'TUWyf3wBFCFU0qRJTauW';
 const hostIpValues = ['127.0.0.1', '::1', '10.1.2.3', '2001:0DB8:AC10:FE01::'];
@@ -148,57 +142,6 @@ describe('PrevalenceCellRenderer', () => {
         );
         expect(container.getElementsByClassName('euiLoadingSpinner')).toHaveLength(0);
         expect(screen.queryByText('123')).toBeInTheDocument();
-      });
-    });
-  });
-
-  describe('From Process Tree', () => {
-    describe('When data is loading', () => {
-      test('it should show the loading spinner', async () => {
-        mockUseAlertPrevalenceFromProcessTree.mockImplementation(() => ({
-          loading: true,
-          error: true,
-        }));
-        const { container } = render(
-          <TestProviders>
-            <PrevalenceCellRenderer {...enrichedProcessEntityData} />
-          </TestProviders>
-        );
-        expect(container.getElementsByClassName('euiLoadingSpinner')).toHaveLength(1);
-      });
-    });
-
-    describe('When an error was returned', () => {
-      test('it should return empty value placeholder', async () => {
-        mockUseAlertPrevalenceFromProcessTree.mockImplementation(() => ({
-          loading: false,
-          error: true,
-        }));
-        const { container } = render(
-          <TestProviders>
-            <PrevalenceCellRenderer {...enrichedProcessEntityData} />
-          </TestProviders>
-        );
-        expect(container.getElementsByClassName('euiLoadingSpinner')).toHaveLength(0);
-        expect(screen.queryByText('123')).toBeNull();
-        expect(screen.queryByText(getEmptyValue())).toBeTruthy();
-      });
-    });
-
-    describe('When an actual count is returned', () => {
-      test('it should show the count', async () => {
-        mockUseAlertPrevalenceFromProcessTree.mockImplementation(() => ({
-          loading: false,
-          alertIds: ['87f4dc72-3dd6-4cc3-bcd3-6faf42a21667', '2039j20-3dd6-4cc3-bcd3-6faf42a21667'],
-          error: false,
-        }));
-        const { container } = render(
-          <TestProviders>
-            <PrevalenceCellRenderer {...enrichedProcessEntityData} />
-          </TestProviders>
-        );
-        expect(container.getElementsByClassName('euiLoadingSpinner')).toHaveLength(0);
-        expect(screen.queryByText('2')).toBeInTheDocument();
       });
     });
   });
