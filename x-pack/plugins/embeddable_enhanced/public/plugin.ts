743146d8a835f1a47c6fd3503e886cb8aa12b2a4
Tiago Costa <tiago.costa@elastic.co>
2022-07-18 19:17:29 +0100

    chore(NA): specific rules for a few naked eslint disable

--- x-pack/plugins/embeddable_enhanced/public/plugin.ts
+++ x-pack/plugins/embeddable_enhanced/public/plugin.ts
@@ -126,7 +126,7 @@ export class EmbeddableEnhancedPlugin
     });
 
     dynamicActions.start().catch((error) => {
-      /* eslint-disable */
+      /* eslint-disable no-console */
 
       console.log('Failed to start embeddable dynamic actions', embeddable);
       console.error(error);
@@ -135,7 +135,7 @@ export class EmbeddableEnhancedPlugin
 
     const stop = () => {
       dynamicActions.stop().catch((error) => {
-        /* eslint-disable */
+        /* eslint-disable no-console */
 
         console.log('Failed to stop embeddable dynamic actions', embeddable);
         console.error(error);
