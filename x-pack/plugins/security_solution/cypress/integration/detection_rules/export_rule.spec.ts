39de6fd0997435af4ce5369d1c8e6f7ff9755401
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2022-07-19 16:02:05 +0100

    add cypress tests

--- x-pack/plugins/security_solution/cypress/integration/detection_rules/export_rule.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/detection_rules/export_rule.spec.ts
@@ -5,13 +5,24 @@
  * 2.0.
  */
 
-import { expectedExportedRule, getNewRule } from '../../objects/rule';
+import { expectedExportedRule, getNewRule, totalNumberOfPrebuiltRules } from '../../objects/rule';
 
-import { TOASTER_BODY } from '../../screens/alerts_detection_rules';
+import {
+  TOASTER_BODY,
+  MODAL_CONFIRMATION_BODY,
+  MODAL_CONFIRMATION_BTN,
+} from '../../screens/alerts_detection_rules';
 
-import { exportFirstRule } from '../../tasks/alerts_detection_rules';
+import {
+  exportFirstRule,
+  loadPrebuiltDetectionRulesFromHeaderBtn,
+  switchToElasticRules,
+  selectNumberOfRules,
+  bulkExportRules,
+  selectAllRules,
+} from '../../tasks/alerts_detection_rules';
 import { createCustomRule } from '../../tasks/api_calls/rules';
-import { cleanKibana } from '../../tasks/common';
+import { cleanKibana, deleteAlertsAndRules } from '../../tasks/common';
 import { login, visitWithoutDateRange } from '../../tasks/login';
 
 import { DETECTIONS_RULE_MANAGEMENT_URL } from '../../urls/navigation';
@@ -23,6 +34,7 @@ describe('Export rules', () => {
   });
 
   beforeEach(() => {
+    deleteAlertsAndRules();
     // Rules get exported via _bulk_action endpoint
     cy.intercept('POST', '/api/detection_engine/rules/_bulk_action').as('bulk_action');
     visitWithoutDateRange(DETECTIONS_RULE_MANAGEMENT_URL);
@@ -38,5 +50,43 @@ describe('Export rules', () => {
         'Successfully exported 1 of 1 rule. Prebuilt rules were excluded from the resulting file.'
       );
     });
+  });
+
+  it("shows modal that no rules can't be exported if all rules are prebuilt", function () {
+    const expectedElasticRulesCount = 7;
+
+    loadPrebuiltDetectionRulesFromHeaderBtn();
+
+    switchToElasticRules();
+    selectNumberOfRules(expectedElasticRulesCount);
+    bulkExportRules();
+
+    cy.get(MODAL_CONFIRMATION_BODY).contains(
+      `${expectedElasticRulesCount} prebuilt Elastic rules (exporting prebuilt rules is not supported)`
+    );
+  });
+
+  it('exports only custom rules', function () {
+    const expectedNumberCustomRulesToBeExported = 1;
+    const totalNumberOfRules = expectedNumberCustomRulesToBeExported + totalNumberOfPrebuiltRules;
+
+    loadPrebuiltDetectionRulesFromHeaderBtn();
+
+    selectAllRules();
+    bulkExportRules();
+
+    cy.get(MODAL_CONFIRMATION_BODY).contains(
+      `${totalNumberOfPrebuiltRules} prebuilt Elastic rules (exporting prebuilt rules is not supported)`
+    );
+
+    // proceed with exporting only custom rules
+    cy.get(MODAL_CONFIRMATION_BTN)
+      .should('have.text', `Export ${expectedNumberCustomRulesToBeExported} Custom rule`)
+      .click();
+
+    cy.get(TOASTER_BODY).should(
+      'contain',
+      `Successfully exported ${expectedNumberCustomRulesToBeExported} of ${totalNumberOfRules} rules`
+    );
   });
 });
