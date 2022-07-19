336a9e5b5da4f6a765096685f4437939e6135537
Tre' Seymour <wayne.seymour@elastic.co>
2022-07-19 15:55:41 +0100

    fixup

--- scripts/archive_migration_functions.sh
+++ scripts/archive_migration_functions.sh
@@ -2,9 +2,9 @@
 
 standard_list="url,index-pattern,query,graph-workspace,tag,visualization,canvas-element,canvas-workpad,dashboard,search,lens,map,cases,uptime-dynamic-settings,osquery-saved-query,osquery-pack,infrastructure-ui-source,metrics-explorer-view,inventory-view,infrastructure-monitoring-log-view,apm-indices"
 
-orig_archive="x-pack/test/functional/es_archives/banners/multispace"
-new_archive="x-pack/test/functional/fixtures/kbn_archiver/banners/multispace"
-test_config="x-pack/test/functional/apps/spaces/config.ts"
+orig_archive="x-pack/test/functional/es_archives/reporting/ecommerce_kibana_spaces"
+new_archive="x-pack/test/functional/fixtures/kbn_archiver/reporting/ecommerce_kibana_spaces"
+test_config="x-pack/test/reporting_api_integration/reporting_and_security.config.ts"
 
 arrayify_csv() {
   local xs=${1}
