e70602817ee922f4e6ad8440fe9f486fc79302f0
Yulia Cech <yulia.cech@elastic.co>
2022-07-19 14:44:02 +0200

    [Guided onboarding] Fixed the skip link on the onboarding landing page

--- src/plugins/home/public/application/components/guided_onboarding/getting_started.test.tsx
+++ src/plugins/home/public/application/components/guided_onboarding/getting_started.test.tsx
@@ -8,24 +8,50 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
+import { findTestSubject, mountWithIntl } from '@kbn/test-jest-helpers';
 
 import { GettingStarted } from './getting_started';
+import { KEY_ENABLE_WELCOME } from '../home';
 
 jest.mock('../../kibana_services', () => {
   const { chromeServiceMock, applicationServiceMock } =
     jest.requireActual('@kbn/core/public/mocks');
+  const { uiSettingsServiceMock } = jest.requireActual('@kbn/core-ui-settings-browser-mocks');
+
+  const uiSettingsMock = uiSettingsServiceMock.createSetupContract();
+  uiSettingsMock.get.mockReturnValue(false);
   return {
     getServices: () => ({
       chrome: chromeServiceMock.createStartContract(),
       application: applicationServiceMock.createStartContract(),
       trackUiMetric: jest.fn(),
+      uiSettings: uiSettingsMock,
+      http: {
+        basePath: {
+          prepend: jest.fn(),
+        },
+      },
     }),
   };
 });
 describe('getting started', () => {
+  beforeAll(() => {
+    localStorage.removeItem(KEY_ENABLE_WELCOME);
+  });
+  afterAll(() => {
+    localStorage.removeItem(KEY_ENABLE_WELCOME);
+  });
   test('should render getting started component', async () => {
     const component = await shallow(<GettingStarted />);
 
     expect(component).toMatchSnapshot();
+  });
+
+  test('skip button should disable home welcome screen', async () => {
+    const component = mountWithIntl(<GettingStarted />);
+    const skipButton = findTestSubject(component, 'onboarding--skipUseCaseTourLink');
+    skipButton.simulate('click');
+
+    expect(localStorage.getItem(KEY_ENABLE_WELCOME)).toBe('false');
   });
 });
