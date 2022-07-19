eb1c48d635bb7a55ac12fb6f43a3db36de8c8830
semd <sergi.massaneda@elastic.co>
2022-07-19 20:40:00 +0200

    add create security dashboard button

--- x-pack/plugins/security_solution/public/landing_pages/pages/dashboards.tsx
+++ x-pack/plugins/security_solution/public/landing_pages/pages/dashboards.tsx
@@ -4,24 +4,61 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EuiHorizontalRule, EuiSpacer, EuiTitle } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiHorizontalRule,
+  EuiSpacer,
+  EuiTitle,
+} from '@elastic/eui';
 import React from 'react';
 import { SecurityPageName } from '../../app/types';
 import { DashboardsTable } from '../../common/components/dashboards/dashboards_table';
-import { HeaderPage } from '../../common/components/header_page';
+import { Title } from '../../common/components/header_page/title';
 import { useAppRootNavLink } from '../../common/components/navigation/nav_links';
 import { SecuritySolutionPageWrapper } from '../../common/components/page_wrapper';
+import { useCreateSecurityDashboard } from '../../common/containers/dashboards/use_create_security_dashboard';
+import { useNavigateTo } from '../../common/lib/kibana';
 import { SpyRoute } from '../../common/utils/route/spy_routes';
 import { LandingImageCards } from '../components/landing_links_images';
 import * as i18n from './translations';
+
+/* eslint-disable @elastic/eui/href-or-on-click */
+const Header = () => {
+  const { isLoading, url } = useCreateSecurityDashboard();
+  const { navigateTo } = useNavigateTo();
+  return (
+    <EuiFlexGroup gutterSize="none" direction="row">
+      <EuiFlexItem>
+        <Title title={i18n.DASHBOARDS_PAGE_TITLE} />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiButton
+          isDisabled={isLoading}
+          color="primary"
+          fill
+          iconType="plusInCircle"
+          href={url}
+          onClick={(ev) => {
+            ev.preventDefault();
+            navigateTo({ url });
+          }}
+        >
+          {i18n.DASHBOARDS_PAGE_CREATE_BUTTON}
+        </EuiButton>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
 
 export const DashboardsLandingPage = () => {
   const dashboardLinks = useAppRootNavLink(SecurityPageName.dashboardsLanding)?.links ?? [];
 
   return (
     <SecuritySolutionPageWrapper>
-      <HeaderPage title={i18n.DASHBOARDS_PAGE_TITLE} />
-      <EuiSpacer size="s" />
+      <Header />
+      <EuiSpacer size="xl" />
 
       <EuiTitle size="xxxs">
         <h2>{i18n.DASHBOARDS_PAGE_SECTION_DEFAULT}</h2>
