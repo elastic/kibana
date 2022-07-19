7f062bb094912602ee08f00740763906fd1cbec1
Greg Thompson <thompson.glowe@gmail.com>
2022-07-19 15:00:39 -0500

    update regex capture groups to ignore categorical info for text

--- x-pack/test/functional/services/ml/common_data_grid.ts
+++ x-pack/test/functional/services/ml/common_data_grid.ts
@@ -43,7 +43,7 @@ export function MachineLearningCommonDataGridProvider({ getService }: FtrProvide
         .toArray()
         .map((cell) => {
           const cellText = $(cell).text();
-          const pattern = /^(.*), column (\d+), row (\d+)$/;
+          const pattern = /^(.*)-(?:.*), column (\d+), row (\d+)$/;
           const matches = cellText.match(pattern);
           expect(matches).to.not.eql(null, `Cell text should match pattern '${pattern}'`);
           return { text: matches![1], column: Number(matches![2]), row: Number(matches![3]) };
