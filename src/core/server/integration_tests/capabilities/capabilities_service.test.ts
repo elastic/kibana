370c1dbe7e0b22773ff5f4572e2081ca10264645
pgayvallet <pierre.gayvallet@elastic.co>
2022-07-20 11:26:15 +0200

    adapt integration tests

--- src/core/server/integration_tests/capabilities/capabilities_service.test.ts
+++ src/core/server/integration_tests/capabilities/capabilities_service.test.ts
@@ -19,7 +19,8 @@ import {
   InternalHttpServiceSetup,
 } from '@kbn/core-http-server-internal';
 import { createHttpServer } from '@kbn/core-http-server-mocks';
-import { CapabilitiesService, CapabilitiesSetup } from '../../capabilities';
+import type { CapabilitiesSetup } from '@kbn/core-capabilities-server';
+import { CapabilitiesService } from '../../capabilities';
 
 const coreId = Symbol('core');
 
