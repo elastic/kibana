7649ec01e23050bbac1617256a6e8bb2099f102c
Or Ouziel <or.ouziel@elastic.co>
2022-07-20 11:51:25 +0300

    fix benchmark test conflicts

--- x-pack/plugins/cloud_security_posture/server/routes/benchmarks/benchmarks.test.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/benchmarks/benchmarks.test.ts
@@ -4,30 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-<<<<<<< HEAD
 import { httpServerMock, httpServiceMock, savedObjectsClientMock } from '@kbn/core/server/mocks';
-||||||| 7fa68136b43
-import {
-  httpServerMock,
-  httpServiceMock,
-  loggingSystemMock,
-  savedObjectsClientMock,
-} from '@kbn/core/server/mocks';
-import {
-  ElasticsearchClientMock,
-  // eslint-disable-next-line @kbn/eslint/no-restricted-paths
-} from '@kbn/core/server/elasticsearch/client/mocks';
-import type { KibanaRequest } from '@kbn/core/server';
-=======
-import {
-  httpServerMock,
-  httpServiceMock,
-  loggingSystemMock,
-  savedObjectsClientMock,
-} from '@kbn/core/server/mocks';
-import type { ElasticsearchClientMock } from '@kbn/core-elasticsearch-client-server-mocks';
-import type { KibanaRequest } from '@kbn/core/server';
->>>>>>> upstream/main
 import {
   benchmarksQueryParamsSchema,
   DEFAULT_BENCHMARKS_PER_PAGE,
