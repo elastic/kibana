283e4bb82af83f5c324b1584fe7d0c854b6dc10d
CJ Cenizal <cj.cenizal@elastic.co>
2022-07-20 10:30:49 -0700

    Preserve getResponseStatus page object function.

--- test/functional/page_objects/console_page.ts
+++ test/functional/page_objects/console_page.ts
@@ -269,4 +269,10 @@ export class ConsolePageObject extends FtrService {
       return false;
     }
   }
+
+  public async getResponseStatus() {
+    const statusBadge = await this.testSubjects.find('consoleResponseStatusBadge');
+    const text = await statusBadge.getVisibleText();
+    return text.replace(/[^\d.]+/, '');
+  }
 }
