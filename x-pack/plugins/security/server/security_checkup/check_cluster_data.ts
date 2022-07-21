d22d97f7c3c6909013e6710a101350244998ca2f
Kurt Greiner <kurt.greiner@elastic.co>
2022-07-21 16:43:10 -0400

    Replacing call to 'cat' API with 'indices' API

--- x-pack/plugins/security/server/security_checkup/check_cluster_data.ts
+++ x-pack/plugins/security/server/security_checkup/check_cluster_data.ts
@@ -13,18 +13,19 @@ export const createClusterDataCheck = () => {
   return async function doesClusterHaveUserData(esClient: ElasticsearchClient, log: Logger) {
     if (!clusterHasUserData) {
       try {
-        const indices = await esClient.cat.indices({
-          format: 'json',
-          h: ['index', 'docs.count'],
-        });
-        clusterHasUserData = indices.some((indexCount) => {
-          const isInternalIndex =
-            indexCount.index?.startsWith('.') || indexCount.index?.startsWith('kibana_sample_');
+        const { indices = {} } = await esClient.indices.stats();
+        const indexIds = indices ? Object.keys(indices) : [];
 
-          return !isInternalIndex && parseInt(indexCount['docs.count']!, 10) > 0;
+        clusterHasUserData = indexIds.some((indexName: string) => {
+          const isInternalIndex =
+            indexName.startsWith('.') || indexName.startsWith('kibana_sample_');
+          const hasDocs = (indices[indexName].primaries?.docs?.count || 0) > 0;
+
+          return !isInternalIndex && hasDocs;
         });
       } catch (e) {
         log.warn(`Error encountered while checking cluster for user data: ${e}`);
+
         clusterHasUserData = false;
       }
     }
