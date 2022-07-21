347d74b4a65a12fc4967dd95d347705463e8c303
Zizhou Wang <zizhou.wang@elastic.co>
2022-07-21 18:47:28 -0400

    Add entry leader entity id filter to all queries

--- x-pack/plugins/kubernetes_security/public/utils/add_timerange_and_default_filter_to_query.test.ts
+++ x-pack/plugins/kubernetes_security/public/utils/add_timerange_and_default_filter_to_query.test.ts
@@ -5,8 +5,8 @@
  * 2.0.
  */
 
-import { DEFAULT_QUERY } from '../../common/constants';
-import { addTimerangeToQuery } from './add_timerange_to_query';
+import { DEFAULT_FILTER_QUERY } from '../../common/constants';
+import { addTimerangeAndDefaultFilterToQuery } from './add_timerange_and_default_filter_to_query';
 
 const TEST_QUERY =
   '{"bool":{"must":[],"filter":[{"bool":{"should":[{"match":{"process.entry_leader.same_as_process":true}}],"minimum_should_match":1}}],"should":[],"must_not":[]}}';
@@ -14,24 +14,36 @@ const TEST_INVALID_QUERY = '{"bool":{"must":[';
 const TEST_EMPTY_STRING = '';
 const TEST_DATE = '2022-06-09T22:36:46.628Z';
 const VALID_RESULT =
-  '{"bool":{"must":[],"filter":[{"bool":{"should":[{"match":{"process.entry_leader.same_as_process":true}}],"minimum_should_match":1}},{"range":{"@timestamp":{"gte":"2022-06-09T22:36:46.628Z","lte":"2022-06-09T22:36:46.628Z"}}}],"should":[],"must_not":[]}}';
+  '{"bool":{"must":[],"filter":[{"bool":{"should":[{"exists":{"field":"process.entry_leader.entity_id"}}],"minimum_should_match":1}},{"bool":{"should":[{"match":{"process.entry_leader.same_as_process":true}}],"minimum_should_match":1}},{"range":{"@timestamp":{"gte":"2022-06-09T22:36:46.628Z","lte":"2022-06-09T22:36:46.628Z"}}}],"should":[],"must_not":[]}}';
 
-describe('addTimerangeToQuery(query, startDate, endDate)', () => {
+describe('addTimerangeAndDefaultFilterToQuery(query, startDate, endDate)', () => {
   it('works for valid query, startDate, and endDate', () => {
-    expect(addTimerangeToQuery(TEST_QUERY, TEST_DATE, TEST_DATE)).toEqual(VALID_RESULT);
+    expect(addTimerangeAndDefaultFilterToQuery(TEST_QUERY, TEST_DATE, TEST_DATE)).toEqual(
+      VALID_RESULT
+    );
   });
   it('works with missing filter in bool', () => {
-    expect(addTimerangeToQuery('{"bool":{}}', TEST_DATE, TEST_DATE)).toEqual(
+    expect(addTimerangeAndDefaultFilterToQuery('{"bool":{}}', TEST_DATE, TEST_DATE)).toEqual(
       '{"bool":{"filter":[{"range":{"@timestamp":{"gte":"2022-06-09T22:36:46.628Z","lte":"2022-06-09T22:36:46.628Z"}}}]}}'
     );
   });
   it('returns default query with invalid JSON query', () => {
-    expect(addTimerangeToQuery(TEST_INVALID_QUERY, TEST_DATE, TEST_DATE)).toEqual(DEFAULT_QUERY);
-    expect(addTimerangeToQuery(TEST_EMPTY_STRING, TEST_DATE, TEST_DATE)).toEqual(DEFAULT_QUERY);
-    expect(addTimerangeToQuery('{}', TEST_DATE, TEST_DATE)).toEqual(DEFAULT_QUERY);
+    expect(addTimerangeAndDefaultFilterToQuery(TEST_INVALID_QUERY, TEST_DATE, TEST_DATE)).toEqual(
+      DEFAULT_FILTER_QUERY
+    );
+    expect(addTimerangeAndDefaultFilterToQuery(TEST_EMPTY_STRING, TEST_DATE, TEST_DATE)).toEqual(
+      DEFAULT_FILTER_QUERY
+    );
+    expect(addTimerangeAndDefaultFilterToQuery('{}', TEST_DATE, TEST_DATE)).toEqual(
+      DEFAULT_FILTER_QUERY
+    );
   });
   it('returns default query with invalid startDate or endDate', () => {
-    expect(addTimerangeToQuery(TEST_QUERY, TEST_EMPTY_STRING, TEST_DATE)).toEqual(DEFAULT_QUERY);
-    expect(addTimerangeToQuery(TEST_QUERY, TEST_DATE, TEST_EMPTY_STRING)).toEqual(DEFAULT_QUERY);
+    expect(addTimerangeAndDefaultFilterToQuery(TEST_QUERY, TEST_EMPTY_STRING, TEST_DATE)).toEqual(
+      DEFAULT_FILTER_QUERY
+    );
+    expect(addTimerangeAndDefaultFilterToQuery(TEST_QUERY, TEST_DATE, TEST_EMPTY_STRING)).toEqual(
+      DEFAULT_FILTER_QUERY
+    );
   });
 });
