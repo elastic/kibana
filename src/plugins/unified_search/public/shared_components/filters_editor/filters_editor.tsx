5573adc4a75d516971e014e09a4eda97390f4d81
Alexey Antonov <alexwizp@gmail.com>
2022-07-21 13:58:19 +0300

    apply some changes

---
+++ src/plugins/unified_search/public/shared_components/filters_editor/filters_editor.tsx
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import type { DataView } from '@kbn/data-views-plugin/common';
+import type { Filter } from '@kbn/es-query';
+
+export interface FiltersEditorProps {
+  filters: Filter[];
+  dataViews: DataView[];
+  onChange: (filters: []) => void;
+}
+
+export function FiltersEditor({ onChange, dataViews, filters }: FiltersEditorProps) {
+  return <h2> Filter Editor</h2>;
+}
--- src/plugins/unified_search/public/shared_components/index.ts
+++ src/plugins/unified_search/public/shared_components/index.ts
@@ -1,9 +1,23 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
-export { FiltersEditor } from './filters_editor';
-export type { filtersEditorProps } from './filters_editor';
+import React from 'react';
+import { withSuspense } from '@kbn/shared-ux-utility';
+
+/**
+ * The Lazily-loaded `FiltersEditor` component.  Consumers should use `React.Suspense` or
+ * the withSuspense` HOC to load this component.
+ */
+export const FiltersEditorLazy = React.lazy(() => import('./filters_editor'));
+
+/**
+ * A `FiltersEditor` component that is wrapped by the `withSuspense` HOC. This component can
+ * be used directly by consumers and will load the `FiltersEditorLazy` component lazily with
+ * a predefined fallback and error boundary.
+ */
+export const CustomizablePalette = withSuspense(FiltersEditorLazy);
