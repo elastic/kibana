afff958bcb4570ace8fb9d7c708f2958380ef2da
Julian Gernun <julian.gernun@elastic.co>
2022-07-21 17:25:50 +0200

    [RAM] Bulk Actions for Alerts Table (#135797)
    * update to almost working selection
    
    * working bulk actions
    
    * fix selection through pages
    
    * working callback
    
    * fix some types and remove logs
    
    * refactoring bulk actions reducer
    
    * add test and update types
    
    * remove console.logs
    
    * update last page test
    
    * fix test performance
    
    * fix initial context state
    
    * abstract bulkactions into own hook
    
    * remove old checkbox and bulkactions props
    
    * update usage of enum
    
    * first commit
    
    * remove logs and update contract
    
    * update o11y integration working
    
    * remove not needed code
    
    * improve performance
    
    * update on bulkAction integration mapping
    
    * data field name type as optional
    
    * update translations
    
    * fix test and remove not needed param
    
    * remove not finished test
    
    * remove not needed translations
    
    * performance + readability
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_table_state.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_table_state.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { useState, useCallback, useRef, useMemo } from 'react';
+import React, { useState, useCallback, useRef, useMemo, useReducer } from 'react';
 import { isEmpty } from 'lodash';
 import {
   EuiDataGridColumn,
@@ -22,15 +22,34 @@ import type {
 } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { useFetchAlerts } from './hooks/use_fetch_alerts';
 import { AlertsTable } from './alerts_table';
+import { BulkActionsContext } from './bulk_actions/context';
 import { EmptyState } from './empty_state';
-import { AlertsTableConfigurationRegistry } from '../../../types';
+import {
+  AlertsTableConfigurationRegistry,
+  AlertsTableProps,
+  BulkActionsReducerAction,
+  BulkActionsState,
+} from '../../../types';
 import { ALERTS_TABLE_CONF_ERROR_MESSAGE, ALERTS_TABLE_CONF_ERROR_TITLE } from './translations';
 import { TypeRegistry } from '../../type_registry';
+import { bulkActionsReducer } from './bulk_actions/reducer';
 
 const DefaultPagination = {
   pageSize: 10,
   pageIndex: 0,
 };
+
+interface CasePermission {
+  all: boolean;
+  read: boolean;
+}
+
+interface CaseUi {
+  ui: {
+    getCasesContext: () => React.FC<any>;
+  };
+  permissions: CasePermission;
+}
 
 export interface AlertsTableStateProps {
   alertsTableConfigurationRegistry: TypeRegistry<AlertsTableConfigurationRegistry>;
@@ -41,6 +60,7 @@ export interface AlertsTableStateProps {
   query: Pick<QueryDslQueryContainer, 'bool' | 'ids'>;
   pageSize?: number;
   showExpandToDetails: boolean;
+  cases?: CaseUi;
 }
 
 interface AlertsTableStorage {
@@ -66,6 +86,17 @@ const EmptyConfiguration = {
   getRenderCellValue: () => () => null,
 };
 
+const AlertsTableWithBulkActionsContextComponent: React.FunctionComponent<{
+  tableProps: AlertsTableProps;
+  initialBulkActionsState: [BulkActionsState, React.Dispatch<BulkActionsReducerAction>];
+}> = ({ tableProps, initialBulkActionsState }) => (
+  <BulkActionsContext.Provider value={initialBulkActionsState}>
+    <AlertsTable {...tableProps} />
+  </BulkActionsContext.Provider>
+);
+
+const AlertsTableWithBulkActionsContext = React.memo(AlertsTableWithBulkActionsContextComponent);
+
 const AlertsTableState = ({
   alertsTableConfigurationRegistry,
   configurationId,
@@ -75,6 +106,7 @@ const AlertsTableState = ({
   query,
   pageSize,
   showExpandToDetails,
+  cases,
 }: AlertsTableStateProps) => {
   const hasAlertsTableConfiguration =
     alertsTableConfigurationRegistry?.has(configurationId) ?? false;
@@ -108,7 +140,6 @@ const AlertsTableState = ({
         : columnsLocal.map((c) => c.id),
   });
 
-  const [showCheckboxes] = useState(false);
   const [sort, setSort] = useState<SortCombinations[]>(storageAlertsTable.current.sort);
   const [pagination, setPagination] = useState({
     ...DefaultPagination,
@@ -131,6 +162,14 @@ const AlertsTableState = ({
   const onPageChange = useCallback((_pagination: RuleRegistrySearchRequestPagination) => {
     setPagination(_pagination);
   }, []);
+
+  const initialBulkActionsState = useReducer(bulkActionsReducer, {
+    rowSelection: new Set<number>(),
+    isAllSelected: false,
+    areAllVisibleRowsSelected: false,
+    rowCount: alerts.length,
+  });
+
   const onSortChange = useCallback(
     (_sort: EuiDataGridSorting['columns']) => {
       const newSort = _sort.map((sortItem) => {
@@ -202,7 +241,6 @@ const AlertsTableState = ({
       pageSize: pagination.pageSize,
       pageSizeOptions: [10, 20, 50, 100],
       leadingControlColumns: [],
-      showCheckboxes,
       showExpandToDetails,
       trailingControlColumns: [],
       useFetchAlertsData,
@@ -214,11 +252,12 @@ const AlertsTableState = ({
       columns,
       flyoutSize,
       pagination.pageSize,
-      showCheckboxes,
       showExpandToDetails,
       useFetchAlertsData,
     ]
   );
+
+  const CasesContext = cases?.ui.getCasesContext();
 
   return hasAlertsTableConfiguration ? (
     <>
@@ -226,7 +265,24 @@ const AlertsTableState = ({
       {isLoading && (
         <EuiProgress size="xs" color="accent" data-test-subj="internalAlertsPageLoading" />
       )}
-      {alertsCount !== 0 && <AlertsTable {...tableProps} />}
+      {alertsCount !== 0 && CasesContext && cases && (
+        <CasesContext
+          owner={[configurationId]}
+          permissions={cases.permissions}
+          features={{ alerts: { sync: false } }}
+        >
+          <AlertsTableWithBulkActionsContext
+            tableProps={tableProps}
+            initialBulkActionsState={initialBulkActionsState}
+          />
+        </CasesContext>
+      )}
+      {alertsCount !== 0 && (!CasesContext || !cases) && (
+        <AlertsTableWithBulkActionsContext
+          tableProps={tableProps}
+          initialBulkActionsState={initialBulkActionsState}
+        />
+      )}
     </>
   ) : (
     <EuiEmptyPrompt
