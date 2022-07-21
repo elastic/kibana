adbe0d470313c3379969b06c35dad5e1da613c43
Mark Hopkin <mark.hopkin@elastic.co>
2022-07-21 20:30:51 +0100

    mount zip packages as part of tests

--- x-pack/test/fleet_api_integration/config.ts
+++ x-pack/test/fleet_api_integration/config.ts
@@ -28,19 +28,17 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const registryPort: string | undefined = process.env.FLEET_PACKAGE_REGISTRY_PORT;
 
   // mount the config file for the package registry as well as
-  // the directory containing additional packages into the container
-  const dockerArgs: string[] = [
-    '-v',
-    `${path.join(
-      path.dirname(__filename),
-      './apis/fixtures/package_registry_config.yml'
-    )}:/package-registry/config.yml`,
-    '-v',
-    `${path.join(
-      path.dirname(__filename),
-      './apis/fixtures/test_packages'
-    )}:/packages/test-packages`,
-  ];
+  // the directories containing additional packages into the container
+  const volumes = {
+    // src : dest
+    './apis/fixtures/package_registry_config.yml': '/package-registry/config.yml',
+    './apis/fixtures/test_packages': '/packages/test-packages',
+    './apis/fixtures/package_verification/packages/zips': '/packages/signed-test-packages',
+  };
+  const dockerArgs: string[] = Object.entries(volumes).flatMap(([src, dest]) => {
+    const relativeSrc = path.join(path.dirname(__filename), src);
+    return ['-v', `${relativeSrc}:${dest}`];
+  });
 
   return {
     testFiles: [require.resolve('./apis')],
