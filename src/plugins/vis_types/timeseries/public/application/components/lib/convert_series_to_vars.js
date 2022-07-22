db2ce95a29289e4b6326f349b8bf0dc74475f490
Joe Reuter <johannes.reuter@elastic.co>
2022-07-22 10:55:30 +0200

    format the label with the right default formatter

--- src/plugins/vis_types/timeseries/public/application/components/lib/convert_series_to_vars.js
+++ src/plugins/vis_types/timeseries/public/application/components/lib/convert_series_to_vars.js
@@ -17,7 +17,7 @@ import { createFieldFormatter } from './create_field_formatter';
 import moment from 'moment';
 import { getFieldsForTerms } from '../../../../common/fields_utils';
 
-export const convertSeriesToVars = (series, model, getConfig = null, fieldFormatMap) => {
+export const convertSeriesToVars = (series, model, getConfig = null, fieldFormatMap, dataView) => {
   const variables = {};
   const dateFormat = getConfig?.('dateFormat') ?? 'lll';
   model.series.forEach((seriesModel) => {
@@ -57,7 +57,13 @@ export const convertSeriesToVars = (series, model, getConfig = null, fieldFormat
           const fieldsForTerms = getFieldsForTerms(seriesModel.terms_field);
 
           if (fieldsForTerms.length === 1) {
-            rowLabel = createFieldFormatter(fieldsForTerms[0], fieldFormatMap)(row.label);
+            rowLabel = createFieldFormatter(
+              fieldsForTerms[0],
+              fieldFormatMap,
+              undefined,
+              false,
+              dataView
+            )(row.label);
           }
         }
 
