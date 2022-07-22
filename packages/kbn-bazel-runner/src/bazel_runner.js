a4b2591efe39008fa39adf0f83746afca75b5697
spalger <spencer@elastic.co>
2022-07-22 10:54:38 -0700

    ignore lines that are "from *" some step

--- packages/kbn-bazel-runner/src/bazel_runner.js
+++ packages/kbn-bazel-runner/src/bazel_runner.js
@@ -105,7 +105,7 @@ async function runBazelRunner(runner, args, options = undefined) {
   if (process.env.CI) {
     // on CI it's useful to reduce the logging output, but we still want to see basic info from Bazel so continue to log the INFO: lines from bazel
     for (const line of buffer) {
-      if (line.startsWith('INFO:')) {
+      if (line.startsWith('INFO:') && !line.startsWith('INFO: From ')) {
         console.log(options?.logPrefix ? `${options.logPrefix} ${line}` : line);
       }
     }
