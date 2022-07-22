c469244ec331c818ea0584204c079f7f1aaee007
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2022-07-22 15:30:08 +0300

    Update src/plugins/discover/public/application/main/hooks/use_discover_state.ts
    Co-authored-by: Matthias Wilhelm <matthias.wilhelm@elastic.co>

--- src/plugins/discover/public/application/main/hooks/use_discover_state.ts
+++ src/plugins/discover/public/application/main/hooks/use_discover_state.ts
@@ -284,7 +284,7 @@ export function useDiscoverState({
             index: dataViewObj.id,
             ...(columns.length && { columns }),
           };
-          stateContainer.setAppState(nextState);
+          stateContainer.replaceUrlAppState(nextState);
         }
       }
     }
