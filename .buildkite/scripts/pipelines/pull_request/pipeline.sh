107bd52227e479eac767219fcdb49842f293af76
Brian Seeders <brian.seeders@elastic.co>
2022-07-22 14:12:16 -0400

    Combine some steps that run quickly and on default agents

--- .buildkite/scripts/pipelines/pull_request/pipeline.sh
+++ .buildkite/scripts/pipelines/pull_request/pipeline.sh
@@ -2,4 +2,14 @@
 
 set -euo pipefail
 
+echo --- Pre-Build
+.buildkite/scripts/lifecycle/pre_build.sh
+
+echo --- Upload Pipeline
 ts-node .buildkite/scripts/pipelines/pull_request/pipeline.ts
+
+echo --- Pick Test Group Run Order
+export JEST_UNIT_SCRIPT=.buildkite/scripts/steps/test/jest.sh
+export JEST_INTEGRATION_SCRIPT=.buildkite/scripts/steps/test/jest_integration.sh
+export FTR_CONFIGS_SCRIPT=.buildkite/scripts/steps/test/ftr_configs.sh
+.buildkite/scripts/steps/test/pick_test_group_run_order.sh
