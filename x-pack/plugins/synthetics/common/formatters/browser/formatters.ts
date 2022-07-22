0958e55c1706de5bc7dbd5e06d8fc56174b2e2b4
Shahzad <shahzad.muhammad@elastic.co>
2022-07-22 14:53:09 +0200

    [Synthetics] Enable private locations via elastic agent (#135782)
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Nicolas Chaulet <nicolas.chaulet@elastic.co>

--- x-pack/plugins/synthetics/common/formatters/browser/formatters.ts
+++ x-pack/plugins/synthetics/common/formatters/browser/formatters.ts
@@ -4,8 +4,8 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { BrowserFields, ConfigKey } from '../../runtime_types/monitor_management';
 
-import { BrowserFields, ConfigKey } from '../types';
 import {
   Formatter,
   commonFormatters,
