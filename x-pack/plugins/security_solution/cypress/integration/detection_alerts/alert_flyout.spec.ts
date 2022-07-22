e3829de927dcb4bdeb19c65c6a7980b4d598fbb7
Jan Monschke <jan.monschke@elastic.co>
2022-07-22 10:39:47 +0200

    test: the `auditbeat` archive is loaded by default
    - also: removing the `auditbeat` archive can lead to unwanted side-effects (TIL)

--- x-pack/plugins/security_solution/cypress/integration/detection_alerts/alert_flyout.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/detection_alerts/alert_flyout.spec.ts
@@ -19,7 +19,6 @@ import { openOverview } from '../../tasks/alerts_details';
 import { createCustomRuleEnabled } from '../../tasks/api_calls/rules';
 import { cleanKibana } from '../../tasks/common';
 import { waitForAlertsToPopulate } from '../../tasks/create_new_rule';
-import { esArchiverLoad, esArchiverUnload } from '../../tasks/es_archiver';
 import { login, visitWithoutDateRange } from '../../tasks/login';
 
 import { getNewRule } from '../../objects/rule';
@@ -30,15 +29,11 @@ import { CLOSE_TIMELINE_BUTTON } from '../../screens/security_main';
 describe('Alert Flyout', () => {
   before(() => {
     cleanKibana();
-    esArchiverLoad('auditbeat');
     login();
     createCustomRuleEnabled(getNewRule(), 'rule1');
     visitWithoutDateRange(ALERTS_URL);
     waitForAlertsToPopulate();
     expandFirstAlert();
-  });
-  after(() => {
-    esArchiverUnload('auditbeat');
   });
 
   afterEach(() => {
