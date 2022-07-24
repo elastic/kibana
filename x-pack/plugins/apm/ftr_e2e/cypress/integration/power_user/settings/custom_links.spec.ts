6a64043173d3a1eb859e66ef2900c9468be07b57
Oliver Gupte <oliver.gupte@elastic.co>
2022-07-24 19:15:57 -0400

    adds e2e test to validate the fix

--- x-pack/plugins/apm/ftr_e2e/cypress/integration/power_user/settings/custom_links.spec.ts
+++ x-pack/plugins/apm/ftr_e2e/cypress/integration/power_user/settings/custom_links.spec.ts
@@ -34,4 +34,17 @@ describe('Custom links', () => {
     cy.get('[data-test-subj="editCustomLink"]').click();
     cy.contains('Delete').click();
   });
+
+  it('clears filter values when field is selected', () => {
+    cy.visit(basePath);
+    cy.contains('Create custom link').click();
+    cy.get('[data-test-subj="filter-0"]').select('service.name');
+    cy.get(
+      '[data-test-subj="service.name.value"] [data-test-subj="comboBoxSearchInput"]'
+    ).type('foo');
+    cy.get('[data-test-subj="filter-0"]').select('service.environment');
+    cy.get(
+      '[data-test-subj="service.environment.value"] [data-test-subj="comboBoxInput"]'
+    ).should('not.contain', 'foo');
+  });
 });
