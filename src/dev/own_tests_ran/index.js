bb541ed2a055e1766cb108558d7e76f1a05f31c2
Tre' Seymour <wayne.seymour@elastic.co>
2022-07-25 12:01:39 +0100

    Let's just see what this does in ci.

--- src/dev/own_tests_ran/index.js
+++ src/dev/own_tests_ran/index.js
@@ -10,8 +10,9 @@ import { run } from '@kbn/dev-cli-runner';
 // import { createFlagError, createFailError } from '@kbn/dev-cli-errors';
 import { REPO_ROOT } from '@kbn/utils';
 import yaml from 'js-yaml';
-import { readFileSync } from "fs";
-import { resolve } from "path";
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { getPrChanges } from '../../../.buildkite/pipeline-utils';
 
 const flags = {
   string: ['path'],
@@ -24,14 +25,17 @@ const resolveRoot = resolve.bind(null, REPO_ROOT);
 
 export function runCheckOwnTestsRanCli() {
   run(
-    ({ flags, log }) => {
+    async ({ flags, log }) => {
       log.info('\n### Running runCheckOwnTestsRanCli()');
 
       console.log(`\n### flags: \n${JSON.stringify(flags, null, 2)}`);
 
-      yaml.load(readFileSync(resolveRoot('src/dev/own_tests_ran/test_roots.yml'), 'utf8'))
-        .general
-        .forEach(x => console.log(`\n### x: \n\t${x}`))
+      yaml
+        .load(readFileSync(resolveRoot('src/dev/own_tests_ran/test_roots.yml'), 'utf8'))
+        .general.forEach((x) => console.log(`\n### x: \n\t${x}`));
+
+      const res = await getPrChanges();
+      console.log(`\n### res: \n${JSON.stringify(res, null, 2)}`);
     },
     {
       description: `
