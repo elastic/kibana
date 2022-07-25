213146025af31a7ea223b4e4beae7c6ca9c6bb72
Tre' Seymour <wayne.seymour@elastic.co>
2022-07-25 13:38:20 +0100

    put this back how it was

--- .buildkite/scripts/steps/test/pick_test_group_run_order.sh
+++ .buildkite/scripts/steps/test/pick_test_group_run_order.sh
@@ -4,8 +4,5 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
-#echo '--- Pick Test Group Run Order'
-#ts-node "$(dirname "${0}")/pick_test_group_run_order.ts"
-
-echo '--- Hijacked Pick Test Group Run Order'
-.buildkite/scripts/steps/checks/own_tests_ran.sh
+echo '--- Pick Test Group Run Order'
+ts-node "$(dirname "${0}")/pick_test_group_run_order.ts"
