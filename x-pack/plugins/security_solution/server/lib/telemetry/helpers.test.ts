9f4871a772c9e9e9633876a6cf81477ce89f0052
JD Kurma <JDKurma@gmail.com>
2022-07-25 11:58:22 -0400

    linting

--- x-pack/plugins/security_solution/server/lib/telemetry/helpers.test.ts
+++ x-pack/plugins/security_solution/server/lib/telemetry/helpers.test.ts
@@ -434,7 +434,7 @@ describe('test advanced policy config overlap ', () => {
       },
     },
   } as unknown as PolicyConfig;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line
   const defaultStubPolicyConfigResponse = {
     linux: {
       events: {
