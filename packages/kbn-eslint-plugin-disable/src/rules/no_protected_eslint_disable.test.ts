888a186053cae8c962aa809e8413a154ea7b8a80
spalger <spencer@elastic.co>
2022-07-26 15:47:15 -0500

    add failing test

--- packages/kbn-eslint-plugin-disable/src/rules/no_protected_eslint_disable.test.ts
+++ packages/kbn-eslint-plugin-disable/src/rules/no_protected_eslint_disable.test.ts
@@ -244,6 +244,26 @@ for (const [name, tester] of [tsTester, babelTester]) {
             const a = 1;
           `,
         },
+        {
+          filename: 'foo.ts',
+          code: dedent`
+            /* eslint-disable no-var, @kbn/disable/no_protected_eslint_disable */
+            const a = 1;
+          `,
+          errors: [
+            {
+              line: 1,
+              messageId: PROTECTED_DISABLE_MSG_ID,
+              data: {
+                disabledRuleName: '@kbn/disable/no_protected_eslint_disable',
+              },
+            },
+          ],
+          output: dedent`
+            /* eslint-disable no-var */
+            const a = 1;
+          `,
+        },
         // generic invalid tests for disable comments
         {
           filename: 'foo.ts',
