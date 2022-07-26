97c0b32159f92f4d85143085a7b64f8dbbbefb29
Walter Rafelsberger <walter@elastic.co>
2022-07-26 18:54:42 +0200

    [ML] Explain log rate spikes: Improve analysis workflow. (#137192)
    - Adds an empty prompt when there's no current selection to describe the necessary click action and some general feature description.
    - Adds an option to clear the current selection and start from scratch.
    - Adds badges to the brush areas to label them as Baseline and Deviation.
    - Adds EuiPanel as wrappers for the chart and table sections. The distinct sections make it easier to identify to which context an action like the analysis refresh button refers.

--- x-pack/plugins/aiops/public/components/document_count_content/document_count_content/document_count_content.tsx
+++ x-pack/plugins/aiops/public/components/document_count_content/document_count_content/document_count_content.tsx
@@ -4,8 +4,11 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { FC } from 'react';
+import React, { useEffect, useState, FC } from 'react';
 
+import { EuiButtonEmpty, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+
+import { i18n } from '@kbn/i18n';
 import type { WindowParameters } from '@kbn/aiops-utils';
 import type { ChangePoint } from '@kbn/ml-agg-utils';
 
@@ -13,21 +16,38 @@ import { DocumentCountChart, DocumentCountChartPoint } from '../document_count_c
 import { TotalCountHeader } from '../total_count_header';
 import { DocumentCountStats } from '../../../get_document_stats';
 
+const clearSelectionLabel = i18n.translate(
+  'xpack.aiops.documentCountContent.clearSelectionAriaLabel',
+  {
+    defaultMessage: 'Clear selection',
+  }
+);
+
 export interface DocumentCountContentProps {
   brushSelectionUpdateHandler: (d: WindowParameters) => void;
+  clearSelectionHandler: () => void;
   changePoint?: ChangePoint;
   documentCountStats?: DocumentCountStats;
   documentCountStatsSplit?: DocumentCountStats;
   totalCount: number;
+  windowParameters?: WindowParameters;
 }
 
 export const DocumentCountContent: FC<DocumentCountContentProps> = ({
   brushSelectionUpdateHandler,
+  clearSelectionHandler,
   changePoint,
   documentCountStats,
   documentCountStatsSplit,
   totalCount,
+  windowParameters,
 }) => {
+  const [isBrushCleared, setIsBrushCleared] = useState(true);
+
+  useEffect(() => {
+    setIsBrushCleared(windowParameters === undefined);
+  }, [windowParameters]);
+
   if (documentCountStats === undefined) {
     return totalCount !== undefined ? <TotalCountHeader totalCount={totalCount} /> : null;
   }
@@ -48,18 +68,48 @@ export const DocumentCountContent: FC<DocumentCountContentProps> = ({
     chartPointsSplit = Object.entries(buckets).map(([time, value]) => ({ time: +time, value }));
   }
 
+  function brushSelectionUpdate(d: WindowParameters, force: boolean) {
+    if (!isBrushCleared || force) {
+      brushSelectionUpdateHandler(d);
+    }
+    if (force) {
+      setIsBrushCleared(false);
+    }
+  }
+
+  function clearSelection() {
+    setIsBrushCleared(true);
+    clearSelectionHandler();
+  }
+
   return (
     <>
-      <TotalCountHeader totalCount={totalCount} />
+      <EuiFlexGroup gutterSize="xs">
+        <EuiFlexItem>
+          <TotalCountHeader totalCount={totalCount} />
+        </EuiFlexItem>
+        {!isBrushCleared && (
+          <EuiFlexItem grow={false}>
+            <EuiButtonEmpty
+              onClick={clearSelection}
+              size="xs"
+              data-test-sub="aiopsClearSelectionBadge"
+            >
+              {clearSelectionLabel}
+            </EuiButtonEmpty>
+          </EuiFlexItem>
+        )}
+      </EuiFlexGroup>
       {documentCountStats.interval !== undefined && (
         <DocumentCountChart
-          brushSelectionUpdateHandler={brushSelectionUpdateHandler}
+          brushSelectionUpdateHandler={brushSelectionUpdate}
           chartPoints={chartPoints}
           chartPointsSplit={chartPointsSplit}
           timeRangeEarliest={timeRangeEarliest}
           timeRangeLatest={timeRangeLatest}
           interval={documentCountStats.interval}
           changePoint={changePoint}
+          isBrushCleared={isBrushCleared}
         />
       )}
     </>
