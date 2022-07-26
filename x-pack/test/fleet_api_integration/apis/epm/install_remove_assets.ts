670b94b9efe4d72ec9d6f77dc6ebb30ebc3616b5
Mark Hopkin <mark.hopkin@elastic.co>
2022-07-26 23:26:52 +0100

    [Fleet] Enable package signature verification feature (#137239)
    * [Fleet] Enable package verification feature
    
    * handle cases where verification_key_id is set to null

--- x-pack/test/fleet_api_integration/apis/epm/install_remove_assets.ts
+++ x-pack/test/fleet_api_integration/apis/epm/install_remove_assets.ts
@@ -528,6 +528,12 @@ const expectAssetsInstalled = ({
     // during a reinstall the items can change
     const sortedRes = {
       ...res.attributes,
+      // verification_key_id can be null or undefined for install or reinstall cases,
+      // kbn/expect only does strict equality so undefined is normalised to null
+      verification_key_id:
+        res.attributes.verification_key_id === undefined
+          ? null
+          : res.attributes.verification_key_id,
       installed_kibana: sortBy(res.attributes.installed_kibana, (o: AssetReference) => o.type),
       installed_es: sortBy(res.attributes.installed_es, (o: AssetReference) => o.type),
       package_assets: sortBy(res.attributes.package_assets, (o: AssetReference) => o.type),
@@ -768,6 +774,7 @@ const expectAssetsInstalled = ({
       install_source: 'registry',
       install_format_schema_version: FLEET_INSTALL_FORMAT_VERSION,
       verification_status: 'unknown',
+      verification_key_id: null,
     });
   });
 };
