96c6182db857146a53ace9a49e96314fe36e5248
Clint Andrew Hall <clint.hall@elastic.co>
2022-07-25 20:34:32 -0500

    [home] Sample Data Tab w/ callout UI (#136790)
    * [home] Sample Data Tab w/ callout UI
    
    * Fix tests
    
    * Update packages/home/sample_data_tab_content/src/demo_env_panel.tsx
    
    Co-authored-by: Kelly Murphy <kelly.murphy@elastic.co>
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * Fixes
    
    * Fixes
    
    * Update test/functional/page_objects/home_page.ts
    
    * Fix tests
    
    * Add telemetry
    
    * Add docs, more telemetry
    
    Co-authored-by: Kelly Murphy <kelly.murphy@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

---
+++ packages/home/sample_data_card/src/sample_data_card.stories.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import type { SampleDataSet } from '@kbn/home-sample-data-types';
+import { Params, getStoryArgTypes, getStoryServices, mockDataSet } from './mocks';
+import { SampleDataCardProvider } from './services';
+import { SampleDataCard } from './sample_data_card';
+
+import mdx from '../README.mdx';
+
+export default {
+  title: 'Sample Data/Card',
+  description:
+    'A card describing a Sample Data Set, with options to install it, remove it, or see its saved objects.',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+  decorators: [(Story) => <div style={{ width: '433px', padding: '25px' }}>{Story()}</div>],
+} as ComponentMeta<typeof SampleDataCard>;
+
+const argTypes = getStoryArgTypes();
+
+export const Card = (params: Params) => {
+  const { includeAppLinks, ...rest } = params;
+  const sampleDataSet: SampleDataSet = {
+    ...mockDataSet,
+    ...rest,
+    appLinks: includeAppLinks ? mockDataSet.appLinks : [],
+  };
+
+  return (
+    <SampleDataCardProvider {...getStoryServices(params)}>
+      <SampleDataCard sampleDataSet={sampleDataSet} onStatusChange={action('onStatusChange')} />
+    </SampleDataCardProvider>
+  );
+};
+
+Card.argTypes = argTypes;
