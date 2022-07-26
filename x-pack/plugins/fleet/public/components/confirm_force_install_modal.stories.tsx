65e7dded420be134bcc5a6056f97b8a3ec1cb5f8
Mark Hopkin <mark.hopkin@elastic.co>
2022-07-26 20:23:22 +0100

    fix types

--- x-pack/plugins/fleet/public/components/confirm_force_install_modal.stories.tsx
+++ x-pack/plugins/fleet/public/components/confirm_force_install_modal.stories.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type { DocLinksStart } from '@kbn/core/types';
+import type { DocLinksStart } from '@kbn/core/public';
 import React from 'react';
 
 import { ConfirmForceInstallModal } from './confirm_force_install_modal';
@@ -48,6 +48,7 @@ const argTypes = {
 export const UnverifiedIntegrationModal = ({ packageName, packageVersion }: Args) => {
   const mockDocLinks: DocLinksStart = {
     links: {
+      // @ts-ignore only defining what we need
       fleet: {
         packageSignatures: 'elastic.co',
       },
