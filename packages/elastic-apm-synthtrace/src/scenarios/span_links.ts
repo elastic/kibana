76944d2a9fd983b64321d4d1f211d585ab1f83ac
spalger <spencer@elastic.co>
2022-07-25 20:17:04 -0500

    rename "scripts" dir to "cli"

--- packages/elastic-apm-synthtrace/src/scenarios/span_links.ts
+++ packages/elastic-apm-synthtrace/src/scenarios/span_links.ts
@@ -9,7 +9,7 @@
 import { compact, shuffle } from 'lodash';
 import { apm, ApmFields, EntityArrayIterable, timerange } from '..';
 import { generateLongId, generateShortId } from '../lib/utils/generate_id';
-import { Scenario } from '../scripts/scenario';
+import { Scenario } from '../cli/scenario';
 
 function generateExternalSpanLinks() {
   // randomly creates external span links 0 - 10
