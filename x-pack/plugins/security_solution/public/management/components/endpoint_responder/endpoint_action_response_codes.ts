6b30801fdcab8f760b94091df3fb54facc38fab4
Paul Tavares <paul.tavares@elastic.co>
2022-07-27 16:48:07 -0400

    add some jsdocs to the endpoint codes

--- x-pack/plugins/security_solution/public/management/components/endpoint_responder/endpoint_action_response_codes.ts
+++ x-pack/plugins/security_solution/public/management/components/endpoint_responder/endpoint_action_response_codes.ts
@@ -11,11 +11,19 @@ const CODES = Object.freeze({
   // -----------------------------------------------------------------
   // SUSPEND-PROCESS CODES
   // -----------------------------------------------------------------
+  /**
+   * Code will be used whenever you provide an entity_id or pid that isn't found.
+   * suspend_process will always be an error because the process was not found to be suspended
+   */
   'ra_suspend-process_error_not-found': i18n.translate(
     'xpack.securitySolution.endpointActionResponseCodes.suspendProcess.notFoundError',
     { defaultMessage: 'The provided process was not found' }
   ),
 
+  /**
+   * Code will be used when the provided process can not be killed (for stability reasons).
+   * Example: This occurs if you try to kill Endpoint Security
+   */
   'ra_suspend-process_error_not-permitted': i18n.translate(
     'xpack.securitySolution.endpointActionResponseCodes.suspendProcess.notPermittedSuccess',
     { defaultMessage: 'The provided process cannot be suspended' }
@@ -24,16 +32,30 @@ const CODES = Object.freeze({
   // -----------------------------------------------------------------
   // KILL-PROCESS CODES
   // -----------------------------------------------------------------
+  /**
+   * Code will be used whenever you provide an entity_id that isn't found. Since entity_id is
+   * unique, we can guarantee that it was legitimately not found and not just that the process
+   * was already killed.
+   */
   'ra_kill-process_error_not-found': i18n.translate(
     'xpack.securitySolution.endpointActionResponseCodes.killProcess.notFoundError',
     { defaultMessage: 'The provided process was not found' }
   ),
 
+  /**
+   * Code will be used whenever you provide a pid that isn't found. Since pid is reused, we aren't
+   * sure if the process was already killed or just wasn't found. In either case, a process with
+   * that pid will no longer be running.
+   */
   'ra_kill-process_success_no-action': i18n.translate(
     'xpack.securitySolution.endpointActionResponseCodes.killProcess.noActionSuccess',
     { defaultMessage: 'Action completed. The provided process was not found or already killed' }
   ),
 
+  /**
+   * Code will be used when the provided process can not be killed (for stability reasons).
+   * Example: This occurs if you try to kill Endpoint Security
+   */
   'ra_kill-process_error_not-permitted': i18n.translate(
     'xpack.securitySolution.endpointActionResponseCodes.killProcess.notPermittedSuccess',
     { defaultMessage: 'The provided process cannot be killed' }
