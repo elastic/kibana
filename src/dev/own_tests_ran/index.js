e72bd6bb12ae06621de6c119bbffe245cb413a54
Tre' Seymour <wayne.seymour@elastic.co>
2022-07-27 17:06:32 +0100

    Little more progress.

--- src/dev/own_tests_ran/index.js
+++ src/dev/own_tests_ran/index.js
@@ -6,13 +6,15 @@
  * Side Public License, v 1.
  */
 
-import { run } from '@kbn/dev-cli-runner';
+import {run} from '@kbn/dev-cli-runner';
 // import { createFlagError, createFailError } from '@kbn/dev-cli-errors';
-import { REPO_ROOT } from '@kbn/utils';
+import {REPO_ROOT} from '@kbn/utils';
 import yaml from 'js-yaml';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-import { getPrChanges } from '../../../.buildkite/pipeline-utils';
+import {readFileSync} from 'fs';
+import {resolve} from 'path';
+import {getPrChanges} from '../../../.buildkite/pipeline-utils';
+import {filter, from} from 'rxjs';
+import {pluck} from 'rxjs/operators';
 
 const flags = {
   string: ['path'],
@@ -24,40 +26,49 @@ const flags = {
 const resolveRoot = resolve.bind(null, REPO_ROOT);
 
 export function runCheckOwnTestsRanCli() {
-  run(
-    async ({ flags, log }) => {
-      log.info('\n### Running runCheckOwnTestsRanCli()');
-
-      console.log(`\n### flags: \n${JSON.stringify(flags, null, 2)}`);
-
-      // TODO-TRE: We may or may not use this test roots idea.
-      yaml
-        .load(readFileSync(resolveRoot('src/dev/own_tests_ran/test_roots.yml'), 'utf8'))
-        .general.forEach((x) => console.log(`\n### x: \n\t${x}`));
-
-      // TODO-TRE: This gets the info I need about the pr (filenames, etc)
-      const xs = await getPrChanges();
-      console.log(`\n### xs: \n${JSON.stringify(xs, null, 2)}`);
-
-      const fileNames = xs.map((x) => x.filename);
-
-      console.log(`\n### fileNames: \n${JSON.stringify(fileNames, null, 2)}`);
-
-      // TODO-TRE: Next, we need access to the log output, from ci.
-      // TODO-TRE: We prolly can use the fileNames above, find the test(s) file(s)
-      // TODO-TRE: , find their configs and use the configs and see where they are used in
-      // TODO-TRE: , the "pick test group run order" output.
-      // TODO-TRE: From there, we should be able to stream through the ouput.
-      // TODO-TRE: This link mentions it's possible: https://buildkite.com/docs/apis/rest-api/jobs#get-a-jobs-log-output
-
-    },
-    {
-      description: `
+  run(process, {
+    description: `
 
 blah blah blah
 
       `,
-      flags,
-    }
-  );
+    flags,
+  });
+}
+
+const roots = () =>
+  yaml
+    .load(
+      readFileSync(
+        resolveRoot('src/dev/own_tests_ran/test_roots.yml'), 'utf8'
+      )
+    )
+    .general;
+
+const isTest = (regexes) => (filePath) =>
+  regexes
+    .some((re) => re.test(filePath));
+
+async function process({flags, log}) {
+  log.info('\n### Running runCheckOwnTestsRanCli()');
+
+  console.log(`\n### flags: \n${JSON.stringify(flags, null, 2)}`);
+
+  // TODO-TRE: Next, we need access to the log output, from ci.
+  // TODO-TRE: We prolly can use the fileNames above, find the test(s) file(s)
+  // TODO-TRE: , find their configs and use the configs and see where they are used in
+  // TODO-TRE: , the "pick test group run order" output.
+  // TODO-TRE: From there, we should be able to stream through the ouput.
+  // TODO-TRE: This link mentions it's possible: https://buildkite.com/docs/apis/rest-api/jobs#get-a-jobs-log-output
+
+  const $ = from(await getPrChanges());
+  $.pipe(
+    pluck('filename'),
+    filter(isTest(roots().map((x) => new RegExp(x)))))
+    .subscribe({
+      next: (x) => console.log(`\n### x2: \n\t${x}`),
+      // next: noop,
+      error: (x) => console.error(`\n### x: \n\t${x}`),
+      complete: () => console.log('\n### Complete'),
+    });
 }
