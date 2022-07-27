850dd16e950cb842504796c956d6b0e8c2a1f5b9
rshen91 <rshen@elastic.co>
2022-07-27 10:53:41 -0600

    update test

---
+++ packages/shared-ux/button_toolbar/src/toolbar/toolbar.test.tsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import { mountWithIntl } from '@kbn/test-jest-helpers';
+
+import { Toolbar } from './toolbar';
+import { PrimaryButton } from '../buttons';
+
+describe('<Toolbar />', () => {
+  test('is rendered', () => {
+    const primaryButton =   <PrimaryButton
+    label="Create chart"
+    onClick={() => 'click'}
+  />
+    const children = {primaryButton, };
+    const component = mountWithIntl(<Toolbar children={children} />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
