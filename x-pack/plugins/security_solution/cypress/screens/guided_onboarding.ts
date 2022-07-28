ecbf90d93755d16ce6c3fd31f4a289296e7dd106
Yulia Čech <6585477+yuliacech@users.noreply.github.com>
2022-07-28 17:38:23 +0200

    [Guided onboarding] Security tour fixes (#136958)
    * [Guided onboarding] Security tour: Fixed localStorage value parsing, also "add integrations" closes the tour
    
    * [Guided onboarding] Security tour: Cypress tests
    
    * [Guided onboarding] Security tour: Update text copy
    
    * [Guided onboarding] Security tour: Added useCallback

---
+++ x-pack/plugins/security_solution/cypress/screens/guided_onboarding.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export const WELCOME_STEP = '[data-test-subj="welcomeStep"]';
+export const MANAGE_STEP = '[data-test-subj="manageStep"]';
+export const ALERTS_STEP = '[data-test-subj="alertsStep"]';
+export const CASES_STEP = '[data-test-subj="casesStep"]';
+export const DATA_STEP = '[data-test-subj="dataStep"]';
+
+export const NEXT_STEP_BUTTON = '[data-test-subj="onboarding--securityTourNextStepButton"]';
+export const END_TOUR_BUTTON = '[data-test-subj="onboarding--securityTourEndButton"]';
+export const SKIP_TOUR_BUTTON = '[data-test-subj="onboarding--securityTourSkipButton"]';
