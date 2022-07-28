16cbf42f47b1c0b32a06ee68482b51e1b1fe983e
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2022-07-28 15:22:58 +0200

    Fix hardcoded doclinks in SO migration integ tests (#137414)
    * fix hardcoded doc links in SO migration IT
    
    * unskip tests
    
    * fix link value

---
+++ src/core/server/integration_tests/saved_objects/migrations/test_utils.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { Env } from '@kbn/config';
+import { getDocLinksMeta, getDocLinks } from '@kbn/doc-links';
+import { REPO_ROOT } from '@kbn/utils';
+import { getEnvOptions } from '@kbn/config-mocks';
+
+export const getDocVersion = () => {
+  const env = Env.createDefault(REPO_ROOT, getEnvOptions());
+  return getDocLinksMeta({ kibanaBranch: env.packageInfo.branch }).version;
+};
+
+export const getMigrationDocLink = () => {
+  const env = Env.createDefault(REPO_ROOT, getEnvOptions());
+  const docLinks = getDocLinks({ kibanaBranch: env.packageInfo.branch });
+  return docLinks.kibanaUpgradeSavedObjects;
+};
