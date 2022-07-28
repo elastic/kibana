aa30995cd146ae77c7ba36ad220f1aa3d17d274b
Byron Hulcher <byron.hulcher@elastic.co>
2022-07-28 16:55:39 -0400

    Add remaining Search Index docLinks (#137502)
    (cherry picked from commit 8bfbfd2ff68b4fed7ea526b7d59303b7ac8a1730)

--- packages/kbn-doc-links/src/get_doc_links.ts
+++ packages/kbn-doc-links/src/get_doc_links.ts
@@ -118,6 +118,7 @@ export const getDocLinks = ({ kibanaBranch }: GetDocLinkOptions): DocLinks => {
       webCrawlerReference: `${APP_SEARCH_DOCS}web-crawler-reference.html`,
     },
     enterpriseSearch: {
+      apiKeys: `${KIBANA_DOCS}api-keys.html`,
       bulkApi: `${ELASTICSEARCH_DOCS}docs-bulk.html`,
       configuration: `${ENTERPRISE_SEARCH_DOCS}configuration.html`,
       crawlerGettingStarted: `${ENTERPRISE_SEARCH_DOCS}crawler-getting-started.html`,
