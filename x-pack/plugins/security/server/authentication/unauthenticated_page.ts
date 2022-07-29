f9fdf356a3bfe7861772bbd7814488b1a13f5bc1
Najmieh Sadat <98463228+najmiehsa@users.noreply.github.com>
2022-07-29 21:34:32 +0430

    further edits

--- x-pack/plugins/security/server/authentication/unauthenticated_page.ts
+++ x-pack/plugins/security/server/authentication/unauthenticated_page.ts
@@ -15,50 +15,38 @@ export default function ({
   getPageObjects,
   updateBaselines,
 }): : FtrProviderContext & { updateBaselines: boolean }) {
-  const pageObjects = getPageObjects(['common']);
   const testSubjects = getService('testSubjects');
   const screenshot = getService('screenshots');
   const log = getService('log');
   const browser = getService('browser');
   const PageObjects = getPageObjects(['common', 'unauthenticatedPage']);
 
-
-  it('navigate to the unauthenticated Page', function () {
-    before(async function () {
-      await PageObjects.common.navigateToUrl('home', '/some/url?some-query=some-value#some-hash', {
-        useActualUrl: true,
+    describe('navigate to the unauthenticated Page', function () {
+      before(async function () {
+        await PageObjects.common.navigateToUrl('home', '/some/url?some-query=some-value#some-hash', {
+          useActualUrl: true,
+        });
+      });
+      it('check unauthenticaed page', function () {
+        before(async function () {
+          await PageObjects.common.sleep(2000);
+          await PageObjects.common.dismissBanner();
+          await PageObjects.unauthenticatedPage.waitForRenderComplete();
+          await PageObjects.common.sleep(2000);
+          await browser.setScreenshotSize(1000, 1337);
+          await PageObjects.common.sleep(2000);
+        });
+      it('unauthenticatedPage overview should match snapshot', async function () {
+        try {
+          const percentDifference = await screenshot.compareAgainstBaseline(
+            'unauthenticated_page',
+            updateBaselines
+          );
+          expect(percentDifference).to.be.lessThan();
+        } finally {
+          log.debug('### Screenshot taken');
+        }
       });
     });
   });
-
-
-  it('check unauthenticaed page', function () {
-    before(async function () {
-      await PageObjects.common.sleep(2000);
-      await PageObjects.common.dismissBanner();
-      await PageObjects.unauthenticatedPage.waitForRenderComplete();
-      await PageObjects.common.sleep(2000);
-      await browser.setScreenshotSize(1000, 1337);
-      await PageObjects.common.sleep(2000);
-    });
-
-
-    it('unauthenticatedPage overview should match snapshot', async function () {
-      try {
-        const percentDifference = await screenshot.compareAgainstBaseline(
-          'unauthenticated_page',
-          updateBaselines
-        );
-        expect(percentDifference).to.be.lessThan();
-      } finally {
-        log.debug('### Screenshot taken');
-      }
-    });
-  });
-
-
-
-
-
-
-
+}
