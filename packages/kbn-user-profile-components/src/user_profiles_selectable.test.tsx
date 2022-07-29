6d7bcd4c3752a71751927aa9e7147c69711d9fd0
Thom Heymann <thom.heymann@elastic.co>
2022-07-29 07:59:45 +0100

    Addd tests

--- packages/kbn-user-profile-components/src/user_profiles_selectable.test.tsx
+++ packages/kbn-user-profile-components/src/user_profiles_selectable.test.tsx
@@ -6,7 +6,6 @@
  * Side Public License, v 1.
  */
 
-import { EuiSelectable } from '@elastic/eui';
 import { mount } from 'enzyme';
 import React from 'react';
 
@@ -52,47 +51,149 @@ const userProfiles = [
 ];
 
 describe('UserProfilesSelectable', () => {
-  it('should render selected options before default options separated by a group label', () => {
-    const [firstOption, secondOption, ...defaultOptions] = userProfiles;
+  it('should render `selectedOptions` before `defaultOptions` separated by a group label', () => {
+    const [firstOption, secondOption, thirdOption] = userProfiles;
     const wrapper = mount(
       <UserProfilesSelectable
-        selectedOptions={[secondOption]}
-        defaultOptions={[...defaultOptions, firstOption]}
+        selectedOptions={[firstOption]}
+        defaultOptions={[secondOption, thirdOption]}
       />
     );
-    expect(wrapper.find(EuiSelectable).prop('options')).toEqual([
+    expect(wrapper.find('EuiSelectable').prop('options')).toEqual([
       expect.objectContaining({
-        key: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0',
+        key: firstOption.uid,
+        checked: 'on',
       }),
       expect.objectContaining({
         isGroupLabel: true,
         label: 'Suggested',
       }),
       expect.objectContaining({
-        key: 'u_A_tM4n0wPkdiQ9smmd8o0Hr_h61XQfu8aRPh9GMoRoc_0',
+        key: secondOption.uid,
+        checked: undefined,
       }),
       expect.objectContaining({
-        key: 'u_9xDEQqUqoYCnFnPPLq5mIRHKL8gBTo_NiKgOnd5gGk0_0',
-      }),
-      expect.objectContaining({
-        key: 'u_BOulL4QMPSyV9jg5lQI2JmCkUnokHTazBnet3xVHNv0_0',
+        key: thirdOption.uid,
+        checked: undefined,
       }),
     ]);
   });
 
-  it('should not render selected or default options if options prop has been provided', () => {
-    const [firstOption, secondOption, ...defaultOptions] = userProfiles;
+  it('should hide `selectedOptions` and `defaultOptions` when `options` has been provided', () => {
+    const [firstOption, secondOption, thirdOption] = userProfiles;
     const wrapper = mount(
       <UserProfilesSelectable
         selectedOptions={[firstOption]}
-        defaultOptions={defaultOptions}
-        options={[secondOption]}
+        defaultOptions={[secondOption]}
+        options={[thirdOption]}
       />
     );
-    expect(wrapper.find(EuiSelectable).prop('options')).toEqual([
+    expect(wrapper.find('EuiSelectable').prop('options')).toEqual([
       expect.objectContaining({
-        key: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0',
+        key: thirdOption.uid,
+        checked: undefined,
       }),
     ]);
+  });
+
+  it('should hide `selectedOptions` and `defaultOptions` when `options` gets updated', () => {
+    const [firstOption, secondOption, thirdOption] = userProfiles;
+    const wrapper = mount(
+      <UserProfilesSelectable selectedOptions={[firstOption]} defaultOptions={[secondOption]} />
+    );
+    expect(wrapper.find('EuiSelectable').prop('options')).toEqual([
+      expect.objectContaining({
+        key: firstOption.uid,
+        checked: 'on',
+      }),
+      expect.objectContaining({
+        isGroupLabel: true,
+        label: 'Suggested',
+      }),
+      expect.objectContaining({
+        key: secondOption.uid,
+        checked: undefined,
+      }),
+    ]);
+
+    wrapper.setProps({ options: [thirdOption] }).update();
+
+    expect(wrapper.find('EuiSelectable').prop('options')).toEqual([
+      expect.objectContaining({
+        key: thirdOption.uid,
+        checked: undefined,
+      }),
+    ]);
+  });
+
+  it('should render `options` with correct checked status', () => {
+    const [firstOption, secondOption] = userProfiles;
+    const wrapper = mount(
+      <UserProfilesSelectable
+        selectedOptions={[firstOption]}
+        options={[firstOption, secondOption]}
+      />
+    );
+    expect(wrapper.find('EuiSelectable').prop('options')).toEqual([
+      expect.objectContaining({
+        key: firstOption.uid,
+        checked: 'on',
+      }),
+      expect.objectContaining({
+        key: secondOption.uid,
+        checked: undefined,
+      }),
+    ]);
+  });
+
+  it('should trigger `onChange` callback when selection changes', () => {
+    const onChange = jest.fn();
+    const [firstOption, secondOption] = userProfiles;
+    const wrapper = mount(
+      <UserProfilesSelectable
+        selectedOptions={[firstOption]}
+        defaultOptions={[secondOption]}
+        onChange={onChange}
+      />
+    );
+    wrapper.find('EuiSelectableListItem').last().simulate('click');
+    expect(onChange).toHaveBeenCalledWith(
+      expect.arrayContaining([
+        expect.objectContaining({
+          uid: firstOption.uid,
+        }),
+        expect.objectContaining({
+          uid: secondOption.uid,
+        }),
+      ])
+    );
+  });
+
+  it('should continue to display `selectedOptions` when getting unchecked', () => {
+    const onChange = jest.fn();
+    const [firstOption] = userProfiles;
+    const wrapper = mount(
+      <UserProfilesSelectable selectedOptions={[firstOption]} onChange={onChange} />
+    );
+    expect(wrapper.find('EuiSelectable').prop('options')).toEqual([
+      expect.objectContaining({
+        key: firstOption.uid,
+        checked: 'on',
+      }),
+    ]);
+    wrapper.setProps({ selectedOptions: [] }).update();
+    expect(wrapper.find('EuiSelectable').prop('options')).toEqual([
+      expect.objectContaining({
+        key: firstOption.uid,
+        checked: undefined,
+      }),
+    ]);
+  });
+
+  it('should trigger `onSearchChange` callback when search term changes', () => {
+    const onSearchChange = jest.fn();
+    const wrapper = mount(<UserProfilesSelectable onSearchChange={onSearchChange} />);
+    wrapper.find('input[type="search"]').simulate('change', { target: { value: 'search' } });
+    expect(onSearchChange).toHaveBeenCalledWith('search');
   });
 });
