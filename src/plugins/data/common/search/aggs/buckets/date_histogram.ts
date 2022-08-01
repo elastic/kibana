c5925529110d6d2ffa7cd029c4e2476081085073
ppisljar <peter.pisljar@elastic.co>
2022-08-01 12:46:21 +0200

    date_histogram: force fixed interval for tsdb rollups

--- src/plugins/data/common/search/aggs/buckets/date_histogram.ts
+++ src/plugins/data/common/search/aggs/buckets/date_histogram.ts
@@ -219,10 +219,17 @@ export const getDateHistogramBucketAgg = ({
             // this DSL will anyway not be used before we're passing this code with an actual interval.
             return;
           }
-          output.params = {
-            ...output.params,
-            ...dateHistogramInterval(interval.expression),
-          };
+
+          const shouldForceFixedInterval = agg.params.field?.meta?.time_zone?.includes('UTC');
+
+          if (shouldForceFixedInterval) {
+            output.params.fixed_interval = interval.expression;
+          } else {
+            output.params = {
+              ...output.params,
+              ...dateHistogramInterval(interval.expression),
+            };
+          }
 
           const scaleMetrics =
             scaleMetricValues && interval.scaled && interval.scale && interval.scale < 1;
