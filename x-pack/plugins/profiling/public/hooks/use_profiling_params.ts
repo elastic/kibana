c5db312957850245e7bb5149d952dae708bf54ac
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-08-01 17:01:37 +0200

    [Profiling] Differential views comparison UI (#137695)

--- x-pack/plugins/profiling/public/hooks/use_profiling_params.ts
+++ x-pack/plugins/profiling/public/hooks/use_profiling_params.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 import { PathsOf, TypeOf, useParams } from '@kbn/typed-react-router-config';
+import { ValuesType } from 'utility-types';
 import { ProfilingRoutes } from '../routing';
 
 export function useProfilingParams<T extends PathsOf<ProfilingRoutes>>(
@@ -12,4 +13,10 @@ export function useProfilingParams<T extends PathsOf<ProfilingRoutes>>(
   ...args: any[]
 ): TypeOf<ProfilingRoutes, T> {
   return useParams(path, ...args) as TypeOf<ProfilingRoutes, T>;
+}
+
+export function useAnyOfProfilingParams<TPaths extends Array<PathsOf<ProfilingRoutes>>>(
+  ...paths: TPaths
+): TypeOf<ProfilingRoutes, ValuesType<TPaths>> {
+  return useParams(...paths)! as TypeOf<ProfilingRoutes, ValuesType<TPaths>>;
 }
