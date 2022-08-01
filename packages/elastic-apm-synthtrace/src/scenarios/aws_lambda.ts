2748691c5fe2cf9c7240d5c2278da5140a22b21c
Søren Louv-Jansen <soren.louv@elastic.co>
2022-08-01 15:16:04 +0200

    [APM] Improve synthtrace environment (#137697)

--- packages/elastic-apm-synthtrace/src/scenarios/aws_lambda.ts
+++ packages/elastic-apm-synthtrace/src/scenarios/aws_lambda.ts
@@ -11,8 +11,9 @@ import { ApmFields } from '../lib/apm/apm_fields';
 import { Scenario } from '../cli/scenario';
 import { getLogger } from '../cli/utils/get_common_services';
 import { RunOptions } from '../cli/utils/parse_run_cli_flags';
+import { getSynthtraceEnvironment } from '../lib/utils/get_synthtrace_environment';
 
-const ENVIRONMENT = __filename;
+const ENVIRONMENT = getSynthtraceEnvironment(__filename);
 
 const scenario: Scenario<ApmFields> = async (runOptions: RunOptions) => {
   const logger = getLogger(runOptions);
