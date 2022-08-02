f7b2f1a3b8563266aea745963f3eca6ebc22b4a7
Jeramy Soucy <jeramy.soucy@elastic.co>
2022-08-02 10:14:46 -0400

    Fixes Spaces Menu Keyboard and Screen Reader Navigation (#134454)
    * Refactor of spaces pop-over using EuiSelectable.
    
    * Updated to use Eui type instead of interface.
    
    * Reorganization of code, began to add EuiSelectableOnChangeEvent usage.
    
    * Repathed import after rebase
    
    * Integrates the EuiSelectableOnChangeEvent argument to intercept keyboard and mouse event details (shift, ctrl, middle click).
    
    * Resolves missing property error in nav popover test.
    
    * Handles reintroduction of translated strings into empty and no match messages of the Spaces selectable.
    
    * Refactor of spaces_popover_list to use EuiSelectable, resolves keyboard navigation.
    
    * Updated tests for spaces popover list and created more comprehensive tests for nav control popover.
    Eliminated tab selection of popover panel itself. Tested screen reader extensively with Chrome on Mac.
    Note: Safari screen reader and tabbing behavior is not consistent and differs significantly from Chrome.
    
    * Adjuted focus behavior in popover. Rebased to use EUI 60.1.2
    
    * Fixed typo in roles spaces popover list. Keyboard navigation with options (shift, ctrl, etc.) is not yet working - event argument in selectable onChange is null for keyboard events.
    
    * Updated comments.
    
    * Fixes issue with keyboard operation of manage spaces button of spaces_menu. Fixes naming of translated string resources in spaces_popover_list.
    
    * Fixes name of existing string reference in spaces_menu.
    
    * Fixes CSS selector calls in spaces functional test.
    
    * Fixes space selection in functional tests.
    
    * Fixes popover close issue on Manage spaces button click. Implements check for re-selecting active space. Implements popover close when opening a space in a new window.
    
    * Updated jest snapshot for spaces nav test.
    
    * Fixes ML functional tests to accommodate new behavior when selecting already active space.
    
    * Added active space highlight feedback. Removed state from spaces menu class.
    
    * Rebased, added check mark for active space, resolved activeOption keyboard nav break from EUI update
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * Fixes initial active space highlight which was caused by initial empty spaces state in nav_control_popover and loading render logic.
    Removes isLoading from spaces_menu - no longer necessary, SpacesDescription is now used during loading.
    
    * Added debug output of actual URL in route expects.
    
    * Added loading message to spaces navigation.
    
    * Updated jest snapshot.
    
    * Addressed flaky test behavior related to space menu nav. Implemented review feedback suggestions.
    
    * Adds sleep to functional test UI interactions.
    
    * Replaced use of any with ExclusiveUnion for search props of EuiSelectable.
    Resolved flaky test for spaces nav.
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/spaces/public/nav_control/nav_control_popover.test.tsx
+++ x-pack/plugins/spaces/public/nav_control/nav_control_popover.test.tsx
@@ -5,20 +5,68 @@
  * 2.0.
  */
 
-import { EuiHeaderSectionItemButton } from '@elastic/eui';
-import { waitFor } from '@testing-library/react';
+import {
+  EuiFieldSearch,
+  EuiHeaderSectionItemButton,
+  EuiPopover,
+  EuiSelectable,
+  EuiSelectableListItem,
+} from '@elastic/eui';
+import { act, waitFor } from '@testing-library/react';
 import { shallow } from 'enzyme';
 import React from 'react';
 import * as Rx from 'rxjs';
 
 import { mountWithIntl } from '@kbn/test-jest-helpers';
 
+import type { Space } from '../../common';
 import { SpaceAvatarInternal } from '../space_avatar/space_avatar_internal';
 import type { SpacesManager } from '../spaces_manager';
 import { spacesManagerMock } from '../spaces_manager/mocks';
 import { NavControlPopover } from './nav_control_popover';
 
+const mockSpaces = [
+  {
+    id: 'default',
+    name: 'Default Space',
+    description: 'this is your default space',
+    disabledFeatures: [],
+  },
+  {
+    id: 'space-1',
+    name: 'Space 1',
+    disabledFeatures: [],
+  },
+  {
+    id: 'space-2',
+    name: 'Space 2',
+    disabledFeatures: [],
+  },
+];
+
 describe('NavControlPopover', () => {
+  async function setup(spaces: Space[]) {
+    const spacesManager = spacesManagerMock.create();
+    spacesManager.getSpaces = jest.fn().mockResolvedValue(spaces);
+
+    const wrapper = mountWithIntl(
+      <NavControlPopover
+        spacesManager={spacesManager as unknown as SpacesManager}
+        serverBasePath={'/server-base-path'}
+        anchorPosition={'rightCenter'}
+        capabilities={{ navLinks: {}, management: {}, catalogue: {}, spaces: { manage: true } }}
+        navigateToApp={jest.fn()}
+        navigateToUrl={jest.fn()}
+      />
+    );
+
+    await waitFor(() => {
+      wrapper.update();
+    });
+
+    return wrapper;
+  }
+
   it('renders without crashing', () => {
     const spacesManager = spacesManagerMock.create();
 
@@ -29,6 +77,7 @@ describe('NavControlPopover', () => {
         anchorPosition={'downRight'}
         capabilities={{ navLinks: {}, management: {}, catalogue: {}, spaces: { manage: true } }}
         navigateToApp={jest.fn()}
+        navigateToUrl={jest.fn()}
       />
     );
     expect(wrapper).toMatchSnapshot();
@@ -36,22 +85,12 @@ describe('NavControlPopover', () => {
 
   it('renders a SpaceAvatar with the active space', async () => {
     const spacesManager = spacesManagerMock.create();
-    spacesManager.getSpaces = jest.fn().mockResolvedValue([
-      {
-        id: 'foo-space',
-        name: 'foo',
-        disabledFeatures: [],
-      },
-      {
-        id: 'bar-space',
-        name: 'bar',
-        disabledFeatures: [],
-      },
-    ]);
+    spacesManager.getSpaces = jest.fn().mockResolvedValue(mockSpaces);
     // @ts-ignore readonly check
     spacesManager.onActiveSpaceChange$ = Rx.of({
-      id: 'foo-space',
-      name: 'foo',
+      id: 'default',
+      name: 'Default Space',
+      description: 'this is your default space',
       disabledFeatures: [],
     });
 
@@ -62,6 +101,7 @@ describe('NavControlPopover', () => {
         anchorPosition={'rightCenter'}
         capabilities={{ navLinks: {}, management: {}, catalogue: {}, spaces: { manage: true } }}
         navigateToApp={jest.fn()}
+        navigateToUrl={jest.fn()}
       />
     );
 
@@ -70,7 +110,117 @@ describe('NavControlPopover', () => {
     // Wait for `getSpaces` promise to resolve
     await waitFor(() => {
       wrapper.update();
-      expect(wrapper.find(SpaceAvatarInternal)).toHaveLength(3);
+      expect(wrapper.find(SpaceAvatarInternal)).toHaveLength(mockSpaces.length + 1); // one additional avatar for the button itself
     });
+  });
+
+  it('clicking the button renders an EuiSelectable menu with the provided spaces', async () => {
+    const wrapper = await setup(mockSpaces);
+
+    await act(async () => {
+      wrapper.find(EuiHeaderSectionItemButton).simulate('click');
+    });
+    wrapper.update();
+
+    await act(async () => {
+      const menu = wrapper.find(EuiSelectable);
+      expect(menu).toHaveLength(1);
+
+      const items = menu.find(EuiSelectableListItem);
+      expect(items).toHaveLength(mockSpaces.length);
+
+      mockSpaces.forEach((space, index) => {
+        const spaceAvatar = items.at(index).find(SpaceAvatarInternal);
+        expect(spaceAvatar.props().space).toEqual(space);
+      });
+    });
+  });
+
+  it('should render a search box when there are 8 or more spaces', async () => {
+    const eightSpaces = mockSpaces.concat([
+      {
+        id: 'space-3',
+        name: 'Space-3',
+        disabledFeatures: [],
+      },
+      {
+        id: 'space-4',
+        name: 'Space 4',
+        disabledFeatures: [],
+      },
+      {
+        id: 'space-5',
+        name: 'Space 5',
+        disabledFeatures: [],
+      },
+      {
+        id: 'space-6',
+        name: 'Space 6',
+        disabledFeatures: [],
+      },
+      {
+        id: 'space-7',
+        name: 'Space 7',
+        disabledFeatures: [],
+      },
+    ]);
+    const wrapper = await setup(eightSpaces);
+
+    await act(async () => {
+      wrapper.find(EuiHeaderSectionItemButton).simulate('click');
+    });
+    wrapper.update();
+
+    expect(wrapper.find(EuiFieldSearch)).toHaveLength(1);
+  });
+
+  it('should NOT render a search box when there are less than 8 spaces', async () => {
+    const sevenSpaces = mockSpaces.concat([
+      {
+        id: 'space-3',
+        name: 'Space-3',
+        disabledFeatures: [],
+      },
+      {
+        id: 'space-4',
+        name: 'Space 4',
+        disabledFeatures: [],
+      },
+      {
+        id: 'space-5',
+        name: 'Space 5',
+        disabledFeatures: [],
+      },
+      {
+        id: 'space-6',
+        name: 'Space 6',
+        disabledFeatures: [],
+      },
+    ]);
+    const wrapper = await setup(sevenSpaces);
+
+    await act(async () => {
+      wrapper.find(EuiHeaderSectionItemButton).simulate('click');
+    });
+    wrapper.update();
+
+    expect(wrapper.find(EuiFieldSearch)).toHaveLength(0);
+  });
+
+  it('can close its popover', async () => {
+    const wrapper = await setup(mockSpaces);
+
+    await act(async () => {
+      wrapper.find(EuiHeaderSectionItemButton).simulate('click');
+    });
+    wrapper.update();
+    expect(wrapper.find(EuiPopover).props().isOpen).toEqual(true);
+
+    await act(async () => {
+      wrapper.find(EuiPopover).props().closePopover();
+    });
+    wrapper.update();
+
+    expect(wrapper.find(EuiPopover).props().isOpen).toEqual(false);
   });
 });
