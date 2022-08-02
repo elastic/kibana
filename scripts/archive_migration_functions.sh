74c6404ceb6d5d1ae0b323658f51c0eb410f375f
Tre' Seymour <wayne.seymour@elastic.co>
2022-08-02 14:47:48 +0100

    [Archive Migrations] x-pack spaces/multi_space
    Replaces the old es archive with kbn archive.
    Change test to use new archive.
    
    Helps with: elastic#102552

--- scripts/archive_migration_functions.sh
+++ scripts/archive_migration_functions.sh
@@ -2,11 +2,16 @@
 
 standard_list="url,index-pattern,query,graph-workspace,tag,visualization,canvas-element,canvas-workpad,dashboard,search,lens,map,cases,uptime-dynamic-settings,osquery-saved-query,osquery-pack,infrastructure-ui-source,metrics-explorer-view,inventory-view,infrastructure-monitoring-log-view,apm-indices"
 
-orig_archive="x-pack/test/functional/es_archives/reporting/ecommerce_kibana_spaces"
-new_archive="x-pack/test/functional/fixtures/kbn_archiver/reporting/ecommerce_kibana_spaces"
-newArchives=("x-pack/test/functional/fixtures/kbn_archiver/reporting/ecommerce_kibana_non_default_space")
-newArchives+=("x-pack/test/functional/fixtures/kbn_archiver/reporting/ecommerce_kibana_non_timezone_space")
-test_config="x-pack/test/reporting_api_integration/reporting_and_security.config.ts"
+orig_archive="x-pack/test/functional/es_archives/spaces/multi_space"
+new_archive="x-pack/test/functional/fixtures/kbn_archiver/spaces/multi_space"
+# newArchives=("x-pack/test/functional/fixtures/kbn_archiver/reporting/ecommerce_kibana_non_default_space")
+# newArchives+=("x-pack/test/functional/fixtures/kbn_archiver/reporting/ecommerce_kibana_non_timezone_space")
+test_config="x-pack/test/functional/apps/dashboard/group1/config.ts"
+# test_config="x-pack/test/functional/apps/discover/config.ts"
+# test_config="x-pack/test/functional/apps/visualize/config.ts"
+testFiles=("x-pack/test/functional/apps/dashboard/group1/preserve_url.ts")
+# testFiles+=("x-pack/test/functional/apps/discover/preserve_url.ts")
+# testFiles+=("x-pack/test/functional/apps/visualize/preserve_url.ts")
 
 arrayify_csv() {
   local xs=${1}
