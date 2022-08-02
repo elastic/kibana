701bcb0989ab0da86939008477e5f808c917bf56
Joe Reuter <johannes.reuter@elastic.co>
2022-08-02 07:29:35 +0200

    [Lens] Fix default value count label (#137708)
    * fix default value count label
    
    * adjust test

--- x-pack/plugins/lens/public/indexpattern_datasource/operations/definitions.test.ts
+++ x-pack/plugins/lens/public/indexpattern_datasource/operations/definitions.test.ts
@@ -211,7 +211,7 @@ describe('time scale transition', () => {
     ).toEqual(
       expect.objectContaining({
         timeScale: undefined,
-        label: 'Value count of bytesLabel',
+        label: 'Count of bytesLabel',
       })
     );
   });
