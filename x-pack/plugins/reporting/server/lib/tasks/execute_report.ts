04365e63bf1bbc5d5e98e60e3c9ce98cec9babaf
Timothy Sullivan <tsullivan@elastic.co>
2022-08-01 19:38:35 -0700

    wip comment

--- x-pack/plugins/reporting/server/lib/tasks/execute_report.ts
+++ x-pack/plugins/reporting/server/lib/tasks/execute_report.ts
@@ -261,7 +261,7 @@ export class ExecuteReportTask implements ReportingTask {
     const queueTimeout = durationToNumber(this.config.queue.timeout);
     return Rx.lastValueFrom(
       Rx.from(runner.jobExecutor(task.id, task.payload, cancellationToken, stream)).pipe(
-        timeout(queueTimeout)
+        timeout(queueTimeout) // FIXME: CSV search request timeout should block this timeout
       ) // throw an error if a value is not emitted before timeout
     );
   }
