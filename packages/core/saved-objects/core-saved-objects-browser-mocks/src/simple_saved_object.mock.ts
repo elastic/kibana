48895565fe19c478d08bee8395d9dc589c38b588
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-08-03 14:50:42 -0700

    Creates simple saved object mock

--- packages/core/saved-objects/core-saved-objects-browser-mocks/src/simple_saved_object.mock.ts
+++ packages/core/saved-objects/core-saved-objects-browser-mocks/src/simple_saved_object.mock.ts
@@ -6,15 +6,47 @@
  * Side Public License, v 1.
  */
 
-import type { SavedObjectsClientContract } from '@kbn/core-saved-objects-api-browser';
-import { SimpleSavedObjectImpl } from '@kbn/core-saved-objects-browser-internal';
+import type {
+  SavedObjectsClientContract,
+  SimpleSavedObject,
+} from '@kbn/core-saved-objects-api-browser';
 import type { SavedObject } from '@kbn/core-saved-objects-common';
 
+type T = unknown;
 const createSimpleSavedObjectMock = (
-  client: SavedObjectsClientContract,
-  savedObject: SavedObject<unknown>
-) => new SimpleSavedObjectImpl(client, savedObject);
+  savedObject: SavedObject
+): jest.Mocked<SimpleSavedObject<T>> => {
+  const mock = {
+    attributes: savedObject.attributes || ({} as T),
+    _version: savedObject.version ?? '',
+    id: savedObject.id ?? 'id',
+    type: savedObject.type ?? 'type',
+    migrationVersion: savedObject.migrationVersion ?? {},
+    coreMigrationVersion: savedObject.coreMigrationVersion ?? '8.0.0',
+    error: undefined,
+    references: savedObject.references ?? [],
+    updatedAt: savedObject.updated_at ?? '',
+    namespaces: savedObject.namespaces ?? undefined,
+    get: jest.fn(),
+    set: jest.fn(),
+    has: jest.fn(),
+    save: jest.fn(),
+    delete: jest.fn(),
+  };
+  mock.get.mockImplementation(
+    (key: string): any => (savedObject.attributes as any)[key] || undefined
+  );
+  mock.set.mockReturnValue((key: string, value: any) => {
+    (savedObject as any)[key] = value;
+    return savedObject;
+  });
+  mock.has.mockReturnValue(true);
+  mock.save.mockImplementation(() => Promise.resolve(mock));
+  mock.delete.mockImplementation(() => Promise.resolve({}));
+  return mock;
+};
 
 export const simpleSavedObjectMock = {
-  create: createSimpleSavedObjectMock,
+  create: (client: SavedObjectsClientContract, savedObject: SavedObject) =>
+    createSimpleSavedObjectMock(savedObject),
 };
