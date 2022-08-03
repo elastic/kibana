615447ca21caea88585a10955066b33e90b47f97
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2022-08-03 16:27:20 +0200

    add debug log

--- test/functional/apps/discover/group2/_data_grid_context.ts
+++ test/functional/apps/discover/group2/_data_grid_context.ts
@@ -16,6 +16,7 @@ const TEST_FILTER_COLUMN_NAMES = [
 ];
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
+  const log = getService('log');
   const retry = getService('retry');
   const filterBar = getService('filterBar');
   const dataGrid = getService('dataGrid');
@@ -112,6 +113,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await PageObjects.header.waitUntilLoadingHasFinished();
       await retry.waitFor('document table has a length of 6', async () => {
         const nrOfDocs = (await dataGrid.getBodyRows()).length;
+        log.debug('document table length', nrOfDocs);
         return nrOfDocs === 6;
       });
     });
