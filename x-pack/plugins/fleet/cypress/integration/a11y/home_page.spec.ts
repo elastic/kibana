53c084a9f2c62a81e9ab2da676bc2def1724573f
John Dorlus <jsdorlus@gmail.com>
2022-08-03 15:59:50 -0400

    Added initial files for adding Fleet A11y tests.

---
+++ x-pack/plugins/fleet/cypress/integration/a11y/home_page.spec.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { checkA11y } from '../../support/commands';
+import { FLEET, navigateTo } from '../../tasks/navigation';
+describe('Home page', function () {
+    before(async () => {
+        navigateTo(FLEET);
+    });
+    describe('Agents', async () => {
+        describe('Quick Start', async () => {
+            it('Get started with fleet', async () => {
+                checkA11y({skipFailures: false});
+                //await pageObjects.fleet.createNewFleetServer('https://localhost:8220');
+            });
+            it('Install Fleet Server', async () => {
+                checkA11y({skipFailures: false});
+            });
+        });
+    //     describe('Advanced', async () => {
+    //         before(async () => {
+    //             await pageObjects.fleet.clickAdvanceOption();
+    //         });
+    //         it('Select policy for fleet', async () => {
+    //             await a11y.testAppSnapshot();
+    //         });
+    //         it('Add your fleet sever host', async () => {
+    //             await pageObjects.fleet.addFleetServerInAdvanced();
+    //             await a11y.testAppSnapshot();
+    //         });
+    //         it('Generate service token', async () => {
+    //             await pageObjects.fleet.addGeneratedServiceToken();
+    //             await a11y.testAppSnapshot();
+    //         });
+    //     });
+    });
+
+    it('should edit package policy', () => {
+        // cy.getBySel('toastCloseButton').click();
+        // cy.getBySel('packagePolicyDescriptionInput').clear().type('desc');
+
+        // cy.intercept('PUT', '/api/fleet/package_policies/policy-1', {
+        //     name: 'fleet_server-1',
+        //     description: 'desc',
+        //     namespace: 'default',
+        // }).as('updatePackagePolicy');
+        // cy.get('.euiButton').contains('Save integration').click();
+
+        // cy.wait('@updatePackagePolicy').then((interception) => {
+        //     expect(interception.request.body.description).to.equal('desc');
+        // });
+    });
+});
