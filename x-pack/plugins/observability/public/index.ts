a1fb7d5fbc1021d98bfb4032a99b97d58e752677
Shahzad <shahzad.muhammad@elastic.co>
2022-08-03 10:58:00 +0100

    [User Experience App] Pass filters down to page view and page load charts (#137822)

--- x-pack/plugins/observability/public/index.ts
+++ x-pack/plugins/observability/public/index.ts
@@ -87,7 +87,7 @@ export { NavigationWarningPromptProvider, Prompt } from './utils/navigation_warn
 export { getApmTraceUrl } from './utils/get_apm_trace_url';
 export { createExploratoryViewUrl } from './components/shared/exploratory_view/configurations/exploratory_view_url';
 export type { AllSeries } from './components/shared/exploratory_view/hooks/use_series_storage';
-export type { SeriesUrl } from './components/shared/exploratory_view/types';
+export type { SeriesUrl, UrlFilter } from './components/shared/exploratory_view/types';
 
 export type {
   ObservabilityRuleTypeFormatter,
