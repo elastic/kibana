67dc1952d1a5b39ccc9967c179f2ddce9a3aff4d
Jean-Louis Leysens <jeanlouis.leysens@elastic.co>
2022-08-03 14:29:15 +0200

    export function arg types

--- x-pack/plugins/files/server/file_service/index.ts
+++ x-pack/plugins/files/server/file_service/index.ts
@@ -15,4 +15,12 @@ export type {
   UpdateFileArgs,
 } from './file_action_types';
 export type { FileServiceStart } from './file_service';
+export {
+  CreateFileArgs,
+  DeleteFileArgs,
+  FindFileArgs,
+  GetByIdArgs,
+  ListFilesArgs,
+  UpdateFileArgs,
+} from './file_action_types';
 export * as errors from './errors';
