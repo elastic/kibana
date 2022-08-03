84cc0fc74f7c7230c873b47cb451ea331cd2486a
Davis McPhee <davis.mcphee@elastic.co>
2022-08-03 11:58:37 -0300

    [Discover] Create new saved_objects_finder plugin

---
+++ src/plugins/saved_objects_finder/jest.config.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+module.exports = {
+  preset: '@kbn/test',
+  rootDir: '../../..',
+  roots: ['<rootDir>/src/plugins/saved_objects_finder'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/saved_objects_finder',
+  coverageReporters: ['text', 'html'],
+  collectCoverageFrom: [
+    '<rootDir>/src/plugins/saved_objects_finder/{common,public,server}/**/*.{ts,tsx}',
+  ],
+};
