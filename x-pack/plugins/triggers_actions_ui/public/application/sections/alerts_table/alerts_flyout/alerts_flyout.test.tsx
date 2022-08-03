a52040a192debbd8d9d25ffad3bc71232be40371
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2022-08-03 06:22:01 -0400

    [RAM] Fix alert table with new props cases (#137728)
    * Remove cases props and try to get it directly inside of the components by passing casesFeatureId instead
    
    * add cases to apm
    
    * fix unit test
    
    * skip test until we are bringingt them back without adding dependency in trigger action UI
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit fb49ff39b151922e689a73605938bacf3ae634f7)

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_flyout/alerts_flyout.test.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_flyout/alerts_flyout.test.tsx
@@ -22,6 +22,7 @@ const props = {
   } as unknown as EcsFieldsResponse,
   alertsTableConfiguration: {
     id: 'test',
+    casesFeatureId: 'testCases',
     columns: [
       {
         id: AlertsField.name,
