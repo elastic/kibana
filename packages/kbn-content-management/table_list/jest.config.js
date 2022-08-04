975a9f7e014cb617c8b706fea3227d22bef2d0fc
Sébastien Loix <sebastien.loix@elastic.co>
2022-08-04 16:52:24 +0100

    Fix paths (2)

--- packages/kbn-content-management/table_list/jest.config.js
+++ packages/kbn-content-management/table_list/jest.config.js
@@ -9,5 +9,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../..',
-  roots: ['<rootDir>/packages/kbn-content-management/table-list'],
+  roots: ['<rootDir>/packages/kbn-content-management/table_list'],
 };
