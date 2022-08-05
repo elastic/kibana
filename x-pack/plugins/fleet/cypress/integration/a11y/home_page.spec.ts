8c4079096bb684a2880871828a1fa0e81af7da98
John Dorlus <jsdorlus@gmail.com>
2022-08-05 00:44:02 -0400

    Added tests for Agents tag.

--- x-pack/plugins/fleet/cypress/integration/a11y/home_page.spec.ts
+++ x-pack/plugins/fleet/cypress/integration/a11y/home_page.spec.ts
@@ -7,53 +7,49 @@
 import 'cypress-real-events/support';
 import { checkA11y } from '../../support/commands';
 import { FLEET, navigateTo } from '../../tasks/navigation';
-import { GENERATE_FLEET_SERVER_POLICY_BUTTON, QUICK_START_TAB_BUTTON } from '../../screens/fleet';
+import {
+    GENERATE_FLEET_SERVER_POLICY_BUTTON,
+    AGENTS_QUICK_START_TAB_BUTTON,
+    PLATFORM_TYPE_LINUX_BUTTON,
+    AGENTS_ADVANCED_TAB_BUTTON,
+    ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON,
+    ADVANCED_FLEET_SERVER_GENERATE_SERVICE_TOKEN_BUTTON
+} from '../../screens/fleet';
 describe('Home page', () => {
-  before(() => {
-    navigateTo(FLEET);
-    cy.getBySel(QUICK_START_TAB_BUTTON, { timeout: 15000 }).should('be.visible');
-  });
-  describe('Agents', () => {
-    const fleetServerHost = 'https://localhost:8220';
-    describe('Quick Start', () => {
-      it('Get started with fleet', () => {
-        checkA11y({ skipFailures: false });
-      });
-      it('Install Fleet Server', () => {
-        cy.getBySel('fleetServerHostInput').getBySel('comboBoxSearchInput').type(fleetServerHost);
-        cy.getBySel(GENERATE_FLEET_SERVER_POLICY_BUTTON).click();
-        checkA11y({ skipFailures: false });
-      });
+    before(() => {
+        navigateTo(FLEET);
+        cy.getBySel(AGENTS_QUICK_START_TAB_BUTTON, { timeout: 15000 }).should('be.visible');
     });
-    //     describe('Advanced', async () => {
-    //         before(async () => {
-    //             await pageObjects.fleet.clickAdvanceOption();
-    //         });
-    //         it('Select policy for fleet', async () => {
-    //             await a11y.testAppSnapshot();
-    //         });
-    //         it('Add your fleet sever host', async () => {
-    //             await pageObjects.fleet.addFleetServerInAdvanced();
-    //             await a11y.testAppSnapshot();
-    //         });
-    //         it('Generate service token', async () => {
-    //             await pageObjects.fleet.addGeneratedServiceToken();
-    //             await a11y.testAppSnapshot();
-    //         });
-    //     });
-  });
-
-  // it('should edit package policy', () => {
-  // cy.getBySel('toastCloseButton').click();
-  // cy.getBySel('packagePolicyDescriptionInput').clear().type('desc');
-  // cy.intercept('PUT', '/api/fleet/package_policies/policy-1', {
-  //     name: 'fleet_server-1',
-  //     description: 'desc',
-  //     namespace: 'default',
-  // }).as('updatePackagePolicy');
-  // cy.get('.euiButton').contains('Save integration').click();
-  // cy.wait('@updatePackagePolicy').then((interception) => {
-  //     expect(interception.request.body.description).to.equal('desc');
-  // });
-  // });
+    describe('Agents', () => {
+        const fleetServerHost = 'https://localhost:8220';
+        describe('Quick Start', () => {
+            it('Get started with fleet', () => {
+                checkA11y({ skipFailures: false });
+            });
+            it('Install Fleet Server', () => {
+                cy.getBySel('fleetServerHostInput').getBySel('comboBoxSearchInput').type(fleetServerHost);
+                cy.getBySel(GENERATE_FLEET_SERVER_POLICY_BUTTON).click();
+                cy.getBySel(PLATFORM_TYPE_LINUX_BUTTON, { timeout: 15000 }).should('be.visible');
+                checkA11y({ skipFailures: false });
+            });
+        });
+        describe('Advanced', () => {
+            before(() => {
+                cy.getBySel(AGENTS_ADVANCED_TAB_BUTTON).click();
+            });
+            it('Select policy for fleet', () => {
+                checkA11y({ skipFailures: false });
+            });
+            it('Add your fleet sever host', () => {
+                cy.getBySel(ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON).click();
+                checkA11y({ skipFailures: false })
+            });
+            it('Generate service token', () => {
+                cy.getBySel(ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON, { timeout: 15000 }).should('be.visible');
+                cy.getBySel(ADVANCED_FLEET_SERVER_GENERATE_SERVICE_TOKEN_BUTTON).click();
+                cy.getBySel(PLATFORM_TYPE_LINUX_BUTTON, { timeout: 15000 }).should('be.visible');
+                checkA11y({ skipFailures: false })
+            });
+        });
+    });
 });
