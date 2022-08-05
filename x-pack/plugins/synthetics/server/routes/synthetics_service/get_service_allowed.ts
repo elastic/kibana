bcae7e2b87828a600ad91b4170fe09435f29076a
Dominique Clarke <dominique.clarke@elastic.co>
2022-08-05 18:47:25 -0400

    synthetics - keep front end service allowed logic

--- x-pack/plugins/synthetics/server/routes/synthetics_service/get_service_allowed.ts
+++ x-pack/plugins/synthetics/server/routes/synthetics_service/get_service_allowed.ts
@@ -14,7 +14,7 @@ export const getServiceAllowedRoute: SyntheticsRestApiRouteFactory = () => ({
   validate: {},
   handler: async ({ syntheticsMonitorClient }): Promise<any> => {
     return {
-      serviceAllowed: true,
+      serviceAllowed: server.syntheticsService.isAllowed,
       signupUrl: syntheticsMonitorClient.syntheticsService.signupUrl,
     };
   },
