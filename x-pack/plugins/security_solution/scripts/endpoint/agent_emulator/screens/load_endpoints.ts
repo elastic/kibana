3b8ecb24b665fc5b4b4b227a3b9cc60e89b0434b
Paul Tavares <paul.tavares@elastic.co>
2022-08-05 18:42:33 -0400

    load endpoints screens views for prompt and loading

--- x-pack/plugins/security_solution/scripts/endpoint/agent_emulator/screens/load_endpoints.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/agent_emulator/screens/load_endpoints.ts
@@ -5,17 +5,31 @@
  * 2.0.
  */
 
+import { ProgressFormatter } from '../../common/screen/progress_formatter';
 import type { DataFormatter } from '../../common/screen';
 import { ScreenBaseClass } from '../../common/screen';
 import { TOOL_TITLE } from '../constants';
 
+interface LoadOptions {
+  count: number;
+  progress: ProgressFormatter;
+  isRunning: boolean;
+  isDone: boolean;
+}
+
 export class LoadEndpointsScreen extends ScreenBaseClass {
+  private runInfo: LoadOptions | undefined = undefined;
+
   protected header() {
     return super.header(TOOL_TITLE, 'Endpoint loader');
   }
 
   protected body(): string | DataFormatter {
-    return super.body();
+    if (this.runInfo) {
+      return this.loadingView();
+    }
+
+    return this.promptView();
   }
 
   protected onEnterChoice(choice: string) {
@@ -30,12 +44,53 @@ export class LoadEndpointsScreen extends ScreenBaseClass {
         if (!Number.isFinite(count)) {
           throw new Error(`Invalid number: ${choice}`);
         }
-    }
 
-    this.throwUnknownChoiceError(choice);
+        this.runInfo = {
+          count,
+          progress: new ProgressFormatter(),
+          isRunning: false,
+          isDone: false,
+        };
+
+        this.show();
+        this.loadEndpoints();
+    }
   }
 
-  private async loadEndpoints(count: number) {
-    //
+  private async loadEndpoints() {
+    await new Promise((r) => setTimeout(r, 2000));
+    this.runInfo?.progress.setProgress(20);
+    this.show();
+
+    await new Promise((r) => setTimeout(r, 2000));
+    this.runInfo?.progress.setProgress(40);
+    this.show();
+
+    await new Promise((r) => setTimeout(r, 2000));
+    this.runInfo?.progress.setProgress(80);
+    this.show();
+
+    await new Promise((r) => setTimeout(r, 2000));
+    this.runInfo?.progress.setProgress(100);
+    this.show();
+  }
+
+  private promptView(): string | DataFormatter {
+    return `
+
+  How many endpoints to load?
+  `;
+  }
+
+  private loadingView(): string | DataFormatter {
+    if (this.runInfo) {
+      return `
+
+  Creating ${this.runInfo.count} endpoint(s):
+  ${this.runInfo.progress.output}
+`;
+    }
+
+    return 'Unknown state';
   }
 }
