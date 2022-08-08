e1b9c6822a911f9ef9ce5735abee9bcbfe4cf33d
CJ Cenizal <cj.cenizal@elastic.co>
2022-08-08 13:03:02 -0700

    Refactor Action Status Model tests for readability.

--- x-pack/plugins/watcher/server/models/action_status_model/action_status_model.test.ts
+++ x-pack/plugins/watcher/server/models/action_status_model/action_status_model.test.ts
@@ -17,7 +17,6 @@ describe('ActionStatusModel', () => {
     beforeEach(() => {
       upstreamJson = {
         id: 'my-action',
-        errors: {},
         lastCheckedRawFormat: '2017-03-01T20:55:49.679Z',
         actionStatusJson: {
           ack: {
@@ -41,307 +40,46 @@ describe('ActionStatusModel', () => {
       };
     });
 
-    // it(`throws an error if no 'id' property in json`, () => {
-    //   delete upstreamJson.id;
-    //   expect(() => {
-    //     buildServerActionStatusModel(upstreamJson);
-    //   }).toThrow('JSON argument must contain an "id" property');
-    // });
+    it(`throws an error if no 'id' property in json`, () => {
+      expect(() => {
+        // @ts-ignore
+        buildServerActionStatusModel({});
+      }).toThrow('JSON argument must contain an "id" property');
+    });
 
-    // it(`throws an error if no 'actionStatusJson' property in json`, () => {
-    //   delete upstreamJson.actionStatusJson;
-    //   expect(() => {
-    //     buildServerActionStatusModel(upstreamJson);
-    //   }).toThrow('JSON argument must contain an "actionStatusJson" property');
-    // });
+    it(`throws an error if no 'actionStatusJson' property in json`, () => {
+      expect(() => {
+        // @ts-ignore
+        buildServerActionStatusModel({ id: 'test' });
+      }).toThrow('JSON argument must contain an "actionStatusJson" property');
+    });
 
     it('returns correct ActionStatus instance', () => {
-      const actionStatus = buildServerActionStatusModel({
+      const serverActionStatusModel = buildServerActionStatusModel({
         ...upstreamJson,
         errors: { foo: 'bar' },
       });
 
-      expect(actionStatus.id).toBe(upstreamJson.id);
-      expect(actionStatus.lastAcknowledged).toEqual(
+      expect(serverActionStatusModel.id).toBe(upstreamJson.id);
+      expect(serverActionStatusModel.lastAcknowledged).toEqual(
         moment(upstreamJson.actionStatusJson.ack.timestamp)
       );
-      expect(actionStatus.lastExecution).toEqual(
+      expect(serverActionStatusModel.lastExecution).toEqual(
         moment(upstreamJson.actionStatusJson.last_execution?.timestamp)
       );
-      expect(actionStatus.lastExecutionSuccessful).toEqual(
+      expect(serverActionStatusModel.lastExecutionSuccessful).toEqual(
         upstreamJson.actionStatusJson.last_execution?.successful
       );
-      expect(actionStatus.lastExecutionReason).toBe(
+      expect(serverActionStatusModel.lastExecutionReason).toBe(
         upstreamJson.actionStatusJson.last_execution?.reason
       );
-      expect(actionStatus.lastThrottled).toEqual(
+      expect(serverActionStatusModel.lastThrottled).toEqual(
         moment(upstreamJson.actionStatusJson.last_throttle?.timestamp)
       );
-      expect(actionStatus.lastSuccessfulExecution).toEqual(
+      expect(serverActionStatusModel.lastSuccessfulExecution).toEqual(
         moment(upstreamJson.actionStatusJson.last_successful_execution?.timestamp)
       );
-      expect(actionStatus.errors).toEqual({ foo: 'bar' });
-    });
-  });
-
-  describe('state', () => {
-    let upstreamJson: ActionStatusUpstreamJson;
-    beforeEach(() => {
-      upstreamJson = {
-        id: 'my-action',
-        errors: {},
-        lastCheckedRawFormat: '2017-03-01T20:55:49.679Z',
-        actionStatusJson: {
-          ack: {
-            timestamp: '2017-03-01T20:56:58.442Z',
-            state: 'acked',
-          },
-          last_execution: {
-            timestamp: '2017-03-01T20:55:49.679Z',
-            successful: true,
-            reason: 'reasons',
-          },
-          last_throttle: {
-            timestamp: '2017-03-01T20:55:49.679Z',
-            reason: 'reasons',
-          },
-          last_successful_execution: {
-            timestamp: '2017-03-01T20:55:49.679Z',
-            successful: true,
-          },
-        },
-      };
-    });
-
-    describe(`correctly calculates ACTION_STATES.ERROR`, () => {
-      it('lastExecutionSuccessful is equal to false and it is the most recent execution', () => {
-        upstreamJson.actionStatusJson.last_execution!.successful = false;
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-        expect(actionStatus.state).toBe(ACTION_STATES.ERROR);
-      });
-
-      it('action is acked and lastAcknowledged is less than lastExecution', () => {
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel({
-            ...upstreamJson,
-            actionStatusJson: {
-              ack: {
-                state: 'acked',
-                timestamp: '2017-03-01T00:00:00.000Z',
-              },
-              last_execution: {
-                timestamp: '2017-03-02T00:00:00.000Z',
-                successful: true,
-                reason: 'reasons',
-              },
-            },
-          })
-        );
-        expect(actionStatus.state).toBe(ACTION_STATES.ERROR);
-      });
-
-      it('action is ackable and lastSuccessfulExecution is less than lastExecution', () => {
-        delete upstreamJson.actionStatusJson.last_throttle;
-        upstreamJson.actionStatusJson.ack.state = 'ackable';
-        upstreamJson.actionStatusJson.last_successful_execution!.timestamp =
-          '2017-03-01T00:00:00.000Z';
-        upstreamJson.actionStatusJson.last_execution!.timestamp = '2017-03-02T00:00:00.000Z';
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-
-        expect(actionStatus.state).toBe(ACTION_STATES.ERROR);
-      });
-    });
-
-    it('correctly calculates ACTION_STATES.CONFIG_ERROR', () => {
-      const actionStatus = buildClientActionStatusModel(
-        buildServerActionStatusModel({
-          ...upstreamJson,
-          errors: { foo: 'bar' },
-        })
-      );
-      expect(actionStatus.state).toBe(ACTION_STATES.CONFIG_ERROR);
-    });
-
-    it(`correctly calculates ACTION_STATES.OK`, () => {
-      upstreamJson.actionStatusJson.ack.state = 'awaits_successful_execution';
-      const actionStatus = buildClientActionStatusModel(buildServerActionStatusModel(upstreamJson));
-
-      expect(actionStatus.state).toBe(ACTION_STATES.OK);
-    });
-
-    describe(`correctly calculates ACTION_STATES.ACKNOWLEDGED`, () => {
-      it(`when lastAcknowledged is equal to lastExecution`, () => {
-        upstreamJson.actionStatusJson.ack.state = 'acked';
-        upstreamJson.actionStatusJson.ack.timestamp = '2017-03-01T00:00:00.000Z';
-        upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-
-        expect(actionStatus.state).toBe(ACTION_STATES.ACKNOWLEDGED);
-      });
-
-      it(`when lastAcknowledged is greater than lastExecution`, () => {
-        upstreamJson.actionStatusJson.ack.state = 'acked';
-        upstreamJson.actionStatusJson.ack.timestamp = '2017-03-02T00:00:00.000Z';
-        upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-
-        expect(actionStatus.state).toBe(ACTION_STATES.ACKNOWLEDGED);
-      });
-    });
-
-    describe(`correctly calculates ACTION_STATES.THROTTLED`, () => {
-      it(`when lastThrottled is equal to lastExecution`, () => {
-        upstreamJson.actionStatusJson.ack.state = 'ackable';
-        upstreamJson.actionStatusJson.last_throttle.timestamp = '2017-03-01T00:00:00.000Z';
-        upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-
-        expect(actionStatus.state).toBe(ACTION_STATES.THROTTLED);
-      });
-
-      it(`when lastThrottled is greater than lastExecution`, () => {
-        upstreamJson.actionStatusJson.ack.state = 'ackable';
-        upstreamJson.actionStatusJson.last_throttle.timestamp = '2017-03-02T00:00:00.000Z';
-        upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-
-        expect(actionStatus.state).toBe(ACTION_STATES.THROTTLED);
-      });
-    });
-
-    describe(`correctly calculates ACTION_STATES.FIRING`, () => {
-      it(`when lastSuccessfulExecution is equal to lastExecution`, () => {
-        delete upstreamJson.actionStatusJson.last_throttle;
-        upstreamJson.actionStatusJson.ack.state = 'ackable';
-        upstreamJson.actionStatusJson.last_successful_execution.timestamp =
-          '2017-03-01T00:00:00.000Z';
-        upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-
-        expect(actionStatus.state).toBe(ACTION_STATES.FIRING);
-      });
-
-      it(`when lastSuccessfulExecution is greater than lastExecution`, () => {
-        delete upstreamJson.actionStatusJson.last_throttle;
-        upstreamJson.actionStatusJson.ack.state = 'ackable';
-        upstreamJson.actionStatusJson.last_successful_execution.timestamp =
-          '2017-03-02T00:00:00.000Z';
-        upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-        const actionStatus = buildClientActionStatusModel(
-          buildServerActionStatusModel(upstreamJson)
-        );
-
-        expect(actionStatus.state).toBe(ACTION_STATES.FIRING);
-      });
-    });
-
-    it(`correctly calculates ACTION_STATES.UNKNOWN if it can not determine state`, () => {
-      upstreamJson = {
-        id: 'my-action',
-        actionStatusJson: {
-          ack: { state: 'foo' },
-          last_successful_execution: { successful: true },
-        },
-      };
-      const actionStatus = buildClientActionStatusModel(buildServerActionStatusModel(upstreamJson));
-
-      expect(actionStatus.state).toBe(ACTION_STATES.UNKNOWN);
-    });
-  });
-
-  describe('isAckable', () => {
-    let upstreamJson: ActionStatusUpstreamJson;
-    beforeEach(() => {
-      upstreamJson = {
-        id: 'my-action',
-        errors: {},
-        lastCheckedRawFormat: '2017-03-01T20:55:49.679Z',
-        actionStatusJson: {
-          ack: {
-            timestamp: '2017-03-01T20:56:58.442Z',
-            state: 'acked',
-          },
-          last_execution: {
-            timestamp: '2017-03-01T20:55:49.679Z',
-            successful: true,
-            reason: 'reasons',
-          },
-          last_throttle: {
-            timestamp: '2017-03-01T20:55:49.679Z',
-            reason: 'reasons',
-          },
-          last_successful_execution: {
-            timestamp: '2017-03-01T20:55:49.679Z',
-            successful: true,
-          },
-        },
-      };
-    });
-
-    it(`correctly calculated isAckable when in ACTION_STATES.OK`, () => {
-      upstreamJson.actionStatusJson.ack.state = 'awaits_successful_execution';
-      const actionStatus = buildClientActionStatusModel(buildServerActionStatusModel(upstreamJson));
-
-      expect(actionStatus.state).toBe(ACTION_STATES.OK);
-      expect(actionStatus.isAckable).toBe(false);
-    });
-
-    it(`correctly calculated isAckable when in ACTION_STATES.ACKNOWLEDGED`, () => {
-      upstreamJson.actionStatusJson.ack.state = 'acked';
-      upstreamJson.actionStatusJson.ack.timestamp = '2017-03-01T00:00:00.000Z';
-      upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-      const actionStatus = buildClientActionStatusModel(buildServerActionStatusModel(upstreamJson));
-
-      expect(actionStatus.state).toBe(ACTION_STATES.ACKNOWLEDGED);
-      expect(actionStatus.isAckable).toBe(false);
-    });
-
-    it(`correctly calculated isAckable when in ACTION_STATES.THROTTLED`, () => {
-      upstreamJson.actionStatusJson.ack.state = 'ackable';
-      upstreamJson.actionStatusJson.last_throttle.timestamp = '2017-03-01T00:00:00.000Z';
-      upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-      const actionStatus = buildClientActionStatusModel(buildServerActionStatusModel(upstreamJson));
-
-      expect(actionStatus.state).toBe(ACTION_STATES.THROTTLED);
-      expect(actionStatus.isAckable).toBe(true);
-    });
-
-    it(`correctly calculated isAckable when in ACTION_STATES.FIRING`, () => {
-      delete upstreamJson.actionStatusJson.last_throttle;
-      upstreamJson.actionStatusJson.ack.state = 'ackable';
-      upstreamJson.actionStatusJson.last_successful_execution.timestamp =
-        '2017-03-01T00:00:00.000Z';
-      upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-01T00:00:00.000Z';
-      const actionStatus = buildClientActionStatusModel(buildServerActionStatusModel(upstreamJson));
-
-      expect(actionStatus.state).toBe(ACTION_STATES.FIRING);
-      expect(actionStatus.isAckable).toBe(true);
-    });
-
-    it(`correctly calculated isAckable when in ACTION_STATES.ERROR`, () => {
-      delete upstreamJson.actionStatusJson.last_throttle;
-      upstreamJson.actionStatusJson.ack.state = 'ackable';
-      upstreamJson.actionStatusJson.last_successful_execution.timestamp =
-        '2017-03-01T00:00:00.000Z';
-      upstreamJson.actionStatusJson.last_execution.timestamp = '2017-03-02T00:00:00.000Z';
-      const actionStatus = buildClientActionStatusModel(buildServerActionStatusModel(upstreamJson));
-
-      expect(actionStatus.state).toBe(ACTION_STATES.ERROR);
-      expect(actionStatus.isAckable).toBe(false);
+      expect(serverActionStatusModel.errors).toEqual({ foo: 'bar' });
     });
   });
 
@@ -350,7 +88,6 @@ describe('ActionStatusModel', () => {
     beforeEach(() => {
       upstreamJson = {
         id: 'my-action',
-        errors: {},
         lastCheckedRawFormat: '2017-03-01T20:55:49.679Z',
         actionStatusJson: {
           ack: {
@@ -375,19 +112,29 @@ describe('ActionStatusModel', () => {
     });
 
     it('returns correct JSON for client', () => {
-      const actionStatus = buildServerActionStatusModel(upstreamJson);
+      const serverActionStatusModel = buildServerActionStatusModel(upstreamJson);
+      const clientActionStatusModel = buildClientActionStatusModel(serverActionStatusModel);
 
-      const json = buildClientActionStatusModel(actionStatus);
+      // These properties should be transcribed 1:1.
+      expect(clientActionStatusModel.id).toBe(serverActionStatusModel.id);
+      expect(clientActionStatusModel.lastAcknowledged).toBe(
+        serverActionStatusModel.lastAcknowledged
+      );
+      expect(clientActionStatusModel.lastThrottled).toBe(serverActionStatusModel.lastThrottled);
+      expect(clientActionStatusModel.lastExecution).toBe(serverActionStatusModel.lastExecution);
+      expect(clientActionStatusModel.lastExecutionSuccessful).toBe(
+        serverActionStatusModel.lastExecutionSuccessful
+      );
+      expect(clientActionStatusModel.lastExecutionReason).toBe(
+        serverActionStatusModel.lastExecutionReason
+      );
+      expect(clientActionStatusModel.lastSuccessfulExecution).toBe(
+        serverActionStatusModel.lastSuccessfulExecution
+      );
 
-      expect(json.id).toBe(actionStatus.id);
-      expect(json.state).toBe(actionStatus.state);
-      expect(json.isAckable).toBe(actionStatus.isAckable);
-      expect(json.lastAcknowledged).toBe(actionStatus.lastAcknowledged);
-      expect(json.lastThrottled).toBe(actionStatus.lastThrottled);
-      expect(json.lastExecution).toBe(actionStatus.lastExecution);
-      expect(json.lastExecutionSuccessful).toBe(actionStatus.lastExecutionSuccessful);
-      expect(json.lastExecutionReason).toBe(actionStatus.lastExecutionReason);
-      expect(json.lastSuccessfulExecution).toBe(actionStatus.lastSuccessfulExecution);
+      // These properties are derived when clientActionStatusModel is created.
+      expect(clientActionStatusModel.state).toBe(ACTION_STATES.ACKNOWLEDGED);
+      expect(clientActionStatusModel.isAckable).toBe(false);
     });
   });
 });
--- x-pack/plugins/watcher/server/models/action_status_model/action_status_model.ts
+++ x-pack/plugins/watcher/server/models/action_status_model/action_status_model.ts
@@ -8,9 +8,9 @@
 import { badRequest } from '@hapi/boom';
 import { i18n } from '@kbn/i18n';
 
-import { ACTION_STATES } from '../../../common/constants';
 import { ActionStatusUpstreamJson, ServerActionStatusModel } from '../../../common/types';
 import { getMoment } from '../../../common/lib/get_moment';
+import { deriveState, deriveIsAckable } from './action_status_model_utils';
 
 // Replaces fromUpstreamJson factory method.
 export const buildServerActionStatusModel = (
@@ -77,77 +77,4 @@ export const buildClientActionStatusModel = (serverActionStatusModel: ServerActi
     state,
     isAckable,
   };
-};
-
-const deriveState = (serverActionStatusModel: ServerActionStatusModel) => {
-  const {
-    actionStatusJson,
-    lastExecutionSuccessful,
-    lastCheckedRawFormat,
-    lastExecutionRawFormat,
-    errors,
-    lastAcknowledged,
-    lastExecution,
-    lastThrottled,
-    lastSuccessfulExecution,
-  } = serverActionStatusModel;
-  const ackState = actionStatusJson.ack.state;
-
-  if (lastExecutionSuccessful === false && lastCheckedRawFormat === lastExecutionRawFormat) {
-    return ACTION_STATES.ERROR;
-  }
-
-  if (errors) {
-    return ACTION_STATES.CONFIG_ERROR;
-  }
-
-  if (ackState === 'awaits_successful_execution') {
-    return ACTION_STATES.OK;
-  }
-
-  if (lastExecution) {
-    // Might be null
-    if (lastAcknowledged) {
-      // Might be null
-      if (ackState === 'acked' && lastAcknowledged >= lastExecution) {
-        return ACTION_STATES.ACKNOWLEDGED;
-      }
-
-      // A user could potentially land in this state if running on multiple nodes and timing is off
-      if (ackState === 'acked' && lastAcknowledged < lastExecution) {
-        return ACTION_STATES.ERROR;
-      }
-    }
-
-    if (lastThrottled) {
-      // Might be null
-      if (ackState === 'ackable' && lastThrottled >= lastExecution) {
-        return ACTION_STATES.THROTTLED;
-      }
-    }
-
-    if (lastSuccessfulExecution) {
-      // Might be null
-      if (ackState === 'ackable' && lastSuccessfulExecution >= lastExecution) {
-        return ACTION_STATES.FIRING;
-      }
-
-      if (ackState === 'ackable' && lastSuccessfulExecution < lastExecution) {
-        return ACTION_STATES.ERROR;
-      }
-    }
-  }
-
-  // At this point, we cannot determine the action status so mark it as "unknown".
-  // We should never get to this point in the code. If we do, it means we are
-  // missing an action status and the logic to determine it.
-  return ACTION_STATES.UNKNOWN;
-};
-
-const deriveIsAckable = (state: keyof typeof ACTION_STATES) => {
-  if (state === ACTION_STATES.THROTTLED || state === ACTION_STATES.FIRING) {
-    return true;
-  }
-
-  return false;
 };
