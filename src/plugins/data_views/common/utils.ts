c663cd1f5f53f83494c51698c7684b61e146ccd0
Matt Kime <matt@mattki.me>
2022-08-08 09:54:12 -0500

    fix data view create uniqueness check

--- src/plugins/data_views/common/utils.ts
+++ src/plugins/data_views/common/utils.ts
@@ -20,14 +20,14 @@ import { DATA_VIEW_SAVED_OBJECT_TYPE } from './constants';
  */
 export async function findByName(client: SavedObjectsClientCommon, name: string) {
   if (name) {
-    const savedObjects = await client.find<DataViewSavedObjectAttrs>({
+    const savedObjects = await client.find<{ name: DataViewSavedObjectAttrs['name'] }>({
       type: DATA_VIEW_SAVED_OBJECT_TYPE,
-      perPage: 10,
+      perPage: 10000,
       search: `"${name}"`,
       searchFields: ['name'],
       fields: ['name'],
     });
 
-    return savedObjects ? savedObjects[0] : undefined;
+    return !!savedObjects.find((savedObject) => savedObject.attributes.name === name);
   }
 }
