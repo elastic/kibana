4939cee5d22e994c0e4a631e217ba621214d3584
Tiago Costa <tiago.costa@elastic.co>
2022-08-08 17:17:09 +0100

    skip flaky suite failing es promotion (#138293)

--- x-pack/test/functional/apps/ml/anomaly_detection/anomaly_explorer.ts
+++ x-pack/test/functional/apps/ml/anomaly_detection/anomaly_explorer.ts
@@ -63,7 +63,8 @@ export default function ({ getService }: FtrProviderContext) {
   const ml = getService('ml');
   const elasticChart = getService('elasticChart');
 
-  describe('anomaly explorer', function () {
+  // FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/138293
+  describe.skip('anomaly explorer', function () {
     // FAILING ES FORWARDS COMPATIBILITY TESTS: https://github.com/elastic/kibana/issues/137602
     this.onlyEsVersion('<8');
 
