291ce0dbf22755bc8b31e1bccfb100b8513ecd01
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-08-09 19:09:11 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- src/dev/typescript/run_type_check_cli.ts
+++ src/dev/typescript/run_type_check_cli.ts
@@ -55,7 +55,8 @@ export async function runTypeCheckCli() {
         }
       }
 
-      const concurrencyArg = typeof flags.concurrency === 'string' && parseInt(flags.concurrency, 10);
+      const concurrencyArg =
+        typeof flags.concurrency === 'string' && parseInt(flags.concurrency, 10);
       const concurrency =
         concurrencyArg && concurrencyArg > 0
           ? concurrencyArg
