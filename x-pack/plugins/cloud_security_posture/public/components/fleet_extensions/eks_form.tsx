15f52cd844f9a919c9535b3fe303c16dbe9f5bed
Or Ouziel <or.ouziel@elastic.co>
2022-08-09 21:47:40 +0300

    fix AWS form text

--- x-pack/plugins/cloud_security_posture/public/components/fleet_extensions/eks_form.tsx
+++ x-pack/plugins/cloud_security_posture/public/components/fleet_extensions/eks_form.tsx
@@ -5,7 +5,15 @@
  * 2.0.
  */
 import React from 'react';
-import { EuiFormRow, EuiFieldText, EuiDescribedFormGroup, EuiText, EuiSpacer } from '@elastic/eui';
+import {
+  EuiFormRow,
+  EuiFieldText,
+  EuiDescribedFormGroup,
+  EuiLink,
+  EuiText,
+  EuiSpacer,
+  EuiMarkdownFormat,
+} from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import type { NewPackagePolicyInput } from '@kbn/fleet-plugin/common';
 import { i18n } from '@kbn/i18n';
@@ -75,6 +83,25 @@ const EksForm = ({ onChange, inputs }: Props) => {
   const eksFormDescription = (
     <>
       <FormattedMessage
+        id="xpack.csp.createPackagePolicy.eksIntegrationSettingsSection.awsCredentialsDescription"
+        defaultMessage={`In order to run some of the rules in the benchmark, we need elevated access. You can follow {link} to create an IAM user with programmatic access that is associated with a policy with the set of permissions captured in the table below. You can either create a new policy or append these service permissions to an existing one- we recommend creating a new one.`}
+        values={{
+          // TODO: add link
+          link: (
+            <EuiLink
+              href="https://docs.aws.amazon.com/IAM/latest/UserGuide/id_users_create.html"
+              target="_blank"
+            >
+              <FormattedMessage
+                id="xpack.csp.createPackagePolicy.eksIntegrationSettingsSection.awsCredentialsInstructionsLink"
+                defaultMessage="these instructions"
+              />
+            </EuiLink>
+          ),
+        }}
+      />
+      <EuiSpacer />
+      <FormattedMessage
         id="xpack.csp.createPackagePolicy.eksIntegrationSettingsSection.awsCredentialsNote"
         defaultMessage="If you choose not to provide credentials, only a subset of the benchmark rules will be evaluated against your cluster(s)."
       />
@@ -82,26 +109,38 @@ const EksForm = ({ onChange, inputs }: Props) => {
   );
 
   return (
-    <EuiDescribedFormGroup title={eksFormTitle} description={eksFormDescription}>
-      {eksVars.map((field) => (
-        <EuiFormRow
-          key={field.id}
-          label={field.label}
-          labelAppend={
-            <EuiText size="xs" color="subdued">
-              <FormattedMessage
-                id="xpack.csp.createPackagePolicy.eksIntegrationSettingsSection.optionalField"
-                defaultMessage="Optional"
-              />
-            </EuiText>
-          }
-        >
-          <EuiFieldText
-            value={values[field.id]}
-            onChange={(event) => onChange(field.id, event.target.value)}
-          />
-        </EuiFormRow>
-      ))}
-    </EuiDescribedFormGroup>
+    <div>
+      <EuiDescribedFormGroup title={eksFormTitle} description={eksFormDescription}>
+        {eksVars.map((field) => (
+          <EuiFormRow
+            key={field.id}
+            label={field.label}
+            labelAppend={
+              <EuiText size="xs" color="subdued">
+                <FormattedMessage
+                  id="xpack.csp.createPackagePolicy.eksIntegrationSettingsSection.optionalField"
+                  defaultMessage="Optional"
+                />
+              </EuiText>
+            }
+          >
+            <EuiFieldText
+              value={values[field.id]}
+              onChange={(event) => onChange(field.id, event.target.value)}
+            />
+          </EuiFormRow>
+        ))}
+      </EuiDescribedFormGroup>
+      <EuiMarkdownFormat textSize="xs" textAlign="left" grow={false} style={{ textAlign: 'left' }}>
+        {`| Service                           | Permissions |
+| --------------------------------- | ----------- |
+| EKS                               | List, Read  |
+| Elastic Container Registry        | List, Read  |
+| Elastic Container Registry Public | List, Read  |
+| ELB                               | List, Read  |
+| ELB v2                            | Read        | `}
+      </EuiMarkdownFormat>
+      <EuiSpacer size="xl" />
+    </div>
   );
 };
