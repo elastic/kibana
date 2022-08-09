442d507049c9ac04950873433b26f9a25defa925
Byron Hulcher <byron.hulcher@elastic.co>
2022-08-09 15:20:46 -0400

    Utilize elasticsearchErrorHandler for all enterprise_search routes

--- x-pack/plugins/enterprise_search/server/routes/enterprise_search/config_data.ts
+++ x-pack/plugins/enterprise_search/server/routes/enterprise_search/config_data.ts
@@ -7,6 +7,7 @@
 
 import { callEnterpriseSearchConfigAPI } from '../../lib/enterprise_search_config_api';
 import { RouteDependencies } from '../../plugin';
+import { elasticsearchErrorHandler } from '../../utils/elasticsearch_error_handler';
 
 export function registerConfigDataRoute({ router, config, log }: RouteDependencies) {
   router.get(
@@ -14,18 +15,18 @@ export function registerConfigDataRoute({ router, config, log }: RouteDependenci
       path: '/internal/enterprise_search/config_data',
       validate: false,
     },
-    async (context, request, response) => {
-      const data = await callEnterpriseSearchConfigAPI({ request, config, log });
+    elasticsearchErrorHandler(log, async (context, request, response) => {
+      const data = await callEnterpriseSearchConfigAPI({ config, log, request });
 
       if ('responseStatus' in data) {
         return response.customError({
+          body: 'Error fetching data from Enterprise Search', // TODO i18n
           statusCode: data.responseStatus,
-          body: 'Error fetching data from Enterprise Search',
         });
       } else if (!Object.keys(data).length) {
         return response.customError({
+          body: 'Error fetching data from Enterprise Search', // TODO i18n
           statusCode: 502,
-          body: 'Error fetching data from Enterprise Search',
         });
       } else {
         return response.ok({
@@ -33,6 +34,6 @@ export function registerConfigDataRoute({ router, config, log }: RouteDependenci
           headers: { 'content-type': 'application/json' },
         });
       }
-    }
+    })
   );
 }
