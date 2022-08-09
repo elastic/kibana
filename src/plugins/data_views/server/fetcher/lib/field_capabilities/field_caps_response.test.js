f3044efcfa363674c9634bb6279cf096dc3268af
ppisljar <peter.pisljar@elastic.co>
2022-08-09 10:54:40 +0200

    fixing test

--- src/plugins/data_views/server/fetcher/lib/field_capabilities/field_caps_response.test.js
+++ src/plugins/data_views/server/fetcher/lib/field_capabilities/field_caps_response.test.js
@@ -49,6 +49,10 @@ describe('index_patterns/field_capabilities/field_caps_response', () => {
               'aggregatable',
               'readFromDocValues',
               'metadata_field',
+              'fixedInterval',
+              'timeZone',
+              'timeSeriesMetric',
+              'timeSeriesDimension',
             ]);
           });
         }
