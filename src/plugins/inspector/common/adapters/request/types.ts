457e307778e58a0c7120f79cdc0256e24dbd0dd4
Timothy Sullivan <tsullivan@elastic.co>
2022-08-10 12:33:29 -0700

    comments for api items

--- src/plugins/inspector/common/adapters/request/types.ts
+++ src/plugins/inspector/common/adapters/request/types.ts
@@ -51,8 +51,17 @@ export interface RequestStatistic {
   value: any;
 }
 
+/**
+ * The format of a response as seen from Inspector
+ */
 export interface Response {
+  /**
+   * Unknown data of response. Not assumed to be from Elasticsearch;
+   */
   json?: object;
+  /**
+   * Response load time metric
+   */
   time?: number;
 }
 
@@ -60,6 +69,12 @@ export interface Response {
  * The format of a warning in an Elasticsearch search response.
  */
 export interface ResponseWarning {
+  /**
+   * Title string for context
+   */
   title?: string;
+  /**
+   * Text of the warning message from the remote
+   */
   text?: string;
 }
