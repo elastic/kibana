248700369e9126c95e8d622ef160ae54291d8005
Alexey Antonov <alexwizp@gmail.com>
2022-08-10 17:46:53 +0300

    cleanup

--- packages/kbn-chart-icons/src/assets/annotation_icons/triangle.tsx
+++ packages/kbn-chart-icons/src/assets/annotation_icons/triangle.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import * as React from 'react';
+import React from 'react';
 import { EuiIconProps } from '@elastic/eui';
 import { cx } from '@emotion/css';
 import { noFill } from '../common_styles';
--- packages/kbn-chart-icons/src/assets/drop_illustration.tsx
+++ packages/kbn-chart-icons/src/assets/drop_illustration.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import * as React from 'react';
+import React from 'react';
 import { EuiIconProps } from '@elastic/eui';
 
 export const DropIllustration = ({ title, titleId, ...props }: Omit<EuiIconProps, 'type'>) => (
