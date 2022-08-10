d78b532b95c48bc64db0e83fa394df9e73bc7f42
Brian Seeders <brian.seeders@elastic.co>
2022-08-10 16:11:59 -0400

    [CI] Improve reliability of macos bazel cache jobs (#138554)

--- .buildkite/scripts/steps/bazel_cache/bootstrap_mac.sh
+++ .buildkite/scripts/steps/bazel_cache/bootstrap_mac.sh
@@ -7,6 +7,9 @@ source .buildkite/scripts/common/util.sh
 export BAZEL_CACHE_MODE=populate-local-gcs
 export DISABLE_BOOTSTRAP_VALIDATION=true
 
+# Because we're manually deleting node_modules and bazel directories in-between runs, we need to --force-install
+export BOOTSTRAP_ALWAYS_FORCE_INSTALL=true
+
 # Clear out bazel cache between runs to make sure that any artifacts that don't exist in the cache are uploaded
 rm -rf ~/.bazel-cache
 
