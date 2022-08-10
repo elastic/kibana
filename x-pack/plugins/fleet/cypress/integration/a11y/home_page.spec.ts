6e8bcf405e898bd5a1eff496e95a73bbe0fda60c
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-08-10 01:05:48 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/fleet/cypress/integration/a11y/home_page.spec.ts
+++ x-pack/plugins/fleet/cypress/integration/a11y/home_page.spec.ts
@@ -8,67 +8,71 @@ import 'cypress-real-events/support';
 import { checkA11y } from '../../support/commands';
 import { FLEET, navigateTo } from '../../tasks/navigation';
 import {
-    GENERATE_FLEET_SERVER_POLICY_BUTTON,
-    AGENTS_QUICK_START_TAB_BUTTON,
-    PLATFORM_TYPE_LINUX_BUTTON,
-    AGENTS_ADVANCED_TAB_BUTTON,
-    ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON,
-    ADVANCED_FLEET_SERVER_GENERATE_SERVICE_TOKEN_BUTTON,
-    AGENT_POLICIES_TAB,
-    AGENT_POLICIES_CREATE_AGENT_POLICY_BUTTON,
-    AGENT_POLICIES_CREATE_AGENT_POLICY_FLYOUT_TITLE,
-    AGENT_POLICY_CREATE_AGENT_POLICY_NAME_FIELD
+  GENERATE_FLEET_SERVER_POLICY_BUTTON,
+  AGENTS_QUICK_START_TAB_BUTTON,
+  PLATFORM_TYPE_LINUX_BUTTON,
+  AGENTS_ADVANCED_TAB_BUTTON,
+  ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON,
+  ADVANCED_FLEET_SERVER_GENERATE_SERVICE_TOKEN_BUTTON,
+  AGENT_POLICIES_TAB,
+  AGENT_POLICIES_CREATE_AGENT_POLICY_BUTTON,
+  AGENT_POLICIES_CREATE_AGENT_POLICY_FLYOUT_TITLE,
+  AGENT_POLICY_CREATE_AGENT_POLICY_NAME_FIELD,
 } from '../../screens/fleet';
 describe('Home page', () => {
-    before(() => {
-        navigateTo(FLEET);
-        cy.getBySel(AGENTS_QUICK_START_TAB_BUTTON, { timeout: 15000 }).should('be.visible');
-    });
+  before(() => {
+    navigateTo(FLEET);
+    cy.getBySel(AGENTS_QUICK_START_TAB_BUTTON, { timeout: 15000 }).should('be.visible');
+  });
 
-    describe('Agents', () => {
-        const fleetServerHost = 'https://localhost:8220';
-        describe('Quick Start', () => {
-            it('Get started with fleet', () => {
-                checkA11y({ skipFailures: false });
-            });
-            it('Install Fleet Server', () => {
-                cy.getBySel('fleetServerHostInput').getBySel('comboBoxSearchInput').type(fleetServerHost);
-                cy.getBySel(GENERATE_FLEET_SERVER_POLICY_BUTTON).click();
-                cy.getBySel(PLATFORM_TYPE_LINUX_BUTTON, { timeout: 15000 }).should('be.visible');
-                checkA11y({ skipFailures: false });
-            });
-        });
-        describe('Advanced', () => {
-            before(() => {
-                cy.getBySel(AGENTS_ADVANCED_TAB_BUTTON).click();
-            });
-            it('Select policy for fleet', () => {
-                checkA11y({ skipFailures: false });
-            });
-            it('Add your fleet sever host', () => {
-                cy.getBySel(ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON).click();
-                checkA11y({ skipFailures: false })
-            });
-            it('Generate service token', () => {
-                cy.getBySel(ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON, { timeout: 15000 }).should('be.visible');
-                cy.getBySel(ADVANCED_FLEET_SERVER_GENERATE_SERVICE_TOKEN_BUTTON).click();
-                cy.getBySel(PLATFORM_TYPE_LINUX_BUTTON, { timeout: 15000 }).should('be.visible');
-                checkA11y({ skipFailures: false })
-            });
-        });
+  describe('Agents', () => {
+    const fleetServerHost = 'https://localhost:8220';
+    describe('Quick Start', () => {
+      it('Get started with fleet', () => {
+        checkA11y({ skipFailures: false });
+      });
+      it('Install Fleet Server', () => {
+        cy.getBySel('fleetServerHostInput').getBySel('comboBoxSearchInput').type(fleetServerHost);
+        cy.getBySel(GENERATE_FLEET_SERVER_POLICY_BUTTON).click();
+        cy.getBySel(PLATFORM_TYPE_LINUX_BUTTON, { timeout: 15000 }).should('be.visible');
+        checkA11y({ skipFailures: false });
+      });
     });
-    describe('Agent Policies', () => {
-        before(() => {
-            cy.getBySel(AGENT_POLICIES_TAB).click();
-            cy.getBySel(AGENT_POLICIES_CREATE_AGENT_POLICY_BUTTON, { timeout: 15000 }).should('be.visible');
-        })
-        it('Agent Table', () => {
-            checkA11y({ skipFailures: false });
-        });
-        it('Create Policy Flyout', () => {
-            cy.getBySel(AGENT_POLICIES_CREATE_AGENT_POLICY_BUTTON).click();
-            cy.getBySel(AGENT_POLICIES_CREATE_AGENT_POLICY_FLYOUT_TITLE, { timeout: 15000 }).should('be.visible');
-            cy.getBySel(AGENT_POLICY_CREATE_AGENT_POLICY_NAME_FIELD).type('testName');
-        });
+    describe('Advanced', () => {
+      before(() => {
+        cy.getBySel(AGENTS_ADVANCED_TAB_BUTTON).click();
+      });
+      it('Select policy for fleet', () => {
+        checkA11y({ skipFailures: false });
+      });
+      it('Add your fleet sever host', () => {
+        cy.getBySel(ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON).click();
+        checkA11y({ skipFailures: false });
+      });
+      it('Generate service token', () => {
+        cy.getBySel(ADVANCED_FLEET_SERVER_ADD_HOST_BUTTON, { timeout: 15000 }).should('be.visible');
+        cy.getBySel(ADVANCED_FLEET_SERVER_GENERATE_SERVICE_TOKEN_BUTTON).click();
+        cy.getBySel(PLATFORM_TYPE_LINUX_BUTTON, { timeout: 15000 }).should('be.visible');
+        checkA11y({ skipFailures: false });
+      });
     });
+  });
+  describe('Agent Policies', () => {
+    before(() => {
+      cy.getBySel(AGENT_POLICIES_TAB).click();
+      cy.getBySel(AGENT_POLICIES_CREATE_AGENT_POLICY_BUTTON, { timeout: 15000 }).should(
+        'be.visible'
+      );
+    });
+    it('Agent Table', () => {
+      checkA11y({ skipFailures: false });
+    });
+    it('Create Policy Flyout', () => {
+      cy.getBySel(AGENT_POLICIES_CREATE_AGENT_POLICY_BUTTON).click();
+      cy.getBySel(AGENT_POLICIES_CREATE_AGENT_POLICY_FLYOUT_TITLE, { timeout: 15000 }).should(
+        'be.visible'
+      );
+      cy.getBySel(AGENT_POLICY_CREATE_AGENT_POLICY_NAME_FIELD).type('testName');
+    });
+  });
 });
