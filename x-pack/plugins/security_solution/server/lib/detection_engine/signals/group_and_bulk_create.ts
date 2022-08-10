9206c6c013d1908069b73883e6eb92eec84075f1
Madi Caldwell <madison.caldwell@elastic.co>
2022-08-10 14:19:27 -0400

    Refactor

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/group_and_bulk_create.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/group_and_bulk_create.ts
@@ -100,125 +100,142 @@ export const groupAndBulkCreate = async ({
   secondaryTimestamp,
 }: SearchAfterAndBulkCreateParams): Promise<SearchAfterAndBulkCreateReturnType> => {
   return withSecuritySpan('groupAndBulkCreate', async () => {
-    let toReturn = createSearchAfterReturnType();
+    if (tuple == null || tuple.to == null || tuple.from == null) {
+      ruleExecutionLogger.error(`[-] malformed date tuple`);
+      return createSearchAfterReturnType({
+        success: false,
+        errors: ['malformed date tuple'],
+      });
+    }
 
     const to = tuple.to.toISOString();
     const from = tuple.from.toISOString();
 
-    const rangeFilter = buildTimeRangeFilter({
-      to,
-      from,
-      primaryTimestamp,
-      secondaryTimestamp,
-    });
+    try {
+      const rangeFilter = buildTimeRangeFilter({
+        to,
+        from,
+        primaryTimestamp,
+        secondaryTimestamp,
+      });
 
-    const filterWithTime: estypes.QueryDslQueryContainer[] = [filter, rangeFilter];
+      const filterWithTime: estypes.QueryDslQueryContainer[] = [filter, rangeFilter];
 
-    const baseQuery: estypes.SearchRequest & {
-      runtime_mappings: estypes.MappingRuntimeFields | undefined;
-    } = {
-      allow_no_indices: true,
-      runtime_mappings: runtimeMappings,
-      index: inputIndexPattern,
-      ignore_unavailable: true,
-      track_total_hits: trackTotalHits,
-      body: {
-        query: {
-          bool: {
-            filter: [...filterWithTime],
+      const baseQuery: estypes.SearchRequest & {
+        runtime_mappings: estypes.MappingRuntimeFields | undefined;
+      } = {
+        allow_no_indices: true,
+        runtime_mappings: runtimeMappings,
+        index: inputIndexPattern,
+        ignore_unavailable: true,
+        track_total_hits: trackTotalHits,
+        body: {
+          query: {
+            bool: {
+              filter: filterWithTime,
+            },
           },
         },
-      },
-    };
+      };
 
-    const sort: estypes.Sort = [];
-    sort.push({
-      [primaryTimestamp]: {
-        order: sortOrder,
-        unmapped_type: 'date',
-      },
-    });
-    if (secondaryTimestamp) {
+      const sort: estypes.Sort = [];
       sort.push({
-        [secondaryTimestamp]: {
+        [primaryTimestamp]: {
           order: sortOrder,
           unmapped_type: 'date',
         },
       });
-    }
+      if (secondaryTimestamp) {
+        sort.push({
+          [secondaryTimestamp]: {
+            order: sortOrder,
+            unmapped_type: 'date',
+          },
+        });
+      }
 
-    // TODO: Determine field(s) to group by
-    // Current thoughts...
-    // - analyze query for fields/wildcards
-    // - get a random sample of docs... analyze fields in the docs
-    // - use that to determine which fields to group by
-    // - low cardinality means we may want to group by that field?
-    // - also, should we sort first by severity?
+      // TODO: Determine field(s) to group by
+      // Current thoughts...
+      // - analyze query for fields/wildcards
+      // - get a random sample of docs... analyze fields in the docs
+      // - use that to determine which fields to group by
+      // - low cardinality means we may want to group by that field?
+      // - also, should we sort first by severity?
 
-    // For now, group by host name
-    const groupByFields: string[] = ['host.name'];
+      // For now, group by host name
+      const groupByFields: string[] = ['host.name'];
 
-    // Get aggregated results
-    const eventsByGroupResponse = await getEventsByGroup({
-      baseQuery,
-      groupByFields,
-      services,
-      maxSignals: tuple.maxSignals,
-      sort,
-    });
+      // Get aggregated results
+      const eventsByGroupResponse = await getEventsByGroup({
+        baseQuery,
+        groupByFields,
+        services,
+        maxSignals: tuple.maxSignals,
+        sort,
+      });
 
-    const eventsByGroupResponseWithAggs =
-      eventsByGroupResponse as EventGroupingMultiBucketAggregationResult;
-    if (!eventsByGroupResponseWithAggs.aggregations) {
-      throw new Error('expected to find aggregations on search result');
-    }
+      const eventsByGroupResponseWithAggs =
+        eventsByGroupResponse as EventGroupingMultiBucketAggregationResult;
+      if (!eventsByGroupResponseWithAggs.aggregations) {
+        throw new Error('expected to find aggregations on search result');
+      }
 
-    const buckets = eventsByGroupResponseWithAggs.aggregations.eventGroups.buckets;
+      const buckets = eventsByGroupResponseWithAggs.aggregations.eventGroups.buckets;
 
-    // Index
-    const toIndex: Array<estypes.SearchHit<SignalSource>> = [];
+      // Reverse so we can pop() instead of shift() for efficiency
+      for (const bucket of buckets) {
+        bucket.topHits.hits.hits.reverse();
+      }
 
-    let indexedThisPass = [];
-    let indexedLastPass = [];
-    for (let i = 0; ; i = (i + 1) % buckets.length) {
-      if (i === 0) {
-        if (!indexedThisPass.length && indexedLastPass.length) {
+      // Index
+      const toIndex: Array<estypes.SearchHit<SignalSource>> = [];
+
+      let indexedThisPass = [];
+      let indexedLastPass = [];
+      for (let i = 0; ; i = (i + 1) % buckets.length) {
+        if (i === 0) {
+          if (!indexedThisPass.length && indexedLastPass.length) {
+            break;
+          }
+          // spread to copy the array
+          indexedLastPass = [...indexedThisPass];
+          indexedThisPass = [];
+        }
+
+        const event = buckets[i].topHits.hits.hits.pop();
+        if (event != null) {
+          toIndex.push(event);
+          indexedThisPass.push(buckets[i].key);
+        }
+
+        if (toIndex.length >= tuple.maxSignals) {
           break;
         }
-        indexedLastPass = [...indexedThisPass];
-        indexedThisPass = [];
       }
 
-      const event = buckets[i].topHits.hits.hits.pop();
-      if (event != null) {
-        toIndex.push(event);
-        indexedThisPass.push(buckets[i].key);
-      }
+      const enrichedEvents = await enrichment(toIndex);
+      const wrappedDocs = wrapHits(enrichedEvents, buildReasonMessage);
 
-      if (toIndex.length >= tuple.maxSignals) {
-        break;
-      }
+      const {
+        bulkCreateDuration: bulkDuration,
+        createdItemsCount: createdCount,
+        createdItems,
+        success: bulkSuccess,
+        errors: bulkErrors,
+      } = await bulkCreate(wrappedDocs);
+
+      return createSearchAfterReturnType({
+        success: bulkSuccess,
+        createdSignalsCount: createdCount,
+        createdSignals: createdItems,
+        bulkCreateTimes: [bulkDuration],
+        errors: bulkErrors,
+      });
+    } catch (exc: unknown) {
+      return createSearchAfterReturnType({
+        success: false,
+        errors: [`${exc}`],
+      });
     }
-
-    const enrichedEvents = await enrichment(toIndex);
-    const wrappedDocs = wrapHits(enrichedEvents, buildReasonMessage);
-
-    const {
-      bulkCreateDuration: bulkDuration,
-      createdItemsCount: createdCount,
-      createdItems,
-      success: bulkSuccess,
-      errors: bulkErrors,
-    } = await bulkCreate(wrappedDocs);
-
-    toReturn = createSearchAfterReturnType({
-      success: bulkSuccess,
-      createdSignalsCount: createdCount,
-      createdSignals: createdItems,
-      bulkCreateTimes: [bulkDuration],
-      errors: bulkErrors,
-    });
-
-    return toReturn;
   });
 };
