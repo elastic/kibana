7be72450941df0d6adfc0ef4da578ce314f60106
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-08-11 12:58:21 +0200

    [kbn-performance-testing-dataset-extractor] organise kibana concurrent calls into streams (#138263)
    * use streams for both es and kibana calls
    
    * improve run speed
    
    * update comments
    
    * fixes for review
    
    * update test
    
    * use msearch to get all ES requests
    
    * remove unused method
    
    * Update packages/kbn-performance-testing-dataset-extractor/src/es_client.ts
    
    Co-authored-by: Spencer <email@spalger.com>
    
    * fix code
    
    * specify index
    
    * check span is defined
    
    Co-authored-by: Spencer <email@spalger.com>

--- packages/kbn-performance-testing-dataset-extractor/src/constants.ts
+++ packages/kbn-performance-testing-dataset-extractor/src/constants.ts
@@ -6,8 +6,4 @@
  * Side Public License, v 1.
  */
 
-module.exports = {
-  preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-performance-testing-dataset-extractor'],
-};
+export const DATE_FORMAT = `YYYY-MM-DD'T'HH:mm:ss.SSS'Z'`;
