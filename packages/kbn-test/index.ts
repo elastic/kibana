f5b8381fbbb6e3f2b3c8e2510d998121b4742546
Tiago Costa <tiago.costa@elastic.co>
2022-08-11 21:16:40 +0100

    chore(NA): remove src folder from kbn-t* p2 pkgs

--- packages/kbn-test/index.ts
+++ packages/kbn-test/index.ts
@@ -13,32 +13,32 @@ import {
   startServersCli,
   processStartServersCliOptions,
   // @ts-ignore not typed yet
-} from './functional_tests/cli';
+} from './src/functional_tests/cli';
 
-export { KbnClientRequesterError } from './kbn_client/kbn_client_requester_error';
+export { KbnClientRequesterError } from './src/kbn_client/kbn_client_requester_error';
 
 // @internal
 export { runTestsCli, processRunTestsCliOptions, startServersCli, processStartServersCliOptions };
 
 // @ts-ignore not typed yet
 // @internal
-export { runTests, startServers } from './functional_tests/tasks';
+export { runTests, startServers } from './src/functional_tests/tasks';
 
-export { getKibanaCliArg, getKibanaCliLoggers } from './functional_tests/lib/kibana_cli_args';
+export { getKibanaCliArg, getKibanaCliLoggers } from './src/functional_tests/lib/kibana_cli_args';
 
 export type {
   CreateTestEsClusterOptions,
   EsTestCluster,
   ICluster,
   EsClientForTestingOptions,
-} from './es';
+} from './src/es';
 export {
   esTestConfig,
   createTestEsCluster,
   createEsClientForTesting,
   createEsClientForFtrConfig,
   createRemoteEsClientForFtrConfig,
-} from './es';
+} from './src/es';
 
 export {
   kbnTestConfig,
@@ -46,27 +46,27 @@ export {
   kibanaTestUser,
   adminTestUser,
   systemIndicesSuperuser,
-} from './kbn';
+} from './src/kbn';
 
-export { readConfigFile } from './functional_test_runner/lib/config/read_config_file';
+export { readConfigFile } from './src/functional_test_runner/lib/config/read_config_file';
 
-export { runFtrCli } from './functional_test_runner/cli';
+export { runFtrCli } from './src/functional_test_runner/cli';
 
 // @internal
-export { setupJUnitReportGeneration, escapeCdata } from './mocha';
+export { setupJUnitReportGeneration, escapeCdata } from './src/mocha';
 
-export { runFailedTestsReporterCli } from './failed_tests_reporter';
+export { runFailedTestsReporterCli } from './src/failed_tests_reporter';
 
-export { CI_PARALLEL_PROCESS_PREFIX } from './ci_parallel_process_prefix';
+export { CI_PARALLEL_PROCESS_PREFIX } from './src/ci_parallel_process_prefix';
 
-export * from './functional_test_runner';
+export * from './src/functional_test_runner';
 
-export { getUrl } from './jest/get_url';
+export { getUrl } from './src/jest/get_url';
 
-export { runCheckJestConfigsCli } from './jest/run_check_jest_configs_cli';
+export { runCheckJestConfigsCli } from './src/jest/run_check_jest_configs_cli';
 
-export { runJest } from './jest/run';
+export { runJest } from './src/jest/run';
 
-export * from './kbn_archiver_cli';
+export * from './src/kbn_archiver_cli';
 
-export * from './kbn_client';
+export * from './src/kbn_client';
