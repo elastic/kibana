bf9fc3147b4af3d26adac5e38e4d9b9eca624746
liza-mae <liza.dayoub@elastic.co>
2022-08-11 11:34:41 -0600

    Fix test to run against cloud

--- test/functional/apps/management/_exclude_index_pattern.ts
+++ test/functional/apps/management/_exclude_index_pattern.ts
@@ -12,27 +12,43 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const PageObjects = getPageObjects(['settings']);
   const es = getService('es');
+  const security = getService('security');
 
   describe('creating and deleting default index', function describeIndexTests() {
-    it('data view creation with exclusion', async () => {
+    before(async function () {
+      await security.testUser.setRoles(['kibana_admin', 'index_a', 'index_b']);
+      await PageObjects.settings.navigateTo();
       await es.transport.request({
-        path: '/index-a/_doc',
+        path: '/index_a/_doc',
         method: 'POST',
         body: { user: 'matt' },
       });
 
       await es.transport.request({
-        path: '/index-b/_doc',
+        path: '/index_b/_doc',
         method: 'POST',
         body: { title: 'hello' },
       });
+      await PageObjects.settings.createIndexPattern('index_*,-index_b');
+    });
 
-      await PageObjects.settings.createIndexPattern('index-*,-index-b');
-
+    it('data view creation with exclusion', async () => {
       const fieldCount = await PageObjects.settings.getFieldsTabCount();
-
       // five metafields plus keyword and text version of 'user' field
       expect(parseInt(fieldCount, 10)).to.be(6);
+    });
+
+    after(async () => {
+      await es.transport.request({
+        path: '/index_a',
+        method: 'DELETE',
+      });
+      await es.transport.request({
+        path: '/index_b',
+        method: 'DELETE',
+      });
+      await PageObjects.settings.removeIndexPattern();
+      await security.testUser.restoreDefaults();
     });
   });
 }
