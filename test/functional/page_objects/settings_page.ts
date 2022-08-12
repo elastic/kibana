233000503b11e00e78bd5886cb81a660d0de1379
John Dorlus <jsdorlus@elastic.co>
2022-08-12 19:47:50 -0400

    Optimized hybrid index pattern test (#138760)
    * Updated test to use uiSettings.
    
    * Optimized tests and removed some code.
    
    * Fixed merge resolution.

--- test/functional/page_objects/settings_page.ts
+++ test/functional/page_objects/settings_page.ts
@@ -453,12 +453,6 @@ export class SettingsPageObject extends FtrService {
     await this.retry.try(async () => {
       await this.header.waitUntilLoadingHasFinished();
       await this.clickKibanaIndexPatterns();
-      const exists = await this.hasIndexPattern(indexPatternName);
-
-      if (exists) {
-        await this.clickIndexPatternByName(indexPatternName);
-        return;
-      }
 
       await this.header.waitUntilLoadingHasFinished();
       const flyOut = await this.testSubjects.exists('createAnyway');
@@ -500,8 +494,10 @@ export class SettingsPageObject extends FtrService {
 
     if (!isStandardIndexPattern) {
       const badges = await this.find.allByCssSelector('.euiBadge__text');
-      const text = await badges[0].getVisibleText();
-      expect(text).to.equal('Rollup');
+      const rollupBadge = badges.filter(async (badge) => {
+        return (await badge.getVisibleText()) === 'Rollup';
+      });
+      expect(rollupBadge.length).to.equal(1);
     }
 
     return await this.getIndexPatternIdFromUrl();
