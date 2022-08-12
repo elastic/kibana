5125f252b97c5f797edcc9c77b93c3e3088ba466
Aleh Zasypkin <aleh.zasypkin@elastic.co>
2022-08-12 21:56:13 +0200

    Sync `UserProfile` interface in `kbn/user-profile-components` package with the one in Security plugin. (#138704)

--- packages/kbn-user-profile-components/src/user_profile.ts
+++ packages/kbn-user-profile-components/src/user_profile.ts
@@ -27,6 +27,11 @@ export interface UserProfile<D extends UserProfileData = UserProfileData> {
   uid: string;
 
   /**
+   * Indicates whether user profile is enabled or not.
+   */
+  enabled: boolean;
+
+  /**
    * Information about the user that owns profile.
    */
   user: UserProfileUserInfo;
