f1b9cf541e0f4d69c99a0854a88b2673da854eeb
Faisal Kanout <faisal.kanout@elastic.co>
2022-08-12 15:16:34 +0200

    [ResponseOps][ActionableObservability] - Fix flaky test in rule details page  (#138492)
    * Add waitFor to get ruleTypeIndex from the hook
    
    * Fix typo
    
    * Remove the skip from the test suite
    
    * Add loaders for useLoadRuleTypes hook

--- x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_definition.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_definition.tsx
@@ -13,6 +13,7 @@ import {
   EuiFlexItem,
   EuiPanel,
   EuiTitle,
+  EuiLoadingSpinner,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { formatDuration } from '@kbn/alerting-plugin/common';
@@ -38,7 +39,7 @@ export const RuleDefinition: React.FunctionComponent<RuleDefinitionProps> = ({
 
   const [editFlyoutVisible, setEditFlyoutVisible] = useState<boolean>(false);
   const [ruleType, setRuleType] = useState<RuleType>();
-  const { ruleTypes, ruleTypeIndex } = useLoadRuleTypes({
+  const { ruleTypes, ruleTypeIndex, ruleTypesIsLoading } = useLoadRuleTypes({
     filteredRuleTypes,
   });
 
@@ -93,14 +94,20 @@ export const RuleDefinition: React.FunctionComponent<RuleDefinitionProps> = ({
               })}
             </EuiFlexItem>
           </EuiTitle>
-          {hasEditButton && (
+          {ruleTypesIsLoading ? (
             <EuiFlexItem grow={false}>
-              <EuiButtonEmpty
-                data-test-subj="ruleDetailsEditButton"
-                iconType={'pencil'}
-                onClick={() => setEditFlyoutVisible(true)}
-              />
+              <EuiLoadingSpinner data-test-subj="ruleDetailsEditButtonLoadingSpinner" />
             </EuiFlexItem>
+          ) : (
+            hasEditButton && (
+              <EuiFlexItem grow={false}>
+                <EuiButtonEmpty
+                  data-test-subj="ruleDetailsEditButton"
+                  iconType={'pencil'}
+                  onClick={() => setEditFlyoutVisible(true)}
+                />
+              </EuiFlexItem>
+            )
           )}
         </EuiFlexGroup>
 
@@ -114,10 +121,16 @@ export const RuleDefinition: React.FunctionComponent<RuleDefinitionProps> = ({
                   defaultMessage: 'Rule type',
                 })}
               </ItemTitleRuleSummary>
-              <ItemValueRuleSummary
-                data-test-subj="ruleSummaryRuleType"
-                itemValue={ruleTypeIndex.get(rule.ruleTypeId)?.name || rule.ruleTypeId}
-              />
+              {ruleTypesIsLoading ? (
+                <EuiFlexItem>
+                  <EuiLoadingSpinner data-test-subj="ruleSummaryRuleTypeLoadingSpinner" />
+                </EuiFlexItem>
+              ) : (
+                <ItemValueRuleSummary
+                  data-test-subj="ruleSummaryRuleType"
+                  itemValue={ruleTypeIndex.get(rule.ruleTypeId)?.name || rule.ruleTypeId}
+                />
+              )}
             </EuiFlexGroup>
 
             <EuiSpacer size="m" />
