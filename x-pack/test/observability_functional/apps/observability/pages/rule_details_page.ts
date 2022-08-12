d4019e3fa2730119cd078bd886971d11ba430ceb
Faisal Kanout <faisal.kanout@elastic.co>
2022-08-12 15:16:34 +0200

    [ResponseOps][ActionableObservability] - Fix flaky test in rule details page  (#138492)
    * Add waitFor to get ruleTypeIndex from the hook
    
    * Fix typo
    
    * Remove the skip from the test suite
    
    * Add loaders for useLoadRuleTypes hook

--- x-pack/test/observability_functional/apps/observability/pages/rule_details_page.ts
+++ x-pack/test/observability_functional/apps/observability/pages/rule_details_page.ts
@@ -35,8 +35,7 @@ export default ({ getService }: FtrProviderContext) => {
     return true;
   }
 
-  // FLAKY: https://github.com/elastic/kibana/issues/136000
-  describe.skip('Observability Rule Details page', function () {
+  describe('Observability Rule Details page', function () {
     this.tags('includeFirefox');
 
     let uptimeRuleId: string;
@@ -133,6 +132,10 @@ export default ({ getService }: FtrProviderContext) => {
       });
 
       it('maps correctly the rule type with the human readable rule type', async () => {
+        await retry.waitFor(
+          'ruleTypeIndex to be loaded from hook',
+          async () => await testSubjects.exists('ruleSummaryRuleType')
+        );
         const ruleType = await testSubjects.getVisibleText('ruleSummaryRuleType');
         expect(ruleType).to.be('Log threshold');
       });
