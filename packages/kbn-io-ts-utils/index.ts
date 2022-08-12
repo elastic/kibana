79a9e2abaf9e57a128258af311625c6e2274e1ff
Tiago Costa <tiago.costa@elastic.co>
2022-08-12 03:41:32 +0100

    chore(NA): remove src folder from kbn-io-ts-utils* pkgs

--- packages/kbn-io-ts-utils/index.ts
+++ packages/kbn-io-ts-utils/index.ts
@@ -6,14 +6,14 @@
  * Side Public License, v 1.
  */
 
-export type { NonEmptyStringBrand } from './non_empty_string_rt';
+export type { NonEmptyStringBrand } from './src/non_empty_string_rt';
 
-export { deepExactRt } from './deep_exact_rt';
-export { jsonRt } from './json_rt';
-export { mergeRt } from './merge_rt';
-export { strictKeysRt } from './strict_keys_rt';
-export { isoToEpochRt } from './iso_to_epoch_rt';
-export { toNumberRt } from './to_number_rt';
-export { toBooleanRt } from './to_boolean_rt';
-export { toJsonSchema } from './to_json_schema';
-export { nonEmptyStringRt } from './non_empty_string_rt';
+export { deepExactRt } from './src/deep_exact_rt';
+export { jsonRt } from './src/json_rt';
+export { mergeRt } from './src/merge_rt';
+export { strictKeysRt } from './src/strict_keys_rt';
+export { isoToEpochRt } from './src/iso_to_epoch_rt';
+export { toNumberRt } from './src/to_number_rt';
+export { toBooleanRt } from './src/to_boolean_rt';
+export { toJsonSchema } from './src/to_json_schema';
+export { nonEmptyStringRt } from './src/non_empty_string_rt';
