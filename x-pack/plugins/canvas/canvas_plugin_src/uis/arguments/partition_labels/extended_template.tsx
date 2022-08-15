145150c14bc6d805a733e762b242e969190a6292
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-08-15 15:03:55 +0300

    [Canvas] PieVis integration to Canvas. (#124325)
    * Added the sidebar of the pieVis.
    
    * Added the element of the pieVis.
    
    * Removed pieVis from exposed elements.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/canvas/canvas_plugin_src/uis/arguments/partition_labels/extended_template.tsx
@@ -0,0 +1,154 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { ChangeEvent, MouseEvent, FunctionComponent, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  EuiFormRow,
+  EuiRange,
+  EuiSelect,
+  EuiSelectOption,
+  EuiSpacer,
+  EuiSwitch,
+  EuiSwitchEvent,
+  EuiText,
+} from '@elastic/eui';
+import { ExpressionAstExpression } from '@kbn/expressions-plugin/common';
+import { set } from 'lodash';
+import { defaultExpression } from './default_expression';
+import { Fields } from './types';
+import { getFieldPath, getFieldValue } from './utils';
+import { ArgumentStrings } from '../../../../i18n';
+
+const { PartitionLabels: strings } = ArgumentStrings;
+
+export interface Props {
+  onValueChange: (argValue: ExpressionAstExpression) => void;
+  argValue: null | ExpressionAstExpression;
+}
+
+const SHOW_FIELD = 'show';
+const POSITION_FIELD = 'position';
+const VALUES_FIELD = 'values';
+const VALUES_FORMAT_FIELD = 'valuesFormat';
+const PERCENT_DECIMALS_FIELD = 'percentDecimals';
+
+export const ExtendedTemplate: FunctionComponent<Props> = ({ onValueChange, argValue }) => {
+  const showLabels = getFieldValue(argValue, SHOW_FIELD);
+  const showValues = getFieldValue(argValue, VALUES_FIELD);
+  const valueFormat = getFieldValue(argValue, VALUES_FORMAT_FIELD);
+  const percentDecimals = getFieldValue(argValue, PERCENT_DECIMALS_FIELD);
+
+  const positions: EuiSelectOption[] = [
+    { text: strings.getPositionDefaultLabel(), value: 'default' },
+    { text: strings.getPositionInsideLabel(), value: 'inside' },
+  ];
+
+  const valuesFormats: EuiSelectOption[] = [
+    { text: strings.getValuesFormatValueLabel(), value: 'value' },
+    { text: strings.getValuesFormatPercentLabel(), value: 'percent' },
+  ];
+
+  useEffect(() => {
+    if (!argValue) {
+      onValueChange(defaultExpression());
+    }
+  }, [argValue, onValueChange]);
+
+  const onChangeField = useCallback(
+    (field: Fields, value: unknown) => {
+      const path = getFieldPath(field);
+      const oldArgValue = argValue ?? defaultExpression();
+      const newArgValue = set(oldArgValue, path, value);
+
+      onValueChange(newArgValue);
+    },
+    [argValue, onValueChange]
+  );
+
+  const onToggleFieldChange = useCallback(
+    (field: Fields) => (event: EuiSwitchEvent) => {
+      onChangeField(field, event.target.checked);
+    },
+    [onChangeField]
+  );
+
+  const onCommonFieldChange = useCallback(
+    (field: Fields) =>
+      (
+        event: ChangeEvent<HTMLInputElement | HTMLSelectElement> | MouseEvent<HTMLButtonElement>
+      ) => {
+        onChangeField(field, event.currentTarget.value);
+      },
+    [onChangeField]
+  );
+
+  if (!showLabels) {
+    return (
+      <EuiText color="subdued" size="xs">
+        <p>{strings.getSwitchedOffShowLabelsLabel()}</p>
+      </EuiText>
+    );
+  }
+
+  return (
+    <>
+      <EuiFormRow label={strings.getPositionLabel()} display="columnCompressed">
+        <EuiSelect
+          compressed
+          value={getFieldValue(argValue, POSITION_FIELD)}
+          options={positions}
+          onChange={onCommonFieldChange(POSITION_FIELD)}
+        />
+      </EuiFormRow>
+      <EuiSpacer size="s" />
+      <EuiFormRow label={strings.getValuesLabel()} display="columnCompressedSwitch">
+        <EuiSwitch
+          compressed
+          checked={showValues}
+          onChange={onToggleFieldChange(VALUES_FIELD)}
+          label={strings.getValuesToggle()}
+        />
+      </EuiFormRow>
+      {showValues && (
+        <EuiFormRow label={strings.getValuesFormatLabel()} display="columnCompressed">
+          <EuiSelect
+            compressed
+            value={valueFormat}
+            options={valuesFormats}
+            onChange={onCommonFieldChange(VALUES_FORMAT_FIELD)}
+          />
+        </EuiFormRow>
+      )}
+      {showValues && valueFormat === 'percent' && (
+        <EuiFormRow label={strings.getPercentDecimalsLabel()} display="columnCompressed">
+          <EuiRange
+            compressed
+            min={0}
+            max={10}
+            step={1}
+            showLabels
+            showInput
+            value={percentDecimals}
+            onChange={(e, isValid) => {
+              if (isValid) {
+                onCommonFieldChange(PERCENT_DECIMALS_FIELD)(e);
+              }
+            }}
+          />
+        </EuiFormRow>
+      )}
+    </>
+  );
+};
+
+ExtendedTemplate.propTypes = {
+  onValueChange: PropTypes.func.isRequired,
+  argValue: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]).isRequired,
+};
+
+ExtendedTemplate.displayName = 'PartitionLabelsExtendedArg';
