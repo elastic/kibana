7fe4448a772e5bea66893c5372f90e0034376c99
Quynh Nguyen <quynh.nguyen@elastic.co>
2022-08-15 13:49:48 -0500

    Remove toast check for deletion

--- x-pack/test/functional/apps/transform/deleting.ts
+++ x-pack/test/functional/apps/transform/deleting.ts
@@ -127,7 +127,6 @@ export default function ({ getService }: FtrProviderContext) {
 
           await transform.testExecution.logTestStep('should delete the transform');
           await transform.table.confirmDeleteTransform();
-          await transform.wizard.assertErrorToastsNotExist();
           await transform.table.assertTransformRowNotExists(testData.originalConfig.id);
         });
       });
