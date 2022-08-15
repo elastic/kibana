5e7581436995a16a5095d9f76c15329d2a52e354
nreese <reese.nathan@elastic.co>
2022-08-15 08:41:38 -0600

    clean up wrap logic

--- src/plugins/controls/public/control_types/time_slider_2/components/time_slider.tsx
+++ src/plugins/controls/public/control_types/time_slider_2/components/time_slider.tsx
@@ -102,7 +102,7 @@ export const TimeSlider: FC<Props> = (props) => {
   const to = value ? value[TO_INDEX] : timeRangeMax;
   
   return (
-    <EuiFlexGroup>
+    <EuiFlexGroup gutterSize="none">
       <EuiFlexItem grow={false}>
         <EuiButtonIcon
           onClick={() => {
