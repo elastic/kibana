70d63f8eee6f85bbf4d48a99a9dcd0997f7a6eb6
James Rucker <james.rucker@elastic.co>
2022-08-15 13:48:30 -0700

    Add tests for access based navigation

--- x-pack/plugins/enterprise_search/public/applications/shared/layout/nav.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/shared/layout/nav.test.tsx
@@ -9,11 +9,18 @@ jest.mock('./nav_link_helpers', () => ({
   generateNavLink: jest.fn(({ to, items }) => ({ href: to, items })),
 }));
 
+import { ProductAccess } from '../../../../common/types';
+
 import { useEnterpriseSearchNav } from './nav';
 
 describe('useEnterpriseSearchContentNav', () => {
   it('returns an array of top-level Enterprise Search nav items', () => {
-    expect(useEnterpriseSearchNav()).toEqual([
+    const fullProductAccess: ProductAccess = {
+      hasAppSearchAccess: true,
+      hasWorkplaceSearchAccess: true,
+    };
+
+    expect(useEnterpriseSearchNav(fullProductAccess)).toEqual([
       {
         href: '/app/enterprise_search/overview',
         id: 'es_overview',
@@ -52,5 +59,72 @@ describe('useEnterpriseSearchContentNav', () => {
         name: 'Search',
       },
     ]);
+  });
+
+  it('excludes legacy products when the user has no access to them', () => {
+    const noProductAccess: ProductAccess = {
+      hasAppSearchAccess: false,
+      hasWorkplaceSearchAccess: false,
+    };
+
+    expect(useEnterpriseSearchNav(noProductAccess)[2]).toEqual({
+      id: 'search',
+      items: [
+        {
+          href: '/app/enterprise_search/elasticsearch',
+          id: 'elasticsearch',
+          name: 'Elasticsearch',
+        },
+      ],
+      name: 'Search',
+    });
+  });
+
+  it('excludes App Search when the user has no access to it', () => {
+    const workplaceSearchProductAccess: ProductAccess = {
+      hasAppSearchAccess: false,
+      hasWorkplaceSearchAccess: true,
+    };
+
+    expect(useEnterpriseSearchNav(workplaceSearchProductAccess)[2]).toEqual({
+      id: 'search',
+      items: [
+        {
+          href: '/app/enterprise_search/elasticsearch',
+          id: 'elasticsearch',
+          name: 'Elasticsearch',
+        },
+        {
+          href: '/app/enterprise_search/workplace_search',
+          id: 'workplace_search',
+          name: 'Workplace Search',
+        },
+      ],
+      name: 'Search',
+    });
+  });
+
+  it('excludes Workplace Search when the user has no access to it', () => {
+    const appSearchProductAccess: ProductAccess = {
+      hasAppSearchAccess: true,
+      hasWorkplaceSearchAccess: false,
+    };
+
+    expect(useEnterpriseSearchNav(appSearchProductAccess)[2]).toEqual({
+      id: 'search',
+      items: [
+        {
+          href: '/app/enterprise_search/elasticsearch',
+          id: 'elasticsearch',
+          name: 'Elasticsearch',
+        },
+        {
+          href: '/app/enterprise_search/app_search',
+          id: 'app_search',
+          name: 'App Search',
+        },
+      ],
+      name: 'Search',
+    });
   });
 });
