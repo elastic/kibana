080ebb1f028df34d7306affabcca75ce71fd52d6
Joe Reuter <johannes.reuter@elastic.co>
2022-08-16 11:33:35 +0200

    migrate embedded ad hoc data views

--- x-pack/plugins/lens/server/migrations/saved_object_migrations.ts
+++ x-pack/plugins/lens/server/migrations/saved_object_migrations.ts
@@ -16,6 +16,7 @@ import {
 import type { Query, Filter } from '@kbn/es-query';
 import { mergeSavedObjectMigrationMaps } from '@kbn/core/server';
 import { MigrateFunctionsObject } from '@kbn/kibana-utils-plugin/common';
+import { DataViewSpec } from '@kbn/data-views-plugin/common';
 import { PersistableFilter } from '../../common';
 import {
   LensDocShapePost712,
@@ -50,6 +51,7 @@ import {
   commonFixValueLabelsInXY,
   commonLockOldMetricVisSettings,
   commonPreserveOldLegendSizeDefault,
+  getLensDataViewMigrations,
 } from './common_migrations';
 
 interface LensDocShapePre710<VisualizationState = unknown> {
@@ -101,6 +103,7 @@ export interface LensDocShape<VisualizationState = unknown> {
     visualization: VisualizationState;
     query: Query;
     filters: PersistableFilter[];
+    adHocDataViews?: Record<string, DataViewSpec>;
   };
 }
 
@@ -534,12 +537,16 @@ const lensMigrations: SavedObjectMigrationMap = {
 
 export const getAllMigrations = (
   filterMigrations: MigrateFunctionsObject,
+  dataViewMigrations: MigrateFunctionsObject,
   customVisualizationMigrations: CustomVisualizationMigrations
 ): SavedObjectMigrationMap =>
   mergeSavedObjectMigrationMaps(
     mergeSavedObjectMigrationMaps(
-      lensMigrations,
-      getLensFilterMigrations(filterMigrations) as unknown as SavedObjectMigrationMap
+      mergeSavedObjectMigrationMaps(
+        lensMigrations,
+        getLensFilterMigrations(filterMigrations) as unknown as SavedObjectMigrationMap
+      ),
+      getLensCustomVisualizationMigrations(customVisualizationMigrations)
     ),
-    getLensCustomVisualizationMigrations(customVisualizationMigrations)
+    getLensDataViewMigrations(dataViewMigrations) as unknown as SavedObjectMigrationMap
   );
