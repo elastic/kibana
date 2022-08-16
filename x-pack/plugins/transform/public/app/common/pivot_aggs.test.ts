7a271fc76b7dfce01cf70de0cbf78506ea662646
Dima Arnautov <dmitrii.arnautov@elastic.co>
2022-08-16 15:10:56 +0200

    [ML] Transform: Fix `percentiles` and `terms` sub-aggs (#138821)
    * update percentiles agg
    
    * types
    
    * optional isValid
    
    * terms agg
    
    * update getPivotDropdownOptions tests
    
    * update getAggConfigFromEsAgg percentiles tests
    
    * add terms tests
    
    * rename isPivotAggsConfigWithUiBase type guard
    
    * remove non-null assertion

--- x-pack/plugins/transform/public/app/common/pivot_aggs.test.ts
+++ x-pack/plugins/transform/public/app/common/pivot_aggs.test.ts
@@ -68,7 +68,7 @@ describe('getAggConfigFromEsAgg', () => {
     });
   });
 
-  test('should resolve percentiles agg in sub-aggregations', () => {
+  test('should resolve percentiles and terms agg in sub-aggregations', () => {
     const esConfig = {
       filter: {
         exists: {
@@ -82,6 +82,12 @@ describe('getAggConfigFromEsAgg', () => {
             percents: [1, 5, 25, 50, 75, 95, 99],
           },
         },
+        check_terms: {
+          terms: {
+            field: 'products.base_price',
+            size: 3,
+          },
+        },
       },
     };
 
@@ -93,6 +99,20 @@ describe('getAggConfigFromEsAgg', () => {
       dropDownName: 'products.base_price.percentiles',
       field: 'products.base_price',
       parentAgg: result,
+      aggConfig: {
+        percents: '1,5,25,50,75,95,99',
+      },
+    });
+
+    expect(result.subAggs!.check_terms).toMatchObject({
+      agg: 'terms',
+      aggName: 'check_terms',
+      dropDownName: 'check_terms',
+      field: 'products.base_price',
+      parentAgg: result,
+      aggConfig: {
+        size: 3,
+      },
     });
   });
 
