af937d7b5ca73361f6d5563a8851f60bc234f210
James Rucker <james.rucker@elastic.co>
2022-08-16 09:05:29 -0700

    [Enterprise Search] Vary Enterprise Search navigation options based on product access (#138775)
    * Added an EnterpriseSearchProvider to propagate access data
    
    Co-authored-by: Brian McGue <mcgue.brian@gmail.com>
    Co-authored-by: Rodney Norris <rodney.norris@elastic.co>
    
    * Move productAccess into KibanaLogic
    
    * Fix PageTemplate tests
    
    * Add tests for access based navigation
    
    * Push useValues for productAccess down into useEnterpriseSearchNav
    
    Co-authored-by: Brian McGue <mcgue.brian@gmail.com>
    Co-authored-by: Rodney Norris <rodney.norris@elastic.co>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/enterprise_search/public/applications/shared/layout/nav.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/shared/layout/nav.test.tsx
@@ -9,10 +9,24 @@ jest.mock('./nav_link_helpers', () => ({
   generateNavLink: jest.fn(({ to, items }) => ({ href: to, items })),
 }));
 
+import { setMockValues } from '../../__mocks__/kea_logic';
+
+import { ProductAccess } from '../../../../common/types';
+
 import { useEnterpriseSearchNav } from './nav';
 
 describe('useEnterpriseSearchContentNav', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('returns an array of top-level Enterprise Search nav items', () => {
+    const fullProductAccess: ProductAccess = {
+      hasAppSearchAccess: true,
+      hasWorkplaceSearchAccess: true,
+    };
+    setMockValues({ productAccess: fullProductAccess });
+
     expect(useEnterpriseSearchNav()).toEqual([
       {
         href: '/app/enterprise_search/overview',
@@ -52,5 +66,78 @@ describe('useEnterpriseSearchContentNav', () => {
         name: 'Search',
       },
     ]);
+  });
+
+  it('excludes legacy products when the user has no access to them', () => {
+    const noProductAccess: ProductAccess = {
+      hasAppSearchAccess: false,
+      hasWorkplaceSearchAccess: false,
+    };
+
+    setMockValues({ productAccess: noProductAccess });
+
+    expect(useEnterpriseSearchNav()[2]).toEqual({
+      id: 'search',
+      items: [
+        {
+          href: '/app/enterprise_search/elasticsearch',
+          id: 'elasticsearch',
+          name: 'Elasticsearch',
+        },
+      ],
+      name: 'Search',
+    });
+  });
+
+  it('excludes App Search when the user has no access to it', () => {
+    const workplaceSearchProductAccess: ProductAccess = {
+      hasAppSearchAccess: false,
+      hasWorkplaceSearchAccess: true,
+    };
+
+    setMockValues({ productAccess: workplaceSearchProductAccess });
+
+    expect(useEnterpriseSearchNav()[2]).toEqual({
+      id: 'search',
+      items: [
+        {
+          href: '/app/enterprise_search/elasticsearch',
+          id: 'elasticsearch',
+          name: 'Elasticsearch',
+        },
+        {
+          href: '/app/enterprise_search/workplace_search',
+          id: 'workplace_search',
+          name: 'Workplace Search',
+        },
+      ],
+      name: 'Search',
+    });
+  });
+
+  it('excludes Workplace Search when the user has no access to it', () => {
+    const appSearchProductAccess: ProductAccess = {
+      hasAppSearchAccess: true,
+      hasWorkplaceSearchAccess: false,
+    };
+
+    setMockValues({ productAccess: appSearchProductAccess });
+
+    expect(useEnterpriseSearchNav()[2]).toEqual({
+      id: 'search',
+      items: [
+        {
+          href: '/app/enterprise_search/elasticsearch',
+          id: 'elasticsearch',
+          name: 'Elasticsearch',
+        },
+        {
+          href: '/app/enterprise_search/app_search',
+          id: 'app_search',
+          name: 'App Search',
+        },
+      ],
+      name: 'Search',
+    });
   });
 });
