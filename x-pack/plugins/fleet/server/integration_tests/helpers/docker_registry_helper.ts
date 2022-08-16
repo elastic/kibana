fab8932497d65b647150115ed121503bede6dcf0
Dominique Clarke <dominique.clarke@elastic.co>
2022-08-16 10:53:56 -0600

    [Fleet] tests - update package registry image (#138915)
    * fleet tests - update package registry image
    
    * Apply suggestions from code review

--- x-pack/plugins/fleet/server/integration_tests/helpers/docker_registry_helper.ts
+++ x-pack/plugins/fleet/server/integration_tests/helpers/docker_registry_helper.ts
@@ -18,7 +18,7 @@ import pRetry from 'p-retry';
 const BEFORE_SETUP_TIMEOUT = 30 * 60 * 1000; // 30 minutes;
 
 const DOCKER_START_TIMEOUT = 5 * 60 * 1000; // 5 minutes
-const DOCKER_IMAGE = `docker.elastic.co/package-registry/distribution:d32582b3bbeef5283e4450a9403f26c2a5e415f4`;
+const DOCKER_IMAGE = `docker.elastic.co/package-registry/distribution:c9f9a83da2c00118d5451cc69c680ade74494d4e`;
 
 function firstWithTimeout(source$: Rx.Observable<any>, errorMsg: string, ms = 30 * 1000) {
   return Rx.race(
