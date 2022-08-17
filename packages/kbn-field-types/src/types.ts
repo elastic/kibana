3f062b7bd148eb1d96be5e700b513ffb9dfeead2
Joe Reuter <johannes.reuter@elastic.co>
2022-08-17 15:34:47 +0200

    fix duplicated entries

--- packages/kbn-field-types/src/types.ts
+++ packages/kbn-field-types/src/types.ts
@@ -47,8 +47,6 @@ export enum ES_FIELD_TYPES {
   UNSIGNED_LONG = 'unsigned_long',
   AGGREGATE_METRIC_DOUBLE = 'aggregate_metric_double',
 
-  AGGREGATE_METRIC_DOUBLE = 'aggregate_metric_double',
-
   FLOAT_RANGE = 'float_range',
   DOUBLE_RANGE = 'double_range',
   INTEGER_RANGE = 'integer_range',
