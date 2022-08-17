b279eeb3d6cdd41aa84046deee96fa20fe32a63e
Timothy Sullivan <tsullivan@elastic.co>
2022-08-17 12:09:57 -0700

    [DataViews] fix behavior for loading a missing data view

--- src/plugins/data_view_management/public/components/edit_index_pattern/edit_index_pattern_container.tsx
+++ src/plugins/data_view_management/public/components/edit_index_pattern/edit_index_pattern_container.tsx
@@ -6,31 +6,43 @@
  * Side Public License, v 1.
  */
 
-import React, { useEffect, useState } from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { DataView } from '@kbn/data-views-plugin/public';
+import { i18n } from '@kbn/i18n';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
+import React, { useEffect, useState } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { EditIndexPattern } from '.';
 import { IndexPatternManagmentContext } from '../../types';
 import { getEditBreadcrumbs } from '../breadcrumbs';
 
-import { EditIndexPattern } from '.';
-
 const EditIndexPatternCont: React.FC<RouteComponentProps<{ id: string }>> = ({ ...props }) => {
-  const { dataViews, setBreadcrumbs } = useKibana<IndexPatternManagmentContext>().services;
+  const { dataViews, setBreadcrumbs, notifications } =
+    useKibana<IndexPatternManagmentContext>().services;
+  const [error, setError] = useState<Error | undefined>();
   const [indexPattern, setIndexPattern] = useState<DataView>();
 
   useEffect(() => {
-    dataViews.get(props.match.params.id).then((ip: DataView) => {
-      setIndexPattern(ip);
-      setBreadcrumbs(getEditBreadcrumbs(ip));
-    });
-  }, [dataViews, props.match.params.id, setBreadcrumbs]);
+    dataViews
+      .get(props.match.params.id)
+      .then((ip: DataView) => {
+        setIndexPattern(ip);
+        setBreadcrumbs(getEditBreadcrumbs(ip));
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, [dataViews, props.match.params.id, setBreadcrumbs, setError]);
 
   if (indexPattern) {
     return <EditIndexPattern indexPattern={indexPattern} />;
-  } else {
-    return <></>;
+  } else if (error) {
+    const errorTitle = i18n.translate('indexPatternManagement.editIndexPattern.couldNotLoad', {
+      defaultMessage: 'Could not load that data view',
+    });
+    notifications.toasts.addError(error ?? new Error(errorTitle), { title: errorTitle });
+    props.history.push('/');
   }
+  return <></>;
 };
 
 export const EditIndexPatternContainer = withRouter(EditIndexPatternCont);
