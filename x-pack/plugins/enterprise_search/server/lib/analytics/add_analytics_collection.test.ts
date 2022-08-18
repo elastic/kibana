9bcb14b144e3658af5fac3bc6f086d4ca83d2ee8
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-08-18 15:48:21 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/enterprise_search/server/lib/analytics/add_analytics_collection.test.ts
+++ x-pack/plugins/enterprise_search/server/lib/analytics/add_analytics_collection.test.ts
@@ -6,10 +6,12 @@
  */
 
 import { IScopedClusterClient } from '@kbn/core-elasticsearch-server';
+
 import { ANALYTICS_COLLECTIONS_INDEX } from '../..';
-import { fetchAnalyticsCollectionByName } from './fetch_analytics_collection';
+import { ErrorCode } from '../../../common/types/error_codes';
+
 import { addAnalyticsCollection } from './add_analytics_collection';
-import { ErrorCode } from '@kbn/enterprise-search-plugin/common/types/error_codes';
+import { fetchAnalyticsCollectionByName } from './fetch_analytics_collection';
 import { setupAnalyticsCollectionIndex } from './setup_indices';
 
 jest.mock('./fetch_analytics_collection', () => ({ fetchAnalyticsCollectionByName: jest.fn() }));
--- x-pack/plugins/enterprise_search/server/lib/analytics/fetch_analytics_collection.test.ts
+++ x-pack/plugins/enterprise_search/server/lib/analytics/fetch_analytics_collection.test.ts
@@ -6,7 +6,9 @@
  */
 
 import { IScopedClusterClient } from '@kbn/core-elasticsearch-server';
+
 import { ANALYTICS_COLLECTIONS_INDEX } from '../..';
+
 import { fetchAnalyticsCollectionByName } from './fetch_analytics_collection';
 import { setupAnalyticsCollectionIndex } from './setup_indices';
 
