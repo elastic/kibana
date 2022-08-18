5a78b05cb9cccf0f4ab8a17a46a0d847a010a50d
Clint Andrew Hall <clint.hall@elastic.co>
2022-08-17 23:23:35 -0500

    Revise Storybook config package; make mock packages compatible with web

--- packages/shared-ux/storybook/config/preview.ts
+++ packages/shared-ux/storybook/config/preview.ts
@@ -6,6 +6,16 @@
  * Side Public License, v 1.
  */
 
+/* eslint-disable @typescript-eslint/no-namespace,@typescript-eslint/no-empty-interface */
+declare global {
+  namespace NodeJS {
+    interface Global {}
+    interface InspectOptions {}
+    type ConsoleConstructor = console.ConsoleConstructor;
+  }
+}
+/* eslint-enable */
+
 import jest from 'jest-mock';
 /* @ts-expect-error TS doesn't see jest as a property of window, and I don't want to edit our global config. */
 window.jest = jest;
