9b615719f9dab1f56c97b01261602f79ac7e5cc0
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2022-08-18 18:18:40 +0200

    [Security Solution] [Enhancement] Preview Results option should refresh the results and Histogram on each click (#136094) (#139016)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/types.ts
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/types.ts
@@ -19,6 +19,7 @@ import type {
 import type { Filter } from '@kbn/es-query';
 import type { RuleAction } from '@kbn/alerting-plugin/common';
 import type { DataViewListItem } from '@kbn/data-views-plugin/common';
+import type { Unit } from '@kbn/datemath';
 
 import type { RuleAlertAction } from '../../../../../common/detection_engine/types';
 import type { FieldValueQueryBar } from '../../../components/rules/query_bar';
@@ -240,6 +241,11 @@ export interface ActionsStepRuleJson {
   enabled: boolean;
   throttle?: string | null;
   meta?: unknown;
+}
+
+export interface QuickQueryPreviewOptions {
+  timeframe: Unit;
+  timeframeEnd: moment.Moment;
 }
 
 export interface AdvancedPreviewForm {
