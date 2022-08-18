63755488a1cf4ead833650c7ad61de042823154d
Uladzislau Lasitsa <vlad.lasitsa@gmail.com>
2022-08-18 10:59:40 +0300

    [XY] Remove vis type xy renderer (#138735)
    * Remove vis type xy renderer
    
    * Fix check
    
    * Remove unused translations
    
    * Some
    
    * Updated limits
    
    * Update limits
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

--- src/plugins/vis_types/xy/public/services.ts
+++ src/plugins/vis_types/xy/public/services.ts
@@ -6,29 +6,13 @@
  * Side Public License, v 1.
  */
 
-import type { CoreSetup, DocLinksStart } from '@kbn/core/public';
-import type { FieldFormatsStart } from '@kbn/field-formats-plugin/public';
-import type { DataPublicPluginStart } from '@kbn/data-plugin/public';
-import type { ChartsPluginSetup, ChartsPluginStart } from '@kbn/charts-plugin/public';
+import type { CoreSetup } from '@kbn/core/public';
+import type { ChartsPluginSetup } from '@kbn/charts-plugin/public';
 
 import { createGetterSetter } from '@kbn/kibana-utils-plugin/public';
 
 export const [getUISettings, setUISettings] =
   createGetterSetter<CoreSetup['uiSettings']>('xy core.uiSettings');
 
-export const [getDataActions, setDataActions] =
-  createGetterSetter<DataPublicPluginStart['actions']>('xy data.actions');
-
-export const [getFormatService, setFormatService] =
-  createGetterSetter<FieldFormatsStart>('xy fieldFormats');
-
-export const [getThemeService, setThemeService] =
-  createGetterSetter<ChartsPluginSetup['theme']>('xy charts.theme');
-
-export const [getActiveCursor, setActiveCursor] =
-  createGetterSetter<ChartsPluginStart['activeCursor']>('xy charts.activeCursor');
-
 export const [getPalettesService, setPalettesService] =
   createGetterSetter<ChartsPluginSetup['palettes']>('xy charts.palette');
-
-export const [getDocLinks, setDocLinks] = createGetterSetter<DocLinksStart>('DocLinks');
