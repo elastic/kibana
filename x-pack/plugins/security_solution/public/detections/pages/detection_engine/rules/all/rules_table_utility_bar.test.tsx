3556b7d7b2e6a774bbc0fffe21929e35a480d69a
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2022-08-23 13:35:53 +0200

    [Security Solution][Detections] Update rules count indicator in the Rules table to show pagination info (#138902)
    **Fixes:** https://github.com/elastic/kibana/issues/121754
    
    ## Summary
    
    Changes rules count pagination count to format:
    
    **"Showing 1-10 of 596 rules"** -> when page 1, rules per page is 10 and total rules is 596
    **"Showing 6-6 of 6 rules"** -> when page 2, rules per page is 5 and total rules is 6
    See other cases in tests.
    
    **Before changes**
    ![image](https://user-images.githubusercontent.com/5354282/184883106-118f0041-5567-4cf8-bfd5-8383e8146018.png)
    ![image](https://user-images.githubusercontent.com/5354282/184884145-22d30482-cc2d-4796-8f84-d809a8ca6d8a.png)
    
    
    **After changes**
    ![image](https://user-images.githubusercontent.com/5354282/184882149-d6c55dff-39a0-4a72-94a9-217b2e5ca7ac.png)
    ![image](https://user-images.githubusercontent.com/5354282/184886334-a107a2c5-31dc-4a7b-9e70-54c87c1630e0.png)
    
    ## Codebase changes
    
    - Refactors `AllRulesUtilityBar` component to separate usage of the utility bar between the all-rules table use-case and the Exceptions table use-case, by creating a new `ExceptionsTableUtilityBar` component. 
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html) were updated or added to match the most common scenarios
    - [x] This renders correctly on smaller devices using a responsive layout. (You can test this [in your browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/rules_table_utility_bar.test.tsx
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/rules_table_utility_bar.test.tsx
@@ -9,67 +9,80 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { waitFor } from '@testing-library/react';
 
-import { AllRulesUtilityBar } from './utility_bar';
+import { getShowingRulesParams, RulesTableUtilityBar } from './rules_table_utility_bar';
 import { TestProviders } from '../../../../../common/mock';
 
-describe('AllRules', () => {
-  it('renders AllRulesUtilityBar total rules and selected rules', () => {
+jest.mock('./rules_table/rules_table_context');
+
+describe('RulesTableUtilityBar', () => {
+  it('renders RulesTableUtilityBar total rules and selected rules', () => {
     const wrapper = mount(
       <TestProviders>
-        <AllRulesUtilityBar
+        <RulesTableUtilityBar
           canBulkEdit
           onRefresh={jest.fn()}
-          paginationTotal={4}
+          pagination={{
+            page: 1,
+            perPage: 10,
+            total: 21,
+          }}
           numberSelectedItems={1}
           onGetBulkItemsPopoverContent={jest.fn()}
           isAutoRefreshOn={true}
           onRefreshSwitch={jest.fn()}
-          hasBulkActions
+          onToggleSelectAll={jest.fn()}
         />
       </TestProviders>
     );
 
-    expect(wrapper.find('[data-test-subj="showingRules"]').at(0).text()).toEqual('Showing 4 rules');
+    expect(wrapper.find('[data-test-subj="showingRules"]').at(0).text()).toEqual(
+      'Showing 1-10 of 21 rules'
+    );
     expect(wrapper.find('[data-test-subj="selectedRules"]').at(0).text()).toEqual(
       'Selected 1 rule'
     );
   });
 
-  it('does not render total selected and bulk actions when "hasBulkActions" is false', () => {
+  it('renders correct pagination label according to pagination data', () => {
     const wrapper = mount(
       <TestProviders>
-        <AllRulesUtilityBar
+        <RulesTableUtilityBar
           canBulkEdit
           onRefresh={jest.fn()}
-          paginationTotal={4}
+          pagination={{
+            page: 1,
+            perPage: 10,
+            total: 21,
+          }}
           numberSelectedItems={1}
           onGetBulkItemsPopoverContent={jest.fn()}
           isAutoRefreshOn={true}
           onRefreshSwitch={jest.fn()}
-          hasBulkActions={false}
+          onToggleSelectAll={jest.fn()}
         />
       </TestProviders>
     );
-
-    expect(wrapper.find('[data-test-subj="showingRules"]').exists()).toBeFalsy();
-    expect(wrapper.find('[data-test-subj="tableBulkActions"]').exists()).toBeFalsy();
-    expect(wrapper.find('[data-test-subj="showingExceptionLists"]').at(0).text()).toEqual(
-      'Showing 4 lists'
+    expect(wrapper.find('[data-test-subj="showingRules"]').at(0).text()).toEqual(
+      'Showing 1-10 of 21 rules'
     );
   });
 
   it('renders utility actions if user has permissions', () => {
     const wrapper = mount(
       <TestProviders>
-        <AllRulesUtilityBar
+        <RulesTableUtilityBar
           canBulkEdit
           onRefresh={jest.fn()}
-          paginationTotal={4}
+          pagination={{
+            page: 1,
+            perPage: 10,
+            total: 21,
+          }}
           numberSelectedItems={1}
           onGetBulkItemsPopoverContent={jest.fn()}
           isAutoRefreshOn={true}
           onRefreshSwitch={jest.fn()}
-          hasBulkActions
+          onToggleSelectAll={jest.fn()}
         />
       </TestProviders>
     );
@@ -80,15 +93,19 @@ describe('AllRules', () => {
   it('renders no utility actions if user has no permissions', () => {
     const wrapper = mount(
       <TestProviders>
-        <AllRulesUtilityBar
+        <RulesTableUtilityBar
           canBulkEdit={false}
           onRefresh={jest.fn()}
-          paginationTotal={4}
+          pagination={{
+            page: 1,
+            perPage: 10,
+            total: 21,
+          }}
           numberSelectedItems={1}
           onGetBulkItemsPopoverContent={jest.fn()}
           isAutoRefreshOn={true}
           onRefreshSwitch={jest.fn()}
-          hasBulkActions
+          onToggleSelectAll={jest.fn()}
         />
       </TestProviders>
     );
@@ -100,15 +117,19 @@ describe('AllRules', () => {
     const mockRefresh = jest.fn();
     const wrapper = mount(
       <TestProviders>
-        <AllRulesUtilityBar
+        <RulesTableUtilityBar
           canBulkEdit
           onRefresh={mockRefresh}
-          paginationTotal={4}
+          pagination={{
+            page: 1,
+            perPage: 10,
+            total: 21,
+          }}
           numberSelectedItems={1}
           onGetBulkItemsPopoverContent={jest.fn()}
           isAutoRefreshOn={true}
           onRefreshSwitch={jest.fn()}
-          hasBulkActions
+          onToggleSelectAll={jest.fn()}
         />
       </TestProviders>
     );
@@ -122,15 +143,19 @@ describe('AllRules', () => {
     const mockSwitch = jest.fn();
     const wrapper = mount(
       <TestProviders>
-        <AllRulesUtilityBar
+        <RulesTableUtilityBar
           canBulkEdit
           onRefresh={jest.fn()}
-          paginationTotal={4}
+          pagination={{
+            page: 1,
+            perPage: 10,
+            total: 21,
+          }}
           numberSelectedItems={0}
           onGetBulkItemsPopoverContent={jest.fn()}
           isAutoRefreshOn={true}
           onRefreshSwitch={mockSwitch}
-          hasBulkActions
+          onToggleSelectAll={jest.fn()}
         />
       </TestProviders>
     );
@@ -146,15 +171,19 @@ describe('AllRules', () => {
     const mockSwitch = jest.fn();
     const wrapper = mount(
       <TestProviders>
-        <AllRulesUtilityBar
+        <RulesTableUtilityBar
           canBulkEdit
           onRefresh={jest.fn()}
-          paginationTotal={4}
+          pagination={{
+            page: 1,
+            perPage: 10,
+            total: 21,
+          }}
           numberSelectedItems={1}
           onGetBulkItemsPopoverContent={jest.fn()}
           isAutoRefreshOn={true}
           onRefreshSwitch={mockSwitch}
-          hasBulkActions
+          onToggleSelectAll={jest.fn()}
         />
       </TestProviders>
     );
@@ -163,6 +192,74 @@ describe('AllRules', () => {
       wrapper.find('[data-test-subj="refreshSettings"] button').first().simulate('click');
       wrapper.find('[data-test-subj="refreshSettingsSwitch"] button').first().simulate('click');
       expect(mockSwitch).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('getShowingRulesParams creates correct label when', () => {
+    it('there are 0 rules to display', () => {
+      const pagination = {
+        page: 1,
+        perPage: 10,
+        total: 0,
+      };
+      const [firstInPage, lastInPage] = getShowingRulesParams(pagination);
+      expect(firstInPage).toEqual(0);
+      expect(lastInPage).toEqual(0);
+    });
+
+    it('there is 1 rule to display', () => {
+      const pagination = {
+        page: 1,
+        perPage: 10,
+        total: 1,
+      };
+      const [firstInPage, lastInPage] = getShowingRulesParams(pagination);
+      expect(firstInPage).toEqual(1);
+      expect(lastInPage).toEqual(1);
+    });
+
+    it('the table displays the first page, and rules per page is less than total rules', () => {
+      const pagination = {
+        page: 1,
+        perPage: 10,
+        total: 21,
+      };
+      const [firstInPage, lastInPage] = getShowingRulesParams(pagination);
+      expect(firstInPage).toEqual(1);
+      expect(lastInPage).toEqual(10);
+    });
+
+    it('the table displays the first page, and rules per page is greater than total rules', () => {
+      const pagination = {
+        page: 1,
+        perPage: 10,
+        total: 8,
+      };
+      const [firstInPage, lastInPage] = getShowingRulesParams(pagination);
+      expect(firstInPage).toEqual(1);
+      expect(lastInPage).toEqual(8);
+    });
+
+    it('the table displays the second page, and rules per page is less than total rules', () => {
+      const pagination = {
+        page: 2,
+        perPage: 10,
+        total: 31,
+      };
+      const [firstInPage, lastInPage] = getShowingRulesParams(pagination);
+      expect(firstInPage).toEqual(11);
+      expect(lastInPage).toEqual(20);
+    });
+
+    it('the table displays the last page, displaying the remaining rules', () => {
+      const pagination = {
+        page: 2,
+        perPage: 100,
+        total: 101,
+      };
+      const [firstInPage, lastInPage] = getShowingRulesParams(pagination);
+      expect(firstInPage).toEqual(101);
+      expect(lastInPage).toEqual(101);
     });
   });
 });
--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/rules_tables.tsx
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/rules_tables.tsx
@@ -5,8 +5,6 @@
  * 2.0.
  */
 
-/* eslint-disable complexity */
-
 import {
   EuiBasicTable,
   EuiConfirmModal,
@@ -33,7 +31,7 @@ import { showRulesTable } from './helpers';
 import { useRulesTableContext } from './rules_table/rules_table_context';
 import { useAsyncConfirmation } from './rules_table/use_async_confirmation';
 import { RulesTableFilters } from './rules_table_filters/rules_table_filters';
-import { AllRulesUtilityBar } from './utility_bar';
+import { RulesTableUtilityBar } from './rules_table_utility_bar';
 import { useBulkActionsDryRun } from './bulk_actions/use_bulk_actions_dry_run';
 import { useBulkActionsConfirmation } from './bulk_actions/use_bulk_actions_confirmation';
 import { useBulkEditFormFlyout } from './bulk_actions/use_bulk_edit_form_flyout';
@@ -147,7 +145,6 @@ export const RulesTables = React.memo<RulesTableProps>(
     } = useBulkEditFormFlyout();
 
     const selectedItemsCount = isAllSelected ? pagination.total : selectedRuleIds.length;
-    const hasPagination = pagination.total > pagination.perPage;
 
     const { isBulkActionsDryRunLoading, executeBulkActionsDryRun } = useBulkActionsDryRun();
 
@@ -334,10 +331,9 @@ export const RulesTables = React.memo<RulesTableProps>(
         )}
         {shouldShowRulesTable && (
           <>
-            <AllRulesUtilityBar
+            <RulesTableUtilityBar
               canBulkEdit={hasPermissions}
-              hasPagination={hasPagination}
-              paginationTotal={pagination.total ?? 0}
+              pagination={pagination}
               numberSelectedItems={selectedItemsCount}
               onGetBulkItemsPopoverContent={getBulkItemsPopoverContent}
               onRefresh={handleRefreshRules}
@@ -347,7 +343,6 @@ export const RulesTables = React.memo<RulesTableProps>(
               onToggleSelectAll={toggleSelectAll}
               isBulkActionInProgress={isBulkActionsDryRunLoading || loadingRulesAction != null}
               hasDisabledActions={loadingRulesAction != null}
-              hasBulkActions
             />
             <EuiBasicTable
               itemId="id"
