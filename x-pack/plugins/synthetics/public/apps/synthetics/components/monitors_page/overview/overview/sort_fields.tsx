d67bd174af60f657784ee3c8809e2609cdb143cd
Dominique Clarke <dominique.clarke@elastic.co>
2022-08-23 11:36:13 -0400

    adjust styling

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/overview/overview/sort_fields.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/overview/overview/sort_fields.tsx
@@ -4,64 +4,75 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { i18n } from '@kbn/i18n';
-import {
-  EuiPopover,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiTitle,
-  EuiButtonEmpty,
-  EuiSelectable,
-} from '@elastic/eui';
-import { EuiSelectableOptionCheckedType } from '@elastic/eui/src/components/selectable/selectable_option';
+import { EuiFlexGroup, EuiFlexItem, EuiTitle } from '@elastic/eui';
 import { ConfigKey } from '../../../../../../../common/runtime_types';
 
 import { selectOverviewState, setOverviewPageStateAction } from '../../../../state/overview';
+import { SortMenu } from './sort_menu';
 
 export const SortFields = () => {
   const {
     pageState: { sortOrder, sortField },
   } = useSelector(selectOverviewState);
   const dispatch = useDispatch();
-  const [isSortByOpen, setIsSortByOpen] = useState(false);
   const { asc, desc, label } = getOrderContent(sortField);
   const orderByOptions = [
     {
-      label: ORDER_BY_TITLE,
-      isGroupLabel: true,
-    },
-    {
       label: asc,
       value: 'asc',
-      checked: sortOrder === 'asc' ? 'on' : (undefined as EuiSelectableOptionCheckedType),
+      checked: sortOrder === 'asc',
+      onClick: () => {
+        dispatch(
+          setOverviewPageStateAction({
+            sortOrder: 'asc',
+          })
+        );
+      },
     },
     {
       label: desc,
       value: 'desc',
-      checked: sortOrder === 'desc' ? 'on' : (undefined as EuiSelectableOptionCheckedType),
+      checked: sortOrder === 'desc',
+      onClick: () => {
+        dispatch(
+          setOverviewPageStateAction({
+            sortOrder: 'desc',
+          })
+        );
+      },
     },
   ];
   const sortByOptions = [
     {
-      label: SORT_BY_TITLE,
-      isGroupLabel: true,
-    },
-    {
       label: ALPHABETICAL_LABEL,
       value: `${ConfigKey.NAME}.keyword`,
-      checked:
-        sortField === `${ConfigKey.NAME}.keyword`
-          ? 'on'
-          : (undefined as EuiSelectableOptionCheckedType),
+      checked: sortField === `${ConfigKey.NAME}.keyword`,
       defaultSortOrder: 'asc',
+      onClick: () => {
+        dispatch(
+          setOverviewPageStateAction({
+            sortField: `${ConfigKey.NAME}.keyword`,
+            sortOrder: 'asc',
+          })
+        );
+      },
     },
     {
       label: LAST_MODIFIED_LABEL,
       value: 'updated_at',
-      checked: sortField === 'updated_at' ? 'on' : (undefined as EuiSelectableOptionCheckedType),
+      checked: sortField === 'updated_at',
       defaultSortOrder: 'desc',
+      onClick: () => {
+        dispatch(
+          setOverviewPageStateAction({
+            sortField: 'updated_at',
+            sortOrder: 'desc',
+          })
+        );
+      },
     },
   ];
 
@@ -71,58 +82,11 @@ export const SortFields = () => {
         <EuiFlexGroup responsive={false} gutterSize="none" alignItems="center">
           <EuiFlexItem grow={false}>
             <EuiTitle size="xxxs">
-              <h6>{SORT_BY_TITLE}</h6>
+              <h3>{SORT_TITLE}</h3>
             </EuiTitle>
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
-            <EuiPopover
-              button={
-                <EuiButtonEmpty
-                  size="xs"
-                  iconSide="right"
-                  iconType="arrowDown"
-                  onClick={() => setIsSortByOpen((isOpen: boolean) => !isOpen)}
-                >
-                  {label}
-                </EuiButtonEmpty>
-              }
-              isOpen={isSortByOpen}
-              closePopover={() => setIsSortByOpen(false)}
-            >
-              <EuiSelectable
-                options={sortByOptions}
-                style={{ width: '228px' }}
-                singleSelection={'always'}
-                onChange={(options) => {
-                  const checkedOption = options.find((option) => option.checked === 'on');
-                  dispatch(
-                    setOverviewPageStateAction({
-                      sortField: checkedOption?.value || sortField,
-                      sortOrder: (checkedOption?.defaultSortOrder as 'asc' | 'desc') || sortOrder,
-                    })
-                  );
-                }}
-              >
-                {(list) => list}
-              </EuiSelectable>
-              <EuiSelectable
-                options={orderByOptions}
-                style={{ width: '228px' }}
-                singleSelection={'always'}
-                onChange={(options) => {
-                  dispatch(
-                    setOverviewPageStateAction({
-                      sortOrder:
-                        (options.find((option) => option.checked === 'on')?.value as
-                          | 'asc'
-                          | 'desc') || sortOrder,
-                    })
-                  );
-                }}
-              >
-                {(list) => list}
-              </EuiSelectable>
-            </EuiPopover>
+            <SortMenu sortOptions={sortByOptions} orderOptions={orderByOptions} sortField={label} />
           </EuiFlexItem>
         </EuiFlexGroup>
       </EuiFlexItem>
@@ -153,12 +117,8 @@ const getOrderContent = (sortField: string) => {
   }
 };
 
-const SORT_BY_TITLE = i18n.translate('xpack.synthetics.overview.sortPopover.sortBy.title', {
-  defaultMessage: 'Sort by',
-});
-
-const ORDER_BY_TITLE = i18n.translate('xpack.synthetics.overview.sortPopover.orderBy.title', {
-  defaultMessage: 'Order',
+const SORT_TITLE = i18n.translate('xpack.synthetics.overview.sortPopover.sort.title', {
+  defaultMessage: 'Sort',
 });
 
 const SORT_ALPHABETICAL_ASC = i18n.translate(
