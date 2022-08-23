618f0c248588ec40f56639f74ef9d921fa332152
Muhammad Ibragimov <muhammad.ibragimov@elastic.co>
2022-08-23 15:03:39 +0500

    Address CR changes

--- test/functional/apps/console/_xjson.ts
+++ test/functional/apps/console/_xjson.ts
@@ -29,6 +29,12 @@ export default ({ getService, getPageObjects }: FtrProviderContext) => {
       await PageObjects.console.clearTextArea();
     });
 
+    const executeRequest = async (request = '\n GET _search') => {
+      await PageObjects.console.enterRequest(request);
+      await PageObjects.console.clickPlay();
+      await PageObjects.header.waitUntilLoadingHasFinished();
+    };
+
     describe('inline HTTP requests', () => {
       it('should not trigger validation errors with a valid request', async () => {
         await PageObjects.console.enterRequest('\nGET test/doc/1 \n{\n "foo": "bar"');
@@ -79,11 +85,7 @@ export default ({ getService, getPageObjects }: FtrProviderContext) => {
 
     describe('triple quoted strings', () => {
       it('should allow escaping quotation mark by wrapping it in triple quotes', async () => {
-        await PageObjects.console.enterRequest(
-          '\nPOST test/_doc/1 \n{\n "foo": """look "escaped" quotes"""'
-        );
-        await PageObjects.console.clickPlay();
-        await PageObjects.header.waitUntilLoadingHasFinished();
+        await executeRequest('\nPOST test/_doc/1 \n{\n "foo": """look "escaped" quotes"""');
         expect(await PageObjects.console.hasErrorMarker()).to.be(false);
       });
     });
@@ -98,38 +100,28 @@ export default ({ getService, getPageObjects }: FtrProviderContext) => {
       });
 
       it('should allow inline comments in request url row', async () => {
-        await PageObjects.console.enterRequest('\n GET _search // inline comment');
+        await executeRequest('\n GET _search // inline comment');
         expect(await PageObjects.console.hasErrorMarker()).to.be(false);
-        await PageObjects.console.clickPlay();
-        await PageObjects.header.waitUntilLoadingHasFinished();
         expect(await PageObjects.console.getResponseStatus()).to.eql(200);
       });
 
       it('should allow inline comments in request body', async () => {
-        await PageObjects.console.enterRequest(
-          '\n GET _search \n{\n "query": {\n "match_all": {} // inline comment'
-        );
+        await executeRequest('\n GET _search \n{\n "query": {\n "match_all": {} // inline comment');
         expect(await PageObjects.console.hasErrorMarker()).to.be(false);
-        await PageObjects.console.clickPlay();
-        await PageObjects.header.waitUntilLoadingHasFinished();
         expect(await PageObjects.console.getResponseStatus()).to.eql(200);
       });
     });
 
     describe('with a request using a deprecated feature', () => {
       it('should print a warning into the response pane above the JSON', async () => {
-        await PageObjects.console.enterRequest('\nGET .kibana');
-        await PageObjects.console.clickPlay();
-        await PageObjects.header.waitUntilLoadingHasFinished();
+        await executeRequest('\nGET .kibana');
         expect(await PageObjects.console.responseHasDeprecationWarning()).to.be(true);
       });
     });
 
     describe('with a request using no deprecated feature', () => {
       it('should not print a warning into the response pane', async () => {
-        await PageObjects.console.enterRequest();
-        await PageObjects.console.clickPlay();
-        await PageObjects.header.waitUntilLoadingHasFinished();
+        await executeRequest();
         expect(await PageObjects.console.responseHasDeprecationWarning()).to.be(false);
       });
     });
