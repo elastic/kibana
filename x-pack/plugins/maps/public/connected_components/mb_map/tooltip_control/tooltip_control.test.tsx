a97084df1bbe1d6857a17c5526c37ab3fbc65c1d
Nick Peihl <nick.peihl@elastic.co>
2022-08-23 09:08:59 -0400

    [Maps] Add ability to disable tooltips for layer (#138275)
    * Add ability to disable tooltips for layer
    
    * Disable Show tooltips switch when no tooltip fields are available
    
    * Typo
    
    * Review feedback
    
    * Fix loop termination

--- x-pack/plugins/maps/public/connected_components/mb_map/tooltip_control/tooltip_control.test.tsx
+++ x-pack/plugins/maps/public/connected_components/mb_map/tooltip_control/tooltip_control.test.tsx
@@ -38,6 +38,9 @@ const mockLayer = {
   canShowTooltip: () => {
     return true;
   },
+  areTooltipsDisabled: () => {
+    return false;
+  },
   getMbTooltipLayerIds: () => {
     return ['foo', 'bar'];
   },
