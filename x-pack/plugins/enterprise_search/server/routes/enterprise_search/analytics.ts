50a3856fb339d9774a299c4529be97cd5bcec224
Joseph McElroy <joseph.mcelroy@elastic.co>
2022-08-24 10:22:10 +0100

    adds delete analytics collection endpoint

--- x-pack/plugins/enterprise_search/server/routes/enterprise_search/analytics.ts
+++ x-pack/plugins/enterprise_search/server/routes/enterprise_search/analytics.ts
@@ -7,10 +7,12 @@
 
 import { schema } from '@kbn/config-schema';
 
+import { KibanaResponseFactory } from '@kbn/core-http-server';
 import { i18n } from '@kbn/i18n';
 
 import { ErrorCode } from '../../../common/types/error_codes';
 import { addAnalyticsCollection } from '../../lib/analytics/add_analytics_collection';
+import { deleteAnalyticsCollectionByName } from '../../lib/analytics/delete_analytics_collection';
 import {
   fetchAnalyticsCollectionByName,
   fetchAnalyticsCollections,
@@ -18,6 +20,20 @@ import {
 import { RouteDependencies } from '../../plugin';
 import { createError } from '../../utils/create_error';
 import { elasticsearchErrorHandler } from '../../utils/elasticsearch_error_handler';
+
+const createIndexNotFoundError = (error: Error, response: KibanaResponseFactory) => {
+  return createError({
+    errorCode: error.message as ErrorCode,
+    message: i18n.translate(
+      'xpack.enterpriseSearch.server.routes.addAnalyticsCollection.analyticsCollectionNotFoundErrorMessage',
+      {
+        defaultMessage: 'Analytics collection not found',
+      }
+    ),
+    response,
+    statusCode: 404,
+  });
+};
 
 export function registerAnalyticsRoutes({ router, log }: RouteDependencies) {
   router.get(
@@ -57,17 +73,7 @@ export function registerAnalyticsRoutes({ router, log }: RouteDependencies) {
         return response.ok({ body: collection });
       } catch (error) {
         if ((error as Error).message === ErrorCode.ANALYTICS_COLLECTION_NOT_FOUND) {
-          return createError({
-            errorCode: (error as Error).message as ErrorCode,
-            message: i18n.translate(
-              'xpack.enterpriseSearch.server.routes.addAnalyticsCollection.analyticsCollectionNotFoundError',
-              {
-                defaultMessage: 'Analytics collection not found',
-              }
-            ),
-            response,
-            statusCode: 404,
-          });
+          return createIndexNotFoundError(error, response);
         }
 
         throw error;
@@ -102,6 +108,29 @@ export function registerAnalyticsRoutes({ router, log }: RouteDependencies) {
             response,
             statusCode: 409,
           });
+        }
+        throw error;
+      }
+    })
+  );
+
+  router.delete(
+    {
+      path: '/internal/enterprise_search/analytics/collections/{collection_name}',
+      validate: {
+        params: schema.object({
+          collection_name: schema.string(),
+        }),
+      },
+    },
+    elasticsearchErrorHandler(log, async (context, request, response) => {
+      const { client } = (await context.core).elasticsearch;
+      try {
+        await deleteAnalyticsCollectionByName(client, request.params.collection_name);
+        return response.ok();
+      } catch (error) {
+        if ((error as Error).message === ErrorCode.ANALYTICS_COLLECTION_NOT_FOUND) {
+          return createIndexNotFoundError(error, response);
         }
         throw error;
       }
