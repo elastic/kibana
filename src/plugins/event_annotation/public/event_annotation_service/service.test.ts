3523006aeb4f1584e7626c276d228923e02d588f
dej611 <marco.liberati@elastic.co>
2022-08-24 14:57:44 +0200

    :white_check_mark: Add service unit tests

---
+++ src/plugins/event_annotation/public/event_annotation_service/service.test.ts
@@ -0,0 +1,333 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { getEventAnnotationService } from './service';
+import { EventAnnotationServiceType } from './types';
+
+describe('Event Annotation Service', () => {
+  let eventAnnotationService: EventAnnotationServiceType;
+  beforeAll(() => {
+    eventAnnotationService = getEventAnnotationService();
+  });
+  describe('toExpression', () => {
+    it('should work for an empty list', () => {
+      expect(eventAnnotationService.toExpression([])).toEqual([]);
+    });
+
+    it('should skip hidden annotations', () => {
+      expect(
+        eventAnnotationService.toExpression([
+          {
+            id: 'myEvent',
+            type: 'manual',
+            key: {
+              type: 'point_in_time',
+              timestamp: '2022',
+            },
+            label: 'Hello',
+            isHidden: true,
+          },
+          {
+            id: 'myRangeEvent',
+            type: 'manual',
+            key: {
+              type: 'range',
+              timestamp: '2021',
+              endTimestamp: '2022',
+            },
+            label: 'Hello Range',
+            isHidden: true,
+          },
+          {
+            id: 'myEvent',
+            type: 'query',
+            key: {
+              type: 'point_in_time',
+              field: '@timestamp',
+            },
+            label: 'Hello Range',
+            isHidden: true,
+            query: { query: '', language: 'kql' },
+          },
+        ])
+      ).toEqual([]);
+    });
+    it('should process manual point annotations', () => {
+      expect(
+        eventAnnotationService.toExpression([
+          {
+            id: 'myEvent',
+            type: 'manual',
+            key: {
+              type: 'point_in_time',
+              timestamp: '2022',
+            },
+            label: 'Hello',
+          },
+        ])
+      ).toEqual([
+        {
+          type: 'expression',
+          chain: [
+            {
+              type: 'function',
+              function: 'manual_point_event_annotation',
+              arguments: {
+                time: ['2022'],
+                label: ['Hello'],
+                color: ['#f04e98'],
+                lineWidth: [1],
+                lineStyle: ['solid'],
+                icon: ['triangle'],
+                textVisibility: [false],
+                isHidden: [false],
+              },
+            },
+          ],
+        },
+      ]);
+    });
+    it('should process manual range annotations', () => {
+      expect(
+        eventAnnotationService.toExpression([
+          {
+            id: 'myEvent',
+            type: 'manual',
+            key: {
+              type: 'range',
+              timestamp: '2021',
+              endTimestamp: '2022',
+            },
+            label: 'Hello',
+          },
+        ])
+      ).toEqual([
+        {
+          type: 'expression',
+          chain: [
+            {
+              type: 'function',
+              function: 'manual_range_event_annotation',
+              arguments: {
+                time: ['2021'],
+                endTime: ['2022'],
+                label: ['Hello'],
+                color: ['#F04E981A'],
+                outside: [false],
+                isHidden: [false],
+              },
+            },
+          ],
+        },
+      ]);
+    });
+    it('should process query based annotations', () => {
+      expect(
+        eventAnnotationService.toExpression([
+          {
+            id: 'myEvent',
+            type: 'query',
+            key: {
+              type: 'point_in_time',
+              field: '@timestamp',
+            },
+            label: 'Hello',
+            query: { query: '', language: 'kql' },
+          },
+        ])
+      ).toEqual([
+        {
+          type: 'expression',
+          chain: [
+            {
+              type: 'function',
+              function: 'query_point_event_annotation',
+              arguments: {
+                field: ['@timestamp'],
+                label: ['Hello'],
+                color: ['#f04e98'],
+                lineWidth: [1],
+                lineStyle: ['solid'],
+                icon: ['triangle'],
+                textVisibility: [false],
+                textSource: [],
+                textField: [],
+                isHidden: [false],
+                query: [
+                  {
+                    type: 'expression',
+                    chain: [{ arguments: { q: ['""'] }, function: 'lucene', type: 'function' }],
+                  },
+                ],
+                additionalFields: [],
+              },
+            },
+          ],
+        },
+      ]);
+    });
+    it('should process mixed annotations', () => {
+      expect(
+        eventAnnotationService.toExpression([
+          {
+            id: 'myEvent',
+            type: 'manual',
+            key: {
+              type: 'point_in_time',
+              timestamp: '2022',
+            },
+            label: 'Hello',
+          },
+          {
+            id: 'myRangeEvent',
+            type: 'manual',
+            key: {
+              type: 'range',
+              timestamp: '2021',
+              endTimestamp: '2022',
+            },
+            label: 'Hello Range',
+          },
+          {
+            id: 'myEvent',
+            type: 'query',
+            key: {
+              type: 'point_in_time',
+              field: '@timestamp',
+            },
+            label: 'Hello',
+            query: { query: '', language: 'kql' },
+          },
+        ])
+      ).toEqual([
+        {
+          type: 'expression',
+          chain: [
+            {
+              type: 'function',
+              function: 'manual_point_event_annotation',
+              arguments: {
+                time: ['2022'],
+                label: ['Hello'],
+                color: ['#f04e98'],
+                lineWidth: [1],
+                lineStyle: ['solid'],
+                icon: ['triangle'],
+                textVisibility: [false],
+                isHidden: [false],
+              },
+            },
+          ],
+        },
+        {
+          type: 'expression',
+          chain: [
+            {
+              type: 'function',
+              function: 'manual_range_event_annotation',
+              arguments: {
+                time: ['2021'],
+                endTime: ['2022'],
+                label: ['Hello Range'],
+                color: ['#F04E981A'],
+                outside: [false],
+                isHidden: [false],
+              },
+            },
+          ],
+        },
+        {
+          type: 'expression',
+          chain: [
+            {
+              type: 'function',
+              function: 'query_point_event_annotation',
+              arguments: {
+                field: ['@timestamp'],
+                label: ['Hello'],
+                color: ['#f04e98'],
+                lineWidth: [1],
+                lineStyle: ['solid'],
+                icon: ['triangle'],
+                textVisibility: [false],
+                textSource: [],
+                textField: [],
+                isHidden: [false],
+                query: [
+                  {
+                    type: 'expression',
+                    chain: [{ arguments: { q: ['""'] }, function: 'lucene', type: 'function' }],
+                  },
+                ],
+                additionalFields: [],
+              },
+            },
+          ],
+        },
+      ]);
+    });
+    it.each`
+      textSource | textField    | expected
+      ${''}      | ${''}        | ${'name'}
+      ${'name'}  | ${''}        | ${'name'}
+      ${'name'}  | ${'myField'} | ${'name'}
+      ${'field'} | ${''}        | ${'field'}
+      ${'field'} | ${'myField'} | ${'field'}
+    `(
+      "should handle correctly textVisibility when textSource is set to '$textSource' and textField to '$textField'",
+      ({ textSource, textField, expected }) => {
+        expect(
+          eventAnnotationService.toExpression([
+            {
+              id: 'myEvent',
+              type: 'query',
+              key: {
+                type: 'point_in_time',
+                field: '@timestamp',
+              },
+              label: 'Hello',
+              query: { query: '', language: 'kql' },
+              textVisibility: true,
+              textSource,
+              textField,
+            },
+          ])
+        ).toEqual([
+          {
+            type: 'expression',
+            chain: [
+              {
+                type: 'function',
+                function: 'query_point_event_annotation',
+                arguments: {
+                  field: ['@timestamp'],
+                  label: ['Hello'],
+                  color: ['#f04e98'],
+                  lineWidth: [1],
+                  lineStyle: ['solid'],
+                  icon: ['triangle'],
+                  textVisibility: [true],
+                  textSource: [expected],
+                  textField: textSource === 'field' && textField ? [textField] : [],
+                  isHidden: [false],
+                  query: [
+                    {
+                      type: 'expression',
+                      chain: [{ arguments: { q: ['""'] }, function: 'lucene', type: 'function' }],
+                    },
+                  ],
+                  additionalFields: [],
+                },
+              },
+            ],
+          },
+        ]);
+      }
+    );
+  });
+});
