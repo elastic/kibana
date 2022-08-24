fbaa09a5024af54be3eabd8e6aab36684b89608b
spalger <spencer@elastic.co>
2022-08-24 16:49:39 -0700

    use safeAct in ilm jest tests

--- x-pack/plugins/index_lifecycle_management/__jest__/client_integration/edit_policy/features/delete_phase.test.ts
+++ x-pack/plugins/index_lifecycle_management/__jest__/client_integration/edit_policy/features/delete_phase.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { act } from 'react-dom/test-utils';
+import { safeAct as act } from '@kbn/test-jest-helpers';
 import { API_BASE_PATH } from '../../../../common/constants';
 import { setupEnvironment } from '../../helpers';
 import {
--- x-pack/plugins/index_lifecycle_management/__jest__/client_integration/helpers/actions/request_flyout_actions.ts
+++ x-pack/plugins/index_lifecycle_management/__jest__/client_integration/helpers/actions/request_flyout_actions.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { act } from 'react-dom/test-utils';
+import { safeAct as act } from '@kbn/test-jest-helpers';
 import { TestBed } from '@kbn/test-jest-helpers';
 
 const jsonSelector = 'policyRequestJson';
