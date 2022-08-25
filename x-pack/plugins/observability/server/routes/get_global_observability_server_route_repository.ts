ce290e4c335c53fa9c6def20c74d0fd347064242
Kevin Delemme <kevin.delemme@elastic.co>
2022-08-25 10:33:50 -0400

    Create basic SLO route behind feature flag

--- x-pack/plugins/observability/server/routes/get_global_observability_server_route_repository.ts
+++ x-pack/plugins/observability/server/routes/get_global_observability_server_route_repository.ts
@@ -4,10 +4,18 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { ObservabilityConfig } from '..';
 import { rulesRouteRepository } from './rules';
+import { slosRouteRepository } from './slo/route';
 
-export function getGlobalObservabilityServerRouteRepository() {
-  return rulesRouteRepository;
+export function getGlobalObservabilityServerRouteRepository(config: ObservabilityConfig) {
+  const isSloFeatureEnabled = config.unsafe.slo.enabled;
+
+  const repository = {
+    ...rulesRouteRepository,
+    ...(isSloFeatureEnabled ? slosRouteRepository : {}),
+  };
+  return repository;
 }
 
 export type ObservabilityServerRouteRepository = ReturnType<
