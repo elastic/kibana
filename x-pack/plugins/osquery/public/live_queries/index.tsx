d9a60da03df3e8dff1b08040a8ce4f89b57b2825
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-08-25 16:05:18 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/osquery/public/live_queries/index.tsx
+++ x-pack/plugins/osquery/public/live_queries/index.tsx
@@ -54,7 +54,7 @@ const LiveQueryComponent: React.FC<LiveQueryProps> = ({
   packId,
   agentSelection,
   addToTimeline,
-                                                        hideSubmitButton,
+  hideSubmitButton,
 }) => {
   const { data: hasActionResultsPrivileges, isLoading } = useActionResultsPrivileges();
 
