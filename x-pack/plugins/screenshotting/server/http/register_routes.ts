77fc6c6abc9cd38e802d574e7ecf009f78c9fb88
vadimkibana <vadimkibana@gmail.com>
2022-08-25 15:03:53 +0200

    improve route registration - 2

--- x-pack/plugins/screenshotting/server/http/register_routes.ts
+++ x-pack/plugins/screenshotting/server/http/register_routes.ts
@@ -1,6 +1,6 @@
 import { ScreenshottingCoreSetup } from "../plugin";
-import { schema } from '@kbn/config-schema';
-import { firstValueFrom } from 'rxjs';
+import {registerRenderExpressionRaw} from "./routes/render_expression_raw";
+import {registerRenderExpression} from "./routes/render_expression";
 
 export interface RegisterRoutesParams {
   core: ScreenshottingCoreSetup;
@@ -9,35 +9,6 @@ export interface RegisterRoutesParams {
 export const registerRoutes = ({core}: RegisterRoutesParams) => {
   const router = core.http.createRouter();
 
-  router.post({
-    path: '/api/screenshotting/render/expression',
-    validate: {
-      body: schema.object(
-        {
-          expression: schema.string(),
-        },
-      ),
-    },
-  }, async (context, request, response) => {
-    const { expression } = request.body;
-    const [, , screenshotting] = await core.getStartServices();
-    const capture = await firstValueFrom(screenshotting.getScreenshots({
-      expression,
-      format: 'png',
-      request,
-    }));
-
-    const result = capture.results[0];
-    if (!result) throw new Error('No screenshot capture result.');
-
-    const screenshot = result!.screenshots[0];
-    if (!screenshot) throw new Error('No screenshot in results.');
-
-    return response.ok({
-      headers: {
-        'Content-Type': 'image/png',
-      },
-      body: screenshot.data,
-    });
-  });
+  registerRenderExpressionRaw({core, router});
+  registerRenderExpression({core, router});
 };
