dbbeee28293ae58ff3c7d6763998822cbf740dde
jpdjere <jpdjeredjian@gmail.com>
2022-08-26 17:26:31 +0200

    Refactor Cypress tests

--- x-pack/plugins/security_solution/cypress/integration/detection_rules/rules_selection.spec.ts
+++ x-pack/plugins/security_solution/cypress/integration/detection_rules/rules_selection.spec.ts
@@ -4,16 +4,16 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-import { DETECTION_ENGINE_RULES_URL_FIND } from '../../../common/constants';
+import { totalNumberOfPrebuiltRules } from '../../objects/rule';
 import {
-  ruleCheckboxByIdSelector,
   SELECTED_RULES_NUMBER_LABEL,
   SELECT_ALL_RULES_BTN,
   SELECT_ALL_RULES_ON_PAGE_CHECKBOX,
 } from '../../screens/alerts_detection_rules';
 import {
   loadPrebuiltDetectionRules,
+  selectNumberOfRules,
+  unselectNumberOfRules,
   waitForPrebuiltDetectionRulesToBeLoaded,
 } from '../../tasks/alerts_detection_rules';
 import { cleanKibana } from '../../tasks/common';
@@ -21,66 +21,58 @@ import { login, visitWithoutDateRange } from '../../tasks/login';
 import { DETECTIONS_RULE_MANAGEMENT_URL } from '../../urls/navigation';
 
 describe('Rules selection', () => {
-  before(() => {
+  beforeEach(() => {
     cleanKibana();
     login();
-
     visitWithoutDateRange(DETECTIONS_RULE_MANAGEMENT_URL);
-    loadPrebuiltDetectionRules();
-    waitForPrebuiltDetectionRulesToBeLoaded();
   });
 
   it('should correctly update the selection label when rules are individually selected and unselected', () => {
-    cy.request({ url: DETECTION_ENGINE_RULES_URL_FIND }).then(({ body }) => {
-      const [firstRule, secondRule] = body.data;
+    loadPrebuiltDetectionRules();
+    waitForPrebuiltDetectionRulesToBeLoaded();
 
-      cy.get(ruleCheckboxByIdSelector(firstRule.id)).click().should('be.checked');
-      cy.get(ruleCheckboxByIdSelector(secondRule.id)).click().should('be.checked');
+    selectNumberOfRules(2);
 
-      cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '2');
+    cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '2');
 
-      cy.get(ruleCheckboxByIdSelector(firstRule.id)).click().should('not.be.checked');
-      cy.get(ruleCheckboxByIdSelector(secondRule.id)).click().should('not.be.checked');
+    unselectNumberOfRules(2);
 
-      cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '0');
-    });
+    cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '0');
   });
 
   it('should correctly update the selection label when rules are bulk selected and then bulk un-selected', () => {
-    cy.request({ url: DETECTION_ENGINE_RULES_URL_FIND }).then(({ body }) => {
-      const numberOfRules = body.total;
+    loadPrebuiltDetectionRules();
+    waitForPrebuiltDetectionRulesToBeLoaded();
 
-      cy.get(SELECT_ALL_RULES_BTN).click();
+    cy.get(SELECT_ALL_RULES_BTN).click();
 
-      cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', numberOfRules);
+    cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', totalNumberOfPrebuiltRules);
 
-      const bulkSelectButton = cy.get(SELECT_ALL_RULES_BTN);
+    const bulkSelectButton = cy.get(SELECT_ALL_RULES_BTN);
 
-      // Un-select all rules via the Bulk Selection button from the Utility bar
-      bulkSelectButton.click();
+    // Un-select all rules via the Bulk Selection button from the Utility bar
+    bulkSelectButton.click();
 
-      // Current selection should be 0 rules
-      cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '0');
-      // Bulk selection button should be back to displaying all rules
-      cy.get(SELECT_ALL_RULES_BTN).should('contain.text', numberOfRules);
-    });
+    // Current selection should be 0 rules
+    cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '0');
+    // Bulk selection button should be back to displaying all rules
+    cy.get(SELECT_ALL_RULES_BTN).should('contain.text', totalNumberOfPrebuiltRules);
   });
 
   it('should correctly update the selection label when rules are bulk selected and then unselected via the table select all checkbox', () => {
-    cy.request({ url: DETECTION_ENGINE_RULES_URL_FIND }).then(({ body }) => {
-      const numberOfRules = body.total;
+    loadPrebuiltDetectionRules();
+    waitForPrebuiltDetectionRulesToBeLoaded();
 
-      cy.get(SELECT_ALL_RULES_BTN).click();
+    cy.get(SELECT_ALL_RULES_BTN).click();
 
-      cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', numberOfRules);
+    cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', totalNumberOfPrebuiltRules);
 
-      // Un-select all rules via the Un-select All checkbox from the table
-      cy.get(SELECT_ALL_RULES_ON_PAGE_CHECKBOX).click();
+    // Un-select all rules via the Un-select All checkbox from the table
+    cy.get(SELECT_ALL_RULES_ON_PAGE_CHECKBOX).click();
 
-      // Current selection should be 0 rules
-      cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '0');
-      // Bulk selection button should be back to displaying all rules
-      cy.get(SELECT_ALL_RULES_BTN).should('contain.text', numberOfRules);
-    });
+    // Current selection should be 0 rules
+    cy.get(SELECTED_RULES_NUMBER_LABEL).should('contain.text', '0');
+    // Bulk selection button should be back to displaying all rules
+    cy.get(SELECT_ALL_RULES_BTN).should('contain.text', totalNumberOfPrebuiltRules);
   });
 });
