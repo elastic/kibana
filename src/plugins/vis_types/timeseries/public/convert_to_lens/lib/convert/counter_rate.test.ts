ab7ed74993256fcba8b57388f2a9b7d670409be0
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-08-27 10:36:34 +0300

    Added tests for counter_rate.

---
+++ src/plugins/vis_types/timeseries/public/convert_to_lens/lib/convert/counter_rate.test.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { stubLogstashDataView } from '@kbn/data-views-plugin/common/data_view.stub';
+import { TSVB_METRIC_TYPES } from '../../../../common/enums';
+import type { Metric } from '../../../../common/types';
+import { createSeries } from '../__mocks__';
+import { convertToCounterRateFormulaColumn } from './counter_rate';
+
+describe('convertToCounterRateFormulaColumn', () => {
+  const dataView = stubLogstashDataView;
+  const series = createSeries();
+  const metric: Metric = {
+    id: 'some-id',
+    type: TSVB_METRIC_TYPES.POSITIVE_RATE,
+  };
+
+  test.each([
+    ['null if metric contains empty field param', { series, metrics: [metric], dataView }, null],
+    [
+      'formula column if metric contains field param',
+      { series, metrics: [{ ...metric, field: dataView.fields[0].name }], dataView },
+      {
+        operationType: 'formula',
+        params: { formula: 'pick_max(differences(max(bytes)), 0)', format: { id: 'bytes' } },
+        meta: { metricId: 'some-id' },
+      },
+    ],
+    [
+      'differences formula with shift if metric contains unit',
+      { series, metrics: [{ ...metric, field: dataView.fields[0].name, unit: '1h' }], dataView },
+      {
+        operationType: 'formula',
+        params: {
+          formula: 'pick_max(differences(max(bytes), shift=1h), 0)',
+          format: { id: 'bytes' },
+        },
+        meta: { metricId: 'some-id' },
+      },
+    ],
+  ])('should return %s', (_, input, expected) => {
+    expect(convertToCounterRateFormulaColumn(input)).toEqual(expect.objectContaining(expected));
+  });
+});
