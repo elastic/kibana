b3d9a18672d6d30b7f9b8e4a439cc8f82d88a441
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2022-08-29 15:16:47 +0300

    [Lens] Enables ad-hoc dataviews (#138732)
    * [Lens] Enables ad-hoc dataviews
    
    * Fix bug in legacy field existence logic
    
    * lift ad hoc state to frame level and rename data view on incompatible change
    
    * fix some tests
    
    * migrate embedded ad hoc data views
    
    * fix tests and inject/extract ad hoc data view references
    
    * fix bugs and add functional tests
    
    * fix unit tests
    
    * do not show geo fields for ad hoc data views
    
    * Fix functional test
    
    * Refactor to use the new metric viz on the functional tests
    
    Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

--- x-pack/plugins/lens/public/indexpattern_datasource/types.ts
+++ x-pack/plugins/lens/public/indexpattern_datasource/types.ts
@@ -4,7 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
 import type { DragDropIdentifier } from '../drag_drop/providers';
 import type { IncompleteColumn, GenericIndexPatternColumn } from './operations';
 import type { DragDropOperation } from '../types';
