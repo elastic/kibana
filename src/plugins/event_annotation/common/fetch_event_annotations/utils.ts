07c4cab74176cae2e943106b7542811f18b83379
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2022-08-29 17:10:26 +0200

    [Event Annotations] Fetching annotations (#138618)
    * allow handleEsaggsRequest to be used externally for event_annotation plugin
    
    * fetch annotation expression
    
    * fix skippedCount
    
    * add tests
    
    * added textField
    
    * timezone fix
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * query to query point
    
    * cr
    
    * code review changes
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/event_annotation/common/fetch_event_annotations/utils.ts
+++ src/plugins/event_annotation/common/fetch_event_annotations/utils.ts
@@ -7,12 +7,22 @@
  */
 
 import { TimeRange } from '@kbn/data-plugin/common';
-
+import { Datatable, DatatableColumn, DatatableRow } from '@kbn/expressions-plugin/common';
+import { omit, pick } from 'lodash';
+import moment from 'moment';
 import {
-  EventAnnotationOutput,
+  ManualEventAnnotationOutput,
   ManualPointEventAnnotationOutput,
   ManualRangeEventAnnotationOutput,
 } from '../manual_event_annotation/types';
+import { QueryPointEventAnnotationOutput } from '../query_point_event_annotation/types';
+import {
+  annotationColumns,
+  AvailableAnnotationIcon,
+  EventAnnotationOutput,
+  LineStyle,
+  PointStyleProps,
+} from '../types';
 
 export const isRangeAnnotation = (
   annotation: EventAnnotationOutput
@@ -26,7 +36,12 @@ export const isManualPointAnnotation = (
   return 'time' in annotation && !('endTime' in annotation);
 };
 
-export const filterOutOfTimeRange = (annotation: EventAnnotationOutput, timerange?: TimeRange) => {
+export const isManualAnnotation = (
+  annotation: EventAnnotationOutput
+): annotation is ManualPointEventAnnotationOutput | ManualRangeEventAnnotationOutput =>
+  isRangeAnnotation(annotation) || isManualPointAnnotation(annotation);
+
+export const isInRange = (annotation: ManualEventAnnotationOutput, timerange?: TimeRange) => {
   if (!timerange) {
     return false;
   }
@@ -36,8 +51,175 @@ export const filterOutOfTimeRange = (annotation: EventAnnotationOutput, timerang
   if (isManualPointAnnotation(annotation)) {
     return annotation.time >= timerange.from && annotation.time <= timerange.to;
   }
+  return true;
 };
 
-export const sortByTime = (a: EventAnnotationOutput, b: EventAnnotationOutput) => {
-  return a.time.localeCompare(b.time);
+export const sortByTime = (a: DatatableRow, b: DatatableRow) => {
+  return 'time' in a && 'time' in b ? a.time.localeCompare(b.time) : 0;
 };
+
+export const wrapRowsInDatatable = (rows: DatatableRow[], columns = annotationColumns) => {
+  const datatable: Datatable = {
+    type: 'datatable',
+    columns,
+    rows,
+  };
+  return datatable;
+};
+
+export const ANNOTATIONS_PER_BUCKET = 10;
+
+export const postprocessAnnotations = (
+  esaggsResponses: Array<{
+    response: Datatable;
+    fieldsColIdMap: Record<string, string>;
+  }>,
+  queryAnnotationConfigs: QueryPointEventAnnotationOutput[],
+  manualAnnotationDatatableRows: Array<{
+    type: string;
+    id: string;
+    time: string;
+    label: string;
+    color?: string | undefined;
+    icon?: AvailableAnnotationIcon | undefined;
+    lineWidth?: number | undefined;
+    lineStyle?: LineStyle | undefined;
+    textVisibility?: boolean | undefined;
+    isHidden?: boolean | undefined;
+    timebucket: string;
+  }> // todo: simplify types
+) => {
+  const datatableColumns: DatatableColumn[] = esaggsResponses
+    .flatMap(({ response, fieldsColIdMap }) => {
+      const swappedFieldsColIdMap = Object.fromEntries(
+        Object.entries(fieldsColIdMap).map(([k, v]) => [v, k])
+      );
+      return response.columns
+        .filter((col) => swappedFieldsColIdMap[col.id])
+        .map((col) => {
+          return {
+            ...col,
+            id: `field:${swappedFieldsColIdMap[col.id]}`,
+          };
+        });
+    })
+    .reduce<DatatableColumn[]>((acc, col) => {
+      if (!acc.find((c) => c.id === col.id)) {
+        acc.push(col);
+      }
+      return acc;
+    }, [])
+    .concat(annotationColumns);
+
+  const modifiedRows = esaggsResponses
+    .flatMap(({ response, fieldsColIdMap }) =>
+      response.rows.map((row) => {
+        const annotationConfig = queryAnnotationConfigs.find(({ id }) => id === row['col-0-1']);
+        if (!annotationConfig) {
+          throw new Error(`Could not find annotation config for id: ${row['col-0-1']}`);
+        }
+
+        let modifiedRow: TimebucketRow = {
+          ...passStylesFromAnnotationConfig(annotationConfig),
+          id: row['col-0-1'],
+          timebucket: moment(row['col-1-2']).toISOString(),
+          time: row['col-3-4'],
+          type: 'point',
+          label: annotationConfig.textField
+            ? row[fieldsColIdMap[annotationConfig.textField]]
+            : annotationConfig.label,
+        };
+        const countRow = row['col-2-3'];
+        if (countRow > ANNOTATIONS_PER_BUCKET) {
+          modifiedRow = {
+            skippedCount: countRow - ANNOTATIONS_PER_BUCKET,
+            ...modifiedRow,
+          };
+        }
+
+        if (annotationConfig?.extraFields?.length) {
+          modifiedRow.extraFields = annotationConfig.extraFields.reduce(
+            (acc, field) => ({ ...acc, [`field:${field}`]: row[fieldsColIdMap[field]] }),
+            {}
+          );
+        }
+
+        return modifiedRow;
+      })
+    )
+    .concat(...manualAnnotationDatatableRows)
+    .sort((a, b) => a.timebucket.localeCompare(b.timebucket));
+
+  const skippedCountPerBucket = getSkippedCountPerBucket(modifiedRows);
+
+  const flattenedRows = modifiedRows
+    .reduce<DatatableRow[]>((acc, row) => {
+      if (!Array.isArray(row.time)) {
+        acc.push({
+          ...omit(row, ['extraFields', 'skippedCount']),
+          ...row.extraFields,
+        });
+      } else {
+        row.time.forEach((time, index) => {
+          const extraFields: Record<string, string | number | boolean> = {};
+          if (row.extraFields) {
+            Object.entries(row?.extraFields).forEach(([fieldKey, fieldValue]) => {
+              extraFields[fieldKey] = Array.isArray(fieldValue) ? fieldValue[index] : fieldValue;
+            });
+          }
+
+          acc.push({
+            ...omit(row, ['extraFields', 'skippedCount']),
+            ...extraFields,
+            label: Array.isArray(row.label) ? row.label[index] : row.label,
+            time,
+          });
+        });
+      }
+      return acc;
+    }, [])
+    .sort(sortByTime)
+    .reduce<DatatableRow[]>((acc, row, index, arr) => {
+      if (index === arr.length - 1 || row.timebucket !== arr[index + 1].timebucket) {
+        acc.push({ ...row, skippedCount: skippedCountPerBucket[row.timebucket] });
+        return acc;
+      }
+      acc.push(row);
+      return acc;
+    }, []);
+
+  return wrapRowsInDatatable(flattenedRows, datatableColumns);
+};
+
+type TimebucketRow = {
+  id: string;
+  timebucket: string;
+  time: string;
+  type: string;
+  skippedCount?: number;
+  extraFields?: Record<string, string | number | string[] | number[]>;
+} & PointStyleProps;
+
+function getSkippedCountPerBucket(rows: TimebucketRow[]) {
+  return rows.reduce<Record<string, number>>((acc, current) => {
+    if (current.skippedCount) {
+      acc[current.timebucket] = (acc[current.timebucket] || 0) + current.skippedCount;
+    }
+    return acc;
+  }, {});
+}
+
+function passStylesFromAnnotationConfig(
+  annotationConfig: QueryPointEventAnnotationOutput
+): PointStyleProps {
+  return {
+    ...pick(annotationConfig, [
+      `label`,
+      `color`,
+      `icon`,
+      `lineWidth`,
+      `lineStyle`,
+      `textVisibility`,
+    ]),
+  };
+}
