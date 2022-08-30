d352b1a893432c53885d73e5ed835c006ec7f8b4
Matt Kime <matt@mattki.me>
2022-08-30 16:54:03 -0500

    add getFieldPreviewChanges jest test

---
+++ src/plugins/data_view_field_editor/public/components/field_editor/lib.test.ts
@@ -0,0 +1,69 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { getFieldPreviewChanges } from './lib';
+import { Subject } from 'rxjs';
+import { ChangeType, FieldPreview } from '../preview/types';
+
+describe('getFieldPreviewChanges', () => {
+  it('should return new keys', (done) => {
+    const subj = new Subject<FieldPreview[]>();
+    const changes = getFieldPreviewChanges(subj);
+    changes.subscribe((change) => {
+      expect(change).toStrictEqual({ hello: { changeType: ChangeType.UPSERT, type: 'keyword' } });
+      done();
+    });
+    subj.next([]);
+    subj.next([{ key: 'hello', value: 'world', type: 'keyword' }]);
+
+    // expect(changes.length).toBe(0);
+  });
+
+  it('should return updated type', (done) => {
+    const subj = new Subject<FieldPreview[]>();
+    const changes = getFieldPreviewChanges(subj);
+    changes.subscribe((change) => {
+      expect(change).toStrictEqual({ hello: { changeType: ChangeType.UPSERT, type: 'long' } });
+      done();
+    });
+    subj.next([{ key: 'hello', value: 'world', type: 'keyword' }]);
+    subj.next([{ key: 'hello', value: 1, type: 'long' }]);
+  });
+
+  it('should remove keys', (done) => {
+    const subj = new Subject<FieldPreview[]>();
+    const changes = getFieldPreviewChanges(subj);
+    changes.subscribe((change) => {
+      expect(change).toStrictEqual({ hello: { changeType: ChangeType.DELETE } });
+      done();
+    });
+    subj.next([{ key: 'hello', value: 'world', type: 'keyword' }]);
+    subj.next([]);
+  });
+
+  it('should add, update, and remove keys in a single change', (done) => {
+    const subj = new Subject<FieldPreview[]>();
+    const changes = getFieldPreviewChanges(subj);
+    changes.subscribe((change) => {
+      expect(change).toStrictEqual({
+        hello: { changeType: ChangeType.UPSERT, type: 'long' },
+        hello2: { changeType: ChangeType.DELETE },
+        hello3: { changeType: ChangeType.UPSERT, type: 'keyword' },
+      });
+      done();
+    });
+    subj.next([
+      { key: 'hello', value: 'world', type: 'keyword' },
+      { key: 'hello2', value: 'world', type: 'keyword' },
+    ]);
+    subj.next([
+      { key: 'hello', value: 1, type: 'long' },
+      { key: 'hello3', value: 'world', type: 'keyword' },
+    ]);
+  });
+});
