fc702405dbf11d38385ba2900f337afd9af0acee
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-08-31 17:34:05 +0200

    add validation in config

--- x-pack/test/performance/scalability/config.ts
+++ x-pack/test/performance/scalability/config.ts
@@ -8,36 +8,67 @@
 import { FtrConfigProviderContext } from '@kbn/test';
 import fs from 'fs';
 import path from 'path';
+import { REPO_ROOT } from '@kbn/utils';
+import { createFlagError } from '@kbn/dev-cli-errors';
 import { serializeApmGlobalLabels } from '../utils';
 import { ScalabilityTestRunner } from './runner';
+import { FtrProviderContext } from '../ftr_provider_context';
 
 // These "secret" values are intentionally written in the source.
 const APM_SERVER_URL = 'https://142fea2d3047486e925eb8b223559cae.apm.europe-west1.gcp.cloud.es.io';
 const APM_PUBLIC_TOKEN = 'pWFFEym07AKBBhUE2i';
 const AGGS_SHARD_DELAY = process.env.LOAD_TESTING_SHARD_DELAY;
 const DISABLE_PLUGINS = process.env.LOAD_TESTING_DISABLE_PLUGINS;
+const scalabilityJsonPath = process.env.SCALABILITY_JOURNEY_PATH;
+const gatlingProjectRootPath: string =
+  process.env.GATLING_PROJECT_PATH || path.resolve(REPO_ROOT, '../kibana-load-testing');
+
+const readScalabilityJourney = (filePath: string): ScalabilityJourney => {
+  if (path.extname(filePath) !== '.json') {
+    throw createFlagError(`Path to scalability journey json is non-json file: '${filePath}'`);
+  }
+  try {
+    return JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw createFlagError(`Path to scalability journey json is invalid: ${filePath}`);
+    }
+    throw createFlagError(`Invalid JSON provided: '${filePath}', ${error}`);
+  }
+};
+
+interface ScalabilityJourney {
+  journeyName: string;
+}
 
 export default async function ({ readConfigFile, log }: FtrConfigProviderContext) {
   const performanceConfig = await readConfigFile(require.resolve('../journeys/base.config.ts'));
 
-  const scalabilityJsonPath = process.env.SCALABILITY_JOURNEY_PATH;
-  const isJsonPathDefined =
-    scalabilityJsonPath &&
-    fs.existsSync(scalabilityJsonPath) &&
-    path.extname(scalabilityJsonPath) === '.json';
+  if (!fs.existsSync(gatlingProjectRootPath)) {
+    throw createFlagError(
+      `Incorrect path to load testing project: '${gatlingProjectRootPath}'\n
+    Clone 'elastic/kibana-load-testing' and set path using 'GATLING_PROJECT_PATH' env var`
+    );
+  }
+
+  if (!scalabilityJsonPath) {
+    throw createFlagError(
+      `Set path to scalability journey json using 'SCALABILITY_JOURNEY_PATH' env var`
+    );
+  }
+  const scalabilityJourney = readScalabilityJourney(scalabilityJsonPath);
 
   const apmGlobalLabels = {
     ...performanceConfig.get('kbnTestServer').env.ELASTIC_APM_GLOBAL_LABELS,
-    journeyFilePath: isJsonPathDefined ? path.basename(scalabilityJsonPath) : 'unknown',
-    journeyName: isJsonPathDefined
-      ? JSON.parse(fs.readFileSync(scalabilityJsonPath, 'utf8')).journeyName
-      : 'unknown',
+    journeyFilePath: path.basename(scalabilityJsonPath),
+    journeyName: scalabilityJourney.journeyName,
   };
 
   return {
     ...performanceConfig.getAll(),
 
-    testRunner: ScalabilityTestRunner,
+    testRunner: (context: FtrProviderContext) =>
+      ScalabilityTestRunner(context, scalabilityJsonPath, gatlingProjectRootPath),
 
     esTestCluster: {
       ...performanceConfig.get('esTestCluster'),
@@ -63,12 +94,12 @@ export default async function ({ readConfigFile, log }: FtrConfigProviderContext
         ELASTIC_APM_SECRET_TOKEN: APM_PUBLIC_TOKEN,
         ELASTIC_APM_BREAKDOWN_METRICS: false,
         ELASTIC_APM_CAPTURE_SPAN_STACK_TRACES: false,
-        ELASTIC_APM_METRICS_INTERVAL: '120s',
+        ELASTIC_APM_METRICS_INTERVAL: '80s',
         ELASTIC_APM_MAX_QUEUE_SIZE: 20480,
         ELASTIC_APM_GLOBAL_LABELS: serializeApmGlobalLabels(apmGlobalLabels),
       },
-      // delay shutdown by 150 seconds to ensure that APM can report the data it collects during test execution
-      delayShutdown: 150_000,
+      // delay shutdown to ensure that APM can report the data it collects during test execution
+      delayShutdown: 90_000,
     },
   };
 }
--- x-pack/test/performance/scalability/runner.ts
+++ x-pack/test/performance/scalability/runner.ts
@@ -6,43 +6,21 @@
  */
 
 import { withProcRunner } from '@kbn/dev-proc-runner';
-import { REPO_ROOT } from '@kbn/utils';
-import fs from 'fs';
-import path from 'path';
-import { createFlagError } from '@kbn/dev-cli-errors';
 import { FtrProviderContext } from '../ftr_provider_context';
-
-const gatlingProjectRootPath: string =
-  process.env.GATLING_PROJECT_PATH || path.resolve(REPO_ROOT, '../kibana-load-testing');
-const journeyJsonPath = process.env.SCALABILITY_JOURNEY_PATH;
 
 /**
  * ScalabilityTestRunner is used to run load simulation against local Kibana instance
- * Use SCALABILITY_JOURNEY_PATH to set path to scalability json file
- * Use GATLING_PROJECT_PATH to set root path to kibana-load-testing repo
+ * scalabilityJsonPath defines path to the file, parsed and executed by Gatling runner
+ * gatlingProjectRootPath defines root path to the kibana-load-testing repo
  */
-export async function ScalabilityTestRunner({ getService }: FtrProviderContext) {
+export async function ScalabilityTestRunner(
+  { getService }: FtrProviderContext,
+  scalabilityJsonPath: string,
+  gatlingProjectRootPath: string
+) {
   const log = getService('log');
 
-  if (!fs.existsSync(gatlingProjectRootPath)) {
-    throw createFlagError(
-      `Incorrect path to load testing project: '${gatlingProjectRootPath}'\n
-    Clone 'elastic/kibana-load-testing' and set path using 'GATLING_PROJECT_PATH' env var`
-    );
-  }
-
-  if (!journeyJsonPath) {
-    throw createFlagError(
-      `Set path to scalability journey json using 'SCALABILITY_JOURNEY_PATH' env var`
-    );
-  }
-  if (!fs.existsSync(journeyJsonPath) || path.extname(journeyJsonPath) !== '.json') {
-    throw createFlagError(
-      `Path to scalability journey json is invalid or non-json file: '${journeyJsonPath}'`
-    );
-  }
-
-  log.info(`Running scalability test with json file: '${journeyJsonPath}'`);
+  log.info(`Running scalability test with json file: '${scalabilityJsonPath}'`);
 
   await withProcRunner(log, async (procs) => {
     await procs.run('gatling: test', {
@@ -51,7 +29,7 @@ export async function ScalabilityTestRunner({ getService }: FtrProviderContext) 
         'gatling:test',
         '-q',
         '-Dgatling.simulationClass=org.kibanaLoadTest.simulation.generic.GenericJourney',
-        `-DjourneyPath=${journeyJsonPath}`,
+        `-DjourneyPath=${scalabilityJsonPath}`,
       ],
       cwd: gatlingProjectRootPath,
       env: {
