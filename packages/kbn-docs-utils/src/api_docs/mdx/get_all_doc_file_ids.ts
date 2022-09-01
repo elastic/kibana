3c300b202b920484dacfceea5bc26039687c6b2b
spalger <spencer@elastic.co>
2022-09-01 07:07:11 -0500

    fix frontmatter parsing

--- packages/kbn-docs-utils/src/api_docs/mdx/get_all_doc_file_ids.ts
+++ packages/kbn-docs-utils/src/api_docs/mdx/get_all_doc_file_ids.ts
@@ -37,7 +37,7 @@ export async function getAllDocFileIds(outputDir: string) {
 
     let fm;
     try {
-      fm = Yaml.safeLoad(fmYaml.slice(9, fmEnd.index));
+      fm = Yaml.safeLoad(fmYaml.slice(0, fmEnd.index));
       if (typeof fm !== 'object' || fm === null) {
         throw new Error('expected yaml to produce an object');
       }
