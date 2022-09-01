1af1bbfebb98f1ab4a2b541c0bc056f997d927f6
Thom Heymann <190132+thomheymann@users.noreply.github.com>
2022-09-01 11:15:53 +0100

    Update x-pack/plugins/security/server/authentication/authenticator.ts
    Co-authored-by: Thomas Watson <w@tson.dk>

--- x-pack/plugins/security/server/authentication/authenticator.ts
+++ x-pack/plugins/security/server/authentication/authenticator.ts
@@ -792,7 +792,7 @@ export class Authenticator {
     sessionValue,
     skipAuditEvent,
   }: InvalidateSessionValueParams) {
-    if (sessionValue && isSessionAuthenticated(sessionValue) && !skipAuditEvent) {
+    if (isSessionAuthenticated(sessionValue) && !skipAuditEvent) {
       const auditLogger = this.options.audit.asScoped(request);
       auditLogger.log(
         userLogoutEvent({
