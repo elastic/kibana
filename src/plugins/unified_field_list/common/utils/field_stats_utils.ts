a49b96590b08aba8cbb7913c5c597a87d64a7b2d
Joe Reuter <johannes.reuter@elastic.co>
2022-09-05 18:55:03 +0200

    Make sure shard size stays stable for low number of sizes in TSVB, Lens and Agg based (#139791)
    * make sure shard size stays stable for low number of sizes
    
    * add terms test
    
    * adjust test
    
    * fix tests
    
    * add explanatory comment

--- src/plugins/unified_field_list/common/utils/field_stats_utils.ts
+++ src/plugins/unified_field_list/common/utils/field_stats_utils.ts
@@ -234,6 +234,9 @@ export async function getStringSamples(
           terms: {
             ...fieldRef,
             size,
+            // 25 is the default shard size set for size:10 by Elasticsearch.
+            // Setting it to 25 for every size below 10 makes sure the shard size doesn't change for sizes 1-10, keeping the top terms stable.
+            shard_size: size <= 10 ? 25 : undefined,
           },
         },
       },
