cc8e835eb7f70edad108b6064a7f22c70857f1ba
Ersin Erdal <ersin.erdal@elastic.co>
2022-09-05 12:24:40 +0200

    fix telemetry task interval

--- x-pack/plugins/alerting/server/usage/task.ts
+++ x-pack/plugins/alerting/server/usage/task.ts
@@ -204,5 +204,5 @@ export function telemetryTaskRunner(
 }
 
 function getNextMidnight() {
-  return moment().add(1, 'm').startOf('m').toDate();
+  return moment().add(1, 'd').startOf('d').toDate();
 }
