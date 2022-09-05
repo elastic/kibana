596836e3d568fe0c55cca235d00ebc9aafa6d427
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2022-09-05 10:11:20 +0200

    Use `splitApisByFolder`

--- packages/kbn-docs-utils/src/api_docs/mdx/write_plugin_split_by_folder.ts
+++ packages/kbn-docs-utils/src/api_docs/mdx/write_plugin_split_by_folder.ts
@@ -6,10 +6,16 @@
  * Side Public License, v 1.
  */
 
+import { asyncForEachWithLimit } from '@kbn/std';
 import { snakeToCamel } from '../utils';
 import { PluginApi, ApiDeclaration } from '../types';
 import { writePluginDoc } from './write_plugin_mdx_docs';
 import { WritePluginDocsOpts } from './types';
+
+// There is no science behind this 10.
+// When it was first introduced, it was using synchronous APIs, so the concurrency was 1.
+// Feel free to adapt it when more data is gathered.
+const CONCURRENT_WRITES = 10;
 
 export async function writePluginDocSplitByFolder(
   folder: string,
@@ -18,13 +24,11 @@ export async function writePluginDocSplitByFolder(
   const apisByFolder = splitApisByFolder(doc);
 
   log.debug(`Split ${doc.id} into ${apisByFolder.length} services`);
-  await Promise.all(
-    apisByFolder.map((docDef) => {
-      // TODO: we should probably see if we can break down these stats by service folder. As it is, they will represent stats for
-      // the entire plugin.
-      return writePluginDoc(folder, { doc: docDef, plugin, pluginStats, log });
-    })
-  );
+  await asyncForEachWithLimit(apisByFolder, CONCURRENT_WRITES, async (docDef) => {
+    // TODO: we should probably see if we can break down these stats by service folder. As it is, they will represent stats for
+    // the entire plugin.
+    await writePluginDoc(folder, { doc: docDef, plugin, pluginStats, log });
+  });
 }
 
 export function splitApisByFolder(pluginDoc: PluginApi): PluginApi[] {
