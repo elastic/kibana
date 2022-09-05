3b1aea8ed5835417cc4d73bd94ff093270d18326
jpdjere <jpdjeredjian@gmail.com>
2022-09-05 13:19:38 +0200

    Update CSS of ScheduleItem

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/bulk_actions/forms/schedule_form.tsx
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/all/bulk_actions/forms/schedule_form.tsx
@@ -86,7 +86,7 @@ export const ScheduleForm = ({ rulesCount, onClose, onConfirm }: ScheduleFormCom
         componentProps={{
           idAria: 'bulkEditRulesScheduleIntervalSelector',
           dataTestSubj: 'bulkEditRulesScheduleIntervalSelector',
-          placeholder: 'test',
+          fullWidth: true,
         }}
       />
       <UseField
@@ -95,7 +95,7 @@ export const ScheduleForm = ({ rulesCount, onClose, onConfirm }: ScheduleFormCom
         componentProps={{
           idAria: 'bulkEditRulesScheduleLookbackSelector',
           dataTestSubj: 'bulkEditRulesScheduleLookbackSelector',
-          placeholder: 'test',
+          fullWidth: true,
         }}
       />
     </BulkEditFormWrapper>
