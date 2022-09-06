fe3369cdb82479f393a52f6e560bfe7f6cafcc13
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2022-09-06 19:22:08 +0200

    Add Jest Mocks

---
+++ x-pack/plugins/cloud_experiments/common/mocks.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { CloudExperimentsPluginSetup, CloudExperimentsPluginStart } from './types';
+
+function createStartMock(): jest.Mocked<CloudExperimentsPluginStart> {
+  return {
+    getVariation: jest.fn(),
+    reportMetric: jest.fn(),
+  };
+}
+
+function createSetupMock(): jest.Mocked<CloudExperimentsPluginSetup> {
+  return {
+    ...createStartMock(),
+    identifyUser: jest.fn(),
+  };
+}
+
+export const cloudExperimentsMock = {
+  createSetupMock,
+  createStartMock,
+};
