2ef9ecca3b7ea2bd77ea5897df213a344724293f
Paul Tavares <paul.tavares@elastic.co>
2022-09-06 16:28:55 -0400

    FTR test for validating isolate action from Timeline event details

--- x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_solution_integrations.ts
+++ x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_solution_integrations.ts
@@ -6,6 +6,7 @@
  */
 
 import { IndexedHostsAndAlertsResponse } from '@kbn/security-solution-plugin/common/endpoint/index_data';
+import { TimelineResponse } from '@kbn/security-solution-plugin/common/types';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 /**
@@ -14,11 +15,15 @@ import { FtrProviderContext } from '../../ftr_provider_context';
  */
 export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const endpointService = getService('endpointTestResources');
+  const timelineTestService = getService('timeline');
+  const detectionsTestService = getService('detections');
   const log = getService('log');
+  const testSubjects = getService('testSubjects');
   const pageObjects = getPageObjects(['common', 'timeline']);
 
   describe('App level Endpoint functionality', () => {
     let indexedData: IndexedHostsAndAlertsResponse;
+    let endpointAgentId: string;
 
     before(async () => {
       indexedData = await endpointService.loadEndpointData({
@@ -26,7 +31,11 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
         generatorSeed: `app-level-endpoint-${Math.random()}`,
       });
 
-      // stop/start the Endpoint Detetions Rule so that alerts get loaded faster
+      endpointAgentId = indexedData.hosts[0].agent.id;
+
+      // start/stop the endpoint rule. This should cause the rule to run immediately
+      // and avoid us having to wait for the interval (of 5 minutes)
+      await detectionsTestService.stopStartEndpointRule();
     });
 
     after(async () => {
@@ -36,13 +45,50 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       }
     });
 
-    describe('Isolation action access', () => {
-      describe('from Timeline', () => {
-        before(async () => {
-          // stop/start endpoint rule
-          //
-          // wait for alerts
-        });
+    describe('from Timeline', () => {
+      let timeline: TimelineResponse;
+
+      before(async () => {
+        timeline = await timelineTestService.createTimelineForEndpointAlerts(
+          'endpoint in timeline',
+          {
+            endpointAgentId,
+          }
+        );
+
+        // wait for alerts to be available for the Endpoint ID
+        await detectionsTestService.waitForAlerts(
+          timelineTestService.getEndpointAlertsKqlQuery(endpointAgentId).esQuery,
+          // The Alerts rules seems to run every 5 minutes, so we wait here a max
+          // of 6 minutes to ensure it runs and completes and alerts are available.
+          60_000 * 6
+        );
+
+        await pageObjects.timeline.navigateToTimelineList();
+        await pageObjects.timeline.openTimelineById(
+          timeline.data.persistTimeline.timeline.savedObjectId
+        );
+        await pageObjects.timeline.setDateRange('Last 1 year');
+        await pageObjects.timeline.waitForEvents(60_000);
+      });
+
+      after(async () => {
+        if (timeline) {
+          log.info(
+            `Cleaning up created timeline [${timeline.data.persistTimeline.timeline.title} - ${timeline.data.persistTimeline.timeline.savedObjectId}]`
+          );
+          await timelineTestService.deleteTimeline(
+            timeline.data.persistTimeline.timeline.savedObjectId
+          );
+        }
+      });
+
+      it('should show Isolation action in alert details', async () => {
+        await pageObjects.timeline.showEventDetails();
+        await testSubjects.click('take-action-dropdown-btn');
+        await testSubjects.clickWhenNotDisabled('isolate-host-action-item');
+        await testSubjects.existOrFail('endpointHostIsolationForm');
+        await testSubjects.click('hostIsolateCancelButton');
       });
     });
   });
