92b2b4eff500da5d6347a1e096923c14ae32111f
lcawl <lcawley@elastic.co>
2022-09-06 16:48:22 -0700

    Remove unnecessary declarations

--- x-pack/plugins/stack_alerts/public/alert_types/es_query/rule_common_expressions/threshold_help_popover.tsx
+++ x-pack/plugins/stack_alerts/public/alert_types/es_query/rule_common_expressions/threshold_help_popover.tsx
@@ -11,8 +11,6 @@ import {
   EuiPopover,
   EuiPopoverTitle,
   EuiText,
-  EuiCallOut,
-  EuiSpacer,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
