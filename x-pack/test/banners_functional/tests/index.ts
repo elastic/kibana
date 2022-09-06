00a6415d4bcfe43636c2c429f5dc1efd3ddec452
Tre' Seymour <wayne.seymour@elastic.co>
2022-09-06 17:52:42 +0100

    Just run my stuff this go-round.

--- x-pack/test/banners_functional/tests/index.ts
+++ x-pack/test/banners_functional/tests/index.ts
@@ -9,7 +9,7 @@ import { FtrProviderContext } from '../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('banners - functional tests', function () {
-    loadTestFile(require.resolve('./global'));
+    // loadTestFile(require.resolve('./global'));
     loadTestFile(require.resolve('./spaces'));
   });
 }
