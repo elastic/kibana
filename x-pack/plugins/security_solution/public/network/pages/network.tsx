fa6185e3406118eb845d5f3addc7c56e19ecc02e
Nick Peihl <nick.peihl@elastic.co>
2022-09-06 16:09:31 -0400

    Track usage of Security solution network map embeddable

--- x-pack/plugins/security_solution/public/network/pages/network.tsx
+++ x-pack/plugins/security_solution/public/network/pages/network.tsx
@@ -14,6 +14,7 @@ import styled from 'styled-components';
 
 import { isTab } from '@kbn/timelines-plugin/public';
 import { getEsQueryConfig } from '@kbn/data-plugin/common';
+import { TrackApplicationView } from '@kbn/usage-collection-plugin/public';
 import { InputsModelId } from '../../common/store/inputs/constants';
 import { SecurityPageName } from '../../app/types';
 import type { UpdateDateRange } from '../../common/components/charts/common';
@@ -178,19 +179,21 @@ const NetworkComponent = React.memo<NetworkComponentProps>(
 
                 {canUseMaps && (
                   <>
-                    <EuiPanel
-                      hasBorder
-                      paddingSize="none"
-                      data-test-subj="conditional-embeddable-map"
-                    >
-                      <EmbeddedMap
-                        query={query}
-                        filters={filters}
-                        startDate={from}
-                        endDate={to}
-                        setQuery={setQuery}
-                      />
-                    </EuiPanel>
+                    <TrackApplicationView viewId="network-map">
+                      <EuiPanel
+                        hasBorder
+                        paddingSize="none"
+                        data-test-subj="conditional-embeddable-map"
+                      >
+                        <EmbeddedMap
+                          query={query}
+                          filters={filters}
+                          startDate={from}
+                          endDate={to}
+                          setQuery={setQuery}
+                        />
+                      </EuiPanel>
+                    </TrackApplicationView>
                     <EuiSpacer />
                   </>
                 )}
