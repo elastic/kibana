468b6ea3f0c70ad9f0ab511701223db3e4010b31
Maryam Saeidi <maryam.saeidi@elastic.co>
2022-09-07 15:45:13 +0200

    Add functional test for overview page

---
+++ x-pack/test/functional/services/observability/overview/common.ts
@@ -0,0 +1,74 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { FtrProviderContext } from '../../../ftr_provider_context';
+
+// Based on the x-pack/test/functional/es_archives/observability/alerts archive.
+const DATE_WITH_DATA = {
+  rangeFrom: '2021-10-18T13:36:22.109Z',
+  rangeTo: '2021-10-20T13:36:22.109Z',
+};
+
+const ALERTS_SECTION_SELECTOR = 'Alerts';
+const ALERTS_TABLE_NO_DATA_SELECTOR = 'alertsStateTableEmptyState';
+const ALERTS_TABLE_WITH_DATA_SELECTOR = 'alertsTable';
+
+export function ObservabilityOverviewCommonProvider({
+  getPageObjects,
+  getService,
+}: FtrProviderContext) {
+  const find = getService('find');
+  const pageObjects = getPageObjects(['common']);
+  const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+
+  const navigateToOverviewPageWithAlerts = async () => {
+    return await pageObjects.common.navigateToUrlWithBrowserHistory(
+      'observability',
+      '/overview',
+      `?rangeFrom=${DATE_WITH_DATA.rangeFrom}&rangeTo=${DATE_WITH_DATA.rangeTo}`,
+      { ensureCurrentUrl: false }
+    );
+  };
+
+  const navigateToOverviewPage = async () => {
+    return await pageObjects.common.navigateToUrlWithBrowserHistory(
+      'observability',
+      '/overview',
+      undefined,
+      { ensureCurrentUrl: false }
+    );
+  };
+
+  const openAlertsSection = async () => {
+    return await (
+      await find.byCssSelector(`button[aria-controls="${ALERTS_SECTION_SELECTOR}"]`)
+    ).click();
+  };
+
+  const waitForAlertsSectionToOpen = async () => {
+    await retry.waitFor('alert table to appear', async () => {
+      await openAlertsSection();
+
+      return (
+        (await testSubjects.exists(ALERTS_TABLE_NO_DATA_SELECTOR)) ||
+        (await testSubjects.exists(ALERTS_TABLE_WITH_DATA_SELECTOR))
+      );
+    });
+  };
+
+  const getAlertsTableNoDataOrFail = async () => {
+    return await testSubjects.existOrFail(ALERTS_TABLE_NO_DATA_SELECTOR);
+  };
+
+  return {
+    navigateToOverviewPageWithAlerts,
+    waitForAlertsSectionToOpen,
+    navigateToOverviewPage,
+    getAlertsTableNoDataOrFail,
+  };
+}
---
+++ x-pack/test/observability_functional/apps/observability/pages/overview/alert_table.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../../../../ftr_provider_context';
+
+const ALL_ALERTS = 10;
+
+export default ({ getService }: FtrProviderContext) => {
+  const esArchiver = getService('esArchiver');
+
+  describe('Observability overview', function () {
+    this.tags('includeFirefox');
+
+    const observability = getService('observability');
+    const retry = getService('retry');
+
+    beforeEach(async () => {
+      await esArchiver.load('x-pack/test/functional/es_archives/observability/alerts');
+      await esArchiver.load('x-pack/test/functional/es_archives/infra/metrics_and_logs');
+    });
+
+    afterEach(async () => {
+      await esArchiver.unload('x-pack/test/functional/es_archives/observability/alerts');
+      await esArchiver.unload('x-pack/test/functional/es_archives/infra/metrics_and_logs');
+    });
+
+    describe('Without alerts', function () {
+      it('navigate and open alerts section', async () => {
+        await observability.overview.common.navigateToOverviewPage();
+        await observability.overview.common.waitForAlertsSectionToOpen();
+      });
+
+      it('should show no data message', async () => {
+        await retry.try(async () => {
+          await observability.overview.common.getAlertsTableNoDataOrFail();
+        });
+      });
+    });
+
+    describe('With alerts', function () {
+      it('navigate and open alerts section', async () => {
+        await observability.overview.common.navigateToOverviewPageWithAlerts();
+        await observability.overview.common.waitForAlertsSectionToOpen();
+      });
+
+      it('should show alerts correctly', async () => {
+        await retry.try(async () => {
+          const tableRows = await observability.alerts.common.getTableCellsInRows();
+          expect(tableRows.length).to.be(ALL_ALERTS);
+        });
+      });
+    });
+  });
+};
