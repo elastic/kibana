59f6d367ba1946f0abeeafbf18840fb67ca1e7d2
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2022-09-07 18:20:48 +0100

    fix typings

--- x-pack/plugins/security_solution/server/lib/detection_engine/signals/executors/query.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/signals/executors/query.ts
@@ -75,7 +75,6 @@ export const queryExecutor = async ({
 
     return searchAfterAndBulkCreate({
       tuple,
-      completeRule,
       services,
       listClient,
       exceptionsList: exceptionItems,
