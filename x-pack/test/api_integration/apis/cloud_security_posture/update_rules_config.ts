0722bebcc7233646b8405f4cc157a391c772ddb4
Or Ouziel <or.ouziel@elastic.co>
2022-09-07 17:42:43 +0300

    improve rules updates integration test

--- x-pack/test/api_integration/apis/cloud_security_posture/update_rules_config.ts
+++ x-pack/test/api_integration/apis/cloud_security_posture/update_rules_config.ts
@@ -5,83 +5,87 @@
  * 2.0.
  */
 import expect from '@kbn/expect';
-import Chance from 'chance';
-import { FtrProviderContext } from '../../ftr_provider_context';
+import type { SuperTest, Test } from 'supertest';
+import type { FtrProviderContext } from '../../ftr_provider_context';
+import type { PackageInfo, PackagePolicy } from '@kbn/fleet-plugin/common';
+import type { CspRule } from '@kbn/cloud-security-posture-plugin/common/schemas';
+import {
+  CLOUD_SECURITY_POSTURE_PACKAGE_NAME,
+  CSP_RULE_SAVED_OBJECT_TYPE,
+  UPDATE_RULES_CONFIG_ROUTE_PATH,
+} from '@kbn/cloud-security-posture-plugin/common/constants';
+import type { SavedObjectsFindResponse } from '@kbn/core-saved-objects-api-server';
+import { packageToPackagePolicy } from '@kbn/fleet-plugin/common/services/package_to_package_policy';
+
+const getNewPackagePolicy = (packageInfo: PackageInfo) =>
+  packageToPackagePolicy(
+    packageInfo,
+    'policy2', // policy2 from 'x-pack/test/functional/es_archives/fleet/agents'
+    'default',
+    'cloud_security_posture-1'
+  );
+
+const installIntegration = async (supertest: SuperTest<Test>) => {
+  const packageInfo = await getPackageInfo(supertest);
+  return await supertest
+    .post('/api/fleet/package_policies')
+    .set('kbn-xsrf', 'xxxx')
+    .send(getNewPackagePolicy(packageInfo))
+    .expect(200)
+    .then<PackagePolicy>((response) => response.body.item);
+};
+
+const getPackageInfo = (supertest: SuperTest<Test>) =>
+  supertest
+    .get(`/api/fleet/epm/packages/${CLOUD_SECURITY_POSTURE_PACKAGE_NAME}`)
+    .set('kbn-xsrf', 'xxxx')
+    .expect(200)
+    .then<PackageInfo>((response) => response.body.item);
+
+const getCspRulesSO = (supertest: SuperTest<Test>) =>
+  supertest
+    .get(`/api/saved_objects/_find?type=${CSP_RULE_SAVED_OBJECT_TYPE}`)
+    .expect(200)
+    .then<SavedObjectsFindResponse<CspRule>>((response) => response.body);
 
 export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
   const esArchiver = getService('esArchiver');
-  const chance = new Chance();
-  const kibanaServer = getService('kibanaServer');
 
-  // Failing: See https://github.com/elastic/kibana/issues/139683
-  describe.skip('POST /internal/cloud_security_posture/update_rules_config', () => {
-    let agentPolicyId: string;
-
+  describe('POST /internal/cloud_security_posture/update_rules_config', () => {
     before(async () => {
-      await kibanaServer.savedObjects.cleanStandardList();
       await esArchiver.load('x-pack/test/functional/es_archives/fleet/empty_fleet_server');
-
-      const { body: agentPolicyResponse } = await supertest
-        .post(`/api/fleet/agent_policies`)
-        .set('kbn-xsrf', 'xxxx')
-        .send({
-          name: 'Test policy',
-          namespace: 'default',
-        });
-      agentPolicyId = agentPolicyResponse.item.id;
+      await esArchiver.load('x-pack/test/functional/es_archives/fleet/agents');
     });
 
     after(async () => {
-      await kibanaServer.savedObjects.cleanStandardList();
       await esArchiver.unload('x-pack/test/functional/es_archives/fleet/empty_fleet_server');
+      await esArchiver.unload('x-pack/test/functional/es_archives/fleet/agents');
     });
 
-    it('returns a 404 response when package policy id does not exist', async () => {
-      const packagePolicyId = chance.guid();
+    it('updates package policy vars with new rules configuration', async () => {
+      const packagePolicy = await installIntegration(supertest);
+      const currentRules = await getCspRulesSO(supertest);
 
-      const { body: response } = await supertest
-        .post(`/internal/cloud_security_posture/update_rules_config`)
+      const ruleToDisable = currentRules.saved_objects[0];
+      const ruleToEnable = currentRules.saved_objects[1];
+
+      const { body: updatedPackagePolicy } = await supertest
+        .post(UPDATE_RULES_CONFIG_ROUTE_PATH)
         .set('kbn-xsrf', 'xxxx')
         .send({
-          package_policy_id: packagePolicyId,
-          rules: [],
-        })
-        .expect(404);
-
-      expect(response.error).to.be('Not Found');
-    });
-
-    it(`Should return 200 for existing package policy id`, async () => {
-      const { body: postPackageResponse } = await supertest
-        .post(`/api/fleet/package_policies`)
-        .set('kbn-xsrf', 'xxxx')
-        .send({
-          force: true,
-          name: 'cloud_security_posture-1',
-          description: '',
-          namespace: 'default',
-          policy_id: agentPolicyId,
-          enabled: true,
-          inputs: [],
-          package: {
-            name: 'cloud_security_posture',
-            title: 'Kubernetes Security Posture Management',
-            version: '0.0.26', // TODO: Find a method of maintaining the most recent version of the package
-          },
+          package_policy_id: packagePolicy.id,
+          rules: [
+            { id: ruleToDisable.id, enabled: false },
+            { id: ruleToEnable.id, enabled: true },
+          ],
         })
         .expect(200);
 
-      const { body: res } = await supertest
-        .post(`/internal/cloud_security_posture/update_rules_config`)
-        .set('kbn-xsrf', 'xxxx')
-        .send({
-          rules: [],
-          package_policy_id: postPackageResponse.item.id,
-        })
-        .expect(200);
+      const runtimeCfgValue = updatedPackagePolicy.vars.runtimeCfg.value;
 
-      expect(res.name).to.be('cloud_security_posture-1');
+      expect(runtimeCfgValue).to.contain(ruleToEnable.attributes.metadata.rego_rule_id);
+      expect(runtimeCfgValue).to.not.contain(ruleToDisable.attributes.metadata.rego_rule_id);
     });
   });
 }
