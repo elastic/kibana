46a080020394c6667729e7820829f7ad7fe5b4b7
Alexey Antonov <alexwizp@gmail.com>
2022-09-08 23:16:45 +0300

    [TSVB / VEGA / TABLE ] fix `UI` render counter doesn't track renders (#140284)
    * [TSVB] fix broken TSVB regression
    
    * fix VEGA
    
    * fix table

--- src/plugins/vis_types/table/public/services.ts
+++ src/plugins/vis_types/table/public/services.ts
@@ -7,11 +7,7 @@
  */
 
 import { createGetterSetter } from '@kbn/kibana-utils-plugin/public';
-import type { UsageCollectionStart } from '@kbn/usage-collection-plugin/public';
 import type { FieldFormatsStart } from '@kbn/field-formats-plugin/public';
 
 export const [getFormatService, setFormatService] =
   createGetterSetter<FieldFormatsStart>('FieldFormats');
-
-export const [getUsageCollectionStart, setUsageCollectionStart] =
-  createGetterSetter<UsageCollectionStart>('UsageCollection', false);
