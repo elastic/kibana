e11bea9178fccfca309c219797ac52fe182377fc
Dmitry Tomashevich <39378793+dimaanj@users.noreply.github.com>
2022-09-08 16:59:33 +0300

    [Lens] Move field existence from Lens to UnifiedFieldList plugin (#139453)
    * [Lens] move field existence from to unified field list plugin
    
    * [Lens] update readme, move integration tests
    
    * [Lens] update wording paths
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    * [Discover] fix loader tests, clean up code
    
    * [Discover] update datapanel tests, clean up code
    
    * [Discover] remove comments
    
    * [Discover] fix problem with filters
    
    * [Lens] apply suggestions
    
    * [Discover] remove spread
    
    * [Discover] fix type checks
    
    Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

--- src/plugins/unified_field_list/server/ui_settings.ts
+++ src/plugins/unified_field_list/server/ui_settings.ts
@@ -1,34 +1,37 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { i18n } from '@kbn/i18n';
 import { schema } from '@kbn/config-schema';
 
 import { UiSettingsParams } from '@kbn/core/server';
-
-export const FIELD_EXISTENCE_SETTING = 'lens:useFieldExistenceSampling';
+import { FIELD_EXISTENCE_SETTING } from '../common';
 
 export const getUiSettings: () => Record<string, UiSettingsParams> = () => ({
   [FIELD_EXISTENCE_SETTING]: {
-    name: i18n.translate('xpack.lens.advancedSettings.useFieldExistenceSampling.title', {
+    name: i18n.translate('unifiedFieldList.advancedSettings.useFieldExistenceSampling.title', {
       defaultMessage: 'Use field existence sampling',
     }),
     value: false,
     description: i18n.translate(
-      'xpack.lens.advancedSettings.useFieldExistenceSampling.description',
+      'unifiedFieldList.advancedSettings.useFieldExistenceSampling.description',
       {
         defaultMessage:
           'If enabled, document sampling is used to determine field existence (available or empty) for the Lens field list instead of relying on index mappings.',
       }
     ),
     deprecation: {
-      message: i18n.translate('xpack.lens.advancedSettings.useFieldExistenceSampling.deprecation', {
-        defaultMessage: 'This setting is deprecated and will not be supported as of 8.6.',
-      }),
+      message: i18n.translate(
+        'unifiedFieldList.advancedSettings.useFieldExistenceSampling.deprecation',
+        {
+          defaultMessage: 'This setting is deprecated and will not be supported as of 8.6.',
+        }
+      ),
       docLinksKey: 'visualizationSettings',
     },
     category: ['visualization'],
