05f110255abb3475ebbc27c019fc39402091d558
Tre' Seymour <wayne.seymour@elastic.co>
2022-09-08 17:10:01 +0100

    Drop redundant test.

--- x-pack/test/banners_functional/tests/index.ts
+++ x-pack/test/banners_functional/tests/index.ts
@@ -9,7 +9,6 @@ import { FtrProviderContext } from '../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('banners - functional tests', function () {
-    loadTestFile(require.resolve('./global'));
     loadTestFile(require.resolve('./spaces'));
   });
 }
