de556e9da5744620b8f22186c92f79ebdaa6179c
Chris Cressman <chris@chriscressman.com>
2022-09-08 12:51:48 -0400

    Add more missing references

--- x-pack/plugins/enterprise_search/public/applications/shared/doc_links/doc_links.ts
+++ x-pack/plugins/enterprise_search/public/applications/shared/doc_links/doc_links.ts
@@ -176,6 +176,7 @@ class DocLinks {
     this.enterpriseSearchUsersAccess = '';
     this.kibanaSecurity = '';
     this.languageAnalyzers = '';
+    this.languageClients = '';
     this.licenseManagement = '';
     this.pluginsIngestAttachment = '';
     this.queryDsl = '';
@@ -283,6 +284,7 @@ class DocLinks {
     this.enterpriseSearchUsersAccess = docLinks.links.enterpriseSearch.usersAccess;
     this.kibanaSecurity = docLinks.links.kibana.xpackSecurity;
     this.languageAnalyzers = docLinks.links.enterpriseSearch.languageAnalyzers;
+    this.languageClients = docLinks.links.enterpriseSearch.languageClients;
     this.licenseManagement = docLinks.links.enterpriseSearch.licenseManagement;
     this.pluginsIngestAttachment = docLinks.links.plugins.ingestAttachment;
     this.queryDsl = docLinks.links.query.queryDsl;
