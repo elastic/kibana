c9a2a77c4b7f4bd71ad35fa4f9fca9b5fe56a0fc
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2022-09-08 14:13:04 +0100

    Add ConnectorCrawlerIndex type

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/utils/indices.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/utils/indices.ts
@@ -13,6 +13,7 @@ import { ENTERPRISE_SEARCH_CONNECTOR_CRAWLER_SERVICE_TYPE } from '../../../../co
 import { SyncStatus, ConnectorStatus } from '../../../../common/types/connectors';
 import {
   ConnectorIndex,
+  ConnectorCrawlerIndex,
   CrawlerIndex,
   ElasticsearchIndexWithIngestion,
 } from '../../../../common/types/indices';
@@ -38,7 +39,7 @@ export function isConnectorIndex(
 
 export function isConnectorCrawlerIndex(
   index: ElasticsearchIndexWithIngestion | undefined
-): index is CrawlerIndex {
+): index is ConnectorCrawlerIndex {
   const crawlerIndex = index as CrawlerIndex;
   return (
     !!crawlerIndex?.connector &&
