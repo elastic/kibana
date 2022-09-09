21fb5e0c70424a74c947e101a51ddcd4eeda0e38
Byron Hulcher <byronhulcher@gmail.com>
2022-09-09 12:28:02 -0400

    Revert "[Enterprise Search] Use connector scheduling for crawler (#140268)"
    This reverts commit ad0b42781b975e38ea3223e785a7d6006c8746ad.

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/utils/indices.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/utils/indices.ts
@@ -13,7 +13,6 @@ import { ENTERPRISE_SEARCH_CONNECTOR_CRAWLER_SERVICE_TYPE } from '../../../../co
 import { SyncStatus, ConnectorStatus } from '../../../../common/types/connectors';
 import {
   ConnectorIndex,
-  ConnectorCrawlerIndex,
   CrawlerIndex,
   ElasticsearchIndexWithIngestion,
 } from '../../../../common/types/indices';
@@ -34,16 +33,6 @@ export function isConnectorIndex(
   return (
     !!connectorIndex?.connector &&
     connectorIndex.connector.service_type !== ENTERPRISE_SEARCH_CONNECTOR_CRAWLER_SERVICE_TYPE
-  );
-}
-
-export function isConnectorCrawlerIndex(
-  index: ElasticsearchIndexWithIngestion | undefined
-): index is ConnectorCrawlerIndex {
-  const crawlerIndex = index as CrawlerIndex;
-  return (
-    !!crawlerIndex?.connector &&
-    crawlerIndex.connector.service_type === ENTERPRISE_SEARCH_CONNECTOR_CRAWLER_SERVICE_TYPE
   );
 }
 
