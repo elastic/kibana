139e9aee9f2eb5b316203700eb3eb1609324481e
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2022-09-09 18:55:06 +0200

    fix tests

--- x-pack/test/detection_engine_api_integration/security_and_spaces/group6/alerts/alerts_compatibility.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/group6/alerts/alerts_compatibility.ts
@@ -525,6 +525,7 @@ export default ({ getService }: FtrProviderContext) => {
             query: '*:*',
             related_integrations: [],
             required_fields: [],
+            response_actions: [],
             setup: '',
           },
           'kibana.alert.rule.actions': [],
