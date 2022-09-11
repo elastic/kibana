22d5f727c5bbcf2571afde21aa70668bce6c5917
Khristinin Nikita <nikita.khristinin@elastic.co>
2022-09-11 11:34:17 +0200

    Add cypress tests

--- x-pack/plugins/security_solution/public/common/components/event_details/cti_details/host_risk_summary.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/cti_details/host_risk_summary.tsx
@@ -18,60 +18,58 @@ import { RISKY_HOSTS_DOC_LINK } from '../../../../../common/constants';
 const HostRiskSummaryComponent: React.FC<{
   hostRisk: HostRisk;
   originalHostRisk: RiskSeverity | undefined;
-}> = ({ hostRisk, originalHostRisk }) => (
-  <>
-    <EuiPanel hasBorder paddingSize="s" grow={false}>
-      <ThreatSummaryPanelHeader
-        title={i18n.HOST_RISK_DATA_TITLE}
-        toolTipContent={
-          <FormattedMessage
-            id="xpack.securitySolution.alertDetails.overview.hostDataTooltipContent"
-            defaultMessage="Risk classification is displayed only when available for a host. Ensure {hostRiskScoreDocumentationLink} is enabled within your environment."
-            values={{
-              hostRiskScoreDocumentationLink: (
-                <EuiLink href={RISKY_HOSTS_DOC_LINK} target="_blank">
-                  <FormattedMessage
-                    id="xpack.securitySolution.alertDetails.overview.hostRiskScoreLink"
-                    defaultMessage="Host Risk Score"
-                  />
-                </EuiLink>
-              ),
-            }}
-          />
-        }
-      />
+}> = ({ hostRisk, originalHostRisk }) => {
+  const currentHostRiskScore = hostRisk?.result?.[0]?.host?.risk?.calculated_level;
+  return (
+    <>
+      <EuiPanel hasBorder paddingSize="s" grow={false}>
+        <ThreatSummaryPanelHeader
+          title={i18n.HOST_RISK_DATA_TITLE}
+          toolTipContent={
+            <FormattedMessage
+              id="xpack.securitySolution.alertDetails.overview.hostDataTooltipContent"
+              defaultMessage="Risk classification is displayed only when available for a host. Ensure {hostRiskScoreDocumentationLink} is enabled within your environment."
+              values={{
+                hostRiskScoreDocumentationLink: (
+                  <EuiLink href={RISKY_HOSTS_DOC_LINK} target="_blank">
+                    <FormattedMessage
+                      id="xpack.securitySolution.alertDetails.overview.hostRiskScoreLink"
+                      defaultMessage="Host Risk Score"
+                    />
+                  </EuiLink>
+                ),
+              }}
+            />
+          }
+        />
 
-      {hostRisk.loading && <EuiLoadingSpinner data-test-subj="loading" />}
+        {hostRisk.loading && <EuiLoadingSpinner data-test-subj="loading" />}
 
-      {!hostRisk.loading && (
-        <>
-            <>
-              <EnrichedDataRow
-                field={i18n.CURRENT_HOST_RISK_CLASSIFICATION}
-                value={hostRisk?.result?.length > 0 ? (
-                  <RiskScore
-                    severity={hostRisk?.result?.[0]?.risk?.calculated_level}
-                    hideBackgroundColor
-                  />
-                  ): (
+
+        {!hostRisk.loading && (
+          <>
+            {currentHostRiskScore && (
+              <>
+                <EnrichedDataRow
+                  field={i18n.CURRENT_HOST_RISK_CLASSIFICATION}
+                  value={currentHostRiskScore ? (<RiskScore severity={currentHostRiskScore} hideBackgroundColor />) : (
                     getEmptyValue()
-                  )
-                }
-              />
-            </>
-         
-          {originalHostRisk && hostRisk?.result?.[0]?.risk?.calculated_level !== originalHostRisk && (
-            <>
-              <EnrichedDataRow
-                field={i18n.ORIGINAL_HOST_RISK_CLASSIFICATION}
-                value={<RiskScore severity={originalHostRisk} hideBackgroundColor />}
-              />
-            </>
-          )}
-        </>
-      )}
-    </EuiPanel>
-  </>
-);
-
+                  )}
+                />
+              </>
+            )}
+            {originalHostRisk && currentHostRiskScore !== originalHostRisk && (
+              <>
+                <EnrichedDataRow
+                  field={i18n.ORIGINAL_HOST_RISK_CLASSIFICATION}
+                  value={<RiskScore severity={originalHostRisk} hideBackgroundColor />}
+                />
+              </>
+            )}
+          </>
+        )}
+      </EuiPanel>
+    </>
+  );
+};
 export const HostRiskSummary = React.memo(HostRiskSummaryComponent);
