8f94e1edc70b4c9f7fd5fb682c745d2ff6c023a3
Melissa <melissa.alvarez@elastic.co>
2022-09-12 16:35:46 -0600

    show significant terms data in groups expanded row

--- x-pack/plugins/aiops/public/components/explain_log_rate_spikes/mock_data.ts
+++ x-pack/plugins/aiops/public/components/explain_log_rate_spikes/mock_data.ts
@@ -5,13 +5,6 @@
  * 2.0.
  */
 
-// TODO: go through each group, remove duplicate values and create table items like
-// { id: 1, group: {...}, doc_count }
-// create itemIdToExpandedRowMap using the id
-// will need to go through changePoints and find matching fieldName and fieldValue to get the expanded row contents
-// could make an object keeping track of the count for each value of each key
-// { log.logger.keyword: { request: 3, publisher_pipeline_output: 1 }, http.request.method.keyword: { POST: 1 } }
-
 export const mockData = [
   {
     'log.logger.keyword': 'request',
@@ -20,25 +13,28 @@ export const mockData = [
     'meta.cloud.machine_type.keyword': 'r4.4xlarge',
     'docker.container.labels.description.keyword': 'Elastic APM Server',
     'meta.cloud.availability_zone.keyword': 'eu-west-1c',
+    'meta.cloud.instance_id': 'i-0303af698b02ba2ca',
     'docker.container.labels.co.elastic.cloud.allocator.instance_id.keyword': 'instance-0000000009',
     'url.original.keyword': '/intake/v2/rum/events',
     'docker.container.labels.org.label-schema.version.keyword': '7.9.0',
     'beat.name.keyword': 'i-021e32907877b6060',
-    'docker.container.id.keyword': 'cb7d242c6447e54c96da4333cc9b513baa0c2b20ee47f5',
+    'docker.container.id.keyword': '7818fa8d964fd1f1c7e956bb458bc9e449c1377aab311f71fe',
     'error.message': 'rate limit exceeded',
     message: 'too many requests',
     'user_agent.original.keyword': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple',
+    source: '/app/logging/services/allocator/i-0303af698b02ba2c',
     doc_count: 26549,
   },
   {
     'log.logger.keyword': 'publisher_pipeline_output',
     'log.origin.file.name.keyword': 'pipeline/output.go',
     'http.request.method.keyword': 'POST',
-    'meta.cloud.machine_type.keyword': 'r5d.8xlarge',
+    'meta.cloud.machine_type.keyword': 'i3.8xlarge',
     'docker.container.labels.description.keyword':
       'Agent manages other beats based on configuration',
-    'meta.cloud.availability_zone.keyword': 'eu-west-1a',
-    'docker.container.labels.co.elastic.cloud.allocator.instance_id.keyword': 'instance-0000000001',
+    'meta.cloud.availability_zone.keyword': 'ap-southeast-1c',
+    'meta.cloud.instance_id': 'i-0303af698b02ba2ca',
+    'docker.container.labels.co.elastic.cloud.allocator.instance_id.keyword': 'instance-0000000032',
     'url.original.keyword': '/intake/v2/events',
     'docker.container.labels.org.label-schema.version.keyword': '7.6.0',
     'beat.name.keyword': 'i-0b687bc60a868903e',
@@ -46,34 +42,38 @@ export const mockData = [
     'error.message': 'rate limit exceeded',
     message: 'too many requests',
     'user_agent.original.keyword': 'elasticapm-python/5.8.1',
+    source: '/app/logging/services/allocator/i-0303af698b02ba2c',
     doc_count: 20970,
   },
   {
     'log.logger.keyword': 'request',
     'log.origin.file.name.keyword': 'middleware/log_middleware.go',
     'http.request.method.keyword': 'POST',
-    'meta.cloud.machine_type.keyword': 'r4.4xlarge',
+    'meta.cloud.machine_type.keyword': 'i3.8xlarge',
     'docker.container.labels.description.keyword':
       'Agent manages other beats based on configuration',
     'meta.cloud.availability_zone.keyword': 'eu-west-1a',
-    'docker.container.labels.co.elastic.cloud.allocator.instance_id.keyword': 'instance-0000000001',
+    'meta.cloud.instance_id': 'i-0303af698b02ba2ca',
+    'docker.container.labels.co.elastic.cloud.allocator.instance_id.keyword': 'instance-0000000032',
     'url.original.keyword': '/intake/v2/events',
     'docker.container.labels.org.label-schema.version.keyword': '7.6.0',
     'beat.name.keyword': 'i-0b687bc60a868903e',
-    'docker.container.id.keyword': 'd8fd7ff10fda3e20583b0abcc2ff25916935407e87bce3',
+    'docker.container.id.keyword': '7818fa8d964fd1f1c7e956bb458bc9e449c1377aab311f71fe',
     'error.message': 'rate limit exceeded',
     message: 'too many requests',
     'user_agent.original.keyword': 'elasticapm-python/5.8.1',
+    source: '/app/logging/services/allocator/i-0303af698b02ba2c',
     doc_count: 19153,
   },
   {
     'log.logger.keyword': 'request',
     'log.origin.file.name.keyword': 'middleware/log_middleware.go',
     'http.request.method.keyword': 'POST',
-    'meta.cloud.machine_type.keyword': 'r5d.8xlarge',
+    'meta.cloud.machine_type.keyword': 'i3.8xlarge',
     'docker.container.labels.description.keyword':
       'Agent manages other beats based on configuration',
     'meta.cloud.availability_zone.keyword': 'eu-west-1a',
+    'meta.cloud.instance_id': 'i-0303af698b02ba2ca',
     'docker.container.labels.co.elastic.cloud.allocator.instance_id.keyword': 'instance-0000000001',
     'url.original.keyword': '/intake/v2/events',
     'docker.container.labels.org.label-schema.version.keyword': '7.6.0',
@@ -82,23 +82,26 @@ export const mockData = [
     'error.message': 'rate limit exceeded',
     message: 'too many requests',
     'user_agent.original.keyword': 'elasticapm-python/5.8.1',
+    source: '/app/logging/services/allocator/i-0303af698b02ba2c',
     doc_count: 18222,
   },
   {
     'log.logger.keyword': 'request',
     'log.origin.file.name.keyword': 'middleware/log_middleware.go',
     'http.request.method.keyword': 'POST',
-    'meta.cloud.machine_type.keyword': 'r4.4xlarge',
+    'meta.cloud.machine_type.keyword': 'i3.8xlarge',
     'docker.container.labels.description.keyword': 'Elastic APM Server',
-    'meta.cloud.availability_zone.keyword': 'eu-west-1a',
+    'meta.cloud.availability_zone.keyword': 'ap-southeast-1c',
+    'meta.cloud.instance_id': 'i-0303af698b02ba2ca',
     'docker.container.labels.co.elastic.cloud.allocator.instance_id.keyword': 'instance-0000000001',
     'url.original.keyword': '/intake/v2/events',
     'docker.container.labels.org.label-schema.version.keyword': '7.6.0',
     'beat.name.keyword': 'i-0b687bc60a868903e',
-    'docker.container.id.keyword': 'd8fd7ff10fda3e20583b0abcc2ff25916935407e87bce3',
+    'docker.container.id.keyword': '7818fa8d964fd1f1c7e956bb458bc9e449c1377aab311f71fe',
     'error.message': 'rate limit exceeded',
     message: 'too many requests',
     'user_agent.original.keyword': 'elasticapm-python/5.8.1',
+    source: '/app/logging/services/allocator/i-0303af698b02ba2c',
     doc_count: 17997,
   },
 ];
