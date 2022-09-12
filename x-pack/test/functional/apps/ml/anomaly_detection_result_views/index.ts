f1bafd5182211e061c1e3a0a4da41e6d187ea0ef
Robert Oskamp <robert.oskamp@elastic.co>
2022-09-12 17:14:35 +0200

    [ML] Functional tests - split anomaly detection test config (#140430)
    This PR splits the ML functional anomaly detection test configuration into smaller parts.

--- x-pack/test/functional/apps/ml/anomaly_detection_result_views/index.ts
+++ x-pack/test/functional/apps/ml/anomaly_detection_result_views/index.ts
@@ -28,27 +28,14 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
 
       await esArchiver.unload('x-pack/test/functional/es_archives/ml/farequote');
       await esArchiver.unload('x-pack/test/functional/es_archives/ml/ecommerce');
-      await esArchiver.unload('x-pack/test/functional/es_archives/ml/categorization_small');
-      await esArchiver.unload('x-pack/test/functional/es_archives/ml/event_rate_nanos');
 
       await ml.testResources.resetKibanaTimeZone();
     });
 
-    loadTestFile(require.resolve('./single_metric_job'));
-    loadTestFile(require.resolve('./single_metric_job_without_datafeed_start'));
-    loadTestFile(require.resolve('./multi_metric_job'));
-    loadTestFile(require.resolve('./population_job'));
-    loadTestFile(require.resolve('./saved_search_job'));
-    loadTestFile(require.resolve('./advanced_job'));
-    loadTestFile(require.resolve('./single_metric_viewer'));
-    loadTestFile(require.resolve('./anomaly_explorer'));
-    loadTestFile(require.resolve('./categorization_job'));
-    loadTestFile(require.resolve('./date_nanos_job'));
-    loadTestFile(require.resolve('./annotations'));
     loadTestFile(require.resolve('./aggregated_scripted_job'));
-    loadTestFile(require.resolve('./custom_urls'));
+    loadTestFile(require.resolve('./annotations'));
+    loadTestFile(require.resolve('./anomaly_explorer'));
     loadTestFile(require.resolve('./forecasts'));
-    loadTestFile(require.resolve('./lens_to_ml'));
-    loadTestFile(require.resolve('./lens_to_ml_with_wizard'));
+    loadTestFile(require.resolve('./single_metric_viewer'));
   });
 }
