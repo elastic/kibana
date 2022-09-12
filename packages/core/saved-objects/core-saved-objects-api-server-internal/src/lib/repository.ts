d492f5fc8fcf98a69d012bdda8c7c7b001a1c590
Rudolf Meijering <skaapgif@gmail.com>
2022-09-12 15:55:31 +0200

    Don't return the _source for SO#incrementCounter requests

--- packages/core/saved-objects/core-saved-objects-api-server-internal/src/lib/repository.ts
+++ packages/core/saved-objects/core-saved-objects-api-server-internal/src/lib/repository.ts
@@ -1758,7 +1758,7 @@ export class SavedObjectsRepository implements ISavedObjectsRepository {
       index: this.getIndexForType(type),
       refresh,
       require_alias: true,
-      _source: true,
+      _source: false,
       body: {
         script: {
           source: `
