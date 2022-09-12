93563ad91f0e317396ae5979f88383b306894bbc
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-09-12 12:08:44 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- packages/kbn-es-query/src/es_query/from_filters.ts
+++ packages/kbn-es-query/src/es_query/from_filters.ts
@@ -83,12 +83,14 @@ export const buildQueryFromFilters = (
       .filter(filterNegate(negate))
       .filter((filter) => {
         const indexPattern = findIndexPattern(filter.meta.index);
-        return !ignoreFilterIfFieldNotInIndex || filterMatchesIndex(filter, indexPattern)
+        return !ignoreFilterIfFieldNotInIndex || filterMatchesIndex(filter, indexPattern);
       })
       .map((filter) => {
         const indexPattern = findIndexPattern(filter.meta?.index);
         const migratedFilter = migrateFilter(filter, indexPattern);
-        return handleNestedFilter(migratedFilter, indexPattern, { ignoreUnmapped: nestedIgnoreUnmapped })
+        return handleNestedFilter(migratedFilter, indexPattern, {
+          ignoreUnmapped: nestedIgnoreUnmapped,
+        });
       })
       .map(cleanFilter)
       .map(translateToQuery);
