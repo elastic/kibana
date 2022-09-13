85d46127897a8bd3b51b8eadfdbc4eae51b7937e
Davey Holler <daveyholler@hey.com>
2022-09-13 12:35:28 -0700

    Stubs out UI for the ML Inference Pipeline panel (#140456)
    * Stubs out UI for the ML Inference Pipeline panel
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * Inference pipeline card test
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    * Massaging the types definition
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    * Fixing type errors.
    
    * Fixing data panel test.
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/pipelines.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/pipelines.tsx
@@ -7,13 +7,39 @@
 
 import React from 'react';
 
-import { EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
+import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiLink, EuiSpacer } from '@elastic/eui';
 
 import { i18n } from '@kbn/i18n';
 
 import { DataPanel } from '../../../../shared/data_panel/data_panel';
 
+import { InferencePipelineCard } from './inference_pipeline_card';
+import { InferencePipeline } from './types';
+
 export const SearchIndexPipelines: React.FC = () => {
+  // TODO: REPLACE THIS DATA WITH REAL DATA
+
+  const inferencePipelines: InferencePipeline[] = [
+    {
+      pipelineName: 'NER Processor',
+      trainedModelName: 'elastic_dslim_bert_base_ner',
+      isDeployed: true,
+      modelType: 'pytorch',
+    },
+    {
+      pipelineName: 'Sentiment Analysis',
+      trainedModelName: 'elastic_dslim_bert_base_ner',
+      isDeployed: false,
+      modelType: 'pytorch',
+    },
+    {
+      pipelineName: 'Sentiment Analysis',
+      trainedModelName: 'elastic_dslim_bert_base_ner',
+      isDeployed: false,
+      modelType: 'pytorch',
+    },
+  ];
+
   return (
     <>
       <EuiSpacer />
@@ -21,6 +47,16 @@ export const SearchIndexPipelines: React.FC = () => {
         <EuiFlexItem>
           <DataPanel
             hasBorder
+            footerDocLink={
+              <EuiLink href="" external color="subdued">
+                {i18n.translate(
+                  'xpack.enterpriseSearch.content.indices.pipelines.ingestionPipeline.docLink',
+                  {
+                    defaultMessage: 'Learn more about using pipelines in Enterprise Search',
+                  }
+                )}
+              </EuiLink>
+            }
             title={
               <h2>
                 {i18n.translate(
@@ -31,13 +67,30 @@ export const SearchIndexPipelines: React.FC = () => {
                 )}
               </h2>
             }
+            subtitle={i18n.translate(
+              'xpack.enterpriseSearch.content.indices.pipelines.ingestionPipeline.subtitle',
+              {
+                defaultMessage: 'Ingest pipelines optimize your index for search applications',
+              }
+            )}
             iconType="logstashInput"
           >
             <div />
           </DataPanel>
-          <EuiSpacer />
+        </EuiFlexItem>
+        <EuiFlexItem>
           <DataPanel
             hasBorder
+            footerDocLink={
+              <EuiLink href="" external color="subdued">
+                {i18n.translate(
+                  'xpack.enterpriseSearch.content.indices.pipelines.mlInferencePipelines.docLink',
+                  {
+                    defaultMessage: 'Learn more about deploying ML models in Elastic',
+                  }
+                )}
+              </EuiLink>
+            }
             title={
               <h2>
                 {i18n.translate(
@@ -48,12 +101,41 @@ export const SearchIndexPipelines: React.FC = () => {
                 )}
               </h2>
             }
+            subtitle={i18n.translate(
+              'xpack.enterpriseSearch.content.indices.pipelines.mlInferencePipelines.subtitle',
+              {
+                defaultMessage:
+                  'Inference pipelines will be run as processors from the Enterprise Search Ingest Pipeline',
+              }
+            )}
             iconType="compute"
+            action={
+              <EuiButton color="success" size="s" iconType="plusInCircle">
+                {i18n.translate(
+                  'xpack.enterpriseSearch.content.indices.pipelines.mlInferencePipelines.newButton',
+                  {
+                    defaultMessage: 'Add ML inference pipeline',
+                  }
+                )}
+              </EuiButton>
+            }
           >
-            <div />
+            {inferencePipelines.length > 0 && (
+              <EuiFlexGroup direction="column" gutterSize="s">
+                {inferencePipelines.map((item: InferencePipeline, index: number) => (
+                  <EuiFlexItem key={index}>
+                    <InferencePipelineCard
+                      trainedModelName={item.trainedModelName}
+                      pipelineName={item.pipelineName}
+                      isDeployed={item.isDeployed}
+                      modelType={item.modelType}
+                    />
+                  </EuiFlexItem>
+                ))}
+              </EuiFlexGroup>
+            )}
           </DataPanel>
         </EuiFlexItem>
-        <EuiFlexItem />
       </EuiFlexGroup>
     </>
   );
