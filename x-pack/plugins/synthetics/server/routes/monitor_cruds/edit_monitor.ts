abc1fe50ff5e6a1b06571b23039ef87eef91df8c
Dominique Clarke <dominique.clarke@elastic.co>
2022-09-13 14:07:52 -0400

    [Synthetics] remove hydrating saved objects (#140545)
    * synthetics - remove hydrating saved objects
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * remove logic to set url back to empty string
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/synthetics/server/routes/monitor_cruds/edit_monitor.ts
+++ x-pack/plugins/synthetics/server/routes/monitor_cruds/edit_monitor.ts
@@ -20,7 +20,6 @@ import {
   SyntheticsMonitorWithSecrets,
   SyntheticsMonitor,
   ConfigKey,
-  FormMonitorType,
 } from '../../../common/runtime_types';
 import { SyntheticsRestApiRouteFactory } from '../../legacy_uptime/routes/types';
 import { API_URLS } from '../../../common/constants';
@@ -146,10 +145,7 @@ export const syncEditedMonitor = async ({
     const editedSOPromise = savedObjectsClient.update<MonitorFields>(
       syntheticsMonitorType,
       previousMonitor.id,
-      monitorWithRevision.type === 'browser' &&
-        monitorWithRevision[ConfigKey.FORM_MONITOR_TYPE] !== FormMonitorType.SINGLE
-        ? { ...monitorWithRevision, urls: '' }
-        : monitorWithRevision
+      monitorWithRevision
     );
 
     const editSyncPromise = syntheticsMonitorClient.editMonitor(
