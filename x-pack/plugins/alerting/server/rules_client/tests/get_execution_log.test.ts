fa995d9f554be5cf0dc408e4d323da3c154a58bb
doakalexi <109488926+doakalexi@users.noreply.github.com>
2022-09-13 14:56:08 -0400

    [ResponseOps][Alerting] Store rule name in alerting event log documents (#140441)
    * Removing secondary query
    
    * Adding field to agg
    
    * Removing log statement
    
    * Fixing types
    
    * Adding rule name to function tests

--- x-pack/plugins/alerting/server/rules_client/tests/get_execution_log.test.ts
+++ x-pack/plugins/alerting/server/rules_client/tests/get_execution_log.test.ts
@@ -145,6 +145,7 @@ const aggregateResults = {
                       _source: {
                         rule: {
                           id: 'a348a740-9e2c-11ec-bd64-774ed95c43ef',
+                          name: 'rule-name',
                         },
                         event: {
                           outcome: 'success',
@@ -248,7 +249,7 @@ const aggregateResults = {
                       _id: 'a4wIZX8B8TGQpG7Xwpnz',
                       _score: 1.0,
                       _source: {
-                        rule: { id: 'a348a740-9e2c-11ec-bd64-774ed95c43ef' },
+                        rule: { id: 'a348a740-9e2c-11ec-bd64-774ed95c43ef', name: 'rule-name' },
                         event: {
                           outcome: 'success',
                         },
@@ -377,6 +378,7 @@ describe('getExecutionLogForRule()', () => {
           timed_out: false,
           schedule_delay_ms: 3126,
           rule_id: 'a348a740-9e2c-11ec-bd64-774ed95c43ef',
+          rule_name: 'rule-name',
         },
         {
           id: '41b2755e-765a-4044-9745-b03875d5e79a',
@@ -398,6 +400,7 @@ describe('getExecutionLogForRule()', () => {
           timed_out: false,
           schedule_delay_ms: 3345,
           rule_id: 'a348a740-9e2c-11ec-bd64-774ed95c43ef',
+          rule_name: 'rule-name',
         },
       ],
     });
