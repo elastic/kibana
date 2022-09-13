67900435c62f178d007caa9ea8ce88484adcc0a3
Alison Goryachev <alison.goryachev@elastic.co>
2022-09-13 09:22:56 -0400

    move guided_onboarding out of x-pack

--- src/plugins/guided_onboarding/server/config.ts
+++ src/plugins/guided_onboarding/server/config.ts
@@ -1,14 +1,15 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { PluginConfigDescriptor } from '@kbn/core/server';
 import { schema, TypeOf } from '@kbn/config-schema';
 
-// By default, hide any guided onboarding UI. Change it with xpack.guided_onboarding.ui:true in kibana.dev.yml
+// By default, hide any guided onboarding UI. Change it with guidedOnboarding.ui:true in kibana.dev.yml
 const configSchema = schema.object({
   ui: schema.boolean({ defaultValue: false }),
 });
