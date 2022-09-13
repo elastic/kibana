59cd3b5bff579e4ce15aef822ac13a0c968d2509
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-09-13 14:19:49 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/observability/public/pages/alert_details/components/alert_details.tsx
+++ x-pack/plugins/observability/public/pages/alert_details/components/alert_details.tsx
@@ -19,22 +19,22 @@ import PageNotFound from '../../404';
 
 function getStaticAlertData() {
   const alert: any = {
-    "_index": "",
-    "_id": "",
-    "kibana.alert.rule.category": "Inventory",
-    "kibana.alert.rule.execution.uuid": "b64c05ac-9c41-4dea-a305-37f57706fb47",
-    "kibana.alert.rule.name": "CPU Usage",
-    "kibana.alert.rule.producer": "infrastructure",
-    "kibana.alert.rule.rule_type_id": "metrics.alert.inventory.threshold",
-    "kibana.alert.rule.uuid": "bfa42390-3357-11ed-9349-2388d5bf2748",
-    "kibana.alert.rule.tags": ["tag1"],
-    "@timestamp": "2022-09-13T11:37:48.633Z",
-    "kibana.alert.reason": "CPU usage is 10.9% in the last 1 min for Benas-MBP. Alert when > 1%.",
-    "kibana.alert.duration.us": 315031000,
-    "kibana.alert.instance.id": "Benas-MBP",
-    "kibana.alert.start": "2022-09-13T11:32:33.602Z",
-    "kibana.alert.uuid": "15f1b46c-6b78-4dca-964a-dcda5d71ad50",
-    "kibana.alert.status": "active"
+    _index: '',
+    _id: '',
+    'kibana.alert.rule.category': 'Inventory',
+    'kibana.alert.rule.execution.uuid': 'b64c05ac-9c41-4dea-a305-37f57706fb47',
+    'kibana.alert.rule.name': 'CPU Usage',
+    'kibana.alert.rule.producer': 'infrastructure',
+    'kibana.alert.rule.rule_type_id': 'metrics.alert.inventory.threshold',
+    'kibana.alert.rule.uuid': 'bfa42390-3357-11ed-9349-2388d5bf2748',
+    'kibana.alert.rule.tags': ['tag1'],
+    '@timestamp': '2022-09-13T11:37:48.633Z',
+    'kibana.alert.reason': 'CPU usage is 10.9% in the last 1 min for Benas-MBP. Alert when > 1%.',
+    'kibana.alert.duration.us': 315031000,
+    'kibana.alert.instance.id': 'Benas-MBP',
+    'kibana.alert.start': '2022-09-13T11:32:33.602Z',
+    'kibana.alert.uuid': '15f1b46c-6b78-4dca-964a-dcda5d71ad50',
+    'kibana.alert.status': 'active',
   };
 
   return {
