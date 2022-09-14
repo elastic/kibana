63b8b05038664706d3d3851e0fb4959dfacbb19e
dej611 <marco.liberati@elastic.co>
2022-09-14 18:14:01 +0200

    :bug: Fix new queryinput changes

--- x-pack/test/functional/page_objects/lens_page.ts
+++ x-pack/test/functional/page_objects/lens_page.ts
@@ -119,6 +119,7 @@ export function LensPageProvider({ getService, getPageObjects }: FtrProviderCont
       // type * in the query editor
       const queryInput = await testSubjects.find('lnsXY-annotation-query-based-query-input');
       await queryInput.type(opts.queryString);
+      await testSubjects.click('indexPattern-filters-existingFilterTrigger');
       await this.selectOptionFromComboBox(
         'lnsXY-annotation-query-based-field-picker',
         opts.timeField
