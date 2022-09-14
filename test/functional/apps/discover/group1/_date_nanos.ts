811b678dc48f188d7ae13b41053227bb749bef74
rashmikulkarni <rashmi.kulkarni@elastic.co>
2022-09-14 16:34:15 -0700

    timepicker change

--- test/functional/apps/discover/group1/_date_nanos.ts
+++ test/functional/apps/discover/group1/_date_nanos.ts
@@ -15,30 +15,32 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const PageObjects = getPageObjects(['common', 'timePicker', 'discover']);
   const kibanaServer = getService('kibanaServer');
   const security = getService('security');
-  const fromTime = 'Sep 22, 2019 @ 20:31:44.000';
-  const toTime = 'Sep 23, 2019 @ 03:31:44.000';
+  const from = 'Sep 22, 2019 @ 20:31:44.000';
+  const to = 'Sep 23, 2019 @ 03:31:44.000';
 
   describe('date_nanos', function () {
     before(async function () {
       await esArchiver.loadIfNeeded('test/functional/fixtures/es_archiver/date_nanos');
       await kibanaServer.savedObjects.clean({ types: ['search', 'index-pattern'] });
       await kibanaServer.importExport.load('test/functional/fixtures/kbn_archiver/date_nanos');
+      await PageObjects.common.setTime({ from, to });
       await kibanaServer.uiSettings.replace({ defaultIndex: 'date-nanos' });
       await security.testUser.setRoles(['kibana_admin', 'kibana_date_nanos']);
       await PageObjects.common.navigateToApp('discover');
-      await PageObjects.timePicker.setAbsoluteRange(fromTime, toTime);
+      await PageObjects.timePicker.setAbsoluteRange(from, to);
     });
 
     after(async function unloadMakelogs() {
       await security.testUser.restoreDefaults();
       await esArchiver.unload('test/functional/fixtures/es_archiver/date_nanos');
       await kibanaServer.savedObjects.clean({ types: ['search', 'index-pattern'] });
+      await PageObjects.common.unsetTime();
     });
 
     it('should show a timestamp with nanoseconds in the first result row', async function () {
       const time = await PageObjects.timePicker.getTimeConfig();
-      expect(time.start).to.be(fromTime);
-      expect(time.end).to.be(toTime);
+      expect(time.start).to.be(from);
+      expect(time.end).to.be(to);
       const rowData = await PageObjects.discover.getDocTableIndex(1);
       expect(rowData.startsWith('Sep 22, 2019 @ 23:50:13.253123345')).to.be.ok();
     });
