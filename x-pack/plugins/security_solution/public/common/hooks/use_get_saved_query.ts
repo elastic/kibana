95bccfe9a834ba7a22d3a8a916b1c031109dbe6e
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2022-09-14 17:26:31 +0100

    CR: useFetch

--- x-pack/plugins/security_solution/public/common/hooks/use_get_saved_query.ts
+++ x-pack/plugins/security_solution/public/common/hooks/use_get_saved_query.ts
@@ -5,43 +5,50 @@
  * 2.0.
  */
 
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 
 import { useSavedQueryServices } from '../utils/saved_query_services';
 import type { DefineStepRule } from '../../detections/pages/detection_engine/rules/types';
 
+import { useFetch, REQUEST_NAMES } from './use_fetch';
 import { SAVED_QUERY_LOAD_ERROR_TOAST } from './translations';
 import { useAppToasts } from './use_app_toasts';
 
 export const useGetSavedQuery = (savedQueryId: string | undefined) => {
   const savedQueryServices = useSavedQueryServices();
-  const [isSavedQueryLoading, setIsSavedQueryLoading] = useState(false);
-  const [savedQueryBar, setSavedQueryBar] = useState<DefineStepRule['queryBar'] | null>(null);
   const { addError } = useAppToasts();
+  const { fetch, data, isLoading, error } = useFetch(
+    REQUEST_NAMES.GET_SAVED_QUERY,
+    savedQueryServices.getSavedQuery
+  );
 
   useEffect(() => {
     if (savedQueryId) {
-      setIsSavedQueryLoading(true);
-      savedQueryServices
-        .getSavedQuery(savedQueryId)
-        .then((newSavedQuery) => {
-          setIsSavedQueryLoading(false);
-          setSavedQueryBar({
-            saved_id: newSavedQuery.id,
-            filters: newSavedQuery.attributes.filters ?? [],
-            query: newSavedQuery.attributes.query,
-            title: newSavedQuery.attributes.title,
-          });
-        })
-        .catch((err) => {
-          addError(err, { title: SAVED_QUERY_LOAD_ERROR_TOAST });
-        })
-        .finally(() => setIsSavedQueryLoading(false));
+      fetch(savedQueryId);
     }
-  }, [savedQueryId, savedQueryServices, addError]);
+  }, [savedQueryId, fetch]);
+
+  useEffect(() => {
+    if (error) {
+      addError(error, { title: SAVED_QUERY_LOAD_ERROR_TOAST });
+    }
+  }, [error, addError]);
+
+  const savedQueryBar = useMemo<DefineStepRule['queryBar'] | null>(
+    () =>
+      data
+        ? {
+            saved_id: data.id,
+            filters: data.attributes.filters ?? [],
+            query: data.attributes.query,
+            title: data.attributes.title,
+          }
+        : null,
+    [data]
+  );
 
   return {
-    isSavedQueryLoading,
+    isSavedQueryLoading: isLoading,
     savedQueryBar,
   };
 };
