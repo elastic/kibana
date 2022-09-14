bc9e06452fff6a043e2b5d141422df5673d74919
Justin Kambic <jk@elastic.co>
2022-09-14 16:47:07 -0400

    Add unit test for `getCounts`.

--- x-pack/plugins/synthetics/server/routes/status/current_status.test.ts
+++ x-pack/plugins/synthetics/server/routes/status/current_status.test.ts
@@ -5,7 +5,41 @@
  * 2.0.
  */
 
-import { periodToMs } from './current_status';
+import { getCounts, periodToMs } from './current_status';
+
+jest.mock('../util', () => ({
+  getMonitors: jest.fn().mockReturnValue({
+    per_page: 10,
+    saved_objects: [
+      {
+        id: 'mon-1',
+        attributes: {
+          enabled: false,
+          locations: ['us-east1', 'us-west1', 'japan'],
+        },
+      },
+      {
+        id: 'mon-2',
+        attributes: {
+          enabled: true,
+          locations: ['us-east1', 'us-west1', 'japan'],
+          schedule: {
+            number: '10',
+            unit: 'm',
+          },
+        },
+      },
+    ],
+  }),
+}));
+
+jest.mock('../../legacy_uptime/lib/requests/get_snapshot_counts', () => ({
+  getSnapshotCount: jest.fn().mockReturnValue({
+    up: 2,
+    down: 1,
+    total: 3,
+  }),
+}));
 
 describe('current status route', () => {
   describe('periodToMs', () => {
@@ -21,6 +55,22 @@ describe('current status route', () => {
     });
     it('converts hours', () => {
       expect(periodToMs({ number: '1', unit: 'h' })).toEqual(3_600_000);
+    });
+  });
+
+  describe('getCounts', () => {
+    it('parses responses and returns id and count data', async () => {
+      // @ts-expect-error mock implementation for test purposes
+      expect(await getCounts(jest.fn(), jest.fn(), jest.fn())).toEqual({
+        disabledCount: 3,
+        disabledIds: ['mon-1'],
+        enabledIds: ['mon-2'],
+        snapshot: {
+          down: 1,
+          total: 3,
+          up: 2,
+        },
+      });
     });
   });
 });
