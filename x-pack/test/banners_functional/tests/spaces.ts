3522d3956aa1ae7ee1d4f1a210e47c360f1b8822
Tre' Seymour <wayne.seymour@elastic.co>
2022-09-14 09:03:12 +0100

    back to space case but without quotes in the config

--- x-pack/test/banners_functional/tests/spaces.ts
+++ x-pack/test/banners_functional/tests/spaces.ts
@@ -52,7 +52,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       await PageObjects.common.navigateToApp('home', { basePath: '/s/another-space' });
 
       expect(await PageObjects.banners.isTopBannerVisible()).to.eql(true);
-      expect(await PageObjects.banners.getTopBannerText()).to.eql('global_banner_text');
+      expect(await PageObjects.banners.getTopBannerText()).to.eql('global banner text');
     });
 
     // Reference: https://github.com/elastic/kibana/pull/135783#issuecomment-1178178075
@@ -65,7 +65,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       await PageObjects.common.navigateToApp('login');
 
       expect(await PageObjects.banners.isTopBannerVisible()).to.eql(true);
-      expect(await PageObjects.banners.getTopBannerText()).to.eql('global_banner_text');
+      expect(await PageObjects.banners.getTopBannerText()).to.eql('global banner text');
     });
   });
 }
