7d59be778a3be6c25aaa1ec029b63d5442812c83
Jiawei Wu <jiawei.wu@cmd.com>
2022-09-14 11:45:55 -0700

    Fix bulk operations type

--- x-pack/plugins/alerting/server/rules_client/rules_client.ts
+++ x-pack/plugins/alerting/server/rules_client/rules_client.ts
@@ -269,6 +269,7 @@ export type BulkEditOperation =
   | {
       operation: 'set';
       field: Extract<BulkEditFields, 'apiKey'>;
+      value: undefined;
     };
 
 type RuleParamsModifier<Params extends RuleTypeParams> = (params: Params) => Promise<Params>;
