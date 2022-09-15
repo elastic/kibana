f313c707c891e9a29add86c97984c04906543c30
Paulo Henrique <paulo.henrique@elastic.co>
2022-09-15 16:48:54 -0700

    css pollish

--- x-pack/plugins/session_view/public/components/tty_player_controls/tty_player_controls_markers/styles.ts
+++ x-pack/plugins/session_view/public/components/tty_player_controls/tty_player_controls_markers/styles.ts
@@ -6,98 +6,102 @@
  */
 
 import { useMemo } from 'react';
-import { css, CSSObject } from '@emotion/react';
+import { CSSObject } from '@emotion/react';
 import { useEuiTheme } from '../../../hooks';
-import scrubberDefault from './assets/scrubber.svg';
-import scrubberYellow from './assets/scrubber-yellow.svg';
+
+type TTYPlayerLineMarkerType = 'output' | 'data_limited';
 
 export const useStyles = () => {
-  const { euiTheme } = useEuiTheme();
+  const { euiTheme, euiVars } = useEuiTheme();
   const cached = useMemo(() => {
-    const { size } = euiTheme;
+    const { border } = euiTheme;
 
-    const wrapper = css`
-      top: 5px;
-      z-index: 2;
-      position: absolute;
-      width: 100%;
-    `;
-
-    const getMarkerBg = (type, selected) => {
-      if (type === 'data_limited') {
-        return '#F3D371';
-      }
-      if (selected) {
-        return '#F04E98';
-      }
-      return 'rgb(96, 146, 192)';
+    const markersOverlay: CSSObject = {
+      top: 5,
+      zIndex: 2,
+      position: 'absolute',
+      width: '100%',
     };
 
-    const marker = (type, selected): CSSObject => ({
+    const getMarkerBackgroundColor = (type: TTYPlayerLineMarkerType, selected: boolean) => {
+      if (type === 'data_limited') {
+        return euiVars.terminalOutputMarkerWarning;
+      }
+      if (selected) {
+        return euiVars.terminalOutputMarkerAccent;
+      }
+      return euiVars.euiColorVis1;
+    };
+
+    const marker = (type: TTYPlayerLineMarkerType, selected: boolean): CSSObject => ({
       fontSize: 0,
+      overflow: 'hidden',
       position: 'absolute',
       padding: 0,
-      overflow: 'hidden',
       width: 3,
       height: 12,
-      backgroundColor: getMarkerBg(type, selected),
-      border: '2px solid rgb(29, 30, 35)',
-      borderRadius: 4,
+      backgroundColor: getMarkerBackgroundColor(type, selected),
+      border: `${border.width.thick} solid ${euiVars.terminalOutputBackground}`,
+      borderRadius: border.radius.small,
       boxSizing: 'content-box',
       top: 0,
       pointerEvents: 'none',
       marginLeft: '-3.5px',
     });
 
-    const range = css`
-      height: 25px;
-      input[type='range']:focus:not(:focus-visible)::-webkit-slider-thumb {
-        box-shadow: none;
-        background-color: transparent;
-      }
-      input[type='range']::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        cursor: pointer;
-        margin-top: -20px;
-        width: 9px;
-        height: 30px;
-        border: none;
-        box-shadow: none;
-        background-color: transparent;
-        border-radius: 0;
-        margin-left: -4.5px;
-        opacity: 0;
-      }
-      .euiRangeHighlight__progress {
-        background-color: #6dccb1;
-        border-radius: 8px 0px 0px 8px;
-      }
-      .euiRangeSlider:focus:not(:focus-visible) ~ .euiRangeHighlight .euiRangeHighlight__progress {
-        background-color: #6dccb1;
-      }
-      .euiRangeTrack::after {
-        background: #343741;
-      }
-    `;
+    const scrubberThumb: CSSObject = {
+      cursor: 'pointer',
+      marginTop: -20,
+      width: 9,
+      height: 30,
+      marginLeft: -4.5,
+    };
 
-    const scrubber = (type) => css`
-      position: absolute;
-      top: 16px;
-      cursor: pointer;
-      margin-top: -20px;
-      width: 9px;
-      height: 30px;
-      border: none;
-      box-shadow: none;
-      background-color: transparent;
-      border-radius: 0;
-      background-image: url(${type === 'data_limited' ? scrubberYellow : scrubberDefault});
-      margin-left: -4.5px;
-    `;
+    const customThumb: CSSObject = {
+      ...scrubberThumb,
+      border: 'none',
+      boxShadow: 'none',
+      backgroundColor: 'transparent',
+      borderRadius: 0,
+      opacity: 0,
+      appearance: 'none',
+    };
+
+    // Custom css for input type range, overrinding some options of EuiRange
+    // The custom thumb below is visually hidden and shares the same css properties with the scrubber component
+    // Source (Chrome): https://developer.mozilla.org/en-US/docs/Web/CSS/::-webkit-slider-thumb
+    // Source (Firefox): https://developer.mozilla.org/en-US/docs/Web/CSS/::-moz-range-thumb
+    const range: CSSObject = {
+      height: 25,
+      '&:focus:not(:focus-visible)::-webkit-slider-thumb': {
+        boxShadow: 'none',
+        backgroundColor: 'transparent',
+      },
+      "input[type='range']::-webkit-slider-thumb": customThumb,
+      "input[type='range']::-moz-range-thumb": customThumb,
+      '.euiRangeHighlight__progress': {
+        backgroundColor: euiVars.euiColorVis0_behindText,
+      },
+      '.euiRangeSlider:focus:not(:focus-visible) ~ .euiRangeHighlight .euiRangeHighlight__progress':
+        { backgroundColor: euiVars.euiColorVis0_behindText },
+      '.euiRangeTrack::after': {
+        background: euiVars.terminalOutputSliderBackground,
+      },
+    };
+
+    const scrubber = (type: TTYPlayerLineMarkerType): CSSObject => ({
+      ...scrubberThumb,
+      position: 'absolute',
+      top: 16,
+      fill:
+        type === 'data_limited'
+          ? euiVars.terminalOutputMarkerWarning
+          : euiVars.terminalOutputMarkerAccent,
+    });
 
     return {
       marker,
-      wrapper,
+      markersOverlay,
       range,
       scrubber,
     };
