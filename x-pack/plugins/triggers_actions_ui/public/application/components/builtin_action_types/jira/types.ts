914d1cd41c87e34df4144032603b99a281c31ba6
Ying Mao <ying.mao@elastic.co>
2022-09-15 15:50:15 -0400

    [Response Ops] [Connectors] Move connectors to own plugin (#139867)
    * Initial commit of stack connectors plugin
    
    * [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'
    
    * Fixing up xmatters. Moving library functions around
    
    * [CI] Auto-commit changed files from 'node scripts/build_plugin_list_docs'
    
    * Fixing up webhook
    
    * Fixing up teams and slack
    
    * Fixing up index, email, pagerduty, server log
    
    * Fixing i18n
    
    * wip
    
    * Moving well know email route to stack connectors plugin
    
    * Fixing types
    
    * Fixing unit tests
    
    * Adding index.ts
    
    * Cleanup
    
    * Updating READMEs
    
    * [CI] Auto-commit changed files from 'node scripts/build_plugin_list_docs'
    
    * Moving to domain specific folders and updating codeowners
    
    * Fixing codeowners
    
    * Fixing types
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/jira/types.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/components/builtin_action_types/jira/types.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { ExecutorSubActionPushParams } from '@kbn/actions-plugin/server/builtin_action_types/jira/types';
+import type { ExecutorSubActionPushParams } from '@kbn/stack-connectors-plugin/server/connector_types/cases/jira/types';
 import { UserConfiguredActionConnector } from '../../../../types';
 
 export type JiraActionConnector = UserConfiguredActionConnector<JiraConfig, JiraSecrets>;
