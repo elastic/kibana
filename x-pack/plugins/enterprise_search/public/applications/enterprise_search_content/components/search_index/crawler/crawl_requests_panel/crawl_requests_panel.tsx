721a1cd89d1656c3b3a4c05a97dac0351b9927fe
Byron Hulcher <byronhulcher@gmail.com>
2022-09-15 09:38:01 -0400

    [Enterprise Search] Display user agent in crawl requests table (#140777)
    * Display user agent in crawl requests table
    
    * Fix mocks

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/crawler/crawl_requests_panel/crawl_requests_panel.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/crawler/crawl_requests_panel/crawl_requests_panel.tsx
@@ -7,29 +7,58 @@
 
 import React from 'react';
 
+import { useValues } from 'kea';
+
+import { EuiCode, EuiLink, EuiPanel, EuiSpacer, EuiText } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
 import { DataPanel } from '../../../../../shared/data_panel/data_panel';
+import { CrawlerLogic } from '../crawler_logic';
 
 import { CrawlRequestsTable } from './crawl_requests_table';
 
-export const CrawlRequestsPanel: React.FC = () => (
-  <DataPanel
-    hasBorder
-    title={
-      <h2>
-        {i18n.translate('xpack.enterpriseSearch.crawler.crawlRequestsPanel.title', {
-          defaultMessage: 'Crawl requests',
-        })}
-      </h2>
-    }
-    titleSize="s"
-    iconType="documents"
-    subtitle={i18n.translate('xpack.enterpriseSearch.crawler.crawlRequestsPanel.description', {
-      defaultMessage:
-        "Recent crawl requests are logged here. You can track progress and examine crawl events in Kibana's Discover or Logs user intefaces",
-    })}
-  >
-    <CrawlRequestsTable />
-  </DataPanel>
-);
+export const CrawlRequestsPanel: React.FC = () => {
+  const { data } = useValues(CrawlerLogic);
+  return (
+    <DataPanel
+      hasBorder
+      title={
+        <h2>
+          {i18n.translate('xpack.enterpriseSearch.crawler.crawlRequestsPanel.title', {
+            defaultMessage: 'Crawl requests',
+          })}
+        </h2>
+      }
+      titleSize="s"
+      iconType="documents"
+      subtitle={i18n.translate('xpack.enterpriseSearch.crawler.crawlRequestsPanel.description', {
+        defaultMessage:
+          "Recent crawl requests are logged here. You can track progress and examine crawl events in Kibana's Discover or Logs user intefaces",
+      })}
+    >
+      <EuiPanel color="subdued">
+        <EuiText size="s">
+          {i18n.translate(
+            'xpack.enterpriseSearch.crawler.crawlRequestsPanel.userAgentDescription',
+            {
+              defaultMessage:
+                'Requests originating from the crawler can be identified by the following User Agent. This is configured in your enterprise-search.yml file.',
+            }
+          )}{' '}
+          <EuiLink target="_blank" href={'' /* needs docLink */}>
+            {i18n.translate(
+              'xpack.enterpriseSearch.crawler.crawlRequestsPanel.userAgentDocumentationLink',
+              {
+                defaultMessage: 'Learn more about Elastic crawler user agents',
+              }
+            )}
+          </EuiLink>
+        </EuiText>
+        <EuiSpacer size="s" />
+        <EuiCode>{data ? data.userAgent : ''}</EuiCode>
+      </EuiPanel>
+      <EuiSpacer />
+      <CrawlRequestsTable />
+    </DataPanel>
+  );
+};
