a6aecc280c9e657082ac8f5453e5feecb7f0526c
Dima Arnautov <dmitrii.arnautov@elastic.co>
2022-09-15 10:56:17 +0200

    remove storage code

--- x-pack/plugins/ml/public/application/notifications/page.tsx
+++ x-pack/plugins/ml/public/application/notifications/page.tsx
@@ -5,31 +5,20 @@
  * 2.0.
  */
 
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { NotificationsList } from './components/notifications_list';
-import { ML_NOTIFICATIONS_LAST_CHECKED_AT } from '../../../common/types/storage';
 import { useMlKibana, useTimefilter } from '../contexts/kibana';
 import { MlPageHeader } from '../components/page_header';
 import { NodeAvailableWarning } from '../components/node_available_warning';
 import { UpgradeWarning } from '../components/upgrade';
 import { HelpMenu } from '../components/help_menu';
-import { useStorage } from '../contexts/ml/use_storage';
 
 export const NotificationsPage: FC = () => {
   const {
     services: { docLinks },
   } = useMlKibana();
   const helpLink = docLinks.links.ml.guide;
-
-  const [, setNotificationLastCheckedAt] = useStorage(ML_NOTIFICATIONS_LAST_CHECKED_AT);
-
-  useEffect(function updateLastChecked() {
-    setNotificationLastCheckedAt(Date.now());
-    return () => {
-      setNotificationLastCheckedAt(Date.now());
-    };
-  }, []);
 
   useTimefilter({ timeRangeSelector: true, autoRefreshSelector: true });
 
