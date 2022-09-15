3669be513f6d0163606fc1d80fbb03c3b2e45dee
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-09-15 12:16:52 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/test/spaces_api_integration/common/suites/resolve_copy_to_space_conflicts.ts
+++ x-pack/test/spaces_api_integration/common/suites/resolve_copy_to_space_conflicts.ts
@@ -11,8 +11,8 @@ import { DEFAULT_SPACE_ID } from '@kbn/spaces-plugin/common/constants';
 import { CopyResponse } from '@kbn/spaces-plugin/server/lib/copy_to_spaces';
 import { getUrlPrefix } from '../lib/space_test_utils';
 import { DescribeFn, TestDefinitionAuthentication } from '../lib/types';
-import {FtrProviderContext} from "../ftr_provider_context";
-import {getTestDataLoader} from "../lib/test_data_loader";
+import { FtrProviderContext } from '../ftr_provider_context';
+import { getTestDataLoader } from '../lib/test_data_loader';
 
 type TestResponse = Record<string, any>;
 
