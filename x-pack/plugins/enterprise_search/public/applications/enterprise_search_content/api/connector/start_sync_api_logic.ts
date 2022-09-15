a440e219bc40ea0dbef91f277b0a1aade68cbc34
Dmitrii Burlutckii <dmitrii.burlutckii@elastic.co>
2022-09-15 18:20:15 +0200

    More detailed comment

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/api/connector/start_sync_api_logic.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/api/connector/start_sync_api_logic.ts
@@ -17,7 +17,8 @@ export interface StartSyncArgs {
 export const startSync = async ({ connectorId, nextSyncConfig }: StartSyncArgs) => {
   const route = `/internal/enterprise_search/connectors/${connectorId}/start_sync`;
   return await HttpLogic.values.http.post(route, {
-    // nextSyncConfig will be stored as a JSON string
+    // ConnectorConfiguration type is a record of key-value pair where value is a string
+    // To store nextSyncConfig into ConnectorConfiguration the object should be casted to string
     body: JSON.stringify({ nextSyncConfig: JSON.stringify(nextSyncConfig) }),
   });
 };
