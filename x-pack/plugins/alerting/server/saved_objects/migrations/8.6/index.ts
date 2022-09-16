bbfc35a1bf9152f7415e596e49f7b90bb1651820
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-09-16 17:24:14 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/alerting/server/saved_objects/migrations/8.6/index.ts
+++ x-pack/plugins/alerting/server/saved_objects/migrations/8.6/index.ts
@@ -4,10 +4,3 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-import { SavedObjectMigrationContext, SavedObjectUnsanitizedDoc } from "@kbn/core-saved-objects-server";
-import { EncryptedSavedObjectsPluginSetup } from '@kbn/encrypted-saved-objects-plugin/server';
-import { isSerializedSearchSource } from '@kbn/data-plugin/common';
-import { pick } from "lodash";
-import { RawRule } from "../../../types";
-import { createEsoMigration, isEsQueryRuleType, pipeMigrations } from "../utils";
