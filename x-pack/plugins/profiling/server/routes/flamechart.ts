d503d4a5654e18cb9c3c6da4369466547ee6b365
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-09-17 16:33:00 +0200

    Pprof profiling

--- x-pack/plugins/profiling/server/routes/flamechart.ts
+++ x-pack/plugins/profiling/server/routes/flamechart.ts
@@ -6,6 +6,10 @@
  */
 
 import { schema } from '@kbn/config-schema';
+import * as _PProf from 'pprof';
+import fs from 'fs';
+import path from 'path';
+import execa from 'execa';
 import { RouteRegisterParameters } from '.';
 import { getRoutePaths } from '../../common';
 import { createCalleeTree } from '../../common/callee';
@@ -14,6 +18,39 @@ import { withProfilingSpan } from '../utils/with_profiling_span';
 import { getClient } from './compat';
 import { getExecutablesAndStackTraces } from './get_executables_and_stacktraces';
 import { createCommonFilter } from './query';
+
+const pprof: typeof _PProf = require('pprof');
+
+function withPprof<T>(fn: () => Promise<T>): Promise<T> {
+  const stop = pprof.time.start();
+  return fn().then((response) => {
+    const profile = stop();
+    pprof.encode(profile).then((buffer) => {
+      const dir = path.join(__dirname, '../../../../../profiles');
+
+      const filename = path.join(dir, new Date().getTime() + '.pb.gz');
+
+      const folderExists = fs.existsSync(dir);
+
+      if (!folderExists) {
+        fs.mkdirSync(dir);
+      }
+
+      fs.writeFile(filename, buffer, (err) => {
+        if (err) {
+          console.log('Error writing file');
+          console.log(err);
+          return;
+        }
+
+        console.log('Wrote profile to disk');
+
+        execa('pprof', ['-web', filename]);
+      });
+    });
+    return response;
+  });
+}
 
 export function registerFlameChartSearchRoute({
   router,
@@ -33,78 +70,80 @@ export function registerFlameChartSearchRoute({
       },
     },
     async (context, request, response) => {
-      const { timeFrom, timeTo, kuery } = request.query;
-      const targetSampleSize = 20000; // minimum number of samples to get statistically sound results
+      return withPprof(async () => {
+        const { timeFrom, timeTo, kuery } = request.query;
+        const targetSampleSize = 20000; // minimum number of samples to get statistically sound results
 
-      try {
-        const esClient = await getClient(context);
-        const filter = createCommonFilter({
-          timeFrom,
-          timeTo,
-          kuery,
-        });
-        const totalSeconds = timeTo - timeFrom;
+        try {
+          const esClient = await getClient(context);
+          const filter = createCommonFilter({
+            timeFrom,
+            timeTo,
+            kuery,
+          });
+          const totalSeconds = timeTo - timeFrom;
 
-        const {
-          stackTraces,
-          executables,
-          stackFrames,
-          eventsIndex,
-          totalCount,
-          totalFrames,
-          stackTraceEvents,
-        } = await getExecutablesAndStackTraces({
-          logger,
-          client: createProfilingEsClient({ request, esClient }),
-          filter,
-          sampleSize: targetSampleSize,
-        });
-
-        const flamegraph = await withProfilingSpan('create_flamegraph', async () => {
-          const t0 = Date.now();
-          const tree = createCalleeTree(
-            stackTraceEvents,
+          const {
             stackTraces,
-            stackFrames,
             executables,
-            totalFrames
-          );
-          logger.info(`creating callee tree took ${Date.now() - t0} ms`);
+            stackFrames,
+            eventsIndex,
+            totalCount,
+            totalFrames,
+            stackTraceEvents,
+          } = await getExecutablesAndStackTraces({
+            logger,
+            client: createProfilingEsClient({ request, esClient }),
+            filter,
+            sampleSize: targetSampleSize,
+          });
 
-          // sampleRate is 1/5^N, with N being the downsampled index the events were fetched from.
-          // N=0: full events table (sampleRate is 1)
-          // N=1: downsampled by 5 (sampleRate is 0.2)
-          // ...
+          const flamegraph = await withProfilingSpan('create_flamegraph', async () => {
+            const t0 = Date.now();
+            const tree = createCalleeTree(
+              stackTraceEvents,
+              stackTraces,
+              stackFrames,
+              executables,
+              totalFrames
+            );
+            logger.info(`creating callee tree took ${Date.now() - t0} ms`);
 
-          // totalCount is the sum(Count) of all events in the filter range in the
-          // downsampled index we were looking at.
-          // To estimate how many events we have in the full events index: totalCount / sampleRate.
-          // Do the same for single entries in the events array.
+            // sampleRate is 1/5^N, with N being the downsampled index the events were fetched from.
+            // N=0: full events table (sampleRate is 1)
+            // N=1: downsampled by 5 (sampleRate is 0.2)
+            // ...
 
-          const t1 = Date.now();
-          const fg = createFlameGraph(
-            tree,
-            totalSeconds,
-            Math.floor(totalCount / eventsIndex.sampleRate),
-            totalCount
-          );
-          logger.info(`creating flamegraph took ${Date.now() - t1} ms`);
+            // totalCount is the sum(Count) of all events in the filter range in the
+            // downsampled index we were looking at.
+            // To estimate how many events we have in the full events index: totalCount / sampleRate.
+            // Do the same for single entries in the events array.
 
-          return fg;
-        });
+            const t1 = Date.now();
+            const fg = createFlameGraph(
+              tree,
+              totalSeconds,
+              Math.floor(totalCount / eventsIndex.sampleRate),
+              totalCount
+            );
+            logger.info(`creating flamegraph took ${Date.now() - t1} ms`);
 
-        logger.info('returning payload response to client');
+            return fg;
+          });
 
-        return response.ok({ body: flamegraph });
-      } catch (e) {
-        logger.error(e);
-        return response.customError({
-          statusCode: e.statusCode ?? 500,
-          body: {
-            message: e.message,
-          },
-        });
-      }
+          logger.info('returning payload response to client');
+
+          return response.ok({ body: flamegraph });
+        } catch (e) {
+          logger.error(e);
+          return response.customError({
+            statusCode: e.statusCode ?? 500,
+            body: {
+              message: e.message,
+            },
+          });
+        }
+      });
     }
   );
 }
