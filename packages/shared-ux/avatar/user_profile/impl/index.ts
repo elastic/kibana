ff8f30d752b6d1549528ea9bae3242c73440152c
Rachel Shen <rshen@elastic.co>
2022-09-19 13:18:31 -0600

    [Shared UX] Migrate avatar user profile component from packages/kbn-user-profile-components to Shared UX (#139247)

---
+++ packages/shared-ux/avatar/user_profile/impl/index.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export type { UserAvatarProps, UserProfileWithAvatar } from './user_avatar';
+export type { UserProfilesSelectableProps } from './user_profiles_selectable';
+export type { UserProfilesPopoverProps } from './user_profiles_popover';
+export { UserAvatar } from './user_avatar';
+export { UserProfilesSelectable } from './user_profiles_selectable';
+export { UserProfilesPopover } from './user_profiles_popover';
+export type { UserProfile, UserProfileUserInfo, UserProfileAvatarData } from './user_profile';
