a7a44c825eb6900bcc34991f6fec877ad7441e01
Søren Louv-Jansen <soren.louv@elastic.co>
2022-09-19 15:04:08 +0200

    Remove watch

--- src/dev/typescript/run_type_check_cli.ts
+++ src/dev/typescript/run_type_check_cli.ts
@@ -68,7 +68,6 @@ export async function runTypeCheckCli() {
         ...['--emitDeclarationOnly', 'false'],
         '--noEmit',
         '--pretty',
-        '--watch',
         ...(flags['skip-lib-check']
           ? ['--skipLibCheck', flags['skip-lib-check'] as string]
           : ['--skipLibCheck', 'false']),
