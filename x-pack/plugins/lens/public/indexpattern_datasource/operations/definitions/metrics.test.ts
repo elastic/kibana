3a4e613b631125ef641414a2cf660854e50a22fe
Andrew Tate <andrew.tate@elastic.co>
2022-09-19 13:50:15 -0500

    test collapsing duplicate filtered aggs

--- x-pack/plugins/lens/public/indexpattern_datasource/operations/definitions/metrics.test.ts
+++ x-pack/plugins/lens/public/indexpattern_datasource/operations/definitions/metrics.test.ts
@@ -9,12 +9,13 @@ import {
   buildExpression,
   buildExpressionFunction,
   ExpressionAstExpressionBuilder,
+  parseExpression,
 } from '@kbn/expressions-plugin/common';
 import { OriginalColumn } from '../../to_expression';
 import { operationDefinitionMap } from '.';
 
 const medianOperation = operationDefinitionMap.median;
-const standardDeviationOperation = operationDefinitionMap.standard_deviation;
+const sumOperation = operationDefinitionMap.sum;
 
 describe('metrics', () => {
   describe('optimizeEsAggs', () => {
@@ -156,9 +157,129 @@ describe('metrics', () => {
       expect(newAggs[4].functions[0].getArgument('timeShift')![0]).toBe(timeShift2);
       expect(newAggs[4].functions[0].getArgument('emptyAsNull')).toBeUndefined();
 
-      expect(newAggs).toMatchSnapshot();
+      expect(newIdMap).toMatchInlineSnapshot(`
+        Object {
+          "col-0-0": Array [
+            Object {
+              "id": "original-0",
+            },
+            Object {
+              "id": "original-1",
+            },
+          ],
+          "col-2-2": Array [
+            Object {
+              "id": "original-2",
+            },
+            Object {
+              "id": "original-3",
+            },
+          ],
+          "col-4-4": Array [
+            Object {
+              "id": "original-4",
+            },
+            Object {
+              "id": "original-5",
+            },
+          ],
+          "col-6-6": Array [
+            Object {
+              "id": "original-6",
+            },
+            Object {
+              "id": "original-7",
+            },
+          ],
+          "col-8-8": Array [
+            Object {
+              "id": "original-8",
+            },
+            Object {
+              "id": "original-9",
+            },
+          ],
+        }
+      `);
 
-      expect(newIdMap).toMatchSnapshot();
+      expect(newAggs).toMatchSnapshot();
+    });
+
+    it('should collapse filtered aggs with matching parameters', () => {
+      const aggs = [
+        // group 1
+        'aggSum id="0" enabled=true schema="metric" field="bytes" emptyAsNull=false',
+        'aggSum id="1" enabled=true schema="metric" field="bytes" emptyAsNull=false',
+        // group 2
+        'aggFilteredMetric id="2" enabled=true schema="metric" \n  customBucket={aggFilter id="2-filter" enabled=true schema="bucket" filter={kql q="geo.dest: \\"GA\\" "}} \n  customMetric={aggSum id="2-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+        'aggFilteredMetric id="3" enabled=true schema="metric" \n  customBucket={aggFilter id="3-filter" enabled=true schema="bucket" filter={kql q="geo.dest: \\"GA\\" "}} \n  customMetric={aggSum id="3-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+        // group 3
+        'aggFilteredMetric id="4" enabled=true schema="metric" \n  customBucket={aggFilter id="4-filter" enabled=true schema="bucket" filter={kql q="geo.dest: \\"AL\\" "}} \n  customMetric={aggSum id="4-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+        'aggFilteredMetric id="5" enabled=true schema="metric" \n  customBucket={aggFilter id="5-filter" enabled=true schema="bucket" filter={kql q="geo.dest: \\"AL\\" "}} \n  customMetric={aggSum id="5-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+        // group 4
+        'aggFilteredMetric id="6" enabled=true schema="metric" \n  customBucket={aggFilter id="6-filter" enabled=true schema="bucket" filter={lucene q="\\"geo.dest: \\\\\\"AL\\\\\\" \\""}} \n  customMetric={aggSum id="6-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+        'aggFilteredMetric id="7" enabled=true schema="metric" \n  customBucket={aggFilter id="7-filter" enabled=true schema="bucket" filter={lucene q="\\"geo.dest: \\\\\\"AL\\\\\\" \\""}} \n  customMetric={aggSum id="7-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+        // group 5
+        'aggFilteredMetric id="8" enabled=true schema="metric" \n  customBucket={aggFilter id="8-filter" enabled=true schema="bucket" filter={kql q="geo.dest: \\"AL\\" "} timeWindow="1m"} \n  customMetric={aggSum id="8-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+        'aggFilteredMetric id="9" enabled=true schema="metric" \n  customBucket={aggFilter id="9-filter" enabled=true schema="bucket" filter={kql q="geo.dest: \\"AL\\" "} timeWindow="1m"} \n  customMetric={aggSum id="9-metric" enabled=true schema="metric" field="bytes" emptyAsNull=false}',
+      ].map((expression) => buildExpression(parseExpression(expression)));
+
+      const { esAggsIdMap, aggsToIdsMap } = buildMapsFromAggBuilders(aggs);
+
+      const { esAggsIdMap: newIdMap, aggs: newAggs } = sumOperation.optimizeEsAggs!(
+        aggs,
+        esAggsIdMap,
+        aggsToIdsMap
+      );
+
+      expect(newAggs.length).toBe(5);
+
+      expect(newIdMap).toMatchInlineSnapshot(`
+        Object {
+          "col-0-0": Array [
+            Object {
+              "id": "original-0",
+            },
+            Object {
+              "id": "original-1",
+            },
+          ],
+          "col-2-2": Array [
+            Object {
+              "id": "original-2",
+            },
+            Object {
+              "id": "original-3",
+            },
+          ],
+          "col-4-4": Array [
+            Object {
+              "id": "original-4",
+            },
+            Object {
+              "id": "original-5",
+            },
+          ],
+          "col-6-6": Array [
+            Object {
+              "id": "original-6",
+            },
+            Object {
+              "id": "original-7",
+            },
+          ],
+          "col-8-8": Array [
+            Object {
+              "id": "original-8",
+            },
+            Object {
+              "id": "original-9",
+            },
+          ],
+        }
+      `);
+
+      expect(newAggs).toMatchSnapshot();
     });
 
     it("shouldn't touch unrelated aggs or aggs with no siblings", () => {
