352a14814e3dabfc7ea923a66b4734b0d91974e8
Alejandro Fernández Haro <afharo@gmail.com>
2022-09-20 00:16:56 +0200

    Update x-pack/plugins/cloud_integrations/cloud_experiments/common/types.ts
    Co-authored-by: Luke Elmers <lukeelmers@gmail.com>

--- x-pack/plugins/cloud_integrations/cloud_experiments/common/types.ts
+++ x-pack/plugins/cloud_integrations/cloud_experiments/common/types.ts
@@ -26,7 +26,9 @@ export interface CloudExperimentsPluginSetup {
    * Fetch the configuration assigned to variation `configKey`. If nothing is found, fallback to `defaultValue`.
    * @param featureFlagName The name of the key to find the config variation. {@link CloudExperimentsFeatureFlagNames}.
    * @param defaultValue The fallback value in case no variation is found.
-   */
+ *
+ * @public
+ */
   getVariation: <Data>(
     featureFlagName: CloudExperimentsFeatureFlagNames,
     defaultValue: Data
