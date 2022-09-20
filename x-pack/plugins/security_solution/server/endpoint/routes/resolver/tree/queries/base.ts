6d6ba26de22cfad0e6006648c2bd31610eae3a8e
Kevin Qualters <56408403+kqualters-elastic@users.noreply.github.com>
2022-09-20 10:29:45 -0400

    [Security Solution] Make timerange an optional request param, fire unbounded request when 0 results (#140831)
    * Make timerange an optional request param, fire unbounded request when 0 results
    
    * WIP working hook, request running twice sometimes
    
    * Add cypress test, update reducer/selector tests, fix types
    
    * Remove unneeded ternary

---
+++ x-pack/plugins/security_solution/server/endpoint/routes/resolver/tree/queries/base.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { JsonValue } from '@kbn/utility-types';
+import type { ResolverSchema } from '../../../../../../common/endpoint/types';
+import type { TimeRange } from '../utils';
+import { resolverFields } from '../utils';
+
+export interface ResolverQueryParams {
+  readonly schema: ResolverSchema;
+  readonly indexPatterns: string | string[];
+  readonly timeRange: TimeRange | undefined;
+  readonly isInternalRequest: boolean;
+  readonly resolverFields?: JsonValue[];
+  getRangeFilter?: () => Array<{
+    range: { '@timestamp': { gte: string; lte: string; format: string } };
+  }>;
+}
+
+export class BaseResolverQuery implements ResolverQueryParams {
+  readonly schema: ResolverSchema;
+  readonly indexPatterns: string | string[];
+  readonly timeRange: TimeRange | undefined;
+  readonly isInternalRequest: boolean;
+  readonly resolverFields?: JsonValue[];
+
+  constructor({ schema, indexPatterns, timeRange, isInternalRequest }: ResolverQueryParams) {
+    this.resolverFields = resolverFields(schema);
+    this.schema = schema;
+    this.indexPatterns = indexPatterns;
+    this.timeRange = timeRange;
+    this.isInternalRequest = isInternalRequest;
+  }
+
+  getRangeFilter() {
+    return this.timeRange
+      ? [
+          {
+            range: {
+              '@timestamp': {
+                gte: this.timeRange.from,
+                lte: this.timeRange.to,
+                format: 'strict_date_optional_time',
+              },
+            },
+          },
+        ]
+      : [];
+  }
+}
