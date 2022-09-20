ee801f5cb9f949a3ce04c53910c7c449285aa483
Candace Park <candace.park@elastic.co>
2022-09-19 23:09:48 -0400

    remove merge tags

--- x-pack/plugins/security_solution/public/management/components/console/components/command_input/components/input_capture.tsx
+++ x-pack/plugins/security_solution/public/management/components/console/components/command_input/components/input_capture.tsx
@@ -130,7 +130,6 @@ export const InputCapture = memo<InputCaptureProps>(
 
     const handleOnKeyDown = useCallback<KeyboardEventHandler>(
       (ev) => {
-<<<<<<< HEAD
         // handles the ctrl + a select and allows for clipboard events to be captured via onPaste event handler
         if (ev.metaKey || ev.ctrlKey) {
           if (ev.key === 'a') {
