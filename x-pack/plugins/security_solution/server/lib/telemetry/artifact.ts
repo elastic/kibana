ba84e999d247b333a400c1352818502a91516506
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-09-20 14:35:45 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/security_solution/server/lib/telemetry/artifact.ts
+++ x-pack/plugins/security_solution/server/lib/telemetry/artifact.ts
@@ -1,10 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
 
-
-import axios from "axios";
-import { ITelemetryReceiver } from "./receiver";
-import { ESClusterInfo } from "./types";
+import axios from 'axios';
 import { AdmZip } from 'adm-zip';
-
+import type { ITelemetryReceiver } from './receiver';
+import type { ESClusterInfo } from './types';
 
 export interface IArtifact {
   start(receiver: ITelemetryReceiver): Promise<void>;
@@ -14,7 +18,7 @@ export interface IArtifact {
 class Artifact implements IArtifact {
   private manifestUrl?: string;
   private readonly PROD_CDN_URL = 'https://artifacts.security.elastic.co';
-  //private readonly STAGING_CDN_URL = 'https://artifacts.security.elastic.co/downloads/endpoint/manifest/artifacts-8.4.0.zip';
+  // private readonly STAGING_CDN_URL = 'https://artifacts.security.elastic.co/downloads/endpoint/manifest/artifacts-8.4.0.zip';
   private readonly AXIOS_TIMEOUT_MS = 10_000;
   private receiver?: ITelemetryReceiver;
   private esClusterInfo?: ESClusterInfo;
@@ -26,15 +30,17 @@ class Artifact implements IArtifact {
     this.manifestUrl = `${this.PROD_CDN_URL}/downloads/kibana/manifest/artifacts-${version}.zip`;
   }
 
-
   public async getArtifact(name: string): Promise<unknown> {
     try {
       if (this.manifestUrl) {
-        const response = await axios.get(this.manifestUrl, { timeout: this.AXIOS_TIMEOUT_MS, responseType: 'arraybuffer' });
+        const response = await axios.get(this.manifestUrl, {
+          timeout: this.AXIOS_TIMEOUT_MS,
+          responseType: 'arraybuffer',
+        });
         const zip = new AdmZip(response.data);
         const entries = zip.getEntries();
         const manifest = JSON.parse(entries[0].getData().toString());
-        const relativeUrl = manifest['artifacts'][name]['relative_url'];
+        const relativeUrl = manifest.artifacts[name].relative_url;
         if (relativeUrl) {
           const url = `${this.PROD_CDN_URL}/${relativeUrl}`;
           const artifactResponse = await axios.get(url, { timeout: this.AXIOS_TIMEOUT_MS });
@@ -50,10 +56,6 @@ class Artifact implements IArtifact {
       throw err;
     }
   }
-
-
-
-
 }
 
 export const artifactService = new Artifact();
