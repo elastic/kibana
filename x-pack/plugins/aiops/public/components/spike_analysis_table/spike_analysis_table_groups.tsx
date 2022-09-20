6ec00a56454ceaf14d78b476bda2a3772db8dade
Melissa Alvarez <melissa.alvarez@elastic.co>
2022-09-20 11:37:31 -0600

    [ML] Explain Log Rate Spikes: merge duplicate spikeAnalysisTable components (#141116)
    * merge duplicate spikeAnalysisTable components
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/aiops/public/components/spike_analysis_table/spike_analysis_table_groups.tsx
+++ x-pack/plugins/aiops/public/components/spike_analysis_table/spike_analysis_table_groups.tsx
@@ -25,7 +25,7 @@ import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import type { ChangePoint } from '@kbn/ml-agg-utils';
 import { useEuiTheme } from '../../hooks/use_eui_theme';
-import { SpikeAnalysisTableExpandedRow } from './spike_analysis_table_expanded_row';
+import { SpikeAnalysisTable } from './spike_analysis_table';
 
 const NARROW_COLUMN_WIDTH = '120px';
 const EXPAND_COLUMN_WIDTH = '40px';
@@ -100,7 +100,7 @@ export const SpikeAnalysisGroupsTable: FC<SpikeAnalysisTableProps> = ({
       }
 
       itemIdToExpandedRowMapValues[item.id] = (
-        <SpikeAnalysisTableExpandedRow
+        <SpikeAnalysisTable
           changePoints={expandedTableItems as ChangePoint[]}
           loading={loading}
           onPinnedChangePoint={onPinnedChangePoint}
@@ -127,7 +127,21 @@ export const SpikeAnalysisGroupsTable: FC<SpikeAnalysisTableProps> = ({
         <EuiButtonIcon
           data-test-subj={'aiopsSpikeAnalysisGroupsTableRowExpansionButton'}
           onClick={() => toggleDetails(item)}
-          aria-label={itemIdToExpandedRowMap[item.id] ? 'Collapse' : 'Expand'}
+          aria-label={
+            itemIdToExpandedRowMap[item.id]
+              ? i18n.translate(
+                  'xpack.aiops.explainLogRateSpikes.spikeAnalysisTable.collapseAriaLabel',
+                  {
+                    defaultMessage: 'Collapse',
+                  }
+                )
+              : i18n.translate(
+                  'xpack.aiops.explainLogRateSpikes.spikeAnalysisTable.expandAriaLabel',
+                  {
+                    defaultMessage: 'Expand',
+                  }
+                )
+          }
           iconType={itemIdToExpandedRowMap[item.id] ? 'arrowUp' : 'arrowDown'}
         />
       ),
