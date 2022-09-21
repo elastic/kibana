581598c67090bbc32157f0fb96fe345dc615c704
Karl Godard <karl.godard@elastic.co>
2022-09-20 19:06:09 -0700

    [Terminal Output] tty toggle now shows number of bytes in tooltip, if no output we disa… (#141174)
    * tty toggle now shows number of bytes in tooltip, if no output we disable button (instead of hide)
    
    * fixed tests
    
    Co-authored-by: Karl Godard <karlgodard@elastic.co>

--- x-pack/plugins/session_view/public/components/tty_player/styles.ts
+++ x-pack/plugins/session_view/public/components/tty_player/styles.ts
@@ -38,6 +38,7 @@ export const useStyles = (tty?: Teletype, show?: boolean) => {
     };
 
     const header: CSSObject = {
+      display: show ? 'block' : 'none',
       backgroundColor: `${euiVars.euiFormBackgroundDisabledColor}`,
       padding: `${size.m} ${size.base}`,
     };
@@ -73,7 +74,7 @@ export const useStyles = (tty?: Teletype, show?: boolean) => {
     const scrollPane: CSSObject = {
       position: 'relative',
       transform: `translateY(${show ? 0 : '100%'})`,
-      transition: 'transform .2s',
+      transition: 'transform .2s ease-in-out',
       width: '100%',
       height: 'calc(100% - 112px)',
       overflow: 'auto',
