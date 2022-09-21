7c98f82f5f690ea16c09734935f86a8cb114ddcb
Luke Gmys <lgmys@users.noreply.github.com>
2022-09-21 13:40:58 +0200

    [TIP] Add inspect capability for barchart and grid requests (#140810)

--- x-pack/plugins/threat_intelligence/public/common/mocks/test_providers.tsx
+++ x-pack/plugins/threat_intelligence/public/common/mocks/test_providers.tsx
@@ -16,6 +16,7 @@ import { Storage } from '@kbn/kibana-utils-plugin/public';
 import { unifiedSearchPluginMock } from '@kbn/unified-search-plugin/public/mocks';
 import { createTGridMocks } from '@kbn/timelines-plugin/public/mock';
 import { EuiThemeProvider } from '@kbn/kibana-react-plugin/common';
+import { RequestAdapter } from '@kbn/inspector-plugin/common';
 import { KibanaContext } from '../../hooks/use_kibana';
 import { SecuritySolutionPluginContext } from '../../types';
 import { getSecuritySolutionContextMock } from './mock_security_context';
@@ -25,6 +26,7 @@ import { IndicatorsFiltersContext } from '../../modules/indicators/context';
 import { mockIndicatorsFiltersContext } from './mock_indicators_filters_context';
 import { FieldTypesContext } from '../../containers/field_types_provider';
 import { generateFieldTypeMap } from './mock_field_type_map';
+import { InspectorContext } from '../../containers/inspector';
 
 export const localStorageMock = (): IStorage => {
   let store: Record<string, unknown> = {};
@@ -125,19 +127,21 @@ export const mockedServices = {
 };
 
 export const TestProvidersComponent: FC = ({ children }) => (
-  <FieldTypesContext.Provider value={generateFieldTypeMap()}>
-    <EuiThemeProvider>
-      <SecuritySolutionContext.Provider value={mockSecurityContext}>
-        <KibanaContext.Provider value={{ services: mockedServices } as any}>
-          <I18nProvider>
-            <IndicatorsFiltersContext.Provider value={mockIndicatorsFiltersContext}>
-              {children}
-            </IndicatorsFiltersContext.Provider>
-          </I18nProvider>
-        </KibanaContext.Provider>
-      </SecuritySolutionContext.Provider>
-    </EuiThemeProvider>
-  </FieldTypesContext.Provider>
+  <InspectorContext.Provider value={{ requests: new RequestAdapter() }}>
+    <FieldTypesContext.Provider value={generateFieldTypeMap()}>
+      <EuiThemeProvider>
+        <SecuritySolutionContext.Provider value={mockSecurityContext}>
+          <KibanaContext.Provider value={{ services: mockedServices } as any}>
+            <I18nProvider>
+              <IndicatorsFiltersContext.Provider value={mockIndicatorsFiltersContext}>
+                {children}
+              </IndicatorsFiltersContext.Provider>
+            </I18nProvider>
+          </KibanaContext.Provider>
+        </SecuritySolutionContext.Provider>
+      </EuiThemeProvider>
+    </FieldTypesContext.Provider>
+  </InspectorContext.Provider>
 );
 
 export type MockedSearch = jest.Mocked<typeof mockedServices.data.search>;
