d07e55378d0cc9d1200cd84f4f46934f43972f31
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-09-21 15:30:26 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/test/api_integration/apis/uptime/rest/common.ts
+++ x-pack/test/api_integration/apis/uptime/rest/common.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 import { SimpleSavedObject } from '@kbn/core-saved-objects-api-browser';
 import { MonitorFields } from '@kbn/synthetics-plugin/common/runtime_types';
 import { SuperTest, Test } from 'supertest';
