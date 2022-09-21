d1f5f97b9b006288251cf5f5ed59e82932f8e3e7
Devon Thomson <devon.thomson@elastic.co>
2022-09-21 14:27:16 -0400

    import viewMode from embeddable common

--- src/plugins/dashboard/public/dashboard_constants.ts
+++ src/plugins/dashboard/public/dashboard_constants.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import { ViewMode } from '@kbn/embeddable-plugin/public';
+import { ViewMode } from '@kbn/embeddable-plugin/common';
 import type { DashboardState } from './types';
 
 export const DASHBOARD_STATE_STORAGE_KEY = '_a';
