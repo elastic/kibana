12d17e2c795c5a0e5676245b0c8152c6bbbe8c13
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-09-21 13:54:59 +0300

    Added tests for getValidColumns.

--- src/plugins/visualizations/common/convert_to_lens/lib/utils.test.ts
+++ src/plugins/visualizations/common/convert_to_lens/lib/utils.test.ts
@@ -6,9 +6,11 @@
  * Side Public License, v 1.
  */
 
+import { stubLogstashDataView } from '@kbn/data-views-plugin/common/data_view.stub';
+import { AggBasedColumn, Operations } from '../..';
 import { METRIC_TYPES } from '@kbn/data-plugin/common';
 import { SchemaConfig } from '../../types';
-import { getLabel, getLabelForPercentile } from './utils';
+import { getLabel, getLabelForPercentile, getValidColumns } from './utils';
 
 describe('getLabel', () => {
   const label = 'some label';
@@ -70,5 +72,44 @@ describe('getLabelForPercentile', () => {
     const aggParams = { ...agg.aggParams!, customLabel };
     const aggWithCustomLabel = { ...agg, aggParams };
     expect(getLabelForPercentile(aggWithCustomLabel)).toEqual(label);
+  });
+});
+
+describe('getValidColumns', () => {
+  const dataView = stubLogstashDataView;
+  const columns: AggBasedColumn[] = [
+    {
+      operationType: Operations.AVERAGE,
+      sourceField: dataView.fields[0].name,
+      columnId: 'some-id-0',
+      dataType: 'number',
+      params: {},
+      meta: { aggId: 'aggId-0' },
+      isSplit: false,
+      isBucketed: true,
+    },
+    {
+      operationType: Operations.SUM,
+      sourceField: dataView.fields[0].name,
+      columnId: 'some-id-1',
+      dataType: 'number',
+      params: {},
+      meta: { aggId: 'aggId-1' },
+      isSplit: false,
+      isBucketed: true,
+    },
+  ];
+  test.each<[string, Parameters<typeof getValidColumns>, AggBasedColumn[] | null]>([
+    ['null if array contains null', [[null, ...columns]], null],
+    ['null if columns is null', [null], null],
+    ['null if columns is undefined', [undefined], null],
+    ['columns', [columns], columns],
+    ['columns if one column is passed', [columns[0]], [columns[0]]],
+  ])('should return %s', (_, input, expected) => {
+    if (expected === null) {
+      expect(getValidColumns(...input)).toBeNull();
+    } else {
+      expect(getValidColumns(...input)).toEqual(expect.objectContaining(expected));
+    }
   });
 });
