2100ee804e297ce321def0d5c1f034bb658e06b7
cauemarcondes <caue.marcondes@elastic.co>
2022-09-22 11:46:46 -0400

    adding telemetry track

--- x-pack/plugins/apm/public/components/shared/apm_header_action_menu/labs/labs_flyout.tsx
+++ x-pack/plugins/apm/public/components/shared/apm_header_action_menu/labs/labs_flyout.tsx
@@ -54,7 +54,7 @@ export function LabsFlyout({ onClose }: Props) {
         return settingsEditableConfig[key].requiresPageReload;
       });
 
-      await saveAll();
+      await saveAll({ trackMetricName: 'labs_save' });
 
       if (reloadPage) {
         window.location.reload();
