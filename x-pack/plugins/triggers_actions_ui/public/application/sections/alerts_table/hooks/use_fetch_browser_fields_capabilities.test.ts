b26568c529f4ec81210e2567139101cc32a3b84d
Julian Gernun <julian.gernun@elastic.co>
2022-09-22 15:31:03 +0200

    add test

---
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/hooks/use_fetch_browser_fields_capabilities.test.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { renderHook } from '@testing-library/react-hooks';
+import { useFetchBrowserFieldCapabilities } from './use_fetch_browser_fields_capabilities';
+import { useKibana } from '../../../../common/lib/kibana';
+
+jest.mock('../../../../common/lib/kibana');
+
+describe('useFetchBrowserFieldCapabilities', () => {
+  let httpMock: jest.Mock;
+
+  beforeEach(() => {
+    httpMock = useKibana().services.http.get as jest.Mock;
+  });
+
+  afterEach(() => {
+    httpMock.mockReset();
+  });
+
+  it('should not fetch for siem', () => {
+    const { result } = renderHook(() => useFetchBrowserFieldCapabilities({ featureIds: ['siem'] }));
+
+    expect(httpMock).toHaveBeenCalledTimes(0);
+    expect(result.current).toEqual([undefined, {}]);
+  });
+});
