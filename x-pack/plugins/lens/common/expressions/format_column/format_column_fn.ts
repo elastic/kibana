bb8140074fabe1f04966a07c6f16446b4051ebe3
Joe Reuter <johannes.reuter@elastic.co>
2022-09-22 16:07:02 +0200

    make sure to set format id correctly

--- x-pack/plugins/lens/common/expressions/format_column/format_column_fn.ts
+++ x-pack/plugins/lens/common/expressions/format_column/format_column_fn.ts
@@ -64,7 +64,7 @@ export const formatColumnFn: FormatColumnExpressionFunction['fn'] = (
               id: parentFormatId,
               params: {
                 ...col.meta.params?.params,
-                id: format,
+                id: supportedFormats[format].formatId,
                 ...parentFormatParams,
                 // some wrapper formatters require params to be flatten out (i.e. terms) while others
                 // require them to be in the params property (i.e. ranges)
@@ -83,7 +83,7 @@ export const formatColumnFn: FormatColumnExpressionFunction['fn'] = (
             id: parentFormatId,
             params: {
               ...col.meta.params?.params,
-              id: format,
+              id: supportedFormats[format].formatId,
               // some wrapper formatters require params to be flatten out (i.e. terms) while others
               // require them to be in the params property (i.e. ranges)
               // so for now duplicate
