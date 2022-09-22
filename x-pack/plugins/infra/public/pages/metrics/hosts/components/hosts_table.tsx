d5725a85de9250d6e394d34691e9bd62f8c3677f
neptunian <sandra.gonzales@elastic.co>
2022-09-22 09:39:29 -0400

    change from host.os.name to host.os.type

--- x-pack/plugins/infra/public/pages/metrics/hosts/components/hosts_table.tsx
+++ x-pack/plugins/infra/public/pages/metrics/hosts/components/hosts_table.tsx
@@ -64,7 +64,7 @@ const getLensHostsTable = (
                   dataType: 'string',
                   operationType: 'terms',
                   scale: 'ordinal',
-                  sourceField: 'host.os.name',
+                  sourceField: 'host.os.type',
                   isBucketed: true,
                   params: {
                     size: 10000,
