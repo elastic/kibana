fff7832af5e01ad20d3bdef2915c8640d326c5a7
spalger <spencer@elastic.co>
2022-09-22 15:28:19 -0500

    [journeys] restart ES for each journey, fix flakiness

--- .buildkite/scripts/steps/functional/performance_playwright.sh
+++ .buildkite/scripts/steps/functional/performance_playwright.sh
@@ -11,27 +11,6 @@ is_test_execution_step
 rm -rf "$KIBANA_BUILD_LOCATION"
 .buildkite/scripts/download_build_artifacts.sh
 
-echo "--- 🦺 Starting Elasticsearch"
-
-node scripts/es snapshot&
-export esPid=$!
-trap 'kill ${esPid}' EXIT
-
-export TEST_ES_URL=http://elastic:changeme@localhost:9200
-export TEST_ES_DISABLE_STARTUP=true
-
-# Pings the es server every second for up to 2 minutes until it is green
-curl \
-  --fail \
-  --silent \
-  --retry 120 \
-  --retry-delay 1 \
-  --retry-connrefused \
-  -XGET "${TEST_ES_URL}/_cluster/health?wait_for_nodes=>=1&wait_for_status=yellow" \
-  > /dev/null
-
-echo "✅ ES is ready and will continue to run in the background"
-
 # unset env vars defined in other parts of CI for automatic APM collection of
 # Kibana. We manage APM config in our FTR config and performance service, and
 # APM treats config in the ENV with a very high precedence.
@@ -46,29 +25,57 @@ unset ELASTIC_APM_SERVER_URL
 unset ELASTIC_APM_SECRET_TOKEN
 unset ELASTIC_APM_GLOBAL_LABELS
 
-journeys=("login" "ecommerce_dashboard" "flight_dashboard" "web_logs_dashboard" "promotion_tracking_dashboard" "many_fields_discover" "data_stress_test_lens")
+for journey in x-pack/performance/journeys/*; do
+  echo "--- $journey"
 
-for journey in "${journeys[@]}"; do
-  set +e
+  echo "---     🔎 Start es"
+  node scripts/es snapshot&
+  export esPid=$!
+  trap 'kill ${esPid}' EXIT
+
+  export TEST_ES_URL=http://elastic:changeme@localhost:9200
+  export TEST_ES_DISABLE_STARTUP=true
+
+  # Pings the es server every second for up to 2 minutes until it is green
+  curl \
+    --fail \
+    --silent \
+    --retry 120 \
+    --retry-delay 1 \
+    --retry-connrefused \
+    -XGET "${TEST_ES_URL}/_cluster/health?wait_for_nodes=>=1&wait_for_status=yellow" \
+    > /dev/null
+
+  echo "✅ ES is ready and will run in the background"
 
   phases=("WARMUP" "TEST")
+  status=0
   for phase in "${phases[@]}"; do
-    echo "--- $journey - $phase"
+    echo "---     $phase"
 
     export TEST_PERFORMANCE_PHASE="$phase"
+
+    set +e
     node scripts/functional_tests \
-      --config "x-pack/performance/journeys/$journey.ts" \
+      --config "$journey" \
       --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
       --debug \
       --bail
-
     status=$?
+    set -e
+
     if [ $status -ne 0 ]; then
       echo "^^^ +++"
       echo "❌ FTR failed with status code: $status"
-      exit 1
+      break
     fi
   done
 
-  set -e
+  echo "---     Shutdown ES"
+  kill $esPid;
+  trap - EXIT;
+
+  if [ $status -ne 0 ]; then
+    exit 1
+  fi
 done
