010c0a5dc3efd3f8e32655c3c5212d0aefd53a64
Tre' Seymour <wayne.seymour@elastic.co>
2022-09-23 13:11:12 +0100

    Change refs of "global_banner_text" to  "global banner text"

--- x-pack/test/banners_functional/tests/spaces.ts
+++ x-pack/test/banners_functional/tests/spaces.ts
@@ -52,7 +52,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       await PageObjects.common.navigateToApp('home', { basePath: '/s/another-space' });
 
       expect(await PageObjects.banners.isTopBannerVisible()).to.eql(true);
-      expect(await PageObjects.banners.getTopBannerText()).to.eql('global_banner_text');
+      expect(await PageObjects.banners.getTopBannerText()).to.eql('global banner text');
     });
 
     it('displays the global banner on the login page', async () => {
@@ -60,7 +60,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       await PageObjects.common.navigateToApp('login');
 
       expect(await PageObjects.banners.isTopBannerVisible()).to.eql(true);
-      expect(await PageObjects.banners.getTopBannerText()).to.eql('global_banner_text');
+      expect(await PageObjects.banners.getTopBannerText()).to.eql('global banner text');
     });
   });
 }
