49e1a0f2eaa089ee03892a12778f9aacc66fc081
Quynh Nguyen (Quinn) <43350163+qn895@users.noreply.github.com>
2022-09-23 05:09:03 -0500

    [ML] Fix Anomaly charts time and query not being added to dashboard and case (#140147)

--- x-pack/plugins/ml/public/application/explorer/dashboard_controls/add_anomaly_charts_to_dashboard_controls.tsx
+++ x-pack/plugins/ml/public/application/explorer/dashboard_controls/add_anomaly_charts_to_dashboard_controls.tsx
@@ -4,35 +4,41 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
-import { EuiFieldNumber, EuiFormRow, formatDate, htmlIdGenerator } from '@elastic/eui';
-import { TimeRange } from '@kbn/data-plugin/common/query';
+import { EuiFieldNumber, EuiFormRow, htmlIdGenerator, formatDate } from '@elastic/eui';
+import type { Query, TimeRange } from '@kbn/es-query';
+import useObservable from 'react-use/lib/useObservable';
+import { AppStateSelectedCells, getSelectionInfluencers, getSelectionTimeRange } from '../explorer_utils';
+import { isDefined } from '../../../../common/types/guards';
+import { useAnomalyExplorerContext } from '../anomaly_explorer_context';
+import { escapeKueryForFieldValuePair } from '../../util/string_utils';
+import { SEARCH_QUERY_LANGUAGE } from '../../../../common/constants/search';
 import { useDashboardTable } from './use_dashboards_table';
 import { AddToDashboardControl } from './add_to_dashboard_controls';
 import { useAddToDashboardActions } from './use_add_to_dashboard_actions';
-import { AppStateSelectedCells, getSelectionTimeRange } from '../explorer_utils';
 import { DEFAULT_MAX_SERIES_TO_PLOT } from '../../services/anomaly_explorer_charts_service';
 import { JobId } from '../../../../common/types/anomaly_detection_jobs';
 import { ANOMALY_EXPLORER_CHARTS_EMBEDDABLE_TYPE } from '../../../embeddables';
 import { getDefaultExplorerChartsPanelTitle } from '../../../embeddables/anomaly_charts/anomaly_charts_embeddable';
-import { TimeRangeBounds } from '../../util/time_buckets';
 import { useTableSeverity } from '../../components/controls/select_severity';
 import { MAX_ANOMALY_CHARTS_ALLOWED } from '../../../embeddables/anomaly_charts/anomaly_charts_initializer';
+import { TimeRangeBounds } from '@kbn/ml-plugin/public/application/util/time_buckets';
+import { useTimeRangeUpdates } from '@kbn/ml-plugin/public/application/contexts/kibana/use_timefilter';
 
-function getDefaultEmbeddablePanelConfig(jobIds: JobId[]) {
+function getDefaultEmbeddablePanelConfig(jobIds: JobId[], queryString?: string) {
   return {
     id: htmlIdGenerator()(),
-    title: getDefaultExplorerChartsPanelTitle(jobIds),
+    title: getDefaultExplorerChartsPanelTitle(jobIds).concat(queryString ? `- ${queryString}` : ''),
   };
 }
 
 export interface AddToDashboardControlProps {
   jobIds: string[];
+  onClose: (callback?: () => Promise<void>) => void;
   selectedCells?: AppStateSelectedCells | null;
   bounds?: TimeRangeBounds;
   interval?: number;
-  onClose: (callback?: () => Promise<void>) => void;
 }
 
 /**
@@ -41,34 +47,85 @@ export interface AddToDashboardControlProps {
 export const AddAnomalyChartsToDashboardControl: FC<AddToDashboardControlProps> = ({
   onClose,
   jobIds,
-  selectedCells,
-  bounds,
-  interval,
+                                                                                     interval,
+                                                                                     bounds
+
 }) => {
   const [severity] = useTableSeverity();
   const [maxSeriesToPlot, setMaxSeriesToPlot] = useState(DEFAULT_MAX_SERIES_TO_PLOT);
+  const { anomalyExplorerCommonStateService, anomalyTimelineStateService, chartsStateService } =
+    useAnomalyExplorerContext();
+  const { queryString } = useObservable(
+    anomalyExplorerCommonStateService.getFilterSettings$(),
+    anomalyExplorerCommonStateService.getFilterSettings()
+  );
+  const globalTimeRange = useTimeRangeUpdates();
 
-  const getEmbeddableInput = useCallback(() => {
-    let timeRange: TimeRange | undefined;
+
+  const selectedCells = useObservable(
+    anomalyTimelineStateService.getSelectedCells$(),
+    anomalyTimelineStateService.getSelectedCells()
+  );
+
+  const chartsData = useObservable(
+    chartsStateService.getChartsData$(),
+    chartsStateService.getChartsData()
+  );
+
+  const timeRangeToPlot: TimeRange = useMemo(() => {
+    if (chartsData.seriesToPlot.length > 0) {
+      return {
+        from: formatDate(chartsData.seriesToPlot[0].plotEarliest, 'MMM D, YYYY @ HH:mm:ss.SSS'),
+        to: formatDate(chartsData.seriesToPlot[0].plotLatest, 'MMM D, YYYY @ HH:mm:ss.SSS'),
+      } as TimeRange;
+    }
     if (!!selectedCells && interval !== undefined && bounds !== undefined) {
       const { earliestMs, latestMs } = getSelectionTimeRange(selectedCells, bounds);
-      timeRange = {
+      return {
         from: formatDate(earliestMs, 'MMM D, YYYY @ HH:mm:ss.SSS'),
         to: formatDate(latestMs, 'MMM D, YYYY @ HH:mm:ss.SSS'),
         mode: 'absolute',
       };
     }
 
-    const config = getDefaultEmbeddablePanelConfig(jobIds);
+    return globalTimeRange;
+  }, [chartsData.seriesToPlot, globalTimeRange, selectedCells, bounds, interval]);
 
+  const getEmbeddableInput = useCallback(() => {
+    // Respect the query and the influencers selected
+    // If no query or filter set, filter out to the lanes the selected cells
+    // And if no selected cells, show everything
+
+    const selectionInfluencers = getSelectionInfluencers(
+      selectedCells,
+      selectedCells?.viewByFieldName!
+    );
+
+    const influencers = selectionInfluencers ?? [];
+    const config = getDefaultEmbeddablePanelConfig(jobIds, queryString);
+    const queryFromSelectedCells = influencers
+      .map((s) => escapeKueryForFieldValuePair(s.fieldName, s.fieldValue))
+      .join(' or ');
+
+    // When adding anomaly charts to Dashboard, we want to respect the Dashboard's time range
+    // so we are not passing the time range here
     return {
       ...config,
       jobIds,
       maxSeriesToPlot: maxSeriesToPlot ?? DEFAULT_MAX_SERIES_TO_PLOT,
       severityThreshold: severity.val,
-      ...(timeRange ?? {}),
+      timeRange: timeRangeToPlot,
+      ...((isDefined(queryString) && queryString !== '') ||
+      (queryFromSelectedCells !== undefined && queryFromSelectedCells !== '')
+        ? {
+            query: {
+              query: queryString === '' ? queryFromSelectedCells : queryString,
+              language: SEARCH_QUERY_LANGUAGE.KUERY,
+            } as Query,
+          }
+        : {}),
     };
-  }, [selectedCells, interval, bounds, jobIds, maxSeriesToPlot, severity]);
+  }, [jobIds, maxSeriesToPlot, severity, queryString, selectedCells, timeRangeToPlot]);
 
   const { dashboardItems, isLoading, search } = useDashboardTable();
   const { addToDashboardAndEditCallback } = useAddToDashboardActions(
