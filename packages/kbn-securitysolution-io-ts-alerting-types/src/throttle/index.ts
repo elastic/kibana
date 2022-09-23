a62a216d87e3022cd78f321d75856dfe11646e54
jpdjere <jpdjeredjian@gmail.com>
2022-09-23 11:03:25 +0200

    Fix throttle literals

--- packages/kbn-securitysolution-io-ts-alerting-types/src/throttle/index.ts
+++ packages/kbn-securitysolution-io-ts-alerting-types/src/throttle/index.ts
@@ -8,16 +8,17 @@
 
 import * as t from 'io-ts';
 
-export const throttle = t.union([
-  t.null,
-  t.literal('onActiveAlert'),
-  t.literal('onThrottleInterval'),
-  t.literal('onActionGroupChange'),
-]);
+export const throttle = t.union([t.literal('no_actions'), t.literal('rule')]);
 export type Throttle = t.TypeOf<typeof throttle>;
+
+export const throttleForBulkActions = t.literal('rule');
+export type ThrottleForBulkActions = t.TypeOf<typeof throttleForBulkActions>;
 
 export const throttleOrNull = t.union([throttle, t.null]);
 export type ThrottleOrNull = t.TypeOf<typeof throttleOrNull>;
+
+export const throttleForBulkActionsOrNull = t.union([throttleForBulkActions, t.null]);
+export type ThrottleForBulkActionsOrNull = t.TypeOf<typeof throttleForBulkActionsOrNull>;
 
 export const throttleOrNullOrUndefined = t.union([throttle, t.null, t.undefined]);
 export type ThrottleOrUndefinedOrNull = t.TypeOf<typeof throttleOrNullOrUndefined>;
