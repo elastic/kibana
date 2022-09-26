75711c3cf1914e4b949ec6bd2d00718df846177b
Joseph McElroy <joseph.mcelroy@elastic.co>
2022-09-26 19:24:21 +0100

    [Enterprise Search] [Behavioural analytics] BUG hide UI settings option (#141786)
    * hides behavioural analytics ui setting
    
    * update test for uiSettings call

--- x-pack/plugins/enterprise_search/server/ui_settings.ts
+++ x-pack/plugins/enterprise_search/server/ui_settings.ts
@@ -5,29 +5,9 @@
  * 2.0.
  */
 
-import { schema } from '@kbn/config-schema';
 import { UiSettingsParams } from '@kbn/core/types';
-import { i18n } from '@kbn/i18n';
-
-import {
-  enterpriseSearchFeatureId,
-  enableBehavioralAnalyticsSection,
-} from '../common/ui_settings_keys';
 
 /**
  * uiSettings definitions for Enterprise Search
  */
-export const uiSettings: Record<string, UiSettingsParams<boolean>> = {
-  [enableBehavioralAnalyticsSection]: {
-    category: [enterpriseSearchFeatureId],
-    description: i18n.translate('xpack.enterpriseSearch.uiSettings.analytics.description', {
-      defaultMessage: 'Enable the new Analytics section in Enterprise Search.',
-    }),
-    name: i18n.translate('xpack.enterpriseSearch.uiSettings.analytics.name', {
-      defaultMessage: 'Enable Behavioral Analytics',
-    }),
-    requiresPageReload: true,
-    schema: schema.boolean(),
-    value: false,
-  },
-};
+export const uiSettings: Record<string, UiSettingsParams<boolean>> = {};
