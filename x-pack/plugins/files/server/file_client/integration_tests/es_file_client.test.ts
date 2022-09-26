943716d9912363ac903ab3091d517ed513cfde71
Jean-Louis Leysens <jloleysens@gmail.com>
2022-09-26 16:26:21 +0200

    [revert this] only run the flaky test

--- x-pack/plugins/files/server/file_client/integration_tests/es_file_client.test.ts
+++ x-pack/plugins/files/server/file_client/integration_tests/es_file_client.test.ts
@@ -11,11 +11,7 @@ import { createEsFileClient } from '../create_es_file_client';
 import { FileClient } from '../types';
 import { FileMetadata } from '../../../common';
 
-/**
- * This file client is using Elasticsearch interfaces directly to manage files.
- */
-// FLAKY: https://github.com/elastic/kibana/issues/139565
-describe.skip('ES-index-backed file client', () => {
+describe.only('ES-index-backed file client', () => {
   let esClient: TestEnvironmentUtils['esClient'];
   let fileClient: FileClient;
   let testHarness: TestEnvironmentUtils;
