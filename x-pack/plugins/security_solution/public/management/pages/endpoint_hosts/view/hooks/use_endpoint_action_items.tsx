c8f7fb23c485c625ae91a809dc832c4da928e38b
Kevin Logan <kevin.logan@elastic.co>
2022-09-26 17:18:01 -0400

    update translation name

--- x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/hooks/use_endpoint_action_items.tsx
+++ x-pack/plugins/security_solution/public/management/pages/endpoint_hosts/view/hooks/use_endpoint_action_items.tsx
@@ -156,7 +156,7 @@ export const useEndpointActionItems = (
                 href: getAppUrl({ path: endpointActionsPath }),
                 children: (
                   <FormattedMessage
-                    id="xpack.securitySolution.endpoint.actions.responseActionsLog"
+                    id="xpack.securitySolution.endpoint.actions.responseActionsHistory"
                     defaultMessage="View response actions history"
                   />
                 ),
