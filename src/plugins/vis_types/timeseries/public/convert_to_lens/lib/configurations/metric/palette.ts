8b3a9f5c073fccd7176d6ec3bba57e2254a29100
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-09-27 12:23:05 +0300

    Allowed to convert to lens with invalid color rules.

--- src/plugins/vis_types/timeseries/public/convert_to_lens/lib/configurations/metric/palette.ts
+++ src/plugins/vis_types/timeseries/public/convert_to_lens/lib/configurations/metric/palette.ts
@@ -175,7 +175,7 @@ export const getPalette = (model: Panel): PaletteOutput<CustomPaletteParams> | n
   // lnsMetric is supporting lte only, if one rule is defined
   if (validRules.length === 1) {
     if (validRules[0].operator !== Operators.LTE) {
-      return null;
+      return;
     }
     return getCustomPalette(getColorStopWithMinMaxForLte(validRules[0]));
   }
@@ -188,18 +188,18 @@ export const getPalette = (model: Panel): PaletteOutput<CustomPaletteParams> | n
     kindOfHeadRules.length > 1 ||
     (kindOfHeadRules[0].operator !== tailRule.operator && tailRule.operator !== Operators.LTE)
   ) {
-    return null;
+    return;
   }
 
   const [rule] = kindOfHeadRules;
 
   if (rule.operator === Operators.LTE) {
-    return null;
+    return;
   }
 
   if (rule.operator === Operators.LT) {
     if (tailRule.operator !== Operators.LTE) {
-      return null;
+      return;
     }
     return getCustomPalette(getColorStopsWithMinMaxForLtWithLte(validRules));
   }
