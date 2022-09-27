cfe3e0f77c459dff931366293620f5ae06ecc1f2
Anton Dosov <anton.dosov@elastic.co>
2022-09-27 12:59:50 +0200

    also coverage

--- x-pack/plugins/index_lifecycle_management/jest.integration.config.js
+++ x-pack/plugins/index_lifecycle_management/jest.integration.config.js
@@ -10,4 +10,10 @@ module.exports = {
   rootDir: '../../..',
   roots: ['<rootDir>/x-pack/plugins/index_lifecycle_management'],
   testMatch: ['/**/integration_tests/**/*.test.{js,mjs,ts,tsx}'],
+  coverageDirectory:
+    '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/index_lifecycle_management',
+  coverageReporters: ['text', 'html'],
+  collectCoverageFrom: [
+    '<rootDir>/x-pack/plugins/index_lifecycle_management/{common,public,server}/**/*.{ts,tsx}',
+  ],
 };
