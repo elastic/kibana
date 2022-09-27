eb1b893dd08a96f02e3784cb075e970924f33206
Matthew Kime <matt@mattki.me>
2022-09-27 06:56:46 -0500

    [data views] Allow data view rename via rest api (#141869)
    * allow rename of data view via rest api

--- test/api_integration/apis/data_views/index.js
+++ test/api_integration/apis/data_views/index.js
@@ -10,7 +10,7 @@ export default function ({ loadTestFile }) {
   describe('index_patterns', () => {
     loadTestFile(require.resolve('./es_errors'));
     loadTestFile(require.resolve('./fields_for_wildcard_route'));
-    loadTestFile(require.resolve('./index_pattern_crud'));
+    loadTestFile(require.resolve('./data_views_crud'));
     loadTestFile(require.resolve('./scripted_fields_crud'));
     loadTestFile(require.resolve('./fields_api'));
     loadTestFile(require.resolve('./default_index_pattern'));
