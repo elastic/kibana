10476457d2993db59d256ceb647a398d1b739ca7
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2022-09-27 17:59:06 -0400

    lisa review

--- x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_event_log_list_kpi.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/rule_event_log_list_kpi.tsx
@@ -35,7 +35,7 @@ const API_FAILED_MESSAGE = i18n.translate(
 const RESPONSE_TOOLTIP = i18n.translate(
   'xpack.triggersActionsUI.sections.ruleDetails.ruleEventLogListKpi.responseTooltip',
   {
-    defaultMessage: 'Response status of recent rule runs.',
+    defaultMessage: 'Response of the latest rule runs.',
   }
 );
 
