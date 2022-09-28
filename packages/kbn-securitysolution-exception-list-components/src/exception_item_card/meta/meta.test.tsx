3a1c9b2cf392eebf3ec11a93ea2cc1828421cadf
wafaanasr <wafaa.aanasr@gmail.com>
2022-09-28 07:53:22 +0200

    fix meta tests

--- packages/kbn-securitysolution-exception-list-components/src/exception_item_card/meta/meta.test.tsx
+++ packages/kbn-securitysolution-exception-list-components/src/exception_item_card/meta/meta.test.tsx
@@ -6,139 +6,150 @@
  * Side Public License, v 1.
  */
 
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { getExceptionListItemSchemaMock } from '@kbn/lists-plugin/common/schemas/response/exception_list_item_schema.mock';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { getExceptionListItemSchemaMock } from '@kbn/lists-plugin/common/schemas/response/exception_list_item_schema.mock';
 
-// import { ExceptionItemCardMetaInfo } from './meta';
+import { ExceptionItemCardMetaInfo } from './meta';
+import { RuleReference } from '../../types';
 
-// const ruleReferences = [
-//   {
-//     exception_lists: [
-//       {
-//         id: '123',
-//         list_id: 'i_exist',
-//         namespace_type: 'single',
-//         type: 'detection',
-//       },
-//       {
-//         id: '456',
-//         list_id: 'i_exist_2',
-//         namespace_type: 'single',
-//         type: 'detection',
-//       },
-//     ],
-//     id: '1a2b3c',
-//     name: 'Simple Rule Query',
-//     rule_id: 'rule-2',
-//   },
-// ];
+const ruleReferences = [
+  {
+    exception_lists: [
+      {
+        id: '123',
+        list_id: 'i_exist',
+        namespace_type: 'single',
+        type: 'detection',
+      },
+      {
+        id: '456',
+        list_id: 'i_exist_2',
+        namespace_type: 'single',
+        type: 'detection',
+      },
+    ],
+    id: '1a2b3c',
+    name: 'Simple Rule Query',
+    rule_id: 'rule-2',
+  },
+];
 describe('ExceptionItemCardMetaInfo', () => {
-  it('temp', () => {
-    expect(true).toEqual(true);
+  it('it should render creation info with sending custom formattedDateComponent', () => {
+    const wrapper = render(
+      <ExceptionItemCardMetaInfo
+        item={getExceptionListItemSchemaMock()}
+        references={ruleReferences as unknown as RuleReference[]}
+        dataTestSubj="exceptionItemMeta"
+        securityLinkAnchorComponent={() => null}
+        formattedDateComponent={({ fieldName, value }) => (
+          <>
+            <p>{new Date(value).toDateString()}</p>
+          </>
+        )}
+      />
+    );
+
+    expect(wrapper.getByTestId('exceptionItemMetaCreatedBylastUpdate')).toHaveTextContent(
+      'Mon Apr 20 2020'
+    );
+    expect(wrapper.getByTestId('exceptionItemMetaCreatedBylastUpdateValue')).toHaveTextContent(
+      'some user'
+    );
   });
-  // it('it renders item creation info', () => {
-  //   const wrapper = render(
-  //     <ExceptionItemCardMetaInfo
-  //       item={getExceptionListItemSchemaMock()}
-  //       references={[]}
-  //       dataTestSubj="exceptionItemMeta"
-  //       securityLinkAnchorComponent={() => null}
-  //     />
-  //   );
 
-  //   expect(wrapper.getByTestId('exceptionItemMetaCreatedBy')).toHaveTextContent(
-  //     'Apr 20, 2020 @ 15:25:31.830'
-  //   );
-  //   expect(wrapper.getByTestId('exceptionItemMeta-createdBy-value2')).toHaveTextContent(
-  //     'some user'
-  //   );
-  // });
+  it('it should render udate info with sending custom formattedDateComponent', () => {
+    const wrapper = render(
+      <ExceptionItemCardMetaInfo
+        item={getExceptionListItemSchemaMock()}
+        references={ruleReferences as unknown as RuleReference[]}
+        dataTestSubj="exceptionItemMeta"
+        securityLinkAnchorComponent={() => null}
+        formattedDateComponent={({ fieldName, value }) => (
+          <>
+            <p>{new Date(value).toDateString()}</p>
+          </>
+        )}
+      />
+    );
+    expect(wrapper.getByTestId('exceptionItemMetaUpdatedBylastUpdate')).toHaveTextContent(
+      'Mon Apr 20 2020'
+    );
+    expect(wrapper.getByTestId('exceptionItemMetaUpdatedBylastUpdateValue')).toHaveTextContent(
+      'some user'
+    );
+  });
 
-  // it('it renders item update info', () => {
-  //   const wrapper = render(
-  //     <ExceptionItemCardMetaInfo
-  //       item={getExceptionListItemSchemaMock()}
-  //       references={ruleReferences}
-  //       dataTestSubj="exceptionItemMeta"
-  //       securityLinkAnchorComponent={() => null}
-  //     />
-  //   );
+  it('it should render references info', () => {
+    const wrapper = render(
+      <ExceptionItemCardMetaInfo
+        item={getExceptionListItemSchemaMock()}
+        references={ruleReferences as unknown as RuleReference[]}
+        dataTestSubj="exceptionItemMeta"
+        securityLinkAnchorComponent={() => null}
+        formattedDateComponent={() => null}
+      />
+    );
 
-  //   expect(wrapper.getByTestId('exceptionItemMeta-updatedBy-value1')).toHaveTextContent(
-  //     'Apr 20, 2020 @ 15:25:31.830'
-  //   );
-  //   expect(wrapper.getByTestId('exceptionItemMeta-updatedBy-value2')).toHaveTextContent(
-  //     'some user'
-  //   );
-  // });
+    expect(wrapper.getByTestId('exceptionItemMetaAffectedRulesButton')).toHaveTextContent(
+      'Affects 1 rule'
+    );
+  });
 
-  // it('it renders references info', () => {
-  //   const wrapper = render(
-  //     <ExceptionItemCardMetaInfo
-  //       item={getExceptionListItemSchemaMock()}
-  //       references={ruleReferences}
-  //       dataTestSubj="exceptionItemMeta"
-  //       securityLinkAnchorComponent={() => null}
-  //     />
-  //   );
+  it('it renders references info when multiple references exist', () => {
+    const wrapper = render(
+      <ExceptionItemCardMetaInfo
+        item={getExceptionListItemSchemaMock()}
+        references={
+          [
+            {
+              exception_lists: [
+                {
+                  id: '123',
+                  list_id: 'i_exist',
+                  namespace_type: 'single',
+                  type: 'detection',
+                },
+                {
+                  id: '456',
+                  list_id: 'i_exist_2',
+                  namespace_type: 'single',
+                  type: 'detection',
+                },
+              ],
+              id: '1a2b3c',
+              name: 'Simple Rule Query',
+              rule_id: 'rule-2',
+            },
+            {
+              exceptionLists: [
+                {
+                  id: '123',
+                  list_id: 'i_exist',
+                  namespace_type: 'single',
+                  type: 'detection',
+                },
+                {
+                  id: '456',
+                  list_id: 'i_exist_2',
+                  namespace_type: 'single',
+                  type: 'detection',
+                },
+              ],
+              id: 'aaa',
+              name: 'Simple Rule Query 2',
+              rule_id: 'rule-3',
+            },
+          ] as unknown as RuleReference[]
+        }
+        dataTestSubj="exceptionItemMeta"
+        securityLinkAnchorComponent={() => null}
+        formattedDateComponent={() => null}
+      />
+    );
 
-  //   expect(wrapper.getByTestId('exceptionItemMeta-affectedRulesButton')).toHaveTextContent(
-  //     'Affects 1 rule'
-  //   );
-  // });
-
-  // it('it renders references info when multiple references exist', () => {
-  //   const wrapper = render(
-  //     <ExceptionItemCardMetaInfo
-  //       item={getExceptionListItemSchemaMock()}
-  //       references={[
-  //         {
-  //           exceptionLists: [
-  //             {
-  //               id: '123',
-  //               list_id: 'i_exist',
-  //               namespace_type: 'single',
-  //               type: 'detection',
-  //             },
-  //             {
-  //               id: '456',
-  //               list_id: 'i_exist_2',
-  //               namespace_type: 'single',
-  //               type: 'detection',
-  //             },
-  //           ],
-  //           id: '1a2b3c',
-  //           name: 'Simple Rule Query',
-  //           rule_id: 'rule-2',
-  //         },
-  //         {
-  //           exceptionLists: [
-  //             {
-  //               id: '123',
-  //               list_id: 'i_exist',
-  //               namespace_type: 'single',
-  //               type: 'detection',
-  //             },
-  //             {
-  //               id: '456',
-  //               list_id: 'i_exist_2',
-  //               namespace_type: 'single',
-  //               type: 'detection',
-  //             },
-  //           ],
-  //           id: 'aaa',
-  //           name: 'Simple Rule Query 2',
-  //           rule_id: 'rule-3',
-  //         },
-  //       ]}
-  //       dataTestSubj="exceptionItemMeta"
-  //       securityLinkAnchorComponent={() => null}
-  //     />
-  //   );
-
-  //   expect(wrapper.getByTestId('exceptionItemMeta-affectedRulesButton')).toHaveTextContent(
-  //     'Affects 2 rules'
-  //   );
-  // });
+    expect(wrapper.getByTestId('exceptionItemMetaAffectedRulesButton')).toHaveTextContent(
+      'Affects 2 rules'
+    );
+  });
 });
