350c709efd32cd77a15fc5cea6595a019c65f7f9
Ying Mao <ying.mao@elastic.co>
2022-09-28 11:17:33 -0400

    Pinning to 1.8

--- .buildkite/scripts/steps/checks/event_log.sh
+++ .buildkite/scripts/steps/checks/event_log.sh
@@ -7,7 +7,7 @@ source .buildkite/scripts/common/util.sh
 echo --- Check Event Log Schema
 
 # event log schema is pinned to a specific version of ECS
-ECS_STABLE_VERSION=1.11
+ECS_STABLE_VERSION=1.8
 git clone -b $ECS_STABLE_VERSION https://github.com/elastic/ecs.git ../ecs
 
 node x-pack/plugins/event_log/scripts/create_schemas.js
