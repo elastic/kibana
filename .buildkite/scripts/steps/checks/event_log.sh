08a77045ac75e29bc88398b5d878555d9e86e779
Ying Mao <ying.mao@elastic.co>
2022-09-28 10:11:00 -0400

    Can I check out ECS

--- .buildkite/scripts/steps/checks/event_log.sh
+++ .buildkite/scripts/steps/checks/event_log.sh
@@ -6,5 +6,11 @@ source .buildkite/scripts/common/util.sh
 
 echo --- Check Event Log Schema
 
+cd ..
+
+git clone https://github.com/elastic/ecs.git
+
+cd kibana
 node x-pack/plugins/event_log/scripts/create_schemas.js
+
 check_for_changed_files 'node x-pack/plugins/event_log/scripts/create_schemas.js' false
