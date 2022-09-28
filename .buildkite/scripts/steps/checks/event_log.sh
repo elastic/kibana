0f6226e27c484daa9df4ad27ab64f1ec6cc8f509
Ying Mao <ying.mao@elastic.co>
2022-09-28 10:36:26 -0400

    Checking out specific ECS branch

--- .buildkite/scripts/steps/checks/event_log.sh
+++ .buildkite/scripts/steps/checks/event_log.sh
@@ -6,11 +6,10 @@ source .buildkite/scripts/common/util.sh
 
 echo --- Check Event Log Schema
 
-cd ..
+# check out the latest stable version of ECS
+ECS_STABLE_VERSION=8.4
+git clone -b $ECS_STABLE_VERSION https://github.com/elastic/ecs.git ../ecs
 
-git clone https://github.com/elastic/ecs.git
-
-cd kibana
 node x-pack/plugins/event_log/scripts/create_schemas.js
 
 check_for_changed_files 'node x-pack/plugins/event_log/scripts/create_schemas.js' false
