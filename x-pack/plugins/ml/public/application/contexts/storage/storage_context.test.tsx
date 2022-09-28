c58d4811655fb3c1e17880070f85898601f3708c
Dima Arnautov <dmitrii.arnautov@elastic.co>
2022-09-28 13:11:18 +0200

    update tests

--- x-pack/plugins/ml/public/application/contexts/storage/storage_context.test.tsx
+++ x-pack/plugins/ml/public/application/contexts/storage/storage_context.test.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { MlStorageContextProvider, useStorage } from './storage_context';
 import { MlStorageKey } from '../../../../common/types/storage';
 
@@ -38,7 +38,7 @@ describe('useStorage', () => {
     jest.clearAllMocks();
   });
 
-  test('shows render a default value', () => {
+  test('returns the default value', () => {
     const { result } = renderHook(() => useStorage('ml.jobSelectorFlyout.applyTimeRange', true), {
       wrapper: MlStorageContextProvider,
     });
@@ -46,7 +46,7 @@ describe('useStorage', () => {
     expect(result.current[0]).toBe(true);
   });
 
-  test('shows value from provider', () => {
+  test('returns the value from storage', () => {
     const { result } = renderHook(() => useStorage('ml.gettingStarted.isDismissed', false), {
       wrapper: MlStorageContextProvider,
     });
@@ -54,31 +54,45 @@ describe('useStorage', () => {
     expect(result.current[0]).toBe(true);
   });
 
-  test('updates the value', () => {
-    const { result } = renderHook(() => useStorage('ml.gettingStarted.isDismissed'), {
-      wrapper: MlStorageContextProvider,
-    });
+  test('updates the storage value', async () => {
+    const { result, waitForNextUpdate } = renderHook(
+      () => useStorage('ml.gettingStarted.isDismissed'),
+      {
+        wrapper: MlStorageContextProvider,
+      }
+    );
 
     const [value, setValue] = result.current;
 
     expect(value).toBe(true);
 
-    setValue(false);
+    await act(async () => {
+      setValue(false);
+      await waitForNextUpdate();
+    });
 
+    expect(result.current[0]).toBe(false);
     expect(mockSet).toHaveBeenCalledWith('ml.gettingStarted.isDismissed', false);
   });
 
-  test('removes the value', () => {
-    const { result } = renderHook(() => useStorage('ml.gettingStarted.isDismissed'), {
-      wrapper: MlStorageContextProvider,
-    });
+  test('removes the storage value', async () => {
+    const { result, waitForNextUpdate } = renderHook(
+      () => useStorage('ml.gettingStarted.isDismissed'),
+      {
+        wrapper: MlStorageContextProvider,
+      }
+    );
 
     const [value, setValue] = result.current;
 
     expect(value).toBe(true);
 
-    setValue(undefined);
+    await act(async () => {
+      setValue(undefined);
+      await waitForNextUpdate();
+    });
 
+    expect(result.current[0]).toBe(undefined);
     expect(mockRemove).toHaveBeenCalledWith('ml.gettingStarted.isDismissed');
   });
 });
