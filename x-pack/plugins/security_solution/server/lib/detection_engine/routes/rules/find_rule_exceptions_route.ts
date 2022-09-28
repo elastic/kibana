9c78331aac4fe44aa8a0995f610ca77e6e0725ca
Yara Tercero <yara.tercero@elastic.co>
2022-09-28 02:24:33 -0700

    modified rule references route to return list as well

--- x-pack/plugins/security_solution/server/lib/detection_engine/routes/rules/find_rule_exceptions_route.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/routes/rules/find_rule_exceptions_route.ts
@@ -8,7 +8,6 @@
 import { transformError } from '@kbn/securitysolution-es-utils';
 import { getSavedObjectType } from '@kbn/securitysolution-list-utils';
 import { validate } from '@kbn/securitysolution-io-ts-utils';
-import type { FindResult } from '@kbn/alerting-plugin/server';
 
 import type { SecuritySolutionPluginRouter } from '../../../../types';
 import { DETECTION_ENGINE_RULES_EXCEPTIONS_REFERENCE_URL } from '../../../../../common/constants';
@@ -17,9 +16,7 @@ import { enrichFilterWithRuleTypeMapping } from '../../rules/enrich_filter_with_
 import type { FindExceptionReferencesOnRuleSchemaDecoded } from '../../../../../common/detection_engine/schemas/request/find_exception_list_references_schema';
 import { findExceptionReferencesOnRuleSchema } from '../../../../../common/detection_engine/schemas/request/find_exception_list_references_schema';
 import { buildRouteValidation } from '../../../../utils/build_validation/route_validation';
-import type { RuleReferencesSchema } from '../../../../../common/detection_engine/schemas/response/find_exception_list_references_schema';
 import { rulesReferencedByExceptionListsSchema } from '../../../../../common/detection_engine/schemas/response/find_exception_list_references_schema';
-import type { RuleParams } from '../../schemas/rule_schemas';
 
 export const findRuleExceptionReferencesRoute = (router: SecuritySolutionPluginRouter) => {
   router.get(
@@ -43,38 +40,66 @@ export const findRuleExceptionReferencesRoute = (router: SecuritySolutionPluginR
 
         const ctx = await context.resolve(['core', 'securitySolution', 'alerting']);
         const rulesClient = ctx.alerting.getRulesClient();
+        const listsClient = ctx.securitySolution.getExceptionListClient();
 
-        if (ids.length !== namespaceTypes.length || ids.length !== listIds.length) {
+        if (
+          ids != null &&
+          listIds != null &&
+          (ids.length !== namespaceTypes.length || ids.length !== listIds.length)
+        ) {
           return siemResponse.error({
             body: `"ids", "list_ids" and "namespace_types" need to have the same comma separated number of values. Expected "ids" length: ${ids.length} to equal "namespace_types" length: ${namespaceTypes.length} and "list_ids" length: ${listIds.length}.`,
             statusCode: 400,
           });
         }
 
-        const foundRules: Array<FindResult<RuleParams>> = await Promise.all(
-          ids.map(async (id, index) => {
-            return rulesClient.find({
+        const fetchExact = ids != null && listIds != null;
+
+        const foundExceptionLists = await listsClient?.findExceptionList({
+          filter: fetchExact
+            ? `(${listIds
+                .map((listId) => `exception-list.attributes.list_id:${listId}`)
+                .join(' OR ')})`
+            : undefined,
+          namespaceType: ['agnostic', 'single'],
+          page: 1,
+          perPage: 10000,
+          sortField: undefined,
+          sortOrder: undefined,
+        });
+
+        if (foundExceptionLists == null) {
+          return response.ok({ body: { references: [] } });
+        }
+
+        const references = await Promise.all(
+          foundExceptionLists.data.map(async (list, index) => {
+            const foundRules = await rulesClient.find({
               options: {
                 perPage: 10000,
                 filter: enrichFilterWithRuleTypeMapping(null),
                 hasReference: {
-                  id,
+                  id: list.id,
                   type: getSavedObjectType({ namespaceType: namespaceTypes[index] }),
                 },
               },
             });
+
+            const ruleData = foundRules.data.map(({ name, id, params }) => ({
+              name,
+              id,
+              rule_id: params.ruleId,
+              exception_lists: params.exceptionsList,
+            }));
+
+            return {
+              [list.list_id]: {
+                ...list,
+                referenced_rules: ruleData,
+              },
+            };
           })
         );
-
-        const references = foundRules.map<RuleReferencesSchema>(({ data }, index) => {
-          const wantedData = data.map(({ name, id, params }) => ({
-            name,
-            id,
-            rule_id: params.ruleId,
-            exception_lists: params.exceptionsList,
-          }));
-          return { [listIds[index]]: wantedData };
-        });
 
         const [validated, errors] = validate({ references }, rulesReferencedByExceptionListsSchema);
 
