7b9adee3e7287854cbfb256315a6f2d9b4ada7c7
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-09-29 15:10:21 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- packages/shared-ux/markdown/markdown.tsx
+++ packages/shared-ux/markdown/markdown.tsx
@@ -1,23 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 import { EuiMarkdownEditor } from '@elastic/eui';
 import React, { useState } from 'react';
 
 interface MarkdownProps {
-    initialContent: string;
-    ariaLabelContent: string;
+  initialContent: string;
+  ariaLabelContent: string;
 }
 
 export const Markdown = ({ initialContent, ariaLabelContent }: MarkdownProps) => {
-    const [value, setValue] = useState(initialContent);
-    
-    return (
-        <EuiMarkdownEditor  
-        aria-label={ariaLabelContent ?? "markdown component"}
-        placeholder="Your markdown here..."
-        initialViewMode="viewing"
-        value={value}
-        onChange={setValue}
-        height={200}
-        />
-    )
-    
-}
+  const [value, setValue] = useState(initialContent);
+
+  return (
+    <EuiMarkdownEditor
+      aria-label={ariaLabelContent ?? 'markdown component'}
+      placeholder="Your markdown here..."
+      initialViewMode="viewing"
+      value={value}
+      onChange={setValue}
+      height={200}
+    />
+  );
+};
