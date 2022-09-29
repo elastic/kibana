2495d7ccd6b23922d3bd2461e88b0c40b123601b
Gloria Hornero <gloria.hornero@elastic.co>
2022-09-29 08:06:55 +0200

    fixes issues generated by the conflicts during merge

--- x-pack/plugins/security_solution/cypress/tasks/api_calls/rules.ts
+++ x-pack/plugins/security_solution/cypress/tasks/api_calls/rules.ts
@@ -77,94 +77,89 @@ export const createCustomRule = (
 };
 
 export const createEventCorrelationRule = (rule: CustomRule, ruleId = 'rule_testing') => {
-  if (rule.dataSource.type === 'indexPatterns') {
-    const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
-    const severity = rule.severity != null ? rule.severity.toLowerCase() : undefined;
+  const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
+  const severity = rule.severity != null ? rule.severity.toLowerCase() : undefined;
 
-    cy.request({
-      method: 'POST',
-      url: 'api/detection_engine/rules',
-      body: {
-        rule_id: ruleId,
-        risk_score: riskScore,
-        description: rule.description,
-        interval: `${rule.runsEvery?.interval}${rule.runsEvery?.type}`,
-        from: `now-${rule.lookBack?.interval}${rule.lookBack?.type}`,
-        name: rule.name,
-        severity,
-        type: 'eql',
-        index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
-        data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
-        query: rule.customQuery,
-        language: 'eql',
-        enabled: true,
-        tags: rule.tags,
-      },
-      headers: { 'kbn-xsrf': 'cypress-creds' },
-    });
-  }
+  cy.request({
+    method: 'POST',
+    url: 'api/detection_engine/rules',
+    body: {
+      rule_id: ruleId,
+      risk_score: riskScore,
+      description: rule.description,
+      interval: `${rule.runsEvery?.interval}${rule.runsEvery?.type}`,
+      from: `now-${rule.lookBack?.interval}${rule.lookBack?.type}`,
+      name: rule.name,
+      severity,
+      type: 'eql',
+      index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
+      data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
+      query: rule.customQuery,
+      language: 'eql',
+      enabled: true,
+      tags: rule.tags,
+    },
+    headers: { 'kbn-xsrf': 'cypress-creds' },
+  });
 };
 
 export const createThresholdRule = (rule: ThresholdRule, ruleId = 'rule_testing') => {
-  if (rule.dataSource.type === 'indexPatterns') {
-    const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
-    const severity = rule.severity != null ? rule.severity.toLocaleLowerCase() : undefined;
+  const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
+  const severity = rule.severity != null ? rule.severity.toLocaleLowerCase() : undefined;
 
-    cy.request({
-      method: 'POST',
-      url: 'api/detection_engine/rules',
-      body: {
-        rule_id: ruleId,
-        risk_score: riskScore,
-        description: rule.description,
-        interval: `${rule.runsEvery?.interval}${rule.runsEvery?.type}`,
-        from: `now-${rule.lookBack?.interval}${rule.lookBack?.type}`,
-        name: rule.name,
-        severity,
-        type: 'threshold',
-        index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
-        data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
-        query: rule.customQuery,
-        threshold: {
-          field: [rule.thresholdField],
-          value: parseInt(rule.threshold, 10),
-          cardinality: [],
-        },
-        enabled: true,
-        tags: rule.tags,
+  cy.request({
+    method: 'POST',
+    url: 'api/detection_engine/rules',
+    body: {
+      rule_id: ruleId,
+      risk_score: riskScore,
+      description: rule.description,
+      interval: `${rule.runsEvery?.interval}${rule.runsEvery?.type}`,
+      from: `now-${rule.lookBack?.interval}${rule.lookBack?.type}`,
+      name: rule.name,
+      severity,
+      type: 'threshold',
+      index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
+      data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
+      query: rule.customQuery,
+      threshold: {
+        field: [rule.thresholdField],
+        value: parseInt(rule.threshold, 10),
+        cardinality: [],
+      },
+      enabled: true,
+      tags: rule.tags,
     },
     headers: { 'kbn-xsrf': 'cypress-creds' },
   });
 };
 
 export const createNewTermsRule = (rule: NewTermsRule, ruleId = 'rule_testing') => {
-  if (rule.dataSource.type === 'indexPatterns') {
-    const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
-    const severity = rule.severity != null ? rule.severity.toLocaleLowerCase() : undefined;
+  const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
+  const severity = rule.severity != null ? rule.severity.toLocaleLowerCase() : undefined;
 
-    cy.request({
-      method: 'POST',
-      url: 'api/detection_engine/rules',
-      body: {
-        rule_id: ruleId,
-        risk_score: riskScore,
-        description: rule.description,
-        interval: `${rule.runsEvery?.interval}${rule.runsEvery?.type}`,
-        from: `now-${rule.lookBack?.interval}${rule.lookBack?.type}`,
-        name: rule.name,
-        severity,
-        type: 'new_terms',
-        index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
-        data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
-        query: rule.customQuery,
-        new_terms_fields: rule.newTermsFields,
-        history_window_start: `now-${rule.historyWindowSize.interval}${rule.historyWindowSize.type}`,
-        enabled: true,
-        tags: rule.tags,
-      },
-      headers: { 'kbn-xsrf': 'cypress-creds' },
-    });
-  }
+  cy.request({
+    method: 'POST',
+    url: 'api/detection_engine/rules',
+    body: {
+      rule_id: ruleId,
+      risk_score: riskScore,
+      description: rule.description,
+      interval: `${rule.runsEvery?.interval}${rule.runsEvery?.type}`,
+      from: `now-${rule.lookBack?.interval}${rule.lookBack?.type}`,
+      name: rule.name,
+      severity,
+      type: 'new_terms',
+      index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
+      data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
+      query: rule.customQuery,
+      new_terms_fields: rule.newTermsFields,
+      history_window_start: `now-${rule.historyWindowSize.interval}${rule.historyWindowSize.type}`,
+      enabled: true,
+      tags: rule.tags,
+    },
+    headers: { 'kbn-xsrf': 'cypress-creds' },
+  });
 };
 
 export const createSavedQueryRule = (
@@ -207,54 +202,53 @@ export const createSavedQueryRule = (
 };
 
 export const createCustomIndicatorRule = (rule: ThreatIndicatorRule, ruleId = 'rule_testing') => {
-  if (rule.dataSource.type === 'indexPatterns') {
-    const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
-    const severity = rule.severity != null ? rule.severity.toLocaleLowerCase() : undefined;
-    const timeline = rule.timeline != null ? rule.timeline : undefined;
+  const riskScore = rule.riskScore != null ? parseInt(rule.riskScore, 10) : undefined;
+  const severity = rule.severity != null ? rule.severity.toLocaleLowerCase() : undefined;
+  const timeline = rule.timeline != null ? rule.timeline : undefined;
 
-    cy.request({
-      method: 'POST',
-      url: 'api/detection_engine/rules',
-      body: {
-        rule_id: ruleId,
-        risk_score: riskScore,
-        description: rule.description,
-        // Default interval is 1m, our tests config overwrite this to 1s
-        // See https://github.com/elastic/kibana/pull/125396 for details
-        interval: '10s',
-        name: rule.name,
-        severity,
-        type: 'threat_match',
-        timeline_id: timeline?.templateTimelineId,
-        timeline_title: timeline?.title,
-        threat_mapping: [
-          {
-            entries: [
-              {
-                field: rule.indicatorMappingField,
-                type: 'mapping',
-                value: rule.indicatorIndexField,
-              },
-            ],
-          },
-        ],
-        threat_query: '*:*',
-        threat_language: 'kuery',
-        threat_filters: [],
-        threat_index: rule.indicatorIndexPattern,
-        threat_indicator_path: rule.threatIndicatorPath,
-        from: 'now-50000h',
-        index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
-        data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
-        query: rule.customQuery || '*:*',
-        language: 'kuery',
-        enabled: true,
-        tags: rule.tags,
-      },
-      headers: { 'kbn-xsrf': 'cypress-creds' },
-      failOnStatusCode: false,
-    });
-  }
+  cy.request({
+    method: 'POST',
+    url: 'api/detection_engine/rules',
+    body: {
+      rule_id: ruleId,
+      risk_score: riskScore,
+      description: rule.description,
+      // Default interval is 1m, our tests config overwrite this to 1s
+      // See https://github.com/elastic/kibana/pull/125396 for details
+      interval: '10s',
+      name: rule.name,
+      severity,
+      type: 'threat_match',
+      timeline_id: timeline?.templateTimelineId,
+      timeline_title: timeline?.title,
+      threat_mapping: [
+        {
+          entries: [
+            {
+              field: rule.indicatorMappingField,
+              type: 'mapping',
+              value: rule.indicatorIndexField,
+            },
+          ],
+        },
+      ],
+      threat_query: '*:*',
+      threat_language: 'kuery',
+      threat_filters: [],
+      threat_index: rule.indicatorIndexPattern,
+      threat_indicator_path: rule.threatIndicatorPath,
+      from: 'now-50000h',
+      index: rule.dataSource.type === 'indexPatterns' ? rule.dataSource.index : undefined,
+      data_view_id: rule.dataSource.type === 'dataView' ? rule.dataSource.dataView : undefined,
+      query: rule.customQuery || '*:*',
+      language: 'kuery',
+      enabled: true,
+      tags: rule.tags,
+    },
+    headers: { 'kbn-xsrf': 'cypress-creds' },
+    failOnStatusCode: false,
+  });
+};
 
 export const createCustomRuleEnabled = (
   rule: CustomRule,
