288565981891255891024502fc37b051698587b9
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-09-30 17:29:43 -0700

    replaces core mock with duplicate

--- src/core/server/http_resources/http_resources_service.test.ts
+++ src/core/server/http_resources/http_resources_service.test.ts
@@ -12,6 +12,10 @@ import type { RouteConfig } from '@kbn/core-http-server';
 
 import { mockCoreContext } from '@kbn/core-base-server-mocks';
 import { httpServiceMock, httpServerMock } from '@kbn/core-http-server-mocks';
+import { savedObjectsClientMock } from '@kbn/core-saved-objects-api-server-mocks';
+import { elasticsearchServiceMock } from '@kbn/core-elasticsearch-server-mocks';
+import { uiSettingsServiceMock } from '@kbn/core-ui-settings-server-mocks';
+import { deprecationsServiceMock } from '@kbn/core-deprecations-server-mocks';
 import { coreMock } from '../mocks';
 
 import { renderingServiceMock } from '@kbn/core-rendering-server-mocks';
@@ -22,13 +26,51 @@ import type { HttpResources } from '@kbn/core-http-resources-server';
 
 const coreContext = mockCoreContext.create();
 
+function createCoreRequestHandlerContextMock() {
+  return {
+    savedObjects: {
+      client: savedObjectsClientMock.create(),
+    },
+    elasticsearch: {
+      client: elasticsearchServiceMock.createScopedClusterClient(),
+    },
+    uiSettings: {
+      client: uiSettingsServiceMock.createClient(),
+    },
+    deprecations: {
+      client: deprecationsServiceMock.createClient(),
+    },
+  };
+}
+
+export type CustomRequestHandlerMock<T> = {
+  core: Promise<ReturnType<typeof createCoreRequestHandlerContextMock>>;
+} & {
+  [Key in keyof T]: T[Key] extends Promise<unknown> ? T[Key] : Promise<T[Key]>;
+};
+
+const createCustomRequestHandlerContextMock = <T>(contextParts: T): CustomRequestHandlerMock<T> => {
+  const mock = Object.entries(contextParts).reduce(
+    (context, [key, value]) => {
+      // @ts-expect-error type matching from inferred types is hard
+      context[key] = isPromise(value) ? value : Promise.resolve(value);
+      return context;
+    },
+    {
+      core: Promise.resolve(createCoreRequestHandlerContextMock()),
+    } as CustomRequestHandlerMock<T>
+  );
+
+  return mock;
+};
+
 describe('HttpResources service', () => {
   let service: HttpResourcesService;
   let prebootDeps: PrebootDeps;
   let setupDeps: SetupDeps;
   let router: ReturnType<typeof httpServiceMock.createRouter>;
   const kibanaRequest = httpServerMock.createKibanaRequest();
-  const context = coreMock.createCustomRequestHandlerContext({});
+  const context = createCustomRequestHandlerContextMock({});
   const apmConfig = { mockApmConfig: true };
 
   beforeEach(() => {
