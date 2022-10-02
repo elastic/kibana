96ca26abd6f1e6883d65b94109c9c9e23a8573d0
wafaanasr <wafaa.aanasr@gmail.com>
2022-10-02 13:22:34 +0200

    add List header with readonly, fix tests, add hook

--- packages/kbn-securitysolution-exception-list-components/src/exception_item_card/header/header.tsx
+++ packages/kbn-securitysolution-exception-list-components/src/exception_item_card/header/header.tsx
@@ -6,18 +6,10 @@
  * Side Public License, v 1.
  */
 
-import React, { memo, useMemo, useState } from 'react';
-import type { EuiContextMenuPanelProps } from '@elastic/eui';
-import {
-  EuiButtonIcon,
-  EuiContextMenuPanel,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiPopover,
-  EuiTitle,
-  EuiContextMenuItem,
-} from '@elastic/eui';
+import React, { memo } from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiTitle } from '@elastic/eui';
 import type { ExceptionListItemSchema } from '@kbn/securitysolution-io-ts-list-types';
+import { HeaderMenu } from '../../header_menu';
 
 export interface ExceptionItemCardHeaderProps {
   item: ExceptionListItemSchema;
@@ -28,52 +20,20 @@ export interface ExceptionItemCardHeaderProps {
 
 export const ExceptionItemCardHeader = memo<ExceptionItemCardHeaderProps>(
   ({ item, actions, disableActions = false, dataTestSubj }) => {
-    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-
-    const onItemActionsClick = () => setIsPopoverOpen((isOpen) => !isOpen);
-    const onClosePopover = () => setIsPopoverOpen(false);
-
-    const itemActions = useMemo((): EuiContextMenuPanelProps['items'] => {
-      return actions.map((action) => (
-        <EuiContextMenuItem
-          data-test-subj={`${dataTestSubj}ActionItem${action.key}`}
-          key={action.key}
-          icon={action.icon}
-          onClick={() => {
-            onClosePopover();
-            action.onClick();
-          }}
-        >
-          {action.label}
-        </EuiContextMenuItem>
-      ));
-    }, [dataTestSubj, actions]);
-
     return (
-      <EuiFlexGroup data-test-subj={dataTestSubj} justifyContent="spaceBetween">
+      <EuiFlexGroup responsive data-test-subj={dataTestSubj} justifyContent="spaceBetween">
         <EuiFlexItem grow={9}>
           <EuiTitle size="xs" textTransform="uppercase" data-test-subj={`${dataTestSubj}Title`}>
             <h3>{item.name}</h3>
           </EuiTitle>
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
-          <EuiPopover
-            button={
-              <EuiButtonIcon
-                isDisabled={disableActions}
-                aria-label="Exception item actions menu"
-                iconType="boxesHorizontal"
-                onClick={onItemActionsClick}
-                data-test-subj={`${dataTestSubj}ActionButton`}
-              />
-            }
-            panelPaddingSize="none"
-            isOpen={isPopoverOpen}
-            closePopover={onClosePopover}
-            data-test-subj={`${dataTestSubj}Items`}
-          >
-            <EuiContextMenuPanel size="s" items={itemActions} />
-          </EuiPopover>
+          <HeaderMenu
+            disableActions={disableActions}
+            actions={actions}
+            aria-label="Exception item actions menu"
+            dataTestSubj={dataTestSubj}
+          />
         </EuiFlexItem>
       </EuiFlexGroup>
     );
