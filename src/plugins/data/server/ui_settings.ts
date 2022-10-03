82be3af99fc558064b9403808ec490674d9e6f56
Liza Katz <lizka.k@gmail.com>
2022-10-03 16:11:45 +0300

    Update src/plugins/data/server/ui_settings.ts
    Co-authored-by: Jonathan Budzenski <jon@budzenski.me>

--- src/plugins/data/server/ui_settings.ts
+++ src/plugins/data/server/ui_settings.ts
@@ -548,7 +548,7 @@ export function getUiSettings(
         defaultMessage: 'Async search wait for completion',
       }),
       value: 100,
-      description: i18n.translate('data.advancedSettings.asyncSearchWaitForCompletioMsnDesc', {
+      description: i18n.translate('data.advancedSettings.asyncSearchWaitForCompletionMsDesc', {
         defaultMessage:
           'Change the amount of time kibana awaits responses from Elasticsearch. If results are not returned within that time, Elasticsearch will return partial results and Kibana will poll again later.',
       }),
