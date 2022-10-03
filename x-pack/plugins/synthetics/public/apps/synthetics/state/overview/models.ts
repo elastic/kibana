bcb5038aa62cc8127de8d420c1fe2f0f140f8c9e
Dominique Clarke <dominique.clarke@elastic.co>
2022-10-03 15:10:44 -0400

    merge main

--- x-pack/plugins/synthetics/public/apps/synthetics/state/overview/models.ts
+++ x-pack/plugins/synthetics/public/apps/synthetics/state/overview/models.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { MonitorOverviewResult } from '../../../../../common/runtime_types';
+import { MonitorOverviewResult, OverviewStatus } from '../../../../../common/runtime_types';
 
 import { IHttpSerializedFetchError } from '../utils/http_error';
 
@@ -22,4 +22,6 @@ export interface MonitorOverviewState {
   loading: boolean;
   loaded: boolean;
   error: IHttpSerializedFetchError | null;
+  status: OverviewStatus | null;
+  statusError: IHttpSerializedFetchError | null;
 }
