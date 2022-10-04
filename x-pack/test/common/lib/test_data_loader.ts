95e32aaab5c15bc4492dfa41fa426c69f7c8b9bb
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-10-04 07:46:20 -0600

    Removing esArchiver in favor of testDataLoader for `bulk_get` Saved Objects integration tests (#140998) (#142600)
    * Removing esArchiver in favor of testDataLoader
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * Adding test data for loader
    
    * [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'
    
    * Adding generic TestDataLoader
    
    * Importing just the type per PR feedback
    
    * Changing testDataLoader function names to be more descriptive
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Larry Gregory <larry.gregory@elastic.co>
    Co-authored-by: Thomas Watson <watson@elastic.co>
    (cherry picked from commit 6875d18d0f1340d684e234ef866970bcb9ed087b)
    
    Co-authored-by: Kurt <kc13greiner@users.noreply.github.com>

--- x-pack/test/common/lib/test_data_loader.ts
+++ x-pack/test/common/lib/test_data_loader.ts
@@ -5,16 +5,14 @@
  * 2.0.
  */
 
-import { FtrProviderContext } from '../ftr_provider_context';
-
-const SPACE_1 = {
+export const SPACE_1 = {
   id: 'space_1',
   name: 'Space 1',
   description: 'This is the first test space',
   disabledFeatures: [],
 };
 
-const SPACE_2 = {
+export const SPACE_2 = {
   id: 'space_2',
   name: 'Space 2',
   description: 'This is the second test space',
@@ -64,36 +62,38 @@ const OBJECTS_TO_SHARE: Array<{
   },
 ];
 
-export function getTestDataLoader({ getService }: FtrProviderContext) {
+// @ts-ignore
+export function getTestDataLoader({ getService }) {
   const spacesService = getService('spaces');
   const kbnServer = getService('kibanaServer');
   const supertest = getService('supertest');
   const log = getService('log');
 
   return {
-    before: async () => {
+    createFtrSpaces: async () => {
       await Promise.all([await spacesService.create(SPACE_1), await spacesService.create(SPACE_2)]);
     },
 
-    after: async () => {
+    deleteFtrSpaces: async () => {
       await Promise.all([spacesService.delete(SPACE_1.id), spacesService.delete(SPACE_2.id)]);
     },
 
-    beforeEach: async () => {
+    createFtrSavedObjectsData: async (
+      spaceData: Array<{ spaceName: string | null; dataUrl: string }>
+    ) => {
       log.debug('Loading test data for the following spaces: default, space_1 and space_2');
-      await Promise.all([
-        kbnServer.importExport.load(
-          'x-pack/test/spaces_api_integration/common/fixtures/kbn_archiver/default_space.json'
-        ),
-        kbnServer.importExport.load(
-          'x-pack/test/spaces_api_integration/common/fixtures/kbn_archiver/space_1.json',
-          { space: SPACE_1.id }
-        ),
-        kbnServer.importExport.load(
-          'x-pack/test/spaces_api_integration/common/fixtures/kbn_archiver/space_2.json',
-          { space: SPACE_2.id }
-        ),
-      ]);
+
+      await Promise.all(
+        spaceData.map((spaceDataObj) => {
+          if (spaceDataObj.spaceName) {
+            return kbnServer.importExport.load(spaceDataObj.dataUrl, {
+              space: spaceDataObj.spaceName,
+            });
+          } else {
+            return kbnServer.importExport.load(spaceDataObj.dataUrl);
+          }
+        })
+      );
 
       // Adjust spaces for the imported saved objects.
       for (const { objects, spacesToAdd = [], spacesToRemove = [] } of OBJECTS_TO_SHARE) {
@@ -111,9 +111,9 @@ export function getTestDataLoader({ getService }: FtrProviderContext) {
       }
     },
 
-    afterEach: async () => {
+    deleteFtrSavedObjectsData: async () => {
       const allSpacesIds = [
-        ...(await spacesService.getAll()).map((space) => space.id),
+        ...(await spacesService.getAll()).map((space: { id: string }) => space.id),
         'non_existent_space',
       ];
       log.debug(`Removing data from the following spaces: ${allSpacesIds.join(', ')}`);
