e1980e6c68e228a268a3323c0daacae60ad1d3ff
John Dorlus <jsdorlus@elastic.co>
2022-10-04 16:13:24 -0400

    Added more testing stuff.

--- x-pack/test/functional/apps/remote_clusters/ccs/remote_clusters_index_management_flow.ts
+++ x-pack/test/functional/apps/remote_clusters/ccs/remote_clusters_index_management_flow.ts
@@ -9,32 +9,45 @@ import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default ({ getPageObjects, getService }: FtrProviderContext) => {
-  const pageObjects = getPageObjects(['common', 'remoteClusters']);
+  const pageObjects = getPageObjects(['common', 'remoteClusters', 'indexManagement']);
   const security = getService('security');
   const retry = getService('retry');
   const testSubjects = getService('testSubjects');
+  const remoteEs = getService('remoteEs' as 'es');
 
   describe('CCS Remote Clusters > Index Management', function () {
     before(async () => {
       await security.testUser.setRoles(['global_ccr_role']);
-      await pageObjects.common.navigateToApp('remoteClusters');
     });
 
     after(async () => {
       await security.testUser.restoreDefaults();
     });
 
-    it('Verify ftr-remote in remote clusters', async () => {
-      await retry.waitFor('table to be visible', async () => {
-        return await testSubjects.isDisplayed('remoteClusterListTable');
+    describe('Remote Clusters', function () {
+      before(async () => {
+        await pageObjects.common.navigateToApp('remoteClusters');
       });
-      const remotes = await pageObjects.remoteClusters.getRemoteClustersList();
-      expect(remotes.length).to.eql(1);
-      expect(remotes[0].remoteName).to.eql('ftr-remote');
-      expect(remotes[0].remoteAddress).to.contain('localhost');
-      expect(remotes[0].remoteStatus).to.eql('Connected');
-      expect(remotes[0].remoteConnectionCount).to.eql('1');
-      expect(remotes[0].remoteMode).to.eql('default');
+
+      it('Verify ftr-remote in remote clusters', async () => {
+        await retry.waitFor('table to be visible', async () => {
+          return await testSubjects.isDisplayed('remoteClusterListTable');
+        });
+        const remotes = await pageObjects.remoteClusters.getRemoteClustersList();
+        expect(remotes.length).to.eql(1);
+        expect(remotes[0].remoteName).to.eql('ftr-remote');
+        expect(remotes[0].remoteAddress).to.contain('localhost');
+        expect(remotes[0].remoteStatus).to.eql('Connected');
+        expect(remotes[0].remoteConnectionCount).to.eql('1');
+        expect(remotes[0].remoteMode).to.eql('default');
+      });
+    });
+
+    describe('Index Mangement', function () {
+      before(async () => {
+        // await remoteEs.
+        await pageObjects.common.navigateToApp('indexManagement');
+      })
     });
   });
 };
