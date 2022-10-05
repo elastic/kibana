7dadc09d0b9172bee18c993349f458c4b70f6412
Dima Arnautov <dmitrii.arnautov@elastic.co>
2022-10-05 18:25:59 +0200

    all options, preserve state wip

--- x-pack/plugins/ml/public/application/notifications/components/entity_filter.tsx
+++ x-pack/plugins/ml/public/application/notifications/components/entity_filter.tsx
@@ -6,67 +6,76 @@
  */
 
 import React, { type FC, useState, useCallback } from 'react';
+import { uniqBy } from 'lodash';
 import { EuiFilterButton, EuiPanel, EuiPopover } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n-react';
 import type { CustomComponentProps } from '@elastic/eui/src/components/search_bar/filters/custom_component_filter';
 import { MlEntitySelector, MlEntitySelectorProps } from './ml_entity_selector';
 
-export const EntityFilter: FC<Required<CustomComponentProps>> = React.memo(
-  ({ query, onChange }) => {
-    const [isOpen, setIsOpen] = useState(false);
+/**
+ * Custom filter component to use with {@link EuiInMemoryTable}
+ */
+export const EntityFilter: FC<CustomComponentProps> = React.memo(({ query, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const hasActiveFilters = query.hasOrFieldClause('job_id');
-    const fieldClauses = query.getOrFieldClause('job_id');
+  const hasActiveFilters = query.hasOrFieldClause('job_id') || query.hasSimpleFieldClause('job_id');
+  const fieldClauses = query.getOrFieldClause('job_id');
 
-    const numActiveFilters = Array.isArray(fieldClauses?.value)
-      ? fieldClauses!.value.length
-      : !!fieldClauses?.value
-      ? 1
-      : 0;
+  const numActiveFilters = Array.isArray(fieldClauses?.value)
+    ? fieldClauses!.value.length
+    : !!fieldClauses?.value
+    ? 1
+    : 0;
 
-    const closePopover = () => {
-      setIsOpen(false);
-    };
+  const closePopover = setIsOpen.bind(null, false);
 
-    const onSelectionChange = useCallback<
-      Exclude<MlEntitySelectorProps['onSelectionChange'], undefined>
-    >(
-      (entitiesSelection) => {
-        const newQuery = entitiesSelection.reduce((acc, curr) => {
-          return acc.addOrFieldValue('job_id', curr.id, true, 'eq');
-        }, query);
+  const selectedOptions = (
+    Array.isArray(fieldClauses?.value) ? fieldClauses!.value : []
+  ) as MlEntitySelectorProps['selectedOptions'];
 
-        onChange(newQuery);
-      },
-      [onChange, query]
-    );
+  const onSelectionChange = useCallback<
+    Exclude<MlEntitySelectorProps['onSelectionChange'], undefined>
+  >(
+    (entitiesSelection) => {
+      // first clean up
+      let newQuery = query.removeOrFieldClauses('job_id');
+      newQuery = newQuery.removeSimpleFieldClauses('job_id');
 
-    const button = (
-      <EuiFilterButton
-        iconType="arrowDown"
-        iconSide="right"
-        onClick={() => setIsOpen((prev) => !prev)}
-        hasActiveFilters={hasActiveFilters}
-        numActiveFilters={hasActiveFilters ? numActiveFilters : undefined}
-        grow
-      >
-        Custom
-      </EuiFilterButton>
-    );
+      if (entitiesSelection.length > 0) {
+        newQuery = uniqBy(entitiesSelection, 'value').reduce((acc, curr) => {
+          return acc.addOrFieldValue('job_id', curr.value, true, 'eq');
+        }, newQuery);
+      }
 
-    return (
-      <>
-        <EuiPopover
-          button={button}
-          isOpen={isOpen}
-          closePopover={closePopover}
-          panelPaddingSize="none"
-          anchorPosition="downCenter"
-        >
-          <EuiPanel paddingSize="s" css={{ width: '300px' }}>
-            <MlEntitySelector onSelectionChange={onSelectionChange} />
-          </EuiPanel>
-        </EuiPopover>
-      </>
-    );
-  }
-);
+      onChange!(newQuery);
+    },
+    [onChange, query]
+  );
+
+  const button = (
+    <EuiFilterButton
+      iconType="arrowDown"
+      iconSide="right"
+      onClick={setIsOpen.bind(null, (prev) => !prev)}
+      hasActiveFilters={hasActiveFilters}
+      numActiveFilters={hasActiveFilters ? numActiveFilters : undefined}
+      grow
+    >
+      <FormattedMessage id="xpack.ml.notifications.entityFilter" defaultMessage="Entity" />
+    </EuiFilterButton>
+  );
+
+  return (
+    <EuiPopover
+      button={button}
+      isOpen={isOpen}
+      closePopover={closePopover}
+      panelPaddingSize="none"
+      anchorPosition="downCenter"
+    >
+      <EuiPanel paddingSize="s" css={{ width: '300px' }}>
+        <MlEntitySelector selectedOptions={selectedOptions} onSelectionChange={onSelectionChange} />
+      </EuiPanel>
+    </EuiPopover>
+  );
+});
--- x-pack/plugins/ml/public/application/notifications/components/ml_entity_selector.tsx
+++ x-pack/plugins/ml/public/application/notifications/components/ml_entity_selector.tsx
@@ -5,72 +5,116 @@
  * 2.0.
  */
 
-import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
-import { EuiComboBox, type EuiComboBoxOptionOption, type EuiComboBoxProps } from '@elastic/eui';
+import React, { FC, useCallback, useEffect, useState, useMemo } from 'react';
+import {
+  EuiComboBox,
+  type EuiComboBoxOptionOption,
+  type EuiComboBoxProps,
+  euiPaletteColorBlindBehindText,
+} from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n-react';
 import { useMlApiContext } from '../../contexts/kibana';
 
 type EntityType = 'anomaly_detector' | 'data_frame_analytics' | 'trained_models';
 
-type EntitiesSelection = Array<{ id: string; type: EntityType }>;
+type EntitiesSelection = Array<{ value: string; label: string }>;
 
 export interface MlEntitySelectorProps {
-  entityTypes?: EntityType[];
+  entityTypes?: Record<EntityType, boolean>;
   multiSelect?: boolean;
-  selectedOptions?: any;
+  /**
+   * Array of selected ids
+   */
+  selectedOptions?: string[];
   onSelectionChange?: (jobSelection: EntitiesSelection) => void;
 }
 
 export const MlEntitySelector: FC<MlEntitySelectorProps> = ({
-  entityTypes = ['anomaly_detector', 'data_frame_analytics', 'trained_models'],
+  entityTypes = { anomaly_detector: true, data_frame_analytics: true, trained_models: true },
   multiSelect = true,
   selectedOptions,
   onSelectionChange,
 }) => {
   const { jobs: jobsApi, trainedModels, dataFrameAnalytics } = useMlApiContext();
 
-  const jobIds = useMemo(() => new Set(), []);
-  const groupIds = useMemo(() => new Set(), []);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [options, setOptions] = useState<Array<EuiComboBoxOptionOption<string>>>([]);
+
+  const visColorsBehindText = euiPaletteColorBlindBehindText();
 
   const fetchOptions = useCallback(async () => {
     try {
-      const { jobIds: jobIdOptions, groupIds: groupIdOptions } =
-        await jobsApi.getAllJobAndGroupIds();
+      const newOptions: Array<EuiComboBoxOptionOption<string>> = [];
+      if (entityTypes?.anomaly_detector) {
+        const { jobIds: jobIdOptions } = await jobsApi.getAllJobAndGroupIds();
 
-      jobIdOptions.forEach((v) => {
-        jobIds.add(v);
-      });
-      groupIdOptions.forEach((v) => {
-        groupIds.add(v);
-      });
-
-      setOptions([
-        {
-          label: i18n.translate('xpack.ml.jobSelector.jobOptionsLabel', {
-            defaultMessage: 'Jobs',
+        newOptions.push({
+          label: i18n.translate('xpack.ml.mlEntitySelector.jobOptionsLabel', {
+            defaultMessage: 'Anomaly detection jobs',
           }),
           isGroupLabelOption: true,
-          options: jobIdOptions.map((v) => ({ label: v, value: v })),
-        },
-        ...(multiSelect
-          ? [
-              {
-                label: i18n.translate('xpack.ml.jobSelector.groupOptionsLabel', {
-                  defaultMessage: 'Groups',
-                }),
-                isGroupLabelOption: true,
-                options: groupIdOptions.map((v) => ({ label: v, value: v })),
-              },
-            ]
-          : []),
-      ]);
+          color: visColorsBehindText[0],
+          options: jobIdOptions.map((v) => ({
+            label: v,
+            value: v,
+            key: `anomaly_detector:${v}`,
+            color: visColorsBehindText[0],
+          })),
+        });
+      }
+
+      if (entityTypes?.data_frame_analytics) {
+        const dfa = await dataFrameAnalytics.getDataFrameAnalytics();
+        if (dfa.count > 0) {
+          newOptions.push({
+            label: i18n.translate('xpack.ml.mlEntitySelector.dfaOptionsLabel', {
+              defaultMessage: 'Data frame analytics',
+            }),
+            isGroupLabelOption: true,
+            options: dfa.data_frame_analytics.map((v) => ({
+              label: v.id,
+              value: v.id,
+              key: `data_frame_analytics:${v.id}`,
+              color: visColorsBehindText[2],
+            })),
+          });
+        }
+      }
+
+      if (entityTypes?.trained_models) {
+        const models = await trainedModels.getTrainedModels();
+        if (models.length > 0) {
+          newOptions.push({
+            label: i18n.translate('xpack.ml.mlEntitySelector.trainedModelsLabel', {
+              defaultMessage: 'Trained models',
+            }),
+            isGroupLabelOption: true,
+            options: models.map((v) => ({
+              label: v.model_id,
+              value: v.model_id,
+              key: `trained_models:${v.model_id}`,
+              color: visColorsBehindText[3],
+            })),
+          });
+        }
+      }
+
+      setOptions(newOptions);
     } catch (e) {
       // TODO add error handling
     }
+    setIsLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [jobsApi]);
+  }, [jobsApi, trainedModels, dataFrameAnalytics]);
+
+  const selectedEntities = useMemo<Array<EuiComboBoxOptionOption<string>>>(() => {
+    return (selectedOptions ?? []).flatMap((o) => {
+      const fromOptions = options
+        .flatMap((g) => g.options)
+        .filter((op): op is EuiComboBoxOptionOption<string> => op!.value === o);
+      return fromOptions.length > 0 ? fromOptions : [{ value: o, label: o, key: `unknown:${o}` }];
+    });
+  }, [options, selectedOptions]);
 
   useEffect(
     function fetchOptionsOnMount() {
@@ -82,15 +126,21 @@ export const MlEntitySelector: FC<MlEntitySelectorProps> = ({
   const onChange = useCallback<Exclude<EuiComboBoxProps<string>['onChange'], undefined>>(
     (selection) => {
       if (!onSelectionChange) return;
-      onSelectionChange(selection.map((s) => ({ id: s.value as string, type: '' as EntityType })));
+      onSelectionChange(
+        selection.map((s) => {
+          return { value: s.value!, label: s.label };
+        })
+      );
     },
     [onSelectionChange]
   );
 
   return (
     <EuiComboBox<string>
+      autoFocus={true}
+      isLoading={isLoading}
       singleSelection={!multiSelect}
-      selectedOptions={selectedOptions}
+      selectedOptions={selectedEntities}
       options={options}
       onChange={onChange}
       fullWidth
