117b5045b8ddd362ecf0f60ee86d4f6e93026e4e
gbamparop <georgios.bamparopoulos@elastic.co>
2022-10-05 17:06:15 +0100

    Disable recording and test parallelization in the flaky test runner

--- .buildkite/scripts/steps/functional/apm_cypress.sh
+++ .buildkite/scripts/steps/functional/apm_cypress.sh
@@ -12,14 +12,17 @@ APM_CYPRESS_RECORD_KEY="$(retry 5 5 vault read -field=CYPRESS_RECORD_KEY secret/
 export JOB=kibana-apm-cypress
 CLI_NUMBER=$((BUILDKITE_PARALLEL_JOB+1))
 
+# Disable parallel tests and dashboard recording when running them in the flaky test runner
+if [[ "$CLI_COUNT" -ne 1 ]]; then
+  CYPRESS_DASHBOARD_ARGS="--record --key "$APM_CYPRESS_RECORD_KEY""
+  CYPRESS_PARALLELIZATION_ARGS="--parallel --ci-build-id "${BUILDKITE_BUILD_ID}""
+fi
+
 echo "--- APM Cypress Tests"
 
 cd "$XPACK_DIR"
 
 checks-reporter-with-killswitch "APM Cypress Tests $CLI_NUMBER" \
   node plugins/apm/scripts/test/e2e.js \
-  --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
-  --record \
-  --key "$APM_CYPRESS_RECORD_KEY" \
-  --parallel \
-  --ci-build-id "${BUILDKITE_BUILD_ID}"
+  $CYPRESS_DASHBOARD_ARGS \
+  $CYPRESS_PARALLELIZATION_ARGS
