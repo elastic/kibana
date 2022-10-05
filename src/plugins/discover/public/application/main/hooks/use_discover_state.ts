1462ee1224869cb9f56ec6d1463f71f31f134bb8
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-10-05 13:44:07 +0300

    [Discover] fix checks

--- src/plugins/discover/public/application/main/hooks/use_discover_state.ts
+++ src/plugins/discover/public/application/main/hooks/use_discover_state.ts
@@ -132,7 +132,6 @@ export function useDiscoverState({
    */
   const { adHocDataViewList, persistDataView, updateAdHocDataViewId } = useAdHocDataViews({
     dataView,
-    dataViews,
     stateContainer,
     savedSearch,
     setUrlTracking,
