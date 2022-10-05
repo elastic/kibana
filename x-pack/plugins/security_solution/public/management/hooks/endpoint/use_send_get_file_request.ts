089d85f9e62b77fe14c6a5be3a1805e83fcd312b
Paul Tavares <paul.tavares@elastic.co>
2022-10-05 14:44:04 -0400

    Hook to handle creating a `get-file` action request

---
+++ x-pack/plugins/security_solution/public/management/hooks/endpoint/use_send_get_file_request.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { UseMutationOptions, UseMutationResult } from '@tanstack/react-query';
+import type { IHttpFetchError } from '@kbn/core-http-browser';
+import { useMutation } from '@tanstack/react-query';
+import type { ResponseActionApiResponse } from '../../../../common/endpoint/types';
+import type { GetFileRequestRequestBody } from '../../../../common/endpoint/schema/actions';
+import { KibanaServices } from '../../../common/lib/kibana';
+import { GET_FILE_ROUTE } from '../../../../common/endpoint/constants';
+
+export const useSendGetFileRequest = (
+  customOptions?: UseMutationOptions<
+    ResponseActionApiResponse,
+    IHttpFetchError,
+    GetFileRequestRequestBody
+  >
+): UseMutationResult<ResponseActionApiResponse, IHttpFetchError, GetFileRequestRequestBody> => {
+  return useMutation<ResponseActionApiResponse, IHttpFetchError, GetFileRequestRequestBody>(
+    (reqBody) => {
+      return KibanaServices.get().http.post<ResponseActionApiResponse>(GET_FILE_ROUTE, {
+        body: JSON.stringify(reqBody),
+      });
+    },
+    customOptions
+  );
+};
