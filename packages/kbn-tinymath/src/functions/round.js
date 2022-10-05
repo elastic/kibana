0bb5e4ddccf3900b76bdfc7c70fa3ff7b683d852
dej611 <marco.liberati@elastic.co>
2022-10-05 12:35:27 +0200

    :ok_hand: Integrate more feedback

--- packages/kbn-tinymath/src/functions/round.js
+++ packages/kbn-tinymath/src/functions/round.js
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-const rounder = (a, b) => Math.round(a * Math.pow(10, b)) / Math.pow(10, b);
+const rounder = (a, b = 0) => Math.round(a * Math.pow(10, b)) / Math.pow(10, b);
 
 /**
  * Rounds a number towards the nearest integer by default or decimal place if specified. For arrays, the function will be applied index-wise to each element.
@@ -24,8 +24,8 @@ const rounder = (a, b) => Math.round(a * Math.pow(10, b)) / Math.pow(10, b);
 
 function round(a, b) {
   if (Array.isArray(a)) {
-    return a.map((a) => rounder(a, b || 0));
+    return a.map((a) => rounder(a, b));
   }
-  return rounder(a, b || 0);
+  return rounder(a, b);
 }
 module.exports = { round };
