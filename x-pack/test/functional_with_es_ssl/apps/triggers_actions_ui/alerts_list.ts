5624686562a9448379d32fcf0900e42b9cc4589f
Alexandra Doak <alexandra.doak@elastic.co>
2022-10-06 09:52:13 -0400

    Fixing test

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alerts_list.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alerts_list.ts
@@ -30,8 +30,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     await testSubjects.click('rulesTab');
   }
 
-  // Failing: See https://github.com/elastic/kibana/issues/141093
-  describe.skip('rules list', function () {
+  describe('rules list', function () {
     const assertRulesLength = async (length: number) => {
       return await retry.try(async () => {
         const rules = await pageObjects.triggersActionsUI.getAlertsList();
@@ -198,7 +197,9 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
       await testSubjects.click('collapsedItemActions');
 
-      await testSubjects.click('disableButton');
+      await retry.waitForWithTimeout('disable button to show up', 30000, async () => {
+        return await testSubjects.isDisplayed('disableButton');
+      });
 
       await refreshAlertsList();
       await find.waitForDeletedByCssSelector('.euiBasicTable-loading');
