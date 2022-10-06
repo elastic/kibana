e787f6265d80844ed2b04f0b42fe22be20a53b04
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2022-10-06 18:13:52 +0200

    Fix uncompressed stream.

--- x-pack/plugins/aiops/server/routes/explain_log_rate_spikes.ts
+++ x-pack/plugins/aiops/server/routes/explain_log_rate_spikes.ts
@@ -94,8 +94,7 @@ export const defineExplainLogRateSpikesRoute = (
         end();
       }
 
-      // Async IIFE to run the analysis while not blocking returning `responseWithHeaders`.
-      (async () => {
+      async function runAnalysis() {
         push(resetAction());
         push(
           updateLoadingStateAction({
@@ -292,7 +291,11 @@ export const defineExplainLogRateSpikesRoute = (
         }
 
         endWithUpdatedLoadingState();
-      })();
+      }
+
+      // Do not call this using `await` so it will run asynchronously while we return the stream already.
+      // The timeout is used because the response needs to be passed on to the client before we start pushing to the stream.
+      setTimeout(() => runAnalysis(), 100);
 
       return response.ok(responseWithHeaders);
     }
