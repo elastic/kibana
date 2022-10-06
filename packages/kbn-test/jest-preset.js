b538afc01f26c7d455f5f05307aa8623c1a6e31d
Patryk Kopycinski <contact@patrykkopycinski.com>
2022-10-06 11:51:22 +0200

    revert

--- packages/kbn-test/jest-preset.js
+++ packages/kbn-test/jest-preset.js
@@ -118,7 +118,7 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.(js|tsx?)$': 'ts-jest',
+    '^.+\\.(js|tsx?)$': '<rootDir>/node_modules/@kbn/test/target_node/src/jest/babel_transform.js',
     '^.+\\.txt?$': 'jest-raw-loader',
     '^.+\\.html?$': 'jest-raw-loader',
   },
