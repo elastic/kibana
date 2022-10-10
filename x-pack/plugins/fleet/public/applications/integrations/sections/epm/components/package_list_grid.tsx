26259cd3888756edd36395487113e6725cd79278
Mark Hopkin <mark.hopkin@elastic.co>
2022-10-10 15:34:07 +0100

    Revert "put new functionality behind feature flag"
    This reverts commit ef210f22e03fc2b75e1af3a23c500d6eed3d6327.

--- x-pack/plugins/fleet/public/applications/integrations/sections/epm/components/package_list_grid.tsx
+++ x-pack/plugins/fleet/public/applications/integrations/sections/epm/components/package_list_grid.tsx
@@ -34,8 +34,6 @@ import type { IntegrationCardItem } from '../../../../../../common/types/models'
 
 import type { ExtendedIntegrationCategory, CategoryFacet } from '../screens/home/category_facets';
 
-import { ExperimentalFeaturesService } from '../../../services';
-
 import { promoteFeaturedIntegrations } from './utils';
 
 import { PackageCard } from './package_card';
@@ -77,7 +75,7 @@ export const PackageListGrid: FunctionComponent<Props> = ({
   const [isSticky, setIsSticky] = useState(false);
   const [windowScrollY] = useState(window.scrollY);
   const { euiTheme } = useEuiTheme();
-  const { noLargeFeaturedIntegrations } = ExperimentalFeaturesService.get();
+
   useEffect(() => {
     const menuRefCurrent = menuRef.current;
     const onScroll = () => {
@@ -113,10 +111,8 @@ export const PackageListGrid: FunctionComponent<Props> = ({
         )
       : list;
 
-    return noLargeFeaturedIntegrations
-      ? promoteFeaturedIntegrations(filteredList, selectedCategory)
-      : filteredList;
-  }, [isLoading, list, localSearchRef, noLargeFeaturedIntegrations, searchTerm, selectedCategory]);
+    return promoteFeaturedIntegrations(filteredList, selectedCategory);
+  }, [isLoading, list, localSearchRef, searchTerm, selectedCategory]);
 
   const controlsContent = <ControlsColumn title={title} controls={controls} sticky={isSticky} />;
   let gridContent: JSX.Element;
@@ -135,7 +131,7 @@ export const PackageListGrid: FunctionComponent<Props> = ({
 
   return (
     <>
-      {!noLargeFeaturedIntegrations && featuredList}
+      {featuredList}
       <div ref={menuRef}>
         <EuiFlexGroup
           alignItems="flexStart"
