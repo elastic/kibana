1f5bec897c011079b29d6a71fdd2b29d4d58cbd1
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2022-10-10 12:54:18 +0200

    Adds backpressure handling.

--- x-pack/packages/ml/aiops_utils/src/stream_factory.ts
+++ x-pack/packages/ml/aiops_utils/src/stream_factory.ts
@@ -14,6 +14,12 @@ import type { Headers, ResponseHeaders } from '@kbn/core-http-server';
 
 import { acceptCompression } from './accept_compression';
 
+const flushPayloadSize = 4 * 1024;
+const highWaterMark = 512 * 1024;
+const zLibChunkSize = 512 * 1024;
+
+const flushPayload = crypto.randomBytes(flushPayloadSize).toString('hex');
+
 // We need this otherwise Kibana server will crash with a 'ERR_METHOD_NOT_IMPLEMENTED' error.
 class ResponseStream extends Stream.PassThrough {
   flush() {}
@@ -69,13 +75,34 @@ export function streamFactory<T = unknown>(
   let streamType: StreamType;
   const isCompressed = compressOverride && acceptCompression(headers);
 
-  const stream = isCompressed ? zlib.createGzip() : new ResponseStream();
+  const stream = isCompressed
+    ? zlib.createGzip({ chunkSize: zLibChunkSize })
+    : new ResponseStream({
+        readableHighWaterMark: highWaterMark,
+        writableHighWaterMark: highWaterMark,
+      });
+
+  const backPressureBuffer: T[] = [];
+  let waitForDrain = false;
+  let tryToEnd = false;
 
   function end() {
+    tryToEnd = true;
+    logger.info(`backPressureBuffer BEFORE END: ${backPressureBuffer.length}`);
+    // Before ending the stream, we need to empty the backPressureBuffer
+    if (backPressureBuffer.length > 0) {
+      const el = backPressureBuffer.shift();
+      if (el !== undefined) {
+        push(el, true);
+      }
+      return;
+    }
+    logger.info('ENDENDENDENDENDENDENDEND');
     stream.end();
   }
 
-  function push(d: T) {
+  function push(d: T, drain = false) {
+    logger.info(`PUSH - backPressure: ${backPressureBuffer.length}, drain: ${drain}`);
     if (d === undefined) {
       logger.error('Stream chunk must not be undefined.');
       return;
@@ -92,25 +119,66 @@ export function streamFactory<T = unknown>(
       return;
     }
 
+    if (backPressureBuffer.length > 0 && drain === false) {
+      logger.info('BACKPRESSURE!!');
+      backPressureBuffer.push(d);
+      return;
+    }
+
     try {
       const line =
         streamType === 'ndjson'
           ? `${JSON.stringify({
               ...d,
               // This is a temporary fix for response streaming with proxy configurations that buffer responses up to 4KB in size.
-              ...(flushFix ? { flushPayload: crypto.randomBytes(4096).toString('hex') } : {}),
+              ...(flushFix ? { flushPayload } : {}),
             })}${DELIMITER}`
           : d;
-      stream.write(line);
+      const writeResult = stream.write(line, () => {
+        // The data has been passed to zlib, but the compression algorithm may
+        // have decided to buffer the data for more efficient compression.
+        // Calling .flush() will make the data available as soon as the client
+        // is ready to receive it.
+
+        // Calling .flush() on a compression stream will
+        // make zlib return as much output as currently possible.
+        if (typeof stream.flush === 'function') {
+          stream.flush();
+        }
+      });
+      logger.info(`writeResult: ${writeResult}`);
+
+      if (!writeResult) {
+        if (drain) {
+          backPressureBuffer.unshift(d);
+        } else {
+          backPressureBuffer.push(d);
+        }
+
+        logger.info(`ADD DRAIN?: ${!waitForDrain}`);
+        if (!waitForDrain) {
+          waitForDrain = true;
+          stream.once('drain', () => {
+            logger.info('DRAIN!!!');
+            waitForDrain = false;
+            const el = backPressureBuffer.shift();
+            if (el !== undefined) {
+              push(el, true);
+            }
+          });
+        }
+      } else if (writeResult && drain && backPressureBuffer.length > 0) {
+        logger.info('MANUAL DRAIN!!!');
+        const el = backPressureBuffer.shift();
+        if (el !== undefined) {
+          push(el, true);
+        }
+      } else if (writeResult && tryToEnd) {
+        end();
+      }
     } catch (e) {
       logger.error(`Could not serialize or stream data chunk: ${e.toString()}`);
       return;
-    }
-
-    // Calling .flush() on a compression stream will
-    // make zlib return as much output as currently possible.
-    if (isCompressed) {
-      stream.flush();
     }
   }
 
