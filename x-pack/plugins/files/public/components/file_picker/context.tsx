42459a21e0839ad33234d96d0c01ee561c0c6648
Jean-Louis Leysens <jloleysens@gmail.com>
2022-10-10 14:50:56 +0200

    expanded files context;

--- x-pack/plugins/files/public/components/file_picker/context.tsx
+++ x-pack/plugins/files/public/components/file_picker/context.tsx
@@ -5,11 +5,31 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { createContext, useContext, useMemo } from 'react';
 import type { FunctionComponent } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { FilePickerState, createFilePickerState } from './file_picker_state';
+
+interface FilePickerContextValue {
+  state: FilePickerState;
+}
+
+const FilePickerCtx = createContext<FilePickerContextValue>(
+  null as unknown as FilePickerContextValue
+);
 
 const client = new QueryClient();
-export const FilePickerContext: FunctionComponent = ({ children }) => (
-  <QueryClientProvider client={client}>{children}</QueryClientProvider>
-);
+export const FilePickerContext: FunctionComponent = ({ children }) => {
+  const state = useMemo(createFilePickerState, []);
+  return (
+    <QueryClientProvider client={client}>
+      <FilePickerCtx.Provider value={{ state }}>{children}</FilePickerCtx.Provider>
+    </QueryClientProvider>
+  );
+};
+
+export const useFilePickerContext = (): FilePickerContextValue => {
+  const ctx = useContext(FilePickerCtx);
+  if (!ctx) throw new Error('FilePickerContext not found!');
+  return ctx;
+};
