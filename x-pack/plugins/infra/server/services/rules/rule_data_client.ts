a5df51dcd3836d2aa7946a4fe569b02ecab94aff
Bena Kansara <bena.kansara@elastic.co>
2022-10-11 17:49:27 +0200

    adding component template ref

--- x-pack/plugins/infra/server/services/rules/rule_data_client.ts
+++ x-pack/plugins/infra/server/services/rules/rule_data_client.ts
@@ -12,6 +12,7 @@ import { experimentalRuleFieldMap } from '@kbn/rule-registry-plugin/common/asset
 import { Dataset, RuleRegistryPluginSetupContract } from '@kbn/rule-registry-plugin/server';
 import type { InfraFeatureId } from '../../../common/constants';
 import { RuleRegistrationContext, RulesServiceStartDeps } from './types';
+import { ECS_COMPONENT_TEMPLATE_NAME } from '@kbn/rule-registry-plugin/common/assets';
 
 export const createRuleDataClient = ({
   ownerFeatureId,
@@ -30,7 +31,7 @@ export const createRuleDataClient = ({
     feature: ownerFeatureId,
     registrationContext,
     dataset: Dataset.alerts,
-    componentTemplateRefs: [],
+    componentTemplateRefs: [ECS_COMPONENT_TEMPLATE_NAME],
     componentTemplates: [
       {
         name: 'mappings',
