2b6be4629f818c5344b710d6f3777291ba2c448d
Kevin Logan <kevin.logan@elastic.co>
2022-10-11 17:07:54 -0400

    [Security Solution] Unskip Endpoint functional tests

--- x-pack/test/security_solution_endpoint/apps/endpoint/trusted_apps_list.ts
+++ x-pack/test/security_solution_endpoint/apps/endpoint/trusted_apps_list.ts
@@ -17,7 +17,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const policyTestResources = getService('policyTestResources');
 
   // FLAKY: https://github.com/elastic/kibana/issues/114309
-  describe.skip('When on the Trusted Apps list', function () {
+  describe('When on the Trusted Apps list', function () {
     let indexedData: IndexedHostsAndAlertsResponse;
     before(async () => {
       const endpointPackage = await policyTestResources.getEndpointPackage();
