1806632baaee338a21408736c217a0510c0f79db
Dzmitry Tamashevich <diaamnj@mail.ru>
2022-10-12 20:06:17 +0300

    [Discover] remove redundant function

--- test/functional/services/data_grid.ts
+++ test/functional/services/data_grid.ts
@@ -296,10 +296,6 @@ export class DataGridService extends FtrService {
     await this.openColMenuByField(field);
     await this.find.clickByButtonText('Copy name');
   }
-  public async clickEditField(field: string) {
-    await this.openColMenuByField(field);
-    await this.testSubjects.click('gridEditFieldButton');
-  }
 
   public async clickEditField(field: string) {
     await this.openColMenuByField(field);
