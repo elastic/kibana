d7b2e8aab69a5ece5fc27e7a202a3b16b08216ed
Uladzislau Lasitsa <vlad.lasitsa@gmail.com>
2022-10-12 14:38:55 +0300

    Added functional tests for convert to lens top n

---
+++ x-pack/test/functional/apps/lens/group3/open_in_lens/tsvb/top_n.ts
@@ -0,0 +1,172 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../../../../../ftr_provider_context';
+
+export default function ({ getPageObjects, getService }: FtrProviderContext) {
+  const { visualize, visualBuilder, lens, header } = getPageObjects([
+    'visualBuilder',
+    'visualize',
+    'header',
+    'lens',
+  ]);
+
+  const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+  const filterBar = getService('filterBar');
+  const queryBar = getService('queryBar');
+
+  describe('Top N', function describeIndexTests() {
+    before(async () => {
+      await visualize.initTests();
+    });
+
+    beforeEach(async () => {
+      await visualize.navigateToNewVisualization();
+      await visualize.clickVisualBuilder();
+      await visualBuilder.checkVisualBuilderIsPresent();
+      await visualBuilder.resetPage();
+      await visualBuilder.clickTopN();
+      await visualBuilder.checkTopNTabIsPresent();
+    });
+
+    it('should not allow converting of not valid panel', async () => {
+      await visualBuilder.selectAggType('Max');
+      await header.waitUntilLoadingHasFinished();
+      const canEdit = await testSubjects.exists('visualizeEditInLensButton');
+      expect(canEdit).to.be(false);
+    });
+
+    it('should not allow converting of unsupported aggregations', async () => {
+      await visualBuilder.selectAggType('Variance');
+      await visualBuilder.setFieldForAggregation('machine.ram');
+
+      await header.waitUntilLoadingHasFinished();
+      const canEdit = await testSubjects.exists('visualizeEditInLensButton');
+      expect(canEdit).to.be(false);
+    });
+
+    it('should hide the "Edit Visualization in Lens" menu item for a sibling pipeline aggregations', async () => {
+      await visualBuilder.createNewAgg();
+
+      await visualBuilder.selectAggType('Overall Average', 1);
+      await visualBuilder.setFieldForAggregation('Count', 1);
+      await header.waitUntilLoadingHasFinished();
+      const canEdit = await testSubjects.exists('visualizeEditInLensButton');
+      expect(canEdit).to.be(false);
+    });
+
+    it('should hide the "Edit Visualization in Lens" menu item for a parent pipeline aggregations', async () => {
+      await visualBuilder.clickPanelOptions('topN');
+      await visualBuilder.setMetricsDataTimerangeMode('Last value');
+      await visualBuilder.clickDataTab('topN');
+      await visualBuilder.createNewAgg();
+
+      await visualBuilder.selectAggType('Cumulative Sum', 1);
+      await visualBuilder.setFieldForAggregation('Count', 1);
+      await header.waitUntilLoadingHasFinished();
+      const canEdit = await testSubjects.exists('visualizeEditInLensButton');
+      expect(canEdit).to.be(false);
+    });
+
+    it('should show the "Edit Visualization in Lens" menu item for a count aggregation', async () => {
+      const canEdit = await testSubjects.exists('visualizeEditInLensButton');
+      expect(canEdit).to.be(true);
+    });
+
+    it('should convert to horizontal bar', async () => {
+      const button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+      const chartSwitcher = await testSubjects.find('lnsChartSwitchPopover');
+      const type = await chartSwitcher.getVisibleText();
+      expect(type).to.be('Bar horizontal');
+    });
+
+    it('should convert group by to vertical axis', async () => {
+      await visualBuilder.setMetricsGroupByTerms('extension.raw');
+      await header.waitUntilLoadingHasFinished();
+      const button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+      await retry.try(async () => {
+        const layerCount = await lens.getLayerCount();
+        expect(layerCount).to.be(1);
+
+        const xDimensionText = await lens.getDimensionTriggerText('lnsXY_xDimensionPanel', 0);
+        const yDimensionText = await lens.getDimensionTriggerText('lnsXY_yDimensionPanel', 0);
+        expect(xDimensionText).to.be('Top 10 values of extension.raw');
+        expect(yDimensionText).to.be('Count of records');
+      });
+    });
+
+    it('should convert last value mode to reduced time range', async () => {
+      await visualBuilder.clickPanelOptions('topN');
+      await visualBuilder.setMetricsDataTimerangeMode('Last value');
+      await visualBuilder.setIntervalValue('1m');
+      await visualBuilder.clickDataTab('topN');
+      await header.waitUntilLoadingHasFinished();
+
+      const button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+      await retry.try(async () => {
+        const layerCount = await lens.getLayerCount();
+        expect(layerCount).to.be(1);
+        const yDimensionText = await lens.getDimensionTriggerText('lnsXY_yDimensionPanel', 0);
+        expect(yDimensionText).to.be('Count of records last 1m');
+      });
+    });
+
+    it('visualizes field to Lens and loads fields to the dimesion editor', async () => {
+      const button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+      await retry.try(async () => {
+        const yDimensionText = await lens.getDimensionTriggerText('lnsXY_yDimensionPanel', 0);
+        expect(yDimensionText).to.be('Count of records');
+      });
+    });
+
+    it('navigates back to TSVB when the Back button is clicked', async () => {
+      const button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+      const goBackBtn = await testSubjects.find('lnsApp_goBackToAppButton');
+      goBackBtn.click();
+      await visualBuilder.checkTopNTabIsPresent();
+    });
+
+    it('should preserve app filters in lens', async () => {
+      await filterBar.addFilter('extension', 'is', 'css');
+      await header.waitUntilLoadingHasFinished();
+      const button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+
+      expect(await filterBar.hasFilter('extension', 'css')).to.be(true);
+    });
+
+    it('should preserve query in lens', async () => {
+      let button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+      const goBackBtn = await testSubjects.find('lnsApp_goBackToAppButton');
+      goBackBtn.click();
+      await visualBuilder.checkTopNTabIsPresent();
+      await queryBar.setQuery('machine.os : ios');
+      await queryBar.submitQuery();
+      await header.waitUntilLoadingHasFinished();
+      button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('xyVisChart');
+
+      expect(await queryBar.getQueryString()).to.equal('machine.os : ios');
+    });
+  });
+}
