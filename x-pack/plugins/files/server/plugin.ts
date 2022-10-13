4fef1444e099d55b6a77685bc2924f9089bce632
Jean-Louis Leysens <jloleysens@gmail.com>
2022-10-13 13:12:28 +0200

    added files management privileges definition and locked down metrics and find endpoint to management role

--- x-pack/plugins/files/server/plugin.ts
+++ x-pack/plugins/files/server/plugin.ts
@@ -27,6 +27,7 @@ import type { FilesPluginSetupDependencies, FilesSetup, FilesStart } from './typ
 import type { FilesRequestHandlerContext, FilesRouter } from './routes/types';
 import { registerRoutes, registerFileKindRoutes } from './routes';
 import { Counters, registerUsageCollector } from './usage';
+import { filesFeature } from './feature';
 
 export class FilesPlugin implements Plugin<FilesSetup, FilesStart, FilesPluginSetupDependencies> {
   private readonly logger: Logger;
