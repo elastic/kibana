20ff4cc5ffa24f6f897225f065cbb6205de9bd1e
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2022-10-13 14:57:08 +0300

    Fix add filter story error

--- src/plugins/unified_search/public/__stories__/search_bar.stories.tsx
+++ src/plugins/unified_search/public/__stories__/search_bar.stories.tsx
@@ -22,7 +22,7 @@ const mockIndexPatterns = [
     title: 'logstash-*',
     fields: [
       {
-        name: 'response',
+        name: 'bytes',
         type: 'number',
         esTypes: ['integer'],
         aggregatable: true,
@@ -30,6 +30,7 @@ const mockIndexPatterns = [
         searchable: true,
       },
     ],
+    getName: () => 'logstash-*',
   },
   {
     id: '1235',
@@ -44,6 +45,7 @@ const mockIndexPatterns = [
         searchable: true,
       },
     ],
+    getName: () => 'test-*',
   },
 ] as DataView[];
 
