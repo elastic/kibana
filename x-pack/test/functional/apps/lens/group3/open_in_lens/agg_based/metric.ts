43010277667088b0cce829f142e68f6e889f0b8d
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-10-13 18:02:42 +0300

    Added tests for color ranges.

--- x-pack/test/functional/apps/lens/group3/open_in_lens/agg_based/metric.ts
+++ x-pack/test/functional/apps/lens/group3/open_in_lens/agg_based/metric.ts
@@ -9,14 +9,16 @@ import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../../../ftr_provider_context';
 
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
-  const { visEditor, visualize, lens, timePicker } = getPageObjects([
+  const { visEditor, visualize, lens, timePicker, visChart } = getPageObjects([
     'visEditor',
     'visualize',
+    'visChart',
     'lens',
     'timePicker',
   ]);
 
   const testSubjects = getService('testSubjects');
+  const find = getService('find');
 
   describe('Metric', function describeIndexTests() {
     const isNewChartsLibraryEnabled = true;
@@ -121,6 +123,100 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
 
       const button = await testSubjects.exists('visualizeEditInLensButton');
       expect(button).to.be(false);
+    });
+
+    it('should convert color ranges', async () => {
+      await visEditor.clickMetricEditor();
+      await visEditor.selectAggregation('Average', 'metrics');
+      await visEditor.selectField('machine.ram', 'metrics');
+      await visEditor.clickBucket('Split group');
+      await visEditor.selectAggregation('Terms');
+      await visEditor.selectField('machine.os.raw');
+
+      await visEditor.clickOptionsTab();
+      await testSubjects.setValue('metricColorRange0__to', '10000');
+      await testSubjects.click('metricColorRange__addRangeButton');
+
+      await testSubjects.setValue('metricColorRange1__to', '20000');
+      await visChart.waitForVisualizationRenderingStabilized();
+
+      const backgroundButton = await find.byCssSelector('[data-text="Background"]');
+      await backgroundButton.click();
+      await visEditor.clickGo();
+
+      const button = await testSubjects.find('visualizeEditInLensButton');
+      await button.click();
+      await lens.waitForVisualization('mtrVis');
+
+      expect(await lens.getLayerCount()).to.be(1);
+
+      const dimensions = await testSubjects.findAll('lns-dimensionTrigger');
+      expect(dimensions).to.have.length(2);
+      expect(await dimensions[0].getVisibleText()).to.be('Average machine.ram');
+      expect(await dimensions[1].getVisibleText()).to.be('machine.os.raw: Descending');
+
+      expect((await lens.getMetricVisualizationData()).length).to.be.equal(6);
+      expect(await lens.getMetricVisualizationData()).to.eql([
+        {
+          title: 'osx',
+          subtitle: 'Average machine.ram',
+          extraText: '',
+          value: '13.23B',
+          color: 'rgba(245, 247, 250, 1)',
+          showingBar: false,
+        },
+        {
+          title: 'win 7',
+          subtitle: 'Average machine.ram',
+          extraText: '',
+          value: '13.19B',
+          color: 'rgba(245, 247, 250, 1)',
+          showingBar: false,
+        },
+        {
+          title: 'win xp',
+          subtitle: 'Average machine.ram',
+          extraText: '',
+          value: '13.07B',
+          color: 'rgba(245, 247, 250, 1)',
+          showingBar: false,
+        },
+        {
+          title: 'win 8',
+          subtitle: 'Average machine.ram',
+          extraText: '',
+          value: '13.03B',
+          color: 'rgba(245, 247, 250, 1)',
+          showingBar: false,
+        },
+        {
+          title: 'ios',
+          subtitle: 'Average machine.ram',
+          extraText: '',
+          value: '13.01B',
+          color: 'rgba(245, 247, 250, 1)',
+          showingBar: false,
+        },
+        {
+          title: undefined,
+          subtitle: undefined,
+          extraText: undefined,
+          value: undefined,
+          color: 'rgba(0, 0, 0, 0)',
+          showingBar: false,
+        },
+      ]);
+
+      dimensions[0].click();
+
+      await lens.openPalettePanel('lnsMetric');
+      const colorStops = await lens.getPaletteColorStops();
+
+      expect(colorStops).to.eql([
+        { stop: '0', color: 'rgba(0, 104, 55, 1)' },
+        { stop: '10000', color: 'rgba(165, 0, 38, 1)' },
+        { stop: '20000', color: undefined },
+      ]);
     });
   });
 }
