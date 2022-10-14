b8a08680508e078df439ee4ceb99f8dc4b23fbed
Julia Guskova <iuliia.guskova@elastic.co>
2022-10-14 08:30:46 +0200

    add integrational tests

--- x-pack/test/alerting_api_integration/security_and_spaces/group1/tests/alerting/bulk_delete.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/group1/tests/alerting/bulk_delete.ts
@@ -6,7 +6,7 @@
  */
 
 import expect from '@kbn/expect';
-import { SuperuserAtSpace1 } from '../../../scenarios';
+import { UserAtSpaceScenarios } from '../../../scenarios';
 import { FtrProviderContext } from '../../../../common/ftr_provider_context';
 import { getUrlPrefix, getTestRuleData, ObjectRemover } from '../../../../common/lib';
 
@@ -15,140 +15,219 @@ export default ({ getService }: FtrProviderContext) => {
   const supertest = getService('supertest');
   const supertestWithoutAuth = getService('supertestWithoutAuth');
 
-  describe('bulkDeleteRules', () => {
+  describe('bulkDelete', () => {
     const objectRemover = new ObjectRemover(supertest);
-    after(() => objectRemover.removeAll());
 
-    it('should handle bulk delete of one rule appropriately based on id', async () => {
-      const { user, space } = SuperuserAtSpace1;
+    for (const scenario of UserAtSpaceScenarios) {
+      const { user, space } = scenario;
 
-      const { body: createdRule1 } = await supertest
-        .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
-        .set('kbn-xsrf', 'foo')
-        .send(getTestRuleData({ tags: ['foo'] }))
-        .expect(200);
+      describe(scenario.id, () => {
+        after(() => objectRemover.removeAll());
 
-      const response = await supertestWithoutAuth
-        .patch(`${getUrlPrefix(space.id)}/api/alerting/rules/_bulk_delete`)
-        .set('kbn-xsrf', 'foo')
-        .send({ ids: [createdRule1.id] })
-        .auth(user.username, user.password);
-
-      expect(response.statusCode).to.eql(200);
-      expect(response.body).to.eql({ errors: [], total: 1 });
-    });
-
-    it('should handle bulk delete of several rules ids appropriately based on ids', async () => {
-      const { user, space } = SuperuserAtSpace1;
-
-      const rules = await Promise.all(
-        Array.from({ length: 3 }).map(() =>
-          supertest
+        it('should handle bulk delete of one rule appropriately based on id', async () => {
+          const { body: createdRule1 } = await supertest
             .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
             .set('kbn-xsrf', 'foo')
-            .send(getTestRuleData({ tags: ['multiple-rules-edit'] }))
-            .expect(200)
-        )
-      );
+            .send(getTestRuleData({ tags: ['foo'] }))
+            .expect(200);
 
-      const response = await supertestWithoutAuth
-        .patch(`${getUrlPrefix(space.id)}/api/alerting/rules/_bulk_delete`)
-        .set('kbn-xsrf', 'foo')
-        .send({ ids: rules.map((rule) => rule.body.id) })
-        .auth(user.username, user.password);
+          const response = await supertestWithoutAuth
+            .patch(`${getUrlPrefix(space.id)}/internal/alerting/rules/_bulk_delete`)
+            .set('kbn-xsrf', 'foo')
+            .send({ ids: [createdRule1.id] })
+            .auth(user.username, user.password);
 
-      expect(response.statusCode).to.eql(200);
-      expect(response.body).to.eql({ errors: [], total: 3 });
-    });
+          switch (scenario.id) {
+            case 'no_kibana_privileges at space1':
+            case 'space_1_all at space2':
+              expect(response.body).to.eql({
+                error: 'Forbidden',
+                message: 'Unauthorized to find rules for any rule types',
+                statusCode: 403,
+              });
+              expect(response.statusCode).to.eql(403);
+              break;
+            case 'global_read at space1':
+              expect(response.body).to.eql({
+                error: 'Forbidden',
+                message: 'Unauthorized to bulkDelete a "test.noop" rule for "alertsFixture"',
+                statusCode: 403,
+              });
+              expect(response.statusCode).to.eql(403);
+              break;
+            case 'space_1_all_alerts_none_actions at space1':
+            case 'superuser at space1':
+            case 'space_1_all at space1':
+            case 'space_1_all_with_restricted_fixture at space1':
+              expect(response.body).to.eql({ errors: [], total: 1 });
+              expect(response.statusCode).to.eql(200);
+              break;
+            default:
+              throw new Error(`Scenario untested: ${JSON.stringify(scenario)}`);
+          }
+        });
 
-    it('should handle bulk delete of several rules ids appropriately based on filter', async () => {
-      const { user, space } = SuperuserAtSpace1;
+        it('should handle bulk delete of several rules ids appropriately based on ids', async () => {
+          const rules = await Promise.all(
+            Array.from({ length: 3 }).map(() =>
+              supertest
+                .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
+                .set('kbn-xsrf', 'foo')
+                .send(getTestRuleData({ tags: ['multiple-rules-edit'] }))
+                .expect(200)
+            )
+          );
 
-      await Promise.all(
-        Array.from({ length: 3 }).map(() =>
-          supertest
+          const response = await supertestWithoutAuth
+            .patch(`${getUrlPrefix(space.id)}/internal/alerting/rules/_bulk_delete`)
+            .set('kbn-xsrf', 'foo')
+            .send({ ids: rules.map((rule) => rule.body.id) })
+            .auth(user.username, user.password);
+
+          switch (scenario.id) {
+            case 'no_kibana_privileges at space1':
+            case 'space_1_all at space2':
+              expect(response.body).to.eql({
+                error: 'Forbidden',
+                message: 'Unauthorized to find rules for any rule types',
+                statusCode: 403,
+              });
+              expect(response.statusCode).to.eql(403);
+              break;
+            case 'global_read at space1':
+              expect(response.body).to.eql({
+                error: 'Forbidden',
+                message: 'Unauthorized to bulkDelete a "test.noop" rule for "alertsFixture"',
+                statusCode: 403,
+              });
+              expect(response.statusCode).to.eql(403);
+              break;
+            case 'space_1_all_alerts_none_actions at space1':
+            case 'superuser at space1':
+            case 'space_1_all at space1':
+            case 'space_1_all_with_restricted_fixture at space1':
+              expect(response.body).to.eql({ errors: [], total: 3 });
+              expect(response.statusCode).to.eql(200);
+              break;
+            default:
+              throw new Error(`Scenario untested: ${JSON.stringify(scenario)}`);
+          }
+        });
+
+        it.skip('should handle bulk delete of several rules ids appropriately based on filter', async () => {
+          await Promise.all(
+            Array.from({ length: 3 }).map(() =>
+              supertest
+                .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
+                .set('kbn-xsrf', 'foo')
+                .send(getTestRuleData({ tags: ['multiple-rules-delete'] }))
+                .expect(200)
+            )
+          );
+
+          const response = await supertestWithoutAuth
+            .patch(`${getUrlPrefix(space.id)}/internal/alerting/rules/_bulk_delete`)
+            .set('kbn-xsrf', 'foo')
+            .send({ filter: `alert.attributes.tags: "multiple-rules-delete"` })
+            .auth(user.username, user.password);
+
+          switch (scenario.id) {
+            case 'no_kibana_privileges at space1':
+            case 'space_1_all at space2':
+              expect(response.body).to.eql({
+                error: 'Forbidden',
+                message: 'Unauthorized to find rules for any rule types',
+                statusCode: 403,
+              });
+              expect(response.statusCode).to.eql(403);
+              break;
+            case 'global_read at space1':
+              expect(response.body).to.eql({
+                error: 'Forbidden',
+                message: 'Unauthorized to bulkDelete a "test.noop" rule for "alertsFixture"',
+                statusCode: 403,
+              });
+              expect(response.statusCode).to.eql(403);
+              break;
+            case 'space_1_all_alerts_none_actions at space1':
+            case 'superuser at space1':
+            case 'space_1_all at space1':
+            case 'space_1_all_with_restricted_fixture at space1':
+              expect(response.body).to.eql({ errors: [], total: 3 });
+              expect(response.statusCode).to.eql(200);
+              break;
+            default:
+              throw new Error(`Scenario untested: ${JSON.stringify(scenario)}`);
+          }
+        });
+
+        it('should throw an error when bulk delete of rules when both ids and filter supplied in payload', async () => {
+          const { body: createdRule1 } = await supertest
             .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
             .set('kbn-xsrf', 'foo')
-            .send(getTestRuleData({ tags: ['multiple-rules-edit'] }))
-            .expect(200)
-        )
-      );
+            .send(getTestRuleData({ tags: ['foo'] }))
+            .expect(200);
 
-      const response = await supertestWithoutAuth
-        .patch(`${getUrlPrefix(space.id)}/api/alerting/rules/_bulk_delete`)
-        .set('kbn-xsrf', 'foo')
-        .send({ filter: `alert.attributes.tags: "multiple-rules-edit"` })
-        .auth(user.username, user.password);
+          const response = await supertestWithoutAuth
+            .patch(`${getUrlPrefix(space.id)}/internal/alerting/rules/_bulk_delete`)
+            .set('kbn-xsrf', 'foo')
+            .send({ filter: 'fake_filter', ids: [createdRule1.id] })
+            .auth(user.username, user.password);
 
-      expect(response.statusCode).to.eql(200);
-      expect(response.body).to.eql({ errors: [], total: 3 });
-    });
+          switch (scenario.id) {
+            case 'no_kibana_privileges at space1':
+            case 'space_1_all at space2':
+            case 'global_read at space1':
+            case 'space_1_all_alerts_none_actions at space1':
+            case 'superuser at space1':
+            case 'space_1_all at space1':
+            case 'space_1_all_with_restricted_fixture at space1':
+              expect(response.statusCode).to.eql(400);
+              expect(response.body.message).to.eql(
+                "Both 'filter' and 'ids' are supplied. Define either 'ids' or 'filter' properties in method arguments"
+              );
+              break;
+            default:
+              throw new Error(`Scenario untested: ${JSON.stringify(scenario)}`);
+          }
+        });
 
-    it('should throw an error when bulk delete of rules when both ids and filter supplied in payload', async () => {
-      const { user, space } = SuperuserAtSpace1;
-
-      const { body: createdRule1 } = await supertest
-        .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
-        .set('kbn-xsrf', 'foo')
-        .send(getTestRuleData({ tags: ['foo'] }))
-        .expect(200);
-
-      const response = await supertestWithoutAuth
-        .patch(`${getUrlPrefix(space.id)}/api/alerting/rules/_bulk_delete`)
-        .set('kbn-xsrf', 'foo')
-        .send({ filter: 'fake_filter', ids: [createdRule1.id] })
-        .auth(user.username, user.password);
-
-      expect(response.statusCode).to.eql(400);
-      expect(response.body.message).to.eql(
-        "Both 'filter' and 'ids' are supplied. Define either 'ids' or 'filter' properties in method arguments"
-      );
-    });
-
-    it('should handle bulk delete of rules when one of deletions throw an error', async () => {
-      const { user, space } = SuperuserAtSpace1;
-
-      const rules = await Promise.all(
-        Array.from({ length: 3 }).map(() =>
-          supertest
+        it('shouldn not delete rule from another space', async () => {
+          const { body: createdRule } = await supertest
             .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
             .set('kbn-xsrf', 'foo')
-            .send(getTestRuleData({ tags: ['multiple-rules-edit'] }))
-            .expect(200)
-        )
-      );
+            .send(getTestRuleData())
+            .expect(200);
 
-      const response = await supertestWithoutAuth
-        .patch(`${getUrlPrefix(space.id)}/api/alerting/rules/_bulk_delete`)
-        .set('kbn-xsrf', 'foo')
-        .send({ ids: rules.map((rule) => rule.body.id) })
-        .auth(user.username, user.password);
+          const response = await supertestWithoutAuth
+            .patch(`${getUrlPrefix('other')}/internal/alerting/rules/_bulk_delete`)
+            .set('kbn-xsrf', 'foo')
+            .auth(user.username, user.password)
+            .send({ ids: [createdRule.id] });
 
-      expect(response.statusCode).to.eql(200);
-      expect(response.body).to.eql({ errors: [], total: 3 });
-    });
-
-    it('shouldn not update rule from another space', async () => {
-      const { user, space } = SuperuserAtSpace1;
-
-      const { body: createdRule } = await supertest
-        .post(`${getUrlPrefix(space.id)}/api/alerting/rule`)
-        .set('kbn-xsrf', 'foo')
-        .send(getTestRuleData())
-        .expect(200);
-
-      const response = await supertestWithoutAuth
-        .post(`${getUrlPrefix('other')}/internal/alerting/rules/_bulk_edit`)
-        .set('kbn-xsrf', 'foo')
-        .auth(user.username, user.password)
-        .send({ ids: [createdRule.id] });
-
-      expect(response.body).to.eql({
-        error: 'Forbidden',
-        message: 'Unauthorized to find rules for any rule types',
-        statusCode: 403,
+          switch (scenario.id) {
+            case 'superuser at space1':
+            case 'global_read at space1':
+              expect(response.body).to.eql({ errors: [], total: 0 });
+              expect(response.statusCode).to.eql(200);
+              break;
+            case 'no_kibana_privileges at space1':
+            case 'space_1_all at space2':
+            case 'space_1_all at space1':
+            case 'space_1_all_alerts_none_actions at space1':
+            case 'space_1_all_with_restricted_fixture at space1':
+              expect(response.body).to.eql({
+                error: 'Forbidden',
+                message: 'Unauthorized to find rules for any rule types',
+                statusCode: 403,
+              });
+              expect(response.statusCode).to.eql(403);
+              break;
+            default:
+              throw new Error(`Scenario untested: ${JSON.stringify(scenario)}`);
+          }
+        });
       });
-      expect(response.statusCode).to.eql(403);
-    });
+    }
   });
 };
