98670f85e1add9c180725a167f934e9fbce29e8a
Dominique Clarke <dominique.clarke@elastic.co>
2022-10-14 10:41:56 -0400

    Update x-pack/plugins/synthetics/server/routes/common.ts
    Co-authored-by: Shahzad <shahzad31comp@gmail.com>

--- x-pack/plugins/synthetics/server/routes/common.ts
+++ x-pack/plugins/synthetics/server/routes/common.ts
@@ -24,7 +24,6 @@ const querySchema = schema.object({
   locations: schema.maybe(schema.oneOf([schema.string(), schema.arrayOf(schema.string())])),
   status: schema.maybe(schema.oneOf([schema.string(), schema.arrayOf(schema.string())])),
   fields: schema.maybe(schema.arrayOf(schema.string())),
-  aggs: schema.any(),
   searchAfter: schema.maybe(schema.arrayOf(schema.string())),
 });
 
