e483c687d29ce124cecd5ade2fa13f35317f1ea6
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2022-10-14 14:22:26 +0200

    Scaffold history tab contents

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_history/monitor_history.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_history/monitor_history.tsx
@@ -4,9 +4,37 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EuiText } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiPanel } from '@elastic/eui';
 import React from 'react';
 
 export const MonitorHistory = () => {
-  return <EuiText>Monitor history tab content</EuiText>;
+  return (
+    <EuiFlexGroup direction="column" gutterSize="m">
+      <EuiFlexItem>Date picker</EuiFlexItem>
+      <EuiFlexItem>
+        <EuiFlexGroup gutterSize="m">
+          <EuiFlexItem>
+            <EuiPanel hasShadow={false} hasBorder={true}>
+              Stats
+            </EuiPanel>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiPanel hasShadow={false} hasBorder={true}>
+              Duration trend
+            </EuiPanel>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiPanel hasShadow={false} hasBorder={true}>
+          Status
+        </EuiPanel>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiPanel hasShadow={false} hasBorder={true}>
+          Test runs
+        </EuiPanel>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
 };
