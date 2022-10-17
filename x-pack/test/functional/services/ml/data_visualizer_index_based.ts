aabc8446168c13551781c8c4000e48cc85a01e88
Quynh Nguyen (Quinn) <43350163+qn895@users.noreply.github.com>
2022-10-17 10:31:26 -0500

    [ML] Add functional tests for Index data visualizer's random sampler controls (#142278)
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/functional/services/ml/data_visualizer_index_based.ts
+++ x-pack/test/functional/services/ml/data_visualizer_index_based.ts
@@ -17,6 +17,7 @@ export function MachineLearningDataVisualizerIndexBasedProvider({
   const PageObjects = getPageObjects(['discover']);
   const queryBar = getService('queryBar');
   const filterBar = getService('filterBar');
+  const browser = getService('browser');
 
   return {
     async assertTimeRangeSelectorSectionExists() {
@@ -230,6 +231,89 @@ export function MachineLearningDataVisualizerIndexBasedProvider({
           );
         }
       );
+    },
+
+    async assertRandomSamplingOptionsButtonExists() {
+      await testSubjects.existOrFail('dvRandomSamplerOptionsButton');
+    },
+
+    async assertRandomSamplingOption(
+      expectedOption:
+        | 'dvRandomSamplerOptionOnAutomatic'
+        | 'dvRandomSamplerOptionOnManual'
+        | 'dvRandomSamplerOptionOff',
+      expectedProbability?: number
+    ) {
+      await retry.tryForTime(20000, async () => {
+        await browser.pressKeys(browser.keys.ESCAPE);
+        await testSubjects.clickWhenNotDisabled('dvRandomSamplerOptionsButton');
+        await testSubjects.existOrFail('dvRandomSamplerOptionsPopover');
+
+        if (expectedOption === 'dvRandomSamplerOptionOff') {
+          await testSubjects.existOrFail('dvRandomSamplerOptionOff', { timeout: 1000 });
+          await testSubjects.missingOrFail('dvRandomSamplerProbabilityRange', { timeout: 1000 });
+          await testSubjects.missingOrFail('dvRandomSamplerAutomaticProbabilityMsg', {
+            timeout: 1000,
+          });
+        }
+
+        if (expectedOption === 'dvRandomSamplerOptionOnManual') {
+          await testSubjects.existOrFail('dvRandomSamplerOptionOnManual', { timeout: 1000 });
+          await testSubjects.existOrFail('dvRandomSamplerProbabilityRange', { timeout: 1000 });
+          if (expectedProbability !== undefined) {
+            const probability = await testSubjects.getAttribute(
+              'dvRandomSamplerProbabilityRange',
+              'value'
+            );
+            expect(probability).to.eql(
+              `${expectedProbability}`,
+              `Expected probability to be ${expectedProbability}, got ${probability}`
+            );
+          }
+        }
+
+        if (expectedOption === 'dvRandomSamplerOptionOnAutomatic') {
+          await testSubjects.existOrFail('dvRandomSamplerOptionOnAutomatic', { timeout: 1000 });
+          await testSubjects.existOrFail('dvRandomSamplerAutomaticProbabilityMsg', {
+            timeout: 1000,
+          });
+
+          if (expectedProbability !== undefined) {
+            const probabilityText = await testSubjects.getVisibleText(
+              'dvRandomSamplerAutomaticProbabilityMsg'
+            );
+            expect(probabilityText).to.contain(
+              `${expectedProbability}`,
+              `Expected probability text to contain ${expectedProbability}, got ${probabilityText}`
+            );
+          }
+        }
+      });
+    },
+
+    async setRandomSamplingOption(
+      option:
+        | 'dvRandomSamplerOptionOnAutomatic'
+        | 'dvRandomSamplerOptionOnManual'
+        | 'dvRandomSamplerOptionOff'
+    ) {
+      await retry.tryForTime(20000, async () => {
+        // escape popover
+        await browser.pressKeys(browser.keys.ESCAPE);
+        await this.assertRandomSamplingOptionsButtonExists();
+        await testSubjects.clickWhenNotDisabled('dvRandomSamplerOptionsButton');
+        await testSubjects.existOrFail('dvRandomSamplerOptionsPopover', { timeout: 1000 });
+
+        await testSubjects.clickWhenNotDisabled('dvRandomSamplerOptionsSelect');
+
+        await testSubjects.existOrFail('dvRandomSamplerOptionOff', { timeout: 1000 });
+        await testSubjects.existOrFail('dvRandomSamplerOptionOnManual', { timeout: 1000 });
+        await testSubjects.existOrFail('dvRandomSamplerOptionOnAutomatic', { timeout: 1000 });
+
+        await testSubjects.click(option);
+
+        await this.assertRandomSamplingOption(option);
+      });
     },
   };
 }
