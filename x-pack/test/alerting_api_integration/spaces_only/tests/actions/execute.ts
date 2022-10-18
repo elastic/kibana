77e4e3e9dd92408b9ea93a9f011765421b9566f2
Alexandra Doak <alexandra.doak@elastic.co>
2022-10-18 11:38:35 -0400

    Updating a missed test

--- x-pack/test/alerting_api_integration/spaces_only/tests/actions/execute.ts
+++ x-pack/test/alerting_api_integration/spaces_only/tests/actions/execute.ts
@@ -142,7 +142,7 @@ export default function ({ getService }: FtrProviderContext) {
         actionTypeId: 'test.failing',
         outcome: 'failure',
         message: `action execution failure: test.failing:${createdAction.id}: failing action`,
-        errorMessage: `an error occurred while running the action: expected failure for .kibana-alerting-test-data actions-failure-1:space1`,
+        errorMessage: `an error occurred while running the action: expected failure for .kibana-alerting-test-data actions-failure-1:space1; retry: true`,
       });
     });
 
