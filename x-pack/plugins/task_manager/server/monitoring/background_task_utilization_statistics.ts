2c30ecce35ce2deccf1a0b0762d338fe8e3fd78d
Alexandra Doak <alexandra.doak@elastic.co>
2022-10-19 08:47:44 -0400

    Changing the key name

--- x-pack/plugins/task_manager/server/monitoring/background_task_utilization_statistics.ts
+++ x-pack/plugins/task_manager/server/monitoring/background_task_utilization_statistics.ts
@@ -142,7 +142,7 @@ export function createBackgroundTaskUtilizationAggregator(
         Pick<BackgroundTaskUtilizationStat, 'recurring'>
       ]) => {
         return {
-          key: 'runtime',
+          key: 'utilization',
           value: {
             ...adhoc,
             ...recurring,
