d5a3a29489b0c535acdace200d1ed31b902b7381
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-10-19 13:25:39 +0300

    Added base code for converting heatmap to lens.

--- src/plugins/visualizations/common/convert_to_lens/types/configurations.ts
+++ src/plugins/visualizations/common/convert_to_lens/types/configurations.ts
@@ -10,6 +10,7 @@ import { HorizontalAlignment, LayoutDirection, Position, VerticalAlignment } fro
 import { $Values } from '@kbn/utility-types';
 import type { CustomPaletteParams, PaletteOutput } from '@kbn/coloring';
 import { KibanaQueryOutput } from '@kbn/data-plugin/common';
+import { CustomPaletteState } from '@kbn/charts-plugin/common';
 import { LegendSize } from '../../constants';
 import {
   CategoryDisplayTypes,
@@ -28,6 +29,9 @@ import {
   GaugeCentralMajorModes,
   CollapseFunctions,
 } from '../constants';
+import { ExpressionValueVisDimension } from '../../expression_functions';
+
+export type ChartShapes = 'heatmap';
 
 export type CollapseFunction = typeof CollapseFunctions[number];
 
@@ -276,9 +280,55 @@ export type GaugeVisConfiguration = GaugeState & {
   layerType: typeof LayerTypes.DATA;
 };
 
+export interface HeatmapLegendConfig {
+  isVisible: boolean;
+  position: Position;
+  maxLines?: number;
+  shouldTruncate?: boolean;
+  legendSize?: LegendSize;
+}
+export interface HeatmapArguments {
+  percentageMode?: boolean;
+  lastRangeIsRightOpen?: boolean;
+  showTooltip?: boolean;
+  highlightInHover?: boolean;
+  palette?: PaletteOutput<CustomPaletteState>;
+  xAccessor?: string | ExpressionValueVisDimension;
+  yAccessor?: string | ExpressionValueVisDimension;
+  valueAccessor?: string | ExpressionValueVisDimension;
+  splitRowAccessor?: string | ExpressionValueVisDimension;
+  splitColumnAccessor?: string | ExpressionValueVisDimension;
+  legend: HeatmapLegendConfig;
+  gridConfig: HeatmapLegendConfig;
+  ariaLabel?: string;
+}
+
+export type HeatmapLayerState = HeatmapArguments & {
+  layerId: string;
+  layerType: LayerType;
+  valueAccessor?: string;
+  xAccessor?: string;
+  yAccessor?: string;
+  shape: ChartShapes;
+};
+
+export type Palette = PaletteOutput<CustomPaletteParams> & { accessor: string };
+
+export type HeatmapVisualizationState = HeatmapLayerState & {
+  // need to store the current accessor to reset the color stops at accessor change
+  palette?: Palette;
+};
+
+export interface HeatmapConfiguration {
+  shape: PartitionChartType;
+  layers: PartitionLayerState[];
+  palette?: PaletteOutput;
+}
+
 export type Configuration =
   | XYConfiguration
   | TableVisConfiguration
   | PartitionVisConfiguration
   | MetricVisConfiguration
-  | GaugeVisConfiguration;
+  | GaugeVisConfiguration
+  | HeatmapConfiguration;
