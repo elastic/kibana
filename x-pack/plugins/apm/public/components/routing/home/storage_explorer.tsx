ca5cd360d114593d2e067049441499974687b438
gbamparop <georgios.bamparopoulos@elastic.co>
2022-10-19 15:43:52 +0100

    Remove beta badge

--- x-pack/plugins/apm/public/components/routing/home/storage_explorer.tsx
+++ x-pack/plugins/apm/public/components/routing/home/storage_explorer.tsx
@@ -6,12 +6,11 @@
  */
 
 import React from 'react';
-import { EuiTitle, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiTitle } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import * as t from 'io-ts';
 import { EuiLink } from '@elastic/eui';
 import { StorageExplorer } from '../../app/storage_explorer';
-import { BetaBadge } from '../../shared/beta_badge';
 import { ApmMainTemplate } from '../templates/apm_main_template';
 import { Breadcrumb } from '../../app/breadcrumb';
 import {
@@ -33,24 +32,13 @@ export const storageExplorer = {
           pageHeader={{
             alignItems: 'center',
             pageTitle: (
-              <EuiFlexGroup
-                justifyContent="flexStart"
-                gutterSize="s"
-                alignItems="baseline"
-              >
-                <EuiFlexItem grow={false}>
-                  <EuiTitle size="l">
-                    <h1>
-                      {i18n.translate('xpack.apm.views.storageExplorer.title', {
-                        defaultMessage: 'Storage explorer',
-                      })}
-                    </h1>
-                  </EuiTitle>
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  <BetaBadge />
-                </EuiFlexItem>
-              </EuiFlexGroup>
+              <EuiTitle size="l">
+                <h1>
+                  {i18n.translate('xpack.apm.views.storageExplorer.title', {
+                    defaultMessage: 'Storage explorer',
+                  })}
+                </h1>
+              </EuiTitle>
             ),
             rightSideItems: [
               <EuiLink href={getStorageExplorerFeedbackHref()} target="_blank">
