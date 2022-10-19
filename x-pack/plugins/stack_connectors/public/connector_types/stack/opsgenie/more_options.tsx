82cc52bd75725c7da2b236a6c1c18d4aa5935264
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-10-19 21:30:22 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/stack_connectors/public/connector_types/stack/opsgenie/more_options.tsx
+++ x-pack/plugins/stack_connectors/public/connector_types/stack/opsgenie/more_options.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
 const MoreOptionsComponent: React.FC<{}> = ({}) => {};
 
