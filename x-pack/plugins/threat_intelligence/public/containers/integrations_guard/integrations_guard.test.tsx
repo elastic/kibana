a3222cf4f6328b1fbcac8dff6db38ee33a7cb019
PhilippeOberti <philippe.oberti@elastic.co>
2022-10-20 13:59:24 -0500

    [TIP] Extract useQuery logic to custom hook
    - extract logic to use_integrations hook to better unit test
    - move filterIntegration logic to utils to also better unit tests
    - add more unit tests to the IntegrationGuard

--- x-pack/plugins/threat_intelligence/public/containers/integrations_guard/integrations_guard.test.tsx
+++ x-pack/plugins/threat_intelligence/public/containers/integrations_guard/integrations_guard.test.tsx
@@ -5,19 +5,27 @@
  * 2.0.
  */
 
+import { UseQueryResult } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
 import React from 'react';
 import { IntegrationsGuard } from '.';
 import { TestProvidersComponent } from '../../common/mocks/test_providers';
-import { useIntegrationsPageLink, useTIDocumentationLink } from '../../hooks';
+import {
+  Integration,
+  useIntegrations,
+  useIntegrationsPageLink,
+  useTIDocumentationLink,
+} from '../../hooks';
 import { useIndicatorsTotalCount } from '../../modules/indicators';
+import { INSTALLATION_STATUS, THREAT_INTELLIGENCE_CATEGORY } from '../../utils';
 
 jest.mock('../../modules/indicators/hooks/use_total_count');
 jest.mock('../../hooks/use_integrations_page_link');
 jest.mock('../../hooks/use_documentation_link');
+jest.mock('../../hooks/use_integrations');
 
 describe('IntegrationsGuard', () => {
-  it('should render nothing at all when indicator count and integrations are being loaded', () => {
+  it('should render loading when indicator count and integrations are being loaded', async () => {
     (
       useIndicatorsTotalCount as jest.MockedFunction<typeof useIndicatorsTotalCount>
     ).mockReturnValue({
@@ -30,7 +38,60 @@ describe('IntegrationsGuard', () => {
     (useTIDocumentationLink as jest.MockedFunction<typeof useTIDocumentationLink>).mockReturnValue(
       ''
     );
-    // const mockIntegrations = { items: [] };
+    (useIntegrations as jest.MockedFunction<typeof useIntegrations>).mockReturnValue({
+      isLoading: true,
+      data: [],
+    } as unknown as UseQueryResult<Integration[]>);
+
+    const { asFragment } = render(<IntegrationsGuard>should be restricted</IntegrationsGuard>, {
+      wrapper: TestProvidersComponent,
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render loading when indicator only is loading', async () => {
+    (
+      useIndicatorsTotalCount as jest.MockedFunction<typeof useIndicatorsTotalCount>
+    ).mockReturnValue({
+      count: 0,
+      isLoading: true,
+    });
+    (
+      useIntegrationsPageLink as jest.MockedFunction<typeof useIntegrationsPageLink>
+    ).mockReturnValue('');
+    (useTIDocumentationLink as jest.MockedFunction<typeof useTIDocumentationLink>).mockReturnValue(
+      ''
+    );
+    (useIntegrations as jest.MockedFunction<typeof useIntegrations>).mockReturnValue({
+      isLoading: false,
+      data: [],
+    } as unknown as UseQueryResult<Integration[]>);
+
+    const { asFragment } = render(<IntegrationsGuard>should be restricted</IntegrationsGuard>, {
+      wrapper: TestProvidersComponent,
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('should render loading when integrations only are loading', async () => {
+    (
+      useIndicatorsTotalCount as jest.MockedFunction<typeof useIndicatorsTotalCount>
+    ).mockReturnValue({
+      count: 0,
+      isLoading: true,
+    });
+    (
+      useIntegrationsPageLink as jest.MockedFunction<typeof useIntegrationsPageLink>
+    ).mockReturnValue('');
+    (useTIDocumentationLink as jest.MockedFunction<typeof useTIDocumentationLink>).mockReturnValue(
+      ''
+    );
+    (useIntegrations as jest.MockedFunction<typeof useIntegrations>).mockReturnValue({
+      isLoading: true,
+      data: [],
+    } as unknown as UseQueryResult<Integration[]>);
 
     const { asFragment } = render(<IntegrationsGuard>should be restricted</IntegrationsGuard>, {
       wrapper: TestProvidersComponent,
@@ -52,7 +113,10 @@ describe('IntegrationsGuard', () => {
     (useTIDocumentationLink as jest.MockedFunction<typeof useTIDocumentationLink>).mockReturnValue(
       ''
     );
-    // const mockIntegrations = { items: [] };
+    (useIntegrations as jest.MockedFunction<typeof useIntegrations>).mockReturnValue({
+      isLoading: false,
+      data: [],
+    } as unknown as UseQueryResult<Integration[]>);
 
     const { asFragment } = render(<IntegrationsGuard>should be restricted</IntegrationsGuard>, {
       wrapper: TestProvidersComponent,
@@ -60,14 +124,17 @@ describe('IntegrationsGuard', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it('should render indicators table when loading is done and we have some indicators', async () => {
+  it('should render indicators table when we have some indicators', async () => {
     (
       useIndicatorsTotalCount as jest.MockedFunction<typeof useIndicatorsTotalCount>
     ).mockReturnValue({
       count: 7,
       isLoading: false,
     });
-    // const mockIntegrations = { items: [] };
+    (useIntegrations as jest.MockedFunction<typeof useIntegrations>).mockReturnValue({
+      isLoading: false,
+      data: [],
+    } as unknown as UseQueryResult<Integration[]>);
 
     const { asFragment } = render(<IntegrationsGuard>should be restricted</IntegrationsGuard>, {
       wrapper: TestProvidersComponent,
@@ -75,22 +142,23 @@ describe('IntegrationsGuard', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it('should render indicators page when loading is done and we have some ti integrations installed', async () => {
+  it('should render indicators page when we have some ti integrations installed', async () => {
     (
       useIndicatorsTotalCount as jest.MockedFunction<typeof useIndicatorsTotalCount>
     ).mockReturnValue({
       count: 0,
-      isLoading: true,
+      isLoading: false,
     });
-    // const mockIntegrations = {
-    //   items: [
-    //     {
-    //       categories: [THREAT_INTELLIGENCE_CATEGORY],
-    //       id: '123',
-    //       status: INSTALLATION_STATUS.Installed,
-    //     },
-    //   ],
-    // };
+    (useIntegrations as jest.MockedFunction<typeof useIntegrations>).mockReturnValue({
+      isLoading: false,
+      data: [
+        {
+          categories: [THREAT_INTELLIGENCE_CATEGORY],
+          id: '123',
+          status: INSTALLATION_STATUS.Installed,
+        },
+      ],
+    } as unknown as UseQueryResult<Integration[]>);
 
     const { asFragment } = render(<IntegrationsGuard>should be restricted</IntegrationsGuard>, {
       wrapper: TestProvidersComponent,
--- x-pack/plugins/threat_intelligence/public/containers/integrations_guard/integrations_guard.tsx
+++ x-pack/plugins/threat_intelligence/public/containers/integrations_guard/integrations_guard.tsx
@@ -7,67 +7,22 @@
 
 import { EuiLoadingLogo } from '@elastic/eui';
 import React, { FC } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { useIntegrations } from '../../hooks';
 import { EmptyPage } from '../../modules/empty_page';
 import { useIndicatorsTotalCount } from '../../modules/indicators';
 import { SecuritySolutionPluginTemplateWrapper } from '../security_solution_plugin_template_wrapper';
-import { useKibana } from '../../hooks';
-
-type IntegrationInstallStatus = 'installed' | 'installing' | 'install_failed';
-
-const INTEGRATIONS_URL = '/api/fleet/epm/packages';
-
-const INTEGRATIONS_STALE_TIME = 2000;
-
-export const THREAT_INTELLIGENCE_CATEGORY = 'threat_intel';
-
-export const THREAT_INTELLIGENCE_UTILITIES = 'ti_util';
-
-export const INSTALLATION_STATUS = {
-  Installed: 'installed',
-  Installing: 'installing',
-  InstallFailed: 'install_failed',
-  NotInstalled: 'not_installed',
-};
-
-interface IntegrationResponse {
-  items: Integration[];
-}
-
-interface Integration {
-  categories: string[];
-  id: string;
-  status: IntegrationInstallStatus;
-}
 
 /**
- * Renders children only if TI integrations are enabled and indicators are received
+ * Renders the indicators page if the user has some Threat Intelligence integrations installed or
+ * the user is receiving indicators.
+ * If none are received, show the EmptyPage with a link to go install integrations.
+ * While the indicators call and the integrations call are loading, display a loading screen.
  */
 export const IntegrationsGuard: FC = ({ children }) => {
-  const { http } = useKibana().services;
-
   const { isLoading: indicatorsTotalCountLoading, count: indicatorsTotalCount } =
     useIndicatorsTotalCount();
 
-  // we only select integrations: (see https://github.com/elastic/security-team/issues/4374)
-  // - of status `installed`
-  // - with `threat_intel` category
-  // - excluding `ti_util` integration
-  const { isLoading: integrationLoading, data: installedTIIntegrations } = useQuery(
-    ['integrations'],
-    () => http.get<IntegrationResponse>(INTEGRATIONS_URL),
-    {
-      select: (data: IntegrationResponse) =>
-        data?.items.filter(
-          (pkg: any) =>
-            pkg.status === INSTALLATION_STATUS.Installed &&
-            pkg.categories.find((category: string) => category === THREAT_INTELLIGENCE_CATEGORY) !=
-              null &&
-            pkg.id !== THREAT_INTELLIGENCE_UTILITIES
-        ) || [],
-      staleTime: INTEGRATIONS_STALE_TIME,
-    }
-  );
+  const { isLoading: integrationLoading, data: installedTIIntegrations } = useIntegrations();
 
   if (integrationLoading || indicatorsTotalCountLoading) {
     return (
@@ -78,7 +33,6 @@ export const IntegrationsGuard: FC = ({ children }) => {
   }
 
   // show indicators page if there are indicators, or if some ti integrations have been added
-  // @ts-ignore
-  const showIndicatorsPage = indicatorsTotalCount > 0 || installedTIIntegrations.length > 0;
+  const showIndicatorsPage = indicatorsTotalCount > 0 || (installedTIIntegrations || []).length > 0;
   return showIndicatorsPage ? <>{children}</> : <EmptyPage />;
 };
