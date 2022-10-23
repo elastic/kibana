d08f2f65295a0d23274bd1a7c6b794b64b9dc42f
Julia Guskova <iuliia.guskova@elastic.co>
2022-10-23 10:10:41 +0200

    wait before next retry func tests

--- x-pack/plugins/alerting/server/rules_client/lib/wait_before_next_retry.ts
+++ x-pack/plugins/alerting/server/rules_client/lib/wait_before_next_retry.ts
@@ -11,14 +11,17 @@ export const RETRY_IF_CONFLICTS_ATTEMPTS = 2;
 // note: we considered making this random, to help avoid a stampede, but
 // with 1 retry it probably doesn't matter, and adding randomness could
 // make it harder to diagnose issues
-const RETRY_IF_CONFLICTS_DELAY = 250;
+export const RETRY_IF_CONFLICTS_DELAY = 250;
+
+export const randomDelayMs = Math.floor(Math.random() * 100);
+export const getExponentialDelayMultiplier = (retries: number) =>
+  1 + (RETRY_IF_CONFLICTS_ATTEMPTS - retries) ** 2;
 
 /**
  * exponential delay before retry with adding random delay
  */
 export const waitBeforeNextRetry = async (retries: number): Promise<void> => {
-  const exponentialDelayMultiplier = 1 + (RETRY_IF_CONFLICTS_ATTEMPTS - retries) ** 2;
-  const randomDelayMs = Math.floor(Math.random() * 100);
+  const exponentialDelayMultiplier = getExponentialDelayMultiplier(retries);
 
   await new Promise((resolve) =>
     setTimeout(resolve, RETRY_IF_CONFLICTS_DELAY * exponentialDelayMultiplier + randomDelayMs)
