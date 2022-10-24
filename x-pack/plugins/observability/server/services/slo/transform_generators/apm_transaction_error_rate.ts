8c8731ff067b29562cc05b563c49562892b686d4
Kevin Delemme <kevin.delemme@elastic.co>
2022-10-24 10:21:26 -0400

    feat(slo): Support custom KQL indicator type (#143786)

--- x-pack/plugins/observability/server/services/slo/transform_generators/apm_transaction_error_rate.ts
+++ x-pack/plugins/observability/server/services/slo/transform_generators/apm_transaction_error_rate.ts
@@ -10,6 +10,7 @@ import {
   MappingRuntimeFieldType,
   TransformPutTransformRequest,
 } from '@elastic/elasticsearch/lib/api/types';
+import { InvalidTransformError } from '../../../errors';
 import { ALL_VALUE, apmTransactionErrorRateIndicatorSchema } from '../../../types/schema';
 import { getSLOTransformTemplate } from '../../../assets/transform_templates/slo_transform_template';
 import { TransformGenerator } from '.';
@@ -27,7 +28,7 @@ const DEFAULT_GOOD_STATUS_CODES = ['2xx', '3xx', '4xx'];
 export class ApmTransactionErrorRateTransformGenerator implements TransformGenerator {
   public getTransformParams(slo: SLO): TransformPutTransformRequest {
     if (!apmTransactionErrorRateIndicatorSchema.is(slo.indicator)) {
-      throw new Error(`Cannot handle SLO of indicator type: ${slo.indicator.type}`);
+      throw new InvalidTransformError(`Cannot handle SLO of indicator type: ${slo.indicator.type}`);
     }
 
     return getSLOTransformTemplate(
