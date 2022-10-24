41475bdaeca8e3ea41dd8da7e0a307615b77021a
nreese <reese.nathan@elastic.co>
2022-10-24 13:05:34 -0600

    [Maps] add support for hex aggregation with geo_shape field

--- x-pack/plugins/maps/public/classes/sources/es_geo_grid_source/update_source_editor.tsx
+++ x-pack/plugins/maps/public/classes/sources/es_geo_grid_source/update_source_editor.tsx
@@ -13,13 +13,7 @@ import { EuiPanel, EuiSpacer, EuiComboBoxOptionOption, EuiTitle } from '@elastic
 import { DataViewField } from '@kbn/data-views-plugin/public';
 import { indexPatterns } from '@kbn/data-plugin/public';
 import { getDataViewNotFoundMessage } from '../../../../common/i18n_getters';
-import {
-  AGG_TYPE,
-  ES_GEO_FIELD_TYPE,
-  GRID_RESOLUTION,
-  LAYER_TYPE,
-  RENDER_AS,
-} from '../../../../common/constants';
+import { AGG_TYPE, GRID_RESOLUTION, LAYER_TYPE, RENDER_AS } from '../../../../common/constants';
 import { MetricsEditor } from '../../../components/metrics_editor';
 import { getIndexPatternService } from '../../../kibana_services';
 import { ResolutionEditor } from './resolution_editor';
@@ -41,7 +35,6 @@ interface Props {
 }
 
 interface State {
-  geoFieldType?: ES_GEO_FIELD_TYPE;
   metricsEditorKey: string;
   fields: DataViewField[];
   loadError?: string;
@@ -80,11 +73,8 @@ export class UpdateSourceEditor extends Component<Props, State> {
       return;
     }
 
-    const geoField = indexPattern.fields.getByName(this.props.geoFieldName);
-
     this.setState({
       fields: indexPattern.fields.filter((field) => !indexPatterns.isNestedField(field)),
-      geoFieldType: geoField ? (geoField.type as ES_GEO_FIELD_TYPE) : undefined,
     });
   }
 
@@ -188,7 +178,6 @@ export class UpdateSourceEditor extends Component<Props, State> {
           />
           <RenderAsSelect
             isColumnCompressed
-            geoFieldType={this.state.geoFieldType}
             renderAs={this.props.renderAs}
             onChange={this._onRequestTypeSelect}
           />
