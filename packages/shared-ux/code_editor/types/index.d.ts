eb59e9ae8d80e1d72e24a538a36d1a88882e2492
rshen91 <rshen@elastic.co>
2022-10-25 09:40:21 -0600

    add some types

--- packages/shared-ux/code_editor/types/index.d.ts
+++ packages/shared-ux/code_editor/types/index.d.ts
@@ -5,7 +5,95 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
+import { monaco } from '@kbn/monaco';
 
-export function foo() {
-  return 'hello world';
+export interface Props {
+  /** Width of editor. Defaults to 100%. */
+  width?: string | number;
+
+  /** Height of editor. Defaults to 100%. */
+  height?: string | number;
+
+  /** ID of the editor language */
+  languageId: string;
+
+  /** Value of the editor */
+  value: string;
+
+  /** Function invoked when text in editor is changed */
+  onChange?: (value: string, event: monaco.editor.IModelContentChangedEvent) => void;
+
+  /**
+   * Options for the Monaco Code Editor
+   * Documentation of options can be found here:
+   * https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.istandaloneeditorconstructionoptions.html
+   */
+  options?: monaco.editor.IStandaloneEditorConstructionOptions;
+
+  /**
+   * Suggestion provider for autocompletion
+   * Documentation for the provider can be found here:
+   * https://microsoft.github.io/monaco-editor/api/interfaces/monaco.languages.completionitemprovider.html
+   */
+  suggestionProvider?: monaco.languages.CompletionItemProvider;
+
+  /**
+   * Signature provider for function parameter info
+   * Documentation for the provider can be found here:
+   * https://microsoft.github.io/monaco-editor/api/interfaces/monaco.languages.signaturehelpprovider.html
+   */
+  signatureProvider?: monaco.languages.SignatureHelpProvider;
+
+  /**
+   * Hover provider for hover documentation
+   * Documentation for the provider can be found here:
+   * https://microsoft.github.io/monaco-editor/api/interfaces/monaco.languages.hoverprovider.html
+   */
+  hoverProvider?: monaco.languages.HoverProvider;
+
+  /**
+   * Language config provider for bracket
+   * Documentation for the provider can be found here:
+   * https://microsoft.github.io/monaco-editor/api/interfaces/monaco.languages.languageconfiguration.html
+   */
+  languageConfiguration?: monaco.languages.LanguageConfiguration;
+
+  /**
+   * Function called before the editor is mounted in the view
+   */
+  editorWillMount?: () => void;
+  /**
+   * Function called before the editor is mounted in the view
+   * and completely replaces the setup behavior called by the component
+   */
+  overrideEditorWillMount?: () => void;
+
+  /**
+   * Function called after the editor is mounted in the view
+   */
+  editorDidMount?: (editor: monaco.editor.IStandaloneCodeEditor) => void;
+
+  /**
+   * Should the editor use the dark theme
+   */
+  useDarkTheme?: boolean;
+
+  /**
+   * Should the editor use a transparent background
+   */
+  transparentBackground?: boolean;
+
+  /**
+   * Should the editor be rendered using the fullWidth EUI attribute
+   */
+  fullWidth?: boolean;
+
+  placeholder?: string;
+  /**
+   * Accessible name for the editor. (Defaults to "Code editor")
+   */
+  'aria-label'?: string;
+
+  isCopyable?: boolean;
+  allowFullScreen?: boolean;
 }
