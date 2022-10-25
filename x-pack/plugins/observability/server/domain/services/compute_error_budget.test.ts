019a07c6e762c268683b2e11333036ced9714e7a
Kevin Delemme <kevin.delemme@elastic.co>
2022-10-25 13:45:14 -0400

    Fix computing error budget for timeslices calendar aligned slo

--- x-pack/plugins/observability/server/domain/services/compute_error_budget.test.ts
+++ x-pack/plugins/observability/server/domain/services/compute_error_budget.test.ts
@@ -10,6 +10,7 @@ import { oneMinute } from '../../services/slo/fixtures/duration';
 import { createSLO } from '../../services/slo/fixtures/slo';
 import { sevenDaysRolling, weeklyCalendarAligned } from '../../services/slo/fixtures/time_window';
 import { computeErrorBudget } from './compute_error_budget';
+import { toDateRange } from './date_range';
 
 describe('computeErrorBudget', () => {
   describe('for occurrences based SLO', () => {
@@ -20,7 +21,12 @@ describe('computeErrorBudget', () => {
           time_window: sevenDaysRolling(),
           objective: { target: 0.95 },
         });
-        const errorBudget = computeErrorBudget(slo, { good: 97, total: 100 });
+        const dateRange = toDateRange(slo.time_window);
+        const errorBudget = computeErrorBudget(slo, {
+          good: 97,
+          total: 100,
+          date_range: dateRange,
+        });
 
         expect(errorBudget).toEqual({
           initial: 0.05,
@@ -37,7 +43,12 @@ describe('computeErrorBudget', () => {
           time_window: weeklyCalendarAligned(twoDaysAgo()),
           objective: { target: 0.95 },
         });
-        const errorBudget = computeErrorBudget(slo, { good: 97, total: 100 });
+        const dateRange = toDateRange(slo.time_window);
+        const errorBudget = computeErrorBudget(slo, {
+          good: 97,
+          total: 100,
+          date_range: dateRange,
+        });
 
         expect(errorBudget).toEqual({
           initial: 0.05,
@@ -56,8 +67,13 @@ describe('computeErrorBudget', () => {
           time_window: sevenDaysRolling(),
           objective: { target: 0.95, timeslice_target: 0.95, timeslice_window: oneMinute() },
         });
+        const dateRange = toDateRange(slo.time_window);
         // 7 days sliced in 1m buckets = 10,080 slices
-        const errorBudget = computeErrorBudget(slo, { good: 9987, total: 10080 });
+        const errorBudget = computeErrorBudget(slo, {
+          good: 9987,
+          total: 10080,
+          date_range: dateRange,
+        });
 
         expect(errorBudget).toEqual({
           initial: 0.05,
@@ -74,13 +90,17 @@ describe('computeErrorBudget', () => {
           time_window: weeklyCalendarAligned(twoDaysAgo()),
           objective: { target: 0.95, timeslice_target: 0.95, timeslice_window: oneMinute() },
         });
+        const dateRange = toDateRange(slo.time_window);
         // 2 days sliced in 1m buckets = 2,880 slices (slices we have data for) = total
         // 7 days sliced in 1m buckets = 10,080 slices (all slices for the window) = window_total
-        const errorBudget = computeErrorBudget(slo, { good: 2823, total: 2880 });
+        const errorBudget = computeErrorBudget(slo, {
+          good: 2823,
+          total: 2880,
+          date_range: dateRange,
+        });
 
         // error rate = (total - good) / window_total = (2880 - 2823) / 10080 = 0.00565476
         // consumed = error rate / error budget = 0.00565476 / 0.05 = 0.1130952
-
         expect(errorBudget).toEqual({
           initial: 0.05,
           consumed: 0.113095,
@@ -92,7 +112,8 @@ describe('computeErrorBudget', () => {
 
   it("returns default values when total events is '0'", () => {
     const slo = createSLO();
-    const errorBudget = computeErrorBudget(slo, { good: 100, total: 0 });
+    const dateRange = toDateRange(slo.time_window);
+    const errorBudget = computeErrorBudget(slo, { good: 100, total: 0, date_range: dateRange });
 
     expect(errorBudget).toEqual({
       initial: 0.001, // 0.1%
@@ -103,7 +124,8 @@ describe('computeErrorBudget', () => {
 
   it("computes the error budget when 'good > total' events", () => {
     const slo = createSLO();
-    const errorBudget = computeErrorBudget(slo, { good: 9999, total: 9 });
+    const dateRange = toDateRange(slo.time_window);
+    const errorBudget = computeErrorBudget(slo, { good: 9999, total: 9, date_range: dateRange });
 
     expect(errorBudget).toEqual({
       initial: 0.001,
@@ -114,7 +136,8 @@ describe('computeErrorBudget', () => {
 
   it('computes the error budget with all good events', () => {
     const slo = createSLO();
-    const errorBudget = computeErrorBudget(slo, { good: 100, total: 100 });
+    const dateRange = toDateRange(slo.time_window);
+    const errorBudget = computeErrorBudget(slo, { good: 100, total: 100, date_range: dateRange });
 
     expect(errorBudget).toEqual({
       initial: 0.001,
@@ -125,7 +148,8 @@ describe('computeErrorBudget', () => {
 
   it('computes the error budget when exactly consumed', () => {
     const slo = createSLO();
-    const errorBudget = computeErrorBudget(slo, { good: 999, total: 1000 });
+    const dateRange = toDateRange(slo.time_window);
+    const errorBudget = computeErrorBudget(slo, { good: 999, total: 1000, date_range: dateRange });
 
     expect(errorBudget).toEqual({
       initial: 0.001,
@@ -136,7 +160,8 @@ describe('computeErrorBudget', () => {
 
   it('computes the error budget with rounded values', () => {
     const slo = createSLO();
-    const errorBudget = computeErrorBudget(slo, { good: 333, total: 777 });
+    const dateRange = toDateRange(slo.time_window);
+    const errorBudget = computeErrorBudget(slo, { good: 333, total: 777, date_range: dateRange });
 
     expect(errorBudget).toEqual({
       initial: 0.001,
@@ -147,7 +172,8 @@ describe('computeErrorBudget', () => {
 
   it('computes the error budget with no good events', () => {
     const slo = createSLO();
-    const errorBudget = computeErrorBudget(slo, { good: 0, total: 100 });
+    const dateRange = toDateRange(slo.time_window);
+    const errorBudget = computeErrorBudget(slo, { good: 0, total: 100, date_range: dateRange });
 
     expect(errorBudget).toEqual({
       initial: 0.001,
--- x-pack/plugins/observability/server/domain/services/compute_error_budget.ts
+++ x-pack/plugins/observability/server/domain/services/compute_error_budget.ts
@@ -5,17 +5,43 @@
  * 2.0.
  */
 
-import { ErrorBudget, IndicatorData, SLO } from '../../types/models';
+import moment from 'moment';
+import { ErrorBudget, IndicatorData, SLO, toMomentUnitOfTime } from '../../types/models';
+import {
+  calendarAlignedTimeWindowSchema,
+  timeslicesBudgetingMethodSchema,
+} from '../../types/schema';
 import { toHighPrecision } from '../../utils/number';
 
 export function computeErrorBudget(slo: SLO, sliData: IndicatorData): ErrorBudget {
-  const { good, total } = sliData;
+  const { good, total, date_range: dateRange } = sliData;
   const initialErrorBudget = toHighPrecision(1 - slo.objective.target);
   if (total === 0 || good >= total) {
     return {
       initial: initialErrorBudget,
       consumed: 0,
       remaining: 1,
+    };
+  }
+
+  if (
+    timeslicesBudgetingMethodSchema.is(slo.budgeting_method) &&
+    calendarAlignedTimeWindowSchema.is(slo.time_window)
+  ) {
+    const dateRangeDurationInUnit = moment(dateRange.to).diff(
+      dateRange.from,
+      toMomentUnitOfTime(slo.objective.timeslice_window!.unit)
+    );
+    const totalSlices = Math.ceil(dateRangeDurationInUnit / slo.objective.timeslice_window!.value);
+
+    const consumedErrorBudget = toHighPrecision(
+      (total - good) / (totalSlices * initialErrorBudget)
+    );
+    const remainingErrorBudget = Math.max(toHighPrecision(1 - consumedErrorBudget), 0);
+    return {
+      initial: initialErrorBudget,
+      consumed: consumedErrorBudget,
+      remaining: remainingErrorBudget,
     };
   }
 
