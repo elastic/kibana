f4b9070aca7fb80287ff3d6a546be12e03081578
Paul Tavares <paul.tavares@elastic.co>
2022-10-25 15:58:09 -0400

    test outline for ResponseActionFileDownloadLink component

---
+++ x-pack/plugins/security_solution/public/management/components/response_action_file_download_link/response_action_file_download_link.test.tsx
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+describe('When using the `ResponseActionFileDownloadLink` component', () => {
+  it.todo('should show download button if file is available');
+
+  it.todo('should show file no longer available message if status is DELETED');
+
+  it.todo('should show file no longer available message if file info api returns 404');
+
+  it.todo('should show loading indicator while calling file info api');
+
+  it.todo('should show file info API error if one was encountered');
+
+  it.todo('should show nothing if user does not have authz');
+
+  it.todo('should show nothing if action is not complete');
+
+  it.todo('should show nothing if action was not successful');
+});
