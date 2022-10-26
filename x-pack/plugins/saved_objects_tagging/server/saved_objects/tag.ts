ef55832cf3413a89ff380dc1359d6e95ce855eaf
Mark Hopkin <mark.hopkin@elastic.co>
2022-10-26 10:54:05 +0100

    Revert "Add SO migration"
    This reverts commit 4aeeea658c79d30cfb7ad96090b87418a3b72ad2.

--- x-pack/plugins/saved_objects_tagging/server/saved_objects/tag.ts
+++ x-pack/plugins/saved_objects_tagging/server/saved_objects/tag.ts
@@ -8,8 +8,6 @@
 import { SavedObject, SavedObjectsType } from '@kbn/core/server';
 import { tagSavedObjectTypeName, TagAttributes } from '../../common';
 
-import { migrateFleetManagedTagToV860 } from './migrations';
-
 export const tagType: SavedObjectsType = {
   name: tagSavedObjectTypeName,
   hidden: false,
@@ -33,8 +31,5 @@ export const tagType: SavedObjectsType = {
     defaultSearchField: 'name',
     icon: 'tag',
     getTitle: (obj: SavedObject<TagAttributes>) => obj.attributes.name,
-  },
-  migrations: {
-    '8.6.0': migrateFleetManagedTagToV860,
   },
 };
