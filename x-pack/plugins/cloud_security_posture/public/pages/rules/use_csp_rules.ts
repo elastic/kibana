3cd5b7bf36b5caae33831fce4f056b0b9d707ab6
ofiriro3 <ofiriro3@Gmail.com>
2022-10-26 14:21:34 +0300

    Removing the UI of update_rules_config

--- x-pack/plugins/cloud_security_posture/public/pages/rules/use_csp_rules.ts
+++ x-pack/plugins/cloud_security_posture/public/pages/rules/use_csp_rules.ts
@@ -4,12 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { FunctionKeys } from 'utility-types';
 import type { SavedObjectsFindOptions, SimpleSavedObject } from '@kbn/core/public';
-import { i18n } from '@kbn/i18n';
 import {
-  UPDATE_RULES_CONFIG_ROUTE_PATH,
   CSP_RULE_SAVED_OBJECT_TYPE,
 } from '../../../common/constants';
 import type { CspRule } from '../../../common/schemas';
@@ -38,39 +36,3 @@ export const useFindCspRules = ({ search, page, perPage, filter }: RulesQuery) =
   );
 };
 
-const UPDATE_FAILED_TEXT = i18n.translate('xpack.csp.rules.rulesErrorToast.updateFailedTitle', {
-  defaultMessage: 'Update failed',
-});
-
-export const useBulkUpdateCspRules = () => {
-  const { notifications, http } = useKibana().services;
-  const queryClient = useQueryClient();
-
-  return useMutation(
-    async ({
-      savedObjectRules,
-      packagePolicyId,
-    }: {
-      savedObjectRules: RuleSavedObject[];
-      packagePolicyId: CspRule['package_policy_id'];
-    }) =>
-      http.post(UPDATE_RULES_CONFIG_ROUTE_PATH, {
-        body: JSON.stringify({
-          package_policy_id: packagePolicyId,
-          rules: savedObjectRules.map((savedObjectRule) => ({
-            id: savedObjectRule.id,
-            enabled: savedObjectRule.attributes.enabled,
-          })),
-        }),
-      }),
-    {
-      onError: (err) => {
-        if (err instanceof Error) notifications.toasts.addError(err, { title: UPDATE_FAILED_TEXT });
-        else notifications.toasts.addDanger(UPDATE_FAILED_TEXT);
-      },
-      onSettled: () =>
-        // Invalidate all queries for simplicity
-        queryClient.invalidateQueries([CSP_RULE_SAVED_OBJECT_TYPE]),
-    }
-  );
-};
