6958b73c32b7f2a3a5efee4eb2589fd6582dd3f9
Jean-Louis Leysens <jloleysens@gmail.com>
2022-10-26 16:04:25 +0200

    various updates

--- src/plugins/files/.storybook/manager.ts
+++ src/plugins/files/.storybook/manager.ts
@@ -1,8 +1,9 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { addons } from '@storybook/addons';
@@ -13,7 +14,7 @@ addons.setConfig({
   theme: create({
     base: 'light',
     brandTitle: 'Kibana React Storybook',
-    brandUrl: 'https://github.com/elastic/kibana/tree/main/x-pack/plugins/files',
+    brandUrl: 'https://github.com/elastic/kibana/tree/main/src/plugins/files',
   }),
   showPanel: true.valueOf,
   selectedPanel: PANEL_ID,
--- src/plugins/files/jest.config.js
+++ src/plugins/files/jest.config.js
@@ -1,15 +1,16 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/files'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/files',
+  roots: ['<rootDir>/src/plugins/files'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/files',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/files/{common,public,server}/**/*.{js,ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/plugins/files/{common,public,server}/**/*.{js,ts,tsx}'],
 };
