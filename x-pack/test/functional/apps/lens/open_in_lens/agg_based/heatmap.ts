f782daf2255edb4970729a1a2a62fcfc8f617126
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-10-26 14:00:37 +0300

    Added test for custom ranges.

--- x-pack/test/functional/apps/lens/open_in_lens/agg_based/heatmap.ts
+++ x-pack/test/functional/apps/lens/open_in_lens/agg_based/heatmap.ts
@@ -9,9 +9,10 @@ import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
-  const { visualize, lens, timePicker, visEditor } = getPageObjects([
+  const { visualize, lens, visChart, timePicker, visEditor } = getPageObjects([
     'visualize',
     'lens',
+    'visChart',
     'timePicker',
     'visEditor',
   ]);
@@ -88,6 +89,119 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       await visEditor.clickGo();
 
       expect(await visualize.hasNavigateToLensButton()).to.eql(false);
+    });
+
+    it('should respect heatmap colors number', async () => {
+      await visEditor.clickBucket('X-axis');
+      await visEditor.selectAggregation('Terms');
+      await visEditor.selectField('machine.os.raw');
+      await visEditor.clickGo();
+
+      await visEditor.clickOptionsTab();
+      await visEditor.changeHeatmapColorNumbers(6);
+      await visEditor.clickGo();
+      await visChart.waitForVisualizationRenderingStabilized();
+
+      await visualize.navigateToLensFromAnotherVisulization();
+      await lens.waitForVisualization('heatmapChart');
+      const debugState = await lens.getCurrentChartDebugState('heatmapChart');
+
+      if (!debugState) {
+        throw new Error('Debug state is not available');
+      }
+
+      expect(debugState.legend!.items).to.eql([
+        {
+          color: '#006837',
+          key: '0 - 16.67',
+          name: '0 - 16.67',
+        },
+        {
+          color: '#4CB15D',
+          key: '16.67 - 33.33',
+          name: '16.67 - 33.33',
+        },
+        {
+          color: '#B7E075',
+          key: '33.33 - 50',
+          name: '33.33 - 50',
+        },
+        {
+          color: '#FEFEBD',
+          key: '50 - 66.67',
+          name: '50 - 66.67',
+        },
+        {
+          color: '#FDBF6F',
+          key: '66.67 - 83.33',
+          name: '66.67 - 83.33',
+        },
+        {
+          color: '#EA5839',
+          key: '83.33 - 100',
+          name: '83.33 - 100',
+        },
+      ]);
+    });
+
+    it('should show respect heatmap custom color ranges', async () => {
+      await visEditor.clickBucket('X-axis');
+      await visEditor.selectAggregation('Terms');
+      await visEditor.selectField('machine.os.raw');
+      await visEditor.clickGo();
+
+      await visEditor.clickOptionsTab();
+      await visEditor.clickOptionsTab();
+      await visEditor.clickEnableCustomRanges();
+      await visEditor.clickAddRange();
+      await visEditor.clickAddRange();
+      await visEditor.clickAddRange();
+      await visEditor.clickAddRange();
+      await visEditor.clickAddRange();
+
+      await visEditor.clickGo();
+      await visChart.waitForVisualizationRenderingStabilized();
+
+      await visualize.navigateToLensFromAnotherVisulization();
+      await lens.waitForVisualization('heatmapChart');
+      const debugState = await lens.getCurrentChartDebugState('heatmapChart');
+
+      if (!debugState) {
+        throw new Error('Debug state is not available');
+      }
+
+      expect(debugState.legend!.items).to.eql([
+        {
+          color: '#006837',
+          key: '0 - 100',
+          name: '0 - 100',
+        },
+        {
+          color: '#65BC62',
+          key: '100 - 200',
+          name: '100 - 200',
+        },
+        {
+          color: '#D8EF8C',
+          key: '200 - 300',
+          name: '200 - 300',
+        },
+        {
+          color: '#FEDF8B',
+          key: '300 - 400',
+          name: '300 - 400',
+        },
+        {
+          color: '#F36D43',
+          key: '400 - 500',
+          name: '400 - 500',
+        },
+        {
+          color: '#A50026',
+          key: '500 - 600',
+          name: '500 - 600',
+        },
+      ]);
     });
   });
 }
