6298d14e3156d973b629acce47de045dbf639a94
Jean-Louis Leysens <jloleysens@gmail.com>
2022-10-26 16:07:59 +0200

    update all license headers

--- src/plugins/files/server/usage/register_usage_collector.ts
+++ src/plugins/files/server/usage/register_usage_collector.ts
@@ -1,9 +1,11 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
+
 import type { UsageCollectionSetup } from '@kbn/usage-collection-plugin/server';
 import type { FileServiceStart } from '../file_service';
 import { filesSchema, FileKindUsageSchema } from './schema';
