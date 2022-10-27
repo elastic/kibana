a6a3f6397fe0412daa5d4d1207f0e11112a34944
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-10-27 14:47:58 +0200

    [x-pack/performance] use es archives with sample data, load dashboards w/out map

--- x-pack/performance/journeys/web_logs_dashboard.ts
+++ x-pack/performance/journeys/web_logs_dashboard.ts
@@ -6,51 +6,12 @@
  */
 
 import { Journey } from '@kbn/journeys';
-import { subj } from '@kbn/test-subj-selector';
-
-import { ToastsService } from '../services/toasts';
 import { waitForVisualizations } from '../utils';
 
 export const journey = new Journey({
-  extendContext: ({ page, log }) => ({
-    toasts: new ToastsService(log, page),
-  }),
-})
-  .step('Go to Sample Data Page', async ({ page, kbnUrl }) => {
-    await page.goto(kbnUrl.get(`/app/home#/tutorial_directory/sampleData`));
-
-    await page.waitForSelector(subj('showSampleDataButton'));
-  })
-
-  .step('Open Sample Data pane', async ({ page }) => {
-    // open the "other sample data sets" section
-    await page.click(subj('showSampleDataButton'));
-    // wait for the logs card to be visible
-    await page.waitForSelector(subj('sampleDataSetCardlogs'));
-  })
-
-  .step('Remove Sample Data Logs if installed', async ({ page, log, toasts }) => {
-    if (!(await page.$(subj('removeSampleDataSetlogs')))) {
-      log.info('Logs data does not need to be removed');
-      return;
-    }
-
-    // click the "remove" button
-    await page.click(subj('removeSampleDataSetlogs'));
-    // wait for the toast acknowledging uninstallation
-    await toasts.waitForAndClear('uninstalled');
-  })
-
-  .step('Install Logs Sample Data', async ({ page, toasts }) => {
-    // click the "add data" button
-    await page.click(subj('addSampleDataSetlogs'));
-    // wait for the toast acknowledging installation
-    await toasts.waitForAndClear('installed');
-  })
-
-  .step('Go to Web Logs Dashboard', async ({ page }) => {
-    await page.click(subj('launchSampleDataSetlogs'));
-    await page.click(subj('viewSampleDataSetlogs-dashboard'));
-
-    await waitForVisualizations(page, 11);
-  });
+  esArchives: ['x-pack/performance/es_archives/sample_data_logs'],
+  kbnArchives: ['x-pack/performance/kbn_archives/logs_no_map_dashboard'],
+}).step('Go to Web Logs Dashboard', async ({ page, kbnUrl }) => {
+  await page.goto(kbnUrl.get(`/app/dashboards#/view/edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b`));
+  await waitForVisualizations(page, 11);
+});
