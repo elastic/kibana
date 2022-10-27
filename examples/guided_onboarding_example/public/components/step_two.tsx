96c962dcd038d2eb1410d5dddf74450f3a5b7a95
Yulia Cech <yulia.cech@elastic.co>
2022-10-27 14:07:11 +0200

    [Guided onboarding] Delete unneeded code in examples

--- examples/guided_onboarding_example/public/components/step_two.tsx
+++ examples/guided_onboarding_example/public/components/step_two.tsx
@@ -6,37 +6,17 @@
  * Side Public License, v 1.
  */
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { EuiSpacer, EuiText, EuiTitle, EuiTourStep } from '@elastic/eui';
+import { EuiText, EuiTitle } from '@elastic/eui';
 
-import { GuidedOnboardingPluginStart } from '@kbn/guided-onboarding-plugin/public/types';
 import { FormattedMessage } from '@kbn/i18n-react';
 import {
   EuiPageContentHeader_Deprecated as EuiPageContentHeader,
   EuiPageContentBody_Deprecated as EuiPageContentBody,
 } from '@elastic/eui';
 
-interface StepTwoProps {
-  guidedOnboarding: GuidedOnboardingPluginStart;
-}
-
-export const StepTwo = (props: StepTwoProps) => {
-  const {
-    guidedOnboarding: { guidedOnboardingApi },
-  } = props;
-
-  const [isTourStepOpen, setIsTourStepOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    const subscription = guidedOnboardingApi
-      ?.isGuideStepActive$('testGuide', 'step2')
-      .subscribe((isStepActive) => {
-        setIsTourStepOpen(isStepActive);
-      });
-    return () => subscription?.unsubscribe();
-  }, [guidedOnboardingApi]);
-
+export const StepTwo = () => {
   return (
     <>
       <EuiPageContentHeader>
