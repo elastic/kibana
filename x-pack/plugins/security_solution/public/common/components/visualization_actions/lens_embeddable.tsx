7f19fe97830aa7a0e12f15db924704b7fef3a0ff
Angela Chuang <yi-chun.chuang@elastic.co>
2022-10-27 12:50:04 +0100

    fix up

--- x-pack/plugins/security_solution/public/common/components/visualization_actions/lens_embeddable.tsx
+++ x-pack/plugins/security_solution/public/common/components/visualization_actions/lens_embeddable.tsx
@@ -135,6 +135,9 @@ const LensEmbeddableComponent: React.FC<LensEmbeddableComponentProps> = ({
   const [response, ...additionalResponses] = visData.responses ?? [];
 
   const onLoad = useCallback((isLoading, adapters) => {
+    if (!adapters) {
+      return;
+    }
     const data = adapters?.requests?.getRequests().reduce(
       (acc: { requests: string[]; responses: string[] }, d: Request) => {
         return {
