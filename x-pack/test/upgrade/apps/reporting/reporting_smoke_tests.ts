e2daf4d39f99ba73bcc12598a714683a48e04a61
Lee Drengenberg <lee.drengenberg@elastic.co>
2022-10-27 11:52:06 -0500

    fix type error

--- x-pack/test/upgrade/apps/reporting/reporting_smoke_tests.ts
+++ x-pack/test/upgrade/apps/reporting/reporting_smoke_tests.ts
@@ -6,6 +6,7 @@
  */
 
 import expect from '@kbn/expect';
+import { number } from 'joi';
 import { parse } from 'url';
 import { FtrProviderContext } from '../../ftr_provider_context';
 import { ReportingUsageStats } from '../../services/reporting_upgrade_services';
@@ -121,7 +122,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
               usage = (await usageAPI.getUsageStats()) as UsageStats;
               reportingAPI.expectCompletedReportCount(usage, completedReportCount + 1);
             });
-            log.debug(`Elapsed Time: ${new Date() - startTime}`);
+            log.debug(`Elapsed Time: ${(new Date()).getTime() - startTime.getTime()}`);
           });
         });
       });
