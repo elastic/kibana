41b325e2ddf7fd77d80a9a527ebd6d8e71f8740b
spalger <spencer@elastic.co>
2022-10-28 14:25:51 -0500

    [ts] ts refs cache was removed, remove capture task

--- .buildkite/scripts/steps/on_merge_ts_refs_api_docs.sh
+++ .buildkite/scripts/steps/on_merge_ts_refs_api_docs.sh
@@ -3,10 +3,7 @@
 set -euo pipefail
 
 export BAZEL_CACHE_MODE=buildbuddy # Populate Buildbuddy bazel remote cache for linux
-export BUILD_TS_REFS_CACHE_ENABLE=true
-export BUILD_TS_REFS_CACHE_CAPTURE=true
 export DISABLE_BOOTSTRAP_VALIDATION=true
-export BUILD_TS_REFS_DISABLE=false
 
 .buildkite/scripts/bootstrap.sh
 
