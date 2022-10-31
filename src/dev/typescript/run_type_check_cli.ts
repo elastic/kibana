4a51b91104f0c7c7a402793189c71027cfa96eb7
Tiago Costa <tiago.costa@elastic.co>
2022-10-31 23:41:38 +0000

    chore(NA): removes warning about no progress when building types for typechecking

--- src/dev/typescript/run_type_check_cli.ts
+++ src/dev/typescript/run_type_check_cli.ts
@@ -142,9 +142,6 @@ function createTypeCheckConfigs(projects: Project[], bazelPackages: BazelPackage
 export async function runTypeCheckCli() {
   run(
     async ({ log, flagsReader, procRunner }) => {
-      log.warning(
-        `Building types for all bazel packages. This can take a while depending on your changes and won't show any progress while it runs.`
-      );
       await runBazel(['build', '//packages:build_types', '--show_result=1'], {
         cwd: REPO_ROOT,
         logPrefix: '\x1b[94m[bazel]\x1b[39m',
