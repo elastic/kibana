575e908862619b4d436f04c172aeb92efed0696b
Ying Mao <ying.mao@elastic.co>
2022-10-31 15:44:49 -0400

    Updating other rules

--- x-pack/test/alerting_api_integration/common/fixtures/plugins/alerts/server/alert_types.ts
+++ x-pack/test/alerting_api_integration/common/fixtures/plugins/alerts/server/alert_types.ts
@@ -95,21 +95,9 @@ function getAlwaysFiringAlertType() {
 }
 
 async function alwaysFiringExecutor(alertExecutorOptions: any) {
-  const {
-    services,
-    params,
-    state,
-    alertId,
-    spaceId,
-    namespace,
-    name,
-    tags,
-    createdBy,
-    updatedBy,
-    rule,
-  } = alertExecutorOptions;
+  const { services, params, state, spaceId, namespace, rule } = alertExecutorOptions;
   let group: string | null = 'default';
-  const alertInfo = { alertId, spaceId, namespace, name, tags, createdBy, updatedBy, ...rule };
+  const alertInfo = { alertId: rule.id, spaceId, namespace, ...rule };
 
   if (params.groupsToScheduleActionsInSeries) {
     const index = state.groupInSeriesIndex || 0;
