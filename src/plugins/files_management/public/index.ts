b6974bab94bc21c6be5104b424c8c45083cfd77d
Jean-Louis Leysens <jloleysens@gmail.com>
2022-10-31 15:41:07 +0100

    remove bad scss import

--- src/plugins/files_management/public/index.ts
+++ src/plugins/files_management/public/index.ts
@@ -6,13 +6,8 @@
  * Side Public License, v 1.
  */
 
-import './index.scss';
-
 import { FilesManagementPlugin } from './plugin';
 
-// This exports static code and TypeScript types,
-// as well as, Kibana Platform `plugin()` initializer.
 export function plugin() {
   return new FilesManagementPlugin();
 }
-export { FilesManagementPluginSetup, FilesManagementPluginStart } from './types';
