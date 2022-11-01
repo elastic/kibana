889763564147917e23f9a590204eaa4056ec32cc
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2022-11-01 11:46:23 +0000

    Update new_terms.ts

--- x-pack/test/detection_engine_api_integration/security_and_spaces/rule_execution_logic/new_terms.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/rule_execution_logic/new_terms.ts
@@ -295,7 +295,6 @@ export default ({ getService }: FtrProviderContext) => {
     });
 
     it('should generate 5 alerts, 1 for each new unique combination in 2 fields', async () => {
-      // ensure there are no alerts for single new terms fields, it means values are not new
       const rule: NewTermsRuleCreateProps = {
         ...getCreateNewTermsRulesSchemaMock('rule-1', true),
         index: ['new_terms'],
