209037ad057d2cdb415381065180062d04ca73fb
shahzad31 <shahzad.muhammad@elastic.co>
2022-11-01 18:47:34 +0100

    add object weight breakdown

---
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/step_details_page/components/color_palette.tsx
@@ -0,0 +1,66 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
+import { useTheme } from '@kbn/observability-plugin/public';
+import { colourPalette } from './network_waterfall/step_detail/waterfall/data_formatting';
+export const ColorPalette = ({
+  label,
+  mimeType,
+  percent,
+  value,
+}: {
+  label: string;
+  mimeType: string;
+  percent: number;
+  value: string;
+}) => {
+  return (
+    <EuiFlexGroup gutterSize="s">
+      <EuiFlexItem grow={false} style={{ width: 30 }}>
+        <EuiText size="xs">{label}</EuiText>
+      </EuiFlexItem>
+      <EuiFlexItem grow={true}>
+        <ColorPaletteFlexItem mimeType={mimeType} percent={isNaN(percent) ? 0 : percent} />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false} style={{ width: 45 }}>
+        <EuiText size="xs">{value}</EuiText>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
+
+export const ColorPaletteFlexItem = ({
+  mimeType,
+  percent,
+}: {
+  mimeType: string;
+  percent: number;
+}) => {
+  const { eui } = useTheme();
+
+  return (
+    <EuiFlexGroup
+      gutterSize="none"
+      style={{
+        borderRadius: 8,
+        overflow: 'hidden',
+      }}
+    >
+      <EuiFlexItem grow={true} style={{ backgroundColor: eui.euiColorLightShade }}>
+        <span
+          style={{
+            backgroundColor: (colourPalette as Record<string, string>)[mimeType],
+            height: 16,
+            width: `${percent}%`,
+          }}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
