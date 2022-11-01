6d86b4fd522103076ae8f97f94450b8204a987f8
Jonathan Budzenski <jon@elastic.co>
2022-11-01 17:23:24 -0500

    Remove buildbuddy cache (#144356)
    * Remove buildbuddy cache
    
    * Update .buildkite/scripts/steps/on_merge_ts_refs_api_docs.sh
    
    * Update .buildkite/scripts/common/setup_bazel.sh

--- .buildkite/scripts/steps/on_merge_ts_refs_api_docs.sh
+++ .buildkite/scripts/steps/on_merge_ts_refs_api_docs.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-export BAZEL_CACHE_MODE=buildbuddy # Populate Buildbuddy bazel remote cache for linux
 export DISABLE_BOOTSTRAP_VALIDATION=true
 
 .buildkite/scripts/bootstrap.sh
