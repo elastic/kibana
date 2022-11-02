41b92b220fcc172a0525a58ac1e59a2370654460
Paul Tavares <paul.tavares@elastic.co>
2022-11-02 15:06:51 -0400

    add test for validating ttl calculation

--- x-pack/plugins/security_solution/server/endpoint/services/actions/action_files.test.ts
+++ x-pack/plugins/security_solution/server/endpoint/services/actions/action_files.test.ts
@@ -9,7 +9,7 @@ import type { ElasticsearchClientMock } from '@kbn/core/server/mocks';
 import { elasticsearchServiceMock, loggingSystemMock } from '@kbn/core/server/mocks';
 import type { Logger } from '@kbn/core/server';
 import { createEsFileClient as _createEsFileClient } from '@kbn/files-plugin/server';
-import { createFileClientMock } from '@kbn/files-plugin/server/mocks';
+import { createFileClientMock, createFileMock } from '@kbn/files-plugin/server/mocks';
 import { getFileDownloadStream, getFileInfo } from './action_files';
 import type { DiagnosticResult } from '@elastic/elasticsearch';
 import { errors } from '@elastic/elasticsearch';
@@ -17,6 +17,7 @@ import { NotFoundError } from '../../errors';
 import { FILE_STORAGE_DATA_INDEX } from '../../../../common/endpoint/constants';
 import { BaseDataGenerator } from '../../../../common/endpoint/data_generators/base_data_generator';
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
+import type { IlmGetLifecycleLifecycle } from '@elastic/elasticsearch/lib/api/types';
 
 jest.mock('@kbn/files-plugin/server');
 const createEsFileClient = _createEsFileClient as jest.Mock;
@@ -57,6 +58,7 @@ describe('Action Files service', () => {
 
   describe('#getFileInfo()', () => {
     let fileChunksEsResponseMock: estypes.SearchResponse;
+    let ilmPolicyMock: IlmGetLifecycleLifecycle;
 
     beforeEach(() => {
       fileChunksEsResponseMock = BaseDataGenerator.toEsSearchResponse([
@@ -70,6 +72,42 @@ describe('Action Files service', () => {
 
         return BaseDataGenerator.toEsSearchResponse([]);
       });
+
+      ilmPolicyMock = {
+        version: 1,
+        modified_date: '2022-11-01T19:56:10.124Z',
+        policy: {
+          phases: {
+            hot: {
+              min_age: '0ms',
+              actions: {
+                set_priority: {
+                  priority: 100,
+                },
+                rollover: {
+                  max_primary_shard_size: '50gb',
+                  max_age: '30d',
+                },
+              },
+            },
+            delete: {
+              min_age: '5d',
+              actions: {
+                delete: {
+                  delete_searchable_snapshot: true,
+                },
+              },
+            },
+          },
+        },
+      };
+
+      esClientMock.ilm.getLifecycle.mockImplementation(async () => {
+        return {
+          // FIXME:PT get policy name from fleet when available
+          paul: ilmPolicyMock,
+        };
+      });
     });
 
     it('should return expected output', async () => {
@@ -80,6 +118,7 @@ describe('Action Files service', () => {
         name: 'test.txt',
         size: 1234,
         status: 'READY',
+        ttl: -1,
       });
     });
 
@@ -93,6 +132,7 @@ describe('Action Files service', () => {
         name: 'test.txt',
         size: 1234,
         status: 'DELETED',
+        ttl: -1,
       });
 
       expect(loggerMock.debug).toHaveBeenCalledWith(
@@ -110,6 +150,26 @@ describe('Action Files service', () => {
       await expect(getFileInfo(esClientMock, loggerMock, '123')).rejects.toBeInstanceOf(
         NotFoundError
       );
+    });
+
+    it('it should calculate TTL based on ILM policy', async () => {
+      const file = createFileMock();
+      const createdDate = new Date();
+
+      createdDate.setHours(1, 0, 0, 0);
+
+      // When Moment is called with no date string, `Date.now` is used, so we mock this here
+      // in order to ensure we get consistent test results
+      const dateNowSpy = jest.spyOn(Date, 'now').mockReturnValue(createdDate.getTime());
+
+      file.data.created = createdDate.toISOString();
+      fileClientMock.get.mockResolvedValue(file);
+
+      await expect(getFileInfo(esClientMock, loggerMock, '123')).resolves.toEqual(
+        expect.objectContaining({ ttl: 5 })
+      );
+
+      dateNowSpy.mockRestore();
     });
   });
 });
