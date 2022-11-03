8d92010316bdb2c6e5fe128b71c0af5e0599bc38
Dominique Clarke <dominique.clarke@elastic.co>
2022-11-03 13:25:06 -0400

    adjust no monitors found styles

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/common/no_monitors_found.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/common/no_monitors_found.tsx
@@ -6,7 +6,7 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import { EuiEmptyPrompt, EuiButtonEmpty, EuiText } from '@elastic/eui';
+import { EuiEmptyPrompt, EuiLink, EuiText } from '@elastic/eui';
 import React from 'react';
 import { useUrlParams } from '../../../hooks';
 
@@ -14,13 +14,12 @@ export function NoMonitorsFound() {
   return (
     <EuiEmptyPrompt
       iconType="search"
-      iconColor="default"
+      iconColor="subdued"
       title={<span>{NO_MONITORS_FOUND_HEADING}</span>}
       titleSize="s"
       body={
         <EuiText size="s">
-          {NO_MONITORS_FOUND_CONTENT}
-          <ClearFilters />
+          {NO_MONITORS_FOUND_CONTENT} <ClearFilters />
         </EuiText>
       }
     />
@@ -29,9 +28,7 @@ export function NoMonitorsFound() {
 
 export function ClearFilters() {
   const [_, updateUrlParams] = useUrlParams();
-  return (
-    <EuiButtonEmpty onClick={() => updateUrlParams(null)}>{CLEAR_FILTERS_LABEL}</EuiButtonEmpty>
-  );
+  return <EuiLink onClick={() => updateUrlParams(null)}>{CLEAR_FILTERS_LABEL}</EuiLink>;
 }
 
 const NO_MONITORS_FOUND_HEADING = i18n.translate(
