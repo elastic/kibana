3bae9cae5e2f70ceebd28bdcf0a0eefe7b1494be
Timothy Sullivan <tsullivan@elastic.co>
2022-11-03 15:59:41 -0700

    test2

--- x-pack/test/reporting_api_integration/reporting_and_security/index.ts
+++ x-pack/test/reporting_api_integration/reporting_and_security/index.ts
@@ -19,13 +19,13 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
       await reportingAPI.createTestReportingUser();
     });
 
-    // loadTestFile(require.resolve('./bwc_existing_indexes'));
+    loadTestFile(require.resolve('./bwc_existing_indexes'));
     loadTestFile(require.resolve('./security_roles_privileges'));
-    // loadTestFile(require.resolve('./download_csv_dashboard'));
-    // loadTestFile(require.resolve('./generate_csv_discover'));
-    // loadTestFile(require.resolve('./network_policy'));
-    // loadTestFile(require.resolve('./spaces'));
-    // loadTestFile(require.resolve('./usage'));
+    loadTestFile(require.resolve('./download_csv_dashboard'));
+    loadTestFile(require.resolve('./generate_csv_discover'));
+    loadTestFile(require.resolve('./network_policy'));
+    loadTestFile(require.resolve('./spaces'));
+    loadTestFile(require.resolve('./usage'));
     loadTestFile(require.resolve('./error_codes'));
     loadTestFile(require.resolve('./validation'));
     // loadTestFile(require.resolve('./ilm_migration_apis')); // FIXME is this the cause of flaky?
