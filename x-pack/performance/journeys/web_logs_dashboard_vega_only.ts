da279a961808b3ab6354d75e032dda7ada5099b6
lizozom <liza.katz@elastic.co>
2022-11-03 18:57:28 +0200

    log

--- x-pack/performance/journeys/web_logs_dashboard_vega_only.ts
+++ x-pack/performance/journeys/web_logs_dashboard_vega_only.ts
@@ -19,7 +19,7 @@ export const journey = new Journey({
     await page.waitForSelector('#dashboardListingHeading');
   })
 
-  .step('Go to Web Logs Dashboard', async ({ page }) => {
+  .step('Go to Web Logs Dashboard', async ({ page, log }) => {
     await page.click(subj('dashboardListingTitleLink-vega-only-dashboard'));
-    await waitForVisualizations(page, 1);
+    await waitForVisualizations(page, log, 1);
   });
