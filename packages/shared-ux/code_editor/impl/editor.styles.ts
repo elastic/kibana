5c4bf20f228ec332ecf4d3b766f055741d9aff24
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-11-04 17:26:33 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- packages/shared-ux/code_editor/impl/editor.styles.ts
+++ packages/shared-ux/code_editor/impl/editor.styles.ts
@@ -1,54 +1,73 @@
-import { useEuiTheme } from "@elastic/eui";
-import { css } from "@emotion/react";
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { useEuiTheme } from '@elastic/eui';
+import { css } from '@emotion/react';
 
 const { euiTheme } = useEuiTheme();
 
-export const CodeEditorMonacoStyles = () => css`{
-        animation: none !important; // Removes textarea EUI blue underline animation from EUI
-}`;
-
-export const CodeEditorStyles = () => css`{
-  position: relative;
-  height: 100%;
-}`;
-
-export const CodeEditorControlsStyles = () => css`{
-  top: ${euiTheme.size.xs};
-  right: ${euiTheme.base};
-  position: absolute;
-  z-index: 1000
-}
+export const CodeEditorMonacoStyles = () => css`
+   {
+    animation: none !important; // Removes textarea EUI blue underline animation from EUI
+  }
 `;
-  
-export const CodeEditorFullScreenStyles = () => css`{
-  position: absolute;
-  left: 0;
-  top: 0;
-  .kibanaCodeEditor__controls {
-    top: ${euiTheme.size.l};
-    right: ${euiTheme.size.l};
+
+export const CodeEditorStyles = () => css`
+   {
+    position: relative;
+    height: 100%;
   }
-}`
+`;
 
-export const CodeEditorPlaceholderStyles = () => css`{
-  color: ${euiTheme.colors.subduedText};
-  width: max-content;
-  pointer-events: none;
-}`
-
-export const CodeEditorKeyboardHintStyles = () => css`{
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  left: 0;
-
-  &:focus {
-    // $euiZLevel1
-    z-index: ${euiTheme.levels.mask};
+export const CodeEditorControlsStyles = () => css`
+   {
+    top: ${euiTheme.size.xs};
+    right: ${euiTheme.base};
+    position: absolute;
+    z-index: 1000;
   }
+`;
 
-  &--isInactive {
-    display: none;
+export const CodeEditorFullScreenStyles = () => css`
+   {
+    position: absolute;
+    left: 0;
+    top: 0;
+    .kibanaCodeEditor__controls {
+      top: ${euiTheme.size.l};
+      right: ${euiTheme.size.l};
+    }
   }
-}`
+`;
+
+export const CodeEditorPlaceholderStyles = () => css`
+   {
+    color: ${euiTheme.colors.subduedText};
+    width: max-content;
+    pointer-events: none;
+  }
+`;
+
+export const CodeEditorKeyboardHintStyles = () => css`
+   {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    &:focus {
+      // $euiZLevel1
+      z-index: ${euiTheme.levels.mask};
+    }
+
+    &--isInactive {
+      display: none;
+    }
+  }
+`;
