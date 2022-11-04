b1d0a73e58007e3670e2025b81844dc3fee54f0f
Joe Reuter <johannes.reuter@elastic.co>
2022-11-04 12:41:07 +0100

    fix tests

--- src/plugins/data/common/search/aggs/buckets/terms.ts
+++ src/plugins/data/common/search/aggs/buckets/terms.ts
@@ -78,7 +78,12 @@ export const getTermsBucketAgg = () =>
       return {
         id: 'terms',
         params: {
-          ...format,
+          id: format.id,
+          ...(format.params
+            ? {
+                params: format.params,
+              }
+            : {}),
           otherBucketLabel: agg.params.otherBucketLabel,
           missingBucketLabel: agg.params.missingBucketLabel,
         },
