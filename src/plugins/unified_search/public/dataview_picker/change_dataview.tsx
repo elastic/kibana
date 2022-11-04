2b501821ff83794769824a1dd1fb94fb5e1c6de5
Bree Hall <briannajdhall@gmail.com>
2022-11-04 10:44:14 -0400

    EuiButton - added textProps to EuiButton to prevent very long button names from spilling over outside of the container

--- src/plugins/unified_search/public/dataview_picker/change_dataview.tsx
+++ src/plugins/unified_search/public/dataview_picker/change_dataview.tsx
@@ -171,6 +171,7 @@ export function ChangeDataView({
         title={triggerLabel}
         fullWidth={fullWidth}
         disabled={isDisabled}
+        textProps={{ className: 'className=eui-textTruncate' }}
         {...rest}
       >
         <>
