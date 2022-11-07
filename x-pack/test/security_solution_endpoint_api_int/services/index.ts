381c3c0fe090ab93f7805749010550e1b15adb0e
David Sanchez Soler <david.sanchezsoler@elastic.co>
2022-11-07 15:58:32 +0100

    Adds ftr service for roles and users create/delete using endpoint security rbac privileges

--- x-pack/test/security_solution_endpoint_api_int/services/index.ts
+++ x-pack/test/security_solution_endpoint_api_int/services/index.ts
@@ -7,6 +7,7 @@
 
 import { services as xPackAPIServices } from '../../api_integration/services';
 import { ResolverGeneratorProvider } from './resolver';
+import { RolesUsersProvider } from './roles_users';
 import { EndpointTestResources } from '../../security_solution_endpoint/services/endpoint';
 import { EndpointPolicyTestResourcesProvider } from '../../security_solution_endpoint/services/endpoint_policy';
 import { EndpointArtifactsTestResources } from '../../security_solution_endpoint/services/endpoint_artifacts';
@@ -17,4 +18,5 @@ export const services = {
   endpointTestResources: EndpointTestResources,
   endpointPolicyTestResources: EndpointPolicyTestResourcesProvider,
   endpointArtifactTestResources: EndpointArtifactsTestResources,
+  rolesUsersProvider: RolesUsersProvider,
 };
---
+++ x-pack/test/security_solution_endpoint_api_int/services/soc_manager.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { Role } from '@kbn/security-plugin/common';
+
+export const socManager: Omit<Role, 'name'> = {
+  elasticsearch: {
+    cluster: ['manage'],
+    indices: [
+      {
+        names: [
+          '.alerts-security.alerts-default',
+          '.alerts-security.alerts-*',
+          '.siem-signals-*',
+          '.items-*',
+          '.lists-*',
+        ],
+        privileges: ['manage', 'write', 'read', 'view_index_metadata'],
+      },
+    ],
+    run_as: [],
+  },
+  kibana: [
+    {
+      base: [],
+      feature: {
+        actions: ['all'],
+        advancedSettings: ['all'],
+        dev_tools: ['all'],
+        fleet: ['all'],
+        generalCases: ['all'],
+        indexPatterns: ['all'],
+        osquery: ['all'],
+        savedObjectsManagement: ['all'],
+        savedObjectsTagging: ['all'],
+        siem: [
+          'minimal_all',
+          'actions_log_management_all',
+          'host_isolation_all',
+          'process_operations_all',
+        ],
+        stackAlerts: ['all'],
+      },
+      spaces: ['*'],
+    },
+  ],
+};
