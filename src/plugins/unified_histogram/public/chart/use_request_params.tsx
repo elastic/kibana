2848897b65d1c6867a76ac73b14ebe4bda776df4
Davis McPhee <davis.mcphee@elastic.co>
2022-11-07 17:08:23 -0400

    [Discover] Update queryStateContainer to be a ref

--- src/plugins/unified_histogram/public/chart/use_request_params.tsx
+++ src/plugins/unified_histogram/public/chart/use_request_params.tsx
@@ -8,7 +8,7 @@
 
 import { connectToQueryState, QueryState } from '@kbn/data-plugin/public';
 import { createStateContainer, useContainerState } from '@kbn/kibana-utils-plugin/public';
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import type { UnifiedHistogramRequestContext, UnifiedHistogramServices } from '../types';
 
 export const useRequestParams = ({
@@ -22,14 +22,14 @@ export const useRequestParams = ({
 }) => {
   const { data } = services;
 
-  const queryStateContainer = useMemo(() => {
-    return createStateContainer<QueryState>({
+  const queryStateContainer = useRef(
+    createStateContainer<QueryState>({
       filters: data.query.filterManager.getFilters(),
       query: data.query.queryString.getQuery(),
       refreshInterval: data.query.timefilter.timefilter.getRefreshInterval(),
       time: data.query.timefilter.timefilter.getTime(),
-    });
-  }, [data.query.filterManager, data.query.queryString, data.query.timefilter.timefilter]);
+    })
+  ).current;
 
   const queryState = useContainerState(queryStateContainer);
 
