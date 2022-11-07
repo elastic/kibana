6c06ed3d8ba7727f32806d0c89d7bbf0d4987a9d
nreese <reese.nathan@elastic.co>
2022-11-07 10:50:44 -0700

    fix label form

--- x-pack/plugins/maps/public/classes/styles/vector/components/label/vector_style_label_editor.tsx
+++ x-pack/plugins/maps/public/classes/styles/vector/components/label/vector_style_label_editor.tsx
@@ -11,14 +11,16 @@ import { Props, StylePropEditor } from '../style_prop_editor';
 import { DynamicLabelForm } from './dynamic_label_form';
 import { StaticLabelForm } from './static_label_form';
 import { LabelDynamicOptions, LabelStaticOptions } from '../../../../../../common/descriptor_types';
+import { DynamicTextProperty } from '../../properties/dynamic_text_property';
+import { StaticTextProperty } from '../../properties/static_text_property';
 
 type LabelEditorProps = Omit<Props<LabelStaticOptions, LabelDynamicOptions>, 'children'>;
 
 export function VectorStyleLabelEditor(props: LabelEditorProps) {
   const labelForm = props.styleProperty.isDynamic() ? (
-    <DynamicLabelForm {...props} />
+    <DynamicLabelForm {...props} styleProperty={props.styleProperty as DynamicTextProperty} />
   ) : (
-    <StaticLabelForm {...props} />
+    <StaticLabelForm {...props} styleProperty={props.styleProperty as StaticTextProperty} />
   );
 
   return <StylePropEditor {...props}>{labelForm}</StylePropEditor>;
