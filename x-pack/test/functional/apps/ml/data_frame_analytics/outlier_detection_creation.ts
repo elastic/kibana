fdf014061773294980222af2b827fb7a7bade95d
Melissa <melissa.alvarez@elastic.co>
2022-11-07 17:38:40 -0500

    update percentage for color in functional test followup

--- x-pack/test/functional/apps/ml/data_frame_analytics/outlier_detection_creation.ts
+++ x-pack/test/functional/apps/ml/data_frame_analytics/outlier_detection_creation.ts
@@ -72,7 +72,7 @@ export default function ({ getService }: FtrProviderContext) {
             // regular markers
             { color: '#6496BE', percentage: 15 },
             // tick/grid/axis
-            { color: '#6E6E82', percentage: 28 },
+            { color: '#6E6E82', percentage: 11 },
             { color: '#D2DCE6', percentage: 10 },
             // anti-aliasing
             { color: '#F5F7FA', percentage: 35 },
