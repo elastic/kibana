e4c6ad17f39137a5497393f8f774ef12304103a3
Dominique Clarke <dominique.clarke@elastic.co>
2022-11-07 14:11:31 -0500

    adjust test

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/common/no_monitors_found.test.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitors_page/common/no_monitors_found.test.tsx
@@ -8,17 +8,14 @@ import React from 'react';
 import * as URL from '../../../hooks/use_url_params';
 import { fireEvent, waitFor } from '@testing-library/react';
 import { render } from '../../../utils/testing/rtl_helpers';
-import { SyntheticsUrlParams } from '../../../utils/url_params/get_supported_url_params';
 import { NoMonitorsFound } from './no_monitors_found';
 
 describe('NoMonitorsFound', () => {
   let useUrlParamsSpy: jest.SpyInstance<[URL.GetUrlParams, URL.UpdateUrlParams]>;
-  let useGetUrlParamsSpy: jest.SpyInstance<SyntheticsUrlParams>;
   let updateUrlParamsMock: jest.Mock;
 
   beforeEach(() => {
     useUrlParamsSpy = jest.spyOn(URL, 'useUrlParams');
-    useGetUrlParamsSpy = jest.spyOn(URL, 'useGetUrlParams');
     updateUrlParamsMock = jest.fn();
 
     useUrlParamsSpy.mockImplementation(() => [jest.fn(), updateUrlParamsMock]);
