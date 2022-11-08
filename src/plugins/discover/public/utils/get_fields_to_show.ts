757b46963a94ba832c2b440f39b4fae9908fe2de
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2022-11-08 21:01:39 +0100

    Improve code

--- src/plugins/discover/public/utils/get_fields_to_show.ts
+++ src/plugins/discover/public/utils/get_fields_to_show.ts
@@ -8,31 +8,38 @@
 import { getFieldSubtypeMulti } from '@kbn/data-views-plugin/public';
 import type { DataView } from '@kbn/data-views-plugin/public';
 
+/**
+ * Returns am array of fields to display in the Documents column of the data table
+ * If showMultiFields is set to false, it filters out multifields that have a parent, to prevent entries for multifields
+ * like this: field, field.keyword, field.whatever
+ * @param fields
+ * @param dataView
+ * @param showMultiFields
+ */
 export const getFieldsToShow = (fields: string[], dataView: DataView, showMultiFields: boolean) => {
   if (showMultiFields) {
     return fields;
   }
-  const fieldMap = {} as Record<string, boolean>;
-  const childParentFieldsMap = {} as Record<string, string>;
-  const parentFieldMap = {} as Record<string, boolean>;
-  const mapping = (name: string) => dataView.fields.getByName(name);
+  const fieldSet = new Set();
+  const childParentFieldsMap = new Map();
+  const parentFieldSet = new Set();
   fields.forEach((key) => {
-    const mapped = mapping(key);
+    const mapped = dataView.fields.getByName(key);
     const subTypeMulti = mapped && getFieldSubtypeMulti(mapped.spec);
     const isMultiField = Boolean(subTypeMulti?.multi);
     if (mapped && subTypeMulti?.multi?.parent) {
-      childParentFieldsMap[key] = subTypeMulti.multi.parent;
+      childParentFieldsMap.set(key, subTypeMulti.multi.parent);
     }
     if (mapped && isMultiField) {
-      parentFieldMap[key] = true;
+      parentFieldSet.add(key);
     }
-    fieldMap[key] = true;
+    fieldSet.add(key);
   });
   return fields.filter((key: string) => {
-    if (!parentFieldMap[key]) {
+    if (!parentFieldSet.has(key)) {
       return true;
     }
-    const parent = childParentFieldsMap[key];
-    return parent && !fieldMap[parent];
+    const parent = childParentFieldsMap.get(key);
+    return parent && !fieldSet.has(parent);
   });
 };
