2cf01cb6ea7fc67a16e34b76eef472d8f52d0704
Tiago Costa <tiago.costa@elastic.co>
2022-11-08 20:37:19 +0000

    chore(NA): set everything as module in typescript by default starting in v4.7.x

--- src/plugins/console/public/application/containers/editor/legacy/console_editor/editor.test.mock.tsx
+++ src/plugins/console/public/application/containers/editor/legacy/console_editor/editor.test.mock.tsx
@@ -6,7 +6,10 @@
  * Side Public License, v 1.
  */
 
-// @ts-ignore
+// TODO(jbudz): should be removed when upgrading to TS@4.8
+// this is a skip for the errors created when typechecking with isolatedModules
+// export {};
+
 jest.mock('../../../../contexts/editor_context/editor_registry', () => ({
   instance: {
     setInputEditor: () => {},
