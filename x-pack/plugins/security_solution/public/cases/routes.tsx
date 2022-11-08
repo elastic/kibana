0a26e9daa9948a0c3c0d2d30aad59db2548724d6
Steph Milovic <stephanie.milovic@elastic.co>
2022-11-08 09:37:00 -0700

    add tests

--- x-pack/plugins/security_solution/public/cases/routes.tsx
+++ x-pack/plugins/security_solution/public/cases/routes.tsx
@@ -5,31 +5,15 @@
  * 2.0.
  */
 
-import React, { useEffect, useMemo } from 'react';
+import React from 'react';
 
 import { TrackApplicationView } from '@kbn/usage-collection-plugin/public';
-import {
-  AlertsCasesTourSteps,
-  SecurityStepId,
-} from '../common/components/guided_onboarding_tour/tour_config';
 import type { SecuritySubPluginRoutes } from '../app/types';
 import { CASES_PATH } from '../../common/constants';
 import { Cases } from './pages';
 import { PluginTemplateWrapper } from '../common/components/plugin_template_wrapper';
-import { useTourContext } from '../common/components/guided_onboarding_tour';
 
 export const CasesRoutes = () => {
-  const { activeStep, endTourStep, isTourShown } = useTourContext();
-
-  const isTourActive = useMemo(
-    () => activeStep === AlertsCasesTourSteps.viewCase && isTourShown(SecurityStepId.alertsCases),
-    [activeStep, isTourShown]
-  );
-
-  useEffect(() => {
-    if (isTourActive) endTourStep(SecurityStepId.alertsCases);
-  }, [endTourStep, isTourActive]);
-
   return (
     <PluginTemplateWrapper>
       <TrackApplicationView viewId="case">
