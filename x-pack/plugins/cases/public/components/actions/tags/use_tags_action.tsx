9228ae0e8e0ee20b74af730837af16519b761c5a
Christos Nasikas <christos.nasikas@elastic.co>
2022-11-08 20:03:00 +0200

    [Cases] Tags bulk & row actions (#143450)
    ## Summary
    
    This PR adds the ability to bulk edit tags from the cases table. It also
    adds the tag row action to edit tags on an individual case.
    
    ## User flow
    
    https://user-images.githubusercontent.com/7871006/199962211-cb57f34d-1319-4008-b204-7adee623c9e4.mov
    
    ## Screenshots
    <img width="1922" alt="Screenshot 2022-11-04 at 1 29 15 PM"
    src="https://user-images.githubusercontent.com/7871006/199962454-663d163b-f1a6-4fd5-a5df-d2e5b1704f4f.png">
    <img width="479" alt="Screenshot 2022-11-04 at 1 30 01 PM"
    src="https://user-images.githubusercontent.com/7871006/199962445-b1194e1b-efff-4fdd-8e50-1fa4bef7c960.png">
    <img width="482" alt="Screenshot 2022-11-04 at 1 29 29 PM"
    src="https://user-images.githubusercontent.com/7871006/199962452-5664a4fd-459d-422c-b08c-c9f00ea88190.png">
    
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    ### For maintainers
    
    - [x] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ## Release notes
    
    Add the ability to bulk edit tags in the cases table

---
+++ x-pack/plugins/cases/public/components/actions/tags/use_tags_action.tsx
@@ -0,0 +1,76 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiIcon } from '@elastic/eui';
+import React, { useCallback, useState } from 'react';
+import { difference } from 'lodash';
+import { useUpdateCases } from '../../../containers/use_bulk_update_case';
+import type { Case } from '../../../../common';
+import { useCasesContext } from '../../cases_context/use_cases_context';
+import type { UseActionProps } from '../types';
+import * as i18n from './translations';
+import type { TagsSelectionState } from './types';
+
+export const useTagsAction = ({ onAction, onActionSuccess, isDisabled }: UseActionProps) => {
+  const { mutate: updateCases } = useUpdateCases();
+  const { permissions } = useCasesContext();
+  const [isFlyoutOpen, setIsFlyoutOpen] = useState<boolean>(false);
+  const [selectedCasesToEditTags, setSelectedCasesToEditTags] = useState<Case[]>([]);
+  const canUpdateStatus = permissions.update;
+  const isActionDisabled = isDisabled || !canUpdateStatus;
+
+  const onFlyoutClosed = useCallback(() => setIsFlyoutOpen(false), []);
+  const openFlyout = useCallback(
+    (selectedCases: Case[]) => {
+      onAction();
+      setIsFlyoutOpen(true);
+      setSelectedCasesToEditTags(selectedCases);
+    },
+    [onAction]
+  );
+
+  const onSaveTags = useCallback(
+    (tagsSelection: TagsSelectionState) => {
+      onAction();
+      onFlyoutClosed();
+      const casesToUpdate = selectedCasesToEditTags.map((theCase) => {
+        const tags = difference(theCase.tags, tagsSelection.unSelectedTags);
+        const uniqueTags = new Set([...tags, ...tagsSelection.selectedTags]);
+
+        return {
+          tags: Array.from(uniqueTags.values()),
+          id: theCase.id,
+          version: theCase.version,
+        };
+      });
+
+      updateCases(
+        {
+          cases: casesToUpdate,
+          successToasterTitle: i18n.EDITED_TAGS(selectedCasesToEditTags.length),
+        },
+        { onSuccess: onActionSuccess }
+      );
+    },
+    [onAction, onActionSuccess, onFlyoutClosed, selectedCasesToEditTags, updateCases]
+  );
+
+  const getAction = (selectedCases: Case[]) => {
+    return {
+      name: i18n.EDIT_TAGS,
+      onClick: () => openFlyout(selectedCases),
+      disabled: isActionDisabled,
+      'data-test-subj': 'cases-bulk-action-tags',
+      icon: <EuiIcon type="tag" size="m" />,
+      key: 'cases-bulk-action-tags',
+    };
+  };
+
+  return { getAction, isFlyoutOpen, onFlyoutClosed, onSaveTags };
+};
+
+export type UseTagsAction = ReturnType<typeof useTagsAction>;
