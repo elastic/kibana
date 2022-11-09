506ef794bfae22ec142e7ee3869758e77d7a7a16
semd <sergi.massaneda@elastic.co>
2022-11-09 19:15:16 +0100

    use SimpleConnectorForm

--- x-pack/plugins/stack_connectors/public/connector_types/security/tines/tines_connector.tsx
+++ x-pack/plugins/stack_connectors/public/connector_types/security/tines/tines_connector.tsx
@@ -7,64 +7,44 @@
 
 import React from 'react';
 
-import { EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiTitle } from '@elastic/eui';
-import { UseField } from '@kbn/es-ui-shared-plugin/static/forms/hook_form_lib';
-import { Field } from '@kbn/es-ui-shared-plugin/static/forms/components';
-import { fieldValidators } from '@kbn/es-ui-shared-plugin/static/forms/helpers';
-import { ActionConnectorFieldsProps, PasswordField } from '@kbn/triggers-actions-ui-plugin/public';
+import {
+  ActionConnectorFieldsProps,
+  ConfigFieldSchema,
+  SecretsFieldSchema,
+  SimpleConnectorForm,
+} from '@kbn/triggers-actions-ui-plugin/public';
 import * as i18n from './translations';
 
-const { emptyField, urlField } = fieldValidators;
+const configFormSchema: ConfigFieldSchema[] = [
+  {
+    id: 'url',
+    label: i18n.URL_LABEL,
+    isUrlField: true,
+  },
+];
+
+const secretsFormSchema: SecretsFieldSchema[] = [
+  {
+    id: 'email',
+    label: i18n.EMAIL_LABEL,
+  },
+  {
+    id: 'token',
+    label: i18n.TOKEN_LABEL,
+    isPasswordField: true,
+  },
+];
 
 const TinesActionConnectorFields: React.FunctionComponent<ActionConnectorFieldsProps> = ({
   readOnly,
+  isEdit,
 }) => (
-  <EuiFlexGroup direction="column" justifyContent="spaceBetween">
-    <EuiFlexItem>
-      <EuiSpacer size="m" />
-      <UseField
-        path="config.url"
-        config={{
-          label: i18n.URL_LABEL,
-          validations: [{ validator: urlField(i18n.URL_INVALID) }],
-        }}
-        component={Field}
-        componentProps={{
-          euiFieldProps: { readOnly, 'data-test-subj': 'tinesUrlInput', fullWidth: true },
-        }}
-      />
-    </EuiFlexItem>
-    <EuiFlexItem>
-      <EuiSpacer size="s" />
-      <EuiTitle size="xxs">
-        <h4>{i18n.AUTHENTICATION_TITLE}</h4>
-      </EuiTitle>
-    </EuiFlexItem>
-    <EuiFlexItem>
-      <UseField
-        path="secrets.email"
-        config={{
-          label: i18n.EMAIL_LABEL,
-          validations: [{ validator: emptyField(i18n.EMAIL_REQUIRED) }],
-        }}
-        component={Field}
-        componentProps={{
-          euiFieldProps: { readOnly, 'data-test-subj': 'tinesEmailInput' },
-        }}
-      />
-    </EuiFlexItem>
-    <EuiFlexItem>
-      <PasswordField
-        path="secrets.token"
-        label={i18n.TOKEN_LABEL}
-        config={{
-          validations: [{ validator: emptyField(i18n.TOKEN_REQUIRED) }],
-        }}
-        readOnly={readOnly}
-        data-test-subj="tinesTokenInput"
-      />
-    </EuiFlexItem>
-  </EuiFlexGroup>
+  <SimpleConnectorForm
+    isEdit={isEdit}
+    readOnly={readOnly}
+    configFormSchema={configFormSchema}
+    secretsFormSchema={secretsFormSchema}
+  />
 );
 
 // eslint-disable-next-line import/no-default-export
