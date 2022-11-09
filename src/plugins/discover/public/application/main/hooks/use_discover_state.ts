c40eda8eb744ddd174707ae644ec98e1da99353e
Davis McPhee <davis.mcphee@elastic.co>
2022-11-09 17:29:52 -0400

    [Discover] Refetch data when chart visibility is toggled or breakdown field is changed to keep total hits, chart, and documents in sync

--- src/plugins/discover/public/application/main/hooks/use_discover_state.ts
+++ src/plugins/discover/public/application/main/hooks/use_discover_state.ts
@@ -178,10 +178,10 @@ export function useDiscoverState({
    */
   useEffect(() => {
     const unsubscribe = appStateContainer.subscribe(async (nextState) => {
-      const { hideChart, interval, sort, index } = state;
-      // chart was hidden, now it should be displayed, so data is needed
-      const chartDisplayChanged = nextState.hideChart !== hideChart && hideChart;
+      const { hideChart, interval, breakdownField, sort, index } = state;
+      const chartDisplayChanged = nextState.hideChart !== hideChart;
       const chartIntervalChanged = nextState.interval !== interval;
+      const breakdownFieldChanged = nextState.breakdownField !== breakdownField;
       const docTableSortChanged = !isEqual(nextState.sort, sort);
       const dataViewChanged = !isEqual(nextState.index, index);
       // NOTE: this is also called when navigating from discover app to context app
@@ -214,9 +214,15 @@ export function useDiscoverState({
         reset();
       }
 
-      if (chartDisplayChanged || chartIntervalChanged || docTableSortChanged) {
+      if (
+        chartDisplayChanged ||
+        chartIntervalChanged ||
+        breakdownFieldChanged ||
+        docTableSortChanged
+      ) {
         refetch$.next(undefined);
       }
+
       setState(nextState);
     });
     return () => unsubscribe();
