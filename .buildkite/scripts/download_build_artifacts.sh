d382258e004fcf6a44fcbb70c1d2a2644ec61f7f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-11-09 13:13:39 +0100

    print KIBANA_BUILD_ID & BUILDKITE_BUILD_ID

--- .buildkite/scripts/download_build_artifacts.sh
+++ .buildkite/scripts/download_build_artifacts.sh
@@ -11,6 +11,8 @@ if [[ ! -d "$KIBANA_BUILD_LOCATION/bin" ]]; then
 
   download_artifact kibana-default.tar.gz . --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
   download_artifact kibana-default-plugins.tar.gz . --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
+  echo "KIBANA_BUILD_ID = $KIBANA_BUILD_ID"
+  echo "BUILDKITE_BUILD_ID = $BUILDKITE_BUILD_ID"
 
   mkdir -p "$KIBANA_BUILD_LOCATION"
   tar -xzf kibana-default.tar.gz -C "$KIBANA_BUILD_LOCATION" --strip=1
