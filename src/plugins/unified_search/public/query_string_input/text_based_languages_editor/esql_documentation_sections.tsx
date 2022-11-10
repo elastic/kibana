03ea04d1c95f4c3b3e3a4847ec81188b0a0b9521
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2022-11-10 10:18:05 +0200

    final documentation entries

--- src/plugins/unified_search/public/query_string_input/text_based_languages_editor/esql_documentation_sections.tsx
+++ src/plugins/unified_search/public/query_string_input/text_based_languages_editor/esql_documentation_sections.tsx
@@ -21,7 +21,7 @@ Elasticsearch ESQL is a piped language where you can combine aggregations, filte
 ESQL query example:
                     
 \`\`\`
-from "index" | stats average = avg("field") by "field2"
+from index | stats average = avg(field) by field2
 \`\`\`
                                         
                                       `,
@@ -35,7 +35,7 @@ export const commands = {
     defaultMessage: 'Commands',
   }),
   description: i18n.translate('unifiedSearch.query.textBasedLanguagesEditor.commandsDescription', {
-    defaultMessage: `Supported commands.`,
+    defaultMessage: `To create Elasticsearch ESQL expressions, use the supported commands.`,
   }),
   items: [
     {
@@ -51,10 +51,10 @@ export const commands = {
 Retrieves data from one or more datasets. A dataset is a collection of data that you want to search. The only supported dataset is an index. 
 In a query or subquery, you must use the from command first and it does not need a leading pipe.
 
-For example, to…
+For example, to retrieve data from an index:
               
 \`\`\`
-from "index" 
+from index
 \`\`\`
             `,
               description:
@@ -77,10 +77,10 @@ from "index"
 Calculates aggregate statistics, such as average, count, and sum, over the incoming search results set. Similar to SQL aggregation, if the stats command is used without a BY clause, only one row is returned, which is the aggregation over the entire incoming search results set. When you use a BY clause, one row is returned for each distinct value in the field specified in the BY clause. 
 The stats command returns only the fields in the aggregation, and you can use a wide range of statistical functions with the stats command. When you perform more than one aggregation, separate each aggregation with a comma.
 
-For example, to…
+For example, to calculate the average of a field:
               
 \`\`\`
-from "index" | stats average = avg("field")
+from index | stats average = avg(field)
 \`\`\`
             `,
               description:
@@ -102,10 +102,10 @@ from "index" | stats average = avg("field")
               defaultMessage: `### eval
 Calculates an expression and puts the resulting value into a search results field.
 
-For example to...
+For example to increment by 1 the average of a field:
 
 \`\`\`
-from "index" | stats average = avg("field") | eval new_average = average + 1
+from index | stats average = avg(field) | eval new_average = average + 1
 \`\`\`
             `,
               description:
@@ -127,10 +127,10 @@ from "index" | stats average = avg("field") | eval new_average = average + 1
               defaultMessage: `### sort
 Sorts all results by the specified fields. When in descending order, the results missing a field are considered the smallest possible value of the field, or the largest possible value of the field when in ascending order.
 
-For example to...
+For example to sort with ascending order:
 
 \`\`\`
-from "index" | stats average = avg("field") | sort average desc
+from index | stats average = avg(field) | sort average asc
 \`\`\`
             `,
               description:
@@ -152,10 +152,10 @@ from "index" | stats average = avg("field") | sort average desc
               defaultMessage: `### limit
 Returns the first search results, in search order, based on the <limit> specified.
 
-For example, to…
+For example, to fetch only the top 10 results:
               
 \`\`\`
-from "index" | limit 10
+from index | limit 10
 \`\`\`
             `,
               description:
@@ -177,10 +177,10 @@ from "index" | limit 10
               defaultMessage: `### where
 Uses <predicate-expressions> to filter search results. A predicate expression, when evaluated, returns TRUE or FALSE. The where command only returns the results that evaluate to TRUE.
 
-For example, to…
+For example, to filter results for a specific field value:
               
 \`\`\`
-from "index" where field="value"
+from index where field="value"
 \`\`\`
             `,
               description:
@@ -220,8 +220,8 @@ export const mathematicalFunctions = {
 Returns a number rounded to the decimal, specified by the precision. The default is to round to an integer.
 
 \`\`\`
-from "index" where field="value" | 
-eval rounded = round("field", 3)
+from index where field="value" | 
+eval rounded = round(field, 3)
 \`\`\`
               `,
               description:
@@ -259,7 +259,7 @@ export const operators = {
             {
               defaultMessage: `### Equality (=)
 \`\`\`
-from "index" | stats average = avg("field") | eval new_average = average + 1
+from index | stats average = avg(field) | eval new_average = average + 1
 \`\`\`
               `,
               description:
@@ -283,7 +283,7 @@ from "index" | stats average = avg("field") | eval new_average = average + 1
             {
               defaultMessage: `### Add (+)
 \`\`\`
-from "index" | stats average = avg("field") | eval new_average = average + 1
+from index | stats average = avg(field) | eval new_average = average + 1
 \`\`\`
               `,
               description:
@@ -307,7 +307,55 @@ from "index" | stats average = avg("field") | eval new_average = average + 1
             {
               defaultMessage: `### Subtract (-)
 \`\`\`
-from "index" | stats average = avg("field") | eval new_average = average - 1
+from index | stats average = avg(field) | eval new_average = average - 1
+\`\`\`
+              `,
+              description:
+                'Text is in markdown. Do not translate function names, special characters, or field names like sum(bytes)',
+            }
+          )}
+        />
+      ),
+    },
+    {
+      label: i18n.translate(
+        'unifiedSearch.query.textBasedLanguagesEditor.documentationESQL.divideOperator',
+        {
+          defaultMessage: 'Divide',
+        }
+      ),
+      description: (
+        <Markdown
+          markdown={i18n.translate(
+            'unifiedSearch.query.textBasedLanguagesEditor.documentationESQL.divideOperator.markdown',
+            {
+              defaultMessage: `### Divide (/)
+\`\`\`
+from index | stats average = avg(field) | eval new_average = average / 10
+\`\`\`
+              `,
+              description:
+                'Text is in markdown. Do not translate function names, special characters, or field names like sum(bytes)',
+            }
+          )}
+        />
+      ),
+    },
+    {
+      label: i18n.translate(
+        'unifiedSearch.query.textBasedLanguagesEditor.documentationESQL.multiplyOperator',
+        {
+          defaultMessage: 'Multiply',
+        }
+      ),
+      description: (
+        <Markdown
+          markdown={i18n.translate(
+            'unifiedSearch.query.textBasedLanguagesEditor.documentationESQL.multiplyOperator.markdown',
+            {
+              defaultMessage: `### Multiply (*)
+\`\`\`
+from index | stats average = avg(field) | eval new_average = average * 10
 \`\`\`
               `,
               description:
@@ -346,7 +394,7 @@ export const aggregateFunctions = {
               defaultMessage: `### avg
 Returns the average of the values in a field.
 \`\`\`
-from "index" | stats average = avg("field")
+from index | stats average = avg(field)
 \`\`\`
               `,
               description:
@@ -372,7 +420,7 @@ from "index" | stats average = avg("field")
 Returns the maximum value in a field.
 
 \`\`\`
-from "index" | stats max = max("field")
+from index | stats max = max(field)
 \`\`\`
 
               `,
@@ -399,7 +447,7 @@ from "index" | stats max = max("field")
 Returns the minimum value in a field.
 
 \`\`\`
-from "index" | stats min = min("field")
+from index | stats min = min(field)
 \`\`\`
 
               `,
@@ -426,7 +474,7 @@ from "index" | stats min = min("field")
 Returns the sum of the values in a field.
 
 \`\`\`
-from "index" | stats sum = sum("field")
+from index | stats sum = sum(field)
 \`\`\`
               `,
               description:
