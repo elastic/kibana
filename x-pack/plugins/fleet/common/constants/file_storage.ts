5c93eda70a41140de562ed07fd7a8320f7d90785
Paul Tavares <paul.tavares@elastic.co>
2022-11-10 12:24:36 -0500

    add jsdocs

--- x-pack/plugins/fleet/common/constants/file_storage.ts
+++ x-pack/plugins/fleet/common/constants/file_storage.ts
@@ -6,5 +6,7 @@
  */
 
 // File storage indexes supporting endpoint Upload/download
+// If needing to get an integration specific index name, use the utility functions
+// found in `common/services/file_storage`
 export const FILE_STORAGE_METADATA_INDEX_PATTERN = '.fleet-files-*';
 export const FILE_STORAGE_DATA_INDEX_PATTERN = '.fleet-file-data-*';
