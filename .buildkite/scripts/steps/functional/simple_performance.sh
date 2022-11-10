bbf36d91123c747af86803e2b220cc0c3aef117b
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-11-10 14:51:12 +0100

    Revert "restart ES"
    This reverts commit a9bf6e24402ec0e199b4c1c6ae9abc4e3bf0e729.

--- .buildkite/scripts/steps/functional/simple_performance.sh
+++ .buildkite/scripts/steps/functional/simple_performance.sh
@@ -29,104 +29,106 @@ export TEST_ES_URL=http://elastic:changeme@localhost:9200
 export TEST_ES_DISABLE_STARTUP=true
 export KIBANA_BASE_URL=http://localhost:5620
 
-for ((i=1;i<=15;i++)); do
-  echo "--- 🔎 Start es"
 
-  cd "$KIBANA_DIR"
-  node scripts/es snapshot&
-  export esPid=$!
+echo "--- 🔎 Start es"
 
-  # Pings the es server every second for up to 2 minutes until it is green
-  curl \
-    --fail \
-    --silent \
-    --retry 120 \
-    --retry-delay 1 \
-    --retry-connrefused \
-    -XGET "${TEST_ES_URL}/_cluster/health?wait_for_nodes=>=1&wait_for_status=yellow" \
-    > /dev/null
+node scripts/es snapshot&
+export esPid=$!
 
-  echo "✅ ES is ready and will run in the background"
+# Pings the es server every second for up to 2 minutes until it is green
+curl \
+  --fail \
+  --silent \
+  --retry 120 \
+  --retry-delay 1 \
+  --retry-connrefused \
+  -XGET "${TEST_ES_URL}/_cluster/health?wait_for_nodes=>=1&wait_for_status=yellow" \
+  > /dev/null
 
-  curl -I -XGET "${TEST_ES_URL}/_cat/indices"
-  curl -I -XGET "${TEST_ES_URL}/_cat/count?v=true"
+echo "✅ ES is ready and will run in the background"
 
-  cd "$KIBANA_BUILD_LOCATION"
+curl -I -XGET "${TEST_ES_URL}/_cat/indices"
+curl -I -XGET "${TEST_ES_URL}/_cat/count?v=true"
 
-  echo "--- Starting kibana"
+cd "$KIBANA_BUILD_LOCATION"
 
-  ./bin/kibana \
-    --csp.disableUnsafeEval=true  \
-    --csp.strict=false  \
-    --csp.warnLegacyBrowsers=false  \
-    --elasticsearch.hosts=http://localhost:9200  \
-    --elasticsearch.password=changeme  \
-    --elasticsearch.username=kibana_system  \
-    --env.name=development \
-    --savedObjects.maxImportPayloadBytes=10485760  \
-    --server.maxPayload=1679958  \
-    --server.port=5620  \
-    --server.uuid=5b2de169-2785-441b-ae8c-186a1936b17d  \
-    --status.allowAnonymous=true  \
-    --telemetry.banner=false  \
-    --telemetry.labels="{\"branch\": \"$BUILDKITE_BRANCH\", \"ciBuildNumber\": \"10\", \"ciBuildName\": \"kibana-single-user-performance-debug\", \"journeyName\":\"simple-test\"}"  \
-    --telemetry.optIn=true  \
-    --telemetry.sendUsageTo=staging  \
-    --xpack.security.encryptionKey="wuGNaIhoMpk5sO4UBxgr3NyW1sFcLgIf" &
-    export kbnPid=$!
+echo "starting kibana"
 
-  echo "kibana started on $kbnPid";
+./bin/kibana \
+--csp.disableUnsafeEval=true  \
+--csp.strict=false  \
+--csp.warnLegacyBrowsers=false  \
+--elasticsearch.hosts=http://localhost:9200  \
+--elasticsearch.password=changeme  \
+--elasticsearch.username=kibana_system  \
+--env.name=development \
+--savedObjects.maxImportPayloadBytes=10485760  \
+--server.maxPayload=1679958  \
+--server.port=5620  \
+--server.uuid=5b2de169-2785-441b-ae8c-186a1936b17d  \
+--status.allowAnonymous=true  \
+--telemetry.banner=false  \
+--telemetry.labels="{\"branch\": \"$BUILDKITE_BRANCH\", \"ciBuildNumber\": \"10\", \"ciBuildName\": \"kibana-single-user-performance-debug\", \"journeyName\":\"simple-test\"}"  \
+--telemetry.optIn=true  \
+--telemetry.sendUsageTo=staging  \
+--xpack.security.encryptionKey="wuGNaIhoMpk5sO4UBxgr3NyW1sFcLgIf" &
+export kbnPid=$!
 
-  # wait until we can login to kibana
-  curl 'http://localhost:5620/internal/security/login' \
-    -H 'Content-Type: application/json' \
-    -H 'kbn-version: 8.6.0-SNAPSHOT' \
-    -H 'x-kbn-context: %7B%22name%22%3A%22security_login%22%2C%22url%22%3A%22%2Flogin%22%7D' \
-    --retry 120 \
-    --retry-delay 5 \
-    --retry-connrefused \
-    --data-raw '{"providerType":"basic","providerName":"basic","currentURL":"http://localhost:5620/login?next=%2F","params":{"username":"elastic","password":"changeme"}}' \
-    --compressed || true
+echo "kibana started on $kbnPid";
 
-  echo "logged in to kibana";
-  cd "$KIBANA_DIR"
+# wait until we can login to kibana
+curl 'http://localhost:5620/internal/security/login' \
+  -H 'Content-Type: application/json' \
+  -H 'kbn-version: 8.6.0-SNAPSHOT' \
+  -H 'x-kbn-context: %7B%22name%22%3A%22security_login%22%2C%22url%22%3A%22%2Flogin%22%7D' \
+  --retry 120 \
+  --retry-delay 5 \
+  --retry-connrefused \
+  --data-raw '{"providerType":"basic","providerName":"basic","currentURL":"http://localhost:5620/login?next=%2F","params":{"username":"elastic","password":"changeme"}}' \
+  --compressed || true
 
-  echo "--- Running archivers";
-  node scripts/es_archiver load test/functional/fixtures/es_archiver/stress_test --es-url "$TEST_ES_URL"
-  node scripts/kbn_archiver load test/functional/fixtures/kbn_archiver/stress_test --kibana-url "http://elastic:changeme@localhost:5620/"
-  sleep 30;
 
+echo "logged in to kibana";
+cd "$KIBANA_DIR"
+
+echo "running archivers";
+node scripts/es_archiver load test/functional/fixtures/es_archiver/stress_test --es-url "$TEST_ES_URL"
+node scripts/kbn_archiver load test/functional/fixtures/kbn_archiver/stress_test --kibana-url "http://elastic:changeme@localhost:5620/"
+
+sleep 30;
+
+for ((i=1;i<=25;i++)); do
   echo "--- Run simple test - #$i"
   node scripts/simple_test.js
   sleep 5;
+done
 
-  echo "--- 🔎 Shutdown Kibana"
-  echo "waiting for $kbnPid to exit gracefully";
+echo "--- 🔎 Shutdown Kibana"
+echo "waiting for $kbnPid to exit gracefully";
 
-  timeout=30 #seconds
-  dur=0
-  while is_running $kbnPid; do
-    sleep 1;
-    ((dur=dur+1))
-    if [ $dur -ge $timeout ]; then
-      echo "es still running after $dur seconds, killing Kibana forcefully";
-      kill -9 "$kbnPid"
-      sleep 5;
-    fi
-  done
+timeout=30 #seconds
+dur=0
+while is_running $kbnPid; do
+  sleep 1;
+  ((dur=dur+1))
+  if [ $dur -ge $timeout ]; then
+    echo "es still running after $dur seconds, killing Kibana forcefully";
+    kill -9 "$kbnPid"
+    sleep 5;
+  fi
+done
 
-  echo "--- 🔎 Shutdown ES"
-  echo "waiting for $esPid to exit gracefully";
+echo "--- 🔎 Shutdown ES"
+echo "waiting for $esPid to exit gracefully";
 
-  timeout=30 #seconds
-  dur=0
-  while is_running $esPid; do
-    sleep 1;
-    ((dur=dur+1))
-    if [ $dur -ge $timeout ]; then
-      echo "es still running after $dur seconds, killing ES forcefully";
-      kill -9 "$esPid"
-      sleep 5;
-    fi
-  done
+timeout=30 #seconds
+dur=0
+while is_running $esPid; do
+  sleep 1;
+  ((dur=dur+1))
+  if [ $dur -ge $timeout ]; then
+    echo "es still running after $dur seconds, killing ES forcefully";
+    kill -9 "$esPid"
+    sleep 5;
+  fi
 done
