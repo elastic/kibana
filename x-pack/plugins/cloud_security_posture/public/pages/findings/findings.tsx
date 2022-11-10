409d6c44f745ec1e423cfea064e652b8518973dd
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-11-10 09:08:09 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/cloud_security_posture/public/pages/findings/findings.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/findings/findings.tsx
@@ -7,7 +7,6 @@
 import React from 'react';
 import type { UseQueryResult } from '@tanstack/react-query';
 import { Redirect, Switch, Route, useLocation } from 'react-router-dom';
-import { TrackApplicationView } from '@kbn/usage-collection-plugin/public';
 import { useCspSetupStatusApi } from '../../common/api/use_setup_status_api';
 import { NoFindingsStates } from '../../components/no_findings_states';
 import { CloudPosturePage } from '../../components/cloud_posture_page';
