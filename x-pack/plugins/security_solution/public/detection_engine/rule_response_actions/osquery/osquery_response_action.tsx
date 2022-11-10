d9c571aeea2ed4f82211499ba15fe8eae44fe8fd
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2022-11-10 12:05:31 +0100

    add field validation for params, show errors

--- x-pack/plugins/security_solution/public/detection_engine/rule_response_actions/osquery/osquery_response_action.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_response_actions/osquery/osquery_response_action.tsx
@@ -8,15 +8,29 @@
 import React, { useMemo } from 'react';
 import { EuiCode, EuiEmptyPrompt } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
+import { useIsMounted } from '@kbn/securitysolution-hook-utils';
 import type { ResponseActionValidatorRef } from '../response_actions_form';
-import type { ArrayItem } from '../../../shared_imports';
+import type { ArrayItem, ValidationFunc } from '../../../shared_imports';
 import { useKibana } from '../../../common/lib/kibana';
 import { NOT_AVAILABLE, PERMISSION_DENIED, SHORT_EMPTY_TITLE } from './translations';
+import { UseField } from '../../../shared_imports';
 
 interface IProps {
   item: ArrayItem;
   formRef: React.RefObject<ResponseActionValidatorRef>;
 }
+
+const emptyParamsValidator = (...args: Parameters<ValidationFunc>): ReturnType<ValidationFunc> => {
+  const [{ path }] = args;
+
+  return {
+    code: 'ERR_FIELD_MISSING',
+    path,
+    message: '**ResponseActions:**\n Osquery Response Action is not available.\n ',
+  };
+};
+
+const GhostFormField = () => <></>;
 
 export const OsqueryResponseAction = React.memo((props: IProps) => {
   const { osquery, application } = useKibana().services;
@@ -24,9 +38,10 @@ export const OsqueryResponseAction = React.memo((props: IProps) => {
     () => osquery?.OsqueryResponseActionTypeForm,
     [osquery?.OsqueryResponseActionTypeForm]
   );
+  const isMounted = useIsMounted();
 
   if (osquery) {
-    const { disabled, permissionDenied } = osquery?.fetchInstallationStatus();
+    const { disabled, permissionDenied } = osquery.fetchInstallationStatus();
     const disabledOsqueryPermission = !(
       application?.capabilities?.osquery?.writeLiveQueries ||
       (application?.capabilities?.osquery?.runSavedQueries &&
@@ -37,6 +52,17 @@ export const OsqueryResponseAction = React.memo((props: IProps) => {
     if (permissionDenied || disabledOsqueryPermission) {
       return (
         <>
+          <UseField
+            path={`${props.item.path}.params`}
+            component={GhostFormField}
+            config={{
+              validations: [
+                {
+                  validator: emptyParamsValidator,
+                },
+              ],
+            }}
+          />
           <EuiEmptyPrompt
             title={<h2>{PERMISSION_DENIED}</h2>}
             titleSize="xs"
@@ -61,15 +87,28 @@ export const OsqueryResponseAction = React.memo((props: IProps) => {
 
     if (disabled) {
       return (
-        <EuiEmptyPrompt
-          iconType="logoOsquery"
-          title={<h2>{SHORT_EMPTY_TITLE}</h2>}
-          titleSize="xs"
-          body={<p>{NOT_AVAILABLE}</p>}
-        />
+        <>
+          <UseField
+            path={`${props.item.path}.params`}
+            component={GhostFormField}
+            config={{
+              validations: [
+                {
+                  validator: emptyParamsValidator,
+                },
+              ],
+            }}
+          />
+          <EuiEmptyPrompt
+            iconType="logoOsquery"
+            title={<h2>{SHORT_EMPTY_TITLE}</h2>}
+            titleSize="xs"
+            body={<p>{NOT_AVAILABLE}</p>}
+          />
+        </>
       );
     }
-    if (OsqueryForm) {
+    if (isMounted() && OsqueryForm) {
       return <OsqueryForm {...props} />;
     }
   }
--- x-pack/plugins/security_solution/public/detection_engine/rule_response_actions/response_actions_form.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_response_actions/response_actions_form.tsx
@@ -5,14 +5,27 @@
  * 2.0.
  */
 
-import React, { useEffect, useMemo, useRef } from 'react';
-import { EuiSpacer } from '@elastic/eui';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { EuiCallOut, EuiSpacer } from '@elastic/eui';
 import { isEmpty, map, some } from 'lodash';
+import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
+import type { ValidationError } from '@kbn/osquery-plugin/public/shared_imports';
+import { FORM_ERRORS_TITLE } from '../../detections/components/rules/rule_actions_field/translations';
 import { ResponseActionsHeader } from './response_actions_header';
 import { ResponseActionsList } from './response_actions_list';
 
 import type { ArrayItem } from '../../shared_imports';
+import { useFormContext } from '../../shared_imports';
+import { useFormData } from '../../shared_imports';
 import { useSupportedResponseActionTypes } from './use_supported_response_action_types';
+import { RESPONSE_ACTION_TYPES } from '../../../common/detection_engine/rule_response_actions/schemas';
+
+const FieldErrorsContainer = styled.div`
+  p {
+    margin-bottom: 0;
+  }
+`;
 
 export interface ResponseActionValidatorRef {
   validation: {
@@ -32,16 +45,26 @@ interface IProps {
 export const ResponseActionsForm = ({ items, addItem, removeItem, saveClickRef }: IProps) => {
   const responseActionsValidationRef = useRef<ResponseActionValidatorRef>({ validation: {} });
   const supportedResponseActionTypes = useSupportedResponseActionTypes();
+  const [fieldErrors, setFieldErrors] = useState<string | null>(null);
+  const [formData] = useFormData();
+  const { getFields, validate } = useFormContext();
+  const fields = getFields();
 
-  useEffect(() => {
-    if (saveClickRef && saveClickRef.current) {
-      saveClickRef.current.onSaveClick = () => {
-        return validateResponseActions();
-      };
+  const validateResponseActions = useCallback(async () => {
+    const paramsErrors: Array<ValidationError<string>> = [];
+    await validate();
+    if (formData?.responseActions?.length) {
+      map(formData.responseActions, async (action, index) => {
+        if (action.actionTypeId === RESPONSE_ACTION_TYPES.OSQUERY && isEmpty(action.params)) {
+          paramsErrors.push(fields[`responseActions[${index}].params`].errors[0]);
+        }
+      });
+      const errorsString = paramsErrors.map(({ message }) => message).join('\n');
+      setFieldErrors(errorsString);
+    } else {
+      setFieldErrors(null);
     }
-  }, [saveClickRef]);
 
-  const validateResponseActions = async () => {
     if (!isEmpty(responseActionsValidationRef.current?.validation)) {
       const response = await Promise.all(
         map(responseActionsValidationRef.current?.validation, async (validation) => {
@@ -51,7 +74,15 @@ export const ResponseActionsForm = ({ items, addItem, removeItem, saveClickRef }
 
       return some(response, (val) => !val);
     }
-  };
+  }, [fields, formData.responseActions, validate]);
+
+  useEffect(() => {
+    if (saveClickRef && saveClickRef.current) {
+      saveClickRef.current.onSaveClick = () => {
+        return validateResponseActions();
+      };
+    }
+  }, [saveClickRef, validateResponseActions]);
 
   const form = useMemo(() => {
     if (!supportedResponseActionTypes?.length) {
@@ -72,6 +103,16 @@ export const ResponseActionsForm = ({ items, addItem, removeItem, saveClickRef }
     <>
       <EuiSpacer size="xxl" data-test-subj={'response-actions-form'} />
       <ResponseActionsHeader />
+      {fieldErrors ? (
+        <>
+          <FieldErrorsContainer>
+            <EuiCallOut title={FORM_ERRORS_TITLE} color="danger" iconType="alert">
+              <ReactMarkdown>{fieldErrors}</ReactMarkdown>
+            </EuiCallOut>
+          </FieldErrorsContainer>
+          <EuiSpacer />
+        </>
+      ) : null}
       {form}
     </>
   );
