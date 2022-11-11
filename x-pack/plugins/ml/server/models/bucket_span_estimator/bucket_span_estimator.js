806571c8481f7c84ec0570f3bda2a04cc441dee5
James Gowdy <jgowdy@elastic.co>
2022-11-11 08:40:45 +0000

    adding correct single series checker args

--- x-pack/plugins/ml/server/models/bucket_span_estimator/bucket_span_estimator.js
+++ x-pack/plugins/ml/server/models/bucket_span_estimator/bucket_span_estimator.js
@@ -117,6 +117,7 @@ export function estimateBucketSpanFactory(client) {
               });
               this.checkers.push({
                 check: new SingleSeriesChecker(
+                  asCurrentUser,
                   this.index,
                   this.timeField,
                   this.aggTypes[i],
