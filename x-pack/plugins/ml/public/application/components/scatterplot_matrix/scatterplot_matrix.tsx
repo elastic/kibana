a87f77a160baf8c5468b4e6315eebcb5a7924d0e
Melissa <melissa.alvarez@elastic.co>
2022-11-12 11:36:08 -0500

    update helptext copy

--- x-pack/plugins/ml/public/application/components/scatterplot_matrix/scatterplot_matrix.tsx
+++ x-pack/plugins/ml/public/application/components/scatterplot_matrix/scatterplot_matrix.tsx
@@ -468,7 +468,7 @@ export const ScatterplotMatrix: FC<ScatterplotMatrixProps> = ({
                     fullWidth
                     helpText={i18n.translate('xpack.ml.splom.backgroundLayerHelpText', {
                       defaultMessage:
-                        'The portion of the data not reflecting the current filter is presented in a blurred gray.',
+                        'If the data points match your filter, they\'re shown in color; otherwise, they\'re blurred gray',
                     })}
                   >
                     <></>
