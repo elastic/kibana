4f84167d7c7b1a6d8d7a89b01ad0a3e8a5cbf544
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-11-13 22:51:45 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/observability/public/components/shared/alert_search_bar/alert_search_bar.tsx
+++ x-pack/plugins/observability/public/components/shared/alert_search_bar/alert_search_bar.tsx
@@ -17,7 +17,6 @@ import { AlertsStatusFilter } from './components';
 import { ALERT_STATUS_QUERY, DEFAULT_QUERIES, DEFAULT_QUERY_STRING } from './constants';
 import { AlertSearchBarProps } from './types';
 import { buildEsQuery } from '../../../utils/build_es_query';
-import { isValidKuery } from '../../../utils/kuery';
 import { AlertStatus } from '../../../../common/typings';
 
 const getAlertStatusQuery = (status: string): Query[] => {
