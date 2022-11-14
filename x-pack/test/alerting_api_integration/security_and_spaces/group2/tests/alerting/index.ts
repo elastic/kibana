1f918162ae2d9a2f2b7c4ec7abc38b3bddf53b6c
Zacqary Xeper <zacqary.xeper@elastic.co>
2022-11-14 13:20:47 -0600

    Add namespaces tests to global logs apis

--- x-pack/test/alerting_api_integration/security_and_spaces/group2/tests/alerting/index.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/group2/tests/alerting/index.ts
@@ -45,6 +45,7 @@ export default function alertingTests({ loadTestFile, getService }: FtrProviderC
       loadTestFile(require.resolve('./excluded'));
       loadTestFile(require.resolve('./snooze'));
       loadTestFile(require.resolve('./global_execution_log'));
+      loadTestFile(require.resolve('./get_global_execution_kpi'));
     });
   });
 }
