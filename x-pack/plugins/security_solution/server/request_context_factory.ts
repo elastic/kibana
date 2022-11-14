2181272a9e36ed726759c82fbba511ef3172fa3c
Paul Tavares <paul.tavares@elastic.co>
2022-11-14 14:43:05 -0500

    Change `SecuritySolutionApiRequestHandlerContext` to use logic in `endpointAppContextService.getEndpointAuthz()`

--- x-pack/plugins/security_solution/server/request_context_factory.ts
+++ x-pack/plugins/security_solution/server/request_context_factory.ts
@@ -9,9 +9,6 @@ import { memoize } from 'lodash';
 
 import type { Logger, KibanaRequest, RequestHandlerContext } from '@kbn/core/server';
 
-import type { FleetAuthz } from '@kbn/fleet-plugin/common';
-import { ENDPOINT_HOST_ISOLATION_EXCEPTIONS_LIST_ID } from '@kbn/securitysolution-list-constants';
-import { doesArtifactHaveData } from './endpoint/services';
 import { DEFAULT_SPACE_ID } from '../common/constants';
 import { AppClientFactory } from './client';
 import type { ConfigType } from './config';
@@ -27,13 +24,6 @@ import type {
 } from './types';
 import type { Immutable } from '../common/endpoint/types';
 import type { EndpointAuthz } from '../common/endpoint/types/authz';
-import {
-  calculateEndpointAuthz,
-  calculatePermissionsFromPrivileges,
-  defaultEndpointPermissions,
-  getEndpointAuthzInitialState,
-} from '../common/endpoint/service/authz';
-import { licenseService } from './lib/license';
 import type { EndpointAppContextService } from './endpoint/endpoint_app_context_services';
 
 export interface IRequestContextFactory {
@@ -64,8 +54,7 @@ export class RequestContextFactory implements IRequestContextFactory {
     request: KibanaRequest
   ): Promise<SecuritySolutionApiRequestHandlerContext> {
     const { options, appClientFactory } = this;
-    const { config, core, plugins, endpointAppContextService, ruleExecutionLogService, logger } =
-      options;
+    const { config, core, plugins, endpointAppContextService, ruleExecutionLogService } = options;
     const { lists, ruleRegistry, security, licensing, osquery } = plugins;
 
     const [, startPlugins] = await core.getStartServices();
@@ -77,73 +66,17 @@ export class RequestContextFactory implements IRequestContextFactory {
       config,
     });
 
+    // List of endpoint authz for the current request's user. Will be initialized the first
+    // time it is requested (see `getEndpointAuthz()` below)
     let endpointAuthz: Immutable<EndpointAuthz>;
-    let fleetAuthz: FleetAuthz;
-
-    // If Fleet is enabled, then get its Authz
-    if (startPlugins.fleet) {
-      fleetAuthz =
-        (await context.fleet)?.authz ?? (await startPlugins.fleet?.authz.fromRequest(request));
-    }
-
-    const getExceptionListClient = () => {
-      if (!lists) {
-        return null;
-      }
-
-      const username = security?.authc.getCurrentUser(request)?.username || 'elastic';
-      return lists.getExceptionListClient(coreContext.savedObjects.client, username);
-    };
-
-    // Some of the Endpoint authz properties needs to know if Host Isolation Exceptions exist
-    // when license is not Platinum+
-    let hasHostIsolationException = false;
-    if (lists && !endpointAppContextService.getLicenseService().isPlatinumPlus()) {
-      const listClient = getExceptionListClient();
-      if (listClient) {
-        hasHostIsolationException = await doesArtifactHaveData(
-          listClient,
-          ENDPOINT_HOST_ISOLATION_EXCEPTIONS_LIST_ID,
-          logger
-        );
-      }
-    }
-
-    let endpointPermissions = defaultEndpointPermissions();
-    if (endpointAppContextService.security) {
-      const checkPrivileges =
-        endpointAppContextService.security.authz.checkPrivilegesDynamicallyWithRequest(request);
-      const { privileges } = await checkPrivileges({
-        kibana: [
-          endpointAppContextService.security.authz.actions.ui.get('siem', 'crud'),
-          endpointAppContextService.security.authz.actions.ui.get('siem', 'show'),
-        ],
-      });
-      endpointPermissions = calculatePermissionsFromPrivileges(privileges.kibana);
-    }
 
     return {
       core: coreContext,
 
-      get endpointAuthz(): Immutable<EndpointAuthz> {
-        // Lazy getter of endpoint Authz. No point in defining it if it is never used.
+      getEndpointAuthz: async (): Promise<Immutable<EndpointAuthz>> => {
         if (!endpointAuthz) {
-          // If no fleet (fleet plugin is optional in the configuration), then just turn off all permissions
-          if (!startPlugins.fleet) {
-            endpointAuthz = getEndpointAuthzInitialState();
-          } else {
-            const { endpointRbacEnabled, endpointRbacV1Enabled } =
-              endpointAppContextService.experimentalFeatures;
-            const userRoles = security?.authc.getCurrentUser(request)?.roles ?? [];
-            endpointAuthz = calculateEndpointAuthz(
-              licenseService,
-              fleetAuthz,
-              userRoles,
-              endpointRbacEnabled || endpointRbacV1Enabled,
-              endpointPermissions,
-              hasHostIsolationException
-            );
-          }
+          // eslint-disable-next-line require-atomic-updates
+          endpointAuthz = await endpointAppContextService.getEndpointAuthz(request);
         }
 
         return endpointAuthz;
@@ -168,7 +101,14 @@ export class RequestContextFactory implements IRequestContextFactory {
         })
       ),
 
-      getExceptionListClient,
+      getExceptionListClient: () => {
+        if (!lists) {
+          return null;
+        }
+
+        const username = security?.authc.getCurrentUser(request)?.username || 'elastic';
+        return lists.getExceptionListClient(coreContext.savedObjects.client, username);
+      },
 
       getInternalFleetServices: memoize(() => endpointAppContextService.getInternalFleetServices()),
 
