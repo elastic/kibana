d7edbb857410512f381434916705b63fbd9cd2cd
Jonathan Buttner <jonathan.buttner@elastic.co>
2022-11-14 12:58:04 -0500

    Fixing tests

--- x-pack/plugins/triggers_actions_ui/public/application/lib/action_variables.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/lib/action_variables.ts
@@ -107,6 +107,14 @@ function getAlwaysProvidedActionVariables(): ActionVariable[] {
   });
 
   result.push({
+    name: AlertProvidedActionVariables.ruleUrl,
+    description: i18n.translate('xpack.triggersActionsUI.actionVariables.ruleUrlLabel', {
+      defaultMessage:
+        'The URL to the Stack Management rule page that generated the alert. This will be an empty string if the server.publicBaseUrl is not configured.',
+    }),
+  });
+
+  result.push({
     name: AlertProvidedActionVariables.date,
     description: i18n.translate('xpack.triggersActionsUI.actionVariables.dateLabel', {
       defaultMessage: 'The date the rule scheduled the action.',
@@ -153,14 +161,6 @@ function getAlwaysProvidedActionVariables(): ActionVariable[] {
     description: i18n.translate('xpack.triggersActionsUI.actionVariables.kibanaBaseUrlLabel', {
       defaultMessage:
         'The configured server.publicBaseUrl value or empty string if not configured.',
-    }),
-  });
-
-  result.push({
-    name: AlertProvidedActionVariables.ruleUrl,
-    description: i18n.translate('xpack.triggersActionsUI.actionVariables.ruleUrlLabel', {
-      defaultMessage:
-        'The URL to the Stack Management rule page that generated the alert. This will be an empty string if the server.publicBaseUrl is not configured.',
     }),
   });
 
