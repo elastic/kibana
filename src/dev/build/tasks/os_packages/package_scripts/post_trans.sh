36bc3a22afc770dc4dfc1cd074c84d150106343b
Jonathan Budzenski <jon@elastic.co>
2022-11-14 14:48:55 -0600

    [build/os-packages] Remove keystore on uninstall

--- src/dev/build/tasks/os_packages/package_scripts/post_trans.sh
+++ src/dev/build/tasks/os_packages/package_scripts/post_trans.sh
@@ -4,5 +4,4 @@ if [ ! -f "${KBN_PATH_CONF}"/kibana.keystore ]; then
     /usr/share/kibana/bin/kibana-keystore create
     chown root:<%= group %> "${KBN_PATH_CONF}"/kibana.keystore
     chmod 660 "${KBN_PATH_CONF}"/kibana.keystore
-    md5sum "${KBN_PATH_CONF}"/kibana.keystore > "${KBN_PATH_CONF}"/.kibana.keystore.initial_md5sum
 fi
