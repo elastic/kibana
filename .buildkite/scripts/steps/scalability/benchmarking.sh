568ff69f371f1d1820ae088d64d3adb4ac7effb0
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-11-14 17:53:57 +0100

    [scalability] benchmark single apis

--- .buildkite/scripts/steps/scalability/benchmarking.sh
+++ .buildkite/scripts/steps/scalability/benchmarking.sh
@@ -18,6 +18,28 @@ rm -rf "${KIBANA_BUILD_LOCATION}"
 rm -rf "${KIBANA_LOAD_TESTING_DIR}"
 rm -rf "${GCS_ARTIFACTS_DIR}"
 
+# unset env vars defined in other parts of CI for automatic APM collection of
+# Kibana. We manage APM config in our FTR config and performance service, and
+# APM treats config in the ENV with a very high precedence.
+unset ELASTIC_APM_ENVIRONMENT
+unset ELASTIC_APM_TRANSACTION_SAMPLE_RATE
+unset ELASTIC_APM_SERVER_URL
+unset ELASTIC_APM_SECRET_TOKEN
+unset ELASTIC_APM_ACTIVE
+unset ELASTIC_APM_CONTEXT_PROPAGATION_ONLY
+unset ELASTIC_APM_GLOBAL_LABELS
+unset ELASTIC_APM_MAX_QUEUE_SIZE
+unset ELASTIC_APM_METRICS_INTERVAL
+unset ELASTIC_APM_CAPTURE_SPAN_STACK_TRACES
+unset ELASTIC_APM_BREAKDOWN_METRICS
+
+# `kill $esPid` doesn't work, seems that kbn-es doesn't listen to signals correctly, this does work
+trap 'killall node -q' EXIT
+
+function is_running {
+  kill -0 "$1" &>/dev/null
+}
+
 download_artifacts() {
   mkdir -p "${GCS_ARTIFACTS_DIR}"
 
@@ -74,29 +96,7 @@ download_artifacts
 echo "--- Clone kibana-load-testing repo and compile project"
 checkout_and_compile_load_runner
 
-echo "--- Run Scalability Tests with Elasticsearch started only once and Kibana restart before each journey"
 cd "$KIBANA_DIR"
-node scripts/es snapshot&
-
-esPid=$!
-# Set trap on EXIT to stop Elasticsearch process
-trap "kill -9 $esPid" EXIT
-
-# unset env vars defined in other parts of CI for automatic APM collection of
-# Kibana. We manage APM config in our FTR config and performance service, and
-# APM treats config in the ENV with a very high precedence.
-unset ELASTIC_APM_ENVIRONMENT
-unset ELASTIC_APM_TRANSACTION_SAMPLE_RATE
-unset ELASTIC_APM_SERVER_URL
-unset ELASTIC_APM_SECRET_TOKEN
-unset ELASTIC_APM_ACTIVE
-unset ELASTIC_APM_CONTEXT_PROPAGATION_ONLY
-unset ELASTIC_APM_GLOBAL_LABELS
-unset ELASTIC_APM_MAX_QUEUE_SIZE
-unset ELASTIC_APM_METRICS_INTERVAL
-unset ELASTIC_APM_CAPTURE_SPAN_STACK_TRACES
-unset ELASTIC_APM_BREAKDOWN_METRICS
-
 
 export TEST_ES_DISABLE_STARTUP=true
 ES_HOST="localhost:9200"
@@ -104,22 +104,51 @@ export TEST_ES_URL="http://elastic:changeme@${ES_HOST}"
 # Overriding Gatling default configuration
 export ES_URL="http://${ES_HOST}"
 
-# Pings the ES server every second for 2 mins until its status is green
-curl --retry 120 \
-  --retry-delay 1 \
-  --retry-connrefused \
-  -I -XGET "${TEST_ES_URL}/_cluster/health?wait_for_nodes=>=1&wait_for_status=yellow"
+for journey in x-pack/test/scalability/apis/*; do
 
-export ELASTIC_APM_ACTIVE=true
+  echo "--- Running $journey"
 
-for journey in scalability_traces/server/*; do
-    export SCALABILITY_JOURNEY_PATH="$KIBANA_DIR/$journey"
-    echo "--- Run scalability file: $SCALABILITY_JOURNEY_PATH"
+  echo "--- $journey - Start ES"
+  node scripts/es snapshot&
+  export esPid=$!
+
+  # Pings the ES server every second for 2 mins until its status is green
+  curl \
+    --retry 120 \
+    --silent \
+    --retry-delay 1 \
+    --retry-connrefused \
+    -I -XGET "${TEST_ES_URL}/_cluster/health?wait_for_nodes=>=1&wait_for_status=yellow" \
+    > /dev/null
+  echo "✅ ES is ready and will run in the background"
+
+  export SCALABILITY_JOURNEY_PATH="$KIBANA_DIR/$journey"
+  echo "--- Run scalability file: $SCALABILITY_JOURNEY_PATH"
     node scripts/functional_tests \
       --config x-pack/test/scalability/config.ts \
       --kibana-install-dir "$KIBANA_BUILD_LOCATION" \
       --logToFile \
       --debug
+
+  # remove trap, we're manually shutting down
+  trap - EXIT;
+
+  echo "--- $journey - Shutdown ES"
+  killall node
+  echo "waiting for $esPid to exit gracefully";
+
+  timeout=30 #seconds
+  dur=0
+  while is_running $esPid; do
+    sleep 1;
+    ((dur=dur+1))
+    if [ $dur -ge $timeout ]; then
+      echo "es still running after $dur seconds, killing ES and node forcefully";
+      killall -SIGKILL java
+      killall -SIGKILL node
+      sleep 5;
+    fi
+  done
 done
 
 echo "--- Upload test results"
