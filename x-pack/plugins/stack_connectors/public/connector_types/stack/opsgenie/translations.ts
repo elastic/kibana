19d9e8d171b95dbed15cef7a9d5e676568d8847e
Lisa Cawley <lcawley@elastic.co>
2022-11-15 11:25:33 -0800

    [ResponseOps][Stack Connectors] Add helptext for Opsgenie connector (#145195)

--- x-pack/plugins/stack_connectors/public/connector_types/stack/opsgenie/translations.ts
+++ x-pack/plugins/stack_connectors/public/connector_types/stack/opsgenie/translations.ts
@@ -118,3 +118,31 @@ export const OPSGENIE_DOCUMENTATION = i18n.translate(
     defaultMessage: 'Opsgenie documentation',
   }
 );
+
+export const OPSGENIE_ALIAS_HELP = i18n.translate(
+  'xpack.stackConnectors.components.opsgenie.fieldAliasHelpText',
+  {
+    defaultMessage: 'The unique alert identifier used for de-deduplication in Opsgenie.',
+  }
+);
+
+export const OPSGENIE_ENTITY_HELP = i18n.translate(
+  'xpack.stackConnectors.components.opsgenie.fieldEntityHelpText',
+  {
+    defaultMessage: 'The domain of the alert. For example, the application name.',
+  }
+);
+
+export const OPSGENIE_SOURCE_HELP = i18n.translate(
+  'xpack.stackConnectors.components.opsgenie.fieldSourceHelpText',
+  {
+    defaultMessage: 'The display name for the source of the alert.',
+  }
+);
+
+export const OPSGENIE_USER_HELP = i18n.translate(
+  'xpack.stackConnectors.components.opsgenie.fieldUserHelpText',
+  {
+    defaultMessage: 'The display name for the owner.',
+  }
+);
