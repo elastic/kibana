9608929967d501bd8d4266c277df60af82d22ba8
Sean Sullivan <ssullivan@spectric.com>
2022-11-16 16:28:41 -0500

    Use better icons, I couldn't find a good one in eui icons

--- src/plugins/unified_search/public/filter_bar/filter_editor/map/draw_filter.tsx
+++ src/plugins/unified_search/public/filter_bar/filter_editor/map/draw_filter.tsx
@@ -1,4 +1,4 @@
-import { Component, RefObject } from 'react';
+import { Component, RefObject, FunctionComponent } from 'react';
 import ReactDOM from "react-dom";
 import type { Map as MbMap } from '@kbn/mapbox-gl';
 // @ts-expect-error
@@ -49,7 +49,7 @@ export class DrawFilter extends Component<Props, State> {
         this.props.mbMap.addControl(this._mbDrawControl);
         this.props.mbMap.on('draw.create', this._onDraw);
         const {  DRAW_POLYGON   } = this._mbDrawControl.modes;
-        let controlTypes = [{mode:DRAW_POLYGON,icon:"grid"},{mode:DRAW_RECTANGLE,icon:"grabHorizontal"},]
+        let controlTypes = [{mode:DRAW_POLYGON,icon:DrawPolygonIcon},{mode:DRAW_RECTANGLE,icon:DrawRectIcon},]
         controlTypes.forEach(c=>{
             let control = new MBMapButton({
                 icon:c.icon,
@@ -161,3 +161,56 @@ class MBMapButton implements IControl {
         }
     }
 }
+
+
+export const DrawPolygonIcon: FunctionComponent = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="49"
+    height="25"
+    fill="none"
+    viewBox="0 0 49 25"
+    className="mapLayersWizardIcon"
+  >
+    <path
+      className="mapLayersWizardIcon__background"
+      d="M12.281 3l-6.625 7.625 1.657 8.938 35.218-.813v-13l-10.625-3.5-9.781 9.5L12.281 3z"
+    />
+    <path
+      className="mapLayersWizardIcon__highlight"
+      fillRule="evenodd"
+      d="M31.775 1.68l11.256 3.708v13.85l-36.133.834-1.777-9.593 7.114-8.189 9.875 8.778 9.665-9.388zm.262 1.14l-9.897 9.612-9.813-8.722-6.135 7.06 1.535 8.283 34.304-.792V6.111L32.037 2.82z"
+      clipRule="evenodd"
+    />
+    <circle cx="7.281" cy="19.5" r="2.5" className="mapLayersWizardIcon__highlight" />
+    <circle cx="5.656" cy="10.25" r="2.5" className="mapLayersWizardIcon__highlight" />
+    <circle cx="12.156" cy="3.625" r="2.5" className="mapLayersWizardIcon__highlight" />
+    <circle cx="22" cy="11.6" r="2.5" className="mapLayersWizardIcon__highlight" />
+    <circle cx="31.969" cy="2.5" r="2.5" className="mapLayersWizardIcon__highlight" />
+    <circle cx="42.344" cy="6.125" r="2.5" className="mapLayersWizardIcon__highlight" />
+    <circle cx="42.344" cy="19" r="2.5" className="mapLayersWizardIcon__highlight" />
+  </svg>
+);
+
+export const DrawRectIcon: FunctionComponent = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="49"
+      height="25"
+      fill="none"
+      viewBox="0 0 49 25"
+      className="mapLayersWizardIcon"
+    >
+
+      <rect x="10" y="0" width="25" height="25" style={{strokeWidth:1,stroke:"rgb(0,0,0)"}} />
+
+      <circle cx="10" cy="0" r="2.5" className="mapLayersWizardIcon__highlight" />
+      <circle cx="10" cy="25" r="2.5" className="mapLayersWizardIcon__highlight" />
+      <circle cx="35" cy="25" r="2.5" className="mapLayersWizardIcon__highlight" />
+      <circle cx="35" cy="0" r="2.5" className="mapLayersWizardIcon__highlight" />
+
+    </svg>
+  );
+
+
+  
