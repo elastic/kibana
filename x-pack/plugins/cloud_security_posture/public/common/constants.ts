c64e4e79396e366c7ecab2936a2ea5ad2e0613b6
Or Ouziel <ouzielor@gmail.com>
2022-11-16 15:08:54 +0200

    change default rows per page to 25

--- x-pack/plugins/cloud_security_posture/public/common/constants.ts
+++ x-pack/plugins/cloud_security_posture/public/common/constants.ts
@@ -16,7 +16,7 @@ export const statusColors = {
 
 export const CSP_MOMENT_FORMAT = 'MMMM D, YYYY @ HH:mm:ss.SSS';
 export const MAX_FINDINGS_TO_LOAD = 500;
-export const DEFAULT_VISIBLE_ROWS_PER_PAGE = 10;
+export const DEFAULT_VISIBLE_ROWS_PER_PAGE = 25;
 
 export const LOCAL_STORAGE_PAGE_SIZE_FINDINGS_KEY = 'cloudPosture:findings:pageSize';
 export const LOCAL_STORAGE_PAGE_SIZE_BENCHMARK_KEY = 'cloudPosture:benchmark:pageSize';
