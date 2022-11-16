958725256f11a1d2174a5e5cb5b11e7fcba5238f
Davis Plumlee <davis.plumlee@elastic.co>
2022-11-16 01:29:56 -0500

    adds expires label to exception list

--- packages/kbn-securitysolution-exception-list-components/src/exception_item_card/meta/details_info/index.tsx
+++ packages/kbn-securitysolution-exception-list-components/src/exception_item_card/meta/details_info/index.tsx
@@ -15,7 +15,7 @@ import * as i18n from '../../translations';
 interface MetaInfoDetailsProps {
   label: string;
   lastUpdate: JSX.Element | string;
-  lastUpdateValue: string;
+  lastUpdateValue?: string;
   dataTestSubj?: string;
 }
 
@@ -42,20 +42,24 @@ export const MetaInfoDetails = memo<MetaInfoDetailsProps>(
             {lastUpdate}
           </EuiBadge>
         </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiText size="xs" css={euiBadgeFontFamily}>
-            {i18n.EXCEPTION_ITEM_CARD_META_BY}
-          </EuiText>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false} data-test-subj={`${dataTestSubj || ''}lastUpdateValue`}>
-          <EuiFlexGroup responsive gutterSize="xs" alignItems="center">
+        {lastUpdateValue != null && (
+          <>
             <EuiFlexItem grow={false}>
-              <EuiBadge color="hollow" css={euiBadgeFontFamily}>
-                {lastUpdateValue}
-              </EuiBadge>
+              <EuiText size="xs" css={euiBadgeFontFamily}>
+                {i18n.EXCEPTION_ITEM_CARD_META_BY}
+              </EuiText>
             </EuiFlexItem>
-          </EuiFlexGroup>
-        </EuiFlexItem>
+            <EuiFlexItem grow={false} data-test-subj={`${dataTestSubj || ''}lastUpdateValue`}>
+              <EuiFlexGroup responsive gutterSize="xs" alignItems="center">
+                <EuiFlexItem grow={false}>
+                  <EuiBadge color="hollow" css={euiBadgeFontFamily}>
+                    {lastUpdateValue}
+                  </EuiBadge>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          </>
+        )}
       </EuiFlexGroup>
     );
   }
