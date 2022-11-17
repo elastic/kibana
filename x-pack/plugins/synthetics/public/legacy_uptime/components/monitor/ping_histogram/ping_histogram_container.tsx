7a0d41554bb4d9d06fff9960731c6b6020916dfa
shahzad31 <shahzad.muhammad@elastic.co>
2022-11-17 13:49:23 +0100

    type

--- x-pack/plugins/synthetics/public/legacy_uptime/components/monitor/ping_histogram/ping_histogram_container.tsx
+++ x-pack/plugins/synthetics/public/legacy_uptime/components/monitor/ping_histogram/ping_histogram_container.tsx
@@ -7,7 +7,7 @@
 
 import React, { useContext, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useTimeZone } from '@kbn/observability-plugin/public/hooks/use_time_zone';
+import { useTimeZone } from '@kbn/observability-plugin/public';
 import { PingHistogramComponent } from '../../common/charts';
 import { getPingHistogram } from '../../../state/actions';
 import { esKuerySelector, selectPingHistogram } from '../../../state/selectors';
