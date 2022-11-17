fd6d5bedf41592ea3c4eee0fe138d2e7e7f11ac4
Marshall Main <55718608+marshallmain@users.noreply.github.com>
2022-11-17 13:02:27 -0800

    [Security Solution][Alerts] Unskip ML rule test suite (#145503)
    ## Summary
    
    Fixes https://github.com/elastic/kibana/issues/142993
    
    The failure is in ES archiver, not the test, and there's seemingly
    nothing wrong with the ES archive.
    
    Passed 100x here:
    https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/1564

--- x-pack/test/detection_engine_api_integration/security_and_spaces/rule_execution_logic/machine_learning.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/rule_execution_logic/machine_learning.ts
@@ -64,8 +64,7 @@ export default ({ getService }: FtrProviderContext) => {
     rule_id: 'ml-rule-id',
   };
 
-  // FLAKY: https://github.com/elastic/kibana/issues/142993
-  describe.skip('Machine learning type rules', () => {
+  describe('Machine learning type rules', () => {
     before(async () => {
       // Order is critical here: auditbeat data must be loaded before attempting to start the ML job,
       // as the job looks for certain indices on start
