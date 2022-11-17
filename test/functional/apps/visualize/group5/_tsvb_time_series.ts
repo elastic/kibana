6dc9110ab9f19b7db4030d3d0771d1334b31a42e
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-11-17 10:41:31 -0500

    skip failing test suite (#145560)

--- test/functional/apps/visualize/group5/_tsvb_time_series.ts
+++ test/functional/apps/visualize/group5/_tsvb_time_series.ts
@@ -29,6 +29,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const kibanaServer = getService('kibanaServer');
 
   // Failing: See https://github.com/elastic/kibana/issues/145563
+  // Failing: See https://github.com/elastic/kibana/issues/145560
   describe.skip('visual builder', function describeIndexTests() {
     before(async () => {
       await security.testUser.setRoles([
