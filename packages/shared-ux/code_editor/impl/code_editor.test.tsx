dbac5354854fa56cdf3e016247bf385ee6549b72
rshen91 <rshen@elastic.co>
2022-11-18 14:52:46 -0700

    test update

--- packages/shared-ux/code_editor/impl/code_editor.test.tsx
+++ packages/shared-ux/code_editor/impl/code_editor.test.tsx
@@ -5,3 +5,53 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
+
+import { CodeEditor } from './code_editor';
+import { mountWithIntl } from '@kbn/test-jest-helpers';
+import React from 'react';
+import { monaco } from '@kbn/monaco';
+
+// A sample language definition with a few example tokens
+const simpleLogLang: monaco.languages.IMonarchLanguage = {
+  tokenizer: {
+    root: [
+      [/\[error.*/, 'constant'],
+      [/\[notice.*/, 'variable'],
+      [/\[info.*/, 'string'],
+      [/\[[a-zA-Z 0-9:]+\]/, 'tag'],
+    ],
+  },
+};
+
+describe('<CodeEditor />', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+    window.ResizeObserver = class ResizeObserver {
+      observe() {}
+      unobserve() {}
+      disconnect() {}
+    };
+
+    monaco.languages.register({ id: 'loglang' });
+    monaco.languages.setMonarchTokensProvider('loglang', simpleLogLang);
+  });
+  test('is rendered', () => {
+    const component = mountWithIntl(
+      <CodeEditor languageId="loglang" height={250} value="test" onChange={() => {}} />
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
--- src/plugins/kibana_react/public/code_editor/code_editor.tsx
+++ src/plugins/kibana_react/public/code_editor/code_editor.tsx
@@ -426,11 +426,7 @@ export const CodeEditor: React.FC<Props> = ({
   const { CopyButton } = useCopy({ isCopyable, value });
 
   return (
-    <div
-      className="kibanaCodeEditor"
-      onKeyDown={onKeyDown}
-      style={{ backgroundColor: 'yellowgreen' }}
-    >
+    <div className="kibanaCodeEditor" onKeyDown={onKeyDown}>
       {renderPrompt()}
 
       <FullScreenDisplay>
