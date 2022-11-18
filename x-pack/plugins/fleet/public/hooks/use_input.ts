9ac2a8f1f9301ea2872e3cff9f7f6b9158a2aac4
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2022-11-18 10:01:20 -0500

    Fix types

--- x-pack/plugins/fleet/public/hooks/use_input.ts
+++ x-pack/plugins/fleet/public/hooks/use_input.ts
@@ -9,6 +9,18 @@ import { useState, useCallback, useEffect } from 'react';
 import type React from 'react';
 import type { EuiSwitchEvent } from '@elastic/eui';
 
+export interface FormInput {
+  validate: () => boolean;
+}
+
+export function validateInputs(inputs: { [k: string]: FormInput }) {
+  return Object.values(inputs).reduce((acc, input) => {
+    const res = input.validate();
+
+    return acc === false ? acc : res;
+  }, true);
+}
+
 export function useInput(
   defaultValue = '',
   validate?: (value: string) => string[] | undefined,
