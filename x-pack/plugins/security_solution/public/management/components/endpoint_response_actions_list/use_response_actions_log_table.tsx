d5f93d327e153105f57dabe1d981ee8ce3404527
Constance Chen <constance.chen@elastic.co>
2022-11-18 09:10:32 -0800

    [Security] EuiDescriptionList tweaks
    - per request

--- x-pack/plugins/security_solution/public/management/components/endpoint_response_actions_list/use_response_actions_log_table.tsx
+++ x-pack/plugins/security_solution/public/management/components/endpoint_response_actions_list/use_response_actions_log_table.tsx
@@ -50,11 +50,11 @@ const customDescriptionListCss = css`
     > .euiDescriptionList__title {
       color: ${(props) => props.theme.eui.euiColorDarkShade};
       font-size: ${(props) => props.theme.eui.euiFontSizeXS};
-      margin-top: ${(props) => props.theme.eui.euiSizeS};
     }
 
+    > .euiDescriptionList__title,
     > .euiDescriptionList__description {
-      font-weight: ${(props) => props.theme.eui.euiFontWeightSemiBold};
+      font-weight: ${(props) => props.theme.eui.euiFontWeightRegular};
       margin-top: ${(props) => props.theme.eui.euiSizeS};
     }
   }
