312028dca26f49788a70b09153d716b7932ac5f3
Rodney Norris <rodney.norris@elastic.co>
2022-11-18 10:25:00 -0600

    refactor: code review feedback

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/target_field_help_text.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/target_field_help_text.tsx
@@ -10,6 +10,7 @@ import React from 'react';
 import { EuiText } from '@elastic/eui';
 
 import { i18n } from '@kbn/i18n';
+import { FormattedMessage, FormattedNumber } from '@kbn/i18n-react';
 
 import {
   getMlModelTypesForModelConfig,
@@ -29,49 +30,42 @@ export const TargetFieldHelpText: React.FC<TargetFieldHelpTextProps> = ({
   targetField,
   model,
 }) => {
-  const baseText =
-    targetField.length > 0
-      ? i18n.translate(
-          'xpack.enterpriseSearch.content.indices.pipelines.addInferencePipelineModal.steps.configure.targetField.helpText.userProvided',
-          {
-            defaultMessage:
-              'This names the field that holds the inference result. It will be prefixed with "ml.inference", ml.inference.{targetField}',
-            values: {
-              targetField,
-            },
-          }
-        )
-      : i18n.translate(
-          'xpack.enterpriseSearch.content.indices.pipelines.addInferencePipelineModal.steps.configure.targetField.helpText.default',
-          {
-            defaultMessage:
-              'This names the field that holds the inference result. It will be prefixed with "ml.inference", if not set it will be defaulted to "ml.inference.{pipelineName}"',
-            values: {
-              pipelineName: pipelineName.length > 0 ? pipelineName : '<Pipeline Name>',
-            },
-          }
-        );
+  const baseText = targetField
+    ? i18n.translate(
+        'xpack.enterpriseSearch.content.indices.pipelines.addInferencePipelineModal.steps.configure.targetField.helpText.userProvided',
+        {
+          defaultMessage:
+            'This names the field that holds the inference result. It will be prefixed with "ml.inference", ml.inference.{targetField}',
+          values: {
+            targetField,
+          },
+        }
+      )
+    : i18n.translate(
+        'xpack.enterpriseSearch.content.indices.pipelines.addInferencePipelineModal.steps.configure.targetField.helpText.default',
+        {
+          defaultMessage:
+            'This names the field that holds the inference result. It will be prefixed with "ml.inference", if not set it will be defaulted to "ml.inference.{pipelineName}"',
+          values: {
+            pipelineName: pipelineName || '<Pipeline Name>',
+          },
+        }
+      );
+  const fieldName = targetField || pipelineName || '<Pipeline Name>';
   const modelType = model ? getMLType(getMlModelTypesForModelConfig(model)) : '';
   if (modelType === SUPPORTED_PYTORCH_TASKS.TEXT_CLASSIFICATION) {
     return (
       <EuiText size="xs">
         <p>{baseText}</p>
         <p>
-          {i18n.translate(
-            'xpack.enterpriseSearch.content.indices.pipelines.addInferencePipelineModal.steps.configure.targetField.helpText.textClassificationModel',
-            {
-              defaultMessage:
-                'Additionally the predicted_value will be copied to "{fieldName}", if the prediction_probability is greater than 0.5.',
-              values: {
-                fieldName:
-                  targetField.length > 0
-                    ? targetField
-                    : pipelineName.length > 0
-                    ? pipelineName
-                    : '<Pipeline Name>',
-              },
-            }
-          )}
+          <FormattedMessage
+            id="xpack.enterpriseSearch.content.indices.pipelines.addInferencePipelineModal.steps.configure.targetField.helpText.textClassificationModel"
+            defaultMessage='Additionally the predicted_value will be copied to "{fieldName}", if the prediction_probability is greater than {probabilityThreshold}'
+            values={{
+              fieldName,
+              probabilityThreshold: <FormattedNumber value={0.5} />,
+            }}
+          />
         </p>
       </EuiText>
     );
@@ -86,12 +80,7 @@ export const TargetFieldHelpText: React.FC<TargetFieldHelpTextProps> = ({
             {
               defaultMessage: 'Additionally the predicted_value will be copied to "{fieldName}"',
               values: {
-                fieldName:
-                  targetField.length > 0
-                    ? targetField
-                    : pipelineName.length > 0
-                    ? pipelineName
-                    : '<Pipeline Name>',
+                fieldName,
               },
             }
           )}
