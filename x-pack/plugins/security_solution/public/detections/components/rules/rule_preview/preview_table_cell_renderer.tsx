9be31fc3b9249ed61a576b479700e246fb0382de
Marshall Main <marshall.main@elastic.co>
2022-11-18 13:40:13 -0800

    Remove dead code

--- x-pack/plugins/security_solution/public/detections/components/rules/rule_preview/preview_table_cell_renderer.tsx
+++ x-pack/plugins/security_solution/public/detections/components/rules/rule_preview/preview_table_cell_renderer.tsx
@@ -5,65 +5,11 @@
  * 2.0.
  */
 
-import React, { useMemo } from 'react';
+import type React from 'react';
 import type { EuiDataGridCellValueElementProps } from '@elastic/eui';
 import type { CellValueElementProps } from '@kbn/timelines-plugin/common';
-import { StyledContent } from '../../../../common/lib/cell_actions/expanded_cell_value_actions';
-import { getLinkColumnDefinition } from '../../../../common/lib/cell_actions/helpers';
-import { useGetMappedNonEcsValue } from '../../../../timelines/components/timeline/body/data_driven_columns';
-import { columnRenderers } from '../../../../timelines/components/timeline/body/renderers';
-import { getColumnRenderer } from '../../../../timelines/components/timeline/body/renderers/get_column_renderer';
 import { RenderCellValue } from '../../../configurations/security_solution_detections';
 
 export const PreviewRenderCellValue: React.FC<
   EuiDataGridCellValueElementProps & CellValueElementProps
 > = (props) => RenderCellValue({ ...props, enableActions: false });
-
-export const PreviewTableCellRenderer: React.FC<CellValueElementProps> = ({
-  data,
-  ecsData,
-  eventId,
-  header,
-  isDetails,
-  isDraggable,
-  isTimeline,
-  linkValues,
-  rowRenderers,
-  scopeId,
-  truncate,
-  key,
-}) => {
-  const asPlainText = useMemo(() => {
-    return getLinkColumnDefinition(header.id, header.type, undefined) !== undefined && !isTimeline;
-  }, [header.id, header.type, isTimeline]);
-
-  const values = useGetMappedNonEcsValue({
-    data,
-    fieldName: header.id,
-  });
-  const styledContentClassName = isDetails
-    ? 'eui-textBreakWord'
-    : 'eui-displayInlineBlock eui-textTruncate';
-
-  return (
-    <>
-      <StyledContent className={styledContentClassName} $isDetails={isDetails}>
-        {getColumnRenderer(header.id, columnRenderers, data).renderColumn({
-          asPlainText,
-          columnName: header.id,
-          ecsData,
-          eventId,
-          field: header,
-          isDetails,
-          isDraggable,
-          linkValues,
-          rowRenderers,
-          scopeId,
-          truncate,
-          values,
-          key,
-        })}
-      </StyledContent>
-    </>
-  );
-};
