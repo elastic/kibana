c36a966df36bba299431995119b7bcd40f754913
Dmitrii <dmitrii.shevchenko@elastic.co>
2022-11-21 15:04:04 +0100

    Restrict direct Route imports from `react-router-dom`

--- .eslintrc.js
+++ .eslintrc.js
@@ -251,6 +251,11 @@ const RESTRICTED_IMPORTS = [
     name: 'react-use',
     message: 'Please use react-use/lib/{method} instead.',
   },
+  {
+    name: 'react-router-dom',
+    importNames: ['Route'],
+    message: "import { Route } from '@kbn/kibana-react-plugin/public'",
+  },
 ];
 
 module.exports = {
@@ -1004,13 +1009,6 @@ module.exports = {
             // to help deprecation and prevent accidental re-use/continued use of code we plan on removing. If you are
             // finding yourself turning this off a lot for "new code" consider renaming the file and functions if it is has valid uses.
             patterns: ['*legacy*'],
-            paths: [
-              {
-                name: 'react-router-dom',
-                importNames: ['Route'],
-                message: "import { Route } from '@kbn/kibana-react-plugin/public'",
-              },
-            ],
           },
         ],
       },
