9f700e56433f4e086a7e0b2dfab9c82c56eb9a87
David Sánchez <david.sanchezsoler@elastic.co>
2022-11-21 13:36:19 +0100

    [Security Solution][Endpoint] Hide/show add endpoint event filters action from an endpoint event depending on RBAC privileges (#144989)
    ## Summary
    
    Hides/shows `Add endpoint event filter` option in event context menu and
    event details flyout depending on the RBAC privileges.
    
    
    ### With `all` event filters privileges
    
    ![event filters with
    privileges](https://user-images.githubusercontent.com/15727784/201128159-51058de9-1cdc-4bc1-8e1d-70f80678239a.gif)
    
    
    
    ### With `read` and `none` event filters privileges
    
    ![event filters no
    privileges](https://user-images.githubusercontent.com/15727784/201128157-ff719001-9599-492c-a650-93ead81e1e06.gif)
    
    
    
    
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/timelines/components/timeline/body/actions/index.test.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/body/actions/index.test.tsx
@@ -19,8 +19,11 @@ import {
   GuidedOnboardingTourStep,
   SecurityTourStep,
 } from '../../../../../common/components/guided_onboarding_tour/tour_step';
+import { initialUserPrivilegesState as mockInitialUserPrivilegesState } from '../../../../../common/components/user_privileges/user_privileges_context';
+import { useUserPrivileges } from '../../../../../common/components/user_privileges';
 import { SecurityStepId } from '../../../../../common/components/guided_onboarding_tour/tour_config';
 
+jest.mock('../../../../../common/components/user_privileges');
 jest.mock('../../../../../common/components/guided_onboarding_tour');
 jest.mock('../../../../../detections/components/user_info', () => ({
   useUserData: jest.fn().mockReturnValue([{ canUserCRUD: true, hasIndexWrite: true }]),
@@ -268,6 +271,12 @@ describe('Actions', () => {
   });
 
   describe('Alert context menu enabled?', () => {
+    beforeEach(() => {
+      (useUserPrivileges as jest.Mock).mockReturnValue({
+        ...mockInitialUserPrivilegesState(),
+        endpointPrivileges: { loading: false, canWriteEventFilters: true },
+      });
+    });
     test('it disables for eventType=raw', () => {
       const wrapper = mount(
         <TestProviders>
