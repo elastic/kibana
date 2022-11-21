f5f168b0c328856117e0c3e34af589c694834ce4
Jean-Louis Leysens <jloleysens@gmail.com>
2022-11-21 12:38:42 +0100

    added comment about text overflow issue

--- src/plugins/files/public/components/file_picker/components/file_card.tsx
+++ src/plugins/files/public/components/file_picker/components/file_card.tsx
@@ -37,7 +37,7 @@ export const FileCard: FunctionComponent<Props> = ({ file }) => {
       css={css`
         place-self: stretch;
         > * {
-          // TODO: Once content no longer overflows card, remove this style
+          // TODO: Once content no longer overflows card, remove once on @elastic/eui ^70.3.0
           width: 100%;
         }
       `}
