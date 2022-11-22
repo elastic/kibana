5edcac687bb2c95c447217d2da5a17fb8b103b41
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2022-11-22 15:49:51 +0100

    [`NOTICE.txt`] Fix notices for Gainsight and FullStory

--- x-pack/plugins/cloud_integrations/cloud_full_story/server/assets/fullstory_library.js
+++ x-pack/plugins/cloud_integrations/cloud_full_story/server/assets/fullstory_library.js
@@ -1,10 +1,3 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
 /* @notice
  * Portions of this code are licensed under the following license:
  * For license information please see https://edge.fullstory.com/s/fs.js.LICENSE.txt
