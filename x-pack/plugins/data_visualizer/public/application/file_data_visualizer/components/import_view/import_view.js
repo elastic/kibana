1f44d6b5bae7098da85cc87c06c87e2080b1c92f
James Gowdy <jgowdy@elastic.co>
2022-11-23 09:39:25 +0000

    using isPopulatedObject

--- x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/import_view/import_view.js
+++ x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/import_view/import_view.js
@@ -20,6 +20,7 @@ import {
 
 import { i18n } from '@kbn/i18n';
 import { debounce } from 'lodash';
+import { isPopulatedObject } from '@kbn/ml-is-populated-object';
 import { ResultsLinks } from '../../../common/components/results_links';
 import { FilebeatConfigFlyout } from '../../../common/components/filebeat_config_flyout';
 import { ImportProgress, IMPORT_STATUS } from '../import_progress';
@@ -206,7 +207,10 @@ export class ImportView extends Component {
                 // mappings, use this field as the time field.
                 // This relies on the field being populated by
                 // the ingest pipeline on ingest
-                if (mappings.properties && mappings.properties[DEFAULT_TIME_FIELD] !== undefined) {
+                if (
+                  isPopulatedObject(mappings.properties) &&
+                  mappings.properties[DEFAULT_TIME_FIELD] !== undefined
+                ) {
                   timeFieldName = DEFAULT_TIME_FIELD;
                   this.setState({ timeFieldName });
                 }
