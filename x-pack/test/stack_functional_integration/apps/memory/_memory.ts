739fc3e9e6ef1b526ca6adc5178863fd84d0fa59
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2022-11-23 17:54:34 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/test/stack_functional_integration/apps/memory/_memory.ts
+++ x-pack/test/stack_functional_integration/apps/memory/_memory.ts
@@ -15,39 +15,33 @@ export default function ({
   const browser = getService('browser');
   const renderable = getService('renderable');
 
-
   describe('setup dashboard for memory test', function () {
     before(async function () {
       this.timeout(0);
-      await PageObjects.common.navigateToActualUrl('home', '/tutorial_directory/sampleData', {
-      });
+      await PageObjects.common.navigateToActualUrl('home', '/tutorial_directory/sampleData', {});
       await PageObjects.header.waitUntilLoadingHasFinished();
       await PageObjects.common.dismissBanner();
       await browser.refresh();
 
-      await PageObjects.home.launchSampleDashboard("flights");
+      await PageObjects.home.launchSampleDashboard('flights');
       await PageObjects.header.waitUntilLoadingHasFinished();
       await renderable.waitForRender();
       await PageObjects.timePicker.startAutoRefresh();
       await PageObjects.common.sleep(3000000);
 
-      await PageObjects.common.navigateToActualUrl('home', '/tutorial_directory/sampleData', {
-      });
-      await PageObjects.home.launchSampleDashboard("logs");
+      await PageObjects.common.navigateToActualUrl('home', '/tutorial_directory/sampleData', {});
+      await PageObjects.home.launchSampleDashboard('logs');
       await PageObjects.header.waitUntilLoadingHasFinished();
       await renderable.waitForRender();
       await PageObjects.timePicker.startAutoRefresh();
       await PageObjects.common.sleep(3000000);
 
-
-      await PageObjects.common.navigateToActualUrl('home', '/tutorial_directory/sampleData', {
-      });
-      await PageObjects.home.launchSampleDashboard("ecommerce");
+      await PageObjects.common.navigateToActualUrl('home', '/tutorial_directory/sampleData', {});
+      await PageObjects.home.launchSampleDashboard('ecommerce');
       await PageObjects.header.waitUntilLoadingHasFinished();
       await renderable.waitForRender();
       await PageObjects.timePicker.startAutoRefresh();
       await PageObjects.common.sleep(3000000);
-
     });
     it('dashboard auto refresh finished successfully', async function () {
       expect(0).to.be.lessThan(1);
