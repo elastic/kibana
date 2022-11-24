04d3038749d7f388348eab76247d7715ddf2000a
Liza Katz <lizka.k@gmail.com>
2022-11-24 11:53:33 +0200

    Update journey_ftr_config.ts

--- packages/kbn-journeys/journey/journey_ftr_config.ts
+++ packages/kbn-journeys/journey/journey_ftr_config.ts
@@ -82,7 +82,7 @@ export function makeFtrConfigProvider(
       kbnTestServer: {
         ...baseConfig.kbnTestServer,
         // delay shutdown by 2 seconds to ensure that APM can report the data it collects during test execution
-        delayShutdown: process.env.TEST_PERFORMANCE_PHASE === 'TEST' ? 2000 : 0,
+        delayShutdown: process.env.TEST_PERFORMANCE_PHASE === 'TEST' ? 15_000 : 0,
 
         serverArgs: [
           ...baseConfig.kbnTestServer.serverArgs,
