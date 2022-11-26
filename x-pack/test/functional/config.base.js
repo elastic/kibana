0aab44bd779f06edf1cfd35f1c7c30bfdbf12753
Bamieh <ahmad.bamyeh@elastic.co>
2022-11-26 14:32:59 +0200

    fix typo in comment

--- x-pack/test/functional/config.base.js
+++ x-pack/test/functional/config.base.js
@@ -39,7 +39,7 @@ export default async function ({ readConfigFile }) {
       serverArgs: [
         'path.repo=/tmp/',
         'xpack.security.authc.api_key.enabled=true',
-        'cluster.routing.allocation.disk.threshold_enabled=true', // make sure disk thresholds are enabeld for UA cluster testing
+        'cluster.routing.allocation.disk.threshold_enabled=true', // make sure disk thresholds are enabled for UA cluster testing
       ],
     },
 
