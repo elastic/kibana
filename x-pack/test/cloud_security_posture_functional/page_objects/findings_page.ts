bffd1450eaa84a82ad7e7051575209d71108b631
Or Ouziel <or.ouziel@elastic.co>
2022-11-27 20:58:44 +0200

    remove pagination and clean

--- x-pack/test/cloud_security_posture_functional/page_objects/findings_page.ts
+++ x-pack/test/cloud_security_posture_functional/page_objects/findings_page.ts
@@ -8,16 +8,12 @@
 import expect from '@kbn/expect';
 import type { FtrProviderContext } from '../ftr_provider_context';
 
-// Defined in EUI
-const getEuiPaginationText = (size: 10 | 25) => `Rows per page: ${size}`;
-const getEuiPaginationRowTestId = (size: 10 | 25) => `tablePagination-${size}-rows`;
-const getEuiPaginationButtonTestId = (index: number) => `pagination-button-${index}`;
-const EUI_PAGINATION_BUTTON = 'tablePaginationPopoverButton';
-
 // Defined in CSP plugin
+const STATUS_API_PATH = '/internal/cloud_security_posture/status?check=init';
+const FINDINGS_INDEX = 'logs-cloud_security_posture.findings_latest-default';
 const FINDINGS_ROUTE = 'cloud_security_posture/findings';
 const FINDINGS_TABLE_TESTID = 'findings_table';
-const getCellSelector = (columnIndex: number) =>
+const getFilterValueSelector = (columnIndex: number) =>
   `tbody tr td:nth-child(${columnIndex + 1}) div[data-test-subj="filter_cell_value"]`;
 
 // Defined in Security Solution plugin
@@ -26,26 +22,94 @@ const SECURITY_SOLUTION_APP_NAME = 'securitySolution';
 export function FindingsPageProvider({ getService, getPageObjects }: FtrProviderContext) {
   const testSubjects = getService('testSubjects');
   const PageObjects = getPageObjects(['common']);
+  const retry = getService('retry');
+  const es = getService('es');
+  const supertest = getService('supertest');
+  const log = getService('log');
 
-  const getColumnIndex = async (columnName: string) => {
-    const table = await testSubjects.find(FINDINGS_TABLE_TESTID);
-    const headers = await table.findAllByCssSelector('thead tr :is(th,td)');
-    const headerIndexes = await Promise.all(headers.map((header) => header.getVisibleText()));
-    const columnIndex = headerIndexes.findIndex((i) => i === columnName);
+  /**
+   * required before indexing findings
+   */
+  const waitForPluginInitialized = (): Promise<void> =>
+    retry.try(async () => {
+      log.debug('Check CSP plugin is initialized');
+      const response = await supertest.get(STATUS_API_PATH).expect(200);
+      expect(response.body).to.eql({ isPluginInitialized: true });
+      log.debug('CSP plugin is initialized');
+    });
 
-    expect(columnIndex).to.be.greaterThan(-1);
-
-    return [columnIndex, headers[columnIndex]] as [
-      number,
-      Awaited<ReturnType<typeof testSubjects.find>>
-    ];
+  const index = {
+    remove: () => es.indices.delete({ index: FINDINGS_INDEX, ignore_unavailable: true }),
+    add: async <T>(findingsMock: T[]) => {
+      await waitForPluginInitialized();
+      await Promise.all(
+        findingsMock.map((finding) =>
+          es.index({
+            index: FINDINGS_INDEX,
+            body: finding,
+          })
+        )
+      );
+    },
   };
 
-  const getFilterColumnValues = async (columnName: string) => {
-    const table = await testSubjects.find(FINDINGS_TABLE_TESTID);
-    const [columnIndex] = await getColumnIndex(columnName);
-    const columnCells = await table.findAllByCssSelector(getCellSelector(columnIndex));
-    return await Promise.all(columnCells.map((h) => h.getVisibleText()));
+  const table = {
+    getTableElement: () => testSubjects.find(FINDINGS_TABLE_TESTID),
+
+    getColumnIndex: async (columnName: string) => {
+      const tableElement = await table.getTableElement();
+      const headers = await tableElement.findAllByCssSelector('thead tr :is(th,td)');
+      const headerIndexes = await Promise.all(headers.map((header) => header.getVisibleText()));
+      const columnIndex = headerIndexes.findIndex((i) => i === columnName);
+      expect(columnIndex).to.be.greaterThan(-1);
+      return [columnIndex, headers[columnIndex]] as [
+        number,
+        Awaited<ReturnType<typeof testSubjects.find>>
+      ];
+    },
+
+    getFilterColumnValues: async (columnName: string) => {
+      const tableElement = await table.getTableElement();
+      const [columnIndex] = await table.getColumnIndex(columnName);
+      const columnCells = await tableElement.findAllByCssSelector(
+        getFilterValueSelector(columnIndex)
+      );
+
+      return await Promise.all(columnCells.map((h) => h.getVisibleText()));
+    },
+
+    assertColumnSorting: async (columnName: string, direction: 'asc' | 'desc') => {
+      const values = (await table.getFilterColumnValues(columnName)).filter(Boolean);
+      expect(values).to.not.be.empty();
+      const sorted = values
+        .slice()
+        .sort((a, b) => (direction === 'asc' ? a.localeCompare(b) : b.localeCompare(a)));
+      values.every((value, i) => expect(value).to.be(sorted[i]));
+    },
+
+    toggleColumnSorting: async (columnName: string, direction: 'asc' | 'desc') => {
+      const getColumnElement = async () => (await table.getColumnIndex(columnName))[1];
+      const element = await getColumnElement();
+      const currentSort = await element.getAttribute('aria-sort');
+      if (currentSort === 'none') {
+        // a click is needed to focus on Eui column header
+        await element.click();
+
+        // default is ascending
+        if (direction === 'desc') {
+          const nonStaleElement = await getColumnElement();
+          await nonStaleElement.click();
+        }
+      }
+      if (
+        (currentSort === 'ascending' && direction === 'desc') ||
+        (currentSort === 'descending' && direction === 'asc')
+      ) {
+        // Without getting the element again, the click throws an error (stale element reference)
+        const nonStaleElement = await getColumnElement();
+        await nonStaleElement.click();
+      }
+    },
   };
 
   const navigateToFindingsPage = async () => {
@@ -54,74 +118,9 @@ export function FindingsPageProvider({ getService, getPageObjects }: FtrProvider
     });
   };
 
-  const goToPageIndex = async (index: number) => {
-    await testSubjects.click(getEuiPaginationButtonTestId(index));
-  };
-
-  const assertPageIndex = async (index: number) => {
-    const isSelected = await testSubjects.getAttribute(
-      getEuiPaginationButtonTestId(index),
-      'aria-current'
-    );
-    expect(isSelected).to.be('true');
-  };
-
-  const changePageSize = async (size: 10 | 25) => {
-    await testSubjects.click(EUI_PAGINATION_BUTTON);
-    await testSubjects.click(getEuiPaginationRowTestId(size));
-  };
-
-  const assertPageSize = async (size: 10 | 25) => {
-    const text = await testSubjects.getVisibleText(EUI_PAGINATION_BUTTON);
-    expect(text).to.be(getEuiPaginationText(size));
-  };
-
-  const assertColumnSorting = async (columnName: string, direction: 'asc' | 'desc') => {
-    const values = (await getFilterColumnValues(columnName)).filter(Boolean);
-
-    expect(values).to.not.be.empty();
-
-    const sorted = values
-      .slice()
-      .sort((a, b) => (direction === 'asc' ? a.localeCompare(b) : b.localeCompare(a)));
-
-    values.every((value, i) => expect(value).to.be(sorted[i]));
-  };
-
-  const toggleColumnSorting = async (columnName: string, direction: 'asc' | 'desc') => {
-    const [_, element] = await getColumnIndex(columnName);
-
-    const currentSort = await element.getAttribute('aria-sort');
-
-    if (currentSort === 'none') {
-      // a click is needed to focus on Eui column header
-      await element.click();
-
-      // default is ascending
-      if (direction === 'desc') {
-        const [, nonStaleElement] = await getColumnIndex(columnName);
-        await nonStaleElement.click();
-      }
-    }
-
-    if (
-      (currentSort === 'ascending' && direction === 'desc') ||
-      (currentSort === 'descending' && direction === 'asc')
-    ) {
-      // Without getting the element again, the click throws an error (stale element reference)
-      const [, nonStaleElement] = await getColumnIndex(columnName);
-
-      await nonStaleElement.click();
-    }
-  };
-
   return {
-    assertColumnSorting,
-    assertPageIndex,
-    goToPageIndex,
-    assertPageSize,
-    changePageSize,
     navigateToFindingsPage,
-    toggleColumnSorting,
+    table,
+    index,
   };
 }
--- x-pack/test/cloud_security_posture_functional/pages/findings.ts
+++ x-pack/test/cloud_security_posture_functional/pages/findings.ts
@@ -5,96 +5,44 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect';
 import type { FtrProviderContext } from '../ftr_provider_context';
-
-const FINDINGS_INDEX = 'logs-cloud_security_posture.findings_latest-default';
 
 // eslint-disable-next-line import/no-default-export
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const pageObjects = getPageObjects(['common', 'findings']);
-  const retry = getService('retry');
-  const es = getService('es');
-  const supertest = getService('supertest');
-  const log = getService('log');
-
-  /**
-   * This is required prior to indexing findings
-   */
-  const waitForPluginInitialized = (): Promise<void> =>
-    retry.try(async () => {
-      log.debug('Check CSP plugin is initialized');
-
-      const response = await supertest
-        .get('/internal/cloud_security_posture/status?check=init')
-        .expect(200);
-
-      expect(response.body).to.eql({ isPluginInitialized: true });
-
-      log.debug('CSP plugin is initialized');
-    });
-
-  const addFindingsIndex = async () => {
-    await waitForPluginInitialized();
-    await Promise.all(
-      Array.from({ length: 11 }, (_, id) => {
-        return es.index({
-          index: FINDINGS_INDEX,
-          body: {
-            resource: { id, name: `Resource ${id}` },
-            result: { evaluation: 'passed' },
-            rule: {
-              name: `Rule ${id}`,
-              section: 'Kubelet',
-              tags: ['Kubernetes'],
-              type: 'process',
-            },
-          },
-        });
-      })
-    );
-  };
-
-  const removeFindingsIndex = () =>
-    es.indices.delete({ index: FINDINGS_INDEX, ignore_unavailable: true });
+  const FINDINGS_SIZE = 2;
+  const findingsMock = Array.from({ length: FINDINGS_SIZE }, (_, id) => {
+    return {
+      resource: { id, name: `Resource ${id}` },
+      result: { evaluation: 'passed' },
+      rule: {
+        name: `Rule ${id}`,
+        section: 'Kubelet',
+        tags: ['Kubernetes'],
+        type: 'process',
+      },
+    };
+  });
 
   describe('Findings Page', () => {
     before(async () => {
-      await addFindingsIndex();
+      await pageObjects.findings.index.add(findingsMock);
       await pageObjects.findings.navigateToFindingsPage();
     });
 
     after(async () => {
-      await removeFindingsIndex();
+      await pageObjects.findings.index.remove();
     });
 
-    describe('Sorting', () => {
+    describe('Sort', () => {
       it('Sorts by rule name', async () => {
-        await pageObjects.findings.toggleColumnSorting('Rule', 'asc');
-        await pageObjects.findings.assertColumnSorting('Rule', 'asc');
+        await pageObjects.findings.table.toggleColumnSorting('Rule', 'asc');
+        await pageObjects.findings.table.assertColumnSorting('Rule', 'asc');
       });
 
       it('Sorts by resource name', async () => {
-        await pageObjects.findings.toggleColumnSorting('Resource Name', 'desc');
-        await pageObjects.findings.assertColumnSorting('Resource Name', 'desc');
-      });
-    });
-
-    describe('Pagination', () => {
-      it('Changes rows per page', async () => {
-        await pageObjects.findings.assertPageSize(25);
-        await pageObjects.findings.changePageSize(10);
-        await pageObjects.findings.assertPageSize(10);
-      });
-
-      it('Navigates to next page', async () => {
-        await pageObjects.findings.goToPageIndex(1);
-        await pageObjects.findings.assertPageIndex(1);
-      });
-
-      it('Navigates to prev page', async () => {
-        await pageObjects.findings.goToPageIndex(0);
-        await pageObjects.findings.assertPageIndex(0);
+        await pageObjects.findings.table.toggleColumnSorting('Resource Name', 'desc');
+        await pageObjects.findings.table.assertColumnSorting('Resource Name', 'desc');
       });
     });
   });
