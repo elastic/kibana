1d493ce4f0c079c7097fc422fb6aea112f798573
Kurt <kc13greiner@users.noreply.github.com>
2022-11-27 20:56:09 -0500

    plugin test fix

--- x-pack/plugins/security/server/plugin.test.ts
+++ x-pack/plugins/security/server/plugin.test.ts
@@ -141,6 +141,7 @@ describe('Security Plugin', () => {
               "grantAsInternalUser": [Function],
               "invalidate": [Function],
               "invalidateAsInternalUser": [Function],
+              "update": [Function],
             },
             "getCurrentUser": [Function],
           },
