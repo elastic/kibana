7ec40dda48adefed63686eed0b327036544184b2
Jean-Louis Leysens <jloleysens@gmail.com>
2022-11-28 13:02:53 +0100

    * as

--- packages/shared-ux/file/util/src/image_metadata.ts
+++ packages/shared-ux/file/util/src/image_metadata.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import bh from 'blurhash';
+import * as bh from 'blurhash';
 import type { FileImageMetadata } from '@kbn/shared-ux-file-types';
 
 export function isImage(file: { type?: string }): boolean {
