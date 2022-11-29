ffd243f87fc6cc11c8375434368a0b3075696a20
István Zoltán Szabó <szabosteve@gmail.com>
2022-11-29 14:57:06 +0100

    [ML] Adds test suite to index.

--- x-pack/test/screenshot_creation/apps/ml_docs/anomaly_detection/generate_anomaly_alerts.ts
+++ x-pack/test/screenshot_creation/apps/ml_docs/anomaly_detection/generate_anomaly_alerts.ts
@@ -8,7 +8,7 @@
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { Job, Datafeed } from '@kbn/ml-plugin/common/types/anomaly_detection_jobs';
 import { DATAFEED_STATE } from '@kbn/ml-plugin/common/constants/states';
-import { FtrProviderContext } from '../../ftr_provider_context';
+import { FtrProviderContext } from '../../../ftr_provider_context';
 
 import { ECOMMERCE_INDEX_PATTERN } from '..';
 
@@ -58,7 +58,7 @@ function createTestJobAndDatafeed() {
         },
       },
       query_delay: '120s',
-      indices: ['ft_ecommerce'],
+      indices: [ECOMMERCE_INDEX_PATTERN],
     } as unknown as estypes.MlDatafeed,
   };
 }
@@ -66,34 +66,60 @@ function createTestJobAndDatafeed() {
 export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const esArchiver = getService('esArchiver');
   const ml = getService('ml');
-  const pageObjects = getPageObjects(['triggersActionsUI']);
+ // const pageObjects = getPageObjects(['triggersActionsUI']);
+  const commonScreenshots = getService('commonScreenshots');
+
+  const screenshotDirectories = ['ml_docs', 'anomaly_detection']
 
   let testJobId = '';
 
   describe('anomaly detection alert', function () {
     before(async () => {
-        await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/ml/ecommerce');
-        await ml.testResources.createIndexPatternIfNeeded('ft_ecommerce', 'order_date');
-        await ml.testResources.setKibanaTimeZoneToUTC();
-  
-        await ml.securityUI.loginAsMlPowerUser();
-  
-        const { job, datafeed } = createTestJobAndDatafeed();
-  
-        testJobId = job.job_id;
+      await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/ml/ecommerce');
+      await ml.testResources.createIndexPatternIfNeeded('ft_ecommerce', 'order_date');
 
-        // Set up jobs
-        // @ts-expect-error not full interface
-        await ml.api.createAnomalyDetectionJob(job);
-        await ml.api.openAnomalyDetectionJob(job.job_id);
-        await ml.api.createDatafeed(datafeed);
-        await ml.api.startDatafeed(datafeed.datafeed_id);
-        await ml.api.waitForDatafeedState(datafeed.datafeed_id, DATAFEED_STATE.STARTED);
-        await ml.api.assertJobResultsExist(job.job_id);
-      });
+      const { job, datafeed } = createTestJobAndDatafeed();
 
-      after(async () => {
-        await ml.api.deleteAnomalyDetectionJobES(testJobId);
-        await ml.api.cleanMlIndices();
-        await ml.alerting.cleanAnomalyDetectionRules();
+      testJobId = job.job_id;
+
+      // Set up jobs
+      // @ts-expect-error not full interface
+      await ml.api.createAnomalyDetectionJob(job);
+      await ml.api.openAnomalyDetectionJob(job.job_id);
+      await ml.api.createDatafeed(datafeed);
+      await ml.api.startDatafeed(datafeed.datafeed_id);
+      await ml.api.waitForDatafeedState(datafeed.datafeed_id, DATAFEED_STATE.STARTED);
+      await ml.api.assertJobResultsExist(job.job_id);
+    });
+
+    after(async () => {
+      await ml.api.deleteAnomalyDetectionJobES(testJobId);
+      await ml.api.cleanMlIndices();
+      await ml.alerting.cleanAnomalyDetectionRules();
+    });
+
+    describe.only('overview page alert flyout controls', () => {
+      it('can create an anomaly detection alert', async () => {
+        await ml.navigation.navigateToAlertsAndAction();
+      //  await pageObjects.triggersActionsUI.clickCreateAlertButton();
+        await ml.alerting.selectAnomalyDetectionAlertType();
+        await ml.testExecution.logTestStep('should have correct default values');
+        await ml.alerting.assertSeverity(75);
+        await ml.alerting.assertPreviewButtonState(false);
+        await ml.testExecution.logTestStep('should complete the alert params');
+        await ml.alerting.selectJobs([testJobId]);
+        await ml.alerting.selectResultType('bucket');
+        await ml.alerting.setSeverity(75);
+        await ml.testExecution.logTestStep('should populate advanced settings with default values');
+        await ml.alerting.assertTopNBuckets(1);
+        await ml.alerting.assertLookbackInterval('123m');
+        await ml.testExecution.logTestStep('should preview the alert condition');
+        await ml.alerting.assertPreviewButtonState(false);
+        await ml.alerting.setTestInterval('1y');
+        await ml.alerting.assertPreviewButtonState(true);
+        await ml.testExecution.logTestStep('take screenshot');
+        await commonScreenshots.takeScreenshot('ml-anomaly-alert-severity', screenshotDirectories);
       });
+    });
+  });
+};
