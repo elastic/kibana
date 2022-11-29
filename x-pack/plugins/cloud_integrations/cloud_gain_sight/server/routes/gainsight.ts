3f6cef9d508b39b3e75ba86c633b6502cb768018
Cecilia Bollini <cecilia.bollini@elastic.co>
2022-11-29 17:01:10 +0000

    path

--- x-pack/plugins/cloud_integrations/cloud_gain_sight/server/routes/gainsight.ts
+++ x-pack/plugins/cloud_integrations/cloud_gain_sight/server/routes/gainsight.ts
@@ -174,7 +174,7 @@ export const registerGainsightProxyRoute = ({
   httpResources.register(
     {
       // Use the build number in the URL path to leverage max-age caching on production builds
-      path: `/api/guide-editor.min.js`,
+      path: `/internal/cloud/${packageInfo.buildNum}/api/guide-editor.min.js`,
       validate: false,
       options: {
         authRequired: false,
