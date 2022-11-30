de6c99fc203726dfb07945fa123a8be04c28af47
lizozom <liza.katz@elastic.co>
2022-11-30 18:20:11 +0200

    Improve warmup Add log

--- x-pack/performance/journeys/warmup.ts
+++ x-pack/performance/journeys/warmup.ts
@@ -92,9 +92,14 @@ export const journey = new Journey({
     expect((asyncSearchResponse.response.hits.total as SearchTotalHits).value).to.be(3);
     expect(asyncSearchResponse.response.aggregations?.the_date).not.to.be(undefined);
   })
-  .step('Open Kibana', async ({ page, kbnUrl }) => {
+  .step('Open Kibana and skip welcome screen', async ({ page, kbnUrl }) => {
     await page.goto(kbnUrl.get(`/app/home`));
-    await page.waitForSelector('.kbnAppWrapper');
+    const btn = await page.waitForSelector('[data-test-subj="skipWelcomeScreen"]');
+    await btn.click()
+  })
+  .step('Load homepage', async ({ page, kbnUrl }) => {
+    await page.waitForSelector('.homDataAdd'); 
+    await new Promise(f => setTimeout(f, 1000));
   })
   .step('Cleanup', async ({ esClient }) => {
     const resp = await esClient.indices.delete({
