ebb5c79e8beacbde0cdcefd39020248f18a45e8a
Paul Tavares <paul.tavares@elastic.co>
2022-11-30 17:32:50 -0500

    Show a confirmation modal when user clicks to clear input history

---
+++ x-pack/plugins/security_solution/public/management/components/console/components/command_input/components/command_input_clear_history.tsx
@@ -0,0 +1,69 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { memo, useCallback, useState } from 'react';
+import { EuiButtonEmpty, EuiConfirmModal, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { i18n } from '@kbn/i18n';
+
+export const CommandInputClearHistory = memo(() => {
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const handleClearInputHistory = useCallback(() => {
+    setShowConfirmModal(true);
+  }, []);
+
+  const handleConfirmModalCancel = useCallback(() => {
+    setShowConfirmModal(false);
+  }, []);
+
+  const handleConfirmModalConfirm = useCallback(() => {
+    //
+  }, []);
+
+  return (
+    <>
+      {showConfirmModal && (
+        <EuiConfirmModal
+          title={i18n.translate('xpack.securitySolution.commandInputClearHistory.title', {
+            defaultMessage: 'Clear input history',
+          })}
+          cancelButtonText={i18n.translate(
+            'xpack.securitySolution.commandInputClearHistory.cancelButton',
+            { defaultMessage: 'Cancel' }
+          )}
+          confirmButtonText={i18n.translate(
+            'xpack.securitySolution.commandInputClearHistory.confirmButton',
+            { defaultMessage: 'Clear' }
+          )}
+          buttonColor="danger"
+          onCancel={handleConfirmModalCancel}
+          onConfirm={handleConfirmModalConfirm}
+        >
+          {'This action cannot be undone. Are you sure you wish to continue?'}
+        </EuiConfirmModal>
+      )}
+
+      <EuiFlexGroup responsive={false} justifyContent="flexEnd" gutterSize="none">
+        <EuiFlexItem grow={false}>
+          <EuiButtonEmpty
+            size="xs"
+            tabIndex={-1}
+            onClick={handleClearInputHistory}
+            disabled={showConfirmModal}
+          >
+            <FormattedMessage
+              id="xpack.securitySolution.commandInputHistory.clearHistoryButtonLabel"
+              defaultMessage="Clear input history"
+            />
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </>
+  );
+});
+CommandInputClearHistory.displayName = 'CommandInputClearHistory';
