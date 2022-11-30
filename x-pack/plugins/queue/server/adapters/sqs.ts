ef739202fd27ef0e4359fa2aff78fe3ca9ec610f
Mike Cote <michel.cote@elastic.co>
2022-11-30 13:32:22 -0500

    100 max workers

--- x-pack/plugins/queue/server/adapters/sqs.ts
+++ x-pack/plugins/queue/server/adapters/sqs.ts
@@ -21,7 +21,7 @@ import type { PluginSetupDeps, PluginStartDeps, Job, Adapter } from '../plugin';
 let sqs: AWS.SQS;
 const workers: Record<string, Worker<unknown>> = {};
 const QUEUE_URL = 'https://sqs.us-east-2.amazonaws.com/946960629917/ResponseOps';
-const MAX_WORKERS = 10;
+const MAX_WORKERS = 100;
 
 export const sqsAdapter: Adapter = {
   setup() {
