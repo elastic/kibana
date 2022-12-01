2e34a754c57680922e8bc317848da9627fee2530
Yulia Cech <yulia.cech@elastic.co>
2022-12-01 15:35:41 +0100

    [Guided onboarding] Fix translation ids

--- x-pack/plugins/enterprise_search/common/guided_onboarding/search_guide_config.ts
+++ x-pack/plugins/enterprise_search/common/guided_onboarding/search_guide_config.ts
@@ -10,10 +10,10 @@ import { i18n } from '@kbn/i18n';
 
 export const searchGuideId = 'search';
 export const searchGuideConfig: GuideConfig = {
-  title: i18n.translate('guidedOnboarding.searchGuide.title', {
+  title: i18n.translate('xpack.enterpriseSearch.guideConfig.title', {
     defaultMessage: 'Search my data',
   }),
-  description: i18n.translate('guidedOnboarding.searchGuide.description', {
+  description: i18n.translate('xpack.enterpriseSearch.guideConfig.description', {
     defaultMessage:
       'Build custom search experiences with your data using Elastic’s out-of-the-box web crawler, connectors, and robust APIs. Gain deep insights from the built-in search analytics to curate results and optimize relevance.',
   }),
@@ -21,17 +21,17 @@ export const searchGuideConfig: GuideConfig = {
   steps: [
     {
       id: 'add_data',
-      title: i18n.translate('guidedOnboarding.searchGuide.addDataStep.title', {
+      title: i18n.translate('xpack.enterpriseSearch.guideConfig.addDataStep.title', {
         defaultMessage: 'Add data',
       }),
       descriptionList: [
-        i18n.translate('guidedOnboarding.searchGuide.addDataStep.description1', {
+        i18n.translate('xpack.enterpriseSearch.guideConfig.addDataStep.description1', {
           defaultMessage: 'Select an ingestion method.',
         }),
-        i18n.translate('guidedOnboarding.searchGuide.addDataStep.description2', {
+        i18n.translate('xpack.enterpriseSearch.guideConfig.addDataStep.description2', {
           defaultMessage: 'Create a new Elasticsearch index.',
         }),
-        i18n.translate('guidedOnboarding.searchGuide.addDataStep.description3', {
+        i18n.translate('xpack.enterpriseSearch.guideConfig.addDataStep.description3', {
           defaultMessage: 'Configure your ingestion settings.',
         }),
       ],
@@ -42,20 +42,29 @@ export const searchGuideConfig: GuideConfig = {
     },
     {
       id: 'search_experience',
-      title: i18n.translate('guidedOnboarding.searchGuide.searchExperienceStep.title', {
+      title: i18n.translate('xpack.enterpriseSearch.guideConfig.searchExperienceStep.title', {
         defaultMessage: 'Build a search experience',
       }),
       descriptionList: [
-        i18n.translate('guidedOnboarding.searchGuide.searchExperienceStep.descriptionList.item1', {
-          defaultMessage: 'Learn more about Elastic’s Search UI framework.',
-        }),
-        i18n.translate('guidedOnboarding.searchGuide.searchExperienceStep.descriptionList.item2', {
-          defaultMessage: 'Try the Search UI tutorial for Elasticsearch.',
-        }),
-        i18n.translate('guidedOnboarding.searchGuide.searchExperienceStep.descriptionList.item3', {
-          defaultMessage:
-            'Build a world-class search experience for your customers, employees, or users.',
-        }),
+        i18n.translate(
+          'xpack.enterpriseSearch.guideConfig.searchExperienceStep.descriptionList.item1',
+          {
+            defaultMessage: 'Learn more about Elastic’s Search UI framework.',
+          }
+        ),
+        i18n.translate(
+          'xpack.enterpriseSearch.guideConfig.searchExperienceStep.descriptionList.item2',
+          {
+            defaultMessage: 'Try the Search UI tutorial for Elasticsearch.',
+          }
+        ),
+        i18n.translate(
+          'xpack.enterpriseSearch.guideConfig.searchExperienceStep.descriptionList.item3',
+          {
+            defaultMessage:
+              'Build a world-class search experience for your customers, employees, or users.',
+          }
+        ),
       ],
       location: {
         appID: 'searchExperiences',
@@ -63,13 +72,13 @@ export const searchGuideConfig: GuideConfig = {
       },
       manualCompletion: {
         title: i18n.translate(
-          'guidedOnboarding.searchGuide.searchExperienceStep.manualCompletionPopoverTitle',
+          'xpack.enterpriseSearch.guideConfig.searchExperienceStep.manualCompletionPopoverTitle',
           {
             defaultMessage: 'Explore Search UI',
           }
         ),
         description: i18n.translate(
-          'guidedOnboarding.searchGuide.searchExperienceStep.manualCompletionPopoverDescription',
+          'xpack.enterpriseSearch.guideConfig.searchExperienceStep.manualCompletionPopoverDescription',
           {
             defaultMessage:
               'Take your time to explore how to use Search UI to build world-class search experiences. When you’re ready, click the Setup guide button to continue.',
