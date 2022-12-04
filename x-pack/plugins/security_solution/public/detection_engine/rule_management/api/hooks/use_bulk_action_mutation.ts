27498d515217b581e402c8268b1a647d807fe1fc
Maxim Palenov <maxim.palenov@elastic.co>
2022-12-04 18:53:09 +0100

    fix tags data invalidation after bulk tags update

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/api/hooks/use_bulk_action_mutation.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/api/hooks/use_bulk_action_mutation.ts
@@ -83,6 +83,7 @@ export const useBulkActionMutation = (
             invalidateFindRulesQuery();
           }
           invalidateFetchRuleByIdQuery();
+          invalidateFetchRulesInfo();
           break;
       }
 
