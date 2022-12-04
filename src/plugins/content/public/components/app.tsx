f29b48396e6a532e912e4da36e4ef388445b1d20
vadimkibana <vadimkibana@gmail.com>
2022-12-04 11:53:21 +0100

    simplify react component

--- src/plugins/content/public/components/app.tsx
+++ src/plugins/content/public/components/app.tsx
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
-import { i18n } from '@kbn/i18n';
-import { FormattedMessage, I18nProvider } from '@kbn/i18n-react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
 
 import {
   EuiButton,
-  EuiHorizontalRule,
   EuiPage,
   EuiPageBody,
   EuiPageContent_Deprecated as EuiPageContent,
   EuiPageContentBody_Deprecated as EuiPageContentBody,
-  EuiPageContentHeader_Deprecated as EuiPageContentHeader,
   EuiPageHeader,
-  EuiTitle,
   EuiText,
 } from '@elastic/eui';
 
 import { CoreStart } from '../../../../core/public';
 
-import { PLUGIN_NAME } from '../../common';
 import { ContentRegistry } from '../service/registry/content_registry';
 
 interface ContentAppDeps {
@@ -28,79 +21,28 @@ interface ContentAppDeps {
   registry: ContentRegistry;
 }
 
-export const ContentApp = ({ basename, notifications, http, registry }: ContentAppDeps) => {
-  // Use React hooks to manage state.
-  const [timestamp, setTimestamp] = useState<string | undefined>();
-
-  console.log(registry);
-
-  const onClickHandler = () => {
-    // Use the core http service to make a response to the server API.
-    http.get('/api/content/example').then((res) => {
-      setTimestamp(res.time);
-      // Use the core notifications service to display a success message.
-      notifications.toasts.addSuccess(
-        i18n.translate('content.dataUpdated', {
-          defaultMessage: 'Data updated',
-        })
-      );
-    });
-  };
+export const ContentApp = ({ registry }: ContentAppDeps) => {
 
   return (
-    <Router basename={basename}>
-      <I18nProvider>
-        <>
-          <EuiPage restrictWidth="1000px">
-            <EuiPageBody>
-              <EuiPageHeader>
-                <EuiTitle size="l">
-                  <h1>
-                    <FormattedMessage
-                      id="content.helloWorldText"
-                      defaultMessage="{name}"
-                      values={{ name: PLUGIN_NAME }}
-                    />
-                  </h1>
-                </EuiTitle>
-              </EuiPageHeader>
-              <EuiPageContent>
-                <EuiPageContentHeader>
-                  <EuiTitle>
-                    <h2>
-                      <FormattedMessage
-                        id="content.congratulationsTitle"
-                        defaultMessage="Congratulations, you have successfully created a new Kibana Plugin!"
-                      />
-                    </h2>
-                  </EuiTitle>
-                </EuiPageContentHeader>
-                <EuiPageContentBody>
-                  <EuiText>
-                    <p>
-                      <FormattedMessage
-                        id="content.content"
-                        defaultMessage="Look through the generated code and check out the plugin development documentation."
-                      />
-                    </p>
-                    <EuiHorizontalRule />
-                    <p>
-                      <FormattedMessage
-                        id="content.timestampText"
-                        defaultMessage="Last timestamp: {time}"
-                        values={{ time: timestamp ? timestamp : 'Unknown' }}
-                      />
-                    </p>
-                    <EuiButton type="primary" size="s" onClick={onClickHandler}>
-                      <FormattedMessage id="content.buttonText" defaultMessage="Get data" />
-                    </EuiButton>
-                  </EuiText>
-                </EuiPageContentBody>
-              </EuiPageContent>
-            </EuiPageBody>
-          </EuiPage>
-        </>
-      </I18nProvider>
-    </Router>
+    <EuiPage restrictWidth="1000px">
+      <EuiPageBody>
+        <EuiPageHeader>
+          <br />
+        </EuiPageHeader>
+        <EuiPageContent>
+          <EuiPageContentBody>
+            <div style={{width: 1000}}>
+              <EuiText>
+                <EuiButton type="primary" size="s" onClick={() => {
+                  console.log(registry);
+                }}>
+                  Do something...
+                </EuiButton>
+              </EuiText>
+            </div>
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiPageBody>
+    </EuiPage>
   );
 };
