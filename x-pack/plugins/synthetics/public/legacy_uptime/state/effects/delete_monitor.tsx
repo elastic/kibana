1155af2e7fc4c371eb317c332949b2b5ba78162b
Dominique Clarke <dominique.clarke@elastic.co>
2022-12-05 11:39:10 -0500

    adjust effect logic to pass data test subj

--- x-pack/plugins/synthetics/public/legacy_uptime/state/effects/delete_monitor.tsx
+++ x-pack/plugins/synthetics/public/legacy_uptime/state/effects/delete_monitor.tsx
@@ -4,7 +4,8 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
+import React from 'react';
+import { toMountPoint } from '@kbn/kibana-react-plugin/public';
 import { put, call, takeEvery } from 'redux-saga/effects';
 import { Action } from 'redux-actions';
 import { i18n } from '@kbn/i18n';
@@ -20,12 +21,19 @@ export function* deleteMonitorEffect() {
         const { id, name } = action.payload;
         yield call(deleteMonitor, { id });
         yield put(deleteMonitorAction.success(id));
-        kibanaService.core.notifications.toasts.addSuccess(
-          i18n.translate('xpack.synthetics.monitorManagement.monitorDeleteSuccessMessage.name', {
-            defaultMessage: 'Deleted "{name}"',
-            values: { name },
-          })
-        );
+        kibanaService.core.notifications.toasts.addSuccess({
+          title: toMountPoint(
+            <p data-test-subj="uptimeDeleteMonitorSuccess">
+              {i18n.translate(
+                'xpack.synthetics.monitorManagement.monitorDeleteSuccessMessage.name',
+                {
+                  defaultMessage: 'Deleted "{name}"',
+                  values: { name },
+                }
+              )}
+            </p>
+          ),
+        });
       } catch (err) {
         kibanaService.core.notifications.toasts.addError(err, {
           title: MONITOR_DELETE_FAILURE_LABEL,
