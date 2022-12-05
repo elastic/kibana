018dc3728457259151c173d142a2ed1a5d3f67af
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2022-12-05 15:06:02 +0200

    Fixes test

--- src/plugins/vis_types/vislib/public/vislib/response_handler.test.ts
+++ src/plugins/vis_types/vislib/public/vislib/response_handler.test.ts
@@ -15,40 +15,15 @@ jest.mock('./helpers', () => ({
 // @ts-ignore
 import { vislibSeriesResponseHandler } from './response_handler';
 import { buildPointSeriesData } from './helpers';
-import { Table } from './types';
 
 describe('response_handler', () => {
   describe('vislibSeriesResponseHandler', () => {
-    let resp: Table;
-    let expected: any;
-
     beforeAll(() => {
       setFormatService({
         deserialize: () => ({
           convert: jest.fn((v) => v),
         }),
       } as any);
-    });
-
-    beforeAll(() => {
-      resp = {
-        rows: [
-          { 'col-0-3': 158599872, 'col-1-1': 1 },
-          { 'col-0-3': 158599893, 'col-1-1': 2 },
-          { 'col-0-3': 158599908, 'col-1-1': 1 },
-        ],
-        columns: [
-          { id: 'col-0-3', name: 'timestamp per 30 seconds' },
-          { id: 'col-1-1', name: 'Count' },
-        ],
-      } as Table;
-
-      const colId = resp.columns[0].id;
-      expected = [
-        { label: `${resp.rows[0][colId]}: ${resp.columns[0].name}` },
-        { label: `${resp.rows[1][colId]}: ${resp.columns[0].name}` },
-        { label: `${resp.rows[2][colId]}: ${resp.columns[0].name}` },
-      ];
     });
 
     test('should not call buildPointSeriesData when no columns', () => {
