b8c15e72b5258fd1d3e756892ad52e404a2a102e
rshen91 <rshen@elastic.co>
2022-12-05 14:58:47 -0700

    apply custom branding type to not internal package

--- packages/core/chrome/core-chrome-browser/src/custom_branding.ts
+++ packages/core/chrome/core-chrome-browser/src/custom_branding.ts
@@ -6,14 +6,6 @@
  * Side Public License, v 1.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
-
 /**
  * Combined parts of custom branding
  * Properties are each optional to provide flexibility for the Kibana
@@ -37,18 +29,4 @@ export interface CustomBranding {
    * @link packages/core/chrome/core-chrome-browser-internal/src/ui/header/elastic_mark.tsx
    */
   customizedLogo?: string;
-}
-
-/** @internal */
-export class CustomBrandingService {
-  private licenseType: '';
-
-  async start(): Promise<CustomBranding> {
-    return {
-      get: () => {
-        return customBranding$.get();
-      },
-      set: () => {},
-    };
-  }
 }
