a5b2abd80ab14d951fde392b84e8e78e336d4215
spalger <spencer@elastic.co>
2022-12-06 11:53:32 -0600

    update tsconfig file with references

--- kbn_pm/src/commands/x_command.mjs
+++ kbn_pm/src/commands/x_command.mjs
@@ -7,6 +7,7 @@
  */
 
 import Path from 'path';
+import Fs from 'fs';
 
 import External from '../lib/external_packages.js';
 import { REPO_ROOT } from '../lib/paths.mjs';
@@ -16,7 +17,7 @@ const __dirname = Path.dirname(new URL(import.meta.url).pathname);
 /** @type {import('../lib/command').Command} */
 export const command = {
   name: '_x',
-  async run() {
+  async run({ log }) {
     const filtered = (
       await External['@kbn/bazel-packages']().discoverBazelPackages(REPO_ROOT)
     ).filter((p) => p.hasBuildTypesRule());
@@ -29,22 +30,39 @@ export const command = {
       External.reqAbs(Path.resolve(__dirname, 'get_refs.ts'))
     );
 
-    const parsed = await parseBazelFiles(
+    const { Jsonc } = External['@kbn/bazel-packages']();
+
+    const parsedBazelFiles = await parseBazelFiles(
       filtered.map((p) => Path.resolve(REPO_ROOT, p.normalizedRepoRelativeDir, 'BUILD.bazel'))
     );
 
     for (const pkg of filtered) {
       const dir = Path.resolve(REPO_ROOT, pkg.normalizedRepoRelativeDir);
-      const ast = parsed.get(Path.resolve(dir, 'BUILD.bazel'));
+      const ast = parsedBazelFiles.get(Path.resolve(dir, 'BUILD.bazel'));
       if (!ast) {
         throw new Error(`missing ast for ${pkg.manifest.id}`);
       }
 
-      console.log(
-        pkg.normalizedRepoRelativeDir,
-        '->',
-        getRefdPkgs(ast).map((p) => Path.relative(dir, Path.resolve(REPO_ROOT, p, 'tsconfig.json')))
+      const tsconfigPath = Path.resolve(dir, 'tsconfig.json');
+      const jsonc = Fs.readFileSync(tsconfigPath, 'utf8');
+      const parsed = Jsonc.parse(jsonc);
+
+      Fs.writeFileSync(
+        tsconfigPath,
+        JSON.stringify(
+          // eslint-disable-next-line prefer-object-spread/prefer-object-spread
+          Object.assign({}, parsed, {
+            kbn_references: getRefdPkgs(ast).map((p) =>
+              Path.relative(dir, Path.resolve(REPO_ROOT, p, 'tsconfig.json'))
+            ),
+          }),
+          null,
+          2
+        ),
+        'utf8'
       );
+
+      log.success('updated', tsconfigPath);
     }
   },
 };
