102694457026734210d358f2af86ef9b319269b7
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2022-12-06 12:19:27 -0500

    separate more

--- x-pack/test/alerting_api_integration/security_and_spaces/group3/tests/alerting/index.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/group3/tests/alerting/index.ts
@@ -20,6 +20,10 @@ export default function alertingTests({ loadTestFile, getService }: FtrProviderC
         await tearDown(getService);
       });
 
+      loadTestFile(require.resolve('./bulk_edit'));
+      loadTestFile(require.resolve('./bulk_delete'));
+      loadTestFile(require.resolve('./bulk_enable'));
+      loadTestFile(require.resolve('./bulk_disable'));
       loadTestFile(require.resolve('./clone'));
     });
   });
