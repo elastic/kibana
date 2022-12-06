41fbcecb6724be1a190418b192407d480bf00b0d
Kevin Delemme <kevin.delemme@elastic.co>
2022-12-06 15:21:57 -0500

    Use BurnRate in Editor

--- x-pack/plugins/observability/public/components/app/burn_rate_rule_editor/burn_rate_rule_editor.tsx
+++ x-pack/plugins/observability/public/components/app/burn_rate_rule_editor/burn_rate_rule_editor.tsx
@@ -6,11 +6,33 @@
  */
 
 import { EuiFieldText, EuiFlexGroup, EuiFlexItem, EuiFormRow, EuiSpacer } from '@elastic/eui';
-import React from 'react';
+import { Duration } from '@kbn/observability-plugin/public/typings';
+import React, { useEffect, useState } from 'react';
+import { BurnRate } from './burn_rate';
 
-import { LongTimeWindow } from './long_time_window';
+import { LongWindowDuration } from './long_window_duration';
 
 export function BurnRateRuleEditor() {
+  const [longWindowDuration, setLongWindowDuration] = useState<Duration>({ value: 1, unit: 'h' });
+  const [shortWindowDuration, setShortWindowDuration] = useState<Duration>({ value: 5, unit: 'm' });
+  const [burnRate, setBurnRate] = useState<number>(1);
+  const [maxBurnRate, setMaxBurnRate] = useState<number>(1);
+
+  const onLongWindowDurationChange = (duration: Duration) => {
+    setLongWindowDuration(duration);
+    setShortWindowDuration({ value: 5, unit: 'm' }); // TODO: compute 1/12th of long window duration
+  };
+
+  const onBurnRateChange = (value: number) => {
+    setBurnRate(value);
+  };
+
+  useEffect(() => {
+    const sloWindowDurationInMinutes = 43200; // TODO: change when SloSelector used. 30 days in minutes
+    const longWindowDurationInMinutes = toMinutes(longWindowDuration);
+    setMaxBurnRate(Math.floor(sloWindowDurationInMinutes / longWindowDurationInMinutes));
+  }, [longWindowDuration]); // depends on selected SLO as well as long window
+
   return (
     <EuiFlexGroup direction="column">
       <EuiFlexGroup direction="row">
@@ -23,16 +45,21 @@ export function BurnRateRuleEditor() {
 
       <EuiFlexGroup direction="row">
         <EuiFlexItem>
-          <LongTimeWindow />
+          <LongWindowDuration
+            initialDuration={longWindowDuration}
+            onChange={(duration) => onLongWindowDurationChange(duration)}
+          />
         </EuiFlexItem>
         <EuiFlexItem>
-          <EuiFormRow label="Burn rate threshold" fullWidth>
-            <EuiFieldText name="threshold" fullWidth />
-          </EuiFormRow>
+          <BurnRate maxBurnRate={maxBurnRate} onChange={(burnRate) => onBurnRateChange(burnRate)} />
         </EuiFlexItem>
       </EuiFlexGroup>
 
       <EuiSpacer size="m" />
     </EuiFlexGroup>
   );
+}
+
+function toMinutes(longWindowDuration: Duration) {
+  return longWindowDuration.unit === 'm' ? longWindowDuration.value : longWindowDuration.value * 60;
 }
