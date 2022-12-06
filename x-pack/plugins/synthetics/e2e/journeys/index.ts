f1138ad1c03db0be72cdfc898b98a488e4152e87
shahzad31 <shahzad31comp@gmail.com>
2022-12-06 19:41:26 +0100

    test

--- x-pack/plugins/synthetics/e2e/journeys/index.ts
+++ x-pack/plugins/synthetics/e2e/journeys/index.ts
@@ -6,7 +6,6 @@
  */
 
 export * from './data_view_permissions';
-export * from './private_locations';
 export * from './synthetics';
 export * from './alerts';
 export * from './uptime.journey';
@@ -18,4 +17,5 @@ export * from './monitor_management.journey';
 export * from './monitor_management_enablement.journey';
 export * from './monitor_details';
 export * from './locations';
+export * from './private_locations';
 export * from './detail_flyout';
