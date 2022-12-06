b91b9e922e9dd65cce4b2cc41ce2caab04e1cc01
gbamparop <georgios.bamparopoulos@elastic.co>
2022-12-06 17:06:43 +0000

    Remove CLI_NUMBER variable

--- .buildkite/scripts/steps/functional/apm_cypress.sh
+++ .buildkite/scripts/steps/functional/apm_cypress.sh
@@ -10,7 +10,6 @@ APM_CYPRESS_RECORD_KEY="$(retry 5 5 vault read -field=CYPRESS_RECORD_KEY secret/
 .buildkite/scripts/download_build_artifacts.sh
 
 export JOB=kibana-apm-cypress
-CLI_NUMBER=$((BUILDKITE_PARALLEL_JOB+1))
 IS_FLAKY_TEST_RUNNER=${CLI_COUNT:-0}
 
 # Disable parallel tests and dashboard recording when running them in the flaky test runner
