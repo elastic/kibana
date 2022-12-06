5c575f9d97b81275c81db5583e16a85332df3cdb
Constance Chen <constance.chen@elastic.co>
2022-12-06 14:33:13 -0800

    [EuiDualRange] Fix typing of EuiDualRange's `ref` usage
    - now that the component is wrapped in `withEuiTheme`, unfortunately things are extremely messy
    
    - unfortunately converting EuiDualRange to a function component is not necesssarily the answer as these specific use cases then lose access to the `onResize` instance method
    
    + fix incorrect usage of `rangeRef` in `src/plugins/controls` - the rangeRef was never actually getting passed down and thus never actually calling `onResize` - also `onResize` needs an actual width number

--- src/plugins/controls/public/time_slider/components/time_slider_popover_content.tsx
+++ src/plugins/controls/public/time_slider/components/time_slider_popover_content.tsx
@@ -7,7 +7,7 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import React, { Ref } from 'react';
+import React, { Ref, ComponentProps } from 'react';
 import {
   EuiButtonIcon,
   EuiDualRange,
@@ -16,6 +16,10 @@ import {
   EuiFlexItem,
   EuiToolTip,
 } from '@elastic/eui';
+import type { EuiDualRangeClass } from '@elastic/eui/src/components/form/range/dual_range';
+
+// Unfortunately, wrapping EuiDualRange in `withEuiTheme` has created a super annoying/verbose typing
+export type EuiDualRangeRef = EuiDualRangeClass & ComponentProps<typeof EuiDualRange>;
 
 interface Props {
   value: [number, number];
@@ -25,7 +29,7 @@ interface Props {
   ticks: EuiRangeTick[];
   timeRangeMin: number;
   timeRangeMax: number;
-  rangeRef?: Ref<EuiDualRange>;
+  rangeRef?: Ref<EuiDualRangeRef>;
 }
 
 export function TimeSliderPopoverContent(props: Props) {
