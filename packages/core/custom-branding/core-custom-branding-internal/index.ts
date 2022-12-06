6f552080fbfab3b4466f0208d4efbabc728a97b7
rshen91 <rshen@elastic.co>
2022-12-06 11:03:25 -0700

    remove unnecessary folder

--- packages/core/custom-branding/core-custom-branding-internal/index.ts
+++ packages/core/custom-branding/core-custom-branding-internal/index.ts
@@ -6,6 +6,5 @@
  * Side Public License, v 1.
  */
 
-export function foo() {
-  return 'hello world';
-}
+export { CustomBrandingService } from './custom_branding_service';
+export type { CustomBranding } from './custom_branding_service';
