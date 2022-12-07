d8b50beaa8e18ccf1ad0f08ee21872393a345169
shahzad31 <shahzad31comp@gmail.com>
2022-12-07 14:26:23 +0100

    Project api keys settings

---
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/settings/project_api_keys/help_commands.tsx
@@ -0,0 +1,69 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiCallOut, EuiCodeBlock, EuiSpacer, EuiText } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { useSyntheticsSettingsContext } from '../../../contexts';
+
+export const HelpCommands = ({ apiKey }: { apiKey: string }) => {
+  const { basePath } = useSyntheticsSettingsContext();
+
+  return (
+    <div className="text-left">
+      <EuiCallOut title={API_KEY_WARNING_LABEL} iconType="iInCircle" size="s" />
+      <EuiSpacer size="s" />
+      <EuiText size="s">
+        <strong>{API_KEY_LABEL}</strong>
+      </EuiText>
+      <EuiSpacer size="s" />
+      <EuiCodeBlock language="javascript" isCopyable fontSize="s" paddingSize="m" whiteSpace="pre">
+        {apiKey}
+      </EuiCodeBlock>
+      <EuiSpacer size="s" />
+      <EuiText size="s">
+        <strong>{USE_AS_ENV}</strong>
+      </EuiText>
+      <EuiSpacer size="s" />
+      <EuiCodeBlock language="javascript" isCopyable fontSize="s" paddingSize="m">
+        export SYNTHETICS_API_KEY={apiKey}
+      </EuiCodeBlock>
+      <EuiSpacer size="s" />
+      <EuiText size="s">
+        <strong>{PROJECT_SETUP_COMMAND}</strong>
+      </EuiText>
+      <EuiSpacer size="s" />
+      <EuiCodeBlock language="javascript" isCopyable fontSize="s" paddingSize="m">
+        npx @elastic/synthetics init --api-key={apiKey} --kbn-url=
+        {window.location.origin + basePath}
+      </EuiCodeBlock>
+    </div>
+  );
+};
+
+const API_KEY_LABEL = i18n.translate('xpack.synthetics.monitorManagement.apiKey.label', {
+  defaultMessage: 'API key',
+});
+
+const USE_AS_ENV = i18n.translate('xpack.synthetics.monitorManagement.useEnv.label', {
+  defaultMessage: 'Use as environment variable',
+});
+
+const PROJECT_SETUP_COMMAND = i18n.translate(
+  'xpack.synthetics.monitorManagement.projectSetup.label',
+  {
+    defaultMessage: 'Project setup command',
+  }
+);
+
+const API_KEY_WARNING_LABEL = i18n.translate(
+  'xpack.synthetics.monitorManagement.apiKeyWarning.label',
+  {
+    defaultMessage:
+      'This API key will only be shown once. Please keep a copy for your own records.',
+  }
+);
