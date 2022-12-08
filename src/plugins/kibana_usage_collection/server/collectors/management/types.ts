cd9c2b5730a264e660fc2610b6726a025d36b62a
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-12-08 13:55:38 +0100

    Update telemetry for advanced setting

--- src/plugins/kibana_usage_collection/server/collectors/management/types.ts
+++ src/plugins/kibana_usage_collection/server/collectors/management/types.ts
@@ -46,6 +46,7 @@ export interface UsageStats {
   'observability:apmAWSLambdaRequestCostPerMillion': number;
   'observability:enableInfrastructureHostsView': boolean;
   'observability:apmAgentExplorerView': boolean;
+  'observability:profilingElasticsearchPlugin': boolean;
   'visualize:enableLabs': boolean;
   'visualization:heatmap:maxBuckets': number;
   'visualization:colorMapping': string;
