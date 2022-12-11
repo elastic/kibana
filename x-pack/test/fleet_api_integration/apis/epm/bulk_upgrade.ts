0a34ae697036fec845a95e1d2727dc7bcd0b9ece
Paul Tavares <paul.tavares@elastic.co>
2022-12-10 20:25:25 -0500

    Add `supertest` middleware in bulk_upgrade tests

--- x-pack/test/fleet_api_integration/apis/epm/bulk_upgrade.ts
+++ x-pack/test/fleet_api_integration/apis/epm/bulk_upgrade.ts
@@ -11,6 +11,7 @@ import {
   BulkInstallPackagesResponse,
   IBulkInstallPackageHTTPError,
 } from '@kbn/fleet-plugin/common';
+import { Request } from 'supertest';
 import { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
 import { skipIfNoDockerRegistry } from '../../helpers';
 import { setupFleetAndAgents } from '../agents/services';
@@ -20,20 +21,19 @@ export default function (providerContext: FtrProviderContext) {
   const { getService } = providerContext;
   const supertest = getService('supertest');
   const supertestWithoutAuth = getService('supertestWithoutAuth');
-  const log = getService('log');
 
   const deletePackage = async (name: string, version: string) => {
     await supertest.delete(`/api/fleet/epm/packages/${name}/${version}`).set('kbn-xsrf', 'xxxx');
   };
 
-  const getSupertestApiFailureHandler = (): ((err: Error) => never) => {
-    return (error: any): never => {
-      const responseBody = error?.response?.body;
-      const responseText = error?.response?.text;
-      log.error(JSON.stringify(responseBody || responseText, null, 2));
-      log.error(error);
-      throw error;
-    };
+  const superTestVerboseErrors = (req: Request) => {
+    req.on('error', (err) => {
+      if (!err.response || !err.response.body || !err.response.text) {
+        return;
+      }
+
+      err.message += `\n${JSON.stringify(err.response.body ?? err.response.text, null, 2)}`;
+    });
   };
 
   describe('bulk package upgrade api', async () => {
@@ -44,10 +44,10 @@ export default function (providerContext: FtrProviderContext) {
       beforeEach(async () => {
         await supertest
           .post(`/api/fleet/epm/packages/multiple_versions/0.1.0`)
+          .use(superTestVerboseErrors)
           .set('kbn-xsrf', 'xxxx')
           .send({ force: true })
-          .expect(200)
-          .catch(getSupertestApiFailureHandler());
+          .expect(200);
       });
       afterEach(async () => {
         await deletePackage('multiple_versions', '0.1.0');
@@ -56,35 +56,30 @@ export default function (providerContext: FtrProviderContext) {
       });
 
       it('should return 400 if no packages are requested for upgrade', async function () {
-        await supertest
-          .post(`/api/fleet/epm/packages/_bulk`)
-          .set('kbn-xsrf', 'xxxx')
-          .expect(400)
-          .catch(getSupertestApiFailureHandler());
+        await supertest.post(`/api/fleet/epm/packages/_bulk`).set('kbn-xsrf', 'xxxx').expect(400);
       });
       it('should return 403 if user without integrations all requests upgrade', async function () {
         await supertestWithoutAuth
           .post(`/api/fleet/epm/packages/_bulk`)
+          .use(superTestVerboseErrors)
           .auth(testUsers.fleet_all_int_read.username, testUsers.fleet_all_int_read.password)
           .set('kbn-xsrf', 'xxxx')
-          .expect(403)
-          .catch(getSupertestApiFailureHandler());
+          .expect(403);
       });
       it('should return 403 if user without fleet access requests upgrade', async function () {
         await supertestWithoutAuth
           .post(`/api/fleet/epm/packages/_bulk`)
           .auth(testUsers.integr_all_only.username, testUsers.integr_all_only.password)
           .set('kbn-xsrf', 'xxxx')
-          .expect(403)
-          .catch(getSupertestApiFailureHandler());
+          .expect(403);
       });
       it('should return 200 and an array for upgrading a package', async function () {
         const { body }: { body: BulkInstallPackagesResponse } = await supertest
           .post(`/api/fleet/epm/packages/_bulk?prerelease=true`)
+          .use(superTestVerboseErrors)
           .set('kbn-xsrf', 'xxxx')
           .send({ packages: ['multiple_versions'] })
-          .expect(200)
-          .catch(getSupertestApiFailureHandler());
+          .expect(200);
         expect(body.items.length).equal(1);
         expect(body.items[0].name).equal('multiple_versions');
         const entry = body.items[0] as BulkInstallPackageInfo;
@@ -93,10 +88,10 @@ export default function (providerContext: FtrProviderContext) {
       it('should return an error for packages that do not exist', async function () {
         const { body }: { body: BulkInstallPackagesResponse } = await supertest
           .post(`/api/fleet/epm/packages/_bulk?prerelease=true`)
+          .use(superTestVerboseErrors)
           .set('kbn-xsrf', 'xxxx')
           .send({ packages: ['multiple_versions', 'blahblah'] })
-          .expect(200)
-          .catch(getSupertestApiFailureHandler());
+          .expect(200);
         expect(body.items.length).equal(2);
         expect(body.items[0].name).equal('multiple_versions');
         const entry = body.items[0] as BulkInstallPackageInfo;
@@ -109,10 +104,10 @@ export default function (providerContext: FtrProviderContext) {
       it('should upgrade multiple packages', async function () {
         const { body }: { body: BulkInstallPackagesResponse } = await supertest
           .post(`/api/fleet/epm/packages/_bulk?prerelease=true`)
+          .use(superTestVerboseErrors)
           .set('kbn-xsrf', 'xxxx')
           .send({ packages: ['multiple_versions', 'overrides'] })
-          .expect(200)
-          .catch(getSupertestApiFailureHandler());
+          .expect(200);
         expect(body.items.length).equal(2);
         expect(body.items[0].name).equal('multiple_versions');
         let entry = body.items[0] as BulkInstallPackageInfo;
@@ -132,10 +127,10 @@ export default function (providerContext: FtrProviderContext) {
       it('should return 200 and an array for upgrading a package', async function () {
         const { body }: { body: BulkInstallPackagesResponse } = await supertest
           .post(`/api/fleet/epm/packages/_bulk?prerelease=true`)
+          .use(superTestVerboseErrors)
           .set('kbn-xsrf', 'xxxx')
           .send({ packages: ['multiple_versions'] })
-          .expect(200)
-          .catch(getSupertestApiFailureHandler());
+          .expect(200);
         expect(body.items.length).equal(1);
         expect(body.items[0].name).equal('multiple_versions');
         const entry = body.items[0] as BulkInstallPackageInfo;
