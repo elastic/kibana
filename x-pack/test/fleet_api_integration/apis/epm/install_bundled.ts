elastic/kibana

commit 2e23aa0c3178452015e88807733f2f00a23e62b2

added fix for add and edit integrations page not loading for prerelease versions (#144431)

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/multi_page_layout/index.tsx
@@ -70,7 +70,7 @@ export const CreatePackagePolicyMultiPage: CreatePackagePolicyParams = ({
     data: packageInfoData,
     error: packageInfoError,
     isLoading: isPackageInfoLoading,
-  } = useGetPackageInfoByKey(pkgName, pkgVersion);
+  } = useGetPackageInfoByKey(pkgName, pkgVersion, { prerelease: true });
 
   const {
     agentPolicy,

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.tsx
@@ -99,7 +99,7 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
     data: packageInfoData,
     error: packageInfoError,
     isLoading: isPackageInfoLoading,
-  } = useGetPackageInfoByKey(pkgName, pkgVersion);
+  } = useGetPackageInfoByKey(pkgName, pkgVersion, { prerelease: true });
   const packageInfo = useMemo(() => {
     if (packageInfoData && packageInfoData.item) {
       return packageInfoData.item;

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/edit_package_policy_page/index.tsx
@@ -265,7 +265,8 @@ export const EditPackagePolicyForm = memo<{
 
             const { data: packageData } = await sendGetPackageInfoByKey(
               _packageInfo!.name,
-              _packageInfo!.version
+              _packageInfo!.version,
+              { prerelease: true }
             );
 
             if (packageData?.item) {

commit 24e2fcf47014d937a5019d83b95565a010ae4e22

[Fleet] fix for apm not found in epr + prerelease flag (#147257)
## Summary

Fixes https://github.com/elastic/kibana/issues/146312

Fall back to bundled packages when loading package archive.

Also fixed an issue with prerelease flag caused by
https://github.com/elastic/kibana/pull/144431

Loading prerelease setting on Add/Edit integration page, so that it
works well for cases where the url doesn't contain a concrete version
like APM

Steps to verify:
- Start kibana locally, make sure that APM bundled package is there in
`x-pack/plugins/fleet/target/bundled_packages`
- Load http://localhost:5601/julia/app/integrations/detail/apm/overview
(with default "beta integrations" off flag)
- Expect that 8.6.0 version is showing up (or 8.7.0 depending on the
build)
- When clicking on Add Elastic APM, expect 8.6.0 to be loaded
successfully

<img width="1773" alt="image"
src="https://user-images.githubusercontent.com/90178898/206482128-634ca823-bb21-46a9-a8b9-fc81d5dc126d.png">

Note: there is still a UI issue with loading assets on Overview page if
the package is not in EPR.

<img width="1747" alt="image"
src="https://user-images.githubusercontent.com/90178898/206478275-ee8e9494-234f-44c8-88d9-a91c791b0196.png">

- switch "beta integrations" on on Integrations page
- Open APM overview page again
- expect `8.7.0-preview` version with the selector to change to last GA

<img width="1100" alt="image"
src="https://user-images.githubusercontent.com/90178898/206480986-2ed05b12-49e4-4c88-97c6-6cdf12f819da.png">


### Checklist

- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/index.tsx
@@ -4,9 +4,11 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useMemo } from 'react';
 import { useLocation, useRouteMatch } from 'react-router-dom';
+
+import { useGetSettings } from '../../../hooks';
 
 import type { AddToPolicyParams, EditPackagePolicyFrom } from './types';
 
@@ -22,6 +24,17 @@ export const CreatePackagePolicyPage: React.FC<{}> = () => {
     () => queryParams.get('policyId') ?? undefined,
     [queryParams]
   );
+  const [prerelease, setPrerelease] = React.useState<boolean>(false);
+
+  const { data: settings } = useGetSettings();
+
+  useEffect(() => {
+    const isEnabled = Boolean(settings?.item.prerelease_integrations_enabled);
+    if (settings?.item) {
+      setPrerelease(isEnabled);
+    }
+  }, [settings?.item]);
+
   /**
    * Please note: policyId can come from one of two sources. The URL param (in the URL path) or
    * in the query params (?policyId=foo).
@@ -39,6 +52,7 @@ export const CreatePackagePolicyPage: React.FC<{}> = () => {
   const pageParams = {
     from,
     queryParamsPolicyId,
+    prerelease,
   };
 
   if (useMultiPageLayout) {

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/multi_page_layout/index.tsx
@@ -52,6 +52,7 @@ const standaloneSteps = [addIntegrationStep, installAgentStep, confirmDataStep];
 export const CreatePackagePolicyMultiPage: CreatePackagePolicyParams = ({
   from,
   queryParamsPolicyId,
+  prerelease,
 }) => {
   const { params } = useRouteMatch<AddToPolicyParams>();
   const { pkgkey, policyId, integration } = params;
@@ -70,7 +71,7 @@ export const CreatePackagePolicyMultiPage: CreatePackagePolicyParams = ({
     data: packageInfoData,
     error: packageInfoError,
     isLoading: isPackageInfoLoading,
-  } = useGetPackageInfoByKey(pkgName, pkgVersion, { prerelease: true, full: true });
+  } = useGetPackageInfoByKey(pkgName, pkgVersion, { prerelease, full: true });
 
   const {
     agentPolicy,

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.tsx
@@ -73,6 +73,7 @@ const CustomEuiBottomBar = styled(EuiBottomBar)`
 export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
   from,
   queryParamsPolicyId,
+  prerelease,
 }) => {
   const {
     agents: { enabled: isFleetEnabled },
@@ -99,7 +100,7 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
     data: packageInfoData,
     error: packageInfoError,
     isLoading: isPackageInfoLoading,
-  } = useGetPackageInfoByKey(pkgName, pkgVersion, { full: true, prerelease: true });
+  } = useGetPackageInfoByKey(pkgName, pkgVersion, { full: true, prerelease });
   const packageInfo = useMemo(() => {
     if (packageInfoData && packageInfoData.item) {
       return packageInfoData.item;

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/types.ts
@@ -32,4 +32,5 @@ export interface AddToPolicyParams {
 export type CreatePackagePolicyParams = React.FunctionComponent<{
   from: EditPackagePolicyFrom;
   queryParamsPolicyId?: string;
+  prerelease: boolean;
 }>;

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/edit_package_policy_page/index.tsx
@@ -35,6 +35,7 @@ import {
   sendGetPackageInfoByKey,
   sendUpgradePackagePolicyDryRun,
   useAuthz,
+  useGetSettings,
 } from '../../../hooks';
 import {
   useBreadcrumbs as useIntegrationsBreadcrumbs,
@@ -128,6 +129,17 @@ export const EditPackagePolicyForm = memo<{
   const [isUpgrade, setIsUpgrade] = useState<boolean>(false);
 
   const canWriteIntegrationPolicies = useAuthz().integrations.writeIntegrationPolicies;
+
+  const [prerelease, setPrerelease] = React.useState<boolean>(false);
+
+  const { data: settings } = useGetSettings();
+
+  useEffect(() => {
+    const isEnabled = Boolean(settings?.item.prerelease_integrations_enabled);
+    if (settings?.item) {
+      setPrerelease(isEnabled);
+    }
+  }, [settings?.item]);
 
   useEffect(() => {
     if (forceUpgrade) {
@@ -266,7 +278,7 @@ export const EditPackagePolicyForm = memo<{
             const { data: packageData } = await sendGetPackageInfoByKey(
               _packageInfo!.name,
               _packageInfo!.version,
-              { prerelease: true, full: true }
+              { prerelease, full: true }
             );
 
             if (packageData?.item) {
@@ -293,7 +305,7 @@ export const EditPackagePolicyForm = memo<{
       setIsLoadingData(false);
     };
     getData();
-  }, [policyId, packagePolicyId, isUpgrade]);
+  }, [policyId, packagePolicyId, isUpgrade, prerelease]);
 
   // Retrieve agent count
   const [agentCount, setAgentCount] = useState<number>(0);

x-pack/plugins/fleet/server/services/epm/packages/get.ts
@@ -23,7 +23,12 @@ import type {
   GetCategoriesRequest,
 } from '../../../../common/types';
 import type { Installation, PackageInfo } from '../../../types';
-import { FleetError, PackageFailedVerificationError, PackageNotFoundError } from '../../../errors';
+import {
+  FleetError,
+  PackageFailedVerificationError,
+  PackageNotFoundError,
+  RegistryResponseError,
+} from '../../../errors';
 import { appContextService } from '../..';
 import * as Registry from '../registry';
 import { getEsPackage } from '../archive/storage';
@@ -305,8 +310,16 @@ export async function getPackageFromSource(options: {
     if (res) {
       logger.debug(`retrieved package ${pkgName}-${pkgVersion} from cache`);
     } else {
-      res = await Registry.getPackage(pkgName, pkgVersion, { ignoreUnverified });
-      logger.debug(`retrieved package ${pkgName}-${pkgVersion} from registry`);
+      try {
+        res = await Registry.getPackage(pkgName, pkgVersion, { ignoreUnverified });
+        logger.debug(`retrieved package ${pkgName}-${pkgVersion} from registry`);
+      } catch (err) {
+        if (err instanceof RegistryResponseError && err.status === 404) {
+          res = await Registry.getBundledArchive(pkgName, pkgVersion);
+        } else {
+          throw err;
+        }
+      }
     }
   }
   if (!res) {

x-pack/plugins/fleet/server/services/epm/registry/index.ts
@@ -184,21 +184,30 @@ export async function fetchInfo(
     return res;
   } catch (err) {
     if (err instanceof RegistryResponseError && err.status === 404) {
-      // Check bundled packages in case the exact package being requested is available on disk
-      const bundledPackage = await getBundledPackageByName(pkgName);
-
-      if (bundledPackage && bundledPackage.version === pkgVersion) {
-        const archivePackage = await generatePackageInfoFromArchiveBuffer(
-          bundledPackage.buffer,
-          'application/zip'
-        );
-
+      const archivePackage = await getBundledArchive(pkgName, pkgVersion);
+      if (archivePackage) {
         return archivePackage.packageInfo;
       }
-
       throw new PackageNotFoundError(`${pkgName}@${pkgVersion} not found`);
     }
     throw err;
+  }
+}
+
+export async function getBundledArchive(
+  pkgName: string,
+  pkgVersion: string
+): Promise<{ paths: string[]; packageInfo: ArchivePackage } | undefined> {
+  // Check bundled packages in case the exact package being requested is available on disk
+  const bundledPackage = await getBundledPackageByName(pkgName);
+
+  if (bundledPackage && bundledPackage.version === pkgVersion) {
+    const archivePackage = await generatePackageInfoFromArchiveBuffer(
+      bundledPackage.buffer,
+      'application/zip'
+    );
+
+    return archivePackage;
   }
 }
 

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.test.tsx
@@ -109,7 +109,11 @@ describe('when on the package policy create page', () => {
   const render = (queryParamsPolicyId?: string) =>
     (renderResult = testRenderer.render(
       <Route path={FLEET_ROUTING_PATHS.add_integration_to_policy}>
-        <CreatePackagePolicySinglePage from="package" queryParamsPolicyId={queryParamsPolicyId} />
+        <CreatePackagePolicySinglePage
+          from="package"
+          queryParamsPolicyId={queryParamsPolicyId}
+          prerelease={false}
+        />
       </Route>
     ));
   let mockPackageInfo: any;

x-pack/test/fleet_api_integration/apis/epm/install_bundled.ts
@@ -100,6 +100,17 @@ export default function (providerContext: FtrProviderContext) {
 
         expect(updateResponse.body._meta.install_source).to.be('bundled');
       });
+
+      it('should load package archive from bundled package', async () => {
+        await bundlePackage('nginx-1.2.1');
+
+        const response = await supertest
+          .get(`/api/fleet/epm/packages/nginx/1.2.1?full=true`)
+          .expect(200);
+
+        expect(response.body.item.name).to.eql('nginx');
+        expect(response.body.item.version).to.eql('1.2.1');
+      });
     });
 
     describe('with registry', () => {