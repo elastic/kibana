47580dabc31c22f41e802a38e764f989041da354
Kevin Delemme <kevin.delemme@elastic.co>
2022-12-12 11:16:26 -0500

    extract constant in scope

--- x-pack/plugins/observability/server/services/slo/transform_generators/apm_transaction_duration.test.ts
+++ x-pack/plugins/observability/server/services/slo/transform_generators/apm_transaction_duration.test.ts
@@ -43,13 +43,14 @@ describe('APM Transaction Duration Transform Generator', () => {
   });
 
   it('uses the provided index params as source index', async () => {
+    const index = 'my-custom-apm-index*';
     const anSLO = createSLO({
       indicator: createAPMTransactionDurationIndicator({
-        index: 'my-custom-apm-index*',
+        index,
       }),
     });
     const transform = generator.getTransformParams(anSLO);
 
-    expect(transform.source.index).toEqual('my-custom-apm-index*');
+    expect(transform.source.index).toEqual(index);
   });
 });
--- x-pack/plugins/observability/server/services/slo/transform_generators/apm_transaction_error_rate.test.ts
+++ x-pack/plugins/observability/server/services/slo/transform_generators/apm_transaction_error_rate.test.ts
@@ -52,13 +52,14 @@ describe('APM Transaction Error Rate Transform Generator', () => {
   });
 
   it('uses the provided index params as source index', async () => {
+    const index = 'my-custom-apm-index*';
     const anSLO = createSLO({
       indicator: createAPMTransactionErrorRateIndicator({
-        index: 'my-custom-apm-index*',
+        index,
       }),
     });
     const transform = generator.getTransformParams(anSLO);
 
-    expect(transform.source.index).toEqual('my-custom-apm-index*');
+    expect(transform.source.index).toEqual(index);
   });
 });
