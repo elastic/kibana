7c4be0c626eef63d6ad41ad0cbd8c4fbd837a752
Coen Warmer <coen.warmer@gmail.com>
2022-12-13 21:03:55 +0100

    Add SLO searching

--- x-pack/plugins/observability/public/pages/slos/components/slo_list.tsx
+++ x-pack/plugins/observability/public/pages/slos/components/slo_list.tsx
@@ -5,19 +5,24 @@
  * 2.0.
  */
 
-import React, { useEffect, useState } from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiPagination } from '@elastic/eui';
+import React, { useEffect, useMemo, useState } from 'react';
+import { EuiFieldSearch, EuiFlexGroup, EuiFlexItem, EuiPagination } from '@elastic/eui';
+import { debounce } from 'lodash';
+import { i18n } from '@kbn/i18n';
 
 import { useFetchSloList } from '../../../hooks/slo/use_fetch_slo_list';
 import { SloListItem } from './slo_list_item';
 
 export function SloList() {
   const [activePage, setActivePage] = useState(0);
+  const [query, setQuery] = useState('');
+
   const [shouldReload, setShouldReload] = useState(false);
 
   const {
+    loading,
     sloList: { results: slos = [], total, perPage },
-  } = useFetchSloList({ page: activePage + 1, refetch: shouldReload });
+  } = useFetchSloList({ page: activePage + 1, name: query, refetch: shouldReload });
 
   useEffect(() => {
     if (shouldReload) {
@@ -34,8 +39,25 @@ export function SloList() {
     setShouldReload(true);
   };
 
+  const handleChangeQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const debounceChange = useMemo(() => debounce(handleChangeQuery, 300), []);
+
   return (
     <EuiFlexGroup direction="column" gutterSize="m" data-test-subj="sloList">
+      <EuiFlexItem grow>
+        <EuiFieldSearch
+          fullWidth
+          isLoading={loading}
+          onChange={debounceChange}
+          placeholder={i18n.translate('observability.slos.list.search', {
+            defaultMessage: 'Search',
+          })}
+        />
+      </EuiFlexItem>
+
       <EuiFlexItem>
         <EuiFlexGroup direction="column" gutterSize="s">
           {slos.length
@@ -47,6 +69,7 @@ export function SloList() {
             : null}
         </EuiFlexGroup>
       </EuiFlexItem>
+
       <EuiFlexItem>
         <EuiFlexGroup direction="column" gutterSize="s" alignItems="flexEnd">
           <EuiFlexItem>
