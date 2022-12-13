c31b8dd0a5656777fc706003ba7f38a3bcb36e6b
Julian Gernun <17549662+jcger@users.noreply.github.com>
2022-12-13 11:31:34 +0100

    remove forgotten log

--- x-pack/plugins/triggers_actions_ui/public/application/sections/common/components/rule_quick_edit_buttons.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/common/components/rule_quick_edit_buttons.tsx
@@ -104,7 +104,6 @@ export const RuleQuickEditButtons: React.FunctionComponent<ComponentOpts> = ({
         });
       }
     } catch (e) {
-      console.log(e);
       toasts.addDanger({
         title: i18n.translate(
           'xpack.triggersActionsUI.sections.rulesList.bulkActionPopover.failedToUpdateRuleAPIKeysMessage',
