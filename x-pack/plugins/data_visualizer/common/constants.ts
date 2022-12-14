61813d5eff1706c482b1b240dc44bb2a5d71499b
Julia Rechkunova <julia.rechkunova@elastic.co>
2022-12-14 17:26:59 +0100

    [UnifiedFieldList] Revert some locale changes

--- x-pack/plugins/data_visualizer/common/constants.ts
+++ x-pack/plugins/data_visualizer/common/constants.ts
@@ -71,7 +71,6 @@ const UNKNOWN_FIELD_TYPE_DESC = i18n.translate(
   }
 );
 
-// TODO: unify with `getFieldTypeDescription` from UnifiedFieldList plugin
 export function getFieldTypeDescription(type: string, docLinks: DocLinksStart) {
   switch (type) {
     case SUPPORTED_FIELD_TYPES.BOOLEAN:
