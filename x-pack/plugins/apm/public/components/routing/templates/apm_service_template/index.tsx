89d76d32a4396fed2146f47c83a8421c31897d91
Kate Patticha <aikaterini.patticha@elastic.co>
2022-12-14 19:58:54 +0100

    Fix pathname

--- x-pack/plugins/apm/public/components/routing/templates/apm_service_template/index.tsx
+++ x-pack/plugins/apm/public/components/routing/templates/apm_service_template/index.tsx
@@ -110,10 +110,7 @@ function TemplateWithContext({
 
   if (isMobileAgentName(agentName)) {
     replace(history, {
-      pathname: location.pathname.replace(
-        '/apm/services/',
-        '/apm/mobile-services/'
-      ),
+      pathname: location.pathname.replace('/services/', '/mobile-services/'),
     });
   }
 
