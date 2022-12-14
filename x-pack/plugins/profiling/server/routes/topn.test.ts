5ae5f7a7003ba4adf1d9731d458551aacf7904a2
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-12-14 15:02:05 +0100

    Update test to use transport.request

--- x-pack/plugins/profiling/server/routes/topn.test.ts
+++ x-pack/plugins/profiling/server/routes/topn.test.ts
@@ -42,9 +42,16 @@ describe('TopN data from Elasticsearch', () => {
       (operationName, request) =>
         context.elasticsearch.client.asCurrentUser.search(request) as Promise<any>
     ),
-    transport: jest.fn(
-      (operationName, request) =>
-        context.elasticsearch.client.asCurrentUser.search(request) as Promise<any>
+    profilingStacktraces: jest.fn(
+      (request) =>
+        context.elasticsearch.client.asCurrentUser.transport.request({
+          method: 'POST',
+          path: encodeURI('_profiling/stacktraces'),
+          body: {
+            query: request.query,
+            sample_size: request.sampleSize,
+          },
+        }) as Promise<any>
     ),
   };
   const logger = loggerMock.create();
