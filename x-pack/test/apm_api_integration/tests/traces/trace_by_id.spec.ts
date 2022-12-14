66443f73a58894c3b2c4360fd68feb306e9d767c
cauemarcondes <caue.marcondes@elastic.co>
2022-12-14 14:43:30 -0500

    fix test

--- x-pack/test/apm_api_integration/tests/traces/trace_by_id.spec.ts
+++ x-pack/test/apm_api_integration/tests/traces/trace_by_id.spec.ts
@@ -50,6 +50,7 @@ export default function ApiTest({ getService }: FtrProviderContext) {
           traceDocs: [],
           errorDocs: [],
           spanLinksCountById: {},
+          totalItems: 0,
         },
       });
     });
