69f63093946a181a4ad4801f1145af2a7def7094
Matthew Kime <matt@mattki.me>
2022-12-14 18:23:55 -0600

    Update src/plugins/bfetch/common/bfetch_error.ts
    Co-authored-by: gchaps <33642766+gchaps@users.noreply.github.com>

--- src/plugins/bfetch/common/bfetch_error.ts
+++ src/plugins/bfetch/common/bfetch_error.ts
@@ -24,7 +24,7 @@ export class BfetchRequestError extends Error {
             defaultMessage: 'Please check your network connection and try again.',
           })
         : i18n.translate('bfetch.networkErrorWithStatus', {
-            defaultMessage: 'Please check your network connection and try again. Code {code}',
+            defaultMessage: 'Check your network connection and try again. Code {code}',
             values: { code },
           });
 
