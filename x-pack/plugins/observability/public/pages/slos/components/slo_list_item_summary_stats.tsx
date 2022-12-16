bdf028420fc2dd8f6c731ca491b8f4cf55644d3e
Coen Warmer <coen.warmer@gmail.com>
2022-12-16 12:56:05 +0100

    Make SummaryStats responsive

--- x-pack/plugins/observability/public/pages/slos/components/slo_list_item_summary_stats.tsx
+++ x-pack/plugins/observability/public/pages/slos/components/slo_list_item_summary_stats.tsx
@@ -18,53 +18,60 @@ export interface SloListItemSummaryStatsProps {
 
 export function SloListItemSummaryStats({ slo }: SloListItemSummaryStatsProps) {
   const isHealthy = isSloHealthy(slo);
-
   const { label } = getSloDifference(slo);
 
   return (
-    <EuiFlexGroup direction="row" responsive={false}>
-      <EuiFlexItem grow={false} style={{ width: 120 }}>
-        <EuiStat
-          description={i18n.translate('xpack.observability.slos.slo.stats.observedValue', {
-            defaultMessage: 'Observed value',
-          })}
-          title={asPercentWithTwoDecimals(slo.summary.sliValue, 1, 'n/a')}
-          titleColor={isHealthy ? '' : 'danger'}
-          titleSize="m"
-          reverse
-        />
+    <EuiFlexGroup direction="row">
+      <EuiFlexItem>
+        <EuiFlexGroup direction="row" responsive={false}>
+          <EuiFlexItem grow={false} style={{ width: 120 }}>
+            <EuiStat
+              description={i18n.translate('xpack.observability.slos.slo.stats.observedValue', {
+                defaultMessage: 'Observed value',
+              })}
+              title={asPercentWithTwoDecimals(slo.summary.sliValue, 1, 'n/a')}
+              titleColor={isHealthy ? '' : 'danger'}
+              titleSize="m"
+              reverse
+            />
+          </EuiFlexItem>
+          <EuiFlexItem grow={false} style={{ width: 110 }}>
+            <EuiStat
+              description={i18n.translate('xpack.observability.slos.slo.stats.slo', {
+                defaultMessage: 'SLO',
+              })}
+              title={asPercentWithTwoDecimals(slo.objective.target, 1, 'n/a')}
+              titleSize="m"
+              reverse
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>
       </EuiFlexItem>
-      <EuiFlexItem grow={false} style={{ width: 110 }}>
-        <EuiStat
-          description={i18n.translate('xpack.observability.slos.slo.stats.slo', {
-            defaultMessage: 'SLO',
-          })}
-          title={asPercentWithTwoDecimals(slo.objective.target, 1, 'n/a')}
-          titleSize="m"
-          reverse
-        />
-      </EuiFlexItem>
-      <EuiFlexItem grow={false} style={{ width: 120 }}>
-        <EuiStat
-          description={i18n.translate('xpack.observability.slos.slo.stats.difference', {
-            defaultMessage: 'Difference',
-          })}
-          title={label}
-          titleColor={isHealthy ? '' : 'danger'}
-          titleSize="m"
-          reverse
-        />
-      </EuiFlexItem>
-      <EuiFlexItem grow={false} style={{ width: 120 }}>
-        <EuiStat
-          description={i18n.translate('xpack.observability.slos.slo.stats.budgetRemaining', {
-            defaultMessage: 'Budget remaining',
-          })}
-          title={asPercentWithTwoDecimals(slo.summary.errorBudget.remaining, 1, 'n/a')}
-          titleColor={isHealthy ? '' : 'danger'}
-          titleSize="m"
-          reverse
-        />
+      <EuiFlexItem>
+        <EuiFlexGroup direction="row" responsive={false}>
+          <EuiFlexItem grow={false} style={{ width: 120 }}>
+            <EuiStat
+              description={i18n.translate('xpack.observability.slos.slo.stats.difference', {
+                defaultMessage: 'Difference',
+              })}
+              title={label}
+              titleColor={isHealthy ? '' : 'danger'}
+              titleSize="m"
+              reverse
+            />
+          </EuiFlexItem>
+          <EuiFlexItem grow={false} style={{ width: 120 }}>
+            <EuiStat
+              description={i18n.translate('xpack.observability.slos.slo.stats.budgetRemaining', {
+                defaultMessage: 'Budget remaining',
+              })}
+              title={asPercentWithTwoDecimals(slo.summary.errorBudget.remaining, 1, 'n/a')}
+              titleColor={isHealthy ? '' : 'danger'}
+              titleSize="m"
+              reverse
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>
       </EuiFlexItem>
     </EuiFlexGroup>
   );
