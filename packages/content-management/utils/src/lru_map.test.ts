9b26f6905f48791496bda05143f1879eb4cf5206
vadimkibana <vadimkibana@gmail.com>
2022-12-18 22:50:06 +0100

    add LRU cache utils class

---
+++ packages/content-management/utils/src/lru_map.test.ts
@@ -0,0 +1,89 @@
+import {LruMap} from './lru_map';
+
+describe('LruMap', () => {
+  test('.get() / .set() / .size', () => {
+    const lru = new LruMap(123);
+    lru.set('foo', 1);
+    expect(lru.size).toBe(1);
+    lru.set('bar', 2);
+    expect(lru.get('foo')).toBe(1);
+    expect(lru.get('bar')).toBe(2);
+    expect(lru.size).toBe(2);
+  });
+
+  test('keeps limit', () => {
+    const lru = new LruMap(2);
+    lru.set('1', 1);
+    expect(lru.size).toBe(1);
+    lru.set('2', 2);
+    expect(lru.size).toBe(2);
+    lru.set('3', 3);
+    expect(lru.size).toBe(2);
+    lru.set('4', 4);
+    expect(lru.size).toBe(2);
+    expect(lru.get('1')).toBe(undefined);
+    expect(lru.get('2')).toBe(undefined);
+    expect(lru.get('3')).toBe(3);
+    expect(lru.get('4')).toBe(4);
+  });
+
+  test('rotates keys on .get()', () => {
+    const lru = new LruMap(3);
+    lru.set('1', 1);
+    expect(lru.size).toBe(1);
+    lru.set('2', 2);
+    expect(lru.size).toBe(2);
+    lru.set('3', 3);
+    expect(lru.size).toBe(3);
+    expect(lru.get('2')).toBe(2);
+    lru.set('4', 4);
+    expect(lru.size).toBe(3);
+    expect(lru.get('1')).toBe(undefined);
+    expect(lru.get('2')).toBe(2);
+  });
+
+  test('rotates keys on .set()', () => {
+    const lru = new LruMap(3);
+    lru.set('1', 1);
+    expect(lru.size).toBe(1);
+    lru.set('2', 2);
+    expect(lru.size).toBe(2);
+    lru.set('3', 3);
+    expect(lru.size).toBe(3);
+    lru.set('2', 2);
+    lru.set('4', 4);
+    expect(lru.size).toBe(3);
+    expect(lru.get('1')).toBe(undefined);
+    expect(lru.get('2')).toBe(2);
+  });
+
+  test('should drop key least used on max size', () => {
+    const cache = new LruMap(3);
+    cache.set('a', '1');
+    cache.set('b', '2');
+    cache.set('c', '3');
+    expect(cache.get('a')).toBe('1');
+    expect(cache.get('b')).toBe('2');
+    expect(cache.get('c')).toBe('3');
+    cache.set('d', '4');
+    expect(cache.get('d')).toBe('4');
+    expect(cache.get('a')).toBe(undefined);
+    cache.get('b');
+    cache.set('e', '5');
+    expect(cache.get('b')).toBe('2');
+    expect(cache.get('e')).toBe('5');
+    expect(cache.get('c')).toBe(undefined);
+  });
+
+  test('should remove all objects on reset', () => {
+    const cache = new LruMap(3);
+    cache.set('a', '1');
+    cache.set('b', '2');
+    cache.set('c', '3');
+    expect(cache.size).toBe(3);
+    expect([...cache.keys()].length).toBe(3);
+    cache.clear();
+    expect(cache.size).toBe(0);
+    expect([...cache.keys()].length).toBe(0);
+  });
+});
