1c5545cefe1ba1bd15c4b621ecc7bfe70f9feba5
Patryk Kopycinski <contact@patrykkopycinski.com>
2022-12-20 00:25:39 +0100

    WIP

---
+++ x-pack/plugins/security_solution/public/management/cypress.config.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'cypress';
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  defaultCommandTimeout: 60000,
+  execTimeout: 120000,
+  pageLoadTimeout: 12000,
+
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+
+  screenshotsFolder:
+    '../../../target/kibana-security-solution/public/management/cypress/screenshots',
+  trashAssetsBeforeRuns: false,
+  video: false,
+  videosFolder: '../../../target/kibana-security-solution/public/management/cypress/videos',
+  viewportHeight: 900,
+  viewportWidth: 1440,
+  experimentalStudio: true,
+
+  env: {
+    'cypress-react-selector': {
+      root: '#security-solution-app',
+    },
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:5620',
+    supportFile: 'public/management/cypress/support/e2e.ts',
+    specPattern: 'public/management/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    experimentalSessionAndOrigin: true,
+    testIsolation: 'on',
+    experimentalRunAllSpecs: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
