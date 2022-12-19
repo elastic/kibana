87044495db8c52326a2427e5511255898fddf3a6
vadimkibana <vadimkibana@gmail.com>
2022-12-19 10:53:50 +0100

    cleanup plugin

--- src/plugins/content/public/plugin.ts
+++ src/plugins/content/public/plugin.ts
@@ -9,67 +9,51 @@
 import { CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
 import { ContentPluginSetup, ContentPluginStart } from './types';
 import { ContentService } from './service/content_service';
+import { i18n } from '@kbn/i18n';
+import {ContentItemDetails} from '@kbn/content-management-state';
 
 export class ContentPlugin implements Plugin<ContentPluginSetup, ContentPluginStart> {
   private content?: ContentService;
 
   public setup(core: CoreSetup): ContentPluginSetup {
     this.content = new ContentService();
-    const content = this.content.setup();
 
-    // content.registry.register({
-    //   id: 'dashboard',
-    //   name: i18n.translate('content.dashboard.name', {
-    //     defaultMessage: 'Dashboard',
-    //   }),
-    //   description: i18n.translate('content.dashboard.description', {
-    //     defaultMessage: 'Dashboard',
-    //   }),
-    //   icon: 'dashboardApp',
-    //   operations: {
-    //     read: async (id: string) => {
-    //       const [coreStart] = await core.getStartServices();
-    //       const so = coreStart.savedObjects.client;
-    //       const res = await so.get('dashboard', id);
-    //       const details: ContentItemDetails = {
-    //         id: res.id,
-    //         fields: {
-    //           title: res.attributes.title,
-    //           description: res.attributes.description,
-    //         },
-    //         data: res.attributes,
-    //       };
-    //       return details;
-    //     },
-    //     list: async () => {
-    //       const [coreStart] = await core.getStartServices();
-    //       const soc = coreStart.savedObjects.client;
-    //       const res = await soc.find({
-    //         type: 'dashboard',
-    //       });
-    //       const details: ContentItemDetails[] = res.savedObjects.map((so) => ({
-    //         id: so.id,
-    //         fields: {
-    //           title: so.attributes.title,
-    //           description: so.attributes.description,
-    //         },
-    //         data: so.attributes,
-    //       }));
-    //       return details;
-    //     },
-    //   },
-    // });
+    this.content.registry.register({
+      id: 'dashboard',
+      name: i18n.translate('content.dashboard.name', {
+        defaultMessage: 'Dashboard',
+      }),
+      description: i18n.translate('content.dashboard.description', {
+        defaultMessage: 'Dashboard',
+      }),
+      icon: 'dashboardApp',
+      operations: {
+        read: async (id: string) => {
+          const [coreStart] = await core.getStartServices();
+          const so = coreStart.savedObjects.client;
+          const res = await so.get('dashboard', id);
+          const attributes = res.attributes as { title: string; description: string };
+          const details: ContentItemDetails = {
+            id: res.id,
+            fields: {
+              title: attributes.title,
+              description: attributes.description,
+            },
+            content: attributes,
+          };
+          return details;
+        },
+      },
+    });
 
     return {
-      content,
+      content: this.content.setup(),
     };
   }
 
   public start(core: CoreStart): ContentPluginStart {
-    const content = this.content!.start();
-
     return {
-      content,
+      content: this.content!.start(),
     };
   }
 
