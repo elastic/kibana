e28d2778cd468076c9218274dcff961104c7d1c8
Kate Patticha <aikaterini.patticha@elastic.co>
2022-12-19 15:15:41 +0100

    [APM] Display mobile stats on service overview page

---
+++ x-pack/plugins/apm/public/components/app/mobile/service_overview/stats/metric_item.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React from 'react';
+import { Chart, Settings, Metric, MetricDatum } from '@elastic/charts';
+import { DARK_THEME } from '@elastic/charts';
+import { EuiLoadingContent, EuiPanel } from '@elastic/eui';
+
+export const MetricItem = ({
+  data,
+  id,
+  isLoading,
+}: {
+  data: MetricDatum[];
+  id: number;
+  isLoading: boolean;
+}) => {
+  return (
+    <div
+      style={{
+        resize: 'none',
+        padding: '0px',
+        overflow: 'auto',
+        height: '100px',
+        borderRadius: '6px',
+      }}
+    >
+      {isLoading ? (
+        <EuiPanel hasBorder={true}>
+          <EuiLoadingContent lines={3} />
+        </EuiPanel>
+      ) : (
+        <Chart>
+          <Settings baseTheme={DARK_THEME} />
+          <Metric id={`metric_${id}`} data={[data]} />
+        </Chart>
+      )}
+    </div>
+  );
+};
--- x-pack/plugins/apm/server/routes/mobile/route.ts
+++ x-pack/plugins/apm/server/routes/mobile/route.ts
@@ -10,6 +10,7 @@ import { getApmEventClient } from '../../lib/helpers/get_apm_event_client';
 import { createApmServerRoute } from '../apm_routes/create_apm_server_route';
 import { environmentRt, kueryRt, rangeRt } from '../default_api_types';
 import { getMobileFilters } from './get_mobile_filters';
+import { getMobileStats } from './get_mobile_stats';
 
 const mobileFilters = createApmServerRoute({
   endpoint: 'GET /internal/apm/services/{serviceName}/mobile/filters',
@@ -50,6 +51,49 @@ const mobileFilters = createApmServerRoute({
   },
 });
 
+const mobileStats = createApmServerRoute({
+  endpoint: 'GET /internal/apm/mobile-services/{serviceName}/stats',
+  params: t.type({
+    path: t.type({
+      serviceName: t.string,
+    }),
+    query: t.intersection([
+      kueryRt,
+      rangeRt,
+      environmentRt,
+      t.partial({
+        transactionType: t.string,
+        device: t.string,
+        osVersion: t.string,
+        appVersion: t.string,
+        netConnectionType: t.string,
+      }),
+    ]),
+  }),
+  options: { tags: ['access:apm'] },
+  handler: async (
+    resources
+  ): Promise<Awaited<ReturnType<typeof getMobileStats>>> => {
+    const apmEventClient = await getApmEventClient(resources);
+    const { params } = resources;
+    const { serviceName } = params.path;
+    const { kuery, environment, start, end, transactionType } = params.query;
+
+    const mobileMetrics = await getMobileStats({
+      kuery,
+      environment,
+      transactionType,
+      start,
+      end,
+      serviceName,
+      apmEventClient,
+    });
+
+    return mobileMetrics;
+  },
+});
+
 export const mobileRouteRepository = {
   ...mobileFilters,
+  ...mobileStats,
 };
