566668dddb35daff24f7c144786b2422b471e09f
Mark Hopkin <mark.hopkin@elastic.co>
2022-12-19 17:22:17 +0000

    update snapshots

--- x-pack/plugins/fleet/server/services/agents/build_status_runtime_field.test.ts
+++ x-pack/plugins/fleet/server/services/agents/build_status_runtime_field.test.ts
@@ -21,7 +21,44 @@ describe('buildStatusRuntimeField', () => {
         "status": Object {
           "script": Object {
             "lang": "painless",
-            "source": "long lastCheckinMillis = doc['last_checkin'].size() > 0? doc['last_checkin'].value.toInstant().toEpochMilli(): -1;if (doc['active'].size() > 0 && doc['active'].value == false) {  emit('unenrolled');  } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && false) {  emit('inactive');} else if (lastCheckinMillis > 0  && lastCheckinMillis  < (1234567590123L)) {emit('offline');  } else if (  doc['policy_revision_idx'].size() == 0 || (doc['upgrade_started_at'].size() > 0 &&doc['upgraded_at'].size() == 0  )) {emit('updating');  } else if (doc['last_checkin'].size() == 0) {  emit('enrolling');  } else if (doc['unenrollment_started_at'].size() > 0) {  emit('unenrolling');  } else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'error') {  emit('error');} else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'degraded') {emit('degraded');} else {emit('online');  }",
+            "source": "
+          long lastCheckinMillis = doc['last_checkin'].size() > 0 
+            ? doc['last_checkin'].value.toInstant().toEpochMilli() 
+            : -1;
+          if (doc['active'].size() > 0 && doc['active'].value == false) {
+            emit('unenrolled'); 
+          } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && false) {
+            emit('inactive');
+          } else if (
+              lastCheckinMillis > 0 
+              && lastCheckinMillis 
+              < (1234567590123L)
+          ) { 
+            emit('offline'); 
+          } else if (
+            doc['policy_revision_idx'].size() == 0 || (
+              doc['upgrade_started_at'].size() > 0 &&
+              doc['upgraded_at'].size() == 0
+            )
+          ) { 
+            emit('updating'); 
+          } else if (doc['last_checkin'].size() == 0) {
+            emit('enrolling'); 
+          } else if (doc['unenrollment_started_at'].size() > 0) {
+            emit('unenrolling'); 
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'error'
+          ) { 
+              emit('error');
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'degraded'
+          ) { 
+            emit('degraded');
+          } else { 
+            emit('online'); 
+          }",
           },
           "type": "keyword",
         },
@@ -36,7 +73,44 @@ describe('buildStatusRuntimeField', () => {
         "status": Object {
           "script": Object {
             "lang": "painless",
-            "source": "long lastCheckinMillis = doc['my.prefix.last_checkin'].size() > 0? doc['my.prefix.last_checkin'].value.toInstant().toEpochMilli(): -1;if (doc['my.prefix.active'].size() > 0 && doc['my.prefix.active'].value == false) {  emit('unenrolled');  } else if (lastCheckinMillis > 0 && doc['my.prefix.policy_id'].size() > 0 && false) {  emit('inactive');} else if (lastCheckinMillis > 0  && lastCheckinMillis  < (1234567590123L)) {emit('offline');  } else if (  doc['my.prefix.policy_revision_idx'].size() == 0 || (doc['my.prefix.upgrade_started_at'].size() > 0 &&doc['my.prefix.upgraded_at'].size() == 0  )) {emit('updating');  } else if (doc['my.prefix.last_checkin'].size() == 0) {  emit('enrolling');  } else if (doc['my.prefix.unenrollment_started_at'].size() > 0) {  emit('unenrolling');  } else if (  doc['my.prefix.last_checkin_status'].size() > 0 &&  doc['my.prefix.last_checkin_status'].value.toLowerCase() == 'error') {  emit('error');} else if (  doc['my.prefix.last_checkin_status'].size() > 0 &&  doc['my.prefix.last_checkin_status'].value.toLowerCase() == 'degraded') {emit('degraded');} else {emit('online');  }",
+            "source": "
+          long lastCheckinMillis = doc['my.prefix.last_checkin'].size() > 0 
+            ? doc['my.prefix.last_checkin'].value.toInstant().toEpochMilli() 
+            : -1;
+          if (doc['my.prefix.active'].size() > 0 && doc['my.prefix.active'].value == false) {
+            emit('unenrolled'); 
+          } else if (lastCheckinMillis > 0 && doc['my.prefix.policy_id'].size() > 0 && false) {
+            emit('inactive');
+          } else if (
+              lastCheckinMillis > 0 
+              && lastCheckinMillis 
+              < (1234567590123L)
+          ) { 
+            emit('offline'); 
+          } else if (
+            doc['my.prefix.policy_revision_idx'].size() == 0 || (
+              doc['my.prefix.upgrade_started_at'].size() > 0 &&
+              doc['my.prefix.upgraded_at'].size() == 0
+            )
+          ) { 
+            emit('updating'); 
+          } else if (doc['my.prefix.last_checkin'].size() == 0) {
+            emit('enrolling'); 
+          } else if (doc['my.prefix.unenrollment_started_at'].size() > 0) {
+            emit('unenrolling'); 
+          } else if (
+            doc['my.prefix.last_checkin_status'].size() > 0 &&
+            doc['my.prefix.last_checkin_status'].value.toLowerCase() == 'error'
+          ) { 
+              emit('error');
+          } else if (
+            doc['my.prefix.last_checkin_status'].size() > 0 &&
+            doc['my.prefix.last_checkin_status'].value.toLowerCase() == 'degraded'
+          ) { 
+            emit('degraded');
+          } else { 
+            emit('online'); 
+          }",
           },
           "type": "keyword",
         },
@@ -56,7 +130,44 @@ describe('buildStatusRuntimeField', () => {
         "status": Object {
           "script": Object {
             "lang": "painless",
-            "source": "long lastCheckinMillis = doc['last_checkin'].size() > 0? doc['last_checkin'].value.toInstant().toEpochMilli(): -1;if (doc['active'].size() > 0 && doc['active'].value == false) {  emit('unenrolled');  } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && (doc['policy_id'].value == 'policy-1') && lastCheckinMillis < 1234567590123L) {  emit('inactive');} else if (lastCheckinMillis > 0  && lastCheckinMillis  < (1234567590123L)) {emit('offline');  } else if (  doc['policy_revision_idx'].size() == 0 || (doc['upgrade_started_at'].size() > 0 &&doc['upgraded_at'].size() == 0  )) {emit('updating');  } else if (doc['last_checkin'].size() == 0) {  emit('enrolling');  } else if (doc['unenrollment_started_at'].size() > 0) {  emit('unenrolling');  } else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'error') {  emit('error');} else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'degraded') {emit('degraded');} else {emit('online');  }",
+            "source": "
+          long lastCheckinMillis = doc['last_checkin'].size() > 0 
+            ? doc['last_checkin'].value.toInstant().toEpochMilli() 
+            : -1;
+          if (doc['active'].size() > 0 && doc['active'].value == false) {
+            emit('unenrolled'); 
+          } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && (doc['policy_id'].value == 'policy-1') && lastCheckinMillis < 1234567590123L) {
+            emit('inactive');
+          } else if (
+              lastCheckinMillis > 0 
+              && lastCheckinMillis 
+              < (1234567590123L)
+          ) { 
+            emit('offline'); 
+          } else if (
+            doc['policy_revision_idx'].size() == 0 || (
+              doc['upgrade_started_at'].size() > 0 &&
+              doc['upgraded_at'].size() == 0
+            )
+          ) { 
+            emit('updating'); 
+          } else if (doc['last_checkin'].size() == 0) {
+            emit('enrolling'); 
+          } else if (doc['unenrollment_started_at'].size() > 0) {
+            emit('unenrolling'); 
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'error'
+          ) { 
+              emit('error');
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'degraded'
+          ) { 
+            emit('degraded');
+          } else { 
+            emit('online'); 
+          }",
           },
           "type": "keyword",
         },
@@ -76,7 +187,44 @@ describe('buildStatusRuntimeField', () => {
         "status": Object {
           "script": Object {
             "lang": "painless",
-            "source": "long lastCheckinMillis = doc['last_checkin'].size() > 0? doc['last_checkin'].value.toInstant().toEpochMilli(): -1;if (doc['active'].size() > 0 && doc['active'].value == false) {  emit('unenrolled');  } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && (doc['policy_id'].value == 'policy-1' || doc['policy_id'].value == 'policy-2') && lastCheckinMillis < 1234567590123L) {  emit('inactive');} else if (lastCheckinMillis > 0  && lastCheckinMillis  < (1234567590123L)) {emit('offline');  } else if (  doc['policy_revision_idx'].size() == 0 || (doc['upgrade_started_at'].size() > 0 &&doc['upgraded_at'].size() == 0  )) {emit('updating');  } else if (doc['last_checkin'].size() == 0) {  emit('enrolling');  } else if (doc['unenrollment_started_at'].size() > 0) {  emit('unenrolling');  } else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'error') {  emit('error');} else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'degraded') {emit('degraded');} else {emit('online');  }",
+            "source": "
+          long lastCheckinMillis = doc['last_checkin'].size() > 0 
+            ? doc['last_checkin'].value.toInstant().toEpochMilli() 
+            : -1;
+          if (doc['active'].size() > 0 && doc['active'].value == false) {
+            emit('unenrolled'); 
+          } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && (doc['policy_id'].value == 'policy-1' || doc['policy_id'].value == 'policy-2') && lastCheckinMillis < 1234567590123L) {
+            emit('inactive');
+          } else if (
+              lastCheckinMillis > 0 
+              && lastCheckinMillis 
+              < (1234567590123L)
+          ) { 
+            emit('offline'); 
+          } else if (
+            doc['policy_revision_idx'].size() == 0 || (
+              doc['upgrade_started_at'].size() > 0 &&
+              doc['upgraded_at'].size() == 0
+            )
+          ) { 
+            emit('updating'); 
+          } else if (doc['last_checkin'].size() == 0) {
+            emit('enrolling'); 
+          } else if (doc['unenrollment_started_at'].size() > 0) {
+            emit('unenrolling'); 
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'error'
+          ) { 
+              emit('error');
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'degraded'
+          ) { 
+            emit('degraded');
+          } else { 
+            emit('online'); 
+          }",
           },
           "type": "keyword",
         },
@@ -100,7 +248,44 @@ describe('buildStatusRuntimeField', () => {
         "status": Object {
           "script": Object {
             "lang": "painless",
-            "source": "long lastCheckinMillis = doc['last_checkin'].size() > 0? doc['last_checkin'].value.toInstant().toEpochMilli(): -1;if (doc['active'].size() > 0 && doc['active'].value == false) {  emit('unenrolled');  } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && (doc['policy_id'].value == 'policy-1' || doc['policy_id'].value == 'policy-2') && lastCheckinMillis < 1234567590123L || (doc['policy_id'].value == 'policy-3') && lastCheckinMillis < 1234567490123L) {  emit('inactive');} else if (lastCheckinMillis > 0  && lastCheckinMillis  < (1234567590123L)) {emit('offline');  } else if (  doc['policy_revision_idx'].size() == 0 || (doc['upgrade_started_at'].size() > 0 &&doc['upgraded_at'].size() == 0  )) {emit('updating');  } else if (doc['last_checkin'].size() == 0) {  emit('enrolling');  } else if (doc['unenrollment_started_at'].size() > 0) {  emit('unenrolling');  } else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'error') {  emit('error');} else if (  doc['last_checkin_status'].size() > 0 &&  doc['last_checkin_status'].value.toLowerCase() == 'degraded') {emit('degraded');} else {emit('online');  }",
+            "source": "
+          long lastCheckinMillis = doc['last_checkin'].size() > 0 
+            ? doc['last_checkin'].value.toInstant().toEpochMilli() 
+            : -1;
+          if (doc['active'].size() > 0 && doc['active'].value == false) {
+            emit('unenrolled'); 
+          } else if (lastCheckinMillis > 0 && doc['policy_id'].size() > 0 && (doc['policy_id'].value == 'policy-1' || doc['policy_id'].value == 'policy-2') && lastCheckinMillis < 1234567590123L || (doc['policy_id'].value == 'policy-3') && lastCheckinMillis < 1234567490123L) {
+            emit('inactive');
+          } else if (
+              lastCheckinMillis > 0 
+              && lastCheckinMillis 
+              < (1234567590123L)
+          ) { 
+            emit('offline'); 
+          } else if (
+            doc['policy_revision_idx'].size() == 0 || (
+              doc['upgrade_started_at'].size() > 0 &&
+              doc['upgraded_at'].size() == 0
+            )
+          ) { 
+            emit('updating'); 
+          } else if (doc['last_checkin'].size() == 0) {
+            emit('enrolling'); 
+          } else if (doc['unenrollment_started_at'].size() > 0) {
+            emit('unenrolling'); 
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'error'
+          ) { 
+              emit('error');
+          } else if (
+            doc['last_checkin_status'].size() > 0 &&
+            doc['last_checkin_status'].value.toLowerCase() == 'degraded'
+          ) { 
+            emit('degraded');
+          } else { 
+            emit('online'); 
+          }",
           },
           "type": "keyword",
         },
