edc44c39d62e96241462da47146e4c1299e1ddb9
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2022-12-19 17:59:38 +0200

    Added translations.

--- src/plugins/unified_search/public/query_string_input/close_filter_editor_confirm_modal.tsx
+++ src/plugins/unified_search/public/query_string_input/close_filter_editor_confirm_modal.tsx
@@ -8,26 +8,50 @@
 
 import React, { memo } from 'react';
 import { EuiConfirmModal } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
 
 interface CloseFilterEditorConfirmModalProps {
   onCancel: () => void;
   onConfirm: () => void;
 }
 
+const strings = {
+  getTitle: () =>
+    i18n.translate('unifiedSearch.filter.closeEditorConfirmModal.title', {
+      defaultMessage: 'Add filter',
+    }),
+  getCancelButton: () =>
+    i18n.translate('unifiedSearch.filter.closeEditorConfirmModal.cancelButton', {
+      defaultMessage: 'Cancel',
+    }),
+  getConfirmButton: () =>
+    i18n.translate('unifiedSearch.filter.closeEditorConfirmModal.confirmButton', {
+      defaultMessage: 'Close',
+    }),
+  getWarningLabel: () =>
+    i18n.translate('unifiedSearch.filter.closeEditorConfirmModal.warningLabel', {
+      defaultMessage: 'If you close this now you will lose any unsaved changes.',
+    }),
+  getConfirmationQuestionLabel: () =>
+    i18n.translate('unifiedSearch.filter.closeEditorConfirmModal.confirmationQuestionLabel', {
+      defaultMessage: 'Are you sure you want to do this?',
+    }),
+};
+
 export const CloseFilterEditorConfirmModal = memo(function CloseFilterEditorConfirmModal(
   props: CloseFilterEditorConfirmModalProps
 ) {
   return (
     <EuiConfirmModal
-      title="Filter in progress"
-      cancelButtonText="Cancel"
-      confirmButtonText="Close"
+      title={strings.getTitle()}
+      cancelButtonText={strings.getCancelButton()}
+      confirmButtonText={strings.getConfirmButton()}
       buttonColor="primary"
       defaultFocusedButton="confirm"
       {...props}
     >
-      <p>If you close this now you will lose any unsaved changes.</p>
-      <p>Are you sure you want to do this?</p>
+      <p>{strings.getWarningLabel()}</p>
+      <p>{strings.getConfirmationQuestionLabel()}</p>
     </EuiConfirmModal>
   );
 });
