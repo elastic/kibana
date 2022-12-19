e96b4e0c27106b46d57fb03067ca2938d2d5748a
Nodir Latipov <nodir.latypov@gmail.com>
2022-12-20 00:19:21 +0500

    fix: test change dataView

--- src/plugins/discover/public/__mocks__/services.ts
+++ src/plugins/discover/public/__mocks__/services.ts
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 import { Observable, of } from 'rxjs';
+import { SortDirection } from '@elastic/eui';
 import { EUI_CHARTS_THEME_LIGHT } from '@elastic/eui/dist/eui_charts_theme';
 import { DiscoverServices } from '../build_services';
 import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
@@ -133,6 +134,11 @@ export function createDiscoverServicesMock(): DiscoverServices {
       useChartsBaseTheme: jest.fn(() => EUI_CHARTS_THEME_LIGHT.theme),
     },
     storage: new LocalStorageMock({}) as unknown as Storage,
+    sortingService: {
+      getSorting: () => {
+        return { by: 'alphabetically', direction: SortDirection.ASC };
+      },
+    },
     addBasePath: jest.fn(),
     toastNotifications: {
       addInfo: jest.fn(),
