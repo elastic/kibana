1b47b300344e0ca996b20e4d2273122263010d7c
Paul Tavares <paul.tavares@elastic.co>
2022-12-20 17:14:14 -0500

    changes to `populatePackagePolicyAssignedAgentsCount()` to use new service method from package policy

--- x-pack/plugins/fleet/server/services/package_policies/populate_package_policy__assigned_agents_count.ts
+++ x-pack/plugins/fleet/server/services/package_policies/populate_package_policy__assigned_agents_count.ts
@@ -7,12 +7,9 @@
 
 import type { ElasticsearchClient } from '@kbn/core/server';
 
-import pMap from 'p-map';
+import { getAgentCountForAgentPolicies } from '../agent_policies/agent_policy_agent_count';
 
 import type { PackagePolicy } from '../../../common';
-
-import { AGENTS_PREFIX } from '../../../common';
-import { getAgentsByKuery } from '../agents';
 
 /**
  * Mutates each of the Package Policies passed on input and adds `agents` property to it with the
@@ -23,30 +20,14 @@ import { getAgentsByKuery } from '../agents';
 export const populatePackagePolicyAssignedAgentsCount = async (
   esClient: ElasticsearchClient,
   packagePolicies: PackagePolicy[]
-) => {
-  const agentPolicyRequestsByIdCache: Record<string, ReturnType<typeof getAgentsByKuery>> = {};
-
-  await pMap(
-    packagePolicies,
-    async (packagePolicy) => {
-      const agentPolicyId = packagePolicy.policy_id;
-      let agentsByQueryPromise: ReturnType<typeof getAgentsByKuery>;
-
-      if (Boolean(agentPolicyRequestsByIdCache[agentPolicyId])) {
-        agentsByQueryPromise = agentPolicyRequestsByIdCache[agentPolicyId];
-      } else {
-        agentsByQueryPromise = getAgentsByKuery(esClient, {
-          showInactive: false,
-          perPage: 0,
-          page: 1,
-          kuery: `${AGENTS_PREFIX}.policy_id:${agentPolicyId}`,
-        });
-
-        agentPolicyRequestsByIdCache[agentPolicyId] = agentsByQueryPromise;
-      }
-
-      packagePolicy.agents = (await agentsByQueryPromise).total;
-    },
-    { concurrency: 10 }
+): Promise<void> => {
+  const agentPolicyIds = Array.from(
+    new Set<string>(packagePolicies.map((policy) => policy.policy_id))
   );
+
+  const agentPolicyAgentCounts = await getAgentCountForAgentPolicies(esClient, agentPolicyIds);
+
+  for (const packagePolicy of packagePolicies) {
+    packagePolicy.agents = agentPolicyAgentCounts[packagePolicy.policy_id] ?? 0;
+  }
 };
