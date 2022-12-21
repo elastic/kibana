566ce75da22f1f7a91228c017bee1975228426bd
Abdul Zahid <awahab07@yahoo.com>
2022-12-22 00:32:37 +0100

    Hide "View History" button from monitor history page. Link "View History" button on monitor details page to Synthetics.

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_summary/test_runs_table.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_summary/test_runs_table.tsx
@@ -7,6 +7,7 @@
 
 import React, { MouseEvent, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
 import { i18n } from '@kbn/i18n';
 import {
   EuiBasicTable,
@@ -14,6 +15,7 @@ import {
   EuiButtonEmpty,
   EuiFlexGroup,
   EuiFlexItem,
+  EuiLink,
   EuiPanel,
   EuiText,
   EuiTitle,
@@ -21,13 +23,12 @@ import {
 import { Criteria } from '@elastic/eui/src/components/basic_table/basic_table';
 import { EuiTableSortingType } from '@elastic/eui/src/components/basic_table/table_types';
 
-import { useHistory, useParams } from 'react-router-dom';
-import { MONITOR_TYPES } from '../../../../../../common/constants';
+import { MONITOR_HISTORY_ROUTE, MONITOR_TYPES } from '../../../../../../common/constants';
 import { TestDetailsLink } from '../../common/links/test_details_link';
 import { ConfigKey, DataStream, Ping } from '../../../../../../common/runtime_types';
 import { formatTestDuration } from '../../../utils/monitor_test_result/test_time_formats';
-import { useSyntheticsSettingsContext } from '../../../contexts/synthetics_settings_context';
-
+import { useGetUrlParams } from '../../../hooks';
+import { stringifyUrlParams } from '../../../utils/url_params';
 import { sortPings } from '../../../utils/monitor_test_result/sort_pings';
 import { selectPingsError } from '../../../state';
 import { parseBadgeStatus, StatusBadge } from '../../common/monitor_test_result/status_badge';
@@ -43,12 +44,18 @@ interface TestRunsTableProps {
   from: string;
   to: string;
   paginable?: boolean;
+  showViewHistoryButton?: boolean;
 }
 
-export const TestRunsTable = ({ paginable = true, from, to }: TestRunsTableProps) => {
+export const TestRunsTable = ({
+  paginable = true,
+  from,
+  to,
+  showViewHistoryButton = true,
+}: TestRunsTableProps) => {
   const history = useHistory();
+  const params = useGetUrlParams();
   const { monitorId } = useParams<{ monitorId: string }>();
-  const { basePath } = useSyntheticsSettingsContext();
   const [page, setPage] = useState({ index: 0, size: 10 });
 
   const [sortField, setSortField] = useState<SortableField>('timestamp');
@@ -162,8 +169,6 @@ export const TestRunsTable = ({ paginable = true, from, to }: TestRunsTableProps
     };
   };
 
-  const historyIdParam =
-    monitor?.[ConfigKey.CUSTOM_HEARTBEAT_ID] ?? monitor?.[ConfigKey.MONITOR_QUERY_ID];
   return (
     <EuiPanel hasShadow={false} hasBorder css={{ minHeight: 200 }}>
       <EuiFlexGroup alignItems="center" gutterSize="s">
@@ -174,18 +179,34 @@ export const TestRunsTable = ({ paginable = true, from, to }: TestRunsTableProps
         </EuiFlexItem>
         <EuiFlexItem grow={true} />
         <EuiFlexItem grow={false}>
-          <EuiButtonEmpty
-            size="xs"
-            iconType="list"
-            iconSide="left"
-            data-test-subj="monitorSummaryViewLastTestRun"
-            disabled={!historyIdParam}
-            href={`${basePath}/app/uptime/monitor/${btoa(historyIdParam ?? '')}`}
-          >
-            {i18n.translate('xpack.synthetics.monitorDetails.summary.viewHistory', {
-              defaultMessage: 'View History',
-            })}
-          </EuiButtonEmpty>
+          {showViewHistoryButton ? (
+            <EuiLink
+              href={
+                monitor?.[ConfigKey.CONFIG_ID]
+                  ? history.createHref({
+                      pathname: MONITOR_HISTORY_ROUTE.replace(
+                        ':monitorId',
+                        monitor[ConfigKey.CONFIG_ID]
+                      ),
+                      search: stringifyUrlParams(
+                        { ...params, dateRangeStart: 'now-24h', dateRangeEnd: 'now' },
+                        true
+                      ),
+                    })
+                  : undefined
+              }
+            >
+              <EuiButtonEmpty
+                data-test-subj="monitorStatusChartViewHistoryButton"
+                size="xs"
+                iconType="list"
+              >
+                {i18n.translate('xpack.synthetics.monitorDetails.summary.viewHistory', {
+                  defaultMessage: 'View History',
+                })}
+              </EuiButtonEmpty>
+            </EuiLink>
+          ) : null}
         </EuiFlexItem>
       </EuiFlexGroup>
       <EuiBasicTable
