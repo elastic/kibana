0144b2ed8d50db567f4a762b0acea3af20cc59f7
Dario Gieselaar <dario.gieselaar@elastic.co>
2022-12-21 09:27:20 +0100

    [Profiling] Strip leading subdirs for frame group ID

--- x-pack/plugins/profiling/common/frame_group.ts
+++ x-pack/plugins/profiling/common/frame_group.ts
@@ -4,10 +4,14 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
+import { last } from 'lodash';
 import { StackFrameMetadata } from './profiling';
 
 export type FrameGroupID = string;
+
+function stripLeadingSubdirs(sourceFileName: string) {
+  return last(sourceFileName.split('/'));
+}
 
 // createFrameGroupID is the "standard" way of grouping frames, by commonly
 // shared group identifiers.
@@ -30,5 +34,5 @@ export function createFrameGroupID(
     return `elf;${exeFilename};${functionName}`;
   }
 
-  return `full;${exeFilename};${functionName};${sourceFilename}`;
+  return `full;${exeFilename};${functionName};${stripLeadingSubdirs(sourceFilename || '')}`;
 }
