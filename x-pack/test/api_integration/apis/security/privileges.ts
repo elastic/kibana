bf6bccbcbd89e6043c6d7a685e554369caa5ca69
Paulo Henrique <paulo.henrique@elastic.co>
2022-12-21 09:16:21 -0800

    removing feature under FF from functional test

--- x-pack/test/api_integration/apis/security/privileges.ts
+++ x-pack/test/api_integration/apis/security/privileges.ts
@@ -38,7 +38,6 @@ export default function ({ getService }: FtrProviderContext) {
         'actions_log_management_read',
         'host_isolation_all',
         'process_operations_all',
-        'kubernetes_commands_all',
       ],
       uptime: ['all', 'read', 'minimal_all', 'minimal_read'],
       securitySolutionCases: ['all', 'read', 'minimal_all', 'minimal_read', 'cases_delete'],
