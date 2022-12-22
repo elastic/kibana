411ddb9d159b6d189897757d3ecaec48da095069
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2022-12-22 14:53:34 -0500

    [7.17] defaultIndex setting description fix - its really a data view! (#147730) (#148022)
    # Backport
    
    This will backport the following commits from `main` to `7.17`:
    - [defaultIndex setting description fix - its really a data view!
    (#147730)](https://github.com/elastic/kibana/pull/147730)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Matthew
    Kime","email":"matt@mattki.me"},"sourceCommit":{"committedDate":"2022-12-22T17:14:56Z","message":"defaultIndex
    setting description fix - its really a data view! (#147730)\n\n##
    Summary\r\n\r\nThe defaultIndex setting title and description were poor.
    I've updated\r\nthem to correctly reference data views and be more
    specific about where\r\nthey're used.\r\n\r\nCloses
    https://github.com/elastic/kibana/issues/143609","sha":"b69770463981cc2270b3bb95ac8dd7e02741bf32","branchLabelMapping":{"^v8.7.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["Feature:Data
    Views","Feature:Kibana
    Management","release_note:skip","Team:DataDiscovery","backport:all-open","v8.7.0"],"number":147730,"url":"https://github.com/elastic/kibana/pull/147730","mergeCommit":{"message":"defaultIndex
    setting description fix - its really a data view! (#147730)\n\n##
    Summary\r\n\r\nThe defaultIndex setting title and description were poor.
    I've updated\r\nthem to correctly reference data views and be more
    specific about where\r\nthey're used.\r\n\r\nCloses
    https://github.com/elastic/kibana/issues/143609","sha":"b69770463981cc2270b3bb95ac8dd7e02741bf32"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v8.7.0","labelRegex":"^v8.7.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/147730","number":147730,"mergeCommit":{"message":"defaultIndex
    setting description fix - its really a data view! (#147730)\n\n##
    Summary\r\n\r\nThe defaultIndex setting title and description were poor.
    I've updated\r\nthem to correctly reference data views and be more
    specific about where\r\nthey're used.\r\n\r\nCloses
    https://github.com/elastic/kibana/issues/143609","sha":"b69770463981cc2270b3bb95ac8dd7e02741bf32"}}]}]
    BACKPORT-->
    
    Co-authored-by: Matthew Kime <matt@mattki.me>

--- src/plugins/data/server/ui_settings.ts
+++ src/plugins/data/server/ui_settings.ts
@@ -164,12 +164,12 @@ export function getUiSettings(): Record<string, UiSettingsParams<unknown>> {
     },
     defaultIndex: {
       name: i18n.translate('data.advancedSettings.defaultIndexTitle', {
-        defaultMessage: 'Default index',
+        defaultMessage: 'Default data view',
       }),
       value: null,
       type: 'string',
       description: i18n.translate('data.advancedSettings.defaultIndexText', {
-        defaultMessage: 'The index to access if no index is set',
+        defaultMessage: 'Used by discover and visualizations when a data view is not set.',
       }),
       schema: schema.nullable(schema.string()),
     },
