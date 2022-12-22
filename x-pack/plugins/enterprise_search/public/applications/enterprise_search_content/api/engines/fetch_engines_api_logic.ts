dd797a0b154dfd6c50446eb0cb2843658148c565
saarikabhasi <nairsaarika.bhasi@elastic.co>
2022-12-22 11:58:14 -0500

    PR comments - Move mocked engine to Fetch-API

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/api/engines/fetch_engines_api_logic.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/api/engines/fetch_engines_api_logic.ts
@@ -6,14 +6,62 @@
  */
 
 import { createApiLogic } from '../../../shared/api_logic/create_api_logic';
-import { mockedEngines } from '../../components/engines/_mocks_/mocked_engines';
-import { EngineListDetails } from '../../components/engines/types';
 
-export type FetchEnginesApiLogicResponse = EngineListDetails[];
+import { EnginesListAPIResponse } from '../../components/engines/types';
+
+// export type FetchEnginesApiLogicResponse = EnginesListAPIResponse[];
+export const mockedEngines: EnginesListAPIResponse[] = [
+  {
+    meta: {
+      from: 0,
+      size: 5,
+      total: 10,
+    },
+    results: [
+      {
+        name: 'engine-name-1',
+        indices: ['index-18', 'index-23'],
+        last_updated: '10 Aug',
+        document_count: 10,
+      },
+      {
+        name: 'engine-name-2',
+        indices: ['index-123', 'index-2345'],
+        last_updated: '10 Aug',
+        document_count: 10,
+      },
+      {
+        name: 'engine-name-1',
+        indices: ['index-18', 'index-23'],
+        last_updated: '10 Aug',
+        document_count: 10,
+      },
+      {
+        name: 'engine-name-2',
+        indices: ['index-123', 'index-2345'],
+        last_updated: '10 Aug',
+        document_count: 10,
+      },
+      {
+        name: 'engine-name-1',
+        indices: ['index-18', 'index-23'],
+        last_updated: '10 Aug',
+        document_count: 10,
+      },
+      {
+        name: 'engine-name-2',
+        indices: ['index-123', 'index-2345'],
+        last_updated: '10 Aug',
+        document_count: 10,
+      },
+    ],
+  },
+];
 
 export const fetchEngines = async () => {
-  // TODO replace with http call when backed is ready
-  return mockedEngines;
+  // TODO replace with http call when backend is ready
+
+  return mockedEngines[0];
 };
 
 export const FetchEnginesAPILogic = createApiLogic(['content', 'engines_api_logic'], fetchEngines);
