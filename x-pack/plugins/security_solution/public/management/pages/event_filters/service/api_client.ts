0bd90e703a7e81ca47b1b5925d7a5bb932d0f96e
David Sánchez <david.sanchezsoler@elastic.co>
2022-12-22 18:12:39 +0100

    [Security Solution][Endpoint] Removes id field on nested entries for event filters (#148000)
    ## Summary
    
    - Removes id field on nested entries for event filters using the write
    transform method.
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    (cherry picked from commit 6bdbf70fe3efc6fdf1877aaabbe4e525411b6d31)
    
    # Conflicts:
    #	x-pack/plugins/security_solution/public/management/pages/event_filters/service/api_client.ts

--- x-pack/plugins/security_solution/public/management/pages/event_filters/service/api_client.ts
+++ x-pack/plugins/security_solution/public/management/pages/event_filters/service/api_client.ts
@@ -7,8 +7,19 @@
 
 import { ENDPOINT_EVENT_FILTERS_LIST_ID } from '@kbn/securitysolution-list-constants';
 import type { HttpStart } from '@kbn/core/public';
+import type {
+  CreateExceptionListItemSchema,
+  UpdateExceptionListItemSchema,
+} from '@kbn/securitysolution-io-ts-list-types';
+import { removeIdFromExceptionItemsEntries } from '@kbn/securitysolution-list-hooks';
 import { ExceptionsListApiClient } from '../../../services/exceptions_list/exceptions_list_api_client';
 import { EVENT_FILTER_LIST_DEFINITION } from '../constants';
+
+function writeTransform<T extends CreateExceptionListItemSchema | UpdateExceptionListItemSchema>(
+  item: T
+): T {
+  return removeIdFromExceptionItemsEntries(item) as T;
+}
 
 /**
  * Event filters Api client class using ExceptionsListApiClient as base class
@@ -17,10 +28,22 @@ import { EVENT_FILTER_LIST_DEFINITION } from '../constants';
  */
 export class EventFiltersApiClient extends ExceptionsListApiClient {
   constructor(http: HttpStart) {
-    super(http, ENDPOINT_EVENT_FILTERS_LIST_ID, EVENT_FILTER_LIST_DEFINITION);
+    super(
+      http,
+      ENDPOINT_EVENT_FILTERS_LIST_ID,
+      EVENT_FILTER_LIST_DEFINITION,
+      undefined,
+      writeTransform
+    );
   }
 
   public static getInstance(http: HttpStart): ExceptionsListApiClient {
-    return super.getInstance(http, ENDPOINT_EVENT_FILTERS_LIST_ID, EVENT_FILTER_LIST_DEFINITION);
+    return super.getInstance(
+      http,
+      ENDPOINT_EVENT_FILTERS_LIST_ID,
+      EVENT_FILTER_LIST_DEFINITION,
+      undefined,
+      writeTransform
+    );
   }
 }
