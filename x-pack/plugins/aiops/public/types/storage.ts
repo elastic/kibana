ef2454e62d0e6d3fd0ff652d6bf29dea967d0aeb
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2022-12-23 11:45:09 +0100

    use local storage package for aiops

---
+++ x-pack/plugins/aiops/public/types/storage.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export const AIOPS_FROZEN_TIER_PREFERENCE = 'aiops.frozenDataTierPreference';
+
+export const FROZEN_TIER_PREFERENCE = {
+  EXCLUDE: 'exclude-frozen',
+  INCLUDE: 'include-frozen',
+} as const;
+
+export type FrozenTierPreference =
+  typeof FROZEN_TIER_PREFERENCE[keyof typeof FROZEN_TIER_PREFERENCE];
+
+export type AiOps = Partial<{
+  [AIOPS_FROZEN_TIER_PREFERENCE]: FrozenTierPreference;
+}> | null;
+
+export type AiOpsKey = keyof Exclude<AiOps, null>;
+
+export type AiOpsStorageMapped<T extends AiOpsKey> = T extends typeof AIOPS_FROZEN_TIER_PREFERENCE
+  ? FrozenTierPreference | undefined
+  : null;
