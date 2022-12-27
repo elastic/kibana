854207542c121b4ec6845409cadde5cebcc7ea17
Nodir Latipov <nodir.latypov@gmail.com>
2022-12-27 20:35:54 +0500

    fix: open superDataPicker in one click

--- src/plugins/unified_search/public/query_string_input/query_string_input.tsx
+++ src/plugins/unified_search/public/query_string_input/query_string_input.tsx
@@ -622,7 +622,7 @@ export default class QueryStringInputUI extends PureComponent<QueryStringInputPr
           this.onSubmit(this.props.query);
         }
       }
-    }, 50);
+    }, 100);
   };
 
   private onInputBlur = () => {
