bc59457041e76660f29d5841255bad999cd0fe47
saarikabhasi <nairsaarika.bhasi@elastic.co>
2022-12-28 16:45:54 -0500

    Tests for Engine logic file

---
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engines/engine_list_logic.test.ts
@@ -0,0 +1,86 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { HttpError, Status } from '@kbn/enterprise-search-plugin/common/types/api';
+import { nextTick } from '@kbn/test-jest-helpers';
+import { LogicMounter, mockFlashMessageHelpers } from '../../../__mocks__/kea_logic';
+
+import { FetchEnginesAPILogic, mockedEngines } from '../../api/engines/fetch_engines_api_logic';
+
+import { EnginesListLogic } from './engines_list_logic';
+
+const DEFAULT_META = {
+  meta: {
+    from: 0,
+    size: 5,
+    total: 10,
+  },
+};
+const DEFAULT_VALUES = {
+  data: undefined,
+  enginesList: [], // meta is not added for jest case, pagination is yet to be implemented
+  // meta: DEFAULT_META,
+  status: 0,
+};
+
+describe('EnginesListLogic', () => {
+  const { mount: apiLogicMount } = new LogicMounter(FetchEnginesAPILogic);
+  const { mount } = new LogicMounter(EnginesListLogic);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
+    apiLogicMount();
+    mount();
+  });
+  it('has expected default values', () => {
+    expect(EnginesListLogic.values).toEqual(DEFAULT_VALUES);
+  });
+  describe('actions', () => {
+    describe('onPaginate', () => {
+      it('updates meta with newPageIndex', () => {});
+    });
+  });
+  describe('reducers', () => {});
+  describe('listeners', () => {
+    it('calls flashAPIErrors on apiError', () => {
+      EnginesListLogic.actions.apiError({} as HttpError);
+      expect(mockFlashMessageHelpers.flashAPIErrors).toHaveBeenCalledTimes(1);
+      expect(mockFlashMessageHelpers.flashAPIErrors).toHaveBeenCalledWith({});
+    });
+
+    it('calls makeRequest on fetchEngines', async () => {
+      jest.useFakeTimers({ legacyFakeTimers: true });
+      EnginesListLogic.actions.makeRequest = jest.fn();
+      EnginesListLogic.actions.fetchEngines({ meta: DEFAULT_META});
+      await nextTick();
+      expect(EnginesListLogic.actions.makeRequest).toHaveBeenCalledWith({
+        meta: DEFAULT_META,
+      });
+    });
+  });
+  describe('selectors', () => {
+    describe('enginesList', () => {
+      it('updates when apiSuccess listener triggered', () => {
+        expect(EnginesListLogic.values).toEqual(DEFAULT_VALUES);
+        EnginesListLogic.actions.apiSuccess({
+          indices: mockedEngines,
+          meta: DEFAULT_META,
+        });
+        expect(EnginesListLogic.values).toEqual({
+          ...DEFAULT_VALUES,
+          data: {
+            indices: mockedEngines,
+            meta: DEFAULT_META,
+          },
+
+          status: Status.SUCCESS,
+        });
+      });
+    });
+  });
+});
