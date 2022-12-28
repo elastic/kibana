a4324bc6cb971949301b82380ba7c9bdb7aec8f0
Nodir Latipov <nodir.latypov@gmail.com>
2022-12-28 21:33:48 +0500

    fix: jest test

--- src/plugins/unified_search/public/query_string_input/query_string_input.test.tsx
+++ src/plugins/unified_search/public/query_string_input/query_string_input.test.tsx
@@ -244,7 +244,7 @@ describe('QueryStringInput', () => {
 
     expect(mockCallback).toHaveBeenCalledTimes(0);
 
-    jest.advanceTimersByTime(100);
+    jest.advanceTimersByTime(300);
 
     expect(mockCallback).toHaveBeenCalledTimes(1);
     expect(mockCallback).toHaveBeenCalledWith(false);
@@ -273,7 +273,7 @@ describe('QueryStringInput', () => {
     expect(mockCallback).toHaveBeenCalledTimes(1);
     expect(mockCallback).toHaveBeenCalledWith(true);
 
-    jest.advanceTimersByTime(100);
+    jest.advanceTimersByTime(300);
     expect(mockCallback).toHaveBeenCalledTimes(1);
   });
 
@@ -297,7 +297,7 @@ describe('QueryStringInput', () => {
 
     expect(mockCallback).toHaveBeenCalledTimes(0);
 
-    jest.advanceTimersByTime(100);
+    jest.advanceTimersByTime(350);
 
     expect(mockCallback).toHaveBeenCalledTimes(1);
     expect(mockCallback).toHaveBeenCalledWith(kqlQuery);
