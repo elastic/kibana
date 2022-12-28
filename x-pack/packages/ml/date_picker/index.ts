0239f8c7801129cf3fcbda9f0d4b3fb6432376d7
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2022-12-28 11:52:58 +0100

    fix useTimeFilter and observable for data visualizer

--- x-pack/packages/ml/date_picker/index.ts
+++ x-pack/packages/ml/date_picker/index.ts
@@ -6,6 +6,7 @@
  */
 
 export { MlDatePickerContextProvider } from './src/hooks/use_ml_date_picker_context';
+export { useTimefilter } from './src/hooks/use_time_filter';
 export { MlDatePickerWrapper } from './src/components/ml_date_picker_wrapper';
 export {
   MlFullTimeRangeSelector,
@@ -15,4 +16,5 @@ export {
   getTimeFilterRange,
   type GetTimeFieldRangeResponse,
 } from './src/services/full_time_range_selector_service';
+export { mlDatePickerRefresh$, type Refresh } from './src/services/timefilter_refresh_service';
 export { type FrozenTierPreference, FROZEN_TIER_PREFERENCE } from './src/storage';
