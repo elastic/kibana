a2111f2d0dd726e06416fb7b4b6c9a4e1a1c5992
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-01-03 14:46:18 +0100

    [Discover] Unskip tests

--- test/functional/apps/discover/group1/_sidebar.ts
+++ test/functional/apps/discover/group1/_sidebar.ts
@@ -26,8 +26,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const filterBar = getService('filterBar');
   const fieldEditor = getService('fieldEditor');
 
-  // Failing: See https://github.com/elastic/kibana/issues/147687
-  describe.skip('discover sidebar', function describeIndexTests() {
+  describe('discover sidebar', function describeIndexTests() {
     before(async function () {
       await esArchiver.loadIfNeeded('test/functional/fixtures/es_archiver/logstash_functional');
     });
