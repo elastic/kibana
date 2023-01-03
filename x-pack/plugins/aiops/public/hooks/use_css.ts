f02424a6321eedf5252b393c5e2271a0d4479672
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-01-03 11:10:32 +0100

    Tweak hook structure.

--- x-pack/plugins/aiops/public/hooks/use_css.ts
+++ x-pack/plugins/aiops/public/hooks/use_css.ts
@@ -9,15 +9,17 @@ import { css } from '@emotion/react';
 import { useEuiBreakpoint } from '@elastic/eui';
 import { euiThemeVars } from '@kbn/ui-theme';
 
-export const useCss = () => {
-  const dataViewTitleHeader = css({
-    'min-width': '300px',
-    padding: `${euiThemeVars.euiSizeS} 0`,
-    display: 'flex',
-    'flex-direction': 'row',
-    'align-items': 'center',
-  });
+// Define fully static CSS outside hook.
+const dataViewTitleHeader = css({
+  'min-width': '300px',
+  padding: `${euiThemeVars.euiSizeS} 0`,
+  display: 'flex',
+  'flex-direction': 'row',
+  'align-items': 'center',
+});
 
+export const useCss = () => {
+  // Define CSS referencing inline dependencies within hook.
   const aiopsPageHeader = css({
     [useEuiBreakpoint(['xs', 's', 'm', 'l'])]: {
       'flex-direction': 'column',
