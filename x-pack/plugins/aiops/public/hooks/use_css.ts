cd2da59816676184c6238003aa96b6af4cde393b
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-01-03 11:05:42 +0100

    Migrate breakpoints to emotion for Explain Log Rate Spikes.

---
+++ x-pack/plugins/aiops/public/hooks/use_css.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { css } from '@emotion/react';
+import { useEuiBreakpoint } from '@elastic/eui';
+import { euiThemeVars } from '@kbn/ui-theme';
+
+export const useCss = () => {
+  const dataViewTitleHeader = css({
+    'min-width': '300px',
+    padding: `${euiThemeVars.euiSizeS} 0`,
+    display: 'flex',
+    'flex-direction': 'row',
+    'align-items': 'center',
+  });
+
+  const aiopsPageHeader = css({
+    [useEuiBreakpoint(['xs', 's', 'm', 'l'])]: {
+      'flex-direction': 'column',
+      'align-items': 'flex-start',
+    },
+  });
+
+  return { dataViewTitleHeader, aiopsPageHeader };
+};
