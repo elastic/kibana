a65a0eeb84a3974e125550164fdaad8e09906008
Kurt <kc13greiner@users.noreply.github.com>
2023-01-03 10:09:16 -0500

    Reverting unrelated commit

--- packages/core/rendering/core-rendering-server-internal/src/rendering_service.tsx
+++ packages/core/rendering/core-rendering-server-internal/src/rendering_service.tsx
@@ -17,7 +17,6 @@ import type { CoreContext } from '@kbn/core-base-server-internal';
 import type { KibanaRequest, HttpAuth } from '@kbn/core-http-server';
 import type { IUiSettingsClient } from '@kbn/core-ui-settings-server';
 import type { UiPlugins } from '@kbn/core-plugins-base-server-internal';
-import { AuthenticatedUser } from '@kbn/security-plugin/common';
 import { Template } from './views';
 import {
   IRenderOptions,
@@ -99,9 +98,6 @@ export class RenderingService {
     const basePath = http.basePath.get(request);
     const { serverBasePath, publicBaseUrl } = http.basePath;
 
-    const user: AuthenticatedUser = http.auth.get<AuthenticatedUser>(request).state ?? null;
-    console.log(user);
-
     const settings = {
       defaults: uiSettings.client?.getRegistered() ?? {},
       user: isAnonymousPage ? {} : await uiSettings.client?.getUserProvided(),
@@ -110,11 +106,6 @@ export class RenderingService {
       defaults: uiSettings.globalClient?.getRegistered() ?? {},
       user: isAnonymousPage ? {} : await uiSettings.globalClient?.getUserProvided(),
     };
-
-    // console.log(settings.user['theme:darkMode'].userValue);
-    // if (user) {
-    //   settings.user['theme:darkMode'].userValue = true;
-    // }
 
     let clusterInfo = {};
     try {
