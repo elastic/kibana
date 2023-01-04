33460cfb3713d81a606d803de2b30aca01aa8b4f
Maryam Saeidi <maryam.saeidi@elastic.co>
2023-01-04 15:35:00 +0100

    Correct storybook data and use mock data

--- x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/alert_summary/components/alert_summary_widget_full_size.stories.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/rule_details/components/alert_summary/components/alert_summary_widget_full_size.stories.tsx
@@ -6,6 +6,7 @@
  */
 
 import { AlertsSummaryWidgetFullSize as Component } from './alert_summary_widget_full_size';
+import { mockAlertSummaryResponse } from '../../../../../mock/alert_summary_widget';
 
 export default {
   component: Component,
@@ -14,43 +15,6 @@ export default {
 
 export const FullSize = {
   args: {
-    activeAlertCount: 94,
-    activeAlerts: [
-      { key: 1670112000000, doc_count: 0 },
-      { key: 1670198400000, doc_count: 0 },
-      { key: 1670284800000, doc_count: 0 },
-      { key: 1670371200000, doc_count: 2 },
-      { key: 1670457600000, doc_count: 4 },
-      { key: 1670544000000, doc_count: 5 },
-      { key: 1670630400000, doc_count: 3 },
-      { key: 1670716800000, doc_count: 6 },
-      { key: 1670803200000, doc_count: 14 },
-      { key: 1670889600000, doc_count: 15 },
-      { key: 1670976000000, doc_count: 15 },
-      { key: 1671062400000, doc_count: 10 },
-      { key: 1671148800000, doc_count: 9 },
-      { key: 1671235200000, doc_count: 7 },
-      { key: 1671321600000, doc_count: 2 },
-      { key: 1671408000000, doc_count: 2 },
-    ],
-    recoveredAlertCount: 15,
-    recoveredAlerts: [
-      { key: 1670112000000, doc_count: 0 },
-      { key: 1670198400000, doc_count: 0 },
-      { key: 1670284800000, doc_count: 0 },
-      { key: 1670371200000, doc_count: 0 },
-      { key: 1670457600000, doc_count: 0 },
-      { key: 1670544000000, doc_count: 0 },
-      { key: 1670630400000, doc_count: 2 },
-      { key: 1670716800000, doc_count: 0 },
-      { key: 1670803200000, doc_count: 0 },
-      { key: 1670889600000, doc_count: 0 },
-      { key: 1670976000000, doc_count: 0 },
-      { key: 1671062400000, doc_count: 5 },
-      { key: 1671148800000, doc_count: 1 },
-      { key: 1671235200000, doc_count: 2 },
-      { key: 1671321600000, doc_count: 5 },
-      { key: 1671408000000, doc_count: 0 },
-    ],
+    ...mockAlertSummaryResponse,
   },
 };
