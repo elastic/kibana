503b466b72865433cfa0148409e982aa39bc2f40
Dmitry Tomashevich <39378793+dimaanj@users.noreply.github.com>
2023-01-04 15:46:14 +0300

    [Discover][Alerting] Use Discover locator for alert results link (#146403)
    ## Summary
    
    Closes #145815, #134232
    
    - Moves Discover locator to common area
    - Builds alerts results link from the server
    - Now there are two implementations of `setStateToKbnUrl` which is used
    in locator. New one in common are lost `HashedItemStore` support, since
    sessions storage are actual only for browser
    - Toasts `Alert rule has changed`, `Data View has changed` removed
    - link generated per each alert will be unique representation of those
    `rule params` and `data view state` which were at the time of invocation
    - Restuls link will live even after data view and rule removal
    
    
    
    
    ### How to create rule
    
    - Create an output index and data view `test` 
    <details>
      <summary>Query to use</summary>
      
    ```
    PUT test
    {
        "settings" : {
            "number_of_shards" : 1
        },
        "mappings" : {
            "properties" : {
                "rule_id" : { "type" : "text" },
                "rule_name" : { "type" : "text" },
                "alert_id" : { "type" : "text" },
                "context_message": { "type" : "text" }
            }
        }
    }
    ```
    </details>
    
    - Create alerts connector using `test` index
    - Open `Elasticsearch query` alert in `KQL or Lucene` mode or just using
    Discover `Alerts` button
    - Specify the following params: `IS ABOVE: 1`, `FOR THE LAST: 30 min`
    - Try execute it by clicking `Test query`. It should match some results
    - When choosing connector, use the following config
    ```
    {
        "rule_id": "{{rule.id}}",
        "rule_name": "{{rule.name}}",
        "alert_id": "{{alert.id}}",
        "context_message": "{{context.message}}"
    }
    ```
    - Create the alert
    
    ### How to test
    
    - Create `Elasticsearch query` rule in `KQL or Lucene` mode like
    described above
    - Wait for some seconds and find the triggered alert document by
    browsing `test` data view in Discover. There should be a link to results
    in `context_message` field. Save the link somewhere
    - Change rule params by adding/removing filters / changing query /
    changing data view
    - Follow saved link, you should see previous filters, query and data
    view state
    - Open rule in management and click `View in app`, you should see actual
    state of rule
    - Try to remove used data view and then follow saved link, you should
    still see the results
    - Try to remove rule and then follow saved link, you should still see
    the results.
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    Co-authored-by: Davis McPhee <davis.mcphee@elastic.co>

---
+++ src/plugins/kibana_utils/common/state_management/format.test.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { replaceUrlHashQuery } from './format';
+
+describe('format', () => {
+  describe('replaceUrlHashQuery', () => {
+    it('should add hash query to url without hash', () => {
+      const url = 'http://localhost:5601/oxf/app/kibana';
+      expect(replaceUrlHashQuery(url, () => ({ test: 'test' }))).toMatchInlineSnapshot(
+        `"http://localhost:5601/oxf/app/kibana#?test=test"`
+      );
+    });
+
+    it('should replace hash query', () => {
+      const url = 'http://localhost:5601/oxf/app/kibana#?test=test';
+      expect(
+        replaceUrlHashQuery(url, (query) => ({
+          ...query,
+          test1: 'test1',
+        }))
+      ).toMatchInlineSnapshot(`"http://localhost:5601/oxf/app/kibana#?test=test&test1=test1"`);
+    });
+  });
+});
---
+++ src/plugins/kibana_utils/common/state_management/parse.test.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { parseUrlHash } from './parse';
+
+describe('parseUrlHash', () => {
+  it('should return null if no hash', () => {
+    expect(parseUrlHash('http://localhost:5601/oxf/app/kibana')).toBeNull();
+  });
+
+  it('should return parsed hash', () => {
+    expect(parseUrlHash('http://localhost:5601/oxf/app/kibana/#/path?test=test')).toMatchObject({
+      pathname: '/path',
+      query: {
+        test: 'test',
+      },
+    });
+  });
+});
