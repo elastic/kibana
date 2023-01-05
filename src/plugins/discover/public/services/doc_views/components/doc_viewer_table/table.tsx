0ed590cbf29dbf58ea4679525129894dd080d8d4
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-01-05 08:58:47 +0100

    [Discover] Support case-insensitive search in Document Viewer (#148312)
    Closes https://github.com/elastic/kibana/issues/147087
    
    ## Summary
    
    This PR:
    - adds support for case-insensitive search
    - adds search highlights
    - fixes the tooltip when a field has a custom label
    
    <img width="744" alt="Screenshot 2023-01-03 at 17 09 20"
    src="https://user-images.githubusercontent.com/1415710/210396131-529a3e76-a6cb-4026-b65a-018e1f627ac6.png">
    
    (cherry picked from commit 84693f6f312449327e86abfc5b1dc20871507da9)

--- src/plugins/discover/public/services/doc_views/components/doc_viewer_table/table.tsx
+++ src/plugins/discover/public/services/doc_views/components/doc_viewer_table/table.tsx
@@ -238,7 +238,7 @@ export const DocViewerTable = ({
         } else {
           const fieldMapping = mapping(curFieldName);
           const displayName = fieldMapping?.displayName ?? curFieldName;
-          if (displayName.includes(searchText)) {
+          if (displayName.toLowerCase().includes(searchText.toLowerCase())) {
             // filter only unpinned fields
             acc.restItems.push(fieldToItem(curFieldName));
           }
@@ -270,6 +270,7 @@ export const DocViewerTable = ({
   const headers = [
     !isSingleDocView && (
       <EuiTableHeaderCell
+        key="header-cell-actions"
         align="left"
         width={showActionsInsideTableCell ? 150 : 62}
         isSorted={false}
@@ -284,14 +285,14 @@ export const DocViewerTable = ({
         </EuiText>
       </EuiTableHeaderCell>
     ),
-    <EuiTableHeaderCell align="left" width="30%" isSorted={false}>
+    <EuiTableHeaderCell key="header-cell-name" align="left" width="30%" isSorted={false}>
       <EuiText size="xs">
         <strong>
           <FormattedMessage id="discover.fieldChooser.discoverField.name" defaultMessage="Field" />
         </strong>
       </EuiText>
     </EuiTableHeaderCell>,
-    <EuiTableHeaderCell align="left" isSorted={false}>
+    <EuiTableHeaderCell key="header-cell-value" align="left" isSorted={false}>
       <EuiText size="xs">
         <strong>
           <FormattedMessage id="discover.fieldChooser.discoverField.value" defaultMessage="Value" />
@@ -302,10 +303,11 @@ export const DocViewerTable = ({
 
   const renderRows = useCallback(
     (items: FieldRecord[]) => {
+      const highlight = searchText?.toLowerCase();
       return items.map(
         ({
           action: { flattenedField, onFilter },
-          field: { field, fieldMapping, displayName, fieldType, scripted, pinned },
+          field: { field, fieldMapping, fieldType, scripted, pinned },
           value: { formattedValue, ignored },
         }: FieldRecord) => {
           return (
@@ -341,10 +343,11 @@ export const DocViewerTable = ({
                 mobileOptions={MOBILE_OPTIONS}
               >
                 <FieldName
-                  fieldName={displayName}
+                  fieldName={field}
                   fieldType={fieldType}
                   fieldMapping={fieldMapping}
                   scripted={scripted}
+                  highlight={highlight}
                 />
               </EuiTableRowCell>
               <EuiTableRowCell
@@ -366,7 +369,7 @@ export const DocViewerTable = ({
         }
       );
     },
-    [onToggleColumn, onTogglePinned, isSingleDocView, showActionsInsideTableCell]
+    [onToggleColumn, onTogglePinned, isSingleDocView, showActionsInsideTableCell, searchText]
   );
 
   const rowElements = [
