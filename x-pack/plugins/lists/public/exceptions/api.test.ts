7d81489a7dbf90595e3f0bee0df5d13b4433b2b8
Davis Plumlee <davis.plumlee@elastic.co>
2023-01-09 16:31:15 -0500

    updates tests

--- x-pack/plugins/lists/public/exceptions/api.test.ts
+++ x-pack/plugins/lists/public/exceptions/api.test.ts
@@ -708,6 +708,7 @@ describe('Exceptions Lists API', () => {
         method: 'POST',
         query: {
           id: 'some-id',
+          include_expired_exceptions: 'true',
           list_id: 'list-id',
           namespace_type: 'single',
         },
