e399b261caadc1112e30d4e3e172189f069d518b
Shahzad <shahzad31comp@gmail.com>
2023-01-09 21:32:46 +0100

    [Synthetics] Error details page (#148375)
    Fixes https://github.com/elastic/kibana/issues/148377

--- x-pack/plugins/synthetics/public/apps/synthetics/components/error_details/hooks/use_error_details_breadcrumbs.ts
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/error_details/hooks/use_error_details_breadcrumbs.ts
@@ -6,38 +6,30 @@
  */
 import { i18n } from '@kbn/i18n';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
-import { useSelectedLocation } from '../../monitor_details/hooks/use_selected_location';
+import { useTestRunDetailsBreadcrumbs } from '../../test_run_details/hooks/use_test_run_details_breadcrumbs';
 import { useSelectedMonitor } from '../../monitor_details/hooks/use_selected_monitor';
-import { useBreadcrumbs } from '../../../hooks/use_breadcrumbs';
 import { ConfigKey } from '../../../../../../common/runtime_types';
-import { MONITOR_ROUTE, MONITORS_ROUTE } from '../../../../../../common/constants';
 import { PLUGIN } from '../../../../../../common/constants/plugin';
 
-export const useTestRunDetailsBreadcrumbs = (
+export const useErrorDetailsBreadcrumbs = (
   extraCrumbs?: Array<{ text: string; href?: string }>
 ) => {
   const kibana = useKibana();
   const appPath = kibana.services.application?.getUrlForApp(PLUGIN.SYNTHETICS_PLUGIN_ID) ?? '';
 
   const { monitor } = useSelectedMonitor();
-  const selectedLocation = useSelectedLocation();
 
-  useBreadcrumbs([
+  const errorsBreadcrumbs = [
     {
-      text: MONITOR_MANAGEMENT_CRUMB,
-      href: `${appPath}${MONITORS_ROUTE}`,
-    },
-    {
-      text: monitor?.name ?? '',
-      href: `${appPath}${MONITOR_ROUTE.replace(
-        ':monitorId',
-        monitor?.[ConfigKey.CONFIG_ID] ?? ''
-      )}?locationId=${selectedLocation?.id ?? ''}`,
+      text: ERRORS_CRUMB,
+      href: `${appPath}/monitor/${monitor?.[ConfigKey.CONFIG_ID]}/errors`,
     },
     ...(extraCrumbs ?? []),
-  ]);
+  ];
+
+  useTestRunDetailsBreadcrumbs(errorsBreadcrumbs);
 };
 
-const MONITOR_MANAGEMENT_CRUMB = i18n.translate('xpack.synthetics.monitorsPage.monitorsMCrumb', {
-  defaultMessage: 'Monitors',
+const ERRORS_CRUMB = i18n.translate('xpack.synthetics.monitorsPage.errors', {
+  defaultMessage: 'Errors',
 });
