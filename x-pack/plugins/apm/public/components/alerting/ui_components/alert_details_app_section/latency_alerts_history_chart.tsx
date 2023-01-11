1f29c837eb5eb1e4645a9ad5e15ff165f36dec80
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-01-11 14:39:47 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/apm/public/components/alerting/ui_components/alert_details_app_section/latency_alerts_history_chart.tsx
+++ x-pack/plugins/apm/public/components/alerting/ui_components/alert_details_app_section/latency_alerts_history_chart.tsx
@@ -20,7 +20,6 @@ import {
   Position,
 } from '@elastic/charts';
 import { EuiIcon } from '@elastic/eui';
-import { EuiBadge } from '@elastic/eui';
 import { getDurationFormatter } from '../../../../../common/utils/formatters';
 import { getLatencyChartSelector } from '../../../../selectors/latency_chart_selectors';
 import { LatencyAggregationType } from '../../../../../common/latency_aggregation_types';
