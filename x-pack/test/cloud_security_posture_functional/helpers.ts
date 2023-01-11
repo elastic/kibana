9f810febb6393a0f27fabe206a665cc5670e4ff4
Or Ouziel <or.ouziel@elastic.co>
2023-01-11 14:29:36 +0200

    update comment on testing new packages

--- x-pack/test/cloud_security_posture_functional/helpers.ts
+++ x-pack/test/cloud_security_posture_functional/helpers.ts
@@ -22,23 +22,16 @@ export const getCloudSecurityFleetConfig = () => [
   '--xpack.fleet.agentPolicies.0.package_policies.0.name=example-integration-csp',
   `--xpack.fleet.agentPolicies.0.package_policies.0.package.name=${CLOUD_SECURITY_POSTURE_PACKAGE_NAME}`,
 ];
-
 /**
  * Package version is fixed (not latest) so FTR won't suddenly break when package is changed.
  *
  * test a new package:
- * 1. run a local package registry serving a new package
- * 2. uncomment the 'registryUrl' flag
- * 3. update the version to the new package version
- * 4. run the FTR test
- *
- * when tests pass:
- * 1. merge the new package
- * 2. update the package version used in FTR
- * 3. merge the FTR changes with the new fixed package version
+ * 1. merge a new package to EPR
+ * 2. locally change the fixed version in kibana to the new package version
+ * 3. run the FTR test locally
+ * 4. when test pass, merge new kibana changes with the new package version.
  */
 const getCloudSecurityPackageConfig = () => [
   `--xpack.fleet.packages.0.name=${CLOUD_SECURITY_POSTURE_PACKAGE_NAME}`,
-  '--xpack.fleet.packages.0.version=1.0.8',
-  // '--xpack.fleet.registryUrl=http://localhost:8080',
+  '--xpack.fleet.packages.0.version=1.0.8', // fixed version
 ];
