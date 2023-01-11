763a1630155da6000add2ab404b28dc2d2f55d7e
klacabane <kevin.lacabane@elastic.co>
2023-01-11 22:12:50 +0100

    add entsearch test package

--- x-pack/test/monitoring_api_integration/packages.ts
+++ x-pack/test/monitoring_api_integration/packages.ts
@@ -7,7 +7,10 @@
 
 import path from 'path';
 
-const PACKAGES = [{ name: 'beat', version: '0.0.1' }];
+const PACKAGES = [
+  { name: 'beat', version: '0.0.1' },
+  { name: 'enterprisesearch', version: '0.1.0' },
+];
 
 export const getPackagesArgs = (): string[] => {
   return PACKAGES.flatMap((pkg, i) => {
