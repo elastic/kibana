a29b9565606dbf5075acf15a98f4c3391abfad62
Catherine Liu <catherine.liu@elastic.co>
2023-01-11 23:28:56 -0800

    Fixed a11y tests

--- src/plugins/presentation_util/public/components/index.tsx
+++ src/plugins/presentation_util/public/components/index.tsx
@@ -42,7 +42,7 @@ export const LazyDataViewPicker = React.lazy(() => import('./data_view_picker/da
 
 export const LazyFieldPicker = React.lazy(() => import('./field_picker/field_picker'));
 
-export const FloatingActions = React.lazy(() => import('./floating_actions/floating_actions'));
+export { FloatingActions } from './floating_actions/floating_actions';
 
 /**
  * A lazily-loaded ExpressionInput component.
