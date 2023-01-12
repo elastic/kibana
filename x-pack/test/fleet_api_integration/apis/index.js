61a39e7665c2bf28b0b8eeac9e408958ac9fd161
Mark Hopkin <mark.hopkin@elastic.co>
2023-01-12 00:20:57 +0000

    revert accidental test omits

--- x-pack/test/fleet_api_integration/apis/index.js
+++ x-pack/test/fleet_api_integration/apis/index.js
@@ -13,53 +13,53 @@ export default function ({ loadTestFile, getService }) {
       await setupTestUsers(getService('security'));
     });
 
-    // // EPM
-    // loadTestFile(require.resolve('./epm'));
+    // EPM
+    loadTestFile(require.resolve('./epm'));
 
-    // // Fleet setup
-    // loadTestFile(require.resolve('./fleet_setup'));
+    // Fleet setup
+    loadTestFile(require.resolve('./fleet_setup'));
 
-    // // Agents
-    // loadTestFile(require.resolve('./agents'));
+    // Agents
+    loadTestFile(require.resolve('./agents'));
 
-    // // Enrollment API keys
-    // loadTestFile(require.resolve('./enrollment_api_keys/crud'));
+    // Enrollment API keys
+    loadTestFile(require.resolve('./enrollment_api_keys/crud'));
 
-    // // Package policies
-    // loadTestFile(require.resolve('./package_policy/create'));
-    // loadTestFile(require.resolve('./package_policy/update'));
-    // loadTestFile(require.resolve('./package_policy/get'));
-    // loadTestFile(require.resolve('./package_policy/delete'));
-    loadTestFile(require.resolve('./package_policy/input_package_create_upgrade'));
+    // Package policies
+    loadTestFile(require.resolve('./package_policy/create'));
+    loadTestFile(require.resolve('./package_policy/update'));
+    loadTestFile(require.resolve('./package_policy/get'));
+    loadTestFile(require.resolve('./package_policy/delete'));
+    loadTestFile(require.resolve('./package_policy/upgrade'));
 
-    // // Agent policies
-    // loadTestFile(require.resolve('./agent_policy'));
+    // Agent policies
+    loadTestFile(require.resolve('./agent_policy'));
 
-    // // Data Streams
-    // loadTestFile(require.resolve('./data_streams'));
+    // Data Streams
+    loadTestFile(require.resolve('./data_streams'));
 
-    // // Settings
-    // loadTestFile(require.resolve('./settings'));
+    // Settings
+    loadTestFile(require.resolve('./settings'));
 
-    // // Service tokens
-    // loadTestFile(require.resolve('./service_tokens'));
+    // Service tokens
+    loadTestFile(require.resolve('./service_tokens'));
 
-    // // Outputs
-    // loadTestFile(require.resolve('./outputs'));
+    // Outputs
+    loadTestFile(require.resolve('./outputs'));
 
-    // // Download sources
-    // loadTestFile(require.resolve('./download_sources'));
+    // Download sources
+    loadTestFile(require.resolve('./download_sources'));
 
-    // // Telemetry
-    // loadTestFile(require.resolve('./fleet_telemetry'));
+    // Telemetry
+    loadTestFile(require.resolve('./fleet_telemetry'));
 
-    // // Integrations
-    // loadTestFile(require.resolve('./integrations'));
+    // Integrations
+    loadTestFile(require.resolve('./integrations'));
 
-    // // Fleet server hosts
-    // loadTestFile(require.resolve('./fleet_server_hosts/crud'));
+    // Fleet server hosts
+    loadTestFile(require.resolve('./fleet_server_hosts/crud'));
 
-    // // Fleet proxies
-    // loadTestFile(require.resolve('./fleet_proxies/crud'));
+    // Fleet proxies
+    loadTestFile(require.resolve('./fleet_proxies/crud'));
   });
 }
