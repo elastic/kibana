e2261805557e90b8507b4446a73bc1516f99ed76
István Zoltán Szabó <szabosteve@gmail.com>
2023-01-12 16:13:14 +0100

    Removes perlGuide from doc link service (#148803)
    (cherry picked from commit a82e62f0cd29497a8dbbb5f2f7eff07ed14fbf2e)

--- packages/kbn-doc-links/src/types.ts
+++ packages/kbn-doc-links/src/types.ts
@@ -411,7 +411,6 @@ export interface DocLinks {
     readonly netIntroduction: string;
     readonly netNest: string;
     readonly netSingleNode: string;
-    readonly perlGuide: string;
     readonly phpGuide: string;
     readonly phpConnecting: string;
     readonly phpInstallation: string;
