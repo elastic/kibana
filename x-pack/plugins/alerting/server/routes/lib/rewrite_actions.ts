e05a80399721ec6f27e29bd2dd92b2b286b7adb8
Zacqary Xeper <zacqary.xeper@elastic.co>
2023-01-12 11:59:27 -0600

    [RAM] Fix case conversion of frequency.notify_when in API

--- x-pack/plugins/alerting/server/routes/lib/rewrite_actions.ts
+++ x-pack/plugins/alerting/server/routes/lib/rewrite_actions.ts
@@ -14,7 +14,7 @@ type ReqRuleAction = Omit<RuleAction, 'actionTypeId' | 'frequency'> & {
     >[K];
   };
 };
-export const rewriteActions: (
+export const rewriteActionsReq: (
   actions?: ReqRuleAction[]
 ) => Array<Omit<RuleAction, 'actionTypeId'>> = (actions) => {
   const rewriteFrequency: RewriteRequestCase<NonNullable<RuleAction['frequency']>> = ({
@@ -28,5 +28,22 @@ export const rewriteActions: (
         ...action,
         ...(action.frequency ? { frequency: rewriteFrequency(action.frequency) } : {}),
       } as RuleAction)
+  );
+};
+
+export const rewriteActionsRes = (actions?: RuleAction[]) => {
+  const rewriteFrequency = ({ notifyWhen, ...rest }: NonNullable<RuleAction['frequency']>) =>
+    ({
+      ...rest,
+      notify_when: notifyWhen,
+    } as ReqRuleAction['frequency']);
+  if (!actions) return [];
+  return actions.map(
+    ({ actionTypeId, ...action }) =>
+      ({
+        ...action,
+        connector_type_id: actionTypeId,
+        ...(action.frequency ? { frequency: rewriteFrequency(action.frequency) } : {}),
+      } as ReqRuleAction & { connector_type_id: RuleAction['actionTypeId'] })
   );
 };
