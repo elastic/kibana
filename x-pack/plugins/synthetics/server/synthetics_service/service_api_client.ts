7f7b53110ba7be1544d3c135f8af126875c7a2f4
shahzad31 <shahzad31comp@gmail.com>
2023-01-13 11:33:05 +0100

    added api test

--- x-pack/plugins/synthetics/server/synthetics_service/service_api_client.ts
+++ x-pack/plugins/synthetics/server/synthetics_service/service_api_client.ts
@@ -139,37 +139,10 @@ export class ServiceAPIClient {
     method: 'POST' | 'PUT' | 'DELETE',
     { monitors: allMonitors, output, runOnce, isEdit }: ServiceData
   ) {
-    console.log(JSON.stringify(this.locations));
     if (this.username === TEST_SERVICE_USERNAME) {
       // we don't want to call service while local integration tests are running
       return;
     }
-
-    const callServiceEndpoint = (monitors: ServiceData['monitors'], url: string) => {
-      // don't need to pass locations to heartbeat
-      const monitorsStreams = monitors.map(({ locations, ...rest }) =>
-        convertToDataStreamFormat(rest)
-      );
-
-      return axios(
-        this.addVersionHeader({
-          method,
-          url: url + (runOnce ? '/run' : '/monitors'),
-          data: {
-            monitors: monitorsStreams,
-            output,
-            stack_version: this.stackVersion,
-            is_edit: isEdit,
-          },
-          headers: this.authorization
-            ? {
-                Authorization: this.authorization,
-              }
-            : undefined,
-          httpsAgent: this.getHttpsAgent(url),
-        })
-      );
-    };
 
     const pushErrors: ServiceLocationErrors = [];
 
@@ -181,7 +154,13 @@ export class ServiceAPIClient {
       );
       if (locMonitors.length > 0) {
         promises.push(
-          rxjsFrom(callServiceEndpoint(locMonitors, url)).pipe(
+          rxjsFrom(
+            this.callServiceEndpoint(
+              { monitors: locMonitors, isEdit, runOnce, output },
+              method,
+              url
+            )
+          ).pipe(
             tap((result) => {
               this.logger.debug(result.data);
               this.logger.debug(
@@ -214,5 +193,35 @@ export class ServiceAPIClient {
     await forkJoin(promises).toPromise();
 
     return pushErrors;
+  }
+
+  async callServiceEndpoint(
+    { monitors, output, runOnce, isEdit }: ServiceData,
+    method: 'POST' | 'PUT' | 'DELETE',
+    url: string
+  ) {
+    // don't need to pass locations to heartbeat
+    const monitorsStreams = monitors.map(({ locations, ...rest }) =>
+      convertToDataStreamFormat(rest)
+    );
+
+    return axios(
+      this.addVersionHeader({
+        method,
+        url: url + (runOnce ? '/run' : '/monitors'),
+        data: {
+          monitors: monitorsStreams,
+          output,
+          stack_version: this.stackVersion,
+          is_edit: isEdit,
+        },
+        headers: this.authorization
+          ? {
+              Authorization: this.authorization,
+            }
+          : undefined,
+        httpsAgent: this.getHttpsAgent(url),
+      })
+    );
   }
 }
