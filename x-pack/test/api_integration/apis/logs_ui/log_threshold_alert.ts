a345e28dc83dc2dc9446b9b891fcde21fccd8561
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-01-13 17:08:20 +0100

    test(logs): update functional test types

--- x-pack/test/api_integration/apis/logs_ui/log_threshold_alert.ts
+++ x-pack/test/api_integration/apis/logs_ui/log_threshold_alert.ts
@@ -17,6 +17,7 @@ import {
   Comparator,
   TimeUnit,
   RatioCriteria,
+  RuleParams,
 } from '@kbn/infra-plugin/common/alerting/logs/log_threshold/types';
 import { DATES } from '../metrics_ui/constants';
 import { FtrProviderContext } from '../../ftr_provider_context';
@@ -37,7 +38,7 @@ export default function ({ getService }: FtrProviderContext) {
             getValue: sinon.fake.returns(10),
             setLimitReached: sinon.fake(),
           } as SinonSpiesOf<LogThresholdAlertLimit>;
-          const ruleParams = {
+          const ruleParams: RuleParams = {
             logView: {
               logViewId: 'Default',
               type: 'log-view-reference',
@@ -99,7 +100,7 @@ export default function ({ getService }: FtrProviderContext) {
             getValue: sinon.fake.returns(2),
             setLimitReached: sinon.fake(),
           } as SinonSpiesOf<LogThresholdAlertLimit>;
-          const ruleParams = {
+          const ruleParams: RuleParams = {
             logView: {
               logViewId: 'Default',
               type: 'log-view-reference',
@@ -163,7 +164,7 @@ export default function ({ getService }: FtrProviderContext) {
             getValue: sinon.fake.returns(1),
             setLimitReached: sinon.fake(),
           } as SinonSpiesOf<LogThresholdAlertLimit>;
-          const ruleParams = {
+          const ruleParams: RuleParams = {
             logView: {
               logViewId: 'Default',
               type: 'log-view-reference',
@@ -234,7 +235,7 @@ export default function ({ getService }: FtrProviderContext) {
             getValue: sinon.fake.returns(2),
             setLimitReached: sinon.fake(),
           } as SinonSpiesOf<LogThresholdAlertLimit>;
-          const ruleParams = {
+          const ruleParams: RuleParams = {
             logView: {
               logViewId: 'Default',
               type: 'log-view-reference',
@@ -293,7 +294,7 @@ export default function ({ getService }: FtrProviderContext) {
             getValue: sinon.fake.returns(2),
             setLimitReached: sinon.fake(),
           } as SinonSpiesOf<LogThresholdAlertLimit>;
-          const ruleParams = {
+          const ruleParams: RuleParams = {
             logView: {
               logViewId: 'Default',
               type: 'log-view-reference',
