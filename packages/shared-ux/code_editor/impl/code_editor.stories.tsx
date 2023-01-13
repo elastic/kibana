0e510d4bccb3fdf1ddc35a57f557944e1e9339b4
rshen91 <rshen@elastic.co>
2023-01-13 09:04:15 -0700

    fix log language story

--- packages/shared-ux/code_editor/impl/code_editor.stories.tsx
+++ packages/shared-ux/code_editor/impl/code_editor.stories.tsx
@@ -9,13 +9,14 @@
 import React from 'react';
 
 import { CodeEditorStorybookMock, CodeEditorStorybookParams } from '@kbn/code-editor-mocks';
+import { monaco as monacoEditor } from '@kbn/monaco';
 
 import mdx from './README.mdx';
 
 import { CodeEditor } from './code_editor';
 
 export default {
-  title: 'Code Editor/Basic',
+  title: 'Code Editor/Code Editor',
   description: 'A code editor',
   parameters: {
     docs: {
@@ -27,18 +28,34 @@ export default {
 const mock = new CodeEditorStorybookMock();
 const argTypes = mock.getArgumentTypes();
 
-export const CodeEditorStorybookComponent = (params: CodeEditorStorybookParams) => {
+export const Basic = (params: CodeEditorStorybookParams) => {
   return <CodeEditor {...params} languageId="plainText" />;
 };
 
-CodeEditorStorybookComponent.argTypes = argTypes;
+Basic.argTypes = argTypes;
+
+// A sample language definition with a few example tokens
+// Taken from https://microsoft.github.io/monaco-editor/playground.html#extending-language-services-custom-languages
+const simpleLogLang: monacoEditor.languages.IMonarchLanguage = {
+  tokenizer: {
+    root: [
+      [/\[error.*/, 'constant'],
+      [/\[notice.*/, 'variable'],
+      [/\[info.*/, 'string'],
+      [/\[[a-zA-Z 0-9:]+\]/, 'tag'],
+    ],
+  },
+};
+
+monacoEditor.languages.register({ id: 'loglang' });
+monacoEditor.languages.setMonarchTokensProvider('loglang', simpleLogLang);
 
 const logs = `[Sun Mar 7 20:54:27 2004] [notice] [client xx.xx.xx.xx] This is a notice!
-  [Sun Mar 7 20:58:27 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed
-  [Sun Mar 7 21:16:17 2004] [error] [client xx.xx.xx.xx] File does not exist: /home/httpd/twiki/view/Main/WebHome
+[Sun Mar 7 20:58:27 2004] [info] [client xx.xx.xx.xx] (104)Connection reset by peer: client stopped connection before send body completed
+[Sun Mar 7 21:16:17 2004] [error] [client xx.xx.xx.xx] File does not exist: /home/httpd/twiki/view/Main/WebHome
   `;
 
-export const CodeEditorStorybookCustomLogLanguage = (params: CodeEditorStorybookParams) => {
+export const CustomLogLanguage = (params: CodeEditorStorybookParams) => {
   return (
     <div>
       <CodeEditor
@@ -48,7 +65,7 @@ export const CodeEditorStorybookCustomLogLanguage = (params: CodeEditorStorybook
         value={logs}
         options={{
           minimap: {
-            enabled: true,
+            enabled: false,
           },
         }}
       />
@@ -56,4 +73,4 @@ export const CodeEditorStorybookCustomLogLanguage = (params: CodeEditorStorybook
   );
 };
 
-CodeEditorStorybookCustomLogLanguage.argTypes = argTypes;
+CustomLogLanguage.argTypes = argTypes;
