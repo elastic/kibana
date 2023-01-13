e42f001853b2f5ac285cf5dd502036437d0a4865
Ashokaditya <ashokaditya@elastic.co>
2023-01-13 11:58:00 +0100

    responder access license checks
    fixes elastic/security-team/issues/2801

---
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/responder.cy.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { login, loginWithRole, ROLE } from '../tasks/login';
+import { setupLicense } from '../tasks/license';
+import { licenses } from '../fixtures/licenses';
+import { runEndpointLoaderScript } from '../tasks/run_endpoint_loader';
+
+const loginWithWriteAccess = (url: string) => {
+  loginWithRole(ROLE.analyst_hunter);
+  cy.visit(url);
+};
+
+describe('Responder', () => {
+  before(() => {
+    login();
+    runEndpointLoaderScript();
+  });
+
+  describe('Enterprise license', () => {
+    beforeEach(() => {
+      setupLicense(licenses.enterprise);
+    });
+
+    it('should display responder action item for an endpoint', () => {
+      loginWithWriteAccess('/app/security/administration/endpoints');
+      cy.getBySel('endpointTableRowActions').first().click();
+      cy.getBySel('console').should('exist');
+    });
+  });
+
+  describe('Platinum license', () => {
+    beforeEach(() => {
+      setupLicense(licenses.platinum);
+    });
+
+    it('should not display responder action item for an endpoint', () => {
+      loginWithWriteAccess('/app/security/administration/endpoints');
+      cy.getBySel('endpointTableRowActions').first().click();
+      cy.getBySel('console').should('not.exist');
+    });
+  });
+});
