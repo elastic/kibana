bcb23e10979bd263469f20d9f5ff5d0b84616421
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-01-13 15:08:37 +0100

    cleanup

--- x-pack/plugins/security_solution/server/endpoint/utils/action_list_helpers.ts
+++ x-pack/plugins/security_solution/server/endpoint/utils/action_list_helpers.ts
@@ -99,8 +99,6 @@ export const getActions = async ({
     },
   };
 
-  console.log('actionsSearchQuery: ', JSON.stringify(actionsSearchQuery));
-
   const actionRequests: TransportResult<
     estypes.SearchResponse<LogsEndpointAction>,
     unknown
