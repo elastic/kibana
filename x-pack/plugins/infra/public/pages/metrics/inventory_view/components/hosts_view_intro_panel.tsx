9ea3e2638499c482b4a9316cf849a111a16f66be
Jenny <dzheni.pavlova@elastic.co>
2023-01-13 20:04:43 +0100

    Add beta panel for host view on inventory page

---
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/hosts_view_intro_panel.tsx
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiBetaBadge, EuiLink, EuiIcon } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+
+export const HostViewIntroPanel = () => (
+  <EuiFlexGroup alignItems="center" gutterSize="m">
+    <EuiFlexItem grow={false}>
+      <EuiBetaBadge
+        color={'accent'}
+        href="#"
+        label={i18n.translate('xpack.infra.layout.tryIt', {
+          defaultMessage: 'Try it',
+        })}
+      />
+    </EuiFlexItem>
+    <EuiFlexItem grow={false}>
+      <EuiIcon type="beaker" />
+    </EuiFlexItem>
+    <EuiFlexItem grow={false}>
+      <EuiLink>Introducing a new Hosts analysis experience</EuiLink>
+    </EuiFlexItem>
+  </EuiFlexGroup>
+);
