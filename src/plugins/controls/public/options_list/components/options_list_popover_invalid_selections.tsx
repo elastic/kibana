a85c9418431c1b7dbdb19620353ba2e14b8de278
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-01-16 17:25:49 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/plugins/controls/public/options_list/components/options_list_popover_invalid_selections.tsx
+++ src/plugins/controls/public/options_list/components/options_list_popover_invalid_selections.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   EuiSelectableOption,
