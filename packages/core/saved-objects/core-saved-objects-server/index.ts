cafb03d24e48b8620eb97eb4f7ffb6fe75292c01
Jean-Louis Leysens <jloleysens@gmail.com>
2023-01-16 16:03:09 +0100

    move a bunch of common types to be server-side only, but re-export from common with deprecation notice for now

--- packages/core/saved-objects/core-saved-objects-server/index.ts
+++ packages/core/saved-objects/core-saved-objects-server/index.ts
@@ -89,3 +89,10 @@ export {
   SECURITY_EXTENSION_ID,
   SPACES_EXTENSION_ID,
 } from './src/extensions/extensions';
+export type {
+  SavedObject,
+  SavedObjectAttribute,
+  SavedObjectAttributeSingle,
+  SavedObjectAttributes,
+  SavedObjectReference,
+} from './src/saved_objects';
