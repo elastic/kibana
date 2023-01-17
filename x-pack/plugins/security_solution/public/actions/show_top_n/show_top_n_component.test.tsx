29545fd67fdb8e090f39d8055d42af1c337616d1
Angela Chuang <yi-chun.chuang@elastic.co>
2023-01-17 09:43:29 +0000

    unit tests

--- x-pack/plugins/security_solution/public/actions/show_top_n/show_top_n_component.test.tsx
+++ x-pack/plugins/security_solution/public/actions/show_top_n/show_top_n_component.test.tsx
@@ -20,6 +20,7 @@ jest.mock('react-router-dom', () => {
     useLocation: jest.fn().mockReturnValue({ pathname: '/test' }),
   };
 });
+jest.mock('../../common/components/visualization_actions');
 
 const casesService = {
   ui: { getCasesContext: () => mockCasesContext },
