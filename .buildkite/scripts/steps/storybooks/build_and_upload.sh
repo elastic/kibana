050b3ce4c792093a7f71b36e8c09bd15a45ed326
Jonathan Budzenski <jon@elastic.co>
2023-01-17 14:46:15 -0600

    [ci] Fix storybooks

--- .buildkite/scripts/steps/storybooks/build_and_upload.sh
+++ .buildkite/scripts/steps/storybooks/build_and_upload.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 .buildkite/scripts/bootstrap.sh
 
+# Broken during Node 18 upgrade
+export NODE_OPTIONS="--openssl-legacy-provider"
 ts-node .buildkite/scripts/steps/storybooks/build_and_upload.ts
