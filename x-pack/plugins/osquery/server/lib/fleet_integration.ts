7d9cd2b681ee2ccba5fe5569a0150587c6dded19
Kevin Logan <kevin.logan@elastic.co>
2023-01-17 11:52:23 -0500

    revert fleet integration change

--- x-pack/plugins/osquery/server/lib/fleet_integration.ts
+++ x-pack/plugins/osquery/server/lib/fleet_integration.ts
@@ -5,78 +5,16 @@
  * 2.0.
  */
 
-import type {
-  ElasticsearchClient,
-  SavedObjectReference,
-  SavedObjectsClient,
-  Logger,
-} from '@kbn/core/server';
-import { filter, map, orderBy } from 'lodash';
-import deepequal from 'fast-deep-equal';
-import { satisfies } from 'semver';
+import type { SavedObjectReference, SavedObjectsClient } from '@kbn/core/server';
+import { filter, map } from 'lodash';
 import type { PostPackagePolicyPostDeleteCallback } from '@kbn/fleet-plugin/server';
-import type { UpdatePackagePolicy } from '@kbn/fleet-plugin/common';
 import { AGENT_POLICY_SAVED_OBJECT_TYPE } from '@kbn/fleet-plugin/common';
 import { packSavedObjectType } from '../../common/types';
 import { OSQUERY_INTEGRATION_NAME } from '../../common';
 
-export const getPackagePolicyUpdateCallback =
-  (esClient: ElasticsearchClient) => async (updatePackagePolicy: UpdatePackagePolicy, logger: Logger) => {
-    //logger.info('trying to update.....');
-    if (
-      updatePackagePolicy.package?.name === OSQUERY_INTEGRATION_NAME &&
-      satisfies(updatePackagePolicy.package?.version ?? '', '>=1.6.0')
-    ) {
-      const dataStreams = await esClient.indices.getDataStream({
-        name: `logs-${OSQUERY_INTEGRATION_NAME}.result-*`,
-      })
-
-      logger.debug('data streams ' + dataStreams.data_streams);
-
-      const mapping = await esClient.indices.getMapping({
-        index: `logs-${OSQUERY_INTEGRATION_NAME}.result-*`,
-      });
-      //logger.info('mapping', mapping);
-      // Sort by index name to get the latest index
-      const dataStreamMapping = orderBy(Object.entries(mapping), [0], 'desc')?.[0][1]?.mappings
-        ?.properties?.data_stream;
-
-      if (
-        dataStreamMapping &&
-        deepequal(dataStreamMapping, {
-          properties: {
-            dataset: {
-              type: 'constant_keyword',
-              value: 'generic',
-            },
-            namespace: {
-              type: 'constant_keyword',
-              value: 'default',
-            },
-            type: {
-              type: 'constant_keyword',
-              value: 'osquery',
-            },
-          },
-        })
-      ) {
-        try {
-          esClient.indices.rollover({
-            alias: `logs-${OSQUERY_INTEGRATION_NAME}.result-default`,
-          });
-        } catch (e) {
-          // Ignore errors
-        }
-      }
-    }
-
-    return updatePackagePolicy;
-  };
-
 export const getPackagePolicyDeleteCallback =
-  (packsClient: SavedObjectsClient, logger: Logger): PostPackagePolicyPostDeleteCallback =>
+  (packsClient: SavedObjectsClient): PostPackagePolicyPostDeleteCallback =>
   async (deletedPackagePolicy) => {
-    logger.info('deleting package.....');
     const deletedOsqueryManagerPolicies = filter(deletedPackagePolicy, [
       'package.name',
       OSQUERY_INTEGRATION_NAME,
