02b74761721a116ed135799da48716b94e6dd952
Rodney Norris <rodney.norris@elastic.co>
2023-01-17 09:50:13 -0600

    refactor: simplify from - to calculation

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engines/engines_list.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engines/engines_list.tsx
@@ -119,16 +119,16 @@ export const EnginesList: React.FC = () => {
         <EuiText size="s">
           <FormattedMessage
             id="xpack.enterpriseSearch.content.engines.enginesList.description"
-            defaultMessage="Showing {currentPage}-{size} of {total}"
+            defaultMessage="Showing {from}-{to} of {total}"
             values={{
-              currentPage: (
+              from: (
                 <strong>
-                  <FormattedNumber value={meta.from} />
+                  <FormattedNumber value={meta.from + 1} />
                 </strong>
               ),
-              size: (
+              to: (
                 <strong>
-                  <FormattedNumber value={meta.size} />
+                  <FormattedNumber value={meta.from + (results?.length ?? 0)} />
                 </strong>
               ),
               total: <FormattedNumber value={meta.total} />,
