f8cc94eaab20a0db26ac4e6e18d57a7ceb092a0e
Robert Oskamp <robert.oskamp@elastic.co>
2023-01-17 14:35:56 +0100

    only run ML and AIOps suites

--- x-pack/test/api_integration/apis/ml/index.ts
+++ x-pack/test/api_integration/apis/ml/index.ts
@@ -11,7 +11,7 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
   const ml = getService('ml');
 
-  describe.only('Machine Learning', function () {
+  describe('Machine Learning', function () {
     this.tags(['ml']);
 
     before(async () => {
