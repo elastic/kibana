eeb76d69e71ba7fb0b33366a9551a031262f043f
shahzad31 <shahzad31comp@gmail.com>
2023-01-18 14:51:06 +0100

    update

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_errors/monitor_errors.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_errors/monitor_errors.tsx
@@ -4,27 +4,16 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiPanel,
-  EuiSpacer,
-  EuiTitle,
-  useEuiTheme,
-} from '@elastic/eui';
+import { EuiSpacer } from '@elastic/eui';
 import React from 'react';
-import { i18n } from '@kbn/i18n';
-import { PanelWithTitle } from '../../common/components/panel_with_title';
+import { useMonitorErrors } from '../hooks/use_monitor_errors';
 import { useMonitorQueryId } from '../hooks/use_monitor_query_id';
-import { FailedTestsCount } from './failed_tests_count';
 import { useAbsoluteDate, useGetUrlParams } from '../../../hooks';
 import { SyntheticsDatePicker } from '../../common/date_picker/synthetics_date_picker';
-import { MonitorErrorsCount } from '../monitor_summary/monitor_errors_count';
-import { ErrorsList } from './errors_list';
-import { MonitorFailedTests } from './failed_tests';
+import { ErrorsTabContent } from './errors_tab_content';
 
 export const MonitorErrors = () => {
-  const { euiTheme } = useEuiTheme();
+  const { errorStates, loading } = useMonitorErrors();
 
   const { dateRangeStart, dateRangeEnd } = useGetUrlParams();
 
@@ -32,64 +21,11 @@ export const MonitorErrors = () => {
 
   const monitorId = useMonitorQueryId();
 
-  const panelTitleStyle = { margin: euiTheme.size.s, marginBottom: 0 };
-
   return (
     <>
       <SyntheticsDatePicker fullWidth={true} />
       <EuiSpacer size="m" />
-      <EuiFlexGroup gutterSize="m">
-        <EuiFlexItem grow={1}>
-          <PanelWithTitle title={OVERVIEW_LABEL}>
-            <EuiFlexGroup>
-              <EuiFlexItem>
-                {monitorId && (
-                  <MonitorErrorsCount from={time.from} to={time.to} monitorId={[monitorId]} />
-                )}
-              </EuiFlexItem>
-              <EuiFlexItem>
-                <FailedTestsCount from={time.from} to={time.to} />
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </PanelWithTitle>
-        </EuiFlexItem>
-        <EuiFlexItem grow={3}>
-          <PanelWithTitle title={FAILED_TESTS_LABEL}>
-            <MonitorFailedTests time={time} />
-          </PanelWithTitle>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-      <EuiSpacer size="m" />
-      <EuiFlexGroup gutterSize="m">
-        <EuiFlexItem grow={2}>
-          <PanelWithTitle title={ERRORS_LABEL}>
-            <ErrorsList />
-          </PanelWithTitle>
-        </EuiFlexItem>
-        <EuiFlexItem grow={1}>
-          <EuiPanel hasBorder>
-            <EuiTitle size="xs">
-              <h3 css={panelTitleStyle}>{FAILED_TESTS_BY_STEPS_LABEL}</h3>
-            </EuiTitle>
-          </EuiPanel>
-        </EuiFlexItem>
-      </EuiFlexGroup>
+      <ErrorsTabContent errorStates={errorStates} loading={loading} />
     </>
   );
 };
-
-const ERRORS_LABEL = i18n.translate('xpack.synthetics.errors.label', {
-  defaultMessage: 'Errors',
-});
-
-const OVERVIEW_LABEL = i18n.translate('xpack.synthetics.errors.overview', {
-  defaultMessage: 'Overview',
-});
-
-const FAILED_TESTS_LABEL = i18n.translate('xpack.synthetics.errors.failedTests', {
-  defaultMessage: 'Failed tests',
-});
-
-const FAILED_TESTS_BY_STEPS_LABEL = i18n.translate('xpack.synthetics.errors.failedTests.byStep', {
-  defaultMessage: 'Failed tests by step',
-});
