88008c37f7fa0b5450c167030362b1300b67aea8
Jenny <dzheni.pavlova@elastic.co>
2023-01-18 14:37:16 +0100

    Add Inventory link

--- x-pack/plugins/infra/public/pages/metrics/inventory_view/components/hosts_view_intro_panel.tsx
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/hosts_view_intro_panel.tsx
@@ -8,24 +8,43 @@
 import React from 'react';
 import { EuiFlexGroup, EuiFlexItem, EuiBetaBadge, EuiLink, EuiIcon } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
+import { useLinkProps } from '@kbn/observability-plugin/public';
+import { css } from '@emotion/react';
 
-export const HostViewIntroPanel = () => (
-  <EuiFlexGroup alignItems="center" gutterSize="m">
-    <EuiFlexItem grow={false}>
-      <EuiBetaBadge
-        color={'accent'}
-        href="#"
-        data-test-subj="inventory-hostsView-button"
-        label={i18n.translate('xpack.infra.layout.tryIt', {
-          defaultMessage: 'Try it',
-        })}
-      />
-    </EuiFlexItem>
-    <EuiFlexItem grow={false}>
-      <EuiIcon type="beaker" />
-    </EuiFlexItem>
-    <EuiFlexItem grow={false}>
-      <EuiLink>Introducing a new Hosts analysis experience</EuiLink>
-    </EuiFlexItem>
-  </EuiFlexGroup>
-);
+export const HostViewIntroPanel = () => {
+  const link = useLinkProps({
+    app: 'metrics',
+    pathname: '/hosts',
+  });
+
+  return (
+    <EuiFlexGroup
+      responsive={false}
+      alignItems="center"
+      gutterSize="m"
+      css={css`
+        position: relative;
+        z-index: 100;
+      `}
+    >
+      <EuiFlexItem grow={false}>
+        <EuiBetaBadge
+          color={'accent'}
+          href={link.href ?? ''}
+          data-test-subj="inventory-hostsView-button"
+          label={i18n.translate('xpack.infra.layout.tryIt', {
+            defaultMessage: 'Try it',
+          })}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiIcon type="beaker" />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiLink data-test-subj="inventory-hostsView-button" {...link}>
+          Introducing a new Hosts analysis experience
+        </EuiLink>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
