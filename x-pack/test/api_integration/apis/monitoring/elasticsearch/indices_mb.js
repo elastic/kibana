e5ce76430ef71ae803f6d84e69fe0a618d66216e
klacabane <kevin.lacabane@elastic.co>
2023-01-18 22:06:12 +0100

    enable flaky test

--- x-pack/test/api_integration/apis/monitoring/elasticsearch/indices_mb.js
+++ x-pack/test/api_integration/apis/monitoring/elasticsearch/indices_mb.js
@@ -18,7 +18,7 @@ export default function ({ getService }) {
 
   // Failing: See https://github.com/elastic/kibana/issues/146068
   // Failing: See https://github.com/elastic/kibana/issues/146067
-  describe.skip('indices - metricbeat and package', () => {
+  describe('indices - metricbeat and package', () => {
     ['mb', 'package'].forEach((source) => {
       describe(`indices ${source}`, () => {
         describe('shard-relocation', () => {
