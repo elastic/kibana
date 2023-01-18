8481ff95b435edff0b40cfd51132430ac4867ab7
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2023-01-18 15:12:07 +0100

    Update tests

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engine/engine_view_logic.test.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engine/engine_view_logic.test.ts
@@ -9,6 +9,11 @@ import { LogicMounter } from '../../../__mocks__/kea_logic';
 
 import { Status } from '../../../../../common/types/api';
 
+import { KibanaLogic } from '../../../shared/kibana';
+import { DeleteEnginesApiLogicResponse } from '../../api/engines/delete_engines_api_logic';
+import { ENGINES_PATH } from '../../routes';
+import { EnginesListLogic } from '../engines/engines_list_logic';
+
 import { EngineViewLogic, EngineViewValues } from './engine_view_logic';
 
 const DEFAULT_VALUES: EngineViewValues = {
@@ -21,14 +26,30 @@ const DEFAULT_VALUES: EngineViewValues = {
 
 describe('EngineViewLogic', () => {
   const { mount } = new LogicMounter(EngineViewLogic);
+  const { mount: mountEnginesListLogic } = new LogicMounter(EnginesListLogic);
   beforeEach(() => {
     jest.clearAllMocks();
     jest.useRealTimers();
 
+    mountEnginesListLogic();
     mount({ engineName: DEFAULT_VALUES.engineName }, { engineName: DEFAULT_VALUES.engineName });
   });
 
   it('has expected default values', () => {
     expect(EngineViewLogic.values).toEqual(DEFAULT_VALUES);
+  });
+
+  describe('listeners', () => {
+    describe('deleteSuccess', () => {
+      it('should navigate to the engines list when an engine is deleted', () => {
+        jest.spyOn(EngineViewLogic.actions, 'deleteSuccess');
+        jest
+          .spyOn(KibanaLogic.values, 'navigateToUrl')
+          .mockImplementationOnce(() => Promise.resolve());
+        EnginesListLogic.actions.deleteSuccess({} as DeleteEnginesApiLogicResponse);
+
+        expect(KibanaLogic.values.navigateToUrl).toHaveBeenCalledWith(ENGINES_PATH);
+      });
+    });
   });
 });
