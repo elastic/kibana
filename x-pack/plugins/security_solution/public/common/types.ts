e0eb42d7694f91b9db7e8767523684c50e2579ac
Angela Chuang <yi-chun.chuang@elastic.co>
2023-01-18 16:48:10 +0000

    add unit tests

--- x-pack/plugins/security_solution/public/common/types.ts
+++ x-pack/plugins/security_solution/public/common/types.ts
@@ -25,6 +25,7 @@ export interface SecuritySolutionUiConfigType {
  */
 export interface SecuritySolutionDataViewBase extends DataViewBase {
   fields: FieldSpec[];
+  getName?: () => string;
 }
 
 export type AlertWorkflowStatus = 'open' | 'closed' | 'acknowledged';
