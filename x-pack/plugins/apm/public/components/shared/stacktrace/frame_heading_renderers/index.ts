22fd94ca71ef1e91e04e8c4f88853f9c39781a9a
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2023-01-18 14:57:28 +0100

    add stacktrace support for php

--- x-pack/plugins/apm/public/components/shared/stacktrace/frame_heading_renderers/index.ts
+++ x-pack/plugins/apm/public/components/shared/stacktrace/frame_heading_renderers/index.ts
@@ -11,6 +11,7 @@ import { Stackframe } from '../../../../../typings/es_schemas/raw/fields/stackfr
 export interface FrameHeadingRendererProps {
   fileDetailComponent: ComponentType;
   stackframe: Stackframe;
+  idx: string;
 }
 
 export { CSharpFrameHeadingRenderer } from './c_sharp_frame_heading_renderer';
@@ -18,3 +19,4 @@ export { DefaultFrameHeadingRenderer } from './default_frame_heading_renderer';
 export { JavaFrameHeadingRenderer } from './java_frame_heading_renderer';
 export { JavaScriptFrameHeadingRenderer } from './java_script_frame_heading_renderer';
 export { RubyFrameHeadingRenderer } from './ruby_frame_heading_renderer';
+export { PhpFrameHeadingRenderer } from './php_frame_heading_renderer';
