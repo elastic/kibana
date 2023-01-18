f584044b2452eb57c66280e14778e968cf7ae665
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-01-18 16:37:47 +0100

    feat(infra): implement TelemetryService

---
+++ x-pack/plugins/infra/public/services/telemetry/telemetry_client.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { AnalyticsServiceSetup } from '@kbn/core-analytics-server';
+import { InfraTelemetryEventTypes, ITelemetryClient } from './types';
+
+/**
+ * Client which aggregate all the available telemetry tracking functions
+ * for the Infra plugin
+ */
+export class TelemetryClient implements ITelemetryClient {
+  constructor(private analytics: AnalyticsServiceSetup) {}
+
+  public reportHostEntryClicked: ITelemetryClient['reportHostEntryClicked'] = ({
+    hostname,
+    cloud_provider: cloudProvider = 'unknown',
+  }) => {
+    this.analytics.reportEvent(InfraTelemetryEventTypes.HOSTS_ENTRY_CLICKED, {
+      hostname,
+      cloud_provider: cloudProvider,
+    });
+  };
+
+  public reportHostsViewQuerySubmitted: ITelemetryClient['reportHostsViewQuerySubmitted'] = (
+    params
+  ) => {
+    this.analytics.reportEvent(InfraTelemetryEventTypes.HOSTS_VIEW_QUERY_SUBMITTED, params);
+  };
+}
---
+++ x-pack/plugins/infra/public/services/telemetry/telemetry_events.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import {
+  InfraTelemetryEventTypes,
+  InfraTelemetryEvent,
+  HostsViewQuerySubmittedSchema,
+  HostEntryClickedSchema,
+} from './types';
+
+const hostsViewQuerySubmittedSchema: HostsViewQuerySubmittedSchema = {
+  control_filters: {
+    type: 'array',
+    items: {
+      type: 'keyword',
+      _meta: {
+        description: 'Selected host control filter.',
+        optional: false,
+      },
+    },
+  },
+  filters: {
+    type: 'array',
+    items: {
+      type: 'text',
+      _meta: {
+        description: 'Applied host search filter.',
+        optional: false,
+      },
+    },
+  },
+  interval: {
+    type: 'text',
+    _meta: {
+      description: 'Time interval for the performed search.',
+      optional: false,
+    },
+  },
+  query: {
+    type: 'text',
+    _meta: {
+      description: 'KQL query search for hosts',
+      optional: false,
+    },
+  },
+};
+
+const hostsEntryClickedSchema: HostEntryClickedSchema = {
+  hostname: {
+    type: 'keyword',
+    _meta: {
+      description: 'List of selected host control filters.',
+      optional: false,
+    },
+  },
+  cloud_provider: {
+    type: 'keyword',
+    _meta: {
+      description: 'List of applied host search filters.',
+      optional: true,
+    },
+  },
+};
+
+export const infraTelemetryEvents: InfraTelemetryEvent[] = [
+  {
+    eventType: InfraTelemetryEventTypes.HOSTS_VIEW_QUERY_SUBMITTED,
+    schema: hostsViewQuerySubmittedSchema,
+  },
+  {
+    eventType: InfraTelemetryEventTypes.HOSTS_ENTRY_CLICKED,
+    schema: hostsEntryClickedSchema,
+  },
+];
