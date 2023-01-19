ec3c666a2df0f2436150bf498c9d5576da308f35
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-01-19 12:53:00 +0100

    update useDatePicker to return timestamp based range.

--- x-pack/plugins/transform/public/app/sections/create_transform/components/step_define/hooks/use_date_picker.ts
+++ x-pack/plugins/transform/public/app/sections/create_transform/components/step_define/hooks/use_date_picker.ts
@@ -5,7 +5,11 @@
  * 2.0.
  */
 
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { merge } from 'rxjs';
+
+import type { TimeRange } from '@kbn/es-query';
+import { mlTimefilterRefresh$, useTimefilter } from '@kbn/ml-date-picker';
 
 import { StepDefineExposedState } from '../common';
 import { StepDefineFormProps } from '../step_define_form';
@@ -14,13 +18,57 @@ export const useDatePicker = (
   defaults: StepDefineExposedState,
   dataView: StepDefineFormProps['searchItems']['dataView']
 ) => {
+  const hasValidTimeField = useMemo(
+    () => dataView.timeFieldName !== undefined && dataView.timeFieldName !== '',
+    [dataView.timeFieldName]
+  );
+
+  const timefilter = useTimefilter({
+    timeRangeSelector: hasValidTimeField,
+    autoRefreshSelector: false,
+  });
+
   // The internal state of the date picker apply button.
   const [isDatePickerApplyEnabled, setDatePickerApplyEnabled] = useState(
     defaults.isDatePickerApplyEnabled
   );
 
+  // The time range selected via the date picker
+  const [timeRange, setTimeRange] = useState<TimeRange>();
+
+  useEffect(() => {
+    const updateTimeRange = () => setTimeRange(timefilter.getTime());
+
+    const timefilterUpdateSubscription = merge(
+      timefilter.getAutoRefreshFetch$(),
+      timefilter.getTimeUpdate$(),
+      mlTimefilterRefresh$
+    ).subscribe(updateTimeRange);
+
+    const timefilterEnabledSubscription = timefilter
+      .getEnabledUpdated$()
+      .subscribe(updateTimeRange);
+
+    return () => {
+      timefilterUpdateSubscription.unsubscribe();
+      timefilterEnabledSubscription.unsubscribe();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const timeRangeMs = useMemo(() => {
+    const timefilterActiveBounds = timefilter.getActiveBounds();
+    if (timefilterActiveBounds !== undefined) {
+      return {
+        from: timefilterActiveBounds.min?.valueOf(),
+        to: timefilterActiveBounds.max?.valueOf(),
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timeRange]);
+
   return {
     actions: { setDatePickerApplyEnabled },
-    state: { isDatePickerApplyEnabled },
+    state: { isDatePickerApplyEnabled, hasValidTimeField, timeRange, timeRangeMs },
   };
 };
