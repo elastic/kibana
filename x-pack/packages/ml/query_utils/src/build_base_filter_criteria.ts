01feb891f2625d817e4ee56437200b4963710d41
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-01-19 14:16:50 +0100

    move query_utils to packages

--- x-pack/packages/ml/query_utils/src/build_base_filter_criteria.ts
+++ x-pack/packages/ml/query_utils/src/build_base_filter_criteria.ts
@@ -8,10 +8,6 @@
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { Query } from '@kbn/es-query';
 
-/*
- * Contains utility functions for building and processing queries.
- */
-
 // Builds the base filter criteria used in queries,
 // adding criteria for the time range and an optional query.
 export function buildBaseFilterCriteria(
@@ -21,6 +17,7 @@ export function buildBaseFilterCriteria(
   query?: Query['query']
 ): estypes.QueryDslQueryContainer[] {
   const filterCriteria = [];
+
   if (timeFieldName && earliestMs && latestMs) {
     filterCriteria.push({
       range: {
@@ -38,12 +35,4 @@ export function buildBaseFilterCriteria(
   }
 
   return filterCriteria;
-}
-
-// Returns a name which is safe to use in elasticsearch aggregations for the supplied
-// field name. Aggregation names must be alpha-numeric and can only contain '_' and '-' characters,
-// so if the supplied field names contains disallowed characters, the provided index
-// identifier is used to return a safe 'dummy' name in the format 'field_index' e.g. field_0, field_1
-export function getSafeAggregationName(fieldName: string, index: number): string {
-  return fieldName.match(/^[a-zA-Z0-9-_.]+$/) ? fieldName : `field_${index}`;
 }
---
+++ x-pack/packages/ml/query_utils/src/get_safe_aggregation_name.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+// Returns a name which is safe to use in elasticsearch aggregations for the supplied
+// field name. Aggregation names must be alpha-numeric and can only contain '_' and '-' characters,
+// so if the supplied field names contains disallowed characters, the provided index
+// identifier is used to return a safe 'dummy' name in the format 'field_index' e.g. field_0, field_1
+export function getSafeAggregationName(fieldName: string, index: number): string {
+  return fieldName.match(/^[a-zA-Z0-9-_.]+$/) ? fieldName : `field_${index}`;
+}
