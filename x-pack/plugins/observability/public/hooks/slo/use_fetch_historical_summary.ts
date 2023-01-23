ab6b49ea561db5087500c7fa69b9dc7446cf36ff
Kevin Delemme <kevin.delemme@elastic.co>
2023-01-23 13:46:19 -0500

    Create hooks for fetching historical summary

---
+++ x-pack/plugins/observability/public/hooks/slo/use_fetch_historical_summary.ts
@@ -0,0 +1,75 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { HttpSetup } from '@kbn/core/public';
+import { FetchHistoricalSummaryResponse } from '@kbn/slo-schema';
+
+import { useDataFetcher } from '../use_data_fetcher';
+
+const EMPTY_RESPONSE: FetchHistoricalSummaryResponse = {};
+
+export interface UseFetchHistoricalSummaryResponse {
+  data: FetchHistoricalSummaryResponse;
+  loading: boolean;
+  error: boolean;
+}
+
+interface Params {
+  sloIds: string[];
+}
+
+export function useFetchHistoricalSummary({
+  sloIds = [],
+  refetch = false,
+}: {
+  sloIds: string[];
+  refetch: boolean;
+}): UseFetchHistoricalSummaryResponse {
+  const [historicalSummary, setHistoricalSummary] = useState(EMPTY_RESPONSE);
+
+  const params: Params = useMemo(() => ({ sloIds }), [sloIds]);
+  const shouldExecuteApiCall = useCallback(
+    (apiCallParams: Params) => apiCallParams.sloIds.length > 0 || refetch,
+    [refetch]
+  );
+
+  const { data, loading, error } = useDataFetcher<Params, FetchHistoricalSummaryResponse>({
+    paramsForApiCall: params,
+    initialDataState: historicalSummary,
+    executeApiCall: fetchHistoricalSummary,
+    shouldExecuteApiCall,
+  });
+
+  useEffect(() => {
+    setHistoricalSummary(data);
+  }, [data]);
+
+  return { data: historicalSummary, loading, error };
+}
+
+const fetchHistoricalSummary = async (
+  params: Params,
+  abortController: AbortController,
+  http: HttpSetup
+): Promise<FetchHistoricalSummaryResponse> => {
+  try {
+    const response = await http.post<FetchHistoricalSummaryResponse>(
+      '/internal/observability/slos/_historical_summary',
+      {
+        body: JSON.stringify({ sloIds: params.sloIds }),
+        signal: abortController.signal,
+      }
+    );
+
+    return response;
+  } catch (error) {
+    // ignore error for retrieving slos
+  }
+
+  return EMPTY_RESPONSE;
+};
