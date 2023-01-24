966600e1b2597554380c0120275993b08c6dc2b5
Matt Kime <matt@mattki.me>
2023-01-24 17:03:44 -0600

    fix test and stub out new

---
+++ test/functional/apps/discover/group2/_data_view_edit.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../ftr_provider_context';
+
+export default function ({ getService, getPageObjects }: FtrProviderContext) {
+  describe('data view edit', function () {
+    it('dummy test', async function () {
+      expect(1).to.be(1);
+    });
+  });
+}
