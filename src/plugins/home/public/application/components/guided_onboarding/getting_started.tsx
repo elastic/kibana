e173d84e93741bdfa55f13567ac88736c77dba71
Yulia Čech <6585477+yuliacech@users.noreply.github.com>
2023-01-25 18:39:33 +0100

    Update src/plugins/home/public/application/components/guided_onboarding/getting_started.tsx
    Co-authored-by: gchaps <33642766+gchaps@users.noreply.github.com>

--- src/plugins/home/public/application/components/guided_onboarding/getting_started.tsx
+++ src/plugins/home/public/application/components/guided_onboarding/getting_started.tsx
@@ -37,7 +37,7 @@ const title = i18n.translate('home.guidedOnboarding.gettingStarted.useCaseSelect
   defaultMessage: 'What would you like to do first?',
 });
 const subtitle = i18n.translate('home.guidedOnboarding.gettingStarted.useCaseSelectionSubtitle', {
-  defaultMessage: `Select an option below and we'll help you get started`,
+  defaultMessage: `Select an option and we'll help you get started.`,
 });
 const skipText = i18n.translate('home.guidedOnboarding.gettingStarted.skip.buttonLabel', {
   defaultMessage: `I’d like to do something else (skip)`,
