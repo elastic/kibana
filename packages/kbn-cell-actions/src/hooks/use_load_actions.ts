2c91688d288911a903e7148c1ac7726992bacf4e
semd <sergi.massaneda@elastic.co>
2023-01-25 14:13:52 +0100

    cell actions type and fix add to timeline empty value

--- packages/kbn-cell-actions/src/hooks/use_load_actions.ts
+++ packages/kbn-cell-actions/src/hooks/use_load_actions.ts
@@ -6,31 +6,53 @@
  * Side Public License, v 1.
  */
 
+import { useMemo } from 'react';
 import useAsync from 'react-use/lib/useAsync';
-import useAsyncFn from 'react-use/lib/useAsyncFn';
+import useAsyncFn, { type AsyncState } from 'react-use/lib/useAsyncFn';
 import { useCellActionsContext } from '../context/cell_actions_context';
-import { CellActionExecutionContext } from '../types';
+import type { CellAction, CellActionCompatibilityContext, GetActions } from '../types';
+
+type AsyncActions<V = CellAction[]> = Omit<AsyncState<V>, 'error'>;
+
+const useThrowError = (error?: Error) => {
+  useMemo(() => {
+    if (error) {
+      throw error;
+    }
+  }, [error]);
+};
 
 /**
  * Performs the getActions async call and returns its value
  */
-export const useLoadActions = (context: CellActionExecutionContext) => {
+export const useLoadActions = (context: CellActionCompatibilityContext): AsyncActions => {
   const { getActions } = useCellActionsContext();
-  return useAsync(() => getActions(context), []);
+  const { error, ...actionsState } = useAsync(() => getActions(context), []);
+  useThrowError(error);
+  return actionsState;
 };
 
 /**
  * Returns a function to perform the getActions async call
  */
-export const useLoadActionsFn = () => {
+export const useLoadActionsFn = (): [AsyncActions, GetActions] => {
   const { getActions } = useCellActionsContext();
-  return useAsyncFn(getActions, []);
+  const [{ error, ...actionsState }, loadActions] = useAsyncFn(getActions, []);
+  useThrowError(error);
+  return [actionsState, loadActions];
 };
 
 /**
  * Groups getActions calls for an array of contexts in one async bulk operation
  */
-export const useBulkLoadActions = (contexts: CellActionExecutionContext[]) => {
+export const useBulkLoadActions = (
+  contexts: CellActionCompatibilityContext[]
+): AsyncActions<CellAction[][]> => {
   const { getActions } = useCellActionsContext();
-  return useAsync(() => Promise.all(contexts.map((context) => getActions(context))), []);
+  const { error, ...actionsState } = useAsync(
+    () => Promise.all(contexts.map((context) => getActions(context))),
+    []
+  );
+  useThrowError(error);
+  return actionsState;
 };
