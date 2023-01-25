a24762024c48dac6052fcf9d255cb8a7036f6df0
semd <sergi.massaneda@elastic.co>
2023-01-25 18:28:38 +0100

    async chunk size improvement

--- x-pack/plugins/security_solution/public/actions/utils.ts
+++ x-pack/plugins/security_solution/public/actions/utils.ts
@@ -7,7 +7,14 @@
 import type { IEmbeddable } from '@kbn/embeddable-plugin/public';
 import { LENS_EMBEDDABLE_TYPE, type Embeddable as LensEmbeddable } from '@kbn/lens-plugin/public';
 import { APP_UI_ID } from '../../common/constants';
-import { FIELDS_WITHOUT_CELL_ACTIONS } from '../common/lib/cell_actions/constants';
+
+/** actions are disabled for these fields in tables and popovers */
+export const FIELDS_WITHOUT_CELL_ACTIONS = [
+  'signal.rule.risk_score',
+  'signal.reason',
+  'kibana.alert.risk_score',
+  'kibana.alert.reason',
+];
 
 export const isInSecurityApp = (currentAppId?: string): boolean => {
   return !!currentAppId && currentAppId === APP_UI_ID;
