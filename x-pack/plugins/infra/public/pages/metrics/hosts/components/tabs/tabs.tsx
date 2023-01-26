645aaae853a97bb05bdddac008f195af8b4c3d90
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-01-26 11:16:42 +0100

    feat(infra): add alerts tab entry into hosts view

--- x-pack/plugins/infra/public/pages/metrics/hosts/components/tabs/tabs.tsx
+++ x-pack/plugins/infra/public/pages/metrics/hosts/components/tabs/tabs.tsx
@@ -6,13 +6,20 @@
  */
 
 import React from 'react';
-import { EuiTabbedContent, EuiSpacer, type EuiTabbedContentTab } from '@elastic/eui';
+import {
+  EuiTabbedContent,
+  EuiSpacer,
+  type EuiTabbedContentTab,
+  EuiNotificationBadge,
+} from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { MetricsGrid } from './metrics/metrics_grid';
+import { AlertsTabContent } from './alerts';
 
 interface WrapperProps {
   children: React.ReactElement;
 }
+
 const Wrapper = ({ children }: WrapperProps) => {
   return (
     <>
@@ -21,21 +28,40 @@ const Wrapper = ({ children }: WrapperProps) => {
     </>
   );
 };
-export const Tabs = () => {
-  const tabs: EuiTabbedContentTab[] = [
-    {
-      id: 'metrics',
-      name: i18n.translate('xpack.infra.hostsViewPage.tabs.metricsCharts.title', {
-        defaultMessage: 'Metrics',
-      }),
-      'data-test-subj': 'hostsView-tabs-metrics',
-      content: (
-        <Wrapper>
-          <MetricsGrid />
-        </Wrapper>
-      ),
-    },
-  ];
 
+const tabs: EuiTabbedContentTab[] = [
+  {
+    id: 'metrics',
+    name: i18n.translate('xpack.infra.hostsViewPage.tabs.metricsCharts.title', {
+      defaultMessage: 'Metrics',
+    }),
+    'data-test-subj': 'hostsView-tabs-metrics',
+    content: (
+      <Wrapper>
+        <MetricsGrid />
+      </Wrapper>
+    ),
+  },
+  {
+    id: 'alerts',
+    name: i18n.translate('xpack.infra.hostsViewPage.tabs.alerts.title', {
+      defaultMessage: 'Alerts',
+    }),
+    append: (
+      <EuiNotificationBadge className="eui-alignCenter" size="m">
+        {/* TODO: replace with number of real alerts */}
+        10
+      </EuiNotificationBadge>
+    ),
+    'data-test-subj': 'hostsView_tab_alerts',
+    content: (
+      <Wrapper>
+        <AlertsTabContent />
+      </Wrapper>
+    ),
+  },
+];
+
+export const Tabs = () => {
   return <EuiTabbedContent tabs={tabs} initialSelectedTab={tabs[0]} autoFocus="selected" />;
 };
