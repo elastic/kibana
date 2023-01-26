1cf4c8229a544a57eb6667a8c5e7b2d8a39fd8c3
István Zoltán Szabó <szabosteve@gmail.com>
2023-01-26 11:28:30 +0100

    [ML] Adjusts wording.

--- x-pack/plugins/ml/public/application/jobs/new_job/pages/job_type/page.tsx
+++ x-pack/plugins/ml/public/application/jobs/new_job/pages/job_type/page.tsx
@@ -235,7 +235,7 @@ export const Page: FC = () => {
         defaultMessage: 'Geo',
       }),
       description: i18n.translate('xpack.ml.newJob.wizard.jobType.geoDescription', {
-        defaultMessage: 'Detect anomalies in the geographic location of the input data.',
+        defaultMessage: 'Detect anomalies in the geographic location of the data.',
       }),
       id: 'mlJobTypeLinkGeoJob',
     });
