fc4a6b8435866b0d5df8979ef3fdef59f690a1bb
Marco Vettorello <marco.vettorello@elastic.co>
2023-01-27 15:52:01 +0100

    [8.6] [Lens] gracefully handle JS Object method names as field names (#148118) (#149688)
    # Backport
    
    This will backport the following commits from `main` to `8.6`:
    - [[Lens] gracefully handle JS Object method names as field names
    (#148118)](https://github.com/elastic/kibana/pull/148118)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Marco
    Vettorello","email":"marco.vettorello@elastic.co"},"sourceCommit":{"committedDate":"2023-01-27T13:40:51Z","message":"[Lens]
    gracefully handle JS Object method names as field names (#148118)\n\nThe
    commit fixed the bits of code where we were using directly an index
    field name as an object key. This can cause errors if the field name is
    a reserved JS keyword.\r\nThe fix reconfigured that part of the code by
    replacing plain Objects with `Map` that doesn't suffer from this
    issue.","sha":"5a2f51c347425ce34fe99a1824ae47f0c5c56f79","branchLabelMapping":{"^v8.7.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["bug","release_note:fix","Team:Visualizations","Feature:Lens","backport:skip","v8.7.0"],"number":148118,"url":"https://github.com/elastic/kibana/pull/148118","mergeCommit":{"message":"[Lens]
    gracefully handle JS Object method names as field names (#148118)\n\nThe
    commit fixed the bits of code where we were using directly an index
    field name as an object key. This can cause errors if the field name is
    a reserved JS keyword.\r\nThe fix reconfigured that part of the code by
    replacing plain Objects with `Map` that doesn't suffer from this
    issue.","sha":"5a2f51c347425ce34fe99a1824ae47f0c5c56f79"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v8.7.0","labelRegex":"^v8.7.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/148118","number":148118,"mergeCommit":{"message":"[Lens]
    gracefully handle JS Object method names as field names (#148118)\n\nThe
    commit fixed the bits of code where we were using directly an index
    field name as an object key. This can cause errors if the field name is
    a reserved JS keyword.\r\nThe fix reconfigured that part of the code by
    replacing plain Objects with `Map` that doesn't suffer from this
    issue.","sha":"5a2f51c347425ce34fe99a1824ae47f0c5c56f79"}}]}]
    BACKPORT-->

--- x-pack/plugins/lens/public/datasources/form_based/dimension_panel/dimension_panel.test.tsx
+++ x-pack/plugins/lens/public/datasources/form_based/dimension_panel/dimension_panel.test.tsx
@@ -122,6 +122,15 @@ const fields = [
     searchable: true,
     exists: true,
   },
+  // Added to test issue#148062 about the use of Object method names as fields name
+  ...Object.getOwnPropertyNames(Object.getPrototypeOf({})).map((name) => ({
+    name,
+    displayName: name,
+    type: 'string',
+    aggregatable: true,
+    searchable: true,
+    exists: true,
+  })),
   documentField,
 ];
 
@@ -330,7 +339,7 @@ describe('FormBasedDimensionEditor', () => {
       .filter('[data-test-subj="indexPattern-dimension-field"]')
       .prop('options');
 
-    expect(options).toHaveLength(2);
+    expect(options).toHaveLength(3);
 
     expect(options![0].label).toEqual('Records');
     expect(options![1].options!.map(({ label }) => label)).toEqual([
@@ -339,6 +348,11 @@ describe('FormBasedDimensionEditor', () => {
       'memory',
       'source',
     ]);
+
+    // these fields are generated to test the issue #148062 about fields that are using JS Object method names
+    expect(options![2].options!.map(({ label }) => label)).toEqual(
+      Object.getOwnPropertyNames(Object.getPrototypeOf({})).sort()
+    );
   });
 
   it('should hide fields that have no data', () => {
