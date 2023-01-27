ed7965df483e00a17b14702b328843882b75275c
Maxim Palenov <maxim.palenov@elastic.co>
2023-01-27 11:52:24 +0100

    remove TBC acronym

--- packages/core/saved-objects/core-saved-objects-migration-server-internal/src/core/unused_types.ts
+++ packages/core/saved-objects/core-saved-objects-migration-server-internal/src/core/unused_types.ts
@@ -33,7 +33,7 @@ export const REMOVED_TYPES: string[] = [
   'osquery-usage-metric',
   // Was removed in 8.1 https://github.com/elastic/kibana/issues/91265
   'siem-detection-engine-rule-status',
-  // Was removed in 8.7 (TBC) https://github.com/elastic/kibana/issues/130966
+  // Was removed in 8.7 https://github.com/elastic/kibana/issues/130966
   'siem-detection-engine-rule-execution-info',
   // Was removed in 7.16
   'timelion-sheet',
