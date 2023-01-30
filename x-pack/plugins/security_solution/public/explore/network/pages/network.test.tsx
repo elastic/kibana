9bea4560754ad7b5bd8f0761f4afe3da0fd38e67
Angela Chuang <yi-chun.chuang@elastic.co>
2023-01-30 15:55:12 +0000

    fix unit tests

--- x-pack/plugins/security_solution/public/explore/network/pages/network.test.tsx
+++ x-pack/plugins/security_solution/public/explore/network/pages/network.test.tsx
@@ -39,9 +39,7 @@ jest.mock('../../../common/components/search_bar', () => ({
 jest.mock('../../../common/components/query_bar', () => ({
   QueryBar: () => null,
 }));
-jest.mock('../../../common/components/visualization_actions', () => ({
-  VisualizationActions: jest.fn(() => <div data-test-subj="mock-viz-actions" />),
-}));
+jest.mock('../../../common/components/visualization_actions/actions');
 
 type Action = 'PUSH' | 'POP' | 'REPLACE';
 const pop: Action = 'POP';
