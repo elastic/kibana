0469fdc76240bcd9b9ecb833934350c34d7bd1e5
Jonathan Budzenski <jon@elastic.co>
2023-01-30 12:17:37 -0600

    [build] Build canvas runtime before copying source files

--- src/dev/build/build_distributables.ts
+++ src/dev/build/build_distributables.ts
@@ -64,6 +64,10 @@ export async function buildDistributables(log: ToolingLog, options: BuildOptions
    * run platform-generic build tasks
    */
   if (options.createGenericFolders) {
+    if (options.buildCanvasShareableRuntime) {
+      await run(Tasks.BuildCanvasShareableRuntime);
+    }
+
     await run(Tasks.CopySource);
     await run(Tasks.CopyBinScripts);
 
@@ -71,9 +75,6 @@ export async function buildDistributables(log: ToolingLog, options: BuildOptions
     await run(Tasks.CreateReadme);
     await run(Tasks.BuildBazelPackages);
     await run(Tasks.ReplaceFavicon);
-    if (options.buildCanvasShareableRuntime) {
-      await run(Tasks.BuildCanvasShareableRuntime);
-    }
     await run(Tasks.BuildKibanaPlatformPlugins);
     if (options.buildExamplePlugins) {
       await run(Tasks.BuildKibanaExamplePlugins);
