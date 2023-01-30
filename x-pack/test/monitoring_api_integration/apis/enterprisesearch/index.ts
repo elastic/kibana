870f79df8caf86900cfba17e0f05be9dce012990
Kevin Lacabane <kevin.lacabane@elastic.co>
2023-01-30 13:43:13 +0100

    [Stack Monitoring] support entsearch package (#148668)
    ### Summary
    
    Update stack monitoring to read from data streams created by the
    [enterprisesearch
    package](https://github.com/elastic/integrations/pull/4926)
    
    ### Testing
    - build [enterprisesearch
    package](https://github.com/elastic/integrations/pull/4926)
    - start stack: `elastic-package stack up -v -d --version
    8.7.0-SNAPSHOT`; make sure your image contains [this
    change](https://github.com/elastic/elastic-agent/pull/2121)
    - start enterprisesearch service: `elastic-package service up -v`
    - install elasticsearch integration with variables
    - `hosts: http://host.docker.internal:9201; username: elastic; password:
    changeme`
    - install enterprisesearch integration with variables
    - `hosts: http://host.docker.internal:3002; username: elastic; password:
    changeme`
    - connect [local
    kibana](https://github.com/elastic/kibana/blob/2d893bf40b47864058065f8fd2998d63c87f65f9/x-pack/plugins/monitoring/dev_docs/how_to/work_with_packages.md#connecting-a-local-kibana)
    - navigate to Stack Monitoring on the local kibana, enterprise search
    section shows up with populated graphs
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/monitoring_api_integration/apis/enterprisesearch/index.ts
+++ x-pack/test/monitoring_api_integration/apis/enterprisesearch/index.ts
@@ -5,12 +5,10 @@
  * 2.0.
  */
 
-import { FtrProviderContext } from '../../api_integration/ftr_provider_context';
+import { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
-  describe('Monitoring Endpoints', function () {
-    loadTestFile(require.resolve('./apm'));
-    loadTestFile(require.resolve('./beats'));
-    loadTestFile(require.resolve('./enterprisesearch'));
+  describe('Enterprisesearch', () => {
+    loadTestFile(require.resolve('./overview'));
   });
 }
