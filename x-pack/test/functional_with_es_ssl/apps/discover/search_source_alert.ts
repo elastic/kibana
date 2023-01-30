18e157a1a0d3e6de0873d19ef03e88df3d82b5ab
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2023-01-30 13:22:10 -0500

    skip failing test suite (#148388)

--- x-pack/test/functional_with_es_ssl/apps/discover/search_source_alert.ts
+++ x-pack/test/functional_with_es_ssl/apps/discover/search_source_alert.ts
@@ -306,7 +306,8 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     expect(await titleElem.getAttribute('value')).to.equal(dataView);
   };
 
-  describe('Search source Alert', () => {
+  // Failing: See https://github.com/elastic/kibana/issues/148388
+  describe.skip('Search source Alert', () => {
     before(async () => {
       await security.testUser.setRoles(['discover_alert']);
 
