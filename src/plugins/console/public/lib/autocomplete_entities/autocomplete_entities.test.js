80283c627f3733890eb0a0b190f6766aa753c66e
Dima Arnautov <dmitrii.arnautov@elastic.co>
2023-01-31 14:34:36 +0100

    update unit tests

--- src/plugins/console/public/lib/autocomplete_entities/autocomplete_entities.test.js
+++ src/plugins/console/public/lib/autocomplete_entities/autocomplete_entities.test.js
@@ -9,6 +9,9 @@
 import '../../application/models/sense_editor/sense_editor.test.mocks';
 import { setAutocompleteInfo, AutocompleteInfo } from '../../services';
 import { expandAliases } from './expand_aliases';
+import { httpServiceMock } from '@kbn/core-http-browser-mocks';
+import { SettingsMock } from '../../services/settings.mock';
+import { StorageMock } from '../../services/storage.mock';
 
 function fc(f1, f2) {
   if (f1.name < f2.name) {
@@ -32,10 +35,18 @@ describe('Autocomplete entities', () => {
   let componentTemplate;
   let dataStream;
   let autocompleteInfo;
+  let settingsMock;
+
   beforeEach(() => {
     autocompleteInfo = new AutocompleteInfo();
     setAutocompleteInfo(autocompleteInfo);
     mapping = autocompleteInfo.mapping;
+
+    const storage = new StorageMock({}, 'test');
+    settingsMock = new SettingsMock(storage);
+
+    mapping.setup(httpServiceMock.createSetupContract(), settingsMock);
+
     alias = autocompleteInfo.alias;
     legacyTemplate = autocompleteInfo.legacyTemplate;
     indexTemplate = autocompleteInfo.indexTemplate;
@@ -48,61 +59,45 @@ describe('Autocomplete entities', () => {
   });
 
   describe('Mappings', function () {
-    test('Multi fields 1.0 style', function () {
-      mapping.loadMappings({
-        index: {
-          properties: {
-            first_name: {
-              type: 'string',
-              index: 'analyzed',
-              path: 'just_name',
-              fields: {
-                any_name: { type: 'string', index: 'analyzed' },
+    describe('When fields autocomplete is enabled', () => {
+      beforeEach(() => {
+        settingsMock.getAutocomplete.mockReturnValue({ fields: true });
+      });
+
+      test('Multi fields 1.0 style', function () {
+        mapping.loadMappings({
+          index: {
+            properties: {
+              first_name: {
+                type: 'string',
+                index: 'analyzed',
+                path: 'just_name',
+                fields: {
+                  any_name: { type: 'string', index: 'analyzed' },
+                },
               },
-            },
-            last_name: {
-              type: 'string',
-              index: 'no',
-              fields: {
-                raw: { type: 'string', index: 'analyzed' },
+              last_name: {
+                type: 'string',
+                index: 'no',
+                fields: {
+                  raw: { type: 'string', index: 'analyzed' },
+                },
               },
             },
           },
-        },
+        });
+
+        expect(mapping.getMappings('index').sort(fc)).toEqual([
+          f('any_name', 'string'),
+          f('first_name', 'string'),
+          f('last_name', 'string'),
+          f('last_name.raw', 'string'),
+        ]);
       });
 
-      expect(mapping.getMappings('index').sort(fc)).toEqual([
-        f('any_name', 'string'),
-        f('first_name', 'string'),
-        f('last_name', 'string'),
-        f('last_name.raw', 'string'),
-      ]);
-    });
-
-    test('Simple fields', function () {
-      mapping.loadMappings({
-        index: {
-          properties: {
-            str: {
-              type: 'string',
-            },
-            number: {
-              type: 'int',
-            },
-          },
-        },
-      });
-
-      expect(mapping.getMappings('index').sort(fc)).toEqual([
-        f('number', 'int'),
-        f('str', 'string'),
-      ]);
-    });
-
-    test('Simple fields - 1.0 style', function () {
-      mapping.loadMappings({
-        index: {
-          mappings: {
+      test('Simple fields', function () {
+        mapping.loadMappings({
+          index: {
             properties: {
               str: {
                 type: 'string',
@@ -112,108 +107,130 @@ describe('Autocomplete entities', () => {
               },
             },
           },
-        },
+        });
+
+        expect(mapping.getMappings('index').sort(fc)).toEqual([
+          f('number', 'int'),
+          f('str', 'string'),
+        ]);
       });
 
-      expect(mapping.getMappings('index').sort(fc)).toEqual([
-        f('number', 'int'),
-        f('str', 'string'),
-      ]);
-    });
-
-    test('Nested fields', function () {
-      mapping.loadMappings({
-        index: {
-          properties: {
-            person: {
-              type: 'object',
+      test('Simple fields - 1.0 style', function () {
+        mapping.loadMappings({
+          index: {
+            mappings: {
               properties: {
-                name: {
-                  properties: {
-                    first_name: { type: 'string' },
-                    last_name: { type: 'string' },
+                str: {
+                  type: 'string',
+                },
+                number: {
+                  type: 'int',
+                },
+              },
+            },
+          },
+        });
+
+        expect(mapping.getMappings('index').sort(fc)).toEqual([
+          f('number', 'int'),
+          f('str', 'string'),
+        ]);
+      });
+
+      test('Nested fields', function () {
+        mapping.loadMappings({
+          index: {
+            properties: {
+              person: {
+                type: 'object',
+                properties: {
+                  name: {
+                    properties: {
+                      first_name: { type: 'string' },
+                      last_name: { type: 'string' },
+                    },
                   },
+                  sid: { type: 'string', index: 'not_analyzed' },
                 },
-                sid: { type: 'string', index: 'not_analyzed' },
               },
+              message: { type: 'string' },
             },
-            message: { type: 'string' },
           },
-        },
+        });
+
+        expect(mapping.getMappings('index', []).sort(fc)).toEqual([
+          f('message'),
+          f('person.name.first_name'),
+          f('person.name.last_name'),
+          f('person.sid'),
+        ]);
       });
 
-      expect(mapping.getMappings('index', []).sort(fc)).toEqual([
-        f('message'),
-        f('person.name.first_name'),
-        f('person.name.last_name'),
-        f('person.sid'),
-      ]);
-    });
-
-    test('Enabled fields', function () {
-      mapping.loadMappings({
-        index: {
-          properties: {
-            person: {
-              type: 'object',
-              properties: {
-                name: {
-                  type: 'object',
-                  enabled: false,
+      test('Enabled fields', function () {
+        mapping.loadMappings({
+          index: {
+            properties: {
+              person: {
+                type: 'object',
+                properties: {
+                  name: {
+                    type: 'object',
+                    enabled: false,
+                  },
+                  sid: { type: 'string', index: 'not_analyzed' },
                 },
-                sid: { type: 'string', index: 'not_analyzed' },
               },
-            },
-            message: { type: 'string' },
-          },
-        },
-      });
-
-      expect(mapping.getMappings('index', []).sort(fc)).toEqual([f('message'), f('person.sid')]);
-    });
-
-    test('Path tests', function () {
-      mapping.loadMappings({
-        index: {
-          properties: {
-            name1: {
-              type: 'object',
-              path: 'just_name',
-              properties: {
-                first1: { type: 'string' },
-                last1: { type: 'string', index_name: 'i_last_1' },
-              },
-            },
-            name2: {
-              type: 'object',
-              path: 'full',
-              properties: {
-                first2: { type: 'string' },
-                last2: { type: 'string', index_name: 'i_last_2' },
-              },
+              message: { type: 'string' },
             },
           },
-        },
+        });
+
+        expect(mapping.getMappings('index', []).sort(fc)).toEqual([f('message'), f('person.sid')]);
       });
 
-      expect(mapping.getMappings().sort(fc)).toEqual([
-        f('first1'),
-        f('i_last_1'),
-        f('name2.first2'),
-        f('name2.i_last_2'),
-      ]);
-    });
-
-    test('Use index_name tests', function () {
-      mapping.loadMappings({
-        index: {
-          properties: {
-            last1: { type: 'string', index_name: 'i_last_1' },
+      test('Path tests', function () {
+        mapping.loadMappings({
+          index: {
+            properties: {
+              name1: {
+                type: 'object',
+                path: 'just_name',
+                properties: {
+                  first1: { type: 'string' },
+                  last1: { type: 'string', index_name: 'i_last_1' },
+                },
+              },
+              name2: {
+                type: 'object',
+                path: 'full',
+                properties: {
+                  first2: { type: 'string' },
+                  last2: { type: 'string', index_name: 'i_last_2' },
+                },
+              },
+            },
           },
-        },
+        });
+
+        expect(mapping.getMappings().sort(fc)).toEqual([
+          f('first1'),
+          f('i_last_1'),
+          f('name2.first2'),
+          f('name2.i_last_2'),
+        ]);
       });
 
-      expect(mapping.getMappings().sort(fc)).toEqual([f('i_last_1')]);
+      test('Use index_name tests', function () {
+        mapping.loadMappings({
+          index: {
+            properties: {
+              last1: { type: 'string', index_name: 'i_last_1' },
+            },
+          },
+        });
+
+        expect(mapping.getMappings().sort(fc)).toEqual([f('i_last_1')]);
+      });
     });
   });
 
