10d5677ae6dfa7c477a7afd7c3228e3c28ab749b
shahzad31 <shahzad31comp@gmail.com>
2023-01-31 16:53:06 +0100

    fix type

--- packages/shared-ux/prompt/not_found/src/not_found_prompt.tsx
+++ packages/shared-ux/prompt/not_found/src/not_found_prompt.tsx
@@ -73,7 +73,7 @@ export const NotFoundPrompt = ({ actions, title, body }: NotFoundProps) => {
       color="subdued"
       titleSize="m"
       icon={icon}
-      title={typeof title === 'string' ? <h2>{title ?? NOT_FOUND_TITLE}</h2> : title}
+      title={typeof title === 'string' || !title ? <h2>{title ?? NOT_FOUND_TITLE}</h2> : title}
       body={body ?? NOT_FOUND_BODY}
       actions={actions ?? DEFAULT_ACTIONS}
     />
