2eabbf36b5bc983466eedd9a2bdd28cb44441020
Jatin Kathuria <jatin.kathuria@elastic.co>
2023-01-31 10:34:08 +0100

    reverted unnecessary changes in rule strategy

--- x-pack/plugins/rule_registry/server/search_strategy/search_strategy.ts
+++ x-pack/plugins/rule_registry/server/search_strategy/search_strategy.ts
@@ -146,7 +146,6 @@ export const ruleRegistrySearchStrategyProvider = (
         map((response) => {
           // Do we have to loop over each hit? Yes.
           // ecs auditLogger requires that we log each alert independently
-          //
           if (securityAuditLogger != null) {
             response.rawResponse.hits?.hits?.forEach((hit) => {
               securityAuditLogger.log(
