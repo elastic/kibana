0707a207b49d0d19cb949742642afc3a872a1b85
Jenny <dzheni.pavlova@elastic.co>
2023-01-31 20:25:18 +0100

    Show the kubernetes tour under the drop-down button label

--- x-pack/plugins/infra/public/pages/metrics/inventory_view/components/dropdown_button.tsx
+++ x-pack/plugins/infra/public/pages/metrics/inventory_view/components/dropdown_button.tsx
@@ -8,17 +8,35 @@
 import { EuiFlexGroup, EuiFlexItem, EuiButtonEmpty } from '@elastic/eui';
 import React, { ReactNode } from 'react';
 import { withTheme, EuiTheme } from '@kbn/kibana-react-plugin/common';
+import { ShowKubernetesTour } from './show_kubernetes_tour';
 
 interface Props {
   'data-test-subj'?: string;
   label: string;
+  onLabelClick?: () => void;
   onClick: () => void;
   theme: EuiTheme | undefined;
   children: ReactNode;
+  showKubernetesInfo: boolean;
 }
 
+const ButtonLabel = (label: string, theme?: EuiTheme) => (
+  <EuiFlexItem
+    grow={false}
+    style={{
+      padding: 12,
+      background: theme?.eui.euiFormInputGroupLabelBackground,
+      fontSize: '0.75em',
+      fontWeight: 600,
+      color: theme?.eui.euiTitleColor,
+    }}
+  >
+    {label}
+  </EuiFlexItem>
+);
+
 export const DropdownButton = withTheme((props: Props) => {
-  const { onClick, label, theme, children } = props;
+  const { onClick, label, theme, children, showKubernetesInfo } = props;
   return (
     <EuiFlexGroup
       alignItems="center"
@@ -28,18 +46,11 @@ export const DropdownButton = withTheme((props: Props) => {
         boxShadow: `0px 3px 2px ${theme?.eui.euiTableActionsBorderColor}, 0px 1px 1px ${theme?.eui.euiTableActionsBorderColor}`,
       }}
     >
-      <EuiFlexItem
-        grow={false}
-        style={{
-          padding: 12,
-          background: theme?.eui.euiFormInputGroupLabelBackground,
-          fontSize: '0.75em',
-          fontWeight: 600,
-          color: theme?.eui.euiTitleColor,
-        }}
-      >
-        {label}
-      </EuiFlexItem>
+      {showKubernetesInfo ? (
+        <ShowKubernetesTour>{ButtonLabel(label, theme)}</ShowKubernetesTour>
+      ) : (
+        ButtonLabel(label, theme)
+      )}
       <EuiFlexItem grow={false}>
         <EuiButtonEmpty
           data-test-subj={props['data-test-subj']}
