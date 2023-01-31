4e6a546b6cd4e683bfdbf8e757f3c837e3b46a1f
Sloane Perrault <sloane.perrault@gmail.com>
2023-01-31 14:23:04 -0500

    add indices, documents counts to overview

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engine/engine_overview.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engine/engine_overview.tsx
@@ -5,10 +5,11 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { useValues } from 'kea';
 
+import { EuiFlexGroup, EuiFlexItem, EuiIcon, EuiPanel, EuiStat } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
 import { EngineViewTabs } from '../../routes';
@@ -18,7 +19,13 @@ import { EngineViewHeaderActions } from './engine_view_header_actions';
 import { EngineViewLogic } from './engine_view_logic';
 
 export const EngineOverview: React.FC = () => {
-  const { engineName, isLoadingEngine } = useValues(EngineViewLogic);
+  const { engineName, engineData, isLoadingEngine } = useValues(EngineViewLogic);
+
+  const indicesCount = engineData?.indices?.length;
+  const documentsCount = useMemo(
+    () => engineData?.indices?.reduce((sum, { count }) => sum + count, 0),
+    [engineData]
+  );
 
   return (
     <EnterpriseSearchEnginesPageTemplate
@@ -32,6 +39,57 @@ export const EngineOverview: React.FC = () => {
         rightSideItems: [<EngineViewHeaderActions />],
       }}
       engineName={engineName}
-    />
+    >
+      <>
+        <EuiPanel hasShadow={false} hasBorder paddingSize="l">
+          <EuiFlexGroup justifyContent="spaceBetween" alignItems="center">
+            <EuiFlexItem>
+              <EuiFlexGroup alignItems="center">
+                <EuiIcon size="xxl" type="visTable" color="#98A2B3" />
+                <EuiStat
+                  titleSize="l"
+                  isLoading={!engineData}
+                  title={`${indicesCount}`}
+                  description={i18n.translate(
+                    'xpack.enterpriseSearch.content.engine.overview.indicesDescription',
+                    { defaultMessage: 'Indices' }
+                  )}
+                  titleColor="primary"
+                />
+              </EuiFlexGroup>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiFlexGroup alignItems="center">
+                <EuiIcon size="xxl" type="documents" color="#98A2B3" />
+                <EuiStat
+                  titleSize="l"
+                  isLoading={!engineData}
+                  title={`${documentsCount}`}
+                  description={i18n.translate(
+                    'xpack.enterpriseSearch.content.engine.overview.documentsDescription',
+                    { defaultMessage: 'Documents' }
+                  )}
+                  titleColor="primary"
+                />
+              </EuiFlexGroup>
+            </EuiFlexItem>
+            {/* <EuiFlexItem>
+              <EuiFlexGroup alignItems="center">
+                <EuiIcon size="xxl" type="kqlField" color="#98A2B3" />
+                <EuiStat
+                  titleSize="l"
+                  isLoading
+                  description={i18n.translate(
+                    'xpack.enterpriseSearch.content.engine.overview.fieldsDescription',
+                    { defaultMessage: 'Fields' }
+                  )}
+                  titleColor="primary"
+                />
+              </EuiFlexGroup>
+            </EuiFlexItem> */}
+          </EuiFlexGroup>
+        </EuiPanel>
+      </>
+    </EnterpriseSearchEnginesPageTemplate>
   );
 };
