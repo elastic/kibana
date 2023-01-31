af11722a0857fcb27fb007f701d799a5d7c90601
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-01-31 15:13:18 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/fleet/public/applications/integrations/sections/epm/screens/home/index.tsx
+++ x-pack/plugins/fleet/public/applications/integrations/sections/epm/screens/home/index.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import type { CustomIntegration } from '@kbn/custom-integrations-plugin/common';
