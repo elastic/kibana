bf4d720f50d4b1d276feef0c532e2ed4100533b6
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-02-01 12:25:49 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/infra/public/pages/metrics/hosts/hooks/use_unified_search_url_state.ts
+++ x-pack/plugins/infra/public/pages/metrics/hosts/hooks/use_unified_search_url_state.ts
@@ -26,8 +26,8 @@ const DEFAULT_FROM_IN_MILLISECONDS = DEFAULT_FROM_MINUTES_VALUE * 60000;
 
 export const INITIAL_DATE_RANGE = { from: `now-${DEFAULT_FROM_MINUTES_VALUE}m`, to: 'now' };
 
-const getDefaultFromTimestamp = () => Date.now() - DEFAULT_FROM_IN_MILLISECONDS
-const getDefaultToTimestamp = () => Date.now()
+const getDefaultFromTimestamp = () => Date.now() - DEFAULT_FROM_IN_MILLISECONDS;
+const getDefaultToTimestamp = () => Date.now();
 
 const INITIAL_HOSTS_STATE: HostsState = {
   query: DEFAULT_QUERY,
