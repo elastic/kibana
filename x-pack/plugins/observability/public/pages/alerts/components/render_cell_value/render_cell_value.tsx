3d90dddd5d46b473c75e944627d79e863f6b5bc7
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-02-01 16:48:52 +0100

    fix(observability): render cell as button with display contents

--- x-pack/plugins/observability/public/pages/alerts/components/render_cell_value/render_cell_value.tsx
+++ x-pack/plugins/observability/public/pages/alerts/components/render_cell_value/render_cell_value.tsx
@@ -17,6 +17,7 @@ import {
 } from '@kbn/rule-data-utils';
 import type { CellValueElementProps, TimelineNonEcsData } from '@kbn/timelines-plugin/common';
 import { isEmpty } from 'lodash';
+import { css } from '@emotion/react';
 import { AlertStatusIndicator } from '../../../../components/shared/alert_status_indicator';
 import { TimestampTooltip } from '../../../../components/shared/timestamp_tooltip';
 import { asDuration } from '../../../../../common/utils/formatters';
@@ -52,6 +53,8 @@ const getRenderValue = (mappedNonEcsValue: any) => {
 
   return '—';
 };
+
+const displayCss = { display: 'contents' };
 
 /**
  * This implementation of `EuiDataGrid`'s `renderCellValue`
@@ -94,15 +97,7 @@ export const getRenderCellValue = ({
         const alert = parseAlert(observabilityRuleTypeRegistry)(dataFieldEs);
 
         return (
-          // NOTE: EuiLink automatically renders links using a <button>
-          // instead of an <a> when an `onClick` prop is provided, but this
-          // breaks text-truncation in `EuiDataGrid`, because (per the HTML
-          // spec), buttons are *always* rendered as `inline-block`, even if
-          // `display` is overridden. Passing an empty `href` prop forces
-          // `EuiLink` to render the link as an (inline) <a>, which enables
-          // text truncation, but requires overriding the linter warning below:
-          // eslint-disable-next-line @elastic/eui/href-or-on-click
-          <EuiLink href="" onClick={() => setFlyoutAlert && setFlyoutAlert(alert)}>
+          <EuiLink css={displayCss} onClick={() => setFlyoutAlert && setFlyoutAlert(alert)}>
             {alert.reason}
           </EuiLink>
         );
