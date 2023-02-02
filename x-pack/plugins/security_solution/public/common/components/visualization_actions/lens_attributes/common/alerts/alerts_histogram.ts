9cfec581c803a16209b8645141ee96fb55973361
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2023-02-02 15:06:06 +0000

    [SecuritySolution] Render histograms with Lens (#147261)
    ## Summary
    
    
    Relevant issue: https://github.com/elastic/kibana/issues/136409
    
    These are all behind feature flag `chartEmbeddablesEnabled`
    
    ---
    ### Changes:
    1. Legends are all moved to the left side of the chart to avoid
    overlapping with chart actions.
    2. The second `group by` of Alerts Trend Chart is removed (as it is
    always disabled).
    
    
    ---
    
    ### Events:
    <img width="2545" alt="Screenshot 2023-01-25 at 15 33 27"
    src="https://user-images.githubusercontent.com/6295984/214605761-2e08d848-feb3-42e3-9850-08cd7f8b9c99.png">
    
    ---
    ### Top N
    
    <img width="2552" alt="Screenshot 2023-01-25 at 15 34 21"
    src="https://user-images.githubusercontent.com/6295984/214605889-91f74c33-e8c4-43ac-b137-6098f4764a6d.png">
    
    ---
    
    ### No indices:
    <img width="1671" alt="Screenshot 2022-12-21 at 17 03 05"
    src="https://user-images.githubusercontent.com/6295984/208963467-ccbb6bf4-11b3-4bc8-b568-208dd0791828.png">
    
    ---
    
    ### Alerts - Trend
    <img width="2548" alt="Screenshot 2023-01-25 at 15 34 52"
    src="https://user-images.githubusercontent.com/6295984/214605993-ea1a2fe5-6f44-4c70-9152-79f76ed9e48f.png">
    
    ---
    
    ### ~Alerts - Treemap~ (Not included in this PR -
    https://github.com/elastic/kibana/issues/149592)
    **Big Difference after converting to Lens. Likely to have a redesign:**
    
    Known issues:
    1. Alerts tree map: Is not rendered exactly the same due to the limits
    of dimension of Lens.
    3. No value display in each legend item for alerts tree map
    4. Background color cannot be decided by risk score
    
    
    Before:
    <img width="1661" alt="Screenshot 2023-01-10 at 12 00 51"
    src="https://user-images.githubusercontent.com/6295984/211546444-f98b0f4a-0666-493c-b57e-934b3d154211.png">
    
    After:
    <img width="2550" alt="Screenshot 2023-01-25 at 15 35 16"
    src="https://user-images.githubusercontent.com/6295984/214606117-7a1e78a4-8947-4d95-9994-2acb106ca3af.png">
    
    ---
    ### ~Alerts - Charts~ (Not included in this PR -
    https://github.com/elastic/kibana/issues/149592)
    **Lens does not support the
    [design](https://github.com/elastic/security-team/issues/5599). Likely
    to have a redesign:**
    
    
    <img width="2536" alt="Screenshot 2023-01-25 at 15 35 55"
    src="https://user-images.githubusercontent.com/6295984/214606298-bcc97920-bb52-4367-901f-102a778799b6.png">
    
    ---
    
    ### Alerts - Table
    <img width="1666" alt="Screenshot 2022-12-21 at 17 12 25"
    src="https://user-images.githubusercontent.com/6295984/208964514-b39e40ae-ecb4-4e06-8cc5-32d63e28823a.png">
    
    Known issues:
    1. https://github.com/elastic/kibana/issues/149828
    2. Unable to restore a column after hiding it.
    3. https://github.com/elastic/kibana/issues/150048
    4. https://github.com/elastic/kibana/issues/150158
    
    
    ---
    ### Alerts - Preview
    
    Known issue:
    There's no legend in alerts preview as its legend action, filter in,
    filter out are not useful on rule creation page. -
    https://github.com/elastic/kibana/issues/149220
    
    <img width="2543" alt="Screenshot 2023-01-26 at 13 32 00"
    src="https://user-images.githubusercontent.com/6295984/214848163-437ca866-46ee-47ee-a550-aa3b9c97eef6.png">
    
    
    
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/common/components/visualization_actions/lens_attributes/common/alerts/alerts_histogram.ts
+++ x-pack/plugins/security_solution/public/common/components/visualization_actions/lens_attributes/common/alerts/alerts_histogram.ts
@@ -4,15 +4,16 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { v4 as uuidv4 } from 'uuid';
+import type { GetLensAttributes } from '../../../types';
+const layerId = uuidv4();
 
-import { COUNT, TOP_VALUE } from '../../translations';
-import type { GetLensAttributes, LensAttributes } from '../../types';
-
-export const getExternalAlertLensAttributes: GetLensAttributes = (
-  stackByField = 'event.module'
+export const getAlertsHistogramLensAttributes: GetLensAttributes = (
+  stackByField = 'kibana.alert.rule.name',
+  extraOptions
 ) => {
   return {
-    title: 'External alerts',
+    title: 'Alerts',
     description: '',
     visualizationType: 'lnsXY',
     state: {
@@ -21,19 +22,20 @@ export const getExternalAlertLensAttributes: GetLensAttributes = (
         legend: {
           isVisible: true,
           position: 'left',
+          legendSize: 'xlarge',
         },
         valueLabels: 'hide',
         preferredSeriesType: 'bar_stacked',
         layers: [
           {
-            layerId: 'a3c54471-615f-4ff9-9fda-69b5b2ea3eef',
-            accessors: ['0a923af2-c880-4aa3-aa93-a0b9c2801f6d'],
+            layerId,
+            accessors: ['e09e0380-0740-4105-becc-0a4ca12e3944'],
             position: 'top',
             seriesType: 'bar_stacked',
             showGridlines: false,
             layerType: 'data',
-            xAccessor: '37bdf546-3c11-4b08-8c5d-e37debc44f1d',
-            splitAccessor: '42334c6e-98d9-47a2-b4cb-a445abb44c93',
+            xAccessor: 'aac9d7d0-13a3-480a-892b-08207a787926',
+            splitAccessor: '34919782-4546-43a5-b668-06ac934d3acd',
           },
         ],
         yRightExtent: {
@@ -42,40 +44,24 @@ export const getExternalAlertLensAttributes: GetLensAttributes = (
         yLeftExtent: {
           mode: 'full',
         },
+        axisTitlesVisibilitySettings: {
+          x: false,
+          yLeft: false,
+          yRight: true,
+        },
+        valuesInLegend: true,
       },
       query: {
         query: '',
         language: 'kuery',
       },
-      filters: [
-        {
-          meta: {
-            index: 'a04472fc-94a3-4b8d-ae05-9d30ea8fbd6a',
-            alias: null,
-            negate: false,
-            disabled: false,
-            type: 'phrase',
-            key: 'event.kind',
-            params: {
-              query: 'alert',
-            },
-          },
-          query: {
-            match_phrase: {
-              'event.kind': 'alert',
-            },
-          },
-          $state: {
-            store: 'appState',
-          },
-        },
-      ],
+      filters: extraOptions?.filters ? extraOptions.filters : [],
       datasourceStates: {
         formBased: {
           layers: {
-            'a3c54471-615f-4ff9-9fda-69b5b2ea3eef': {
+            [layerId]: {
               columns: {
-                '37bdf546-3c11-4b08-8c5d-e37debc44f1d': {
+                'aac9d7d0-13a3-480a-892b-08207a787926': {
                   label: '@timestamp',
                   dataType: 'date',
                   operationType: 'date_histogram',
@@ -86,26 +72,26 @@ export const getExternalAlertLensAttributes: GetLensAttributes = (
                     interval: 'auto',
                   },
                 },
-                '0a923af2-c880-4aa3-aa93-a0b9c2801f6d': {
-                  label: COUNT,
+                'e09e0380-0740-4105-becc-0a4ca12e3944': {
+                  label: 'Count of records',
                   dataType: 'number',
                   operationType: 'count',
                   isBucketed: false,
                   scale: 'ratio',
                   sourceField: '___records___',
                 },
-                '42334c6e-98d9-47a2-b4cb-a445abb44c93': {
-                  label: TOP_VALUE(`${stackByField}`), // could be event.category
+                '34919782-4546-43a5-b668-06ac934d3acd': {
+                  label: `Top values of ${stackByField}`,
                   dataType: 'string',
                   operationType: 'terms',
                   scale: 'ordinal',
-                  sourceField: `${stackByField}`, // could be event.category
+                  sourceField: stackByField,
                   isBucketed: true,
                   params: {
-                    size: 10,
+                    size: 1000,
                     orderBy: {
                       type: 'column',
-                      columnId: '0a923af2-c880-4aa3-aa93-a0b9c2801f6d',
+                      columnId: 'e09e0380-0740-4105-becc-0a4ca12e3944',
                     },
                     orderDirection: 'desc',
                     otherBucket: true,
@@ -113,41 +99,29 @@ export const getExternalAlertLensAttributes: GetLensAttributes = (
                     parentFormat: {
                       id: 'terms',
                     },
+                    secondaryFields: [],
                   },
                 },
               },
               columnOrder: [
-                '42334c6e-98d9-47a2-b4cb-a445abb44c93',
-                '37bdf546-3c11-4b08-8c5d-e37debc44f1d',
-                '0a923af2-c880-4aa3-aa93-a0b9c2801f6d',
+                '34919782-4546-43a5-b668-06ac934d3acd',
+                'aac9d7d0-13a3-480a-892b-08207a787926',
+                'e09e0380-0740-4105-becc-0a4ca12e3944',
               ],
               incompleteColumns: {},
             },
           },
         },
       },
+      internalReferences: [],
+      adHocDataViews: {},
     },
     references: [
       {
         type: 'index-pattern',
         id: '{dataViewId}',
-        name: 'indexpattern-datasource-current-indexpattern',
-      },
-      {
-        type: 'index-pattern',
-        id: '{dataViewId}',
-        name: 'indexpattern-datasource-layer-a3c54471-615f-4ff9-9fda-69b5b2ea3eef',
-      },
-      {
-        type: 'index-pattern',
-        name: '723c4653-681b-4105-956e-abef287bf025',
-        id: '{dataViewId}',
-      },
-      {
-        type: 'index-pattern',
-        name: 'a04472fc-94a3-4b8d-ae05-9d30ea8fbd6a',
-        id: '{dataViewId}',
+        name: `indexpattern-datasource-layer-${layerId}`,
       },
     ],
-  } as LensAttributes;
+  };
 };
