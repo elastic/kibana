fb638942ef1f23847a5bc9b73e5da93ff294ad2f
Candace Park <candace.park@elastic.co>
2023-02-02 17:50:57 -0500

    unskip endpoint list functional test

--- x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_list.ts
+++ x-pack/test/security_solution_endpoint/apps/endpoint/endpoint_list.ts
@@ -82,8 +82,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     return tableData;
   };
 
-  // Failing: See https://github.com/elastic/kibana/issues/148111
-  describe.skip('endpoint list', function () {
+  describe('endpoint list', function () {
     const sleep = (ms = 100) => new Promise((resolve) => setTimeout(resolve, ms));
     let indexedData: IndexedHostsAndAlertsResponse;
     describe('when initially navigating to page', () => {
