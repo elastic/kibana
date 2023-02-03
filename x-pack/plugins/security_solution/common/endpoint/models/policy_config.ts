01a7075725fdb1c58df59cac4a63b14637d5c6b5
Kevin Logan <kevin.logan@elastic.co>
2023-02-03 11:30:13 -0500

    [Security Solution] Mac env variables advanced option and default values

--- x-pack/plugins/security_solution/common/endpoint/models/policy_config.ts
+++ x-pack/plugins/security_solution/common/endpoint/models/policy_config.ts
@@ -104,6 +104,9 @@ export const policyFactory = (): PolicyConfig => {
       logging: {
         file: 'info',
       },
+      advanced: {
+        capture_env_vars: 'DYLD_INSERT_LIBRARIES, DYLD_FRAMEWORK_PATH, DYLD_LIBRARY_PATH, LD_PRELOAD',
+      }
     },
     linux: {
       events: {
