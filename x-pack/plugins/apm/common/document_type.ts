23e97feccbd3750dbd2c85d521b4893fff651f89
Dario Gieselaar <dario.gieselaar@elastic.co>
2023-02-04 12:00:25 +0100

    [APM] Use (rolled up) service metrics for service inventory (#149938)
    Closes https://github.com/elastic/kibana/issues/146682

--- x-pack/plugins/apm/common/document_type.ts
+++ x-pack/plugins/apm/common/document_type.ts
@@ -5,9 +5,9 @@
  * 2.0.
  */
 
-import { ApmDataSource } from './data_source';
-
-export interface TimeRangeMetadata {
-  isUsingServiceDestinationMetrics: boolean;
-  sources: Array<ApmDataSource & { hasDocs: boolean }>;
+export enum ApmDocumentType {
+  TransactionMetric = 'transactionMetric',
+  ServiceTransactionMetric = 'serviceTransactionMetric',
+  TransactionEvent = 'transactionEvent',
+  ServiceDestinationMetric = 'serviceDestinationMetric',
 }
