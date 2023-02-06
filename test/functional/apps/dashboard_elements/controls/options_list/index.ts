7f3f5f7e5efc36edcc5c24af09d7f3ec2d9a537d
Hannah Mudge <hannah.wright@elastic.co>
2023-02-06 10:42:00 -0700

    Remove `.only`

--- test/functional/apps/dashboard_elements/controls/options_list/index.ts
+++ test/functional/apps/dashboard_elements/controls/options_list/index.ts
@@ -34,7 +34,7 @@ export default function ({ loadTestFile, getService, getPageObjects }: FtrProvid
     await security.testUser.restoreDefaults();
   };
 
-  describe.only('Options list control', async () => {
+  describe('Options list control', async () => {
     before(setup);
     after(teardown);
 
