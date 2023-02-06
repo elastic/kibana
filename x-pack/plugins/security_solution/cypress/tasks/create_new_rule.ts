c26c3c6b9f7ee0a8b3746e3b765d0e8ec1de5d33
Jatin Kathuria <jatin.kathuria@elastic.co>
2023-02-06 16:43:45 +0100

    Refresh retry in cypress tests

--- x-pack/plugins/security_solution/cypress/tasks/create_new_rule.ts
+++ x-pack/plugins/security_solution/cypress/tasks/create_new_rule.ts
@@ -116,7 +116,6 @@ import { fillIndexConnectorForm, fillEmailConnectorForm } from './common/rule_ac
 import { TOAST_ERROR } from '../screens/shared';
 import { ALERTS_TABLE_COUNT } from '../screens/timeline';
 import { TIMELINE } from '../screens/timelines';
-import { refreshPage } from './security_header';
 import { EUI_FILTER_SELECT_ITEM, COMBO_BOX_INPUT } from '../screens/common/controls';
 import { ruleFields } from '../data/detection_engine';
 import { BACK_TO_RULES_TABLE } from '../screens/rule_details';
@@ -669,20 +668,24 @@ export const selectNewTermsRuleType = () => {
 };
 
 export const waitForAlertsToPopulate = async (alertCountThreshold = 1) => {
-  cy.waitUntil(
-    () => {
-      cy.log('Waiting for alerts to appear');
-      refreshPage();
-      return cy
-        .get(ALERTS_TABLE_COUNT)
-        .invoke('text')
-        .then((countText) => {
-          const alertCount = parseInt(countText, 10) || 0;
-          return alertCount >= alertCountThreshold;
-        });
-    },
-    { interval: 500, timeout: 12000 }
-  );
+  /*
+   *
+   * Some times alert table can take a few second to show up with updated results.
+   * This change retries refresh till either alertCount >= threshold or timeout
+   * is reached
+   * */
+  cy.get('body')
+    .pipe(
+      ($el) => {
+        $el.find(REFRESH_BUTTON).trigger('click');
+        return $el.find(ALERTS_TABLE_COUNT);
+      },
+      { timeout: 5000 }
+    )
+    .should(($el) => {
+      const alertCount = parseInt($el.text(), 10) || 0;
+      expect(alertCount).to.not.be.lessThan(alertCountThreshold);
+    });
   waitForAlerts();
 };
 
