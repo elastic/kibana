62d5a2a702268a7f807072740a9db30c620c92a2
Maxim Palenov <maxim.palenov@elastic.co>
2023-02-06 22:59:03 +0100

    [Security Solution] Add clearing rules table state functionality (#150059)
    **Addresses:** https://github.com/elastic/kibana/issues/145968
    
    ## Summary
    
    This PR adds functionality to clear persisted rules table state implemented in https://github.com/elastic/kibana/issues/140263.
    
    https://user-images.githubusercontent.com/3775283/216311325-e19e81e1-f232-4c16-b9df-a49fcc8c98d0.mov
    
    ### Checklist
    
    - [x] Any text added follows [EUI's writing guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses sentence case text and includes [i18n support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ] [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html) was added for features that require explanation or tutorials
    - [x] [Unit or functional tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html) were updated or added to match the most common scenarios
    - [x] Any UI touched in this PR is usable by keyboard only (learn more about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [x] Any UI touched in this PR does not create any new axe failures (run axe in browser: [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/), [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [x] This renders correctly on smaller devices using a responsive layout. (You can test this [in your browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [x] This was checked for [cross-browser compatibility](https://www.elastic.co/support/matrix#matrix_browsers)

--- x-pack/plugins/security_solution/public/detection_engine/rule_management_ui/components/rules_table/rules_table/use_rules_table_saved_state.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management_ui/components/rules_table/rules_table/use_rules_table_saved_state.ts
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-import { useEffect } from 'react';
 import type { Storage } from '@kbn/kibana-utils-plugin/public';
 import { validateNonExact } from '@kbn/securitysolution-io-ts-utils';
 import { useGetInitialUrlParamValue } from '../../../../../common/utils/global_query_string/helpers';
@@ -13,19 +12,16 @@ import { RULES_TABLE_MAX_PAGE_SIZE } from '../../../../../../common/constants';
 import { useKibana } from '../../../../../common/lib/kibana';
 import { URL_PARAM_KEY } from '../../../../../common/hooks/use_url_state';
 import { RULES_TABLE_STATE_STORAGE_KEY } from '../constants';
-import { useRulesTableContext } from './rules_table_context';
 import type {
   RulesTableStorageSavedState,
   RulesTableUrlSavedState,
 } from './rules_table_saved_state';
 import {
-  RuleSource,
   RulesTableSavedFilter,
   RulesTableStorageSavedPagination,
   RulesTableUrlSavedPagination,
   RulesTableSavedSorting,
 } from './rules_table_saved_state';
-import { DEFAULT_FILTER_OPTIONS, DEFAULT_SORTING_OPTIONS } from './rules_table_defaults';
 
 function readStorageState(storage: Storage): RulesTableStorageSavedState | null {
   try {
@@ -35,67 +31,64 @@ function readStorageState(storage: Storage): RulesTableStorageSavedState | null 
   }
 }
 
+export function useRulesTableSavedState(): {
+  filter?: RulesTableSavedFilter;
+  sorting?: RulesTableSavedSorting;
+  pagination?: RulesTableUrlSavedPagination;
+} {
+  const getUrlParam = useGetInitialUrlParamValue<RulesTableUrlSavedState>(URL_PARAM_KEY.rulesTable);
+  const {
+    services: { sessionStorage },
+  } = useKibana();
+
+  const urlState = getUrlParam();
+  const storageState = readStorageState(sessionStorage);
+
+  if (!urlState && !storageState) {
+    return {};
+  }
+
+  const [filter, sorting, pagination] = validateState(urlState, storageState);
+
+  return { filter, sorting, pagination };
+}
+
 function validateState(
   urlState: RulesTableUrlSavedState | null,
   storageState: RulesTableStorageSavedState | null
 ): [RulesTableSavedFilter, RulesTableSavedSorting, RulesTableUrlSavedPagination] {
   const [filterFromUrl] = validateNonExact(urlState, RulesTableSavedFilter);
   const [filterFromStorage] = validateNonExact(storageState, RulesTableSavedFilter);
-  const filter = { ...filterFromStorage, ...filterFromUrl };
+  // We have to expose filter, sorting and pagination objects by explicitly specifying each field
+  // since urlState and/or storageState may contain unnecessary fields (e.g. outdated or explicitly added by user)
+  // and validateNonExact doesn't truncate fields not included in the type RulesTableSavedFilter and etc.
+  const filter: RulesTableSavedFilter = {
+    searchTerm: filterFromUrl?.searchTerm ?? filterFromStorage?.searchTerm,
+    source: filterFromUrl?.source ?? filterFromStorage?.source,
+    tags: filterFromUrl?.tags ?? filterFromStorage?.tags,
+    enabled: filterFromUrl?.enabled ?? filterFromStorage?.enabled,
+  };
 
   const [sortingFromUrl] = validateNonExact(urlState, RulesTableSavedSorting);
   const [sortingFromStorage] = validateNonExact(storageState, RulesTableSavedSorting);
-  const sorting = { ...sortingFromStorage, ...sortingFromUrl };
+  const sorting = {
+    field: sortingFromUrl?.field ?? sortingFromStorage?.field,
+    order: sortingFromUrl?.order ?? sortingFromStorage?.order,
+  };
 
   const [paginationFromUrl] = validateNonExact(urlState, RulesTableUrlSavedPagination);
   const [paginationFromStorage] = validateNonExact(storageState, RulesTableStorageSavedPagination);
-  const pagination = { perPage: paginationFromStorage?.perPage, ...paginationFromUrl };
+  const pagination = {
+    page: paginationFromUrl?.page, // We don't persist page number in the session storage since it may be outdated when restored
+    perPage: paginationFromUrl?.perPage ?? paginationFromStorage?.perPage,
+  };
+
+  if (
+    pagination.perPage &&
+    (pagination.perPage < 0 || pagination.perPage > RULES_TABLE_MAX_PAGE_SIZE)
+  ) {
+    delete pagination.perPage;
+  }
 
   return [filter, sorting, pagination];
-}
-
-export function useInitializeRulesTableSavedState(): void {
-  const getUrlParam = useGetInitialUrlParamValue<RulesTableUrlSavedState>(URL_PARAM_KEY.rulesTable);
-  const { actions } = useRulesTableContext();
-  const {
-    services: { sessionStorage },
-  } = useKibana();
-
-  useEffect(() => {
-    const urlState = getUrlParam();
-    const storageState = readStorageState(sessionStorage);
-
-    if (!urlState && !storageState) {
-      return;
-    }
-
-    const [filter, sorting, pagination] = validateState(urlState, storageState);
-
-    actions.setFilterOptions({
-      filter: filter.searchTerm ?? DEFAULT_FILTER_OPTIONS.filter,
-      showElasticRules: filter.source === RuleSource.Prebuilt,
-      showCustomRules: filter.source === RuleSource.Custom,
-      tags: Array.isArray(filter.tags) ? filter.tags : DEFAULT_FILTER_OPTIONS.tags,
-      enabled: filter.enabled,
-    });
-
-    if (sorting.field || sorting.order) {
-      actions.setSortingOptions({
-        field: sorting.field ?? DEFAULT_SORTING_OPTIONS.field,
-        order: sorting.order ?? DEFAULT_SORTING_OPTIONS.order,
-      });
-    }
-
-    if (pagination.page) {
-      actions.setPage(pagination.page);
-    }
-
-    if (
-      pagination.perPage &&
-      pagination.perPage > 0 &&
-      pagination.perPage <= RULES_TABLE_MAX_PAGE_SIZE
-    ) {
-      actions.setPerPage(pagination.perPage);
-    }
-  }, [getUrlParam, actions, sessionStorage]);
 }
