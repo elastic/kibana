18021631adadbac1050e0c97e0a648aa351eaaeb
inge4pres <francesco.gualazzi@elastic.co>
2023-02-06 12:43:05 +0100

    update mappings to include symbolization data
    Signed-off-by: inge4pres <francesco.gualazzi@elastic.co>

--- x-pack/plugins/profiling/server/lib/setup/steps/get_create_indices_step.ts
+++ x-pack/plugins/profiling/server/lib/setup/steps/get_create_indices_step.ts
@@ -11,6 +11,7 @@ import { catchResourceAlreadyExistsException } from './catch_resource_already_ex
 
 const SQ_EXECUTABLES_INDEX = 'profiling-sq-executables';
 const LEAFFRAMES_INDEX = 'profiling-sq-leafframes';
+const SYMBOLS_INDEX = 'profiling-symbols';
 const ILM_LOCK_INDEX = '.profiling-ilm-lock';
 
 const getKeyValueIndices = () => {
@@ -36,7 +37,7 @@ export function getCreateIndicesStep({
   return {
     name: 'create_indices',
     hasCompleted: async () => {
-      const nonKvIndices = [SQ_EXECUTABLES_INDEX, LEAFFRAMES_INDEX, ILM_LOCK_INDEX];
+      const nonKvIndices = [SQ_EXECUTABLES_INDEX, LEAFFRAMES_INDEX, SYMBOLS_INDEX, ILM_LOCK_INDEX];
 
       const results = await Promise.all([
         esClient.cat
@@ -175,6 +176,119 @@ export function getCreateIndicesStep({
                 'Symbolization.retries': {
                   type: 'short',
                   index: true,
+                },
+              },
+            },
+          })
+          .catch(catchResourceAlreadyExistsException),
+        esClient.indices
+          .create({
+            index: SYMBOLS_INDEX,
+            settings: {
+              index: {
+                number_of_shards: '16',
+                refresh_interval: '10s',
+              },
+            },
+            mappings: {
+              _source: {
+                enabled: true,
+              } as MappingSourceField,
+              properties: {
+                'ecs.version': {
+                  type: 'keyword',
+                  index: true,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.function.name': {
+                  // name of the function
+                  type: 'keyword',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.file.name': {
+                  // file path
+                  type: 'keyword',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.call.file.name': {
+                  // (for inlined functions) file path where inline function was called
+                  type: 'keyword',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.call.line': {
+                  // (for inlined functions) line where inline function was called
+                  type: 'integer',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.function.line': {
+                  // function start line (only available from DWARF). Currently unused.
+                  type: 'integer',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.depth': {
+                  // inline depth
+                  type: 'integer',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                // pairs of (32bit PC offset, 32bit line number) followed by 64bit PC range base at the end.
+                // To find line number for a given PC: find lowest offset such as offsetBase+PC >= offset, then read corresponding line number.
+                // offsetBase could seemingly be available from exec_pc_range (it's the first value of the pair), but it's not the case.
+                // Ranges are stored as points, which cannot be retrieve when disabling _source.
+                // See https://www.elastic.co/guide/en/elasticsearch/reference/current/point.html .
+                'Symbol.linetable.base': {
+                  // Linetable: base for offsets (64bit PC range base)
+                  type: 'unsigned_long',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.linetable.length': {
+                  // Linetable: length of range (PC range is [base, base+length))
+                  type: 'unsigned_long',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.linetable.offsets': {
+                  // Linetable: concatenated offsets (each value is ULEB128encoded)
+                  type: 'keyword',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.linetable.lines': {
+                  // Linetable: concatenated lines (each value is ULEB128 encoded)
+                  type: 'keyword',
+                  index: false,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.file.id': {
+                  // fileID. used for deletion and Symbol.exec.pcrange collision handling on symbolization
+                  type: 'keyword',
+                  index: true,
+                  doc_values: false,
+                  store: false,
+                },
+                'Symbol.exec.pcrange': {
+                  // PC ranges [begin, end)
+                  type: 'ip_range',
+                  index: true,
+                  doc_values: false,
+                  store: false,
                 },
               },
             },
