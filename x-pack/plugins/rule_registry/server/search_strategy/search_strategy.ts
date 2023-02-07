7347e0dc524fce3150c2faa03f3b9d4b2e185f1f
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-02-07 09:42:21 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/rule_registry/server/search_strategy/search_strategy.ts
+++ x-pack/plugins/rule_registry/server/search_strategy/search_strategy.ts
@@ -170,7 +170,6 @@ export const ruleRegistrySearchStrategyProvider = (
             return hit;
           });
 
-
           try {
             response.inspect = { dsl: [JSON.stringify(params)] };
           } catch (error) {
