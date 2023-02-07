b32b2621b7ad0a0ee7efa0507f82f39651823bb6
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2023-02-07 15:45:25 +0100

    Extract callout to its own component

--- x-pack/plugins/synthetics/public/legacy_uptime/components/overview/integration_deprecation/index.tsx
+++ x-pack/plugins/synthetics/public/legacy_uptime/components/overview/integration_deprecation/index.tsx
@@ -6,13 +6,12 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import { FormattedMessage } from '@kbn/i18n-react';
-import { EuiCallOut, EuiLink, EuiButton, EuiFlexItem, EuiFlexGroup, EuiSpacer } from '@elastic/eui';
+import { EuiSpacer } from '@elastic/eui';
 import { useFetcher } from '@kbn/observability-plugin/public';
 import { useSelector } from 'react-redux';
 import { getHasIntegrationMonitors } from '../../../state/api/has_integration_monitors';
-import { getDocLinks } from '../../../../kibana_services';
 import { monitorListSelector } from '../../../state/selectors';
+import { IntegrationDeprecationCallout } from './integration_deprecation_callout';
 
 export const INTEGRATION_DEPRECATION_SESSION_STORAGE_KEY =
   'SYNTHETICS_INTEGRATION_DEPRECATION_HAS_BEEN_DISMISSED';
@@ -44,48 +43,9 @@ export const IntegrationDeprecation = () => {
 
   return shouldShowNotice ? (
     <>
-      <EuiCallOut
-        title={
-          <FormattedMessage
-            id="xpack.synthetics.integration.deprecation.title"
-            defaultMessage="Migrate your Elastic Synthetics integration monitors before Elastic 8.8"
-          />
-        }
-        color="warning"
-      >
-        <EuiFlexGroup alignItems="center" gutterSize="s">
-          <EuiFlexItem>
-            <span>
-              <FormattedMessage
-                id="xpack.synthetics.integration.deprecation.content"
-                defaultMessage="You have at least one monitor configured using the Elastic Synthetics integration. From Elastic 8.8, the integration will be deprecated and you will no longer be able to edit these monitors. To avoid this, migrate them to Project monitors or add them to the new Synthetics app directly available in Observability before the 8.8 update. Check our {link} for more details."
-                values={{
-                  link: (
-                    <EuiLink
-                      target="_blank"
-                      href={getDocLinks()?.links?.observability?.syntheticsProjectMonitors}
-                      external
-                    >
-                      <FormattedMessage
-                        id="xpack.synthetics.integration.deprecation.link"
-                        defaultMessage="Synthetics migration docs"
-                      />
-                    </EuiLink>
-                  ),
-                }}
-              />
-            </span>
-          </EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            <EuiButton onClick={handleDismissDeprecationNotice} color="warning">
-              <FormattedMessage
-                id="xpack.synthetics.integration.deprecation.dismiss"
-                defaultMessage="Dismiss"
-              />
-            </EuiButton>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiCallOut>
+      <IntegrationDeprecationCallout
+        handleDismissDeprecationNotice={handleDismissDeprecationNotice}
+      />
       <EuiSpacer size="s" />
     </>
   ) : null;
