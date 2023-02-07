301d1e1121563d0f02bcf5b43af98d9fb0976e0a
Alexandra Doak <alexandra.doak@elastic.co>
2023-02-07 13:25:56 -0500

    Fixing mistake

--- x-pack/plugins/alerting/server/rules_client/methods/get_alert_summary.ts
+++ x-pack/plugins/alerting/server/rules_client/methods/get_alert_summary.ts
@@ -57,7 +57,7 @@ export async function getAlertSummary(
           sort: [{ sort_field: '@timestamp', sort_order: 'desc' }],
           end: dateNow.toISOString(),
           // filter out execute-action event logs
-          filter: 'NOT event.action: execute-action && event.provider: alerting',
+          filter: 'NOT event.action: execute-action AND event.provider: alerting',
         },
         rule.legacyId !== null ? [rule.legacyId] : undefined
       ),
