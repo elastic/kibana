60c06d0399b02e1d29f3134156f92972f28e59e1
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-02-07 17:51:40 +0100

    cleanup

--- .buildkite/scripts/steps/functional/defend_workflows.sh
+++ .buildkite/scripts/steps/functional/defend_workflows.sh
@@ -11,27 +11,6 @@ export JOB=kibana-defend-workflows-cypress
 
 echo "--- Defend Workflows Cypress tests"
 
-set -x
-
-# sudo apt update
-
-echo "Installing snapd"
-
-sudo apt-get -y install snapd
-
-# echo "snap install snapd"
-
-# snap install snapd
-
-echo "Installing multipass"
-
-snap install multipass
-
-echo "run tests"
-
-set +x
-
 node scripts/functional_tests \
   --debug --bail \
   --config x-pack/test/defend_workflows_cypress/cli_config.ts
-
