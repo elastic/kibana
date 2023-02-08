ca63f9ad4ca432058254283303a0a8f21d8d3134
Thomas Watson <watson@elastic.co>
2023-02-08 08:25:18 +0100

    [@kbn/handlebars] Refactor types

--- packages/kbn-handlebars/src/types.ts
+++ packages/kbn-handlebars/src/types.ts
@@ -5,66 +5,49 @@
 
 import { kHelper, kAmbiguous, kSimple } from './symbols';
 
+// Unexported `CompileOptions` lifted from node_modules/handlebars/types/index.d.ts
+// While it could also be extracted using `NonNullable<Parameters<typeof Handlebars.compile>[1]>`, this isn't possible since we declare the handlebars module below
+interface HandlebarsCompileOptions {
+  data?: boolean;
+  compat?: boolean;
+  knownHelpers?: KnownHelpers;
+  knownHelpersOnly?: boolean;
+  noEscape?: boolean;
+  strict?: boolean;
+  assumeObjects?: boolean;
+  preventIndent?: boolean;
+  ignoreStandalone?: boolean;
+  explicitPartialContext?: boolean;
+}
+
 /**
  * A custom version of the Handlesbars module with an extra `compileAST` function and fixed typings.
  */
 declare module 'handlebars' {
+  /**
+   * Compiles the given Handlebars template without the use of `eval`.
+   *
+   * @returns A render function with the same API as the return value from the regular Handlebars `compile` function.
+   */
   export function compileAST(
     input: string | hbs.AST.Program,
-    options?: ExtendedCompileOptions
-  ): (context?: any, options?: ExtendedRuntimeOptions) => string;
+    options?: CompileOptions
+  ): TemplateDelegateFixed;
 
   // --------------------------------------------------------
-  // Override/Extend inherited types below that are incorrect
+  // Override/Extend inherited funcions and interfaces below that are incorrect.
+  //
+  // Any exported `const` or `type` types can't be overwritten, so we'll just
+  // have to live with those and cast them to the correct types in our code.
+  // Some of these fixed types, we'll instead export outside the scope of this
+  // 'handlebars' module so consumers of @kbn/handlebars at least have a way to
+  // access the correct types.
   // --------------------------------------------------------
 
-  export interface TemplateDelegate<T = any> {
-    (context?: T, options?: RuntimeOptions): string; // Override to ensure `context` is optional
-    blockParams?: number; // TODO: Can this really be optional?
-    partials?: any; // TODO: Narrow type to something better than any?
-  }
+  export interface HelperDelegate extends HelperDelegateFixed {} // eslint-disable-line @typescript-eslint/no-empty-interface
+  export interface TemplateDelegate<T = any> extends TemplateDelegateFixed<T> {} // eslint-disable-line @typescript-eslint/no-empty-interface
 
-  export interface HelperOptions {
-    name: string;
-    loc: { start: hbs.AST.SourceLocation['start']; end: hbs.AST.SourceLocation['end'] };
-    lookupProperty: LookupProperty;
-  }
-
-  export interface HelperDelegate {
-    // eslint-disable-next-line @typescript-eslint/prefer-function-type
-    (...params: any[]): any;
-  }
-
-  export function registerPartial(spec: { [name: string]: Handlebars.Template }): void; // Ensure `spec` object values can be strings
-}
-
-export type NodeType = typeof kHelper | typeof kAmbiguous | typeof kSimple;
-
-type LookupProperty = <T = any>(parent: { [name: string]: any }, propertyName: string) => T;
-
-export type ProcessableStatementNode =
-  | hbs.AST.MustacheStatement
-  | hbs.AST.PartialStatement
-  | hbs.AST.SubExpression;
-export type ProcessableBlockStatementNode = hbs.AST.BlockStatement | hbs.AST.PartialBlockStatement;
-export type ProcessableNode = ProcessableStatementNode | ProcessableBlockStatementNode;
-export type ProcessableNodeWithPathParts = ProcessableNode & { path: hbs.AST.PathExpression };
-export type ProcessableNodeWithPathPartsOrLiteral = ProcessableNode & {
-  path: hbs.AST.PathExpression | hbs.AST.Literal;
-};
-
-export interface Helper {
-  fn?: Handlebars.HelperDelegate;
-  context: any[];
-  params: any[];
-  options: AmbiguousHelperOptions;
-}
-
-export type NonBlockHelperOptions = Omit<Handlebars.HelperOptions, 'fn' | 'inverse'>;
-export type AmbiguousHelperOptions = Handlebars.HelperOptions | NonBlockHelperOptions;
-
-export interface DecoratorOptions extends Omit<Handlebars.HelperOptions, 'lookupProperties'> {
-  args?: any[];
+  export function registerPartial(spec: Record<string, TemplateFixed>): void; // Ensure `spec` object values can be strings
 }
 
 /**
@@ -73,8 +56,8 @@ export interface DecoratorOptions extends Omit<Handlebars.HelperOptions, 'lookup
  * This is a subset of all the compile options supported by the upstream
  * Handlebars module.
  */
-export type ExtendedCompileOptions = Pick<
-  CompileOptions,
+export type CompileOptions = Pick<
+  HandlebarsCompileOptions,
   | 'data'
   | 'knownHelpers'
   | 'knownHelpersOnly'
@@ -91,46 +74,114 @@ export type ExtendedCompileOptions = Pick<
  * This is a subset of all the runtime options supported by the upstream
  * Handlebars module.
  */
-export type ExtendedRuntimeOptions = Pick<
-  RuntimeOptions,
-  'data' | 'helpers' | 'partials' | 'decorators' | 'blockParams'
->;
+export interface RuntimeOptions extends Pick<Handlebars.RuntimeOptions, 'data' | 'blockParams'> {
+  // The upstream `helpers` property is too loose and allows all functions.
+  helpers?: HelpersHash;
+  // The upstream `partials` property is incorrectly typed and doesn't allow
+  // partials to be strings.
+  partials?: PartialsHash;
+  // The upstream `decorators` property is too loose and allows all functions.
+  decorators?: DecoratorsHash;
+}
 
-/**
- * According to the [decorator docs]{@link https://github.com/handlebars-lang/handlebars.js/blob/4.x/docs/decorators-api.md},
- * a decorator will be called with a different set of arugments than what's actually happening in the upstream code.
- * So here I assume that the docs are wrong and that the upstream code is correct. In reality, `context` is the last 4
- * documented arguments rolled into one object.
- */
-export type DecoratorFunction = (
-  prog: Handlebars.TemplateDelegate,
+export interface HelperOptions extends Omit<Handlebars.HelperOptions, 'fn' | 'inverse'> {
+  name: string;
+  fn: TemplateDelegateFixed;
+  inverse: TemplateDelegateFixed;
+  loc: { start: hbs.AST.SourceLocation['start']; end: hbs.AST.SourceLocation['end'] };
+  lookupProperty: LookupProperty;
+}
+
+// Use the post-fix `Fixed` to allow us to acces it inside the 'handlebars' module declared above
+interface HelperDelegateFixed {
+  // eslint-disable-next-line @typescript-eslint/prefer-function-type
+  (...params: any[]): any;
+}
+export type { HelperDelegateFixed as HelperDelegate };
+
+// Use the post-fix `Fixed` to allow us to acces it inside the 'handlebars' module declared above
+interface TemplateDelegateFixed<T = any> {
+  (context?: T, options?: RuntimeOptions): string; // Override to ensure `context` is optional
+  blockParams?: number; // TODO: Can this really be optional?
+  partials?: PartialsHash;
+}
+export type { TemplateDelegateFixed as TemplateDelegate };
+
+// According to the decorator docs
+// (https://github.com/handlebars-lang/handlebars.js/blob/4.x/docs/decorators-api.md)
+// a decorator will be called with a different set of arugments than what's
+// actually happening in the upstream code. So here I assume that the docs are
+// wrong and that the upstream code is correct. In reality, `context` is the
+// last 4 documented arguments rolled into one object.
+export type DecoratorDelegate = (
+  prog: TemplateDelegateFixed,
   props: Record<string, any>,
   container: Container,
   options: any
 ) => any;
 
-export interface HelpersHash {
-  [name: string]: Handlebars.HelperDelegate;
+// -----------------------------------------------------------------------------
+// INTERNAL TYPES
+// -----------------------------------------------------------------------------
+
+export type NodeType = typeof kHelper | typeof kAmbiguous | typeof kSimple;
+
+type LookupProperty = <T = any>(parent: Record<string, any>, propertyName: string) => T;
+
+export type NonBlockHelperOptions = Omit<HelperOptions, 'fn' | 'inverse'>;
+export type AmbiguousHelperOptions = HelperOptions | NonBlockHelperOptions;
+
+export type ProcessableStatementNode =
+  | hbs.AST.MustacheStatement
+  | hbs.AST.PartialStatement
+  | hbs.AST.SubExpression;
+export type ProcessableBlockStatementNode = hbs.AST.BlockStatement | hbs.AST.PartialBlockStatement;
+export type ProcessableNode = ProcessableStatementNode | ProcessableBlockStatementNode;
+export type ProcessableNodeWithPathParts = ProcessableNode & { path: hbs.AST.PathExpression };
+export type ProcessableNodeWithPathPartsOrLiteral = ProcessableNode & {
+  path: hbs.AST.PathExpression | hbs.AST.Literal;
+};
+
+export type HelpersHash = Record<string, HelperDelegateFixed>;
+export type PartialsHash = Record<string, TemplateFixed>;
+export type DecoratorsHash = Record<string, DecoratorDelegate>;
+
+// Use the post-fix `Fixed` to allow us to acces it inside the 'handlebars' module declared above
+type TemplateFixed = TemplateDelegateFixed | string;
+export type { TemplateFixed as Template };
+
+export interface DecoratorOptions extends Omit<HelperOptions, 'lookupProperties'> {
+  args?: any[];
 }
 
-export interface PartialsHash {
-  [name: string]: HandlebarsTemplateDelegate;
+export interface VisitorHelper {
+  fn?: HelperDelegateFixed;
+  context: any[];
+  params: any[];
+  options: AmbiguousHelperOptions;
 }
 
-export interface DecoratorsHash {
-  [name: string]: DecoratorFunction;
+export interface ResolvePartialOptions
+  extends Omit<Handlebars.ResolvePartialOptions, 'helpers' | 'partials' | 'decorators'> {
+  // The upstream `helpers` property is too loose and allows all functions.
+  helpers?: HelpersHash;
+  // The upstream `partials` property is incorrectly typed and doesn't allow
+  // partials to be strings.
+  partials?: PartialsHash;
+  // The upstream `decorators` property is too loose and allows all functions.
+  decorators?: DecoratorsHash;
 }
 
 export interface Container {
   helpers: HelpersHash;
   partials: PartialsHash;
   decorators: DecoratorsHash;
-  strict: (obj: { [name: string]: any }, name: string, loc: hbs.AST.SourceLocation) => any;
+  strict: (obj: Record<string, any>, name: string, loc: hbs.AST.SourceLocation) => any;
   lookupProperty: LookupProperty;
   lambda: (current: any, context: any) => any;
   data: (value: any, depth: number) => any;
   hooks: {
-    helperMissing?: Handlebars.HelperDelegate;
-    blockHelperMissing?: Handlebars.HelperDelegate;
+    helperMissing?: HelperDelegateFixed;
+    blockHelperMissing?: HelperDelegateFixed;
   };
 }
