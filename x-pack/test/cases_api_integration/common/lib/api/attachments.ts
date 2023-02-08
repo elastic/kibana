c1149aa2f95fd7c5493a08dc2796615f5b455767
Jonathan Buttner <jonathan.buttner@elastic.co>
2023-02-08 09:00:29 -0500

    Moving attachment and comments

--- x-pack/test/cases_api_integration/common/lib/api/attachments.ts
+++ x-pack/test/cases_api_integration/common/lib/api/attachments.ts
@@ -8,13 +8,20 @@
 import type SuperTest from 'supertest';
 import { CASES_INTERNAL_URL, CASES_URL } from '@kbn/cases-plugin/common/constants';
 import {
+  AllCommentsResponse,
+  BulkCreateCommentRequest,
   BulkGetAttachmentsResponse,
   CaseResponse,
+  CommentPatchRequest,
   CommentRequest,
+  CommentResponse,
+  CommentType,
 } from '@kbn/cases-plugin/common/api';
 import { User } from '../authentication/types';
 import { superUser } from '../authentication/users';
 import { getSpaceUrlPrefix, setupAuth } from './helpers';
+import { createCase } from './case';
+import { postCaseReq } from '../mock';
 
 export const bulkGetAttachments = async ({
   supertest,
@@ -67,4 +74,187 @@ export const createComment = async ({
     .expect(expectedHttpCode);
 
   return theCase;
+};
+
+export const bulkCreateAttachments = async ({
+  supertest,
+  caseId,
+  params,
+  auth = { user: superUser, space: null },
+  expectedHttpCode = 200,
+}: {
+  supertest: SuperTest.SuperTest<SuperTest.Test>;
+  caseId: string;
+  params: BulkCreateCommentRequest;
+  auth?: { user: User; space: string | null };
+  expectedHttpCode?: number;
+}): Promise<CaseResponse> => {
+  const { body: theCase } = await supertest
+    .post(
+      `${getSpaceUrlPrefix(auth.space)}${CASES_INTERNAL_URL}/${caseId}/attachments/_bulk_create`
+    )
+    .auth(auth.user.username, auth.user.password)
+    .set('kbn-xsrf', 'true')
+    .send(params)
+    .expect(expectedHttpCode);
+
+  return theCase;
+};
+
+export const createCaseAndBulkCreateAttachments = async ({
+  supertest,
+  numberOfAttachments = 3,
+  auth = { user: superUser, space: null },
+  expectedHttpCode = 200,
+}: {
+  supertest: SuperTest.SuperTest<SuperTest.Test>;
+  numberOfAttachments?: number;
+  auth?: { user: User; space: string | null };
+  expectedHttpCode?: number;
+}): Promise<{ theCase: CaseResponse; attachments: BulkCreateCommentRequest }> => {
+  const postedCase = await createCase(supertest, postCaseReq);
+  const attachments = getAttachments(numberOfAttachments);
+  const patchedCase = await bulkCreateAttachments({
+    supertest,
+    caseId: postedCase.id,
+    params: attachments,
+  });
+
+  return { theCase: patchedCase, attachments };
+};
+
+export const getAttachments = (numberOfAttachments: number): BulkCreateCommentRequest => {
+  return [...Array(numberOfAttachments)].map((index) => {
+    if (index % 0) {
+      return {
+        type: CommentType.user,
+        comment: `Test ${index + 1}`,
+        owner: 'securitySolutionFixture',
+      };
+    }
+
+    return {
+      type: CommentType.alert,
+      alertId: `test-id-${index + 1}`,
+      index: `test-index-${index + 1}`,
+      rule: {
+        id: `rule-test-id-${index + 1}`,
+        name: `Test ${index + 1}`,
+      },
+      owner: 'securitySolutionFixture',
+    };
+  });
+};
+
+export const deleteComment = async ({
+  supertest,
+  caseId,
+  commentId,
+  expectedHttpCode = 204,
+  auth = { user: superUser, space: null },
+}: {
+  supertest: SuperTest.SuperTest<SuperTest.Test>;
+  caseId: string;
+  commentId: string;
+  expectedHttpCode?: number;
+  auth?: { user: User; space: string | null };
+}): Promise<{} | Error> => {
+  const { body: comment } = await supertest
+    .delete(`${getSpaceUrlPrefix(auth.space)}${CASES_URL}/${caseId}/comments/${commentId}`)
+    .set('kbn-xsrf', 'true')
+    .auth(auth.user.username, auth.user.password)
+    .expect(expectedHttpCode)
+    .send();
+
+  return comment;
+};
+
+export const deleteAllComments = async ({
+  supertest,
+  caseId,
+  expectedHttpCode = 204,
+  auth = { user: superUser, space: null },
+}: {
+  supertest: SuperTest.SuperTest<SuperTest.Test>;
+  caseId: string;
+  expectedHttpCode?: number;
+  auth?: { user: User; space: string | null };
+}): Promise<{} | Error> => {
+  const { body: comment } = await supertest
+    .delete(`${getSpaceUrlPrefix(auth.space)}${CASES_URL}/${caseId}/comments`)
+    .set('kbn-xsrf', 'true')
+    .auth(auth.user.username, auth.user.password)
+    .expect(expectedHttpCode)
+    .send();
+
+  return comment;
+};
+
+export const getAllComments = async ({
+  supertest,
+  caseId,
+  expectedHttpCode = 200,
+  auth = { user: superUser, space: null },
+}: {
+  supertest: SuperTest.SuperTest<SuperTest.Test>;
+  caseId: string;
+  auth?: { user: User; space: string | null };
+  expectedHttpCode?: number;
+}): Promise<AllCommentsResponse> => {
+  const { body: comments } = await supertest
+    .get(`${getSpaceUrlPrefix(auth.space)}${CASES_URL}/${caseId}/comments`)
+    .auth(auth.user.username, auth.user.password)
+    .expect(expectedHttpCode);
+
+  return comments;
+};
+
+export const getComment = async ({
+  supertest,
+  caseId,
+  commentId,
+  expectedHttpCode = 200,
+  auth = { user: superUser, space: null },
+}: {
+  supertest: SuperTest.SuperTest<SuperTest.Test>;
+  caseId: string;
+  commentId: string;
+  expectedHttpCode?: number;
+  auth?: { user: User; space: string | null };
+}): Promise<CommentResponse> => {
+  const { body: comment } = await supertest
+    .get(`${getSpaceUrlPrefix(auth.space)}${CASES_URL}/${caseId}/comments/${commentId}`)
+    .auth(auth.user.username, auth.user.password)
+    .expect(expectedHttpCode);
+
+  return comment;
+};
+
+export const updateComment = async ({
+  supertest,
+  caseId,
+  req,
+  expectedHttpCode = 200,
+  auth = { user: superUser, space: null },
+  headers = {},
+}: {
+  supertest: SuperTest.SuperTest<SuperTest.Test>;
+  caseId: string;
+  req: CommentPatchRequest;
+  expectedHttpCode?: number;
+  auth?: { user: User; space: string | null } | null;
+  headers?: Record<string, unknown>;
+}): Promise<CaseResponse> => {
+  const apiCall = supertest.patch(
+    `${getSpaceUrlPrefix(auth?.space)}${CASES_URL}/${caseId}/comments`
+  );
+
+  setupAuth({ apiCall, headers, auth });
+  const { body: res } = await apiCall
+    .set('kbn-xsrf', 'true')
+    .set(headers)
+    .send(req)
+    .expect(expectedHttpCode);
+
+  return res;
 };
