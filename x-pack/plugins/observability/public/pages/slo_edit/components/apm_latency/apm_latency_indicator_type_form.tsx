7c01e101b143d919b850200110ac2fc5b01e424a
Kevin Delemme <kevin.delemme@elastic.co>
2023-02-08 16:21:13 -0500

    Create apm service name selector

--- x-pack/plugins/observability/public/pages/slo_edit/components/apm_latency/apm_latency_indicator_type_form.tsx
+++ x-pack/plugins/observability/public/pages/slo_edit/components/apm_latency/apm_latency_indicator_type_form.tsx
@@ -5,16 +5,103 @@
  * 2.0.
  */
 
-import React from 'react';
-import { EuiFlexGroup } from '@elastic/eui';
-import { Control, UseFormWatch } from 'react-hook-form';
+import React, { useEffect, useState } from 'react';
+import { EuiComboBox, EuiComboBoxOptionOption, EuiFlexGroup, EuiFormRow } from '@elastic/eui';
+import { Control, Controller, UseFormWatch } from 'react-hook-form';
 import type { CreateSLOInput } from '@kbn/slo-schema';
+import { i18n } from '@kbn/i18n';
+import {
+  Suggestion,
+  useFetchApmSuggestions,
+} from '../../../../hooks/slo/use_fetch_apm_suggestions';
 
 export interface Props {
   control: Control<CreateSLOInput>;
   watch: UseFormWatch<CreateSLOInput>;
 }
 
+interface Option {
+  value: string;
+  label: string;
+}
+
 export function ApmLatencyIndicatorTypeForm({ control, watch }: Props) {
-  return <EuiFlexGroup direction="column" gutterSize="l" />;
+  const [search, setSearch] = useState<string>('');
+  const { data: suggestions, loading } = useFetchApmSuggestions({
+    fieldName: 'service.name',
+    search,
+  });
+  const [options, setOptions] = useState<Option[]>([]);
+
+  useEffect(() => {
+    setOptions(createOptions(suggestions));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [suggestions.length]);
+
+  return (
+    <EuiFlexGroup direction="column" gutterSize="l">
+      <EuiFormRow
+        label={i18n.translate('xpack.observability.slos.sloEdit.apmLatency.serviceName', {
+          defaultMessage: 'Service name',
+        })}
+      >
+        <Controller
+          name="indicator.params.service"
+          control={control}
+          rules={{ required: true }}
+          render={({ field, fieldState }) => (
+            <EuiComboBox
+              {...field}
+              aria-label={i18n.translate(
+                'xpack.observability.slos.sloEdit.apmLatency.serviceName.placeholder',
+                {
+                  defaultMessage: 'Select the APM service',
+                }
+              )}
+              async
+              data-test-subj="apmLatencyServiceSelector"
+              isClearable={true}
+              isInvalid={!!fieldState.error}
+              isLoading={loading}
+              onChange={(selected: EuiComboBoxOptionOption[]) => {
+                if (selected.length) {
+                  return field.onChange(selected[0].value);
+                }
+
+                field.onChange('');
+              }}
+              onSearchChange={(value: string) => {
+                setSearch(value);
+              }}
+              options={options}
+              placeholder={i18n.translate(
+                'xpack.observability.slos.sloEdit.apmLatency.serviceName.placeholder',
+                {
+                  defaultMessage: 'Select the APM service',
+                }
+              )}
+              selectedOptions={
+                !!field.value
+                  ? [
+                      {
+                        value: field.value,
+                        label: field.value,
+                        'data-test-subj': 'apmLatencyServiceSelectedValue',
+                      },
+                    ]
+                  : []
+              }
+              singleSelection
+            />
+          )}
+        />
+      </EuiFormRow>
+    </EuiFlexGroup>
+  );
+}
+
+function createOptions(suggestions: Suggestion[]): Option[] {
+  return suggestions
+    .map((suggestion) => ({ label: suggestion, value: suggestion }))
+    .sort((a, b) => String(a.label).localeCompare(b.label));
 }
