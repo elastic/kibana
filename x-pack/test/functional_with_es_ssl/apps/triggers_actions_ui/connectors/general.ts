c8c759119c41b1c72eaac7dd708d4f1a0efa7c65
Julia Guskova <iuliia.guskova@elastic.co>
2023-02-08 14:15:32 +0100

    fix create slack connector functional test

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/connectors/general.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/connectors/general.ts
@@ -54,8 +54,7 @@ export default ({ getPageObjects, getPageObject, getService }: FtrProviderContex
       await testSubjects.click('.slack-card');
 
       await testSubjects.setValue('nameInput', connectorName);
-
-      await testSubjects.setValue('slackWebhookUrlInput', 'https://test.com');
+      await testSubjects.setValue('secrets.token-input', 'some token');
 
       await find.clickByCssSelector(
         '[data-test-subj="create-connector-flyout-save-btn"]:not(disabled)'
@@ -96,7 +95,7 @@ export default ({ getPageObjects, getPageObject, getService }: FtrProviderContex
 
       await testSubjects.setValue('nameInput', updatedConnectorName);
 
-      await testSubjects.setValue('slackWebhookUrlInput', 'https://test.com');
+      await testSubjects.setValue('secrets.token-input', 'https://test.com');
 
       await find.clickByCssSelector(
         '[data-test-subj="edit-connector-flyout-save-btn"]:not(disabled)'
