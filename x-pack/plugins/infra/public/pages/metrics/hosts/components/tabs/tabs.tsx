cf88bb84c08f2d80c104f9fc430ba4056e2ccebf
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-02-09 16:47:31 +0100

    fix(infra): switch back to local tabId to prevent tabs rerender

--- x-pack/plugins/infra/public/pages/metrics/hosts/components/tabs/tabs.tsx
+++ x-pack/plugins/infra/public/pages/metrics/hosts/components/tabs/tabs.tsx
@@ -5,14 +5,14 @@
  * 2.0.
  */
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { EuiTabs, EuiTab, EuiSpacer } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { MetricsGrid } from './metrics/metrics_grid';
 import { AlertsTabContent } from './alerts';
 
 import { AlertsTabBadge } from './alerts_tab_badge';
-import { tabIds, useTabId } from '../../hooks/use_tab_id';
+import { tabIds } from '../../constants';
 
 const tabs = [
   {
@@ -32,12 +32,14 @@ const tabs = [
   },
 ];
 
-export const Tabs = () => {
-  const [selectedTabId, setSelectedTabId] = useTabId();
+const initialRenderedTabsSet = new Set([tabs[0].id]);
 
+export const Tabs = () => {
   // This map allow to keep track of which tabs content have been rendered the first time.
   // We need it in order to load a tab content only if it gets clicked, and then keep it in the DOM for performance improvement.
-  const renderedTabsSet = useRef(new Set([selectedTabId]));
+  const renderedTabsSet = useRef(initialRenderedTabsSet);
+
+  const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
 
   const tabEntries = tabs.map((tab, index) => (
     <EuiTab
