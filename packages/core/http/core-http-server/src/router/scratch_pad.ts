e1c77e9d1059d472e37e67ed75c3ec85c5b4e0d1
Jean-Louis Leysens <jloleysens@gmail.com>
2023-02-14 15:04:52 +0100

    added a bunch of comments about the strengths and weaknesses of each approach and added the toolkit/lib idea

--- packages/core/http/core-http-server/src/router/scratch_pad.ts
+++ packages/core/http/core-http-server/src/router/scratch_pad.ts
@@ -7,10 +7,12 @@
  */
 
 import { schema } from '@kbn/config-schema';
+import { Logger } from '@kbn/logging';
+import { pipe } from 'fp-ts/lib/function';
 import { RequestHandler } from './request_handler';
 import { RequestHandlerContextBase } from './request_handler_context';
 import { RouteConfig, RouteMethod } from './route';
-import { IRouter } from './router';
+import { IRouter, RouteRegistrar } from './router';
 import { RouteValidatorFullConfig } from './route_validator';
 import { HttpServiceSetup } from '../..';
 
@@ -27,6 +29,7 @@ import { HttpServiceSetup } from '../..';
 // Current API
 
 const router: IRouter = {} as any;
+const logger: Logger = {} as any;
 
 // 1
 router.get(
@@ -124,6 +127,13 @@ type Version = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10';
   );
 }
 /**
+ *  * STRENGTHS:
+ *   1. Preserves full power of route declaration, even per version of a route
+ *   2. Fairly simple, does not require any refactoring to adopt
+ * WEAKNESSES:
+ *   1. More complicated for router implementation because we must
+ *      accept duplicate paths + version to uniquely identify a route.
+ *   2. Need to redeclare handlers for every version (increased effort to make new versions)
  * ===================== End first design =====================
  */
 
@@ -199,6 +209,17 @@ type Version = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10';
   );
 }
 /**
+ * STRENGTHS:
+ *   1. API clearly ties a route handler to a version
+ *   2. Declare path and options once
+ * WEAKNESSES:
+ *   1. Repeated declare the options and the path... Introducing a new version
+ *      will result in a lot of boilerplate.
+ *   2. Handlers know versions by passed into a given router so you also need
+ *      to redeclare handlers
+ *   3. Perhaps an overfit for the problem at hand. The router implementation
+ *      will fully internalize the notion of versions. How do we handle routes
+ *      that are not versioned?
  * ===================== End second design =====================
  */
 
@@ -260,5 +281,96 @@ type Version = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10';
   );
 }
 /**
+ *
+ * STRENGTHS:
+ *   1. Flexible, simple and easy to adopt
+ *   2. Not very opinionated about how you structure your code
+ *   3. The router API remains largely the same
+ * WEAKNESSES:
+ *   1. Repeatedly declare the options and the path... Introducing a new version
+ *      will result in a lot of boilerplate.
+ *   2. Building from (1) also need to redeclare handlers...
+ *   3. Consumer code will need to pass around an increasing number of router
+ *      version instances
  * ===================== End third design =====================
+ */
+
+/**
+ * ===================== Fourth design =====================
+ * Take the lib/toolkit approach. Instead of baking the idea of versioning into
+ * the router we can expose a toolkit for building a versioned API that uses our
+ * HTTP router. The toolkit just wraps the router ensuring that the spec for
+ * applying/extracting version information is followed.
+ */
+
+{
+  type VersionedRouteOpts = Omit<RouteConfig<unknown, unknown, unknown, RouteMethod>, 'validate'>;
+  type Registrar = RouteRegistrar<RouteMethod, RequestHandlerContextBase>;
+
+  type CreateVersionedRoute = (registrar: Registrar, opts: VersionedRouteOpts) => VersionedRoute;
+  interface VersionedRoute {
+    add<P, Q, B>(
+      opts: { validation: RouteValidatorFullConfig<P, Q, B> },
+      handler: RequestHandler<P, Q, B>
+    ): VersionedRoute;
+  }
+
+  interface VersionedAPIToolkit {
+    createRoute: CreateVersionedRoute;
+  }
+
+  const vtk: VersionedAPIToolkit = {} as any;
+
+  const vRouter = vtk
+    .createRoute(router.post, { path: '/api/my-plugin/my-route', options: {} })
+    .add(
+      {
+        validation: { body: schema.object({ n: schema.number({ min: 0, max: 1 }) }) },
+      },
+      async (ctx, req, res) => {
+        logger.info(String(req.body.n));
+        return res.ok();
+      }
+    )
+    .add(
+      {
+        validation: { body: schema.object({ b: schema.number({ min: 2, max: 3 }) }) },
+      },
+      async (ctx, req, res) => {
+        logger.info(String(req.body.b));
+        return res.ok();
+      }
+    );
+
+  // OR point-free style
+  const add =
+    <P, Q, B>(
+      opts: { validation: RouteValidatorFullConfig<P, Q, B> },
+      handler: RequestHandler<P, Q, B>
+    ) =>
+    (versionedRouter: VersionedRoute) =>
+      versionedRouter;
+
+  pipe(
+    vtk.createRoute(router.post, { path: '/api/my-plugin/my-route', options: {} }),
+
+    add(
+      { validation: { body: schema.object({ n: schema.number({ min: 0, max: 1 }) }) } },
+      async (ctx, req, res) => {
+        logger.info(String(req.body.n));
+        return res.ok();
+      }
+    ),
+
+    add(
+      { validation: { body: schema.object({ b: schema.number({ min: 2, max: 3 }) }) } },
+      async (ctx, req, res) => {
+        logger.info(String(req.body.b));
+        return res.ok();
+      }
+    )
+  );
+}
+/**
+ * ===================== End fourth design =====================
  */
