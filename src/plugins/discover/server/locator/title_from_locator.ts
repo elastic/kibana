d6c519b84e86fc57ca03b1d396b4af3bc7a72561
Timothy Sullivan <tsullivan@elastic.co>
2023-02-14 11:39:31 -0700

    update title_from_locator and add test

--- src/plugins/discover/server/locator/title_from_locator.ts
+++ src/plugins/discover/server/locator/title_from_locator.ts
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 
+import { i18n } from '@kbn/i18n';
 import { SavedObject } from '@kbn/core/server';
 import { LocatorServicesDeps } from '.';
 import { DiscoverAppLocatorParams } from '../../common';
@@ -15,18 +16,38 @@ import { DiscoverAppLocatorParams } from '../../common';
  */
 export const titleFromLocatorFactory = (services: LocatorServicesDeps) => {
   /**
-   * Allows consumers to derive a title of a search in Disocver from DiscoverAppLocatorParams
+   * Allows consumers to derive a title of a search in Disocver from DiscoverAppLocatorParams.
+   * For now, this assumes the DiscoverAppLocatorParams contain a reference to a saved search. In the future,
+   * the params may only contain a reference to a DataView
    *
    * @public
    */
   const titleFromLocator = async (params: DiscoverAppLocatorParams): Promise<string> => {
+    const { savedSearchId, title: paramsTitle } = params as {
+      savedSearchId?: string;
+      title?: string;
+    };
+
+    if (paramsTitle) {
+      return paramsTitle;
+    }
+
+    if (!savedSearchId) {
+      throw new Error(`DiscoverAppLocatorParams must contain a saved search reference`);
+    }
+
     const { savedObjects } = services;
     const searchObject: SavedObject<{ title?: string }> = await savedObjects.get(
       'search',
-      params.savedSearchId! // TODO: allow default to DavaView name
+      savedSearchId // assumes params contains saved search reference
     );
 
-    return (params.title ?? searchObject.attributes.title ?? 'Unknown search') as string;
+    return (
+      searchObject.attributes.title ??
+      i18n.translate('discover.serverLocatorExtension.titleFromLocatorUnknown', {
+        defaultMessage: 'Unknown search',
+      })
+    );
   };
 
   return titleFromLocator;
