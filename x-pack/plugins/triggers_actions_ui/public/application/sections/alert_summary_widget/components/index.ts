50b83014a3e2947e495c8b0f86aa57078573bd0c
Maryam Saeidi <maryam.saeidi@elastic.co>
2023-02-14 13:35:40 +0100

    [AO] Fix overview alerts loading state (#150868)
    Fixes #140387
    
    ## Summary
    
    Fix alerts table lazy loading state
    
    Before
    
    ![image](https://user-images.githubusercontent.com/12370520/218094792-24d00334-5414-4737-8f64-d619cbcc9f12.png)
    
    After
    
    ![image](https://user-images.githubusercontent.com/12370520/218094828-6b3559ce-1f7a-45a7-ae0d-96f5dad141a1.png)

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alert_summary_widget/components/index.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alert_summary_widget/components/index.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
-export { AlertsSummaryWidgetCompact } from './alert_summary_widget_compact';
+export { AlertSummaryWidgetCompact } from './alert_summary_widget_compact';
 export { AlertSummaryWidgetError } from './alert_summary_widget_error';
-export { AlertsSummaryWidgetFullSize } from './alert_summary_widget_full_size';
+export { AlertSummaryWidgetFullSize } from './alert_summary_widget_full_size';
+export { AlertSummaryWidgetLoader } from './alert_summary_widget_loader';
