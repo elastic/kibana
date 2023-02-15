7752a13dfaf301cd21d738a8d58cd2a80650d4a3
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2023-02-15 18:18:00 +0100

    remove logged state from service modal

--- packages/kbn-apm-synthtrace-ui/src/components/modal/span-form.tsx
+++ packages/kbn-apm-synthtrace-ui/src/components/modal/span-form.tsx
@@ -77,7 +77,6 @@ const SpanForm = ({
     <EuiModal onClose={onClose} initialFocus="[name=name]">
       <EuiModalHeader>
         <EuiModalHeaderTitle>Create Span</EuiModalHeaderTitle>
-        <p>{JSON.stringify(formState, null, 2)}</p>
       </EuiModalHeader>
       <EuiModalBody>
         <EuiForm id={formId} component="form">
