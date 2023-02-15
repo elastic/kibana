e5379a217284bb482da71642742eb7d1f185b402
Ying <ying.mao@elastic.co>
2023-02-15 16:15:38 -0500

    Removing index and doc values fields

--- packages/kbn-alerts-as-data-utils/src/field_maps/ecs_field_map.ts
+++ packages/kbn-alerts-as-data-utils/src/field_maps/ecs_field_map.ts
@@ -19,8 +19,6 @@ export const ecsFieldMap: FieldMap = Object.keys(EcsFlat).reduce((acc, currKey) 
       required: !!value.required,
       ...(value.scaling_factor ? { scaling_factor: value.scaling_factor } : {}),
       ...(value.ignore_above ? { ignore_above: value.ignore_above } : {}),
-      ...(value.doc_values != null ? { doc_values: value.doc_values } : {}),
-      ...(value.index != null ? { index: value.index } : {}),
     },
   };
 }, {});
