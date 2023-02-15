893da767a4955ef4c31f126ad91cd41f459da91c
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2023-02-15 18:10:16 +0100

    push logic to reuse service selector

--- packages/kbn-apm-synthtrace-ui/src/components/new_scenario_form/index.tsx
+++ packages/kbn-apm-synthtrace-ui/src/components/new_scenario_form/index.tsx
@@ -1,10 +1,27 @@
-import { EuiFieldText, EuiForm, EuiFormRow, EuiPanel, EuiSwitch } from '@elastic/eui';
+import {
+  EuiComboBoxOptionOption,
+  EuiFieldText,
+  EuiForm,
+  EuiFormRow,
+  EuiPanel,
+  EuiSwitch,
+} from '@elastic/eui';
 import React from 'react';
 import { useScenarioContext } from '../../context/use_scenario_context';
 import { ServiceSelector } from '../service_selector';
+import { ServiceNames } from '../../common/constants';
+import { ElasticAgentName } from '@kbn/apm-synthtrace-ui/src/typings';
 
 export function NewScenarioForm() {
   const { state, dispatch } = useScenarioContext();
+
+  const serviceOptions: Array<EuiComboBoxOptionOption<ElasticAgentName>> = ServiceNames.map(
+    (agentName) => ({
+      key: agentName,
+      label: agentName,
+      value: agentName as ElasticAgentName,
+    })
+  );
 
   return (
     <EuiPanel>
@@ -31,6 +48,8 @@ export function NewScenarioForm() {
         <EuiFormRow label="Top level service">
           <ServiceSelector
             value={state.topLevelService?.agentName}
+            options={serviceOptions}
+            optionType={'single'}
             onChange={(agentName) => {
               dispatch({
                 type: 'change_top_level_service',
