07b768291ef258eb85498197d3f510393826dd14
pgayvallet <pierre.gayvallet@elastic.co>
2023-02-15 15:40:47 +0100

    nit export format

--- packages/core/saved-objects/core-saved-objects-migration-server-internal/src/core/index.ts
+++ packages/core/saved-objects/core-saved-objects-migration-server-internal/src/core/index.ts
@@ -12,10 +12,9 @@ export { excludeUnusedTypesQuery, REMOVED_TYPES } from './unused_types';
 export { TransformSavedObjectDocumentError } from './transform_saved_object_document_error';
 export { deterministicallyRegenerateObjectId } from './regenerate_object_id';
 export { buildTypesMappings } from './build_types_mappings';
-export { createIndexMap } from './build_index_map';
+export { createIndexMap, type IndexMap, type CreateIndexMapOptions } from './build_index_map';
 export type {
   DocumentsTransformFailed,
   DocumentsTransformSuccess,
   TransformErrorObjects,
 } from './migrate_raw_docs';
-export type { IndexMap, CreateIndexMapOptions } from './build_index_map';
