8d861633236f5520353ca042913dce637bb188da
jillguyonnet <jill.guyonnet@gmail.com>
2023-02-16 16:41:09 +0100

    [Fleet] Improve error message in package policy validation

--- x-pack/plugins/fleet/common/services/validate_package_policy.ts
+++ x-pack/plugins/fleet/common/services/validate_package_policy.ts
@@ -258,7 +258,11 @@ export const validatePackagePolicyConfig = (
     if (parsedValue && !Array.isArray(parsedValue)) {
       errors.push(
         i18n.translate('xpack.fleet.packagePolicyValidation.invalidArrayErrorMessage', {
-          defaultMessage: 'Invalid format',
+          defaultMessage: 'Invalid format for {fieldName}: expected array, found {fieldType}',
+          values: {
+            fieldName: varDef.title || varDef.name,
+            fieldType: typeof parsedValue
+          },
         })
       );
       return errors;
