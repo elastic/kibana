cb1cb236cf2a2acb8461b9a8e51363b72c84a69c
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2023-02-16 06:53:03 -0500

    [8.7] [Cloud Security] change `Belongs To` column to use runtime field (#151038) (#151420)
    # Backport
    
    This will backport the following commits from `main` to `8.7`:
    - [[Cloud Security] change `Belongs To` column to use runtime field
    (#151038)](https://github.com/elastic/kibana/pull/151038)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT
    [{"author":{"name":"Jordan","email":"51442161+JordanSh@users.noreply.github.com"},"sourceCommit":{"committedDate":"2023-02-16T10:43:17Z","message":"[Cloud
    Security] change `Belongs To` column to use runtime field
    (#151038)","sha":"e974e53b3206031b101de4a843b713bdb349bcdf","branchLabelMapping":{"^v8.8.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Team:Cloud
    Security","v8.7.0","v8.8.0"],"number":151038,"url":"https://github.com/elastic/kibana/pull/151038","mergeCommit":{"message":"[Cloud
    Security] change `Belongs To` column to use runtime field
    (#151038)","sha":"e974e53b3206031b101de4a843b713bdb349bcdf"}},"sourceBranch":"main","suggestedTargetBranches":["8.7"],"targetPullRequestStates":[{"branch":"8.7","label":"v8.7.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.8.0","labelRegex":"^v8.8.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/151038","number":151038,"mergeCommit":{"message":"[Cloud
    Security] change `Belongs To` column to use runtime field
    (#151038)","sha":"e974e53b3206031b101de4a843b713bdb349bcdf"}}]}]
    BACKPORT-->
    
    Co-authored-by: Jordan <51442161+JordanSh@users.noreply.github.com>

--- x-pack/plugins/cloud_security_posture/public/pages/findings/latest_findings_by_resource/findings_by_resource_table.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/findings/latest_findings_by_resource/findings_by_resource_table.tsx
@@ -17,6 +17,8 @@ import {
 import { FormattedMessage } from '@kbn/i18n-react';
 import numeral from '@elastic/numeral';
 import { Link, generatePath } from 'react-router-dom';
+import { i18n } from '@kbn/i18n';
+import { ColumnNameWithTooltip } from '../../../components/column_name_with_tooltip';
 import { ComplianceScoreBar } from '../../../components/compliance_score_bar';
 import * as TEST_SUBJECTS from '../test_subjects';
 import type { FindingsByResourcePage } from './use_findings_by_resource';
@@ -76,7 +78,7 @@ const FindingsByResourceTableComponent = ({
         getNonSortableColumn(findingsByResourceColumns['rule.benchmark.name']),
         { onAddFilter }
       ),
-      createColumnWithFilters(getNonSortableColumn(findingsByResourceColumns.cluster_id), {
+      createColumnWithFilters(getNonSortableColumn(findingsByResourceColumns.belongs_to), {
         onAddFilter,
       }),
       findingsByResourceColumns.compliance_score,
@@ -149,7 +151,22 @@ const baseColumns: Array<EuiTableFieldDataColumnType<FindingsByResourcePage>> = 
       );
     },
   },
-  baseFindingsColumns.cluster_id,
+  {
+    field: 'belongs_to',
+    name: (
+      <ColumnNameWithTooltip
+        columnName={i18n.translate(
+          'xpack.csp.findings.findingsTable.findingsTableColumn.clusterIdColumnLabel',
+          { defaultMessage: 'Belongs To' }
+        )}
+        tooltipContent={i18n.translate(
+          'xpack.csp.findings.findingsTable.findingsTableColumn.clusterIdColumnTooltipLabel',
+          { defaultMessage: 'Kubernetes Cluster ID or Cloud Account Name' }
+        )}
+      />
+    ),
+    truncateText: true,
+  },
   {
     field: 'compliance_score',
     width: '150px',
