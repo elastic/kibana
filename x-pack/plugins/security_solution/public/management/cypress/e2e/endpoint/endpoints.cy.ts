9071164410df535e52f323558560779ff8dc0b30
Gergő Ábrahám <gergo.abraham@elastic.co>
2023-02-20 17:38:12 +0100

    add e2e test for endpoint policy update

--- x-pack/plugins/security_solution/public/management/cypress/e2e/endpoint/endpoints.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/endpoint/endpoints.cy.ts
@@ -16,4 +16,38 @@ describe('Endpoints page', () => {
     cy.visit('/app/security/administration/endpoints');
     cy.contains('Hosts running Elastic Defend').should('exist');
   });
+
+  it('should update endpoint policy on Endpoint', () => {
+    const parseRevNumber = (revString: string) => Number(revString.match(/\d+/)?.[0]);
+
+    cy.visit('/app/security/administration/endpoints');
+
+    cy.getBySel('policyListRevNo')
+      .eq(0)
+      .invoke('text')
+      .then(parseRevNumber)
+      .then((initialRevisionNumber) => {
+        // Update policy
+        cy.getBySel('policyNameCellLink').eq(0).click();
+
+        cy.getBySel('policyDetailsSaveButton').click();
+        cy.getBySel('policyDetailsConfirmModal').should('exist');
+        cy.getBySel('confirmModalConfirmButton').click();
+        cy.contains(/has been updated/);
+
+        cy.getBySel('policyDetailsBackLink').click();
+
+        // Assert disappearing 'Out-of-date' indicator, Success Policy Status and increased revision number
+        cy.getBySel('rowPolicyOutOfDate').should('exist');
+        cy.getBySel('rowPolicyOutOfDate').should('not.exist'); // depends on the 10s auto-refresh
+
+        cy.getBySel('policyStatusCellLink').eq(0).should('contain', 'Success');
+
+        cy.getBySel('policyListRevNo')
+          .eq(0)
+          .invoke('text')
+          .then(parseRevNumber)
+          .should('equal', initialRevisionNumber + 1);
+      });
+  });
 });
