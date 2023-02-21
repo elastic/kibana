a640fd618664cfe027e51cc7bd476733c263b313
Abdul Zahid <awahab07@yahoo.com>
2023-02-21 18:25:55 +0100

    - Include missing `locationId` from deep links. - Disable Monitor Step Selector when there is only one step. - Add missing translations.

--- x-pack/plugins/synthetics/public/apps/synthetics/components/common/components/monitor_location_select.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/common/components/monitor_location_select.tsx
@@ -11,8 +11,8 @@ import {
   EuiHealth,
   EuiIcon,
   EuiLink,
-  EuiLoadingContent,
   EuiPopover,
+  EuiSkeletonText,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React, { useMemo, useState, useCallback } from 'react';
@@ -58,7 +58,7 @@ export const MonitorLocationSelect = ({
     if (monitorLocations.length > 1 || showSelection) {
       const button = (
         <EuiLink onClick={openLocationList} disabled={isDisabled}>
-          {selectedLocation.label} <EuiIcon type="arrowDown" />
+          {selectedLocation.label} {!isDisabled ? <EuiIcon type="arrowDown" /> : null}
         </EuiLink>
       );
 
@@ -118,7 +118,7 @@ export const MonitorLocationSelect = ({
     return (
       <EuiDescriptionList
         compressed={compressed}
-        listItems={[{ title: LOCATION_LABEL, description: <EuiLoadingContent lines={1} /> }]}
+        listItems={[{ title: LOCATION_LABEL, description: <EuiSkeletonText lines={1} /> }]}
       />
     );
   }
