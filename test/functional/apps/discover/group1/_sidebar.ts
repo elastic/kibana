2d9305876108fe270ea3ab9aa3bca5a90687e2f4
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2023-02-21 19:19:51 +0100

    Fix functional

--- test/functional/apps/discover/group1/_sidebar.ts
+++ test/functional/apps/discover/group1/_sidebar.ts
@@ -423,7 +423,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         await PageObjects.discover.waitUntilSidebarHasLoaded();
 
         expect(await PageObjects.discover.getSidebarAriaDescription()).to.be(
-          '0 available fields. 0 meta fields.'
+          '0 available fields. 0 meta fields. 3 meta fields.'
         );
         await testSubjects.existOrFail(
           `${PageObjects.discover.getSidebarSectionSelector('available')}-fetchWarning`
