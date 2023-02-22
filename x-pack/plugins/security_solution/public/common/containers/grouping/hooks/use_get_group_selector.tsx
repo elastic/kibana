fe04a4c34628d8230e80388f3e793516613d3b32
Jatin Kathuria <jatin.kathuria@elastic.co>
2023-02-22 18:04:21 +0100

    [Security Solution] [Trigger Actions] Alert Table Refactoring (#149128)
    ## Summary
    
    This PR replaces the existing `Alert Table` used in Security solution &
    cases with that of `triggers-actions-ui`.
    
    Ideally, this PR does make any changes to the functionality of the
    product and from user perspective. Nothing should Change.
    
    
    ‼️ Note for @elastic/security-threat-hunting-explore : This PR makes no
    changes to the table used in Host/Users page.
    
    ## Things to test and changes
    
    - @elastic/actionable-observability 
    - The changes in observability plugin are done to accommodate the
    changes in the API of `triggers-actions-ui` alert table.
    - Requesting you to do desk-testing this PR once by using Alert Table as
    you do on the daily basis.
    
    - @elastic/response-ops  
    - changes have been done in API as `security-solution` needed some
    parameters/values to achieve the parity in the functionality as compared
    to the current alert Table.
    
    ---------
    
    Co-authored-by: Xavier Mouligneau <xavier.mouligneau@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/security_solution/public/common/containers/grouping/hooks/use_get_group_selector.tsx
@@ -0,0 +1,119 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { FieldSpec } from '@kbn/data-views-plugin/common';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import type { TableId } from '../../../../../common/types';
+import { getDefaultGroupingOptions } from '../../../../detections/components/alerts_table/grouping_settings';
+import type { State } from '../../../store';
+import { defaultGroup } from '../../../store/grouping/defaults';
+import type { GroupOption } from '../../../store/grouping';
+import { groupActions, groupSelectors } from '../../../store/grouping';
+import { GroupsSelector, isNoneGroup } from '../../../components/grouping';
+
+export interface UseGetGroupSelectorArgs {
+  fields: FieldSpec[];
+  groupingId: string;
+  tableId: TableId;
+}
+
+export const useGetGroupingSelector = ({
+  fields,
+  groupingId,
+  tableId,
+}: UseGetGroupSelectorArgs) => {
+  const dispatch = useDispatch();
+
+  const getGroupByIdSelector = groupSelectors.getGroupByIdSelector();
+
+  const { activeGroup: selectedGroup, options } =
+    useSelector((state: State) => getGroupByIdSelector(state, groupingId)) ?? defaultGroup;
+
+  const setGroupsActivePage = useCallback(
+    (activePage: number) => {
+      dispatch(groupActions.updateGroupActivePage({ id: groupingId, activePage }));
+    },
+    [dispatch, groupingId]
+  );
+
+  const setSelectedGroup = useCallback(
+    (activeGroup: string) => {
+      dispatch(groupActions.updateActiveGroup({ id: groupingId, activeGroup }));
+    },
+    [dispatch, groupingId]
+  );
+
+  const setOptions = useCallback(
+    (newOptions: GroupOption[]) => {
+      dispatch(groupActions.updateGroupOptions({ id: groupingId, newOptionList: newOptions }));
+    },
+    [dispatch, groupingId]
+  );
+  const defaultGroupingOptions = getDefaultGroupingOptions(tableId);
+
+  useEffect(() => {
+    if (options.length > 0) return;
+    setOptions(
+      defaultGroupingOptions.find((o) => o.key === selectedGroup)
+        ? defaultGroupingOptions
+        : [
+            ...defaultGroupingOptions,
+            ...(!isNoneGroup(selectedGroup)
+              ? [
+                  {
+                    key: selectedGroup,
+                    label: selectedGroup,
+                  },
+                ]
+              : []),
+          ]
+    );
+  }, [defaultGroupingOptions, selectedGroup, setOptions, options]);
+
+  const groupsSelector = useMemo(
+    () => (
+      <GroupsSelector
+        groupSelected={selectedGroup}
+        data-test-subj="alerts-table-group-selector"
+        onGroupChange={(groupSelection: string) => {
+          if (groupSelection === selectedGroup) {
+            return;
+          }
+          setGroupsActivePage(0);
+          setSelectedGroup(groupSelection);
+
+          if (!isNoneGroup(groupSelection) && !options.find((o) => o.key === groupSelection)) {
+            setOptions([
+              ...defaultGroupingOptions,
+              {
+                label: groupSelection,
+                key: groupSelection,
+              },
+            ]);
+          } else {
+            setOptions(defaultGroupingOptions);
+          }
+        }}
+        fields={fields}
+        options={options}
+        title={'Group Alerts Selector'}
+      />
+    ),
+    [
+      defaultGroupingOptions,
+      fields,
+      options,
+      selectedGroup,
+      setGroupsActivePage,
+      setOptions,
+      setSelectedGroup,
+    ]
+  );
+
+  return groupsSelector;
+};
