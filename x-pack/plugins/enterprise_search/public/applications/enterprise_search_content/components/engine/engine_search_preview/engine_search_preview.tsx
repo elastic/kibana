f7e70d4b8fb2772b55e172c591598d887b6134be
Sloane Perrault <sloane.perrault@elastic.co>
2023-02-22 10:35:54 -0500

    [Enterprise Search] Engines Search Preview Document Flyout (#151517)
    ## Summary
    
    Adds a flyout to preview an entire document. Also cleans up some of the
    existing search preview code.
    
    
    https://user-images.githubusercontent.com/1699281/219487342-4186cbbc-0da1-443c-bc98-dd20654d3049.mov
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [x] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [x] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engine/engine_search_preview/engine_search_preview.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/engine/engine_search_preview/engine_search_preview.tsx
@@ -9,20 +9,9 @@ import React from 'react';
 
 import { useValues } from 'kea';
 
-import {
-  EuiBadge,
-  EuiBasicTable,
-  EuiBasicTableColumn,
-  EuiButton,
-  EuiFieldSearch,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiPanel,
-  EuiSpacer,
-} from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
 import { SearchProvider, SearchBox, Results } from '@elastic/react-search-ui';
-import { ResultsViewProps, ResultViewProps, InputViewProps } from '@elastic/react-search-ui-views';
-import { SearchDriverOptions, SearchResult } from '@elastic/search-ui';
+import { SearchDriverOptions } from '@elastic/search-ui';
 import EnginesAPIConnector, {
   Transporter,
   SearchRequest,
@@ -38,7 +27,11 @@ import { EnterpriseSearchEnginesPageTemplate } from '../../layout/engines_page_t
 import { EngineIndicesLogic } from '../engine_indices_logic';
 import { EngineViewLogic } from '../engine_view_logic';
 
+import { DocumentProvider } from './document_context';
+import { DocumentFlyout } from './document_flyout';
 import { EngineSearchPreviewLogic } from './engine_search_preview_logic';
+
+import { InputView, ResultView, ResultsView } from './search_ui_components';
 
 class InternalEngineTransporter implements Transporter {
   constructor(private http: HttpSetup, private engineName: string) {}
@@ -64,81 +57,6 @@ class InternalEngineTransporter implements Transporter {
     return withUniqueIds;
   }
 }
-
-const ResultsView: React.FC<ResultsViewProps> = ({ children }) => {
-  return <EuiFlexGroup direction="column">{children}</EuiFlexGroup>;
-};
-
-const ResultView: React.FC<ResultViewProps> = ({ result }) => {
-  const fields = Object.entries(result)
-    .filter(([key]) => !key.startsWith('_') && key !== 'id')
-    .map(([key, value]) => {
-      return {
-        name: key,
-        value: value.raw,
-      };
-    });
-
-  const {
-    _meta: {
-      id: encodedId,
-      rawHit: { _index: index },
-    },
-  } = result;
-
-  const [, id] = JSON.parse(atob(encodedId));
-
-  const columns: Array<EuiBasicTableColumn<SearchResult>> = [
-    {
-      field: 'name',
-      name: 'name',
-      render: (name: string) => {
-        return <code>&quot;{name}&quot;</code>;
-      },
-      truncateText: true,
-      width: '20%',
-    },
-    {
-      field: 'value',
-      name: 'value',
-      render: (value: string) => value,
-    },
-  ];
-
-  return (
-    <EuiPanel paddingSize="m">
-      <EuiFlexGroup direction="column">
-        <EuiFlexGroup justifyContent="spaceBetween">
-          <code>ID: {id}</code>
-          <EuiFlexItem grow={false}>
-            <EuiFlexGroup gutterSize="xs" alignItems="center">
-              <code>from</code>
-              <EuiBadge color="hollow">{index}</EuiBadge>
-            </EuiFlexGroup>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiBasicTable items={fields} columns={columns} />
-      </EuiFlexGroup>
-    </EuiPanel>
-  );
-};
-
-const InputView: React.FC<InputViewProps> = ({ getInputProps }) => {
-  return (
-    <EuiFlexGroup gutterSize="s">
-      <EuiFieldSearch
-        fullWidth
-        placeholder="search"
-        {...getInputProps({})}
-        isClearable
-        aria-label="Search Input"
-      />
-      <EuiButton type="submit" color="primary" fill>
-        Search
-      </EuiButton>
-    </EuiFlexGroup>
-  );
-};
 
 export const EngineSearchPreview: React.FC = () => {
   const { http } = useValues(HttpLogic);
@@ -175,20 +93,23 @@ export const EngineSearchPreview: React.FC = () => {
       }}
       engineName={engineName}
     >
-      <SearchProvider config={config}>
-        <EuiFlexGroup>
-          <EuiFlexItem>
-            <SearchBox inputView={InputView} />
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiSpacer size="m" />
-        <EuiFlexGroup>
-          <EuiFlexItem grow={false} css={{ minWidth: '240px' }} />
-          <EuiFlexItem>
-            <Results view={ResultsView} resultView={ResultView} />
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </SearchProvider>
+      <DocumentProvider>
+        <SearchProvider config={config}>
+          <EuiFlexGroup>
+            <EuiFlexItem>
+              <SearchBox inputView={InputView} />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+          <EuiSpacer size="m" />
+          <EuiFlexGroup>
+            <EuiFlexItem grow={false} css={{ minWidth: '240px' }} />
+            <EuiFlexItem>
+              <Results view={ResultsView} resultView={ResultView} />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </SearchProvider>
+        <DocumentFlyout />
+      </DocumentProvider>
     </EnterpriseSearchEnginesPageTemplate>
   );
 };
