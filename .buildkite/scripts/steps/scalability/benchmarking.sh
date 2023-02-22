be52495f3436030ebc064f7c2e926ea6c8cb61cf
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-02-22 18:37:20 +0100

    move common logic to util

--- .buildkite/scripts/steps/scalability/benchmarking.sh
+++ .buildkite/scripts/steps/scalability/benchmarking.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
+source .buildkite/scripts/steps/scalability/util.sh
+
 #.buildkite/scripts/bootstrap.sh
 echo "--- yarn kbn reset && yarn kbn bootstrap"
 yarn kbn reset && yarn kbn bootstrap
@@ -37,38 +39,8 @@ download_artifacts() {
   tar -xzf "${LATEST_RUN_ARTIFACTS_DIR}/scalability_traces.tar.gz"
 }
 
-checkout_and_compile_load_runner() {
-  mkdir -p "${KIBANA_LOAD_TESTING_DIR}" && cd "${KIBANA_LOAD_TESTING_DIR}"
-
-  if [[ ! -d .git ]]; then
-    git init
-    git remote add origin https://github.com/elastic/kibana-load-testing.git
-  fi
-  git fetch origin --depth 1 "main"
-  git reset --hard FETCH_HEAD
-
-  KIBANA_LOAD_TESTING_GIT_COMMIT="$(git rev-parse HEAD)"
-  export KIBANA_LOAD_TESTING_GIT_COMMIT
-
-  mvn -q test-compile
-  echo "Set 'GATLING_PROJECT_PATH' env var for ScalabilityTestRunner"
-  export GATLING_PROJECT_PATH="$(pwd)"
-}
-
-upload_test_results() {
-  cd "${KIBANA_DIR}"
-  echo "Upload server logs as build artifacts"
-  tar -czf server-logs.tar.gz data/ftr_servers_logs/**/*
-  buildkite-agent artifact upload server-logs.tar.gz
-  echo "--- Upload Gatling reports as build artifacts"
-  tar -czf "scalability_test_report.tar.gz" --exclude=simulation.log -C kibana-load-testing/target gatling
-  buildkite-agent artifact upload "scalability_test_report.tar.gz"
-  cd "${LATEST_RUN_ARTIFACTS_DIR}"
-  echo "Upload scalability traces as build artifacts"
-  buildkite-agent artifact upload "scalability_traces.tar.gz"
-}
-
 echo "--- Clone kibana-load-testing repo and compile project"
+mkdir -p "${KIBANA_LOAD_TESTING_DIR}" && cd "${KIBANA_LOAD_TESTING_DIR}"
 checkout_and_compile_load_runner
 
 cd "$KIBANA_DIR"
@@ -80,3 +52,7 @@ node scripts/run_scalability --kibana-install-dir "$KIBANA_BUILD_LOCATION" --jou
 
 echo "--- Upload test results"
 upload_test_results
+
+cd "${LATEST_RUN_ARTIFACTS_DIR}"
+echo "Upload scalability traces as build artifacts"
+buildkite-agent artifact upload "scalability_traces.tar.gz"
---
+++ .buildkite/scripts/steps/scalability/util.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+checkout_and_compile_load_runner() {
+  if [[ ! -d .git ]]; then
+    git init
+    git remote add origin https://github.com/elastic/kibana-load-testing.git
+  fi
+  git fetch origin --depth 1 "main"
+  git reset --hard FETCH_HEAD
+
+  KIBANA_LOAD_TESTING_GIT_COMMIT="$(git rev-parse HEAD)"
+  export KIBANA_LOAD_TESTING_GIT_COMMIT
+
+  mvn -q test-compile
+  echo "Set 'GATLING_PROJECT_PATH' env var for ScalabilityTestRunner"
+  export GATLING_PROJECT_PATH="$(pwd)"
+}
+
+upload_test_results() {
+  echo "Upload server logs as build artifacts"
+  tar -czf server-logs.tar.gz data/ftr_servers_logs/**/*
+  buildkite-agent artifact upload server-logs.tar.gz
+  echo "--- Upload Gatling reports as build artifacts"
+  tar -czf "scalability_test_report.tar.gz" --exclude=simulation.log -C kibana-load-testing/target gatling
+  buildkite-agent artifact upload "scalability_test_report.tar.gz"
+}
