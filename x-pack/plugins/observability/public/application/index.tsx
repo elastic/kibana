6247ae7a23d3a772f9da46018c207dfb89e699bc
Coen Warmer <coen.warmer@gmail.com>
2023-02-23 17:51:46 +0100

    Remove hasData provider

--- x-pack/plugins/observability/public/application/index.tsx
+++ x-pack/plugins/observability/public/application/index.tsx
@@ -22,7 +22,6 @@ import {
 import { Storage } from '@kbn/kibana-utils-plugin/public';
 import { UsageCollectionSetup } from '@kbn/usage-collection-plugin/public';
 import type { LazyObservabilityPageTemplateProps } from '../components/shared/page_template/lazy_page_template';
-import { HasDataContextProvider } from '../context/has_data_context';
 import { PluginContext } from '../context/plugin_context';
 import { useRouteParams } from '../hooks/use_route_params';
 import { ConfigSchema, ObservabilityPublicPluginsStart } from '../plugin';
@@ -111,9 +110,8 @@ export const renderApp = ({
                 <i18nCore.Context>
                   <RedirectAppLinks application={core.application} className={APP_WRAPPER_CLASS}>
                     <QueryClientProvider client={queryClient}>
-                      <HasDataContextProvider>
-                        <App />
-                      </HasDataContextProvider>
+                      <App />
+
                       <ReactQueryDevtools />
                     </QueryClientProvider>
                   </RedirectAppLinks>
