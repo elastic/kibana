3b13d44207bf4e040ae42fd0f3086460e1fc9dd4
Mike Cote <michel.cote@elastic.co>
2023-02-23 14:44:44 -0500

    Fix failing test

--- x-pack/plugins/task_manager/server/constants.ts
+++ x-pack/plugins/task_manager/server/constants.ts
@@ -8,6 +8,7 @@ export const TASK_MANAGER_INDEX = '.kibana_task_manager';
 export const CONCURRENCY_ALLOW_LIST_BY_TASK_TYPE: string[] = [
   // for testing
   'sampleTaskWithSingleConcurrency',
+  'sampleTaskWithLimitedConcurrency',
 
   // task types requiring a concurrency
   'report:execute',
