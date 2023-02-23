e8fa342fa67fb695f7f2bcfbcd354766f1a180bd
RubixSolver99 <blackbattiger@outlook.com>
2023-02-23 17:03:35 -0600

    Removed requires reload for ui_settings

--- x-pack/plugins/security_solution/server/ui_settings.ts
+++ x-pack/plugins/security_solution/server/ui_settings.ts
@@ -380,7 +380,7 @@ export const initUiSettings = (
           }
         ),
         category: [APP_ID],
-        requiresPageReload: true,
+        requiresPageReload: false,
         schema: schema.arrayOf(schema.string()),
       }
   };
