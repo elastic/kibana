7cdfc8a8de9b2cab0ce5636708e359fa724ece27
PhilippeOberti <philippe.oberti@elastic.co>
2023-02-24 11:57:57 -0600

    [Security Solution] expandable flyout left section skeleton
    - expand/collapse button in right section header
    - header with 4 tabs (visualize, insights, investigation and history)
    - 3 empty tabs content and visualize has a button group for session view and analyser graph (empty components)

---
+++ x-pack/plugins/security_solution/cypress/screens/document_expandable_flyout.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import {
+  ANALYSER_GRAPH_TEST_ID,
+  SESSION_VIEW_TEST_ID,
+} from '../../public/flyout/left/components/test_ids';
+import {
+  HISTORY_TAB_CONTENT_TEST_ID,
+  INSIGHTS_TAB_CONTENT_TEST_ID,
+  INVESTIGATIONS_TAB_CONTENT_TEST_ID,
+  VISUALIZE_TAB_BUTTON_GROUP_TEST_ID,
+  VISUALIZE_TAB_GRAPH_ANALYSER_BUTTON_TEST_ID,
+  VISUALIZE_TAB_SESSION_VIEW_BUTTON_TEST_ID,
+} from '../../public/flyout/left/tabs/test_ids';
+import {
+  HISTORY_TAB_TEST_ID,
+  INSIGHTS_TAB_TEST_ID,
+  INVESTIGATIONS_TAB_TEST_ID,
+  VISUALIZE_TAB_TEST_ID,
+} from '../../public/flyout/left/test_ids';
+import {
+  JSON_TAB_TEST_ID,
+  OVERVIEW_TAB_TEST_ID,
+  TABLE_TAB_TEST_ID,
+} from '../../public/flyout/right/test_ids';
+import {
+  JSON_TAB_CONTENT_TEST_ID,
+  OVERVIEW_TAB_CONTENT_TEST_ID,
+  TABLE_TAB_CONTENT_TEST_ID,
+} from '../../public/flyout/right/tabs/test_ids';
+import {
+  COLLAPSE_DETAILS_BUTTON_TEST_ID,
+  EXPAND_DETAILS_BUTTON_TEST_ID,
+  FLYOUT_HEADER_TITLE_TEST_ID,
+} from '../../public/flyout/right/components/test_ids';
+
+export const DOCUMENT_DETAILS_FLYOUT_HEADER_TITLE = `[data-test-subj="${FLYOUT_HEADER_TITLE_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_EXPAND_DETAILS_BUTTON = `[data-test-subj="${EXPAND_DETAILS_BUTTON_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_COLLAPSE_DETAILS_BUTTON = `[data-test-subj="${COLLAPSE_DETAILS_BUTTON_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_OVERVIEW_TAB = `[data-test-subj="${OVERVIEW_TAB_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_TABLE_TAB = `[data-test-subj="${TABLE_TAB_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_JSON_TAB = `[data-test-subj="${JSON_TAB_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_OVERVIEW_TAB_CONTENT = `[data-test-subj="${OVERVIEW_TAB_CONTENT_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_TABLE_TAB_CONTENT = `[data-test-subj="${TABLE_TAB_CONTENT_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_JSON_TAB_CONTENT = `[data-test-subj="${JSON_TAB_CONTENT_TEST_ID}"]`;
+
+export const DOCUMENT_DETAILS_FLYOUT_VISUALIZE_TAB = `[data-test-subj="${VISUALIZE_TAB_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_INSIGHTS_TAB = `[data-test-subj="${INSIGHTS_TAB_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_INVESTIGATIONS_TAB = `[data-test-subj="${INVESTIGATIONS_TAB_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_HISTORY_TAB = `[data-test-subj="${HISTORY_TAB_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_VISUALIZE_TAB_BUTTON_GROUP = `[data-test-subj="${VISUALIZE_TAB_BUTTON_GROUP_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_VISUALIZE_TAB_SESSION_VIEW_BUTTON = `[data-test-subj="${VISUALIZE_TAB_SESSION_VIEW_BUTTON_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_VISUALIZE_TAB_SESSION_VIEW_CONTENT = `[data-test-subj="${SESSION_VIEW_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_VISUALIZE_TAB_GRAPH_ANALYSER_BUTTON = `[data-test-subj="${VISUALIZE_TAB_GRAPH_ANALYSER_BUTTON_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_VISUALIZE_TAB_GRAPH_ANALYSER_CONTENT = `[data-test-subj="${ANALYSER_GRAPH_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_INSIGHTS_TAB_CONTENT = `[data-test-subj="${INSIGHTS_TAB_CONTENT_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_INVESTIGATIONS_TAB_CONTENT = `[data-test-subj="${INVESTIGATIONS_TAB_CONTENT_TEST_ID}"]`;
+export const DOCUMENT_DETAILS_FLYOUT_HISTORY_TAB_CONTENT = `[data-test-subj="${HISTORY_TAB_CONTENT_TEST_ID}"]`;
