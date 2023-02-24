dbbdc3848bbfd784c07ae1a5f53e3b395e3e9008
Catherine Liu <catherine.liu@elastic.co>
2023-02-23 16:32:08 -0800

    Disable layout changes when panel is expanded

--- src/plugins/dashboard/public/dashboard_container/component/grid/dashboard_grid.tsx
+++ src/plugins/dashboard/public/dashboard_container/component/grid/dashboard_grid.tsx
@@ -167,22 +167,24 @@ export const DashboardGrid = () => {
 
   const onLayoutChange = useCallback(
     (newLayout: PanelLayout[]) => {
-      const updatedPanels: { [key: string]: DashboardPanelState } = newLayout.reduce(
-        (updatedPanelsAcc, panelLayout) => {
-          updatedPanelsAcc[panelLayout.i] = {
-            ...panels[panelLayout.i],
-            gridData: _.pick(panelLayout, ['x', 'y', 'w', 'h', 'i']),
-          };
-          return updatedPanelsAcc;
-        },
-        {} as { [key: string]: DashboardPanelState }
-      );
-      // onLayoutChange gets called by react grid layout a lot more than it should, so only dispatch the updated panels if the layout has actually changed
-      if (!getPanelLayoutsAreEqual(panels, updatedPanels)) {
-        dispatch(setPanels(updatedPanels));
+      if (!expandedPanelId) {
+        const updatedPanels: { [key: string]: DashboardPanelState } = newLayout.reduce(
+          (updatedPanelsAcc, panelLayout) => {
+            updatedPanelsAcc[panelLayout.i] = {
+              ...panels[panelLayout.i],
+              gridData: _.pick(panelLayout, ['x', 'y', 'w', 'h', 'i']),
+            };
+            return updatedPanelsAcc;
+          },
+          {} as { [key: string]: DashboardPanelState }
+        );
+        // onLayoutChange gets called by react grid layout a lot more than it should, so only dispatch the updated panels if the layout has actually changed
+        if (!getPanelLayoutsAreEqual(panels, updatedPanels)) {
+          dispatch(setPanels(updatedPanels));
+        }
       }
     },
-    [dispatch, panels, setPanels]
+    [dispatch, panels, setPanels, expandedPanelId]
   );
 
   const dashboardPanels = useMemo(() => {
