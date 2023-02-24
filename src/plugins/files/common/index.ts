a69413d98ff2c5fb6e8533da9183322e42e55721
vadimkibana <vadimkibana@gmail.com>
2023-02-24 14:47:20 +0100

    import correctly default file kind in image embeddable

--- src/plugins/files/common/index.ts
+++ src/plugins/files/common/index.ts
@@ -7,7 +7,6 @@
  */
 
 export { FILE_SO_TYPE, PLUGIN_ID, PLUGIN_NAME, ES_FIXED_SIZE_INDEX_BLOB_STORE } from './constants';
-export { defaultImageFileKind } from './default_image_file_kind';
 
 export type {
   File,
@@ -30,3 +29,6 @@ export type {
   FileShareJSONWithToken,
   UpdatableFileShareMetadata,
 } from './types';
+
+import  * as DefaultFileKind from './default_image_file_kind';
+export { DefaultFileKind };
