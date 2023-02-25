332219e77ceb02e7335cb5b118b7e7c731caf921
vadimkibana <vadimkibana@gmail.com>
2023-02-25 16:32:08 +0100

    add rpc service types

---
+++ src/plugins/files/server/file_rpc_service/types.ts
@@ -0,0 +1,94 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import type { File } from '../../common/types';
+import type { CreateFileArgs } from '../file_service/file_action_types';
+import type { KibanaRequest } from '@kbn/core-http-server'
+
+/**
+ * Represents a single RPC method. The `In` and `Out` types are the input and
+ * output types of the method.
+ */
+export type FileRpcMethod<In, Out> = (req: FileRpcRequest<In>) => Promise<FileRpcResponse<Out>>;
+
+/**
+ * Represents the input object of a {@link FileRpcMethod}.
+ */
+export interface FileRpcRequest<Data = unknown> {
+  /**
+   * The context of the request. Should contain information about connection,
+   * authentication, etc.
+   */
+  ctx: FileRpcRequestContext;
+
+  /**
+   * Custom data, different for each method.
+   */
+  data: Data;
+}
+
+/**
+ * The context of the request. Should contain information about connection,
+ * authentication, etc. This object allows to abstract away the underlying
+ * transport layer, currently it uses {@link KibanaRequest}, but in the future
+ * it can be changed to something else.
+ */
+export interface FileRpcRequestContext {
+  req: KibanaRequest;
+}
+
+/**
+ * Represents the output object of a {@link FileRpcMethod}.
+ */
+export interface FileRpcResponse<Data = unknown> {
+  /**
+   * Custom data, different for each method.
+   */
+  data: Data;
+
+  /**
+   * Custom HTTP status code, used for compatibility with HTTP network layers.
+   * If not specified, assumes 200.
+   */
+  httpCode?: number;
+}
+
+/**
+ * Represents an error that can be returned by a {@link FileRpcMethod}.
+ */
+export interface FileRpcError {
+  /**
+   * Human readable error message. Usually, the `.message` property of the
+   * {@link Error} object.
+   */
+  message: string;
+
+  /**
+   * Error code, technical identifier of the error. Does not have to be human
+   * readable.
+   */
+  code: string;
+
+  /**
+   * Custom HTTP status code, used for compatibility with HTTP network layers.
+   * If not specified, assumes 400.
+   */
+  httpCode?: number;
+}
+
+/**
+ * The {@link FileRpcService} is a wrapper around {@link FileService}. It allows
+ * to expose the service to the outside world via HTTP or other network layers.
+ */
+export interface IFileRpcService {
+  /**
+   * See {@link FileMetadataClient.create}. Creates a file and returns the
+   * {@link File} object.
+   * */
+  create: FileRpcMethod<CreateFileArgs, File>;
+}
