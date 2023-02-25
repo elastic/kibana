add1219efa37a5b6e0d2ddbb24caa75f43f2008c
Karl Godard <karl.godard@elastic.co>
2023-02-24 16:44:26 -0800

    [Defend 4 containers] ignoreVolumeMounts + ignoreVolumeFiles selector conditions added. (#151956)
    ## Summary
    
    This adds two new selector conditions to our policy schema.
    ignoreVolumeMounts and ignoreVolumeFiles. They are a easy route to avoid
    users having to list all their k8s mounts in targetFilePath selector (as
    an exclude selector). This PR also refactors the json schema for a
    policy into it's own json file, so that it may be sync with the json
    file in the cloud-defend repo.
    
    Bonus commit: selectors now support "createFile, modifyFile and
    deleteFile" operations. (FIM capabilities)
    
    Context: 
    https://github.com/elastic/security-team/issues/5718
    
    https://github.com/elastic/integrations/tree/main/packages/cloud_defend#selectors
    
    
    ![image](https://user-images.githubusercontent.com/16198204/220793759-15faa228-05f5-436e-aa52-4c712bcdaf3b.png)
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [x] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [x] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)

--- x-pack/plugins/cloud_defend/public/types.ts
+++ x-pack/plugins/cloud_defend/public/types.ts
@@ -30,6 +30,8 @@ export enum ControlSelectorCondition {
   containerImageName = 'containerImageName',
   containerImageTag = 'containerImageTag',
   targetFilePath = 'targetFilePath',
+  ignoreVolumeFiles = 'ignoreVolumeFiles',
+  ignoreVolumeMounts = 'ignoreVolumeMounts',
   orchestratorClusterId = 'orchestratorClusterId',
   orchestratorClusterName = 'orchestratorClusterName',
   orchestratorNamespace = 'orchestratorNamespace',
@@ -37,6 +39,11 @@ export enum ControlSelectorCondition {
   orchestratorResourceName = 'orchestratorResourceName',
   orchestratorResourceType = 'orchestratorResourceType',
   orchestratorType = 'orchestratorType',
+}
+
+export enum ControlSelectorBooleanConditions {
+  ignoreVolumeFiles = 'ignoreVolumeFiles',
+  ignoreVolumeMounts = 'ignoreVolumeMounts',
 }
 
 export enum ControlSelectorOperation {
@@ -66,6 +73,8 @@ export interface ControlSelector {
   containerImageName?: string[];
   containerImageTag?: string[];
   targetFilePath?: string[];
+  ignoreVolumeFiles?: boolean;
+  ignoreVolumeMounts?: boolean;
   orchestratorClusterId?: string[];
   orchestratorClusterName?: string[];
   orchestratorNamespace?: string[];
