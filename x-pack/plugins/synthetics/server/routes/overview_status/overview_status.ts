1dded2f653ab33914a2ccbf1d1cf68372d6aa9df
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2023-02-27 05:18:15 -0500

    [8.7] [Synthetics] Fix private location filter usage (#151968) (#152181)
    # Backport
    
    This will backport the following commits from `main` to `8.7`:
    - [[Synthetics] Fix private location filter usage
    (#151968)](https://github.com/elastic/kibana/pull/151968)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT
    [{"author":{"name":"Shahzad","email":"shahzad31comp@gmail.com"},"sourceCommit":{"committedDate":"2023-02-27T09:07:12Z","message":"[Synthetics]
    Fix private location filter usage (#151968)\n\nCo-authored-by: Dominique
    Clarke
    <dominique.clarke@elastic.co>","sha":"aa5d089ef16a61485d0ee9c592c12e357ec36724","branchLabelMapping":{"^v8.8.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["Team:uptime","release_note:skip","v8.7.0","v8.8.0"],"number":151968,"url":"https://github.com/elastic/kibana/pull/151968","mergeCommit":{"message":"[Synthetics]
    Fix private location filter usage (#151968)\n\nCo-authored-by: Dominique
    Clarke
    <dominique.clarke@elastic.co>","sha":"aa5d089ef16a61485d0ee9c592c12e357ec36724"}},"sourceBranch":"main","suggestedTargetBranches":["8.7"],"targetPullRequestStates":[{"branch":"8.7","label":"v8.7.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.8.0","labelRegex":"^v8.8.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/151968","number":151968,"mergeCommit":{"message":"[Synthetics]
    Fix private location filter usage (#151968)\n\nCo-authored-by: Dominique
    Clarke
    <dominique.clarke@elastic.co>","sha":"aa5d089ef16a61485d0ee9c592c12e357ec36724"}}]}]
    BACKPORT-->
    
    Co-authored-by: Shahzad <shahzad31comp@gmail.com>

--- x-pack/plugins/synthetics/server/routes/overview_status/overview_status.ts
+++ x-pack/plugins/synthetics/server/routes/overview_status/overview_status.ts
@@ -6,20 +6,16 @@
  */
 import { intersection } from 'lodash';
 import datemath, { Unit } from '@kbn/datemath';
-import { SavedObjectsClientContract } from '@kbn/core/server';
 import moment from 'moment';
 import { ConfigKey } from '../../../common/runtime_types';
 import {
   getAllMonitors,
   processMonitors,
 } from '../../saved_objects/synthetics_monitor/get_all_monitors';
-import { UptimeServerSetup } from '../../legacy_uptime/lib/adapters';
 import { queryMonitorStatus } from '../../queries/query_monitor_status';
 import { SYNTHETICS_API_URLS } from '../../../common/constants';
 import { UMServerLibs } from '../../legacy_uptime/uptime_server';
-import { SyntheticsRestApiRouteFactory } from '../../legacy_uptime/routes';
-import { UptimeEsClient } from '../../legacy_uptime/lib/lib';
-import { SyntheticsMonitorClient } from '../../synthetics_service/synthetics_monitor/synthetics_monitor_client';
+import { RouteContext, SyntheticsRestApiRouteFactory } from '../../legacy_uptime/routes';
 import { getMonitorFilters, OverviewStatusSchema, OverviewStatusQuery } from '../common';
 
 /**
@@ -40,14 +36,10 @@ export function periodToMs(schedule: { number: string; unit: Unit }) {
  * Subsequently, fetch the status for each monitor per location in the data streams.
  * @returns The counts of up/down/disabled monitor by location, and a map of each monitor:location status.
  */
-export async function getStatus(
-  server: UptimeServerSetup,
-  uptimeEsClient: UptimeEsClient,
-  soClient: SavedObjectsClientContract,
-  syntheticsMonitorClient: SyntheticsMonitorClient,
-  params: OverviewStatusQuery
-) {
-  const { query, locations: queryLocations } = params;
+export async function getStatus(context: RouteContext, params: OverviewStatusQuery) {
+  const { uptimeEsClient, syntheticsMonitorClient, savedObjectsClient, server } = context;
+
+  const { query, locations: queryLocations, scopeStatusByLocation = true } = params;
   /**
    * Walk through all monitor saved objects, bucket IDs by disabled/enabled status.
    *
@@ -55,12 +47,13 @@ export async function getStatus(
    * latest ping for all enabled monitors.
    */
 
-  const filtersStr = getMonitorFilters({
+  const filtersStr = await getMonitorFilters({
     ...params,
-    serviceLocations: syntheticsMonitorClient.syntheticsService.locations,
+    context,
   });
+
   const allMonitors = await getAllMonitors({
-    soClient,
+    soClient: savedObjectsClient,
     search: query ? `${query}*` : undefined,
     filter: filtersStr,
     fields: [
@@ -83,14 +76,15 @@ export async function getStatus(
     disabledMonitorsCount,
     projectMonitorsCount,
     monitorQueryIdToConfigIdMap,
-  } = await processMonitors(allMonitors, server, soClient, syntheticsMonitorClient);
+  } = await processMonitors(allMonitors, server, savedObjectsClient, syntheticsMonitorClient);
 
   // Account for locations filter
   const queryLocationsArray =
     queryLocations && !Array.isArray(queryLocations) ? [queryLocations] : queryLocations;
-  const listOfLocationAfterFilter = queryLocationsArray
-    ? intersection(listOfLocations, queryLocationsArray)
-    : listOfLocations;
+  const listOfLocationAfterFilter =
+    queryLocationsArray && scopeStatusByLocation
+      ? intersection(listOfLocations, queryLocationsArray)
+      : listOfLocations;
 
   const range = {
     from: moment().subtract(maxPeriod, 'milliseconds').subtract(20, 'minutes').toISOString(),
@@ -128,20 +122,10 @@ export const createGetCurrentStatusRoute: SyntheticsRestApiRouteFactory = (libs:
   validate: {
     query: OverviewStatusSchema,
   },
-  handler: async ({
-    server,
-    uptimeEsClient,
-    savedObjectsClient,
-    syntheticsMonitorClient,
-    request,
-  }): Promise<any> => {
+  handler: async (routeContext): Promise<any> => {
+    const { request } = routeContext;
+
     const params = request.query as OverviewStatusQuery;
-    return await getStatus(
-      server,
-      uptimeEsClient,
-      savedObjectsClient,
-      syntheticsMonitorClient,
-      params
-    );
+    return await getStatus(routeContext, params);
   },
 });
