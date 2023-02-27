cd7edc7ecb7c5fa17978e4726f57b7814a65ae64
Jason Rhodes <jason.rhodes@elastic.co>
2023-02-27 16:23:03 -0500

    Baseline asset manager plugin with two simple routes

---
+++ x-pack/plugins/asset_manager/server/lib/get_assets.ts
@@ -0,0 +1,122 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { QueryDslQueryContainer, SearchRequest } from '@elastic/elasticsearch/lib/api/types';
+import { debug } from '../../common/debug_log';
+import { Asset, AssetFilters } from '../../common/types_api';
+import { ASSETS_INDEX } from '../constants';
+import { ElasticsearchAccessorOptions } from '../types';
+
+interface GetAssetsOptions extends ElasticsearchAccessorOptions {
+  filters?: AssetFilters;
+  from?: string;
+  to?: string;
+}
+
+export async function getAssets({
+  esClient,
+  filters = {},
+  from = 'now-24h',
+  to = 'now',
+}: GetAssetsOptions): Promise<Asset[]> {
+  const dsl: SearchRequest = {
+    index: ASSETS_INDEX,
+    query: {
+      bool: {
+        filter: [
+          {
+            range: {
+              '@timestamp': {
+                gte: from,
+                lte: to,
+              },
+            },
+          },
+        ],
+      },
+    },
+    collapse: {
+      field: 'asset.ean',
+    },
+    sort: {
+      '@timestamp': {
+        order: 'desc',
+      },
+    },
+  };
+
+  if (filters && Object.keys(filters).length > 0) {
+    const musts: QueryDslQueryContainer[] = [];
+
+    if (typeof filters.collectionVersion === 'number') {
+      musts.push({
+        term: {
+          ['asset.collection_version']: filters.collectionVersion,
+        },
+      });
+    }
+
+    if (filters.type) {
+      musts.push({
+        term: {
+          ['asset.type']: filters.type,
+        },
+      });
+    }
+
+    if (filters.kind) {
+      musts.push({
+        term: {
+          ['asset.kind']: filters.kind,
+        },
+      });
+    }
+
+    if (filters.ean) {
+      musts.push({
+        term: {
+          ['asset.ean']: filters.ean,
+        },
+      });
+    }
+
+    if (filters.id) {
+      musts.push({
+        term: {
+          ['asset.id']: filters.id,
+        },
+      });
+    }
+
+    if (filters.typeLike) {
+      musts.push({
+        wildcard: {
+          ['asset.type']: filters.typeLike,
+        },
+      });
+    }
+
+    if (filters.eanLike) {
+      musts.push({
+        wildcard: {
+          ['asset.ean']: filters.eanLike,
+        },
+      });
+    }
+
+    if (musts.length > 0) {
+      dsl.query = dsl.query || {};
+      dsl.query.bool = dsl.query.bool || {};
+      dsl.query.bool.must = musts;
+    }
+  }
+
+  debug('Performing Asset Query', '\n\n', JSON.stringify(dsl, null, 2));
+
+  const response = await esClient.search<{}>(dsl);
+  return response.hits.hits.map((hit) => hit._source as Asset);
+}
---
+++ x-pack/plugins/asset_manager/server/plugin.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { schema } from '@kbn/config-schema';
+import { Plugin, CoreSetup, RequestHandlerContext } from '@kbn/core/server';
+import { debug } from '../common/debug_log';
+import { AssetFilters } from '../common/types_api';
+import { ASSET_MANAGER_API_BASE } from './constants';
+import { getAssets } from './lib/get_assets';
+import { getAssetByEan } from './lib/get_asset_by_ean';
+
+export type AssetManagerServerPluginSetup = ReturnType<AssetManagerServerPlugin['setup']>;
+
+async function getEsClientFromContext(context: RequestHandlerContext) {
+  return (await context.core).elasticsearch.client.asCurrentUser;
+}
+
+export class AssetManagerServerPlugin implements Plugin<AssetManagerServerPluginSetup> {
+  public async setup(core: CoreSetup) {
+    const router = core.http.createRouter();
+
+    router.get(
+      {
+        path: `${ASSET_MANAGER_API_BASE}/ping`,
+        validate: false,
+      },
+      async (_context, _req, res) => {
+        return res.ok({
+          body: { message: 'Asset Manager OK' },
+          headers: { 'content-type': 'application/json' },
+        });
+      }
+    );
+
+    router.get<unknown, AssetFilters | undefined, unknown>(
+      {
+        path: `${ASSET_MANAGER_API_BASE}/assets`,
+        validate: {
+          query: schema.any({}),
+        },
+      },
+      async (context, req, res) => {
+        const filters = req.query || {};
+        const esClient = await getEsClientFromContext(context);
+
+        try {
+          const assets = await getAssets({ esClient, filters });
+          return res.ok({ body: { assets } });
+        } catch (error: unknown) {
+          debug('error looking up asset records', error);
+          return res.customError({ statusCode: 500 });
+        }
+      }
+    );
+
+    router.get<{ ean: string }, AssetFilters | undefined, unknown>(
+      {
+        path: `${ASSET_MANAGER_API_BASE}/assets/{ean}`,
+        validate: {
+          params: schema.object({
+            ean: schema.string(),
+          }),
+        },
+      },
+      async (context, req, res) => {
+        const { ean } = req.params;
+        const esClient = await getEsClientFromContext(context);
+
+        try {
+          const asset = await getAssetByEan({ esClient, ean });
+
+          return res.ok({ body: { asset } });
+        } catch (error: unknown) {
+          debug('error looking up asset by ean', error);
+          return res.customError({ statusCode: 500 });
+        }
+      }
+    );
+
+    return {};
+  }
+
+  public start() {}
+
+  public stop() {}
+}
