06472050547c210817e8a92da0c147ce43c83b32
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-02-27 15:40:29 +0100

    tests(infra): add tests for hosts view alerts

--- x-pack/test/functional/apps/infra/hosts_view.ts
+++ x-pack/test/functional/apps/infra/hosts_view.ts
@@ -6,6 +6,8 @@
  */
 
 import expect from '@kbn/expect';
+import { enableInfrastructureHostsView } from '@kbn/observability-plugin/common';
+import { ALERT_STATUS_ACTIVE, ALERT_STATUS_RECOVERED } from '@kbn/rule-data-utils';
 import moment from 'moment';
 import { FtrProviderContext } from '../../ftr_provider_context';
 import { DATES, HOSTS_LINK_LOCAL_STORAGE_KEY } from './constants';
@@ -29,6 +31,8 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   ]);
 
   // Helpers
+  const setHostViewEnabled = (value: boolean = true) =>
+    kibanaServer.uiSettings.update({ [enableInfrastructureHostsView]: value });
 
   const loginWithReadOnlyUserAndNavigateToInfra = async () => {
     await security.role.create('global_hosts_read_privileges_role', {
@@ -78,16 +82,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     await esArchiver.load('x-pack/test/functional/es_archives/infra/metrics_and_logs');
     await browser.removeLocalStorageItem(HOSTS_LINK_LOCAL_STORAGE_KEY);
     await pageObjects.common.navigateToApp('infraOps');
-    await pageObjects.common.navigateToUrl('management', 'kibana/settings', {
-      basePath: `/s/default`,
-      ensureCurrentUrl: false,
-      shouldLoginIfPrompted: false,
-      shouldUseHashForSubUrl: false,
-    });
-    await pageObjects.settings.toggleAdvancedSettingCheckbox(
-      'observability:enableInfrastructureHostsView',
-      false
-    );
+    await setHostViewEnabled(false);
     return esArchiver.unload('x-pack/test/functional/es_archives/infra/metrics_and_logs');
   };
 
@@ -111,6 +106,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     describe('shows hosts view landing page for admin', () => {
       before(async () => {
         await esArchiver.load('x-pack/test/functional/es_archives/infra/metrics_and_logs');
+        await setHostViewEnabled(false);
         await pageObjects.common.navigateToApp('infraOps');
         await pageObjects.infraHome.clickDismissKubernetesTourButton();
         await pageObjects.infraHostsView.clickTryHostViewBadge();
@@ -250,11 +246,17 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     });
 
     describe('Alerts Tab', () => {
+      const observability = getService('observability');
       const testSubjects = getService('testSubjects');
       const retry = getService('retry');
-      const observability = getService('observability');
+
+      const ACTIVE_ALERTS = 6;
+      const RECOVERED_ALERTS = 4;
+      const ALL_ALERTS = ACTIVE_ALERTS + RECOVERED_ALERTS;
+      const COLUMNS = 5;
 
       before(async () => {
+        await esArchiver.load('x-pack/test/functional/es_archives/infra/alerts');
         await navigateAndEnableHostView();
         await loginWithReadOnlyUserAndNavigateToInfra();
         await browser.removeLocalStorageItem(HOSTS_LINK_LOCAL_STORAGE_KEY);
@@ -263,267 +265,62 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
           START_DATE.format(timepickerFormat),
           END_DATE.format(timepickerFormat)
         );
-        await esArchiver.load('x-pack/test/functional/es_archives/observability/alerts');
+        await pageObjects.infraHostsView.visitAlertTab();
       });
 
       after(async () => {
-        await esArchiver.unload('x-pack/test/functional/es_archives/observability/alerts');
+        await esArchiver.unload('x-pack/test/functional/es_archives/infra/alerts');
       });
 
       it('should correctly load the Alerts tab section when clicking on it', async () => {
-        const alertsTab = await pageObjects.infraHostsView.getAlertsTab();
-        await alertsTab.click();
-
-        // Assert section is rendered
         testSubjects.existOrFail('hostsView-alerts');
       });
 
-      // describe('Alerts table', () => {
-      //   before(async () => {
-      //     await esArchiver.load('x-pack/test/functional/es_archives/infra/simple_logs');
-      //     await observability.alerts.common.navigateToTimeWithData();
-      //   });
+      it('should correctly render a badge with the active alerts count', async () => {
+        const alertsCountBadge = await pageObjects.infraHostsView.getAlertsTabCountBadge();
+        const alertsCount = await alertsCountBadge.getVisibleText();
 
-      //   after(async () => {
-      //     await esArchiver.unload('x-pack/test/functional/es_archives/infra/simple_logs');
-      //   });
+        expect(alertsCount).to.be('6');
+      });
 
-      //   it('Renders the table', async () => {
-      //     await observability.alerts.common.getTableOrFail();
-      //   });
+      describe('#FilterButtonGroup', () => {
+        it('can be filtered to only show "active" alerts using the filter button', async () => {
+          await pageObjects.infraHostsView.setAlertStatusFilter(ALERT_STATUS_ACTIVE);
+          await retry.try(async () => {
+            const tableRows = await observability.alerts.common.getTableCellsInRows();
+            expect(tableRows.length).to.be(ACTIVE_ALERTS);
+          });
+        });
 
-      //   it('Renders the correct number of cells', async () => {
-      //     await retry.try(async () => {
-      //       const cells = await observability.alerts.common.getTableCells();
-      //       expect(cells.length).to.be(TOTAL_ALERTS_CELL_COUNT);
-      //     });
-      //   });
+        it('can be filtered to only show "recovered" alerts using the filter button', async () => {
+          await pageObjects.infraHostsView.setAlertStatusFilter(ALERT_STATUS_RECOVERED);
+          await retry.try(async () => {
+            const tableRows = await observability.alerts.common.getTableCellsInRows();
+            expect(tableRows.length).to.be(RECOVERED_ALERTS);
+          });
+        });
 
-      //   describe('Filtering', () => {
-      //     afterEach(async () => {
-      //       await observability.alerts.common.clearQueryBar();
-      //     });
+        it('can be filtered to only show "all" alerts using the filter button', async () => {
+          await pageObjects.infraHostsView.setAlertStatusFilter();
+          await retry.try(async () => {
+            const tableRows = await observability.alerts.common.getTableCellsInRows();
+            expect(tableRows.length).to.be(ALL_ALERTS);
+          });
+        });
+      });
 
-      //     after(async () => {
-      //       // NOTE: We do this as the query bar takes the place of the datepicker when it is in focus, so we'll reset
-      //       // back to default.
-      //       await observability.alerts.common.submitQuery('');
-      //     });
+      describe('#AlertsTable', () => {
+        it('should correctly render', async () => {
+          await observability.alerts.common.getTableOrFail();
+        });
 
-      //     it('Autocompletion works', async () => {
-      //       await observability.alerts.common.typeInQueryBar('kibana.alert.s');
-      //       await testSubjects.existOrFail('autocompleteSuggestion-field-kibana.alert.start-');
-      //       await testSubjects.existOrFail('autocompleteSuggestion-field-kibana.alert.status-');
-      //     });
-
-      //     it('Invalid input should not break the page', async () => {
-      //       await observability.alerts.common.submitQuery('""""');
-      //       await testSubjects.existOrFail('errorToastMessage');
-      //       // Page should not go blank with invalid input
-      //       await testSubjects.existOrFail('alertsPageWithData');
-      //     });
-
-      //     it('Applies filters correctly', async () => {
-      //       await observability.alerts.common.submitQuery('kibana.alert.status: recovered');
-      //       await retry.try(async () => {
-      //         const cells = await observability.alerts.common.getTableCells();
-      //         expect(cells.length).to.be(RECOVERED_ALERTS_CELL_COUNT);
-      //       });
-      //     });
-
-      //     it('Displays a no data state when filters produce zero results', async () => {
-      //       await observability.alerts.common.submitQuery('kibana.alert.consumer: uptime');
-      //       await observability.alerts.common.getNoDataStateOrFail();
-      //     });
-      //   });
-
-      //   describe('Date selection', () => {
-      //     after(async () => {
-      //       await observability.alerts.common.navigateToTimeWithData();
-      //     });
-
-      //     it('Correctly applies date picker selections', async () => {
-      //       await retry.try(async () => {
-      //         await (await testSubjects.find('superDatePickerToggleQuickMenuButton')).click();
-      //         // We shouldn't expect any data for the last 15 minutes
-      //         await (
-      //           await testSubjects.find('superDatePickerCommonlyUsed_Last_15 minutes')
-      //         ).click();
-      //         await observability.alerts.common.getNoDataStateOrFail();
-      //       });
-      //     });
-      //   });
-
-      //   describe('Flyout', () => {
-      //     it('Can be opened', async () => {
-      //       await observability.alerts.common.openAlertsFlyout();
-      //       await observability.alerts.common.getAlertsFlyoutOrFail();
-      //     });
-
-      //     it('Can be closed', async () => {
-      //       await observability.alerts.common.closeAlertsFlyout();
-      //       await testSubjects.missingOrFail('alertsFlyout');
-      //     });
-
-      //     describe('When open', async () => {
-      //       before(async () => {
-      //         await observability.alerts.common.openAlertsFlyout();
-      //       });
-
-      //       after(async () => {
-      //         await observability.alerts.common.closeAlertsFlyout();
-      //       });
-
-      //       it('Displays the correct title', async () => {
-      //         await retry.try(async () => {
-      //           const titleText = await (
-      //             await observability.alerts.common.getAlertsFlyoutTitle()
-      //           ).getVisibleText();
-      //           expect(titleText).to.contain('APM Failed Transaction Rate (one)');
-      //         });
-      //       });
-
-      //       it('Displays the correct content', async () => {
-      //         const flyoutTitles =
-      //           await observability.alerts.common.getAlertsFlyoutDescriptionListTitles();
-      //         const flyoutDescriptions =
-      //           await observability.alerts.common.getAlertsFlyoutDescriptionListDescriptions();
-
-      //         const expectedTitles = [
-      //           'Status',
-      //           'Started at',
-      //           'Last updated',
-      //           'Duration',
-      //           'Expected value',
-      //           'Actual value',
-      //           'Rule type',
-      //         ];
-      //         const expectedDescriptions = [
-      //           'Active',
-      //           'Oct 19, 2021 @ 15:00:41.555',
-      //           'Oct 19, 2021 @ 15:20:38.749',
-      //           '20 minutes',
-      //           '5.0%',
-      //           '31%',
-      //           'Failed transaction rate threshold',
-      //         ];
-
-      //         await asyncForEach(flyoutTitles, async (title, index) => {
-      //           expect(await title.getVisibleText()).to.be(expectedTitles[index]);
-      //         });
-
-      //         await asyncForEach(flyoutDescriptions, async (description, index) => {
-      //           expect(await description.getVisibleText()).to.be(expectedDescriptions[index]);
-      //         });
-      //       });
-
-      //       it('Displays a View in App button', async () => {
-      //         await observability.alerts.common.getAlertsFlyoutViewInAppButtonOrFail();
-      //       });
-
-      //       it('Displays a View rule details link', async () => {
-      //         await observability.alerts.common.getAlertsFlyoutViewRuleDetailsLinkOrFail();
-      //       });
-      //     });
-      //   });
-
-      //   describe.skip('Cell actions', () => {
-      //     beforeEach(async () => {
-      //       await retry.try(async () => {
-      //         const cells = await observability.alerts.common.getTableCells();
-      //         const alertStatusCell = cells[2];
-      //         await alertStatusCell.moveMouseTo();
-      //         await retry.waitFor(
-      //           'cell actions visible',
-      //           async () => await observability.alerts.common.filterForValueButtonExists()
-      //         );
-      //       });
-      //     });
-
-      //     afterEach(async () => {
-      //       await observability.alerts.common.clearQueryBar();
-      //       // Reset the query bar by hiding the dropdown
-      //       await observability.alerts.common.submitQuery('');
-      //     });
-
-      //     it.skip('Filter for value works', async () => {
-      //       await (await observability.alerts.common.getFilterForValueButton()).click();
-      //       const queryBarValue = await (
-      //         await observability.alerts.common.getQueryBar()
-      //       ).getAttribute('value');
-      //       expect(queryBarValue).to.be('kibana.alert.status: "active"');
-      //       // Wait for request
-      //       await retry.try(async () => {
-      //         const cells = await observability.alerts.common.getTableCells();
-      //         expect(cells.length).to.be(ACTIVE_ALERTS_CELL_COUNT);
-      //       });
-      //     });
-      //   });
-
-      //   describe('Actions Button', () => {
-      //     it('Opens rule details page when click on "View Rule Details"', async () => {
-      //       const actionsButton = await observability.alerts.common.getActionsButtonByIndex(0);
-      //       await actionsButton.click();
-      //       await observability.alerts.common.viewRuleDetailsButtonClick();
-      //       expect(
-      //         await (
-      //           await find.byCssSelector('[data-test-subj="breadcrumb first"]')
-      //         ).getVisibleText()
-      //       ).to.eql('Observability');
-      //     });
-      //   });
-
-      //   describe.skip('Bulk Actions', () => {
-      //     before(async () => {
-      //       await security.testUser.setRoles(['global_alerts_logs_all_else_read']);
-      //       await observability.alerts.common.submitQuery('kibana.alert.status: "active"');
-      //     });
-      //     after(async () => {
-      //       await observability.alerts.common.submitQuery('');
-      //       await security.testUser.restoreDefaults();
-      //     });
-
-      //     it('Only logs alert should be enable for bulk actions', async () => {
-      //       const disabledCheckBoxes =
-      //         await observability.alerts.common.getAllDisabledCheckBoxInTable();
-      //       const enabledCheckBoxes =
-      //         await observability.alerts.common.getAllEnabledCheckBoxInTable();
-
-      //       expect(disabledCheckBoxes.length).to.eql(DISABLED_ALERTS_CHECKBOX);
-      //       expect(enabledCheckBoxes.length).to.eql(ENABLED_ALERTS_CHECKBOX);
-      //     });
-
-      //     it('validate formatting of the bulk actions button', async () => {
-      //       const selectAll = await testSubjects.find('select-all-events');
-      //       await selectAll.click();
-      //       const bulkActionsButton = await testSubjects.find('selectedShowBulkActionsButton');
-      //       expect(await bulkActionsButton.getVisibleText()).to.be('Selected 4 alerts');
-      //       await selectAll.click();
-      //     });
-
-      //     it('validate functionality of the bulk actions button', async () => {
-      //       const selectAll = await testSubjects.find('select-all-events');
-      //       await selectAll.click();
-
-      //       const bulkActionsButton = await testSubjects.find('selectedShowBulkActionsButton');
-      //       await bulkActionsButton.click();
-
-      //       const bulkActionsAcknowledgedAlertStatusButton = await testSubjects.find(
-      //         'acknowledged-alert-status'
-      //       );
-      //       await bulkActionsAcknowledgedAlertStatusButton.click();
-      //       await observability.alerts.common.submitQuery(
-      //         'kibana.alert.workflow_status : "acknowledged"'
-      //       );
-
-      //       await retry.try(async () => {
-      //         const enabledCheckBoxes =
-      //           await observability.alerts.common.getAllEnabledCheckBoxInTable();
-      //         expect(enabledCheckBoxes.length).to.eql(1);
-      //       });
-      //     });
-      //   });
-      // });
+        it('should renders the correct number of cells', async () => {
+          await retry.try(async () => {
+            const cells = await observability.alerts.common.getTableCells();
+            expect(cells.length).to.be(ALL_ALERTS * COLUMNS);
+          });
+        });
+      });
     });
   });
 };
