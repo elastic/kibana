c5808d551bf1ae989c7773f720a8494417388866
Dima Arnautov <dmitrii.arnautov@elastic.co>
2023-02-28 13:15:46 +0100

    update service, extract ML related props

--- x-pack/plugins/ml/server/models/json_schema_service/json_schema_service.ts
+++ x-pack/plugins/ml/server/models/json_schema_service/json_schema_service.ts
@@ -7,15 +7,32 @@
 
 import Fs from 'fs';
 import Path from 'path';
+import { SupportedPath } from '../../routes/schemas/json_schema_schema';
 import { jsonSchemaOverrides } from './schema_overrides';
 import { type PropertyDefinition } from './types';
 
+const supportedEndpoints = [
+  {
+    path: '/_ml/anomaly_detectors/{job_id}' as const,
+    method: 'put',
+  },
+  {
+    path: '/_ml/datafeeds/{datafeed_id}' as const,
+    method: 'put',
+  },
+];
+
+/**
+ *
+ */
 export class JsonSchemaService {
   /**
    * Dictionary with the schema components
    * @private
    */
   private _schemaComponents: Record<string, PropertyDefinition> = {};
+
+  private _requiredComponents: Set<string> = new Set<string>();
 
   /**
    * Extracts properties definition
@@ -24,8 +41,10 @@ export class JsonSchemaService {
   private extractProperties(propertyDef: PropertyDefinition): void {
     for (const key in propertyDef) {
       if (propertyDef.hasOwnProperty(key)) {
+        const propValue = propertyDef[key as keyof PropertyDefinition]!;
+
         if (key === '$ref') {
-          const comp = propertyDef[key].split('/');
+          const comp = (propValue as string).split('/');
           const refKey = comp[comp.length - 1];
 
           delete propertyDef.$ref;
@@ -34,6 +53,8 @@ export class JsonSchemaService {
           if (!refKey.startsWith('Ml_Types_')) return;
 
           const schemaComponent = this._schemaComponents[refKey];
+
+          this._requiredComponents.add(refKey);
 
           for (const k in schemaComponent) {
             if (schemaComponent.hasOwnProperty(k)) {
@@ -44,20 +65,20 @@ export class JsonSchemaService {
           this.extractProperties(propertyDef);
         }
 
-        if (Array.isArray(propertyDef[key])) {
-          propertyDef[key].forEach((v) => {
+        if (Array.isArray(propValue)) {
+          propValue.forEach((v) => {
             if (typeof v === 'object') {
               this.extractProperties(v);
             }
           });
-        } else if (typeof propertyDef[key] === 'object') {
-          this.extractProperties(propertyDef[key]);
+        } else if (typeof propValue === 'object') {
+          this.extractProperties(propValue as PropertyDefinition);
         }
       }
     }
   }
 
-  private applyOverrides(path: string, schema: PropertyDefinition): PropertyDefinition {
+  private applyOverrides(path: SupportedPath, schema: PropertyDefinition): PropertyDefinition {
     const overrides = jsonSchemaOverrides[path];
     return {
       ...schema,
@@ -74,9 +95,9 @@ export class JsonSchemaService {
    * @param path
    * @param method
    */
-  public async extractSchema(path: string, method: string) {
+  public async extractSchema(path: SupportedPath, method: string) {
     const fileContent = JSON.parse(
-      Fs.readFileSync(Path.resolve(__dirname, 'openapi.json'), 'utf8')
+      Fs.readFileSync(Path.resolve(__dirname, 'openapi_source.json'), 'utf8')
     );
 
     const definition = fileContent.paths[path][method];
@@ -92,5 +113,43 @@ export class JsonSchemaService {
     this.extractProperties(bodySchema);
 
     return this.applyOverrides(path, bodySchema);
+  }
+
+  /**
+   * Generates openapi filed, removing redundant content.
+   */
+  public async generateSchemaFile() {
+    const schema = JSON.parse(
+      Fs.readFileSync(Path.resolve(__dirname, 'openapi_source.json'), 'utf8')
+    );
+
+    supportedEndpoints.forEach((e) => {
+      // need to extract schema in order to keep required components
+      this.extractSchema(e.path, e.method);
+    });
+
+    for (const pathName in schema.paths) {
+      if (!schema.paths.hasOwnProperty(pathName)) continue;
+
+      const supportedEndpoint = supportedEndpoints.find((v) => v.path === pathName);
+      if (supportedEndpoint) {
+        for (const methodName in schema.paths[pathName]) {
+          if (methodName !== supportedEndpoint.method) {
+            delete schema.paths[pathName][methodName];
+          }
+        }
+      } else {
+        delete schema.paths[pathName];
+      }
+    }
+
+    const components = schema.components.schemas;
+    for (const componentName in components) {
+      if (!this._requiredComponents.has(componentName)) {
+        delete components[componentName];
+      }
+    }
+
+    Fs.writeFileSync(Path.resolve(__dirname, 'openapi.json'), JSON.stringify(schema, null, 2));
   }
 }
