f17642d984ab9b2bd1e8a0843d1ae091a54fd803
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2023-02-28 16:53:35 +0000

    runs migrations concurrently

--- x-pack/plugins/alerting/server/rules_client/lib/migrate_rules_hook.ts
+++ x-pack/plugins/alerting/server/rules_client/lib/migrate_rules_hook.ts
@@ -5,21 +5,41 @@
  * 2.0.
  */
 
+import pMap from 'p-map';
 import type { MigrateRules } from '../..';
+import type { SanitizedRule } from '../../types';
 
+interface MigrateExecutor {
+  rules: SanitizedRule[];
+  executor?: MigrateRules;
+}
+
+/**
+ * runs concurrently migrations for rules in batches based on their rule type
+ * 1. collects rules by rule types in batches
+ * 2. runs in parallel migrateRules hook for each rule type using pMap
+ * As migrateRules takes multiple rules as an argument, it allows to avoid single request for each rule
+ */
 export const migrateRulesHook: MigrateRules = async ({ rules }, context) => {
-  const migratedRules = [];
-
-  for (let i = 0; i < rules.length; i++) {
-    const rule = rules[i];
+  const ruleTypeMap = new Map<string, MigrateExecutor>();
+  rules.forEach((rule) => {
     const ruleType = context.ruleTypeRegistry.get(rule.alertTypeId);
+    if (ruleTypeMap.has(ruleType.id)) {
+      ruleTypeMap.get(ruleType.id)?.rules.push(rule);
+    } else {
+      ruleTypeMap.set(ruleType.id, { executor: ruleType?.migrateRules, rules: [rule] });
+    }
+  });
 
-    const migratedRule = ruleType?.migrateRules
-      ? (await ruleType?.migrateRules({ rules: [rule] }, context))?.[0]
-      : rule;
+  const migrated = await pMap(
+    Array.from(ruleTypeMap.values()),
+    async ({ executor, rules: rulesToMigrate }) => {
+      return executor ? executor({ rules: rulesToMigrate }, context) : rulesToMigrate;
+    },
+    {
+      concurrency: 5,
+    }
+  );
 
-    migratedRules.push(migratedRule);
-  }
-
-  return migratedRules;
+  return migrated.flat();
 };
