a86cb40d9b95ae4bedba095b83a8ba50287fec18
Sander Philipse <sander.philipse@elastic.co>
2023-02-28 17:51:13 +0100

    Fix i18n

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/crawler/no_connector_record.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/crawler/no_connector_record.tsx
@@ -81,7 +81,7 @@ export const NoConnectorRecord: React.FC = () => {
             onClick={() => openDeleteModal(indexName)}
           >
             {i18n.translate(
-              'xpack.enterpriseSearch.content.searchIndex.noCrawlerConnectorFound.recreateConnectorRecord',
+              'xpack.enterpriseSearch.content.searchIndex.noCrawlerConnectorFound.deleteIndex',
               {
                 defaultMessage: 'Delete index',
               }
