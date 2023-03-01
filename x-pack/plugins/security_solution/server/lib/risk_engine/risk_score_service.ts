35ab7e3980b74eb7467ffd8dcbe173512456d94f
Ryland Herrick <ryalnd@gmail.com>
2023-02-28 22:45:51 -0600

    Fleshing out the logic of calculating risk scores
    The API now calculates and returns scores if there's data. The happy
    path works but I can't guarantee much else. Most parameters are not
    respected.

---
+++ x-pack/plugins/security_solution/server/lib/risk_engine/risk_score_service.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { ElasticsearchClient } from '@kbn/core/server';
+import type { GetScoresParams, GetScoresResponse } from './types';
+import { calculateRiskScores } from './calculate_risk_scores';
+
+export interface RiskScoreService {
+  getScores: (params: GetScoresParams) => Promise<GetScoresResponse>;
+}
+
+export const buildRiskScoreService = ({
+  esClient,
+}: {
+  esClient: ElasticsearchClient;
+}): RiskScoreService => ({
+  getScores: (params) => calculateRiskScores({ ...params, esClient }),
+});
---
+++ x-pack/plugins/security_solution/server/lib/risk_engine/types.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { SearchResponse } from '@elastic/elasticsearch/lib/api/types';
+import type { DataViewId } from '../../../common/detection_engine/rule_schema';
+
+export type IdentifierType = 'user' | 'host';
+
+export interface GetScoresParams {
+  dataViewId?: DataViewId;
+  filters?: unknown[];
+  range: { start: string; end: string };
+  identifierType?: IdentifierType;
+}
+
+export interface GetScoresResponse {
+  hosts: RiskScores;
+  users: RiskScores;
+}
+
+export interface RiskScore {
+  '@timestamp': string;
+  identifierField: string;
+  identifierValue: string;
+  // calculatedLevel: string;
+  // calculatedScore: number;
+  calculatedScoreNorm: number;
+}
+
+export type RiskScores = RiskScore[];
+
+export interface CalculateRiskScoreAggregations {
+  users: {
+    buckets: RiskScoreBucket[];
+  };
+  hosts: {
+    buckets: RiskScoreBucket[];
+  };
+}
+
+interface RiskScoreBucket {
+  key: string;
+  doc_count: number;
+  normalized_score: number;
+  riskiest_inputs: SearchResponse<{ _index: string; _id: string; sort: [number] }>;
+}
