736a91a2e9f0a1a156cc4af391ef4b85b41ec35a
ppisljar <peter.pisljar@elastic.co>
2023-03-01 12:55:04 +0100

    sample data change

---
+++ src/plugins/home/server/services/sample_data/data_sets/logs_tsdb/index.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import path from 'path';
+import { i18n } from '@kbn/i18n';
+import { getSavedObjects } from './saved_objects';
+import { fieldMappings } from './field_mappings';
+import { SampleDatasetSchema } from '../../lib/sample_dataset_registry_types';
+
+const logsName = i18n.translate('home.sampleData.logsSpecTitle', {
+  defaultMessage: 'Sample web logs (TSDB)',
+});
+const logsDescription = i18n.translate('home.sampleData.logsSpecDescription', {
+  defaultMessage: 'Sample data, visualizations, and dashboards for monitoring web logs.',
+});
+
+export const GLOBE_ICON_PATH = '/plugins/home/assets/sample_data_resources/logs/icon.svg';
+export const logsTSDBSpecProvider = function (): SampleDatasetSchema {
+  return {
+    id: 'logstsdb',
+    name: logsName,
+    description: logsDescription,
+    previewImagePath: '/plugins/home/assets/sample_data_resources/logs/dashboard.webp',
+    darkPreviewImagePath: '/plugins/home/assets/sample_data_resources/logs/dashboard_dark.webp',
+    overviewDashboard: 'edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b',
+    defaultIndex: '90943e30-9a47-11e8-b64d-95841ca0b247',
+    savedObjects: getSavedObjects(),
+    dataIndices: [
+      {
+        id: 'logstsdb',
+        dataPath: path.join(__dirname, './logs.json.gz'),
+        fields: fieldMappings,
+        timeFields: ['@timestamp', 'utc_time'],
+        currentTimeMarker: '2018-08-01T00:00:00',
+        preserveDayOfWeekTimeOfDay: true,
+        indexSettings: {
+          number_of_shards: 1,
+          auto_expand_replicas: '0-1',
+          mode: 'time_series',
+          routing_path: 'request',
+          'time_series.start_time': '2022-11-01T00:00:00.000000Z',
+          'time_series.end_time': '2023-05-01T00:00:00.000000Z',
+        },
+      },
+    ],
+    status: 'not_installed',
+    iconPath: GLOBE_ICON_PATH,
+  };
+};
