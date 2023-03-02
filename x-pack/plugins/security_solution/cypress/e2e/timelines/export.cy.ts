f8bf90d86642d1e5482e9d630af6b12a90f11437
christineweng <weng67osu@gmail.com>
2023-03-02 16:20:45 -0600

    address comments

--- x-pack/plugins/security_solution/cypress/e2e/timelines/export.cy.ts
+++ x-pack/plugins/security_solution/cypress/e2e/timelines/export.cy.ts
@@ -5,28 +5,25 @@
  * 2.0.
  */
 
-import { exportTimeline, selectAllTimelines, exportSelectedTimelines } from '../../tasks/timelines';
+import {
+  exportTimeline,
+  selectTimeline,
+  selectAllTimelines,
+  exportSelectedTimelines,
+} from '../../tasks/timelines';
 import { login, visitWithoutDateRange } from '../../tasks/login';
 
 import { TIMELINES_URL } from '../../urls/navigation';
 import { TOASTER } from '../../screens/alerts_detection_rules';
-import {
-  TIMELINE_CHECKBOX,
-  TIMELINE_ITEM_ACTION_BTN,
-  EXPORT_TIMELINE,
-} from '../../screens/timelines';
+import { TIMELINE_CHECKBOX } from '../../screens/timelines';
 import { createTimeline } from '../../tasks/api_calls/timelines';
 import { expectedExportedTimeline, getTimeline } from '../../objects/timeline';
-import { cleanKibana, deleteTimelines } from '../../tasks/common';
+import { cleanKibana } from '../../tasks/common';
 
 describe('Export timelines', () => {
   before(() => {
     cleanKibana();
     login();
-  });
-
-  beforeEach(() => {
-    deleteTimelines();
     cy.intercept({
       method: 'POST',
       path: '/api/timeline/_export?file_name=timelines_export.ndjson',
@@ -42,26 +39,28 @@ describe('Export timelines', () => {
     visitWithoutDateRange(TIMELINES_URL);
   });
 
-  it('Exports a custom timeline using timeline actions', function () {
-    cy.get(TIMELINE_ITEM_ACTION_BTN(this.timelineId1)).click();
-    cy.get(EXPORT_TIMELINE).click();
+  it('Exports custom timeline(s)', function () {
+    cy.log('Export a custom timeline via timeline actions');
 
-    cy.wait('@export').then(({ response }) => {
-      cy.wrap(response?.statusCode).should('eql', 200);
-      cy.wrap(response?.body).should('eql', expectedExportedTimeline(this.timelineResponse1));
-    });
-  });
-
-  it('Exports a custom timeline using bulk action', function () {
     exportTimeline(this.timelineId1);
-
     cy.wait('@export').then(({ response }) => {
       cy.wrap(response?.statusCode).should('eql', 200);
       cy.wrap(response?.body).should('eql', expectedExportedTimeline(this.timelineResponse1));
     });
-  });
+    cy.get('[data-test-subj="toastCloseButton"]').click();
 
-  it('Bulk export all created timeline', function () {
+    cy.log('Export a custom timeline via bulk actions');
+
+    selectTimeline(this.timelineId1);
+    exportSelectedTimelines();
+    cy.wait('@export').then(({ response }) => {
+      cy.wrap(response?.statusCode).should('eql', 200);
+      cy.wrap(response?.body).should('eql', expectedExportedTimeline(this.timelineResponse1));
+    });
+    cy.get('[data-test-subj="toastCloseButton"]').click();
+
+    cy.log('Export all custom timelines via bulk actions');
+
     cy.get(TIMELINE_CHECKBOX(this.timelineId1)).should('exist'); // wait for all timelines to be loaded
     selectAllTimelines();
     exportSelectedTimelines();
