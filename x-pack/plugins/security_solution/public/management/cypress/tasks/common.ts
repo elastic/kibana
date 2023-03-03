d92957b12de14517ec291d31048f563cb7e352b5
Paul Tavares <paul.tavares@elastic.co>
2023-03-02 19:39:32 -0500

    new common tasks - createCyChainable() and toPromise()

--- x-pack/plugins/security_solution/public/management/cypress/tasks/common.ts
+++ x-pack/plugins/security_solution/public/management/cypress/tasks/common.ts
@@ -5,6 +5,17 @@
  * 2.0.
  */
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import type { UnwrapPromise } from '@kbn/infra-plugin/common/utility_types';
+import type { ReturnTypeFromChainable } from '../types';
+
+type AnyFunction = () => any;
+
+type ReturnTypeFromAnyFunction<Fn extends AnyFunction> = Fn extends () => Promise<any>
+  ? UnwrapPromise<ReturnType<Fn>>
+  : ReturnType<Fn>;
+
 export const ENDPOINT_VM_NAME = 'ENDPOINT_VM_NAME';
 
 export const API_AUTH = Object.freeze({
@@ -22,3 +33,33 @@ export const request = <T = unknown>(
     headers: API_HEADERS,
     ...options,
   });
+
+/**
+ * Wraps a function into a `Cypress.Chainable` that resolves with the value return by that function
+ * @param fn
+ */
+export const createCyChainable = <T extends AnyFunction = AnyFunction>(
+  fn: T
+): Cypress.Chainable<ReturnTypeFromAnyFunction<T>> => {
+  return cy.wrap<ReturnTypeFromAnyFunction<T>>(fn());
+};
+
+/**
+ * Takes a Cypress Chainable and convert it to a Promise that resolves to the chainable's value
+ * @param chain
+ */
+export const toPromise = <C extends Cypress.Chainable<any> = Cypress.Chainable<any>>(
+  chain: C
+): Promise<ReturnTypeFromChainable<C>> => {
+  return new Promise((resolve, reject) => {
+    cy.on('fail', reject);
+
+    try {
+      chain.then(resolve);
+      cy.off('fail', reject);
+    } catch (error) {
+      cy.off('fail', reject);
+      reject(error);
+    }
+  });
+};
