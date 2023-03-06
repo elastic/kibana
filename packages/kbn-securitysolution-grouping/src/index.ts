be7f9261662964e97b685ec606c7db34edc10592
Steph Milovic <stephanie.milovic@elastic.co>
2023-03-06 08:14:54 -0700

    fixes

--- packages/kbn-securitysolution-grouping/src/index.ts
+++ packages/kbn-securitysolution-grouping/src/index.ts
@@ -10,3 +10,4 @@ export * from './components';
 export * from './containers';
 export * from './helpers';
 export * from './hooks/use_grouping';
+export * from './hooks/types';
--- x-pack/plugins/security_solution/public/detections/components/alerts_table/grouping_settings/index.tsx
+++ x-pack/plugins/security_solution/public/detections/components/alerts_table/grouping_settings/index.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type { GroupOption } from '../../../../common/containers/grouping/hooks/types';
+import type { GroupOption } from '@kbn/securitysolution-grouping';
 import { TableId } from '../../../../../common/types';
 import * as i18n from '../translations';
 
