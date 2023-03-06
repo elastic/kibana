350cee0bc355382c4b8d1cdee57ca3ea7caef4a3
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-03-06 19:24:35 +0100

    Excluding the android agent from general settings

--- x-pack/plugins/apm/common/agent_configuration/setting_definitions/general_settings.ts
+++ x-pack/plugins/apm/common/agent_configuration/setting_definitions/general_settings.ts
@@ -28,7 +28,7 @@ export const generalSettings: RawSettingDefinition[] = [
           'The maximum total compressed size of the request body which is sent to the APM Server intake api via a chunked encoding (HTTP streaming).\nNote that a small overshoot is possible.\n\nAllowed byte units are `b`, `kb` and `mb`. `1kb` is equal to `1024b`.',
       }
     ),
-    excludeAgents: ['js-base', 'rum-js', 'dotnet', 'go', 'nodejs', 'php'],
+    excludeAgents: ['js-base', 'rum-js', 'dotnet', 'go', 'nodejs', 'php', 'android/java'],
   },
 
   // API Request Time
@@ -46,7 +46,7 @@ export const generalSettings: RawSettingDefinition[] = [
           "Maximum time to keep an HTTP request to the APM Server open for.\n\nNOTE: This value has to be lower than the APM Server's `read_timeout` setting.",
       }
     ),
-    excludeAgents: ['js-base', 'rum-js', 'dotnet', 'go', 'nodejs', 'php'],
+    excludeAgents: ['js-base', 'rum-js', 'dotnet', 'go', 'nodejs', 'php', 'android/java'],
   },
 
   // Capture body
@@ -71,7 +71,7 @@ export const generalSettings: RawSettingDefinition[] = [
       { text: 'transactions', value: 'transactions' },
       { text: 'all', value: 'all' },
     ],
-    excludeAgents: ['js-base', 'rum-js', 'php'],
+    excludeAgents: ['js-base', 'rum-js', 'php', 'android/java'],
   },
 
   {
@@ -112,7 +112,7 @@ export const generalSettings: RawSettingDefinition[] = [
           'If set to `true`, the agent will capture HTTP request and response headers (including cookies), as well as message headers/properties when using messaging frameworks (like Kafka).\n\nNOTE: Setting this to `false` reduces network bandwidth, disk space and object allocations.',
       }
     ),
-    excludeAgents: ['js-base', 'rum-js', 'nodejs', 'php'],
+    excludeAgents: ['js-base', 'rum-js', 'nodejs', 'php', 'android/java'],
   },
 
   {
@@ -309,7 +309,7 @@ export const generalSettings: RawSettingDefinition[] = [
       defaultMessage:
         'When recording, the agent instruments incoming HTTP requests, tracks errors, and collects and sends metrics. When set to non-recording, the agent works as a noop, not collecting data and not communicating with the APM Server except for polling for updated configuration. As this is a reversible switch, agent threads are not being killed when set to non-recording, but they will be mostly idle in this state, so the overhead should be negligible. You can use this setting to dynamically control whether Elastic APM is enabled or disabled.',
     }),
-    excludeAgents: ['nodejs', 'rum-js', 'js-base'],
+    excludeAgents: ['nodejs', 'rum-js', 'js-base', 'android/java'],
   },
 
   // SERVER_TIMEOUT
@@ -408,7 +408,7 @@ export const generalSettings: RawSettingDefinition[] = [
           '(Deprecated, use `span_stack_trace_min_duration` instead!) In its default settings, the APM agent will collect a stack trace with every recorded span.\nWhile this is very helpful to find the exact place in your code that causes the span, collecting this stack trace does have some overhead. \nWhen setting this option to a negative value, like `-1ms`, stack traces will be collected for all spans. Setting it to a positive value, e.g. `5ms`, will limit stack trace collection to spans with durations equal to or longer than the given value, e.g. 5 milliseconds.\n\nTo disable stack trace collection for spans completely, set the value to `0ms`.',
       }
     ),
-    excludeAgents: ['js-base', 'rum-js', 'nodejs', 'php'],
+    excludeAgents: ['js-base', 'rum-js', 'nodejs', 'php', 'android/java'],
   },
 
   {
@@ -509,7 +509,7 @@ export const generalSettings: RawSettingDefinition[] = [
           'Limits the amount of spans that are recorded per transaction.',
       }
     ),
-    excludeAgents: ['js-base', 'rum-js'],
+    excludeAgents: ['js-base', 'rum-js', 'android/java'],
   },
 
   // Transaction sample rate
@@ -527,6 +527,7 @@ export const generalSettings: RawSettingDefinition[] = [
           'By default, the agent will sample every transaction (e.g. request to your service). To reduce overhead and storage requirements, you can set the sample rate to a value between 0.0 and 1.0. We still record overall time and the result for unsampled transactions, but not context information, labels, or spans.',
       }
     ),
+    excludeAgents: ['android/java'],
   },
 
   // Sanitize field names
