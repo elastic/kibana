f24bc786b27b3dedd108e1a33c9743d296e1dbea
christineweng <weng67osu@gmail.com>
2023-03-07 17:16:04 -0600

    hide type in alert by type chart

--- x-pack/plugins/security_solution/common/experimental_features.ts
+++ x-pack/plugins/security_solution/common/experimental_features.ts
@@ -91,6 +91,7 @@ export const allowedExperimentalValues = Object.freeze({
    * Enables top charts on Alerts Page
    */
   alertsPageChartsEnabled: true,
+  alertTypeEnabled: false,
   /**
    * Enables the new security flyout over the current alert details flyout
    */
