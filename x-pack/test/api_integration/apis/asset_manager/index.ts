60c8741a8e204b8af2209b602854b9967b28b0db
Jason Rhodes <jason.rhodes@elastic.co>
2023-03-07 15:29:56 -0500

    Adds basic endpoint and ES index template tests for enabled and disabled

--- x-pack/test/api_integration/apis/asset_manager/index.ts
+++ x-pack/test/api_integration/apis/asset_manager/index.ts
@@ -8,6 +8,6 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('Asset Manager API Endpoints', () => {
-    loadTestFile(require.resolve('./ping'));
+    loadTestFile(require.resolve('./basics'));
   });
 }
