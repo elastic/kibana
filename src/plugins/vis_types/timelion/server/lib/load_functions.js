elastic/kibana

commit 6bf11a7d3e9fefa464c2dab2699506421613e164

Clean up glob related dependencies (#138571)
* Clean up glob related dependencies

* wip

* update snapshots

* updates

* fix tests

* update package.json

* i18n

* fix tests

* onlyFiles: false

* preserve folders on **/* globs

* one more

* revert instance of onlyFiles: false

src/plugins/vis_types/timelion/server/lib/load_functions.js
@@ -7,7 +7,7 @@
  */
 
 import _ from 'lodash';
-import glob from 'glob';
+import globby from 'globby';
 import path from 'path';
 import processFunctionDefinition from './process_function_definition';
 
@@ -18,7 +18,7 @@ export default function (directory) {
 
   // Get a list of all files and use the filename as the object key
   const files = _.map(
-    glob
+    globby
       .sync(path.resolve(__dirname, '../' + directory + '/*.js'))
       .filter((filename) => !filename.includes('.test')),
     function (file) {
@@ -28,7 +28,9 @@ export default function (directory) {
   );
 
   // Get a list of all directories with an index.js, use the directory name as the key in the object
-  const directories = _.chain(glob.sync(path.resolve(__dirname, '../' + directory + '/*/index.js')))
+  const directories = _.chain(
+    globby.sync(path.resolve(__dirname, '../' + directory + '/*/index.js'))
+  )
     .map(function (file) {
       const parts = file.split('/');
       const name = parts[parts.length - 2];

packages/kbn-es/BUILD.bazel
@@ -44,7 +44,7 @@ RUNTIME_DEPS = [
   "@npm//del",
   "@npm//execa",
   "@npm//getopts",
-  "@npm//glob",
+  "@npm//globby",
   "@npm//node-fetch",
   "@npm//simple-git",
   "@npm//tree-kill",

packages/kbn-es/src/utils/find_most_recently_changed.test.ts
@@ -28,8 +28,11 @@ jest.mock('fs', () => ({
       };
     }
   }),
-  readdirSync: jest.fn().mockImplementation(() => {
-    return ['oldest.yml', 'newest.yml', 'middle.yml'];
+}));
+
+jest.mock('globby', () => ({
+  sync: jest.fn().mockImplementation(() => {
+    return ['/data/oldest.yml', '/data/newest.yml', '/data/middle.yml'];
   }),
 }));
 

packages/kbn-es/src/utils/find_most_recently_changed.ts
@@ -8,7 +8,7 @@
 
 import path from 'path';
 import fs from 'fs';
-import glob from 'glob';
+import globby from 'globby';
 
 /**
  *  Find the most recently modified file that matches the pattern pattern
@@ -20,8 +20,8 @@ export function findMostRecentlyChanged(pattern: string) {
 
   const ctime = (p: string) => fs.statSync(p).ctime.getTime();
 
-  return glob
-    .sync(pattern)
+  return globby
+    .sync(pattern, { onlyFiles: false })
     .sort((a, b) => ctime(a) - ctime(b))
     .pop();
 }

packages/kbn-spec-to-console/BUILD.bazel
@@ -28,7 +28,9 @@ NPM_MODULE_EXTRA_FILES = [
   "README.md",
 ]
 
-RUNTIME_DEPS = []
+RUNTIME_DEPS = [
+  "@npm//globby",
+]
 
 js_library(
   name = PKG_BASE_NAME,

packages/kbn-spec-to-console/bin/spec_to_console.js
@@ -9,7 +9,7 @@
 const fs = require('fs');
 const path = require('path');
 const program = require('commander');
-const glob = require('glob');
+const globby = require('globby');
 const chalk = require('chalk');
 
 const packageJSON = require('../package.json');
@@ -26,7 +26,7 @@ if (!program.glob) {
   process.exit(1);
 }
 
-const files = glob.sync(program.glob);
+const files = globby.sync(program.glob);
 const totalFilesCount = files.length;
 let convertedFilesCount = 0;
 

packages/kbn-telemetry-tools/BUILD.bazel
@@ -42,7 +42,7 @@ RUNTIME_DEPS = [
   "//packages/kbn-eslint-plugin-imports",
   "//packages/kbn-utility-types",
   "//packages/kbn-utils",
-  "@npm//glob",
+  "@npm//globby",
   "@npm//listr",
   "@npm//normalize-path",
 ]
@@ -52,8 +52,8 @@ TYPES_DEPS = [
   "//packages/kbn-eslint-plugin-imports:npm_module_types",
   "//packages/kbn-utility-types:npm_module_types",
   "//packages/kbn-utils:npm_module_types",
+  "@npm//globby",
   "@npm//tslib",
-  "@npm//@types/glob",
   "@npm//@types/jest",
   "@npm//@types/listr",
   "@npm//@types/lodash",

packages/kbn-telemetry-tools/src/tools/extract_collectors.ts
@@ -6,10 +6,10 @@
  * Side Public License, v 1.
  */
 
+import globby from 'globby';
 import * as ts from 'typescript';
 import * as path from 'path';
 import { parseUsageCollection } from './ts_parser';
-import { globAsync } from './utils';
 import { TelemetryRC } from './config';
 import { compilerHost } from './compiler_host';
 
@@ -17,21 +17,24 @@ export async function getProgramPaths({
   root,
   exclude,
 }: Pick<TelemetryRC, 'root' | 'exclude'>): Promise<string[]> {
-  const filePaths = await globAsync('**/*.ts', {
-    cwd: root,
-    ignore: [
-      '**/node_modules/**',
-      '**/*.test.*',
-      '**/*.mock.*',
-      '**/mocks.*',
-      '**/__fixture__/**',
-      '**/__tests__/**',
-      '**/public/**',
-      '**/dist/**',
-      '**/target/**',
-      '**/*.d.ts',
+  const filePaths = await globby(
+    [
+      '**/*.ts',
+      '!**/node_modules/**',
+      '!**/*.test.*',
+      '!**/*.mock.*',
+      '!**/mocks.*',
+      '!**/__fixture__/**',
+      '!**/__tests__/**',
+      '!**/public/**',
+      '!**/dist/**',
+      '!**/target/**',
+      '!**/*.d.ts',
     ],
-  });
+    {
+      cwd: root,
+    }
+  );
 
   if (filePaths.length === 0) {
     throw Error(`No files found in ${root}`);

commit 05ff6d3cc11f44a2b3528daced2691bcaad79def

[timelion] Fix glob path on windows
Fixes a bug introduced in #138571.  Paths need to be normalized before
being passed to our current version of globby.

Closes #150396

src/plugins/vis_types/timelion/server/lib/load_functions.js
@@ -9,6 +9,7 @@
 import _ from 'lodash';
 import globby from 'globby';
 import path from 'path';
+import normalizePath from 'normalize-path';
 import processFunctionDefinition from './process_function_definition';
 
 export default function (directory) {
@@ -19,7 +20,7 @@ export default function (directory) {
   // Get a list of all files and use the filename as the object key
   const files = _.map(
     globby
-      .sync(path.resolve(__dirname, '../' + directory + '/*.js'))
+      .sync(normalizePath(path.resolve(__dirname, '../' + directory + '/*.js')))
       .filter((filename) => !filename.includes('.test')),
     function (file) {
       const name = file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'));
@@ -29,7 +30,7 @@ export default function (directory) {
 
   // Get a list of all directories with an index.js, use the directory name as the key in the object
   const directories = _.chain(
-    globby.sync(path.resolve(__dirname, '../' + directory + '/*/index.js'))
+    globby.sync(normalizePath(path.resolve(__dirname, '../' + directory + '/*/index.js')))
   )
     .map(function (file) {
       const parts = file.split('/');