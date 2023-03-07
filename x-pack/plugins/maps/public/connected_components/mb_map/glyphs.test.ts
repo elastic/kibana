2ed0221b73865cde44d82a1d4f9884b225f49698
nreese <reese.nathan@elastic.co>
2023-03-07 13:06:25 -0700

    tslint

--- x-pack/plugins/maps/public/connected_components/mb_map/glyphs.test.ts
+++ x-pack/plugins/maps/public/connected_components/mb_map/glyphs.test.ts
@@ -11,7 +11,7 @@ jest.mock('../../kibana_services', () => ({
   getHttp: () => {
     return {
       basePath: {
-        prepend: (path) => `abc${path}`,
+        prepend: (path: string) => `abc${path}`,
       },
     };
   },
