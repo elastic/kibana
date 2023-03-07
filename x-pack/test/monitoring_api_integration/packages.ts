34475690a162e2329fa3486562aa7298845b2d91
klacabane <kevin.lacabane@elastic.co>
2023-03-07 18:01:53 +0100

    bump elasticsearch package

--- x-pack/test/monitoring_api_integration/packages.ts
+++ x-pack/test/monitoring_api_integration/packages.ts
@@ -9,7 +9,7 @@ import path from 'path';
 
 const PACKAGES = [
   { name: 'beat', version: '0.0.1' },
-  { name: 'elasticsearch', version: '1.2.0' },
+  { name: 'elasticsearch', version: '1.4.1' },
   { name: 'enterprisesearch', version: '1.0.0' },
   { name: 'logstash', version: '2.2.2-preview1' },
 ];
