492acc76cfe10f7e7580b8edc1c09321d0282467
Jonathan Budzenski <jon@elastic.co>
2023-03-07 13:52:46 -0600

    fix pipeline name

--- .buildkite/scripts/steps/functional/e2e.sh
+++ .buildkite/scripts/steps/functional/e2e.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 echo "--- Trigger kibana e2e pipeline"
-ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana_e2e "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT"
+ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-e2e-tests "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT"
