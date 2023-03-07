ecef05c80ddd883d3003bb7f3ae4dfc717f0184f
Ashokaditya <ashokaditya@elastic.co>
2023-03-07 11:53:30 +0100

    Show platform icon on responder
    closes elastic/security-team/issues/6102

---
+++ x-pack/plugins/security_solution/public/management/components/endpoint_responder/components/platforms/platform_icon.tsx
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiIcon, type EuiIconProps } from '@elastic/eui';
+import React, { memo, useMemo } from 'react';
+import linuxSvg from './logos/linux.svg';
+import windowsSvg from './logos/windows.svg';
+import macosSvg from './logos/macos.svg';
+
+const getPlatformIcon = (platform: string) => {
+  switch (platform) {
+    case 'darwin':
+      return macosSvg;
+    case 'linux':
+      return linuxSvg;
+    case 'windows':
+      return windowsSvg;
+    default:
+      return `${platform}`;
+  }
+};
+
+export const PlatformIcon = memo<{
+  platform: string;
+  size?: EuiIconProps['size'];
+}>(({ platform, size = 'xl' }) => {
+  const platformIcon = useMemo(() => getPlatformIcon(platform), [platform]);
+
+  return <EuiIcon type={platformIcon} title={platform} size={size} />;
+});
+
+PlatformIcon.displayName = 'PlatformIcon';
