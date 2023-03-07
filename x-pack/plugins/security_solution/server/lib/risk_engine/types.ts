980060daa14b2b45f59ac815ff6b54e421c875bb
Ryland Herrick <ryalnd@gmail.com>
2023-03-07 15:14:41 -0600

    Rename parameter to reflect (lack of) plurality

--- x-pack/plugins/security_solution/server/lib/risk_engine/types.ts
+++ x-pack/plugins/security_solution/server/lib/risk_engine/types.ts
@@ -13,7 +13,7 @@ export type IdentifierType = 'user' | 'host';
 
 export interface GetScoresParams {
   dataViewId?: DataViewId;
-  filters?: unknown;
+  filter?: unknown;
   identifierType?: IdentifierType;
   enrichInputs?: boolean;
   range: { start: string; end: string };
