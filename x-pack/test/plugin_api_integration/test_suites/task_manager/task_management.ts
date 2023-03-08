3aba90bbeca826d98e6ecfc891717b365ccb7cb3
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2023-03-08 12:10:06 -0500

    fix some mistake with not using mapings json

--- x-pack/test/plugin_api_integration/test_suites/task_manager/task_management.ts
+++ x-pack/test/plugin_api_integration/test_suites/task_manager/task_management.ts
@@ -9,14 +9,12 @@ import moment from 'moment';
 import { random } from 'lodash';
 import expect from '@kbn/expect';
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
-import TaskManagerMapping from '@kbn/task-manager-plugin/server/saved_objects/mappings.json';
+import { taskMappings as TaskManagerMapping } from '@kbn/task-manager-plugin/server/saved_objects/mappings';
 import { DEFAULT_POLL_INTERVAL } from '@kbn/task-manager-plugin/server/config';
 import { ConcreteTaskInstance, BulkUpdateTaskResult } from '@kbn/task-manager-plugin/server';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
-const {
-  task: { properties: taskManagerIndexMapping },
-} = TaskManagerMapping;
+const { properties: taskManagerIndexMapping } = TaskManagerMapping;
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
