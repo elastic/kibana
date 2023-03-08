bc9d9e02896df6e4a4485616c33af886ff61f5eb
rshen91 <rshen@elastic.co>
2023-03-08 13:08:44 -0700

    fix i18n headers

--- packages/kbn-reporting/generate_csv/impl/i18n_texts.ts
+++ packages/kbn-reporting/generate_csv/impl/i18n_texts.ts
@@ -9,15 +9,12 @@
 import { i18n } from '@kbn/i18n';
 
 export const i18nTexts = {
-  escapedFormulaValuesMessage: i18n.translate(
-    'xpack.reporting.exportTypes.csv.generateCsv.escapedFormulaValues',
-    {
-      defaultMessage: 'CSV may contain formulas whose values have been escaped',
-    }
-  ),
+  escapedFormulaValuesMessage: i18n.translate('reporting.generateCsv.escapedFormulaValues', {
+    defaultMessage: 'CSV may contain formulas whose values have been escaped',
+  }),
   authenticationError: {
     partialResultsMessage: i18n.translate(
-      'xpack.reporting.exportTypes.csv.generateCsv.authenticationExpired.partialResultsMessage',
+      'reporting.generateCsv.authenticationExpired.partialResultsMessage',
       {
         defaultMessage:
           'This report contains partial CSV results because the authentication token expired. Export a smaller amount of data or increase the timeout of the authentication token.',
@@ -25,23 +22,23 @@ export const i18nTexts = {
     ),
   },
   esErrorMessage: (statusCode: number, message: string) =>
-    i18n.translate('xpack.reporting.exportTypes.csv.generateCsv.esErrorMessage', {
+    i18n.translate('reporting.generateCsv.esErrorMessage', {
       defaultMessage: 'Received a {statusCode} response from Elasticsearch: {message}',
       values: { statusCode, message },
     }),
   unknownError: (message: string = 'unknown') =>
-    i18n.translate('xpack.reporting.exportTypes.csv.generateCsv.unknownErrorMessage', {
+    i18n.translate('reporting.generateCsv.unknownErrorMessage', {
       defaultMessage: 'Encountered an unknown error: {message}',
       values: { message },
     }),
   csvRowCountError: ({ expected, received }: { expected?: number; received: number }) =>
-    i18n.translate('xpack.reporting.exportTypes.csv.generateCsv.incorrectRowCount', {
+    i18n.translate('reporting.generateCsv.incorrectRowCount', {
       defaultMessage:
         'Encountered an error with the number of CSV rows generated from the search: expected {expected}, received {received}.',
       values: { expected, received },
     }),
   csvUnableToClosePit: () =>
-    i18n.translate('xpack.reporting.exportTypes.csv.generateCsv.csvUnableToClosePit', {
+    i18n.translate('reporting.generateCsv.csvUnableToClosePit', {
       defaultMessage:
         'Unable to close the Point-In-Time used for search. Check the Kibana server logs.',
     }),
