d4fd9954b86cf5d16323aed702bbf57c5860ae2c
Marshall Main <55718608+marshallmain@users.noreply.github.com>
2023-03-07 22:36:20 -0800

    [Security Solution][Alerts] Removes custom cypress schemas in favor of io-ts schemas (#151520)
    ## Summary
    
    This PR removes the rule schemas that are specific to Cypress tests, and
    replaces them with the HTTP API schemas from the Detection Engine API.
    As a result, the rule schemas used in Cypress tests will now
    automatically update when we add new fields to rules. In addition, we
    can try to start removing some of mock rule data in Cypress tests and
    share mocks across integration/e2e/unit tests.
    
    Finally, this PR removes the specific API call functions like
    `createCustomRule` and `createCustomRuleEnabled` in favor of a generic
    `createRule` function that can create any type of rule, without
    restrictions on which fields can be specified (e.g.
    `createMachineLearningRule` hard coded `from: 'now-50000h',` in the
    function body).
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/cypress/e2e/exceptions/add_edit_flyout/flyout_validation.cy.ts
+++ x-pack/plugins/security_solution/cypress/e2e/exceptions/add_edit_flyout/flyout_validation.cy.ts
@@ -9,7 +9,7 @@ import { getNewRule } from '../../../objects/rule';
 
 import { RULE_STATUS } from '../../../screens/create_new_rule';
 
-import { createCustomRule } from '../../../tasks/api_calls/rules';
+import { createRule } from '../../../tasks/api_calls/rules';
 import { goToRuleDetails } from '../../../tasks/alerts_detection_rules';
 import {
   esArchiverLoad,
@@ -78,13 +78,11 @@ describe('Exceptions flyout', { testIsolation: false }, () => {
     esArchiverLoad('conflicts_2');
     login();
     createExceptionList(getExceptionList(), getExceptionList().list_id).then((response) =>
-      createCustomRule({
+      createRule({
         ...getNewRule(),
-        dataSource: {
-          index: ['auditbeat-*', 'exceptions-*', 'conflicts-*'],
-          type: 'indexPatterns',
-        },
-        exceptionLists: [
+        index: ['auditbeat-*', 'exceptions-*', 'conflicts-*'],
+        enabled: false,
+        exceptions_list: [
           {
             id: response.body.id,
             list_id: getExceptionList().list_id,
