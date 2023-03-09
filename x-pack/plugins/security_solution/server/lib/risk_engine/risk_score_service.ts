4f5f72882384110d29e776ac4ab41b191cb56a5f
Ryland Herrick <ryalnd@gmail.com>
2023-03-08 20:21:48 -0600

    Log the request/response to kibana in debug mode
    If the request times out, or if the payload is too large, this will be
    useful information.

--- x-pack/plugins/security_solution/server/lib/risk_engine/risk_score_service.ts
+++ x-pack/plugins/security_solution/server/lib/risk_engine/risk_score_service.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { ElasticsearchClient } from '@kbn/core/server';
+import type { ElasticsearchClient, Logger } from '@kbn/core/server';
 import type { GetScoresParams, GetScoresResponse } from './types';
 import { calculateRiskScores } from './calculate_risk_scores';
 
@@ -15,8 +15,10 @@ export interface RiskScoreService {
 
 export const buildRiskScoreService = ({
   esClient,
+  logger,
 }: {
   esClient: ElasticsearchClient;
+  logger: Logger;
 }): RiskScoreService => ({
-  getScores: (params) => calculateRiskScores({ ...params, esClient }),
+  getScores: (params) => calculateRiskScores({ ...params, esClient, logger }),
 });
