206a9d114d87565ecd05b8c97f6e041c4a5128ab
Sébastien Loix <sebastien.loix@elastic.co>
2023-03-10 14:36:17 +0000

    [Content management] BWC - versioned content (#152729)

--- src/plugins/content_management/public/content_client/content_client_query_hooks.test.tsx
+++ src/plugins/content_management/public/content_client/content_client_query_hooks.test.tsx
@@ -13,10 +13,16 @@ import { ContentClient } from './content_client';
 import { createCrudClientMock } from '../crud_client/crud_client.mock';
 import { useGetContentQuery, useSearchContentQuery } from './content_client_query_hooks';
 import type { GetIn, SearchIn } from '../../common';
+import { ContentTypeRegistry } from '../registry';
 
 const setup = () => {
   const crudClient = createCrudClientMock();
-  const contentClient = new ContentClient(() => crudClient);
+  const contentTypeRegistry = new ContentTypeRegistry();
+  contentTypeRegistry.register({
+    id: 'testType',
+    version: { latest: 'v2' },
+  });
+  const contentClient = new ContentClient(() => crudClient, contentTypeRegistry);
 
   const Wrapper: React.FC = ({ children }) => (
     <ContentClientProvider contentClient={contentClient}>{children}</ContentClientProvider>
@@ -32,7 +38,7 @@ const setup = () => {
 describe('useGetContentQuery', () => {
   test('should call rpcClient.get with input and resolve with output', async () => {
     const { crudClient, Wrapper } = setup();
-    const input: GetIn = { id: 'test', contentTypeId: 'testType' };
+    const input: GetIn = { id: 'test', contentTypeId: 'testType', version: 'v2' };
     const output = { test: 'test' };
     crudClient.get.mockResolvedValueOnce(output);
     const { result, waitFor } = renderHook(() => useGetContentQuery(input), { wrapper: Wrapper });
@@ -44,7 +50,7 @@ describe('useGetContentQuery', () => {
 describe('useSearchContentQuery', () => {
   test('should call rpcClient.search with input and resolve with output', async () => {
     const { crudClient, Wrapper } = setup();
-    const input: SearchIn = { contentTypeId: 'testType', query: {} };
+    const input: SearchIn = { contentTypeId: 'testType', query: {}, version: 'v2' };
     const output = { hits: [{ id: 'test' }] };
     crudClient.search.mockResolvedValueOnce(output);
     const { result, waitFor } = renderHook(() => useSearchContentQuery(input), {
