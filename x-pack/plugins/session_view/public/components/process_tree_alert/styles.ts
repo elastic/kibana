f68fe0a3e6c88cbd36dced9f26128d22cb1a6c03
Karl Godard <karl.godard@elastic.co>
2023-03-14 23:49:42 +0000

    revert output preview experiment

--- x-pack/plugins/session_view/public/components/process_tree_alert/styles.ts
+++ x-pack/plugins/session_view/public/components/process_tree_alert/styles.ts
@@ -92,15 +92,6 @@ export const useStyles = ({ isInvestigated, isSelected }: StylesDeps) => {
       padding: `${size.xs} ${size.s}`,
     };
 
-    const outputPreview: CSSObject = {
-      backgroundColor: colors.ink,
-      fontFamily: font.familyCode,
-      color: colors.ghost,
-      fontSize: size.m,
-      padding: size.xs,
-      whiteSpace: 'nowrap',
-    };
-
     return {
       alert,
       alertStatus,
@@ -108,7 +99,6 @@ export const useStyles = ({ isInvestigated, isSelected }: StylesDeps) => {
       actionBadge,
       processPanel,
       processAlertDisplayContainer,
-      outputPreview,
     };
   }, [euiTheme, isInvestigated, isSelected, euiVars]);
 
