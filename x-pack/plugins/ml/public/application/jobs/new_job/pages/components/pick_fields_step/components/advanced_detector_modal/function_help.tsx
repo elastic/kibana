154a2ad0325ffe9e32db1b528e48953890bbe81f
István Zoltán Szabó <szabosteve@gmail.com>
2023-03-14 14:20:44 -0400

    [ML] Adds translation functions.

--- x-pack/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/advanced_detector_modal/function_help.tsx
+++ x-pack/plugins/ml/public/application/jobs/new_job/pages/components/pick_fields_step/components/advanced_detector_modal/function_help.tsx
@@ -44,92 +44,174 @@ export const FunctionHelpPopover: FC = memo(() => {
     {
       id: 0,
       function: 'count, high_count, low_count',
-      description: 'Detect anomalies when the number of events in a bucket is anomalous.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.count',
+        {
+          defaultMessage: 'Detect anomalies when the number of events in a bucket is anomalous.',
+        }
+      ),
     },
     {
       id: 1,
       function: 'non_zero_count, high_non_zero_count, low_non_zero_count',
-      description:
-        'Detect anomalies when the number of events in a bucket is anomalous, but it ignores cases where the bucket count is zero.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.nonZeroCount',
+        {
+          defaultMessage:
+            'Detect anomalies when the number of events in a bucket is anomalous, but it ignores cases where the bucket count is zero.',
+        }
+      ),
     },
     {
       id: 2,
       function: 'distinct_count, high_distinct_count, low_distinct_count',
-      description: 'Detect anomalies where the number of distinct values in one field is unusual.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.distinctCount',
+        {
+          defaultMessage:
+            'Detect anomalies where the number of distinct values in one field is unusual.',
+        }
+      ),
     },
     {
       id: 3,
       function: 'lat_long',
-      description: 'Detects anomalies in the geographic location of the input data.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.latLong',
+        {
+          defaultMessage: 'Detects anomalies in the geographic location of the input data.',
+        }
+      ),
     },
     {
       id: 4,
       function: 'info_content, high_info_content, low_info_content',
-      description:
-        'Detect anomalies in the amount of information that is contained in strings in a bucket.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.infoContent',
+        {
+          defaultMessage:
+            'Detect anomalies in the amount of information that is contained in strings in a bucket.',
+        }
+      ),
     },
     {
       id: 5,
       function: 'min',
-      description:
-        'Detects anomalies in the arithmetic minimum of a value. The minimum value is calculated for each bucket.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.min',
+        {
+          defaultMessage:
+            'Detects anomalies in the arithmetic minimum of a value. The minimum value is calculated for each bucket.',
+        }
+      ),
     },
     {
       id: 6,
       function: 'max',
-      description:
-        'Detects anomalies in the arithmetic maximum of a value. The maximum value is calculated for each bucket.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.max',
+        {
+          defaultMessage:
+            'Detects anomalies in the arithmetic maximum of a value. The maximum value is calculated for each bucket.',
+        }
+      ),
     },
     {
       id: 7,
       function: 'median, high_median, low_median',
-      description:
-        'Detect anomalies in the statistical median of a value. The median value is calculated for each bucket.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.median',
+        {
+          defaultMessage:
+            'Detect anomalies in the statistical median of a value. The median value is calculated for each bucket.',
+        }
+      ),
     },
     {
       id: 8,
       function: 'mean, high_mean, low_mean',
-      description:
-        'Detect anomalies in the arithmetic mean of a value. The mean value is calculated for each bucket.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.mean',
+        {
+          defaultMessage:
+            'Detect anomalies in the arithmetic mean of a value. The mean value is calculated for each bucket.',
+        }
+      ),
     },
     {
       id: 9,
       function: 'metric',
-      description:
-        'Combines min, max, and mean functions. You can use it as a shorthand for a combined analysis. If you do not specify a function in a detector, this is the default function.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.metric',
+        {
+          defaultMessage:
+            'Combines min, max, and mean functions. You can use it as a shorthand for a combined analysis. If you do not specify a function in a detector, this is the default function.',
+        }
+      ),
     },
     {
       id: 10,
       function: 'varp, high_varp, low_varp',
-      description:
-        'Detect anomalies in the variance of a value which is a measure of the variability and spread in the data.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.varp',
+        {
+          defaultMessage:
+            'Detect anomalies in the variance of a value which is a measure of the variability and spread in the data.',
+        }
+      ),
     },
     {
       id: 11,
       function: 'rare',
-      description: 'Detects anomalies according to the number of distinct rare values.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.rare',
+        {
+          defaultMessage: 'Detects anomalies according to the number of distinct rare values.',
+        }
+      ),
     },
     {
       id: 12,
       function: 'freq_rare',
-      description:
-        'Detects anomalies according to the number of times (frequency) rare values occur.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.freqRare',
+        {
+          defaultMessage:
+            'Detects anomalies according to the number of times (frequency) rare values occur.',
+        }
+      ),
     },
     {
       id: 13,
       function: 'sum, high_sum, low_sum',
-      description: 'Detect anomalies where the sum of a field in a bucket is anomalous.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.sum',
+        {
+          defaultMessage: 'Detect anomalies where the sum of a field in a bucket is anomalous.',
+        }
+      ),
     },
     {
       id: 14,
       function: 'non_null_sum, high_non_null_sum, low_non_null_sum',
-      description:
-        'These functions are useful if your data is sparse. Buckets without values are ignored and buckets with a zero value are analyzed.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.nonZeroCount',
+        {
+          defaultMessage:
+            'These functions are useful if your data is sparse. Buckets without values are ignored and buckets with a zero value are analyzed.',
+        }
+      ),
     },
     {
       id: 15,
       function: 'time_of_day, time_of_week',
-      description: 'Detect events that happen at unusual times, either of the day or of the week.',
+      description: i18n.translate(
+        'xpack.ml.newJob.wizard.pickFieldsStep.advancedDetectorModal.functionHelp.time',
+        {
+          defaultMessage:
+            'Detect events that happen at unusual times, either of the day or of the week.',
+        }
+      ),
     },
   ];
 
