0c5f28067a1e17ccf26c0ea5ebdd9bb11dd13a87
Hannah Mudge <hannah.wright@elastic.co>
2023-03-15 12:25:23 -0600

    Add scroll lock to edit control flyout

--- src/plugins/controls/public/control_group/actions/edit_control_action.tsx
+++ src/plugins/controls/public/control_group/actions/edit_control_action.tsx
@@ -118,6 +118,8 @@ export class EditControlAction implements Action<EditControlActionContext> {
           flyout.close();
         },
         ownFocus: true,
+        // @ts-ignore - TODO: Remove this once https://github.com/elastic/eui/pull/6645 lands in Kibana
+        focusTrapProps: { scrollLock: true },
       }
     );
     setFlyoutRef(flyoutInstance);
