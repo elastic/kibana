c7cf4a826ed19956d96b94499360b68fac424d13
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2023-03-15 10:13:46 -0400

    update saved-objects hash

--- src/core/server/integration_tests/saved_objects/migrations/group2/check_registered_types.test.ts
+++ src/core/server/integration_tests/saved_objects/migrations/group2/check_registered_types.test.ts
@@ -135,7 +135,7 @@ describe('checking migration metadata changes on all registered SO types', () =>
         "siem-ui-timeline-note": "13c9d4c142f96624a93a623c6d7cba7e1ae9b5a6",
         "siem-ui-timeline-pinned-event": "96a43d59b9e2fc11f12255a0cb47ef0a3d83af4c",
         "slo": "9a138b459c7efef7fecfda91f22db8b7655d0e61",
-        "space": "42abce78624007871d21e4e65fb58ccd07a4ac4c",
+        "space": "6d6af2d8c811da477d881dd929556dad665f0ec6",
         "spaces-usage-stats": "084bd0f080f94fb5735d7f3cf12f13ec92f36bad",
         "synthetics-monitor": "96cc312bfa597022f83dfb3b5d1501e27a73e8d5",
         "synthetics-param": "9776c9b571d35f0d0397e8915e035ea1dc026db7",
