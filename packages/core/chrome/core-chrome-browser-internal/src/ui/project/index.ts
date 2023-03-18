94993bb1bf65dc9fcfff35cbc484ffa341e4ae8e
Clint Andrew Hall <clint@clintandrewhall.com>
2023-03-18 16:57:12 -0400

    Add chrome style, project header.

--- packages/core/chrome/core-chrome-browser-internal/src/ui/project/index.ts
+++ packages/core/chrome/core-chrome-browser-internal/src/ui/project/index.ts
@@ -6,7 +6,4 @@
  * Side Public License, v 1.
  */
 
-export { Header } from './header';
-export { ProjectHeader } from './project';
-export { LoadingIndicator } from './loading_indicator';
-export type { NavType } from './header';
+export { ProjectHeader } from './header';
