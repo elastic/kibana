833da589101de9110bb258e7b4c0cfc9ca53ae60
“jeramysoucy” <jeramy.soucy@elastic.co>
2023-03-20 10:40:13 -0400

    Unskips test for troubleshooting

--- x-pack/test/security_api_integration/tests/session_idle/cleanup.ts
+++ x-pack/test/security_api_integration/tests/session_idle/cleanup.ts
@@ -81,7 +81,7 @@ export default function ({ getService }: FtrProviderContext) {
 
   // Failing: See https://github.com/elastic/kibana/issues/121482
   // Failing: See https://github.com/elastic/kibana/issues/152260
-  describe.skip('Session Idle cleanup', () => {
+  describe('Session Idle cleanup', () => {
     beforeEach(async () => {
       await es.cluster.health({ index: '.kibana_security_session*', wait_for_status: 'green' });
       await esDeleteAllIndices('.kibana_security_session*');
