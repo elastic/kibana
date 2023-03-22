6edb387f56a5af13056fc36ec486caf2a7c34cd6
parkiino <candace.park@elastic.co>
2023-03-22 14:30:01 -0400

    add migration to index

--- x-pack/plugins/fleet/server/saved_objects/index.ts
+++ x-pack/plugins/fleet/server/saved_objects/index.ts
@@ -54,7 +54,10 @@ import {
   migrateInstallationToV860,
   migratePackagePolicyToV860,
 } from './migrations/to_v8_6_0';
-import { migratePackagePolicyToV870 } from './migrations/security_solution';
+import {
+  migratePackagePolicyToV870,
+  migratePackagePolicyToV880,
+} from './migrations/security_solution';
 
 /*
  * Saved object types and mappings
@@ -212,6 +215,7 @@ const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
       '8.5.0': migratePackagePolicyToV850,
       '8.6.0': migratePackagePolicyToV860,
       '8.7.0': migratePackagePolicyToV870,
+      '8.8.0': migratePackagePolicyToV880,
     },
   },
   [PACKAGES_SAVED_OBJECT_TYPE]: {
