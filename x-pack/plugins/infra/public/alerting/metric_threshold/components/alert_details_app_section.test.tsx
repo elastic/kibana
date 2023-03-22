fda5011f5706f7e7a3f70399c6f4e77ad1ff4c5e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-03-22 14:56:43 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/infra/public/alerting/metric_threshold/components/alert_details_app_section.test.tsx
+++ x-pack/plugins/infra/public/alerting/metric_threshold/components/alert_details_app_section.test.tsx
@@ -11,7 +11,7 @@ import { __IntlProvider as IntlProvider } from '@kbn/i18n-react';
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { buildMetricThresholdRule } from '../mocks/metric_threshold_rule';
-import {AlertDetailsAppSection} from './alert_details_app_section';
+import { AlertDetailsAppSection } from './alert_details_app_section';
 
 jest.mock('../../../hooks/use_kibana', () => ({
   useKibanaContextForPlugin: () => ({
