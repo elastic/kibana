f18b24315ee1b09ac301e02a3afd4844b41f66d9
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-03-23 21:31:47 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/new_index/licensing_callout.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/new_index/licensing_callout.tsx
@@ -44,11 +44,11 @@ export const LicensingCallout: React.FC<{ feature: LICENSING_FEATURE }> = ({ fea
           'Inference processors require a Platinum license or higher and are not available to Standard license self-managed deployments. You need to upgrade to use this feature.',
       }
     ),
-   [LICENSING_FEATURE.SEARCH_APPLICATIONS]: i18n.translate(
+    [LICENSING_FEATURE.SEARCH_APPLICATIONS]: i18n.translate(
       'xpack.enterpriseSearch.content.licensingCallout.inference.contentOne',
-    {
-      defaultMessage:
-        'Search Applications require a Platinum license or higher and are not available to Standard license self-managed deployments. You need to upgrade to use this feature.',
+      {
+        defaultMessage:
+          'Search Applications require a Platinum license or higher and are not available to Standard license self-managed deployments. You need to upgrade to use this feature.',
       }
     ),
   };
@@ -79,7 +79,7 @@ export const LicensingCallout: React.FC<{ feature: LICENSING_FEATURE }> = ({ fea
       'xpack.enterpriseSearch.content.licensingCallout.inference.contentTwo',
       {
         defaultMessage:
-          "Did you know that Search Applications are available with a Standard Elastic Cloud license? Elastic Cloud gives you the flexibility to run where you want. Deploy our managed service on Google Cloud, Microsoft Azure, or Amazon Web Services and we\'ll handle the maintenance and upkeep for you.",
+          "Did you know that Search Applications are available with a Standard Elastic Cloud license? Elastic Cloud gives you the flexibility to run where you want. Deploy our managed service on Google Cloud, Microsoft Azure, or Amazon Web Services and we'll handle the maintenance and upkeep for you.",
       }
     ),
   };
