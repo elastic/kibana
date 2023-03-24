45efba34e414051ffa066b9513cf40aba9d86682
Robert Oskamp <robert.oskamp@elastic.co>
2023-03-24 11:05:38 +0100

    Revert "Use the correct way to run browser in headless mode"
    This reverts commit 2e2d7b72391219249bb7898ab5459a5ce58cac73.

--- test/functional/services/remote/webdriver.ts
+++ test/functional/services/remote/webdriver.ts
@@ -99,12 +99,9 @@ function initChromiumOptions(browserType: Browsers, acceptInsecureCerts: boolean
   }
 
   if (headlessBrowser === '1') {
-    // Using the new headless mode (instead of `options.headless()`)
-    // See: https://www.selenium.dev/blog/2023/headless-is-going-away/
-    options.addArguments('headless=new');
-
     // Use --disable-gpu to avoid an error from a missing Mesa library, as per
     // See: https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+    options.headless();
     options.addArguments('disable-gpu');
   }
 
@@ -114,10 +111,7 @@ function initChromiumOptions(browserType: Browsers, acceptInsecureCerts: boolean
 
   if (remoteDebug === '1') {
     // Visit chrome://inspect in chrome to remotely view/debug
-
-    // Using the new headless mode (instead of `options.headless()`)
-    // See: https://www.selenium.dev/blog/2023/headless-is-going-away/
-    options.addArguments('headless=new');
+    options.headless();
     options.addArguments('disable-gpu', 'remote-debugging-port=9222');
   }
 
