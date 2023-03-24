154d3b9a402ad2cb7ab0b7a5a9a8d0d7f7598fab
Adam Demjen <demjened@gmail.com>
2023-03-24 15:34:19 -0400

    Update logic to accept pipeline definition

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/api/pipelines/create_ml_inference_pipeline.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/api/pipelines/create_ml_inference_pipeline.ts
@@ -6,12 +6,14 @@
  */
 import {
   CreateMlInferencePipelineParameters,
+  CreateMLInferencePipelineDefinition,
+  MlInferencePipeline,
   InferencePipelineInferenceConfig,
 } from '../../../../../common/types/pipelines';
 import { createApiLogic } from '../../../shared/api_logic/create_api_logic';
 import { HttpLogic } from '../../../shared/http';
 
-export interface CreateMlInferencePipelineApiLogicArgs {
+type CreateMlInferencePipelineApiLogicArgsWithPipelineParameters = {
   destinationField?: string;
   indexName: string;
   inferenceConfig?: InferencePipelineInferenceConfig;
@@ -20,21 +22,46 @@ export interface CreateMlInferencePipelineApiLogicArgs {
   sourceField: string;
 }
 
+type CreateMlInferencePipelineApiLogicArgsWithPipelineDefinition = {
+  indexName: string;
+  pipelineName: string;
+  pipelineDefinition: MlInferencePipeline;
+}
+
+export type CreateMlInferencePipelineApiLogicArgs = CreateMlInferencePipelineApiLogicArgsWithPipelineParameters | CreateMlInferencePipelineApiLogicArgsWithPipelineDefinition;
+
 export interface CreateMlInferencePipelineResponse {
   created: string;
+}
+
+const isArgsWithPipelineParameters = (args: any): args is CreateMlInferencePipelineApiLogicArgsWithPipelineParameters => {
+  return typeof args.modelId === 'string';
+}
+
+const isArgsWithPipelineDefinition = (args: any): args is CreateMlInferencePipelineApiLogicArgsWithPipelineDefinition => {
+  return typeof args.pipelineDefinition === 'object';
 }
 
 export const createMlInferencePipeline = async (
   args: CreateMlInferencePipelineApiLogicArgs
 ): Promise<CreateMlInferencePipelineResponse> => {
   const route = `/internal/enterprise_search/indices/${args.indexName}/ml_inference/pipeline_processors`;
-  const params: CreateMlInferencePipelineParameters = {
-    destination_field: args.destinationField,
-    inference_config: args.inferenceConfig,
-    model_id: args.modelId,
-    pipeline_name: args.pipelineName,
-    source_field: args.sourceField,
-  };
+  let params: CreateMlInferencePipelineParameters | CreateMLInferencePipelineDefinition | undefined;
+  if (isArgsWithPipelineParameters(args)) {
+    params = {
+      destination_field: args.destinationField,
+      inference_config: args.inferenceConfig,
+      model_id: args.modelId,
+      pipeline_name: args.pipelineName,
+      pipeline_definition: undefined,
+      source_field: args.sourceField,
+    }
+  } else if (isArgsWithPipelineDefinition(args)) {
+    params = {
+      pipeline_name: args.pipelineName,
+      pipeline_definition: args.pipelineDefinition,
+    }
+  }
   return await HttpLogic.values.http.post<CreateMlInferencePipelineResponse>(route, {
     body: JSON.stringify(params),
   });
