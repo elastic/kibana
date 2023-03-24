5401c604f4b7c9745f6c8625e06f02991ea31ffe
obscloudnativemonitoring <obs-cloudnative-monitoring@elastic.co>
2023-03-24 13:46:39 +0000

    [Automated PR] Publish kubernetes templates for elastic-agent

--- x-pack/plugins/fleet/server/services/elastic_agent_manifest.ts
+++ x-pack/plugins/fleet/server/services/elastic_agent_manifest.ts
@@ -1,9 +1,9 @@
 /*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
+* Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+* or more contributor license agreements. Licensed under the Elastic License
+* 2.0; you may not use this file except in compliance with the Elastic License
+* 2.0.
+*/
 
 export const elasticAgentStandaloneManifest = `---
 # For more information refer https://www.elastic.co/guide/en/fleet/current/running-on-kubernetes-standalone.html
@@ -49,10 +49,7 @@ spec:
       containers:
         - name: elastic-agent
           image: docker.elastic.co/beats/elastic-agent:VERSION
-          args: [
-            "-c", "/etc/elastic-agent/agent.yml",
-            "-e",
-          ]
+          args: ["-c", "/etc/elastic-agent/agent.yml", "-e"]
           env:
             # The basic authentication username used to connect to Elasticsearch
             # This user needs the privileges required to publish events to Elasticsearch.
@@ -75,11 +72,11 @@ spec:
             runAsUser: 0
             capabilities:
               add:
-              # The following capabilities are needed for 'Defend for containers' integration (cloud-defend)
-              # If you are not using this integration, then these capabilites can be removed.
+                # The following capabilities are needed for 'Defend for containers' integration (cloud-defend)
+                # If you are not using this integration, then these capabilites can be removed.
                 - BPF # (since Linux 5.8) allows loading of BPF programs, create most map types, load BTF, iterate programs and maps.
                 - PERFMON # (since Linux 5.8) allows attaching of BPF programs used for performance metrics and observability operations.
-                - SYS_RESOURCE # Allow use of special resources or raising of resource limits. Used by Defend for Containers to modify rlimit_memlock
+                - SYS_RESOURCE # Allow use of special resources or raising of resource limits. Used by 'Defend for containers' to modify 'rlimit_memlock'
           resources:
             limits:
               memory: 700Mi
@@ -370,11 +367,11 @@ spec:
             runAsUser: 0
             capabilities:
               add:
-              # The following capabilities are needed for 'Defend for containers' integration (cloud-defend)
-              # If you are not using this integration, then these capabilites can be removed.
+                # The following capabilities are needed for 'Defend for containers' integration (cloud-defend)
+                # If you are not using this integration, then these capabilites can be removed.
                 - BPF # (since Linux 5.8) allows loading of BPF programs, create most map types, load BTF, iterate programs and maps.
                 - PERFMON # (since Linux 5.8) allows attaching of BPF programs used for performance metrics and observability operations.
-                - SYS_RESOURCE # Allow use of special resources or raising of resource limits. Used by Defend for Containers to modify rlimit_memlock
+                - SYS_RESOURCE # Allow use of special resources or raising of resource limits. Used by 'Defend for Containers' to modify 'rlimit_memlock'
           resources:
             limits:
               memory: 700Mi
