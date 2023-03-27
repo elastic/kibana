4ee248f2d8d7404e918371cf8a3bf4fa8dd4cc8c
rshen <rshen@elastic.co>
2023-03-27 14:05:07 -0600

    fix import to pass test

--- x-pack/plugins/reporting/server/export_types/csv_searchsource/execute_job.test.ts
+++ x-pack/plugins/reporting/server/export_types/csv_searchsource/execute_job.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-jest.mock('@kbn/generate-csv', () => ({
+jest.mock('@kbn/generate-csv/src', () => ({
   CsvGenerator: class CsvGeneratorMock {
     generateData() {
       return {
@@ -20,7 +20,7 @@ import nodeCrypto from '@elastic/node-crypto';
 import { loggingSystemMock } from '@kbn/core/server/mocks';
 import { Writable } from 'stream';
 import { ReportingCore } from '../..';
-import { CancellationToken } from '@kbn/reporting-common';
+import { CancellationToken } from '@kbn/reporting-common/index';
 import { createMockConfigSchema, createMockReportingCore } from '../../test_helpers';
 import { runTaskFnFactory } from './execute_job';
 
