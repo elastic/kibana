ce98092362c8c7cb437ab05580b593b19a992ebc
Jean-Louis Leysens <jloleysens@gmail.com>
2023-03-28 12:34:04 +0200

    added versioned router mock

--- packages/core/http/core-http-router-server-mocks/src/router.mock.ts
+++ packages/core/http/core-http-router-server-mocks/src/router.mock.ts
@@ -21,6 +21,7 @@ import type {
   KibanaResponseFactory,
 } from '@kbn/core-http-server';
 import { CoreKibanaRequest } from '@kbn/core-http-router-server-internal';
+import { createVersionedRouterMock } from '@kbn/core-http-versioned-router-server-mocks';
 
 export type RouterMock = jest.Mocked<IRouter<any>>;
 
@@ -34,6 +35,7 @@ function createRouterMock({ routerPath = '' }: { routerPath?: string } = {}): Ro
     patch: jest.fn(),
     getRoutes: jest.fn(),
     handleLegacyErrors: jest.fn().mockImplementation((handler) => handler),
+    versioned: createVersionedRouterMock(),
   };
 }
 
