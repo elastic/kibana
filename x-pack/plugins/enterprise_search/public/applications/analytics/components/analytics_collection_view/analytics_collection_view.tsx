7fed3ddcb876e18b0f0696f2dcdd95e5319d880b
Yan Savitski <yan.savitski@elastic.co>
2023-03-28 21:32:34 +0200

    [Behavioral Analytics] Remove unused code for getting data_view_id

--- x-pack/plugins/enterprise_search/public/applications/analytics/components/analytics_collection_view/analytics_collection_view.tsx
+++ x-pack/plugins/enterprise_search/public/applications/analytics/components/analytics_collection_view/analytics_collection_view.tsx
@@ -10,19 +10,15 @@ import { useParams } from 'react-router-dom';
 
 import { useActions, useValues } from 'kea';
 
-import { EuiEmptyPrompt, EuiIconTip, EuiLink } from '@elastic/eui';
+import { EuiEmptyPrompt } from '@elastic/eui';
 
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n-react';
-import { RedirectAppLinks } from '@kbn/kibana-react-plugin/public';
 
-import { KibanaLogic } from '../../../shared/kibana';
 import { AddAnalyticsCollection } from '../add_analytics_collections/add_analytics_collection';
 
 import { EnterpriseSearchAnalyticsPageTemplate } from '../layout/page_template';
 
 import { AnalyticsCollectionChartWithLens } from './analytics_collection_chart';
-import { AnalyticsCollectionDataViewIdLogic } from './analytics_collection_data_view_id_logic';
 
 import { FetchAnalyticsCollectionLogic } from './fetch_analytics_collection_logic';
 
@@ -34,15 +30,11 @@ export const collectionViewBreadcrumbs = [
 
 export const AnalyticsCollectionView: React.FC = () => {
   const { fetchAnalyticsCollection } = useActions(FetchAnalyticsCollectionLogic);
-  const { fetchAnalyticsCollectionDataViewId } = useActions(AnalyticsCollectionDataViewIdLogic);
   const { analyticsCollection, isLoading } = useValues(FetchAnalyticsCollectionLogic);
-  const { dataViewId } = useValues(AnalyticsCollectionDataViewIdLogic);
   const { name, section } = useParams<{ name: string; section: string }>();
-  const { application } = useValues(KibanaLogic);
 
   useEffect(() => {
     fetchAnalyticsCollection(name);
-    fetchAnalyticsCollectionDataViewId(name);
   }, []);
 
   return (
@@ -56,28 +48,6 @@ export const AnalyticsCollectionView: React.FC = () => {
         pageTitle: i18n.translate('xpack.enterpriseSearch.analytics.collectionsView.title', {
           defaultMessage: 'Overview',
         }),
-        rightSideItems: dataViewId
-          ? [
-              <RedirectAppLinks application={application}>
-                <EuiLink
-                  href={application.getUrlForApp('discover', {
-                    path: `#/?_a=(index:'${dataViewId}')`,
-                  })}
-                >
-                  <EuiIconTip
-                    position="bottom"
-                    content={
-                      <FormattedMessage
-                        id="xpack.enterpriseSearch.analytics.collectionsView.exploreTooltip"
-                        defaultMessage="For a deeper analysis, explore event logs on Discover."
-                      />
-                    }
-                    type="inspect"
-                  />
-                </EuiLink>
-              </RedirectAppLinks>,
-            ]
-          : undefined,
       }}
     >
       {analyticsCollection ? (
