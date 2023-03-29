0ee23684e34b5ebb39cd2a9fb0134db77be1628b
Coen Warmer <coen.warmer@gmail.com>
2023-03-29 17:01:35 +0200

    Remove Playwright e2e test from Observability (#153934)

--- x-pack/plugins/synthetics/e2e/journeys/synthetics/alert_rules/default_status_alert.journey.ts
+++ x-pack/plugins/synthetics/e2e/journeys/synthetics/alert_rules/default_status_alert.journey.ts
@@ -9,7 +9,7 @@ import { journey, step, before, after, expect } from '@elastic/synthetics';
 import { byTestId } from '@kbn/ux-plugin/e2e/journeys/utils';
 import { RetryService } from '@kbn/ftr-common-functional-services';
 import { v4 as uuidv4 } from 'uuid';
-import { recordVideo } from '@kbn/observability-plugin/e2e/record_video';
+import { recordVideo } from '../../../helpers/record_video';
 import { getReasonMessage } from '../../../../server/legacy_uptime/lib/alerts/status_check';
 import { syntheticsAppPageProvider } from '../../../page_objects/synthetics/synthetics_app';
 import { SyntheticsServices } from '../services/synthetics_services';
--- x-pack/plugins/synthetics/e2e/journeys/synthetics/detail_flyout.ts
+++ x-pack/plugins/synthetics/e2e/journeys/synthetics/detail_flyout.ts
@@ -6,7 +6,7 @@
  */
 
 import { expect, journey, step } from '@elastic/synthetics';
-import { recordVideo } from '@kbn/observability-plugin/e2e/record_video';
+import { recordVideo } from '../../helpers/record_video';
 import { syntheticsAppPageProvider } from '../../page_objects/synthetics/synthetics_app';
 
 journey('TestMonitorDetailFlyout', async ({ page, params }) => {
