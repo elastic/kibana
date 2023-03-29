d9ffbd7de2bce6a28c77cdce52edbdbd683e55bf
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2023-03-29 16:43:03 +0100

    Update x-pack/plugins/rule_registry/server/rule_data_plugin_service/index_info.ts
    Co-authored-by: Marshall Main <55718608+marshallmain@users.noreply.github.com>

--- x-pack/plugins/rule_registry/server/rule_data_plugin_service/index_info.ts
+++ x-pack/plugins/rule_registry/server/rule_data_plugin_service/index_info.ts
@@ -114,7 +114,7 @@ export class IndexInfo {
    *   - include nested registration contexts eagerly
    *   - e.g. if baseName='.alerts-observability', include '.alerts-observability.apm'
    *
-   * @example '.alerts-security.alerts-default*', '.alerts-security.alerts*'
+   * @example '.alerts-security.alerts-default', '.alerts-security.alerts*'
    */
   public getPatternForReading(namespace?: string): string {
     return namespace ? `${joinWithDash(this.baseName, namespace)}` : `${this.baseName}*`;
