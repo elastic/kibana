b0ccd1c887194f710c50d9d967d10ea6bccf6f8c
Ying <ying.mao@elastic.co>
2023-03-30 16:06:28 -0400

    Fixing functional tests

--- x-pack/test/detection_engine_api_integration/security_and_spaces/group10/finalize_signals_migrations.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/group10/finalize_signals_migrations.ts
@@ -178,7 +178,6 @@ export default ({ getService }: FtrProviderContext): void => {
 
       const statusAfter: StatusResponse[] = bodyAfter.indices;
       expect(statusAfter.map((s) => s.index)).to.eql([
-        '.internal.alerts-security.alerts-default-000001',
         ...createdMigrations.map((c) => c.migration_index),
       ]);
       expect(statusAfter.map((s) => s.is_outdated)).to.eql([false, false]);
