96990aeb3262bd843ce19ccda6231bf63233de5a
Jiawei Wu <jiawei.wu@cmd.com>
2023-03-30 09:40:13 -0700

    remove tz in test

--- x-pack/plugins/alerting/jest.config.js
+++ x-pack/plugins/alerting/jest.config.js
@@ -4,9 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-process.env.TZ = 'Asia/Calcutta';
-
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../..',
