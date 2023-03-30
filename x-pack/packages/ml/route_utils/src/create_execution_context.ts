563a06deb151cfb227179208e3d60ff67dcafa22
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-03-30 12:47:04 +0200

    fix API docs

--- x-pack/packages/ml/route_utils/src/create_execution_context.ts
+++ x-pack/packages/ml/route_utils/src/create_execution_context.ts
@@ -7,6 +7,16 @@
 
 import type { CoreStart } from '@kbn/core/server';
 
+/**
+ * Creates an execution context to be passed on as part of ES queries.
+ * This allows to identify the source triggering a request when debugging slow logs.
+ *
+ * @param coreStart Kibana CoreStart
+ * @param name Context name, usually the plugin id
+ * @param id Optional context id, can be used to override the default usage of page as id
+ * @param type Optional context type, defaults to `application`.
+ * @returns
+ */
 export function createExecutionContext(
   coreStart: CoreStart,
   name: string,
