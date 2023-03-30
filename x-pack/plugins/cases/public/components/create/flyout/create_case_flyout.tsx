64a7f9268960a797bba42e50ac9175ac7eb06aef
konrad.szwarc <konrad.szwarc@elastic.co>
2023-03-30 09:37:38 +0200

    Revert "fix mask issue"
    This reverts commit 6f8f8787d18f1f8f671da7ec1fc9e1ef5545ebab.

--- x-pack/plugins/cases/public/components/create/flyout/create_case_flyout.tsx
+++ x-pack/plugins/cases/public/components/create/flyout/create_case_flyout.tsx
@@ -72,7 +72,7 @@ export const CreateCaseFlyout = React.memo<CreateCaseFlyoutProps>(
           tour-step="create-case-flyout"
           data-test-subj="create-case-flyout"
           // maskProps is needed in order to apply the z-index to the parent overlay element, not to the flyout only
-          maskProps={{ headerZindexLocation: 'below' }}
+          maskProps={{ headerZindexLocation: 'above' }}
         >
           <EuiFlyoutHeader data-test-subj="create-case-flyout-header" hasBorder>
             <EuiTitle size="m">
