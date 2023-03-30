99b8296cf53e5ce50a0f7184414d1b7284d57a9c
Cauê Hironimuz Marcondes <caue.marcondes@elastic.co>
2023-03-30 10:15:20 -0400

    addressing PR comments

--- x-pack/plugins/profiling/public/components/flame_graphs_view/calculate_impact_estimates.test.ts
+++ x-pack/plugins/profiling/public/components/flame_graphs_view/calculate_impact_estimates.test.ts
@@ -7,6 +7,31 @@
 import { calculateImpactEstimates } from './calculate_impact_estimates';
 
 describe('calculateImpactEstimates', () => {
+  it('calculates impact when countExclusive is lower than countInclusive', () => {
+    expect(
+      calculateImpactEstimates({
+        countExclusive: 500,
+        countInclusive: 1000,
+        totalSamples: 10000,
+        totalSeconds: 15 * 60, // 15m
+      })
+    ).toEqual({
+      annualizedCo2: 17.909333333333336,
+      annualizedCo2NoChildren: 8.954666666666668,
+      annualizedCoreSeconds: 1752000,
+      annualizedCoreSecondsNoChildren: 876000,
+      annualizedDollarCost: 20.683333333333334,
+      annualizedDollarCostNoChildren: 10.341666666666667,
+      co2: 0.0005111111111111112,
+      co2NoChildren: 0.0002555555555555556,
+      coreSeconds: 50,
+      coreSecondsNoChildren: 25,
+      dollarCost: 0.0005902777777777778,
+      dollarCostNoChildren: 0.0002951388888888889,
+      percentage: 0.1,
+      percentageNoChildren: 0.05,
+    });
+  });
   it('calculates impact', () => {
     expect(
       calculateImpactEstimates({
