a44dd619638562211c787efc68825ffef1b5d0b8
Mark Hopkin <mark.hopkin@elastic.co>
2023-03-31 14:02:45 +0100

    move to getBySel

--- x-pack/plugins/fleet/cypress/e2e/package_policy_real.cy.ts
+++ x-pack/plugins/fleet/cypress/e2e/package_policy_real.cy.ts
@@ -4,17 +4,20 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+import { ADD_INTEGRATION_POLICY_BTN } from '../screens/integrations';
+
 export {};
 
 const TEST_PACKAGE = 'input_package-1.0.0';
-const agentPolicyId = 'test-input-package-policy';
-const agentPolicyName = 'Test input package policy';
-const inputPackagePolicyName = 'input-package-policy';
+const agentPolicyId = 'test-input-package-policy-2';
+const agentPolicyName = 'Test input package policy-2';
+const inputPackagePolicyName = 'input-package-policy-2';
 
 function editPackagePolicyandShowAdvanced() {
   cy.visit(`/app/integrations/detail/${TEST_PACKAGE}/policies`);
 
-  cy.get('[data-test-subj="integrationNameLink"]').contains(inputPackagePolicyName).click();
+  cy.getBySel('integrationNameLink').contains(inputPackagePolicyName).click();
 
   cy.get('button').contains('Change defaults').click();
   cy.get('[data-test-subj^="advancedStreamOptionsToggle"]').click();
@@ -50,44 +53,41 @@ describe('Input package create and edit package policy', () => {
   });
   it('should successfully create a package policy', () => {
     cy.visit(`/app/integrations/detail/${TEST_PACKAGE}/overview`);
-    cy.get('[data-test-subj="addIntegrationPolicyButton"]').click();
+    cy.getBySel(ADD_INTEGRATION_POLICY_BTN).click();
 
-    cy.get('[data-test-subj="packagePolicyNameInput"]')
-      .click()
-      .clear()
-      .type(inputPackagePolicyName);
-    cy.get('[data-test-subj="multiTextInput-paths"]')
+    cy.getBySel('packagePolicyNameInput').click().clear().type(inputPackagePolicyName);
+    cy.getBySel('multiTextInput-paths')
       .find('[data-test-subj="multiTextInputRow-0"]')
       .click()
       .type('/var/log/test.log');
 
-    cy.get('[data-test-subj="multiTextInput-tags"]')
+    cy.getBySel('multiTextInput-tags')
       .find('[data-test-subj="multiTextInputRow-0"]')
       .click()
       .type('tag1');
 
-    cy.get('[data-test-subj="datasetComboBox"]').click().type('testdataset');
+    cy.getBySel('datasetComboBox').click().type('testdataset');
 
-    cy.get('[data-test-subj="existingHostsTab"]').click();
+    cy.getBySel('existingHostsTab').click();
 
-    cy.get('[data-test-subj="agentPolicySelect"]').click().get(`#${agentPolicyId}`).click();
+    cy.getBySel('agentPolicySelect').click().get(`#${agentPolicyId}`).click();
     cy.wait(500); // wait for policy id to be set
-    cy.get('[data-test-subj="createPackagePolicySaveButton"]').click();
+    cy.getBySel('createPackagePolicySaveButton').click();
 
-    cy.get('[data-test-subj="confirmModalCancelButton"]').click();
+    cy.getBySel('confirmModalCancelButton').click();
   });
 
   it('should show pipelines editor with link to pipeline', () => {
     editPackagePolicyandShowAdvanced();
-    cy.get('[data-test-subj="datastreamInspectPipelineBtn"]').click();
-    cy.get('[data-test-subj="confirmModalConfirmButton"]').click();
+    cy.getBySel('datastreamInspectPipelineBtn').click();
+    cy.getBySel('confirmModalConfirmButton').click();
     cy.get('body').should('not.contain', 'Pipeline not found');
     cy.get('body').should('contain', '"managed_by": "fleet"');
   });
   it('should show mappings editor with link to create custom template', () => {
     editPackagePolicyandShowAdvanced();
-    cy.get('[data-test-subj="datastreamEditMappingsBtn"]').click();
-    cy.get('[data-test-subj="confirmModalConfirmButton"]').click();
+    cy.getBySel('datastreamEditMappingsBtn').click();
+    cy.getBySel('confirmModalConfirmButton').click();
     cy.get('body').should('contain', 'logs-testdataset@custom');
   });
 });
