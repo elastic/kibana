c02914871d7e942b3352c6a80522e3ba22d56730
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2023-03-31 09:02:45 +0100

    find

--- x-pack/plugins/alerting/server/rules_client/methods/find.ts
+++ x-pack/plugins/alerting/server/rules_client/methods/find.ts
@@ -9,7 +9,8 @@ import Boom from '@hapi/boom';
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { pick } from 'lodash';
 import { KueryNode, nodeBuilder } from '@kbn/es-query';
-import { RawRule, RuleTypeParams, SanitizedRule } from '../../types';
+import { AlertConsumers } from '@kbn/rule-data-utils';
+import { RawRule, RuleTypeParams, SanitizedRule, Rule } from '../../types';
 import { AlertingAuthorizationEntity } from '../../authorization';
 import { ruleAuditEvent, RuleAuditAction } from '../common/audit_events';
 import {
@@ -27,7 +28,7 @@ import {
 import { alertingAuthorizationFilterOpts } from '../common/constants';
 import { getAlertFromRaw } from '../lib/get_alert_from_raw';
 import type { IndexType, RulesClientContext } from '../types';
-import { formatRulesHook } from '../lib';
+import { formatLegacyActionsForSiemRules } from '../lib';
 
 export interface FindParams {
   options?: FindOptions;
@@ -133,6 +134,8 @@ export async function find<Params extends RuleTypeParams = never>(
     type: 'alert',
   });
 
+  const siemRules: Rule[] = [];
+
   const authorizedData = data.map(({ id, attributes, references }) => {
     try {
       ensureRuleTypeIsAuthorized(
@@ -150,7 +153,8 @@ export async function find<Params extends RuleTypeParams = never>(
       );
       throw error;
     }
-    return getAlertFromRaw<Params>(
+
+    const rule = getAlertFromRaw<Params>(
       context,
       id,
       attributes.alertTypeId,
@@ -160,6 +164,13 @@ export async function find<Params extends RuleTypeParams = never>(
       excludeFromPublicApi,
       includeSnoozeData
     );
+
+    // collect SIEM rule for further formatting legacy actions
+    if (attributes.consumer === AlertConsumers.SIEM) {
+      siemRules.push(rule);
+    }
+
+    return rule;
   });
 
   authorizedData.forEach(({ id }) =>
@@ -171,10 +182,31 @@ export async function find<Params extends RuleTypeParams = never>(
     )
   );
 
+  // format legacy actions for SIEM rules, if there any
+  if (siemRules.length) {
+    const formattedRules = await formatLegacyActionsForSiemRules(siemRules, {
+      savedObjectsClient: context.unsecuredSavedObjectsClient,
+      logger: context.logger,
+    });
+
+    const formattedRulesMap = formattedRules.reduce<Record<string, Rule>>((acc, rule) => {
+      acc[rule.id] = rule;
+      return acc;
+    }, {});
+
+    return {
+      page,
+      perPage,
+      total,
+      // replace siem formatted rules
+      data: authorizedData.map((rule) => formattedRulesMap[rule.id] ?? rule),
+    };
+  }
+
   return {
     page,
     perPage,
     total,
-    data: await formatRulesHook(context, { rules: authorizedData }),
+    data: authorizedData,
   };
 }
