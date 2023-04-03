9a764883bc7e721f1c5dacd342586c8a6e7c27a3
Jenny <dzheni.pavlova@elastic.co>
2023-04-03 16:15:29 +0200

    Add APM link

--- x-pack/plugins/infra/public/pages/metrics/hosts/components/host_details_flyout/flyout.tsx
+++ x-pack/plugins/infra/public/pages/metrics/hosts/components/host_details_flyout/flyout.tsx
@@ -19,12 +19,14 @@ import { EuiSpacer, EuiTabs, EuiTab } from '@elastic/eui';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
 import { EuiIcon } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
+import { css } from '@emotion/react';
 import { InventoryItemType } from '../../../../../../common/inventory_models/types';
 import { InfraClientCoreStart, InfraClientStartDeps } from '../../../../../types';
 import { MetadataTab } from './metadata/metadata';
 import type { HostNodeRow } from '../../hooks/use_hosts_table';
 import { useUnifiedSearchContext } from '../../hooks/use_unified_search';
 import { navigateToUptime } from './links/navigate_to_uptime';
+import { LinkToApmTraces } from './links/link-to-apm-traces';
 
 interface Props {
   node: HostNodeRow;
@@ -58,14 +60,13 @@ export const Flyout = ({ node, closeFlyout }: Props) => {
   return (
     <EuiFlyout onClose={closeFlyout} ownFocus={false}>
       <EuiFlyoutHeader hasBorder>
-        <EuiFlexGroup>
+        <EuiFlexGroup justifyContent="spaceBetween">
           <EuiFlexItem>
-            {' '}
             <EuiTitle size="xs">
               <h2>{node.name}</h2>
             </EuiTitle>
           </EuiFlexItem>
-          <EuiFlexItem>
+          <EuiFlexItem grow={false}>
             <EuiLink
               data-test-subj="infraFlyoutLink"
               onClick={() => navigateToUptime(share.url.locators, NODE_TYPE, node)}
@@ -76,6 +77,14 @@ export const Flyout = ({ node, closeFlyout }: Props) => {
                 defaultMessage="Uptime"
               />
             </EuiLink>
+          </EuiFlexItem>
+          <EuiFlexItem
+            grow={false}
+            css={css`
+              padding-right: 20px;
+            `}
+          >
+            <LinkToApmTraces hostName={node.name} apmField={'host.hostname'} />
           </EuiFlexItem>
         </EuiFlexGroup>
         <EuiSpacer size="s" />
---
+++ x-pack/plugins/infra/public/pages/metrics/hosts/components/host_details_flyout/links/link-to-apm-traces.tsx
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React from 'react';
+import { stringify } from 'querystring';
+import { encode } from '@kbn/rison';
+import { RedirectAppLinks } from '@kbn/shared-ux-link-redirect-app';
+import { EuiLink } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { EuiIcon } from '@elastic/eui';
+import { useKibanaContextForPlugin } from '../../../../../../hooks/use_kibana';
+
+interface LinkToApmTracesProps {
+  hostName: string;
+  apmField: string;
+}
+
+export const LinkToApmTraces = ({ hostName, apmField }: LinkToApmTracesProps) => {
+  const { services } = useKibanaContextForPlugin();
+  const { http } = services;
+
+  const queryString = new URLSearchParams(
+    encode(
+      stringify({
+        kuery: `${apmField}:"${hostName}"`,
+      })
+    )
+  );
+
+  const linkToApmTraces = http.basePath.prepend(`/app/apm/traces?${queryString}`);
+
+  return (
+    <RedirectAppLinks coreStart={services}>
+      <EuiLink href={linkToApmTraces} data-test-subj="hostsView-flyout-link-to-apm-button">
+        <EuiIcon type="popout" />{' '}
+        <FormattedMessage
+          id="xpack.infra.hostsViewPage.flyout.linkToApmTraces"
+          defaultMessage="APM Traces"
+        />
+      </EuiLink>
+    </RedirectAppLinks>
+  );
+};
