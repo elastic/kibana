6eb2c5ed91cfa31281602903cb3a5960c0ee1b7b
Janki Salvi <117571355+js-jankisalvi@users.noreply.github.com>
2023-04-04 09:25:21 +0200

    [Cases] Filter user activities pagination (#152702)
    ## Summary
    
    Implements #130227
    
    
    
    https://user-images.githubusercontent.com/117571355/224038340-6b1c8cc7-3795-4412-8e67-a026dfe10776.mov
    
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [x] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    ### Flaky Test Runner
    https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/2059
    
    ### For maintainers
    
    - [x] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/cases/public/containers/use_infinite_find_case_user_actions.tsx
+++ x-pack/plugins/cases/public/containers/use_infinite_find_case_user_actions.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import type { FindCaseUserActions, CaseUserActionTypeWithAll } from '../../common/ui/types';
 import { findCaseUserActions } from './api';
 import type { ServerError } from '../types';
@@ -13,12 +13,11 @@ import { useCasesToast } from '../common/use_cases_toast';
 import { ERROR_TITLE } from './translations';
 import { casesQueriesKeys } from './constants';
 
-export const useFindCaseUserActions = (
+export const useInfiniteFindCaseUserActions = (
   caseId: string,
   params: {
     type: CaseUserActionTypeWithAll;
     sortOrder: 'asc' | 'desc';
-    page: number;
     perPage: number;
   },
   isEnabled: boolean
@@ -26,18 +25,26 @@ export const useFindCaseUserActions = (
   const { showErrorToast } = useCasesToast();
   const abortCtrlRef = new AbortController();
 
-  return useQuery<FindCaseUserActions, ServerError>(
+  return useInfiniteQuery<FindCaseUserActions, ServerError>(
     casesQueriesKeys.caseUserActions(caseId, params),
-    async () => {
-      return findCaseUserActions(caseId, params, abortCtrlRef.signal);
+    async ({ pageParam = 1 }) => {
+      return findCaseUserActions(caseId, { ...params, page: pageParam }, abortCtrlRef.signal);
     },
     {
       enabled: isEnabled,
       onError: (error: ServerError) => {
         showErrorToast(error, { title: ERROR_TITLE });
       },
+      getNextPageParam: (lastPage, pages) => {
+        const lastPageNumber = Math.ceil(lastPage.total / lastPage.perPage);
+        // here last page fetching is skipped because last page is fetched separately using useQuery hook
+        if (lastPage.page < lastPageNumber - 1) {
+          return lastPage.page + 1;
+        }
+        return undefined;
+      },
     }
   );
 };
 
-export type UseFindCaseUserActions = ReturnType<typeof useFindCaseUserActions>;
+export type UseInfiniteFindCaseUserActions = ReturnType<typeof useInfiniteFindCaseUserActions>;
