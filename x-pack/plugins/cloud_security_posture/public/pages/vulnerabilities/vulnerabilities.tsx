861e5aa2c731e8b6cdada423d996a8f9988e60a0
Omolola Akinleye <omolola.akinleye@elastic.co>
2023-04-05 14:07:35 -0400

    change file name and use hasVulnMgmtFindings reference

--- x-pack/plugins/cloud_security_posture/public/pages/vulnerabilities/vulnerabilities.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/vulnerabilities/vulnerabilities.tsx
@@ -6,12 +6,10 @@
  */
 import React from 'react';
 
-import {
-  LATEST_FINDINGS_INDEX_DEFAULT_NS,
-  LATEST_VULNERABILITIES_INDEX_DEFAULT_NS,
-} from '../../../common/constants';
+import { LATEST_VULNERABILITIES_INDEX_DEFAULT_NS } from '../../../common/constants';
 import { useLatestFindingsDataView } from '../../common/api/use_latest_findings_data_view';
 import { useCspSetupStatusApi } from '../../common/api/use_setup_status_api';
+import { getCpmStatus } from '../../common/utils/get_cpm_status';
 import { CloudPosturePage } from '../../components/cloud_posture_page';
 import { NoVulnerabilitiesStates } from '../../components/no_vulnerabilities_states';
 import { VULNERABILITIES_CONTAINER_TEST_SUBJ } from '../../components/test_subjects';
@@ -19,11 +17,9 @@ import { VULNERABILITIES_CONTAINER_TEST_SUBJ } from '../../components/test_subje
 export const Vulnerabilities = () => {
   const dataViewQuery = useLatestFindingsDataView(LATEST_VULNERABILITIES_INDEX_DEFAULT_NS);
   const getSetupStatus = useCspSetupStatusApi();
-  const hasVulnerabilities =
-    getSetupStatus.data?.indicesDetails.some(
-      (index) => index.status === 'not-empty' && index.index === LATEST_FINDINGS_INDEX_DEFAULT_NS
-    ) || getSetupStatus.data?.vuln_mgmt.status === 'indexed';
-  if (!hasVulnerabilities) return <NoVulnerabilitiesStates />;
+
+  const { hasVulnMgmtFindings } = getCpmStatus(getSetupStatus.data);
+  if (!hasVulnMgmtFindings) return <NoVulnerabilitiesStates />;
 
   return (
     <CloudPosturePage query={dataViewQuery}>
