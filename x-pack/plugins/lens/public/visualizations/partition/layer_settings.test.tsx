11573f365ea6a6896597b9443cdd685a3cffe2e1
dej611 <dej611@gmail.com>
2023-04-05 14:50:21 +0200

    :white_check_mark: Add test for appereance section

--- x-pack/plugins/lens/public/visualizations/partition/layer_settings.test.tsx
+++ x-pack/plugins/lens/public/visualizations/partition/layer_settings.test.tsx
@@ -25,12 +25,15 @@ describe('layer settings', () => {
     });
 
     const layerId = 'layer-id';
-    const props: VisualizationLayerSettingsProps<PieVisualizationState> = {
+    const props: VisualizationLayerSettingsProps<PieVisualizationState> & {
+      section: 'data' | 'appereance';
+    } = {
       setState: jest.fn(),
       layerId,
       state: getState(false),
       frame: {} as FramePublicAPI,
       panelRef: {} as React.MutableRefObject<HTMLDivElement | null>,
+      section: 'data',
     };
 
     it('toggles multiple metrics', () => {
@@ -89,6 +92,10 @@ describe('layer settings', () => {
           <LayerSettings {...props} state={{ ...getState(false), shape: 'mosaic' }} />
         ).isEmptyRender()
       ).toBeTruthy();
+    });
+
+    test('should not render anything for the appereance section', () => {
+      expect(shallow(<LayerSettings {...props} section="appereance" />).isEmptyRender());
     });
   });
 });
