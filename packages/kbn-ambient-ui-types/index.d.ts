b03686d46fd4b9d66882783f7a961c0a1a2681b9
rshen <rshen@elastic.co>
2023-04-05 09:37:30 -0600

    add to index.d.ts in kbn-ambient-ui-types attributes export in mdx

--- packages/kbn-ambient-ui-types/index.d.ts
+++ packages/kbn-ambient-ui-types/index.d.ts
@@ -31,7 +31,8 @@ declare module '*.gif' {
 }
 
 declare module '*.mdx' {
-  let MDXComponent: (props: any) => JSX.Element;
+  const MDXComponent: (props: any) => JSX.Element;
   // eslint-disable-next-line import/no-default-export
   export default MDXComponent;
+  export const attributes: Record<string, unknown> | undefined;
 }
