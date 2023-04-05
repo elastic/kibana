c59d9c79f570132cd9bd77a525770dd8f419a322
Davis McPhee <davis.mcphee@elastic.co>
2023-04-05 13:22:45 -0300

    Add initial extension types to DiscoverExtension registry

---
+++ src/plugins/discover/public/extensions/extension_types/data_grid_extension.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import type { EuiDataGridControlColumn } from '@elastic/eui';
+
+export interface DefaultLeadingControlColumn {
+  disabled?: boolean;
+}
+
+export interface DefaultLeadingControlColumns {
+  expand?: DefaultLeadingControlColumn;
+  select?: DefaultLeadingControlColumn;
+}
+
+export interface DataGridExtension {
+  id: 'data_grid';
+  defaultLeadingControlColumns?: DefaultLeadingControlColumns;
+  getLeadingControlColumns?: () => EuiDataGridControlColumn[];
+}
