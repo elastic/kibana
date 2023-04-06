834239f7bb662b473444fff843e786da4332b248
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-04-06 16:20:09 +0200

    [Discover] Unskip context filters tests

--- test/functional/services/data_grid.ts
+++ test/functional/services/data_grid.ts
@@ -202,12 +202,23 @@ export class DataGridService extends FtrService {
   public async clickRowToggle(
     options: SelectOptions = { isAnchorRow: false, rowIndex: 0 }
   ): Promise<void> {
+    const isFlyoutOpen = await this.testSubjects.exists('docTableDetailsFlyout');
     const row = await this.getRow(options);
     const testSubj = options.isAnchorRow
       ? '~docTableExpandToggleColumnAnchor'
       : '~docTableExpandToggleColumn';
     const toggle = await row[0].findByTestSubject(testSubj);
-    await toggle.click();
+
+    await this.retry.try(async () => {
+      await toggle.click();
+      if (isFlyoutOpen) {
+        // should close the flyout
+        await this.testSubjects.missingOrFail('docTableDetailsFlyout');
+      } else {
+        // should open the flyout
+        await this.testSubjects.existOrFail('docTableDetailsFlyout');
+      }
+    });
   }
 
   public async getDetailsRows(): Promise<WebElementWrapper[]> {
