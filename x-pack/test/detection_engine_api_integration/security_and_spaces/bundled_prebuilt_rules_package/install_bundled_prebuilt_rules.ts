956ef8a8a00adbb7f5a768fb172d2aa5a989a72f
jpdjere <jpdjeredjian@gmail.com>
2023-04-06 12:08:17 +0200

    Add tests for bundled local zip package

---
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/bundled_prebuilt_rules_package/install_bundled_prebuilt_rules.ts
@@ -0,0 +1,114 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import fs from 'fs/promises';
+import path from 'path';
+
+import expect from 'expect';
+import { FtrProviderContext } from '../../common/ftr_provider_context';
+import {
+  deleteAllRules,
+  deleteAllTimelines,
+  getPrebuiltRulesAndTimelinesStatus,
+} from '../../utils';
+import { deleteAllPrebuiltRuleAssets } from '../../utils/prebuilt_rules/delete_all_prebuilt_rule_assets';
+import { installPrebuiltRulesFleetPackage } from '../../utils/prebuilt_rules/install_prebuilt_rules_fleet_package';
+import { installPrebuiltRulesAndTimelines } from '../../utils/prebuilt_rules/install_prebuilt_rules_and_timelines';
+import { deletePrebuiltRulesFleetPackage } from '../../utils/prebuilt_rules/delete_prebuilt_rules_fleet_package';
+import { BUNDLED_PACKAGE_DIR } from './config';
+
+// eslint-disable-next-line import/no-default-export
+export default ({ getService }: FtrProviderContext): void => {
+  const es = getService('es');
+  const supertest = getService('supertest');
+  const log = getService('log');
+
+  const BUNDLED_FLEET_PACKAGE_DIR = path.join(
+    path.dirname(__filename),
+    './tmp/fleet_bundled_packages'
+  );
+
+  const bundlePackage = async (name: string) => {
+    try {
+      await fs.access(BUNDLED_PACKAGE_DIR);
+    } catch (error) {
+      await fs.mkdir(BUNDLED_PACKAGE_DIR);
+    }
+
+    await fs.copyFile(
+      path.join(BUNDLED_FLEET_PACKAGE_DIR, `${name}.zip`),
+      path.join(BUNDLED_PACKAGE_DIR, `${name}.zip`)
+    );
+  };
+
+  const removeBundledPackages = async () => {
+    try {
+      const files = await fs.readdir(BUNDLED_PACKAGE_DIR);
+
+      for (const file of files) {
+        await fs.unlink(path.join(BUNDLED_PACKAGE_DIR, file));
+      }
+    } catch (error) {
+      log.error('Error removing bundled packages');
+      log.error(error);
+    }
+  };
+
+  describe('install_bundled_prebuilt_rules', () => {
+    beforeEach(async () => {
+      await deletePrebuiltRulesFleetPackage(supertest);
+      await deleteAllRules(supertest, log);
+      await deleteAllTimelines(es);
+      await deleteAllPrebuiltRuleAssets(es);
+    });
+
+    afterEach(async () => {
+      await removeBundledPackages();
+    });
+
+    it('should install prebuilt rules from the package that comes bundled with Kibana', async () => {
+      // Verify that status is empty before package installation
+      const statusBeforePackageInstallation = await getPrebuiltRulesAndTimelinesStatus(supertest);
+      expect(statusBeforePackageInstallation.rules_installed).toBe(0);
+      expect(statusBeforePackageInstallation.rules_not_installed).toBe(0);
+      expect(statusBeforePackageInstallation.rules_not_updated).toBe(0);
+
+      await bundlePackage('security_detection_engine-8.7.1');
+      await installPrebuiltRulesFleetPackage({
+        supertest,
+        overrideExistingPackage: true,
+        version: '8.7.1',
+      });
+
+      const bundledInstallResponse = await supertest
+        .post(`/api/fleet/epm/packages/security_detection_engine/8.7.1`)
+        .set('kbn-xsrf', 'xxxx')
+        .type('application/json')
+        .send({ force: true })
+        .expect(200);
+
+      // As opposed to "registry"
+      expect(bundledInstallResponse.body._meta.install_source).toBe('bundled');
+
+      // Verify that status is updated after package installation
+      const statusAfterPackageInstallation = await getPrebuiltRulesAndTimelinesStatus(supertest);
+      expect(statusAfterPackageInstallation.rules_installed).toBe(0);
+      expect(statusAfterPackageInstallation.rules_not_installed).toBeGreaterThan(0);
+      expect(statusAfterPackageInstallation.rules_not_updated).toBe(0);
+
+      // Verify that all previously not installed rules were installed
+      const response = await installPrebuiltRulesAndTimelines(supertest);
+      expect(response.rules_installed).toBe(statusAfterPackageInstallation.rules_not_installed);
+      expect(response.rules_updated).toBe(0);
+
+      // Verify that status is updated after rules installation
+      const statusAfterRuleInstallation = await getPrebuiltRulesAndTimelinesStatus(supertest);
+      expect(statusAfterRuleInstallation.rules_installed).toBe(response.rules_installed);
+      expect(statusAfterRuleInstallation.rules_not_installed).toBe(0);
+      expect(statusAfterRuleInstallation.rules_not_updated).toBe(0);
+    });
+  });
+};
