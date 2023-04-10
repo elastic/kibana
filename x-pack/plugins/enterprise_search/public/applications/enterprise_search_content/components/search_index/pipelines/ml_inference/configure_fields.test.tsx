36e9a085afc014e96368f1995ebf65fad32183fc
Adam Demjen <demjened@gmail.com>
2023-04-10 17:32:58 -0400

    Add unit tests

---
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/configure_fields.test.tsx
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { setMockValues } from '../../../../../__mocks__/kea_logic';
+
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { ConfigureFields } from './configure_fields';
+import { MultiFieldMapping, SelectedFieldMappings } from './multi_field_selector';
+import { SingleFieldMapping } from './single_field_selector';
+
+describe('ConfigureFields', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setMockValues({});
+  });
+
+  it('renders multi-field selector components if non-text expansion model is selected', () => {
+    setMockValues({
+      isTextExpansionModelSelected: false,
+    });
+    const wrapper = shallow(<ConfigureFields />);
+    expect(wrapper.find(SingleFieldMapping)).toHaveLength(1);
+    expect(wrapper.find(MultiFieldMapping)).toHaveLength(0);
+    expect(wrapper.find(SelectedFieldMappings)).toHaveLength(0);
+  });
+  it('renders multi-field selector components if text expansion model is selected', () => {
+    setMockValues({
+      isTextExpansionModelSelected: true,
+    });
+    const wrapper = shallow(<ConfigureFields />);
+    expect(wrapper.find(SingleFieldMapping)).toHaveLength(0);
+    expect(wrapper.find(MultiFieldMapping)).toHaveLength(1);
+    expect(wrapper.find(SelectedFieldMappings)).toHaveLength(1);
+  });
+});
---
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/multi_field_selector.test.tsx
@@ -0,0 +1,104 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { setMockValues } from '../../../../../__mocks__/kea_logic';
+
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { MultiFieldMapping, SelectedFieldMappings } from './multi_field_selector';
+import { EuiBasicTable, EuiComboBox } from '@elastic/eui';
+
+const DEFAULT_VALUES = {
+  addInferencePipelineModal: {
+    configuration: {},
+  },
+  sourceFields: ['my-source-field1', 'my-source-field2', 'my-source-field3'],
+};
+
+describe('MultiFieldMapping', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setMockValues({});
+  });
+  it('renders multi field selector with options', () => {
+    setMockValues(DEFAULT_VALUES);
+    const wrapper = shallow(<MultiFieldMapping />);
+
+    expect(wrapper.find(EuiComboBox)).toHaveLength(1);
+    const comboBox = wrapper.find(EuiComboBox);
+    expect(comboBox.prop('options')).toEqual([
+      {
+        label: 'my-source-field1',
+      },
+      {
+        label: 'my-source-field2',
+      },
+      {
+        label: 'my-source-field3',
+      }
+    ]);
+  });
+  it('renders multi field selector with options excluding mapped and selected fields', () => {
+    setMockValues({
+      ...DEFAULT_VALUES,
+      addInferencePipelineModal: {
+        configuration: {
+          fieldMappings: [
+            {
+              sourceField: 'my-source-field2',
+              targetField: 'my-target-field2',
+            },
+          ],
+        },
+        selectedSourceFields: ['my-source-field1'],
+      },
+    });
+    const wrapper = shallow(<MultiFieldMapping />);
+
+    expect(wrapper.find(EuiComboBox)).toHaveLength(1);
+    const comboBox = wrapper.find(EuiComboBox);
+    expect(comboBox.prop('options')).toEqual([
+      {
+        label: 'my-source-field3',
+      },
+    ]);
+  });
+});
+
+describe('SelectedFieldMappings', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setMockValues({});
+  });
+  it('renders field mapping list', () => {
+    const mockValues = {
+      ...DEFAULT_VALUES,
+      addInferencePipelineModal: {
+        configuration: {
+          fieldMappings: [
+            {
+              sourceField: 'my-source-field1',
+              targetField: 'my-target-field1',
+            },
+            {
+              sourceField: 'my-source-field2',
+              targetField: 'my-target-field2',
+            },
+          ],
+        },
+      },
+    }
+    setMockValues(mockValues);
+    const wrapper = shallow(<SelectedFieldMappings />);
+
+    expect(wrapper.find(EuiBasicTable)).toHaveLength(1);
+    const table = wrapper.find(EuiBasicTable);
+    expect(table.prop('items')).toEqual(mockValues.addInferencePipelineModal.configuration.fieldMappings);
+  });
+});
