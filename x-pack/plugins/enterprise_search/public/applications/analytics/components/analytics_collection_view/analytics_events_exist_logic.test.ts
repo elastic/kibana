e81febccbaf526ec3b98c1ced9b0ad4a37135f60
Klim Markelov <klim.markelov@elastic.co>
2023-04-11 18:14:38 +0200

    Replace check_analytics_events_index check with check_analytics_events that checks the number of documents for the datastream

--- x-pack/plugins/enterprise_search/public/applications/analytics/components/analytics_collection_view/analytics_events_exist_logic.test.ts
+++ x-pack/plugins/enterprise_search/public/applications/analytics/components/analytics_collection_view/analytics_events_exist_logic.test.ts
@@ -9,10 +9,10 @@ import { LogicMounter } from '../../../__mocks__/kea_logic';
 
 import { Status } from '../../../../../common/types/api';
 
-import { AnalyticsEventsIndexExistsLogic } from './analytics_events_index_exists_logic';
+import { AnalyticsEventsExistLogic } from './analytics_events_exist_logic';
 
-describe('analyticsEventsIndexExistsLogic', () => {
-  const { mount } = new LogicMounter(AnalyticsEventsIndexExistsLogic);
+describe('analyticsEventsExistLogic', () => {
+  const { mount } = new LogicMounter(AnalyticsEventsExistLogic);
   const indexName = true;
 
   beforeEach(() => {
@@ -23,25 +23,23 @@ describe('analyticsEventsIndexExistsLogic', () => {
 
   const DEFAULT_VALUES = {
     data: undefined,
-    isLoading: true,
-    isPresent: false,
+    hasEvents: false,
     status: Status.IDLE,
   };
 
   it('has expected default values', () => {
-    expect(AnalyticsEventsIndexExistsLogic.values).toEqual(DEFAULT_VALUES);
+    expect(AnalyticsEventsExistLogic.values).toEqual(DEFAULT_VALUES);
   });
 
   describe('selectors', () => {
     it('updates when apiSuccess listener triggered', () => {
-      AnalyticsEventsIndexExistsLogic.actions.apiSuccess({ exists: indexName });
+      AnalyticsEventsExistLogic.actions.apiSuccess({ exist: indexName });
 
-      expect(AnalyticsEventsIndexExistsLogic.values).toEqual({
+      expect(AnalyticsEventsExistLogic.values).toEqual({
         ...DEFAULT_VALUES,
-        isLoading: false,
-        isPresent: true,
+        hasEvents: true,
         status: Status.SUCCESS,
-        data: { exists: indexName },
+        data: { exist: indexName },
       });
     });
   });
