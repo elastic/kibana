081166bb271bbdd21ed74b649b7ace78db044b20
Kyle Pollich <kyle.pollich@elastic.co>
2023-04-12 13:49:58 -0400

    [8.7] [Fleet] fix pr condition to unblock prs if available versions not ava… (#154835)
    …ilable (#154808)
    
    ## Summary
    
    Fixing pr condition in https://github.com/elastic/kibana/pull/154649
    
    ---------
    
    Co-authored-by: kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Kyle Pollich <kyle.pollich@elastic.co>
    (cherry picked from commit 0efdf351e607714c4a9d8208515f4a40498da360)
    
    # Conflicts:
    # src/dev/build/tasks/fetch_agent_versions_list.test.ts #
    src/dev/build/tasks/fetch_agent_versions_list.ts
    
    ## Summary
    
    Summarize your PR. If it involves visual changes include a screenshot or
    gif.
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [ ] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [ ] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    
    ### Risk Matrix
    
    Delete this section if it is not applicable to this PR.
    
    Before closing this PR, invite QA, stakeholders, and other developers to
    identify risks that should be tested prior to the change/feature
    release.
    
    When forming the risk matrix, consider some of the following examples
    and how they may potentially impact the change:
    
    | Risk | Probability | Severity | Mitigation/Notes |
    
    |---------------------------|-------------|----------|-------------------------|
    | Multiple Spaces&mdash;unexpected behavior in non-default Kibana Space.
    | Low | High | Integration tests will verify that all features are still
    supported in non-default Kibana Space and when user switches between
    spaces. |
    | Multiple nodes&mdash;Elasticsearch polling might have race conditions
    when multiple Kibana nodes are polling for the same tasks. | High | Low
    | Tasks are idempotent, so executing them multiple times will not result
    in logical error, but will degrade performance. To test for this case we
    add plenty of unit tests around this logic and document manual testing
    procedure. |
    | Code should gracefully handle cases when feature X or plugin Y are
    disabled. | Medium | High | Unit tests will verify that any feature flag
    or plugin combination still results in our service operational. |
    | [See more potential risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx) |
    
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: Julia Bardi <90178898+juliaElastic@users.noreply.github.com>

--- src/dev/build/tasks/fetch_agent_versions_list.ts
+++ src/dev/build/tasks/fetch_agent_versions_list.ts
@@ -11,6 +11,9 @@ import fetch from 'node-fetch';
 import { ToolingLog } from '@kbn/tooling-log';
 import { write, Task } from '../lib';
 
+const isPr = () =>
+  !!process.env.BUILDKITE_PULL_REQUEST && process.env.BUILDKITE_PULL_REQUEST !== 'false';
+
 const getAvailableVersions = async (log: ToolingLog) => {
   const options = {
     headers: {
@@ -41,7 +44,7 @@ const getAvailableVersions = async (log: ToolingLog) => {
   } catch (error) {
     const errorMessage = 'Failed to fetch Elastic Agent versions list';
 
-    if (process.env.BUILDKITE_PULL_REQUEST === 'true') {
+    if (isPr()) {
       // For PR jobs, just log the error as a warning and continue
       log.warning(errorMessage);
       log.warning(error);
@@ -61,7 +64,7 @@ export const FetchAgentVersionsList: Task = {
 
   async run(config, log, build) {
     // Agent version list task is skipped for PR's, so as not to overwhelm the versions API
-    if (process.env.BUILDKITE_PULL_REQUEST === 'true') {
+    if (isPr()) {
       return;
     }
 
