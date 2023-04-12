5adbb6465b5ca7356ce33d8ae2ecce42cd4b3d69
Sébastien Loix <sabee77@gmail.com>
2023-04-12 16:32:02 +0100

    Revert "Return full map object from search"
    This reverts commit 0725ebbe53920d3c3b6eb2ed474648ea030bd64f.

--- x-pack/plugins/maps/server/content_management/maps_storage.ts
+++ x-pack/plugins/maps/server/content_management/maps_storage.ts
@@ -285,6 +285,7 @@ export class MapsStorage implements ContentStorage<MapItem, PartialMapItem> {
       page: query.cursor ? +query.cursor : undefined,
       defaultSearchOperator: 'AND',
       searchFields: onlyTitle ? ['title'] : ['title^3', 'description'],
+      fields: ['description', 'title'],
       hasReference,
       hasNoReference,
     };
