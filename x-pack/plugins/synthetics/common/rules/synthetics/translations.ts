e42dc2b0f79a4e1d30ac6d1ba94644a7aa4e4d34
Dominique Clarke <dominique.clarke@elastic.co>
2023-04-13 13:26:23 -0400

    add dynamic subject line for recovery and add tests

--- x-pack/plugins/synthetics/common/rules/synthetics/translations.ts
+++ x-pack/plugins/synthetics/common/rules/synthetics/translations.ts
@@ -33,6 +33,16 @@ export const SyntheticsMonitorStatusTranslations = {
       },
     }
   ),
+  defaultRecoverySubjectMessage: i18n.translate(
+    'xpack.synthetics.alerts.syntheticsMonitorStatus.defaultRecoverySubjectMessage',
+    {
+      defaultMessage: 'The monitor {monitorName} checking {monitorUrl} has recoverd.',
+      values: {
+        monitorName: '{{context.monitorName}}',
+        monitorUrl: '{{{context.monitorUrl}}}',
+      },
+    }
+  ),
   defaultRecoveryMessage: i18n.translate(
     'xpack.synthetics.alerts.syntheticsMonitorStatus.defaultRecoveryMessage',
     {
