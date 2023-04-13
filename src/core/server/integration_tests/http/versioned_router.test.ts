569d267e2039c659172ba82aa0ec4180a08dafcd
Jean-Louis Leysens <jloleysens@gmail.com>
2023-04-13 14:30:04 +0200

    added integration test for versioned router in "dev" and in "prod"

--- src/core/server/integration_tests/http/versioned_router.test.ts
+++ src/core/server/integration_tests/http/versioned_router.test.ts
@@ -10,21 +10,46 @@ import Supertest from 'supertest';
 import { schema } from '@kbn/config-schema';
 import { loggingSystemMock } from '@kbn/core-logging-server-mocks';
 import { executionContextServiceMock } from '@kbn/core-execution-context-server-mocks';
+import { deprecationsServiceMock } from '@kbn/core-deprecations-server-mocks';
+import { elasticsearchServiceMock } from '@kbn/core-elasticsearch-server-mocks';
+import { savedObjectsServiceMock } from '@kbn/core-saved-objects-server-mocks';
+import { uiSettingsServiceMock } from '@kbn/core-ui-settings-server-mocks';
+import { CoreRouteHandlerContext } from '@kbn/core-http-request-handler-context-server-internal';
 import { contextServiceMock } from '@kbn/core-http-context-server-mocks';
 import { createHttpServer } from '@kbn/core-http-server-mocks';
 import type { HttpService } from '@kbn/core-http-server-internal';
 import type { IRouterWithVersion } from '@kbn/core-http-server';
+import type { Env } from '@kbn/config';
 
 let server: HttpService;
 let logger: ReturnType<typeof loggingSystemMock.create>;
 
+const getContextSetup = (
+  { env }: { env: Pick<Env, 'mode'> } = {
+    env: { mode: { dev: false, prod: true, name: 'production' } },
+  }
+) =>
+  contextServiceMock.createSetupContract({
+    core: Promise.resolve(
+      new CoreRouteHandlerContext(
+        {
+          deprecations: deprecationsServiceMock.createInternalStartContract(),
+          elasticsearch: elasticsearchServiceMock.createInternalStart(),
+          savedObjects: savedObjectsServiceMock.createInternalStartContract(),
+          uiSettings: uiSettingsServiceMock.createStartContract(),
+          env,
+        },
+        {} as any
+      )
+    ),
+  });
+
 describe('Routing versioned requests', () => {
-  const contextSetup = contextServiceMock.createSetupContract();
   let router: IRouterWithVersion;
   let supertest: Supertest.SuperTest<Supertest.Test>;
 
   const setupDeps = {
-    context: contextSetup,
+    context: getContextSetup(),
     executionContext: executionContextServiceMock.createInternalSetupContract(),
   };
 
@@ -114,11 +139,17 @@ describe('Routing versioned requests', () => {
     );
   });
 
-  it('returns the expected output for failed validation', async () => {
+  it('returns the expected responses for failed validation', async () => {
     router.versioned
-      .post({ access: 'internal', path: '/test/{id}' })
+      .post({ path: '/my-path', access: 'internal' })
+      // Bad request validation
       .addVersion(
-        { validate: { request: { body: schema.object({ foo: schema.number() }) } }, version: '1' },
+        {
+          validate: {
+            request: { body: schema.object({ foo: schema.number() }) },
+          },
+          version: '1',
+        },
         async (ctx, req, res) => {
           return res.ok({ body: { v: '1' } });
         }
@@ -128,7 +159,7 @@ describe('Routing versioned requests', () => {
 
     await expect(
       supertest
-        .post('/test/1')
+        .post('/my-path')
         .send({})
         .set('Elastic-Api-Version', '1')
         .expect(400)
@@ -157,6 +188,65 @@ describe('Routing versioned requests', () => {
         .expect(200)
         .then(({ header }) => header)
     ).resolves.toEqual(expect.objectContaining({ 'elastic-api-version': '2020-02-02' }));
+  });
+
+  it('runs response validation when in dev', async () => {
+    logger = loggingSystemMock.create();
+    await server.stop(); // stop the already start server
+    server = createHttpServer({ logger });
+    await server.preboot({ context: contextServiceMock.createPrebootContract() });
+
+    const { server: innerServer, createRouter } = await server.setup({
+      ...setupDeps,
+      context: getContextSetup({ env: { mode: { dev: true, prod: false, name: 'development' } } }),
+    });
+
+    router = createRouter('/');
+    supertest = Supertest(innerServer.listener);
+
+    router.versioned
+      .get({ path: '/my-path', access: 'internal' })
+      .addVersion(
+        { validate: { response: { 200: { body: schema.number() } } }, version: '1' },
+        async (ctx, req, res) => {
+          return res.ok({ body: { v: '1' } });
+        }
+      );
+
+    await server.start();
+
+    await expect(
+      supertest
+        .get('/my-path')
+        .set('Elastic-Api-Version', '1')
+        .expect(500)
+        .then(({ body }) => body)
+    ).resolves.toEqual(
+      expect.objectContaining({
+        message: expect.stringMatching(/Failed output validation/),
+      })
+    );
+  });
+
+  it('does not run response validation in prod', async () => {
+    router.versioned
+      .get({ path: '/my-path', access: 'internal' })
+      .addVersion(
+        { validate: { response: { 200: { body: schema.number() } } }, version: '1' },
+        async (ctx, req, res) => {
+          return res.ok({ body: { v: '1' } });
+        }
+      );
+
+    await server.start();
+
+    await expect(
+      supertest
+        .get('/my-path')
+        .set('Elastic-Api-Version', '1')
+        .expect(200)
+        .then(({ body }) => body.v)
+    ).resolves.toEqual('1');
   });
 
   it('errors when no handler could be found', async () => {
