3a1e267b36c4159a6dc95db21669d297023d1075
Alexandra Doak <alexandra.doak@elastic.co>
2023-04-13 08:35:17 -0400

    Updating rrule convert function

--- x-pack/plugins/alerting/public/pages/maintenance_windows/helpers/convert_to_rrule.ts
+++ x-pack/plugins/alerting/public/pages/maintenance_windows/helpers/convert_to_rrule.ts
@@ -6,7 +6,7 @@
  */
 
 import { Moment } from 'moment';
-import { RRule, RRuleFrequencyMap } from '../types';
+import { RRule, RRuleFrequency, RRuleFrequencyMap } from '../types';
 import { Frequency, ISO_WEEKDAYS_TO_RRULE } from '../constants';
 import { getNthByWeekday } from './get_nth_by_weekday';
 import { RecurringScheduleFormProps } from '../components/schema';
@@ -24,7 +24,14 @@ export const convertToRRule = (
     tzid: timezone,
   };
 
-  if (!recurringForm) return rRule;
+  if (!recurringForm)
+    return {
+      ...rRule,
+      // default to yearly and a count of 1
+      // if the maintenance window is not recurring
+      freq: RRuleFrequency.YEARLY,
+      count: 1,
+    };
 
   if (recurringForm) {
     let form = recurringForm;
