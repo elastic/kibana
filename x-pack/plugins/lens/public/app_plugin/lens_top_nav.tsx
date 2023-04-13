bb81bd7cc6c1bc9daef0aa8232c15768874913da
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2023-04-13 13:38:18 +0300

    also apply for newTitle

--- x-pack/plugins/lens/public/app_plugin/lens_top_nav.tsx
+++ x-pack/plugins/lens/public/app_plugin/lens_top_nav.tsx
@@ -646,9 +646,7 @@ export const LensTopNavMenu = ({
                 {
                   newTitle:
                     title ||
-                    (initialContext &&
-                    'isEmbeddable' in initialContext &&
-                    initialContext.isEmbeddable
+                    (contextFromEmbeddable
                       ? i18n.translate('xpack.lens.app.convertedLabel', {
                           defaultMessage: '{title} (converted)',
                           values: {
