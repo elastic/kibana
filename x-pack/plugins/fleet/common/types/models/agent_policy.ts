22be93fbcea2e0aaa340dc7d703a4c73984456ea
Paulo Henrique <paulo.henrique@elastic.co>
2023-04-17 08:50:39 -0300

    add cloud formation types

--- x-pack/plugins/fleet/common/types/models/agent_policy.ts
+++ x-pack/plugins/fleet/common/types/models/agent_policy.ts
@@ -33,6 +33,8 @@ export interface NewAgentPolicy {
   fleet_server_host_id?: string | null;
   schema_version?: string;
   agent_features?: Array<{ name: string; enabled: boolean }>;
+  cloud_formation_stack_name?: string;
+  cloud_formation_template_url?: string;
 }
 
 export interface AgentPolicy extends Omit<NewAgentPolicy, 'id'> {
