620abf34a69095037c27cc4c8d7306cd9f39e209
rshen <rshen@elastic.co>
2023-04-17 12:45:32 -0600

    wip

--- x-pack/plugins/reporting/public/plugin.ts
+++ x-pack/plugins/reporting/public/plugin.ts
@@ -268,6 +268,7 @@ export class ReportingPublicPlugin
         catchError((err) => handleError(notifications, err, core.theme))
       )
       .subscribe();
+
     return this.getContract();
   }
 
