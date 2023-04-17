0537924cb50b2dc62dc31d1eb1588b427ce9dd19
Jean-Louis Leysens <jloleysens@gmail.com>
2023-04-17 10:14:19 +0200

    address pr feedback

--- packages/core/http/core-http-common/src/versioning.ts
+++ packages/core/http/core-http-common/src/versioning.ts
@@ -11,13 +11,12 @@
  *
  * @note
  * For public APIs: conforms to the Elastic API version specification APIs as a date string formatted as YYYY-MM-DD.
+ * Ex. 2021-01-01 -> 2022-02-02
  *
  * @note
  * For internal APIs: follow the convention of monotonic increasing integers.
+ * Ex. 1 -> 2 -> 3
  *
  * @experimental
  */
 export type ApiVersion = string;
-
-/** @internal */
-export const ELASTIC_HTTP_VERSION_HEADER = 'elastic-api-version' as const;
