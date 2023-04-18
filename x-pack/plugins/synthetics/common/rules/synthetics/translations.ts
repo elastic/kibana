56f637b82edf81b7ab6dafd54c197f1b945c487b
Dominique Clarke <dominique.clarke@elastic.co>
2023-04-18 19:54:38 -0400

    Update x-pack/plugins/synthetics/common/rules/synthetics/translations.ts

--- x-pack/plugins/synthetics/common/rules/synthetics/translations.ts
+++ x-pack/plugins/synthetics/common/rules/synthetics/translations.ts
@@ -36,7 +36,7 @@ export const SyntheticsMonitorStatusTranslations = {
   defaultRecoverySubjectMessage: i18n.translate(
     'xpack.synthetics.alerts.syntheticsMonitorStatus.defaultRecoverySubjectMessage',
     {
-      defaultMessage: 'The monitor {monitorName} checking {monitorUrl} has recoverd.',
+      defaultMessage: 'The monitor {monitorName} checking {monitorUrl} has recovered.',
       values: {
         monitorName: '{{context.monitorName}}',
         monitorUrl: '{{{context.monitorUrl}}}',
