8ab820ed8b36699612ec6d8420019a37078ea95e
Jean-Louis Leysens <jloleysens@gmail.com>
2023-04-18 15:11:32 +0200

    update full integration to pass down env value, also updated integration test

--- src/core/server/integration_tests/http/versioned_router.test.ts
+++ src/core/server/integration_tests/http/versioned_router.test.ts
@@ -7,49 +7,24 @@
  */
 
 import Supertest from 'supertest';
+import { createTestEnv, getEnvOptions } from '@kbn/config-mocks';
 import { schema } from '@kbn/config-schema';
 import { loggingSystemMock } from '@kbn/core-logging-server-mocks';
 import { executionContextServiceMock } from '@kbn/core-execution-context-server-mocks';
-import { deprecationsServiceMock } from '@kbn/core-deprecations-server-mocks';
-import { elasticsearchServiceMock } from '@kbn/core-elasticsearch-server-mocks';
-import { savedObjectsServiceMock } from '@kbn/core-saved-objects-server-mocks';
-import { uiSettingsServiceMock } from '@kbn/core-ui-settings-server-mocks';
-import { CoreRouteHandlerContext } from '@kbn/core-http-request-handler-context-server-internal';
 import { contextServiceMock } from '@kbn/core-http-context-server-mocks';
 import { createHttpServer } from '@kbn/core-http-server-mocks';
 import type { HttpService } from '@kbn/core-http-server-internal';
 import type { IRouterWithVersion } from '@kbn/core-http-server';
-import type { Env } from '@kbn/config';
 
 let server: HttpService;
 let logger: ReturnType<typeof loggingSystemMock.create>;
-
-const getContextSetup = (
-  { env }: { env: Pick<Env, 'mode'> } = {
-    env: { mode: { dev: false, prod: true, name: 'production' } },
-  }
-) =>
-  contextServiceMock.createSetupContract({
-    core: Promise.resolve(
-      new CoreRouteHandlerContext(
-        {
-          deprecations: deprecationsServiceMock.createInternalStartContract(),
-          elasticsearch: elasticsearchServiceMock.createInternalStart(),
-          savedObjects: savedObjectsServiceMock.createInternalStartContract(),
-          uiSettings: uiSettingsServiceMock.createStartContract(),
-          env,
-        },
-        {} as any
-      )
-    ),
-  });
 
 describe('Routing versioned requests', () => {
   let router: IRouterWithVersion;
   let supertest: Supertest.SuperTest<Supertest.Test>;
 
   const setupDeps = {
-    context: getContextSetup(),
+    context: contextServiceMock.createSetupContract(),
     executionContext: executionContextServiceMock.createInternalSetupContract(),
   };
 
@@ -191,19 +166,6 @@ describe('Routing versioned requests', () => {
   });
 
   it('runs response validation when in dev', async () => {
-    logger = loggingSystemMock.create();
-    await server.stop(); // stop the already start server
-    server = createHttpServer({ logger });
-    await server.preboot({ context: contextServiceMock.createPrebootContract() });
-
-    const { server: innerServer, createRouter } = await server.setup({
-      ...setupDeps,
-      context: getContextSetup({ env: { mode: { dev: true, prod: false, name: 'development' } } }),
-    });
-
-    router = createRouter('/');
-    supertest = Supertest(innerServer.listener);
-
     router.versioned
       .get({ path: '/my-path', access: 'internal' })
       .addVersion(
@@ -229,6 +191,17 @@ describe('Routing versioned requests', () => {
   });
 
   it('does not run response validation in prod', async () => {
+    logger = loggingSystemMock.create();
+    await server.stop(); // stop the already started server
+    server = createHttpServer({
+      logger,
+      env: createTestEnv({ envOptions: getEnvOptions({ cliArgs: { dev: false } }) }),
+    });
+    await server.preboot({ context: contextServiceMock.createPrebootContract() });
+    const { server: innerServer, createRouter } = await server.setup(setupDeps);
+
+    router = createRouter('/');
+    supertest = Supertest(innerServer.listener);
     router.versioned
       .get({ path: '/my-path', access: 'internal' })
       .addVersion(
