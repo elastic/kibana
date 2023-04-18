3d5facebfd0dadff0fbe9e0e22d34a1f93930a5c
Mark Hoy <mark.hoy@elastic.co>
2023-04-18 19:30:35 -0400

    redo baseline framework for ent-search 4295

---
+++ x-pack/plugins/enterprise_search/server/lib/ml/get_ml_model_deployment_status.ts
@@ -0,0 +1,105 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { MlGetTrainedModelsStatsRequest, MlGetTrainedModelsRequest } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
+import { MlTrainedModels } from '@kbn/ml-plugin/server';
+
+import { MlModelDeploymentStatus, MlModelDeploymentState } from '../../../common/types/ml';
+
+import { ElasticsearchResponseError } from '../../utils/identify_exceptions';
+
+export const getMlModelDeploymentStatus = async (
+  modelName: string,
+  trainedModelsProvider: MlTrainedModels | undefined
+): Promise<MlModelDeploymentStatus> => {
+  if (!trainedModelsProvider) {
+    throw new Error('Machine Learning is not enabled');
+  }
+
+  const modelDetailsRequest: MlGetTrainedModelsRequest = {
+    model_id: modelName,
+    include: 'definition_status',
+  };
+
+  // get the model details to see if we're downloaded...
+  const modelDetailsResponse = await trainedModelsProvider.getTrainedModels(modelDetailsRequest);
+  if (!modelDetailsResponse || modelDetailsResponse.count === 0) {
+    // no model? return no status
+    return {
+      deploymentState: MlModelDeploymentState.NotDeployed,
+      modelId: modelName,
+      nodeAllocationCount: 0,
+      startTime: 0,
+      targetAllocationCount: 0,
+    };
+  }
+
+  // are we downloaded? If not - we should be downloading...
+  // TODO: ensure the types are updated to include this
+  if (!modelDetailsResponse.trained_model_configs[0].fully_defined) {
+    // not downloaded yet!
+    return {
+      deploymentState: MlModelDeploymentState.Downloading,
+      modelId: modelName,
+      nodeAllocationCount: 0,
+      startTime: 0,
+      targetAllocationCount: 0,
+    };
+  }
+
+  const modelRequest: MlGetTrainedModelsStatsRequest = {
+    model_id: modelName,
+  };
+
+  const modelStatsResponse = await trainedModelsProvider.getTrainedModelsStats(modelRequest);
+  if (
+    !modelStatsResponse.trained_model_stats ||
+    modelStatsResponse.trained_model_stats.length < 1
+  ) {
+    const notFoundError: ElasticsearchResponseError = {
+      meta: {
+        statusCode: 404,
+      },
+      name: 'ResponseError',
+    };
+    throw notFoundError;
+  }
+
+  const modelDeployment = modelStatsResponse.trained_model_stats[0].deployment_stats;
+
+  return {
+    deploymentState: getMlModelDeploymentStateForStatus(modelDeployment?.allocation_status.state),
+    modelId: modelName,
+    nodeAllocationCount: modelDeployment?.allocation_status.allocation_count || 0,
+    startTime: modelDeployment?.start_time || 0,
+    targetAllocationCount: modelDeployment?.allocation_status.target_allocation_count || 0,
+  };
+};
+
+function getMlModelDeploymentStateForStatus(state?: string): MlModelDeploymentState {
+  if (!state) {
+    return MlModelDeploymentState.NotDeployed;
+  }
+
+  switch (state) {
+    case 'downloading':
+      return MlModelDeploymentState.Downloading;
+    case 'is_fully_downloaded':
+      return MlModelDeploymentState.Downloaded;
+    case 'starting':
+      return MlModelDeploymentState.Starting;
+    case 'started':
+      return MlModelDeploymentState.Started;
+    case 'fully_allocated':
+      return MlModelDeploymentState.FullyAllocated;
+    case 'error':
+      return MlModelDeploymentState.Error;
+  }
+
+  // unknown state? return default
+  return MlModelDeploymentState.NotDeployed;
+}
--- x-pack/plugins/enterprise_search/server/routes/enterprise_search/indices.ts
+++ x-pack/plugins/enterprise_search/server/routes/enterprise_search/indices.ts
@@ -11,8 +11,11 @@ import {
 } from '@elastic/elasticsearch/lib/api/types';
 
 import { schema } from '@kbn/config-schema';
+import { IScopedClusterClient } from '@kbn/core-elasticsearch-server';
 
 import { i18n } from '@kbn/i18n';
+import { getMlClient } from '@kbn/ml-plugin/server/lib/ml_client';
+import { mlSavedObjectServiceFactory } from '@kbn/ml-plugin/server/saved_objects';
 
 import { DEFAULT_PIPELINE_NAME } from '../../../common/constants';
 import { ErrorCode } from '../../../common/types/error_codes';
@@ -37,6 +40,8 @@ import { preparePipelineAndIndexForMlInference } from '../../lib/indices/pipelin
 import { deleteMlInferencePipeline } from '../../lib/indices/pipelines/ml_inference/pipeline_processors/delete_ml_inference_pipeline';
 import { detachMlInferencePipeline } from '../../lib/indices/pipelines/ml_inference/pipeline_processors/detach_ml_inference_pipeline';
 import { fetchMlInferencePipelineProcessors } from '../../lib/indices/pipelines/ml_inference/pipeline_processors/get_ml_inference_pipeline_processors';
+import { getMlModelDeploymentStatus } from '../../lib/ml/get_ml_model_deployment_status';
+import { startMlModelDeployment } from '../../lib/ml/start_ml_model_deployment';
 import { createIndexPipelineDefinitions } from '../../lib/pipelines/create_pipeline_definitions';
 import { deleteIndexPipelines } from '../../lib/pipelines/delete_pipelines';
 import { getCustomPipelines } from '../../lib/pipelines/get_custom_pipelines';
@@ -977,6 +982,111 @@ export function registerIndexRoutes({
       } catch (error) {
         if (isResourceNotFoundException(error)) {
           // return specific message if pipeline doesn't exist
+          return createError({
+            errorCode: ErrorCode.RESOURCE_NOT_FOUND,
+            message: error.meta?.body?.error?.reason,
+            response,
+            statusCode: 404,
+          });
+        }
+        // otherwise, let the default handler wrap it
+        throw error;
+      }
+    })
+  );
+
+  router.post(
+    {
+      path: '/internal/enterprise_search/ml/models/{modelName}/deploy',
+      validate: {
+        params: schema.object({
+          modelName: schema.string(),
+        }),
+      },
+    },
+    elasticsearchErrorHandler(log, async (context, request, response) => {
+      const modelName = decodeURIComponent(request.params.modelName);
+      const {
+        elasticsearch: { client },
+        savedObjects: { client: savedObjectsClient },
+      } = await context.core;
+      const trainedModelsProvider = ml
+        ? await ml.trainedModelsProvider(request, savedObjectsClient)
+        : undefined;
+
+      const scopedClusterClient = client as unknown as IScopedClusterClient;
+
+      // TODO : double check params for spaces and authorization
+      const savedObjectService = mlSavedObjectServiceFactory(
+        savedObjectsClient,
+        savedObjectsClient,
+        true,
+        undefined,
+        scopedClusterClient,
+        () => Promise.resolve() // pretend isMlReady, to allow us to initialize the saved objects
+      );
+
+      const mlClient = savedObjectService
+        ? await getMlClient(scopedClusterClient, savedObjectService)
+        : undefined;
+
+      try {
+        const deployResult = await startMlModelDeployment(
+          modelName,
+          client.asCurrentUser,
+          mlClient,
+          trainedModelsProvider,
+          savedObjectService
+        );
+
+        return response.ok({
+          body: deployResult,
+          headers: { 'content-type': 'application/json' },
+        });
+      } catch (error) {
+        if (isResourceNotFoundException(error)) {
+          // return specific message if model doesn't exist
+          return createError({
+            errorCode: ErrorCode.RESOURCE_NOT_FOUND,
+            message: error.meta?.body?.error?.reason,
+            response,
+            statusCode: 404,
+          });
+        }
+        // otherwise, let the default handler wrap it
+        throw error;
+      }
+    })
+  );
+
+  router.get(
+    {
+      path: '/internal/enterprise_search/ml/models/{modelName}',
+      validate: {
+        params: schema.object({
+          modelName: schema.string(),
+        }),
+      },
+    },
+    elasticsearchErrorHandler(log, async (context, request, response) => {
+      const modelName = decodeURIComponent(request.params.modelName);
+      const {
+        savedObjects: { client: savedObjectsClient },
+      } = await context.core;
+      const trainedModelsProvider = ml
+        ? await ml.trainedModelsProvider(request, savedObjectsClient)
+        : undefined;
+
+      try {
+        const getStatusResult = await getMlModelDeploymentStatus(modelName, trainedModelsProvider);
+
+        return response.ok({
+          body: getStatusResult,
+          headers: { 'content-type': 'application/json' },
+        });
+      } catch (error) {
+        if (isResourceNotFoundException(error)) {
+          // return specific message if model doesn't exist
           return createError({
             errorCode: ErrorCode.RESOURCE_NOT_FOUND,
             message: error.meta?.body?.error?.reason,
