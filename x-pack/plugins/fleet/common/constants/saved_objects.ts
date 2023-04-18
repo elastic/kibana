3763a32bf1ffb8a9a3639f3de5688e671dce2d1a
pgayvallet <pierre.gayvallet@elastic.co>
2023-04-18 12:20:06 +0200

    update index pattern for fleet types

---
+++ x-pack/plugins/fleet/common/constants/saved_objects.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { INGEST_SAVED_OBJECT_INDEX } from '@kbn/core-saved-objects-server';
+
+export { INGEST_SAVED_OBJECT_INDEX }
