2daaf09aa7da0ccaf242c42533118c52948f9e47
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2023-04-18 11:58:06 +0300

    [Inpector] Improve the display when there are many columns

--- src/plugins/data/public/utils/table_inspector_view/components/data_table.tsx
+++ src/plugins/data/public/utils/table_inspector_view/components/data_table.tsx
@@ -8,7 +8,8 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import classNames from 'classnames';
+import { css } from '@emotion/react';
 import {
   EuiButtonIcon,
   EuiFlexGroup,
@@ -192,12 +193,25 @@ export class DataTableFormat extends Component<DataTableFormatProps, DataTableFo
 
     return (
       <EuiInMemoryTable
-        className="insDataTableFormat__table"
+        tableLayout="auto"
+        className={classNames('insDataTableFormat__table', 'eui-xScroll')}
         data-test-subj="inspectorTable"
         columns={columns}
         items={rows}
         sorting={true}
         pagination={pagination}
+        css={css`
+          // Set a min width on each column - you can use [data-test-subj] to target specific columns
+          .euiTableHeaderCell {
+            min-width: 100px;
+          }
+
+          // Make sure the pagination follows the scroll
+          > div:last-child {
+            position: sticky;
+            left: 0;
+          }
+        `}
       />
     );
   }
