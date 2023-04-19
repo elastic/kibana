fce3664397fe71db2062d0c77e64af8204b18357
Carlos Crespo <crespocarlos@users.noreply.github.com>
2023-04-19 16:04:51 +0200

    [Infrastructure UI] KPI charts with Lens Embeddable (#154903)
    ## Summary
    
    This PR replaces all but Host Count metric charts with Lens and aligns
    the formulas used in Lens with the formulas used in the Snapshot API.
    
    The Hosts Count was not converted yet, because Snapshot API
    [post-process the results and filters out hosts that don't have metrics
    and come from
    APM](https://github.com/elastic/kibana/blob/47c71b30253cf1bdd1339af5f471a6d747871c64/x-pack/plugins/infra/server/routes/snapshot/lib/transform_metrics_ui_response.ts#L61).
    I decided to keep it unchanged until we start using a new API, which
    won't have that step.
    
    In order to avoid multiple requests going to the server simultaneously,
    I introduced an intersection observer in the Lens chart component. It
    will make them only trigger a request once the user has scrolled over a
    chart component. This aims to prevent the occurrence of
    `circuit_breaking_exception` exceptions when async_search has to process
    too much data.
    
    ### Differences between Lens and current KPI charts
    
    <img width="533" alt="image"
    src="https://user-images.githubusercontent.com/2767137/232526591-4812059e-ba1f-4e59-a060-820570230084.png">
    
    Currently, Lens doesn't provide an option for adding a suffix to the
    metric value nor set a max number of decimal places. (e.g (e.g 3.8 Mbit
    **/s**)
    
    ### For reviewer
    
    Unfortunately, it's a big PR. Everything in the `lens/formulas` folder
    is just related to the metric formulas that are reused between KPIs and
    Metrics Charts.
    
    The core of the changes is in the `lens/visualization_types` folder. It
    contains the details on how to build different objects to render either
    a metric type of chart or a lineXY one.
    
    I have also aligned `cpu` and `memory` formulas with what we have in the
    Inventory Model. When comparing the current KPI with the new ones in
    Lens Embeddable, the results were not matching.
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/infra/public/types.ts
+++ x-pack/plugins/infra/public/types.ts
@@ -34,7 +34,7 @@ import type {
 // import type { OsqueryPluginStart } from '../../osquery/public';
 import type { SpacesPluginStart } from '@kbn/spaces-plugin/public';
 import type { IStorageWrapper } from '@kbn/kibana-utils-plugin/public';
-import { type TypedLensByValueInput, LensPublicStart } from '@kbn/lens-plugin/public';
+import { LensPublicStart } from '@kbn/lens-plugin/public';
 import type { ChartsPluginStart } from '@kbn/charts-plugin/public';
 import { CasesUiStart } from '@kbn/cases-plugin/public';
 import type { UnwrapPromise } from '../common/utility_types';
@@ -110,12 +110,6 @@ export interface InfraHttpError extends IHttpFetchError {
     statusCode: number;
     message?: string;
   };
-}
-
-export type LensAttributes = TypedLensByValueInput['attributes'];
-
-export interface LensOptions {
-  breakdownSize: number;
 }
 
 export interface ExecutionTimeRange {
