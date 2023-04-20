5303a6b3bb17b08ccff302045d24dbc36eb40c90
Matt Kime <matt@mattki.me>
2023-04-19 22:03:48 -0500

    schema abstraction

---
+++ packages/kbn-content-management-utils/src/schema.ts
@@ -0,0 +1,93 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+import { schema, ObjectType } from '@kbn/config-schema';
+
+export const apiError = schema.object({
+  error: schema.string(),
+  message: schema.string(),
+  statusCode: schema.number(),
+  metadata: schema.object({}, { unknowns: 'allow' }),
+});
+
+export const referenceSchema = schema.object(
+  {
+    name: schema.maybe(schema.string()),
+    type: schema.string(),
+    id: schema.string(),
+  },
+  { unknowns: 'forbid' }
+);
+
+export const referencesSchema = schema.arrayOf(referenceSchema);
+
+/*
+export const mapAttributesSchema = schema.object(
+  {
+    title: schema.string(),
+    description: schema.maybe(schema.string()),
+    mapStateJSON: schema.maybe(schema.string()),
+    layerListJSON: schema.maybe(schema.string()),
+    uiStateJSON: schema.maybe(schema.string()),
+  },
+  { unknowns: 'forbid' }
+);
+*/
+
+export const savedObjectSchema = (attributesSchema: ObjectType<any>) =>
+  schema.object(
+    {
+      id: schema.string(),
+      type: schema.string(),
+      version: schema.maybe(schema.string()),
+      createdAt: schema.maybe(schema.string()),
+      updatedAt: schema.maybe(schema.string()),
+      error: schema.maybe(apiError),
+      attributes: attributesSchema,
+      references: referencesSchema,
+      namespaces: schema.maybe(schema.arrayOf(schema.string())),
+      originId: schema.maybe(schema.string()),
+    },
+    { unknowns: 'allow' }
+  );
+
+export const objectTypeToGetResultSchema = (soSchema: ObjectType<any>) =>
+  schema.object(
+    {
+      item: soSchema,
+      meta: schema.object(
+        {
+          outcome: schema.oneOf([
+            schema.literal('exactMatch'),
+            schema.literal('aliasMatch'),
+            schema.literal('conflict'),
+          ]),
+          aliasTargetId: schema.maybe(schema.string()),
+          aliasPurpose: schema.maybe(
+            schema.oneOf([
+              schema.literal('savedObjectConversion'),
+              schema.literal('savedObjectImport'),
+            ])
+          ),
+        },
+        { unknowns: 'forbid' }
+      ),
+    },
+    { unknowns: 'forbid' }
+  );
+
+export const createOptionsSchema = schema.object({
+  references: schema.maybe(referencesSchema),
+});
+
+export const createResultSchema = (soSchema: ObjectType<any>) =>
+  schema.object(
+    {
+      item: soSchema,
+    },
+    { unknowns: 'forbid' }
+  );
