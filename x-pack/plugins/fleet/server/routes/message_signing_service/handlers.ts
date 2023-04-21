fd244e384ed1f784b02803d1cf146235df16c450
Ashokaditya <1849116+ashokaditya@users.noreply.github.com>
2023-04-21 12:32:43 +0200

    bulk update fleet policies with new signing key

--- x-pack/plugins/fleet/server/routes/message_signing_service/handlers.ts
+++ x-pack/plugins/fleet/server/routes/message_signing_service/handlers.ts
@@ -5,19 +5,21 @@
  * 2.0.
  */
 
+import { AGENT_POLICY_INDEX, SO_SEARCH_LIMIT } from '../../../common';
 import type { FleetRequestHandler } from '../../types';
-
 import { defaultFleetErrorHandler, AgentPolicyNotFoundError } from '../../errors';
 import { appContextService } from '../../services';
 
-export const rotateKeyPairHandler: FleetRequestHandler<undefined, undefined, unknown> = async (
+export const rotateKeyPairHandler: FleetRequestHandler<undefined, undefined, undefined> = async (
+  context,
   _,
-  __,
   response
 ) => {
+  let newPublicKey: string | undefined;
+
   try {
     await appContextService.getMessageSigningService()?.rotateKeyPair();
-    return response.ok();
+    newPublicKey = await appContextService.getMessageSigningService()?.getPublicKey();
   } catch (error) {
     if (error instanceof AgentPolicyNotFoundError) {
       return response.notFound({
@@ -26,7 +28,54 @@ export const rotateKeyPairHandler: FleetRequestHandler<undefined, undefined, unk
         },
       });
     }
+    return defaultFleetErrorHandler({ error, response });
+  }
 
+  if (!newPublicKey) {
+    return response.customError({
+      statusCode: 424,
+      body: {
+        message: 'Unable to update agent policies as no public key was found!',
+      },
+    });
+  }
+
+  const esClient = (await context.core).elasticsearch.client.asInternalUser;
+  const agentPolicies = await esClient.search({ index: AGENT_POLICY_INDEX, size: SO_SEARCH_LIMIT });
+
+  // bulk update .fleet-policies docs signing_key with the new public key
+  const bulkUpdateList = agentPolicies.hits.hits.flatMap((agentPolicy) => [
+    {
+      update: {
+        _id: agentPolicy._id,
+      },
+    },
+    {
+      doc: {
+        data: {
+          agent: {
+            protection: {
+              signing_key: newPublicKey,
+            },
+          },
+        },
+      },
+    },
+  ]);
+
+  try {
+    await esClient.bulk({
+      index: AGENT_POLICY_INDEX,
+      body: bulkUpdateList,
+      refresh: 'wait_for',
+      filter_path: 'items.*errors',
+    });
+    return response.ok({
+      body: {
+        message: 'Successfully rotated key pair',
+      },
+    });
+  } catch (error) {
     return defaultFleetErrorHandler({ error, response });
   }
 };
