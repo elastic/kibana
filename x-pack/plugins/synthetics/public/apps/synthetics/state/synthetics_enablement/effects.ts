b6428ca5d6a9641e56d8f1a5086cf1da5ee49a16
Dominique Clarke <dominique.clarke@elastic.co>
2023-04-20 21:19:25 -0400

    adjust effects

--- x-pack/plugins/synthetics/public/apps/synthetics/state/synthetics_enablement/effects.ts
+++ x-pack/plugins/synthetics/public/apps/synthetics/state/synthetics_enablement/effects.ts
@@ -25,7 +25,7 @@ export function* fetchSyntheticsEnablementEffect() {
       fetchGetSyntheticsEnablement,
       getSyntheticsEnablementSuccess,
       getSyntheticsEnablementFailure,
-      successMessage,
+      null,
       failureMessage
     )
   );
