859b5619de8834f4ee3f810836d1a91701dfbec3
Maxim Palenov <maxim.palenov@elastic.co>
2023-04-22 08:33:40 +0200

    adopt changes to the rules list

--- x-pack/plugins/triggers_actions_ui/public/application/sections/rules_list/components/rules_list_table.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/rules_list/components/rules_list_table.tsx
@@ -208,7 +208,6 @@ export const RulesListTable = (props: RulesListTableProps) => {
   } = props;
 
   const [tagPopoverOpenIndex, setTagPopoverOpenIndex] = useState<number>(-1);
-  const [currentlyOpenNotify, setCurrentlyOpenNotify] = useState<string>();
   const [isLoadingMap, setIsLoadingMap] = useState<Record<string, boolean>>({});
 
   const isRuleUsingExecutionStatus = getIsExperimentalFeatureEnabled('ruleUseExecutionStatus');
@@ -485,12 +484,7 @@ export const RulesListTable = (props: RulesListTableProps) => {
           return (
             <RulesListNotifyBadge
               showOnHover
-              snoozeSettings={rule}
-              isLoading={!!isLoadingMap[rule.id]}
-              onLoading={(newIsLoading) => onLoading(rule.id, newIsLoading)}
-              isOpen={currentlyOpenNotify === rule.id}
-              onClick={() => setCurrentlyOpenNotify(rule.id)}
-              onClose={() => setCurrentlyOpenNotify('')}
+              snoozeSettings={!!isLoadingMap[rule.id] ? undefined : rule}
               onRuleChanged={onRuleChanged}
               snoozeRule={async (snoozeSchedule) => {
                 await onSnoozeRule(rule, snoozeSchedule);
@@ -769,7 +763,6 @@ export const RulesListTable = (props: RulesListTableProps) => {
     ];
   }, [
     config.minimumScheduleInterval,
-    currentlyOpenNotify,
     isLoadingMap,
     isRuleTypeEditableInContext,
     onRuleChanged,
