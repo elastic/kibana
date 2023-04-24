1bc43a3aceeee872f43db009a2f3e4259ec77972
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-04-24 11:46:31 +0200

    fix imports

--- x-pack/plugins/ml/public/application/data_frame_analytics/common/get_index_data.ts
+++ x-pack/plugins/ml/public/application/data_frame_analytics/common/get_index_data.ts
@@ -8,8 +8,8 @@
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { extractErrorMessage } from '@kbn/ml-error-utils';
 import { type DataFrameAnalyticsConfig, INDEX_STATUS } from '@kbn/ml-data-frame-analytics-utils';
+import { EsSorting, UseDataGridReturnType, getProcessedFields } from '@kbn/ml-data-grid';
 
-import { EsSorting, UseDataGridReturnType, getProcessedFields } from '../../components/data_grid';
 import { ml } from '../../services/ml_api_service';
 import { newJobCapsServiceAnalytics } from '../../services/new_job_capabilities/new_job_capabilities_service_analytics';
 
