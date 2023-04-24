f7d2c88cb1a3b1e31ea60a0ed5fd2baf94787a05
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-04-24 19:21:41 +0200

    add missing file

---
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/connectors/slack.ts
@@ -0,0 +1,209 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+/* eslint-disable no-console */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../../../ftr_provider_context';
+import { ObjectRemover } from '../../../lib/object_remover';
+import { generateUniqueKey } from '../../../lib/get_test_data';
+import { createSlackConnectorAndObjectRemover, getConnectorByName } from './utils';
+
+export default ({ getPageObjects, getService }: FtrProviderContext) => {
+  const testSubjects = getService('testSubjects');
+  const pageObjects = getPageObjects(['common', 'triggersActionsUI', 'header']);
+  const find = getService('find');
+  const retry = getService('retry');
+  const supertest = getService('supertest');
+  const actions = getService('actions');
+  const rules = getService('rules');
+  let objectRemover: ObjectRemover;
+
+  describe('Slack', () => {
+    before(async () => {
+      objectRemover = await createSlackConnectorAndObjectRemover({ getService });
+    });
+
+    after(async () => {
+      await objectRemover.removeAll();
+    });
+
+    describe('connector page', () => {
+      beforeEach(async () => {
+        await pageObjects.common.navigateToApp('triggersActionsConnectors');
+      });
+
+      it('should create the webhook connector', async () => {
+        const connectorName = generateUniqueKey();
+        await actions.slack.createNewWebhook({
+          name: connectorName,
+          url: 'https://test.com',
+        });
+
+        const toastTitle = await pageObjects.common.closeToast();
+        expect(toastTitle).to.eql(`Created '${connectorName}'`);
+
+        await pageObjects.triggersActionsUI.searchConnectors(connectorName);
+
+        const searchResults = await pageObjects.triggersActionsUI.getConnectorsList();
+        expect(searchResults).to.eql([
+          {
+            name: connectorName,
+            actionType: 'Slack',
+          },
+        ]);
+        const connector = await getConnectorByName(connectorName, supertest);
+        objectRemover.add(connector.id, 'action', 'actions');
+      });
+
+      it('should create the web api connector', async () => {
+        const connectorName = generateUniqueKey();
+        await actions.slack.createNewWebAPI({
+          name: connectorName,
+          token: 'supersecrettoken',
+        });
+
+        const toastTitle = await pageObjects.common.closeToast();
+        expect(toastTitle).to.eql(`Created '${connectorName}'`);
+
+        await pageObjects.triggersActionsUI.searchConnectors(connectorName);
+
+        const searchResults = await pageObjects.triggersActionsUI.getConnectorsList();
+        expect(searchResults).to.eql([
+          {
+            name: connectorName,
+            actionType: 'Slack API',
+          },
+        ]);
+        const connector = await getConnectorByName(connectorName, supertest);
+        objectRemover.add(connector.id, 'action', 'actions');
+      });
+    });
+
+    describe('rule creation', async () => {
+      const webhookConnectorName = generateUniqueKey();
+      const webApiConnectorName = generateUniqueKey();
+      let webApiAction: { id: string };
+      let webhookAction: { id: string };
+
+      const setupRule = async () => {
+        const ruleName = generateUniqueKey();
+        await retry.try(async () => {
+          await rules.common.defineIndexThresholdAlert(ruleName);
+        });
+        return ruleName;
+      };
+
+      const getRuleIdByName = async (name: string) => {
+        const response = await supertest
+          .get(`/api/alerts/_find?search=${name}&search_fields=name`)
+          .expect(200);
+        return response.body.data[0].id;
+      };
+
+      const selectSlackConnectorInRuleAction = async ({
+        name,
+        connectorId,
+      }: {
+        name: string;
+        connectorId: string;
+      }) => {
+        await testSubjects.click('.slack-alerting-ActionTypeSelectOption'); // "Slack" in connector list
+        // @xavier start here
+        // console.log('opening', name, 'connectorId', connectorId);
+        // const comboBoxInputs = await find.allByCssSelector(
+        //   '[data-test-subj="comboBoxInput"]',
+        //   1000
+        // );
+        // await comboBoxInputs[1].click();
+      };
+
+      before(async () => {
+        webApiAction = await actions.api.createConnector({
+          name: webApiConnectorName,
+          config: {},
+          secrets: { token: 'supersecrettoken' },
+          connectorTypeId: '.slack_api',
+        });
+
+        webhookAction = await actions.api.createConnector({
+          name: webhookConnectorName,
+          config: {},
+          secrets: { webhookUrl: 'https://test.com' },
+          connectorTypeId: '.slack',
+        });
+
+        objectRemover.add(webhookAction.id, 'action', 'actions');
+        objectRemover.add(webApiAction.id, 'action', 'actions');
+        await pageObjects.common.navigateToApp('triggersActions');
+      });
+
+      it('should save webhook type slack connectors', async () => {
+        const ruleName = await setupRule();
+
+        await selectSlackConnectorInRuleAction({
+          name: webhookConnectorName,
+          connectorId: webhookAction.id,
+        });
+        await testSubjects.click('saveRuleButton');
+        await pageObjects.triggersActionsUI.searchAlerts(ruleName);
+
+        const ruleId = await getRuleIdByName(ruleName);
+        console.log('ruleId:', ruleId);
+        objectRemover.add(ruleId, 'rule', 'alerting');
+
+        const searchResults = await pageObjects.triggersActionsUI.getAlertsList();
+        expect(searchResults).to.eql([
+          {
+            duration: '00:00',
+            interval: '1 min',
+            name: `${ruleName}Index threshold`,
+            tags: '',
+          },
+        ]);
+
+        const toastTitle = await pageObjects.common.closeToast();
+        expect(toastTitle).to.eql(`Created rule "${ruleName}"`);
+      });
+
+      // it('should save web api type slack connectors', async () => {
+      //   // select the web api connector
+      //   // click on submit
+      //   // check that the rule with this name exists in the rule list
+      //   await testSubjects.click('saveRuleButton');
+      // });
+
+      it.only('should save webapi type slack connectors', async () => {
+        const ruleName = await setupRule();
+
+        console.log('webApiConnectorName:', webApiConnectorName);
+        await selectSlackConnectorInRuleAction({
+          name: webApiConnectorName,
+          connectorId: webApiAction.id,
+        });
+        await new Promise((resolve) => setTimeout(resolve, 100000));
+        await testSubjects.click('saveRuleButton');
+        await pageObjects.triggersActionsUI.searchAlerts(ruleName);
+
+        const ruleId = await getRuleIdByName(ruleName);
+        objectRemover.add(ruleId, 'rule', 'alerting');
+
+        const searchResults = await pageObjects.triggersActionsUI.getAlertsList();
+        expect(searchResults).to.eql([
+          {
+            duration: '00:00',
+            interval: '1 min',
+            name: `${ruleName}Index threshold`,
+            tags: '',
+          },
+        ]);
+        const toastTitle = await pageObjects.common.closeToast();
+        expect(toastTitle).to.eql(`Created rule "${ruleName}"`);
+      });
+    });
+  });
+};
