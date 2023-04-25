d87fa7a832252a813b662e5ce24215caf5b08152
Dima Arnautov <dmitrii.arnautov@elastic.co>
2023-04-25 12:20:56 +0200

    Update x-pack/plugins/ml/public/application/model_management/deployment_setup.tsx
    Co-authored-by: István Zoltán Szabó <istvan.szabo@elastic.co>

--- x-pack/plugins/ml/public/application/model_management/deployment_setup.tsx
+++ x-pack/plugins/ml/public/application/model_management/deployment_setup.tsx
@@ -102,7 +102,7 @@ export const DeploymentSetup: FC<DeploymentSetupProps> = ({
         description={
           <FormattedMessage
             id="xpack.ml.trainedModels.modelsList.startDeployment.deploymentIdHelp"
-            defaultMessage="Specify unique identifier for the model deployment"
+            defaultMessage="Specify unique identifier for the model deployment."
           />
         }
       >
