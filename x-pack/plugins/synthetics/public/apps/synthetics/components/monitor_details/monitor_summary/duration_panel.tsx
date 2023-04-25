c27d633f039ba9535404e1dc97b7ba5ae1f7f3e6
shahzad31 <shahzad31comp@gmail.com>
2023-04-25 12:21:10 +0200

    update median label

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_summary/duration_panel.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_details/monitor_summary/duration_panel.tsx
@@ -42,7 +42,7 @@ export const DurationPanel = (props: DurationPanelProps) => {
       attributes={[
         {
           time: props,
-          name: AVG_DURATION_LABEL,
+          name: MEDIAN_DURATION_LABEL,
           dataType: 'synthetics',
           selectedMetricField: 'monitor_duration',
           reportDefinitions: {
@@ -55,7 +55,7 @@ export const DurationPanel = (props: DurationPanelProps) => {
   );
 };
 
-export const AVG_DURATION_LABEL = i18n.translate(
+export const MEDIAN_DURATION_LABEL = i18n.translate(
   'xpack.synthetics.monitorDetails.summary.medianDuration',
   {
     defaultMessage: 'Median duration',
