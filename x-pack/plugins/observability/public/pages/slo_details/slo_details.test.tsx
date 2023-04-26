e6df4b5ec6895db98f38b708b5ce186c879b332e
Coen Warmer <coen.warmer@gmail.com>
2023-04-26 19:20:43 +0200

    [SLO][SLO Detail] Make SLO Detail > Header Actions menu and SLO List > SLO List item > Actions menu more consistent (#155868)
    Co-authored-by: Kevin Delemme <kdelemme@gmail.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 1dcf1f86a856a7251df7bd06b47820ed64d033b8)
    
    # Conflicts:
    #	x-pack/plugins/observability/public/pages/slo_details/components/header_control.tsx
    #	x-pack/plugins/observability/public/pages/slo_details/slo_details.test.tsx

--- x-pack/plugins/observability/public/pages/slo_details/slo_details.test.tsx
+++ x-pack/plugins/observability/public/pages/slo_details/slo_details.test.tsx
@@ -60,6 +60,12 @@ const mockKibana = () => {
           prepend: mockBasePathPrepend,
         },
       },
+      notifications: {
+        toasts: {
+          addSuccess: jest.fn(),
+          addError: jest.fn(),
+        },
+      },
       triggersActionsUi: {
         getAddRuleFlyout: jest.fn(() => (
           <div data-test-subj="add-rule-flyout">mocked component</div>
@@ -180,6 +186,45 @@ describe('SLO Details Page', () => {
 
     fireEvent.click(screen.getByTestId('o11yHeaderControlActionsButton'));
     expect(screen.queryByTestId('sloDetailsHeaderControlPopoverCreateRule')).toBeTruthy();
+  });
+
+  it("renders a 'Manage rules' button under actions menu", async () => {
+    const slo = buildSlo();
+    useParamsMock.mockReturnValue(slo.id);
+    useFetchSloDetailsMock.mockReturnValue({ isLoading: false, slo });
+    useLicenseMock.mockReturnValue({ hasAtLeast: () => true });
+
+    render(<SloDetailsPage />);
+
+    fireEvent.click(screen.getByTestId('o11yHeaderControlActionsButton'));
+    expect(screen.queryByTestId('sloDetailsHeaderControlPopoverManageRules')).toBeTruthy();
+  });
+
+  it("renders a 'Clone' button under actions menu", async () => {
+    const slo = buildSlo();
+    useParamsMock.mockReturnValue(slo.id);
+    useFetchSloDetailsMock.mockReturnValue({ isLoading: false, slo });
+    useLicenseMock.mockReturnValue({ hasAtLeast: () => true });
+
+    render(<SloDetailsPage />);
+
+    fireEvent.click(screen.getByTestId('o11yHeaderControlActionsButton'));
+    expect(screen.queryByTestId('sloDetailsHeaderControlPopoverClone')).toBeTruthy();
+  });
+
+  it("renders a 'Delete' button under actions menu", async () => {
+    const slo = buildSlo();
+    useParamsMock.mockReturnValue(slo.id);
+    useFetchSloDetailsMock.mockReturnValue({ isLoading: false, slo });
+    useLicenseMock.mockReturnValue({ hasAtLeast: () => true });
+
+    render(<SloDetailsPage />);
+
+    fireEvent.click(screen.getByTestId('o11yHeaderControlActionsButton'));
+    expect(screen.queryByTestId('sloDetailsHeaderControlPopoverDelete')).toBeTruthy();
+
+    const manageRulesButton = screen.queryByTestId('sloDetailsHeaderControlPopoverManageRules');
+    expect(manageRulesButton).toBeTruthy();
   });
 
   it('renders the Overview tab by default', async () => {
