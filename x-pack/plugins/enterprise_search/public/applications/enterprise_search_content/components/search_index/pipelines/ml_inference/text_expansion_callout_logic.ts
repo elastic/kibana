ea7d865685eb5986de3cc229de47b4f17b16f9a4
Adam Demjen <demjened@gmail.com>
2023-04-26 17:16:52 -0400

    Remove comments

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/text_expansion_callout_logic.ts
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/text_expansion_callout_logic.ts
@@ -20,26 +20,6 @@ import {
 } from '../../../../api/ml_models/text_expansion/fetch_text_expansion_model_api_logic';
 import { MlModelDeploymentState } from '../../../../../../../common/types/ml';
 
-// On page load: call Get API
-// - If no model -> Deploy button
-// - If model is downloading -> Start progress bar
-// - If model is starting -> Progress bar to 90%
-// - If model is fully allocated -> Deployed panel
-
-// Deploy button -> on click:
-//  - Disable
-//  - Call Create API, wait for response
-//  - Call Sync API, wait for response
-//  - Start progress bar
-//  - Begin polling for status
-
-// Status polling
-// - If model is starting -> Progress bar to 90%
-// - If model is fully allocated -> Deployed panel
-
-// Error handling
-// - If any API call returns error -> Error panel
-
 const FETCH_TEXT_EXPANSION_MODEL_POLLING_DURATION = 5000; // 5 seconds
 const FETCH_TEXT_EXPANSION_MODEL_POLLING_DURATION_ON_FAILURE = 30000; // 30 seconds
 
