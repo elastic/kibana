c0b03901f3866d402b3ce7abae5143e308215255
jpdjere <jpdjeredjian@gmail.com>
2023-04-26 16:59:41 +0200

    WIP

--- x-pack/plugins/security_solution/cypress/e2e/detection_rules/prebuilt_rules_notifications.cy.ts
+++ x-pack/plugins/security_solution/cypress/e2e/detection_rules/prebuilt_rules_notifications.cy.ts
@@ -24,13 +24,13 @@ describe('Detection rules, Prebuilt Rules Installation and Update workflow', () 
     login();
   });
   beforeEach(() => {
-    // Make sure persisted rules table state is cleared
+    /* Make sure persisted rules table state is cleared */
     resetRulesTableState();
     deleteAlertsAndRules();
     esArchiverResetKibana();
 
-    // Prevent the installation of the package
-    // `security_detection_engine` from Fleet
+    /* Prevent the installation of the package */
+    /* `security_detection_engine` from Fleet */
     cy.intercept('POST', '/api/fleet/epm/packages/_bulk?prerelease=true', {}).as(
       'getPrebuiltRules'
     );
@@ -40,11 +40,12 @@ describe('Detection rules, Prebuilt Rules Installation and Update workflow', () 
         rule_id: 'rule_1',
       }),
     });
-    visitWithoutDateRange(SECURITY_DETECTIONS_RULES_URL);
+    
   });
 
   describe('Rules installation notification when no rules have been installed', () => {
     beforeEach(() => {
+      visitWithoutDateRange(SECURITY_DETECTIONS_RULES_URL);
       waitForRulesTableToBeLoaded();
     });
 
@@ -56,12 +57,12 @@ describe('Detection rules, Prebuilt Rules Installation and Update workflow', () 
 
   describe('No notifications', () => {
     it('should display no install or update notifications when latest rules are installed', () => {
-      // Install current available rules
+      /* Install current available rules */
       installAvailableRules();
-
+      visitWithoutDateRange(SECURITY_DETECTIONS_RULES_URL);
       waitForRulesTableToBeLoaded();
 
-      // Assert that there are no installation or update notifications
+      /* Assert that there are no installation or update notifications */
       cy.get(LOAD_PREBUILT_RULES_BTN).should('not.exist');
       cy.get(LOAD_PREBUILT_RULES_ON_PAGE_HEADER_BTN).should('not.exist');
       cy.get(UPDATE_PREBUILT_RULES_CALLOUT).should('not.exist');
@@ -71,24 +72,32 @@ describe('Detection rules, Prebuilt Rules Installation and Update workflow', () 
   describe('Rule installation notification when at least one rule already installed', () => {
     beforeEach(() => {
       installAvailableRules();
-      // Create new rule asset with a different rule_id as the one that was
-      // installed before in order to trigger the installation notification
+      /* Create new rule assets with a different rule_id as the one that was */
+      /* installed before in order to trigger the installation notification */
       createNewRuleAsset({
         rule: createRuleAssetSavedObject({
           name: 'Test rule 2',
           rule_id: 'rule_2',
         }),
       });
+      createNewRuleAsset({
+        rule: createRuleAssetSavedObject({
+          name: 'Test rule 3',
+          rule_id: 'rule_3',
+        }),
+      });
+      visitWithoutDateRange(SECURITY_DETECTIONS_RULES_URL);
       waitForRulesTableToBeLoaded();
     });
 
     it('should notify user about prebuilt rules package available for installation', () => {
       cy.get(LOAD_PREBUILT_RULES_ON_PAGE_HEADER_BTN).should('be.visible');
+      cy.get(LOAD_PREBUILT_RULES_ON_PAGE_HEADER_BTN).contains('2 Elastic prebuilt rules')
     });
 
     it('should notify user a rule is again available for installation if it is deleted', () => {
-      // Install a second rule, assert that the notification is gone
-      // then delete the rule and assert that the notification is back
+      /* Install available rules, assert that the notification is gone */
+      /* then delete one rule and assert that the notification is back */
       cy.get(LOAD_PREBUILT_RULES_ON_PAGE_HEADER_BTN).click();
       cy.get(LOAD_PREBUILT_RULES_ON_PAGE_HEADER_BTN).should('not.exist');
       deleteFirstRule();
@@ -99,8 +108,8 @@ describe('Detection rules, Prebuilt Rules Installation and Update workflow', () 
   describe('Rule update notification', () => {
     beforeEach(() => {
       installAvailableRules();
-      // Create new rule asset with the same rule_id as the one that was installed
-      // but with a higher version, in order to trigger the update notification
+      /* Create new rule asset with the same rule_id as the one that was installed  */
+      /* but with a higher version, in order to trigger the update notification     */
       createNewRuleAsset({
         rule: createRuleAssetSavedObject({
           name: 'Test rule 1.1 (updated)',
@@ -108,11 +117,13 @@ describe('Detection rules, Prebuilt Rules Installation and Update workflow', () 
           version: 2,
         }),
       });
+      visitWithoutDateRange(SECURITY_DETECTIONS_RULES_URL);
       waitForRulesTableToBeLoaded();
     });
 
     it('should notify user about prebuilt rules package available for update', () => {
       cy.get(UPDATE_PREBUILT_RULES_CALLOUT).should('be.visible');
+      cy.get(UPDATE_PREBUILT_RULES_CALLOUT).contains('Update 1 Elastic prebuilt rule');
     });
   });
 });
