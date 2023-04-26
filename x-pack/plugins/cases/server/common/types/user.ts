ed3d037759309a6fb27d4b20850c51e961cd5fca
Jonathan Buttner <56361221+jonathan-buttner@users.noreply.github.com>
2023-04-26 11:47:39 -0400

    [Cases] Separating http request contract from saved object persistence layer (#155325)
    This PR separates the http API io-ts types from the types that are used
    in the cases service layer to interact with the saved object client.
    This PR is specifically for the user actions it only affects the types
    used when interacting with the saved object client and doesn't touch the
    transformation logic yet.
    
    Issue: https://github.com/elastic/kibana/issues/153726

--- x-pack/plugins/cases/server/common/types/user.ts
+++ x-pack/plugins/cases/server/common/types/user.ts
@@ -9,7 +9,7 @@ export interface User {
   email: string | null | undefined;
   full_name: string | null | undefined;
   username: string | null | undefined;
-  profile_uid?: string | undefined;
+  profile_uid?: string;
 }
 
 export interface UserProfile {
