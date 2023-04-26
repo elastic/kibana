e550eaef48b18a47bfe8ed880e3591293ca44cc7
Kate Patticha <aikaterini.patticha@elastic.co>
2023-04-26 18:33:22 +0200

    back up

--- x-pack/test/apm_api_integration/tests/alerts/duration_threshold.spec.ts
+++ x-pack/test/apm_api_integration/tests/alerts/duration_threshold.spec.ts
@@ -16,11 +16,14 @@ import {
   fetchServiceTabAlertCount,
 } from './alerting_api_helper';
 import { generateData } from './generate_data';
-import { waitForRuleStatus, waitForDocumentInIndex } from './wait_for_rule_status';
+import {
+  waitForRuleStatus,
+  waitForDocumentInIndex,
+  waitForAlertInIndex,
+} from './wait_for_rule_status';
 
 export default function ApiTest({ getService }: FtrProviderContext) {
   const registry = getService('registry');
-  const INDEX_PATTERN = 'duration_threshold';
 
   const supertest = getService('supertest');
   const es = getService('es');
@@ -29,9 +32,12 @@ export default function ApiTest({ getService }: FtrProviderContext) {
 
   const synthtraceEsClient = getService('synthtraceEsClient');
 
-  registry.when('duration threshold threshold alert', { config: 'basic', archives: [] }, () => {
+  registry.when('transaction duration alert', { config: 'basic', archives: [] }, () => {
     let ruleId: string;
     let actionId: string | undefined;
+
+    const APM_ALERTS_INDEX = '.alerts-observability.apm.alerts-default';
+    const ALERT_ACTION_PATTERN = 'alert-action-';
 
     before(async () => {
       await generateData({ synthtraceEsClient });
@@ -41,43 +47,56 @@ export default function ApiTest({ getService }: FtrProviderContext) {
       await synthtraceEsClient.clean();
       await supertest.delete(`/api/alerting/rule/${ruleId}`).set('kbn-xsrf', 'foo');
       await supertest.delete(`/api/actions/connector/${actionId}`).set('kbn-xsrf', 'foo');
-      await esDeleteAllIndices(`${INDEX_PATTERN}*`);
-      await es.deleteByQuery({ index: '.alerts*', query: { match_all: {} } });
+      await esDeleteAllIndices([ALERT_ACTION_PATTERN]);
+
+      // TODO add them as helpers
+      await es.deleteByQuery({
+        index: APM_ALERTS_INDEX,
+        query: { term: { 'kibana.alert.rule.uuid': ruleId } },
+      });
+
+      // can we clean them all? delete only the data we created,
+      // think about wrapping the delete in one function
       await es.deleteByQuery({
         index: '.kibana-event-log-*',
         query: { term: { 'kibana.alert.rule.consumer': 'apm' } },
       });
     });
 
-    describe('Create alert with empty params', () => {
-      const INDEX_NAME = `${INDEX_PATTERN}_empty_params`;
+    describe('create alert with transaction.name group by', () => {
+      const ALERT_ACTION_INDEX_NAME = `${ALERT_ACTION_PATTERN}-transaction-group-by`;
 
       before(async () => {
         actionId = await createIndexConnector({
           supertest,
-          name: 'Duration threshold API test - empty params',
-          indexName: INDEX_NAME,
+          name: 'Transation duration API test',
+          indexName: ALERT_ACTION_INDEX_NAME,
         });
-
         const createdRule = await createApmRule({
           supertest,
           ruleTypeId: ApmRuleType.TransactionDuration,
-          name: 'Apm duration threshold - with empty params',
+          name: 'Apm transaction duration group by transaction name',
           params: {
+            threshold: 3000,
+            windowSize: 5,
+            windowUnit: 'm',
+            transactionType: 'request',
+            serviceName: 'opbeans-java',
             environment: 'production',
-            serviceName: '',
             aggregationType: AggregationType.Avg,
-            transactionName: '',
-            threshold: 90,
-            windowSize: 1,
-            windowUnit: 'h',
+            groupBy: [
+              'service.name',
+              'service.environment',
+              'transaction.type',
+              'transaction.name',
+            ],
           },
           actions: [
             {
               group: 'threshold_met',
               id: actionId,
               params: {
-                documents: [{ message: transactionDurationMessage }],
+                documents: [{ message: transactionDurationMessage, id: actionId }],
               },
               frequency: {
                 notify_when: 'onActionGroupChange',
@@ -91,151 +110,59 @@ export default function ApiTest({ getService }: FtrProviderContext) {
         ruleId = createdRule.id;
       });
 
-      it('checks if alert is active', async () => {
+      it('checks if rule is active', async () => {
         const executionStatus = await waitForRuleStatus({
           id: ruleId,
           expectedStatus: 'active',
           supertest,
         });
-
         expect(executionStatus.status).to.be('active');
       });
 
       it('returns correct message', async () => {
         const resp = await waitForDocumentInIndex<{ message: string }>({
           es,
-          indexName: INDEX_NAME,
+          indexName: ALERT_ACTION_INDEX_NAME,
         });
 
-        expect(resp.hits.hits[0]._source?.message).eql(
-          `Apm duration threshold - with empty params alert is firing because of the following conditions:
+        expect(resp.hits.hits[0]._source?.message).eql(`Transaction Name: tx-java`);
+      });
 
-- Service name: opbeans-node
-- Transaction type: request
-- Transaction name: 
-- Environment: production
-- Latency threshold: 90ms
-- Latency observed: 150 ms over the last 1 hr`
-        );
+      it('indexes alert document with all group-by fields', async () => {
+        const resp = await waitForAlertInIndex({
+          es,
+          indexName: APM_ALERTS_INDEX,
+          ruleId,
+        });
+
+        expect(resp.hits.hits[0]._source).property('service.name', 'opbeans-java');
+        expect(resp.hits.hits[0]._source).property('service.environment', 'production');
+        expect(resp.hits.hits[0]._source).property('transaction.type', 'request');
+        expect(resp.hits.hits[0]._source).property('transaction.name', 'tx-java');
       });
 
       it('shows the correct alert count for each service on service inventory', async () => {
         const serviceInventoryAlertCounts = await fetchServiceInventoryAlertCounts(apmApiClient);
         expect(serviceInventoryAlertCounts).to.eql({
-          'opbeans-node': 1,
-          'opbeans-java': 0,
+          'opbeans-node': 0,
+          'opbeans-java': 1,
         });
       });
 
-      it('shows the correct alert count in opbeans-node service', async () => {
+      it('shows the correct alert count in opbeans-java service', async () => {
         const serviceTabAlertCount = await fetchServiceTabAlertCount({
           apmApiClient,
           serviceName: 'opbeans-java',
-        });
-        expect(serviceTabAlertCount).to.be(0);
-      });
-
-      it('shows the correct alert count in opbeans-node service', async () => {
-        const serviceTabAlertCount = await fetchServiceTabAlertCount({
-          apmApiClient,
-          serviceName: 'opbeans-node',
         });
         expect(serviceTabAlertCount).to.be(1);
       });
-    });
-
-    describe('Create alert with transaction name filter', () => {
-      const INDEX_NAME = `${INDEX_PATTERN}_transaction_name_filter`;
-
-      before(async () => {
-        actionId = await createIndexConnector({
-          supertest,
-          name: 'Duration threshold API test',
-          indexName: INDEX_NAME,
-        });
-
-        const createdRule = await createApmRule({
-          supertest,
-          ruleTypeId: ApmRuleType.TransactionDuration,
-          name: 'Apm duration threshold - with transaction name filter',
-          params: {
-            environment: 'production',
-            serviceName: '',
-            aggregationType: AggregationType.Avg,
-            transactionName: 'tx-node-2',
-            threshold: 90,
-            windowSize: 1,
-            windowUnit: 'h',
-          },
-          actions: [
-            {
-              group: 'threshold_met',
-              id: actionId,
-              params: {
-                documents: [{ message: transactionDurationMessage }],
-              },
-              frequency: {
-                notify_when: 'onActionGroupChange',
-                throttle: null,
-                summary: false,
-              },
-            },
-          ],
-        });
-        expect(createdRule.id).to.not.eql(undefined);
-        ruleId = createdRule.id;
-      });
-
-      it('checks if alert is active', async () => {
-        const executionStatus = await waitForRuleStatus({
-          id: ruleId,
-          expectedStatus: 'active',
-          supertest,
-        });
-
-        expect(executionStatus.status).to.be('active');
-      });
-
-      it('returns correct message', async () => {
-        const resp = await waitForDocumentInIndex<{ message: string }>({
-          es,
-          indexName: INDEX_NAME,
-        });
-
-        expect(resp.hits.hits[0]._source?.message).eql(
-          `Apm duration threshold - with transaction name filter alert is firing because of the following conditions:
-
-- Service name: opbeans-node
-- Transaction type: request
-- Transaction name: tx-node-2
-- Environment: production
-- Latency threshold: 90ms
-- Latency observed: 200 ms over the last 1 hr`
-        );
-      });
-
-      it('shows the correct alert count for each service on service inventory', async () => {
-        const serviceInventoryAlertCounts = await fetchServiceInventoryAlertCounts(apmApiClient);
-        expect(serviceInventoryAlertCounts).to.eql({
-          'opbeans-node': 2,
-          'opbeans-java': 0,
-        });
-      });
-
-      it('shows the correct alert count in opbeans-node service', async () => {
-        const serviceTabAlertCount = await fetchServiceTabAlertCount({
-          apmApiClient,
-          serviceName: 'opbeans-java',
-        });
-        expect(serviceTabAlertCount).to.be(0);
-      });
 
       it('shows the correct alert count in opbeans-node service', async () => {
         const serviceTabAlertCount = await fetchServiceTabAlertCount({
           apmApiClient,
           serviceName: 'opbeans-node',
         });
-        expect(serviceTabAlertCount).to.be(2);
+        expect(serviceTabAlertCount).to.be(0);
       });
     });
   });
