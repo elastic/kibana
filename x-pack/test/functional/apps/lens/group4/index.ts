09266f061f754de3c8bd0003155c47798136df35
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-04-27 13:41:21 +0200

    reorder test files to stabilize

--- x-pack/test/functional/apps/lens/group4/index.ts
+++ x-pack/test/functional/apps/lens/group4/index.ts
@@ -72,13 +72,14 @@ export default ({ getService, loadTestFile, getPageObjects }: FtrProviderContext
     });
 
     // total run time ~16m 30s
+    loadTestFile(require.resolve('./colors')); // 1m 2s
+    loadTestFile(require.resolve('./chart_data')); // 1m 10s
+    loadTestFile(require.resolve('./time_shift')); // 1m
     loadTestFile(require.resolve('./dashboard')); // 6m 45s
     loadTestFile(require.resolve('./show_underlying_data')); // 2m
     loadTestFile(require.resolve('./show_underlying_data_dashboard')); // 2m 10s
     loadTestFile(require.resolve('./share')); // 1m 20s
+    // keep it last in the group
     loadTestFile(require.resolve('./tsdb')); // 1m
-    loadTestFile(require.resolve('./colors')); // 1m 2s
-    loadTestFile(require.resolve('./chart_data')); // 1m 10s
-    loadTestFile(require.resolve('./time_shift')); // 1m
   });
 };
