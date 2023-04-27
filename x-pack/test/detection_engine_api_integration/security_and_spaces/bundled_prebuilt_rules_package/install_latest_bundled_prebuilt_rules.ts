356b7263fd69bd6917bf166b26de42adb65aff21
jpdjere <jpdjeredjian@gmail.com>
2023-04-27 15:37:22 +0200

    Add test explanation

--- x-pack/test/detection_engine_api_integration/security_and_spaces/bundled_prebuilt_rules_package/install_latest_bundled_prebuilt_rules.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/bundled_prebuilt_rules_package/install_latest_bundled_prebuilt_rules.ts
@@ -37,7 +37,9 @@ export default ({ getService }: FtrProviderContext): void => {
 
   /* This test simulates an air-gapped environment in which the user doesn't have access to EPR.
   /* We first download the package from the registry as done during build time, and then
-  /* attempt to install it from the local file system. */
+  /* attempt to install it from the local file system. The API response from EPM provides
+  /* us with the information of whether the package was installed from the registry or
+  /* from a package that was bundled with Kibana */
   describe('install_bundled_prebuilt_rules', () => {
     beforeEach(async () => {
       await deleteAllRules(supertest, log);
