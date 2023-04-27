59afd3a07c1e41933145da4f94dd954cd87bb5e4
Dominique Clarke <dominique.clarke@elastic.co>
2023-04-27 11:12:07 -0400

    Update x-pack/plugins/synthetics/public/apps/synthetics/components/settings/private_locations/delete_location.tsx

--- x-pack/plugins/synthetics/public/apps/synthetics/components/settings/private_locations/delete_location.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/settings/private_locations/delete_location.tsx
@@ -38,7 +38,7 @@ export const DeleteLocation = ({
     ? CANNOT_SAVE_INTEGRATION_LABEL
     : i18n.translate('xpack.synthetics.monitorManagement.cannotDelete.description', {
         defaultMessage: `This location cannot be deleted, because it has {monCount, number} {monCount, plural,one {monitor} other {monitors}} configured.
-                Please remove this location from your {monCount, plural,one {monitor} other {monitors}} before deleting this location.`,
+                Remove this location from your {monCount, plural,one {monitor} other {monitors}} before deleting this location.`,
         values: { monCount },
       });
 
