764ec1f4bf7e3492afcd89e8fccf488a6e4ebd8f
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-04-28 08:16:20 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- src/plugins/data/common/exports/escape_value.test.ts
+++ src/plugins/data/common/exports/escape_value.test.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import { createEscapeValue } from './escape_value';
 
 describe('escapeValue', function () {
