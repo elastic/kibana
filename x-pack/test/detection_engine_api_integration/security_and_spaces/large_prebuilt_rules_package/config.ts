407818fabf8423e1625d8ccd8353bf2e7514a491
jpdjere <jpdjeredjian@gmail.com>
2023-04-28 17:01:52 +0200

    New files

---
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/large_prebuilt_rules_package/config.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { FtrConfigProviderContext } from '@kbn/test';
+import path from 'path';
+
+export const BUNDLED_PACKAGE_DIR = path.join(
+  path.dirname(__filename),
+  './fleet_bundled_packages/fixtures'
+);
+
+// eslint-disable-next-line import/no-default-export
+export default async function ({ readConfigFile }: FtrConfigProviderContext) {
+  const functionalConfig = await readConfigFile(require.resolve('../config.base.ts'));
+
+  return {
+    ...functionalConfig.getAll(),
+    testFiles: [require.resolve('./install_large_prebuilt_rules_package.ts')],
+    kbnTestServer: {
+      ...functionalConfig.get('kbnTestServer'),
+      serverArgs: [
+        ...functionalConfig.get('kbnTestServer.serverArgs'),
+        `--xpack.fleet.registryUrl=http://invalidURL:8080`,
+        `--xpack.fleet.developer.bundledPackageLocation=${BUNDLED_PACKAGE_DIR}`,
+      ],
+      env: {
+        NODE_OPTIONS: '--max-old-space-size=700',
+      },
+    },
+  };
+}
