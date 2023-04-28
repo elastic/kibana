d50bf78f5d6692173cf139ee0b01033aa305e762
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-04-28 20:28:13 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/plugins/data_view_field_editor/public/components/preview/preview_controller.tsx
+++ src/plugins/data_view_field_editor/public/components/preview/preview_controller.tsx
@@ -6,7 +6,6 @@
  * Side Public License, v 1.
  */
 
-import { i18n } from '@kbn/i18n';
 import type { DataView } from '@kbn/data-views-plugin/public';
 import type { ISearchStart } from '@kbn/data-plugin/public';
 import { BehaviorSubject } from 'rxjs';
