93fdbfc102f3759ee5d9bfd9ccdebe57ac464fc4
dej611 <dej611@gmail.com>
2023-04-28 17:31:18 +0200

    :bug: Fix issue

--- packages/kbn-coloring/src/shared_components/coloring/utils.test.ts
+++ packages/kbn-coloring/src/shared_components/coloring/utils.test.ts
@@ -6,15 +6,10 @@
  * Side Public License, v 1.
  */
 
-import {
-  getColorStops,
-  isValidColor,
-  mergePaletteParams,
-  updateRangeType,
-  changeColorPalette,
-} from './utils';
+import { getColorStops, mergePaletteParams, updateRangeType, changeColorPalette } from './utils';
 
 import { getPaletteRegistry } from './mocks/palettes_registry';
+import { isValidColor } from '../../color_manipulation';
 
 describe('getColorStops', () => {
   const paletteRegistry = getPaletteRegistry();
