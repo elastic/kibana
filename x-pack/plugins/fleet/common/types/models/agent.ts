4bedae75cac8abeecc51a8dbbcae492ddc0f406b
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2023-04-28 09:48:50 -0400

    Fix type check

--- x-pack/plugins/fleet/common/types/models/agent.ts
+++ x-pack/plugins/fleet/common/types/models/agent.ts
@@ -190,7 +190,7 @@ export interface FleetServerAgentComponentUnit {
   };
 }
 
-interface FleetServerAgentComponent {
+export interface FleetServerAgentComponent {
   id: string;
   type: string;
   status: FleetServerAgentComponentStatus;
