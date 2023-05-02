86a4cb80d67f4ef9ae9da000105ac6a1ab53bf29
jpdjere <jpdjeredjian@gmail.com>
2023-05-02 14:59:53 +0200

    test

--- x-pack/plugins/security_solution/cypress/e2e/detection_rules/prebuilt_rules_install_update_workflows.cy.ts
+++ x-pack/plugins/security_solution/cypress/e2e/detection_rules/prebuilt_rules_install_update_workflows.cy.ts
@@ -7,7 +7,6 @@
 
 import { createRuleAssetSavedObject } from '../../helpers/rules';
 import {
-  ELASTIC_RULES_BTN,
   LOAD_PREBUILT_RULES_BTN,
   LOAD_PREBUILT_RULES_ON_PAGE_HEADER_BTN,
   RULES_MANAGEMENT_TABLE,
@@ -26,18 +25,6 @@ import { esArchiverResetKibana } from '../../tasks/es_archiver';
 import { login, visitWithoutDateRange } from '../../tasks/login';
 import { SECURITY_DETECTIONS_RULES_URL } from '../../urls/navigation';
 
-interface Asset {
-  id: string;
-  type: string;
-}
-interface PackageItem {
-  name: string;
-  result: {
-    installSource: string;
-    assets: Asset[];
-  };
-}
-
 describe('Detection rules, Prebuilt Rules Installation and Update workflow', () => {
   before(() => {
     cleanKibana();
@@ -49,60 +36,6 @@ describe('Detection rules, Prebuilt Rules Installation and Update workflow', () 
     esArchiverResetKibana();
 
     visitWithoutDateRange(SECURITY_DETECTIONS_RULES_URL);
-  });
-
-  describe('Installation of prebuilt rules package via Fleet', () => {
-    beforeEach(() => {
-      waitForRulesTableToBeLoaded();
-    });
-
-    it('should install package from Fleet in the background', () => {
-      cy.intercept('POST', '/api/fleet/epm/packages/_bulk*').as('installPackage');
-
-      /* Assert that the package in installed from Fleet by checking that
-      /* the installSource is "registry", as opposed to "bundle" */
-      cy.wait('@installPackage').then(({ response }) => {
-        cy.wrap(response?.statusCode).should('eql', 200);
-
-        const packages = response?.body.items.map(({ name, result }: PackageItem) => ({
-          name,
-          installSource: result.installSource,
-        }));
-
-        expect(packages.length).to.have.greaterThan(0);
-        expect(packages).to.deep.include.members([
-          { name: 'security_detection_engine', installSource: 'registry' },
-        ]);
-      });
-    });
-
-    it('should install rules from the Fleet package when user clicks on CTA', () => {
-      cy.intercept('POST', '/api/fleet/epm/packages/_bulk*').as('installPackage');
-
-      /* Retrieve how many rules were installed from the Fleet package */
-      cy.wait('@installPackage').then(({ response }) => {
-        const packageAssets = response?.body.items.find(
-          ({ name }: PackageItem) => name === 'security_detection_engine'
-        ).result.assets;
-
-        const rulesWithHistoricalVersions = packageAssets.filter(
-          ({ type }: Asset) => type === 'security-rule'
-        );
-
-        // Get unique rules to install by removing version appendix
-        // from rule id and then removing duplicates
-        const numberOfRulesToInstall = [
-          ...new Set(rulesWithHistoricalVersions.map(({ id }: Asset) => id.split('_')[0])),
-        ].length;
-
-        cy.get(LOAD_PREBUILT_RULES_BTN).click();
-        cy.get(LOAD_PREBUILT_RULES_BTN).should('have.attr', 'disabled');
-        cy.get(LOAD_PREBUILT_RULES_BTN).should('not.exist');
-        cy.get(TOASTER).should('be.visible').contains('Installed pre-packaged rules');
-
-        cy.get(ELASTIC_RULES_BTN).contains(numberOfRulesToInstall);
-      });
-    });
   });
 
   describe('Installation of prebuilt rules', () => {
