0b231c60a2dac0534db5e8a47da6f0a5f44e2270
Jean-Louis Leysens <jloleysens@gmail.com>
2023-05-02 10:08:32 +0200

    boolean

--- packages/kbn-schema/src/types/boolean_type.ts
+++ packages/kbn-schema/src/types/boolean_type.ts
@@ -6,18 +6,11 @@
  * Side Public License, v 1.
  */
 
-import typeDetect from 'type-detect';
 import { internals } from '../internals';
 import { Type, TypeOptions } from './type';
 
 export class BooleanType extends Type<boolean> {
   constructor(options?: TypeOptions<boolean>) {
     super(internals.boolean(), options);
-  }
-
-  protected handleError(type: string, { value }: Record<string, any>) {
-    if (type === 'any.required' || type === 'boolean.base') {
-      return `expected value of type [boolean] but got [${typeDetect(value)}]`;
-    }
   }
 }
