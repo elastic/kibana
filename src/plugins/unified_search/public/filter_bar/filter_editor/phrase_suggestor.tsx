elastic/kibana

commit 2459819c622c58552ad548ce5b7ead846a6501b4

[unified search] allowing to pass additional filters used for suggestions (#154522)

src/plugins/unified_search/public/filter_bar/filter_editor/phrase_suggestor.tsx
@@ -12,6 +12,7 @@ import { UI_SETTINGS } from '@kbn/data-plugin/common';
 import { DataView, DataViewField } from '@kbn/data-views-plugin/common';
 import { debounce } from 'lodash';
 
+import { buildQueryFromFilters, Filter } from '@kbn/es-query';
 import { IUnifiedSearchPluginServices } from '../../types';
 
 export interface PhraseSuggestorProps {
@@ -19,6 +20,7 @@ export interface PhraseSuggestorProps {
   indexPattern: DataView;
   field: DataViewField;
   timeRangeForSuggestionsOverride?: boolean;
+  filtersForSuggestions?: Filter[];
 }
 
 export interface PhraseSuggestorState {
@@ -73,7 +75,7 @@ export class PhraseSuggestorUI<T extends PhraseSuggestorProps> extends React.Com
   protected updateSuggestions = debounce(async (query: string = '') => {
     if (this.abortController) this.abortController.abort();
     this.abortController = new AbortController();
-    const { indexPattern, field, timeRangeForSuggestionsOverride } = this
+    const { indexPattern, field, timeRangeForSuggestionsOverride, filtersForSuggestions } = this
       .props as PhraseSuggestorProps;
     if (!field || !this.isSuggestingValues()) {
       return;
@@ -85,6 +87,8 @@ export class PhraseSuggestorUI<T extends PhraseSuggestorProps> extends React.Com
       query,
       signal: this.abortController.signal,
       useTimeRange: timeRangeForSuggestionsOverride,
+      boolFilter: buildQueryFromFilters(filtersForSuggestions, undefined).filter,
+      method: filtersForSuggestions?.length ? 'terms_agg' : 'terms_enum',
     });
 
     this.setState({ suggestions, isLoading: false });

src/plugins/unified_search/public/query_string_input/query_string_input.tsx
@@ -36,6 +36,7 @@ import { getFieldSubtypeNested, KIBANA_USER_QUERY_LANGUAGE_KEY } from '@kbn/data
 import { toMountPoint } from '@kbn/kibana-react-plugin/public';
 import type { IStorageWrapper } from '@kbn/kibana-utils-plugin/public';
 import type { UsageCollectionStart } from '@kbn/usage-collection-plugin/public';
+import { buildQueryFromFilters, Filter } from '@kbn/es-query';
 import { matchPairs } from './match_pairs';
 import { toUser } from './to_user';
 import { fromUser } from './from_user';
@@ -137,6 +138,11 @@ export interface QueryStringInputProps {
    * Override whether autocomplete suggestions are restricted by time range.
    */
   timeRangeForSuggestionsOverride?: boolean;
+
+  /**
+   * Add additional filters used for suggestions
+   */
+  filtersForSuggestions?: Filter[];
 }
 
 interface State {
@@ -279,6 +285,8 @@ export default class QueryStringInputUI extends PureComponent<QueryStringInputPr
           selectionEnd,
           signal: this.abortController.signal,
           useTimeRange: this.props.timeRangeForSuggestionsOverride,
+          boolFilter: buildQueryFromFilters(this.props.filtersForSuggestions, undefined).filter,
+          method: this.props.filtersForSuggestions?.length ? 'terms_agg' : 'terms_enum',
         })) || [];
       return [...suggestions, ...recentSearchSuggestions];
     } catch (e) {

src/plugins/unified_search/public/__stories__/search_bar.stories.tsx
@@ -8,11 +8,12 @@
 
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import type { Query } from '@kbn/es-query';
+import type { DataViewBase, Query } from '@kbn/es-query';
 import { storiesOf } from '@storybook/react';
 import { I18nProvider } from '@kbn/i18n-react';
 import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
 import type { DataView, DataViewsContract } from '@kbn/data-views-plugin/public';
+import { buildExistsFilter } from '@kbn/es-query';
 import { SearchBar, SearchBarProps } from '../search_bar';
 import { setIndexPatterns } from '../services';
 
@@ -259,6 +260,15 @@ storiesOf('SearchBar', module)
       showFilterBar: false,
       showQueryInput: false,
     } as SearchBarProps)
+  )
+  .add('with additional filters used for suggestions', () =>
+    wrapSearchBarInContext({
+      filtersForSuggestions: [
+        buildExistsFilter({ type: 'keyword', name: 'geo.src' }, {
+          id: undefined,
+        } as unknown as DataViewBase),
+      ],
+    } as unknown as SearchBarProps)
   )
   .add('with only the filter bar on', () =>
     wrapSearchBarInContext({

src/plugins/unified_search/public/filter_bar/filter_bar.tsx
@@ -22,6 +22,7 @@ export interface Props {
   indexPatterns: DataView[];
   intl: InjectedIntl;
   timeRangeForSuggestionsOverride?: boolean;
+  filtersForSuggestions?: Filter[];
   hiddenPanelOptions?: FilterItemsProps['hiddenPanelOptions'];
   /**
    * Applies extra styles necessary when coupled with the query bar
@@ -54,6 +55,7 @@ const FilterBarUI = React.memo(function FilterBarUI(props: Props) {
         onFiltersUpdated={props.onFiltersUpdated}
         indexPatterns={props.indexPatterns!}
         timeRangeForSuggestionsOverride={props.timeRangeForSuggestionsOverride}
+        filtersForSuggestions={props.filtersForSuggestions}
         hiddenPanelOptions={props.hiddenPanelOptions}
         readOnly={props.isDisabled}
       />

src/plugins/unified_search/public/filter_bar/filter_editor/filter_editor.tsx
@@ -126,6 +126,7 @@ export interface FilterEditorComponentProps {
   onLocalFilterCreate?: (initialState: { filter: Filter; queryDslFilter: QueryDslFilter }) => void;
   onLocalFilterUpdate?: (filter: Filter | QueryDslFilter) => void;
   timeRangeForSuggestionsOverride?: boolean;
+  filtersForSuggestions?: Filter[];
   mode?: 'edit' | 'add';
 }
 
@@ -334,6 +335,7 @@ class FilterEditorComponent extends Component<FilterEditorProps, State> {
             <FiltersBuilder
               filters={[localFilter]}
               timeRangeForSuggestionsOverride={this.props.timeRangeForSuggestionsOverride}
+              filtersForSuggestions={this.props.filtersForSuggestions}
               dataView={selectedDataView!}
               onChange={this.onLocalFilterChange}
               disabled={!selectedDataView}

src/plugins/unified_search/public/filter_bar/filter_item/filter_item.tsx
@@ -56,6 +56,7 @@ export interface FilterItemProps extends WithCloseFilterEditorConfirmModalProps 
   uiSettings: IUiSettingsClient;
   hiddenPanelOptions?: FilterPanelOption[];
   timeRangeForSuggestionsOverride?: boolean;
+  filtersForSuggestions?: Filter[];
   readOnly?: boolean;
 }
 
@@ -391,6 +392,7 @@ function FilterItemComponent(props: FilterItemProps) {
                 onLocalFilterCreate={onLocalFilterCreate}
                 onCancel={() => setIsPopoverOpen(false)}
                 timeRangeForSuggestionsOverride={props.timeRangeForSuggestionsOverride}
+                filtersForSuggestions={props.filtersForSuggestions}
               />
             </div>,
           ]}

src/plugins/unified_search/public/filter_bar/filter_item/filter_items.tsx
@@ -35,6 +35,8 @@ export interface FilterItemsProps {
   intl: InjectedIntl;
   /** Controls whether or not filter suggestions are influenced by the global time */
   timeRangeForSuggestionsOverride?: boolean;
+  /** adds additional filters to be used for suggestions */
+  filtersForSuggestions?: Filter[];
   /** Array of panel options that controls the styling of each filter pill */
   hiddenPanelOptions?: FilterItemProps['hiddenPanelOptions'];
 }
@@ -74,6 +76,7 @@ const FilterItemsUI = React.memo(function FilterItemsUI(props: FilterItemsProps)
           uiSettings={uiSettings!}
           hiddenPanelOptions={props.hiddenPanelOptions}
           timeRangeForSuggestionsOverride={props.timeRangeForSuggestionsOverride}
+          filtersForSuggestions={props.filtersForSuggestions}
           readOnly={readOnly}
         />
       </EuiFlexItem>

src/plugins/unified_search/public/filters_builder/context.ts
@@ -8,6 +8,7 @@
 
 import React, { Dispatch } from 'react';
 import type { DataView } from '@kbn/data-views-plugin/common';
+import { Filter } from '@kbn/es-query';
 import type { FiltersBuilderActions } from './reducer';
 
 interface FiltersBuilderContextType {
@@ -19,6 +20,7 @@ interface FiltersBuilderContextType {
   };
   dropTarget: string;
   timeRangeForSuggestionsOverride?: boolean;
+  filtersForSuggestions?: Filter[];
   disabled: boolean;
 }
 

commit 63c1f204608082f43802788a7e2fed751b3aa79f

[Unified search] Fixes the bar suggestions method selection (#156329)
## Summary

Closes https://github.com/elastic/kibana/issues/155801

Fixes the bug described here
https://github.com/elastic/kibana/issues/155801

The bug was introduced by
https://github.com/elastic/kibana/pull/154522/files

If the filtersForSuggestions prop is there, we want to default to
terms_agg, otherwise we want to keep it as undefined to use the advanced
settings method.

src/plugins/unified_search/public/filter_bar/filter_editor/phrase_suggestor.tsx
@@ -88,7 +88,7 @@ export class PhraseSuggestorUI<T extends PhraseSuggestorProps> extends React.Com
       signal: this.abortController.signal,
       useTimeRange: timeRangeForSuggestionsOverride,
       boolFilter: buildQueryFromFilters(filtersForSuggestions, undefined).filter,
-      method: filtersForSuggestions?.length ? 'terms_agg' : 'terms_enum',
+      method: filtersForSuggestions?.length ? 'terms_agg' : undefined,
     });
 
     this.setState({ suggestions, isLoading: false });

src/plugins/unified_search/public/query_string_input/query_string_input.tsx
@@ -286,7 +286,7 @@ export default class QueryStringInputUI extends PureComponent<QueryStringInputPr
           signal: this.abortController.signal,
           useTimeRange: this.props.timeRangeForSuggestionsOverride,
           boolFilter: buildQueryFromFilters(this.props.filtersForSuggestions, undefined).filter,
-          method: this.props.filtersForSuggestions?.length ? 'terms_agg' : 'terms_enum',
+          method: this.props.filtersForSuggestions?.length ? 'terms_agg' : undefined,
         })) || [];
       return [...suggestions, ...recentSearchSuggestions];
     } catch (e) {