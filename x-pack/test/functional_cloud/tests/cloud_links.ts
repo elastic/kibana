elastic/kibana

commit b75546f7eb59f49e0d2daefa904736fe7e481f52

[Guided onboarding] Use Kibana features to grant access (#155065)
## Summary

Fixes https://github.com/elastic/kibana/issues/149132

This PR adds a Kibana feature for the guided onboarding plugin for
better permissions handling. By default `kibana_admin` and `editor`
roles are granted access to guided onboarding. The role `viewer` on the
other hand doesn't have enough permissions to see or use guided
onboarding. For any roles that don't have the correct permissions,
guided onboarding is completely disabled, the same as it's disabled
on-prem.
When creating a new role, the feature "Setup guides" can be enabled or
disabled.

### How to test
1. Add `xpack.cloud.id: 'testID'` to `/config/kibana.dev.yml`
1. Start ES with `yarn es snapshot` and Kibana with `yarn start``
2. Login as elastic and create a test user with the role `viewer`
3. Clear everything from your browser's local storage 
4. Login as the test user and check the following
- On the first visit, the "on-prem" welcome message is shown (not the
guided onboarding landing page)
- The url `/app/home#/getting_started` is unknown and redirects back to
the home page
- There is no button "Setup guides" in the header
- There is no link "Setup guides" in the help menu

### Checklist

- [x] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/cloud_integrations/cloud_links/kibana.jsonc
@@ -9,7 +9,8 @@
     "browser": true,
     "optionalPlugins": [
       "cloud",
-      "security"
+      "security",
+      "guided_onboarding"
     ]
   }
 }

src/plugins/guided_onboarding/kibana.jsonc
@@ -8,7 +8,8 @@
     "server": true,
     "browser": true,
     "optionalPlugins": [
-      "cloud"
+      "cloud",
+      "features"
     ],
     "requiredBundles": [
       "kibanaReact"

examples/guided_onboarding_example/public/components/app.tsx
@@ -11,13 +11,7 @@ import { FormattedMessage, I18nProvider } from '@kbn/i18n-react';
 import { Router, Switch } from 'react-router-dom';
 import { Route } from '@kbn/shared-ux-router';
 
-import {
-  EuiPage,
-  EuiPageBody,
-  EuiPageContent_Deprecated as EuiPageContent,
-  EuiPageHeader,
-  EuiTitle,
-} from '@elastic/eui';
+import { EuiPageTemplate } from '@elastic/eui';
 
 import { CoreStart, ScopedHistory } from '@kbn/core/public';
 
@@ -39,19 +33,17 @@ export const GuidedOnboardingExampleApp = (props: GuidedOnboardingExampleAppDeps
 
   return (
     <I18nProvider>
-      <EuiPage restrictWidth="1000px">
-        <EuiPageBody>
-          <EuiPageHeader>
-            <EuiTitle size="l">
-              <h1>
-                <FormattedMessage
-                  id="guidedOnboardingExample.title"
-                  defaultMessage="Guided onboarding examples"
-                />
-              </h1>
-            </EuiTitle>
-          </EuiPageHeader>
-          <EuiPageContent>
+      <EuiPageTemplate restrictWidth={true} panelled={true}>
+        <EuiPageTemplate.Header
+          pageTitle={
+            <FormattedMessage
+              id="guidedOnboardingExample.title"
+              defaultMessage="Guided onboarding examples"
+            />
+          }
+        />
+        {guidedOnboarding.guidedOnboardingApi?.isEnabled ? (
+          <EuiPageTemplate.Section>
             <Router history={history}>
               <Switch>
                 <Route exact path="/">
@@ -75,9 +67,31 @@ export const GuidedOnboardingExampleApp = (props: GuidedOnboardingExampleAppDeps
                 />
               </Switch>
             </Router>
-          </EuiPageContent>
-        </EuiPageBody>
-      </EuiPage>
+          </EuiPageTemplate.Section>
+        ) : (
+          <EuiPageTemplate.EmptyPrompt
+            iconType="error"
+            color="danger"
+            title={
+              <h2>
+                <FormattedMessage
+                  id="guidedOnboardingExample.errorTitle"
+                  defaultMessage="Guided onboarding is disabled"
+                />
+              </h2>
+            }
+            body={
+              <p>
+                <FormattedMessage
+                  id="guidedOnboardingExample.errorDescription"
+                  defaultMessage="Make sure your Kibana instance runs on Cloud and/or
+                  your user has access to Setup guides feature."
+                />
+              </p>
+            }
+          />
+        )}
+      </EuiPageTemplate>
     </I18nProvider>
   );
 };

src/plugins/guided_onboarding/common/constants.ts
@@ -9,4 +9,6 @@
 export const PLUGIN_ID = 'guidedOnboarding';
 export const PLUGIN_NAME = 'guidedOnboarding';
 
+export const PLUGIN_FEATURE = 'guidedOnboardingFeature';
+
 export const API_BASE_PATH = '/internal/guided_onboarding';

src/plugins/guided_onboarding/public/mocks.ts
@@ -28,6 +28,7 @@ const apiServiceMock: jest.Mocked<GuidedOnboardingPluginStart> = {
     isGuidePanelOpen$: new BehaviorSubject(false),
     isLoading$: new BehaviorSubject(false),
     getGuideConfig: jest.fn(),
+    isEnabled: true,
   },
 };
 

src/plugins/guided_onboarding/public/plugin.tsx
@@ -21,6 +21,8 @@ import {
 } from '@kbn/core/public';
 
 import { KibanaThemeProvider } from '@kbn/kibana-react-plugin/public';
+
+import { PLUGIN_FEATURE } from '../common/constants';
 import type {
   AppPluginStartDependencies,
   GuidedOnboardingPluginSetup,
@@ -43,11 +45,12 @@ export class GuidedOnboardingPlugin
   ): GuidedOnboardingPluginStart {
     const { chrome, http, theme, application, notifications, uiSettings } = core;
 
+    // Guided onboarding UI is only available on cloud and if the access to the Kibana feature is granted
+    const isEnabled = !!(cloud?.isCloudEnabled && application.capabilities[PLUGIN_FEATURE].enabled);
     // Initialize services
-    apiService.setup(http, !!cloud?.isCloudEnabled);
+    apiService.setup(http, isEnabled);
 
-    // Guided onboarding UI is only available on cloud
-    if (cloud?.isCloudEnabled) {
+    if (isEnabled) {
       chrome.navControls.registerExtension({
         order: 1000,
         mount: (target) =>

src/plugins/guided_onboarding/public/services/api.service.ts
@@ -41,15 +41,15 @@ import {
 import { ConfigService } from './config.service';
 
 export class ApiService implements GuidedOnboardingApi {
-  private isCloudEnabled: boolean | undefined;
+  private _isEnabled: boolean = false;
   private client: HttpSetup | undefined;
   private pluginState$!: BehaviorSubject<PluginState | undefined>;
   public isLoading$ = new BehaviorSubject<boolean>(false);
   public isGuidePanelOpen$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private configService = new ConfigService();
 
-  public setup(httpClient: HttpSetup, isCloudEnabled: boolean) {
-    this.isCloudEnabled = isCloudEnabled;
+  public setup(httpClient: HttpSetup, isEnabled: boolean) {
+    this._isEnabled = isEnabled;
     this.client = httpClient;
     this.pluginState$ = new BehaviorSubject<PluginState | undefined>(undefined);
     this.isGuidePanelOpen$ = new BehaviorSubject<boolean>(false);
@@ -94,7 +94,7 @@ export class ApiService implements GuidedOnboardingApi {
    * Subsequently, the observable is updated automatically, when the state changes.
    */
   public fetchPluginState$(): Observable<PluginState | undefined> {
-    if (!this.isCloudEnabled) {
+    if (!this._isEnabled) {
       return of(undefined);
     }
     if (!this.client) {
@@ -118,7 +118,7 @@ export class ApiService implements GuidedOnboardingApi {
    * where all guides are displayed with their corresponding status.
    */
   public async fetchAllGuidesState(): Promise<{ state: GuideState[] } | undefined> {
-    if (!this.isCloudEnabled) {
+    if (!this._isEnabled) {
       return undefined;
     }
     if (!this.client) {
@@ -143,7 +143,7 @@ export class ApiService implements GuidedOnboardingApi {
     state: { status?: PluginStatus; guide?: GuideState },
     panelState: boolean
   ): Promise<{ pluginState: PluginState } | undefined> {
-    if (!this.isCloudEnabled) {
+    if (!this._isEnabled) {
       return undefined;
     }
     if (!this.client) {
@@ -467,7 +467,7 @@ export class ApiService implements GuidedOnboardingApi {
    * @return {Promise} a promise with the guide config or undefined if the config is not found
    */
   public async getGuideConfig(guideId: GuideId): Promise<GuideConfig | undefined> {
-    if (!this.isCloudEnabled) {
+    if (!this._isEnabled) {
       return undefined;
     }
     if (!this.client) {
@@ -477,6 +477,10 @@ export class ApiService implements GuidedOnboardingApi {
     const config = await this.configService.getGuideConfig(guideId);
     this.isLoading$.next(false);
     return config;
+  }
+
+  public get isEnabled() {
+    return this._isEnabled;
   }
 }
 

src/plugins/guided_onboarding/public/types.ts
@@ -62,4 +62,5 @@ export interface GuidedOnboardingApi {
   isGuidePanelOpen$: Observable<boolean>;
   isLoading$: Observable<boolean>;
   getGuideConfig: (guideId: GuideId) => Promise<GuideConfig | undefined>;
+  readonly isEnabled: boolean;
 }

commit d00e2a366f872d80b5cd598765f8a5aee50cc399

[Guided onboarding] Fix the missing help link (#156399)
## Summary

This PR fixes a bug introduced in
https://github.com/elastic/kibana/pull/155065/ that I noticed when
testing guided onboarding for v8.9.0: the help link was missing from the
help menu. I added a functional test for the help link but we also need
a test that the link is hidden when the user doesn't have access to
guided onboarding (opened an issue for that
[here](https://github.com/elastic/kibana/issues/156410)).

### How to test
1. Add xpack.cloud.id: 'testID' to /config/kibana.dev.yml
2. Start ES with yarn es snapshot and Kibana with `yarn start``
3. Check that the help link "Setup guides" is displayed in the help menu

### Screenshot 
<img width="336" alt="Screenshot 2023-05-02 at 17 27 20"
src="https://user-images.githubusercontent.com/6585477/235712599-812b422d-3092-45b3-a726-37f6b90e81c1.png">

x-pack/plugins/cloud_integrations/cloud_links/kibana.jsonc
@@ -10,7 +10,7 @@
     "optionalPlugins": [
       "cloud",
       "security",
-      "guided_onboarding"
+      "guidedOnboarding"
     ]
   }
 }

x-pack/test/functional_cloud/tests/cloud_links.ts
@@ -30,10 +30,22 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await PageObjects.header.waitUntilLoadingHasFinished();
     });
 
-    it('The button "Setup guides" is loaded', async () => {
-      expect(await find.byCssSelector('[data-test-subj="guideButtonRedirect"]')).to.not.be(null);
-      const cloudLink = await find.byLinkText('Setup guides');
-      expect(cloudLink).to.not.be(null);
+    describe('Guided onboarding', () => {
+      it('The button "Setup guides" is loaded', async () => {
+        expect(await find.byCssSelector('[data-test-subj="guideButtonRedirect"]')).to.not.be(null);
+        const cloudLink = await find.byLinkText('Setup guides');
+        expect(cloudLink).to.not.be(null);
+      });
+
+      it('The help link "Setup guides" is added', async () => {
+        await PageObjects.common.clickAndValidate(
+          'helpMenuButton',
+          'cloudOnboardingSetupGuideLink'
+        );
+        expect(
+          await find.byCssSelector('[data-test-subj="cloudOnboardingSetupGuideLink"]')
+        ).to.not.be(null);
+      });
     });
 
     it('"Manage this deployment" is appended to the nav list', async () => {