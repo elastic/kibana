98ec2a610ca8593f4fdf475a4a4ce874e29b8e2a
Ashokaditya <ashokaditya@elastic.co>
2023-05-03 16:46:40 +0200

    replace all . with -
    refs elastic/kibana/pull/155360

--- x-pack/plugins/security_solution/scripts/endpoint/endpoint_agent_runner/elastic_endpoint.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/endpoint_agent_runner/elastic_endpoint.ts
@@ -35,7 +35,7 @@ export const enrollEndpointHost = async (): Promise<string | undefined> => {
 
   try {
     const uniqueId = Math.random().toString().substring(2, 6);
-    const username = userInfo().username.toLowerCase().replace('.', '-'); // Multipass doesn't like periods in username
+    const username = userInfo().username.toLowerCase().replaceAll('.', '-'); // Multipass doesn't like periods in username
     const policyId: string = policy || (await getOrCreateAgentPolicyId());
 
     if (!policyId) {
