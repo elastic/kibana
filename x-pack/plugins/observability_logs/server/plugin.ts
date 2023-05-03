90433a86e09ccb98f123a86e5af8122a3a3992c5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-05-03 10:10:39 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/observability_logs/server/plugin.ts
+++ x-pack/plugins/observability_logs/server/plugin.ts
@@ -1,10 +1,11 @@
-import {
-  PluginInitializerContext,
-  CoreSetup,
-  CoreStart,
-  Plugin,
-  Logger,
-} from '../../../../src/core/server';
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { PluginInitializerContext, CoreSetup, CoreStart, Plugin, Logger } from '@kbn/core/server';
 
 import { ObservabilityLogsPluginSetup, ObservabilityLogsPluginStart } from './types';
 import { defineRoutes } from './routes';
