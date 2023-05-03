6bcf4f8e6b0a82246a45c8f7dfbe239e9728764b
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-05-03 10:10:39 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/observability_logs/server/routes/index.ts
+++ x-pack/plugins/observability_logs/server/routes/index.ts
@@ -1,4 +1,11 @@
-import { IRouter } from '../../../../../src/core/server';
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { IRouter } from '@kbn/core/server';
 
 export function defineRoutes(router: IRouter) {
   router.get(
