4d1d94424ef4e9b0ce86945e2ef94c71e7353f4e
Nick Peihl <nick.peihl@elastic.co>
2023-05-03 18:23:48 -0400

    Create and register navigation embeddable plugin

---
+++ src/plugins/navigation_embeddable/public/navigation_embeddable/index.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export { NAVIGATION_EMBEDDABLE_TYPE, NavigationEmbeddable } from './navigation_embeddable';
+export type { NavigationEmbeddableFactory } from './navigation_embeddable_factory';
+export { NavigationEmbeddableFactoryDefinition } from './navigation_embeddable_factory';
