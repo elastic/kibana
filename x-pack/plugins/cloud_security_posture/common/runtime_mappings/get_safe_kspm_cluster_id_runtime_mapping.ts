17528b291af9fc7601a9c408cb235f0de083acce
Kfir Peled <61654899+kfirpeled@users.noreply.github.com>
2023-05-04 08:41:27 -0600

    [Cloud security] Fix runtime fields scripts - checking field exist (#156458)

--- x-pack/plugins/cloud_security_posture/common/runtime_mappings/get_safe_kspm_cluster_id_runtime_mapping.ts
+++ x-pack/plugins/cloud_security_posture/common/runtime_mappings/get_safe_kspm_cluster_id_runtime_mapping.ts
@@ -11,19 +11,20 @@ import { MappingRuntimeFields } from '@elastic/elasticsearch/lib/api/types';
  * Creates the `safe_posture_type` runtime field with the value of either
  * `kspm` or `cspm` based on the value of `rule.benchmark.posture_type`
  */
-export const getSafePostureTypeRuntimeMapping = (): MappingRuntimeFields => ({
-  safe_posture_type: {
+export const getSafeKspmClusterIdRuntimeMapping = (): MappingRuntimeFields => ({
+  safe_kspm_cluster_id: {
     type: 'keyword',
     script: {
       source: `
-        def postureTypeAvailable = doc.containsKey("rule.benchmark.posture_type") &&
-          !doc["rule.benchmark.posture_type"].empty;
+        def orchestratorIdAvailable = doc.containsKey("orchestrator.cluster.id") &&
+          !doc["orchestrator.cluster.id"].empty;
+        def clusterIdAvailable = doc.containsKey("cluster_id") &&
+          !doc["cluster_id"].empty;
 
-        if (!postureTypeAvailable) {
-          // Before 8.7 release
-          emit("kspm");
-        } else {
-          emit(doc["rule.benchmark.posture_type"].value);
+        if (orchestratorIdAvailable) {
+          emit(doc["orchestrator.cluster.id"].value);
+        } else if (clusterIdAvailable) {
+          emit(doc["cluster_id"].value);
         }
       `,
     },
