5782352cabcc7abba272bcf16c1155937b06d7f1
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-04 14:00:58 +0100

    [FAKE SOLUTION] BLAH BLAH (#156659) yada yada

--- .buildkite/scripts/steps/gh_labels.ts
+++ .buildkite/scripts/steps/gh_labels.ts
@@ -9,9 +9,36 @@
 // import { GithubApi } from '../../../src/dev/prs/github_api';
 
 const parseTarget = process.argv[2] ?? 'BUILDKITE_MESSAGE';
-console.log(`\n### parseTarget: \n  ${parseTarget}`);
 
 console.log(`\n### process.env.BUILDKITE_MESSAGE:
 ${process.env[parseTarget]}`);
 
+// const mockBkMsg = `[Security Solution] Add technical preview tag to risk info panel and risk table tooltip (#156659)
+
+// blah blah blah
+// ### Checklist
+
+// - [x] [Unit or functional
+// tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
+// BLAH BLAH THE END`;
+
+// console.log(`\n### mockBkMsg: \n  ${mockBkMsg}`);
+
+const head = (x: string) => x.split('\n');
+const prNumWithinMsgRe = /\(\#(\d+)\)/;
+// @ts-expect-error
+const parse = (x: string) => x[0].match(prNumWithinMsgRe)[1];
+// const prNum = pipe(head, parse)(mockBkMsg);
+const prNum = pipe(head, parse)(`${process.env[parseTarget]}`);
+
+console.log(`\n### prNum: \n  ${prNum}`);
+
 export {};
+
+function pipe(...fns: any[]) {
+  return fns.reduce(
+    (f, g) =>
+      (...args: any[]) =>
+        g(f(...args))
+  );
+}
