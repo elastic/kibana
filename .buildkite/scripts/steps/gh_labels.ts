8f08ae14bcc871309a0e6c70de831260ac004e15
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-04 13:06:52 +0100

    let's see

---
+++ .buildkite/scripts/steps/gh_labels.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+// import { GithubApi } from '../../../src/dev/prs/github_api';
+
+const parseTarget = process.argv[2] ?? 'BUILDKITE_MESSAGE';
+
+console.log(`\n### parseTarget: \n  ${parseTarget}`);
+
+export {};
