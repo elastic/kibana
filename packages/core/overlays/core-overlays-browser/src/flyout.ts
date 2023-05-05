2fa3b1228f3f1aa9e718b3d5b831bc5340a14885
nreese <reese.nathan@elastic.co>
2023-05-05 10:58:38 -0600

    fix console errors in inspector

--- packages/core/overlays/core-overlays-browser/src/flyout.ts
+++ packages/core/overlays/core-overlays-browser/src/flyout.ts
@@ -5,7 +5,7 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import type { EuiFlyoutSize, EuiOverlayMaskProps } from '@elastic/eui';
+import type { EuiFlyoutSize, EuiFlyoutProps } from '@elastic/eui';
 import type { MountPoint, OverlayRef } from '@kbn/core-mount-utils-browser';
 
 /**
@@ -28,20 +28,4 @@ export interface OverlayFlyoutStart {
 /**
  * @public
  */
-export interface OverlayFlyoutOpenOptions {
-  className?: string;
-  closeButtonAriaLabel?: string;
-  ownFocus?: boolean;
-  'data-test-subj'?: string;
-  'aria-label'?: string;
-  size?: EuiFlyoutSize;
-  maxWidth?: boolean | number | string;
-  hideCloseButton?: boolean;
-  outsideClickCloses?: boolean;
-  maskProps?: EuiOverlayMaskProps;
-  /**
-   * EuiFlyout onClose handler.
-   * If provided the consumer is responsible for calling flyout.close() to close the flyout;
-   */
-  onClose?: (flyout: OverlayRef) => void;
-}
+export type OverlayFlyoutOpenOptions = EuiFlyoutProps;
