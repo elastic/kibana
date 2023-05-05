f81c723bd9857e80a062b1debe2b38d4c3328166
Patrick Mueller <patrick.mueller@elastic.co>
2023-05-05 15:43:44 -0400

    more diagnostics, more fixing type-y issues

--- packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
+++ packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 
+import execa from 'execa';
 import Path from 'path';
 import Fs from 'fs';
 
@@ -30,6 +31,17 @@ interface FtrConfigsManifest {
 console.log(`REPO_ROOT: ${REPO_ROOT}`);
 // eslint-disable-next-line no-console
 console.log(`cwd: ${process.cwd()}`);
+
+ls('.');
+ls(REPO_ROOT);
+ls(`${REPO_ROOT}/.buildkite`);
+
+async function ls(dir: string) {
+  const { stdout } = await execa('ls', ['-la', dir]);
+  // eslint-disable-next-line no-console
+  console.log(`ls ${dir}:\n${stdout}\nls (end)`);
+}
+
 const ftrConfigsManifest: FtrConfigsManifest = JsYaml.safeLoad(
   Fs.readFileSync(Path.resolve(REPO_ROOT, FTR_CONFIGS_MANIFEST_REL), 'utf8')
 );
