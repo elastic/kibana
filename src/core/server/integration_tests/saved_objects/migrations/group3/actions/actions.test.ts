bb8bf99b434bbaafe5f3e7c7414d231f2effce2d
Tiago Costa <tiago.costa@elastic.co>
2023-05-05 19:14:07 +0100

    skip failing es promotion suites (#156903)

--- src/core/server/integration_tests/saved_objects/migrations/group3/actions/actions.test.ts
+++ src/core/server/integration_tests/saved_objects/migrations/group3/actions/actions.test.ts
@@ -706,7 +706,9 @@ describe('migration actions', () => {
 
   // Reindex doesn't return any errors on it's own, so we have to test
   // together with waitForReindexTask
-  describe('reindex & waitForReindexTask', () => {
+  //
+  // FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/156903
+  describe.skip('reindex & waitForReindexTask', () => {
     it('resolves right when reindex succeeds without reindex script', async () => {
       const res = (await reindex({
         client,
