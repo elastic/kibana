063b9771b165738d20c7df10b3b16a331ce10f4f
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-05 11:34:45 +0100

    reorg

--- .buildkite/scripts/steps/gh_labels.ts
+++ .buildkite/scripts/steps/gh_labels.ts
@@ -6,80 +6,38 @@
  * Side Public License, v 1.
  */
 
-// import { GithubApi } from '../../../src/dev/prs/github_api';
-// import execa from 'execa';
-// import { REPO_ROOT } from '@kbn/repo-info';
 import { execSync } from 'child_process';
 
-// const github = new Octokit({
-//   auth: process.env.GITHUB_TOKEN,
-// });
 const parseTarget = process.argv[2] ?? 'BUILDKITE_MESSAGE';
+// console.log(`\n### process.env.BUILDKITE_MESSAGE:
+// ${process.env[parseTarget]}`);
 
-console.log(`\n### process.env.BUILDKITE_MESSAGE:
-${process.env[parseTarget]}`);
-
-// const mockBkMsg = `[Security Solution] Add technical preview tag to risk info panel and risk table tooltip (#156659)
-
-// blah blah blah
-// ### Checklist
-
-// - [x] [Unit or functional
-// tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
-// BLAH BLAH THE END`;
-
-// console.log(`\n### mockBkMsg: \n  ${mockBkMsg}`);
-
-const head = (x: string): string[] => x.split('\n');
-const prNumWithinMsgRe = /\(\#(\d+)\)/;
 // @ts-expect-error
 const parse = (x: string) => x[0].match(prNumWithinMsgRe)[1];
-// const prNum = pipe(head, parse, parseInt)(mockBkMsg);
-const prNum = pipe(head, parse, parseInt)(`${process.env[parseTarget]}`);
-
-console.log(`\n### prNum: \n  ${prNum}`);
-
-// curl https://api.github.com/repos/elastic/kibana/issues/156373
-// curl https://api.github.com/repos/elastic/kibana/issues/156373 | jq '.labels[].name'
-// (async () => {
-//   try {
-//     const { stdout } = await execa(
-//       'curl',
-//       [`https://api.github.com/repos/elastic/kibana/issues/${prNum}`],
-//       { cwd: REPO_ROOT }
-//     );
-
-//     console.log(`\n### stdout: \n  ${stdout}`);
-//   } catch (e) {
-//     console.log('\n### Whoops, something happenned:');
-//     console.log(`\n### Error (stringified): \n${JSON.stringify(e, null, 2)}`);
-//   }
-// })();
-const fetchLabels = (x: string) =>
-  execSync(`curl -s https://api.github.com/repos/elastic/kibana/issues/${x} | jq '.labels[].name'`)
+const fetchLabels = (prNumber: string | number) =>
+  execSync(
+    `curl -s https://api.github.com/repos/elastic/kibana/issues/${prNumber} | jq '.labels[].name'`
+  )
     .toString()
     .trim()
     .split('\n')
-    .join('');
+    .map((x: string) => x.replaceAll('"', ''));
+
+const head = (x: string): string[] => x.split('\n');
+const prNumWithinMsgRe = /\(\#(\d+)\)/;
+
 try {
-  const labels = fetchLabels(prNum);
-  console.log(`\n### labels from execSync w/ toString(): \n  ${labels}`);
+  const labels = pipe(head, parse, parseInt, fetchLabels)(`${process.env[parseTarget]}`);
   execSync(`buildkite-agent meta-data set gh_labels ${labels}`);
-  console.log('\n### Labels from GET:');
+  console.log('\n--- Labels from GET:');
   execSync(`buildkite-agent meta-data get gh_labels`);
 } catch (e) {
   console.log('\n### Whoops, something happenned, prolly with jq:');
   console.log(`\n### Error (stringified): \n${JSON.stringify(e, null, 2)}`);
 }
-// try {
-//   execSync(`curl https://api.github.com/repos/elastic/kibana/issues/${prNum}`, {
-//     stdio: ['ignore', 'inherit', 'inherit'],
-//   });
-// } catch (e) {
-//   console.log('\n### Whoops, something happenned:');
-//   console.log(`\n### Error (stringified): \n${JSON.stringify(e, null, 2)}`);
-// }
+
 export {};
+
 function pipe(...fns: any[]) {
   return fns.reduce(
     (f, g) =>
