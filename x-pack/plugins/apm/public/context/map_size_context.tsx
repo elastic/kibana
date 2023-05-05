e0cd017c49d018e0bb3325f87a5302bbf8d8274e
Nathan L Smith <smith@nlsmith.com>
2023-05-05 14:16:56 -0500

    Make a bunch of stuff work, break some other stuff

--- x-pack/plugins/apm/public/context/map_size_context.tsx
+++
@@ -1,6 +0,0 @@
-import { Dispatch, SetStateAction, createContext } from 'react';
-
-export const MapSizeContext = createContext<{
-  mapSize: 'big' | 'small';
-  setMapSize: Dispatch<SetStateAction<'big' | 'small'>>;
-}>({ mapSize: 'small', setMapSize: () => {} });
