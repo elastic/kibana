cd24cb71a3eb7bcd815e91a10ca1a146bb2e226e
Ricky Ang <rickyangwyn@gmail.com>
2023-05-05 10:45:38 -0700

    clean up

--- x-pack/plugins/cloud_security_posture/public/components/no_vulnerabilities_states.tsx
+++ x-pack/plugins/cloud_security_posture/public/components/no_vulnerabilities_states.tsx
@@ -11,7 +11,6 @@ import {
   EuiEmptyPrompt,
   EuiIcon,
   EuiMarkdownFormat,
-  EuiLink,
   EuiButton,
   EuiButtonEmpty,
   EuiFlexGroup,
@@ -110,40 +109,6 @@ const VulnerabilitiesFindingsInstalledEmptyPrompt = ({
     />
   );
 };
-
-const IndexTimeout = () => (
-  <EuiEmptyPrompt
-    data-test-subj={NO_VULNERABILITIES_STATUS_TEST_SUBJ.INDEX_TIMEOUT}
-    color="plain"
-    icon={<EuiLoadingLogo logo="logoSecurity" size="xl" />}
-    title={
-      <h2>
-        <FormattedMessage
-          id="xpack.csp.noVulnerabilitiesStates.indexTimeout.indexTimeoutTitle"
-          defaultMessage="Findings Delayed"
-        />
-      </h2>
-    }
-    body={
-      <p>
-        <FormattedMessage
-          id="xpack.csp.noVulnerabilitiesStates.indexTimeout.indexTimeoutDescription"
-          defaultMessage="Data should appear in less than 10 minutes after elastic-agent is successfully deployed. {docs}"
-          values={{
-            docs: (
-              <EuiLink href="https://ela.st/findings" target="_blank">
-                <FormattedMessage
-                  id="xpack.csp.noVulnerabilitiesStates.indexTimeout.indexTimeoutDocLink"
-                  defaultMessage="Learn more"
-                />
-              </EuiLink>
-            ),
-          }}
-        />
-      </p>
-    }
-  />
-);
 
 const Unprivileged = ({ unprivilegedIndices }: { unprivilegedIndices: string[] }) => (
   <EuiEmptyPrompt
