a4d6648e89b86806f0e400483573c76d4b15d194
klacabane <kevin.lacabane@elastic.co>
2023-05-05 13:43:12 +0200

    add signal indices

--- x-pack/plugins/asset_manager/server/constants.ts
+++ x-pack/plugins/asset_manager/server/constants.ts
@@ -7,3 +7,7 @@
 
 export const ASSETS_INDEX_PREFIX = 'assets';
 export const ASSET_MANAGER_API_BASE = '/api/asset-manager';
+
+export const LOGS_INDICES = 'logs-*,filebeat-*';
+export const APM_INDICES = 'traces-*,apm*,metrics-apm*,logs-apm*';
+export const METRICS_INDICES = 'metrics-*,metricbeat-*';
