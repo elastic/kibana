83f9e0dd7ccc9a19f5d87683f894da3080d66361
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-08 13:57:50 +0100

    intall stuff so I can use the new options.

--- .buildkite/scripts/steps/nodejs_std_test_runner/custom_reporter.sh
+++ .buildkite/scripts/steps/nodejs_std_test_runner/custom_reporter.sh
@@ -10,9 +10,18 @@ is_test_execution_step
 
 
 
+echo '--- Install Stuff'
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v$INSTALL_NVM_VER/install.sh | bash
+# Make nvm command available to terminal
+source ~/.nvm/nvm.sh
+nvm install 20.0.0
+
+echo '--- Verify Install'
+node --version
+npm version
 
 echo '--- New NodeJS Std Test Runner with Custom Reporter'
-nvm install 20.0.0
 node --test \
   packages/kbn-test/new_test_runner \
   --test-reporter=packages/kbn-test/new_test_runner/lifecycle_stream.mjs
