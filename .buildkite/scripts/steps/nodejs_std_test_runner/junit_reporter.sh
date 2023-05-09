aa940f35fb5a70442122fbd6b91f974164f54b2b
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-09 11:12:39 +0100

    I think I got it, maybe.

--- .buildkite/scripts/steps/nodejs_std_test_runner/junit_reporter.sh
+++ .buildkite/scripts/steps/nodejs_std_test_runner/junit_reporter.sh
@@ -2,25 +2,25 @@
 
 set -euo pipefail
 
-source .buildkite/scripts/common/util.sh
+#source .buildkite/scripts/common/util.sh
+#
+#is_test_execution_step
+#
+#.buildkite/scripts/bootstrap.sh
 
-is_test_execution_step
 
-.buildkite/scripts/bootstrap.sh
-
-
-
-echo '--- Install Stuff'
-# Install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.2/install.sh | bash
-# Make nvm command available to terminal
-source ~/.nvm/nvm.sh
-# Install nodejs v20
-nvm install 20.0.0
-
-echo '--- Verify Install'
-node --version
-npm --version
+#
+#echo '--- Install Stuff'
+## Install nvm
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.2/install.sh | bash
+## Make nvm command available to terminal
+#source ~/.nvm/nvm.sh
+## Install nodejs v20
+#nvm install 20.0.0
+#
+#echo '--- Verify Install'
+#node --version
+#npm --version
 
 # echo '--- New NodeJS Std Test Runner with Custom Reporter [Transform Stream]'
 # pushd packages/kbn-test/new_test_runner
@@ -31,17 +31,17 @@ echo '--- New NodeJS Std Test Runner using Tap-Junit reporter'
 pushd packages/kbn-test/new_test_runner
 # echo "### pwd: $(pwd)"
 tgt="../../../target/junit"
-shutdown() {
-  exit_code=$?
-  echo "### exit_code: ${exit_code}"
-
-  echo "### tgt: ${tgt}"
-  echo "### Contents of ${tgt}:"
-  ls -la "$tgt"
-
-  # exit $exit_code
-}
-trap "shutdown" EXIT
+#shutdown() {
+#  exit_code=$?
+#  echo "### exit_code: ${exit_code}"
+#
+#  echo "### tgt: ${tgt}"
+#  echo "### Contents of ${tgt}:"
+#  ls -la "$tgt"
+#
+#  # exit $exit_code
+#}
+#trap "shutdown" EXIT
 node --test-reporter tap --test ./ | ../../../node_modules/tap-junit/bin/tap-junit --output "$tgt"
 ls -R "$tgt"
 popd
