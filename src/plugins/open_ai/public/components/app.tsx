6a365f6e2551518121a4587167d1781b7483cd8b
Davis McPhee <davis.mcphee@elastic.co>
2023-05-08 21:57:03 -0300

    Add ChatGPT UI

--- src/plugins/open_ai/public/components/app.tsx
+++ src/plugins/open_ai/public/components/app.tsx
@@ -6,28 +6,22 @@
  * Side Public License, v 1.
  */
 
-import React, { useState } from 'react';
-import { i18n } from '@kbn/i18n';
-import { FormattedMessage, I18nProvider } from '@kbn/i18n-react';
+import React, { useRef, useState, ReactNode } from 'react';
+import { I18nProvider } from '@kbn/i18n-react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import {
+  EuiAvatar,
   EuiButton,
-  EuiHorizontalRule,
-  EuiPage,
-  EuiPageBody,
-  EuiPageContent_Deprecated as EuiPageContent,
-  EuiPageContentBody_Deprecated as EuiPageContentBody,
-  EuiPageContentHeader_Deprecated as EuiPageContentHeader,
-  EuiPageHeader,
-  EuiTitle,
-  EuiText,
+  EuiComment,
+  EuiCommentList,
+  EuiFieldText,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiMarkdownFormat,
+  EuiPageTemplate,
 } from '@elastic/eui';
-
 import { CoreStart } from '@kbn/core/public';
 import { NavigationPublicPluginStart } from '@kbn/navigation-plugin/public';
-
-import { PLUGIN_ID, PLUGIN_NAME } from '../../common';
 
 interface OpenAiAppDeps {
   basename: string;
@@ -36,83 +30,148 @@ interface OpenAiAppDeps {
   navigation: NavigationPublicPluginStart;
 }
 
-export const OpenAiApp = ({ basename, notifications, http, navigation }: OpenAiAppDeps) => {
-  // Use React hooks to manage state.
-  const [timestamp, setTimestamp] = useState<string | undefined>();
+interface ChatMessage {
+  username: string;
+  message: string;
+  timestamp: string;
+  avatar: ReactNode;
+}
 
-  const onClickHandler = () => {
-    // Use the core http service to make a response to the server API.
-    http.get('/api/open_ai/example').then((res) => {
-      setTimestamp(res.time);
-      // Use the core notifications service to display a success message.
-      notifications.toasts.addSuccess(
-        i18n.translate('openAi.dataUpdated', {
-          defaultMessage: 'Data updated',
-        })
-      );
+const formatDate = (date: Date) => date.toLocaleTimeString();
+const kibanaUsername = 'Kibana';
+const kibanaAvatar = (
+  <EuiAvatar name={kibanaUsername} size="l" iconType="logoKibana" iconSize="xl" color="plain" />
+);
+
+export const OpenAiApp = ({ basename, notifications, http, navigation }: OpenAiAppDeps) => {
+  const chatRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState(false);
+  const [question, setQuestion] = useState('');
+  const [messages, setMessages] = useState<ChatMessage[]>([
+    {
+      username: kibanaUsername,
+      message:
+        "Hi 👋 I'm a ChatGPT powered assistant who can help answer questions about Kibana by searching the documentation. Ask me a question about Kibana to get started!",
+      timestamp: formatDate(new Date()),
+      avatar: kibanaAvatar,
+    },
+  ]);
+
+  const scrollToBottom = () =>
+    setTimeout(() => chatRef.current?.scrollTo(0, chatRef.current.scrollHeight));
+
+  const askQuestion = async () => {
+    const trimmedQuestion = question.trim();
+
+    if (!trimmedQuestion) {
+      return;
+    }
+
+    const newMessages = [
+      ...messages,
+      {
+        username: 'You',
+        message: trimmedQuestion,
+        timestamp: formatDate(new Date()),
+        avatar: <EuiAvatar name="You" size="l" />,
+      },
+    ];
+
+    setQuestion('');
+    setLoading(true);
+    setMessages([
+      ...newMessages,
+      {
+        username: kibanaUsername,
+        message: '...',
+        timestamp: ' ',
+        avatar: kibanaAvatar,
+      },
+    ]);
+    scrollToBottom();
+
+    const response = await http.get<{
+      answer: string;
+      references: Array<{ title: string; url: string }>;
+    }>('/internal/open_ai/kibana_docs', {
+      query: { query: trimmedQuestion },
     });
+
+    const messageParts = [response.answer];
+
+    if (response.references.length) {
+      const references = response.references
+        .map(({ title, url }) => `* [${title}](${url})`)
+        .join('\n');
+
+      messageParts.push(`##### References:\n${references}`);
+    }
+
+    setLoading(false);
+    setMessages([
+      ...newMessages,
+      {
+        username: kibanaUsername,
+        message: messageParts.join('\n\n'),
+        timestamp: formatDate(new Date()),
+        avatar: kibanaAvatar,
+      },
+    ]);
+    scrollToBottom();
   };
 
-  // Render the application DOM.
-  // Note that `navigation.ui.TopNavMenu` is a stateful component exported on the `navigation` plugin's start contract.
   return (
     <Router basename={basename}>
       <I18nProvider>
-        <>
-          <navigation.ui.TopNavMenu
-            appName={PLUGIN_ID}
-            showSearchBar={true}
-            useDefaultBehaviors={true}
-          />
-          <EuiPage restrictWidth="1000px">
-            <EuiPageBody>
-              <EuiPageHeader>
-                <EuiTitle size="l">
-                  <h1>
-                    <FormattedMessage
-                      id="openAi.helloWorldText"
-                      defaultMessage="{name}"
-                      values={{ name: PLUGIN_NAME }}
+        <EuiPageTemplate
+          style={{
+            paddingBlockStart: 0,
+            minBlockSize: 'unset',
+            height: 'calc(100vh - 96px)',
+          }}
+        >
+          <EuiPageTemplate.Header pageTitle="Kibana Docs GPT" />
+          <EuiPageTemplate.Section
+            style={{ overflow: 'hidden' }}
+            contentProps={{ style: { height: '100%' } }}
+          >
+            <EuiFlexGroup direction="column" responsive={false} style={{ height: '100%' }}>
+              <EuiFlexItem grow={1} style={{ overflow: 'hidden' }}>
+                <div ref={chatRef} style={{ overflow: 'auto' }}>
+                  <EuiCommentList>
+                    {messages.map((message, index) => (
+                      <EuiComment
+                        key={index}
+                        username={message.username}
+                        timelineAvatar={message.avatar}
+                        timestamp={message.timestamp}
+                      >
+                        <EuiMarkdownFormat>{message.message}</EuiMarkdownFormat>
+                      </EuiComment>
+                    ))}
+                  </EuiCommentList>
+                </div>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiFlexGroup responsive={false}>
+                  <EuiFlexItem>
+                    <EuiFieldText
+                      placeholder="Ask Kibana a question"
+                      value={question}
+                      onChange={(e) => setQuestion(e.target.value)}
+                      fullWidth={true}
                     />
-                  </h1>
-                </EuiTitle>
-              </EuiPageHeader>
-              <EuiPageContent>
-                <EuiPageContentHeader>
-                  <EuiTitle>
-                    <h2>
-                      <FormattedMessage
-                        id="openAi.congratulationsTitle"
-                        defaultMessage="Congratulations, you have successfully created a new Kibana Plugin!"
-                      />
-                    </h2>
-                  </EuiTitle>
-                </EuiPageContentHeader>
-                <EuiPageContentBody>
-                  <EuiText>
-                    <p>
-                      <FormattedMessage
-                        id="openAi.content"
-                        defaultMessage="Look through the generated code and check out the plugin development documentation."
-                      />
-                    </p>
-                    <EuiHorizontalRule />
-                    <p>
-                      <FormattedMessage
-                        id="openAi.timestampText"
-                        defaultMessage="Last timestamp: {time}"
-                        values={{ time: timestamp ? timestamp : 'Unknown' }}
-                      />
-                    </p>
-                    <EuiButton type="primary" size="s" onClick={onClickHandler}>
-                      <FormattedMessage id="openAi.buttonText" defaultMessage="Get data" />
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    <EuiButton isLoading={loading} onClick={askQuestion}>
+                      Send
                     </EuiButton>
-                  </EuiText>
-                </EuiPageContentBody>
-              </EuiPageContent>
-            </EuiPageBody>
-          </EuiPage>
-        </>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPageTemplate.Section>
+        </EuiPageTemplate>
       </I18nProvider>
     </Router>
   );
