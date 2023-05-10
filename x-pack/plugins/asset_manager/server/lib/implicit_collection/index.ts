79df2b6c5d2058eb53d36355bed0f5f7466bd72c
klacabane <kevin.lacabane@elastic.co>
2023-05-10 13:35:44 +0200

    tests

--- x-pack/plugins/asset_manager/server/lib/implicit_collection/index.ts
+++ x-pack/plugins/asset_manager/server/lib/implicit_collection/index.ts
@@ -6,16 +6,10 @@
  */
 import { ElasticsearchClient, Logger } from '@kbn/core/server';
 
-import {
-  Collector,
-  collectContainers,
-  collectHosts,
-  collectPods,
-  collectServices,
-} from './collectors';
-import { Asset } from '../../../common/types_api';
+import { collectContainers, collectHosts, collectPods, collectServices } from './collectors';
+import { CollectorRunner } from './collector_runner';
 
-interface ImplicitCollectionOptions {
+export interface ImplicitCollectionOptions {
   inputClient: ElasticsearchClient;
   outputClient: ElasticsearchClient;
   intervalMs: number;
@@ -39,57 +33,4 @@ export function startImplicitCollection(options: ImplicitCollectionOptions): () 
     options.logger.debug('Stopping periodic collection');
     clearInterval(timer);
   };
-}
-
-class CollectorRunner {
-  private collectors: Array<{ name: string; collector: Collector }> = [];
-
-  constructor(private options: ImplicitCollectionOptions) {}
-
-  registerCollector(name: string, collector: Collector) {
-    this.collectors.push({ name, collector });
-  }
-
-  async run() {
-    const collectorOptions = {
-      client: this.options.inputClient,
-      from: Date.now() - this.options.intervalMs,
-    };
-
-    for (let i = 0; i < this.collectors.length; i++) {
-      const { name, collector } = this.collectors[i];
-      this.options.logger.info(`Collector '${name}' started`);
-
-      const assets = await collector(collectorOptions)
-        .then((collectedAssets) => {
-          this.options.logger.info(`Collector '${name}' found ${collectedAssets.length} assets`);
-          return collectedAssets;
-        })
-        .catch((err) => {
-          this.options.logger.error(`Collector '${name}' execution failure: ${err}`);
-          return [];
-        });
-
-      if (assets.length) {
-        const bulkBody = assets.flatMap((asset: Asset) => {
-          return [{ create: { _index: `assets-${asset['asset.kind']}-default` } }, asset];
-        });
-
-        await this.options.outputClient
-          .bulk({ body: bulkBody })
-          .then((res) => {
-            if (res.errors) {
-              this.options.logger.error(
-                `Failure writing assets documents from collector '${name}': ${JSON.stringify(res)}`
-              );
-            }
-          })
-          .catch((err) => {
-            this.options.logger.error(
-              `Failure writing assets documents from collector '${name}': ${err}`
-            );
-          });
-      }
-    }
-  }
 }
