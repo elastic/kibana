f597d56207f27b10a18a5af657b2782f203c5a36
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-10 12:46:44 +0100

    Not sure what's going on, mo' debug.

--- .buildkite/scripts/steps/nodejs_std_test_runner/junit_reporter.sh
+++ .buildkite/scripts/steps/nodejs_std_test_runner/junit_reporter.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
 
-#source .buildkite/scripts/common/util.sh
-#
-#is_test_execution_step
-#
-.buildkite/scripts/bootstrap.sh
-
+boot() {
+  .buildkite/scripts/bootstrap.sh
+}
 install() {
+  echo '--- Install Stuff'
   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.2/install.sh | bash
   source ~/.nvm/nvm.sh
   nvm install v20.0.0
@@ -17,36 +14,40 @@ install() {
   node --version
   npm --version
 }
-echo '--- Install Stuff'
-install
+TGT=${TGT:-../../../target/junit}
+shutdown() {
+  exit_code=$?
+  echo "### Runner - Exit Code: ${exit_code}"
 
-# echo '--- New NodeJS Std Test Runner with Custom Reporter [Transform Stream]'
-# pushd packages/kbn-test/new_test_runner
-# node --test-reporter=./lifecycle_stream.mjs --test
-# popd
+  echo "### TGT: ${TGT}"
+  echo "### Contents of ${TGT}:"
+  ls -la "$TGT"
 
-junitReport() {
-  nvm use v20.0.0
-  pushd packages/kbn-test/new_test_runner
-  tgt="../../../target/junit"
-  node --test-reporter tap --test ./ | ../../../node_modules/tap-junit/bin/tap-junit --output "$tgt"
-  ls -R "$tgt"
-  popd
+  exit $exit_code
+}
+testAndReport() {
+  trap "shutdown" EXIT
+
+  echo '--- New NodeJS Std Test Runner using Tap-Junit reporter'
+  pushd packages/kbn-test/new_test_runner > /dev/null
+
+  node --test-reporter tap --test ./ | ../../../node_modules/tap-junit/bin/tap-junit --output "$TGT"
+  popd > /dev/null
+
   echo '--- Junit Xml to JSON'
   npx junit2json target/junit/tap.xml | jq
 }
-echo '--- New NodeJS Std Test Runner using Tap-Junit reporter'
-junitReport
 
-#shutdown() {
-#  exit_code=$?
-#  echo "### exit_code: ${exit_code}"
-#
-#  echo "### tgt: ${tgt}"
-#  echo "### Contents of ${tgt}:"
-#  ls -la "$tgt"
-#
-#  # exit $exit_code
-#}
-#trap "shutdown" EXIT
+while getopts i:r: flag; do
+  case "${flag}" in
+  i) install=${OPTARG} ;;
+  r) report=${OPTARG} ;;
+  *) ;;
+  esac
+done
+echo "### install: ${install}"
+echo "### report: ${report}"
+
+if [ "${install}" == "true" ]; then boot; fi
+if [ "${report}" == "true" ]; then testAndReport; fi
 
--- test_runner_junit.sh
+++ test_runner_junit.sh
@@ -2,21 +2,5 @@
 
 set -euo pipefail
 
-echo '--- New NodeJS Std Test Runner using Tap-Junit reporter'
-pushd packages/kbn-test/new_test_runner > /dev/null
-tgt="../../../target/junit"
-# shutdown() {
-#   exit_code=$?
-
-#   echo "### tgt: ${tgt}"
-#   echo "### Contents of ${tgt}:"
-#   ls -la "$tgt"
-
-#   exit $exit_code
-# }
-# trap "shutdown" EXIT
-node --test-reporter tap --test ./ | ../../../node_modules/tap-junit/bin/tap-junit --output "$tgt"
-#npx junit2json target/junit/tap.xml | jq
-ls -R "$tgt"
-popd > /dev/null
-npx junit2json target/junit/tap.xml | jq
+echo "--- Install, Test and Report (junit)"
+.buildkite/scripts/steps/nodejs_std_test_runner/junit_reporter.sh -i true -r true
