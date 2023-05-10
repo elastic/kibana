2c345235e65f690b43de6705939d8cdef52acd04
Paul Tavares <paul.tavares@elastic.co>
2023-05-10 10:40:46 -0400

    simplified implemenation of console's `truthy` validator

--- x-pack/plugins/security_solution/public/management/components/console/components/console_state/state_update_handlers/handle_execute_command.tsx
+++ x-pack/plugins/security_solution/public/management/components/console/components/console_state/state_update_handlers/handle_execute_command.tsx
@@ -344,7 +344,7 @@ export const handleExecuteCommand: ConsoleStoreReducer<
                 break;
 
               case 'truthy':
-                if (typeof argValue !== 'number' && !argValue) {
+                if (!argValue) {
                   dataValidationError = executionTranslations.mustHaveValue(argName);
                 }
                 break;
--- x-pack/plugins/security_solution/public/management/components/console/types.ts
+++ x-pack/plugins/security_solution/public/management/components/console/types.ts
@@ -46,8 +46,7 @@ export interface CommandArgDefinition {
    *   the value entered will first be `trim()`'d.
    * - `number`: user's value will be converted to a Number and ensured to be a `safe integer`
    * - `number-greater-than-zero`: user's value must be a number greater than zero
-   * - `truthy`: The argument must have a value and the values must be "truthy"
-   *   (no `null` or `undefined`). Note that `0` (zero) will be considered `truthy`
+   * - `truthy`: The argument must have a value and the values must be "truthy" (evaluate to `Boolean` true)
    */
   mustHaveValue?: boolean | 'non-empty-string' | 'number' | 'number-greater-than-zero' | 'truthy';
   exclusiveOr?: boolean;
