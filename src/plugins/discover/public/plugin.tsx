c3c8b1f9a8bf9d44284b3746d62707931aead75b
semd <sergi.massaneda@elastic.co>
2023-05-11 13:49:46 +0200

    fix weird dependencies problem

--- src/plugins/discover/public/plugin.tsx
+++ src/plugins/discover/public/plugin.tsx
@@ -71,6 +71,7 @@ import {
   DiscoverSingleDocLocatorDefinition,
 } from './application/doc/locator';
 import { DiscoverAppLocator, DiscoverAppLocatorDefinition } from '../common';
+import { SEARCH_EMBEDDABLE_CELL_ACTIONS_TRIGGER } from './embeddable/constants';
 
 const DocViewerLegacyTable = React.lazy(
   () => import('./services/doc_views/components/doc_viewer_table/legacy')
@@ -382,6 +383,8 @@ export class DiscoverPlugin
     // when the application/embeddable is mounted
 
     const { uiActions } = plugins;
+
+    uiActions.registerTrigger(SEARCH_EMBEDDABLE_CELL_ACTIONS_TRIGGER);
 
     const viewSavedSearchAction = new ViewSavedSearchAction(core.application);
     uiActions.addTriggerAction('CONTEXT_MENU_TRIGGER', viewSavedSearchAction);
