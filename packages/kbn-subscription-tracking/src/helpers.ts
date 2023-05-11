13e7b28ec0dae7a5a4dbe09a97f11bbba22b300d
Jan Monschke <jan.monschke@elastic.co>
2023-05-11 14:56:13 +0200

    initialize package @kbn/subscription-tracking

---
+++ packages/kbn-subscription-tracking/src/helpers.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import type { SubscriptionContext } from '../types';
+
+const sourceStringRegEx = /[\w-_]+__[\w-_]+/;
+export function isValidContext(context: SubscriptionContext): boolean {
+  return context.feature.length > 0 && sourceStringRegEx.test(context.source);
+}
