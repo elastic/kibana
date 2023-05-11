70f81d290a198f0718faa272f48637a80f5b1016
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2023-05-11 12:41:26 -0300

    revert: Set allowLeadingWildcards: false in query validation

--- x-pack/plugins/infra/public/pages/metrics/hosts/hooks/use_unified_search.ts
+++ x-pack/plugins/infra/public/pages/metrics/hosts/hooks/use_unified_search.ts
@@ -12,6 +12,7 @@ import { map, skip, startWith } from 'rxjs/operators';
 import { combineLatest } from 'rxjs';
 import deepEqual from 'fast-deep-equal';
 import useEffectOnce from 'react-use/lib/useEffectOnce';
+import { useKibanaQuerySettings } from '../../../../utils/use_kibana_query_settings';
 import { useKibanaContextForPlugin } from '../../../../hooks/use_kibana';
 import { telemetryTimeRangeFormatter } from '../../../../../common/formatters/telemetry_time_range';
 import { useMetricsDataViewContext } from './use_data_view';
@@ -51,6 +52,7 @@ export const useUnifiedSearch = () => {
   const [searchCriteria, setSearch] = useHostsUrlState();
   const { dataView } = useMetricsDataViewContext();
   const { services } = useKibanaContextForPlugin();
+  const kibanaQuerySettings = useKibanaQuerySettings();
 
   const {
     data: {
@@ -63,10 +65,12 @@ export const useUnifiedSearch = () => {
     telemetry,
   } = services;
 
-  const validateQuery = (query: Query) => {
-    // allowLeadingWildcards: false is the default settings in `buildEsQuery`. Set to false for compatibility with the Control component
-    fromKueryExpression(query.query, { allowLeadingWildcards: false });
-  };
+  const validateQuery = useCallback(
+    (query: Query) => {
+      fromKueryExpression(query.query, kibanaQuerySettings);
+    },
+    [kibanaQuerySettings]
+  );
 
   const onSubmit = useCallback(
     (params?: HostsSearchPayload) => {
@@ -90,7 +94,7 @@ export const useUnifiedSearch = () => {
         setError(err);
       }
     },
-    [queryStringService, setSearch]
+    [queryStringService, setSearch, validateQuery]
   );
 
   const getParsedDateRange = useCallback(() => {
@@ -113,11 +117,19 @@ export const useUnifiedSearch = () => {
   }, [getParsedDateRange]);
 
   const buildQuery = useCallback(() => {
-    return buildEsQuery(dataView, searchCriteria.query, [
-      ...searchCriteria.filters,
-      ...searchCriteria.panelFilters,
-    ]);
-  }, [dataView, searchCriteria.query, searchCriteria.filters, searchCriteria.panelFilters]);
+    return buildEsQuery(
+      dataView,
+      searchCriteria.query,
+      [...searchCriteria.filters, ...searchCriteria.panelFilters],
+      kibanaQuerySettings
+    );
+  }, [
+    dataView,
+    searchCriteria.query,
+    searchCriteria.filters,
+    searchCriteria.panelFilters,
+    kibanaQuerySettings,
+  ]);
 
   useEffectOnce(() => {
     // Sync filtersService from the URL state
