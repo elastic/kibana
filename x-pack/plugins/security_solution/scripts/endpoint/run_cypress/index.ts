f6c0c5c412fecac008dc2a8321cbd4c5fbe8a20a
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-05-15 01:19:54 +0200

    WIP

--- x-pack/plugins/security_solution/scripts/endpoint/run_cypress/index.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/run_cypress/index.ts
@@ -7,18 +7,13 @@
 
 /* eslint-disable import/no-default-export */
 
-import Url from 'url';
-import { EsArchiver } from '@kbn/es-archiver';
-import { omit } from 'lodash';
 import yargs from 'yargs';
 import cypress from 'cypress';
 import deepMerge from 'deepmerge';
 import { ToolingLog } from '@kbn/tooling-log';
 import pMap from 'p-map';
-import { runElasticsearch, runKibanaServer, KbnClient, readConfigFile, EsVersion } from '@kbn/test';
-import { createTestServers } from '@kbn/core-test-helpers-kbn-server/src/create_root';
-import { extendEsArchiver } from '@kbn/ftr-common-functional-services/services/kibana_server';
-import { withProcRunner } from '@kbn/dev-proc-runner';
+import { runElasticsearch, runKibanaServer, readConfigFile, EsVersion } from '@kbn/test';
+import { ProcRunner } from '@kbn/dev-proc-runner';
 import {
   Lifecycle,
   ProviderCollection,
@@ -91,9 +86,7 @@ export default async (
 
   console.error('ftrConfig', ftrConfig, ftrConfig.get('servers'), ftrConfig.get('kbnTestServer'));
 
-  console.error('dupa', ftrConfig.get('esArchiver.archives'));
-
-  // console.log('esarchied', await ftrConfig.module.provider, ftrConfig.get('services.esArchiver')());
+  console.error('esArchiver.archives', ftrConfig.get('esArchiver.archives'));
 
   const lifecycle = new Lifecycle(log);
   const providers = new ProviderCollection(log, [
@@ -101,7 +94,6 @@ export default async (
       lifecycle: () => lifecycle,
       log: () => log,
       config: () => ftrConfig,
-      // esVersion: () => this.esVersion,
     }),
     ...readProviderSpec('Service', ftrConfig.get('services')),
   ]);
@@ -123,70 +115,76 @@ export default async (
     esFrom: 'snapshot',
   });
 
-  await withProcRunner(log, async (procs) => {
-    await runKibanaServer({
-      procs,
-      config: ftrConfig,
-      installDir: options?.installDir,
-      extraKbnOpts: options?.installDir ? [] : ['--dev', '--no-dev-config', '--no-dev-credentials'],
-    }).then(async () => {
-      await providers.loadAll();
+  const procs = new ProcRunner(log);
 
-      if (ftrConfig.get('esArchiver.archives')?.length) {
-        const esArchiver = await providers.getService('esArchiver');
-        await pMap(ftrConfig.get('esArchiver.archives'), (path) => esArchiver.load(path), {
-          concurrency: 1,
-        });
-      }
-
-      const commonCypressConfig = {
-        env: {
-          KIBANA_URL: `http://${HOSTNAME}:${KIBANA_PORT}`,
-          ELASTICSEARCH_USERNAME: 'elastic',
-          ELASTICSEARCH_PASSWORD: 'changeme',
-          ELASTICSEARCH_URL: `http://${HOSTNAME}:${ES_PORT}`,
-          configport: KIBANA_PORT,
-          hostname: HOSTNAME,
-        },
-        config: {
-          e2e: {
-            baseUrl: `http://${HOSTNAME}:${KIBANA_PORT}`,
-            ...(singleSpecPath ? { specPattern: `**/${singleSpecPath}` } : {}),
-          },
-        },
-      };
-
-      const ftrConfigIndex = argv?.indexOf('--ftr-config-file');
-      if (ftrConfigIndex !== -1) {
-        argv.splice(ftrConfigIndex, 2);
-      }
-
-      if (yargs.parse(argv)._.includes('open')) {
-        return cypress.open({
-          configFile: require.resolve(`../../../${yargsData.argv.configFile}`),
-          ...commonCypressConfig,
-        });
-      }
-
-      const runOptions = await cypress.cli.parseRunArguments(argv);
-      console.log('runOptions', runOptions);
-      return cypress.run(
-        deepMerge.all([
-          runOptions,
-          commonCypressConfig,
-          {
-            numTestsKeptInMemory: 0,
-          },
-        ])
-      );
-      // .then((results) => {
-      //   process.exit();
-      // })
-      // .catch((e) => {
-      //   process.exit();
-      // });
-    });
+  await runKibanaServer({
+    procs,
+    config: ftrConfig,
+    installDir: options?.installDir,
+    extraKbnOpts: options?.installDir
+      ? []
+      : [
+          '--dev',
+          '--no-dev-config',
+          '--no-dev-credentials',
+          `--server.port=${kibanaPort}`,
+          `--elasticsearch.hosts=http://localhost:${esPort}`,
+        ],
   });
 
-  console.log('shutdownEs', shutdownEs);
+  await providers.loadAll();
+
+  if (ftrConfig.get('esArchiver.archives')?.length) {
+    const esArchiver = await providers.getService('esArchiver');
+    await pMap(ftrConfig.get('esArchiver.archives'), (path) => esArchiver.load(path), {
+      concurrency: 1,
+    });
+  }
+
+  const commonCypressConfig = {
+    env: {
+      KIBANA_URL: `http://${HOSTNAME}:${KIBANA_PORT}`,
+      ELASTICSEARCH_USERNAME: 'elastic',
+      ELASTICSEARCH_PASSWORD: 'changeme',
+      ELASTICSEARCH_URL: `http://${HOSTNAME}:${ES_PORT}`,
+      configport: KIBANA_PORT,
+      hostname: HOSTNAME,
+    },
+    config: {
+      e2e: {
+        baseUrl: `http://${HOSTNAME}:${KIBANA_PORT}`,
+        ...(singleSpecPath ? { specPattern: `**/${singleSpecPath}` } : {}),
+      },
+    },
+  };
+
+  const ftrConfigIndex = argv?.indexOf('--ftr-config-file');
+  if (ftrConfigIndex !== -1) {
+    argv.splice(ftrConfigIndex, 2);
+  }
+
+  if (yargs.parse(argv)._.includes('open')) {
+    return cypress.open({
+      configFile: require.resolve(`../../../${yargsData.argv.configFile}`),
+      ...commonCypressConfig,
+    });
+  }
+
+  const runOptions = await cypress.cli.parseRunArguments(argv);
+  console.log('runOptions', runOptions);
+  return cypress.run(
+    deepMerge.all([
+      runOptions,
+      commonCypressConfig,
+      {
+        numTestsKeptInMemory: 0,
+      },
+    ])
+  );
+  // .then((results) => {
+  //   process.exit();
+  // })
+  // .catch((e) => {
+  //   process.exit();
+  // });
 };
