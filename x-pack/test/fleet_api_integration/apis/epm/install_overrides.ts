7ac30d3a63c3e2372f38da9863c251a1cac7499a
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2023-05-15 16:31:55 +0100

    Update x-pack/test/fleet_api_integration/apis/epm/install_overrides.ts

--- x-pack/test/fleet_api_integration/apis/epm/install_overrides.ts
+++ x-pack/test/fleet_api_integration/apis/epm/install_overrides.ts
@@ -149,7 +149,7 @@ export default function (providerContext: FtrProviderContext) {
             dynamic: 'false',
             properties: {
               '@timestamp': {
-                ignore_malformed: `false`,
+                ignore_malformed: false,
                 type: 'date',
               },
               data_stream: {
