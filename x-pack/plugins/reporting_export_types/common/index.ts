80e70b7bfe410d570a474083e79da3e8da9965b9
rshen <rshen@elastic.co>
2023-05-15 16:01:14 -0600

    remove reporting export type package for now

--- x-pack/plugins/reporting_export_types/common/index.ts
+++
@@ -1,2 +0,0 @@
-export const PLUGIN_ID = 'reportingExportTypes';
-export const PLUGIN_NAME = 'reportingExportTypes';
--- x-pack/plugins/reporting_export_types/server/types.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ReportingExportTypesPluginSetup {}
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ReportingExportTypesPluginStart {}
