77a6615135b35a9936f17298b24b18fd0c2fc80c
miriam.aparicio <miriam.aparicio@gmail.com>
2023-05-15 19:33:29 +0100

    fix jest tests

--- x-pack/plugins/apm/public/components/routing/templates/apm_service_template/index.test.tsx
+++ x-pack/plugins/apm/public/components/routing/templates/apm_service_template/index.test.tsx
@@ -45,12 +45,18 @@ describe('APM service template', () => {
   describe('isInfraTabHidden', () => {
     describe('hides infra tab', () => {
       [
-        { agentName: undefined },
-        { agentName: 'js-base' },
-        { agentName: 'rum-js' },
-        { agentName: 'opentelemetry/webjs' },
-        { serverlessType: ServerlessType.AWS_LAMBDA },
-        { serverlessType: ServerlessType.AZURE_FUNCTIONS },
+        { agentName: undefined, isInfraTabAvailable: false },
+        { agentName: 'js-base', isInfraTabAvailable: false },
+        { agentName: 'rum-js', isInfraTabAvailable: false },
+        { agentName: 'opentelemetry/webjs', isInfraTabAvailable: false },
+        {
+          serverlessType: ServerlessType.AWS_LAMBDA,
+          isInfraTabAvailable: false,
+        },
+        {
+          serverlessType: ServerlessType.AZURE_FUNCTIONS,
+          isInfraTabAvailable: false,
+        },
       ].map((input) => {
         it(`when input ${JSON.stringify(input)}`, () => {
           expect(isInfraTabHidden(input)).toBeTruthy();
@@ -59,16 +65,16 @@ describe('APM service template', () => {
     });
     describe('shows infra tab', () => {
       [
-        { agentName: 'ruby', runtimeName: 'ruby' },
-        { agentName: 'ruby', runtimeName: 'jruby' },
-        { agentName: 'ruby' },
-        { agentName: 'dotnet' },
-        { agentName: 'go' },
-        { agentName: 'nodejs' },
-        { agentName: 'php' },
-        { agentName: 'python' },
-        { agentName: 'java' },
-        { agentName: 'opentelemetry/java' },
+        { agentName: 'ruby', runtimeName: 'ruby', isInfraTabAvailable: true },
+        { agentName: 'ruby', runtimeName: 'jruby', isInfraTabAvailable: true },
+        { agentName: 'ruby', isInfraTabAvailable: true },
+        { agentName: 'dotnet', isInfraTabAvailable: true },
+        { agentName: 'go', isInfraTabAvailable: true },
+        { agentName: 'nodejs', isInfraTabAvailable: true },
+        { agentName: 'php', isInfraTabAvailable: true },
+        { agentName: 'python', isInfraTabAvailable: true },
+        { agentName: 'java', isInfraTabAvailable: true },
+        { agentName: 'opentelemetry/java', isInfraTabAvailable: true },
       ].map((input) => {
         it(`when input ${JSON.stringify(input)}`, () => {
           expect(isInfraTabHidden(input)).toBeFalsy();
