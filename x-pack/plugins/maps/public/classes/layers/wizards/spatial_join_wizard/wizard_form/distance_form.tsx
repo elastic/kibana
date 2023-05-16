eee5423a4089452c033d103abea09ef7a55ce15c
nreese <reese.nathan@elastic.co>
2023-05-16 08:54:41 -0600

    update LayerStatsCollector for distance join

--- x-pack/plugins/maps/public/classes/layers/wizards/spatial_join_wizard/wizard_form/distance_form.tsx
+++ x-pack/plugins/maps/public/classes/layers/wizards/spatial_join_wizard/wizard_form/distance_form.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { i18n } from '@kbn/i18n';
 import {
   EuiButtonEmpty,
