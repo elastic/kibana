a086cbefae83548f8a83310049eee0b48a3fbe28
Brad White <brad.white@elastic.co>
2023-05-16 09:06:18 -0600

    Fix non-distributable date, update snapshots

--- packages/kbn-config/src/env.ts
+++ packages/kbn-config/src/env.ts
@@ -123,7 +123,9 @@ export class Env {
       buildSha: isKibanaDistributable ? pkg.build.sha : 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
       version: pkg.version,
       dist: isKibanaDistributable,
-      buildDate: isKibanaDistributable ? new Date(pkg.build.date) : new Date(),
+      buildDate: isKibanaDistributable
+        ? new Date(pkg.build.date)
+        : new Date('2023-05-15T23:12:09+0000'),
     });
   }
 }
