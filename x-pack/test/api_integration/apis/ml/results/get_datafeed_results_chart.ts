25a9f6ff77ac35907d67d583bedba93e8320d794
James Gowdy <jgowdy@elastic.co>
2023-05-16 18:21:25 +0100

    fixing tests

--- x-pack/test/api_integration/apis/ml/results/get_datafeed_results_chart.ts
+++ x-pack/test/api_integration/apis/ml/results/get_datafeed_results_chart.ts
@@ -107,7 +107,7 @@ export default ({ getService }: FtrProviderContext) => {
 
       expect(body.error).to.eql('Bad Request');
       expect(body.message).to.eql(
-        '[request body.jobId]: expected value of type [string] but got [undefined]'
+        '[get 1 body.jobId]: expected value of type [string] but got [undefined]'
       );
     });
 
