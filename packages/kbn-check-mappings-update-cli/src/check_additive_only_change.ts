631f1bc27e7cc725471b1d07c2c0ce4b40ab841f
Jean-Louis Leysens <jloleysens@gmail.com>
2023-05-17 10:01:20 +0200

    rename variable

--- packages/kbn-check-mappings-update-cli/src/check_additive_only_change.ts
+++ packages/kbn-check-mappings-update-cli/src/check_additive_only_change.ts
@@ -27,14 +27,14 @@ export function checkAdditiveOnlyChange(
   next: SavedObjectsTypeMappingDefinitions
 ) {
   let checkedCount = 0;
-  const Os: Array<[path: string[], value: unknown]> = [[[], current]];
+  const pathValueTuples: Array<[path: string[], value: unknown]> = [[[], current]];
   const missingProps: string[] = [];
 
-  while (Os.length) {
-    const [path, value] = Os.shift()!;
+  while (pathValueTuples.length) {
+    const [path, value] = pathValueTuples.shift()!;
     // "Recurse" into the value if it's an object
     if (isObject(value)) {
-      Object.entries(value).forEach(([k, v]) => Os.push([[...path, k], v]));
+      Object.entries(value).forEach(([k, v]) => pathValueTuples.push([[...path, k], v]));
     }
     // If we're at a "properties" object, check that the next mappings have the same field
     if (path.length > 1 && path[path.length - 2] === 'properties') {
