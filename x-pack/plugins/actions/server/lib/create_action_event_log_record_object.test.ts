f295a3454baca99fa3f5ee3f53cc78069397f3ee
Mike Cote <michel.cote@elastic.co>
2023-05-17 10:53:06 -0400

    Fix failing jest test

--- x-pack/plugins/actions/server/lib/create_action_event_log_record_object.test.ts
+++ x-pack/plugins/actions/server/lib/create_action_event_log_record_object.test.ts
@@ -59,7 +59,7 @@ describe('createActionEventLogRecordObject', () => {
         ],
         space_ids: ['default'],
         task: {
-          schedule_delay: 0,
+          schedule_delay: '0',
           scheduled: '1970-01-01T00:00:00.000Z',
         },
         action: {
