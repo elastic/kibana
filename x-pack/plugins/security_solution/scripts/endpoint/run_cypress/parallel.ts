ce77753583f2a2915e6b3c12680da75e722dad47
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-05-17 08:07:00 +0200

    test

--- x-pack/plugins/security_solution/scripts/endpoint/run_cypress/parallel.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/run_cypress/parallel.ts
@@ -278,7 +278,7 @@ export default async () => {
           cleanupServerPorts({ esPort, kibanaPort });
         });
     },
-    { concurrency: 2 }
+    { concurrency: 3 }
   ).then((results) => {
     console.error('results', results);
     // renderSummaryTable(undefined, deepMerge.all(results));
