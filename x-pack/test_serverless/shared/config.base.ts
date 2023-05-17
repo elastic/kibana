58de8b85f1c4d85664031cf61cec5ee12a41ddbe
Robert Oskamp <robert.oskamp@elastic.co>
2023-05-17 16:55:25 +0200

    Override FTR timeouts for serverless

--- x-pack/test_serverless/shared/config.base.ts
+++ x-pack/test_serverless/shared/config.base.ts
@@ -56,5 +56,18 @@ export default async () => {
         ])}`,
       ],
     },
+
+    // overriding default timeouts from packages/kbn-test/src/functional_test_runner/lib/config/schema.ts
+    // so we can easily adjust them for serverless where needed
+    timeouts: {
+      find: 10 * 1000,
+      try: 120 * 1000,
+      waitFor: 20 * 1000,
+      esRequestTimeout: 30 * 1000,
+      kibanaReportCompletion: 60 * 1000,
+      kibanaStabilize: 15 * 1000,
+      navigateStatusPageCheck: 250,
+      waitForExists: 2500,
+    },
   };
 };
