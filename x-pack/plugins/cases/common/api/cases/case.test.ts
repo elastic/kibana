db6dcf749a977edb1656b5a481531794e007e716
Janki Salvi <117571355+js-jankisalvi@users.noreply.github.com>
2023-05-17 12:38:17 +0200

    fix findCaseUserActionsResponse mock

--- x-pack/plugins/cases/common/api/cases/case.test.ts
+++ x-pack/plugins/cases/common/api/cases/case.test.ts
@@ -30,60 +30,62 @@ import { CommentType } from './comment';
 import { CaseStatuses } from './status';
 
 const basicCase = {
-    owner: 'cases',
-            closed_at: null,
-            closed_by: null,
-            id: 'basic-case-id',
-            comments: [{
-                comment: 'Solve this fast!',
-                type: CommentType.user,
-                id: 'basic-comment-id',
-                created_at: '2020-02-19T23:06:33.798Z',
-                created_by: {
-                                full_name: 'Leslie Knope',
-                                username: 'lknope',
-                                email: 'leslie.knope@elastic.co',
-                              },
-                owner: 'cases',
-                pushed_at: null,
-                pushed_by: null,
-                updated_at: null,
-                updated_by: null,
-                version: 'WzQ3LDFc',
-              }],
-            created_at: '2020-02-19T23:06:33.798Z',
-            created_by: {
-                            full_name: 'Leslie Knope',
-                            username: 'lknope',
-                            email: 'leslie.knope@elastic.co',
-                          },
-            connector: {
-              id: 'none',
-              name: 'My Connector',
-              type: ConnectorTypes.none,
-              fields: null,
-            },
-            description: 'Security banana Issue',
-            severity: CaseSeverity.LOW,
-            duration: null,
-            external_service: null,
-            status: CaseStatuses.open,
-            tags: ['coke', 'pepsi'],
-            title: 'Another horrible breach!!',
-            totalComment: 1,
-            totalAlerts: 0,
-            updated_at: '2020-02-20T15:02:57.995Z',
-            updated_by: {
-                full_name: 'Leslie Knope',
-                username: 'lknope',
-                email: 'leslie.knope@elastic.co',
-              },
-            version: 'WzQ3LDFd',
-            settings: {
-              syncAlerts: true,
-            },
-            // damaged_raccoon uid
-            assignees: [{ uid: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0' }],
+  owner: 'cases',
+  closed_at: null,
+  closed_by: null,
+  id: 'basic-case-id',
+  comments: [
+    {
+      comment: 'Solve this fast!',
+      type: CommentType.user,
+      id: 'basic-comment-id',
+      created_at: '2020-02-19T23:06:33.798Z',
+      created_by: {
+        full_name: 'Leslie Knope',
+        username: 'lknope',
+        email: 'leslie.knope@elastic.co',
+      },
+      owner: 'cases',
+      pushed_at: null,
+      pushed_by: null,
+      updated_at: null,
+      updated_by: null,
+      version: 'WzQ3LDFc',
+    },
+  ],
+  created_at: '2020-02-19T23:06:33.798Z',
+  created_by: {
+    full_name: 'Leslie Knope',
+    username: 'lknope',
+    email: 'leslie.knope@elastic.co',
+  },
+  connector: {
+    id: 'none',
+    name: 'My Connector',
+    type: ConnectorTypes.none,
+    fields: null,
+  },
+  description: 'Security banana Issue',
+  severity: CaseSeverity.LOW,
+  duration: null,
+  external_service: null,
+  status: CaseStatuses.open,
+  tags: ['coke', 'pepsi'],
+  title: 'Another horrible breach!!',
+  totalComment: 1,
+  totalAlerts: 0,
+  updated_at: '2020-02-20T15:02:57.995Z',
+  updated_by: {
+    full_name: 'Leslie Knope',
+    username: 'lknope',
+    email: 'leslie.knope@elastic.co',
+  },
+  version: 'WzQ3LDFd',
+  settings: {
+    syncAlerts: true,
+  },
+  // damaged_raccoon uid
+  assignees: [{ uid: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0' }],
 };
 
 describe('Case', () => {
@@ -195,115 +197,121 @@ describe('Case', () => {
     });
   });
 
-    describe('CaseAttributesRt', () => {
-      const defaultRequest = {
-          description: 'A description',
-          status: CaseStatuses.open,
-          tags: ['new', 'case'],
-          title: 'My new case',
-          connector: {
-              id: '123',
-              name: 'My connector',
-              type: ConnectorTypes.jira,
-              fields: { issueType: 'Task', priority: 'High', parent: null },
-          },
-          settings: {
-              syncAlerts: true,
-          },
-          owner: 'cases',
-          severity: CaseSeverity.LOW,
-          assignees: [{ uid: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0' }],
-          duration: null,
-          closed_at: null,
-          closed_by: null,
-          created_at: '2020-02-19T23:06:33.798Z',
-          created_by: {
-              fullName: 'Leslie Knope',
-              username: 'lknope',
-              email: 'leslie.knope@elastic.co',
-              },
-          external_service: null,
-          updated_at: '2020-02-20T15:02:57.995Z',
-          updated_by: {
-              fullName: 'Leslie Knope',
-              username: 'lknope',
-              email: 'leslie.knope@elastic.co',
-          },
-      }
-      it('has expected attributes in request', () => {
-        const query = CaseAttributesRt.decode(defaultRequest);
+  describe('CaseAttributesRt', () => {
+    const defaultRequest = {
+      description: 'A description',
+      status: CaseStatuses.open,
+      tags: ['new', 'case'],
+      title: 'My new case',
+      connector: {
+        id: '123',
+        name: 'My connector',
+        type: ConnectorTypes.jira,
+        fields: { issueType: 'Task', priority: 'High', parent: null },
+      },
+      settings: {
+        syncAlerts: true,
+      },
+      owner: 'cases',
+      severity: CaseSeverity.LOW,
+      assignees: [{ uid: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0' }],
+      duration: null,
+      closed_at: null,
+      closed_by: null,
+      created_at: '2020-02-19T23:06:33.798Z',
+      created_by: {
+        fullName: 'Leslie Knope',
+        username: 'lknope',
+        email: 'leslie.knope@elastic.co',
+      },
+      external_service: null,
+      updated_at: '2020-02-20T15:02:57.995Z',
+      updated_by: {
+        fullName: 'Leslie Knope',
+        username: 'lknope',
+        email: 'leslie.knope@elastic.co',
+      },
+    };
+    it('has expected attributes in request', () => {
+      const query = CaseAttributesRt.decode(defaultRequest);
 
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
       });
-
-      it('removes foo:bar attributes from request', () => {
-        const query = CaseAttributesRt.decode({...defaultRequest, foo: 'bar'});
-
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
-      });
-
-      it('removes foo:bar attributes from connector', () => {
-          const query = CaseAttributesRt.decode({...defaultRequest, connector: {...defaultRequest.connector, foo: 'bar' }});
-
-          expect(query).toMatchObject({
-            _tag: 'Right',
-            right: defaultRequest,
-          });
-        });
     });
+
+    it('removes foo:bar attributes from request', () => {
+      const query = CaseAttributesRt.decode({ ...defaultRequest, foo: 'bar' });
+
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
+      });
+    });
+
+    it('removes foo:bar attributes from connector', () => {
+      const query = CaseAttributesRt.decode({
+        ...defaultRequest,
+        connector: { ...defaultRequest.connector, foo: 'bar' },
+      });
+
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
+      });
+    });
+  });
 
   describe('CasePostRequestRt', () => {
-      const defaultRequest = {
-          description: 'A description',
-          tags: ['new', 'case'],
-          title: 'My new case',
-          connector: {
-              id: '123',
-              name: 'My connector',
-              type: ConnectorTypes.jira,
-              fields: { issueType: 'Task', priority: 'High', parent: null },
-          },
-          settings: {
-              syncAlerts: true,
-          },
-          owner: 'cases',
-          severity: CaseSeverity.LOW,
-          assignees: [{ uid: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0' }],
-      }
+    const defaultRequest = {
+      description: 'A description',
+      tags: ['new', 'case'],
+      title: 'My new case',
+      connector: {
+        id: '123',
+        name: 'My connector',
+        type: ConnectorTypes.jira,
+        fields: { issueType: 'Task', priority: 'High', parent: null },
+      },
+      settings: {
+        syncAlerts: true,
+      },
+      owner: 'cases',
+      severity: CaseSeverity.LOW,
+      assignees: [{ uid: 'u_J41Oh6L9ki-Vo2tOogS8WRTENzhHurGtRc87NgEAlkc_0' }],
+    };
 
-      it('has expected attributes in request', () => {
-        const query = CasePostRequestRt.decode(defaultRequest);
+    it('has expected attributes in request', () => {
+      const query = CasePostRequestRt.decode(defaultRequest);
 
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
       });
-
-      it('removes foo:bar attributes from request', () => {
-        const query = CasePostRequestRt.decode({...defaultRequest, foo: 'bar'});
-
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
-      });
-
-      it('removes foo:bar attributes from connector', () => {
-          const query = CasePostRequestRt.decode({...defaultRequest, connector: {...defaultRequest.connector, foo: 'bar' }});
-
-          expect(query).toMatchObject({
-            _tag: 'Right',
-            right: defaultRequest,
-          });
-        });
     });
+
+    it('removes foo:bar attributes from request', () => {
+      const query = CasePostRequestRt.decode({ ...defaultRequest, foo: 'bar' });
+
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
+      });
+    });
+
+    it('removes foo:bar attributes from connector', () => {
+      const query = CasePostRequestRt.decode({
+        ...defaultRequest,
+        connector: { ...defaultRequest.connector, foo: 'bar' },
+      });
+
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
+      });
+    });
+  });
 
   describe('CasesFindRequestRt', () => {
     const defaultRequest = {
@@ -364,62 +372,62 @@ describe('Case', () => {
     });
   });
 
-    describe('CaseResolveResponseRt', () => {
-      const defaultRequest = {
-          case: {...basicCase},
-          outcome: 'exactMatch',
-          alias_target_id: 'sample-target-id',
-          alias_purpose: 'savedObjectConversion',
-      }
+  describe('CaseResolveResponseRt', () => {
+    const defaultRequest = {
+      case: { ...basicCase },
+      outcome: 'exactMatch',
+      alias_target_id: 'sample-target-id',
+      alias_purpose: 'savedObjectConversion',
+    };
 
-      it('has expected attributes in request', () => {
-        const query = CaseResolveResponseRt.decode(defaultRequest);
+    it('has expected attributes in request', () => {
+      const query = CaseResolveResponseRt.decode(defaultRequest);
 
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
-      });
-
-      it('removes foo:bar attributes from request', () => {
-        const query = CaseResolveResponseRt.decode({...defaultRequest, foo: 'bar'});
-
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
       });
     });
 
-    describe('CasesFindResponseRt', () => {
-      const defaultRequest = {
-          cases: [{...basicCase}],
-          page: 1,
-          per_page: 10,
-          total: 20,
-          count_open_cases: 10,
-          count_in_progress_cases: 5,
-          count_closed_cases: 5,
-      };
+    it('removes foo:bar attributes from request', () => {
+      const query = CaseResolveResponseRt.decode({ ...defaultRequest, foo: 'bar' });
 
-      it('has expected attributes in request', () => {
-        const query = CasesFindResponseRt.decode(defaultRequest);
-
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
-      });
-
-      it('removes foo:bar attributes from request', () => {
-        const query = CasesFindResponseRt.decode({...defaultRequest, foo: 'bar'});
-
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
       });
     });
+  });
+
+  describe('CasesFindResponseRt', () => {
+    const defaultRequest = {
+      cases: [{ ...basicCase }],
+      page: 1,
+      per_page: 10,
+      total: 20,
+      count_open_cases: 10,
+      count_in_progress_cases: 5,
+      count_closed_cases: 5,
+    };
+
+    it('has expected attributes in request', () => {
+      const query = CasesFindResponseRt.decode(defaultRequest);
+
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
+      });
+    });
+
+    it('removes foo:bar attributes from request', () => {
+      const query = CasesFindResponseRt.decode({ ...defaultRequest, foo: 'bar' });
+
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
+      });
+    });
+  });
 
   describe('CasePatchRequestRt', () => {
     const defaultRequest = {
@@ -477,29 +485,31 @@ describe('Case', () => {
     });
   });
 
-    describe('CasesRt', () => {
-      const defaultRequest = [{
-          ...basicCase,
-          }];
+  describe('CasesRt', () => {
+    const defaultRequest = [
+      {
+        ...basicCase,
+      },
+    ];
 
-      it('has expected attributes in request', () => {
-        const query = CasesRt.decode(defaultRequest);
+    it('has expected attributes in request', () => {
+      const query = CasesRt.decode(defaultRequest);
 
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
-      });
-
-      it('removes foo:bar attributes from request', () => {
-        const query = CasesRt.decode({...defaultRequest, foo: 'bar'});
-
-        expect(query).toMatchObject({
-          _tag: 'Right',
-          right: defaultRequest,
-        });
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
       });
     });
+
+    it('removes foo:bar attributes from request', () => {
+      const query = CasesRt.decode({ ...defaultRequest, foo: 'bar' });
+
+      expect(query).toMatchObject({
+        _tag: 'Right',
+        right: defaultRequest,
+      });
+    });
+  });
 
   describe('CasePushRequestParamsRt', () => {
     const defaultRequest = {
