e8d2c7d6ecdf795ada42e62cf42c6bfc7a7ef913
Kyle Pollich <kyle.pollich@elastic.co>
2023-05-17 12:00:07 -0400

    Fix syntax error

--- x-pack/test/fleet_api_integration/apis/epm/data_stream.ts
+++ x-pack/test/fleet_api_integration/apis/epm/data_stream.ts
@@ -339,7 +339,7 @@ export default function (providerContext: FtrProviderContext) {
         );
 
         return resLogsDatastream.body.data_streams[0].indices.length;
-      }
+      });
 
       it('should rollover datstream after enabling a experimental datastream feature that need a rollover', async () => {
         expect(await getLogsDefaultBackingIndicesLength()).to.be(1);
