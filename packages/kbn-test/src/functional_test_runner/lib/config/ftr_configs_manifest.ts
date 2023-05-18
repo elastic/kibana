e0a4f738972e92d250b5db085d3d624d3ca50ecb
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-18 13:44:27 +0100

    mo debug

--- packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
+++ packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
@@ -9,7 +9,7 @@
 import execa from 'execa';
 import Path from 'path';
 import Fs from 'fs';
-
+import { execSync } from 'child_process';
 import { REPO_ROOT } from '@kbn/repo-info';
 import JsYaml from 'js-yaml';
 
@@ -34,19 +34,46 @@ console.log(`cwd: ${process.cwd()}`);
 
 ls('.');
 ls(REPO_ROOT);
+// REPO_ROOT: /var/lib/buildkite-agent/builds/kb-n2-4-spot-816c5d0c23ef2bc6/elastic/kibana-pull-request/kibana
+// cwd:       /var/lib/buildkite-agent/builds/kb-n2-4-spot-816c5d0c23ef2bc6/elastic/kibana-pull-request/kibana
 ls(`${REPO_ROOT}/.buildkite`);
-ls(
-  '/var/lib/buildkite-agent/builds/kb-n2-4-spot-8d94c28da2235528/elastic/kibana-pull-request/kibana-build-xpack/'
-);
-ls(
-  '/var/lib/buildkite-agent/builds/kb-n2-4-spot-8d94c28da2235528/elastic/kibana-pull-request/kibana-build-xpack/.buildkite/'
-);
+// ls(
+//   '/var/lib/buildkite-agent/builds/kb-n2-4-spot-8d94c28da2235528/elastic/kibana-pull-request/kibana-build-xpack/'
+// );
+// ls(
+//   '/var/lib/buildkite-agent/builds/kb-n2-4-spot-8d94c28da2235528/elastic/kibana-pull-request/kibana-build-xpack/.buildkite/'
+// );
+
+// const paths = [
+//   REPO_ROOT,
+//   `${REPO_ROOT}/.buildkite`,
+// ]
+
+const parentDir = (x: string) => execSync(`dirname ${x}`, { cwd: REPO_ROOT, encoding: 'utf8' });
+let repoRootParent;
+
+// eslint-disable-next-line no-console
+console.log('\n--- Show some path info!');
+
+try {
+  repoRootParent = parentDir(REPO_ROOT);
+  // eslint-disable-next-line no-console
+  console.log(`\n### repoRootParent: \n  ${repoRootParent}`);
+} catch (e) {
+  // eslint-disable-next-line no-console
+  console.error(`\n### Error looking at paths e: \n  ${e}`);
+}
+process.exit(999);
 
 async function ls(dir: string) {
   try {
     const { stdout } = await execa('ls', ['-la', dir]);
     // eslint-disable-next-line no-console
-    console.log(`ls ${dir}:\n${stdout}\nls (end)`);
+    console.log(`--- ls of ${dir}`);
+    // eslint-disable-next-line no-console
+    console.log(`ls ${dir}:\n${stdout}\n`);
+    // eslint-disable-next-line no-console
+    console.log(`### END ls of ${dir}`);
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(`ls ${dir}: ERROR: ${e}`);
