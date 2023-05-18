90f94daf8eb9a0d8f79f72bfa5542186f896f4c3
Coen Warmer <coen.warmer@gmail.com>
2023-05-18 19:38:36 +0200

    Reorganize Observability Plugin (#157970)

--- x-pack/plugins/observability/public/pages/slo_edit/slo_edit.test.tsx
+++ x-pack/plugins/observability/public/pages/slo_edit/slo_edit.test.tsx
@@ -31,7 +31,7 @@ jest.mock('react-router-dom', () => ({
   useParams: jest.fn(),
 }));
 
-jest.mock('../../hooks/use_breadcrumbs');
+jest.mock('@kbn/observability-shared-plugin/public');
 jest.mock('../../hooks/use_license');
 jest.mock('../../hooks/use_fetch_indices');
 jest.mock('../../hooks/slo/use_fetch_slo_details');
---
+++ x-pack/plugins/observability/public/plugin.mock.tsx
@@ -0,0 +1,88 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React from 'react';
+import { mockCasesContract } from '@kbn/cases-plugin/public/mocks';
+
+const triggersActionsUiStartMock = {
+  createStart() {
+    return {
+      getAlertsSearchBar: jest.fn(() => (
+        <div data-test-subj="alerts-search-bar">mocked component</div>
+      )),
+      getAlertsStateTable: jest.fn(() => (
+        <div data-test-subj="alerts-state-table">mocked component</div>
+      )),
+      getAddRuleFlyout: jest.fn(() => <div data-test-subj="add-rule-flyout">mocked component</div>),
+      getEditRuleFlyout: jest.fn(() => (
+        <div data-test-subj="edit-rule-flyout">mocked component</div>
+      )),
+      getRuleAlertsSummary: jest.fn(() => (
+        <div data-test-subj="rule-alerts-summary">mocked component</div>
+      )),
+      getRuleDefinition: jest.fn(() => (
+        <div data-test-subj="rule-definition">mocked component</div>
+      )),
+      getRuleEventLogList: jest.fn(() => (
+        <div data-test-subj="rule-event-log-list">mocked component</div>
+      )),
+      getRuleStatusDropdown: jest.fn(() => (
+        <div data-test-subj="rule-status-dropdown">mocked component</div>
+      )),
+      getRuleStatusPanel: jest.fn(() => (
+        <div data-test-subj="rule-status-panel">mocked component</div>
+      )),
+      getRuleTagBadge: jest.fn(() => <div data-test-subj="rule-tag-badge">mocked component</div>),
+      getRuleStatusFilter: jest.fn(() => (
+        <div data-test-subj="rule-status-filter">mocked component</div>
+      )),
+      getRuleTagFilter: jest.fn(() => <div data-test-subj="rule-tag-filter">mocked component</div>),
+      getRulesList: jest.fn(() => <div data-test-subj="rules-list">mocked component</div>),
+      getRulesSettingsLink: jest.fn(() => (
+        <div data-test-subj="rules-settings-link">mocked component</div>
+      )),
+      ruleTypeRegistry: {
+        has: jest.fn(),
+        register: jest.fn(),
+        get: jest.fn(),
+        list: jest.fn(),
+      },
+      actionTypeRegistry: {
+        has: jest.fn((x) => true),
+        register: jest.fn(),
+        get: jest.fn(),
+        list: jest.fn(),
+      },
+    };
+  },
+};
+
+const data = {
+  createStart() {
+    return {
+      dataViews: {
+        create: jest.fn(),
+      },
+      query: {
+        timefilter: {
+          timefilter: jest.fn(),
+        },
+      },
+    };
+  },
+};
+
+export const observabilityPublicPluginsStartMock = {
+  createStart() {
+    return {
+      cases: mockCasesContract(),
+      triggersActionsUi: triggersActionsUiStartMock.createStart(),
+      data: data.createStart(),
+      lens: null,
+      discover: null,
+    };
+  },
+};
