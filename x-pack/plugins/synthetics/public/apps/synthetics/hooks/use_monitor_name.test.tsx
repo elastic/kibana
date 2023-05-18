ae26b129aef87887e23877f9222449c9d5c52c14
Dominique Clarke <dominique.clarke@elastic.co>
2023-05-17 21:46:40 -0400

    adjust tests

--- x-pack/plugins/synthetics/public/apps/synthetics/hooks/use_monitor_name.test.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/hooks/use_monitor_name.test.tsx
@@ -4,77 +4,123 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-import { defaultCore, WrappedHelper } from '../utils/testing/rtl_helpers';
+import React from 'react';
+import { WrappedHelper } from '../utils/testing/rtl_helpers';
 import { renderHook } from '@testing-library/react-hooks';
 import { useMonitorName } from './use_monitor_name';
-import { Router, useParams } from 'react-router-dom';
-
-const mockRouter = {
-  useParams,
-  ...Router,
-};
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useParams: jest.fn().mockReturnValue({}),
+  useParams: jest.fn().mockReturnValue({ monitorId: '12345' }),
 }));
 
 describe('useMonitorName', () => {
-  it('returns expected results', () => {
-    const { result } = renderHook(() => useMonitorName({}), { wrapper: WrappedHelper });
+  const Wrapper = ({ children }: { children: React.ReactElement }) => {
+    return (
+      <WrappedHelper
+        state={{
+          monitorList: {
+            error: null,
+            loading: true,
+            loaded: false,
+            monitorUpsertStatuses: {},
+            data: {
+              absoluteTotal: 1,
+              perPage: 5,
+              page: 1,
+              total: 1,
+              monitors: [
+                {
+                  attributes: {
+                    name: 'Test monitor name',
+                    config_id: '12345',
+                    locations: [
+                      {
+                        id: 'us_central_qa',
+                      },
+                    ],
+                  },
+                },
+                {
+                  attributes: {
+                    name: 'Test monitor name 2',
+                    config_id: '12346',
+                    locations: [
+                      {
+                        id: 'us_central_qa',
+                      },
+                    ],
+                  },
+                },
+              ],
+              syncErrors: [],
+            },
+            pageState: {
+              pageIndex: 1,
+              pageSize: 10,
+              sortOrder: 'asc',
+              sortField: `name.keyword`,
+            },
+          },
+        }}
+      >
+        {children}
+      </WrappedHelper>
+    );
+  };
 
-    expect(result.current).toStrictEqual({ nameAlreadyExists: false, validName: '' });
-    expect(defaultCore.savedObjects.client.find).toHaveBeenCalledWith({
-      aggs: {
-        monitorNames: {
-          terms: { field: 'synthetics-monitor.attributes.name.keyword', size: 10000 },
+  it('returns expected results', () => {
+    const { result } = renderHook(() => useMonitorName({}), { wrapper: Wrapper });
+
+    expect(result.current).toStrictEqual({
+      loading: true,
+      values: [
+        {
+          key: '12346',
+          label: 'Test monitor name 2',
+          locationIds: ['us_central_qa'],
         },
-      },
-      perPage: 0,
-      type: 'synthetics-monitor',
+      ],
+      nameAlreadyExists: false,
+      validName: '',
     });
   });
 
   it('returns expected results after data', async () => {
-    defaultCore.savedObjects.client.find = jest.fn().mockReturnValue({
-      aggregations: {
-        monitorNames: {
-          buckets: [{ key: 'Test' }, { key: 'Test 1' }],
+    const { result } = renderHook(() => useMonitorName({ search: 'Test monitor name 2' }), {
+      wrapper: Wrapper,
+    });
+
+    expect(result.current).toStrictEqual({
+      loading: true,
+      nameAlreadyExists: false,
+      validName: 'Test monitor name 2',
+      values: [
+        {
+          key: '12346',
+          label: 'Test monitor name 2',
+          locationIds: ['us_central_qa'],
         },
-      },
+      ],
     });
-
-    const { result, waitForNextUpdate } = renderHook(() => useMonitorName({ search: 'Test' }), {
-      wrapper: WrappedHelper,
-    });
-
-    expect(result.current).toStrictEqual({ nameAlreadyExists: false, validName: 'Test' });
-
-    await waitForNextUpdate();
-
-    expect(result.current).toStrictEqual({ nameAlreadyExists: true, validName: '' });
   });
 
   it('returns expected results after data while editing monitor', async () => {
-    defaultCore.savedObjects.client.find = jest.fn().mockReturnValue({
-      aggregations: {
-        monitorNames: {
-          buckets: [{ key: 'Test' }, { key: 'Test 1' }],
+    const { result } = renderHook(() => useMonitorName({ search: 'Test monitor name' }), {
+      wrapper: Wrapper,
+    });
+
+    expect(result.current).toStrictEqual({
+      loading: true,
+      nameAlreadyExists: false,
+      validName: 'Test monitor name',
+      values: [
+        {
+          key: '12346',
+          label: 'Test monitor name 2',
+          locationIds: ['us_central_qa'],
         },
-      },
+      ],
     });
-
-    jest.spyOn(mockRouter, 'useParams').mockReturnValue({ monitorId: 'test-id' });
-
-    const { result, waitForNextUpdate } = renderHook(() => useMonitorName({ search: 'Test' }), {
-      wrapper: WrappedHelper,
-    });
-
-    expect(result.current).toStrictEqual({ nameAlreadyExists: false, validName: 'Test' });
-
-    await waitForNextUpdate();
-
-    expect(result.current).toStrictEqual({ nameAlreadyExists: false, validName: 'Test' });
   });
 });
