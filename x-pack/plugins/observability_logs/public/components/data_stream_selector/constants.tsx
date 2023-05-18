7aef4cad57855bf7a549e0d4bf07ba2d71d3b4ff
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-05-18 16:27:28 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/observability_logs/public/components/data_stream_selector/constants.tsx
+++ x-pack/plugins/observability_logs/public/components/data_stream_selector/constants.tsx
@@ -5,10 +5,7 @@
  * 2.0.
  */
 
-import { EuiText, EuiToolTip } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n-react';
-import React from 'react';
 
 export const POPOVER_ID = 'data-stream-selector-popover';
 export const INTEGRATION_PANEL_ID = 'integrations_panel';
