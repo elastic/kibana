02825a662760e8897bb2bc34e897bd2b2dc7e562
Timothy Sullivan <tsullivan@elastic.co>
2023-05-18 13:59:53 -0700

    Fix bug to allow mounter to clear

--- packages/core/chrome/core-chrome-browser-internal/src/ui/header/header_action_menu.tsx
+++ packages/core/chrome/core-chrome-browser-internal/src/ui/header/header_action_menu.tsx
@@ -10,31 +10,29 @@ import React, { FC, useRef, useLayoutEffect, useState } from 'react';
 import { Observable } from 'rxjs';
 import type { MountPoint, UnmountCallback } from '@kbn/core-mount-utils-browser';
 
+interface HeaderActionMenuProps {
+  mounter: { mount: MountPoint | undefined };
+}
+
 export const useHeaderActionMenuMounter = (
   actionMenu$: Observable<MountPoint<HTMLElement> | undefined>
 ) => {
-  const [mounter, setMounter] = useState<{ mount: MountPoint | undefined }>({ mount: undefined });
   // useObservable relies on useState under the hood. The signature is type SetStateAction<S> = S | ((prevState: S) => S);
   // As we got a Observable<Function> here, React's setState setter assume he's getting a `(prevState: S) => S` signature,
   // therefore executing the mount method, causing everything to crash.
   // piping the observable before calling `useObservable` causes the effect to always having a new reference, as
   // the piped observable is a new instance on every render, causing infinite loops.
   // this is why we use `useLayoutEffect` manually here.
+  const [mounter, setMounter] = useState<{ mount: MountPoint | undefined }>({ mount: undefined });
   useLayoutEffect(() => {
     const s = actionMenu$.subscribe((value) => {
-      if (value) {
-        setMounter({ mount: value });
-      }
+      setMounter({ mount: value });
     });
     return () => s.unsubscribe();
   }, [actionMenu$]);
 
   return mounter;
 };
-
-interface HeaderActionMenuProps {
-  mounter: { mount: MountPoint | undefined };
-}
 
 export const HeaderActionMenu: FC<HeaderActionMenuProps> = ({ mounter }) => {
   const elementRef = useRef<HTMLDivElement>(null);
