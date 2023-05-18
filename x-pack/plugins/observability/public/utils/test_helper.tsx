ab27caff474b96d0753f52a6d002e8e453e942e4
Coen Warmer <coen.warmer@gmail.com>
2023-05-18 19:38:36 +0200

    Reorganize Observability Plugin (#157970)

--- x-pack/plugins/observability/public/utils/test_helper.tsx
+++ x-pack/plugins/observability/public/utils/test_helper.tsx
@@ -18,7 +18,7 @@ import translations from '@kbn/translations-plugin/translations/ja-JP.json';
 import { EuiThemeProvider } from '@kbn/kibana-react-plugin/common';
 import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
 
-import { PluginContext } from '../context/plugin_context';
+import { PluginContext } from '../context/plugin_context/plugin_context';
 import { createObservabilityRuleTypeRegistryMock } from '../rules/observability_rule_type_registry_mock';
 import { ConfigSchema } from '../plugin';
 import { Subset } from '../typings';
