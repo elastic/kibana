429074d41f39959090f60eac1e55fe7344edf456
semd <sergi.massaneda@elastic.co>
2023-05-20 11:34:13 +0200

    lazy load tab_navigation component

---
+++ x-pack/plugins/security_solution/public/common/components/navigation/tab_navigation/index.tsx
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiLoadingSpinner } from '@elastic/eui';
+import type { TabNavigationProps } from './types';
+
+// export { TabNavigation } from './tab_navigation';
+const TabNavigationLazy = React.lazy(() => import('./tab_navigation'));
+
+export const TabNavigation = (props: TabNavigationProps) => (
+  <React.Suspense fallback={<EuiLoadingSpinner size="l" />}>
+    <TabNavigationLazy {...props} />
+  </React.Suspense>
+);
