029d7cc93134f965c7e2635aaf66f2dabf8e64b7
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-23 11:27:09 +0100

    as it was

--- packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
+++ packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
@@ -8,11 +8,8 @@
 
 import Path from 'path';
 import Fs from 'fs';
-import { execSync } from 'child_process';
 import { REPO_ROOT } from '@kbn/repo-info';
 import JsYaml from 'js-yaml';
-import * as E from 'fp-ts/lib/Either';
-import { pipe } from 'fp-ts/lib/pipeable';
 
 export const FTR_CONFIGS_MANIFEST_REL = '.buildkite/ftr_configs.yml';
 
@@ -28,48 +25,10 @@ interface FtrConfigsManifest {
   enabled: Array<string | FtrConfigWithOptions>;
 }
 
-// eslint-disable-next-line no-console
-console.log(`--- REPO_ROOT: ${REPO_ROOT}`);
-// eslint-disable-next-line no-console
-console.log(`--- cwd: ${process.cwd()}`);
-
-const parentDir = (root: string) => (x: string) =>
-  execSync(`dirname ${x}`, { cwd: root, encoding: 'utf8' });
-const ls = (root: string) => (x: string) => execSync(`ls ${x}`, { cwd: root, encoding: 'utf8' });
-// const lsMsg = (root: string) => (x: string) => {
-//   // eslint-disable-next-line no-console
-//   console.log(`--- Contents of: ${Path.resolve(root, x)}\n`);
-//   return ls(x);
-// };
-
-let repoRootParent;
-let repoRootParentContents;
-// eslint-disable-next-line no-console
-console.log('\n--- Show some path info!');
-
-try {
-  repoRootParent = parentDir(REPO_ROOT)(REPO_ROOT);
-  repoRootParentContents = ls(REPO_ROOT)(repoRootParent);
-  // eslint-disable-next-line no-console
-  console.log(`\n### Parent Directory of REPO_ROOT: ${repoRootParent}
-### Contents of ${repoRootParent}
-${repoRootParentContents}
-`);
-} catch (e) {
-  // eslint-disable-next-line no-console
-  console.error(`\n### Error looking at paths e: \n  ${e}`);
-}
-const loadManifest = (a: string, b: string) => () =>
-  JsYaml.safeLoad(Fs.readFileSync(Path.resolve(a, b), 'utf8'));
-const manifest = (root: string, configsRelativePath: string) =>
-  E.tryCatch(loadManifest(root, configsRelativePath), E.toError);
-const ftrConfigsManifest: FtrConfigsManifest = pipe(
-  manifest(REPO_ROOT, FTR_CONFIGS_MANIFEST_REL),
-  E.fold(
-    (_: any) => _,
-    (x: FtrConfigsManifest) => x
-  )
+const ftrConfigsManifest: FtrConfigsManifest = JsYaml.safeLoad(
+  Fs.readFileSync(Path.resolve(REPO_ROOT, FTR_CONFIGS_MANIFEST_REL), 'utf8')
 );
+
 export const FTR_CONFIGS_MANIFEST_PATHS = [
   Object.values(ftrConfigsManifest.enabled),
   Object.values(ftrConfigsManifest.disabled),
