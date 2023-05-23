6550e426eb2664ca206ad53143a147a367dcce8c
Dario Gieselaar <dario.gieselaar@elastic.co>
2023-05-23 16:50:43 +0200

    [APM] Version public APIs (#158233)
    Closes #155245
    
    Adds a version to the `endpoint` name for public APIs. It's required for
    public APIs and enforced via type checks. Example:
    `GET /api/apm/settings/agent-configuration/environments 2023-05-22`. The
    reason why it's part of the endpoint is because it's the simplest change
    to make, requires very little runtime changes and doesn't dramatically
    change the way we handle type-safety for our server routes.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/observability/server/routes/rules/route.ts
+++ x-pack/plugins/observability/server/routes/rules/route.ts
@@ -10,7 +10,7 @@ import { Dataset } from '@kbn/rule-registry-plugin/server';
 import { createObservabilityServerRoute } from '../create_observability_server_route';
 
 const alertsDynamicIndexPatternRoute = createObservabilityServerRoute({
-  endpoint: 'GET /api/observability/rules/alerts/dynamic_index_pattern',
+  endpoint: 'GET /api/observability/rules/alerts/dynamic_index_pattern 2023-05-22',
   options: {
     tags: [],
   },
