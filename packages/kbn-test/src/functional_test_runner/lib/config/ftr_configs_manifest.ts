523cc008abf3ca3ed8b786b6dd55a0438d50440a
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-23 12:29:31 +0100

    try to load abnormally if nomal fails

--- packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
+++ packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
@@ -24,10 +24,38 @@ interface FtrConfigsManifest {
   disabled: string[];
   enabled: Array<string | FtrConfigWithOptions>;
 }
+let ftrConfigsManifest: FtrConfigsManifest;
 
-const ftrConfigsManifest: FtrConfigsManifest = JsYaml.safeLoad(
-  Fs.readFileSync(Path.resolve(REPO_ROOT, FTR_CONFIGS_MANIFEST_REL), 'utf8')
-);
+const loadNormal = (): FtrConfigsManifest => {
+  const x = Path.resolve(REPO_ROOT, FTR_CONFIGS_MANIFEST_REL);
+  // eslint-disable-next-line no-console
+  console.log(`\n### Trying to load: \n  ${x}`);
+  return JsYaml.safeLoad(Fs.readFileSync(x, 'utf8'));
+};
+
+const loadAbnormal = (): FtrConfigsManifest => {
+  const x = Path.resolve(REPO_ROOT, '../kibana', FTR_CONFIGS_MANIFEST_REL);
+  // eslint-disable-next-line no-console
+  console.log(`\n### Trying to load (abnormally): \n  ${x}`);
+  return JsYaml.safeLoad(
+    Fs.readFileSync(Path.resolve(REPO_ROOT, '../kibana', FTR_CONFIGS_MANIFEST_REL), 'utf8')
+  );
+};
+try {
+  ftrConfigsManifest = loadNormal();
+} catch (_) {
+  // eslint-disable-next-line no-console
+  console.error(`\n### Could not load ${FTR_CONFIGS_MANIFEST_REL} normally, Error: \n  ${_}`);
+}
+
+try {
+  ftrConfigsManifest = loadAbnormal();
+} catch (_) {
+  const msg = `\n### Could not load ${FTR_CONFIGS_MANIFEST_REL} abnormally either, Error: \n  ${_}`;
+  // eslint-disable-next-line no-console
+  console.error(msg);
+  throw new Error(msg);
+}
 
 export const FTR_CONFIGS_MANIFEST_PATHS = [
   Object.values(ftrConfigsManifest.enabled),
