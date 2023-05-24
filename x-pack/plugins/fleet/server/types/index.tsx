0ff105db02fcf85c4bbc8838fc8993fcadd507cc
criamico <mariacristina.amico@elastic.co>
2023-05-24 10:37:07 +0200

    removed type

--- x-pack/plugins/fleet/server/types/index.tsx
+++ x-pack/plugins/fleet/server/types/index.tsx
@@ -78,7 +78,6 @@ export type {
   FullAgentPolicyInputStream,
   DownloadSourceBase,
   DownloadSource,
-  DownloadSourceAttributes,
   PackageVerificationStatus,
   BulkInstallPackageInfo,
   PackageAssetReference,
