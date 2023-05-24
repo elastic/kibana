d283753031d4862593d49fd8cddc59143006ec68
Sébastien Loix <sabee77@gmail.com>
2023-05-24 14:09:13 +0100

    Add more storybook examles

--- packages/shared-ux/chrome/navigation/src/ui/v2/navigation.stories.tsx
+++ packages/shared-ux/chrome/navigation/src/ui/v2/navigation.stories.tsx
@@ -13,9 +13,13 @@ import { action } from '@storybook/addon-actions';
 import type { ChromeNavLink, ChromeProjectNavigationNode } from '@kbn/core-chrome-browser';
 
 import {
+  EuiButton,
   EuiButtonIcon,
   EuiCollapsibleNav,
+  EuiFlexGroup,
+  EuiFlexItem,
   EuiLink,
+  EuiText,
   EuiThemeProvider,
   EuiTitle,
 } from '@elastic/eui';
@@ -255,29 +259,6 @@ export const DefaultUI = (args: ChromeNavigationViewModel & NavigationServices) 
 
           <Navigation.RecentlyAccessed />
 
-          <Navigation.Group id="myCustomGroup" unstyled>
-            <div css={{ backgroundColor: 'yellow', padding: '16px' }}>
-              <EuiTitle size="m">
-                <h3>I am unstyled</h3>
-              </EuiTitle>
-              <ul>
-                <Navigation.Item id="item1" element="li" title="But this item" unstyled />
-                <Navigation.Item id="item2" element="li" title="...and this item" unstyled />
-                <Navigation.Item id="item3" title="...are all registered" unstyled>
-                  <li>
-                    <EuiLink>...are all registered</EuiLink>
-                  </li>
-                </Navigation.Item>
-                <Navigation.Item
-                  id="item4"
-                  element="li"
-                  title="...in the chrome service"
-                  unstyled
-                />
-              </ul>
-            </div>
-          </Navigation.Group>
-
           <Navigation.Group
             id="example_projet"
             title="Example project"
@@ -287,8 +268,10 @@ export const DefaultUI = (args: ChromeNavigationViewModel & NavigationServices) 
             <Navigation.Group id="root">
               <Navigation.Item id="item1" title="Get started" />
               <Navigation.Item id="item2" title="Alerts" />
-              <Navigation.Item id="seb2" title="I am also cool">
-                <span style={{ color: 'blue', fontStyle: 'italic' }}>I am also cool </span>
+              <Navigation.Item id="item3" title="Some children node">
+                <EuiText size="s">
+                  <EuiLink>Some children node</EuiLink>
+                </EuiText>
               </Navigation.Item>
             </Navigation.Group>
 
@@ -306,6 +289,170 @@ export const DefaultUI = (args: ChromeNavigationViewModel & NavigationServices) 
             <Navigation.Bucket preset="devtools" />
             <Navigation.Bucket preset="management" />
           </Navigation.Footer>
+        </Navigation>
+      </NavigationProvider>
+    </NavigationWrapper>
+  );
+};
+
+export const MinimalUICustomCloudLink = (args: ChromeNavigationViewModel & NavigationServices) => {
+  const services = storybookMock.getServices({
+    ...args,
+    navLinks$: of(deepLinks),
+    onProjectNavigationChange: (updated) => {
+      action('Update chrome navigation')(JSON.stringify(updated, null, 2));
+    },
+    recentlyAccessed$: of([
+      { label: 'This is an example', link: '/app/example/39859', id: '39850' },
+      { label: 'Another example', link: '/app/example/5235', id: '5235' },
+    ]),
+  });
+
+  return (
+    <NavigationWrapper>
+      <NavigationProvider {...services}>
+        <Navigation homeRef="/">
+          <Navigation.CloudLink
+            title="Some other cool page"
+            href="https://elastic.co"
+            icon="spaces"
+          />
+
+          <Navigation.RecentlyAccessed defaultIsCollapsed />
+
+          <Navigation.Group
+            id="example_projet"
+            title="Minimal project"
+            icon="gear"
+            defaultIsCollapsed={false}
+          >
+            <Navigation.Group id="root">
+              <Navigation.Item id="item1" title="Get started" />
+              <Navigation.Item id="item2" title="Alerts" />
+              <Navigation.Item id="item3" title="Some children node">
+                <EuiText size="s">
+                  <EuiLink>Some children node</EuiLink>
+                </EuiText>
+              </Navigation.Item>
+            </Navigation.Group>
+
+            <Navigation.Group id="group:settings" title="Settings">
+              <Navigation.Item id="logs" title="Logs" />
+              <Navigation.Item id="signals" title="Signals" />
+              <Navigation.Item id="tracing" title="Tracing" />
+
+              <Navigation.Group id="sub:settings" title="More" defaultIsCollapsed={false}>
+                <Navigation.Item id="logs" title="Coolio" />
+              </Navigation.Group>
+            </Navigation.Group>
+          </Navigation.Group>
+        </Navigation>
+      </NavigationProvider>
+    </NavigationWrapper>
+  );
+};
+
+export const CreativeUI = (args: ChromeNavigationViewModel & NavigationServices) => {
+  const services = storybookMock.getServices({
+    ...args,
+    navLinks$: of(deepLinks),
+    onProjectNavigationChange: (updated) => {
+      action('Update chrome navigation')(JSON.stringify(updated, null, 2));
+    },
+    recentlyAccessed$: of([
+      { label: 'This is an example', link: '/app/example/39859', id: '39850' },
+      { label: 'Another example', link: '/app/example/5235', id: '5235' },
+    ]),
+  });
+
+  return (
+    <NavigationWrapper>
+      <NavigationProvider {...services}>
+        <Navigation homeRef="/" unstyled>
+          <EuiFlexGroup direction="column" css={{ backgroundColor: 'pink', height: '100%' }}>
+            <EuiFlexItem grow>
+              <EuiFlexGroup
+                direction="column"
+                gutterSize="none"
+                style={{ overflowY: 'auto' }}
+                justifyContent="spaceBetween"
+              >
+                <EuiFlexItem grow={false} css={{ padding: '12px' }}>
+                  <EuiTitle>
+                    <h3>Hello!</h3>
+                  </EuiTitle>
+
+                  <Navigation.Group id="myProject" title="My project" icon="logoObservability">
+                    <ul css={{ margin: '1em' }}>
+                      <Navigation.Item id="item1" title="Star wars movies">
+                        <li
+                          css={{
+                            marginLeft: '20px',
+                            listStyle: 'disc outside none',
+                            marginBottom: '8px',
+                          }}
+                        >
+                          <a href="/">Star wars movies</a>
+                        </li>
+                      </Navigation.Item>
+
+                      <Navigation.Item id="item2" title="My library">
+                        {(navNode) => {
+                          return (
+                            <li
+                              css={{
+                                marginLeft: '20px',
+                                listStyle: 'disc outside none',
+                                marginBottom: '8px',
+                              }}
+                            >
+                              <a href="/">From prop -- {navNode.title}</a>
+                            </li>
+                          );
+                        }}
+                      </Navigation.Item>
+
+                      <Navigation.Item id="item3" title="You must see this">
+                        <li
+                          css={{
+                            marginLeft: '20px',
+                            listStyle: 'disc outside none',
+                            marginBottom: '8px',
+                            color: 'yellow',
+                            fontWeight: 700,
+                          }}
+                        >
+                          You must see this
+                        </li>
+                      </Navigation.Item>
+                    </ul>
+                  </Navigation.Group>
+                  <p>
+                    <em>As you can see there is really no limit in what UI you can create!</em>
+                    <br />
+                  </p>
+                  <p css={{ marginTop: '24px', textAlign: 'center' }}>
+                    <EuiButton>Have fun!</EuiButton>
+                  </p>
+                </EuiFlexItem>
+
+                <EuiFlexItem
+                  grow={false}
+                  css={{
+                    borderTop: '2px solid yellow',
+                    padding: '20px',
+                    height: '160px',
+                    backgroundImage: `url('https://images.unsplash.com/photo-1606041008023-472dfb5e530f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2333&q=80')`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    color: 'white',
+                  }}
+                />
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+          <Navigation.CloudLink preset="deployments" />
         </Navigation>
       </NavigationProvider>
     </NavigationWrapper>
