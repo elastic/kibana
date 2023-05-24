3e59f55c73391391a10e966f56174e60099aa4d9
Tre' Seymour <wayne.seymour@elastic.co>
2023-05-24 13:35:16 +0100

    [CI] Fix cant find ftr configs yaml fle

--- packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
+++ packages/kbn-test/src/functional_test_runner/lib/config/ftr_configs_manifest.ts
@@ -8,7 +8,6 @@
 
 import Path from 'path';
 import Fs from 'fs';
-
 import { REPO_ROOT } from '@kbn/repo-info';
 import JsYaml from 'js-yaml';
 
@@ -25,10 +24,24 @@ interface FtrConfigsManifest {
   disabled: string[];
   enabled: Array<string | FtrConfigWithOptions>;
 }
+let ftrConfigsManifest: FtrConfigsManifest;
 
-const ftrConfigsManifest: FtrConfigsManifest = JsYaml.safeLoad(
-  Fs.readFileSync(Path.resolve(REPO_ROOT, FTR_CONFIGS_MANIFEST_REL), 'utf8')
-);
+const load = (x: string): FtrConfigsManifest => JsYaml.safeLoad(Fs.readFileSync(x, 'utf8'));
+try {
+  ftrConfigsManifest = load(Path.resolve(REPO_ROOT, FTR_CONFIGS_MANIFEST_REL));
+} catch (_) {
+  const abnormalPath = Path.resolve(REPO_ROOT, '../kibana', FTR_CONFIGS_MANIFEST_REL);
+  // eslint-disable-next-line no-console
+  console.error(`\n### Could not load ${FTR_CONFIGS_MANIFEST_REL} normally,
+(meaning we could not load it from ${REPO_ROOT}),
+Error: \n  ${_},
+Now we'll try to load it from ${abnormalPath}`);
+  try {
+    ftrConfigsManifest = load(abnormalPath);
+  } catch (e) {
+    throw new Error(`\n### Could not load ${abnormalPath} either, Error: \n  ${e}`);
+  }
+}
 
 export const FTR_CONFIGS_MANIFEST_PATHS = [
   Object.values(ftrConfigsManifest.enabled),
