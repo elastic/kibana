41b5b4ae8d5a9b30abc6cae7472e86bc33c1539a
nreese <reese.nathan@elastic.co>
2023-05-24 11:30:04 -0600

    revert changes to x-pack/plugins/maps/public/components/tooltip_selector/tooltip_selector.tsx

--- x-pack/plugins/maps/public/components/tooltip_selector/tooltip_selector.tsx
+++ x-pack/plugins/maps/public/components/tooltip_selector/tooltip_selector.tsx
@@ -191,8 +191,8 @@ export class TooltipSelector extends Component<Props, State> {
                 >
                   {(provided, state) => (
                     <div
-                      className={classNames({
-                        'mapToolbarOverlay__buttonIcon-filled': state.isDragging,
+                      className={classNames('mapTooltipSelector__propertyRow', {
+                        'mapTooltipSelector__propertyRow-isDragging': state.isDragging,
                         'mapTooltipSelector__propertyRow-isDraggingOver': snapshot.isDraggingOver,
                       })}
                     >
