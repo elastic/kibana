e96df9d351bdc521d14c6a7e27ecea0a3dcbb7be
James Gowdy <jgowdy@elastic.co>
2023-05-24 08:12:27 +0100

    adding type keyword

--- x-pack/test/functional/services/ml/common_api.ts
+++ x-pack/test/functional/services/ml/common_api.ts
@@ -6,9 +6,9 @@
  */
 
 import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
-import { ProvidedType } from '@kbn/test';
+import type { ProvidedType } from '@kbn/test';
 
-import { FtrProviderContext } from '../../ftr_provider_context';
+import type { FtrProviderContext } from '../../ftr_provider_context';
 
 const COMMON_REQUEST_HEADERS = {
   'kbn-xsrf': 'some-xsrf-token',
