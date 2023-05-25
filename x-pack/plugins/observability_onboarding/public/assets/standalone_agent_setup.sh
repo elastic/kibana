58394e72c41f030d89ec098f72f2d9af0a237c16
Oliver Gupte <oliver.gupte@elastic.co>
2023-05-25 16:32:18 -0400

    Setup script improvements for checking elastic-agent status

--- x-pack/plugins/observability_onboarding/public/assets/standalone_agent_setup.sh
+++ x-pack/plugins/observability_onboarding/public/assets/standalone_agent_setup.sh
@@ -7,7 +7,7 @@ updateStepProgress() {
   local STEPNAME="$1"
   local STATUS="$2" # "incomplete" | "complete" | "disabled" | "loading" | "warning" | "danger" | "current"
   curl --request GET \
-    --url "${API_ENDPOINT}/step/${STEPNAME}?status=${STATUS} 2023-05-24" \
+    --url "${API_ENDPOINT}/step/${STEPNAME}?status=${STATUS}" \
     --header "Authorization: ApiKey ${API_KEY_ENCODED}" \
     --header "Content-Type: application/json" \
     --header "kbn-xsrf: true" \
@@ -24,6 +24,7 @@ if [ "$?" -eq 0 ]; then
   updateStepProgress "ea-download" "complete"
 else
   updateStepProgress "ea-download" "danger"
+  echo "Failed to download Elastic Agent"
   exit 1
 fi
 
@@ -51,13 +52,34 @@ else
   exit 1
 fi
 
+waitForElasticAgentStatus() {
+  local MAX_RETRIES=10
+  local i=0
+  echo -n "."
+  elastic-agent status >/dev/null
+  local ELASTIC_AGENT_STATUS_EXIT_CODE="$?"
+  while [ "$ELASTIC_AGENT_STATUS_EXIT_CODE" -ne 0 ] && [ $i -le $MAX_RETRIES ]; do
+    sleep 1
+    echo -n "."
+    elastic-agent status >/dev/null
+    ELASTIC_AGENT_STATUS_EXIT_CODE="$?"
+    ((i++))
+  done
+  echo ""
+  return $ELASTIC_AGENT_STATUS_EXIT_CODE
+}
+
 echo "Checking Elastic Agent status"
 updateStepProgress "ea-status" "loading"
-sleep 4 # wait for Elastic agent to start
+waitForElasticAgentStatus
+if [[ "$?" -ne 0 ]]; then
+  updateStepProgress "ea-status" "warning"
+  exit 1
+fi
 ELASTIC_AGENT_STATE="$(elastic-agent status | grep -m1 State | sed 's/State: //')"
 ELASTIC_AGENT_MESSAGE="$(elastic-agent status | grep -m1 Message | sed 's/Message: //')"
 if [ "${ELASTIC_AGENT_STATE}" = "HEALTHY" ] && [ "${ELASTIC_AGENT_MESSAGE}" = "Running" ]; then
-  echo "Elastic Agent connected"
+  echo "Elastic Agent running"
   echo "Download and save configuration to /opt/Elastic/Agent/elastic-agent.yml"
   updateStepProgress "ea-status" "complete"
 else
