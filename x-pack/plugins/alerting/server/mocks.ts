eaf6a7acaae39da505a9ab960be4c969a7686bc4
Zacqary Xeper <zacqary.xeper@elastic.co>
2023-05-25 16:57:03 -0500

    Fix mock type

--- x-pack/plugins/alerting/server/mocks.ts
+++ x-pack/plugins/alerting/server/mocks.ts
@@ -56,6 +56,7 @@ const createShareStartMock = () => {
 const createStartMock = () => {
   const mock: jest.Mocked<PluginStartContract> = {
     listTypes: jest.fn(),
+    getType: jest.fn(),
     getAllTypes: jest.fn(),
     getAlertingAuthorizationWithRequest: jest.fn(),
     getRulesClientWithRequest: jest.fn().mockResolvedValue(rulesClientMock.create()),
