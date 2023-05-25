13317f3b91b33aaed1427db095bb9852129b5fd1
Matt Kime <matt@mattki.me>
2023-05-25 13:58:39 -0500

    better mocks for jest tests

--- src/plugins/saved_search/public/mocks.ts
+++ src/plugins/saved_search/public/mocks.ts
@@ -6,17 +6,39 @@
  * Side Public License, v 1.
  */
 
-import { searchSourceInstanceMock } from '@kbn/data-plugin/common/search/search_source/mocks';
+// import { searchSourceInstanceMock } from '@kbn/data-plugin/common/search/search_source/mocks';
+import { of } from 'rxjs';
+import { SearchSource, IKibanaSearchResponse } from '@kbn/data-plugin/public';
+import { SearchSourceDependencies } from '@kbn/data-plugin/common/search';
+import type { SearchResponse } from '@elastic/elasticsearch/lib/api/types';
+
+const createEmptySearchSource = jest.fn(() => {
+  const deps = {
+    getConfig: jest.fn(),
+  } as unknown as SearchSourceDependencies;
+  const searchSource = new SearchSource({}, deps);
+  searchSource.fetch$ = jest.fn().mockReturnValue(of({ rawResponse: { hits: { total: 2 } } }));
+  searchSource.createChild = jest.fn((options = {}) => {
+    const childSearchSource = new SearchSource({}, deps);
+    childSearchSource.setParent(searchSource, options);
+    childSearchSource.fetch$ = <T>() =>
+      of({ rawResponse: { hits: { hits: [] } } } as unknown as IKibanaSearchResponse<
+        SearchResponse<T>
+      >);
+    return childSearchSource;
+  });
+  return searchSource;
+});
 
 const savedSearchStartMock = () => ({
   get: jest.fn().mockImplementation(() => ({
     id: 'savedSearch',
     title: 'savedSearchTitle',
-    searchSource: searchSourceInstanceMock,
+    searchSource: createEmptySearchSource(), // searchSourceInstanceMock,
   })),
   getAll: jest.fn(),
   getNew: jest.fn().mockImplementation(() => ({
-    searchSource: searchSourceInstanceMock,
+    searchSource: createEmptySearchSource(), // searchSourceInstanceMock,
   })),
   save: jest.fn(),
   find: jest.fn(),
