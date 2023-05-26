b691fac31b96d8b4bf1adedc1718d84b32b4190e
Pablo Neves Machado <pablo.nevesmachado@elastic.co>
2023-05-26 10:43:07 +0200

    Create SecurityPage component to handle upsell pages

--- x-pack/plugins/security_solution/public/common/hooks/use_upselling.ts
+++ x-pack/plugins/security_solution/public/common/hooks/use_upselling.ts
@@ -9,10 +9,18 @@ import { useMemo } from 'react';
 import useObservable from 'react-use/lib/useObservable';
 import type { UpsellingSectionId } from '../lib/upsellings';
 import { useKibana } from '../lib/kibana';
+import type { SecurityPageName } from '../../../common';
 
 export const useUpsellingComponent = (id: UpsellingSectionId): React.ComponentType | null => {
   const { upselling } = useKibana().services;
   const upsellingSections = useObservable(upselling.sections$);
 
   return useMemo(() => upsellingSections?.get(id) ?? null, [id, upsellingSections]);
+};
+
+export const useUpsellingPage = (pageName: SecurityPageName): React.ComponentType | null => {
+  const { upselling } = useKibana().services;
+  const UpsellingPage = useMemo(() => upselling.getPageUpselling(pageName), [pageName, upselling]);
+
+  return UpsellingPage ?? null;
 };
