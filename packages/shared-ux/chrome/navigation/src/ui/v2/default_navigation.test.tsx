0132c59517b183772b38ac5e95074576353cb4af
Sébastien Loix <sabee77@gmail.com>
2023-05-26 12:18:54 +0100

    Fix tests

--- packages/shared-ux/chrome/navigation/src/ui/v2/default_navigation.test.tsx
+++ packages/shared-ux/chrome/navigation/src/ui/v2/default_navigation.test.tsx
@@ -28,112 +28,6 @@ const defaultProps = {
 describe('<DefaultNavigation />', () => {
   const services = getServicesMock();
 
-  describe('builds the full navigation tree when only custom project is provided', () => {
-    test('reading the title from config or deeplink', async () => {
-      const navLinks$: Observable<ChromeNavLink[]> = of([
-        {
-          id: 'item2',
-          title: 'Title from deeplink!',
-          baseUrl: '',
-          url: '',
-          href: '',
-        },
-      ]);
-
-      const onProjectNavigationChange = jest.fn();
-
-      // Custom project navigation tree definition
-      const projectNavigationTree: ProjectNavigationTreeDefinition = [
-        {
-          id: 'group1',
-          title: 'Group 1',
-          children: [
-            {
-              id: 'item1',
-              title: 'Item 1',
-            },
-            {
-              id: 'item2',
-              link: 'item2', // Title from deeplink
-            },
-            {
-              id: 'item3',
-              link: 'item2',
-              title: 'Deeplink title overriden', // Override title from deeplink
-            },
-            {
-              link: 'disabled',
-              title: 'Should NOT be there',
-            },
-          ],
-        },
-      ];
-
-      render(
-        <NavigationProvider
-          {...services}
-          navLinks$={navLinks$}
-          onProjectNavigationChange={onProjectNavigationChange}
-        >
-          <DefaultNavigation {...defaultProps} projectNavigationTree={projectNavigationTree} />
-        </NavigationProvider>
-      );
-
-      expect(onProjectNavigationChange).toHaveBeenCalled();
-      const lastCall =
-        onProjectNavigationChange.mock.calls[onProjectNavigationChange.mock.calls.length - 1];
-      const [navTreeGenerated] = lastCall;
-
-      expect(navTreeGenerated).toEqual({
-        homeRef: 'htts://elastic.co',
-        navigationTree: [
-          {
-            id: 'group1',
-            title: 'Group 1',
-            path: ['group1'],
-            children: [
-              {
-                id: 'item1',
-                title: 'Item 1',
-                path: ['group1', 'item1'],
-              },
-              {
-                id: 'item2',
-                path: ['group1', 'item2'],
-                title: 'Title from deeplink!',
-                deepLink: {
-                  id: 'item2',
-                  title: 'Title from deeplink!',
-                  baseUrl: '',
-                  url: '',
-                  href: '',
-                },
-              },
-              {
-                id: 'item3',
-                title: 'Deeplink title overriden',
-                path: ['group1', 'item3'],
-                deepLink: {
-                  id: 'item2',
-                  title: 'Title from deeplink!',
-                  baseUrl: '',
-                  url: '',
-                  href: '',
-                },
-              },
-            ],
-          },
-          ...defaultNavigationTree, // The default navigation tree is added at the end
-        ],
-      });
-    });
-
-    test.skip('does not render in the UI the nodes that points to unexisting deeplinks', async () => {
-      // TODO: This test will be added when we'll have the UI and be able to add
-      // data-test-subj to all the nodes with their paths
-    });
-  });
-
   describe('builds custom navigation tree', () => {
     test('render reference UI and build the navigation tree', async () => {
       const onProjectNavigationChange = jest.fn();
@@ -500,5 +394,112 @@ describe('<DefaultNavigation />', () => {
     //     ],
     //   });
     // });
+  });
+
+  describe('builds the full navigation tree when only custom project is provided', () => {
+    test('reading the title from config or deeplink', async () => {
+      const navLinks$: Observable<ChromeNavLink[]> = of([
+        {
+          id: 'item2',
+          title: 'Title from deeplink!',
+          baseUrl: '',
+          url: '',
+          href: '',
+        },
+      ]);
+
+      const onProjectNavigationChange = jest.fn();
+
+      // Custom project navigation tree definition
+      const projectNavigationTree: ProjectNavigationTreeDefinition = [
+        {
+          id: 'group1',
+          title: 'Group 1',
+          children: [
+            {
+              id: 'item1',
+              title: 'Item 1',
+            },
+            {
+              id: 'item2',
+              link: 'item2', // Title from deeplink
+            },
+            {
+              id: 'item3',
+              link: 'item2',
+              title: 'Deeplink title overriden', // Override title from deeplink
+            },
+            {
+              link: 'disabled',
+              title: 'Should NOT be there',
+            },
+          ],
+        },
+      ];
+
+      render(
+        <NavigationProvider
+          {...services}
+          navLinks$={navLinks$}
+          onProjectNavigationChange={onProjectNavigationChange}
+        >
+          <DefaultNavigation {...defaultProps} projectNavigationTree={projectNavigationTree} />
+        </NavigationProvider>
+      );
+
+      expect(onProjectNavigationChange).toHaveBeenCalled();
+      const lastCall =
+        onProjectNavigationChange.mock.calls[onProjectNavigationChange.mock.calls.length - 1];
+      const [navTreeGenerated] = lastCall;
+
+      expect(navTreeGenerated).toEqual({
+        homeRef: 'htts://elastic.co',
+        navigationTree: [
+          {
+            id: 'group1',
+            title: 'Group 1',
+            type: 'navGroup',
+            path: ['group1'],
+            children: [
+              {
+                id: 'item1',
+                title: 'Item 1',
+                path: ['group1', 'item1'],
+              },
+              {
+                id: 'item2',
+                path: ['group1', 'item2'],
+                title: 'Title from deeplink!',
+                deepLink: {
+                  id: 'item2',
+                  title: 'Title from deeplink!',
+                  baseUrl: '',
+                  url: '',
+                  href: '',
+                },
+              },
+              {
+                id: 'item3',
+                title: 'Deeplink title overriden',
+                path: ['group1', 'item3'],
+                deepLink: {
+                  id: 'item2',
+                  title: 'Title from deeplink!',
+                  baseUrl: '',
+                  url: '',
+                  href: '',
+                },
+              },
+            ],
+          },
+          ...defaultNavigationTree, // The default navigation tree is added at the end
+        ],
+      });
+    });
+
+    test.skip('does not render in the UI the nodes that points to unexisting deeplinks', async () => {
+      // TODO: This test will be added when we'll have the UI and be able to add
+      // data-test-subj to all the nodes with their paths
+    });
   });
 });
