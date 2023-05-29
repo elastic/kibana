425adedc0e04bd6cc7c32846adc9a041a2adf9c7
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2023-05-29 15:49:59 -0400

    [Fleet] Disable requirement for Fleet server with standalone fleet server

--- x-pack/plugins/fleet/server/routes/setup/handlers.ts
+++ x-pack/plugins/fleet/server/routes/setup/handlers.ts
@@ -19,10 +19,13 @@ export const getFleetStatusHandler: FleetRequestHandler = async (context, reques
       .getSecurity()
       .authc.apiKeys.areAPIKeysEnabled();
     const coreContext = await context.core;
-    const isFleetServerSetup = await hasFleetServers(
+    const isFleetServerMissing = !(await hasFleetServers(
       coreContext.elasticsearch.client.asInternalUser
-    );
+    ));
 
+    const isNotFleetServerStandalone = !(
+      appContextService.getConfig()?.internal?.fleetServerStandalone ?? false
+    );
     const missingRequirements: GetFleetStatusResponse['missing_requirements'] = [];
     const missingOptionalFeatures: GetFleetStatusResponse['missing_optional_features'] = [];
 
@@ -30,7 +33,7 @@ export const getFleetStatusHandler: FleetRequestHandler = async (context, reques
       missingRequirements.push('api_keys');
     }
 
-    if (!isFleetServerSetup) {
+    if (isNotFleetServerStandalone && isFleetServerMissing) {
       missingRequirements.push('fleet_server');
     }
 
