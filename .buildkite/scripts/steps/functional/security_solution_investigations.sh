423a51feae826050f321d107524dd33b856964f4
konrad.szwarc <konrad.szwarc@elastic.co>
2023-05-29 13:31:06 +0200

    separate investigations tests

--- .buildkite/scripts/steps/functional/security_solution_investigations.sh
+++ .buildkite/scripts/steps/functional/security_solution_investigations.sh
@@ -5,13 +5,13 @@ set -euo pipefail
 source .buildkite/scripts/steps/functional/common.sh
 
 export JOB=kibana-security-solution-chrome
-export CLI_NUMBER=${CLI_NUMBER:-$((BUILDKITE_PARALLEL_JOB+1))}
-export CLI_COUNT=${CLI_COUNT:-$BUILDKITE_PARALLEL_JOB_COUNT}
+export KIBANA_INSTALL_DIR=${KIBANA_BUILD_LOCATION}
+
 
 Xvfb :99 -screen 0 1600x1200x24 &
 
 export DISPLAY=:99
 
-echo "--- Security Solution tests (Chrome)"
+echo "--- Investigations Cypress Tests on Security Solution"
 
 yarn --cwd x-pack/plugins/security_solution cypress:investigations:run
