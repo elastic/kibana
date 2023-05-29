fd4759b93e031f6d7b66f6129199a8b591c480ae
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2023-05-29 10:29:28 +0300

    Add some unit tests

--- x-pack/plugins/lens/public/app_plugin/lens_top_nav.tsx
+++ x-pack/plugins/lens/public/app_plugin/lens_top_nav.tsx
@@ -1080,6 +1080,7 @@ export const LensTopNavMenu = ({
       dataViewPickerComponentProps={dataViewPickerProps}
       showDatePicker={
         indexPatterns.some((ip) => ip.isTimeBased()) ||
+        // always show the timepicker for text based languages
         isOnTextBasedMode ||
         Boolean(
           allLoaded &&
