b3c8b3340d751a5e38bbaf7d7ee7b0e8d27a4657
Kevin Qualters <kevin.qualters@elastic.co>
2023-05-30 14:54:07 +0000

    Update hot code path

--- x-pack/plugins/security_solution/public/common/containers/source/use_data_view.tsx
+++ x-pack/plugins/security_solution/public/common/containers/source/use_data_view.tsx
@@ -9,9 +9,9 @@ import { useCallback, useRef } from 'react';
 import type { Subscription } from 'rxjs';
 import { useDispatch } from 'react-redux';
 import memoizeOne from 'memoize-one';
-import type { BrowserField } from '@kbn/timelines-plugin/common';
+import type { BrowserField, BrowserFields } from '@kbn/timelines-plugin/common';
 import { getCategory } from '@kbn/triggers-actions-ui-plugin/public';
-import type { DataView } from '@kbn/data-views-plugin/public';
+import type { DataViewSpec } from '@kbn/data-views-plugin/public';
 
 import { useKibana } from '../../lib/kibana';
 import { sourcererActions } from '../../store/sourcerer';
@@ -48,32 +48,33 @@ interface DataViewInfo {
  * VERY mutatious on purpose to improve the performance of the transform.
  */
 export const getDataViewStateFromIndexFields = memoizeOne(
-  (_title: string, fields: DataView['fields'], _includeUnmapped: boolean = false): DataViewInfo => {
+  (
+    _title: string,
+    fields: DataViewSpec['fields'],
+    _includeUnmapped: boolean = false
+  ): DataViewInfo => {
     // Adds two dangerous casts to allow for mutations within this function
     type DangerCastForMutation = Record<string, {}>;
-
-    return fields.reduce<DataViewInfo>(
-      (acc, field) => {
-        // mutate browserFields
-        const category = getCategory(field.name);
-        if (acc.browserFields[category] == null) {
-          (acc.browserFields as DangerCastForMutation)[category] = {};
+    if (fields == null) {
+      return { browserFields: {} };
+    } else {
+      const browserFields: BrowserFields = {};
+      for (const [name, field] of Object.entries(fields)) {
+        const category = getCategory(name);
+        if (browserFields[category] == null) {
+          (browserFields as DangerCastForMutation)[category] = { fields: {} };
         }
-        if (acc.browserFields[category].fields == null) {
-          acc.browserFields[category].fields = {};
+        const categoryFields = browserFields[category].fields;
+        if (categoryFields) {
+          categoryFields[name] = field as BrowserField;
         }
-        acc.browserFields[category].fields[field.name] = field.spec as BrowserField;
-
-        return acc;
-      },
-      {
-        browserFields: {},
       }
-    );
+      return { browserFields: browserFields as DangerCastForBrowserFieldsMutation };
+    }
   },
   (newArgs, lastArgs) =>
     newArgs[0] === lastArgs[0] &&
-    newArgs[1].length === lastArgs[1].length &&
+    newArgs[1]?.length === lastArgs[1]?.length &&
     newArgs[2] === lastArgs[2]
 );
 
