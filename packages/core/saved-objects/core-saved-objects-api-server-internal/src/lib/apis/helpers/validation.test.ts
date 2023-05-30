69f80a82d91853e7bcb70c1d532dc9b53966f483
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2023-05-29 17:11:01 -0700

    Assert validation against virtual model version

--- packages/core/saved-objects/core-saved-objects-api-server-internal/src/lib/apis/helpers/validation.test.ts
+++ packages/core/saved-objects/core-saved-objects-api-server-internal/src/lib/apis/helpers/validation.test.ts
@@ -14,6 +14,7 @@ import { typedef, typedef1, typedef2 } from './validation_fixtures';
 import { SavedObjectTypeRegistry } from '@kbn/core-saved-objects-base-server-internal';
 
 const defaultVersion = '8.10.0';
+const modelVirtualVersion = '10.1.0';
 const typeA = 'my-typeA';
 const typeB = 'my-typeB';
 const typeC = 'my-typeC';
@@ -53,27 +54,32 @@ describe('Saved Objects type validation helper', () => {
 
   describe('validation helper', () => {
     beforeEach(() => {
-      helper = new ValidationHelper({
-        logger,
-        registry: typeRegistry,
-        kibanaVersion: defaultVersion,
-      });
       registerType(typeA, typedef);
       registerType(typeB, typedef1);
       registerType(typeC, typedef2);
     });
 
     it('should validate objects against stack versions', () => {
+      helper = new ValidationHelper({
+        logger,
+        registry: typeRegistry,
+        kibanaVersion: defaultVersion,
+      });
       const data = createMockObject(typeA, { attributes: { foo: 'hi', count: 1 } });
       expect(() => helper.validateObjectForCreate(typeA, data)).not.toThrowError();
     });
 
     it('should validate objects against model versions', () => {
+      helper = new ValidationHelper({
+        logger,
+        registry: typeRegistry,
+        kibanaVersion: modelVirtualVersion,
+      });
       const data = createMockObject(typeB, { attributes: { foo: 'hi', count: 1 } });
       expect(() => helper.validateObjectForCreate(typeB, data)).not.toThrowError();
     });
 
-    it('should fail validation against invalid objects', () => {
+    it('should fail validation against invalid objects when version requested does not support a field', () => {
       const validationError = new Error(
         '[attributes.count]: definition for this key is missing: Bad Request'
       );
