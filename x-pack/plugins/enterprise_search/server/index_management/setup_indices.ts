a5c79f95ea600f3608b959b136b9d0ccede6f86b
Chenhui Wang <chenhui.wang@elastic.co>
2023-05-30 10:50:36 +0800

    Add last_incremental_sync_scheduled_at to connector index mapping

--- x-pack/plugins/enterprise_search/server/index_management/setup_indices.ts
+++ x-pack/plugins/enterprise_search/server/index_management/setup_indices.ts
@@ -116,6 +116,7 @@ const connectorMappingsProperties: Record<string, MappingProperty> = {
   is_native: { type: 'boolean' },
   language: { type: 'keyword' },
   last_deleted_document_count: { type: 'long' },
+  last_incremental_sync_scheduled_at: { type: 'date' },
   last_indexed_document_count: { type: 'long' },
   last_seen: { type: 'date' },
   last_sync_error: { type: 'keyword' },
