cbbf4a3a6f138963c231558b05a2454d522625be
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-05-31 14:07:55 +0200

    [UnifiedFieldList] Fix more

--- x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_creation/components/configuration_step/configuration_step_form.tsx
+++ x-pack/plugins/ml/public/application/data_frame_analytics/pages/analytics_creation/components/configuration_step/configuration_step_form.tsx
@@ -20,7 +20,7 @@ import { debounce, cloneDeep } from 'lodash';
 
 import { Query } from '@kbn/data-plugin/common/query';
 import { ES_FIELD_TYPES } from '@kbn/field-types';
-import { FieldStatsServices } from '@kbn/unified-field-list-plugin/public';
+import { FieldStatsServices } from '@kbn/unified-field-list';
 import {
   type FieldSelectionItem,
   ANALYSIS_CONFIG_TYPE,
