c665c6ff54b05dade85f98ebe0c619661e243b90
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-05-31 19:36:59 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/plugins/unified_doc_viewer/public/components/doc_viewer_source/index.tsx
+++ src/plugins/unified_doc_viewer/public/components/doc_viewer_source/index.tsx
@@ -9,8 +9,8 @@
 import React from 'react';
 import type { DataView } from '@kbn/data-views-plugin/public';
 import { CodeEditor } from '@kbn/kibana-react-plugin/public';
-import { useEsDocSearch } from '../../hooks';
 import { DocViewerSource } from '@kbn/unified-doc-viewer';
+import { useEsDocSearch } from '../../hooks';
 
 export interface SourceViewerProps {
   id: string;
