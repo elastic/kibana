576e92b0b167a0677b7a2d0f0346e29ff6d5e498
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-05-31 14:14:32 +0200

    fix(logs-ui): type

--- x-pack/plugins/infra/public/components/log_stream/log_stream.story_decorators.tsx
+++ x-pack/plugins/infra/public/components/log_stream/log_stream.story_decorators.tsx
@@ -21,7 +21,7 @@ import {
   ISearchOptions,
 } from '@kbn/data-plugin/public';
 import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
-import { getLogViewResponsePayloadRT } from '../../../common/http_api/log_views';
+import { getLogViewResponsePayloadRT } from '../../../common/http_api/latest';
 import { defaultLogViewAttributes } from '../../../common/log_views';
 import {
   LogEntriesSearchResponsePayload,
@@ -144,7 +144,7 @@ export const decorateWithKibanaContext = <StoryFnReactReturnType extends React.R
   };
 
   const overlays = {
-    openFlyout: () => {},
+    openFlyout: () => { },
   };
 
   return (
