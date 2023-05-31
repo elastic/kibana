e59847f3c1990754def0a342d5125a8dfd387b7b
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-05-31 07:20:54 +0200

    [Osquery] Switch Osquery Cypress to run in parallel

--- .buildkite/scripts/steps/functional/osquery_cypress.sh
+++ .buildkite/scripts/steps/functional/osquery_cypress.sh
@@ -7,11 +7,12 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 node scripts/build_kibana_platform_plugins.js
 
+Xvfb :99 -screen 0 1600x1200x24 &
+
+export DISPLAY=:99
+
 export JOB=kibana-osquery-cypress
 
 echo "--- Osquery Cypress tests"
 
-node scripts/functional_tests \
-  --debug --bail \
-  --config x-pack/test/osquery_cypress/cli_config.ts
-
+yarn --cwd x-pack/plugins/security_solution cypress:run
