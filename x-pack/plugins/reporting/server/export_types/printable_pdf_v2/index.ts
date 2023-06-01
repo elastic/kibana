3153bee609a44e4c98732ad93e176ff6c101c329
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-06-01 20:05:42 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/reporting/server/export_types/printable_pdf_v2/index.ts
+++ x-pack/plugins/reporting/server/export_types/printable_pdf_v2/index.ts
@@ -40,7 +40,7 @@ import {
   REPORTING_TRANSACTION_TYPE,
   PDF_JOB_TYPE_V2,
 } from '../../../common/constants';
-import { JobParamsPDFV2, UrlOrUrlLocatorTuple } from '../../../common/types';
+import { JobParamsPDFV2 } from '../../../common/types';
 import { TaskPayloadPDFV2 } from '../../../common/types/export_types/printable_pdf_v2';
 import { ReportingConfigType } from '../../config';
 import { ReportingServerInfo } from '../../core';
