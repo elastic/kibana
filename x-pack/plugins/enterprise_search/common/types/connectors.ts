8e9fd97b3abeb23ac30385e8e991b31e20b2ad39
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-06-01 15:27:08 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/enterprise_search/common/types/connectors.ts
+++ x-pack/plugins/enterprise_search/common/types/connectors.ts
@@ -194,15 +194,15 @@ export interface SchedulingConfiguraton {
   access_control: {
     enabled: boolean;
     interval: string;
-  },
+  };
   incremental: {
     enabled: boolean;
     interval: string;
-  },
+  };
   full: {
     enabled: boolean;
     interval: string;
-  }
+  };
 }
 
 export interface Connector {
@@ -227,7 +227,7 @@ export interface Connector {
   last_synced: string | null;
   name: string;
   pipeline?: IngestPipelineParams | null;
-  scheduling: SchedulingConfiguraton
+  scheduling: SchedulingConfiguraton;
   service_type: string | null;
   status: ConnectorStatus;
   sync_now: boolean;
