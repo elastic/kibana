aeefd09e8a050a2d9d00349e0e3724fbfd6b6a4c
Faisal Kanout <faisal.kanout@elastic.co>
2023-06-01 17:48:32 +0200

    Fix i18n keys on server

--- x-pack/plugins/observability/server/lib/rules/threshold/messages.ts
+++ x-pack/plugins/observability/server/lib/rules/threshold/messages.ts
@@ -11,33 +11,39 @@ import { AlertStates, Comparator } from './types';
 import { UNGROUPED_FACTORY_KEY } from './utils';
 
 export const DOCUMENT_COUNT_I18N = i18n.translate(
-  'xpack.infra.metrics.alerting.threshold.documentCount',
+  'xpack.observability.threshold.rule.threshold.documentCount',
   {
     defaultMessage: 'Document count',
   }
 );
 
 export const CUSTOM_EQUATION_I18N = i18n.translate(
-  'xpack.infra.metrics.alerting.threshold.customEquation',
+  'xpack.observability.threshold.rule.threshold.customEquation',
   {
     defaultMessage: 'Custom equation',
   }
 );
 
 export const stateToAlertMessage = {
-  [AlertStates.ALERT]: i18n.translate('xpack.infra.metrics.alerting.threshold.alertState', {
+  [AlertStates.ALERT]: i18n.translate('xpack.observability.threshold.rule.threshold.alertState', {
     defaultMessage: 'ALERT',
   }),
-  [AlertStates.WARNING]: i18n.translate('xpack.infra.metrics.alerting.threshold.warningState', {
-    defaultMessage: 'WARNING',
-  }),
-  [AlertStates.NO_DATA]: i18n.translate('xpack.infra.metrics.alerting.threshold.noDataState', {
-    defaultMessage: 'NO DATA',
-  }),
-  [AlertStates.ERROR]: i18n.translate('xpack.infra.metrics.alerting.threshold.errorState', {
+  [AlertStates.WARNING]: i18n.translate(
+    'xpack.observability.threshold.rule.threshold.warningState',
+    {
+      defaultMessage: 'WARNING',
+    }
+  ),
+  [AlertStates.NO_DATA]: i18n.translate(
+    'xpack.observability.threshold.rule.threshold.noDataState',
+    {
+      defaultMessage: 'NO DATA',
+    }
+  ),
+  [AlertStates.ERROR]: i18n.translate('xpack.observability.threshold.rule.threshold.errorState', {
     defaultMessage: 'ERROR',
   }),
-  [AlertStates.OK]: i18n.translate('xpack.infra.metrics.alerting.threshold.okState', {
+  [AlertStates.OK]: i18n.translate('xpack.observability.threshold.rule.threshold.okState', {
     defaultMessage: 'OK [Recovered]',
   }),
 };
@@ -50,17 +56,17 @@ const recoveredComparatorToI18n = (
   threshold: number[],
   currentValue: number
 ) => {
-  const belowText = i18n.translate('xpack.infra.metrics.alerting.threshold.belowRecovery', {
+  const belowText = i18n.translate('xpack.observability.threshold.rule.threshold.belowRecovery', {
     defaultMessage: 'below',
   });
-  const aboveText = i18n.translate('xpack.infra.metrics.alerting.threshold.aboveRecovery', {
+  const aboveText = i18n.translate('xpack.observability.threshold.rule.threshold.aboveRecovery', {
     defaultMessage: 'above',
   });
   switch (comparator) {
     case Comparator.BETWEEN:
       return currentValue < threshold[0] ? belowText : aboveText;
     case Comparator.OUTSIDE_RANGE:
-      return i18n.translate('xpack.infra.metrics.alerting.threshold.betweenRecovery', {
+      return i18n.translate('xpack.observability.threshold.rule.threshold.betweenRecovery', {
         defaultMessage: 'between',
       });
     case Comparator.GT:
@@ -74,7 +80,7 @@ const recoveredComparatorToI18n = (
 
 const thresholdToI18n = ([a, b]: Array<number | string>) => {
   if (typeof b === 'undefined') return a;
-  return i18n.translate('xpack.infra.metrics.alerting.threshold.thresholdRange', {
+  return i18n.translate('xpack.observability.threshold.rule.threshold.thresholdRange', {
     defaultMessage: '{a} and {b}',
     values: { a, b },
   });
@@ -91,7 +97,7 @@ export const buildFiredAlertReason: (alertResult: {
   timeSize: number;
   timeUnit: TimeUnitChar;
 }) => string = ({ group, metric, comparator, threshold, currentValue, timeSize, timeUnit }) =>
-  i18n.translate('xpack.infra.metrics.alerting.threshold.firedAlertReason', {
+  i18n.translate('xpack.observability.threshold.rule.threshold.firedAlertReason', {
     defaultMessage:
       '{metric} is {currentValue} in the last {duration}{group}. Alert when {comparator} {threshold}.',
     values: {
@@ -112,7 +118,7 @@ export const buildRecoveredAlertReason: (alertResult: {
   threshold: Array<number | string>;
   currentValue: number | string;
 }) => string = ({ group, metric, comparator, threshold, currentValue }) =>
-  i18n.translate('xpack.infra.metrics.alerting.threshold.recoveredAlertReason', {
+  i18n.translate('xpack.observability.threshold.rule.threshold.recoveredAlertReason', {
     defaultMessage:
       '{metric} is now {comparator} a threshold of {threshold} (current value is {currentValue}) for {group}',
     values: {
@@ -134,7 +140,7 @@ export const buildNoDataAlertReason: (alertResult: {
   timeSize: number;
   timeUnit: string;
 }) => string = ({ group, metric, timeSize, timeUnit }) =>
-  i18n.translate('xpack.infra.metrics.alerting.threshold.noDataAlertReason', {
+  i18n.translate('xpack.observability.threshold.rule.threshold.noDataAlertReason', {
     defaultMessage: '{metric} reported no data in the last {interval}{group}',
     values: {
       metric,
@@ -144,7 +150,7 @@ export const buildNoDataAlertReason: (alertResult: {
   });
 
 export const buildErrorAlertReason = (metric: string) =>
-  i18n.translate('xpack.infra.metrics.alerting.threshold.errorAlertReason', {
+  i18n.translate('xpack.observability.threshold.rule.threshold.errorAlertReason', {
     defaultMessage: 'Elasticsearch failed when attempting to query data for {metric}',
     values: {
       metric,
@@ -152,7 +158,7 @@ export const buildErrorAlertReason = (metric: string) =>
   });
 
 export const buildInvalidQueryAlertReason = (filterQueryText: string) =>
-  i18n.translate('xpack.infra.metrics.alerting.threshold.queryErrorAlertReason', {
+  i18n.translate('xpack.observability.threshold.rule.threshold.queryErrorAlertReason', {
     defaultMessage: 'Alert is using a malformed KQL query: {filterQueryText}',
     values: {
       filterQueryText,
@@ -160,21 +166,21 @@ export const buildInvalidQueryAlertReason = (filterQueryText: string) =>
   });
 
 export const groupByKeysActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.groupByKeysActionVariableDescription',
+  'xpack.observability.threshold.rule.groupByKeysActionVariableDescription',
   {
     defaultMessage: 'The object containing groups that are reporting data',
   }
 );
 
 export const alertStateActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.alertStateActionVariableDescription',
+  'xpack.observability.threshold.rule.alertStateActionVariableDescription',
   {
     defaultMessage: 'Current state of the alert',
   }
 );
 
 export const alertDetailUrlActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.alertDetailUrlActionVariableDescription',
+  'xpack.observability.threshold.rule.alertDetailUrlActionVariableDescription',
   {
     defaultMessage:
       'Link to the view within Elastic that shows further details and context surrounding this alert',
@@ -182,21 +188,21 @@ export const alertDetailUrlActionVariableDescription = i18n.translate(
 );
 
 export const reasonActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.reasonActionVariableDescription',
+  'xpack.observability.threshold.rule.reasonActionVariableDescription',
   {
     defaultMessage: 'A concise description of the reason for the alert',
   }
 );
 
 export const timestampActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.timestampDescription',
+  'xpack.observability.threshold.rule.timestampDescription',
   {
     defaultMessage: 'A timestamp of when the alert was detected.',
   }
 );
 
 export const valueActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.valueActionVariableDescription',
+  'xpack.observability.threshold.rule.valueActionVariableDescription',
   {
     defaultMessage:
       'The value of the metric in the specified condition. Usage: (ctx.value.condition0, ctx.value.condition1, etc...).',
@@ -204,7 +210,7 @@ export const valueActionVariableDescription = i18n.translate(
 );
 
 export const metricActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.metricActionVariableDescription',
+  'xpack.observability.threshold.rule.metricActionVariableDescription',
   {
     defaultMessage:
       'The metric name in the specified condition. Usage: (ctx.metric.condition0, ctx.metric.condition1, etc...).',
@@ -212,7 +218,7 @@ export const metricActionVariableDescription = i18n.translate(
 );
 
 export const thresholdActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.thresholdActionVariableDescription',
+  'xpack.observability.threshold.rule.thresholdActionVariableDescription',
   {
     defaultMessage:
       'The threshold value of the metric for the specified condition. Usage: (ctx.threshold.condition0, ctx.threshold.condition1, etc...).',
@@ -220,7 +226,7 @@ export const thresholdActionVariableDescription = i18n.translate(
 );
 
 export const viewInAppUrlActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.viewInAppUrlActionVariableDescription',
+  'xpack.observability.threshold.rule.viewInAppUrlActionVariableDescription',
   {
     defaultMessage:
       'Link to the view or feature within Elastic that can assist with further investigation',
@@ -228,49 +234,49 @@ export const viewInAppUrlActionVariableDescription = i18n.translate(
 );
 
 export const cloudActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.cloudActionVariableDescription',
+  'xpack.observability.threshold.rule.cloudActionVariableDescription',
   {
     defaultMessage: 'The cloud object defined by ECS if available in the source.',
   }
 );
 
 export const hostActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.hostActionVariableDescription',
+  'xpack.observability.threshold.rule.hostActionVariableDescription',
   {
     defaultMessage: 'The host object defined by ECS if available in the source.',
   }
 );
 
 export const containerActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.containerActionVariableDescription',
+  'xpack.observability.threshold.rule.containerActionVariableDescription',
   {
     defaultMessage: 'The container object defined by ECS if available in the source.',
   }
 );
 
 export const orchestratorActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.orchestratorActionVariableDescription',
+  'xpack.observability.threshold.rule.orchestratorActionVariableDescription',
   {
     defaultMessage: 'The orchestrator object defined by ECS if available in the source.',
   }
 );
 
 export const labelsActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.labelsActionVariableDescription',
+  'xpack.observability.threshold.rule.labelsActionVariableDescription',
   {
     defaultMessage: 'List of labels associated with the entity where this alert triggered.',
   }
 );
 
 export const tagsActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.tagsActionVariableDescription',
+  'xpack.observability.threshold.rule.tagsActionVariableDescription',
   {
     defaultMessage: 'List of tags associated with the entity where this alert triggered.',
   }
 );
 
 export const originalAlertStateActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.originalAlertStateActionVariableDescription',
+  'xpack.observability.threshold.rule.originalAlertStateActionVariableDescription',
   {
     defaultMessage:
       'The state of the alert before it recovered. This is only available in the recovery context',
@@ -278,7 +284,7 @@ export const originalAlertStateActionVariableDescription = i18n.translate(
 );
 
 export const originalAlertStateWasActionVariableDescription = i18n.translate(
-  'xpack.infra.metrics.alerting.originalAlertStateWasWARNINGActionVariableDescription',
+  'xpack.observability.threshold.rule.originalAlertStateWasWARNINGActionVariableDescription',
   {
     defaultMessage:
       'Boolean value of the state of the alert before it recovered. This can be used for template conditions. This is only available in the recovery context',
