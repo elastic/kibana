c9928740334fd5f0e908967b06275a9f10ae809b
Drew Tate <andrew.tate@elastic.co>
2023-06-02 13:50:24 -0500

    update snapshot

--- src/plugins/event_annotation/public/components/table_list.test.tsx
+++ src/plugins/event_annotation/public/components/table_list.test.tsx
@@ -89,7 +89,7 @@ describe('annotation list view', () => {
   it('renders a table list view', () => {
     expect(wrapper.debug()).toMatchInlineSnapshot(`
       "<Fragment>
-        <Memo(TableListComp) refreshListBouncer={false} tableCaption=\\"Annotation Library\\" findItems={[Function (anonymous)]} deleteItems={[Function (anonymous)]} editItem={[Function (anonymous)]} listingLimit={30} initialPageSize={10} initialFilter=\\"\\" entityName=\\"annotation group\\" entityNamePlural=\\"annotation groups\\" onClickTitle={[Function (anonymous)]} onFetchSuccess={[Function: onFetchSuccess]} />
+        <Memo(TableListComp) refreshListBouncer={false} tableCaption=\\"Annotation Library\\" findItems={[Function (anonymous)]} deleteItems={[Function (anonymous)]} editItem={[Function (anonymous)]} listingLimit={30} initialPageSize={10} initialFilter=\\"\\" entityName=\\"annotation group\\" entityNamePlural=\\"annotation groups\\" onClickTitle={[Function (anonymous)]} onFetchSuccess={[Function: onFetchSuccess]} setPageDataTestSubject={[Function: setPageDataTestSubject]} />
       </Fragment>"
     `);
   });
