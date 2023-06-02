937c1d237565322aff187128a066601b996c0570
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-06-02 20:36:10 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- src/plugins/discover/public/application/doc/locator.test.ts
+++ src/plugins/discover/public/application/doc/locator.test.ts
@@ -45,7 +45,7 @@ describe('Discover single doc url generator', () => {
       `"#/doc/c367b774-a4c2-11ea-bb37-0242ac130002/mock-row-index?id=id%20with%20special%20characters%3A%20%26%3F%23%2B"`
     );
   });
-  
+
   test('can specify profile', async () => {
     const { locator } = await setup();
     const { path } = await locator.getLocation({
