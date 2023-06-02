dcafe2a92f6b36a737eb5726214276d114efd46f
lgestc <lukasz.gmys@elastic.co>
2023-06-02 08:30:42 +0200

    correlation details test

---
+++ x-pack/plugins/security_solution/public/flyout/left/components/correlations_details/correlations_details.test.tsx
@@ -0,0 +1,108 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { useTimelineEventsDetails } from '../../../../timelines/containers/details';
+import { useSourcererDataView } from '../../../../common/containers/sourcerer';
+import { useCorrelations, type UseCorrelationsResult } from '../../../right/hooks/use_correlations';
+import { CorrelationsDetails } from './correlations_details';
+import { type CasesByAlertId, CaseStatuses } from '@kbn/cases-plugin/common/api';
+import type { SelectedDataView } from '../../../../common/store/sourcerer/model';
+import { TestProviders } from '../../../../common/mock';
+import { LeftFlyoutContext } from '../../context';
+
+jest.mock('../../../../timelines/containers/details');
+jest.mock('../../../../common/containers/sourcerer');
+jest.mock('../../../right/hooks/use_correlations');
+
+const mockCasesByAlertId: CasesByAlertId = [
+  {
+    id: '123',
+    title: 'Mock Case',
+    description: 'This is a mock case for testing purposes',
+    status: CaseStatuses.open,
+    createdAt: '2021-10-01T12:00:00Z',
+    totals: {
+      alerts: 5,
+      userComments: 2,
+    },
+  },
+];
+
+const mockUseCorrelationsResult: UseCorrelationsResult = {
+  loading: false,
+  error: false,
+  data: [],
+  dataCount: 0,
+  alertsBySessionIds: ['alert1', 'alert2', 'alert3'],
+  sameSourceAlertsIds: ['alert1', 'alert2'],
+  ancestryAlertsIds: ['alert3'],
+  cases: mockCasesByAlertId,
+};
+
+const contextValue = {
+  indexName: 'index',
+  eventId: 'event',
+  getFieldsData: () => {},
+  dataFormattedForFieldBrowser: [],
+};
+
+const renderCorrelationDetails = () => {
+  return render(
+    <TestProviders>
+      <LeftFlyoutContext.Provider value={contextValue}>
+        <CorrelationsDetails />
+      </LeftFlyoutContext.Provider>
+    </TestProviders>
+  );
+};
+
+describe('CorrelationsDetails', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    jest
+      .mocked(useSourcererDataView)
+      .mockReturnValue({ runtimeMappings: {} } as unknown as SelectedDataView);
+  });
+
+  it('renders loading spinner when data is loading', () => {
+    jest
+      .mocked(useTimelineEventsDetails)
+      .mockReturnValue([true, null, undefined, null, async () => {}]);
+    jest.mocked(useCorrelations).mockReturnValue(mockUseCorrelationsResult);
+
+    renderCorrelationDetails();
+
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();
+  });
+
+  it('renders null when there is an error', () => {
+    jest
+      .mocked(useTimelineEventsDetails)
+      .mockReturnValue([false, null, undefined, null, async () => {}]);
+    jest.mocked(useCorrelations).mockReturnValue({ ...mockUseCorrelationsResult, error: true });
+
+    const { container } = renderCorrelationDetails();
+
+    expect(container.firstChild).toBeNull();
+  });
+
+  it('renders alerts tables when data is loaded', () => {
+    jest
+      .mocked(useTimelineEventsDetails)
+      .mockReturnValue([false, null, undefined, null, async () => {}]);
+    jest.mocked(useCorrelations).mockReturnValue(mockUseCorrelationsResult);
+
+    renderCorrelationDetails();
+
+    expect(screen.getByText('1 alerts related by ancestry')).toBeInTheDocument();
+    expect(screen.getByText('2 alerts related by source event')).toBeInTheDocument();
+    expect(screen.getByText('3 alerts related by session')).toBeInTheDocument();
+  });
+});
