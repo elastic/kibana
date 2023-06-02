7bdc7532f4e185a4bcb187d28118e94945b08365
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-06-02 17:14:46 +0200

    add forgotten -- to link param

--- .buildkite/scripts/steps/build_api_docs.sh
+++ .buildkite/scripts/steps/build_api_docs.sh
@@ -11,7 +11,7 @@ echo "--- Run scripts/type_check to ensure that all build available"
 
 if is_pr_with_label "ci:link-docs"; then
   echo "--- Link API Docs"
-  node --max-old-space-size=12000 scripts/build_api_docs link
+  node --max-old-space-size=12000 scripts/build_api_docs --link
 
   check_for_changed_files "scripts/build_api_docs link" true
 fi
