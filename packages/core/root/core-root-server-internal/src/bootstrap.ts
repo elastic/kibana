570f866efff1de3778620d08249add51c06c48ca
Alex Szabo <alex.szabo@elastic.co>
2023-06-05 15:01:57 +0200

    refactor: remove log line that was for integration tests

--- packages/core/root/core-root-server-internal/src/bootstrap.ts
+++ packages/core/root/core-root-server-internal/src/bootstrap.ts
@@ -50,8 +50,6 @@ export async function bootstrap({ configs, cliArgs, applyConfigOverrides }: Boot
   const root = new Root(rawConfigService, env, onRootShutdown);
   const cliLogger = root.logger.get('cli');
 
-  cliLogger.info('Configurations parsed in this order: ' + env.configs.join(', '));
-
   process.on('SIGHUP', () => reloadConfiguration());
 
   // This is only used by the LogRotator service
