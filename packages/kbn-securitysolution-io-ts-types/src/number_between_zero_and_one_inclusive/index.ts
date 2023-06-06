28cd88efe614e3b1c6a28aea2d33f0e01d1e154d
Ryland Herrick <ryalnd@gmail.com>
2023-06-06 17:05:15 -0500

    Adds a type representing probabilities/weights
    We want to be able to specify a number between 0 and 1 for EA
    weightings, but I suspect this will be more broadly useful.

---
+++ packages/kbn-securitysolution-io-ts-types/src/number_between_zero_and_one_inclusive/index.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import * as t from 'io-ts';
+import { Either } from 'fp-ts/lib/Either';
+
+/**
+ * Types a number between 0 and 1 inclusive. Useful for specifying a probability, weighting, etc.
+ */
+export const NumberBetweenZeroAndOneInclusive = new t.Type<number, number, unknown>(
+  'NumberBetweenZeroAndOneInclusive',
+  t.number.is,
+  (input, context): Either<t.Errors, number> => {
+    return typeof input === 'number' &&
+      !Number.isNaN(input) &&
+      Number.isFinite(input) &&
+      input >= 0 &&
+      input <= 1
+      ? t.success(input)
+      : t.failure(input, context);
+  },
+  t.identity
+);
