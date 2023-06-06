167986ab067eab2598f9e7384e5dfe693eab45f1
Dima Arnautov <dmitrii.arnautov@elastic.co>
2023-06-06 16:44:17 +0200

    recognize routes

--- x-pack/plugins/ml/public/application/jobs/new_job/recognize/resolvers.ts
+++ x-pack/plugins/ml/public/application/jobs/new_job/recognize/resolvers.ts
@@ -5,60 +5,8 @@
  * 2.0.
  */
 
-import { i18n } from '@kbn/i18n';
-import { getToastNotifications, getSavedObjectsClient } from '../../../util/dependency_cache';
-import { ml } from '../../../services/ml_api_service';
+import { getSavedObjectsClient } from '../../../util/dependency_cache';
 import { KibanaObjects } from './page';
-import { NavigateToPath } from '../../../contexts/kibana';
-import { CreateLinkWithUserDefaults } from '../../../components/custom_hooks/use_create_ad_links';
-
-/**
- * Checks whether the jobs in a data recognizer module have been created.
- * Redirects to the Anomaly Explorer to view the jobs if they have been created,
- * or the recognizer job wizard for the module if not.
- */
-export function checkViewOrCreateJobs(
-  moduleId: string,
-  dataViewId: string,
-  createLinkWithUserDefaults: CreateLinkWithUserDefaults,
-  navigateToPath: NavigateToPath
-): Promise<any> {
-  return new Promise((resolve, reject) => {
-    // Load the module, and check if the job(s) in the module have been created.
-    // If so, load the jobs in the Anomaly Explorer.
-    // Otherwise open the data recognizer wizard for the module.
-    // Always want to call reject() so as not to load original page.
-    ml.dataRecognizerModuleJobsExist({ moduleId })
-      .then(async (resp: any) => {
-        if (resp.jobsExist === true) {
-          // also honor user's time filter setting in Advanced Settings
-          const url = createLinkWithUserDefaults('explorer', resp.jobs);
-          await navigateToPath(url);
-          reject();
-        } else {
-          await navigateToPath(`/jobs/new_job/recognize?id=${moduleId}&index=${dataViewId}`);
-          reject();
-        }
-      })
-      .catch(async (err: Error) => {
-        // eslint-disable-next-line no-console
-        console.error(`Error checking whether jobs in module ${moduleId} exists`, err);
-        const toastNotifications = getToastNotifications();
-        toastNotifications.addWarning({
-          title: i18n.translate('xpack.ml.newJob.recognize.moduleCheckJobsExistWarningTitle', {
-            defaultMessage: 'Error checking module {moduleId}',
-            values: { moduleId },
-          }),
-          text: i18n.translate('xpack.ml.newJob.recognize.moduleCheckJobsExistWarningDescription', {
-            defaultMessage:
-              'An error occurred trying to check whether the jobs in the module have been created.',
-          }),
-        });
-        await navigateToPath(`/jobs`);
-        reject();
-      });
-  });
-}
 
 /**
  * Gets kibana objects with an existence check.
