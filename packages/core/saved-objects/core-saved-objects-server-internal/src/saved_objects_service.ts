e3f249e6800bffb7c85097f3fbf0c50099b386d8
Jean-Louis Leysens <jloleysens@gmail.com>
2023-06-06 16:12:02 +0200

    Revert "remove fake failure"
    This reverts commit 73565cd964cb2413cfce3f1817bc6c8ea87921d1.

--- packages/core/saved-objects/core-saved-objects-server-internal/src/saved_objects_service.ts
+++ packages/core/saved-objects/core-saved-objects-server-internal/src/saved_objects_service.ts
@@ -266,13 +266,13 @@ export class SavedObjectsService
         take(1)
       ).toPromise();
 
-      // this.logger.info('!!!!!!!About to start migrations!!!!!!!');
-      // // Pretend to be busy...
-      // await new Promise((res) => {
-      //   setTimeout(res, 1_000 * 60 * 5);
-      // });
+      this.logger.info('!!!!!!!About to start migrations!!!!!!!');
+      // Pretend to be busy...
+      await new Promise((res) => {
+        setTimeout(res, 1_000 * 60 * 5);
+      });
 
-      // throw new Error('!!!!!FAKE FAILURE!!!!!');
+      throw new Error('!!!!!FAKE FAILURE!!!!!');
 
       // Running migrations only if we got compatible nodes.
       // It may happen that the observable completes due to Kibana shutting down
