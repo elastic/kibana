e7cf629bfbbef07520971d655f9eb7178fe75842
Justin Kambic <jk@elastic.co>
2023-06-07 15:16:54 -0400

    Remove testing logging and fix reference to SO attributes.

--- x-pack/plugins/synthetics/e2e/synthetics/journeys/project_monitor_read_only.journey.ts
+++ x-pack/plugins/synthetics/e2e/synthetics/journeys/project_monitor_read_only.journey.ts
@@ -65,8 +65,6 @@ journey('ProjectMonitorReadOnly', async ({ page, params }) => {
     // hash is always reset to empty string when monitor is edited
     // this ensures that when the monitor is pushed again, the monitor
     // config in the process takes precedence
-    console.log('the new config', JSON.stringify(newConfiguration, null, 2));
-    console.log('the original config', JSON.stringify(originalMonitorConfiguration, null, 2));
     expect(newConfiguration).toEqual({
       ...originalMonitorConfiguration,
       hash: '',
