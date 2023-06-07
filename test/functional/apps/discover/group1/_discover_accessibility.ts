945f25616ebb62df05a4a5f75369af6e701bd934
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-06-07 09:08:44 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- test/functional/apps/discover/group1/_discover_accessibility.ts
+++ test/functional/apps/discover/group1/_discover_accessibility.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 
 import { WebElementWrapper } from '../../../services/lib/web_element_wrapper';
 import { FtrProviderContext } from '../ftr_provider_context';
