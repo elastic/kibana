90b269dda2f88839d578294c1bd8ae8c41a729f8
nickofthyme <nicholas.partridge@elastic.co>
2023-06-07 14:33:40 -0700

    fix frt error

--- test/functional/apps/visualize/replaced_vislib_chart_types/_line_chart_split_chart.ts
+++ test/functional/apps/visualize/replaced_vislib_chart_types/_line_chart_split_chart.ts
@@ -223,7 +223,18 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         await PageObjects.visEditor.changeYAxisFilterLabelsCheckbox(axisId, true);
         await PageObjects.visEditor.clickGo(true);
         const labels = await PageObjects.visChart.getYAxisLabels(xyChartSelector);
-        const expectedLabels = ['0', '2,000', '4,000', '6,000', '8,000'];
+        const expectedLabels = [
+          '0',
+          '1,000',
+          '2,000',
+          '3,000',
+          '4,000',
+          '5,000',
+          '6,000',
+          '7,000',
+          '8,000',
+          '9,000',
+        ];
         expect(labels).to.eql(expectedLabels);
       });
 
