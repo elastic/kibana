508ec463ae9101ef67d5a33066be4f30dbd4533c
adcoelho <antonio.coelho@elastic.co>
2023-06-07 15:27:42 +0200

    Removed an unnecessary mock.

--- x-pack/test/cases_api_integration/common/lib/mock.ts
+++ x-pack/test/cases_api_integration/common/lib/mock.ts
@@ -38,7 +38,6 @@ export const postCaseReq: CasePostRequest = {
   title: 'Super Bad Security Issue',
   tags: ['defacement'],
   severity: CaseSeverity.LOW,
-  category: 'foobar',
   connector: {
     id: 'none',
     name: 'none',
