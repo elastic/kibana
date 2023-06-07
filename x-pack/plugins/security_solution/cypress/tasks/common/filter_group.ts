4fec748235f3e1191fc741e228dac133fb6832e1
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-06-07 10:13:01 +0200

    fix investigation filter_group test

--- x-pack/plugins/security_solution/cypress/tasks/common/filter_group.ts
+++ x-pack/plugins/security_solution/cypress/tasks/common/filter_group.ts
@@ -1,8 +1,9 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import {
@@ -51,7 +52,8 @@ export const editFilterGroupControls = () => {
 
 export const cancelFieldEditing = () => {
   cy.get(FILTER_GROUP_EDIT_CONTROLS_PANEL).should('be.visible');
-  cy.get(FILTER_GROUP_EDIT_CONTROL_PANEL_ITEMS.CANCEL).should('be.visible').trigger('click');
+  cy.get(FILTER_GROUP_EDIT_CONTROL_PANEL_ITEMS.CANCEL).click();
+  cy.get(FILTER_GROUP_CONTROL_CONFIRM_BTN).click();
   cy.get(FILTER_GROUP_EDIT_CONTROLS_PANEL).should('not.exist');
 };
 
