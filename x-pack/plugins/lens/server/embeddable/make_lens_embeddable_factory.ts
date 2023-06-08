c6c8b49e14339ba2630434fab507f0f3a92cf2ed
dej611 <dej611@gmail.com>
2023-06-08 16:24:27 +0200

    :fire: Last bit of unused migration

--- x-pack/plugins/lens/server/embeddable/make_lens_embeddable_factory.ts
+++ x-pack/plugins/lens/server/embeddable/make_lens_embeddable_factory.ts
@@ -34,7 +34,6 @@ import {
   commonMigratePartitionChartGroups,
   commonMigratePartitionMetrics,
   commonMigrateIndexPatternDatasource,
-  commonMigrateMetricFormatter,
 } from '../migrations/common_migrations';
 import {
   CustomVisualizationMigrations,
