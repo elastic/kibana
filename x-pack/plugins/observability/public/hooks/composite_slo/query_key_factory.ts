8187bb58f365ae74284b46ab0ad6044a49263eeb
Chris Cowan <chris@elastic.co>
2023-06-08 14:25:56 -0600

    removing the compositeSlo prefix from the keys

--- x-pack/plugins/observability/public/hooks/composite_slo/query_key_factory.ts
+++ x-pack/plugins/observability/public/hooks/composite_slo/query_key_factory.ts
@@ -13,16 +13,16 @@ interface CompositeSloKeyFilter {
 
 export const compositeSloKeys = {
   all: ['compositeSlo'] as const,
-  lists: () => [...compositeSloKeys.all, 'compositeSloList'] as const,
+  lists: () => [...compositeSloKeys.all, 'list'] as const,
   list: (filters: CompositeSloKeyFilter) => [...compositeSloKeys.lists(), filters] as const,
-  details: () => [...compositeSloKeys.all, 'compositeSloDetails'] as const,
+  details: () => [...compositeSloKeys.all, 'details'] as const,
   detail: (sloId?: string) => [...compositeSloKeys.details(), sloId] as const,
-  rules: () => [...compositeSloKeys.all, 'compositeSloRules'] as const,
+  rules: () => [...compositeSloKeys.all, 'rules'] as const,
   rule: (sloIds: string[]) => [...compositeSloKeys.rules(), sloIds] as const,
-  activeAlerts: () => [...compositeSloKeys.all, 'compositeSloActiveAlerts'] as const,
+  activeAlerts: () => [...compositeSloKeys.all, 'activeAlerts'] as const,
   activeAlert: (sloIds: string[]) => [...compositeSloKeys.activeAlerts(), sloIds] as const,
-  historicalSummaries: () => [...compositeSloKeys.all, 'compositeSloHistoricalSummary'] as const,
+  historicalSummaries: () => [...compositeSloKeys.all, 'historicalSummary'] as const,
   historicalSummary: (sloIds: string[]) =>
     [...compositeSloKeys.historicalSummaries(), sloIds] as const,
-  globalDiagnosis: () => [...compositeSloKeys.all, 'compositeSloGlobalDiagnosis'] as const,
+  globalDiagnosis: () => [...compositeSloKeys.all, 'globalDiagnosis'] as const,
 };
