922a97be00e50826666d25fd9ae3c3fc722b1fbb
Abdon Pijpelink <abdon.pijpelink@elastic.co>
2023-06-08 15:48:05 +0200

    Add IN operator and AUTO_BUCKET function

--- packages/kbn-text-based-editor/src/esql_documentation_sections.tsx
+++ packages/kbn-text-based-editor/src/esql_documentation_sections.tsx
@@ -517,15 +517,21 @@ FROM employees
 \`\`\`
 
 #### Operators
-These comparison operators are supported:
+These binary comparison operators are supported:
 
 * equality: \`==\`
 * inequality: \`!=\`
-* comparison:
-  * less than: \`<\`
-  * less than or equal: \`<=\`
-  * larger than: \`>\`
-  * larger than or equal: \`>=\`
+* less than: \`<\`
+* less than or equal: \`<=\`
+* larger than: \`>\`
+* larger than or equal: \`>=\`
+
+The \`IN\` operator allows testing whether a field or expression equals an element in a list of literals, fields or expressions:
+
+\`\`\`
+ROW a = 1, b = 4, c = 3
+| WHERE c-a IN (3, b / 2, a)
+\`\`\`
 
 For string comparison using wildcards or regular expressions, use \`LIKE\` or \`RLIKE\`:
 
@@ -603,6 +609,81 @@ FROM employees
 | PROJECT first_name, last_name, height
 | EVAL abs_height = ABS(0.0 - height)
 \`\`\`
+              `,
+              description:
+                'Text is in markdown. Do not translate function names, special characters, or field names like sum(bytes)',
+            }
+          )}
+        />
+      ),
+    },
+    {
+      label: i18n.translate(
+        'textBasedEditor.query.textBasedLanguagesEditor.documentationESQL.absFunction',
+        {
+          defaultMessage: 'AUTO_BUCKET',
+        }
+      ),
+      description: (
+        <Markdown
+          markdown={i18n.translate(
+            'textBasedEditor.query.textBasedLanguagesEditor.documentationESQL.absFunction.markdown',
+            {
+              defaultMessage: `### AUTO_BUCKET
+Creates human-friendly buckets and returns a \`datetime\` value for each row that corresponds to the resulting bucket the row falls into. Combine \`AUTO_BUCKET\`with \`STATS ... BY\` to create a date histogram.
+
+You provide a target number of buckets, a start date, and an end date, and it picks an appropriate bucket size to generate the target number of buckets or fewer. For example, this asks for at most 20 buckets over a whole year, which picks monthly buckets:
+
+\`\`\`
+ROW date=TO_DATETIME("1985-07-09T00:00:00.000Z")
+| EVAL bucket=AUTO_BUCKET(date, 20, "1985-01-01T00:00:00Z", "1986-01-01T00:00:00Z")
+\`\`\`
+
+Returning:
+\`\`\`
+1985-07-09T00:00:00.000Z | 1985-07-01T00:00:00.000Z
+\`\`\`
+
+The goal isn't to provide *exactly* the target number of buckets, it's to pick a
+range that people are comfortable with that provides at most the target number of
+buckets.
+
+If you ask for more buckets then \`AUTO_BUCKET\` can pick a smaller range. For example,
+asking for at most 100 buckets in a year will get you week long buckets:
+
+\`\`\`
+ROW date=TO_DATETIME("1985-07-09T00:00:00.000Z")
+| EVAL bucket=AUTO_BUCKET(date, 100, "1985-01-01T00:00:00Z", "1986-01-01T00:00:00Z")
+\`\`\`
+
+Returning:
+\`\`\`
+1985-07-09T00:00:00.000Z | 1985-07-08T00:00:00.000Z
+\`\`\`
+
+\`AUTO_BUCKET\` does not filter any rows. It only uses the provided time range to pick a good bucket size. For rows with a date outside of the range, it returns a datetime that corresponds to a bucket outside the range. Combine \`AUTO_BUCKET\` with \`WHERE\` to filter rows.
+
+A more complete example might look like:
+
+\`\`\`
+FROM employees
+| WHERE hire_date >= "1985-01-01T00:00:00Z" AND hire_date < "1986-01-01T00:00:00Z"
+| EVAL bucket = AUTO_BUCKET(hire_date, 20, "1985-01-01T00:00:00Z", "1986-01-01T00:00:00Z")
+| STATS AVG(salary) BY bucket
+| SORT bucket
+\`\`\`
+
+Returning:
+\`\`\`
+46305.0 | 1985-02-01T00:00:00.000Z
+44817.0 | 1985-05-01T00:00:00.000Z
+62405.0 | 1985-07-01T00:00:00.000Z
+49095.0 | 1985-09-01T00:00:00.000Z
+51532.0 | 1985-10-01T00:00:00.000Z
+54539.75 | 1985-11-01T00:00:00.000
+\`\`\`
+
+NOTE: \`AUTO_BUCKET\` does not create buckets that don’t match any documents. That’s why the example above is missing 1985-02-01 and other dates.
               `,
               description:
                 'Text is in markdown. Do not translate function names, special characters, or field names like sum(bytes)',
