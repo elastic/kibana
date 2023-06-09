165acaa2d66282ccd6753b5b7d225be90ca277ae
Jonathan Budzenski <jon@elastic.co>
2023-06-09 13:55:06 -0500

    reuse build

--- .buildkite/scripts/steps/functional/on_merge_serverless_ftrs.sh
+++ .buildkite/scripts/steps/functional/on_merge_serverless_ftrs.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
   echo "--- Trigger serverless ftr tests"
-  ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-serverless "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT"
+  ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-serverless "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT" "$BUILDKITE_BUILD_ID"
 fi
