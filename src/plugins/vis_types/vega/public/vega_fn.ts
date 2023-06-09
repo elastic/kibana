ddff4c91faab243817df66d7f903fc7e4d1f354e
Matt Kime <matt@mattki.me>
2023-06-08 23:23:39 -0500

    remove some unneeded changes

--- src/plugins/vis_types/vega/public/vega_fn.ts
+++ src/plugins/vis_types/vega/public/vega_fn.ts
@@ -8,7 +8,7 @@
 
 import { get } from 'lodash';
 import { i18n } from '@kbn/i18n';
-import { ExecutionContextSearch } from '@kbn/data-plugin/common';
+import { ExecutionContextSearch } from '@kbn/data-plugin/public';
 import {
   ExecutionContext,
   ExpressionFunctionDefinition,
--- x-pack/plugins/lens/public/embeddable/expression_wrapper.tsx
+++ x-pack/plugins/lens/public/embeddable/expression_wrapper.tsx
@@ -13,7 +13,7 @@ import {
   ReactExpressionRendererType,
 } from '@kbn/expressions-plugin/public';
 import type { KibanaExecutionContext } from '@kbn/core/public';
-import { ExecutionContextSearch } from '@kbn/data-plugin/common';
+import { ExecutionContextSearch } from '@kbn/data-plugin/public';
 import { DefaultInspectorAdapters, RenderMode } from '@kbn/expressions-plugin/common';
 import classNames from 'classnames';
 import { getOriginalRequestErrorMessages } from '../editor_frame_service/error_helper';
