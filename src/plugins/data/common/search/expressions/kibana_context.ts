c391355cff5ff7c68460da88b14524d2d430eff3
Matt Kime <matt@mattki.me>
2023-06-08 22:08:23 -0500

    move some code back to data plugin

---
+++ src/plugins/data/common/search/expressions/kibana_context.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { ExpressionFunctionDefinition, ExecutionContext } from '@kbn/expressions-plugin/common';
+import { Adapters } from '@kbn/inspector-plugin/common';
+import {
+  KibanaTimerangeOutput,
+  ExecutionContextSearch,
+  KibanaContext,
+  KibanaFilter,
+  KibanaQueryOutput,
+} from '../..';
+/*
+import { SavedSearch } from '../types';
+
+export interface KibanaContextStartDependencies {
+  getSavedSearch: (id: string) => Promise<SavedSearch>;
+}
+*/
+
+interface Arguments {
+  q?: KibanaQueryOutput[] | null;
+  filters?: KibanaFilter[] | null;
+  timeRange?: KibanaTimerangeOutput | null;
+  savedSearchId?: string | null;
+}
+
+export type ExpressionFunctionKibanaContext = ExpressionFunctionDefinition<
+  'kibana_context',
+  KibanaContext | null,
+  Arguments,
+  Promise<KibanaContext>,
+  ExecutionContext<Adapters, ExecutionContextSearch>
+>;
