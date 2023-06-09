95edefb221820226a986e64da03d0a0ed2983d0e
Jan Monschke <jan.monschke@elastic.co>
2023-06-09 09:50:27 +0200

    [SecuritySolution] Decouple timeline SO types from server representation types (#158566)
    ## Summary
    
    https://github.com/elastic/security-team/issues/6479
    
    In this PR we're separating the types for the `Timeline` saved object
    and the type of it's API response equivalent. Doing so will allow us to
    make changes to either representations individually which is necessary
    for versioning the SO and routes (individually) in the future.
    
    The saved object definition of timeline and its API equivalent now live
    in `*/saved_object.ts` and `*/api.ts` respectively. A clean cut of these
    two, now distinct types. You will encounter a lot of duplication of
    types in these files which is unavoidable. In the future, depending on
    how both representations evolve when versioned, these two definitions
    will diverge.
    
    You will notice that only few types (and values) defined in
    `saved_object.ts` are exported. They are only used for the conversion
    logic. They are not exported so they're not accidentally required by
    frontend or server code that is not dealing with the conversion. The
    exported types all start with `SavedObject*` to clearly mark them as SO
    representations.
    
    The conversion files (`convert_saved_object_to_savedtimeline.ts` and
    `security_solution/server/lib/timeline/saved_object/timelines/index.ts`)
    have been updated to use the new representations and there is no
    implicit conversion between them (e.g. through spreading or rest
    parameters). In some places, an explicit conversion of fields was
    necessary (e.g. to translate between timeline types).
    
    The bulk of the changes are updates of `import` statements to change the
    import to `**/api.ts`. If you are on a security team other than the
    investigations team, you're most likely only required to look at those
    import changes :)
    
    
    ### Checklist
    
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/suricata/suricata_row_renderer.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/suricata/suricata_row_renderer.tsx
@@ -9,7 +9,7 @@ import { get } from 'lodash/fp';
 import React from 'react';
 
 import type { RowRenderer } from '../../../../../../../common/types/timeline';
-import { RowRendererId } from '../../../../../../../common/types/timeline';
+import { RowRendererId } from '../../../../../../../common/types/timeline/api';
 
 import { RowRendererContainer } from '../row_renderer';
 import { SuricataDetails } from './suricata_details';
