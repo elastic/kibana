42720e818c83dd80f7c3693848c626186879dfe6
Ashokaditya <ashokaditya@elastic.co>
2023-06-09 18:01:47 +0200

    fix types
    review changes @paul-tavares

--- x-pack/plugins/security_solution/server/endpoint/endpoint_app_context_services.ts
+++ x-pack/plugins/security_solution/server/endpoint/endpoint_app_context_services.ts
@@ -40,7 +40,6 @@ import type { EndpointAuthz } from '../../common/endpoint/types/authz';
 import { calculateEndpointAuthz } from '../../common/endpoint/service/authz';
 import type { FeatureUsageService } from './services/feature_usage/service';
 import type { ExperimentalFeatures } from '../../common/experimental_features';
-import type { actionCreateService } from './services/actions';
 import type { ActionCreateService } from './services/actions/create/types';
 
 export interface EndpointAppContextServiceSetupContract {
@@ -65,7 +64,7 @@ export interface EndpointAppContextServiceStartContract {
   featureUsageService: FeatureUsageService;
   experimentalFeatures: ExperimentalFeatures;
   messageSigningService: MessageSigningServiceInterface | undefined;
-  actionCreateService: ReturnType<typeof actionCreateService> | undefined;
+  actionCreateService: ActionCreateService | undefined;
   cloud: CloudSetup;
 }
 
