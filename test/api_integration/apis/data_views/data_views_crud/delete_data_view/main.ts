f49f8cd55ce7acc0fae8243f8dba88f17634e0bf
Matt Kime <matt@mattki.me>
2023-06-11 23:30:13 -0500

    restore test, check for empty body

--- test/api_integration/apis/data_views/data_views_crud/delete_data_view/main.ts
+++ test/api_integration/apis/data_views/data_views_crud/delete_data_view/main.ts
@@ -7,8 +7,11 @@
  */
 
 import expect from '@kbn/expect';
+import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 import { configArray } from '../../constants';
+
+const version = '2023-10-31';
 
 export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
@@ -18,29 +21,54 @@ export default function ({ getService }: FtrProviderContext) {
       describe(config.name, () => {
         it('deletes an index_pattern', async () => {
           const title = `foo-${Date.now()}-${Math.random()}*`;
-          const response1 = await supertest.post(config.path).send({
+          const response1 = await supertest
+            .post(config.path)
+            .set(ELASTIC_HTTP_VERSION_HEADER, version)
+            .send({
+              [config.serviceKey]: {
+                title,
+              },
+            });
+
+          const response2 = await supertest
+            .get(`${config.path}/${response1.body[config.serviceKey].id}`)
+            .set(ELASTIC_HTTP_VERSION_HEADER, version);
+
+          expect(response2.status).to.be(200);
+
+          const response3 = await supertest
+            .delete(`${config.path}/${response1.body[config.serviceKey].id}`)
+            .set(ELASTIC_HTTP_VERSION_HEADER, version);
+
+          expect(response3.status).to.be(200);
+
+          const response4 = await supertest
+            .get(`${config.path}/${response1.body[config.serviceKey].id}`)
+            .set(ELASTIC_HTTP_VERSION_HEADER, version);
+
+          expect(response4.status).to.be(404);
+        });
+      });
+
+      it('returns nothing', async () => {
+        const title = `foo-${Date.now()}-${Math.random()}*`;
+        const response1 = await supertest
+
+          .post(config.path)
+          .set(ELASTIC_HTTP_VERSION_HEADER, version)
+          .send({
             [config.serviceKey]: {
               title,
             },
           });
-          const response2 = await supertest.get(
-            `${config.path}/${response1.body[config.serviceKey].id}`
-          );
 
-          expect(response2.status).to.be(200);
+        await supertest.get(`${config.path}/${response1.body[config.serviceKey].id}`);
+        const response2 = await supertest
+          .delete(`${config.path}/${response1.body[config.serviceKey].id}`)
+          .set(ELASTIC_HTTP_VERSION_HEADER, version);
 
-          const response3 = await supertest.delete(
-            `${config.path}/${response1.body[config.serviceKey].id}`
-          );
-
-          expect(response3.status).to.be(200);
-
-          const response4 = await supertest.get(
-            `${config.path}/${response1.body[config.serviceKey].id}`
-          );
-
-          expect(response4.status).to.be(404);
-        });
+        // verify empty response
+        expect(Object.keys(response2.body).length).to.be(0);
       });
     });
   });
