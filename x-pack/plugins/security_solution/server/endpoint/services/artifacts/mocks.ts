40787dab108fde4a9efd33c1b1fb8c06068f75fa
David Sanchez Soler <david.sanchezsoler@elastic.co>
2023-06-12 15:52:20 +0200

    Fixes missing logger and config in endpoint artifacts mock

--- x-pack/plugins/security_solution/server/endpoint/services/artifacts/mocks.ts
+++ x-pack/plugins/security_solution/server/endpoint/services/artifacts/mocks.ts
@@ -9,8 +9,13 @@ import type { SavedObjectsClientContract } from '@kbn/core/server';
 import { savedObjectsClientMock, elasticsearchServiceMock } from '@kbn/core/server/mocks';
 import type { ElasticsearchClientMock } from '@kbn/core-elasticsearch-client-server-mocks';
 // Because mocks are for testing only, should be ok to import the FleetArtifactsClient directly
-import { FleetArtifactsClient } from '@kbn/fleet-plugin/server/services';
-import { createArtifactsClientMock } from '@kbn/fleet-plugin/server/mocks';
+import { appContextService, FleetArtifactsClient } from '@kbn/fleet-plugin/server/services';
+import {
+  createAppContextStartContractMock,
+  createArtifactsClientMock,
+} from '@kbn/fleet-plugin/server/mocks';
+import type { FleetConfigType } from '@kbn/fleet-plugin/server';
+
 import type { EndpointArtifactClientInterface } from './artifact_client';
 import { EndpointArtifactClient } from './artifact_client';
 import { ManifestClient } from './manifest_client';
@@ -37,16 +42,29 @@ export const createEndpointArtifactClientMock = (
   const fleetArtifactClientMocked = createArtifactsClientMock();
   const endpointArtifactClientMocked = new EndpointArtifactClient(fleetArtifactClientMocked);
 
+  appContextService.start(createAppContextStartContractMock());
+  const fleetLogger = appContextService.getLogger().get('artifactsTest');
+  const fleetConfig = appContextService.getConfig() as FleetConfigType;
   // Return the interface mocked with jest.fn() that fowards calls to the real instance
   return {
     createArtifact: jest.fn(async (...args) => {
-      const fleetArtifactClient = new FleetArtifactsClient(esClient, 'endpoint');
+      const fleetArtifactClient = new FleetArtifactsClient(
+        esClient,
+        'endpoint',
+        fleetConfig,
+        fleetLogger
+      );
       const endpointArtifactClient = new EndpointArtifactClient(fleetArtifactClient);
       const response = await endpointArtifactClient.createArtifact(...args);
       return response;
     }),
     bulkCreateArtifacts: jest.fn(async (...args) => {
-      const fleetArtifactClient = new FleetArtifactsClient(esClient, 'endpoint');
+      const fleetArtifactClient = new FleetArtifactsClient(
+        esClient,
+        'endpoint',
+        fleetConfig,
+        fleetLogger
+      );
       const endpointArtifactClient = new EndpointArtifactClient(fleetArtifactClient);
       const response = await endpointArtifactClient.bulkCreateArtifacts(...args);
       return response;
