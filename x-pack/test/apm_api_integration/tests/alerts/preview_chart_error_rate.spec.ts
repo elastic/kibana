5e91ed67597f8aaf84419f6ef2831fbb9ff9242b
Bena Kansara <bena.kansara@elastic.co>
2023-06-13 17:00:01 +0200

    update tests to use synthtrace

--- x-pack/test/apm_api_integration/tests/alerts/preview_chart_error_rate.spec.ts
+++ x-pack/test/apm_api_integration/tests/alerts/preview_chart_error_rate.spec.ts
@@ -12,22 +12,23 @@ import {
   TRANSACTION_TYPE,
 } from '@kbn/apm-plugin/common/es_fields/apm';
 import expect from '@kbn/expect';
-import archives from '../../common/fixtures/es_archiver/archives_metadata';
 import { FtrProviderContext } from '../../common/ftr_provider_context';
+import { generateData } from '../errors/generate_data';
 
 export default function ApiTest({ getService }: FtrProviderContext) {
   const registry = getService('registry');
   const apmApiClient = getService('apmApiClient');
-  const archiveName = 'apm_8.0.0';
-  const { end } = archives[archiveName];
-  const start = new Date(Date.parse(end) - 600000).toISOString();
+  const synthtraceEsClient = getService('synthtraceEsClient');
+  const start = new Date('2021-01-01T00:00:00.000Z').getTime();
+  const end = new Date('2021-01-01T00:15:00.000Z').getTime() - 1;
+  const serviceName = 'synth-go';
 
   const getOptions = () => ({
     params: {
       query: {
-        start,
-        end,
-        serviceName: 'opbeans-java',
+        start: new Date(start).toISOString(),
+        end: new Date(end).toISOString(),
+        serviceName: 'synth-go',
         transactionType: 'request' as string | undefined,
         environment: 'ENVIRONMENT_ALL',
         interval: '5m',
@@ -48,243 +49,244 @@ export default function ApiTest({ getService }: FtrProviderContext) {
     });
   });
 
-  registry.when(`with data loaded`, { config: 'basic', archives: [archiveName] }, () => {
-    it('transaction_error_rate (with data)', async () => {
-      const options = getOptions();
-      const response = await apmApiClient.readUser({
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
-        ...options,
+  registry.when(`with data loaded`, { config: 'basic', archives: [] }, () => {
+    describe('errors distribution', () => {
+      before(async () => {
+        await generateData({ serviceName, start, end, synthtraceEsClient });
       });
 
-      expect(response.status).to.be(200);
-      expect(
-        response.body.errorRateChartPreview.some(
-          (item: { name: string; data: Array<{ x: number; y: number | null }> }) =>
-            item.data.some((coordinate) => coordinate.x && coordinate.y)
-        )
-      ).to.equal(true);
-    });
+      after(() => synthtraceEsClient.clean());
 
-    it('transaction_error_rate with transaction name', async () => {
-      const options = {
-        params: {
-          query: {
-            start,
-            end,
-            serviceName: 'opbeans-java',
-            transactionName: 'APIRestController#product',
-            transactionType: 'request',
-            environment: 'ENVIRONMENT_ALL',
-            interval: '5m',
+      it('transaction_error_rate (with data)', async () => {
+        const options = getOptions();
+        const response = await apmApiClient.readUser({
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+          ...options,
+        });
+
+        expect(response.status).to.be(200);
+        expect(
+          response.body.errorRateChartPreview.some(
+            (item: { name: string; data: Array<{ x: number; y: number | null }> }) =>
+              item.data.some((coordinate) => coordinate.x && coordinate.y)
+          )
+        ).to.equal(true);
+
+        expect(response.body.errorRateChartPreview[0].name).to.eql('synth-go_production_request');
+        expect(response.body.errorRateChartPreview[0].data[1]).to.eql({
+          x: 1609459500000,
+          y: 37.5,
+        });
+      });
+
+      it('transaction_error_rate with transaction name', async () => {
+        const options = {
+          params: {
+            query: {
+              start: new Date(start).toISOString(),
+              end: new Date(end).toISOString(),
+              serviceName: 'synth-go',
+              transactionName: 'GET /banana 🍌',
+              transactionType: 'request',
+              environment: 'ENVIRONMENT_ALL',
+              interval: '5m',
+            },
           },
-        },
-      };
+        };
 
-      const response = await apmApiClient.readUser({
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
-        ...options,
+        const response = await apmApiClient.readUser({
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+          ...options,
+        });
+
+        expect(response.status).to.be(200);
+        expect(response.body.errorRateChartPreview[0].data[0]).to.eql({
+          x: 1609459200000,
+          y: 50,
+        });
       });
 
-      expect(response.status).to.be(200);
-      expect(response.body.errorRateChartPreview[0].data[0]).to.eql({
-        x: 1627973700000,
-        y: 100,
-      });
-    });
-
-    it('transaction_error_rate with nonexistent transaction name', async () => {
-      const options = {
-        params: {
-          query: {
-            start,
-            end,
-            serviceName: 'opbeans-java',
-            transactionName: 'foo',
-            transactionType: 'request',
-            environment: 'ENVIRONMENT_ALL',
-            interval: '5m',
+      it('transaction_error_rate with nonexistent transaction name', async () => {
+        const options = {
+          params: {
+            query: {
+              start: new Date(start).toISOString(),
+              end: new Date(end).toISOString(),
+              serviceName: 'synth-go',
+              transactionName: 'foo',
+              transactionType: 'request',
+              environment: 'ENVIRONMENT_ALL',
+              interval: '5m',
+            },
           },
-        },
-      };
+        };
 
-      const response = await apmApiClient.readUser({
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
-        ...options,
+        const response = await apmApiClient.readUser({
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+          ...options,
+        });
+
+        expect(response.status).to.be(200);
+        expect(response.body.errorRateChartPreview).to.eql([]);
       });
 
-      expect(response.status).to.be(200);
-      expect(response.body.errorRateChartPreview).to.eql([]);
-    });
+      it('transaction_error_rate with no group by parameter', async () => {
+        const options = getOptions();
+        const response = await apmApiClient.readUser({
+          ...options,
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        });
 
-    it('transaction_error_rate with no group by parameter', async () => {
-      const options = getOptions();
-      const response = await apmApiClient.readUser({
-        ...options,
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        expect(response.status).to.be(200);
+        expect(response.body.errorRateChartPreview.length).to.equal(1);
+        expect(
+          response.body.errorRateChartPreview.map(
+            (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
+          )
+        ).to.eql(['synth-go_production_request']);
       });
 
-      expect(response.status).to.be(200);
-      expect(response.body.errorRateChartPreview.length).to.equal(1);
-      expect(
-        response.body.errorRateChartPreview.map(
-          (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
-        )
-      ).to.eql(['opbeans-java_production_request']);
-    });
-
-    it('transaction_error_rate with default group by fields', async () => {
-      const options = {
-        params: {
-          query: {
-            ...getOptions().params.query,
-            groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE],
+      it('transaction_error_rate with default group by fields', async () => {
+        const options = {
+          params: {
+            query: {
+              ...getOptions().params.query,
+              groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE],
+            },
           },
-        },
-      };
+        };
 
-      const response = await apmApiClient.readUser({
-        ...options,
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        const response = await apmApiClient.readUser({
+          ...options,
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        });
+
+        expect(response.status).to.be(200);
+        expect(response.body.errorRateChartPreview.length).to.equal(1);
+        expect(
+          response.body.errorRateChartPreview.map(
+            (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
+          )
+        ).to.eql(['synth-go_production_request']);
       });
 
-      expect(response.status).to.be(200);
-      expect(response.body.errorRateChartPreview.length).to.equal(1);
-      expect(
-        response.body.errorRateChartPreview.map(
-          (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
-        )
-      ).to.eql(['opbeans-java_production_request']);
-    });
-
-    it('transaction_error_rate with group by on transaction name', async () => {
-      const options = {
-        params: {
-          query: {
-            ...getOptions().params.query,
-            groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE, TRANSACTION_NAME],
+      it('transaction_error_rate with group by on transaction name', async () => {
+        const options = {
+          params: {
+            query: {
+              ...getOptions().params.query,
+              groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE, TRANSACTION_NAME],
+            },
           },
-        },
-      };
+        };
 
-      const response = await apmApiClient.readUser({
-        ...options,
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        const response = await apmApiClient.readUser({
+          ...options,
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        });
+
+        expect(response.status).to.be(200);
+        expect(response.body.errorRateChartPreview.length).to.equal(2);
+        expect(response.body.errorRateChartPreview[0].name).to.eql(
+          'synth-go_production_request_GET /apple 🍎 '
+        );
+        expect(response.body.errorRateChartPreview[1].name).to.eql(
+          'synth-go_production_request_GET /banana 🍌'
+        );
+        expect(response.body.errorRateChartPreview[0].data[0]).to.eql({
+          x: 1609459200000,
+          y: 25,
+        });
+        expect(response.body.errorRateChartPreview[1].data[0]).to.eql({
+          x: 1609459200000,
+          y: 50,
+        });
       });
 
-      expect(response.status).to.be(200);
-      expect(response.body.errorRateChartPreview.length).to.equal(12);
-      expect(
-        response.body.errorRateChartPreview.map(
-          (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
-        )
-      ).to.eql([
-        'opbeans-java_production_request_APIRestController#customer',
-        'opbeans-java_production_request_APIRestController#topProducts',
-        'opbeans-java_production_request_DispatcherServlet#doGet',
-        'opbeans-java_production_request_ResourceHttpRequestHandler',
-        'opbeans-java_production_request_APIRestController#customerWhoBought',
-        'opbeans-java_production_request_APIRestController#order',
-        'opbeans-java_production_request_APIRestController#stats',
-        'opbeans-java_production_request_APIRestController#product',
-        'opbeans-java_production_request_APIRestController#orders',
-        'opbeans-java_production_request_APIRestController#products',
-        'opbeans-java_production_request_APIRestController#customers',
-        'opbeans-java_production_request_DispatcherServlet#doPost',
-      ]);
-    });
-
-    it('transaction_error_rate with group by on transaction name and filter on transaction name', async () => {
-      const options = {
-        params: {
-          query: {
-            ...getOptions().params.query,
-            transactionName: 'DispatcherServlet#doGet',
-            groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE, TRANSACTION_NAME],
+      it('transaction_error_rate with group by on transaction name and filter on transaction name', async () => {
+        const options = {
+          params: {
+            query: {
+              ...getOptions().params.query,
+              transactionName: 'GET /apple 🍎 ',
+              groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE, TRANSACTION_NAME],
+            },
           },
-        },
-      };
+        };
 
-      const response = await apmApiClient.readUser({
-        ...options,
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        const response = await apmApiClient.readUser({
+          ...options,
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        });
+
+        expect(response.status).to.be(200);
+        expect(response.body.errorRateChartPreview.length).to.equal(1);
+        expect(
+          response.body.errorRateChartPreview.map(
+            (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
+          )
+        ).to.eql(['synth-go_production_request_GET /apple 🍎 ']);
       });
 
-      expect(response.status).to.be(200);
-      expect(response.body.errorRateChartPreview.length).to.equal(1);
-      expect(
-        response.body.errorRateChartPreview.map(
-          (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
-        )
-      ).to.eql(['opbeans-java_production_request_DispatcherServlet#doGet']);
-    });
-
-    it('transaction_error_rate with empty service name, transaction name and transaction type', async () => {
-      const options = {
-        params: {
-          query: {
-            start,
-            end,
-            serviceName: '',
-            transactionName: '',
-            transactionType: '',
-            environment: 'ENVIRONMENT_ALL',
-            interval: '5m',
+      it('transaction_error_rate with empty service name, transaction name and transaction type', async () => {
+        const options = {
+          params: {
+            query: {
+              start: new Date(start).toISOString(),
+              end: new Date(end).toISOString(),
+              serviceName: '',
+              transactionName: '',
+              transactionType: '',
+              environment: 'ENVIRONMENT_ALL',
+              interval: '5m',
+            },
           },
-        },
-      };
-      const response = await apmApiClient.readUser({
-        ...options,
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        };
+        const response = await apmApiClient.readUser({
+          ...options,
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        });
+
+        expect(response.status).to.be(200);
+        expect(
+          response.body.errorRateChartPreview.map(
+            (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
+          )
+        ).to.eql(['synth-go_production_request']);
       });
 
-      expect(response.status).to.be(200);
-      expect(
-        response.body.errorRateChartPreview.map(
-          (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
-        )
-      ).to.eql([
-        'opbeans-python_production_celery',
-        'opbeans-go_testing_request',
-        'opbeans-ruby_production_request',
-        'opbeans-dotnet_production_request',
-        'opbeans-java_production_request',
-        'opbeans-node_testing_request',
-        'opbeans-python_production_request',
-      ]);
-    });
-
-    it('transaction_error_rate with empty service name, transaction name, transaction type and group by on transaction name', async () => {
-      const options = {
-        params: {
-          query: {
-            start,
-            end,
-            serviceName: '',
-            transactionName: '',
-            transactionType: '',
-            environment: 'ENVIRONMENT_ALL',
-            interval: '5m',
-            groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE, TRANSACTION_NAME],
+      it('transaction_error_rate with empty service name, transaction name, transaction type and group by on transaction name', async () => {
+        const options = {
+          params: {
+            query: {
+              start: new Date(start).toISOString(),
+              end: new Date(end).toISOString(),
+              serviceName: '',
+              transactionName: '',
+              transactionType: '',
+              environment: 'ENVIRONMENT_ALL',
+              interval: '5m',
+              groupBy: [SERVICE_NAME, SERVICE_ENVIRONMENT, TRANSACTION_TYPE, TRANSACTION_NAME],
+            },
           },
-        },
-      };
-      const response = await apmApiClient.readUser({
-        ...options,
-        endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
-      });
+        };
+        const response = await apmApiClient.readUser({
+          ...options,
+          endpoint: 'GET /internal/apm/rule_types/transaction_error_rate/chart_preview',
+        });
 
-      expect(response.status).to.be(200);
-      expect(
-        response.body.errorRateChartPreview
-          .map((item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name)
-          .slice(0, 5)
-      ).to.eql([
-        'opbeans-python_production_celery_opbeans.tasks.update_stats',
-        'opbeans-ruby_production_request_Rack',
-        'opbeans-node_testing_request_GET /api',
-        'opbeans-go_testing_request_GET /api/products/:id',
-        'opbeans-go_testing_request_GET /api/products/:id/customers',
-      ]);
+        expect(response.status).to.be(200);
+        expect(
+          response.body.errorRateChartPreview
+            .map(
+              (item: { name: string; data: Array<{ x: number; y: number | null }> }) => item.name
+            )
+            .slice(0, 5)
+        ).to.eql([
+          'synth-go_production_request_GET /apple 🍎 ',
+          'synth-go_production_request_GET /banana 🍌',
+        ]);
+      });
     });
   });
 }
