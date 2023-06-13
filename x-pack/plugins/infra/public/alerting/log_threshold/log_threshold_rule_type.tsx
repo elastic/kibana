bab5904070f65f096a79cf3bb3e4b3096d043c66
Maryam Saeidi <maryam.saeidi@elastic.co>
2023-06-13 13:43:10 +0200

    Update default action and recovery messages of Infra

--- x-pack/plugins/infra/public/alerting/log_threshold/log_threshold_rule_type.tsx
+++ x-pack/plugins/infra/public/alerting/log_threshold/log_threshold_rule_type.tsx
@@ -19,6 +19,32 @@ import { InfraClientCoreSetup } from '../../types';
 import { createRuleFormatter } from './rule_data_formatters';
 import { validateExpression } from './validation';
 
+export const logThresholdDefaultActionMessage = i18n.translate(
+  'xpack.infra.logs.alerting.threshold.defaultActionMessage',
+  {
+    defaultMessage: `\\{\\{context.reason\\}\\}
+
+\\{\\{rule.name\\}\\} is active.
+
+\\{\\{^context.isRatio\\}\\}\\{\\{#context.group\\}\\}\\{\\{context.group\\}\\} - \\{\\{/context.group\\}\\}\\{\\{context.matchingDocuments\\}\\} log entries have matched the following conditions: \\{\\{context.conditions\\}\\}\\{\\{/context.isRatio\\}\\}
+\\{\\{#context.isRatio\\}\\}\\{\\{#context.group\\}\\}\\{\\{context.group\\}\\} - \\{\\{/context.group\\}\\} Ratio of the count of log entries matching \\{\\{context.numeratorConditions\\}\\} to the count of log entries matching \\{\\{context.denominatorConditions\\}\\} was \\{\\{context.ratio\\}\\}\\{\\{/context.isRatio\\}\\}
+
+[View alert details](\\{\\{context.alertDetailsUrl\\}\\})
+`,
+  }
+);
+export const logThresholdDefaultRecoveryMessage = i18n.translate(
+  'xpack.infra.logs.alerting.threshold.defaultRecoveryMessage',
+  {
+    defaultMessage: `\\{\\{context.reason\\}\\}
+
+\\{\\{rule.name\\}\\} has recovered.
+
+[View alert details](\\{\\{context.alertDetailsUrl\\}\\})
+`,
+  }
+);
+
 export function createLogThresholdRuleType(
   core: InfraClientCoreSetup,
   logsLocator: LocatorPublic<LogsLocatorParams>
@@ -40,12 +66,8 @@ export function createLogThresholdRuleType(
     alertDetailsAppSection: lazy(() => import('./components/alert_details_app_section')),
     ruleParamsExpression,
     validate: validateExpression,
-    defaultActionMessage: i18n.translate(
-      'xpack.infra.logs.alerting.threshold.defaultActionMessage',
-      {
-        defaultMessage: `\\{\\{^context.isRatio\\}\\}\\{\\{#context.group\\}\\}\\{\\{context.group\\}\\} - \\{\\{/context.group\\}\\}\\{\\{context.matchingDocuments\\}\\} log entries have matched the following conditions: \\{\\{context.conditions\\}\\}\\{\\{/context.isRatio\\}\\}\\{\\{#context.isRatio\\}\\}\\{\\{#context.group\\}\\}\\{\\{context.group\\}\\} - \\{\\{/context.group\\}\\} Ratio of the count of log entries matching \\{\\{context.numeratorConditions\\}\\} to the count of log entries matching \\{\\{context.denominatorConditions\\}\\} was \\{\\{context.ratio\\}\\}\\{\\{/context.isRatio\\}\\}`,
-      }
-    ),
+    defaultActionMessage: logThresholdDefaultActionMessage,
+    defaultRecoveryMessage: logThresholdDefaultRecoveryMessage,
     requiresAppContext: false,
     format: createRuleFormatter(logsLocator),
   };
--- x-pack/plugins/infra/public/alerting/metric_threshold/index.ts
+++ x-pack/plugins/infra/public/alerting/metric_threshold/index.ts
@@ -20,6 +20,38 @@ export interface MetricThresholdRuleTypeParams extends RuleTypeParams {
   criteria: MetricExpressionParams[];
 }
 
+export const metricThresholdDefaultActionMessage = i18n.translate(
+  'xpack.infra.metrics.alerting.metric.threshold.defaultActionMessage',
+  {
+    defaultMessage: `\\{\\{context.reason\\}\\}
+
+\\{\\{rule.name\\}\\} is active with the following conditions:
+
+- Affected: \\{\\{context.group\\}\\}
+- Metric: \\{\\{context.metric\\}\\}
+- Observed value: \\{\\{context.value\\}\\}
+- Threshold: \\{\\{context.threshold\\}\\}
+
+[View alert details](\\{\\{context.alertDetailsUrl\\}\\})
+`,
+  }
+);
+export const metricThresholdDefaultRecoveryMessage = i18n.translate(
+  'xpack.infra.metrics.alerting.metric.threshold.defaultRecoveryMessage',
+  {
+    defaultMessage: `\\{\\{context.reason\\}\\}
+
+\\{\\{rule.name\\}\\} has recovered.
+
+- Affected: \\{\\{context.group\\}\\}
+- Metric: \\{\\{context.metric\\}\\}
+- Threshold: \\{\\{context.threshold\\}\\}
+
+[View alert details](\\{\\{context.alertDetailsUrl\\}\\})
+`,
+  }
+);
+
 export function createMetricThresholdRuleType(): ObservabilityRuleTypeModel<MetricThresholdRuleTypeParams> {
   return {
     id: METRIC_THRESHOLD_ALERT_TYPE_ID,
@@ -32,16 +64,8 @@ export function createMetricThresholdRuleType(): ObservabilityRuleTypeModel<Metr
     },
     ruleParamsExpression: lazy(() => import('./components/expression')),
     validate: validateMetricThreshold,
-    defaultActionMessage: i18n.translate(
-      'xpack.infra.metrics.alerting.threshold.defaultActionMessage',
-      {
-        defaultMessage: `\\{\\{alertName\\}\\} - \\{\\{context.group\\}\\} is in a state of \\{\\{context.alertState\\}\\}
-
-Reason:
-\\{\\{context.reason\\}\\}
-`,
-      }
-    ),
+    defaultActionMessage: metricThresholdDefaultActionMessage,
+    defaultRecoveryMessage: metricThresholdDefaultRecoveryMessage,
     requiresAppContext: false,
     format: formatReason,
     alertDetailsAppSection: lazy(() => import('./components/alert_details_app_section')),
