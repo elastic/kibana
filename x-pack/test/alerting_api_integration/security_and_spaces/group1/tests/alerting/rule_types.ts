84c092b22924fc24fdd88ea8e834e4bcaedb8461
Zacqary Xeper <zacqary.xeper@elastic.co>
2023-06-13 14:57:27 -0500

    Fix functional tests

--- x-pack/test/alerting_api_integration/security_and_spaces/group1/tests/alerting/rule_types.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/group1/tests/alerting/rule_types.ts
@@ -37,6 +37,7 @@ export default function listRuleTypes({ getService }: FtrProviderContext) {
       name: 'Recovered',
     },
     enabled_in_license: true,
+    has_fields_for_a_a_d: false,
     has_get_summarized_alerts: false,
     rule_task_timeout: '5m',
   };
@@ -63,6 +64,7 @@ export default function listRuleTypes({ getService }: FtrProviderContext) {
     minimum_license_required: 'basic',
     is_exportable: true,
     enabled_in_license: true,
+    has_fields_for_a_a_d: false,
     has_get_summarized_alerts: false,
     rule_task_timeout: '5m',
   };
