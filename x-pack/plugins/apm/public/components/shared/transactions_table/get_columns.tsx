8144921d2041f0f7fc1460770e0571c76921b528
Kate Patticha <aikaterini.patticha@elastic.co>
2023-06-13 18:12:27 +0200

    Fix types

--- x-pack/plugins/apm/public/components/shared/transactions_table/get_columns.tsx
+++ x-pack/plugins/apm/public/components/shared/transactions_table/get_columns.tsx
@@ -113,7 +113,7 @@ export function getColumns({
                 </EuiToolTip>
               );
             },
-          },
+          } as EuiBasicTableColumn<ServiceTransactionGroupItem>,
         ]
       : []),
     {
