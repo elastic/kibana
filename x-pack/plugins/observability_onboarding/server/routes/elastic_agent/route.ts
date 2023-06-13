2b42341d044d8cf04710854192741eeb6f8525be
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2023-06-13 04:58:31 -0400

    Expose decoded cloudId components from the cloud plugin's contract (#159442)
    ## Summary
    
    Fix https://github.com/elastic/kibana/issues/138813
    
    - decode the cloudId and expose the id components from the `cloud`
    plugin's browser and server-side contracts
    - remove the existing `decodeCloudId` helpers from the other plugins
    - adapt the usages accordingly
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/observability_onboarding/server/routes/elastic_agent/route.ts
+++ x-pack/plugins/observability_onboarding/server/routes/elastic_agent/route.ts
@@ -9,7 +9,6 @@ import { getAuthenticationAPIKey } from '../../lib/get_authentication_api_key';
 import { createObservabilityOnboardingServerRoute } from '../create_observability_onboarding_server_route';
 import { getObservabilityOnboardingState } from '../custom_logs/get_observability_onboarding_state';
 import { generateYml } from './generate_yml';
-import { getCloudUrls } from '../custom_logs/get_cloud_urls';
 import { getFallbackUrls } from '../custom_logs/get_fallback_urls';
 
 const generateConfig = createObservabilityOnboardingServerRoute({
@@ -23,9 +22,9 @@ const generateConfig = createObservabilityOnboardingServerRoute({
     const savedObjectsClient =
       coreStart.savedObjects.createInternalRepository();
 
-    const cloudId = plugins.cloud.setup.cloudId;
-    const { elasticsearchUrl } =
-      (cloudId && getCloudUrls(cloudId)) || getFallbackUrls(coreStart);
+    const elasticsearchUrl =
+      plugins.cloud?.setup?.elasticsearchUrl ??
+      getFallbackUrls(coreStart).elasticsearchUrl;
 
     const savedState = await getObservabilityOnboardingState({
       savedObjectsClient,
