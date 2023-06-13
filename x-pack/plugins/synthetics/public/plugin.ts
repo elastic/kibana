cd8adaa730483de5f0aa257ec0f1b187bb165bec
shahzad31 <shahzad31comp@gmail.com>
2023-06-13 16:33:15 +0200

    update types

--- x-pack/plugins/synthetics/public/plugin.ts
+++ x-pack/plugins/synthetics/public/plugin.ts
@@ -102,7 +102,6 @@ export interface ClientPluginsStart {
   docLinks: DocLinksStart;
   uiSettings: CoreStart['uiSettings'];
   usageCollection: UsageCollectionStart;
-  savedObjects: CoreStart['savedObjects'];
 }
 
 export interface UptimePluginServices extends Partial<CoreStart> {
