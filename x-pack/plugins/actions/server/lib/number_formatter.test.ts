537723f93c9436709029f88b68df98657f75f609
Patrick Mueller <patrick.mueller@elastic.co>
2023-06-14 01:38:06 -0400

    [ResponseOps] adds FormatNumber mustache lambda
    resolves https://github.com/elastic/kibana/issues/155869
    
    Adds mustache lambda `{{#FormatNumber}}`, which uses
    [Intl.NumberFormat][] to format the number.
    
    [Intl.NumberFormat]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat

---
+++ x-pack/plugins/actions/server/lib/number_formatter.test.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { formatNumber } from './number_formatter';
+
+describe('formatNumber()', () => {
+  it('number with defaults is successful', () => {
+    const format = '1;;';
+
+    expect(formatNumber(format)).toEqual('1');
+  });
+});
