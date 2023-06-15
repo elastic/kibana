61655c5377d75763fe3c8178e3f212e854ba8d8b
Cauê Marcondes <55978943+cauemarcondes@users.noreply.github.com>
2023-06-15 14:37:31 +0100

    dot not replace protocol

--- x-pack/plugins/profiling/public/views/no_data_view/index.tsx
+++ x-pack/plugins/profiling/public/views/no_data_view/index.tsx
@@ -43,7 +43,7 @@ export function NoDataView({ subTitle }: { subTitle: string }) {
 
   const secretToken = data?.variables.collector.secretToken;
   const collectionAgentHost = data?.variables.collector?.host?.replace('https://', '');
-  const symbolUrl = data?.variables?.symbolizer?.host?.replace('https://', '');
+  const symbolUrl = data?.variables?.symbolizer?.host;
   const hostAgentVersion = 'v3';
 
   const tabs = [
