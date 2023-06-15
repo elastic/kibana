3a6b781a2c8614fbcdc52c7811a56532c402b045
Quynh Nguyen <quynh.nguyen@elastic.co>
2023-06-15 09:55:40 -0500

    Increase limit of MAX_ANOMALY_CHARTS_ALLOWED to 50

--- x-pack/plugins/ml/public/embeddables/anomaly_charts/anomaly_charts_initializer.tsx
+++ x-pack/plugins/ml/public/embeddables/anomaly_charts/anomaly_charts_initializer.tsx
@@ -23,7 +23,7 @@ import { FormattedMessage } from '@kbn/i18n-react';
 import { AnomalyChartsEmbeddableInput } from '..';
 import { DEFAULT_MAX_SERIES_TO_PLOT } from '../../application/services/anomaly_explorer_charts_service';
 
-export const MAX_ANOMALY_CHARTS_ALLOWED = 48;
+export const MAX_ANOMALY_CHARTS_ALLOWED = 50;
 export interface AnomalyChartsInitializerProps {
   defaultTitle: string;
   initialInput?: Partial<Pick<AnomalyChartsEmbeddableInput, 'jobIds' | 'maxSeriesToPlot'>>;
