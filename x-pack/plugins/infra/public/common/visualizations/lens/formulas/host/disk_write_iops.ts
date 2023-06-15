8aab6fa522cee384611bfcdad94ff4dcaae71fed
Carlos Crespo <crespocarlos@users.noreply.github.com>
2023-06-15 14:33:31 +0200

    [Infrastructure UI] Reorganize metric charts in the hosts view (#159457)
    ## Summary
    
    This PR is the first step towards making metrics across infra
    consistent. For the Hosts view, these are the new metrics:
    
    <img width="667" alt="image"
    src="https://github.com/elastic/kibana/assets/2767137/6bcbc9a2-b9d5-42fc-bee0-d74edffa9960">
    
    _note: KPI's titles are now capitalized._
    
    
    <img width="667" alt="image"
    src="https://github.com/elastic/kibana/assets/2767137/16c51b3f-ccfc-400f-ad86-cbca53dd2f6a">
    
    
    
    ### How to test
    - Start a local Kibana instance
    - Navigate to `Infastructure > Hosts`
    
    ### For reviewer
    
    - Some translation keys had to be changed and others removed because the
    metrics no longer exist or the copy has completely changed. No new
    translations have been added.
    - All tooltips are temporary, they will be addressed in another PR
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/infra/public/common/visualizations/lens/formulas/host/disk_write_iops.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { LensChartConfig } from '../../../types';
+import { getFilters } from './utils';
+
+export const diskIOWrite: LensChartConfig = {
+  title: 'Disk Write IOPS',
+  formula: {
+    formula: "counter_rate(max(system.diskio.write.count), kql='system.diskio.write.count: *')",
+    format: {
+      id: 'number',
+      params: {
+        decimals: 0,
+      },
+    },
+  },
+  getFilters,
+};
