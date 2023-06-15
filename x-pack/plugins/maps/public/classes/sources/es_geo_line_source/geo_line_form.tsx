5f0858cb807648ef5accbb9b40c2946274cd7879
nreese <reese.nathan@elastic.co>
2023-06-15 13:49:37 -0600

    create form

--- x-pack/plugins/maps/public/classes/sources/es_geo_line_source/geo_line_form.tsx
+++ x-pack/plugins/maps/public/classes/sources/es_geo_line_source/geo_line_form.tsx
@@ -6,9 +6,8 @@
  */
 
 import React, { useMemo } from 'react';
-
 import { DataView } from '@kbn/data-plugin/common';
-import { EuiFormRow } from '@elastic/eui';
+import { EuiFormRow, EuiSwitch, EuiSwitchEvent, EuiToolTip } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { indexPatterns } from '@kbn/data-plugin/public';
 import { SingleFieldSelect } from '../../../components/single_field_select';
@@ -25,8 +24,20 @@ interface Props {
 }
 
 export function GeoLineForm(props: Props) {
-  const isTimeseries = useMemo(() => {
-    return getIsTimeseries(props.indexPattern);
+  const { isTimeseries, dimensionLabels } = useMemo(() => {
+    const isTimeseries = getIsTimeseries(props.indexPattern);
+    return {
+      isTimeseries,
+      dimensionLabels: isTimeseries
+        ? props.indexPattern.fields
+            .filter(field => {
+              return field.timeSeriesDimension;
+            })
+            .map(field => {
+              return field.displayName;
+            })
+        : [],
+    };
   }, [props.indexPattern]);
 
   function onSortFieldChange(fieldName: string | undefined) {
@@ -39,47 +50,73 @@ export function GeoLineForm(props: Props) {
       props.onSplitFieldChange(fieldName);
     }
   }
+
   return (
     <>
-      <EuiFormRow
-        label={i18n.translate('xpack.maps.source.esGeoLine.splitFieldLabel', {
-          defaultMessage: 'Entity',
-        })}
-      >
-        <SingleFieldSelect
-          placeholder={i18n.translate('xpack.maps.source.esGeoLine.splitFieldPlaceholder', {
-            defaultMessage: 'Select entity field',
-          })}
-          value={props.splitField}
-          onChange={onSplitFieldChange}
-          fields={getTermsFields(props.indexPattern.fields)}
-          isClearable={false}
-        />
-      </EuiFormRow>
+      {isTimeseries && 
+        <EuiFormRow>
+          <EuiToolTip
+            position="top"
+            content={i18n.translate('xpack.maps.source.esGeoLine.groupByTimeseriesTooltip', {
+              defaultMessage: 'When enabled, create a track for each unique time series. Dimensions: {dimensionLabels}',
+              values: { dimensionLabels: dimensionLabels.join(',') }
+            })}
+          >
+            <EuiSwitch
+              label={i18n.translate('xpack.maps.source.esGeoLine.groupByTimeseriesLabel', {
+                defaultMessage: 'Group by time series',
+              })}
+              checked={props.groupByTimeseries}
+              onChange={(event: EuiSwitchEvent) => {
+                props.onGroupByTimeseriesChange(event.target.checked);
+              }}
+            />
+          </EuiToolTip>
+        </EuiFormRow>
+      }
+      {!props.groupByTimeseries &&
+        <>
+          <EuiFormRow
+            label={i18n.translate('xpack.maps.source.esGeoLine.splitFieldLabel', {
+              defaultMessage: 'Entity',
+            })}
+          >
+            <SingleFieldSelect
+              placeholder={i18n.translate('xpack.maps.source.esGeoLine.splitFieldPlaceholder', {
+                defaultMessage: 'Select entity field',
+              })}
+              value={props.splitField}
+              onChange={onSplitFieldChange}
+              fields={getTermsFields(props.indexPattern.fields)}
+              isClearable={false}
+            />
+          </EuiFormRow>
 
-      <EuiFormRow
-        label={i18n.translate('xpack.maps.source.esGeoLine.sortFieldLabel', {
-          defaultMessage: 'Sort',
-        })}
-      >
-        <SingleFieldSelect
-          placeholder={i18n.translate('xpack.maps.source.esGeoLine.sortFieldPlaceholder', {
-            defaultMessage: 'Select sort field',
-          })}
-          value={props.sortField}
-          onChange={onSortFieldChange}
-          fields={props.indexPattern.fields.filter((field) => {
-            const isSplitField = props.splitField ? field.name === props.splitField : false;
-            return (
-              !isSplitField &&
-              field.sortable &&
-              !indexPatterns.isNestedField(field) &&
-              ['number', 'date'].includes(field.type)
-            );
-          })}
-          isClearable={false}
-        />
-      </EuiFormRow>
+          <EuiFormRow
+            label={i18n.translate('xpack.maps.source.esGeoLine.sortFieldLabel', {
+              defaultMessage: 'Sort',
+            })}
+          >
+            <SingleFieldSelect
+              placeholder={i18n.translate('xpack.maps.source.esGeoLine.sortFieldPlaceholder', {
+                defaultMessage: 'Select sort field',
+              })}
+              value={props.sortField}
+              onChange={onSortFieldChange}
+              fields={props.indexPattern.fields.filter((field) => {
+                const isSplitField = props.splitField ? field.name === props.splitField : false;
+                return (
+                  !isSplitField &&
+                  field.sortable &&
+                  !indexPatterns.isNestedField(field) &&
+                  ['number', 'date'].includes(field.type)
+                );
+              })}
+              isClearable={false}
+            />
+          </EuiFormRow>
+        </>
+      }
     </>
   );
 }
