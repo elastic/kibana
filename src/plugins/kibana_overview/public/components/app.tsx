5c9484b9cd35c197535b9b959ef02885cb4ce6e1
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-06-15 19:46:07 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/plugins/kibana_overview/public/components/app.tsx
+++ src/plugins/kibana_overview/public/components/app.tsx
@@ -9,7 +9,7 @@
 import React, { useEffect, useState } from 'react';
 import { Observable } from 'rxjs';
 import { I18nProvider } from '@kbn/i18n-react';
-import { HashRouter as Router, Switch } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Route } from '@kbn/shared-ux-router';
 import { CoreStart } from '@kbn/core/public';
 import { NavigationPublicPluginStart } from '@kbn/navigation-plugin/public';
