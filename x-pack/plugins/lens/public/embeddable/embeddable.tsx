ecacc9b029b40387905be8e84d03b950e862bec0
Devon Thomson <devon.thomson@elastic.co>
2023-06-15 13:38:43 -0400

    remove accidental Lens changes

--- x-pack/plugins/lens/public/embeddable/embeddable.tsx
+++ x-pack/plugins/lens/public/embeddable/embeddable.tsx
@@ -710,11 +710,6 @@ export class Embeddable
     return this.lensInspector.adapters;
   }
 
-  private fullAttributes: LensSavedObjectAttributes | undefined;
-  public getFullAttributes() {
-    return this.fullAttributes;
-  }
-
   async initializeSavedVis(input: LensEmbeddableInput) {
     const unwrapResult: LensUnwrapResult | false = await this.deps.attributeService
       .unwrapAttributes(input)
@@ -727,8 +722,6 @@ export class Embeddable
     }
 
     const { metaInfo, attributes } = unwrapResult;
-
-    this.fullAttributes = attributes;
 
     this.savedVis = {
       ...attributes,
