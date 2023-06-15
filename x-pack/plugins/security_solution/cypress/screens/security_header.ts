4444d1cc9661aaa5d8989d3cf8e2020a35e5be5b
Angela Chuang <yi-chun.chuang@elastic.co>
2023-06-15 13:21:15 +0100

    revert change

--- x-pack/plugins/security_solution/cypress/screens/security_header.ts
+++ x-pack/plugins/security_solution/cypress/screens/security_header.ts
@@ -70,6 +70,7 @@ export const LOADING_INDICATOR = '[data-test-subj="globalLoadingIndicator"]';
 export const LOADING_INDICATOR_HIDDEN = '[data-test-subj="globalLoadingIndicator-hidden"]';
 
 export const KIBANA_LOADING_ICON = '[data-test-subj="kbnLoadingMessage"]';
+
 // opens the navigation panel for a given nested link
 export const openNavigationPanelFor = (page: string) => {
   let panel;
