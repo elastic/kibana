e6de3a7e11bc153230ef1ddd31534635979af72b
Omolola Akinleye <omolola.akinleye@elastic.co>
2023-06-16 11:31:27 -0400

    fix agents counts return counts other than 0

--- x-pack/plugins/cloud_security_posture/server/lib/telemetry/collectors/installation_stats_collector.ts
+++ x-pack/plugins/cloud_security_posture/server/lib/telemetry/collectors/installation_stats_collector.ts
@@ -4,14 +4,15 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { agentPolicyService } from '@kbn/fleet-plugin/server/services';
 import type { CoreStart, Logger, SavedObjectsClientContract } from '@kbn/core/server';
 import type { ElasticsearchClient } from '@kbn/core-elasticsearch-server';
 import {
+  AgentPolicy,
   PackagePolicy,
   PACKAGE_POLICY_SAVED_OBJECT_TYPE,
   SO_SEARCH_LIMIT,
 } from '@kbn/fleet-plugin/common';
+import { agentPolicyService } from '@kbn/fleet-plugin/server/services';
 import type { CloudSecurityInstallationStats } from './types';
 import type { CspServerPluginStart, CspServerPluginStartDeps } from '../../../types';
 import { CLOUD_SECURITY_POSTURE_PACKAGE_NAME } from '../../../../common/constants';
@@ -33,36 +34,47 @@ export const getInstallationStats = async (
     isPluginInitialized,
   };
 
+  const getInstalledPackagePolicies = async (
+    packagePolicies: PackagePolicy[],
+    agentPolicies: AgentPolicy[]
+  ) => {
+    const installationStats = await packagePolicies.map(
+      (packagePolicy: PackagePolicy): CloudSecurityInstallationStats => {
+        const agentCounts =
+          agentPolicies?.find((agentPolicy) => agentPolicy?.id === packagePolicy.policy_id)
+            ?.agents ?? 0;
+
+        return {
+          package_policy_id: packagePolicy.id,
+          feature: packagePolicy.vars?.posture?.value as string,
+          deployment_mode: packagePolicy.vars?.deployment?.value as string,
+          package_version: packagePolicy.package?.version as string,
+          created_at: packagePolicy.created_at,
+          created_by: packagePolicy.created_by,
+          agent_policy_id: packagePolicy.policy_id,
+          agent_count: agentCounts,
+        };
+      }
+    );
+    return installationStats;
+  };
+
   const packagePolicies = await cspContext.packagePolicyService.list(soClient, {
     perPage: SO_SEARCH_LIMIT,
     kuery: `${PACKAGE_POLICY_SAVED_OBJECT_TYPE}.package.name:"${CLOUD_SECURITY_POSTURE_PACKAGE_NAME}"`,
   });
 
-  if (!packagePolicies?.items) return [];
-
   const agentPolicies = await agentPolicyService.list(soClient, {
     perPage: SO_SEARCH_LIMIT,
-    page: 1,
-    withAgentCount: true,
     kuery: '',
+    esClient,
+    withAgentCount: true,
   });
+  if (!packagePolicies) return [];
 
-  const installationStats: CloudSecurityInstallationStats[] = packagePolicies.items.map(
-    (packagePolicy: PackagePolicy): CloudSecurityInstallationStats => {
-      const agentCounts = agentPolicies?.items.find(
-        (agentPolicy) => agentPolicy?.id === packagePolicy.policy_id
-      )?.agents;
-      return {
-        package_policy_id: packagePolicy.id,
-        feature: packagePolicy.vars?.posture?.value as string,
-        deployment_mode: packagePolicy.vars?.deployment?.value as string,
-        package_version: packagePolicy.package?.version as string,
-        created_at: packagePolicy.created_at,
-        created_by: packagePolicy.created_by,
-        agent_policy_id: packagePolicy.policy_id,
-        agent_count: agentCounts ?? 0,
-      };
-    }
+  const installationStats: CloudSecurityInstallationStats[] = await getInstalledPackagePolicies(
+    packagePolicies.items,
+    agentPolicies?.items || []
   );
 
   return installationStats;
