elastic/kibana

commit 26d4ba5c3e6a053ea5505247f49885e2fe8821bb

[data views] Use versioned router for REST routes (#158608)
## Summary

Version alllll the data view routes.

Best viewed with whitespace hidden -
https://github.com/elastic/kibana/pull/158608/files?diff=unified&w=1

In this PR:
- All REST (public and internal) routes are versioned
- Internal routes are called with version specified
- Internal and public routes are now stored in directories labeled as
such
- All routes have a response schema
- All responses are typed with `response` types, separate from internal
api types. This is to help prevent unacknowledged changes to the api.
- Moves some functional tests from js => ts

For follow up PRs:
- Move to `internal` path for internal routes
- Proper typing and schema for `fields_for_wildcard` filter

Closes https://github.com/elastic/kibana/issues/157099
Closes https://github.com/elastic/kibana/issues/157100

---------

Co-authored-by: Julia Rechkunova <julia.rechkunova@gmail.com>

src/plugins/data_views/server/rest_api_routes/internal/fields_for.ts
@@ -1,0 +1,189 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { estypes } from '@elastic/elasticsearch';
+import { schema } from '@kbn/config-schema';
+import { IRouter, RequestHandler, StartServicesAccessor } from '@kbn/core/server';
+import { FullValidationConfig } from '@kbn/core-http-server';
+import { INITIAL_REST_VERSION_INTERNAL as version } from '../../constants';
+import { IndexPatternsFetcher } from '../../fetcher';
+import type {
+  DataViewsServerPluginStart,
+  DataViewsServerPluginStartDependencies,
+} from '../../types';
+import type { FieldDescriptorRestResponse } from '../route_types';
+
+/**
+ * Accepts one of the following:
+ * 1. An array of field names
+ * 2. A JSON-stringified array of field names
+ * 3. A single field name (not comma-separated)
+ * @returns an array of field names
+ * @param fields
+ */
+export const parseFields = (fields: string | string[]): string[] => {
+  if (Array.isArray(fields)) return fields;
+  try {
+    return JSON.parse(fields);
+  } catch (e) {
+    if (!fields.includes(',')) return [fields];
+    throw new Error(
+      'metaFields should be an array of field names, a JSON-stringified array of field names, or a single field name'
+    );
+  }
+};
+
+const path = '/api/index_patterns/_fields_for_wildcard';
+const access = 'internal';
+
+type IBody = { index_filter?: estypes.QueryDslQueryContainer } | undefined;
+interface IQuery {
+  pattern: string;
+  meta_fields: string | string[];
+  type?: string;
+  rollup_index?: string;
+  allow_no_index?: boolean;
+  include_unmapped?: boolean;
+  fields?: string[];
+}
+
+const querySchema = schema.object({
+  pattern: schema.string(),
+  meta_fields: schema.oneOf([schema.string(), schema.arrayOf(schema.string())], {
+    defaultValue: [],
+  }),
+  type: schema.maybe(schema.string()),
+  rollup_index: schema.maybe(schema.string()),
+  allow_no_index: schema.maybe(schema.boolean()),
+  include_unmapped: schema.maybe(schema.boolean()),
+  fields: schema.maybe(schema.oneOf([schema.string(), schema.arrayOf(schema.string())])),
+});
+
+const fieldSubTypeSchema = schema.object({
+  multi: schema.maybe(schema.object({ parent: schema.string() })),
+  nested: schema.maybe(schema.object({ path: schema.string() })),
+});
+
+const FieldDescriptorSchema = schema.object({
+  aggregatable: schema.boolean(),
+  name: schema.string(),
+  readFromDocValues: schema.boolean(),
+  searchable: schema.boolean(),
+  type: schema.string(),
+  esTypes: schema.maybe(schema.arrayOf(schema.string())),
+  subType: fieldSubTypeSchema,
+  metadata_field: schema.maybe(schema.boolean()),
+  fixedInterval: schema.maybe(schema.arrayOf(schema.string())),
+  timeZone: schema.maybe(schema.arrayOf(schema.string())),
+  timeSeriesMetric: schema.maybe(
+    schema.oneOf([
+      schema.literal('histogram'),
+      schema.literal('summary'),
+      schema.literal('counter'),
+      schema.literal('gauge'),
+    ])
+  ),
+  timeSeriesDimension: schema.maybe(schema.boolean()),
+});
+
+const validate: FullValidationConfig<any, any, any> = {
+  request: {
+    query: querySchema,
+    // not available to get request
+    body: schema.maybe(schema.object({ index_filter: schema.any() })),
+  },
+  response: {
+    200: {
+      body: schema.object({
+        fields: schema.arrayOf(FieldDescriptorSchema),
+        indices: schema.arrayOf(schema.string()),
+      }),
+    },
+  },
+};
+
+const handler: RequestHandler<{}, IQuery, IBody> = async (context, request, response) => {
+  const { asCurrentUser } = (await context.core).elasticsearch.client;
+  const indexPatterns = new IndexPatternsFetcher(asCurrentUser);
+  const {
+    pattern,
+    meta_fields: metaFields,
+    type,
+    rollup_index: rollupIndex,
+    allow_no_index: allowNoIndex,
+    include_unmapped: includeUnmapped,
+  } = request.query;
+
+  // not available to get request
+  const indexFilter = request.body?.index_filter;
+
+  let parsedFields: string[] = [];
+  let parsedMetaFields: string[] = [];
+  try {
+    parsedMetaFields = parseFields(metaFields);
+    parsedFields = parseFields(request.query.fields ?? []);
+  } catch (error) {
+    return response.badRequest();
+  }
+
+  try {
+    const { fields, indices } = await indexPatterns.getFieldsForWildcard({
+      pattern,
+      metaFields: parsedMetaFields,
+      type,
+      rollupIndex,
+      fieldCapsOptions: {
+        allow_no_indices: allowNoIndex || false,
+        includeUnmapped,
+      },
+      indexFilter,
+      ...(parsedFields.length > 0 ? { fields: parsedFields } : {}),
+    });
+
+    const body: { fields: FieldDescriptorRestResponse[]; indices: string[] } = { fields, indices };
+
+    return response.ok({
+      body,
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  } catch (error) {
+    if (
+      typeof error === 'object' &&
+      !!error?.isBoom &&
+      !!error?.output?.payload &&
+      typeof error?.output?.payload === 'object'
+    ) {
+      const payload = error?.output?.payload;
+      return response.notFound({
+        body: {
+          message: payload.message,
+          attributes: payload,
+        },
+      });
+    } else {
+      return response.notFound();
+    }
+  }
+};
+
+export const registerFieldForWildcard = (
+  router: IRouter,
+  getStartServices: StartServicesAccessor<
+    DataViewsServerPluginStartDependencies,
+    DataViewsServerPluginStart
+  >
+) => {
+  // handler
+  router.versioned.put({ path, access }).addVersion({ version, validate }, handler);
+  router.versioned.post({ path, access }).addVersion({ version, validate }, handler);
+  router.versioned
+    .get({ path, access })
+    .addVersion({ version, validate: { request: { query: querySchema } } }, handler);
+};

src/plugins/data_views/server/routes/fields_for.ts
@@ -1,142 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
-
-import { estypes } from '@elastic/elasticsearch';
-import { schema } from '@kbn/config-schema';
-import {
-  IRouter,
-  RequestHandler,
-  RouteValidatorFullConfig,
-  StartServicesAccessor,
-} from '@kbn/core/server';
-import { IndexPatternsFetcher } from '../fetcher';
-import type { DataViewsServerPluginStart, DataViewsServerPluginStartDependencies } from '../types';
-
-/**
- * Accepts one of the following:
- * 1. An array of field names
- * 2. A JSON-stringified array of field names
- * 3. A single field name (not comma-separated)
- * @returns an array of field names
- * @param fields
- */
-export const parseFields = (fields: string | string[]): string[] => {
-  if (Array.isArray(fields)) return fields;
-  try {
-    return JSON.parse(fields);
-  } catch (e) {
-    if (!fields.includes(',')) return [fields];
-    throw new Error(
-      'metaFields should be an array of field names, a JSON-stringified array of field names, or a single field name'
-    );
-  }
-};
-
-const path = '/api/index_patterns/_fields_for_wildcard';
-
-type IBody = { index_filter?: estypes.QueryDslQueryContainer } | undefined;
-interface IQuery {
-  pattern: string;
-  meta_fields: string | string[];
-  type?: string;
-  rollup_index?: string;
-  allow_no_index?: boolean;
-  include_unmapped?: boolean;
-  fields?: string[];
-}
-
-const validate: RouteValidatorFullConfig<{}, IQuery, IBody> = {
-  query: schema.object({
-    pattern: schema.string(),
-    meta_fields: schema.oneOf([schema.string(), schema.arrayOf(schema.string())], {
-      defaultValue: [],
-    }),
-    type: schema.maybe(schema.string()),
-    rollup_index: schema.maybe(schema.string()),
-    allow_no_index: schema.maybe(schema.boolean()),
-    include_unmapped: schema.maybe(schema.boolean()),
-    fields: schema.maybe(schema.oneOf([schema.string(), schema.arrayOf(schema.string())])),
-  }),
-  // not available to get request
-  body: schema.maybe(schema.object({ index_filter: schema.any() })),
-};
-const handler: RequestHandler<{}, IQuery, IBody> = async (context, request, response) => {
-  const { asCurrentUser } = (await context.core).elasticsearch.client;
-  const indexPatterns = new IndexPatternsFetcher(asCurrentUser);
-  const {
-    pattern,
-    meta_fields: metaFields,
-    type,
-    rollup_index: rollupIndex,
-    allow_no_index: allowNoIndex,
-    include_unmapped: includeUnmapped,
-  } = request.query;
-
-  // not available to get request
-  const indexFilter = request.body?.index_filter;
-
-  let parsedFields: string[] = [];
-  let parsedMetaFields: string[] = [];
-  try {
-    parsedMetaFields = parseFields(metaFields);
-    parsedFields = parseFields(request.query.fields ?? []);
-  } catch (error) {
-    return response.badRequest();
-  }
-
-  try {
-    const { fields, indices } = await indexPatterns.getFieldsForWildcard({
-      pattern,
-      metaFields: parsedMetaFields,
-      type,
-      rollupIndex,
-      fieldCapsOptions: {
-        allow_no_indices: allowNoIndex || false,
-        includeUnmapped,
-      },
-      indexFilter,
-      ...(parsedFields.length > 0 ? { fields: parsedFields } : {}),
-    });
-
-    return response.ok({
-      body: { fields, indices },
-      headers: {
-        'content-type': 'application/json',
-      },
-    });
-  } catch (error) {
-    if (
-      typeof error === 'object' &&
-      !!error?.isBoom &&
-      !!error?.output?.payload &&
-      typeof error?.output?.payload === 'object'
-    ) {
-      const payload = error?.output?.payload;
-      return response.notFound({
-        body: {
-          message: payload.message,
-          attributes: payload,
-        },
-      });
-    } else {
-      return response.notFound();
-    }
-  }
-};
-
-export const registerFieldForWildcard = (
-  router: IRouter,
-  getStartServices: StartServicesAccessor<
-    DataViewsServerPluginStartDependencies,
-    DataViewsServerPluginStart
-  >
-) => {
-  router.put({ path, validate }, handler);
-  router.post({ path, validate }, handler);
-  router.get({ path, validate }, handler);
-};

commit 4a8bbd970a632a82b2bdd54ff9a944c023f3c33d

[data views] fields_for_wildcard - disable schema response validation (#159758)
## Summary

Response validation is failing when there are field conflicts. Disable
response validation until a complete schema can be created.

Problem was introduced in https://github.com/elastic/kibana/pull/158608

src/plugins/data_views/server/rest_api_routes/internal/fields_for.ts
@@ -69,6 +69,7 @@ const fieldSubTypeSchema = schema.object({
   nested: schema.maybe(schema.object({ path: schema.string() })),
 });
 
+// @ts-expect-error
 const FieldDescriptorSchema = schema.object({
   aggregatable: schema.boolean(),
   name: schema.string(),
@@ -97,6 +98,7 @@ const validate: FullValidationConfig<any, any, any> = {
     // not available to get request
     body: schema.maybe(schema.object({ index_filter: schema.any() })),
   },
+  /*
   response: {
     200: {
       body: schema.object({
@@ -105,6 +107,7 @@ const validate: FullValidationConfig<any, any, any> = {
       }),
     },
   },
+  */
 };
 
 const handler: RequestHandler<{}, IQuery, IBody> = async (context, request, response) => {