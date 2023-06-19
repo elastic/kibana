1471c9246771e1dc606992ab7a5b5ee6d663fc78
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-06-19 20:10:36 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.test.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.test.tsx
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-import { useHistory } from 'react-router-dom';
 import { Route } from '@kbn/shared-ux-router';
 import React from 'react';
 import { fireEvent, act, waitFor } from '@testing-library/react';
