296f182cb4a8eaceb648fefc33b48074256ebc76
pgayvallet <pierre.gayvallet@elastic.co>
2023-06-19 08:26:22 +0200

    restructure test file

--- src/core/server/integration_tests/status/routes/status.test.ts
+++ src/core/server/integration_tests/status/routes/status.test.ts
@@ -180,262 +180,264 @@ describe('GET /api/status', () => {
     });
   });
 
-  it('returns basic server info & metrics', async () => {
-    await setupServer();
-    const result = await supertest(httpSetup.server.listener).get('/api/status').expect(200);
-
-    expect(result.body.name).toEqual('xkibana');
-    expect(result.body.uuid).toEqual('xxxx-xxxxx');
-    expect(result.body.version).toEqual({
-      number: '9.9.9',
-      build_hash: 'xsha',
-      build_number: 1234,
-      build_snapshot: true,
-      build_date: new Date('2023-05-15T23:12:09.000Z').toISOString(),
-    });
-    const metricsMockValue = await firstValueFrom(metrics.getOpsMetrics$());
-    expect(result.body.metrics).toEqual({
-      last_updated: expect.any(String),
-      collection_interval_in_millis: metrics.collectionInterval,
-      ...omit(metricsMockValue, ['collected_at']),
-      requests: {
-        ...metricsMockValue.requests,
-        status_codes: metricsMockValue.requests.statusCodes,
-      },
-    });
-  });
-
-  describe('legacy status format', () => {
-    const legacyFormat = {
-      overall: {
-        icon: 'success',
-        nickname: 'Looking good',
-        since: expect.any(String),
-        state: 'green',
-        title: 'Green',
-        uiColor: 'success',
-      },
-      statuses: [
-        {
-          icon: 'success',
-          id: 'core:elasticsearch@9.9.9',
-          message: 'Service is working',
-          since: expect.any(String),
-          state: 'green',
-          uiColor: 'success',
-        },
-        {
-          icon: 'success',
-          id: 'core:savedObjects@9.9.9',
-          message: 'Service is working',
-          since: expect.any(String),
-          state: 'green',
-          uiColor: 'success',
-        },
-        {
-          icon: 'success',
-          id: 'plugin:a@9.9.9',
-          message: 'a is available',
-          since: expect.any(String),
-          state: 'green',
-          uiColor: 'success',
-        },
-        {
-          icon: 'warning',
-          id: 'plugin:b@9.9.9',
-          message: 'b is degraded',
-          since: expect.any(String),
-          state: 'yellow',
-          uiColor: 'warning',
-        },
-        {
-          icon: 'danger',
-          id: 'plugin:c@9.9.9',
-          message: 'c is unavailable',
-          since: expect.any(String),
-          state: 'red',
-          uiColor: 'danger',
-        },
-        {
-          icon: 'danger',
-          id: 'plugin:d@9.9.9',
-          message: 'd is critical',
-          since: expect.any(String),
-          state: 'red',
-          uiColor: 'danger',
-        },
-      ],
-    };
-
-    it('returns legacy status format when v7format=true is provided', async () => {
-      await setupServer();
-      const result = await supertest(httpSetup.server.listener)
-        .get('/api/status?v7format=true')
-        .expect(200);
-      expect(result.body.status).toEqual(legacyFormat);
-      expect(incrementUsageCounter).toHaveBeenCalledTimes(1);
-      expect(incrementUsageCounter).toHaveBeenCalledWith({ counterName: 'status_v7format' });
-    });
-
-    it('returns legacy status format when v8format=false is provided', async () => {
-      await setupServer();
-      const result = await supertest(httpSetup.server.listener)
-        .get('/api/status?v8format=false')
-        .expect(200);
-      expect(result.body.status).toEqual(legacyFormat);
-      expect(incrementUsageCounter).toHaveBeenCalledTimes(1);
-      expect(incrementUsageCounter).toHaveBeenCalledWith({ counterName: 'status_v7format' });
-    });
-  });
-
-  describe('v8format', () => {
-    const newFormat = {
-      core: {
-        elasticsearch: {
-          level: 'available',
-          summary: 'Service is working',
-        },
-        savedObjects: {
-          level: 'available',
-          summary: 'Service is working',
-        },
-      },
-      overall: {
-        level: 'available',
-        summary: 'Service is working',
-      },
-      plugins: {
-        a: {
-          level: 'available',
-          summary: 'a is available',
-        },
-        b: {
-          level: 'degraded',
-          summary: 'b is degraded',
-        },
-        c: {
-          level: 'unavailable',
-          summary: 'c is unavailable',
-        },
-        d: {
-          level: 'critical',
-          summary: 'd is critical',
-        },
-      },
-    };
-
-    it('returns new status format when no query params are provided', async () => {
+  describe('allowAnonymous: true', () => {
+    it('returns basic server info & metrics', async () => {
       await setupServer();
       const result = await supertest(httpSetup.server.listener).get('/api/status').expect(200);
-      expect(result.body.status).toEqual(newFormat);
-      expect(incrementUsageCounter).not.toHaveBeenCalled();
-    });
 
-    it('returns new status format when v8format=true is provided', async () => {
-      await setupServer();
-      const result = await supertest(httpSetup.server.listener)
-        .get('/api/status?v8format=true')
-        .expect(200);
-      expect(result.body.status).toEqual(newFormat);
-      expect(incrementUsageCounter).not.toHaveBeenCalled();
-    });
-
-    it('returns new status format when v7format=false is provided', async () => {
-      await setupServer();
-      const result = await supertest(httpSetup.server.listener)
-        .get('/api/status?v7format=false')
-        .expect(200);
-      expect(result.body.status).toEqual(newFormat);
-      expect(incrementUsageCounter).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('invalid query parameters', () => {
-    it('v8format=true and v7format=true', async () => {
-      await setupServer();
-      await supertest(httpSetup.server.listener)
-        .get('/api/status?v8format=true&v7format=true')
-        .expect(400);
-      expect(incrementUsageCounter).not.toHaveBeenCalled();
-    });
-
-    it('v8format=true and v7format=false', async () => {
-      await setupServer();
-      await supertest(httpSetup.server.listener)
-        .get('/api/status?v8format=true&v7format=false')
-        .expect(400);
-      expect(incrementUsageCounter).not.toHaveBeenCalled();
-    });
-
-    it('v8format=false and v7format=false', async () => {
-      await setupServer();
-      await supertest(httpSetup.server.listener)
-        .get('/api/status?v8format=false&v7format=false')
-        .expect(400);
-      expect(incrementUsageCounter).not.toHaveBeenCalled();
-    });
-
-    it('v8format=false and v7format=true', async () => {
-      await setupServer();
-      await supertest(httpSetup.server.listener)
-        .get('/api/status?v8format=false&v7format=true')
-        .expect(400);
-      expect(incrementUsageCounter).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('status level and http response code', () => {
-    describe('using standard format', () => {
-      it('respond with a 200 when core.overall.status is available', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.available),
-        });
-        await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(200);
+      expect(result.body.name).toEqual('xkibana');
+      expect(result.body.uuid).toEqual('xxxx-xxxxx');
+      expect(result.body.version).toEqual({
+        number: '9.9.9',
+        build_hash: 'xsha',
+        build_number: 1234,
+        build_snapshot: true,
+        build_date: new Date('2023-05-15T23:12:09.000Z').toISOString(),
       });
-      it('respond with a 200 when core.overall.status is degraded', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.degraded),
-        });
-        await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(200);
-      });
-      it('respond with a 503 when core.overall.status is unavailable', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.unavailable),
-        });
-        await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(503);
-      });
-      it('respond with a 503 when core.overall.status is critical', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.critical),
-        });
-        await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(503);
+      const metricsMockValue = await firstValueFrom(metrics.getOpsMetrics$());
+      expect(result.body.metrics).toEqual({
+        last_updated: expect.any(String),
+        collection_interval_in_millis: metrics.collectionInterval,
+        ...omit(metricsMockValue, ['collected_at']),
+        requests: {
+          ...metricsMockValue.requests,
+          status_codes: metricsMockValue.requests.statusCodes,
+        },
       });
     });
 
-    describe('using legacy format', () => {
-      it('respond with a 200 when core.overall.status is available', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.available),
-        });
-        await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(200);
+    describe('legacy status format', () => {
+      const legacyFormat = {
+        overall: {
+          icon: 'success',
+          nickname: 'Looking good',
+          since: expect.any(String),
+          state: 'green',
+          title: 'Green',
+          uiColor: 'success',
+        },
+        statuses: [
+          {
+            icon: 'success',
+            id: 'core:elasticsearch@9.9.9',
+            message: 'Service is working',
+            since: expect.any(String),
+            state: 'green',
+            uiColor: 'success',
+          },
+          {
+            icon: 'success',
+            id: 'core:savedObjects@9.9.9',
+            message: 'Service is working',
+            since: expect.any(String),
+            state: 'green',
+            uiColor: 'success',
+          },
+          {
+            icon: 'success',
+            id: 'plugin:a@9.9.9',
+            message: 'a is available',
+            since: expect.any(String),
+            state: 'green',
+            uiColor: 'success',
+          },
+          {
+            icon: 'warning',
+            id: 'plugin:b@9.9.9',
+            message: 'b is degraded',
+            since: expect.any(String),
+            state: 'yellow',
+            uiColor: 'warning',
+          },
+          {
+            icon: 'danger',
+            id: 'plugin:c@9.9.9',
+            message: 'c is unavailable',
+            since: expect.any(String),
+            state: 'red',
+            uiColor: 'danger',
+          },
+          {
+            icon: 'danger',
+            id: 'plugin:d@9.9.9',
+            message: 'd is critical',
+            since: expect.any(String),
+            state: 'red',
+            uiColor: 'danger',
+          },
+        ],
+      };
+
+      it('returns legacy status format when v7format=true is provided', async () => {
+        await setupServer();
+        const result = await supertest(httpSetup.server.listener)
+          .get('/api/status?v7format=true')
+          .expect(200);
+        expect(result.body.status).toEqual(legacyFormat);
+        expect(incrementUsageCounter).toHaveBeenCalledTimes(1);
+        expect(incrementUsageCounter).toHaveBeenCalledWith({ counterName: 'status_v7format' });
       });
-      it('respond with a 200 when core.overall.status is degraded', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.degraded),
-        });
-        await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(200);
+
+      it('returns legacy status format when v8format=false is provided', async () => {
+        await setupServer();
+        const result = await supertest(httpSetup.server.listener)
+          .get('/api/status?v8format=false')
+          .expect(200);
+        expect(result.body.status).toEqual(legacyFormat);
+        expect(incrementUsageCounter).toHaveBeenCalledTimes(1);
+        expect(incrementUsageCounter).toHaveBeenCalledWith({ counterName: 'status_v7format' });
       });
-      it('respond with a 503 when core.overall.status is unavailable', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.unavailable),
-        });
-        await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(503);
+    });
+
+    describe('v8format', () => {
+      const newFormat = {
+        core: {
+          elasticsearch: {
+            level: 'available',
+            summary: 'Service is working',
+          },
+          savedObjects: {
+            level: 'available',
+            summary: 'Service is working',
+          },
+        },
+        overall: {
+          level: 'available',
+          summary: 'Service is working',
+        },
+        plugins: {
+          a: {
+            level: 'available',
+            summary: 'a is available',
+          },
+          b: {
+            level: 'degraded',
+            summary: 'b is degraded',
+          },
+          c: {
+            level: 'unavailable',
+            summary: 'c is unavailable',
+          },
+          d: {
+            level: 'critical',
+            summary: 'd is critical',
+          },
+        },
+      };
+
+      it('returns new status format when no query params are provided', async () => {
+        await setupServer();
+        const result = await supertest(httpSetup.server.listener).get('/api/status').expect(200);
+        expect(result.body.status).toEqual(newFormat);
+        expect(incrementUsageCounter).not.toHaveBeenCalled();
       });
-      it('respond with a 503 when core.overall.status is critical', async () => {
-        await setupServer({
-          coreOverall: createServiceStatus(ServiceStatusLevels.critical),
+
+      it('returns new status format when v8format=true is provided', async () => {
+        await setupServer();
+        const result = await supertest(httpSetup.server.listener)
+          .get('/api/status?v8format=true')
+          .expect(200);
+        expect(result.body.status).toEqual(newFormat);
+        expect(incrementUsageCounter).not.toHaveBeenCalled();
+      });
+
+      it('returns new status format when v7format=false is provided', async () => {
+        await setupServer();
+        const result = await supertest(httpSetup.server.listener)
+          .get('/api/status?v7format=false')
+          .expect(200);
+        expect(result.body.status).toEqual(newFormat);
+        expect(incrementUsageCounter).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('invalid query parameters', () => {
+      it('v8format=true and v7format=true', async () => {
+        await setupServer();
+        await supertest(httpSetup.server.listener)
+          .get('/api/status?v8format=true&v7format=true')
+          .expect(400);
+        expect(incrementUsageCounter).not.toHaveBeenCalled();
+      });
+
+      it('v8format=true and v7format=false', async () => {
+        await setupServer();
+        await supertest(httpSetup.server.listener)
+          .get('/api/status?v8format=true&v7format=false')
+          .expect(400);
+        expect(incrementUsageCounter).not.toHaveBeenCalled();
+      });
+
+      it('v8format=false and v7format=false', async () => {
+        await setupServer();
+        await supertest(httpSetup.server.listener)
+          .get('/api/status?v8format=false&v7format=false')
+          .expect(400);
+        expect(incrementUsageCounter).not.toHaveBeenCalled();
+      });
+
+      it('v8format=false and v7format=true', async () => {
+        await setupServer();
+        await supertest(httpSetup.server.listener)
+          .get('/api/status?v8format=false&v7format=true')
+          .expect(400);
+        expect(incrementUsageCounter).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('status level and http response code', () => {
+      describe('using standard format', () => {
+        it('respond with a 200 when core.overall.status is available', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.available),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(200);
         });
-        await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(503);
+        it('respond with a 200 when core.overall.status is degraded', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.degraded),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(200);
+        });
+        it('respond with a 503 when core.overall.status is unavailable', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.unavailable),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(503);
+        });
+        it('respond with a 503 when core.overall.status is critical', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.critical),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v8format=true').expect(503);
+        });
+      });
+
+      describe('using legacy format', () => {
+        it('respond with a 200 when core.overall.status is available', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.available),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(200);
+        });
+        it('respond with a 200 when core.overall.status is degraded', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.degraded),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(200);
+        });
+        it('respond with a 503 when core.overall.status is unavailable', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.unavailable),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(503);
+        });
+        it('respond with a 503 when core.overall.status is critical', async () => {
+          await setupServer({
+            coreOverall: createServiceStatus(ServiceStatusLevels.critical),
+          });
+          await supertest(httpSetup.server.listener).get('/api/status?v7format=true').expect(503);
+        });
       });
     });
   });
