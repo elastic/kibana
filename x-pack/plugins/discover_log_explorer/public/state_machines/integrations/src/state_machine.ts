906e7cd2386a703eb3a059f58d01c85bf8814327
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-06-19 16:26:57 +0200

    refactor(discover-log-explorer): fix local search value

--- x-pack/plugins/discover_log_explorer/public/state_machines/integrations/src/state_machine.ts
+++ x-pack/plugins/discover_log_explorer/public/state_machines/integrations/src/state_machine.ts
@@ -201,7 +201,7 @@ const searchIntegrationStreams = (
     return {
       ...integration,
       // Filter and sort the datasets by the search criteria
-      datasets: new EntityList<Dataset>(integration.dataStreams)
+      dataStreams: new EntityList<Dataset>(integration.dataStreams)
         .filterBy((stream) => Boolean(stream.title?.includes(nameQuery ?? '')))
         .sortBy('name', sortOrder)
         .build(),
