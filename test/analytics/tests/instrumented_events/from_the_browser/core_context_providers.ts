a831040e08e38feaed5828fb8cc08bbf7c872691
pgayvallet <pierre.gayvallet@elastic.co>
2023-06-19 16:09:12 +0200

    add the page's title to browser EBT context

--- test/analytics/tests/instrumented_events/from_the_browser/core_context_providers.ts
+++ test/analytics/tests/instrumented_events/from_the_browser/core_context_providers.ts
@@ -98,5 +98,10 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       expect(event.context).to.have.property('viewport_height');
       expect(event.context.viewport_height).to.be.a('number');
     });
+
+    it('should have the properties provided by the "page title" context provider', async () => {
+      expect(event.context).to.have.property('page_title');
+      expect(event.context.page_title).to.be.a('string');
+    });
   });
 }
