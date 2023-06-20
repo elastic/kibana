2afaa0f6da0fbc73d2fc8698b07d8510bfd062ff
Georgii Gorbachev <georgii.gorbachev@elastic.co>
2023-06-20 04:46:05 +0200

    Fix the dashboard itself

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_monitoring/logic/detection_engine_health/assets/install_assets_for_rule_monitoring.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_monitoring/logic/detection_engine_health/assets/install_assets_for_rule_monitoring.ts
@@ -7,11 +7,14 @@
 
 import type { ISavedObjectsImporter, Logger } from '@kbn/core/server';
 import { SavedObjectsUtils } from '@kbn/core/server';
+import { cloneDeep } from 'lodash';
 import pRetry from 'p-retry';
 import { Readable } from 'stream';
 
-import kibanaEventLogDataView from './kibana_event_log_data_view.json';
-import ruleMonitoringDashboard from './rule_monitoring_dashboard.json';
+import sourceRuleMonitoringDashboard from './dashboard_rule_monitoring.json';
+import sourceKibanaEventLogDataView from './data_view_kibana_event_log.json';
+import sourceManagedTag from './tag_managed.json';
+import sourceSecuritySolutionTag from './tag_security_solution.json';
 
 const MAX_RETRIES = 2;
 
@@ -64,7 +67,40 @@ export const installAssetsForRuleMonitoring = async (
 };
 
 const getAssetsForRuleMonitoring = (currentSpaceId: string) => {
-  return [kibanaEventLogDataView, ruleMonitoringDashboard];
+  const withSpaceId = appendSpaceId(currentSpaceId);
+
+  const assetRuleMonitoringDashboard = cloneDeep(sourceRuleMonitoringDashboard);
+  const assetKibanaEventLogDataView = cloneDeep(sourceKibanaEventLogDataView);
+  const assetManagedTag = cloneDeep(sourceManagedTag);
+  const assetSecuritySolutionTag = cloneDeep(sourceSecuritySolutionTag);
+
+  // Update ids of the assets to include the current space id
+  assetRuleMonitoringDashboard.id = withSpaceId('security-detection-rule-monitoring');
+  assetManagedTag.id = withSpaceId('fleet-managed');
+  assetSecuritySolutionTag.id = withSpaceId('security-solution');
+
+  // Update saved object references of the dashboard accordingly
+  assetRuleMonitoringDashboard.references = assetRuleMonitoringDashboard.references.map(
+    (reference) => {
+      if (reference.id === 'fleet-managed-<spaceId>') {
+        return { ...reference, id: assetManagedTag.id };
+      }
+      if (reference.id === 'security-solution-<spaceId>') {
+        return { ...reference, id: assetSecuritySolutionTag.id };
+      }
+
+      return reference;
+    }
+  );
+
+  return [
+    assetManagedTag,
+    assetSecuritySolutionTag,
+    assetKibanaEventLogDataView,
+    assetRuleMonitoringDashboard,
+  ];
 };
+
+const appendSpaceId = (spaceId: string) => (str: string) => `${str}-${spaceId}`;
 
 const spaceIdToNamespace = SavedObjectsUtils.namespaceStringToId;
