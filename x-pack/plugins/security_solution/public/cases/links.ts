2bc3bc40f24275aaa3b681c6a08fdd6ee9fbfeb4
semd <sergi.massaneda@elastic.co>
2023-06-20 12:35:12 +0200

    fix cases links types

--- x-pack/plugins/security_solution/public/cases/links.ts
+++ x-pack/plugins/security_solution/public/cases/links.ts
@@ -14,28 +14,30 @@ import { getCasesDeepLinks } from '@kbn/cases-plugin/public';
 import { CASES_FEATURE_ID, CASES_PATH, SecurityPageName } from '../../common/constants';
 import type { LinkItem } from '../common/links/types';
 
-const casesLinks = getCasesDeepLinks<LinkItem>({
-  basePath: CASES_PATH,
-  extend: {
-    [SecurityPageName.case]: {
-      globalNavPosition: 5,
-      capabilities: [`${CASES_FEATURE_ID}.${READ_CASES_CAPABILITY}`],
-    },
-    [SecurityPageName.caseConfigure]: {
+const casesDeepLinks = getCasesDeepLinks({ basePath: CASES_PATH });
+const { id, deepLinks, ...casesDeepLink } = casesDeepLinks;
+// Mind the array order returned by getCasesDeepLinks
+const [casesConfigureDeepLink, casesCreateDeepLink] = deepLinks;
+
+// Extends AppDeepLink to return LinkItem type
+export const links: LinkItem = {
+  ...casesDeepLink,
+  id: SecurityPageName.case,
+  globalNavPosition: 5,
+  capabilities: [`${CASES_FEATURE_ID}.${READ_CASES_CAPABILITY}`],
+  links: [
+    {
+      ...casesConfigureDeepLink,
+      id: SecurityPageName.caseConfigure,
       capabilities: [`${CASES_FEATURE_ID}.${UPDATE_CASES_CAPABILITY}`],
       licenseType: 'gold',
       sideNavDisabled: true,
     },
-    [SecurityPageName.caseCreate]: {
+    {
+      ...casesCreateDeepLink,
+      id: SecurityPageName.caseCreate,
       capabilities: [`${CASES_FEATURE_ID}.${CREATE_CASES_CAPABILITY}`],
       sideNavDisabled: true,
     },
-  },
-});
-const { id, deepLinks, ...rest } = casesLinks;
-
-export const links: LinkItem = {
-  ...rest,
-  id: SecurityPageName.case,
-  links: deepLinks as LinkItem[],
+  ],
 };
--- x-pack/plugins/security_solution/public/timelines/components/flyout/add_timeline_button/index.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/flyout/add_timeline_button/index.tsx
@@ -42,7 +42,7 @@ const AddTimelineButtonComponent: React.FC<AddTimelineButtonComponentProps> = ({
         data-test-subj="settings-plus-in-circle"
         iconType="plusInCircle"
         color="primary"
-        size="s"
+        size="m"
         onClick={onButtonClick}
         aria-label={i18n.ADD_TIMELINE}
       />
