526c8808b9526fdfd758936aa7eae78b22fa3578
Sloane Perrault <sloane.perrault@gmail.com>
2023-06-20 15:49:32 -0400

    add vector search content, no styling

--- x-pack/plugins/enterprise_search/public/applications/vector_search/components/vector_search_guide/vector_search_guide.tsx
+++ x-pack/plugins/enterprise_search/public/applications/vector_search/components/vector_search_guide/vector_search_guide.tsx
@@ -7,6 +7,7 @@
 
 import React from 'react';
 
+import { EuiLink, EuiHorizontalRule } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 
 import { SetVectorSearchChrome as SetPageChrome } from '../../../shared/kibana_chrome';
@@ -16,6 +17,20 @@ export const VectorSearchGuide: React.FC = () => (
   <EnterpriseSearchVectorSearchPageTemplate
     restrictWidth
     pageHeader={{
+      description: (
+        <p>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.description"
+            defaultMessage="Elasticsearch can be used as a vector database and search along with other semantic search methods."
+          />
+          <EuiLink>
+            <FormattedMessage
+              id="xpack.enterpriseSearch.vectorSearch.guide.descriptionLink"
+              defaultMessage="Learn more about vector searches."
+            />
+          </EuiLink>
+        </p>
+      ),
       pageTitle: (
         <FormattedMessage
           id="xpack.enterpriseSearch.vectorSearch.guide.pageTitle"
@@ -25,6 +40,166 @@ export const VectorSearchGuide: React.FC = () => (
     }}
   >
     <SetPageChrome />
-    <p>Vector Search Placeholder</p>
+    <section>
+      <div>
+        <h2>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.createIndex.title"
+            defaultMessage="Create an index"
+          />
+        </h2>
+        <p>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.createIndex.description"
+            defaultMessage="Start by creating an index with one or more {denseVector} fields."
+            values={{ denseVector: <span>dense_vector</span> }}
+          />
+        </p>
+      </div>
+      <div>
+        <pre>
+          <code>
+            {`PUT image-index
+{
+  "mappings": {
+    "properties": {
+      "image-vector": {
+        "type": "dense_vector",
+        "dims": 3,
+        "index": true,
+        "similarity": "l2_norm"
+      },
+      "title-vector": {
+        "type": "dense_vector",
+        "dims": 5,
+        "index": true,
+        "similarity": "l2_norm"
+      },
+      "title": {
+        "type": "text"
+      },
+      "file-type": {
+        "type": "keyword"
+      }
+    }
+  }
+}`}
+          </code>
+        </pre>
+      </div>
+    </section>
+    <EuiHorizontalRule />
+    <section>
+      <div>
+        <h2>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.ingest.title"
+            defaultMessage="Ingest your data"
+          />
+        </h2>
+        <p>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.ingest.description"
+            defaultMessage="Add data to your index to make it searchable."
+          />
+        </p>
+      </div>
+      <div>
+        <pre>
+          <code>
+            {`POST image-index/_bulk?refresh=true
+{ "index": { "_id": "1" } }
+{ "image-vector": [1, 5, -20], "title-vector": [12, 50, -10, 0, 1], "title": "moose family", "file-type": "jpg" }
+{ "index": { "_id": "2" } }
+{ "image-vector": [42, 8, -15], "title-vector": [25, 1, 4, -12, 2], "title": "alpine lake", "file-type": "png" }
+{ "index": { "_id": "3" } }
+{ "image-vector": [15, 11, 23], "title-vector": [1, 5, 25, 50, 20], "title": "full moon", "file-type": "jpg" }
+...`}
+          </code>
+        </pre>
+      </div>
+    </section>
+    <EuiHorizontalRule />
+    <section>
+      <div>
+        <h2>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.query.title"
+            defaultMessage="Build your vector search query"
+          />
+        </h2>
+        <p>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.query.description"
+            defaultMessage="Now you're ready to explore your data with searches and aggregations."
+          />
+        </p>
+      </div>
+      <div>
+        <pre>
+          <code>
+            {`POST image-index/_search
+{
+  "knn": {
+    "field": "image-vector",
+    "query_vector": [-5, 9, -12],
+    "k": 10,
+    "num_candidates": 100
+  },
+  "fields": [ "title", "file-type" ]
+}`}
+          </code>
+        </pre>
+      </div>
+    </section>
+    <EuiHorizontalRule />
+    <section>
+      <div>
+        <h2>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.deployedModel.title"
+            defaultMessage="Don’t have a model deployed?"
+          />
+        </h2>
+        <p>
+          <FormattedMessage
+            id="xpack.enterpriseSearch.vectorSearch.guide.deployedModel.description"
+            defaultMessage="Elastic can help you generate embeddings."
+          />
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <h3>
+              <FormattedMessage
+                id="xpack.enterpriseSearch.vectorSearch.guide.deployedModel.elser.title"
+                defaultMessage="Elastic Learned Sparse Encoder (ELSER)"
+              />
+            </h3>
+            <p>
+              <FormattedMessage
+                id="xpack.enterpriseSearch.vectorSearch.guide.deployedModel.elser.description"
+                defaultMessage="Learn about the configuration-free semantic search"
+              />
+            </p>
+          </li>
+          <li>
+            <h3>
+              <FormattedMessage
+                id="xpack.enterpriseSearch.vectorSearch.guide.deployedModel.byoModel.title"
+                defaultMessage="Run your models in Elastic"
+              />
+            </h3>
+            <p>
+              <FormattedMessage
+                id="xpack.enterpriseSearch.vectorSearch.guide.deployedModel.byoModel.description"
+                defaultMessage="Learn how to load in compatible third-party models"
+              />
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
   </EnterpriseSearchVectorSearchPageTemplate>
 );
