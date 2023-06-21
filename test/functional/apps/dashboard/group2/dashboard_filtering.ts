dd81f57b7a9842c87017b548eda48aadadc4eb26
Hannah Mudge <Heenawter@users.noreply.github.com>
2023-06-21 12:37:48 -0600

    [Dashboard] Unskip flaky "Add from library" flyout tests (#160187)
    Closes https://github.com/elastic/kibana/issues/158529
    Closes https://github.com/elastic/kibana/issues/159931
    Closes https://github.com/elastic/kibana/issues/159932
    
    ## Summary
    
    Now that https://github.com/elastic/kibana/pull/159943 is
    [backported](https://github.com/elastic/kibana/pull/160186) to 8.8,
    these tests should no longer be flaky on 8.8. Unskipping the tests.
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- test/functional/apps/dashboard/group2/dashboard_filtering.ts
+++ test/functional/apps/dashboard/group2/dashboard_filtering.ts
@@ -135,9 +135,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
     });
 
-    // FLAKY: https://github.com/elastic/kibana/issues/159931
-    // FLAKY: https://github.com/elastic/kibana/issues/159932
-    describe.skip('using a pinned filter that excludes all data', () => {
+    describe('using a pinned filter that excludes all data', () => {
       before(async () => {
         // Functional tests clear session storage after each suite, so it is important to repopulate unsaved panels
         await populateDashboard();
@@ -198,8 +196,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
     });
 
-    // FLAKY: https://github.com/elastic/kibana/issues/159933
-    describe.skip('disabling a filter unfilters the data on', function () {
+    describe('disabling a filter unfilters the data on', function () {
       before(async () => {
         // Functional tests clear session storage after each suite, so it is important to repopulate unsaved panels
         await populateDashboard();
