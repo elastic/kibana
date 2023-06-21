f965ad54d6f1e4a91fc0fa483d957480ab01058c
Maxim Palenov <maxim.palenov@elastic.co>
2023-06-21 15:23:23 +0200

    update response schema

--- x-pack/plugins/security_solution/common/detection_engine/rule_management/api/rules/coverage_overview/response_schema.ts
+++ x-pack/plugins/security_solution/common/detection_engine/rule_management/api/rules/coverage_overview/response_schema.ts
@@ -7,25 +7,19 @@
 
 import * as t from 'io-ts';
 import { NonEmptyArray } from '@kbn/securitysolution-io-ts-types';
-import { CoverageOverviewRuleActivity } from './request_schema';
+import { CoverageOverviewRuleActivitySchema } from './request_schema';
 
 export type CoverageOverviewRuleData = t.TypeOf<typeof CoverageOverviewRuleData>;
 export const CoverageOverviewRuleData = t.type({
   name: t.string,
-  activity: CoverageOverviewRuleActivity,
-});
-
-export type CoverageOverviewUnmappedRuleData = t.TypeOf<typeof CoverageOverviewUnmappedRuleData>;
-export const CoverageOverviewUnmappedRuleData = t.type({
-  id: t.string,
-  name: t.string,
+  activity: CoverageOverviewRuleActivitySchema,
 });
 
 export type CoverageOverviewResponse = t.TypeOf<typeof CoverageOverviewResponse>;
 export const CoverageOverviewResponse = t.exact(
   t.type({
     coverage: t.record(t.string, NonEmptyArray(t.string)),
+    unmapped_rule_ids: t.array(t.string),
     rules_data: t.record(t.string, NonEmptyArray(CoverageOverviewRuleData)),
-    unmapped_rules_data: t.array(CoverageOverviewUnmappedRuleData),
   })
 );
