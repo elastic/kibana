b0d64499073cdda36c5945ed092dabfa2af49dba
Sloane Perrault <sloane.perrault@gmail.com>
2023-06-21 16:03:13 -0400

    use proper colors for request examples

--- x-pack/plugins/enterprise_search/public/applications/vector_search/components/dev_tools_console_code_block/dev_tools_console_code_block.tsx
+++ x-pack/plugins/enterprise_search/public/applications/vector_search/components/dev_tools_console_code_block/dev_tools_console_code_block.tsx
@@ -18,6 +18,7 @@ import {
   EuiFlexGroup,
   EuiHorizontalRule,
   EuiPanel,
+  EuiThemeProvider,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 
@@ -47,33 +48,46 @@ export const DevToolsConsoleCodeBlock: React.FC<DevToolsConsoleCodeBlockProps> =
       );
 
   return (
-    <EuiPanel color="subdued" hasShadow={false}>
-      <EuiFlexGroup direction="column" gutterSize="xs">
-        <EuiFlexGroup direction="rowReverse">
-          {consolePreviewLink && (
-            <EuiButtonEmpty iconType="popout" href={consolePreviewLink} target="_blank">
-              <FormattedMessage
-                id="xpack.enterpriseSearch.component.devToolsConsoleCodeBlock.tryInConsole"
-                defaultMessage="Try in Console"
-              />
-            </EuiButtonEmpty>
-          )}
-          <EuiCopy textToCopy={children}>
-            {(copy) => (
-              <EuiButtonEmpty iconType="copyClipboard" onClick={copy}>
+    <EuiThemeProvider colorMode="dark">
+      <EuiPanel hasShadow={false}>
+        <EuiFlexGroup direction="column" gutterSize="xs">
+          <EuiFlexGroup direction="rowReverse" gutterSize="s">
+            {consolePreviewLink && (
+              <EuiButtonEmpty
+                iconType="popout"
+                color="success"
+                href={consolePreviewLink}
+                target="_blank"
+              >
                 <FormattedMessage
-                  id="xpack.enterpriseSearch.component.devToolsConsoleCodeBlock.copy"
-                  defaultMessage="Copy"
+                  id="xpack.enterpriseSearch.component.devToolsConsoleCodeBlock.tryInConsole"
+                  defaultMessage="Try in Console"
                 />
               </EuiButtonEmpty>
             )}
-          </EuiCopy>
+            <EuiCopy textToCopy={children}>
+              {(copy) => (
+                <EuiButtonEmpty iconType="copyClipboard" onClick={copy} color="text">
+                  <FormattedMessage
+                    id="xpack.enterpriseSearch.component.devToolsConsoleCodeBlock.copy"
+                    defaultMessage="Copy"
+                  />
+                </EuiButtonEmpty>
+              )}
+            </EuiCopy>
+          </EuiFlexGroup>
+          <EuiHorizontalRule margin="xs" />
+          <EuiCodeBlock
+            paddingSize="s"
+            fontSize="m"
+            transparentBackground
+            color="subduedText"
+            {...props}
+          >
+            {children}
+          </EuiCodeBlock>
         </EuiFlexGroup>
-        <EuiHorizontalRule margin="xs" />
-        <EuiCodeBlock paddingSize="s" fontSize="m" transparentBackground {...props}>
-          {children}
-        </EuiCodeBlock>
-      </EuiFlexGroup>
-    </EuiPanel>
+      </EuiPanel>
+    </EuiThemeProvider>
   );
 };
