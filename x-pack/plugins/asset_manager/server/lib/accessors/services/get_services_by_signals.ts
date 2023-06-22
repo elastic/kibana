1c00b0be8631ee999584d2bc6f28db037a4a9859
klacabane <kevin.lacabane@elastic.co>
2023-06-22 17:56:01 +0200

    services route

---
+++ x-pack/plugins/asset_manager/server/lib/accessors/services/get_services_by_signals.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { Asset } from '../../../../common/types_api';
+import { GetServicesOptionsInjected } from '.';
+import { collectServices } from '../../implicit_collection/collectors/services';
+
+export async function getServicesBySignals(
+  options: GetServicesOptionsInjected
+): Promise<{ services: Asset[] }> {
+  const filters: { [term: string]: string } = {};
+  if (options.parent) {
+    filters['host.hostname'] = options.parent;
+  }
+
+  const { assets } = await collectServices({
+    client: options.esClient,
+    from: options.from,
+    to: options.to,
+    sourceIndices: options.sourceIndices,
+    filters,
+  });
+
+  return { services: assets };
+}
