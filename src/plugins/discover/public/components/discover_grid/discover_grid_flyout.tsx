054ad7f0ce503f3614b756b66da56e3015493b76
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-06-22 14:09:04 +0200

    update data-test-subj for discover_grid_flyout

--- src/plugins/discover/public/components/discover_grid/discover_grid_flyout.tsx
+++ src/plugins/discover/public/components/discover_grid/discover_grid_flyout.tsx
@@ -156,7 +156,7 @@ export function DiscoverGridFlyout({
                     iconSize="s"
                     iconType="document"
                     flush="left"
-                    data-test-subj="docTableRowAction"
+                    data-test-subj="docTableViewSingleDocument"
                     href={singleDocHref}
                     onClick={onOpenSingleDoc}
                   >
@@ -176,7 +176,7 @@ export function DiscoverGridFlyout({
                         flush="left"
                         onClick={onOpenContextView}
                         href={contextViewHref}
-                        data-test-subj="docTableRowAction"
+                        data-test-subj="docTableViewSurroundingDocuments"
                       >
                         {i18n.translate(
                           'discover.grid.tableRow.viewSurroundingDocumentsLinkTextSimple',
