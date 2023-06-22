44f1f95328c346cc58dc5cceeabe7a0f2b9fb17e
Tiago Costa <tiago.costa@elastic.co>
2023-06-22 18:01:00 +0100

    chore(NA): eslint fix

--- x-pack/test/apm_api_integration/tests/transactions/transactions_groups_main_statistics.ts
+++ x-pack/test/apm_api_integration/tests/transactions/transactions_groups_main_statistics.ts
@@ -50,7 +50,6 @@ export default function ApiTest({ getService }: FtrProviderContext) {
     }
   );
 
-
   // FAILING FORWARD ES COMPATIBILITY: https://github.com/elastic/kibana/issues/160281
   registry.when.skip(
     'Transaction groups main statistics when data is loaded',
