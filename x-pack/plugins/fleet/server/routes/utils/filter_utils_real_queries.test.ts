fe94085cfb330b2dc0535899cf70fd13f138d4b5
criamico <mariacristina.amico@elastic.co>
2023-06-22 15:17:03 +0200

    Add more tests

---
+++ x-pack/plugins/fleet/server/routes/utils/filter_utils_real_queries.test.ts
@@ -0,0 +1,590 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import * as esKuery from '@kbn/es-query';
+
+import {
+  AGENT_POLICY_SAVED_OBJECT_TYPE,
+  PACKAGE_POLICY_SAVED_OBJECT_TYPE,
+  AGENTS_PREFIX,
+} from '../../constants';
+
+import { normalizeKuery } from '../../services/saved_object';
+
+import { validateFilterKueryNode } from './filter_utils';
+
+const agentPoliciesMappings = {
+  properties: {
+    name: { type: 'keyword' },
+    schema_version: { type: 'version' },
+    description: { type: 'text' },
+    namespace: { type: 'keyword' },
+    is_managed: { type: 'boolean' },
+    is_default: { type: 'boolean' },
+    is_default_fleet_server: { type: 'boolean' },
+    status: { type: 'keyword' },
+    unenroll_timeout: { type: 'integer' },
+    inactivity_timeout: { type: 'integer' },
+    updated_at: { type: 'date' },
+    updated_by: { type: 'keyword' },
+    revision: { type: 'integer' },
+    monitoring_enabled: { type: 'keyword', index: false },
+    is_preconfigured: { type: 'keyword' },
+    data_output_id: { type: 'keyword' },
+    monitoring_output_id: { type: 'keyword' },
+    download_source_id: { type: 'keyword' },
+    fleet_server_host_id: { type: 'keyword' },
+    agent_features: {
+      properties: {
+        name: { type: 'keyword' },
+        enabled: { type: 'boolean' },
+      },
+    },
+    is_protected: { type: 'boolean' },
+    overrides: { type: 'flattened', index: false },
+  },
+} as const;
+
+const packagePoliciesMappings = {
+  properties: {
+    name: { type: 'keyword' },
+    description: { type: 'text' },
+    namespace: { type: 'keyword' },
+    enabled: { type: 'boolean' },
+    is_managed: { type: 'boolean' },
+    policy_id: { type: 'keyword' },
+    package: {
+      properties: {
+        name: { type: 'keyword' },
+        title: { type: 'keyword' },
+        version: { type: 'keyword' },
+      },
+    },
+    elasticsearch: {
+      dynamic: false,
+      properties: {},
+    },
+    vars: { type: 'flattened' },
+    inputs: {
+      dynamic: false,
+      properties: {},
+    },
+    secret_references: { properties: { id: { type: 'keyword' } } },
+    revision: { type: 'integer' },
+    updated_at: { type: 'date' },
+    updated_by: { type: 'keyword' },
+    created_at: { type: 'date' },
+    created_by: { type: 'keyword' },
+  },
+} as const;
+
+const agentMappings = {
+  properties: {
+    access_api_key_id: {
+      type: 'keyword',
+    },
+    action_seq_no: {
+      type: 'integer',
+      index: false,
+    },
+    active: {
+      type: 'boolean',
+    },
+    agent: {
+      properties: {
+        id: {
+          type: 'keyword',
+        },
+        version: {
+          type: 'keyword',
+        },
+      },
+    },
+    default_api_key: {
+      type: 'keyword',
+    },
+    default_api_key_id: {
+      type: 'keyword',
+    },
+    default_api_key_history: {
+      type: 'object',
+      enabled: false,
+    },
+    enrolled_at: {
+      type: 'date',
+    },
+    last_checkin: {
+      type: 'date',
+    },
+    last_checkin_status: {
+      type: 'keyword',
+    },
+    last_checkin_message: {
+      type: 'text',
+      fields: {
+        keyword: {
+          type: 'keyword',
+          ignore_above: 1024,
+        },
+      },
+    },
+    components: {
+      type: 'object',
+      enabled: false,
+    },
+    last_updated: {
+      type: 'date',
+    },
+    local_metadata: {
+      properties: {
+        elastic: {
+          properties: {
+            agent: {
+              properties: {
+                build: {
+                  properties: {
+                    original: {
+                      type: 'text',
+                      fields: {
+                        keyword: {
+                          type: 'keyword',
+                          ignore_above: 256,
+                        },
+                      },
+                    },
+                  },
+                },
+                id: {
+                  type: 'keyword',
+                },
+                log_level: {
+                  type: 'keyword',
+                },
+                snapshot: {
+                  type: 'boolean',
+                },
+                upgradeable: {
+                  type: 'boolean',
+                },
+                version: {
+                  type: 'text',
+                  fields: {
+                    keyword: {
+                      type: 'keyword',
+                      ignore_above: 16,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        host: {
+          properties: {
+            architecture: {
+              type: 'keyword',
+            },
+            hostname: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 256,
+                },
+              },
+            },
+            id: {
+              type: 'keyword',
+            },
+            ip: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 64,
+                },
+              },
+            },
+            mac: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 17,
+                },
+              },
+            },
+            name: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 256,
+                },
+              },
+            },
+          },
+        },
+        os: {
+          properties: {
+            family: {
+              type: 'keyword',
+            },
+            full: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 128,
+                },
+              },
+            },
+            kernel: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 128,
+                },
+              },
+            },
+            name: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 256,
+                },
+              },
+            },
+            platform: {
+              type: 'keyword',
+            },
+            version: {
+              type: 'text',
+              fields: {
+                keyword: {
+                  type: 'keyword',
+                  ignore_above: 32,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    packages: {
+      type: 'keyword',
+    },
+    policy_coordinator_idx: {
+      type: 'integer',
+    },
+    policy_id: {
+      type: 'keyword',
+    },
+    policy_revision_idx: {
+      type: 'integer',
+    },
+    shared_id: {
+      type: 'keyword',
+    },
+    enrollment_id: {
+      type: 'keyword',
+    },
+    type: {
+      type: 'keyword',
+    },
+    unenrolled_at: {
+      type: 'date',
+    },
+    unenrollment_started_at: {
+      type: 'date',
+    },
+    updated_at: {
+      type: 'date',
+    },
+    upgrade_started_at: {
+      type: 'date',
+    },
+    upgraded_at: {
+      type: 'date',
+    },
+    user_provided_metadata: {
+      type: 'object',
+      enabled: false,
+    },
+    tags: {
+      type: 'keyword',
+    },
+  },
+} as const;
+
+describe('ValidateFilterKueryNode validates real kueries through KueryNode', () => {
+  describe('Agent policies kuerys', () => {
+    it('Test 1', async () => {
+      const validationObject = validateFilterKueryNode({
+        astFilter: esKuery.fromKueryExpression(
+          `${AGENT_POLICY_SAVED_OBJECT_TYPE}.download_source_id:some_id or (not ${AGENT_POLICY_SAVED_OBJECT_TYPE}.download_source_id:*)`
+        ),
+        types: [AGENT_POLICY_SAVED_OBJECT_TYPE],
+        indexMapping: agentPoliciesMappings,
+        storeValue: true,
+      });
+
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'ingest-agent-policies.download_source_id',
+          type: 'ingest-agent-policies',
+        },
+        {
+          astPath: 'arguments.1.arguments.0',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'ingest-agent-policies.download_source_id',
+          type: 'ingest-agent-policies',
+        },
+      ]);
+    });
+
+    it('Test 2', async () => {
+      const astFilter = esKuery.fromKueryExpression(
+        `${AGENT_POLICY_SAVED_OBJECT_TYPE}.data_output_id: test_id or ${AGENT_POLICY_SAVED_OBJECT_TYPE}.monitoring_output_id: test_id  or (not ${AGENT_POLICY_SAVED_OBJECT_TYPE}.data_output_id:*)`
+      );
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [AGENT_POLICY_SAVED_OBJECT_TYPE],
+        indexMapping: agentPoliciesMappings,
+        storeValue: true,
+      });
+
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'ingest-agent-policies.data_output_id',
+          type: 'ingest-agent-policies',
+        },
+        {
+          astPath: 'arguments.1',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'ingest-agent-policies.monitoring_output_id',
+          type: 'ingest-agent-policies',
+        },
+        {
+          astPath: 'arguments.2.arguments.0',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'ingest-agent-policies.data_output_id',
+          type: 'ingest-agent-policies',
+        },
+      ]);
+    });
+
+    it('Test 3', async () => {
+      const astFilter = esKuery.fromKueryExpression(
+        `${AGENT_POLICY_SAVED_OBJECT_TYPE}.data_output_id: test_id`
+      );
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [AGENT_POLICY_SAVED_OBJECT_TYPE],
+        indexMapping: agentPoliciesMappings,
+        storeValue: true,
+      });
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'ingest-agent-policies.data_output_id',
+          type: 'ingest-agent-policies',
+        },
+      ]);
+    });
+
+    it('Test 4', async () => {
+      const astFilter = esKuery.fromKueryExpression(
+        `${AGENT_POLICY_SAVED_OBJECT_TYPE}.inactivity_timeout:*`
+      );
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [AGENT_POLICY_SAVED_OBJECT_TYPE],
+        indexMapping: agentPoliciesMappings,
+        storeValue: true,
+      });
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'ingest-agent-policies.inactivity_timeout',
+          type: 'ingest-agent-policies',
+        },
+      ]);
+    });
+
+    it('returns error if the attribute does not exist - test 5', async () => {
+      const astFilter = esKuery.fromKueryExpression(
+        `${AGENT_POLICY_SAVED_OBJECT_TYPE}.package_policies:test_id_1 or ${AGENT_POLICY_SAVED_OBJECT_TYPE}.package_policies:test_id_2`
+      );
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [AGENT_POLICY_SAVED_OBJECT_TYPE],
+        indexMapping: agentPoliciesMappings,
+        storeValue: true,
+      });
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error:
+            "This key 'ingest-agent-policies.package_policies' does NOT exist in ingest-agent-policies saved object index patterns",
+          isSavedObjectAttr: false,
+          key: 'ingest-agent-policies.package_policies',
+          type: 'ingest-agent-policies',
+        },
+        {
+          astPath: 'arguments.1',
+          error:
+            "This key 'ingest-agent-policies.package_policies' does NOT exist in ingest-agent-policies saved object index patterns",
+          isSavedObjectAttr: false,
+          key: 'ingest-agent-policies.package_policies',
+          type: 'ingest-agent-policies',
+        },
+      ]);
+    });
+  });
+
+  describe('Package policies kuerys', () => {
+    it('Test 1', async () => {
+      const astFilter = esKuery.fromKueryExpression(
+        normalizeKuery(
+          PACKAGE_POLICY_SAVED_OBJECT_TYPE,
+          `${PACKAGE_POLICY_SAVED_OBJECT_TYPE}.package.name:packageName`
+        )
+      );
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [PACKAGE_POLICY_SAVED_OBJECT_TYPE],
+        indexMapping: packagePoliciesMappings,
+        storeValue: true,
+      });
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: false,
+          key: 'ingest-package-policies.attributes.package.name',
+          type: 'ingest-package-policies',
+        },
+      ]);
+    });
+  });
+
+  describe('Agents kuerys', () => {
+    it('Test 1 - search policy id', async () => {
+      const astFilter = esKuery.fromKueryExpression(`${AGENTS_PREFIX}.policy_id: "policy_id"`);
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [AGENTS_PREFIX],
+        indexMapping: agentMappings,
+        storeValue: true,
+      });
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'fleet-agents.policy_id',
+          type: 'fleet-agents',
+        },
+      ]);
+    });
+
+    it('Test 2 - search by multiple ids', async () => {
+      const astFilter = esKuery.fromKueryExpression(
+        `${AGENTS_PREFIX}.agent.attributes.id : (id_1 or id_2)`
+      );
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [AGENTS_PREFIX],
+        indexMapping: agentMappings,
+        storeValue: true,
+      });
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: false,
+          key: 'fleet-agents.agent.attributes.id',
+          type: 'fleet-agents',
+        },
+        {
+          astPath: 'arguments.1',
+          error: null,
+          isSavedObjectAttr: false,
+          key: 'fleet-agents.agent.attributes.id',
+          type: 'fleet-agents',
+        },
+      ]);
+    });
+
+    it('Test 3 - search agent by policy Id and enrolled since more than 10m', async () => {
+      const astFilter = esKuery.fromKueryExpression(
+        `${AGENTS_PREFIX}.policy_id: "policyId" and not (_exists_: "${AGENTS_PREFIX}.unenrolled_at") and ${AGENTS_PREFIX}.enrolled_at >= now-10m`
+      );
+      const validationObject = validateFilterKueryNode({
+        astFilter,
+        types: [AGENTS_PREFIX],
+        indexMapping: agentMappings,
+        storeValue: true,
+      });
+      expect(validationObject).toEqual([
+        {
+          astPath: 'arguments.0',
+          error: null,
+          isSavedObjectAttr: false,
+          key: 'fleet-agents.policy_id',
+          type: 'fleet-agents',
+        },
+        {
+          astPath: 'arguments.1',
+          error: null,
+          isSavedObjectAttr: false,
+          key: 'fleet-agents.agent.attributes.id',
+          type: 'fleet-agents',
+        },
+        {
+          astPath: 'arguments.2',
+          error: null,
+          isSavedObjectAttr: true,
+          key: 'fleet-agents.enrolled_at',
+          type: 'fleet-agents',
+        },
+      ]);
+    });
+  });
+
+  // describe('#validateConvertFilterToKueryNode', () => {
+  //   describe('Validate real kueries', () => {
+  //     it('Agent policies kuerys - test 1', async () => {
+  //       const converted = validateConvertFilterToKueryNode(
+  //         [AGENT_POLICY_SAVED_OBJECT_TYPE],
+  //         `${AGENT_POLICY_SAVED_OBJECT_TYPE}.is_managed:true`,
+  //         agentPoliciesMappings
+  //       );
+
+  //       expect(validationObject).toEqual(
+  //         esKuery.fromKueryExpression(`${AGENT_POLICY_SAVED_OBJECT_TYPE}.is_managed: "true"`)
+  //       );
+  //     });
+  //   });
+  // });
+});
