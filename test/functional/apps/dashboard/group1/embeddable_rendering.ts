3e974f36162bb8d527a37e91653b75c93a8bc8a7
Hannah Mudge <Heenawter@users.noreply.github.com>
2023-06-23 14:28:34 -0600

    [Dashboard] Re-skip flaky embeddable rendering tests (#160449)
    Closes https://github.com/elastic/kibana/issues/132865
    Closes https://github.com/elastic/kibana/issues/158529
    Closes https://github.com/elastic/kibana/issues/160070
    Closes https://github.com/elastic/kibana/issues/160071
    Closes https://github.com/elastic/kibana/issues/160072
    Closes https://github.com/elastic/kibana/issues/160073
    Closes https://github.com/elastic/kibana/issues/160074
    
    ## Summary
    
    https://github.com/elastic/kibana/pull/160187 didn't work as a fix, so
    re-skipping these tests **just for 8.8** as described
    [here](https://github.com/elastic/kibana/issues/159931#issuecomment-1602793541)
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- test/functional/apps/dashboard/group1/embeddable_rendering.ts
+++ test/functional/apps/dashboard/group1/embeddable_rendering.ts
@@ -100,7 +100,14 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     await dashboardExpect.vegaTextsDoNotExist(['5,000']);
   };
 
-  describe('dashboard embeddable rendering', function describeIndexTests() {
+  // FLAKY: https://github.com/elastic/kibana/issues/132865
+  // FLAKY: https://github.com/elastic/kibana/issues/158529
+  // FLAKY: https://github.com/elastic/kibana/issues/160070
+  // FLAKY: https://github.com/elastic/kibana/issues/160071
+  // FLAKY: https://github.com/elastic/kibana/issues/160072
+  // FLAKY: https://github.com/elastic/kibana/issues/160073
+  // FLAKY: https://github.com/elastic/kibana/issues/160074
+  describe.skip('dashboard embeddable rendering', function describeIndexTests() {
     before(async () => {
       await security.testUser.setRoles(['kibana_admin', 'animals', 'test_logstash_reader']);
       await kibanaServer.savedObjects.cleanStandardList();
