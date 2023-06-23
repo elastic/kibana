c1749a41e480f924ae01d6b260cef5c26efd1d83
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-06-23 13:51:48 +0200

    sort results before handing off to AI assistant

--- x-pack/plugins/infra/public/alerting/log_threshold/components/alert_details_app_section/components/explain_log_rate_spike.tsx
+++ x-pack/plugins/infra/public/alerting/log_threshold/components/alert_details_app_section/components/explain_log_rate_spike.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { FC, useEffect, useState } from 'react';
-import { pick } from 'lodash';
+import { pick, orderBy } from 'lodash';
 import moment from 'moment';
 
 import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiTitle } from '@elastic/eui';
@@ -164,15 +164,17 @@ export const ExplainLogRateSpikes: FC<AlertDetailsExplainLogRateSpikesSectionPro
   const onAnalysisCompleted = (
     analysisResults: ExplainLogRateSpikesAnalysisResults | undefined
   ) => {
-    const fieldValuePairs = analysisResults?.significantTerms?.map((item) => ({
-      field: item.fieldName,
-      value: item.fieldValue,
-      docCount: item.doc_count,
-      pValue: item.pValue,
-    }));
-    setLogSpikeParams(
-      fieldValuePairs ? { significantFieldValues: fieldValuePairs?.slice(0, 50) } : undefined
-    );
+    const significantFieldValues = orderBy(
+      analysisResults?.significantTerms?.map((item) => ({
+        field: item.fieldName,
+        value: item.fieldValue,
+        docCount: item.doc_count,
+        pValue: item.pValue,
+      })),
+      ['pValue', 'docCount'],
+      ['asc', 'asc']
+    ).slice(0, 50);
+    setLogSpikeParams(significantFieldValues ? { significantFieldValues } : undefined);
   };
 
   const coPilotService = useCoPilot();
