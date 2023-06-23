fb3431c2ab790899c3bf5926c63681f67a92183f
Marshall Main <marshall.main@elastic.co>
2023-06-23 13:29:14 -0700

    Fix imports

--- x-pack/test/detection_engine_api_integration/utils/set_signal_status.ts
+++ x-pack/test/detection_engine_api_integration/utils/set_signal_status.ts
@@ -5,10 +5,7 @@
  * 2.0.
  */
 
-import type {
-  Status,
-  SignalIds,
-} from '@kbn/security-solution-plugin/common/detection_engine/schemas/common';
+import type { Status, SignalIds } from '@kbn/security-solution-plugin/common/api/detection_engine';
 
 export const setSignalStatus = ({
   signalIds,
