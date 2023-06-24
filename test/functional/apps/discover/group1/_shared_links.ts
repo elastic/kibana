306d6ca923f067718f387be7ce36dd033abfb2f7
Davis McPhee <davis.mcphee@elastic.co>
2023-06-23 22:06:50 -0300

    Revert "skip failing test suite (#158465)"
    This reverts commit 9bb0b0dd01fba13648972037d23cea3a73280923.

--- test/functional/apps/discover/group1/_shared_links.ts
+++ test/functional/apps/discover/group1/_shared_links.ts
@@ -21,8 +21,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const toasts = getService('toasts');
   const deployment = getService('deployment');
 
-  // Failing: See https://github.com/elastic/kibana/issues/158465
-  describe.skip('shared links', function describeIndexTests() {
+  describe('shared links', function describeIndexTests() {
     let baseUrl: string;
 
     async function setup({ storeStateInSessionStorage }: { storeStateInSessionStorage: boolean }) {
