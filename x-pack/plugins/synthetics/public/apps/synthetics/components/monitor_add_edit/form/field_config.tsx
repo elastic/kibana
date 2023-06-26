b4d3df589ba73d6a1cecf8a40cc24d9300099ef8
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-06-26 19:13:59 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_add_edit/form/field_config.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/monitor_add_edit/form/field_config.tsx
@@ -445,7 +445,8 @@ export const FIELD = (readOnly?: boolean): FieldMap => ({
               content={
                 option.isInvalid
                   ? i18n.translate('xpack.synthetics.monitorConfig.locations.attachedPolicy', {
-                      defaultMessage: 'The attached agent policy for this location has been deleted.',
+                      defaultMessage:
+                        'The attached agent policy for this location has been deleted.',
                     })
                   : ''
               }
