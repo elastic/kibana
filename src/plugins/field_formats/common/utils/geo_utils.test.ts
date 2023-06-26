15e5d2aedb1f26cd6c8b5f4c1e783a004d2b350b
Sean Sullivan <ssullivan@spectric.com>
2023-06-26 11:14:52 -0400

    Move tests to be colocated with implementation

--- src/plugins/field_formats/common/utils/geo_utils.test.ts
+++ src/plugins/field_formats/common/utils/geo_utils.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { ddToMGRS, mgrsToDD, ddToUTM, utmToDD } from './utils';
+import { ddToMGRS, mgrsToDD, ddToUTM, utmToDD } from './geo_utils';
 
 describe('MGRS', () => {
   test('ddToMGRS should convert lat lon to MGRS', () => {
