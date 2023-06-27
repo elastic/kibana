ac12b89beee5c00069acf6f04c088f1731c5dcb6
nreese <reese.nathan@elastic.co>
2023-06-27 15:58:18 -0600

    fix Failing test: X-Pack Saved Object Tagging Functional Tests.x-pack/test/saved_object_tagging/functional/tests/dashboard_integration·ts - saved objects tagging - functional tests dashboard integration creating allows to select tags for a new dashboard

--- test/functional/page_objects/dashboard_page.ts
+++ test/functional/page_objects/dashboard_page.ts
@@ -473,10 +473,9 @@ export class DashboardPageObject extends FtrService {
   public async saveDashboard(
     dashboardName: string,
     saveOptions: SaveDashboardOptions = { waitDialogIsClosed: true, exitFromEditMode: true },
-    clickMenuItem = true
   ) {
     await this.retry.try(async () => {
-      await this.enterDashboardTitleAndClickSave(dashboardName, saveOptions, clickMenuItem);
+      await this.enterDashboardTitleAndClickSave(dashboardName, saveOptions);
 
       if (saveOptions.needsConfirm) {
         await this.ensureDuplicateTitleCallout();
@@ -517,9 +516,11 @@ export class DashboardPageObject extends FtrService {
   public async enterDashboardTitleAndClickSave(
     dashboardTitle: string,
     saveOptions: SaveDashboardOptions = { waitDialogIsClosed: true },
-    clickMenuItem = true
   ) {
-    if (clickMenuItem) {
+    const isSaveModalOpen = await this.testSubjects.exists('savedObjectSaveModal', {
+      timeout: 2000,
+    });
+    if (!isSaveModalOpen) {
       await this.testSubjects.click('dashboardSaveMenuItem');
     }
     const modalDialog = await this.testSubjects.find('savedObjectSaveModal');
