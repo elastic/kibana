34a0afd109665a39745e0e36506474311863798d
Sébastien Loix <sabee77@gmail.com>
2023-06-28 14:26:47 +0100

    Set the help menu links from cloud integration plugin

---
+++ x-pack/plugins/cloud_integrations/cloud_links/public/maybe_add_cloud_links/help_menu_links.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { i18n } from '@kbn/i18n';
+import { ChromeHelpMenuLink } from '@kbn/core-chrome-browser';
+import { DocLinksStart } from '@kbn/core-doc-links-browser';
+import { KIBANA_FEEDBACK_LINK } from '@kbn/core-chrome-browser-internal';
+
+export const createHelpMenuLinks = ({
+  docLinks,
+  helpSupportUrl,
+}: {
+  docLinks: DocLinksStart;
+  helpSupportUrl: string;
+}) => {
+  const helpMenuLinks: ChromeHelpMenuLink[] = [
+    {
+      title: i18n.translate('xpack.cloudLinks.helpMenuLinks.documentation', {
+        defaultMessage: 'Documentation',
+      }),
+      href: docLinks.links.elasticStackGetStarted,
+    },
+    {
+      title: i18n.translate('xpack.cloudLinks.helpMenuLinks.support', {
+        defaultMessage: 'Support',
+      }),
+      href: helpSupportUrl,
+    },
+    {
+      title: i18n.translate('xpack.cloudLinks.helpMenuLinks.giveFeedback', {
+        defaultMessage: 'Give feedback',
+      }),
+      href: KIBANA_FEEDBACK_LINK,
+    },
+  ];
+
+  return helpMenuLinks;
+};
