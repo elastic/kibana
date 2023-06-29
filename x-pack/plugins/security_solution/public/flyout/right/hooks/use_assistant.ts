4235fec6516ddb0bb1ecd24a46cd23440e08af49
Steph Milovic <stephanie.milovic@elastic.co>
2023-06-29 08:48:05 -0500

    create hook

--- x-pack/plugins/security_solution/public/flyout/right/hooks/use_assistant.ts
+++ x-pack/plugins/security_solution/public/flyout/right/hooks/use_assistant.ts
@@ -8,7 +8,7 @@
 import type { TimelineEventsDetailsItem } from '@kbn/timelines-plugin/common';
 import { useAssistantOverlay } from '@kbn/elastic-assistant';
 import { useCallback } from 'react';
-import { useIsExperimentalFeatureEnabled } from '../../../common/hooks/use_experimental_features';
+import { useAssistantAvailability } from '../../../assistant/use_assistant_availability';
 import { getPromptContextFromEventDetailsItem } from '../../../assistant/helpers';
 import {
   ALERT_SUMMARY_CONTEXT_DESCRIPTION,
@@ -56,7 +56,7 @@ export const useAssistant = ({
   dataFormattedForFieldBrowser,
   isAlert,
 }: UseAssistantParams): UseAssistantResult => {
-  const isAssistantEnabled = useIsExperimentalFeatureEnabled('assistantEnabled');
+  const { isAssistantEnabled } = useAssistantAvailability();
   const useAssistantHook = isAssistantEnabled ? useAssistantOverlay : useAssistantNoop;
   const getPromptContext = useCallback(
     async () => getPromptContextFromEventDetailsItem(dataFormattedForFieldBrowser ?? []),
