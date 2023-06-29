22db7eef688594f71b40cecd57f46bc756333396
Sander Philipse <sander.philipse@elastic.co>
2023-06-29 16:52:25 +0200

    Add batching and unit test

--- x-pack/plugins/serverless_search/server/routes/indices_routes.ts
+++ x-pack/plugins/serverless_search/server/routes/indices_routes.ts
@@ -5,11 +5,9 @@
  * 2.0.
  */
 
-import { IndicesIndexState } from '@elastic/elasticsearch/lib/api/types';
 import { schema } from '@kbn/config-schema';
-import { IScopedClusterClient } from '@kbn/core/server';
 
-import { isNotNullish } from '../../common/utils/is_not_nullish';
+import { fetchIndices } from '../lib/indices/fetch_indices';
 import { RouteDependencies } from '../plugin';
 
 export const registerIndicesRoutes = ({ router, security }: RouteDependencies) => {
@@ -25,7 +23,7 @@ export const registerIndicesRoutes = ({ router, security }: RouteDependencies) =
       },
     },
     async (context, request, response) => {
-      const client = (await context.core).elasticsearch.client;
+      const client = (await context.core).elasticsearch.client.asCurrentUser;
       const user = security.authc.getCurrentUser(request);
 
       if (!user) {
@@ -34,35 +32,10 @@ export const registerIndicesRoutes = ({ router, security }: RouteDependencies) =
           body: 'Could not retrieve current user, security plugin is not ready',
         });
       }
-      const { from, size, search_query: searchQuery } = request.query;
-      const indexPattern = searchQuery ? `*${searchQuery}*` : '*';
-      const indexMatches = await client.asCurrentUser.indices.get({
-        expand_wildcards: ['open'],
-        // for better performance only compute aliases and settings of indices but not mappings
-        features: ['aliases', 'settings'],
-        index: indexPattern,
-      });
-      const indexNames = Object.keys(indexMatches).filter(
-        (indexName) =>
-          indexMatches[indexName] &&
-          !isHidden(indexMatches[indexName]) &&
-          !isClosed(indexMatches[indexName])
-      );
-      const indexNameSlice = indexNames.slice(from, from + size).filter(isNotNullish);
-      if (indexNameSlice.length === 0) {
-        return response.ok({
-          body: {
-            indices: [],
-          },
-          headers: { 'content-type': 'application/json' },
-        });
-      }
-      const indexCounts = await fetchIndexCounts(client, indexNameSlice);
 
-      const indices = indexNameSlice.map((name) => ({
-        name,
-        count: indexCounts[name] ?? 0,
-      }));
+      const { from, size, search_query: searchQuery } = request.query;
+
+      const indices = await fetchIndices(client, from, size, searchQuery);
       return response.ok({
         body: {
           indices,
@@ -71,24 +44,4 @@ export const registerIndicesRoutes = ({ router, security }: RouteDependencies) =
       });
     }
   );
-};
-
-function isHidden(index: IndicesIndexState): boolean {
-  return index.settings?.index?.hidden === true || index.settings?.index?.hidden === 'true';
-}
-function isClosed(index: IndicesIndexState): boolean {
-  return (
-    index.settings?.index?.verified_before_close === true ||
-    index.settings?.index?.verified_before_close === 'true'
-  );
-}
-
-const fetchIndexCounts = async (client: IScopedClusterClient, indicesNames: string[]) => {
-  // TODO: is there way to batch this? Passing multiple index names or a pattern still returns a singular count
-  const countPromises = indicesNames.map(async (indexName) => {
-    const { count } = await client.asCurrentUser.count({ index: indexName });
-    return { [indexName]: count };
-  });
-  const indexCountArray = await Promise.all(countPromises);
-  return indexCountArray.reduce((acc, current) => Object.assign(acc, current), {});
 };
