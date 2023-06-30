f4dfb937057ab4367afa7bd59e33bb91d8458fc8
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2023-06-30 11:05:38 -0400

    [8.9] [RAM] fix 159174 & 155991 (#160455) (#161004)
    # Backport
    
    This will backport the following commits from `main` to `8.9`:
    - [[RAM] fix 159174 & 155991
    (#160455)](https://github.com/elastic/kibana/pull/160455)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Xavier
    Mouligneau","email":"xavier.mouligneau@elastic.co"},"sourceCommit":{"committedDate":"2023-06-30T13:59:47Z","message":"[RAM]
    fix 159174 & 155991 (#160455)\n\n## Summary\r\n\r\nFix:
    https://github.com/elastic/kibana/issues/155991
    &\r\nhttps://github.com/elastic/kibana/issues/159174\r\n\r\n\r\n###
    Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"73d1ed4ef8ce58a3f344a36f8c5904601d0a5741","branchLabelMapping":{"^v8.10.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["bug","release_note:fix","Team:ResponseOps","v8.9.0","v8.8.2","v8.10.0"],"number":160455,"url":"https://github.com/elastic/kibana/pull/160455","mergeCommit":{"message":"[RAM]
    fix 159174 & 155991 (#160455)\n\n## Summary\r\n\r\nFix:
    https://github.com/elastic/kibana/issues/155991
    &\r\nhttps://github.com/elastic/kibana/issues/159174\r\n\r\n\r\n###
    Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"73d1ed4ef8ce58a3f344a36f8c5904601d0a5741"}},"sourceBranch":"main","suggestedTargetBranches":["8.9","8.8"],"targetPullRequestStates":[{"branch":"8.9","label":"v8.9.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.8","label":"v8.8.2","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.10.0","labelRegex":"^v8.10.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/160455","number":160455,"mergeCommit":{"message":"[RAM]
    fix 159174 & 155991 (#160455)\n\n## Summary\r\n\r\nFix:
    https://github.com/elastic/kibana/issues/155991
    &\r\nhttps://github.com/elastic/kibana/issues/159174\r\n\r\n\r\n###
    Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"73d1ed4ef8ce58a3f344a36f8c5904601d0a5741"}}]}]
    BACKPORT-->
    
    Co-authored-by: Xavier Mouligneau <xavier.mouligneau@elastic.co>

--- x-pack/plugins/triggers_actions_ui/public/types.ts
+++ x-pack/plugins/triggers_actions_ui/public/types.ts
@@ -24,6 +24,7 @@ import type {
   EuiDataGridColumnCellAction,
   EuiDataGridToolBarVisibilityOptions,
   EuiSuperSelectOption,
+  EuiDataGridOnColumnResizeHandler,
 } from '@elastic/eui';
 import { EuiDataGridColumn, EuiDataGridControlColumn, EuiDataGridSorting } from '@elastic/eui';
 import { HttpSetup } from '@kbn/core/public';
@@ -538,6 +539,7 @@ export type AlertsTableProps = {
   onResetColumns: () => void;
   onColumnsChange: (columns: EuiDataGridColumn[], visibleColumns: string[]) => void;
   onChangeVisibleColumns: (newColumns: string[]) => void;
+  onColumnResize?: EuiDataGridOnColumnResizeHandler;
   query: Pick<QueryDslQueryContainer, 'bool' | 'ids'>;
   controls?: EuiDataGridToolBarAdditionalControlsOptions;
   showInspectButton?: boolean;
