f8c3155a511c15798af1ad47c9c8915fe91278cc
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-07-03 15:47:54 +0200

    [Discover] Switch to a simpler view

--- src/plugins/data/public/shard_failure_modal/shard_failure_description.tsx
+++ src/plugins/data/public/shard_failure_modal/shard_failure_description.tsx
@@ -6,9 +6,17 @@
  * Side Public License, v 1.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import { i18n } from '@kbn/i18n';
+import { css } from '@emotion/react';
 import { getFlattenedObject } from '@kbn/std';
-import { EuiCodeBlock, EuiDescriptionList } from '@elastic/eui';
+import {
+  EuiButtonEmpty,
+  EuiCodeBlock,
+  EuiDescriptionList,
+  EuiFlexGroup,
+  EuiFlexItem,
+} from '@elastic/eui';
 import { ShardFailure } from './shard_failure_types';
 
 /**
@@ -40,14 +48,84 @@ export function formatValueByKey(value: unknown, key: string): string | JSX.Elem
 }
 
 export function ShardFailureDescription(props: ShardFailure) {
+  const [showDetails, setShowDetails] = useState<boolean>(false);
+
   const flattendReason = getFlattenedObject(props.reason);
 
-  const listItems = Object.entries(flattendReason).map(([key, value]) => ({
-    title: formatKey(key),
-    description: formatValueByKey(value, key),
-  }));
+  const reasonItems = Object.entries(flattendReason)
+    .filter(([key]) => key !== 'type')
+    .map(([key, value]) => ({
+      title: formatKey(key),
+      description: formatValueByKey(value, key),
+    }));
+
+  const items = [
+    {
+      title: i18n.translate('data.search.searchSource.fetch.shardsFailedModal.shardTitle', {
+        defaultMessage: 'Shard',
+      }),
+      description: props.shard,
+    },
+    {
+      title: i18n.translate('data.search.searchSource.fetch.shardsFailedModal.indexTitle', {
+        defaultMessage: 'Index',
+      }),
+      description: props.index,
+    },
+    {
+      title: i18n.translate('data.search.searchSource.fetch.shardsFailedModal.reasonTypeTitle', {
+        defaultMessage: 'Type',
+      }),
+      description: props.reason.type,
+    },
+    ...(showDetails
+      ? [
+          {
+            title: i18n.translate('data.search.searchSource.fetch.shardsFailedModal.nodeTitle', {
+              defaultMessage: 'Node',
+            }),
+            description: props.node,
+          },
+          ...reasonItems,
+        ]
+      : []),
+  ];
 
   return (
-    <EuiDescriptionList listItems={listItems} compressed className="shardFailureModal__desc" />
+    <EuiFlexGroup direction="column" gutterSize="none" alignItems="stretch">
+      <EuiFlexItem grow={false}>
+        <EuiDescriptionList
+          type="responsiveColumn"
+          gutterSize="s"
+          listItems={items}
+          compressed
+          className="shardFailureModal__desc"
+          titleProps={{ className: 'shardFailureModal__descTitle' }}
+          descriptionProps={{ className: 'shardFailureModal__descValue' }}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem
+        grow={false}
+        css={css`
+          align-self: flex-start;
+        `}
+      >
+        <EuiButtonEmpty onClick={() => setShowDetails((prev) => !prev)} flush="left">
+          {showDetails
+            ? i18n.translate(
+                'data.search.searchSource.fetch.shardsFailedModal.showLessButtonLabel',
+                {
+                  defaultMessage: 'Show less',
+                }
+              )
+            : i18n.translate(
+                'data.search.searchSource.fetch.shardsFailedModal.showMoreButtonLabel',
+                {
+                  defaultMessage: 'Show details',
+                }
+              )}
+        </EuiButtonEmpty>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 }
