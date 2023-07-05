e0142064505b828a45b880541f50543e0ae68563
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-07-05 00:01:32 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/logic/diff/calculation/diff_calculation_helpers.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/logic/diff/calculation/diff_calculation_helpers.ts
@@ -23,7 +23,8 @@ export const calculateFieldsDiffFor = <TObject extends object>(
     return fieldDiff;
   });
   // if (ruleVersions.target_version.rule_id === 'd76b02ef-fc95-4001-9297-01cb7412232f') { // Python
-  if (ruleVersions.target_version.rule_id === 'a00681e3-9ed6-447c-ab2c-be648821c622') { // First seen AWS
+  if (ruleVersions.target_version.rule_id === 'a00681e3-9ed6-447c-ab2c-be648821c622') {
+    // First seen AWS
     debugger;
   }
   // TODO: try to improve strict typing and get rid of this "as" operator.
