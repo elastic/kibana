26941054f230d36f993ef7d57b913429a4f97588
Khristinin Nikita <nikita.khristinin@elastic.co>
2023-07-05 09:08:46 +0200

    make search bar in one line

--- x-pack/plugins/security_solution/public/entity_analytics/components/risk_score_preview_section.tsx
+++ x-pack/plugins/security_solution/public/entity_analytics/components/risk_score_preview_section.tsx
@@ -162,6 +162,7 @@ export const RiskScorePreviewSection = () => {
             showFilterBar={false}
             showDatePicker={true}
             displayStyle={'inPage'}
+            submitButtonStyle={'iconOnly'}
           />
         )}
       </EuiFormRow>
