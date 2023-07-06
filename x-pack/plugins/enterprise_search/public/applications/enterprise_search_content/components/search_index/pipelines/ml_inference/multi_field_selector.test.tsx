97052c94ad26c821f375a63c3dcd7f181a454108
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-07-06 19:43:25 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/multi_field_selector.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/multi_field_selector.test.tsx
@@ -133,6 +133,10 @@ describe('SelectedFieldMappings', () => {
 
     expect(wrapper.find(EuiBasicTable)).toHaveLength(1);
     const table = wrapper.find(EuiBasicTable);
-    expect(table.prop('columns').map((c) => c.name)).toEqual(['Source fields', '', 'Target fields']);
+    expect(table.prop('columns').map((c) => c.name)).toEqual([
+      'Source fields',
+      '',
+      'Target fields',
+    ]);
   });
 });
