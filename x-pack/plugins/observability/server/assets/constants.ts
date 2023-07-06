a9830ec50ad94b37133f7e970801a01e38c6b603
Kevin Delemme <kevin.delemme@elastic.co>
2023-07-06 13:40:18 -0400

    Refactor pipeline prefix with destination index name

--- x-pack/plugins/observability/server/assets/constants.ts
+++ x-pack/plugins/observability/server/assets/constants.ts
@@ -13,12 +13,12 @@ export const SLO_COMPONENT_TEMPLATE_SETTINGS_NAME = '.slo-observability.sli-sett
 export const SLO_INDEX_TEMPLATE_NAME = '.slo-observability.sli';
 export const SLO_INDEX_TEMPLATE_PATTERN = `${SLO_INDEX_TEMPLATE_NAME}-*`;
 
-export const SLO_INGEST_PIPELINE_NAME = `${SLO_INDEX_TEMPLATE_NAME}.monthly`;
-// slo-observability.sli-v<version>.(YYYY-MM-DD)
-export const SLO_INGEST_PIPELINE_INDEX_NAME_PREFIX = `${SLO_INDEX_TEMPLATE_NAME}-v${SLO_RESOURCES_VERSION}.`;
-
 export const SLO_DESTINATION_INDEX_NAME = `${SLO_INDEX_TEMPLATE_NAME}-v${SLO_RESOURCES_VERSION}`;
 export const SLO_DESTINATION_INDEX_PATTERN = `${SLO_DESTINATION_INDEX_NAME}*`;
+
+export const SLO_INGEST_PIPELINE_NAME = `${SLO_INDEX_TEMPLATE_NAME}.monthly`;
+// slo-observability.sli-v<version>.(YYYY-MM-DD)
+export const SLO_INGEST_PIPELINE_INDEX_NAME_PREFIX = `${SLO_DESTINATION_INDEX_NAME}.`;
 
 export const getSLOTransformId = (sloId: string, sloRevision: number) =>
   `slo-${sloId}-${sloRevision}`;
