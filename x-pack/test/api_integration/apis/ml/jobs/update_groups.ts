de3b736003bfd98004761b93c2cb3c4015647764
James Gowdy <jgowdy@elastic.co>
2023-07-06 14:43:38 +0100

    cleaning before running tests

--- x-pack/test/api_integration/apis/ml/jobs/update_groups.ts
+++ x-pack/test/api_integration/apis/ml/jobs/update_groups.ts
@@ -62,10 +62,12 @@ export default ({ getService }: FtrProviderContext) => {
       await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/ml/farequote');
       await ml.testResources.setKibanaTimeZoneToUTC();
 
+      await ml.api.cleanMlIndices();
+      await ml.testResources.cleanMLSavedObjects();
+
       for (const job of testSetupJobConfigs) {
         await ml.api.createAnomalyDetectionJob(job);
       }
-      await ml.api.syncSavedObjects();
     });
 
     after(async () => {
