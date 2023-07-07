9ca4013e27bf705381dcebcebbc0c3b866c47f4e
Shahzad <shahzad31comp@gmail.com>
2023-07-07 18:01:29 +0200

    [Synthetics] Fix TLS alert recovery (#161254)
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Abdul Wahab Zahid <awahab07@yahoo.com>
    (cherry picked from commit 0fe62fba0bb651d034021cbeab34dd2eb4cd2278)

--- x-pack/plugins/synthetics/server/alert_rules/tls_rule/tls_rule.ts
+++ x-pack/plugins/synthetics/server/alert_rules/tls_rule/tls_rule.ts
@@ -23,7 +23,7 @@ import {
   CERT_VALID_NOT_AFTER,
   CERT_VALID_NOT_BEFORE,
 } from '../../../common/field_names';
-import { getCertSummary } from './message_utils';
+import { getCertSummary, setTLSRecoveredAlertsContext } from './message_utils';
 import { SyntheticsCommonState } from '../../../common/runtime_types/alert_rules/common';
 import { UptimeCorePluginsSetup, UptimeServerSetup } from '../../legacy_uptime/lib/adapters';
 import { TLSRuleExecutor } from './tls_rule_executor';
@@ -38,7 +38,6 @@ import { UMServerLibs } from '../../legacy_uptime/uptime_server';
 import { SyntheticsMonitorClient } from '../../synthetics_service/synthetics_monitor/synthetics_monitor_client';
 import {
   generateAlertMessage,
-  setRecoveredAlertsContext,
   UptimeRuleTypeAlertDefinition,
 } from '../../legacy_uptime/lib/alerts/common';
 
@@ -98,53 +97,51 @@ export const registerSyntheticsTLSCheckRule = (
         syntheticsMonitorClient
       );
 
-      const { foundCerts, certs, absoluteExpirationThreshold, absoluteAgeThreshold } =
+      const { foundCerts, certs, absoluteExpirationThreshold, absoluteAgeThreshold, latestPings } =
         await tlsRule.getExpiredCertificates();
 
-      if (foundCerts) {
-        await asyncForEach(certs, async (cert) => {
-          const summary = getCertSummary(cert, absoluteExpirationThreshold, absoluteAgeThreshold);
+      await asyncForEach(certs, async (cert) => {
+        const summary = getCertSummary(cert, absoluteExpirationThreshold, absoluteAgeThreshold);
 
-          if (!summary.summary || !summary.status) {
-            return;
-          }
+        if (!summary.summary || !summary.status) {
+          return;
+        }
 
-          const alertId = `${cert.common_name}-${cert.issuer?.replace(/\s/g, '_')}-${cert.sha256}`;
-          const alertUuid = getAlertUuid(alertId);
-          const indexedStartedAt = getAlertStartedDate(alertId) ?? startedAt.toISOString();
+        const alertId = cert.sha256;
+        const alertUuid = getAlertUuid(alertId);
+        const indexedStartedAt = getAlertStartedDate(alertId) ?? startedAt.toISOString();
 
-          const alertInstance = alertWithLifecycle({
-            id: alertId,
-            fields: {
-              [CERT_COMMON_NAME]: cert.common_name,
-              [CERT_ISSUER_NAME]: cert.issuer,
-              [CERT_VALID_NOT_AFTER]: cert.not_after,
-              [CERT_VALID_NOT_BEFORE]: cert.not_before,
-              [CERT_HASH_SHA256]: cert.sha256,
-              [ALERT_UUID]: alertUuid,
-              [ALERT_REASON]: generateAlertMessage(TlsTranslations.defaultActionMessage, summary),
-            },
-          });
-
-          alertInstance.replaceState({
-            ...updateState(ruleState, foundCerts),
-            ...summary,
-          });
-
-          alertInstance.scheduleActions(TLS_CERTIFICATE.id, {
-            [ALERT_DETAILS_URL]: await getAlertUrl(
-              alertUuid,
-              spaceId,
-              indexedStartedAt,
-              alertsLocator,
-              basePath.publicBaseUrl
-            ),
-            ...summary,
-          });
+        const alertInstance = alertWithLifecycle({
+          id: alertId,
+          fields: {
+            [CERT_COMMON_NAME]: cert.common_name,
+            [CERT_ISSUER_NAME]: cert.issuer,
+            [CERT_VALID_NOT_AFTER]: cert.not_after,
+            [CERT_VALID_NOT_BEFORE]: cert.not_before,
+            [CERT_HASH_SHA256]: cert.sha256,
+            [ALERT_UUID]: alertUuid,
+            [ALERT_REASON]: generateAlertMessage(TlsTranslations.defaultActionMessage, summary),
+          },
         });
-      }
 
-      await setRecoveredAlertsContext({
+        alertInstance.replaceState({
+          ...updateState(ruleState, foundCerts),
+          ...summary,
+        });
+
+        alertInstance.scheduleActions(TLS_CERTIFICATE.id, {
+          [ALERT_DETAILS_URL]: await getAlertUrl(
+            alertUuid,
+            spaceId,
+            indexedStartedAt,
+            alertsLocator,
+            basePath.publicBaseUrl
+          ),
+          ...summary,
+        });
+      });
+
+      await setTLSRecoveredAlertsContext({
         alertFactory,
         basePath,
         defaultStartedAt: startedAt.toISOString(),
@@ -152,6 +149,7 @@ export const registerSyntheticsTLSCheckRule = (
         getAlertUuid,
         spaceId,
         alertsLocator,
+        latestPings,
       });
 
       return { state: updateState(ruleState, foundCerts) };
