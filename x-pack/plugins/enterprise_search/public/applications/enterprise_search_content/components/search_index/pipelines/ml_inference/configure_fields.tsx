9b4c712ba5ec54250f977156ca80e0e31d43ba05
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-07-07 15:19:00 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/configure_fields.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/configure_fields.tsx
@@ -11,11 +11,12 @@ import { useValues } from 'kea';
 
 import { EuiForm, EuiSpacer, EuiTitle, EuiText, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
+import { FormattedMessage } from '@kbn/i18n-react';
+
 import { InferenceConfiguration } from './inference_config';
 import { MLInferenceLogic } from './ml_inference_logic';
 import { MultiFieldMapping, SelectedFieldMappings } from './multi_field_selector';
 import { SingleFieldMapping } from './single_field_selector';
-import { FormattedMessage } from '@kbn/i18n-react';
 
 export const ConfigureFields: React.FC = () => {
   const {
