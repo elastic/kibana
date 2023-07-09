d5e87f246dc869dc954bc27f50962c46b7b7747e
shahzad31 <shahzad31comp@gmail.com>
2023-07-09 23:20:21 +0200

    fix types

--- x-pack/plugins/synthetics/common/runtime_types/certs.ts
+++ x-pack/plugins/synthetics/common/runtime_types/certs.ts
@@ -34,9 +34,9 @@ export const CertType = t.intersection([
   t.type({
     monitors: t.array(CertMonitorType),
     sha256: t.string,
-    configId: t.string,
   }),
   t.partial({
+    configId: t.string,
     not_after: t.string,
     not_before: t.string,
     common_name: t.string,
