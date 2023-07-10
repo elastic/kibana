dec3b748f95fa0f32db02f3b0d5ff86aa104c17a
pgayvallet <pierre.gayvallet@elastic.co>
2023-07-10 13:42:16 +0200

    better doc

--- packages/core/saved-objects/core-saved-objects-migration-server-internal/src/zdt/utils/check_index_algorithm.ts
+++ packages/core/saved-objects/core-saved-objects-migration-server-internal/src/zdt/utils/check_index_algorithm.ts
@@ -11,11 +11,15 @@ import type { IndexMapping } from '@kbn/core-saved-objects-base-server-internal'
 /**
  * The list of values returned by `checkIndexCurrentAlgorithm`.
  *
- * - `zdt`
- * - `zdt-partially-migrated`
- * - `v2-compatible`
- * - `v2-incompatible`
- * - `unknown`
+ * - `zdt`: last algo that ran was zdt
+ *
+ * - `v2-compatible`: last running algo was a v2 version that zdt can take over
+ *
+ * - `v2-incompatible`: last running algo was a v2 version that zdt can not take over
+ *
+ * - `v2-partially-migrated`: last running algo was zdt taking over v2, but the migration failed at some point
+ *
+ * - `unknown`: last running algo cannot be determined
  */
 export type CheckCurrentAlgorithmResult =
   | 'zdt'
