3fe0b211d03f7c76521cfd744b624169356efff0
nreese <reese.nathan@elastic.co>
2023-07-10 07:48:38 -0600

    fix test

--- test/examples/embeddables/list_container.ts
+++ test/examples/embeddables/list_container.ts
@@ -22,7 +22,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
     it('list containers render', async () => {
       await retry.try(async () => {
         const title = await testSubjects.getVisibleText('listContainerTitle');
-        expect(title).to.be('todo list');
+        expect(title).to.be('Todo list');
 
         const titles = await testSubjects.getVisibleTextAll('todoEmbeddableTitle');
         expect(titles).to.eql(['Take out the trash', 'Vaccum the floor']);
