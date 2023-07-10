f1f8db8d6c671d2912982500a8971179a0beca70
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-07-10 10:04:03 +0200

    refactor(discover): remove action

--- x-pack/plugins/discover_log_explorer/public/state_machines/log_explorer_profile/src/state_machine.ts
+++ x-pack/plugins/discover_log_explorer/public/state_machines/log_explorer_profile/src/state_machine.ts
@@ -97,7 +97,6 @@ export const createPureLogExplorerProfileStateMachine = (
     },
     {
       actions: {
-        notifyDatasetSelectionRestoreFailed: actions.pure(() => undefined),
         storeDatasetSelection: actions.assign((_context, event) =>
           'data' in event && isDatasetSelection(event.data)
             ? {
