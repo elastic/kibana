2a45806044e5f0cfa34ab53512857225dde2da1d
nreese <reese.nathan@elastic.co>
2023-07-10 06:40:29 -0600

    update expects

--- test/examples/embeddables/list_container.ts
+++ test/examples/embeddables/list_container.ts
@@ -22,19 +22,13 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
     it('list containers render', async () => {
       await retry.try(async () => {
         const title = await testSubjects.getVisibleText('listContainerTitle');
-        expect(title).to.be('My todo list');
+        expect(title).to.be('todo list');
 
         const titles = await testSubjects.getVisibleTextAll('todoEmbeddableTitle');
-        expect(titles).to.eql(['Take out the trash', 'Vaccum the floor', 'Take out the trash']);
-
-        const searchableTitle = await testSubjects.getVisibleText('searchableListContainerTitle');
-        expect(searchableTitle).to.be('My searchable todo list');
+        expect(titles).to.eql(['Take out the trash', 'Vaccum the floor']);
 
         const text = await testSubjects.getVisibleTextAll('helloWorldEmbeddable');
-        expect(text).to.eql(['HELLO WORLD!', 'HELLO WORLD!']);
-
-        const tasks = await testSubjects.getVisibleTextAll('multiTaskTodoTask');
-        expect(tasks).to.eql(['Go to school', 'Watch planet earth', 'Read the encyclopedia']);
+        expect(text).to.eql(['HELLO WORLD!']);
       });
     });
   });
