ad1ed219f83280a337361ff761ef67d3eecda4cb
Kurt <kc13greiner@users.noreply.github.com>
2023-07-11 10:40:05 -0400

    CodeReview feedback Oliver G. - Removing TODO

--- x-pack/plugins/apm/public/components/fleet_integration/apm_policy_form/settings_form/form_row_setting.tsx
+++ x-pack/plugins/apm/public/components/fleet_integration/apm_policy_form/settings_form/form_row_setting.tsx
@@ -125,7 +125,7 @@ export function FormRowSetting({ row, value, onChange, isDisabled }: Props) {
     case 'yaml': {
       return (
         <FixedHeightDiv>
-          <CodeEditor // TODO Unable to find context that provides theme. Need CODEOWNER Input
+          <CodeEditor
             languageId="yaml"
             width="100%"
             height="300px"
