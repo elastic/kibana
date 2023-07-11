488e8098e359b623324b89ebfe69d1dda3900a29
Gerard Soldevila <gerard.soldevila@elastic.co>
2023-07-11 10:47:28 +0200

    Unfocus focussed test

--- src/core/server/integration_tests/saved_objects/migrations/group3/actions/actions.test.ts
+++ src/core/server/integration_tests/saved_objects/migrations/group3/actions/actions.test.ts
@@ -1269,8 +1269,7 @@ describe('migration actions', () => {
       );
     });
 
-    // eslint-disable-next-line jest/no-focused-tests
-    fit('returns a left es_response_too_large error when a read batch exceeds the maxResponseSize', async () => {
+    it('returns a left es_response_too_large error when a read batch exceeds the maxResponseSize', async () => {
       const openPitTask = openPit({ client, index: 'existing_index_with_docs' });
       const pitResponse = (await openPitTask()) as Either.Right<OpenPitResponse>;
 
