d2b631012d7e24d37a67e538df0eb07d71b903d3
Mike Cote <michel.cote@elastic.co>
2023-07-12 07:54:31 -0400

    Update comment

--- x-pack/plugins/ml/server/saved_objects/sync_task.ts
+++ x-pack/plugins/ml/server/saved_objects/sync_task.ts
@@ -25,6 +25,9 @@ const SAVED_OBJECTS_SYNC_INTERVAL_DEFAULT = '1h';
 /**
  * WARNING: Do not modify the existing versioned schema(s) below, instead define a new version (ex: 2, 3, 4).
  * This is required to support zero-downtime upgrades and rollbacks. See https://github.com/elastic/kibana/issues/155764.
+ *
+ * As you add a new schema version, don't forget to change latestTaskStateSchema variable to reference the latest schema.
+ * For example, changing stateSchemaByVersion[1].schema to stateSchemaByVersion[2].schema.
  */
 const stateSchemaByVersion = {
   1: {
