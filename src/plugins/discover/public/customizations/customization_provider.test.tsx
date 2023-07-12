ec9e6362de967742056bfaf9425cd24dec76baf6
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-07-12 13:09:44 +0200

    refactor(discover): remove unused import

--- src/plugins/discover/public/customizations/customization_provider.test.tsx
+++ src/plugins/discover/public/customizations/customization_provider.test.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import { act, renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import React from 'react';
 import { getDiscoverStateMock } from '../__mocks__/discover_state.mock';
 import {
