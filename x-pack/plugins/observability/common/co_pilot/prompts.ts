4ae94c35024f2037ee02502037b873d93256d200
Dario Gieselaar <dario.gieselaar@elastic.co>
2023-07-12 21:58:13 +0200

    [Observability] Add feedback buttons for prompts (#160351)
    Co-authored-by: Sean Heelan <sean@optimyze.cloud>

--- x-pack/plugins/observability/common/co_pilot/prompts.ts
+++ x-pack/plugins/observability/common/co_pilot/prompts.ts
@@ -5,26 +5,12 @@
  * 2.0.
  */
 import * as t from 'io-ts';
-import {
-  type ChatCompletionRequestMessage,
-  type CreateChatCompletionResponse,
-  type CreateChatCompletionResponseChoicesInner,
+import type {
+  ChatCompletionRequestMessage,
+  CreateChatCompletionResponse,
+  CreateChatCompletionResponseChoicesInner,
 } from 'openai';
-
-export enum OpenAIProvider {
-  OpenAI = 'openAI',
-  AzureOpenAI = 'azureOpenAI',
-}
-
-export enum CoPilotPromptId {
-  ProfilingExplainFunction = 'profilingExplainFunction',
-  ProfilingOptimizeFunction = 'profilingOptimizeFunction',
-  ApmExplainError = 'apmExplainError',
-  LogsExplainMessage = 'logsExplainMessage',
-  LogsFindSimilar = 'logsFindSimilar',
-  InfraExplainProcess = 'infraExplainProcess',
-  ExplainLogSpike = 'explainLogSpike',
-}
+import { CoPilotPromptId } from '.';
 
 const PERF_GPT_SYSTEM_MESSAGE = {
   content: `You are perf-gpt, a helpful assistant for performance analysis and optimisation
@@ -91,62 +77,53 @@ export const coPilotPrompts = {
       return [
         PERF_GPT_SYSTEM_MESSAGE,
         {
-          content: `Assuming the function ${functionName} from the library ${library} is consuming significant CPU resources.
-        Suggest ways to optimize or improve the system that involve the ${functionName} function from the
-        ${library} library. Types of improvements that would be useful to me are improvements that result in:
-
-        - Higher performance so that the system runs faster or uses less CPU
-        - Better memory efficient so that the system uses less RAM
-        - Better storage efficient so that the system stores less data on disk.
-        - Better network I/O efficiency so that less data is sent over the network
-        - Better disk I/O efficiency so that less data is read and written from disk
-
-        Make up to five suggestions. Your suggestions must meet all of the following criteria:
-        1. Your suggestions should be detailed, technical and include concrete examples.
-        2. Your suggestions should be specific to improving performance of a system in which the ${functionName} function from
-        the ${library} library is consuming significant CPU.
-        2. If you suggest replacing the function or library with a more efficient replacement you must suggest at least
-        one concrete replacement.
-
-        If you know of fewer than five ways to improve the performance of a system in which the ${functionName} function from the
-        ${library} library is consuming significant CPU, then provide fewer than five suggestions. If you do not know of any
-        way in which to improve the performance then say "I do not know how to improve the performance of systems where
-        this function is consuming a significant amount of CPU".
-
-        If you have suggestions, the output format should look as follows:
-
-        Here are some suggestions as to how you might optimize your system if ${functionName} in ${library} is consuming
-        significant CPU resources:
-        1. Insert first suggestion
-        2. Insert second suggestion
-        etc.`,
-          role: 'user',
-        },
-      ];
-    },
-  }),
-  [CoPilotPromptId.ProfilingExplainFunction]: prompt({
-    params: t.type({
-      library: t.string,
-      functionName: t.string,
-    }),
-    messages: ({ library, functionName }) => {
-      return [
-        PERF_GPT_SYSTEM_MESSAGE,
-        {
           content: `I am a software engineer. I am trying to understand what a function in a particular
-            software library does.
-
-            The library is: ${library}
-            The function is: ${functionName}
-
-            Your task is to describe what the library is and what its use cases are, and to describe what the function
-            does. The output format should look as follows:
-
-            Library description: Provide a concise description of the library
-            Library use-cases: Provide a concise description of what the library is typically used for.
-            Function description: Provide a concise, technical, description of what the function does.
-            `,
+          software library does.
+          
+          The library is: ${library}
+          The function is: ${functionName}
+          
+          Your have two tasks. Your first task is to desribe what the library is and what its use cases are, and to
+          describe what the function does. The output format should look as follows:
+          
+          Library description: Provide a concise description of the library
+          Library use-cases: Provide a concise description of what the library is typically used for.
+          Function description: Provide a concise, technical, description of what the function does.
+          
+          Assume the function ${functionName} from the library ${library} is consuming significant CPU resources.
+          Your second task is to suggest ways to optimize or improve the system that involve the ${functionName} function from the
+          ${library} library. Types of improvements that would be useful to me are improvements that result in:
+          
+          - Higher performance so that the system runs faster or uses less CPU
+          - Better memory efficient so that the system uses less RAM
+          - Better storage efficient so that the system stores less data on disk.
+          - Better network I/O efficiency so that less data is sent over the network
+          - Better disk I/O efficiency so that less data is read and written from disk
+          
+          Make up to five suggestions. Your suggestions must meet all of the following criteria:
+          1. Your suggestions should detailed, technical and include concrete examples.
+          2. Your suggestions should be specific to improving performance of a system in which the ${functionName} function from
+          the ${library} library is consuming significant CPU.
+          3. If you suggest replacing the function or library with a more efficient replacement you must suggest at least
+          one concrete replacement.
+          
+          If you know of fewer than five ways to improve the performance of a system in which the ${functionName} function from the
+          ${library} library is consuming significant CPU, then provide fewer than five suggestions. If you do not know of any
+          way in which to improve the performance then say "I do not know how to improve the performance of systems where
+          this function is consuming a significant amount of CPU".
+          
+          Do not suggest using a CPU profiler. I have already profiled my code. The profiler I used is Elastic Universal Profiler.
+          If there is specific information I should look for in the profiler output then tell me what information to look for
+          in the output of Elastic Universal Profiler.
+          
+          You must not include URLs, web addresses or websites of any kind in your output.
+          
+          If you have suggestions, the output format should look as follows:
+          
+          Here are some suggestions as to how you might optimize your system if ${functionName} in ${library} is consuming
+          significant CPU resources:
+          1. Insert first suggestion
+          2. Insert second suggestion`,
           role: 'user',
         },
       ];
