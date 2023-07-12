0fee2ce3b16b9ecbe1d1d4d00e8fc26f20222287
pgayvallet <pierre.gayvallet@elastic.co>
2023-07-12 13:03:22 +0200

    fix FTR test

--- test/plugin_functional/test_suites/core_plugins/rendering.ts
+++ test/plugin_functional/test_suites/core_plugins/rendering.ts
@@ -219,6 +219,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
         'xpack.cloud.organization_url (string)',
         'xpack.cloud.billing_url (string)',
         'xpack.cloud.profile_url (string)',
+        'xpack.cloud.serverless.project_id (string)',
         'xpack.discoverEnhanced.actions.exploreDataInChart.enabled (boolean)',
         'xpack.discoverEnhanced.actions.exploreDataInContextMenu.enabled (boolean)',
         'xpack.fleet.agents.enabled (boolean)',
