aea52a53dc188e6ff9b777aa296649cc7e8edd17
Ignacio Rivas <rivasign@gmail.com>
2023-07-12 10:36:08 +0200

    Fix test

--- x-pack/plugins/index_management/__jest__/client_integration/home/indices_tab.test.ts
+++ x-pack/plugins/index_management/__jest__/client_integration/home/indices_tab.test.ts
@@ -270,7 +270,9 @@ describe('<IndexManagementHome />', () => {
     });
 
     test('should be able to open a closed index', async () => {
-      testBed = await setup(httpSetup);
+      testBed = await setup(httpSetup, {
+        enableIndexActions: true,
+      });
       const { component, find, actions } = testBed;
 
       component.update();
