8ccb78671015b3052948d70d09a8599832b6c60c
Drew Tate <andrew.tate@elastic.co>
2023-07-12 11:04:44 -0600

    remove event_annotation_application plugin

--- src/plugins/event_annotation/public/get_table_list.tsx
+++ src/plugins/event_annotation/public/get_table_list.tsx
@@ -16,8 +16,8 @@ import type { SavedObjectsTaggingApi } from '@kbn/saved-objects-tagging-oss-plug
 import type { DataView, DataViewSpec } from '@kbn/data-views-plugin/common';
 import type { QueryInputServices } from '@kbn/visualization-ui-components';
 import { RootDragDropProvider } from '@kbn/dom-drag-drop';
-import type { EventAnnotationServiceType } from '@kbn/event-annotation-plugin/public';
 import { EventAnnotationGroupTableList } from '@kbn/event-annotation-components';
+import type { EventAnnotationServiceType } from '.';
 
 export interface EventAnnotationListingPageServices {
   core: CoreStart;
