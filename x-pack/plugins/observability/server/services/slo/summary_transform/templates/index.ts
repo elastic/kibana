78e54d41bc259bfd5027da4a9e56dfd9048c1e00
Kevin Delemme <kevin.delemme@elastic.co>
2023-07-12 12:51:50 -0400

    Add calendar aligned templates

--- x-pack/plugins/observability/server/services/slo/summary_transform/templates/index.ts
+++ x-pack/plugins/observability/server/services/slo/summary_transform/templates/index.ts
@@ -11,12 +11,20 @@ import { SUMMARY_OCCURRENCES_90D_ROLLING } from './summary_occurrences_90d_rolli
 import { SUMMARY_TIMESLICES_7D_ROLLING } from './summary_timeslices_7d_rolling';
 import { SUMMARY_TIMESLICES_30D_ROLLING } from './summary_timeslices_30d_rolling';
 import { SUMMARY_TIMESLICES_90D_ROLLING } from './summary_timeslices_90d_rolling';
+import { SUMMARY_OCCURRENCES_WEEKLY_ALIGNED } from './summary_occurrences_weekly_aligned';
+import { SUMMARY_OCCURRENCES_MONTHLY_ALIGNED } from './summary_occurrences_monthly_aligned';
+import { SUMMARY_TIMESLICES_WEEKLY_ALIGNED } from './summary_timeslices_weekly_aligned';
+import { SUMMARY_TIMESLICES_MONTHLY_ALIGNED } from './summary_timeslices_monthly_aligned';
 
 export const ALL_TRANSFORM_TEMPLATES = [
   SUMMARY_OCCURRENCES_7D_ROLLING,
   SUMMARY_OCCURRENCES_30D_ROLLING,
   SUMMARY_OCCURRENCES_90D_ROLLING,
+  SUMMARY_OCCURRENCES_WEEKLY_ALIGNED,
+  SUMMARY_OCCURRENCES_MONTHLY_ALIGNED,
   SUMMARY_TIMESLICES_7D_ROLLING,
   SUMMARY_TIMESLICES_30D_ROLLING,
   SUMMARY_TIMESLICES_90D_ROLLING,
+  SUMMARY_TIMESLICES_WEEKLY_ALIGNED,
+  SUMMARY_TIMESLICES_MONTHLY_ALIGNED,
 ];
---
+++ x-pack/plugins/observability/server/services/slo/summary_transform/templates/summary_occurrences_monthly_aligned.ts
@@ -0,0 +1,194 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { TransformPutTransformRequest } from '@elastic/elasticsearch/lib/api/types';
+import {
+  SLO_DESTINATION_INDEX_PATTERN,
+  SLO_RESOURCES_VERSION,
+  SLO_SUMMARY_DESTINATION_INDEX_NAME,
+} from '../../../../assets/constants';
+
+export const SUMMARY_OCCURRENCES_MONTHLY_ALIGNED: TransformPutTransformRequest = {
+  transform_id: 'slo-summary-occurrences-monthly-aligned',
+  dest: {
+    index: SLO_SUMMARY_DESTINATION_INDEX_NAME,
+  },
+  source: {
+    index: SLO_DESTINATION_INDEX_PATTERN,
+    query: {
+      bool: {
+        filter: [
+          {
+            range: {
+              '@timestamp': {
+                gte: 'now/M',
+                lte: 'now/m',
+              },
+            },
+          },
+          {
+            term: {
+              'slo.budgetingMethod': 'occurrences',
+            },
+          },
+          {
+            term: {
+              'slo.timeWindow.type': 'calendarAligned',
+            },
+          },
+          {
+            term: {
+              'slo.timeWindow.duration': '1M',
+            },
+          },
+        ],
+      },
+    },
+  },
+  pivot: {
+    group_by: {
+      'slo.id': {
+        terms: {
+          field: 'slo.id',
+        },
+      },
+      'slo.revision': {
+        terms: {
+          field: 'slo.revision',
+        },
+      },
+      'slo.instanceId': {
+        terms: {
+          field: 'slo.instanceId',
+        },
+      },
+      'slo.budgetingMethod': {
+        terms: {
+          field: 'slo.budgetingMethod',
+        },
+      },
+      'slo.timeWindow.duration': {
+        terms: {
+          field: 'slo.timeWindow.duration',
+        },
+      },
+      'slo.timeWindow.type': {
+        terms: {
+          field: 'slo.timeWindow.type',
+        },
+      },
+    },
+    aggregations: {
+      _totalEventsEstimated: {
+        bucket_script: {
+          buckets_path: {
+            totalEvents: 'totalEvents',
+          },
+          script: {
+            source: `
+              Date d = new Date(); 
+              Instant instant = Instant.ofEpochMilli(d.getTime());
+          
+              LocalDateTime now = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+              LocalDateTime startOfMonth = now
+                .withDayOfMonth(1)
+                .withHour(0)
+                .withMinute(0)
+                .withSecond(0);
+              LocalDateTime startOfNextMonth = startOfMonth.plusMonths(1);
+
+              long elapsedDuration = Duration.between(startOfMonth, now).toMinutes();
+              long monthDuration = Duration.between(startOfMonth, startOfNextMonth).toMinutes();
+              return Math.ceil(params.totalEvents / elapsedDuration * monthDuration);
+            `,
+          },
+        },
+      },
+      _objectiveTarget: {
+        max: {
+          field: 'slo.objective.target',
+        },
+      },
+      goodEvents: {
+        sum: {
+          field: 'slo.numerator',
+        },
+      },
+      totalEvents: {
+        sum: {
+          field: 'slo.denominator',
+        },
+      },
+      sliValue: {
+        bucket_script: {
+          buckets_path: {
+            goodEvents: 'goodEvents',
+            totalEvents: 'totalEvents',
+          },
+          script:
+            'if (params.totalEvents == 0) { return -1 } else { return params.goodEvents / params.totalEvents }',
+        },
+      },
+      errorBudgetInitial: {
+        bucket_script: {
+          buckets_path: {
+            objective: '_objectiveTarget',
+          },
+          script: '1 - params.objective',
+        },
+      },
+      errorBudgetConsumed: {
+        bucket_script: {
+          buckets_path: {
+            goodEvents: 'goodEvents',
+            totalEvents: 'totalEvents',
+            totalEventsEstimated: '_totalEventsEstimated',
+            errorBudgetInitial: 'errorBudgetInitial',
+          },
+          script:
+            'if (params.totalEvents == 0) { return 0 } else { return (params.totalEvents - params.goodEvents) / (params.totalEventsEstimated * params.errorBudgetInitial) }',
+        },
+      },
+      errorBudgetRemaining: {
+        bucket_script: {
+          buckets_path: {
+            errorBudgetConsumed: 'errorBudgetConsumed',
+          },
+          script: '1 - params.errorBudgetConsumed',
+        },
+      },
+      status: {
+        bucket_script: {
+          buckets_path: {
+            sliValue: 'sliValue',
+            objective: '_objectiveTarget',
+            errorBudgetRemaining: 'errorBudgetRemaining',
+          },
+          script:
+            'if (params.sliValue == -1) { return 0 } else if (params.sliValue >= params.objective) { return 4 } else if (params.errorBudgetRemaining > 0) { return 2 } else { return 1 }',
+        },
+      },
+    },
+  },
+  description:
+    'Summarize every SLO with occurrences budgeting method and a monthly calendar aligned time window',
+  frequency: '1m',
+  sync: {
+    time: {
+      field: '@timestamp',
+      delay: '60s',
+    },
+  },
+  settings: {
+    deduce_mappings: false,
+  },
+  _meta: {
+    version: SLO_RESOURCES_VERSION,
+    managed: true,
+    managed_by: 'observability',
+  },
+};
