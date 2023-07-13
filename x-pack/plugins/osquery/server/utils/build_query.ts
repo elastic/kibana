e7939fc2d5bf5428ce0005f854b61f98db90a11c
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-07-13 12:39:45 +0200

    remove redundant config

--- x-pack/plugins/osquery/server/utils/build_query.ts
+++ x-pack/plugins/osquery/server/utils/build_query.ts
@@ -5,21 +5,14 @@
  * 2.0.
  */
 
-import type { EsQueryConfig, Query } from '@kbn/es-query';
+import type { Query } from '@kbn/es-query';
 import { buildEsQuery } from '@kbn/es-query';
 
 export const getQueryFilter = ({ filter }: { filter: string }) => {
-  // export const getQueryFilter = ({ filter }: GetQueryFilterOptions): GetQueryFilterReturn => {
   const kqlQuery: Query = {
     language: 'kuery',
     query: filter,
   };
-  const config: EsQueryConfig = {
-    allowLeadingWildcards: true,
-    dateFormatTZ: 'Zulu',
-    ignoreFilterIfFieldNotInIndex: false,
-    queryStringOptions: { analyze_wildcard: true },
-  };
 
-  return buildEsQuery(undefined, kqlQuery, [], config);
+  return buildEsQuery(undefined, kqlQuery, []);
 };
