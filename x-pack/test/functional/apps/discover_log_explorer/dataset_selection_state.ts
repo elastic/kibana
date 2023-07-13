524137ca64c5ab2eeabddfa0b40f31ab587fa88a
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-07-13 15:18:30 +0200

    refactor(discover): update test

--- x-pack/test/functional/apps/discover_log_explorer/dataset_selection_state.ts
+++ x-pack/test/functional/apps/discover_log_explorer/dataset_selection_state.ts
@@ -9,25 +9,10 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const browser = getService('browser');
-  const kibanaServer = getService('kibanaServer');
   const retry = getService('retry');
   const PageObjects = getPageObjects(['common', 'discoverLogExplorer']);
-  const defaultSettings = {
-    defaultIndex: 'logstash-*',
-    'doc_table:legacy': false,
-  };
 
   describe('DatasetSelection initialization and update', () => {
-    before('initialize tests', async () => {
-      await kibanaServer.importExport.load('test/functional/fixtures/kbn_archiver/discover');
-      await kibanaServer.uiSettings.update(defaultSettings);
-    });
-
-    after('clean up archives', async () => {
-      await kibanaServer.importExport.unload('test/functional/fixtures/kbn_archiver/discover');
-      await kibanaServer.uiSettings.unset('doc_table:legacy');
-    });
-
     describe('when the "index" query param does not exist', () => {
       it('should initialize the "All log datasets" selection', async () => {
         await PageObjects.common.navigateToActualUrl('discover', 'p/log-explorer');
@@ -41,7 +26,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     describe('when the "index" query param exist', () => {
       it('should decode and restore the selection from a valid encoded index', async () => {
         const azureActivitylogsIndex =
-          'BQZwpgNmDGAuCWB7AdgLmAEwIay+W6yWAtmKgOQSIDmIAtFgF4CuATmAHRZzwBu8sAJ5VadAFTkANAlhRU3BPyEiQASklFS8lu0m8wrEEjTkAjBwCsHAEwBmcuvBQeKACqCADmSPJqUVUA==';
+          'BQZwpgNmDGAuCWB7AdgLmAEwIay+W6yWAtmKgOQSIDmIAtFgF4CuATmAHRZzwBu8sAJ5VadAFTkANAlhRU3BPyEiQASklFS8lu2kC55AII6wAAgAyNEFN5hWIJGnIBGDgFYOAJgDM5deCgeFAAVQQAHMgdkaihVIA===';
         await PageObjects.common.navigateToActualUrl(
           'discover',
           `p/log-explorer?_a=(index:${encodeURIComponent(azureActivitylogsIndex)})`
@@ -50,7 +35,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         const datasetSelectionTitle =
           await PageObjects.discoverLogExplorer.getDatasetSelectorButtonText();
 
-        expect(datasetSelectionTitle).to.be('[azure] activitylogs');
+        expect(datasetSelectionTitle).to.be('[Azure Logs] activitylogs');
       });
 
       it('should fallback to "All log datasets" selection and notify the user for an invalid encoded index', async () => {
@@ -76,30 +61,28 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         expect(allDatasetSelectionTitle).to.be('All log datasets');
 
         const azureActivitylogsIndex =
-          'BQZwpgNmDGAuCWB7AdgLmAEwIay+W6yWAtmKgOQSIDmIAtFgF4CuATmAHRZzwBu8sAJ5VadAFTkANAlhRU3BPyEiQASklFS8lu0m8wrEEjTkAjBwCsHAEwBmcuvBQeKACqCADmSPJqUVUA==';
+          'BQZwpgNmDGAuCWB7AdgLmAEwIay+W6yWAtmKgOQSIDmIAtFgF4CuATmAHRZzwBu8sAJ5VadAFTkANAlhRU3BPyEiQASklFS8lu2kC55AII6wAAgAyNEFN5hWIJGnIBGDgFYOAJgDM5deCgeFAAVQQAHMgdkaihVIA===';
         await PageObjects.common.navigateToActualUrl(
           'discover',
           `p/log-explorer?_a=(index:${encodeURIComponent(azureActivitylogsIndex)})`
         );
         const azureDatasetSelectionTitle =
           await PageObjects.discoverLogExplorer.getDatasetSelectorButtonText();
-        expect(azureDatasetSelectionTitle).to.be('[azure] activitylogs');
+        expect(azureDatasetSelectionTitle).to.be('[Azure Logs] activitylogs');
 
         // Go back to previous page selection
-        await retry.try(async () => {
-          await browser.goBack();
-          const backNavigationDatasetSelectionTitle =
-            await PageObjects.discoverLogExplorer.getDatasetSelectorButtonText();
-          expect(backNavigationDatasetSelectionTitle).to.be('All log datasets');
-        });
+
+        await browser.goBack();
+        const backNavigationDatasetSelectionTitle =
+          await PageObjects.discoverLogExplorer.getDatasetSelectorButtonText();
+        expect(backNavigationDatasetSelectionTitle).to.be('All log datasets');
 
         // Go forward to previous page selection
-        await retry.try(async () => {
-          await browser.goForward();
-          const forwardNavigationDatasetSelectionTitle =
-            await PageObjects.discoverLogExplorer.getDatasetSelectorButtonText();
-          expect(forwardNavigationDatasetSelectionTitle).to.be('[azure] activitylogs');
-        });
+
+        await browser.goForward();
+        const forwardNavigationDatasetSelectionTitle =
+          await PageObjects.discoverLogExplorer.getDatasetSelectorButtonText();
+        expect(forwardNavigationDatasetSelectionTitle).to.be('[Azure Logs] activitylogs');
       });
     });
   });
