b77e76ac497291d8159f508feb412c923304b1ba
rshen <rshen@elastic.co>
2023-07-13 07:37:08 -0600

    fix jest config files

--- x-pack/plugins/reporting/jest.config.js
+++ x-pack/plugins/reporting/jest.config.js
@@ -11,16 +11,4 @@ module.exports = {
   roots: ['<rootDir>/x-pack/plugins/reporting'],
   coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/reporting',
   coverageReporters: ['text', 'html'],
-  testPathIgnorePatterns: [
-    '<rootDir>/x-pack/plugins/reporting/server/export_types/common/get_full_urls.test.ts',
-    '<rootDir>/x-pack/plugins/reporting/server/usage/reporting_usage_collector.test.ts',
-    '<rootDir>/x-pack/plugins/reporting/server/export_types/png/execute_job/index.test.ts',
-    '<rootDir>/x-pack/plugins/reporting/server/export_types/printable_pdf/execute_job/index.test.ts',
-    '<rootDir>/x-pack/plugins/reporting/server/usage/get_export_stats.test.ts',
-    '<rootDir/x-pack/plugins/reporting/server/usage/reporting_usage_collector.test.ts',
-    '<rootDir>x-pack/plugins/reporting/server/routes/management/integration_tests/jobs.test.ts',
-  ],
-  collectCoverageFrom: [
-    '<rootDir>/x-pack/plugins/reporting/{common, public, server}/**/*.{js,ts,tsx}',
-  ],
 };
