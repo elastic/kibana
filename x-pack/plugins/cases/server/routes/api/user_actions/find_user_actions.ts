cc9adf20b01617370e42fef86237577f424f32af
Christos Nasikas <christos.nasikas@elastic.co>
2023-07-14 09:55:38 +0300

    Version routes

--- x-pack/plugins/cases/server/routes/api/user_actions/find_user_actions.ts
+++ x-pack/plugins/cases/server/routes/api/user_actions/find_user_actions.ts
@@ -6,7 +6,7 @@
  */
 
 import { schema } from '@kbn/config-schema';
-import type { UserActionFindRequest } from '../../../../common/types/api';
+import type { userActionApiV1 } from '../../../../common/types/api';
 
 import { CASE_FIND_USER_ACTIONS_URL } from '../../../../common/constants';
 import { createCaseError } from '../../../common/error';
@@ -25,10 +25,15 @@ export const findUserActionsRoute = createCasesRoute({
       const caseContext = await context.cases;
       const casesClient = await caseContext.getCasesClient();
       const caseId = request.params.case_id;
-      const options = request.query as UserActionFindRequest;
+      const options = request.query as userActionApiV1.UserActionFindRequest;
+
+      const res: userActionApiV1.UserActionFindResponse = await casesClient.userActions.find({
+        caseId,
+        params: options,
+      });
 
       return response.ok({
-        body: await casesClient.userActions.find({ caseId, params: options }),
+        body: res,
       });
     } catch (error) {
       throw createCaseError({
