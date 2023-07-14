a27b2321903257d06996c95970270c546b7d9aaa
Søren Louv-Jansen <soren.louv@elastic.co>
2023-07-14 13:15:09 +0200

    [APM] Prevent console.error from failing tests

--- x-pack/plugins/apm/jest_setup.js
+++ x-pack/plugins/apm/jest_setup.js
@@ -4,12 +4,3 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-/* global jest */
-
-// When a `console.error` is encountered, throw the error to make the test fail.
-// This effectively treats logged errors during the test run as failures.
-jest.spyOn(console, 'error').mockImplementation((message, ...args) => {
-  console.log(message, ...args);
-  throw new Error(message);
-});
