5e4ee2c7b96dc7c4a24edd439540ef27d3386cb1
gbamparop <georgios.bamparopoulos@elastic.co>
2023-07-14 14:39:15 +0100

    Hide the top five affected transactions table for OTel agents

--- x-pack/plugins/apm/public/components/app/error_group_details/index.tsx
+++ x-pack/plugins/apm/public/components/app/error_group_details/index.tsx
@@ -17,6 +17,7 @@ import { i18n } from '@kbn/i18n';
 import React, { useEffect } from 'react';
 import { omit } from 'lodash';
 import { useHistory } from 'react-router-dom';
+import { isOpenTelemetryAgentName } from '../../../../common/agent_name';
 import { NOT_AVAILABLE_LABEL } from '../../../../common/i18n';
 import { useApmServiceContext } from '../../../context/apm_service/use_apm_service_context';
 import { useBreadcrumb } from '../../../context/breadcrumbs/use_breadcrumb';
@@ -31,6 +32,7 @@ import { ErrorDistribution } from './distribution';
 import { TopErroneousTransactions } from './top_erroneous_transactions';
 import { maybe } from '../../../../common/utils/maybe';
 import { fromQuery, toQuery } from '../../shared/links/url_helpers';
+import { AgentName } from '../../../../typings/es_schemas/ui/fields/agent';
 
 type ErrorSamplesAPIResponse =
   APIReturnType<'GET /internal/apm/services/{serviceName}/errors/{groupId}/samples'>;
@@ -188,6 +190,9 @@ export function ErrorGroupDetails() {
     }
   }, [history, errorId, errorSamplesData, errorSamplesFetchStatus]);
 
+  const { agentName } = useApmServiceContext();
+  const isOpenTelemetryAgent = isOpenTelemetryAgentName(agentName as AgentName);
+
   // If there are 0 occurrences, show only charts w. empty message
   const showDetails = errorSamplesData.occurrencesCount !== 0;
 
@@ -216,11 +221,13 @@ export function ErrorGroupDetails() {
             />
           </EuiPanel>
         </EuiFlexItem>
-        <EuiFlexItem grow={2}>
-          <EuiPanel hasBorder={true}>
-            <TopErroneousTransactions serviceName={serviceName} />
-          </EuiPanel>
-        </EuiFlexItem>
+        {!isOpenTelemetryAgent && (
+          <EuiFlexItem grow={2}>
+            <EuiPanel hasBorder={true}>
+              <TopErroneousTransactions serviceName={serviceName} />
+            </EuiPanel>
+          </EuiFlexItem>
+        )}
       </EuiFlexGroup>
       <EuiSpacer size="s" />
       {showDetails && (
