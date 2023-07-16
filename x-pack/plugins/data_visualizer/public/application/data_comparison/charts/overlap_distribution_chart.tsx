86eef58c83a254c22c1958ba20b37e9a87858b80
Quynh Nguyen <quynh.nguyen@elastic.co>
2023-07-16 12:16:19 -0500

    Refactor components

---
+++ x-pack/plugins/data_visualizer/public/application/data_comparison/charts/overlap_distribution_chart.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { AreaSeries, Chart, CurveType, ScaleType, Settings, Tooltip } from '@elastic/charts';
+import { i18n } from '@kbn/i18n';
+import React from 'react';
+import { ComparisionHistogram, DataComparisonField } from '../types';
+import { DataComparisonChartTooltipBody } from '../data_comparison_chart_tooltip_body';
+import { COMPARISON_LABEL, DATA_COMPARISON_TYPE } from '../constants';
+
+export const OverlapDistributionComparison = ({
+  data,
+  colors,
+  fieldType,
+  fieldName,
+}: {
+  data: ComparisionHistogram[];
+  colors: { referenceColor: string; productionColor: string };
+  fieldType?: DataComparisonField['type'];
+  fieldName?: DataComparisonField['field'];
+}) => {
+  return (
+    <Chart>
+      <Tooltip body={DataComparisonChartTooltipBody} />
+
+      <Settings showLegend={false} />
+      <AreaSeries
+        id="dataVisualizer.overlapDistributionComparisonChart"
+        name={i18n.translate(
+          'xpack.dataVisualizer.dataComparison.distributionComparisonChartName',
+          {
+            defaultMessage:
+              'Distribution comparison of reference and comparison data for {fieldName}',
+            values: { fieldName },
+          }
+        )}
+        xScaleType={
+          fieldType === DATA_COMPARISON_TYPE.NUMERIC ? ScaleType.Linear : ScaleType.Ordinal
+        }
+        yScaleType={ScaleType.Linear}
+        xAccessor="key"
+        yAccessors={['percentage']}
+        splitSeriesAccessors={['g']}
+        data={data}
+        curve={CurveType.CURVE_STEP_AFTER}
+        color={(identifier) => {
+          const key = identifier.seriesKeys[0];
+          return key === COMPARISON_LABEL ? colors.productionColor : colors.referenceColor;
+        }}
+      />
+    </Chart>
+  );
+};
--- x-pack/plugins/data_visualizer/public/application/data_comparison/data_comparison_utils.ts
+++ x-pack/plugins/data_visualizer/public/application/data_comparison/data_comparison_utils.ts
@@ -48,3 +48,18 @@ export const computeChi2PValue = (
   const result = chi2test(table);
   return result.pValue;
 };
+
+/**
+ * formatSignificanceLevel
+ * @param significanceLevel
+ */
+export const formatSignificanceLevel = (significanceLevel: number) => {
+  if (typeof significanceLevel !== 'number' || isNaN(significanceLevel)) return '';
+  if (significanceLevel < 1e-6) {
+    return '< 0.000001';
+  } else if (significanceLevel < 0.01) {
+    return significanceLevel.toExponential(0);
+  } else {
+    return significanceLevel.toFixed(2);
+  }
+};
