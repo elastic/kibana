ee2d46b8d892c65adc8d2a8e761fb66ab1f73748
Gloria Hornero <gloria.hornero@elastic.co>
2023-07-17 17:17:27 +0200

    adjusting timeouts

--- x-pack/test_serverless/functional/test_suites/security/cypress/cypress.config.ts
+++ x-pack/test_serverless/functional/test_suites/security/cypress/cypress.config.ts
@@ -8,9 +8,9 @@
 import { defineCypressConfig } from '@kbn/cypress-config';
 
 export default defineCypressConfig({
-  defaultCommandTimeout: 60000,
-  execTimeout: 60000,
-  pageLoadTimeout: 60000,
+  defaultCommandTimeout: 150000,
+  execTimeout: 150000,
+  pageLoadTimeout: 150000,
   responseTimeout: 60000,
   trashAssetsBeforeRuns: false,
   video: false,
