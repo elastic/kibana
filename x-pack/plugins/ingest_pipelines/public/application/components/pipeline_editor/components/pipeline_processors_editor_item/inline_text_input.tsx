74cd2dbd1edd67ad7016ee93f6ded2dbcbab151c
Bree Hall <briannajdhall@gmail.com>
2023-07-18 16:46:14 -0400

    [Ingest Node Pipelines] Install EuiInlineEdit into the inline_text_input component

--- x-pack/plugins/ingest_pipelines/public/application/components/pipeline_editor/components/pipeline_processors_editor_item/inline_text_input.tsx
+++ x-pack/plugins/ingest_pipelines/public/application/components/pipeline_editor/components/pipeline_processors_editor_item/inline_text_input.tsx
@@ -5,9 +5,8 @@
  * 2.0.
  */
 
-import classNames from 'classnames';
-import React, { useState, useEffect, useCallback, memo } from 'react';
-import { EuiFieldText, EuiText, keys, EuiToolTip } from '@elastic/eui';
+import React, { memo } from 'react';
+import { EuiInlineEditText } from '@elastic/eui';
 
 export interface Props {
   placeholder: string;
@@ -32,75 +31,17 @@ function _InlineTextInput({
   disabled = false,
   onChange,
 }: Props): React.ReactElement<any, any> | null {
-  const [isShowingTextInput, setIsShowingTextInput] = useState<boolean>(false);
-  const [textValue, setTextValue] = useState<string>(() => text ?? '');
-
-  const containerClasses = classNames('pipelineProcessorsEditor__item__textContainer', {
-    'pipelineProcessorsEditor__item__textContainer--notEditing': !isShowingTextInput && !disabled,
-  });
-
-  const submitChange = useCallback(() => {
-    // Give any on blur handlers the chance to complete if the user is
-    // tabbing over this component.
-    setTimeout(() => {
-      setIsShowingTextInput(false);
-      onChange(textValue);
-    });
-  }, [setIsShowingTextInput, onChange, textValue]);
-
-  useEffect(() => {
-    setTextValue(text ?? '');
-  }, [text]);
-
-  useEffect(() => {
-    const keyboardListener = (event: KeyboardEvent) => {
-      if (event.key === keys.ESCAPE || event.code === 'Escape') {
-        setIsShowingTextInput(false);
-      }
-      if (event.key === keys.ENTER || event.code === 'Enter') {
-        submitChange();
-      }
-    };
-    if (isShowingTextInput) {
-      window.addEventListener('keyup', keyboardListener);
-    }
-    return () => {
-      window.removeEventListener('keyup', keyboardListener);
-    };
-  }, [isShowingTextInput, submitChange, setIsShowingTextInput]);
-
-  return isShowingTextInput && !disabled ? (
-    <div className={`pipelineProcessorsEditor__item__textContainer ${containerClasses}`}>
-      <EuiFieldText
-        controlOnly
-        onBlur={submitChange}
-        fullWidth
-        compressed
-        value={textValue}
-        aria-label={ariaLabel}
-        className="pipelineProcessorsEditor__item__textInput"
-        inputRef={(el) => el?.focus()}
-        onChange={(event) => setTextValue(event.target.value)}
-      />
-    </div>
-  ) : (
-    <div
-      className={containerClasses}
-      tabIndex={disabled ? -1 : 0}
-      onFocus={() => setIsShowingTextInput(true)}
-    >
-      <EuiToolTip content={text ?? placeholder}>
-        <EuiText size="s" color="subdued">
-          <div
-            className="pipelineProcessorsEditor__item__description"
-            data-test-subj="inlineTextInputNonEditableText"
-          >
-            {text || <em>{placeholder}</em>}
-          </div>
-        </EuiText>
-      </EuiToolTip>
-    </div>
+  const euiInlineEdit = (
+    <EuiInlineEditText
+      size="s"
+      defaultValue={text || ''}
+      placeholder={placeholder}
+      inputAriaLabel={ariaLabel}
+      isReadOnly={disabled}
+    />
   );
+
+  return euiInlineEdit;
 }
 
 export const InlineTextInput = memo(_InlineTextInput);
