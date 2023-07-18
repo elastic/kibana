03d5f60b5a07b5d4352777f508b8959563aba8d5
Mike Cote <michel.cote@elastic.co>
2023-07-18 13:32:40 -0400

    Same can be done for general threshold rule

--- x-pack/plugins/observability/server/index.ts
+++ x-pack/plugins/observability/server/index.ts
@@ -44,7 +44,7 @@ const configSchema = schema.object({
       }),
     }),
     thresholdRule: schema.object({
-      enabled: schema.boolean({ defaultValue: false }),
+      enabled: schema.boolean({ defaultValue: true }),
     }),
   }),
   thresholdRule: schema.object({
