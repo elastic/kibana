a9c59b6b6c20b5adacecfbd7672605ca7e99001a
Coen Warmer <coen.warmer@gmail.com>
2023-07-19 22:38:01 +0200

    Added Insight component

---
+++ x-pack/plugins/observability_ai_assistant/public/components/insight/insight_missing_credentials.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiButton, EuiCallOut, EuiSpacer } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+
+export function InsightMissingCredentials() {
+  return (
+    <EuiCallOut
+      title={i18n.translate('xpack.obsAiAssistant.insight.missing.title', {
+        defaultMessage: 'Missing credentials',
+      })}
+      color="primary"
+      iconType="iInCircle"
+    >
+      {i18n.translate('xpack.obsAiAssistant.insight.missing.description', {
+        defaultMessage:
+          'You haven’t authorised OpenAI in order to generate responses from the Elastic Assistant. Authorise the model in order to proceed.',
+      })}
+
+      <EuiSpacer size="m" />
+
+      <EuiButton fill color="primary">
+        {i18n.translate('xpack.obsAiAssistant.insight.missing.buttonLabel', {
+          defaultMessage: 'Connect Assistant',
+        })}
+      </EuiButton>
+    </EuiCallOut>
+  );
+}
