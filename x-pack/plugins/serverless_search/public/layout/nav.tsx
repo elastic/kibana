14d5cac7fca11ce23f2372e0e8b576330e6bfd1f
Sander Philipse <94373878+sphilipse@users.noreply.github.com>
2023-07-19 16:21:02 +0800

    [Serverless Search] Remove unwanted alterting rule and connector types (#162105)
    ## Summary
    
    This moves alerting in Serverless Search to the Stack Management
    alerting page, and removes
    
    To remove these rule types, I had to disable the observability,
    securitySolution, uptime, monitoring and infra plugins and make sure
    that their server plugins (not just the frontend plugins) respected the
    `enabled: false` flag.

--- x-pack/plugins/serverless_search/public/layout/nav.tsx
+++ x-pack/plugins/serverless_search/public/layout/nav.tsx
@@ -67,7 +67,12 @@ const navigationTree: NavigationTreeDefinition = {
                 );
               },
             },
-            { link: 'observability-overview:alerts' },
+            {
+              link: 'management:triggersActions',
+              title: i18n.translate('xpack.serverlessSearch.nav.alerts', {
+                defaultMessage: 'Alerts',
+              }),
+            },
           ],
         },
         {
