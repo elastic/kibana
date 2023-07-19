8bc6909391a2ba8055b9d337b3e92610958f284c
Chris Cowan <chris@elastic.co>
2023-07-19 13:09:11 -0600

    Updating design based on feedback

--- x-pack/plugins/observability/public/pages/slo_edit/components/custom_metric/custom_metric_type_form.tsx
+++ x-pack/plugins/observability/public/pages/slo_edit/components/custom_metric/custom_metric_type_form.tsx
@@ -39,134 +39,150 @@ export function CustomMetricIndicatorTypeForm() {
   const timestampFields = (indexFields ?? []).filter((field) => field.type === 'date');
 
   return (
-    <EuiFlexGroup direction="column" gutterSize="l">
-      <EuiFlexGroup direction="row" gutterSize="l">
-        <EuiFlexItem>
-          <IndexSelection />
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiFormRow
-            label={i18n.translate(
-              'xpack.observability.slo.sloEdit.sliType.customMetric.timestampField.label',
-              { defaultMessage: 'Timestamp field' }
-            )}
-            isInvalid={getFieldState('indicator.params.timestampField').invalid}
-          >
-            <Controller
-              name="indicator.params.timestampField"
-              defaultValue=""
-              rules={{ required: true }}
-              control={control}
-              render={({ field: { ref, ...field }, fieldState }) => (
-                <EuiComboBox
-                  {...field}
-                  async
-                  placeholder={i18n.translate(
-                    'xpack.observability.slo.sloEdit.sliType.customMetric.timestampField.placeholder',
-                    { defaultMessage: 'Select a timestamp field' }
-                  )}
-                  aria-label={i18n.translate(
-                    'xpack.observability.slo.sloEdit.sliType.customMetric.timestampField.placeholder',
-                    { defaultMessage: 'Select a timestamp field' }
-                  )}
-                  data-test-subj="customMetricIndicatorFormTimestampFieldSelect"
-                  isClearable
-                  isDisabled={!watch('indicator.params.index')}
-                  isInvalid={fieldState.invalid}
-                  isLoading={!!watch('indicator.params.index') && isLoading}
-                  onChange={(selected: EuiComboBoxOptionOption[]) => {
-                    if (selected.length) {
-                      return field.onChange(selected[0].value);
+    <>
+      <EuiTitle size="xs">
+        <h3>
+          <FormattedMessage
+            id="xpack.observability.slo.sloEdit.sliType.histogram.sourceTitle"
+            defaultMessage="Source"
+          />
+        </h3>
+      </EuiTitle>
+      <EuiSpacer size="s" />
+      <EuiFlexGroup direction="column" gutterSize="l">
+        <EuiFlexGroup direction="row" gutterSize="l">
+          <EuiFlexItem>
+            <IndexSelection />
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiFormRow
+              label={i18n.translate(
+                'xpack.observability.slo.sloEdit.sliType.customMetric.timestampField.label',
+                { defaultMessage: 'Timestamp field' }
+              )}
+              isInvalid={getFieldState('indicator.params.timestampField').invalid}
+            >
+              <Controller
+                name="indicator.params.timestampField"
+                defaultValue=""
+                rules={{ required: true }}
+                control={control}
+                render={({ field: { ref, ...field }, fieldState }) => (
+                  <EuiComboBox
+                    {...field}
+                    async
+                    placeholder={i18n.translate(
+                      'xpack.observability.slo.sloEdit.sliType.customMetric.timestampField.placeholder',
+                      { defaultMessage: 'Select a timestamp field' }
+                    )}
+                    aria-label={i18n.translate(
+                      'xpack.observability.slo.sloEdit.sliType.customMetric.timestampField.placeholder',
+                      { defaultMessage: 'Select a timestamp field' }
+                    )}
+                    data-test-subj="customMetricIndicatorFormTimestampFieldSelect"
+                    isClearable
+                    isDisabled={!watch('indicator.params.index')}
+                    isInvalid={fieldState.invalid}
+                    isLoading={!!watch('indicator.params.index') && isLoading}
+                    onChange={(selected: EuiComboBoxOptionOption[]) => {
+                      if (selected.length) {
+                        return field.onChange(selected[0].value);
+                      }
+
+                      field.onChange('');
+                    }}
+                    options={createOptionsFromFields(timestampFields)}
+                    selectedOptions={
+                      !!watch('indicator.params.index') &&
+                      !!field.value &&
+                      timestampFields.some((timestampField) => timestampField.name === field.value)
+                        ? [
+                            {
+                              value: field.value,
+                              label: field.value,
+                              'data-test-subj': `customMetricIndicatorFormTimestampFieldSelectedValue`,
+                            },
+                          ]
+                        : []
                     }
+                    singleSelection={{ asPlainText: true }}
+                  />
+                )}
+              />
+            </EuiFormRow>
+          </EuiFlexItem>
+        </EuiFlexGroup>
 
-                    field.onChange('');
-                  }}
-                  options={createOptionsFromFields(timestampFields)}
-                  selectedOptions={
-                    !!watch('indicator.params.index') &&
-                    !!field.value &&
-                    timestampFields.some((timestampField) => timestampField.name === field.value)
-                      ? [
-                          {
-                            value: field.value,
-                            label: field.value,
-                            'data-test-subj': `customMetricIndicatorFormTimestampFieldSelectedValue`,
-                          },
-                        ]
-                      : []
+        <EuiFlexItem>
+          <QueryBuilder
+            dataTestSubj="customMetricIndicatorFormQueryFilterInput"
+            indexPatternString={watch('indicator.params.index')}
+            label={i18n.translate(
+              'xpack.observability.slo.sloEdit.sliType.customMetric.queryFilter',
+              {
+                defaultMessage: 'Query filter',
+              }
+            )}
+            name="indicator.params.filter"
+            placeholder={i18n.translate(
+              'xpack.observability.slo.sloEdit.sliType.customMetric.customFilter',
+              { defaultMessage: 'Custom filter to apply on the index' }
+            )}
+            tooltip={
+              <EuiIconTip
+                content={i18n.translate(
+                  'xpack.observability.slo.sloEdit.sliType.customMetric.customFilter.tooltip',
+                  {
+                    defaultMessage:
+                      'This KQL query can be used to filter the documents with some relevant criteria.',
                   }
-                  singleSelection={{ asPlainText: true }}
-                />
-              )}
-            />
-          </EuiFormRow>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-
-      <EuiFlexItem>
-        <QueryBuilder
-          dataTestSubj="customMetricIndicatorFormQueryFilterInput"
-          indexPatternString={watch('indicator.params.index')}
-          label={i18n.translate(
-            'xpack.observability.slo.sloEdit.sliType.customMetric.queryFilter',
-            {
-              defaultMessage: 'Query filter',
+                )}
+                position="top"
+              />
             }
-          )}
-          name="indicator.params.filter"
-          placeholder={i18n.translate(
-            'xpack.observability.slo.sloEdit.sliType.customMetric.customFilter',
-            { defaultMessage: 'Custom filter to apply on the index' }
-          )}
-          tooltip={
-            <EuiIconTip
-              content={i18n.translate(
-                'xpack.observability.slo.sloEdit.sliType.customMetric.customFilter.tooltip',
-                {
-                  defaultMessage:
-                    'This KQL query can be used to filter the documents with some relevant criteria.',
-                }
-              )}
-              position="top"
-            />
-          }
-        />
-      </EuiFlexItem>
+          />
+        </EuiFlexItem>
 
-      <EuiFlexItem>
-        <EuiHorizontalRule margin="none" />
-      </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiHorizontalRule margin="none" />
+        </EuiFlexItem>
 
-      <EuiFlexItem>
-        <EuiTitle size="xs">
-          <h3>
-            <FormattedMessage
-              id="xpack.observability.slo.sloEdit.sliType.customMetric.goodTitle"
-              defaultMessage="Good events"
-            />
-          </h3>
-        </EuiTitle>
-        <EuiSpacer size="s" />
-        <MetricIndicator type="good" indexFields={indexFields} isLoadingIndex={isLoading} />
-      </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiTitle size="xs">
+            <h3>
+              <FormattedMessage
+                id="xpack.observability.slo.sloEdit.sliType.customMetric.goodTitle"
+                defaultMessage="Good events"
+              />
+            </h3>
+          </EuiTitle>
+          <EuiSpacer size="s" />
+          <MetricIndicator type="good" indexFields={indexFields} isLoadingIndex={isLoading} />
+        </EuiFlexItem>
 
-      <EuiFlexItem>
-        <EuiHorizontalRule margin="none" />
-      </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiHorizontalRule margin="none" />
+        </EuiFlexItem>
 
-      <EuiFlexItem>
-        <EuiTitle size="xs">
-          <h3>
-            <FormattedMessage
-              id="xpack.observability.slo.sloEdit.sliType.customMetric.totalTitle"
-              defaultMessage="Total events"
-            />
-          </h3>
-        </EuiTitle>
-        <EuiSpacer size="s" />
-        <MetricIndicator type="total" indexFields={indexFields} isLoadingIndex={isLoading} />
-      </EuiFlexItem>
-      <DataPreviewChart />
-    </EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiTitle size="xs">
+            <h3>
+              <FormattedMessage
+                id="xpack.observability.slo.sloEdit.sliType.customMetric.totalTitle"
+                defaultMessage="Total events"
+              />
+            </h3>
+          </EuiTitle>
+          <EuiSpacer size="s" />
+          <MetricIndicator type="total" indexFields={indexFields} isLoadingIndex={isLoading} />
+        </EuiFlexItem>
+
+        <EuiFlexItem>
+          <EuiHorizontalRule margin="none" />
+        </EuiFlexItem>
+
+        <DataPreviewChart />
+      </EuiFlexGroup>
+    </>
   );
 }
