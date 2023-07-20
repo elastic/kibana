fe2567192a6dbfad36b45fcf5ee312b25290d348
oatkiller <robert.austin@elastic.co>
2023-07-20 10:21:27 -0400

    hope this works

--- x-pack/plugins/security_solution/scripts/junit_transformer/junit_transformer.ts
+++ x-pack/plugins/security_solution/scripts/junit_transformer/junit_transformer.ts
@@ -35,25 +35,56 @@ run(
       const unvalidatedReportJson: unknown = await parseStringPromise(source);
 
       // Apply validation and return the validated report, or an error message
-      const maybeValidationResult: { result: CypressJunitReport } | { error : string } = validatedCypressJunitReport(unvalidatedReportJson);
+      const maybeValidationResult: { result: t.TypeOf<typeof CypressJunitReport> } | { error: string } = validatedCypressJunitReport(unvalidatedReportJson);
+
+      const logError = (error: string) => {
+        log.error(`Error while validating ${path}: ${error}
+
+This script validates each Junit report to ensure that it was produced by Cypress and that it has not already been processed by this script
+This script relies on various assumptions. If your junit report is valid, then you must enhance this script in order to have support for it. If you are not trying to transform a Cypress junit report into a report that is compatible with Kibana Operations workflows, then you are running this script in error.`);
+      };
 
       if ('error' in maybeValidationResult) {
+        logError(maybeValidationResult.error);
         // If there is an error, continue trying to process other files.
-        log.error(`Error while validating ${path}: ${maybeValidationResult.error}`);
-      } else {
-        const reportJson: CypressJunitReport = maybeValidationResult.result;
-        const reportString: string = await transformedReport({
-          reportJson,
-          reportName: flags.reportName,
-          rootDirectory: flags.rootDirectory
-        });
+        continue;
+      }
 
-        // If the writeInPlace flag was passed, overwrite the original file, otherwise log the output to stdout
-        if (flags.writeInPlace) {
-          await fs.writeFile(path, reportString);
-        } else {
-          log.write(reportString);
-        }
+      const reportJson: t.TypeOf<typeof CypressJunitReport> = maybeValidationResult.result;
+
+      const maybeAlreadyProcessedResult = isReportAlreadyProcessed(reportJson);
+      if ('error' in maybeAlreadyProcessedResult) {
+        logError(maybeAlreadyProcessedResult.error);
+        // If there is an error, continue trying to process other files.
+        continue
+      }
+
+      if (maybeAlreadyProcessedResult.result) {
+        logError("This report appears to have already been transformed because a '·' character was found in the classname. If your test intentionally includes this character as part of its name, remove it. This character is reserved for encoding file paths in the classname attribute.");
+        // If there is an error, continue trying to process other files.
+        continue;
+      }
+
+      const maybeSpecFilePath: { result: string } | { error: string } = findSpecFilePathFromRootSuite(reportJson);
+
+      if ('error' in maybeSpecFilePath) {
+        logError(maybeSpecFilePath.error);
+        // If there is an error, continue trying to process other files.
+        continue;
+      }
+
+      const reportString: string = await transformedReport({
+        reportJson,
+        specFilePath: maybeSpecFilePath.result,
+        reportName: flags.reportName,
+        rootDirectory: flags.rootDirectory
+      });
+
+      // If the writeInPlace flag was passed, overwrite the original file, otherwise log the output to stdout
+      if (flags.writeInPlace) {
+        await fs.writeFile(path, reportString);
+      } else {
+        log.write(reportString);
       }
     }
     log.success('task complete');
@@ -82,29 +113,22 @@ run(
  * `name` will have the value of `classname` appended to it. This makes sense because they each contain part of the bdd spec.
  * `classname` is replaced with the file path, relative to the kibana project directory, and encoded (by replacing periods with a non-ascii character.) This is the format expected by the failed test reporter and the Kibana Operations flaky test triage workflows.
  */
-async function transformedReport({ reportJson, rootDirectory, reportName }: { reportJson: CypressJunitReport, rootDirectory: string, reportName: string }): Promise<string> {
+async function transformedReport({ reportJson, specFilePath, rootDirectory, reportName }: { reportJson: t.TypeOf<typeof CypressJunitReport>, specFilePath: string, rootDirectory: string, reportName: string }): Promise<string> {
 
-  // The first testsuite is the 'root' test suite and contains the file path to the spec
-  const rootSuite = reportJson.testsuites.testsuite[0];
+  for (const testsuite of reportJson.testsuites.testsuite) {
+    for (const testcase of testsuite.testcase) {
 
-  const specFile = rootSuite.$.file;
+      // append the `classname` attribute to the `name` attribute
+      testcase.$.name = `${testcase.$.name} ${testcase.$.classname}`;
 
-  for (const testsuite of reportJson.testsuites.testsuite.slice(1)) {
-    if (CypressJunitTestSuite.is(testsuite)) {
-      for (const testcase of testsuite.testcase) {
+      // calculate the path of the spec file relative to the kibana project directory
+      const projectRelativePath = relative(rootDirectory, specFilePath);
 
-        // append the `classname` attribute to the `name` attribute
-        testcase.$.name = `${testcase.$.name} ${testcase.$.classname}`;
+      // encode the path by relacing dots with a non-ascii character
+      const encodedPath = projectRelativePath.replace(/\./g, '·');
 
-        // calculate the path of the spec file relative to the kibana project directory
-        const projectRelativePath = relative(rootDirectory, specFile);
-
-        // encode the path by relacing dots with a non-ascii character
-        const encodedPath = projectRelativePath.replace(/\./g, '·');
-
-        // prepend the encoded path with a report name. This is for display purposes and shows up in the github issue. It is required. Store the value in the `classname` attribute.
-        testcase.$.classname = `${reportName}.${encodedPath}`;
-      }
+      // prepend the encoded path with a report name. This is for display purposes and shows up in the github issue. It is required. Store the value in the `classname` attribute.
+      testcase.$.classname = `${reportName}.${encodedPath}`;
     }
   }
 
@@ -127,103 +151,70 @@ const CypressJunitTestCase = t.type({
  * Standard testsuites contain testcase elements, each representing a specific test execution.
  */
 const CypressJunitTestSuite = t.type({
-  testcase: t.array(CypressJunitTestCase)
+  testcase: t.array(CypressJunitTestCase),
+  $: t.intersection([
+    t.type({
+      name: t.string,
+    }),
+    t.partial({
+      file: t.string
+    }),
+  ])
 });
 
 /**
- * The root suite is a special test suite, found at the same level as other test suites, but which contains no test cases and has the file name as an attribute.
- */
-const CypressJunitRootTestSuite = t.type({
-  $: t.type({
-    name: t.literal("Root Suite"),
-    file: t.string
-  })
-})
-
-/** This type represents the Cypress-specific flavor of junit report. It is 'Loose', because it does not enforce the following rule:
- * The first test suite must be the 'root' test suite, which has no test cases and has a 'file' attribute. All subsequent testsuite's must have testcases.
- *
- * See `CypressJunitReport` for a stricter type, which is returned from `validatedCypressJunitReport`
- */
-const LooseCypressJunitReport = t.type({
+ * This type represents the Cypress-specific flavor of junit report.
+ **/
+const CypressJunitReport = t.type({
   testsuites: t.type({
-    /** The testsuite's created by the Cypress junit reporter are non-standard. The first testsuite has a name: 'Root Suite' and contains the path of the spec file, relative to where Cypress was invoked, in the 'file' attribute. Other testsuites are standard and relate to a 'describe' block. They contain 'testcase' elements.
-     *
-     * For this reason, the testsuite array is a union of two types, the CypressJunitRootTestSuite, which is the first element, and which is non-standard, and the CypressJunitTestSuite, which is the type representing the remaining, standard, testsuite elements.
-     */
-    testsuite: t.array(t.union([CypressJunitRootTestSuite, CypressJunitTestSuite]))
+    testsuite: t.array(CypressJunitTestSuite)
   })
 })
 
-/**
- * Type representing a Cypress Junit report. Created by validatedCypressJunitReport. This is more specific than LooseCypressJunitReport.
+/*
+ * This checks if the junit report contains '·' characters in the classname. This character is used by the kibana operations triage scripts, and the failed test reporter, to replace `.` characters in a path as part of its encoding scheme. If this character is found, we assume that the encoding has already taken place.
  */
-type CypressJunitReport = {
-  testsuites: {
-    testsuite: [t.TypeOf<typeof CypressJunitRootTestSuite>, ...t.TypeOf<typeof CypressJunitTestSuite>[]]
+function isReportAlreadyProcessed(report: t.TypeOf<typeof CypressJunitReport>): { result: boolean } | { error: string } {
+  for (const testsuite of report.testsuites.testsuite) {
+    for (const testcase of testsuite.testcase) {
+      if (testcase.$.classname.indexOf('·') !== -1) {
+        return { result: true }
+      } else {
+        return { result: false }
+      }
+    }
   }
+  return { error: 'the report had no test cases.' }
 }
+
 
 /**
  * Validate the JSON representation of the Junit XML.
  * If there are no errors, this returns `{ result: 'successs' }`, otherwise it returns an error, wrapped in `{ error: string }`.
  *
- * This uses io-ts to do a base level of validation, and then does additional assertions. io-ts has, at the time of this writing, poor support for specifiying an array type where the first element has a different type than all subsequent elements. Therefore we do that validation using the custom predicate functions (i.e. `.is`.)
- *
- * This also asserts that the junit report contains no '·' characters in the classname. This character is used by the kibana operations triage scripts, and the failed test reporter, to replace `.` characters in a path as part of its encoding scheme. If this character is found, we assume that the encoding has already taken place.
  */
-function validatedCypressJunitReport(parsedReport: unknown): { result: CypressJunitReport } | { error: string } {
-  const decoded = LooseCypressJunitReport.decode(parsedReport);
-
-  // Error text to append to each error message.
-  const boilerplate: string = 'This script relies on this assumption. If your junit report is valid, then you must enhance this script in order to have support for it. If you are not trying to transform a Cypress junit report into a report that is compatible with Kibana Operations workflows, then you are running this script in error.';
+function validatedCypressJunitReport(parsedReport: unknown): { result: t.TypeOf<typeof CypressJunitReport> } | { error: string } {
+  const decoded = CypressJunitReport.decode(parsedReport);
 
   if (isLeft(decoded)) {
-    return { error: `Could not validate data: ${PathReporter.report(decoded).join("\n")}. This script uses an io-ts schema to validate that parsed Junit reports match the expected schema. This script is only designed to process Junit reports generated by Cypress. ${boilerplate}` }
+    return {
+      error: `Could not validate data: ${PathReporter.report(decoded).join("\n")}.
+` }
   }
+  return { result: decoded.right }
+}
 
-  // Used to store validation error messages from `predicate`.
-  const errorBox: { value: null | string } = { value: null };
 
-  // This predicate will narrow the type from LooseCypressJunitReport to CypressJunitReport
-  if (isCypressJunitReport(decoded.right, errorBox)) {
-    return { result: decoded.right };
-  } else {
-    if (errorBox.value === null) {
-      throw new Error('The predicate returned false without assigning predicate error.');
+/**
+ * Iterate over the test suites and find the root suite, which Cypress populates with the path to the spec file. Return the path.
+ */
+function findSpecFilePathFromRootSuite(reportJson: t.TypeOf<typeof CypressJunitReport>): { result: string } | { error: string } {
+  for (const testsuite of reportJson.testsuites.testsuite) {
+    if (testsuite.$.name === "Root Suite" && testsuite.$.file) {
+      return { result: testsuite.$.file }
     }
-    return { error: errorBox.value };
   }
-
-  /*
-   * Narrows `report` to a CypressJunitReport, which is a type that has CypressJunitRootTestSuite as the first test suite and CypressJunitTestSuite as all subsequent test suites.
-   *
-   * This typescript predicate needs to log error messages to the console, but predicates can only return boolean types.
-   * Therefore this populates the `errorBox` parameter with an error message when returning false.
-   */
-  function isCypressJunitReport(report: t.TypeOf<typeof LooseCypressJunitReport>, errorBox: { value: null | string }): report is CypressJunitReport {
-    for (let index = 0; index < report.testsuites.testsuite.length; index++) {
-      const testsuite = report.testsuites.testsuite[index];
-
-      if (index === 0) {
-        if (!CypressJunitRootTestSuite.is(testsuite)) {
-          errorBox.value = `The first suite must be the Root Suite, which contains the spec file name. ${boilerplate}`;
-          return false
-        }
-      } else {
-        if (!CypressJunitTestSuite.is(testsuite)) {
-          errorBox.value = `All testsuite elements except for the first one must have testcase elements. ${boilerplate}`;
-          return false
-        } else {
-          for (const testcase of testsuite.testcase) {
-            if (testcase.$.classname.indexOf('·') !== -1) {
-              errorBox.value = `This report appears to have already been transformed because a '·' character was found in the classname. If your test intentionally includes this character as part of its name, remove it. This character is reserved for encoding file paths in the classname attribute. ${boilerplate}`;
-              return false;
-            }
-          }
-        }
-      }
-    }
-    return true
+  return {
+    error: "No Root Suite containing a 'file' attribute was found."
   }
 }
