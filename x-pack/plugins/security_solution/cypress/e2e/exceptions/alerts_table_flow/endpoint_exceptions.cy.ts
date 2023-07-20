c83de5a856806d1baed6f4d94edb71a6d2618053
wafaanasr <wafaa.aanasr@gmail.com>
2023-07-20 16:52:42 +0100

    remove cy multiple running

--- x-pack/plugins/security_solution/cypress/e2e/exceptions/alerts_table_flow/endpoint_exceptions.cy.ts
+++ x-pack/plugins/security_solution/cypress/e2e/exceptions/alerts_table_flow/endpoint_exceptions.cy.ts
@@ -45,83 +45,81 @@ import {
 } from '../../../screens/exceptions';
 import { goToEndpointExceptionsTab } from '../../../tasks/rule_details';
 
-Cypress._.times(50, () => {
-  describe('Endpoint Exceptions workflows from Alert', () => {
-    const ITEM_NAME = 'Sample Exception List Item';
-    const ITEM_NAME_EDIT = 'Sample Exception List Item';
-    const ADDITIONAL_ENTRY = 'host.hostname';
-    beforeEach(() => {
-      esArchiverResetKibana();
-      login();
-      deleteAlertsAndRules();
-      esArchiverLoad('endpoint');
-      createRule(getEndpointRule());
-      visitWithoutDateRange(DETECTIONS_RULE_MANAGEMENT_URL);
-      goToRuleDetails();
-      waitForTheRuleToBeExecuted();
-      waitForAlertsToPopulate();
-    });
+describe('Endpoint Exceptions workflows from Alert', () => {
+  const ITEM_NAME = 'Sample Exception List Item';
+  const ITEM_NAME_EDIT = 'Sample Exception List Item';
+  const ADDITIONAL_ENTRY = 'host.hostname';
+  beforeEach(() => {
+    esArchiverResetKibana();
+    login();
+    deleteAlertsAndRules();
+    esArchiverLoad('endpoint');
+    createRule(getEndpointRule());
+    visitWithoutDateRange(DETECTIONS_RULE_MANAGEMENT_URL);
+    goToRuleDetails();
+    waitForTheRuleToBeExecuted();
+    waitForAlertsToPopulate();
+  });
 
-    after(() => {
-      esArchiverUnload('endpoint');
-      esArchiverUnload('endpoint_2');
-    });
+  after(() => {
+    esArchiverUnload('endpoint');
+    esArchiverUnload('endpoint_2');
+  });
 
-    it('Should be able to create and close single Endpoint exception from overflow menu', () => {
-      // The Endpoint will populated with predefined fields
-      openAddEndpointExceptionFromFirstAlert();
+  it('Should be able to create and close single Endpoint exception from overflow menu', () => {
+    // The Endpoint will populated with predefined fields
+    openAddEndpointExceptionFromFirstAlert();
 
-      // As the endpoint.alerts-* is used to trigger the alert the
-      // file.Ext.code_signature will be auto-populated
-      validateExceptionConditionField('file.Ext.code_signature');
+    // As the endpoint.alerts-* is used to trigger the alert the
+    // file.Ext.code_signature will be auto-populated
+    validateExceptionConditionField('file.Ext.code_signature');
 
-      selectCloseSingleAlerts();
-      addExceptionFlyoutItemName(ITEM_NAME);
-      submitNewExceptionItem();
+    selectCloseSingleAlerts();
+    addExceptionFlyoutItemName(ITEM_NAME);
+    submitNewExceptionItem();
 
-      // Instead of immediately checking if the Opened Alert has moved to the closed tab,
-      // use the waitForAlerts method to create a buffer, allowing the alerts some time to
-      // be moved to the Closed Alert tab.
-      waitForAlerts();
+    // Instead of immediately checking if the Opened Alert has moved to the closed tab,
+    // use the waitForAlerts method to create a buffer, allowing the alerts some time to
+    // be moved to the Closed Alert tab.
+    waitForAlerts();
 
-      // Closed alert should appear in table
-      goToClosedAlertsOnRuleDetailsPage();
-      cy.get(ALERTS_COUNT).should('exist');
-    });
+    // Closed alert should appear in table
+    goToClosedAlertsOnRuleDetailsPage();
+    cy.get(ALERTS_COUNT).should('exist');
+  });
 
-    it('Should be able to create Endpoint exception from Alerts take action button, and change multiple exception items without resetting to initial auto-prefilled entries', () => {
-      // Open first Alert Summary
-      expandFirstAlert();
+  it('Should be able to create Endpoint exception from Alerts take action button, and change multiple exception items without resetting to initial auto-prefilled entries', () => {
+    // Open first Alert Summary
+    expandFirstAlert();
 
-      // The Endpoint should populated with predefined fields
-      openAddEndpointExceptionFromAlertActionButton();
+    // The Endpoint should populated with predefined fields
+    openAddEndpointExceptionFromAlertActionButton();
 
-      // As the endpoint.alerts-* is used to trigger the alert the
-      // file.Ext.code_signature will be auto-populated
-      validateExceptionConditionField('file.Ext.code_signature');
-      addExceptionFlyoutItemName(ITEM_NAME);
+    // As the endpoint.alerts-* is used to trigger the alert the
+    // file.Ext.code_signature will be auto-populated
+    validateExceptionConditionField('file.Ext.code_signature');
+    addExceptionFlyoutItemName(ITEM_NAME);
 
-      cy.get(ADD_AND_BTN).click();
-      // edit conditions
-      addExceptionEntryFieldValue(ADDITIONAL_ENTRY, 6);
-      addExceptionEntryFieldValueValue('foo', 4);
+    cy.get(ADD_AND_BTN).click();
+    // edit conditions
+    addExceptionEntryFieldValue(ADDITIONAL_ENTRY, 6);
+    addExceptionEntryFieldValueValue('foo', 4);
 
-      // Change the name again
-      editExceptionFlyoutItemName(ITEM_NAME_EDIT);
+    // Change the name again
+    editExceptionFlyoutItemName(ITEM_NAME_EDIT);
 
-      // validate the condition is still "agent.name" or got rest after the name is changed
-      validateExceptionConditionField(ADDITIONAL_ENTRY);
+    // validate the condition is still "agent.name" or got rest after the name is changed
+    validateExceptionConditionField(ADDITIONAL_ENTRY);
 
-      selectCloseSingleAlerts();
-      submitNewExceptionItem();
+    selectCloseSingleAlerts();
+    submitNewExceptionItem();
 
-      // Endpoint Exception will move to Endpoint List under Exception tab of rule
-      goToEndpointExceptionsTab();
+    // Endpoint Exception will move to Endpoint List under Exception tab of rule
+    goToEndpointExceptionsTab();
 
-      // new exception item displays
-      cy.get(EXCEPTION_ITEM_VIEWER_CONTAINER).should('have.length', 1);
-      cy.get(EXCEPTION_CARD_ITEM_NAME).should('have.text', ITEM_NAME_EDIT);
-      cy.get(EXCEPTION_CARD_ITEM_CONDITIONS).contains('span', ADDITIONAL_ENTRY);
-    });
+    // new exception item displays
+    cy.get(EXCEPTION_ITEM_VIEWER_CONTAINER).should('have.length', 1);
+    cy.get(EXCEPTION_CARD_ITEM_NAME).should('have.text', ITEM_NAME_EDIT);
+    cy.get(EXCEPTION_CARD_ITEM_CONDITIONS).contains('span', ADDITIONAL_ENTRY);
   });
 });
