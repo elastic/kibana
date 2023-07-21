3c11621194779d44870d9a3b5eb0fe2626b61d3d
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-07-21 21:07:52 +0200

    burn

---
+++ .buildkite/scripts/steps/functional/security_solution_burn.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/steps/functional/common.sh
+source .buildkite/scripts/steps/functional/common_cypress.sh
+
+export JOB=kibana-security-solution-chrome
+export KIBANA_INSTALL_DIR=${KIBANA_BUILD_LOCATION}
+
+echo "--- Security Solution tests (Chrome)"
+
+yarn --cwd x-pack/plugins/security_solution cypress:burn
