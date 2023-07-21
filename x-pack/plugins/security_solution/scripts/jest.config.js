654b0c71a133ef77a753e400bd7cb3d3ff076763
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-07-21 00:12:56 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/security_solution/scripts/jest.config.js
+++ x-pack/plugins/security_solution/scripts/jest.config.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../..',
@@ -5,6 +12,5 @@ module.exports = {
   coverageDirectory:
     '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/security_solution/scripts',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/security_solution/scripts/**/*.{ts,tsx}']
+  collectCoverageFrom: ['<rootDir>/x-pack/plugins/security_solution/scripts/**/*.{ts,tsx}'],
 };
-
