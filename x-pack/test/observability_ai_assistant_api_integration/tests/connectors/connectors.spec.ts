47965bff81f2bff97e3458c23cb24ae4f3809dd2
Dario Gieselaar <dario.gieselaar@elastic.co>
2023-07-24 11:14:00 +0200

    Correct type import

--- x-pack/test/observability_ai_assistant_api_integration/tests/connectors/connectors.spec.ts
+++ x-pack/test/observability_ai_assistant_api_integration/tests/connectors/connectors.spec.ts
@@ -6,7 +6,7 @@
  */
 
 import expect from '@kbn/expect';
-import { FtrProviderContext } from '../../../common/ftr_provider_context';
+import { FtrProviderContext } from '../../common/ftr_provider_context';
 
 export default function ApiTest({ getService }: FtrProviderContext) {
   const observabilityAIAssistantAPIClient = getService('observabilityAIAssistantAPIClient');
