fb0c45ba297510a97ccc2f78e4ffa0d92b4f4d34
Gloria Hornero <gloria.hornero@elastic.co>
2023-07-24 19:24:35 +0200

    adjustment

--- x-pack/plugins/security_solution/cypress/tasks/timeline.ts
+++ x-pack/plugins/security_solution/cypress/tasks/timeline.ts
@@ -110,12 +110,10 @@ export const addNameToTimeline = (name: string, modalAlreadyOpen: boolean = fals
   if (!modalAlreadyOpen) {
     cy.get(TIMELINE_EDIT_MODAL_OPEN_BUTTON).first().click();
   }
-  cy.intercept('PATCH', ' /api/timeline').as('update');
   cy.get(TIMELINE_TITLE_INPUT).should('not.be.disabled');
   cy.get(TIMELINE_TITLE_INPUT).type(`${name}{enter}`);
   cy.get(TIMELINE_TITLE_INPUT).should('have.attr', 'value', name);
   cy.get(TIMELINE_EDIT_MODAL_SAVE_BUTTON).click();
-  cy.wait('@update', { timeout: 12000 });
   cy.get(TIMELINE_TITLE_INPUT).should('not.exist');
 };
 
