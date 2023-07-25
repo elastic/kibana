1770772ef10274a63deba4511557786af9bb35b4
Tiago Costa <tiago.costa@elastic.co>
2023-07-25 13:22:18 +0100

    chore(NA): bump version to 8.9.1

--- x-pack/test/functional/apps/ml/data_frame_analytics/classification_creation_saved_search.ts
+++ x-pack/test/functional/apps/ml/data_frame_analytics/classification_creation_saved_search.ts
@@ -123,7 +123,7 @@ export default function ({ getService }: FtrProviderContext) {
                   'Model memory limit',
                   '20mb',
                   'Version',
-                  '8.9.0',
+                  '8.9.1',
                 ],
               },
               {
@@ -224,7 +224,7 @@ export default function ({ getService }: FtrProviderContext) {
                   'Model memory limit',
                   '20mb',
                   'Version',
-                  '8.9.0',
+                  '8.9.1',
                 ],
               },
               {
@@ -324,7 +324,7 @@ export default function ({ getService }: FtrProviderContext) {
                   'Model memory limit',
                   '7mb',
                   'Version',
-                  '8.9.0',
+                  '8.9.1',
                 ],
               },
               {
@@ -421,7 +421,7 @@ export default function ({ getService }: FtrProviderContext) {
                   'Model memory limit',
                   '6mb',
                   'Version',
-                  '8.9.0',
+                  '8.9.1',
                 ],
               },
               {
