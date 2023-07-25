5142cfa08cf7871d46ca95c754a4231fef105a12
Adam Demjen <demjened@gmail.com>
2023-07-25 12:43:13 -0400

    Revert to single field mapping component

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/configure_fields.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/configure_fields.tsx
@@ -16,6 +16,7 @@ import { FormattedMessage } from '@kbn/i18n-react';
 import { InferenceConfiguration } from './inference_config';
 import { MLInferenceLogic } from './ml_inference_logic';
 import { MultiFieldMapping, SelectedFieldMappings } from './multi_field_selector';
+import { SingleFieldMapping } from './single_field_selector';
 
 export const ConfigureFields: React.FC = () => {
   const {
@@ -81,9 +82,7 @@ export const ConfigureFields: React.FC = () => {
           </>
         ) : (
           <>
-            {/* <SingleFieldMapping /> */}
-            {areInputsDisabled || <MultiFieldMapping />}
-            <SelectedFieldMappings isReadOnly={areInputsDisabled} />
+            <SingleFieldMapping />
             <InferenceConfiguration />
           </>
         )}
