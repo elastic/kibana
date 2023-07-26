a77a00ea35c2aeee7ddbd43caec255599418033d
Tre' Seymour <wayne.seymour@elastic.co>
2023-07-26 11:47:26 +0100

    [Es Archiver] Fixed HighWaterMark for Archives Impl Without Es Archiver Benchmark Tool
    Resolves: https://github.com/elastic/appex-qa-team/issues/31

--- packages/kbn-es-archiver/src/lib/docs/index_doc_records_stream.ts
+++ packages/kbn-es-archiver/src/lib/docs/index_doc_records_stream.ts
@@ -9,6 +9,8 @@
 import type { Client } from '@elastic/elasticsearch';
 import AggregateError from 'aggregate-error';
 import { Writable } from 'stream';
+import * as os from 'os';
+import { flow } from 'fp-ts/function';
 import { Stats } from '../stats';
 import { Progress } from '../progress';
 import { ES_CLIENT_HEADERS } from '../../client_headers';
@@ -91,4 +93,83 @@ export function createIndexDocRecordsStream(
       }
     },
   });
+}
+
+const buildRequest =
+  (indexOrCreate: BulkOperation) => (ops: WeakMap<any, any>) => (jsonStanza: any) => {
+    const body = jsonStanza.source;
+    ops.set(body, {
+      [jsonStanza.data_stream ? BulkOperation.Create : indexOrCreate]: {
+        _index: jsonStanza.data_stream || jsonStanza.index,
+        _id: jsonStanza.id,
+      },
+    });
+    return body;
+  };
+
+const cpuCount = () => os.cpus().length;
+const concurrencyMaxMinus1 = () => cpuCount() - 1;
+const eagerUpdate = (stats: Stats) => (jsonStanza: any) => {
+  stats.indexedDoc(jsonStanza.data_stream || jsonStanza.index);
+  return jsonStanza;
+};
+export function indexDocRecordsWritable$(
+  client: Client,
+  stats: Stats,
+  useCreate: boolean = false
+): Writable {
+  const bulk = bulkIndex(stats, client, useCreate);
+
+  return new Writable({
+    highWaterMark: parseInt((process.env.HIGH_WATER_MARK as string) ?? 5000, 10),
+    objectMode: true,
+    async write(record, enc, callback): Promise<void> {
+      try {
+        await bulk([record.value]);
+        callback(null);
+      } catch (err) {
+        callback(err);
+      }
+    },
+    async writev(chunks, callback): Promise<void> {
+      try {
+        await bulk(chunks.map(({ chunk: record }) => record.value));
+        callback(null);
+      } catch (err) {
+        callback(err);
+      }
+    },
+  });
+}
+
+function bulkIndex(stats: Stats, client: Client, useCreate: boolean = false) {
+  return async (jsonStanzasWithinArchive: any[]): Promise<void> => {
+    const indexOrCreate: BulkOperation = useCreate ? BulkOperation.Create : BulkOperation.Index;
+    const ops: WeakMap<any, any> = new WeakMap<any, any>();
+    const errors: string[] = [];
+
+    await client.helpers.bulk(
+      {
+        retries: 5,
+        concurrency: concurrencyMaxMinus1(),
+        datasource: jsonStanzasWithinArchive.map(
+          flow(eagerUpdate(stats), buildRequest(indexOrCreate)(ops))
+        ),
+        onDocument(doc) {
+          return ops.get(doc);
+        },
+        onDrop(dropped): void {
+          errors.push(`
+Bulk doc failure [operation=${indexOrCreate}]:\n  doc: ${JSON.stringify(
+            dropped.document
+          )}\n  error: ${JSON.stringify(dropped.error)}`);
+        },
+      },
+      {
+        headers: ES_CLIENT_HEADERS,
+      }
+    );
+
+    if (errors.length) throw new AggregateError(errors);
+  };
 }
