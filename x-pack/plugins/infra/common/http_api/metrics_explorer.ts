f1213c53cb46f725e8b820e983f14ecb04f357c0
Maryam Saeidi <maryam.saeidi@elastic.co>
2023-07-26 16:53:39 +0200

    Make timeFieldName optional

--- x-pack/plugins/infra/common/http_api/metrics_explorer.ts
+++ x-pack/plugins/infra/common/http_api/metrics_explorer.ts
@@ -74,12 +74,16 @@ export const metricsExplorerMetricRT = rt.intersection([
   metricsExplorerMetricOptionalFieldsRT,
 ]);
 
-export const timeRangeRT = rt.type({
-  from: rt.number,
-  to: rt.number,
-  interval: rt.string,
-  timeFieldName: rt.union([rt.string, rt.undefined]),
-});
+export const timeRangeRT = rt.intersection([
+  rt.type({
+    from: rt.number,
+    to: rt.number,
+    interval: rt.string,
+  }),
+  rt.partial({
+    timeFieldName: rt.string,
+  }),
+]);
 
 export const metricsExplorerRequestBodyRequiredFieldsRT = rt.type({
   timerange: timeRangeRT,
