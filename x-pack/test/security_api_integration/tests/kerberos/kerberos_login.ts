edb9561daa926ac299a9f1748fa14d676b44c567
Tiago Costa <tiago.costa@elastic.co>
2023-07-26 16:51:16 +0100

    skip failing es promotion suite (#162581)

--- x-pack/test/security_api_integration/tests/kerberos/kerberos_login.ts
+++ x-pack/test/security_api_integration/tests/kerberos/kerberos_login.ts
@@ -381,7 +381,8 @@ export default function ({ getService }: FtrProviderContext) {
         expect(nonAjaxResponse.headers['www-authenticate']).to.be(undefined);
       });
 
-      describe('post-authentication stage', () => {
+      // FLAKY: https://github.com/elastic/kibana/issues/162581
+      describe.skip('post-authentication stage', () => {
         for (const client of ['start-contract', 'request-context', 'custom']) {
           it(`expired access token should be automatically refreshed by the ${client} client`, async function () {
             this.timeout(60000);
