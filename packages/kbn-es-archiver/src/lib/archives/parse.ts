f1445407ab0f0e54ba8b0e42c6caa074d987ca09
Tre' Seymour <wayne.seymour@elastic.co>
2023-07-26 11:47:26 +0100

    [Investigation][Es Archiver] Fixed HighWaterMark for Archives Impl
    Resolves: https://github.com/elastic/appex-qa-team/issues/31

--- packages/kbn-es-archiver/src/lib/archives/parse.ts
+++ packages/kbn-es-archiver/src/lib/archives/parse.ts
@@ -14,15 +14,19 @@ import {
   createReplaceStream,
   createMapStream,
 } from '@kbn/utils';
+import { flow } from 'fp-ts/function';
 
 import { RECORD_SEPARATOR } from './constants';
+
+const anyCharButNotWhiteSpaces = (): RegExp => /[^\s]/;
+const bool = (x: RegExpMatchArray | null) => Boolean(x);
 
 export function createParseArchiveStreams({ gzip = false } = {}) {
   return [
     gzip ? createGunzip() : new PassThrough(),
     createReplaceStream('\r\n', '\n'),
     createSplitStream(RECORD_SEPARATOR),
-    createFilterStream<string>((l) => !!l.match(/[^\s]/)),
-    createMapStream<string>((json) => JSON.parse(json.trim())),
+    createFilterStream<string>(flow((x) => x.match(anyCharButNotWhiteSpaces()), bool)),
+    createMapStream<string>((json: string) => JSON.parse(json.trim())),
   ];
 }
--- packages/kbn-es-archiver/src/lib/docs/index.ts
+++ packages/kbn-es-archiver/src/lib/docs/index.ts
@@ -6,5 +6,5 @@
  * Side Public License, v 1.
  */
 
-export { createIndexDocRecordsStream } from './index_doc_records_stream';
+export { createIndexDocRecordsStream, indexDocRecordsWritable$ } from './index_doc_records_stream';
 export { createGenerateDocRecordsStream } from './generate_doc_records_stream';
