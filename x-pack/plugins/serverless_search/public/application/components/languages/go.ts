022d747d5a4b04d82d84d87c8756e15b837f32b1
Rodney Norris <rodney.norris@elastic.co>
2023-07-27 14:57:03 +0000

    refactor(serverless_search): update go snippets for serverless client

--- x-pack/plugins/serverless_search/public/application/components/languages/go.ts
+++ x-pack/plugins/serverless_search/public/application/components/languages/go.ts
@@ -12,36 +12,37 @@ import { LanguageDefinition, Languages } from './types';
 export const goDefinition: LanguageDefinition = {
   advancedConfig: docLinks.goAdvancedConfig,
   basicConfig: docLinks.goBasicConfig,
-  buildSearchQuery: `searchResp, err := es.Search(
-  es.Search.WithIndex("books"),
-  es.Search.WithPretty(),
-  es.Search.WithQuery("snow"),
-)
+  buildSearchQuery: `searchResp, err := es.Search().
+  Index("books").
+  Q("snow").
+  Do(context.Background())
 
 fmt.Println(searchResp, err)`,
   configureClient: ({ url, apiKey }) => `import (
+  "context"
   "fmt"
   "log"
   "strings"
-
-  "github.com/elastic/go-elasticsearch/v8"
+​
+  "github.com/elastic/elasticsearch-serverless-go"
 )
 
-cfg := elasticsearch.Config{
-  Addresses: []string{
-      "${url}",
-  },
-  APIKey: "${apiKey}",
-}
-es, err := elasticsearch.NewClient(cfg)
-if err != nil {
-  log.Fatalf("Error creating the client: %s", err)
+func main() {
+  cfg := elasticsearch.Config{
+    Address: "${url}",
+    APIKey: "${apiKey}",
+  }
+  es, err := elasticsearch.NewClient(cfg)
+  if err != nil {
+    log.Fatalf("Error creating the client: %s", err)
+  }
 }`,
   docLink: docLinks.goClient,
   iconType: 'go.svg',
   id: Languages.GO,
-  ingestData: `ingestResult, err := es.Bulk(
-  strings.NewReader(\`
+  ingestData: `ingestResult, err := es.Bulk().
+  Index("books").
+  Raw(strings.NewReader(\`
 {"index":{"_id":"9780553351927"}}
 {"name":"Snow Crash","author":"Neal Stephenson","release_date":"1992-06-01","page_count": 470}
 { "index": { "_id": "9780441017225"}}
@@ -53,45 +54,45 @@ if err != nil {
 { "index": { "_id": "9780060850524"}}
 {"name": "Brave New World", "author": "Aldous Huxley", "release_date": "1932-06-01", "page_count": 268}
 { "index": { "_id": "9780385490818"}}
-{"name": "The Handmaid's Tale", "author": "Margaret Atwood", "release_date": "1985-06-01", "page_count": 311}\n\`),
-  es.Bulk.WithIndex("books"),
-)
+{"name": "The Handmaid's Tale", "author": "Margaret Atwood", "release_date": "1985-06-01", "page_count": 311}\n\`)).
+  Do(context.Background())
+
 fmt.Println(ingestResult, err)`,
   ingestDataIndex: ({ apiKey, url, indexName }) => `import (
+  "context"
   "fmt"
   "log"
   "strings"
-
-  "github.com/elastic/go-elasticsearch/v8"
+​
+  "github.com/elastic/elasticsearch-serverless-go"
 )
-
-cfg := elasticsearch.Config{
-  Addresses: []string{
-      "${url}",
-  },
-  APIKey: "${apiKey}",
-}
-es, err := elasticsearch.NewClient(cfg)
-if err != nil {
-  log.Fatalf("Error creating the client: %s", err)
-}
-
-res, err := es.Bulk(
-	strings.NewReader(\`
+​
+func main() {
+  cfg := elasticsearch.Config{
+    Address: "${url}",
+    APIKey: "${apiKey}",
+  }
+  es, err := elasticsearch.NewClient(cfg)
+  if err != nil {
+    log.Fatalf("Error creating the client: %s", err)
+  }
+  res, err := es.Bulk().
+    Index("${indexName}").
+    Raw(strings.NewReader(\`
 { "index": { "_id": "1"}}
-{"name": "foo", "title": "bar"}\n\`),
-  es.Bulk.WithIndex("${indexName}"),
-)
-fmt.Println(res, err)`,
-  installClient: 'go get github.com/elastic/go-elasticsearch/v8@8.8',
+{"name": "foo", "title": "bar"}\n\`)).
+    Do(context.Background())
+  ​
+  fmt.Println(res, err)
+}`,
+  installClient: 'go get -u github.com/elastic/elasticsearch-serverless-go@latest',
   name: i18n.translate('xpack.serverlessSearch.languages.go', {
     defaultMessage: 'Go',
   }),
-  testConnection: `res, err := es.Info()
+  testConnection: `infores, err := es.Info().Do(context.Background())
 if err != nil {
   log.Fatalf("Error getting response: %s", err)
 }
 
-defer res.Body.Close()
-fmt.Println(res)`,
+fmt.Println(infores)`,
 };
