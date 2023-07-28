8a885d2cdb4c39a4ec239bd897d0efc3132e45fc
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-07-28 08:07:29 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/security_solution/public/common/components/alerts_treemap/lib/helpers.ts
+++ x-pack/plugins/security_solution/public/common/components/alerts_treemap/lib/helpers.ts
@@ -14,7 +14,6 @@ import type {
   XYChartElementEvent,
 } from '@elastic/charts';
 import { firstNonNullValue } from '../../../../../common/endpoint/models/ecs_safety_helpers';
-import type { DataQualityCheckedParams, TelemetryClientStart } from '../../../lib/telemetry';
 
 import type { RawBucket } from '../types';
 
