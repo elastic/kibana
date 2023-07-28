897681c8d70536d042f74c1bb71a1c261a3c744c
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-07-28 10:05:21 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/cases/public/api/index.test.ts
+++ x-pack/plugins/cases/public/api/index.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { CaseMetricsFeature } from '@kbn/cases-plugin/common/api';
+import { CaseMetricsFeature } from '../../common/api';
 import { httpServiceMock } from '@kbn/core/public/mocks';
 import { bulkGetCases, getCases, getCasesMetrics } from '.';
 import { allCases, allCasesSnake, casesSnake } from '../containers/mock';
