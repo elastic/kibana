49b8de920a991c9b187ad4fe5c9fb5319ccb18bc
Kevin Logan <kevin.logan@elastic.co>
2023-07-28 17:53:58 -0400

    update test

--- x-pack/plugins/security_solution/public/management/cypress/fixtures/artifacts_page.ts
+++ x-pack/plugins/security_solution/public/management/cypress/fixtures/artifacts_page.ts
@@ -196,6 +196,9 @@ export const getArtifactsListTestsData = (): ArtifactsFixtureType[] => [
           value: 'agent.type',
         },
         {
+          type: 'wait',
+        },
+        {
           type: 'click',
           customSelector: 'button[title="agent.type"]',
         },
