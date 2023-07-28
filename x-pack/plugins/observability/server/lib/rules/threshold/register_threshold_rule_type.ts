949d51a7ab72fdb08abf30706b894e02c3cdb251
Faisal Kanout <f.kanout@gmail.com>
2023-07-28 17:04:14 +0200

    revert changes

--- x-pack/plugins/observability/server/lib/rules/threshold/register_threshold_rule_type.ts
+++ x-pack/plugins/observability/server/lib/rules/threshold/register_threshold_rule_type.ts
@@ -121,7 +121,7 @@ export function thresholdRuleType(
   return {
     id: OBSERVABILITY_THRESHOLD_RULE_TYPE_ID,
     name: i18n.translate('xpack.observability.threshold.ruleName', {
-      defaultMessage: 'Threshold (BETA)',
+      defaultMessage: 'Threshold (Technical Preview)',
     }),
     validate: {
       params: schema.object(
