d55754e3d034196b2b89c427274d9be4bb05b24a
Paul Tavares <paul.tavares@elastic.co>
2023-07-28 11:49:36 -0400

    Added tests to Essentials to validate no access to response actions

--- x-pack/test_serverless/functional/test_suites/security/cypress/e2e/endpoint_management/feature_access/essentials.cy.ts
+++ x-pack/test_serverless/functional/test_suites/security/cypress/e2e/endpoint_management/feature_access/essentials.cy.ts
@@ -5,9 +5,11 @@
  * 2.0.
  */
 
+import { RESPONSE_ACTION_API_COMMANDS_NAMES } from '@kbn/security-solution-plugin/common/endpoint/service/response_actions/constants';
 import { login } from '../../../tasks/login';
 import { getNoPrivilegesPage } from '../../../screens/endpoint_management/common';
 import { getEndpointManagementPageList } from '../../../screens/endpoint_management';
+import { ensureResponseActionAuthzAccess } from '../../../tasks/endpoint_management';
 
 describe(
   'App Features for Essential PLI',
@@ -20,15 +22,26 @@ describe(
   },
   () => {
     const pages = getEndpointManagementPageList();
+    let username: string;
+    let password: string;
 
     beforeEach(() => {
-      login();
+      login('endpoint_operations_analyst').then((response) => {
+        username = response.username;
+        password = response.password;
+      });
     });
 
     for (const { url, title } of pages) {
       it(`should not allow access to ${title}`, () => {
         cy.visit(url);
         getNoPrivilegesPage().should('exist');
+      });
+    }
+
+    for (const actionName of RESPONSE_ACTION_API_COMMANDS_NAMES) {
+      it(`should not allow access to Response Action: ${actionName}`, () => {
+        ensureResponseActionAuthzAccess('none', actionName, username, password);
       });
     }
   }
