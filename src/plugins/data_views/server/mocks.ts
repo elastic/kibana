c638dea669afa60cac8ad365c41017a69a1d6c29
Matt Kime <matt@mattki.me>
2023-07-30 00:21:52 -0500

    better mock

--- src/plugins/data_views/server/mocks.ts
+++ src/plugins/data_views/server/mocks.ts
@@ -12,6 +12,7 @@ export function createIndexPatternsStartMock() {
   const dataViewsServiceFactory = jest.fn().mockResolvedValue({ get: jest.fn() });
   return {
     dataViewsServiceFactory,
+    isRollupsEnabled: jest.fn().mockReturnValue(true),
   };
 }
 
