e1ed39e776fb2298056a8b9ca56bb9115ee88050
Khristinin Nikita <nikita.khristinin@elastic.co>
2023-07-31 17:00:22 +0200

    Try to enable feature in cypress tests

--- x-pack/plugins/security_solution/cypress/e2e/entity_analytics/entity_analytics_management_page.cy.ts
+++ x-pack/plugins/security_solution/cypress/e2e/entity_analytics/entity_analytics_management_page.cy.ts
@@ -24,71 +24,75 @@ import { createRule } from '../../tasks/api_calls/rules';
 import { updateDateRangeInLocalDatePickers } from '../../tasks/date_picker';
 import { fillLocalSearchBar, submitLocalSearch } from '../../tasks/search_bar';
 
-describe('Entity analytics management page', () => {
-  before(() => {
-    cleanKibana();
-    cy.task('esArchiverLoad', 'all_users');
-  });
-
-  beforeEach(() => {
-    login();
-    visitWithoutDateRange(ALERTS_URL);
-    createRule(getNewRule({ query: 'user.name:* or host.name:*', risk_score: 70 }));
-    visit(ENTITY_ANALYTICS_MANAGEMENT_URL);
-  });
-
-  after(() => {
-    cy.task('esArchiverUnload', 'all_users');
-  });
-
-  it('renders page as expected', () => {
-    cy.get(PAGE_TITLE).should('have.text', 'Entity Risk Score');
-  });
-
-  describe('Risk preview', () => {
-    it('risk scores reacts on change in datepicker', () => {
-      const START_DATE = 'Jan 18, 2019 @ 20:33:29.186';
-      const END_DATE = 'Jan 19, 2019 @ 20:33:29.186';
-
-      cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
-      cy.get(USER_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
-
-      updateDateRangeInLocalDatePickers(LOCAL_QUERY_BAR_SELECTOR, START_DATE, END_DATE);
-
-      cy.get(HOST_RISK_PREVIEW_TABLE).contains('No items found');
-      cy.get(USER_RISK_PREVIEW_TABLE).contains('No items found');
+describe(
+  'Entity analytics management page',
+  { env: { ftrConfig: { enableExperimental: ['riskScoringRoutesEnabled'] } } },
+  () => {
+    before(() => {
+      cleanKibana();
+      cy.task('esArchiverLoad', 'all_users');
     });
 
-    it('risk scores reacts on change in search bar query', () => {
-      cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
-      cy.get(USER_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
-
-      fillLocalSearchBar('host.name: "test-host1"');
-      submitLocalSearch(LOCAL_QUERY_BAR_SELECTOR);
-
-      cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).should('have.length', 1);
-      cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).contains('test-host1');
-      cy.get(USER_RISK_PREVIEW_TABLE_ROWS).should('have.length', 1);
-      cy.get(USER_RISK_PREVIEW_TABLE_ROWS).contains('test1');
+    beforeEach(() => {
+      login();
+      visitWithoutDateRange(ALERTS_URL);
+      createRule(getNewRule({ query: 'user.name:* or host.name:*', risk_score: 70 }));
+      visit(ENTITY_ANALYTICS_MANAGEMENT_URL);
     });
 
-    it('show error panel if API returns error and then try to refetch data', () => {
-      cy.intercept('POST', '/internal/risk_score/preview', {
-        statusCode: 500,
+    after(() => {
+      cy.task('esArchiverUnload', 'all_users');
+    });
+
+    it('renders page as expected', () => {
+      cy.get(PAGE_TITLE).should('have.text', 'Entity Risk Score');
+    });
+
+    describe('Risk preview', () => {
+      it('risk scores reacts on change in datepicker', () => {
+        const START_DATE = 'Jan 18, 2019 @ 20:33:29.186';
+        const END_DATE = 'Jan 19, 2019 @ 20:33:29.186';
+
+        cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
+        cy.get(USER_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
+
+        updateDateRangeInLocalDatePickers(LOCAL_QUERY_BAR_SELECTOR, START_DATE, END_DATE);
+
+        cy.get(HOST_RISK_PREVIEW_TABLE).contains('No items found');
+        cy.get(USER_RISK_PREVIEW_TABLE).contains('No items found');
       });
 
-      cy.get(RISK_PREVIEW_ERROR).contains('Preview failed');
+      it('risk scores reacts on change in search bar query', () => {
+        cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
+        cy.get(USER_RISK_PREVIEW_TABLE_ROWS).should('have.length', 5);
 
-      cy.intercept('POST', '/internal/risk_score/preview', {
-        statusCode: 200,
-        body: {
-          scores: { host: [], user: [] },
-        },
+        fillLocalSearchBar('host.name: "test-host1"');
+        submitLocalSearch(LOCAL_QUERY_BAR_SELECTOR);
+
+        cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).should('have.length', 1);
+        cy.get(HOST_RISK_PREVIEW_TABLE_ROWS).contains('test-host1');
+        cy.get(USER_RISK_PREVIEW_TABLE_ROWS).should('have.length', 1);
+        cy.get(USER_RISK_PREVIEW_TABLE_ROWS).contains('test1');
       });
 
-      cy.get(RISK_PREVIEW_ERROR_BUTTON).click();
+      it('show error panel if API returns error and then try to refetch data', () => {
+        cy.intercept('POST', '/internal/risk_score/preview', {
+          statusCode: 500,
+        });
 
-      cy.get(RISK_PREVIEW_ERROR).should('not.exist');
+        cy.get(RISK_PREVIEW_ERROR).contains('Preview failed');
+
+        cy.intercept('POST', '/internal/risk_score/preview', {
+          statusCode: 200,
+          body: {
+            scores: { host: [], user: [] },
+          },
+        });
+
+        cy.get(RISK_PREVIEW_ERROR_BUTTON).click();
+
+        cy.get(RISK_PREVIEW_ERROR).should('not.exist');
+      });
     });
-  });
-});
+  }
+);
