7543bf86ae126ac15b3ce2127d3782722ac98dc6
Ido Cohen <90558359+CohenIdo@users.noreply.github.com>
2023-07-31 13:20:38 +0300

    clear code

--- x-pack/plugins/security_solution_serverless/server/task_manager/usage_reporting_task.ts
+++ x-pack/plugins/security_solution_serverless/server/task_manager/usage_reporting_task.ts
@@ -72,7 +72,6 @@ export class SecurityUsageReportingTask {
     if (!taskManager) {
       return;
     }
-    taskManager.bulkUpdateSchedules([this.taskId], { interval: '30s' });
 
     this.wasStarted = true;
 
