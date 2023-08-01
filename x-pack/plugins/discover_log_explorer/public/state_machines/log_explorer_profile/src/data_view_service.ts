5974cd58a4ff5d7e073df5c4884dd2dc12da463c
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2023-08-01 14:57:29 +0100

    Refactor code

--- x-pack/plugins/discover_log_explorer/public/state_machines/log_explorer_profile/src/data_view_service.ts
+++ x-pack/plugins/discover_log_explorer/public/state_machines/log_explorer_profile/src/data_view_service.ts
@@ -21,8 +21,6 @@ export const createAndSetDataView =
     LogExplorerProfileEvent
   > =>
   async (context) => {
-    if (!('datasetSelection' in context)) return;
-
     const dataView = await stateContainer.actions.createAndAppendAdHocDataView(
       context.datasetSelection.toDataviewSpec()
     );
