5ce30958419862e86c53a8370cc0c7b53f026b05
Tiago Costa <tiago.costa@elastic.co>
2023-08-01 19:50:11 +0100

    chore(NA): get back function refactor

--- packages/kbn-repo-packages/modern/plugins.js
+++ packages/kbn-repo-packages/modern/plugins.js
@@ -107,16 +107,7 @@ function getPluginPackagesFilter(selector = {}) {
  */
 function getDistributablePacakgesFilter() {
   return (pkg) => {
-    // if (pkg.isDevOnly()) {
-    //   return false;
-    // }
-
-    if (
-      // @ts-expect-error upgrade ts
-      pkg.isDevOnly ||
-      pkg.manifest.type === 'functional-tests' ||
-      pkg.manifest.type === 'test-helper'
-    ) {
+    if (pkg.isDevOnly()) {
       return false;
     }
 
