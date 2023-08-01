7a4d410a6d9fb335c53842a6ab783578a68182d4
Tiago Costa <tiago.costa@elastic.co>
2023-08-01 15:48:38 +0100

    skip flaky suite (#156745)

--- x-pack/plugins/cases/public/components/user_actions/index.test.tsx
+++ x-pack/plugins/cases/public/components/user_actions/index.test.tsx
@@ -377,6 +377,7 @@ describe(`UserActions`, () => {
   // FLAKY: https://github.com/elastic/kibana/issues/156748
   // FLAKY: https://github.com/elastic/kibana/issues/156747
   // FLAKY: https://github.com/elastic/kibana/issues/156746
+  // FLAKY: https://github.com/elastic/kibana/issues/156745
   describe.skip('pagination', () => {
     beforeEach(() => {
       jest.clearAllMocks();
