31d34d58b24e38b8486b3654b0dce72e9b419124
Hannah Mudge <hannah.wright@elastic.co>
2023-08-02 16:59:59 -0600

    Adding levels + adjust min/max as necessary based on selections

--- src/plugins/controls/public/range_slider/components/range_slider_control.tsx
+++ src/plugins/controls/public/range_slider/components/range_slider_control.tsx
@@ -54,10 +54,6 @@ export const RangeSliderControl: FC = () => {
     [rangeSlider.dispatch]
   );
 
-  useEffect(() => {
-    debouncedOnChange(displayedValue);
-  }, [debouncedOnChange, displayedValue]);
-
   // derive field formatter from fieldSpec and dataViewId
   useEffect(() => {
     (async () => {
@@ -72,20 +68,46 @@ export const RangeSliderControl: FC = () => {
     })();
   }, [fieldSpec, dataViewId, getDataViewById]);
 
+  const recalculateMinMax = useCallback(
+    (selectedValue) => {
+      if (min === undefined || max === undefined) return;
+
+      console.log('recalculateMinMax');
+      const [selectedMin, selectedMax] = [
+        selectedValue[0] === '' ? min : parseFloat(selectedValue[0]),
+        selectedValue[1] === '' ? max : parseFloat(selectedValue[1]),
+      ];
+      setDisplayedMin(Math.min(selectedMin, min));
+      setDisplayedMax(Math.max(selectedMax, max));
+    },
+    [min, max]
+  );
+
   /**
-   * The following `useEffects` ensure that the changes to min, max, and value that come from the embeddable (for example,
+   * The following `useEffect` ensures that the changes to min, max, and value that come from the embeddable (for example,
    * from the `reset` button on the dashboard or via chaining) are reflected in the displayed value
    */
-  useEffect(() => setDisplayedValue(value ?? ['', '']), [value]);
-  useEffect(() => setDisplayedMin(min ?? -Infinity), [min]);
-  useEffect(() => setDisplayedMax(max ?? Infinity), [max]);
+  useEffect(() => {
+    setDisplayedValue(value ?? ['', '']);
+    recalculateMinMax(value ?? ['', '']);
+  }, [value, recalculateMinMax]);
 
   const ticks: EuiRangeTick[] = useMemo(() => {
     return [
-      { value: displayedMin, label: fieldFormatter(String(displayedMin)) },
-      { value: displayedMax, label: fieldFormatter(String(displayedMax)) },
+      { value: min ?? -Infinity, label: fieldFormatter(String(min)) },
+      { value: max ?? Infinity, label: fieldFormatter(String(max)) },
     ];
-  }, [displayedMin, displayedMax, fieldFormatter]);
+  }, [min, max, fieldFormatter]);
+
+  const levels = useMemo(() => {
+    return [
+      {
+        min: min ?? -Infinity,
+        max: max ?? Infinity,
+        color: 'success',
+      },
+    ];
+  }, [min, max]);
 
   const disablePopover = useMemo(
     () =>
@@ -126,26 +148,33 @@ export const RangeSliderControl: FC = () => {
         id={id}
         fullWidth
         showTicks
+        // isDraggable
         ticks={ticks}
+        levels={levels}
         min={displayedMin}
         max={displayedMax}
         isLoading={isLoading}
+        onMouseUp={() => {
+          debouncedOnChange.cancel();
+          rangeSlider.dispatch.setSelectedRange(displayedValue);
+        }}
         readOnly={disablePopover}
         showInput={'inputWithPopover'}
         data-test-subj="rangeSlider__slider"
         minInputProps={getCommonInputProps({
           inputValue: displayedValue[0],
           testSubj: 'lowerBoundFieldNumber',
-          placeholder: String(displayedMin),
+          placeholder: String(min),
         })}
         maxInputProps={getCommonInputProps({
           inputValue: displayedValue[1],
           testSubj: 'upperBoundFieldNumber',
-          placeholder: String(displayedMax),
+          placeholder: String(max),
         })}
         value={[displayedValue[0] || displayedMin, displayedValue[1] || displayedMax]}
         onChange={([minSelection, maxSelection]: [number | string, number | string]) => {
           setDisplayedValue([String(minSelection), String(maxSelection)]);
+          debouncedOnChange([String(minSelection), String(maxSelection)]);
         }}
       />
     </span>
