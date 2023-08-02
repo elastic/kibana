e5421ada9e4de0f30b602ea5dc78efce82d96011
miriam.aparicio <miriam.aparicio@gmail.com>
2023-08-02 12:51:14 +0100

    rename and reuse functions for filters and charts

--- x-pack/plugins/apm/server/routes/mobile/get_mobile_filters.ts
+++ x-pack/plugins/apm/server/routes/mobile/get_mobile_filters.ts
@@ -6,30 +6,15 @@
  */
 
 import {
-  termQuery,
-  kqlQuery,
-  rangeQuery,
-} from '@kbn/observability-plugin/server';
-import { ProcessorEvent } from '@kbn/observability-plugin/common';
-import {
-  DEVICE_MODEL_IDENTIFIER,
-  HOST_OS_VERSION,
-  NETWORK_CONNECTION_TYPE,
-  SERVICE_NAME,
-  SERVICE_VERSION,
-  TRANSACTION_TYPE,
-} from '../../../common/es_fields/apm';
-import { environmentQuery } from '../../../common/utils/environment_query';
+  MobileProperty,
+  MobilePropertyType,
+} from '../../../common/mobile_types';
 import { APMEventClient } from '../../lib/helpers/create_es_client/create_apm_event_client';
-
-type MobileFiltersTypes =
-  | 'device'
-  | 'appVersion'
-  | 'osVersion'
-  | 'netConnectionType';
+import { getDeviceOSApp } from './get_device_os_app';
+import { getNCT } from './get_nct';
 
 export type MobileFiltersResponse = Array<{
-  key: MobileFiltersTypes;
+  key: MobilePropertyType;
   options: string[];
 }>;
 
@@ -50,134 +35,51 @@ export async function getMobileFilters({
   start: number;
   end: number;
 }): Promise<MobileFiltersResponse> {
-  async function getMobileTransactionEventsFilters(): Promise<
-    Array<{
-      key: MobileFiltersTypes;
-      options: string[];
-    }>
-  > {
-    const response = await apmEventClient.search(
-      'get_mobile_transaction_events_filters',
-      {
-        apm: {
-          events: [ProcessorEvent.transaction],
-        },
-        body: {
-          track_total_hits: false,
-          size: 0,
-          query: {
-            bool: {
-              filter: [
-                ...termQuery(SERVICE_NAME, serviceName),
-                ...termQuery(TRANSACTION_TYPE, transactionType),
-                ...rangeQuery(start, end),
-                ...environmentQuery(environment),
-                ...kqlQuery(kuery),
-              ],
-            },
-          },
-          aggs: {
-            devices: {
-              terms: {
-                field: DEVICE_MODEL_IDENTIFIER,
-                size: 10,
-              },
-            },
-            osVersions: {
-              terms: {
-                field: HOST_OS_VERSION,
-                size: 10,
-              },
-            },
-            appVersions: {
-              terms: {
-                field: SERVICE_VERSION,
-                size: 10,
-              },
-            },
-          },
-        },
-      }
-    );
+  const MAX_ITEMS = 10;
+  const commonProps = {
+    kuery,
+    apmEventClient,
+    serviceName,
+    transactionType,
+    environment,
+    start,
+    end,
+    size: MAX_ITEMS,
+  };
+  const mobileTransactionEventsFiltersResponse = await getDeviceOSApp(
+    commonProps
+  );
 
-    return [
-      {
-        key: 'device',
-        options:
-          response.aggregations?.devices?.buckets?.map(
-            ({ key }) => key as string
-          ) || [],
-      },
-      {
-        key: 'osVersion',
-        options:
-          response.aggregations?.osVersions?.buckets?.map(
-            ({ key }) => key as string
-          ) || [],
-      },
-      {
-        key: 'appVersion',
-        options:
-          response.aggregations?.appVersions?.buckets?.map(
-            ({ key }) => key as string
-          ) || [],
-      },
-    ];
-  }
+  const mobileNetworkConnectionTypeFiltersResponse = await getNCT(commonProps);
 
-  async function getMobileNetworkConnectionTypeFilters(): Promise<
-    Array<{
-      key: MobileFiltersTypes;
-      options: string[];
-    }>
-  > {
-    const response = await apmEventClient.search(
-      'get_mobile_network_connection_type_filters',
-      {
-        apm: {
-          events: [ProcessorEvent.span],
-        },
-        body: {
-          track_total_hits: false,
-          size: 0,
-          query: {
-            bool: {
-              filter: [
-                ...termQuery(SERVICE_NAME, serviceName),
-                ...rangeQuery(start, end),
-                ...environmentQuery(environment),
-                ...kqlQuery(kuery),
-              ],
-            },
-          },
-          aggs: {
-            netConnectionTypes: {
-              terms: {
-                field: NETWORK_CONNECTION_TYPE,
-                size: 10,
-              },
-            },
-          },
-        },
-      }
-    );
-
-    return [
-      {
-        key: 'netConnectionType',
-        options:
-          response.aggregations?.netConnectionTypes?.buckets?.map(
-            ({ key }) => key as string
-          ) || [],
-      },
-    ];
-  }
-
-  const [transactionEventFilters, networkConnectionTypeFilters] =
-    await Promise.all([
-      getMobileTransactionEventsFilters(),
-      getMobileNetworkConnectionTypeFilters(),
-    ]);
-
-  return [...transactionEventFilters, ...networkConnectionTypeFilters];
+  return [
+    {
+      key: MobileProperty.Device,
+      options:
+        mobileTransactionEventsFiltersResponse.aggregations?.devices?.buckets?.map(
+          ({ key }) => key as string
+        ) || [],
+    },
+    {
+      key: MobileProperty.OsVersion,
+      options:
+        mobileTransactionEventsFiltersResponse.aggregations?.osVersions?.buckets?.map(
+          ({ key }) => key as string
+        ) || [],
+    },
+    {
+      key: MobileProperty.AppVersion,
+      options:
+        mobileTransactionEventsFiltersResponse.aggregations?.appVersions?.buckets?.map(
+          ({ key }) => key as string
+        ) || [],
+    },
+    {
+      key: MobileProperty.NetworkConnectionType,
+      options:
+        mobileNetworkConnectionTypeFiltersResponse.aggregations?.netConnectionTypes?.buckets?.map(
+          ({ key }) => key as string
+        ) || [],
+    },
+  ];
 }
