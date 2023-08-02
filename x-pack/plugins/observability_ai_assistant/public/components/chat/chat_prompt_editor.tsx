fcf22d4dd27abbc635dd763f3b57c4e4ecd16156
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-08-02 19:11:11 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/observability_ai_assistant/public/components/chat/chat_prompt_editor.tsx
+++ x-pack/plugins/observability_ai_assistant/public/components/chat/chat_prompt_editor.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { Ref, useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import {
   EuiButtonIcon,
   EuiButtonEmpty,
