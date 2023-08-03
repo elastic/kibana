77cb5dfe3ee3e8b654905ce00cb69a70997fb8ab
maxcold <mxcold@gmail.com>
2023-08-03 15:24:02 +0200

    add more tests to ComplianceDashboard component, testing different no data states

--- x-pack/plugins/cloud_security_posture/public/pages/compliance_dashboard/compliance_dashboard.test.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/compliance_dashboard/compliance_dashboard.test.tsx
@@ -20,7 +20,7 @@ import {
   DASHBOARD_CONTAINER,
   KUBERNETES_DASHBOARD_CONTAINER,
   KUBERNETES_DASHBOARD_TAB,
-  CLOUD_DASHBOARD_TAB,
+  CLOUD_DASHBOARD_TAB, CLOUD_POSTURE_DASHBOARD_PAGE_HEADER,
 } from './test_subjects';
 import { mockDashboardData } from './mock';
 import { createReactQueryResponse } from '../../test/fixtures/react_query';
@@ -28,7 +28,7 @@ import { NO_FINDINGS_STATUS_TEST_SUBJ } from '../../components/test_subjects';
 import { expectIdsInDoc } from '../../test/utils';
 import {
   CSPM_INTEGRATION_NOT_INSTALLED_TEST_SUBJECT,
-  KSPM_INTEGRATION_NOT_INSTALLED_TEST_SUBJECT,
+  KSPM_INTEGRATION_NOT_INSTALLED_TEST_SUBJECT, PACKAGE_NOT_INSTALLED_TEST_SUBJECT,
 } from '../../components/cloud_posture_page';
 import { BaseCspSetupStatus, ComplianceDashboardData, CspStatusCode } from '../../../common/types';
 
@@ -96,6 +96,47 @@ describe('<ComplianceDashboard />', () => {
     );
   };
 
+  it('shows package not installed page instead of tabs', () => {
+    (useCspSetupStatusApi as jest.Mock).mockImplementation(() =>
+      createReactQueryResponse({
+        status: 'success',
+        data: {
+          kspm: { status: 'not-installed', healthyAgents: 0, installedPackagePolicies: 0 },
+          cspm: { status: 'not-installed', healthyAgents: 0, installedPackagePolicies: 0 },
+          isPluginInitialized: false,
+          indicesDetails: [
+            { index: 'logs-cloud_security_posture.findings_latest-default', status: 'empty' },
+            { index: 'logs-cloud_security_posture.findings-default*', status: 'empty' },
+          ],
+        },
+      })
+    );
+    (useKspmStatsApi as jest.Mock).mockImplementation(() => ({
+      isSuccess: true,
+      isLoading: false,
+      data: { stats: { totalFindings: 0 } },
+    }));
+    (useCspmStatsApi as jest.Mock).mockImplementation(() => ({
+      isSuccess: true,
+      isLoading: false,
+      data: { stats: { totalFindings: 0 } },
+    }));
+
+    renderComplianceDashboardPage();
+
+    expectIdsInDoc({
+      be: [PACKAGE_NOT_INSTALLED_TEST_SUBJECT, CLOUD_POSTURE_DASHBOARD_PAGE_HEADER],
+      notToBe: [
+        NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED,
+        NO_FINDINGS_STATUS_TEST_SUBJ.INDEXING,
+        NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
+        NO_FINDINGS_STATUS_TEST_SUBJ.UNPRIVILEGED,
+        CLOUD_DASHBOARD_CONTAINER,
+        KUBERNETES_DASHBOARD_CONTAINER
+      ],
+    });
+  });
+
   it('no findings state: not-deployed - shows NotDeployed instead of dashboard', () => {
     (useCspSetupStatusApi as jest.Mock).mockImplementation(() =>
       createReactQueryResponse({
@@ -125,11 +166,13 @@ describe('<ComplianceDashboard />', () => {
     renderComplianceDashboardPage();
 
     expectIdsInDoc({
-      be: [NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED],
+      be: [NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED, CLOUD_DASHBOARD_TAB],
       notToBe: [
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEXING,
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
         NO_FINDINGS_STATUS_TEST_SUBJ.UNPRIVILEGED,
+        CLOUD_DASHBOARD_CONTAINER,
+        KUBERNETES_DASHBOARD_CONTAINER,
       ],
     });
   });
@@ -163,11 +206,53 @@ describe('<ComplianceDashboard />', () => {
     renderComplianceDashboardPage();
 
     expectIdsInDoc({
-      be: [NO_FINDINGS_STATUS_TEST_SUBJ.INDEXING],
+      be: [NO_FINDINGS_STATUS_TEST_SUBJ.INDEXING, CLOUD_DASHBOARD_TAB],
       notToBe: [
         NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED,
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
         NO_FINDINGS_STATUS_TEST_SUBJ.UNPRIVILEGED,
+        CLOUD_DASHBOARD_CONTAINER,
+        KUBERNETES_DASHBOARD_CONTAINER,
+      ],
+    });
+  });
+
+  it('no findings state: indexing - shows Indexing instead of dashboard when waiting_for_results', () => {
+    (useCspSetupStatusApi as jest.Mock).mockImplementation(() =>
+      createReactQueryResponse({
+        status: 'success',
+        data: {
+          kspm: { status: 'waiting_for_results', healthyAgents: 1, installedPackagePolicies: 1 },
+          cspm: { status: 'waiting_for_results', healthyAgents: 1, installedPackagePolicies: 1 },
+          installedPackageVersion: '1.2.13',
+          indicesDetails: [
+            { index: 'logs-cloud_security_posture.findings_latest-default', status: 'empty' },
+            { index: 'logs-cloud_security_posture.findings-default*', status: 'empty' },
+          ],
+        },
+      })
+    );
+    (useKspmStatsApi as jest.Mock).mockImplementation(() => ({
+      isSuccess: true,
+      isLoading: false,
+      data: { stats: { totalFindings: 1 } },
+    }));
+    (useCspmStatsApi as jest.Mock).mockImplementation(() => ({
+      isSuccess: true,
+      isLoading: false,
+      data: { stats: { totalFindings: 1 } },
+    }));
+
+    renderComplianceDashboardPage();
+
+    expectIdsInDoc({
+      be: [NO_FINDINGS_STATUS_TEST_SUBJ.INDEXING, CLOUD_DASHBOARD_TAB],
+      notToBe: [
+        NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED,
+        NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
+        NO_FINDINGS_STATUS_TEST_SUBJ.UNPRIVILEGED,
+        CLOUD_DASHBOARD_CONTAINER,
+        KUBERNETES_DASHBOARD_CONTAINER,
       ],
     });
   });
@@ -201,11 +286,13 @@ describe('<ComplianceDashboard />', () => {
     renderComplianceDashboardPage();
 
     expectIdsInDoc({
-      be: [NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT],
+      be: [NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT, CLOUD_DASHBOARD_TAB],
       notToBe: [
         NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED,
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEXING,
         NO_FINDINGS_STATUS_TEST_SUBJ.UNPRIVILEGED,
+        CLOUD_DASHBOARD_CONTAINER,
+        KUBERNETES_DASHBOARD_CONTAINER,
       ],
     });
   });
@@ -239,11 +326,13 @@ describe('<ComplianceDashboard />', () => {
     renderComplianceDashboardPage();
 
     expectIdsInDoc({
-      be: [NO_FINDINGS_STATUS_TEST_SUBJ.UNPRIVILEGED],
+      be: [NO_FINDINGS_STATUS_TEST_SUBJ.UNPRIVILEGED, CLOUD_DASHBOARD_TAB],
       notToBe: [
         NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED,
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEXING,
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
+        CLOUD_DASHBOARD_CONTAINER,
+        KUBERNETES_DASHBOARD_CONTAINER,
       ],
     });
   });
@@ -277,7 +366,7 @@ describe('<ComplianceDashboard />', () => {
     renderComplianceDashboardPage();
 
     expectIdsInDoc({
-      be: [DASHBOARD_CONTAINER],
+      be: [CLOUD_DASHBOARD_TAB, DASHBOARD_CONTAINER, CLOUD_DASHBOARD_CONTAINER],
       notToBe: [
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
         NO_FINDINGS_STATUS_TEST_SUBJ.NO_AGENTS_DEPLOYED,
@@ -316,7 +405,7 @@ describe('<ComplianceDashboard />', () => {
     renderComplianceDashboardPage();
 
     expectIdsInDoc({
-      be: [KUBERNETES_DASHBOARD_CONTAINER],
+      be: [KUBERNETES_DASHBOARD_TAB, KUBERNETES_DASHBOARD_CONTAINER],
       notToBe: [
         CLOUD_DASHBOARD_CONTAINER,
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
@@ -355,7 +444,7 @@ describe('<ComplianceDashboard />', () => {
     renderComplianceDashboardPage();
 
     expectIdsInDoc({
-      be: [CLOUD_DASHBOARD_CONTAINER],
+      be: [CLOUD_DASHBOARD_TAB, CLOUD_DASHBOARD_CONTAINER],
       notToBe: [
         KUBERNETES_DASHBOARD_CONTAINER,
         NO_FINDINGS_STATUS_TEST_SUBJ.INDEX_TIMEOUT,
