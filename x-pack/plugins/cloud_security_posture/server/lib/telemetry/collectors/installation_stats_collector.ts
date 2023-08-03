d187ea92ab22b18bbe6ad03fe6cfe9aafceecf42
Jordan <yarden.shalom@elastic.co>
2023-08-03 14:34:19 +0300

    init

--- x-pack/plugins/cloud_security_posture/server/lib/telemetry/collectors/installation_stats_collector.ts
+++ x-pack/plugins/cloud_security_posture/server/lib/telemetry/collectors/installation_stats_collector.ts
@@ -17,6 +17,36 @@ import type { CloudSecurityInstallationStats } from './types';
 import type { CspServerPluginStart, CspServerPluginStartDeps } from '../../../types';
 import { CLOUD_SECURITY_POSTURE_PACKAGE_NAME } from '../../../../common/constants';
 
+const getInstalledPackagePolicies = (
+  packagePolicies: PackagePolicy[],
+  agentPolicies: AgentPolicy[],
+  logger: any
+) => {
+  const installationStats = packagePolicies.map(
+    (packagePolicy: PackagePolicy): CloudSecurityInstallationStats => {
+      const agentCounts =
+        agentPolicies?.find((agentPolicy) => agentPolicy?.id === packagePolicy.policy_id)?.agents ??
+        0;
+
+      console.log('packagePolicy ##################################');
+      logger(packagePolicy);
+      console.log(JSON.stringify(packagePolicy, null, 2));
+
+      return {
+        package_policy_id: packagePolicy.id,
+        feature: packagePolicy.vars?.posture?.value as string,
+        deployment_mode: packagePolicy.vars?.deployment?.value as string,
+        package_version: packagePolicy.package?.version as string,
+        created_at: packagePolicy.created_at,
+        agent_policy_id: packagePolicy.policy_id,
+        agent_count: agentCounts,
+      };
+    }
+  );
+
+  return installationStats;
+};
+
 export const getInstallationStats = async (
   esClient: ElasticsearchClient,
   soClient: SavedObjectsClientContract,
@@ -34,34 +64,11 @@ export const getInstallationStats = async (
     isPluginInitialized,
   };
 
-  const getInstalledPackagePolicies = async (
-    packagePolicies: PackagePolicy[],
-    agentPolicies: AgentPolicy[]
-  ) => {
-    const installationStats = await packagePolicies.map(
-      (packagePolicy: PackagePolicy): CloudSecurityInstallationStats => {
-        const agentCounts =
-          agentPolicies?.find((agentPolicy) => agentPolicy?.id === packagePolicy.policy_id)
-            ?.agents ?? 0;
-
-        return {
-          package_policy_id: packagePolicy.id,
-          feature: packagePolicy.vars?.posture?.value as string,
-          deployment_mode: packagePolicy.vars?.deployment?.value as string,
-          package_version: packagePolicy.package?.version as string,
-          created_at: packagePolicy.created_at,
-          agent_policy_id: packagePolicy.policy_id,
-          agent_count: agentCounts,
-        };
-      }
-    );
-    return installationStats;
-  };
-
   const packagePolicies = await cspContext.packagePolicyService.list(soClient, {
     perPage: SO_SEARCH_LIMIT,
     kuery: `${PACKAGE_POLICY_SAVED_OBJECT_TYPE}.package.name:"${CLOUD_SECURITY_POSTURE_PACKAGE_NAME}"`,
   });
+  if (!packagePolicies) return [];
 
   const agentPolicies = await agentPolicyService.list(soClient, {
     perPage: SO_SEARCH_LIMIT,
@@ -69,11 +76,11 @@ export const getInstallationStats = async (
     esClient,
     withAgentCount: true,
   });
-  if (!packagePolicies) return [];
 
-  const installationStats: CloudSecurityInstallationStats[] = await getInstalledPackagePolicies(
+  const installationStats: CloudSecurityInstallationStats[] = getInstalledPackagePolicies(
     packagePolicies.items,
-    agentPolicies?.items || []
+    agentPolicies?.items || [],
+    logger
   );
 
   return installationStats;
