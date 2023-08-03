e2bfd3d147c1ddda57560ba21e89f57b6e26d662
Ersin Erdal <ersin.erdal@elastic.co>
2023-08-03 16:34:10 +0300

    fix checkType

--- x-pack/plugins/stack_alerts/server/rule_types/es_query/rule_type.ts
+++ x-pack/plugins/stack_alerts/server/rule_types/es_query/rule_type.ts
@@ -146,7 +146,7 @@ export function getRuleType(
     }
   );
 
-  const alerts: IRuleTypeAlerts = {
+  const alerts: IRuleTypeAlerts<RuleAlertData> = {
     context: 'stack.esquery',
     mappings: {
       fieldMap: {
