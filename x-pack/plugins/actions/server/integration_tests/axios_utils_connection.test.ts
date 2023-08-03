be9b446ac4fca808d13f20cd8f4dbc1ae001331f
Zacqary Xeper <zacqary.xeper@elastic.co>
2023-08-03 07:38:37 -0400

    Add integration tests for SSL client auth

--- x-pack/plugins/actions/server/integration_tests/axios_utils_connection.test.ts
+++ x-pack/plugins/actions/server/integration_tests/axios_utils_connection.test.ts
@@ -27,16 +27,28 @@ import { resolveCustomHosts } from '../lib/custom_host_settings';
 const logger = loggingSystemMock.create().get() as jest.Mocked<Logger>;
 
 const CERT_DIR = '../../../../../../packages/kbn-dev-utils/certs';
+const MOCK_CERT_DIR = '../mock_certs';
 
 const KIBANA_CRT_FILE = pathResolve(__filename, pathJoin(CERT_DIR, 'kibana.crt'));
 const KIBANA_KEY_FILE = pathResolve(__filename, pathJoin(CERT_DIR, 'kibana.key'));
 const KIBANA_P12_FILE = pathResolve(__filename, pathJoin(CERT_DIR, 'kibana.p12'));
 const CA_FILE = pathResolve(__filename, pathJoin(CERT_DIR, 'ca.crt'));
 
+const UNAUTHORIZED_CA_FILE = pathResolve(
+  __filename,
+  pathJoin(MOCK_CERT_DIR, 'unauthorized_ca.crt')
+);
+const UNAUTHORIZED_CRT_FILE = pathResolve(__filename, pathJoin(MOCK_CERT_DIR, 'unauthorized.crt'));
+const UNAUTHORIZED_KEY_FILE = pathResolve(__filename, pathJoin(MOCK_CERT_DIR, 'unauthorized.key'));
+
 const KIBANA_KEY = fsReadFileSync(KIBANA_KEY_FILE, 'utf8');
 const KIBANA_CRT = fsReadFileSync(KIBANA_CRT_FILE, 'utf8');
 const KIBANA_P12 = fsReadFileSync(KIBANA_P12_FILE);
 const CA = fsReadFileSync(CA_FILE, 'utf8');
+
+const UNAUTHORIZED_KEY = fsReadFileSync(UNAUTHORIZED_KEY_FILE);
+const UNAUTHORIZED_CRT = fsReadFileSync(UNAUTHORIZED_CRT_FILE);
+const UNAUTHORIZED_CA = fsReadFileSync(UNAUTHORIZED_CA_FILE);
 
 const Auth = 'elastic:changeme';
 const AuthB64 = Buffer.from(Auth).toString('base64');
@@ -224,7 +236,7 @@ describe('axios connections', () => {
     });
 
     test('it works with cert, key, and ca in SSL overrides', async () => {
-      const { url, server } = await createServer({ useHttps: true });
+      const { url, server } = await createServer({ useHttps: true, requestCert: true });
       testServer = server;
 
       const configurationUtilities = getACUfromConfig();
@@ -238,7 +250,7 @@ describe('axios connections', () => {
     });
 
     test('it works with pfx and passphrase in SSL overrides', async () => {
-      const { url, server } = await createServer({ useHttps: true });
+      const { url, server } = await createServer({ useHttps: true, requestCert: true });
       testServer = server;
 
       const configurationUtilities = getACUfromConfig();
@@ -251,7 +263,7 @@ describe('axios connections', () => {
     });
 
     test('it fails with cert and key but no ca in SSL overrides', async () => {
-      const { url, server } = await createServer({ useHttps: true });
+      const { url, server } = await createServer({ useHttps: true, requestCert: true });
       testServer = server;
 
       const configurationUtilities = getACUfromConfig();
@@ -265,7 +277,7 @@ describe('axios connections', () => {
     });
 
     test('it fails with pfx but no passphrase in SSL overrides', async () => {
-      const { url, server } = await createServer({ useHttps: true });
+      const { url, server } = await createServer({ useHttps: true, requestCert: true });
       testServer = server;
 
       const configurationUtilities = getACUfromConfig();
@@ -275,6 +287,21 @@ describe('axios connections', () => {
       const fn = async () =>
         await request({ axios, url, logger, configurationUtilities, sslOverrides });
       await expect(fn()).rejects.toThrow('mac verify');
+    });
+
+    test('it fails with a certificate issued by an invalid ca', async () => {
+      const { url, server } = await createServer({ useHttps: true, requestCert: true });
+      testServer = server;
+
+      const configurationUtilities = getACUfromConfig();
+      const sslOverrides = {
+        ca: UNAUTHORIZED_CA,
+        cert: UNAUTHORIZED_CRT,
+        key: UNAUTHORIZED_KEY,
+      };
+      const fn = async () =>
+        await request({ axios, url, logger, configurationUtilities, sslOverrides });
+      await expect(fn()).rejects.toThrow('certificate');
     });
   });
 
@@ -526,11 +553,13 @@ function removePassword(url: string) {
 const TlsOptions = {
   cert: KIBANA_CRT,
   key: KIBANA_KEY,
+  ca: CA,
 };
 
 interface CreateServerOptions {
   useHttps: boolean;
   requireAuth?: boolean;
+  requestCert?: boolean;
 }
 
 interface CreateServerResult {
@@ -539,7 +568,7 @@ interface CreateServerResult {
 }
 
 async function createServer(options: CreateServerOptions): Promise<CreateServerResult> {
-  const { useHttps, requireAuth = false } = options;
+  const { useHttps, requireAuth = false, requestCert = false } = options;
   const port = await getPort();
   const url = `http${useHttps ? 's' : ''}://${requireAuth ? `${Auth}@` : ''}localhost:${port}`;
 
@@ -567,7 +596,7 @@ async function createServer(options: CreateServerOptions): Promise<CreateServerR
   if (!useHttps) {
     server = http.createServer(requestHandler);
   } else {
-    server = https.createServer(TlsOptions, requestHandler);
+    server = https.createServer({ ...TlsOptions, requestCert }, requestHandler);
   }
   server.unref();
 
