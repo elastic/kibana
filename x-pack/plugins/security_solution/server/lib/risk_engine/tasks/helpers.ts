1ca8915b46dc363fb97bcb84860120e29fd70d5c
Ryland Herrick <ryalnd@gmail.com>
2023-08-03 23:10:14 -0500

    Add some helpers for converting a relative date range to an absolute one
    Most queries that use 'now' cannot be cached by elasticsearch.
    
    Ref: https://www.elastic.co/guide/en/elasticsearch/reference/current/shard-request-cache.html

---
+++ x-pack/plugins/security_solution/server/lib/risk_engine/tasks/helpers.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import datemath from '@kbn/datemath';
+import type { Range } from '../../../../common/risk_engine';
+
+export const convertDateToISOString = (dateString: string): string => {
+  const date = datemath.parse(dateString);
+
+  if (date?.isValid()) {
+    return date.toISOString();
+  } else {
+    throw new Error(`Could not convert string "${dateString}" to ISO string`);
+  }
+};
+
+export const convertRangeToISO = (range: Range): Range => ({
+  start: convertDateToISOString(range.start),
+  end: convertDateToISOString(range.end),
+});
