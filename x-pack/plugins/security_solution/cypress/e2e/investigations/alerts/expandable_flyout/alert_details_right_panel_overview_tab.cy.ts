0eaed3bfbbb06ee8bfb993b0c7956f7df6468c0b
christineweng <weng67osu@gmail.com>
2023-08-04 14:23:16 -0500

    test

--- x-pack/plugins/security_solution/cypress/e2e/investigations/alerts/expandable_flyout/alert_details_right_panel_overview_tab.cy.ts
+++ x-pack/plugins/security_solution/cypress/e2e/investigations/alerts/expandable_flyout/alert_details_right_panel_overview_tab.cy.ts
@@ -318,6 +318,7 @@ describe(
 
       // TODO work on getting proper data to make the prevalence section work here
       //  we need to generate enough data to have at least one field with prevalence
+      //
       it.skip('should display prevalence section', () => {
         toggleOverviewTabAboutSection();
         toggleOverviewTabInsightsSection();
