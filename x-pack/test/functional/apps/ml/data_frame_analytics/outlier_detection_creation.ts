ca8a9dd2d66dc0d7bd9b1a3337ee1e68b99015e3
Tiago Costa <tiago.costa@elastic.co>
2023-08-05 00:56:24 +0100

    skip flaky suite (#163220)

--- x-pack/test/functional/apps/ml/data_frame_analytics/outlier_detection_creation.ts
+++ x-pack/test/functional/apps/ml/data_frame_analytics/outlier_detection_creation.ts
@@ -191,7 +191,8 @@ export default function ({ getService }: FtrProviderContext) {
     ];
 
     for (const testData of testDataList) {
-      describe(`${testData.suiteTitle}`, function () {
+      // FLAKY: https://github.com/elastic/kibana/issues/163220
+      describe.skip(`${testData.suiteTitle}`, function () {
         after(async () => {
           await ml.api.deleteIndices(testData.destinationIndex);
           await ml.testResources.deleteIndexPatternByTitle(testData.destinationIndex);
