74db2510c1ef63162b6f87dc1c1b4312bb613250
Marshall Main <marshall.main@elastic.co>
2023-08-04 07:48:57 -0700

    Move export to correct place

--- x-pack/plugins/security_solution/common/api/detection_engine/signals/index.ts
+++ x-pack/plugins/security_solution/common/api/detection_engine/signals/index.ts
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-export * from '../index_management/create_index/create_index_route';
 export * from './query_signals/query_signals_route';
 export * from './set_signal_status/set_signal_status_route';
 export * from './set_signal_status/set_signal_status_type_dependents';
