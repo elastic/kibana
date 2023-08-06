e311aef924391c2b2bd36e171cdb35df8b789e8d
Dario Gieselaar <dario.gieselaar@elastic.co>
2023-08-06 14:05:06 +0200

    Prevent cursor animation from being unmounted and reset

--- x-pack/plugins/observability_ai_assistant/public/components/message_panel/message_text.tsx
+++ x-pack/plugins/observability_ai_assistant/public/components/message_panel/message_text.tsx
@@ -4,43 +4,37 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { v4 } from 'uuid';
-import React from 'react';
-import type { Node } from 'unist';
-import { css } from '@emotion/css';
-import type { Parent, Text, InlineCode, Code } from 'mdast';
-import ReactMarkdown from 'react-markdown';
 import { EuiText } from '@elastic/eui';
+import { css } from '@emotion/css';
 import { euiThemeVars } from '@kbn/ui-theme';
+import classNames from 'classnames';
+import type { Code, InlineCode, Parent, Text } from 'mdast';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import type { Node } from 'unist';
+import { v4 } from 'uuid';
 
 interface Props {
   content: string;
   loading: boolean;
 }
 
-const containerClassName = css`
-  overflow-wrap: break-word;
-
-  code {
-    background: ${euiThemeVars.euiColorLightestShade};
-    padding: 0 8px;
-  }
-`;
+const ANIMATION_TIME = 1;
 
 const cursorCss = css`
   @keyframes blink {
     0% {
-      opacity: 1;
-    }
-    50% {
       opacity: 0;
     }
-    100% {
+    50% {
       opacity: 1;
+    }
+    100% {
+      opacity: 0;
     }
   }
 
-  animation: blink 1s infinite;
+  animation: blink ${ANIMATION_TIME}s infinite;
   width: 10px;
   height: 16px;
   vertical-align: middle;
@@ -48,7 +42,7 @@ const cursorCss = css`
   background: rgba(0, 0, 0, 0.25);
 `;
 
-const cursor = <span className={cursorCss} />;
+const Cursor = () => <span key="cursor" className={classNames(cursorCss, 'cursor')} />;
 
 const CURSOR = `{{${v4()}}`;
 
@@ -90,13 +84,22 @@ const loadingCursorPlugin = () => {
 };
 
 export function MessageText(props: Props) {
+  const containerClassName = css`
+    overflow-wrap: break-word;
+
+    code {
+      background: ${euiThemeVars.euiColorLightestShade};
+      padding: 0 8px;
+    }
+  `;
+
   return (
     <EuiText size="s" className={containerClassName}>
       <ReactMarkdown
         plugins={[loadingCursorPlugin]}
         components={
           {
-            cursor: () => cursor,
+            cursor: Cursor,
           } as Record<string, any>
         }
       >
