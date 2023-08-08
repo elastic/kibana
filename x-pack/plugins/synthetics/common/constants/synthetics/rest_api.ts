f2d26764386e650b9a8c9d1b4eef639c9ac09532
shahzad31 <shahzad31comp@gmail.com>
2023-08-08 15:01:58 +0200

    update

--- x-pack/plugins/synthetics/common/constants/synthetics/rest_api.ts
+++ x-pack/plugins/synthetics/common/constants/synthetics/rest_api.ts
@@ -49,9 +49,4 @@ export enum SYNTHETICS_API_URLS {
   SYNTHETICS_MONITORS_PROJECT_DELETE = '/api/synthetics/project/{projectName}/monitors/_bulk_delete',
 
   DYNAMIC_SETTINGS = `/internal/uptime/dynamic_settings`,
-  RULE_CONNECTORS = '/api/actions/connectors',
-  CREATE_RULE = '/api/alerting/rule',
-  DELETE_RULE = '/api/alerting/rule/',
-  RULES_FIND = '/api/alerting/rules/_find',
-  CONNECTOR_TYPES = '/api/actions/connector_types',
 }
