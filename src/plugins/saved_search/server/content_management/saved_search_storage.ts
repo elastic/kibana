d4684432fa77fbbbbbb723d4d4754fe85a012c08
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-08-09 15:16:42 +0200

    [Discover] Fix reading from saved search SO

--- src/plugins/saved_search/server/content_management/saved_search_storage.ts
+++ src/plugins/saved_search/server/content_management/saved_search_storage.ts
@@ -36,6 +36,7 @@ export class SavedSearchStorage extends SOContentStorage<SavedSearchCrudTypes> {
         'refreshInterval',
         'rowsPerPage',
         'breakdownField',
+        'sampleSize',
       ],
     });
   }
