7c35110d80819bfde34eb6ba2d702baa95deab2a
vadimkibana <vadimkibana@gmail.com>
2023-08-10 10:49:54 +0200

    update theme provider import in redirect app

--- src/plugins/share/public/url_service/redirect/components/page.tsx
+++ src/plugins/share/public/url_service/redirect/components/page.tsx
@@ -10,7 +10,7 @@ import * as React from 'react';
 import useObservable from 'react-use/lib/useObservable';
 import { EuiPageTemplate } from '@elastic/eui';
 import { ThemeServiceSetup } from '@kbn/core/public';
-import { KibanaThemeProvider } from '@kbn/kibana-react-plugin/public';
+import { KibanaThemeProvider } from '@kbn/react-kibana-context-theme';
 import { CustomBrandingStart } from '@kbn/core-custom-branding-browser';
 import { Error } from './error';
 import { RedirectManager } from '../redirect_manager';
@@ -28,7 +28,7 @@ export const Page: React.FC<PageProps> = ({ manager, theme, customBranding }) =>
 
   if (error) {
     return (
-      <KibanaThemeProvider theme$={theme.theme$}>
+      <KibanaThemeProvider theme={{ theme$: theme.theme$ }}>
         <EuiPageTemplate>
           <Error error={error} />
         </EuiPageTemplate>
@@ -37,7 +37,7 @@ export const Page: React.FC<PageProps> = ({ manager, theme, customBranding }) =>
   }
 
   return (
-    <KibanaThemeProvider theme$={theme.theme$}>
+    <KibanaThemeProvider theme={{ theme$: theme.theme$ }}>
       <EuiPageTemplate>
         <Spinner showPlainSpinner={Boolean(hasCustomBranding)} />
       </EuiPageTemplate>
