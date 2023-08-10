ff72859add25d44eaebdc87f9473f1eac80b6c88
Tiago Costa <tiago.costa@elastic.co>
2023-08-10 18:19:29 +0100

    fix(NA): eslint errors on ci checks

--- x-pack/plugins/osquery/cypress/cypress.config.ts
+++ x-pack/plugins/osquery/cypress/cypress.config.ts
@@ -1,9 +1,9 @@
 /*
-* Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-* or more contributor license agreements. Licensed under the Elastic License
-* 2.0; you may not use this file except in compliance with the Elastic License
-* 2.0.
-*/
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'cypress';
@@ -35,6 +35,7 @@ export default defineConfig({
 
   e2e: {
     baseUrl: 'http://localhost:5601',
+    // eslint-disable-next-line
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
