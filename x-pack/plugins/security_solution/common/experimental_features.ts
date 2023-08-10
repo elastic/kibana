94c24182e6664bc22020f2634b6dfe4481658206
Jatin Kathuria <jatin.kathuria@elastic.co>
2023-08-10 15:26:14 +0200

    discoverInTimeline feature flag should be false by default

--- x-pack/plugins/security_solution/common/experimental_features.ts
+++ x-pack/plugins/security_solution/common/experimental_features.ts
@@ -116,7 +116,7 @@ export const allowedExperimentalValues = Object.freeze({
    * Enables Discover embedded within timeline
    *
    * */
-  discoverInTimeline: true,
+  discoverInTimeline: false,
 });
 
 type ExperimentalConfigKeys = Array<keyof ExperimentalFeatures>;
