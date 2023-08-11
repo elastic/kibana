elastic/kibana

commit 22b85f83fbf7433c3649a9bba9719599a037e0ee

[ML] Expose Transform capabilities to Kibana's core.capabilities (#154007)
Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/transform/common/privilege/has_privilege_factory.ts
@@ -8,9 +8,11 @@
 import { i18n } from '@kbn/i18n';
 import { isPopulatedObject } from '@kbn/ml-is-populated-object';
 
-import { Privileges } from '../../../../../common/types/privileges';
+import { cloneDeep } from 'lodash';
+import { APP_INDEX_PRIVILEGES } from '../constants';
+import { Privileges } from '../types/privileges';
 
-export interface Capabilities {
+export interface TransformCapabilities {
   canGetTransform: boolean;
   canDeleteTransform: boolean;
   canPreviewTransform: boolean;
@@ -21,6 +23,19 @@ export interface Capabilities {
   canUseTransformAlerts: boolean;
   canResetTransform: boolean;
 }
+export type Capabilities = { [k in keyof TransformCapabilities]: boolean };
+
+export const INITIAL_CAPABILITIES = Object.freeze<Capabilities>({
+  canGetTransform: false,
+  canDeleteTransform: false,
+  canPreviewTransform: false,
+  canCreateTransform: false,
+  canScheduleNowTransform: false,
+  canStartStopTransform: false,
+  canCreateTransformAlerts: false,
+  canUseTransformAlerts: false,
+  canResetTransform: false,
+});
 
 export type Privilege = [string, string];
 
@@ -58,10 +73,69 @@ export const hasPrivilegeFactory =
     );
   };
 
+export const extractMissingPrivileges = (
+  privilegesObject: { [key: string]: boolean } = {}
+): string[] =>
+  Object.keys(privilegesObject).reduce((privileges: string[], privilegeName: string): string[] => {
+    if (!privilegesObject[privilegeName]) {
+      privileges.push(privilegeName);
+    }
+    return privileges;
+  }, []);
+
+export const getPrivilegesAndCapabilities = (
+  clusterPrivileges: Record<string, boolean>,
+  hasOneIndexWithAllPrivileges: boolean,
+  hasAllPrivileges: boolean
+) => {
+  const privilegesResult: Privileges = {
+    hasAllPrivileges: true,
+    missingPrivileges: {
+      cluster: [],
+      index: [],
+    },
+  };
+
+  // Find missing cluster privileges and set overall app privileges
+  privilegesResult.missingPrivileges.cluster = extractMissingPrivileges(clusterPrivileges);
+  privilegesResult.hasAllPrivileges = hasAllPrivileges;
+
+  if (!hasOneIndexWithAllPrivileges) {
+    privilegesResult.missingPrivileges.index = [...APP_INDEX_PRIVILEGES];
+  }
+
+  const hasPrivilege = hasPrivilegeFactory(privilegesResult);
+
+  const capabilities = cloneDeep<Capabilities>(INITIAL_CAPABILITIES);
+  capabilities.canGetTransform =
+    hasPrivilege(['cluster', 'cluster:monitor/transform/get']) &&
+    hasPrivilege(['cluster', 'cluster:monitor/transform/stats/get']);
+
+  capabilities.canCreateTransform = hasPrivilege(['cluster', 'cluster:admin/transform/put']);
+
+  capabilities.canDeleteTransform = hasPrivilege(['cluster', 'cluster:admin/transform/delete']);
+
+  capabilities.canResetTransform = hasPrivilege(['cluster', 'cluster:admin/transform/reset']);
+
+  capabilities.canPreviewTransform = hasPrivilege(['cluster', 'cluster:admin/transform/preview']);
+
+  capabilities.canStartStopTransform =
+    hasPrivilege(['cluster', 'cluster:admin/transform/start']) &&
+    hasPrivilege(['cluster', 'cluster:admin/transform/start_task']) &&
+    hasPrivilege(['cluster', 'cluster:admin/transform/stop']);
+
+  capabilities.canCreateTransformAlerts = capabilities.canCreateTransform;
+
+  capabilities.canUseTransformAlerts = capabilities.canGetTransform;
+
+  capabilities.canScheduleNowTransform = capabilities.canStartStopTransform;
+
+  return { privileges: privilegesResult, capabilities };
+};
 // create the text for button's tooltips if the user
 // doesn't have the permission to press that button
 export function createCapabilityFailureMessage(
-  capability: keyof Capabilities | 'noTransformNodes'
+  capability: keyof TransformCapabilities | 'noTransformNodes'
 ) {
   let message = '';
 

x-pack/plugins/transform/public/app/lib/authorization/components/authorization_provider.tsx
@@ -11,26 +11,17 @@ import { Privileges } from '../../../../../common/types/privileges';
 
 import { useRequest } from '../../../hooks';
 
-import { hasPrivilegeFactory, Capabilities } from './common';
+import {
+  TransformCapabilities,
+  INITIAL_CAPABILITIES,
+} from '../../../../../common/privilege/has_privilege_factory';
 
 interface Authorization {
   isLoading: boolean;
   apiError: Error | null;
   privileges: Privileges;
-  capabilities: Capabilities;
+  capabilities: TransformCapabilities;
 }
-
-const initialCapabilities: Capabilities = {
-  canGetTransform: false,
-  canDeleteTransform: false,
-  canPreviewTransform: false,
-  canCreateTransform: false,
-  canScheduleNowTransform: false,
-  canStartStopTransform: false,
-  canCreateTransformAlerts: false,
-  canUseTransformAlerts: false,
-  canResetTransform: false,
-};
 
 const initialValue: Authorization = {
   isLoading: true,
@@ -39,7 +30,7 @@ const initialValue: Authorization = {
     hasAllPrivileges: false,
     missingPrivileges: {},
   },
-  capabilities: initialCapabilities,
+  capabilities: INITIAL_CAPABILITIES,
 };
 
 export const AuthorizationContext = createContext<Authorization>({ ...initialValue });
@@ -61,41 +52,10 @@ export const AuthorizationProvider = ({ privilegesEndpoint, children }: Props) =
 
   const value = {
     isLoading,
-    privileges: isLoading ? { ...initialValue.privileges } : privilegesData,
-    capabilities: { ...initialCapabilities },
+    privileges: isLoading ? { ...initialValue.privileges } : privilegesData.privileges,
+    capabilities: isLoading ? { ...INITIAL_CAPABILITIES } : privilegesData.capabilities,
     apiError: error ? (error as Error) : null,
   };
-
-  const hasPrivilege = hasPrivilegeFactory(value.privileges);
-
-  value.capabilities.canGetTransform =
-    hasPrivilege(['cluster', 'cluster:monitor/transform/get']) &&
-    hasPrivilege(['cluster', 'cluster:monitor/transform/stats/get']);
-
-  value.capabilities.canCreateTransform = hasPrivilege(['cluster', 'cluster:admin/transform/put']);
-
-  value.capabilities.canDeleteTransform = hasPrivilege([
-    'cluster',
-    'cluster:admin/transform/delete',
-  ]);
-
-  value.capabilities.canResetTransform = hasPrivilege(['cluster', 'cluster:admin/transform/reset']);
-
-  value.capabilities.canPreviewTransform = hasPrivilege([
-    'cluster',
-    'cluster:admin/transform/preview',
-  ]);
-
-  value.capabilities.canStartStopTransform =
-    hasPrivilege(['cluster', 'cluster:admin/transform/start']) &&
-    hasPrivilege(['cluster', 'cluster:admin/transform/start_task']) &&
-    hasPrivilege(['cluster', 'cluster:admin/transform/stop']);
-
-  value.capabilities.canCreateTransformAlerts = value.capabilities.canCreateTransform;
-
-  value.capabilities.canUseTransformAlerts = value.capabilities.canGetTransform;
-
-  value.capabilities.canScheduleNowTransform = value.capabilities.canStartStopTransform;
 
   return (
     <AuthorizationContext.Provider value={{ ...value }}>{children}</AuthorizationContext.Provider>

x-pack/plugins/transform/server/routes/api/privileges.ts
@@ -5,10 +5,13 @@
  * 2.0.
  */
 
+import type { IScopedClusterClient } from '@kbn/core/server';
+import type { SecurityHasPrivilegesResponse } from '@elastic/elasticsearch/lib/api/types';
+import { getPrivilegesAndCapabilities } from '../../../common/privilege/has_privilege_factory';
 import { APP_CLUSTER_PRIVILEGES, APP_INDEX_PRIVILEGES } from '../../../common/constants';
-import { Privileges } from '../../../common/types/privileges';
+import type { Privileges } from '../../../common/types/privileges';
 
-import { RouteDependencies } from '../../types';
+import type { RouteDependencies } from '../../types';
 import { addBasePath } from '..';
 
 export function registerPrivilegesRoute({ router, license }: RouteDependencies) {
@@ -28,50 +31,42 @@ export function registerPrivilegesRoute({ router, license }: RouteDependencies) 
         return res.ok({ body: privilegesResult });
       }
 
-      const esClient = (await ctx.core).elasticsearch.client;
-      // Get cluster privileges
-      const { has_all_requested: hasAllPrivileges, cluster } =
-        await esClient.asCurrentUser.security.hasPrivileges({
+      const esClient: IScopedClusterClient = (await ctx.core).elasticsearch.client;
+
+      const esClusterPrivilegesReq: Promise<SecurityHasPrivilegesResponse> =
+        esClient.asCurrentUser.security.hasPrivileges({
           body: {
             // @ts-expect-error SecurityClusterPrivilege doesn’t contain all the priviledges
             cluster: APP_CLUSTER_PRIVILEGES,
           },
         });
+      const [esClusterPrivileges, userPrivileges] = await Promise.all([
+        // Get cluster privileges
+        esClusterPrivilegesReq,
+        // // Get all index privileges the user has
+        esClient.asCurrentUser.security.getUserPrivileges(),
+      ]);
 
-      // Find missing cluster privileges and set overall app privileges
-      privilegesResult.missingPrivileges.cluster = extractMissingPrivileges(cluster);
-      privilegesResult.hasAllPrivileges = hasAllPrivileges;
-
-      // Get all index privileges the user has
-      const { indices } = await esClient.asCurrentUser.security.getUserPrivileges();
+      const { has_all_requested: hasAllPrivileges, cluster } = esClusterPrivileges;
+      const { indices } = userPrivileges;
 
       // Check if they have all the required index privileges for at least one index
-      const oneIndexWithAllPrivileges = indices.find(({ privileges }: { privileges: string[] }) => {
-        if (privileges.includes('all')) {
-          return true;
-        }
+      const hasOneIndexWithAllPrivileges =
+        indices.find(({ privileges }: { privileges: string[] }) => {
+          if (privileges.includes('all')) {
+            return true;
+          }
 
-        const indexHasAllPrivileges = APP_INDEX_PRIVILEGES.every((privilege) =>
-          privileges.includes(privilege)
-        );
+          const indexHasAllPrivileges = APP_INDEX_PRIVILEGES.every((privilege) =>
+            privileges.includes(privilege)
+          );
 
-        return indexHasAllPrivileges;
+          return indexHasAllPrivileges;
+        }) !== undefined;
+
+      return res.ok({
+        body: getPrivilegesAndCapabilities(cluster, hasOneIndexWithAllPrivileges, hasAllPrivileges),
       });
-
-      // If they don't, return list of required index privileges
-      if (!oneIndexWithAllPrivileges) {
-        privilegesResult.missingPrivileges.index = [...APP_INDEX_PRIVILEGES];
-      }
-
-      return res.ok({ body: privilegesResult });
     })
   );
 }
-
-const extractMissingPrivileges = (privilegesObject: { [key: string]: boolean } = {}): string[] =>
-  Object.keys(privilegesObject).reduce((privileges: string[], privilegeName: string): string[] => {
-    if (!privilegesObject[privilegeName]) {
-      privileges.push(privilegeName);
-    }
-    return privileges;
-  }, []);

x-pack/plugins/transform/public/app/lib/authorization/components/index.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-export { createCapabilityFailureMessage } from './common';
+export { createCapabilityFailureMessage } from '../../../../../common/privilege/has_privilege_factory';
 export { AuthorizationProvider, AuthorizationContext } from './authorization_provider';
 export { PrivilegesWrapper } from './with_privileges';
 export { NotAuthorizedSection } from './not_authorized_section';

commit ecaa8a73707f97c99ef2d57574ac3dcbac3096df

[ML] Transforms: Fix privileges check. (#163687)
Fixes a regression introduced in #154007.

When security is disabled, the check for transform
privileges/capabilities would fail and the transform page would not
render.

- This fixes the endpoint to return data in the correct format should
security be disabled.
- The endpoint's TypeScript has been updated to catch malformed
responses.
- The client side custom `useRequest` is replaced with `useQuery` from
`'@tanstack/react-query'` which also fixes TypeScript support on the
client side.

x-pack/plugins/transform/common/privilege/has_privilege_factory.ts
@@ -12,6 +12,11 @@ import { cloneDeep } from 'lodash';
 import { APP_INDEX_PRIVILEGES } from '../constants';
 import { Privileges } from '../types/privileges';
 
+export interface PrivilegesAndCapabilities {
+  privileges: Privileges;
+  capabilities: Capabilities;
+}
+
 export interface TransformCapabilities {
   canGetTransform: boolean;
   canDeleteTransform: boolean;
@@ -89,7 +94,7 @@ export const getPrivilegesAndCapabilities = (
   clusterPrivileges: Record<string, boolean>,
   hasOneIndexWithAllPrivileges: boolean,
   hasAllPrivileges: boolean
-) => {
+): PrivilegesAndCapabilities => {
   const privilegesResult: Privileges = {
     hasAllPrivileges: true,
     missingPrivileges: {

x-pack/plugins/transform/public/__mocks__/shared_imports.ts
@@ -8,11 +8,6 @@
 // actual mocks
 export const expandLiteralStrings = jest.fn();
 export const XJsonMode = jest.fn();
-export const useRequest = jest.fn(() => ({
-  isLoading: false,
-  error: null,
-  data: undefined,
-}));
 export const getSavedSearch = jest.fn();
 
 // just passing through the reimports

x-pack/plugins/transform/public/app/app.tsx
@@ -7,14 +7,13 @@
 
 import React, { useContext, FC } from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
-import { Router, Routes, Route } from '@kbn/shared-ux-router';
-
-import { ScopedHistory } from '@kbn/core/public';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { EuiErrorBoundary } from '@elastic/eui';
 
+import { Router, Routes, Route } from '@kbn/shared-ux-router';
+import { ScopedHistory } from '@kbn/core/public';
 import { FormattedMessage } from '@kbn/i18n-react';
-
 import { KibanaContextProvider, KibanaThemeProvider } from '@kbn/kibana-react-plugin/public';
 
 import { addInternalBasePath } from '../../common/constants';
@@ -23,7 +22,6 @@ import { SectionError } from './components';
 import { SECTION_SLUG } from './common/constants';
 import { AuthorizationContext, AuthorizationProvider } from './lib/authorization';
 import { AppDependencies } from './app_dependencies';
-
 import { CloneTransformSection } from './sections/clone_transform';
 import { CreateTransformSection } from './sections/create_transform';
 import { TransformManagementSection } from './sections/transform_management';
@@ -63,20 +61,23 @@ export const App: FC<{ history: ScopedHistory }> = ({ history }) => {
 
 export const renderApp = (element: HTMLElement, appDependencies: AppDependencies) => {
   const I18nContext = appDependencies.i18n.Context;
+  const queryClient = new QueryClient();
 
   render(
     <EuiErrorBoundary>
-      <KibanaThemeProvider theme$={appDependencies.theme.theme$}>
-        <KibanaContextProvider services={appDependencies}>
-          <AuthorizationProvider
-            privilegesEndpoint={{ path: addInternalBasePath(`privileges`), version: '1' }}
-          >
-            <I18nContext>
-              <App history={appDependencies.history} />
-            </I18nContext>
-          </AuthorizationProvider>
-        </KibanaContextProvider>
-      </KibanaThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <KibanaThemeProvider theme$={appDependencies.theme.theme$}>
+          <KibanaContextProvider services={appDependencies}>
+            <AuthorizationProvider
+              privilegesEndpoint={{ path: addInternalBasePath(`privileges`), version: '1' }}
+            >
+              <I18nContext>
+                <App history={appDependencies.history} />
+              </I18nContext>
+            </AuthorizationProvider>
+          </KibanaContextProvider>
+        </KibanaThemeProvider>
+      </QueryClientProvider>
     </EuiErrorBoundary>,
     element
   );

x-pack/plugins/transform/public/app/hooks/index.ts
@@ -12,4 +12,3 @@ export { useResetTransforms } from './use_reset_transform';
 export { useScheduleNowTransforms } from './use_schedule_now_transform';
 export { useStartTransforms } from './use_start_transform';
 export { useStopTransforms } from './use_stop_transform';
-export { useRequest } from './use_request';

x-pack/plugins/transform/public/app/hooks/use_request.ts
@@ -1,15 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import { UseRequestConfig, useRequest as _useRequest } from '../../shared_imports';
-
-import { useAppDependencies } from '../app_dependencies';
-
-export const useRequest = (config: UseRequestConfig) => {
-  const { http } = useAppDependencies();
-  return _useRequest(http, config);
-};

x-pack/plugins/transform/public/app/lib/authorization/components/authorization_provider.tsx
@@ -6,15 +6,19 @@
  */
 
 import React, { createContext } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import { Privileges } from '../../../../../common/types/privileges';
+import type { IHttpFetchError } from '@kbn/core-http-browser';
 
-import { useRequest } from '../../../hooks';
+import type { Privileges } from '../../../../../common/types/privileges';
 
 import {
-  TransformCapabilities,
+  type PrivilegesAndCapabilities,
+  type TransformCapabilities,
   INITIAL_CAPABILITIES,
 } from '../../../../../common/privilege/has_privilege_factory';
+
+import { useAppDependencies } from '../../../app_dependencies';
 
 interface Authorization {
   isLoading: boolean;
@@ -41,22 +45,36 @@ interface Props {
 }
 
 export const AuthorizationProvider = ({ privilegesEndpoint, children }: Props) => {
+  const { http } = useAppDependencies();
+
   const { path, version } = privilegesEndpoint;
+
   const {
     isLoading,
     error,
     data: privilegesData,
-  } = useRequest({
-    path,
-    version,
-    method: 'get',
-  });
+  } = useQuery<PrivilegesAndCapabilities, IHttpFetchError>(
+    ['transform-privileges-and-capabilities'],
+    async ({ signal }) => {
+      return await http.fetch<PrivilegesAndCapabilities>(path, {
+        version,
+        method: 'GET',
+        signal,
+      });
+    }
+  );
 
   const value = {
     isLoading,
-    privileges: isLoading ? { ...initialValue.privileges } : privilegesData.privileges,
-    capabilities: isLoading ? { ...INITIAL_CAPABILITIES } : privilegesData.capabilities,
-    apiError: error ? (error as Error) : null,
+    privileges:
+      isLoading || privilegesData === undefined
+        ? { ...initialValue.privileges }
+        : privilegesData.privileges,
+    capabilities:
+      isLoading || privilegesData === undefined
+        ? { ...INITIAL_CAPABILITIES }
+        : privilegesData.capabilities,
+    apiError: error ? error : null,
   };
 
   return (

x-pack/plugins/transform/public/shared_imports.ts
@@ -6,8 +6,6 @@
  */
 
 export { XJsonMode } from '@kbn/ace';
-export type { UseRequestConfig } from '@kbn/es-ui-shared-plugin/public';
-export { useRequest } from '@kbn/es-ui-shared-plugin/public';
 
 export type { GetMlSharedImportsReturnType } from '@kbn/ml-plugin/public';
 export { getMlSharedImports } from '@kbn/ml-plugin/public';

x-pack/plugins/transform/server/routes/api/privileges.ts
@@ -5,15 +5,17 @@
  * 2.0.
  */
 
-import type { IScopedClusterClient } from '@kbn/core/server';
+import type { IKibanaResponse, IScopedClusterClient } from '@kbn/core/server';
 import type { SecurityHasPrivilegesResponse } from '@elastic/elasticsearch/lib/api/types';
-import { getPrivilegesAndCapabilities } from '../../../common/privilege/has_privilege_factory';
+import {
+  getPrivilegesAndCapabilities,
+  type PrivilegesAndCapabilities,
+} from '../../../common/privilege/has_privilege_factory';
 import {
   addInternalBasePath,
   APP_CLUSTER_PRIVILEGES,
   APP_INDEX_PRIVILEGES,
 } from '../../../common/constants';
-import type { Privileges } from '../../../common/types/privileges';
 
 import type { RouteDependencies } from '../../types';
 
@@ -23,64 +25,60 @@ export function registerPrivilegesRoute({ router, license }: RouteDependencies) 
       path: addInternalBasePath('privileges'),
       access: 'internal',
     })
-    .addVersion(
+    .addVersion<undefined, undefined, undefined>(
       {
         version: '1',
         validate: false,
       },
-      license.guardApiRoute(async (ctx, req, res) => {
-        const privilegesResult: Privileges = {
-          hasAllPrivileges: true,
-          missingPrivileges: {
-            cluster: [],
-            index: [],
-          },
-        };
+      license.guardApiRoute<undefined, undefined, undefined>(
+        async (ctx, req, res): Promise<IKibanaResponse<PrivilegesAndCapabilities>> => {
+          if (license.getStatus().isSecurityEnabled === false) {
+            // If security isn't enabled, let the user use app.
+            return res.ok({
+              body: getPrivilegesAndCapabilities({}, true, true),
+            });
+          }
 
-        if (license.getStatus().isSecurityEnabled === false) {
-          // If security isn't enabled, let the user use app.
-          return res.ok({ body: privilegesResult });
-        }
+          const esClient: IScopedClusterClient = (await ctx.core).elasticsearch.client;
 
-        const esClient: IScopedClusterClient = (await ctx.core).elasticsearch.client;
+          const esClusterPrivilegesReq: Promise<SecurityHasPrivilegesResponse> =
+            esClient.asCurrentUser.security.hasPrivileges({
+              body: {
+                cluster: APP_CLUSTER_PRIVILEGES,
+              },
+            });
+          const [esClusterPrivileges, userPrivileges] = await Promise.all([
+            // Get cluster privileges
+            esClusterPrivilegesReq,
+            // // Get all index privileges the user has
+            esClient.asCurrentUser.security.getUserPrivileges(),
+          ]);
 
-        const esClusterPrivilegesReq: Promise<SecurityHasPrivilegesResponse> =
-          esClient.asCurrentUser.security.hasPrivileges({
-            body: {
-              cluster: APP_CLUSTER_PRIVILEGES,
-            },
+          const { has_all_requested: hasAllPrivileges, cluster } = esClusterPrivileges;
+          const { indices } = userPrivileges;
+
+          // Check if they have all the required index privileges for at least one index
+          const hasOneIndexWithAllPrivileges =
+            indices.find(({ privileges }: { privileges: string[] }) => {
+              if (privileges.includes('all')) {
+                return true;
+              }
+
+              const indexHasAllPrivileges = APP_INDEX_PRIVILEGES.every((privilege) =>
+                privileges.includes(privilege)
+              );
+
+              return indexHasAllPrivileges;
+            }) !== undefined;
+
+          return res.ok({
+            body: getPrivilegesAndCapabilities(
+              cluster,
+              hasOneIndexWithAllPrivileges,
+              hasAllPrivileges
+            ),
           });
-        const [esClusterPrivileges, userPrivileges] = await Promise.all([
-          // Get cluster privileges
-          esClusterPrivilegesReq,
-          // // Get all index privileges the user has
-          esClient.asCurrentUser.security.getUserPrivileges(),
-        ]);
-
-        const { has_all_requested: hasAllPrivileges, cluster } = esClusterPrivileges;
-        const { indices } = userPrivileges;
-
-        // Check if they have all the required index privileges for at least one index
-        const hasOneIndexWithAllPrivileges =
-          indices.find(({ privileges }: { privileges: string[] }) => {
-            if (privileges.includes('all')) {
-              return true;
-            }
-
-            const indexHasAllPrivileges = APP_INDEX_PRIVILEGES.every((privilege) =>
-              privileges.includes(privilege)
-            );
-
-            return indexHasAllPrivileges;
-          }) !== undefined;
-
-        return res.ok({
-          body: getPrivilegesAndCapabilities(
-            cluster,
-            hasOneIndexWithAllPrivileges,
-            hasAllPrivileges
-          ),
-        });
-      })
+        }
+      )
     );
 }