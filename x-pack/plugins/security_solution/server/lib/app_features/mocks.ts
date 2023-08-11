207d8f8d0c007c2da6c542db56d5101c373b2750
David Sanchez Soler <david.sanchezsoler@elastic.co>
2023-08-11 16:09:59 +0200

    Fixes test mock an adds new test cases

--- x-pack/plugins/security_solution/server/lib/app_features/mocks.ts
+++ x-pack/plugins/security_solution/server/lib/app_features/mocks.ts
@@ -13,7 +13,7 @@ import { AppFeatures } from './app_features';
 import type { AppFeatureKeys, ExperimentalFeatures } from '../../../common';
 import { ALL_APP_FEATURE_KEYS, allowedExperimentalValues } from '../../../common';
 
-class AppFeaturesMock extends AppFeatures {
+export class AppFeaturesMock extends AppFeatures {
   protected registerEnabledKibanaFeatures() {
     // NOOP
   }
