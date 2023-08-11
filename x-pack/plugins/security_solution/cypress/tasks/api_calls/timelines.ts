bbf4acc82891a02f05f15a2f2a93bee87328ec20
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-08-11 17:22:52 +0200

    fix

--- x-pack/plugins/security_solution/cypress/tasks/api_calls/timelines.ts
+++ x-pack/plugins/security_solution/cypress/tasks/api_calls/timelines.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { TimelineResponse } from '../../common/types/timeline';
+import { TimelineResponse } from '../../../common/types/timeline';
 import { CompleteTimeline } from '../../objects/timeline';
 import { rootRequest } from '../common';
 
@@ -54,7 +54,7 @@ export const createTimeline = (timeline: CompleteTimeline) =>
   });
 
 export const createTimelineTemplate = (timeline: CompleteTimeline) =>
-  rootRequest({
+  rootRequest<TimelineResponse>({
     method: 'POST',
     url: 'api/timeline',
     body: {
