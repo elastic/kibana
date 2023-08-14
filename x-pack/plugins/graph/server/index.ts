cfbe7a9bbeab2ae824be4bbeba5f9684b22eadef
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2023-08-14 11:47:42 +0300

    Apply PR comments

--- x-pack/plugins/graph/server/index.ts
+++ x-pack/plugins/graph/server/index.ts
@@ -16,7 +16,6 @@ export const config: PluginConfigDescriptor<ConfigSchema> = {
   exposeToBrowser: {
     canEditDrillDownUrls: true,
     savePolicy: true,
-    enabled: true,
   },
   schema: configSchema,
 };
