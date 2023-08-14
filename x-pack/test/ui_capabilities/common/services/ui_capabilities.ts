9b410002fb2484b718619e9c2a858bd4303fe85d
Alex Szabo <alex.szabo@elastic.co>
2023-08-14 15:03:17 +0200

    chore: fix more type errors related to axios

--- x-pack/test/ui_capabilities/common/services/ui_capabilities.ts
+++ x-pack/test/ui_capabilities/common/services/ui_capabilities.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import axios, { AxiosInstance, AxiosRequestHeaders } from 'axios';
+import axios, { AxiosHeaders, AxiosInstance, AxiosRequestHeaders } from 'axios';
 import type { Capabilities as UICapabilities } from '@kbn/core/types';
 import { format as formatUrl } from 'url';
 import util from 'util';
@@ -62,12 +62,12 @@ export class UICapabilitiesService {
       `requesting ${spaceUrlPrefix}/api/core/capabilities to parse the uiCapabilities`
     );
     const requestHeaders: AxiosRequestHeaders = credentials
-      ? {
+      ? new AxiosHeaders({
           Authorization: `Basic ${Buffer.from(
             `${credentials.username}:${credentials.password}`
           ).toString('base64')}`,
-        }
-      : {};
+        })
+      : new AxiosHeaders({});
     const response = await this.axios.post(
       `${spaceUrlPrefix}/api/core/capabilities`,
       { applications: [...applications, 'kibana:stack_management'] },
