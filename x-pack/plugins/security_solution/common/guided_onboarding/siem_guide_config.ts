817af1e29d848a963f70e5ff6076f743aaf1bf29
Steph Milovic <stephanie.milovic@elastic.co>
2023-08-15 10:35:42 -0600

    add tests

--- x-pack/plugins/security_solution/common/guided_onboarding/siem_guide_config.ts
+++ x-pack/plugins/security_solution/common/guided_onboarding/siem_guide_config.ts
@@ -6,33 +6,33 @@
  */
 
 import type { GuideConfig } from '@kbn/guided-onboarding';
-import * as i18t from './translations';
+import * as i18n from './translations';
 
 export const siemGuideId = 'siem';
 
 export const defaultGuideTranslations = {
-  title: i18t.TITLE,
-  description: i18t.DESCRIPTION,
-  docs: i18t.DOCS,
+  title: i18n.TITLE,
+  description: i18n.DESCRIPTION,
+  docs: i18n.DOCS,
   steps: {
     add_data: {
-      title: i18t.ADD_DATA_TITLE,
-      description: i18t.ADD_DATA_DESCRIPTION,
+      title: i18n.ADD_DATA_TITLE,
+      description: i18n.ADD_DATA_DESCRIPTION,
     },
     rules: {
-      title: i18t.RULES_TITLE,
-      description: i18t.RULES_DESCRIPTION,
+      title: i18n.RULES_TITLE,
+      description: i18n.RULES_DESCRIPTION,
       manualCompletion: {
-        title: i18t.RULES_MANUAL_TITLE,
-        description: i18t.RULES_MANUAL_DESCRIPTION,
+        title: i18n.RULES_MANUAL_TITLE,
+        description: i18n.RULES_MANUAL_DESCRIPTION,
       },
     },
     alertsCases: {
-      title: i18t.CASES_TITLE,
-      description: i18t.CASES_DESCRIPTION,
+      title: i18n.CASES_TITLE,
+      description: i18n.CASES_DESCRIPTION,
       manualCompletion: {
-        title: i18t.CASES_MANUAL_TITLE,
-        description: i18t.CASES_MANUAL_DESCRIPTION,
+        title: i18n.CASES_MANUAL_TITLE,
+        description: i18n.CASES_MANUAL_DESCRIPTION,
       },
     },
   },
@@ -40,33 +40,29 @@ export const defaultGuideTranslations = {
 
 export const getSiemGuideConfig = (launchDarkly = defaultGuideTranslations): GuideConfig => ({
   // check each launchDarkly property in case data is misformatted
-  title: launchDarkly.title ? launchDarkly.title : defaultGuideTranslations.title,
+  title: launchDarkly.title ?? defaultGuideTranslations.title,
   guideName: 'Security',
   telemetryId: siemGuideId,
   completedGuideRedirectLocation: {
     appID: 'securitySolutionUI',
     path: '/dashboards',
   },
-  description: launchDarkly.description
-    ? launchDarkly.description
-    : defaultGuideTranslations.description,
+  description: launchDarkly.description ?? defaultGuideTranslations.description,
   docs: {
-    text: launchDarkly.docs ? launchDarkly.docs : defaultGuideTranslations.docs,
+    text: launchDarkly.docs ?? defaultGuideTranslations.docs,
     url: 'https://www.elastic.co/guide/en/security/current/ingest-data.html',
   },
   steps: [
     {
       id: 'add_data',
-      title: launchDarkly.steps?.add_data?.title
-        ? launchDarkly.steps.add_data.title
-        : defaultGuideTranslations.steps.add_data.title,
+      title: launchDarkly.steps?.add_data?.title ?? defaultGuideTranslations.steps.add_data.title,
       description: {
-        descriptionText: launchDarkly.steps?.add_data?.description
-          ? launchDarkly.steps.add_data.description
-          : defaultGuideTranslations.steps.add_data.description,
+        descriptionText:
+          launchDarkly.steps?.add_data?.description ??
+          defaultGuideTranslations.steps.add_data.description,
         linkUrl: 'https://docs.elastic.co/en/integrations/endpoint',
         isLinkExternal: true,
-        linkText: i18t.LINK_TEXT,
+        linkText: i18n.LINK_TEXT,
       },
       integration: 'endpoint',
       location: {
@@ -76,19 +72,16 @@ export const getSiemGuideConfig = (launchDarkly = defaultGuideTranslations): Gui
     },
     {
       id: 'rules',
-      title: launchDarkly.steps?.rules?.title
-        ? launchDarkly.steps.rules.title
-        : defaultGuideTranslations.steps.rules.title,
-      description: launchDarkly.steps?.rules?.description
-        ? launchDarkly.steps.rules.description
-        : defaultGuideTranslations.steps.rules.description,
+      title: launchDarkly.steps?.rules?.title ?? defaultGuideTranslations.steps.rules.title,
+      description:
+        launchDarkly.steps?.rules?.description ?? defaultGuideTranslations.steps.rules.description,
       manualCompletion: {
-        title: launchDarkly.steps?.rules?.manualCompletion?.title
-          ? launchDarkly.steps.rules.manualCompletion.title
-          : defaultGuideTranslations.steps.rules.manualCompletion.title,
-        description: launchDarkly.steps?.rules?.manualCompletion?.description
-          ? launchDarkly.steps.rules.manualCompletion.description
-          : defaultGuideTranslations.steps.rules.manualCompletion.description,
+        title:
+          launchDarkly.steps?.rules?.manualCompletion?.title ??
+          defaultGuideTranslations.steps.rules.manualCompletion.title,
+        description:
+          launchDarkly.steps?.rules?.manualCompletion?.description ??
+          defaultGuideTranslations.steps.rules.manualCompletion.description,
       },
       location: {
         appID: 'securitySolutionUI',
@@ -97,23 +90,22 @@ export const getSiemGuideConfig = (launchDarkly = defaultGuideTranslations): Gui
     },
     {
       id: 'alertsCases',
-      title: launchDarkly.steps?.alertsCases?.title
-        ? launchDarkly.steps.alertsCases.title
-        : defaultGuideTranslations.steps.alertsCases.title,
-      description: launchDarkly.steps?.alertsCases?.description
-        ? launchDarkly.steps.alertsCases.description
-        : defaultGuideTranslations.steps.alertsCases.description,
+      title:
+        launchDarkly.steps?.alertsCases?.title ?? defaultGuideTranslations.steps.alertsCases.title,
+      description:
+        launchDarkly.steps?.alertsCases?.description ??
+        defaultGuideTranslations.steps.alertsCases.description,
       location: {
         appID: 'securitySolutionUI',
         path: '/alerts',
       },
       manualCompletion: {
-        title: launchDarkly.steps?.alertsCases?.manualCompletion?.title
-          ? launchDarkly.steps.alertsCases.manualCompletion.title
-          : defaultGuideTranslations.steps.alertsCases.manualCompletion.title,
-        description: launchDarkly.steps?.alertsCases?.manualCompletion?.description
-          ? launchDarkly.steps.alertsCases.manualCompletion.description
-          : defaultGuideTranslations.steps.alertsCases.manualCompletion.description,
+        title:
+          launchDarkly.steps?.alertsCases?.manualCompletion?.title ??
+          defaultGuideTranslations.steps.alertsCases.manualCompletion.title,
+        description:
+          launchDarkly.steps?.alertsCases?.manualCompletion?.description ??
+          defaultGuideTranslations.steps.alertsCases.manualCompletion.description,
       },
     },
   ],
