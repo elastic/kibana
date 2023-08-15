9e07ad4050e18fc7d53353ebcc52ca7268fc3c3f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-08-15 11:08:29 +0200

    update journey

--- x-pack/performance/journeys/many_fields_lens_editor.ts
+++ x-pack/performance/journeys/many_fields_lens_editor.ts
@@ -12,15 +12,17 @@ export const journey = new Journey({
   kbnArchives: ['x-pack/performance/kbn_archives/lens_many_fields'],
   esArchives: ['test/functional/fixtures/es_archiver/stress_test'],
 })
-  .step('Go to Visualize Library', async ({ page, kbnUrl }) => {
+  .step('Go to Visualize Library landing page', async ({ page, kbnUrl }) => {
     await page.goto(
       kbnUrl.get(
         `/app/visualize#/?_g=(filters:!(),time:(from:'2022-09-07T10:53:30.262Z',to:'2022-09-07T10:55:09.280Z'))`
       )
     );
     await page.waitForSelector(subj('table-is-ready'));
+    // wait extra 5 seconds
+    await page.waitForTimeout(5000);
   })
-  .step('Open existing Lens', async ({ page, kibanaPage }) => {
+  .step('Open existing Lens visualization', async ({ page, kibanaPage }) => {
     await page.click(subj('visListingTitleLink-Lens-Stress-Test'));
     await kibanaPage.waitForCharts(6);
   });
