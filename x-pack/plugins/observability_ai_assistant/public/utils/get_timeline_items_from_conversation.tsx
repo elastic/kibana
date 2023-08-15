e500731eb3de17849413f75b7042fc0066c0d454
Coen Warmer <coen.warmer@gmail.com>
2023-08-15 16:25:53 +0200

    Handle case where function responses don't return JSON

--- x-pack/plugins/observability_ai_assistant/public/utils/get_timeline_items_from_conversation.tsx
+++ x-pack/plugins/observability_ai_assistant/public/utils/get_timeline_items_from_conversation.tsx
@@ -116,8 +116,14 @@ export function getTimelineItemsfromConversation({
 
           // User executed a function:
           if (message.message.name && prevFunctionCall) {
-            const parsedContent = JSON.parse(message.message.content ?? 'null');
-            const isError = !!(parsedContent && 'error' in parsedContent);
+            let parsedContent;
+            try {
+              parsedContent = JSON.parse(message.message.content ?? 'null');
+            } catch (error) {
+              parsedContent = message.message.content;
+            }
+
+            const isError = typeof parsedContent === 'object' && 'error' in parsedContent;
 
             title = !isError ? (
               <FormattedMessage
