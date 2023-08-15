ce73741b43d99681bb8ded8e3aae1e1fb482d809
Coen Warmer <coen.warmer@gmail.com>
2023-08-15 21:48:25 +0200

    Fix story

--- x-pack/plugins/observability_ai_assistant/public/components/chat/chat_header.stories.tsx
+++ x-pack/plugins/observability_ai_assistant/public/components/chat/chat_header.stories.tsx
@@ -9,6 +9,7 @@ import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
 import { FindActionResult } from '@kbn/actions-plugin/server';
 import { EuiPanel } from '@elastic/eui';
 import { ChatHeader as Component } from './chat_header';
+import { AbortableAsyncState } from '../../hooks/use_abortable_async';
 
 const meta: ComponentMeta<typeof Component> = {
   component: Component,
@@ -19,7 +20,7 @@ export default meta;
 
 export const ChatHeaderLoaded: ComponentStoryObj<typeof Component> = {
   args: {
-    title: 'My conversation jdhf lskdjhfsdlk jfh sdlkjfh sdlkfjhsd klfjhds flkjdsh flksdjh fl',
+    title: 'My conversation',
     connectors: {
       loading: false,
       selectedConnector: 'gpt-4',
@@ -28,6 +29,18 @@ export const ChatHeaderLoaded: ComponentStoryObj<typeof Component> = {
         { id: 'gpt-3.5-turbo', name: 'OpenAI GPT-3.5 Turbo' },
       ] as FindActionResult[],
       selectConnector: () => {},
+    },
+    knowledgeBase: {
+      status: {
+        loading: false,
+        value: {
+          ready: true,
+        },
+        refresh: () => {},
+      },
+      isInstalling: false,
+      installError: undefined,
+      install: async () => {},
     },
   },
   render: (props) => {
