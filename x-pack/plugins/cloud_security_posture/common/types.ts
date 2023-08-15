6b5b512cb0060d8d169464b59788882a420264d4
Paulo Henrique <paulo.henrique@elastic.co>
2023-08-14 21:55:41 -0700

    revert type change

--- x-pack/plugins/cloud_security_posture/common/types.ts
+++ x-pack/plugins/cloud_security_posture/common/types.ts
@@ -15,8 +15,6 @@ import { getComplianceDashboardSchema } from './schemas/stats';
 
 export type Evaluation = 'passed' | 'failed' | 'NA';
 
-export type Integration_PostureTypes = 'cspm' | 'kspm' | 'vuln_mgmt' | 'all';
-
 export type PostureTypes = 'cspm' | 'kspm' | 'vuln_mgmt' | 'all';
 /** number between 1-100 */
 export type Score = number;
