6446dbf7926a6aa2a947b9f47fe91fa617a6ff79
Coen Warmer <coen.warmer@gmail.com>
2023-08-15 17:28:52 +0200

    Add TechPreview Badge

--- x-pack/plugins/observability_ai_assistant/public/components/chat/chat_header.tsx
+++ x-pack/plugins/observability_ai_assistant/public/components/chat/chat_header.tsx
@@ -19,6 +19,7 @@ import { AssistantAvatar } from '../assistant_avatar';
 import { ConnectorSelectorBase } from '../connector_selector/connector_selector_base';
 import { EMPTY_CONVERSATION_TITLE } from '../../i18n';
 import { KnowledgeBaseCallout } from './knowledge_base_callout';
+import { TechnicalPreviewBadge } from '../technical_preview_badge';
 import { useUnmountAndRemountWhenPropChanges } from '../../hooks/use_unmount_and_remount_when_prop_changes';
 import type { UseGenAIConnectorsResult } from '../../hooks/use_genai_connectors';
 import type { UseKnowledgeBaseResult } from '../../hooks/use_knowledge_base';
@@ -47,6 +48,7 @@ export function ChatHeader({
   const theme = useEuiTheme();
 
   // Component only works uncontrolled at the moment, so need to unmount and remount on prop change.
+  // https://github.com/elastic/eui/issues/7084
   const shouldRender = useUnmountAndRemountWhenPropChanges(displayedTitle);
 
   const inputRef = useRef<HTMLInputElement>(null);
@@ -60,25 +62,37 @@ export function ChatHeader({
         <EuiFlexItem grow className={minWidthClassName}>
           <EuiFlexGroup direction="column" gutterSize="none" className={minWidthClassName}>
             <EuiFlexItem grow={false} className={minWidthClassName}>
-              {shouldRender ? (
-                <EuiInlineEditTitle
-                  heading="h2"
-                  size="s"
-                  defaultValue={displayedTitle}
-                  className={css`
-                    color: ${hasTitle
-                      ? theme.euiTheme.colors.text
-                      : theme.euiTheme.colors.subduedText};
-                  `}
-                  inputAriaLabel={i18n.translate(
-                    'xpack.observabilityAiAssistant.chatHeader.editConversationInput',
-                    { defaultMessage: 'Edit conversation' }
-                  )}
-                  editModeProps={{ inputProps: { inputRef } }}
-                  isReadOnly={!Boolean(onSaveTitle)}
-                  onSave={onSaveTitle}
-                />
-              ) : null}
+              <EuiFlexGroup
+                direction="row"
+                gutterSize="m"
+                className={minWidthClassName}
+                alignItems="center"
+              >
+                <EuiFlexItem grow className={minWidthClassName}>
+                  {shouldRender ? (
+                    <EuiInlineEditTitle
+                      heading="h2"
+                      size="s"
+                      defaultValue={displayedTitle}
+                      className={css`
+                        color: ${hasTitle
+                          ? theme.euiTheme.colors.text
+                          : theme.euiTheme.colors.subduedText};
+                      `}
+                      inputAriaLabel={i18n.translate(
+                        'xpack.observabilityAiAssistant.chatHeader.editConversationInput',
+                        { defaultMessage: 'Edit conversation' }
+                      )}
+                      editModeProps={{ inputProps: { inputRef } }}
+                      isReadOnly={!Boolean(onSaveTitle)}
+                      onSave={onSaveTitle}
+                    />
+                  ) : null}
+                </EuiFlexItem>
+                <EuiFlexItem grow={false} className={minWidthClassName}>
+                  <TechnicalPreviewBadge />
+                </EuiFlexItem>
+              </EuiFlexGroup>
             </EuiFlexItem>
             <EuiFlexItem>
               <KnowledgeBaseCallout knowledgeBase={knowledgeBase} />
