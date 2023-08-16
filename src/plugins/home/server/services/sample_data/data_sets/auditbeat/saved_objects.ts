97806b2195b955597200493f51481b1ba2b88eea
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-08-16 11:12:40 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- src/plugins/home/server/services/sample_data/data_sets/auditbeat/saved_objects.ts
+++ src/plugins/home/server/services/sample_data/data_sets/auditbeat/saved_objects.ts
@@ -8,9 +8,29 @@
 
 /* eslint max-len: 0 */
 
-import { i18n } from '@kbn/i18n';
 import { SavedObject } from '@kbn/core/server';
 
 export const getSavedObjects = (): SavedObject[] => [
-  {"attributes":{"fieldAttrs":"{}","fieldFormatMap":"{}","fields":"[]","name":"Kibana Sample Data Auditbeat","runtimeFieldMap":"{}","sourceFilters":"[]","timeFieldName":"@timestamp","title":"auditbeat-sample-data","typeMeta":"{}"},"coreMigrationVersion":"8.8.0","created_at":"2023-08-16T10:09:47.143Z","id":"c89b196d-d0cd-4cfb-8d95-787e4ce51551","managed":false,"references":[],"type":"index-pattern","typeMigrationVersion":"8.0.0","updated_at":"2023-08-16T10:09:47.143Z","version":"WzQ1LDFd"}
+  {
+    attributes: {
+      fieldAttrs: '{}',
+      fieldFormatMap: '{}',
+      fields: '[]',
+      name: 'Kibana Sample Data Auditbeat',
+      runtimeFieldMap: '{}',
+      sourceFilters: '[]',
+      timeFieldName: '@timestamp',
+      title: 'auditbeat-sample-data',
+      typeMeta: '{}',
+    },
+    coreMigrationVersion: '8.8.0',
+    created_at: '2023-08-16T10:09:47.143Z',
+    id: 'c89b196d-d0cd-4cfb-8d95-787e4ce51551',
+    managed: false,
+    references: [],
+    type: 'index-pattern',
+    typeMigrationVersion: '8.0.0',
+    updated_at: '2023-08-16T10:09:47.143Z',
+    version: 'WzQ1LDFd',
+  },
 ];
