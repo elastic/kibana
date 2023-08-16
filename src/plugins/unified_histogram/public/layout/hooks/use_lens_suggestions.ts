1f6c0045d25d718611b994d8a1953855851aa67b
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2023-08-16 13:24:32 +0300

    Add more unit tests

--- src/plugins/unified_histogram/public/layout/hooks/use_lens_suggestions.ts
+++ src/plugins/unified_histogram/public/layout/hooks/use_lens_suggestions.ts
@@ -18,53 +18,7 @@ import type { DatatableColumn } from '@kbn/expressions-plugin/common';
 import { LensSuggestionsApi, Suggestion } from '@kbn/lens-plugin/public';
 import { isEqual } from 'lodash';
 import { useEffect, useMemo, useRef, useState } from 'react';
-
-const roundInterval = (interval: number) => {
-  {
-    switch (true) {
-      case interval <= 500: // <= 0.5s
-        return '100 millisecond';
-      case interval <= 5000: // <= 5s
-        return '1 second';
-      case interval <= 7500: // <= 7.5s
-        return '5 second';
-      case interval <= 15000: // <= 15s
-        return '10 second';
-      case interval <= 45000: // <= 45s
-        return '30 second';
-      case interval <= 180000: // <= 3m
-        return '1 minute';
-      case interval <= 450000: // <= 9m
-        return '5 minute';
-      case interval <= 1200000: // <= 20m
-        return '10 minute';
-      case interval <= 2700000: // <= 45m
-        return '30 minute';
-      case interval <= 7200000: // <= 2h
-        return '1 hour';
-      case interval <= 21600000: // <= 6h
-        return '3 hour';
-      case interval <= 86400000: // <= 24h
-        return '12 hour';
-      case interval <= 604800000: // <= 1w
-        return '24 hour';
-      case interval <= 1814400000: // <= 3w
-        return '1 week';
-      case interval < 3628800000: // <  2y
-        return '30 day';
-      default:
-        return '1 year';
-    }
-  }
-};
-
-const computeInterval = (timeRange: TimeRange, data: DataPublicPluginStart): string => {
-  const bounds = data.query.timefilter.timefilter.calculateBounds(timeRange!);
-  const min = bounds.min!.valueOf();
-  const max = bounds.max!.valueOf();
-  const interval = (max - min) / 50;
-  return roundInterval(interval);
-};
+import { computeInterval } from './compute_interval';
 
 export const useLensSuggestions = ({
   dataView,
