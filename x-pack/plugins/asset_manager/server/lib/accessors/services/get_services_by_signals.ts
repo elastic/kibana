0d63919122efd1dea893f5890357dd57d207f660
Kevin Lacabane <kevin.lacabane@elastic.co>
2023-08-21 21:06:36 +0200

    [Asset Manager] services endpoint (#164181)
    Closes https://github.com/elastic/kibana/issues/159641
    
    Implements `/assets/services` endpoint that returns service assets found
    in the configured source (signals or assets indices). Consumer can
    provide a `parent` query to filter the returned services. While the
    _assets_ mode supports any kind of parent/depth thanks to its common
    interface, the _signals_ mode only supports host parent for the moment.
    
    1. pull this branch and point it at an oblt-cli created cluster that
    uses cross-cluster search to read from the edge cluster
    2. add the following[1] to your kibana.yml file
    3. hit
    `/api/asset-manager/assets/services?from=<from>&to=<to>&(parent=<host>)?`.
    services should be returned. Add/remove parent query string to filter
    services only running on specific host.
    
    [1]
    ```
    xpack.assetManager:
      alphaEnabled: true
      sourceIndices:
        metrics: remote_cluster:metricbeat*,remote_cluster:metrics-*
        logs: remote_cluster:filebeat*,remote_cluster:logs-*
        traces: remote_cluster:traces-*
        serviceMetrics: remote_cluster:metrics-apm*
        serviceLogs: remote_cluster:logs-apm*
      lockedSource: signals
    ```
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/asset_manager/server/lib/accessors/services/get_services_by_signals.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { Asset } from '../../../../common/types_api';
+import { GetServicesOptionsInjected } from '.';
+import { collectServices } from '../../collectors/services';
+
+export async function getServicesBySignals(
+  options: GetServicesOptionsInjected
+): Promise<{ services: Asset[] }> {
+  const filters = [];
+
+  if (options.parent) {
+    filters.push({
+      bool: {
+        should: [
+          { term: { 'host.name': options.parent } },
+          { term: { 'host.hostname': options.parent } },
+        ],
+        minimum_should_match: 1,
+      },
+    });
+  }
+
+  const { assets } = await collectServices({
+    client: options.esClient,
+    from: options.from,
+    to: options.to,
+    sourceIndices: options.sourceIndices,
+    filters,
+  });
+
+  return { services: assets };
+}
