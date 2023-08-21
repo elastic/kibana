fa701edffb2d2af12f70f98d73854384ac26d1e1
Anton Dosov <anton.dosov@elastic.co>
2023-08-21 11:02:46 +0200

    flaky test

--- x-pack/test_serverless/functional/test_suites/observability/navigation.ts
+++ x-pack/test_serverless/functional/test_suites/observability/navigation.ts
@@ -14,95 +14,97 @@ export default function ({ getPageObject, getService }: FtrProviderContext) {
   const svlCommonNavigation = getPageObject('svlCommonNavigation');
   const browser = getService('browser');
 
-  // eslint-disable-next-line ban/ban
-  describe.only('navigation', function () {
-    before(async () => {
-      await svlObltNavigation.navigateToLandingPage();
-    });
-
-    it('navigate observability sidenav & breadcrumbs', async () => {
-      const expectNoPageReload = await svlCommonNavigation.createNoPageReloadCheck();
-
-      // check serverless search side nav exists
-      await svlCommonNavigation.expectExists();
-      await svlCommonNavigation.breadcrumbs.expectExists();
-      await svlObltOnboardingPage.assertQuickstartBadgeExists();
-
-      // check side nav links
-      await svlCommonNavigation.sidenav.expectSectionOpen('observability_project_nav');
-      await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'observabilityOnboarding' });
-      await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
-        deepLinkId: 'observabilityOnboarding',
-      });
-      await svlCommonNavigation.sidenav.expectSectionClosed('project_settings_project_nav');
-
-      // navigate to discover
-      await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'discover:log-explorer' });
-      await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'discover:log-explorer' });
-      await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
-        deepLinkId: 'discover:log-explorer',
-      });
-      await expect(await browser.getCurrentUrl()).contain('/app/discover');
-
-      // check the aiops subsection
-      await svlCommonNavigation.sidenav.clickLink({ navId: 'aiops' }); // open ai ops subsection
-      await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'ml:anomalyDetection' });
-      await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'ml:anomalyDetection' });
-      await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({ text: 'AIOps' });
-      await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
-        deepLinkId: 'ml:anomalyDetection',
+  for (let i = 0; i < 10; i++) {
+    // eslint-disable-next-line ban/ban
+    describe.only('navigation' + i, function () {
+      before(async () => {
+        await svlObltNavigation.navigateToLandingPage();
       });
 
-      // navigate to a different section
-      await svlCommonNavigation.sidenav.openSection('project_settings_project_nav');
-      await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'management' });
-      await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'management' });
-      await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({ deepLinkId: 'management' });
+      it('navigate observability sidenav & breadcrumbs', async () => {
+        const expectNoPageReload = await svlCommonNavigation.createNoPageReloadCheck();
 
-      // navigate back to serverless oblt overview
-      await svlCommonNavigation.breadcrumbs.clickHome();
-      await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'observabilityOnboarding' });
-      await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
-        deepLinkId: 'observabilityOnboarding',
+        // check serverless search side nav exists
+        await svlCommonNavigation.expectExists();
+        await svlCommonNavigation.breadcrumbs.expectExists();
+        await svlObltOnboardingPage.assertQuickstartBadgeExists();
+
+        // check side nav links
+        await svlCommonNavigation.sidenav.expectSectionOpen('observability_project_nav');
+        await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'observabilityOnboarding' });
+        await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
+          deepLinkId: 'observabilityOnboarding',
+        });
+        await svlCommonNavigation.sidenav.expectSectionClosed('project_settings_project_nav');
+
+        // navigate to discover
+        await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'discover:log-explorer' });
+        await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'discover:log-explorer' });
+        await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
+          deepLinkId: 'discover:log-explorer',
+        });
+        await expect(await browser.getCurrentUrl()).contain('/app/discover');
+
+        // check the aiops subsection
+        await svlCommonNavigation.sidenav.clickLink({ navId: 'aiops' }); // open ai ops subsection
+        await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'ml:anomalyDetection' });
+        await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'ml:anomalyDetection' });
+        await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({ text: 'AIOps' });
+        await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
+          deepLinkId: 'ml:anomalyDetection',
+        });
+
+        // navigate to a different section
+        await svlCommonNavigation.sidenav.openSection('project_settings_project_nav');
+        await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'management' });
+        await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'management' });
+        await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({ deepLinkId: 'management' });
+
+        // navigate back to serverless oblt overview
+        await svlCommonNavigation.breadcrumbs.clickHome();
+        await svlCommonNavigation.sidenav.expectLinkActive({ deepLinkId: 'observabilityOnboarding' });
+        await svlCommonNavigation.breadcrumbs.expectBreadcrumbExists({
+          deepLinkId: 'observabilityOnboarding',
+        });
+        await svlCommonNavigation.sidenav.expectSectionOpen(`project_settings_project_nav`); // remains open
+
+        await expectNoPageReload();
       });
-      await svlCommonNavigation.sidenav.expectSectionOpen(`project_settings_project_nav`); // remains open
 
-      await expectNoPageReload();
-    });
-
-    // FLAKY/BUG?: https://github.com/elastic/kibana/issues/162781
-    it.skip('active sidenav section is auto opened on load', async () => {
-      await svlCommonNavigation.sidenav.openSection('project_settings_project_nav');
-      await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'management' });
-      await browser.refresh();
-      await svlCommonNavigation.expectExists();
-      await svlCommonNavigation.sidenav.expectSectionOpen('project_settings_project_nav');
-    });
-
-    it('navigate using search', async () => {
-      await svlCommonNavigation.search.showSearch();
-      await svlCommonNavigation.search.searchFor('discover log explorer');
-      await svlCommonNavigation.search.clickOnOption(0);
-      await svlCommonNavigation.search.hideSearch();
-
-      await expect(await browser.getCurrentUrl()).contain('/app/discover#/p/log-explorer');
-    });
-
-    it('shows cases in sidebar navigation', async () => {
-      await svlCommonNavigation.expectExists();
-
-      await svlCommonNavigation.sidenav.expectLinkExists({
-        deepLinkId: 'observability-overview:cases',
+      // FLAKY/BUG?: https://github.com/elastic/kibana/issues/162781
+      it('active sidenav section is auto opened on load', async () => {
+        await svlCommonNavigation.sidenav.openSection('project_settings_project_nav');
+        await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'management' });
+        await browser.refresh();
+        await svlCommonNavigation.expectExists();
+        await svlCommonNavigation.sidenav.expectSectionOpen('project_settings_project_nav');
       });
-    });
 
-    it('navigates to cases app', async () => {
-      await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'observability-overview:cases' });
+      it('navigate using search', async () => {
+        await svlCommonNavigation.search.showSearch();
+        await svlCommonNavigation.search.searchFor('discover log explorer');
+        await svlCommonNavigation.search.clickOnOption(0);
+        await svlCommonNavigation.search.hideSearch();
 
-      await svlCommonNavigation.sidenav.expectLinkActive({
-        deepLinkId: 'observability-overview:cases',
+        await expect(await browser.getCurrentUrl()).contain('/app/discover#/p/log-explorer');
       });
-      expect(await browser.getCurrentUrl()).contain('/app/observability/cases');
-    });
-  });
+
+      it('shows cases in sidebar navigation', async () => {
+        await svlCommonNavigation.expectExists();
+
+        await svlCommonNavigation.sidenav.expectLinkExists({
+          deepLinkId: 'observability-overview:cases',
+        });
+      });
+
+      it('navigates to cases app', async () => {
+        await svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'observability-overview:cases' });
+
+        await svlCommonNavigation.sidenav.expectLinkActive({
+          deepLinkId: 'observability-overview:cases',
+        });
+        expect(await browser.getCurrentUrl()).contain('/app/observability/cases');
+      });
+  }
+  }
 }
