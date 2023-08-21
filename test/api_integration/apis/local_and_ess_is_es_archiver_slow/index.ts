50538c3df8a0703d040b5a57a0937ec9a50b310b
Tre' Seymour <wayne.seymour@elastic.co>
2023-08-21 13:35:41 +0100

    fix pointers

--- test/api_integration/apis/local_and_ess_is_es_archiver_slow/index.ts
+++ test/api_integration/apis/local_and_ess_is_es_archiver_slow/index.ts
@@ -10,6 +10,6 @@ import { FtrProviderContext } from '../../../functional/ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('Es Archiver Load Action Performance on Local and ESS', function () {
-    loadTestFile(require.resolve('./local_and_ess_big_loop'));
+    loadTestFile(require.resolve('./es_archiver_benchmark'));
   });
 }
