867594395c40fa102a96d717724b8bcea5a5a90d
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-08-22 13:18:35 +0200

    fix

--- x-pack/plugins/security_solution/public/common/components/event_details/osquery_tab.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/osquery_tab.tsx
@@ -61,7 +61,7 @@ export const useOsqueryTab = ({
   const { OsqueryResults, fetchAllLiveQueries } = osquery;
 
   const { data: actionsData } = fetchAllLiveQueries({
-    kql: `alert_ids: ( ${alertId} )`,
+    kuery: `alert_ids: ( ${alertId} )`,
     alertId,
     skip: shouldEarlyReturn,
   });
