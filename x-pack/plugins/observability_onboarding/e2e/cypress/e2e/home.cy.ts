22fdf23cb3b12f6bc0e1cde58062cf1f9726e61c
Yngrid Coello <yngrid.coello@elastic.co>
2023-08-22 17:50:52 +0200

    e2e for statefull entry points

--- x-pack/plugins/observability_onboarding/e2e/cypress/e2e/home.cy.ts
+++ x-pack/plugins/observability_onboarding/e2e/cypress/e2e/home.cy.ts
@@ -10,6 +10,27 @@ describe('[Observability onboarding] Landing page', () => {
     cy.loginAsElastic();
   });
 
+  describe('Entry point', () => {
+    it('from setup guides', () => {
+      cy.get('[data-test-subj="guideButtonRedirect"]').click();
+      cy.get('[data-test-subj="onboarding--observability--logs"]').click();
+
+      cy.url().should('include', '/app/observabilityOnboarding');
+    });
+
+    it('from observability overview', () => {
+      cy.visitKibana('/app/observability');
+      cy.get('[data-test-subj="observability-onboarding-callout"]').should(
+        'exist'
+      );
+      cy.get(
+        '[data-test-subj="o11yObservabilityOnboardingGetStartedButton"]'
+      ).click();
+
+      cy.url().should('include', '/app/observabilityOnboarding');
+    });
+  });
+
   it('shows landing page', () => {
     cy.visitKibana('/app/observabilityOnboarding');
     cy.contains('Get started with Observability');
