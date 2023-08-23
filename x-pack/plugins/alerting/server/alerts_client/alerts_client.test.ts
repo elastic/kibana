b16931819aa9cdfee3c80d08d40ec8c2ff5f4612
Tiago Costa <tiago.costa@elastic.co>
2023-08-23 18:42:43 +0100

    skip flaky suite (#163195)

--- x-pack/plugins/alerting/server/alerts_client/alerts_client.test.ts
+++ x-pack/plugins/alerting/server/alerts_client/alerts_client.test.ts
@@ -1311,10 +1311,11 @@ describe('Alerts Client', () => {
       expect(clusterClient.bulk).not.toHaveBeenCalled();
     });
   });
-
+  
   // FLAKY: https://github.com/elastic/kibana/issues/163192
   // FLAKY: https://github.com/elastic/kibana/issues/163193
   // FLAKY: https://github.com/elastic/kibana/issues/163194
+  // FLAKY: https://github.com/elastic/kibana/issues/163195
   describe.skip('getSummarizedAlerts', () => {
     beforeEach(() => {
       clusterClient.search.mockReturnValue({
