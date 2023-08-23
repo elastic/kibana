5cefc399aabfd6c2e49d8972735342bd9a684c85
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2023-08-23 07:22:56 -0400

    [8.10] [Security Solution] Fix flaky Event Filters test (#164473) (#164550)
    # Backport
    
    This will backport the following commits from `main` to `8.10`:
    - [[Security Solution] Fix flaky Event Filters test
    (#164473)](https://github.com/elastic/kibana/pull/164473)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Kevin
    Logan","email":"56395104+kevinlog@users.noreply.github.com"},"sourceCommit":{"committedDate":"2023-08-23T10:18:14Z","message":"[Security
    Solution] Fix flaky Event Filters test (#164473)\n\n##
    Summary\r\n\r\nFix flaky test for Event Filters and re-enable. Also,
    needed to adjust a\r\nBlocklist test for a previous bugfix that was
    missed because the suite\r\nwas skipped.\r\n\r\nFlaky test
    runner:\r\nhttps://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/2952\r\n\r\nCloses:
    https://github.com/elastic/kibana/issues/163140\r\n\r\n###
    Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"2dc4e4301426fbf03f10ed98b1541e3d1a1839bc","branchLabelMapping":{"^v8.11.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Team:Defend
    Workflows","v8.10.0","v8.11.0"],"number":164473,"url":"https://github.com/elastic/kibana/pull/164473","mergeCommit":{"message":"[Security
    Solution] Fix flaky Event Filters test (#164473)\n\n##
    Summary\r\n\r\nFix flaky test for Event Filters and re-enable. Also,
    needed to adjust a\r\nBlocklist test for a previous bugfix that was
    missed because the suite\r\nwas skipped.\r\n\r\nFlaky test
    runner:\r\nhttps://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/2952\r\n\r\nCloses:
    https://github.com/elastic/kibana/issues/163140\r\n\r\n###
    Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"2dc4e4301426fbf03f10ed98b1541e3d1a1839bc"}},"sourceBranch":"main","suggestedTargetBranches":["8.10"],"targetPullRequestStates":[{"branch":"8.10","label":"v8.10.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.11.0","labelRegex":"^v8.11.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/164473","number":164473,"mergeCommit":{"message":"[Security
    Solution] Fix flaky Event Filters test (#164473)\n\n##
    Summary\r\n\r\nFix flaky test for Event Filters and re-enable. Also,
    needed to adjust a\r\nBlocklist test for a previous bugfix that was
    missed because the suite\r\nwas skipped.\r\n\r\nFlaky test
    runner:\r\nhttps://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/2952\r\n\r\nCloses:
    https://github.com/elastic/kibana/issues/163140\r\n\r\n###
    Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"2dc4e4301426fbf03f10ed98b1541e3d1a1839bc"}}]}]
    BACKPORT-->
    
    Co-authored-by: Kevin Logan <56395104+kevinlog@users.noreply.github.com>

--- x-pack/test/security_solution_endpoint/apps/integrations/artifact_entries_list.ts
+++ x-pack/test/security_solution_endpoint/apps/integrations/artifact_entries_list.ts
@@ -234,8 +234,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     };
 
     for (const testData of getArtifactsListTestsData()) {
-      // FLAKY: https://github.com/elastic/kibana/issues/163140
-      describe.skip(`When on the ${testData.title} entries list`, function () {
+      describe(`When on the ${testData.title} entries list`, function () {
         beforeEach(async () => {
           policyInfo = await policyTestResources.createPolicy();
           await removeAllArtifacts();
