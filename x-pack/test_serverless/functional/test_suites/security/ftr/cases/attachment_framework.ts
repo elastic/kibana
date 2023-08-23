666a85a3b901096b67196fba63a13c0ce843bf5e
Angela Chuang <yi-chun.chuang@elastic.co>
2023-08-23 12:45:41 +0100

    test

--- x-pack/test_serverless/functional/test_suites/security/ftr/cases/attachment_framework.ts
+++ x-pack/test_serverless/functional/test_suites/security/ftr/cases/attachment_framework.ts
@@ -31,7 +31,6 @@ export default ({ getPageObject, getService }: FtrProviderContext) => {
         await testSubjects.click('solutionSideNavItemLink-dashboards');
 
         await testSubjects.click('createDashboardButton');
-        await dashboard.clickNewDashboard();
 
         await lens.createAndAddLensFromDashboard({});
 
