365a7c6523a1063a9b19fd06dba405bf5db32a2f
Ying <ying.mao@elastic.co>
2023-08-23 15:39:34 -0400

    Using new structure

--- x-pack/plugins/actions/common/routes/connector/response/schemas/latest.ts
+++ x-pack/plugins/actions/common/routes/connector/response/schemas/latest.ts
@@ -5,4 +5,5 @@
  * 2.0.
  */
 
-export { connectorResponseSchema, connectorTypesResponseSchema } from './v1';
+export { connectorResponseSchema } from './v1';
+export { connectorTypesResponseSchema } from './v1';
