98402acaebb5621426d95b9fbb269d5b4cd22421
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-08-24 19:51:13 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/hooks/use_fetch_alerts.test.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/hooks/use_fetch_alerts.test.tsx
@@ -287,7 +287,9 @@ describe('useFetchAlerts', () => {
   });
 
   it('handles search error', () => {
-    const obs$ = throwError('simulated search response error, which could be 1) undefined response, 2) response without rawResponse, or 3) partial response');
+    const obs$ = throwError(
+      'simulated search response error, which could be 1) undefined response, 2) response without rawResponse, or 3) partial response'
+    );
     dataSearchMock.mockReturnValue(obs$);
     const { result } = renderHook(() => useFetchAlerts(args));
 
