b6642b6a71db36011ff140fe543fb5f2fc92c504
Gergő Ábrahám <gergo.abraham@elastic.co>
2023-08-24 14:01:20 +0200

    add `cypress:dw:serverless:*` scripts

---
+++ x-pack/plugins/security_solution/public/management/cypress/cypress_serverless.config.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { defineCypressConfig } from '@kbn/cypress-config';
+
+import { dataLoaders } from './support/data_loaders';
+
+// eslint-disable-next-line import/no-default-export
+export default defineCypressConfig({
+  defaultCommandTimeout: 60000,
+  execTimeout: 120000,
+  pageLoadTimeout: 12000,
+
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+
+  screenshotsFolder:
+    '../../../target/kibana-security-solution/public/management/cypress/screenshots',
+  trashAssetsBeforeRuns: false,
+  video: false,
+  viewportHeight: 900,
+  viewportWidth: 1440,
+  experimentalStudio: true,
+
+  env: {
+    KIBANA_URL: 'http://localhost:5601',
+    ELASTICSEARCH_URL: 'http://localhost:9200',
+    FLEET_SERVER_URL: 'https://localhost:8220',
+    // Username/password used for both elastic and kibana
+    KIBANA_USERNAME: 'elastic',
+    KIBANA_PASSWORD: 'changeme',
+    ELASTICSEARCH_USERNAME: 'system_indices_superuser',
+    ELASTICSEARCH_PASSWORD: 'changeme',
+
+    grepTags: '@serverless --@brokenInServerless',
+  },
+
+  e2e: {
+    // baseUrl: To override, set Env. variable `CYPRESS_BASE_URL`
+    baseUrl: 'http://localhost:5601',
+    supportFile: 'public/management/cypress/support/e2e.ts',
+    specPattern: 'public/management/cypress/e2e/mocked_data/',
+    experimentalRunAllSpecs: true,
+    setupNodeEvents: (on, config) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require('@cypress/grep/src/plugin')(config);
+      return dataLoaders(on, config);
+    },
+  },
+});
