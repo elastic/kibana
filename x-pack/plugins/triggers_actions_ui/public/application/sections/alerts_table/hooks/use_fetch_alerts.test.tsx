c3189d0a4b5ff7563acc64298e2d33925c7621b9
nreese <reese.nathan@elastic.co>
2023-08-24 13:45:31 -0600

    tslint

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/hooks/use_fetch_alerts.test.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/hooks/use_fetch_alerts.test.tsx
@@ -287,7 +287,6 @@ describe('useFetchAlerts', () => {
   });
 
   it('handles search error', () => {
-    // @ts-expect-error
     const obs$ = throwError('simulated search response error, which could be 1) undefined response, 2) response without rawResponse, or 3) partial response');
     dataSearchMock.mockReturnValue(obs$);
     const { result } = renderHook(() => useFetchAlerts(args));
