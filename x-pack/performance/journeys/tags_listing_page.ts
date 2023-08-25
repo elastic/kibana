5485830a9474f21d508ab747630cdaa9f8cb4715
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-08-25 13:02:10 +0200

    remove extra div block

--- x-pack/performance/journeys/tags_listing_page.ts
+++ x-pack/performance/journeys/tags_listing_page.ts
@@ -17,33 +17,33 @@ export const journey = new Journey({
 })
   .step('Go to Tags Page', async ({ page, kbnUrl }) => {
     await page.goto(kbnUrl.get(`/app/management/kibana/tags`));
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
   })
   .step('Delete the first 20 tags', async ({ page }) => {
     await page.click(subj('checkboxSelectAll'));
     await page.click(subj('actionBar-contextMenuButton'));
     await page.click(subj('actionBar-button-delete'));
     await page.click(subj('confirmModalConfirmButton'));
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
   })
   .step(`Search for 'stream' tag`, async ({ page, inputDelays }) => {
     await page.type(subj('tagsManagementSearchBar'), 'stream', {
       delay: inputDelays.TYPING,
     });
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
   })
   .step('Create a new tag', async ({ page, inputDelays, kibanaPage }) => {
     await kibanaPage.clearInput(subj('tagsManagementSearchBar'));
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
     await page.click(subj('createTagButton'));
     await page.type(subj('createModalField-name'), TAG_NAME, { delay: inputDelays.TYPING });
     await kibanaPage.clickAndWaitFor(subj('createModalConfirmButton'), 'detached');
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
     // search for newly created tag
     await page.type(subj('tagsManagementSearchBar'), TAG_NAME, {
       delay: inputDelays.TYPING,
     });
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
     await page.waitForSelector(subj('tagsTableRowName'), { state: 'visible' });
   })
   .step('Update tag', async ({ page, inputDelays, kibanaPage }) => {
@@ -52,13 +52,13 @@ export const journey = new Journey({
       delay: inputDelays.TYPING,
     });
     await kibanaPage.clickAndWaitFor(subj('createModalConfirmButton'), 'detached');
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
   })
   .step('Delete tag', async ({ page }) => {
     const tagRow = page.locator(subj('tagsTableRowName'));
     await page.click(subj('euiCollapsedItemActionsButton'));
     await page.click(subj('tagsTableAction-delete'));
     await page.click(subj('confirmModalConfirmButton'));
-    await page.waitForSelector(subj('table-is-ready'));
+    await page.waitForSelector(subj('tagsManagementTable table-is-ready'));
     await tagRow.waitFor({ state: 'detached' });
   });
--- x-pack/plugins/saved_objects_tagging/public/management/components/table.tsx
+++ x-pack/plugins/saved_objects_tagging/public/management/components/table.tsx
@@ -142,49 +142,47 @@ export const TagTable: FC<TagTableProps> = ({
   const testSubjectState = !loading ? 'table-is-ready' : 'table-is-loading';
 
   return (
-    <div data-test-subj={testSubjectState}>
-      <EuiInMemoryTable
-        data-test-subj="tagsManagementTable"
-        ref={tableRef}
-        childrenBetween={actionBar}
-        loading={loading}
-        itemId={'id'}
-        columns={columns}
-        items={tags}
-        pagination={tablePagination}
-        sorting={sorting}
-        tableCaption={i18n.translate('xpack.savedObjectsTagging.management.table.columns.caption', {
-          defaultMessage: 'Tags',
-        })}
-        rowHeader="name"
-        selection={
-          allowSelection
-            ? {
-                initialSelected: selectedTags,
-                onSelectionChange,
-              }
-            : undefined
-        }
-        search={{
-          defaultQuery: initialQuery,
-          onChange: ({ query }) => {
-            onQueryChange(query || undefined);
-          },
-          box: {
-            'data-test-subj': 'tagsManagementSearchBar',
-            incremental: true,
-            schema: {
-              fields: {
-                name: { type: 'string' },
-                description: { type: 'string' },
-              },
+    <EuiInMemoryTable
+      data-test-subj={`tagsManagementTable ${testSubjectState}`}
+      ref={tableRef}
+      childrenBetween={actionBar}
+      loading={loading}
+      itemId={'id'}
+      columns={columns}
+      items={tags}
+      pagination={tablePagination}
+      sorting={sorting}
+      tableCaption={i18n.translate('xpack.savedObjectsTagging.management.table.columns.caption', {
+        defaultMessage: 'Tags',
+      })}
+      rowHeader="name"
+      selection={
+        allowSelection
+          ? {
+              initialSelected: selectedTags,
+              onSelectionChange,
+            }
+          : undefined
+      }
+      search={{
+        defaultQuery: initialQuery,
+        onChange: ({ query }) => {
+          onQueryChange(query || undefined);
+        },
+        box: {
+          'data-test-subj': 'tagsManagementSearchBar',
+          incremental: true,
+          schema: {
+            fields: {
+              name: { type: 'string' },
+              description: { type: 'string' },
             },
           },
-        }}
-        rowProps={(item) => ({
-          'data-test-subj': 'tagsTableRow',
-        })}
-      />
-    </div>
+        },
+      }}
+      rowProps={(item) => ({
+        'data-test-subj': 'tagsTableRow',
+      })}
+    />
   );
 };
