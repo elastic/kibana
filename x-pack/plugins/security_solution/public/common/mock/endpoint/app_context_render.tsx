007e9c027c89576c9db869d99eb4f1624dc51795
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-08-25 04:18:22 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/security_solution/public/common/mock/endpoint/app_context_render.tsx
+++ x-pack/plugins/security_solution/public/common/mock/endpoint/app_context_render.tsx
@@ -212,13 +212,10 @@ export const createAppRootMockRenderer = (): AppContextTestRender => {
     app: experimentalFeaturesReducer,
   };
 
-  const store = createStore(
-    mockGlobalState,
-    storeReducer,
-    kibanaObservable,
-    storage,
-    [...managementMiddlewareFactory(coreStart, depsStart), middlewareSpy.actionSpyMiddleware]
-  );
+  const store = createStore(mockGlobalState, storeReducer, kibanaObservable, storage, [
+    ...managementMiddlewareFactory(coreStart, depsStart),
+    middlewareSpy.actionSpyMiddleware,
+  ]);
 
   const queryClient = new QueryClient({
     defaultOptions: {
