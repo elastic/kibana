f454366260e467881b423e721a2ad107cdfd74be
miriam.aparicio <miriam.aparicio@gmail.com>
2023-08-25 09:56:32 +0100

    add visual regression test to service map

--- x-pack/plugins/apm/ftr_e2e/cypress/support/commands.ts
+++ x-pack/plugins/apm/ftr_e2e/cypress/support/commands.ts
@@ -8,6 +8,7 @@ import 'cypress-real-events/support';
 import { Interception } from 'cypress/types/net-stubbing';
 import 'cypress-axe';
 import moment from 'moment';
+import '@frsource/cypress-plugin-visual-regression-diff';
 import { AXE_CONFIG, AXE_OPTIONS } from '@kbn/axe-config';
 import { ApmUsername } from '../../../server/test_helpers/create_apm_users/authentication';
 
