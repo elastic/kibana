cd48e354721fe81e824dc8561bdca7608419f210
Timothy Sullivan <tsullivan@elastic.co>
2023-08-25 17:32:33 -0700

    remove close button wrappers in core chrome-browser

--- packages/core/chrome/core-chrome-browser-internal/src/ui/project/header.tsx
+++ packages/core/chrome/core-chrome-browser-internal/src/ui/project/header.tsx
@@ -12,10 +12,7 @@ import {
   EuiHeaderLogo,
   EuiHeaderSection,
   EuiHeaderSectionItem,
-  EuiHeaderSectionItemButton,
-  EuiIcon,
   EuiLoadingSpinner,
-  htmlIdGenerator,
   useEuiTheme,
   EuiThemeComputed,
 } from '@elastic/eui';
@@ -35,8 +32,7 @@ import { MountPoint } from '@kbn/core-mount-utils-browser';
 import { i18n } from '@kbn/i18n';
 import { RedirectAppLinks } from '@kbn/shared-ux-link-redirect-app';
 import { Router } from '@kbn/shared-ux-router';
-import React, { createRef, useCallback, useState } from 'react';
-import useLocalStorage from 'react-use/lib/useLocalStorage';
+import React, { useCallback } from 'react';
 import useObservable from 'react-use/lib/useObservable';
 import { debounceTime, Observable, of } from 'rxjs';
 import { useHeaderActionMenuMounter } from '../header/header_action_menu';
@@ -64,13 +60,6 @@ const getHeaderCss = ({ size }: EuiThemeComputed) => ({
       position: relative;
       left: 4px;
       top: 2px;
-    `,
-  },
-  nav: {
-    toggleNavButton: css`
-      border-right: 1px solid #d3dae6;
-      margin-left: -1px;
-      padding-right: ${size.xs};
     `,
   },
 });
@@ -116,7 +105,6 @@ export interface Props {
   prependBasePath: (url: string) => string;
 }
 
-const LOCAL_STORAGE_IS_OPEN_KEY = 'PROJECT_NAVIGATION_OPEN' as const;
 const LOADING_DEBOUNCE_TIME = 80;
 
 type LogoProps = Pick<Props, 'application' | 'homeHref$' | 'loadingCount$' | 'prependBasePath'> & {
@@ -179,9 +167,6 @@ export const ProjectHeader = ({
   docLinks,
   ...observables
 }: Props) => {
-  const [navId] = useState(htmlIdGenerator()());
-  const [isOpen, setIsOpen] = useLocalStorage(LOCAL_STORAGE_IS_OPEN_KEY, true);
-  const toggleCollapsibleNavRef = createRef<HTMLButtonElement & { euiAnimate: () => void }>();
   const headerActionMenuMounter = useHeaderActionMenuMounter(observables.actionMenu$);
   const projectsUrl = useObservable(observables.projectsUrl$);
   const { euiTheme } = useEuiTheme();
@@ -202,34 +187,9 @@ export const ProjectHeader = ({
         <div id="globalHeaderBars" data-test-subj="headerGlobalNav" className="header__bars">
           <EuiHeader position="fixed" className="header__firstBar">
             <EuiHeaderSection grow={false}>
-              <EuiHeaderSectionItem css={headerCss.nav.toggleNavButton}>
-                <Router history={application.history}>
-                  <ProjectNavigation
-                    isOpen={isOpen!}
-                    closeNav={() => {
-                      setIsOpen(false);
-                      if (toggleCollapsibleNavRef.current) {
-                        toggleCollapsibleNavRef.current.focus();
-                      }
-                    }}
-                    button={
-                      <EuiHeaderSectionItemButton
-                        data-test-subj="toggleNavButton"
-                        aria-label={headerStrings.nav.closeNavAriaLabel}
-                        onClick={() => setIsOpen(!isOpen)}
-                        aria-expanded={isOpen!}
-                        aria-pressed={isOpen!}
-                        aria-controls={navId}
-                        ref={toggleCollapsibleNavRef}
-                      >
-                        <EuiIcon type={isOpen ? 'menuLeft' : 'menuRight'} size="m" />
-                      </EuiHeaderSectionItemButton>
-                    }
-                  >
-                    {children}
-                  </ProjectNavigation>
-                </Router>
-              </EuiHeaderSectionItem>
+              <Router history={application.history}>
+                <ProjectNavigation>{children}</ProjectNavigation>
+              </Router>
 
               <EuiHeaderSectionItem>
                 <Logo
--- packages/core/chrome/core-chrome-browser-internal/src/ui/project/navigation.tsx
+++ packages/core/chrome/core-chrome-browser-internal/src/ui/project/navigation.tsx
@@ -7,54 +7,16 @@
  */
 
 import React from 'react';
-import { css } from '@emotion/react';
-import { EuiCollapsibleNav, EuiCollapsibleNavProps } from '@elastic/eui';
+import { EuiCollapsibleNavBeta } from '@elastic/eui';
 
-const SIZE_EXPANDED = 248;
-const SIZE_COLLAPSED = 0;
-
-export interface ProjectNavigationProps {
-  isOpen: boolean;
-  closeNav: () => void;
-  button: EuiCollapsibleNavProps['button'];
-}
-
-export const ProjectNavigation: React.FC<ProjectNavigationProps> = ({
-  children,
-  isOpen,
-  closeNav,
-  button,
-}) => {
-  const collabsibleNavCSS = css`
-    border-inline-end-width: 1,
-    display: flex,
-    flex-direction: row,
-  `;
-
-  const DOCKED_BREAKPOINT = 's' as const;
-  const isVisible = isOpen;
-
+export const ProjectNavigation: React.FC = ({ children }) => {
   return (
     <>
       {
-        /* must render the tree to initialize the navigation, even if it shouldn't be visible */
-        !isOpen && <div hidden>{children}</div>
+        /* must render the tree to initialize the navigation, even if hidden internally in EUI */
+        <div hidden>{children}</div>
       }
-      <EuiCollapsibleNav
-        className="projectLayoutSideNav"
-        css={collabsibleNavCSS}
-        isOpen={isVisible /* only affects docked state */}
-        showButtonIfDocked={true}
-        onClose={closeNav}
-        isDocked={true}
-        size={isVisible ? SIZE_EXPANDED : SIZE_COLLAPSED}
-        hideCloseButton={false}
-        dockedBreakpoint={DOCKED_BREAKPOINT}
-        ownFocus={false}
-        button={button}
-      >
-        {isOpen && children}
-      </EuiCollapsibleNav>
+      <EuiCollapsibleNavBeta>{children}</EuiCollapsibleNavBeta>
     </>
   );
 };
