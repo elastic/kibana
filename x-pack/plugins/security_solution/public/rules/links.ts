9a4cccbfb227682c2265e05fe8f69038e715af06
Davis Plumlee <56367316+dplumlee@users.noreply.github.com>
2023-08-28 16:39:20 -0400

    [Security Solution] Coverage Overview follow-up 2 (#164986)
    (cherry picked from commit 3835392e329b1a3cc1dba0a1b6f36a36a87c1cfa)

--- x-pack/plugins/security_solution/public/rules/links.ts
+++ x-pack/plugins/security_solution/public/rules/links.ts
@@ -103,7 +103,6 @@ export const links: LinkItem = {
           defaultMessage: 'MITRE ATT&CK Coverage',
         }),
       ],
-      experimentalKey: 'detectionsCoverageOverview',
     },
   ],
   categories: [
