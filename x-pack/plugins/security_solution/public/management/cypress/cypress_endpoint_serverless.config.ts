96681c46089006e0b26e8b61056787a69ddbf812
Gergő Ábrahám <gergo.abraham@elastic.co>
2023-08-29 14:51:05 +0200

    fine tune grep config

--- x-pack/plugins/security_solution/public/management/cypress/cypress_endpoint_serverless.config.ts
+++ x-pack/plugins/security_solution/public/management/cypress/cypress_endpoint_serverless.config.ts
@@ -39,6 +39,8 @@ export default defineCypressConfig({
     ELASTICSEARCH_USERNAME: 'system_indices_superuser',
     ELASTICSEARCH_PASSWORD: 'changeme',
 
+    grepFilterSpecs: true,
+    grepOmitFiltered: true,
     grepTags: '@serverless --@brokenInServerless',
 
     'cypress-react-selector': {
