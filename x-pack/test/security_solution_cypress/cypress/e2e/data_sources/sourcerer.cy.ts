62f581ea2514e46d7c958f8d1237e6a6e8f6e82d
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-08-29 18:43:02 +0200

    [security_solution] Improve Cypress burn logic

--- x-pack/test/security_solution_cypress/cypress/e2e/data_sources/sourcerer.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/data_sources/sourcerer.cy.ts
@@ -5,52 +5,42 @@
  * 2.0.
  */
 
-import {
-  DEFAULT_ALERTS_INDEX,
-  DEFAULT_INDEX_PATTERN,
-} from '@kbn/security-solution-plugin/common/constants';
+import { DEFAULT_INDEX_PATTERN } from '@kbn/security-solution-plugin/common/constants';
 
 import { login, loginWithUser, visit, visitWithUser } from '../../tasks/login';
 
-import { HOSTS_URL, TIMELINES_URL } from '../../urls/navigation';
+import { HOSTS_URL } from '../../urls/navigation';
 import {
   addIndexToDefault,
-  clickAlertCheckbox,
   deselectSourcererOptions,
   isDataViewSelection,
   isHostsStatValue,
   isKibanaDataViewOption,
-  isNotSourcererOption,
   isNotSourcererSelection,
   isSourcererOptions,
   isSourcererSelection,
   openAdvancedSettings,
   openDataViewSelection,
   openSourcerer,
-  refreshUntilAlertsIndexExists,
   resetSourcerer,
   saveSourcerer,
 } from '../../tasks/sourcerer';
 import { postDataView } from '../../tasks/common';
-import { openTimelineUsingToggle } from '../../tasks/security_main';
 import { createUsersAndRoles, secReadCasesAll, secReadCasesAllUser } from '../../tasks/privileges';
 import { TOASTER } from '../../screens/configure_cases';
 import { SOURCERER } from '../../screens/sourcerer';
-import { createTimeline } from '../../tasks/api_calls/timelines';
-import { getTimeline, getTimelineModifiedSourcerer } from '../../objects/timeline';
-import { closeTimeline, openTimelineById } from '../../tasks/timeline';
 
 const usersToCreate = [secReadCasesAllUser];
 const rolesToCreate = [secReadCasesAll];
 const siemDataViewTitle = 'Security Default Data View';
 const dataViews = ['auditbeat-*,fakebeat-*', 'auditbeat-*,*beat*,siem-read*,.kibana*,fakebeat-*'];
 
-describe('Sourcerer', () => {
+describe('Sourcerer', { tags: ['@ess', '@serverless', '@brokenInSserverless'] }, () => {
   before(() => {
     cy.task('esArchiverResetKibana');
     dataViews.forEach((dataView: string) => postDataView(dataView));
   });
-  describe('permissions', { tags: '@ess' }, () => {
+  describe('permissions', () => {
     before(() => {
       createUsersAndRoles(usersToCreate, rolesToCreate);
     });
@@ -61,7 +51,7 @@ describe('Sourcerer', () => {
     });
   });
 
-  describe('Default scope', { tags: ['@ess', '@serverless'] }, () => {
+  describe('Default scope', () => {
     beforeEach(() => {
       cy.clearLocalStorage();
       login();
@@ -110,6 +100,7 @@ describe('Sourcerer', () => {
         openSourcerer();
         cy.get(SOURCERER.badgeModifiedOption).should(`not.exist`);
         isDataViewSelection(siemDataViewTitle);
+        isDataViewSelection(siemDataViewTitle);
       });
     });
 
@@ -136,132 +127,5 @@ describe('Sourcerer', () => {
         isSourcererSelection('*beat*');
       }
     );
-  });
-});
-describe('Timeline scope', { tags: '@brokenInServerless' }, () => {
-  beforeEach(() => {
-    cy.clearLocalStorage();
-    login();
-    visit(TIMELINES_URL);
-  });
-
-  it('correctly loads SIEM data view', () => {
-    openTimelineUsingToggle();
-    openSourcerer('timeline');
-    isDataViewSelection(siemDataViewTitle);
-    openAdvancedSettings();
-    isSourcererSelection(`auditbeat-*`);
-    isSourcererSelection(`${DEFAULT_ALERTS_INDEX}-default`);
-    isSourcererOptions(DEFAULT_INDEX_PATTERN.filter((pattern) => pattern !== 'auditbeat-*'));
-    isNotSourcererOption(`${DEFAULT_ALERTS_INDEX}-default`);
-  });
-
-  describe('Modified badge', () => {
-    it('Selecting new data view does not add a modified badge', () => {
-      openTimelineUsingToggle();
-      cy.get(SOURCERER.badgeModified).should(`not.exist`);
-      openSourcerer('timeline');
-      cy.get(SOURCERER.badgeModifiedOption).should(`not.exist`);
-      openDataViewSelection();
-      isKibanaDataViewOption(dataViews);
-      cy.get(SOURCERER.selectListDefaultOption).should(`contain`, siemDataViewTitle);
-      cy.get(SOURCERER.selectListOption).contains(dataViews[1]).click();
-      isDataViewSelection(dataViews[1]);
-      saveSourcerer();
-      cy.get(SOURCERER.badgeModified).should(`not.exist`);
-      openSourcerer('timeline');
-      cy.get(SOURCERER.badgeModifiedOption).should(`not.exist`);
-    });
-
-    it('shows modified badge when index patterns change and removes when reset', () => {
-      openTimelineUsingToggle();
-      openSourcerer('timeline');
-      openDataViewSelection();
-      cy.get(SOURCERER.selectListOption).contains(dataViews[1]).click();
-      isDataViewSelection(dataViews[1]);
-      openAdvancedSettings();
-      const patterns = dataViews[1].split(',');
-      deselectSourcererOptions([patterns[0]]);
-      saveSourcerer();
-      cy.get(SOURCERER.badgeModified).should(`exist`);
-      openSourcerer('timeline');
-      cy.get(SOURCERER.badgeModifiedOption).should(`exist`);
-      resetSourcerer();
-      saveSourcerer();
-      cy.get(SOURCERER.badgeModified).should(`not.exist`);
-      openSourcerer('timeline');
-      cy.get(SOURCERER.badgeModifiedOption).should(`not.exist`);
-      isDataViewSelection(siemDataViewTitle);
-    });
-  });
-  describe('Alerts checkbox', () => {
-    before(() => {
-      login();
-      createTimeline(getTimeline()).then((response) =>
-        cy.wrap(response.body.data.persistTimeline.timeline.savedObjectId).as('timelineId')
-      );
-      createTimeline(getTimelineModifiedSourcerer()).then((response) =>
-        cy.wrap(response.body.data.persistTimeline.timeline.savedObjectId).as('auditbeatTimelineId')
-      );
-    });
-
-    beforeEach(() => {
-      login();
-      visit(TIMELINES_URL);
-      refreshUntilAlertsIndexExists();
-    });
-
-    it('Modifies timeline to alerts only, and switches to different saved timeline without issue', function () {
-      openTimelineById(this.timelineId).then(() => {
-        cy.get(SOURCERER.badgeAlerts).should(`not.exist`);
-        cy.get(SOURCERER.badgeModified).should(`not.exist`);
-        openSourcerer('timeline');
-        clickAlertCheckbox();
-        saveSourcerer();
-        cy.get(SOURCERER.badgeAlerts).should(`exist`);
-        cy.get(SOURCERER.badgeModified).should(`not.exist`);
-        closeTimeline();
-
-        openTimelineById(this.auditbeatTimelineId).then(() => {
-          cy.get(SOURCERER.badgeModified).should(`exist`);
-          cy.get(SOURCERER.badgeAlerts).should(`not.exist`);
-          openSourcerer('timeline');
-          openAdvancedSettings();
-          isSourcererSelection(`auditbeat-*`);
-        });
-      });
-    });
-
-    const defaultPatterns = [`auditbeat-*`, `${DEFAULT_ALERTS_INDEX}-default`];
-    it('alerts checkbox behaves as expected', () => {
-      isDataViewSelection(siemDataViewTitle);
-      defaultPatterns.forEach((pattern) => isSourcererSelection(pattern));
-      openDataViewSelection();
-      cy.get(SOURCERER.selectListOption).contains(dataViews[1]).click();
-      isDataViewSelection(dataViews[1]);
-      dataViews[1]
-        .split(',')
-        .filter((pattern) => pattern !== 'fakebeat-*' && pattern !== 'siem-read*')
-        .forEach((pattern) => isSourcererSelection(pattern));
-
-      clickAlertCheckbox();
-      isNotSourcererSelection(`auditbeat-*`);
-      isSourcererSelection(`${DEFAULT_ALERTS_INDEX}-default`);
-      cy.get(SOURCERER.alertCheckbox).uncheck({ force: true });
-      defaultPatterns.forEach((pattern) => isSourcererSelection(pattern));
-    });
-
-    it('shows alerts badge when index patterns change and removes when reset', () => {
-      clickAlertCheckbox();
-      saveSourcerer();
-      cy.get(SOURCERER.badgeAlerts).should(`exist`);
-      openSourcerer('timeline');
-      cy.get(SOURCERER.badgeAlertsOption).should(`exist`);
-      resetSourcerer();
-      saveSourcerer();
-      cy.get(SOURCERER.badgeAlerts).should(`not.exist`);
-      openSourcerer('timeline');
-      cy.get(SOURCERER.badgeAlertsOption).should(`not.exist`);
-    });
   });
 });
