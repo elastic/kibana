4a50874f6956a24edc31716862cce603870e8f7e
lcawl <lcawley@elastic.co>
2023-08-30 11:50:25 -0700

    Automate opsgenie-params-test.png

--- x-pack/test/screenshot_creation/apps/response_ops_docs/stack_connectors/connector_types.ts
+++ x-pack/test/screenshot_creation/apps/response_ops_docs/stack_connectors/connector_types.ts
@@ -177,7 +177,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await pageObjects.header.waitUntilLoadingHasFinished();
       await actions.common.openNewConnectorForm('opsgenie');
       await testSubjects.setValue('nameInput', 'Opsgenie test connector');
+      await testSubjects.setValue('secrets.apiKey-input', 'testkey');
       await commonScreenshots.takeScreenshot('opsgenie-connector', screenshotDirectories);
+      await testSubjects.click('create-connector-flyout-save-test-btn');
+      await testSubjects.click('toastCloseButton');
+      await commonScreenshots.takeScreenshot('opsgenie-params-test', screenshotDirectories);
       await testSubjects.click('euiFlyoutCloseButton');
     });
   });
