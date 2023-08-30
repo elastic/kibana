5b3f9e53377eb7415d035b4dac69d220e3f32fe2
Gergő Ábrahám <gergo.abraham@elastic.co>
2023-08-30 12:53:18 +0200

    add `@brokenInServerless` to all "real endpoint" tests, until we got a workaround

--- x-pack/plugins/security_solution/public/management/cypress/e2e/endpoint/artifacts.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/endpoint/artifacts.cy.ts
@@ -36,7 +36,7 @@ const yieldAppliedEndpointRevision = (): Cypress.Chainable<number> =>
 
 const parseRevNumber = (revString: string) => Number(revString.match(/\d+/)?.[0]);
 
-describe('Artifact pages', { tags: ['@ess', '@serverless'] }, () => {
+describe('Artifact pages', { tags: ['@ess', '@serverless', '@brokenInServerless'] }, () => {
   let indexedPolicy: IndexedFleetEndpointPolicyResponse;
   let policy: PolicyData;
   let createdHost: CreateAndEnrollEndpointHostResponse;
