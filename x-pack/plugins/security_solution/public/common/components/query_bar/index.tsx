f55945c9c2eb7e7398fd26d6c94eb38eae173e0e
Devin Hurley <devin.hurley@elastic.co>
2023-08-30 16:01:07 -0400

    updates tests, creates data view correctly

--- x-pack/plugins/security_solution/public/common/components/query_bar/index.tsx
+++ x-pack/plugins/security_solution/public/common/components/query_bar/index.tsx
@@ -15,6 +15,8 @@ import { DataView } from '@kbn/data-views-plugin/public';
 import type { SearchBarProps } from '@kbn/unified-search-plugin/public';
 import { SearchBar } from '@kbn/unified-search-plugin/public';
 import { Storage } from '@kbn/kibana-utils-plugin/public';
+import type { DataViewFieldMap } from '@kbn/data-views-plugin/common';
+
 import { useKibana } from '../../lib/kibana';
 
 export interface QueryBarComponentProps {
@@ -57,7 +59,8 @@ export const QueryBar = memo<QueryBarComponentProps>(
     displayStyle,
     isDisabled,
   }) => {
-    const { fieldFormats } = useKibana().services;
+    const { data, fieldFormats } = useKibana().services;
+    // const [dataView, setDataView] = useState([indexPattern] as DataView[]);
     const onQuerySubmit = useCallback(
       (payload: { dateRange: TimeRange; query?: Query }) => {
         if (payload.query != null && !deepEqual(payload.query, filterQuery)) {
@@ -104,10 +107,38 @@ export const QueryBar = memo<QueryBarComponentProps>(
       [filterManager]
     );
 
-    const indexPatterns = useMemo(
-      () => [new DataView({ ...indexPattern, fieldFormats })],
-      [fieldFormats, indexPattern]
-    );
+    // useEffect(() => {
+    //   if (Object.hasOwn(indexPattern, 'getName')) {
+    //     setDataView([indexPattern] as DataView[]);
+    //   } else {
+    //     const createDataView = async () => {
+    //       const dv = await data.dataViews.create({ title: indexPattern.title });
+    //       setDataView([dv]);
+    //     };
+    //     createDataView();
+    //   }
+    // }, [data.dataViews, indexPattern]);
+
+    const dataView = useMemo(() => {
+      if (Object.hasOwn(indexPattern, 'getName')) {
+        return [indexPattern] as DataView[];
+      }
+      const dv = new DataView({
+        spec: {
+          ...indexPattern,
+          fields: indexPattern.fields.reduce((acc, field) => {
+            // @ts-expect-error missing 'searchable' , aggregatable properties
+            // this is okay because it's better than having the whole app
+            // explode
+            acc[field.name] = field;
+            return acc;
+          }, {} as DataViewFieldMap),
+        },
+        fieldFormats,
+      });
+      return [dv];
+    }, [indexPattern, fieldFormats]);
+
     const timeHistory = useMemo(() => new TimeHistory(new Storage(localStorage)), []);
 
     return (
@@ -116,7 +147,7 @@ export const QueryBar = memo<QueryBarComponentProps>(
         dateRangeFrom={dateRangeFrom}
         dateRangeTo={dateRangeTo}
         filters={filters}
-        indexPatterns={indexPatterns as DataView[]}
+        indexPatterns={dataView}
         isLoading={isLoading}
         isRefreshPaused={isRefreshPaused}
         query={filterQuery}
