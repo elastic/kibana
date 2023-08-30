b192889fd4a46d118d0f948ec3c7548f53e609ca
Felix Stürmer <felix.stuermer@elastic.co>
2023-08-30 09:28:59 +0000

    Add comment to conditional config key in test

--- test/plugin_functional/test_suites/core_plugins/rendering.ts
+++ test/plugin_functional/test_suites/core_plugins/rendering.ts
@@ -292,7 +292,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
         'xpack.observability.unsafe.alertDetails.observability.enabled (boolean)',
         'xpack.observability.unsafe.thresholdRule.enabled (boolean)',
         'xpack.observability_onboarding.ui.enabled (boolean)',
-        'xpack.observabilityLogExplorer.navigation.showAppLink (any)',
+        'xpack.observabilityLogExplorer.navigation.showAppLink (any)', // conditional, is actually a boolean
       ];
       // We don't assert that actualExposedConfigKeys and expectedExposedConfigKeys are equal, because test failure messages with large
       // arrays are hard to grok. Instead, we take the difference between the two arrays and assert them separately, that way it's
