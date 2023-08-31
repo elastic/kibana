5cd7abf0f5060f41f07d81f50cc30d3be524e062
lcawl <lcawley@elastic.co>
2023-08-31 13:41:27 -0700

    [DOCS] Automate PagerDuty connector screenshots

--- x-pack/test/screenshot_creation/apps/response_ops_docs/stack_connectors/connector_types.ts
+++ x-pack/test/screenshot_creation/apps/response_ops_docs/stack_connectors/connector_types.ts
@@ -171,5 +171,14 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await commonScreenshots.takeScreenshot('webhook-params-test', screenshotDirectories);
       await testSubjects.click('euiFlyoutCloseButton');
     });
+
+    it('pagerduty connector screenshots', async () => {
+      await pageObjects.common.navigateToApp('connectors');
+      await pageObjects.header.waitUntilLoadingHasFinished();
+      await actions.common.openNewConnectorForm('pagerduty');
+      await testSubjects.setValue('nameInput', 'PagerDuty test connector');
+      await commonScreenshots.takeScreenshot('pagerduty-connector', screenshotDirectories);
+      await testSubjects.click('euiFlyoutCloseButton');
+    });
   });
 }
