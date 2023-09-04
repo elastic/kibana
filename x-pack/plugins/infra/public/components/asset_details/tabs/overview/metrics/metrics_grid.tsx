88d7a48b3e9820efc55d5f8fc9dec9239a93d6f7
Jenny <dzheni.pavlova@elastic.co>
2023-09-04 18:08:56 +0200

    Metrics Grid refactoring

--- x-pack/plugins/infra/public/components/asset_details/tabs/overview/metrics/metrics_grid.tsx
+++ x-pack/plugins/infra/public/components/asset_details/tabs/overview/metrics/metrics_grid.tsx
@@ -11,6 +11,7 @@ import type { DataView } from '@kbn/data-views-plugin/public';
 import type { TimeRange } from '@kbn/es-query';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { LensEmbeddableInput } from '@kbn/lens-plugin/public';
+import type { XYConfig } from '../../../../../common/visualizations/lens/dashboards/asset_details/metric_charts/types';
 import {
   nginxStubstatusMetrics,
   nginxAccessMetrics,
@@ -34,212 +35,20 @@ interface CompactProps {
   metricsDataView?: DataView;
   logsDataView?: DataView;
 }
+
+interface MetricsChartsWrapperProps {
+  nodeName: string;
+  timeRange: TimeRange;
+  metricsDataView?: DataView;
+  logsDataView?: DataView;
+  charts: XYConfig[];
+  SectionTitle: React.FunctionComponent;
+}
+
 interface Props extends CompactProps {
   showNginxStubstatus: boolean;
   showNginxAccess: boolean;
 }
-
-export const MetricsGrid = React.memo(
-  ({
-    nodeName,
-    metricsDataView,
-    logsDataView,
-    timeRange,
-    showNginxStubstatus,
-    showNginxAccess,
-  }: Props) => {
-    const { setDateRange } = useDateRangeProviderContext();
-    const getDataView = useCallback(
-      (dataViewOrigin: DataViewOrigin) => {
-        return dataViewOrigin === 'metrics' ? metricsDataView : logsDataView;
-      },
-      [logsDataView, metricsDataView]
-    );
-
-    const nginxStubstatusCharts = showNginxStubstatus ? nginxStubstatusMetrics : [];
-    const nginxAccessCharts = showNginxAccess ? nginxAccessMetrics : [];
-    const shouldShowNginxSection = nginxStubstatusCharts.length > 0 || nginxAccessCharts.length > 0;
-
-    const getFilters = useCallback(
-      (dataViewOrigin: DataViewOrigin) => {
-        return [
-          buildCombinedHostsFilter({
-            field: 'host.name',
-            values: [nodeName],
-            dataView: getDataView(dataViewOrigin),
-          }),
-        ];
-      },
-      [getDataView, nodeName]
-    );
-
-    const handleBrushEnd = useCallback(
-      ({ range, preventDefault }: BrushEndArgs) => {
-        setDateRange({
-          from: new Date(range[0]).toISOString(),
-          to: new Date(range[1]).toISOString(),
-        });
-
-        preventDefault();
-      },
-      [setDateRange]
-    );
-
-    return (
-      <EuiFlexGroup gutterSize="m" direction="column">
-        <EuiFlexItem grow={false}>
-          <MetricsSectionTitle />
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiSpacer size="s" />
-          <EuiFlexGrid
-            columns={2}
-            gutterSize="s"
-            data-test-subj="infraAssetDetailsMetricsChartGrid"
-          >
-            {[...assetDetailsDashboards.host.hostMetricChartsFullPage].map(
-              ({ dataViewOrigin, id, layers, title, overrides }, index) => (
-                <EuiFlexItem key={index} grow={false}>
-                  <LensChart
-                    id={`infraAssetDetailsMetricsChart${id}`}
-                    borderRadius="m"
-                    dataView={getDataView(dataViewOrigin)}
-                    dateRange={timeRange}
-                    height={METRIC_CHART_HEIGHT}
-                    visualOptions={XY_MISSING_VALUE_DOTTED_LINE_CONFIG}
-                    layers={layers}
-                    filters={getFilters(dataViewOrigin)}
-                    title={title}
-                    overrides={overrides}
-                    visualizationType="lnsXY"
-                    onBrushEnd={handleBrushEnd}
-                  />
-                </EuiFlexItem>
-              )
-            )}
-          </EuiFlexGrid>
-        </EuiFlexItem>
-        {shouldShowNginxSection && (
-          <>
-            <EuiFlexItem grow={false}>
-              <EuiTitle size="xxs">
-                <span>
-                  <FormattedMessage
-                    id="xpack.infra.assetDetails.overview.nginxMetricsSectionTitle"
-                    defaultMessage="Nginx Metrics"
-                  />
-                </span>
-              </EuiTitle>
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <EuiSpacer size="s" />
-              <EuiFlexGrid
-                columns={2}
-                gutterSize="s"
-                data-test-subj="infraAssetDetailsNginxMetricsChartGrid"
-              >
-                {[...nginxStubstatusCharts, ...nginxAccessCharts].map(
-                  ({ dataViewOrigin, id, layers, title, overrides }, index) => (
-                    <EuiFlexItem key={index} grow={false}>
-                      <LensChart
-                        id={`infraAssetDetailsNginxMetricsChart${id}`}
-                        borderRadius="m"
-                        dataView={getDataView(dataViewOrigin)}
-                        dateRange={timeRange}
-                        height={METRIC_CHART_HEIGHT}
-                        visualOptions={XY_MISSING_VALUE_DOTTED_LINE_CONFIG}
-                        layers={layers}
-                        filters={getFilters(dataViewOrigin)}
-                        title={title}
-                        overrides={overrides}
-                        visualizationType="lnsXY"
-                        onBrushEnd={handleBrushEnd}
-                      />
-                    </EuiFlexItem>
-                  )
-                )}
-              </EuiFlexGrid>
-            </EuiFlexItem>
-          </>
-        )}
-      </EuiFlexGroup>
-    );
-  }
-);
-
-export const MetricsGridCompact = React.memo(
-  ({ nodeName, metricsDataView, logsDataView, timeRange }: CompactProps) => {
-    const { setDateRange } = useDateRangeProviderContext();
-    const getDataView = useCallback(
-      (dataViewOrigin: DataViewOrigin) => {
-        return dataViewOrigin === 'metrics' ? metricsDataView : logsDataView;
-      },
-      [logsDataView, metricsDataView]
-    );
-
-    const getFilters = useCallback(
-      (dataViewOrigin: DataViewOrigin) => {
-        return [
-          buildCombinedHostsFilter({
-            field: 'host.name',
-            values: [nodeName],
-            dataView: getDataView(dataViewOrigin),
-          }),
-        ];
-      },
-      [getDataView, nodeName]
-    );
-
-    const handleBrushEnd = useCallback(
-      ({ range, preventDefault }: BrushEndArgs) => {
-        setDateRange({
-          from: new Date(range[0]).toISOString(),
-          to: new Date(range[1]).toISOString(),
-        });
-
-        preventDefault();
-      },
-      [setDateRange]
-    );
-
-    return (
-      <EuiFlexGroup gutterSize="m" direction="column">
-        <EuiFlexItem grow={false}>
-          <MetricsSectionTitle />
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiSpacer size="s" />
-          <EuiFlexGrid
-            columns={2}
-            gutterSize="s"
-            data-test-subj="infraAssetDetailsMetricsChartGrid"
-          >
-            {assetDetailsDashboards.host.hostMetricCharts.map(
-              ({ dataViewOrigin, id, layers, title, overrides }, index) => (
-                <EuiFlexItem key={index} grow={false}>
-                  <LensChart
-                    id={`infraAssetDetailsMetricsChart${id}`}
-                    borderRadius="m"
-                    dataView={getDataView(dataViewOrigin)}
-                    dateRange={timeRange}
-                    height={METRIC_CHART_HEIGHT}
-                    visualOptions={XY_MISSING_VALUE_DOTTED_LINE_CONFIG}
-                    layers={layers}
-                    filters={getFilters(dataViewOrigin)}
-                    title={title}
-                    overrides={overrides}
-                    visualizationType="lnsXY"
-                    onBrushEnd={handleBrushEnd}
-                  />
-                </EuiFlexItem>
-              )
-            )}
-          </EuiFlexGrid>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    );
-  }
-);
 
 const MetricsSectionTitle = () => {
   return (
@@ -262,3 +71,148 @@ const MetricsSectionTitle = () => {
     </EuiFlexGroup>
   );
 };
+
+const NginxMetricsSectionTitle = () => (
+  <EuiTitle size="xxs">
+    <span>
+      <FormattedMessage
+        id="xpack.infra.assetDetails.overview.nginxMetricsSectionTitle"
+        defaultMessage="Nginx Metrics"
+      />
+    </span>
+  </EuiTitle>
+);
+
+export const MetricsGrid = React.memo(
+  ({
+    nodeName,
+    metricsDataView,
+    logsDataView,
+    timeRange,
+    showNginxStubstatus,
+    showNginxAccess,
+  }: Props) => {
+    const nginxStubstatusCharts = showNginxStubstatus ? nginxStubstatusMetrics : [];
+    const nginxAccessCharts = showNginxAccess ? nginxAccessMetrics : [];
+    const shouldShowNginxSection = nginxStubstatusCharts.length > 0 || nginxAccessCharts.length > 0;
+
+    return (
+      <>
+        <MetricsChartsWrapper
+          nodeName={nodeName}
+          timeRange={timeRange}
+          charts={assetDetailsDashboards.host.hostMetricChartsFullPage}
+          SectionTitle={MetricsSectionTitle}
+          metricsDataView={metricsDataView}
+          logsDataView={logsDataView}
+        />
+        <EuiSpacer size="s" />
+        {shouldShowNginxSection && (
+          <MetricsChartsWrapper
+            nodeName={nodeName}
+            timeRange={timeRange}
+            charts={[...nginxStubstatusCharts, ...nginxAccessCharts]}
+            SectionTitle={NginxMetricsSectionTitle}
+            metricsDataView={metricsDataView}
+            logsDataView={logsDataView}
+          />
+        )}
+      </>
+    );
+  }
+);
+
+export const MetricsGridCompact = ({
+  nodeName,
+  metricsDataView,
+  logsDataView,
+  timeRange,
+}: CompactProps) => (
+  <MetricsChartsWrapper
+    nodeName={nodeName}
+    timeRange={timeRange}
+    charts={assetDetailsDashboards.host.hostMetricCharts}
+    SectionTitle={MetricsSectionTitle}
+    metricsDataView={metricsDataView}
+    logsDataView={logsDataView}
+  />
+);
+
+const MetricsChartsWrapper = React.memo(
+  ({
+    nodeName,
+    metricsDataView,
+    logsDataView,
+    timeRange,
+    charts,
+    SectionTitle,
+  }: MetricsChartsWrapperProps) => {
+    const { setDateRange } = useDateRangeProviderContext();
+    const getDataView = useCallback(
+      (dataViewOrigin: DataViewOrigin) => {
+        return dataViewOrigin === 'metrics' ? metricsDataView : logsDataView;
+      },
+      [logsDataView, metricsDataView]
+    );
+
+    const getFilters = useCallback(
+      (dataViewOrigin: DataViewOrigin) => {
+        return [
+          buildCombinedHostsFilter({
+            field: 'host.name',
+            values: [nodeName],
+            dataView: getDataView(dataViewOrigin),
+          }),
+        ];
+      },
+      [getDataView, nodeName]
+    );
+
+    const handleBrushEnd = useCallback(
+      ({ range, preventDefault }: BrushEndArgs) => {
+        setDateRange({
+          from: new Date(range[0]).toISOString(),
+          to: new Date(range[1]).toISOString(),
+        });
+
+        preventDefault();
+      },
+      [setDateRange]
+    );
+
+    return (
+      <EuiFlexGroup gutterSize="m" direction="column">
+        <EuiFlexItem grow={false}>
+          <SectionTitle />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiSpacer size="s" />
+          <EuiFlexGrid
+            columns={2}
+            gutterSize="s"
+            data-test-subj="infraAssetDetailsMetricsChartGrid"
+          >
+            {charts.map(({ dataViewOrigin, id, layers, title, overrides }, index) => (
+              <EuiFlexItem key={index} grow={false}>
+                <LensChart
+                  id={`infraAssetDetailsMetricsChart${id}`}
+                  borderRadius="m"
+                  dataView={getDataView(dataViewOrigin)}
+                  dateRange={timeRange}
+                  height={METRIC_CHART_HEIGHT}
+                  visualOptions={XY_MISSING_VALUE_DOTTED_LINE_CONFIG}
+                  layers={layers}
+                  filters={getFilters(dataViewOrigin)}
+                  title={title}
+                  overrides={overrides}
+                  visualizationType="lnsXY"
+                  onBrushEnd={handleBrushEnd}
+                />
+              </EuiFlexItem>
+            ))}
+          </EuiFlexGrid>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  }
+);
