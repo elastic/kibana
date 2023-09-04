107500acc102589455f7faf06cca31cd8a01106a
Coen Warmer <coen.warmer@gmail.com>
2023-09-04 14:55:45 +0200

    Add max height of 200 to text area

--- x-pack/plugins/observability_ai_assistant/public/components/chat/chat_prompt_editor.tsx
+++ x-pack/plugins/observability_ai_assistant/public/components/chat/chat_prompt_editor.tsx
@@ -238,6 +238,7 @@ export function ChatPromptEditor({
                 </EuiPanel>
               ) : (
                 <EuiTextArea
+                  css={{ maxHeight: 200 }}
                   disabled={disabled}
                   fullWidth
                   inputRef={textAreaRef}
