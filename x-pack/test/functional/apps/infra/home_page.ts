1a7f88cba0944b5e9a8e3b7b782b05cd9c4a59ea
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2023-09-04 17:06:39 +0200

    Unskips test

--- x-pack/test/functional/apps/infra/home_page.ts
+++ x-pack/test/functional/apps/infra/home_page.ts
@@ -15,9 +15,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const esArchiver = getService('esArchiver');
   const pageObjects = getPageObjects(['common', 'infraHome', 'infraSavedViews']);
 
-  // Failing: See https://github.com/elastic/kibana/issues/164452
-  // Failing: See https://github.com/elastic/kibana/issues/157767
-  describe.skip('Home page', function () {
+  describe('Home page', function () {
     this.tags('includeFirefox');
     before(async () => {
       await esArchiver.load('x-pack/test/functional/es_archives/empty_kibana');
