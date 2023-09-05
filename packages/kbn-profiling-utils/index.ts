4ccdfac816952d10b8f274bfd08cddfd73c31c9b
Caue Marcondes <caue.marcondes@elastic.co>
2023-09-05 10:48:24 +0100

    fixing export issue

--- packages/kbn-profiling-utils/index.ts
+++ packages/kbn-profiling-utils/index.ts
@@ -27,8 +27,13 @@ export {
 export { getFieldNameForTopNType, TopNType, StackTracesDisplayOption } from './common/stack_traces';
 export { createFrameGroupID } from './common/frame_group';
 
-export type { ProfilingStatusResponse, StackTraceResponse } from './common/stack_traces';
-export type { ElasticFlameGraph } from './common/flamegraph';
+export type { CalleeTree } from './common/callee';
+export type {
+  ProfilingStatusResponse,
+  StackTraceResponse,
+  DecodedStackTraceResponse,
+} from './common/stack_traces';
+export type { ElasticFlameGraph, BaseFlameGraph } from './common/flamegraph';
 export type { FrameGroupID } from './common/frame_group';
 export type {
   Executable,
