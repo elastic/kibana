cf1f1bc1cacf85d413c6629a38010e2503ac8866
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-09-06 19:23:27 +0200

    [Discover] Fix drag handle theme

--- packages/kbn-unified-field-list/src/containers/unified_field_list_item/field_list_item_handle.tsx
+++ packages/kbn-unified-field-list/src/containers/unified_field_list_item/field_list_item_handle.tsx
@@ -9,22 +9,41 @@
 import React from 'react';
 import { EuiIcon } from '@elastic/eui';
 import { css } from '@emotion/react';
-import dragHandleIcon from './drag_handle_icon.svg';
 
 /**
  * A temporary icon for the field list drag handle
  * @constructor
  */
 export const FieldListItemHandle: React.FC = () => {
+  // TODO: replace with a new Eui icon type once it's available and remove svg
   return (
     <div
       css={css`
-        transform: translateY(-1px);
         pointer-events: none;
+        padding-top: 3px;
       `}
     >
-      {/* TODO: replace with a new Eui icon type once it's available and remove svg file */}
-      <EuiIcon type={dragHandleIcon} size="original" />
+      <EuiIcon type={DragHandleIcon} size="original" />
     </div>
   );
 };
+
+const DragHandleIcon = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    pointerEvents="none"
+  >
+    <path d="M10 3H9V4H10V3Z" fill="currentColor" />
+    <path d="M9 6H10V7H9V6Z" fill="currentColor" />
+    <path d="M9 9H10V10H9V9Z" fill="currentColor" />
+    <path d="M9 12H10V13H9V12Z" fill="currentColor" />
+    <path d="M7 3H6V4H7V3Z" fill="currentColor" />
+    <path d="M7 6H6V7H7V6Z" fill="currentColor" />
+    <path d="M7 9H6V10H7V9Z" fill="currentColor" />
+    <path d="M7 12H6V13H7V12Z" fill="currentColor" />
+  </svg>
+);
