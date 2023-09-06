ec3b875fda40b170f849882eff3dff10619f63f4
Tiago Costa <tiago.costa@elastic.co>
2023-09-06 17:03:38 +0100

    skip flaky suite (#165624)

--- x-pack/test_serverless/functional/test_suites/common/examples/search/warnings.ts
+++ x-pack/test_serverless/functional/test_suites/common/examples/search/warnings.ts
@@ -28,6 +28,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   // FLAKY: https://github.com/elastic/kibana/issues/165379
   // FLAKY: https://github.com/elastic/kibana/issues/165502
   // FLAKY: https://github.com/elastic/kibana/issues/165503
+  // FLAKY: https://github.com/elastic/kibana/issues/165624
   describe.skip('handling warnings with search source fetch', function () {
     const dataViewTitle = 'sample-01,sample-01-rollup';
     const fromTime = 'Jun 17, 2022 @ 00:00:00.000';
