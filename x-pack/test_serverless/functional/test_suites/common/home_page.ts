86a757e80f8001efbbeb9ef8aedd4b866d370062
Anton Dosov <anton.dosov@elastic.co>
2023-09-06 12:54:04 +0200

    unskip
    fix https://github.com/elastic/kibana/issues/165386

--- x-pack/test_serverless/functional/test_suites/common/home_page.ts
+++ x-pack/test_serverless/functional/test_suites/common/home_page.ts
@@ -11,8 +11,7 @@ export default function ({ getPageObject, getService }: FtrProviderContext) {
   const svlCommonPage = getPageObject('svlCommonPage');
   const svlCommonNavigation = getService('svlCommonNavigation');
 
-  // Failing: See https://github.com/elastic/kibana/issues/165386
-  describe.skip('home page', function () {
+  describe('home page', function () {
     it('has project header', async () => {
       await svlCommonNavigation.navigateToKibanaHome();
       await svlCommonPage.assertProjectHeaderExists();
