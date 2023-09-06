1bc99c142c4b457b6efbd285805856788e385680
lcawl <lcawley@elastic.co>
2023-09-06 15:30:35 -0700

    [DOCS] Automate xmatters-connector-basic.png

---
+++ x-pack/test/screenshot_creation/apps/response_ops_docs/stack_connectors/xmatters_connector.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { FtrProviderContext } from '../../../ftr_provider_context';
+
+export default function ({ getService, getPageObjects }: FtrProviderContext) {
+  const commonScreenshots = getService('commonScreenshots');
+  const screenshotDirectories = ['response_ops_docs', 'stack_connectors'];
+  const pageObjects = getPageObjects(['common', 'header']);
+  const actions = getService('actions');
+  const testSubjects = getService('testSubjects');
+
+  describe('xmatters connector', function () {
+    beforeEach(async () => {
+      await pageObjects.common.navigateToApp('connectors');
+      await pageObjects.header.waitUntilLoadingHasFinished();
+    });
+
+    it('generative ai connector screenshots', async () => {
+      await pageObjects.common.navigateToApp('connectors');
+      await pageObjects.header.waitUntilLoadingHasFinished();
+      await actions.common.openNewConnectorForm('xmatters');
+      await testSubjects.setValue('nameInput', 'xMatters test connector');
+      await commonScreenshots.takeScreenshot('xmatters-connector-basic', screenshotDirectories);
+      // await testSubjects.click('create-connector-flyout-save-test-btn');
+      // await testSubjects.click('toastCloseButton');
+      // const editor = await testSubjects.find('kibanaCodeEditor');
+      // await editor.clearValue();
+      // await testSubjects.setValue('kibanaCodeEditor', actionBody, {
+      //   clearWithKeyboard: true,
+      // });
+      // await commonScreenshots.takeScreenshot('gen-ai-params-test', screenshotDirectories);
+      await testSubjects.click('euiFlyoutCloseButton');
+    });
+  });
+}
