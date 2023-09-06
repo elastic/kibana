c322fcd6666e62a802a6fb30f273c50891d55d44
Jatin Kathuria <jatin.kathuria@elastic.co>
2023-09-07 00:42:55 +0200

    fix: more experiments with cypress tags

--- x-pack/test/security_solution_cypress/cypress/e2e/investigations/timelines/discover/discover_timeline_state_integration.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/investigations/timelines/discover/discover_timeline_state_integration.cy.ts
@@ -186,31 +186,27 @@ describe(
           });
       });
     });
-    context('saved search tags', () => {
-      it(
-        'should save discover saved search with `Security Solution` tag',
-        { tags: '@brokenInServerless' },
-        () => {
-          const timelineSuffix = Date.now();
-          const timelineName = `SavedObject timeline-${timelineSuffix}`;
-          const kqlQuery = '_id: *';
-          addDiscoverKqlQuery(kqlQuery);
-          addNameToTimeline(timelineName);
-          cy.wait(`@${TIMELINE_REQ_WITH_SAVED_SEARCH}`);
-          openKibanaNavigation();
-          navigateFromKibanaCollapsibleTo(STACK_MANAGEMENT_PAGE);
-          cy.get(LOADING_INDICATOR).should('not.exist');
-          goToSavedObjectSettings();
-          cy.get(LOADING_INDICATOR).should('not.exist');
-          cy.get(SAVED_OBJECTS_TAGS_FILTER).trigger('click');
-          cy.get(GET_SAVED_OBJECTS_TAGS_OPTION('Security_Solution')).trigger('click');
-          cy.get(BASIC_TABLE_LOADING).should('not.exist');
-          cy.get(SAVED_OBJECTS_ROW_TITLES).should(
-            'contain.text',
-            `Saved Search for timeline - ${timelineName}`
-          );
-        }
-      );
+    context('saved search tags', { tags: '@brokenInServerless' }, () => {
+      it('should save discover saved search with `Security Solution` tag', () => {
+        const timelineSuffix = Date.now();
+        const timelineName = `SavedObject timeline-${timelineSuffix}`;
+        const kqlQuery = '_id: *';
+        addDiscoverKqlQuery(kqlQuery);
+        addNameToTimeline(timelineName);
+        cy.wait(`@${TIMELINE_REQ_WITH_SAVED_SEARCH}`);
+        openKibanaNavigation();
+        navigateFromKibanaCollapsibleTo(STACK_MANAGEMENT_PAGE);
+        cy.get(LOADING_INDICATOR).should('not.exist');
+        goToSavedObjectSettings();
+        cy.get(LOADING_INDICATOR).should('not.exist');
+        cy.get(SAVED_OBJECTS_TAGS_FILTER).trigger('click');
+        cy.get(GET_SAVED_OBJECTS_TAGS_OPTION('Security_Solution')).trigger('click');
+        cy.get(BASIC_TABLE_LOADING).should('not.exist');
+        cy.get(SAVED_OBJECTS_ROW_TITLES).should(
+          'contain.text',
+          `Saved Search for timeline - ${timelineName}`
+        );
+      });
     });
     context('saved search', () => {
       it('should rename the saved search on timeline rename', () => {
