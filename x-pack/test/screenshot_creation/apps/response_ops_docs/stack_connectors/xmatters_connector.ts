b9925eb3c067f7a0206dee1f52f07d571b32aff9
lcawl <lcawley@elastic.co>
2023-09-06 16:55:25 -0700

    [DOCS] Automate xmatters-connector-url.png

--- x-pack/test/screenshot_creation/apps/response_ops_docs/stack_connectors/xmatters_connector.ts
+++ x-pack/test/screenshot_creation/apps/response_ops_docs/stack_connectors/xmatters_connector.ts
@@ -26,14 +26,15 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await actions.common.openNewConnectorForm('xmatters');
       await testSubjects.setValue('nameInput', 'xMatters test connector');
       await commonScreenshots.takeScreenshot('xmatters-connector-basic', screenshotDirectories);
+      const authentication = await testSubjects.find('button-group');
+      // a radio button consists of a div tag that contains an input, a div, and a label
+      // we can't click the input directly, need to click the label
+      const label = await authentication.findByCssSelector('label[title="URL Authentication"]');
+      await label.click();
+      await commonScreenshots.takeScreenshot('xmatters-connector-url', screenshotDirectories);
       // await testSubjects.click('create-connector-flyout-save-test-btn');
       // await testSubjects.click('toastCloseButton');
-      // const editor = await testSubjects.find('kibanaCodeEditor');
-      // await editor.clearValue();
-      // await testSubjects.setValue('kibanaCodeEditor', actionBody, {
-      //   clearWithKeyboard: true,
-      // });
-      // await commonScreenshots.takeScreenshot('gen-ai-params-test', screenshotDirectories);
+      // await commonScreenshots.takeScreenshot('xmatters-params-test', screenshotDirectories);
       await testSubjects.click('euiFlyoutCloseButton');
     });
   });
