f44671c8c5769caa893f9916cb552975dc7042d0
Yulia Cech <yulia.cech@elastic.co>
2023-09-07 16:53:08 +0200

    [Index Management] Fix functional tests

--- x-pack/plugins/index_management/public/application/store/reducers/index_management.js
+++ x-pack/plugins/index_management/public/application/store/reducers/index_management.js
@@ -10,10 +10,9 @@ import { indices } from './indices';
 import { rowStatus } from './row_status';
 import { tableState } from './table_state';
 
-export const getReducer = ({ uiMetricService }) =>
+export const getReducer = () =>
   combineReducers({
     indices,
     rowStatus,
     tableState,
-    detailPanel: getDetailPanelReducer(uiMetricService),
   });
