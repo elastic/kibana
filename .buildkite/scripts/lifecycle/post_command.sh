4bbbac4f87fd96e9ed5daed97582b07592a0a61f
Maxim Palenov <maxim.palenov@elastic.co>
2023-09-07 14:11:52 +0200

    use double brackets

--- .buildkite/scripts/lifecycle/post_command.sh
+++ .buildkite/scripts/lifecycle/post_command.sh
@@ -31,7 +31,7 @@ if [[ "$IS_TEST_EXECUTION_STEP" == "true" ]]; then
   buildkite-agent artifact upload '.es/**/*.hprof'
   buildkite-agent artifact upload 'data/es_debug_*.tar.gz'
 
-  if [ ${BUILDKITE_COMMAND_EXIT_STATUS} -ne 0 ]; then
+  if [[ $BUILDKITE_COMMAND_EXIT_STATUS -ne 0 ]]; then
     echo "--- Run Failed Test Reporter"
     node scripts/report_failed_tests --build-url="${BUILDKITE_BUILD_URL}#${BUILDKITE_JOB_ID}" 'target/junit/**/*.xml'
   fi
