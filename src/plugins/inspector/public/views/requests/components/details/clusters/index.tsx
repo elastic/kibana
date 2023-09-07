66c3f088290ae6ae4a58921a36ef014386804b50
nreese <reese.nathan@elastic.co>
2023-09-07 14:25:13 -0600

    [inspector] show cluster details tab in inspector

---
+++ src/plugins/inspector/public/views/requests/components/details/clusters/index.tsx
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React, { Component } from 'react';
+import type { RequestDetailsProps } from '../../types';
+
+export class Clusters extends Component<RequestDetailsProps> {
+  static shouldShow = (request: Request) =>
+    Boolean(request.response?.json);
+
+  render() {
+    return this.props.request.response?.json
+      ? <div>Cluster details go here</div>
+      : null;
+  }
+}
