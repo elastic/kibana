e85471ba3687bb0bdefa3119798c93d659261f28
wafaanasr <wafaa.aanasr@gmail.com>
2023-09-07 19:11:23 +0200

    fix new exception components with temp component until moved to exceptionlist package

---
+++ x-pack/plugins/security_solution/public/exceptions/components/link_to_list_details/index.tsx
@@ -0,0 +1,71 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import type { FC } from 'react';
+import { EuiLink } from '@elastic/eui';
+
+import { SecuritySolutionLinkAnchor } from '../../../common/components/links';
+import { APP_UI_ID, SecurityPageName } from '../../../../common/constants';
+import { useKibana } from '../../../common/lib/kibana';
+
+interface LinkToListDetailsProps {
+  linkTitle: string;
+  listId: string;
+  external?: boolean;
+  dataTestSubj?: string;
+}
+// This component should be removed and moved to @kbn/securitysolution-exception-list-components
+// once all the building components get moved
+
+const LinkToListDetailsComponent: FC<LinkToListDetailsProps> = ({
+  linkTitle,
+  listId,
+  external,
+  dataTestSubj,
+}) => {
+  const {
+    application: { navigateToApp },
+  } = useKibana().services;
+
+  return external ? (
+    // To achieve a consistent user experience similar to an external link,
+    // it's necessary to employ the href attribute in this context.
+    // eslint-disable-next-line @elastic/eui/href-or-on-click
+    <EuiLink
+      target="_blank"
+      href="#"
+      external={external}
+      data-test-subj={`linkEuiLink${dataTestSubj}`}
+      onClick={(e) => {
+        e.preventDefault();
+        navigateToApp(APP_UI_ID, {
+          deepLinkId: SecurityPageName.exceptions,
+          path: `/details/${listId}`,
+          openInNewTab: true,
+        });
+      }}
+    >
+      {linkTitle}
+    </EuiLink>
+  ) : (
+    <SecuritySolutionLinkAnchor
+      data-test-subj={`linkSecuritySolutionLinkAnchor${dataTestSubj}`}
+      deepLinkId={SecurityPageName.exceptions}
+      path={`/details/${listId}`}
+      external={external}
+    >
+      {linkTitle}
+    </SecuritySolutionLinkAnchor>
+  );
+};
+
+LinkToListDetailsComponent.displayName = 'LinkToListDetailsComponent';
+
+export const LinkToListDetails = React.memo(LinkToListDetailsComponent);
+
+LinkToListDetails.displayName = 'LinkToListDetails';
---
+++ x-pack/plugins/security_solution/public/exceptions/components/link_to_rule_details/index.tsx
@@ -0,0 +1,71 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import type { FC } from 'react';
+import { EuiLink } from '@elastic/eui';
+
+import { SecuritySolutionLinkAnchor } from '../../../common/components/links';
+import { RuleDetailTabs } from '../../../detection_engine/rule_details_ui/pages/rule_details/use_rule_details_tabs';
+import { APP_UI_ID, SecurityPageName } from '../../../../common/constants';
+import { getRuleDetailsTabUrl } from '../../../common/components/link_to/redirect_to_detection_engine';
+import { useKibana } from '../../../common/lib/kibana';
+
+interface LinkToRuleDetailsProps {
+  referenceName: string;
+  referenceId: string;
+  external?: boolean;
+}
+// This component should be removed and moved to @kbn/securitysolution-exception-list-components
+// once all the building components get moved
+
+const LinkToRuleDetailsComponent: FC<LinkToRuleDetailsProps> = ({
+  referenceName,
+  referenceId,
+  external,
+}) => {
+  const {
+    application: { navigateToApp },
+  } = useKibana().services;
+
+  return external ? (
+    // To achieve a consistent user experience similar to an external link,
+    // it's necessary to employ the href attribute in this context.
+    // eslint-disable-next-line @elastic/eui/href-or-on-click
+    <EuiLink
+      target="_blank"
+      href="#"
+      external={external}
+      data-test-subj="linkToRuleDetailsUsingEuiLink"
+      onClick={(e) => {
+        e.preventDefault();
+        navigateToApp(APP_UI_ID, {
+          deepLinkId: SecurityPageName.rules,
+          path: getRuleDetailsTabUrl(referenceId, RuleDetailTabs.alerts),
+          openInNewTab: true,
+        });
+      }}
+    >
+      {referenceName}
+    </EuiLink>
+  ) : (
+    <SecuritySolutionLinkAnchor
+      data-test-subj="linkToRuleDetailsUsingSecuritySolutionLinkAnchor"
+      deepLinkId={SecurityPageName.rules}
+      path={getRuleDetailsTabUrl(referenceId, RuleDetailTabs.alerts)}
+      external={external}
+    >
+      {referenceName}
+    </SecuritySolutionLinkAnchor>
+  );
+};
+
+LinkToRuleDetailsComponent.displayName = 'LinkToRuleDetailsComponent';
+
+export const LinkToRuleDetails = React.memo(LinkToRuleDetailsComponent);
+
+LinkToRuleDetails.displayName = 'LinkToRuleDetails';
