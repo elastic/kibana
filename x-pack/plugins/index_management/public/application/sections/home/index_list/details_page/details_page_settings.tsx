b1fa647c993b173291ae551d06c1b7c13f48c742
Yulia Cech <yulia.cech@elastic.co>
2023-09-07 17:18:53 +0200

    [Index Management] Update breadcrumbs on the index settings page

--- x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/details_page_settings.tsx
+++ x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/details_page_settings.tsx
@@ -5,13 +5,14 @@
  * 2.0.
  */
 
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { EuiButton, EuiPageTemplate, EuiSpacer, EuiText } from '@elastic/eui';
 import { SectionLoading } from '@kbn/es-ui-shared-plugin/public';
 import { FormattedMessage } from '@kbn/i18n-react';
 
 import { useLoadIndexSettings } from '../../../../services';
+import { breadcrumbService, IndexManagementBreadcrumb } from '../../../../services/breadcrumbs';
 import { DetailsPageSettingsContent } from './details_page_settings_content';
 
 export const DetailsPageSettings: FunctionComponent<
@@ -23,6 +24,10 @@ export const DetailsPageSettings: FunctionComponent<
   isIndexOpen,
 }) => {
   const { isLoading, data, error, resendRequest } = useLoadIndexSettings(indexName);
+
+  useEffect(() => {
+    breadcrumbService.setBreadcrumbs(IndexManagementBreadcrumb.indexDetailsSettings);
+  }, []);
 
   if (isLoading) {
     return (
