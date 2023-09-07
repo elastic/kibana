26677afe251357d0edbdfdf6f7e52960a9652d05
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2023-09-07 13:38:34 +0300

    Update text

--- src/plugins/discover/public/application/main/components/layout/selected_vs_available_callout.tsx
+++ src/plugins/discover/public/application/main/components/layout/selected_vs_available_callout.tsx
@@ -33,7 +33,7 @@ export const SelectedVSAvailableCallout = ({
             iconType="iInCircle"
             title={i18n.translate('discover.textBasedMode.selectedColumnsCallout', {
               defaultMessage:
-                'Displaying first {selectedColumnsNumber} fields out of {textBasedQueryColumnsNumber}. You can add more fields to this table using Available fields of the Field list.',
+                'Displaying {selectedColumnsNumber} of {textBasedQueryColumnsNumber} fields. Add more from the Available fields list.',
               values: {
                 textBasedQueryColumnsNumber: textBasedQueryColumns.length,
                 selectedColumnsNumber: selectedColumns.length,
