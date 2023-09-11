6bfe3da1a07327c64d97ed9d7f6e6e4029efc62a
Elena Stoeva <elenastoeva99@gmail.com>
2023-09-11 13:26:49 +0100

    Validate allowlist only in development mode

--- packages/core/ui-settings/core-ui-settings-server-internal/src/ui_settings_service.ts
+++ packages/core/ui-settings/core-ui-settings-server-internal/src/ui_settings_service.ts
@@ -105,7 +105,10 @@ export class UiSettingsService
 
   public async start(): Promise<InternalUiSettingsServiceStart> {
     if (this.allowlist) {
-      this.validateAllowlist();
+      // If we are in development mode, check if all settings in the allowlist are registered
+      if (process.env.NODE_ENV !== 'production'){
+        this.validateAllowlist();
+      }
       this.applyAllowlist(this.uiSettingsDefaults, false);
       this.applyAllowlist(this.uiSettingsGlobalDefaults, true);
     }
