f1025241fa1818a21ea687ba57aa3ea3e946f21e
Nick Peihl <nick.peihl@elastic.co>
2023-09-12 16:42:23 -0400

    Remove unneeded mock

--- src/plugins/navigation_embeddable/public/mocks.tsx
+++ src/plugins/navigation_embeddable/public/mocks.tsx
@@ -10,7 +10,6 @@ import { coreMock } from '@kbn/core/public/mocks';
 import { dashboardPluginMock } from '@kbn/dashboard-plugin/public/mocks';
 import { embeddablePluginMock } from '@kbn/embeddable-plugin/public/mocks';
 import { contentManagementMock } from '@kbn/content-management-plugin/public/mocks';
-import { presentationUtilPluginMock } from '@kbn/presentation-util-plugin/public/mocks';
 import { setKibanaServices } from './services/kibana_services';
 
 export const setStubKibanaServices = () => {
@@ -20,6 +19,5 @@ export const setStubKibanaServices = () => {
     dashboard: dashboardPluginMock.createStartContract(),
     embeddable: embeddablePluginMock.createStartContract(),
     contentManagement: contentManagementMock.createStartContract(),
-    presentationUtil: presentationUtilPluginMock.createStartContract(core),
   });
 };
