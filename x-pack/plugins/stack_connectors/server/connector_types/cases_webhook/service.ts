17a7fd61503a1cc318906b7e11e53a8ced9e5540
Steph Milovic <stephanie.milovic@elastic.co>
2023-09-12 13:15:14 -0600

    server side done

--- x-pack/plugins/stack_connectors/server/connector_types/cases_webhook/service.ts
+++ x-pack/plugins/stack_connectors/server/connector_types/cases_webhook/service.ts
@@ -118,11 +118,7 @@ export const createExternalService = (
     incident,
   }: CreateIncidentParams): Promise<ExternalServiceIncidentResponse> => {
     try {
-      console.log('incident', {
-        incident,
-        createIncidentJson,
-      });
-      const { tags, title, description } = incident;
+      const { description, id, severity, status: incidentStatus, tags, title } = incident;
       const normalizedUrl = validateAndNormalizeUrl(
         `${createIncidentUrl}`,
         configurationUtilities,
@@ -131,9 +127,12 @@ export const createExternalService = (
       const json = renderMustacheStringNoEscape(
         createIncidentJson,
         stringifyObjValues({
-          title,
           description: description ?? '',
+          id: id ?? '',
+          severity: severity ?? '',
+          status: incidentStatus ?? '',
           tags: tags ?? [],
+          title,
         })
       );
 
@@ -201,13 +200,16 @@ export const createExternalService = (
         'Update case URL'
       );
 
-      const { tags, title, description } = incident;
+      const { description, id, severity, status: incidentStatus, tags, title } = incident;
 
       const json = renderMustacheStringNoEscape(updateIncidentJson, {
         ...stringifyObjValues({
-          title,
           description: description ?? '',
+          id: id ?? '',
+          severity: severity ?? '',
+          status: incidentStatus ?? '',
           tags: tags ?? [],
+          title,
         }),
         external: {
           system: {
