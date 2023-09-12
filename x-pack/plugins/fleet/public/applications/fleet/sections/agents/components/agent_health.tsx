ede9386a1de04dedd9eebb7c6d1b0797099db345
Julia Bardi <julia.bardi@elastic.co>
2023-09-12 17:48:11 +0200

    added callout and warning

--- x-pack/plugins/fleet/public/applications/fleet/sections/agents/components/agent_health.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agents/components/agent_health.tsx
@@ -6,8 +6,9 @@
  */
 
 import React, { useMemo } from 'react';
+import styled from 'styled-components';
 import { FormattedMessage, FormattedRelative } from '@kbn/i18n-react';
-import { EuiBadge, EuiToolTip } from '@elastic/eui';
+import { EuiBadge, EuiButton, EuiCallOut, EuiIcon, EuiSpacer, EuiToolTip } from '@elastic/eui';
 
 import { euiLightVars as euiVars } from '@kbn/ui-theme';
 
@@ -17,7 +18,7 @@ import type { Agent } from '../../../types';
 
 interface Props {
   agent: Agent;
-  showOfflinePreviousStatus?: boolean;
+  fromDetails?: boolean;
 }
 
 const Status = {
@@ -79,10 +80,21 @@ function getStatusComponent(status: Agent['status']): React.ReactElement {
   }
 }
 
-export const AgentHealth: React.FunctionComponent<Props> = ({
-  agent,
-  showOfflinePreviousStatus,
-}) => {
+const AGENT_UPDATING_TIMEOUT = 60 * 60 * 1000; // 1 hour
+
+function isStuckInUpdating(agent: Agent): boolean {
+  return (
+    agent.status === 'updating' &&
+    !!agent.upgrade_started_at &&
+    Date.now() - Date.parse(agent.upgrade_started_at) > AGENT_UPDATING_TIMEOUT
+  );
+}
+
+const WrappedEuiCallOut = styled(EuiCallOut)`
+  white-space: wrap !important;
+`;
+
+export const AgentHealth: React.FunctionComponent<Props> = ({ agent, fromDetails }) => {
   const { last_checkin: lastCheckIn, last_checkin_message: lastCheckInMessage } = agent;
   const msLastCheckIn = new Date(lastCheckIn || 0).getTime();
   const lastCheckInMessageText = lastCheckInMessage ? (
@@ -112,27 +124,72 @@ export const AgentHealth: React.FunctionComponent<Props> = ({
   );
 
   const previousToOfflineStatus = useMemo(() => {
-    if (!showOfflinePreviousStatus || agent.status !== 'offline') {
+    if (!fromDetails || agent.status !== 'offline') {
       return;
     }
 
     return getPreviousAgentStatusForOfflineAgents(agent);
-  }, [showOfflinePreviousStatus, agent]);
+  }, [fromDetails, agent]);
 
   return (
-    <EuiToolTip
-      position="top"
-      content={
+    <>
+      <EuiToolTip
+        position="top"
+        content={
+          <>
+            <p>{lastCheckinText}</p>
+            <p>{lastCheckInMessageText}</p>
+            {isStuckInUpdating(agent) ? (
+              <p>
+                <FormattedMessage
+                  id="xpack.fleet.agentHealth.restartUpgradeTooltipText"
+                  defaultMessage="Agent may be stuck updating. Consider restarting the upgrade."
+                />
+              </p>
+            ) : null}
+          </>
+        }
+      >
         <>
-          <p>{lastCheckinText}</p>
-          <p>{lastCheckInMessageText}</p>
+          {getStatusComponent(agent.status)}
+          {previousToOfflineStatus ? getStatusComponent(previousToOfflineStatus) : null}
+          {isStuckInUpdating(agent) && !fromDetails ? (
+            <>
+              &nbsp;
+              <EuiIcon type="warning" />
+            </>
+          ) : null}
         </>
-      }
-    >
-      <>
-        {getStatusComponent(agent.status)}
-        {previousToOfflineStatus ? getStatusComponent(previousToOfflineStatus) : null}
-      </>
-    </EuiToolTip>
+      </EuiToolTip>
+      {fromDetails && isStuckInUpdating(agent) ? (
+        <>
+          <EuiSpacer size="m" />
+          <WrappedEuiCallOut
+            iconType="warning"
+            size="m"
+            color="warning"
+            title={
+              <FormattedMessage
+                id="xpack.fleet.agentHealth.stuckUpdatingTitle"
+                defaultMessage="Agent may be stuck updating."
+              />
+            }
+          >
+            <p>
+              <FormattedMessage
+                id="xpack.fleet.agentHealth.stuckUpdatingText"
+                defaultMessage="Agent has been updating for a while, and may be stuck. Consider restarting the upgrade."
+              />
+            </p>
+            <EuiButton href="#" color="warning">
+              <FormattedMessage
+                id="xpack.fleet.agentHealth.restartUpgradeBtn"
+                defaultMessage="Restart upgrade"
+              />
+            </EuiButton>
+          </WrappedEuiCallOut>
+        </>
+      ) : null}
+    </>
   );
 };
