42a81a2a07c6365c2777fb47309f43b745654a36
Faisal Kanout <f.kanout@gmail.com>
2023-09-13 14:02:53 +0200

    Update test files name

--- x-pack/test/alerting_api_integration/observability/index.ts
+++ x-pack/test/alerting_api_integration/observability/index.ts
@@ -16,7 +16,7 @@ export default function ({ loadTestFile }: any) {
       loadTestFile(require.resolve('./custom_threshold_rule/custom_eq_avg_bytes_fired'));
       loadTestFile(require.resolve('./custom_threshold_rule/documents_count_fired'));
       loadTestFile(require.resolve('./custom_threshold_rule/group_by_fired'));
-      loadTestFile(require.resolve('./threshold_rule_data_view'));
+      loadTestFile(require.resolve('./custom_threshold_rule_data_view'));
     });
     describe('Synthetics', () => {
       loadTestFile(require.resolve('./synthetics_rule'));
