74573cfa1ca949a2da8633403676a956214b491a
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-09-13 12:13:11 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/reporting/public/management/mount_management_section.tsx
+++ x-pack/plugins/reporting/public/management/mount_management_section.tsx
@@ -11,13 +11,13 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import { Observable } from 'rxjs';
 import { CoreSetup, CoreStart } from '@kbn/core/public';
 import { ILicense } from '@kbn/licensing-plugin/public';
+import { KibanaThemeProvider } from '@kbn/react-kibana-context-theme';
 import { ReportingAPIClient, InternalApiClientProvider } from '../lib/reporting_api_client';
 import { IlmPolicyStatusContextProvider } from '../lib/ilm_policy_status_context';
 import { ClientConfigType } from '../plugin';
 import type { ManagementAppMountParams, SharePluginSetup } from '../shared_imports';
 import { KibanaContextProvider } from '../shared_imports';
 import { ReportListing } from '.';
-import { KibanaThemeProvider } from '@kbn/react-kibana-context-theme';
 
 export async function mountManagementSection(
   coreSetup: CoreSetup,
@@ -26,10 +26,10 @@ export async function mountManagementSection(
   config: ClientConfigType,
   apiClient: ReportingAPIClient,
   urlService: SharePluginSetup['url'],
-  params: ManagementAppMountParams,
+  params: ManagementAppMountParams
 ) {
   render(
-    <KibanaThemeProvider theme={{theme$: params.theme$}}>
+    <KibanaThemeProvider theme={{ theme$: params.theme$ }}>
       <I18nProvider>
         <KibanaContextProvider
           services={{
