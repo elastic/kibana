c03a8ed9ec225cae340da6d0b5947b08e354223b
Ashokaditya <ashokaditya@elastic.co>
2023-09-14 11:11:27 +0200

    fix type imports
    refs elastic/kibana/pull/164107

--- x-pack/plugins/security_solution_serverless/public/upselling/pages/osquery_automated_response_actions.tsx
+++ x-pack/plugins/security_solution_serverless/public/upselling/pages/osquery_automated_response_actions.tsx
@@ -8,11 +8,11 @@
 import { EuiEmptyPrompt, EuiIcon } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import React from 'react';
-import type { AppFeatureKey } from '@kbn/security-solution-plugin/common';
+import type { AppFeatureKeyType } from '@kbn/security-solution-features/keys';
 import { getProductTypeByPLI } from '../hooks/use_product_type_by_pli';
 
-const OsqueryResponseActionsUpsellingSection: React.FC<{ requiredPLI: AppFeatureKey }> = React.memo(
-  ({ requiredPLI }) => {
+const OsqueryResponseActionsUpsellingSection: React.FC<{ requiredPLI: AppFeatureKeyType }> =
+  React.memo(({ requiredPLI }) => {
     const productTypeRequired = getProductTypeByPLI(requiredPLI);
 
     return (
@@ -38,8 +38,7 @@ const OsqueryResponseActionsUpsellingSection: React.FC<{ requiredPLI: AppFeature
         }
       />
     );
-  }
-);
+  });
 
 OsqueryResponseActionsUpsellingSection.displayName = 'OsqueryResponseActionsUpsellingSection';
 
