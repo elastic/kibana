774ccfb238dc7d4b250d5d85bf51bb2dfe84ca41
Faisal Kanout <f.kanout@gmail.com>
2023-09-14 11:41:09 +0200

    remove comment

--- x-pack/test/observability_functional/apps/observability/pages/rule_details_page.ts
+++ x-pack/test/observability_functional/apps/observability/pages/rule_details_page.ts
@@ -36,7 +36,6 @@ export default ({ getService }: FtrProviderContext) => {
     return true;
   }
 
-  // FLAKY: https://github.com/elastic/kibana/issues/165619
   describe('Observability Rule Details page', function () {
     this.tags('includeFirefox');
 
