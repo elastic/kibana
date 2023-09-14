3feeab13e7c434916b8070038ae7fe498b942abd
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-09-14 19:35:21 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/reporting/public/lib/reporting_api_client/hooks.ts
+++ x-pack/plugins/reporting/public/lib/reporting_api_client/hooks.ts
@@ -9,10 +9,14 @@ import { INTERNAL_ROUTES } from '../../../common/constants';
 import { IlmPolicyStatusResponse } from '../../../common/types';
 import { useKibana, useRequest, UseRequestResponse } from '../../shared_imports';
 
-export const useCheckIlmPolicyStatus = (statefulSettings: boolean): UseRequestResponse<IlmPolicyStatusResponse> => {
+export const useCheckIlmPolicyStatus = (
+  statefulSettings: boolean
+): UseRequestResponse<IlmPolicyStatusResponse> => {
   const {
     services: { http },
   } = useKibana();
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  return statefulSettings ?  useRequest(http, { path: INTERNAL_ROUTES.MIGRATE.GET_ILM_POLICY_STATUS, method: 'get' }) : useRequest(http, { path: '', method: 'get' });
+
+  return statefulSettings
+    ? useRequest(http, { path: INTERNAL_ROUTES.MIGRATE.GET_ILM_POLICY_STATUS, method: 'get' })
+    : useRequest(http, { path: '', method: 'get' });
 };
