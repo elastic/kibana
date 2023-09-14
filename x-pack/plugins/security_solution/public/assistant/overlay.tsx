a6fe6e54b576f76541744c0416ea144ee575cd39
semd <sergi.massaneda@elastic.co>
2023-09-14 11:24:01 +0200

    ai assistant logic centralized

---
+++ x-pack/plugins/security_solution/public/assistant/overlay.tsx
@@ -0,0 +1,17 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React from 'react';
+import { AssistantOverlay as ElasticAssistantOverlay } from '@kbn/elastic-assistant';
+import { useAssistantContext } from '@kbn/elastic-assistant/impl/assistant_context';
+
+export const AssistantOverlay: React.FC = () => {
+  const { assistantAvailability } = useAssistantContext();
+  if (!assistantAvailability.hasAssistantPrivilege) {
+    return null;
+  }
+  return <ElasticAssistantOverlay />;
+};
