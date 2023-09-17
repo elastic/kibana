15cc3394291ce91766b1ffbf6b7d63d92c758e8d
Davis McPhee <davis.mcphee@elastic.co>
2023-09-16 21:48:31 -0300

    Create useComparisonCellValue hook

--- packages/kbn-unified-data-table/src/components/compare_documents/compare_documents.tsx
+++ packages/kbn-unified-data-table/src/components/compare_documents/compare_documents.tsx
@@ -8,33 +8,21 @@
 
 import {
   EuiDataGrid,
-  EuiDataGridCellValueElementProps,
   EuiDataGridColumnVisibility,
   EuiDataGridInMemory,
   EuiDataGridRowHeightsOptions,
   EuiDataGridSchemaDetector,
   EuiDataGridToolBarVisibilityOptions,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiText,
-  tint,
-  useEuiBackgroundColor,
   useGeneratedHtmlId,
 } from '@elastic/eui';
-import { css } from '@emotion/react';
 import { DataView } from '@kbn/data-views-plugin/common';
-import { formatFieldValue, getFieldTypeName } from '@kbn/discover-utils';
 import { DataTableRecord } from '@kbn/discover-utils/types';
 import type { FieldFormatsStart } from '@kbn/field-formats-plugin/public';
-import { FieldIcon } from '@kbn/react-field';
-import { euiThemeVars } from '@kbn/ui-theme';
-import { diffChars, diffLines, diffWords } from 'diff';
-import { isEqual } from 'lodash';
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import useLocalStorage from 'react-use/lib/useLocalStorage';
 import { GRID_STYLE } from '../../constants';
-import { CELL_CLASS } from '../../utils/get_render_cell_value';
 import { ComparisonControls } from './comparison_controls';
+import { useComparisonCellValue } from './hooks/use_comparison_cell_value';
 import { useComparisonColumns } from './hooks/use_comparison_columns';
 import { useComparisonFields } from './hooks/use_comparison_fields';
 
@@ -53,6 +41,8 @@ export interface CompareDocumentsProps {
   setIsCompareActive: (isCompareActive: boolean) => void;
 }
 
+const getStorageKey = (consumer: string, key: string) => `${consumer}:dataGrid${key}`;
+
 const CompareDocuments = ({
   consumer,
   ariaDescribedBy,
@@ -67,27 +57,32 @@ const CompareDocuments = ({
   setSelectedDocs,
   setIsCompareActive,
 }: CompareDocumentsProps) => {
-  const [showDiff, setShowDiff] = useLocalStorage(`${consumer}:dataGridShowDiff`, true);
-  const [showAllFields, setShowAllFields] = useLocalStorage(
-    `${consumer}:dataGridShowAllFields`,
-    false
-  );
+  const [showDiff, setShowDiff] = useLocalStorage(getStorageKey(consumer, 'ShowDiff'), true);
   const [diffMode, setDiffMode] = useLocalStorage<'basic' | 'chars' | 'words' | 'lines'>(
-    `${consumer}:dataGridDiffMode`,
+    getStorageKey(consumer, 'DiffMode'),
     'basic'
   );
   const [showDiffDecorations, setShowDiffDecorations] = useLocalStorage(
-    `${consumer}:dataGridShowDiffDecorations`,
+    getStorageKey(consumer, 'ShowDiffDecorations'),
     true
   );
+  const [showAllFields, setShowAllFields] = useLocalStorage(
+    getStorageKey(consumer, 'ShowAllFields'),
+    false
+  );
+
   const fieldsColumnId = useGeneratedHtmlId({ prefix: 'fields' });
+  const comparisonFields = useComparisonFields({
+    dataView,
+    selectedFieldNames,
+    showAllFields: Boolean(forceShowAllFields || showAllFields),
+  });
   const comparisonColumns = useComparisonColumns({
     fieldsColumnId,
     selectedDocs,
     getDocById,
     setSelectedDocs,
   });
-  const comparisonInMemory: EuiDataGridInMemory = useMemo(() => ({ level: 'sorting' }), []);
   const comparisonColumnVisibility: EuiDataGridColumnVisibility = useMemo(
     () => ({
       visibleColumns: comparisonColumns.map(({ id }) => id),
@@ -95,40 +90,30 @@ const CompareDocuments = ({
     }),
     [comparisonColumns]
   );
+  const comparisonRowCount = useMemo(() => comparisonFields.length, [comparisonFields.length]);
   const comparisonRowHeight: EuiDataGridRowHeightsOptions = useMemo(
     () => ({ defaultHeight: 'auto' }),
     []
   );
-  const comparisonFields = useComparisonFields({
-    dataView,
-    selectedFieldNames,
-    showAllFields: Boolean(forceShowAllFields || showAllFields),
-  });
-  const comparisonRowCount = useMemo(() => comparisonFields.length, [comparisonFields.length]);
-  const comparisonToolbarVisibility: EuiDataGridToolBarVisibilityOptions = useMemo(
-    () => ({
-      showColumnSelector: false,
-      showDisplaySelector: {
-        allowDensity: false,
-      },
-      additionalControls: (
-        <ComparisonControls
-          showDiff={showDiff}
-          diffMode={diffMode}
-          showDiffDecorations={showDiffDecorations}
-          showAllFields={showAllFields}
-          forceShowAllFields={forceShowAllFields}
-          setIsCompareActive={setIsCompareActive}
-          setShowDiff={setShowDiff}
-          setDiffMode={setDiffMode}
-          setShowDiffDecorations={setShowDiffDecorations}
-          setShowAllFields={setShowAllFields}
-        />
-      ),
-    }),
+  const comparisonInMemory: EuiDataGridInMemory = useMemo(() => ({ level: 'sorting' }), []);
+  const additionalControls = useMemo(
+    () => (
+      <ComparisonControls
+        showDiff={showDiff}
+        diffMode={diffMode}
+        showDiffDecorations={showDiffDecorations}
+        showAllFields={showAllFields}
+        forceShowAllFields={forceShowAllFields}
+        setIsCompareActive={setIsCompareActive}
+        setShowDiff={setShowDiff}
+        setDiffMode={setDiffMode}
+        setShowDiffDecorations={setShowDiffDecorations}
+        setShowAllFields={setShowAllFields}
+      />
+    ),
     [
-      forceShowAllFields,
       diffMode,
+      forceShowAllFields,
       setDiffMode,
       setIsCompareActive,
       setShowAllFields,
@@ -139,237 +124,40 @@ const CompareDocuments = ({
       showDiffDecorations,
     ]
   );
-  const comparisonBaseDocId = selectedDocs[0];
-  const comparisonBaseDoc = useMemo(
-    () => getDocById(comparisonBaseDocId)?.flattened,
-    [comparisonBaseDocId, getDocById]
+  const comparisonToolbarVisibility: EuiDataGridToolBarVisibilityOptions = useMemo(
+    () => ({
+      showColumnSelector: false,
+      showDisplaySelector: false,
+      additionalControls,
+    }),
+    [additionalControls]
   );
-  const matchBackgroundColor = useEuiBackgroundColor('success');
-  const diffBackgroundColor = useEuiBackgroundColor('danger');
-  const baseDocCellCss = css`
-    background-color: ${useEuiBackgroundColor('success', { method: 'transparent' })};
-  `;
-  const matchingCellCss = css`
-    .unifiedDataTable__cellValue {
-      &,
-      & * {
-        color: ${euiThemeVars.euiColorSuccessText} !important;
-      }
-    }
-  `;
-  const differentCellCss = css`
-    .unifiedDataTable__cellValue {
-      &,
-      & * {
-        color: ${euiThemeVars.euiColorDangerText} !important;
-      }
-    }
-  `;
-  const renderComparisonCellValue = useCallback(
-    function ComparisonCellValue(props: EuiDataGridCellValueElementProps) {
-      const { rowIndex, columnId, setCellProps } = props;
-      const fieldName = comparisonFields[rowIndex];
-      const field = useMemo(() => dataView.fields.getByName(fieldName), [fieldName]);
-      const doc = useMemo(() => getDocById(columnId), [columnId]);
-
-      useEffect(() => {
-        if (!showDiff || diffMode !== 'basic') {
-          setCellProps({ css: undefined });
-          return;
-        }
-
-        if (columnId === comparisonBaseDocId) {
-          setCellProps({ css: baseDocCellCss });
-        } else if (columnId !== fieldsColumnId) {
-          const baseValue = comparisonBaseDoc?.[fieldName];
-          const currentValue = doc?.flattened[fieldName];
-
-          if (isEqual(baseValue, currentValue)) {
-            setCellProps({ css: matchingCellCss });
-          } else {
-            setCellProps({ css: differentCellCss });
-          }
-        }
-      }, [columnId, doc?.flattened, fieldName, setCellProps]);
-
-      if (columnId === fieldsColumnId) {
-        return (
-          <EuiFlexGroup responsive={false} gutterSize="s">
-            <EuiFlexItem grow={false}>
-              <FieldIcon
-                type={field?.type ?? 'unknown'}
-                label={getFieldTypeName(field?.type)}
-                scripted={field?.scripted}
-              />
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiText
-                size="relative"
-                css={css`
-                  font-weight: ${euiThemeVars.euiFontWeightSemiBold};
-                `}
-              >
-                {field?.displayName ?? fieldName}
-              </EuiText>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        );
-      }
-
-      if (!doc) {
-        return '-';
-      }
-
-      const baseValue = comparisonBaseDoc?.[fieldName];
-      const currentValue = doc?.flattened[fieldName];
-
-      if (showDiff && diffMode !== 'basic' && baseValue && currentValue) {
-        const hasLengthOne = (value: unknown): value is unknown[] => {
-          return Array.isArray(value) && value.length === 1;
-        };
-        const getStringifiedValue = (value: unknown, forceJson: boolean) => {
-          const extractedValue = !forceJson && hasLengthOne(value) ? value[0] : value;
-          if (forceJson || typeof extractedValue === 'object') {
-            return JSON.stringify(extractedValue, null, 2);
-          }
-          return String(extractedValue ?? '-');
-        };
-        const forceJson =
-          (hasLengthOne(baseValue) && !hasLengthOne(currentValue)) ||
-          (!hasLengthOne(baseValue) && hasLengthOne(currentValue));
-        const baseValueString = getStringifiedValue(baseValue, forceJson);
-        const currentValueString = getStringifiedValue(currentValue, forceJson);
-        const diff =
-          diffMode === 'chars'
-            ? diffChars(baseValueString, currentValueString)
-            : diffMode === 'words'
-            ? diffWords(baseValueString, currentValueString)
-            : diffLines(baseValueString, currentValueString);
-        const indicatorCss = css`
-          position: absolute;
-          width: ${euiThemeVars.euiSizeS};
-          height: 100%;
-          margin-left: calc(-${euiThemeVars.euiSizeS} - calc(${euiThemeVars.euiSizeXS} / 2));
-          text-align: center;
-          line-height: ${euiThemeVars.euiFontSizeM};
-          font-weight: ${euiThemeVars.euiFontWeightMedium};
-        `;
-        const matchCss = css`
-          background-color: ${matchBackgroundColor};
-          color: ${euiThemeVars.euiColorSuccessText};
-        `;
-        const matchIndicatorCss = css`
-          &:before {
-            content: '+';
-            ${indicatorCss}
-            background-color: ${euiThemeVars.euiColorSuccess};
-            color: ${euiThemeVars.euiColorLightestShade};
-          }
-        `;
-        const diffCss = css`
-          background-color: ${diffBackgroundColor};
-          color: ${euiThemeVars.euiColorDangerText};
-        `;
-        const diffIndicatorCss = css`
-          &:before {
-            content: '-';
-            ${indicatorCss}
-            background-color: ${tint(euiThemeVars.euiColorDanger, 0.25)};
-            color: ${euiThemeVars.euiColorLightestShade};
-          }
-        `;
-        const SegmentTag: keyof JSX.IntrinsicElements = diffMode === 'lines' ? 'div' : 'span';
-
-        return (
-          <div className={CELL_CLASS}>
-            {diff.map((part) => (
-              <SegmentTag
-                css={[
-                  css`
-                    position: relative;
-                  `,
-                  part.added ? matchCss : part.removed ? diffCss : undefined,
-                  diffMode === 'lines'
-                    ? css`
-                        padding-left: calc(${euiThemeVars.euiSizeXS} / 2);
-                      `
-                    : undefined,
-                  showDiffDecorations
-                    ? diffMode === 'lines'
-                      ? part.added
-                        ? matchIndicatorCss
-                        : part.removed
-                        ? diffIndicatorCss
-                        : undefined
-                      : part.added
-                      ? css`
-                          text-decoration: underline;
-                        `
-                      : part.removed
-                      ? css`
-                          text-decoration: line-through;
-                        `
-                      : undefined
-                    : undefined,
-                ]}
-              >
-                {part.value}
-              </SegmentTag>
-            ))}
-          </div>
-        );
-      }
-
-      return (
-        <span
-          className={CELL_CLASS}
-          // formatFieldValue guarantees sanitized values
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{
-            __html: formatFieldValue(
-              doc.flattened[fieldName],
-              doc.raw,
-              fieldFormats,
-              dataView,
-              dataView.getFieldByName(fieldName)
-            ),
-          }}
-        />
-      );
-    },
-    [
-      comparisonFields,
-      fieldsColumnId,
-      comparisonBaseDoc,
-      showDiff,
-      diffMode,
-      fieldFormats,
-      dataView,
-      getDocById,
-      comparisonBaseDocId,
-      baseDocCellCss,
-      matchingCellCss,
-      differentCellCss,
-      matchBackgroundColor,
-      diffBackgroundColor,
-      showDiffDecorations,
-    ]
-  );
+  const ComparisonCellValue = useComparisonCellValue({
+    dataView,
+    comparisonFields,
+    fieldsColumnId,
+    selectedDocs,
+    showDiff,
+    diffMode,
+    showDiffDecorations,
+    fieldFormats,
+    getDocById,
+  });
 
   return (
     <EuiDataGrid
       aria-describedby={ariaDescribedBy}
       aria-labelledby={ariaLabelledBy}
+      gridStyle={GRID_STYLE}
+      toolbarVisibility={comparisonToolbarVisibility}
       columns={comparisonColumns}
       columnVisibility={comparisonColumnVisibility}
-      data-test-subj="comparisonTable"
-      renderCellValue={renderComparisonCellValue}
       rowCount={comparisonRowCount}
-      schemaDetectors={schemaDetectors}
-      toolbarVisibility={comparisonToolbarVisibility}
-      inMemory={comparisonInMemory}
-      gridStyle={GRID_STYLE}
       rowHeightsOptions={comparisonRowHeight}
+      inMemory={comparisonInMemory}
+      schemaDetectors={schemaDetectors}
+      renderCellValue={ComparisonCellValue}
+      data-test-subj="comparisonTable"
     />
   );
 };
