2b19a8cce6c058e5d82525a878b7c5cba90040dc
mgiota <panagiota.mitsopoulou@elastic.co>
2023-09-18 12:22:11 +0200

    use Metric chart

--- x-pack/plugins/observability/public/embeddable/slo/slo_overview.tsx
+++ x-pack/plugins/observability/public/embeddable/slo/slo_overview.tsx
@@ -5,30 +5,64 @@
  * 2.0.
  */
 
-import React from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiTitle, EuiPanel } from '@elastic/eui';
+import React, { useCallback } from 'react';
+import { EuiFlexGroup, EuiIcon, EuiFlexItem, EuiStat } from '@elastic/eui';
+import { Chart, Metric, MetricTrendShape } from '@elastic/charts';
+import { i18n } from '@kbn/i18n';
+import numeral from '@elastic/numeral';
+import { css } from '@emotion/react';
 import { SloStatusBadge } from '../../components/slo/slo_status_badge';
 import { SloActiveAlertsBadge } from '../../components/slo/slo_status_badge/slo_active_alerts_badge';
 import { SloSummary } from './slo_summary';
+import { NOT_AVAILABLE_LABEL } from '../../../common/i18n';
+import { useKibana } from '../../utils/kibana_react';
+
 import { EmbeddableSloProps } from './types';
 
 export function SloOverview({ slo }: EmbeddableSloProps) {
+  console.log(slo, '!!sloooo');
+  const { uiSettings } = useKibana().services;
+
+  const percentFormat = uiSettings.get('format:percent:defaultPattern');
+
+  const getIcon = useCallback(
+    (type: string) =>
+      ({ width = 20, height = 20, color }: { width: number; height: number; color: string }) => {
+        return <EuiIcon type={type} width={width} height={height} fill={color} />;
+      },
+    []
+  );
+  // const valueFormatter = (value: number, suffix = '') => {
+  //   return `${value} ${suffix}`;
+  // };
+
+  const color =
+    slo.summary.status === 'NO_DATA'
+      ? 'subdued'
+      : slo.summary.status !== 'HEALTHY'
+      ? '#f8e9e9' // #f8e9e9 (danger), #e6f9f7
+      : '#e6f9f7';
+
+  const extra = `Target ${numeral(slo.objective.target).format(percentFormat)}`;
+  const metricData = [
+    {
+      color,
+      title: slo.name,
+      subtitle: slo.groupBy === '*' ? `${slo.groupBy}:${slo.instanceId}` : '',
+      icon: getIcon('visGauge'),
+      value:
+        slo.summary.status === 'NO_DATA'
+          ? NOT_AVAILABLE_LABEL
+          : numeral(Number.parseFloat(slo.summary.sliValue.toString())).format(percentFormat),
+      valueFormatter: (value: number) => `${value}%`,
+      extra,
+      trend: [],
+      trendShape: MetricTrendShape.Area,
+    },
+  ];
   return (
-    <EuiPanel paddingSize="m" color="transparent">
-      <EuiFlexGroup direction="column" gutterSize="xs" data-test-subj="sloList">
-        <EuiFlexItem grow={false}>
-          <EuiTitle size="l">
-            <h1>{slo.name}</h1>
-          </EuiTitle>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiFlexGroup direction="row" responsive={false}>
-            <SloStatusBadge slo={slo} />
-            <SloActiveAlertsBadge slo={slo} activeAlerts={1} />
-          </EuiFlexGroup>
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>{slo.summary ? <SloSummary slo={slo} /> : null}</EuiFlexItem>
-      </EuiFlexGroup>
-    </EuiPanel>
+    <Chart>
+      <Metric id="1" data={[metricData]} />
+    </Chart>
   );
 }
