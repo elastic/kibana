88220e3841a590b36e9d7c8838aca31f13a8c43e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-09-18 18:31:40 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- src/plugins/controls/public/options_list/components/options_list_popover.test.tsx
+++ src/plugins/controls/public/options_list/components/options_list_popover.test.tsx
@@ -17,7 +17,7 @@ import { mockOptionsListEmbeddable } from '../../../common/mocks';
 import { ControlOutput, OptionsListEmbeddableInput } from '../..';
 import { OptionsListComponentState, OptionsListReduxState } from '../types';
 import { OptionsListEmbeddableContext } from '../embeddable/options_list_embeddable';
-import { OptionsListPopover, OptionsListPopoverProps } from './options_list_popover';
+import { OptionsListPopover } from './options_list_popover';
 import { pluginServices } from '../../services';
 
 describe('Options list popover', () => {
