093abe6e40afe0ab16a92dcff29b6c58c8039734
jennypavlova <dzheni.pavlova@elastic.co>
2023-09-19 10:43:36 +0200

    [Infra UI] Use i18n in Lens formulas labels (#166649)
    Closes [#166386](https://github.com/elastic/kibana/issues/166386)
    
    ## Summary
    
    This PR adds i18n translation to lens formulas
    
    ## Testing
    
    Check if all formula labels have i18n translation. Follow [this
    guide](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/GUIDELINE.md)
    to check if the translations are correctly shown

--- x-pack/plugins/infra/public/common/visualizations/lens/formulas/host/memory_usage.ts
+++ x-pack/plugins/infra/public/common/visualizations/lens/formulas/host/memory_usage.ts
@@ -5,10 +5,13 @@
  * 2.0.
  */
 
+import { i18n } from '@kbn/i18n';
 import type { FormulaValueConfig } from '@kbn/lens-embeddable-utils';
 
 export const memoryUsage: FormulaValueConfig = {
-  label: 'Memory Usage',
+  label: i18n.translate('xpack.infra.assetDetails.formulas.memoryUsage', {
+    defaultMessage: 'Memory Usage',
+  }),
   value: 'average(system.memory.actual.used.pct)',
   format: {
     id: 'percent',
