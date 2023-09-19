4a46432789f60755c5e4fcda3ca33b79668fe192
Jan Monschke <jan.monschke@elastic.co>
2023-09-19 16:04:24 +0200

    rename timeline-saving-related components
    SaveTimelineButton -> EditTimelineButton
    
    TimelineTitleAndDescription -> EditTimelineModal

--- x-pack/plugins/security_solution/public/timelines/components/timeline/header/edit_timeline_button.test.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/header/edit_timeline_button.test.tsx
@@ -7,15 +7,15 @@
 
 import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import type { SaveTimelineComponentProps } from './save_timeline_button';
-import { SaveTimelineButton } from './save_timeline_button';
+import type { EditTimelineComponentProps } from './edit_timeline_button';
+import { EditTimelineButton } from './edit_timeline_button';
 import { TestProviders } from '../../../../common/mock';
 import { useUserPrivileges } from '../../../../common/components/user_privileges';
 
 const TEST_ID = {
-  SAVE_TIMELINE_MODAL: 'save-timeline-modal',
-  SAVE_TIMELINE_BTN: 'save-timeline-button-icon',
-  SAVE_TIMELINE_TOOLTIP: 'save-timeline-tooltip',
+  EDIT_TIMELINE_MODAL: 'edit-timeline-modal',
+  EDIT_TIMELINE_BTN: 'edit-timeline-button-icon',
+  EDIT_TIMELINE_TOOLTIP: 'edit-timeline-tooltip',
 };
 
 jest.mock('react-redux', () => {
@@ -36,9 +36,9 @@ const props = {
   toolTip: 'tooltip message',
 };
 
-const TestSaveTimelineButton = (_props: SaveTimelineComponentProps) => (
+const TestEditTimelineButton = (_props: EditTimelineComponentProps) => (
   <TestProviders>
-    <SaveTimelineButton {..._props} />
+    <EditTimelineButton {..._props} />
   </TestProviders>
 );
 
@@ -46,7 +46,7 @@ jest.mock('raf', () => {
   return jest.fn().mockImplementation((cb) => cb());
 });
 
-describe('SaveTimelineButton', () => {
+describe('EditTimelineButton', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
@@ -55,10 +55,10 @@ describe('SaveTimelineButton', () => {
   //
   // Raised a bug with eui team: https://github.com/elastic/eui/issues/6065
   xit('Show tooltip', async () => {
-    render(<TestSaveTimelineButton {...props} />);
-    const saveTimelineIcon = screen.queryAllByTestId(TEST_ID.SAVE_TIMELINE_BTN)[0];
+    render(<TestEditTimelineButton {...props} />);
+    const editTimelineIcon = screen.queryAllByTestId(TEST_ID.EDIT_TIMELINE_BTN)[0];
 
-    fireEvent.mouseOver(saveTimelineIcon);
+    fireEvent.mouseOver(editTimelineIcon);
 
     jest.runAllTimers();
 
@@ -68,9 +68,9 @@ describe('SaveTimelineButton', () => {
   });
 
   it('should show a button with pencil icon', () => {
-    render(<TestSaveTimelineButton {...props} />);
+    render(<TestEditTimelineButton {...props} />);
 
-    expect(screen.getByTestId(TEST_ID.SAVE_TIMELINE_BTN).firstChild).toHaveAttribute(
+    expect(screen.getByTestId(TEST_ID.EDIT_TIMELINE_BTN).firstChild).toHaveAttribute(
       'data-euiicon-type',
       'pencil'
     );
@@ -82,26 +82,26 @@ describe('SaveTimelineButton', () => {
     });
     render(
       <TestProviders>
-        <SaveTimelineButton {...props} />
+        <EditTimelineButton {...props} />
       </TestProviders>
     );
-    expect(screen.getByTestId(TEST_ID.SAVE_TIMELINE_BTN)).toBeDisabled();
+    expect(screen.getByTestId(TEST_ID.EDIT_TIMELINE_BTN)).toBeDisabled();
   });
 
   it('should not show modal if user does not have write access', async () => {
     (useUserPrivileges as jest.Mock).mockReturnValue({
       kibanaSecuritySolutionsPrivileges: { crud: false },
     });
-    render(<TestSaveTimelineButton {...props} />);
+    render(<TestEditTimelineButton {...props} />);
 
-    expect(screen.queryByTestId(TEST_ID.SAVE_TIMELINE_MODAL)).not.toBeInTheDocument();
+    expect(screen.queryByTestId(TEST_ID.EDIT_TIMELINE_MODAL)).not.toBeInTheDocument();
 
-    const saveTimelineIcon = screen.getByTestId(TEST_ID.SAVE_TIMELINE_BTN);
+    const editTimelineIcon = screen.getByTestId(TEST_ID.EDIT_TIMELINE_BTN);
 
-    fireEvent.click(saveTimelineIcon);
+    fireEvent.click(editTimelineIcon);
 
     await waitFor(() => {
-      expect(screen.queryAllByTestId(TEST_ID.SAVE_TIMELINE_MODAL)).toHaveLength(0);
+      expect(screen.queryAllByTestId(TEST_ID.EDIT_TIMELINE_MODAL)).toHaveLength(0);
     });
   });
 
@@ -109,16 +109,16 @@ describe('SaveTimelineButton', () => {
     (useUserPrivileges as jest.Mock).mockReturnValue({
       kibanaSecuritySolutionsPrivileges: { crud: true },
     });
-    render(<TestSaveTimelineButton {...props} />);
-    expect(screen.queryByTestId(TEST_ID.SAVE_TIMELINE_MODAL)).not.toBeInTheDocument();
+    render(<TestEditTimelineButton {...props} />);
+    expect(screen.queryByTestId(TEST_ID.EDIT_TIMELINE_MODAL)).not.toBeInTheDocument();
 
-    const saveTimelineIcon = screen.queryAllByTestId(TEST_ID.SAVE_TIMELINE_BTN)[0];
+    const editTimelineIcon = screen.queryAllByTestId(TEST_ID.EDIT_TIMELINE_BTN)[0];
 
-    fireEvent.click(saveTimelineIcon);
+    fireEvent.click(editTimelineIcon);
 
     await waitFor(() => {
-      expect(screen.queryByTestId(TEST_ID.SAVE_TIMELINE_TOOLTIP)).not.toBeInTheDocument();
-      expect(screen.queryAllByTestId(TEST_ID.SAVE_TIMELINE_MODAL)[0]).toBeVisible();
+      expect(screen.queryByTestId(TEST_ID.EDIT_TIMELINE_TOOLTIP)).not.toBeInTheDocument();
+      expect(screen.queryAllByTestId(TEST_ID.EDIT_TIMELINE_MODAL)[0]).toBeVisible();
     });
   });
 });
