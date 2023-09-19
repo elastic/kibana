a42fac9ae4ff2bd74373f6757821b06a9ed7a0f4
Quynh Nguyen <quynh.nguyen@elastic.co>
2023-09-19 11:20:20 -0500

    update invalidate query

--- x-pack/plugins/transform/public/app/common/reauthorization_utils.ts
+++ x-pack/plugins/transform/public/app/common/reauthorization_utils.ts
@@ -9,11 +9,12 @@ import { isPopulatedObject } from '@kbn/ml-is-populated-object';
 import type { TransformHealthIssue } from '../../../common/types/transform_stats';
 import { TRANSFORM_HEALTH } from '../../../common/constants';
 import type { TransformListRow } from './transform_list';
+import { isTransformListRowWithStats } from './transform_list';
 
 export const needsReauthorization = (transform: Partial<TransformListRow>) => {
   return (
+    isTransformListRowWithStats(transform) &&
     isPopulatedObject(transform.config?.authorization, ['api_key']) &&
-    isPopulatedObject(transform.stats) &&
     transform.stats.health.status === TRANSFORM_HEALTH.red &&
     transform.stats.health.issues?.find(
       (issue) => (issue as TransformHealthIssue).issue === 'Privileges check failed'
