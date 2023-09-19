007011765e41e68f1f84b19abba645feedefbac6
Kate Patticha <aikaterini.patticha@elastic.co>
2023-09-19 23:26:43 +0200

    Add unlink option

---
+++ x-pack/plugins/apm/public/components/app/service_dashboards/actions/unlink_dashboard.tsx
@@ -0,0 +1,107 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { EuiButtonEmpty, EuiConfirmModal } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import React, { useCallback, useState } from 'react';
+import { SavedServiceDashboard } from '../../../../../common/service_dashboards';
+import { useApmPluginContext } from '../../../../context/apm_plugin/use_apm_plugin_context';
+import { callApmApi } from '../../../../services/rest/create_call_apm_api';
+
+export function UnlinkDashboard({
+  selectedDashboard,
+  onRefresh,
+}: {
+  selectedDashboard: SavedServiceDashboard;
+  onRefresh: () => void;
+}) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const {
+    core: { notifications },
+  } = useApmPluginContext();
+
+  const onConfirm = useCallback(
+    async function () {
+      try {
+        await callApmApi('DELETE /internal/apm/service-dashboard', {
+          params: { query: { serviceDashboardId: selectedDashboard.id } },
+          signal: null,
+        });
+
+        notifications.toasts.addSuccess({
+          title: i18n.translate(
+            'xpack.apm.serviceDashboards.unlinkSuccess.toast.title',
+            {
+              defaultMessage: 'Unlinked "{dashboardName}" dashboard',
+              values: { dashboardName: selectedDashboard.dashboardTitle },
+            }
+          ),
+        });
+        onRefresh();
+      } catch (error) {
+        console.error(error);
+        notifications.toasts.addDanger({
+          title: i18n.translate(
+            'xpack.apm.serviceDashboards.unlinkFailure.toast.title',
+            {
+              defaultMessage:
+                'Error while unlinking "{dashboardName}" dashboard',
+              values: { dashboardName: selectedDashboard.dashboardTitle },
+            }
+          ),
+          text: error.body.message,
+        });
+      }
+      setIsModalVisible(!isModalVisible);
+    },
+    [selectedDashboard, notifications.toasts]
+  );
+  return (
+    <>
+      <EuiButtonEmpty
+        color="danger"
+        size="s"
+        iconType={'unlink'}
+        data-test-subj="apmUnLinkServiceDashboardMenu"
+        onClick={() => setIsModalVisible(true)}
+      >
+        {i18n.translate('xpack.apm.serviceDashboards.unlinkEmptyButtonLabel', {
+          defaultMessage: 'Unlink dashboard',
+        })}
+      </EuiButtonEmpty>
+      {isModalVisible && (
+        <EuiConfirmModal
+          title={i18n.translate(
+            'xpack.apm.serviceDashboards.unlinkEmptyButtonLabel.confirm.title',
+            {
+              defaultMessage: 'Unlink Dashboard',
+            }
+          )}
+          onCancel={() => setIsModalVisible(false)}
+          onConfirm={onConfirm}
+          confirmButtonText={i18n.translate(
+            'xpack.apm.serviceDashboards.unlinkEmptyButtonLabel.confirm.button',
+            {
+              defaultMessage: 'Unlink dashboard',
+            }
+          )}
+          buttonColor="danger"
+          defaultFocusedButton="confirm"
+        >
+          <p>
+            {i18n.translate(
+              'xpack.apm.serviceDashboards.unlinkEmptyButtonLabel.confirm.body',
+              {
+                defaultMessage:
+                  'You are about to unlink the dashboard from the service context',
+              }
+            )}
+          </p>
+        </EuiConfirmModal>
+      )}
+    </>
+  );
+}
---
+++ x-pack/plugins/apm/server/routes/service_dashboards/remove_service_dashboard.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { SavedObjectsClientContract } from '@kbn/core/server';
+import { APM_SERVICE_DASHBOARD_SAVED_OBJECT_TYPE } from '../../../common/service_dashboards';
+
+interface Options {
+  savedObjectsClient: SavedObjectsClientContract;
+  serviceDashboardId: string;
+}
+export async function deleteServiceDashboard({
+  savedObjectsClient,
+  serviceDashboardId,
+}: Options) {
+  return savedObjectsClient.delete(
+    APM_SERVICE_DASHBOARD_SAVED_OBJECT_TYPE,
+    serviceDashboardId
+  );
+}
