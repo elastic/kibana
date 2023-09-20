2487946ff715cbe0569dabdf78609c1bc712adc9
Angela Chuang <yi-chun.chuang@elastic.co>
2023-09-20 13:44:43 +0100

    unit tests

--- src/plugins/home/jest.config.js
+++ src/plugins/home/jest.config.js
@@ -15,5 +15,6 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/plugins/home/{common,public,server}/**/*.{js,ts,tsx}',
     '<rootDir>/src/plugins/home/server/services/sample_data/routes/*.{js,ts,tsx}',
+    '<rootDir>/src/plugins/home/server/services/sample_data/data_set/security_solution/*.{js,ts,tsx}',
   ],
 };
