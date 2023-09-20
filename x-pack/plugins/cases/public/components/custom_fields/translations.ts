99ec3673ce00bb174e2addd082ef2f3b8a59dd48
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-09-20 08:07:19 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/cases/public/components/custom_fields/translations.ts
+++ x-pack/plugins/cases/public/components/custom_fields/translations.ts
@@ -14,8 +14,7 @@ export const TITLE = i18n.translate('xpack.cases.customFields.title', {
 });
 
 export const DESCRIPTION = i18n.translate('xpack.cases.customFields.description', {
-  defaultMessage:
-    'Add more optional and required fields for customized case collaboration.',
+  defaultMessage: 'Add more optional and required fields for customized case collaboration.',
 });
 
 export const NO_CUSTOM_FIELDS = i18n.translate('xpack.cases.customFields.noCustomFields', {
