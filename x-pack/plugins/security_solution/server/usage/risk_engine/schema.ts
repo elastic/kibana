d84cd24ac8a05ae2de87a77a9b6874dd8f149c9c
Nikita Khristinin <nkhristinin@gmail.com>
2023-09-20 12:14:24 +0200

    Remove risk scores fields from schema

--- x-pack/plugins/security_solution/server/usage/risk_engine/schema.ts
+++ x-pack/plugins/security_solution/server/usage/risk_engine/schema.ts
@@ -9,66 +9,64 @@ import type { MakeSchemaFrom } from '@kbn/usage-collection-plugin/server';
 import type { RiskEngineMetrics } from './types';
 
 export const riskEngineMetricsSchema: MakeSchemaFrom<RiskEngineMetrics> = {
-  risk_scores: {
-    unique_user_risk_score_total: {
-      type: 'long',
-      _meta: {
-        description: 'Total unique user risk scores',
-      },
+  unique_user_risk_score_total: {
+    type: 'long',
+    _meta: {
+      description: 'Total unique user risk scores',
     },
-    unique_host_risk_score_total: {
-      type: 'long',
-      _meta: {
-        description: 'Total unique host risk scores',
-      },
+  },
+  unique_host_risk_score_total: {
+    type: 'long',
+    _meta: {
+      description: 'Total unique host risk scores',
     },
-    unique_user_risk_score_day: {
-      type: 'long',
-      _meta: {
-        description: 'Unique user risk scores per day',
-      },
+  },
+  unique_user_risk_score_day: {
+    type: 'long',
+    _meta: {
+      description: 'Unique user risk scores per day',
     },
-    unique_host_risk_score_day: {
-      type: 'long',
-      _meta: {
-        description: 'Unique host risk scores per day',
-      },
+  },
+  unique_host_risk_score_day: {
+    type: 'long',
+    _meta: {
+      description: 'Unique host risk scores per day',
     },
-    all_host_risk_scores_total: {
-      type: 'long',
-      _meta: {
-        description: 'Total number of host risk score executions',
-      },
+  },
+  all_host_risk_scores_total: {
+    type: 'long',
+    _meta: {
+      description: 'Total number of host risk score executions',
     },
-    all_user_risk_scores_total: {
-      type: 'long',
-      _meta: {
-        description: 'Total number of user risk score executions',
-      },
+  },
+  all_user_risk_scores_total: {
+    type: 'long',
+    _meta: {
+      description: 'Total number of user risk score executions',
     },
-    all_host_risk_scores_total_day: {
-      type: 'long',
-      _meta: {
-        description: 'Number of host risk score executions per day',
-      },
+  },
+  all_host_risk_scores_total_day: {
+    type: 'long',
+    _meta: {
+      description: 'Number of host risk score executions per day',
     },
-    all_user_risk_scores_total_day: {
-      type: 'long',
-      _meta: {
-        description: 'Number of user risk score executions per day',
-      },
+  },
+  all_user_risk_scores_total_day: {
+    type: 'long',
+    _meta: {
+      description: 'Number of user risk score executions per day',
     },
-    all_risk_scores_index_size: {
-      type: 'long',
-      _meta: {
-        description: 'Total size of the Risk Score indice (MB)',
-      },
+  },
+  all_risk_scores_index_size: {
+    type: 'long',
+    _meta: {
+      description: 'Total size of the Risk Score indice (MB)',
     },
-    unique_risk_scores_index_size: {
-      type: 'long',
-      _meta: {
-        description: 'Total size of the Risk Score indice (MB)',
-      },
+  },
+  unique_risk_scores_index_size: {
+    type: 'long',
+    _meta: {
+      description: 'Total size of the Risk Score indice (MB)',
     },
   },
 };
