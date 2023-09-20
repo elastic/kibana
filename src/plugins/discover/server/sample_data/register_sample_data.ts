12f0ce23570c92f1dbc576e270aeb6a948c9fe26
Angela Chuang <yi-chun.chuang@elastic.co>
2023-09-20 20:34:23 +0100

    revert

--- src/plugins/discover/server/sample_data/register_sample_data.ts
+++ src/plugins/discover/server/sample_data/register_sample_data.ts
@@ -28,7 +28,6 @@ export function registerSampleData(sampleDataRegistry: SampleDataRegistrySetup) 
     const sampleSavedSearchObject = sampleDataset.savedObjects.find(
       (object) => object.type === 'search'
     );
-    console.log('sampleDataset====', sampleDataset.id);
 
     if (sampleSavedSearchObject) {
       addAppLinksToSampleDataset(sampleDataset.id, [
