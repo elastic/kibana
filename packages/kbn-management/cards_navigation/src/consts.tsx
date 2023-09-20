473479b69fdbb3340223106f82a7e3ce9480efa5
István Zoltán Szabó <szabosteve@gmail.com>
2023-09-20 16:26:35 +0200

    [ML] Amends the ML app description on the management page.

--- packages/kbn-management/cards_navigation/src/consts.tsx
+++ packages/kbn-management/cards_navigation/src/consts.tsx
@@ -81,7 +81,7 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     category: appCategories.DATA,
     description: i18n.translate('management.landing.withCardNavigation.mlDescription', {
       defaultMessage:
-        'View, export, and import machine learning analytics and anomaly detection items.',
+        'View, export, and import analytics, anomaly detection, and trained model items.',
     }),
     icon: <EuiIcon size="l" type="indexMapping" />,
   },
