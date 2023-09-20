1e26ef6df6d75be4cad8cc254e427899acb38549
Caue Marcondes <caue.marcondes@elastic.co>
2023-09-20 15:07:29 +0100

    adding co2 section and add better desc

--- x-pack/plugins/observability/server/ui_settings.ts
+++ x-pack/plugins/observability/server/ui_settings.ts
@@ -380,12 +380,11 @@ export const uiSettings: Record<string, UiSettings> = {
   [profilingPerCoreWatt]: {
     category: [observabilityFeatureId],
     name: i18n.translate('xpack.observability.profilingPerCoreWattUiSettingName', {
-      defaultMessage: 'Per core watts',
+      defaultMessage: 'Per Core Watts',
     }),
     value: 7,
     description: i18n.translate('xpack.observability.profilingPerCoreWattUiSettingDescription', {
-      defaultMessage:
-        'The assumed amortized per-core average power consumption (based on 100% CPU Utilization)',
+      defaultMessage: `The average amortized per-core power consumption (based on 100% CPU utilization).`,
     }),
     schema: schema.number({ min: 0 }),
     requiresPageReload: false,
@@ -393,11 +392,28 @@ export const uiSettings: Record<string, UiSettings> = {
   [profilingDatacenterPUE]: {
     category: [observabilityFeatureId],
     name: i18n.translate('xpack.observability.profilingDatacenterPUEUiSettingName', {
-      defaultMessage: 'Datacenter PUE',
+      defaultMessage: 'Data Centre PUE',
     }),
     value: 1.7,
     description: i18n.translate('xpack.observability.profilingDatacenterPUEUiSettingDescription', {
-      defaultMessage: 'The assumed PUE(Power Usage Effectiveness) of the datacenter',
+      defaultMessage: `Data center power usage effectiveness (PUE) is a metric used to measure how efficiently a data center uses energy. The {uptimeLink} survey has found average on-premise data center PUE to  be 1.7.  
+      </br></br>
+      The Cloud Provider PUEs are: 
+      <ul style="list-style-type: none;margin-left: 4px;">
+        <li><strong>AWS:</strong> 1.135</li>
+        <li><strong>GCP:</strong> 1.1</li>
+        <li><strong>Azure:</strong> 1.185</li>
+      </ul>
+      `,
+      values: {
+        uptimeLink:
+          '<a href="https://ela.st/uptimeinstitute" target="_blank" rel="noopener noreferrer">' +
+          i18n.translate(
+            'xpack.observability.profilingDatacenterPUEUiSettingDescription.uptimeLink',
+            { defaultMessage: 'Uptime Institute' }
+          ) +
+          '</a>',
+      },
     }),
     schema: schema.number({ min: 0 }),
     requiresPageReload: false,
@@ -405,11 +421,22 @@ export const uiSettings: Record<string, UiSettings> = {
   [profilingCo2PerKWH]: {
     category: [observabilityFeatureId],
     name: i18n.translate('xpack.observability.profilingCo2PerKWHUiSettingName', {
-      defaultMessage: 'CO2 per KWH',
+      defaultMessage: 'Regional Carbon Intensity (ton/kWh)',
     }),
-    value: 0.379069,
+    value: 0.000379069,
     description: i18n.translate('xpack.observability.profilingCo2PerKWHUiSettingDescription', {
-      defaultMessage: 'The assumed CO2 emissions in kg per kWh',
+      defaultMessage: `Carbon intensity is a measure of how clean your data centre electricity is.  
+      It is a measure of the average amount of CO2 emitted per kilowatt-hour (kWh) of electricity consumed in a particular region.
+      You may use the cloud carbon footprint {datasheetLink} to update this value. Defaults to US East (N. Virginia).`,
+      values: {
+        datasheetLink:
+          '<a href="https://ela.st/grid-datasheet" target="_blank" rel="noopener noreferrer">' +
+          i18n.translate(
+            'xpack.observability.profilingCo2PerKWHUiSettingDescription.datasheetLink',
+            { defaultMessage: 'datasheet' }
+          ) +
+          '</a>',
+      },
     }),
     schema: schema.number({ min: 0 }),
     requiresPageReload: false,
--- x-pack/plugins/profiling/public/views/settings/index.tsx
+++ x-pack/plugins/profiling/public/views/settings/index.tsx
@@ -5,9 +5,9 @@
  * 2.0.
  */
 
-import React from 'react';
-import { i18n } from '@kbn/i18n';
+import { EuiPanel, EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
 import { LazyField } from '@kbn/advanced-settings-plugin/public';
+import { i18n } from '@kbn/i18n';
 import {
   profilingCo2PerKWH,
   profilingDatacenterPUE,
@@ -15,9 +15,10 @@ import {
 } from '@kbn/observability-plugin/common';
 import { useEditableSettings } from '@kbn/observability-shared-plugin/public';
 import { isEmpty } from 'lodash';
+import React from 'react';
+import { useProfilingDependencies } from '../../components/contexts/profiling_dependencies/use_profiling_dependencies';
 import { ProfilingAppPageTemplate } from '../../components/profiling_app_page_template';
 import { BottomBarActions } from './bottom_bar_actions';
-import { useProfilingDependencies } from '../../components/contexts/profiling_dependencies/use_profiling_dependencies';
 
 const settingKeys = [profilingCo2PerKWH, profilingDatacenterPUE, profilingPerCoreWatt];
 
@@ -58,27 +59,43 @@ export function Settings() {
   }
 
   return (
-    <ProfilingAppPageTemplate
-      hideSearchBar
-      pageTitle={i18n.translate('xpack.profiling.settings.title', {
-        defaultMessage: 'Advanced Settings',
-      })}
-    >
+    <ProfilingAppPageTemplate hideSearchBar>
       <>
-        {settingKeys.map((settingKey) => {
-          const editableConfig = settingsEditableConfig[settingKey];
-          return (
-            <LazyField
-              key={settingKey}
-              setting={editableConfig}
-              handleChange={handleFieldChange}
-              enableSaving
-              docLinks={docLinks.links}
-              toasts={notifications.toasts}
-              unsavedChanges={unsavedChanges[settingKey]}
-            />
-          );
-        })}
+        <EuiTitle>
+          <EuiText>
+            {i18n.translate('xpack.profiling.settings.title', {
+              defaultMessage: 'Advanced Settings',
+            })}
+          </EuiText>
+        </EuiTitle>
+        <EuiSpacer />
+        <EuiPanel grow={false} hasShadow={false} hasBorder paddingSize="none">
+          <EuiPanel color="subdued" hasShadow={false}>
+            <EuiTitle size="s">
+              <EuiText>
+                {i18n.translate('xpack.profiling.settings.co2Sections', {
+                  defaultMessage: 'CO2',
+                })}
+              </EuiText>
+            </EuiTitle>
+          </EuiPanel>
+          <EuiPanel hasShadow={false}>
+            {settingKeys.map((settingKey) => {
+              const editableConfig = settingsEditableConfig[settingKey];
+              return (
+                <LazyField
+                  key={settingKey}
+                  setting={editableConfig}
+                  handleChange={handleFieldChange}
+                  enableSaving
+                  docLinks={docLinks.links}
+                  toasts={notifications.toasts}
+                  unsavedChanges={unsavedChanges[settingKey]}
+                />
+              );
+            })}
+          </EuiPanel>
+        </EuiPanel>
         {!isEmpty(unsavedChanges) && (
           <BottomBarActions
             isLoading={isSaving}
