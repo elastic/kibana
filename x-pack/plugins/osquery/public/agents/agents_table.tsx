2302954dfe0323bd803f7fff112c10b8d35b3ebb
konrad.szwarc <konrad.szwarc@elastic.co>
2023-09-20 10:42:52 +0200

    eslint

--- x-pack/plugins/osquery/public/agents/agents_table.tsx
+++ x-pack/plugins/osquery/public/agents/agents_table.tsx
@@ -204,6 +204,17 @@ const AgentsTableComponent: React.FC<AgentsTableProps> = ({ agentSelection, onCh
   const isFetched = groupsFetched && agentsFetched && agentGroupsData;
 
   const renderNoAgentAvailableWarning = () => {
+    const docsLink = {
+      docsLink: (
+        <EuiLink href={`${docLinks.links.fleet.agentPolicy}#apply-a-policy`} target={'_blank'}>
+          <FormattedMessage
+            id={'xpack.osquery.agents.noAgentAvailableDescription.docsLink'}
+            defaultMessage="Apply a policy"
+          />
+        </EuiLink>
+      ),
+    };
+
     if (isFetched && !options.length) {
       return (
         <>
@@ -211,19 +222,7 @@ const AgentsTableComponent: React.FC<AgentsTableProps> = ({ agentSelection, onCh
             <FormattedMessage
               id={'xpack.osquery.agents.noAgentAvailableDescription'}
               defaultMessage="Before you can query agents, they must be enrolled in an agent policy with the Osquery integration installed. Refer to {docsLink} for more information."
-              values={{
-                docsLink: (
-                  <EuiLink
-                    href={`${docLinks.links.fleet.agentPolicy}#apply-a-policy`}
-                    target={'_blank'}
-                  >
-                    <FormattedMessage
-                      id={'xpack.osquery.agents.noAgentAvailableDescription.docsLink'}
-                      defaultMessage="Apply a policy"
-                    />
-                  </EuiLink>
-                ),
-              }}
+              values={docsLink}
             />
           </EuiCallOut>
           <EuiSpacer size="s" />
