42130fd56c69ca29b783ca1ceff74e8e8af0ea6a
Paul Tavares <paul.tavares@elastic.co>
2023-09-20 10:10:54 -0400

    updates following resolution of merge conflicts from Gergo's PR

--- x-pack/plugins/security_solution/public/management/cypress/cypress_base.config.ts
+++ x-pack/plugins/security_solution/public/management/cypress/cypress_base.config.ts
@@ -5,51 +5,77 @@
  * 2.0.
  */
 
-export const getCypressBaseConfig = () => ({
-  reporter: '../../../../node_modules/cypress-multi-reporters',
-  reporterOptions: {
-    configFile: './public/management/reporter_config.json',
-  },
+import { merge } from 'lodash';
+import { dataLoaders } from './support/data_loaders';
+import { responseActionTasks } from './support/response_actions';
 
-  defaultCommandTimeout: 60000,
-  execTimeout: 120000,
-  pageLoadTimeout: 12000,
+export const getCypressBaseConfig = (
+  overrides: Cypress.ConfigOptions = {}
+): Cypress.ConfigOptions => {
+  return merge(
+    {
+      reporter: '../../../../node_modules/cypress-multi-reporters',
+      reporterOptions: {
+        configFile: './public/management/reporter_config.json',
+      },
 
-  retries: {
-    runMode: 1,
-    openMode: 0,
-  },
+      defaultCommandTimeout: 60000,
+      execTimeout: 120000,
+      pageLoadTimeout: 12000,
 
-  screenshotsFolder:
-    '../../../target/kibana-security-solution/public/management/cypress/screenshots',
-  trashAssetsBeforeRuns: false,
-  video: false,
-  viewportHeight: 900,
-  viewportWidth: 1440,
-  experimentalStudio: true,
+      retries: {
+        runMode: 1,
+        openMode: 0,
+      },
 
-  env: {
-    'cypress-react-selector': {
-      root: '#security-solution-app',
+      screenshotsFolder:
+        '../../../target/kibana-security-solution/public/management/cypress/screenshots',
+      trashAssetsBeforeRuns: false,
+      video: false,
+      viewportHeight: 900,
+      viewportWidth: 1440,
+      experimentalStudio: true,
+
+      env: {
+        'cypress-react-selector': {
+          root: '#security-solution-app',
+        },
+        KIBANA_URL: 'http://localhost:5601',
+        ELASTICSEARCH_URL: 'http://localhost:9200',
+        FLEET_SERVER_URL: 'https://localhost:8220',
+        KIBANA_USERNAME: 'system_indices_superuser',
+        KIBANA_PASSWORD: 'changeme',
+        ELASTICSEARCH_USERNAME: 'system_indices_superuser',
+        ELASTICSEARCH_PASSWORD: 'changeme',
+
+        // grep related configs
+        grepFilterSpecs: true,
+        grepOmitFiltered: true,
+      },
+
+      e2e: {
+        // baseUrl: To override, set Env. variable `CYPRESS_BASE_URL`
+        baseUrl: 'http://localhost:5601',
+        supportFile: 'public/management/cypress/support/e2e.ts',
+        specPattern: 'public/management/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        experimentalRunAllSpecs: true,
+        setupNodeEvents: (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+          dataLoaders(on, config);
+
+          // skip dataLoadersForRealEndpoints()
+          // https://github.com/elastic/security-team/issues/7467
+          // Data loaders specific to "real" Endpoint testing
+          // dataLoadersForRealEndpoints(on, config);
+
+          responseActionTasks(on, config);
+
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          require('@cypress/grep/src/plugin')(config);
+
+          return config;
+        },
+      },
     },
-    KIBANA_URL: 'http://localhost:5601',
-    ELASTICSEARCH_URL: 'http://localhost:9200',
-    FLEET_SERVER_URL: 'https://localhost:8220',
-    KIBANA_USERNAME: 'system_indices_superuser',
-    KIBANA_PASSWORD: 'changeme',
-    ELASTICSEARCH_USERNAME: 'system_indices_superuser',
-    ELASTICSEARCH_PASSWORD: 'changeme',
-
-    // grep related configs
-    grepFilterSpecs: true,
-    grepOmitFiltered: true,
-  },
-
-  e2e: {
-    // baseUrl: To override, set Env. variable `CYPRESS_BASE_URL`
-    baseUrl: 'http://localhost:5601',
-
-    supportFile: 'public/management/cypress/support/e2e.ts',
-    experimentalRunAllSpecs: true,
-  },
-});
+    overrides
+  );
+};
