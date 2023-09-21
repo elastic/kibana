237cbd099ec2e3fd4eeebd4e5cd7da75c8d88d05
nreese <reese.nathan@elastic.co>
2023-09-21 17:08:06 -0600

    handle skipped clusters

--- src/plugins/inspector/public/views/requests/components/details/clusters_view/clusters_table/cluster_view.test.tsx
+++ src/plugins/inspector/public/views/requests/components/details/clusters_view/clusters_table/cluster_view.test.tsx
@@ -9,42 +9,126 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import type { ClusterDetails } from '@kbn/es-types';
-import { ClusterView } from './cluster_view';
+import { ClusterView, getFailures } from './cluster_view';
+
+const clusterDetailsWithShardFailure = {
+  status: 'partial',
+  indices: 'kibana_sample_data_logs,kibana_sample_data_flights',
+  took: 5,
+  timed_out: false,
+  _shards: {
+    total: 2,
+    successful: 1,
+    skipped: 0,
+    failed: 1,
+  },
+  failures: [
+    {
+      shard: 0,
+      index: 'remote1:.ds-kibana_sample_data_logs-2023.08.21-000001',
+      node: 'NVzFRd6SS4qT9o0k2vIzlg',
+      reason: {
+        type: 'query_shard_exception',
+        reason:
+          'failed to create query: [.ds-kibana_sample_data_logs-2023.08.21-000001][0] local shard failure message 123',
+        index_uuid: 'z1sPO8E4TdWcijNgsL_BxQ',
+        index: 'remote1:.ds-kibana_sample_data_logs-2023.08.21-000001',
+        caused_by: {
+          type: 'runtime_exception',
+          reason:
+            'runtime_exception: [.ds-kibana_sample_data_logs-2023.08.21-000001][0] local shard failure message 123',
+        },
+      },
+    },
+  ],
+} as ClusterDetails;
+
+const clusterDetailsWhenClusterIsNotAvailable = {
+  "status": "skipped",
+  "indices": "kibana_sample_data*",
+  "timed_out": false,
+  "failures": [
+    {
+      "shard": -1,
+      "index": null,
+      "reason": {
+        "type": "no_such_remote_cluster_exception",
+        "reason": "no such remote cluster: [remote1]"
+      }
+    }
+  ]
+} as ClusterDetails;
+
+describe('getFailures', () => {
+  test('should get failures from response with shard failures', () => {
+    expect(getFailures(clusterDetailsWithShardFailure)).toEqual({
+      clusterFailures: [],
+      shardFailures: clusterDetailsWithShardFailure.failures,
+    });
+  });
+
+  test('should get failures from response when cluster is not accessable', () => {
+    expect(getFailures(clusterDetailsWhenClusterIsNotAvailable)).toEqual({
+      clusterFailures: [clusterDetailsWhenClusterIsNotAvailable.failures[0]],
+      shardFailures: [],
+    });
+  });
+
+  test('should get failures from response when all shards fail', () => {
+    const clusterDetails = {
+          "status": "skipped",
+          "indices": "kibana_sample_data*",
+          "timed_out": false,
+          "failures": [
+            {
+              "shard": -1,
+              "index": null,
+              "reason": {
+                "type": "search_phase_execution_exception",
+                "reason": "all shards failed",
+                "phase": "query",
+                "grouped": true,
+                "failed_shards": [
+                  {
+                    "shard": 0,
+                    "index": "remote1:.ds-kibana_sample_data_logs-2023.09.21-000001",
+                    "node": "_JVoOnN5QKidGGXFJAlgpA",
+                    "reason": {
+                      "type": "query_shard_exception",
+                      "reason": "failed to create query: [.ds-kibana_sample_data_logs-2023.09.21-000001][0] local shard failure message 123",
+                      "index_uuid": "PAa7v-dKRIyo4kv6b8dxkQ",
+                      "index": "remote1:.ds-kibana_sample_data_logs-2023.09.21-000001",
+                      "caused_by": {
+                        "type": "runtime_exception",
+                        "reason": "runtime_exception: [.ds-kibana_sample_data_logs-2023.09.21-000001][0] local shard failure message 123"
+                      }
+                    }
+                  }
+                ],
+                "caused_by": {
+                  "type": "query_shard_exception",
+                  "reason": "failed to create query: [.ds-kibana_sample_data_logs-2023.09.21-000001][0] local shard failure message 123",
+                  "index_uuid": "PAa7v-dKRIyo4kv6b8dxkQ",
+                  "index": "remote1:.ds-kibana_sample_data_logs-2023.09.21-000001",
+                  "caused_by": {
+                    "type": "runtime_exception",
+                    "reason": "runtime_exception: [.ds-kibana_sample_data_logs-2023.09.21-000001][0] local shard failure message 123"
+                  }
+                }
+              }
+            }
+          ]
+        };
+    expect(getFailures(clusterDetails)).toEqual({
+      clusterFailures: [clusterDetails.failures[0]],
+      shardFailures: clusterDetails.failures[0].reason.failed_shards,
+    });
+  });
+});
 
 describe('render', () => {
   test('should show failures', () => {
-    const clusterDetails = {
-      status: 'partial',
-      indices: 'kibana_sample_data_logs,kibana_sample_data_flights',
-      took: 5,
-      timed_out: false,
-      _shards: {
-        total: 2,
-        successful: 1,
-        skipped: 0,
-        failed: 1,
-      },
-      failures: [
-        {
-          shard: 0,
-          index: 'remote1:.ds-kibana_sample_data_logs-2023.08.21-000001',
-          node: 'NVzFRd6SS4qT9o0k2vIzlg',
-          reason: {
-            type: 'query_shard_exception',
-            reason:
-              'failed to create query: [.ds-kibana_sample_data_logs-2023.08.21-000001][0] local shard failure message 123',
-            index_uuid: 'z1sPO8E4TdWcijNgsL_BxQ',
-            index: 'remote1:.ds-kibana_sample_data_logs-2023.08.21-000001',
-            caused_by: {
-              type: 'runtime_exception',
-              reason:
-                'runtime_exception: [.ds-kibana_sample_data_logs-2023.08.21-000001][0] local shard failure message 123',
-            },
-          },
-        },
-      ],
-    } as ClusterDetails;
-    const wrapper = shallow(<ClusterView clusterDetails={clusterDetails} />);
+    const wrapper = shallow(<ClusterView clusterDetails={clusterDetailsWithShardFailure} />);
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -64,4 +148,9 @@ describe('render', () => {
     const wrapper = shallow(<ClusterView clusterDetails={clusterDetails} />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  test('should display callout when cluster is skipped', () => {
+    const wrapper = shallow(<ClusterView clusterDetails={clusterDetailsWhenClusterIsNotAvailable} />);
+    expect(wrapper).toMatchSnapshot();
+  })
 });
--- src/plugins/inspector/public/views/requests/components/details/clusters_view/clusters_table/cluster_view.tsx
+++ src/plugins/inspector/public/views/requests/components/details/clusters_view/clusters_table/cluster_view.tsx
@@ -7,16 +7,38 @@
  */
 
 import React from 'react';
+import { estypes } from '@elastic/elasticsearch';
 import { i18n } from '@kbn/i18n';
 import type { ClusterDetails } from '@kbn/es-types';
 import { EuiCallOut, EuiText } from '@elastic/eui';
 import { ShardsView } from './shards_view';
+
+export function getFailures(clusterDetails: ClusterDetails) {
+  const clusterFailures: estypes.ShardFailure[] = [];
+  const shardFailures: estypes.ShardFailure[] = [];
+  (clusterDetails.failures ?? []).forEach(failure => {
+    if (failure.shard < 0) {
+      clusterFailures.push(failure);
+      (failure?.reason?.failed_shards ?? []).forEach(reasonFailure => {
+        shardFailures.push(reasonFailure);
+      });
+    } else {
+      shardFailures.push(failure);
+    }
+  });
+
+  return {
+    clusterFailures,
+    shardFailures,
+  };
+}
 
 interface Props {
   clusterDetails: ClusterDetails;
 }
 
 export function ClusterView({ clusterDetails }: Props) {
+  const { clusterFailures, shardFailures } = getFailures(clusterDetails);
   return (
     <EuiText style={{ width: '100%' }} size="xs">
       {clusterDetails.timed_out ? (
@@ -31,7 +53,31 @@ export function ClusterView({ clusterDetails }: Props) {
         />
       ) : null}
 
-      <ShardsView failures={clusterDetails.failures ?? []} shardStats={clusterDetails._shards} />
+      {clusterDetails.status === 'skipped' ? (
+        <EuiCallOut
+          size="s"
+          color="warning"
+          title={i18n.translate('inspector.requests.clusters.skippedClusterTitle', {
+            defaultMessage:
+              'Cluster skipped',
+          })}
+          iconType="warning"
+        >
+          <p>
+            {i18n.translate('inspector.requests.clusters.skippedClusterMessage', {
+              defaultMessage:
+                'Search failed{reason}',
+                values: {
+                  reason: clusterFailures[0]?.reason?.reason
+                    ? `, ${clusterFailures[0].reason.reason}`
+                    : '.'
+                }
+            })}
+          </p>
+        </EuiCallOut>
+      ) : null}
+
+      <ShardsView failures={shardFailures} shardStats={clusterDetails._shards} />
     </EuiText>
   );
 }
