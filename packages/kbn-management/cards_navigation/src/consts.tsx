de32c8ee6e789330f09a05e74b57f96a4982ebad
István Zoltán Szabó <szabosteve@gmail.com>
2023-09-21 08:26:36 +0200

    [ML] Amends ML intro text.

--- packages/kbn-management/cards_navigation/src/consts.tsx
+++ packages/kbn-management/cards_navigation/src/consts.tsx
@@ -81,7 +81,7 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     category: appCategories.DATA,
     description: i18n.translate('management.landing.withCardNavigation.mlDescription', {
       defaultMessage:
-        'View, export, and import analytics, anomaly detection, and trained model items.',
+        'View, export, and import anomaly detection, analytics, and trained model items.',
     }),
     icon: <EuiIcon size="l" type="indexMapping" />,
   },
