a6babf2922a3a87033c5cbec585abe500855f2f2
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2023-09-21 14:31:14 -0400

    fix view rule list in o11y

--- x-pack/plugins/observability/public/pages/rules/rules_tab.tsx
+++ x-pack/plugins/observability/public/pages/rules/rules_tab.tsx
@@ -11,7 +11,7 @@ import { createKbnUrlStateStorage } from '@kbn/kibana-utils-plugin/public';
 import { RuleStatus } from '@kbn/triggers-actions-ui-plugin/public';
 import { useKibana } from '../../utils/kibana_react';
 import { useGetFilteredRuleTypes } from '../../hooks/use_get_filtered_rule_types';
-import { observabilityRuleCreationValidConsumers } from '../../../common/constants';
+import { observabilityAlertFeatureIds } from '../../../common/constants';
 
 interface RulesTabProps {
   setRefresh: React.Dispatch<React.SetStateAction<Date>>;
@@ -74,7 +74,7 @@ export function RulesTab({ setRefresh, stateRefresh }: RulesTabProps) {
 
   return (
     <RuleList
-      filterConsumers={observabilityRuleCreationValidConsumers}
+      filterConsumers={observabilityAlertFeatureIds}
       filteredRuleTypes={filteredRuleTypes}
       lastRunOutcomeFilter={stateLastResponse}
       refresh={stateRefresh}
