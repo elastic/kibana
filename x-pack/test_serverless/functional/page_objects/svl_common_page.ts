d96ee7ddb550122f4786e1aa8271e5316602b32c
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-09-21 15:24:59 +0200

    [ftr] pass password for UI login + retry login via API in advance

--- x-pack/test_serverless/functional/page_objects/svl_common_page.ts
+++ x-pack/test_serverless/functional/page_objects/svl_common_page.ts
@@ -5,17 +5,64 @@
  * 2.0.
  */
 
+import Url from 'url';
 import { FtrProviderContext } from '../ftr_provider_context';
 
 export function SvlCommonPageProvider({ getService, getPageObjects }: FtrProviderContext) {
   const testSubjects = getService('testSubjects');
   const config = getService('config');
   const pageObjects = getPageObjects(['security']);
+  const retry = getService('retry');
+  const kibanaServer = getService('kibanaServer');
+  const log = getService('log');
+
+  const delay = (ms: number) =>
+    new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
 
   return {
     async login() {
       await pageObjects.security.forceLogout({ waitForLoginPage: false });
-      return await pageObjects.security.login(config.get('servers.kibana.username'));
+
+      await retry.waitForWithTimeout(
+        'Login successfully via API',
+        10_000,
+        async () => {
+          const response = await kibanaServer.request({
+            method: 'POST',
+            path: Url.format({
+              protocol: config.get('servers.kibana.protocol'),
+              hostname: config.get('servers.kibana.hostname'),
+              pathname: 'internal/security/login',
+            }),
+            body: {
+              providerType: 'basic',
+              providerName: 'cloud-basic',
+              currentURL: Url.format({
+                protocol: config.get('servers.kibana.protocol'),
+                hostname: config.get('servers.kibana.hostname'),
+                pathname: 'login',
+              }),
+              params: {
+                username: config.get('servers.kibana.username'),
+                password: config.get('servers.kibana.password'),
+              },
+            },
+          });
+          log.debug(`Login via API completed with status: ${response.status}`);
+          return response.status === 200;
+        },
+        async () => {
+          log.debug('Waiting 2000 ms before retry');
+          await delay(2000);
+        }
+      );
+
+      return await pageObjects.security.login(
+        config.get('servers.kibana.username'),
+        config.get('servers.kibana.password')
+      );
     },
 
     async forceLogout() {
