04134c69f0210e5fbf4c0c689f247ddb3da0dc27
Dima Arnautov <dmitrii.arnautov@elastic.co>
2023-09-22 12:35:00 +0200

    remove unused code, update shared export

--- x-pack/plugins/ml/public/application/services/get_shared_ml_services.ts
+++ x-pack/plugins/ml/public/application/services/get_shared_ml_services.ts
@@ -6,34 +6,20 @@
  */
 
 import { type HttpStart } from '@kbn/core-http-browser';
-import { type UsageCollectionSetup } from '@kbn/usage-collection-plugin/public';
 import { ElasticModels } from './elastic_models_service';
 import { HttpService } from './http_service';
 import { mlApiServicesProvider } from './ml_api_service';
-import { mlUsageCollectionProvider } from './usage_collection';
-import { MlCapabilitiesService } from '../capabilities/check_capabilities';
-import { MlLicense } from '../../../common/license';
 
 export type MlSharedServices = ReturnType<typeof getMlSharedServices>;
 
 /**
- * Provides global services available across the entire ML app.
+ * Provides ML services exposed from the plugin start.
  */
-export function getMlSharedServices(
-  httpStart: HttpStart,
-  isServerless: boolean,
-  usageCollection?: UsageCollectionSetup
-) {
+export function getMlSharedServices(httpStart: HttpStart) {
   const httpService = new HttpService(httpStart);
   const mlApiServices = mlApiServicesProvider(httpService);
 
   return {
-    httpService,
-    mlApiServices,
-    mlUsageCollection: mlUsageCollectionProvider(usageCollection),
-    mlCapabilities: new MlCapabilitiesService(mlApiServices),
-    mlLicense: new MlLicense(),
     elasticModels: new ElasticModels(mlApiServices.trainedModels),
-    isServerless,
   };
 }
