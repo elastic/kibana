615122e5f9eb03ade7d6aa7d66b92a960bb5ad62
Søren Louv-Jansen <soren.louv@elastic.co>
2023-09-22 16:07:42 +0200

    [APM] Add support for versioned APIs

--- x-pack/plugins/apm/scripts/diagnostics_bundle/cli.ts
+++ x-pack/plugins/apm/scripts/diagnostics_bundle/cli.ts
@@ -9,101 +9,146 @@
 
 import datemath from '@elastic/datemath';
 import { errors } from '@elastic/elasticsearch';
-import { AxiosError } from 'axios';
+import axios, { AxiosError } from 'axios';
 import yargs from 'yargs';
 import { initDiagnosticsBundle } from './diagnostics_bundle';
 
-const { argv } = yargs(process.argv.slice(2))
-  .option('esHost', {
-    type: 'string',
-    description: 'Elasticsearch host name',
-  })
-  .option('kbHost', {
-    type: 'string',
-    description: 'Kibana host name',
-  })
-  .option('username', {
-    type: 'string',
-    description: 'Kibana host name',
-  })
-  .option('password', {
-    type: 'string',
-    description: 'Kibana host name',
-  })
-  .option('cloudId', {
-    type: 'string',
-  })
-  .option('apiKey', {
-    type: 'string',
-  })
-  .option('rangeFrom', {
-    type: 'string',
-    description: 'Time-range start',
-    coerce: convertDate,
-  })
-  .option('rangeTo', {
-    type: 'string',
-    description: 'Time range end',
-    coerce: convertDate,
-  })
-  .option('kuery', {
-    type: 'string',
-    description: 'KQL query to filter documents by',
-  })
-  .help();
+async function init() {
+  const { argv } = yargs(process.argv.slice(2))
+    .option('esHost', {
+      type: 'string',
+      description: 'Elasticsearch host name',
+    })
+    .option('kbHost', {
+      type: 'string',
+      description: 'Kibana host name',
+    })
+    .option('username', {
+      type: 'string',
+      description: 'Kibana host name',
+    })
+    .option('password', {
+      type: 'string',
+      description: 'Kibana host name',
+    })
+    .option('local', {
+      type: 'boolean',
+      description: 'Connect to local cluster',
+      default: false,
+    })
+    .option('cloudId', {
+      type: 'string',
+    })
+    .option('apiKey', {
+      type: 'string',
+    })
+    .option('rangeFrom', {
+      type: 'string',
+      description: 'Time-range start',
+      coerce: convertDate,
+    })
+    .option('rangeTo', {
+      type: 'string',
+      description: 'Time range end',
+      coerce: convertDate,
+    })
+    .option('kuery', {
+      type: 'string',
+      description: 'KQL query to filter documents by',
+    })
+    .help();
 
-const { esHost, kbHost, password, username, kuery, apiKey, cloudId } = argv;
-const rangeFrom = argv.rangeFrom as unknown as number;
-const rangeTo = argv.rangeTo as unknown as number;
+  const { kuery, apiKey, cloudId } = argv;
+  let esHost = argv.esHost;
+  let kbHost = argv.kbHost;
+  let password = argv.password;
+  let username = argv.username;
 
-if ((!esHost || !kbHost) && !cloudId) {
-  console.error('Either esHost and kbHost or cloudId must be provided');
-  process.exit(1);
-}
+  const rangeFrom = argv.rangeFrom as unknown as number;
+  const rangeTo = argv.rangeTo as unknown as number;
 
-if ((!username || !password) && !apiKey) {
-  console.error('Either username and password or apiKey must be provided');
-  process.exit(1);
-}
+  if (argv.local) {
+    esHost = 'http://localhost:9200';
+    kbHost = await getHostnameWithBasePath('http://127.0.0.1:5601');
+    password = 'changeme';
+    username = 'elastic';
+  }
 
-if (rangeFrom) {
-  console.log(`rangeFrom = ${new Date(rangeFrom).toISOString()}`);
-}
+  if ((!esHost || !kbHost) && !cloudId) {
+    console.error(
+      'Please provide either: --esHost and --kbHost or --cloudId\n'
+    );
 
-if (rangeTo) {
-  console.log(`rangeTo = ${new Date(rangeTo).toISOString()}`);
-}
+    console.log('Example 1:');
+    console.log(
+      '--kbHost https://foo.kb.us-west2.gcp.elastic-cloud.com --esHost https://foo.es.us-west2.gcp.elastic-cloud.com\n'
+    );
 
-initDiagnosticsBundle({
-  esHost,
-  kbHost,
-  password,
-  apiKey,
-  cloudId,
-  username,
-  start: rangeFrom,
-  end: rangeTo,
-  kuery,
-})
-  .then((res) => {
-    console.log(res);
+    console.log('Example 2:');
+    console.log('--cloudId foo:very_secret');
+
+    console.log('Example 3:');
+    console.log('--local');
+    process.exit(1);
+  }
+
+  if ((!username || !password) && !apiKey) {
+    console.error(
+      'Please provide either: --username and --password or --apiKey \n'
+    );
+
+    console.log('Example 1:');
+    console.log('--username elastic --password changeme\n');
+
+    console.log('Example 2:');
+    console.log('--apiKey very_secret');
+
+    console.log('Example 3:');
+    console.log('--local');
+    process.exit(1);
+  }
+
+  if (rangeFrom) {
+    console.log(`rangeFrom = ${new Date(rangeFrom).toISOString()}`);
+  }
+
+  if (rangeTo) {
+    console.log(`rangeTo = ${new Date(rangeTo).toISOString()}`);
+  }
+
+  initDiagnosticsBundle({
+    esHost,
+    kbHost,
+    password,
+    apiKey,
+    cloudId,
+    username,
+    start: rangeFrom,
+    end: rangeTo,
+    kuery,
   })
-  .catch((err) => {
-    process.exitCode = 1;
-    if (err instanceof AxiosError && err.response?.data) {
-      console.error(err.response.data);
-      return;
-    }
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      process.exitCode = 1;
+      if (err instanceof AxiosError && err.response?.data) {
+        console.error(err.response.data);
+        return;
+      }
 
-    // @ts-expect-error
-    if (err instanceof errors.ResponseError && err.meta.body.error.reason) {
       // @ts-expect-error
-      console.error(err.meta.body.error.reason);
-      return;
-    }
+      if (err instanceof errors.ResponseError && err.meta.body.error.reason) {
+        // @ts-expect-error
+        console.error(err.meta.body.error.reason);
+        return;
+      }
 
-    console.error(err);
-  });
+      console.error(err);
+    });
+}
+
+init();
 
 function convertDate(dateString: string): number {
   const parsed = datemath.parse(dateString);
@@ -112,4 +157,25 @@ function convertDate(dateString: string): number {
   }
 
   throw new Error(`Incorrect argument: ${dateString}`);
+}
+
+async function getHostnameWithBasePath(kibanaHostname: string) {
+  try {
+    await axios.get(kibanaHostname, { maxRedirects: 0 });
+  } catch (e) {
+    if (isAxiosError(e)) {
+      const location = e.response?.headers?.location ?? '';
+      const hasBasePath = RegExp(/^\/\w{3}$/).test(location);
+      const basePath = hasBasePath ? location : '';
+      return `${kibanaHostname}${basePath}`;
+    }
+
+    throw e;
+  }
+
+  return kibanaHostname;
+}
+
+export function isAxiosError(e: AxiosError | Error): e is AxiosError {
+  return 'isAxiosError' in e;
 }
