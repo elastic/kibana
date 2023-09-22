b84bc8eed112dea6bce6c6fcf1aed729e8a97c01
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-09-22 09:05:15 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/test/functional/apps/observability_log_explorer/dataset_selector.ts
+++ x-pack/test/functional/apps/observability_log_explorer/dataset_selector.ts
@@ -5,7 +5,6 @@
  * 2.0.
  */
 import expect from '@kbn/expect';
-import { WebElementWrapper } from '../../../../../test/functional/services/lib/web_element_wrapper';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 const initialPackageMap = {
