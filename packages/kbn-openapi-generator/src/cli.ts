816c51d66040e544a82fb38d77323f20f55067de
Dmitrii Shevchenko <dmshevch@gmail.com>
2023-09-22 18:21:09 +0200

    Update packages/kbn-openapi-generator/src/cli.ts
    Co-authored-by: Garrett Spong <spong@users.noreply.github.com>

--- packages/kbn-openapi-generator/src/cli.ts
+++ packages/kbn-openapi-generator/src/cli.ts
@@ -5,14 +5,6 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
 import yargs from 'yargs/yargs';
 import { generate } from './openapi_generator';
 import { AVAILABLE_TEMPLATES } from './template_service/template_service';
