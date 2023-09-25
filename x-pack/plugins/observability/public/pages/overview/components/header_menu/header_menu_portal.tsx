9b793a7475c0d9b0730085014ccf24b526794a60
mgiota <panagiota.mitsopoulou@elastic.co>
2023-09-25 11:20:47 +0200

    use toMountPoint from react-kibana-mount for header menu portal

--- x-pack/plugins/observability/public/pages/overview/components/header_menu/header_menu_portal.tsx
+++ x-pack/plugins/observability/public/pages/overview/components/header_menu/header_menu_portal.tsx
@@ -7,26 +7,22 @@
 
 import React, { ReactNode, useEffect, useMemo } from 'react';
 import { createHtmlPortalNode, InPortal, OutPortal } from 'react-reverse-portal';
-import { toMountPoint } from '@kbn/kibana-react-plugin/public';
+import { toMountPoint } from '@kbn/react-kibana-mount';
 import { AppMountParameters } from '@kbn/core/public';
-
+import { useKibana } from '../../../../utils/kibana_react';
 export interface HeaderMenuPortalProps {
   children: ReactNode;
   setHeaderActionMenu: AppMountParameters['setHeaderActionMenu'];
-  theme$: AppMountParameters['theme$'];
 }
 
 // eslint-disable-next-line import/no-default-export
-export default function HeaderMenuPortal({
-  children,
-  setHeaderActionMenu,
-  theme$,
-}: HeaderMenuPortalProps) {
+export default function HeaderMenuPortal({ children, setHeaderActionMenu }: HeaderMenuPortalProps) {
+  const { theme, i18n } = useKibana().services;
   const portalNode = useMemo(() => createHtmlPortalNode(), []);
 
   useEffect(() => {
     setHeaderActionMenu((element) => {
-      const mount = toMountPoint(<OutPortal node={portalNode} />, { theme$ });
+      const mount = toMountPoint(<OutPortal node={portalNode} />, { theme, i18n });
       return mount(element);
     });
 
@@ -34,7 +30,7 @@ export default function HeaderMenuPortal({
       portalNode.unmount();
       setHeaderActionMenu(undefined);
     };
-  }, [portalNode, setHeaderActionMenu, theme$]);
+  }, [portalNode, setHeaderActionMenu, i18n, theme]);
 
   return <InPortal node={portalNode}>{children}</InPortal>;
 }
