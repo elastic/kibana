89ff96d0ae56aa6e4a1e6219d894dd6e320150c0
Maxim Kholod <maxim.kholod@elastic.co>
2023-09-25 17:40:17 +0200

    move api integration test for different index statuses

--- x-pack/test_serverless/api_integration/test_suites/security/cloud_security_posture/index.ts
+++ x-pack/test_serverless/api_integration/test_suites/security/cloud_security_posture/index.ts
@@ -10,7 +10,17 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('cloud_security_posture', function () {
     this.tags(['cloud_security_posture']);
+    loadTestFile(require.resolve('./status/status_not_deployed_not_installed'));
+    loadTestFile(require.resolve('./status/status_indexed'));
+    loadTestFile(require.resolve('./status/status_indexing'));
     loadTestFile(require.resolve('./benchmark'));
     loadTestFile(require.resolve('./get_csp_rule_template'));
+
+    // TODO: migrate status_unprivileged tests from stateful, if it feasible in serverless with the new security model
+    // loadTestFile(require.resolve('./status/status_unprivileged'));
+
+    // TODO: migrate tests relying on fleet_api_integration helpers  from stateful
+    // loadTestFile(require.resolve('./status/status_waiting_for_results'));
+    // loadTestFile(require.resolve('./status/status_index_timeout'));
   });
 }
