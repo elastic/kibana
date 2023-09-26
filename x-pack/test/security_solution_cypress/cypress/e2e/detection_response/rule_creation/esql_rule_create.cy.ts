b3dbb6ca68bf8c8249df3069ec4e44176c69e214
Vitalii Dmyterko <vitalii.dmyterko@elastic.co>
2023-09-26 17:55:25 +0100

    add serverless tests

--- x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_creation/esql_rule_create.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_creation/esql_rule_create.cy.ts
@@ -10,7 +10,12 @@ import { getEsqlRule } from '../../../objects/rule';
 import { RULES_MANAGEMENT_TABLE, RULE_NAME } from '../../../screens/alerts_detection_rules';
 import { RULE_NAME_HEADER, RULE_TYPE_DETAILS } from '../../../screens/rule_details';
 
-import { ESQL_TYPE, ESQL_QUERY_BAR } from '../../../screens/create_new_rule';
+import {
+  ESQL_TYPE,
+  NEW_TERMS_TYPE,
+  THRESHOLD_TYPE,
+  ESQL_QUERY_BAR,
+} from '../../../screens/create_new_rule';
 
 import { getDetails, goBackToRulesTable } from '../../../tasks/rule_details';
 import { expectNumberOfRules } from '../../../tasks/alerts_detection_rules';
@@ -28,6 +33,7 @@ import { login } from '../../../tasks/login';
 import { visit } from '../../../tasks/navigation';
 
 import { CREATE_RULE_URL } from '../../../urls/navigation';
+import { createRule } from '../../../tasks/api_calls/rules';
 
 describe('Detection ES|QL rules, creation', { tags: ['@ess'] }, () => {
   before(() => {
@@ -132,6 +138,35 @@ describe('Detection ES|QL rules, creation', { tags: ['@ess'] }, () => {
       getDefineContinueButton().click();
 
       cy.get(ESQL_QUERY_BAR).contains('Error validating ES|QL');
+    });
+  });
+});
+
+describe('Detection ES|QL rules, creation', { tags: ['@serverless'] }, () => {
+  before(() => {
+    cleanKibana();
+    login();
+  });
+
+  it('does not display ES|QL rule on form', function () {
+    visit(CREATE_RULE_URL);
+
+    // ensure, page is loaded and rule types are displayed
+    cy.get(NEW_TERMS_TYPE).should('be.visible');
+    cy.get(THRESHOLD_TYPE).should('be.visible');
+
+    // ES|QL rule tile should not be rendered
+    cy.get(ESQL_TYPE).should('not.exist');
+  });
+
+  it('does not allow to create rule by API call', function () {
+    createRule(getEsqlRule()).then((response) => {
+      expect(response.status).to.equal(400);
+
+      expect(response.body).to.deep.equal({
+        status_code: 400,
+        message: 'Rule type "siem.esqlRule" is not registered.',
+      });
     });
   });
 });
