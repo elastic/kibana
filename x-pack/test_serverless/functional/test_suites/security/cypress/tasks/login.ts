95b5c9961b3d6968a8915e6a4927bcbadca02d97
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-09-26 12:28:44 +0200

    make a direct import to fix fs problem

--- x-pack/test_serverless/functional/test_suites/security/cypress/tasks/login.ts
+++ x-pack/test_serverless/functional/test_suites/security/cypress/tasks/login.ts
@@ -1,14 +1,15 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { request } from '@kbn/security-solution-plugin/public/management/cypress/tasks/common';
 import { LoginState } from '@kbn/security-plugin/common/login_state';
 import type { ServerlessRoleName } from '../../../../../shared/lib';
-import { ServerlessRoleName as RoleName } from '../../../../../shared/lib';
+import { ServerlessRoleName as RoleName } from '../../../../../shared/lib/security/types';
 import { STANDARD_HTTP_HEADERS } from '../../../../../shared/lib/security/default_http_headers';
 
 /**
