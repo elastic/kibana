ce855f8fd140480e42d117b4857abbf1e1005f9b
Kevin Delemme <kevin.delemme@elastic.co>
2023-09-26 08:30:20 -0400

    Handle partial indicator url state

--- x-pack/plugins/observability/public/pages/slo_edit/helpers/process_slo_form_values.ts
+++ x-pack/plugins/observability/public/pages/slo_edit/helpers/process_slo_form_values.ts
@@ -5,8 +5,17 @@
  * 2.0.
  */
 
-import { CreateSLOInput, SLOWithSummaryResponse, UpdateSLOInput } from '@kbn/slo-schema';
+import { CreateSLOInput, Indicator, SLOWithSummaryResponse, UpdateSLOInput } from '@kbn/slo-schema';
+import { assertNever } from '@kbn/std';
+import { RecursivePartial } from '@kbn/utility-types';
 import { toDuration } from '../../../utils/slo/duration';
+import {
+  APM_AVAILABILITY_DEFAULT_VALUES,
+  APM_LATENCY_DEFAULT_VALUES,
+  CUSTOM_KQL_DEFAULT_VALUES,
+  CUSTOM_METRIC_DEFAULT_VALUES,
+  HISTOGRAM_DEFAULT_VALUES,
+} from '../constants';
 import { CreateSLOForm } from '../types';
 
 export function transformSloResponseToCreateSloForm(
@@ -91,21 +100,50 @@ export function transformValuesToUpdateSLOInput(values: CreateSLOForm): UpdateSL
   };
 }
 
-export function transformPartialCreateSLOInputToPartialCreateSLOForm(
-  values: Partial<CreateSLOInput>
-): Partial<CreateSLOForm> {
-  return {
-    ...values,
-    ...(values.objective && {
-      objective: {
-        target: values.objective.target * 100,
-        ...(values.objective.timesliceTarget && {
-          timesliceTarget: values.objective.timesliceTarget * 100,
-        }),
-        ...(values.objective.timesliceWindow && {
-          timesliceWindow: String(toDuration(values.objective.timesliceWindow).value),
-        }),
-      },
-    }),
-  };
+function transformPartialIndicatorState(
+  indicator?: RecursivePartial<Indicator>
+): Indicator | undefined {
+  if (indicator === undefined || indicator.type === undefined) return undefined;
+
+  const indicatorType = indicator.type;
+  switch (indicatorType) {
+    case 'sli.apm.transactionDuration':
+      return {
+        type: 'sli.apm.transactionDuration' as const,
+        params: Object.assign({}, APM_LATENCY_DEFAULT_VALUES.params, indicator.params ?? {}),
+      };
+    case 'sli.apm.transactionErrorRate':
+      return {
+        type: 'sli.apm.transactionErrorRate' as const,
+        params: Object.assign({}, APM_AVAILABILITY_DEFAULT_VALUES.params, indicator.params ?? {}),
+      };
+    case 'sli.histogram.custom':
+      return {
+        type: 'sli.histogram.custom' as const,
+        params: Object.assign({}, HISTOGRAM_DEFAULT_VALUES.params, indicator.params ?? {}),
+      };
+    case 'sli.kql.custom':
+      return {
+        type: 'sli.kql.custom' as const,
+        params: Object.assign({}, CUSTOM_KQL_DEFAULT_VALUES.params, indicator.params ?? {}),
+      };
+    case 'sli.metric.custom':
+      return {
+        type: 'sli.metric.custom' as const,
+        params: Object.assign({}, CUSTOM_METRIC_DEFAULT_VALUES.params, indicator.params ?? {}),
+      };
+    default:
+      assertNever(indicatorType);
+  }
+}
+
+export function transformPartialUrlStateToFormState(
+  values: RecursivePartial<Pick<CreateSLOInput, 'indicator'>>
+): Partial<CreateSLOForm> | {} {
+  const state: Partial<CreateSLOForm> = {};
+
+  const parsedIndicator = transformPartialIndicatorState(values.indicator);
+  if (parsedIndicator !== undefined) state.indicator = parsedIndicator;
+
+  return state;
 }
