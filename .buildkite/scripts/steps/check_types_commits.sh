4b53a5f7d31cd147a4f7768ac00d384e9f5e2848
Thomas Watson <watson@elastic.co>
2023-09-27 16:37:14 +0200

    debug

--- .buildkite/scripts/steps/check_types_commits.sh
+++ .buildkite/scripts/steps/check_types_commits.sh
@@ -10,8 +10,9 @@ if [[ "${CI-}" == "true" ]]; then
   .buildkite/scripts/bootstrap.sh
 
   targetBranch="${GITHUB_PR_TARGET_BRANCH-}"
+  git config -l
   git fetch origin $targetBranch
-  diffArgs+=("--merge-base" "origin/$targetBranch" "${GITHUB_PR_TRIGGERED_SHA-}")
+  diffArgs+=("--merge-base" "$targetBranch" "${GITHUB_PR_TRIGGERED_SHA-}")
 elif [[ "${1-}" == "--merge-base" ]]; then
   # Similar to when CI=true, but locally
   diffArgs+=("--merge-base" "${2-main}" "${3-HEAD}")
