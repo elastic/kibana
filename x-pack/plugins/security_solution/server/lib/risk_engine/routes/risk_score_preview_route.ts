7bb9491fbde5c0421508863ed35c5206da8393aa
Ryland Herrick <ryalnd@gmail.com>
2023-09-27 00:06:45 -0500

    Move new Risk Engine routes to versioned router
    Declares all but the public "calculation" API as internal.
    Next up: updating client calls to specify a version.

--- x-pack/plugins/security_solution/server/lib/risk_engine/routes/risk_score_preview_route.ts
+++ x-pack/plugins/security_solution/server/lib/risk_engine/routes/risk_score_preview_route.ts
@@ -17,73 +17,78 @@ import { riskScoreServiceFactory } from '../risk_score_service';
 import { getRiskInputsIndex } from '../get_risk_inputs_index';
 
 export const riskScorePreviewRoute = (router: SecuritySolutionPluginRouter, logger: Logger) => {
-  router.post(
-    {
+  router.versioned
+    .post({
+      access: 'internal',
       path: RISK_SCORE_PREVIEW_URL,
-      validate: { body: buildRouteValidation(riskScorePreviewRequestSchema) },
       options: {
         tags: ['access:securitySolution'],
       },
-    },
-    async (context, request, response) => {
-      const siemResponse = buildSiemResponse(response);
-      const securityContext = await context.securitySolution;
-      const coreContext = await context.core;
-      const esClient = coreContext.elasticsearch.client.asCurrentUser;
-      const soClient = coreContext.savedObjects.client;
-      const spaceId = securityContext.getSpaceId();
-      const riskEngineDataClient = securityContext.getRiskEngineDataClient();
+    })
+    .addVersion(
+      {
+        version: '1',
+        validate: { request: { body: buildRouteValidation(riskScorePreviewRequestSchema) } },
+      },
+      async (context, request, response) => {
+        const siemResponse = buildSiemResponse(response);
+        const securityContext = await context.securitySolution;
+        const coreContext = await context.core;
+        const esClient = coreContext.elasticsearch.client.asCurrentUser;
+        const soClient = coreContext.savedObjects.client;
+        const spaceId = securityContext.getSpaceId();
+        const riskEngineDataClient = securityContext.getRiskEngineDataClient();
 
-      const riskScoreService = riskScoreServiceFactory({
-        esClient,
-        logger,
-        riskEngineDataClient,
-        spaceId,
-      });
-
-      const {
-        after_keys: userAfterKeys,
-        data_view_id: dataViewId,
-        debug,
-        page_size: userPageSize,
-        identifier_type: identifierType,
-        filter,
-        range: userRange,
-        weights,
-      } = request.body;
-
-      try {
-        const { index, runtimeMappings } = await getRiskInputsIndex({
-          dataViewId,
+        const riskScoreService = riskScoreServiceFactory({
+          esClient,
           logger,
-          soClient,
+          riskEngineDataClient,
+          spaceId,
         });
 
-        const afterKeys = userAfterKeys ?? {};
-        const range = userRange ?? { start: 'now-15d', end: 'now' };
-        const pageSize = userPageSize ?? DEFAULT_RISK_SCORE_PAGE_SIZE;
-
-        const result = await riskScoreService.calculateScores({
-          afterKeys,
+        const {
+          after_keys: userAfterKeys,
+          data_view_id: dataViewId,
           debug,
+          page_size: userPageSize,
+          identifier_type: identifierType,
           filter,
-          identifierType,
-          index,
-          pageSize,
-          range,
-          runtimeMappings,
+          range: userRange,
           weights,
-        });
+        } = request.body;
 
-        return response.ok({ body: result });
-      } catch (e) {
-        const error = transformError(e);
+        try {
+          const { index, runtimeMappings } = await getRiskInputsIndex({
+            dataViewId,
+            logger,
+            soClient,
+          });
 
-        return siemResponse.error({
-          statusCode: error.statusCode,
-          body: { message: error.message, full_error: JSON.stringify(e) },
-        });
+          const afterKeys = userAfterKeys ?? {};
+          const range = userRange ?? { start: 'now-15d', end: 'now' };
+          const pageSize = userPageSize ?? DEFAULT_RISK_SCORE_PAGE_SIZE;
+
+          const result = await riskScoreService.calculateScores({
+            afterKeys,
+            debug,
+            filter,
+            identifierType,
+            index,
+            pageSize,
+            range,
+            runtimeMappings,
+            weights,
+          });
+
+          return response.ok({ body: result });
+        } catch (e) {
+          const error = transformError(e);
+
+          return siemResponse.error({
+            statusCode: error.statusCode,
+            body: { message: error.message, full_error: JSON.stringify(e) },
+          });
+        }
       }
-    }
-  );
+    );
 };
