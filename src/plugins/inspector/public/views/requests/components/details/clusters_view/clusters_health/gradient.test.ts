f47a321e13756806c5d8f12b62ff58e24824b284
nreese <reese.nathan@elastic.co>
2023-09-26 19:07:23 -0600

    update test expects

--- src/plugins/inspector/public/views/requests/components/details/clusters_view/clusters_health/gradient.test.ts
+++ src/plugins/inspector/public/views/requests/components/details/clusters_view/clusters_health/gradient.test.ts
@@ -6,18 +6,18 @@
  * Side Public License, v 1.
  */
 
-import { getHeathBarLinearGradient } from './gradient';
+import { getHeathBarLinearGradient, HEALTH_HEX_CODES } from './gradient';
 
 describe('getHeathBarLinearGradient', () => {
   test('should return linear-gradient with percentages for each status', () => {
     expect(getHeathBarLinearGradient(5, 1, 1, 2)).toBe(
-      'linear-gradient(to right, #54B399 0% 56%, #D6BF57 56% 67%, #DA8B45 67% 78%, #E7664C 78% 100%)'
+      `linear-gradient(to right, ${HEALTH_HEX_CODES.successful} 0% 56%, ${HEALTH_HEX_CODES.partial} 56% 67%, ${HEALTH_HEX_CODES.skipped} 67% 78%, ${HEALTH_HEX_CODES.failed} 78% 100%)`
     );
   });
 
   test('should return linear-gradient with percentages for each status with count above zero', () => {
     expect(getHeathBarLinearGradient(5, 0, 0, 2)).toBe(
-      'linear-gradient(to right, #54B399 0% 71%, #E7664C 71% 100%)'
+      `linear-gradient(to right, ${HEALTH_HEX_CODES.successful} 0% 71%, ${HEALTH_HEX_CODES.failed} 71% 100%)`
     );
   });
 });
