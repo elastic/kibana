52dbf993d9d589de86cc67b89f7376167736e80b
semd <sergi.massaneda@elastic.co>
2023-09-27 11:45:38 +0200

    lazy load shared nav and update limits

--- x-pack/plugins/security_solution_serverless/public/navigation/default_navigation.tsx
+++ x-pack/plugins/security_solution_serverless/public/navigation/default_navigation.tsx
@@ -4,24 +4,44 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import React, { Suspense } from 'react';
+import { EuiLoadingSpinner } from '@elastic/eui';
 import type { NavigationTreeDefinition } from '@kbn/shared-ux-chrome-navigation';
-import { DefaultNavigation, NavigationKibanaProvider } from '@kbn/shared-ux-chrome-navigation';
-import React from 'react';
 import type { SideNavComponent } from '@kbn/core-chrome-browser';
 import type { Services } from '../common/services';
+
+const Navigation = React.lazy(() =>
+  import('@kbn/shared-ux-chrome-navigation').then(
+    ({ DefaultNavigation, NavigationKibanaProvider }) => ({
+      default: React.memo<{
+        navigationTree: NavigationTreeDefinition;
+        services: Services;
+      }>(function SecurityDefaultNavigation({ navigationTree, services }) {
+        return (
+          <NavigationKibanaProvider
+            core={services}
+            serverless={services.serverless}
+            cloud={services.cloud}
+          >
+            <DefaultNavigation
+              navigationTree={navigationTree}
+              dataTestSubj="securitySolutionSideNav"
+            />
+          </NavigationKibanaProvider>
+        );
+      }),
+    })
+  )
+);
 
 export const getDefaultNavigationComponent = (
   navigationTree: NavigationTreeDefinition,
   services: Services
 ): SideNavComponent =>
-  React.memo(function SecurityDefaultNavigation() {
+  function SecuritySideNavComponent() {
     return (
-      <NavigationKibanaProvider
-        core={services}
-        serverless={services.serverless}
-        cloud={services.cloud}
-      >
-        <DefaultNavigation navigationTree={navigationTree} dataTestSubj="securitySolutionSideNav" />
-      </NavigationKibanaProvider>
+      <Suspense fallback={<EuiLoadingSpinner size="m" />}>
+        <Navigation navigationTree={navigationTree} services={services} />
+      </Suspense>
     );
-  });
+  };
