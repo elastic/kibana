298cc5901238583a7a4de08669e7b07113f5e2f8
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-09-27 11:16:24 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- packages/kbn-timelion-grammar/index.d.ts
+++ packages/kbn-timelion-grammar/index.d.ts
@@ -1,3 +1,11 @@
-/// <reference types="@kbn/ambient-common-types" />
-declare const _exports: typeof import("*.peggy");
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+// / <reference types="@kbn/ambient-common-types" />
+declare const _exports: typeof import('*.peggy');
 export = _exports;
