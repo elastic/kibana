62c8cf078b061fead1705f68c2ae5d04527bd607
Garrett Spong <garrett.spong@elastic.co>
2023-09-28 16:36:11 -0600

    Updates one more mock

--- x-pack/plugins/security_solution/public/common/mock/mock_assistant_provider.tsx
+++ x-pack/plugins/security_solution/public/common/mock/mock_assistant_provider.tsx
@@ -38,6 +38,7 @@ export const MockAssistantProviderComponent: React.FC<Props> = ({ children }) =>
       augmentMessageCodeBlocks={jest.fn(() => [])}
       baseAllow={[]}
       baseAllowReplacement={[]}
+      basePath={'https://localhost:5601/kbn'}
       defaultAllow={[]}
       docLinks={{
         ELASTIC_WEBSITE_URL: 'https://www.elastic.co/',
