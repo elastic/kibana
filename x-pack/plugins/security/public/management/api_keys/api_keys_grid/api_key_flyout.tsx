b738ab327afcd2f543e58674ce038f908affaabe
Sid Mantri <sid.mantri@elastic.co>
2023-09-28 17:38:02 +0200

    hacky fix for multiple enters on api key name input

--- x-pack/plugins/security/public/management/api_keys/api_keys_grid/api_key_flyout.tsx
+++ x-pack/plugins/security/public/management/api_keys/api_keys_grid/api_key_flyout.tsx
@@ -271,6 +271,12 @@ export const ApiKeyFlyout: FunctionComponent<ApiKeyFlyoutProps> = ({
                   ),
                 }}
                 fullWidth
+                onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                  if (e.key === 'Enter' && formik.submitCount > 0) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                  }
+                }}
               />
             </FormRow>
 
