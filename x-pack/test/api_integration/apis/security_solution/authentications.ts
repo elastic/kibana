a6936a3cd849485f9a10fa547ecf13beebd57ff1
Brad White <brad.white@elastic.co>
2023-09-28 12:50:57 -0600

    Fix security import

--- x-pack/test/api_integration/apis/security_solution/authentications.ts
+++ x-pack/test/api_integration/apis/security_solution/authentications.ts
@@ -12,7 +12,7 @@ import {
   UserAuthenticationsStrategyResponse,
   UsersQueries,
 } from '@kbn/security-solution-plugin/common/search_strategy';
-import type { UserAuthenticationsRequestOptions } from '@kbn/security-solution-plugin/common/api/search_strategy';
+import type { UserAuthenticationsRequestOptions } from '@kbn/security-solution-plugin/common/search_strategy';
 
 import { FtrProviderContext } from '../../ftr_provider_context';
 
