c92676964b248538add771554b1d9cb852874d9d
James Gowdy <jgowdy@elastic.co>
2023-09-28 13:12:16 +0100

    removing redundant code

--- x-pack/test_serverless/functional/services/ml/index.ts
+++ x-pack/test_serverless/functional/services/ml/index.ts
@@ -6,23 +6,18 @@
  */
 
 import { FtrProviderContext } from '../../ftr_provider_context';
-import { MachineLearningAPIProvider } from './api';
 
 import { MachineLearningNavigationProviderObservability } from './observability_navigation';
 import { MachineLearningNavigationProviderSecurity } from './security_navigation';
-import { MachineLearningTestResourcesProvider } from './test_resources';
 
 export function MachineLearningProvider(context: FtrProviderContext) {
-  const api = MachineLearningAPIProvider(context);
   const observabilityNavigation = MachineLearningNavigationProviderObservability(context);
   const securityNavigation = MachineLearningNavigationProviderSecurity(context);
-  const testResources = MachineLearningTestResourcesProvider(context, api);
 
   return {
     navigation: {
       observability: observabilityNavigation,
       security: securityNavigation,
     },
-    testResources,
   };
 }
