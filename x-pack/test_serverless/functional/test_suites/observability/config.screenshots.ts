71aa8476731d32e499468bb0da5dd32b16073c2c
lcawl <lcawley@elastic.co>
2023-09-29 13:51:22 -0700

    Automate server log connector screenshots in Observabiilty serverless project

---
+++ x-pack/test_serverless/functional/test_suites/observability/config.screenshots.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { createTestConfig } from '../../config.base';
+
+const enabledActionTypes = ['.index', '.server-log'];
+
+export default createTestConfig({
+  serverlessProject: 'oblt',
+  testFiles: [require.resolve('./screenshot_creation')],
+  kbnServerArgs: [`--xpack.actions.enabledActionTypes=${JSON.stringify(enabledActionTypes)}`],
+  junit: {
+    reportName: 'Serverless Observability Screenshot Creation',
+  },
+
+  // include settings from project controller
+  // https://github.com/elastic/project-controller/blob/main/internal/project/esproject/config/elasticsearch.yml
+  esServerArgs: ['xpack.ml.ad.enabled=false', 'xpack.ml.dfa.enabled=false'],
+});
