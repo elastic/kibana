1b8eb7147bb40983b49382a8e7df14ef8c2fd227
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-09-29 14:04:18 +0200

    unskip many_fields_lens_editor journey

--- x-pack/performance/journeys/many_fields_lens_editor.ts
+++ x-pack/performance/journeys/many_fields_lens_editor.ts
@@ -10,7 +10,6 @@ import { subj } from '@kbn/test-subj-selector';
 
 export const journey = new Journey({
   // Failing: See https://github.com/elastic/kibana/issues/167496
-  skipped: true,
   kbnArchives: ['x-pack/performance/kbn_archives/lens_many_fields'],
   esArchives: ['test/functional/fixtures/es_archiver/stress_test'],
 })
