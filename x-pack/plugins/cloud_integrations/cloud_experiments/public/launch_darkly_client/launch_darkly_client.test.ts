d82c6c1272b38739f0b5acc78ccde1f4ae5f0449
Anton Dosov <anton.dosov@elastic.co>
2023-09-29 16:35:28 +0200

    fix ld client race condition

--- x-pack/plugins/cloud_integrations/cloud_experiments/public/launch_darkly_client/launch_darkly_client.test.ts
+++ x-pack/plugins/cloud_integrations/cloud_experiments/public/launch_darkly_client/launch_darkly_client.test.ts
@@ -11,6 +11,7 @@ import { LaunchDarklyClient, type LaunchDarklyClientConfig } from './launch_dark
 describe('LaunchDarklyClient - browser', () => {
   beforeEach(() => {
     jest.clearAllMocks();
+    launchDarklyLibraryMock.initialize.mockReturnValue(ldClientMock);
   });
 
   const config: LaunchDarklyClientConfig = {
@@ -29,8 +30,6 @@ describe('LaunchDarklyClient - browser', () => {
     describe('updateUserMetadata', () => {
       test("calls the client's initialize method with all the possible values", async () => {
         expect(client).toHaveProperty('launchDarklyClient', undefined);
-
-        launchDarklyLibraryMock.initialize.mockReturnValue(ldClientMock);
 
         const topFields = {
           name: 'First Last',
@@ -61,8 +60,6 @@ describe('LaunchDarklyClient - browser', () => {
             logger: undefined,
           }
         );
-
-        expect(client).toHaveProperty('launchDarklyClient', ldClientMock);
       });
 
       test('sets a minimum amount of info', async () => {
@@ -86,7 +83,6 @@ describe('LaunchDarklyClient - browser', () => {
       test('calls identify if an update comes after initializing the client', async () => {
         expect(client).toHaveProperty('launchDarklyClient', undefined);
 
-        launchDarklyLibraryMock.initialize.mockReturnValue(ldClientMock);
         await client.updateUserMetadata({ userId: 'fake-user-id', kibanaVersion: 'version' });
 
         expect(launchDarklyLibraryMock.initialize).toHaveBeenCalledWith(
@@ -102,8 +98,6 @@ describe('LaunchDarklyClient - browser', () => {
         );
         expect(ldClientMock.identify).not.toHaveBeenCalled();
 
-        expect(client).toHaveProperty('launchDarklyClient', ldClientMock);
-
         // Update user metadata a 2nd time
         await client.updateUserMetadata({ userId: 'fake-user-id', kibanaVersion: 'version' });
         expect(ldClientMock.identify).toHaveBeenCalledWith({
@@ -114,17 +108,31 @@ describe('LaunchDarklyClient - browser', () => {
     });
 
     describe('getVariation', () => {
-      test('returns the default value if the user has not been defined', async () => {
-        await expect(client.getVariation('my-feature-flag', 123)).resolves.toStrictEqual(123);
-        expect(ldClientMock.variation).toHaveBeenCalledTimes(0);
-      });
-
       test('calls the LaunchDarkly client when the user has been defined', async () => {
         ldClientMock.variation.mockResolvedValue(1234);
         await client.updateUserMetadata(testUserMetadata);
         await expect(client.getVariation('my-feature-flag', 123)).resolves.toStrictEqual(1234);
         expect(ldClientMock.variation).toHaveBeenCalledTimes(1);
         expect(ldClientMock.variation).toHaveBeenCalledWith('my-feature-flag', 123);
+      });
+
+      test('waits for initialization to complete before calling the LaunchDarkly client', async () => {
+        ldClientMock.variation.mockResolvedValue(1234);
+        client.updateUserMetadata(testUserMetadata); // don't await
+        await expect(client.getVariation('my-feature-flag', 123)).resolves.toStrictEqual(1234);
+      });
+
+      test('returns default value if ld client initialization errors', async () => {
+        const consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation();
+        launchDarklyLibraryMock.initialize.mockImplementationOnce(() => {
+          throw new Error('Something went wrong');
+        });
+        client.updateUserMetadata(testUserMetadata); // don't await
+        await expect(client.getVariation('my-feature-flag', 123)).resolves.toStrictEqual(123);
+        expect(consoleWarnSpy).toHaveBeenCalledWith(
+          `Failed to initialize launchDarkly client`,
+          new Error('Something went wrong')
+        );
       });
     });
 
@@ -136,7 +144,7 @@ describe('LaunchDarklyClient - browser', () => {
 
       test('calls the LaunchDarkly client when the user has been defined', async () => {
         await client.updateUserMetadata(testUserMetadata);
-        client.reportMetric('my-feature-flag', {}, 123);
+        await client.reportMetric('my-feature-flag', {}, 123);
         expect(ldClientMock.track).toHaveBeenCalledTimes(1);
         expect(ldClientMock.track).toHaveBeenCalledWith('my-feature-flag', {}, 123);
       });
@@ -148,6 +156,7 @@ describe('LaunchDarklyClient - browser', () => {
 
         ldClientMock.flush.mockResolvedValue();
         expect(() => client.stop()).not.toThrow();
+        await new Promise((resolve) => process.nextTick(resolve)); // wait for the flush resolution
         expect(ldClientMock.flush).toHaveBeenCalledTimes(1);
         await new Promise((resolve) => process.nextTick(resolve)); // wait for the flush resolution
       });
@@ -159,6 +168,7 @@ describe('LaunchDarklyClient - browser', () => {
         const err = new Error('Something went terribly wrong');
         ldClientMock.flush.mockRejectedValue(err);
         expect(() => client.stop()).not.toThrow();
+        await new Promise((resolve) => process.nextTick(resolve)); // wait for the flush resolution
         expect(ldClientMock.flush).toHaveBeenCalledTimes(1);
         await new Promise((resolve) => process.nextTick(resolve)); // wait for the flush resolution
         expect(consoleWarnSpy).toHaveBeenCalledWith(err);
--- x-pack/plugins/cloud_integrations/cloud_experiments/public/launch_darkly_client/launch_darkly_client.ts
+++ x-pack/plugins/cloud_integrations/cloud_experiments/public/launch_darkly_client/launch_darkly_client.ts
@@ -6,6 +6,7 @@
  */
 
 import { type LDClient, type LDUser, type LDLogLevel } from 'launchdarkly-js-client-sdk';
+import { defer, Defer } from '@kbn/kibana-utils-plugin/public';
 
 export interface LaunchDarklyClientConfig {
   client_id: string;
@@ -26,7 +27,8 @@ export interface LaunchDarklyUserMetadata
 }
 
 export class LaunchDarklyClient {
-  private launchDarklyClient?: LDClient;
+  private launchDarklyClient?: Promise<LDClient | null>;
+  private waitUntilReady: Defer<void> = defer();
 
   constructor(
     private readonly ldConfig: LaunchDarklyClientConfig,
@@ -49,31 +51,68 @@ export class LaunchDarklyClient {
       custom: custom as Record<string, string | boolean | number>,
     };
     if (this.launchDarklyClient) {
-      await this.launchDarklyClient.identify(launchDarklyUser);
+      await (await this.launchDarklyClient)?.identify(launchDarklyUser);
     } else {
-      const { initialize, basicLogger } = await import('launchdarkly-js-client-sdk');
-      this.launchDarklyClient = initialize(this.ldConfig.client_id, launchDarklyUser, {
-        application: { id: 'kibana-browser', version: this.kibanaVersion },
-        logger: basicLogger({ level: this.ldConfig.client_log_level }),
+      this.launchDarklyClient = new Promise<LDClient | null>((resolve) => {
+        const timeoutHandle = setTimeout(() => {
+          // eslint-disable-next-line no-console
+          console.warn(`Failed to initialize launchDarkly client: timeout`);
+          resolve(null);
+          this.waitUntilReady?.resolve();
+        }, 10000); // Timeout after 10 seconds
+        import('launchdarkly-js-client-sdk')
+          .then(({ initialize, basicLogger }) => {
+            const client = initialize(this.ldConfig.client_id, launchDarklyUser, {
+              application: { id: 'kibana-browser', version: this.kibanaVersion },
+              logger: basicLogger({ level: this.ldConfig.client_log_level }),
+            });
+            resolve(client);
+            this.waitUntilReady?.resolve();
+          })
+          .catch((err) => {
+            // eslint-disable-next-line no-console
+            console.warn(`Failed to initialize launchDarkly client`, err);
+
+            resolve(null);
+            this.waitUntilReady?.resolve();
+          })
+          .finally(() => {
+            clearTimeout(timeoutHandle);
+          });
       });
+      await this.launchDarklyClient;
     }
   }
 
   public async getVariation<Data>(configKey: string, defaultValue: Data): Promise<Data> {
+    if (this.waitUntilReady) await this.waitUntilReady.promise;
     if (!this.launchDarklyClient) return defaultValue; // Skip any action if no LD User is defined
-    await this.launchDarklyClient.waitForInitialization();
-    return await this.launchDarklyClient.variation(configKey, defaultValue);
+    const client = await this.launchDarklyClient;
+    if (!client) return defaultValue;
+    try {
+      await client.waitForInitialization();
+      return client.variation(configKey, defaultValue);
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.warn(`Failed to get variation for ${configKey}`, e);
+      return defaultValue;
+    }
   }
 
-  public reportMetric(metricName: string, meta?: unknown, value?: number): void {
+  public reportMetric(metricName: string, meta?: unknown, value?: number) {
     if (!this.launchDarklyClient) return; // Skip any action if no LD User is defined
-    this.launchDarklyClient.track(metricName, meta, value);
+    this.launchDarklyClient.then((client) => {
+      client?.track(metricName, meta, value);
+    });
   }
 
   public stop() {
-    this.launchDarklyClient
-      ?.flush()
-      // eslint-disable-next-line no-console
-      .catch((err) => console.warn(err));
+    if (!this.launchDarklyClient) return;
+    this.launchDarklyClient.then((client) => {
+      client
+        ?.flush()
+        // eslint-disable-next-line no-console
+        .catch((err) => console.warn(err));
+    });
   }
 }
