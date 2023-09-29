06de23b718bb7473104265f3c949faa23410a083
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2023-09-29 12:52:27 +0200

    Fix cypress memory issue due to log panel

--- x-pack/plugins/apm/ftr_e2e/cypress_test_runner.ts
+++ x-pack/plugins/apm/ftr_e2e/cypress_test_runner.ts
@@ -76,6 +76,7 @@ export async function cypressTestRunner({ getService }: FtrProviderContext) {
       ES_NODE: esNode,
       ES_REQUEST_TIMEOUT: esRequestTimeout,
       TEST_CLOUD: process.env.TEST_CLOUD,
+      NO_COMMAND_LOG: 1,
     },
   });
 
