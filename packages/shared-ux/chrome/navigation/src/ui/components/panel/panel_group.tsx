3b1ea09e93c40c961de540086da61039ae5b2039
Sébastien Loix <sabee77@gmail.com>
2023-10-02 14:13:20 +0100

    Add default content for groups with no title

--- packages/shared-ux/chrome/navigation/src/ui/components/panel/panel_group.tsx
+++ packages/shared-ux/chrome/navigation/src/ui/components/panel/panel_group.tsx
@@ -32,9 +32,11 @@ const getClassnames = (euiTheme: EuiThemeComputed<{}>) => ({
 
 interface Props {
   navNode: PanelNavNode;
+  /** Flag to indicate if the group is the first in the list of groups when looping */
+  isFirstInList?: boolean;
 }
 
-export const PanelGroup: FC<Props> = ({ navNode }) => {
+export const PanelGroup: FC<Props> = ({ navNode, isFirstInList }) => {
   const { euiTheme } = useEuiTheme();
 
   if (!navNode.children?.length) {
@@ -42,13 +44,19 @@ export const PanelGroup: FC<Props> = ({ navNode }) => {
   }
 
   const classNames = getClassnames(euiTheme);
+  const hasTitle = !!navNode.title && navNode.title !== '';
+  const removePaddingTop = !hasTitle && !isFirstInList;
 
   return (
     <>
-      <EuiTitle size="xxxs" className={classNames.title}>
-        <h2>{navNode.title}</h2>
-      </EuiTitle>
-      <EuiListGroup css={{ paddingLeft: 0, paddingRight: 0 }}>
+      {hasTitle && (
+        <EuiTitle size="xxxs" className={classNames.title}>
+          <h2>{navNode.title}</h2>
+        </EuiTitle>
+      )}
+      <EuiListGroup
+        css={{ paddingLeft: 0, paddingRight: 0, paddingTop: removePaddingTop ? 0 : undefined }}
+      >
         {navNode.children.map((item) => (
           <PanelNavItem key={item.id} item={item} />
         ))}
