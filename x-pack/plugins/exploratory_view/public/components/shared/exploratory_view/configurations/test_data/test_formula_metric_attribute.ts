e6537f127003e93bb83724091f674b83df338d83
Shahzad <shahzad31comp@gmail.com>
2023-10-02 20:50:15 +0200

    Update x-pack/plugins/exploratory_view/public/components/shared/exploratory_view/configurations/test_data/test_formula_metric_attribute.ts
    Co-authored-by: Justin Kambic <jk@elastic.co>

--- x-pack/plugins/exploratory_view/public/components/shared/exploratory_view/configurations/test_data/test_formula_metric_attribute.ts
+++ x-pack/plugins/exploratory_view/public/components/shared/exploratory_view/configurations/test_data/test_formula_metric_attribute.ts
@@ -81,7 +81,7 @@ export const sampleMetricFormulaAttribute = {
                 filter: {
                   language: 'kuery',
                   query:
-                    'summary.final_attempt: true or ( not summary.final_attempt: * and summary:*)',
+                    'summary.final_attempt: true or (not summary.final_attempt: * and summary:*)',
                 },
                 isBucketed: false,
                 label: 'Part of Availability',
