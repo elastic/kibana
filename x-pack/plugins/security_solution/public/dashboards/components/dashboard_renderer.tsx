2c8113e3f865a3a3c370bab6e68d738f6b39dac4
Angela Chuang <yi-chun.chuang@elastic.co>
2023-10-02 10:34:10 +0100

    review

--- x-pack/plugins/security_solution/public/dashboards/components/dashboard_renderer.tsx
+++ x-pack/plugins/security_solution/public/dashboards/components/dashboard_renderer.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import type { DashboardAPI, DashboardCreationOptions } from '@kbn/dashboard-plugin/public';
 import { DashboardRenderer as DashboardContainerRenderer } from '@kbn/dashboard-plugin/public';
 import { ViewMode } from '@kbn/embeddable-plugin/public';
@@ -75,15 +75,10 @@ const DashboardRendererComponent = ({
       }),
     [embeddable, filters, query, timeRange, viewMode]
   );
-  const [dashboardContainerRenderer, setDashboardContainerRenderer] = useState<
-    React.ReactElement | undefined
-  >(undefined);
 
   const refetchByForceRefresh = useCallback(() => {
     dashboardContainer?.forceRefresh();
   }, [dashboardContainer]);
-
-  const wrapperRef = useRef(null);
 
   useEffect(() => {
     dispatch(
@@ -109,23 +104,13 @@ const DashboardRendererComponent = ({
       dashboardContainer?.updateInput({ tags: [firstSecurityTagId] });
   }, [dashboardContainer, firstSecurityTagId, isCreateDashboard]);
 
-  useEffect(() => {
-    if (wrapperRef) {
-      setDashboardContainerRenderer(
-        <DashboardContainerRenderer
-          ref={onDashboardContainerLoaded}
-          savedObjectId={savedObjectId}
-          getCreationOptions={getCreationOptions}
-        />
-      );
-    }
-
-    return () => {
-      setDashboardContainerRenderer(undefined);
-    };
-  }, [getCreationOptions, onDashboardContainerLoaded, refetchByForceRefresh, savedObjectId]);
-
-  return canReadDashboard ? <div ref={wrapperRef}>{dashboardContainerRenderer}</div> : null;
+  return canReadDashboard ? (
+    <DashboardContainerRenderer
+      ref={onDashboardContainerLoaded}
+      savedObjectId={savedObjectId}
+      getCreationOptions={getCreationOptions}
+    />
+  ) : null;
 };
 DashboardRendererComponent.displayName = 'DashboardRendererComponent';
 export const DashboardRenderer = React.memo(DashboardRendererComponent);
