e4ebdf8a3389722603443992301a38d2129549af
Faisal Kanout <f.kanout@gmail.com>
2023-10-02 15:57:56 +0200

    Feature Flag true for stateful with Tech Preview badge

--- x-pack/plugins/observability/server/lib/rules/custom_threshold/register_custom_threshold_rule_type.ts
+++ x-pack/plugins/observability/server/lib/rules/custom_threshold/register_custom_threshold_rule_type.ts
@@ -121,7 +121,7 @@ export function thresholdRuleType(
   return {
     id: OBSERVABILITY_THRESHOLD_RULE_TYPE_ID,
     name: i18n.translate('xpack.observability.threshold.ruleName', {
-      defaultMessage: 'Custom threshold (BETA)',
+      defaultMessage: 'Custom threshold (Technical Preview)',
     }),
     validate: {
       params: schema.object(
