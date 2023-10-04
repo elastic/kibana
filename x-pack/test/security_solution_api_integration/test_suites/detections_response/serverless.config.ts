08a2f8e9f9f004a724bbee20fc0bab9df4d81d01
wafaanasr <wafaa.aanasr@gmail.com>
2023-10-04 12:06:32 +0200

    add junit in ess and fix the serverless

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/serverless.config.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/serverless.config.ts
@@ -10,6 +10,6 @@ import { createTestConfig } from '../../config/serverless/config.base';
 export default createTestConfig({
   testFiles: [require.resolve('.')],
   junit: {
-    reportName: 'Detection Engine API Integration Tests',
+    reportName: 'Detection Engine Serverless API Integration Tests',
   },
 });
