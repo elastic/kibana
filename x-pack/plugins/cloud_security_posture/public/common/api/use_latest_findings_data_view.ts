7050e51de6a76bd1f94db79bc0848f5449dc25cd
Paulo Henrique <paulo.scape@gmail.com>
2023-10-04 15:10:00 -0700

    Update use_latest_findings_data_view.ts
    address PR comment

--- x-pack/plugins/cloud_security_posture/public/common/api/use_latest_findings_data_view.ts
+++ x-pack/plugins/cloud_security_posture/public/common/api/use_latest_findings_data_view.ts
@@ -15,9 +15,7 @@ import { CspClientPluginStartDeps } from '../../types';
 const cloudSecurityFieldLabels: Record<string, string> = {
   'result.evaluation': i18n.translate(
     'xpack.csp.findings.findingsTable.findingsTableColumn.resultColumnLabel',
-    {
-      defaultMessage: 'Result',
-    }
+    { defaultMessage: 'Result' }
   ),
   'resource.id': i18n.translate(
     'xpack.csp.findings.findingsTable.findingsTableColumn.resourceIdColumnLabel',
@@ -33,9 +31,7 @@ const cloudSecurityFieldLabels: Record<string, string> = {
   ),
   'rule.benchmark.rule_number': i18n.translate(
     'xpack.csp.findings.findingsTable.findingsTableColumn.ruleNumberColumnLabel',
-    {
-      defaultMessage: 'Rule Number',
-    }
+    { defaultMessage: 'Rule Number' }
   ),
   'rule.name': i18n.translate(
     'xpack.csp.findings.findingsTable.findingsTableColumn.ruleNameColumnLabel',
