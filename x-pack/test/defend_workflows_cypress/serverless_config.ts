5693c7c89f95060fba7dddb13f4cc8bd4e6b5df8
Paul Tavares <paul.tavares@elastic.co>
2023-10-04 11:50:07 -0400

    fix config again

--- x-pack/test/defend_workflows_cypress/serverless_config.ts
+++ x-pack/test/defend_workflows_cypress/serverless_config.ts
@@ -31,7 +31,7 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
       serverArgs: [...config.esTestCluster.serverArgs, 'http.host=0.0.0.0'],
     },
     esServerlessOptions: {
-      ...(config.get('esServerlessOptions') ?? {}),
+      ...(config.esServerlessOptions ?? {}),
       resources: Object.values(ES_RESOURCES),
     },
     servers: {
