c6fe3d021d8be6873610f85505d0acf1f29af66f
lcawl <lcawley@elastic.co>
2023-10-05 08:08:58 -0700

    Remove comments

--- x-pack/test_serverless/functional/test_suites/observability/screenshot_creation/response_ops_docs/index.ts
+++ x-pack/test_serverless/functional/test_suites/observability/screenshot_creation/response_ops_docs/index.ts
@@ -7,32 +7,20 @@
 
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 
-// export const ECOMMERCE_INDEX_PATTERN = 'kibana_sample_data_ecommerce';
-// export const FLIGHTS_INDEX_PATTERN = 'kibana_sample_data_flights';
-// export const LOGS_INDEX_PATTERN = 'kibana_sample_data_logs';
-
 export default function ({ getService, loadTestFile }: FtrProviderContext) {
   const browser = getService('browser');
   const ml = getService('ml');
-  // const sampleData = getService('sampleData');
-  // const svlCommonApi = getService('svlCommonApi');
 
   describe('response ops docs', function () {
     this.tags(['responseOps']);
 
     before(async () => {
-      // await sampleData.testResources.installAllKibanaSampleData(
-      //   svlCommonApi.getInternalRequestHeader()
-      // );
       await ml.testResources.setKibanaTimeZoneToUTC();
       await ml.testResources.disableKibanaAnnouncements();
       await browser.setWindowSize(1920, 1080);
     });
 
     after(async () => {
-      // await sampleData.testResources.removeAllKibanaSampleData(
-      //   svlCommonApi.getInternalRequestHeader()
-      // );
       await ml.testResources.resetKibanaTimeZone();
       await ml.testResources.resetKibanaAnnouncements();
     });
