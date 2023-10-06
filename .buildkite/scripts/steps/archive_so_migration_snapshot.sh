cb0bbfa81d9a87e0a969acd5f582f87eb273d1c7
Alex Szabo <alex.szabo@elastic.co>
2023-10-06 10:46:00 +0200

    chore(so_migrations): Upload generated manifest to google storage

--- .buildkite/scripts/steps/archive_so_migration_snapshot.sh
+++ .buildkite/scripts/steps/archive_so_migration_snapshot.sh
@@ -3,4 +3,14 @@ set -euo pipefail
 
 .buildkite/scripts/bootstrap.sh
 
-node scripts/snapshot_plugin_types
+SO_MIGRATIONS_SNAPSHOT_FOLDER=kibana-so-types-snapshots
+
+echo "--Creating snapshot of Saved Object migration info"
+node scripts/snapshot_plugin_types --outputPath "$1"
+
+echo "--Uploading as ${BUILDKITE_COMMIT}.json"
+SNAPSHOT_PATH="${SO_MIGRATIONS_SNAPSHOT_FOLDER}/${BUILDKITE_COMMIT}.json"
+gsutil cp "$1" "gs://$SNAPSHOT_PATH"
+
+buildkite-agent annotate --context so_migration_snapshot --style success \
+  'Snapshot is available at <a href="https://storage.googleapis.com/'"$SNAPSHOT_PATH"'">'"$SNAPSHOT_PATH"'</a>'
