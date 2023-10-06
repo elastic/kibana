35835caa777dc717c77cc5f1f47ff420af457e86
Kevin Logan <kevin.logan@elastic.co>
2023-10-06 09:55:39 -0400

    [Security Solution] Make the starting date either 18 months ago or the first date of this feature availability for controlled artifacts

--- x-pack/plugins/security_solution/public/management/pages/policy/view/protection_updates/protection_updates_layout.tsx
+++ x-pack/plugins/security_solution/public/management/pages/policy/view/protection_updates/protection_updates_layout.tsx
@@ -92,7 +92,9 @@ export const ProtectionUpdatesLayout = React.memo<ProtectionUpdatesLayoutProps>(
     const internalDateFormat = 'YYYY-MM-DD';
     const displayDateFormat = 'MMMM DD, YYYY';
     const formattedDate = moment.utc(deployedVersion, internalDateFormat).format(displayDateFormat);
-    const cutoffDate = moment.utc().subtract(18, 'months').add(1, 'day'); // Earliest selectable date
+    const startingDate = moment('2023-10-01').utc();
+    const eighteenMonthsAgo = moment.utc().subtract(18, 'months').add(1, 'day');
+    const cutoffDate = moment(startingDate).isAfter(eighteenMonthsAgo) ? startingDate : eighteenMonthsAgo // Earliest selectable date
 
     const viewModeSwitchLabel = automaticUpdatesEnabled
       ? AUTOMATIC_UPDATES_CHECKBOX_LABEL
