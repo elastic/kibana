b2ff606c6a4257373bb345e150c827a5997c5c62
Patryk Kopyciński <contact@patrykkopycinski.com>
2023-10-06 22:09:56 +0200

    Update artifacts.cy.ts

--- x-pack/plugins/security_solution/public/management/cypress/e2e/artifacts/artifacts.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/artifacts/artifacts.cy.ts
@@ -36,8 +36,7 @@ const yieldAppliedEndpointRevision = (): Cypress.Chainable<number> =>
 
 const parseRevNumber = (revString: string) => Number(revString.match(/\d+/)?.[0]);
 
-// Failing: See https://github.com/elastic/kibana/issues/168272
-describe.skip('Artifact pages', { tags: ['@ess', '@serverless', '@brokenInServerless'] }, () => {
+describe('Artifact pages', { tags: ['@ess', '@serverless', '@brokenInServerless'] }, () => {
   let indexedPolicy: IndexedFleetEndpointPolicyResponse;
   let policy: PolicyData;
   let createdHost: CreateAndEnrollEndpointHostResponse;
