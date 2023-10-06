26e997fcb7b6ec8bb0bccefee64a90cd3d211111
Kevin Logan <kevin.logan@elastic.co>
2023-10-06 15:48:37 -0400

    add test

--- x-pack/plugins/security_solution/common/endpoint/utils/controlled_artifact_rollout.ts
+++ x-pack/plugins/security_solution/common/endpoint/utils/controlled_artifact_rollout.ts
@@ -7,8 +7,7 @@
 
 import moment from 'moment';
 
-export function getControlledArtifactCutoffDate(): moment.Moment {
-    const startingDate = moment('2023-10-01T00:00:00Z').utc();
+export function getControlledArtifactCutoffDate(startingDate: moment.Moment = moment('2023-10-01T00:00:00Z').utc()): moment.Moment {
     const eighteenMonthsAgo = moment.utc().subtract(18, 'months').add(1, 'day');
     return moment(startingDate).isAfter(eighteenMonthsAgo) ? startingDate : eighteenMonthsAgo;
 }
