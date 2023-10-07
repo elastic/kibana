83c31d5f64a002348f483b6159ef3a318c96d17a
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2023-10-07 18:13:18 -0400

    [RAM] Bring back toggle column on alert table (#168158)
    FIX -> https://github.com/elastic/kibana/issues/155243
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: PhilippeOberti <philippe.oberti@elastic.co>
    (cherry picked from commit 44a9d283b25acd5e9862fe500e83a619ace87654)

--- x-pack/plugins/security_solution/public/flyout/left/components/host_details.tsx
+++ x-pack/plugins/security_solution/public/flyout/left/components/host_details.tsx
@@ -141,8 +141,8 @@ export const HostDetails: React.FC<HostDetailsProps> = ({ hostName, timestamp, s
                 value: user,
               }}
               mode={CellActionsMode.HOVER_RIGHT}
-              triggerId={SecurityCellActionsTrigger.DEFAULT} // TODO use SecurityCellActionsTrigger.DETAILS_FLYOUT when https://github.com/elastic/kibana/issues/155243 is fixed
-              visibleCellActions={5} // TODO use 6 when https://github.com/elastic/kibana/issues/155243 is fixed
+              triggerId={SecurityCellActionsTrigger.DETAILS_FLYOUT}
+              visibleCellActions={6}
               sourcererScopeId={getSourcererScopeId(scopeId)}
               metadata={{ scopeId }}
               showActionTooltips
