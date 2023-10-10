b47192dfb7e6a450c699af977bc9c77abe119c02
Abdul Zahid <awahab07@yahoo.com>
2023-10-10 16:32:37 +0200

    Fix comment.

--- x-pack/plugins/synthetics/public/apps/synthetics/render_app.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/render_app.tsx
@@ -66,7 +66,7 @@ export function renderApp(
           },
           {
             linkType: 'discuss',
-            href: 'https://discuss.elastic.co/c/uptime', // Redirects to https://discuss.elastic.co/c/uptime
+            href: 'https://discuss.elastic.co/c/uptime', // Redirects to https://discuss.elastic.co/c/observability/synthetics/75
           },
         ],
       }),
