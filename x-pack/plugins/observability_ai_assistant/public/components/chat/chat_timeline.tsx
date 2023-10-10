941b1c2217c5e270b7ec53bc823e7e501ac84be9
Coen Warmer <coen.warmer@gmail.com>
2023-10-10 17:21:45 +0200

    Consolidate collapsed items

--- x-pack/plugins/observability_ai_assistant/public/components/chat/chat_timeline.tsx
+++ x-pack/plugins/observability_ai_assistant/public/components/chat/chat_timeline.tsx
@@ -5,17 +5,17 @@
  * 2.0.
  */
 
-import React, { ReactNode } from 'react';
-import { css } from '@emotion/react';
-import { compact } from 'lodash';
+import React, { ReactNode, useMemo } from 'react';
+import { css } from '@emotion/css';
 import { EuiCommentList } from '@elastic/eui';
 import type { AuthenticatedUser } from '@kbn/security-plugin/common';
 import { ChatItem } from './chat_item';
 import { ChatWelcomePanel } from './chat_welcome_panel';
+import { ChatCollapsedItems } from './chat_collapsed_items';
 import type { Feedback } from '../feedback_buttons';
 import type { Message } from '../../../common';
-import { UseKnowledgeBaseResult } from '../../hooks/use_knowledge_base';
-import { ChatActionClickHandler } from './types';
+import type { UseKnowledgeBaseResult } from '../../hooks/use_knowledge_base';
+import type { ChatActionClickHandler } from './types';
 
 export interface ChatTimelineItem
   extends Pick<Message['message'], 'role' | 'content' | 'function_call'> {
@@ -56,7 +56,9 @@ export function ChatTimeline({
   onStopGenerating,
   onActionClick,
 }: ChatTimelineProps) {
-  const filteredItems = items.filter((item) => !item.display.hide);
+  const consolidatedItems = useMemo(() => {
+    return consolidateCollapsedItems(items);
+  }, [items]);
 
   return (
     <EuiCommentList
@@ -64,27 +66,61 @@ export function ChatTimeline({
         padding-bottom: 32px;
       `}
     >
-      {compact(
-        filteredItems.map((item, index) => (
-          <ChatItem
-            // use index, not id to prevent unmounting of component when message is persisted
-            key={index}
-            {...item}
-            onFeedbackClick={(feedback) => {
-              onFeedback(item, feedback);
-            }}
-            onRegenerateClick={() => {
-              onRegenerate(item);
-            }}
-            onEditSubmit={(message) => {
-              return onEdit(item, message);
-            }}
-            onStopGeneratingClick={onStopGenerating}
-            onActionClick={onActionClick}
-          />
-        ))
+      {consolidatedItems.length <= 1 ? (
+        <ChatWelcomePanel knowledgeBase={knowledgeBase} />
+      ) : (
+        consolidatedItems.map((item, index) =>
+          Array.isArray(item) ? (
+            <ChatCollapsedItems
+              key={index}
+              consolidatedItem={item}
+              onFeedback={onFeedback}
+              onRegenerate={onRegenerate}
+              onEditSubmit={onEdit}
+              onStopGenerating={onStopGenerating}
+              onActionClick={onActionClick}
+            />
+          ) : (
+            <ChatItem
+              // use index, not id to prevent unmounting of component when message is persisted
+              key={index}
+              {...item}
+              onFeedbackClick={(feedback) => {
+                onFeedback(item, feedback);
+              }}
+              onRegenerateClick={() => {
+                onRegenerate(item);
+              }}
+              onEditSubmit={(message) => onEdit(item, message)}
+              onStopGeneratingClick={onStopGenerating}
+              onActionClick={onActionClick}
+            />
+          )
+        )
       )}
-      {filteredItems.length === 1 ? <ChatWelcomePanel knowledgeBase={knowledgeBase} /> : null}
     </EuiCommentList>
   );
 }
+
+const consolidateCollapsedItems = (items: ChatTimelineItem[]) => {
+  const result: Array<ChatTimelineItem | ChatTimelineItem[]> = [];
+  let currentGroup: ChatTimelineItem[] | null = null;
+
+  for (const item of items) {
+    if (item.display.hide || !item) continue;
+
+    if (item.display.collapsed) {
+      if (currentGroup) {
+        currentGroup.push(item);
+      } else {
+        currentGroup = [item];
+        result.push(currentGroup);
+      }
+    } else {
+      result.push(item);
+      currentGroup = null;
+    }
+  }
+
+  return result;
+};
