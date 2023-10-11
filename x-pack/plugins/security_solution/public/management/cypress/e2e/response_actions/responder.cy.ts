72cbbc5e6cf03c77cec4c5af24c1077d45330e87
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-10-11 07:54:36 +0200

    test tags

--- x-pack/plugins/security_solution/public/management/cypress/e2e/response_actions/responder.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/response_actions/responder.cy.ts
@@ -1,8 +1,9 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import { closeAllToasts } from '../../tasks/toasts';
@@ -39,7 +40,7 @@ describe('When accessing Endpoint Response Console', { tags: ['@ess', '@serverle
     login();
   });
 
-  describe('from Cases', () => {
+  describe('from Cases', { tags: ['@ess', '@serverless'] }, () => {
     let endpointData: ReturnTypeFromChainable<typeof indexEndpointHosts>;
     let caseData: ReturnTypeFromChainable<typeof indexNewCase>;
     let alertData: ReturnTypeFromChainable<typeof indexEndpointRuleAlerts>;
