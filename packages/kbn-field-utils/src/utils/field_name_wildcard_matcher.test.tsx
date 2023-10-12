b6a4429ec2b48c1e877325c2b042113e43854539
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-10-12 10:59:00 +0200

    [UnifiedDocViewer] Update tests

--- packages/kbn-field-utils/src/utils/field_name_wildcard_matcher.test.tsx
+++ packages/kbn-field-utils/src/utils/field_name_wildcard_matcher.test.tsx
@@ -7,45 +7,68 @@
  */
 
 import { type DataViewField } from '@kbn/data-views-plugin/common';
-import { fieldNameWildcardMatcher } from './field_name_wildcard_matcher';
+import {
+  fieldNameWildcardMatcher,
+  getFieldSearchMatchingHighlight,
+} from './field_name_wildcard_matcher';
 
 const name = 'test.this_value.maybe';
-describe('UnifiedFieldList fieldNameWildcardMatcher()', () => {
-  it('should work correctly with wildcard', async () => {
-    expect(fieldNameWildcardMatcher({ displayName: 'test' } as DataViewField, 'no')).toBe(false);
-    expect(
-      fieldNameWildcardMatcher({ displayName: 'test', name: 'yes' } as DataViewField, 'yes')
-    ).toBe(true);
+describe('fieldNameWildcardMatcher', function () {
+  describe('fieldNameWildcardMatcher()', () => {
+    it('should work correctly with wildcard', async () => {
+      expect(fieldNameWildcardMatcher({ displayName: 'test' } as DataViewField, 'no')).toBe(false);
+      expect(
+        fieldNameWildcardMatcher({ displayName: 'test', name: 'yes' } as DataViewField, 'yes')
+      ).toBe(true);
 
-    const search = 'test*ue';
-    expect(fieldNameWildcardMatcher({ displayName: 'test' } as DataViewField, search)).toBe(false);
-    expect(fieldNameWildcardMatcher({ displayName: 'test.value' } as DataViewField, search)).toBe(
-      true
-    );
-    expect(fieldNameWildcardMatcher({ name: 'test.this_value' } as DataViewField, search)).toBe(
-      true
-    );
-    expect(fieldNameWildcardMatcher({ name: 'message.test' } as DataViewField, search)).toBe(false);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, search)).toBe(false);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, `${search}*`)).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, '*value*')).toBe(true);
+      const search = 'test*ue';
+      expect(fieldNameWildcardMatcher({ displayName: 'test' } as DataViewField, search)).toBe(
+        false
+      );
+      expect(fieldNameWildcardMatcher({ displayName: 'test.value' } as DataViewField, search)).toBe(
+        true
+      );
+      expect(fieldNameWildcardMatcher({ name: 'test.this_value' } as DataViewField, search)).toBe(
+        true
+      );
+      expect(fieldNameWildcardMatcher({ name: 'message.test' } as DataViewField, search)).toBe(
+        false
+      );
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, search)).toBe(false);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, `${search}*`)).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, '*value*')).toBe(true);
+    });
+
+    it('should work correctly with spaces', async () => {
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test maybe    ')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test maybe*')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test. this')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'this      _value be')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'this')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, '  value  ')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'be')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test this here')).toBe(false);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test that')).toBe(false);
+      expect(fieldNameWildcardMatcher({ name } as DataViewField, '    ')).toBe(false);
+      expect(fieldNameWildcardMatcher({ name: 'geo.location3' } as DataViewField, '3')).toBe(true);
+      expect(fieldNameWildcardMatcher({ name: 'geo_location3' } as DataViewField, 'geo 3')).toBe(
+        true
+      );
+    });
   });
 
-  it('should work correctly with spaces', async () => {
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test maybe    ')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test maybe*')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test. this')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'this      _value be')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'this')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, '  value  ')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'be')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test this here')).toBe(false);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, 'test that')).toBe(false);
-    expect(fieldNameWildcardMatcher({ name } as DataViewField, '    ')).toBe(false);
-    expect(fieldNameWildcardMatcher({ name: 'geo.location3' } as DataViewField, '3')).toBe(true);
-    expect(fieldNameWildcardMatcher({ name: 'geo_location3' } as DataViewField, 'geo 3')).toBe(
-      true
-    );
+  describe('getFieldSearchMatchingHighlight()', function () {
+    it('should correctly return only partial match', async () => {
+      expect(getFieldSearchMatchingHighlight('test this', 'test')).toBe('test');
+      expect(getFieldSearchMatchingHighlight('test this', 'this')).toBe('this');
+      expect(getFieldSearchMatchingHighlight('test this')).toBe('');
+    });
+
+    it('should correctly return a full match for a wildcard search', async () => {
+      expect(getFieldSearchMatchingHighlight('Test this', 'test*')).toBe('Test this');
+      expect(getFieldSearchMatchingHighlight('test this', '*this')).toBe('test this');
+      expect(getFieldSearchMatchingHighlight('test this', ' te th')).toBe('test this');
+    });
   });
 });
