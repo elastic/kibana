92b8c5fe8c21954a9f527fac45fb385199403f88
Jan Monschke <jan.monschke@elastic.co>
2023-10-13 18:38:46 +0200

    make dataprovider tests mergeable

--- x-pack/test/security_solution_cypress/cypress/e2e/investigations/timelines/data_providers.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/investigations/timelines/data_providers.cy.ts
@@ -31,65 +31,61 @@ import { hostsUrl } from '../../../urls/navigation';
 import { cleanKibana, scrollToBottom } from '../../../tasks/common';
 
 // Failing in serverless
-describe(
-  'timeline data providers',
-  { tags: ['@ess', '@serverless', '@brokenInServerless'] },
-  () => {
-    before(() => {
-      cleanKibana();
-    });
+describe('timeline data providers', { tags: ['@ess', '@serverless'] }, () => {
+  before(() => {
+    cleanKibana();
+  });
 
-    beforeEach(() => {
-      login();
-      visitWithTimeRange(hostsUrl('allHosts'));
-      waitForAllHostsToBeLoaded();
-      scrollToBottom();
-      createNewTimeline();
-      addNameAndDescriptionToTimeline(getTimeline());
-      populateTimeline();
-    });
+  beforeEach(() => {
+    login();
+    visitWithTimeRange(hostsUrl('allHosts'));
+    waitForAllHostsToBeLoaded();
+    scrollToBottom();
+    createNewTimeline();
+    addNameAndDescriptionToTimeline(getTimeline());
+    populateTimeline();
+  });
 
-    it('displays the data provider action menu when Enter is pressed', () => {
-      addDataProvider({ field: 'host.name', operator: 'exists' });
+  it('displays the data provider action menu when Enter is pressed', () => {
+    addDataProvider({ field: 'host.name', operator: 'exists' });
 
-      cy.get(TIMELINE_DATA_PROVIDERS_ACTION_MENU).should('not.exist');
-      cy.get(`${TIMELINE_FLYOUT_HEADER} ${TIMELINE_DROPPED_DATA_PROVIDERS}`).focus();
-      cy.get(`${TIMELINE_FLYOUT_HEADER} ${TIMELINE_DROPPED_DATA_PROVIDERS}`)
-        .first()
-        .parent()
-        .type('{enter}');
+    cy.get(TIMELINE_DATA_PROVIDERS_ACTION_MENU).should('not.exist');
+    cy.get(`${TIMELINE_FLYOUT_HEADER} ${TIMELINE_DROPPED_DATA_PROVIDERS}`).focus();
+    cy.get(`${TIMELINE_FLYOUT_HEADER} ${TIMELINE_DROPPED_DATA_PROVIDERS}`)
+      .first()
+      .parent()
+      .type('{enter}');
 
-      cy.get(TIMELINE_DATA_PROVIDERS_ACTION_MENU).should('exist');
-    });
+    cy.get(TIMELINE_DATA_PROVIDERS_ACTION_MENU).should('exist');
+  });
 
-    it(
-      'persists timeline when data provider is updated by dragging a field from data grid',
-      { tags: ['@brokenInServerless'] },
-      () => {
-        updateDataProviderbyDraggingField('host.name', 0);
-        saveTimeline();
-        cy.reload();
-        cy.get(`${GET_TIMELINE_GRID_CELL('host.name')}`)
-          .first()
-          .then((hostname) => {
-            cy.get(TIMELINE_DATA_PROVIDERS_CONTAINER).contains(`host.name: "${hostname.text()}"`);
-          });
-      }
-    );
-
-    it('persists timeline when a field is added by hover action "Add To Timeline" in data provider ', () => {
-      addDataProvider({ field: 'host.name', operator: 'exists' });
-      saveTimeline();
-      updateDataProviderByFieldHoverAction('host.name', 0);
+  it(
+    'persists timeline when data provider is updated by dragging a field from data grid',
+    { tags: ['@brokenInServerless'] },
+    () => {
+      updateDataProviderbyDraggingField('host.name', 0);
       saveTimeline();
       cy.reload();
       cy.get(`${GET_TIMELINE_GRID_CELL('host.name')}`)
         .first()
         .then((hostname) => {
-          cy.get(TIMELINE_DATA_PROVIDERS_CONTAINER).should((dataProviderContainer) => {
-            expect(dataProviderContainer).to.contain(`host.name: "${hostname.text()}"`);
-          });
+          cy.get(TIMELINE_DATA_PROVIDERS_CONTAINER).contains(`host.name: "${hostname.text()}"`);
         });
-    });
-  }
-);
+    }
+  );
+
+  it('persists timeline when a field is added by hover action "Add To Timeline" in data provider ', () => {
+    addDataProvider({ field: 'host.name', operator: 'exists' });
+    saveTimeline();
+    updateDataProviderByFieldHoverAction('host.name', 0);
+    saveTimeline();
+    cy.reload();
+    cy.get(`${GET_TIMELINE_GRID_CELL('host.name')}`)
+      .first()
+      .then((hostname) => {
+        cy.get(TIMELINE_DATA_PROVIDERS_CONTAINER).should((dataProviderContainer) => {
+          expect(dataProviderContainer).to.contain(`host.name: "${hostname.text()}"`);
+        });
+      });
+  });
+});
