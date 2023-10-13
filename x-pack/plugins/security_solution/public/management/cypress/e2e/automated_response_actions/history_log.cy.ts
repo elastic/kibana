6b7e2c69a97f4ab3352e47e84266c6dddf7ef5c3
Paul Tavares <paul.tavares@elastic.co>
2023-10-13 15:16:06 -0400

    skip history log for automated response actions

--- x-pack/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/history_log.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/history_log.cy.ts
@@ -12,81 +12,92 @@ import { indexEndpointRuleAlerts } from '../../tasks/index_endpoint_rule_alerts'
 
 import { login, ROLE } from '../../tasks/login';
 
-describe('Response actions history page', { tags: ['@ess', '@serverless'] }, () => {
-  let endpointData: ReturnTypeFromChainable<typeof indexEndpointHosts> | undefined;
-  let endpointDataWithAutomated: ReturnTypeFromChainable<typeof indexEndpointHosts> | undefined;
-  let alertData: ReturnTypeFromChainable<typeof indexEndpointRuleAlerts> | undefined;
-  const [endpointAgentId, endpointHostname] = generateRandomStringName(2);
+describe(
+  'Response actions history page',
+  {
+    tags: [
+      '@ess',
+      '@serverless',
+      // Not supported in serverless! Currently using a custom role that is not available in serverless
+      '@brokenInServerless',
+    ],
+  },
+  () => {
+    let endpointData: ReturnTypeFromChainable<typeof indexEndpointHosts> | undefined;
+    let endpointDataWithAutomated: ReturnTypeFromChainable<typeof indexEndpointHosts> | undefined;
+    let alertData: ReturnTypeFromChainable<typeof indexEndpointRuleAlerts> | undefined;
+    const [endpointAgentId, endpointHostname] = generateRandomStringName(2);
 
-  before(() => {
-    indexEndpointHosts({ numResponseActions: 2 }).then((indexEndpoints) => {
-      endpointData = indexEndpoints;
-    });
-    indexEndpointRuleAlerts({
-      endpointAgentId,
-      endpointHostname,
-      endpointIsolated: false,
-    }).then((indexedAlert) => {
-      alertData = indexedAlert;
-      const alertId = alertData.alerts[0]._id;
-      return indexEndpointHosts({
-        numResponseActions: 1,
-        alertIds: [alertId],
-      }).then((indexEndpoints) => {
-        endpointDataWithAutomated = indexEndpoints;
+    before(() => {
+      indexEndpointHosts({ numResponseActions: 2 }).then((indexEndpoints) => {
+        endpointData = indexEndpoints;
+      });
+      indexEndpointRuleAlerts({
+        endpointAgentId,
+        endpointHostname,
+        endpointIsolated: false,
+      }).then((indexedAlert) => {
+        alertData = indexedAlert;
+        const alertId = alertData.alerts[0]._id;
+        return indexEndpointHosts({
+          numResponseActions: 1,
+          alertIds: [alertId],
+        }).then((indexEndpoints) => {
+          endpointDataWithAutomated = indexEndpoints;
+        });
       });
     });
-  });
 
-  after(() => {
-    if (endpointDataWithAutomated) {
-      endpointDataWithAutomated.cleanup();
-      endpointDataWithAutomated = undefined;
-    }
-    if (endpointData) {
-      endpointData.cleanup();
-      endpointData = undefined;
-    }
+    after(() => {
+      if (endpointDataWithAutomated) {
+        endpointDataWithAutomated.cleanup();
+        endpointDataWithAutomated = undefined;
+      }
+      if (endpointData) {
+        endpointData.cleanup();
+        endpointData = undefined;
+      }
 
-    if (alertData) {
-      alertData.cleanup();
-      alertData = undefined;
-    }
-  });
-
-  beforeEach(() => {
-    login(ROLE.soc_manager);
-  });
-
-  it('enable filtering by type', () => {
-    cy.visit(`/app/security/administration/response_actions_history`);
-
-    let maxLength: number;
-    cy.getByTestSubj('response-actions-list').then(($table) => {
-      maxLength = $table.find('tbody .euiTableRow').length;
-      cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength);
+      if (alertData) {
+        alertData.cleanup();
+        alertData = undefined;
+      }
     });
 
-    cy.getByTestSubj('response-actions-list-type-filter-popoverButton').click();
-    cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
-    cy.getByTestSubj('response-actions-list').within(() => {
-      cy.get('tbody .euiTableRow').should('have.lengthOf', 1);
-      cy.get('tbody .euiTableRow').eq(0).contains('Triggered by rule');
+    beforeEach(() => {
+      login(ROLE.endpoint_response_actions_access);
     });
-    cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
-    cy.getByTestSubj('response-actions-list').within(() => {
-      cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength);
+
+    it('enable filtering by type', () => {
+      cy.visit(`/app/security/administration/response_actions_history`);
+
+      let maxLength: number;
+      cy.getByTestSubj('response-actions-list').then(($table) => {
+        maxLength = $table.find('tbody .euiTableRow').length;
+        cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength);
+      });
+
+      cy.getByTestSubj('response-actions-list-type-filter-popoverButton').click();
+      cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
+      cy.getByTestSubj('response-actions-list').within(() => {
+        cy.get('tbody .euiTableRow').should('have.lengthOf', 1);
+        cy.get('tbody .euiTableRow').eq(0).contains('Triggered by rule');
+      });
+      cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
+      cy.getByTestSubj('response-actions-list').within(() => {
+        cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength);
+      });
+      cy.getByTestSubj('type-filter-option').contains('Triggered manually').click();
+      cy.getByTestSubj('response-actions-list').within(() => {
+        cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength - 1);
+      });
+      cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
+      cy.getByTestSubj('response-actions-list').within(() => {
+        cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength);
+        cy.get('tbody .euiTableRow').eq(0).contains('Triggered by rule').click();
+      });
+      // check if we were moved to Rules app after clicking Triggered by rule
+      cy.getByTestSubj('breadcrumb last').contains('Detection rules (SIEM)');
     });
-    cy.getByTestSubj('type-filter-option').contains('Triggered manually').click();
-    cy.getByTestSubj('response-actions-list').within(() => {
-      cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength - 1);
-    });
-    cy.getByTestSubj('type-filter-option').contains('Triggered by rule').click();
-    cy.getByTestSubj('response-actions-list').within(() => {
-      cy.get('tbody .euiTableRow').should('have.lengthOf', maxLength);
-      cy.get('tbody .euiTableRow').eq(0).contains('Triggered by rule').click();
-    });
-    // check if we were moved to Rules app after clicking Triggered by rule
-    cy.getByTestSubj('breadcrumb last').contains('Detection rules (SIEM)');
-  });
-});
+  }
+);
