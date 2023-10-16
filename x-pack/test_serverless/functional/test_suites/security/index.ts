fb6d2bfdc90bf16ddfaca3f694b657e861cf00d1
Christos Nasikas <christos.nasikas@elastic.co>
2023-10-16 14:17:07 +0300

    Enable all tests

--- x-pack/test_serverless/functional/test_suites/security/index.ts
+++ x-pack/test_serverless/functional/test_suites/security/index.ts
@@ -9,11 +9,11 @@ import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('serverless security UI', function () {
-    // loadTestFile(require.resolve('./ftr/landing_page'));
-    // loadTestFile(require.resolve('./ftr/navigation'));
-    // loadTestFile(require.resolve('./ftr/management'));
+    loadTestFile(require.resolve('./ftr/landing_page'));
+    loadTestFile(require.resolve('./ftr/navigation'));
+    loadTestFile(require.resolve('./ftr/management'));
     loadTestFile(require.resolve('./ftr/cases'));
-    // loadTestFile(require.resolve('./advanced_settings'));
-    // loadTestFile(require.resolve('./ml'));
+    loadTestFile(require.resolve('./advanced_settings'));
+    loadTestFile(require.resolve('./ml'));
   });
 }
