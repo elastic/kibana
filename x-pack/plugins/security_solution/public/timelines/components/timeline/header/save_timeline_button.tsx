17acc0d2fbbc4c839da75d06cfff6f9d6925dec9
Jan Monschke <jan.monschke@elastic.co>
2023-10-16 20:35:48 +0200

    move use_edit_timeline_modal logic into save timeline button

--- x-pack/plugins/security_solution/public/timelines/components/timeline/header/save_timeline_button.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/header/save_timeline_button.tsx
@@ -5,13 +5,14 @@
  * 2.0.
  */
 
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { EuiButton, EuiToolTip } from '@elastic/eui';
 import { FormattedRelative } from '@kbn/i18n-react';
 import { getTimelineStatusByIdSelector } from '../../flyout/header/selectors';
 import { useDeepEqualSelector } from '../../../../common/hooks/use_selector';
 import { TimelineStatus } from '../../../../../common/api/timeline';
-import { useEditTimelineModal } from './use_edit_timeline_modal';
+import { useUserPrivileges } from '../../../../common/components/user_privileges';
+import { SaveTimelineModal } from './save_timeline_modal';
 import * as timelineTranslations from './translations';
 import * as sharedTranslations from '../../flyout/header/translations';
 
@@ -20,9 +21,24 @@ export interface SaveTimelineButtonProps {
 }
 
 export const SaveTimelineButton = React.memo<SaveTimelineButtonProps>(({ timelineId }) => {
-  const { EditModal, canEditTimeline, openEditTimeline } = useEditTimelineModal({
-    timelineId,
-  });
+  const [showEditTimelineOverlay, setShowEditTimelineOverlay] = useState<boolean>(false);
+
+  const closeSaveTimeline = useCallback(() => {
+    setShowEditTimelineOverlay(false);
+  }, []);
+
+  const openEditTimeline = useCallback(() => {
+    setShowEditTimelineOverlay(true);
+  }, []);
+
+  // Case: 1
+  // check if user has crud privileges so that user can be allowed to edit the timeline
+  // Case: 2
+  // TODO: User may have Crud privileges but they may not have access to timeline index.
+  // Do we need to check that?
+  const {
+    kibanaSecuritySolutionsPrivileges: { crud: canEditTimeline },
+  } = useUserPrivileges();
   const getTimelineStatus = useMemo(() => getTimelineStatusByIdSelector(), []);
   const {
     status: timelineStatus,
@@ -32,20 +48,8 @@ export const SaveTimelineButton = React.memo<SaveTimelineButtonProps>(({ timelin
   } = useDeepEqualSelector((state) => getTimelineStatus(state, timelineId));
 
   const isUnsaved = timelineStatus === TimelineStatus.draft;
+  // Urge the user to save the timeline because it's not been saved yet or it has changed
   const urgeToSave = isUnsaved || changed;
-  // const { startTransaction } = useStartTransaction();
-  // const dispatch = useDispatch();
-
-  // const isDraft = useMemo(() => timelineStatus === TimelineStatus.draft, [timelineStatus]);
-
-  // const onOpen = useCallback(() => {
-  //   if (isDraft) {
-  //     openEditTimeline();
-  //   } else {
-  //     startTransaction({ name: TIMELINE_ACTIONS.SAVE });
-  //     dispatch(timelineActions.saveTimeline({ id: timelineId }));
-  //   }
-  // }, [startTransaction, timelineId, dispatch, isDraft, openEditTimeline]);
 
   let tooltipContent: React.ReactNode = null;
   if (canEditTimeline) {
@@ -68,7 +72,6 @@ export const SaveTimelineButton = React.memo<SaveTimelineButtonProps>(({ timelin
     tooltipContent = timelineTranslations.CALL_OUT_UNAUTHORIZED_MSG;
   }
 
-  const buttonColor = urgeToSave ? 'success' : 'primary';
   return (
     <EuiToolTip
       content={tooltipContent}
@@ -78,7 +81,7 @@ export const SaveTimelineButton = React.memo<SaveTimelineButtonProps>(({ timelin
       <>
         <EuiButton
           fill
-          color={buttonColor}
+          color={urgeToSave ? 'success' : 'primary'}
           onClick={openEditTimeline}
           iconType="save"
           isLoading={isSaving}
@@ -87,7 +90,14 @@ export const SaveTimelineButton = React.memo<SaveTimelineButtonProps>(({ timelin
         >
           {timelineTranslations.SAVE}
         </EuiButton>
-        {EditModal}
+        {showEditTimelineOverlay && canEditTimeline ? (
+          <SaveTimelineModal
+            closeSaveTimeline={closeSaveTimeline}
+            initialFocus="title"
+            timelineId={timelineId}
+            showWarning={false}
+          />
+        ) : null}
       </>
     </EuiToolTip>
   );
