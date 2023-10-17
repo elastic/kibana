ba354b293bb4666721ee088121577b013bd4880d
Jonathan Budzenski <jon@elastic.co>
2023-10-17 14:33:22 -0500

    update yarn.lock

--- .buildkite/scripts/steps/checks/yarn_deduplicate.sh
+++ .buildkite/scripts/steps/checks/yarn_deduplicate.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 source .buildkite/scripts/common/util.sh
 
-echo "--- Deduplicating yarn.lock"
+echo "--- Check yarn.lock for duplicated modules"
 node scripts/yarn_deduplicate
 
 check_for_changed_files 'node scripts/yarn_deduplicate && yarn kbn bootstrap' false
