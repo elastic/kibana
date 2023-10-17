9e951f59ac3e63238d8e64048c38173f69bb1feb
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2023-10-17 12:12:38 +0200

    [Lens] passing locale to charts (#168571)
    ## Summary
    
    <img width="973" alt="Screenshot 2023-10-17 at 11 13 20"
    src="https://github.com/elastic/kibana/assets/4283304/ff08efbe-dec7-4a45-ad25-17debb679864">
    
    Passes `locale={i18n.getLocale()}` to <Settings/> Component for elastic
    charts. For now it only matters for translating months, days etc in time
    histogram chart, but I'd rather pass it everywhere in case if we
    introduce some copy in the future.
    
    Fixes https://github.com/elastic/kibana/issues/166240

--- src/plugins/chart_expressions/expression_gauge/public/components/gauge_component.tsx
+++ src/plugins/chart_expressions/expression_gauge/public/components/gauge_component.tsx
@@ -14,6 +14,7 @@ import type { CustomPaletteState } from '@kbn/charts-plugin/public';
 import { EmptyPlaceholder } from '@kbn/charts-plugin/public';
 import { getOverridesFor } from '@kbn/chart-expressions-common';
 import { isVisDimension } from '@kbn/visualizations-plugin/common/utils';
+import { i18n } from '@kbn/i18n';
 import {
   GaugeRenderProps,
   GaugeLabelMajorMode,
@@ -370,6 +371,7 @@ export const GaugeComponent: FC<GaugeRenderProps> = memo(
             ariaLabel={args.ariaLabel}
             ariaUseDefaultSummary={!args.ariaLabel}
             onRenderChange={onRenderChange}
+            locale={i18n.getLocale()}
             {...getOverridesFor(overrides, 'settings')}
           />
           <Goal
