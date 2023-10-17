7b2700841b8a9b0ef94b332f8515e35455e3f904
Christos Nasikas <christos.nasikas@elastic.co>
2023-10-17 14:47:16 +0300

    Define connector params initial schema

--- x-pack/plugins/cases/server/connectors/cases/schema.ts
+++ x-pack/plugins/cases/server/connectors/cases/schema.ts
@@ -7,13 +7,38 @@
 
 import { schema } from '@kbn/config-schema';
 
+const AlertSchema = schema.recordOf(schema.string(), schema.string(), {
+  validate: (value) => {
+    if (!Object.hasOwn(value, 'id') || !Object.hasOwn(value, 'index')) {
+      return 'Alert ID and index must be defined';
+    }
+  },
+});
+
+/**
+ * At the moment only one field is supported for grouping
+ */
+const GroupingSchema = schema.arrayOf(schema.string(), { minSize: 0, maxSize: 1 });
+
+const RuleSchema = schema.object({
+  id: schema.string(),
+  name: schema.string(),
+  /**
+   * TODO: Verify limits
+   */
+  tags: schema.arrayOf(schema.string({ minLength: 1, maxLength: 50 }), { minSize: 0, maxSize: 10 }),
+});
+
 /**
  * The case connector does not have any configuration
  * or secrets.
  */
 export const CasesConnectorConfigSchema = schema.object({});
 export const CasesConnectorSecretsSchema = schema.object({});
-/**
- * TODO: Add needed properties in the params schema.
- */
-export const CasesConnectorParamsSchema = schema.object({});
+
+export const CasesConnectorParamsSchema = schema.object({
+  alerts: schema.arrayOf(AlertSchema),
+  groupingBy: GroupingSchema,
+  owner: schema.string(),
+  rule: RuleSchema,
+});
