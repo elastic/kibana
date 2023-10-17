2cce10d53dad2af632b1fb8bbebbfe4e1e7ffa87
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2023-10-17 22:48:20 +0200

    Fix osquery

--- x-pack/plugins/infra/public/components/asset_details/tabs/osquery/osquery.tsx
+++ x-pack/plugins/infra/public/components/asset_details/tabs/osquery/osquery.tsx
@@ -6,22 +6,22 @@
  */
 
 import { EuiSkeletonText } from '@elastic/eui';
-import React, { useMemo } from 'react';
-import usePreviousDistinct from 'react-use/lib/usePreviousDistinct';
+import React, { useRef } from 'react';
 import { usePluginConfig } from '../../../../containers/plugin_config_context';
 import { useKibanaContextForPlugin } from '../../../../hooks/use_kibana';
 import { useMetadataStateProviderContext } from '../../hooks/use_metadata_state';
 
 export const Osquery = () => {
   const { metadata, loading } = useMetadataStateProviderContext();
+  const agentId = useRef<string | null>(null);
 
-  const prevAgentId = usePreviousDistinct(metadata?.info?.agent?.id, (prev, curr) => {
-    // When a host has multiple agents reporting metrics, it might be that one of them is not reporting an agent id.
-    // This returns the previous state when the current agent.id is null and the previous is not,
-    // ensuring that it doesn't lose the valid known agent.id
-    return !!prev && !curr;
-  });
-  const agentId = useMemo(() => prevAgentId ?? 'null', [prevAgentId]);
+  // When a host has multiple agents reporting metrics, it might be that one of them is not reporting an agent id.
+  // This returns the previous state when the current agent.id is null and the previous is not,
+  // ensuring that it doesn't lose the valid known agent.id
+  if (!loading && !!metadata?.info?.agent?.id && !agentId.current) {
+    agentId.current = metadata?.info?.agent?.id;
+  }
+
   const { featureFlags } = usePluginConfig();
 
   if (!featureFlags.osqueryEnabled) {
@@ -29,7 +29,11 @@ export const Osquery = () => {
   }
 
   const isLoading = loading && !metadata;
-  return isLoading ? <EuiSkeletonText lines={10} /> : <MemoOsQueryAction agentId={agentId} />;
+  return isLoading ? (
+    <EuiSkeletonText lines={10} />
+  ) : (
+    <MemoOsQueryAction agentId={agentId.current ?? 'null'} />
+  );
 };
 
 const MemoOsQueryAction = React.memo(({ agentId }: { agentId: string }) => {
