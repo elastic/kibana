baccca87117cad5e151ad3940d31406a2c17b9a5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-10-18 23:01:56 +0000

    [CI] Auto-commit changed files from 'node scripts/precommit_hook.js --ref HEAD~1..HEAD --fix'

--- x-pack/plugins/apm/public/components/app/service_dashboards/dashboard_selector.tsx
+++ x-pack/plugins/apm/public/components/app/service_dashboards/dashboard_selector.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { EuiComboBox } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
