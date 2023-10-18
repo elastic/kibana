075e849cd1aba6bf0855caa6c040282c3c54e7dc
Sébastien Loix <sabee77@gmail.com>
2023-10-18 16:44:26 +0100

    Fix observability nav

--- x-pack/plugins/serverless_observability/public/components/side_navigation/index.tsx
+++ x-pack/plugins/serverless_observability/public/components/side_navigation/index.tsx
@@ -31,85 +31,80 @@ const navigationTree: NavigationTreeDefinition = {
       breadcrumbStatus: 'hidden',
       children: [
         {
+          title: i18n.translate('xpack.serverlessObservability.nav.logExplorer', {
+            defaultMessage: 'Log Explorer',
+          }),
+          link: 'observability-log-explorer',
+        },
+        {
+          title: i18n.translate('xpack.serverlessObservability.nav.dashboards', {
+            defaultMessage: 'Dashboards',
+          }),
+          link: 'dashboards',
+          getIsActive: ({ pathNameSerialized, prepend }) => {
+            return pathNameSerialized.startsWith(prepend('/app/dashboards'));
+          },
+        },
+        {
+          title: i18n.translate('xpack.serverlessObservability.nav.visualizations', {
+            defaultMessage: 'Visualizations',
+          }),
+          link: 'visualize',
+          getIsActive: ({ pathNameSerialized, prepend }) => {
+            return (
+              pathNameSerialized.startsWith(prepend('/app/visualize')) ||
+              pathNameSerialized.startsWith(prepend('/app/lens')) ||
+              pathNameSerialized.startsWith(prepend('/app/maps'))
+            );
+          },
+        },
+        {
+          link: 'observability-overview:alerts',
+        },
+        {
+          link: 'observability-overview:cases',
+        },
+        {
+          link: 'observability-overview:slos',
+        },
+        {
+          id: 'aiops',
+          title: 'AIOps',
+          renderAs: 'accordion',
+          accordionProps: {
+            arrowProps: { css: { display: 'none' } },
+          },
+          spaceBefore: null,
           children: [
             {
-              title: i18n.translate('xpack.serverlessObservability.nav.logExplorer', {
-                defaultMessage: 'Log Explorer',
+              title: i18n.translate('xpack.serverlessObservability.nav.ml.jobs', {
+                defaultMessage: 'Anomaly detection',
               }),
-              link: 'observability-log-explorer',
+              link: 'ml:anomalyDetection',
             },
             {
-              title: i18n.translate('xpack.serverlessObservability.nav.dashboards', {
-                defaultMessage: 'Dashboards',
+              title: i18n.translate('xpack.serverlessObservability.ml.logRateAnalysis', {
+                defaultMessage: 'Log rate analysis',
               }),
-              link: 'dashboards',
+              link: 'ml:logRateAnalysis',
               getIsActive: ({ pathNameSerialized, prepend }) => {
-                return pathNameSerialized.startsWith(prepend('/app/dashboards'));
+                return pathNameSerialized.includes(prepend('/app/ml/aiops/log_rate_analysis'));
               },
             },
             {
-              title: i18n.translate('xpack.serverlessObservability.nav.visualizations', {
-                defaultMessage: 'Visualizations',
+              title: i18n.translate('xpack.serverlessObservability.ml.changePointDetection', {
+                defaultMessage: 'Change point detection',
               }),
-              link: 'visualize',
+              link: 'ml:changePointDetections',
               getIsActive: ({ pathNameSerialized, prepend }) => {
-                return (
-                  pathNameSerialized.startsWith(prepend('/app/visualize')) ||
-                  pathNameSerialized.startsWith(prepend('/app/lens')) ||
-                  pathNameSerialized.startsWith(prepend('/app/maps'))
-                );
+                return pathNameSerialized.includes(prepend('/app/ml/aiops/change_point_detection'));
               },
             },
             {
-              link: 'observability-overview:alerts',
-            },
-            {
-              link: 'observability-overview:cases',
-            },
-            {
-              link: 'observability-overview:slos',
-            },
-            {
-              id: 'aiops',
-              title: 'AIOps',
-              renderAs: 'accordion',
-              accordionProps: {
-                arrowProps: { css: { display: 'none' } },
-              },
-              children: [
-                {
-                  title: i18n.translate('xpack.serverlessObservability.nav.ml.jobs', {
-                    defaultMessage: 'Anomaly detection',
-                  }),
-                  link: 'ml:anomalyDetection',
-                },
-                {
-                  title: i18n.translate('xpack.serverlessObservability.ml.logRateAnalysis', {
-                    defaultMessage: 'Log rate analysis',
-                  }),
-                  link: 'ml:logRateAnalysis',
-                  getIsActive: ({ pathNameSerialized, prepend }) => {
-                    return pathNameSerialized.includes(prepend('/app/ml/aiops/log_rate_analysis'));
-                  },
-                },
-                {
-                  title: i18n.translate('xpack.serverlessObservability.ml.changePointDetection', {
-                    defaultMessage: 'Change point detection',
-                  }),
-                  link: 'ml:changePointDetections',
-                  getIsActive: ({ pathNameSerialized, prepend }) => {
-                    return pathNameSerialized.includes(
-                      prepend('/app/ml/aiops/change_point_detection')
-                    );
-                  },
-                },
-                {
-                  title: i18n.translate('xpack.serverlessObservability.nav.ml.job.notifications', {
-                    defaultMessage: 'Job notifications',
-                  }),
-                  link: 'ml:notifications',
-                },
-              ],
+              title: i18n.translate('xpack.serverlessObservability.nav.ml.job.notifications', {
+                defaultMessage: 'Job notifications',
+              }),
+              link: 'ml:notifications',
             },
           ],
         },
