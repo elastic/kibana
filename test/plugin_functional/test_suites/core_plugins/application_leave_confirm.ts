1e089983aac9a8ecf7a47626eaa2a5dda94a6d38
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2023-10-18 15:05:44 -0700

    Refactors tests, helpers and plugin to take delayed rendering into account. The delays stem from multiple background tasks that run simultaneously and cannot be turned off

--- test/plugin_functional/test_suites/core_plugins/application_leave_confirm.ts
+++ test/plugin_functional/test_suites/core_plugins/application_leave_confirm.ts
@@ -6,8 +6,8 @@
  * Side Public License, v 1.
  */
 
-import url from 'url';
 import expect from '@kbn/expect';
+import url from 'url';
 import { PluginFunctionalProviderContext } from '../../services';
 
 const getKibanaUrl = (pathname?: string, search?: string) =>
@@ -25,45 +25,81 @@ export default function ({ getService, getPageObjects }: PluginFunctionalProvide
   const appsMenu = getService('appsMenu');
   const log = getService('log');
   const retry = getService('retry');
+  const testSubjects = getService('testSubjects');
+  const config = getService('config');
 
-  const waitForUrlToBe = (pathname?: string, search?: string) => {
+  const waitForUrlToBe = async (pathname?: string, search?: string) => {
     const expectedUrl = getKibanaUrl(pathname, search);
-    return retry.waitFor(`Url to be ${expectedUrl}`, async () => {
+    return await retry.waitFor(`Url to be ${expectedUrl}`, async () => {
       const currentUrl = await browser.getCurrentUrl();
       log.debug(`waiting for currentUrl ${currentUrl} to be expectedUrl ${expectedUrl}`);
       return currentUrl === expectedUrl;
     });
   };
 
+  const ensureModalOpen = async (
+    defaultTryTimeout: number,
+    attempts: number,
+    timeMultiplier: number,
+    action: 'cancel' | 'confirm'
+  ): Promise<void> => {
+    let isConfirmCancelModalOpenState = false;
+    await retry.tryForTime(defaultTryTimeout * timeMultiplier, async () => {
+      isConfirmCancelModalOpenState = await testSubjects.exists('confirmModalCancelButton');
+    });
+    if (isConfirmCancelModalOpenState) {
+      log.debug(`defaultTryTimeout * ${timeMultiplier} is long enough`);
+      return action === 'cancel'
+        ? await PageObjects.common.clickCancelOnModal(true, false)
+        : await PageObjects.common.clickConfirmOnModal();
+    } else {
+      log.debug(`defaultTryTimeout * ${timeMultiplier} is not long enough`);
+      return await ensureModalOpen(
+        defaultTryTimeout,
+        (attempts = attempts > 0 ? attempts - 1 : 0),
+        (timeMultiplier = timeMultiplier < 10 ? timeMultiplier + 1 : 10),
+        action
+      );
+    }
+  };
+
   describe('application using leave confirmation', () => {
+    const defaultTryTimeout = config.get('timeouts.try');
+    const attempts = 5;
     describe('when navigating to another app', () => {
-      it('allows navigation if user click confirm on the confirmation dialog', async () => {
-        await PageObjects.common.navigateToApp('appleave1');
-        await waitForUrlToBe('/app/appleave1');
-
-        await appsMenu.clickLink('AppLeave 2');
-
-        await PageObjects.common.expectConfirmModalOpenState(true);
-        await PageObjects.common.clickConfirmOnModal();
-
-        const currentUrl = await browser.getCurrentUrl();
-        const kibanaUrl = getKibanaUrl('/app/appleave2');
-        log.debug(`currentUrl ${currentUrl} kibanaUrl ${kibanaUrl}`);
-        expect(currentUrl).to.eql(kibanaUrl);
-      });
+      const timeMultiplier = 2;
       it('prevents navigation if user click cancel on the confirmation dialog', async () => {
         await PageObjects.common.navigateToApp('appleave1');
         await waitForUrlToBe('/app/appleave1');
 
         await appsMenu.clickLink('AppLeave 2');
-
-        await PageObjects.common.expectConfirmModalOpenState(true);
-        await PageObjects.common.clickCancelOnModal(false);
-
+        await ensureModalOpen(defaultTryTimeout, attempts, timeMultiplier, 'cancel');
+        await PageObjects.header.waitUntilLoadingHasFinished();
+        await retry.waitFor('navigate to appleave1', async () => {
+          const currentUrl = await browser.getCurrentUrl();
+          log.debug(`currentUrl ${currentUrl}`);
+          return currentUrl.includes('appleave1');
+        });
         const currentUrl = await browser.getCurrentUrl();
-        const kibanaUrl = getKibanaUrl('/app/appleave1');
-        log.debug(`currentUrl ${currentUrl} kibanaUrl ${kibanaUrl}`);
-        expect(currentUrl).to.eql(kibanaUrl);
+        expect(currentUrl).to.contain('appleave1');
+        await PageObjects.common.navigateToApp('home');
+      });
+
+      it('allows navigation if user click confirm on the confirmation dialog', async () => {
+        await PageObjects.common.navigateToApp('appleave1');
+        await waitForUrlToBe('/app/appleave1');
+
+        await appsMenu.clickLink('AppLeave 2');
+        await ensureModalOpen(defaultTryTimeout, attempts, timeMultiplier, 'confirm');
+        await PageObjects.header.waitUntilLoadingHasFinished();
+        await retry.waitFor('navigate to appleave1', async () => {
+          const currentUrl = await browser.getCurrentUrl();
+          log.debug(`currentUrl ${currentUrl}`);
+          return currentUrl.includes('appleave2');
+        });
+        const currentUrl = await browser.getCurrentUrl();
+        expect(currentUrl).to.contain('appleave2');
+        await PageObjects.common.navigateToApp('home');
       });
     });
   });
