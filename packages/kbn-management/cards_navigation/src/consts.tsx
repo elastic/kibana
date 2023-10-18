6a63dd65ce3ad65d2f94230fdb624c74c76526c2
Eyo Okon Eyo <eyo.eyo@elastic.co>
2023-10-18 12:04:13 +0200

    rename enum value

--- packages/kbn-management/cards_navigation/src/consts.tsx
+++ packages/kbn-management/cards_navigation/src/consts.tsx
@@ -10,12 +10,12 @@ import React from 'react';
 import { i18n } from '@kbn/i18n';
 import { EuiIcon } from '@elastic/eui';
 
-import { appIds, AppId, AppDefinition, appCategories } from './types';
+import { AppIds, AppId, AppDefinition, appCategories } from './types';
 
-export { appIds, appCategories } from './types';
+export { AppIds, appCategories } from './types';
 
 export const appDefinitions: Record<AppId, AppDefinition> = {
-  [appIds.INDEX_MANAGEMENT]: {
+  [AppIds.INDEX_MANAGEMENT]: {
     category: appCategories.DATA,
     description: i18n.translate(
       'management.landing.withCardNavigation.indexmanagementDescription',
@@ -26,14 +26,14 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     ),
     icon: <EuiIcon size="l" type="indexSettings" />,
   },
-  [appIds.TRANSFORM]: {
+  [AppIds.TRANSFORM]: {
     category: appCategories.DATA,
     description: i18n.translate('management.landing.withCardNavigation.transformDescription', {
       defaultMessage: 'Pivot your data or copy the latest documents into an entity-centric index.',
     }),
     icon: <EuiIcon size="l" type="indexFlush" />,
   },
-  [appIds.INGEST_PIPELINES]: {
+  [AppIds.INGEST_PIPELINES]: {
     category: appCategories.DATA,
     description: i18n.translate(
       'management.landing.withCardNavigation.ingestPipelinesDescription',
@@ -43,14 +43,14 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     ),
     icon: <EuiIcon size="l" type="logstashInput" />,
   },
-  [appIds.DATA_VIEWS]: {
+  [AppIds.DATA_VIEWS]: {
     category: appCategories.DATA,
     description: i18n.translate('management.landing.withCardNavigation.dataViewsDescription', {
       defaultMessage: 'Create and manage the Elasticsearch data you selected for exploration.',
     }),
     icon: <EuiIcon size="l" type="indexEdit" />,
   },
-  [appIds.ML]: {
+  [AppIds.ML]: {
     category: appCategories.DATA,
     description: i18n.translate('management.landing.withCardNavigation.mlDescription', {
       defaultMessage:
@@ -58,7 +58,7 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     }),
     icon: <EuiIcon size="l" type="indexMapping" />,
   },
-  [appIds.PIPELINES]: {
+  [AppIds.PIPELINES]: {
     category: appCategories.DATA,
     description: i18n.translate('management.landing.withCardNavigation.ingestDescription', {
       defaultMessage:
@@ -67,21 +67,21 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     icon: <EuiIcon size="l" type="logstashQueue" />,
   },
 
-  [appIds.RULES]: {
+  [AppIds.RULES]: {
     category: appCategories.ALERTS,
     description: i18n.translate('management.landing.withCardNavigation.rulesDescription', {
       defaultMessage: 'Define when to generate alerts and notifications.',
     }),
     icon: <EuiIcon size="l" type="editorChecklist" />,
   },
-  [appIds.CONNECTORS]: {
+  [AppIds.CONNECTORS]: {
     category: appCategories.ALERTS,
     description: i18n.translate('management.landing.withCardNavigation.connectorsDescription', {
       defaultMessage: 'Configure connections to third party systems for use in cases and rules.',
     }),
     icon: <EuiIcon size="l" type="desktop" />,
   },
-  [appIds.MAINTENANCE_WINDOWS]: {
+  [AppIds.MAINTENANCE_WINDOWS]: {
     category: appCategories.ALERTS,
     description: i18n.translate(
       'management.landing.withCardNavigation.maintenanceWindowsDescription',
@@ -93,28 +93,28 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     icon: <EuiIcon size="l" type="wrench" />,
   },
 
-  [appIds.SAVED_OBJECTS]: {
+  [AppIds.SAVED_OBJECTS]: {
     category: appCategories.CONTENT,
     description: i18n.translate('management.landing.withCardNavigation.objectsDescription', {
       defaultMessage: 'Manage your saved dashboards, maps, data views, and Canvas workpads.',
     }),
     icon: <EuiIcon size="l" type="save" />,
   },
-  [appIds.FILES_MANAGEMENT]: {
+  [AppIds.FILES_MANAGEMENT]: {
     category: appCategories.CONTENT,
     description: i18n.translate('management.landing.withCardNavigation.fileManagementDescription', {
       defaultMessage: 'Access all files that you uploaded.',
     }),
     icon: <EuiIcon size="l" type="documents" />,
   },
-  [appIds.REPORTING]: {
+  [AppIds.REPORTING]: {
     category: appCategories.CONTENT,
     description: i18n.translate('management.landing.withCardNavigation.reportingDescription', {
       defaultMessage: 'Manage generated PDF, PNG and CSV reports.',
     }),
     icon: <EuiIcon size="l" type="visPie" />,
   },
-  [appIds.TAGS]: {
+  [AppIds.TAGS]: {
     category: appCategories.CONTENT,
     description: i18n.translate('management.landing.withCardNavigation.tagsDescription', {
       defaultMessage: 'Organize, search, and filter your saved objects by specific criteria.',
@@ -122,7 +122,7 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     icon: <EuiIcon size="l" type="tag" />,
   },
 
-  [appIds.API_KEYS]: {
+  [AppIds.API_KEYS]: {
     category: appCategories.OTHER,
     description: i18n.translate('management.landing.withCardNavigation.apiKeysDescription', {
       defaultMessage: 'Allow programmatic access to your project data and capabilities.',
@@ -130,19 +130,11 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
     icon: <EuiIcon size="l" type="lockOpen" />,
   },
 
-  [appIds.SERVERLESS_SETTINGS]: {
+  [AppIds.SERVERLESS_SETTINGS]: {
     category: appCategories.OTHER,
     description: i18n.translate('management.landing.withCardNavigation.settingsDescription', {
       defaultMessage: 'Control project behavior, such as date display and default sorting.',
     }),
     icon: <EuiIcon size="l" type="gear" />,
   },
-};
-
-// Compose a list of app ids that belong to a given category
-export const getAppIdsByCategory = (category: string) => {
-  const appKeys = Object.keys(appDefinitions) as AppId[];
-  return appKeys.filter((appId: AppId) => {
-    return appDefinitions[appId].category === category;
-  });
 };
