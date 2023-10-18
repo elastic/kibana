347aa54e4c5095cec139aac8c08dd4a78c249dbf
Eyo Okon Eyo <eyo.eyo@elastic.co>
2023-10-18 15:22:00 +0200

    fix nomenclature, also ensure correct path is passed to card nav

--- src/plugins/management/public/components/landing/landing.tsx
+++ src/plugins/management/public/components/landing/landing.tsx
@@ -38,7 +38,7 @@ export const ManagementLandingPage = ({
           sections={sections}
           appBasePath={appBasePath}
           hideLinksTo={cardsNavigationConfig?.hideLinksTo}
-          extendCardNavigationDefinitions={cardsNavigationConfig?.extendCardNavDefinitons}
+          cardNavExtensionDefinitions={cardsNavigationConfig?.extendCardNavDefinitons}
         />
       </EuiPageBody>
     );
