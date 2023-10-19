bc68067968e10930a45e89a7b423206e227d623c
Jean-Louis Leysens <jloleysens@gmail.com>
2023-10-19 13:19:31 +0200

    added cdn test file

---
+++ packages/core/http/core-http-server-internal/src/cdn.test.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { CdnConfig } from './cdn';
+
+describe('CdnConfig', () => {
+  it.each([
+    ['https://cdn.elastic.co', 'cdn.elastic.co'],
+    ['https://foo.bar', 'foo.bar'],
+    ['http://foo.bar', 'foo.bar'],
+    ['https://cdn.elastic.co:9999', 'cdn.elastic.co:9999'],
+    ['https://cdn.elastic.co:9999/with-a-path', 'cdn.elastic.co:9999'],
+  ])('host as expected for %p', (url, expected) => {
+    expect(CdnConfig.from({ url }).host).toEqual(expected);
+  });
+
+  it.each([
+    ['https://cdn.elastic.co', 'https://cdn.elastic.co'],
+    ['https://foo.bar', 'https://foo.bar'],
+    ['http://foo.bar', 'http://foo.bar'],
+    ['https://cdn.elastic.co:9999', 'https://cdn.elastic.co:9999'],
+    ['https://cdn.elastic.co:9999/with-a-path', 'https://cdn.elastic.co:9999/with-a-path'],
+  ])('base HREF as expected for %p', (url, expected) => {
+    expect(CdnConfig.from({ url }).baseHref).toEqual(expected);
+  });
+
+  it.each([['foo'], ['@#']])('throws for invalid URLs (%p)', (url) => {
+    expect(() => CdnConfig.from({ url })).toThrow(/Invalid URL/);
+  });
+
+  it('handles empty urls', () => {
+    expect(CdnConfig.from({ url: '' }).baseHref).toBeUndefined();
+    expect(CdnConfig.from({ url: '' }).host).toBeUndefined();
+  });
+});
