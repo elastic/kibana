e1166e3ed83471b815e8caf9017ea5c84e7c70aa
Kevin Delemme <kevin.delemme@elastic.co>
2023-10-19 15:53:06 -0400

    Add instanceId to slo details locator

--- x-pack/plugins/observability/public/locators/slo_details.ts
+++ x-pack/plugins/observability/public/locators/slo_details.ts
@@ -7,26 +7,26 @@
 
 import type { SerializableRecord } from '@kbn/utility-types';
 import type { LocatorDefinition } from '@kbn/share-plugin/public';
+import { ALL_VALUE } from '@kbn/slo-schema';
 import { sloDetailsLocatorID } from '../../common';
 import { SLOS_PATH } from '../../common/locators/paths';
 
-export interface SloDetailsParams {
+interface SloDetailsLocatorParams extends SerializableRecord {
   sloId?: string;
+  instanceId?: string;
 }
-
-export interface SloDetailsLocatorParams extends SloDetailsParams, SerializableRecord {}
-
-export const getSloDetailsPath = (sloId: string) => {
-  return `${SLOS_PATH}/${encodeURI(sloId)}`;
-};
 
 export class SloDetailsLocatorDefinition implements LocatorDefinition<SloDetailsLocatorParams> {
   public readonly id = sloDetailsLocatorID;
 
-  public readonly getLocation = async ({ sloId = '' }: SloDetailsLocatorParams) => {
+  public readonly getLocation = async ({ sloId, instanceId }: SloDetailsLocatorParams) => {
+    const queryParams =
+      !!instanceId && instanceId !== ALL_VALUE ? `?instanceId=${encodeURI(instanceId)}` : '';
+    const path = !!sloId ? `${SLOS_PATH}/${encodeURI(sloId)}${queryParams}` : SLOS_PATH;
+
     return {
       app: 'observability',
-      path: getSloDetailsPath(sloId),
+      path,
       state: {},
     };
   };
