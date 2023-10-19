59f7c94c3bf2f61609cd728cb44d09cd63964c2c
Chris Cowan <chris@elastic.co>
2023-10-18 18:12:23 -0600

    Removing extra line

--- x-pack/plugins/observability/server/lib/rules/slo_burn_rate/executor.ts
+++ x-pack/plugins/observability/server/lib/rules/slo_burn_rate/executor.ts
@@ -124,7 +124,6 @@ export const getRuleExecutor = ({
           const alertId = instanceId;
           const alert = alertWithLifecycle({
             id: alertId,
-
             fields: {
               [ALERT_REASON]: reason,
               [ALERT_EVALUATION_THRESHOLD]: windowDef.burnRateThreshold,
