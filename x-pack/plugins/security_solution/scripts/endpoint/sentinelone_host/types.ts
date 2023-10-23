7a5aa714d4cf80df25b3a53dde0cc28c1e119a35
Paul Tavares <paul.tavares@elastic.co>
2023-10-23 13:07:11 -0400

    clean up commented out data

--- x-pack/plugins/security_solution/scripts/endpoint/sentinelone_host/types.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/sentinelone_host/types.ts
@@ -7,44 +7,6 @@
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-// {
-//   data: {
-//     allSites: { activeLicenses: 815, totalLicenses: 25 },
-//     sites: [
-//       {
-//         accountId: '1392053568574369781',
-//         accountName: 'Elastic',
-//         activeLicenses: 10,
-//         createdAt: '2022-04-05T16:01:56.927213Z',
-//         creator: 'Sandeep Minhas',
-//         creatorId: '1336326529793417045',
-//         description: null,
-//         expiration: null,
-//         externalId: '1b8a963e-0a0f-2861-09cb-42d27bd27d72',
-//         healthStatus: true,
-//         id: '1392053568582758390',
-//         isDefault: true,
-//         licenses: {
-//           bundles: [ [Object] ],
-//           modules: [ [Object] ],
-//           settings: [ [Object], [Object], [Object], [Object] ]
-//         },
-//         name: 'Default site',
-//         registrationToken: 'eyJ1cmwiOiAiaHR0cHM6Ly91c2VhMS1wYXJ0bmVycy5zZW50aW5lbG9uZS5uZXQiLCAic2l0ZV9rZXkiOiAiOTJlYzQyMGJjNDhjNjM1OCJ9',
-//         siteType: 'Trial',
-//         sku: 'Complete',
-//         state: 'active',
-//         suite: 'Complete',
-//         totalLicenses: 25,
-//         unlimitedExpiration: true,
-//         unlimitedLicenses: false,
-//         updatedAt: '2023-08-27T08:04:39.306240Z'
-//       }
-//     ]
-//   },
-//   pagination: { nextCursor: null, totalItems: 1 }
-// }
-
 export interface S1ListResponse<D = any> {
   data: D;
   pagination: {
