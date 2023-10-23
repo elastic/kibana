56527617d9ca3cd8021930eac8f24266c4eee230
Janki Salvi <117571355+js-jankisalvi@users.noreply.github.com>
2023-10-23 18:28:37 +0200

    update toast success message

--- x-pack/test_serverless/functional/test_suites/observability/cases/configure.ts
+++ x-pack/test_serverless/functional/test_suites/observability/cases/configure.ts
@@ -54,7 +54,7 @@ export default ({ getPageObject, getService }: FtrProviderContext) => {
       it('change closure option successfully', async () => {
         await cases.common.selectRadioGroupValue('closure-options-radio-group', 'close-by-pushing');
         const toast = await toasts.getToastElement(1);
-        expect(await toast.getVisibleText()).to.be('Saved external connection settings');
+        expect(await toast.getVisibleText()).to.be('Settings successfully updated');
         await toasts.dismissAllToasts();
       });
     });
