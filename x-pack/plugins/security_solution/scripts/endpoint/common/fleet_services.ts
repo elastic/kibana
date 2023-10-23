e68126cb0bc5df5a9926943f893e49d628caa488
konrad.szwarc <konrad.szwarc@elastic.co>
2023-10-23 12:57:27 +0200

    align form as well

--- x-pack/plugins/security_solution/scripts/endpoint/common/fleet_services.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/common/fleet_services.ts
@@ -44,6 +44,8 @@ import type {
 import nodeFetch from 'node-fetch';
 import semver from 'semver';
 import axios from 'axios';
+import execa from 'execa';
+import fs from 'fs';
 import {
   RETRYABLE_TRANSIENT_ERRORS,
   retryOnError,
@@ -51,6 +53,7 @@ import {
 import { fetchKibanaStatus } from './stack_services';
 import { catchAxiosErrorFormatAndThrow } from './format_axios_error';
 import { FleetAgentGenerator } from '../../../common/endpoint/data_generators/fleet_agent_generator';
+import { VAGRANT_CWD } from './endpoint_host_services';
 
 const fleetGenerator = new FleetAgentGenerator();
 
@@ -169,6 +172,40 @@ export const waitForHostToEnroll = async (
       // sleep and check again
       await new Promise((r) => setTimeout(r, 2000));
     }
+  }
+
+  try {
+    let stdout: string;
+    if (process.env.CI) {
+      const { stdout: vagrantSTDOUT } = await execa(
+        'vagrant',
+        ['ssh', '--', "sudo sh -c 'cat /opt/Elastic/Agent/elastic-agent-*'"],
+        {
+          env: {
+            VAGRANT_CWD,
+          },
+        }
+      );
+      stdout = vagrantSTDOUT;
+    } else {
+      stdout = '';
+    }
+    await execa('mkdir', [
+      '-p',
+      '../../../../../../target/kibana-security-solution/cypress/results/',
+    ]).catch((e) => {
+      logger?.info(e);
+    });
+
+    const abc = stdout.toString();
+
+    fs.writeFileSync(
+      '../../../../../../target/kibana-security-solution/cypress/results/agent-logs.txt',
+      abc
+    );
+    logger?.info(stdout);
+  } catch (e) {
+    logger?.info(e);
   }
 
   if (!found) {
