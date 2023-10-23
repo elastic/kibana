b95a0728f9df00c575af8475087447aa931309b1
Elena Stoeva <elenastoeva99@gmail.com>
2023-10-23 12:31:39 +0100

    [CCR] Fix a11y issue in form

--- x-pack/plugins/cross_cluster_replication/public/app/components/remote_clusters_form_field.js
+++ x-pack/plugins/cross_cluster_replication/public/app/components/remote_clusters_form_field.js
@@ -143,6 +143,12 @@ export class RemoteClustersFormField extends PureComponent {
           hasNoInitialSelection={!hasClusters}
           isInvalid={areErrorsVisible && Boolean(errorMessage)}
           data-test-subj="remoteClusterSelect"
+          aria-label={i18n.translate(
+            'xpack.crossClusterReplication.remoteClustersFormField.remoteClusterSelectAriaLabel',
+            {
+              defaultMessage: 'Remote cluster',
+            }
+          )}
         />
         {areErrorsVisible && Boolean(errorMessage) ? this.renderValidRemoteClusterRequired() : null}
         {errorMessage}
