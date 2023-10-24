206e6e57fd54e08b3cd7b0177bbbe1e66d5ce238
shahzad31 <shahzad31comp@gmail.com>
2023-10-24 12:05:46 +0200

    update jest test

--- x-pack/plugins/synthetics/server/routes/settings/private_locations/helpers.test.ts
+++ x-pack/plugins/synthetics/server/routes/settings/private_locations/helpers.test.ts
@@ -56,65 +56,61 @@ const testLocations2 = {
 describe('toClientContract', () => {
   it('formats SO attributes to client contract with falsy geo location', () => {
     // @ts-ignore fixtures are purposely wrong types for testing
-    expect(toClientContract(testLocations)).toEqual({
-      locations: [
-        {
-          agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728deb',
-          geo: {
-            lat: 0,
-            lon: 0,
-          },
-          id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
-          isInvalid: true,
-          isServiceManaged: false,
-          label: 'BEEP',
-          tags: ['a tag 2'],
+    expect(toClientContract(testLocations)).toEqual([
+      {
+        agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728deb',
+        geo: {
+          lat: 0,
+          lon: 0,
         },
-        {
-          agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728dec',
-          geo: {
-            lat: '',
-            lon: '',
-          },
-          id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
-          isInvalid: true,
-          isServiceManaged: false,
-          label: 'BEEP',
-          tags: ['a tag'],
+        id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
+        isInvalid: true,
+        isServiceManaged: false,
+        label: 'BEEP',
+        tags: ['a tag 2'],
+      },
+      {
+        agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728dec',
+        geo: {
+          lat: '',
+          lon: '',
         },
-      ],
-    });
+        id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
+        isInvalid: true,
+        isServiceManaged: false,
+        label: 'BEEP',
+        tags: ['a tag'],
+      },
+    ]);
   });
 
   it('formats SO attributes to client contract with truthy geo location', () => {
     // @ts-ignore fixtures are purposely wrong types for testing
-    expect(toClientContract(testLocations2)).toEqual({
-      locations: [
-        {
-          agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728deb',
-          geo: {
-            lat: -10,
-            lon: 20,
-          },
-          id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
-          isInvalid: true,
-          isServiceManaged: false,
-          label: 'BEEP',
-          tags: ['a tag 2'],
+    expect(toClientContract(testLocations2)).toEqual([
+      {
+        agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728deb',
+        geo: {
+          lat: -10,
+          lon: 20,
         },
-        {
-          agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728dec',
-          geo: {
-            lat: -10,
-            lon: 20,
-          },
-          id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
-          isInvalid: true,
-          isServiceManaged: false,
-          label: 'BEEP',
-          tags: ['a tag'],
+        id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
+        isInvalid: true,
+        isServiceManaged: false,
+        label: 'BEEP',
+        tags: ['a tag 2'],
+      },
+      {
+        agentPolicyId: 'e3134290-0f73-11ee-ba15-159f4f728dec',
+        geo: {
+          lat: -10,
+          lon: 20,
         },
-      ],
-    });
+        id: 'e3134290-0f73-11ee-ba15-159f4f728dec',
+        isInvalid: true,
+        isServiceManaged: false,
+        label: 'BEEP',
+        tags: ['a tag'],
+      },
+    ]);
   });
 });
