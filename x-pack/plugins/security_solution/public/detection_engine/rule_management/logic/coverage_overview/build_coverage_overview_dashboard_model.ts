e049eaa6df12bffb759b92f740805093a2121956
Davis Plumlee <davis.plumlee@elastic.co>
2023-10-24 15:01:07 -0400

    fixes count and duplication bugs

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/logic/coverage_overview/build_coverage_overview_dashboard_model.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/logic/coverage_overview/build_coverage_overview_dashboard_model.ts
@@ -40,12 +40,20 @@ export async function buildCoverageOverviewDashboardModel(
 
     for (const technique of tactic.techniques) {
       for (const ruleId of apiResponse.coverage[technique.id] ?? []) {
-        addRule(technique, ruleId, apiResponse.rules_data[ruleId]);
+        if (apiResponse.coverage[tactic.id] && apiResponse.coverage[tactic.id].includes(ruleId)) {
+          addRule(technique, ruleId, apiResponse.rules_data[ruleId]);
+        }
       }
 
       for (const subtechnique of technique.subtechniques) {
         for (const ruleId of apiResponse.coverage[subtechnique.id] ?? []) {
-          addRule(subtechnique, ruleId, apiResponse.rules_data[ruleId]);
+          if (
+            apiResponse.coverage[tactic.id] &&
+            apiResponse.coverage[technique.id] &&
+            apiResponse.coverage[tactic.id].includes(ruleId)
+          ) {
+            addRule(subtechnique, ruleId, apiResponse.rules_data[ruleId]);
+          }
         }
       }
     }
