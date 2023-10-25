ff13951b58b2e6d2e37833927d6e4a7be24082f3
jpdjere <jpdjeredjian@gmail.com>
2023-10-25 14:14:31 +0200

    Control again

--- x-pack/test/detection_engine_api_integration/security_and_spaces/bundled_prebuilt_rules_package/prerelease_packages.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/bundled_prebuilt_rules_package/prerelease_packages.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 import expect from 'expect';
-import { ALL_SAVED_OBJECT_INDICES } from '@kbn/core-saved-objects-server';
+// import { ALL_SAVED_OBJECT_INDICES } from '@kbn/core-saved-objects-server';
 import { FtrProviderContext } from '../../common/ftr_provider_context';
 import { deleteAllPrebuiltRuleAssets, deleteAllRules } from '../../utils';
 import { getInstalledRules } from '../../utils/prebuilt_rules/get_installed_rules';
@@ -41,7 +41,7 @@ export default ({ getService }: FtrProviderContext): void => {
       await installPrebuiltRules(es, supertest);
 
       // Refresh ES indices to avoid race conditions between write and reading of indeces
-      await es.indices.refresh({ index: ALL_SAVED_OBJECT_INDICES });
+      // await es.indices.refresh({ index: ALL_SAVED_OBJECT_INDICES });
 
       // Verify that status is updated after package installation
       const statusAfterPackageInstallation = await getPrebuiltRulesStatus(supertest);
