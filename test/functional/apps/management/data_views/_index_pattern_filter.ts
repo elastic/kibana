4476090c9a1732439fd9a4ff469ca491c6d1c758
Julia Rechkunova <julia.rechkunova@elastic.co>
2023-10-25 12:36:21 +0200

    [Discover] Add functional tests

--- test/functional/apps/management/data_views/_index_pattern_filter.ts
+++ test/functional/apps/management/data_views/_index_pattern_filter.ts
@@ -12,7 +12,9 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const kibanaServer = getService('kibanaServer');
   const retry = getService('retry');
+  const testSubjects = getService('testSubjects');
   const PageObjects = getPageObjects(['settings']);
+  const es = getService('es');
 
   describe('index pattern filter', function describeIndexTests() {
     before(async function () {
@@ -57,6 +59,60 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         }
       });
       await PageObjects.settings.clearFieldTypeFilter('long');
+    });
+
+    it('should set "conflict" filter when "View conflicts" button is pressed', async function () {
+      const additionalIndexWithWrongMapping = 'logstash-wrong';
+      await PageObjects.settings.navigateTo();
+      await PageObjects.settings.clickKibanaIndexPatterns();
+
+      if (await es.indices.exists({ index: additionalIndexWithWrongMapping })) {
+        await es.indices.delete({ index: additionalIndexWithWrongMapping });
+      }
+
+      await es.indices.create({
+        index: additionalIndexWithWrongMapping,
+        body: {
+          mappings: {
+            properties: {
+              bytes: {
+                type: 'keyword',
+              },
+            },
+          },
+        },
+      });
+
+      await es.index({
+        index: additionalIndexWithWrongMapping,
+        body: {
+          bytes: 'wrong_value',
+        },
+        refresh: 'wait_for',
+      });
+
+      await PageObjects.settings.clickIndexPatternLogstash();
+
+      await testSubjects.existOrFail('dataViewMappingConflict');
+
+      expect(await PageObjects.settings.getFieldTypes()).to.eql([
+        'text',
+        'keyword',
+        'text',
+        'keyword',
+        'date',
+        '_id',
+        '_index',
+        '',
+        '_source',
+        'text',
+      ]);
+
+      await testSubjects.click('viewDataViewMappingConflictsButton');
+
+      expect(await PageObjects.settings.getFieldTypes()).to.eql(['keyword, long\nConflict']);
+
+      await es.indices.delete({ index: additionalIndexWithWrongMapping });
     });
   });
 }
