a0cf2b6e78e4d2e1cea883565d7d2c16830d37a8
Tiago Costa <tiago.costa@elastic.co>
2023-10-25 18:51:51 +0100

    skip flaky suite (#169459)

--- test/functional/apps/discover/group4/_chart_hidden.ts
+++ test/functional/apps/discover/group4/_chart_hidden.ts
@@ -21,6 +21,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   };
 
   // FLAKY: https://github.com/elastic/kibana/issues/169458
+  // FLAKY: https://github.com/elastic/kibana/issues/169459
   describe.skip('discover show/hide chart test', function () {
     before(async function () {
       await security.testUser.setRoles(['kibana_admin', 'test_logstash_reader']);
