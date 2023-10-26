4ece0f116416b51f68538b73997057ae6b6c2ada
Dario Gieselaar <dario.gieselaar@elastic.co>
2023-10-26 14:27:40 +0200

    [Obs AI Assistant] Fixes issue w/ duplicated recalls

--- x-pack/plugins/apm/server/routes/assistant_functions/get_apm_timeseries/fetch_timeseries.ts
+++ x-pack/plugins/apm/server/routes/assistant_functions/get_apm_timeseries/fetch_timeseries.ts
@@ -122,13 +122,19 @@ export async function fetchSeries<T extends ValueAggregationMap>({
   }
 
   return response.aggregations.groupBy.buckets.map((bucket) => {
+    let value =
+      bucket.value?.value === undefined || bucket.value?.value === null
+        ? null
+        : Number(bucket.value.value);
+
+    if (value !== null && !(value < 100)) {
+      value = Number(value.toPrecision(3));
+    }
+
     return {
       groupBy: bucket.key_as_string || String(bucket.key),
       data: bucket.timeseries.buckets,
-      value:
-        bucket.value?.value === undefined || bucket.value?.value === null
-          ? null
-          : Math.round(bucket.value.value),
+      value,
       change_point: bucket.change_point,
       unit,
     };
