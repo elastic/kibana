cbebeb1277f9cc2968bd9da4cc75d24e892380f7
Yara Tercero <yctercero@users.noreply.github.com>
2023-10-26 06:58:35 -0700

    [Security Solution][DE] Migrate investigation_fields (#169061)
    ## Summary
    
    **TLDR:** SO will support both `string[]` and `{ field_names: string[]
    }`, but detection engine APIs will only support the object format in
    8.11+.
    
    (cherry picked from commit bb3673f2eb24013b11c736986928c3b73370f6bf)
    
    # Conflicts:
    #	x-pack/test/detection_engine_api_integration/security_and_spaces/group1/create_rule_exceptions.ts
    #	x-pack/test/security_solution_api_integration/test_suites/detections_response/utils/index.ts
    #	x-pack/test/security_solution_api_integration/tsconfig.json

---
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/utils/index.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+export * from './rules';
+export * from './exception_list_and_item';
+export * from './alerts';
+export * from './actions';
+export * from './get_rule_so_by_id';
+export * from './create_rule_saved_object';
+export * from './get_rule_with_legacy_investigation_fields';
+
+export * from './count_down_test';
+export * from './count_down_es';
+export * from './update_username';
+export * from './refresh_index';
