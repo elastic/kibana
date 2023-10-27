594d8c787d6ae1317545d2b0fedc21d1b4e474f4
Jenny <dzheni.pavlova@elastic.co>
2023-10-27 16:06:36 +0200

    Update types and rename

--- x-pack/plugins/infra/public/components/asset_details/tabs/metadata/utils.ts
+++ x-pack/plugins/infra/public/components/asset_details/tabs/metadata/utils.ts
@@ -5,59 +5,62 @@
  * 2.0.
  */
 
-import type {
-  InfraMetadata,
-  InfraMetadataHost,
-  InfraMetadataAgent,
-  InfraMetadataCloud,
-} from '../../../../../common/http_api';
+import type { InfraMetadata } from '../../../../../common/http_api';
 
 export interface Field {
   name: string;
-  value: string | string[] | undefined;
+  value?: string | boolean | string[] | undefined;
+}
+
+export interface FieldsByCategoryValue {
+  value?: string | boolean | string[] | undefined | { [key: string]: string };
+}
+export interface FieldsByCategory {
+  [key: string]: FieldsByCategoryValue['value'];
 }
 
 export const getAllFields = (metadata: InfraMetadata | null) => {
   if (!metadata?.info) return [];
-  const tempPropertyMap = new Map();
+  const tempPropertyMap = new Map<string, FieldsByCategoryValue['value']>();
 
-  const mapNestedProperties = (
-    category: 'host' | 'agent' | 'cloud',
-    property: keyof InfraMetadataHost | keyof InfraMetadataCloud | keyof InfraMetadataHost
-  ) => {
-    tempPropertyMap.set(property, metadata?.info[`${category}`][property]);
+  const mapNestedProperties = (category: 'cloud' | 'host' | 'agent', property: string) => {
+    const fieldsByCategory: FieldsByCategory = metadata?.info?.[`${category}`] ?? {};
+    if (fieldsByCategory.hasOwnProperty(property)) {
+      tempPropertyMap.set(property, fieldsByCategory[property]);
 
-    if (
-      typeof tempPropertyMap.get(property) === 'string' ||
-      typeof tempPropertyMap.get(property) === 'boolean' ||
-      tempPropertyMap.get(property)?.length
-    ) {
-      return {
-        name: `${category}.${property}`,
-        value:
-          tempPropertyMap.get(property) === false
-            ? String(tempPropertyMap.get(property))
-            : tempPropertyMap.get(property),
-      };
-    } else {
-      const subProps = [];
-      for (const [prop, subProp] of Object.entries(tempPropertyMap.get(property))) {
-        subProps.push({
-          name: `${category}.${property}.${prop}`,
-          value: subProp,
-        });
+      if (
+        typeof tempPropertyMap.get(property) === 'string' ||
+        typeof tempPropertyMap.get(property) === 'boolean' ||
+        Array.isArray(tempPropertyMap.get(property))
+      ) {
+        return {
+          name: `${category}.${property}`,
+          value:
+            tempPropertyMap.get(property) === false
+              ? String(tempPropertyMap.get(property))
+              : tempPropertyMap.get(property),
+        };
+      } else {
+        const subProps = [];
+        for (const [prop, subProp] of Object.entries(tempPropertyMap.get(property) ?? {})) {
+          subProps.push({
+            name: `${category}.${property}.${prop}`,
+            value: subProp,
+          });
+        }
+        return subProps;
       }
-      return subProps;
     }
+    return [];
   };
 
-  const agent = Object.keys(metadata.info.agent ?? {}).flatMap((prop: keyof InfraMetadataAgent) =>
+  const agent = Object.keys(metadata.info.agent ?? {}).flatMap((prop) =>
     mapNestedProperties('agent', prop)
   );
-  const cloud = Object.keys(metadata.info.cloud ?? {}).flatMap((prop: keyof InfraMetadataCloud) =>
+  const cloud = Object.keys(metadata.info.cloud ?? {}).flatMap((prop) =>
     mapNestedProperties('cloud', prop)
   );
-  const host = Object.keys(metadata.info.host ?? {}).flatMap((prop: keyof InfraMetadataHost) =>
+  const host = Object.keys(metadata?.info?.host ?? {}).flatMap((prop) =>
     mapNestedProperties('host', prop)
   );
 
