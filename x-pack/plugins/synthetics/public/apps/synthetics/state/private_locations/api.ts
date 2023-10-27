d35933bb6205a7cfb03746df1a6f0ec7886915be
shahzad31 <shahzad31comp@gmail.com>
2023-10-27 17:11:34 +0200

    version

--- x-pack/plugins/synthetics/public/apps/synthetics/state/private_locations/api.ts
+++ x-pack/plugins/synthetics/public/apps/synthetics/state/private_locations/api.ts
@@ -7,7 +7,7 @@
 
 import { NewLocation } from '../../components/settings/private_locations/add_location_flyout';
 import { AgentPolicyInfo } from '../../../../../common/types';
-import { SYNTHETICS_API_URLS } from '../../../../../common/constants';
+import { INITIAL_REST_VERSION, SYNTHETICS_API_URLS } from '../../../../../common/constants';
 import { SyntheticsPrivateLocations } from '../../../../../common/runtime_types';
 import { apiService } from '../../../../utils/api_service/api_service';
 
@@ -18,15 +18,21 @@ export const fetchAgentPolicies = async (): Promise<AgentPolicyInfo[]> => {
 export const addSyntheticsPrivateLocations = async (
   newLocation: NewLocation
 ): Promise<SyntheticsPrivateLocations> => {
-  return await apiService.post(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS, newLocation);
+  return await apiService.post(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS, newLocation, undefined, {
+    version: INITIAL_REST_VERSION,
+  });
 };
 
 export const getSyntheticsPrivateLocations = async (): Promise<SyntheticsPrivateLocations> => {
-  return await apiService.get(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS);
+  return await apiService.get(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS, {
+    version: INITIAL_REST_VERSION,
+  });
 };
 
 export const deleteSyntheticsPrivateLocations = async (
   locationId: string
 ): Promise<SyntheticsPrivateLocations> => {
-  return await apiService.delete(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS + `/${locationId}`);
+  return await apiService.delete(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS + `/${locationId}`, {
+    version: INITIAL_REST_VERSION,
+  });
 };
