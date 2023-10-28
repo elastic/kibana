01372b56c76fef97561af2e3c6ea8b1c17f4cb18
Brad White <brad.white@elastic.co>
2023-10-27 19:03:04 -0600

    Fix isLegacy conditional rendering

--- x-pack/plugins/index_management/public/application/sections/home/template_list/template_details/tabs/tab_summary.tsx
+++ x-pack/plugins/index_management/public/application/sections/home/template_list/template_details/tabs/tab_summary.tsx
@@ -195,7 +195,7 @@ export const TabSummary: React.FunctionComponent<Props> = ({ templateDetails }) 
             </EuiDescriptionListDescription>
 
             {/* Allow auto create */}
-            {isLegacy !== true && (
+            {isLegacy === false && (
               <>
                 <EuiDescriptionListTitle>
                   <FormattedMessage
