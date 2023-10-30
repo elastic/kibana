34da769e998459bda8f337939ee5edcbcdf063f4
Cee Chen <constance.chen@elastic.co>
2023-10-30 14:00:54 -0700

    [EuiCommentEvent] Remove now-unnecessary color override
    - should now be handled OOTB by EUI 7288

--- x-pack/plugins/security_solution/public/assistant/get_comments/index.tsx
+++ x-pack/plugins/security_solution/public/assistant/get_comments/index.tsx
@@ -14,13 +14,10 @@ import {
   EuiText,
   getDefaultEuiMarkdownParsingPlugins,
   getDefaultEuiMarkdownProcessingPlugins,
-  tint,
 } from '@elastic/eui';
 import React from 'react';
 
 import { AssistantAvatar } from '@kbn/elastic-assistant';
-import { css } from '@emotion/react';
-import { euiThemeVars } from '@kbn/ui-theme';
 import { CommentActions } from '../comment_actions';
 import * as i18n from './translations';
 import { customCodeBlockLanguagePlugin } from './custom_codeblock/custom_codeblock_markdown_plugin';
@@ -105,20 +102,7 @@ export const getComments = ({
         message.timestamp.length === 0 ? new Date().toLocaleString() : message.timestamp
       ),
       username: isUser ? i18n.YOU : i18n.ASSISTANT,
-      ...(message.isError
-        ? {
-            eventColor: 'danger',
-            css: css`
-              .euiCommentEvent {
-                border: 1px solid ${tint(euiThemeVars.euiColorDanger, 0.75)};
-              }
-              .euiCommentEvent__header {
-                padding: 0 !important;
-                border-block-end: 1px solid ${tint(euiThemeVars.euiColorDanger, 0.75)};
-              }
-            `,
-          }
-        : {}),
+      eventColor: message.isError ? 'danger' : undefined,
     };
   });
 };
