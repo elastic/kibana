cde26a59eeaa1da973e5d9de9753416080f0c21a
Gerard Soldevila <gerard.soldevila@elastic.co>
2023-10-30 14:42:31 +0100

    Update the rest of calls to `fillForm` (saved_object_tagging scope)

--- x-pack/test/saved_object_tagging/functional/tests/create.ts
+++ x-pack/test/saved_object_tagging/functional/tests/create.ts
@@ -67,7 +67,10 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
           description: 'The name will fails validation',
           color: '#FF00CC',
         },
-        { submit: true }
+        {
+          submit: true,
+          clearWithKeyboard: true,
+        }
       );
 
       expect(await tagModal.isOpened()).to.be(true);
@@ -86,7 +89,10 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
           description: 'The name will fails validation',
           color: '#FF00CC',
         },
-        { submit: true }
+        {
+          submit: true,
+          clearWithKeyboard: true,
+        }
       );
 
       expect(await tagModal.isOpened()).to.be(true);
@@ -96,7 +102,10 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
         {
           name: 'valid name',
         },
-        { submit: true }
+        {
+          submit: true,
+          clearWithKeyboard: true,
+        }
       );
 
       await tagModal.waitUntilClosed();
@@ -116,7 +125,10 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
           description: 'I will not add this tag',
           color: '#FF00CC',
         },
-        { submit: false }
+        {
+          submit: false,
+          clearWithKeyboard: true,
+        }
       );
       await tagModal.clickCancel();
       await tagModal.waitUntilClosed();
