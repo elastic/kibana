9486df0adea8c45263e1b37ebf4ff4d072118257
Sébastien Loix <sabee77@gmail.com>
2023-10-30 14:30:59 +0000

    Jest tests for auto-generated panels

--- packages/shared-ux/chrome/navigation/__jest__/panel.test.tsx
+++ packages/shared-ux/chrome/navigation/__jest__/panel.test.tsx
@@ -284,4 +284,263 @@ describe('Panel', () => {
       }
     });
   });
+
+  describe('auto generated content', () => {
+    test('should rendre block groups with title', async () => {
+      const runTests = async (
+        type: TestType,
+        { queryByTestId, queryAllByTestId }: RenderResult
+      ) => {
+        try {
+          queryByTestId(/panelOpener-root.group1/)?.click(); // open the panel
+
+          expect(queryByTestId(/panelGroupId-foo/)).toBeVisible();
+          expect(queryByTestId(/panelGroupTitleId-foo/)?.textContent).toBe('Foo');
+
+          const panelNavItems = queryAllByTestId(/panelNavItem/);
+          expect(panelNavItems.length).toBe(2); // "item2" has been filtered out as it is hidden
+          expect(panelNavItems.map(({ textContent }) => textContent?.trim())).toEqual([
+            'Item 1',
+            'Item 3',
+          ]);
+        } catch (e) {
+          errorHandler(type)(e);
+        }
+      };
+
+      // -- Default navigation
+      {
+        const navigationBody: Array<RootNavigationItemDefinition<any>> = [
+          {
+            type: 'navGroup',
+            id: 'root',
+            isCollapsible: false,
+            children: [
+              {
+                id: 'group1',
+                link: 'dashboards',
+                renderAs: 'panelOpener',
+                children: [
+                  {
+                    id: 'foo',
+                    title: 'Foo',
+                    children: [
+                      { id: 'item1', link: 'management', title: 'Item 1' },
+                      { id: 'item2', link: 'management', title: 'Item 2', sideNavStatus: 'hidden' },
+                      { id: 'item3', link: 'management', title: 'Item 3' },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ];
+
+        const renderResult = renderNavigation({
+          navTreeDef: { body: navigationBody },
+        });
+
+        await runTests('treeDef', renderResult);
+
+        renderResult.unmount();
+      }
+
+      // -- With UI Components
+      {
+        const renderResult = renderNavigation({
+          navigationElement: (
+            <Navigation>
+              <Navigation.Group id="root" isCollapsible={false}>
+                <Navigation.Group id="group1" link="dashboards" renderAs="panelOpener">
+                  <Navigation.Group id="foo" title="Foo">
+                    <Navigation.Item id="item1" link="management" title="Item 1" />
+                    <Navigation.Item
+                      id="item2"
+                      link="management"
+                      sideNavStatus="hidden"
+                      title="Item 2"
+                    />
+                    <Navigation.Item id="item3" link="management" title="Item 3" />
+                  </Navigation.Group>
+                </Navigation.Group>
+              </Navigation.Group>
+            </Navigation>
+          ),
+        });
+        await runTests('uiComponents', renderResult);
+      }
+    });
+
+    test('should rendre block groups without title', async () => {
+      const runTests = async (
+        type: TestType,
+        { queryByTestId, queryAllByTestId }: RenderResult
+      ) => {
+        try {
+          queryByTestId(/panelOpener-root.group1/)?.click(); // open the panel
+
+          expect(queryByTestId(/panelGroupTitleId-foo/)).toBeNull(); // No title rendered
+
+          const panelNavItems = queryAllByTestId(/panelNavItem/);
+          expect(panelNavItems.length).toBe(2); // "item2" has been filtered out as it is hidden
+          expect(panelNavItems.map(({ textContent }) => textContent?.trim())).toEqual([
+            'Item 1',
+            'Item 3',
+          ]);
+        } catch (e) {
+          errorHandler(type)(e);
+        }
+      };
+
+      // -- Default navigation
+      {
+        const navigationBody: Array<RootNavigationItemDefinition<any>> = [
+          {
+            type: 'navGroup',
+            id: 'root',
+            isCollapsible: false,
+            children: [
+              {
+                id: 'group1',
+                link: 'dashboards',
+                renderAs: 'panelOpener',
+                children: [
+                  {
+                    id: 'foo',
+                    children: [
+                      { id: 'item1', link: 'management', title: 'Item 1' },
+                      { id: 'item2', link: 'management', title: 'Item 2', sideNavStatus: 'hidden' },
+                      { id: 'item3', link: 'management', title: 'Item 3' },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ];
+
+        const renderResult = renderNavigation({
+          navTreeDef: { body: navigationBody },
+        });
+
+        await runTests('treeDef', renderResult);
+
+        renderResult.unmount();
+      }
+
+      // -- With UI Components
+      {
+        // const renderResult = renderNavigation({
+        //   navigationElement: (
+        //     <Navigation>
+        //       <Navigation.Group id="root" isCollapsible={false}>
+        //         <Navigation.Group id="group1" link="dashboards" renderAs="panelOpener">
+        //           <Navigation.Group id="foo" title="Foo">
+        //             <Navigation.Item id="item1" link="management" title="Item 1" />
+        //             <Navigation.Item
+        //               id="item2"
+        //               link="management"
+        //               sideNavStatus="hidden"
+        //               title="Item 2"
+        //             />
+        //             <Navigation.Item id="item3" link="management" title="Item 3" />
+        //           </Navigation.Group>
+        //         </Navigation.Group>
+        //       </Navigation.Group>
+        //     </Navigation>
+        //   ),
+        // });
+        // await runTests('uiComponents', renderResult);
+      }
+    });
+
+    test('should rendre accordion groups', async () => {
+      const runTests = async (
+        type: TestType,
+        { queryByTestId, queryAllByTestId, findByRole }: RenderResult
+      ) => {
+        try {
+          queryByTestId(/panelOpener-root.group1/)?.click(); // open the panel
+
+          expect(queryByTestId(/panelGroupId-foo/)).toBeVisible();
+
+          const panelNavItems = queryAllByTestId(/panelNavItem/);
+          expect(panelNavItems.length).toBe(2); // "item2" has been filtered out as it is hidden
+
+          expect(queryByTestId(/panelNavItem-id-item1/)).not.toBeVisible(); // Accordion is collapsed
+          expect(queryByTestId(/panelNavItem-id-item3/)).not.toBeVisible(); // Accordion is collapsed
+
+          queryByTestId(/panelAccordionBtnId-foo/)?.click(); // Expand accordion
+
+          expect(queryByTestId(/panelNavItem-id-item1/)).toBeVisible();
+          expect(queryByTestId(/panelNavItem-id-item3/)).toBeVisible();
+        } catch (e) {
+          errorHandler(type)(e);
+        }
+      };
+
+      // -- Default navigation
+      {
+        const navigationBody: Array<RootNavigationItemDefinition<any>> = [
+          {
+            type: 'navGroup',
+            id: 'root',
+            isCollapsible: false,
+            children: [
+              {
+                id: 'group1',
+                link: 'dashboards',
+                renderAs: 'panelOpener',
+                children: [
+                  {
+                    id: 'foo',
+                    title: 'Foo',
+                    renderAs: 'accordion',
+                    children: [
+                      { id: 'item1', link: 'management', title: 'Item 1' },
+                      { id: 'item2', link: 'management', title: 'Item 2', sideNavStatus: 'hidden' },
+                      { id: 'item3', link: 'management', title: 'Item 3' },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ];
+
+        const renderResult = renderNavigation({
+          navTreeDef: { body: navigationBody },
+        });
+
+        await runTests('treeDef', renderResult);
+
+        renderResult.unmount();
+      }
+
+      // -- With UI Components
+      {
+        const renderResult = renderNavigation({
+          navigationElement: (
+            <Navigation>
+              <Navigation.Group id="root" isCollapsible={false}>
+                <Navigation.Group id="group1" link="dashboards" renderAs="panelOpener">
+                  <Navigation.Group id="foo" title="Foo" renderAs="accordion">
+                    <Navigation.Item id="item1" link="management" title="Item 1" />
+                    <Navigation.Item
+                      id="item2"
+                      link="management"
+                      sideNavStatus="hidden"
+                      title="Item 2"
+                    />
+                    <Navigation.Item id="item3" link="management" title="Item 3" />
+                  </Navigation.Group>
+                </Navigation.Group>
+              </Navigation.Group>
+            </Navigation>
+          ),
+        });
+        await runTests('uiComponents', renderResult);
+      }
+    });
+  });
 });
