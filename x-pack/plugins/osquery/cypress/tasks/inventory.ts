b7610b5caf9f7c6a42ca19c53c323d453c276a83
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-10-30 15:24:06 +0100

    fix metrics

--- x-pack/plugins/osquery/cypress/tasks/inventory.ts
+++ x-pack/plugins/osquery/cypress/tasks/inventory.ts
@@ -9,7 +9,15 @@ export const triggerLoadData = () => {
   cy.getBySel('infraWaffleTimeControlsAutoRefreshButton').should('exist');
   cy.wait(1000);
   cy.getBySel('infraWaffleTimeControlsAutoRefreshButton').click();
-  cy.getBySel('nodeContainer').last().should('exist');
+  cy.getBySel('nodeContainer', { multiple: true })
+    .not(':contains("dev-fleet-server")')
+    .first()
+    .should('exist');
   cy.getBySel('infraWaffleTimeControlsStopRefreshingButton').click();
-  cy.getBySel('nodeContainer').last().click();
+  cy.getBySel('nodeContainer', {
+    multiple: true,
+  })
+    .not(':contains("dev-fleet-server")')
+    .first()
+    .click();
 };
