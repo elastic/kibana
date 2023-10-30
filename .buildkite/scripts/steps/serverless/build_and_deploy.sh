1a5ed97819d2fe0af5cba9fdca3ca24eabc7f7b6
Jonathan Budzenski <jon@elastic.co>
2023-10-30 10:49:54 -0500

    support updates

--- .buildkite/scripts/steps/serverless/build_and_deploy.sh
+++ .buildkite/scripts/steps/serverless/build_and_deploy.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+# Defines KIBANA_IMAGE
 source .buildkite/scripts/steps/artifacts/docker_image.sh
 
 PROJECT_TYPE=""
@@ -12,43 +13,70 @@ if [ -z "${PROJECT_TYPE}" ]; then
   echo "Mising project type"
   exit 10
 fi
+
 PROJECT_NAME="kibana-$PROJECT_TYPE-pr-$BUILDKITE_PULL_REQUEST"
+PROJECT_CREATE_CONFIGURATION='{
+  "name": "'"$PROJECT_NAME"'",
+  "region_id": "aws-eu-west-1",
+  "overrides": {
+      "kibana": {
+          "docker_image": "'"$KIBANA_IMAGE"'"
+      }
+  }
+}'
+PROJECT_UPDATE_CONFIGURATION='{
+  "name": "'"$PROJECT_NAME"'",
+  "overrides": {
+      "kibana": {
+          "docker_image": "'"$KIBANA_IMAGE"'"
+      }
+  }
+}'
 
 echo "--- Create project"
 DEPLOY_LOGS=$(mktemp --suffix ".json")
 
-echo "Creating project..."
+echo "Checking if project already exists..."
 curl -s \
   -H "Authorization: ApiKey $PROJECT_API_KEY" \
-  -H "Content-Type: application/json" \
   "${PROJECT_API_DOMAIN}/api/v1/serverless/projects/${PROJECT_TYPE}" \
-  -XPOST -d '{
-    "name": "'"$PROJECT_NAME"'",
-    "region_id": "aws-eu-west-1",
-    "overrides": {
-        "kibana": {
-            "docker_image": "'"$KIBANA_IMAGE"'"
-        }
-    }
-  }' &>> $DEPLOY_LOGS
+  -XGET &>> $DEPLOY_LOGS
 
-PROJECT_KIBANA_URL=$(jq -r --slurp '.[0].endpoints.kibana' $DEPLOY_LOGS)
-PROJECT_ELASTICSEARCH_URL=$(jq -r --slurp '.[0].endpoints.elasticsearch' $DEPLOY_LOGS)
-PROJECT_ID=$(jq -r --slurp '.[0].id' $DEPLOY_LOGS)
+PROJECT_ID=$(jq -r --slurp '[.[0].items[] | select(.name == "'$PROJECT_NAME'")] | .[0].id' $DEPLOY_LOGS)
+if [ -z "${PROJECT_ID}" ] || [ "$PROJECT_ID" = 'null' ]; then
+  echo "Creating project..."
+  curl -s \
+    -H "Authorization: ApiKey $PROJECT_API_KEY" \
+    -H "Content-Type: application/json" \
+    "${PROJECT_API_DOMAIN}/api/v1/serverless/projects/${PROJECT_TYPE}" \
+    -XPOST -d "$PROJECT_CREATE_CONFIGURATION" &>> $DEPLOY_LOGS
 
-echo "Get credentials..."
-curl -s -XPOST -H "Authorization: ApiKey $PROJECT_API_KEY" \
-  "${PROJECT_API_DOMAIN}/api/v1/serverless/projects/${PROJECT_TYPE}/${PROJECT_ID}/_reset-credentials" &>> $DEPLOY_LOGS
+  PROJECT_ID=$(jq -r --slurp '.[1].id' $DEPLOY_LOGS)
 
-PROJECT_USERNAME=$(jq -r --slurp '.[1].username' $DEPLOY_LOGS)
-PROJECT_PASSWORD=$(jq -r --slurp '.[1].password' $DEPLOY_LOGS)
+  echo "Get credentials..."
+  curl -s -XPOST -H "Authorization: ApiKey $PROJECT_API_KEY" \
+    "${PROJECT_API_DOMAIN}/api/v1/serverless/projects/${PROJECT_TYPE}/${PROJECT_ID}/_reset-credentials" &>> $DEPLOY_LOGS
 
-echo "Write to vault..."
-VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-role-id)"
-VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-secret-id)"
-VAULT_TOKEN=$(retry 5 30 vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
-retry 5 30 vault login -no-print "$VAULT_TOKEN"
-retry 5 5 vault write "secret/kibana-issues/dev/cloud-deploy/$PROJECT_NAME" username="$PROJECT_USERNAME" password="$PROJECT_PASSWORD" id="$PROJECT_ID"
+  PROJECT_USERNAME=$(jq -r --slurp '.[2].username' $DEPLOY_LOGS)
+  PROJECT_PASSWORD=$(jq -r --slurp '.[2].password' $DEPLOY_LOGS)
+
+  echo "Write to vault..."
+  VAULT_ROLE_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-role-id)"
+  VAULT_SECRET_ID="$(retry 5 15 gcloud secrets versions access latest --secret=kibana-buildkite-vault-secret-id)"
+  VAULT_TOKEN=$(retry 5 30 vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
+  retry 5 30 vault login -no-print "$VAULT_TOKEN"
+  retry 5 5 vault write "secret/kibana-issues/dev/cloud-deploy/$PROJECT_NAME" username="$PROJECT_USERNAME" password="$PROJECT_PASSWORD" id="$PROJECT_ID"
+else
+  echo "Updating project..."
+  curl -s \
+    -H "Authorization: ApiKey $PROJECT_API_KEY" \
+    -H "Content-Type: application/json" \
+    "${PROJECT_API_DOMAIN}/api/v1/serverless/projects/${PROJECT_TYPE}/${PROJECT_ID}" \
+    -XPUT -d "$PROJECT_UPDATE_CONFIGURATION" &>> $DEPLOY_LOGS
+fi
+
+PROJECT_KIBANA_URL=$(jq -r --slurp '.[1].endpoints.kibana' $DEPLOY_LOGS)
+PROJECT_ELASTICSEARCH_URL=$(jq -r --slurp '.[1].endpoints.elasticsearch' $DEPLOY_LOGS)
 
 cat << EOF | buildkite-agent annotate --style "info" --context project
   ### Project Deployment
