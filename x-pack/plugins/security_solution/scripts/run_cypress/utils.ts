c22ec0e11275637cffd0b198ac84dabe6b73dc6b
Gloria Hornero <gloria.hornero@elastic.co>
2023-10-31 13:40:14 +0100

    reuses existing types

--- x-pack/plugins/security_solution/scripts/run_cypress/utils.ts
+++ x-pack/plugins/security_solution/scripts/run_cypress/utils.ts
@@ -11,6 +11,7 @@ import * as parser from '@babel/parser';
 import generate from '@babel/generator';
 import type { ExpressionStatement, ObjectExpression, ObjectProperty } from '@babel/types';
 import { schema, type TypeOf } from '@kbn/config-schema';
+import { productType as ProductTypeSchema } from '@kbn/security-solution-serverless/common/config';
 import { getExperimentalAllowedValues } from '../../common/experimental_features';
 
 export const isSkipped = (filePath: string): boolean => {
@@ -100,22 +101,10 @@ const TestFileFtrConfigSchema = schema.object(
         })
       )
     ),
-    productTypes: schema.maybe(
-      // TODO:PT write validate function to ensure that only the correct combinations are used
-      schema.arrayOf(
-        schema.object({
-          product_line: schema.oneOf([
-            schema.literal('security'),
-            schema.literal('endpoint'),
-            schema.literal('cloud'),
-          ]),
-
-          product_tier: schema.oneOf([schema.literal('essentials'), schema.literal('complete')]),
-        })
-      )
-    ),
+    productTypes: schema.maybe(schema.arrayOf(ProductTypeSchema)),
   },
   { defaultValue: {}, unknowns: 'forbid' }
 );
 
 export type SecuritySolutionDescribeBlockFtrConfig = TypeOf<typeof TestFileFtrConfigSchema>;
+export type ProductType = TypeOf<typeof ProductTypeSchema>;
