f5d3313b4f30d4ff57dbade2bfcb90670603622b
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2023-10-31 10:51:15 +0100

    Fix datastream regex

--- packages/kbn-apm-synthtrace/src/lib/logs/logs_synthtrace_es_client.ts
+++ packages/kbn-apm-synthtrace/src/lib/logs/logs_synthtrace_es_client.ts
@@ -22,7 +22,7 @@ export class LogsSynthtraceEsClient extends SynthtraceEsClient<LogDocument> {
       ...options,
       pipeline: logsPipeline(),
     });
-    this.dataStreams = ['logs-*'];
+    this.dataStreams = ['logs-*-*'];
   }
 }
 
