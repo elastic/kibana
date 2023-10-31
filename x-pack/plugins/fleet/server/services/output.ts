38d4e4a82d0af65f31b96b4e6b90f290d1a88e73
Mark Hopkin <mark.hopkin@elastic.co>
2023-10-31 17:05:06 +0000

    use argon2 to diff preconfigured secrets

--- x-pack/plugins/fleet/server/services/output.ts
+++ x-pack/plugins/fleet/server/services/output.ts
@@ -418,7 +418,12 @@ class OutputService {
     soClient: SavedObjectsClientContract,
     esClient: ElasticsearchClient,
     output: NewOutput,
-    options?: { id?: string; fromPreconfiguration?: boolean; overwrite?: boolean }
+    options?: {
+      id?: string;
+      fromPreconfiguration?: boolean;
+      overwrite?: boolean;
+      secretHashes?: Record<string, any>;
+    }
   ): Promise<Output> {
     const data: OutputSOAttributes = { ...omit(output, ['ssl', 'secrets']) };
     const defaultDataOutputId = await this.getDefaultDataOutputId(soClient);
@@ -544,6 +549,7 @@ class OutputService {
       const { output: outputWithSecrets } = await extractAndWriteOutputSecrets({
         output,
         esClient,
+        secretHashes: output.is_preconfigured ? options?.secretHashes : undefined,
       });
 
       if (outputWithSecrets.secrets) data.secrets = outputWithSecrets.secrets;
@@ -705,7 +711,10 @@ class OutputService {
     esClient: ElasticsearchClient,
     id: string,
     data: Partial<Output>,
-    { fromPreconfiguration = false }: { fromPreconfiguration: boolean } = {
+    {
+      fromPreconfiguration = false,
+      secretHashes,
+    }: { fromPreconfiguration: boolean; secretHashes?: Record<string, any> } = {
       fromPreconfiguration: false,
     }
   ) {
@@ -728,6 +737,7 @@ class OutputService {
         oldOutput: originalOutput,
         outputUpdate: data,
         esClient,
+        secretHashes: data.is_preconfigured ? secretHashes : undefined,
       });
 
       updateData.secrets = secretsRes.outputUpdate.secrets;
