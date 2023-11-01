1db2b0466cfe221199de4fbc3f492dfd2cc482e5
Davis Plumlee <davis.plumlee@elastic.co>
2023-11-01 10:50:52 -0400

    addresses comments

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/logic/coverage_overview/build_coverage_overview_dashboard_model.test.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/logic/coverage_overview/build_coverage_overview_dashboard_model.test.ts
@@ -33,7 +33,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
     jest.clearAllMocks();
   });
 
-  it('correctly maps API response', async () => {
+  it('maps API response', async () => {
     const mockApiResponse: CoverageOverviewResponse = {
       coverage: {
         TA001: ['test-rule-1'],
@@ -53,10 +53,116 @@ describe('buildCoverageOverviewDashboardModel', () => {
 
     const model = await buildCoverageOverviewDashboardModel(mockApiResponse);
 
-    expect(model).toMatchSnapshot();
+    expect(model).toEqual({
+      metrics: {
+        totalEnabledRulesCount: 1,
+        totalRulesCount: 1,
+      },
+      mitreTactics: [
+        {
+          availableRules: [],
+          disabledRules: [],
+          enabledRules: [
+            {
+              id: 'test-rule-1',
+              name: 'Test rule',
+            },
+          ],
+          id: 'TA001',
+          name: 'Tactic 1',
+          reference: 'https://some-link/TA001',
+          techniques: [
+            {
+              availableRules: [],
+              disabledRules: [],
+              enabledRules: [
+                {
+                  id: 'test-rule-1',
+                  name: 'Test rule',
+                },
+              ],
+              id: 'T001',
+              name: 'Technique 1',
+              reference: 'https://some-link/T001',
+              subtechniques: [
+                {
+                  availableRules: [],
+                  disabledRules: [],
+                  enabledRules: [
+                    {
+                      id: 'test-rule-1',
+                      name: 'Test rule',
+                    },
+                  ],
+                  id: 'T001.001',
+                  name: 'Subtechnique 1',
+                  reference: 'https://some-link/T001/001',
+                },
+                {
+                  availableRules: [],
+                  disabledRules: [],
+                  enabledRules: [],
+                  id: 'T001.002',
+                  name: 'Subtechnique 2',
+                  reference: 'https://some-link/T001/002',
+                },
+              ],
+            },
+            {
+              availableRules: [],
+              disabledRules: [],
+              enabledRules: [
+                {
+                  id: 'test-rule-1',
+                  name: 'Test rule',
+                },
+              ],
+              id: 'T002',
+              name: 'Technique 2',
+              reference: 'https://some-link/T002',
+              subtechniques: [],
+            },
+          ],
+        },
+        {
+          availableRules: [],
+          disabledRules: [],
+          enabledRules: [
+            {
+              id: 'test-rule-1',
+              name: 'Test rule',
+            },
+          ],
+          id: 'TA002',
+          name: 'Tactic 2',
+          reference: 'https://some-link/TA002',
+          techniques: [
+            {
+              availableRules: [],
+              disabledRules: [],
+              enabledRules: [
+                {
+                  id: 'test-rule-1',
+                  name: 'Test rule',
+                },
+              ],
+              id: 'T002',
+              name: 'Technique 2',
+              reference: 'https://some-link/T002',
+              subtechniques: [],
+            },
+          ],
+        },
+      ],
+      unmappedRules: {
+        availableRules: [],
+        disabledRules: [],
+        enabledRules: [],
+      },
+    });
   });
 
-  it('correctly maps techniques that appear in multiple tactics', async () => {
+  it('maps techniques that appear in multiple tactics', async () => {
     const mockApiResponse: CoverageOverviewResponse = {
       coverage: {
         TA001: ['test-rule-1', 'test-rule-2'],
@@ -78,6 +184,110 @@ describe('buildCoverageOverviewDashboardModel', () => {
 
     const model = await buildCoverageOverviewDashboardModel(mockApiResponse);
 
-    expect(model).toMatchSnapshot();
+    expect(model).toEqual({
+      metrics: {
+        totalEnabledRulesCount: 2,
+        totalRulesCount: 2,
+      },
+      mitreTactics: [
+        {
+          availableRules: [],
+          disabledRules: [],
+          enabledRules: [
+            {
+              id: 'test-rule-1',
+              name: 'Test rule',
+            },
+            {
+              id: 'test-rule-2',
+              name: 'Test rule 2',
+            },
+          ],
+          id: 'TA001',
+          name: 'Tactic 1',
+          reference: 'https://some-link/TA001',
+          techniques: [
+            {
+              availableRules: [],
+              disabledRules: [],
+              enabledRules: [],
+              id: 'T001',
+              name: 'Technique 1',
+              reference: 'https://some-link/T001',
+              subtechniques: [
+                {
+                  availableRules: [],
+                  disabledRules: [],
+                  enabledRules: [],
+                  id: 'T001.001',
+                  name: 'Subtechnique 1',
+                  reference: 'https://some-link/T001/001',
+                },
+                {
+                  availableRules: [],
+                  disabledRules: [],
+                  enabledRules: [],
+                  id: 'T001.002',
+                  name: 'Subtechnique 2',
+                  reference: 'https://some-link/T001/002',
+                },
+              ],
+            },
+            {
+              availableRules: [],
+              disabledRules: [],
+              enabledRules: [
+                {
+                  id: 'test-rule-1',
+                  name: 'Test rule',
+                },
+                {
+                  id: 'test-rule-2',
+                  name: 'Test rule 2',
+                },
+              ],
+              id: 'T002',
+              name: 'Technique 2',
+              reference: 'https://some-link/T002',
+              subtechniques: [],
+            },
+          ],
+        },
+        {
+          availableRules: [],
+          disabledRules: [],
+          enabledRules: [
+            {
+              id: 'test-rule-2',
+              name: 'Test rule 2',
+            },
+          ],
+          id: 'TA002',
+          name: 'Tactic 2',
+          reference: 'https://some-link/TA002',
+          techniques: [
+            {
+              availableRules: [],
+              disabledRules: [],
+              enabledRules: [
+                {
+                  id: 'test-rule-2',
+                  name: 'Test rule 2',
+                },
+              ],
+              id: 'T002',
+              name: 'Technique 2',
+              reference: 'https://some-link/T002',
+              subtechniques: [],
+            },
+          ],
+        },
+      ],
+      unmappedRules: {
+        availableRules: [],
+        disabledRules: [],
+        enabledRules: [],
+      },
+    });
   });
 });
