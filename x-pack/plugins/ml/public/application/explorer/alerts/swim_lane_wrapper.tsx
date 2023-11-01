12d6e2d787d6c91a2ababac04539295d054bc4a4
Dima Arnautov <dmitrii.arnautov@elastic.co>
2023-11-01 13:57:22 +0100

    popover table view

--- x-pack/plugins/ml/public/application/explorer/alerts/swim_lane_wrapper.tsx
+++ x-pack/plugins/ml/public/application/explorer/alerts/swim_lane_wrapper.tsx
@@ -5,11 +5,13 @@
  * 2.0.
  */
 
+import { FormattedMessage } from '@kbn/i18n-react';
 import {
   EuiButtonIcon,
   EuiDescriptionList,
   EuiFlexGroup,
   EuiFlexItem,
+  EuiInMemoryTable,
   EuiPopover,
   EuiPopoverTitle,
 } from '@elastic/eui';
@@ -19,11 +21,17 @@ import { pick } from 'lodash';
 import React, { useMemo, useRef, type FC } from 'react';
 import useObservable from 'react-use/lib/useObservable';
 import { i18n } from '@kbn/i18n';
+import { EuiBasicTableColumn } from '@elastic/eui/src/components/basic_table/basic_table';
+import { AnomalyDetectionAlert } from '../anomaly_detection_alerts_state_service';
 import {
   ALERT_ANOMALY_DETECTION_JOB_ID,
   ALERT_ANOMALY_TIMESTAMP,
+  alertFieldNameMap,
 } from '../../../../common/constants/alerts';
-import { getAlertEntryFormatter } from '../../../alerting/anomaly_detection_alerts_table/render_cell_value';
+import {
+  getAlertEntryFormatter,
+  getAlertFormatters,
+} from '../../../alerting/anomaly_detection_alerts_table/render_cell_value';
 import { useMlKibana } from '../../contexts/kibana';
 import { useAnomalyExplorerContext } from '../anomaly_explorer_context';
 import type { AppStateSelectedCells, SwimlaneData } from '../explorer_utils';
@@ -82,6 +90,8 @@ export const SwimLaneWrapper: FC<SwimLaneWrapperProps> = ({
 
   const alertFormatter = useMemo(() => getAlertEntryFormatter(fieldFormats), [fieldFormats]);
 
+  const viewType = 'table';
+
   return (
     <div
       ref={containerRef}
@@ -108,49 +118,128 @@ export const SwimLaneWrapper: FC<SwimLaneWrapperProps> = ({
           closePopover={() => {}}
           panelPaddingSize="s"
         >
-          <EuiPopoverTitle paddingSize={'none'}>
-            <EuiButtonIcon
-              size="s"
-              color={'text'}
-              iconType={'cross'}
-              onClick={() => anomalyTimelineStateService.setSelectedCells()}
-              aria-label={i18n.translate(
-                'xpack.ml.explorer.cellSelectionPopover.closeButtonAriaLabel',
-                {
-                  defaultMessage: 'Close popover',
-                }
-              )}
-            />
+          <EuiPopoverTitle paddingSize={'xs'}>
+            <EuiFlexGroup gutterSize={'none'} justifyContent={'spaceBetween'} alignItems={'center'}>
+              <EuiFlexItem grow={false}>
+                <FormattedMessage
+                  id="xpack.ml.explorer.totalSelectedAlerts"
+                  defaultMessage="Total alerts: {total}"
+                  values={{ total: selectedAlerts?.length ?? 0 }}
+                />
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiButtonIcon
+                  size="s"
+                  color={'text'}
+                  iconType={'cross'}
+                  onClick={() => anomalyTimelineStateService.setSelectedCells()}
+                  aria-label={i18n.translate(
+                    'xpack.ml.explorer.cellSelectionPopover.closeButtonAriaLabel',
+                    {
+                      defaultMessage: 'Close popover',
+                    }
+                  )}
+                />
+              </EuiFlexItem>
+            </EuiFlexGroup>
           </EuiPopoverTitle>
-          {(selectedAlerts ?? []).map((alert) => {
-            const fields = Object.entries(
-              pick(alert, [
-                ALERT_RULE_NAME,
-                ALERT_ANOMALY_DETECTION_JOB_ID,
-                ALERT_ANOMALY_TIMESTAMP,
-                ALERT_START,
-                ALERT_DURATION,
-              ])
-            ).map(([prop, value]) => {
-              return alertFormatter(prop, value);
-            });
+          {viewType === 'table' && !!selectedAlerts?.length ? (
+            <MiniAlertTable data={selectedAlerts} />
+          ) : (
+            (selectedAlerts ?? []).map((alert) => {
+              const fields = Object.entries(
+                pick(alert, [
+                  ALERT_RULE_NAME,
+                  ALERT_ANOMALY_DETECTION_JOB_ID,
+                  ALERT_ANOMALY_TIMESTAMP,
+                  ALERT_START,
+                  ALERT_DURATION,
+                ])
+              ).map(([prop, value]) => {
+                return alertFormatter(prop, value);
+              });
 
-            return (
-              <EuiFlexGroup>
-                {fields.map(({ title, description }) => {
-                  return (
-                    <EuiFlexItem>
-                      <EuiDescriptionList compressed listItems={[{ title, description }]} />
-                    </EuiFlexItem>
-                  );
-                })}
-              </EuiFlexGroup>
-            );
-          })}
+              return (
+                <EuiFlexGroup>
+                  {fields.map(({ title, description }) => {
+                    return (
+                      <EuiFlexItem>
+                        <EuiDescriptionList compressed listItems={[{ title, description }]} />
+                      </EuiFlexItem>
+                    );
+                  })}
+                </EuiFlexGroup>
+              );
+            })
+          )}
         </EuiPopover>
       </div>
 
       {children}
     </div>
+  );
+};
+
+export interface MiniAlertTableProps {
+  data: AnomalyDetectionAlert[];
+}
+
+const ALERT_PER_PAGE = 3;
+
+export const MiniAlertTable: FC<MiniAlertTableProps> = ({ data }) => {
+  const {
+    services: { fieldFormats },
+  } = useMlKibana();
+
+  const alertValueFormatter = useMemo(() => getAlertFormatters(fieldFormats), [fieldFormats]);
+
+  const columns = useMemo<Array<EuiBasicTableColumn<AnomalyDetectionAlert>>>(() => {
+    return [
+      {
+        field: ALERT_RULE_NAME,
+        width: `150px`,
+        name: alertFieldNameMap[ALERT_RULE_NAME],
+        sortable: true,
+      },
+      {
+        field: ALERT_START,
+        width: `200px`,
+        name: alertFieldNameMap[ALERT_START],
+        sortable: true,
+        render: (value: number) => alertValueFormatter(ALERT_START, value),
+      },
+      {
+        field: ALERT_DURATION,
+        width: `110px`,
+        name: alertFieldNameMap[ALERT_DURATION],
+        sortable: true,
+        render: (value: number) => alertValueFormatter(ALERT_DURATION, value),
+      },
+    ];
+  }, [alertValueFormatter]);
+
+  return (
+    <EuiInMemoryTable
+      css={{ width: '510px' }}
+      compressed
+      columns={columns}
+      items={data}
+      sorting={{
+        sort: {
+          field: ALERT_START,
+          direction: 'asc',
+        },
+      }}
+      pagination={
+        data.length > ALERT_PER_PAGE
+          ? {
+              compressed: true,
+              initialPageSize: ALERT_PER_PAGE,
+              pageSizeOptions: [ALERT_PER_PAGE],
+              pageSize: ALERT_PER_PAGE,
+            }
+          : false
+      }
+    />
   );
 };
