d761a4aed813081d7b29d12a71a2199fe6d6b65a
Gloria Hornero <gloria.hornero@elastic.co>
2023-11-02 14:40:15 +0100

    fixes merge conflict issue

--- x-pack/test/security_solution_cypress/cypress/e2e/investigations/alerts/alerts_details.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/investigations/alerts/alerts_details.cy.ts
@@ -205,74 +205,61 @@ describe('Alert details flyout', { tags: ['@ess', '@serverless'] }, () => {
     const getFlyoutConfig = (dataTable: { [alertTableKey]: DataTableModel }) =>
       dataTable?.[alertTableKey]?.expandedDetail?.query;
 
-      beforeEach(() => {
-        login();
-        disableExpandableFlyout();
-        visitWithTimeRange(ALERTS_URL);
-        waitForAlertsToPopulate();
-        expandFirstAlert();
-      });
+    /**
+     * Localstorage is updated after a delay here x-pack/plugins/security_solution/public/common/store/data_table/epic_local_storage.ts
+     * We create this config to re-check localStorage 3 times, every 500ms to avoid any potential flakyness from that delay
+     */
+    const storageCheckRetryConfig = {
+      timeout: 1500,
+      interval: 500,
+    };
 
-      const alertTableKey = 'alerts-page';
-      const getFlyoutConfig = (dataTable: { [alertTableKey]: DataTableModel }) =>
-        dataTable?.[alertTableKey]?.expandedDetail?.query;
+    it('should store the flyout state in localstorage', () => {
+      cy.get(OVERVIEW_RULE).should('be.visible');
+      const localStorageCheck = () =>
+        cy.getAllLocalStorage().then((storage) => {
+          const securityDataTable = getLocalstorageEntryAsObject(storage, 'securityDataTable');
+          return getFlyoutConfig(securityDataTable)?.panelView === 'eventDetail';
+        });
 
-      /**
-       * Localstorage is updated after a delay here x-pack/plugins/security_solution/public/common/store/data_table/epic_local_storage.ts
-       * We create this config to re-check localStorage 3 times, every 500ms to avoid any potential flakyness from that delay
-       */
-      const storageCheckRetryConfig = {
-        timeout: 1500,
-        interval: 500,
-      };
+      cy.waitUntil(localStorageCheck, storageCheckRetryConfig);
+    });
 
-      it('should store the flyout state in localstorage', () => {
-        cy.get(OVERVIEW_RULE).should('be.visible');
-        const localStorageCheck = () =>
-          cy.getAllLocalStorage().then((storage) => {
-            const securityDataTable = getLocalstorageEntryAsObject(storage, 'securityDataTable');
-            return getFlyoutConfig(securityDataTable)?.panelView === 'eventDetail';
-          });
+    it('should remove the flyout details from local storage when closed', () => {
+      cy.get(OVERVIEW_RULE).should('be.visible');
+      closeAlertFlyout();
+      const localStorageCheck = () =>
+        cy.getAllLocalStorage().then((storage) => {
+          const securityDataTable = getLocalstorageEntryAsObject(storage, 'securityDataTable');
+          return getFlyoutConfig(securityDataTable)?.panelView === undefined;
+        });
 
-        cy.waitUntil(localStorageCheck, storageCheckRetryConfig);
-      });
+      cy.waitUntil(localStorageCheck, storageCheckRetryConfig);
+    });
 
-      it('should remove the flyout details from local storage when closed', () => {
-        cy.get(OVERVIEW_RULE).should('be.visible');
-        closeAlertFlyout();
-        const localStorageCheck = () =>
-          cy.getAllLocalStorage().then((storage) => {
-            const securityDataTable = getLocalstorageEntryAsObject(storage, 'securityDataTable');
-            return getFlyoutConfig(securityDataTable)?.panelView === undefined;
-          });
+    it('should remove the flyout state from localstorage when navigating away without closing the flyout', () => {
+      cy.get(OVERVIEW_RULE).should('be.visible');
 
-        cy.waitUntil(localStorageCheck, storageCheckRetryConfig);
-      });
+      visitRuleDetailsPage(ARCHIVED_RULE_ID);
+      waitForRuleDetailsPageToBeLoaded(ARCHIVED_RULE_NAME);
 
-      it('should remove the flyout state from localstorage when navigating away without closing the flyout', () => {
-        cy.get(OVERVIEW_RULE).should('be.visible');
+      const localStorageCheck = () =>
+        cy.getAllLocalStorage().then((storage) => {
+          const securityDataTable = getLocalstorageEntryAsObject(storage, 'securityDataTable');
+          return getFlyoutConfig(securityDataTable)?.panelView === undefined;
+        });
 
-        visitRuleDetailsPage(ARCHIVED_RULE_ID);
-        waitForRuleDetailsPageToBeLoaded(ARCHIVED_RULE_NAME);
+      cy.waitUntil(localStorageCheck, storageCheckRetryConfig);
+    });
 
-        const localStorageCheck = () =>
-          cy.getAllLocalStorage().then((storage) => {
-            const securityDataTable = getLocalstorageEntryAsObject(storage, 'securityDataTable');
-            return getFlyoutConfig(securityDataTable)?.panelView === undefined;
-          });
+    it('should not reopen the flyout when navigating away from the alerts page and returning to it', () => {
+      cy.get(OVERVIEW_RULE).should('be.visible');
 
-        cy.waitUntil(localStorageCheck, storageCheckRetryConfig);
-      });
+      visitRuleDetailsPage(ARCHIVED_RULE_ID);
+      waitForRuleDetailsPageToBeLoaded(ARCHIVED_RULE_NAME);
 
-      it('should not reopen the flyout when navigating away from the alerts page and returning to it', () => {
-        cy.get(OVERVIEW_RULE).should('be.visible');
-
-        visitRuleDetailsPage(ARCHIVED_RULE_ID);
-        waitForRuleDetailsPageToBeLoaded(ARCHIVED_RULE_NAME);
-
-        visit(ALERTS_URL);
-        cy.get(OVERVIEW_RULE).should('not.exist');
-      });
-    }
-  );
+      visit(ALERTS_URL);
+      cy.get(OVERVIEW_RULE).should('not.exist');
+    });
+  });
 });
