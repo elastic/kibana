fa594e553a69af4ee094d78075b1840df099283a
wafaanasr <wafaa.aanasr@gmail.com>
2023-11-02 09:50:50 +0100

    remove old exports

--- x-pack/test/detection_engine_api_integration/security_and_spaces/group10/index.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/group10/index.ts
@@ -27,10 +27,6 @@ export default ({ loadTestFile }: FtrProviderContext): void => {
     loadTestFile(require.resolve('./patch_rules'));
     loadTestFile(require.resolve('./read_privileges'));
     loadTestFile(require.resolve('./open_close_signals'));
-    loadTestFile(require.resolve('./get_signals_migration_status'));
-    loadTestFile(require.resolve('./create_signals_migrations'));
-    loadTestFile(require.resolve('./finalize_signals_migrations'));
-    loadTestFile(require.resolve('./delete_signals_migrations'));
     loadTestFile(require.resolve('./timestamps'));
     loadTestFile(require.resolve('./runtime'));
     loadTestFile(require.resolve('./throttle'));
