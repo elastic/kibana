cff21fff63a8b0b8d61b51bbb29dbaf27f5c21e6
Gergő Ábrahám <gergo.abraham@elastic.co>
2023-11-02 14:32:32 +0100

    revert separation of tests: instead create roles conditionally only when running against ESS

--- x-pack/test/security_solution_endpoint_api_int/apis/index.ts
+++ x-pack/test/security_solution_endpoint_api_int/apis/index.ts
@@ -4,8 +4,8 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
 import { getRegistryUrl as getRegistryUrlFromIngest } from '@kbn/fleet-plugin/server';
+import { isServerlessKibanaFlavor } from '@kbn/security-solution-plugin/scripts/endpoint/common/stack_services';
 import { FtrProviderContext } from '../ftr_provider_context';
 import { getRegistryUrlFromTestEnv, isRegistryEnabled } from '../registry';
 import { ROLE } from '../services/roles_users';
@@ -16,7 +16,7 @@ export default function endpointAPIIntegrationTests(providerContext: FtrProvider
   describe('Endpoint plugin', function () {
     const ingestManager = getService('ingestManager');
     const rolesUsersProvider = getService('rolesUsersProvider');
-
+    const kbnClient = getService('kibanaServer');
     const log = getService('log');
 
     if (!isRegistryEnabled()) {
@@ -26,42 +26,40 @@ export default function endpointAPIIntegrationTests(providerContext: FtrProvider
     const registryUrl = getRegistryUrlFromTestEnv() ?? getRegistryUrlFromIngest();
     log.info(`Package registry URL for tests: ${registryUrl}`);
 
+    const roles = Object.values(ROLE);
     before(async () => {
       try {
         await ingestManager.setup();
       } catch (err) {
         log.warning(`Error setting up ingestManager: ${err}`);
       }
-    });
 
-    describe('tests using roles', () => {
-      const roles = Object.values(ROLE);
-      before(async () => {
+      if (!(await isServerlessKibanaFlavor(kbnClient))) {
         // create role/user
         for (const role of roles) {
           await rolesUsersProvider.createRole({ predefinedRole: role });
           await rolesUsersProvider.createUser({ name: role, roles: [role] });
         }
-      });
-      after(async () => {
+      }
+    });
+
+    after(async () => {
+      if (!(await isServerlessKibanaFlavor(kbnClient))) {
         // delete role/user
         await rolesUsersProvider.deleteUsers(roles);
         await rolesUsersProvider.deleteRoles(roles);
-      });
-
-      loadTestFile(require.resolve('./endpoint_authz'));
-      loadTestFile(require.resolve('./endpoint_response_actions/execute'));
-      loadTestFile(require.resolve('./endpoint_artifacts/trusted_apps'));
-      loadTestFile(require.resolve('./endpoint_artifacts/event_filters'));
-      loadTestFile(require.resolve('./endpoint_artifacts/host_isolation_exceptions'));
-      loadTestFile(require.resolve('./endpoint_artifacts/blocklists'));
+      }
     });
 
-    describe('tests not using roles', () => {
-      loadTestFile(require.resolve('./resolver'));
-      loadTestFile(require.resolve('./policy'));
-      loadTestFile(require.resolve('./package'));
-      loadTestFile(require.resolve('./metadata'));
-    });
+    loadTestFile(require.resolve('./resolver'));
+    loadTestFile(require.resolve('./metadata'));
+    loadTestFile(require.resolve('./policy'));
+    loadTestFile(require.resolve('./package'));
+    loadTestFile(require.resolve('./endpoint_authz'));
+    loadTestFile(require.resolve('./endpoint_response_actions/execute'));
+    loadTestFile(require.resolve('./endpoint_artifacts/trusted_apps'));
+    loadTestFile(require.resolve('./endpoint_artifacts/event_filters'));
+    loadTestFile(require.resolve('./endpoint_artifacts/host_isolation_exceptions'));
+    loadTestFile(require.resolve('./endpoint_artifacts/blocklists'));
   });
 }
