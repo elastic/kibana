24c79b91f80507e45e9af6522a4c81fd57de693b
James Rodewig <james.rodewig@elastic.co>
2023-11-02 16:48:53 -0400

    Update UI copy for `tech preview` (#170143)
    (cherry picked from commit 0997cb25238f812a3eff67733bb16ef1521abea4)
    
    # Conflicts:
    #	src/plugins/links/public/components/links_strings.ts
    #	x-pack/plugins/cases/public/common/translations.ts
    #	x-pack/plugins/security_solution/public/flyout/right/components/suppressed_alerts.tsx
    #	x-pack/plugins/security_solution/public/timelines/components/timeline/tabs_content/index.tsx
    #	x-pack/plugins/stack_alerts/public/rule_types/es_query/expression/query_form_type_chooser.tsx

--- x-pack/plugins/security_solution/public/flyout/right/components/translations.ts
+++ x-pack/plugins/security_solution/public/flyout/right/components/translations.ts
@@ -296,6 +296,6 @@ export const TECHNICAL_PREVIEW_MESSAGE = i18n.translate(
   'xpack.securitySolution.flyout.documentDetails.technicalPreviewMessage',
   {
     defaultMessage:
-      'This functionality is in technical preview and may be changed or removed completely in a future release. Elastic will take a best effort approach to fix any issues, but features in technical preview are not subject to the support SLA of official GA features.',
+      'This functionality is in technical preview and may be changed or removed completely in a future release. Elastic will work to fix any issues, but features in technical preview are not subject to the support SLA of official GA features.',
   }
 );
