74509cdc33850043fafc4d74d6a971b59a01ca2a
Jason Rhodes <jason.rhodes@elastic.co>
2023-11-03 09:38:10 -0400

    [Asset Manager] Asset client updates (#170184)
    ## Summary
    
    Updates the assets client with new methods, new tests, and better types
    - found while developing a hosts inventory table POC in a separate PR.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Kevin Lacabane <kevin.lacabane@elastic.co>

--- x-pack/plugins/asset_manager/server/routes/assets/containers.ts
+++ x-pack/plugins/asset_manager/server/routes/assets/containers.ts
@@ -7,21 +7,24 @@
 
 import { createRouteValidationFunction } from '@kbn/io-ts-utils';
 import { RequestHandlerContext } from '@kbn/core-http-request-handler-context-server';
-import { GetHostAssetsQueryOptions, getHostAssetsQueryOptionsRT } from '../../../common/types_api';
+import {
+  GetContainerAssetsQueryOptions,
+  getContainerAssetsQueryOptionsRT,
+} from '../../../common/types_api';
 import { debug } from '../../../common/debug_log';
 import { SetupRouteOptions } from '../types';
 import * as routePaths from '../../../common/constants_routes';
 import { getClientsFromContext, validateStringAssetFilters } from '../utils';
 import { AssetsValidationError } from '../../lib/validators/validation_error';
 
-export function hostsRoutes<T extends RequestHandlerContext>({
+export function containersRoutes<T extends RequestHandlerContext>({
   router,
   assetClient,
 }: SetupRouteOptions<T>) {
-  const validate = createRouteValidationFunction(getHostAssetsQueryOptionsRT);
-  router.get<unknown, GetHostAssetsQueryOptions, unknown>(
+  const validate = createRouteValidationFunction(getContainerAssetsQueryOptionsRT);
+  router.get<unknown, GetContainerAssetsQueryOptions, unknown>(
     {
-      path: routePaths.GET_HOSTS,
+      path: routePaths.GET_CONTAINERS,
       validate: {
         query: (q, res) => {
           const [invalidResponse, validatedFilters] = validateStringAssetFilters(q, res);
@@ -40,7 +43,7 @@ export function hostsRoutes<T extends RequestHandlerContext>({
       const { elasticsearchClient, savedObjectsClient } = await getClientsFromContext(context);
 
       try {
-        const response = await assetClient.getHosts({
+        const response = await assetClient.getContainers({
           from,
           to,
           filters, // safe due to route validation, are there better ways to do this?
@@ -50,19 +53,19 @@ export function hostsRoutes<T extends RequestHandlerContext>({
 
         return res.ok({ body: response });
       } catch (error: unknown) {
-        debug('Error while looking up HOST asset records', error);
+        debug('Error while looking up CONTAINER asset records', error);
 
         if (error instanceof AssetsValidationError) {
           return res.customError({
             statusCode: error.statusCode,
             body: {
-              message: `Error while looking up host asset records - ${error.message}`,
+              message: `Error while looking up container asset records - ${error.message}`,
             },
           });
         }
         return res.customError({
           statusCode: 500,
-          body: { message: 'Error while looking up host asset records - ' + `${error}` },
+          body: { message: 'Error while looking up container asset records - ' + `${error}` },
         });
       }
     }
