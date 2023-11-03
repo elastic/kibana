cda8ecdf516d535dd8f319fdc92696ea203efb3b
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-11-03 16:31:17 +0200

    Enabled tests to run on proper kibana nodes with parallelism

--- .buildkite/scripts/pipelines/security_solution_quality_gate/pipeline.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/pipeline.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
+
 set -euo pipefail
 
-source .buildkite/scripts/common/util.sh
-source .buildkite/scripts/steps/functional/common_cypress.sh
-.buildkite/scripts/bootstrap.sh
-
-export JOB=kibana-security-solution-chrome
-
-buildkite-agent meta-data set "${BUILDKITE_JOB_ID}_is_test_execution_step" "true"
-
-echo "--- Serverless Security Second Quality Gate"
-cd x-pack/test/security_solution_cypress
-set +e
-
-QA_API_KEY=$(retry 5 5 vault read -field=qa_api_key secret/kibana-issues/dev/security-solution-qg-enc-key)
-
-PARALLEL_COUNT=4 CLOUD_QA_API_KEY=$QA_API_KEY yarn cypress:run:qa:serverless:parallel; status=$?; yarn junit:merge || :; exit $status
+ts-node .buildkite/scripts/pipelines/pull_request/pipeline.ts
