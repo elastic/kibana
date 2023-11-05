6fe8fe9c3a5cbe040e266bba7435be90c5844750
jpdjere <jpdjeredjian@gmail.com>
2023-11-06 00:30:13 +0100

    Test rule actions cypress

--- x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_actions/rule_actions.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_actions/rule_actions.cy.ts
@@ -28,53 +28,48 @@ import { visit } from '../../../tasks/navigation';
 import { openRuleManagementPageViaBreadcrumbs } from '../../../tasks/rules_management';
 import { CREATE_RULE_URL } from '../../../urls/navigation';
 
-// TODO: https://github.com/elastic/kibana/issues/161539
-describe(
-  'Rule actions during detection rule creation',
-  { tags: ['@ess', '@serverless', '@brokenInServerless'] },
-  () => {
-    const indexConnector = getIndexConnector();
+describe('Rule actions during detection rule creation', { tags: ['@ess', '@serverless'] }, () => {
+  const indexConnector = getIndexConnector();
 
-    before(() => {
-      cleanKibana();
+  before(() => {
+    cleanKibana();
+  });
+
+  beforeEach(() => {
+    login();
+    deleteAlertsAndRules();
+    deleteConnectors();
+    deleteIndex(indexConnector.index);
+    deleteDataView(indexConnector.index);
+  });
+
+  const rule = getSimpleCustomQueryRule();
+  const actions = { connectors: [indexConnector] };
+  const index = actions.connectors[0].index;
+  const initialNumberOfDocuments = 0;
+  const expectedJson = JSON.parse(actions.connectors[0].document);
+
+  it('Indexes a new document after the index action is triggered', function () {
+    visit(CREATE_RULE_URL);
+    fillDefineCustomRuleAndContinue(rule);
+    fillAboutRuleAndContinue(rule);
+    fillScheduleRuleAndContinue(rule);
+    fillRuleAction(actions);
+    createAndEnableRule();
+    openRuleManagementPageViaBreadcrumbs();
+
+    goToRuleDetailsOf(rule.name);
+
+    /* When the rule is executed, the action is triggered. We wait for the new document to be indexed */
+    waitForNewDocumentToBeIndexed(index, initialNumberOfDocuments);
+
+    /* We assert that the new indexed document is the one set on the index action */
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('ELASTICSEARCH_URL')}/${index}/_search`,
+      headers: { 'kbn-xsrf': 'cypress-creds', 'x-elastic-internal-origin': 'security-solution' },
+    }).then((response) => {
+      expect(response.body.hits.hits[0]._source).to.deep.equal(expectedJson);
     });
-
-    beforeEach(() => {
-      login();
-      deleteAlertsAndRules();
-      deleteConnectors();
-      deleteIndex(indexConnector.index);
-      deleteDataView(indexConnector.index);
-    });
-
-    const rule = getSimpleCustomQueryRule();
-    const actions = { connectors: [indexConnector] };
-    const index = actions.connectors[0].index;
-    const initialNumberOfDocuments = 0;
-    const expectedJson = JSON.parse(actions.connectors[0].document);
-
-    it('Indexes a new document after the index action is triggered', function () {
-      visit(CREATE_RULE_URL);
-      fillDefineCustomRuleAndContinue(rule);
-      fillAboutRuleAndContinue(rule);
-      fillScheduleRuleAndContinue(rule);
-      fillRuleAction(actions);
-      createAndEnableRule();
-      openRuleManagementPageViaBreadcrumbs();
-
-      goToRuleDetailsOf(rule.name);
-
-      /* When the rule is executed, the action is triggered. We wait for the new document to be indexed */
-      waitForNewDocumentToBeIndexed(index, initialNumberOfDocuments);
-
-      /* We assert that the new indexed document is the one set on the index action */
-      cy.request({
-        method: 'GET',
-        url: `${Cypress.env('ELASTICSEARCH_URL')}/${index}/_search`,
-        headers: { 'kbn-xsrf': 'cypress-creds', 'x-elastic-internal-origin': 'security-solution' },
-      }).then((response) => {
-        expect(response.body.hits.hits[0]._source).to.deep.equal(expectedJson);
-      });
-    });
-  }
-);
+  });
+});
