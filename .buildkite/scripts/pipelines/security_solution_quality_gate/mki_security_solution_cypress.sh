46649ed4e64e915fa4269807464eb94653a62e22
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-11-06 17:28:31 +0200

    Split all tests to non investigations/explore

--- .buildkite/scripts/pipelines/security_solution_quality_gate/mki_security_solution_cypress.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/mki_security_solution_cypress.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+if [ -z "$1" ]
+  then
+    echo "No target script from the package.json file, is supplied"
+    exit 1
+fi
+
 source .buildkite/scripts/common/util.sh
 source .buildkite/scripts/steps/functional/common_cypress.sh
 .buildkite/scripts/bootstrap.sh
@@ -10,10 +16,9 @@ export JOB=kibana-security-solution-chrome
 
 buildkite-agent meta-data set "${BUILDKITE_JOB_ID}_is_test_execution_step" "true"
 
-echo "--- Serverless Security Solution Cypress for Second Quality Gate"
 cd x-pack/test/security_solution_cypress
 set +e
 
 QA_API_KEY=$(retry 5 5 vault read -field=qa_api_key secret/kibana-issues/dev/security-solution-qg-enc-key)
 
-CLOUD_QA_API_KEY=$QA_API_KEY yarn cypress:run:qa:serverless:parallel; status=$?; yarn junit:merge || :; exit $status
+CLOUD_QA_API_KEY=$QA_API_KEY yarn $1; status=$?; yarn junit:merge || :; exit $status
