3b17006a00a4783ef3a7af0336a7bdd4e2922f98
Davis Plumlee <davis.plumlee@elastic.co>
2023-11-06 13:48:20 -0500

    addresses comments

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/logic/coverage_overview/build_coverage_overview_dashboard_model.test.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/logic/coverage_overview/build_coverage_overview_dashboard_model.test.ts
@@ -42,10 +42,14 @@ describe('buildCoverageOverviewDashboardModel', () => {
         T002: ['test-rule-1'],
         'T001.001': ['test-rule-1'],
       },
-      unmapped_rule_ids: [],
+      unmapped_rule_ids: ['test-rule-2'],
       rules_data: {
         'test-rule-1': {
-          name: 'Test rule',
+          name: 'Test rule 1',
+          activity: CoverageOverviewRuleActivity.Enabled,
+        },
+        'test-rule-2': {
+          name: 'Test rule 2',
           activity: CoverageOverviewRuleActivity.Enabled,
         },
       },
@@ -55,8 +59,8 @@ describe('buildCoverageOverviewDashboardModel', () => {
 
     expect(model).toEqual({
       metrics: {
-        totalEnabledRulesCount: 1,
-        totalRulesCount: 1,
+        totalEnabledRulesCount: 2,
+        totalRulesCount: 2,
       },
       mitreTactics: [
         {
@@ -65,7 +69,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
           enabledRules: [
             {
               id: 'test-rule-1',
-              name: 'Test rule',
+              name: 'Test rule 1',
             },
           ],
           id: 'TA001',
@@ -78,7 +82,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
               enabledRules: [
                 {
                   id: 'test-rule-1',
-                  name: 'Test rule',
+                  name: 'Test rule 1',
                 },
               ],
               id: 'T001',
@@ -91,7 +95,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
                   enabledRules: [
                     {
                       id: 'test-rule-1',
-                      name: 'Test rule',
+                      name: 'Test rule 1',
                     },
                   ],
                   id: 'T001.001',
@@ -114,7 +118,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
               enabledRules: [
                 {
                   id: 'test-rule-1',
-                  name: 'Test rule',
+                  name: 'Test rule 1',
                 },
               ],
               id: 'T002',
@@ -130,7 +134,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
           enabledRules: [
             {
               id: 'test-rule-1',
-              name: 'Test rule',
+              name: 'Test rule 1',
             },
           ],
           id: 'TA002',
@@ -143,7 +147,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
               enabledRules: [
                 {
                   id: 'test-rule-1',
-                  name: 'Test rule',
+                  name: 'Test rule 1',
                 },
               ],
               id: 'T002',
@@ -157,7 +161,7 @@ describe('buildCoverageOverviewDashboardModel', () => {
       unmappedRules: {
         availableRules: [],
         disabledRules: [],
-        enabledRules: [],
+        enabledRules: [expect.objectContaining({ id: 'test-rule-2' })],
       },
     });
   });
@@ -184,110 +188,101 @@ describe('buildCoverageOverviewDashboardModel', () => {
 
     const model = await buildCoverageOverviewDashboardModel(mockApiResponse);
 
-    expect(model).toEqual({
-      metrics: {
-        totalEnabledRulesCount: 2,
-        totalRulesCount: 2,
+    expect(model.mitreTactics).toEqual([
+      expect.objectContaining({
+        id: 'TA001',
+        enabledRules: [
+          expect.objectContaining({ id: 'test-rule-1' }),
+          expect.objectContaining({ id: 'test-rule-2' }),
+        ],
+        techniques: [
+          expect.objectContaining({ id: 'T001', enabledRules: [] }),
+          expect.objectContaining({
+            id: 'T002',
+            enabledRules: [
+              expect.objectContaining({ id: 'test-rule-1' }),
+              expect.objectContaining({ id: 'test-rule-2' }),
+            ],
+          }),
+        ],
+      }),
+      expect.objectContaining({
+        id: 'TA002',
+        enabledRules: [expect.objectContaining({ id: 'test-rule-2' })],
+        techniques: [
+          expect.objectContaining({
+            id: 'T002',
+            enabledRules: [expect.objectContaining({ id: 'test-rule-2' })],
+          }),
+        ],
+      }),
+    ]);
+  });
+
+  it('maps unmapped rules', async () => {
+    const mockApiResponse: CoverageOverviewResponse = {
+      coverage: {
+        TA001: ['test-rule-1'],
+        T002: ['test-rule-1'],
       },
-      mitreTactics: [
-        {
-          availableRules: [],
-          disabledRules: [],
-          enabledRules: [
-            {
-              id: 'test-rule-1',
-              name: 'Test rule',
-            },
-            {
-              id: 'test-rule-2',
-              name: 'Test rule 2',
-            },
-          ],
+      unmapped_rule_ids: ['test-rule-2'],
+      rules_data: {
+        'test-rule-1': {
+          name: 'Test rule 1',
+          activity: CoverageOverviewRuleActivity.Enabled,
+        },
+        'test-rule-2': {
+          name: 'Test rule 2',
+          activity: CoverageOverviewRuleActivity.Enabled,
+        },
+      },
+    };
+
+    const model = await buildCoverageOverviewDashboardModel(mockApiResponse);
+
+    expect(model.unmappedRules).toEqual({
+      availableRules: [],
+      disabledRules: [],
+      enabledRules: [expect.objectContaining({ id: 'test-rule-2' })],
+    });
+    expect(model.mitreTactics).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
           id: 'TA001',
-          name: 'Tactic 1',
-          reference: 'https://some-link/TA001',
-          techniques: [
-            {
-              availableRules: [],
-              disabledRules: [],
-              enabledRules: [],
-              id: 'T001',
-              name: 'Technique 1',
-              reference: 'https://some-link/T001',
-              subtechniques: [
-                {
-                  availableRules: [],
-                  disabledRules: [],
-                  enabledRules: [],
-                  id: 'T001.001',
-                  name: 'Subtechnique 1',
-                  reference: 'https://some-link/T001/001',
-                },
-                {
-                  availableRules: [],
-                  disabledRules: [],
-                  enabledRules: [],
-                  id: 'T001.002',
-                  name: 'Subtechnique 2',
-                  reference: 'https://some-link/T001/002',
-                },
-              ],
-            },
-            {
-              availableRules: [],
-              disabledRules: [],
-              enabledRules: [
-                {
-                  id: 'test-rule-1',
-                  name: 'Test rule',
-                },
-                {
-                  id: 'test-rule-2',
-                  name: 'Test rule 2',
-                },
-              ],
-              id: 'T002',
-              name: 'Technique 2',
-              reference: 'https://some-link/T002',
-              subtechniques: [],
-            },
-          ],
-        },
-        {
-          availableRules: [],
-          disabledRules: [],
-          enabledRules: [
-            {
-              id: 'test-rule-2',
-              name: 'Test rule 2',
-            },
-          ],
-          id: 'TA002',
-          name: 'Tactic 2',
-          reference: 'https://some-link/TA002',
-          techniques: [
-            {
-              availableRules: [],
-              disabledRules: [],
-              enabledRules: [
-                {
-                  id: 'test-rule-2',
-                  name: 'Test rule 2',
-                },
-              ],
-              id: 'T002',
-              name: 'Technique 2',
-              reference: 'https://some-link/T002',
-              subtechniques: [],
-            },
-          ],
-        },
-      ],
-      unmappedRules: {
-        availableRules: [],
-        disabledRules: [],
-        enabledRules: [],
+          enabledRules: expect.not.arrayContaining(['test-rule-2']),
+        }),
+      ])
+    );
+  });
+
+  it('maps metrics fields', async () => {
+    const mockApiResponse: CoverageOverviewResponse = {
+      coverage: {
+        TA001: ['test-rule-1'],
+        T002: ['test-rule-1'],
       },
+      unmapped_rule_ids: ['test-rule-2'],
+      rules_data: {
+        'test-rule-1': {
+          name: 'Test rule 1',
+          activity: CoverageOverviewRuleActivity.Enabled,
+        },
+        'test-rule-2': {
+          name: 'Test rule 2',
+          activity: CoverageOverviewRuleActivity.Enabled,
+        },
+        'test-rule-3': {
+          name: 'Test rule 3',
+          activity: CoverageOverviewRuleActivity.Disabled,
+        },
+      },
+    };
+
+    const model = await buildCoverageOverviewDashboardModel(mockApiResponse);
+
+    expect(model.metrics).toEqual({
+      totalEnabledRulesCount: 2,
+      totalRulesCount: 3,
     });
   });
 });
