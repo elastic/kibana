4735a9d16fcf36085797118de793965c127805a2
Timothy Sullivan <tsullivan@elastic.co>
2023-11-07 08:46:33 -0700

    Remove validations of CSV file for test

--- x-pack/test/functional/apps/discover/reporting.ts
+++ x-pack/test/functional/apps/discover/reporting.ts
@@ -175,8 +175,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         // match file length, the beginning and the end of the csv file contents
         const { text: csvFile } = await getReport();
         expect(csvFile.length).to.be(4826973);
-        expectSnapshot(csvFile.slice(0, 5000)).toMatch();
-        expectSnapshot(csvFile.slice(-5000)).toMatch();
+
+        /*
+         * NOTE: This test can not check against the snapshot. Data ingestion from
+         * the ES archive is concurrent and dynamic, and affects the search output
+         */
       });
 
       it('generate a report using ES|QL', async () => {
