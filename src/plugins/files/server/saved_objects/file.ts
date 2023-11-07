c1289c3d00625d1dfa3c00adda0bd7dff0617476
Catherine Liu <catherine.liu@elastic.co>
2023-11-07 08:20:28 -0800

    Disable import/export

--- src/plugins/files/server/saved_objects/file.ts
+++ src/plugins/files/server/saved_objects/file.ts
@@ -69,7 +69,7 @@ export const fileObjectType: SavedObjectsType<FileMetadata> = {
     getTitle(obj) {
       return `${obj.attributes.name}.${obj.attributes.extension}`;
     },
-    importableAndExportable: true,
+    importableAndExportable: false,
   },
   mappings: {
     dynamic: false,
