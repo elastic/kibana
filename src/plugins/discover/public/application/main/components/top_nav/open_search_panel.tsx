61291a57c5f89f0c2784ab708b6a8eecaaf0e8de
Nick Peihl <nick.peihl@elastic.co>
2023-11-07 15:32:28 -0500

    Add Spaces column to Open Search Panel

--- src/plugins/discover/public/application/main/components/top_nav/open_search_panel.tsx
+++ src/plugins/discover/public/application/main/components/top_nav/open_search_panel.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import {
@@ -19,7 +19,9 @@ import {
   EuiFlyoutBody,
   EuiTitle,
 } from '@elastic/eui';
+import { SpacesContextProps } from '@kbn/spaces-plugin/public';
 import { SavedObjectFinder } from '@kbn/saved-objects-finder-plugin/public';
+import { PLUGIN_ID } from '../../../../../common';
 import { useDiscoverServices } from '../../../../hooks/use_discover_services';
 
 const SEARCH_OBJECT_TYPE = 'search';
@@ -29,75 +31,85 @@ interface OpenSearchPanelProps {
   onOpenSavedSearch: (id: string) => void;
 }
 
+const getEmptyFunctionComponent: React.FC<SpacesContextProps> = ({ children }) => <>{children}</>;
+
 export function OpenSearchPanel(props: OpenSearchPanelProps) {
-  const { addBasePath, capabilities, savedObjectsTagging, contentClient, uiSettings } =
+  const { addBasePath, capabilities, savedObjectsTagging, contentClient, spaces, uiSettings } =
     useDiscoverServices();
   const hasSavedObjectPermission =
     capabilities.savedObjectsManagement?.edit || capabilities.savedObjectsManagement?.delete;
 
+  const SpacesContextWrapper = useMemo(
+    () => (spaces ? spaces.ui.components.getSpacesContextProvider : getEmptyFunctionComponent),
+    [spaces]
+  );
+
   return (
-    <EuiFlyout ownFocus onClose={props.onClose} data-test-subj="loadSearchForm">
-      <EuiFlyoutHeader hasBorder>
-        <EuiTitle size="m">
-          <h2>
-            <FormattedMessage
-              id="discover.topNav.openSearchPanel.openSearchTitle"
-              defaultMessage="Open search"
-            />
-          </h2>
-        </EuiTitle>
-      </EuiFlyoutHeader>
-      <EuiFlyoutBody>
-        <SavedObjectFinder
-          services={{
-            savedObjectsTagging,
-            contentClient,
-            uiSettings,
-          }}
-          noItemsMessage={
-            <FormattedMessage
-              id="discover.topNav.openSearchPanel.noSearchesFoundDescription"
-              defaultMessage="No matching searches found."
-            />
-          }
-          savedObjectMetaData={[
-            {
-              type: SEARCH_OBJECT_TYPE,
-              getIconForSavedObject: () => 'discoverApp',
-              name: i18n.translate('discover.savedSearch.savedObjectName', {
-                defaultMessage: 'Saved search',
-              }),
-            },
-          ]}
-          onChoose={(id) => {
-            props.onOpenSavedSearch(id);
-            props.onClose();
-          }}
-          showFilter={true}
-        />
-      </EuiFlyoutBody>
-      {hasSavedObjectPermission && (
-        <EuiFlyoutFooter>
-          <EuiFlexGroup justifyContent="flexEnd">
-            <EuiFlexItem grow={false}>
-              {/* eslint-disable-next-line @elastic/eui/href-or-on-click */}
-              <EuiButton
-                fill
-                onClick={props.onClose}
-                data-test-subj="manageSearchesBtn"
-                href={addBasePath(
-                  `/app/management/kibana/objects?initialQuery=type:(${SEARCH_OBJECT_TYPE})`
-                )}
-              >
-                <FormattedMessage
-                  id="discover.topNav.openSearchPanel.manageSearchesButtonLabel"
-                  defaultMessage="Manage searches"
-                />
-              </EuiButton>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        </EuiFlyoutFooter>
-      )}
-    </EuiFlyout>
+    <SpacesContextWrapper feature={PLUGIN_ID}>
+      <EuiFlyout ownFocus onClose={props.onClose} data-test-subj="loadSearchForm">
+        <EuiFlyoutHeader hasBorder>
+          <EuiTitle size="m">
+            <h2>
+              <FormattedMessage
+                id="discover.topNav.openSearchPanel.openSearchTitle"
+                defaultMessage="Open search"
+              />
+            </h2>
+          </EuiTitle>
+        </EuiFlyoutHeader>
+        <EuiFlyoutBody>
+          <SavedObjectFinder
+            services={{
+              savedObjectsTagging,
+              contentClient,
+              uiSettings,
+              spaces,
+            }}
+            noItemsMessage={
+              <FormattedMessage
+                id="discover.topNav.openSearchPanel.noSearchesFoundDescription"
+                defaultMessage="No matching searches found."
+              />
+            }
+            savedObjectMetaData={[
+              {
+                type: SEARCH_OBJECT_TYPE,
+                getIconForSavedObject: () => 'discoverApp',
+                name: i18n.translate('discover.savedSearch.savedObjectName', {
+                  defaultMessage: 'Saved search',
+                }),
+              },
+            ]}
+            onChoose={(id) => {
+              props.onOpenSavedSearch(id);
+              props.onClose();
+            }}
+            showFilter={true}
+          />
+        </EuiFlyoutBody>
+        {hasSavedObjectPermission && (
+          <EuiFlyoutFooter>
+            <EuiFlexGroup justifyContent="flexEnd">
+              <EuiFlexItem grow={false}>
+                {/* eslint-disable-next-line @elastic/eui/href-or-on-click */}
+                <EuiButton
+                  fill
+                  onClick={props.onClose}
+                  data-test-subj="manageSearchesBtn"
+                  href={addBasePath(
+                    `/app/management/kibana/objects?initialQuery=type:(${SEARCH_OBJECT_TYPE})`
+                  )}
+                >
+                  <FormattedMessage
+                    id="discover.topNav.openSearchPanel.manageSearchesButtonLabel"
+                    defaultMessage="Manage searches"
+                  />
+                </EuiButton>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlyoutFooter>
+        )}
+      </EuiFlyout>
+    </SpacesContextWrapper>
   );
 }
