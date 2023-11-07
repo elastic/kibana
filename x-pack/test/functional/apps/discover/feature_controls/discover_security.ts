4099571b9f0ae693e92c9a415ad8d560dde2c3bb
Matt Kime <matt@mattki.me>
2023-11-07 12:15:27 -0600

    fix test

--- x-pack/test/functional/apps/discover/feature_controls/discover_security.ts
+++ x-pack/test/functional/apps/discover/feature_controls/discover_security.ts
@@ -448,8 +448,9 @@ export default function (ctx: FtrProviderContext) {
       it('allows to access only via a permitted index alias', async () => {
         await globalNav.badgeExistsOrFail('Read only');
 
-        await PageObjects.settings.refreshDataViewFieldList('logstash-*');
-        await PageObjects.common.navigateToApp('discover');
+        // this doesn't work since the user doesn't have access
+        // await PageObjects.settings.refreshDataViewFieldList('logstash-*');
+        // await PageObjects.common.navigateToApp('discover');
 
         // can't access logstash index directly
         await PageObjects.discover.selectIndexPattern('logstash-*');
