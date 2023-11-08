086f2fd5659a74703c3c86a3e939364386679157
Maxim Kholod <maxim.kholod@elastic.co>
2023-11-08 16:35:39 +0100

    revert size change

--- x-pack/plugins/cloud_security_posture/public/pages/configurations/findings_flyout/findings_flyout.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/configurations/findings_flyout/findings_flyout.tsx
@@ -100,7 +100,7 @@ export const CisKubernetesIcons = ({
   <EuiFlexGroup gutterSize="s" alignItems="center">
     <EuiFlexItem grow={false}>
       <EuiToolTip content="Center for Internet Security">
-        <EuiIcon type={cisLogoIcon} size="xl" />
+        <EuiIcon type={cisLogoIcon} size="xxl" />
       </EuiToolTip>
     </EuiFlexItem>
     <EuiFlexItem grow={false}>
