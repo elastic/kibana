296cbd6e0213a50e452e1499bc81dc9685a93c61
Nikita Khristinin <nkhristinin@gmail.com>
2023-11-08 13:12:07 +0100

    Fix path

--- x-pack/test/detection_engine_api_integration/security_and_spaces/group10/risk_engine/init_and_status_apis.ts
+++ x-pack/test/detection_engine_api_integration/security_and_spaces/group10/risk_engine/init_and_status_apis.ts
@@ -6,7 +6,7 @@
  */
 
 import expect from '@kbn/expect';
-import { riskEngineConfigurationTypeName } from '@kbn/security-solution-plugin/server/lib/entity_analytics/risk_enginesaved_object';
+import { riskEngineConfigurationTypeName } from '@kbn/security-solution-plugin/server/lib/entity_analytics/risk_engine/saved_object';
 import { FtrProviderContext } from '../../../common/ftr_provider_context';
 import {
   cleanRiskEngineConfig,
