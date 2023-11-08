21b637672a460307060a9da6b7f90fb8c9dd5bf1
Nick Peihl <nick.peihl@elastic.co>
2023-11-08 11:48:01 -0500

    Make Tags shareable across Spaces

--- x-pack/plugins/saved_objects_tagging/public/ui_api/index.ts
+++ x-pack/plugins/saved_objects_tagging/public/ui_api/index.ts
@@ -6,6 +6,7 @@
  */
 
 import type { NotificationsStart, OverlayStart, ThemeServiceStart } from '@kbn/core/public';
+import { SpacesApi } from '@kbn/spaces-plugin/public';
 import { SavedObjectsTaggingApiUi } from '@kbn/saved-objects-tagging-oss-plugin/public';
 import { TagsCapabilities } from '../../common';
 import { ITagsCache, ITagInternalClient } from '../services';
@@ -30,6 +31,7 @@ interface GetUiApiOptions {
   cache: ITagsCache;
   client: ITagInternalClient;
   notifications: NotificationsStart;
+  spaces?: SpacesApi;
 }
 
 export const getUiApi = ({
@@ -39,6 +41,7 @@ export const getUiApi = ({
   overlays,
   theme,
   notifications,
+  spaces,
 }: GetUiApiOptions): SavedObjectsTaggingApiUi => {
   const components = getComponents({
     cache,
