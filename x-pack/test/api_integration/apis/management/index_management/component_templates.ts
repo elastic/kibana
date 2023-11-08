f222d04e0bbcd21da81c7529365d2ac35ca25a81
Ignacio Rivas <rivasign@gmail.com>
2023-11-08 14:58:57 +0100

    Update api integration test

--- x-pack/test/api_integration/apis/management/index_management/component_templates.ts
+++ x-pack/test/api_integration/apis/management/index_management/component_templates.ts
@@ -146,6 +146,10 @@ export default function ({ getService }: FtrProviderContext) {
                   },
                 },
               },
+              lifecycle: {
+                enabled: true,
+                data_retention: '2d',
+              },
             },
             _meta: {
               description: 'set number of shards to one',
