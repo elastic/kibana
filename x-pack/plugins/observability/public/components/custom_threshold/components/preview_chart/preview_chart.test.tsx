b2fdbc5123c852cf66c0f6eebfa7fcc6f9b81be5
Faisal Kanout <f.kanout@gmail.com>
2023-11-09 15:18:04 +0100

    Add test to no data view/case

---
+++ x-pack/plugins/observability/public/components/custom_threshold/components/preview_chart/preview_chart.test.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { mountWithIntl, nextTick } from '@kbn/test-jest-helpers';
+import { MetricExpression } from '../../types';
+import { Aggregators, Comparator } from '../../../../../common/custom_threshold_rule/types';
+import PreviewChart from './preview_chart';
+import { DataView } from '@kbn/data-views-plugin/common';
+import { useKibana } from '../../../../utils/kibana_react';
+import { kibanaStartMock } from '../../../../utils/kibana_react.mock';
+
+jest.mock('../../../../utils/kibana_react');
+
+const useKibanaMock = useKibana as jest.Mock;
+
+const mockKibana = () => {
+  useKibanaMock.mockReturnValue({
+    ...kibanaStartMock.startContract(),
+  });
+};
+
+describe('Preview chart', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockKibana();
+  });
+  async function setup(expression: MetricExpression) {
+    const wrapper = mountWithIntl(
+      <PreviewChart
+        metricExpression={expression}
+        dataView={{} as DataView}
+        filterQuery={''}
+        groupBy={[]}
+        error={{}}
+      />
+    );
+
+    const update = async () =>
+      await act(async () => {
+        await nextTick();
+        wrapper.update();
+      });
+
+    await update();
+
+    return { wrapper, update };
+  }
+
+  it('should display no data message', async () => {
+    const expression: MetricExpression = {
+      aggType: Aggregators.AVERAGE,
+      timeSize: 1,
+      timeUnit: 'm',
+      sourceId: 'default',
+      threshold: [1],
+      comparator: Comparator.GT_OR_EQ,
+    };
+    const { wrapper } = await setup(expression);
+    expect(wrapper.find('[data-test-subj="thresholdRuleNoChartData"]').exists()).toBeTruthy();
+  });
+});
