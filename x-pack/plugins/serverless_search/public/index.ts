862e41e977aa978516e50f94895d0a619f3a3db6
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2023-11-10 09:15:09 +0100

    Revert changes in `/public/`

--- x-pack/plugins/serverless_search/public/index.ts
+++ x-pack/plugins/serverless_search/public/index.ts
@@ -4,11 +4,11 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { ServerlessSearchPlugin } from './plugin';
 
 // This exports static code and TypeScript types,
 // as well as, Kibana Platform `plugin()` initializer.
-export async function plugin() {
-  const { ServerlessSearchPlugin } = await import('./plugin');
+export function plugin() {
   return new ServerlessSearchPlugin();
 }
 
