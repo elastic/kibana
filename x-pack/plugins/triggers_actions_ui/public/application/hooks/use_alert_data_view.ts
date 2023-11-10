3a2ec850e9022127df5661ecbba15cd6b9a7929b
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2023-11-10 13:12:00 -0500

    fix Alert search bar

--- x-pack/plugins/triggers_actions_ui/public/application/hooks/use_alert_data_view.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/hooks/use_alert_data_view.ts
@@ -5,72 +5,171 @@
  * 2.0.
  */
 
+import { i18n } from '@kbn/i18n';
 import { DataView, FieldSpec } from '@kbn/data-views-plugin/common';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
 import { BASE_RAC_ALERTS_API_PATH } from '@kbn/rule-registry-plugin/common';
-import type { ValidFeatureId } from '@kbn/rule-data-utils';
-import useAsync from 'react-use/lib/useAsync';
-import { useMemo } from 'react';
+import { AlertConsumers, ValidFeatureId } from '@kbn/rule-data-utils';
+import { useEffect, useMemo, useState } from 'react';
+import { HttpSetup } from '@kbn/core/public';
+import { useQuery } from '@tanstack/react-query';
 import { TriggersAndActionsUiServices } from '../..';
 
 export interface UserAlertDataView {
-  value?: DataView[];
+  dataview?: DataView[];
   loading: boolean;
-  error?: Error;
+}
+
+async function fetchIndexNames({
+  http,
+  features,
+}: {
+  http: HttpSetup;
+  features: string;
+}): Promise<string[]> {
+  const { index_name: indexNamesStr } = await http.get<{ index_name: string[] }>(
+    `${BASE_RAC_ALERTS_API_PATH}/index`,
+    {
+      query: { features },
+    }
+  );
+  return indexNamesStr;
+}
+
+async function fetchAlertFields({
+  http,
+  featureIds,
+}: {
+  http: HttpSetup;
+  featureIds: ValidFeatureId[];
+}): Promise<FieldSpec[]> {
+  const { fields: alertFields } = await http.get<{ fields: FieldSpec[] }>(
+    `${BASE_RAC_ALERTS_API_PATH}/browser_fields`,
+    {
+      query: { featureIds },
+    }
+  );
+  return alertFields;
 }
 
 export function useAlertDataView(featureIds: ValidFeatureId[]): UserAlertDataView {
-  const { http } = useKibana<TriggersAndActionsUiServices>().services;
+  const {
+    http,
+    data: dataService,
+    notifications: { toasts },
+  } = useKibana<TriggersAndActionsUiServices>().services;
+  const [dataview, setDataview] = useState<DataView[] | undefined>(undefined);
   const features = featureIds.sort().join(',');
+  const isOnlySecurity =
+    featureIds.length === 1 && (featureIds as AlertConsumers[]).includes(AlertConsumers.SIEM);
 
-  const indexNames = useAsync(async () => {
-    const { index_name: indexNamesStr } = await http.get<{ index_name: string[] }>(
-      `${BASE_RAC_ALERTS_API_PATH}/index`,
-      {
-        query: { features },
-      }
-    );
-
-    return indexNamesStr;
-  }, [features]);
-
-  const fields = useAsync(async () => {
-    const { fields: alertFields } = await http.get<{ fields: FieldSpec[] }>(
-      `${BASE_RAC_ALERTS_API_PATH}/browser_fields`,
-      {
-        query: { featureIds },
-      }
-    );
-    return alertFields;
-  }, [features]);
-
-  const dataview = useMemo(
-    () =>
-      !fields.loading &&
-      !indexNames.loading &&
-      fields.error === undefined &&
-      indexNames.error === undefined
-        ? ([
-            {
-              title: (indexNames.value ?? []).join(','),
-              fieldFormatMap: {},
-              fields: (fields.value ?? [])?.map((field) => {
-                return {
-                  ...field,
-                  ...(field.esTypes && field.esTypes.includes('flattened')
-                    ? { type: 'string' }
-                    : {}),
-                };
-              }),
-            },
-          ] as unknown as DataView[])
-        : undefined,
-    [fields, indexNames]
-  );
-
-  return {
-    value: dataview,
-    loading: fields.loading || indexNames.loading,
-    error: fields.error ? fields.error : indexNames.error,
+  const queryIndexNameFn = () => {
+    return fetchIndexNames({ http, features });
   };
+
+  const queryAlertFieldsFn = () => {
+    return fetchAlertFields({ http, featureIds });
+  };
+
+  const onErrorFn = () => {
+    toasts.addDanger(
+      i18n.translate('xpack.triggersActionsUI.alertSearchBar.useAlertDataMessage', {
+        defaultMessage: 'Unable to load alert data view',
+      })
+    );
+  };
+
+  const {
+    data: indexNames,
+    isSuccess: isIndexNameSuccess,
+    isFetching: isIndexNameFetching,
+    isInitialLoading: isIndexNameInitialLoading,
+    isLoading: isIndexNameLoading,
+  } = useQuery({
+    queryKey: ['loadAlertIndexNames'],
+    queryFn: queryIndexNameFn,
+    onError: onErrorFn,
+    refetchOnWindowFocus: false,
+    enabled: featureIds.length > 0,
+  });
+
+  const {
+    data: alertFields,
+    isSuccess: isalertFieldsSuccess,
+    isFetching: isalertFieldsFetching,
+    isInitialLoading: isalertFieldsInitialLoading,
+    isLoading: isalertFieldsLoading,
+  } = useQuery({
+    queryKey: ['loadAlertFields'],
+    queryFn: queryAlertFieldsFn,
+    onError: onErrorFn,
+    refetchOnWindowFocus: false,
+    enabled: !isOnlySecurity,
+  });
+
+  useEffect(() => {
+    async function createDataView() {
+      const localDataview = await dataService.dataViews.create({
+        title: (indexNames ?? []).join(','),
+        allowNoIndex: true,
+      });
+      setDataview([localDataview]);
+    }
+
+    if (isOnlySecurity && isIndexNameSuccess && !isIndexNameFetching) {
+      createDataView();
+    }
+  }, [dataService.dataViews, indexNames, isIndexNameFetching, isIndexNameSuccess, isOnlySecurity]);
+
+  useEffect(() => {
+    if (
+      !isOnlySecurity &&
+      isalertFieldsSuccess &&
+      isIndexNameSuccess &&
+      !isIndexNameFetching &&
+      !isalertFieldsFetching
+    ) {
+      setDataview([
+        {
+          title: (indexNames ?? []).join(','),
+          fieldFormatMap: {},
+          fields: (alertFields ?? [])?.map((field) => {
+            return {
+              ...field,
+              ...(field.esTypes && field.esTypes.includes('flattened') ? { type: 'string' } : {}),
+            };
+          }),
+        },
+      ] as unknown as DataView[]);
+    }
+  }, [
+    alertFields,
+    dataService.dataViews,
+    indexNames,
+    isIndexNameFetching,
+    isIndexNameSuccess,
+    isOnlySecurity,
+    isalertFieldsFetching,
+    isalertFieldsSuccess,
+  ]);
+
+  return useMemo(
+    () => ({
+      dataview,
+      loading: isOnlySecurity
+        ? isIndexNameInitialLoading || isIndexNameLoading
+        : isIndexNameInitialLoading ||
+          isIndexNameLoading ||
+          isalertFieldsInitialLoading ||
+          isalertFieldsLoading,
+    }),
+    [
+      dataview,
+      isIndexNameInitialLoading,
+      isIndexNameLoading,
+      isOnlySecurity,
+      isalertFieldsInitialLoading,
+      isalertFieldsLoading,
+    ]
+  );
 }
--- x-pack/plugins/triggers_actions_ui/public/application/hooks/use_load_rule_types_query.ts
+++ x-pack/plugins/triggers_actions_ui/public/application/hooks/use_load_rule_types_query.ts
@@ -13,6 +13,7 @@ import { RuleType, RuleTypeIndex } from '../../types';
 
 interface UseLoadRuleTypesQueryProps {
   filteredRuleTypes: string[];
+  enabled?: boolean;
 }
 
 const getFilteredIndex = (data: Array<RuleType<string, string>>, filteredRuleTypes: string[]) => {
@@ -32,7 +33,7 @@ const getFilteredIndex = (data: Array<RuleType<string, string>>, filteredRuleTyp
 };
 
 export const useLoadRuleTypesQuery = (props: UseLoadRuleTypesQueryProps) => {
-  const { filteredRuleTypes } = props;
+  const { filteredRuleTypes, enabled = true } = props;
   const {
     http,
     notifications: { toasts },
@@ -55,6 +56,7 @@ export const useLoadRuleTypesQuery = (props: UseLoadRuleTypesQueryProps) => {
     queryFn,
     onError: onErrorFn,
     refetchOnWindowFocus: false,
+    enabled,
   });
 
   const filteredIndex = data ? getFilteredIndex(data, filteredRuleTypes) : new Map();
