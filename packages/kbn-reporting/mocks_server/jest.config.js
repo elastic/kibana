f98b3379bec355f165fc4c094f09d09998c7eb7e
Timothy Sullivan <tsullivan@elastic.co>
2023-11-09 22:41:08 -0700

    fix snake_case

--- packages/kbn-reporting/mocks_server/jest.config.js
+++ packages/kbn-reporting/mocks_server/jest.config.js
@@ -9,5 +9,5 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../..',
-  roots: ['<rootDir>/packages/kbn-reporting/mocks-server'],
+  roots: ['<rootDir>/packages/kbn-reporting/mocks_server'],
 };
