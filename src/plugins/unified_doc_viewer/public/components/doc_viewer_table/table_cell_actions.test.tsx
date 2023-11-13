668b7cd1299b7c078fd02140915279f9157970fa
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2023-11-13 14:53:45 +0200

    Cleanup

--- src/plugins/unified_doc_viewer/public/components/doc_viewer_table/table_cell_actions.test.tsx
+++ src/plugins/unified_doc_viewer/public/components/doc_viewer_table/table_cell_actions.test.tsx
@@ -7,7 +7,6 @@
  */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import { TableActions } from './table_cell_actions';
 import { DataViewField } from '@kbn/data-views-plugin/common';
 
