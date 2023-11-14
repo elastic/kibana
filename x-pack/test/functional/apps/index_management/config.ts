eb1112905bc0960af61d40054b3c09109e62a809
Chris Cowan <chris@elastic.co>
2023-11-14 14:20:58 -0700

    Fixing tests... again

--- x-pack/test/functional/apps/index_management/config.ts
+++ x-pack/test/functional/apps/index_management/config.ts
@@ -9,9 +9,20 @@ import { FtrConfigProviderContext } from '@kbn/test';
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const functionalConfig = await readConfigFile(require.resolve('../../config.base.js'));
+  const configBase = functionalConfig.getAll();
 
   return {
-    ...functionalConfig.getAll(),
+    ...configBase,
+    kbnTestServer: {
+      ...configBase.kbnTestServer,
+      serverArgs: [
+        ...configBase.kbnTestServer.serverArgs,
+        // Removing the Observability plugin since the SLO feature now installs
+        // an enrich policy by default. The tests expect to start with a fresh
+        // install without enrich policies.
+        '--xpack.observability.enabled=false',
+      ],
+    },
     testFiles: [require.resolve('.')],
   };
 }
