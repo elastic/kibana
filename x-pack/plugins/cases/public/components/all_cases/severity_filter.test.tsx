11e87092c40531ac96c35913df5e54c995fe12dd
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-11-14 08:15:15 +0100

    fix test

--- x-pack/plugins/cases/public/components/all_cases/severity_filter.test.tsx
+++ x-pack/plugins/cases/public/components/all_cases/severity_filter.test.tsx
@@ -49,7 +49,7 @@ describe('Severity form field', () => {
     userEvent.click(screen.getByRole('option', { name: 'high' }));
 
     await waitFor(() => {
-      expect(onChange).toHaveBeenCalledWith({ filterId: 'severity', options: ['high'] });
+      expect(onChange).toHaveBeenCalledWith({ filterId: 'severity', selectedOptionKeys: ['high'] });
     });
   });
 });
