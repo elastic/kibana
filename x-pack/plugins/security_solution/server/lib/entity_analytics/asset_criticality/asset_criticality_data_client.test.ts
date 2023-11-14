1bb6f181d65134de04a25f63d7c9aad5ccd6f15e
Nikita Khristinin <nkhristinin@gmail.com>
2023-11-14 13:00:32 +0100

    add test

---
+++ x-pack/plugins/security_solution/server/lib/entity_analytics/asset_criticality/asset_criticality_data_client.test.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { loggingSystemMock, elasticsearchServiceMock } from '@kbn/core/server/mocks';
+import { AssetCriticalityDataClient } from './asset_criticality_data_client';
+
+import { createIndex } from '../utils/create_index';
+
+jest.mock('../utils/create_index', () => ({
+  createIndex: jest.fn(),
+}));
+
+describe('AssetCriticalityDataClient', () => {
+  const esClientInternal = elasticsearchServiceMock.createScopedClusterClient().asInternalUser;
+  const logger = loggingSystemMock.createLogger();
+  describe('init', () => {
+    it('should call create index', async () => {
+      const assetCriticalityDataClient = new AssetCriticalityDataClient({
+        esClient: esClientInternal,
+        logger,
+        namespace: 'default',
+      });
+
+      await assetCriticalityDataClient.init();
+
+      expect(createIndex).toHaveBeenCalledWith({
+        esClient: esClientInternal,
+        logger,
+        options: {
+          index: '.asset-criticality.asset-criticality-default',
+          mappings: {
+            dynamic: 'strict',
+            properties: {
+              id_field: {
+                type: 'keyword',
+              },
+              id_value: {
+                type: 'keyword',
+              },
+              criticality: {
+                type: 'keyword',
+              },
+              '@timestamp': {
+                type: 'date',
+                ignore_malformed: false,
+              },
+              updated_at: {
+                type: 'date',
+              },
+            },
+          },
+        },
+      });
+    });
+  });
+});
