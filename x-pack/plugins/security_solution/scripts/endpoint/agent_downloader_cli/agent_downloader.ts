1cdc29e94934ca72da12b1272b57d984375b6b57
David Sanchez Soler <david.sanchezsoler@elastic.co>
2023-11-15 14:54:03 +0100

    Adds logger to cli script

--- x-pack/plugins/security_solution/scripts/endpoint/agent_downloader_cli/agent_downloader.ts
+++ x-pack/plugins/security_solution/scripts/endpoint/agent_downloader_cli/agent_downloader.ts
@@ -7,16 +7,17 @@
 
 import { ok } from 'assert';
 import type { RunFn } from '@kbn/dev-cli-runner';
+import type { ToolingLog } from '@kbn/tooling-log';
 import { getAgentDownloadUrl, getAgentFileName } from '../common/fleet_services';
 import { downloadAndStoreAgent } from '../common/agent_downloads_service';
 
-const downloadAndStoreElasticAgent = async (version: string, closestMatch: boolean) => {
-  const downloadUrlResponse = await getAgentDownloadUrl(version, closestMatch);
-  let fileVersion: string = version;
-  if (downloadUrlResponse.fileName.match('SNAPSHOT')) {
-    fileVersion = `${version}-SNAPSHOT`;
-  }
-  const fileNameNoExtension = getAgentFileName(fileVersion);
+const downloadAndStoreElasticAgent = async (
+  version: string,
+  closestMatch: boolean,
+  log: ToolingLog
+) => {
+  const downloadUrlResponse = await getAgentDownloadUrl(version, closestMatch, log);
+  const fileNameNoExtension = getAgentFileName(version);
   const agentFile = `${fileNameNoExtension}.tar.gz`;
   await downloadAndStoreAgent(downloadUrlResponse.url, agentFile);
 };
@@ -25,6 +26,7 @@ export const agentDownloaderRunner: RunFn = async (cliContext) => {
   ok(cliContext.flags.version, 'version argument is required');
   await downloadAndStoreElasticAgent(
     cliContext.flags.version as string,
-    cliContext.flags.closestMatch as boolean
+    cliContext.flags.closestMatch as boolean,
+    cliContext.log
   );
 };
