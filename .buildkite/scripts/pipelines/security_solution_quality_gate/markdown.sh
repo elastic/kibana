88f57fe3858ebc4dfb729747e6119dd93f68cc38
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-11-15 21:48:51 +0200

    Fixed back the pipeline yml file

--- .buildkite/scripts/pipelines/security_solution_quality_gate/markdown.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/markdown.sh
@@ -16,14 +16,12 @@ if [ "$KIBANA_OVERRIDE_FLAG" = "1" ]; then
     version=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.version"')
     markdown_text="""
         # Kibana Container Metadata
-        * Build Date        : $build_date 
-        * GH commit hash    : $vcs_ref 
-        * Github Repo       : $vcs_url 
-        * Version           : $version
+        - Build Date            : $build_date 
+        - Github Commit Hash    : $vcs_ref 
+        - Github Repo           : $vcs_url 
+        - Version               : $version
     """
-
     echo "${markdown_text//[*\\_]/\\&}" | buildkite-agent annotate --style "info"
-    # echo -e "\`\`\`term\n*Kibana Container Metadata*\n- build-date: $build_date\n- vcs-ref: $vcs_ref\n- vcs-url: $vcs_url\n- version: $version\n\`\`\`" | buildkite-agent annotate --style "info"
 else
     echo "KIBANA_OVERRIDE_FLAG is $KIBANA_OVERRIDE_FLAG"
 fi 
