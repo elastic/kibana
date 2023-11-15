3f0ea0f92dd3c841d182d6582ababbb66ededab5
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2023-11-15 19:27:52 +0100

    [json layout] use json representation of `meta` when available (#171310)
    ## Summary
    
    Use the json representation (`toJSON`) of the log's `meta` when merging
    the message and the meta, if possible.
    
    (cherry picked from commit 3d6ac9d46a64d7f14acd75523706aa2bd33c2aef)
    
    # Conflicts:
    #	src/core/server/elasticsearch/integration_tests/error_logging.test.ts
    #	src/core/server/logging/layouts/json_layout.test.ts

--- src/core/server/logging/layouts/json_layout.ts
+++ src/core/server/logging/layouts/json_layout.ts
@@ -55,7 +55,13 @@ export class JsonLayout implements Layout {
         pid: record.pid,
       },
     };
-    const output = record.meta ? merge({ ...record.meta }, log) : log;
+
+    let output = log;
+    if (record.meta) {
+      // @ts-expect-error toJSON not defined on `LogMeta`, but some structured meta can have it defined
+      const serializedMeta = record.meta.toJSON ? record.meta.toJSON() : { ...record.meta };
+      output = merge(serializedMeta, log);
+    }
 
     return JSON.stringify(output);
   }
