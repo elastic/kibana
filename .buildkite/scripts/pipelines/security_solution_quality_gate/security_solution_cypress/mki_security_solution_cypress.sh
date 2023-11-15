af98b94d1a8cf8db762573c96677c9ed0fc23daa
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-11-15 16:57:30 +0200

    Attemt to enable parameterization

--- .buildkite/scripts/pipelines/security_solution_quality_gate/security_solution_cypress/mki_security_solution_cypress.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/security_solution_cypress/mki_security_solution_cypress.sh
@@ -19,19 +19,20 @@ buildkite-agent meta-data set "${BUILDKITE_JOB_ID}_is_test_execution_step" "true
 cd x-pack/test/security_solution_cypress
 set +e
 
-echo "Environment"
-echo $(buildkite-agent meta-data get environment)
-echo "Kibana override"
-echo $(buildkite-agent meta-data get kibana_override)
+if [ -z "$KIBANA_LATEST" ]; then
+    KIBANA_OVERRIDE_FLAG=$KIBANA_LATEST
+    echo "OVERRIDING KIBANA FROM TRIGGER : $KIBANA_OVERRIDE_FLAG"
+else
+    KIBANA_OVERRIDE_FLAG=$(buildkite-agent meta-data get kibana_override --default 0)
+    echo "OVERRIDING KIBANA FROM CONFIG_PANE : $KIBANA_OVERRIDE_FLAG"
+fi
 
-
-docker pull docker.elastic.co/kibana-ci/kibana-serverless:latest
-echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.build-date"')
-echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.vcs-ref"')
-echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.vcs-url"')
-echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.version"')
+# docker pull docker.elastic.co/kibana-ci/kibana-serverless:latest
+# echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.build-date"')
+# echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.vcs-ref"')
+# echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.vcs-url"')
+# echo $(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.version"')
 
 # QA_API_KEY=$(retry 5 5 vault read -field=qa_api_key secret/kibana-issues/dev/security-solution-qg-enc-key)
 
-# OVERRIDE_KIBANA=1 CLOUD_QA_API_KEY=$QA_API_KEY yarn cypress:run:qa:serverless:explore
-# CLOUD_QA_API_KEY=$QA_API_KEY yarn $1; status=$?; yarn junit:merge || :; exit $status
+# OVERRIDE_KIBANA=$KIBANA_OVERRIDE_FLAG CLOUD_QA_API_KEY=$QA_API_KEY yarn $1; status=$?; yarn junit:merge || :; exit $status
