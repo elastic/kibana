a28849a83e7815baca9d4c070705be77bca0012b
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2023-11-15 16:27:46 -0500

    [7.17] Add custom `inspect` representation for ES client's errors (#171304) (#171326)
    # Backport
    
    This will backport the following commits from `main` to `7.17`:
    - [Add custom `inspect` representation for ES client's errors
    (#171304)](https://github.com/elastic/kibana/pull/171304)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Pierre
    Gayvallet","email":"pierre.gayvallet@elastic.co"},"sourceCommit":{"committedDate":"2023-11-15T16:20:45Z","message":"Add
    custom `inspect` representation for ES client's errors
    (#171304)\n\nFollow-up of
    #171018","sha":"11eb73ee1d734ecdcbbbdbae63b859b5b3795a4d","branchLabelMapping":{"^v8.12.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","backport:prev-minor","backport:prev-MAJOR","v8.12.0"],"number":171304,"url":"https://github.com/elastic/kibana/pull/171304","mergeCommit":{"message":"Add
    custom `inspect` representation for ES client's errors
    (#171304)\n\nFollow-up of
    #171018","sha":"11eb73ee1d734ecdcbbbdbae63b859b5b3795a4d"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v8.12.0","labelRegex":"^v8.12.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/171304","number":171304,"mergeCommit":{"message":"Add
    custom `inspect` representation for ES client's errors
    (#171304)\n\nFollow-up of
    #171018","sha":"11eb73ee1d734ecdcbbbdbae63b859b5b3795a4d"}}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: Pierre Gayvallet <pierre.gayvallet@elastic.co>

--- src/core/server/elasticsearch/integration_tests/errors.test.ts
+++ src/core/server/elasticsearch/integration_tests/errors.test.ts
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 
+import { inspect } from 'util';
 import {
   createTestServers,
   type TestElasticsearchUtils,
@@ -44,6 +45,23 @@ describe('elasticsearch clients errors', () => {
       const stringifiedError = JSON.stringify(e);
       expect(stringifiedError).not.toContain('headers');
       expect(stringifiedError).not.toContain('authorization');
+    }
+  });
+
+  it('has the proper inspect representation', async () => {
+    const esClient = kibanaServer.coreStart.elasticsearch.client.asInternalUser;
+
+    try {
+      await esClient.search({
+        index: '.kibana',
+        // @ts-expect-error yes this is invalid
+        query: { someInvalidQuery: { foo: 'bar' } },
+      });
+      expect('should have thrown').toEqual('but it did not');
+    } catch (e) {
+      const inspectedError = inspect(e);
+      expect(inspectedError).not.toContain('headers');
+      expect(inspectedError).not.toContain('authorization');
     }
   });
 });
