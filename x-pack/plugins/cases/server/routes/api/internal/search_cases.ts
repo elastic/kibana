d4b5113a12ce5effca1ebf136828922242c5274b
Janki Salvi <117571355+js-jankisalvi@users.noreply.github.com>
2023-11-15 10:59:01 +0100

    [Cases] Create internal API to filter cases by custom fields (#170297)
    ## Summary
    
    Implements https://github.com/elastic/kibana/issues/167740
    
    This PR 
    - creates a new `search_cases` internal route with POST
    - allows to filter by customFields (currently only toggle custom fields
    can be filtered)
    - adds unit and integration tests 
    
    ### example of search cases body: 
    `{
       "customFields": {
           "custom_field_key": [true] 
       }
    }`
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ### For maintainers
    
    - [x] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/cases/server/routes/api/internal/search_cases.ts
+++ x-pack/plugins/cases/server/routes/api/internal/search_cases.ts
@@ -4,21 +4,29 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import Boom from '@hapi/boom';
 
-import { CASES_URL } from '../../../../common/constants';
+import { CASES_INTERNAL_URL } from '../../../../common/constants';
 import { createCaseError } from '../../../common/error';
 import { createCasesRoute } from '../create_cases_route';
 import type { caseApiV1 } from '../../../../common/types/api';
 
-export const findCaseRoute = createCasesRoute({
-  method: 'get',
-  path: `${CASES_URL}/_find`,
+export const searchCasesRoute = createCasesRoute({
+  method: 'post',
+  path: `${CASES_INTERNAL_URL}/_search`,
   handler: async ({ context, request, response }) => {
     try {
       const caseContext = await context.cases;
       const casesClient = await caseContext.getCasesClient();
 
-      const options = request.query as caseApiV1.CasesFindRequest;
+      const options = request.body as caseApiV1.CasesSearchRequest;
+
+      /**
+       * throw error if request body does not have owner
+       */
+      if (!Object.hasOwn(options, 'owner')) {
+        throw Boom.badRequest('Owner is required.');
+      }
 
       const res: caseApiV1.CasesFindResponse = await casesClient.cases.search({ ...options });
 
