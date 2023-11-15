352373e698d8a6f5a79731b91b64e19c3cd97ad3
Ido Cohen <90558359+CohenIdo@users.noreply.github.com>
2023-11-15 12:55:59 +0200

    fix cloud defend error log issue

--- x-pack/plugins/security_solution_serverless/server/cloud_security/cloud_security_metering_task.ts
+++ x-pack/plugins/security_solution_serverless/server/cloud_security/cloud_security_metering_task.ts
@@ -205,12 +205,15 @@ const indexHasDataInDateRange = async (
   cloudSecuritySolution: CloudSecuritySolutions,
   searchFrom: Date
 ) => {
-  const response = await esClient.search({
-    index: METERING_CONFIGS[cloudSecuritySolution].index,
-    size: 1,
-    _source: false,
-    query: getSearchQueryByCloudSecuritySolution(cloudSecuritySolution, searchFrom),
-  });
+  const response = await esClient.search(
+    {
+      index: METERING_CONFIGS[cloudSecuritySolution].index,
+      size: 1,
+      _source: false,
+      query: getSearchQueryByCloudSecuritySolution(cloudSecuritySolution, searchFrom),
+    },
+    { ignore: [404] }
+  );
 
   return response.hits.hits.length > 0;
 };
