44fa330086eee20e462296845650ea111d8b76fc
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-11-16 23:59:35 +0100

    [ftr] split alerting api-integration group2 (#171105)
    ## Summary
    
    Splitting some of the long running Alerting FTR configs:
    
    | Config Path | Runtime |
    | ------------- | ------------- |
    |
    x-pack/test/alerting_api_integration/security_and_spaces/group2/config.ts
    | 36m 55s |
    |
    x-pack/test/alerting_api_integration/security_and_spaces/group2/config_non_dedicated_task_runner.ts
    | 36m 58s |
    
    <img width="1576" alt="image"
    src="https://github.com/elastic/kibana/assets/10977896/c73374c6-d769-4536-a277-5a2a38b83fa4">
    
    After split:
    
    | Config Path | Runtime |
    | ------------- | ------------- |
    |
    x-pack/test/alerting_api_integration/security_and_spaces/group2/config.ts
    | 22m 59s |
    |
    x-pack/test/alerting_api_integration/security_and_spaces/group2/config_non_dedicated_task_runner.ts
    | 23m 15s |
    |
    x-pack/test/alerting_api_integration/security_and_spaces/group4/config.ts
    | 22m 26s |
    |
    x-pack/test/alerting_api_integration/security_and_spaces/group4/config_non_dedicated_task_runner.ts
    | 21m 36s |

--- x-pack/test/alerting_api_integration/security_and_spaces/group4/tests/alerting/index.ts
+++ x-pack/test/alerting_api_integration/security_and_spaces/group4/tests/alerting/index.ts
@@ -11,18 +11,6 @@ import { setupSpacesAndUsers, tearDown } from '../../../setup';
 // eslint-disable-next-line import/no-default-export
 export default function alertingTests({ loadTestFile, getService }: FtrProviderContext) {
   describe('Alerts', () => {
-    describe('legacy alerts', function () {
-      before(async () => {
-        await setupSpacesAndUsers(getService);
-      });
-
-      after(async () => {
-        await tearDown(getService);
-      });
-
-      loadTestFile(require.resolve('./rbac_legacy'));
-    });
-
     describe('alerts', () => {
       before(async () => {
         await setupSpacesAndUsers(getService);
@@ -32,12 +20,17 @@ export default function alertingTests({ loadTestFile, getService }: FtrProviderC
         await tearDown(getService);
       });
 
-      loadTestFile(require.resolve('./mute_all'));
-      loadTestFile(require.resolve('./mute_instance'));
-      loadTestFile(require.resolve('./unmute_all'));
-      loadTestFile(require.resolve('./unmute_instance'));
-      loadTestFile(require.resolve('./update'));
-      loadTestFile(require.resolve('./update_api_key'));
+      loadTestFile(require.resolve('./alerts'));
+      loadTestFile(require.resolve('./event_log'));
+      loadTestFile(require.resolve('./mustache_templates'));
+      loadTestFile(require.resolve('./health'));
+      loadTestFile(require.resolve('./excluded'));
+      loadTestFile(require.resolve('./snooze'));
+      loadTestFile(require.resolve('./unsnooze'));
+      loadTestFile(require.resolve('./global_execution_log'));
+      loadTestFile(require.resolve('./get_global_execution_kpi'));
+      loadTestFile(require.resolve('./get_action_error_log'));
+      loadTestFile(require.resolve('./get_rule_execution_kpi'));
     });
   });
 }
