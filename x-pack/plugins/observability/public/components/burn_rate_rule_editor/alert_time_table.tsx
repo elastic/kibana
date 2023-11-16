f5932606bdb6e7498160aac0a0dd165df064181a
Chris Cowan <chris@elastic.co>
2023-11-16 09:58:37 -0700

    [SLO] Making budget consumed the default mode for burn rate rule

---
+++ x-pack/plugins/observability/public/components/burn_rate_rule_editor/alert_time_table.tsx
@@ -0,0 +1,91 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiBasicTable, EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
+import { SLOResponse } from '@kbn/slo-schema';
+import React from 'react';
+import { i18n } from '@kbn/i18n';
+import numeral from '@elastic/numeral';
+import { WindowSchema } from '../../typings';
+import { toDuration, toMinutes } from '../../utils/slo/duration';
+
+interface AlertTimeTableProps {
+  slo: SLOResponse;
+  windows: WindowSchema[];
+}
+
+const ERROR_RATES = [0.01, 0.1, 0.2, 0.5, 1];
+
+export function AlertTimeTable({ windows, slo }: AlertTimeTableProps) {
+  const rows = ERROR_RATES.map((rate) => {
+    const windowTimes = windows.reduce((acc, windowDef, index) => {
+      const windowInMinutes = toMinutes(
+        toDuration(`${windowDef.longWindow.value}${windowDef.longWindow.unit}`)
+      );
+      const timeInMinutes = Math.round(
+        ((1 - slo.objective.target) / rate) * windowInMinutes * windowDef.burnRateThreshold
+      );
+      return {
+        ...acc,
+        [`column_${index + 1}`]: timeInMinutes < windowInMinutes ? timeInMinutes : null,
+      };
+    }, {});
+    return { rate, ...windowTimes };
+  });
+
+  const columns = [
+    {
+      field: 'rate',
+      name: i18n.translate('xpack.observability.slo.rules.timeTable.rateColumnLabel', {
+        defaultMessage: 'Error rate',
+      }),
+      render: (rate: number) => numeral(rate).format('0%'),
+    },
+    ...windows.map((windowDef, index) => ({
+      field: `column_${index + 1}`,
+      name: `${windowDef.longWindow.value}h @ ${windowDef.burnRateThreshold}x`,
+      render: (time: number | null) =>
+        time
+          ? i18n.translate('xpack.observability.slo.rules.timeTable.minuteLabel', {
+              defaultMessage: '{time} mins.',
+              values: { time },
+            })
+          : i18n.translate('xpack.observability.slo.rules.timeTable.neverLabel', {
+              defaultMessage: 'Never',
+            }),
+    })),
+  ];
+  return (
+    <>
+      <EuiTitle size="xs">
+        <h5>
+          {i18n.translate('xpack.observability.slo.rules.timeTable.title', {
+            defaultMessage: 'How long will it take for the alert to fire?',
+          })}
+        </h5>
+      </EuiTitle>
+      <EuiSpacer size="s" />
+      <EuiText size="s">
+        <p>
+          {i18n.translate('xpack.observability.slo.rules.timeTable.description', {
+            defaultMessage:
+              'The table below lists the error rates and approximately how long it would take to receive your first alert with the current configuration.',
+          })}
+        </p>
+      </EuiText>
+      <EuiSpacer size="m" />
+      <EuiBasicTable
+        tableCaption={i18n.translate('xpack.observability.slo.rules.tableCaption', {
+          defaultMessage: 'Alerting time table',
+        })}
+        items={rows}
+        columns={columns}
+      />
+      <EuiSpacer size="l" />
+    </>
+  );
+}
