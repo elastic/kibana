054734b9f27370804ac15cae437c3039232f5ab0
Kevin Delemme <kevin.delemme@elastic.co>
2023-11-15 20:27:21 -0500

    feat(slo): add reset api (#170473)

--- x-pack/plugins/observability/server/assets/constants.ts
+++ x-pack/plugins/observability/server/assets/constants.ts
@@ -4,6 +4,8 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+export const SLO_MODEL_VERSION = 2;
 export const SLO_RESOURCES_VERSION = 3;
 export const SLO_SUMMARY_TRANSFORMS_VERSION = 4;
 
