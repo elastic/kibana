5986b2ce2b7186b0193293bce4f3797dd01e9283
Marta Bondyra <marta.bondyra@elastic.co>
2023-11-16 15:32:44 +0100

    widen US dataview

--- x-pack/plugins/lens/public/shared_components/dataview_picker/dataview_picker.tsx
+++ x-pack/plugins/lens/public/shared_components/dataview_picker/dataview_picker.tsx
@@ -12,6 +12,24 @@ import { DataViewsList } from '@kbn/unified-search-plugin/public';
 import { type IndexPatternRef } from '../../types';
 import { type ChangeIndexPatternTriggerProps, TriggerButton } from './trigger';
 
+const MIN_POPOVER_CONTENT_WIDTH = 280;
+const MIN_POPOVER_CHAR_COUNT = 25;
+const MAX_POPOVER_CONTENT_WIDTH = 600;
+const MAX_POPOVER_CHAR_COUNT = 60;
+
+const AVERAGE_CHAR_WIDTH = 7;
+
+function getPanelMinWidth(labelLength: number) {
+  if (labelLength > MAX_POPOVER_CHAR_COUNT) {
+    return MAX_POPOVER_CONTENT_WIDTH;
+  }
+  if (labelLength > MIN_POPOVER_CHAR_COUNT) {
+    const overflownChars = labelLength - MIN_POPOVER_CHAR_COUNT;
+    return MIN_POPOVER_CONTENT_WIDTH + overflownChars * AVERAGE_CHAR_WIDTH;
+  }
+  return MIN_POPOVER_CONTENT_WIDTH;
+}
+
 export function ChangeIndexPattern({
   indexPatternRefs,
   isMissingCurrent,
@@ -28,6 +46,11 @@ export function ChangeIndexPattern({
   selectableProps?: EuiSelectableProps;
 }) {
   const [isPopoverOpen, setPopoverIsOpen] = useState(false);
+
+  const maxLabelLength = indexPatternRefs.reduce((acc, curr) => {
+    const dataViewName = curr.name || curr.id;
+    return acc > dataViewName.length ? acc : dataViewName.length;
+  }, 20);
 
   return (
     <>
@@ -49,7 +72,11 @@ export function ChangeIndexPattern({
         panelPaddingSize="none"
         ownFocus
       >
-        <div>
+        <div
+          css={{
+            width: getPanelMinWidth(maxLabelLength),
+          }}
+        >
           <EuiPopoverTitle paddingSize="s">
             {i18n.translate('xpack.lens.indexPattern.changeDataViewTitle', {
               defaultMessage: 'Data view',
