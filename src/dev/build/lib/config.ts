16cfcb45130b83cc0403e39cd1e275b8e3255587
Tiago Costa <tiago.costa@elastic.co>
2023-11-17 01:59:54 +0000

    fix(NA): types

--- src/dev/build/lib/config.ts
+++ src/dev/build/lib/config.ts
@@ -251,6 +251,6 @@ export class Config {
   }
 
   getDistPluginsFromRepo() {
-    return getPackages(this.repoRoot).filter((p) => this.pluginFilter(p) && !p.isDevOnly());
+    return getPackages(this.repoRoot).filter((p) => !p.isDevOnly() && this.pluginFilter(p));
   }
 }
