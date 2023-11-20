80dd64f8302369846eb4f866c3d6dc8c27c64f3d
mgiota <panagiota.mitsopoulou@elastic.co>
2023-11-20 12:46:53 +0100

    remove onBrushEnd

--- x-pack/plugins/observability/public/embeddable/slo/alerts/components/slo_alerts_summary.tsx
+++ x-pack/plugins/observability/public/embeddable/slo/alerts/components/slo_alerts_summary.tsx
@@ -85,7 +85,6 @@ export function SloAlertsSummary({ slos, deps, timeRange }: Props) {
   const chartProps = {
     theme: charts.theme.useChartsTheme(),
     baseTheme: charts.theme.useChartsBaseTheme(),
-    onBrushEnd: () => {}, // TODO
   };
   return (
     <AlertSummaryWidget
