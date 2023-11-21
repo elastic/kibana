7ad5e4474d6b0ae2f97b0f0fa83c03bb9e0e1275
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-11-21 15:48:16 +0200

    Added docker imgae build in quality gate

---
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/docker_image.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -euo pipefail
+
+.buildkite/scripts/bootstrap.sh
+
+source .buildkite/scripts/steps/artifacts/env.sh
+
+GIT_ABBREV_COMMIT=${BUILDKITE_COMMIT:0:12}
+if [[ "${BUILDKITE_PULL_REQUEST:-false}" == "false" ]]; then
+  KIBANA_IMAGE_TAG="git-$GIT_ABBREV_COMMIT"
+else
+  KIBANA_IMAGE_TAG="pr-$BUILDKITE_PULL_REQUEST-$GIT_ABBREV_COMMIT"
+fi
+
+KIBANA_BASE_IMAGE="docker.elastic.co/kibana-ci/kibana-serverless"
+export KIBANA_IMAGE="$KIBANA_BASE_IMAGE:$KIBANA_IMAGE_TAG"
+
+echo "--- Verify manifest does not already exist"
+echo "$KIBANA_DOCKER_PASSWORD" | docker login -u "$KIBANA_DOCKER_USERNAME" --password-stdin docker.elastic.co
+trap 'docker logout docker.elastic.co' EXIT
+
+echo "Checking manifest for $KIBANA_IMAGE"
+if docker manifest inspect $KIBANA_IMAGE &> /dev/null; then
+  echo "Manifest already exists, exiting"
+  exit 1
+fi
+
+echo "--- Build images"
+node scripts/build \
+  --debug \
+  --release \
+  --docker-cross-compile \
+  --docker-images \
+  --docker-namespace="kibana-ci" \
+  --docker-tag="$KIBANA_IMAGE_TAG" \
+  --skip-docker-ubuntu \
+  --skip-docker-ubi \
+  --skip-docker-cloud \
+  --skip-docker-contexts \
+  --skip-cdn-assets
+
+echo "--- Tag images"
+docker rmi "$KIBANA_IMAGE"
+docker load < "target/kibana-serverless-$BASE_VERSION-docker-image.tar.gz"
+docker tag "$KIBANA_IMAGE" "$KIBANA_IMAGE-amd64"
+
+docker rmi "$KIBANA_IMAGE"
+docker load < "target/kibana-serverless-$BASE_VERSION-docker-image-aarch64.tar.gz"
+docker tag "$KIBANA_IMAGE" "$KIBANA_IMAGE-arm64"
+
+echo "--- Push images"
+docker image push "$KIBANA_IMAGE-arm64"
+docker image push "$KIBANA_IMAGE-amd64"
+
+echo "--- Create and push manifests"
+docker manifest create \
+  "$KIBANA_IMAGE" \
+  --amend "$KIBANA_IMAGE-arm64" \
+  --amend "$KIBANA_IMAGE-amd64"
+docker manifest push "$KIBANA_IMAGE"
+
+if [[ "$BUILDKITE_BRANCH" == "$KIBANA_BASE_BRANCH" ]] && [[ "${BUILDKITE_PULL_REQUEST:-false}" == "false" ]]; then
+  docker manifest create \
+    "$KIBANA_BASE_IMAGE:latest" \
+    --amend "$KIBANA_IMAGE-arm64" \
+    --amend "$KIBANA_IMAGE-amd64"
+  docker manifest push "$KIBANA_BASE_IMAGE:latest"
+fi
+
+docker logout docker.elastic.co
+
+cat << EOF | buildkite-agent annotate --style "info" --context image
+  ### Serverless Images
+
+  Manifest: \`$KIBANA_IMAGE\`
+
+  AMD64: \`$KIBANA_IMAGE-amd64\`
+
+  ARM64: \`$KIBANA_IMAGE-arm64\`
+EOF
+
+if [[ "${BUILDKITE_PULL_REQUEST:-false}" != "false" ]]; then
+  buildkite-agent meta-data set pr_comment:build_serverless:head "* Kibana Serverless Image: \`$KIBANA_IMAGE\`"
+  buildkite-agent meta-data set pr_comment:early_comment_job_id "$BUILDKITE_JOB_ID"
+fi
+
+echo "--- Build dependencies report"
+node scripts/licenses_csv_report "--csv=target/dependencies-$GIT_ABBREV_COMMIT.csv"
+
+echo "--- Upload artifacts"
+cd target
+buildkite-agent artifact upload "kibana-$BASE_VERSION-linux-x86_64.tar.gz"
+buildkite-agent artifact upload "kibana-$BASE_VERSION-linux-aarch64.tar.gz"
+buildkite-agent artifact upload "kibana-$BASE_VERSION-docker-image.tar.gz"
+buildkite-agent artifact upload "kibana-$BASE_VERSION-docker-image-aarch64.tar.gz"
+buildkite-agent artifact upload "dependencies-$GIT_ABBREV_COMMIT.csv"
+cd -
