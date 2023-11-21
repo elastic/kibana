7e0729ff9b6632270e1aa52dd474c456c0de44f0
Paul Tavares <paul.tavares@elastic.co>
2023-11-21 16:39:06 -0500

    Revert changes to PLI product types

--- x-pack/plugins/security_solution/public/management/cypress/e2e/serverless/endpoint_list_with_security_essentials.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/serverless/endpoint_list_with_security_essentials.cy.ts
@@ -21,10 +21,7 @@ describe(
     tags: ['@serverless'],
     env: {
       ftrConfig: {
-        productTypes: [
-          { product_line: 'security', product_tier: 'essentials' },
-          { product_line: 'endpoint', product_tier: 'essentials' },
-        ],
+        productTypes: [{ product_line: 'security', product_tier: 'essentials' }],
       },
     },
   },
