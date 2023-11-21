d64f38464c2466e9a9cb5f275e0917a5cbe1b1b7
Brad White <Ikuni17@users.noreply.github.com>
2023-11-21 14:59:39 -0700

    Remove CI Composite Storybook (#171258)
    ## Summary
    Closes #160803
    
    This PR removes the `CI Composite` story because it has been broken
    since at least ac23dce29f07f0539eb8d3f0968ac36441729207 (and possibly
    since b862a6c1810f5f4c14f8f657411f781b152dec0d). The functionality is
    covered by the generated `index.html` in
    https://github.com/elastic/kibana/blob/dda4498fee84708143ce4671af880db785f9e652/.buildkite/scripts/steps/storybooks/build_and_upload.ts#L105-L120
    
    To fix the composite story requires generating `stories.json` for every
    storybook, which requires migrating the repo off the deprecated
    `storiesOf` API. That task is quite extensive and would be better
    handled alongside an upgrade to Storybook 7.x
    
    (cherry picked from commit 1919c87b90c4f489c2027d71293631d89034f40f)
    
    # Conflicts:
    #	.buildkite/scripts/steps/storybooks/build_and_upload.js
    #	.ci/.storybook/main.js
    #	src/dev/storybook/aliases.ts

--- .buildkite/scripts/steps/storybooks/build_and_upload.js
+++ .buildkite/scripts/steps/storybooks/build_and_upload.js
@@ -15,7 +15,6 @@ const STORYBOOKS = [
   'apm',
   'canvas',
   'codeeditor',
-  'ci_composite',
   'custom_integrations',
   'url_template_editor',
   'dashboard',
@@ -74,14 +73,12 @@ const upload = () => {
     console.log('--- Generating Storybooks HTML');
 
     process.chdir(path.join('.', 'built_assets', 'storybook'));
-    fs.renameSync('ci_composite', 'composite');
 
     const storybooks = execSync(`ls -1d */`)
       .toString()
       .trim()
       .split('\n')
-      .map((path) => path.replace('/', ''))
-      .filter((path) => path !== 'composite');
+      .map((filePath) => filePath.replace('/', ''));
 
     const listHtml = storybooks
       .map((storybook) => `<li><a href="${STORYBOOK_BASE_URL}/${storybook}">${storybook}</a></li>`)
@@ -91,8 +88,6 @@ const upload = () => {
       <html>
         <body>
           <h1>Storybooks</h1>
-          <p><a href="${STORYBOOK_BASE_URL}/composite">Composite Storybook</a></p>
-          <h2>All</h2>
           <ul>
             ${listHtml}
           </ul>
