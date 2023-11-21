891e15da800669aea8d92ed8cc835e2f417ec52b
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-11-21 15:33:11 +0200

    Building docker image

--- .buildkite/scripts/pipelines/security_solution_quality_gate/upload_runtime_info.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/upload_runtime_info.sh
@@ -4,16 +4,14 @@ else
     KIBANA_OVERRIDE_FLAG=$KIBANA_LATEST
 fi
 
-echo "KIBANA override is: $KIBANA_OVERRIDE_FLAG"
-
 if [ "$KIBANA_OVERRIDE_FLAG" = "1" ]; then
     echo "KIBANA_OVERRIDE_FLAG is equal to 1"
     echo "$KIBANA_DOCKER_PASSWORD" | docker login -u "$KIBANA_DOCKER_USERNAME" --password-stdin docker.elastic.co
-    docker pull docker.elastic.co/kibana-ci/kibana-serverless:latest
-    build_date=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.build-date"')
-    vcs_ref=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.vcs-ref"')
-    vcs_url=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.vcs-url"')
-    version=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:latest | jq -r '.[0].Config.Labels."org.label-schema.version"')
+    docker pull docker.elastic.co/kibana-ci/kibana-serverless:git-${BUILDKITE_COMMIT:0:12}
+    build_date=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:git-${BUILDKITE_COMMIT:0:12} | jq -r '.[0].Config.Labels."org.label-schema.build-date"')
+    vcs_ref=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:git-${BUILDKITE_COMMIT:0:12} | jq -r '.[0].Config.Labels."org.label-schema.vcs-ref"')
+    vcs_url=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:git-${BUILDKITE_COMMIT:0:12} | jq -r '.[0].Config.Labels."org.label-schema.vcs-url"')
+    version=$(docker inspect docker.elastic.co/kibana-ci/kibana-serverless:git-${BUILDKITE_COMMIT:0:12} | jq -r '.[0].Config.Labels."org.label-schema.version"')
     markdown_text="""
         # Kibana Container Metadata
         - Build Date            : $build_date 
