df79e97cc29f78460e09b279599ea6cb9dd17c1a
Alex Szabo <alex.szabo@elastic.co>
2023-11-21 12:22:47 +0100

    chore: add more useful links

--- .buildkite/scripts/serverless/create_deploy_tag/info_sections/useful_links.ts
+++ .buildkite/scripts/serverless/create_deploy_tag/info_sections/useful_links.ts
@@ -6,8 +6,22 @@
  * Side Public License, v 1.
  */
 
-export function getCommitDiffLink(from: string, to: string) {
-  return `<a href="https://github.com/elastic/kibana/compare/${from}...${to}">Commits contained in deploy</a>`;
+function link(text: string, url: string) {
+  return `<a href="${url}">${text}</a>`;
+}
+
+function getDashboardForNonProd(commit: string) {
+  return link(
+    'GPCTL Deployment Status dashboard for nonprod',
+    `https://overview.qa.cld.elstc.co/app/dashboards#/view/serverless-tooling-gpctl-deployment-status?_g=(refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&service-name=kibana&_a=(controlGroupInput:(chainingSystem:HIERARCHICAL,controlStyle:oneLine,ignoreParentSettings:(ignoreFilters:!f,ignoreQuery:!f,ignoreTimerange:!f,ignoreValidations:!f),panels:('18201b8e-3aae-4459-947d-21e007b6a3a5':(explicitInput:(dataViewId:'serverless.logs-*',enhancements:(),fieldName:commit-hash,id:'18201b8e-3aae-4459-947d-21e007b6a3a5',selectedOptions:!('${commit}'),title:commit-hash),grow:!t,order:1,type:optionsListControl,width:medium),'41060e65-ce4c-414e-b8cf-492ccb19245f':(explicitInput:(dataViewId:'serverless.logs-*',enhancements:(),fieldName:service-name,id:'41060e65-ce4c-414e-b8cf-492ccb19245f',selectedOptions:!(kibana),title:service-name),grow:!t,order:0,type:optionsListControl,width:medium),ed96828e-efe9-43ad-be3f-0e04218f79af:(explicitInput:(dataViewId:'serverless.logs-*',enhancements:(),fieldName:to-env,id:ed96828e-efe9-43ad-be3f-0e04218f79af,selectedOptions:!(qa),title:to-env),grow:!t,order:2,type:optionsListControl,width:medium))))`
+  );
+}
+
+function getDashboardForProd(commit: string) {
+  return link(
+    'GPCTL Deployment Status dashboard for prod',
+    `https://overview.elastic-cloud.com/app/dashboards#/view/serverless-tooling-gpctl-deployment-status?_g=(refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&service-name=kibana&_a=(controlGroupInput:(chainingSystem:HIERARCHICAL,controlStyle:oneLine,ignoreParentSettings:(ignoreFilters:!f,ignoreQuery:!f,ignoreTimerange:!f,ignoreValidations:!f),panels:('18201b8e-3aae-4459-947d-21e007b6a3a5':(explicitInput:(dataViewId:'serverless.logs-*',enhancements:(),fieldName:commit-hash,id:'18201b8e-3aae-4459-947d-21e007b6a3a5',selectedOptions:!('${commit}'),title:commit-hash),grow:!t,order:1,type:optionsListControl,width:medium),'41060e65-ce4c-414e-b8cf-492ccb19245f':(explicitInput:(dataViewId:'serverless.logs-*',enhancements:(),fieldName:service-name,id:'41060e65-ce4c-414e-b8cf-492ccb19245f',selectedOptions:!(kibana),title:service-name),grow:!t,order:0,type:optionsListControl,width:medium),ed96828e-efe9-43ad-be3f-0e04218f79af:(explicitInput:(dataViewId:'serverless.logs-*',enhancements:(),fieldName:to-env,id:ed96828e-efe9-43ad-be3f-0e04218f79af,selectedOptions:!(production),title:to-env),grow:!t,order:2,type:optionsListControl,width:medium))))`
+  );
 }
 
 export function getUsefulLinksHtml(
@@ -15,6 +29,19 @@ export function getUsefulLinksHtml(
   data: { previousCommitHash: string; selectedCommitHash: string }
 ) {
   return `<h4>${heading}</h4>
-<div>:link: ${getCommitDiffLink(data.previousCommitHash, data.selectedCommitHash)}</div>
+<div>:link: ${link(
+    'Commits contained in deploy',
+    `https://github.com/elastic/kibana/compare/${data.previousCommitHash}...${data.selectedCommitHash}`
+  )}</div>
+<div>:link: ${link(
+    'Argo Workflow',
+    `https://argo-workflows.us-central1.gcp.qa.cld.elstc.co/workflows?label=hash%3D${data.selectedCommitHash}`
+  )}</div>
+<div>:link: ${getDashboardForNonProd(data.selectedCommitHash)}</div>
+<div>:link: ${getDashboardForProd(data.selectedCommitHash)}</div>
+<div>:link: ${link(
+    'Quality Gate pipeline',
+    'https://buildkite.com/elastic/kibana-tests/builds?branch=main'
+  )}</div>
 `;
 }
