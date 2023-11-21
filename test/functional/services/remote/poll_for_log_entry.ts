e05c0b3bedda8a1dec869368fe5a53f010fa8624
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-11-21 17:56:16 +0100

    [ftr/remote] improve poll_for_log_entry

--- test/functional/services/remote/poll_for_log_entry.ts
+++ test/functional/services/remote/poll_for_log_entry.ts
@@ -8,40 +8,20 @@
 
 import { WebDriver, logging } from 'selenium-webdriver';
 import * as Rx from 'rxjs';
-import { mergeMap, catchError, mergeMapTo, delay, first } from 'rxjs/operators';
+import { mergeMap, catchError, mergeMapTo, delay } from 'rxjs/operators';
+import { NoSuchSessionError } from 'selenium-webdriver/lib/error';
 
 /**
  * Create an observable that emits log entries representing the calls to log messages
  * available for a specific logger.
  */
-export function pollForLogEntry$(
-  driver: WebDriver,
-  type: string,
-  ms: number,
-  stop$: Rx.Observable<void>
-) {
+export function pollForLogEntry$(driver: WebDriver, type: string, ms: number) {
   const logCtrl = driver.manage().logs();
   const poll$ = new Rx.BehaviorSubject(undefined);
 
   const FINAL_MSG = '@@final@@';
 
   return new Rx.Observable<logging.Entry>((subscriber) => {
-    subscriber.add(
-      stop$.pipe(first()).subscribe(() => {
-        driver
-          .executeScript(
-            `
-              if (window.flushCoverageToLog) {
-                window.flushCoverageToLog();
-              }
-
-              console.log(${JSON.stringify(FINAL_MSG)})
-            `
-          )
-          .catch((error) => subscriber.error(error));
-      })
-    );
-
     subscriber.add(
       poll$
         .pipe(
@@ -79,13 +59,21 @@ export function pollForLogEntry$(
           }),
 
           catchError((error, resubscribe) => {
-            return Rx.concat(
-              // log error as a log entry
-              [new logging.Entry('SEVERE', `ERROR FETCHING BROWSR LOGS: ${error.message}`)],
+            if (error instanceof NoSuchSessionError) {
+              return Rx.concat(
+                // Without Webdriver session we can't fetch logs, stopping
+                [new logging.Entry('SEVERE', `WEBDRIVER SESSION IS OVER: ${error.message}`)],
+                Rx.of(undefined)
+              );
+            } else {
+              return Rx.concat(
+                // log error as a log entry
+                [new logging.Entry('SEVERE', `ERROR FETCHING BROWSR LOGS: ${error.message}`)],
 
-              // pause 10 seconds then resubscribe
-              Rx.of(1).pipe(delay(10 * 1000), mergeMapTo(resubscribe))
-            );
+                // pause 10 seconds then resubscribe
+                Rx.of(1).pipe(delay(10 * 1000), mergeMapTo(resubscribe))
+              );
+            }
           })
         )
         .subscribe(subscriber)
