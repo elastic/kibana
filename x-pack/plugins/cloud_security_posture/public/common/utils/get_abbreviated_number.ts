f3442d428f3e23798c71851368d171dde2ba4e76
Paulo Henrique <paulo.henrique@elastic.co>
2023-11-22 14:49:09 -0800

    add abbr utils

---
+++ x-pack/plugins/cloud_security_posture/public/common/utils/get_abbreviated_number.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import numeral from '@elastic/numeral';
+
+/*
+ * Retrieve a number abbreviated in the following format:
+    thousand: 'k'
+    million: 'm'
+    billion: 'b'
+    trillion: 't'
+ */
+export const getAbbreviatedNumber = (value: number) => {
+  return value < 1000 ? value : numeral(value).format('0.0a');
+};
