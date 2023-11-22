527dbcf0c74b7d43552940d53b9226626b0c1c3f
Mike Cote <michel.cote@elastic.co>
2023-11-22 12:15:19 -0500

    Initial commit

--- x-pack/plugins/alerting/server/lib/wrap_scoped_cluster_client.ts
+++ x-pack/plugins/alerting/server/lib/wrap_scoped_cluster_client.ts
@@ -10,16 +10,8 @@ import {
   TransportResult,
   TransportRequestOptionsWithMeta,
   TransportRequestOptionsWithOutMeta,
+  TransportRequestParams,
 } from '@elastic/elasticsearch';
-import type {
-  SearchRequest,
-  SearchResponse,
-  AggregateName,
-} from '@elastic/elasticsearch/lib/api/types';
-import type {
-  SearchRequest as SearchRequestWithBody,
-  AggregationsAggregate,
-} from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import type { IScopedClusterClient, ElasticsearchClient, Logger } from '@kbn/core/server';
 import { SearchMetrics, RuleInfo } from './types';
 
@@ -89,83 +81,36 @@ function wrapEsClient(opts: WrapEsClientOpts): ElasticsearchClient {
   const wrappedClient = esClient.child({});
 
   // Mutating the functions we want to wrap
-  wrappedClient.search = getWrappedSearchFn({ esClient: wrappedClient, ...rest });
+  wrappedClient.transport.request = getWrappedTransportRequestFn({
+    esClient: wrappedClient,
+    ...rest,
+  });
 
   return wrappedClient;
 }
 
-function getWrappedSearchFn(opts: WrapEsClientOpts) {
-  const originalSearch = opts.esClient.search;
+function getWrappedTransportRequestFn(opts: WrapEsClientOpts) {
+  const originalRequestFn = opts.esClient.transport.request;
 
   // A bunch of overloads to make TypeScript happy
-  async function search<
-    TDocument = unknown,
-    TAggregations = Record<AggregateName, AggregationsAggregate>
-  >(
-    params?: SearchRequest | SearchRequestWithBody,
+  async function request<TResponse = unknown>(
+    params: TransportRequestParams,
     options?: TransportRequestOptionsWithOutMeta
-  ): Promise<SearchResponse<TDocument, TAggregations>>;
-  async function search<
-    TDocument = unknown,
-    TAggregations = Record<AggregateName, AggregationsAggregate>
-  >(
-    params?: SearchRequest | SearchRequestWithBody,
+  ): Promise<TResponse>;
+  async function request<TResponse = unknown, TContext = unknown>(
+    params: TransportRequestParams,
     options?: TransportRequestOptionsWithMeta
-  ): Promise<TransportResult<SearchResponse<TDocument, TAggregations>, unknown>>;
-  async function search<
-    TDocument = unknown,
-    TAggregations = Record<AggregateName, AggregationsAggregate>
-  >(
-    params?: SearchRequest | SearchRequestWithBody,
+  ): Promise<TransportResult<TResponse, TContext>>;
+  async function request<TResponse = unknown>(
+    params: TransportRequestParams,
     options?: TransportRequestOptions
-  ): Promise<SearchResponse<TDocument, TAggregations>>;
-  async function search<
-    TDocument = unknown,
-    TAggregations = Record<AggregateName, AggregationsAggregate>
-  >(
-    params?: SearchRequest | SearchRequestWithBody,
-    options?: TransportRequestOptions
-  ): Promise<
-    | TransportResult<SearchResponse<TDocument, TAggregations>, unknown>
-    | SearchResponse<TDocument, TAggregations>
-  > {
-    try {
-      const searchOptions = options ?? {};
-      const start = Date.now();
-      opts.logger.debug(
-        `executing query for rule ${opts.rule.alertTypeId}:${opts.rule.id} in space ${
-          opts.rule.spaceId
-        } - ${JSON.stringify(params)} - with options ${JSON.stringify(searchOptions)}`
-      );
-      const result = (await originalSearch.call(opts.esClient, params, {
-        ...searchOptions,
-        signal: opts.abortController.signal,
-      })) as
-        | TransportResult<SearchResponse<TDocument, TAggregations>, unknown>
-        | SearchResponse<TDocument, TAggregations>;
-
-      const end = Date.now();
-      const durationMs = end - start;
-
-      let took = 0;
-      if (searchOptions.meta) {
-        // when meta: true, response is TransportResult<SearchResponse<TDocument, TAggregations>, unknown>
-        took = (result as TransportResult<SearchResponse<TDocument, TAggregations>, unknown>).body
-          .took;
-      } else {
-        // when meta: false, response is SearchResponse<TDocument, TAggregations>
-        took = (result as SearchResponse<TDocument, TAggregations>).took;
-      }
-
-      opts.logMetricsFn({ esSearchDuration: took ?? 0, totalSearchDuration: durationMs });
-      return result;
-    } catch (e) {
-      if (opts.abortController.signal.aborted) {
-        throw new Error('Search has been aborted due to cancelled execution');
-      }
-      throw e;
-    }
+  ): Promise<TResponse> {
+    const requestOptions = options ?? {};
+    return (await originalRequestFn.call(opts.esClient.transport, params, {
+      ...requestOptions,
+      signal: opts.abortController.signal,
+    })) as Promise<TResponse>;
   }
 
-  return search;
+  return request;
 }
