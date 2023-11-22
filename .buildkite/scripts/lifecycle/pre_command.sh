268df575fdf6353d5ce00c80f1b7969925bb58d2
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2023-11-22 18:27:19 -0500

    [8.11] fix/redact axios headers (#171803) (#171808)
    # Backport
    
    This will backport the following commits from `main` to `8.11`:
    - [fix/redact axios headers
    (#171803)](https://github.com/elastic/kibana/pull/171803)
    
    <!--- Backport version: 8.9.7 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Brad
    White","email":"Ikuni17@users.noreply.github.com"},"sourceCommit":{"committedDate":"2023-11-22T22:12:33Z","message":"fix/redact
    axios headers (#171803)\n\nFix token
    things\r\n\r\n---------\r\n\r\nCo-authored-by: Kibana Machine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"17ed0748e8d7671df31291d7b2bce5460c74b0f5","branchLabelMapping":{"^v8.12.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["Team:Operations","release_note:skip","backport:all-open","v8.12.0"],"number":171803,"url":"https://github.com/elastic/kibana/pull/171803","mergeCommit":{"message":"fix/redact
    axios headers (#171803)\n\nFix token
    things\r\n\r\n---------\r\n\r\nCo-authored-by: Kibana Machine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"17ed0748e8d7671df31291d7b2bce5460c74b0f5"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v8.12.0","labelRegex":"^v8.12.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/171803","number":171803,"mergeCommit":{"message":"fix/redact
    axios headers (#171803)\n\nFix token
    things\r\n\r\n---------\r\n\r\nCo-authored-by: Kibana Machine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"17ed0748e8d7671df31291d7b2bce5460c74b0f5"}}]}]
    BACKPORT-->
    
    Co-authored-by: Brad White <Ikuni17@users.noreply.github.com>

--- .buildkite/scripts/lifecycle/pre_command.sh
+++ .buildkite/scripts/lifecycle/pre_command.sh
@@ -77,15 +77,15 @@ EOF
 {
   CI_STATS_BUILD_ID="$(buildkite-agent meta-data get ci_stats_build_id --default '')"
   export CI_STATS_BUILD_ID
+  
+  CI_STATS_TOKEN="$(retry 5 5 vault read -field=api_token secret/kibana-issues/dev/kibana_ci_stats)"
+  export CI_STATS_TOKEN
+  
+  CI_STATS_HOST="$(retry 5 5 vault read -field=api_host secret/kibana-issues/dev/kibana_ci_stats)"
+  export CI_STATS_HOST
 
   if [[ "$CI_STATS_BUILD_ID" ]]; then
     echo "CI Stats Build ID: $CI_STATS_BUILD_ID"
-
-    CI_STATS_TOKEN="$(retry 5 5 vault read -field=api_token secret/kibana-issues/dev/kibana_ci_stats)"
-    export CI_STATS_TOKEN
-
-    CI_STATS_HOST="$(retry 5 5 vault read -field=api_host secret/kibana-issues/dev/kibana_ci_stats)"
-    export CI_STATS_HOST
 
     KIBANA_CI_STATS_CONFIG=$(jq -n \
       --arg buildId "$CI_STATS_BUILD_ID" \
