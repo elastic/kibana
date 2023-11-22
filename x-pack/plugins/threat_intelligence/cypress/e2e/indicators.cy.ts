15866c0e968cdd53ee110fd50fba235069c4129f
Tiago Costa <tiago.costa@elastic.co>
2023-11-22 19:53:18 +0000

    skip flaky suite (#171786)

--- x-pack/plugins/threat_intelligence/cypress/e2e/indicators.cy.ts
+++ x-pack/plugins/threat_intelligence/cypress/e2e/indicators.cy.ts
@@ -59,6 +59,7 @@ const URL_WITH_CONTRADICTORY_FILTERS =
 // FLAKY: https://github.com/elastic/kibana/issues/171779
 // FLAKY: https://github.com/elastic/kibana/issues/171778
 // FLAKY: https://github.com/elastic/kibana/issues/171785
+// FLAKY: https://github.com/elastic/kibana/issues/171786
 describe.skip('Invalid Indicators', { tags: '@ess' }, () => {
   describe('verify the grid loads even with missing fields', () => {
     beforeEach(() => {
