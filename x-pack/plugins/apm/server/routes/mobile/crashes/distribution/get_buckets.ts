c5901c8dcdca26238c5fc76ac0b7ef13a9d28c57
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2023-11-22 19:20:41 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/apm/server/routes/mobile/crashes/distribution/get_buckets.ts
+++ x-pack/plugins/apm/server/routes/mobile/crashes/distribution/get_buckets.ts
@@ -49,7 +49,7 @@ export async function getBuckets({
         bool: {
           filter: [
             ...termQuery(ERROR_TYPE, 'crash'),
-           ...termQuery(SERVICE_NAME, serviceName),
+            ...termQuery(SERVICE_NAME, serviceName),
             ...rangeQuery(start, end),
             ...environmentQuery(environment),
             ...kqlQuery(kuery),
