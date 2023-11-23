0da5b02ac21fcb3703394f73feab22908439b013
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2023-11-23 12:58:54 -0500

    Fix tests

--- x-pack/test/fleet_api_integration/apis/epm/update_assets.ts
+++ x-pack/test/fleet_api_integration/apis/epm/update_assets.ts
@@ -516,6 +516,7 @@ export default function (providerContext: FtrProviderContext) {
         install_started_at: res.attributes.install_started_at,
         install_source: 'registry',
         install_format_schema_version: FLEET_INSTALL_FORMAT_VERSION,
+        latest_install_failed_attempts: [],
         verification_status: 'unknown',
         verification_key_id: null,
       });
