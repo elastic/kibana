da2ad1ceedd9551e759542ce93cd982bdae9388f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-11-23 13:17:57 +0100

    fix cypress FTR config by removing SAML args

--- x-pack/plugins/security_solution/scripts/run_cypress/get_ftr_config.ts
+++ x-pack/plugins/security_solution/scripts/run_cypress/get_ftr_config.ts
@@ -172,15 +172,15 @@ export const getFTRConfig = ({
       }
 
       // Serverless Specific
-      if (vars.serverless) {
-        log.info(`Serverless mode detected`);
+      // if (vars.serverless) {
+      //   log.info(`Serverless mode detected`);
 
-        vars.esTestCluster.serverArgs.push(
-          `xpack.security.authc.realms.saml.cloud-saml-kibana.sp.entity_id=http://host.docker.internal:${kibanaPort}`,
-          `xpack.security.authc.realms.saml.cloud-saml-kibana.sp.logout=http://host.docker.internal:${kibanaPort}/logout`,
-          `xpack.security.authc.realms.saml.cloud-saml-kibana.sp.acs=http://host.docker.internal:${kibanaPort}/api/security/saml/callback`
-        );
-      }
+      //   vars.esTestCluster.serverArgs.push(
+      //     `xpack.security.authc.realms.saml.cloud-saml-kibana.sp.entity_id=http://host.docker.internal:${kibanaPort}`,
+      //     `xpack.security.authc.realms.saml.cloud-saml-kibana.sp.logout=http://host.docker.internal:${kibanaPort}/logout`,
+      //     `xpack.security.authc.realms.saml.cloud-saml-kibana.sp.acs=http://host.docker.internal:${kibanaPort}/api/security/saml/callback`
+      //   );
+      // }
 
       if (specFileFTRConfig?.productTypes) {
         if (vars.serverless) {
