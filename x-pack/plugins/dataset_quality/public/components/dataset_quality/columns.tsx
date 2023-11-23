f40fb3287a97dd85b07b63bf56b47e585a33c592
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2023-11-23 15:01:08 +0100

    Merge dataset and integration columns

--- x-pack/plugins/dataset_quality/public/components/dataset_quality/columns.tsx
+++ x-pack/plugins/dataset_quality/public/components/dataset_quality/columns.tsx
@@ -6,19 +6,14 @@
  */
 
 import React from 'react';
-import { EuiBasicTableColumn, EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
+import { EuiBasicTableColumn, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { PackageIcon } from '@kbn/fleet-plugin/public';
-import { Integration } from '../../../common/data_streams_stats/integration';
 import { DataStreamStat } from '../../../common/data_streams_stats/data_stream_stat';
 import { useFormattedTime } from '../../hooks';
 
 const nameColumnName = i18n.translate('xpack.datasetQuality.nameColumnName', {
   defaultMessage: 'Dataset Name',
-});
-
-const integrationColumnName = i18n.translate('xpack.datasetQuality.integrationColumnName', {
-  defaultMessage: 'Integration',
 });
 
 const sizeColumnName = i18n.translate('xpack.datasetQuality.sizeColumnName', {
@@ -35,53 +30,28 @@ export const getDatasetQualitTableColumns = (): Array<EuiBasicTableColumn<DataSt
       name: nameColumnName,
       field: 'title',
       sortable: true,
-      render: (title: string) => {
-        const [dataset, namespace] = title.split('-');
+      render: (title: string, dataStreamStat: DataStreamStat) => {
+        const { integration } = dataStreamStat;
 
         return (
-          <EuiFlexGroup alignItems="center" gutterSize="m">
-            <EuiFlexItem grow={false}>
-              <EuiText>{dataset}</EuiText>
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiText
-                size="xs"
-                color="subdued"
-                style={{
-                  fontStyle: 'italic',
-                }}
-              >
-                {namespace}
-              </EuiText>
-            </EuiFlexItem>
+          <EuiFlexGroup alignItems="center" gutterSize="s">
+            {integration ? (
+              <EuiFlexItem grow={false}>
+                <PackageIcon
+                  packageName={integration.name}
+                  version={integration.version!}
+                  icons={integration.icons}
+                  size="m"
+                  tryApi
+                />
+              </EuiFlexItem>
+            ) : (
+              '-'
+            )}
+            <EuiFlexItem grow={false}>{title}</EuiFlexItem>
           </EuiFlexGroup>
         );
       },
-      width: '400px',
-    },
-    {
-      name: integrationColumnName,
-      field: 'integration',
-      render: (integration?: Integration) => {
-        if (!integration) return ' - ';
-        return (
-          <EuiFlexGroup alignItems="center" gutterSize="m">
-            <EuiFlexItem grow={false}>
-              <PackageIcon
-                packageName={integration.name}
-                version={integration.version!}
-                icons={integration.icons}
-                size="m"
-                tryApi
-              />
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiText>{integration.title}</EuiText>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        );
-      },
-      sortable: true,
     },
     {
       name: sizeColumnName,
