a931eb20215314b30ad874907235cfd749642271
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-11-24 11:51:58 +0100

    update test

--- x-pack/test_serverless/functional/test_suites/common/example/index.ts
+++ x-pack/test_serverless/functional/test_suites/common/example/index.ts
@@ -7,13 +7,16 @@
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
+const VIEWER_ROLE = 'viewer';
+
 export default function ({ getPageObject, getService }: FtrProviderContext) {
-  describe('Login as viewer', function () {
+  describe(`Login as ${VIEWER_ROLE}`, function () {
     const svlCommonPage = getPageObject('svlCommonPage');
     const testSubjects = getService('testSubjects');
+    const svlUserManager = getService('svlUserManager');
 
     before(async () => {
-      await svlCommonPage.loginWithRole('viewer');
+      await svlCommonPage.loginWithRole(VIEWER_ROLE);
     });
 
     it('should be able to see correct profile', async () => {
@@ -21,8 +24,9 @@ export default function ({ getPageObject, getService }: FtrProviderContext) {
       await svlCommonPage.assertUserAvatarExists();
       await svlCommonPage.clickUserAvatar();
       await svlCommonPage.assertUserMenuExists();
-      const actualEmail = await testSubjects.getVisibleText('contextMenuPanelTitle');
-      expect(actualEmail).to.be('elastic_viewer@elastic.co');
+      const actualFullname = await testSubjects.getVisibleText('contextMenuPanelTitle');
+      const session = await svlUserManager.getSessionByRole(VIEWER_ROLE);
+      expect(actualFullname).to.be(session.fullname);
     });
   });
 }
