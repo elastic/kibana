13e98f88a657a1fd3acf96ae85308d0995d096a9
Marta Bondyra <marta.bondyra@elastic.co>
2023-11-24 16:12:22 +0100

    Revert "make types smarter"
    This reverts commit feff1c7f0b0884756f1f8b3c0ddf5ef8a3e300d7.

--- packages/kbn-calculate-width-from-char-count/src/calculate_width_from_entries.ts
+++ packages/kbn-calculate-width-from-char-count/src/calculate_width_from_entries.ts
@@ -8,9 +8,11 @@
 
 import { LIMITS, calculateWidthFromCharCount } from './calculate_width_from_char_count';
 
-const getMaxLabelLengthForObjects = <T extends Record<string, string>>(
-  entries: T[],
-  labelKeys: Array<keyof T>
+type GenericObject<T = Record<string, any>> = T;
+
+const getMaxLabelLengthForObjects = (
+  entries: GenericObject[],
+  labelKeys: Array<keyof GenericObject>
 ) =>
   entries.reduce((acc, curr) => {
     const labelKey = labelKeys.find((key) => curr[key]);
@@ -24,13 +26,13 @@ const getMaxLabelLengthForObjects = <T extends Record<string, string>>(
 const getMaxLabelLengthForStrings = (arr: string[]) =>
   arr.reduce((acc, curr) => (acc > curr.length ? acc : curr.length), 0);
 
-export function calculateWidthFromEntries<T extends Record<string, string>>(
-  entries: T[] | string[],
-  labelKeys?: Array<keyof T>,
+export function calculateWidthFromEntries(
+  entries: GenericObject[] | string[],
+  labelKeys?: Array<keyof GenericObject>,
   overridesPanelWidths?: Partial<LIMITS>
 ) {
   const maxLabelLength = labelKeys
-    ? getMaxLabelLengthForObjects(entries as T[], labelKeys)
+    ? getMaxLabelLengthForObjects(entries as GenericObject[], labelKeys)
     : getMaxLabelLengthForStrings(entries as string[]);
 
   return calculateWidthFromCharCount(maxLabelLength, overridesPanelWidths);
