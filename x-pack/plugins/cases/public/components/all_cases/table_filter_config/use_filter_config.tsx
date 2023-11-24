61817092f40ceb93689ddd74156a9213f66b4ca4
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-11-24 10:23:50 +0100

    remove keys in storage that does not exist + refactor merge fn

--- x-pack/plugins/cases/public/components/all_cases/table_filter_config/use_filter_config.tsx
+++ x-pack/plugins/cases/public/components/all_cases/table_filter_config/use_filter_config.tsx
@@ -13,22 +13,20 @@ import { useCustomFieldsFilterConfig } from './use_custom_fields_filter_config';
 import { MoreFiltersSelectable } from './more_filters_selectable';
 import { useCasesContext } from '../../cases_context/use_cases_context';
 
-const serializeFilterVisibilityMap = (value: Map<string, FilterConfigState>) => {
-  return JSON.stringify(
-    Array.from(value.entries()).map(([key, filter]) => ({
-      key,
-      isActive: filter.isActive,
-    }))
+const mergeSystemAndCustomFieldConfigs = ({
+  systemFilterConfig,
+  customFieldsFilterConfig,
+}: {
+  systemFilterConfig: FilterConfig[];
+  customFieldsFilterConfig: FilterConfig[];
+}) => {
+  const newFilterConfig = new Map(
+    [...systemFilterConfig, ...customFieldsFilterConfig]
+      .filter((filter) => filter.isAvailable)
+      .map((filter) => [filter.key, filter])
   );
-};
 
-const deserializeFilterVisibilityMap = (value: string): Map<string, FilterConfigState> => {
-  return new Map(
-    JSON.parse(value).map(({ key, isActive }: { key: string; isActive: boolean }) => [
-      key,
-      { key, isActive },
-    ])
-  );
+  return newFilterConfig;
 };
 
 export const useFilterConfig = ({
@@ -40,77 +38,86 @@ export const useFilterConfig = ({
 }) => {
   const { appId } = useCasesContext();
   const { customFieldsFilterConfig } = useCustomFieldsFilterConfig({ onFilterOptionChange });
-  const [filters, setFilters] = useState<Map<string, FilterConfig>>(
+  const [filterConfigs, setFilterConfigs] = useState<Map<string, FilterConfig>>(
     () => new Map([...systemFilterConfig].map((filter) => [filter.key, filter]))
   );
-  const [filterVisibilityMap, setFilterVisibilityMap] = useLocalStorage<
-    Map<string, FilterConfigState>
-  >(`${appId}.${LOCAL_STORAGE_KEYS.casesTableFiltersConfig}`, new Map(), {
-    raw: false,
-    serializer: serializeFilterVisibilityMap,
-    deserializer: deserializeFilterVisibilityMap,
-  });
+  const [activeByFilterKey, setActiveByFilterKey] = useLocalStorage<FilterConfigState[]>(
+    `${appId}.${LOCAL_STORAGE_KEYS.casesTableFiltersConfig}`,
+    []
+  );
 
+  // this effect is used to clean up filters that are no longer available
   useEffect(() => {
-    const newFilters = new Map(
-      [...systemFilterConfig, ...customFieldsFilterConfig]
-        .filter((filter) => filter.isAvailable)
-        .map((filter) => [filter.key, filter])
-    );
-    setFilters(newFilters);
-  }, [systemFilterConfig, customFieldsFilterConfig]);
+    const newFilterConfig = mergeSystemAndCustomFieldConfigs({
+      systemFilterConfig,
+      customFieldsFilterConfig,
+    });
 
-  useEffect(() => {
-    const newFilterKeys = new Map(
-      [...systemFilterConfig, ...customFieldsFilterConfig]
-        .filter((filter) => filter.isAvailable)
-        .map((filter) => [filter.key, filter])
-    );
-
-    filters.forEach((filter) => {
-      if (!newFilterKeys.has(filter.key)) {
+    filterConfigs.forEach((filter) => {
+      if (!newFilterConfig.has(filter.key)) {
         filter.deactivate();
       }
     });
-  }, [filters, systemFilterConfig, customFieldsFilterConfig]);
+  }, [filterConfigs, systemFilterConfig, customFieldsFilterConfig]);
+
+  useEffect(() => {
+    setFilterConfigs(
+      mergeSystemAndCustomFieldConfigs({
+        systemFilterConfig,
+        customFieldsFilterConfig,
+      })
+    );
+  }, [systemFilterConfig, customFieldsFilterConfig]);
 
   const onChange = ({ selectedOptionKeys }: { filterId: string; selectedOptionKeys: string[] }) => {
-    const newFilterVisibilityMap = new Map(filterVisibilityMap);
+    const newActiveByFilterKey = [...(activeByFilterKey || [])];
     const deactivatedFilters: string[] = [];
 
-    newFilterVisibilityMap.forEach(({ key, isActive }) => {
-      if (selectedOptionKeys.includes(key)) {
-        if (!isActive) {
-          newFilterVisibilityMap.delete(key);
-          newFilterVisibilityMap.set(key, { key, isActive: true });
+    // for each filter in the current state, this way we keep the order
+    (activeByFilterKey || []).forEach(({ key, isActive: prevIsActive }, currentIndex) => {
+      if (filterConfigs.has(key)) {
+        const isActive = selectedOptionKeys.find((optionKey) => optionKey === key);
+        if (isActive && !prevIsActive) {
+          // remove/insert to the end with isActive = true
+          newActiveByFilterKey.splice(currentIndex, 1);
+          newActiveByFilterKey.push({ key, isActive: true });
+        } else if (!isActive && prevIsActive) {
+          // dont move, just update isActive = false
+          deactivatedFilters.push(key);
+          newActiveByFilterKey[currentIndex] = { key, isActive: false };
         }
       } else {
-        deactivatedFilters.push(key);
-        newFilterVisibilityMap.set(key, { key, isActive: false });
+        // clean up filters that are no longer available
+        newActiveByFilterKey.splice(currentIndex, 1);
       }
     });
 
-    filters.forEach(({ key }) => {
-      if (!newFilterVisibilityMap.has(key)) {
-        const isActive = selectedOptionKeys.includes(key);
+    // for each filter in the config
+    filterConfigs.forEach(({ key: configKey }) => {
+      // add it if its a new filter
+      if (!newActiveByFilterKey.find(({ key }) => key === configKey)) {
+        // first time, the current state is empty, all filters will be added
+        // isActive = true if the filter is in the selectedOptionKeys
+        const isActive = selectedOptionKeys.find((optionKey) => optionKey === configKey);
         if (!isActive) {
-          deactivatedFilters.push(key);
+          // for system filter that is removed as first action
+          deactivatedFilters.push(configKey);
         }
-        newFilterVisibilityMap.set(key, {
-          key,
-          isActive,
+        newActiveByFilterKey.push({
+          key: configKey,
+          isActive: Boolean(isActive),
         });
       }
     });
 
     deactivatedFilters
-      .filter((key) => filters.has(key))
-      .forEach((key) => (filters.get(key) as FilterConfig).deactivate());
+      .filter((key) => filterConfigs.has(key))
+      .forEach((key) => (filterConfigs.get(key) as FilterConfig).deactivate());
 
-    setFilterVisibilityMap(newFilterVisibilityMap);
+    setActiveByFilterKey(newActiveByFilterKey);
   };
 
-  const selectableOptions = Array.from(filters.values())
+  const selectableOptions = Array.from(filterConfigs.values())
     .map(({ key, label }) => ({
       key,
       label,
@@ -121,10 +128,12 @@ export const useFilterConfig = ({
       return a.key > b.key ? 1 : -1;
     });
   const source =
-    filterVisibilityMap && filterVisibilityMap.size > 0 ? filterVisibilityMap : filters;
-  const activeFilters = Array.from(source.values())
-    .filter((filter) => filter.isActive && filters.has(filter.key))
-    .map((filter) => filters.get(filter.key)) as FilterConfig[];
+    activeByFilterKey && activeByFilterKey.length > 0
+      ? activeByFilterKey
+      : Array.from(filterConfigs.values());
+  const activeFilters = source
+    .filter((filter) => filter.isActive && filterConfigs.has(filter.key))
+    .map((filter) => filterConfigs.get(filter.key)) as FilterConfig[];
   const activeFilterKeys = activeFilters.map((filter) => filter.key);
 
   return {
