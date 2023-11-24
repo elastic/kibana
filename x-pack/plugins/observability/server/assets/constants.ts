61566ab880b66fa8f45f470e762ba0c69917de74
Kevin Delemme <kevin.delemme@elastic.co>
2023-11-24 10:53:27 -0500

    Add version into pipeline names

--- x-pack/plugins/observability/server/assets/constants.ts
+++ x-pack/plugins/observability/server/assets/constants.ts
@@ -18,7 +18,7 @@ export const SLO_INDEX_TEMPLATE_PATTERN = `.slo-observability.sli-*`;
 export const SLO_DESTINATION_INDEX_NAME = `.slo-observability.sli-v${SLO_RESOURCES_VERSION}`;
 export const SLO_DESTINATION_INDEX_PATTERN = `.slo-observability.sli-v${SLO_RESOURCES_VERSION}*`;
 
-export const SLO_INGEST_PIPELINE_NAME = `.slo-observability.sli-pipeline-v${SLO_RESOURCES_VERSION}`;
+export const SLO_INGEST_PIPELINE_NAME = `.slo-observability.sli.pipeline-v${SLO_RESOURCES_VERSION}`;
 // slo-observability.sli-v<version>.(YYYY-MM-DD)
 export const SLO_INGEST_PIPELINE_INDEX_NAME_PREFIX = `.slo-observability.sli-v${SLO_RESOURCES_VERSION}.`;
 
@@ -32,7 +32,7 @@ export const SLO_SUMMARY_DESTINATION_INDEX_NAME = `.slo-observability.summary-v$
 export const SLO_SUMMARY_TEMP_INDEX_NAME = `.slo-observability.summary-v${SLO_RESOURCES_VERSION}.temp`; // store the temporary summary document
 export const SLO_SUMMARY_DESTINATION_INDEX_PATTERN = `.slo-observability.summary-v${SLO_RESOURCES_VERSION}*`; // include temp and non-temp summary indices
 
-export const SLO_SUMMARY_INGEST_PIPELINE_NAME = `.slo-observability.summary.pipeline`;
+export const SLO_SUMMARY_INGEST_PIPELINE_NAME = `.slo-observability.summary.pipeline-v${SLO_RESOURCES_VERSION}`;
 export const SLO_SUMMARY_ENRICH_POLICY_NAME = `slo-observability.summary.enrich_policy`;
 
 export const getSLOTransformId = (sloId: string, sloRevision: number) =>
