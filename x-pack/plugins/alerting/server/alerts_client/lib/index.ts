91deef673c461e44c94aa3c0a9e8e2b24c8029d4
Ying <ying.mao@elastic.co>
2023-11-27 14:45:59 -0500

    Remove echoed field value from bulk error responses

--- x-pack/plugins/alerting/server/alerts_client/lib/index.ts
+++ x-pack/plugins/alerting/server/alerts_client/lib/index.ts
@@ -16,3 +16,4 @@ export {
   getContinualAlertsQuery,
 } from './get_summarized_alerts_query';
 export { expandFlattenedAlert } from './format_alert';
+export { sanitizeBulkErrorResponse } from './sanitize_bulk_response';
