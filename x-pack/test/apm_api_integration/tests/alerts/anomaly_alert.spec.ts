7dc0e322c9dc4d9880b5c297dc0347e61610c7ab
miriam.aparicio <miriam.aparicio@gmail.com>
2023-11-28 15:59:39 +0700

    fix api test for anomaly alert

--- x-pack/test/apm_api_integration/tests/alerts/anomaly_alert.spec.ts
+++ x-pack/test/apm_api_integration/tests/alerts/anomaly_alert.spec.ts
@@ -11,6 +11,7 @@ import { apm, timerange } from '@kbn/apm-synthtrace-client';
 import expect from '@kbn/expect';
 import { range } from 'lodash';
 import { ML_ANOMALY_SEVERITY } from '@kbn/ml-anomaly-utils/anomaly_severity';
+import { ANOMALY_DETECTOR_TYPES } from '@kbn/apm-plugin/common/rules/apm_rule_types';
 import { FtrProviderContext } from '../../common/ftr_provider_context';
 import { createAndRunApmMlJobs } from '../../common/utils/create_and_run_apm_ml_jobs';
 import { createApmRule } from './helpers/alerting_api_helper';
@@ -85,6 +86,7 @@ export default function ApiTest({ getService }: FtrProviderContext) {
               windowSize: 5,
               windowUnit: 'h',
               anomalySeverityType: ML_ANOMALY_SEVERITY.WARNING,
+              anomalyDetectorType: ANOMALY_DETECTOR_TYPES.LATENCY,
             },
             ruleTypeId: ApmRuleType.Anomaly,
           });
