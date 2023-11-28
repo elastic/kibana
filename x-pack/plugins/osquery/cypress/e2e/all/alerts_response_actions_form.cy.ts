4bf85dc8e8f0168e621658f6f0b652a5a13add69
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-11-28 09:14:00 +0100

    export selector name

--- x-pack/plugins/osquery/cypress/e2e/all/alerts_response_actions_form.cy.ts
+++ x-pack/plugins/osquery/cypress/e2e/all/alerts_response_actions_form.cy.ts
@@ -15,6 +15,7 @@ import {
   packFixture,
 } from '../../tasks/api_fixtures';
 import {
+  RESPONSE_ACTIONS_ERRORS,
   OSQUERY_RESPONSE_ACTION_ADD_BUTTON,
   RESPONSE_ACTIONS_ITEM_0,
   RESPONSE_ACTIONS_ITEM_1,
@@ -67,7 +68,7 @@ describe('Alert Event Details - Response Actions Form', { tags: ['@ess', '@serve
     cy.contains('Response actions are run on each rule execution.');
     cy.getBySel(OSQUERY_RESPONSE_ACTION_ADD_BUTTON).click();
 
-    cy.getBySel('response-actions-error').within(() => {
+    cy.getBySel(RESPONSE_ACTIONS_ERRORS).within(() => {
       cy.contains('Query is a required field');
       cy.contains('Timeout value must be greater than 60 seconds.').should('not.exist');
     });
@@ -79,7 +80,7 @@ describe('Alert Event Details - Response Actions Form', { tags: ['@ess', '@serve
       cy.contains('Timeout value must be greater than 60 seconds.');
     });
 
-    cy.getBySel('response-actions-error').within(() => {
+    cy.getBySel(RESPONSE_ACTIONS_ERRORS).within(() => {
       cy.contains('Query is a required field');
       cy.contains('Timeout value must be greater than 60 seconds.');
     });
@@ -88,7 +89,7 @@ describe('Alert Event Details - Response Actions Form', { tags: ['@ess', '@serve
       cy.getBySel('timeout-input').type('6');
       cy.contains('Timeout value must be greater than 60 seconds.');
     });
-    cy.getBySel('response-actions-error').within(() => {
+    cy.getBySel(RESPONSE_ACTIONS_ERRORS).within(() => {
       cy.contains('Query is a required field');
       cy.contains('Timeout value must be greater than 60 seconds.');
     });
@@ -96,13 +97,13 @@ describe('Alert Event Details - Response Actions Form', { tags: ['@ess', '@serve
       cy.getBySel('timeout-input').type('6');
       cy.contains('Timeout value must be greater than 60 seconds.').should('not.exist');
     });
-    cy.getBySel('response-actions-error').within(() => {
+    cy.getBySel(RESPONSE_ACTIONS_ERRORS).within(() => {
       cy.contains('Query is a required field');
     });
     cy.getBySel(RESPONSE_ACTIONS_ITEM_0).within(() => {
       cy.getBySel('timeout-input').type('6');
     });
-    cy.getBySel('response-actions-error').within(() => {
+    cy.getBySel(RESPONSE_ACTIONS_ERRORS).within(() => {
       cy.contains('Query is a required field');
       cy.contains('Timeout value must be greater than 60 seconds.').should('not.exist');
     });
@@ -116,7 +117,7 @@ describe('Alert Event Details - Response Actions Form', { tags: ['@ess', '@serve
     cy.getBySel(RESPONSE_ACTIONS_ITEM_1).within(() => {
       cy.contains('Run a set of queries in a pack').click();
     });
-    cy.getBySel('response-actions-error')
+    cy.getBySel(RESPONSE_ACTIONS_ERRORS)
       .within(() => {
         cy.contains('Pack is a required field');
       })
