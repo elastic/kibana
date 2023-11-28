9a7407b3ea937c2af74df407a6bb4574b7694efc
Drew Tate <andrew.tate@elastic.co>
2023-11-28 10:24:18 -0700

    augment functional test

--- x-pack/test/functional/apps/lens/group6/workspace_dimensions.ts
+++ x-pack/test/functional/apps/lens/group6/workspace_dimensions.ts
@@ -8,24 +8,12 @@
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
-const visTypes = [
-  {
-    id: 'bar',
-    searchText: '',
-    expectedWidth: '506px',
-    expectedHeight: '284.625px',
-  },
-  {
-    id: 'bar_horizontal',
-    searchText: '',
-    expectedWidth: '265.5px',
-    expectedHeight: '472px',
-  },
-];
-
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const PageObjects = getPageObjects(['visualize', 'lens', 'common']);
   const browser = getService('browser');
+  const testSubjects = getService('testSubjects');
+  const retry = getService('retry');
+  const log = getService('log');
 
   describe('lens workspace dimensions', () => {
     let originalWindowSize: {
@@ -37,17 +25,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
     before(async () => {
       originalWindowSize = await browser.getWindowSize();
-      await browser.setWindowSize(1200, 1000);
+      await browser.setWindowSize(1600, 1200);
 
       await PageObjects.visualize.navigateToNewVisualization();
       await PageObjects.visualize.clickVisType('lens');
       await PageObjects.lens.goToTimeRange();
-
-      await PageObjects.lens.configureDimension({
-        dimension: 'lnsXY_xDimensionPanel > lns-empty-dimension',
-        operation: 'terms',
-        field: 'ip',
-      });
 
       await PageObjects.lens.configureDimension({
         dimension: 'lnsXY_yDimensionPanel > lns-empty-dimension',
@@ -60,16 +42,83 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await browser.setWindowSize(originalWindowSize.width, originalWindowSize.height);
     });
 
-    it('adjusts dimension for various vis types', async () => {
+    const assertWorkspaceDimensions = async (expectedWidth: string, expectedHeight: string) => {
+      const { width, height } = await PageObjects.lens.getWorkspaceVisContainerDimensions();
+
+      expect(width).to.be(expectedWidth);
+      expect(height).to.be(expectedHeight);
+    };
+
+    it('adjusts dimension for various chart types', async () => {
+      const VERTICAL_16_9 = { expectedWidth: '864px', expectedHeight: '486px' };
+      const HORIZONTAL_16_9 = { expectedWidth: '296.438px', expectedHeight: '527px' };
+      const UNCONSTRAINED = { expectedWidth: '864px', expectedHeight: '527px' };
+
+      /**
+       * This list is specifically designed to test dimension transitions.
+       *
+       * I have attempted to order the vis types to maximize the number of transitions.
+       */
+      const visTypes: Array<{
+        expectedWidth: string;
+        expectedHeight: string;
+        id: string;
+        searchText?: string;
+      }> = [
+        { id: 'lnsDatatable', ...UNCONSTRAINED },
+        { id: 'line', ...VERTICAL_16_9 },
+        { id: 'verticalBullet', searchText: 'gauge', ...UNCONSTRAINED },
+        { id: 'bar_horizontal_percentage_stacked', searchText: 'bar', ...HORIZONTAL_16_9 },
+        { id: 'lnsLegacyMetric', ...UNCONSTRAINED },
+        { id: 'bar_horizontal_stacked', ...HORIZONTAL_16_9 },
+        { id: 'donut', ...UNCONSTRAINED },
+        { id: 'bar', ...VERTICAL_16_9 },
+        { id: 'mosaic', ...UNCONSTRAINED },
+        { id: 'bar_percentage_stacked', searchText: 'bar', ...VERTICAL_16_9 },
+        { id: 'pie', ...UNCONSTRAINED },
+        { id: 'bar_stacked', ...VERTICAL_16_9 },
+        { id: 'lnsMetric', expectedWidth: '300px', expectedHeight: '300px' },
+        { id: 'area', ...VERTICAL_16_9 },
+        { id: 'treemap', ...UNCONSTRAINED },
+        { id: 'area_percentage_stacked', searchText: 'area', ...VERTICAL_16_9 },
+        { id: 'waffle', ...UNCONSTRAINED },
+        { id: 'area_stacked', ...VERTICAL_16_9 },
+        { id: 'horizontalBullet', searchText: 'gauge', ...UNCONSTRAINED },
+        { id: 'bar_horizontal', ...HORIZONTAL_16_9 },
+        // { id: 'heatmap', ...UNCONSTRAINED }, // heatmap blocks render unless it's given two dimensions. This stops the expression renderer from requesting new dimensions.
+        // { id: 'lnsChoropleth', ...UNCONSTRAINED }, // choropleth currently erases all dimensions
+        // { id: 'lnsTagcloud', ...UNCONSTRAINED }, // tag cloud currently erases all dimensions
+      ];
+
       while (visTypes.length) {
         const vis = visTypes.pop()!;
         await PageObjects.lens.switchToVisualization(vis.id, vis.searchText);
 
-        const { width, height } = await PageObjects.lens.getWorkspaceVisContainerDimensions();
+        log.debug(`Testing ${vis.id}... expecting ${vis.expectedWidth}x${vis.expectedHeight}`);
 
-        expect(width).to.be(vis.expectedWidth);
-        expect(height).to.be(vis.expectedHeight);
+        await assertWorkspaceDimensions(vis.expectedWidth, vis.expectedHeight);
       }
+    });
+
+    it('metric dimensions', async () => {
+      await PageObjects.lens.switchToVisualization('lnsMetric');
+
+      await assertWorkspaceDimensions('300px', '300px');
+
+      await PageObjects.lens.configureDimension({
+        dimension: 'lnsMetric_breakdownByDimensionPanel > lns-empty-dimension',
+        operation: 'terms',
+        field: 'ip',
+      });
+
+      await assertWorkspaceDimensions('600px', '400px');
+
+      await PageObjects.lens.openDimensionEditor('lnsMetric_breakdownByDimensionPanel');
+      await testSubjects.setValue('lnsMetric_max_cols', '2');
+
+      retry.try(async () => {
+        await assertWorkspaceDimensions('400px', '600px');
+      });
     });
   });
 }
