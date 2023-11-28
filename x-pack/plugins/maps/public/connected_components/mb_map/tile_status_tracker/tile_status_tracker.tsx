3e37e92f2cbce612c14683223a37fd06c529ee38
nreese <reese.nathan@elastic.co>
2023-11-28 08:00:55 -0700

    add check for isLayerGroup

--- x-pack/plugins/maps/public/connected_components/mb_map/tile_status_tracker/tile_status_tracker.tsx
+++ x-pack/plugins/maps/public/connected_components/mb_map/tile_status_tracker/tile_status_tracker.tsx
@@ -12,6 +12,7 @@ import type { AJAXError, Map as MbMap, MapSourceDataEvent } from '@kbn/mapbox-gl
 import type { TileError, TileMetaFeature } from '../../../../common/descriptor_types';
 import { SPATIAL_FILTERS_LAYER_ID } from '../../../../common/constants';
 import { ILayer } from '../../../classes/layers/layer';
+import { isLayerGroup } from '../../../classes/layers/layer_group';
 import { IVectorSource } from '../../../classes/sources/vector_source';
 import { getTileKey as getCenterTileKey } from '../../../classes/util/geo_tile_utils';
 import { boundsToExtent } from '../../../classes/util/maplibre_utils';
@@ -64,6 +65,10 @@ export class TileStatusTracker extends Component<Props> {
 
   componentDidUpdate() {
     this.props.layerList.forEach((layer) => {
+      if (isLayerGroup(layer)) {
+        return;
+      }
+
       const source = layer.getSource();
       if (
         source.isESSource() &&
