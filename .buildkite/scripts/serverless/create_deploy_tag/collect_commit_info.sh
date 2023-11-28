c08b8d5bce291b63e473ee1e4ffaefe0151b0920
Alex Szabo <alex.szabo@elastic.co>
2023-11-28 09:35:01 +0100

    chore: fix unbound dry_run var

--- .buildkite/scripts/serverless/create_deploy_tag/collect_commit_info.sh
+++ .buildkite/scripts/serverless/create_deploy_tag/collect_commit_info.sh
@@ -11,6 +11,6 @@ ts-node .buildkite/scripts/serverless/create_deploy_tag/collect_commit_info.ts
 cat << EOF | buildkite-agent pipeline upload
   steps:
     - block: "Confirm deployment"
-      prompt: "Are you sure you want to deploy to production? (dry run: $DRY_RUN)"
+      prompt: "Are you sure you want to deploy to production? (dry run: ${DRY_RUN:-false})"
       depends_on: collect_data
 EOF
