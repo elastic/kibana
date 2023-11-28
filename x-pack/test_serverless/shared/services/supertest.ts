1cda908aeb3c41fa231f0e68ea35fa9c6beecf1f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-11-28 09:57:22 +0100

    [supertest] override TestAgent to not cache cookie

--- x-pack/test_serverless/shared/services/supertest.ts
+++ x-pack/test_serverless/shared/services/supertest.ts
@@ -7,7 +7,35 @@
 
 import { format as formatUrl } from 'url';
 import supertest from 'supertest';
+// @ts-ignore internal modules are not typed
+import TestAgent from 'supertest/lib/agent';
+// @ts-ignore internal modules are not typed
+import Test from 'supertest/lib/test';
 import { FtrProviderContext } from '../../functional/ftr_provider_context';
+
+const methods = ['get', 'post', 'put', 'head', 'delete'];
+
+methods.forEach(function (method) {
+  TestAgent.prototype[method] = function (url: any, fn: any) {
+    const req = new Test(this.app, method.toUpperCase(), url);
+    if (this._options.http2) {
+      req.http2();
+    }
+
+    if (this._host) {
+      req.set('host', this._host);
+    }
+
+    req.on('response', this._saveCookies.bind(this));
+    req.on('redirect', this._saveCookies.bind(this));
+    req.on('redirect', this._attachCookies.bind(this, req));
+    this._setDefaults(req);
+    // The only change to the module original logic is to avoid caching cookies in agent
+    // this._attachCookies(req);
+
+    return req;
+  };
+});
 
 export function SupertestProvider({ getService }: FtrProviderContext) {
   const config = getService('config');
