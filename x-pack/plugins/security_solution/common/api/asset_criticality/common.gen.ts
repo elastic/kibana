0c7c302645c7a542e074a45c2b9a32bebaedc79d
Mark Hopkin <mark.hopkin@elastic.co>
2023-11-29 12:10:21 +0000

    fix openAPI query paramters spec

--- x-pack/plugins/security_solution/common/api/asset_criticality/common.gen.ts
+++ x-pack/plugins/security_solution/common/api/asset_criticality/common.gen.ts
@@ -37,15 +37,3 @@ export const AssetCriticalityRecord = CreateAssetCriticalityRecord.and(
     '@timestamp': z.string().datetime(),
   })
 );
-
-export type AssetCriticalityRecordIdParts = z.infer<typeof AssetCriticalityRecordIdParts>;
-export const AssetCriticalityRecordIdParts = z.object({
-  /**
-   * The ID value of the asset.
-   */
-  id_value: z.string(),
-  /**
-   * The field representing the ID.
-   */
-  id_field: z.string(),
-});
