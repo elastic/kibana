943c1979c53b0ea756e694f14a01828b365e7548
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-11-29 15:16:40 +0100

    remove cert as we start Kibana without TLS

--- x-pack/test_serverless/shared/services/supertest.ts
+++ x-pack/test_serverless/shared/services/supertest.ts
@@ -6,46 +6,14 @@
  */
 
 import { format as formatUrl } from 'url';
-import supertest, { type SuperAgentTest } from 'supertest';
+import supertest from 'supertest';
 import { FtrProviderContext } from '../../functional/ftr_provider_context';
-
-/**
- * Return a new SuperAgentTest instance for every API call to avoid cookie caching.
- * If you want to verify cookies are empty use the following code:
- * import { CookieAccessInfo } from 'cookiejar';
- * console.log(JSON.stringify(agent.jar.getCookies(CookieAccessInfo.All)));
- */
-const initSuperAgentTest = (kbnUrl: string, ca?: string[]) => {
-  return {
-    get(url: string) {
-      const agent = supertest.agent(kbnUrl, { ca });
-      return agent.get(url);
-    },
-    delete(url: string) {
-      const agent = supertest.agent(kbnUrl, { ca });
-      return agent.delete(url);
-    },
-    post(url: string) {
-      const agent = supertest.agent(kbnUrl, { ca });
-      return agent.post(url);
-    },
-    patch(url: string) {
-      const agent = supertest.agent(kbnUrl, { ca });
-      return agent.patch(url);
-    },
-    put(url: string) {
-      const agent = supertest.agent(kbnUrl, { ca });
-      return agent.put(url);
-    },
-  } as Pick<SuperAgentTest, 'get' | 'delete' | 'post' | 'patch' | 'put'>;
-};
 
 export function SupertestProvider({ getService }: FtrProviderContext) {
   const config = getService('config');
   const kbnUrl = formatUrl(config.get('servers.kibana'));
-  const ca = config.get('servers.kibana').certificateAuthorities;
 
-  return initSuperAgentTest(kbnUrl, ca);
+  return supertest(kbnUrl);
 }
 
 export function SupertestWithoutAuthProvider({ getService }: FtrProviderContext) {
@@ -54,7 +22,6 @@ export function SupertestWithoutAuthProvider({ getService }: FtrProviderContext)
     ...config.get('servers.kibana'),
     auth: false,
   });
-  const ca = config.get('servers.kibana').certificateAuthorities as string[];
 
-  return initSuperAgentTest(kbnUrl, ca);
+  return supertest(kbnUrl);
 }
