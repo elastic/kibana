7ce0dcdf6a5b9b00032476f29ff29e8bc3f0afb8
Samiul Monir <samiul.monir@elastic.co>
2023-11-29 10:15:58 -0500

    Fixing elements gap with guttersize

--- x-pack/plugins/serverless_search/public/application/components/pipeline_panel.tsx
+++ x-pack/plugins/serverless_search/public/application/components/pipeline_panel.tsx
@@ -27,79 +27,95 @@ export const PipelinePanel: React.FC = () => {
   return (
     <EuiThemeProvider colorMode="dark">
       <EuiPanel paddingSize="xl">
-        <EuiFlexGroup alignItems="flexStart" justifyContent="flexStart">
-          <EuiFlexItem grow={false}>
-            <EuiImage alt="cluster" src={`${assetBasePath}/cluster.svg`} />
+        <EuiFlexGroup direction="column" gutterSize="xl">
+          <EuiFlexItem>
+            <EuiFlexGroup alignItems="flexStart" justifyContent="flexStart">
+              <EuiFlexItem grow={false}>
+                <EuiImage alt="cluster" src={`${assetBasePath}/cluster.svg`} />
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiTitle size="s">
+                  <h3>
+                    {i18n.translate(
+                      'xpack.serverlessSearch.pipeline.overview.dataEnrichment.title',
+                      {
+                        defaultMessage: 'Enrich Data',
+                      }
+                    )}
+                  </h3>
+                </EuiTitle>
+                <EuiSpacer size="s" />
+                <EuiText size="s">
+                  <p>
+                    {i18n.translate(
+                      'xpack.serverlessSearch.pipeline.overview.dataEnrichment.description',
+                      {
+                        defaultMessage:
+                          'Add information from external sources or apply transformations to your documents for more contextual, insightful search.',
+                      }
+                    )}
+                  </p>
+                </EuiText>
+              </EuiFlexItem>
+            </EuiFlexGroup>
           </EuiFlexItem>
           <EuiFlexItem>
-            <EuiTitle size="s">
-              <h3>
-                {i18n.translate('xpack.serverlessSearch.pipeline.overview.dataEnrichment.title', {
-                  defaultMessage: 'Enrich Data',
-                })}
-              </h3>
-            </EuiTitle>
-            <EuiSpacer size="s" />
-            <EuiText size="s">
-              <p>
-                {i18n.translate(
-                  'xpack.serverlessSearch.pipeline.overview.dataEnrichment.description',
-                  {
-                    defaultMessage:
-                      'Add information from external sources or apply transformations to your documents for more contextual, insightful search.',
-                  }
-                )}
-              </p>
-            </EuiText>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiSpacer size="xl" />
-        <EuiFlexGroup alignItems="flexStart" justifyContent="flexStart">
-          <EuiFlexItem grow={false}>
-            <EuiImage alt="cut" src={`${assetBasePath}/cut.svg`} />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiTitle size="s">
-              <h3>
-                {i18n.translate('xpack.serverlessSearch.pipeline.overview.extAndStandard.title', {
-                  defaultMessage: 'Extract and standardize',
-                })}
-              </h3>
-            </EuiTitle>
-            <EuiSpacer size="s" />
-            <EuiText size="s">
-              {i18n.translate(
-                'xpack.serverlessSearch.pipeline.overview.extAndStandard.description',
-                {
-                  defaultMessage:
-                    'Parse information from your documents to ensure they conform to a standardized format.',
-                }
-              )}
-            </EuiText>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiSpacer size="xl" />
-        <EuiFlexGroup alignItems="flexStart" justifyContent="flexStart">
-          <EuiFlexItem grow={false}>
-            <EuiImage alt="reporter" src={`${assetBasePath}/reporter.svg`} />
+            <EuiFlexGroup alignItems="flexStart" justifyContent="flexStart">
+              <EuiFlexItem grow={false}>
+                <EuiImage alt="cut" src={`${assetBasePath}/cut.svg`} />
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiTitle size="s">
+                  <h3>
+                    {i18n.translate(
+                      'xpack.serverlessSearch.pipeline.overview.extAndStandard.title',
+                      {
+                        defaultMessage: 'Extract and standardize',
+                      }
+                    )}
+                  </h3>
+                </EuiTitle>
+                <EuiSpacer size="s" />
+                <EuiText size="s">
+                  {i18n.translate(
+                    'xpack.serverlessSearch.pipeline.overview.extAndStandard.description',
+                    {
+                      defaultMessage:
+                        'Parse information from your documents to ensure they conform to a standardized format.',
+                    }
+                  )}
+                </EuiText>
+              </EuiFlexItem>
+            </EuiFlexGroup>
           </EuiFlexItem>
           <EuiFlexItem>
-            <EuiTitle size="s">
-              <h3>
-                {i18n.translate('xpack.serverlessSearch.pipeline.overview.anonymization.title', {
-                  defaultMessage: 'Anonymize data',
-                })}
-              </h3>
-            </EuiTitle>
-            <EuiSpacer size="s" />
-            <EuiText size="s">
-              {i18n.translate(
-                'xpack.serverlessSearch.pipeline.overview.anonymization.description',
-                {
-                  defaultMessage: 'Remove sensitive information from documents before indexing.',
-                }
-              )}
-            </EuiText>
+            <EuiFlexGroup alignItems="flexStart" justifyContent="flexStart">
+              <EuiFlexItem grow={false}>
+                <EuiImage alt="reporter" src={`${assetBasePath}/reporter.svg`} />
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiTitle size="s">
+                  <h3>
+                    {i18n.translate(
+                      'xpack.serverlessSearch.pipeline.overview.anonymization.title',
+                      {
+                        defaultMessage: 'Anonymize data',
+                      }
+                    )}
+                  </h3>
+                </EuiTitle>
+                <EuiSpacer size="s" />
+                <EuiText size="s">
+                  {i18n.translate(
+                    'xpack.serverlessSearch.pipeline.overview.anonymization.description',
+                    {
+                      defaultMessage:
+                        'Remove sensitive information from documents before indexing.',
+                    }
+                  )}
+                </EuiText>
+              </EuiFlexItem>
+            </EuiFlexGroup>
           </EuiFlexItem>
         </EuiFlexGroup>
       </EuiPanel>
