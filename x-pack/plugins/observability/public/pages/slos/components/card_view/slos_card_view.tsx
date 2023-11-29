59d3d557519269b564bb6f1806789d583df964d9
shahzad31 <shahzad31comp@gmail.com>
2023-11-29 13:20:04 +0100

    fix types

--- x-pack/plugins/observability/public/pages/slos/components/card_view/slos_card_view.tsx
+++ x-pack/plugins/observability/public/pages/slos/components/card_view/slos_card_view.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 import { EuiFlexGrid, EuiFlexItem, EuiPanel, EuiSkeletonText } from '@elastic/eui';
 import { SLOWithSummaryResponse, ALL_VALUE } from '@kbn/slo-schema';
 import { EuiFlexGridProps } from '@elastic/eui/src/components/flex/flex_grid';
-import { ActiveAlerts } from '../../../../hooks/slo/use_fetch_active_alerts';
+import { ActiveAlerts } from '../../../../hooks/slo/active_alerts';
 import type { UseFetchRulesForSloResponse } from '../../../../hooks/slo/use_fetch_rules_for_slo';
 import { useFetchHistoricalSummary } from '../../../../hooks/slo/use_fetch_historical_summary';
 import { SloCardItem } from './slo_card_item';
