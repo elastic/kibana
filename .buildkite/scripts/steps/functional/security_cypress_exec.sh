f5a0bbfb8a3152ac61817c6a6fc9b398a0f661e7
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-11-29 23:13:15 +0200

    Added a guard to check if the provided target script exists in the package.json file

--- .buildkite/scripts/steps/functional/security_cypress_exec.sh
+++ .buildkite/scripts/steps/functional/security_cypress_exec.sh
@@ -12,5 +12,15 @@ echo $MSG
 
 cd $CURRENT_WORKING_DIR
 
+TARGETS_ARRAY=$(jq .scripts package.json | jq 'keys')
+if [[ " ${TARGETS_ARRAY[*]} " == *"$TARGET"* ]]; then
+    echo "Target '$TARGET' exists in the available targets in package.json"
+    echo "Proceeding in test run!"
+else
+    echo "The provided target '$TARGET' could not be found in the available targets in package.json"
+    echo "Abort the test runtime due to unexpected target script"
+    exit 1
+fi
+
 set +e
 yarn $TARGET; status=$?; yarn junit:merge || :; exit $status
