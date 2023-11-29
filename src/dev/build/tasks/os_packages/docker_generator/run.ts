cdcf3f5a1ef0c909613a636031dfef54fd59f101
Jon <jon@elastic.co>
2023-11-29 11:50:22 -0600

    [build] Rename ubi9 image to ubi (#171424)
    Future ubi image names will be version agnostic.  Removal of ubi8 TBD.
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/dev/build/tasks/os_packages/docker_generator/run.ts
+++ src/dev/build/tasks/os_packages/docker_generator/run.ts
@@ -45,7 +45,7 @@ export async function runDockerGenerator(
 
   let imageFlavor = '';
   if (flags.ubi8) imageFlavor += '-ubi8';
-  if (flags.ubi9) imageFlavor += '-ubi9';
+  if (flags.ubi9) imageFlavor += '-ubi';
   if (flags.ironbank) imageFlavor += '-ironbank';
   if (flags.cloud) imageFlavor += '-cloud';
 
