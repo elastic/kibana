d3e9ab7ceaa32124b1762113952e440eb8d83ea7
Ignacio Rivas <rivasign@gmail.com>
2023-11-29 17:10:09 +0100

    [Index Management] Add index component templates api tests (#171981)

--- x-pack/test_serverless/api_integration/test_suites/common/index_management/index_component_templates.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/index_management/index_component_templates.ts
@@ -8,35 +8,50 @@
 import expect from '@kbn/expect';
 
 import { FtrProviderContext } from '../../../ftr_provider_context';
-import { componentTemplatesApi } from './lib/component_templates.api';
-import { componentTemplateHelpers } from './lib/component_template.helpers';
-import { API_BASE_PATH } from './constants';
 
 const CACHE_TEMPLATES = true;
 
 export default function ({ getService }: FtrProviderContext) {
   const log = getService('log');
-  const supertest = getService('supertest');
+  const indexManagementService = getService('indexManagement');
 
-  const {
-    createComponentTemplate,
-    getAllComponentTemplates,
-    getOneComponentTemplate,
-    updateComponentTemplate,
-    deleteComponentTemplate,
-    getComponentTemplateDatastreams,
-  } = componentTemplatesApi(getService);
-  const {
-    addDatastream,
-    addIndexTemplate,
-    addComponentTemplate,
-    removeComponentTemplate,
-    cleanupDatastreams,
-    cleanUpIndexTemplates,
-    cleanUpComponentTemplates,
-  } = componentTemplateHelpers(getService);
+  describe('component templates', () => {
+    // Api methods
+    let getAllComponentTemplates: typeof indexManagementService['componentTemplates']['api']['getAllComponentTemplates'];
+    let getOneComponentTemplate: typeof indexManagementService['componentTemplates']['api']['getOneComponentTemplate'];
+    let updateComponentTemplate: typeof indexManagementService['componentTemplates']['api']['updateComponentTemplate'];
+    let deleteComponentTemplate: typeof indexManagementService['componentTemplates']['api']['deleteComponentTemplate'];
+    let getComponentTemplateDatastreams: typeof indexManagementService['componentTemplates']['api']['getComponentTemplateDatastreams'];
+    // Helpers
+    let addDatastream: typeof indexManagementService['componentTemplates']['helpers']['addDatastream'];
+    let addIndexTemplate: typeof indexManagementService['componentTemplates']['helpers']['addIndexTemplate'];
+    let addComponentTemplate: typeof indexManagementService['componentTemplates']['helpers']['addComponentTemplate'];
+    let cleanupDatastreams: typeof indexManagementService['componentTemplates']['helpers']['cleanupDatastreams'];
+    let cleanUpIndexTemplates: typeof indexManagementService['componentTemplates']['helpers']['cleanUpIndexTemplates'];
+    let cleanUpComponentTemplates: typeof indexManagementService['componentTemplates']['helpers']['cleanUpComponentTemplates'];
 
-  describe('Component templates', function () {
+    before(async () => {
+      ({
+        componentTemplates: {
+          api: {
+            getAllComponentTemplates,
+            getOneComponentTemplate,
+            updateComponentTemplate,
+            deleteComponentTemplate,
+            getComponentTemplateDatastreams,
+          },
+          helpers: {
+            addDatastream,
+            addIndexTemplate,
+            addComponentTemplate,
+            cleanupDatastreams,
+            cleanUpIndexTemplates,
+            cleanUpComponentTemplates,
+          },
+        },
+      } = indexManagementService);
+    });
+
     after(async () => {
       await cleanUpIndexTemplates();
       await cleanUpComponentTemplates();
@@ -114,50 +129,46 @@ export default function ({ getService }: FtrProviderContext) {
       });
     });
 
-    describe('Create', () => {
-      const COMPONENT_NAME = 'test_create_component_template';
-      const REQUIRED_FIELDS_COMPONENT_NAME = 'test_create_required_fields_component_template';
-
-      after(async () => {
-        // Clean up any component templates created in test cases
-        await Promise.all(
-          [COMPONENT_NAME, REQUIRED_FIELDS_COMPONENT_NAME].map(removeComponentTemplate)
-        ).catch((err) => {
-          log.debug(`[Cleanup error] Error deleting component templates: ${err.message}`);
-          throw err;
-        });
-      });
-
-      it('should create a component template', async () => {
-        const { body } = await createComponentTemplate(COMPONENT_NAME, {
-          version: 1,
-          template: {
-            settings: {
+    describe('Update', () => {
+      const COMPONENT_NAME = 'test_update_component_template';
+      const COMPONENT = {
+        template: {
+          settings: {
+            index: {
               number_of_shards: 1,
             },
-            aliases: {
-              alias1: {},
+          },
+          mappings: {
+            _source: {
+              enabled: false,
             },
-            mappings: {
-              properties: {
-                host_name: {
-                  type: 'keyword',
-                },
+            properties: {
+              host_name: {
+                type: 'keyword',
+              },
+              created_at: {
+                type: 'date',
+                format: 'EEE MMM dd HH:mm:ss Z yyyy',
               },
             },
-            lifecycle: {
-              // @ts-expect-error @elastic/elasticsearch enabled prop is still not in the ES types
-              enabled: true,
-              data_retention: '2d',
-            },
           },
-          _meta: {
-            description: 'set number of shards to one',
-            serialization: {
-              class: 'MyComponentTemplate',
-              id: 10,
-            },
-          },
+        },
+      };
+
+      before(async () => {
+        // Create component template that can be used to test PUT request
+        try {
+          await addComponentTemplate({ body: COMPONENT, name: COMPONENT_NAME }, CACHE_TEMPLATES);
+        } catch (err) {
+          log.debug('[Setup error] Error creating component template');
+          throw err;
+        }
+      });
+
+      it('should allow an existing component template to be updated', async () => {
+        const { body } = await updateComponentTemplate(COMPONENT_NAME, {
+          ...COMPONENT,
+          version: 1,
           _kbnMeta: {
             usedBy: [],
             isManaged: false,
@@ -169,34 +180,32 @@ export default function ({ getService }: FtrProviderContext) {
         });
       });
 
-      it('should create a component template with only required fields', async () => {
-        const { body } = await createComponentTemplate(REQUIRED_FIELDS_COMPONENT_NAME, {
-          // Excludes version and _meta fields
-          template: {},
+      it('should not allow a non-existing component template to be updated', async () => {
+        const { body } = await updateComponentTemplate('component_does_not_exist', {
+          ...COMPONENT,
+          version: 1,
           _kbnMeta: {
             usedBy: [],
             isManaged: false,
           },
-        }).expect(200);
+        }).expect(404);
 
         expect(body).to.eql({
-          acknowledged: true,
-        });
-      });
-
-      it('should not allow creation of a component template with the same name of an existing one', async () => {
-        const { body } = await createComponentTemplate(COMPONENT_NAME, {
-          template: {},
-          _kbnMeta: {
-            usedBy: [],
-            isManaged: false,
+          statusCode: 404,
+          error: 'Not Found',
+          message: 'component template matching [component_does_not_exist] not found',
+          attributes: {
+            error: {
+              reason: 'component template matching [component_does_not_exist] not found',
+              root_cause: [
+                {
+                  reason: 'component template matching [component_does_not_exist] not found',
+                  type: 'resource_not_found_exception',
+                },
+              ],
+              type: 'resource_not_found_exception',
+            },
           },
-        }).expect(409);
-
-        expect(body).to.eql({
-          statusCode: 409,
-          error: 'Conflict',
-          message: `There is already a component template with name '${COMPONENT_NAME}'.`,
         });
       });
     });
@@ -357,21 +366,6 @@ export default function ({ getService }: FtrProviderContext) {
           ],
           type: 'resource_not_found_exception',
           reason: 'component_does_not_exist',
-        });
-      });
-    });
-
-    describe('Privileges', () => {
-      it('should return privileges result', async () => {
-        const uri = `${API_BASE_PATH}/component_templates/privileges`;
-
-        const { body } = await supertest.get(uri).set('kbn-xsrf', 'xxx').expect(200);
-
-        expect(body).to.eql({
-          hasAllPrivileges: true,
-          missingPrivileges: {
-            cluster: [],
-          },
         });
       });
     });
