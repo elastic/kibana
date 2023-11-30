1f078266cb1f1b5679b8238a244a6b89ae92c787
Adam Demjen <demjened@gmail.com>
2023-11-30 15:09:38 -0500

    Replace model select dropdown with list, add polling and action buttons

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/model_select_option.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/model_select_option.tsx
@@ -5,55 +5,202 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 
-import { EuiFlexGroup, EuiFlexItem, EuiTextColor, EuiTitle } from '@elastic/eui';
+import { useActions, useValues } from 'kea';
 
 import {
-  getMlModelTypesForModelConfig,
-  parseModelStateFromStats,
-  parseModelStateReasonFromStats,
-} from '../../../../../../../common/ml_inference_pipeline';
-import { TrainedModel } from '../../../../api/ml_models/ml_trained_models_logic';
-import { getMLType, getModelDisplayTitle } from '../../../shared/ml_inference/utils';
+  EuiBadge,
+  EuiButton,
+  EuiButtonEmpty,
+  EuiButtonIcon,
+  EuiContextMenu,
+  EuiContextMenuPanelDescriptor,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPopover,
+  EuiRadio,
+  EuiText,
+  EuiTextColor,
+  EuiTitle,
+} from '@elastic/eui';
 
+import { i18n } from '@kbn/i18n';
+
+import { MlModel, MlModelDeploymentState } from '../../../../../../../common/types/ml';
+import { KibanaLogic } from '../../../../../shared/kibana';
 import { TrainedModelHealth } from '../ml_model_health';
-import { MLModelTypeBadge } from '../ml_model_type_badge';
 
-export interface MlModelSelectOptionProps {
-  model: TrainedModel;
-}
-export const MlModelSelectOption: React.FC<MlModelSelectOptionProps> = ({ model }) => {
-  const type = getMLType(getMlModelTypesForModelConfig(model));
-  const title = getModelDisplayTitle(type);
+import { ModelSelectLogic } from './model_select_logic';
+
+export const getContextMenuPanel = (
+  modelDetailsPageUrl?: string
+): EuiContextMenuPanelDescriptor[] => {
+  return [
+    {
+      id: 0,
+      items: [
+        {
+          name: 'Tune model performance',
+          icon: 'controlsHorizontal',
+          onClick: () =>
+            KibanaLogic.values.navigateToUrl('/app/ml/trained_models', {
+              shouldNotCreateHref: true,
+            }),
+        },
+        ...(modelDetailsPageUrl
+          ? [
+              {
+                name: 'Model details',
+                icon: 'popout',
+                href: modelDetailsPageUrl,
+                target: '_blank',
+              },
+            ]
+          : []),
+      ],
+    },
+  ];
+};
+
+export type ModelSelectOptionProps = MlModel & {
+  label: string;
+  checked?: 'on';
+};
+
+export const DeployModelButton: React.FC<{ onClick: () => void; disabled: boolean }> = ({
+  onClick,
+  disabled,
+}) => {
   return (
-    <EuiFlexGroup direction="column" gutterSize="xs">
-      <EuiFlexItem>
-        <EuiTitle size="xs">
-          <h4>{title ?? model.model_id}</h4>
-        </EuiTitle>
+    <EuiButtonEmpty onClick={onClick} disabled={disabled} iconType="download" size="s">
+      {i18n.translate(
+        'xpack.enterpriseSearch.content.indices.pipelines.modelSelectOption.deployButton.label',
+        {
+          defaultMessage: 'Deploy',
+        }
+      )}
+    </EuiButtonEmpty>
+  );
+};
+
+export const StartModelButton: React.FC<{ onClick: () => void; disabled: boolean }> = ({
+  onClick,
+  disabled,
+}) => {
+  return (
+    <EuiButton onClick={onClick} disabled={disabled} color="success" iconType="play" size="s">
+      {i18n.translate(
+        'xpack.enterpriseSearch.content.indices.pipelines.modelSelectOption.startButton.label',
+        {
+          defaultMessage: 'Start',
+        }
+      )}
+    </EuiButton>
+  );
+};
+
+export const ModelMenuPopover: React.FC<{
+  onClick: () => void;
+  closePopover: () => void;
+  isOpen: boolean;
+  modelDetailsPageUrl?: string;
+}> = ({ onClick, closePopover, isOpen, modelDetailsPageUrl }) => {
+  return (
+    <EuiPopover
+      button={<EuiButtonIcon onClick={onClick} iconType="boxesHorizontal" />}
+      isOpen={isOpen}
+      closePopover={closePopover}
+      anchorPosition="leftCenter"
+      panelPaddingSize="none"
+    >
+      <EuiContextMenu panels={getContextMenuPanel(modelDetailsPageUrl)} initialPanelId={0} />
+    </EuiPopover>
+  );
+};
+
+export const ModelSelectOption: React.FC<ModelSelectOptionProps> = ({
+  modelId,
+  title,
+  description,
+  license,
+  deploymentState,
+  deploymentStateReason,
+  modelDetailsPageUrl,
+  isPlaceholder,
+  checked,
+}) => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const onMenuButtonClick = () => setIsPopoverOpen((isOpen) => !isOpen);
+  const closePopover = () => setIsPopoverOpen(false);
+
+  const { createModel, startModel } = useActions(ModelSelectLogic);
+  const { areActionButtonsDisabled } = useValues(ModelSelectLogic);
+
+  return (
+    <EuiFlexGroup alignItems="center">
+      {/* Selection radio button */}
+      <EuiFlexItem grow={false} style={{ flexShrink: 0 }}>
+        <EuiRadio id={modelId} checked={checked === 'on'} onChange={() => null} />
       </EuiFlexItem>
-      <EuiFlexItem>
-        <EuiFlexGroup gutterSize="s" alignItems="center" justifyContent="flexEnd">
-          {title && (
-            <EuiFlexItem>
-              <EuiTextColor color="subdued">{model.model_id}</EuiTextColor>
-            </EuiFlexItem>
-          )}
-          <EuiFlexItem grow={false}>
-            <TrainedModelHealth
-              modelState={parseModelStateFromStats(model)}
-              modelStateReason={parseModelStateReasonFromStats(model)}
-            />
+      {/* Title, model ID, description, license */}
+      <EuiFlexItem style={{ overflow: 'hidden' }}>
+        <EuiFlexGroup direction="column" gutterSize="xs">
+          <EuiFlexItem>
+            <EuiTitle size="xs">
+              <h4>{title}</h4>
+            </EuiTitle>
           </EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            <EuiFlexGroup gutterSize="xs">
-              <EuiFlexItem>
-                <MLModelTypeBadge type={type} />
-              </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiTextColor color="subdued">{modelId}</EuiTextColor>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiFlexGroup gutterSize="xs" alignItems="center">
+              {license && (
+                <EuiFlexItem grow={false}>
+                  <EuiBadge color="hollow">License: {license}</EuiBadge>
+                </EuiFlexItem>
+              )}
+              {description && (
+                <EuiFlexItem style={{ overflow: 'hidden' }}>
+                  <EuiText size="xs">
+                    <div className="eui-textTruncate" title={description}>
+                      {description}
+                    </div>
+                  </EuiText>
+                </EuiFlexItem>
+              )}
             </EuiFlexGroup>
           </EuiFlexItem>
         </EuiFlexGroup>
+      </EuiFlexItem>
+      {/* Status indicator OR action button */}
+      <EuiFlexItem grow={false} style={{ flexShrink: 0 }}>
+        {isPlaceholder ? (
+          <DeployModelButton
+            onClick={() => createModel(modelId)}
+            disabled={areActionButtonsDisabled}
+          />
+        ) : deploymentState === MlModelDeploymentState.Downloaded ? (
+          <StartModelButton
+            onClick={() => startModel(modelId)}
+            disabled={areActionButtonsDisabled}
+          />
+        ) : (
+          <TrainedModelHealth
+            modelState={deploymentState}
+            modelStateReason={deploymentStateReason}
+          />
+        )}
+      </EuiFlexItem>
+      {/* Actions menu */}
+      <EuiFlexItem grow={false} style={{ flexShrink: 0 }}>
+        <ModelMenuPopover
+          onClick={onMenuButtonClick}
+          isOpen={isPopoverOpen}
+          closePopover={closePopover}
+          modelDetailsPageUrl={modelDetailsPageUrl}
+        />
       </EuiFlexItem>
     </EuiFlexGroup>
   );
