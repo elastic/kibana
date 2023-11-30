f924eca874f86ab9c01d500bb4eb85b1ef1a464b
Chenhui Wang <wangch079@outlook.com>
2023-11-30 21:07:00 +0800

    add collectors

---
+++ x-pack/plugins/enterprise_search/server/collectors/connectors/telemetry.test.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { fetchTelemetryMetrics } from '@kbn/search-connectors';
+import { mockLogger } from '../../__mocks__';
+
+import { registerTelemetryUsageCollector } from './telemetry';
+
+jest.mock('@kbn/search-connectors', () => ({
+  fetchTelemetryMetrics: jest.fn(),
+}));
+
+describe('Connectors Telemetry Usage Collector', () => {
+  const makeUsageCollectorStub = jest.fn();
+  const registerStub = jest.fn();
+  const usageCollectionMock = {
+    makeUsageCollector: makeUsageCollectorStub,
+    registerCollector: registerStub,
+  } as any;
+  const clientMock = {} as any;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('registerTelemetryUsageCollector', () => {
+    it('should make and register the usage collector', () => {
+      registerTelemetryUsageCollector(usageCollectionMock, clientMock, mockLogger);
+
+      expect(registerStub).toHaveBeenCalledTimes(1);
+      expect(makeUsageCollectorStub).toHaveBeenCalledTimes(1);
+      expect(makeUsageCollectorStub.mock.calls[0][0].type).toBe('connectors');
+      expect(makeUsageCollectorStub.mock.calls[0][0].isReady()).toBe(true);
+    });
+  });
+
+  describe('fetchTelemetryMetrics', () => {
+    it('should return telemetry data', async () => {
+      (fetchTelemetryMetrics as jest.Mock).mockImplementationOnce(() =>
+        Promise.resolve({
+          native: { total: 5 },
+          clients: { total: 2 },
+        }))
+      registerTelemetryUsageCollector(usageCollectionMock, clientMock, mockLogger);
+      const telemetryMetrics = await makeUsageCollectorStub.mock.calls[0][0].fetch();
+
+      expect(telemetryMetrics).toEqual({
+        native: {
+          total: 5,
+        },
+        clients: {
+          total: 2,
+        },
+      });
+    });
+  });
+});
---
+++ x-pack/plugins/enterprise_search/server/collectors/connectors/telemetry.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import {IClusterClient, Logger} from '@kbn/core/server';
+
+import { UsageCollectionSetup } from '@kbn/usage-collection-plugin/server';
+import {fetchTelemetryMetrics, Telemetry} from "@kbn/search-connectors";
+
+
+/**
+ * Register the telemetry collector
+ */
+
+export const registerTelemetryUsageCollector = (
+  usageCollection: UsageCollectionSetup,
+  client: IClusterClient,
+  log: Logger
+) => {
+  const telemetryUsageCollector = usageCollection.makeUsageCollector<Telemetry>({
+    type: 'connectors',
+    fetch: async () => fetchTelemetryMetrics(client, log),
+    isReady: () => true,
+    schema: {
+      native: {
+        total: { type: 'long' },
+      },
+      clients: {
+        total: { type: 'long' },
+      },
+    },
+  });
+  usageCollection.registerCollector(telemetryUsageCollector);
+};
