d7105d88b9cdf570b37cc5a4f55731ceb57fa771
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-12-01 14:14:39 +0100

    redundant white space

--- x-pack/plugins/security_solution/server/endpoint/routes/resolver/tree/queries/lifecycle.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/resolver/tree/queries/lifecycle.ts
@@ -18,7 +18,6 @@ import { BaseResolverQuery } from './base';
  */
 export class LifecycleQuery extends BaseResolverQuery {
   declare readonly resolverFields: JsonValue[];
-
   constructor({ schema, indexPatterns, timeRange, isInternalRequest }: ResolverQueryParams) {
     super({ schema, indexPatterns, timeRange, isInternalRequest });
   }
