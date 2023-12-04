e795826c7ebdf0de3ef4210b1b208ea6b0bff539
Aleh Zasypkin <aleh.zasypkin@elastic.co>
2023-12-04 17:24:02 +0100

    Review#1: add more JWT authc tests, update docs with new default headers.

--- x-pack/test/security_api_integration/api_keys.config.ts
+++ x-pack/test/security_api_integration/api_keys.config.ts
@@ -5,62 +5,22 @@
  * 2.0.
  */
 
-import { resolve } from 'path';
 import { FtrConfigProviderContext } from '@kbn/test';
 import { services } from './services';
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
-  const xPackAPITestsConfig = await readConfigFile(require.resolve('../api_integration/config.ts'));
-
-  const testEndpointsPlugin = resolve(__dirname, '../security_functional/plugins/test_endpoints');
-  const jwksPath = require.resolve('@kbn/security-api-integration-helpers/oidc/jwks.json');
+  const httpBearerAPITestsConfig = await readConfigFile(require.resolve('./http_bearer.config.ts'));
 
   return {
     testFiles: [require.resolve('./tests/api_keys')],
-    servers: xPackAPITestsConfig.get('servers'),
-    security: { disableTestUser: true },
+    servers: httpBearerAPITestsConfig.get('servers'),
+    security: httpBearerAPITestsConfig.get('security'),
     services,
     junit: {
       reportName: 'X-Pack Security API Integration Tests (Api Keys)',
     },
 
-    esTestCluster: {
-      ...xPackAPITestsConfig.get('esTestCluster'),
-      serverArgs: [
-        ...xPackAPITestsConfig.get('esTestCluster.serverArgs'),
-        'xpack.security.authc.token.enabled=true',
-
-        // JWT WITH shared secret
-        'xpack.security.authc.realms.jwt.jwt_with_secret.allowed_audiences=elasticsearch',
-        `xpack.security.authc.realms.jwt.jwt_with_secret.allowed_issuer=https://kibana.elastic.co/jwt/`,
-        `xpack.security.authc.realms.jwt.jwt_with_secret.allowed_signature_algorithms=[RS256]`,
-        `xpack.security.authc.realms.jwt.jwt_with_secret.allowed_subjects=elastic-agent`,
-        `xpack.security.authc.realms.jwt.jwt_with_secret.claims.principal=sub`,
-        'xpack.security.authc.realms.jwt.jwt_with_secret.client_authentication.type=shared_secret',
-        `xpack.security.authc.realms.jwt.jwt_with_secret.client_authentication.shared_secret=my_super_secret`,
-        'xpack.security.authc.realms.jwt.jwt_with_secret.order=0',
-        `xpack.security.authc.realms.jwt.jwt_with_secret.pkc_jwkset_path=${jwksPath}`,
-        `xpack.security.authc.realms.jwt.jwt_with_secret.token_type=access_token`,
-
-        // JWT WITHOUT shared secret
-        'xpack.security.authc.realms.jwt.jwt_without_secret.allowed_audiences=elasticsearch',
-        `xpack.security.authc.realms.jwt.jwt_without_secret.allowed_issuer=https://kibana.elastic.co/jwt/no-secret`,
-        `xpack.security.authc.realms.jwt.jwt_without_secret.allowed_signature_algorithms=[RS256]`,
-        `xpack.security.authc.realms.jwt.jwt_without_secret.allowed_subjects=elastic-agent-no-secret`,
-        `xpack.security.authc.realms.jwt.jwt_without_secret.claims.principal=sub`,
-        'xpack.security.authc.realms.jwt.jwt_without_secret.client_authentication.type=none',
-        'xpack.security.authc.realms.jwt.jwt_without_secret.order=1',
-        `xpack.security.authc.realms.jwt.jwt_without_secret.pkc_jwkset_path=${jwksPath}`,
-        `xpack.security.authc.realms.jwt.jwt_without_secret.token_type=access_token`,
-      ],
-    },
-
-    kbnTestServer: {
-      ...xPackAPITestsConfig.get('kbnTestServer'),
-      serverArgs: [
-        ...xPackAPITestsConfig.get('kbnTestServer.serverArgs'),
-        `--plugin-path=${testEndpointsPlugin}`,
-      ],
-    },
+    esTestCluster: httpBearerAPITestsConfig.get('esTestCluster'),
+    kbnTestServer: httpBearerAPITestsConfig.get('kbnTestServer'),
   };
 }
