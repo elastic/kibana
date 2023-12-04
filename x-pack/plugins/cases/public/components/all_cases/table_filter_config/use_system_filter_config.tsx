90d6358add99dd23623a77bde0917b90b261e3a2
Julian Gernun <17549662+jcger@users.noreply.github.com>
2023-12-04 11:47:45 +0100

    [Cases] Cases Table: Configure Available Filters Including Custom Fields (#172276)
    Meta issue https://github.com/elastic/kibana/issues/167651
    Fixes: https://github.com/elastic/kibana/issues/167651
    
    ## Summary
    Previous PRs merged into this feature branch:
    - https://github.com/elastic/kibana/pull/169356
    - https://github.com/elastic/kibana/pull/169371
    - https://github.com/elastic/kibana/pull/170851
    - https://github.com/elastic/kibana/pull/171102
    - https://github.com/elastic/kibana/pull/171176
    
    ## Release notes
    Case list filter bar can now be customised. Filters can be removed and
    custom fields can be used as filters
    
    ## Pending issues
    - Table in modal shouldn’t load in local storage saved filter options of
    status/severity
    - Status & Severity filters in url. Filters must be activated if the
    user has them deactivated
    - UI overflow when to much filters are active
    - Race condition: When a user has a custom field active with an option
    selected and this custom field gets removed in settings, it includes the
    removed custom field when refreshing. This request will fail, triggering
    a second one which won't include the removed custom field
    - Found during QA. In the modal, when trying to select all options in
    the solutions filter, when checking the last unchecked option, it resets
    and there is no checked option anymore
    
    ## Flaky test runner link
    
    https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/4128
    
    ---------
    
    Co-authored-by: Antonio <antoniodcoelho@gmail.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/cases/public/components/all_cases/table_filter_config/use_system_filter_config.tsx
@@ -0,0 +1,272 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { useState, useEffect } from 'react';
+
+import type { FilterOptions } from '../../../../common/ui';
+import type { CaseStatuses } from '../../../../common/types/domain';
+import { MAX_TAGS_FILTER_LENGTH, MAX_CATEGORY_FILTER_LENGTH } from '../../../../common/constants';
+import { MultiSelectFilter, mapToMultiSelectOption } from '../multi_select_filter';
+import { SolutionFilter } from '../solution_filter';
+import { StatusFilter } from '../status_filter';
+import * as i18n from '../translations';
+import { SeverityFilter } from '../severity_filter';
+import { AssigneesFilterPopover } from '../assignees_filter';
+import type { CurrentUserProfile } from '../../types';
+import type { AssigneesFilteringSelection } from '../../user_profiles/types';
+import type { FilterChangeHandler, FilterConfig, FilterConfigRenderParams } from './types';
+
+interface UseFilterConfigProps {
+  availableSolutions: string[];
+  caseAssignmentAuthorized: boolean;
+  categories: string[];
+  countClosedCases: number | null;
+  countInProgressCases: number | null;
+  countOpenCases: number | null;
+  currentUserProfile: CurrentUserProfile;
+  handleSelectedAssignees: (newAssignees: AssigneesFilteringSelection[]) => void;
+  hiddenStatuses?: CaseStatuses[];
+  initialFilterOptions: Partial<FilterOptions>;
+  isLoading: boolean;
+  isSelectorView?: boolean;
+  onFilterOptionsChange: FilterChangeHandler;
+  selectedAssignees: AssigneesFilteringSelection[];
+  tags: string[];
+}
+
+export const getSystemFilterConfig = ({
+  availableSolutions,
+  caseAssignmentAuthorized,
+  categories,
+  countClosedCases,
+  countInProgressCases,
+  countOpenCases,
+  currentUserProfile,
+  handleSelectedAssignees,
+  hiddenStatuses,
+  initialFilterOptions,
+  isLoading,
+  isSelectorView,
+  onFilterOptionsChange,
+  selectedAssignees,
+  tags,
+}: UseFilterConfigProps): FilterConfig[] => {
+  const onSystemFilterChange = ({
+    filterId,
+    selectedOptionKeys,
+  }: {
+    filterId: string;
+    selectedOptionKeys: string[];
+  }) => {
+    onFilterOptionsChange({
+      [filterId]: selectedOptionKeys,
+    });
+  };
+  return [
+    {
+      key: 'severity',
+      label: i18n.SEVERITY,
+      isActive: true,
+      isAvailable: true,
+      getEmptyOptions: () => {
+        return {
+          severity: initialFilterOptions.severity || [],
+        };
+      },
+      render: ({ filterOptions }: FilterConfigRenderParams) => (
+        <SeverityFilter
+          selectedOptionKeys={filterOptions.severity}
+          onChange={onSystemFilterChange}
+        />
+      ),
+    },
+    {
+      key: 'status',
+      label: i18n.STATUS,
+      isActive: true,
+      isAvailable: true,
+      getEmptyOptions: () => {
+        return {
+          status: initialFilterOptions.status || [],
+        };
+      },
+      render: ({ filterOptions }: FilterConfigRenderParams) => (
+        <StatusFilter
+          selectedOptionKeys={filterOptions?.status}
+          onChange={onSystemFilterChange}
+          hiddenStatuses={hiddenStatuses}
+          countClosedCases={countClosedCases}
+          countInProgressCases={countInProgressCases}
+          countOpenCases={countOpenCases}
+        />
+      ),
+    },
+    {
+      key: 'assignees',
+      label: i18n.ASSIGNEES,
+      isActive: true,
+      isAvailable: caseAssignmentAuthorized && !isSelectorView,
+      getEmptyOptions: () => {
+        return {
+          assignees: initialFilterOptions.assignees || [],
+        };
+      },
+      render: ({ filterOptions }: FilterConfigRenderParams) => {
+        return (
+          <AssigneesFilterPopover
+            selectedAssignees={selectedAssignees}
+            currentUserProfile={currentUserProfile}
+            isLoading={isLoading}
+            onSelectionChange={handleSelectedAssignees}
+          />
+        );
+      },
+    },
+    {
+      key: 'tags',
+      label: i18n.TAGS,
+      isActive: true,
+      isAvailable: true,
+      getEmptyOptions: () => {
+        return {
+          tags: initialFilterOptions.tags || [],
+        };
+      },
+      render: ({ filterOptions }: FilterConfigRenderParams) => (
+        <MultiSelectFilter
+          buttonLabel={i18n.TAGS}
+          id={'tags'}
+          limit={MAX_TAGS_FILTER_LENGTH}
+          limitReachedMessage={i18n.MAX_SELECTED_FILTER(MAX_TAGS_FILTER_LENGTH, 'tags')}
+          onChange={onSystemFilterChange}
+          options={mapToMultiSelectOption(tags)}
+          selectedOptionKeys={filterOptions?.tags}
+        />
+      ),
+    },
+    {
+      key: 'category',
+      label: i18n.CATEGORIES,
+      isActive: true,
+      isAvailable: true,
+      getEmptyOptions: () => {
+        return {
+          category: initialFilterOptions.category || [],
+        };
+      },
+      render: ({ filterOptions }: FilterConfigRenderParams) => (
+        <MultiSelectFilter
+          buttonLabel={i18n.CATEGORIES}
+          id={'category'}
+          limit={MAX_CATEGORY_FILTER_LENGTH}
+          limitReachedMessage={i18n.MAX_SELECTED_FILTER(MAX_CATEGORY_FILTER_LENGTH, 'categories')}
+          onChange={onSystemFilterChange}
+          options={mapToMultiSelectOption(categories)}
+          selectedOptionKeys={filterOptions?.category}
+        />
+      ),
+    },
+    {
+      key: 'owner',
+      label: i18n.SOLUTION,
+      isActive: true,
+      isAvailable: availableSolutions.length > 1,
+      getEmptyOptions: () => {
+        return {
+          owner: initialFilterOptions.owner || [],
+        };
+      },
+      render: ({ filterOptions }: FilterConfigRenderParams) => (
+        <SolutionFilter
+          onChange={onSystemFilterChange}
+          selectedOptionKeys={filterOptions?.owner}
+          availableSolutions={availableSolutions}
+        />
+      ),
+    },
+  ];
+};
+
+export const useSystemFilterConfig = ({
+  availableSolutions,
+  caseAssignmentAuthorized,
+  categories,
+  countClosedCases,
+  countInProgressCases,
+  countOpenCases,
+  currentUserProfile,
+  handleSelectedAssignees,
+  hiddenStatuses,
+  initialFilterOptions,
+  isLoading,
+  isSelectorView,
+  onFilterOptionsChange,
+  selectedAssignees,
+  tags,
+}: UseFilterConfigProps) => {
+  const [filterConfig, setFilterConfig] = useState<FilterConfig[]>(() =>
+    getSystemFilterConfig({
+      availableSolutions,
+      caseAssignmentAuthorized,
+      categories,
+      countClosedCases,
+      countInProgressCases,
+      countOpenCases,
+      currentUserProfile,
+      handleSelectedAssignees,
+      hiddenStatuses,
+      initialFilterOptions,
+      isLoading,
+      isSelectorView,
+      onFilterOptionsChange,
+      selectedAssignees,
+      tags,
+    })
+  );
+
+  useEffect(() => {
+    setFilterConfig(
+      getSystemFilterConfig({
+        availableSolutions,
+        caseAssignmentAuthorized,
+        categories,
+        countClosedCases,
+        countInProgressCases,
+        countOpenCases,
+        currentUserProfile,
+        handleSelectedAssignees,
+        hiddenStatuses,
+        initialFilterOptions,
+        isLoading,
+        isSelectorView,
+        onFilterOptionsChange,
+        selectedAssignees,
+        tags,
+      })
+    );
+  }, [
+    availableSolutions,
+    caseAssignmentAuthorized,
+    categories,
+    countClosedCases,
+    countInProgressCases,
+    countOpenCases,
+    currentUserProfile,
+    handleSelectedAssignees,
+    hiddenStatuses,
+    initialFilterOptions,
+    isLoading,
+    isSelectorView,
+    onFilterOptionsChange,
+    selectedAssignees,
+    tags,
+  ]);
+
+  return {
+    systemFilterConfig: filterConfig,
+  };
+};
