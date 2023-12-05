03e3499a4172e93adf327a441c07a63acddd5859
Hannah Mudge <hannah.wright@elastic.co>
2023-12-05 15:37:28 -0700

    Remove `.only`

--- x-pack/test/functional/apps/dashboard/group2/sync_colors.ts
+++ x-pack/test/functional/apps/dashboard/group2/sync_colors.ts
@@ -35,7 +35,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     return colorMapping;
   }
 
-  describe.only('sync colors', function () {
+  describe('sync colors', function () {
     before(async function () {
       await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/logstash_functional');
       await kibanaServer.importExport.load(
