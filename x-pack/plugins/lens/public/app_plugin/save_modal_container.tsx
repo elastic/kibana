elastic/kibana

commit 4a2f63c7e5378a1f0e21da89e458ee680e820024

[SecuritySolution] Add save to library action to Security visualizations (#167019)
## Summary

Previously if uses want to save a visualization from Security Solution.
They have to `Open in Lens` first and save it from there. In this PR, we
add a shortcut to the context menu of visualisation so users can save
the visualization without leaving SecuritySolution.

<img width="2544" alt="Screenshot 2023-11-14 at 15 05 09"
src="https://github.com/elastic/kibana/assets/6295984/76c72224-12e8-4e96-8beb-caf1cceed8ef">

## Where to find the action:

### When clicking on the `...`button of visualizations:


https://github.com/elastic/kibana/assets/6295984/fea12472-311a-4bf7-93e7-37c4f6025a5b



### When `editing` dashboard from SecuritySolution:
- Visualizations existing in the library won't have `Save to library`
action.
- Visualizations unlinked from library or never exists in library have
`Save to library` action.



https://github.com/elastic/kibana/assets/6295984/45d7a868-88c4-43e0-bd88-4b907f028d13


## Scenarios:

### Add to library only:




https://github.com/elastic/kibana/assets/6295984/8c1b6a37-7422-4792-a1f3-28af585fd700




### Save visualization to a new dashboard + Add to library:



https://github.com/elastic/kibana/assets/6295984/6f718164-894f-4cbf-b93b-f63904497be6

### Save visualization to a new dashboard only (**NO** added to
library):



https://github.com/elastic/kibana/assets/6295984/31c9a045-de13-4861-b6f9-afb7096f7fdd




### Save visualization to an existing dashboard + Add to library:



https://github.com/elastic/kibana/assets/6295984/240e3ad4-078e-4937-b931-b3b1d674b6a2



### Save visualization to an existing dashboard only (**NO** added to
library):




https://github.com/elastic/kibana/assets/6295984/174cfe2c-c765-4a19-9e32-6012a50624ba

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/lens/public/app_plugin/save_modal_container.tsx
@@ -25,11 +25,12 @@ type ExtraProps = Pick<LensAppProps, 'initialInput'> &
 
 export type SaveModalContainerProps = {
   originatingApp?: string;
+  getOriginatingPath?: (dashboardId: string) => string;
   persistedDoc?: Document;
   lastKnownDoc?: Document;
   returnToOriginSwitchLabel?: string;
   onClose: () => void;
-  onSave?: () => void;
+  onSave?: (saveProps: SaveProps) => void;
   runSave?: (saveProps: SaveProps, options: { saveToLibrary: boolean }) => void;
   isSaveable?: boolean;
   getAppNameFromId?: () => string | undefined;
@@ -44,6 +45,7 @@ export function SaveModalContainer({
   runSave,
   persistedDoc,
   originatingApp,
+  getOriginatingPath,
   initialInput,
   redirectTo,
   redirectToOrigin,
@@ -119,6 +121,7 @@ export function SaveModalContainer({
           redirectTo,
           redirectToOrigin,
           originatingApp,
+          getOriginatingPath,
           getIsByValueMode: () => false,
           onAppLeave: () => {},
           savedObjectStore: lensServices.savedObjectStore,
@@ -126,7 +129,7 @@ export function SaveModalContainer({
         saveProps,
         options
       ).then(() => {
-        onSave?.();
+        onSave?.(saveProps);
         onClose();
       });
     }
@@ -137,6 +140,7 @@ export function SaveModalContainer({
   return (
     <SaveModal
       originatingApp={originatingApp}
+      getOriginatingPath={getOriginatingPath}
       savingToLibraryPermitted={savingToLibraryPermitted}
       allowByValueEmbeddables={dashboardFeatureFlag?.allowByValueEmbeddables}
       savedObjectsTagging={savedObjectsTagging}
@@ -150,6 +154,7 @@ export function SaveModalContainer({
       description={description}
       savedObjectId={savedObjectId}
       returnToOriginSwitchLabel={returnToOriginSwitchLabel}
+      returnToOrigin={redirectToOrigin != null}
     />
   );
 }
@@ -158,11 +163,15 @@ const redirectToDashboard = ({
   embeddableInput,
   dashboardFeatureFlag,
   dashboardId,
+  originatingApp,
+  getOriginatingPath,
   stateTransfer,
 }: {
   embeddableInput: LensEmbeddableInput;
   dashboardId: string;
   dashboardFeatureFlag: LensAppServices['dashboardFeatureFlag'];
+  originatingApp?: string;
+  getOriginatingPath?: (dashboardId: string) => string | undefined;
   stateTransfer: LensAppServices['stateTransfer'];
 }) => {
   if (!dashboardFeatureFlag.allowByValueEmbeddables) {
@@ -174,8 +183,11 @@ const redirectToDashboard = ({
     type: LENS_EMBEDDABLE_TYPE,
   };
 
-  const path = dashboardId === 'new' ? '#/create' : `#/view/${dashboardId}`;
-  stateTransfer.navigateToWithEmbeddablePackage('dashboards', {
+  const path =
+    getOriginatingPath?.(dashboardId) ??
+    (dashboardId === 'new' ? '#/create' : `#/view/${dashboardId}`);
+  const appId = originatingApp ?? 'dashboards';
+  stateTransfer.navigateToWithEmbeddablePackage(appId, {
     state,
     path,
   });
@@ -208,6 +220,7 @@ export const runSaveLensVisualization = async (
     getIsByValueMode: () => boolean;
     persistedDoc?: Document;
     originatingApp?: string;
+    getOriginatingPath?: (dashboardId: string) => string;
     textBasedLanguageSave?: boolean;
     switchDatasource?: () => void;
     savedObjectStore: SavedObjectIndexStore;
@@ -248,7 +261,6 @@ export const runSaveLensVisualization = async (
       persistedDoc && savedObjectsTagging
         ? savedObjectsTagging.ui.getTagIdsFromReferences(persistedDoc.references)
         : [];
-
     references = savedObjectsTagging.ui.updateTagsReferences(
       references,
       saveProps.newTags || tagsIds
@@ -305,7 +317,6 @@ export const runSaveLensVisualization = async (
         timeRange: saveProps.panelTimeRange,
       };
     }
-
     if (saveProps.returnToOrigin && redirectToOrigin) {
       // disabling the validation on app leave because the document has been saved.
       onAppLeave?.((actions) => {
@@ -323,6 +334,8 @@ export const runSaveLensVisualization = async (
         dashboardId: saveProps.dashboardId,
         stateTransfer,
         dashboardFeatureFlag,
+        originatingApp: props.originatingApp,
+        getOriginatingPath: props.getOriginatingPath,
       });
       return;
     }

x-pack/plugins/lens/public/app_plugin/save_modal.tsx
@@ -24,6 +24,7 @@ export interface Props {
   savingToLibraryPermitted?: boolean;
 
   originatingApp?: string;
+  getOriginatingPath?: (dashboardId: string) => string;
   allowByValueEmbeddables: boolean;
 
   savedObjectsTagging?: SavedObjectTaggingPluginStart;
@@ -35,7 +36,7 @@ export interface Props {
 
   getAppNameFromId: () => string | undefined;
   returnToOriginSwitchLabel?: string;
-
+  returnToOrigin?: boolean;
   onClose: () => void;
   onSave: (props: SaveProps, options: { saveToLibrary: boolean }) => void;
 }
@@ -43,6 +44,7 @@ export interface Props {
 export const SaveModal = (props: Props) => {
   const {
     originatingApp,
+    getOriginatingPath,
     savingToLibraryPermitted,
     savedObjectsTagging,
     tagsIds,
@@ -54,10 +56,11 @@ export const SaveModal = (props: Props) => {
     getAppNameFromId,
     onClose,
     onSave,
+    returnToOrigin,
   } = props;
 
   // Use the modal with return-to-origin features if we're in an app's edit flow or if by-value embeddables are disabled
-  if (originatingApp || !allowByValueEmbeddables) {
+  if ((originatingApp || !allowByValueEmbeddables) && returnToOrigin !== false) {
     return (
       <TagEnhancedSavedObjectSaveModalOrigin
         savedObjectsTagging={savedObjectsTagging}
@@ -100,6 +103,7 @@ export const SaveModal = (props: Props) => {
         defaultMessage: 'Lens visualization',
       })}
       data-test-subj="lnsApp_saveModalDashboard"
+      getOriginatingPath={getOriginatingPath}
     />
   );
 };

x-pack/plugins/lens/public/app_plugin/tags_saved_object_save_modal_dashboard_wrapper.tsx
@@ -28,6 +28,7 @@ export type TagEnhancedSavedObjectSaveModalDashboardProps = Omit<
   initialTags: string[];
   savedObjectsTagging?: SavedObjectTaggingPluginStart;
   onSave: (props: DashboardSaveProps) => void;
+  getOriginatingPath?: (dashboardId: string) => string;
 };
 
 const SavedObjectSaveModalDashboard = withSuspense(LazySavedObjectSaveModalDashboard);

x-pack/plugins/lens/public/plugin.ts
@@ -127,6 +127,8 @@ import { downloadCsvShareProvider } from './app_plugin/csv_download_provider/csv
 import { CONTENT_ID, LATEST_VERSION } from '../common/content_management';
 import type { EditLensConfigurationProps } from './app_plugin/shared/edit_on_the_fly/get_edit_lens_configuration';
 
+export type { SaveProps } from './app_plugin';
+
 export interface LensPluginSetupDependencies {
   urlForwarding: UrlForwardingSetup;
   expressions: ExpressionsSetup;

x-pack/plugins/security_solution/public/common/components/visualization_actions/actions.tsx
@@ -10,6 +10,7 @@ import type { Action, ActionExecutionContext } from '@kbn/ui-actions-plugin/publ
 import React, { useCallback, useMemo, useState } from 'react';
 import styled from 'styled-components';
 
+import type { LensEmbeddableInput } from '@kbn/lens-plugin/public';
 import { InputsModelId } from '../../store/inputs/constants';
 import { useKibana } from '../../lib/kibana/kibana_react';
 import { ModalInspectQuery } from '../inspect/modal';
@@ -18,6 +19,7 @@ import { useInspect } from '../inspect/use_inspect';
 import { useLensAttributes } from './use_lens_attributes';
 import { useAddToExistingCase } from './use_add_to_existing_case';
 import { useAddToNewCase } from './use_add_to_new_case';
+import { useSaveToLibrary } from './use_save_to_library';
 import type { VisualizationActionsProps } from './types';
 import {
   ADD_TO_EXISTING_CASE,
@@ -25,9 +27,11 @@ import {
   INSPECT,
   MORE_ACTIONS,
   OPEN_IN_LENS,
+  ADDED_TO_LIBRARY,
 } from './translations';
 import { VISUALIZATION_ACTIONS_BUTTON_CLASS } from './utils';
 import { SourcererScopeName } from '../../store/sourcerer/model';
+import { useAppToasts } from '../../hooks/use_app_toasts';
 
 const Wrapper = styled.div`
   &.viz-actions {
@@ -62,9 +66,19 @@ const VisualizationActionsComponent: React.FC<VisualizationActionsProps> = ({
 }) => {
   const { lens } = useKibana().services;
 
-  const { canUseEditor, navigateToPrefilledEditor } = lens;
+  const { canUseEditor, navigateToPrefilledEditor, SaveModalComponent } = lens;
   const [isPopoverOpen, setPopover] = useState(false);
   const [isInspectModalOpen, setIsInspectModalOpen] = useState(false);
+  const [isSaveModalVisible, setIsSaveModalVisible] = useState(false);
+  const { addSuccess } = useAppToasts();
+  const onSave = useCallback(() => {
+    setIsSaveModalVisible(false);
+    addSuccess(ADDED_TO_LIBRARY);
+  }, [addSuccess]);
+  const onClose = useCallback(() => {
+    setIsSaveModalVisible(false);
+  }, []);
+  const hasPermission = canUseEditor();
 
   const onButtonClick = useCallback(() => {
     setPopover(!isPopoverOpen);
@@ -116,6 +130,10 @@ const VisualizationActionsComponent: React.FC<VisualizationActionsProps> = ({
     );
   }, [attributes, navigateToPrefilledEditor, timerange]);
 
+  const { openSaveVisualizationFlyout, disableVisualizations } = useSaveToLibrary({
+    attributes,
+  });
+
   const onOpenInspectModal = useCallback(() => {
     closePopover();
     setIsInspectModalOpen(true);
@@ -145,11 +163,6 @@ const VisualizationActionsComponent: React.FC<VisualizationActionsProps> = ({
     onClick: onOpenInspectModal,
     queryId,
   });
-
-  const disabledOpenInLens = useMemo(
-    () => !canUseEditor() || attributes == null,
-    [attributes, canUseEditor]
-  );
 
   const items = useMemo(() => {
     const context = {} as ActionExecutionContext<object>;
@@ -197,11 +210,24 @@ const VisualizationActionsComponent: React.FC<VisualizationActionsProps> = ({
             >
               {ADD_TO_EXISTING_CASE}
             </EuiContextMenuItem>,
+            ...(hasPermission
+              ? [
+                  <EuiContextMenuItem
+                    icon="visArea"
+                    key="visualizationActionsSaveVisualization"
+                    data-test-subj="viz-actions-save-visualization"
+                    disabled={disableVisualizations}
+                    onClick={openSaveVisualizationFlyout}
+                  >
+                    {ADDED_TO_LIBRARY}
+                  </EuiContextMenuItem>,
+                ]
+              : []),
             <EuiContextMenuItem
               icon="visArea"
               key="visualizationActionsOpenInLens"
               data-test-subj="viz-actions-open-in-lens"
-              disabled={disabledOpenInLens}
+              disabled={disableVisualizations}
               onClick={onOpenInLens}
             >
               {OPEN_IN_LENS}
@@ -210,8 +236,9 @@ const VisualizationActionsComponent: React.FC<VisualizationActionsProps> = ({
         : []),
     ];
   }, [
+    hasPermission,
     disableInspectButton,
-    disabledOpenInLens,
+    disableVisualizations,
     extraActions,
     handleInspectButtonClick,
     isAddToExistingCaseDisabled,
@@ -219,6 +246,7 @@ const VisualizationActionsComponent: React.FC<VisualizationActionsProps> = ({
     onAddToExistingCaseClicked,
     onAddToNewCaseClicked,
     onOpenInLens,
+    openSaveVisualizationFlyout,
     withDefaultActions,
   ]);
 
@@ -259,6 +287,13 @@ const VisualizationActionsComponent: React.FC<VisualizationActionsProps> = ({
           request={request}
           response={response}
           title={inspectTitle}
+        />
+      )}
+      {isSaveModalVisible && hasPermission && (
+        <SaveModalComponent
+          initialInput={attributes as unknown as LensEmbeddableInput}
+          onSave={onSave}
+          onClose={onClose}
         />
       )}
     </Wrapper>

x-pack/plugins/security_solution/public/common/components/visualization_actions/lens_attributes/common/authentication.ts
@@ -6,8 +6,8 @@
  */
 
 import {
-  AUTHENCICATION_FAILURE_CHART_LABEL,
-  AUTHENCICATION_SUCCESS_CHART_LABEL,
+  AUTHENTICATION_FAILURE_CHART_LABEL,
+  AUTHENTICATION_SUCCESS_CHART_LABEL,
 } from '../../translations';
 import type { LensAttributes } from '../../types';
 
@@ -115,7 +115,7 @@ export const authenticationLensAttributes: LensAttributes = {
                 },
               },
               '5417777d-d9d9-4268-9cdc-eb29b873bd65': {
-                label: AUTHENCICATION_SUCCESS_CHART_LABEL,
+                label: AUTHENTICATION_SUCCESS_CHART_LABEL,
                 dataType: 'number',
                 operationType: 'count',
                 isBucketed: false,
@@ -148,7 +148,7 @@ export const authenticationLensAttributes: LensAttributes = {
                 },
               },
               'a3bf9dc1-c8d2-42d6-9e60-31892a4c509e': {
-                label: AUTHENCICATION_FAILURE_CHART_LABEL,
+                label: AUTHENTICATION_FAILURE_CHART_LABEL,
                 dataType: 'number',
                 operationType: 'count',
                 isBucketed: false,

x-pack/plugins/security_solution/public/common/components/visualization_actions/translations.ts
@@ -67,7 +67,7 @@ export const SUCCESS_CHART_LABEL = i18n.translate(
   }
 );
 
-export const AUTHENCICATION_SUCCESS_CHART_LABEL = i18n.translate(
+export const AUTHENTICATION_SUCCESS_CHART_LABEL = i18n.translate(
   'xpack.securitySolution.visualizationActions.userAuthentications.authentication.successChartLabel',
   {
     defaultMessage: 'Success',
@@ -81,7 +81,7 @@ export const FAIL_CHART_LABEL = i18n.translate(
   }
 );
 
-export const AUTHENCICATION_FAILURE_CHART_LABEL = i18n.translate(
+export const AUTHENTICATION_FAILURE_CHART_LABEL = i18n.translate(
   'xpack.securitySolution.visualizationActions.userAuthentications.authentication.failureChartLabel',
   {
     defaultMessage: 'Failure',
@@ -111,3 +111,10 @@ export const COUNT_OF = (field: string) =>
     values: { field },
     defaultMessage: 'Count of {field}',
   });
+
+export const ADDED_TO_LIBRARY = i18n.translate(
+  'xpack.securitySolution.visualizationActions.addedToLibrary',
+  {
+    defaultMessage: 'Added to library',
+  }
+);

x-pack/plugins/security_solution/public/common/components/visualization_actions/use_actions.test.tsx
@@ -6,9 +6,13 @@
  */
 
 import { renderHook } from '@testing-library/react-hooks';
+import React from 'react';
+import { NavigationProvider } from '@kbn/security-solution-navigation';
 import { useKibana } from '../../lib/kibana/kibana_react';
 import { mockAttributes } from './mocks';
 import { useActions } from './use_actions';
+import { coreMock } from '@kbn/core/public/mocks';
+import { TestProviders } from '../../mock';
 
 jest.mock('./use_add_to_existing_case', () => {
   return {
@@ -32,6 +36,12 @@ jest.mock('../../lib/kibana/kibana_react', () => {
   };
 });
 
+const coreStart = coreMock.createStart();
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <TestProviders>
+    <NavigationProvider core={coreStart}>{children}</NavigationProvider>
+  </TestProviders>
+);
 describe(`useActions`, () => {
   const mockNavigateToPrefilledEditor = jest.fn();
   beforeAll(() => {
@@ -39,6 +49,15 @@ describe(`useActions`, () => {
       services: {
         lens: {
           navigateToPrefilledEditor: mockNavigateToPrefilledEditor,
+          canUseEditor: jest.fn().mockReturnValue(true),
+          SaveModalComponent: jest
+            .fn()
+            .mockReturnValue(() => <div data-test-subj="saveModalComponent" />),
+        },
+        notifications: {
+          toasts: {
+            addWarning: jest.fn(),
+          },
         },
       },
     });
@@ -49,29 +68,34 @@ describe(`useActions`, () => {
   });
 
   it('should render actions', () => {
-    const { result } = renderHook(() =>
-      useActions({
-        withActions: true,
-        attributes: mockAttributes,
-        timeRange: {
-          from: '2022-10-26T23:00:00.000Z',
-          to: '2022-11-03T15:16:50.053Z',
-        },
-        inspectActionProps: {
-          onInspectActionClicked: jest.fn(),
-          isDisabled: false,
-        },
-      })
+    const { result } = renderHook(
+      () =>
+        useActions({
+          withActions: true,
+          attributes: mockAttributes,
+          timeRange: {
+            from: '2022-10-26T23:00:00.000Z',
+            to: '2022-11-03T15:16:50.053Z',
+          },
+          inspectActionProps: {
+            onInspectActionClicked: jest.fn(),
+            isDisabled: false,
+          },
+        }),
+      {
+        wrapper,
+      }
     );
-
     expect(result.current[0].id).toEqual('inspect');
     expect(result.current[0].order).toEqual(4);
     expect(result.current[1].id).toEqual('addToNewCase');
     expect(result.current[1].order).toEqual(3);
     expect(result.current[2].id).toEqual('addToExistingCase');
     expect(result.current[2].order).toEqual(2);
-    expect(result.current[3].id).toEqual('openInLens');
+    expect(result.current[3].id).toEqual('saveToLibrary');
     expect(result.current[3].order).toEqual(1);
+    expect(result.current[4].id).toEqual('openInLens');
+    expect(result.current[4].order).toEqual(0);
   });
 
   it('should render extra actions if available', () => {
@@ -92,31 +116,37 @@ describe(`useActions`, () => {
         order: 0,
       },
     ];
-    const { result } = renderHook(() =>
-      useActions({
-        withActions: true,
-        attributes: mockAttributes,
-        timeRange: {
-          from: '2022-10-26T23:00:00.000Z',
-          to: '2022-11-03T15:16:50.053Z',
-        },
-        inspectActionProps: {
-          onInspectActionClicked: jest.fn(),
-          isDisabled: false,
-        },
-        extraActions: mockExtraAction,
-      })
+    const { result } = renderHook(
+      () =>
+        useActions({
+          withActions: true,
+          attributes: mockAttributes,
+          timeRange: {
+            from: '2022-10-26T23:00:00.000Z',
+            to: '2022-11-03T15:16:50.053Z',
+          },
+          inspectActionProps: {
+            onInspectActionClicked: jest.fn(),
+            isDisabled: false,
+          },
+          extraActions: mockExtraAction,
+        }),
+      {
+        wrapper,
+      }
     );
 
     expect(result.current[0].id).toEqual('inspect');
-    expect(result.current[0].order).toEqual(4);
+    expect(result.current[0].order).toEqual(5);
     expect(result.current[1].id).toEqual('addToNewCase');
-    expect(result.current[1].order).toEqual(3);
+    expect(result.current[1].order).toEqual(4);
     expect(result.current[2].id).toEqual('addToExistingCase');
-    expect(result.current[2].order).toEqual(2);
-    expect(result.current[3].id).toEqual('openInLens');
-    expect(result.current[3].order).toEqual(1);
-    expect(result.current[4].id).toEqual('mockExtraAction');
-    expect(result.current[4].order).toEqual(0);
+    expect(result.current[2].order).toEqual(3);
+    expect(result.current[3].id).toEqual('saveToLibrary');
+    expect(result.current[3].order).toEqual(2);
+    expect(result.current[4].id).toEqual('openInLens');
+    expect(result.current[4].order).toEqual(1);
+    expect(result.current[5].id).toEqual('mockExtraAction');
+    expect(result.current[5].order).toEqual(0);
   });
 });

commit 07f62bf560c1902b1f79baa04176b732478222ab

[Lens] Fixes the transition to a dashboard when originatingApp is not given (#172543)
## Summary

Closes https://github.com/elastic/kibana/issues/172410

This is a regression caused by
https://github.com/elastic/kibana/pull/167019.

The originatingApp on the navigateToPrefilledEditor defaults to ""
(empty string) and not undefined.

### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/plugins/lens/public/app_plugin/save_modal_container.tsx
@@ -16,10 +16,11 @@ import type { LensAppProps, LensAppServices } from './types';
 import type { SaveProps } from './app';
 import { Document, checkForDuplicateTitle, SavedObjectIndexStore } from '../persistence';
 import type { LensByReferenceInput, LensEmbeddableInput } from '../embeddable';
-import { APP_ID, getFullPath, LENS_EMBEDDABLE_TYPE } from '../../common/constants';
+import { APP_ID, getFullPath } from '../../common/constants';
 import type { LensAppState } from '../state_management';
 import { getPersisted } from '../state_management/init_middleware/load_initial';
 import { VisualizeEditorContext } from '../types';
+import { redirectToDashboard } from './save_modal_container_helpers';
 
 type ExtraProps = Pick<LensAppProps, 'initialInput'> &
   Partial<Pick<LensAppProps, 'redirectToOrigin' | 'redirectTo' | 'onAppLeave'>>;
@@ -170,40 +171,6 @@ export function SaveModalContainer({
     />
   );
 }
-
-const redirectToDashboard = ({
-  embeddableInput,
-  dashboardFeatureFlag,
-  dashboardId,
-  originatingApp,
-  getOriginatingPath,
-  stateTransfer,
-}: {
-  embeddableInput: LensEmbeddableInput;
-  dashboardId: string;
-  dashboardFeatureFlag: LensAppServices['dashboardFeatureFlag'];
-  originatingApp?: string;
-  getOriginatingPath?: (dashboardId: string) => string | undefined;
-  stateTransfer: LensAppServices['stateTransfer'];
-}) => {
-  if (!dashboardFeatureFlag.allowByValueEmbeddables) {
-    throw new Error('redirectToDashboard called with by-value embeddables disabled');
-  }
-
-  const state = {
-    input: embeddableInput,
-    type: LENS_EMBEDDABLE_TYPE,
-  };
-
-  const path =
-    getOriginatingPath?.(dashboardId) ??
-    (dashboardId === 'new' ? '#/create' : `#/view/${dashboardId}`);
-  const appId = originatingApp ?? 'dashboards';
-  stateTransfer.navigateToWithEmbeddablePackage(appId, {
-    state,
-    path,
-  });
-};
 
 const getDocToSave = (
   lastKnownDoc: Document,

x-pack/plugins/lens/public/app_plugin/save_modal_container_helpers.ts
@@ -1,0 +1,44 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { LensAppServices } from './types';
+import type { LensEmbeddableInput } from '../embeddable';
+import { LENS_EMBEDDABLE_TYPE } from '../../common/constants';
+
+export const redirectToDashboard = ({
+  embeddableInput,
+  dashboardFeatureFlag,
+  dashboardId,
+  originatingApp,
+  getOriginatingPath,
+  stateTransfer,
+}: {
+  embeddableInput: LensEmbeddableInput;
+  dashboardId: string;
+  dashboardFeatureFlag: LensAppServices['dashboardFeatureFlag'];
+  originatingApp?: string;
+  getOriginatingPath?: (dashboardId: string) => string | undefined;
+  stateTransfer: LensAppServices['stateTransfer'];
+}) => {
+  if (!dashboardFeatureFlag.allowByValueEmbeddables) {
+    throw new Error('redirectToDashboard called with by-value embeddables disabled');
+  }
+
+  const state = {
+    input: embeddableInput,
+    type: LENS_EMBEDDABLE_TYPE,
+  };
+
+  const path =
+    getOriginatingPath?.(dashboardId) ??
+    (dashboardId === 'new' ? '#/create' : `#/view/${dashboardId}`);
+  const appId = originatingApp || 'dashboards';
+  stateTransfer.navigateToWithEmbeddablePackage(appId, {
+    state,
+    path,
+  });
+};

x-pack/plugins/lens/public/app_plugin/save_modal_container_helpers.test.ts
@@ -1,0 +1,76 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { makeDefaultServices } from '../mocks';
+import type { LensEmbeddableInput } from '../embeddable';
+import type { LensAppServices } from './types';
+import { redirectToDashboard } from './save_modal_container_helpers';
+
+describe('redirectToDashboard', () => {
+  const embeddableInput = {
+    test: 'test',
+  } as unknown as LensEmbeddableInput;
+  const mockServices = makeDefaultServices();
+
+  it('should return error in case of allowByValueEmbeddables false', () => {
+    expect(() => {
+      redirectToDashboard({
+        embeddableInput,
+        dashboardFeatureFlag: {
+          allowByValueEmbeddables: false,
+        },
+        dashboardId: 'id',
+        originatingApp: '',
+        getOriginatingPath: jest.fn(),
+        stateTransfer: mockServices.stateTransfer,
+      });
+    }).toThrow('redirectToDashboard called with by-value embeddables disabled');
+  });
+
+  it('should call the navigateToWithEmbeddablePackage with the correct args if originatingApp is given', () => {
+    const navigateToWithEmbeddablePackageSpy = jest.fn();
+    const transferService = {
+      ...mockServices.stateTransfer,
+      navigateToWithEmbeddablePackage: navigateToWithEmbeddablePackageSpy,
+    } as unknown as LensAppServices['stateTransfer'];
+    redirectToDashboard({
+      embeddableInput,
+      dashboardFeatureFlag: {
+        allowByValueEmbeddables: true,
+      },
+      dashboardId: 'id',
+      originatingApp: 'security',
+      getOriginatingPath: jest.fn(),
+      stateTransfer: transferService,
+    });
+    expect(navigateToWithEmbeddablePackageSpy).toHaveBeenCalledWith('security', {
+      path: '#/view/id',
+      state: { input: { test: 'test' }, type: 'lens' },
+    });
+  });
+
+  it('should call the navigateToWithEmbeddablePackage with the correct args if originatingApp is an empty string', () => {
+    const navigateToWithEmbeddablePackageSpy = jest.fn();
+    const transferService = {
+      ...mockServices.stateTransfer,
+      navigateToWithEmbeddablePackage: navigateToWithEmbeddablePackageSpy,
+    } as unknown as LensAppServices['stateTransfer'];
+    redirectToDashboard({
+      embeddableInput,
+      dashboardFeatureFlag: {
+        allowByValueEmbeddables: true,
+      },
+      dashboardId: 'id',
+      originatingApp: '',
+      getOriginatingPath: jest.fn(),
+      stateTransfer: transferService,
+    });
+    expect(navigateToWithEmbeddablePackageSpy).toHaveBeenCalledWith('dashboards', {
+      path: '#/view/id',
+      state: { input: { test: 'test' }, type: 'lens' },
+    });
+  });
+});