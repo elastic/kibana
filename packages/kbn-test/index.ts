30a9fa14bc839494f560395157dd3ede8d3e47f8
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2023-12-06 17:04:15 +0100

    revert local prettier change

--- packages/kbn-test/index.ts
+++ packages/kbn-test/index.ts
@@ -6,43 +6,58 @@
  * Side Public License, v 1.
  */
 
-export { getDockerFileMountPath } from '@kbn/es';
-export { kbnTestConfig } from './kbn_test_config';
+export { KbnClientRequesterError } from './src/kbn_client/kbn_client_requester_error';
+
+// @internal
+export { startServersCli, startServers } from './src/functional_tests/start_servers';
+
+// @internal
+export { runTestsCli, runTests } from './src/functional_tests/run_tests';
 export { SAMLSessionManager } from './src/auth';
-export { CI_PARALLEL_PROCESS_PREFIX } from './src/ci_parallel_process_prefix';
-export {
-  createEsClientForFtrConfig,
-  createEsClientForTesting,
-  createRemoteEsClientForFtrConfig,
-  createTestEsCluster,
-  esTestConfig,
-} from './src/es';
-export type {
-  CreateTestEsClusterOptions,
-  EsClientForTestingOptions,
-  EsTestCluster,
-  ICluster,
-} from './src/es';
-export * from './src/find_test_plugin_paths';
 export { runElasticsearch, runKibanaServer } from './src/functional_tests/lib';
 export { getKibanaCliArg, getKibanaCliLoggers } from './src/functional_tests/lib/kibana_cli_args';
-// @internal
-export { runTests, runTestsCli } from './src/functional_tests/run_tests';
-// @internal
-export { startServers, startServersCli } from './src/functional_tests/start_servers';
-export * from './src/functional_test_runner';
-export { getUrl } from './src/jest/get_url';
-export { runJest } from './src/jest/run';
-export { runCheckJestConfigsCli } from './src/jest/run_check_jest_configs_cli';
+
+export type {
+  CreateTestEsClusterOptions,
+  EsTestCluster,
+  ICluster,
+  EsClientForTestingOptions,
+} from './src/es';
 export {
-  adminTestUser,
+  esTestConfig,
+  createTestEsCluster,
+  createEsClientForTesting,
+  createEsClientForFtrConfig,
+  createRemoteEsClientForFtrConfig,
+} from './src/es';
+
+export { kbnTestConfig } from './kbn_test_config';
+
+export {
   kibanaServerTestUser,
-  kibanaTestSuperuserServerless,
   kibanaTestUser,
+  adminTestUser,
   systemIndicesSuperuser,
+  kibanaTestSuperuserServerless,
 } from './src/kbn';
-export * from './src/kbn_archiver_cli';
-export * from './src/kbn_client';
-export { KbnClientRequesterError } from './src/kbn_client/kbn_client_requester_error';
+
 // @internal
-export { escapeCdata, setupJUnitReportGeneration } from './src/mocha';
+export { setupJUnitReportGeneration, escapeCdata } from './src/mocha';
+
+export { CI_PARALLEL_PROCESS_PREFIX } from './src/ci_parallel_process_prefix';
+
+export * from './src/functional_test_runner';
+
+export { getUrl } from './src/jest/get_url';
+
+export { runCheckJestConfigsCli } from './src/jest/run_check_jest_configs_cli';
+
+export { runJest } from './src/jest/run';
+
+export * from './src/kbn_archiver_cli';
+
+export * from './src/kbn_client';
+
+export * from './src/find_test_plugin_paths';
+
+export { getDockerFileMountPath } from '@kbn/es';
