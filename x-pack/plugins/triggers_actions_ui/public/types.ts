1fbeb162db9324294a1317400acda2239d004ebd
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2023-12-06 13:44:06 -0500

    formatting

--- x-pack/plugins/triggers_actions_ui/public/types.ts
+++ x-pack/plugins/triggers_actions_ui/public/types.ts
@@ -13,7 +13,6 @@ import type { ChartsPluginSetup } from '@kbn/charts-plugin/public';
 import type { DataPublicPluginStart } from '@kbn/data-plugin/public';
 import type { DataViewsPublicPluginStart } from '@kbn/data-views-plugin/public';
 import type { UnifiedSearchPublicPluginStart } from '@kbn/unified-search-plugin/public';
-
 import type {
   IconType,
   RecursivePartial,
