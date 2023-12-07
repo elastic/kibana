643d523aff21a7096be10628775de9ae29b366f1
Umberto Pepato <umberto.pepato@elastic.co>
2023-12-07 15:44:30 +0100

    Add fields table to stack alerts flyout

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_flyout/default_alerts_flyout.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/alerts_flyout/default_alerts_flyout.tsx
@@ -5,12 +5,25 @@
  * 2.0.
  */
 
-import { get } from 'lodash';
-import React from 'react';
-import { type EuiDataGridColumn, EuiDescriptionList, EuiPanel, EuiTitle } from '@elastic/eui';
+import React, { memo, useCallback, useMemo, useState } from 'react';
+import {
+  type EuiDataGridColumn,
+  EuiDescriptionList,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiInMemoryTable,
+  EuiPanel,
+  EuiTabbedContent,
+  EuiTabbedContentProps,
+  EuiTabbedContentTab,
+  EuiTitle,
+} from '@elastic/eui';
 import { ALERT_RULE_NAME } from '@kbn/rule-data-utils';
-import { AlertsTableFlyoutBaseProps, AlertTableFlyoutComponent } from '../../../..';
+import { get } from 'lodash';
+import { i18n } from '@kbn/i18n';
+import styled from '@emotion/styled';
 import { RegisterFormatter } from '../cells/render_cell_value';
+import { AlertsTableFlyoutBaseProps, AlertTableFlyoutComponent } from '../../../..';
 
 const FlyoutHeader: AlertTableFlyoutComponent = ({ alert }: AlertsTableFlyoutBaseProps) => {
   const name = alert[ALERT_RULE_NAME];
@@ -21,28 +34,189 @@ const FlyoutHeader: AlertTableFlyoutComponent = ({ alert }: AlertsTableFlyoutBas
   );
 };
 
+export const search = {
+  box: {
+    incremental: true,
+    placeholder: i18n.translate(
+      'xpack.triggersActionsUI.sections.alertsTable.alertsFlyout.filter.placeholder',
+      {
+        defaultMessage: 'Filter by Field, Value, or Description...',
+      }
+    ),
+    schema: true,
+  },
+};
+
+const StyledFlexGroup = styled(EuiFlexGroup)`
+  height: 100%;
+`;
+
+const StyledEuiFlexItem = styled(EuiFlexItem)`
+  &.euiFlexItem {
+    flex: 1 0 0;
+    overflow: hidden;
+  }
+`;
+
+const StyledEuiTabbedContent = styled(EuiTabbedContent as React.FC)`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  overflow: hidden;
+
+  > [role='tabpanel'] {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    overflow: hidden;
+    overflow-y: auto;
+
+    ::-webkit-scrollbar {
+      -webkit-appearance: none;
+      width: 7px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      background-color: rgba(0, 0, 0, 0.5);
+      -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, 0.5);
+    }
+  }
+`;
+
+const ScrollableFlyoutTabbedContent = (props: EuiTabbedContentProps) => (
+  <StyledFlexGroup direction="column" gutterSize="none">
+    <StyledEuiFlexItem grow={true}>
+      <StyledEuiTabbedContent {...props} />
+    </StyledEuiFlexItem>
+  </StyledFlexGroup>
+);
+
+type TabId = 'overview';
+
+const COUNT_PER_PAGE_OPTIONS = [25, 50, 100];
+
+const useFieldBrowserPagination = () => {
+  const [pagination, setPagination] = useState<{ pageIndex: number }>({
+    pageIndex: 0,
+  });
+
+  const onTableChange = useCallback(({ page: { index } }: { page: { index: number } }) => {
+    setPagination({ pageIndex: index });
+  }, []);
+  const paginationTableProp = useMemo(
+    () => ({
+      ...pagination,
+      pageSizeOptions: COUNT_PER_PAGE_OPTIONS,
+    }),
+    [pagination]
+  );
+
+  return {
+    onTableChange,
+    paginationTableProp,
+  };
+};
+
+const FieldsTable = memo(({ alert }: Pick<AlertsTableFlyoutBaseProps, 'alert'>) => {
+  const { onTableChange, paginationTableProp } = useFieldBrowserPagination();
+  return (
+    <EuiInMemoryTable
+      items={Object.entries(alert).map(([key, value]) => ({ key, value: value?.[0] }))}
+      itemId="key"
+      columns={[
+        {
+          field: 'key',
+          name: i18n.translate('xpack.triggersActionsUI.sections.alertsTable.alertsFlyout.field', {
+            defaultMessage: 'Field',
+          }),
+        },
+        {
+          field: 'value',
+          name: i18n.translate('xpack.triggersActionsUI.sections.alertsTable.alertsFlyout.value', {
+            defaultMessage: 'Value',
+          }),
+        },
+      ]}
+      onTableChange={onTableChange}
+      pagination={paginationTableProp}
+      search={search}
+    />
+  );
+});
+
 export const getDefaultAlertFlyout =
   (columns: EuiDataGridColumn[], formatter: RegisterFormatter) => () => {
-    const FlyoutBody: AlertTableFlyoutComponent = ({ alert }: AlertsTableFlyoutBaseProps) => (
-      <EuiPanel>
-        <EuiDescriptionList
-          listItems={columns.map((column) => {
-            const value = get(alert, column.id)?.[0];
+    const FlyoutBody: AlertTableFlyoutComponent = ({ alert }: AlertsTableFlyoutBaseProps) => {
+      const overviewTab = useMemo(
+        () => ({
+          id: 'overview',
+          name: i18n.translate(
+            'xpack.triggersActionsUI.sections.alertsTable.alertsFlyout.overview',
+            {
+              defaultMessage: 'Overview',
+            }
+          ),
+          content: (
+            <EuiPanel>
+              <EuiDescriptionList
+                listItems={columns.map((column) => {
+                  const value = get(alert, column.id)?.[0];
 
-            return {
-              title: column.displayAsText as string,
-              description: value != null ? formatter(column.id, value) : '—',
-            };
-          })}
-          type="column"
-          columnWidths={[1, 3]}
+                  return {
+                    title: column.displayAsText as string,
+                    description: value != null ? formatter(column.id, value) : '—',
+                  };
+                })}
+                type="column"
+                columnWidths={[1, 3]}
+              />
+            </EuiPanel>
+          ),
+        }),
+        [alert]
+      );
+
+      const tableTab = useMemo(
+        () => ({
+          id: 'table',
+          name: i18n.translate('xpack.triggersActionsUI.sections.alertsTable.alertsFlyout.table', {
+            defaultMessage: 'Table',
+          }),
+          content: (
+            <EuiPanel>
+              <FieldsTable alert={alert} />
+            </EuiPanel>
+          ),
+        }),
+        [alert]
+      );
+
+      const tabs = useMemo(() => [overviewTab, tableTab], [overviewTab, tableTab]);
+      const [selectedTabId, setSelectedTabId] = useState<TabId>('overview');
+      const handleTabClick = useCallback(
+        (tab: EuiTabbedContentTab) => setSelectedTabId(tab.id as TabId),
+        []
+      );
+
+      const selectedTab = useMemo(
+        () => tabs.find((tab) => tab.id === selectedTabId) ?? tabs[0],
+        [tabs, selectedTabId]
+      );
+
+      return (
+        <ScrollableFlyoutTabbedContent
+          tabs={tabs}
+          selectedTab={selectedTab}
+          onTabClick={handleTabClick}
+          expand
         />
-      </EuiPanel>
-    );
+      );
+    };
 
     return {
-      body: FlyoutBody,
       header: FlyoutHeader,
+      body: FlyoutBody,
       footer: null,
     };
   };
