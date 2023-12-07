2838e29bcfe3f13cae4aafa5898f441989d88e04
PhilippeOberti <philippe.oberti@elastic.co>
2023-12-07 13:08:33 -0600

    test

---
+++ x-pack/test/security_solution_cypress/cypress/e2e/investigations/alerts/expandable_flyout/test.cy.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { deleteAlertsAndRules } from '../../../../tasks/api_calls/common';
+import { login } from '../../../../tasks/login';
+import { visit } from '../../../../tasks/navigation';
+import { createRule } from '../../../../tasks/api_calls/rules';
+import { getNewRule } from '../../../../objects/rule';
+import { ALERTS_URL } from '../../../../urls/navigation';
+import { waitForAlertsToPopulate } from '../../../../tasks/create_new_rule';
+import { expandFirstAlertExpandableFlyout } from '../../../../tasks/expandable_flyout/common';
+
+describe('test', { tags: ['@ess', '@serverless'] }, () => {
+  beforeEach(() => {
+    deleteAlertsAndRules();
+    login();
+    createRule(getNewRule());
+    visit(ALERTS_URL);
+    waitForAlertsToPopulate();
+  });
+
+  Cypress._.times(100, () => {
+    it('test', () => {
+      expandFirstAlertExpandableFlyout();
+    });
+  });
+});
