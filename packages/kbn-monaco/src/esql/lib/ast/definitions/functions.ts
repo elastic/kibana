860f68e5cd516eceec13023f6f5afddfaefe0cc0
dej611 <dej611@gmail.com>
2023-12-07 15:30:28 +0100

    :sparkles: Add to_geopoint fn

--- packages/kbn-monaco/src/esql/lib/ast/definitions/functions.ts
+++ packages/kbn-monaco/src/esql/lib/ast/definitions/functions.ts
@@ -251,6 +251,19 @@ export const evalFunctionsDefinitions: FunctionDefinition[] = [
     ],
   },
   {
+    name: 'to_geopoint',
+    description: i18n.translate('monaco.esql.definitions.toGeopointDoc', {
+      defaultMessage: 'Converts to geo_point.',
+    }),
+    signatures: [
+      {
+        params: [{ name: 'field', type: 'any' }],
+        returnType: 'geo_point',
+        examples: [`from index | EVAL geopoint = to_geopoint(field)`],
+      },
+    ],
+  },
+  {
     name: 'to_integer',
     alias: ['to_int'],
     description: i18n.translate('monaco.esql.definitions.toIntegerDoc', {
