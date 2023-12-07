1d8dbc8d577c00263af96b2401ac9e56a2c0fe81
Mark Hopkin <mark.hopkin@elastic.co>
2023-12-07 16:12:47 +0000

    consolidate entity analytics common code

--- x-pack/plugins/security_solution/server/lib/entity_analytics/asset_criticality/asset_criticality_data_client.ts
+++ x-pack/plugins/security_solution/server/lib/entity_analytics/asset_criticality/asset_criticality_data_client.ts
@@ -8,7 +8,7 @@ import type { Logger, ElasticsearchClient } from '@kbn/core/server';
 import { mappingFromFieldMap } from '@kbn/alerting-plugin/common';
 import type { AssetCriticalityRecord } from '../../../../common/api/entity_analytics/asset_criticality';
 import { createOrUpdateIndex } from '../utils/create_or_update_index';
-import { getAssetCriticalityIndex } from '../../../../common/asset_criticality';
+import { getAssetCriticalityIndex } from '../../../../common/entity_analytics/asset_criticality';
 import { assetCriticalityFieldMap } from './configurations';
 
 interface AssetCriticalityClientOpts {
