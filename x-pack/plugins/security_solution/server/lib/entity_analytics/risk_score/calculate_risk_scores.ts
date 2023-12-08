8f36120fc337a1abcb4ced4b3ecd15bf8e8176f6
Ryland Herrick <ryalnd@gmail.com>
2023-12-08 17:42:46 -0600

    Extract and annotate our summing constants

--- x-pack/plugins/security_solution/server/lib/entity_analytics/risk_score/calculate_risk_scores.ts
+++ x-pack/plugins/security_solution/server/lib/entity_analytics/risk_score/calculate_risk_scores.ts
@@ -43,6 +43,16 @@ import type {
   RiskScoreBucket,
 } from '../types';
 
+/**
+ * The risk scoring algorithm uses a Riemann zeta function to sum an entity's risk inputs to a known, finite value (@see RISK_SCORING_SUM_MAX). It does so by assigning each input a weight based on its position in the list (ordered by score) of inputs. This value represents the complex variable s of Re(s) in traditional Riemann zeta function notation.
+ */
+const RISK_SCORING_SUM_VALUE = 1.5;
+/**
+ * Represents the maximum possible risk score sum. This value is derived from RISK_SCORING_SUM_VALUE, but we store the precomputed value here to be used more conveniently in normalization.
+ * @see RISK_SCORING_SUM_VALUE
+ */
+const RISK_SCORING_SUM_MAX = 261.2;
+
 const formatForResponse = ({
   bucket,
   criticality,
@@ -207,8 +217,8 @@ const buildIdentifierTypeAggregation = ({
           combine_script: 'return state;',
           params: {
             max_risk_inputs_per_identity: 999999,
-            p: 1.5,
-            risk_cap: 261.2,
+            p: RISK_SCORING_SUM_VALUE,
+            risk_cap: RISK_SCORING_SUM_MAX,
           },
           reduce_script: buildReduceScript({ globalIdentifierTypeWeight }),
         },
