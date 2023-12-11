9d254b05483110ec56fb7db58605b06cc471dfcb
Alex Szabo <alex.szabo@elastic.co>
2023-12-11 10:49:25 +0100

    chore: get user identity from github token as a fallback

--- .buildkite/scripts/serverless/create_deploy_tag/create_deploy_tag.sh
+++ .buildkite/scripts/serverless/create_deploy_tag/create_deploy_tag.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 DEPLOY_TAG="deploy@$(date +%s)"
-KIBANA_COMMIT_SHA=$(buildkite-agent meta-data get selected-commit-hash)
+# KIBANA_COMMIT_SHA=$(buildkite-agent meta-data get selected-commit-hash)
+KIBANA_COMMIT_SHA=$(git rev-parse HEAD)
 
 if [[ -z "$KIBANA_COMMIT_SHA" ]]; then
   echo "Commit sha is not set, exiting."
@@ -11,9 +12,12 @@ fi
 
 echo "--- Creating deploy tag $DEPLOY_TAG at $KIBANA_COMMIT_SHA"
 
+echo "Fetching user identity from GitHub..."
+IDENTITY_JSON=$(ts-node .buildkite/scripts/serverless/create_deploy_tag/get_github_identity.ts)
+
 # Set git identity to whomever triggered the buildkite job
-git config user.email "$BUILDKITE_BUILD_CREATOR_EMAIL"
-git config user.name "$BUILDKITE_BUILD_CREATOR"
+git config user.email "${BUILDKITE_BUILD_CREATOR_EMAIL:-$(echo ${IDENTITY_JSON} | jq .email)}"
+git config user.name "${BUILDKITE_BUILD_CREATOR:-$(echo ${IDENTITY_JSON} | jq .name)}"
 
 # Create a tag for the deploy
 git tag -a "$DEPLOY_TAG" "$KIBANA_COMMIT_SHA" \
