b688f85f2628a936a80b2aaee61ce4c2e9ef0753
Omolola Akinleye <omolola.akinleye@elastic.co>
2023-12-11 06:42:15 -0500

    fix module export to use types old

--- x-pack/plugins/cloud_security_posture/server/routes/compliance_dashboard/get_trends.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/compliance_dashboard/get_trends.ts
@@ -11,7 +11,7 @@ import {
   BENCHMARK_SCORE_INDEX_DEFAULT_NS,
   CSPM_FINDINGS_STATS_INTERVAL,
 } from '../../../common/constants';
-import type { PosturePolicyTemplate, Stats } from '../../../common/types';
+import type { PosturePolicyTemplate, Stats } from '../../../common/types_old';
 import { toBenchmarkDocFieldKey } from '../../lib/mapping_field_util';
 
 interface FindingsDetails {
