aaa7c7248f5226b5e5eadfae1f43b570041bb327
Thom Heymann <thom.heymann@elastic.co>
2023-12-12 11:01:15 +0000

    Fix cyclic dependencies

--- packages/kbn-test/src/auth/saml_auth.ts
+++ packages/kbn-test/src/auth/saml_auth.ts
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import { createSAMLResponse as createMockedSAMLResponse } from '@kbn/mock-idp-plugin/common';
+import { createSAMLResponse as createMockedSAMLResponse } from '@kbn/mock-idp-utils';
 import { ToolingLog } from '@kbn/tooling-log';
 import axios, { AxiosResponse } from 'axios';
 import * as cheerio from 'cheerio';
