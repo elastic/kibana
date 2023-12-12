edd161be2614a73679efaabc32396d7ffcce55e1
Tomasz Ciecierski <ciecierskitomek@gmail.com>
2023-12-12 16:03:18 +0100

    fix

--- x-pack/plugins/security_solution/public/detection_engine/rule_response_actions/endpoint/field_name.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_response_actions/endpoint/field_name.tsx
@@ -66,7 +66,8 @@ const FieldNameFieldComponent = ({
 
   const CONFIG = {
     label: i18n.translate('xpack.securitySolution.responseActions.endpoint.fieldLabel', {
-      defaultMessage: `${FIELD_LABEL}`,
+      defaultMessage: '{field}',
+      values: { field: FIELD_LABEL },
     }),
     helpText: renderEntityIdNote,
     validations: [
