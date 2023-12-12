d330c7305499b57302c4b09707ee0624a6c6f75c
Mike Cote <michel.cote@elastic.co>
2023-12-12 07:26:44 -0500

    Fix parseDuration

--- x-pack/plugins/stack_alerts/server/rule_types/user_defined/lib/get_time_range.ts
+++ x-pack/plugins/stack_alerts/server/rule_types/user_defined/lib/get_time_range.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { parseDuration } from '@kbn/alerting-plugin/common/parse_duration';
+import { parseDuration } from '../../../../../alerting/common/parse_duration.ts';
 
 if (!Deno.env.get('QUERY_DELAY_MS')) {
   throw new Error('QUERY_DELAY_MS not set');
