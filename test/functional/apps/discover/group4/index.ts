15cc582977bb76e683d223e8a9167add0264471d
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2023-12-13 19:11:49 +0100

    Add functional test

--- test/functional/apps/discover/group4/index.ts
+++ test/functional/apps/discover/group4/index.ts
@@ -33,5 +33,6 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./_context_encoded_url_params'));
     loadTestFile(require.resolve('./_hide_announcements'));
     loadTestFile(require.resolve('./_data_view_edit'));
+    loadTestFile(require.resolve('./_field_list_new_fields'));
   });
 }
