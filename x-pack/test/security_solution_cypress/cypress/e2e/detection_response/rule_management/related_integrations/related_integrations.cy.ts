047f6cfc28683d067e6714612cf26bf2fdf1f13a
Tiago Costa <tiago.costa@elastic.co>
2023-12-13 22:08:57 +0000

    skip flaky suite (#173307)

--- x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_management/related_integrations/related_integrations.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_management/related_integrations/related_integrations.cy.ts
@@ -45,7 +45,8 @@ import {
   waitForPageToBeLoaded,
 } from '../../../../tasks/rule_details';
 
-describe('Related integrations', { tags: ['@ess', '@serverless', '@brokenInServerlessQA'] }, () => {
+// FLAKY: https://github.com/elastic/kibana/issues/173307
+describe.skip('Related integrations', { tags: ['@ess', '@serverless', '@brokenInServerlessQA'] }, () => {
   const DATA_STREAM_NAME = 'logs-related-integrations-test';
   const PREBUILT_RULE_NAME = 'Prebuilt rule with related integrations';
   const RULE_RELATED_INTEGRATIONS: IntegrationDefinition[] = [
