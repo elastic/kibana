648aeeb9186dacb671190902166487ceb3a31504
Nikita Indik <nikita.indik@elastic.co>
2023-12-13 15:49:33 +0100

    Keep GitHub-like styles, adjust them, remove the rest

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/json_diff/diff_view.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/json_diff/diff_view.tsx
@@ -24,7 +24,7 @@ import type {
   HunkTokens,
 } from 'react-diff-view';
 import unidiff from 'unidiff';
-import { useEuiTheme, hexToRgb, EuiRadioGroup } from '@elastic/eui';
+import { useEuiTheme, COLOR_MODES_STANDARD } from '@elastic/eui';
 import { Hunks } from './hunks';
 import { markEdits, DiffMethod } from './mark_edits';
 
@@ -143,112 +143,74 @@ const CODE_CLASS_NAME = 'rule-update-diff-code';
 const GUTTER_CLASS_NAME = 'rule-update-diff-gutter';
 const DARK_THEME_CLASS_NAME = 'rule-update-diff-dark-theme';
 
-const GITHUB_STYLES_CLASS_NAME = 'NIKITA-DONT-FORGET-TO-REMOVE-THIS-CLASS';
-
 const CustomStyles: React.FC = ({ children }) => {
   const { euiTheme } = useEuiTheme();
 
-  const insertionRgb = hexToRgb(euiTheme.colors.successText);
-  const deletionRgb = hexToRgb(euiTheme.colors.dangerText);
-
   const customCss = css`
-    .${TABLE_CLASS_NAME} .diff-gutter-col {
-      width: ${euiTheme.size.xl};
-    }
-
-    .${CODE_CLASS_NAME}.diff-code, .${GUTTER_CLASS_NAME}.diff-gutter {
-      background: transparent;
-    }
-
-    .${GUTTER_CLASS_NAME}:nth-child(3) {
-      border-left: 1px solid ${euiTheme.colors.mediumShade};
-    }
-
     .${CODE_CLASS_NAME}.diff-code {
       padding: 0 ${euiTheme.size.l} 0 ${euiTheme.size.m};
     }
 
-    //
+    .${TABLE_CLASS_NAME} .diff-gutter-col {
+      width: ${euiTheme.size.xl};
+    }
 
+    /* Vertical line separating two sides of the diff view */
+    .${GUTTER_CLASS_NAME}:nth-child(3) {
+      border-left: 1px solid ${euiTheme.colors.mediumShade};
+    }
+
+    /* Gutter of a line with deletions */
     .${GUTTER_CLASS_NAME}.diff-gutter-delete {
       color: ${euiTheme.colors.dangerText};
       font-weight: bold;
-    }
-    .${GITHUB_STYLES_CLASS_NAME} .${GUTTER_CLASS_NAME}.diff-gutter-delete {
       background: rgb(255, 215, 213);
     }
-    .${DARK_THEME_CLASS_NAME}.${GITHUB_STYLES_CLASS_NAME} .${GUTTER_CLASS_NAME}.diff-gutter-delete {
+    .${DARK_THEME_CLASS_NAME}.${GUTTER_CLASS_NAME}.diff-gutter-delete {
       background: rgba(248, 81, 73, 0.3);
     }
 
+    /* Gutter of a line with insertions */
     .${GUTTER_CLASS_NAME}.diff-gutter-insert {
       color: ${euiTheme.colors.successText};
       font-weight: bold;
-    }
-    .${GITHUB_STYLES_CLASS_NAME} .${GUTTER_CLASS_NAME}.diff-gutter-insert {
       background: rgb(204, 255, 216);
     }
-    .${DARK_THEME_CLASS_NAME}.${GITHUB_STYLES_CLASS_NAME} .${GUTTER_CLASS_NAME}.diff-gutter-insert {
+    .${DARK_THEME_CLASS_NAME}.${GUTTER_CLASS_NAME}.diff-gutter-insert {
       background: rgba(63, 185, 80, 0.3);
     }
 
-    //
-
+    /* Background of a line with deletions */
     .${CODE_CLASS_NAME}.diff-code-delete {
-      background: transparent;
-    }
-    .${GITHUB_STYLES_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-delete {
       background: rgb(255, 235, 233);
     }
-    .${DARK_THEME_CLASS_NAME}.${GITHUB_STYLES_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-delete {
+    .${DARK_THEME_CLASS_NAME}.${CODE_CLASS_NAME}.diff-code-delete {
       background: rgba(248, 81, 73, 0.1);
     }
 
+    /* Background of a line with insertions */
     .${CODE_CLASS_NAME}.diff-code-insert {
-      background: transparent;
-    }
-    .${GITHUB_STYLES_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-insert {
       background: rgb(230, 255, 236);
     }
-    .${DARK_THEME_CLASS_NAME}.${GITHUB_STYLES_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-insert {
+    .${DARK_THEME_CLASS_NAME}.${CODE_CLASS_NAME}.diff-code-insert {
       background: rgba(46, 160, 67, 0.15);
     }
 
-    //
-
+    /* Accented background of removed characters / words */
     .${CODE_CLASS_NAME}.diff-code-delete .diff-code-edit {
-      color: ${euiTheme.colors.dangerText};
-      text-decoration: line-through;
-      background: rgba(${deletionRgb.join()}, 0.05);
-    }
-    .${GITHUB_STYLES_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-delete .diff-code-edit {
-      color: unset;
-      text-decoration: none;
+      font-weight: 700;
       background: rgba(255, 129, 130, 0.4);
     }
-    .${DARK_THEME_CLASS_NAME}.${GITHUB_STYLES_CLASS_NAME}
-      .${CODE_CLASS_NAME}.diff-code-delete
-      .diff-code-edit {
-      color: unset;
-      text-decoration: none;
+    .${DARK_THEME_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-delete .diff-code-edit {
       background: rgba(248, 81, 73, 0.4);
     }
 
+    /* Accented background of inserted characters / words */
     .${CODE_CLASS_NAME}.diff-code-insert .diff-code-edit {
-      color: ${euiTheme.colors.successText};
-      text-decoration: underline;
-      background: rgba(${insertionRgb.join()}, 0.05);
-    }
-    .${GITHUB_STYLES_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-insert .diff-code-edit {
-      color: unset;
-      text-decoration: none;
+      font-weight: 700;
       background: rgb(171, 242, 188);
     }
-    .${DARK_THEME_CLASS_NAME}.${GITHUB_STYLES_CLASS_NAME}
-      .${CODE_CLASS_NAME}.diff-code-insert
-      .diff-code-edit {
-      color: unset;
-      text-decoration: none;
+    .${DARK_THEME_CLASS_NAME} .${CODE_CLASS_NAME}.diff-code-insert .diff-code-edit {
       background: rgba(46, 160, 67, 0.4);
     }
   `;
@@ -297,34 +259,12 @@ export const DiffView = ({
 
   const { colorMode } = useEuiTheme();
 
-  const [selectedStyle, setSelectedStyle] = React.useState('our-styles');
-
   const tableClassName = classNames(TABLE_CLASS_NAME, {
-    [DARK_THEME_CLASS_NAME]: colorMode === 'DARK',
-    [GITHUB_STYLES_CLASS_NAME]: selectedStyle === 'github-styles',
+    [DARK_THEME_CLASS_NAME]: colorMode === COLOR_MODES_STANDARD.dark,
   });
 
   return (
     <CustomStyles>
-      <EuiRadioGroup
-        options={[
-          {
-            id: 'our-styles',
-            label: 'Our styles with added line highlighting',
-          },
-          {
-            id: 'github-styles',
-            label: 'GitHub styles',
-          },
-        ]}
-        idSelected={selectedStyle}
-        onChange={(optionId) => {
-          setSelectedStyle(optionId);
-        }}
-        legend={{
-          children: <span>{'Highlighting style (also try with dark theme)'}</span>,
-        }}
-      />
       <Diff
         /*
           "diffType": can be either 'add', 'delete', 'modify', 'rename' or 'copy'.
