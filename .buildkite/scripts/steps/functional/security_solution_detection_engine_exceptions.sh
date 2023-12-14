0db11129f2995ad360de4c73b92d5bd5c71b4df8
dkirchan <diamantis.kirchantzoglou@elastic.co>
2023-12-14 14:50:11 +0200

    Changes vault_read to vault_get

--- .buildkite/scripts/steps/functional/security_solution_detection_engine_exceptions.sh
+++ .buildkite/scripts/steps/functional/security_solution_detection_engine_exceptions.sh
@@ -13,6 +13,6 @@ echo "--- Detection Engine - Exceptions - Security Solution Cypress Tests"
 cd x-pack/test/security_solution_cypress
 
 set +e
-BK_ANALYTICS_API_KEY=$(retry 5 5 vault read -field=sec-sol-cypress-bk-api-key secret/kibana-issues/dev/security-solution-ci)
+BK_ANALYTICS_API_KEY=$(vault_get security-solution-ci sec-sol-cypress-bk-api-key)
 
 BK_ANALYTICS_API_KEY=$BK_ANALYTICS_API_KEY yarn cypress:detection_engine:exceptions:run:ess; status=$?; yarn junit:merge || :; exit $status
--- .buildkite/scripts/steps/functional/threat_intelligence.sh
+++ .buildkite/scripts/steps/functional/threat_intelligence.sh
@@ -10,6 +10,6 @@ export KIBANA_INSTALL_DIR=${KIBANA_BUILD_LOCATION}
 
 echo "--- Threat Intelligence Cypress tests (Chrome)"
 
-BK_ANALYTICS_API_KEY=$(retry 5 5 vault read -field=threat-intelligence-bk-api-key secret/kibana-issues/dev/security-solution-ci)
+BK_ANALYTICS_API_KEY=$(vault_get security-solution-ci threat-intelligence-bk-api-key)
 
 BK_ANALYTICS_API_KEY=$BK_ANALYTICS_API_KEY yarn --cwd x-pack/plugins/threat_intelligence cypress:run
