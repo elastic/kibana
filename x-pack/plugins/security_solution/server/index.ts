46f79b858bf8df95c0151cec3d99f709c23d2bb6
Jonathan Budzenski <jon@elastic.co>
2023-12-15 14:11:37 -0600

    test

--- x-pack/plugins/security_solution/server/index.ts
+++ x-pack/plugins/security_solution/server/index.ts
@@ -17,6 +17,7 @@ export const plugin = async (context: PluginInitializerContext) => {
   return new Plugin(context);
 };
 
+// testing a change
 export const config: PluginConfigDescriptor<ConfigSchema> = {
   exposeToBrowser: {
     enableExperimental: true,
