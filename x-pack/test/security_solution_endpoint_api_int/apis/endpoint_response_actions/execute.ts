2dce69eebc30ef849d3e8df61be8da73844eddf7
Tiago Costa <tiago.costa@elastic.co>
2023-12-19 17:22:49 +0000

    skip flaky suite (#171666)

--- x-pack/test/security_solution_endpoint_api_int/apis/endpoint_response_actions/execute.ts
+++ x-pack/test/security_solution_endpoint_api_int/apis/endpoint_response_actions/execute.ts
@@ -16,7 +16,7 @@ export default function ({ getService }: FtrProviderContext) {
   const supertestWithoutAuth = getService('supertestWithoutAuth');
   const endpointTestResources = getService('endpointTestResources');
 
-  // Flaky: https://github.com/elastic/kibana/issues/171666
+  // FLAKY: https://github.com/elastic/kibana/issues/171666
   describe.skip('Endpoint `execute` response action', function () {
     targetTags(this, ['@ess', '@serverless']);
 
