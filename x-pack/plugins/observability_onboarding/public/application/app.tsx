ce739fa89bb0d03971a6e22b7deebf79254fcc8a
pgayvallet <pierre.gayvallet@elastic.co>
2023-12-19 16:37:45 +0100

    observability_onboarding plugin

--- x-pack/plugins/observability_onboarding/public/application/app.tsx
+++ x-pack/plugins/observability_onboarding/public/application/app.tsx
@@ -16,7 +16,7 @@ import { i18n } from '@kbn/i18n';
 import {
   KibanaContextProvider,
   KibanaThemeProvider,
-  useUiSetting$,
+  useKibana,
 } from '@kbn/kibana-react-plugin/public';
 import { RedirectAppLinks } from '@kbn/shared-ux-link-redirect-app';
 import { HeaderMenuPortal } from '@kbn/observability-shared-plugin/public';
@@ -112,7 +112,10 @@ function App() {
 }
 
 function ObservabilityOnboardingApp() {
-  const [darkMode] = useUiSetting$<boolean>('theme:darkMode');
+  const {
+    services: { theme },
+  } = useKibana();
+  const darkMode = theme?.getTheme().darkMode ?? false;
 
   return (
     <ThemeProvider
