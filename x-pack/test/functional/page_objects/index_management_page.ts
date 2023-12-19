5f0e238006e3347bbf1d30c4a97a7f4bb54bb8eb
Yulia Cech <yulia.cech@elastic.co>
2023-12-19 15:08:44 +0100

    Fix functional tests

--- x-pack/test/functional/page_objects/index_management_page.ts
+++ x-pack/test/functional/page_objects/index_management_page.ts
@@ -22,9 +22,6 @@ export function IndexManagementPageProvider({ getService }: FtrProviderContext) 
     async reloadIndicesButton() {
       return await testSubjects.find('reloadIndicesButton');
     },
-    async toggleRollupIndices() {
-      await testSubjects.click('checkboxToggles-rollupToggle');
-    },
     async toggleHiddenIndices() {
       await testSubjects.click('indexTableIncludeHiddenIndicesToggle');
     },
@@ -34,9 +31,8 @@ export function IndexManagementPageProvider({ getService }: FtrProviderContext) 
       await policyDetailsLinks[indexOfRow].click();
     },
 
-    async clickDataStreamAt(indexOfRow: number): Promise<void> {
-      const dataStreamLinks = await testSubjects.findAll('nameLink');
-      await dataStreamLinks[indexOfRow].click();
+    async clickDataStreamNameLink(name: string): Promise<void> {
+      await find.clickByLinkText(name);
     },
 
     async clickDeleteEnrichPolicyAt(indexOfRow: number): Promise<void> {
