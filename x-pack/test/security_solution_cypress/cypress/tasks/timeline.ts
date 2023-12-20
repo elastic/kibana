e53c0ff80d1e15add095349d09cf1e73b191a631
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2023-12-20 13:55:05 -0500

    port changes

--- x-pack/test/security_solution_cypress/cypress/tasks/timeline.ts
+++ x-pack/test/security_solution_cypress/cypress/tasks/timeline.ts
@@ -10,7 +10,6 @@ import type { Timeline, TimelineFilter } from '../objects/timeline';
 
 import { ALL_CASES_CREATE_NEW_CASE_TABLE_BTN } from '../screens/all_cases';
 import { FIELDS_BROWSER_CHECKBOX } from '../screens/fields_browser';
-import { LOADING_INDICATOR } from '../screens/security_header';
 import { EQL_QUERY_VALIDATION_SPINNER } from '../screens/create_new_rule';
 
 import {
@@ -65,7 +64,6 @@ import {
   TIMELINE_COLLAPSED_ITEMS_BTN,
   TIMELINE_TAB_CONTENT_EQL,
   TIMESTAMP_HOVER_ACTION_OVERFLOW_BTN,
-  TIMELINE_DATA_PROVIDER_FIELD_INPUT,
   ACTIVE_TIMELINE_BOTTOM_BAR,
   EMPTY_DATA_PROVIDER_AREA,
   EMPTY_DROPPABLE_DATA_PROVIDER_GROUP,
@@ -85,6 +83,7 @@ import {
   TIMELINE_KQLMODE_FILTER,
   TIMELINE_KQLMODE_SEARCH,
   OPEN_TIMELINE_MODAL,
+  TIMELINE_DATA_PROVIDERS_CONTAINER,
 } from '../screens/timeline';
 
 import { REFRESH_BUTTON, TIMELINE, TIMELINES_TAB_TEMPLATE } from '../screens/timelines';
@@ -173,7 +172,6 @@ export const addNotesToTimeline = (notes: string) => {
     .then((notesCount) => {
       cy.get(NOTES_TEXT_AREA).type(notes, {
         parseSpecialCharSequences: false,
-        force: true,
       });
 
       cy.get(ADD_NOTE_BUTTON).click();
@@ -214,19 +212,21 @@ export const changeTimelineQueryLanguage = (language: 'kuery' | 'lucene') => {
 };
 
 export const addDataProvider = (filter: TimelineFilter): Cypress.Chainable<JQuery<HTMLElement>> => {
+  cy.get(TOGGLE_DATA_PROVIDER_BTN).click();
+  cy.get(TIMELINE_DATA_PROVIDERS_CONTAINER).should('be.visible'); // Cypress doesn't properly wait for the data provider to finish expanding, so we wait for the animation to finish.
   cy.get(TIMELINE_ADD_FIELD_BUTTON).click();
-  cy.get(LOADING_INDICATOR).should('not.exist');
-  cy.get('[data-popover-open]').should('exist');
-  cy.get(TIMELINE_DATA_PROVIDER_FIELD).click();
-  cy.get(TIMELINE_DATA_PROVIDER_FIELD)
-    .find(TIMELINE_DATA_PROVIDER_FIELD_INPUT)
-    .should('have.focus'); // make sure the focus is ready before start typing
   cy.get(TIMELINE_DATA_PROVIDER_FIELD)
     .find(COMBO_BOX_INPUT)
     .type(`${filter.field}{downarrow}{enter}`);
+
+  cy.get(TIMELINE_DATA_PROVIDER_OPERATOR)
+    .find(`${COMBO_BOX_INPUT} input`)
+    .type(`{selectall}{backspace}{selectall}{backspace}`);
+
   cy.get(TIMELINE_DATA_PROVIDER_OPERATOR)
     .find(COMBO_BOX_INPUT)
     .type(`${filter.operator}{downarrow}{enter}`);
+
   if (filter.operator !== 'exists') {
     cy.get(TIMELINE_DATA_PROVIDER_VALUE).type(`${filter.value}{enter}`);
   }
@@ -257,7 +257,7 @@ export const updateDataProviderbyDraggingField = (fieldName: string, rowNumber: 
 
 export const updateDataProviderByFieldHoverAction = (fieldName: string, rowNumber: number) => {
   const fieldSelector = GET_TIMELINE_GRID_CELL(fieldName);
-  cy.get(fieldSelector).eq(rowNumber).trigger('mouseover', { force: true });
+  cy.get(fieldSelector).eq(rowNumber).trigger('mouseover');
   cy.get(HOVER_ACTIONS.ADD_TO_TIMELINE).should('be.visible');
   recurse(
     () => {
@@ -292,9 +292,7 @@ export const clickIdToggleField = () => {
   clickIdHoverActionOverflowButton();
   cy.get(ID_HEADER_FIELD).should('not.exist');
 
-  cy.get(ID_TOGGLE_FIELD).click({
-    force: true,
-  });
+  cy.get(ID_TOGGLE_FIELD).click();
 };
 
 export const closeTimeline = () => {
