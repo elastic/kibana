11a96b34505ae9e55edc86fd34cd2092cf7a623d
Shahzad <shahzad31comp@gmail.com>
2023-12-20 18:24:28 +0100

    Update x-pack/plugins/synthetics/public/apps/synthetics/components/certificates/cert_monitors.tsx

--- x-pack/plugins/synthetics/public/apps/synthetics/components/certificates/cert_monitors.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/certificates/cert_monitors.tsx
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import React from 'react';
 import { EuiToolTip } from '@elastic/eui';
 import { MonitorPageLink } from './monitor_page_link';
