735db7e7180f5c97cf7568f9be5814efe1cae69d
Pablo Machado <pablo.nevesmachado@elastic.co>
2023-12-20 09:45:35 +0100

    [Security Solution] Merge risk input and asset integration expandable panels (#173028)
    ## Summary
    
    Update the user expandable flyout left panel to be consistent with
    Alerts flyout implementation. After this change, we always display the
    same left panel but dynamically create tabs for different sub-panels.
    
    This issue can be considered a UI/UX bug because the user's experience
    of navigating between the tabs was poor and had some edge cases.
    
    This PR also moves flyout files and folders to match the Alert flyout
    structure. The new folder structure looks like this:
    ```
    public/flyout
      ...
      entity_details
        shared
        user_right
        user_details_left
          ...
          tabs
            ...
            asset_document.tsx
            risk_inputs.tsx
        
    ```
    
    
    **No tab**
    <img width="400px"
    src="https://github.com/elastic/kibana/assets/1490444/f0546410-7bd8-4685-9933-58879714a653">
    
    **One tab**
    <img width="600px"
    src="https://github.com/elastic/kibana/assets/1490444/7f9291bc-b617-4310-8bd1-acb77bec555e">
    
    
    **Two tabs**
    <img width="600px"
    src="https://github.com/elastic/kibana/assets/1490444/ba347124-ba71-4206-8453-ceff5455f156">
    
    **Okta tab with table**
    <img width="400px"
    src="https://github.com/elastic/kibana/assets/1490444/0cdae267-6be6-4e4f-bacd-5e7d2e778a3b">
    
    **Okta tab with json**
    <img width="400px"
    src="https://github.com/elastic/kibana/assets/1490444/e630581b-6122-4532-9f09-a7616b1fceac">
    
    
    ### How to test it
    Please check the same section on this PR:
    https://github.com/elastic/kibana/pull/171629
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [x] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))

--- x-pack/plugins/security_solution/public/flyout/entity_details/user_detais_left/tabs/risk_inputs.tsx
+++ x-pack/plugins/security_solution/public/flyout/entity_details/user_detais_left/tabs/risk_inputs.tsx
@@ -6,21 +6,18 @@
  */
 
 import type { EuiBasicTableColumn, Pagination } from '@elastic/eui';
-import { useEuiBackgroundColor, EuiSpacer, EuiInMemoryTable, EuiTitle } from '@elastic/eui';
+import { EuiSpacer, EuiInMemoryTable, EuiTitle } from '@elastic/eui';
 import React, { useCallback, useMemo, useState } from 'react';
-import { css } from '@emotion/react';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { get } from 'lodash/fp';
 import { ALERT_RULE_NAME } from '@kbn/rule-data-utils';
-import type { RiskInputs } from '../../../../common/entity_analytics/risk_engine';
-import { ActionColumn } from './components/action_column';
-import { PreferenceFormattedDate } from '../../../common/components/formatted_date';
-import { RiskInputsUtilityBar } from './components/utility_bar';
-import { useAlertsByIds } from '../../../common/containers/alerts/use_alerts_by_ids';
-import { FlyoutBody } from '../../shared/components/flyout_body';
+import { useAlertsByIds } from '../../../../common/containers/alerts/use_alerts_by_ids';
+import { PreferenceFormattedDate } from '../../../../common/components/formatted_date';
+import { ActionColumn } from '../components/action_column';
+import { RiskInputsUtilityBar } from '../components/utility_bar';
 
-export interface RiskInputsPanelContentProps extends Record<string, unknown> {
-  riskInputs: RiskInputs;
+export interface RiskInputsTabProps extends Record<string, unknown> {
+  alertIds: string[];
 }
 
 export interface AlertRawData {
@@ -29,9 +26,8 @@ export interface AlertRawData {
   _id: string;
 }
 
-export const RiskInputsPanelContent = ({ riskInputs }: RiskInputsPanelContentProps) => {
+export const RiskInputsTab = ({ alertIds }: RiskInputsTabProps) => {
   const [selectedItems, setSelectedItems] = useState<AlertRawData[]>([]);
-  const alertIds = useMemo(() => riskInputs.map(({ id }) => id), [riskInputs]);
   const { loading, data: alertsData } = useAlertsByIds({ alertIds });
 
   const euiTableSelectionProps = useMemo(
@@ -102,57 +98,41 @@ export const RiskInputsPanelContent = ({ riskInputs }: RiskInputsPanelContentPro
 
   const pagination: Pagination = useMemo(
     () => ({
-      totalItemCount: riskInputs.length,
+      totalItemCount: alertIds.length,
       pageIndex: currentPage.index,
       pageSize: currentPage.size,
     }),
-    [currentPage.index, currentPage.size, riskInputs.length]
+    [currentPage.index, currentPage.size, alertIds.length]
   );
 
   return (
     <>
-      <FlyoutBody
-        css={css`
-          background-color: ${useEuiBackgroundColor('subdued')};
-        `}
-        data-test-subj="risk-inputs-panel"
-      >
-        <EuiTitle>
-          <h2>
-            <FormattedMessage
-              id="xpack.securitySolution.flyout.entityDetails.riskInputs.title"
-              defaultMessage="Risk Inputs"
-            />
-          </h2>
-        </EuiTitle>
-        <EuiSpacer size="m" />
-        {/* Temporary label. It will be replaced by a filter */}
-        <EuiTitle size="xs">
-          <h3>
-            <FormattedMessage
-              id="xpack.securitySolution.flyout.entityDetails.riskInputs.alertsTitle"
-              defaultMessage="Alerts"
-            />
-          </h3>
-        </EuiTitle>
-        <EuiSpacer size="xs" />
-        <RiskInputsUtilityBar pagination={pagination} selectedAlerts={selectedItems} />
-        <EuiSpacer size="xs" />
-        <EuiInMemoryTable
-          compressed={true}
-          loading={loading}
-          items={alertsData ?? []}
-          columns={columns}
-          pagination
-          sorting
-          selection={euiTableSelectionProps}
-          onTableChange={onTableChange}
-          isSelectable
-          itemId="_id"
-        />
-      </FlyoutBody>
+      {/* Temporary label. It will be replaced by a filter */}
+      <EuiTitle size="xs" data-test-subj="risk-input-tab-title">
+        <h3>
+          <FormattedMessage
+            id="xpack.securitySolution.flyout.entityDetails.riskInputs.alertsTitle"
+            defaultMessage="Alerts"
+          />
+        </h3>
+      </EuiTitle>
+      <EuiSpacer size="xs" />
+      <RiskInputsUtilityBar pagination={pagination} selectedAlerts={selectedItems} />
+      <EuiSpacer size="xs" />
+      <EuiInMemoryTable
+        compressed={true}
+        loading={loading}
+        items={alertsData ?? []}
+        columns={columns}
+        pagination
+        sorting
+        selection={euiTableSelectionProps}
+        onTableChange={onTableChange}
+        isSelectable
+        itemId="_id"
+      />
     </>
   );
 };
 
-RiskInputsPanelContent.displayName = 'RiskInputsPanelContent';
+RiskInputsTab.displayName = 'RiskInputsTab';
