389d095fa2c8f496e9c149709c20d46feaeb0bd9
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2023-12-21 12:06:40 +0100

    refactor(log-shared): ai assistant factory type

--- x-pack/plugins/logs_shared/public/components/log_ai_assistant/index.tsx
+++ x-pack/plugins/logs_shared/public/components/log_ai_assistant/index.tsx
@@ -4,18 +4,16 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { ObservabilityAIAssistantPluginStart } from '@kbn/observability-ai-assistant-plugin/public';
 import React from 'react';
 import { dynamic } from '../../../common/dynamic';
+import { LogAIAssistantProps } from './log_ai_assistant';
 
 export const LogAIAssistant = dynamic(() => import('./log_ai_assistant'));
 
 export function createLogAIAssistant({
   observabilityAIAssistant,
-}: {
-  observabilityAIAssistant: ObservabilityAIAssistantPluginStart;
-}) {
-  return ({ ...props }) => (
+}: Pick<LogAIAssistantProps, 'observabilityAIAssistant'>) {
+  return (props: Omit<LogAIAssistantProps, 'observabilityAIAssistant'>) => (
     <LogAIAssistant observabilityAIAssistant={observabilityAIAssistant} {...props} />
   );
 }
