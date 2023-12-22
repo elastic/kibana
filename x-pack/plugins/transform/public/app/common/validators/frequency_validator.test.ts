0e8ab71711b11ae83d836066592f51e22bfd2216
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2023-12-22 11:57:36 +0100

    remove helper function

--- x-pack/plugins/transform/public/app/common/validators/frequency_validator.test.ts
+++ x-pack/plugins/transform/public/app/common/validators/frequency_validator.test.ts
@@ -8,40 +8,41 @@
 import { frequencyValidator } from './frequency_validator';
 
 describe('Transform: frequencyValidator()', () => {
-  const transformFrequencyValidator = (arg: string) => frequencyValidator(arg).length === 0;
+  // frequencyValidator() returns an array of error messages so
+  // an array with a length of 0 means a successful validation.
 
   it('should fail when the input is not an integer and valid time unit.', () => {
-    expect(transformFrequencyValidator('0')).toBe(false);
-    expect(transformFrequencyValidator('0.1s')).toBe(false);
-    expect(transformFrequencyValidator('1.1m')).toBe(false);
-    expect(transformFrequencyValidator('10.1asdf')).toBe(false);
+    expect(frequencyValidator('0')).toHaveLength(1);
+    expect(frequencyValidator('0.1s')).toHaveLength(1);
+    expect(frequencyValidator('1.1m')).toHaveLength(1);
+    expect(frequencyValidator('10.1asdf')).toHaveLength(1);
   });
 
   it('should only allow s/m/h as time unit.', () => {
-    expect(transformFrequencyValidator('1ms')).toBe(false);
-    expect(transformFrequencyValidator('1s')).toBe(true);
-    expect(transformFrequencyValidator('1m')).toBe(true);
-    expect(transformFrequencyValidator('1h')).toBe(true);
-    expect(transformFrequencyValidator('1d')).toBe(false);
+    expect(frequencyValidator('1ms')).toHaveLength(1);
+    expect(frequencyValidator('1s')).toHaveLength(0);
+    expect(frequencyValidator('1m')).toHaveLength(0);
+    expect(frequencyValidator('1h')).toHaveLength(0);
+    expect(frequencyValidator('1d')).toHaveLength(1);
   });
 
   it('should only allow values above 0 and up to 1 hour.', () => {
-    expect(transformFrequencyValidator('0s')).toBe(false);
-    expect(transformFrequencyValidator('1s')).toBe(true);
-    expect(transformFrequencyValidator('3599s')).toBe(true);
-    expect(transformFrequencyValidator('3600s')).toBe(true);
-    expect(transformFrequencyValidator('3601s')).toBe(false);
-    expect(transformFrequencyValidator('10000s')).toBe(false);
+    expect(frequencyValidator('0s')).toHaveLength(1);
+    expect(frequencyValidator('1s')).toHaveLength(0);
+    expect(frequencyValidator('3599s')).toHaveLength(0);
+    expect(frequencyValidator('3600s')).toHaveLength(0);
+    expect(frequencyValidator('3601s')).toHaveLength(1);
+    expect(frequencyValidator('10000s')).toHaveLength(1);
 
-    expect(transformFrequencyValidator('0m')).toBe(false);
-    expect(transformFrequencyValidator('1m')).toBe(true);
-    expect(transformFrequencyValidator('59m')).toBe(true);
-    expect(transformFrequencyValidator('60m')).toBe(true);
-    expect(transformFrequencyValidator('61m')).toBe(false);
-    expect(transformFrequencyValidator('100m')).toBe(false);
+    expect(frequencyValidator('0m')).toHaveLength(1);
+    expect(frequencyValidator('1m')).toHaveLength(0);
+    expect(frequencyValidator('59m')).toHaveLength(0);
+    expect(frequencyValidator('60m')).toHaveLength(0);
+    expect(frequencyValidator('61m')).toHaveLength(1);
+    expect(frequencyValidator('100m')).toHaveLength(1);
 
-    expect(transformFrequencyValidator('0h')).toBe(false);
-    expect(transformFrequencyValidator('1h')).toBe(true);
-    expect(transformFrequencyValidator('2h')).toBe(false);
+    expect(frequencyValidator('0h')).toHaveLength(1);
+    expect(frequencyValidator('1h')).toHaveLength(0);
+    expect(frequencyValidator('2h')).toHaveLength(1);
   });
 });
