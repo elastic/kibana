994360b9cff61ad2b053507f393a738aa581f798
Patryk Kopyciński <contact@patrykkopycinski.com>
2023-12-28 17:35:17 +0100

    Replace deprecated node-sass with sass #2 (#173942)
    The previous PR https://github.com/elastic/kibana/pull/161813 was
    reverted due to the broken webpack config
    
    https://github.com/elastic/kibana/commit/eef1afcd59334e94e8ef3a99b62176099125e336
    
    ---------
    
    Co-authored-by: Tiago Costa <tiago.costa@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Jon <jon@elastic.co>

--- packages/kbn-optimizer/src/worker/webpack.config.ts
+++ packages/kbn-optimizer/src/worker/webpack.config.ts
@@ -171,12 +171,12 @@ export function getWebpackConfig(bundle: Bundle, bundleRefs: BundleRefs, worker:
                         )
                       )};\n${content}`;
                     },
-                    webpackImporter: false,
-                    implementation: require('node-sass'),
+                    implementation: require('sass-embedded'),
                     sassOptions: {
-                      outputStyle: worker.dist ? 'compressed' : 'nested',
+                      outputStyle: worker.dist ? 'compressed' : 'expanded',
                       includePaths: [Path.resolve(worker.repoRoot, 'node_modules')],
-                      sourceMapRoot: `/${bundle.type}:${bundle.id}`,
+                      sourceMap: true,
+                      quietDeps: true,
                     },
                   },
                 },
