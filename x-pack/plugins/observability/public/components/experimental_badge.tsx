3430d18f07f02d0721e65e2710f17b95bda5cf56
Coen Warmer <coen.warmer@gmail.com>
2023-12-29 21:57:11 +0100

    test commit

--- x-pack/plugins/observability/public/components/experimental_badge.tsx
+++ x-pack/plugins/observability/public/components/experimental_badge.tsx
@@ -15,7 +15,7 @@ export function ExperimentalBadge() {
       label={i18n.translate('xpack.observability.experimentalBadgeLabel', {
         defaultMessage: 'Technical preview',
       })}
-      tooltipContent={i18n.translate('xpack.observability.experimentalBadgeDescription', {
+      tooltipContent={i18n.translate('xpack.observability.experimentalBadgeDescription2', {
         defaultMessage:
           'This functionality is in technical preview and may be changed or removed completely in a future release. Elastic will work to fix any issues, but features in technical preview are not subject to the support SLA of official GA features.',
       })}
