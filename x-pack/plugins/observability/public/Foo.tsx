4e04bf40d19398d0cc409f13095f62ae50ed4dd4
Coen Warmer <coen.warmer@gmail.com>
2023-12-29 20:47:18 +0100

    test commit

---
+++ x-pack/plugins/observability/public/Foo.tsx
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { i18n } from '@kbn/i18n';
+import { AlertStatusIndicator } from './components/alert_status_indicator';
+
+export function Foo() {
+  return (
+    <div>
+      <AlertStatusIndicator />
+      {i18n.translate('xpack.observability.foo.div.helloLabel', { defaultMessage: 'hello' })}
+    </div>
+  );
+}
