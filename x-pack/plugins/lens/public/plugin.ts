0c6943c8df71cbe62b5854cf5e7a294dd0393a7f
rshen91 <rshen@elastic.co>
2023-12-29 07:07:52 -0700

    start services

--- x-pack/plugins/lens/public/plugin.ts
+++ x-pack/plugins/lens/public/plugin.ts
@@ -384,8 +384,8 @@ export class LensPlugin {
           uiSettings: core.uiSettings,
           formatFactoryFn: () => startServices().plugins.fieldFormats.deserialize,
           theme: core.theme,
-          overlays: startServices().core.overlays,
-          i18nStart: startServices().core.i18n,
+          overlays: () => startServices().core.overlays,
+          i18nStart: () => startServices().core.i18n,
         })
       );
     }
