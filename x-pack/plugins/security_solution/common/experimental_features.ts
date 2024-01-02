74594a8292ec2a7ec6c1ac0e3faf9a87c8bf138a
Jatin Kathuria <jatin.kathuria@elastic.co>
2024-01-02 17:53:12 +0100

    [Security Solution] Adds feature flag to enable/disable ESQL in timeline (#174029)
    ## Summary
    
    This PR introduces a feature flag `timelineEsqlTabDisabled` which is by
    default `false`. This gives customer ability to disable the esql tab by
    enabling this experimental feature flag as below in `kibana.yml`
    
    ```yaml
    xpack.securitySolution.enableExperimental:
      - timelineEsqlTabDisabled
    
    ```
    
    The availability of ESQL Tab in timeline also affects `AI Assistant` as
    it facilities re-directing user to timeline with an esql query. That
    `redirect` button should not be available for `esql` query if ESQL Tab
    is disabled.
    
    ## Desk Testing
    
    1. ESQL Tab Presence
        -  timelineEsqlTabDisabled :  true
    - If Tab is disabled, `ESQL` Tab should not show when timeline is open.
    Timeline should also not fire any `bsearch` requests with `esql`
    strategy.
    - ESQL tab is enabled i.e. `timelineEsqlTabDisabled : true` is present
    in kibana.dev.yml
    - User should be able to use ESQL queries without any issue. Below
    should be the default query in both `8.12` and `8.11.4`
           ```esql
    from
    .alerts-security.alerts-default,apm-*-transaction*,auditbeat-*,endgame-*,filebeat-*,logs-*,packetbeat-*,traces-apm*,winlogbeat-*,-*elastic-cloud-logs-*
    | limit 10 | keep @timestamp, message, event.category, event.action,
    host.name, source.ip, destination.ip, user.name
           ```
    2. Open ESQL Tab from URL
        1. Enable ESQL tab and Activate it when in timeline
    2. Now change `kibana.dev.yml` to add experimental flag
    `timelineEsqlTabDisabled` to disable ESQL Tab.
        3. Restart kibana server
        4. Refresh the page in step 1 where `ESQL` tab was active
        5. User should automatically be redirected to `Query` tab.
    
    3. AI Assistant
    Today AI Assistant can help user add an ESQL query to the timeline as
    shown in below video.
    We need to make sure that `Send to timeline` button is not available
    only for `esql` queries when above experimental flag is enabled.
    
    https://github.com/elastic/kibana/assets/7485038/e452a6c6-cf97-462e-a5dc-bd8c0fd38d58
    
    ---------
    
    Co-authored-by: Gloria Hornero <gloria.hornero@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit da0370eafb49f15fe655d0e2b8d254e09c9f814a)

--- x-pack/plugins/security_solution/common/experimental_features.ts
+++ x-pack/plugins/security_solution/common/experimental_features.ts
@@ -153,6 +153,11 @@ export const allowedExperimentalValues = Object.freeze({
    * version and the latest available version.
    */
   jsonPrebuiltRulesDiffingEnabled: true,
+  /*
+   * Disables discover esql tab within timeline
+   *
+   */
+  timelineEsqlTabDisabled: false,
 });
 
 type ExperimentalConfigKeys = Array<keyof ExperimentalFeatures>;
