elastic/kibana

commit 63f4e38f6554c78376d1541f9188cd4d5319e87f

[Fleet] Display message explaining why agent is not upgradeable (#173253)
Closes https://github.com/elastic/kibana/issues/171840
Also implements the UI part of
https://github.com/elastic/kibana/issues/173281

## Summary
When trying to upgrade a single agent that is not upgradeable, the error
message doesn't specify _why_ the agent cannot be upgraded. This PR is
introducing:
- More granular error messages in the endpoint `POST
agent/{agent_id}/upgrade`. The messages are now different depending on
which conditions are met. For the case when the agent is reported not
upgradeable from elastic agent, there is now an error message the states
it explicitly.
- When clicking on the `upgrade 1 agent` from the bulk actions, if the
agent is not upgradeable the submit button is now greyed out and a
message explaining the reason is shown:
- 
![Screenshot 2023-12-18 at 14 41
48](https://github.com/elastic/kibana/assets/16084106/8e0a03b4-c8fc-4a2e-aea6-77c7cc1acdf3)

The same warning appears when clicking on other upgrade actions in the
bulk action menu, but only for a single agent. Multiple upgrades have
not been changed.

- In the agents list, reuse the existing tooltip besides the version to
show the same messages when the agent is not upgradeable:
![Screenshot 2023-12-18 at 14 40
32](https://github.com/elastic/kibana/assets/16084106/ada30bc0-8c58-40d9-b7cf-c5c7a81a75f7)




### Checklist
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

---------

Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/components/agent_upgrade_status.tsx
@@ -219,13 +219,38 @@ export const AgentUpgradeStatus: React.FC<{
   agentUpgradeStartedAt?: string | null;
   agentUpgradedAt?: string | null;
   agentUpgradeDetails?: AgentUpgradeDetails;
-}> = ({ isAgentUpgradable, agentUpgradeStartedAt, agentUpgradedAt, agentUpgradeDetails }) => {
+  notUpgradeableMessage?: string | null;
+}> = ({
+  isAgentUpgradable,
+  agentUpgradeStartedAt,
+  agentUpgradedAt,
+  agentUpgradeDetails,
+  notUpgradeableMessage,
+}) => {
   const isAgentUpgrading = useMemo(
     () => agentUpgradeStartedAt && !agentUpgradedAt,
     [agentUpgradeStartedAt, agentUpgradedAt]
   );
   const status = useMemo(() => getStatusComponents(agentUpgradeDetails), [agentUpgradeDetails]);
   const minVersion = '8.12';
+
+  if (!isAgentUpgradable && notUpgradeableMessage) {
+    return (
+      <EuiIconTip
+        type="iInCircle"
+        content={
+          <FormattedMessage
+            id="xpack.fleet.agentUpgradeStatusBadge.notUpgradeable"
+            defaultMessage="Agent not upgradeable: {reason}"
+            values={{
+              reason: notUpgradeableMessage,
+            }}
+          />
+        }
+        color="subdued"
+      />
+    );
+  }
 
   if (isAgentUpgradable) {
     return (

x-pack/plugins/fleet/common/services/is_agent_upgradeable.test.ts
@@ -11,6 +11,7 @@ import {
   getRecentUpgradeInfoForAgent,
   isAgentUpgradeable,
   isAgentUpgrading,
+  getNotUpgradeableMessage,
 } from './is_agent_upgradeable';
 
 const getAgent = ({
@@ -238,6 +239,154 @@ describe('Fleet - isAgentUpgradeable', () => {
         '8.0.0'
       )
     ).toBe(true);
+  });
+});
+
+describe('Fleet - getNotUpgradeableMessage', () => {
+  it('if agent reports not upgradeable with agent version < latest agent version', () => {
+    expect(getNotUpgradeableMessage(getAgent({ version: '7.9.0' }), '8.0.0')).toBe(
+      'agent cannot be upgraded through Fleet. It may be running in a container or it is not installed as a service.'
+    );
+  });
+
+  it('if agent reports not upgradeable with agent version > latest agent version', () => {
+    expect(getNotUpgradeableMessage(getAgent({ version: '8.0.0' }), '7.9.0')).toBe(
+      'agent cannot be upgraded through Fleet. It may be running in a container or it is not installed as a service.'
+    );
+  });
+
+  it('returns false if agent reports not upgradeable with agent version === latest agent version', () => {
+    expect(getNotUpgradeableMessage(getAgent({ version: '8.0.0' }), '8.0.0')).toBe(
+      'agent cannot be upgraded through Fleet. It may be running in a container or it is not installed as a service.'
+    );
+  });
+
+  it('if agent reports upgradeable, with agent version === latest agent version', () => {
+    expect(
+      getNotUpgradeableMessage(getAgent({ version: '8.0.0', upgradeable: true }), '8.0.0')
+    ).toBe('agent is already running on the latest available version.');
+  });
+
+  it('if agent reports upgradeable, with agent version > latest agent version', () => {
+    expect(
+      getNotUpgradeableMessage(getAgent({ version: '8.0.0', upgradeable: true }), '7.9.0')
+    ).toBe('agent is running on a version greater than the latest available version.');
+  });
+
+  it('if agent reports upgradeable, but agent is unenrolling', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({ version: '7.9.0', upgradeable: true, unenrolling: true }),
+        '8.0.0'
+      )
+    ).toBe('agent is being unenrolled.');
+  });
+
+  it('if agent reports upgradeable, but agent is unenrolled', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({ version: '7.9.0', upgradeable: true, unenrolled: true }),
+        '8.0.0'
+      )
+    ).toBe('agent has been unenrolled.');
+  });
+
+  it('Returns no error message if agent reports upgradeable, with agent version < latest agent version', () => {
+    expect(
+      getNotUpgradeableMessage(getAgent({ version: '7.9.0', upgradeable: true }), '8.0.0')
+    ).toBeUndefined();
+  });
+
+  it('if agent reports upgradeable, with agent snapshot version === latest agent version', () => {
+    expect(
+      getNotUpgradeableMessage(getAgent({ version: '7.9.0-SNAPSHOT', upgradeable: true }), '7.9.0')
+    ).toBe('agent is already running on the latest available version.');
+  });
+
+  it('it does not return message if agent reports upgradeable, with upgrade to agent snapshot version newer than latest agent version', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({ version: '8.10.2', upgradeable: true }),
+        '8.10.2',
+        '8.11.0-SNAPSHOT'
+      )
+    ).toBeUndefined();
+  });
+
+  it('if agent reports upgradeable, with target version < current agent version ', () => {
+    expect(
+      getNotUpgradeableMessage(getAgent({ version: '7.9.0', upgradeable: true }), '8.0.0', '7.8.0')
+    ).toBe('agent does not support downgrades.');
+  });
+
+  it('if agent reports upgradeable, with target version == current agent version ', () => {
+    expect(
+      getNotUpgradeableMessage(getAgent({ version: '7.9.0', upgradeable: true }), '8.0.0', '7.9.0')
+    ).toBe('agent is already running on the selected version.');
+  });
+
+  it('if agent with no upgrade details reports upgradeable, but is already upgrading', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({ version: '7.9.0', upgradeable: true, upgrading: true }),
+        '8.0.0'
+      )
+    ).toBe('agent is already being upgraded.');
+  });
+
+  it('if agent reports upgradeable, but has an upgrade status other than failed', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({
+          version: '7.9.0',
+          upgradeable: true,
+          upgradeDetails: {
+            target_version: '8.0.0',
+            action_id: 'XXX',
+            state: 'UPG_REQUESTED',
+          },
+        }),
+        '8.0.0'
+      )
+    ).toBe('agent is already being upgraded.');
+  });
+
+  it('it does not return a message if agent reports upgradeable and has a failed upgrade status', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({
+          version: '7.9.0',
+          upgradeable: true,
+          upgradeDetails: {
+            target_version: '8.0.0',
+            action_id: 'XXX',
+            state: 'UPG_FAILED',
+            metadata: {
+              error_msg: 'Upgrade timed out',
+            },
+          },
+        }),
+        '8.0.0'
+      )
+    ).toBeUndefined();
+  });
+
+  it('if the agent reports upgradeable but was upgraded less than 10 minutes ago', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({ version: '7.9.0', upgradeable: true, minutesSinceUpgrade: 9 }),
+        '8.0.0'
+      )
+    ).toContain('please wait');
+  });
+
+  it('if agent reports upgradeable and was upgraded more than 10 minutes ago', () => {
+    expect(
+      getNotUpgradeableMessage(
+        getAgent({ version: '7.9.0', upgradeable: true, minutesSinceUpgrade: 11 }),
+        '8.0.0'
+      )
+    ).toBeUndefined();
   });
 });
 

x-pack/plugins/fleet/common/services/is_agent_upgradeable.ts
@@ -8,10 +8,26 @@
 import semverCoerce from 'semver/functions/coerce';
 import semverLt from 'semver/functions/lt';
 import semverGt from 'semver/functions/gt';
+import semverEq from 'semver/functions/eq';
+import moment from 'moment';
 
 import type { Agent } from '../types';
 
 export const AGENT_UPGRADE_COOLDOWN_IN_MIN = 10;
+
+// Error messages for agent not upgradeable
+export const VERSION_MISSING_ERROR = `agent version is missing.`;
+export const UNENROLLED_ERROR = `agent has been unenrolled.`;
+export const ONGOING_UNEROLLMENT_ERROR = `agent is being unenrolled.`;
+export const NOT_UPGRADEABLE_ERROR = `agent cannot be upgraded through Fleet. It may be running in a container or it is not installed as a service.`;
+export const ALREADY_UPGRADED_ERROR = `agent is already being upgraded.`;
+export const INVALID_VERSION_ERROR = 'agent version is not valid.';
+export const SELECTED_VERSION_ERROR = 'the selected version is not valid.';
+export const RUNNING_SELECTED_VERSION_ERROR = `agent is already running on the selected version.`;
+export const DOWNGRADE_NOT_ALLOWED_ERROR = `agent does not support downgrades.`;
+export const LATEST_VERSION_NOT_VALID_ERROR = 'latest version is not valid.';
+export const AGENT_ALREADY_ON_LATEST_ERROR = `agent is already running on the latest available version.`;
+export const AGENT_ON_GREATER_VERSION_ERROR = `agent is running on a version greater than the latest available version.`;
 
 export function isAgentUpgradeable(
   agent: Agent,
@@ -42,21 +58,76 @@ export function isAgentUpgradeable(
   return isAgentVersionLessThanLatest(agentVersion, latestAgentVersion);
 }
 
+// Based on the previous, returns a detailed message explaining why the agent is not upgradeable
+export const getNotUpgradeableMessage = (
+  agent: Agent,
+  latestAgentVersion?: string,
+  versionToUpgrade?: string
+) => {
+  let agentVersion: string;
+  if (typeof agent?.local_metadata?.elastic?.agent?.version === 'string') {
+    agentVersion = agent.local_metadata.elastic.agent.version;
+  } else {
+    return VERSION_MISSING_ERROR;
+  }
+  if (agent.unenrolled_at) {
+    return UNENROLLED_ERROR;
+  }
+  if (agent.unenrollment_started_at) {
+    return ONGOING_UNEROLLMENT_ERROR;
+  }
+  if (!agent.local_metadata.elastic.agent.upgradeable) {
+    return NOT_UPGRADEABLE_ERROR;
+  }
+  if (isAgentUpgrading(agent)) {
+    return ALREADY_UPGRADED_ERROR;
+  }
+  if (getRecentUpgradeInfoForAgent(agent).hasBeenUpgradedRecently) {
+    const timeToWaitMins = getRecentUpgradeInfoForAgent(agent).timeToWaitMins;
+    const elapsedMinsSinceUpgrade = getRecentUpgradeInfoForAgent(agent).elapsedMinsSinceUpgrade;
+    return `agent was upgraded ${elapsedMinsSinceUpgrade} minutes ago, please wait ${timeToWaitMins} minutes before attempting the upgrade again.`;
+  }
+  const agentVersionNumber = semverCoerce(agentVersion);
+  if (!agentVersionNumber) return INVALID_VERSION_ERROR;
+
+  if (versionToUpgrade !== undefined) {
+    const versionToUpgradeNumber = semverCoerce(versionToUpgrade);
+    if (!versionToUpgradeNumber) return SELECTED_VERSION_ERROR;
+
+    if (semverEq(agentVersionNumber, versionToUpgradeNumber)) return RUNNING_SELECTED_VERSION_ERROR;
+
+    if (semverLt(versionToUpgradeNumber, agentVersionNumber)) return DOWNGRADE_NOT_ALLOWED_ERROR;
+
+    // explicitly allow this case - the agent is upgradeable
+    if (semverGt(versionToUpgradeNumber, agentVersionNumber)) return undefined;
+  }
+
+  const latestAgentVersionNumber = semverCoerce(latestAgentVersion);
+  if (!latestAgentVersionNumber) return LATEST_VERSION_NOT_VALID_ERROR;
+
+  if (semverEq(agentVersionNumber, latestAgentVersionNumber)) return AGENT_ALREADY_ON_LATEST_ERROR;
+
+  if (semverGt(agentVersionNumber, latestAgentVersionNumber)) return AGENT_ON_GREATER_VERSION_ERROR;
+
+  // in all the other cases, the agent is upgradeable; don't return any message.
+  return undefined;
+};
+
 const isAgentVersionLessThanLatest = (agentVersion: string, latestAgentVersion: string) => {
   // make sure versions are only the number before comparison
   const agentVersionNumber = semverCoerce(agentVersion);
-  if (!agentVersionNumber) throw new Error('agent version is not valid');
+  if (!agentVersionNumber) throw new Error(`${INVALID_VERSION_ERROR}`);
   const latestAgentVersionNumber = semverCoerce(latestAgentVersion);
-  if (!latestAgentVersionNumber) throw new Error('latest version is not valid');
+  if (!latestAgentVersionNumber) throw new Error(`${LATEST_VERSION_NOT_VALID_ERROR}`);
 
   return semverLt(agentVersionNumber, latestAgentVersionNumber);
 };
 
 const isNotDowngrade = (agentVersion: string, versionToUpgrade: string) => {
   const agentVersionNumber = semverCoerce(agentVersion);
-  if (!agentVersionNumber) throw new Error('agent version is not valid');
+  if (!agentVersionNumber) throw new Error(`${INVALID_VERSION_ERROR}`);
   const versionToUpgradeNumber = semverCoerce(versionToUpgrade);
-  if (!versionToUpgradeNumber) throw new Error('target version is not valid');
+  if (!versionToUpgradeNumber) throw new Error(`${SELECTED_VERSION_ERROR}`);
 
   return semverGt(versionToUpgradeNumber, agentVersionNumber);
 };
@@ -64,19 +135,27 @@ const isNotDowngrade = (agentVersion: string, versionToUpgrade: string) => {
 export function getRecentUpgradeInfoForAgent(agent: Agent): {
   hasBeenUpgradedRecently: boolean;
   timeToWaitMs: number;
+  elapsedMinsSinceUpgrade: number;
+  timeToWaitMins: number;
 } {
   if (!agent.upgraded_at) {
     return {
       hasBeenUpgradedRecently: false,
       timeToWaitMs: 0,
+      timeToWaitMins: 0,
+      elapsedMinsSinceUpgrade: 0,
     };
   }
 
-  const elaspedSinceUpgradeInMillis = Date.now() - Date.parse(agent.upgraded_at);
-  const timeToWaitMs = AGENT_UPGRADE_COOLDOWN_IN_MIN * 6e4 - elaspedSinceUpgradeInMillis;
-  const hasBeenUpgradedRecently = elaspedSinceUpgradeInMillis / 6e4 < AGENT_UPGRADE_COOLDOWN_IN_MIN;
+  const elapsedSinceUpgradeInMillis = Date.now() - Date.parse(agent.upgraded_at);
+  const elapsedMins = moment.duration(elapsedSinceUpgradeInMillis, 'milliseconds').asMinutes();
+  const elapsedMinsSinceUpgrade = Math.ceil(elapsedMins);
 
-  return { hasBeenUpgradedRecently, timeToWaitMs };
+  const timeToWaitMs = AGENT_UPGRADE_COOLDOWN_IN_MIN * 6e4 - elapsedSinceUpgradeInMillis;
+  const hasBeenUpgradedRecently = elapsedSinceUpgradeInMillis / 6e4 < AGENT_UPGRADE_COOLDOWN_IN_MIN;
+  const timeToWait = moment.duration(timeToWaitMs, 'milliseconds').asMinutes();
+  const timeToWaitMins = Math.ceil(timeToWait);
+  return { hasBeenUpgradedRecently, timeToWaitMs, elapsedMinsSinceUpgrade, timeToWaitMins };
 }
 
 export function isAgentUpgrading(agent: Agent) {

x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/components/agent_list_table.tsx
@@ -31,6 +31,8 @@ import { Tags } from '../../components/tags';
 import type { AgentMetrics } from '../../../../../../../common/types';
 import { formatAgentCPU, formatAgentMemory } from '../../services/agent_metrics';
 
+import { getNotUpgradeableMessage } from '../../../../../../../common/services/is_agent_upgradeable';
+
 import { AgentUpgradeStatus } from './agent_upgrade_status';
 
 import { EmptyPrompt } from './empty_prompt';
@@ -303,6 +305,7 @@ export const AgentListTable: React.FC<Props> = (props: Props) => {
                   agentUpgradeStartedAt={agent.upgrade_started_at}
                   agentUpgradedAt={agent.upgraded_at}
                   agentUpgradeDetails={agent.upgrade_details}
+                  notUpgradeableMessage={getNotUpgradeableMessage(agent, latestAgentVersion)}
                 />
               </EuiFlexItem>
             </EuiFlexGroup>

x-pack/plugins/fleet/public/applications/fleet/sections/agents/components/agent_upgrade_modal/index.test.tsx
@@ -29,6 +29,7 @@ jest.mock('../../../../hooks', () => {
     }),
     sendPostBulkAgentUpgrade: jest.fn(),
     useAgentVersion: jest.fn().mockReturnValue('8.10.2'),
+    useKibanaVersion: jest.fn().mockReturnValue('8.10.2'),
   };
 });
 
@@ -199,6 +200,30 @@ describe('AgentUpgradeAgentModal', () => {
         isUpdating: true,
       });
 
+      const el = utils.getByTestId('confirmModalConfirmButton');
+      expect(el).toBeDisabled();
+    });
+  });
+
+  it('should disable submit button and display a warning for a single agent that is not upgradeable', async () => {
+    const { utils } = renderAgentUpgradeAgentModal({
+      agents: [
+        {
+          status: 'offline',
+          upgrade_started_at: '2022-11-21T12:27:24Z',
+          id: 'agent1',
+          local_metadata: { elastic: { agent: { version: '8.9.0' } } },
+        },
+      ] as any,
+      agentCount: 2,
+    });
+    await waitFor(() => {
+      expect(utils.queryByText(/The selected agent is not upgradeable/)).toBeInTheDocument();
+      expect(
+        utils.queryByText(
+          /Reason: agent cannot be upgraded through Fleet. It may be running in a container or it is not installed as a service./
+        )
+      ).toBeInTheDocument();
       const el = utils.getByTestId('confirmModalConfirmButton');
       expect(el).toBeDisabled();
     });

commit 1de5cde975299b1fb9026d96c2e1c90ab2b037e9

[Fleet] Show warning when download upgrade is failing (#173844)
Fixes https://github.com/elastic/kibana/issues/173370
Closes https://github.com/elastic/kibana/issues/171941

## Summary
Show a warning when download upgrade is failing.

This PR addresses a specific case of `upgrade downloading`: when the
upgrade started but it's failing with an error. In this case, since
8.12, the agent metadata have a `retry_msg` that can be used to
distinguish this case from the regular upgrade.
I'm also fixing one smaller bug that I introduced with
https://github.com/elastic/kibana/pull/173253, the tooltip shown in the
case when the agent is not upgradeable was hiding the badge, so I'm
moving the if as the last one in the function.

## Testing
- Have an 8.12 agent installed with Multipass (it needs to be
upgradeable)
- Change the download binary url to something broken: `sourceURI:
https://artifacts.elastic.co/notdownloads/`
- Force upgrade from dev tools:
```
POST kbn:/api/fleet/agents/c3f09103-4e69-4a36-bee2-84223bedef36/upgrade
{
  "version": "8.12.0",
  "force": true
}
```
- Go to agent overview, the badge will show "upgrading" but will also
have a warning icon with a tooltip, showing the retry message and the
`retry_until` time in humanized format (retry until...remaining):
![Screenshot 2024-01-02 at 16 54
20](https://github.com/elastic/kibana/assets/16084106/ac340e7d-5151-4e4e-b6a6-731a113ff984)

This way the message shows the values present in the agent metadata.

- Same is reported in the agent list table:
![Screenshot 2024-01-02 at 16 54
28](https://github.com/elastic/kibana/assets/16084106/1f8823c4-9e5c-4f4c-9cdc-2839b16214bb)


- After a while the retries will be finished and the agent will show the
regular "upgrade failed" badge (this was already implemented):
![Screenshot 2023-12-21 at 12 07
27](https://github.com/elastic/kibana/assets/16084106/c1d5bf67-a4fd-4b04-aa8d-24dc8d4af54e)



### Checklist

x-pack/plugins/fleet/common/types/models/agent.ts
@@ -449,5 +449,7 @@ export interface AgentUpgradeDetails {
     download_rate?: number; // bytes per second
     failed_state?: AgentUpgradeStateType;
     error_msg?: string;
+    retry_error_msg?: string;
+    retry_until?: string;
   };
 }

x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_details_page/components/agent_details/agent_details_overview.tsx
@@ -23,7 +23,11 @@ import { FormattedMessage, FormattedRelative } from '@kbn/i18n-react';
 
 import type { Agent, AgentPolicy } from '../../../../../types';
 import { useAgentVersion } from '../../../../../hooks';
-import { ExperimentalFeaturesService, isAgentUpgradeable } from '../../../../../services';
+import {
+  ExperimentalFeaturesService,
+  isAgentUpgradeable,
+  getNotUpgradeableMessage,
+} from '../../../../../services';
 import { AgentPolicySummaryLine } from '../../../../../components';
 import { AgentHealth } from '../../../components';
 import { Tags } from '../../../components/tags';
@@ -186,6 +190,7 @@ export const AgentDetailsOverviewSection: React.FunctionComponent<{
                         agentUpgradeStartedAt={agent.upgrade_started_at}
                         agentUpgradedAt={agent.upgraded_at}
                         agentUpgradeDetails={agent.upgrade_details}
+                        notUpgradeableMessage={getNotUpgradeableMessage(agent, latestAgentVersion)}
                       />
                     </EuiFlexItem>
                   </EuiFlexGroup>

x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/components/agent_upgrade_status.tsx
@@ -8,6 +8,7 @@
 import React, { useMemo } from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { EuiBadge, EuiFlexGroup, EuiFlexItem, EuiIconTip } from '@elastic/eui';
+import moment from 'moment';
 
 import type { AgentUpgradeDetails } from '../../../../../../../common/types';
 
@@ -62,6 +63,16 @@ const formatRate = (downloadRate: number) => {
   }
   return downloadRate.toFixed(1) + byteUnits[i];
 };
+const formatRetryUntil = (retryUntil: string | undefined) => {
+  if (!retryUntil) return '';
+  const eta = new Date(retryUntil).toISOString();
+  const remainingTime = Date.parse(retryUntil) - Date.now();
+  const duration = moment
+    .utc(moment.duration(remainingTime, 'milliseconds').asMilliseconds())
+    .format('HH:mm');
+
+  return remainingTime > 0 ? `Retrying until: ${eta} (${duration} remaining)` : '';
+};
 
 function getStatusComponents(agentUpgradeDetails?: AgentUpgradeDetails) {
   switch (agentUpgradeDetails?.state) {
@@ -103,6 +114,28 @@ function getStatusComponents(agentUpgradeDetails?: AgentUpgradeDetails) {
         ),
       };
     case 'UPG_DOWNLOADING':
+      if (agentUpgradeDetails?.metadata?.retry_error_msg) {
+        return {
+          Badge: (
+            <EuiBadge color="accent" iconType="download">
+              <FormattedMessage
+                id="xpack.fleet.agentUpgradeStatusBadge.upgradeDownloading"
+                defaultMessage="Upgrade downloading"
+              />
+            </EuiBadge>
+          ),
+          WarningTooltipText: (
+            <FormattedMessage
+              id="xpack.fleet.agentUpgradeStatusTooltip.upgradeDownloadingFailed"
+              defaultMessage="Upgrade failing: {retryMsg}. {retryUntil}"
+              values={{
+                retryMsg: agentUpgradeDetails?.metadata?.retry_error_msg,
+                retryUntil: formatRetryUntil(agentUpgradeDetails?.metadata?.retry_until),
+              }}
+            />
+          ),
+        };
+      }
       return {
         Badge: (
           <EuiBadge color="accent" iconType="download">
@@ -252,24 +285,6 @@ export const AgentUpgradeStatus: React.FC<{
   const status = useMemo(() => getStatusComponents(agentUpgradeDetails), [agentUpgradeDetails]);
   const minVersion = '8.12';
 
-  if (!isAgentUpgradable && notUpgradeableMessage) {
-    return (
-      <EuiIconTip
-        type="iInCircle"
-        content={
-          <FormattedMessage
-            id="xpack.fleet.agentUpgradeStatusBadge.notUpgradeable"
-            defaultMessage="Agent not upgradeable: {reason}"
-            values={{
-              reason: notUpgradeableMessage,
-            }}
-          />
-        }
-        color="subdued"
-      />
-    );
-  }
-
   if (isAgentUpgradable) {
     return (
       <EuiBadge color="hollow" iconType="sortUp">
@@ -285,9 +300,16 @@ export const AgentUpgradeStatus: React.FC<{
     return (
       <EuiFlexGroup gutterSize="s" alignItems="center" responsive={false}>
         <EuiFlexItem grow={false}>{status.Badge}</EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiIconTip type="iInCircle" content={status.TooltipText} color="subdued" />
-        </EuiFlexItem>
+        {status.TooltipText && (
+          <EuiFlexItem grow={false}>
+            <EuiIconTip type="iInCircle" content={status.TooltipText} color="subdued" />
+          </EuiFlexItem>
+        )}
+        {status.WarningTooltipText && (
+          <EuiFlexItem grow={false}>
+            <EuiIconTip type="warning" content={status.WarningTooltipText} color="warning" />
+          </EuiFlexItem>
+        )}
       </EuiFlexGroup>
     );
   }
@@ -302,6 +324,24 @@ export const AgentUpgradeStatus: React.FC<{
             defaultMessage="Detailed upgrade status is available for Elastic Agents on version {minVersion} and higher."
             values={{
               minVersion,
+            }}
+          />
+        }
+        color="subdued"
+      />
+    );
+  }
+
+  if (!isAgentUpgradable && notUpgradeableMessage) {
+    return (
+      <EuiIconTip
+        type="iInCircle"
+        content={
+          <FormattedMessage
+            id="xpack.fleet.agentUpgradeStatusBadge.notUpgradeable"
+            defaultMessage="Agent not upgradeable: {reason}"
+            values={{
+              reason: notUpgradeableMessage,
             }}
           />
         }

x-pack/plugins/fleet/public/services/index.ts
@@ -34,6 +34,7 @@ export {
   isValidNamespace,
   LicenseService,
   isAgentUpgradeable,
+  getNotUpgradeableMessage,
   doesPackageHaveIntegrations,
   validatePackagePolicy,
   validatePackagePolicyConfig,

x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/components/agent_upgrade_status.test.tsx
@@ -7,6 +7,7 @@
 
 import { fireEvent, waitFor } from '@testing-library/react';
 import React from 'react';
+import moment from 'moment';
 
 import { createFleetTestRendererMock } from '../../../../../../mock';
 
@@ -174,6 +175,69 @@ describe('AgentUpgradeStatus', () => {
 
       expectUpgradeStatusBadgeLabel(results, 'Upgrade downloading');
       await expectTooltip(results, 'Downloading the new agent artifact version (16.4%).');
+    });
+
+    it('should render UPG_DOWNLOADING with a warning if agent has a retry_message and retry_until', async () => {
+      const results = render({
+        agentUpgradeDetails: {
+          target_version: 'XXX',
+          action_id: 'xxx',
+          state: 'UPG_DOWNLOADING',
+          metadata: {
+            download_percent: 16.4,
+            retry_error_msg: 'unable to download',
+            retry_until: `${moment().add(10, 'minutes').toISOString()}`,
+          },
+        },
+      });
+
+      expectUpgradeStatusBadgeLabel(results, 'Upgrade downloading');
+      fireEvent.mouseOver(results.getByText('Info'));
+      await waitFor(() => {
+        const tooltip = results.getByRole('tooltip');
+        expect(tooltip).toHaveTextContent('Upgrade failing: unable to download. Retrying until:');
+        expect(tooltip).toHaveTextContent('(00:09 remaining)');
+      });
+    });
+
+    it('should not render retry_until if the remaining time is negative', async () => {
+      const results = render({
+        agentUpgradeDetails: {
+          target_version: 'XXX',
+          action_id: 'xxx',
+          state: 'UPG_DOWNLOADING',
+          metadata: {
+            download_percent: 16.4,
+            retry_error_msg: 'unable to download',
+            retry_until: `${moment().subtract(10, 'minutes').toISOString()}`,
+          },
+        },
+      });
+
+      expectUpgradeStatusBadgeLabel(results, 'Upgrade downloading');
+      fireEvent.mouseOver(results.getByText('Info'));
+      await waitFor(() => {
+        const tooltip = results.getByRole('tooltip');
+        expect(tooltip).toHaveTextContent('Upgrade failing: unable to download.');
+        expect(tooltip).not.toHaveTextContent('remaining');
+      });
+    });
+
+    it('should render UPG_DOWNLOADING with a warning if agent has a retry_message', async () => {
+      const results = render({
+        agentUpgradeDetails: {
+          target_version: 'XXX',
+          action_id: 'xxx',
+          state: 'UPG_DOWNLOADING',
+          metadata: {
+            download_percent: 16.4,
+            retry_error_msg: 'unable to download',
+          },
+        },
+      });
+
+      expectUpgradeStatusBadgeLabel(results, 'Upgrade downloading');
+      await expectTooltip(results, 'Upgrade failing: unable to download.');
     });
 
     it('should render UPG_EXTRACTING state correctly', async () => {