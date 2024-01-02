e834544857dc0d8ca6d80b83afd73047229b8d66
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-01-02 16:06:14 +0200

    Code improvements

--- x-pack/examples/lens_embeddable_inline_editing_example/public/app.tsx
+++ x-pack/examples/lens_embeddable_inline_editing_example/public/app.tsx
@@ -5,26 +5,22 @@
  * 2.0.
  */
 
-import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import {
   EuiFlexGroup,
   EuiFlexItem,
-  EuiSpacer,
   EuiPage,
   EuiPageBody,
   EuiPageHeader,
   EuiPageSection,
-  EuiPanel,
-  EuiButtonIcon,
 } from '@elastic/eui';
 import type { CoreStart } from '@kbn/core/public';
-import type { LensChartLoadEvent } from '@kbn/visualization-utils';
 import { LensConfigBuilder } from '@kbn/lens-embeddable-utils/config_builder/config_builder';
 import type { DataView } from '@kbn/data-views-plugin/public';
-import { LensConfig, LensConfigOptions } from '@kbn/lens-embeddable-utils/config_builder/types';
-import type { TypedLensByValueInput, LensPublicStart } from '@kbn/lens-plugin/public';
+import type { LensPublicStart } from '@kbn/lens-plugin/public';
 import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
 import type { StartDependencies } from './plugin';
+import { DataViewChart } from './dataview_embeddable';
 
 export const App = (props: {
   core: CoreStart;
@@ -32,92 +28,10 @@ export const App = (props: {
   defaultDataView: DataView;
   stateHelpers: Awaited<ReturnType<LensPublicStart['stateHelperApi']>>;
 }) => {
-  const ref = useRef(false);
-  const [embeddableInput, setEmbeddableInput] = useState<TypedLensByValueInput | undefined>(
-    undefined
-  );
-  const [lensLoadEvent, setLensLoadEvent] = useState<LensChartLoadEvent | null>(null);
   const configBuilder = useMemo(
     () => new LensConfigBuilder(props.stateHelpers.formula, props.plugins.dataViews),
     [props.plugins.dataViews, props.stateHelpers.formula]
   );
-  const config = useMemo(() => {
-    return {
-      chartType: 'metric',
-      title: 'metric chart',
-      layers: [
-        {
-          label: 'metric layer',
-          dataset: {
-            esql: 'from kibana_sample_data_logs | stats count=count(bytes)',
-          },
-          value: 'count',
-        },
-      ],
-    } as unknown as LensConfig;
-  }, []);
-
-  const options = useMemo(() => {
-    return {
-      embeddable: true,
-      timeRange: {
-        from: 'now-30d',
-        to: 'now',
-        type: 'relative',
-      },
-      query: {
-        esql: 'from kibana_sample_data_logs | stats count=count(bytes)',
-      },
-    } as unknown as LensConfigOptions;
-  }, []);
-  useEffect(() => {
-    ref.current = true;
-    configBuilder.build(config, options).then((input) => {
-      if (ref.current) {
-        setEmbeddableInput(input as TypedLensByValueInput);
-      }
-    });
-    return () => {
-      ref.current = false;
-    };
-  }, [config, configBuilder, options, props.plugins.dataViews, props.stateHelpers.formula]);
-
-  // get the Lens load event from the embeddable
-  const onLoad = useCallback(
-    (
-      isLoading: boolean,
-      adapters: LensChartLoadEvent['adapters'] | undefined,
-      lensEmbeddableOutput$?: LensChartLoadEvent['embeddableOutput$']
-    ) => {
-      const adapterTables = adapters?.tables?.tables;
-      if (adapterTables && !isLoading) {
-        setLensLoadEvent({
-          adapters,
-          embeddableOutput$: lensEmbeddableOutput$,
-        });
-      }
-    },
-    []
-  );
-  const LensComponent = props.plugins.lens.EmbeddableComponent;
-
-  // type InlineEditLensEmbeddableContext
-  const triggerOptions = {
-    attributes: embeddableInput?.attributes,
-    lensEvent: lensLoadEvent,
-    onUpdate: (newAttributes: TypedLensByValueInput['attributes']) => {
-      if (embeddableInput) {
-        const newInput = {
-          ...embeddableInput,
-          attributes: newAttributes,
-        };
-        setEmbeddableInput(newInput);
-      }
-    },
-    onApply: () => {
-      alert('optional onApply callback!');
-    },
-  };
 
   return (
     <KibanaContextProvider
@@ -142,32 +56,19 @@ export const App = (props: {
               responsive={false}
             >
               <EuiFlexItem className="eui-fullHeight">
-                <EuiPanel hasShadow={false}>
-                  <p>Inline editing of an ES|QL chart.</p>
-                  <EuiSpacer />
-                  <EuiFlexGroup>
-                    <EuiFlexItem>
-                      {embeddableInput && (
-                        <LensComponent
-                          style={{ height: 500 }}
-                          {...embeddableInput}
-                          onLoad={onLoad}
-                        />
-                      )}
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={false}>
-                      <EuiButtonIcon
-                        size="xs"
-                        iconType="pencil"
-                        onClick={() => {
-                          props.plugins.uiActions
-                            .getTrigger('IN_APP_EMBEDDABLE_EDIT_TRIGGER')
-                            .exec(triggerOptions);
-                        }}
-                      />
-                    </EuiFlexItem>
-                  </EuiFlexGroup>
-                </EuiPanel>
+                <DataViewChart
+                  configBuilder={configBuilder}
+                  plugins={props.plugins}
+                  defaultDataView={props.defaultDataView}
+                  isESQL
+                />
+              </EuiFlexItem>
+              <EuiFlexItem className="eui-fullHeight">
+                <DataViewChart
+                  configBuilder={configBuilder}
+                  plugins={props.plugins}
+                  defaultDataView={props.defaultDataView}
+                />
               </EuiFlexItem>
             </EuiFlexGroup>
           </EuiPageSection>
---
+++ x-pack/examples/lens_embeddable_inline_editing_example/public/dataview_embeddable.tsx
@@ -0,0 +1,105 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
+import type { TypedLensByValueInput } from '@kbn/lens-plugin/public';
+import type { DataView } from '@kbn/data-views-plugin/public';
+import { EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiPanel, EuiButtonIcon } from '@elastic/eui';
+import type { LensChartLoadEvent } from '@kbn/visualization-utils';
+import { LensConfigBuilder } from '@kbn/lens-embeddable-utils/config_builder/config_builder';
+import type { StartDependencies } from './plugin';
+import { getConfigOptions } from './utils';
+
+export const DataViewChart = (props: {
+  configBuilder: LensConfigBuilder;
+  plugins: StartDependencies;
+  defaultDataView: DataView;
+  isESQL?: boolean;
+}) => {
+  const ref = useRef(false);
+  const [embeddableInput, setEmbeddableInput] = useState<TypedLensByValueInput | undefined>(
+    undefined
+  );
+  const [lensLoadEvent, setLensLoadEvent] = useState<LensChartLoadEvent | null>(null);
+
+  const { config, options } = useMemo(() => {
+    return getConfigOptions(props.defaultDataView, props.isESQL);
+  }, [props.defaultDataView, props.isESQL]);
+
+  useEffect(() => {
+    ref.current = true;
+    props.configBuilder.build(config, options).then((input) => {
+      if (ref.current) {
+        setEmbeddableInput(input as TypedLensByValueInput);
+      }
+    });
+    return () => {
+      ref.current = false;
+    };
+  }, [config, props.configBuilder, options, props.plugins.dataViews]);
+
+  const onLoad = useCallback(
+    (
+      isLoading: boolean,
+      adapters: LensChartLoadEvent['adapters'] | undefined,
+      lensEmbeddableOutput$?: LensChartLoadEvent['embeddableOutput$']
+    ) => {
+      const adapterTables = adapters?.tables?.tables;
+      if (adapterTables && !isLoading) {
+        setLensLoadEvent({
+          adapters,
+          embeddableOutput$: lensEmbeddableOutput$,
+        });
+      }
+    },
+    []
+  );
+
+  // type InlineEditLensEmbeddableContext
+  const triggerOptions = {
+    attributes: embeddableInput?.attributes,
+    lensEvent: lensLoadEvent,
+    onUpdate: (newAttributes: TypedLensByValueInput['attributes']) => {
+      if (embeddableInput) {
+        const newInput = {
+          ...embeddableInput,
+          attributes: newAttributes,
+        };
+        setEmbeddableInput(newInput);
+      }
+    },
+    onApply: () => {
+      alert('optional onApply callback!');
+    },
+  };
+  const LensComponent = props.plugins.lens.EmbeddableComponent;
+
+  return (
+    <EuiPanel hasShadow={false}>
+      <p>Inline editing of a dataview chart.</p>
+      <EuiSpacer />
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          {embeddableInput && (
+            <LensComponent style={{ height: 500 }} {...embeddableInput} onLoad={onLoad} />
+          )}
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiButtonIcon
+            size="xs"
+            iconType="pencil"
+            onClick={() => {
+              props.plugins.uiActions
+                .getTrigger('IN_APP_EMBEDDABLE_EDIT_TRIGGER')
+                .exec(triggerOptions);
+            }}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiPanel>
+  );
+};
