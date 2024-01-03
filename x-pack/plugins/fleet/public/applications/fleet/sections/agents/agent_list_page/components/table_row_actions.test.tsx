374d3e3eba17a29d3aa8114d3d7696ae5242ef0b
Julia Bardi <julia.bardi@elastic.co>
2024-01-03 14:54:37 +0100

    fix test

--- x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/components/table_row_actions.test.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/components/table_row_actions.test.tsx
@@ -216,7 +216,7 @@ describe('TableRowActions', () => {
       expect(res).toBeEnabled();
     });
 
-    it('should render a disabled action button if agent version is latest', async () => {
+    it('should render an enabled action button if agent version is latest', async () => {
       const res = renderAndGetUpgradeButton({
         agent: {
           active: true,
@@ -229,7 +229,7 @@ describe('TableRowActions', () => {
       });
 
       expect(res).not.toBe(null);
-      expect(res).not.toBeEnabled();
+      expect(res).toBeEnabled();
     });
   });
 });
