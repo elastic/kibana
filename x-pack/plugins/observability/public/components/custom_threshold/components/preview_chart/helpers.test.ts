7ef9a074e991771d6b98f09722a01d8c49f00404
Faisal Kanout <f.kanout@gmail.com>
2024-01-03 13:39:26 +0100

    codereview use testeach

--- x-pack/plugins/observability/public/components/custom_threshold/components/preview_chart/helpers.test.ts
+++ x-pack/plugins/observability/public/components/custom_threshold/components/preview_chart/helpers.test.ts
@@ -9,118 +9,101 @@ import {
   CustomThresholdExpressionMetric,
 } from '../../../../../common/custom_threshold_rule/types';
 import { getLensOperationFromRuleMetric } from './helpers';
-
-describe('getLensOperationFromRuleMetric', () => {
-  it('returns the correct operation for SUM agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+const useCases = [
+  [
+    {
       aggType: Aggregators.SUM,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual('sum(system.cpu.user.pct)');
-  });
-  it('returns the correct operation for MAX agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'sum(system.cpu.user.pct)',
+  ],
+  [
+    {
       aggType: Aggregators.MAX,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual('max(system.cpu.user.pct)');
-  });
-  it('returns the correct operation for MIN agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'max(system.cpu.user.pct)',
+  ],
+  [
+    {
       aggType: Aggregators.MIN,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual('min(system.cpu.user.pct)');
-  });
-  it('returns the correct operation for AVERAGE agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'min(system.cpu.user.pct)',
+  ],
+  [
+    {
       aggType: Aggregators.AVERAGE,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual('average(system.cpu.user.pct)');
-  });
-  it('returns the correct operation for COUNT agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'average(system.cpu.user.pct)',
+  ],
+  [
+    {
       aggType: Aggregators.COUNT,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual('count(___records___)');
-  });
-
-  it('returns the correct operation for COUNT agg with KQL filter', () => {
-    const metric: CustomThresholdExpressionMetric = {
-      aggType: Aggregators.COUNT,
-      filter: 'host.name : "foo"',
-      name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual(
-      `count(___records___, kql='host.name : foo')`
-    );
-  });
-  it('returns the correct operation for CARDINALITY agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'count(___records___)',
+  ],
+  [
+    {
       aggType: Aggregators.CARDINALITY,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual('unique_count(system.cpu.user.pct)');
-  });
-  it('returns the correct operation for P95 agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'unique_count(system.cpu.user.pct)',
+  ],
+  [
+    {
       aggType: Aggregators.P95,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual(
-      'percentile(system.cpu.user.pct, percentile=95)'
-    );
-  });
-  it('returns the correct operation for P95 agg with KQL filter', () => {
-    const metric: CustomThresholdExpressionMetric = {
-      aggType: Aggregators.P95,
-      field: 'system.cpu.user.pct',
-      filter: 'host.name : "foo"',
-      name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual(
-      `percentile(system.cpu.user.pct, percentile=95, kql='host.name : foo')`
-    );
-  });
-
-  it('returns the correct operation for P99 agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'percentile(system.cpu.user.pct, percentile=95)',
+  ],
+  [
+    {
       aggType: Aggregators.P99,
       field: 'system.cpu.user.pct',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual(
-      'percentile(system.cpu.user.pct, percentile=99)'
-    );
-  });
-  it('returns the correct operation for RATE agg', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    'percentile(system.cpu.user.pct, percentile=99)',
+  ],
+  [
+    {
       aggType: Aggregators.RATE,
       field: 'system.network.in.bytes',
+      filter: '',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual(
-      `counter_rate(max(system.network.in.bytes), kql='')`
-    );
-  });
-  it('returns the correct operation for RATE agg with filter', () => {
-    const metric: CustomThresholdExpressionMetric = {
+    },
+    `counter_rate(max(system.network.in.bytes), kql='')`,
+  ],
+  [
+    {
       aggType: Aggregators.RATE,
       field: 'system.network.in.bytes',
       filter: 'host.name : "foo"',
       name: '',
-    };
-    expect(getLensOperationFromRuleMetric(metric)).toEqual(
-      `counter_rate(max(system.network.in.bytes), kql='host.name : foo')`
-    );
-  });
+    },
+    `counter_rate(max(system.network.in.bytes), kql='host.name : foo')`,
+  ],
+];
+
+test.each(useCases)('returns the correct operation from %p. =>  %p', (metric, expectedValue) => {
+  return expect(getLensOperationFromRuleMetric(metric as CustomThresholdExpressionMetric)).toEqual(
+    expectedValue
+  );
 });
