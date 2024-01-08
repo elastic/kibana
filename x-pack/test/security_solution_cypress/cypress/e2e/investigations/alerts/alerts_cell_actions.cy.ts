645fdcdb3d64d4fcd1e3554221e90d962ce7d86c
lgestc <lukasz.gmys@elastic.co>
2024-01-08 10:16:29 +0100

    pr change requests

--- x-pack/test/security_solution_cypress/cypress/e2e/investigations/alerts/alerts_cell_actions.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/investigations/alerts/alerts_cell_actions.cy.ts
@@ -23,6 +23,7 @@ import {
   clickExpandActions,
   filterOutAlertProperty,
 } from '../../../tasks/alerts';
+import { deleteAlertsAndRules } from '../../../tasks/api_calls/common';
 import { createRule } from '../../../tasks/api_calls/rules';
 import { waitForAlertsToPopulate } from '../../../tasks/create_new_rule';
 import { login } from '../../../tasks/login';
@@ -39,113 +40,102 @@ import { ALERTS_URL } from '../../../urls/navigation';
 
 describe('Alerts cell actions', { tags: ['@ess', '@serverless'] }, () => {
   beforeEach(() => {
+    deleteAlertsAndRules();
     createRule(getNewRule());
     login();
     visit(ALERTS_URL);
     waitForAlertsToPopulate();
   });
 
-  describe('Filter', () => {
-    it('should filter for and out', () => {
-      cy.log('should work for a non-empty property');
-      cy.get(ALERT_TABLE_SEVERITY_VALUES)
-        .first()
-        .invoke('text')
-        .then((severityVal) => {
-          scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
-          filterForAlertProperty(ALERT_TABLE_SEVERITY_VALUES, 0);
-          cy.get(FILTER_BADGE).first().should('have.text', `kibana.alert.severity: ${severityVal}`);
-        });
+  it('should filter for and out', () => {
+    cy.log('should work for a non-empty property');
+    cy.get(ALERT_TABLE_SEVERITY_VALUES)
+      .first()
+      .invoke('text')
+      .then((severityVal) => {
+        scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
+        filterForAlertProperty(ALERT_TABLE_SEVERITY_VALUES, 0);
+        cy.get(FILTER_BADGE).first().should('have.text', `kibana.alert.severity: ${severityVal}`);
+      });
 
-      removeKqlFilter();
+    removeKqlFilter();
 
-      cy.log('should work for empty properties');
-      // add query condition to make sure the field is empty
-      fillKqlQueryBar('not file.name: *{enter}');
+    cy.log('should work for empty properties');
+    // add query condition to make sure the field is empty
+    fillKqlQueryBar('not file.name: *{enter}');
 
-      scrollAlertTableColumnIntoView(ALERT_TABLE_FILE_NAME_HEADER);
+    scrollAlertTableColumnIntoView(ALERT_TABLE_FILE_NAME_HEADER);
 
-      cy.log('filter for alert property');
+    cy.log('filter for alert property');
 
-      filterForAlertProperty(ALERT_TABLE_FILE_NAME_VALUES, 0);
+    filterForAlertProperty(ALERT_TABLE_FILE_NAME_VALUES, 0);
 
-      cy.get(FILTER_BADGE).first().should('have.text', 'NOT file.name: exists');
+    cy.get(FILTER_BADGE).first().should('have.text', 'NOT file.name: exists');
 
-      cy.log('filter out alert property');
+    cy.log('filter out alert property');
 
-      filterOutAlertProperty(ALERT_TABLE_FILE_NAME_VALUES, 0);
+    filterOutAlertProperty(ALERT_TABLE_FILE_NAME_VALUES, 0);
 
-      cy.get(FILTER_BADGE).first().should('have.text', 'file.name: exists');
+    cy.get(FILTER_BADGE).first().should('have.text', 'file.name: exists');
 
-      removeKqlFilter();
-      scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
+    removeKqlFilter();
+    scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
 
-      cy.log('should allow copy paste');
+    cy.log('should allow copy paste');
 
-      cy.get(ALERT_TABLE_SEVERITY_VALUES)
-        .first()
-        .invoke('text')
-        .then(() => {
-          scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
-          cy.window().then((win) => {
-            cy.stub(win, 'prompt').returns('DISABLED WINDOW PROMPT');
-          });
-          clickExpandActions(ALERT_TABLE_SEVERITY_VALUES, 0);
-          cy.get(CELL_COPY_BUTTON).should('exist');
-          // We are not able to test the "copy to clipboard" action execution
-          // due to browsers security limitation accessing the clipboard services.
-          // We assume external `copy` library works
-        });
-
-      cy.log('should filter out a non-empty property');
-
-      cy.get(ALERT_TABLE_SEVERITY_VALUES)
-        .first()
-        .invoke('text')
-        .then((severityVal) => {
-          scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
-          filterOutAlertProperty(ALERT_TABLE_SEVERITY_VALUES, 0);
-          cy.get(FILTER_BADGE)
-            .first()
-            .should('have.text', `NOT kibana.alert.severity: ${severityVal}`);
-        });
+    cy.window().then((win) => {
+      cy.stub(win, 'prompt').returns('DISABLED WINDOW PROMPT');
     });
+    clickExpandActions(ALERT_TABLE_SEVERITY_VALUES, 0);
+    // We are not able to test the "copy to clipboard" action execution
+    // due to browsers security limitation accessing the clipboard services.
+    // We assume external `copy` library works
+    cy.get(CELL_COPY_BUTTON).should('exist');
+
+    cy.log('should filter out a non-empty property');
+
+    cy.get(ALERT_TABLE_SEVERITY_VALUES)
+      .first()
+      .invoke('text')
+      .then((severityVal) => {
+        scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
+        filterOutAlertProperty(ALERT_TABLE_SEVERITY_VALUES, 0);
+        cy.get(FILTER_BADGE)
+          .first()
+          .should('have.text', `NOT kibana.alert.severity: ${severityVal}`);
+      });
   });
 
-  describe('Add to timeline', () => {
-    it('should add a non-empty property to default timeline', () => {
-      cy.get(ALERT_TABLE_SEVERITY_VALUES)
-        .first()
-        .invoke('text')
-        .then((severityVal) => {
-          scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
-          addAlertPropertyToTimeline(ALERT_TABLE_SEVERITY_VALUES, 0);
-          openActiveTimeline();
-          cy.get(PROVIDER_BADGE)
-            .first()
-            .should('have.text', `kibana.alert.severity: "${severityVal}"`);
-        });
-    });
-
-    it('should add an empty property to default timeline', () => {
-      // add condition to make sure the field is empty
-      openAddFilterPopover();
-
-      fillAddFilterForm({ key: 'file.name', operator: 'does not exist' });
-      scrollAlertTableColumnIntoView(ALERT_TABLE_FILE_NAME_HEADER);
-      addAlertPropertyToTimeline(ALERT_TABLE_FILE_NAME_VALUES, 0);
-      openActiveTimeline();
-      cy.get(PROVIDER_BADGE).first().should('have.text', 'NOT file.name exists');
-    });
+  it('should add a non-empty property to default timeline', () => {
+    cy.get(ALERT_TABLE_SEVERITY_VALUES)
+      .first()
+      .invoke('text')
+      .then((severityVal) => {
+        scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
+        addAlertPropertyToTimeline(ALERT_TABLE_SEVERITY_VALUES, 0);
+        openActiveTimeline();
+        cy.get(PROVIDER_BADGE)
+          .first()
+          .should('have.text', `kibana.alert.severity: "${severityVal}"`);
+      });
   });
 
-  describe('Show Top N', () => {
-    it('should show top for a property', () => {
-      cy.get(ALERT_TABLE_SEVERITY_VALUES);
+  it('should add an empty property to default timeline', () => {
+    // add condition to make sure the field is empty
+    openAddFilterPopover();
 
-      scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
-      showTopNAlertProperty(ALERT_TABLE_SEVERITY_VALUES, 0);
-      cy.get(SHOW_TOP_N_HEADER).first().should('have.text', `Top kibana.alert.severity`);
-    });
+    fillAddFilterForm({ key: 'file.name', operator: 'does not exist' });
+    scrollAlertTableColumnIntoView(ALERT_TABLE_FILE_NAME_HEADER);
+    addAlertPropertyToTimeline(ALERT_TABLE_FILE_NAME_VALUES, 0);
+    openActiveTimeline();
+    cy.get(PROVIDER_BADGE).first().should('have.text', 'NOT file.name exists');
+  });
+
+  it('should show top for a property', () => {
+    cy.get(ALERT_TABLE_SEVERITY_VALUES);
+
+    scrollAlertTableColumnIntoView(ALERT_TABLE_SEVERITY_HEADER);
+    showTopNAlertProperty(ALERT_TABLE_SEVERITY_VALUES, 0);
+    cy.get(SHOW_TOP_N_HEADER).first().should('have.text', `Top kibana.alert.severity`);
   });
 });
