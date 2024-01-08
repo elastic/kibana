483f190adc89b1fab0bdc728ee2a080efa8fe0c8
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-01-08 14:08:21 +0000

    [Security Solution][Detection Engine][Feature branch] adds Cypress tests fro IM suppression (#174344)
    ## Summary
    
    Adds tests by test plan
    
    **Scenario: Create rule with per rule execution suppression**
    
    Given rule create page
    When user select rule type
    And user adds suppress fields
    And user selects on rule execution suppression only
    And user saves rule
    Then on rule details page suppress by fields should be displayed
    And suppression on rule execution should be rendered
    
    **Scenario: Create rule with time interval suppression**
    
    Given rule create page
    When user select rule type
    And user adds suppress fields
    And user selects time interval suppression option
    And user selects do not suppress missing fields
    And user saves rule
    Then on rule details page suppress by fields should be displayed
    And time interval suppression should be displayed
    
    **Scenario: Edit rule with suppression**
    
    Given rule configured with suppression on rule interval
    When user edits rule
    And changes suppression from time interval to rule execution suppression
    Then on rule details page suppress by fields should be displayed
    And per rule execution suppression should be displayed
    
    **Scenario: Edit rule without suppression**
    
    Given rule without suppression
    When user edits rule
    And user adds suppress fields
    And user selects time interval suppression option
    And user saves rule
    Then on rule details page suppress by fields should be displayed
    And time interval suppression should be displayed
    
    **Scenario: Rule details with suppression**
    
    Given rule configured with suppression time interval
    When user views rule details page
    Then on rule details page suppress by fields should be displayed
    And time interval suppression should be displayed
    
    ### **License tests**
    
    **Scenario: Create rule with rule execution suppression on basic license
    (ESS only)**
    
    Given rule create page
    When user select rule type
    Then user sees suppression options disabled
    And upselling message displayed
    
    **Scenario: Create rule with rule execution suppression on Essentials
    tier (Serverless only)**
    
    Given rule create page
    When user select rule type
    And user adds suppress fields
    And user selects on rule execution suppression only
    And user saves rule
    Then on rule details page suppress by fields should be displayed
    And suppression on rule execution should be rendered
    
    **Scenario: Rule details with suppression on basic license  (ESS only)**
    
    Given rule configured with suppression time interval
    When user views rule details page
    Then on rule details page suppress by fields should be displayed
    And time interval suppression should be displayed
    And upselling warning that suppression is not applied should be rendered
    on details labels

--- x-pack/plugins/security_solution/public/detections/components/rules/description_step/index.tsx
+++ x-pack/plugins/security_solution/public/detections/components/rules/description_step/index.tsx
@@ -56,7 +56,12 @@ import { THREAT_QUERY_LABEL } from './translations';
 import { filterEmptyThreats } from '../../../../detection_engine/rule_creation_ui/pages/rule_creation/helpers';
 import { useLicense } from '../../../../common/hooks/use_license';
 import type { LicenseService } from '../../../../../common/license';
-import { isThresholdRule, isQueryRule } from '../../../../../common/detection_engine/utils';
+import {
+  isThresholdRule,
+  isQueryRule,
+  isThreatMatchRule,
+  isEqlRule,
+} from '../../../../../common/detection_engine/utils';
 
 const DescriptionListContainer = styled(EuiDescriptionList)`
   max-width: 600px;
@@ -206,7 +211,8 @@ export const getDescriptionItem = (
     return [];
   } else if (field === 'groupByFields') {
     const ruleType: Type = get('ruleType', data);
-    const ruleCanHaveGroupByFields = isQueryRule(ruleType);
+    const ruleCanHaveGroupByFields =
+      isQueryRule(ruleType) || isThreatMatchRule(ruleType) || isEqlRule(ruleType);
     if (!ruleCanHaveGroupByFields) {
       return [];
     }
@@ -216,7 +222,11 @@ export const getDescriptionItem = (
     return [];
   } else if (field === 'groupByDuration') {
     const ruleType: Type = get('ruleType', data);
-    const ruleCanHaveDuration = isQueryRule(ruleType) || isThresholdRule(ruleType);
+    const ruleCanHaveDuration =
+      isQueryRule(ruleType) ||
+      isThresholdRule(ruleType) ||
+      isThreatMatchRule(ruleType) ||
+      isEqlRule(ruleType);
     if (!ruleCanHaveDuration) {
       return [];
     }
@@ -239,7 +249,8 @@ export const getDescriptionItem = (
     }
   } else if (field === 'suppressionMissingFields') {
     const ruleType: Type = get('ruleType', data);
-    const ruleCanHaveSuppressionMissingFields = isQueryRule(ruleType);
+    const ruleCanHaveSuppressionMissingFields =
+      isQueryRule(ruleType) || isThreatMatchRule(ruleType) || isEqlRule(ruleType);
     if (!ruleCanHaveSuppressionMissingFields) {
       return [];
     }
